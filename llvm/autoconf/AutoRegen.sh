5ebd8c9a1c14e7d5436c8089938143e0dd949af1
Bill Wendling <isanbard@gmail.com>
2011-10-16 02:52:56 +0000

    Update to compile with assertions turned off. Say that optimizations are turned on by default.
    llvm-svn: 142099

--- llvm/autoconf/AutoRegen.sh
+++ llvm/autoconf/AutoRegen.sh
@@ -14,9 +14,9 @@ clean() {
 ### Periods should be escaped with backslash for use by grep.
 ###
 ### If you update these, please also update docs/GettingStarted.html
-want_autoconf_version='2\.60'
+want_autoconf_version='2\.61'
 want_autoheader_version=$want_autoconf_version
-want_aclocal_version='1\.9\.6'
+want_aclocal_version='1\.10'
 want_libtool_version='1\.5\.22'
 ### END NOTE #########################################################
 
