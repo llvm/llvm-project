9a2ea3e251cf8fc337d5362d24490ad7105b5ff7
Tom Stellard <thomas.stellard@amd.com>
2014-04-08 19:16:08 +0000

    Merging r197577:
    ------------------------------------------------------------------------
    r197577 | rafael.espindola | 2013-12-18 10:16:50 -0500 (Wed, 18 Dec 2013) | 4 lines
    
    Add -f64:32:64 to the darwin ppc32 DataLayout.
    
    A f64 inside a struct can be 32 bit aligned on darwin.
    
    ------------------------------------------------------------------------
    
    llvm-svn: 205788

--- clang/lib/Basic/Targets.cpp
+++ clang/lib/Basic/Targets.cpp
@@ -1271,7 +1271,7 @@ public:
     LongLongAlign = 32;
     SuitableAlign = 128;
     DescriptionString = "E-p:32:32:32-i1:8:8-i8:8:8-i16:16:16-i32:32:32-"
-                        "i64:32:64-f32:32:32-f64:64:64-v128:128:128-n32";
+                        "i64:32:64-f32:32:32-f64:32:64-v128:128:128-n32";
   }
   virtual BuiltinVaListKind getBuiltinVaListKind() const {
     return TargetInfo::CharPtrBuiltinVaList;
