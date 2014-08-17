fcd989333f4031cf03dc992385d6ce5963f53a7a
Sylvestre Ledru <sylvestre@debian.org>
2014-08-17 11:59:37 +0000

    Fix the build. Match the declaration in trunk. Use clang::diag::Flavor::WarningOrError
    llvm-svn: 215833

--- lldb/source/Expression/ClangExpressionParser.cpp
+++ lldb/source/Expression/ClangExpressionParser.cpp
@@ -230,12 +230,12 @@ ClangExpressionParser::ClangExpressionParser (ExecutionContextScope *exe_scope,
         m_compiler->getCodeGenOpts().setDebugInfo(CodeGenOptions::NoDebugInfo);
 
     // Disable some warnings.
-    m_compiler->getDiagnostics().setSeverityForGroup(clang::diag::Flavor::WarningOrError,
-        diag::Flavor::Remark, "unused-value", clang::diag::Severity::Ignored,
-        SourceLocation());
-    m_compiler->getDiagnostics().setSeverityForGroup(clang::diag::Flavor::WarningOrError,
-        diag::Flavor::Remark, "odr", clang::diag::Severity::Ignored,
-        SourceLocation());
+    m_compiler->getDiagnostics().setSeverityForGroup(
+        clang::diag::Flavor::WarningOrError, "unused-value",
+        clang::diag::Severity::Ignored, SourceLocation());
+    m_compiler->getDiagnostics().setSeverityForGroup(
+        clang::diag::Flavor::WarningOrError, "odr",
+        clang::diag::Severity::Ignored, SourceLocation());
 
     // Inform the target of the language options
     //
