4e531b89599316c5be43d375ed9ea46f230d0e22
Todd Fiala <tfiala@apple.com>
2015-11-20 07:54:40 -0800

    Change OS X lldb version line in OSS to include lldb, llvm, clang and swift repo revision numbers.
    Take 3.  This one fixes a race condition in the build by ensuring
    the output directory exists before trying to place something there.
    
    apple-llvm-split-commit: c7ed056755c7edcaf93ea775242303b2b0a9e68e
    apple-llvm-split-dir: lldb/

--- lldb/source/lldb.cpp
+++ lldb/source/lldb.cpp
@@ -13,8 +13,23 @@ using namespace lldb;
 using namespace lldb_private;
 
 #if defined (__APPLE__)
+#include "lldb_revision.h"
 extern "C" const unsigned char liblldb_coreVersionString[];
-#else
+#endif
+
+// The following flag should be 1 ONLY for the Open Source Swift
+// version of LLDB.  Everything else should set this to 0.
+#define LLDB_IS_OSS_VERSION 1
+
+#if LLDB_IS_OSS_VERSION
+
+#include "llvm/Support/raw_ostream.h"
+#include "swift/Basic/Version.h"
+
+#endif
+
+
+#if !defined (__APPLE__) || LLDB_IS_OSS_VERSION
 
 #include "clang/Basic/Version.h"
 
@@ -28,6 +43,10 @@ GetLLDBRevision()
 #endif
 }
 
+#endif
+
+#if !defined (__APPLE__)
+
 static const char *
 GetLLDBRepository()
 {
@@ -36,6 +55,29 @@ GetLLDBRepository()
 #else
     return NULL;
 #endif
+
+}
+
+#endif
+
+#if LLDB_IS_OSS_VERSION
+
+// TODO remove this function once swift revision is directly exposed.
+std::string ExtractSwiftRevision(const std::string &fullVersion)
+{
+    // Find spot right before Swift revision.
+    const std::string search_prefix = "Swift ";
+    const size_t prefix_start_pos = fullVersion.rfind(search_prefix);
+    if (prefix_start_pos == std::string::npos)
+        return "";
+
+    // Find spot where Swift revision ends.
+    const size_t revision_end_pos = fullVersion.rfind(')');
+    if (revision_end_pos == std::string::npos)
+        return "";
+
+    // Return the revision.
+    return fullVersion.substr(prefix_start_pos + search_prefix.length(), revision_end_pos - prefix_start_pos - search_prefix.length());
 }
 
 #endif
@@ -44,6 +86,41 @@ const char *
 lldb_private::GetVersion ()
 {
 #if defined (__APPLE__)
+#if LLDB_IS_OSS_VERSION
+    static std::string g_version_str;
+    if (g_version_str.empty())
+    {
+        llvm::raw_string_ostream out(g_version_str);
+
+        out << "lldb (";
+        std::string lldb_revision = GetLLDBRevision();
+        if (lldb_revision.length() > 0)
+        {
+            const size_t MAX_REVISION_LENGTH = 10;
+
+            out << "LLDB ";
+            out << lldb_revision.substr(0, MAX_REVISION_LENGTH);
+
+            const std::string llvm_revision = clang::getLLVMRevision();
+            if (!llvm_revision.empty())
+                out << ", LLVM " << llvm_revision.substr(0, MAX_REVISION_LENGTH);
+
+            const std::string clang_revision = clang::getClangRevision();
+            if (!clang_revision.empty())
+                out << ", Clang " << clang_revision.substr(0, MAX_REVISION_LENGTH);
+
+            // TODO replace this with a swift::version::GetSwiftRevision() call
+            // once added.
+            const std::string swift_revision = ExtractSwiftRevision(swift::version::getSwiftFullVersion());
+            if (!swift_revision.empty())
+                out << ", Swift " << swift_revision.substr(0, MAX_REVISION_LENGTH);
+        }
+        out << ")";
+
+        g_version_str = out.str();
+    }
+   return g_version_str.c_str();
+#else
     static char g_version_string[32];
     if (g_version_string[0] == '\0')
     {
@@ -66,6 +143,7 @@ lldb_private::GetVersion ()
     }
 
     return g_version_string;
+#endif
 #else
     // On platforms other than Darwin, report a version number in the same style as the clang tool.
     static std::string g_version_str;
