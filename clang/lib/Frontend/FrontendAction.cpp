47d6f5037d063859b468cba3bb2ea2d4219048f5
Argyrios Kyrtzidis <akyrtzi@gmail.com>
2016-02-07 19:28:36 +0000

    [Frontend] Make the memory management of FrontendAction pointers explicit by using unique_ptr.
    git-svn-id: https://llvm.org/svn/llvm-project/cfe/trunk@260048 91177308-0d34-0410-b5e6-96231b3b80d8
    apple-llvm-split-commit: 48986a79be89bdc25d202a621feca2e287ea117d
    apple-llvm-split-dir: clang/

--- clang/lib/Frontend/FrontendAction.cpp
+++ clang/lib/Frontend/FrontendAction.cpp
@@ -594,6 +594,7 @@ bool WrapperFrontendAction::hasCodeCompletionSupport() const {
   return WrappedAction->hasCodeCompletionSupport();
 }
 
-WrapperFrontendAction::WrapperFrontendAction(FrontendAction *WrappedAction)
-  : WrappedAction(WrappedAction) {}
+WrapperFrontendAction::WrapperFrontendAction(
+    std::unique_ptr<FrontendAction> WrappedAction)
+  : WrappedAction(std::move(WrappedAction)) {}
 
