1ca560d215075568a981653aec1553da7a8568a0
Vedant Kumar <vsk@apple.com>
2016-02-15 15:12:46 -0800

    Revert "Fix undefined behavior when compiling in C++14 due to sized operator delete"
    This reverts commit d9809a260be59170ca45a6cabd30a41c97777ea2.
    
    Due to error: no member named 'operator delete' in
    'llvm::TrailingObjects<clang::CodeGen::CGFunctionInfo,
    clang::CodeGen::CGFunctionInfoArgInfo>'
    
    apple-llvm-split-commit: dfc34861a6ef4ff117b0cc0a12247e3830bf03d9
    apple-llvm-split-dir: clang/

--- clang/include/clang/CodeGen/CGFunctionInfo.h
+++ clang/include/clang/CodeGen/CGFunctionInfo.h
@@ -20,7 +20,6 @@
 #include "clang/AST/CharUnits.h"
 #include "clang/AST/Type.h"
 #include "llvm/ADT/FoldingSet.h"
-#include "llvm/Support/TrailingObjects.h"
 #include <cassert>
 
 namespace llvm {
@@ -332,19 +331,13 @@ public:
   }
 };
 
-// Implementation detail of CGFunctionInfo, factored out so it can be named
-// in the TrailingObjects base class of CGFunctionInfo.
-struct CGFunctionInfoArgInfo {
-  CanQualType type;
-  ABIArgInfo info;
-};
-
 /// CGFunctionInfo - Class to encapsulate the information about a
 /// function definition.
-class CGFunctionInfo final
-    : public llvm::FoldingSetNode,
-      private llvm::TrailingObjects<CGFunctionInfo, CGFunctionInfoArgInfo> {
-  typedef CGFunctionInfoArgInfo ArgInfo;
+class CGFunctionInfo : public llvm::FoldingSetNode {
+  struct ArgInfo {
+    CanQualType type;
+    ABIArgInfo info;
+  };
 
   /// The LLVM::CallingConv to use for this function (as specified by the
   /// user).
@@ -381,16 +374,12 @@ class CGFunctionInfo final
   unsigned ArgStructAlign;
 
   unsigned NumArgs;
-
   ArgInfo *getArgsBuffer() {
-    return getTrailingObjects<ArgInfo>();
+    return reinterpret_cast<ArgInfo*>(this+1);
   }
   const ArgInfo *getArgsBuffer() const {
-    return getTrailingObjects<ArgInfo>();
+    return reinterpret_cast<const ArgInfo*>(this + 1);
   }
-
-  size_t numTrailingObjects(OverloadToken<ArgInfo>) { return NumArgs + 1; }
-  friend class TrailingObjects;
 
   CGFunctionInfo() : Required(RequiredArgs::All) {}
 
@@ -402,7 +391,6 @@ public:
                                 CanQualType resultType,
                                 ArrayRef<CanQualType> argTypes,
                                 RequiredArgs required);
-  void operator delete(void *p) { TrailingObjects::operator delete(p); }
 
   typedef const ArgInfo *const_arg_iterator;
   typedef ArgInfo *arg_iterator;
