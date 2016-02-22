b7ffa1130eb55857c4ba4ff3557ed9b91ce005a9
Adam Nemet <anemet@apple.com>
2016-02-22 21:41:22 +0000

    [LoopDataPrefetch] Make it testable with opt
    Summary:
    Since this is an IR pass it's nice to be able to write tests without
    llc.  This is the counterpart of the llc test under
    CodeGen/PowerPC/loop-data-prefetch.ll.
    
    Reviewers: hfinkel
    
    Subscribers: llvm-commits
    
    Differential Revision: http://reviews.llvm.org/D17464
    
    git-svn-id: https://llvm.org/svn/llvm-project/llvm/trunk@261578 91177308-0d34-0410-b5e6-96231b3b80d8
    (cherry picked from commit fddbd94e1db4ab45eff7bbb1b1f7e63b5896b41b)
    
    apple-llvm-split-commit: 5fa5defe6c662e230df96b5ae24d4d4c978e6a2d
    apple-llvm-split-dir: llvm/

--- llvm/include/llvm/InitializePasses.h
+++ llvm/include/llvm/InitializePasses.h
@@ -163,6 +163,8 @@ void initializeLiveStacksPass(PassRegistry&);
 void initializeLiveVariablesPass(PassRegistry&);
 void initializeLoaderPassPass(PassRegistry&);
 void initializeLocalStackSlotPassPass(PassRegistry&);
+void initializeLoopDataPrefetchPass(PassRegistry&);
+void initializeLoopPassPass(PassRegistry&);
 void initializeLoopDeletionPass(PassRegistry&);
 void initializeLoopExtractorPass(PassRegistry&);
 void initializeLoopInfoWrapperPassPass(PassRegistry&);
