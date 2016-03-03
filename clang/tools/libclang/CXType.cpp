14e05706d839e656743776a4251e1e0425e35d9c
John McCall <rjmccall@apple.com>
2016-03-03 06:39:32 +0000

    Semantic analysis for the swiftcall calling convention.
    I've tried to keep the infrastructure behind parameter ABI
    treatments fairly general.
    
    git-svn-id: https://llvm.org/svn/llvm-project/cfe/trunk@262587 91177308-0d34-0410-b5e6-96231b3b80d8
    apple-llvm-split-commit: 88547d6b0837f8e376ed434fe65d2b09caaabb5a
    apple-llvm-split-dir: clang/

--- clang/tools/libclang/CXType.cpp
+++ clang/tools/libclang/CXType.cpp
@@ -533,6 +533,7 @@ CXCallingConv clang_getFunctionTypeCallingConv(CXType X) {
       TCALLINGCONV(AAPCS);
       TCALLINGCONV(AAPCS_VFP);
       TCALLINGCONV(IntelOclBicc);
+      TCALLINGCONV(Swift);
       TCALLINGCONV(PreserveMost);
       TCALLINGCONV(PreserveAll);
     case CC_SpirFunction: return CXCallingConv_Unexposed;
