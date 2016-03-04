9163f8aa7875d8b2e460840c611909eea7c59712
Greg Clayton <gclayton@apple.com>
2016-03-03 16:25:19 -0800

    Added a test case to make sure we don't crash when we have variables that use private typealias types from within structs (see radar 23380933). Also make sure that we wait for radar 24921067 to be fixed which tracks making sure we can have variables whose types use private typealias types and we can see them once the compiler allows us to access these private types in the modules they give to the debugger.
    <rdar://problem/23380933>
    
    apple-llvm-split-commit: d983a4388342d9b4a12e181e3f5cf20cdcb7ba4d
    apple-llvm-split-dir: lldb/

---
+++ lldb/packages/Python/lldbsuite/test/lang/swift/private_typealias/main.swift
@@ -0,0 +1,34 @@
+// main.swift
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2015 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+// -----------------------------------------------------------------------------
+public struct MyStruct {
+    private typealias IntegerType = Int
+    private var m_integer = IntegerType(345)
+    private func Foo(i : IntegerType)
+    {
+        print("i = \(i)") // breakpoint 1
+    }
+    
+    private func Bar()
+    {
+        let a : Dictionary<String, IntegerType> = [ "hello" : 234 ]
+        print("a = \(a)") // breakpoint 2
+    }
+    
+}
+
+func main() {
+    let s = MyStruct()
+    s.Foo(123)
+    s.Bar()
+}
+
+main()
