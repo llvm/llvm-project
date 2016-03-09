ecdde3fd057e2ca8c1e40fecfbd777b346967f2e
Manuel Klimek <klimek@google.com>
2016-03-09 10:06:45 +0000

    Pacify gcc's parenthesis warning, which doesn't realize that parens don't matter here.
    git-svn-id: https://llvm.org/svn/llvm-project/cfe/trunk@263004 91177308-0d34-0410-b5e6-96231b3b80d8
    apple-llvm-split-commit: ceda566233f352cbf75b1ebc5d94ceb4c5552db0
    apple-llvm-split-dir: clang/

--- clang/lib/Index/IndexingContext.cpp
+++ clang/lib/Index/IndexingContext.cpp
@@ -296,9 +296,8 @@ bool IndexingContext::handleDeclOccurrence(const Decl *D, SourceLocation Loc,
   Parent = adjustParent(Parent);
   if (Parent)
     Parent = getCanonicalDecl(Parent);
-  assert(!Parent || !Parent->isImplicit() ||
-         isa<FunctionDecl>(Parent) ||
-         isa<ObjCInterfaceDecl>(Parent) || isa<ObjCMethodDecl>(Parent) &&
+  assert((!Parent || !Parent->isImplicit() || isa<FunctionDecl>(Parent) ||
+          isa<ObjCInterfaceDecl>(Parent) || isa<ObjCMethodDecl>(Parent)) &&
          "unexpected implicit parent!");
 
   SmallVector<SymbolRelation, 6> FinalRelations;
