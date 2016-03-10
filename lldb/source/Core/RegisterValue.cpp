84256f9ade215ef4d1e24c5b95d02d19710d34e6
Enrico Granata <egranata@apple.com>
2016-03-10 00:14:29 +0000

    Certain hardware architectures have registers of 256 bits in size
    This patch extends Scalar such that it can support data living in such registers (e.g. float values living in the XMM registers)
    
    git-svn-id: https://llvm.org/svn/llvm-project/lldb/trunk@263079 91177308-0d34-0410-b5e6-96231b3b80d8
    apple-llvm-split-commit: b62124d4542b534eba0a8f82e8b1a8de15fc8c6e
    apple-llvm-split-dir: lldb/

--- lldb/source/Core/RegisterValue.cpp
+++ lldb/source/Core/RegisterValue.cpp
@@ -242,14 +242,24 @@ RegisterValue::GetScalarValue (Scalar &scalar) const
         {
             switch (buffer.length)
             {
-            default:    break;
-            case  1:     scalar = *(const uint8_t *)buffer.bytes; return true;
-            case  2:     scalar = *(const uint16_t *)buffer.bytes; return true;
-            case  4:     scalar = *(const uint32_t *)buffer.bytes; return true;
-            case  8:     scalar = *(const uint64_t *)buffer.bytes; return true;
-            case 16:
-              scalar = llvm::APInt(128, llvm::ArrayRef<uint64_t>((const uint64_t *)buffer.bytes, 2));
-              return true;
+                switch (buffer.length)
+                {
+                default:    break;
+                case 1:     scalar = *(const uint8_t *)buffer.bytes; return true;
+                case 2:     scalar = *(const uint16_t *)buffer.bytes; return true;
+                case 4:     scalar = *(const uint32_t *)buffer.bytes; return true;
+                case 8:     scalar = *(const uint64_t *)buffer.bytes; return true;
+                case 16:
+                case 32:
+                            if (buffer.length % sizeof(uint64_t) == 0)
+                            {
+                                const auto length_in_bits = buffer.length * 8;
+                                const auto length_in_uint64 = buffer.length / sizeof(uint64_t);
+                                scalar = llvm::APInt(length_in_bits, llvm::ArrayRef<uint64_t>((const uint64_t *)buffer.bytes, length_in_uint64));
+                                return true;
+                            }
+                            break;
+                }
             }
         }
         case eTypeUInt8:
