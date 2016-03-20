bea3ff044e0779ea4b31a692b632d2682e65e3e4
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2016-03-20 02:08:48 +0000

    [VFS] Add 'overlay-relative' field to YAML files
    This reapplies r261552 and r263748. Fixed testcase to reapply.
    
    The VFS overlay mapping between virtual paths and real paths is done through
    the 'external-contents' entries in YAML files, which contains hardcoded paths
    to the real files.
    
    When a module compilation crashes, headers are dumped into <name>.cache/vfs
    directory and are mapped via the <name>.cache/vfs/vfs.yaml. The script
    generated for reproduction uses -ivfsoverlay pointing to file to gather the
    mapping between virtual paths and files inside <name>.cache/vfs. Currently, we
    are only capable of reproducing such crashes in the same machine as they
    happen, because of the hardcoded paths in 'external-contents'.
    
    To be able to reproduce a crash in another machine, this patch introduces a new
    option in the VFS yaml file called 'overlay-relative'. When it's equal to
    'true' it means that the provided path to the YAML file through the
    -ivfsoverlay option should also be used to prefix the final path for every
    'external-contents'.
    
    Example, given the invocation snippet "... -ivfsoverlay
    <name>.cache/vfs/vfs.yaml" and the following entry in the yaml file:
    
    "overlay-relative": "true",
    "roots": [
    ...
      "type": "directory",
      "name": "/usr/include",
      "contents": [
        {
          "type": "file",
          "name": "stdio.h",
          "external-contents": "/usr/include/stdio.h"
        },
    ...
    
    Here, a file manager request for virtual "/usr/include/stdio.h", that will map
    into real path "/<absolute_path_to>/<name>.cache/vfs/usr/include/stdio.h.
    
    This is a useful feature for debugging module crashes in machines other than
    the one where the error happened.
    
    Differential Revision: http://reviews.llvm.org/D17457
    
    rdar://problem/24499339
    
    git-svn-id: https://llvm.org/svn/llvm-project/cfe/trunk@263893 91177308-0d34-0410-b5e6-96231b3b80d8
    (cherry picked from commit 86240eff14fb1ddf5bd68cca062f7048b3c04847)
    git-svn-id: https://llvm.org/svn/llvm-project/cfe/trunk@263912 91177308-0d34-0410-b5e6-96231b3b80d8
    (cherry picked from commit 26d94682bce93b81ae375874df73881438b8dfd8)
    
    apple-llvm-split-commit: d99463d129f329ddc71de1424a62948c90a5f6f8
    apple-llvm-split-dir: clang/

--- clang/test/Modules/crash-vfs-relative-overlay.m
+++ clang/test/Modules/crash-vfs-relative-overlay.m
@@ -1,7 +1,5 @@
-// REQUIRES: crash-recovery, shell, non-ms-sdk, non-ps4-sdk
+// REQUIRES: crash-recovery, shell
 
-// FIXME: Canonicalizing paths to remove relative traversal components
-// currenty fails a unittest on windows and is disable by default.
 // FIXME: This XFAIL is cargo-culted from crash-report.c. Do we need it?
 // XFAIL: mingw32
 
@@ -19,7 +17,7 @@
 // RUN: find %t/crash-vfs-*.cache/vfs | \
 // RUN:   grep "Inputs/crash-recovery/usr/include/stdio.h" | count 1
 
-#include "usr/././//////include/../include/./././../include/stdio.h"
+#include "usr/include/stdio.h"
 
 // CHECK: Preprocessed source(s) and associated run script(s) are located at:
 // CHECK-NEXT: note: diagnostic msg: {{.*}}.m
@@ -36,22 +34,19 @@
 // CHECKSH: "crash-vfs-{{[^ ]*}}.m"
 // CHECKSH: "-ivfsoverlay" "crash-vfs-{{[^ ]*}}.cache/vfs/vfs.yaml"
 
-// CHECKYAML:     'type': 'directory'
-// CHECKYAML:     'name': "/[[PATH:.*]]/Inputs/crash-recovery/usr/include",
+// CHECKYAML: 'type': 'directory'
+// CHECKYAML: 'name': "/[[PATH:.*]]/Inputs/crash-recovery/usr/include",
 // CHECKYAML-NEXT: 'contents': [
 // CHECKYAML-NEXT:   {
 // CHECKYAML-NEXT:     'type': 'file',
 // CHECKYAML-NEXT:     'name': "module.map",
+// CHECKYAML-NOT:      'external-contents': "{{[^ ]*}}.cache
 // CHECKYAML-NEXT:     'external-contents': "/[[PATH]]/Inputs/crash-recovery/usr/include/module.map"
 // CHECKYAML-NEXT:   },
 
-// Replace the paths in the YAML files with relative ".." traversals
-// and fed into clang to test whether we're correctly representing them
-// in the VFS overlay.
+// Test that reading the YAML file will yield the correct path after
+// the overlay dir is prefixed to access headers in .cache/vfs directory.
 
-// RUN: sed -e "s@usr/include@usr/include/../include@g" \
-// RUN:     %t/crash-vfs-*.cache/vfs/vfs.yaml > %t/vfs.yaml
-// RUN: cp %t/vfs.yaml %t/crash-vfs-*.cache/vfs/vfs.yaml
 // RUN: unset FORCE_CLANG_DIAGNOSTICS_CRASH
 // RUN: %clang -E %s -I %S/Inputs/crash-recovery -isysroot %/t/i/ \
 // RUN:     -ivfsoverlay %t/crash-vfs-*.cache/vfs/vfs.yaml -fmodules \
