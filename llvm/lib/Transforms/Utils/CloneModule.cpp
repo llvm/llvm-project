d7669af9357b17e9a741ba9e6d4a44b87c5b7017
Peter Collingbourne <peter@pcc.me.uk>
2016-03-30 22:05:13 +0000

    Cloning: Reduce complexity of debug info cloning and fix correctness issue.
    Commit r260791 contained an error in that it would introduce a cross-module
    reference in the old module. It also introduced O(N^2) complexity in the
    module cloner by requiring the entire module to be visited for each function.
    Fix both of these problems by avoiding use of the CloneDebugInfoMetadata
    function (which is only designed to do intra-module cloning) and cloning
    function-attached metadata in the same way that we clone all other metadata.
    
    Differential Revision: http://reviews.llvm.org/D18583
    
    git-svn-id: https://llvm.org/svn/llvm-project/llvm/trunk@264935 91177308-0d34-0410-b5e6-96231b3b80d8
    (cherry picked from commit af289e04413504c3bdc252e08c3fe17bf7ea6dc8)
    
    apple-llvm-split-commit: 6ec704156cf4e2a680e643d5c6184ff6c2e499b9
    apple-llvm-split-dir: llvm/

--- llvm/lib/Transforms/Utils/CloneModule.cpp
+++ llvm/lib/Transforms/Utils/CloneModule.cpp
@@ -136,7 +136,6 @@ std::unique_ptr<Module> llvm::CloneModule(
         VMap[&*J] = &*DestI++;
       }
 
-      CloneDebugInfoMetadata(F, &*I, VMap);
       SmallVector<ReturnInst*, 8> Returns;  // Ignore returns cloned.
       CloneFunctionInto(F, &*I, VMap, /*ModuleLevelChanges=*/true, Returns);
     }
