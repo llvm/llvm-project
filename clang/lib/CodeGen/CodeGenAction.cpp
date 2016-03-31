fa4e4cefb1fa30a4a6669e266d0babc29dd6e5f0
Tim Northover <tnorthover@apple.com>
2016-03-31 19:19:24 +0000

    Diagnostics: remove dodgy handler for bitcode inlineasm diagnostics.
    Whatever crash it was there to present appears to have been fixed in the
    backend now, and it had the nasty side-effect of causing clang to exit(0) and
    leave a .o containing goodness knows what even when an error hit.
    
    git-svn-id: https://llvm.org/svn/llvm-project/cfe/trunk@265038 91177308-0d34-0410-b5e6-96231b3b80d8
    apple-llvm-split-commit: 7af77768488948c942ac75c6eb0ad8d3d7cd177b
    apple-llvm-split-dir: clang/

--- clang/lib/CodeGen/CodeGenAction.cpp
+++ clang/lib/CodeGen/CodeGenAction.cpp
@@ -725,12 +725,6 @@ CodeGenAction::CreateASTConsumer(CompilerInstance &CI, StringRef InFile) {
   return std::move(Result);
 }
 
-static void BitcodeInlineAsmDiagHandler(const llvm::SMDiagnostic &SM,
-                                         void *Context,
-                                         unsigned LocCookie) {
-  SM.print(nullptr, llvm::errs());
-}
-
 void CodeGenAction::ExecuteAction() {
   // If this is an IR file, we have to treat it specially.
   if (getCurrentFileKind() == IK_LLVM_IR) {
@@ -779,8 +773,6 @@ void CodeGenAction::ExecuteAction() {
       TheModule->setTargetTriple(TargetOpts.Triple);
     }
 
-    LLVMContext &Ctx = TheModule->getContext();
-    Ctx.setInlineAsmDiagnosticHandler(BitcodeInlineAsmDiagHandler);
     EmitBackendOutput(CI.getDiagnostics(), CI.getCodeGenOpts(), TargetOpts,
                       CI.getLangOpts(), CI.getTarget().getDataLayoutString(),
                       TheModule.get(), BA, OS);
