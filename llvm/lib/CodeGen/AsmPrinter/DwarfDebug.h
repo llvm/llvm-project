c31922e943c3912afb55cd8b61938adccf071f9e
Adrian Prantl <aprantl@apple.com>
2016-04-09 18:10:22 +0000

    Drop debug info for DISubprograms that are not referenced by anything
    This patch drops the debug info for all DISubprograms that are
    (a) not attached to an llvm::Function and
    (b) not indirectly reachable via inline scopes from any surviving Function and
    (c) not reachable from a type (i.e.: member functions).
    
    Background: I'm currently working on a patch to reverse the pointers
    between DICompileUnit and DISubprogram (for more info check Duncan's RFC
    on lazy-loading of debug info metadata
    http://lists.llvm.org/pipermail/llvm-dev/2016-March/097419.html).
    The idea is to remove the list of subprograms from DICompileUnit and
    instead point to the owning compile unit from each DISubprogram.
    After doing this all DISubprograms fulfilling the above criteria will be
    implicitly dropped unless we go through an extra effort to preserve them.
    
    http://reviews.llvm.org/D18477
    <rdar://problem/25256815>
    
    git-svn-id: https://llvm.org/svn/llvm-project/llvm/trunk@265876 91177308-0d34-0410-b5e6-96231b3b80d8
    (cherry picked from commit 030f43a4dfa3f275859b6e68e85ebdb452b5b68f)
    
    apple-llvm-split-commit: b6cd585ccb330fd843708a068107a7e028755f24
    apple-llvm-split-dir: llvm/

--- llvm/lib/CodeGen/AsmPrinter/DwarfDebug.h
+++ llvm/lib/CodeGen/AsmPrinter/DwarfDebug.h
@@ -351,9 +351,6 @@ class DwarfDebug : public AsmPrinterHandler {
   /// Construct a DIE for this abstract scope.
   void constructAbstractSubprogramScopeDIE(LexicalScope *Scope);
 
-  /// Collect info for variables that were optimized out.
-  void collectDeadVariables();
-
   void finishVariableDefinitions();
 
   void finishSubprogramDefinitions();
