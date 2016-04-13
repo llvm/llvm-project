8f0163a5d627d465559cd429e1e1aa17615b025c
Duncan P. N. Exon Smith <dexonsmith@apple.com>
2016-04-13 22:54:01 +0000

    ValueMapper: Resolve cycles on the new nodes
    Fix a major bug from r265456.  Although it's now much rarer, ValueMapper
    sometimes has to duplicate cycles.  The
    might-transitively-reference-a-temporary counts don't decrement on their
    own when there are cycles, and you need to call MDNode::resolveCycles to
    fix it.
    
    r265456 was checking the input nodes to see if they were unresolved.
    This is useless; they should never be unresolved.  Instead we should
    check the output nodes and resolve cycles on them.
    
    git-svn-id: https://llvm.org/svn/llvm-project/llvm/trunk@266258 91177308-0d34-0410-b5e6-96231b3b80d8
    (cherry picked from commit 4cd20e7e7da7385edd7d68d9cd4718f707a18c7c)
    
    Conflicts:
    	unittests/Transforms/Utils/ValueMapperTest.cpp
    
    Conflicts from extra #includes in not-yet-cherry-picked commits.
    
    apple-llvm-split-commit: 6bf066ba0beffd8ce81f65c93fa7d082b377a294
    apple-llvm-split-dir: llvm/

--- llvm/lib/Transforms/Utils/ValueMapper.cpp
+++ llvm/lib/Transforms/Utils/ValueMapper.cpp
@@ -541,6 +541,7 @@ void MDNodeMapper::mapDistinctNodes() {
 
 void MDNodeMapper::mapUniquedNodes() {
   // Construct uniqued nodes, building forward references as necessary.
+  SmallVector<MDNode *, 16> CyclicNodes;
   for (auto *N : POT) {
     if (N->isDistinct())
       continue;
@@ -555,11 +556,12 @@ void MDNodeMapper::mapUniquedNodes() {
 
     TempMDNode ClonedN = D.Placeholder ? std::move(D.Placeholder) : N->clone();
     remapOperands(D, *ClonedN);
-    M.mapToMetadata(N, MDNode::replaceWithUniqued(std::move(ClonedN)));
+    CyclicNodes.push_back(MDNode::replaceWithUniqued(std::move(ClonedN)));
+    M.mapToMetadata(N, CyclicNodes.back());
   }
 
   // Resolve cycles.
-  for (auto *N : POT)
+  for (auto *N : CyclicNodes)
     if (!N->isResolved())
       N->resolveCycles();
 }
