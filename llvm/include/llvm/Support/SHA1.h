24f19d65cbfd813c5892888ebff5feee81471f68
Mehdi Amini <mehdi.amini@apple.com>
2016-04-21 05:54:23 +0000

    ThinLTO: add module caching handling.
    Differential Revision: http://reviews.llvm.org/D18494
    
    From: Mehdi Amini <mehdi.amini@apple.com>
    
    git-svn-id: https://llvm.org/svn/llvm-project/llvm/trunk@266959 91177308-0d34-0410-b5e6-96231b3b80d8
    
    Conflicts:
    	include/llvm/Support/SHA1.h
    
    apple-llvm-split-commit: a42f40d4b6598a9cadbc03498a9e0696e45b7872
    apple-llvm-split-dir: llvm/

--- llvm/include/llvm/Support/SHA1.h
+++ llvm/include/llvm/Support/SHA1.h
@@ -34,6 +34,11 @@ public:
   /// Digest more data.
   void update(ArrayRef<uint8_t> Data);
 
+  /// Digest more data.
+  void update(StringRef Str) {
+    update(ArrayRef<uint8_t>((uint8_t *)Str.data(), Str.size()));
+  }
+
   /// Return a reference to the current raw 160-bits SHA1 for the digested data
   /// since the last call to init(). This call will add data to the internal
   /// state and as such is not suited for getting an intermediate result
