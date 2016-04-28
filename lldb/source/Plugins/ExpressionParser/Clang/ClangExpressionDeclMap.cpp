aedcf183f65cf684596ceae0257856cf3a2535d2
Jim Ingham <jingham@apple.com>
2016-04-28 02:17:02 +0000

    Fix an inefficiency in the handling of $__lldb_local_vars in expressions.
    The code in ClangExpressionDeclMap::FindExternalVisibleDecls figures out what the token 
    means, and adds the namespace to the lookup context, but since it doesn't mark it as
    special in the search context, we go on to pass the name $__lldb_local_vars to the ASTSource
    for further lookup.  Unless we've done our job wrong, those lookups will always fail, but
    the can be costly.
    
    So I added a bit to m_found & use that to short-circuit the lookup.
    
    <rdar://problem/25613384>
    
    
    git-svn-id: https://llvm.org/svn/llvm-project/lldb/trunk@267842 91177308-0d34-0410-b5e6-96231b3b80d8
    apple-llvm-split-commit: df6b89e8ae579fa9046a91eaa041de0e1473ee30
    apple-llvm-split-dir: lldb/

--- lldb/source/Plugins/ExpressionParser/Clang/ClangExpressionDeclMap.cpp
+++ lldb/source/Plugins/ExpressionParser/Clang/ClangExpressionDeclMap.cpp
@@ -872,7 +872,7 @@ ClangExpressionDeclMap::FindExternalVisibleDecls (NameSearchContext &context)
                                  current_id);
     }
 
-    if (!context.m_found.variable)
+    if (!context.m_found.variable && !context.m_found.local_vars_nsp)
         ClangASTSource::FindExternalVisibleDecls(context);
 }
 
@@ -1205,6 +1205,7 @@ ClangExpressionDeclMap::FindExternalVisibleDecls (NameSearchContext &context,
                         context.AddNamedDecl(namespace_decl);
                         clang::DeclContext *clang_decl_ctx = clang::Decl::castToDeclContext(namespace_decl);
                         clang_decl_ctx->setHasExternalVisibleStorage(true);
+                        context.m_found.local_vars_nsp = true;
                     }
                 }
             }
