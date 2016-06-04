14cffe0bf4dfb056a7482bc47dad488a2b87a947
Tom Stellard <thomas.stellard@amd.com>
2016-06-04 03:43:06 +0000

    Merging r268287:
    ------------------------------------------------------------------------
    r268287 | thomas.stellard | 2016-05-02 12:37:56 -0700 (Mon, 02 May 2016) | 19 lines
    
    AMDGPU/SI: Set the kill flag on temp VGPRs used to restore SGPRs from scratch
    
    Summary:
    When we restore an SGPR value from scratch, we first load it into a
    temporary VGPR and then use v_readlane_b32 to copy the value from the
    VGPR back into an SGPR.
    
    We weren't setting the kill flag on the VGPR in the v_readlane_b32
    instruction, so the register scavenger wasn't able to re-use this
    temp value later.
    
    I wasn't able to create a lit test for this.
    
    Reviewers: arsenm
    
    Subscribers: arsenm, llvm-commits
    
    Differential Revision: http://reviews.llvm.org/D19744
    
    ------------------------------------------------------------------------
    
    llvm-svn: 271770

--- llvm/lib/Target/AMDGPU/SIRegisterInfo.cpp
+++ llvm/lib/Target/AMDGPU/SIRegisterInfo.cpp
@@ -411,7 +411,7 @@ void SIRegisterInfo::eliminateFrameIndex(MachineBasicBlock::iterator MI,
                   .addMemOperand(MMO);
           BuildMI(*MBB, MI, DL,
                   TII->getMCOpcodeFromPseudo(AMDGPU::V_READLANE_B32), SubReg)
-                  .addReg(TmpReg)
+                  .addReg(TmpReg, RegState::Kill)
                   .addImm(0)
                   .addReg(MI->getOperand(0).getReg(), RegState::ImplicitDefine);
         }
