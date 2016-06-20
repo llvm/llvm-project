503b5b8fd86bda6cb8095724a318fca0816ceb4e
Vedant Kumar <vsk@apple.com>
2016-06-20 21:24:26 +0000

    [tsan] Do not instrument accesses to the gcov counters array
    There is a known intended race here. This is a follow-up to r264805,
    which disabled tsan instrumentation for updates to instrprof counters.
    For more background on this please see the discussion in D18164.
    
    git-svn-id: https://llvm.org/svn/llvm-project/llvm/trunk@273202 91177308-0d34-0410-b5e6-96231b3b80d8
    (cherry picked from commit bdba755f8532624f4b25bc801e175bbcc6ce82af)
    
    apple-llvm-split-commit: 6ac7279585ff1f4211dff13413addf224a83f296
    apple-llvm-split-dir: llvm/

--- llvm/lib/Transforms/Instrumentation/ThreadSanitizer.cpp
+++ llvm/lib/Transforms/Instrumentation/ThreadSanitizer.cpp
@@ -258,6 +258,10 @@ bool shouldInstrumentReadWriteFromAddress(Value *Addr) {
             /*AddSegment=*/false)))
         return false;
     }
+
+    // Check if the global is in the GCOV counters array.
+    if (GV->getName() == "__llvm_gcov_ctr")
+      return false;
   }
   return true;
 }
