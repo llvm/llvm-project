55e023fbedf122105dd0230f37d7ae542054d001
Jim Ingham <jingham@apple.com>
2016-06-28 01:33:03 +0000

    fixits are apparently called fix-its.
    <rdar://problem/26998596>
    
    git-svn-id: https://llvm.org/svn/llvm-project/lldb/trunk@273979 91177308-0d34-0410-b5e6-96231b3b80d8
    apple-llvm-split-commit: 4f5fcdb341e57fc84ec2836b29e5f0bc5f63a1af
    apple-llvm-split-dir: lldb/

--- lldb/source/Target/Target.cpp
+++ lldb/source/Target/Target.cpp
@@ -3598,7 +3598,7 @@ g_properties[] =
     { "swift-module-search-paths"          , OptionValue::eTypeFileSpecList, false, 0                       , nullptr, nullptr, "List of directories to be searched when locating modules for Swift." },
     { "auto-import-clang-modules"          , OptionValue::eTypeBoolean   , false, true                      , nullptr, nullptr, "Automatically load Clang modules referred to by the program." },
     { "use-all-compiler-flags"             , OptionValue::eTypeBoolean   , false, false                     , nullptr, nullptr, "Try to use compiler flags for all modules when setting up the Swift expression parser, not just the main executable." },
-    { "auto-apply-fixits"                  , OptionValue::eTypeBoolean   , false, true                      , nullptr, nullptr, "Automatically apply fixit hints to expressions." },
+    { "auto-apply-fixits"                  , OptionValue::eTypeBoolean   , false, true                      , nullptr, nullptr, "Automatically apply fix-it hints to expressions." },
     { "notify-about-fixits"                , OptionValue::eTypeBoolean   , false, true                      , nullptr, nullptr, "Print the fixed expression text." },
     { "max-children-count"                 , OptionValue::eTypeSInt64    , false, 256                       , nullptr, nullptr, "Maximum number of children to expand in any level of depth." },
     { "max-string-summary-length"          , OptionValue::eTypeSInt64    , false, 1024                      , nullptr, nullptr, "Maximum number of characters to show when using %s in summary strings." },
