7036b2e342097721c1c760d42847a142878964c4
Michael Gottesman <mgottesman@apple.com>
2016-07-15 20:59:55 -0700

    Revert "Fix the cherry pick of 6514935881a04cddd94f020ebc560a7abbd373c9."
    This reverts commit a2fb321815e984a013da2b9ee05403317a740974.
    
    apple-llvm-split-commit: 61a9164d12455547b787e1ca34a0022677cd35a6
    apple-llvm-split-dir: lldb/

--- lldb/source/API/SBThread.cpp
+++ lldb/source/API/SBThread.cpp
@@ -1271,8 +1271,8 @@ SBThread::UnwindInnermostExpression()
 
     Log *log(lldb_private::GetLogIfAllCategoriesSet (LIBLLDB_LOG_API));
 
-    Mutex::Locker api_locker;
-    ExecutionContext exe_ctx (m_opaque_sp.get(), api_locker);
+    std::unique_lock<std::recursive_mutex> lock;
+    ExecutionContext exe_ctx(m_opaque_sp.get(), lock);
 
     if (log)
         log->Printf ("SBThread(%p)::UnwindExpressionEvaluation",
