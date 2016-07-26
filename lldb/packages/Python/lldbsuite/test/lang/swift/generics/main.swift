187be5f20233c5532b3986cad929bbe7c6e994fb
Slava Pestov <spestov@apple.com>
2016-07-26 11:45:05 -0700

    Update for typealiases in protocols change
    apple-llvm-split-commit: c5f8b34ab0d7747afd4e80d9d093a2ba98d075df
    apple-llvm-split-dir: lldb/

--- lldb/packages/Python/lldbsuite/test/lang/swift/generics/main.swift
+++ lldb/packages/Python/lldbsuite/test/lang/swift/generics/main.swift
@@ -11,7 +11,7 @@
 // -----------------------------------------------------------------------------
 
 protocol Proto {
-	typealias FancyType
+	associatedtype FancyType
 	func get() -> FancyType
 }
 
