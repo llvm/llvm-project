ce9e7789e05bfdc3f485ba995b71439351c99771
Nikolay Haustov <Nikolay.Haustov@amd.com>
2016-07-28 15:54:12 +0300

    Always enable llvm-objdump tests.

--- amd/device-libs/OCL.cmake
+++ amd/device-libs/OCL.cmake
@@ -103,11 +103,11 @@ enable_testing()
 macro(clang_opencl_test name dir)
   #clang_opencl_code(${dir} ${name} llvm ocml opencl)
   clang_opencl_code(${name} ${dir} opencl ocml ockl oclc llvm)
+  add_test(
+    NAME ${name}:llvm-objdump
+    COMMAND ${LLVM_OBJDUMP} -disassemble -mcpu=fiji $<TARGET_FILE:${name}_code>
+  )
   if(AMDHSACOD)
-    add_test(
-      NAME ${name}:llvm-objdump
-      COMMAND ${LLVM_OBJDUMP} -disassemble -mcpu=fiji $<TARGET_FILE:${name}_code>
-    )
     add_test(
       NAME ${name}:amdhsacod
       COMMAND ${AMDHSACOD} -test -code $<TARGET_FILE:${name}_code>
