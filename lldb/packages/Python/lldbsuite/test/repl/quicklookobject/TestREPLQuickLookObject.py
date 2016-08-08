ef52eb689c3905b787b1cafb658f954cd3d3738f
Dave Abrahams <dave@Wingy.local>
2016-08-08 09:24:34 -0700

    Moving [Custom]PlaygroundQuickLook[able]=>PlaygroundSupport
    All the quicklook stuff belongs there.
    
    apple-llvm-split-commit: 8852c418af9c91a85322a5c389a8f1f209ed0cd6
    apple-llvm-split-dir: lldb/

--- lldb/packages/Python/lldbsuite/test/repl/quicklookobject/TestREPLQuickLookObject.py
+++ lldb/packages/Python/lldbsuite/test/repl/quicklookobject/TestREPLQuickLookObject.py
@@ -22,7 +22,7 @@ class REPLQuickLookTestCase (REPLTest):
     mydir = REPLTest.compute_mydir(__file__)
 
     def doTest(self):
-        self.command('PlaygroundQuickLook(reflecting: true)', patterns=['bool = true'])
-        self.command('PlaygroundQuickLook(reflecting: 1.25)', patterns=['double = 1.25'])
-        self.command('PlaygroundQuickLook(reflecting: Float(1.25))', patterns=['float = 1.25'])
-        self.command('PlaygroundQuickLook(reflecting: "Hello")', patterns=['text = \"Hello\"'])
+        self.command('_PlaygroundQuickLook(reflecting: true)', patterns=['bool = true'])
+        self.command('_PlaygroundQuickLook(reflecting: 1.25)', patterns=['double = 1.25'])
+        self.command('_PlaygroundQuickLook(reflecting: Float(1.25))', patterns=['float = 1.25'])
+        self.command('_PlaygroundQuickLook(reflecting: "Hello")', patterns=['text = \"Hello\"'])
