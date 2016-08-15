e7fedbcdbbaa0db3b783dbcdaed9ac45f533ab09
Hans Wennborg <hans@hanshq.net>
2016-08-15 16:06:31 +0000

    Merging r278573 (and r277399, r278157, r278569):
    ------------------------------------------------------------------------
    r278573 | timshen | 2016-08-12 15:47:13 -0700 (Fri, 12 Aug 2016) | 8 lines
    
    [LoopVectorize] Detect loops in the innermost loop before creating InnerLoopVectorizer
    
    InnerLoopVectorizer shouldn't handle a loop with cycles inside the loop
    body, even if that cycle isn't a natural loop.
    
    Fixes PR28541.
    
    Differential Revision: https://reviews.llvm.org/D22952
    ------------------------------------------------------------------------
    
    ------------------------------------------------------------------------
    r277399 | timshen | 2016-08-01 15:32:20 -0700 (Mon, 01 Aug 2016) | 9 lines
    
    [ADT] NFC: Generalize GraphTraits requirement of "NodeType *" in interfaces to "NodeRef", and migrate SCCIterator.h to use NodeRef
    
    Summary: By generalize the interface, users are able to inject more flexible Node token into the algorithm, for example, a pair of vector<Node>* and index integer. Currently I only migrated SCCIterator to use NodeRef, but more is coming. It's a NFC.
    
    Reviewers: dblaikie, chandlerc
    
    Subscribers: llvm-commits
    
    Differential Revision: https://reviews.llvm.org/D22937
    ------------------------------------------------------------------------
    
    ------------------------------------------------------------------------
    r278157 | timshen | 2016-08-09 13:23:13 -0700 (Tue, 09 Aug 2016) | 7 lines
    
    [ADT] Change iterator_adaptor_base's default template arguments to forward more underlying typedefs
    
    Reviewers: chandlerc
    
    Subscribers: llvm-commits
    
    Differential Revision: https://reviews.llvm.org/D23217
    ------------------------------------------------------------------------
    
    ------------------------------------------------------------------------
    r278569 | timshen | 2016-08-12 15:03:28 -0700 (Fri, 12 Aug 2016) | 3 lines
    
    [ADT] Add filter_iterator for filtering elements
    
    Differential Revision: https://reviews.llvm.org/D22951
    ------------------------------------------------------------------------
    
    llvm-svn: 278674

--- llvm/include/llvm/ADT/iterator.h
+++ llvm/include/llvm/ADT/iterator.h
@@ -155,7 +155,14 @@ template <
     typename T = typename std::iterator_traits<WrappedIteratorT>::value_type,
     typename DifferenceTypeT =
         typename std::iterator_traits<WrappedIteratorT>::difference_type,
-    typename PointerT = T *, typename ReferenceT = T &,
+    typename PointerT = typename std::conditional<
+        std::is_same<T, typename std::iterator_traits<
+                            WrappedIteratorT>::value_type>::value,
+        typename std::iterator_traits<WrappedIteratorT>::pointer, T *>::type,
+    typename ReferenceT = typename std::conditional<
+        std::is_same<T, typename std::iterator_traits<
+                            WrappedIteratorT>::value_type>::value,
+        typename std::iterator_traits<WrappedIteratorT>::reference, T &>::type,
     // Don't provide these, they are mostly to act as aliases below.
     typename WrappedTraitsT = std::iterator_traits<WrappedIteratorT>>
 class iterator_adaptor_base
@@ -168,15 +175,7 @@ protected:
 
   iterator_adaptor_base() = default;
 
-  template <typename U>
-  explicit iterator_adaptor_base(
-      U &&u,
-      typename std::enable_if<
-          !std::is_base_of<typename std::remove_cv<
-                               typename std::remove_reference<U>::type>::type,
-                           DerivedT>::value,
-          int>::type = 0)
-      : I(std::forward<U &&>(u)) {}
+  explicit iterator_adaptor_base(WrappedIteratorT u) : I(std::move(u)) {}
 
   const WrappedIteratorT &wrapped() const { return I; }
 
