d8147e7bde043337fc178bf5726d06198e2eb2a3
Hans Wennborg <hans@hanshq.net>
2016-08-15 16:32:18 +0000

    Merging r277783 and r278156 to unbreak Clang on the branch after r278674
    ------------------------------------------------------------------------
    r277783 | timshen | 2016-08-04 16:03:44 -0700 (Thu, 04 Aug 2016) | 9 lines
    
    [ADT] Migrate DepthFirstIterator to use NodeRef
    
    Summary: The corresponding LLVM change is D23146.
    
    Reviewers: dblaikie, chandlerc
    
    Subscribers: cfe-commits
    
    Differential Revision: https://reviews.llvm.org/D23147
    ------------------------------------------------------------------------
    
    ------------------------------------------------------------------------
    r278156 | timshen | 2016-08-09 13:22:55 -0700 (Tue, 09 Aug 2016) | 16 lines
    
    [ADT] Change iterator_adaptor_base's default template arguments to forward more underlying typedefs
    
    Summary:
    The corresponding LLVM change: D23217.
    
    LazyVector::iterator breaks, because int isn't an iterator type.
    Since iterator_adaptor_base shouldn't be blamed to break at the call to
    iterator_traits<int>::xxx, I'd rather "fix" LazyVector::iterator.
    
    The perfect solution is to model "relative pointer", but it's beyond the goal of this patch.
    
    Reviewers: chandlerc, bkramer
    
    Subscribers: cfe-commits
    
    Differential Revision: https://reviews.llvm.org/D23218
    ------------------------------------------------------------------------
    
    llvm-svn: 278678

--- clang/include/clang/StaticAnalyzer/Core/PathSensitive/ExplodedGraph.h
+++ clang/include/clang/StaticAnalyzer/Core/PathSensitive/ExplodedGraph.h
@@ -452,6 +452,7 @@ public:
 namespace llvm {
   template<> struct GraphTraits<clang::ento::ExplodedNode*> {
     typedef clang::ento::ExplodedNode NodeType;
+    typedef clang::ento::ExplodedNode *NodeRef;
     typedef NodeType::succ_iterator  ChildIteratorType;
     typedef llvm::df_iterator<NodeType*>      nodes_iterator;
 
@@ -478,6 +479,7 @@ namespace llvm {
 
   template<> struct GraphTraits<const clang::ento::ExplodedNode*> {
     typedef const clang::ento::ExplodedNode NodeType;
+    typedef const clang::ento::ExplodedNode *NodeRef;
     typedef NodeType::const_succ_iterator   ChildIteratorType;
     typedef llvm::df_iterator<NodeType*>       nodes_iterator;
 
