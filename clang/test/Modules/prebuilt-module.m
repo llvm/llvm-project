9f5ae959ea021a75ded7b242ffc872e19275f3a1
NAKAMURA Takumi <geek4civic@gmail.com>
2016-08-19 05:25:26 +0000

    clang/test/Modules/prebuilt-module.m: Prune "-triple" to fix a configuration mismatch.
    git-svn-id: https://llvm.org/svn/llvm-project/cfe/trunk@279207 91177308-0d34-0410-b5e6-96231b3b80d8
    
    Conflicts:
      test/Modules/prebuilt-module.m
    
    apple-llvm-split-commit: 710947c723491db86b59a1d2200f46e502235d57
    apple-llvm-split-dir: clang/

--- clang/test/Modules/prebuilt-module.m
+++ clang/test/Modules/prebuilt-module.m
@@ -1,7 +1,7 @@
 // RUN: rm -rf %t
 //
-// RUN: %clang_cc1 -fmodules -x objective-c -I %S/Inputs/prebuilt-module -triple %itanium_abi_triple -emit-module %S/Inputs/prebuilt-module/module.modulemap -fmodule-name=prebuilt -o %t/prebuilt.pcm
-// RUN: %clang_cc1 -fmodules -triple %itanium_abi_triple -fprebuilt-module-path=%t/ -fdisable-module-hash %s -verify
+// RUN: %clang_cc1 -fmodules -x objective-c -I %S/Inputs/prebuilt-module -emit-module %S/Inputs/prebuilt-module/module.modulemap -fmodule-name=prebuilt -o %t/prebuilt.pcm
+// RUN: %clang_cc1 -fmodules -fprebuilt-module-path=%t/ -fdisable-module-hash %s -verify
 
 // expected-no-diagnostics
 @import prebuilt;
