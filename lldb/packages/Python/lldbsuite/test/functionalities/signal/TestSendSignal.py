26d8b395bca8730983c38318f488dae0348b0c31
Todd Fiala <tfiala@apple.com>
2016-08-25 15:40:50 -0700

    Remove macOS XFAIL marker from TestSendSignal.py
    rdar://problem/23634555
    
    apple-llvm-split-commit: 412e7e2e2d2d14acd8e6beff7cbe2a8c7fb530fc
    apple-llvm-split-dir: lldb/

--- lldb/packages/Python/lldbsuite/test/functionalities/signal/TestSendSignal.py
+++ lldb/packages/Python/lldbsuite/test/functionalities/signal/TestSendSignal.py
@@ -23,7 +23,6 @@ class SendSignalTestCase(TestBase):
 
     @expectedFailureAll(oslist=['freebsd'], bugnumber="llvm.org/pr23318: does not report running state")
     @skipIfWindows # Windows does not support signals
-    @expectedFailureDarwin("rdar://problem/23634555")
     def test_with_run_command(self):
         """Test that lldb command 'process signal SIGUSR1' sends a signal to the inferior process."""
         self.build()
