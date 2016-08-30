eaa9ab645c058e4f223acbbe1dd6fc12e54de99f
Yaxun (Sam) Liu <yaxun.liu@amd.com>
2016-08-30 13:56:52 -0400

    Do not validate pch when -fno-validate-pch is set.
    Differential Revision: https://reviews.llvm.org/D24054

--- clang/lib/Serialization/ASTReader.cpp
+++ clang/lib/Serialization/ASTReader.cpp
@@ -8710,7 +8710,8 @@ ASTReader::ASTReader(
   bool AllowConfigurationMismatch, bool ValidateSystemInputs,
   bool UseGlobalIndex,
   std::unique_ptr<llvm::Timer> ReadTimer)
-    : Listener(new PCHValidator(PP, *this)), DeserializationListener(nullptr),
+    : Listener(DisableValidation ? nullptr : new PCHValidator(PP, *this)),
+      DeserializationListener(nullptr),
       OwnsDeserializationListener(false), SourceMgr(PP.getSourceManager()),
       FileMgr(PP.getFileManager()), PCHContainerRdr(PCHContainerRdr),
       Diags(PP.getDiagnostics()), SemaObj(nullptr), PP(PP), Context(Context),
