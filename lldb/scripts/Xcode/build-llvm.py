8dafd29bc34204272063ecfea09005f6f2b19041
Todd Fiala <todd.fiala@gmail.com>
2016-09-14 13:18:03 -0700

    fix up build-llvm.py for merge into swift-lldb/master
    apple-llvm-split-commit: 9db88c31e57391915c49ddbd0c5f6b31ee3869dc
    apple-llvm-split-dir: lldb/

--- lldb/scripts/Xcode/build-llvm.py
+++ lldb/scripts/Xcode/build-llvm.py
@@ -21,17 +21,17 @@ def LLVM_HASH_INCLUDES_DIFFS():
 
 
 def LLVM_REF():
-    llvm_ref = "stable-next"
+    llvm_ref = "stable"
     return llvm_ref
 
 
 def CLANG_REF():
-    clang_ref = "stable-next"
+    clang_ref = "stable"
     return clang_ref
 
 
 def SWIFT_REF():
-    swift_ref = "master-next"
+    swift_ref = "master"
     return swift_ref
 
 # For use with Xcode-style builds
