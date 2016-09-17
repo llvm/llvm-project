08557f6ca53ada4ac6cb29c4fc3e009698c623d4
Todd Fiala <todd.fiala@gmail.com>
2016-09-16 20:36:12 -0700

    Revert "added tests for SR-2637"
    Something about the Objective-C framework path is not working
    on the CI.  Revert out the test while I fix it.
    
    This reverts commit a646342616653fb17aae53ba82807dfc875ce100.
    
    apple-llvm-split-commit: a4630701c013d8e497266ab48b3d19d6f39758d8
    apple-llvm-split-dir: lldb/

--- lldb/packages/Python/lldbsuite/test/lang/swift/static_linking/macOS/A.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-@objc public class A: NSObject {
-	public func foo() -> Int {
-	  return 4  // Set breakpoint here
-	}
-}
--- lldb/packages/Python/lldbsuite/test/lang/swift/static_linking/macOS/B.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-@objc public class B: NSObject {
-	public func bar() -> Int {
-	  return 8  // Set breakpoint here
-	}
-}
