03f0108e16097bb6eb73438b9b11a5a18533d682
Slava Pestov <spestov@apple.com>
2016-09-19 01:38:23 -0700

    Update for removal of AbstractTypeParamDecl::getArchetype()
    apple-llvm-split-commit: a037ccdc3a97185c8c67844657ebed3057e669a7
    apple-llvm-split-dir: lldb/

--- lldb/source/Symbol/SwiftASTContext.cpp
+++ lldb/source/Symbol/SwiftASTContext.cpp
@@ -8599,18 +8599,6 @@ SwiftASTContext::GetTemplateArgument(void *type, size_t arg_idx,
       return CompilerType(GetASTContext(),
                           substitution.getReplacement().getPointer());
     }
-    case swift::TypeKind::PolymorphicFunction: {
-      swift::PolymorphicFunctionType *polymorhpic_func_type =
-          swift_can_type->getAs<swift::PolymorphicFunctionType>();
-      if (!polymorhpic_func_type)
-        break;
-      if (arg_idx >= polymorhpic_func_type->getGenericParameters().size())
-        break;
-      kind = eTemplateArgumentKindType;
-      return CompilerType(GetASTContext(),
-                          polymorhpic_func_type->getGenericParameters()[arg_idx]
-                              ->getArchetype());
-    } break;
     default:
       break;
     }
