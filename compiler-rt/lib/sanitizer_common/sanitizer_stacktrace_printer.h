037a93b07076610dc61adfbba19180e91378e920
Kostya Serebryany <kcc@google.com>
2016-09-19 05:10:32 +0000

    [sanitizer] add __sanitizer_symbolize_data (can only print the names of the globals for now)
    git-svn-id: https://llvm.org/svn/llvm-project/compiler-rt/trunk@281886 91177308-0d34-0410-b5e6-96231b3b80d8
    apple-llvm-split-commit: 4655824ac5f3375b8fe40bc760f4934d40f472f5
    apple-llvm-split-dir: compiler-rt/

--- compiler-rt/lib/sanitizer_common/sanitizer_stacktrace_printer.h
+++ compiler-rt/lib/sanitizer_common/sanitizer_stacktrace_printer.h
@@ -60,6 +60,13 @@ void RenderModuleLocation(InternalScopedString *buffer, const char *module,
                           uptr offset, ModuleArch arch,
                           const char *strip_path_prefix);
 
+// Same as RenderFrame, but for data section (global variables).
+// Accepts %s, %l from above.
+// Also accepts:
+//   %g - name of the global variable.
+void RenderData(InternalScopedString *buffer, const char *format,
+                const DataInfo *DI, const char *strip_path_prefix = "");
+
 }  // namespace __sanitizer
 
 #endif  // SANITIZER_STACKTRACE_PRINTER_H
