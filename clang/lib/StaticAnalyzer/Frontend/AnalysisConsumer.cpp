d76208d8d66f497a5d6ee82148e2dd107a7a73a1
Alexander Shaposhnikov <shal1t712@gmail.com>
2016-09-23 20:49:01 +0000

    [analyzer] Fix crash in RetainCountChecker::checkEndFunction
    The class BodyFarm creates bodies for
    OSAtomicCompareAndSwap*, objc_atomicCompareAndSwap*, dispatch_sync*, dispatch_once*
    and for them the flag isBodyAutosynthesized is set to true.
    
    This diff
    1. makes AnalysisConsumer::HandleCode skip the autosynthesized code
    2. replaces assert(LCtx->getParent()) in RetainCountChecker::checkEndFunction
    by assert(!LCtx->inTopFrame()) (minor cleanup)
    
    Test plan: make -j8 check-clang-analysis
    
    Differential revision: https://reviews.llvm.org/D24792
    
    git-svn-id: https://llvm.org/svn/llvm-project/cfe/trunk@282293 91177308-0d34-0410-b5e6-96231b3b80d8
    (cherry picked from commit 714e1790a65be61d287c8290a6302eddb5662b86)
    
    apple-llvm-split-commit: de7042b3b14cc454d030ac67feb7d123be9b3b80
    apple-llvm-split-dir: clang/

--- clang/lib/StaticAnalyzer/Frontend/AnalysisConsumer.cpp
+++ clang/lib/StaticAnalyzer/Frontend/AnalysisConsumer.cpp
@@ -654,6 +654,12 @@ void AnalysisConsumer::HandleCode(Decl *D, AnalysisMode Mode,
   if (Mode == AM_None)
     return;
 
+  // Clear the AnalysisManager of old AnalysisDeclContexts.
+  Mgr->ClearContexts();
+  // Ignore autosynthesized code.
+  if (Mgr->getAnalysisDeclContext(D)->isBodyAutosynthesized())
+    return;
+
   DisplayFunction(D, Mode, IMode);
   CFG *DeclCFG = Mgr->getCFG(D);
   if (DeclCFG) {
@@ -661,8 +667,6 @@ void AnalysisConsumer::HandleCode(Decl *D, AnalysisMode Mode,
     MaxCFGSize = MaxCFGSize < CFGSize ? CFGSize : MaxCFGSize;
   }
 
-  // Clear the AnalysisManager of old AnalysisDeclContexts.
-  Mgr->ClearContexts();
   BugReporter BR(*Mgr);
 
   if (Mode & AM_Syntax)
