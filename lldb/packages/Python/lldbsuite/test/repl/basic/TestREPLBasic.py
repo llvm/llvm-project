50f8fbf313b14b37ced154d7ac1b5e17a81f851f
Enrico Granata <egranata@apple.com>
2016-10-05 13:44:57 -0700

    Convert repl/basic away from pexpect
    apple-llvm-split-commit: 152eef2083e063aa07fecf56ce12185c72aaee4b
    apple-llvm-split-dir: lldb/

--- lldb/packages/Python/lldbsuite/test/repl/basic/TestREPLBasic.py
+++ lldb/packages/Python/lldbsuite/test/repl/basic/TestREPLBasic.py
@@ -1,29 +1,4 @@
-# TestREPLBasic.py
-#
-# This source file is part of the Swift.org open source project
-#
-# Copyright (c) 2014 - 2015 Apple Inc. and the Swift project authors
-# Licensed under Apache License v2.0 with Runtime Library Exception
-#
-# See http://swift.org/LICENSE.txt for license information
-# See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
-#
-# ------------------------------------------------------------------------------
-"""Test that we can launch the REPL and define a variable."""
+import lldbsuite.test.lldbinrepl as lldbinrepl
+import lldbsuite.test.lldbtest as lldbtest
 
-import os
-import time
-import unittest2
-import lldb
-from lldbsuite.test.lldbrepl import REPLTest, load_tests
-
-
-class REPLBasicTestCase (REPLTest):
-
-    mydir = REPLTest.compute_mydir(__file__)
-
-    def doTest(self):
-        self.command('var a = 1', patterns=['a: Int = 1'])
-        self.command(
-            'let b = "Hello World"',
-            patterns=['b: String = "Hello World"'])
+lldbinrepl.MakeREPLTest(__file__, globals(), [])
---
+++ lldb/packages/Python/lldbsuite/test/repl/basic/input.swift
@@ -0,0 +1,4 @@
+let a = 1; a
+///= 1
+let b = "Hello World"; b
+///= "Hello World"
