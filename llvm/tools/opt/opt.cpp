0c077604405f74e1019068d6251e8b1bd66a4946
Teresa Johnson <tejohnson@google.com>
2016-11-14 17:12:32 +0000

    Restore "[ThinLTO] Prevent exporting of locals used/defined in module level asm"
    This restores the rest of r286297 (part was restored in r286475).
    Specifically, it restores the part requiring adding a dependency from
    the Analysis to Object library (downstream use changed to correctly
    model split BitReader vs BitWriter libraries).
    
    Original description of this part of patch follows:
    
    Module level asm may also contain defs of values. We need to prevent
    export of any refs to local values defined in module level asm (e.g. a
    ref in normal IR), since that also requires renaming/promotion of the
    local. To do that, the summary index builder looks at all values in the
    module level asm string that are not marked Weak or Global, which is
    exactly the set of locals that are defined. A summary is created for
    each of these local defs and flagged as NoRename.
    
    This required adding handling to the BitcodeWriter to look at GV
    declarations to see if they have a summary (rather than skipping them
    all).
    
    Finally, added an assert to IRObjectFile::CollectAsmUndefinedRefs to
    ensure that an MCAsmParser is available, otherwise the module asm parse
    would silently fail. Initialized the asm parser in the opt tool for use
    in testing this fix.
    
    Fixes PR30610.
    
    git-svn-id: https://llvm.org/svn/llvm-project/llvm/trunk@286844 91177308-0d34-0410-b5e6-96231b3b80d8
    apple-llvm-split-commit: 9685e1c33c7b923a052db20ff5d0e0ef58afbe28
    apple-llvm-split-dir: llvm/

--- llvm/tools/opt/opt.cpp
+++ llvm/tools/opt/opt.cpp
@@ -363,6 +363,7 @@ int main(int argc, char **argv) {
   InitializeAllTargets();
   InitializeAllTargetMCs();
   InitializeAllAsmPrinters();
+  InitializeAllAsmParsers();
 
   // Initialize passes
   PassRegistry &Registry = *PassRegistry::getPassRegistry();
