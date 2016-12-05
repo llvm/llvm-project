cd6b588486c8e37cc9ac2579a2797d3d86d9bdc0
Daniil Fukalov <dfukalov@gmail.com>
2016-12-05 17:44:25 +0300

    Revert "removed declarations processing since the pass should clone kernel functions"
    This reverts commit 8fef8d72c5afa1bcfe52668508c1b1a23c2c42fd.

--- llvm/lib/Target/AMDGPU/AMDGPULowerKernelCalls.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPULowerKernelCalls.cpp
@@ -67,10 +67,18 @@ static void setNameForBody(Function *FBody, const Function &FKernel) {
   FBody->setName(NewName.str());
 }
 
+static Function *copyDeclaration(const Function &F) {
+  Function *NewF = Function::Create(F.getFunctionType(), F.getLinkage());
+  NewF->setAttributes(F.getAttributes());
+  return NewF;
+}
+
 static Function *cloneKernel(Function &F) {
   ValueToValueMapTy ignored;
-  assert(!F.isDeclaration() && "called kernel function should have a body");
-  Function *NewF = CloneFunction(&F, ignored, false);
+  Function *NewF =
+      F.empty() ? copyDeclaration(F) : CloneFunction(&F, ignored, false);
+  F.getParent()->getOrInsertFunction(NewF->getName(), NewF->getFunctionType(),
+    NewF->getAttributes());
   NewF->setCallingConv(CallingConv::C);
   setNameForBody(NewF, F);
   return NewF;
