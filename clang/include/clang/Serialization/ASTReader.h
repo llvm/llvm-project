72645c619d60c2be564873a98cecf83d7c42689e
Duncan P. N. Exon Smith <dexonsmith@apple.com>
2017-01-28 22:15:22 +0000

    Modules: Return ModuleFile& from ModuleManager::begin, etc.; NFC
    Hide the pointer indirection in ModuleManager::begin, ModuleManager::end,
    ModuleManager::rbegin, and ModuleManager::rend.  Besides tidying up the call
    sites, this is preparation for making ownership of ModuleFile explicit.
    
    git-svn-id: https://llvm.org/svn/llvm-project/cfe/trunk@293394 91177308-0d34-0410-b5e6-96231b3b80d8
    (cherry picked from commit 5ee103da4d162eed0c558e81733f5260e75c3533)
    
    apple-llvm-split-commit: ecc014fb7f876d570ca871392cd98ba48945b1a2
    apple-llvm-split-dir: clang/

--- clang/include/clang/Serialization/ASTReader.h
+++ clang/include/clang/Serialization/ASTReader.h
@@ -1635,7 +1635,7 @@ public:
     unsigned Result = 0;
     for (ModuleConstIterator I = ModuleMgr.begin(),
         E = ModuleMgr.end(); I != E; ++I) {
-      Result += (*I)->NumPreprocessedEntities;
+      Result += I->NumPreprocessedEntities;
     }
 
     return Result;
