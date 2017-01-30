e1919c88a5a3e6b90fb9c0e288223ddce02778af
Sean Callanan <scallanan@apple.com>
2017-01-30 13:01:20 -0800

    xfailed this test pending a proper fix.
    (cherry picked from commit dbb730e8ca79a468e87c49c302c31d1d0fe96f22)
    
    apple-llvm-split-commit: 2efb3452cd8d50dbcfa45f827ffb182bd20fbbb7
    apple-llvm-split-dir: lldb/

--- lldb/packages/Python/lldbsuite/test/lang/swift/tmd_symbol/TestSwiftMetadataSymbolsType.py
+++ lldb/packages/Python/lldbsuite/test/lang/swift/tmd_symbol/TestSwiftMetadataSymbolsType.py
@@ -25,6 +25,9 @@ class TestSwiftMetadataSymbol(TestBase):
     mydir = TestBase.compute_mydir(__file__)
 
     @decorators.swiftTest
+    @decorators.expectedFailureAll(
+        oslist=["linux"],
+        bugnumber="rdar://problem/30269976")
     def test_swift_metadata_symbol(self):
         """Test that metadata symbols are properly resolved as such"""
         self.build()
