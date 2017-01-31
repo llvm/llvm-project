48f4143a6f99192af016efd83ed81db6b9b1ff1c
Sean Callanan <scallanan@apple.com>
2017-01-31 14:53:51 -0800

    Verify that LLDB can debug source files compiled with older -swift-versions. (#130)
    * Added a test verifying that LLDB can debug source files compiled with older -swift-versions.
    
    apple-llvm-split-commit: f3f3a8e2cf04ffbe0ffa68b0af1123e9cb75dab8
    apple-llvm-split-dir: lldb/

---
+++ lldb/packages/Python/lldbsuite/test/lang/swift/swift_version/mod3.swift
@@ -0,0 +1,19 @@
+// mod3.swift
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2017 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+// -----------------------------------------------------------------------------
+
+struct S3 {
+  var i = 3
+}
+
+public func f3() -> Int {
+  return 3 // break 3
+}
