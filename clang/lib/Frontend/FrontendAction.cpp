559ac811cd32b330bb39b47b27651b6e11d086a4
Adrian Prantl <aprantl@apple.com>
2017-02-16 21:04:14 -0800

    Prevent nonrecoverable module-out-of-date errors in projects that mix PCH and clang modules.
    This patch introduces a NeededByPCHOrCompilationUsesPCH language
    option which causes clang modules imported by PCH which goes into the
    module hash, thus isolating them from rebuilds triggered by other
    clang modules.  This patch further disables module validation for
    modules imported by PCH, since there is no mechanism in clang to
    rebuild ou-tof-date modules when they were imported by a PCH.
    
    rdar://problem/30384801
    rdar://problem/35056912
    
    apple-llvm-split-commit: 97d7bbb1c58bb505dea390f725460069edb0c547
    apple-llvm-split-dir: clang/

--- clang/lib/Frontend/FrontendAction.cpp
+++ clang/lib/Frontend/FrontendAction.cpp
@@ -694,6 +694,7 @@ bool FrontendAction::BeginSourceFile(CompilerInstance &CI,
                 CI.getLangOpts(), CI.getTargetOpts(), CI.getPreprocessorOpts(),
                 SpecificModuleCachePath)) {
           PPOpts.ImplicitPCHInclude = Dir->getName();
+          CI.getLangOpts().NeededByPCHOrCompilationUsesPCH = true;
           Found = true;
           break;
         }
