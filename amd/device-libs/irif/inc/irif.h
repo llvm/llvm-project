7da991105389ac8f0f06691f5b627afcb589f85c
Guansong Zhang <zhang.guansong@gmail.com>
2017-03-27 19:13:12 +0000

    Add ctlz 64 bit

--- amd/device-libs/irif/inc/irif.h
+++ amd/device-libs/irif/inc/irif.h
@@ -121,6 +121,8 @@ extern bool __llvm_smul_with_overflow_i64(long, long, __private long*);
 extern bool __llvm_umul_with_overflow_i64(ulong, ulong, __private ulong*);
 
 extern __attribute__((const)) int __llvm_ctlz_i32(int);
+extern __attribute__((const)) int __llvm_ctlz_i64(long);
+
 extern __attribute__((const)) int __llvm_cttz_i32(int);
 
 // Fence intrinsics
