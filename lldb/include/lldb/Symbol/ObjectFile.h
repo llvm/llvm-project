fed7a6cb1f07840928158bbda830e746bca8f4d4
Jason Molenda <jmolenda@apple.com>
2017-04-12 23:33:30 +0000

    Use two LC_NOTE load commands for identifying the main binary that lldb should use when given a corefile.
    This uses an LC_NOTE "main bin spec" or an LC_NOTE "kern ver str"
    if they are present in a Mach-O core file.
    
    Core files may have multiple different binaries -- different kernels,
    or a mix of user process and kernel binaries -- and it can be
    difficult for lldb to detect the correct one to use simply by looking
    at the pages of memory.  These two new LC_NOTE load commands allow
    for the correct binary to be recorded unambiguously.
    
    <rdar://problem/20878266>
    
    git-svn-id: https://llvm.org/svn/llvm-project/lldb/trunk@300138 91177308-0d34-0410-b5e6-96231b3b80d8
    (cherry picked from commit 13db56fe1cb83f9a9f02e5559573ed6fc3d907c9)
    
    apple-llvm-split-commit: 64ac602bf00a9359ccdb1ae5c5ec120044a4a9e2
    apple-llvm-split-dir: lldb/

--- lldb/include/lldb/Symbol/ObjectFile.h
+++ lldb/include/lldb/Symbol/ObjectFile.h
@@ -14,6 +14,7 @@
 #include "lldb/Core/FileSpecList.h"
 #include "lldb/Core/ModuleChild.h"
 #include "lldb/Core/PluginInterface.h"
+#include "lldb/Core/UUID.h"
 #include "lldb/Host/Endian.h"
 #include "lldb/Host/FileSpec.h"
 #include "lldb/Symbol/Symtab.h"
@@ -574,6 +575,32 @@ public:
   //------------------------------------------------------------------
   virtual std::string GetIdentifierString () { 
       return std::string(); 
+  }
+
+  //------------------------------------------------------------------
+  /// When the ObjectFile is a core file, lldb needs to locate the
+  /// "binary" in the core file.  lldb can iterate over the pages looking
+  /// for a valid binary, but some core files may have metadata 
+  /// describing where the main binary is exactly which removes ambiguity
+  /// when there are multiple binaries present in the captured memory pages.
+  ///
+  /// @param[out] address
+  ///   If the address of the binary is specified, this will be set.
+  ///   This is an address is the virtual address space of the core file
+  ///   memory segments; it is not an offset into the object file.
+  ///   If no address is available, will be set to LLDB_INVALID_ADDRESS.
+  ///
+  /// @param[out] uuid
+  ///   If the uuid of the binary is specified, this will be set.
+  ///   If no UUID is available, will be cleared.
+  ///
+  /// @return
+  ///   Returns true if either address or uuid has been set.
+  //------------------------------------------------------------------
+  virtual bool GetCorefileMainBinaryInfo (lldb::addr_t &address, UUID &uuid) {
+      address = LLDB_INVALID_ADDRESS;
+      uuid.Clear();
+      return false;
   }
 
   virtual lldb::RegisterContextSP
