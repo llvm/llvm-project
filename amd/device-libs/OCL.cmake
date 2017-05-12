a3020dce8f891d27ec2e5305c0ab1faf7ad364a5
Daniil Fukalov <daniil.fukalov@amd.com>
2017-05-12 20:24:06 +0300

    cmake build reworked, attempt 2: 1. switched to direct .cl -> LLVMIR compilation without .cl->.c copying 2. removed became redundant macros, custom targets, target properties 3. prepare_builtins is now post-build step of a regular target created with    add_library()
    Change-Id: Ie1bb333d43e6a1fd840e1ea419e1b32d28033af7

--- amd/device-libs/OCL.cmake
+++ amd/device-libs/OCL.cmake
@@ -5,102 +5,82 @@
 ## License. See LICENSE.TXT for details.
 ##===--------------------------------------------------------------------------
 
+set (CLANG_OCL_FLAGS "-Werror -x cl -Xclang -cl-std=CL2.0 -fblocks -target ${AMDGPU_TARGET_TRIPLE} -DCL_VERSION_2_0=200 -D__OPENCL_C_VERSION__=200 -Dcl_khr_fp64 -Dcl_khr_fp16 -Dcl_khr_subgroups -Dcl_khr_int64_base_atomics -Dcl_khr_int64_extended_atomics -Xclang -finclude-default-header ${CLANG_OPTIONS_APPEND}")
+set (CLANG_OCL_LINK_FLAGS "-target ${AMDGPU_TARGET_TRIPLE} -mcpu=fiji")
+
 set (LLVM_LINK "${LLVM_TOOLS_BINARY_DIR}/llvm-link")
 set (LLVM_OBJDUMP "${LLVM_TOOLS_BINARY_DIR}/llvm-objdump")
 
-set (BC_EXT .amdgcn.bc)
-set (CMAKE_OCL_COMPILER_ENV_VAR OCLC)
-set (CMAKE_OCL_OUTPUT_EXTENTION .bc)
-set (CMAKE_OCL_OUTPUT_EXTENTION_REPLACE 1)
-set (CMAKE_OCL_COMPILER ${CMAKE_C_COMPILER})
-set (CMAKE_OCL_COMPILE_OBJECT "<CMAKE_OCL_COMPILER> -o <OBJECT> <FLAGS> -c <SOURCE>")
+set (BC_EXT .bc)
+set (LIB_SUFFIX ".lib${BC_EXT}")
+set (CMAKE_INCLUDE_FLAG_OCL "-I")
+set (CMAKE_OCL_COMPILER_ENV_VAR OCL)
+set (CMAKE_OCL_OUTPUT_EXTENTION ${BC_EXT})
+set (CMAKE_OCL_COMPILER ${LLVM_TOOLS_BINARY_DIR}/clang)
+set (CMAKE_OCL_COMPILE_OBJECT "${CMAKE_OCL_COMPILER} <INCLUDES> -o <OBJECT> <FLAGS> -c <SOURCE>")
 set (CMAKE_OCL_LINK_EXECUTABLE "${LLVM_LINK} -o <TARGET> <LINK_LIBRARIES>")
 set (CMAKE_OCL_CREATE_STATIC_LIBRARY "${LLVM_LINK} -o <TARGET> <OBJECTS>")
-set (CMAKE_CXX_IMPLICIT_LINK_LIBRARIES "")
-set (CMAKE_CXX_IMPLICIT_LINK_DIRECTORIES "")
-set (CMAKE_C_IMPLICIT_LINK_LIBRARIES "")
-set (CMAKE_C_IMPLICIT_LINK_DIRECTORIES "")
-set (CMAKE_OCL_IMPLICIT_LINK_LIBRARIES "")
-set (CMAKE_OCL_IMPLICIT_LINK_DIRECTORIES "")
-set (CMAKE_OCL_LINKER_PREFERENCE_PROPAGATES 0)
-set (CMAKE_SHARED_LIBRARY_LINK_C_FLAGS)
+set (CMAKE_OCL_SOURCE_FILE_EXTENSIONS "cl")
+set (CMAKE_EXECUTABLE_SUFFIX_OCL ".co")
 
-macro(clang_csources name dir)
-  set(csources)
+macro(opencl_bc_lib name)
+  set(lib_tgt ${name}_lib)
   foreach(file ${ARGN})
-    file(RELATIVE_PATH rfile ${dir} ${file})
-    get_filename_component(rdir ${rfile} DIRECTORY)
-    get_filename_component(fname ${rfile} NAME_WE)
-    get_filename_component(fext ${rfile} EXT)
-    file(MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/${rdir})
+    get_filename_component(fext ${file} EXT)
+    #mark files as OCL source
     if (fext STREQUAL ".cl")
-      set(cfile ${CMAKE_CURRENT_BINARY_DIR}/${rdir}/${fname}.c)
-      add_custom_command(
-        OUTPUT ${cfile}
-        COMMAND cp -f ${file} ${cfile}
-        DEPENDS ${file}
-      )
-      list(APPEND csources ${cfile})
+      set_source_files_properties(${file} PROPERTIES LANGUAGE "OCL")
     endif()
+    #mark files as OCL object to add them to link
     if (fext STREQUAL ".ll")
-      list(APPEND csources ${file})
-      set(cfile ${CMAKE_CURRENT_BINARY_DIR}/${rdir}/${fname}.o)
-      add_custom_command(
-        OUTPUT ${cfile}
-        COMMAND cp -f ${file} ${cfile}
-        DEPENDS ${file}
-      )
-      list(APPEND csources ${cfile})
+      set_source_files_properties(${file} PROPERTIES EXTERNAL_OBJECT TRUE)
     endif()
   endforeach()
-endmacro()
 
-macro(clang_opencl_bc_lib name dir)
-  set(CMAKE_INCLUDE_CURRENT_DIR ON)
-  clang_csources(${name} ${dir} ${ARGN})
-  add_library(${name}_lib_bc STATIC ${csources})
-  add_dependencies(${name}_lib_bc llvm-link clang)
-  set_target_properties(${name}_lib_bc PROPERTIES OUTPUT_NAME ${name})
-  set_target_properties(${name}_lib_bc PROPERTIES PREFIX "" SUFFIX ".lib.bc")
-  set_target_properties(${name}_lib_bc PROPERTIES COMPILE_FLAGS "${CLANG_OCL_FLAGS} -emit-llvm")
-  set_target_properties(${name}_lib_bc PROPERTIES LANGUAGE OCL)
-  set_target_properties(${name}_lib_bc PROPERTIES LINKER_LANGUAGE OCL)
-endmacro(clang_opencl_bc_lib)
+  add_library(${lib_tgt} STATIC ${ARGN})
 
-macro(prepare_builtins name)
-  add_custom_command(
-    OUTPUT ${name}${BC_EXT}
-    COMMAND ${PREPARE_BUILTINS} ${name}.lib.bc -o ${name}${BC_EXT}
-    DEPENDS prepare-builtins ${name}_lib_bc
+  if(NOT ROCM_DEVICELIB_STANDALONE_BUILD)
+    add_dependencies(${lib_tgt} llvm-link clang)
+  endif(NOT ROCM_DEVICELIB_STANDALONE_BUILD)
+
+  set_target_properties(${lib_tgt} PROPERTIES
+    ARCHIVE_OUTPUT_NAME "${CMAKE_CURRENT_BINARY_DIR}/${name}"
+    PREFIX "" SUFFIX ${LIB_SUFFIX}
+    COMPILE_FLAGS "${CLANG_OCL_FLAGS} -emit-llvm"
+    LANGUAGE "OCL" LINKER_LANGUAGE "OCL")
+  set(output_name "${name}.amdgcn${BC_EXT}")
+
+  if (TARGET prepare-builtins)
+    add_dependencies(${lib_tgt} prepare-builtins)
+  endif()
+
+  add_custom_command(TARGET ${lib_tgt}
+    POST_BUILD
+    COMMAND ${PREPARE_BUILTINS} ${name}${LIB_SUFFIX} -o ${output_name}
+    DEPENDS ${lib_tgt}
+    COMMENT "Generating ${output_name}"
   )
-  add_custom_target(${name}_bc ALL
-    DEPENDS ${name}${BC_EXT}
-  )
-  set(TARGET_FILE_${name} ${CMAKE_CURRENT_BINARY_DIR}/${name}${BC_EXT} CACHE INTERNAL "")
-endmacro(prepare_builtins)
-
-macro(clang_opencl_bc_builtins_lib name dir)
-  clang_opencl_bc_lib(${name} ${dir} ${ARGN})
-  prepare_builtins(${name})
-  install (FILES ${CMAKE_CURRENT_BINARY_DIR}/${name}${BC_EXT} DESTINATION lib)
-endmacro(clang_opencl_bc_builtins_lib)
+  install (FILES ${CMAKE_CURRENT_BINARY_DIR}/${output_name} DESTINATION lib COMPONENT device-libs)
+endmacro(opencl_bc_lib)
 
 macro(clang_opencl_code name dir)
-  clang_csources(${name}_code ${dir} ${dir}/${name}.cl)
-  add_executable(${name}_code ${csources})
+  set(tgt_name ${name}_code)
+  set_source_files_properties(${dir}/${name}.cl PROPERTIES LANGUAGE "OCL")
+  add_executable(${tgt_name} ${dir}/${name}.cl)
   set(mlink_flags)
   foreach (lib ${ARGN})
-    add_dependencies(${name}_code ${lib}_bc)
-    set(mlink_flags "${mlink_flags} -Xclang -mlink-bitcode-file -Xclang ${TARGET_FILE_${lib}}")
+    add_dependencies(${tgt_name} ${lib}_lib)
+    get_target_property(lib_path ${lib}_lib ARCHIVE_OUTPUT_NAME)
+    set(mlink_flags "${mlink_flags} -Xclang -mlink-bitcode-file -Xclang ${lib_path}.amdgcn${BC_EXT}")
   endforeach()
-  set_target_properties(${name}_code PROPERTIES LINKER_LANGUAGE C)
-  set_target_properties(${name}_code PROPERTIES OUTPUT_NAME ${name})
-  set_target_properties(${name}_code PROPERTIES COMPILE_FLAGS "${CLANG_OCL_FLAGS} ${CLANG_OCL_LINK_FLAGS} ${mlink_flags}")
-  set_target_properties(${name}_code PROPERTIES LINK_FLAGS "${CLANG_OCL_LINK_FLAGS}")
-  set_target_properties(${name}_code PROPERTIES PREFIX "" SUFFIX ".co")
-endmacro(clang_opencl_code)
+  set(CMAKE_OCL_FLAGS "${CMAKE_OCL_FLAGS} -mcpu=fiji ${mlink_flags}")
+  #dummy link since clang already generated CodeObject file
+  set(CMAKE_OCL_LINK_EXECUTABLE "${CMAKE_COMMAND} -E copy <OBJECTS> <TARGET>")
+  set_target_properties(${tgt_name} PROPERTIES
+    COMPILE_FLAGS "${CLANG_OCL_FLAGS} ${CMAKE_OCL_FLAGS}"
+    LANGUAGE "OCL" LINKER_LANGUAGE "OCL")
 
-enable_testing()
+endmacro(clang_opencl_code)
 
 set (oclc_default_libs
   oclc_correctly_rounded_sqrt_off
@@ -125,7 +105,6 @@ macro(clang_opencl_test name dir)
 endmacro(clang_opencl_test)
 
 macro(clang_opencl_test_file dir fname)
-  get_filename_component(fext ${fname} EXT)
   get_filename_component(name ${fname} NAME_WE)
   get_filename_component(fdir ${fname} DIRECTORY)
   clang_opencl_test(${name} ${dir}/${fdir})
