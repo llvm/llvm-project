93b5fc6caa0fabffe0773fbfad72fb224c071ba6
Akira Hatanaka <ahatanaka@apple.com>
2017-05-18 03:52:29 +0000

    [ThinLTO] Do not assert when adding a module with a different but compatible target triple
    Currently, an assertion fails in ThinLTOCodeGenerator::addModule when
    the target triple of the module being added doesn't match that of the
    one stored in TMBuilder. This patch relaxes the constraint and makes
    changes to allow target triples that only differ in their version
    numbers on Apple platforms, similarly to what r228999 did.
    
    rdar://problem/30133904
    
    Differential Revision: https://reviews.llvm.org/D33291
    
    git-svn-id: https://llvm.org/svn/llvm-project/llvm/trunk@303326 91177308-0d34-0410-b5e6-96231b3b80d8
    (cherry picked from commit 343e535d9c38cf57173ace6597380752a18a6a67)
    
    apple-llvm-split-commit: 27a708f5c13999b82d780f9a1df862bb0664e2ac
    apple-llvm-split-dir: llvm/

--- llvm/include/llvm/ADT/Triple.h
+++ llvm/include/llvm/ADT/Triple.h
@@ -250,6 +250,10 @@ public:
            ObjectFormat == Other.ObjectFormat;
   }
 
+  bool operator!=(const Triple &Other) const {
+    return !(*this == Other);
+  }
+
   /// @}
   /// @name Normalization
   /// @{
@@ -705,6 +709,12 @@ public:
   ///
   /// \returns true if the triple is little endian, false otherwise.
   bool isLittleEndian() const;
+
+  /// Test whether target triples are compatible.
+  bool isCompatibleWith(const Triple &Other) const;
+
+  /// Merge target triples.
+  std::string merge(const Triple &Other) const;
 
   /// @}
   /// @name Static helpers for IDs.
