bb0950d90e6a5efd3ed6038ef99e207be33bc774
Aaron En Ye Shi <Aaronenye.Shi@amd.com>
2017-06-21 20:12:22 +0000

    SWDEV-123226 - Fix implementation of generic address space to 0 for HCC in rocdl
    Change-Id: I9b801ad2eea5e9e88c7c51e6a686d87d7ee32ac4

--- amd/device-libs/utils/change-addr-space.sh
+++ amd/device-libs/utils/change-addr-space.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
 # Usage:
-# utils/change-addr-space.sh   : apply utils/add_amdgiz.sed
+# utils/change-addr-space.sh src  : apply utils/add_amdgiz.sed
 #                                adopt generic address space is address space 0
-# utils/change-addr-space.sh x : apply utils/remove_amdgiz.sed
+# utils/change-addr-space.sh src x : apply utils/remove_amdgiz.sed
 #                                adopt generic address space is address space 4
 
-if [ $# -lt 1 ]; then
-  find . -name "*.ll" | xargs sed -i -f "utils/add_amdgiz.sed"
+if [ $# -lt 2 ]; then
+  find . -name "*.ll" | xargs sed -i -f "$1/add_amdgiz.sed"
 else
-  find . -name "*.ll" | xargs sed -i -f "utils/remove_amdgiz.sed"
+  find . -name "*.ll" | xargs sed -i -f "$1/remove_amdgiz.sed"
 fi
-
