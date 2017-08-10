5480dba1c17059c51fc9c33d1ebc41a3dcd251c2
Ben Langmuir <blangmuir@apple.com>
2017-08-10 13:34:52 -0700

    [index] Fix a crash indexing a non-canonical template declaration
    rdar://problem/33811115
    
    apple-llvm-split-commit: 549cfcef848556cc1f938766fb8e6dc31425a94d
    apple-llvm-split-dir: clang/

--- clang/test/Index/Store/record-hash-crash.cpp
+++ clang/test/Index/Store/record-hash-crash.cpp
@@ -10,3 +10,22 @@ namespace crash1 {
 // CHECK: [[@LINE+1]]:6 | function/C
 auto getit() { return []() {}; }
 }
+
+namespace crash2 {
+// CHECK: [[@LINE+2]]:7 | class(Gen)/C++ | c:@N@crash2@ST>1#T@Foo | Ref,RelCont | rel: 1
+template <typename T>
+class Foo; // canonical decl
+
+// CHECK: [[@LINE+2]]:7 | class(Gen)/C++ | c:@N@crash2@ST>1#T@Foo | Def,RelChild | rel: 1
+template <typename T>
+class Foo {};
+
+// CHECK: [[@LINE+2]]:8 | struct(Gen)/C++ | c:@N@crash2@ST>1#t>1#pT@Wrapper | Def,RelChild | rel: 1
+template <template <typename... ARGS> class TYPE>
+struct Wrapper {};
+
+// CHECK: [[@LINE+3]]:8 | struct(Gen,TS)/C++ | c:@N@crash2@S@Wrapper>#@N@crash2@ST>1#T@Foo | Def,RelChild,RelSpecialization | rel: 2
+// CHECK:	RelSpecialization | c:@N@crash2@ST>1#t>1#pT@Wrapper
+template <>
+struct Wrapper<Foo> {};
+}
