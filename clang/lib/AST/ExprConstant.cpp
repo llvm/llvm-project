cba3ce1350b9b613849482086618a2b98e1b4a64
Hans Wennborg <hans@hanshq.net>
2017-08-18 20:28:06 +0000

    Merging r311115: ------------------------------------------------------------------------ r311115 | rsmith | 2017-08-17 12:35:50 -0700 (Thu, 17 Aug 2017) | 2 lines
    PR34161: support evaluation of 'void()' expressions in C++14 onwards.
    
    ------------------------------------------------------------------------
    
    llvm-svn: 311194

--- clang/lib/AST/ExprConstant.cpp
+++ clang/lib/AST/ExprConstant.cpp
@@ -9788,6 +9788,8 @@ public:
 
   bool Success(const APValue &V, const Expr *e) { return true; }
 
+  bool ZeroInitialization(const Expr *E) { return true; }
+
   bool VisitCastExpr(const CastExpr *E) {
     switch (E->getCastKind()) {
     default:
