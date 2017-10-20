cf407202585f4374422482cf31000082e7eb468e
Volodymyr Sapsai <vsapsai@apple.com>
2017-10-20 13:24:31 -0700

    [CodeGen] Fix assertion for structs with flexible array members as Obj-C ivars.
    Partial fix for rdar://problem/21054495 Fixes assertion only and doesn't
    add diagnostic to make flexible array members safer. Full implementation
    is pending review.
    
    apple-llvm-split-commit: 76e580ab1c32495ae524bbced52af1e63540836b
    apple-llvm-split-dir: clang/

---
+++ clang/test/CodeGenObjC/ivar-layout-flexible-array.m
@@ -0,0 +1,16 @@
+// RUN: %clang_cc1 -triple x86_64-apple-darwin -Wno-objc-root-class -fobjc-arc -emit-llvm -o - %s | FileCheck %s
+
+// rdar://problem/21054495
+struct Packet {
+  int size;
+  char data[];
+};
+
+@interface VariableSizeIvar {
+  struct Packet flexible_struct;
+}
+@end
+@implementation VariableSizeIvar
+@end
+// CHECK: @OBJC_METH_VAR_NAME_{{.*}} = private unnamed_addr constant {{.*}} c"flexible_struct\00"
+// CHECK-NEXT: @OBJC_METH_VAR_TYPE_{{.*}} = private unnamed_addr constant {{.*}} c"{Packet=\22size\22i\22data\22[0c]}\00"
