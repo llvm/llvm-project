de323bcf6979a05d91577eaa2c59d6f4f7e083fb
Adrian Prantl <aprantl@apple.com>
2017-10-24 22:55:12 +0000

    Implement salavageDebugInfo functionality for SelectionDAG.
    Similar to how llvm::salvagDebugInfo hooks into InstCombine, this adds
    a hook that can be invoked before an SDNode that is associated with an
    SDDbgValue is erased to capture the effect of the deleted node in a
    DIExpression.
    
    The motivating example is an SDDebugValue attached to an ADD operation
    that gets folded into a LOAD+OFFSET operation.
    
    rdar://problem/32121503
    
    git-svn-id: https://llvm.org/svn/llvm-project/llvm/trunk@316525 91177308-0d34-0410-b5e6-96231b3b80d8
    (cherry picked from commit 4a39ef301ec40e4ee6182868df37ee2b379ad289)
    
    apple-llvm-split-commit: 3056cc66e0c12ccee2fa2278a72301ef5fca39cc
    apple-llvm-split-dir: llvm/

--- llvm/include/llvm/CodeGen/SelectionDAG.h
+++ llvm/include/llvm/CodeGen/SelectionDAG.h
@@ -1280,6 +1280,10 @@ public:
     return DbgInfo->ByvalParmDbgEnd();
   }
 
+  /// To be invoked on an SDNode that is slated to be erased. This
+  /// function mirrors \c llvm::salvageDebugInfo.
+  void salvageDebugInfo(SDNode &N);
+
   void dump() const;
 
   /// Create a stack temporary, suitable for holding the specified value type.
