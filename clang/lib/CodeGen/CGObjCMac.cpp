c522f7f91955025e9a0099b176ebe7755662495c
Volodymyr Sapsai <vsapsai@apple.com>
2017-10-26 18:21:10 -0700

    [CodeGen] Backport fix for structs with flexible array members as Obj-C ivars.
    Follow up to insufficient fix 76e580ab1c32495ae524bbced52af1e63540836b.
    This one handles typedefs and nested arrays.
    
    rdar://problem/21054495
    
    apple-llvm-split-commit: 8ad85923d78e3900ee491c3c572037de4409dbeb
    apple-llvm-split-dir: clang/

--- clang/lib/CodeGen/CGObjCMac.cpp
+++ clang/lib/CodeGen/CGObjCMac.cpp
@@ -5084,14 +5084,14 @@ void IvarLayoutBuilder::visitField(const FieldDecl *field,
 
   // Drill down into arrays.
   uint64_t numElts = 1;
+  if (auto arrayType = CGM.getContext().getAsIncompleteArrayType(fieldType)) {
+    numElts = 0;
+    fieldType = arrayType->getElementType();
+  }
+  // Unlike incomplete arrays, constant arrays can be nested.
   while (auto arrayType = CGM.getContext().getAsConstantArrayType(fieldType)) {
     numElts *= arrayType->getSize().getZExtValue();
     fieldType = arrayType->getElementType();
-  }
-
-  if (isa<IncompleteArrayType>(fieldType)) {
-    numElts = 0;
-    fieldType = fieldType->getAsArrayTypeUnsafe()->getElementType();
   }
 
   assert(!fieldType->isArrayType() && "ivar of non-constant array type?");
