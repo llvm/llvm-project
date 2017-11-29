fbef59c233c5054f04c4801bf2967fee6379352c
Adrian Prantl <aprantl@apple.com>
2017-11-29 01:12:22 +0000

    llvm-dwarfdump: honor the --show-children option when dumping a specific DIE.
    git-svn-id: https://llvm.org/svn/llvm-project/llvm/trunk@319271 91177308-0d34-0410-b5e6-96231b3b80d8
    (cherry picked from commit c97df2361b242a5f00ade73e1820f0cbc39ba99b)
    
    apple-llvm-split-commit: 9e5d4b6e3f76df25e9a2a6ab6d82fc728f1bc7ea
    apple-llvm-split-dir: llvm/

--- llvm/lib/DebugInfo/DWARF/DWARFDie.cpp
+++ llvm/lib/DebugInfo/DWARF/DWARFDie.cpp
@@ -493,7 +493,7 @@ void DWARFDie::dump(raw_ostream &OS, unsigned Indent,
         }
 
         DWARFDie child = getFirstChild();
-        if (DumpOpts.RecurseDepth > 0 && child) {
+        if (DumpOpts.ShowChildren && DumpOpts.RecurseDepth > 0 && child) {
           DumpOpts.RecurseDepth--;
           while (child) {
             child.dump(OS, Indent+2, DumpOpts);
