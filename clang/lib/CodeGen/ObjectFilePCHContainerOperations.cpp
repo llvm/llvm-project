7aa59cea552dee0c8e7455910bb500206cfedbd7
Hans Wennborg <hans@hanshq.net>
2018-01-16 14:55:42 +0000

    Merging r321754: ------------------------------------------------------------------------ r321754 | adrian | 2018-01-03 11:10:21 -0800 (Wed, 03 Jan 2018) | 9 lines
    -gmodules: Emit debug info for implicit module imports via #include.
    
    When a type is only used as a template parameter and that type is the
    only type imported from another #include'd module, no skeleton CU for
    that module is generated, so a consumer doesn't know where to find the
    type definition. By emitting an import declaration, we can force a
    skeleton CU to be generated for each imported module.
    
    rdar://problem/36266156
    ------------------------------------------------------------------------
    
    llvm-svn: 322557

--- clang/lib/CodeGen/ObjectFilePCHContainerOperations.cpp
+++ clang/lib/CodeGen/ObjectFilePCHContainerOperations.cpp
@@ -229,6 +229,11 @@ public:
       Builder->getModuleDebugInfo()->completeRequiredType(RD);
   }
 
+  void HandleImplicitImportDecl(ImportDecl *D) override {
+    if (!D->getImportedOwningModule())
+      Builder->getModuleDebugInfo()->EmitImportDecl(*D);
+  }
+
   /// Emit a container holding the serialized AST.
   void HandleTranslationUnit(ASTContext &Ctx) override {
     assert(M && VMContext && Builder);
