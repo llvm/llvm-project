7c8ea12e40778c3c52c77ad4b3a3e3412ccb4844
Davide Italiano <ditaliano@apple.com>
2018-01-17 18:16:22 -0800

    [Swift/tests] Fix more test failures.
    apple-llvm-split-commit: b76faf207cd1f45cf6f4e920ff05332c268ac324
    apple-llvm-split-dir: lldb/

--- lldb/packages/Python/lldbsuite/test/lang/swift/foundation_value_types/notification/TestSwiftFoundationTypeNotification.py
+++ lldb/packages/Python/lldbsuite/test/lang/swift/foundation_value_types/notification/TestSwiftFoundationTypeNotification.py
@@ -13,6 +13,4 @@ import lldbsuite.test.lldbinline as lldbinline
 import lldbsuite.test.decorators as decorators
 
 lldbinline.MakeInlineTest(
-    __file__, globals(), decorators=[
-        decorators.expectedFailureDarwin("rdar://35650693"),
-        decorators.skipUnlessDarwin])
+    __file__, globals(), decorators=[decorators.skipUnlessDarwin])
