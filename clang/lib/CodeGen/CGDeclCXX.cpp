d9c670c1b94b33b8317f463609431d34ad924428
TB Schardl <neboat@mit.edu>
2018-01-18 23:26:47 +0000

    [CilkSanitizer] Added function attribute for Cilk sanitizer.

--- clang/lib/CodeGen/CGDeclCXX.cpp
+++ clang/lib/CodeGen/CGDeclCXX.cpp
@@ -379,7 +379,7 @@ llvm::Function *CodeGenModule::CreateGlobalInitOrDestructFunction(
 
   if (getLangOpts().Sanitize.has(SanitizerKind::Cilk) &&
       !isInSanitizerBlacklist(SanitizerKind::Cilk, Fn, Loc))
-    Fn->addFnAttr(llvm::Attribute::SanitizeThread);
+    Fn->addFnAttr(llvm::Attribute::SanitizeCilk);
 
   if (getLangOpts().Sanitize.has(SanitizerKind::Memory) &&
       !isInSanitizerBlacklist(SanitizerKind::Memory, Fn, Loc))
