108334311b05f676dffe1e5e3ef617007f3f4714
TB Schardl <neboat@mit.edu>
2018-01-30 22:18:56 -0500

    [TRE] Fixed bug where TRE would not replace a removed sync if it failed to eliminate a recursive tail.

--- llvm/lib/Transforms/Scalar/TailRecursionElimination.cpp
+++ llvm/lib/Transforms/Scalar/TailRecursionElimination.cpp
@@ -797,6 +797,8 @@ static bool foldReturnAndProcessPred(
       getReturnBlocksToSync(OldEntryBlock, SI, ReturnBlocksToSync);
 
       // Remove the sync.
+      DEBUG(dbgs() << "FOLDING: " << *BB
+            << "INTO SYNC PRED: " << *Pred);
       ReturnInst *RI = FoldReturnIntoUncondBranch(Ret, BB, Pred);
 
       // Cleanup: if all predecessors of BB have been eliminated by
@@ -824,6 +826,12 @@ static bool foldReturnAndProcessPred(
                               SyncInst::Create(NewRetBlock, SyncRegion));
         }
         Change = true;
+      } else {
+        // Restore the sync that was eliminated.
+        BasicBlock *RetBlock = RI->getParent();
+        BasicBlock *NewRetBlock = SplitBlock(RetBlock, RI);
+        ReplaceInstWithInst(RetBlock->getTerminator(),
+                            SyncInst::Create(NewRetBlock, SyncRegion));
       }
     }
   }
