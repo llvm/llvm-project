982f33e1c2e6b9eed166f67122375a0ff8638d2d
Vedant Kumar <vsk@apple.com>
2018-02-01 12:50:58 -0800

    [test] Create the `swiftpr` test category and populate it
    This test category is for Swift smoke tests run during pull-request
    testing. It must only contain reliable, swift-related tests: failing,
    expected-failing, unexpected-failing, or other flaky tests do not belong
    in this category.
    
    Initial results:
    
    ./swift/utils/build-script --release --skip-build-benchmarks
    --skip-test-cmark --skip-test-swift --lldb --lldb-use-system-debugserver
    --lldb-build-with-cmake --test --lldb-test-swift-only
    
    Test Methods:        256
    Reruns:                0
    Success:              29
    Expected Failure:      0
    Failure:               0
    Error:                 0
    Exceptional Exit:      0
    Unexpected Success:    0
    Skip:                227
    Timeout:               0
    Expected Timeout:      0
    
    apple-llvm-split-commit: eec829ecbd16c64480e75835cb12f84c9bdb60bc
    apple-llvm-split-dir: lldb/

--- lldb/packages/Python/lldbsuite/test/lang/swift/variables/class/TestSwiftClassTypes.py
+++ lldb/packages/Python/lldbsuite/test/lang/swift/variables/class/TestSwiftClassTypes.py
@@ -25,6 +25,7 @@ class TestSwiftClassTypes(TestBase):
     mydir = TestBase.compute_mydir(__file__)
 
     @decorators.swiftTest
+    @decorators.add_test_categories(["swiftpr"])
     def test_swift_class_types(self):
         """Test swift Class types"""
         self.build()
