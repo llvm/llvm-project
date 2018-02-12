b6aa485a235264bd9fc4223e2c9ed9233d91daeb
Jonas Devlieghere <jonas@devlieghere.com>
2018-02-12 14:48:30 +0000

    Re-enable TestSwiftMixAnyObjectType
    apple-llvm-split-commit: 92a672d050fc6a503ef19050396fcd04d4cbe7b9
    apple-llvm-split-dir: lldb/

--- lldb/packages/Python/lldbsuite/test/lang/swift/mix_any_object/TestSwiftMixAnyObjectType.py
+++ lldb/packages/Python/lldbsuite/test/lang/swift/mix_any_object/TestSwiftMixAnyObjectType.py
@@ -26,7 +26,6 @@ class TestSwiftMixAnyObjectType(TestBase):
 
     @decorators.skipUnlessDarwin
     @decorators.swiftTest
-    @decorators.expectedFailureDarwin("rdar://35650693")
     def test_any_object_type(self):
         """Test the AnyObject type in different combinations"""
         self.build()
