cab823e4e407ae3faacab39947ceee08ccbf82ee
Artem Dergachev <artem.dergachev@gmail.com>
2018-02-15 19:34:19 +0000

    [analyzer] Suppress temporary destructors for temporary arrays.
    Array destructors, like constructors, need to be called for each element of the
    array separately. We do not have any mechanisms to do this in the analyzer,
    so for now all we do is evaluate a single constructor or destructor
    conservatively and give up. It automatically causes the necessary invalidation
    and pointer escape for the whole array, because this is how RegionStore works.
    
    Implement this conservative behavior for temporary destructors. This fixes the
    crash on the provided test.
    
    Differential Revision: https://reviews.llvm.org/D43149
    
    
    git-svn-id: https://llvm.org/svn/llvm-project/cfe/trunk@325286 91177308-0d34-0410-b5e6-96231b3b80d8
    apple-llvm-split-commit: 98afdbe50b2e2897cf8ef3c01026edf27a9e7929
    apple-llvm-split-dir: clang/

--- clang/lib/StaticAnalyzer/Core/ExprEngine.cpp
+++ clang/lib/StaticAnalyzer/Core/ExprEngine.cpp
@@ -990,9 +990,22 @@ void ExprEngine::ProcessTemporaryDtor(const CFGTemporaryDtor D,
 
   EvalCallOptions CallOpts;
   CallOpts.IsTemporaryCtorOrDtor = true;
-  if (!MR)
+  if (!MR) {
     CallOpts.IsCtorOrDtorWithImproperlyModeledTargetRegion = true;
-  VisitCXXDestructor(varType, MR, D.getBindTemporaryExpr(),
+
+    // If we have no MR, we still need to unwrap the array to avoid destroying
+    // the whole array at once. Regardless, we'd eventually need to model array
+    // destructors properly, element-by-element.
+    while (const ArrayType *AT = getContext().getAsArrayType(T)) {
+      T = AT->getElementType();
+      CallOpts.IsArrayCtorOrDtor = true;
+    }
+  } else {
+    // We'd eventually need to makeZeroElementRegion() trick here,
+    // but for now we don't have the respective construction contexts,
+    // so MR would always be null in this case. Do nothing for now.
+  }
+  VisitCXXDestructor(T, MR, D.getBindTemporaryExpr(),
                      /*IsBase=*/false, CleanPred, Dst, CallOpts);
 }
 
