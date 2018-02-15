c80e9b71d9e341d68d8fb8a65f261be1a542c064
Vedant Kumar <vsk@apple.com>
2018-02-15 19:13:03 +0000

    [Utils] salvageDI: Add a comment and move a call earlier, NFC
    git-svn-id: https://llvm.org/svn/llvm-project/llvm/trunk@325280 91177308-0d34-0410-b5e6-96231b3b80d8
    (cherry picked from commit 09cae80a9d698ce693ed0ce0d20fbbe3f475683c)
    
    apple-llvm-split-commit: b1679723434856dcfdffd6b54e384cf608fcaec8
    apple-llvm-split-dir: llvm/

--- llvm/lib/Transforms/Utils/Local.cpp
+++ llvm/lib/Transforms/Utils/Local.cpp
@@ -1411,6 +1411,7 @@ void llvm::salvageDebugInfo(Instruction &I) {
     return;
 
   auto &M = *I.getModule();
+  auto &DL = M.getDataLayout();
 
   auto wrapMD = [&](Value *V) {
     return MetadataAsValue::get(I.getContext(), ValueAsMetadata::get(V));
@@ -1438,7 +1439,7 @@ void llvm::salvageDebugInfo(Instruction &I) {
   };
 
   if (auto *CI = dyn_cast<CastInst>(&I)) {
-    if (!CI->isNoopCast(M.getDataLayout()))
+    if (!CI->isNoopCast(DL))
       return;
 
     // No-op casts are irrelevant for debug info.
@@ -1458,6 +1459,7 @@ void llvm::salvageDebugInfo(Instruction &I) {
       for (auto *DII : DbgUsers)
         applyOffset(DII, Offset.getSExtValue());
   } else if (auto *BI = dyn_cast<BinaryOperator>(&I)) {
+    // Rewrite binary operations with constant integer operands.
     auto *ConstInt = dyn_cast<ConstantInt>(I.getOperand(1));
     if (!ConstInt || ConstInt->getBitWidth() > 64)
       return;
