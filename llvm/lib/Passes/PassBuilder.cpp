c65d753a0f609bfacb5ed2bad3f0c7befbc3bba8
Vedant Kumar <vsk@apple.com>
2018-03-12 20:49:42 +0000

    Remove the LoopInstSimplify pass (-loop-instsimplify)
    LoopInstSimplify is unused and untested. Reading through the commit
    history the pass also seems to have a high maintenance burden.
    
    It would be best to retire the pass for now. It should be easy to
    recover if we need something similar in the future.
    
    Differential Revision: https://reviews.llvm.org/D44053
    
    git-svn-id: https://llvm.org/svn/llvm-project/llvm/trunk@327329 91177308-0d34-0410-b5e6-96231b3b80d8
    (cherry picked from commit f7b39ab9dfae95ca7b96648ee80f2e8f52294fa3)
    
    apple-llvm-split-commit: d84a47848721974c8fde0912920f106b1f072951
    apple-llvm-split-dir: llvm/

--- llvm/lib/Passes/PassBuilder.cpp
+++ llvm/lib/Passes/PassBuilder.cpp
@@ -108,7 +108,6 @@
 #include "llvm/Transforms/Scalar/LoopDeletion.h"
 #include "llvm/Transforms/Scalar/LoopDistribute.h"
 #include "llvm/Transforms/Scalar/LoopIdiomRecognize.h"
-#include "llvm/Transforms/Scalar/LoopInstSimplify.h"
 #include "llvm/Transforms/Scalar/LoopLoadElimination.h"
 #include "llvm/Transforms/Scalar/LoopPassManager.h"
 #include "llvm/Transforms/Scalar/LoopPredication.h"
@@ -383,9 +382,8 @@ PassBuilder::buildFunctionSimplificationPipeline(OptimizationLevel Level,
   // FIXME: Currently this is split into two loop pass pipelines because we run
   // some function passes in between them. These can and should be replaced by
   // loop pass equivalenst but those aren't ready yet. Specifically,
-  // `SimplifyCFGPass` and `InstCombinePass` are used. We have
-  // `LoopSimplifyCFGPass` which isn't yet powerful enough, and the closest to
-  // the other we have is `LoopInstSimplify`.
+  // `SimplifyCFGPass` and `InstCombinePass` are used. We just have
+  // `LoopSimplifyCFGPass` which isn't yet powerful enough.
   LoopPassManager LPM1(DebugLogging), LPM2(DebugLogging);
 
   // Rotate Loop - disable header duplication at -Oz
