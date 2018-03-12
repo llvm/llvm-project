8a7976ef5651b1328f897b4d23864c268690ec73
Bjorn Pettersson <bjorn.a.pettersson@ericsson.com>
2018-03-12 18:02:39 +0000

    [SelectionDAG] Improve handling of dangling debug info
    Summary:
    1) Make sure to discard dangling debug info if the variable (or
    variable fragment) is mapped to something new before we had a
    chance to resolve the dangling debug info.
    
    2) When resolving debug info, make sure to bump the associated
    SDNodeOrder to ensure that the DBG_VALUE is emitted after the
    instruction that defines the value used in the DBG_VALUE.
    This will avoid a debug-use before def scenario as seen in
    https://bugs.llvm.org/show_bug.cgi?id=36417.
    
    The new test case, test/DebugInfo/X86/sdag-dangling-dbgvalue.ll,
    show some other limitations in how dangling debug info is
    handled in the SelectionDAG. Since we currently only support
    having one dangling dbg.value per Value, we will end up dropping
    debug info when there are more than one variable that is described
    by the same "dangling value".
    
    Reviewers: aprantl
    
    Reviewed By: aprantl
    
    Subscribers: aprantl, eraman, llvm-commits, JDevlieghere
    
    Tags: #debug-info
    
    Differential Revision: https://reviews.llvm.org/D44369
    
    git-svn-id: https://llvm.org/svn/llvm-project/llvm/trunk@327303 91177308-0d34-0410-b5e6-96231b3b80d8
    (cherry picked from commit 475569518b9d6e3cdd19496dfa3a91156ba37d7a)
    
    apple-llvm-split-commit: 0b78f0a11d03f933617c16d08aa70f0b48eed369
    apple-llvm-split-dir: llvm/

--- llvm/lib/CodeGen/AsmPrinter/CodeViewDebug.cpp
+++ llvm/lib/CodeGen/AsmPrinter/CodeViewDebug.cpp
@@ -1100,7 +1100,7 @@ void CodeViewDebug::calculateRanges(
       auto J = std::next(I);
       const DIExpression *DIExpr = DVInst->getDebugExpression();
       while (J != E &&
-             !fragmentsOverlap(DIExpr, J->first->getDebugExpression()))
+             !DIExpr->fragmentsOverlap(J->first->getDebugExpression()))
         ++J;
       if (J != E)
         End = getLabelBeforeInsn(J->first);
