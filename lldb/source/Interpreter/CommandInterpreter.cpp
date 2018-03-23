68c97f63a4d292da64346457d87cab1d810a9794
Davide Italiano <davide@freebsd.org>
2018-03-23 21:55:48 +0000

    [Commands] Add a (currently empty) `stats` command.
    This one will be used to print statistics about lldb sessions
    (including, e.g. number of expression evaluation succeeded or
    failed). I decided to commit the skeleton first so that we have
    a clean reference on how a command should be implemented.
    My future commits are going to populate this command and test
    it.
    
    <rdar://problem/36555975>
    
    git-svn-id: https://llvm.org/svn/llvm-project/lldb/trunk@328378 91177308-0d34-0410-b5e6-96231b3b80d8
    apple-llvm-split-commit: 5f7bf25e0eb1a5e6ee1899cc3bae3cfa35be5622
    apple-llvm-split-dir: lldb/

--- lldb/source/Interpreter/CommandInterpreter.cpp
+++ lldb/source/Interpreter/CommandInterpreter.cpp
@@ -35,6 +35,7 @@
 #include "../Commands/CommandObjectSettings.h"
 #include "../Commands/CommandObjectSource.h"
 #include "../Commands/CommandObjectSyntax.h"
+#include "../Commands/CommandObjectStats.h"
 #include "../Commands/CommandObjectTarget.h"
 #include "../Commands/CommandObjectThread.h"
 #include "../Commands/CommandObjectType.h"
@@ -430,6 +431,7 @@ void CommandInterpreter::LoadCommandDictionary() {
       CommandObjectSP(new CommandObjectMultiwordSettings(*this));
   m_command_dict["source"] =
       CommandObjectSP(new CommandObjectMultiwordSource(*this));
+  m_command_dict["stats"] = CommandObjectSP(new CommandObjectStats(*this));
   m_command_dict["target"] =
       CommandObjectSP(new CommandObjectMultiwordTarget(*this));
   m_command_dict["thread"] =
