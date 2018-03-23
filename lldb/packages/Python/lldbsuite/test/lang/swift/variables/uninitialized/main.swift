67f5aeb9ac4f6f1171eed774fd7b09b3b93418c0
Jim Ingham <jingham@apple.com>
2018-03-23 12:12:26 -0700

    Add a sanity check to the swift Native Hashed Container data formatter.
    This checks that we can read the used items bitmask, and if we can't we
    don't try to construct children for the container.  The variable can't
    be valid if this is not initialized.
    
    <rdar://problem/37475403>
    
    apple-llvm-split-commit: 1cc3e260edd09376cb56f5df8be1853ea0173592
    apple-llvm-split-dir: lldb/

--- lldb/packages/Python/lldbsuite/test/lang/swift/variables/uninitialized/main.swift
+++ lldb/packages/Python/lldbsuite/test/lang/swift/variables/uninitialized/main.swift
@@ -28,6 +28,8 @@ struct A<T> {
     }
 
     mutating func a() -> T? {
+        var adict : [String: Any] = ["key1" : "val1", "key2" : 9999] //% self.expect("frame var adict", "Frame var of uninitialized variable returns.")
+        adict["key3"] = 1.0
         var t : T? = nil
         let c = cs[0]
 
@@ -35,7 +37,7 @@ struct A<T> {
         let k2 = b(t:c) //% self.expect("expression -- c", "Unreadable variable is ignored", substrs = ["= 3"])
         let k3 = b(t:c)
 
-        if let maybeT = process(i:0, k1:k1, k2:k2, k3:k3) {
+        if let maybeT = process(i : adict.count, k1:k1, k2:k2, k3:k3) {
             t = maybeT
         }
 
