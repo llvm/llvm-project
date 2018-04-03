ac91185ab0e77a9c77d2c92cfbee8a7f5f6abde3
Davide Italiano <ditaliano@apple.com>
2018-04-03 15:49:01 -0700

    [SwiftLanguageRuntime] Fix printing of __NSCFString.
    The internal store went from being a Swift wrapped string:
    Swift._NSContiguousString to just a straight up __NSCFString.
    
    This was broken after the recent String changes, but we
    didn't catch this because this test wasn't run as part
    of the PR workflow. Eventually we won't need to care about
    these details in lldb as everything will be hidden inside
    RemoteAST, but for now, we need to fix this case.
    
    <rdar://problem/39006341>
    
    (cherry picked from commit 09526a948191c0d3b1a8efa363bc14797d531dea)
    
    apple-llvm-split-commit: 9c6fa07cbcdd09da2dac086b24c7ab3b84da1f7f
    apple-llvm-split-dir: lldb/

--- lldb/packages/Python/lldbsuite/test/lang/swift/nsarray_code_running_formatter/main.swift
+++ lldb/packages/Python/lldbsuite/test/lang/swift/nsarray_code_running_formatter/main.swift
@@ -24,7 +24,7 @@ func main() {
 
   print("second stop") //% self.expect('frame variable -d run -- t', substrs=['t = 0x', 'NSArray = {', 'NSObject = {'])
                        //% self.expect('frame variable -d run -- ta', substrs=['ta = {', '_buffer = {', '_storage =', 'rawValue = 0x'])
-                       //% self.expect('frame variable -d run -- tb', substrs=['tb = 1 value {', '[0] = "abc"'])
+                       //% self.expect('frame variable -d run -- tb', substrs=['tb = 1 value {', '"abc"'])
                        //% self.expect('po t', substrs=['0 : abc'])
                        //% self.expect('po ta', substrs=['0 : abc'])
                        //% self.expect('po tb', substrs=['0 : abc'])
