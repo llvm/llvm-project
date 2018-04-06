d69d07374043a565f9a8d10c3d4ab376d6f8beef
Davide Italiano <ditaliano@apple.com>
2018-04-06 13:46:55 -0700

    [TestCPPExceptionInREPL] Make the test working on Linux (and UNXFAIL).
    <rdar://problem/39245862>
    
    apple-llvm-split-commit: ef9c292e586da0b19b27cdbaeddfbe089247138c
    apple-llvm-split-dir: lldb/

--- lldb/packages/Python/lldbsuite/test/repl/cpp_exceptions/TestCPPExceptionsInREPL.py
+++ lldb/packages/Python/lldbsuite/test/repl/cpp_exceptions/TestCPPExceptionsInREPL.py
@@ -9,7 +9,6 @@ import os
 import time
 import re
 import lldb
-from lldbsuite.test.decorators import *
 import lldbsuite.test.lldbutil as lldbutil
 from lldbsuite.test.lldbtest import *
 
@@ -23,7 +22,6 @@ class TestREPLExceptions(TestBase):
     # each debug info format.
     NO_DEBUG_INFO_TESTCASE = True
 
-    @skipIfLinux # <rdar://problem/39245862>
     def test_repl_exceptions(self):
         self.build()
         self.main_source_file = lldb.SBFileSpec("main.swift")
