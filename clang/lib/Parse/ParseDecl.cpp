cc1ac2b96f9d8dcbc7c5026ac07e0944be060498
Volodymyr Sapsai <vsapsai@apple.com>
2018-04-10 18:29:47 +0000

    [Parser] Fix assertion-on-invalid for unexpected typename.
    In `ParseDeclarationSpecifiers` for the code
    
        class A typename A;
    
    we were able to annotate token `kw_typename` because it refers to
    existing type. But later during processing token `annot_typename` we
    failed to `SetTypeSpecType` and exited switch statement leaving
    annotation token unconsumed. The code after the switch statement failed
    because it didn't expect a special token.
    
    The fix is not to assume that switch statement consumes all special
    tokens and consume any token, not just non-special.
    
    rdar://problem/37099386
    
    Reviewers: rsmith, arphaman
    
    Reviewed By: rsmith
    
    Subscribers: jkorous-apple, cfe-commits
    
    Differential Revision: https://reviews.llvm.org/D44449
    
    git-svn-id: https://llvm.org/svn/llvm-project/cfe/trunk@329735 91177308-0d34-0410-b5e6-96231b3b80d8
    (cherry picked from commit 344b6ca4d1b264b8bc2abf5eb91b52713d663a9d)
    
    apple-llvm-split-commit: ec14c39a21f7934ccda60ee84cf751e6b83641ee
    apple-llvm-split-dir: clang/

--- clang/lib/Parse/ParseDecl.cpp
+++ clang/lib/Parse/ParseDecl.cpp
@@ -3831,7 +3831,8 @@ void Parser::ParseDeclarationSpecifiers(DeclSpec &DS,
 
     DS.SetRangeEnd(Tok.getLocation());
     if (DiagID != diag::err_bool_redeclaration)
-      ConsumeToken();
+      // After an error the next token can be an annotation token.
+      ConsumeAnyToken();
 
     AttrsLastTime = false;
   }
