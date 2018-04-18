98d8c39091a5253664120bd058007bac1820b63d
Bjorn Pettersson <bjorn.a.pettersson@ericsson.com>
2018-04-18 08:08:04 +0000

    [DebugInfo] Sink related dbg users when sinking in InstCombine
    Summary:
    When sinking an instruction in InstCombine we now also sink
    the DbgInfoIntrinsics that are using the sunken value.
    
    Example)
    
    When sinking the load in this input
    
    bb.X:
      %0 = load i64, i64* %start, align 4, !dbg !31
      tail call void @llvm.dbg.value(metadata i64 %0, ...)
      br i1 %cond, label %for.end, label %for.body.lr.ph
    for.body.lr.ph:
      br label %for.body
    
    we now also move the dbg.value, like this
    
    bb.X:
      br i1 %cond, label %for.end, label %for.body.lr.ph
    for.body.lr.ph:
      %0 = load i64, i64* %start, align 4, !dbg !31
      tail call void @llvm.dbg.value(metadata i64 %0, ...)
      br label %for.body
    
    In the past we haven't moved the dbg.value so we got
    
    bb.X:
      tail call void @llvm.dbg.value(metadata i64 %0, ...)
      br i1 %cond, label %for.end, label %for.body.lr.ph
    for.body.lr.ph:
      %0 = load i64, i64* %start, align 4, !dbg !31
      br label %for.body
    
    So in the past we got a debug-use before the def of %0.
    And that dbg.value was also on the path jumping to %for.end, for
    which %0 never was defined.
    
    CodeGenPrepare normally comes to rescue later (when not moving
    the dbg.value), since it moves dbg.value instrinsics quite
    brutally, without really analysing if it is correct to move
    the intrinsic (see PR31878).
    So at the moment this patch isn't expected to have much impact,
    besides that it is moving the dbg.value already in opt, making
    the IR look more sane directly.
    
    This can be seen as a preparation to (hopefully) make it possible
    to turn off CodeGenPrepare::placeDbgValues later as a solution
    to PR31878.
    
    I also adjusted test/DebugInfo/X86/sdagsplit-1.ll to make the
    IR in the test case up-to-date with this behavior in InstCombine.
    
    Reviewers: rnk, vsk, aprantl
    
    Reviewed By: vsk, aprantl
    
    Subscribers: mattd, JDevlieghere, llvm-commits
    
    Tags: #debug-info
    
    Differential Revision: https://reviews.llvm.org/D45425
    
    git-svn-id: https://llvm.org/svn/llvm-project/llvm/trunk@330243 91177308-0d34-0410-b5e6-96231b3b80d8
    (cherry picked from commit f31b39d494d815984ff2f7ee32a63b8f76a0f986)
    
    apple-llvm-split-commit: fb8b37f5a934ec802dc1df1ed6de20a611f68ff3
    apple-llvm-split-dir: llvm/

--- llvm/lib/Transforms/InstCombine/InstructionCombining.cpp
+++ llvm/lib/Transforms/InstCombine/InstructionCombining.cpp
@@ -2890,6 +2890,7 @@ Instruction *InstCombiner::visitLandingPadInst(LandingPadInst &LI) {
 /// block.
 static bool TryToSinkInstruction(Instruction *I, BasicBlock *DestBlock) {
   assert(I->hasOneUse() && "Invariants didn't hold!");
+  BasicBlock *SrcBlock = I->getParent();
 
   // Cannot move control-flow-involving, volatile loads, vaarg, etc.
   if (isa<PHINode>(I) || I->isEHPad() || I->mayHaveSideEffects() ||
@@ -2919,10 +2920,20 @@ static bool TryToSinkInstruction(Instruction *I, BasicBlock *DestBlock) {
       if (Scan->mayWriteToMemory())
         return false;
   }
-
   BasicBlock::iterator InsertPos = DestBlock->getFirstInsertionPt();
   I->moveBefore(&*InsertPos);
   ++NumSunkInst;
+
+  // Also sink all related debug uses from the source basic block. Otherwise we
+  // get debug use before the def.
+  SmallVector<DbgInfoIntrinsic *, 1> DbgUsers;
+  findDbgUsers(DbgUsers, I);
+  for (auto *DII : DbgUsers) {
+    if (DII->getParent() == SrcBlock) {
+      DII->moveBefore(&*InsertPos);
+      DEBUG(dbgs() << "SINK: " << *DII << '\n');
+    }
+  }
   return true;
 }
 
