89360e5124d7e7715a23d5653f359a7ec43ffcac
Frederic Riss <friss@apple.com>
2018-05-07 21:50:08 -0700

    Revert "Switch the default on "target.experimental.inject-local-vars" to false, and force the setting in the"
    This reverts commit a69f23993c6cd3334acfbd85dcf21dac0cc9f048.
    
    apple-llvm-split-commit: 524664601c9c0806028e66752ff9bebc498fc433
    apple-llvm-split-dir: lldb/

--- lldb/packages/Python/lldbsuite/test/lang/cpp/member-and-local-vars-with-same-name/TestMembersAndLocalsWithSameName.py
+++ lldb/packages/Python/lldbsuite/test/lang/cpp/member-and-local-vars-with-same-name/TestMembersAndLocalsWithSameName.py
@@ -241,8 +241,6 @@ class TestMembersAndLocalsWithSameName(TestBase):
         # Load the executable
         self.target = self.dbg.CreateTarget(exe_path)
         self.assertTrue(self.target.IsValid(), VALID_TARGET)
-        self.dbg.HandleCommand(
-            "settings set target.experimental.inject-local-vars true")
 
     def _test_globals(self):
         thread = lldbutil.get_stopped_thread(
