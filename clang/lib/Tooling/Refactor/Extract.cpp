d7dcb2d42cf0f34e219b8910d052160aa5dfe298
Alex Lorenz <arphaman@gmail.com>
2018-06-12 14:48:13 -0700

    [Refactor][Extract] Correctly emit the return type for functions to account for block extractions and copy blocks when extracting without ARC
    apple-llvm-split-commit: 0405317acbc87c4335ef4463baf220513232ed24
    apple-llvm-split-dir: clang/

--- clang/lib/Tooling/Refactor/Extract.cpp
+++ clang/lib/Tooling/Refactor/Extract.cpp
@@ -1855,20 +1855,22 @@ llvm::Expected<RefactoringResult> ExtractOperation::perform(
       OS << "static ";
     else if (!isMethodExtraction())
       OS << (isInHeader(FunctionExtractionLoc, SM) ? "inline " : "static ");
-    ReturnType.print(OS, PP);
-    OS << ' ';
+    std::string QualifiedName;
+    llvm::raw_string_ostream NameOS(QualifiedName);
     if (isMethodExtraction() && IsDefinition)
-      printEnclosingMethodScope(FunctionLikeParentDecl, OS, PP);
-    unsigned NameOffset = OS.str().size();
-    OS << ExtractedName << '(';
+      printEnclosingMethodScope(FunctionLikeParentDecl, NameOS, PP);
+    NameOS << ExtractedName;
+    NameOS << '(';
     bool IsFirst = true;
     for (const auto &Var : CapturedVariables) {
       if (!IsFirst)
-        OS << ", ";
+        NameOS << ", ";
       IsFirst = false;
-      Var.ParameterType.print(OS, PP, /*PlaceHolder=*/Var.getName());
+      Var.ParameterType.print(NameOS, PP, /*PlaceHolder=*/Var.getName());
     }
-    OS << ')';
+    NameOS << ')';
+    ReturnType.print(OS, PP, NameOS.str());
+    unsigned NameOffset = OS.str().find(ExtractedName);
     if (isMethodExtraction() && isEnclosingMethodConst(FunctionLikeParentDecl))
       OS << " const";
     return RefactoringReplacement::AssociatedSymbolLocation(
@@ -1926,7 +1928,18 @@ llvm::Expected<RefactoringResult> ExtractOperation::perform(
   auto Semicolons = computeSemicolonExtractionPolicy(
       ExtractedStmtRange ? *(ExtractedStmtRange->Last) : S, ExtractedTokenRange,
       SM, LangOpts);
+  bool ShouldCopyBlock = false;
+  if (IsExpr && !LangOpts.ObjCAutoRefCount &&
+      ReturnType->isBlockPointerType()) {
+    // We can't return local blocks directly without ARC; they should be copied.
+    // FIXME: This is overly pessimistic, as we only need the copy for local
+    // blocks.
+    ExtractedOS << "[(";
+    ShouldCopyBlock = true;
+  }
   ExtractedOS << SourceRewriter.getRewrittenText(ExtractedTokenRange);
+  if (ShouldCopyBlock)
+    ExtractedOS << ") copy]";
   if (Semicolons.IsNeededInExtractedFunction)
     ExtractedOS << ';';
   if (CanUseReturnForVariablesUsedAfterwards)
