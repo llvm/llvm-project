5bd6f8b2c7edafba00f601ad6809e36455aee326
Fred Riss <friss@apple.com>
2018-08-15 09:03:43 -0700

    XFAIL TestSwiftGenericStructDebugInfoGenericFlatMap.py as it seems falky
    rdar://problem/43335151
    
    apple-llvm-split-commit: 698229f8b1efae501b6847b80c3fc290938bb054
    apple-llvm-split-dir: lldb/

--- lldb/packages/Python/lldbsuite/test/lang/swift/variables/generic_struct_debug_info/generic_flatmap/TestSwiftGenericStructDebugInfoGenericFlatMap.py
+++ lldb/packages/Python/lldbsuite/test/lang/swift/variables/generic_struct_debug_info/generic_flatmap/TestSwiftGenericStructDebugInfoGenericFlatMap.py
@@ -13,4 +13,4 @@ import lldbsuite.test.lldbinline as lldbinline
 import lldbsuite.test.decorators as decorators
 
 lldbinline.MakeInlineTest(__file__, globals(),
-        decorators=[])
+        decorators=[lldbinline.expectedFailureAll()])
