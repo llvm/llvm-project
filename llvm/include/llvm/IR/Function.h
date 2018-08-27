21f6f5c987a5f36b442a5fbe4661d1d9e36258d6
Hans Wennborg <hans@hanshq.net>
2018-08-27 10:05:36 +0000

    Merging r340641: ------------------------------------------------------------------------ r340641 | stefanp | 2018-08-24 21:38:29 +0200 (Fri, 24 Aug 2018) | 9 lines
    [Exception Handling] Unwind tables are required for all functions that have an EH personality.
    
    This patch is for defect:
    https://bugs.llvm.org/show_bug.cgi?id=32611
    
    Functions may require unwind tables even if they are marked with the attribute
    nounwind. Any function with an EH personality may require an unwind table.
    
    Differential Revision: https://reviews.llvm.org/D50987
    ------------------------------------------------------------------------
    
    llvm-svn: 340731

--- llvm/include/llvm/IR/Function.h
+++ llvm/include/llvm/IR/Function.h
@@ -557,7 +557,7 @@ public:
 
   /// True if this function needs an unwind table.
   bool needsUnwindTableEntry() const {
-    return hasUWTable() || !doesNotThrow();
+    return hasUWTable() || !doesNotThrow() || hasPersonalityFn();
   }
 
   /// Determine if the function returns a structure through first
