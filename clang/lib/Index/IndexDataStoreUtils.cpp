3280e9ff914aee934e68a5768bbdd41d47d70697
Argyrios Kyrtzidis <kyrtzidis@apple.com>
2018-09-09 10:43:36 -0700

    [index] Some refactoring to simplify how DirectoryWatcher is used for index-store watching
    apple-llvm-split-commit: c40d07e5eeb03320bca4e636d3f14a9935791fac
    apple-llvm-split-dir: clang/

--- clang/lib/Index/IndexDataStoreUtils.cpp
+++ clang/lib/Index/IndexDataStoreUtils.cpp
@@ -9,12 +9,45 @@
 
 #include "clang/Index/IndexDataStoreSymbolUtils.h"
 #include "IndexDataStoreUtils.h"
+#include "llvm/ADT/SmallString.h"
 #include "llvm/Bitcode/BitstreamWriter.h"
+#include "llvm/Support/Path.h"
+#include "llvm/Support/raw_ostream.h"
 
 using namespace clang;
 using namespace clang::index;
 using namespace clang::index::store;
 using namespace llvm;
+
+static void appendSubDir(StringRef subdir, SmallVectorImpl<char> &StorePathBuf) {
+  SmallString<10> VersionPath;
+  raw_svector_ostream(VersionPath) << 'v' << STORE_FORMAT_VERSION;
+
+  sys::path::append(StorePathBuf, VersionPath);
+  sys::path::append(StorePathBuf, subdir);
+}
+
+void store::appendInteriorUnitPath(StringRef UnitName,
+                                   SmallVectorImpl<char> &PathBuf) {
+  sys::path::append(PathBuf, UnitName);
+}
+
+void store::appendUnitSubDir(SmallVectorImpl<char> &StorePathBuf) {
+  return appendSubDir("units", StorePathBuf);
+}
+
+void store::appendRecordSubDir(SmallVectorImpl<char> &StorePathBuf) {
+  return appendSubDir("records", StorePathBuf);
+}
+
+void store::appendInteriorRecordPath(StringRef RecordName,
+                                     SmallVectorImpl<char> &PathBuf) {
+  // To avoid putting a huge number of files into the records directory, create
+  // subdirectories based on the last 2 characters from the hash.
+  StringRef hash2chars = RecordName.substr(RecordName.size()-2);
+  sys::path::append(PathBuf, hash2chars);
+  sys::path::append(PathBuf, RecordName);
+}
 
 void store::emitBlockID(unsigned ID, const char *Name,
                         BitstreamWriter &Stream, RecordDataImpl &Record) {
