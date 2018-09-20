e77a7449e93aaf98447814d4024e9d11022a994e
Adrian Prantl <aprantl@apple.com>
2018-09-20 10:28:17 -0700

    Fix a distracting error message on Linux
    apple-llvm-split-commit: ded3011dc1a736149241767ae1f9ff9da9d2578b
    apple-llvm-split-dir: lldb/

--- lldb/packages/Python/lldbsuite/test/plugins/swift.py
+++ lldb/packages/Python/lldbsuite/test/plugins/swift.py
@@ -84,12 +84,13 @@ swift_sdk_root = None
 def getSwiftSDKRoot():
     """Returns the SDK root to be used for compiling Swift/ObjC interop code."""
     global swift_sdk_root
+    import platform
     if swift_sdk_root is None:
         if "SDKROOT" in os.environ:
             swift_sdk_root = os.environ["SDKROOT"]
         if "SWIFTSDKROOT" in os.environ:
             swift_sdk_root = os.environ["SWIFTSDKROOT"]
-        else:
+        elif platform.system() == 'Darwin':
             try:
                 sdk_path = subprocess.check_output(
                     'xcrun -sdk macosx --show-sdk-path', shell=True)
