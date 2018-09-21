c11e6348fb7a6d97254c72e860acc6d8b121ab99
Davide Italiano <ditaliano@apple.com>
2018-09-20 17:22:04 -0700

    [SwiftLanguageRuntime] Mark a test relying on foundation as darwin only.
    apple-llvm-split-commit: e4d271011ff352d4fa83c252739a71dc74a6c0d8
    apple-llvm-split-dir: lldb/

--- lldb/packages/Python/lldbsuite/test/lang/swift/protocols/class_protocol/TestClassConstrainedProtocolArgument.py
+++ lldb/packages/Python/lldbsuite/test/lang/swift/protocols/class_protocol/TestClassConstrainedProtocolArgument.py
@@ -3,5 +3,7 @@ Test that variables passed in as a class constrained protocol type
 are correctly printed.
 """
 import lldbsuite.test.lldbinline as lldbinline
+import lldbsuite.test.decorators as decorators
 
-lldbinline.MakeInlineTest(__file__, globals())
+lldbinline.MakeInlineTest(
+    __file__, globals(), decorators=[decorators.skipUnlessDarwin])
