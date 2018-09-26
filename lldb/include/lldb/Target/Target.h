654ca10dfdd4ca05d3e57e57c75f78169d4f677b
Tatyana Krasnukha <tatyana@synopsys.com>
2018-09-26 18:50:19 +0000

    Replace "nullptr-terminated" C-arrays of OptionValueEnumeration with safer llvm::ArrayRef
    Differential Revision: https://reviews.llvm.org/D49017
    
    git-svn-id: https://llvm.org/svn/llvm-project/lldb/trunk@343130 91177308-0d34-0410-b5e6-96231b3b80d8
    apple-llvm-split-commit: 093a30ddb7857e1b752f70c16e6078ccf18a8530
    apple-llvm-split-dir: lldb/

--- lldb/include/lldb/Target/Target.h
+++ lldb/include/lldb/Target/Target.h
@@ -51,7 +51,7 @@
 
 namespace lldb_private {
 
-extern OptionEnumValueElement g_dynamic_value_types[];
+OptionEnumValues GetDynamicValueTypes();
 
 typedef enum InlineStrategy {
   eInlineBreakpointsNever = 0,
