0a9b9e6746d87a4dc929be9622c0f559cb2c119b
Tom Stellard <tstellar@redhat.com>
2018-12-08 04:54:24 +0000

    Merging r345838:
    ------------------------------------------------------------------------
    r345838 | erichkeane | 2018-11-01 08:11:41 -0700 (Thu, 01 Nov 2018) | 8 lines
    
    CPU-Dispatch- Fix type of a member function, prevent deferrals
    
    The member type creation for a cpu-dispatch function was not correctly
    including the 'this' parameter, so ensure that the type is properly
    determined. Also, disable defer in the cases of emitting the functoins,
    as it can end up resulting in the wrong version being emitted.
    
    Change-Id: I0b8fc5e0b0d1ae1a9d98fd54f35f27f6e5d5d083
    ------------------------------------------------------------------------
    
    llvm-svn: 348684

---
+++ clang/test/CodeGenCXX/attr-cpuspecific.cpp
@@ -0,0 +1,17 @@
+// RUN: %clang_cc1 -triple x86_64-linux-gnu -emit-llvm -o - %s | FileCheck %s --check-prefixes=CHECK,LINUX
+
+struct S {
+  __attribute__((cpu_specific(atom)))
+  void Func(){}
+  __attribute__((cpu_dispatch(ivybridge,atom)))
+  void Func(){}
+};
+
+void foo() {
+  S s;
+  s.Func();
+}
+
+// LINUX: define void (%struct.S*)* @_ZN1S4FuncEv.resolver
+// LINUX: ret void (%struct.S*)* @_ZN1S4FuncEv.S
+// LINUX: ret void (%struct.S*)* @_ZN1S4FuncEv.O
