05d28f6df3005bf34d0bc551e5f6e7f9b005a506
Carl Ritson <carl.ritson@amd.com>
2018-12-14 10:49:16 +0000

    Do not force skip over export instruction in PS
    This is a temporary fix to avoid a breakage where the export(s) for a
    pixel shader are in control flow (due to a kill). A pixel shader must
    execute an export done, even with exec=0.
    
    A better fix would be to have a pixel-shader-only pass that inserts a
    null export done in the final basic block if there is not one already
    there.
    
    Change-Id: I22f201f95d52ff699aa8cac26bb019717b20432e

--- llvm/lib/Target/AMDGPU/SIInstrInfo.cpp
+++ llvm/lib/Target/AMDGPU/SIInstrInfo.cpp
@@ -2712,7 +2712,11 @@ bool SIInstrInfo::hasUnwantedEffectsWhenEXECEmpty(const MachineInstr &MI) const 
   //       EXEC = 0, but checking for that case here seems not worth it
   //       given the typical code patterns.
   if (Opcode == AMDGPU::S_SENDMSG || Opcode == AMDGPU::S_SENDMSGHALT ||
-      Opcode == AMDGPU::EXP || Opcode == AMDGPU::EXP_DONE ||
+      // FIXME: Skipping EXP/EXP_DONE temporarily disabled in PS.  A better fix
+      // would be to have a pixel-shader-only pass that adds an extra EXP_DONE
+      // if there is no EXP_DONE in the final basic block.
+      ((Opcode == AMDGPU::EXP || Opcode == AMDGPU::EXP_DONE) &&
+       MI.getParent()->getParent()->getFunction().getCallingConv() != CallingConv::AMDGPU_PS) ||
       Opcode == AMDGPU::DS_ORDERED_COUNT || Opcode == AMDGPU::S_TRAP ||
       Opcode == AMDGPU::DS_GWS_INIT || Opcode == AMDGPU::DS_GWS_BARRIER)
     return true;
