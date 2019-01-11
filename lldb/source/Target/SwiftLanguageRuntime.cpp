ea58606d14c5c7156af05aa513f4b518fb0d5e8e
Adrian Prantl <aprantl@apple.com>
2019-01-10 16:14:57 -0800

    Remove accidentally committed line.
    apple-llvm-split-commit: a05f97658f0eddb59370fe6a33a771f0cdd98733
    apple-llvm-split-dir: lldb/

--- lldb/source/Target/SwiftLanguageRuntime.cpp
+++ lldb/source/Target/SwiftLanguageRuntime.cpp
@@ -2042,7 +2042,6 @@ bool SwiftLanguageRuntime::GetDynamicTypeAndAddress_Promise(
     address.SetLoadAddress(val_ptr_addr, &m_process->GetTarget());
     return true;
   } break;
-  case swift::MetadataKind::Function:
   case swift::MetadataKind::Optional:
   case swift::MetadataKind::Struct:
   case swift::MetadataKind::Tuple: {
