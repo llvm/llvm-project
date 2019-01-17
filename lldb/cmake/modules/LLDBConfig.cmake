cc38240ceec6bdeeba863a7185e51085aef660c5
Jonas Devlieghere <jonas@devlieghere.com>
2019-01-17 12:55:08 -0800

    [CMake] Fix RPATH for repl
    Due to r350391 we could no longer construct the correct RPATH for the
    repl. In order to construct the correct path we need to get the path to
    the LLDB framework by reading the LIBRARY_OUTPUT_DIRECTORY property from
    liblldb. We also need the LLDB_FRAMEWORK_RESOURCE_DIR which was
    previously removed, and re-introduced in this patch.
    
    apple-llvm-split-commit: d0f7e070faca54e2cdb943f16423269a99d51773
    apple-llvm-split-dir: lldb/

--- lldb/cmake/modules/LLDBConfig.cmake
+++ lldb/cmake/modules/LLDBConfig.cmake
@@ -66,6 +66,7 @@ if(LLDB_BUILD_FRAMEWORK)
   set(LLDB_FRAMEWORK_VERSION A CACHE STRING "LLDB.framework version (default is A)")
   set(LLDB_FRAMEWORK_BUILD_DIR bin CACHE STRING "Output directory for LLDB.framework")
   set(LLDB_FRAMEWORK_INSTALL_DIR Library/Frameworks CACHE STRING "Install directory for LLDB.framework")
+  set(LLDB_FRAMEWORK_RESOURCE_DIR Versions/${LLDB_FRAMEWORK_VERSION}/Resources)
   set(LLDB_FRAMEWORK_TOOLS darwin-debug;debugserver;lldb-argdumper;lldb-server CACHE INTERNAL
       "List of tools to include in LLDB.framework/Resources")
 
