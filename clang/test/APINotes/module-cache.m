30fc70f349277a42e02550779b464b92a2f06c1c
Jordan Rose <jordan_rose@apple.com>
2019-01-16 20:05:20 -0800

    Remove -apinotes entry point
    First step in removing the notion of "compiled" API notes altogether.
    
    apple-llvm-split-commit: c59c1a43b592fd63ebe960a5cd698be3bebfa76b
    apple-llvm-split-dir: clang/

--- clang/test/APINotes/module-cache.m
+++ clang/test/APINotes/module-cache.m
@@ -46,41 +46,6 @@
 // RUN: FileCheck -check-prefix=CHECK-WITHOUT-REBUILD %s < %t/after.log
 // RUN: FileCheck -check-prefix=CHECK-TWO-ERRORS %s < %t/after.log
 
-// Set up a directory with pre-compiled API notes.
-// RUN: mkdir -p %t/CompiledAPINotes
-// RUN: rm -rf %t/ModulesCache
-// RUN: rm -rf %t/APINotesCache
-// RUN: %clang -cc1apinotes -yaml-to-binary -o %t/CompiledAPINotes/SomeOtherKit.apinotesc %S/Inputs/APINotes/SomeOtherKit.apinotes
-
-// First build: check that 'methodB' is unavailable but 'methodA' is available.
-// RUN: not %clang_cc1 -fmodules -fimplicit-module-maps -Rmodule-build -fmodules-cache-path=%t/ModulesCache -iapinotes-modules %t/CompiledAPINotes  -F %t/Frameworks %s > %t/compiled-before.log 2>&1
-// RUN: FileCheck -check-prefix=CHECK-METHODB %s < %t/compiled-before.log
-// RUN: FileCheck -check-prefix=CHECK-REBUILD %s < %t/compiled-before.log
-// RUN: FileCheck -check-prefix=CHECK-ONE-ERROR %s < %t/compiled-before.log
-
-// Do it again; now we're using caches.
-// RUN: not %clang_cc1 -fmodules -fimplicit-module-maps -Rmodule-build -fmodules-cache-path=%t/ModulesCache -iapinotes-modules %t/CompiledAPINotes  -F %t/Frameworks %s > %t/compiled-before.log 2>&1
-// RUN: FileCheck -check-prefix=CHECK-METHODB %s < %t/compiled-before.log
-// RUN: FileCheck -check-prefix=CHECK-WITHOUT-REBUILD %s < %t/compiled-before.log
-// RUN: FileCheck -check-prefix=CHECK-ONE-ERROR %s < %t/compiled-before.log
-
-// Compile a new API notes file to replace the old one.
-// RUN: %clang -cc1apinotes -yaml-to-binary -o %t/CompiledAPINotes/SomeOtherKit.apinotesc %t/APINotes/SomeOtherKit.apinotes
-
-// Build again: check that both methods are now unavailable and that the module rebuilt.
-// RUN: not %clang_cc1 -fmodules -fimplicit-module-maps -Rmodule-build -fmodules-cache-path=%t/ModulesCache -iapinotes-modules %t/CompiledAPINotes  -F %t/Frameworks %s > %t/compiled-after.log 2>&1
-// RUN: FileCheck -check-prefix=CHECK-METHODA %s < %t/compiled-after.log
-// RUN: FileCheck -check-prefix=CHECK-METHODB %s < %t/compiled-after.log
-// RUN: FileCheck -check-prefix=CHECK-REBUILD %s < %t/compiled-after.log
-// RUN: FileCheck -check-prefix=CHECK-TWO-ERRORS %s < %t/compiled-after.log
-
-// Run the build again: check that both methods are now unavailable
-// RUN: not %clang_cc1 -fmodules -fimplicit-module-maps -Rmodule-build -fmodules-cache-path=%t/ModulesCache -iapinotes-modules %t/CompiledAPINotes  -F %t/Frameworks %s > %t/compiled-after.log 2>&1
-// RUN: FileCheck -check-prefix=CHECK-METHODA %s < %t/compiled-after.log
-// RUN: FileCheck -check-prefix=CHECK-METHODB %s < %t/compiled-after.log
-// RUN: FileCheck -check-prefix=CHECK-WITHOUT-REBUILD %s < %t/compiled-after.log
-// RUN: FileCheck -check-prefix=CHECK-TWO-ERRORS %s < %t/compiled-after.log
-
 @import SomeOtherKit;
 
 void test(A *a) {
