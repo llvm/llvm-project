9bb785a2aba43f26898a18762ef3d5d303b36277
Jonas Devlieghere <jonas@devlieghere.com>
2019-02-06 18:57:42 +0000

    [Reproducers] SBReproducer framework: Capture & Replay
    This is part two of the reproducer instrumentation framework. It
    contains the code to capture and replay function calls. The main user of
    this framework will be the SB API layer.
    
    For all the details refer to the RFC on the mailing list:
    http://lists.llvm.org/pipermail/lldb-dev/2019-January/014530.html
    
    Differential revision: https://reviews.llvm.org/D56322
    
    git-svn-id: https://llvm.org/svn/llvm-project/lldb/trunk@353324 91177308-0d34-0410-b5e6-96231b3b80d8
    (cherry picked from commit 6e7ade8b386731ab5d2efb46e98e780b4c545788)
    
    apple-llvm-split-commit: f11220b7cc42134b07927a9439acaec121567f05
    apple-llvm-split-dir: lldb/

--- lldb/tools/driver/Driver.cpp
+++ lldb/tools/driver/Driver.cpp
@@ -13,6 +13,7 @@
 #include "lldb/API/SBDebugger.h"
 #include "lldb/API/SBHostOS.h"
 #include "lldb/API/SBLanguageRuntime.h"
+#include "lldb/API/SBReproducer.h"
 #include "lldb/API/SBStream.h"
 #include "lldb/API/SBStringList.h"
 
@@ -892,8 +893,10 @@ main(int argc, char const *argv[])
                          << '\n';
   }
 
-  SBInitializerOptions options;
+  // Remember if we're in replay mode for later.
+  bool replay = false;
 
+  SBInitializerOptions options;
   if (auto *arg = input_args.getLastArg(OPT_capture)) {
     auto arg_value = arg->getValue();
     options.SetReproducerPath(arg_value);
@@ -904,6 +907,7 @@ main(int argc, char const *argv[])
     auto arg_value = arg->getValue();
     options.SetReplayReproducer(true);
     options.SetReproducerPath(arg_value);
+    replay = true;
   }
 
   SBError error = SBDebugger::Initialize(options);
@@ -911,6 +915,14 @@ main(int argc, char const *argv[])
     WithColor::error() << "initialization failed: " << error.GetCString()
                        << '\n';
     return 1;
+  }
+
+  if (replay) {
+    SBReproducer reproducer;
+    if (!reproducer.Replay()) {
+      WithColor::error() << "something went wrong running the reporducer.\n";
+    }
+    return 0;
   }
 
   SBHostOS::ThreadCreated("<lldb.driver.main-thread>");
