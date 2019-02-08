4856a9330ee01d30e9e11b6c2f991662b4c04b07
Tom Stellard <tstellar@redhat.com>
2019-02-08 21:00:26 +0000

    cmake: Use 7.1.0 for shared object version instead of 71.0
    llvm-svn: 353565

--- llvm/cmake/modules/AddLLVM.cmake
+++ llvm/cmake/modules/AddLLVM.cmake
@@ -502,7 +502,7 @@ function(llvm_add_library name)
 		# one component indicating the ABI version, while LLVM uses
 		# major+minor for that.
         SOVERSION ${LLVM_VERSION_MAJOR}.${LLVM_VERSION_MINOR}
-        VERSION ${LLVM_VERSION_MAJOR}${LLVM_VERSION_MINOR}.${LLVM_VERSION_PATCH}${LLVM_VERSION_SUFFIX})
+        VERSION ${LLVM_VERSION_MAJOR}.${LLVM_VERSION_MINOR}.${LLVM_VERSION_PATCH}${LLVM_VERSION_SUFFIX})
     endif()
   endif()
 
