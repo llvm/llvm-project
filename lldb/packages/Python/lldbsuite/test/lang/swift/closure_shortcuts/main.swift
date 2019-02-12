99a6dd1526e014a04fb94e395b861efd4da682d7
Davide Italiano <ditaliano@apple.com>
2019-02-12 10:59:14 -0800

    [SwiftExpressionParser] Add tests for the fixed closure behaviour.
    <rdar://problem/47982630>
    
    apple-llvm-split-commit: 7ee8f98f8528ab048d694e119f54d54718b775e5
    apple-llvm-split-dir: lldb/

--- lldb/packages/Python/lldbsuite/test/lang/swift/closure_shortcuts/main.swift
+++ lldb/packages/Python/lldbsuite/test/lang/swift/closure_shortcuts/main.swift
@@ -8,7 +8,11 @@ func main() -> Int {
               //%self.expect('frame var $1', substrs=['foo'])
   )
 
-  return 0
+  var tinky = [1,2].map({$0 * 2})
+
+  return 0 //%self.expect('expr tinky.map({$0 * 2})', substrs=['[0] = 2', '[1] = 4'])
+           //%self.expect('expr [2,4].map({$0 * 2})', substrs=['[0] = 4', '[1] = 8'])
+           //%self.expect('expr $0', substrs=['unresolved identifier \'$0\''], error=True)
 }
 
 _ = main()
