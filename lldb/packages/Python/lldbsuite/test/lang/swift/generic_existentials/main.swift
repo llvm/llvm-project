5845c5c1714cf487e459c3d53cff86888546118e
Slava Pestov <spestov@apple.com>
2019-02-12 21:25:36 -0500

    [Swift] Remove unnecessary GetDynamicValue() calls from materializer
    They were actually unnecessary and doing the wrong thing when we had generic
    parameters wrapping existentials.
    
    apple-llvm-split-commit: 05ff20967df64680a47256ca8f4e4c7963af9c14
    apple-llvm-split-dir: lldb/

--- lldb/packages/Python/lldbsuite/test/lang/swift/generic_existentials/main.swift
+++ lldb/packages/Python/lldbsuite/test/lang/swift/generic_existentials/main.swift
@@ -1,14 +1,50 @@
-class Pat : Error {
-  var x : Int
-  init(_ x : Int) {
+class MyClass {
+  var x: Int
+  init(_ x: Int) {
     self.x = x
   }
 }
 
 func f<T>(_ x : T) -> T {
-  return x //%self.expect("frame var -d run-target -- x", substrs=['(a.Pat) x', '(x = 23)'])
-           //%self.expect("expr -d run-target -- x", substrs=['(a.Pat) $R', '(x = 23)'])
+  return x //%self.expect("frame var -d run-target -- x", substrs=['(a.MyClass) x', '(x = 23)'])
+           //%self.expect("expr -d run-target -- x", substrs=['(a.MyClass) $R', '(x = 23)'])
 }
 
-let patatino = Pat(23) as AnyObject
-f(patatino)
+f(MyClass(23) as Any)
+f(MyClass(23) as AnyObject)
+
+func g<T>(_ x : T) -> T {
+  return x //%self.expect("frame var -d run-target -- x", substrs=['(a.MyStruct) x', '(x = 23)'])
+           //%self.expect("expr -d run-target -- x", substrs=['(a.MyStruct) $R', '(x = 23)'])
+}
+
+struct MyStruct {
+  var x: Int
+
+  init(_ x: Int) {
+    self.x = x
+  }
+}
+
+g(MyStruct(23) as Any)
+
+func h<T>(_ x : T) -> T {
+  return x //%self.expect("frame var -d run-target -- x", substrs=['(a.MyBigStruct) x', '(x = 23, y = 24, z = 25, w = 26)'])
+           //%self.expect("expr -d run-target -- x", substrs=['(a.MyBigStruct) $R', '(x = 23, y = 24, z = 25, w = 26)'])
+}
+
+struct MyBigStruct {
+  var x: Int
+  var y: Int
+  var z: Int
+  var w: Int
+
+  init(_ x: Int) {
+    self.x = x
+    self.y = x + 1
+    self.z = x + 2
+    self.w = x + 3
+  }
+}
+
+h(MyBigStruct(23) as Any)
