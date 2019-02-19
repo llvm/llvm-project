b1f11a30c60c046dc050b4bdc978d034b8db30a5
Nixoncole <nixontcole@gmail.com>
2019-02-18 20:26:28 -0800

    Added declaration of Diagnostic(), receiving errors.

--- clang/lib/AST/RecordLayoutBuilder.cpp
+++ clang/lib/AST/RecordLayoutBuilder.cpp
@@ -2971,6 +2971,8 @@ ASTContext::getASTRecordLayout(const RecordDecl *D) const {
   // not a complete definition (which is what isDefinition() tests)
   // until we *finish* parsing the definition.
 
+  DiagnosticBuilder Diag(SourceLocation Loc, unsigned DiagID);
+
   if (D->hasExternalLexicalStorage() && !D->getDefinition())
     getExternalSource()->CompleteType(const_cast<RecordDecl*>(D));
 
@@ -2989,7 +2991,7 @@ ASTContext::getASTRecordLayout(const RecordDecl *D) const {
   
   bool ShouldBeRandomized = D->getAttr<RandomizeLayoutAttr>() != nullptr;
   bool NotToBeRandomized = D->getAttr<NoRandomizeLayoutAttr>() != nullptr;
-  
+   
   if (ShouldBeRandomized && NotToBeRandomized) {
     Diag(D->getLocation(), diag::warn_randomize_attr_conflict);  
   }
