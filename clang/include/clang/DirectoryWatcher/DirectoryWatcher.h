76a9173bb4608b1e11a52e691a07875b79cea6f7
Jan Korous <jkorous@apple.com>
2019-02-21 13:05:28 -0800

    [DirectoryWatcher] License, end-of-include-guard comment
    https://reviews.llvm.org/D58418
    
    apple-llvm-split-commit: 2bf62460a3206b148a7dc20ac112303b25902ab3
    apple-llvm-split-dir: clang/

--- clang/include/clang/DirectoryWatcher/DirectoryWatcher.h
+++ clang/include/clang/DirectoryWatcher/DirectoryWatcher.h
@@ -1,9 +1,8 @@
 //===- DirectoryWatcher.h - Listens for directory file changes --*- C++ -*-===//
 //
-//                     The LLVM Compiler Infrastructure
-//
-// This file is distributed under the University of Illinois Open Source
-// License. See LICENSE.TXT for details.
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
 /// \file
@@ -75,4 +74,4 @@ private:
 
 } // namespace clang
 
-#endif
+#endif // LLVM_CLANG_DIRECTORYWATCHER_DIRECTORYWATCHER_H
