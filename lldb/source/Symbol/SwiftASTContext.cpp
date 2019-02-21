7ea4dc3880b0bcd1841520761d396465849490d2
Nathan Hawes <nhawes@apple.com>
2019-02-21 13:52:29 -0800

    [Swift] Install the ParseableInterfaceModuleLoader before the SerializedModuleLoader for correct fallback behavior
    Also added a test that writes out empty .swiftmodule files and checks we
    fallback to using the .swiftinterface file when we fail to load them.
    
    apple-llvm-split-commit: 6bfae22698f6b738566813a7ac256fe25ffda865
    apple-llvm-split-dir: lldb/

--- lldb/source/Symbol/SwiftASTContext.cpp
+++ lldb/source/Symbol/SwiftASTContext.cpp
@@ -2987,15 +2987,6 @@ swift::ASTContext *SwiftASTContext::GetASTContext() {
       GetDiagnosticEngine().addConsumer(
           *new swift::PrintingDiagnosticConsumer());
     }
-    // Install the serialized module loader.
-    std::unique_ptr<swift::ModuleLoader> serialized_module_loader_ap(
-        swift::SerializedModuleLoader::create(*m_ast_context_ap));
-
-    if (serialized_module_loader_ap) {
-      m_serialized_module_loader =
-          (swift::SerializedModuleLoader *)serialized_module_loader_ap.get();
-      m_ast_context_ap->addModuleLoader(std::move(serialized_module_loader_ap));
-    }
 
     // Install the parseable interface module loader.
     std::string ModuleCachePath = GetClangImporterOptions().ModuleCachePath;
@@ -3010,6 +3001,16 @@ swift::ASTContext *SwiftASTContext::GetASTContext() {
       m_parseable_module_loader =
       (swift::ParseableInterfaceModuleLoader *)parseable_module_loader_ap.get();
       m_ast_context_ap->addModuleLoader(std::move(parseable_module_loader_ap));
+    }
+
+    // Install the serialized module loader.
+    std::unique_ptr<swift::ModuleLoader> serialized_module_loader_ap(
+        swift::SerializedModuleLoader::create(*m_ast_context_ap));
+
+    if (serialized_module_loader_ap) {
+      m_serialized_module_loader =
+          (swift::SerializedModuleLoader *)serialized_module_loader_ap.get();
+      m_ast_context_ap->addModuleLoader(std::move(serialized_module_loader_ap));
     }
 
     // Set up the required state for the evaluator in the TypeChecker.
