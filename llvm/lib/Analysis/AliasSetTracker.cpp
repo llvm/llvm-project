cdbca02fd1c786a777e14831494e8c9837ca9f9e
TB Schardl <neboat@mit.edu>
2019-02-23 15:43:53 +0000

    [AliasSetTracker] Add syncregion intrinsics to list of 'marker' intrinsics that don't actually affect memory.

--- llvm/lib/Analysis/AliasSetTracker.cpp
+++ llvm/lib/Analysis/AliasSetTracker.cpp
@@ -437,6 +437,7 @@ void AliasSetTracker::addUnknown(Instruction *Inst) {
       // FIXME: Add lifetime/invariant intrinsics (See: PR30807).
     case Intrinsic::assume:
     case Intrinsic::sideeffect:
+    case Intrinsic::syncregion_start:
       return;
     }
   }
