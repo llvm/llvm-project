184695db7726d39fccfcf60deba2eeea0438faba
Jordan Cantrell <cantrel2@pdx.edu>
2019-02-24 01:51:18 -0800

    error: linker -f may not be used without -shared

--- clang/lib/Driver/Job.cpp
+++ clang/lib/Driver/Job.cpp
@@ -335,13 +335,12 @@ int Command::Execute(ArrayRef<llvm::Optional<StringRef>> Redirects,
 
   if (ResponseFile == nullptr) {
     Argv.push_back(Executable);
+    Argv.append(Arguments.begin(), Arguments.end());
     if (!RandstructSeed.empty())
     {
       //Args.push_back("-frandstruct-seed=" + RandstructSeed);
-      Argv.push_back("-frandstruct-seed");
-      Argv.push_back( RandstructSeed.c_str() );
+      Argv.push_back( ("-frandstruct-seed=" + RandstructSeed).c_str() );
     }
-    Argv.append(Arguments.begin(), Arguments.end());
     Argv.push_back(nullptr);
 
     auto Args = llvm::toStringRefArray(Argv.data());
