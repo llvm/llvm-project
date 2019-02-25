ab0393f272b149454fe9ac3b5ef764664d0e78a2
Jordan Cantrell <cantrel2@pdx.edu>
2019-02-25 03:12:04 -0800

    wip fixing linker errors

--- clang/lib/Driver/Job.cpp
+++ clang/lib/Driver/Job.cpp
@@ -336,13 +336,16 @@ int Command::Execute(ArrayRef<llvm::Optional<StringRef>> Redirects,
   if (ResponseFile == nullptr) {
     Argv.push_back(Executable);
     Argv.append(Arguments.begin(), Arguments.end());
+
     // If this is the clang tool, and seed is empty, push argument back
     // FIXME: figure out where Arguments are set, and put this there
+#if 0
     if ( ("clang" == (*this).getCreator().getName() ) && !RandstructSeed.empty())
     {
       //Args.push_back("-frandstruct-seed=" + RandstructSeed);
       Argv.push_back( ("-frandstruct-seed=" + RandstructSeed).c_str() );
     }
+#endif
     Argv.push_back(nullptr);
 
     auto Args = llvm::toStringRefArray(Argv.data());
