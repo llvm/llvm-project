86eb177dd9ac117eb47fb3a21fd9ac648bacd659
Davide Italiano <ditaliano@apple.com>
2019-02-26 01:19:36 +0000

    XPASSES are now reported. Fix all of them on Linux.
    apple-llvm-split-commit: 0d9bcf7a30378f004d529616c66aa7f1bb059f83
    apple-llvm-split-dir: lldb/

--- lldb/packages/Python/lldbsuite/test/tools/lldb-server/TestGdbRemoteRegisterState.py
+++ lldb/packages/Python/lldbsuite/test/tools/lldb-server/TestGdbRemoteRegisterState.py
@@ -113,7 +113,6 @@ class TestGdbRemoteRegisterState(gdbremote_testcase.GdbRemoteTestCaseBase):
         self.grp_register_save_restore_works(USE_THREAD_SUFFIX)
 
     @llgs_test
-    @expectedFailureAll(oslist=["linux"], bugnumber="rdar://29054737")
     def test_grp_register_save_restore_works_with_suffix_llgs(self):
         USE_THREAD_SUFFIX = True
         self.init_llgs_test()
@@ -130,7 +129,6 @@ class TestGdbRemoteRegisterState(gdbremote_testcase.GdbRemoteTestCaseBase):
         self.grp_register_save_restore_works(USE_THREAD_SUFFIX)
 
     @llgs_test
-    @expectedFailureAll(oslist=["linux"], bugnumber="rdar://29054737")
     def test_grp_register_save_restore_works_no_suffix_llgs(self):
         USE_THREAD_SUFFIX = False
         self.init_llgs_test()
