986e00d65e62a3f3b02df6331d70123ba3a3ee0e
Davide Italiano <davide@freebsd.org>
2019-03-18 20:02:27 +0000

    [API] Remove unneded LLDB_DISABLE_PYTHON markers.
    git-svn-id: https://llvm.org/svn/llvm-project/lldb/trunk@356401 91177308-0d34-0410-b5e6-96231b3b80d8
    apple-llvm-split-commit: daeed48caab15e15b43c4176a33c8d763a09d730
    apple-llvm-split-dir: lldb/

--- lldb/source/API/SBTarget.cpp
+++ lldb/source/API/SBTarget.cpp
@@ -2378,9 +2378,7 @@ lldb::SBValue SBTarget::EvaluateExpression(const char *expr,
                      (const char *, const lldb::SBExpressionOptions &), expr,
                      options);
 
-#if !defined(LLDB_DISABLE_PYTHON)
   Log *expr_log(GetLogIfAllCategoriesSet(LIBLLDB_LOG_EXPRESSIONS));
-#endif
   SBValue expr_result;
   ExpressionResults exe_results = eExpressionSetupError;
   ValueObjectSP expr_value_sp;
@@ -2415,13 +2413,10 @@ lldb::SBValue SBTarget::EvaluateExpression(const char *expr,
       expr_result.SetSP(expr_value_sp, options.GetFetchDynamicValue());
     }
   }
-#ifndef LLDB_DISABLE_PYTHON
   if (expr_log)
     expr_log->Printf("** [SBTarget::EvaluateExpression] Expression result is "
                      "%s, summary %s **",
                      expr_result.GetValue(), expr_result.GetSummary());
-#endif
-
   return LLDB_RECORD_RESULT(expr_result);
 }
 
