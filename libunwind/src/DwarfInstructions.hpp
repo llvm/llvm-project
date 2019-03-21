6b293b18528e3435dc58619201ca24c43bc7e991
luporl <luporl@FreeBSD.org>
2019-03-21 18:01:33 -0300

    [PPC64][libunwind] Fix r2 not properly restored
    This change makes each unwind step inspect the instruction at the
    return address and, if needed, read r2 from its saved location and
    modify the context appropriately.
    
    One point that (maybe) could be improved is to try to find if the
    binary being unwound is ELFv1 or ELFv2 at run time, instead of at
    compile time, or, find out the correct save location of r2 in some
    other way.

--- libunwind/src/DwarfInstructions.hpp
+++ libunwind/src/DwarfInstructions.hpp
@@ -234,6 +234,27 @@ int DwarfInstructions<A, R>::stepWithDwarf(A &addressSpace, pint_t pc,
       }
 #endif
 
+#if defined(_LIBUNWIND_TARGET_PPC64)
+      // If the instruction at return address is a TOC (r2) restore
+      // then r2 was saved and needs to be restored
+      if (R::getArch() == REGISTERS_PPC64 && returnAddress != 0 &&
+          addressSpace.get32(returnAddress)
+#if defined(_CALL_ELF) && _CALL_ELF == 2
+              == 0xe8410018 // ld r2,24(r1)
+#else
+              == 0xe8410028 // ld r2,40(r1)
+#endif
+      ) {
+        pint_t sp = newRegisters.getRegister(UNW_REG_SP);
+#if defined(_CALL_ELF) && _CALL_ELF == 2
+        pint_t r2 = addressSpace.get64(sp + 24);
+#else
+        pint_t r2 = addressSpace.get64(sp + 40);
+#endif
+        newRegisters.setRegister(UNW_PPC64_R2, r2);
+      }
+#endif
+
       // Return address is address after call site instruction, so setting IP to
       // that does simualates a return.
       newRegisters.setIP(returnAddress);
