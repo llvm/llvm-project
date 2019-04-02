503a68fa90beaeb9efe6ed7664e8a54acb306c82
Adrian Prantl <aprantl@apple.com>
2019-04-02 09:18:56 -0700

    Simplify testcases.
    apple-llvm-split-commit: 16339f108b0250cae8b1d65ad9e32ffd1d6e81b6
    apple-llvm-split-dir: lldb/

--- lldb/packages/Python/lldbsuite/test/lang/swift/any_object/TestSwiftAnyObjectType.py
+++ lldb/packages/Python/lldbsuite/test/lang/swift/any_object/TestSwiftAnyObjectType.py
@@ -14,7 +14,7 @@ Test the AnyObject type
 """
 import lldb
 from lldbsuite.test.lldbtest import *
-import lldbsuite.test.decorators as decorators
+from lldbsuite.test.decorators import *
 import lldbsuite.test.lldbutil as lldbutil
 import os
 import unittest2
@@ -24,47 +24,20 @@ class TestSwiftAnyObjectType(TestBase):
 
     mydir = TestBase.compute_mydir(__file__)
 
-    @decorators.swiftTest
-    @decorators.add_test_categories(["swiftpr"])
-    def test_any_object_type(self):
-        """Test the Any type"""
-        self.build()
-        self.do_test()
-
     def setUp(self):
         TestBase.setUp(self)
-        self.main_source = "main.swift"
-        self.main_source_spec = lldb.SBFileSpec(self.main_source)
 
-    def do_test(self):
+    @swiftTest
+    @add_test_categories(["swiftpr"])
+    def test_any_object_type(self):
         """Test the AnyObject type"""
-        exe_name = "a.out"
-        exe = self.getBuildArtifact(exe_name)
+        self.build()
+        target, process, thread, bkpt = lldbutil.run_to_source_breakpoint(
+            self, 'Set breakpoint here', lldb.SBFileSpec('main.swift'))
 
-        # Create the target
-        target = self.dbg.CreateTarget(exe)
-        self.assertTrue(target, VALID_TARGET)
+        frame = thread.frames[0]
 
-        # Set the breakpoints
-        breakpoint = target.BreakpointCreateBySourceRegex(
-            'Set breakpoint here', self.main_source_spec)
-        self.assertTrue(breakpoint.GetNumLocations() > 0, VALID_BREAKPOINT)
-
-        # Launch the process, and do not stop at the entry point.
-        process = target.LaunchSimple(None, None, os.getcwd())
-
-        self.assertTrue(process, PROCESS_IS_VALID)
-
-        # Frame #0 should be at our breakpoint.
-        threads = lldbutil.get_threads_stopped_at_breakpoint(
-            process, breakpoint)
-
-        self.assertTrue(len(threads) == 1)
-        self.thread = threads[0]
-        self.frame = self.thread.frames[0]
-        self.assertTrue(self.frame, "Frame 0 is valid.")
-
-        var_object = self.frame.FindVariable("object")
+        var_object = frame.FindVariable("object")
         lldbutil.check_variable(
             self,
             var_object,
