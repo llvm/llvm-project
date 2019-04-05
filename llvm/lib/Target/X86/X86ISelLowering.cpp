ccd2a71de0fa9a6c42877bf6abbd51a999d1c19b
Francis Visoiu Mistrih <francisvm@yahoo.com>
2019-04-05 20:18:25 +0000

    [X86] Enable tail calls for CallingConv::Swift
    It's currently only enabled on AArch64 (enabled in r281376).
    
    rdar://49070326
    
    git-svn-id: https://llvm.org/svn/llvm-project/llvm/trunk@357809 91177308-0d34-0410-b5e6-96231b3b80d8
    apple-llvm-split-commit: 92f68cf4879db3daf7256369b9dc477eda0f5b71
    apple-llvm-split-dir: llvm/

--- llvm/lib/Target/X86/X86ISelLowering.cpp
+++ llvm/lib/Target/X86/X86ISelLowering.cpp
@@ -2901,6 +2901,8 @@ static bool mayTailCallThisCC(CallingConv::ID CC) {
   case CallingConv::X86_StdCall:
   case CallingConv::X86_VectorCall:
   case CallingConv::X86_FastCall:
+  // Swift:
+  case CallingConv::Swift:
     return true;
   default:
     return canGuaranteeTCO(CC);
