4bc252b904034e9ebaeab7f9330a2bdc4fa12b8e
Scott Linder <Scott.Linder@amd.com>
2019-04-30 17:25:50 -0400

    Fix compiling against LLVM build tree
    The path for the LLD include directory is not avilable in an LLVM build
    tree. Add a workaround to find LLD in both an internal and external
    build tree, and halt the build if it is not found. Compiling against an
    installed copy of LLVM is unaffected.
    
    Also avoid relying on the assumption that the Clang CMake directory
    shares its lib prefix with other build artifacts (which is not the case
    with MSVC, which adds a configuration-specific prefix to one and not the
    other). Instead search the entire build-tree/install-prefix for
    opencl-c.h and take the first copy found, halting the build if it is not
    found.
    
    Also add some messages with the paths used for LLVM components and
    device-libs to aid in debugging build issues.
    
    Change-Id: I513fcd3b041196df5ca473c7e17169e3a295f1e8

--- amd/comgr/cmake/opencl_pch.cmake
+++ amd/comgr/cmake/opencl_pch.cmake
@@ -2,7 +2,19 @@ find_package(Clang REQUIRED CONFIG)
 
 # FIXME: CLANG_CMAKE_DIR seems like the most stable way to find this, but
 # really there is no way to reliably discover this header.
-file(GLOB_RECURSE OPENCL_C_H "${CLANG_CMAKE_DIR}/../../*/opencl-c.h")
+#
+# We effectively back up to the Clang output directory (for the case of a build
+# tree) or install prefix (for the case of an installed copy), and then search
+# for a file named opencl-c.h anywhere below that. We take the first result in
+# the case where there are multiple (e.g. if there is an installed copy nested
+# in a build directory). This is a bit imprecise, but it covers cases like MSVC
+# adding some additional configuration-specific subdirectories to the build
+# tree but not to an installed copy.
+file(GLOB_RECURSE OPENCL_C_H_LIST "${CLANG_CMAKE_DIR}/../../../*/opencl-c.h")
+list(GET OPENCL_C_H_LIST 0 OPENCL_C_H)
+if (NOT EXISTS "${OPENCL_C_H}" OR IS_DIRECTORY "${OPENCL_C_H}")
+  message(FATAL_ERROR "Unable to locate opencl-c.h from the supplied Clang. The path '${CLANG_CMAKE_DIR}/../../../*' was searched.")
+endif()
 
 # Macro to create and install a custom target for generating PCH for given
 # OpenCL version.
