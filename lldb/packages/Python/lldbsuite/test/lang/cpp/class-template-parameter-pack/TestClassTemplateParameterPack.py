ee1987b12d6cc7f4040f758caa8ecbade84b469c
Shafik Yaghmour <syaghmour@apple.com>
2019-05-01 16:39:31 +0000

    Disabling test in TestClassTemplateParameterPack.py until we do template lookup correctly
    Summary:
    Some tests currently only work because we are pulling all the local variables when we are evaluating an expression. This will soon
    change and these test are working but for the wrong reasons. The details can be found in the discussion here:
    http://lists.llvm.org/pipermail/lldb-commits/Week-of-Mon-20180507/040689.html
    
    Differential Review:  https://reviews.llvm.org/D61266
    
    git-svn-id: https://llvm.org/svn/llvm-project/lldb/trunk@359699 91177308-0d34-0410-b5e6-96231b3b80d8
    (cherry picked from commit 3ce4f605628023708c30581f84467544b3187492)
    
    apple-llvm-split-commit: 43a8a7c577d1936b6f42d9b1d547aa1337a689c4
    apple-llvm-split-dir: lldb/

--- lldb/packages/Python/lldbsuite/test/lang/cpp/class-template-parameter-pack/TestClassTemplateParameterPack.py
+++ lldb/packages/Python/lldbsuite/test/lang/cpp/class-template-parameter-pack/TestClassTemplateParameterPack.py
@@ -4,6 +4,4 @@ from lldbsuite.test import decorators
 lldbinline.MakeInlineTest(
     __file__, globals(), [
         decorators.expectedFailureAll(
-            compiler="gcc"),
-        decorators.expectedFailureAll(
-            bugnumber="rdar://47370292")])
+            compiler="gcc")])
