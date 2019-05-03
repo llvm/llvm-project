9d93b4e0c8d5dcda9e731a8905a6ec3fee8d68b3
Akira Hatanaka <ahatanaka@apple.com>
2019-05-03 07:19:46 +0000

    [Sema][ObjC] Disable -Wunused-parameter for ObjC methods
    The warning isn't very useful when the function is an ObjC method.
    
    rdar://problem/41561853
    
    Differential Revision: https://reviews.llvm.org/D61147
    
    git-svn-id: https://llvm.org/svn/llvm-project/cfe/trunk@359864 91177308-0d34-0410-b5e6-96231b3b80d8
    (cherry picked from commit 6ef4cf339ce48cf922534c163eaec4bcbf85604f)
    
    apple-llvm-split-commit: e3d6f04d4cb40bec81e57f19d08c89541a42a9e0
    apple-llvm-split-dir: clang/

--- clang/lib/Sema/SemaDecl.cpp
+++ clang/lib/Sema/SemaDecl.cpp
@@ -13311,8 +13311,6 @@ Decl *Sema::ActOnFinishFunctionBody(Decl *dcl, Stmt *Body,
     assert(MD == getCurMethodDecl() && "Method parsing confused");
     MD->setBody(Body);
     if (!MD->isInvalidDecl()) {
-      if (!MD->hasSkippedBody())
-        DiagnoseUnusedParameters(MD->parameters());
       DiagnoseSizeOfParametersAndReturnValue(MD->parameters(),
                                              MD->getReturnType(), MD);
 
