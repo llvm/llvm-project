ef075653c7b5085a007ceba46f982c156fc53ad9
Leonard Chan <leonardchan@google.com>
2019-05-07 08:12:28 +0000

    [Sema] Add missing VisitMacroQualifiedTypeLoc to TypeSpecLocFiller
    To hopefully fix greenbot failures
    
    llvm-svn: 360120

--- clang/lib/Sema/SemaType.cpp
+++ clang/lib/Sema/SemaType.cpp
@@ -5390,6 +5390,11 @@ namespace {
       Visit(TL.getModifiedLoc());
       fillAttributedTypeLoc(TL, State);
     }
+    void VisitMacroQualifiedTypeLoc(MacroQualifiedTypeLoc TL) {
+      Visit(TL.getInnerLoc());
+      TL.setExpansionLoc(
+          State.getExpansionLocForMacroQualifiedType(TL.getTypePtr()));
+    }
     void VisitQualifiedTypeLoc(QualifiedTypeLoc TL) {
       Visit(TL.getUnqualifiedLoc());
     }
