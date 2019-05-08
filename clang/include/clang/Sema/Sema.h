38e442b54dae488fd94b4402aac83e6f1a10eef3
Richard Smith <richard-llvm@metafoo.co.uk>
2019-05-08 01:36:36 +0000

    Split ActOnCallExpr into an ActOnCallExpr to be called by the parser, and a BuildCallExpr to be called internally within Sema to build / rebuild calls.
    llvm-svn: 360217

--- clang/include/clang/Sema/Sema.h
+++ clang/include/clang/Sema/Sema.h
@@ -4606,6 +4606,9 @@ public:
   /// locations.
   ExprResult ActOnCallExpr(Scope *S, Expr *Fn, SourceLocation LParenLoc,
                            MultiExprArg ArgExprs, SourceLocation RParenLoc,
+                           Expr *ExecConfig = nullptr);
+  ExprResult BuildCallExpr(Scope *S, Expr *Fn, SourceLocation LParenLoc,
+                           MultiExprArg ArgExprs, SourceLocation RParenLoc,
                            Expr *ExecConfig = nullptr,
                            bool IsExecConfig = false);
   ExprResult
