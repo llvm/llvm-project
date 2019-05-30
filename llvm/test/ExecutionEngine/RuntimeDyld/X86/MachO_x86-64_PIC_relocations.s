39a818083f79466c5fbe425995c34735b6ac501e
Lang Hames <lhames@gmail.com>
2019-05-30 20:58:28 +0000

    [RuntimeDyld] Update reserveAllocationSpace to account for stub padding.
    This should fix the buildbot failures caused by r362139.
    
    rdar://problem/50968433
    
    git-svn-id: https://llvm.org/svn/llvm-project/llvm/trunk@362151 91177308-0d34-0410-b5e6-96231b3b80d8
    apple-llvm-split-commit: a1524e8554ab9b30931a377e19bf255c872be5b3
    apple-llvm-split-dir: llvm/

--- llvm/test/ExecutionEngine/RuntimeDyld/X86/MachO_x86-64_PIC_relocations.s
+++ llvm/test/ExecutionEngine/RuntimeDyld/X86/MachO_x86-64_PIC_relocations.s
@@ -24,7 +24,7 @@ insn2:
 # Test PC-rel GOT relocation.
 # Verify both the contents of the GOT entry for y, and that the movq instruction
 # references the correct GOT entry address:
-# rtdyld-check: stub_addr(test_x86-64.o/__text, y)[2:0] = 0
+# rtdyld-check: stub_addr(test_x86-64.o,__text, y)[2:0] = 0
 # rtdyld-check: *{8}(stub_addr(test_x86-64.o, __text, y)) = y
 # rtdyld-check: decode_operand(insn3, 4) = stub_addr(test_x86-64.o, __text, y) - next_pc(insn3)
 insn3:
