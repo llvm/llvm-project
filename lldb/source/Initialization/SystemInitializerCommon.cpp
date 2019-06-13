ae58cc1ed27cf33266f1b7e9d0505589a09d4b17
Jonas Devlieghere <jonas@devlieghere.com>
2019-06-13 04:35:22 +0000

    [Reproducers] Include lldb version in the reproducer root
    Generally, reproducers are rev-locked to the version of LLDB, so it's
    valuable to have the LLDB version in the reproducer. For now I just want
    the information to be present, without enforcing it, but I envision
    emitting a warning during replay in the future.
    
    Differential revision: https://reviews.llvm.org/D63229
    
    git-svn-id: https://llvm.org/svn/llvm-project/lldb/trunk@363225 91177308-0d34-0410-b5e6-96231b3b80d8
    (cherry picked from commit c42f16ca5241609afa1d3e8519aa13ed9becd48e)
    
    apple-llvm-split-commit: 0798e2dd308000455b9202e2df4dc4017d3c8367
    apple-llvm-split-dir: lldb/

--- lldb/source/Initialization/SystemInitializerCommon.cpp
+++ lldb/source/Initialization/SystemInitializerCommon.cpp
@@ -15,6 +15,7 @@
 #include "lldb/Utility/Log.h"
 #include "lldb/Utility/Reproducer.h"
 #include "lldb/Utility/Timer.h"
+#include "lldb/lldb-private.h"
 
 #if defined(__linux__) || defined(__FreeBSD__) || defined(__NetBSD__)
 #include "Plugins/Process/POSIX/ProcessPOSIXLog.h"
@@ -66,7 +67,6 @@ llvm::Error SystemInitializerCommon::Initialize() {
       return e;
   }
 
-  // Initialize the file system.
   auto &r = repro::Reproducer::Instance();
   if (repro::Loader *loader = r.GetLoader()) {
     FileSpec vfs_mapping = loader->GetFile<FileProvider::Info>();
@@ -77,6 +77,8 @@ llvm::Error SystemInitializerCommon::Initialize() {
       FileSystem::Initialize();
     }
   } else if (repro::Generator *g = r.GetGenerator()) {
+    repro::VersionProvider &vp = g->GetOrCreate<repro::VersionProvider>();
+    vp.SetVersion(lldb_private::GetVersion());
     repro::FileProvider &fp = g->GetOrCreate<repro::FileProvider>();
     FileSystem::Initialize(fp.GetFileCollector());
   } else {
