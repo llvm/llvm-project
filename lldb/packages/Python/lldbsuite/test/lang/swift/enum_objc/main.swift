7304ac16e7c79cd830de6db5eb8bb80b7d78b445
Davide Italiano <ditaliano@apple.com>
2019-07-01 15:56:21 -0700

    [Swift] Rewrite this test to work on Linux.
    apple-llvm-split-commit: 1223d94c32ee9f89880dbcee3cb017e872475a5e
    apple-llvm-split-dir: lldb/

--- lldb/packages/Python/lldbsuite/test/lang/swift/enum_objc/main.swift
+++ lldb/packages/Python/lldbsuite/test/lang/swift/enum_objc/main.swift
@@ -1,18 +1,16 @@
-import Foundation
+import Enum 
 
 func test()
 {
-    let a : NSNumber = 0.0
-    let b : NSNumber = 1.0
-    let ascending = a.compare(b)
-    let descending = b.compare(a)
-    let same = a.compare(a)
-    return //%self.expect('frame var -d run-target -- ascending', substrs=['orderedAscending'])
-           //%self.expect('frame var -d run-target -- descending', substrs=['orderedDescending'])
-           //%self.expect('frame var -d run-target -- same', substrs=['orderedSame'])
-           //%self.expect('expr -d run-target -- ascending', substrs=['orderedAscending'])
-           //%self.expect('expr -d run-target -- descending', substrs=['orderedDescending'])
-           //%self.expect('expr -d run-target -- same', substrs=['orderedSame'])
+    let ascending = getReturn(-1)
+    let descending = getReturn(1)
+    let same = getReturn(0)
+    return //%self.expect('frame var -d run-target -- ascending', substrs=['OrderedAscending'])
+           //%self.expect('frame var -d run-target -- descending', substrs=['OrderedDescending'])
+           //%self.expect('frame var -d run-target -- same', substrs=['OrderedSame'])
+           //%self.expect('expr -d run-target -- ascending', substrs=['OrderedAscending'])
+           //%self.expect('expr -d run-target -- descending', substrs=['OrderedDescending'])
+           //%self.expect('expr -d run-target -- same', substrs=['OrderedSame'])
 }
 
 _ = test()
