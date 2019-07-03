89161fe327cb465987284b65e5db9071b8fba30d
Alex Lorenz <arphaman@gmail.com>
2019-07-03 01:02:43 +0000

    [triple] Use 'macabi' environment name for the Mac Catalyst triples
    The 'macabi' environment name is preferred instead of 'maccatalyst'.
    
    git-svn-id: https://llvm.org/svn/llvm-project/llvm/trunk@364988 91177308-0d34-0410-b5e6-96231b3b80d8
    (cherry picked from commit 255ffb6b06f5a7c01553134aef36bc4cd8c457e3)
    
    apple-llvm-split-commit: 3f1fd4f46a8e82cee6fffb284ee46daf8ddf66ae
    apple-llvm-split-dir: llvm/

--- llvm/include/llvm/ADT/Triple.h
+++ llvm/include/llvm/ADT/Triple.h
@@ -211,8 +211,8 @@ public:
     Cygnus,
     CoreCLR,
     Simulator, // Simulator variants of other systems, e.g., Apple's iOS
-    MacCatalyst,
-    LastEnvironmentType = MacCatalyst
+    MacABI, // Mac Catalyst variant of Apple's iOS deployment target.
+    LastEnvironmentType = MacABI
   };
   enum ObjectFormatType {
     UnknownObjectFormat,
@@ -482,7 +482,7 @@ public:
   }
 
   bool isMacCatalystEnvironment() const {
-    return getEnvironment() == Triple::MacCatalyst;
+    return getEnvironment() == Triple::MacABI;
   }
 
   bool isOSNetBSD() const {
