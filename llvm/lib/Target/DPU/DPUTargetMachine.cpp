5f11bc9cf4b9bca3d746154ed7f57ea6f74cb697
jchauzi <jchauzi@upmem.com>
2019-07-23 10:20:43 +0200

    dpu: llvm: using generic helper getEffectiveCodeModel)

--- llvm/lib/Target/DPU/DPUTargetMachine.cpp
+++ llvm/lib/Target/DPU/DPUTargetMachine.cpp
@@ -42,12 +42,6 @@ static Reloc::Model getEffectiveRelocModel(Optional<Reloc::Model> RM) {
   return *RM;
 }
 
-static CodeModel::Model getEffectiveCodeModel(Optional<CodeModel::Model> CM) {
-  if (CM)
-    return *CM;
-  return CodeModel::Small;
-}
-
 DPUTargetMachine::DPUTargetMachine(const Target &T, const Triple &TT,
                                    StringRef CPU, StringRef FS,
                                    const TargetOptions &Options,
@@ -56,7 +50,7 @@ DPUTargetMachine::DPUTargetMachine(const Target &T, const Triple &TT,
                                    CodeGenOpt::Level &OL, bool JIT)
     : LLVMTargetMachine(T, computeDataLayout(TT, CPU, Options), TT, CPU, FS,
                         Options, getEffectiveRelocModel(RM),
-                        getEffectiveCodeModel(CM), OL),
+                        getEffectiveCodeModel(CM, CodeModel::Small), OL),
       TLOF(make_unique<TargetLoweringObjectFileELF>()),
       Subtarget(TT, CPU, FS, *this) {
   initAsmInfo();
