5c203c43c59e1e404cb84a4f872eb2f355da17bb
Jonas Devlieghere <jonas@devlieghere.com>
2019-07-29 23:02:11 +0000

    [DependencyCollector] Make maybeAddDependency virtual (NFC)
    Make DependencyCollector::maybeAddDependency, just like its other
    methods, which I made virtual a while ago. The motivation for this
    change is still the LLDB reproducer.
    
    git-svn-id: https://llvm.org/svn/llvm-project/cfe/trunk@367271 91177308-0d34-0410-b5e6-96231b3b80d8
    (cherry picked from commit cb15cf86d9f70d93d4778b85f970346280bb0978)
    
    apple-llvm-split-commit: f0770f6cc4d779ea2ee29f80957bad4230257205
    apple-llvm-split-dir: clang/

--- clang/include/clang/Frontend/Utils.h
+++ clang/include/clang/Frontend/Utils.h
@@ -100,11 +100,11 @@ public:
   /// Return true if system files should be passed to sawDependency().
   virtual bool needSystemDependencies() { return false; }
 
-  // implementation detail
   /// Add a dependency \p Filename if it has not been seen before and
   /// sawDependency() returns true.
-  void maybeAddDependency(StringRef Filename, bool FromModule, bool IsSystem,
-                          bool IsModuleFile, bool IsMissing);
+  virtual void maybeAddDependency(StringRef Filename, bool FromModule,
+                                  bool IsSystem, bool IsModuleFile,
+                                  bool IsMissing);
 
 private:
   llvm::StringSet<> Seen;
