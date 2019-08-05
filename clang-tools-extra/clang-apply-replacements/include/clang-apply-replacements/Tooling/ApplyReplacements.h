ee7ee02172285ef44d9d16a5803a1377074c6f6f
José Santiago Silva Ramírez <48314160+Shago7937@users.noreply.github.com>
2019-08-05 12:43:21 -0700

    Update Applyreplacements.h
    Code of conduct to all Máster
    Shago7932. Shago7937 the two acounts the are mine Outlook and gmail

--- clang-tools-extra/clang-apply-replacements/include/clang-apply-replacements/Tooling/ApplyReplacements.h
+++ clang-tools-extra/clang-apply-replacements/include/clang-apply-replacements/Tooling/ApplyReplacements.h
@@ -1,5 +1,5 @@
 //===-- ApplyReplacements.h - Deduplicate and apply replacements -- C++ -*-===//
-//
+// Code of conduct Shago7932 Shago7937
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
