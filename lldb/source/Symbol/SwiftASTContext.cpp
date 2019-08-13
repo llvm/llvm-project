4302698f28d89a1d6cf35dbffbad636bfa9736eb
Davide Italiano <ditaliano@apple.com>
2019-08-12 18:35:28 -0700

    [SwiftASTContext] Perform archetype binding before going to IRGen.
    apple-llvm-split-commit: 8b1c12f7254616b023b4efa8dd10c7b75bb07f8a
    apple-llvm-split-dir: lldb/

--- lldb/source/Symbol/SwiftASTContext.cpp
+++ lldb/source/Symbol/SwiftASTContext.cpp
@@ -6144,12 +6144,26 @@ SwiftASTContext::GetByteStride(lldb::opaque_compiler_type_t type,
 
 llvm::Optional<size_t> SwiftASTContext::GetTypeBitAlign(void *type,
                                                         ExecutionContextScope *exe_scope) {
-  if (type) {
-    const swift::irgen::FixedTypeInfo *fixed_type_info =
-        GetSwiftFixedTypeInfo(type);
-    if (fixed_type_info)
-      return fixed_type_info->getFixedAlignment().getValue();
+  if (!type)
+    return {};
+
+  // If the type has type parameters, bind them first.
+  swift::CanType swift_can_type(GetCanonicalSwiftType(type));
+  if (swift_can_type->hasTypeParameter()) {
+    if (!exe_scope)
+      return {};
+    ExecutionContext exe_ctx;
+    exe_scope->CalculateExecutionContext(exe_ctx);
+    auto swift_scratch_ctx_lock = SwiftASTContextLock(&exe_ctx);
+    CompilerType bound_type = BindAllArchetypes({this, type}, exe_scope);
+    // Note thay the bound type may be in a different AST context.
+    return bound_type.GetTypeBitAlign(exe_scope);
   }
+
+  const swift::irgen::FixedTypeInfo *fixed_type_info =
+      GetSwiftFixedTypeInfo(type);
+  if (fixed_type_info)
+    return fixed_type_info->getFixedAlignment().getValue();
   return {};
 }
 
