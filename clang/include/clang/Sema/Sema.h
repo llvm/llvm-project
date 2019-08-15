91f23fd7a203926777d5c43ec1c36a9574e3d839
Jonas Devlieghere <jonas@devlieghere.com>
2019-08-15 16:10:18 -0700

    Migrate swift-only instances of llvm::index_sequence.
    Upstream removed llvm::index_sequence(for) and replaced it with its
    C++14 variant. This fixes the remaining Swift-only uses.
    
    apple-llvm-split-commit: c0f7273478dda930bc634a2f0989808ecc39811a
    apple-llvm-split-dir: clang/

--- clang/include/clang/Sema/Sema.h
+++ clang/include/clang/Sema/Sema.h
@@ -1628,7 +1628,7 @@ public:
 
     template <std::size_t... Is>
     void emit(const SemaDiagnosticBuilder &DB,
-              llvm::index_sequence<Is...>) const {
+              std::index_sequence<Is...>) const {
       // Apply all tuple elements to the builder in order.
       bool Dummy[] = {false, (DB << getPrintable(std::get<Is>(Args)))...};
       (void)Dummy;
@@ -1642,7 +1642,7 @@ public:
 
     void diagnose(Sema &S, SourceLocation Loc, QualType T) override {
       const SemaDiagnosticBuilder &DB = S.Diag(Loc, DiagID);
-      emit(DB, llvm::index_sequence_for<Ts...>());
+      emit(DB, std::index_sequence_for<Ts...>());
       DB << T;
     }
   };
