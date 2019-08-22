f474fc67da04a61f0ff0ece6990d72ab8400e55c
Davide Italiano <ditaliano@apple.com>
2019-08-22 15:56:37 -0700

    [Scalar] Remove a now unnecessary check.
    Reduces the difference between llvm.org and github.
    
    <rdar://problem/35898017>
    
    apple-llvm-split-commit: 93958c13d643ecca93f2c3f92afa242893473f8d
    apple-llvm-split-dir: lldb/

--- lldb/source/Utility/Scalar.cpp
+++ lldb/source/Utility/Scalar.cpp
@@ -2677,11 +2677,6 @@ bool Scalar::ExtractBitfield(uint32_t bit_size, uint32_t bit_offset) {
   if (bit_size == 0)
     return true;
 
-  size_t this_bit_size = 8 * GetByteSize();
-  
-  if (bit_offset > this_bit_size)
-    return false;
-
   switch (m_type) {
   case Scalar::e_void:
   case Scalar::e_float:
@@ -2697,7 +2692,7 @@ bool Scalar::ExtractBitfield(uint32_t bit_size, uint32_t bit_offset) {
   case Scalar::e_sint512:
     m_integer = m_integer.ashr(bit_offset)
                     .sextOrTrunc(bit_size)
-                    .sextOrSelf(this_bit_size);
+                    .sextOrSelf(8 * GetByteSize());
     return true;
 
   case Scalar::e_uint:
@@ -2708,7 +2703,7 @@ bool Scalar::ExtractBitfield(uint32_t bit_size, uint32_t bit_offset) {
   case Scalar::e_uint512:
     m_integer = m_integer.lshr(bit_offset)
                     .zextOrTrunc(bit_size)
-                    .zextOrSelf(this_bit_size);
+                    .zextOrSelf(8 * GetByteSize());
     return true;
   }
   return false;
