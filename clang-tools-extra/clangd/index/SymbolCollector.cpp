c95063dc4a64a2e5e0e1873b53439e5e87719ed0
Hans Wennborg <hans@hanshq.net>
2019-08-26 07:56:27 +0000

    Merging r369349: ------------------------------------------------------------------------ r369349 | ibiryukov | 2019-08-20 10:54:30 +0200 (Tue, 20 Aug 2019) | 36 lines
    [clangd] Skip function bodies inside processed files while indexing
    
    Summary:
    This significantly improves performance of background indexing.
    
    We do not collect references and declarations inside the processed
    files, so this does not affect the final indexing results.
    
    The idea is borrowed from libclang, which has a similar optimization in
    its indexing functionality.
    
    Measurements show a nice decrease in indexing time, up to ~40% for
    building the whole index. These are not proper benchmarks, so one should
    not rely on these results too much.
    
    1. Rebuilding the whole index for LLVM:
      - Before. Total time: 14m58s.
        ./bin/clangd -pch-storage=memory < ./clangd.input  23917.67s user 515.86s system 2718% cpu 14:58.68 total
      - After. Total time: 8m41s.
        ./bin/clangd -pch-storage=memory < ./clangd.input  13627.29s user 288.10s system 2672% cpu 8:40.67 total
    
    2. Rebuilding index after removing shards matching '*clangd*' (case-insensitively):
      - Before. Total time: 30s.
        ./bin/clangd -pch-storage=memory < ./clangd.input  130.94s user 6.82s system 452% cpu 30.423 total
      - After. Total time: 26s.
        ./bin/clangd -pch-storage=memory < ./clangd.input  80.51s user 5.40s system 333% cpu 25.777 total
    
    Reviewers: kadircet, sammccall
    
    Reviewed By: kadircet
    
    Subscribers: MaskRay, jkorous, arphaman, cfe-commits
    
    Tags: #clang
    
    Differential Revision: https://reviews.llvm.org/D66226
    ------------------------------------------------------------------------
    
    llvm-svn: 369879

--- clang-tools-extra/clangd/index/SymbolCollector.cpp
+++ clang-tools-extra/clangd/index/SymbolCollector.cpp
@@ -146,17 +146,6 @@ getTokenRange(SourceLocation TokLoc, const SourceManager &SM,
           CreatePosition(TokLoc.getLocWithOffset(TokenLength))};
 }
 
-bool shouldIndexFile(const SourceManager &SM, FileID FID,
-                     const SymbolCollector::Options &Opts,
-                     llvm::DenseMap<FileID, bool> *FilesToIndexCache) {
-  if (!Opts.FileFilter)
-    return true;
-  auto I = FilesToIndexCache->try_emplace(FID);
-  if (I.second)
-    I.first->second = Opts.FileFilter(SM, FID);
-  return I.first->second;
-}
-
 // Return the symbol location of the token at \p TokLoc.
 llvm::Optional<SymbolLocation>
 getTokenLocation(SourceLocation TokLoc, const SourceManager &SM,
@@ -409,7 +398,7 @@ bool SymbolCollector::handleMacroOccurence(const IdentifierInfo *Name,
   S.SymInfo = index::getSymbolInfoForMacro(*MI);
   std::string FileURI;
   // FIXME: use the result to filter out symbols.
-  shouldIndexFile(SM, SM.getFileID(Loc), Opts, &FilesToIndexCache);
+  shouldIndexFile(SM.getFileID(Loc));
   if (auto DeclLoc =
           getTokenLocation(DefLoc, SM, Opts, PP->getLangOpts(), FileURI))
     S.CanonicalDeclaration = *DeclLoc;
@@ -539,7 +528,7 @@ void SymbolCollector::finish() {
         for (const auto &LocAndRole : It.second) {
           auto FileID = SM.getFileID(LocAndRole.first);
           // FIXME: use the result to filter out references.
-          shouldIndexFile(SM, FileID, Opts, &FilesToIndexCache);
+          shouldIndexFile(FileID);
           if (auto FileURI = GetURI(FileID)) {
             auto Range =
                 getTokenRange(LocAndRole.first, SM, ASTCtx->getLangOpts());
@@ -589,7 +578,7 @@ const Symbol *SymbolCollector::addDeclaration(const NamedDecl &ND, SymbolID ID,
   auto Loc = findNameLoc(&ND);
   assert(Loc.isValid() && "Invalid source location for NamedDecl");
   // FIXME: use the result to filter out symbols.
-  shouldIndexFile(SM, SM.getFileID(Loc), Opts, &FilesToIndexCache);
+  shouldIndexFile(SM.getFileID(Loc));
   if (auto DeclLoc =
           getTokenLocation(Loc, SM, Opts, ASTCtx->getLangOpts(), FileURI))
     S.CanonicalDeclaration = *DeclLoc;
@@ -649,7 +638,7 @@ void SymbolCollector::addDefinition(const NamedDecl &ND,
   auto Loc = findNameLoc(&ND);
   const auto &SM = ND.getASTContext().getSourceManager();
   // FIXME: use the result to filter out symbols.
-  shouldIndexFile(SM, SM.getFileID(Loc), Opts, &FilesToIndexCache);
+  shouldIndexFile(SM.getFileID(Loc));
   if (auto DefLoc =
           getTokenLocation(Loc, SM, Opts, ASTCtx->getLangOpts(), FileURI))
     S.Definition = *DefLoc;
@@ -739,6 +728,15 @@ bool SymbolCollector::isDontIncludeMeHeader(llvm::StringRef Content) {
       return true;
   }
   return false;
+}
+
+bool SymbolCollector::shouldIndexFile(FileID FID) {
+  if (!Opts.FileFilter)
+    return true;
+  auto I = FilesToIndexCache.try_emplace(FID);
+  if (I.second)
+    I.first->second = Opts.FileFilter(ASTCtx->getSourceManager(), FID);
+  return I.first->second;
 }
 
 } // namespace clangd
