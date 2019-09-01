1768ea11595c1c7877ee79fb71aa31940b4354be
TB Schardl <neboat@mit.edu>
2019-09-01 19:27:04 +0000

    [ValueTracking] Allow ValueTracking to ignore syncregion_start intrinsics similarly to other assume-like intrinsics.

--- llvm/lib/Analysis/ValueTracking.cpp
+++ llvm/lib/Analysis/ValueTracking.cpp
@@ -516,6 +516,7 @@ bool llvm::isAssumeLikeIntrinsic(const Instruction *I) {
       case Intrinsic::objectsize:
       case Intrinsic::ptr_annotation:
       case Intrinsic::var_annotation:
+      case Intrinsic::syncregion_start:
         return true;
       }
 
