17079dca43924a1f8f6d9209ecb615d269e38751
Romaric JODIN <rjodin@upmem.com>
2019-09-03 11:02:44 +0200

    dpu: lldb: improve dpu_attach and add dpu_list command

--- lldb/scripts/dpu/dpu_commands.py
+++ lldb/scripts/dpu/dpu_commands.py
@@ -4,31 +4,101 @@ import subprocess
 import os
 import lldb
 
-def set_debug_mode(debugger, rank, debug_mode):
-    debugger.HandleCommand(
-        "print (void) hw_set_debug_mode((dpu_rank_t *)"
-        + rank.GetValue()
-        + ", "
-        + str(debug_mode)
-        + ")")
 
-def get_dpu_from_command(command, debugger, target):
-    dpu_addr = 0
+def compute_dpu_pid(region_id, rank_id, slice_id, dpu_id):
+    return dpu_id + (slice_id << 16) + (rank_id << 32) + (region_id << 48) \
+        + 0x80000000
+
+
+def get_value_from_command(debugger, command, base):
+    return_obj = lldb.SBCommandReturnObject()
+    debugger.GetCommandInterpreter().HandleCommand("p " + command, return_obj)
+    if return_obj.GetStatus() != lldb.eReturnStatusSuccessFinishResult:
+        return False, 0
+    if return_obj.GetOutput() is None:
+        return True, 0
+    addr = int(re.search('.*= (.+)', return_obj.GetOutput()).group(1), base)
+    return True, addr
+
+
+def set_debug_mode(debugger, rank, debug_mode):
+    success, unused = get_value_from_command(
+        debugger,
+        "hw_set_debug_mode((dpu_rank_t *)"
+        + rank.GetValue() + ", " + str(debug_mode) + ")",
+        10)
+    return success
+
+
+def get_object_from_command(command, debugger, target, name, object_type,
+                            base):
+    addr = 0
     try:
-        dpu_addr = int(command, 16)
+        addr = int(command, base)
     except:
-        print("Interpreting command: '" + command + "'")
-        return_obj = lldb.SBCommandReturnObject()
-        debugger.GetCommandInterpreter().HandleCommand("p " + command,
-                                                       return_obj)
-        if return_obj.GetStatus() != lldb.eReturnStatusSuccessFinishResult:
+        success, addr = get_value_from_command(debugger, command, base)
+        if not(success):
             print("Could not interpret command '" + command + "'")
             sys.exit(1)
-        dpu_addr = int(re.search('dpu_t \*.*= (.+)', return_obj.GetOutput())
-                       .group(1),16)
-    print("Attaching to dpu: '" + hex(dpu_addr) + "'")
     return target.CreateValueFromExpression(
-        "dpu_to_attach","(struct dpu_t *)" + str(dpu_addr))
+        name, "(" + object_type + ")" + str(addr))
+
+
+def get_dpu_from_command(command, debugger, target):
+    return get_object_from_command(
+        command, debugger, target, "dpu", "struct dpu_t *", 16)
+
+
+def get_rank_from_command(command, debugger, target):
+    return get_object_from_command(
+        command, debugger, target, "rank", "struct dpu_rank_t *", 16)
+
+
+def get_region_id_and_rank_id(rank, target):
+    hw_dpu_rank_allocation_parameters_type = \
+        target.FindFirstType("hw_dpu_rank_allocation_parameters_t")
+    if not(hw_dpu_rank_allocation_parameters_type.IsValid()):
+        return -1, -1
+    rank_path = str(
+        rank.GetChildMemberWithName("description")
+        .GetChildMemberWithName("_internals")
+        .GetChildMemberWithName("data")
+        .Cast(hw_dpu_rank_allocation_parameters_type)
+        .GetChildMemberWithName("rank_fs")
+        .GetChildMemberWithName("rank_path")
+        )
+    region_id = int(re.search('dpu_region(.+)/', rank_path).group(1))
+    rank_id = int(re.search('dpu_rank(.+)"', rank_path).group(1))
+    return region_id, rank_id
+
+
+def get_dpu_program_path(dpu):
+    program_path = dpu.GetChildMemberWithName("runtime_context") \
+                      .GetChildMemberWithName("program_path")
+    if program_path.GetChildAtIndex(0).GetValue() is None:
+        return None
+    return re.search('"(.+)"', str(program_path)).group(1)
+
+
+def get_dpu_status(rank, slice_id, dpu_id):
+    run_context = rank.GetChildMemberWithName("runtime") \
+                      .GetChildMemberWithName("run_context")
+    assert run_context.IsValid()
+    dpus_running = run_context.GetChildMemberWithName("dpu_running")
+    assert dpus_running.IsValid()
+    dpus_in_fault = run_context.GetChildMemberWithName("dpu_in_fault")
+    assert dpus_in_fault.IsValid()
+    dpu_running = int(dpus_running.GetChildAtIndex(slice_id)
+                      .GetValue())
+    dpu_in_fault = int(dpus_in_fault.GetChildAtIndex(slice_id)
+                       .GetValue())
+    dpu_mask = 1 << dpu_id
+    if (dpu_mask & dpu_running) != 0:
+        return "RUNNING"
+    elif (dpu_mask & dpu_in_fault) != 0:
+        return "ERROR  "
+    else:
+        return "IDLE   "
 
 
 def dpu_attach(debugger, command, result, internal_dict):
@@ -38,12 +108,27 @@ def dpu_attach(debugger, command, result, internal_dict):
     target = debugger.GetSelectedTarget()
     dpu = get_dpu_from_command(command, debugger, target)
     assert dpu.IsValid()
+    print("Attaching to dpu '" + dpu.GetValue() + "'")
 
-    slice_id = dpu.GetChildMemberWithName("slice_id").GetValueAsUnsigned()
-    dpu_id = dpu.GetChildMemberWithName("dpu_id").GetValueAsUnsigned()
+    program_path = get_dpu_program_path(dpu)
+    if program_path is None:
+        print("Could not find program loaded in dpu")
+        sys.exit(1)
 
     rank = dpu.GetChildMemberWithName("rank")
     assert rank.IsValid()
+
+    region_id, rank_id = get_region_id_and_rank_id(rank, target)
+    if region_id == -1 or rank_id == -1:
+        print("Could not attach to simulator (fpga only)")
+        sys.exit(1)
+
+    slice_id = dpu.GetChildMemberWithName("slice_id").GetValueAsUnsigned()
+    dpu_id = dpu.GetChildMemberWithName("dpu_id").GetValueAsUnsigned()
+    if get_dpu_status(rank, slice_id, dpu_id) != "RUNNING":
+        print("Dpu is not running! Impossible to attach")
+        sys.exit(1)
+
     slice_info = rank.GetChildMemberWithName("runtime") \
         .GetChildMemberWithName("control_interface") \
         .GetChildMemberWithName("slice_info").GetChildAtIndex(slice_id)
@@ -59,26 +144,9 @@ def dpu_attach(debugger, command, result, internal_dict):
         .GetValueAsUnsigned()
     slice_target = (slice_target_type << 32) + slice_target_dpu_group_id
 
-    rank_path = str(
-        rank.GetChildMemberWithName("description")
-        .GetChildMemberWithName("_internals")
-        .GetChildMemberWithName("data")
-        .Cast(target.FindFirstType("hw_dpu_rank_allocation_parameters_t"))
-        .GetChildMemberWithName("rank_fs")
-        .GetChildMemberWithName("rank_path")
-        )
-    region_id = int(re.search('dpu_region(.+)/', rank_path).group(1))
-    rank_id = int(re.search('dpu_rank(.+)"', rank_path).group(1))
+    pid = compute_dpu_pid(region_id, rank_id, slice_id, dpu_id)
 
-    pid = dpu_id + (slice_id << 16) + (rank_id << 32) + (region_id << 48) \
-        + 0x80000000
-
-    program_path = \
-        re.search('"(.+)"',
-                  str(dpu.GetChildMemberWithName("runtime_context")
-                      .GetChildMemberWithName("program_path"))).group(1)
-
-    set_debug_mode(debugger, rank, 1)
+    assert set_debug_mode(debugger, rank, 1)
 
     lldb_server_dpu_env = os.environ.copy()
     lldb_server_dpu_env["UPMEM_LLDB_STRUCTURE_VALUE"] = str(structure_value)
@@ -104,6 +172,79 @@ def dpu_attach(debugger, command, result, internal_dict):
     assert process_dpu.IsValid()
 
     debugger.SetSelectedTarget(target)
-    set_debug_mode(debugger, rank, 0)
+    assert set_debug_mode(debugger, rank, 0)
 
     debugger.SetSelectedTarget(target_dpu)
+
+
+def dpu_get(rank_addr, slice_id, dpu_id, debugger, target):
+    return get_dpu_from_command("dpu_get((struct dpu_rank_t *)"
+                                + str(rank_addr)
+                                + ", " + str(slice_id)
+                                + ", " + str(dpu_id) + ")",
+                                debugger,
+                                target)
+
+
+def print_list(list, result):
+    if result is None:
+        return
+    result.PutCString("\nADDR \t\t\tID \t\tSTATUS \t\tPROGRAM\n")
+    for (dpu_addr, dpu_id, status, program) in list:
+        result.PutCString(
+            dpu_addr + " \t" + dpu_id + " \t" + status + " \t" + program)
+
+
+def dpu_list(debugger, command, result, internal_dict):
+    '''
+    usage: dpu_list
+    '''
+    success, nb_allocated_rank = \
+        get_value_from_command(
+            debugger, "dpu_rank_handler_dpu_rank_list_size", 10)
+    assert success
+
+    target = debugger.GetSelectedTarget()
+    result_list = []
+
+    for each_rank in range(0, nb_allocated_rank):
+        rank = get_rank_from_command(
+            "dpu_rank_handler_dpu_rank_list[" + str(each_rank) + "]",
+            debugger,
+            target)
+        assert rank.IsValid()
+        rank_addr = rank.GetValue()
+        if int(rank_addr, 16) == 0:
+            continue
+
+        region_id, rank_id = get_region_id_and_rank_id(rank, target)
+
+        slice_id = 0
+        dpu_id = 0
+        dpu = dpu_get(rank_addr, slice_id, dpu_id, debugger, target)
+        assert dpu.IsValid
+        while int(dpu.GetValue(), 16) != 0:
+            while int(dpu.GetValue(), 16) != 0:
+                program_path = get_dpu_program_path(dpu)
+                if program_path is None:
+                    program_path = ""
+
+                dpu_status = get_dpu_status(rank, slice_id, dpu_id)
+
+                result_list.append(("'" + dpu.GetValue() + "'",
+                                    str(region_id)
+                                    + "/" + str(rank_id)
+                                    + "/" + str(slice_id)
+                                    + "/" + str(dpu_id),
+                                    dpu_status,
+                                    "'" + program_path + "'"))
+
+                dpu_id = dpu_id + 1
+                dpu = dpu_get(rank_addr, slice_id, dpu_id, debugger, target)
+
+            dpu_id = 0
+            slice_id = slice_id + 1
+            dpu = dpu_get(rank_addr, slice_id, dpu_id, debugger, target)
+
+        print_list(result_list, result)
+        return result_list
