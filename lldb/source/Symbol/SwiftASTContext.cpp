01eeb9c53abc277c05fd575d10ed584d1198d3c9
Davide Italiano <ditaliano@apple.com>
2019-10-24 17:26:19 -0700

    [SwiftREPL] Registter typechecker & friends only once.
    <rdar://problem/56483355>

--- lldb/source/Symbol/SwiftASTContext.cpp
+++ lldb/source/Symbol/SwiftASTContext.cpp
@@ -3467,6 +3467,7 @@ swift::ASTContext *SwiftASTContext::GetASTContext() {
 
   // Set up the required state for the evaluator in the TypeChecker.
   (void)swift::createTypeChecker(*m_ast_context_ap);
+  registerIDERequestFunctions(m_ast_context_ap->evaluator);
   registerParseRequestFunctions(m_ast_context_ap->evaluator);
   registerTypeCheckerRequestFunctions(m_ast_context_ap->evaluator);
 
