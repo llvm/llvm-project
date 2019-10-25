fa88d6bd5723a10d476c60a66d5227159965459b
Davide Italiano <ditaliano@apple.com>
2019-10-24 17:27:10 -0700

    [REPL] Fix a crash when the completion returned contains only blanks.
    <rdar://problem/56483355>

--- lldb/source/Expression/REPL.cpp
+++ lldb/source/Expression/REPL.cpp
@@ -475,7 +475,7 @@ void REPL::IOHandlerComplete(IOHandler &io_handler,
   // Strip spaces from the line and see if we had only spaces
   if (request.GetRawLineUntilCursor().trim().empty()) {
     // Only spaces on this line, so just indent
-    request.AddCompletion(m_indent_str);
+    request.AddCompletion(m_indent_str, "", CompletionMode::Partial);
     return;
   }
 
