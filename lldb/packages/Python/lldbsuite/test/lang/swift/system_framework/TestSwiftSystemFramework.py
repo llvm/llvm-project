e9e842b72f8a9e7578c493e73eb09786d03fce54
Adrian Prantl <aprantl@apple.com>
2019-10-29 10:06:35 -0700

    Avoid adding any framework paths in /System/Library in process_one_module.
    The merit of process_one_module can be debated, but these are
    definitely wrong, system modules must be imported from the SDK instead.
    
    <rdar://problem/56280346>

---
+++ lldb/packages/Python/lldbsuite/test/lang/swift/system_framework/TestSwiftSystemFramework.py
@@ -0,0 +1,34 @@
+import lldb
+from lldbsuite.test.decorators import *
+import lldbsuite.test.lldbtest as lldbtest
+import lldbsuite.test.lldbutil as lldbutil
+import os
+import unittest2
+
+
+class TestSwiftAnyType(lldbtest.TestBase):
+
+    mydir = lldbtest.TestBase.compute_mydir(__file__)
+
+    @swiftTest
+    def test_system_framework(self):
+        """Make sure no framework paths into /System/Library are added"""
+        self.build()
+        target, process, thread, bkpt = lldbutil.run_to_source_breakpoint(
+            self, 'break here', lldb.SBFileSpec('main.swift'))
+
+        log = self.getBuildArtifact("types.log")
+        self.runCmd('log enable lldb types -f "%s"' % log)
+        self.expect("settings set target.use-all-compiler-flags true")
+        self.expect("expression -- 0")
+        pos = 0
+        neg = 0
+        with open(log, "r") as logfile:
+            for line in logfile:
+                if "process_one_module rejecting framework path" in line:
+                    pos += 1
+                elif "/System/Library/Frameworks" in line:
+                    neg += 1
+
+        self.assertGreater(pos, 0, "sanity check failed")
+        self.assertEqual(neg, 0, "found /System/Library/Frameworks in log")
