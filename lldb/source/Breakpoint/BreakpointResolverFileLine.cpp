abf345cd0debeff9b84f322bc06b83232637bef0
Adrian Prantl <aprantl@apple.com>
2019-10-29 15:02:55 -0700

    Mark all Swift-specific changes in Breakpoint with comments. NFC
    This is to facilitate identifying divergences and thus encourage
    upstreaming them and/or make merge conflict resolutions more obvious.

--- lldb/source/Breakpoint/BreakpointResolverFileLine.cpp
+++ lldb/source/Breakpoint/BreakpointResolverFileLine.cpp
@@ -12,7 +12,6 @@
 #include "lldb/Core/Module.h"
 #include "lldb/Symbol/CompileUnit.h"
 #include "lldb/Symbol/Function.h"
-#include "lldb/Symbol/SymbolFile.h"
 #include "lldb/Utility/Log.h"
 #include "lldb/Utility/StreamString.h"
 
