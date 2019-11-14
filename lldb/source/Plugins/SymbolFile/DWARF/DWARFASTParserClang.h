1f704953be96ece2959bde48af9037470f8f0a15
Adrian Prantl <aprantl@apple.com>
2019-11-14 08:57:32 -0800

    Use ForEachExternalModule in ParseTypeFromClangModule (NFC)
    I wanted to further simplify ParseTypeFromClangModule by replacing the
    hand-rolled loop with ForEachExternalModule, and then realized that
    ForEachExternalModule also had the problem of visiting the same leaf
    node an exponential number of times in the worst-case. This adds a set
    of searched_symbol_files set to the function as well as the ability to
    early-exit from it.
    
    Differential Revision: https://reviews.llvm.org/D70215
    
    (cherry picked from commit 0e45e60c6f316d095d878aea3c098202b39b5bee)
    
     Conflicts:
    	lldb/include/lldb/Symbol/CompileUnit.h

--- lldb/source/Plugins/SymbolFile/DWARF/DWARFASTParserClang.h
+++ lldb/source/Plugins/SymbolFile/DWARF/DWARFASTParserClang.h
@@ -109,7 +109,8 @@ protected:
                                const DWARFDIE &parent_die);
 
   /// Parse a structure, class, or union type DIE.
-  lldb::TypeSP ParseStructureLikeDIE(const DWARFDIE &die,
+  lldb::TypeSP ParseStructureLikeDIE(const lldb_private::SymbolContext &sc,
+                                     const DWARFDIE &die,
                                      ParsedDWARFTypeAttributes &attrs);
 
   lldb_private::Type *GetTypeForDIE(const DWARFDIE &die);
@@ -141,7 +142,8 @@ protected:
                                   const DWARFDIE &die, lldb::TypeSP type_sp);
 
   /// Follow Clang Module Skeleton CU references to find a type definition.
-  lldb::TypeSP ParseTypeFromClangModule(const DWARFDIE &die,
+  lldb::TypeSP ParseTypeFromClangModule(const lldb_private::SymbolContext &sc,
+                                        const DWARFDIE &die,
                                         lldb_private::Log *log);
 
   // Return true if this type is a declaration to a type in an external
