497fe4e6527bf9d9f53343f84b9ecfd8fde24667
Andrei Safronov <safronov@espressif.com>
2019-12-12 14:33:15 +0300

    [Xtensa] Add  descriptions of the Xtensa shift/load/store instructions.
    Modify tests to support new instructions.

--- llvm/test/MC/Xtensa/xtensa-invalid.s
+++ llvm/test/MC/Xtensa/xtensa-invalid.s
@@ -4,16 +4,49 @@
 
 LBL0:
 
+# imm12m
+movi a1, 3000
+# CHECK:      error: expected immediate in range [-2048, 2047]
+
 # imm8
 addi a1, a2, 300
 # CHECK:      error: expected immediate in range [-128, 127]
 
+# imm8
 addi a1, a2, -129
 # CHECK:      error: expected immediate in range [-128, 127]
 
 # imm8_sh8
 addmi a1, a2, 33 
 # CHECK:      error: expected immediate in range [-32768, 32512], first 8 bits should be zero
+
+# shimm1_31
+slli a1, a2, 0
+# CHECK:      error: expected immediate in range [1, 31]
+
+# uimm4
+srli a1, a2, 16
+# CHECK:      error: expected immediate in range [0, 15]
+
+# uimm5
+ssai 32
+# CHECK:      error: expected immediate in range [0, 31]
+
+# imm64n_4n
+ssai 32
+# CHECK:      error: expected immediate in range [0, 31]
+
+# offset8m8
+s8i a1, a2, 300
+# CHECK:      error: expected immediate in range [0, 255]
+
+# offset16m8
+l16si a1, a2, 512
+# CHECK:      error: expected immediate in range [0, 510], first bit should be zero
+
+# offset32m8
+l32i a1, a2, 1024
+# CHECK:      error: expected immediate in range [0, 1020], first 2 bits should be zero
 
 # Invalid number of operands
 addi a1, a2 # CHECK: :[[@LINE]]:1: error: too few operands for instruction
@@ -24,6 +57,7 @@ aaa a10, a12 # CHECK: :[[@LINE]]:1: error: unrecognized instruction mnemonic
 
 # Invalid register names
 addi a101, sp, 10 # CHECK: :[[@LINE]]:6: error: invalid operand for instruction
+wsr.uregister a2 # CHECK: :[[@LINE]]:1: error: invalid register name
 or r2, sp, a3 # CHECK: :[[@LINE]]:4: error: invalid operand for instruction
 
 # Invalid operand types
--- llvm/test/MC/Xtensa/xtensa-valid.s
+++ llvm/test/MC/Xtensa/xtensa-valid.s
@@ -43,6 +43,10 @@ LBL0:
 # CHECK-INST:  esync  
 # CHECK: encoding: [0x20,0x20,0x00]
  esync
+ 
+# CHECK-INST:  extui    a1, a2, 7, 8 
+# CHECK: encoding: [0x20,0x17,0x74]
+ extui a1, a2, 7, 8
 
 # CHECK-INST:  extw  
 # CHECK: encoding: [0xd0,0x20,0x00]
@@ -51,6 +55,19 @@ LBL0:
 # CHECK-INST:  isync 
 # CHECK: encoding: [0x00,0x20,0x00]
  isync
+
+# CHECK-INST:  l8ui    a2, a1, 3
+# CHECK: encoding: [0x22,0x01,0x03]
+ l8ui a2, sp, 3
+# CHECK-INST:  l16si   a3, a1, 4
+# CHECK: encoding: [0x32,0x91,0x02]
+ l16si a3, sp, 4
+# CHECK-INST: l16ui   a4, a1, 6
+# CHECK: encoding: [0x42,0x11,0x03]
+ l16ui a4, sp, 6
+# CHECK-INST: l32i    a5, a1, 8
+# CHECK: encoding: [0x52,0x21,0x02]
+ l32i a5, sp, 8
  
 # CHECK-INST: memw 
 # CHECK: encoding: [0xc0,0x20,0x00]
@@ -62,6 +79,10 @@ LBL0:
 # CHECK-INST: movgez  a3, a11, a12 
 # CHECK: encoding: [0xc0,0x3b,0xb3]
  movgez  a3,a11,a12
+
+# CHECK-INST: movi    a1, -2048 
+# CHECK: encoding: [0x12,0xa8,0x00]
+ movi  a1, -2048
 
 # CHECK-INST: movltz  a7, a8, a9 
 # CHECK: encoding: [0x90,0x78,0xa3]
@@ -81,10 +102,74 @@ LBL0:
 # CHECK-INST: or      a4, a5, a6
 # CHECK: encoding: [0x60,0x45,0x20]
  or a4, a5, a6
+  
+# CHECK-INST: rsr     a8, sar
+# CHECK: encoding: [0x80,0x03,0x03]
+ rsr a8, sar
+# CHECK-INST: rsr     a8, sar
+# CHECK: encoding: [0x80,0x03,0x03]
+ rsr.sar a8
+ # CHECK-INST: rsr     a8, sar
+# CHECK: encoding: [0x80,0x03,0x03]
+ rsr a8, 3
 
 # CHECK-INST: rsync 
 # CHECK: encoding: [0x10,0x20,0x00] 
  rsync
+
+# CHECK-INST: s8i     a2, a1, 3 
+# CHECK: encoding: [0x22,0x41,0x03] 
+ s8i a2, sp, 3
+# CHECK-INST: s16i    a3, a1, 4 
+# CHECK: encoding: [0x32,0x51,0x02] 
+ s16i a3, sp, 4
+# CHECK-INST: s32i    a5, a1, 8 
+# CHECK: encoding: [0x52,0x61,0x02] 
+ s32i a5, sp, 8
+
+# CHECK-INST: sll     a10, a11 
+# CHECK: encoding: [0x00,0xab,0xa1] 
+ sll a10, a11
+
+# CHECK-INST: slli    a5, a1, 15
+# CHECK: encoding: [0x10,0x51,0x11]
+ slli a5, a1, 15
+
+# CHECK-INST: sra     a12, a3
+# CHECK: encoding: [0x30,0xc0,0xb1]
+ sra a12, a3
+
+# CHECK-INST: srai    a8, a5, 0
+# CHECK: encoding: [0x50,0x80,0x21]
+ srai a8, a5, 0
+ 
+# CHECK-INST: src     a3, a4, a5 
+# CHECK: encoding: [0x50,0x34,0x81] 
+ src a3, a4, a5
+
+# CHECK-INST: srl     a6, a7 
+# CHECK: encoding: [0x70,0x60,0x91]
+ srl a6, a7
+
+# CHECK-INST: srli    a3, a4, 8
+# CHECK: encoding: [0x40,0x38,0x41]
+ srli a3, a4, 8
+
+# CHECK-INST: ssa8l   a14 
+# CHECK: encoding: [0x00,0x2e,0x40] 
+ ssa8l a14
+ 
+# CHECK-INST: ssai    31
+# CHECK: encoding: [0x10,0x4f,0x40]
+ ssai 31
+
+# CHECK-INST: ssl     a0
+# CHECK: encoding: [0x00,0x10,0x40]
+ ssl a0
+
+# CHECK-INST: ssr     a2
+# CHECK: encoding: [0x00,0x02,0x40]
+ ssr a2
 
 # CHECK-INST: sub     a8, a2, a1 
 # CHECK: encoding: [0x10,0x82,0xc0] 
@@ -99,7 +184,26 @@ LBL0:
 # CHECK: encoding: [0x70,0x41,0xf0] 
  subx8 a4, sp, a7
 
+# CHECK-INST: wsr     a8, sar
+# CHECK: encoding: [0x80,0x03,0x13] 
+ wsr a8, sar
+# CHECK-INST: wsr     a8, sar
+# CHECK: encoding: [0x80,0x03,0x13] 
+ wsr.sar a8
+# CHECK-INST: wsr     a8, sar
+# CHECK: encoding: [0x80,0x03,0x13]  
+ wsr a8, 3
+
 # CHECK-INST: xor     a6, a4, a5
 # CHECK: encoding: [0x50,0x64,0x30] 
  xor a6, a4, a5
 
+# CHECK-INST: xsr     a8, sar
+# CHECK: encoding: [0x80,0x03,0x61] 
+ xsr a8, sar
+# CHECK-INST: xsr     a8, sar
+# CHECK: encoding: [0x80,0x03,0x61
+ xsr.sar a8
+# CHECK-INST: xsr     a8, sar
+# CHECK: encoding: [0x80,0x03,0x61
+ xsr a8, 3
