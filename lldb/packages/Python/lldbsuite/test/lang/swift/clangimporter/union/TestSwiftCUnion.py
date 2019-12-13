dc9bd48051cb59b934347a9824288ceec1f5f7f8
Adrian Prantl <aprantl@apple.com>
2019-12-13 14:03:30 -0800

    Add support for C unions in Swift.
    C unions are struct types in Swift, but their fields are not
    implemented as stored properties, so the default implementation did
    not work for them. This patch adds special handling for unions by
    iterating over the imported VarDecls.
    
    rdar://problem/55026321
    (cherry picked from commit ca7ab28f66401b1168f7878f074f656223e70694)

---
+++ lldb/packages/Python/lldbsuite/test/lang/swift/clangimporter/union/TestSwiftCUnion.py
@@ -0,0 +1,19 @@
+import lldb
+from lldbsuite.test.decorators import *
+import lldbsuite.test.lldbtest as lldbtest
+import lldbsuite.test.lldbutil as lldbutil
+import os
+import unittest2
+
+
+class TestSwiftAnyType(lldbtest.TestBase):
+
+    mydir = lldbtest.TestBase.compute_mydir(__file__)
+
+    @swiftTest
+    def test_c_unions(self):
+        self.build()
+        target, process, thread, bkpt = lldbutil.run_to_source_breakpoint(
+            self, 'break here', lldb.SBFileSpec('main.swift'))
+        self.expect("target variable -- i", substrs=['42'])
+        self.expect("target variable -- d", substrs=['23'])
