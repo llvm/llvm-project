4876ab38323fbec6e9d808be51b28f7e3fd55cce
Michael Spencer <bigcheesegs@gmail.com>
2020-01-08 16:39:04 -0800

    [Clang][cc1] Support -fno-implicit-module-maps in -cc1.
    Gives the last of -f{no-}implicit-module-maps precedence.
    
    rdar://58883354
    (cherry picked from commit 2d28507121d64dbe9971eb859fd37cc0022f9fd0)

--- clang/lib/Frontend/CompilerInvocation.cpp
+++ clang/lib/Frontend/CompilerInvocation.cpp
@@ -2193,7 +2193,8 @@ static void ParseHeaderSearchArgs(HeaderSearchOptions &Opts, ArgList &Args,
   Opts.ModulesStrictContextHash = Args.hasArg(OPT_fmodules_strict_context_hash);
   Opts.ModulesValidateDiagnosticOptions =
       !Args.hasArg(OPT_fmodules_disable_diagnostic_validation);
-  Opts.ImplicitModuleMaps = Args.hasArg(OPT_fimplicit_module_maps);
+  Opts.ImplicitModuleMaps = Args.hasFlag(OPT_fimplicit_module_maps,
+                                         OPT_fno_implicit_module_maps, false);
   Opts.ModuleMapFileHomeIsCwd = Args.hasArg(OPT_fmodule_map_file_home_is_cwd);
   Opts.ModuleCachePruneInterval =
       getLastArgIntValue(Args, OPT_fmodules_prune_interval, 7 * 24 * 60 * 60);
