4be792c1bac900a4c52c04177955c3b89ee75c72
Jordi Chauzi <jchauzi@upmem.com>
2020-01-20 09:30:19 +0100

    dpu: lldb fixing dpu attach test
    In host.c: dpu_launch needs to be called on the dpu, not the set.

--- lldb/packages/Python/lldbsuite/test/dpu/dpu_attach/host.c
+++ lldb/packages/Python/lldbsuite/test/dpu/dpu_attach/host.c
@@ -7,7 +7,7 @@ int main() {
   DPU_ASSERT(dpu_alloc(1, "backend=hw,cycleAccurate=true", &set));
   DPU_ASSERT(dpu_load(set, DPU_EXE, NULL));
 
-  DPU_FOREACH(set, dpu) { DPU_ASSERT(dpu_launch(set, DPU_SYNCHRONOUS)); }
+  DPU_FOREACH(set, dpu) { DPU_ASSERT(dpu_launch(dpu, DPU_SYNCHRONOUS)); }
   DPU_ASSERT(dpu_free(set));
 
   return 0;
