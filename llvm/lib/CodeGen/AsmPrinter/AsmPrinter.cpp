0e2eea29cc4f360fc95638fbfc8aca24cec3fc5f
Fangrui Song <i@maskray.me>
2020-01-20 15:52:38 -0800

    [AsmPrinter] Don't emit __patchable_function_entries entry if "patchable-function-entry"="0"
    Add improve tests
    
    (cherry picked from commit d232c215669cb57f5eb4ead40a4a336220dbc429)

--- llvm/lib/CodeGen/AsmPrinter/AsmPrinter.cpp
+++ llvm/lib/CodeGen/AsmPrinter/AsmPrinter.cpp
@@ -3199,7 +3199,11 @@ void AsmPrinter::recordSled(MCSymbol *Sled, const MachineInstr &MI,
 
 void AsmPrinter::emitPatchableFunctionEntries() {
   const Function &F = MF->getFunction();
-  if (!F.hasFnAttribute("patchable-function-entry"))
+  unsigned PatchableFunctionEntry = 0;
+  (void)F.getFnAttribute("patchable-function-entry")
+      .getValueAsString()
+      .getAsInteger(10, PatchableFunctionEntry);
+  if (!PatchableFunctionEntry)
     return;
   const unsigned PointerSize = getPointerSize();
   if (TM.getTargetTriple().isOSBinFormatELF()) {
