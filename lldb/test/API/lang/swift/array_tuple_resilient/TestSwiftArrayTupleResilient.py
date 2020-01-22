757d071f697ecfed9b5b7571949d1afaacc83c14
Davide Italiano <ditaliano@apple.com>
2020-01-21 19:30:40 -0800

    [arm64/arm64e] Re-enable a couple of tests that have been fixed.
    <rdar://problem/58065423>
    
    (cherry picked from commit f83c8436e66c96028f2c547f809ae0aa42822bda)

--- lldb/test/API/lang/swift/array_tuple_resilient/TestSwiftArrayTupleResilient.py
+++ lldb/test/API/lang/swift/array_tuple_resilient/TestSwiftArrayTupleResilient.py
@@ -2,7 +2,7 @@ import lldbsuite.test.lldbinline as lldbinline
 from lldbsuite.test.decorators import *
 
 lldbinline.MakeInlineTest(__file__, globals(), decorators=[swiftTest, skipUnlessDarwin,
-    expectedFailureAll(archs=['arm64', 'arm64e', 'arm64_32'], bugnumber="<rdar://problem/58065423>")
+    expectedFailureAll(archs=['arm64_32'], bugnumber="<rdar://problem/58065423>")
                                                            , expectedFailureAll(bugnumber="rdar://60396797",
                         setting=('symbols.use-swift-clangimporter', 'false'))
 ])
--- lldb/test/API/lang/swift/foundation_value_types/url/TestSwiftFoundationTypeURL.py
+++ lldb/test/API/lang/swift/foundation_value_types/url/TestSwiftFoundationTypeURL.py
@@ -14,7 +14,7 @@ from lldbsuite.test.decorators import *
 
 lldbinline.MakeInlineTest(__file__, globals(),
                           decorators=[swiftTest,skipUnlessDarwin,
-                            expectedFailureAll(archs=['arm64', 'arm64e', 'arm64_32'], bugnumber="<rdar://problem/58065423>")
+                            expectedFailureAll(archs=['arm64_32'], bugnumber="<rdar://problem/58065423>")
                    ,        expectedFailureAll(bugnumber="rdar://60396797",
                            setting=('symbols.use-swift-clangimporter', 'false'))
 ])
