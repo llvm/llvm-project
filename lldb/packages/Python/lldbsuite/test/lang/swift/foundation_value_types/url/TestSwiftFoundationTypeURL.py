43bf73bd854963e2f39a3ed5dab0a1203d3bbb68
Davide Italiano <ditaliano@apple.com>
2020-01-23 15:49:03 -0800

    [swift] Re-enable two tests that now pass on arm64_32.
    <rdar://problem/58065423>

--- lldb/packages/Python/lldbsuite/test/lang/swift/foundation_value_types/url/TestSwiftFoundationTypeURL.py
+++ lldb/packages/Python/lldbsuite/test/lang/swift/foundation_value_types/url/TestSwiftFoundationTypeURL.py
@@ -13,5 +13,4 @@ import lldbsuite.test.lldbinline as lldbinline
 from lldbsuite.test.decorators import *
 
 lldbinline.MakeInlineTest(__file__, globals(),
-                          decorators=[swiftTest,skipUnlessDarwin,
-                            expectedFailureAll(archs=['arm64_32'], bugnumber="<rdar://problem/58065423>")])
+                          decorators=[swiftTest,skipUnlessDarwin])
