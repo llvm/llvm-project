llvm/llvm-project

commit 3dab785d525efd49ed491d54304eb049e4cfa24d

[IndexStore] Changed enum values
EventType::Modified was removed and EventType::WatcherGotInvalidated was added in DirectoryWatcher.

apple-llvm-split-commit: ed81bf9c9a60a71452f6b0a689e212ae2dca8583
apple-llvm-split-dir: clang/

clang/include/indexstore/indexstore.h
@@ -134,10 +134,10 @@ INDEXSTORE_PUBLIC bool
 indexstore_unit_event_notification_is_initial(indexstore_unit_event_notification_t);
 
 typedef enum {
-  INDEXSTORE_UNIT_EVENT_ADDED = 1,
-  INDEXSTORE_UNIT_EVENT_REMOVED = 2,
-  INDEXSTORE_UNIT_EVENT_MODIFIED = 3,
-  INDEXSTORE_UNIT_EVENT_DIRECTORY_DELETED = 4,
+  INDEXSTORE_UNIT_EVENT_REMOVED = 1,
+  INDEXSTORE_UNIT_EVENT_MODIFIED = 2,
+  INDEXSTORE_UNIT_EVENT_DIRECTORY_DELETED = 3,
+  INDEXSTORE_UNIT_EVENT_FAILURE = 4,
 } indexstore_unit_event_kind_t;
 
 INDEXSTORE_PUBLIC indexstore_unit_event_kind_t

clang/include/clang/Index/IndexDataStore.h
@@ -36,11 +36,11 @@ public:
   static unsigned getFormatVersion();
 
   enum class UnitEventKind {
-    Added,
     Removed,
     Modified,
     /// The directory got deleted. No more events will follow.
     DirectoryDeleted,
+    Failure
   };
   struct UnitEvent {
     UnitEventKind Kind;

clang/include/indexstore/IndexStoreCXX.h
@@ -152,19 +152,19 @@ public:
     UnitEvent(indexstore_unit_event_t obj) : obj(obj) {}
 
     enum class Kind {
-      Added,
       Removed,
       Modified,
       DirectoryDeleted,
+      Failure
     };
     Kind getKind() const {
       indexstore_unit_event_kind_t c_k = indexstore_unit_event_get_kind(obj);
       Kind K;
       switch (c_k) {
-      case INDEXSTORE_UNIT_EVENT_ADDED: K = Kind::Added; break;
       case INDEXSTORE_UNIT_EVENT_REMOVED: K = Kind::Removed; break;
       case INDEXSTORE_UNIT_EVENT_MODIFIED: K = Kind::Modified; break;
       case INDEXSTORE_UNIT_EVENT_DIRECTORY_DELETED: K = Kind::DirectoryDeleted; break;
+      case INDEXSTORE_UNIT_EVENT_FAILURE: K = Kind::Failure; break;
       }
       return K;
     }

clang/lib/IndexDataStore/IndexDataStore.cpp
@@ -115,14 +115,16 @@ bool IndexDataStoreImpl::startEventListening(bool waitInitialSync, std::string &
       IndexDataStore::UnitEventKind K;
       StringRef UnitName = sys::path::filename(evt.Filename);
       switch (evt.Kind) {
-      case DirectoryWatcher::EventKind::Added:
-        K = IndexDataStore::UnitEventKind::Added; break;
-      case DirectoryWatcher::EventKind::Removed:
+      case DirectoryWatcher::Event::EventKind::Removed:
         K = IndexDataStore::UnitEventKind::Removed; break;
-      case DirectoryWatcher::EventKind::Modified:
+      case DirectoryWatcher::Event::EventKind::Modified:
         K = IndexDataStore::UnitEventKind::Modified; break;
-      case DirectoryWatcher::EventKind::DirectoryDeleted:
+      case DirectoryWatcher::Event::EventKind::WatchedDirRemoved:
         K = IndexDataStore::UnitEventKind::DirectoryDeleted;
+        UnitName = StringRef();
+        break;
+      case DirectoryWatcher::Event::EventKind::WatcherGotInvalidated:
+        K = IndexDataStore::UnitEventKind::Failure;
         UnitName = StringRef();
         break;
       }
@@ -136,9 +138,11 @@ bool IndexDataStoreImpl::startEventListening(bool waitInitialSync, std::string &
   };
 
   DirWatcher = DirectoryWatcher::create(UnitPath.str(), OnUnitsChange,
-                                        waitInitialSync, Error);
-  if (!DirWatcher)
+                                        waitInitialSync);
+  if (!DirWatcher) {
+    Error = "failed to create directory watcher";
     return true;
+  }
 
   return false;
 }

clang/tools/IndexStore/IndexStore.cpp
@@ -164,14 +164,14 @@ indexstore_unit_event_get_kind(indexstore_unit_event_t c_evt) {
   auto *evt = static_cast<IndexDataStore::UnitEvent*>(c_evt);
   indexstore_unit_event_kind_t k;
   switch (evt->Kind) {
-    case IndexDataStore::UnitEventKind::Added:
-      k = INDEXSTORE_UNIT_EVENT_ADDED; break;
     case IndexDataStore::UnitEventKind::Removed:
       k = INDEXSTORE_UNIT_EVENT_REMOVED; break;
     case IndexDataStore::UnitEventKind::Modified:
       k = INDEXSTORE_UNIT_EVENT_MODIFIED; break;
     case IndexDataStore::UnitEventKind::DirectoryDeleted:
       k = INDEXSTORE_UNIT_EVENT_DIRECTORY_DELETED; break;
+    case IndexDataStore::UnitEventKind::Failure:
+      k = INDEXSTORE_UNIT_EVENT_FAILURE; break;
   }
   return k;
 }

clang/tools/c-index-test/core_main.cpp
@@ -765,24 +765,23 @@ static int watchDirectory(StringRef dirPath) {
     OS << "-- " << Events.size() << " :\n";
     for (auto evt : Events) {
       switch (evt.Kind) {
-        case DirectoryWatcher::EventKind::Added:
-          OS << "added: "; break;
-        case DirectoryWatcher::EventKind::Modified:
+        case DirectoryWatcher::Event::EventKind::Modified:
           OS << "modified: "; break;
-        case DirectoryWatcher::EventKind::Removed:
+        case DirectoryWatcher::Event::EventKind::Removed:
           OS << "removed: "; break;
-        case DirectoryWatcher::EventKind::DirectoryDeleted:
+        case DirectoryWatcher::Event::EventKind::WatchedDirRemoved:
           OS << "dir deleted: "; break;
+        case DirectoryWatcher::Event::EventKind::WatcherGotInvalidated:
+          OS << "watcher got invalidated: "; break;
 
       }
       OS << evt.Filename << '\n';
     }
   };
-  std::string Error;
   auto watcher = DirectoryWatcher::create(dirPath, receiver,
-                                          /*waitInitialSync=*/true, Error);
+                                          /*waitInitialSync=*/true);
   if (!watcher) {
-    errs() << "failed creating directory watcher: " << Error << '\n';
+    errs() << "failed creating directory watcher" << '\n';
     return 1;
   }
 

commit 821a6436ba5624c589ca42fe2edf6a94ff60fd71

[indexstore] Fix ABI of indexstore_unit_event_kind_t
Preserve the original numeric constants so that it does not change
meaning across versions. This fixes a breakage introduced in 3dab785d525e.

rdar://58838243

clang/include/indexstore/indexstore.h
@@ -158,10 +158,10 @@ INDEXSTORE_PUBLIC bool
 indexstore_unit_event_notification_is_initial(indexstore_unit_event_notification_t);
 
 typedef enum {
-  INDEXSTORE_UNIT_EVENT_REMOVED = 1,
-  INDEXSTORE_UNIT_EVENT_MODIFIED = 2,
-  INDEXSTORE_UNIT_EVENT_DIRECTORY_DELETED = 3,
-  INDEXSTORE_UNIT_EVENT_FAILURE = 4,
+  INDEXSTORE_UNIT_EVENT_REMOVED = 2,
+  INDEXSTORE_UNIT_EVENT_MODIFIED = 3,
+  INDEXSTORE_UNIT_EVENT_DIRECTORY_DELETED = 4,
+  INDEXSTORE_UNIT_EVENT_FAILURE = 5,
 } indexstore_unit_event_kind_t;
 
 INDEXSTORE_PUBLIC indexstore_unit_event_kind_t