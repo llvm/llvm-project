053a65e762f2b65c51b4cdef7580260fb51525ae
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2020-01-27 17:06:27 -0800

    Include fixit hints in -diagnostic-log-file output
    When you use the -diagnostic-log-file option, the resulting plist file
    does not contain the FixItHints associated with each diagnostic.  The
    serialized diagnostic file format does save the FixItHints. The plist
    diagnostics file should too.
    
    rdar://problem/57327549
    (cherry picked from commit 12fd2bff9ba127e637938c559a8b9c2f8c1d5cd7)

--- clang/include/clang/Frontend/LogDiagnosticPrinter.h
+++ clang/include/clang/Frontend/LogDiagnosticPrinter.h
@@ -19,6 +19,38 @@ class DiagnosticOptions;
 class LangOptions;
 
 class LogDiagnosticPrinter : public DiagnosticConsumer {
+  struct DiagEntryLocation {
+    /// The source file name, if available and if different from DiagEntry's
+    /// Filename.
+    std::string Filename;
+
+    /// The source file line number, if available.
+    unsigned Line;
+
+    /// The source file column number, if available.
+    unsigned Column;
+
+    /// The source file offset, if available.
+    unsigned Offset;
+  };
+
+  struct DiagEntryRange {
+    /// The range start.
+    LogDiagnosticPrinter::DiagEntryLocation Start;
+
+    /// The range end.
+    LogDiagnosticPrinter::DiagEntryLocation End;
+  };
+
+  struct DiagEntryFixIt {
+    /// The range of existing source file to act upon.
+    LogDiagnosticPrinter::DiagEntryRange RemoveRange;
+
+    /// The code to insert at the start of the range,
+    ///   after removal of the range; may be empty for pure removal.
+    std::string CodeToInsert;
+  };
+
   struct DiagEntry {
     /// The primary message line of the diagnostic.
     std::string Message;
@@ -40,7 +72,17 @@ class LogDiagnosticPrinter : public DiagnosticConsumer {
 
     /// The level of the diagnostic.
     DiagnosticsEngine::Level DiagnosticLevel;
+
+    /// The source ranges of the diagnostic.
+    SmallVector<LogDiagnosticPrinter::DiagEntryRange, 2> SourceRanges;
+
+    /// The fix-its for the diagnostic.
+    SmallVector<LogDiagnosticPrinter::DiagEntryFixIt, 2> FixIts;
   };
+
+  void
+  EmitDiagEntryLocation(llvm::raw_ostream &OS, StringRef Indent,
+                        const LogDiagnosticPrinter::DiagEntryLocation &Del);
 
   void EmitDiagEntry(llvm::raw_ostream &OS,
                      const LogDiagnosticPrinter::DiagEntry &DE);
