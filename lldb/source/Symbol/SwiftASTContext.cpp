b80c0a60a846cd9a1553a0d25f5fcbe408e62964
Hamish Knight <hamishknight@posteo.net>
2020-01-30 10:58:25 -0800

    Register SILOptimizer requests and IRGen passes

--- lldb/source/Symbol/SwiftASTContext.cpp
+++ lldb/source/Symbol/SwiftASTContext.cpp
@@ -3410,8 +3410,11 @@ swift::ASTContext *SwiftASTContext::GetASTContext() {
   registerParseRequestFunctions(m_ast_context_ap->evaluator);
   registerTypeCheckerRequestFunctions(m_ast_context_ap->evaluator);
   registerSILGenRequestFunctions(m_ast_context_ap->evaluator);
+  registerSILOptimizerRequestFunctions(m_ast_context_ap->evaluator);
   registerTBDGenRequestFunctions(m_ast_context_ap->evaluator);
   registerIRGenRequestFunctions(m_ast_context_ap->evaluator);
+
+  registerIRGenSILTransforms(*m_ast_context_ap);
 
   GetASTMap().Insert(m_ast_context_ap.get(), this);
 
