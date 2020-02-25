cb3cb4fae98261787fff520a17bbff6d786fe47b
Jordi Chauzi <jchauzi@upmem.com>
2020-02-25 14:14:34 +0100

    dpu: lldb: dpu_list test now uses the UPMEM_PROFILE to chose the DPU backend

--- lldb/packages/Python/lldbsuite/test/dpu/dpu_list/host.c
+++ lldb/packages/Python/lldbsuite/test/dpu/dpu_list/host.c
@@ -3,10 +3,7 @@
 int main() {
   struct dpu_set_t set;
 
-  if (dpu_alloc(1, "backend=hw,cycleAccurate=true", &set) != DPU_OK) {
-    DPU_ASSERT(dpu_alloc(1, "backend=simulator", &set));
-  }
-
+  DPU_ASSERT(dpu_alloc(1, NULL, &set));
   DPU_ASSERT(dpu_load(set, DPU_EXE, NULL));
   DPU_ASSERT(dpu_launch(set, DPU_SYNCHRONOUS));
 
