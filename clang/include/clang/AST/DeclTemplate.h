106e66474fe71014a162c9ef30136a77b5cc0993
Adrian Prantl <aprantl@apple.com>
2020-03-03 12:54:04 -0800

    Make Decl::setOwningModuleID() public. (NFC)
    This API is going to be used by LLDB to recreate owning module
    information for Decls deserialized from DWARF.
    
    Differential Revision: https://reviews.llvm.org/D75560
    
    (cherry picked from commit 2b366e75d41341e7cb2e763b20c4fc8fd3bd4b3c)

--- clang/include/clang/AST/DeclTemplate.h
+++ clang/include/clang/AST/DeclTemplate.h
@@ -1730,6 +1730,10 @@ public:
     return *TemplateArgs;
   }
 
+  void setTemplateArgs(TemplateArgumentList *Args) {
+    TemplateArgs = Args;
+  }
+
   /// Determine the kind of specialization that this
   /// declaration represents.
   TemplateSpecializationKind getSpecializationKind() const {
