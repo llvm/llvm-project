5c70421488026cd02eb583ab64aac2f52d0420a6
David Stuttard <david.stuttard@amd.com>
2020-03-18 22:10:51 +0000

    Revert "Revert "[codegen,amdgpu] Enhance MIR DIE and re-arrange it for AMDGPU.""
    This reverts commit 0e6fea325fb3027dd3e3e060538e3dca0511c34c.
    
    Change-Id: I83f44e76a6aedceab7d723a2f6414e25057929e7

--- llvm/lib/Target/AMDGPU/AMDGPUTargetMachine.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPUTargetMachine.cpp
@@ -1002,7 +1002,7 @@ void GCNPassConfig::addOptimizedRegAlloc() {
   insertPass(&RegisterCoalescerID, &SIPreAllocateWWMRegsID, false);
 
   if (EnableDCEInRA)
-    insertPass(&RenameIndependentSubregsID, &DeadMachineInstructionElimID);
+    insertPass(&DetectDeadLanesID, &DeadMachineInstructionElimID);
 
   TargetPassConfig::addOptimizedRegAlloc();
 }
