3979d6c5d5f2e2b2c16723302e0331478c03a4d9
Nathan Lanza <nathan@lanza.io>
2020-04-01 17:12:56 -0400

    [lldb] Fix a usage of SwiftFunc::getName to account for Swift change
    Swift changes this member to SwiftFunc::getBaseIdentifier in
    swift/master-next

--- lldb/source/Plugins/ExpressionParser/Swift/SwiftASTManipulator.cpp
+++ lldb/source/Plugins/ExpressionParser/Swift/SwiftASTManipulator.cpp
@@ -283,8 +283,8 @@ void SwiftASTManipulatorBase::DoInitialization() {
             break;
           }
         }
-      } else if (FD->hasName() &&
-                 FD->getName().str().startswith(m_wrapper_func_prefix)) {
+      } else if (FD->hasName() && FD->getBaseIdentifier().str().startswith(
+                                      m_wrapper_func_prefix)) {
         m_wrapper_decl = FD;
       }
 
