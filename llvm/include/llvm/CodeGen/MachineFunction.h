df7160df7c762fd82491f7afc1bdbf71b5aacc80
Daniel Sanders <daniel_l_sanders@apple.com>
2020-04-03 15:55:15 -0700

    Allow MachineFunction to obtain non-const Function (to enable MIR-level debugify)
    Summary:
    To debugify MIR, we need to be able to create metadata and to do that, we
    need a non-const Module. However, MachineFunction only had a const reference
    to the Function preventing this.
    
    Reviewers: aprantl, bogner
    
    Subscribers: hiraditya, llvm-commits
    
    Tags: #llvm
    
    Differential Revision: https://reviews.llvm.org/D77439
    
    (cherry picked from commit 35b7b0851b1957d4f6f14f023eb3b4571cadc3d7)

--- llvm/include/llvm/CodeGen/MachineFunction.h
+++ llvm/include/llvm/CodeGen/MachineFunction.h
@@ -221,7 +221,7 @@ struct LandingPadInfo {
 };
 
 class MachineFunction {
-  const Function &F;
+  Function &F;
   const LLVMTargetMachine &Target;
   const TargetSubtargetInfo *STI;
   MCContext &Ctx;
@@ -414,7 +414,7 @@ public:
   using VariableDbgInfoMapTy = SmallVector<VariableDbgInfo, 4>;
   VariableDbgInfoMapTy VariableDbgInfos;
 
-  MachineFunction(const Function &F, const LLVMTargetMachine &Target,
+  MachineFunction(Function &F, const LLVMTargetMachine &Target,
                   const TargetSubtargetInfo &STI, unsigned FunctionNum,
                   MachineModuleInfo &MMI);
   MachineFunction(const MachineFunction &) = delete;
@@ -451,6 +451,9 @@ public:
 
   /// Return the DataLayout attached to the Module associated to this MF.
   const DataLayout &getDataLayout() const;
+
+  /// Return the LLVM function that this machine code represents
+  Function &getFunction() { return F; }
 
   /// Return the LLVM function that this machine code represents
   const Function &getFunction() const { return F; }
