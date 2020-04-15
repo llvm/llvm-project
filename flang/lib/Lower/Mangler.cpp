13be63b100c67ec64fc83262300d5b4f39eb17ce
Varun Jayathirtha <vjayathirtha@nvidia.com>
2020-04-15 10:01:35 -0700

    Instantiate global scalar values. 1. Initialize scalar intrinsic types 2. Add test global inits. 3. Add TODO place holders for things that need to be implemented

--- flang/lib/Lower/Mangler.cpp
+++ flang/lib/Lower/Mangler.cpp
@@ -8,7 +8,6 @@
 
 #include "flang/Lower/Mangler.h"
 #include "flang/Common/reference.h"
-#include "flang/Lower/Todo.h"
 #include "flang/Lower/Utils.h"
 #include "flang/Optimizer/Dialect/FIRType.h"
 #include "flang/Optimizer/Support/InternalNames.h"
@@ -66,8 +65,8 @@ findInterfaceIfSeperateMP(const Fortran::semantics::Symbol &symbol) {
 // Mangle the name of `symbol` to make it unique within FIR's symbol table using
 // the FIR name mangler, `mangler`
 std::string
-Fortran::lower::mangle::mangleName(const Fortran::semantics::Symbol &symbol,
-                                   bool keepExternalInScope) {
+Fortran::lower::mangle::mangleName(fir::NameUniquer &uniquer,
+                                   const Fortran::semantics::Symbol &symbol) {
   // Resolve host and module association before mangling
   const auto &ultimateSymbol = symbol.GetUltimate();
   auto symbolName = toStringRef(ultimateSymbol.name());
@@ -75,14 +74,12 @@ Fortran::lower::mangle::mangleName(const Fortran::semantics::Symbol &symbol,
   return std::visit(
       Fortran::common::visitors{
           [&](const Fortran::semantics::MainProgramDetails &) {
-            return fir::NameUniquer::doProgramEntry().str();
+            return uniquer.doProgramEntry().str();
           },
           [&](const Fortran::semantics::SubprogramDetails &) {
             // Mangle external procedure without any scope prefix.
-            if (!keepExternalInScope &&
-                Fortran::semantics::IsExternal(ultimateSymbol))
-              return fir::NameUniquer::doProcedure(llvm::None, llvm::None,
-                                                   symbolName);
+            if (Fortran::semantics::IsExternal(ultimateSymbol))
+              return uniquer.doProcedure(llvm::None, llvm::None, symbolName);
             // Separate module subprograms must be mangled according to the
             // scope where they were declared (the symbol we have is the
             // definition).
@@ -90,67 +87,38 @@ Fortran::lower::mangle::mangleName(const Fortran::semantics::Symbol &symbol,
             if (const auto *mpIface = findInterfaceIfSeperateMP(ultimateSymbol))
               interface = mpIface;
             auto modNames = moduleNames(*interface);
-            return fir::NameUniquer::doProcedure(modNames, hostName(*interface),
-                                                 symbolName);
+            return uniquer.doProcedure(modNames, hostName(*interface),
+                                       symbolName);
           },
           [&](const Fortran::semantics::ProcEntityDetails &) {
             // Mangle procedure pointers and dummy procedures as variables
             if (Fortran::semantics::IsPointer(ultimateSymbol) ||
                 Fortran::semantics::IsDummy(ultimateSymbol))
-              return fir::NameUniquer::doVariable(moduleNames(ultimateSymbol),
-                                                  hostName(ultimateSymbol),
-                                                  symbolName);
+              return uniquer.doVariable(moduleNames(ultimateSymbol),
+                                        hostName(ultimateSymbol), symbolName);
             // Otherwise, this is an external procedure, even if it does not
             // have an explicit EXTERNAL attribute. Mangle it without any
             // prefix.
-            return fir::NameUniquer::doProcedure(llvm::None, llvm::None,
-                                                 symbolName);
+            return uniquer.doProcedure(llvm::None, llvm::None, symbolName);
           },
           [&](const Fortran::semantics::ObjectEntityDetails &) {
             auto modNames = moduleNames(ultimateSymbol);
             auto optHost = hostName(ultimateSymbol);
             if (Fortran::semantics::IsNamedConstant(ultimateSymbol))
-              return fir::NameUniquer::doConstant(modNames, optHost,
-                                                  symbolName);
-            return fir::NameUniquer::doVariable(modNames, optHost, symbolName);
+              return uniquer.doConstant(modNames, optHost, symbolName);
+            return uniquer.doVariable(modNames, optHost, symbolName);
           },
-          [&](const Fortran::semantics::CommonBlockDetails &) {
-            return fir::NameUniquer::doCommonBlock(symbolName);
+          [&](const Fortran::semantics::ObjectEntityDetails &) {
+            auto modNames = moduleNames(ultimateSymbol);
+            return uniquer.doVariable(modNames, hostName(ultimateSymbol),
+                                      toStringRef(symbolName));
           },
-          [&](const Fortran::semantics::DerivedTypeDetails &) -> std::string {
-            // Derived type mangling must used mangleName(DerivedTypeSpec&) so
-            // that kind type parameter values can be mangled.
-            llvm::report_fatal_error(
-                "only derived type instances can be mangled");
+          [](const auto &) -> std::string {
+            assert(false);
+            return {};
           },
-          [](const auto &) -> std::string { TODO_NOLOC("symbol mangling"); },
       },
       ultimateSymbol.details());
-}
-
-std::string Fortran::lower::mangle::mangleName(
-    const Fortran::semantics::DerivedTypeSpec &derivedType) {
-  // Resolve host and module association before mangling
-  const auto &ultimateSymbol = derivedType.typeSymbol().GetUltimate();
-  auto symbolName = toStringRef(ultimateSymbol.name());
-  auto modNames = moduleNames(ultimateSymbol);
-  auto optHost = hostName(ultimateSymbol);
-  llvm::SmallVector<std::int64_t> kinds;
-  for (const auto &param :
-       Fortran::semantics::OrderParameterDeclarations(ultimateSymbol)) {
-    const auto &paramDetails =
-        param->get<Fortran::semantics::TypeParamDetails>();
-    if (paramDetails.attr() == Fortran::common::TypeParamAttr::Kind) {
-      const auto *paramValue = derivedType.FindParameter(param->name());
-      assert(paramValue && "derived type kind parameter value not found");
-      auto paramExpr = paramValue->GetExplicit();
-      assert(paramExpr && "derived type kind param not explicit");
-      auto init = Fortran::evaluate::ToInt64(paramValue->GetExplicit());
-      assert(init && "derived type kind param is not constant");
-      kinds.emplace_back(*init);
-    }
-  }
-  return fir::NameUniquer::doType(modNames, optHost, symbolName, kinds);
 }
 
 std::string Fortran::lower::mangle::demangleName(llvm::StringRef name) {
