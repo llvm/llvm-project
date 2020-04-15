e08f71a6ebf3ac88916d1e198d4ecbe45a65a276
Eric <eschweitz@nvidia.com>
2020-04-15 09:53:50 -0700

    This is some initial work on getting Burnside to use the new symbol map for lowering.  This piece primarily gets started in the direction of being able to lower dynamic arrays and character types correctly.
    There is still more work in fully leveraging this in the bridge, but all the
    existing tests pass with this rewrite.
    
    FIR changes:
    
    This patch changes the FIR region operations to be more like those found in
    the loop dialect.  The changes are driven by the desire to be able to convert
    FIR into a register ssa form.  The changes are specifically:
    
      - fir.loop has been replaced by fir.do_loop. The new form has the same
        semantics (inclusive bounds, unordered), but can now carry ssa-values
        around the loop and return them as results to the parent.
      - fir.where has been replaced by fir.if. This new form is identical to
        the current loop.if operation.
      - fir.iterate_while has been added. This operation is very similar to
        fir.loop with the addition that it requires a single loop-carried bool
        value that signals an early-exit condition to the operation.  While that
        value is true, iterate_while will continue to iterate. When it becomes
        false, the loop is exited.
      - fir.result is the new terminator for the above ops to facilitate the
        carrying of ssa-values through block arguments (phi nodes).
    
    Change the syntax for fir.iterate_while to make it clear that the iterate condition is required.
    Fixes bugs with lowering of fir.iterate_while to CFG.  The lowered code produced is now as follows.
    
        %0 = llvm.mlir.constant(1 : index) : !llvm.i64
        %1 = llvm.mlir.constant(100 : index) : !llvm.i64
        llvm.br ^bb1(%0, %arg0, %arg1 : !llvm.i64, !llvm.i1, !llvm.i32)
      ^bb1(%2: !llvm.i64, %3: !llvm.i1, %4: !llvm.i32):	// 2 preds: ^bb0, ^bb2
        %5 = llvm.icmp "slt" %2, %1 : !llvm.i64
        %6 = llvm.and %5, %3 : !llvm.i1
        llvm.cond_br %6, ^bb2, ^bb3
      ^bb2:	// pred: ^bb1
        %7 = ... : !llvm.i1
        %8 = llvm.add %2, %0 : !llvm.i64
        llvm.br ^bb1(%8, %7, %4 : !llvm.i64, !llvm.i1, !llvm.i32)
      ^bb3:	// pred: ^bb1
    
    [review comment] improve description text to make it more clear
    
    [review] minor cleanups per review comments

--- flang/test/Lower/character-assignment.f90
+++ flang/test/Lower/character-assignment.f90
@@ -1,23 +1,26 @@
-! RUN: bbc %s -o "-" -emit-fir | FileCheck %s
+! RUN: bbc %s -o - -emit-fir | FileCheck %s
+! RUN: bbc %s -o - | FileCheck --check-prefix=UNBOX %s
 
 ! Simple character assignment tests
+! UNBOX-LABEL: assign1
 ! CHECK-LABEL: assign1
 subroutine assign1(lhs, rhs)
   character(*, 1) :: lhs, rhs
   lhs = rhs
-  ! Unboxing
-  ! CHECK-DAG:[[lhs:%[0-9]+]]:2 = fir.unboxchar %arg0
-  ! CHECK-DAG:[[rhs:%[0-9]+]]:2 = fir.unboxchar %arg1
-
   ! Compute minimum length
-  ! CHECK-DAG:%[[cmp_len:[0-9]+]] = cmpi "slt", [[lhs]]#1, [[rhs]]#1
-  ! CHECK-DAG:[[min_len:%[0-9]+]] = select %[[cmp_len]], [[lhs]]#1, [[rhs]]#1
+  ! UNBOX-DAG: %[[lhs:.*]]:2 = fir.unboxchar %arg0
+  ! UNBOX-DAG: %[[rhs:.*]]:2 = fir.unboxchar %arg1
+  ! UNBOX: %[[cmp_len:[0-9]+]] = cmpi "slt", %[[lhs]]#1, %[[rhs]]#1
+  ! UNBOX-NEXT: %[[min_len:[0-9]+]] = select %[[cmp_len]], %[[lhs]]#1, %[[rhs]]#1
+
+  ! CHECK: %[[cmp_len:[0-9]+]] = cmpi "slt", %[[lhs:.*]]#1, %[[rhs:.*]]#1
+  ! CHECK-NEXT: %[[min_len:[0-9]+]] = select %[[cmp_len]], %[[lhs]]#1, %[[rhs]]#1
 
   ! Allocate temp in case rhs and lhs may overlap
-  ! CHECK: [[tmp:%[0-9]+]] = fir.alloca !fir.char<1>, [[min_len]]
+  ! CHECK: %[[tmp:.*]] = fir.alloca !fir.char<1>, %[[min_len]]
 
   ! Copy of rhs into temp
-  ! CHECK: fir.loop [[i:%[[:alnum:]_]+]]
+  ! CHECK: fir.do_loop [[i:%[[:alnum:]_]+]]
     ! CHECK-DAG: [[rhs_addr:%[0-9]+]] = fir.coordinate_of [[rhs]]#0, [[i]]
     ! CHECK-DAG: [[tmp_addr:%[0-9]+]] = fir.coordinate_of [[tmp]], [[i]]
     ! CHECK-DAG: [[rhs_elt:%[0-9]+]] = fir.load [[rhs_addr]]
@@ -25,7 +28,7 @@ subroutine assign1(lhs, rhs)
   ! CHECK: }
 
   ! Copy of temp into lhs
-  ! CHECK: fir.loop [[i:%[[:alnum:]]+]]
+  ! CHECK: fir.do_loop [[i:%[[:alnum:]]+]]
     ! CHECK-DAG: [[tmp_addr:%[0-9]+]] = fir.coordinate_of [[tmp]], [[i]]
     ! CHECK-DAG: [[lhs_addr:%[0-9]+]] = fir.coordinate_of [[lhs]]#0, [[i]]
     ! CHECK-DAG: [[tmp_elt:%[0-9]+]] = fir.load [[tmp_addr]]
@@ -35,7 +38,7 @@ subroutine assign1(lhs, rhs)
   ! Padding
   ! CHECK: [[c32:%[[:alnum:]_]+]] = constant 32 : i8
   ! CHECK: [[blank:%[0-9]+]] = fir.convert [[c32]] : (i8) -> !fir.char<1>
-  ! CHECK: fir.loop [[i:%[[:alnum:]_]+]]
+  ! CHECK: fir.do_loop [[i:%[[:alnum:]_]+]]
     ! CHECK-DAG: [[lhs_addr:%[0-9]+]] = fir.coordinate_of [[lhs]]#0, [[i]]
     ! CHECK: fir.store [[blank]] to [[lhs_addr]]
   ! CHECK: }
@@ -47,54 +50,57 @@ subroutine assign_substring1(str, rhs, lb, ub)
   character(*, 1) :: rhs, str
   integer(8) :: lb, ub
   str(lb:ub) = rhs
-  ! CHECK-DAG: [[lb:%[0-9]+]] = fir.load %arg2
-  ! CHECK-DAG: [[ub:%[0-9]+]] = fir.load %arg3
-  ! CHECK-DAG: [[str:%[0-9]+]]:2 = fir.unboxchar %arg0
+  ! CHECK-DAG: %[[lb:.*]] = fir.load %arg2
+  ! CHECK-DAG: %[[ub:.*]] = fir.load %arg3
+  ! CHECK: %[[str:.*]]:2 = fir.unboxchar %arg0
 
   ! Compute substring offset
-  ! CHECK-DAG: [[lbi:%[0-9]+]] = fir.convert [[lb]] : (i64) -> index
-  ! CHECK-DAG: [[c1:%[[:alnum:]_]+]] = constant 1
-  ! CHECK-DAG: [[offset:%[0-9]+]] = subi [[lbi]], [[c1]]
-  ! CHECK-DAG: [[lhs_addr:%[0-9]+]] = fir.coordinate_of [[str]]#0, [[offset]]
+  ! CHECK-DAG: %[[lbi:.*]] = fir.convert %[[lb]] : (i64) -> index
+  ! CHECK-DAG: %[[c1:.*]] = constant 1
+  ! CHECK-DAG: %[[offset:.*]] = subi %[[lbi]], %[[c1]]
+  ! CHECK-DAG: %[[lhs_addr:.*]] = fir.coordinate_of %[[str]]#0, %[[offset]]
 
 
   ! Compute substring length
-  ! CHECK-DAG: [[diff:%[0-9]+]] = subi [[ub]], [[lb]]
-  ! CHECK-DAG: [[c1:%[[:alnum:]_]+]] = constant 1
-  ! CHECK-DAG: [[pre_lhs_len:%[0-9]+]] = addi [[diff]], [[c1]]
-  ! CHECK-DAG: [[c0:%[[:alnum:]_]+]] = constant 0
-  ! CHECK-DAG: [[cmp_len:%[0-9]+]] = cmpi "slt", [[pre_lhs_len]], [[c0]]
-  ! CHECK-DAG: [[lhs_len:%[0-9]+]] = select [[cmp_len]], [[c0]], [[pre_lhs_len]]
+  ! CHECK-DAG: %[[diff:.*]] = subi %[[ub]], %[[lb]]
+  ! CHECK-DAG: %[[c1:.*]] = constant 1
+  ! CHECK-DAG: %[[pre_lhs_len:.*]] = addi %[[diff]], %[[c1]]
+  ! CHECK-DAG: %[[c0:.*]] = constant 0
+  ! CHECK-DAG: %[[cmp_len:.*]] = cmpi "slt", %[[pre_lhs_len]], %[[c0]]
+  ! CHECK-DAG: %[[lhs_len:.*]] = select %[[cmp_len]], %[[c0]], %[[pre_lhs_len]]
 
-  ! CHECK: [[lhs_box:%[0-9]+]] = fir.emboxchar [[lhs_addr]], [[lhs_len]]
+  ! CHECK: %[[lhs_box:.*]] = fir.emboxchar %[[lhs_addr]], %[[lhs_len]]
 
   ! The rest of the assignment is just as the one above, only test that the
   ! substring box is the one used
   ! ...
-  ! CHECK: [[lhs:%[0-9]+]]:2 = fir.unboxchar [[lhs_box]]
+  ! CHECK: %[[lhs:.*]]:2 = fir.unboxchar %[[lhs_box]]
   ! ...
-  ! CHECK: fir.coordinate_of [[lhs]]#0, {{.*}}
+  ! CHECK: fir.coordinate_of %[[lhs]]#0,
   ! ...
 end subroutine
 
+! UNBOX-LABEL: assign_constant
 ! CHECK-LABEL: assign_constant
 ! CHECK: (%[[ARG:.*]]:{{.*}})
 subroutine assign_constant(lhs)
   character(*, 1) :: lhs
-  ! CHECK-DAG: %[[lhs:.*]]:2 = fir.unboxchar %[[ARG]] :
+  ! UNBOX: %[[lhs:.*]]:2 = fir.unboxchar %arg0
   ! CHECK-DAG: %[[tmp:.*]] = fir.address_of(@{{.*}}) :
   lhs = "Hello World"
-  ! CHECK: fir.loop %[[i:.*]] = %{{.*}} to %{{.*}} {
+  ! CHECK: fir.do_loop %[[i:.*]] = %{{.*}} to %{{.*}} {
     ! CHECK-DAG: %[[tmp_addr:.*]] = fir.coordinate_of %[[tmp]], %[[i]]
     ! CHECK-DAG: %[[tmp_elt:.*]] = fir.load %[[tmp_addr]]
-    ! CHECK-DAG: %[[lhs_addr:.*]] = fir.coordinate_of %[[lhs]]#0, %[[i]]
+    ! UNBOX: = fir.coordinate_of %[[lhs]]#0, %
+    ! CHECK-DAG: %[[lhs_addr:.*]] = fir.coordinate_of %[[lhs:.*]]#0, %[[i]]
     ! CHECK: fir.store %[[tmp_elt]] to %[[lhs_addr]]
   ! CHECK: }
 
   ! Padding
   ! CHECK-DAG: %[[c32:.*]] = constant 32 : i8
   ! CHECK-DAG: %[[blank:.*]] = fir.convert %[[c32]] : (i8) -> !fir.char<1>
-  ! CHECK: fir.loop %[[j:.*]] = %{{.*}} to %{{.*}} {
+  ! CHECK: fir.do_loop %[[j:.*]] = %{{.*}} to %{{.*}} {
+    ! UNBOX: = fir.coordinate_of %[[lhs]]#0, %
     ! CHECK: %[[jhs_addr:.*]] = fir.coordinate_of %[[lhs]]#0, %[[j]]
     ! CHECK: fir.store %[[blank]] to %[[jhs_addr]]
   ! CHECK: }
