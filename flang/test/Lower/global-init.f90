4b8a1081f3861c6f960ee8ea3c325fd735390e49
Varun Jayathirtha <vjayathirtha@nvidia.com>
2020-04-24 14:23:03 -0700

    Instantiate global single dimensional array values. Add global array initializers test

--- flang/test/Lower/global-init.f90
+++ flang/test/Lower/global-init.f90
@@ -5,7 +5,6 @@ program bar
 ! CHECK: fir.global @[[name1]]
   integer, save :: my_data = 1
   print *, my_data
-call foo
 contains
 subroutine foo()
 ! CHECK: fir.address_of(@[[name2:.*foo.*my_data]])
@@ -19,5 +18,15 @@ subroutine foo2()
   integer, save :: my_data
   my_data = 4
   print *, my_data
+end subroutine
+subroutine foo3()
+! CHECK: fir.address_of(@[[name4:.*foo3.*idata]]){{.*}}fir.array<5xi32>
+! CHECK: fir.address_of(@[[name5:.*foo3.*rdata]]){{.*}}fir.array<3xf16>
+! CHECK: fir.global @[[name4]]{{.*}}fir.array<5xi32>
+! CHECK: fir.global @[[name5]]{{.*}}fir.array<3xf16>
+  integer*4, dimension(5), save :: idata = (/ (i*i, i=1,5) /)
+  real*2, dimension(7:9), save :: rdata = (/100., 99., 98./)
+  print *, rdata(9)
+  print *, idata(3)
 end subroutine
 end program
