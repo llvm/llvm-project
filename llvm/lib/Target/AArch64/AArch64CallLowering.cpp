ba7393989761c81f720559fbfe95813e77d4e1bf
Amara Emerson <aemerson@apple.com>
2020-04-24 12:05:44 -0700

    [AArch64][GlobalISel] Fix sub-64b stack parameter passing on Darwin.
    A previous bug fix for varargs introduced a regression where we would
    incorrectly widen some stores to memory when passing i8/i16 parameters on the
    stack. This didn't show up seemingly because it only happens when there is
    no signext/zeroext parameter attribute, which I think for Darwin clang adds.
    
    Swift however seems to be a different story, and a plain anyext on the parameter
    triggered the bug.
    
    To fix this, I've added a new ValueHandler::assignValueToAddress type override
    which lets us distiguish between varargs and fixed args (we still need this
    widening behaviour for varargs to fix the original bug in 2018).
    
    rdar://61353552
    (cherry picked from commit dbb035677103c0b903b78a561b74b5fb4de3dc99)

--- llvm/lib/Target/AArch64/AArch64CallLowering.cpp
+++ llvm/lib/Target/AArch64/AArch64CallLowering.cpp
@@ -175,15 +175,30 @@ struct OutgoingArgHandler : public CallLowering::ValueHandler {
 
   void assignValueToAddress(Register ValVReg, Register Addr, uint64_t Size,
                             MachinePointerInfo &MPO, CCValAssign &VA) override {
-    if (VA.getLocInfo() == CCValAssign::LocInfo::AExt) {
-      Size = VA.getLocVT().getSizeInBits() / 8;
-      ValVReg = MIRBuilder.buildAnyExt(LLT::scalar(Size * 8), ValVReg)
-                    ->getOperand(0)
-                    .getReg();
-    }
     auto MMO = MIRBuilder.getMF().getMachineMemOperand(
         MPO, MachineMemOperand::MOStore, Size, 1);
     MIRBuilder.buildStore(ValVReg, Addr, *MMO);
+  }
+
+  void assignValueToAddress(const CallLowering::ArgInfo &Arg, Register Addr,
+                            uint64_t Size, MachinePointerInfo &MPO,
+                            CCValAssign &VA) override {
+    unsigned MaxSize = Size * 8;
+    // For varargs, we always want to extend them to 8 bytes, in which case
+    // we disable setting a max.
+    if (!Arg.IsFixed)
+      MaxSize = 0;
+
+    Register ValVReg = VA.getLocInfo() != CCValAssign::LocInfo::FPExt
+                           ? extendRegister(Arg.Regs[0], VA, MaxSize)
+                           : Arg.Regs[0];
+
+    // If we extended we might need to adjust the MMO's Size.
+    const LLT RegTy = MRI.getType(ValVReg);
+    if (RegTy.getSizeInBytes() > Size)
+      Size = RegTy.getSizeInBytes();
+
+    assignValueToAddress(ValVReg, Addr, Size, MPO, VA);
   }
 
   bool assignArg(unsigned ValNo, MVT ValVT, MVT LocVT,
