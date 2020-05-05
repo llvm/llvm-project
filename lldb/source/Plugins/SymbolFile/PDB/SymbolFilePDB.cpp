53f0e98a301254b6faef6f5f48356f4deb1e47ce
Fred Riss <friss@apple.com>
2020-05-05 08:31:20 -0700

    Revert "[lldb/DWARF] Fix a leak in line table construction"
    This reverts commit f4db8e04fce5f1c22078888e7e6678d25af237a1.
    
    This was cherry-picked incorrectly

--- lldb/source/Plugins/SymbolFile/PDB/SymbolFilePDB.cpp
+++ lldb/source/Plugins/SymbolFile/PDB/SymbolFilePDB.cpp
@@ -1817,7 +1817,7 @@ bool SymbolFilePDB::ParseCompileUnitLineTable(CompileUnit &comp_unit,
             prev_source_idx, false, false, false, false, true);
 
         line_table->InsertSequence(sequence.release());
-        sequence = line_table->CreateLineSequenceContainer();
+        sequence.reset(line_table->CreateLineSequenceContainer());
       }
 
       if (ShouldAddLine(match_line, lno, length)) {
@@ -1854,7 +1854,7 @@ bool SymbolFilePDB::ParseCompileUnitLineTable(CompileUnit &comp_unit,
           prev_source_idx, false, false, false, false, true);
     }
 
-    line_table->InsertSequence(sequence.get());
+    line_table->InsertSequence(sequence.release());
   }
 
   if (line_table->GetSize()) {
