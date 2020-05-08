eea5ed38394ba09e21b7c9eb3ce2c68a96b382a1
Eric <eric.schweitz@gmail.com>
2020-05-08 12:52:59 -0700

    fix bug with external names Fix IsDescriptor for dummy with explicit non constant shapes add option to bbc cleanup on bbc need to flip from column-major to row-major when converting to LLVM. use TypeSwitch; fix failing test

--- flang/lib/Evaluate/type.cpp
+++ flang/lib/Evaluate/type.cpp
@@ -37,6 +37,14 @@ static bool IsDescriptor(const ObjectEntityDetails &details) {
   if (IsDescriptor(details.type())) {
     return true;
   }
+  // TODO: Automatic (adjustable) arrays - are they descriptors?
+  if (details.isDummy()) {
+    return details.IsAssumedShape() || details.IsDeferredShape() ||
+        details.IsAssumedRank();
+  }
+  return !details.shape().empty() && !details.shape().IsConstantShape();
+#if 0
+  // FIXME: use this?
   for (const ShapeSpec &shapeSpec : details.shape()) {
     const auto &lb{shapeSpec.lbound().GetExplicit()};
     const auto &ub{shapeSpec.ubound().GetExplicit()};
@@ -45,6 +53,7 @@ static bool IsDescriptor(const ObjectEntityDetails &details) {
     }
   }
   return false;
+#endif
 }
 
 static bool IsDescriptor(const ProcEntityDetails &details) {
