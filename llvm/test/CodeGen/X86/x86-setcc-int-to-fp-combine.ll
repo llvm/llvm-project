llvm/llvm-project

commit 2abe864426b9b0cd78ac7ae307c09ba788d7a9f6

[X86] Allow combineVectorCompareAndMaskUnaryOp to handle 'all-bits' general case
For the sint_to_fp(and(X,C)) -> and(X,sint_to_fp(C)) fold, allow combineVectorCompareAndMaskUnaryOp to match any X that ComputeNumSignBits says is all-bits, not just SETCC.

Noticed while investigating mask promotion issues in PR45808

llvm/lib/Target/X86/X86ISelLowering.cpp
@@ -45564,10 +45564,11 @@ static SDValue combineBrCond(SDNode *N, SelectionDAG &DAG,
   return SDValue();
 }
 
+// TODO: Could we move this to DAGCombine?
 static SDValue combineVectorCompareAndMaskUnaryOp(SDNode *N,
                                                   SelectionDAG &DAG) {
-  // Take advantage of vector comparisons producing 0 or -1 in each lane to
-  // optimize away operation when it's from a constant.
+  // Take advantage of vector comparisons (etc.) producing 0 or -1 in each lane
+  // to optimize away operation when it's from a constant.
   //
   // The general transformation is:
   //    UNARYOP(AND(VECTOR_CMP(x,y), constant)) -->
@@ -45579,10 +45580,10 @@ static SDValue combineVectorCompareAndMaskUnaryOp(SDNode *N,
   // aren't the same.
   EVT VT = N->getValueType(0);
   bool IsStrict = N->isStrictFPOpcode();
+  unsigned NumEltBits = VT.getScalarSizeInBits();
   SDValue Op0 = N->getOperand(IsStrict ? 1 : 0);
-  if (!VT.isVector() || Op0->getOpcode() != ISD::AND ||
-      Op0->getOperand(0)->getOpcode() != ISD::SETCC ||
-      VT.getSizeInBits() != Op0.getValueSizeInBits())
+  if (!VT.isVector() || Op0.getOpcode() != ISD::AND ||
+      DAG.ComputeNumSignBits(Op0.getOperand(0)) != NumEltBits)
     return SDValue();
 
   // Now check that the other operand of the AND is a constant. We could

llvm/test/CodeGen/X86/x86-setcc-int-to-fp-combine.ll
@@ -97,17 +97,16 @@ define void @foo4(<4 x float>* noalias %result) nounwind {
   ret void
 }
 
-; TODO: Test when we're masking against a sign extended setcc.
+; Test when we're masking against a sign extended setcc.
 define <4 x float> @foo5(<4 x i32> %a0, <4 x i32> %a1) {
 ; CHECK-LABEL: LCPI5_0:
-; CHECK-NEXT: .long 1                       ## 0x1
+; CHECK-NEXT: .long 1065353216              ## 0x3f800000
 ; CHECK-NEXT: .long 0                       ## 0x0
-; CHECK-NEXT: .long 1                       ## 0x1
+; CHECK-NEXT: .long 1065353216              ## 0x3f800000
 ; CHECK-NEXT: .long 0                       ## 0x0
 ; CHECK:       ## %bb.0:
 ; CHECK-NEXT:    pcmpgtd %xmm1, %xmm0
 ; CHECK-NEXT:    pand {{.*}}(%rip), %xmm0
-; CHECK-NEXT:    cvtdq2ps %xmm0, %xmm0
 ; CHECK-NEXT:    retq
   %1 = icmp sgt <4 x i32> %a0, %a1
   %2 = sext <4 x i1> %1 to <4 x i32>
@@ -116,12 +115,12 @@ define <4 x float> @foo5(<4 x i32> %a0, <4 x i32> %a1) {
   ret <4 x float> %4
 }
 
-; TODO: Test when we're masking against mask arithmetic, not the setcc's directly.
+; Test when we're masking against mask arithmetic, not the setcc's directly.
 define <4 x float> @foo6(<4 x i32> %a0, <4 x i32> %a1) {
 ; CHECK-LABEL: LCPI6_0:
-; CHECK-NEXT: .long 1                       ## 0x1
+; CHECK-NEXT: .long 1065353216              ## 0x3f800000
 ; CHECK-NEXT: .long 0                       ## 0x0
-; CHECK-NEXT: .long 1                       ## 0x1
+; CHECK-NEXT: .long 1065353216              ## 0x3f800000
 ; CHECK-NEXT: .long 0                       ## 0x0
 ; CHECK:       ## %bb.0:
 ; CHECK-NEXT:    movdqa %xmm0, %xmm2
@@ -130,7 +129,6 @@ define <4 x float> @foo6(<4 x i32> %a0, <4 x i32> %a1) {
 ; CHECK-NEXT:    pcmpgtd %xmm1, %xmm0
 ; CHECK-NEXT:    pand %xmm2, %xmm0
 ; CHECK-NEXT:    pand {{.*}}(%rip), %xmm0
-; CHECK-NEXT:    cvtdq2ps %xmm0, %xmm0
 ; CHECK-NEXT:    retq
   %1 = icmp sgt <4 x i32> %a0, %a1
   %2 = icmp sgt <4 x i32> %a0, zeroinitializer

commit f64fb0c8fbb2a17d1768b3f68708d8ea2e016172

[X86] Fix combineVectorCompareAndMaskUnaryOp regression after 2abe864426b9b0cd78ac7ae307c09ba788d7a9f6

llvm/lib/Target/X86/X86ISelLowering.cpp
@@ -45586,7 +45586,8 @@ static SDValue combineVectorCompareAndMaskUnaryOp(SDNode *N,
   unsigned NumEltBits = VT.getScalarSizeInBits();
   SDValue Op0 = N->getOperand(IsStrict ? 1 : 0);
   if (!VT.isVector() || Op0.getOpcode() != ISD::AND ||
-      DAG.ComputeNumSignBits(Op0.getOperand(0)) != NumEltBits)
+      DAG.ComputeNumSignBits(Op0.getOperand(0)) != NumEltBits ||
+      VT.getSizeInBits() != Op0.getValueSizeInBits())
     return SDValue();
 
   // Now check that the other operand of the AND is a constant. We could

llvm/test/CodeGen/X86/x86-setcc-int-to-fp-combine.ll
@@ -138,3 +138,33 @@ define <4 x float> @foo6(<4 x i32> %a0, <4 x i32> %a1) {
   %6 = uitofp <4 x i32> %5 to <4 x float>
   ret <4 x float> %6
 }
+
+define <4 x float> @foo7(<4 x i64> %a) {
+; CHECK-LABEL: LCPI7_0:
+; CHECK-NEXT:  .byte   0                       ## 0x0
+; CHECK-NEXT:  .byte   255                     ## 0xff
+; CHECK-NEXT:  .byte   0                       ## 0x0
+; CHECK-NEXT:  .byte   0                       ## 0x0
+; CHECK-NEXT:  .byte   0                       ## 0x0
+; CHECK-NEXT:  .byte   255                     ## 0xff
+; CHECK-NEXT:  .byte   0                       ## 0x0
+; CHECK-NEXT:  .byte   0                       ## 0x0
+; CHECK-NEXT:  .byte   0                       ## 0x0
+; CHECK-NEXT:  .byte   255                     ## 0xff
+; CHECK-NEXT:  .byte   0                       ## 0x0
+; CHECK-NEXT:  .byte   0                       ## 0x0
+; CHECK-NEXT:  .byte   0                       ## 0x0
+; CHECK-NEXT:  .byte   255                     ## 0xff
+; CHECK-NEXT:  .byte   0                       ## 0x0
+; CHECK-NEXT:  .byte   0                       ## 0x0
+; CHECK-LABEL: foo7:
+; CHECK:       ## %bb.0:
+; CHECK-NEXT:    shufps {{.*#+}} xmm0 = xmm0[0,2],xmm1[0,2]
+; CHECK-NEXT:    andps {{.*}}(%rip), %xmm0
+; CHECK-NEXT:    cvtdq2ps %xmm0, %xmm0
+; CHECK-NEXT:    retq
+  %b = and <4 x i64> %a, <i64 4278255360, i64 4278255360, i64 4278255360, i64 4278255360>
+  %c = and <4 x i64> %b, <i64 65535, i64 65535, i64 65535, i64 65535>
+  %d = uitofp <4 x i64> %c to <4 x float>
+  ret <4 x float> %d
+}