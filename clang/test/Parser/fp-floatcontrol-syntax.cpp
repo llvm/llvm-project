0895dd6841962716fd79036df362f97fa58aee8f
Mark Searles <mark.searles@amd.com>
2020-05-14 15:35:16 -0700

    Revert "When pragma FENV_ACCESS is ignored do not modify Sema.CurFPFeatures"
    This reverts commit e5578013b199e142864cbf374b4a143e5d0c184a.
    
    Change-Id: I3b6eb1a9f4838c1e644c06d663d5cf5b164cb3c0

--- clang/test/Parser/fp-floatcontrol-syntax.cpp
+++ clang/test/Parser/fp-floatcontrol-syntax.cpp
@@ -30,15 +30,14 @@ double b = 1.0;
 //      document the warning
 #ifdef FAST
 // expected-warning@+1{{pragma STDC FENV_ACCESS ON is not supported, ignoring pragma}}
-#pragma STDC FENV_ACCESS ON
+#pragma STDC FENV_ACCESS ON // expected-error{{'#pragma STDC FENV_ACCESS ON' is illegal when precise is disabled}}
 #else
 #pragma STDC FENV_ACCESS ON // expected-warning{{pragma STDC FENV_ACCESS ON is not supported, ignoring pragma}}
 #endif
 #ifdef STRICT
 #pragma float_control(precise, off) // expected-error {{'#pragma float_control(precise, off)' is illegal when except is enabled}}
 #else
-// Currently FENV_ACCESS cannot be enabled by pragma, skip error check
-#pragma float_control(precise, off) // not-expected-error {{'#pragma float_control(precise, off)' is illegal when fenv_access is enabled}}
+#pragma float_control(precise, off) // expected-error {{'#pragma float_control(precise, off)' is illegal when fenv_access is enabled}}
 #endif
 
 #pragma float_control(precise, on)
