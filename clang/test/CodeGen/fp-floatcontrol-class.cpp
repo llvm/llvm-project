c84bfe5f4dfbc27cca2537c297e871101ec1334d
Mark Searles <mark.searles@amd.com>
2020-05-14 15:35:48 -0700

    Revert "Fix LABEL match for test case for D72841 #pragma float_control"
    This reverts commit 7cbb495ab452877496df270678f9b0a8d57ab7e7.
    
    Change-Id: I22d9f0ef3adf6c4be6ea8bffd9c1bbf5dbf1e48c

--- clang/test/CodeGen/fp-floatcontrol-class.cpp
+++ clang/test/CodeGen/fp-floatcontrol-class.cpp
@@ -1,5 +1,6 @@
 // RUN: %clang_cc1 -ffp-contract=on -triple %itanium_abi_triple -emit-llvm -o - %s | FileCheck %s
 // RUN: %clang_cc1 -ffp-contract=on -triple x86_64-linux-gnu -emit-llvm -o - %s | FileCheck %s
+// RUN: %clang_cc1 -ffp-contract=on -triple x86_64-unknown-unknown -emit-llvm -o - %s | FileCheck %s
 // Verify that float_control does not pertain to initializer expressions
 
 float y();
@@ -7,14 +8,14 @@ float z();
 #pragma float_control(except, on)
 class ON {
   float w = 2 + y() * z();
-  // CHECK-LABEL: define {{.*}} @_ZN2ONC2Ev{{.*}}
+  // CHECK-LABEL: define {{.*}} void @_ZN2ONC2Ev{{.*}}
   //CHECK: call contract float {{.*}}llvm.fmuladd
 };
 ON on;
 #pragma float_control(except, off)
 class OFF {
   float w = 2 + y() * z();
-  // CHECK-LABEL: define {{.*}} @_ZN3OFFC2Ev{{.*}}
+  // CHECK-LABEL: define {{.*}} void @_ZN3OFFC2Ev{{.*}}
   //CHECK: call contract float {{.*}}llvm.fmuladd
 };
 OFF off;
