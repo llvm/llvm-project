71c14cd5aee72a7502a45068ec73487d1f9e019c
Jim Lin <tclin914@gmail.com>
2020-05-18 11:10:46 +0800

    [RISCV] Fix passing two floating-point values in complex separately by two GPRs on RV64
    Summary:
    This patch fixed the error of counting the remaining FPRs. Complex floating-point
    values should be passed by two FPRs for the hard-float ABI. If no two FPRs are
    available, it should be passed via a 64-bit GPR (fp+fp). `ArgFPRsLeft` is only
    decreased one while the type is complex floating-point. It causes two floating-point
    values in the complex are passed separately by two GPRs.
    
    Reviewers: asb, luismarques, lenary
    
    Reviewed By: asb
    
    Subscribers: rbar, johnrusso, simoncook, sabuasal, niosHD, kito-cheng, shiva0217, jrtc27, zzheng, edward-jones, rogfer01, MartinMosbeck, brucehoult, the_o, rkruppe, PkmX, jocewei, psnobl, benna, s.egerton, pzheng, sameer.abuasal, apazos, evandro, cfe-commits
    
    Tags: #clang
    
    Differential Revision: https://reviews.llvm.org/D79770
    
    (cherry picked from commit 7ee479a760e0a4402b4eb7fb6168768a44f66945)

--- clang/lib/CodeGen/TargetInfo.cpp
+++ clang/lib/CodeGen/TargetInfo.cpp
@@ -9613,7 +9613,8 @@ ABIArgInfo RISCVABIInfo::classifyArgumentType(QualType Ty, bool IsFixed,
   uint64_t Size = getContext().getTypeSize(Ty);
 
   // Pass floating point values via FPRs if possible.
-  if (IsFixed && Ty->isFloatingType() && FLen >= Size && ArgFPRsLeft) {
+  if (IsFixed && Ty->isFloatingType() && !Ty->isComplexType() &&
+      FLen >= Size && ArgFPRsLeft) {
     ArgFPRsLeft--;
     return ABIArgInfo::getDirect();
   }
