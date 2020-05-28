36fcbac6b1d4e556e8eccc63c62997c18d8799b1
Eric <eschweitz@nvidia.com>
2020-05-28 10:35:23 -0700

    revert PR #98
    compilation failures in InternalNamesTest.cpp
    
    add back Basic.cpp unit test to finish the revert

---
+++ flang/unittests/Optimizer/Basic.cpp
@@ -0,0 +1,18 @@
+#include "flang/Optimizer/Support/InternalNames.h"
+#include "gtest/gtest.h"
+#include <iostream>
+
+using namespace fir;
+using namespace llvm;
+
+TEST(genericName, MyTest) {
+        NameUniquer obj;
+        std::string val = obj.doCommonBlock("hello");
+        std::cout  << val;
+}
+
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+
