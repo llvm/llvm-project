ba593c3c2481fc186998be5b244bd077ccbf859d
Eric Schweitz <eschweitz@nvidia.com>
2020-06-02 09:49:53 -0700

    rebase fallout

--- flang/lib/Evaluate/type.cpp
+++ flang/lib/Evaluate/type.cpp
@@ -38,13 +38,6 @@ static bool IsDescriptor(const ObjectEntityDetails &details) {
     return true;
   }
   // TODO: Automatic (adjustable) arrays - are they descriptors?
-  if (details.isDummy()) {
-    return details.IsAssumedShape() || details.IsDeferredShape() ||
-        details.IsAssumedRank();
-  }
-  return !details.shape().empty() && !details.shape().IsConstantShape();
-#if 0
-  // FIXME: use this?
   for (const ShapeSpec &shapeSpec : details.shape()) {
     const auto &lb{shapeSpec.lbound().GetExplicit()};
     const auto &ub{shapeSpec.ubound().GetExplicit()};
@@ -53,7 +46,6 @@ static bool IsDescriptor(const ObjectEntityDetails &details) {
     }
   }
   return false;
-#endif
 }
 
 static bool IsDescriptor(const ProcEntityDetails &details) {
--- flang/lib/Optimizer/Transforms/RewriteLoop.cpp
+++ flang/lib/Optimizer/Transforms/RewriteLoop.cpp
@@ -17,30 +17,33 @@
 
 /// disable FIR to scf dialect conversion
 static llvm::cl::opt<bool>
-    disableScfConversion("disable-scf-conversion",
-                         llvm::cl::desc("disable FIR to SCF pass"),
+    disableCfgConversion("disable-cfg-conversion",
+                         llvm::cl::desc("disable FIR to CFG pass"),
                          llvm::cl::init(false));
 
 using namespace fir;
 
 namespace {
 
-// Conversion to CFG
-class ScfLoopConv : public mlir::OpRewritePattern<fir::LoopOp> {
+// Conversion of fir control ops to more primitive control-flow.
+//
+// FIR loops that cannot be converted to the affine dialect will remain as
+// `fir.do_loop` operations.  These can be converted to control-flow operations.
+
+/// Convert `fir.do_loop` to CFG
+class CfgLoopConv : public mlir::OpRewritePattern<fir::LoopOp> {
 public:
   using OpRewritePattern::OpRewritePattern;
 
-  // FIXME: This should NOT be using scf.for. Instead, fir.do_loop should be
-  // lowered to a semantically correct CFG.
   mlir::LogicalResult
   matchAndRewrite(LoopOp loop, mlir::PatternRewriter &rewriter) const override {
-    Location loc = loop.getLoc();
+    auto loc = loop.getLoc();
 
     // Create the start and end blocks that will wrap the LoopOp with an
     // initalizer and an end point
-    mlir::Block *initBlock = rewriter.getInsertionBlock();
+    auto *initBlock = rewriter.getInsertionBlock();
     auto initPos = rewriter.getInsertionPoint();
-    mlir::Block *endBlock = rewriter.splitBlock(initBlock, initPos);
+    auto *endBlock = rewriter.splitBlock(initBlock, initPos);
 
     // Split the first LoopOp block in two parts. The part before will be the
     // conditional block since it already has the induction variable and
@@ -66,9 +69,9 @@ public:
     auto distance = rewriter.create<mlir::AddIOp>(loc, diff, step);
     auto iters = rewriter.create<mlir::SignedDivIOp>(loc, distance, step);
 
-    SmallVector<mlir::Value, 8> loopOperands;
+    llvm::SmallVector<mlir::Value, 8> loopOperands;
     loopOperands.push_back(low);
-    mlir::ValueRange operands = loop.getIterOperands();
+    auto operands = loop.getIterOperands();
     loopOperands.append(operands.begin(), operands.end());
     loopOperands.push_back(iters);
 
@@ -76,14 +79,13 @@ public:
     // onetrip flag determines whether loop should be executed once, before
     // conditionals are checked
     static const bool onetrip = false;
-    if (onetrip) {
-      rewriter.create<BranchOp>(loc, firstBlock, ArrayRef<Value>());
-    } else {
-      rewriter.create<BranchOp>(loc, conditionalBlock, loopOperands);
-    }
+    if (onetrip)
+      rewriter.create<mlir::BranchOp>(loc, firstBlock, ArrayRef<mlir::Value>());
+    else
+      rewriter.create<mlir::BranchOp>(loc, conditionalBlock, loopOperands);
 
     // Last loop block
-    mlir::Operation *terminator = lastBlock->getTerminator();
+    auto *terminator = lastBlock->getTerminator();
     rewriter.setInsertionPointToEnd(lastBlock);
     auto iv = conditionalBlock->getArgument(0);
     mlir::Value steppedIndex = rewriter.create<mlir::AddIOp>(loc, iv, step);
@@ -94,7 +96,7 @@ public:
     mlir::Value itersMinusOne =
         rewriter.create<mlir::SubIOp>(loc, itersLeft, one);
 
-    SmallVector<mlir::Value, 8> loopCarried;
+    llvm::SmallVector<mlir::Value, 8> loopCarried;
     loopCarried.push_back(steppedIndex);
     loopCarried.append(terminator->operand_begin(), terminator->operand_end());
     loopCarried.push_back(itersMinusOne);
@@ -107,6 +109,9 @@ public:
     auto comparison =
         rewriter.create<mlir::CmpIOp>(loc, CmpIPredicate::sgt, itersLeft, zero);
 
+    rewriter.create<mlir::CondBranchOp>(loc, comparison, firstBlock,
+                                        llvm::ArrayRef<mlir::Value>(), endBlock,
+                                        llvm::ArrayRef<mlir::Value>());
 
     // The result of the loop operation is the values of the condition block
     // arguments except the induction variable on the last iteration.
@@ -117,7 +122,7 @@ public:
 };
 
 /// Convert `fir.if` to control-flow
-class ScfIfConv : public mlir::OpRewritePattern<fir::WhereOp> {
+class CfgIfConv : public mlir::OpRewritePattern<fir::WhereOp> {
 public:
   using OpRewritePattern::OpRewritePattern;
 
@@ -138,7 +143,7 @@ public:
     } else {
       continueBlock =
           rewriter.createBlock(remainingOpsBlock, where.getResultTypes());
-      rewriter.create<BranchOp>(loc, remainingOpsBlock);
+      rewriter.create<mlir::BranchOp>(loc, remainingOpsBlock);
     }
 
     // Move blocks from the "then" region to the region containing 'fir.if',
@@ -148,7 +153,8 @@ public:
     auto *whereTerminator = whereRegion.back().getTerminator();
     auto whereTerminatorOperands = whereTerminator->getOperands();
     rewriter.setInsertionPointToEnd(&whereRegion.back());
-    rewriter.create<BranchOp>(loc, continueBlock, whereTerminatorOperands);
+    rewriter.create<mlir::BranchOp>(loc, continueBlock,
+                                    whereTerminatorOperands);
     rewriter.eraseOp(whereTerminator);
     rewriter.inlineRegionBefore(whereRegion, continueBlock);
 
@@ -162,7 +168,8 @@ public:
       auto *otherwiseTerm = otherwiseRegion.back().getTerminator();
       auto otherwiseTermOperands = otherwiseTerm->getOperands();
       rewriter.setInsertionPointToEnd(&otherwiseRegion.back());
-      rewriter.create<BranchOp>(loc, continueBlock, otherwiseTermOperands);
+      rewriter.create<mlir::BranchOp>(loc, continueBlock,
+                                      otherwiseTermOperands);
       rewriter.eraseOp(otherwiseTerm);
       rewriter.inlineRegionBefore(otherwiseRegion, continueBlock);
     }
@@ -177,14 +184,14 @@ public:
 };
 
 /// Convert `fir.iter_while` to control-flow.
-class ScfIterWhileConv : public mlir::OpRewritePattern<fir::IterWhileOp> {
+class CfgIterWhileConv : public mlir::OpRewritePattern<fir::IterWhileOp> {
 public:
   using OpRewritePattern::OpRewritePattern;
 
   mlir::LogicalResult
   matchAndRewrite(fir::IterWhileOp whileOp,
                   mlir::PatternRewriter &rewriter) const override {
-    mlir::Location loc = whileOp.getLoc();
+    auto loc = whileOp.getLoc();
 
     // Start by splitting the block containing the 'fir.do_loop' into two parts.
     // The part before will get the init code, the part after will be the end
@@ -218,7 +225,7 @@ public:
     llvm::SmallVector<mlir::Value, 8> loopCarried;
     loopCarried.push_back(stepped);
     loopCarried.append(terminator->operand_begin(), terminator->operand_end());
-    rewriter.create<BranchOp>(loc, conditionBlock, loopCarried);
+    rewriter.create<mlir::BranchOp>(loc, conditionBlock, loopCarried);
     rewriter.eraseOp(terminator);
 
     // Compute loop bounds before branching to the condition.
@@ -233,17 +240,17 @@ public:
     destOperands.push_back(lowerBound);
     auto iterOperands = whileOp.getIterOperands();
     destOperands.append(iterOperands.begin(), iterOperands.end());
-    rewriter.create<BranchOp>(loc, conditionBlock, destOperands);
+    rewriter.create<mlir::BranchOp>(loc, conditionBlock, destOperands);
 
     // With the body block done, we can fill in the condition block.
     rewriter.setInsertionPointToEnd(conditionBlock);
     auto comp1 =
-        rewriter.create<CmpIOp>(loc, CmpIPredicate::slt, iv, upperBound);
+        rewriter.create<mlir::CmpIOp>(loc, CmpIPredicate::slt, iv, upperBound);
     // Remember to AND in the early-exit bool.
-    auto comparison = rewriter.create<AndOp>(loc, comp1, iterateVar);
-    rewriter.create<CondBranchOp>(loc, comparison, firstBodyBlock,
-                                  llvm::ArrayRef<mlir::Value>(), endBlock,
-                                  llvm::ArrayRef<mlir::Value>());
+    auto comparison = rewriter.create<mlir::AndOp>(loc, comp1, iterateVar);
+    rewriter.create<mlir::CondBranchOp>(loc, comparison, firstBodyBlock,
+                                        llvm::ArrayRef<mlir::Value>(), endBlock,
+                                        llvm::ArrayRef<mlir::Value>());
     // The result of the loop operation is the values of the condition block
     // arguments except the induction variable on the last iteration.
     rewriter.replaceOp(whileOp, conditionBlock->getArguments().drop_front());
@@ -251,46 +258,36 @@ public:
   }
 };
 
-/// Convert FIR structured control flow ops to SCF ops.
-class ScfDialectConversion
-    : public mlir::PassWrapper<ScfDialectConversion, mlir::FunctionPass> {
+/// Convert FIR structured control flow ops to CFG ops.
+class CfgConversion
+    : public mlir::PassWrapper<CfgConversion, mlir::FunctionPass> {
 public:
   void runOnFunction() override {
-    if (disableScfConversion)
+    if (disableCfgConversion)
       return;
 
     auto *context = &getContext();
-    mlir::OwningRewritePatternList patterns1;
-    patterns1.insert<ScfIterWhileConv>(context);
-
-    mlir::OwningRewritePatternList patterns2;
-    patterns2.insert<ScfLoopConv, ScfIfConv, ScfResultConv>(context);
+    mlir::OwningRewritePatternList patterns;
+    patterns.insert<CfgLoopConv, CfgIfConv, CfgIterWhileConv>(context);
     mlir::ConversionTarget target = *context;
     target.addLegalDialect<mlir::AffineDialect, FIROpsDialect,
-                           mlir::scf::SCFDialect, mlir::StandardOpsDialect>();
+                           mlir::StandardOpsDialect>();
 
     // apply the patterns
-    target.addIllegalOp<IterWhileOp>();
+    target.addIllegalOp<ResultOp, LoopOp, WhereOp, IterWhileOp>();
     if (mlir::failed(mlir::applyPartialConversion(getFunction(), target,
-                                                  std::move(patterns1)))) {
+                                                  std::move(patterns)))) {
       mlir::emitError(mlir::UnknownLoc::get(context),
                       "error in converting to CFG\n");
-      signalPassFailure();
-    }
-    target.addIllegalOp<ResultOp, LoopOp, WhereOp>();
-    if (mlir::failed(mlir::applyPartialConversion(getFunction(), target,
-                                                  std::move(patterns2)))) {
-      mlir::emitError(mlir::UnknownLoc::get(context),
-                      "error in converting to scf dialect\n");
       signalPassFailure();
     }
   }
 };
 } // namespace
 
-/// Convert FIR's structured control flow ops to SCF ops.  This
+/// Convert FIR's structured control flow ops to CFG ops.  This
 /// conversion enables the `createLowerToCFGPass` to transform these to CFG
 /// form.
-std::unique_ptr<mlir::Pass> fir::createLowerToScfPass() {
-  return std::make_unique<ScfDialectConversion>();
+std::unique_ptr<mlir::Pass> fir::createFirToCfgPass() {
+  return std::make_unique<CfgConversion>();
 }
