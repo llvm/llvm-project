53bfb4f181ef0f4dad5b9b23b1f5c445a055922e
Eric Schweitz <eschweitz@nvidia.com>
2020-06-04 12:23:51 -0700

    [NFC] merge two tests
    revert changes to Runtime/format.cpp test.
    
    It doesn't compile on MacOS.
    
    revert rename change that snuck in

--- flang/test/Lower/io-stmt02.f90
+++ flang/test/Lower/io-stmt02.f90
@@ -21,3 +21,62 @@
 30 exx = 'Eor'; goto 90
 90 print*, exx, c, m, s
 end
+
+! CHECK-LABEL: func @_QPcontrol0
+subroutine control0(n) ! no I/O condition specifier control flow
+dimension c(n), d(n,n), e(n,n), f(n)
+! CHECK-NOT: fir.if
+! CHECK: BeginExternalFormattedInput
+! CHECK-NOT: fir.if
+! CHECK: SetAdvance
+! CHECK-NOT: fir.if
+! CHECK: InputReal32
+! CHECK-NOT: fir.if
+! CHECK: InputReal32
+! CHECK-NOT: fir.if
+! CHECK: fir.do_loop
+! CHECK-NOT: fir.if
+! CHECK: InputReal32
+! CHECK-NOT: fir.if
+! CHECK: fir.do_loop
+! CHECK-NOT: fir.if
+! CHECK: InputReal32
+! CHECK-NOT: fir.if
+! CHECK: InputReal32
+! CHECK-NOT: fir.if
+! CHECK: InputReal32
+! CHECK-NOT: fir.if
+! CHECK: InputReal32
+! CHECK-NOT: fir.if
+! CHECK: EndIoStatement
+! CHECK-NOT: fir.if
+read(*,'(F7.2)', advance='no') a, b, (c(j), (d(k,j), e(k,j), k=1,n), f(j), j=1,n), g
+end
+
+! CHECK-LABEL: func @_QPcontrol1
+subroutine control1(n) ! I/O condition specifier control flow
+! CHECK: BeginExternalFormattedInput
+! CHECK: EnableHandlers
+! CHECK: SetAdvance
+! CHECK: fir.if
+! CHECK: InputReal32
+! CHECK: fir.if
+! CHECK: InputReal32
+! CHECK: fir.if
+! CHECK: fir.iterate_while
+! CHECK: fir.if
+! CHECK: InputReal32
+! CHECK: fir.if
+! CHECK: fir.iterate_while
+! CHECK: fir.if
+! CHECK: InputReal32
+! CHECK: fir.if
+! CHECK: InputReal32
+! CHECK: fir.if
+! CHECK: InputReal32
+! CHECK: fir.if
+! CHECK: InputReal32
+! CHECK: EndIoStatement
+dimension c(n), d(n,n), e(n,n), f(n)
+read(*,'(F7.2)', iostat=mm, advance='no') a, b, (c(j), (d(k,j), e(k,j), k=1,n), f(j), j=1,n), g
+end
