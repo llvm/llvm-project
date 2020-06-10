237c7f7e9631c3f633faa67c17538aec8388a14e
Eric <eschweitz@nvidia.com>
2020-06-10 13:37:37 -0700

    [rebase] fallout from MLIR changing the output of i1 constants

--- flang/test/Lower/stop.f90
+++ flang/test/Lower/stop.f90
@@ -1,11 +1,10 @@
-! RUN: bbc -emit-fir %s -o - | FileCheck %s
+! RUN: bbc %s -o - | FileCheck %s
 
 ! CHECK-LABEL stop_test
 subroutine stop_test(b)
  ! CHECK-DAG: %[[c0:.*]] = constant 0 : i32
- ! CHECK-DAG: %[[false:.*]] = constant 0 : i1
- ! CHECK-DAG: %[[false_0:.*]] = constant 0 : i1
- ! CHECK: call @_Fortran{{.*}}StopStatement(%[[c0]], %[[false]], %[[false_0]])
+ ! CHECK-DAG: %[[false:.*]] = constant false
+ ! CHECK: call @_Fortran{{.*}}StopStatement(%[[c0]], %[[false]], %[[false]])
  stop
 end subroutine
 ! CHECK: func @_Fortran{{.*}}StopStatement(i32, i1, i1) -> none
@@ -14,17 +13,16 @@ end subroutine
 subroutine stop_code()
   stop 42
  ! CHECK-DAG: %[[c42:.*]] = constant 42 : i32
- ! CHECK-DAG: %[[false:.*]] = constant 0 : i1
- ! CHECK-DAG: %[[false_0:.*]] = constant 0 : i1
- ! CHECK: call @_Fortran{{.*}}StopStatement(%[[c42]], %[[false]], %[[false_0]])
+ ! CHECK-DAG: %[[false:.*]] = constant false
+ ! CHECK: call @_Fortran{{.*}}StopStatement(%[[c42]], %[[false]], %[[false]])
 end subroutine
 
 ! CHECK-LABEL stop_error
 subroutine stop_error()
   error stop
  ! CHECK-DAG: %[[c0:.*]] = constant 0 : i32
- ! CHECK-DAG: %[[true:.*]] = constant 1 : i1
- ! CHECK-DAG: %[[false:.*]] = constant 0 : i1
+ ! CHECK-DAG: %[[true:.*]] = constant true
+ ! CHECK-DAG: %[[false:.*]] = constant false
  ! CHECK: call @_Fortran{{.*}}StopStatement(%[[c0]], %[[true]], %[[false]])
 end subroutine
 
@@ -33,7 +31,7 @@ subroutine stop_quiet(b)
   logical :: b
   stop, quiet = b
  ! CHECK-DAG: %[[c0:.*]] = constant 0 : i32
- ! CHECK-DAG: %[[false:.*]] = constant 0 : i1
+ ! CHECK-DAG: %[[false:.*]] = constant false
  ! CHECK-DAG: %[[b:.*]] = fir.load %arg0
  ! CHECK-DAG: %[[bi1:.*]] = fir.convert %[[b]] : (!fir.logical<4>) -> i1
  ! CHECK: call @_Fortran{{.*}}StopStatement(%[[c0]], %[[false]], %[[bi1]])
@@ -44,7 +42,7 @@ subroutine stop_error_code_quiet(b)
   logical :: b
   error stop 66, quiet = b
  ! CHECK-DAG: %[[c66:.*]] = constant 66 : i32
- ! CHECK-DAG: %[[true:.*]] = constant 1 : i1
+ ! CHECK-DAG: %[[true:.*]] = constant true
  ! CHECK-DAG: %[[b:.*]] = fir.load %arg0
  ! CHECK-DAG: %[[bi1:.*]] = fir.convert %[[b]] : (!fir.logical<4>) -> i1
  ! CHECK: call @_Fortran{{.*}}StopStatement(%[[c66]], %[[true]], %[[bi1]])
