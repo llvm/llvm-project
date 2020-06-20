212faaed72d3c0b4dbd12ffe624681347ebf4288
Adrian Prantl <aprantl@apple.com>
2020-06-19 17:14:34 -0700

    Remove the caching from DWARFImporterDelegate::lookupValue()
    and instead forward all requests from the expression context to the
    individual module contexts. This allows for finding types that were
    not previously loaded into a module context, such as types are first
    referenced by a user expression.
    
    <rdar://problem/64169273>

---
+++ lldb/test/API/lang/swift/dwarfimporter/from_dylib/main.swift
@@ -0,0 +1,3 @@
+import Foo
+
+foo() // break here
--- lldb/test/API/lang/swift/nsarray_code_running_formatter/TestSwiftNSArrayCodeRunningFormatter.py
+++ lldb/test/API/lang/swift/nsarray_code_running_formatter/TestSwiftNSArrayCodeRunningFormatter.py
@@ -13,7 +13,4 @@ import lldbsuite.test.lldbinline as lldbinline
 from lldbsuite.test.decorators import *
 
 lldbinline.MakeInlineTest(__file__, globals(),
-                          decorators=[swiftTest,skipUnlessDarwin,
-expectedFailureAll(bugnumber="rdar://60396797",
-                   setting=('symbols.use-swift-clangimporter', 'false'))
-])
+                          decorators=[swiftTest,skipUnlessDarwin])
