f145f20faf8ef2ab39205ee61fffddf88dcd4326
Adrian Prantl <aprantl@apple.com>
2020-06-26 15:23:14 -0700

    Strip out -Werror arguments when constructing ClangImporter.
    They only cause trouble in the debugger and never help.
    
    <rdar://problem/64810678>

---
+++ lldb/test/API/lang/swift/clangimporter/Werror/TestSwiftStripWerror.py
@@ -0,0 +1,40 @@
+import lldb
+from lldbsuite.test.lldbtest import *
+from lldbsuite.test.decorators import *
+import lldbsuite.test.lldbutil as lldbutil
+
+class TestSwiftW(TestBase):
+
+    mydir = TestBase.compute_mydir(__file__)
+    NO_DEBUG_INFO_TEST = True
+
+    def setUp(self):
+        TestBase.setUp(self)
+
+    # Don't run ClangImporter tests if Clangimporter is disabled.
+    @skipIf(setting=('symbols.use-swift-clangimporter', 'false'))
+    @skipUnlessDarwin
+    @swiftTest
+    def test(self):
+        """This tests that -Werror is removed from ClangImporter options by
+           introducing two conflicting macro definitions in idfferent dylibs.
+        """
+        self.build()
+        target,  _, _, _ = lldbutil.run_to_source_breakpoint(
+            self, "break here", lldb.SBFileSpec('dylib.swift'))
+        # Register shlib so it can run on-device.
+        self.registerSharedLibrariesWithTarget(target, ['Dylib'])
+
+        # Turn on logging.
+        log = self.getBuildArtifact("types.log")
+        self.expect("log enable lldb types -f "+log)
+        
+        self.expect("p foo", DATA_TYPES_DISPLAYED_CORRECTLY, substrs=["42"])
+        sanity = 0
+        logfile = open(log, "r")
+        for line in logfile:
+            self.assertFalse("-Werror" in line)
+            if "-DCONFLICT" in line:
+                sanity += 1
+        # We see it twice, once in the module, once in the expression context.
+        self.assertEqual(sanity, 2)
