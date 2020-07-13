5a26c3f9f0aabcc4e7dc4012dbabffa2e3a20af2
Romaric JODIN <rjodin@upmem.com>
2020-07-13 14:05:31 +0200

    dpu: lldb: remove test suite from llvm repo

--- lldb/packages/Python/lldbsuite/test/dpu/dma_fault/main.c
+++
@@ -1,8 +0,0 @@
-#include <mram.h>
-#include <stdint.h>
-
-int main() {
-        uint64_t buffer;
-        mram_read(0xffffffff, &buffer, sizeof(uint64_t));
-        return (int)buffer;
-}
--- lldb/packages/Python/lldbsuite/test/dpu/write_register/main.c
+++
@@ -1,5 +0,0 @@
-int main() {
-  int res;
-  __asm__("add %[r], r0, 1\n" : [ r ] "=r"(res));
-  return res;
-}
