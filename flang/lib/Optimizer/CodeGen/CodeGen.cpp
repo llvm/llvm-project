102415fbdaff1e8e079a2a7d1281b567bd3741b9
Eric <eschweitz@nvidia.com>
2020-07-14 14:05:08 -0700

    Extend !fir.char to improve disambiguation of CHARACTER type representation. Theextension is not presently used. However, the plan will be to experiment and see if it helps simplify lowering.

--- flang/lib/Optimizer/CodeGen/CodeGen.cpp
+++ flang/lib/Optimizer/CodeGen/CodeGen.cpp
@@ -5,6 +5,10 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
+//
+// Coding style: https://mlir.llvm.org/getting_started/DeveloperGuide/
+//
+//===----------------------------------------------------------------------===//
 
 #include "flang/Optimizer/CodeGen/CodeGen.h"
 #include "DescriptorModel.h"
