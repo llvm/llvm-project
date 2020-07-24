049554993d936b032813e2c50764f43d2f41fd61
Jean Perier <jperier@nvidia.com>
2020-07-24 02:59:32 -0700

    Workaround mlir conversion materialization (D82831)
    Simply add a source and target materialization handler that do nothing
    and that override the default handlers that would add illegal
    `LLVM::DialectCastOp` otherwise.
    
    This is the simplest workaround, but not an actual fix, something may be
    wrong with D82831 (most likely fir lowering to llvm happens in a way that
    mlir infrastructure is not expecting in D82831).

--- flang/lib/Optimizer/CodeGen/CodeGen.cpp
+++ flang/lib/Optimizer/CodeGen/CodeGen.cpp
@@ -126,6 +126,30 @@ public:
     addConversion([&](mlir::NoneType none) {
       return mlir::LLVM::LLVMType::getStructTy(llvmDialect, {});
     });
+
+    // FIXME: https://reviews.llvm.org/D82831 introduced an automatic
+    // materliazation of conversion around function calls that is not working
+    // well with fir lowering to llvm (incorrect llvm.mlir.cast are inserted).
+    // Workaround until better analysis: register a handler that does not insert
+    // any conversions.
+    addSourceMaterialization(
+        [&](mlir::OpBuilder &builder, mlir::Type resultType,
+            mlir::ValueRange inputs,
+            mlir::Location loc) -> llvm::Optional<mlir::Value> {
+          if (inputs.size() != 1)
+            return llvm::None;
+          return inputs[0];
+        });
+    // Similar FIXME workaround here (needed for compare.fir/select-type.fir
+    // tests).
+    addTargetMaterialization(
+        [&](mlir::OpBuilder &builder, mlir::Type resultType,
+            mlir::ValueRange inputs,
+            mlir::Location loc) -> llvm::Optional<mlir::Value> {
+          if (inputs.size() != 1)
+            return llvm::None;
+          return inputs[0];
+        });
   }
 
   // This returns the type of a single column. Rows are added by the caller.
