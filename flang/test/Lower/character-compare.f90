4bb3296bea5462760f85f2dac94ac136e90b9998
Eric Schweitz <eschweitz@nvidia.com>
2020-07-27 09:10:56 -0700

    Standardize calls to use fir.call in lowering.

--- flang/test/Lower/character-compare.f90
+++ flang/test/Lower/character-compare.f90
@@ -4,7 +4,7 @@
 subroutine compare(x, c1, c2)
   character(len=4) c1, c2
   logical x
-  ! CHECK: %[[RES:.*]] = call @_FortranACharacterCompareScalar1
+  ! CHECK: %[[RES:.*]] = fir.call @_FortranACharacterCompareScalar1
   ! CHECK: cmpi "slt", %[[RES]],
   x = c1 < c2
 end subroutine compare
