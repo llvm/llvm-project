161828990d19390c89de1fea32c45a94628a485c
V Donaldson <vdonaldson@nvidia.com>
2020-08-04 15:36:10 -0700

    review update

--- flang/test/Lower/altret.f90
+++ flang/test/Lower/altret.f90
@@ -1,6 +1,23 @@
 ! RUN: bbc -emit-fir -o - %s | FileCheck %s
 
+! CHECK-LABEL: func @_QPss
+subroutine ss(n)
+  print*, n
+  ! CHECK: return{{$}}
+  return
+! CHECK-LABEL: func @_QPee
+entry ee(n,*)
+  print*, n
+  ! CHECK: return %{{.}} : index
+  return 1
+end
+
 ! CHECK-LABEL: func @_QQmain
+  call ss(7)
+  call ee(2, *3)
+  print*, 'default'
+3 print*, 3
+
   print*, k(10,20)
   print*, k(15,15)
   print*, k(20,10)
