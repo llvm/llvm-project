98032d261eaf743ef4713635159d50566d702153
ne75 <ne75@cornell.edu>
2020-08-04 17:08:32 -0700

    fix merge conflict marker

--- llvm/include/llvm/BinaryFormat/ELF.h
+++ llvm/include/llvm/BinaryFormat/ELF.h
@@ -311,12 +311,8 @@ enum {
   EM_RISCV = 243,         // RISC-V
   EM_LANAI = 244,         // Lanai 32-bit processor
   EM_BPF = 247,           // Linux kernel bpf virtual machine
-<<<<<<< HEAD
   EM_VE = 251,            // NEC SX-Aurora VE
-=======
-
   EM_P2 = 300,
->>>>>>> a54cdc85693... Squashing all commits -- P2 Target for LLVM
 };
 
 // Object file classes.
