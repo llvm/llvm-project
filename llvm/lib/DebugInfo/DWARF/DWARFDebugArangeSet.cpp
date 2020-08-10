f5f8d86dc4c91ef492b919edf98335d4d09188a8
James Henderson <james.henderson@sony.com>
2020-08-10 13:36:44 +0100

    Don't error for zero-length arange entries
    Although the DWARF specification states that .debug_aranges entries
    can't have length zero, these can occur in the wild. There's no
    particular reason to enforce this part of the spec, since functionally
    they have no impact. The patch removes the error and introduces a new
    warning for premature terminator entries which does not stop parsing.
    
    This is a relanding of cb3a598c87db, adding the missing obj2yaml part
    that was needed.
    
    Fixes https://bugs.llvm.org/show_bug.cgi?id=46805. See also
    https://reviews.llvm.org/D71932 which originally introduced the error.
    
    Reviewed by: ikudrin, dblaikie, Higuoxing
    
    Differential Revision: https://reviews.llvm.org/D85313

--- llvm/lib/DebugInfo/DWARF/DWARFDebugArangeSet.cpp
+++ llvm/lib/DebugInfo/DWARF/DWARFDebugArangeSet.cpp
@@ -132,19 +132,20 @@ Error DWARFDebugArangeSet::extract(DWARFDataExtractor data,
 
   uint64_t end_offset = Offset + full_length;
   while (*offset_ptr < end_offset) {
+    uint64_t EntryOffset = *offset_ptr;
     arangeDescriptor.Address = data.getUnsigned(offset_ptr, HeaderData.AddrSize);
     arangeDescriptor.Length = data.getUnsigned(offset_ptr, HeaderData.AddrSize);
 
-    if (arangeDescriptor.Length == 0) {
-      // Each set of tuples is terminated by a 0 for the address and 0
-      // for the length.
-      if (arangeDescriptor.Address == 0 && *offset_ptr == end_offset)
+    // Each set of tuples is terminated by a 0 for the address and 0
+    // for the length.
+    if (arangeDescriptor.Length == 0 && arangeDescriptor.Address == 0) {
+      if (*offset_ptr == end_offset)
         return ErrorSuccess();
       return createStringError(
           errc::invalid_argument,
           "address range table at offset 0x%" PRIx64
-          " has an invalid tuple (length = 0) at offset 0x%" PRIx64,
-          Offset, *offset_ptr - tuple_size);
+          " has a premature terminator entry at offset 0x%" PRIx64,
+          Offset, EntryOffset);
     }
 
     ArangeDescriptors.push_back(arangeDescriptor);
