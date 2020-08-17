llvm/llvm-project

commit 3a8fc5073945237a154ea2aaf477af23ff2cfc75

[DebugInfo] Add -fuse-ctor-homing cc1 flag so we can turn on constructor homing only if limited debug info is already on.
This adds a cc1 flag to enable constructor homing but doesn't turn on debug
info if it wasn't enabled already (which is what using
-debug-info-kind=constructor does). This will be used for testing, and won't
be needed anymore once ctor homing is used as default / merged into =limited.

Bug to enable ctor homing: https://bugs.llvm.org/show_bug.cgi?id=46537

Differential Revision: https://reviews.llvm.org/D85799

clang/lib/Frontend/CompilerInvocation.cpp
@@ -775,6 +775,12 @@ static bool ParseCodeGenArgs(CodeGenOptions &Opts, ArgList &Args, InputKind IK,
     else
       Opts.setDebugInfo(static_cast<codegenoptions::DebugInfoKind>(Val));
   }
+  // If -fuse-ctor-homing is set and limited debug info is already on, then use
+  // constructor homing.
+  if (Arg *A = Args.getLastArg(OPT_fuse_ctor_homing))
+    if (Opts.getDebugInfo() == codegenoptions::LimitedDebugInfo)
+      Opts.setDebugInfo(codegenoptions::DebugInfoConstructor);
+
   if (Arg *A = Args.getLastArg(OPT_debugger_tuning_EQ)) {
     unsigned Val = llvm::StringSwitch<unsigned>(A->getValue())
                        .Case("gdb", unsigned(llvm::DebuggerKind::GDB))

clang/include/clang/Driver/Options.td
@@ -3691,6 +3691,8 @@ def mrelocation_model : Separate<["-"], "mrelocation-model">,
   AutoNormalizeEnum;
 def fno_math_builtin : Flag<["-"], "fno-math-builtin">,
   HelpText<"Disable implicit builtin knowledge of math functions">;
+def fuse_ctor_homing: Flag<["-"], "fuse-ctor-homing">,
+    HelpText<"Use constructor homing if we are using limited debug info already">;
 }
 
 def disable_llvm_verifier : Flag<["-"], "disable-llvm-verifier">,

clang/test/CodeGenCXX/debug-info-ctor-homing-flag.cpp
@@ -1,0 +1,20 @@
+// RUN: %clang -cc1 -debug-info-kind=constructor -emit-llvm %s -o - \
+// RUN:        | FileCheck %s -check-prefix=CTOR_HOMING
+// RUN: %clang -cc1 -debug-info-kind=limited -fuse-ctor-homing -emit-llvm %s -o - \
+// RUN:        | FileCheck %s -check-prefix=CTOR_HOMING
+// RUN: %clang -cc1 -debug-info-kind=standalone -fuse-ctor-homing -emit-llvm %s -o - \
+// RUN:        | FileCheck %s -check-prefix=FULL_DEBUG
+// RUN: %clang -cc1 -debug-info-kind=line-tables-only -fuse-ctor-homing -emit-llvm %s -o - \
+// RUN:        | FileCheck %s -check-prefix=NO_DEBUG
+// RUN: %clang -cc1 -fuse-ctor-homing -emit-llvm %s -o - \
+// RUN:        | FileCheck %s -check-prefix=NO_DEBUG
+
+// This tests that the -fuse-ctor-homing is only used if limited debug info would have
+// been used otherwise.
+
+// CTOR_HOMING: !DICompositeType(tag: DW_TAG_structure_type, name: "A"{{.*}}flags: DIFlagFwdDecl
+// FULL_DEBUG: !DICompositeType(tag: DW_TAG_structure_type, name: "A"{{.*}}DIFlagTypePassByValue
+// NO_DEBUG-NOT: !DICompositeType(tag: DW_TAG_structure_type, name: "A"
+struct A {
+  A();
+} TestA;

commit 8b4990cd209a0305c88b3d4579bcedc57e1fec0d

[clang] fix a compilation bug
With gcc 6.3.0, I hit the following compilation bug:
  /home/yhs/work/llvm-project/clang/lib/Frontend/CompilerInvocation.cpp:
  In function ‘bool ParseCodeGenArgs(clang::CodeGenOptions&, llvm::opt::ArgList&,
  clang::InputKind, clang::DiagnosticsEngine&, const clang::TargetOptions&,
  const clang::FrontendOptions&)’:
  /home/yhs/work/llvm-project/clang/lib/Frontend/CompilerInvocation.cpp:780:12:
    error: unused variable ‘A’ [-Werror=unused-variable]
     if (Arg *A = Args.getLastArg(OPT_fuse_ctor_homing))
              ^
  cc1plus: all warnings being treated as errors

The bug is introduced by Commit 3a8fc5073945 ("[DebugInfo] Add
-fuse-ctor-homing cc1 flag so we can turn on constructor homing only
if limited debug info is already on.")

clang/lib/Frontend/CompilerInvocation.cpp
@@ -777,7 +777,7 @@ static bool ParseCodeGenArgs(CodeGenOptions &Opts, ArgList &Args, InputKind IK,
   }
   // If -fuse-ctor-homing is set and limited debug info is already on, then use
   // constructor homing.
-  if (Arg *A = Args.getLastArg(OPT_fuse_ctor_homing))
+  if (Args.getLastArg(OPT_fuse_ctor_homing))
     if (Opts.getDebugInfo() == codegenoptions::LimitedDebugInfo)
       Opts.setDebugInfo(codegenoptions::DebugInfoConstructor);
 