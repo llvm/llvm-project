604f380a7f4d769f7f01f17315a25da0f61aa12a
Shivarama Rao <shivarama.rao@amd.com>
2020-08-21 20:32:37 +0530

    respecting the isystem flag to setup the include path for module files
    fixes https://ontrack.amd.com/browse/ORNLA-12
    [AOMP] [flang] The AOMP 11.7-1 Fortran compiler claims to support the -isystem flag, but ignores it.
    
    Change-Id: I1b5a9b712464f00a01581ee07dcbf47a650b5d4d

--- clang/lib/Driver/ToolChains/Flang.cpp
+++ clang/lib/Driver/ToolChains/Flang.cpp
@@ -771,6 +771,11 @@ void Flang::ConstructJob(Compilation &C, const JobAction &JA,
     UpperCmdArgs.push_back("-idir");
     UpperCmdArgs.push_back(Arg->getValue(0));
   }
+  for (auto Arg : Args.filtered(options::OPT_isystem)) {
+    Arg->claim();
+    UpperCmdArgs.push_back("-idir");
+    UpperCmdArgs.push_back(Arg->getValue(0));
+  }
 
   // Add user-defined module directories
   for (auto Arg : Args.filtered(options::OPT_ModuleDir, options::OPT_J)) {
