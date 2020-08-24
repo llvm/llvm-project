9d4fd1aefad78e2ec558f3c114d7048cb4b84c5a
Gongyu Deng <gy_deng@icloud.com>
2020-08-24 18:58:17 +0200

    [lldb] type category name common completion
    1. Added a new common completion TypeCategoryNames to provide a list of category names for completion;
    2. Applied the completion to these commands: type category delete/enable/disable/list/define;
    3. Added a related test case;
    4. Bound the completion to the arguments of the type 'eArgTypeName'.
    
    Reviewed By: teemperor, JDevlieghere
    
    Differential Revision: https://reviews.llvm.org/D84124
    
    (cherry picked from commit 188f1ac301c5c6da6d2f5697952510fc39cbdd43)

--- lldb/source/Commands/CommandObjectTarget.cpp
+++ lldb/source/Commands/CommandObjectTarget.cpp
@@ -4829,8 +4829,6 @@ public:
   void
   HandleArgumentCompletion(CompletionRequest &request,
                            OptionElementVector &opt_element_vector) override {
-    if (request.GetCursorIndex())
-      return;
     CommandCompletions::InvokeCommonCompletionCallbacks(
         GetCommandInterpreter(), CommandCompletions::eStopHookIDCompletion,
         request, nullptr);
