ba052ddca4e0e5dda9137536f03d0f21cbc36d65
Valentin Clement <clementval@gmail.com>
2020-08-31 21:48:56 -0400

    [flang][openacc] Lower rest of clauses for the loop construct

--- flang/test/Lower/OpenACC/acc-loop.f90
+++ flang/test/Lower/OpenACC/acc-loop.f90
@@ -8,6 +8,10 @@ program acc_loop
   integer, parameter :: n = 10
   real, dimension(n) :: a, b
   real, dimension(n, n) :: c, d
+  integer :: gangNum = 8
+  integer :: gangStatic = 8
+  integer :: vectorLength = 128
+  integer, parameter :: tileSize = 2
 
 
   !$acc loop
@@ -18,7 +22,218 @@ program acc_loop
 !CHECK:      acc.loop {
 !CHECK:        fir.do_loop
 !CHECK:        acc.yield
-!CHECK-NEXT: }
+!CHECK-NEXT: }{{$}}
+
+ !$acc loop seq
+  DO i = 1, n
+    a(i) = b(i)
+  END DO
+
+!CHECK:      acc.loop {
+!CHECK:        fir.do_loop
+!CHECK:        acc.yield
+!CHECK-NEXT: } attributes {seq}
+
+  !$acc loop auto
+  DO i = 1, n
+    a(i) = b(i)
+  END DO
+
+!CHECK:      acc.loop {
+!CHECK:        fir.do_loop
+!CHECK:        acc.yield
+!CHECK-NEXT: } attributes {auto}
+
+  !$acc loop independent
+  DO i = 1, n
+    a(i) = b(i)
+  END DO
+
+!CHECK:      acc.loop {
+!CHECK:        fir.do_loop
+!CHECK:        acc.yield
+!CHECK-NEXT: } attributes {independent}
+
+  !$acc loop gang
+  DO i = 1, n
+    a(i) = b(i)
+  END DO
+
+!CHECK:      acc.loop gang {
+!CHECK:        fir.do_loop
+!CHECK:        acc.yield
+!CHECK-NEXT: }{{$}}
+
+  !$acc loop gang(num: 8)
+  DO i = 1, n
+    a(i) = b(i)
+  END DO
+
+!CHECK:      [[GANGNUM1:%.*]] = constant 8 : i32
+!CHECK-NEXT: acc.loop gang(num: [[GANGNUM1]]) {
+!CHECK:        fir.do_loop
+!CHECK:        acc.yield
+!CHECK-NEXT: }{{$}}
+
+  !$acc loop gang(num: gangNum)
+  DO i = 1, n
+    a(i) = b(i)
+  END DO
+
+!CHECK:      [[GANGNUM2:%.*]] = fir.load %{{.*}} : !fir.ref<i32>
+!CHECK-NEXT: acc.loop gang(num: [[GANGNUM2]]) {
+!CHECK:        fir.do_loop
+!CHECK:        acc.yield
+!CHECK-NEXT: }{{$}}
+
+ !$acc loop gang(num: gangNum, static: gangStatic)
+  DO i = 1, n
+    a(i) = b(i)
+  END DO
+
+!CHECK: acc.loop gang(num: %{{.*}}, static: %{{.*}}) {
+!CHECK:        fir.do_loop
+!CHECK:        acc.yield
+!CHECK-NEXT: }{{$}}
+
+  !$acc loop vector
+  DO i = 1, n
+    a(i) = b(i)
+  END DO
+
+!CHECK:      acc.loop vector {
+!CHECK:        fir.do_loop
+!CHECK:        acc.yield
+!CHECK-NEXT: }{{$}}
+
+  !$acc loop vector(128)
+  DO i = 1, n
+    a(i) = b(i)
+  END DO
+
+!CHECK: [[CONSTANT128:%.*]] = constant 128 : i32
+!CHECK:      acc.loop vector([[CONSTANT128]]) {
+!CHECK:        fir.do_loop
+!CHECK:        acc.yield
+!CHECK-NEXT: }{{$}}
+
+  !$acc loop vector(vectorLength)
+  DO i = 1, n
+    a(i) = b(i)
+  END DO
+
+!CHECK:      [[VECTORLENGTH:%.*]] = fir.load %{{.*}} : !fir.ref<i32>
+!CHECK:      acc.loop vector([[VECTORLENGTH]]) {
+!CHECK:        fir.do_loop
+!CHECK:        acc.yield
+!CHECK-NEXT: }{{$}}
+
+!$acc loop worker
+  DO i = 1, n
+    a(i) = b(i)
+  END DO
+
+!CHECK:      acc.loop worker {
+!CHECK:        fir.do_loop
+!CHECK:        acc.yield
+!CHECK-NEXT: }{{$}}
+
+  !$acc loop worker(128)
+  DO i = 1, n
+    a(i) = b(i)
+  END DO
+
+!CHECK: [[WORKER128:%.*]] = constant 128 : i32
+!CHECK:      acc.loop worker([[WORKER128]]) {
+!CHECK:        fir.do_loop
+!CHECK:        acc.yield
+!CHECK-NEXT: }{{$}}
+
+  !$acc loop private(c)
+  DO i = 1, n
+    a(i) = b(i)
+  END DO
+
+!CHECK:      acc.loop private(%{{.*}}: !fir.ref<!fir.array<10x10xf32>>) {
+!CHECK:        fir.do_loop
+!CHECK:        acc.yield
+!CHECK-NEXT: }{{$}}
+
+  !$acc loop private(c, d)
+  DO i = 1, n
+    a(i) = b(i)
+  END DO
+
+!CHECK:      acc.loop private(%{{.*}}: !fir.ref<!fir.array<10x10xf32>>, %{{.*}}: !fir.ref<!fir.array<10x10xf32>>) {
+!CHECK:        fir.do_loop
+!CHECK:        acc.yield
+!CHECK-NEXT: }{{$}}
+
+  !$acc loop private(c) private(d)
+  DO i = 1, n
+    a(i) = b(i)
+  END DO
+
+!CHECK:      acc.loop private(%{{.*}}: !fir.ref<!fir.array<10x10xf32>>, %{{.*}}: !fir.ref<!fir.array<10x10xf32>>) {
+!CHECK:        fir.do_loop
+!CHECK:        acc.yield
+!CHECK-NEXT: }{{$}}
+
+  !$acc loop tile(2)
+  DO i = 1, n
+    a(i) = b(i)
+  END DO
+!CHECK:      [[TILESIZE:%.*]] = constant 2 : i32
+!CHECK:      acc.loop tile([[TILESIZE]]: i32) {
+!CHECK:        fir.do_loop
+!CHECK:        acc.yield
+!CHECK-NEXT: }{{$}}
+
+ !$acc loop tile(*)
+  DO i = 1, n
+    a(i) = b(i)
+  END DO
+!CHECK:      [[TILESIZEM1:%.*]] = constant -1 : i32
+!CHECK:      acc.loop tile([[TILESIZEM1]]: i32) {
+!CHECK:        fir.do_loop
+!CHECK:        acc.yield
+!CHECK-NEXT: }{{$}}
+
+  !$acc loop tile(2, 2)
+  DO i = 1, n
+    DO j = 1, n
+      c(i, j) = d(i, j)
+    END DO
+  END DO
+
+!CHECK:      [[TILESIZE1:%.*]] = constant 2 : i32
+!CHECK:      [[TILESIZE2:%.*]] = constant 2 : i32
+!CHECK:      acc.loop tile([[TILESIZE1]]: i32, [[TILESIZE2]]: i32) {
+!CHECK:        fir.do_loop
+!CHECK:        acc.yield
+!CHECK-NEXT: }{{$}}
+
+  !$acc loop tile(tileSize)
+  DO i = 1, n
+    a(i) = b(i)
+  END DO
+
+!CHECK:      acc.loop tile(%{{.*}}: i32) {
+!CHECK:        fir.do_loop
+!CHECK:        acc.yield
+!CHECK-NEXT: }{{$}}
+
+  !$acc loop tile(tileSize, tileSize)
+  DO i = 1, n
+    DO j = 1, n
+      c(i, j) = d(i, j)
+    END DO
+  END DO
+
+!CHECK:      acc.loop tile(%{{.*}}: i32, %{{.*}}: i32) {
+!CHECK:        fir.do_loop
+!CHECK:        acc.yield
+!CHECK-NEXT: }{{$}}
 
   !$acc loop collapse(2)
   DO i = 1, n
@@ -46,9 +261,9 @@ program acc_loop
 !CHECK:          acc.loop {
 !CHECK:            fir.do_loop
 !CHECK:            acc.yield
-!CHECK-NEXT:   }
+!CHECK-NEXT:   }{{$}}
 !CHECK:        acc.yield
-!CHECK-NEXT: }
+!CHECK-NEXT: }{{$}}
 
 end program
 
