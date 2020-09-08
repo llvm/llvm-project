3eee689e16f7cc1620c5ad7af3ef6507db19afe7
Adrian Prantl <aprantl@apple.com>
2020-09-08 13:13:33 -0700

    Skip inconsistent DWARFImporter test.

--- lldb/test/API/lang/swift/mix_any_object/TestSwiftMixAnyObjectType.py
+++ lldb/test/API/lang/swift/mix_any_object/TestSwiftMixAnyObjectType.py
@@ -27,8 +27,8 @@ class TestSwiftMixAnyObjectType(TestBase):
     def setUp(self):
         TestBase.setUp(self)
 
-    @expectedFailureAll(bugnumber="rdar://60396797",
-                        setting=('symbols.use-swift-clangimporter', 'false'))
+    @skipIf(bugnumber="rdar://60396797", # inconsistently fails/passes.
+            setting=('symbols.use-swift-clangimporter', 'false'))
     @skipUnlessDarwin
     @swiftTest
     def test_any_object_type(self):
