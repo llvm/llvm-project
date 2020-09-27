40961c251fe3b4778fee132ada710fb85ebd2074
Sourabh Singh Tomar <SourabhSingh.Tomar@amd.com>
2020-09-28 01:56:27 +0530

    [flang][OpenMP] Lower private, firstprivate, shared, copyin clauses for Parallel construct
     TODO:
        Handle Attribute based clauses:
        1. Default clause
        2. ProcBind clause

---
+++ flang/test/Lower/OpenMP/omp-parallel-private-clause.f90
@@ -0,0 +1,40 @@
+! This test checks lowering of OpenMP parallel Directive with
+! `PRIVATE` clause present.
+
+! RUN: bbc -fopenmp -emit-fir %s -o - | \
+! RUN:   FileCheck %s --check-prefix=FIRDialect
+! RUN: bbc -fopenmp -emit-llvm %s -o - | \
+! RUN:   FileCheck %s --check-prefix=LLVMIRDialect
+
+!FIRDialect: func @_QPprivate_clause(%[[ARG1:.*]]: !fir.ref<i32>, %[[ARG2:.*]]: !fir.ref<!fir.array<10xi32>>) {
+!FIRDialect-DAG: %[[ALPHA:.*]] = fir.alloca i32 {name = "alpha"}
+!FIRDialect-DAG: %[[BETA:.*]] = fir.alloca i32 {name = "beta"}
+!FIRDialect-DAG: %[[GAMA:.*]] = fir.alloca i32 {name = "gama"}
+!FIRDialect-DAG: %[[ALPHA_ARRAY:.*]] = fir.alloca !fir.array<10xi32> {name = "alpha_array"}
+!FIRDialect-DAG:  omp.parallel private(%[[ALPHA]] : !fir.ref<i32>, %[[BETA]] : !fir.ref<i32>, %[[GAMA]] : !fir.ref<i32>,
+!%[[ALPHA_ARRAY]] : !fir.ref<!fir.array<10xi32>>, %[[ARG1]] : !fir.ref<i32>, %[[ARG2]] : !fir.ref<!fir.array<10xi32>>)) {
+!FIRDialect:    omp.terminator
+!FIRDialect:  }
+
+!LLVMDialect: llvm.func @_QPprivate_clause(%[[ARG1:.*]]: !llvm.ptr<i32>, %[[ARG2:.*]]: !llvm.ptr<array<10 x i32>>) {
+!LLVMIRDialect-DAG: %[[ALPHA:.*]] = llvm.alloca %{{.*}} x !llvm.i32 {in_type = i32, name = "alpha"} : (!llvm.i64) -> !llvm.ptr<i32>
+!LLVMIRDialect-DAG: %[[BETA:.*]] = llvm.alloca %{{.*}} x !llvm.i32 {in_type = i32, name = "beta"} : (!llvm.i64) -> !llvm.ptr<i32>
+!LLVMIRDialect-DAG: %[[GAMA:.*]] = llvm.alloca %{{.*}} x !llvm.i32 {in_type = i32, name = "gama"} : (!llvm.i64) -> !llvm.ptr<i32>
+!LLVMIRDialect-DAG: %[[ALPHA_ARRAY:.*]] = llvm.alloca %{{.*}} x !llvm.array<10 x i32> {in_type = !fir.array<10xi32>, name = "alpha_array"} : (!llvm.i64) -> !llvm.ptr<array<10 x i32>>
+!LLVMIRDialect:  omp.parallel private(%[[ALPHA]] : !llvm.ptr<i32>, %[[BETA]] : !llvm.ptr<i32>, %[[GAMA]] : !llvm.ptr<i32>,
+!%[[ALPHA_ARRAY]] : !llvm.ptr<array<10 x i32>>, %[[ARG1]] : !llvm.ptr<i32>, %[[ARG2]] : !llvm.ptr<array<10 x i32>>) {
+!LLVMIRDialect:    omp.terminator
+!LLVMIRDialect:  }
+
+subroutine private_clause(arg1, arg2)
+
+        integer :: arg1, arg2(10)
+        integer :: alpha, beta, gama
+        integer :: alpha_array(10)
+
+!$OMP PARALLEL PRIVATE(alpha, beta, gama, alpha_array, arg1, arg2)
+        print*, "PRIVATE"
+        print*, alpha, beta, gama
+!$OMP END PARALLEL
+
+end subroutine
