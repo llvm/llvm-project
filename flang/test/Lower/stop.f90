38144385b6c385c3f74cc332acfbfd49c49131f8
Eric Schweitz <eschweitz@nvidia.com>
2020-09-28 17:12:49 -0700

    Fixes the issue where STOP 'text' was calling the wrong runtime.
    This patch hacks the runtime files simply so things will typecheck and compile. It does not change the runtime library.

--- flang/test/Lower/stop.f90
+++ flang/test/Lower/stop.f90
@@ -4,7 +4,7 @@
 subroutine stop_test(b)
  ! CHECK-DAG: %[[c0:.*]] = constant 0 : i32
  ! CHECK-DAG: %[[false:.*]] = constant false
- ! CHECK: call @_Fortran{{.*}}StopStatement(%[[c0]], %[[false]], %[[false]])
+ ! CHECK: fir.call @_Fortran{{.*}}StopStatement(%[[c0]], %[[false]], %[[false]])
  ! CHECK-NEXT: fir.unreachable
  stop
 end subroutine
@@ -14,7 +14,7 @@ subroutine stop_code()
   stop 42
  ! CHECK-DAG: %[[c42:.*]] = constant 42 : i32
  ! CHECK-DAG: %[[false:.*]] = constant false
- ! CHECK: call @_Fortran{{.*}}StopStatement(%[[c42]], %[[false]], %[[false]])
+ ! CHECK: fir.call @_Fortran{{.*}}StopStatement(%[[c42]], %[[false]], %[[false]])
  ! CHECK-NEXT: fir.unreachable
 end subroutine
 
@@ -24,7 +24,7 @@ subroutine stop_error()
  ! CHECK-DAG: %[[c0:.*]] = constant 0 : i32
  ! CHECK-DAG: %[[true:.*]] = constant true
  ! CHECK-DAG: %[[false:.*]] = constant false
- ! CHECK: call @_Fortran{{.*}}StopStatement(%[[c0]], %[[true]], %[[false]])
+ ! CHECK: fir.call @_Fortran{{.*}}StopStatement(%[[c0]], %[[true]], %[[false]])
  ! CHECK-NEXT: fir.unreachable
 end subroutine
 
@@ -36,7 +36,7 @@ subroutine stop_quiet(b)
  ! CHECK-DAG: %[[false:.*]] = constant false
  ! CHECK-DAG: %[[b:.*]] = fir.load %arg0
  ! CHECK-DAG: %[[bi1:.*]] = fir.convert %[[b]] : (!fir.logical<4>) -> i1
- ! CHECK: call @_Fortran{{.*}}StopStatement(%[[c0]], %[[false]], %[[bi1]])
+ ! CHECK: fir.call @_Fortran{{.*}}StopStatement(%[[c0]], %[[false]], %[[bi1]])
  ! CHECK-NEXT: fir.unreachable
 end subroutine
 
@@ -48,8 +48,22 @@ subroutine stop_error_code_quiet(b)
  ! CHECK-DAG: %[[true:.*]] = constant true
  ! CHECK-DAG: %[[b:.*]] = fir.load %arg0
  ! CHECK-DAG: %[[bi1:.*]] = fir.convert %[[b]] : (!fir.logical<4>) -> i1
- ! CHECK: call @_Fortran{{.*}}StopStatement(%[[c66]], %[[true]], %[[bi1]])
+ ! CHECK: fir.call @_Fortran{{.*}}StopStatement(%[[c66]], %[[true]], %[[bi1]])
  ! CHECK-NEXT: fir.unreachable
 end subroutine
 
-! CHECK: func @_Fortran{{.*}}StopStatement(i32, i1, i1) -> none
+
+! CHECK-LABEL stop_char_lit
+subroutine stop_char_lit
+  ! CHECK-DAG: %[[false:.*]] = constant false
+  ! CHECK-DAG: %[[five:.*]] = constant 5 : index
+  ! CHECK-DAG: %[[lit:.*]] = fir.address_of(@_QQ{{.*}}) : !fir.ref<!fir.array<5x!fir.char<1>>>
+  ! CHECK-DAG: %[[buff:.*]] = fir.convert %[[lit]] : (!fir.ref<!fir.array<5x!fir.char<1>>>) -> !fir.ref<i8>
+  ! CHECK-DAG: %[[len:.*]] = fir.convert %[[five]] : (index) -> i64
+  ! CHECK: fir.call @{{.*}}StopStatementText(%[[buff]], %[[len]], %[[false]], %[[false]]) :
+  ! CHECK-NEXT: fir.unreachable
+  stop 'crash'
+end subroutine stop_char_lit
+
+! CHECK-DAG: func @_Fortran{{.*}}StopStatement(i32, i1, i1) -> none
+! CHECK-DAG: func @_Fortran{{.*}}StopStatementText(!fir.ref<i8>, i64, i1, i1) -> none
