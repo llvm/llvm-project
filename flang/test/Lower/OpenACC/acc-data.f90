59c450e81e9e74afcf0f6a4c87651f6a315c86ba
Valentin Clement <clementval@gmail.com>
2020-10-02 21:43:32 -0400

    [flang][openacc] Fix data construct lowering (#470)

--- flang/test/Lower/OpenACC/acc-data.f90
+++ flang/test/Lower/OpenACC/acc-data.f90
@@ -12,56 +12,56 @@ program acc_data
   !$acc data copy(a, b, c)
   !$acc end data
 
-!CHECK:      acc.data copy([[A]]: !fir.ref<!fir.array<10x10xf32>>, [[B]]: !fir.ref<!fir.array<10x10xf32>>, [[C]]: !fir.ref<!fir.array<10x10xf32>>) {
+!CHECK:      acc.data copy([[A]], [[B]], [[C]] : !fir.ref<!fir.array<10x10xf32>>, !fir.ref<!fir.array<10x10xf32>>, !fir.ref<!fir.array<10x10xf32>>) {
 !CHECK:        acc.terminator
 !CHECK-NEXT: }{{$}}
 
   !$acc data copy(a) copy(b) copy(c)
   !$acc end data
 
-!CHECK:      acc.data copy([[A]]: !fir.ref<!fir.array<10x10xf32>>, [[B]]: !fir.ref<!fir.array<10x10xf32>>, [[C]]: !fir.ref<!fir.array<10x10xf32>>) {
+!CHECK:      acc.data copy([[A]], [[B]], [[C]] : !fir.ref<!fir.array<10x10xf32>>, !fir.ref<!fir.array<10x10xf32>>, !fir.ref<!fir.array<10x10xf32>>) {
 !CHECK:        acc.terminator
 !CHECK-NEXT: }{{$}}
 
   !$acc data copyin(a) copyin(readonly: b, c)
   !$acc end data
 
-!CHECK:      acc.data copyin([[A]]: !fir.ref<!fir.array<10x10xf32>>) copyin_readonly([[B]]: !fir.ref<!fir.array<10x10xf32>>, [[C]]: !fir.ref<!fir.array<10x10xf32>>) {
+!CHECK:      acc.data copyin([[A]] : !fir.ref<!fir.array<10x10xf32>>) copyin_readonly([[B]], [[C]] : !fir.ref<!fir.array<10x10xf32>>, !fir.ref<!fir.array<10x10xf32>>) {
 !CHECK:        acc.terminator
 !CHECK-NEXT: }{{$}}
 
   !$acc data copyout(a) copyout(zero: b) copyout(c)
   !$acc end data
 
-!CHECK:      acc.data copyout([[A]]: !fir.ref<!fir.array<10x10xf32>>, [[C]]: !fir.ref<!fir.array<10x10xf32>>) copyout_zero([[B]]: !fir.ref<!fir.array<10x10xf32>>) {
+!CHECK:      acc.data copyout([[A]], [[C]] : !fir.ref<!fir.array<10x10xf32>>, !fir.ref<!fir.array<10x10xf32>>) copyout_zero([[B]] : !fir.ref<!fir.array<10x10xf32>>) {
 !CHECK:        acc.terminator
 !CHECK-NEXT: }{{$}}
 
   !$acc data create(a, b) create(zero: c)
   !$acc end data
 
-!CHECK:      acc.data create([[A]]: !fir.ref<!fir.array<10x10xf32>>, [[B]]: !fir.ref<!fir.array<10x10xf32>>) create_zero([[C]]: !fir.ref<!fir.array<10x10xf32>>) {
+!CHECK:      acc.data create([[A]], [[B]] : !fir.ref<!fir.array<10x10xf32>>, !fir.ref<!fir.array<10x10xf32>>) create_zero([[C]] : !fir.ref<!fir.array<10x10xf32>>) {
 !CHECK:        acc.terminator
 !CHECK-NEXT: }{{$}}
 
   !$acc data no_create(a, b) create(zero: c)
   !$acc end data
 
-!CHECK:      acc.data create_zero([[C]]: !fir.ref<!fir.array<10x10xf32>>) no_create([[A]]: !fir.ref<!fir.array<10x10xf32>>, [[B]]: !fir.ref<!fir.array<10x10xf32>>) {
+!CHECK:      acc.data create_zero([[C]] : !fir.ref<!fir.array<10x10xf32>>) no_create([[A]], [[B]] : !fir.ref<!fir.array<10x10xf32>>, !fir.ref<!fir.array<10x10xf32>>) {
 !CHECK:        acc.terminator
 !CHECK-NEXT: }{{$}}
 
   !$acc data present(a, b, c)
   !$acc end data
 
-!CHECK:      acc.data present([[A]]: !fir.ref<!fir.array<10x10xf32>>, [[B]]: !fir.ref<!fir.array<10x10xf32>>, [[C]]: !fir.ref<!fir.array<10x10xf32>>) {
+!CHECK:      acc.data present([[A]], [[B]], [[C]] : !fir.ref<!fir.array<10x10xf32>>, !fir.ref<!fir.array<10x10xf32>>, !fir.ref<!fir.array<10x10xf32>>) {
 !CHECK:        acc.terminator
 !CHECK-NEXT: }{{$}}
 
   !$acc data attach(b, c)
   !$acc end data
 
-!CHECK:      acc.data attach([[B]]: !fir.ref<!fir.array<10x10xf32>>, [[C]]: !fir.ref<!fir.array<10x10xf32>>) {
+!CHECK:      acc.data attach([[B]], [[C]] : !fir.ref<!fir.array<10x10xf32>>, !fir.ref<!fir.array<10x10xf32>>) {
 !CHECK:        acc.terminator
 !CHECK-NEXT: }{{$}}
 
