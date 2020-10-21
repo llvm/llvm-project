202bca8ce065d6543aee512cdf37f2ade93521ad
Adrian Prantl <aprantl@apple.com>
2020-10-21 12:52:19 -0700

    Remove obsolote early exit. This fixes a regression hidden by 2c911bceb06ed376801251bdfd992905a66f276c

--- lldb/test/API/lang/swift/protocol_extension_computed_property/TestProtocolExtensionComputerProperty.py
+++ lldb/test/API/lang/swift/protocol_extension_computed_property/TestProtocolExtensionComputerProperty.py
@@ -4,7 +4,6 @@ from lldbsuite.test.decorators import *
 lldbinline.MakeInlineTest(
     __file__, globals(),
         decorators=[
-            expectedFailureAll, #FIXME: This regressed silently due to 2c911bceb06ed376801251bdfd992905a66f276c
             swiftTest,skipUnlessDarwin,
             skipIf(bugnumber="rdar://60396797", # should work but crashes.
                    setting=('symbols.use-swift-clangimporter', 'false'))
