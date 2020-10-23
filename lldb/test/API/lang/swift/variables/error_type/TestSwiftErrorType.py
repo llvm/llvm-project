7e8ae7b5c66b72d9befeccf190c3f97cbf607ce0
Raphael Isemann <teemperor@gmail.com>
2020-10-23 20:16:10 +0200

    [lldb][swift] Use dynamic types as a fallback in AddVariableInfo
    Since commit eaceb46661c6833f11fa98659223652f6d549f4a AddVariableInfo is
    always using the dynamic type if requested. The dynamic types appear
    to not always be usable and caused a few tests to fail(1), but as the swiftTest
    decorator was always skipping all Swift tests since commit
    2c911bceb06ed376801251bdfd992905a66f276c this wasn't discovered before landing.
    
    This patch restores the old behaviour where possible by first trying the
    non-dynamic type and falling back to the dynamic type in case the non-dynamic
    type can't be fully realized.
    
    (1) All the failures seem to be related to us reading corrupt data from memory
    when using the dynamic type in the expression parser. The dynamic types itself
    appear to be perfectly fine, so it's not clear why the new behaviour doesn't work.

--- lldb/test/API/lang/swift/variables/error_type/TestSwiftErrorType.py
+++ lldb/test/API/lang/swift/variables/error_type/TestSwiftErrorType.py
@@ -12,7 +12,4 @@
 import lldbsuite.test.lldbinline as lldbinline
 from lldbsuite.test.decorators import *
 
-lldbinline.MakeInlineTest(__file__, globals(), decorators=[
-#FIXME: This regressed silently due to 2c911bceb06ed376801251bdfd992905a66f276c
-                          expectedFailureAll,
-                          swiftTest])
+lldbinline.MakeInlineTest(__file__, globals(), decorators=[swiftTest])
