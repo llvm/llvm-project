ae3223cb32b87d3e9c4324232a40cd8b758ba17c
Adrian Prantl <aprantl@apple.com>
2020-11-02 13:00:03 -0800

    Use a better workaround for rdar://problem/25587546
    (cherry picked from commit 83ad6796ad40d852536c9b7fdd6932b7edd7cfe1)

--- lldb/test/API/functionalities/data-formatter/data-formatter-objc/TestDataFormatterObjCNSError.py
+++ lldb/test/API/functionalities/data-formatter/data-formatter-objc/TestDataFormatterObjCNSError.py
@@ -15,7 +15,6 @@ from ObjCDataFormatterTestCase import ObjCDataFormatterTestCase
 class ObjCDataFormatterNSError(ObjCDataFormatterTestCase):
 
     @skipUnlessDarwin
-    @expectedFailureAll(debug_info=["dwarf", "dsym", "dwo"], bugnumber="rdar://25587546")
     def test_nserror_with_run_command(self):
         """Test formatters for NSError."""
         self.appkit_tester_impl(self.nserror_data_formatter_commands)
@@ -23,13 +22,14 @@ class ObjCDataFormatterNSError(ObjCDataFormatterTestCase):
     def nserror_data_formatter_commands(self):
         self.expect(
             'frame variable nserror', substrs=['domain: @"Foobar" - code: 12'])
-
+         
         self.expect(
             'frame variable nserrorptr',
             substrs=['domain: @"Foobar" - code: 12'])
 
+        # FIXME: <rdar://problem/25587546> On llvm.org this works without the `-d run`!
         self.expect(
-            'frame variable nserror->_userInfo', substrs=['2 key/value pairs'])
+            'frame variable -d run -- nserror->_userInfo', substrs=['2 key/value pairs'])
 
         self.expect(
             'frame variable nserror->_userInfo --ptr-depth 1 -d run-target',
