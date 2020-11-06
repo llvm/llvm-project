03c7b3e95f4aa7737d07972f0becab2a15226ec9
Jonas Devlieghere <jonas@devlieghere.com>
2020-11-06 12:44:03 -0800

    [lldb] Re-enable TestTargetCreateDeps on Linux
    rdar://44831253
    (cherry picked from commit d353c6326230638f2f733c6712840e1f09337968)

--- lldb/test/API/commands/target/create-deps/TestTargetCreateDeps.py
+++ lldb/test/API/commands/target/create-deps/TestTargetCreateDeps.py
@@ -20,7 +20,6 @@ class TargetDependentsTestCase(TestBase):
         TestBase.setUp(self)
         self.build()
 
-    @skipIf(bugnumber='rdar://44831253', oslist=['linux'])
     def has_exactly_one_image(self, matching, msg=""):
         self.expect(
             "image list",
@@ -49,7 +48,6 @@ class TargetDependentsTestCase(TestBase):
         self.runCmd("target create -ddefault " + exe, CURRENT_EXECUTABLE_SET)
         self.has_exactly_one_image(False)
 
-    @skipIf(bugnumber='rdar://44831253', oslist=['linux'])
     def test_dependents_explicit_true_exe(self):
         """Test default behavior"""
         exe = self.getBuildArtifact("a.out")
@@ -65,14 +63,12 @@ class TargetDependentsTestCase(TestBase):
         self.runCmd("target create -dfalse " + exe, CURRENT_EXECUTABLE_SET)
         self.has_exactly_one_image(False)
 
-    @skipIf(bugnumber='rdar://44831253', oslist=['linux'])
     def test_dependents_implicit_false_exe(self):
         """Test default behavior"""
         exe = self.getBuildArtifact("a.out")
         self.runCmd("target create  -d " + exe, CURRENT_EXECUTABLE_SET)
         self.has_exactly_one_image(True)
 
-    @skipIf(bugnumber='rdar://44831253', oslist=['linux'])
     @expectedFailureAndroid # android will return mutiple images
     def test_dependents_implicit_default_lib(self):
         ctx = self.platformContext
@@ -81,7 +77,6 @@ class TargetDependentsTestCase(TestBase):
         self.runCmd("target create " + lib, CURRENT_EXECUTABLE_SET)
         self.has_exactly_one_image(True)
 
-    @skipIf(bugnumber='rdar://44831253', oslist=['linux'])
     def test_dependents_explicit_default_lib(self):
         ctx = self.platformContext
         dylibName = ctx.shlib_prefix + 'load_a.' + ctx.shlib_extension
@@ -89,7 +84,6 @@ class TargetDependentsTestCase(TestBase):
         self.runCmd("target create -ddefault " + lib, CURRENT_EXECUTABLE_SET)
         self.has_exactly_one_image(True)
 
-    @skipIf(bugnumber='rdar://44831253', oslist=['linux'])
     def test_dependents_explicit_true_lib(self):
         ctx = self.platformContext
         dylibName = ctx.shlib_prefix + 'load_a.' + ctx.shlib_extension
@@ -107,7 +101,6 @@ class TargetDependentsTestCase(TestBase):
         self.runCmd("target create -dfalse " + lib, CURRENT_EXECUTABLE_SET)
         self.has_exactly_one_image(False)
 
-    @skipIf(bugnumber='rdar://44831253', oslist=['linux'])
     def test_dependents_implicit_false_lib(self):
         ctx = self.platformContext
         dylibName = ctx.shlib_prefix + 'load_a.' + ctx.shlib_extension
