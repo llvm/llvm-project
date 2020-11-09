2c43c671afa32d148614874e3a6d30b37d116841
Eric Schweitz <eschweitz@nvidia.com>
2020-11-09 11:15:09 -0800

    Moves some of the symbol map related function out of the expression helper and to their own file. The symbol map related code will be easier to find. This refactoring is long overdue. Restructure symbol map to allow for scoping (modern Fortran).

---
+++ flang/lib/Lower/SymbolMap.cpp
@@ -0,0 +1,162 @@
+//===-- SymbolMap.cpp -------------------------------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// Pretty printers for symbol boxes, etc.
+//
+//===----------------------------------------------------------------------===//
+
+#include "SymbolMap.h"
+#include "mlir/IR/StandardTypes.h"
+
+mlir::Value fir::getBase(const fir::ExtendedValue &exv) {
+  return exv.match([](const fir::UnboxedValue &x) { return x; },
+                   [](const auto &x) { return x.getAddr(); });
+}
+
+mlir::Value fir::getLen(const fir::ExtendedValue &exv) {
+  return exv.match([](const fir::CharBoxValue &x) { return x.getLen(); },
+                   [](const fir::CharArrayBoxValue &x) { return x.getLen(); },
+                   [](const fir::BoxValue &x) { return x.getLen(); },
+                   [](const auto &) { return mlir::Value{}; });
+}
+
+fir::ExtendedValue fir::substBase(const fir::ExtendedValue &exv,
+                                  mlir::Value base) {
+  return exv.match(
+      [=](const fir::UnboxedValue &x) { return fir::ExtendedValue(base); },
+      [=](const auto &x) { return fir::ExtendedValue(x.clone(base)); });
+}
+
+llvm::raw_ostream &fir::operator<<(llvm::raw_ostream &os,
+                                   const fir::CharBoxValue &box) {
+  return os << "boxchar { addr: " << box.getAddr() << ", len: " << box.getLen()
+            << " }";
+}
+
+llvm::raw_ostream &fir::operator<<(llvm::raw_ostream &os,
+                                   const fir::ArrayBoxValue &box) {
+  os << "boxarray { addr: " << box.getAddr();
+  if (box.getLBounds().size()) {
+    os << ", lbounds: [";
+    llvm::interleaveComma(box.getLBounds(), os);
+    os << "]";
+  } else {
+    os << ", lbounds: all-ones";
+  }
+  os << ", shape: [";
+  llvm::interleaveComma(box.getExtents(), os);
+  return os << "]}";
+}
+
+llvm::raw_ostream &fir::operator<<(llvm::raw_ostream &os,
+                                   const fir::CharArrayBoxValue &box) {
+  os << "boxchararray { addr: " << box.getAddr() << ", len : " << box.getLen();
+  if (box.getLBounds().size()) {
+    os << ", lbounds: [";
+    llvm::interleaveComma(box.getLBounds(), os);
+    os << "]";
+  } else {
+    os << " lbounds: all-ones";
+  }
+  os << ", shape: [";
+  llvm::interleaveComma(box.getExtents(), os);
+  return os << "]}";
+}
+
+llvm::raw_ostream &fir::operator<<(llvm::raw_ostream &os,
+                                   const fir::BoxValue &box) {
+  os << "box { addr: " << box.getAddr();
+  if (box.getLen())
+    os << ", size: " << box.getLen();
+  if (box.params.size()) {
+    os << ", type params: [";
+    llvm::interleaveComma(box.params, os);
+    os << "]";
+  }
+  if (box.getLBounds().size()) {
+    os << ", lbounds: [";
+    llvm::interleaveComma(box.getLBounds(), os);
+    os << "]";
+  }
+  if (box.getExtents().size()) {
+    os << ", shape: [";
+    llvm::interleaveComma(box.getExtents(), os);
+    os << "]";
+  }
+  return os << "}";
+}
+
+llvm::raw_ostream &fir::operator<<(llvm::raw_ostream &os,
+                                   const fir::ProcBoxValue &box) {
+  return os << "boxproc: { procedure: " << box.getAddr()
+            << ", context: " << box.hostContext << "}";
+}
+
+llvm::raw_ostream &fir::operator<<(llvm::raw_ostream &os,
+                                   const fir::ExtendedValue &exv) {
+  exv.match([&](const auto &value) { os << value; });
+  return os;
+}
+
+void Fortran::lower::SymMap::addSymbol(Fortran::semantics::SymbolRef sym,
+                                       const fir::ExtendedValue &exv,
+                                       bool force) {
+  exv.match([&](const fir::UnboxedValue &v) { addSymbol(sym, v, force); },
+            [&](const fir::CharBoxValue &v) { makeSym(sym, v, force); },
+            [&](const fir::ArrayBoxValue &v) { makeSym(sym, v, force); },
+            [&](const fir::CharArrayBoxValue &v) { makeSym(sym, v, force); },
+            [&](const fir::BoxValue &v) { makeSym(sym, v, force); },
+            [](auto) {
+              llvm::report_fatal_error(
+                  "box value should not be added to symbol table");
+            });
+}
+
+Fortran::lower::SymbolBox
+Fortran::lower::SymMap::lookupSymbol(Fortran::semantics::SymbolRef sym) {
+  for (auto jmap = symbolMapStack.rbegin(), jend = symbolMapStack.rend();
+       jmap != jend; ++jmap) {
+    auto iter = jmap->find(&*sym);
+    if (iter != jmap->end())
+      return iter->second;
+  }
+  // FIXME BUG: How does this know that the caller is expecting the host
+  // associated symbol?
+  // Follow host association
+  if (const auto *details =
+          sym->detailsIf<Fortran::semantics::HostAssocDetails>())
+    return lookupSymbol(details->symbol());
+  return SymbolBox::None{};
+}
+
+llvm::raw_ostream &
+Fortran::lower::operator<<(llvm::raw_ostream &os,
+                           const Fortran::lower::SymbolBox &symBox) {
+  symBox.match(
+      [&](const Fortran::lower::SymbolBox::None &box) {
+        os << "** symbol not properly mapped **\n";
+      },
+      [&](const Fortran::lower::SymbolBox::Intrinsic &val) {
+        os << val.getAddr() << '\n';
+      },
+      [&](const auto &box) { os << box << '\n'; });
+  return os;
+}
+
+llvm::raw_ostream &
+Fortran::lower::operator<<(llvm::raw_ostream &os,
+                           const Fortran::lower::SymMap &symMap) {
+  os << "Symbol map:\n";
+  for (auto i : llvm::enumerate(symMap.symbolMapStack)) {
+    os << " level " << i.index() << "<{\n";
+    for (auto iter : i.value())
+      os << "  symbol [" << *iter.first << "] ->\n    " << iter.second;
+    os << " }>\n";
+  }
+  return os;
+}
