8da910285d588a4ea572eed40e8406be03d38298
Florian Hahn <flo@fhahn.com>
2020-11-18 11:32:45 +0000

    [Matrix] Adjust matrix pointer type for inline asm arguments.
    Matrix types in memory are represented as arrays, but accessed through
    vector pointers, with the alignment specified on the access operation.
    
    For inline assembly, update pointer arguments to use vector pointers.
    Otherwise there will be a mis-match if the matrix is also an
    input-argument which is represented as vector.
    
    Reviewed By: nickdesaulniers
    
    Differential Revision: https://reviews.llvm.org/D91631

--- clang/test/CodeGen/matrix-type.c
+++ clang/test/CodeGen/matrix-type.c
@@ -156,3 +156,21 @@ void matrix_struct(Matrix *a, Matrix *b) {
   // CHECK-NEXT:    ret void
   b->Data = a->Data;
 }
+
+typedef double dx4x4_t __attribute__((matrix_type(4, 4)));
+void matrix_inline_asm_memory_readwrite() {
+  // CHECK-LABEL: define void @matrix_inline_asm_memory_readwrite()
+  // CHECK-NEXT:  entry:
+  // CHECK-NEXT:    [[ALLOCA:%.+]] = alloca [16 x double], align 8
+  // CHECK-NEXT:    [[PTR1:%.+]] = bitcast [16 x double]* [[ALLOCA]] to <16 x double>*
+  // CHECK-NEXT:    [[PTR2:%.+]] = bitcast [16 x double]* [[ALLOCA]] to <16 x double>*
+  // CHECK-NEXT:    [[VAL:%.+]] = load <16 x double>, <16 x double>* [[PTR2]], align 8
+  // CHECK-NEXT:    call void asm sideeffect "", "=*r|m,0,~{memory},~{dirflag},~{fpsr},~{flags}"(<16 x double>* [[PTR1]], <16 x double> [[VAL]])
+  // CHECK-NEXT:    ret void
+
+  dx4x4_t m;
+  asm volatile(""
+               : "+r,m"(m)
+               :
+               : "memory");
+}
