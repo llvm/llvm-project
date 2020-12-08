9e4118852aec46daf571e84c33e273f03b01ad27
Dave Lee <davelee.com@gmail.com>
2020-12-08 13:48:03 -0800

    use fallback for any failed data dependency

--- lldb/source/Plugins/TypeSystem/Swift/TypeSystemSwiftTypeRef.cpp
+++ lldb/source/Plugins/TypeSystem/Swift/TypeSystemSwiftTypeRef.cpp
@@ -2071,22 +2071,17 @@ TypeSystemSwiftTypeRef::GetNumChildren(opaque_compiler_type_t type,
                                        bool omit_empty_base_classes,
                                        const ExecutionContext *exe_ctx) {
   auto impl = [&]() -> uint32_t {
-    if (!exe_ctx)
-      return 0;
-
     // TODO: which of these should be logged on failure?
-    if (auto *exe_scope = exe_ctx->GetBestExecutionContextScope())
-      if (auto *runtime =
-              SwiftLanguageRuntime::Get(exe_scope->CalculateProcess())) {
-        if (auto num_children =
-                runtime->GetNumChildren(GetCanonicalType(type), nullptr))
-          return *num_children;
-        else
-          return m_swift_ast_context->GetNumChildren(
-              ReconstructType(type), omit_empty_base_classes, exe_ctx);
-      }
+    if (exe_ctx)
+      if (auto *exe_scope = exe_ctx->GetBestExecutionContextScope())
+        if (auto *runtime =
+                SwiftLanguageRuntime::Get(exe_scope->CalculateProcess()))
+          if (auto num_children =
+                  runtime->GetNumChildren(GetCanonicalType(type), nullptr))
+            return *num_children;
 
-    return 0;
+    return m_swift_ast_context->GetNumChildren(
+        ReconstructType(type), omit_empty_base_classes, exe_ctx);
   };
   VALIDATE_AND_RETURN(
       impl, GetNumChildren, type,
