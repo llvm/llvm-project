cd89c6b508ea08b4169b0e82c388efd1a0859e19
Dave Lee <davelee.com@gmail.com>
2020-12-11 17:01:30 -0800

    add ExistentialMetatype
    (cherry picked from commit e97417a7f34d196cbdd8696925ad2f63dcb7f785)

--- lldb/source/Plugins/TypeSystem/Swift/TypeSystemSwiftTypeRef.cpp
+++ lldb/source/Plugins/TypeSystem/Swift/TypeSystemSwiftTypeRef.cpp
@@ -1787,6 +1787,7 @@ bool TypeSystemSwiftTypeRef::IsPossibleDynamicType(opaque_compiler_type_t type,
     case Node::Kind::ProtocolList:
     case Node::Kind::ProtocolListWithClass:
     case Node::Kind::ProtocolListWithAnyObject:
+    case Node::Kind::ExistentialMetatype:
     case Node::Kind::DynamicSelf:
       return true;
     case Node::Kind::TypeAlias: {
