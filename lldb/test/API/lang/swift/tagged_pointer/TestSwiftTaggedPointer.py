8dbc41fab211f895a7460cda295bf56646e6e520
Adrian Prantl <aprantl@apple.com>
2020-12-15 15:47:16 -0800

    Convert inline test to regular test
    (cherry picked from commit f9147f6505abab7ec247b6d11a0c9fa6176d08b0)
    
     Conflicts:
    	lldb/test/API/lang/swift/tagged_pointer/TestSwiftTaggedPointer.py

--- lldb/test/API/lang/swift/tagged_pointer/TestSwiftTaggedPointer.py
+++ lldb/test/API/lang/swift/tagged_pointer/TestSwiftTaggedPointer.py
@@ -1,18 +1,31 @@
-# TestSwiftTaggedPointer.py
-#
-# This source file is part of the Swift.org open source project
-#
-# Copyright (c) 2014 - 2016 Apple Inc. and the Swift project authors
-# Licensed under Apache License v2.0 with Runtime Library Exception
-#
-# See https://swift.org/LICENSE.txt for license information
-# See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
-#
-# ------------------------------------------------------------------------------
-import lldbsuite.test.lldbinline as lldbinline
+import lldb
 from lldbsuite.test.decorators import *
+import lldbsuite.test.lldbtest as lldbtest
+import lldbsuite.test.lldbutil as lldbutil
+import unittest2
 
-# This test depends on NSObject, so it is not available on non-Darwin
-# platforms.
-lldbinline.MakeInlineTest(__file__, globals(),
-                          decorators=[swiftTest,skipUnlessDarwin])
+class TestSwiftAnyType(lldbtest.TestBase):
+
+    mydir = lldbtest.TestBase.compute_mydir(__file__)
+
+    @swiftTest
+    # This test depends on NSObject, so it is not available on non-Darwin
+    # platforms.
+    @skipUnlessDarwin
+    @skipIf(bugnumber="rdar://problem/66842937")
+    def test(self):
+        self.build()
+        target, process, thread, bkpt = lldbutil.run_to_source_breakpoint(
+            self, 'break here', lldb.SBFileSpec('main.swift'))
+
+        self.expect('frame variable -d run -- a', substrs=['Int64(3)'])
+        self.expect('frame variable -d run -- b', substrs=['Int64(3)'])
+         
+        self.expect('frame variable -d run -- c', substrs=['"hi"'])
+        self.expect('frame variable -d run -- d', substrs=['"hi"'])
+         
+        self.expect('expr -d run -- a', substrs=['Int64(3)'])
+        self.expect('expr -d run -- b', substrs=['Int64(3)'])
+         
+        self.expect('expr -d run -- c', substrs=['"hi"'])
+        self.expect('expr -d run -- d', substrs=['"hi"'])
