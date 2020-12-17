92a5c090e41cdefde1c71b68b89c7b4074b6da0f
Tim Northover <t.p.northover@gmail.com>
2020-12-17 13:29:40 +0000

    Swift support: add __attribute__((swift_async_context)) to args.
    This translates to "swiftasync" LLVM attribute.

--- clang/lib/CodeGen/CGCall.cpp
+++ clang/lib/CodeGen/CGCall.cpp
@@ -2356,6 +2356,10 @@ void CodeGenModule::ConstructAttributeList(
     case ParameterABI::SwiftContext:
       Attrs.addAttribute(llvm::Attribute::SwiftSelf);
       break;
+
+    case ParameterABI::SwiftAsyncContext:
+      Attrs.addAttribute(llvm::Attribute::SwiftAsync);
+      break;
     }
 
     if (FI.getExtParameterInfo(ArgNo).isNoEscape())
