1e15425862941a327ab341b3ffb47fe1cffc7f54
Florian Hahn <flo@fhahn.com>
2020-12-17 08:47:46 +0000

    [InstCombine] Preserve !annotation for newly created instructions.
    When replacing an instruction with !annotation with a newly created
    replacement, add the !annotation metadata to the replacement.
    
    This mostly covers cases where the new instructions are created using
    the ::Create helpers. Instructions created by IRBuilder will be handled
    by D91444.
    
    Reviewed By: thegameg
    
    Differential Revision: https://reviews.llvm.org/D93399

--- llvm/lib/Transforms/InstCombine/InstructionCombining.cpp
+++ llvm/lib/Transforms/InstCombine/InstructionCombining.cpp
@@ -3565,8 +3565,8 @@ bool InstCombiner::run() {
         LLVM_DEBUG(dbgs() << "IC: Old = " << *I << '\n'
                           << "    New = " << *Result << '\n');
 
-        if (I->getDebugLoc())
-          Result->setDebugLoc(I->getDebugLoc());
+        Result->copyMetadata(*I,
+                             {LLVMContext::MD_dbg, LLVMContext::MD_annotation});
         // Everything uses the new instruction now.
         I->replaceAllUsesWith(Result);
 
