46264ed1cdb1aa853994f5da4839e54f1a277896
Jean Perier <jc@home>
2021-01-07 12:37:40 +0100

    Fix local character array lowering
    createNewLocal was used to build character arrays without considering
    the dynamic lengths.
    Upgarde createNewLocal and FirOpBuilder::allocateLocal to take an array of lengths
    parameter.
    For now, merge the lengths and the shape in allcoateLocal because fir::AllocaOp is
    ignoring it lengths arguments (fixme added).
    Remove usage of createCharacterTemp in variable lowering (use createNewLocal instead)
    to make character processing similar in all cases and to have the variable name in the
    allocaOp.

---
+++ flang/test/Lower/character-local-variables.f90
@@ -0,0 +1,53 @@
+! RUN: bbc %s -o - | FileCheck %s
+
+! Test lowering of local character variables
+
+! CHECK-LABEL: func @_QPscalar_cst_len
+subroutine scalar_cst_len()
+  character(10) :: c
+  ! CHECK: fir.alloca !fir.char<1,10> {name = "_QFscalar_cst_lenEc"}
+end subroutine
+
+! CHECK-LABEL: func @_QPscalar_dyn_len
+subroutine scalar_dyn_len(l)
+  integer :: l
+  character(l) :: c
+  ! CHECK: %[[l:.*]] = fir.load %arg0 : !fir.ref<i32>
+  ! CHECK: %[[li:.*]] = fir.convert %[[l]] : (i32) -> index
+  ! CHECK: fir.alloca !fir.char<1,?>, %[[li]] {name = "_QFscalar_dyn_lenEc"}
+end subroutine
+
+! CHECK-LABEL: func @_QPcst_array_cst_len
+subroutine cst_array_cst_len()
+  character(10) :: c(20)
+  ! CHECK: fir.alloca !fir.array<20x!fir.char<1,10>> {name = "_QFcst_array_cst_lenEc"}
+end subroutine
+
+! CHECK-LABEL: func @_QPcst_array_dyn_len
+subroutine cst_array_dyn_len(l)
+  integer :: l
+  character(l) :: c
+  ! CHECK: %[[l:.*]] = fir.load %arg0 : !fir.ref<i32>
+  ! CHECK: %[[li:.*]] = fir.convert %[[l]] : (i32) -> index
+  ! CHECK: fir.alloca !fir.char<1,?>, %[[li]] {name = "_QFcst_array_dyn_lenEc"}
+end subroutine
+
+! CHECK-LABEL: func @_QPdyn_array_cst_len
+subroutine dyn_array_cst_len(n)
+  integer :: n
+  character(10) :: c(n)
+  ! CHECK: %[[n:.*]] = fir.load %arg0 : !fir.ref<i32>
+  ! CHECK: %[[ni:.*]] = fir.convert %[[n]] : (i32) -> index
+  ! CHECK: fir.alloca !fir.array<?x!fir.char<1,10>>, %[[ni]] {name = "_QFdyn_array_cst_lenEc"}
+end subroutine
+
+! CHECK: func @_QPdyn_array_dyn_len
+subroutine dyn_array_dyn_len(l, n)
+  integer :: l, n
+  character(l) :: c(n)
+  ! CHECK-DAG: %[[l:.*]] = fir.load %arg0 : !fir.ref<i32>
+  ! CHECK-DAG: %[[li:.*]] = fir.convert %[[l]] : (i32) -> index
+  ! CHECK-DAG: %[[n:.*]] = fir.load %arg1 : !fir.ref<i32>
+  ! CHECK-DAG: %[[ni:.*]] = fir.convert %[[n]] : (i32) -> index
+  ! CHECK: fir.alloca !fir.array<?x!fir.char<1,?>>, %[[li]], %[[ni]] {name = "_QFdyn_array_dyn_lenEc"}
+end subroutine
