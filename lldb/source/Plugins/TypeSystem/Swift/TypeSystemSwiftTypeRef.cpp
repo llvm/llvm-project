5cbe02ab191349255c711d0c05277d561cccda9d
Dave Lee <davelee.com@gmail.com>
2021-01-11 11:59:56 -0800

    explicitly handle Builtin.Vec

--- lldb/source/Plugins/TypeSystem/Swift/TypeSystemSwiftTypeRef.cpp
+++ lldb/source/Plugins/TypeSystem/Swift/TypeSystemSwiftTypeRef.cpp
@@ -2152,7 +2152,12 @@ lldb::Encoding TypeSystemSwiftTypeRef::GetEncoding(opaque_compiler_type_t type,
           node->getText() == swift::BUILTIN_TYPE_NAME_UNSAFEVALUEBUFFER ||
           node->getText() == swift::BUILTIN_TYPE_NAME_BRIDGEOBJECT)
         return lldb::eEncodingUint;
+      if (node->getText() == swift::BUILTIN_TYPE_NAME_VEC) {
+        count = 0;
+        return lldb::eEncodingInvalid;
+      }
 
+      llvm_unreachable("Unhandled builtin");
       count = 0;
       return lldb::eEncodingInvalid;
     }
