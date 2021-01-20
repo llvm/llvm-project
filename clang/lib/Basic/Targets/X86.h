7a2e59cd9ba3ce5c0a734277c1b77f68b9d7f8dc
Varun Gandhi <varun_gandhi@apple.com>
2021-01-20 15:54:44 -0800

    [Clang] Introduce Swift async calling convention.
    This change is intended as initial setup. The plan is to add
    more semantic checks later. Most of the code closely mirrors
    that for the Swift calling convention. Three places are
    marked as [FIXME: swiftasynccc]; those will be addressed in
    follow-up commits.
    
    Differential Revision: https://reviews.llvm.org/D95561
    
    (cherry-pick to apple/stable/20210107 from not-yet-merged
     revision)

--- clang/lib/Basic/Targets/X86.h
+++ clang/lib/Basic/Targets/X86.h
@@ -344,6 +344,7 @@ public:
     case CC_C:
     case CC_PreserveMost:
     case CC_Swift:
+    case CC_SwiftAsync:
     case CC_X86Pascal:
     case CC_IntelOclBicc:
     case CC_OpenCLKernel:
@@ -704,6 +705,7 @@ public:
     switch (CC) {
     case CC_C:
     case CC_Swift:
+    case CC_SwiftAsync:
     case CC_X86VectorCall:
     case CC_IntelOclBicc:
     case CC_Win64:
@@ -785,6 +787,7 @@ public:
     case CC_PreserveAll:
     case CC_X86_64SysV:
     case CC_Swift:
+    case CC_SwiftAsync:
     case CC_X86RegCall:
     case CC_OpenCLKernel:
       return CCCR_OK;
