6e81c55817871e0fda63c13b0d6ebee0ef89ec48
Dave Lee <davelee.com@gmail.com>
2021-01-27 10:34:14 -0800

    add comment

--- lldb/test/API/lang/swift/expression/protocol_extension_self/TestSwiftProtocolExtensionSelf.py
+++ lldb/test/API/lang/swift/expression/protocol_extension_self/TestSwiftProtocolExtensionSelf.py
@@ -15,4 +15,6 @@ class TestSwiftProtocolExtensionSelf(TestBase):
 
         lldbutil.run_to_source_breakpoint(self, "break here",
                                           lldb.SBFileSpec('main.swift'))
+        # This should work without dynamic types. For discussion, see:
+        # https://github.com/apple/llvm-project/pull/2382
         self.expect("e -d no-run -- f", substrs=[' = 12345'])
