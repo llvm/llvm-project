3b9ea2dc8eeb3c3893213f57d532b32ef3619859
Johannes Doerfert <johannes@jdoerfert.de>
2021-01-29 02:42:20 -0600

    [OpenMP] Attribute target diagnostics properly
    Type errors in function declarations were not (always) diagnosed prior
    to this patch. Furthermore, certain remarks did not get associated
    properly which caused them to be emitted multiple times.
    
    Reviewed By: JonChesterfield
    
    Differential Revision: https://reviews.llvm.org/D95912
    
    (cherry picked from commit f9286b434b764b366f1aad9249c04e7741ed5518)

--- clang/lib/Sema/SemaDecl.cpp
+++ clang/lib/Sema/SemaDecl.cpp
@@ -9420,6 +9420,9 @@ Sema::ActOnFunctionDeclarator(Scope *S, Declarator &D, DeclContext *DC,
     }
   }
 
+  if (LangOpts.SYCLIsDevice || (LangOpts.OpenMP && LangOpts.OpenMPIsDevice))
+    checkDeviceDecl(NewFD, D.getBeginLoc());
+
   if (!getLangOpts().CPlusPlus) {
     // Perform semantic checking on the function declaration.
     if (!NewFD->isInvalidDecl() && NewFD->isMain())
