f2039016b5e8b617ae851a4a2b651c7cca637bfb
Jonas Devlieghere <jonas@devlieghere.com>
2021-02-01 23:02:31 -0800

    [lldb] Fix invalid calls to expect() in TestSwiftAsyncFnArgs.py
    (cherry picked from commit d4386ba2738f32cb190780f304c346940e569883)

--- lldb/test/API/lang/swift/async/async_fnargs/TestSwiftAsyncFnArgs.py
+++ lldb/test/API/lang/swift/async/async_fnargs/TestSwiftAsyncFnArgs.py
@@ -17,7 +17,7 @@ class TestSwiftAsyncFnArgs(lldbtest.TestBase):
         target, process, thread, bkpt = lldbutil.run_to_source_breakpoint(
             self, 'Set breakpoint here', src)
 
-        self.expect("frame var -d run-target -- msg", '"world"')
+        self.expect("frame var -d run-target -- msg", substrs=['"world"'])
 
         # Continue into the second coroutine funclet.
         bkpt2 = target.BreakpointCreateBySourceRegex("And also here", src, None)
@@ -26,4 +26,4 @@ class TestSwiftAsyncFnArgs(lldbtest.TestBase):
         self.assertEqual(
              len(lldbutil.get_threads_stopped_at_breakpoint(process, bkpt2)), 1)
 
-        self.expect("frame var -d run-target -- msg", '"world"')
+        self.expect("frame var -d run-target -- msg", substrs=['"world"'])
