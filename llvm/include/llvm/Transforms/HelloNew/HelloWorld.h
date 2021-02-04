8a86aedc5a49bb3b3f04800a2619d1a8ef6f2df7
Jon Kartago Lamida <lamidaj@gmail.com>
2021-02-04 23:56:07 +0800

    Add helloworld pass

--- llvm/include/llvm/Transforms/HelloNew/HelloWorld.h
+++ llvm/include/llvm/Transforms/HelloNew/HelloWorld.h
@@ -9,7 +9,13 @@
 #ifndef LLVM_TRANSFORMS_HELLONEW_HELLOWORLD_H
 #define LLVM_TRANSFORMS_HELLONEW_HELLOWORLD_H
 
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/DebugInfoMetadata.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Instructions.h"
 #include "llvm/IR/PassManager.h"
+#include "llvm/Pass.h"
+#include "llvm/Support/raw_os_ostream.h"
 
 namespace llvm {
 
--- llvm/lib/Transforms/HelloNew/HelloWorld.cpp
+++ llvm/lib/Transforms/HelloNew/HelloWorld.cpp
@@ -12,6 +12,23 @@ using namespace llvm;
 
 PreservedAnalyses HelloWorldPass::run(Function &F,
                                       FunctionAnalysisManager &AM) {
-  errs() << F.getName() << "\n";
+  llvm::errs() << "Visiting function " << F.getName() << "\n";
+
+  for (llvm::BasicBlock &BB : F) {
+    llvm::errs() << "Visiting bb " << B.getName() << "\n";
+    for (llvm::Instruction &II : BB) {
+      llvm::errs() << "Visiting II " << II.getName() << "\n";
+      llvm::Instruction *I = &II;
+      if (llvm::CallInst *CI = llvm::dyn_cast<llvm::CallInst>(I)) {
+        llvm::errs() << "Visiting CI " << CI->getName() << "\n";
+        if (llvm::DILocation *Loc = I->getDebugLoc()) {
+          unsigned Line = Loc->getLine();
+          llvm::StringRef File = Loc->getFilename();
+          llvm::StringRef Dir = Loc->getDirectory();
+          llvm::errs() << Dir << "/" << File << ":" << Line << "\n";
+        }
+      }
+    }
+  }
   return PreservedAnalyses::all();
 }
