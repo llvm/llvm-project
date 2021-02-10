5061de8cc43b0abe4bd22dfd7629b9c32c74178a
Akira Hatanaka <ahatanaka@apple.com>
2021-02-10 14:47:06 -0800

    [ObjC][ARC] Use operand bundle 'clang.arc.attachedcall' instead of explicitly emitting retainRV or claimRV calls in the IR
    Background:
    
    This fixes a longstanding problem where llvm breaks ARC's autorelease
    optimization (see the link below) by separating calls from the marker
    instructions or retainRV/claimRV calls. The backend changes are in
    https://reviews.llvm.org/D92569.
    
    https://clang.llvm.org/docs/AutomaticReferenceCounting.html#arc-runtime-objc-autoreleasereturnvalue
    
    What this patch does to fix the problem:
    
    - The front-end adds operand bundle "clang.arc.attachedcall" to calls,
      which indicates the call is implicitly followed by a marker
      instruction and an implicit retainRV/claimRV call that consumes the
      call result. In addition, it emits a call to
      @llvm.objc.clang.arc.noop.use, which consumes the call result, to
      prevent the middle-end passes from changing the return type of the
      called function. This is currently done only when the target is arm64
      and the optimization level is higher than -O0.
    
    - ARC optimizer temporarily emits retainRV/claimRV calls after the calls
      with the operand bundle in the IR and removes the inserted calls after
      processing the function.
    
    - ARC contract pass emits retainRV/claimRV calls after the call with the
      operand bundle. It doesn't remove the operand bundle on the call since
      the backend needs it to emit the marker instruction. The retainRV and
      claimRV calls are emitted late in the pipeline to prevent optimization
      passes from transforming the IR in a way that makes it harder for the
      ARC middle-end passes to figure out the def-use relationship between
      the call and the retainRV/claimRV calls (which is the cause of
      PR31925).
    
    - The function inliner removes an autoreleaseRV call in the callee if
      nothing in the callee prevents it from being paired up with the
      retainRV/claimRV call in the caller. It then inserts a release call if
      claimRV is attached to the call since autoreleaseRV+claimRV is
      equivalent to a release. If it cannot find an autoreleaseRV call, it
      tries to transfer the operand bundle to a function call in the callee.
      This is important since the ARC optimizer can remove the autoreleaseRV
      returning the callee result, which makes it impossible to pair it up
      with the retainRV/claimRV call in the caller. If that fails, it simply
      emits a retain call in the IR if retainRV is attached to the call and
      does nothing if claimRV is attached to it.
    
    - SCCP refrains from replacing the return value of a call with a
      constant value if the call has the operand bundle. This ensures the
      call always has at least one user (the call to
      @llvm.objc.clang.arc.noop.use).
    
    - This patch also fixes a bug in replaceUsesOfNonProtoConstant where
      multiple operand bundles of the same kind were being added to a call.
    
    Future work:
    
    - Use the operand bundle on x86-64.
    
    - Fix the auto upgrader to convert call+retainRV/claimRV pairs into
      calls with the operand bundles.
    
    rdar://71443534
    
    Differential Revision: https://reviews.llvm.org/D92808
    
    (cherry picked from commit ed4718eccb12bd42214ca4fb17d196d49561c0c7)
    
    Conflicts:
    	llvm/include/llvm/IR/LLVMContext.h
    	llvm/lib/CodeGen/SelectionDAG/SelectionDAGBuilder.cpp
    	llvm/lib/IR/LLVMContext.cpp
    	llvm/lib/IR/Verifier.cpp
    	llvm/lib/Target/AArch64/AArch64ISelLowering.cpp
    	llvm/lib/Transforms/Scalar/TailRecursionElimination.cpp

--- llvm/lib/CodeGen/SelectionDAG/SelectionDAGBuilder.cpp
+++ llvm/lib/CodeGen/SelectionDAG/SelectionDAGBuilder.cpp
@@ -2777,7 +2777,8 @@ void SelectionDAGBuilder::visitInvoke(const InvokeInst &I) {
   assert(!I.hasOperandBundlesOtherThan(
              {LLVMContext::OB_deopt, LLVMContext::OB_gc_transition,
               LLVMContext::OB_gc_live, LLVMContext::OB_funclet,
-              LLVMContext::OB_cfguardtarget, LLVMContext::OB_ptrauth}) &&
+              LLVMContext::OB_cfguardtarget, LLVMContext::OB_ptrauth,
+              LLVMContext::OB_clang_arc_attachedcall}) &&
          "Cannot lower invokes with arbitrary operand bundles yet!");
 
   const Value *Callee(I.getCalledOperand());
@@ -7856,7 +7857,8 @@ void SelectionDAGBuilder::visitCall(const CallInst &I) {
   // CFGuardTarget bundles are lowered in LowerCallTo.
   assert(!I.hasOperandBundlesOtherThan(
              {LLVMContext::OB_deopt, LLVMContext::OB_funclet,
-              LLVMContext::OB_cfguardtarget, LLVMContext::OB_preallocated}) &&
+              LLVMContext::OB_cfguardtarget, LLVMContext::OB_preallocated,
+              LLVMContext::OB_clang_arc_attachedcall}) &&
          "Cannot lower calls with arbitrary operand bundles!");
 
   SDValue Callee = getValue(I.getCalledOperand());
--- llvm/lib/IR/LLVMContext.cpp
+++ llvm/lib/IR/LLVMContext.cpp
@@ -84,6 +84,12 @@ LLVMContext::LLVMContext() : pImpl(new LLVMContextImpl(*this)) {
          "ptrauth operand bundle id drifted!");
   (void)PtrauthEntry;
 
+  auto *ClangAttachedCall =
+      pImpl->getOrInsertBundleTag("clang.arc.attachedcall");
+  assert(ClangAttachedCall->second == LLVMContext::OB_clang_arc_attachedcall &&
+         "clang.arc.attachedcall operand bundle id drifted!");
+  (void)ClangAttachedCall;
+
   SyncScope::ID SingleThreadSSID =
       pImpl->getOrInsertSyncScopeID("singlethread");
   assert(SingleThreadSSID == SyncScope::SingleThread &&
