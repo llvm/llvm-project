32445ff2dbc6d5894fa21e728a9485a30c29db29
V Donaldson <vdonaldson@nvidia.com>
2021-02-11 15:04:48 -0800

    Enable FIR generation for array element associate statement selectors

--- flang/lib/Lower/Bridge.cpp
+++ flang/lib/Lower/Bridge.cpp
@@ -1265,10 +1265,11 @@ private:
           auto &sym = *std::get<Fortran::parser::Name>(assoc.t).symbol;
           const auto &selector =
               *sym.get<Fortran::semantics::AssocEntityDetails>().expr();
-          if (Fortran::evaluate::IsVariable(selector) &&
+          if (Fortran::evaluate::IsVariable(selector) && selector.Rank() &&
               !Fortran::evaluate::UnwrapWholeSymbolDataRef(selector) &&
               !Fortran::evaluate::HasVectorSubscript(selector)) {
-            TODO("subobject association selector");
+            TODO("array section association selector");
+            continue;
           }
           genExprAddr(selector, stmtCtx)
               .match(
