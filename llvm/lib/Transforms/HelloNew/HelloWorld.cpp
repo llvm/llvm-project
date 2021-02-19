d6cd56113ba0206df70133ec4817729e9d7fa763
Jon Kartago Lamida <me@lamida.net>
2021-02-19 14:27:36 +0800

    WIP test pass

--- llvm/lib/Transforms/HelloNew/HelloWorld.cpp
+++ llvm/lib/Transforms/HelloNew/HelloWorld.cpp
@@ -12,6 +12,29 @@ using namespace llvm;
 
 PreservedAnalyses HelloWorldPass::run(Function &F,
                                       FunctionAnalysisManager &AM) {
-  errs() << F.getName() << "\n";
+  outs() << "Function: " << F.getName() << "\n";
+
+  for (auto &BB : F.getBasicBlockList()) {
+    outs() << "BB: " << BB.getName() << "\n";
+    outs() << "BB ValueID: " << BB.getValueID() << "\n";
+    outs() << "BB Parent: " << BB.getParent() << "\n";
+    outs() << "BB NextNode: " << BB.getNextNode() << "\n";
+    for (auto &I : BB) {
+      outs() << "I: " << I.getName() << "\n";
+      outs() << "I ValueID: " << I.getValueID() << "\n";
+      outs() << "I Parent: " << I.getParent() << "\n";
+      outs() << "I NextNode: " << I.getNextNode() << "\n";
+    }
+  }
+  outs() << "\n\n";
+
+  // https://www.cs.mcgill.ca/~zcao7/mutls/release/llvm-2.9/docs/ProgrammersManual.html#inspection
+  outs() << "Iterate basic block \n\n";
+
+  for (auto it = F.begin(), et = F.end(); it != et; ++it) {
+    errs() << "Basic block (name=" << it->getName() << ") has "
+             << it->size() << " instructions.\n";
+  }
+
   return PreservedAnalyses::all();
 }
