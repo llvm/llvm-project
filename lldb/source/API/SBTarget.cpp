39699644582e6d0248826dec6aea45a7bf3f2aff
Jonas Devlieghere <jonas@devlieghere.com>
2021-02-18 16:27:11 -0800

    [lldb] Add missing LLDB_REGISTER_METHOD to SBTarget
    (cherry picked from commit 7b072e1ffec0770529c01c8040f7c402300f09c4)

--- lldb/source/API/SBTarget.cpp
+++ lldb/source/API/SBTarget.cpp
@@ -2678,6 +2678,9 @@ void RegisterMethods<SBTarget>(Registry &R) {
   LLDB_REGISTER_METHOD(lldb::SBBreakpoint, SBTarget,
                        BreakpointCreateForException,
                        (lldb::LanguageType, bool, bool));
+  LLDB_REGISTER_METHOD(lldb::SBBreakpoint, SBTarget,
+                       BreakpointCreateForException,
+                       (lldb::LanguageType, bool, bool, SBStringList &));
   LLDB_REGISTER_METHOD(
       lldb::SBBreakpoint, SBTarget, BreakpointCreateFromScript,
       (const char *, lldb::SBStructuredData &, const lldb::SBFileSpecList &,
