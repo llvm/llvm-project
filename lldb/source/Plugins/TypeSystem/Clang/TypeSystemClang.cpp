0073e719bea50eda313b78db47eb032bd4b81ebb
Jonas Devlieghere <jonas@devlieghere.com>
2021-02-24 11:04:47 -0800

    [lldb] Support debugging utility functions
    LLDB uses utility functions to run code in the inferior for its own
    internal purposes, such as reading classes from the Objective-C runtime
    for example. Because these expressions should be transparent to the
    user, we ignore breakpoints and unwind the stack on errors, which
    makes them hard to debug.
    
    This patch adds a new setting target.debug-utility-expression that, when
    enabled, changes these options to facilitate debugging. It enables
    breakpoints, disables unwinding and writes out the utility function
    source code to disk so it shows up in the source view.
    
    Differential revision: https://reviews.llvm.org/D97249
    
    (cherry picked from commit 38dfb235db19caa1aaa58c1c8153a7464b932087)

--- lldb/source/Plugins/TypeSystem/Clang/TypeSystemClang.cpp
+++ lldb/source/Plugins/TypeSystem/Clang/TypeSystemClang.cpp
@@ -9649,7 +9649,8 @@ ScratchTypeSystemClang::CreateUtilityFunction(std::string text,
     return {};
 
   return std::make_unique<ClangUtilityFunction>(
-      *target_sp.get(), std::move(text), std::move(name));
+      *target_sp.get(), std::move(text), std::move(name),
+      target_sp->GetDebugUtilityExpression());
 }
 
 PersistentExpressionState *
