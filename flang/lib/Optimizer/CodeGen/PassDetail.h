dc6e49da444698eece83745e73edae358cb3ceb2
Eric Schweitz <eschweitz@nvidia.com>
2021-03-03 18:06:41 -0800

    Convert codegen pass to tablegen.
    Converts the first FIR pass written to use the newer tablegen support.

--- flang/lib/Optimizer/CodeGen/PassDetail.h
+++ flang/lib/Optimizer/CodeGen/PassDetail.h
@@ -10,6 +10,9 @@
 #define OPTMIZER_CODEGEN_PASSDETAIL_H
 
 #include "flang/Optimizer/Dialect/FIRDialect.h"
+#include "mlir/Dialect/LLVMIR/LLVMDialect.h"
+#include "mlir/Dialect/OpenMP/OpenMPDialect.h"
+#include "mlir/IR/BuiltinDialect.h"
 #include "mlir/Pass/Pass.h"
 #include "mlir/Pass/PassRegistry.h"
 
