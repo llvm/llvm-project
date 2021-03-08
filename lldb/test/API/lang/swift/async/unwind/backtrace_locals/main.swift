aac4b1bd17235ab34235c900e2a9bc930e9e8fac
Fred Riss <friss@apple.com>
2021-03-08 10:46:24 -0800

    [Swift async] Add a baseline test for arguments and locals in logical frames.
    (cherry picked from commit 38dce71e9b81ff6eca6c19dbe9633d1b8de17531)

---
+++ lldb/test/API/lang/swift/async/unwind/backtrace_locals/main.swift
@@ -0,0 +1,15 @@
+func fibonacci(_ n: Int) async -> Int {
+    if n == 0  || n == 1 { // function start
+        return n // end iteration
+    }
+    let n_1 = await fibonacci(n - 1)
+    let n_2 = await fibonacci(n - 2)
+    return n_1 + n_2
+}
+
+@main struct Main {
+  static func main() async {
+    let n = await fibonacci(10) // main breakpoint
+    print(n)  
+  }
+}
