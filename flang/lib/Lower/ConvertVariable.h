dde0499d894d287e1177809efe31e738628b2c2e
Jean Perier <jperier@nvidia.com>
2021-03-12 12:11:59 -0800

    Move variable instantiation code with no changes
    Move code in ConvertVariable.h/.cpp. Add a few namespace prefix
    in the interface function.

---
+++ flang/lib/Lower/ConvertVariable.h
@@ -0,0 +1,69 @@
+//===- Lower/ConvertVariable.h -- lowering of variables to FIR --*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// Coding style: https://mlir.llvm.org/getting_started/DeveloperGuide/
+//
+//===----------------------------------------------------------------------===//
+///
+/// Instantiation of pft::Variable in FIR/MLIR.
+///
+//===----------------------------------------------------------------------===//
+
+#ifndef FORTRAN_LOWER_CONVERT_VARIABLE_H
+#define FORTRAN_LOWER_CONVERT_VARIABLE_H
+
+#include "mlir/IR/Value.h"
+#include "llvm/ADT/DenseMap.h"
+#include <cstddef>
+#include <tuple>
+
+namespace fir {
+class GlobalOp;
+}
+namespace Fortran {
+namespace semantics {
+class Scope;
+}
+namespace lower {
+class AbstractConverter;
+class StatementContext;
+class SymMap;
+namespace pft {
+struct Variable;
+}
+
+/// AggregateStoreMap is used to keep track of instantiated aggregate stores
+/// when lowering a scope containing equivalences (aliases). It must only be
+/// owned by the code lowering a scope and provided to instantiateVariable.
+using AggregateStoreKey =
+    std::tuple<const Fortran::semantics::Scope *, std::size_t>;
+using AggregateStoreMap = llvm::DenseMap<AggregateStoreKey, mlir::Value>;
+
+/// Instantiate variable \p var and add it to \p symMap.
+/// The AbstractConverter builder must be set.
+/// The AbstractConverter own symbol mapping is not used during the
+/// instantiation and can be different form \p symMap.
+void instantiateVariable(AbstractConverter &, const pft::Variable &var,
+                         SymMap &symMap, AggregateStoreMap &storeMap);
+
+/// Create a fir::GlobalOp given a module variable definition. This is intended
+/// to be used when lowering a module definition, not when lowering variables
+/// used from a module. For used variables instantiateVariable must directly be
+/// called.
+void defineModuleVariable(AbstractConverter &, const pft::Variable &var);
+
+/// Lower a symbol attributes given an optional storage \p and add it to the
+/// provided symbol map. If \preAlloc is not provided, a temporary storage will
+/// be allocated. This is a low level function that should only be used if
+/// instantiateVariable cannot be called.
+void mapSymbolAttributes(AbstractConverter &, const pft::Variable &, SymMap &,
+                         StatementContext &, mlir::Value preAlloc = {});
+
+} // namespace lower
+} // namespace Fortran
+#endif // FORTRAN_LOWER_CONVERT_VARIABLE_H
