4d61fe51af22a1c8b6199da3ff332cf7561193ae
Romaric JODIN <rjodin@upmem.com>
2021-04-01 09:23:36 +0200

    Revert "dpu: llvm: do not defined NR_TASKLETS for crt0.c"
    This reverts commit 2e49f7ef798fe5891fe9a319208ad9521872d586.

--- clang/lib/Driver/ToolChains/DPURTE.cpp
+++ clang/lib/Driver/ToolChains/DPURTE.cpp
@@ -96,16 +96,14 @@ void DPURTE::addClangTargetOptions(
   }
 
   bool nr_tasklets_already_added = false;
-  bool b_crt0 = false;
   for (unsigned int EachArg = 0; EachArg < DriverArgs.size(); EachArg++) {
     std::string arg(DriverArgs.getArgString(EachArg));
     if (arg.find(NR_TASKLETS) != ULLONG_MAX) {
       nr_tasklets_already_added = true;
-    } else if (arg.find("UPMEM_CRT0") != ULLONG_MAX) {
-      b_crt0 = true;
+      break;
     }
   }
-  if (!nr_tasklets_already_added && !b_crt0) {
+  if (!nr_tasklets_already_added) {
     CC1Args.push_back("-D" NR_TASKLETS "=1");
   }
 }
