llvm/llvm-project

commit 521d889860eb2202399ae4be2e17e6bf8eb235a6

[TableGen] Improve handling of template arguments
This requires changes to TableGen files and some C++ files due to
incompatible multiclass template arguments that slipped through
before the improved handling.

llvm/test/TableGen/template-args.td
@@ -1,0 +1,142 @@
+// RUN: llvm-tblgen %s | FileCheck %s
+// RUN: not llvm-tblgen -DERROR1 %s 2>&1 | FileCheck --check-prefix=ERROR1 %s
+// RUN: not llvm-tblgen -DERROR2 %s 2>&1 | FileCheck --check-prefix=ERROR2 %s
+// RUN: not llvm-tblgen -DERROR3 %s 2>&1 | FileCheck --check-prefix=ERROR3 %s
+// RUN: not llvm-tblgen -DERROR4 %s 2>&1 | FileCheck --check-prefix=ERROR4 %s
+// RUN: not llvm-tblgen -DERROR5 %s 2>&1 | FileCheck --check-prefix=ERROR5 %s
+// RUN: not llvm-tblgen -DERROR6 %s 2>&1 | FileCheck --check-prefix=ERROR6 %s
+
+// This file tests that template arguments are type-checked and cast
+// if necessary.
+
+// Class template arguments.
+
+class Class1<string nm> {
+  string Name = nm;
+}
+
+// CHECK: def Rec1
+// CHECK:   string Name = "Alice"
+// CHECK:   string NameName = "AliceAlice"
+
+def Rec1 : Class1<"Alice"> {
+  string NameName = Name # Name;
+}
+
+#ifdef ERROR1
+// ERROR1: Value specified for template argument 'Class1:nm' (#0) is of type int
+
+def Rec2 : Class1<42> {
+}
+#endif
+
+class Class2<bits<8> cd> {
+  int Code = cd;
+}
+
+// CHECK: def Rec3
+// CHECK:   int Code = 42
+// CHECK:   list<int> CodeList = [42]
+
+def Rec3 : Class2<0b00101010> {
+  list<int> CodeList = [Code];
+}
+
+// CHECK: def Rec4
+// CHECK:   int Code = 42
+// CHECK:   list<int> CodeList = [42]
+
+def Rec4 : Class2<42> {
+  list<int> CodeList = [Code];
+}
+
+#ifdef ERROR2
+// ERROR2: Value specified for template argument 'Class2:cd' (#0) is of type string
+
+def Rec5 : Class2<"oops"> {
+  list<int> CodeList = [Code];
+}
+#endif
+
+// Anonymous class instantiation template arguments.
+
+// CHECK: def Rec6
+// CHECK:   string Name = "Ted"
+
+def Rec6 {
+  string Name = Class1<"Ted">.Name;
+}
+
+#ifdef ERROR3
+// ERROR3: Value specified for template argument 'Class1:nm' (#0) is of type int
+
+def Rec7 {
+  string Name = Class1<42>.Name;
+}
+#endif
+
+// CHECK: def Rec8
+// CHECK:   list<int> CodeList = [42]
+
+def Rec8 {
+  list<int> CodeList = [Class2<42>.Code];
+}
+
+#ifdef ERROR4
+// ERROR4: Value specified for template argument 'Class2:cd' (#0) is of type string
+
+def Rec9 {
+  list<int> CodeList = [Class2<"huh?">.Code];
+}
+#endif
+
+// Multiclass template arguments.
+
+multiclass MC1<string nm> {
+  def _1 {
+    string Name = nm;
+  }
+  def _2 {
+    string NameNmae = nm # nm;
+  }
+}
+
+// CHECK: def RecMC1_1
+// CHECK:   string Name = "Carol"
+// CHECK: def RecMC1_2
+// CHECK:   string NameNmae = "CarolCarol"
+
+defm RecMC1 : MC1<"Carol">;
+
+#ifdef ERROR5
+// ERROR5: Value specified for template argument 'MC1::nm' (#0) is of type int
+
+defm RecMC2 : MC1<42>;
+#endif
+
+multiclass MC2<bits<8> cd> {
+  def _1 {
+    bits<8> Code = cd;
+  }
+  def _2 {
+    int Code = cd;
+  }
+  def _3 {
+    list<int> CodeList = [cd];
+  }
+}
+
+// CHECK: def RecMC3_1
+// CHECK:   bits<8> Code = { 0, 0, 1, 0, 1, 0, 1, 0 }
+// CHECK: def RecMC3_2
+// CHECK:   int Code = 42
+// CHECK: def RecMC3_3
+// CHECK:   list<int> CodeList = [42]
+
+defm RecMC3 : MC2<42>;
+
+#ifdef ERROR6
+// ERROR6: Value specified for template argument 'MC2::cd' (#0) is of type string
+
+defm RecMC4 : MC2<"Bob">;
+#endif

clang/utils/TableGen/MveEmitter.cpp
@@ -1272,6 +1272,13 @@ Result::Ptr EmitterBase::getCodeForDagArg(DagInit *D, unsigned ArgNum,
     return it->second;
   }
 
+  // Sometimes the Arg is a bit. Prior to multiclass template argument
+  // checking, integers would sneak through the bit declaration,
+  // but now they really are bits.
+  if (auto *BI = dyn_cast<BitInit>(Arg))
+    return std::make_shared<IntLiteralResult>(getScalarType("u32"),
+                                              BI->getValue());
+
   if (auto *II = dyn_cast<IntInit>(Arg))
     return std::make_shared<IntLiteralResult>(getScalarType("u32"),
                                               II->getValue());
@@ -1287,7 +1294,11 @@ Result::Ptr EmitterBase::getCodeForDagArg(DagInit *D, unsigned ArgNum,
     }
   }
 
-  PrintFatalError("bad dag argument type for code generation");
+  PrintError("bad DAG argument type for code generation");
+  PrintNote("DAG: " + D->getAsString());
+  if (TypedInit *Typed = dyn_cast<TypedInit>(Arg))
+    PrintNote("argument type: " + Typed->getType()->getAsString());
+  PrintFatalNote("argument number " + Twine(ArgNum) + ": " + Arg->getAsString());
 }
 
 Result::Ptr EmitterBase::getCodeForArg(unsigned ArgNum, const Type *ArgType,

llvm/include/llvm/TableGen/Record.h
@@ -2024,6 +2024,12 @@ public:
 
   void set(Init *Key, Init *Value) { Map[Key] = {Value, false}; }
 
+  bool isComplete(Init *VarName) const {
+    auto It = Map.find(VarName);
+    assert(It != Map.end() && "key must be present in map");
+    return It->second.V->isComplete();
+  }
+
   Init *resolve(Init *VarName) override;
 };
 

llvm/lib/TableGen/Record.cpp
@@ -2344,13 +2344,13 @@ void Record::resolveReferences(Resolver &R, const RecordVal *SkipVal) {
         if (TypedInit *VRT = dyn_cast<TypedInit>(VR))
           Type =
               (Twine("of type '") + VRT->getType()->getAsString() + "' ").str();
-        PrintFatalError(getLoc(), Twine("Invalid value ") + Type +
-                                      "is found when setting '" +
-                                      Value.getNameInitAsString() +
-                                      "' of type '" +
-                                      Value.getType()->getAsString() +
-                                      "' after resolving references: " +
-                                      VR->getAsUnquotedString() + "\n");
+        PrintFatalError(
+            getLoc(),
+            Twine("Invalid value ") + Type + "found when setting field '" +
+                Value.getNameInitAsString() + "' of type '" +
+                Value.getType()->getAsString() +
+                "' after resolving references: " + VR->getAsUnquotedString() +
+                "\n");
       }
     }
   }

llvm/lib/TableGen/TGParser.h
@@ -243,8 +243,10 @@ private:  // Parser methods.
                          IDParseMode Mode = ParseValueMode);
   Init *ParseValue(Record *CurRec, RecTy *ItemType = nullptr,
                    IDParseMode Mode = ParseValueMode);
-  void ParseValueList(SmallVectorImpl<llvm::Init*> &Result, Record *CurRec,
-                      Record *ArgsRec = nullptr, RecTy *EltTy = nullptr);
+  void ParseValueList(SmallVectorImpl<llvm::Init*> &Result,
+                      Record *CurRec, RecTy *ItemType = nullptr);
+  bool ParseTemplateArgValueList(SmallVectorImpl<llvm::Init *> &Result,
+                                 Record *CurRec, Record *ArgsRec);
   void ParseDagArgList(
       SmallVectorImpl<std::pair<llvm::Init*, StringInit*>> &Result,
       Record *CurRec);
@@ -264,6 +266,8 @@ private:  // Parser methods.
   MultiClass *ParseMultiClassID();
   bool ApplyLetStack(Record *CurRec);
   bool ApplyLetStack(RecordsEntry &Entry);
+  bool CheckTemplateArgValues(SmallVectorImpl<llvm::Init *> &Values,
+                              SMLoc Loc, Record *ArgsRec);
   void CheckAssert(SMLoc Loc, Init *Condition, Init *Message);
   void CheckRecordAsserts(Record &Rec);
 };

llvm/test/TableGen/self-reference-typeerror.td
@@ -1,13 +1,14 @@
 // RUN: not llvm-tblgen %s 2>&1 | FileCheck %s
 // XFAIL: vg_leak
 
-class A<A x> {
-  A a = x;
+class Cl<Cl rec> {
+  Cl Arec = rec;
 }
 
 // At the time A0 is referenced, A has not yet been established as a superclass.
 // This kind of self-reference is discourage, but if you *really* want it, you
 // can force it with !cast.
 //
-// CHECK: Field 'A:x' of type 'A' is incompatible with value
-def A0 : A<A0>;
+// CHECK: alue specified for template argument 'Cl:rec'
+
+def Rec0 : Cl<Rec0>;

commit 73bc6b34595d4e96695937c0eadc62c88f18ddcd

[TableGen] Report an error message on a missing comma
I recently forgot a comma in a defm argument list and tablegen just
failed with exit code 1 without printing an error message. I believe
this issue was introduced in 521d889860eb2202399ae4be2e17e6bf8eb235a6.

This change prints the following instead:
.../clang/include/clang/Driver/Options.td:569:3: error: Expected comma before next argument

Reviewed By: Paul-C-Anagnostopoulos

Differential Revision: https://reviews.llvm.org/D100178

llvm/lib/TableGen/TGParser.cpp
@@ -2539,9 +2539,9 @@ bool TGParser::ParseTemplateArgValueList(SmallVectorImpl<Init *> &Result,
 
     if (consume(tgtok::greater)) // end of argument list?
       return false;
-    if (!consume(tgtok::comma)) // must be comma
-      return true;
-    ++ArgIndex; 
+    if (!consume(tgtok::comma))
+      return TokError("Expected comma before next argument");
+    ++ArgIndex;
   }
 }
 

llvm/test/TableGen/template-args.td
@@ -5,6 +5,7 @@
 // RUN: not llvm-tblgen -DERROR4 %s 2>&1 | FileCheck --check-prefix=ERROR4 %s
 // RUN: not llvm-tblgen -DERROR5 %s 2>&1 | FileCheck --check-prefix=ERROR5 %s
 // RUN: not llvm-tblgen -DERROR6 %s 2>&1 | FileCheck --check-prefix=ERROR6 %s
+// RUN: not llvm-tblgen -DERROR7 %s 2>&1 | FileCheck --check-prefix=ERROR7 %s
 
 // This file tests that template arguments are type-checked and cast
 // if necessary.
@@ -139,4 +140,14 @@ defm RecMC3 : MC2<42>;
 // ERROR6: Value specified for template argument 'MC2::cd' (#0) is of type string
 
 defm RecMC4 : MC2<"Bob">;
+#endif
+
+#ifdef ERROR7
+multiclass TwoArgs<bits<8> a, string b> {
+  def _1 { bits<8> A = a; }
+  def _2 { string B = b; }
+}
+defm Good : TwoArgs<1, "one">;
+defm MissingComma : TwoArgs<2 "two">;
+// ERROR7: [[#@LINE-1]]:31: error: Expected comma before next argument
 #endif