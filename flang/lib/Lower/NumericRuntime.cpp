17f50740737740be23b2eb032ee2e48dd3ac86a4
Mark Leair <leairmark@gmail.com>
2021-04-14 11:23:52 -0700

    Remove unneeded discoverRealKind() function.

--- flang/lib/Lower/NumericRuntime.cpp
+++ flang/lib/Lower/NumericRuntime.cpp
@@ -18,51 +18,30 @@
 
 using namespace Fortran::runtime;
 
-/// Helper function to recover the KIND from the REAL FIR type.
-static int discoverRealKind(mlir::Type fltTy) {
-  if (fltTy.isF16())
-    return 2;
-  if (fltTy.isBF16())
-    return 3;
-  if (fltTy.isF32())
-    return 4;
-  if (fltTy.isF64())
-    return 8;
-  if (fltTy.isF80())
-    return 10;
-  if (fltTy.isF128())
-    return 16;
-  llvm_unreachable("unexpected type");
-}
-
 /// Generate call to RRSpacing intrinsic runtime routine. 
 mlir::Value
 Fortran::lower::genRRSpacing(Fortran::lower::FirOpBuilder &builder, 
                            mlir::Location loc, mlir::Value x) { 
   mlir::FuncOp func;
-  switch (discoverRealKind(x.getType())) {
-  case 4:
-    func = Fortran::lower::getRuntimeFunc<mkRTKey(RRSpacing4)>(loc, builder);
-    break;
-  case 8:
-    func = Fortran::lower::getRuntimeFunc<mkRTKey(RRSpacing8)>(loc, builder);
-    break;
-#if LONG_DOUBLE == 80
-  case 10:
-    func = Fortran::lower::getRuntimeFunc<mkRTKey(RRSpacing10)>(loc, builder);
-    break;
-#elif LONG_DOUBLE == 128
-  case 16:
-    func = Fortran::lower::getRuntimeFunc<mkRTKey(RRSpacing16)>(loc, builder);
-    break;
-#endif
-  default:
-    TODO(loc, "unsupported real kind in RRSpacing lowering");
-  }
+  mlir::Type fltTy = x.getType();
 
-  auto fTy = func.getType();
+  if (fltTy.isF32())
+    func = Fortran::lower::getRuntimeFunc<mkRTKey(RRSpacing4)>(loc, builder);
+  else if (fltTy.isF64())
+    func = Fortran::lower::getRuntimeFunc<mkRTKey(RRSpacing8)>(loc, builder);
+#if LONG_DOUBLE == 80
+  else if (fltTy.isF80())
+    func = Fortran::lower::getRuntimeFunc<mkRTKey(RRSpacing10)>(loc, builder);
+#elif LONG_DOUBLE == 128
+  else if (fltTy.isF128())
+    func = Fortran::lower::getRuntimeFunc<mkRTKey(RRSpacing16)>(loc, builder);
+#endif
+  else
+    TODO(loc, "unsupported real kind in RRSpacing lowering");
+
+  auto funcTy = func.getType();
   llvm::SmallVector<mlir::Value> args = {
-    builder.createConvert(loc, fTy.getInput(0), x)
+    builder.createConvert(loc, funcTy.getInput(0), x)
   };
 
   return builder.create<fir::CallOp>(loc, func, args).getResult(0);
@@ -73,29 +52,25 @@ mlir::Value
 Fortran::lower::genSpacing(Fortran::lower::FirOpBuilder &builder, 
                            mlir::Location loc, mlir::Value x) { 
   mlir::FuncOp func;
-  switch (discoverRealKind(x.getType())) {
-  case 4:
-    func = Fortran::lower::getRuntimeFunc<mkRTKey(Spacing4)>(loc, builder);
-    break;
-  case 8:
-    func = Fortran::lower::getRuntimeFunc<mkRTKey(Spacing8)>(loc, builder);
-    break;
-#if LONG_DOUBLE == 80
-  case 10:
-    func = Fortran::lower::getRuntimeFunc<mkRTKey(Spacing10)>(loc, builder);
-    break;
-#elif LONG_DOUBLE == 128
-  case 16:
-    func = Fortran::lower::getRuntimeFunc<mkRTKey(Spacing16)>(loc, builder);
-    break;
-#endif
-  default:
-    TODO(loc, "unsupported real kind in Spacing lowering");
-  }
+  mlir::Type fltTy = x.getType();
 
-  auto fTy = func.getType();
+  if (fltTy.isF32())
+    func = Fortran::lower::getRuntimeFunc<mkRTKey(Spacing4)>(loc, builder);
+  else if (fltTy.isF64())
+    func = Fortran::lower::getRuntimeFunc<mkRTKey(Spacing8)>(loc, builder);
+#if LONG_DOUBLE == 80
+  else if (fltTy.isF80())
+    func = Fortran::lower::getRuntimeFunc<mkRTKey(Spacing10)>(loc, builder);
+#elif LONG_DOUBLE == 128
+  else if (fltTy.isF128())
+    func = Fortran::lower::getRuntimeFunc<mkRTKey(Spacing16)>(loc, builder);
+#endif
+  else
+    TODO(loc, "unsupported real kind in Spacing lowering");
+
+  auto funcTy = func.getType();
   llvm::SmallVector<mlir::Value> args = {
-    builder.createConvert(loc, fTy.getInput(0), x)
+    builder.createConvert(loc, funcTy.getInput(0), x)
   };
 
   return builder.create<fir::CallOp>(loc, func, args).getResult(0);
