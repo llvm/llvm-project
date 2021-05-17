b535008d5a45aed475016b8adad944ab9b969fda
Stefan Pejic <stefan.pejic@syrmia.com>
2021-05-17 16:46:07 +0200

    Add MOD instruction

--- llvm/lib/Target/Mips/MipsISelLowering.cpp
+++ llvm/lib/Target/Mips/MipsISelLowering.cpp
@@ -388,9 +388,10 @@ MipsTargetLowering::MipsTargetLowering(const MipsTargetMachine &TM,
   if (Subtarget.isGP64bit())
     setOperationAction(ISD::EH_DWARF_CFA,       MVT::i64,   Custom);
 
-  if (!Subtarget.hasNanoMips())
+  if (!Subtarget.hasNanoMips()) {
     setOperationAction(ISD::SDIV, MVT::i32, Expand);
-  setOperationAction(ISD::SREM, MVT::i32, Expand);
+    setOperationAction(ISD::SREM, MVT::i32, Expand);
+  }
   setOperationAction(ISD::UDIV, MVT::i32, Expand);
   setOperationAction(ISD::UREM, MVT::i32, Expand);
   setOperationAction(ISD::SDIV, MVT::i64, Expand);
@@ -1425,6 +1426,7 @@ MipsTargetLowering::EmitInstrWithCustomInserter(MachineInstr &MI,
     return insertDivByZeroTrap(MI, *BB, *Subtarget.getInstrInfo(), true, false,
                                false);
   case Mips::DIV_NM:
+  case Mips::MOD_NM:
     return insertDivByZeroTrap(MI, *BB, *Subtarget.getInstrInfo(), false, false,
                                true);
 
--- llvm/test/CodeGen/Mips/nanomips/div.ll
+++ llvm/test/CodeGen/Mips/nanomips/div.ll
@@ -1,10 +1,19 @@
 ; RUN: llc -mtriple=nanomips -asm-show-inst -verify-machineinstrs < %s | FileCheck %s
 
-define i32 @test_sdiv(i32 %a, i32 %b) {
+define i32 @test_div(i32 %a, i32 %b) {
 ; CHECK: div $a0, $a0, $a1
 ; CHECK: DIV_NM
 ; CHECK: teq $zero, $a1, 7
 ; CHECK: TEQ_NM
   %div = sdiv i32 %a, %b
   ret i32 %div
+}
+
+define i32 @test_mod(i32 %a, i32 %b) {
+; CHECK: mod $a0, $a0, $a1
+; CHECK: MOD_NM
+; CHECK: teq $zero, $a1, 7
+; CHECK: TEQ_NM
+  %mod = srem i32 %a, %b
+  ret i32 %mod
 }
