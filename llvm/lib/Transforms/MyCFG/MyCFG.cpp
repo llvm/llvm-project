6813ee50866b330695b8dd92f08841f3b1eaeb83
Jon Kartago Lamida <me@lamida.net>
2021-05-21 15:31:33 +0800

    Use cfg printer to show the checkpoint

--- llvm/lib/Transforms/MyCFG/MyCFG.cpp
+++ llvm/lib/Transforms/MyCFG/MyCFG.cpp
@@ -7,14 +7,15 @@
 //===----------------------------------------------------------------------===//
 
 #include "llvm/Transforms/MyCFG/MyCFG.h"
+#include "llvm/ADT/BreadthFirstIterator.h"
+#include "llvm/ADT/DepthFirstIterator.h"
 #include "llvm/ADT/PostOrderIterator.h"
 #include "llvm/ADT/SCCIterator.h"
-#include "llvm/ADT/BreadthFirstIterator.h"
-#include "llvm/IR/InstIterator.h"
-#include "llvm/ADT/DepthFirstIterator.h"
 #include "llvm/Analysis/BranchProbabilityInfo.h"
 #include "llvm/Analysis/CFGPrinter.h"
 #include "llvm/Analysis/HeatUtils.h"
+#include "llvm/IR/InstIterator.h"
+#include <sstream>
 
 using namespace llvm;
 
@@ -25,6 +26,7 @@ void traverse(Function &F) {
            IE = df_end(&F.getEntryBlock());
        iterator != IE; ++iterator) {
     outs() << *iterator << "\n";
+    outs() << iterator->getName() << "\n";
     for (auto &instruction : **iterator) {
       outs() << instruction << "\n";
     }
@@ -37,6 +39,7 @@ void traverse(Function &F) {
            IE = idf_end(&F.getEntryBlock());
        iterator != IE; ++iterator) {
     outs() << *iterator << "\n";
+    outs() << iterator->getName() << "\n";
     for (auto &instruction : **iterator) {
       outs() << instruction << "\n";
     }
@@ -49,6 +52,7 @@ void traverse(Function &F) {
            IE = bf_end(&F.getEntryBlock());
        iterator != IE; ++iterator) {
     outs() << *iterator << "\n";
+    outs() << iterator->getName() << "\n";
     for (auto &instruction : **iterator) {
       outs() << instruction << "\n";
     }
@@ -61,6 +65,7 @@ void traverse(Function &F) {
            IE = po_end(&F.getEntryBlock());
        iterator != IE; ++iterator) {
     outs() << *iterator << "\n";
+    outs() << iterator->getName() << "\n";
     for (auto &instruction : **iterator) {
       outs() << instruction << "\n";
     }
@@ -84,6 +89,7 @@ void traverse(Function &F) {
            IE = succ_end(&F.getEntryBlock());
        iterator != IE; ++iterator) {
     outs() << *iterator << "\n";
+    outs() << iterator->getName() << "\n";
     for (auto &instruction : **iterator) {
       outs() << instruction << "\n";
     }
@@ -99,13 +105,15 @@ void traverseBasicBlock(Function &F, int nestedLevel) {
   bool isThreadStartCheckpoint = true;
   for (auto &bb : F) {
     outs() << prefix << "Basic Block '" << &bb << "' Instructions: '" << bb << "'\n";
+
     bool isThreadEndCheckpoint = false;
     bool isExitPointCheckpoint = false;
     for (auto &i : bb) {
       // if instruction is last in the block and has no more successor,
       // then this will be thread end checkpoint
       if (i.isTerminator()) {
-        if (i.getNumSuccessors() == 0) {
+        // Thread end only in the original function (main)
+        if (i.getNumSuccessors() == 0 && nestedLevel == 0) {
           isThreadEndCheckpoint |= true;
         }
       }
@@ -113,26 +121,38 @@ void traverseBasicBlock(Function &F, int nestedLevel) {
       if (isa<CallInst>(i)) {
         auto *call = &cast<CallBase>(i);
         // use this hack to check if function is external
-        if (!call->getCalledFunction()->empty()) {
+        if (call != nullptr && call->getCalledFunction() != nullptr && !call->getCalledFunction()->empty()) {
           outs() << prefix << "Traversing nestedLevel function " << call->getCalledFunction()->getName() << " Instruction '" << i << "'\n";
           traverseBasicBlock(*call->getCalledFunction(), nestedLevel + 1);
           outs() << prefix << "Finished traversing nestedLevel function " << call->getCalledFunction()->getName() << "\n";
         } else {
           // The function is outside of the translation unit, hence it is an exit point
-          isExitPointCheckpoint |= true;
+          if (!isThreadStartCheckpoint && !isThreadEndCheckpoint) {
+            isExitPointCheckpoint |= true;
+          }
         }
       }
     }
+
+    // Get Identifier from the BB address
+    std::ostringstream st;
+    st << &bb;
+
+    std::string name = st.str();
     if (isThreadStartCheckpoint && !nestedLevel) {
-      outs() << prefix << "This basic block is thread start checkpoint\n";
+      outs() << prefix << "This basic block:'" << name << "' is thread start checkpoint\n";
+      name.append(" - Thread Start");
       isThreadStartCheckpoint = false;
     }
     if (isThreadEndCheckpoint && !nestedLevel) {
-      outs() << prefix << "This basic block is thread end checkpoint\n";
+      name.append(" - Thread End");
+      outs() << prefix << "This basic block:'" << name << "' is thread end checkpoint\n";
     }
     if (isExitPointCheckpoint) {
-      outs() << prefix << "This basic block is an exit-point checkpoint\n";
+      name.append(" - Exit Point");
+      outs() << prefix << "This basic block:'" << name << "' is exit-point checkpoint\n";
     }
+    bb.setName(name);
     if (!nestedLevel) {
       outs() << "\n\n";
     }
@@ -142,8 +162,11 @@ void traverseBasicBlock(Function &F, int nestedLevel) {
 PreservedAnalyses MyCFGPass::run(Function &F, FunctionAnalysisManager &AM) {
   if (F.getName() == "main") {
     outs() << "==================================================\n";
+    outs() << "Function '" << F.getName() << "'\n";
+    traverseBasicBlock(F, 0);
+    //  traverse(F);
+    CFGPrinterPass cfg;
+    cfg.run(F, AM);
   }
-  outs() << "Function '" << F.getName() << "'\n";
-  traverseBasicBlock(F, 0);
   return PreservedAnalyses::all();
 }
