b48c0c33101f64f36b8920bb69964242c110837b
Amir Ayupov <aaupov@fb.com>
2021-06-01 11:37:41 -0700

    Rebase: [Facebook] Add clang driver options to test debug info and BOLT
    Summary:
    This is an essential piece of infrastructure for us to be
    continuously testing debug info with BOLT. We can't only make changes
    to a test repo because we need to change debuginfo tests to call BOLT,
    hence, this diff needs to sit in our opensource repo. But when upstreaming
    to LLVM, this should be kept BOLT-only outside of LLVM. When upstreaming,
    we need to git diff and check all folders that are being modified by our
    commits and discard this one (and leave as an internal diff).
    
    To test BOLT in debuginfo tests, configure it with -DLLVM_TEST_BOLT=ON.
    Then run check-lldb and check-debuginfo.
    
    Manual rebase conflict history:
    https://phabricator.intern.facebook.com/D29205224
    https://phabricator.intern.facebook.com/D29564078
    https://phabricator.intern.facebook.com/D33289118
    https://phabricator.intern.facebook.com/D34957174
    
    Test Plan:
    tested locally
    Configured with:
    -DLLVM_ENABLE_PROJECTS="clang;lld;lldb;compiler-rt;bolt;debuginfo-tests"
    -DLLVM_TEST_BOLT=ON
    Ran test suite with:
    ninja check-debuginfo
    ninja check-lldb
    
    Reviewers: #llvm-bolt
    
    Subscribers: ayermolo, phabricatorlinter
    
    Differential Revision: https://phabricator.intern.facebook.com/D35317341
    
    Tasks: T92898286

--- clang/lib/Driver/ToolChains/Gnu.cpp
+++ clang/lib/Driver/ToolChains/Gnu.cpp
@@ -684,12 +684,41 @@ void tools::gnutools::Linker::ConstructJob(Compilation &C, const JobAction &JA,
     }
   }
 
+  // Facebook T92898286
+  if (Args.hasArg(options::OPT_post_link_optimize))
+    CmdArgs.push_back("-q");
+  // End Facebook T92898286
+
   Args.AddAllArgs(CmdArgs, options::OPT_T);
 
   const char *Exec = Args.MakeArgString(ToolChain.GetLinkerPath());
   C.addCommand(std::make_unique<Command>(JA, *this,
                                          ResponseFileSupport::AtFileCurCP(),
                                          Exec, CmdArgs, Inputs, Output));
+  // Facebook T92898286
+  if (!Args.hasArg(options::OPT_post_link_optimize) || !Output.isFilename())
+    return;
+
+  const char *MvExec = Args.MakeArgString(ToolChain.GetProgramPath("mv"));
+  ArgStringList MoveCmdArgs;
+  MoveCmdArgs.push_back(Output.getFilename());
+  const char *PreBoltBin =
+      Args.MakeArgString(Twine(Output.getFilename()) + ".pre-bolt");
+  MoveCmdArgs.push_back(PreBoltBin);
+  C.addCommand(std::make_unique<Command>(JA, *this, ResponseFileSupport::None(),
+                                         MvExec, MoveCmdArgs, None));
+
+  ArgStringList BoltCmdArgs;
+  const char *BoltExec =
+      Args.MakeArgString(ToolChain.GetProgramPath("llvm-bolt"));
+  BoltCmdArgs.push_back(PreBoltBin);
+  BoltCmdArgs.push_back("-reorder-blocks=reverse");
+  BoltCmdArgs.push_back("-update-debug-sections");
+  BoltCmdArgs.push_back("-o");
+  BoltCmdArgs.push_back(Output.getFilename());
+  C.addCommand(std::make_unique<Command>(JA, *this, ResponseFileSupport::None(),
+                                         BoltExec, BoltCmdArgs, None));
+  // End Facebook T92898286
 }
 
 void tools::gnutools::Assembler::ConstructJob(Compilation &C,
