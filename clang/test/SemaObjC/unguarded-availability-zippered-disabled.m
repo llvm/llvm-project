a36bbcfbe6cedf8a11e858a5b2f4de79789df4e5
Alex Lorenz <arphaman@gmail.com>
2021-07-23 08:16:32 -0700

    [clang] Fix internal tests after merging Mac Catalyst upstream support

--- clang/test/SemaObjC/unguarded-availability-zippered-disabled.m
+++ clang/test/SemaObjC/unguarded-availability-zippered-disabled.m
@@ -1,12 +1,12 @@
-// RUN: %clang_cc1 -triple x86_64-apple-macos10.14 -darwin-target-variant-triple x86_64-apple-ios13.1-macabi -fblocks -fsyntax-only -verify %s
+// RUN: %clang_cc1 -triple x86_64-apple-macos10.15 -darwin-target-variant-triple x86_64-apple-ios13.1-macabi -fblocks -fsyntax-only -Wno-ignored-availability-without-sdk-settings -verify %s
 
-#define AVAILABLE_CUR_MAC __attribute__((availability(macos, introduced = 10.14)))
+#define AVAILABLE_CUR_MAC __attribute__((availability(macos, introduced = 10.15)))
 #define AVAILABLE_NEXT_IOS __attribute__((availability(ios, introduced = 14)))
 
-void uikitformacIntroducedLater() AVAILABLE_CUR_MAC AVAILABLE_NEXT_IOS;
-// expected-warning@-1 {{macOS availability is ignored without a valid 'SDKSettings.json' in the SDK}}
+void macCatalystIntroducedLater() AVAILABLE_CUR_MAC AVAILABLE_NEXT_IOS;
 
 void test() {
-  uikitformacIntroducedLater();
+  macCatalystIntroducedLater();
 }
 
+// expected-no-diagnostics
