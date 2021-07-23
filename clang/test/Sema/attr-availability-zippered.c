1ad993c3c29d44709c5b0a1d03062797ec732d5b
Alex Lorenz <arphaman@gmail.com>
2021-07-23 08:16:32 -0700

    [clang] Fix internal tests after merging Mac Catalyst upstream support

--- clang/test/Sema/attr-availability-zippered.c
+++ clang/test/Sema/attr-availability-zippered.c
@@ -1,20 +1,19 @@
-// RUN: %clang_cc1 -ftarget-variant-availability-checks "-triple" "x86_64-apple-macos10.14" -darwin-target-variant-triple x86_64-apple-ios12-macabi -fsyntax-only -verify %s
-// RUN: %clang_cc1 -ftarget-variant-availability-checks "-triple" x86_64-apple-ios12-macabi -darwin-target-variant-triple "x86_64-apple-macos10.14"  -fsyntax-only -verify -D INVERTED %s
+// RUN: %clang_cc1 "-triple" "x86_64-apple-macos10.15" -darwin-target-variant-triple x86_64-apple-ios13.1-macabi -fsyntax-only -ftarget-variant-availability-checks -verify %s
+// RUN: %clang_cc1 "-triple" x86_64-apple-ios13.1-macabi -darwin-target-variant-triple "x86_64-apple-macos10.15"  -fsyntax-only -ftarget-variant-availability-checks -verify -D INVERTED %s
 
-// XFAIL: *
-__attribute__((availability(macos, introduced=10.10, deprecated=10.14), availability(ios, introduced=9, deprecated=12)))
+__attribute__((availability(macos, introduced=10.10, deprecated=10.15), availability(ios, introduced=9, deprecated=12))) // expected-warning {{macOS availability is ignored without a valid 'SDKSettings.json' in the SDK}}
 void bothDeprecated() { // expected-note {{'bothDeprecated' has been explicitly marked deprecated here}}
 }
 
-__attribute__((availability(macos, introduced=10.10, deprecated=10.14), availability(ios, introduced=9, deprecated=13)))
+__attribute__((availability(macos, introduced=10.10, deprecated=10.15), availability(ios, introduced=9, deprecated=14)))
 void macOSDeprecated() { // expected-note {{'macOSDeprecated' has been explicitly marked deprecated here}}
 }
 
-__attribute__((availability(macos, introduced=10.10, deprecated=10.15), availability(ios, introduced=9, deprecated=12)))
+__attribute__((availability(macos, introduced=10.10, deprecated=11), availability(ios, introduced=9, deprecated=12)))
 void iOSDeprecated() { // expected-note {{'iOSDeprecated' has been explicitly marked deprecated here}}
 }
 
-__attribute__((availability(macos, introduced=10.10, deprecated=10.15), availability(ios, introduced=9, deprecated=13)))
+__attribute__((availability(macos, introduced=10.10, deprecated=11), availability(ios, introduced=13.0, deprecated=14.0)))
 void bothNotDeprecated() {
 }
 
@@ -22,12 +21,12 @@ void checkDeprecated() {
   // The compiler may warn here if the deployment target is older than the version introduced
   bothDeprecated();
 #ifndef INVERTED
-  // expected-warning@-2 {{'bothDeprecated' is deprecated: first deprecated in macOS 10.14 and first deprecated in macCatalyst 12}}
+  // expected-warning@-2 {{'bothDeprecated' is deprecated: first deprecated in macOS 10.15 and first deprecated in macCatalyst 13.1}}
 #else
-  // expected-warning@-4 {{'bothDeprecated' is deprecated: first deprecated in macCatalyst 12 and first deprecated in macOS 10.14}}
+  // expected-warning@-4 {{'bothDeprecated' is deprecated: first deprecated in macCatalyst 13.1 and first deprecated in macOS 10.15}}
 #endif
-  macOSDeprecated(); // expected-warning {{'macOSDeprecated' is deprecated: first deprecated in macOS 10.14}}
-  iOSDeprecated();   // expected-warning {{'iOSDeprecated' is deprecated: first deprecated in macCatalyst 12}}
+  macOSDeprecated(); // expected-warning {{'macOSDeprecated' is deprecated: first deprecated in macOS 10.15}}
+  iOSDeprecated();   // expected-warning {{'iOSDeprecated' is deprecated: first deprecated in macCatalyst 13.1}}
   bothNotDeprecated();
 }
 
@@ -46,7 +45,7 @@ void macOSUnavailable() { // expected-note {{'macOSUnavailable' has been explici
 __attribute__((availability(macos, introduced=10.10), availability(ios, introduced=11)))
 void bothAvailable() { }
 
-__attribute__((availability(macos, introduced=10.10, obsoleted=10.14), availability(ios, introduced=9, obsoleted=12)))
+__attribute__((availability(macos, introduced=10.10, obsoleted=10.15), availability(ios, introduced=9, obsoleted=12)))
 void bothObsoleted() { // expected-note {{'bothObsoleted' has been explicitly marked unavailable here}}
 }
 
@@ -63,15 +62,15 @@ void checkUnavailable() {
   
   bothObsoleted();
 #ifndef INVERTED
-  // expected-error@-2 {{'bothObsoleted' is unavailable: obsoleted in macOS 10.14 and obsoleted in macCatalyst 12}}
+  // expected-error@-2 {{'bothObsoleted' is unavailable: obsoleted in macOS 10.15 and obsoleted in macCatalyst 13.1}}
 #else
-  // expected-error@-4 {{'bothObsoleted' is unavailable: obsoleted in macCatalyst 12 and obsoleted in macOS 10.14}}
+  // expected-error@-4 {{'bothObsoleted' is unavailable: obsoleted in macCatalyst 13.1 and obsoleted in macOS 10.15}}
 #endif
 }
 
-__attribute__((availability(macos, introduced=10.10, deprecated=10.14)))
+__attribute__((availability(macos, introduced=10.10, deprecated=10.15)))
 typedef struct ZipperedTypedefDifferentOffendingDecl { // expected-note {{'ZipperedTypedefDifferentOffendingDecl' has been explicitly marked deprecated here}}
-// expected-warning@-1 {{'ZipperedTypedefDifferentOffendingDecl' is deprecated: first deprecated in macCatalyst 12}}
+// expected-warning@-1 {{'ZipperedTypedefDifferentOffendingDecl' is deprecated: first deprecated in macCatalyst 13}}
 // expected-note@-2 {{'ZipperedTypedefDifferentOffendingDecl' has been explicitly marked deprecated here}}
   int x;
 } __attribute__((availability(ios, introduced=9, deprecated=12))) ZipperedTypedefDifferentOffendingDecl_t; // expected-note {{'ZipperedTypedefDifferentOffendingDecl_t' has been explicitly marked deprecated here}}
@@ -79,29 +78,29 @@ typedef struct ZipperedTypedefDifferentOffendingDecl { // expected-note {{'Zippe
 void checkZipperDiffOffendingDecl() {
   ZipperedTypedefDifferentOffendingDecl_t tt;
 #ifndef INVERTED
-  // expected-warning@-2 {{'ZipperedTypedefDifferentOffendingDecl_t' is deprecated: first deprecated in macOS 10.14 and first deprecated in macCatalyst 12}}
+  // expected-warning@-2 {{'ZipperedTypedefDifferentOffendingDecl_t' is deprecated: first deprecated in macOS 10.15 and first deprecated in macCatalyst 13.1}}
 #else
-  // expected-warning@-4 {{'ZipperedTypedefDifferentOffendingDecl_t' is deprecated: first deprecated in macCatalyst 12 and first deprecated in macOS 10.14}}
+  // expected-warning@-4 {{'ZipperedTypedefDifferentOffendingDecl_t' is deprecated: first deprecated in macCatalyst 13.1 and first deprecated in macOS 10.15}}
 #endif
 }
 
-__attribute__((availability(macos, introduced=10.10, deprecated=10.14), availability(ios, unavailable)))
+__attribute__((availability(macos, introduced=10.10, deprecated=10.15), availability(ios, unavailable)))
 void mixUnavailableAndDeprecated() {
   // expected-note@-1 {{'mixUnavailableAndDeprecated' has been explicitly marked unavailable here}}
   // expected-note@-2 {{'mixUnavailableAndDeprecated' has been explicitly marked deprecated here}}
 }
 
-__attribute__((availability(macos, introduced=10.10, deprecated=10.15), availability(ios, unavailable)))
+__attribute__((availability(macos, introduced=10.10, deprecated=11), availability(ios, unavailable)))
 void iOSUnavailableMacGood() {
   // expected-note@-1 {{'iOSUnavailableMacGood' has been explicitly marked unavailable here}}
 }
 
-__attribute__((availability(macos, introduced=10.10, deprecated=10.14), availability(ios, introduced=11)))
+__attribute__((availability(macos, introduced=10.10, deprecated=10.15), availability(ios, introduced=11)))
 void macOSDeprecatedIOSGood() {
   // expected-note@-1 {{'macOSDeprecatedIOSGood' has been explicitly marked deprecated here}}
 }
 
-__attribute__((availability(macos, introduced=10.10, deprecated=10.14, replacement="foo"), availability(ios, introduced=9, deprecated=12, replacement="bar")))
+__attribute__((availability(macos, introduced=10.10, deprecated=10.15, replacement="foo"), availability(ios, introduced=9, deprecated=12, replacement="bar")))
 void bothDeprecatedDiffReplacement() { // expected-note {{'bothDeprecatedDiffReplacement' has been explicitly marked deprecated here}}
 }
 
@@ -113,26 +112,26 @@ __attribute__((availability(ios, unavailable)))
 void justIosUnavailable() { // expected-note {{'justIosUnavailable' has been explicitly marked unavailable here}}
 }
 
-__attribute__((availability(macos, introduced=10.10, deprecated=10.14)))
+__attribute__((availability(macos, introduced=10.10, deprecated=10.15)))
 void justMacOSDeprecated() { // expected-note {{'justMacOSDeprecated' has been explicitly marked deprecated here}}
 }
 
 void checkMix() {
   mixUnavailableAndDeprecated();
   // expected-error@-1 {{'mixUnavailableAndDeprecated' is unavailable: not available on macCatalyst}}
-  // expected-warning@-2 {{'mixUnavailableAndDeprecated' is deprecated: first deprecated in macOS 10.14}}
+  // expected-warning@-2 {{'mixUnavailableAndDeprecated' is deprecated: first deprecated in macOS 10.15}}
   
   iOSUnavailableMacGood();
   // expected-error@-1 {{'iOSUnavailableMacGood' is unavailable: not available on macCatalyst}}
   
   macOSDeprecatedIOSGood();
-  // expected-warning@-1 {{'macOSDeprecatedIOSGood' is deprecated: first deprecated in macOS 10.14}}
+  // expected-warning@-1 {{'macOSDeprecatedIOSGood' is deprecated: first deprecated in macOS 10.15}}
 
   bothDeprecatedDiffReplacement();
 #ifndef INVERTED
-  // expected-warning@-2 {{'bothDeprecatedDiffReplacement' is deprecated: first deprecated in macOS 10.14 and first deprecated in macCatalyst 12}}
+  // expected-warning@-2 {{'bothDeprecatedDiffReplacement' is deprecated: first deprecated in macOS 10.15 and first deprecated in macCatalyst 13.1}}
 #else
-  // expected-warning@-4 {{'bothDeprecatedDiffReplacement' is deprecated: first deprecated in macCatalyst 12 and first deprecated in macOS 10.14}}
+  // expected-warning@-4 {{'bothDeprecatedDiffReplacement' is deprecated: first deprecated in macCatalyst 13.1 and first deprecated in macOS 10.15}}
 #endif
 
   bothUnavailableSameReplacement();
@@ -144,7 +143,7 @@ void checkMix() {
 
   justIosUnavailable(); // expected-error {{'justIosUnavailable' is unavailable: not available on macCatalyst}}
 
-  justMacOSDeprecated(); // expected-warning {{'justMacOSDeprecated' is deprecated: first deprecated in macOS 10.14}}
+  justMacOSDeprecated(); // expected-warning {{'justMacOSDeprecated' is deprecated: first deprecated in macOS 10.15}}
 }
 
 int deprecatedFunc() __attribute__((deprecated)); // expected-note {{'deprecatedFunc' has been explicitly marked deprecated here}}
@@ -156,18 +155,18 @@ void a() {
 }
 
 
-struct IntroducedLaterBoth { } __attribute__((availability(macos, introduced=10.15), availability(ios, introduced=14)));
+struct IntroducedLaterBoth { } __attribute__((availability(macos, introduced=11), availability(ios, introduced=14)));
 #ifndef INVERTED
-// expected-note@-2 {{'IntroducedLaterBoth' has been marked as being introduced in macOS 10.15 here, but the deployment target is macOS 10.14.0}}
+// expected-note@-2 {{'IntroducedLaterBoth' has been marked as being introduced in macOS 11 here, but the deployment target is macOS 10.15}}
 #else
-// expected-note@-4 {{'IntroducedLaterBoth' has been marked as being introduced in macCatalyst 14 here, but the deployment target is macCatalyst 13.0.0}}
+// expected-note@-4 {{'IntroducedLaterBoth' has been marked as being introduced in macCatalyst 14 here, but the deployment target is macCatalyst 13.1}}
 #endif
 
 struct NotYetIntroduced { // expected-note {{annotate 'NotYetIntroduced' with an availability attribute to silence this warning}}
   struct IntroducedLaterBoth x;
 #ifndef INVERTED
-  // expected-warning@-2 {{'IntroducedLaterBoth' is only available on macOS 10.15 and macCatalyst 13 or newer}}
+  // expected-warning@-2 {{'IntroducedLaterBoth' is only available on macOS 11 and macCatalyst 14 or newer}}
 #else
-  // expected-warning@-4 {{'IntroducedLaterBoth' is only available on macCatalyst 13 and macOS 10.15 or newer}}
+  // expected-warning@-4 {{'IntroducedLaterBoth' is only available on macCatalyst 14 and macOS 11 or newer}}
 #endif
 };
--- clang/test/SemaObjC/unguarded-availability-zippered.m
+++ clang/test/SemaObjC/unguarded-availability-zippered.m
@@ -1,78 +1,78 @@
-// RUN: %clang_cc1 -triple x86_64-apple-macos10.14 -darwin-target-variant-triple x86_64-apple-ios12-macabi -fblocks -fsyntax-only -ftarget-variant-availability-checks -verify %s
-// RUN: %clang_cc1 -triple x86_64-apple-ios12-macabi -darwin-target-variant-triple x86_64-apple-macos10.14 -fblocks -fsyntax-only -ftarget-variant-availability-checks -verify -D INVERTED %s
+// RUN: %clang_cc1 -triple x86_64-apple-macos10.15 -darwin-target-variant-triple x86_64-apple-ios13.1-macabi -fblocks -fsyntax-only -ftarget-variant-availability-checks -Wno-ignored-availability-without-sdk-settings -verify %s
+// RUN: %clang_cc1 -triple x86_64-apple-ios13.1-macabi -darwin-target-variant-triple x86_64-apple-macos10.15 -fblocks -fsyntax-only -ftarget-variant-availability-checks -Wno-ignored-availability-without-sdk-settings -verify -D INVERTED %s
 
 
 // XFAIL: *
 #define AVAILABLE_PREV_MAC __attribute__((availability(macos, introduced = 10.13)))
-#define AVAILABLE_CURRENT_MAC __attribute__((availability(macos, introduced = 10.14)))
-#define AVAILABLE_NEXT_MAC __attribute__((availability(macos, introduced = 10.15)))
+#define AVAILABLE_CURRENT_MAC __attribute__((availability(macos, introduced = 10.15)))
+#define AVAILABLE_NEXT_MAC __attribute__((availability(macos, introduced = 11.0)))
 
-#define AVAILABLE_PREV_IOS __attribute__((availability(ios, introduced = 11)))
-#define AVAILABLE_CURRENT_IOS __attribute__((availability(iOSMac, introduced = 12)))
-#define AVAILABLE_NEXT_IOS __attribute__((availability(ios, introduced = 13)))
+#define AVAILABLE_PREV_IOS __attribute__((availability(macCatalyst, introduced = 11)))
+#define AVAILABLE_CURRENT_IOS __attribute__((availability(macCatalyst, introduced = 13.1)))
+#define AVAILABLE_NEXT_IOS __attribute__((availability(macCatalyst, introduced = 14)))
 
 void bothPreviouslyAvailable() AVAILABLE_PREV_MAC AVAILABLE_PREV_IOS;
 void bothCurrentlyAvailable() AVAILABLE_CURRENT_MAC AVAILABLE_CURRENT_IOS;
 void bothWillBeAvailable() AVAILABLE_NEXT_MAC AVAILABLE_NEXT_IOS;
 #ifndef INVERTED
-// expected-note@-2 3 {{'bothWillBeAvailable' has been marked as being introduced in macOS 10.15 here, but the deployment target is macOS 10.14.0}}
-// expected-note@-3 {{'bothWillBeAvailable' has been marked as being introduced in macCatalyst 14 here, but the deployment target is macCatalyst 13.0.0}}
+// expected-note@-2 3 {{'bothWillBeAvailable' has been marked as being introduced in macOS 11.0 here, but the deployment target is macOS 10.15}}
+// expected-note@-3 {{'bothWillBeAvailable' has been marked as being introduced in macCatalyst 14 here, but the deployment target is macCatalyst 13.1}}
 #else
-// expected-note@-5 3 {{'bothWillBeAvailable' has been marked as being introduced in macCatalyst 14 here, but the deployment target is macCatalyst 13.0.0}}
-// expected-note@-6 {{'bothWillBeAvailable' has been marked as being introduced in macOS 10.15 here, but the deployment target is macOS 10.14.0}}
+// expected-note@-5 3 {{'bothWillBeAvailable' has been marked as being introduced in macCatalyst 14 here, but the deployment target is macCatalyst 13.1}}
+// expected-note@-6 {{'bothWillBeAvailable' has been marked as being introduced in macOS 11.0 here, but the deployment target is macOS 10.15}}
 #endif
 
 void macOSCurrentlyAvailable() AVAILABLE_CURRENT_MAC AVAILABLE_NEXT_IOS;
 // #ifndef INVERTED
-// expected-note@-2 2 {{'macOSCurrentlyAvailable' has been marked as being introduced in macCatalyst 14 here, but the deployment target is macCatalyst 13.0.0}}
+// expected-note@-2 2 {{'macOSCurrentlyAvailable' has been marked as being introduced in macCatalyst 14 here, but the deployment target is macCatalyst 13.1}}
 // #else
 // #endif
 
 void macOSNextAvailableiOSNotAvailable() AVAILABLE_NEXT_MAC __attribute__((availability(ios, unavailable)));
 // expected-note@-1 2 {{'macOSNextAvailableiOSNotAvailable' has been explicitly marked unavailable here}}
-// expected-note@-2 {{'macOSNextAvailableiOSNotAvailable' has been marked as being introduced in macOS 10.15 here, but the deployment target is macOS 10.14.0}}
+// expected-note@-2 {{'macOSNextAvailableiOSNotAvailable' has been marked as being introduced in macOS 11.0 here, but the deployment target is macOS 10.15}}
 
 void test() {
   bothPreviouslyAvailable();
   bothCurrentlyAvailable();
   bothWillBeAvailable(); // expected-note{{enclose 'bothWillBeAvailable' in an @available check to silence this warning}}
 #ifndef INVERTED
-  // expected-warning@-2 {{'bothWillBeAvailable' is only available on macOS 10.15 and macCatalyst 13 or newer}}
+  // expected-warning@-2 {{'bothWillBeAvailable' is only available on macOS 11.0 and macCatalyst 14 or newer}}
 #else
-  // expected-warning@-4 {{'bothWillBeAvailable' is only available on macCatalyst 13 and macOS 10.15 or newer}}
+  // expected-warning@-4 {{'bothWillBeAvailable' is only available on macCatalyst 14 and macOS 11.0 or newer}}
 #endif
   
   
-  macOSCurrentlyAvailable(); // expected-warning {{'macOSCurrentlyAvailable' is only available on macCatalyst 13 or newer}}
+  macOSCurrentlyAvailable(); // expected-warning {{'macOSCurrentlyAvailable' is only available on macCatalyst 14 or newer}}
   // expected-note@-1 {{enclose 'macOSCurrentlyAvailable' in an @available check to silence this warning}}
 
-  if (@available(ios 13, macos 10.15, *))
+  if (@available(ios 14, macos 11.0, *))
     bothWillBeAvailable();
-  if (@available(ios 13, *))
-    bothWillBeAvailable(); // expected-warning {{'bothWillBeAvailable' is only available on macOS 10.15 or newer}}
+  if (@available(ios 14, *))
+    bothWillBeAvailable(); // expected-warning {{'bothWillBeAvailable' is only available on macOS 11.0 or newer}}
   // expected-note@-1 {{enclose}}
-  if (@available(ios 12, *))
+  if (@available(ios 13, *))
     bothWillBeAvailable(); // expected-note {{enclose}}
 #ifndef INVERTED
-  // expected-warning@-2 {{'bothWillBeAvailable' is only available on macOS 10.15 and macCatalyst 13 or newer}}
+  // expected-warning@-2 {{'bothWillBeAvailable' is only available on macOS 11.0 and macCatalyst 14 or newer}}
 #else
-  // expected-warning@-4 {{'bothWillBeAvailable' is only available on macCatalyst 13 and macOS 10.15 or newer}}
+  // expected-warning@-4 {{'bothWillBeAvailable' is only available on macCatalyst 14 and macOS 11.0 or newer}}
 #endif
-  if (@available(macos 10.15, *))
-    bothWillBeAvailable(); // expected-warning {{'bothWillBeAvailable' is only available on macCatalyst 13 or newer}}
+  if (@available(macos 11.0, *))
+    bothWillBeAvailable(); // expected-warning {{'bothWillBeAvailable' is only available on macCatalyst 14 or newer}}
   // expected-note@-1 {{enclose}}
 
-  if (@available(macos 10.15, *))
-    macOSCurrentlyAvailable(); // expected-warning {{'macOSCurrentlyAvailable' is only available on macCatalyst 13 or newer}}
+  if (@available(macos 11.0, *))
+    macOSCurrentlyAvailable(); // expected-warning {{'macOSCurrentlyAvailable' is only available on macCatalyst 14 or newer}}
   // expected-note@-1 {{enclose}}
-  if (@available(ios 13, *))
+  if (@available(ios 14, *))
     macOSCurrentlyAvailable();
 
   macOSNextAvailableiOSNotAvailable();
   // expected-error@-1 {{'macOSNextAvailableiOSNotAvailable' is unavailable: not available on macCatalyst}}
-  // expected-warning@-2 {{'macOSNextAvailableiOSNotAvailable' is only available on macOS 10.15 or newer}}
+  // expected-warning@-2 {{'macOSNextAvailableiOSNotAvailable' is only available on macOS 11.0 or newer}}
   // expected-note@-3 {{enclose}}
 
-  if (@available(macos 10.15, *))
+  if (@available(macos 11.0, *))
     macOSNextAvailableiOSNotAvailable(); // expected-error {{'macOSNextAvailableiOSNotAvailable' is unavailable: not available on macCatalyst}}
 }
