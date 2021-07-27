060583e66abbba375117491b65590660ed1f3e53
Michael Spencer <michael_spencer@apple.com>
2021-07-27 17:57:48 -0600

    Fix linker-script-to-export-list.py

--- clang/tools/libclang/linker-script-to-export-list.py
+++ clang/tools/libclang/linker-script-to-export-list.py
@@ -3,9 +3,8 @@ import os
 import sys
 
 input_file = open(sys.argv[1])
-output_file = open(sys.argv[2])
-
-for line in input_file:
-    m = re.search('clang_[^;]+', line)
-    if m:
-        output_file.write(m.group(0))
+with open(sys.argv[2], "w") as output_file:
+    for line in input_file:
+        m = re.search('clang_[^;]+', line)
+        if m:
+            output_file.write(m.group(0) + '\n')
