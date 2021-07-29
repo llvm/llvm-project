5a589f2bda1db5eeff2fdeb4f55df18b700dae98
Stefan Pejic <stefan.pejic@syrmia.com>
2021-07-29 12:00:11 +0200

    Fails due to bug in nanomips-elf-as
    nanomips-elf-as cannot stream to stdout

--- clang/test/InterfaceStubs/object.c
+++ clang/test/InterfaceStubs/object.c
@@ -1,3 +1,5 @@
+// UNSUPPORTED: nanomips
+
 // RUN: %clang_cc1 -fvisibility default -o - -emit-interface-stubs %s | FileCheck -check-prefix=CHECK-TAPI %s
 // RUN: %clang -fvisibility=default -c -o - %s | llvm-nm - 2>&1 | FileCheck -check-prefix=CHECK-SYMBOLS %s
 
