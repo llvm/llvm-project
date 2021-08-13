460c11ca3de55f3088d1a0882b0db1696867efb4
Adrian Prantl <aprantl@apple.com>
2021-08-13 09:09:38 -0700

    Reapply ptrauth changes after incomplete merge conflict resolution.

--- llvm/lib/Transforms/Coroutines/CoroFrame.cpp
+++ llvm/lib/Transforms/Coroutines/CoroFrame.cpp
@@ -2528,6 +2528,19 @@ void coro::salvageDebugInfo(
       OutermostLoad = false;
     } else if (auto *StInst = dyn_cast<StoreInst>(Inst)) {
       Storage = StInst->getOperand(0);
+    } else if (auto *I2PInst = dyn_cast<llvm::IntToPtrInst>(Inst)) {
+      Storage = I2PInst->getOperand(0);
+    } else if (auto *P2IInst = dyn_cast<llvm::PtrToIntInst>(Inst)) {
+      Storage = P2IInst->getOperand(0);
+    } else if (auto *IInst = dyn_cast<llvm::IntrinsicInst>(Inst)) {
+      if (IInst->getIntrinsicID() == Intrinsic::ptrauth_auth)
+        Storage = IInst->getArgOperand(0);
+      else
+        break;
+    } else if (auto *Phi = dyn_cast<PHINode>(Inst)) {
+      if (Phi->getNumIncomingValues() != 1)
+        return;
+      Storage = Phi->getIncomingValue(0);
     } else {
       SmallVector<uint64_t, 16> Ops;
       SmallVector<Value *, 0> AdditionalValues;
