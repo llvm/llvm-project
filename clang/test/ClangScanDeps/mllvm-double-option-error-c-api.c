1582bf10c93b021bef45df85087234fcd7f9da9b
Jan Svoboda <jan_svoboda@apple.com>
2021-08-16 13:00:26 +0200

    [clang][deps] Upstream tests for libclang API
    Original author: Alex Lorenz <arphaman@gmail.com>

---
+++ clang/test/ClangScanDeps/mllvm-double-option-error-c-api.c
@@ -0,0 +1,9 @@
+// RUN: c-index-test core -scan-deps %S -- clang_tool -Dmz -mllvm -asan-instrumentation-with-call-threshold=0 -mllvm -asan-instrumentation-with-call-threshold=0 %s -I %S/Inputs | FileCheck %s
+
+#ifdef mz
+#include "header.h"
+#endif
+
+// CHECK: file-deps:
+// CHECK-NEXT: mllvm-double-option-error-c-api.c
+// CHECK-NEXT: Inputs/header.h
