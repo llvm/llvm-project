a5f9cd200ce0a2ee5620c882f8521482b6decc44
Adrian Prantl <aprantl@apple.com>
2021-08-27 11:38:59 -0700

    Makefiles: Promote EXCLUDE_WRAPPED_SWIFTMODULE into a more general DYLIB_HIDE_SWIFTMODULE facility.

--- lldb/test/API/lang/swift/implementation_only_imports/dylib.mk
+++ lldb/test/API/lang/swift/implementation_only_imports/dylib.mk
@@ -4,7 +4,7 @@ DYLIB_NAME := $(BASENAME)
 DYLIB_SWIFT_SOURCES := $(DYLIB_NAME).swift
 # Disable debug info for the library.
 override MAKE_DSYM := NO
-EXCLUDE_WRAPPED_SWIFTMODULE := YES
+DYLIB_HIDE_SWIFTMODULE := YES
 SWIFTFLAGS := -gnone -I. -module-link-name $(BASENAME)
 LD_EXTRAS := -L.
 
