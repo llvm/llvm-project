a52b7bf7d1238bc34d5810884404993078beae01
Richard Smith <richard@metafoo.co.uk>
2021-09-01 17:57:48 -0700

    PR50294: Fix a performance regression from 2c9dbcd.
    Per the contract of ReadLateParsedTemplates, we should not be returning
    the same results multiple times. No functionality change intended, other
    than to runtime.
    
    Thanks to Luboš Luňák for identifying the cause of the regression!
    
    (cherry picked from commit 6eda66b0a9f793c65852aa94430ae9bd891bcf63)

--- clang/lib/Serialization/ASTReader.cpp
+++ clang/lib/Serialization/ASTReader.cpp
@@ -8456,6 +8456,8 @@ void ASTReader::ReadLateParsedTemplates(
       LPTMap.insert(std::make_pair(FD, std::move(LT)));
     }
   }
+
+  LateParsedTemplates.clear();
 }
 
 void ASTReader::LoadSelector(Selector Sel) {
