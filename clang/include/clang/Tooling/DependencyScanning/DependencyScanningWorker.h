b30aab155d94a8f65bfbf745f998bab747abdaab
Akira Hatanaka <ahatanaka@apple.com>
2021-09-06 22:23:53 -0700

    Add an API for clang dependency scanner to perform module lookup by name alone (#3127)
    * [clang][clang-scan-deps] Add an experimental C API for returning the list of
    module and file dependencies of a module for a particular compiler invocation
    
    rdar://64538073
    
    * Address review comments
    
    - Define a subclass of FrontendAction and call loadModule there.
    - Remove the API for creating a worker just for
      clang_experimental_DependencyScannerWorkerByModName_getFileDependencies_v1.
    - Add the capability to get the dependencies based on the module name to
      clang-scan-deps.
    
    * Address review comments
    
    - Create a fake virtual source file instead of asking users to create
      one and pass it to the command line.
    - Remove the "translation-units" output from test case
      modules-full-by-mod-name.cpp. The information isn't useful as the
      source file name isn't passed on the command line in this case.
    - Inherit from `PreprocessOnlyAction`.
    
    * Address review comments
    
    * Address review comments
    
    - Rebase.
    - Rename functions.
    - Remove unneeded test cases.
    
    * Address review comments
    
    - Remove member `ModuleName` and thread it through functions. Remove
      member `FakeMemBuffer` too and make it a variable inside
      `computeDependencies`.
    - Use `MemoryBufferRef` instead of `MemoryBuffer *`.
    - Use `StringRef` instead of `const char *`.
    
    * Address review comments
    
    - Use Optional<String> as the type of ModuleName.
    - Add comments explaining what happens if the passed ModuleName isn't
      empty.
    
    (cherry picked from commit 2a3e8df942a2ad32d019457d6c01a550e03dee8e)
    
    Conflicts:
    	clang/tools/libclang/libclang.map

--- clang/include/clang/Tooling/DependencyScanning/DependencyScanningWorker.h
+++ clang/include/clang/Tooling/DependencyScanning/DependencyScanningWorker.h
@@ -75,25 +75,29 @@ public:
   DependencyScanningWorker(DependencyScanningService &Service);
 
   /// Run the dependency scanning tool for a given clang driver invocation, and
-  /// report the discovered dependencies to the provided consumer.
+  /// report the discovered dependencies to the provided consumer. If
+  /// \p ModuleName isn't empty, this function reports the dependencies of
+  /// module \p ModuleName.
   ///
   /// \returns A \c StringError with the diagnostic output if clang errors
   /// occurred, success otherwise.
-  llvm::Error
-  computeDependenciesForClangInvocation(StringRef WorkingDirectory,
-                                        ArrayRef<std::string> Arguments,
-                                        DependencyConsumer &Consumer);
+  llvm::Error computeDependenciesForClangInvocation(
+      StringRef WorkingDirectory, ArrayRef<std::string> Arguments,
+      DependencyConsumer &Consumer,
+      llvm::Optional<StringRef> ModuleName = None);
 
   /// Run the dependency scanning tool for a given clang driver invocation (as
   /// specified for the given Input in the CDB), and report the discovered
-  /// dependencies to the provided consumer.
+  /// dependencies to the provided consumer. If \p ModuleName isn't empty, this
+  /// function reports the dependencies of module \p ModuleName.
   ///
   /// \returns A \c StringError with the diagnostic output if clang errors
   /// occurred, success otherwise.
   llvm::Error computeDependencies(const std::string &Input,
                                   StringRef WorkingDirectory,
                                   const CompilationDatabase &CDB,
-                                  DependencyConsumer &Consumer);
+                                  DependencyConsumer &Consumer,
+                                  llvm::Optional<StringRef> ModuleName = None);
 
   ScanningOutputFormat getFormat() const { return Format; }
 
