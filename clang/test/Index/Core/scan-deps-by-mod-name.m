2a3e8df942a2ad32d019457d6c01a550e03dee8e
Akira Hatanaka <ahatanaka@apple.com>
2021-09-06 22:23:53 -0700

    Add an API for clang dependency scanner to perform module lookup by name alone (#3127)
    * [clang][clang-scan-deps] Add an experimental C API for returning the list of
    module and file dependencies of a module for a particular compiler invocation
    
    rdar://64538073
    
    * Address review comments
    
    - Define a subclass of FrontendAction and call loadModule there.
    - Remove the API for creating a worker just for
      clang_experimental_DependencyScannerWorkerByModName_getFileDependencies_v1.
    - Add the capability to get the dependencies based on the module name to
      clang-scan-deps.
    
    * Address review comments
    
    - Create a fake virtual source file instead of asking users to create
      one and pass it to the command line.
    - Remove the "translation-units" output from test case
      modules-full-by-mod-name.cpp. The information isn't useful as the
      source file name isn't passed on the command line in this case.
    - Inherit from `PreprocessOnlyAction`.
    
    * Address review comments
    
    * Address review comments
    
    - Rebase.
    - Rename functions.
    - Remove unneeded test cases.
    
    * Address review comments
    
    - Remove member `ModuleName` and thread it through functions. Remove
      member `FakeMemBuffer` too and make it a variable inside
      `computeDependencies`.
    - Use `MemoryBufferRef` instead of `MemoryBuffer *`.
    - Use `StringRef` instead of `const char *`.
    
    * Address review comments
    
    - Use Optional<String> as the type of ModuleName.
    - Add comments explaining what happens if the passed ModuleName isn't
      empty.

---
+++ clang/test/Index/Core/scan-deps-by-mod-name.m
@@ -0,0 +1,27 @@
+// Use driver arguments.
+// RUN: rm -rf %t.mcp
+// RUN: echo %S > %t.result
+// RUN: c-index-test core --scan-deps-by-mod-name -module-name=ModA %S -- %clang -c -I %S/Inputs/module \
+// RUN:     -fmodules -fmodules-cache-path=%t.mcp \
+// RUN:     -o FoE.o -x objective-c >> %t.result
+// RUN: cat %t.result | sed 's/\\/\//g' | FileCheck %s
+
+// CHECK: [[PREFIX:.*]]
+// CHECK-NEXT: modules:
+// CHECK-NEXT:   module:
+// CHECK-NEXT:     name: ModA
+// CHECK-NEXT:     context-hash: [[HASH_MOD_A:[A-Z0-9]+]]
+// CHECK-NEXT:     module-map-path: [[PREFIX]]/Inputs/module/module.modulemap
+// CHECK-NEXT:     module-deps:
+// CHECK-NEXT:     file-deps:
+// CHECK-NEXT:       [[PREFIX]]/Inputs/module/ModA.h
+// CHECK-NEXT:       [[PREFIX]]/Inputs/module/SubModA.h
+// CHECK-NEXT:       [[PREFIX]]/Inputs/module/SubSubModA.h
+// CHECK-NEXT:       [[PREFIX]]/Inputs/module/module.modulemap
+// CHECK-NEXT:     build-args: {{.*}} -emit-module {{.*}} -fmodule-name=ModA -fno-implicit-modules {{.*}}
+// CHECK-NEXT: dependencies:
+// CHECK-NEXT:   context-hash:
+// CHECK-NEXT:   module-deps:
+// CHECK-NEXT:     ModA:[[HASH_MOD_A]]
+// CHECK-NEXT:   file-deps:
+// CHECK-NEXT:   additional-build-args: -fno-implicit-modules -fno-implicit-module-maps
--- clang/tools/c-index-test/core_main.cpp
+++ clang/tools/c-index-test/core_main.cpp
@@ -53,6 +53,7 @@ enum class ActionType {
   PrintStoreFormatVersion,
   AggregateAsJSON,
   ScanDeps,
+  ScanDepsByModuleName,
   WatchDir,
 };
 
@@ -75,6 +76,8 @@ Action(cl::desc("Action:"), cl::init(ActionType::None),
                      "aggregate-json", "Aggregate index data in JSON format"),
           clEnumValN(ActionType::ScanDeps, "scan-deps",
                      "Get file dependencies"),
+          clEnumValN(ActionType::ScanDepsByModuleName, "scan-deps-by-mod-name",
+                     "Get file dependencies by module name alone"),
           clEnumValN(ActionType::WatchDir,
                      "watch-dir", "Watch directory for file events")),
        cl::cat(IndexTestCoreCategory));
@@ -112,6 +115,9 @@ static cl::opt<std::string>
 FilePathAndRange("filepath",
                cl::desc("File path that can optionally include a line range"));
 
+static cl::opt<std::string>
+    ModuleName("module-name", cl::desc("name of the module, of which we are "
+                                       "getting file and module dependencies"));
 }
 } // anonymous namespace
 
@@ -715,6 +721,79 @@ static int scanDeps(ArrayRef<const char *> Args, std::string WorkingDirectory) {
   return 0;
 }
 
+static int scanDepsByModuleName(ArrayRef<const char *> Args,
+                                std::string WorkingDirectory,
+                                std::string ModuleName) {
+  CXDependencyScannerService Service =
+      clang_experimental_DependencyScannerService_create_v0(
+          CXDependencyMode_Full);
+  CXDependencyScannerWorker Worker =
+      clang_experimental_DependencyScannerWorker_create_v0(Service);
+  CXString Error;
+
+  auto Callback = [&](CXModuleDependencySet *MDS) {
+    llvm::outs() << "modules:\n";
+    for (const auto &M : llvm::makeArrayRef(MDS->Modules, MDS->Count)) {
+      llvm::outs() << "  module:\n"
+                   << "    name: " << clang_getCString(M.Name) << "\n"
+                   << "    context-hash: " << clang_getCString(M.ContextHash)
+                   << "\n"
+                   << "    module-map-path: "
+                   << clang_getCString(M.ModuleMapPath) << "\n"
+                   << "    module-deps:\n";
+      for (const auto &ModuleName :
+           llvm::makeArrayRef(M.ModuleDeps->Strings, M.ModuleDeps->Count))
+        llvm::outs() << "      " << clang_getCString(ModuleName) << "\n";
+      llvm::outs() << "    file-deps:\n";
+      for (const auto &FileName :
+           llvm::makeArrayRef(M.FileDeps->Strings, M.FileDeps->Count))
+        llvm::outs() << "      " << clang_getCString(FileName) << "\n";
+      llvm::outs() << "    build-args:";
+      for (const auto &Arg : llvm::makeArrayRef(M.BuildArguments->Strings,
+                                                M.BuildArguments->Count))
+        llvm::outs() << " " << clang_getCString(Arg);
+      llvm::outs() << "\n";
+    }
+    clang_experimental_ModuleDependencySet_dispose(MDS);
+  };
+
+  auto CB =
+      functionObjectToCCallbackRef<void(CXModuleDependencySet *)>(Callback);
+
+  CXFileDependencies *Result =
+      clang_experimental_DependencyScannerWorker_getDependenciesByModuleName_v0(
+          Worker, Args.size(), Args.data(), ModuleName.c_str(),
+          WorkingDirectory.c_str(), CB.Callback, CB.Context, &Error);
+  if (!Result) {
+    llvm::errs() << "error: failed to get dependencies\n";
+    llvm::errs() << clang_getCString(Error) << "\n";
+    clang_disposeString(Error);
+    return 1;
+  }
+  llvm::outs() << "dependencies:\n";
+  llvm::outs() << "  context-hash: " << clang_getCString(Result->ContextHash)
+               << "\n"
+               << "  module-deps:\n";
+  for (const auto &ModuleName : llvm::makeArrayRef(Result->ModuleDeps->Strings,
+                                                   Result->ModuleDeps->Count))
+    llvm::outs() << "    " << clang_getCString(ModuleName) << "\n";
+  llvm::outs() << "  file-deps:\n";
+  for (const auto &FileName :
+       llvm::makeArrayRef(Result->FileDeps->Strings, Result->FileDeps->Count))
+    llvm::outs() << "    " << clang_getCString(FileName) << "\n";
+  llvm::outs() << "  additional-build-args:";
+  for (const auto &Arg :
+       llvm::makeArrayRef(Result->AdditionalArguments->Strings,
+                          Result->AdditionalArguments->Count))
+    llvm::outs() << " " << clang_getCString(Arg);
+  llvm::outs() << "\n";
+
+  clang_experimental_FileDependencies_dispose(Result);
+  clang_experimental_DependencyScannerWorker_dispose_v0(Worker);
+  clang_experimental_DependencyScannerService_dispose_v0(Service);
+  return 0;
+}
+
 static void printSymbol(const IndexRecordDecl &Rec, raw_ostream &OS) {
   printSymbolInfo(Rec.SymInfo, OS);
   OS << " | ";
@@ -1009,6 +1088,20 @@ int indextest_core_main(int argc, const char **argv) {
       return 1;
     }
     return scanDeps(CompArgs, options::InputFiles[0]);
+  }
+
+  if (options::Action == ActionType::ScanDepsByModuleName) {
+    // InputFiles should be set to the working directory name.
+    if (options::InputFiles.empty()) {
+      errs() << "error: missing working directory\n";
+      return 1;
+    }
+    if (options::ModuleName.empty()) {
+      errs() << "error: missing module name\n";
+      return 1;
+    }
+    return scanDepsByModuleName(CompArgs, options::InputFiles[0],
+                                options::ModuleName);
   }
 
   if (options::Action == ActionType::WatchDir) {
