c8aefe304854ab9ad0fdc28f10043b1acf82905f
Adrian Prantl <aprantl@apple.com>
2021-09-28 15:06:05 -0700

    SwiftASTContext: Sink module context diagnostic collection into process_one_module (NFC)

--- lldb/source/Plugins/TypeSystem/Swift/SwiftASTContext.cpp
+++ lldb/source/Plugins/TypeSystem/Swift/SwiftASTContext.cpp
@@ -1999,50 +1999,6 @@ lldb::TypeSystemSP SwiftASTContext::CreateInstance(lldb::LanguageType language,
     pool.wait();
   }
 
-  for (size_t mi = 0; mi != num_images; ++mi) {
-    ModuleSP module_sp = target.GetImages().GetModuleAtIndex(mi);
-
-    // Skip images without a serialized Swift AST. This avoids
-    // spurious warning messages.
-    if (!HasSwiftModules(*module_sp))
-      continue;
-
-    auto type_system_or_err =
-        module_sp->GetTypeSystemForLanguage(lldb::eLanguageTypeSwift);
-    if (!type_system_or_err) {
-      llvm::consumeError(type_system_or_err.takeError());
-      continue;
-    }
-
-    auto *module_swift_ast =
-        llvm::dyn_cast_or_null<SwiftASTContext>(&*type_system_or_err);
-    if (module_swift_ast && !module_swift_ast->HasFatalErrors() &&
-        module_swift_ast->GetClangImporter())
-      continue;
-    // Make sure we warn about this module load failure, the one
-    // that comes from loading types often gets swallowed up and not
-    // seen, this is the only reliable point where we can show this.
-    // But only do it once per UUID so we don't overwhelm the user
-    // with warnings.
-    UUID module_uuid(module_sp->GetUUID());
-    bool unique_message =
-        target.RegisterSwiftContextMessageKey(module_uuid.GetAsString());
-    if (!unique_message)
-      continue;
-    std::string buf;
-    {
-      llvm::raw_string_ostream ss(buf);
-      module_sp->GetDescription(ss, eDescriptionLevelBrief);
-      if (module_swift_ast && module_swift_ast->HasFatalErrors())
-        ss << ": "
-           << module_swift_ast->GetFatalErrors().AsCString("unknown error");
-    }
-    target.GetDebugger().GetErrorStreamSP()->Printf(
-        "Error while loading Swift module:\n%s\n"
-        "Debug info from this module will be unavailable in the debugger.\n\n",
-        buf.c_str());
-  }
-
   if (!handled_sdk_path) {
     for (size_t mi = 0; mi != num_images; ++mi) {
       ModuleSP module_sp = target.GetImages().GetModuleAtIndex(mi);
@@ -2243,7 +2199,36 @@ lldb::TypeSystemSP SwiftASTContext::CreateInstance(lldb::LanguageType language,
 
         SwiftASTContext *ast_context =
             llvm::dyn_cast_or_null<SwiftASTContext>(&*type_system_or_err);
-        if (ast_context && !ast_context->HasErrors()) {
+
+        if (!ast_context || ast_context->HasFatalErrors() ||
+            !ast_context->GetClangImporter()) {
+          // Make sure we warn about this module load failure, the one
+          // that comes from loading types often gets swallowed up and not
+          // seen, this is the only reliable point where we can show this.
+          // But only do it once per UUID so we don't overwhelm the user
+          // with warnings.
+          UUID module_uuid(module_sp->GetUUID());
+          bool unique_message =
+              target.RegisterSwiftContextMessageKey(module_uuid.GetAsString());
+          if (!unique_message)
+            return;
+          std::string buf;
+          {
+            llvm::raw_string_ostream ss(buf);
+            module_sp->GetDescription(ss, eDescriptionLevelBrief);
+            if (ast_context && ast_context->HasFatalErrors())
+              ss << ": "
+                 << ast_context->GetFatalErrors().AsCString("unknown error");
+          }
+          target.GetDebugger().GetErrorStreamSP()->Printf(
+              "Error while loading Swift module:\n%s\n"
+              "Debug info from this module will be unavailable in the "
+              "debugger.\n\n",
+              buf.c_str());
+          return;
+        }
+
+        if (!ast_context->HasErrors()) {
           if (use_all_compiler_flags ||
               target.GetExecutableModulePointer() == module_sp.get()) {
 
