llvm/llvm-project

commit dcf9bd0e7f4edcf7e1241ff2e5e44ca4c32cd4ed

[LoopUnswitch] Simplify branch condition if it is select with constant operands
This fixes the miscompilation reported in https://reviews.llvm.org/rG5bb38e84d3d0#986154 .

`select _, true, false` matches both m_LogicalAnd and m_LogicalOr, making later
transformations confused.
Simplify the branch condition to not have the form.

llvm/lib/Transforms/Scalar/SimpleLoopUnswitch.cpp
@@ -2645,6 +2645,13 @@ unswitchBestCondition(Loop &L, DominatorTree &DT, LoopInfo &LI,
         BI->getSuccessor(0) == BI->getSuccessor(1))
       continue;
 
+    // If BI's condition is 'select _, true, false', simplify it to confuse
+    // matchers
+    Value *Cond = BI->getCondition(), *CondNext;
+    while (match(Cond, m_Select(m_Value(CondNext), m_One(), m_Zero())))
+      Cond = CondNext;
+    BI->setCondition(Cond);
+
     if (L.isLoopInvariant(BI->getCondition())) {
       UnswitchCandidates.push_back({BI, {BI->getCondition()}});
       continue;

llvm/test/Transforms/SimpleLoopUnswitch/nontrivial-unswitch.ll
@@ -4248,7 +4248,7 @@ loop_begin:
 ; CHECK-NEXT:    %[[V2:.*]] = load i1, i1* %ptr2
 ; CHECK-NEXT:    %[[AND1:.*]] = select i1 %[[V1]], i1 true, i1 false
 ; CHECK-NEXT:    %[[AND2:.*]] = select i1 %[[AND1]], i1 true, i1 false
-; CHECK-NEXT:    br i1 %[[AND2]], label %loop_a, label %loop_b
+; CHECK-NEXT:    br i1 %[[V1]], label %loop_a, label %loop_b
 
 loop_a:
   call i32 @a()
@@ -4326,7 +4326,7 @@ loop_begin:
 ; CHECK-NEXT:    %[[V2:.*]] = load i1, i1* %ptr2
 ; CHECK-NEXT:    %[[AND1:.*]] = select i1 %[[V1]], i1 true, i1 false
 ; CHECK-NEXT:    %[[AND2:.*]] = select i1 %[[AND1]], i1 true, i1 false
-; CHECK-NEXT:    br i1 %[[AND2]], label %loop_b, label %loop_a
+; CHECK-NEXT:    br i1 %[[V1]], label %loop_b, label %loop_a
 
 loop_a:
   call i32 @a()

commit 72aeb4fa1fc9bd8cbd70af5620e52e9ff09b12e2

[Test] Add a test exposing a miscompile in SimpleLoopUnswitch.
The miscompile was introduced by dcf9bd0e7f4edcf7e1241ff2e5e44ca4c32cd4ed.

llvm/test/Transforms/SimpleLoopUnswitch/nontrivial-unswitch-invariant-select-bug.ll
@@ -1,0 +1,41 @@
+; NOTE: Assertions have been autogenerated by utils/update_test_checks.py
+; RUN: opt -passes='simple-loop-unswitch<nontrivial>' -S < %s | FileCheck %s
+
+; FIXME: We should not replace `true` with `false` here!
+define i1 @bar() {
+; CHECK-LABEL: @bar(
+; CHECK-NEXT:    ret i1 false
+;
+  ret i1 true
+}
+
+; FIXME: We shouldn't unswitch this loop!
+define void @foo() {
+; CHECK-LABEL: @foo(
+; CHECK-NEXT:  entry:
+; CHECK-NEXT:    br i1 true, label [[ENTRY_SPLIT_US:%.*]], label [[ENTRY_SPLIT:%.*]]
+; CHECK:       entry.split.us:
+; CHECK-NEXT:    br label [[HEADER_US:%.*]]
+; CHECK:       header.us:
+; CHECK-NEXT:    [[VAL_US:%.*]] = select i1 true, i1 true, i1 false
+; CHECK-NEXT:    br label [[EXIT_SPLIT_US:%.*]]
+; CHECK:       exit.split.us:
+; CHECK-NEXT:    br label [[EXIT:%.*]]
+; CHECK:       entry.split:
+; CHECK-NEXT:    br label [[HEADER:%.*]]
+; CHECK:       header:
+; CHECK-NEXT:    [[VAL:%.*]] = select i1 false, i1 false, i1 false
+; CHECK-NEXT:    br label [[HEADER]]
+; CHECK:       exit:
+; CHECK-NEXT:    ret void
+;
+entry:
+  br label %header
+
+header:
+  %val = select i1 true, i1 true, i1 false
+  br i1 %val, label %exit, label %header
+
+exit:
+  ret void
+}