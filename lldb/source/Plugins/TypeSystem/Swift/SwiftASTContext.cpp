cc13fd54a3d65e0c9de76029a5c68db8ec79bb9c
Dave Lee <davelee.com@gmail.com>
2021-10-01 11:12:58 -0700

    update RevisionIncompatible string

--- lldb/source/Plugins/TypeSystem/Swift/SwiftASTContext.cpp
+++ lldb/source/Plugins/TypeSystem/Swift/SwiftASTContext.cpp
@@ -1272,7 +1272,8 @@ static const char *getImportFailureString(swift::serialization::Status status) {
     return "The module file was built with a different SDK than the one in use "
            "to build the client.";
   case swift::serialization::Status::RevisionIncompatible:
-    return "The precise revision version doesn't match.";
+    return "The resilient module file was built by a different version of the "
+           "compiler.";
   }
 }
 
