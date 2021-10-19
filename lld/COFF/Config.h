69daf79bbf2b60e11765f9ed38715c59b47ce4cc
KOLANICH <kolan_n@amil.ru>
2021-10-19 12:33:00 +0300

    Added support of Windows XP for the files generated by clang.
    In fact I set the versions to 4.0 (Windows NT 4.0) because MinGW-w64 libs have them set so, but I haven't tested the binaries on anything other than XP and ReactOS.

--- lld/COFF/Config.h
+++ lld/COFF/Config.h
@@ -247,9 +247,9 @@ struct Configuration {
   uint32_t minorImageVersion = 0;
   // If changing the default os/subsys version here, update the default in
   // the MinGW driver accordingly.
-  uint32_t majorOSVersion = 6;
+  uint32_t majorOSVersion = 4;
   uint32_t minorOSVersion = 0;
-  uint32_t majorSubsystemVersion = 6;
+  uint32_t majorSubsystemVersion = 4;
   uint32_t minorSubsystemVersion = 0;
   uint32_t timestamp = 0;
   uint32_t functionPadMin = 0;
