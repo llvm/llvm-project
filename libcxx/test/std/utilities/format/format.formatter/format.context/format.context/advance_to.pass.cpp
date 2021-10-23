91dd07235411ec7743311298546c4f3f767930f3
Mark de Wever <koraq@xs4all.nl>
2021-10-23 19:11:02 +0200

    [libc++][format] Move iterators when needed.
    LWG-3539 was already implemented but not marked as done.
    LWG-3567 is implemented in this commit.
    
    Reviewed By: ldionne, #libc
    
    Differential Revision: https://reviews.llvm.org/D112368

--- libcxx/test/std/utilities/format/format.formatter/format.context/format.context/advance_to.pass.cpp
+++ libcxx/test/std/utilities/format/format.formatter/format.context/format.context/advance_to.pass.cpp
@@ -18,18 +18,20 @@
 
 #include "test_macros.h"
 #include "test_format_context.h"
+#include "test_iterators.h"
 
 template <class OutIt, class CharT>
 void test(
     std::basic_format_args<std::basic_format_context<OutIt, CharT>> args) {
   {
-    std::basic_string<CharT> str[3];
-    std::basic_format_context context = test_format_context_create(OutIt{str[0]}, args);
-    context.out() = CharT('a');
-    context.advance_to(OutIt{str[1]});
-    context.out() = CharT('b');
-    context.advance_to(OutIt{str[2]});
-    context.out() = CharT('c');
+    std::basic_string<CharT> str[3] = {
+        std::basic_string<CharT>{1}, std::basic_string<CharT>{1}, std::basic_string<CharT>{1}};
+    std::basic_format_context context = test_format_context_create(OutIt{str[0].begin()}, args);
+    *context.out()                    = CharT('a');
+    context.advance_to(OutIt{str[1].begin()});
+    *context.out() = CharT('b');
+    context.advance_to(OutIt{str[2].begin()});
+    *context.out() = CharT('c');
 
     assert(str[0].size() == 1);
     assert(str[0].front() == CharT('a'));
@@ -42,27 +44,25 @@ void test(
 
 void test() {
   test(std::basic_format_args(
-      std::make_format_args<std::basic_format_context<
-          std::back_insert_iterator<std::basic_string<char>>, char>>()));
+      std::make_format_args<
+          std::basic_format_context<cpp20_output_iterator<std::basic_string<char>::iterator>, char>>()));
 
 #ifndef TEST_HAS_NO_WIDE_CHARACTERS
   test(std::basic_format_args(
-      std::make_format_args<std::basic_format_context<
-          std::back_insert_iterator<std::basic_string<wchar_t>>, wchar_t>>()));
+      std::make_format_args<
+          std::basic_format_context<cpp20_output_iterator<std::basic_string<wchar_t>::iterator>, wchar_t>>()));
 #endif
 #ifndef TEST_HAS_NO_CHAR8_T
   test(std::basic_format_args(
-      std::make_format_args<std::basic_format_context<
-          std::back_insert_iterator<std::basic_string<char8_t>>, char8_t>>()));
+      std::make_format_args<
+          std::basic_format_context<cpp20_output_iterator<std::basic_string<char8_t>::iterator>, char8_t>>()));
 #endif
   test(std::basic_format_args(
-      std::make_format_args<std::basic_format_context<
-          std::back_insert_iterator<std::basic_string<char16_t>>,
-          char16_t>>()));
+      std::make_format_args<
+          std::basic_format_context<cpp20_output_iterator<std::basic_string<char16_t>::iterator>, char16_t>>()));
   test(std::basic_format_args(
-      std::make_format_args<std::basic_format_context<
-          std::back_insert_iterator<std::basic_string<char32_t>>,
-          char32_t>>()));
+      std::make_format_args<
+          std::basic_format_context<cpp20_output_iterator<std::basic_string<char32_t>::iterator>, char32_t>>()));
 }
 
 int main(int, char**) {
