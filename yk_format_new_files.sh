c329bf2f4fb43375112c2f4a9981d9fe3900df4a
Edd Barrett <vext01@gmail.com>
2021-10-29 15:15:16 +0100

    yk_format_new_files.sh: Automatically determine the LLVM version.

--- yk_format_new_files.sh
+++ yk_format_new_files.sh
@@ -3,7 +3,7 @@
 # Format any new files that we have added in our fork.
 
 # The LLVM version tag we are forked from.
-FORKED_LLVM_VERSION=llvmorg-13.0.0
+FORKED_LLVM_VERSION=`git describe --abbrev=0 --match 'llvmorg-*'`
 
 git diff --name-only --diff-filter=A ${FORKED_LLVM_VERSION} \
     `git branch --show-current` | egrep '\.(cpp|h)$' | xargs clang-format -i
