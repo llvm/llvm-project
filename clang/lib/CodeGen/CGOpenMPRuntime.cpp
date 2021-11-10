98a29374cf0bb343b8471814f71faa38e4d3bb4d
Matt Arsenault <Matthew.Arsenault@amd.com>
2021-11-09 22:43:17 -0500

    OpenMP: Start calling setTargetAttributes for generated kernels
    This wasn't setting any of the attributes the target would expect to
    emit for kernels.
    
    Change-Id: If453f1fabe452b4f84697c4b41a87585fee437df

--- clang/lib/CodeGen/CGOpenMPRuntime.cpp
+++ clang/lib/CodeGen/CGOpenMPRuntime.cpp
@@ -15,6 +15,7 @@
 #include "CGCleanup.h"
 #include "CGRecordLayout.h"
 #include "CodeGenFunction.h"
+#include "TargetInfo.h"
 #include "clang/AST/APValue.h"
 #include "clang/AST/Attr.h"
 #include "clang/AST/Decl.h"
@@ -6649,6 +6650,8 @@ void CGOpenMPRuntime::emitTargetOutlinedFunctionHelper(
     OutlinedFn->addFnAttr("omp_target_thread_limit",
                           std::to_string(DefaultValThreads));
   }
+
+  CGM.getTargetCodeGenInfo().setTargetAttributes(nullptr, OutlinedFn, CGM);
 }
 
 /// Checks if the expression is constant or does not have non-trivial function
