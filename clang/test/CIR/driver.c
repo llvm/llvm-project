640f0c12ee5985861daeb5c47997e62c25cb4f04
Nathan Lanza <nathan@lanza.io>
2021-11-24 02:55:26 -0500

    [CIR][NFC] Rip driver-specific testing out to a new test file

---
+++ clang/test/CIR/driver.c
@@ -0,0 +1,21 @@
+// RUN: %clang -target x86_64-unknown-linux-gnu -fclangir -S -emit-cir %s -o %t.cir
+// RUN: FileCheck --input-file=%t.cir %s -check-prefix=CIR
+// RUN: %clang -target x86_64-unknown-linux-gnu -fclangir -S -emit-llvm %s -o %t.ll
+// RUN: FileCheck --input-file=%t.ll %s -check-prefix=LLVM
+// RUN: %clang -target x86_64-unknown-linux-gnu -fclangir -c %s -o %t.o
+// RUN: llvm-objdump -d %t.o | FileCheck %s -check-prefix=OBJ
+// XFAIL: *
+
+void foo() {}
+
+//      CIR: module  {
+// CIR-NEXT:   func @foo() {
+// CIR-NEXT:     cir.return
+// CIR-NEXT:   }
+// CIR-NEXT: }
+
+//      LLVM: define void @foo()
+// LLVM-NEXT:   ret void,
+// LLVM-NEXT: }
+
+// OBJ: 0: c3 retq
