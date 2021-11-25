dd66aa10bb677cd26c79fdd34c839da56cab23a7
Yatima Santamorena <yatima@santamorena.me>
2021-11-25 11:33:20 +0100

    install.cmd for the build tools

---
+++ llvm/utils/docker/windows/Install.cmd
@@ -0,0 +1,14 @@
+@if not defined _echo echo off
+setlocal enabledelayedexpansion
+
+call %*
+if "%ERRORLEVEL%"=="3010" (
+    exit /b 0
+) else (
+    if not "%ERRORLEVEL%"=="0" (
+        set ERR=%ERRORLEVEL%
+        call C:\TEMP\collect.exe -zip:C:\vslogs.zip
+
+        exit /b !ERR!
+    )
+)
