20b71503dc1fa1b56f0c2f6f138c0dc7af4cd372
Michael Kruse <llvm-project@meinersbur.de>
2021-12-06 04:07:53 -0600

    outputs stores

--- llvm/lib/Transforms/Utils/CodeExtractor.cpp
+++ llvm/lib/Transforms/Utils/CodeExtractor.cpp
@@ -1793,15 +1793,18 @@ CodeExtractor::extractCodeRegion(const CodeExtractorAnalysisCache& CEAC,
         std::advance(OutputArgBegin, inputs.size());
 
 
-    if (!KeepOldBlocks) {
+
         // Store the arguments right after the definition of output value.
         // This should be proceeded after creating exit stubs to be ensure that invoke
         // result restore will be placed in the outlined function.
-        Function::arg_iterator OAI = OutputArgBegin;
+    Function::arg_iterator OAI = OutputArgBegin;
         for (unsigned i = 0, e = outputs.size(); i != e; ++i) {
             auto* OutI = dyn_cast<Instruction>(outputs[i]);
             if (!OutI)
                 continue;
+
+            if (KeepOldBlocks)
+            OutI = cast<Instruction>(VMap.lookup(OutI));
 
             // Find proper insertion point.
             BasicBlock::iterator InsertPt;
@@ -1828,19 +1831,16 @@ CodeExtractor::extractCodeRegion(const CodeExtractorAnalysisCache& CEAC,
                 GetElementPtrInst* GEP = GetElementPtrInst::Create(
                     StructTy, &*OAI, Idx, "gep_" + outputs[i]->getName(),
                     InsertBefore);
-                new StoreInst(outputs[i], GEP, InsertBefore);
+                new StoreInst(OutI, GEP, InsertBefore);
                 // Since there should be only one struct argument aggregating
                 // all the output values, we shouldn't increment OAI, which always
                 // points to the struct argument, in this case.
             }
             else {
-                new StoreInst(outputs[i], &*OAI, InsertBefore);
+                new StoreInst(OutI, &*OAI, InsertBefore);
                 ++OAI;
             }
         }
-
-    }
-
 
 
 
@@ -2098,59 +2098,7 @@ CodeExtractor::extractCodeRegion(const CodeExtractorAnalysisCache& CEAC,
    
 
 
-
-
  
-
-
-
-
-        // Store the arguments right after the definition of output value.
-        // This should be proceeded after creating exit stubs to be ensure that invoke
-        // result restore will be placed in the outlined function.
-        Function::arg_iterator OAI = OutputArgBegin;
-        for (unsigned i = 0, e = outputs.size(); i != e; ++i) {
-            auto* OutI = dyn_cast<Instruction>(outputs[i]);
-            if (!OutI)
-                continue;
-            OutI = cast<Instruction>(VMap.lookup(OutI));
-
-            // Find proper insertion point.
-            BasicBlock::iterator InsertPt;
-            // In case OutI is an invoke, we insert the store at the beginning in the
-            // 'normal destination' BB. Otherwise we insert the store right after OutI.
-            if (auto* InvokeI = dyn_cast<InvokeInst>(OutI))
-                InsertPt = InvokeI->getNormalDest()->getFirstInsertionPt();
-            else if (auto* Phi = dyn_cast<PHINode>(OutI))
-                InsertPt = Phi->getParent()->getFirstInsertionPt();
-            else
-                InsertPt = std::next(OutI->getIterator());
-
-            Instruction* InsertBefore = &*InsertPt;
-            assert((InsertBefore->getFunction() == newFunction ||
-                Blocks.count(InsertBefore->getParent())) &&
-                "InsertPt should be in new function");
-            assert(OAI != newFunction->arg_end() &&
-                "Number of output arguments should match "
-                "the amount of defined values");
-            if (AggregateArgs) {
-                Value* Idx[2];
-                Idx[0] = Constant::getNullValue(Type::getInt32Ty(Context));
-                Idx[1] = ConstantInt::get(Type::getInt32Ty(Context), FirstOut + i);
-                GetElementPtrInst* GEP = GetElementPtrInst::Create(
-                    StructTy, &*OAI, Idx, "gep_" + outputs[i]->getName(),
-                    InsertBefore);
-                new StoreInst(OutI, GEP, InsertBefore);
-                // Since there should be only one struct argument aggregating
-                // all the output values, we shouldn't increment OAI, which always
-                // points to the struct argument, in this case.
-            }
-            else {
-                new StoreInst(OutI, &*OAI, InsertBefore);
-                ++OAI;
-            }
-        }
-
 
         BasicBlock* HeaderCopy = cast<BasicBlock>(VMap.lookup(header));
         assert(HeaderCopy);
