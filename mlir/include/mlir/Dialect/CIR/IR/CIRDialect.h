65d8ad0b9394d9950e2087fbe5c378dc6bfca2c3
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2021-12-07 01:11:01 -0800

    [CIR] Add cir.binop and a series of binop kinds
    - Add builder support and tests.

--- mlir/include/mlir/Dialect/CIR/IR/CIRDialect.h
+++ mlir/include/mlir/Dialect/CIR/IR/CIRDialect.h
@@ -19,6 +19,7 @@
 #include "mlir/IR/Dialect.h"
 #include "mlir/IR/OpDefinition.h"
 #include "mlir/Interfaces/ControlFlowInterfaces.h"
+#include "mlir/Interfaces/InferTypeOpInterface.h"
 #include "mlir/Interfaces/SideEffectInterfaces.h"
 
 namespace mlir {
