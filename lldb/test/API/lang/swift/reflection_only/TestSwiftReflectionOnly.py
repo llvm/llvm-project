accd97e59935c0b6bb7a800b80a261befe72447e
Adrian Prantl <aprantl@apple.com>
2022-02-03 16:40:19 -0800

    Move (most of) No.Swiftmodule into a proper API test.
    Testing debugging from just reflection metadata is very important on
    all platforms. Shell tests only run on the host. By migrating this
    test to an API test we can make sure this is also tested when
    debugging on iOS devices.
    
    (cherry picked from commit dd1c41d0b51a504d40f53cf2fc33d55fbde8b0ee)

---
+++ lldb/test/API/lang/swift/reflection_only/TestSwiftReflectionOnly.py
@@ -0,0 +1,91 @@
+import lldb
+from lldbsuite.test.decorators import *
+import lldbsuite.test.lldbtest as lldbtest
+import lldbsuite.test.lldbutil as lldbutil
+import unittest2
+import re
+
+class TestSwiftReflectionOnly(lldbtest.TestBase):
+    NO_DEBUG_INFO_TESTCASE = True
+
+    mydir = lldbtest.TestBase.compute_mydir(__file__)
+
+    @swiftTest
+    def test(self):
+        """Test debugging a program without swiftmodules is functional"""
+        self.build()
+
+        target, process, thread, bkpt = lldbutil.run_to_source_breakpoint(
+            self, 'Set breakpoint here', lldb.SBFileSpec('main.swift'),
+            extra_images=['dynamic_lib'])
+        log = self.getBuildArtifact('types.log')
+        self.expect('log enable lldb types -f ' + log)
+
+        check_var = lldbutil.check_variable
+        frame = thread.frames[0]
+        var_self = frame.FindVariable("self")
+        var_self_x = var_self.GetChildMemberWithName("x")
+        check_var(self, var_self_x, value="42")
+
+        check_var(self, frame.FindVariable("number"), value="1")
+
+        array = frame.FindVariable("array")
+        check_var(self, array, num_children=3)
+        check_var(self, array.GetChildAtIndex(0), value="1")
+        check_var(self, array.GetChildAtIndex(1), value="2")
+        check_var(self, array.GetChildAtIndex(2), value="3")
+
+        check_var(self, frame.FindVariable("string"), summary='"hello"')
+
+        tup = frame.FindVariable("tuple")
+        check_var(self, tup, num_children=2)
+        check_var(self, tup.GetChildAtIndex(0), value="0")
+        check_var(self, tup.GetChildAtIndex(1), value="1")
+
+        strct = frame.FindVariable("strct")
+        check_var(self, strct, num_children=5)
+        check_var(self, strct.GetChildMemberWithName("pub"), value="1")
+        check_var(self, strct.GetChildMemberWithName("priv"), value="2")
+        check_var(self, strct.GetChildMemberWithName("filepriv"), value="3")
+        s_priv = strct.GetChildMemberWithName("s_priv")
+        check_var(self, s_priv, num_children=1)
+        check_var(self, s_priv.GetChildMemberWithName("i"), value="2")
+        s_filepriv = strct.GetChildMemberWithName("s_filepriv")
+        check_var(self, s_filepriv, num_children=1)
+        check_var(self, s_filepriv.GetChildMemberWithName("i"), value="3")
+
+        # FIXME: scratch context assertion
+        # check_var(self, frame.FindVariable("generic"), use_dynamic=True, value="42")
+
+        # gtup = frame.FindVariable("generic_tuple")
+        # check_var(self, gtup, num_children=2)
+        # check_var(self, gtup.GetChildAtIndex(0), use_dynamic=True, value="42")
+        # check_var(self, gtup.GetChildAtIndex(1), use_dynamic=True, value="42")
+
+        check_var(self, frame.FindVariable("word"), value="0")
+        check_var(self, frame.FindVariable("enum1"), value="second")
+        enum2 = frame.FindVariable("enum2")
+        check_var(self, enum2, value="with")
+        check_var(self, enum2, num_children=1)
+        # FIXME:  Fails in swift::reflection::NoPayloadEnumTypeInfo::projectEnumValue: .second
+        # check_var(self, enum2.GetChildAtIndex(0), value="42")
+
+        # Scan through the types log.
+        logfile = open(log, "r")
+        found_ref_exe = 0
+        found_ref_lib = 0
+        found_ast_exe = 0
+        found_ast_lib = 0
+        for line in logfile:
+            if 'SwiftASTContextForExpressions::RegisterSectionModules("a.out")' in line:
+                found_ast_exe += 1
+            elif 'SwiftASTContextForExpressions::RegisterSectionModules("dyld")' in line:
+                found_ast_lib += 1
+            elif re.search(r'Adding reflection metadata in .*a\.out', line):
+                found_ref_exe += 1
+            elif re.search(r'Adding reflection metadata in .*dynamic_lib', line):
+                found_ref_lib += 1
+        self.assertEqual(found_ref_exe, 1)
+        self.assertEqual(found_ref_lib, 1)
+        self.assertEqual(found_ast_exe, 0)
+        self.assertEqual(found_ast_lib, 0)
--- lldb/test/Shell/Swift/Inputs/No.swiftmodule.swift
+++ lldb/test/Shell/Swift/Inputs/No.swiftmodule.swift
@@ -1,37 +1,13 @@
 import NoSwiftmoduleHelper
 
-// The struct is resolved using type metadata and the Swift runtime.
-struct S { let i = 0 }
-
 func useTypeFromOtherModule(x: S2) {
   // break here
 }
 
-enum NoPayload {
-  case first
-  case second
-}
-
-enum WithPayload {
-  case empty
-  case with(i: Int)
-}
 
 func f<T>(_ t: T) {
-  let number = 1                     // CHECK-DAG: (Int) number {{=}} 1
-  let array = [1, 2, 3]              // CHECK-DAG: ([Int]) array {{=}} 3 values
-  let string = "hello"               // CHECK-DAG: (String) string {{=}} "hello"
-  let tuple = (0, 1)                 // CHECK-DAG: (Int, Int) tuple {{=}} (0 = 0, 1 = 1)
-  let strct = S()                    // CHECK-DAG: strct {{=}} (i = 0)
   let strct2 = S2()                  // CHECK-DAG: strct2 {{=}} {}{{$}}
-  let generic = t                    // CHECK-DAG: (Int) generic {{=}} 23
-  let generic_tuple = (t, t)         // CHECK-DAG: generic_tuple {{=}} (0 = 23, 1 = 23)
-  let word = 0._builtinWordValue     // CHECK-DAG: word {{=}} 0
-  let enum1 = NoPayload.second       // CHECK-DAG: enum1 {{=}}
-                                     // FIXME: Fails in swift::reflection::NoPayloadEnumTypeInfo::projectEnumValue: .second
-  let enum2 = WithPayload.with(i:42) // CHECK-DAG: enum2 {{=}} with
-                                     // CHECK-DAG: i {{=}} 42
-  print(number)
+  print(strct2)
   useTypeFromOtherModule(x: S2())
 }
 
