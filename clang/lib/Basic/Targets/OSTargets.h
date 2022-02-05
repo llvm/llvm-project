7846d590033e8d661198f4c00f56f46a4993c526
David Blaikie <dblaikie@gmail.com>
2022-02-04 20:06:13 -0800

    Extend the C++03 definition of POD to include defaulted functions
    The AST/conditionally-trivial-smfs tests look a bit questionable, but
    are consistent with GCC's POD-ness, at least as far as packing is
    concerned: https://godbolt.org/z/36nqPMbKM
    (questionable because it looks like the type would be non-copyable, so
    how could it be pod? But the calling convention/pass by value seems to
    work correctly (local testing verifies that this behavior is preserved
    even with this patch: https://godbolt.org/z/3Pa89zsv6 ))
    
    Differential Revision: https://reviews.llvm.org/D119051

--- clang/lib/Basic/Targets/OSTargets.h
+++ clang/lib/Basic/Targets/OSTargets.h
@@ -161,6 +161,10 @@ public:
                            : TargetInfo::UnsignedLongLong)
                : TargetInfo::getLeastIntTypeByWidth(BitWidth, IsSigned);
   }
+
+  bool areDefaultedSMFStillPOD(const LangOptions &) const override {
+    return false;
+  }
 };
 
 // DragonFlyBSD Target
@@ -557,6 +561,10 @@ public:
   TargetInfo::CallingConvCheckResult
   checkCallingConvention(CallingConv CC) const override {
     return (CC == CC_C) ? TargetInfo::CCCR_OK : TargetInfo::CCCR_Error;
+  }
+
+  bool areDefaultedSMFStillPOD(const LangOptions &) const override {
+    return false;
   }
 };
 
