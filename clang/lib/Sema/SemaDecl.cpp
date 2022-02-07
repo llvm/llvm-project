f5f0bd8e3d972d040827c88adf1a9880ebcb821e
James Y Knight <jyknight@google.com>
2022-02-07 11:31:22 -0500

    Revert "[Clang] Propagate guaranteed alignment for malloc and others"
    The above change assumed that malloc (and friends) would always
    allocate memory to getNewAlign(), even for allocations which have a
    smaller size. This is not actually required by spec (a 1-byte
    allocation may validly have 1-byte alignment).
    
    Some real-world malloc implementations do not provide this guarantee,
    and thus this optimization is breaking programs.
    
    Fixes #53540
    
    This reverts commit c2297544c04764237cedc523083c7be2fb3833d4.
    
    Differential Revision: https://reviews.llvm.org/D118804
    
    (cherry picked from commit 9545976ff160e19805a84a06a7e59d446f9994d9)

--- clang/lib/Sema/SemaDecl.cpp
+++ clang/lib/Sema/SemaDecl.cpp
@@ -15319,24 +15319,7 @@ void Sema::AddKnownFunctionAttributes(FunctionDecl *FD) {
       if (!FD->hasAttr<AllocAlignAttr>())
         FD->addAttr(AllocAlignAttr::CreateImplicit(Context, ParamIdx(1, FD),
                                                    FD->getLocation()));
-      LLVM_FALLTHROUGH;
-    case Builtin::BIcalloc:
-    case Builtin::BImalloc:
-    case Builtin::BImemalign:
-    case Builtin::BIrealloc:
-    case Builtin::BIstrdup:
-    case Builtin::BIstrndup: {
-      if (!FD->hasAttr<AssumeAlignedAttr>()) {
-        unsigned NewAlign = Context.getTargetInfo().getNewAlign() /
-                            Context.getTargetInfo().getCharWidth();
-        IntegerLiteral *Alignment = IntegerLiteral::Create(
-            Context, Context.MakeIntValue(NewAlign, Context.UnsignedIntTy),
-            Context.UnsignedIntTy, FD->getLocation());
-        FD->addAttr(AssumeAlignedAttr::CreateImplicit(
-            Context, Alignment, /*Offset=*/nullptr, FD->getLocation()));
-      }
       break;
-    }
     default:
       break;
     }
