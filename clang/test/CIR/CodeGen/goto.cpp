73e06c4f69bfb7857babbb1efa1d3c7afb1041bd
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2022-02-08 22:56:25 -0800

    [CIR] Support GotoStmt and LabelStmt
    - Added codegen bit for simple goto and label interaction (intra-scope only for now).
    - Introduced a lexical scope tracker, which among other things, allow us to track cleanup blocks.
    - Change the current way we handle returns.

---
+++ clang/test/CIR/CodeGen/goto.cpp
@@ -0,0 +1,32 @@
+// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fclangir -emit-cir %s -o %t.cir
+// RUN: FileCheck --input-file=%t.cir %s
+
+void g0(int a) {
+  int b = a;
+  goto end;
+  b = b + 1;
+end:
+  b = b + 2;
+}
+
+// CHECK:   func @g0
+// CHECK:     %0 = cir.alloca i32, cir.ptr <i32>, [cinit] {alignment = 4 : i64}
+// CHECK:     %1 = cir.alloca i32, cir.ptr <i32>, [paraminit] {alignment = 4 : i64}
+// CHECK:     cir.store %arg0, %1 : i32, cir.ptr <i32>
+// CHECK:     %2 = cir.load %1 lvalue_to_rvalue : cir.ptr <i32>, i32
+// CHECK:     cir.store %2, %0 : i32, cir.ptr <i32>
+// CHECK:     cir.br ^bb2
+// CHECK:   ^bb1:  // no predecessors
+// CHECK:     %3 = cir.load %0 lvalue_to_rvalue : cir.ptr <i32>, i32
+// CHECK:     %4 = cir.cst(1 : i32) : i32
+// CHECK:     %5 = cir.binop(add, %3, %4) : i32
+// CHECK:     cir.store %5, %0 : i32, cir.ptr <i32>
+// CHECK:     cir.br ^bb2
+// CHECK:   ^bb2:  // 2 preds: ^bb0, ^bb1
+// CHECK:     %6 = cir.load %0 lvalue_to_rvalue : cir.ptr <i32>, i32
+// CHECK:     %7 = cir.cst(2 : i32) : i32
+// CHECK:     %8 = cir.binop(add, %6, %7) : i32
+// CHECK:     cir.store %8, %0 : i32, cir.ptr <i32>
+// CHECK:     cir.br ^bb3
+// CHECK:   ^bb3:  // pred: ^bb2
+// CHECK:     cir.return
