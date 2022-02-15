a3386aa9de31001935d51052baad17a24a4c21a2
Siu Chi Chan <siuchi.chan@amd.com>
2022-02-15 14:44:12 -0500

    [clang-offload-bundler] add -input/-output options
    Currently, clang-offload-bundler has -inputs and -outputs options that accept values with comma as the delimiter. This causes issues with file paths containing commas, which are valid file paths on Linux.
    
    This add two new options -input and -output, which accept one single file, and allow multiple instances. This allows arbitrary file paths. The old -inputs and -outputs options will be kept for backward compatibility, but are not allowed to be used with -input and -output options for simplicity. In the future, -inputs and -outputs options will be phasing out.
    
    RFC: https://discourse.llvm.org/t/rfc-adding-input-and-output-options-to-clang-offload-bundler/60049
    
    Differential Revision: https://reviews.llvm.org/D120662
    
    Added adaptions to AMD's LLVM
    
    Change-Id: I3cb5f58a025cc9c1a40022e5651d48685a058709

--- clang/lib/Driver/ToolChains/Clang.cpp
+++ clang/lib/Driver/ToolChains/Clang.cpp
@@ -8093,8 +8093,10 @@ void OffloadBundler::ConstructJob(Compilation &C, const JobAction &JA,
   // The bundling command looks like this:
   // clang-offload-bundler -type=bc
   //   -targets=host-triple,openmp-triple1,openmp-triple2
-  //   -outputs=input_file
-  //   -inputs=unbundle_file_host,unbundle_file_tgt1,unbundle_file_tgt2"
+  //   -output=output_file
+  //   -input=unbundle_file_host
+  //   -input=unbundle_file_tgt1
+  //   -input=unbundle_file_tgt2
 
   ArgStringList CmdArgs;
 
@@ -8142,14 +8144,12 @@ void OffloadBundler::ConstructJob(Compilation &C, const JobAction &JA,
 
   // Get bundled file command.
   CmdArgs.push_back(
-      TCArgs.MakeArgString(Twine("-outputs=") + Output.getFilename()));
+      TCArgs.MakeArgString(Twine("-output=") + Output.getFilename()));
 
   // Get unbundled files command.
-  SmallString<128> UB;
-  UB += "-inputs=";
   for (unsigned I = 0; I < Inputs.size(); ++I) {
-    if (I)
-      UB += ',';
+    SmallString<128> UB;
+    UB += "-input=";
 
     // Find ToolChain for this input.
     const ToolChain *CurTC = &getToolChain();
@@ -8164,9 +8164,8 @@ void OffloadBundler::ConstructJob(Compilation &C, const JobAction &JA,
     } else {
       UB += CurTC->getInputFilename(Inputs[I]);
     }
+    CmdArgs.push_back(TCArgs.MakeArgString(UB));
   }
-  CmdArgs.push_back(TCArgs.MakeArgString(UB));
-
   // All the inputs are encoded as commands.
   C.addCommand(std::make_unique<Command>(
       JA, *this, ResponseFileSupport::None(),
@@ -8230,10 +8229,10 @@ static void createUnbundleArchiveCommand(Compilation &C,
 
       std::string UnbundleArg("-unbundle");
       std::string TypeArg("-type=a");
-      std::string InputArg("-inputs=");
+      std::string InputArg("-input=");
       InputArg += ArchiveOfBundles;
       std::string OffloadArg("-targets=" + std::string(DeviceTriple));
-      std::string OutputArg("-outputs=" + OutputLib);
+      std::string OutputArg("-output=" + OutputLib);
 
       ArgStringList UBArgs;
       UBArgs.push_back(C.getArgs().MakeArgString(UnbundleArg.c_str()));
@@ -8264,8 +8263,10 @@ void OffloadBundler::ConstructJobMultipleOutputs(
   // The unbundling command looks like this:
   // clang-offload-bundler -type=bc
   //   -targets=host-triple,openmp-triple1,openmp-triple2
-  //   -inputs=input_file
-  //   -outputs=unbundle_file_host,unbundle_file_tgt1,unbundle_file_tgt2"
+  //   -input=input_file
+  //   -output=unbundle_file_host
+  //   -output=unbundle_file_tgt1
+  //   -output=unbundle_file_tgt2
   //   -unbundle
 
   ArgStringList CmdArgs;
@@ -8313,17 +8314,15 @@ void OffloadBundler::ConstructJobMultipleOutputs(
 
   // Get bundled file command.
   CmdArgs.push_back(
-      TCArgs.MakeArgString(Twine("-inputs=") + Input.getFilename()));
+      TCArgs.MakeArgString(Twine("-input=") + Input.getFilename()));
 
   // Get unbundled files command.
-  SmallString<128> UB;
-  UB += "-outputs=";
   for (unsigned I = 0; I < Outputs.size(); ++I) {
-    if (I)
-      UB += ',';
+    SmallString<128> UB;
+    UB += "-output=";
     UB += DepInfo[I].DependentToolChain->getInputFilename(Outputs[I]);
+    CmdArgs.push_back(TCArgs.MakeArgString(UB));
   }
-  CmdArgs.push_back(TCArgs.MakeArgString(UB));
   CmdArgs.push_back("-unbundle");
   CmdArgs.push_back("-allow-missing-bundles");
 
