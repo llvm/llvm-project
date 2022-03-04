de1f8c6568ee3efb1189512a68493750567ca256
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2022-03-03 20:50:01 -0800

    [CIR] Fix source location of plain cir.scope's

--- clang/test/CIR/CodeGen/basic.cpp
+++ clang/test/CIR/CodeGen/basic.cpp
@@ -45,7 +45,7 @@ int *p2() {
 // CHECK-NEXT:      %7 = cir.cst(42 : i32) : i32
 // CHECK-NEXT:      %8 = cir.load deref %0 : cir.ptr <!cir.ptr<i32>>, !cir.ptr<i32>
 // CHECK-NEXT:      cir.store %7, %8 : i32, cir.ptr <i32>
-// CHECK-NEXT:    }
+// CHECK-NEXT:    } loc(#loc15)
 // CHECK-NEXT:    %2 = cir.cst(42 : i32) : i32
 // CHECK-NEXT:    %3 = cir.load deref %0 : cir.ptr <!cir.ptr<i32>>, !cir.ptr<i32>
 // CHECK-NEXT:    cir.store %2, %3 : i32, cir.ptr <i32>
@@ -131,3 +131,5 @@ int if1(int a, bool b, bool c) {
 // CHECK:     cir.store %8, %3 : i32, cir.ptr <i32>
 // CHECK:   }
 // CHECK: }
+
+// CHECK: #loc15 = loc(fused["{{.*}}basic.cpp":26:3, "{{.*}}basic.cpp":30:3])
