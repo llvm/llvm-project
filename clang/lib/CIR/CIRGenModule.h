e674bb870cc8dece7798664583a52c60568fee68
Nathan Lanza <nathan@lanza.io>
2022-03-08 12:13:10 -0500

    [CIR] Add a CIRGenCXXABI member and getter to CIRGenModule

--- clang/lib/CIR/CIRGenModule.h
+++ clang/lib/CIR/CIRGenModule.h
@@ -34,6 +34,8 @@
 
 namespace cir {
 
+class CIRGenCXXABI;
+
 /// Implementation of a CIR/MLIR emission from Clang AST.
 ///
 /// This will emit operations that are specific to C(++)/ObjC(++) language,
@@ -47,7 +49,7 @@ public:
   CIRGenModule(mlir::MLIRContext &context, clang::ASTContext &astctx,
                const clang::CodeGenOptions &CGO);
 
-  ~CIRGenModule() = default;
+  ~CIRGenModule();
 
   using SymTableTy = llvm::ScopedHashTable<const clang::Decl *, mlir::Value>;
   using SymTableScopeTy =
@@ -70,6 +72,8 @@ private:
   mlir::ModuleOp theModule;
 
   const clang::TargetInfo &target;
+
+  std::unique_ptr<CIRGenCXXABI> ABI;
   /// Per-module type mapping from clang AST to CIR.
   CIRGenTypes genTypes;
 
@@ -208,6 +212,8 @@ public:
   const clang::CodeGenOptions &getCodeGenOpts() const { return codeGenOpts; }
   CIRGenTypes &getTypes() { return genTypes; }
   const clang::LangOptions &getLangOpts() const { return langOpts; }
+
+  CIRGenCXXABI &getCXXABI() const { return *ABI; }
 
   /// Helpers to convert Clang's SourceLocation to a MLIR Location.
   mlir::Location getLoc(clang::SourceLocation SLoc);
