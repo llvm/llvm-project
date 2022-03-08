d859de293d0c4249151d04711de34a0e96cdb0ea
Nathan Lanza <nathan@lanza.io>
2022-03-08 12:14:51 -0500

    [CIR] Import TargetInfo.h header from CodeGen

---
+++ clang/lib/CIR/TargetInfo.h
@@ -0,0 +1,36 @@
+//===---- TargetInfo.h - Encapsulate target details -------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// These classes wrap the information about a call or function
+// definition used to handle ABI compliancy.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_CLANG_LIB_CIR_TARGETINFO_H
+#define LLVM_CLANG_LIB_CIR_TARGETINFO_H
+
+#include <memory>
+
+namespace cir {
+class ABIInfo;
+
+/// TargetCIRGenInfo - This class organizes various target-specific
+/// codegeneration issues, like target-specific attributes, builtins and so on.
+class TargetCIRGenInfo {
+  std::unique_ptr<ABIInfo> Info = nullptr;
+
+public:
+  TargetCIRGenInfo(std::unique_ptr<ABIInfo> Info) : Info(std::move(Info)) {}
+
+  /// getABIInfo() - Returns ABI info helper for the target.
+  const ABIInfo &getABIInfo() const { return *Info; }
+};
+
+} // namespace cir
+
+#endif
