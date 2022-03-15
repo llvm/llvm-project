e969360e74772fd169756bfa7921892b893c8a5d
Nathan Lanza <nathan@lanza.io>
2022-03-14 20:28:41 -0400

    [CIR] Add test for call gen

---
+++ clang/test/CIR/CodeGen/call.c
@@ -0,0 +1,19 @@
+// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fclangir -emit-cir %s -o %t.cir
+// RUN: FileCheck --input-file=%t.cir %s
+// XFAIL: *
+
+void a(void) {}
+
+void c(void) {
+  a();
+}
+
+// CHECK: module  {
+// CHECK:   func @a() {
+// CHECK:     cir.return
+// CHECK:   }
+// CHECK:   func @c() {
+// CHECK:     call @a() : () -> ()
+// CHECK:     cir.return
+// CHECK:   }
+
