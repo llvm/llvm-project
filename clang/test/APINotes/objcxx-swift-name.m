c49918f5d77a748f3bb0922a10c99e968a74c9f2
Puyan Lotfi <plotfi@fb.com>
2022-03-14 23:15:25 -0400

    [c++-interop] Teach APINotes to handle DeclContexts that are extern "C"
    In order for APINotes to behave correctly in the presense of Swift's
    C++-Interop, it needs to not assume the top-level DeclContext of a file (ie
    isFileConext) is only either a TU-Decl or a Namespace: it can be a
    LinkageSpecDecl (ie an extern "C").
    
    This change simply allows for APINotes do follow its usual behavior if the
    context of the Decl is within an extern "C".
    
    A real world application of this is something like NSURLSession which resides
    in a module.modulemap that has a leading `module Foundation [extern_c] {` and
    because of this all of the types inside have a LinkageSpecDecl context. When
    APINotes is unable to apply the mapping then a type like NSURLSession misses
    it's mapping to URLSession and everything that may refer to it in the module
    will fail to compile due to a missing type.
    
    (cherry picked from commit 56e3a3b2b66f75489131bf45b92b681cf7f3be5a)

---
+++ clang/test/APINotes/objcxx-swift-name.m
@@ -0,0 +1,9 @@
+// RUN: rm -rf %t && mkdir -p %t
+// RUN: %clang_cc1 -fmodules -fblocks -fimplicit-module-maps -fmodules-cache-path=%t/ModulesCache/CxxInterop -fdisable-module-hash -fapinotes-modules -fsyntax-only -I %S/Inputs/Headers -F %S/Inputs/Frameworks %s -x objective-c++
+// RUN: %clang_cc1 -fmodules -fblocks -fimplicit-module-maps -fmodules-cache-path=%t/ModulesCache/CxxInterop -fdisable-module-hash -fapinotes-modules -fsyntax-only -I %S/Inputs/Headers -F %S/Inputs/Frameworks %s -ast-dump -ast-dump-filter SomeClass -x objective-c++ | FileCheck %s
+
+#import <CxxInteropKit/CxxInteropKit.h>
+
+// CHECK: Dumping NSSomeClass:
+// CHECK-NEXT: ObjCInterfaceDecl {{.+}} imported in CxxInteropKit <undeserialized declarations> NSSomeClass
+// CHECK-NEXT: SwiftNameAttr {{.+}} <<invalid sloc>> "SomeClass"
