7c4c74bf101ecd4a4d5c089e23d802d7df3e8208
Nathan Lanza <nathan@lanza.io>
2022-03-15 22:39:27 -0400

    [CIR] Support return types in buildCall

--- clang/test/CIR/CodeGen/call.c
+++ clang/test/CIR/CodeGen/call.c
@@ -3,7 +3,9 @@
 // XFAIL: *
 
 void a(void) {}
-void b(int a, int b) {}
+int b(int a, int b) {
+  return a + b;
+}
 
 void c(void) {
   a();
@@ -14,17 +16,23 @@ void c(void) {
 // CHECK:   func @a() {
 // CHECK:     cir.return
 // CHECK:   }
-// CHECK:   func @b(%arg0: i32 {{.*}} {
+// CHECK:   func @b(%arg0: i32 {{.*}}, %arg1: i32 {{.*}}) -> i32 {
 // CHECK:     %0 = cir.alloca i32, cir.ptr <i32>, ["a", paraminit]
 // CHECK:     %1 = cir.alloca i32, cir.ptr <i32>, ["b", paraminit]
+// CHECK:     %2 = cir.alloca i32, cir.ptr <i32>, ["__retval", uninitialized]
 // CHECK:     cir.store %arg0, %0 : i32, cir.ptr <i32>
 // CHECK:     cir.store %arg1, %1 : i32, cir.ptr <i32>
-// CHECK:     cir.return
+// CHECK:     %3 = cir.load %0 : cir.ptr <i32>, i32
+// CHECK:     %4 = cir.load %1 : cir.ptr <i32>, i32
+// CHECK:     %5 = cir.binop(add, %3, %4) : i32
+// CHECK:     cir.store %5, %2 : i32, cir.ptr <i32>
+// CHECK:     %6 = cir.load %2 : cir.ptr <i32>, i32
+// CHECK:     cir.return %6
 // CHECK:   }
 // CHECK:   func @c() {
 // CHECK:     call @a() : () -> ()
 // CHECK:     %0 = cir.cst(0 : i32) : i32
 // CHECK:     %1 = cir.cst(1 : i32) : i32
-// CHECK:     call @b(%0, %1) : (i32, i32) -> ()
+// CHECK:     call @b(%0, %1) : (i32, i32) -> i32
 // CHECK:     cir.return
 // CHECK:   }
