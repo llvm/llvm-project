03fdd9cdcdb71ab442f00b04ae2755a24bbd9ddc
Puyan Lotfi <plotfi@fb.com>
2022-03-23 22:13:49 -0400

    [c++-interop] Test that APINotes handles ObjCMethods while in C++-Interop
    In a previous commit I taught APINotes to treat extern-c context as a
    toplevel file context, but this only makes sense when we are handling
    the decls handled in the block like VarDecls, FunctionDecls,
    ObjCInterfaceDecls, etc.
    
    But when the decl being handled for APINote is an ObjCMethod inside of a
    ObjCContainer then it is important to allow the subsequent code in the
    Sema::ProcessAPINotes function to handle things.
    
    This can be excercised by the following APINote:
    
    ```
    ---
    Name: SomeModule
    Classes:
    - Name: NSSomeClass
      SwiftName: SomeClass
      Methods:
      - Selector: 'didMoveToParentViewController:'
        SwiftName: didMove(toParent:)
        MethodKind: Instance
    ```
    
    This commit is now just a test case due to another commit that has
    already landed.
    
    (cherry picked from commit 4416d1515e59451bf01bbf5fd68e78cf921b1411)

--- clang/test/APINotes/Inputs/Frameworks/CXXInteropKit.framework/Headers/CXXInteropKit.h
+++ clang/test/APINotes/Inputs/Frameworks/CXXInteropKit.framework/Headers/CXXInteropKit.h
@@ -2,5 +2,12 @@
   -(instancetype)init;
 @end
 
+// Extension, inspired by UIKit UIViewController.h
+@interface NSSomeClass (UIContainerViewControllerCallbacks)
+
+- (void)didMoveToParentViewController:(NSSomeClass *)parent;
+
+@end
+
 // Named "SomeClassRed" for ast node filtering in the test.
 enum ColorEnum { SomeClassRed, SomeClassGreen, SomeClassBlue };
