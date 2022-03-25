825d7e5bcd8399cf4fd796f44f078e2cfef93feb
Carlo Bertolli <carlo.bertolli@amd.com>
2022-03-25 13:24:01 -0500

    Specialize assert_assume not to use clang builtin_assume on amdgcn due to runtime "abort"
    Change-Id: I7c6a0ea062a4d88ac165669ca44d22bc0c0ac9c6

--- openmp/libomptarget/DeviceRTL/src/Debug.cpp
+++ openmp/libomptarget/DeviceRTL/src/Debug.cpp
@@ -21,7 +21,30 @@ using namespace _OMP;
 #pragma omp declare target
 
 extern "C" {
-void __assert_assume(bool condition) { __builtin_assume(condition); }
+
+/// AMDGCN Implementation
+///
+///{
+#pragma omp begin declare variant match(device = {arch(amdgcn)})
+namespace impl {
+void omp_assert_assume(bool condition) {}
+}
+#pragma omp end declare variant
+///}
+
+/// NVPTX Implementation
+///
+///{
+
+#pragma omp begin declare variant match(                                       \
+    device = {arch(nvptx, nvptx64)}, implementation = {extension(match_any)})
+namespace impl {
+void omp_assert_assume(bool condition) { __builtin_assume(condition); }
+}
+#pragma omp end declare variant
+///}
+
+void __assert_assume(bool condition) { impl::omp_assert_assume(condition); }
 
 void __assert_fail(const char *assertion, const char *file, unsigned line,
                    const char *function) {
