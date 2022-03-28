1007cb795a3c82eab231b58569ea06a2b613636a
Fangrui Song <i@maskray.me>
2022-03-28 01:00:43 -0700

    [llvm-objdump] --private-headers: change errors to warnings for dynamic section dumping
    Fix #54456: `objcopy --only-keep-debug` produces a linked image with invalid
    empty dynamic section. llvm-objdump -p currently reports an error which seems
    excessive.
    
    ```
    % llvm-readelf -l a.out
    llvm-readelf: warning: 'a.out': no valid dynamic table was found
    ...
    ```
    
    Follow the spirit of llvm-readelf -l (D64472) and report a warning instead.
    This allows later files to be dumped despite warnings for an input file, and
    improves objdump compatibility in that the exit code is now 0 instead of 1.
    
    ```
    % llvm-objdump -p a.out  # new behavior
    ...
    Program Header:
    llvm-objdump: warning: 'a.out': invalid empty dynamic section
    % objdump -p a.out
    ...
    Dynamic Section:
    
    ```
    
    Reviewed By: jhenderson, raj.khem
    
    Differential Revision: https://reviews.llvm.org/D122505
    
    (cherry picked from commit 11a8fc685692f56b011f851d974f0cac534f2cb8)

--- llvm/lib/Object/ELF.cpp
+++ llvm/lib/Object/ELF.cpp
@@ -561,11 +561,9 @@ Expected<typename ELFT::DynRange> ELFFile<ELFT>::dynamicEntries() const {
   }
 
   if (Dyn.empty())
-    // TODO: this error is untested.
     return createError("invalid empty dynamic section");
 
   if (Dyn.back().d_tag != ELF::DT_NULL)
-    // TODO: this error is untested.
     return createError("dynamic sections must be DT_NULL terminated");
 
   return Dyn;
