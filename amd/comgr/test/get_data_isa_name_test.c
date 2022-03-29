881a2b1f7589aa775982a3aa0d9492ef0676ba23
Stanislav Mekhanoshin <Stanislav.Mekhanoshin@amd.com>
2022-03-29 13:24:38 -0700

    Add gfx940 target
    Change-Id: If2d4b35b457b1aae38e65aed53b70795a2224b7d

--- amd/comgr/test/get_data_isa_name_test.c
+++ amd/comgr/test/get_data_isa_name_test.c
@@ -84,6 +84,7 @@ static isa_features_t IsaFeatures[] = {
   {"amdgcn-amd-amdhsa--gfx909",  false,     false,     none,       true,      none},
   {"amdgcn-amd-amdhsa--gfx90a",  false,     true,      none,       true,      none},
   {"amdgcn-amd-amdhsa--gfx90c",  true,      false,     none,       true,      off},
+  {"amdgcn-amd-amdhsa--gfx940",  false,     true,      none,       true,      none},
   {"amdgcn-amd-amdhsa--gfx1010", false,     false,     none,       true,      none},
   {"amdgcn-amd-amdhsa--gfx1011", false,     false,     none,       true,      none},
   {"amdgcn-amd-amdhsa--gfx1012", false,     false,     none,       true,      none},
