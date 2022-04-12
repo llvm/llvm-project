55b595a300f7198391631632773aba56b7510623
Bing Ma <bing.ma@amd.com>
2022-04-12 09:39:08 -0700

    SWDEV-332049 - Check filename for nullptr when intercepting dlopen
    Change-Id: I8c3cd8f23fee373a5c2b3765beb1577e011af006

--- compiler-rt/lib/sanitizer_common/sanitizer_linux.cpp
+++ compiler-rt/lib/sanitizer_common/sanitizer_linux.cpp
@@ -2263,9 +2263,8 @@ void CheckNoDeepBind(const char *filename, int flag) {
 
 #if SANITIZER_AMDGPU
 void PatchHsaRuntimeDlopenFlag(const char *filename, int &flag) {
-  if ((internal_strstr(filename, "libhsa-runtime64.so") ||
-       internal_strstr(filename, "libamdocl64.so")) &&
-     !(flag & RTLD_GLOBAL)) {
+  if (filename && (internal_strstr(filename, "libhsa-runtime64.so") ||
+      internal_strstr(filename, "libamdocl64.so")) && !(flag & RTLD_GLOBAL)) {
     flag |= RTLD_GLOBAL;
     if (Verbosity() >= 2) {
       Printf(
