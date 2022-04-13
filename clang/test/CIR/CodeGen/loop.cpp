cf64288cf8b3b382edd8afdd838013b4149ba6f8
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2022-04-13 15:22:29 -0700

    [CIR][CodeGen] Add support for WhileStmt

--- clang/test/CIR/CodeGen/loop.cpp
+++ clang/test/CIR/CodeGen/loop.cpp
@@ -43,3 +43,61 @@ void l1() {
 // CHECK-NEXT:   cir.store %6, %0 : i32, cir.ptr <i32>
 // CHECK-NEXT:   cir.yield
 // CHECK-NEXT: }
+
+void l2(bool cond) {
+  int i = 0;
+  while (cond) {
+    i = i + 1;
+  }
+  while (true) {
+    i = i + 1;
+  }
+  while (1) {
+    i = i + 1;
+  }
+}
+
+// CHECK: func @l2
+// CHECK:         cir.scope {
+// CHECK-NEXT:     cir.loop(cond :  {
+// CHECK-NEXT:       %3 = cir.load %0 : cir.ptr <!cir.bool>, !cir.bool
+// CHECK-NEXT:       cir.yield loopcondition %3 : !cir.bool
+// CHECK-NEXT:     }, step :  {
+// CHECK-NEXT:       cir.yield
+// CHECK-NEXT:     })  {
+// CHECK-NEXT:       %3 = cir.load %1 : cir.ptr <i32>, i32
+// CHECK-NEXT:       %4 = cir.cst(1 : i32) : i32
+// CHECK-NEXT:       %5 = cir.binop(add, %3, %4) : i32
+// CHECK-NEXT:       cir.store %5, %1 : i32, cir.ptr <i32>
+// CHECK-NEXT:       cir.yield
+// CHECK-NEXT:     }
+// CHECK-NEXT:   }
+// CHECK-NEXT:   cir.scope {
+// CHECK-NEXT:     cir.loop(cond :  {
+// CHECK-NEXT:       %3 = cir.cst(true) : !cir.bool
+// CHECK-NEXT:       cir.yield loopcondition %3 : !cir.bool
+// CHECK-NEXT:     }, step :  {
+// CHECK-NEXT:       cir.yield
+// CHECK-NEXT:     })  {
+// CHECK-NEXT:       %3 = cir.load %1 : cir.ptr <i32>, i32
+// CHECK-NEXT:       %4 = cir.cst(1 : i32) : i32
+// CHECK-NEXT:       %5 = cir.binop(add, %3, %4) : i32
+// CHECK-NEXT:       cir.store %5, %1 : i32, cir.ptr <i32>
+// CHECK-NEXT:       cir.yield
+// CHECK-NEXT:     }
+// CHECK-NEXT:   }
+// CHECK-NEXT:   cir.scope {
+// CHECK-NEXT:     cir.loop(cond :  {
+// CHECK-NEXT:       %3 = cir.cst(1 : i32) : i32
+// CHECK-NEXT:       %4 = cir.cast(int_to_bool, %3 : i32), !cir.bool
+// CHECK-NEXT:       cir.yield loopcondition %4 : !cir.bool
+// CHECK-NEXT:     }, step :  {
+// CHECK-NEXT:       cir.yield
+// CHECK-NEXT:     })  {
+// CHECK-NEXT:       %3 = cir.load %1 : cir.ptr <i32>, i32
+// CHECK-NEXT:       %4 = cir.cst(1 : i32) : i32
+// CHECK-NEXT:       %5 = cir.binop(add, %3, %4) : i32
+// CHECK-NEXT:       cir.store %5, %1 : i32, cir.ptr <i32>
+// CHECK-NEXT:       cir.yield
+// CHECK-NEXT:     }
+// CHECK-NEXT:   }
