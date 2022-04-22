431b9cac987921a5572788888edb6d4ecd5a3331
Nathan Lanza <nathan@lanza.io>
2022-04-22 19:07:06 -0400

    [CIR] Add a ModuleNameHash to CGM to assert against for the UniqueInternalLinkageNames argument

--- clang/lib/CIR/CIRGenModule.h
+++ clang/lib/CIR/CIRGenModule.h
@@ -57,6 +57,8 @@ public:
 
   ~CIRGenModule();
 
+  const std::string &getModuleNameHash() const { return ModuleNameHash; }
+
 private:
   mutable std::unique_ptr<TargetCIRGenInfo> TheTargetCIRGenInfo;
 
@@ -80,6 +82,9 @@ private:
   const clang::TargetInfo &target;
 
   std::unique_ptr<CIRGenCXXABI> ABI;
+
+  /// Used for `UniqueInternalLinkageNames` option
+  std::string ModuleNameHash = "";
 
   /// Per-module type mapping from clang AST to CIR.
   CIRGenTypes genTypes;
