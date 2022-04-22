8d898ff14580603eb94e0467d422d8c2eba96069
Nathan Lanza <nathan@lanza.io>
2022-04-22 19:08:49 -0400

    [CIR] Opt into mangling and fix all tests
    This becomes relevant shortly as we attempt to mangle complete and base
    ctors which will collide without turning this on.

--- clang/test/CIR/CodeGen/struct.cpp
+++ clang/test/CIR/CodeGen/struct.cpp
@@ -20,7 +20,7 @@ void baz() {
 //      CHECK: !_22struct2EBar22 = !cir.struct<"struct.Bar", i32, i8>
 // CHECK-NEXT: !_22struct2EFoo22 = !cir.struct<"struct.Foo", i32, i8, !_22struct2EBar22>
 // CHECK-NEXT: module  {
-// CHECK-NEXT:   func @baz() {
+// CHECK-NEXT:   func @_Z3bazv() {
 // CHECK-NEXT:     %0 = cir.alloca !_22struct2EBar22, cir.ptr <!_22struct2EBar22>, ["b", uninitialized] {alignment = 4 : i64}
 // CHECK-NEXT:     %1 = cir.alloca !_22struct2EFoo22, cir.ptr <!_22struct2EFoo22>, ["f", uninitialized] {alignment = 4 : i64}
 // CHECK-NEXT:     cir.return
