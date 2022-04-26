5869d83a788ec3d6e4d8f064def624d473888b53
Dave Lee <davelee.com@gmail.com>
2022-04-26 16:34:10 -0700

    [lldb] Add setting for max depth of value object printing (NFC)
    This adds a setting (`target.max-children-depth`) that will provide a default value for the `--depth` flag used by `expression` and `frame variable`.
    
    The new setting uses the same default that's currently fixed in source: `UINT32_MAX`.
    
    This provides two purposes:
    
    1. Allowing downstream forks to provide a customized default.
    2. Allowing users to set their own default.
    
    Following `target.max-children-count`, a warning is emitted when the max depth is reached. The warning lets users know which flags or settings they can customize. This warning is shown only when the limit is the default value.
    
    rdar://87466495
    
    Differential Revision: https://reviews.llvm.org/D123954
    
    (cherry picked from commit 46db6c556e5272835672da5dc3ad0051dda4554f)

--- lldb/include/lldb/DataFormatters/DumpValueObjectOptions.h
+++ lldb/include/lldb/DataFormatters/DumpValueObjectOptions.h
@@ -69,7 +69,7 @@ public:
   DumpValueObjectOptions &
   SetMaximumPointerDepth(PointerDepth depth = {PointerDepth::Mode::Never, 0});
 
-  DumpValueObjectOptions &SetMaximumDepth(uint32_t depth = 0);
+  DumpValueObjectOptions &SetMaximumDepth(uint32_t depth, bool is_default);
 
   DumpValueObjectOptions &SetDeclPrintingHelper(DeclPrintingHelper helper);
 
@@ -128,6 +128,7 @@ public:
   SetPointerAsArray(const PointerAsArraySettings &ptr_array);
 
   uint32_t m_max_depth = UINT32_MAX;
+  bool m_max_depth_is_default = true;
   lldb::DynamicValueType m_use_dynamic = lldb::eNoDynamicValues;
   uint32_t m_omit_summary_depth = 0;
   lldb::Format m_format = lldb::eFormatDefault;
