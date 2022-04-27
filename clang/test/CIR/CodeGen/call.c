ad597124eec9f121b2d470051ac5f03d91cf8b22
Nathan Lanza <nathan@lanza.io>
2022-04-27 02:02:40 -0400

    [CIR] Add C++ test to call.c

--- clang/test/CIR/CodeGen/call.c
+++ clang/test/CIR/CodeGen/call.c
@@ -1,5 +1,5 @@
-// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fclangir -emit-cir %s -o %t.cir
-// RUN: FileCheck --input-file=%t.cir %s
+// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fclangir -emit-cir %s -o - | FileCheck %s
+// RUN: %clang_cc1 -x c++ -triple x86_64-unknown-linux-gnu -fclangir -emit-cir %s -o - | FileCheck %s --check-prefix=CXX
 // XFAIL: *
 
 void a(void) {}
@@ -52,3 +52,41 @@ void d(void) {
 // CHECK:     call @b(%0, %1) : (i32, i32) -> i32
 // CHECK:     cir.return
 // CHECK:   }
+//
+// CXX: module  {
+// CXX-NEXT:   func @_Z1av() {
+// CXX-NEXT:     cir.return
+// CXX-NEXT:   }
+// CXX-NEXT:   func @_Z1bii(%arg0: i32 {{.*}}, %arg1: i32 {{.*}}) -> i32 {
+// CXX-NEXT:     %0 = cir.alloca i32, cir.ptr <i32>, ["a", paraminit]
+// CXX-NEXT:     %1 = cir.alloca i32, cir.ptr <i32>, ["b", paraminit]
+// CXX-NEXT:     %2 = cir.alloca i32, cir.ptr <i32>, ["__retval", uninitialized]
+// CXX-NEXT:     cir.store %arg0, %0 : i32, cir.ptr <i32>
+// CXX-NEXT:     cir.store %arg1, %1 : i32, cir.ptr <i32>
+// CXX-NEXT:     %3 = cir.load %0 : cir.ptr <i32>, i32
+// CXX-NEXT:     %4 = cir.load %1 : cir.ptr <i32>, i32
+// CXX-NEXT:     %5 = cir.binop(add, %3, %4) : i32
+// CXX-NEXT:     cir.store %5, %2 : i32, cir.ptr <i32>
+// CXX-NEXT:     %6 = cir.load %2 : cir.ptr <i32>, i32
+// CXX-NEXT:     cir.return %6
+// CXX-NEXT:   }
+// CXX-NEXT:   func @_Z1cdd(%arg0: f64 {{.*}}, %arg1: f64 {{.*}}) -> f64 {
+// CXX-NEXT:     %0 = cir.alloca f64, cir.ptr <f64>, ["a", paraminit]
+// CXX-NEXT:     %1 = cir.alloca f64, cir.ptr <f64>, ["b", paraminit]
+// CXX-NEXT:     %2 = cir.alloca f64, cir.ptr <f64>, ["__retval", uninitialized]
+// CXX-NEXT:     cir.store %arg0, %0 : f64, cir.ptr <f64>
+// CXX-NEXT:     cir.store %arg1, %1 : f64, cir.ptr <f64>
+// CXX-NEXT:     %3 = cir.load %0 : cir.ptr <f64>, f64
+// CXX-NEXT:     %4 = cir.load %1 : cir.ptr <f64>, f64
+// CXX-NEXT:     %5 = cir.binop(add, %3, %4) : f64
+// CXX-NEXT:     cir.store %5, %2 : f64, cir.ptr <f64>
+// CXX-NEXT:     %6 = cir.load %2 : cir.ptr <f64>, f64
+// CXX-NEXT:     cir.return %6 : f64
+// CXX-NEXT:   }
+// CXX-NEXT:   func @_Z1dv() {
+// CXX-NEXT:     call @_Z1av() : () -> ()
+// CXX-NEXT:     %0 = cir.cst(0 : i32) : i32
+// CXX-NEXT:     %1 = cir.cst(1 : i32) : i32
+// CXX-NEXT:     call @_Z1bii(%0, %1) : (i32, i32) -> i32
+// CXX-NEXT:     cir.return
+// CXX-NEXT:   }
