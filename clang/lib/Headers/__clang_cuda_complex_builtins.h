c4f1e4110732e7fc93df5fceae3e3358a509b6b1
gregrodgers <Gregory.Rodgers@amd.com>
2022-05-02 12:52:58 -0500

    This fix is already upstream. This gets the correct _nv_names instead of the _ocml_ names for all nvptx archs
    Change-Id: I7e2f6151c2adf1e532dfb459d630da52bfa43477

--- clang/lib/Headers/__clang_cuda_complex_builtins.h
+++ clang/lib/Headers/__clang_cuda_complex_builtins.h
@@ -63,25 +63,6 @@
 #define _fmaxd __ocml_fmax_f64
 #define _fmaxf __ocml_fmax_f32
 #else
-#include <__clang_hip_libdevice_declares.h>
-#define _ISNANd __ocml_isnan_f64
-#define _ISNANf __ocml_isnan_f32
-#define _ISINFd __ocml_isinf_f64
-#define _ISINFf __ocml_isinf_f32
-#define _ISFINITEd __ocml_isfinite_f64
-#define _ISFINITEf __ocml_isfinite_f32
-#define _COPYSIGNd __ocml_copysign_f64
-#define _COPYSIGNf __ocml_copysign_f32
-#define _SCALBNd __ocml_scalbn_f64
-#define _SCALBNf __ocml_scalbn_f32
-#define _ABSd __ocml_fabs_f64
-#define _ABSf __ocml_fabs_f32
-#define _LOGBd __ocml_logb_f64
-#define _LOGBf __ocml_logb_f32
-#define _fmaxd __ocml_fmax_f64
-#define _fmaxf __ocml_fmax_f32
-#endif
-#ifdef __NVPTX__
 #define _ISNANd __nv_isnand
 #define _ISNANf __nv_isnanf
 #define _ISINFd __nv_isinfd
