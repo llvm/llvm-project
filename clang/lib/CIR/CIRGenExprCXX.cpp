97c7ca60813f91e71ef13e6ab4e2375fa396e7bd
Nathan Lanza <nathan@lanza.io>
2022-05-02 22:10:24 -0400

    [CIR][NFC] Move a fn to CIRGenExprCXX to match CodeGen

--- clang/lib/CIR/CIRGenExprCXX.cpp
+++ clang/lib/CIR/CIRGenExprCXX.cpp
@@ -14,6 +14,7 @@
 #include <CIRGenFunction.h>
 #include <CIRGenModule.h>
 #include <CIRGenValue.h>
+#include <UnimplementedFeatureGuarding.h>
 
 #include <clang/AST/DeclCXX.h>
 
@@ -83,4 +84,128 @@ RValue CIRGenFunction::buildCXXMemberOrOperatorCall(
   return buildCall(FnInfo, Callee, ReturnValue, Args, nullptr,
                    CE && CE == MustTailCall,
                    CE ? CE->getExprLoc() : SourceLocation());
+}
+
+RValue CIRGenFunction::buildCXXMemberOrOperatorMemberCallExpr(
+    const CallExpr *CE, const CXXMethodDecl *MD, ReturnValueSlot ReturnValue,
+    bool HasQualifier, NestedNameSpecifier *Qualifier, bool IsArrow,
+    const Expr *Base) {
+  assert(isa<CXXMemberCallExpr>(CE) || isa<CXXOperatorCallExpr>(CE));
+
+  // Compute the object pointer.
+  bool CanUseVirtualCall = MD->isVirtual() && !HasQualifier;
+  assert(!CanUseVirtualCall && "NYI");
+
+  const CXXMethodDecl *DevirtualizedMethod = nullptr;
+  if (CanUseVirtualCall &&
+      MD->getDevirtualizedMethod(Base, getLangOpts().AppleKext)) {
+    llvm_unreachable("NYI");
+  }
+
+  bool TrivialForCodegen =
+      MD->isTrivial() || (MD->isDefaulted() && MD->getParent()->isUnion());
+  bool TrivialAssignment =
+      TrivialForCodegen &&
+      (MD->isCopyAssignmentOperator() || MD->isMoveAssignmentOperator()) &&
+      !MD->getParent()->mayInsertExtraPadding();
+  (void)TrivialAssignment;
+
+  // C++17 demands that we evaluate the RHS of a (possibly-compound) assignment
+  // operator before the LHS.
+  CallArgList RtlArgStorage;
+  CallArgList *RtlArgs = nullptr;
+  LValue TrivialAssignmentRHS;
+  if (auto *OCE = dyn_cast<CXXOperatorCallExpr>(CE)) {
+    llvm_unreachable("NYI");
+  }
+
+  LValue This;
+  if (IsArrow) {
+    llvm_unreachable("NYI");
+  } else {
+    This = buildLValue(Base);
+  }
+
+  if (const CXXConstructorDecl *Ctor = dyn_cast<CXXConstructorDecl>(MD)) {
+    llvm_unreachable("NYI");
+  }
+
+  if (TrivialForCodegen) {
+    llvm_unreachable("NYI");
+  }
+
+  // Compute the function type we're calling
+  const CXXMethodDecl *CalleeDecl =
+      DevirtualizedMethod ? DevirtualizedMethod : MD;
+  const CIRGenFunctionInfo *FInfo = nullptr;
+  if (const auto *Dtor = dyn_cast<CXXDestructorDecl>(CalleeDecl))
+    llvm_unreachable("NYI");
+  else
+    llvm_unreachable("NYI");
+  // FInfo = &CGM.getTypes().arrangeCXXMethodDeclaration(CalleeDecl);
+
+  mlir::FunctionType Ty = CGM.getTypes().GetFunctionType(*FInfo);
+
+  // C++11 [class.mfct.non-static]p2:
+  //   If a non-static member function of a class X is called for an object that
+  //   is not of type X, or of a type derived from X, the behavior is undefined.
+  SourceLocation CallLoc;
+  ASTContext &C = getContext();
+  (void)C;
+  if (CE)
+    CallLoc = CE->getExprLoc();
+
+  SanitizerSet SkippedChecks;
+  if (const auto *cmce = dyn_cast<CXXMemberCallExpr>(CE)) {
+    auto *ioa = cmce->getImplicitObjectArgument();
+    auto isImplicitObjectCXXThis = isWrappedCXXThis(ioa);
+    if (isImplicitObjectCXXThis)
+      SkippedChecks.set(SanitizerKind::Alignment, true);
+    if (isImplicitObjectCXXThis || isa<DeclRefExpr>(ioa))
+      SkippedChecks.set(SanitizerKind::Null, true);
+  }
+
+  if (UnimplementedFeature::buildTypeCheck())
+    llvm_unreachable("NYI");
+
+  // C++ [class.virtual]p12:
+  //   Explicit qualification with the scope operator (5.1) suppresses the
+  //   virtual call mechanism.
+  //
+  // We also don't emit a virtual call if the base expression has a record type
+  // because then we know what the type is.
+  bool useVirtualCall = CanUseVirtualCall && !DevirtualizedMethod;
+
+  if (const auto *dtor = dyn_cast<CXXDestructorDecl>(CalleeDecl)) {
+    llvm_unreachable("NYI");
+  }
+
+  // FIXME: Uses of 'MD' past this point need to be audited. We may need to use
+  // 'CalleeDecl' instead.
+
+  CIRGenCallee Callee;
+  if (useVirtualCall) {
+    llvm_unreachable("NYI");
+  } else {
+    if (SanOpts.has(SanitizerKind::CFINVCall)) {
+      llvm_unreachable("NYI");
+    }
+
+    if (getLangOpts().AppleKext)
+      llvm_unreachable("NYI");
+    else if (!DevirtualizedMethod)
+      Callee = CIRGenCallee::forDirect(CGM.GetAddrOfFunction(MD, Ty),
+                                       GlobalDecl(MD));
+    else {
+      llvm_unreachable("NYI");
+    }
+  }
+
+  if (MD->isVirtual()) {
+    llvm_unreachable("NYI");
+  }
+
+  return buildCXXMemberOrOperatorCall(
+      CalleeDecl, Callee, ReturnValue, This.getPointer(),
+      /*ImplicitParam=*/nullptr, QualType(), CE, RtlArgs);
 }
