eb5af0acf054a73d461ac768d8cb035ee2a64383
Daniel Thornburgh <dthorn@google.com>
2022-05-04 22:47:42 +0000

    [Symbolize] Add log markup --filter to llvm-symbolizer.
    This adds a --filter option to llvm-symbolizer. This takes log-bearing
    symbolizer markup from stdin and writes a human-readable version to
    stdout.
    
    For now, this only implements the "symbol" markup tag; all others are
    passed through unaltered. This is a proof-of-concept bit of
    functionalty; implement the various tags is more-or-less just a matter
    of hooking up various parts of the Symbolize library to the architecture
    established here.
    
    Reviewed By: peter.smith
    
    Differential Revision: https://reviews.llvm.org/D126980

--- llvm/tools/llvm-symbolizer/llvm-symbolizer.cpp
+++ llvm/tools/llvm-symbolizer/llvm-symbolizer.cpp
@@ -19,6 +19,8 @@
 #include "llvm/ADT/StringRef.h"
 #include "llvm/Config/config.h"
 #include "llvm/DebugInfo/Symbolize/DIPrinter.h"
+#include "llvm/DebugInfo/Symbolize/Markup.h"
+#include "llvm/DebugInfo/Symbolize/MarkupFilter.h"
 #include "llvm/DebugInfo/Symbolize/SymbolizableModule.h"
 #include "llvm/DebugInfo/Symbolize/Symbolize.h"
 #include "llvm/Debuginfod/DIFetcher.h"
@@ -337,6 +339,17 @@ static FunctionNameKind decideHowToPrintFunctions(const opt::InputArgList &Args,
   return IsAddr2Line ? FunctionNameKind::None : FunctionNameKind::LinkageName;
 }
 
+static Optional<bool> parseColorArg(const opt::InputArgList &Args) {
+  if (Args.hasArg(OPT_color))
+    return true;
+  if (const opt::Arg *A = Args.getLastArg(OPT_color_EQ))
+    return StringSwitch<Optional<bool>>(A->getValue())
+        .Case("always", true)
+        .Case("never", false)
+        .Case("auto", None);
+  return None;
+}
+
 static SmallVector<uint8_t> parseBuildIDArg(const opt::InputArgList &Args,
                                             int ID) {
   const opt::Arg *A = Args.getLastArg(ID);
@@ -350,6 +363,22 @@ static SmallVector<uint8_t> parseBuildIDArg(const opt::InputArgList &Args,
     exit(1);
   }
   return BuildID;
+}
+
+// Symbolize the markup from stdin and write the result to stdout.
+static void filterMarkup(const opt::InputArgList &Args) {
+  MarkupParser Parser;
+  MarkupFilter Filter(outs(), parseColorArg(Args));
+  for (std::string InputString; std::getline(std::cin, InputString);) {
+    InputString += '\n';
+    Parser.parseLine(InputString);
+    Filter.beginLine(InputString);
+    while (Optional<MarkupNode> Element = Parser.nextNode())
+      Filter.filter(*Element);
+  }
+  Parser.flush();
+  while (Optional<MarkupNode> Element = Parser.nextNode())
+    Filter.filter(*Element);
 }
 
 ExitOnError ExitOnErr;
@@ -411,6 +440,11 @@ int main(int argc, char **argv) {
       errs() << "Warning: invalid dSYM hint: \"" << Hint
              << "\" (must have the '.dSYM' extension).\n";
     }
+  }
+
+  if (Args.hasArg(OPT_filter_markup)) {
+    filterMarkup(Args);
+    return 0;
   }
 
   auto Style = IsAddr2Line ? OutputStyle::GNU : OutputStyle::LLVM;
