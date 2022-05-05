e7bdb8203b8f9ab7350d2d355d5f7f79b63bb0c2
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2022-05-04 22:12:57 -0700

    [CIR][CodeGen] Implement basic const global, only emitted when used

---
+++ clang/lib/CIR/CIRGenDeclCXX.cpp
@@ -0,0 +1,31 @@
+//===--- CIRGenDeclCXX.cpp - Build CIR Code for C++ declarations ----------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This contains code dealing with code generation of C++ declarations
+//
+//===----------------------------------------------------------------------===//
+
+#include "CIRGenModule.h"
+#include "TargetInfo.h"
+#include "clang/AST/Attr.h"
+#include "clang/Basic/LangOptions.h"
+
+using namespace clang;
+using namespace mlir::cir;
+using namespace cir;
+
+void CIRGenModule::buildCXXGlobalInitFunc() {
+  while (!CXXGlobalInits.empty() && !CXXGlobalInits.back())
+    CXXGlobalInits.pop_back();
+
+  if (CXXGlobalInits.empty()) // TODO(cir): &&
+                              // PrioritizedCXXGlobalInits.empty())
+    return;
+
+  assert(0 && "NYE");
+}
