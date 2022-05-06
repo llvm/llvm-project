e14aad16d6ccf3582ac7bba0a2c14f1945766819
Nathan Lanza <nathan@lanza.io>
2022-05-06 17:25:39 -0400

    [CIR] Stub out all the AggExprEmitter fns
    Instead of hitting a failure at the top level Visit that gives you a
    vague idea of what to look for just stub everything out with
    unreachables so that we know what we have to implement next.

--- clang/lib/CIR/CIRGenExprAgg.cpp
+++ clang/lib/CIR/CIRGenExprAgg.cpp
@@ -37,13 +37,115 @@ public:
   // ,IsResultUnused(IsResultUnused)
   {}
 
+  //===--------------------------------------------------------------------===//
+  //                             Visitor Methods
+  //===--------------------------------------------------------------------===//
+
   void Visit(Expr *E) {
     // TODO: CodeGen does ApplyDebugLocation here
     assert(cast<CXXConstructExpr>(E) && "Only CXXConstructExpr implemented");
     StmtVisitor<AggExprEmitter>::Visit(E);
   }
 
+  void VisitStmt(Stmt *S) { llvm_unreachable("NYI"); }
+  void VisitParenExpr(ParenExpr *PE) { llvm_unreachable("NYI"); }
+  void VisitGenericSelectionExpr(GenericSelectionExpr *GE) {
+    llvm_unreachable("NYI");
+  }
+  void VisitCoawaitExpr(CoawaitExpr *E) { llvm_unreachable("NYI"); }
+  void VisitCoyieldExpr(CoyieldExpr *E) { llvm_unreachable("NYI"); }
+  void VisitUnaryCoawait(UnaryOperator *E) { llvm_unreachable("NYI"); }
+  void VisitUnaryExtension(UnaryOperator *E) { llvm_unreachable("NYI"); }
+  void VisitSubstNonTypeTemplateParmExpr(SubstNonTypeTemplateParmExpr *E) {
+    llvm_unreachable("NYI");
+  }
+  void VisitConstantExpr(ConstantExpr *E) { llvm_unreachable("NYI"); }
+
+  // l-values
+  void VisitDeclRefExpr(DeclRefExpr *E) { llvm_unreachable("NYI"); }
+  void VisitMemberExpr(MemberExpr *E) { llvm_unreachable("NYI"); }
+  void VisitUnaryDeref(UnaryOperator *E) { llvm_unreachable("NYI"); }
+  void VisitStringLiteral(StringLiteral *E) { llvm_unreachable("NYI"); }
+  void VisitCompoundLIteralExpr(CompoundLiteralExpr *E) {
+    llvm_unreachable("NYI");
+  }
+  void VisitArraySubscriptExpr(ArraySubscriptExpr *E) {
+    llvm_unreachable("NYI");
+  }
+  void VisitPredefinedExpr(const PredefinedExpr *E) { llvm_unreachable("NYI"); }
+
+  // Operators.
+  void VisitCastExpr(CastExpr *E) { llvm_unreachable("NYI"); }
+  void VisitCallExpr(const CallExpr *E) { llvm_unreachable("NYI"); }
+  void VisitStmtExpr(const StmtExpr *E) { llvm_unreachable("NYI"); }
+  void VisitBinaryOperator(const BinaryOperator *E) { llvm_unreachable("NYI"); }
+  void VisitPointerToDataMemberBinaryOperator(const BinaryOperator *E) {
+    llvm_unreachable("NYI");
+  }
+  void VisitBinAssign(const BinaryOperator *E) { llvm_unreachable("NYI"); }
+  void VisitBinComma(const BinaryOperator *E) { llvm_unreachable("NYI"); }
+  void VisitBinCmp(const BinaryOperator *E) { llvm_unreachable("NYI"); }
+  void VisitCXXRewrittenBinaryOperator(CXXRewrittenBinaryOperator *E) {
+    llvm_unreachable("NYI");
+  }
+
+  void VisitObjCMessageExpr(ObjCMessageExpr *E) { llvm_unreachable("NYI"); }
+  void VisitObjCIVarRefExpr(ObjCIvarRefExpr *E) { llvm_unreachable("NYI"); }
+
+  void VisitDesignatedInitUpdateExpr(DesignatedInitUpdateExpr *E) {
+    llvm_unreachable("NYI");
+  }
+  void VisitAbstractConditionalOperator(const AbstractConditionalOperator *E) {
+    llvm_unreachable("NYI");
+  }
+  void VisitChooseExpr(const ChooseExpr *E) { llvm_unreachable("NYI"); }
+  void VisitInitListExpr(InitListExpr *E) { llvm_unreachable("NYI"); }
+  void VisitArrayInitLoopExpr(const ArrayInitLoopExpr *E,
+                              llvm::Value *outerBegin = nullptr) {
+    llvm_unreachable("NYI");
+  }
+  void VisitImplicitValueInitExpr(ImplicitValueInitExpr *E) {
+    llvm_unreachable("NYI");
+  }
+  void VisitNoInitExpr(NoInitExpr *E) { llvm_unreachable("NYI"); }
+  void VisitCXXDefaultArgExpr(CXXDefaultArgExpr *E) { llvm_unreachable("NYI"); }
+  void VisitXCXDefaultInitExpr(CXXDefaultInitExpr *E) {
+    llvm_unreachable("NYI");
+  }
+  void VisitCXXBindTemporaryExpr(CXXBindTemporaryExpr *E) {
+    llvm_unreachable("NYI");
+  }
   void VisitCXXConstructExpr(const CXXConstructExpr *E);
+  void VisitCXXInheritedCtorInitExpr(const CXXInheritedCtorInitExpr *E) {
+    llvm_unreachable("NYI");
+  }
+  void VisitLambdaExpr(LambdaExpr *E) { llvm_unreachable("NYI"); }
+  void VisitCXXStdInitializerListExpr(CXXStdInitializerListExpr *E) {
+    llvm_unreachable("NYI");
+  }
+  void VisitExprWithCleanups(ExprWithCleanups *E) { llvm_unreachable("NYI"); }
+  void VisitCXXScalarValueInitExpr(CXXScalarValueInitExpr *E) {
+    llvm_unreachable("NYI");
+  }
+  void VisitCXXTypeidExpr(CXXTypeidExpr *E) { llvm_unreachable("NYI"); }
+  void VisitMaterializeTemporaryExpr(MaterializeTemporaryExpr *E) {
+    llvm_unreachable("NYI");
+  }
+  void VisitOpaqueValueExpr(OpaqueValueExpr *E) { llvm_unreachable("NYI"); }
+
+  void VisitPseudoObjectExpr(PseudoObjectExpr *E) { llvm_unreachable("NYI"); }
+
+  void VisitVAArgExpr(VAArgExpr *E) { llvm_unreachable("NYI"); }
+
+  void EmitInitializationToLValue(Expr *E, LValue Address) {
+    llvm_unreachable("NYI");
+  }
+  void EmitNullInitializationToLValue(LValue Address) {
+    llvm_unreachable("NYI");
+  }
+  // case Expr::ChoseExprClass:
+  void VisitCXXThrowExpr(const CXXThrowExpr *E) { llvm_unreachable("NYI"); }
+  void VisitAtomicExpr(AtomicExpr *E) { llvm_unreachable("NYI"); }
 };
 } // namespace
 
