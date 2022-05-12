8184e35f6ff2ae8129ce7f21e3ff353d0274d2c0
Nathan Lanza <nathan@lanza.io>
2022-05-12 18:34:47 -0400

    [CIR] Fix String.cpp to test memberwise initializers

--- clang/test/CIR/CodeGen/String.cpp
+++ clang/test/CIR/CodeGen/String.cpp
@@ -1,5 +1,4 @@
 // RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fclangir -emit-cir %s -o - | FileCheck %s
-// XFAIL: *
 
 class String {
   char *storage;
@@ -7,9 +6,20 @@ class String {
   long capacity;
 
 public:
-  String() : size{size} {}
+  String() : size{0} {}
 };
 
 void test() {
   String s;
 }
+
+//      CHECK: func @_ZN6StringC2Ev
+// CHECK-NEXT:   %0 = cir.alloca !cir.ptr<!_22class2EString22>
+// CHECK-NEXT:   cir.store %arg0, %0
+// CHECK-NEXT:   %1 = cir.load %0
+// CHECK-NEXT:   %2 = "cir.struct_element_addr"(%0) <{member_name = "size"}> : (!cir.ptr<!cir.ptr<!_22class2EString22>>) -> !cir.ptr<i64>
+// CHECK-NEXT:   %3 = cir.cst(0 : i32) : i32
+// CHECK-NEXT:   %4 = cir.cast(integral, %3 : i32), i64
+// CHECK-NEXT:   cir.store %4, %2 : i64, cir.ptr <i64>
+// CHECK-NEXT:   cir.return
+// CHECK-NEXT: }
