d3804e77ed264a9e61b5de59cb084d0dd0ec059d
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2022-05-13 14:42:09 -0700

    [CIR][CodeGen] Globals: more work towards constant lvalue emission for string literals
    - Move ConstantLValueEmitter around, and start using it for StringLiteral codegen.
    - Add getConstantArrayFromStringLiteral to emit our cir::CstArrayAttr.
    - Add incomplete getAddrOfConstantStringFromLiteral, which uses the method above.
    - Add ConstantStringMap to unique global string data, but don't use it yet.
    - No testcase added yet since it currently asserts.

--- mlir/lib/Dialect/CIR/IR/CIRDialect.cpp
+++ mlir/lib/Dialect/CIR/IR/CIRDialect.cpp
@@ -913,14 +913,14 @@ parseGlobalOpTypeAndInitialValue(OpAsmParser &parser, TypeAttr &typeAttr,
   // Parse constant with initializer, examples:
   //  cir.global @y = 3.400000e+00 : f32
   //  cir.global @rgb  = #cir.cst_array<[...] : !cir.array<i8 x 3>>
-  Attribute attr;
-  if (parseConstantValue(parser, attr).failed())
+  if (parseConstantValue(parser, initialValueAttr).failed())
     return failure();
 
-  assert(attr.isa<mlir::TypedAttr>() &&
+  assert(initialValueAttr.isa<mlir::TypedAttr>() &&
          "Non-typed attrs shouldn't appear here.");
-  initialValueAttr = attr.cast<mlir::TypedAttr>();
-  typeAttr = TypeAttr::get(attr.cast<mlir::TypedAttr>().getType());
+  auto typedAttr = initialValueAttr.cast<mlir::TypedAttr>();
+
+  typeAttr = TypeAttr::get(typedAttr.getType());
 
   return success();
 }
@@ -1078,8 +1078,8 @@ LogicalResult mlir::cir::CstArrayAttr::verify(
   // Parse literal '>'
   if (parser.parseGreater())
     return {};
-  return parser.getChecked<CstArrayAttr>(
-      loc, parser.getContext(), resultTy.value(), resultVal.value());
+  return parser.getChecked<CstArrayAttr>(loc, parser.getContext(),
+                                         resultTy.value(), resultVal.value());
 }
 
 void CstArrayAttr::print(::mlir::AsmPrinter &printer) const {
