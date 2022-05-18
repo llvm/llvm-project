4c864981aec195387ff1e828e7072d0842109f35
Nathan Lanza <nathan@lanza.io>
2022-05-17 21:57:04 -0400

    [CIR] Add a CXXDefaultInitExpr via `char *storage=nullptr` to String.cpp
    I actually don't think we'll use this for the final version since we
    want the constructor memberwise inits to cover it. But add a test for it
    anyways.

--- clang/test/CIR/CodeGen/String.cpp
+++ clang/test/CIR/CodeGen/String.cpp
@@ -1,7 +1,7 @@
 // RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fclangir -emit-cir %s -o - | FileCheck %s
 
 class String {
-  char *storage;
+  char *storage{nullptr};
   long size;
   long capacity;
 
@@ -19,10 +19,13 @@ void test() {
 // CHECK-NEXT:   %0 = cir.alloca !cir.ptr<!_22class2EString22>
 // CHECK-NEXT:   cir.store %arg0, %0
 // CHECK-NEXT:   %1 = cir.load %0
-// CHECK-NEXT:   %2 = "cir.struct_element_addr"(%0) <{member_name = "size"}> : (!cir.ptr<!cir.ptr<!_22class2EString22>>) -> !cir.ptr<i64>
-// CHECK-NEXT:   %3 = cir.cst(0 : i32) : i32
-// CHECK-NEXT:   %4 = cir.cast(integral, %3 : i32), i64
-// CHECK-NEXT:   cir.store %4, %2 : i64, cir.ptr <i64>
+// CHECK-NEXT:   %2 = "cir.struct_element_addr"(%0) <{member_name = "storage"}>
+// CHECK-NEXT:   %3 = cir.cst(#cir.null : !cir.ptr<i8>) : !cir.ptr<i8>
+// CHECK-NEXT:   cir.store %3, %2 : !cir.ptr<i8>, cir.ptr <!cir.ptr<i8>>
+// CHECK-NEXT:   %4 = "cir.struct_element_addr"(%0) <{member_name = "size"}> : (!cir.ptr<!cir.ptr<!_22class2EString22>>) -> !cir.ptr<i64>
+// CHECK-NEXT:   %5 = cir.cst(0 : i32) : i32
+// CHECK-NEXT:   %6 = cir.cast(integral, %5 : i32), i64
+// CHECK-NEXT:   cir.store %6, %4 : i64, cir.ptr <i64>
 // CHECK-NEXT:   cir.return
 // CHECK-NEXT: }
 //      CHECK: func @_ZN6StringC2Ei
@@ -31,9 +34,12 @@ void test() {
 // CHECK-NEXT:   cir.store %arg0, %0
 // CHECK-NEXT:   cir.store %arg1, %1
 // CHECK-NEXT:   %2 = cir.load %0
-// CHECK-NEXT:   %3 = "cir.struct_element_addr"(%0) <{member_name = "size"}> : (!cir.ptr<!cir.ptr<!_22class2EString22>>) -> !cir.ptr<i64>
-// CHECK-NEXT:   %4 = cir.load %1 : cir.ptr <i32>, i32
-// CHECK-NEXT:   %5 = cir.cast(integral, %4 : i32), i64
-// CHECK-NEXT:   cir.store %5, %3 : i64, cir.ptr <i64>
+// CHECK-NEXT:   %3 = "cir.struct_element_addr"(%0) <{member_name = "storage"}>
+// CHECK-NEXT:   %4 = cir.cst(#cir.null : !cir.ptr<i8>)
+// CHECK-NEXT:   cir.store %4, %3
+// CHECK-NEXT:   %5 = "cir.struct_element_addr"(%0) <{member_name = "size"}> : (!cir.ptr<!cir.ptr<!_22class2EString22>>) -> !cir.ptr<i64>
+// CHECK-NEXT:   %6 = cir.load %1 : cir.ptr <i32>, i32
+// CHECK-NEXT:   %7 = cir.cast(integral, %6 : i32), i64
+// CHECK-NEXT:   cir.store %7, %5 : i64, cir.ptr <i64>
 // CHECK-NEXT:   cir.return
 // CHECK-NEXT: }
