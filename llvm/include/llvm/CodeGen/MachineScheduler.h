cbfbb117ed0f97f91cfb2129c6d8585237f12727
Jannik Silvanus <jannik.silvanus@amd.com>
2022-05-20 17:21:15 +0200

    [AMDGPU] SIMachineScheduler: Add support for several MachineScheduler features
    The SI machine scheduler inherits from ScheduleDAGMI.
    This patch adds support for a few features that are implemented
    in ScheduleDAGMI (or its base classes) that were missing so far
    because their support is implemented in overridden functions.
    
    * Support cl::opt -view-misched-dags
      This option allows to open a graphical window of the scheduling DAG.
    
    * Support cl::opt -misched-print-dags
      This option allows to print the scheduling DAG in text form.
    
    * After constructing the scheduling DAG, call postprocessDAG()
      to apply any registered DAG mutations.
      Note that currently there are no mutations defined in AMDGPUTargetMachine.cpp
      in case SIScheduler is used.
      Still add this to avoid surprises in the future in case mutations are added.
    
    Differential Revision: https://reviews.llvm.org/D128808

--- llvm/include/llvm/CodeGen/MachineScheduler.h
+++ llvm/include/llvm/CodeGen/MachineScheduler.h
@@ -103,8 +103,10 @@ extern cl::opt<bool> ForceBottomUp;
 extern cl::opt<bool> VerifyScheduling;
 #ifndef NDEBUG
 extern cl::opt<bool> ViewMISchedDAGs;
+extern cl::opt<bool> PrintDAGs;
 #else
 extern const bool ViewMISchedDAGs;
+extern const bool PrintDAGs;
 #endif
 
 class AAResults;
