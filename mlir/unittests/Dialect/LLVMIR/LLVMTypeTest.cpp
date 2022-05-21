d41028610b5372669adcb9b7091fae5250f0a4a8
Min-Yih Hsu <minyihh@uci.edu>
2022-05-20 21:52:49 -0700

    [mlir] Prevent SubElementInterface from going into infinite recursion
    Since only mutable types and attributes can go into infinite recursion
    inside SubElementInterface::walkSubElement, and there are only a few of
    them (mutable types and attributes), we introduce new traits for Type
    and Attribute: TypeTrait::IsMutable and AttributeTrait::IsMutable,
    respectively. They indicate whether a type or attribute is mutable.
    Such traits are required if the ImplType defines a `mutate` function.
    
    Then, inside SubElementInterface, we use a set to record visited mutable
    types and attributes that have been visited before.
    
    Differential Revision: https://reviews.llvm.org/D127537

---
+++ mlir/unittests/Dialect/LLVMIR/LLVMTypeTest.cpp
@@ -0,0 +1,20 @@
+//===- LLVMTypeTest.cpp - Tests for LLVM types ----------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "LLVMTestBase.h"
+#include "mlir/Dialect/LLVMIR/LLVMTypes.h"
+#include "mlir/IR/SubElementInterfaces.h"
+
+using namespace mlir;
+using namespace mlir::LLVM;
+
+TEST_F(LLVMIRTest, IsStructTypeMutable) {
+  auto structTy = LLVMStructType::getIdentified(&context, "foo");
+  ASSERT_TRUE(bool(structTy));
+  ASSERT_TRUE(structTy.hasTrait<TypeTrait::IsMutable>());
+}
