a19fcc2bec81989f90700cfc63b89a0dfd330197
Walter Erquinigo <wallace@fb.com>
2022-05-24 12:16:25 -0700

    [trace][intelpt] Support system-wide tracing [14] - Decode per cpu
    This is the final functional patch to support intel pt decoding per cpu.
    It works by doing the following:
    
    - First, all context switches are split by tid and sorted in order. This produces a list of continuous executes per thread per core.
    - Then, all intel pt subtraces are split by PSB boundaries and assigned to individual thread continuous executions on the same core by doing simple TSC-based comparisons.
    - With this, we have, per thread, a sorted list of continuous executions each one with a list of intel pt subtraces. Up to this point, this is really fast because no instructions were actually decoded.
    - Then, each thread can be decoded by traversing their continuous executions and intel pt subtraces. An advantage of having these continuous executions is that we can identify if a continuous exexecution doesn't have intel pt data, and thus has a gap in it. We can later to more sofisticated comparisons to identify if within a continuous execution there are gaps.
    
    I'm adding a test as well.
    
    Differential Revision: https://reviews.llvm.org/D126394

--- lldb/source/Plugins/Trace/intel-pt/LibiptDecoder.h
+++ lldb/source/Plugins/Trace/intel-pt/LibiptDecoder.h
@@ -9,19 +9,49 @@
 #ifndef LLDB_SOURCE_PLUGINS_TRACE_LIBIPT_DECODER_H
 #define LLDB_SOURCE_PLUGINS_TRACE_LIBIPT_DECODER_H
 
-#include "intel-pt.h"
-
 #include "DecodedThread.h"
+#include "PerfContextSwitchDecoder.h"
 #include "forward-declarations.h"
+
+#include "intel-pt.h"
 
 namespace lldb_private {
 namespace trace_intel_pt {
+
+struct IntelPTThreadSubtrace {
+  uint64_t tsc;
+  uint64_t psb_offset;
+};
+
+/// This struct represents a continuous execution of a thread in a core,
+/// delimited by a context switch in and out, and a list of Intel PT subtraces
+/// that belong to this execution.
+struct IntelPTThreadContinousExecution {
+  ThreadContinuousExecution thread_execution;
+  std::vector<IntelPTThreadSubtrace> intelpt_subtraces;
+
+  IntelPTThreadContinousExecution(
+      const ThreadContinuousExecution &thread_execution)
+      : thread_execution(thread_execution) {}
+
+  /// Comparator by time
+  bool operator<(const IntelPTThreadContinousExecution &o) const;
+};
 
 /// Decode a raw Intel PT trace given in \p buffer and append the decoded
 /// instructions and errors in \p decoded_thread. It uses the low level libipt
 /// library underneath.
 void DecodeTrace(DecodedThread &decoded_thread, TraceIntelPT &trace_intel_pt,
                  llvm::ArrayRef<uint8_t> buffer);
+
+void DecodeTrace(
+    DecodedThread &decoded_thread, TraceIntelPT &trace_intel_pt,
+    const llvm::DenseMap<lldb::core_id_t, llvm::ArrayRef<uint8_t>> &buffers,
+    const std::vector<IntelPTThreadContinousExecution> &executions);
+
+llvm::Expected<std::vector<IntelPTThreadSubtrace>>
+SplitTraceInContinuousExecutions(TraceIntelPT &trace_intel_pt,
+                                 llvm::ArrayRef<uint8_t> buffer);
 
 } // namespace trace_intel_pt
 } // namespace lldb_private
--- lldb/source/Plugins/Trace/intel-pt/TraceIntelPTMultiCoreDecoder.cpp
+++ lldb/source/Plugins/Trace/intel-pt/TraceIntelPTMultiCoreDecoder.cpp
@@ -12,224 +12,10 @@
 
 #include "llvm/Support/Error.h"
 
-#include <linux/perf_event.h>
-
 using namespace lldb;
 using namespace lldb_private;
 using namespace lldb_private::trace_intel_pt;
 using namespace llvm;
-
-struct PerfContextSwitchRecord {
-  struct perf_event_header header;
-  uint32_t next_prev_pid;
-  uint32_t next_prev_tid;
-  uint32_t pid, tid;
-  uint64_t time_in_nanos;
-
-  bool IsOut() const { return header.misc & PERF_RECORD_MISC_SWITCH_OUT; }
-};
-
-struct ContextSwitchRecord {
-  uint64_t tsc;
-  bool is_out;
-  /// A pid of 0 indicates an execution in the kernel
-  lldb::pid_t pid;
-  lldb::tid_t tid;
-
-  bool IsOut() const { return is_out; }
-
-  bool IsIn() const { return !is_out; }
-};
-
-uint64_t ThreadContinuousExecution::GetErrorFreeTSC() const {
-  switch (variant) {
-  case Variant::Complete:
-    return tscs.complete.start; // end would also work
-  case Variant::HintedStart:
-    return tscs.hinted_start.end;
-  case Variant::HintedEnd:
-    return tscs.hinted_end.start;
-  case Variant::OnlyEnd:
-    return tscs.only_end.end;
-  case Variant::OnlyStart:
-    return tscs.only_start.start;
-  }
-}
-
-ThreadContinuousExecution ThreadContinuousExecution::CreateCompleteExecution(
-    lldb::core_id_t core_id, lldb::tid_t tid, uint64_t start, uint64_t end) {
-  ThreadContinuousExecution o(core_id, tid);
-  o.variant = Variant::Complete;
-  o.tscs.complete.start = start;
-  o.tscs.complete.end = end;
-  return o;
-}
-
-ThreadContinuousExecution ThreadContinuousExecution::CreateHintedStartExecution(
-    lldb::core_id_t core_id, lldb::tid_t tid, uint64_t hinted_start,
-    uint64_t end) {
-  ThreadContinuousExecution o(core_id, tid);
-  o.variant = Variant::HintedStart;
-  o.tscs.hinted_start.hinted_start = hinted_start;
-  o.tscs.hinted_start.end = end;
-  return o;
-}
-
-ThreadContinuousExecution ThreadContinuousExecution::CreateHintedEndExecution(
-    lldb::core_id_t core_id, lldb::tid_t tid, uint64_t start,
-    uint64_t hinted_end) {
-  ThreadContinuousExecution o(core_id, tid);
-  o.variant = Variant::HintedEnd;
-  o.tscs.hinted_end.start = start;
-  o.tscs.hinted_end.hinted_end = hinted_end;
-  return o;
-}
-
-ThreadContinuousExecution ThreadContinuousExecution::CreateOnlyEndExecution(
-    lldb::core_id_t core_id, lldb::tid_t tid, uint64_t end) {
-  ThreadContinuousExecution o(core_id, tid);
-  o.variant = Variant::OnlyEnd;
-  o.tscs.only_end.end = end;
-  return o;
-}
-
-ThreadContinuousExecution ThreadContinuousExecution::CreateOnlyStartExecution(
-    lldb::core_id_t core_id, lldb::tid_t tid, uint64_t start) {
-  ThreadContinuousExecution o(core_id, tid);
-  o.variant = Variant::OnlyStart;
-  o.tscs.only_start.start = start;
-  return o;
-}
-
-bool ThreadContinuousExecution::operator<(
-    const ThreadContinuousExecution &o) const {
-  // We can compare by GetErrorFreeTSC because context switches across CPUs can
-  // be sorted by any of its TSC.
-  return GetErrorFreeTSC() < o.GetErrorFreeTSC();
-}
-
-/// Tries to recover a continuous execution by analyzing two consecutive context
-/// switch records.
-static Error
-HandleContextSwitch(core_id_t core_id,
-                    const LinuxPerfZeroTscConversion &tsc_conversion,
-                    const ContextSwitchRecord &record,
-                    const Optional<ContextSwitchRecord> &prev_record,
-                    std::function<void(ThreadContinuousExecution &&execution)>
-                        on_new_thread_execution) {
-  if (!prev_record) {
-    if (record.IsOut())
-      on_new_thread_execution(ThreadContinuousExecution::CreateOnlyEndExecution(
-          core_id, record.tid, record.tsc));
-    // The 'in' case will be handled later when we try to look for its end
-    return Error::success();
-  }
-
-  const ContextSwitchRecord &prev = *prev_record;
-  if (prev.tsc > record.tsc)
-    return createStringError(
-        inconvertibleErrorCode(),
-        formatv("A context switch record out doesn't happen after the previous "
-                "record. Previous TSC= {0}, current TSC = {1}.",
-                prev.tsc, record.tsc));
-
-  if (record.IsIn() && prev.IsIn()) {
-    // We found two consecutive ins, which means that we didn't capture
-    // the end of the previous execution.
-    on_new_thread_execution(ThreadContinuousExecution::CreateHintedEndExecution(
-        core_id, prev.tid, prev.tsc, record.tsc - 1));
-  } else if (record.IsOut() && prev.IsOut()) {
-    // We found two consecutive outs, that means that we didn't capture
-    // the beginning of the current execution.
-    on_new_thread_execution(
-        ThreadContinuousExecution::CreateHintedStartExecution(
-            core_id, record.tid, prev.tsc + 1, record.tsc));
-  } else if (record.IsOut() && prev.IsIn()) {
-    if (record.pid == prev.pid && record.tid == prev.tid) {
-      /// A complete execution
-      on_new_thread_execution(
-          ThreadContinuousExecution::CreateCompleteExecution(
-              core_id, record.tid, prev.tsc, record.tsc));
-    } else {
-      // An out after the in of a different thread. The first one doesn't
-      // have an end, and the second one doesn't have a start.
-      on_new_thread_execution(
-          ThreadContinuousExecution::CreateHintedEndExecution(
-              core_id, prev.tid, prev.tsc, record.tsc - 1));
-      on_new_thread_execution(
-          ThreadContinuousExecution::CreateHintedStartExecution(
-              core_id, record.tid, prev.tsc + 1, record.tsc));
-    }
-  }
-  return Error::success();
-}
-
-/// Decodes a context switch trace gotten with perf_event_open.
-///
-/// \param[in] data
-///   The context switch trace in binary format.
-///
-/// \param[i] core_id
-///   The core_id where the trace were gotten from.
-///
-/// \param[in] tsc_conversion
-///   The conversion values used to confert nanoseconds to TSC.
-///
-/// \param[in] on_new_thread_execution
-///   Callback to be invoked whenever a continuous execution is recovered from
-///   the trace.
-static Error DecodePerfContextSwitchTrace(
-    ArrayRef<uint8_t> data, core_id_t core_id,
-    const LinuxPerfZeroTscConversion &tsc_conversion,
-    std::function<void(ThreadContinuousExecution &&execution)>
-        on_new_thread_execution) {
-  auto CreateError = [&](size_t offset, auto error) -> Error {
-    return createStringError(inconvertibleErrorCode(),
-                             formatv("Malformed perf context switch trace for "
-                                     "cpu {0} at offset {1}. {2}",
-                                     core_id, offset, error));
-  };
-
-  Optional<ContextSwitchRecord> prev_record;
-  for (size_t offset = 0; offset < data.size();) {
-    const PerfContextSwitchRecord &perf_record =
-        *reinterpret_cast<const PerfContextSwitchRecord *>(data.data() +
-                                                           offset);
-    // A record of 1000 uint64_t's or more should mean that the data is wrong
-    if (perf_record.header.size == 0 ||
-        perf_record.header.size > sizeof(uint64_t) * 1000)
-      return CreateError(offset, formatv("A record of {0} bytes was found.",
-                                         perf_record.header.size));
-
-    // We add + 100 to this record because some systems might have custom
-    // records. In any case, we are looking only for abnormal data.
-    if (perf_record.header.type >= PERF_RECORD_MAX + 100)
-      return CreateError(offset, formatv("Invalid record type {0} was found.",
-                                         perf_record.header.type));
-
-    if (perf_record.header.type == PERF_RECORD_SWITCH_CPU_WIDE) {
-      ContextSwitchRecord record{tsc_conversion.ToTSC(std::chrono::nanoseconds(
-                                     perf_record.time_in_nanos)),
-                                 perf_record.IsOut(),
-                                 static_cast<lldb::pid_t>(perf_record.pid),
-                                 static_cast<lldb::tid_t>(perf_record.tid)};
-
-      if (Error err = HandleContextSwitch(core_id, tsc_conversion, record,
-                                          prev_record, on_new_thread_execution))
-        return CreateError(offset, toString(std::move(err)));
-
-      prev_record = record;
-    }
-    offset += perf_record.header.size;
-  }
-
-  // We might have an incomplete last record
-  if (prev_record && prev_record->IsIn())
-    on_new_thread_execution(ThreadContinuousExecution::CreateOnlyStartExecution(
-        core_id, prev_record->tid, prev_record->tsc));
-
-  return Error::success();
-}
 
 TraceIntelPTMultiCoreDecoder::TraceIntelPTMultiCoreDecoder(
     TraceIntelPT &trace, ArrayRef<core_id_t> core_ids, ArrayRef<tid_t> tids,
@@ -245,10 +31,91 @@ DecodedThreadSP TraceIntelPTMultiCoreDecoder::Decode(Thread &thread) {
   if (Error err = DecodeContextSwitchTraces())
     return std::make_shared<DecodedThread>(thread.shared_from_this(),
                                            std::move(err));
+  auto it = m_decoded_threads.find(thread.GetID());
+  if (it != m_decoded_threads.end())
+    return it->second;
 
-  return std::make_shared<DecodedThread>(
-      thread.shared_from_this(),
-      createStringError(inconvertibleErrorCode(), "unimplemented"));
+  DecodedThreadSP decoded_thread_sp =
+      std::make_shared<DecodedThread>(thread.shared_from_this());
+
+  Error err = m_trace.OnCoresBinaryDataRead(
+      m_cores, IntelPTDataKinds::kTraceBuffer,
+      [&](const DenseMap<core_id_t, ArrayRef<uint8_t>> buffers) -> Error {
+        auto it = m_continuous_executions_per_thread->find(thread.GetID());
+        if (it != m_continuous_executions_per_thread->end())
+          DecodeTrace(*decoded_thread_sp, m_trace, buffers, it->second);
+
+        return Error::success();
+      });
+  if (err)
+    decoded_thread_sp->SetAsFailed(std::move(err));
+
+  m_decoded_threads.try_emplace(thread.GetID(), decoded_thread_sp);
+  return decoded_thread_sp;
+}
+
+llvm::Expected<
+    llvm::DenseMap<lldb::tid_t, std::vector<IntelPTThreadContinousExecution>>>
+TraceIntelPTMultiCoreDecoder::CorrelateContextSwitchesAndIntelPtTraces() {
+  llvm::DenseMap<lldb::tid_t, std::vector<IntelPTThreadContinousExecution>>
+      continuous_executions_per_thread;
+
+  for (core_id_t core_id : m_cores) {
+    std::vector<IntelPTThreadSubtrace> intel_pt_executions;
+
+    Error err = m_trace.OnCoreBinaryDataRead(
+        core_id, IntelPTDataKinds::kTraceBuffer,
+        [&](ArrayRef<uint8_t> data) -> Error {
+          Expected<std::vector<IntelPTThreadSubtrace>> split_trace =
+              SplitTraceInContinuousExecutions(m_trace, data);
+          if (!split_trace)
+            return split_trace.takeError();
+
+          intel_pt_executions = std::move(*split_trace);
+          return Error::success();
+        });
+    if (err)
+      return std::move(err);
+
+    // We'll be iterating through the thread continuous executions and the intel
+    // pt subtraces sorted by time.
+    auto it = intel_pt_executions.begin();
+    auto on_new_thread_execution =
+        [&](ThreadContinuousExecution thread_execution) {
+          IntelPTThreadContinousExecution execution(thread_execution);
+
+          for (; it != intel_pt_executions.end() &&
+                 it->tsc < thread_execution.GetEndTSC();
+               it++) {
+            if (it->tsc > thread_execution.GetStartTSC()) {
+              execution.intelpt_subtraces.push_back(*it);
+            } else {
+              m_unattributed_intelpt_subtraces++;
+            }
+          }
+          continuous_executions_per_thread[thread_execution.tid].push_back(
+              execution);
+        };
+    err = m_trace.OnCoreBinaryDataRead(
+        core_id, IntelPTDataKinds::kPerfContextSwitchTrace,
+        [&](ArrayRef<uint8_t> data) -> Error {
+          Expected<std::vector<ThreadContinuousExecution>> executions =
+              DecodePerfContextSwitchTrace(data, core_id, m_tsc_conversion);
+          if (!executions)
+            return executions.takeError();
+          for (const ThreadContinuousExecution &exec : *executions)
+            on_new_thread_execution(exec);
+          return Error::success();
+        });
+    if (err)
+      return std::move(err);
+  }
+  // We now sort the executions of each thread to have them ready for
+  // instruction decoding
+  for (auto &tid_executions : continuous_executions_per_thread)
+    std::sort(tid_executions.second.begin(), tid_executions.second.end());
+
+  return continuous_executions_per_thread;
 }
 
 Error TraceIntelPTMultiCoreDecoder::DecodeContextSwitchTraces() {
@@ -258,38 +125,20 @@ Error TraceIntelPTMultiCoreDecoder::DecodeContextSwitchTraces() {
   if (m_continuous_executions_per_thread)
     return Error::success();
 
-  m_continuous_executions_per_thread.emplace();
-
-  auto do_decode = [&]() -> Error {
-    // We'll decode all context switch traces, identify continuous executions
-    // and group them by thread.
-    for (core_id_t core_id : m_cores) {
-      Error err = m_trace.OnCoreBinaryDataRead(
-          core_id, IntelPTDataKinds::kPerfContextSwitchTrace,
-          [&](ArrayRef<uint8_t> data) -> Error {
-            return DecodePerfContextSwitchTrace(
-                data, core_id, m_tsc_conversion,
-                [&](const ThreadContinuousExecution &execution) {
-                  (*m_continuous_executions_per_thread)[execution.tid]
-                      .push_back(execution);
-                });
-          });
-      if (err) {
-        m_setup_error = toString(std::move(err));
-        return createStringError(inconvertibleErrorCode(),
-                                 m_setup_error->c_str());
-      }
-    }
-    // We now sort the executions of each to have them ready for instruction
-    // decoding
-    for (auto &tid_executions : *m_continuous_executions_per_thread)
-      std::sort(tid_executions.second.begin(), tid_executions.second.end());
-
-    return Error::success();
-  };
-
-  return m_trace.GetTimer().ForGlobal().TimeTask<Error>(
-      "Context switch trace decoding", do_decode);
+  Error err = m_trace.GetTimer().ForGlobal().TimeTask<Error>(
+      "Context switch and Intel PT traces correlation", [&]() -> Error {
+        if (auto correlation = CorrelateContextSwitchesAndIntelPtTraces()) {
+          m_continuous_executions_per_thread.emplace(std::move(*correlation));
+          return Error::success();
+        } else {
+          return correlation.takeError();
+        }
+      });
+  if (err) {
+    m_setup_error = toString(std::move(err));
+    return createStringError(inconvertibleErrorCode(), m_setup_error->c_str());
+  }
+  return Error::success();
 }
 
 size_t TraceIntelPTMultiCoreDecoder::GetNumContinuousExecutionsForThread(
