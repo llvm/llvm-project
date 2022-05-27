a01fc73238e497e515dd117520d581f09337ef5f
Ben Barham <ben_barham@apple.com>
2022-05-27 09:50:44 -0700

    Revert "[lldb] Add the SwiftEnableBareSlashRegexOnPlayground setting"
    This reverts commit 7705a013636b5b53ff08426009b78fd78710c17a. It was
    cherry-picked from e32eb5b3268426438d41f6355d5d1df4be91bb27, but the
    dependent commit 5ab3967107f407012712a5eafb3d8bc3fac05085 was never
    cherry-picked. Revert so both can be cherry-picked without conflicts.

--- lldb/source/Plugins/ExpressionParser/Swift/SwiftExpressionParser.cpp
+++ lldb/source/Plugins/ExpressionParser/Swift/SwiftExpressionParser.cpp
@@ -1361,15 +1361,6 @@ static llvm::Expected<ParsedExpression> ParseAndImport(
   invocation.getFrontendOptions().ModuleName = expr_name_buf;
   invocation.getIRGenOptions().ModuleName = expr_name_buf;
 
-  bool enable_bare_slash_regex_literals =
-      sc.target_sp->GetSwiftEnableBareSlashRegex();
-  invocation.getLangOptions().EnableBareSlashRegexLiterals =
-      enable_bare_slash_regex_literals;
-  invocation.getLangOptions().EnableExperimentalStringProcessing =
-      enable_bare_slash_regex_literals;
-
-  swift_ast_context.LoadImplicitModules(sc.target_sp, process_sp);
-
   auto should_use_prestable_abi = [&]() {
     lldb::StackFrameSP this_frame_sp(stack_frame_wp.lock());
     if (!this_frame_sp)
