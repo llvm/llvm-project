64b7e22ff55c34d650bf2601b37de92b4346df6d
Sam McCall <sam.mccall@gmail.com>
2022-06-09 09:06:19 +0200

    [pseudo] Add xfail tests for a simple-declaration/function-definition ambiguity
    I expect to eliminate this ambiguity at the grammar level by use of guards,
    because it interferes with brace-based error recvoery.
    
    Differential Revision: https://reviews.llvm.org/D127400

---
+++ clang-tools-extra/pseudo/test/cxx/declarator-function.cpp
@@ -0,0 +1,11 @@
+// The standard grammar allows an init-list with any declarator, including
+// a function declarator. This creates an ambiguity where a function-definition
+// is misparsed as a simple-declaration.
+// FIXME: eliminate this false parse.
+// XFAIL: *
+
+// RUN: clang-pseudo -grammar=%cxx-bnf-file -source=%s --print-forest | FileCheck %s
+void s(){};
+// CHECK-NOT:      simple-declaration
+// CHECK:          function-definition := decl-specifier-seq declarator
+// function-body CHECK-NOT:      simple-declaration
