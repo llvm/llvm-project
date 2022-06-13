edc6b02517a2273f257261857548b29464446571
Stephen Thomas <Stephen.Thomas@amd.com>
2022-06-13 13:29:00 +0100

    Add encoding support for new GFX12 S_WAIT_*CNT instructions.
    The pre-GFX12 S_WAITCNT_*CNT instructions are not supported on GFX12
    and are faulted if they are used on GFX12. Similarly, the new GFX12
    are faulted if used on earlier targets.
    
    S_WAIT_ALU is now treated as a synonym for S_WAITCNT_DEPCTR, but only
    on GFX12. "s_waitcnt_depctr" is assembled on GFX12 but will be
    disassembled as "s_wait_alu" on GFX12.
    
    Change-Id: I088901d678a0979b6160bd32e9c8df92c6e2b450

--- llvm/lib/Target/AMDGPU/Disassembler/AMDGPUDisassembler.cpp
+++ llvm/lib/Target/AMDGPU/Disassembler/AMDGPUDisassembler.cpp
@@ -569,6 +569,9 @@ DecodeStatus AMDGPUDisassembler::getInstruction(MCInst &MI, uint64_t &Size,
     Res = tryDecodeInst(DecoderTableGFX1132, MI, DW, Address);
     if (Res) break;
 
+    Res = tryDecodeInst(DecoderTableGFX1232, MI, DW, Address);
+    if (Res) break;
+
     if (Bytes.size() < 4) break;
     const uint64_t QW = ((uint64_t)eatBytes<uint32_t>(Bytes) << 32) | DW;
 
---
+++ llvm/test/MC/AMDGPU/gfx12_unsupported.s
@@ -0,0 +1,17 @@
+// RUN: not llvm-mc -arch=amdgcn -mcpu=gfx1200 %s 2>&1 | FileCheck --implicit-check-not=error: %s
+
+//===----------------------------------------------------------------------===//
+// Unsupported instructions.
+//===----------------------------------------------------------------------===//
+
+s_waitcnt_expcnt exec_hi, 0x1234
+// CHECK: error: instruction not supported on this GPU
+
+s_waitcnt_lgkmcnt exec_hi, 0x1234
+// CHECK: error: instruction not supported on this GPU
+
+s_waitcnt_vmcnt exec_hi, 0x1234
+// CHECK: error: instruction not supported on this GPU
+
+s_waitcnt_vscnt exec_hi, 0x1234
+// CHECK: error: instruction not supported on this GPU
