f5aac2c7bf5159bf9a2c94bceb6782320b7ced56
David Goldman <davg@google.com>
2022-06-13 19:33:22 -0400

    Fix -index-ignore-pcms not being propagated properly to the frontend
    Update the corresponding test to use explicit modules. Implicit
    modules will still be indexed while they are themselves compiled, as
    `-index-ignore-pcms` only ignores imported modules.
    
    cherry-picked from #4820

--- clang/lib/Driver/ToolChains/Clang.cpp
+++ clang/lib/Driver/ToolChains/Clang.cpp
@@ -5578,6 +5578,7 @@ void Clang::ConstructJob(Compilation &C, const JobAction &JA,
     Args.AddLastArg(CmdArgs, options::OPT_index_record_codegen_name);
     Args.AddLastArg(CmdArgs, options::OPT_index_unit_output_path);
     Args.AddLastArg(CmdArgs, options::OPT_index_ignore_macros);
+    Args.AddLastArg(CmdArgs, options::OPT_index_ignore_pcms);
 
     // If '-o' is passed along with '-fsyntax-only' pass it along the cc1
     // invocation so that the index action knows what the out file is.
