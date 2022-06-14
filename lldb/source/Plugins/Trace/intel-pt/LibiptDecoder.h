67c24051450116c2c633e8a2740c08cf904e1605
Walter Erquinigo <wallace@fb.com>
2022-06-14 14:36:26 -0700

    [trace][intelpt] Support system-wide tracing [19] - Some other minor improvements
    This addresses the issues in diffs [13], [14] and [16]
    
    - Add better documentation
    - Fix some castings by making them safer
    - Simplify CorrelateContextSwitchesAndIntelPtTraces
    - Rename some functions
    
    Differential Revision: https://reviews.llvm.org/D127804

--- lldb/source/Plugins/Trace/intel-pt/LibiptDecoder.h
+++ lldb/source/Plugins/Trace/intel-pt/LibiptDecoder.h
@@ -18,9 +18,13 @@
 namespace lldb_private {
 namespace trace_intel_pt {
 
+/// This struct represents a point in the intel pt trace that the decoder can start decoding from without errors.
 struct IntelPTThreadSubtrace {
-  uint64_t tsc;
+  /// The memory offset of a PSB packet that is a synchronization point for the decoder. A decoder normally looks first
+  /// for a PSB packet and then it starts decoding.
   uint64_t psb_offset;
+  /// The timestamp associated with the PSB packet above.
+  uint64_t tsc;
 };
 
 /// This struct represents a continuous execution of a thread in a core,
@@ -38,17 +42,43 @@ struct IntelPTThreadContinousExecution {
   bool operator<(const IntelPTThreadContinousExecution &o) const;
 };
 
-/// Decode a raw Intel PT trace given in \p buffer and append the decoded
+/// Decode a raw Intel PT trace for a single thread given in \p buffer and append the decoded
 /// instructions and errors in \p decoded_thread. It uses the low level libipt
 /// library underneath.
-void DecodeTrace(DecodedThread &decoded_thread, TraceIntelPT &trace_intel_pt,
+void DecodeSingleTraceForThread(DecodedThread &decoded_thread, TraceIntelPT &trace_intel_pt,
                  llvm::ArrayRef<uint8_t> buffer);
 
-void DecodeTrace(
+/// Decode a raw Intel PT trace for a single thread that was collected in a per cpu core basis.
+///
+/// \param[in] decoded_thread
+///   All decoded instructions, errors and events will be appended to this object.
+///
+/// \param[in] trace_intel_pt
+///   The main Trace object that contains all the information related to the trace session.
+///
+/// \param[in] buffers
+///   A map from cpu core id to raw intel pt buffers.
+///
+/// \param[in] executions
+///   A list of chunks of timed executions of the same given thread. It is used to identify if
+///   some executions have missing intel pt data and also to determine in which core a certain
+///   part of the execution ocurred.
+void DecodeSystemWideTraceForThread(
     DecodedThread &decoded_thread, TraceIntelPT &trace_intel_pt,
     const llvm::DenseMap<lldb::core_id_t, llvm::ArrayRef<uint8_t>> &buffers,
     const std::vector<IntelPTThreadContinousExecution> &executions);
 
+/// Given an intel pt trace, split it in chunks delimited by PSB packets. Each of these chunks
+/// is guaranteed to have been executed continuously.
+///
+/// \param[in] trace_intel_pt
+///   The main Trace object that contains all the information related to the trace session.
+///
+/// \param[in] buffer
+///   The intel pt buffer that belongs to a single thread or to a single cpu core.
+///
+/// \return
+///   A list of continuous executions sorted by time, or an \a llvm::Error in case of failures.
 llvm::Expected<std::vector<IntelPTThreadSubtrace>>
 SplitTraceInContinuousExecutions(TraceIntelPT &trace_intel_pt,
                                  llvm::ArrayRef<uint8_t> buffer);
