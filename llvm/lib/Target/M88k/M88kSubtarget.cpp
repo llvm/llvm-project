f4a4d2008f32ab1554db13c747c0d184b316d38f
Kai Nacke <kai@redstar.de>
2022-06-15 08:56:11 -0400

    [m88k] Add subtarget feature for procs and cache info
    - Adds SubtargetFeature<> for both processors
    - Implement methods returning info about cache structure

--- llvm/lib/Target/M88k/M88kSubtarget.cpp
+++ llvm/lib/Target/M88k/M88kSubtarget.cpp
@@ -27,8 +27,6 @@ using namespace llvm;
 #define GET_SUBTARGETINFO_CTOR
 #include "M88kGenSubtargetInfo.inc"
 
-void M88kSubtarget::anchor() {}
-
 M88kSubtarget::M88kSubtarget(const Triple &TT, const std::string &CPU,
                              const std::string &FS, const TargetMachine &TM)
     : M88kGenSubtargetInfo(TT, CPU, /*TuneCPU*/ CPU, FS), TargetTriple(TT),
@@ -40,4 +38,43 @@ M88kSubtarget::M88kSubtarget(const Triple &TT, const std::string &CPU,
   RegBankInfo.reset(RBI);
   InstSelector.reset(createM88kInstructionSelector(
       *static_cast<const M88kTargetMachine *>(&TM), *this, *RBI));
+}
+
+Optional<unsigned> M88kSubtarget::getCacheSize(unsigned Level) const {
+  if (Level > 0)
+    return Optional<unsigned>();
+  switch (M88kProc) {
+  case MC88100:
+    return 16 * 1024; // 16k sharec I+D cache.
+  case MC88110:
+    return 8 * 1024; // 8k sharec D cache.
+  default:
+    return Optional<unsigned>();
+  }
+}
+
+Optional<unsigned> M88kSubtarget::getCacheAssociativity(unsigned Level) const {
+  if (Level > 0)
+    return Optional<unsigned>();
+  switch (M88kProc) {
+  case MC88100:
+    return 4; // Cache is 4-way associative.
+  case MC88110:
+    return 2; // Cache is 4-way associative.
+  default:
+    return Optional<unsigned>();
+  }
+}
+
+Optional<unsigned> M88kSubtarget::getCacheLineSize(unsigned Level) const {
+  if (Level > 0)
+    return Optional<unsigned>();
+  switch (M88kProc) {
+  case MC88100:
+    return 16; // 4 bytes.
+  case MC88110:
+    return 32; // 8 words.
+  default:
+    return Optional<unsigned>();
+  }
 }
