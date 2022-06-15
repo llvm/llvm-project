a179ebe7d8107a54930204468a27f6e826ff7a1c
David Sherwood <david.sherwood@arm.com>
2022-06-15 15:10:16 +0100

    [AArch64][SME] Add SME addha/va intrinsics
    This patch adds new the following SME intrinsics:
    
      @llvm.aarch64.sme.addva
      @llvm.aarch64.sme.addha
    
    Differential Revision: https://reviews.llvm.org/D127861

--- llvm/lib/Target/AArch64/AArch64ISelLowering.h
+++ llvm/lib/Target/AArch64/AArch64ISelLowering.h
@@ -571,6 +571,9 @@ public:
                                             MachineInstr &MI,
                                             MachineBasicBlock *BB) const;
   MachineBasicBlock *EmitZero(MachineInstr &MI, MachineBasicBlock *BB) const;
+  MachineBasicBlock *EmitAddVectorToTile(unsigned Opc, unsigned BaseReg,
+                                         MachineInstr &MI,
+                                         MachineBasicBlock *BB) const;
 
   MachineBasicBlock *
   EmitInstrWithCustomInserter(MachineInstr &MI,
