41f6a03965f7549b88633b9b03fe59dec7899f9e
Daniel Bertalan <dani@danielbertalan.dev>
2022-06-15 19:17:07 -0400

    Reland "[lld-macho] Group undefined symbol diagnostics by symbol".
    This reverts commit 0a0c6d5ca58ce4fa8fab607569de393d46e00fd6.
    
    This relands f830a55bd84e194d6 with the fix described in
    https://reviews.llvm.org/D127753#3587390

--- lld/ELF/Relocations.cpp
+++ lld/ELF/Relocations.cpp
@@ -558,6 +558,7 @@ static std::string maybeReportDiscarded(Undefined &sym) {
   return msg;
 }
 
+namespace {
 // Undefined diagnostics are collected in a vector and emitted once all of
 // them are known, so that some postprocessing on the list of undefined symbols
 // can happen before lld emits diagnostics.
@@ -571,7 +572,8 @@ struct UndefinedDiag {
   bool isWarning;
 };
 
-static std::vector<UndefinedDiag> undefs;
+std::vector<UndefinedDiag> undefs;
+}
 
 // Check whether the definition name def is a mangled function name that matches
 // the reference name ref.
