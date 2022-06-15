2380856eff3518b26f641a19ed9925015b4aff0d
tanmaytirpankar <tirpankartanmay@gmail.com>
2022-06-15 10:45:15 -0600

    Added Storage Table for atomic condition and tested storage

--- llvm/include/llvm/Transforms/ErrorAnalysis/AtomicCondition/AtomicCondition.h
+++ llvm/include/llvm/Transforms/ErrorAnalysis/AtomicCondition/AtomicCondition.h
@@ -6,64 +6,265 @@
 #define LLVM_ATOMICCONDITION_H
 
 #include <math.h>
-#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-// ----------------------------------------------------------------------
-// --------------- Atomic Condition Calculating Functions ---------------
-// ----------------------------------------------------------------------
+// Atomic Condition storage
+template<typename FloatType>
+class ACItem {
+  string XName;
+  FloatType X;
+  string YName;
+  FloatType Y;
+  int OP;
+  int WRTOperand;
+  FloatType AC;
+public:
+  ACItem() {}
+  ACItem(string XName, FloatType X,
+         string YName, FloatType Y,
+         int OP, int WRTOperand) : XName(XName), X(X), YName(YName), Y(Y), OP(OP), WRTOperand(WRTOperand) {}
+  ACItem(string XName, FloatType X,
+         string YName, FloatType Y,
+         int OP, int WRTOperand,
+         FloatType AC) : XName(XName), X(X), YName(YName), Y(Y), OP(OP), WRTOperand(WRTOperand), AC(AC) {}
+  bool operator==(const ACItem &rhs) const {
+    return XName == rhs.XName && X == rhs.X && YName == rhs.YName &&
+           Y == rhs.Y && OP == rhs.OP && WRTOperand == rhs.WRTOperand &&
+           AC == rhs.AC;
+  }
+  bool operator!=(const ACItem &rhs) const { return !(rhs == *this); }
 
-template<typename FP>
-FP acBinaryAdd(FP X, FP Y, int WRTOperand) {
-  FP AC;
+  void setAC(FloatType ACValue) {AC = ACValue;}
+  bool isUnary() { return false; }
+  bool isBinary() { return OP==14 || OP==16 || OP== 18 || OP == 21; }
+  const string &getXName() const { return XName; }
+  const string &getYName() const { return YName; }
+  FloatType getX() const { return X; }
+  FloatType getY() const { return Y; }
+  int getOp() const { return OP; }
+  int getWrtOperand() const { return WRTOperand; }
+  FloatType getAc() const { return AC; }
+};
+
+class ACTable {
+public:
+  uint64_t NumItems;
+  vector<ACItem<float>> FP32ACItems;
+  vector<ACItem<double>> FP64ACItems;
+
+  ACTable() {};
+  ACTable(uint64_t NumItems): NumItems(NumItems) {
+//    ACItems = new ACItem[NumItems];
+//    for(uint64_t i=0; i<NumItems; i++)
+//      ACItems[i] = NULL;
+  }
+
+//  ~ACTable() {
+//    delete[] ACItems;
+//  }
+};
+
+ACTable *StorageTable;
+
+float fACfp32BinaryAdd(string XName, float X, string YName, float Y, int WRTOperand);
+float fACfp32BinarySub(string XName, float X, string YName, float Y, int WRTOperand);
+float fACfp32BinaryMul(string XName, float X, string YName, float Y, int WRTOperand);
+float fACfp32BinaryDiv(string XName, float X, string YName, float Y, int WRTOperand);
+
+double fACfp64BinaryAdd(string XName, double X, string YName, double Y, int WRTOperand);
+double fACfp64BinarySub(string XName, double X, string YName, double Y, int WRTOperand);
+double fACfp64BinaryMul(string XName, double X, string YName, double Y, int WRTOperand);
+double fACfp64BinaryDiv(string XName, double X, string YName, double Y, int WRTOperand);
+
+
+void fACCreate(uint64_t NumItems) {
+  StorageTable = new ACTable(NumItems);
+}
+
+// Driver function selecting atomic condition function for unary float operation
+float fACfp32UnaryDriver(string XName, float X, int OP) {
+  switch (OP) {
+  default:
+    printf("No such operation\n");
+  }
+  return 0.0;
+}
+
+// Driver function selecting atomic condition function for binary float operation
+float fACfp32BinaryDriver(string XName, float X, string YName, float Y, int OP, int WRTOperand) {
+  switch (OP) {
+  case 14:
+    return fACfp32BinaryAdd(XName, X, YName, Y, WRTOperand);
+  case 16:
+    return fACfp32BinarySub(XName, X, YName, Y, WRTOperand);
+  case 18:
+    return fACfp32BinaryMul(XName, X, YName, Y, WRTOperand);
+  case 21:
+    return fACfp32BinaryDiv(XName, X, YName, Y, WRTOperand);
+  default:
+    printf("No such operation\n");
+  }
+  return 0.0;
+}
+
+// Driver function selecting atomic condition function for unary double operation
+double fACfp64UnaryDriver(string XName, double X, int OP) {
+  switch (OP) {
+  default:
+    printf("No such operation\n");
+  }
+  return 0.0;
+}
+
+// Driver function selecting atomic condition function for binary double operation
+double fACfp64BinaryDriver(string XName, double X, string YName, double Y, int OP, int WRTOperand) {
+  switch (OP) {
+  case 14:
+    return fACfp64BinaryAdd(XName, X, YName, Y, WRTOperand);
+  case 16:
+    return fACfp64BinarySub(XName, X, YName, Y, WRTOperand);
+  case 18:
+    return fACfp64BinaryMul(XName, X, YName, Y, WRTOperand);
+  case 21:
+    return fACfp64BinaryDiv(XName, X, YName, Y, WRTOperand);
+  default:
+    printf("No such operation\n");
+  }
+  return 0.0;
+}
+
+// ---------------------------------------------------------------------------
+// --------------- fp32 Atomic Condition Calculating Functions ---------------
+// ---------------------------------------------------------------------------
+
+float fACfp32BinaryAdd(string XName, float X, string YName, float Y, int WRTOperand) {
+  float AC;
   if(WRTOperand == 1) {
     AC = abs(X / (X+Y));
   } else if(WRTOperand == 2) {
     AC = abs(Y / (X+Y));
   } else {
-    cout << "There is no operand " << WRTOperand << ".\n";
+    printf("There is no operand %d.\n", WRTOperand);
+    return AC;
   }
-  cout <<"AC of x+y | x=" << X << ", y=" << Y <<
-         " WRT " << (WRTOperand==1?'x':'y') << " is " << AC << '\n';
+  printf("AC of x+y | x=%f, y=%f WRT %c is %f.\n",
+         X, Y, (WRTOperand==1?'x':'y'), AC);
+  
+  StorageTable->FP32ACItems.push_back(ACItem<float>(XName, X, YName, Y, 14, WRTOperand, AC));
 
   return AC;
 }
 
-template<typename FP>
-FP acBinarySub(FP X, FP Y, int WRTOperand) {
-  FP AC;
+float fACfp32BinarySub(string XName, float X, string YName, float Y, int WRTOperand) {
+  float AC;
   if(WRTOperand == 1) {
     AC = abs(X / (X-Y));
   } else if(WRTOperand == 2) {
     AC = abs(Y / (Y-X));
   } else {
-    cout << "There is no operand " << WRTOperand << ".\n";
+    printf("There is no operand %d.\n", WRTOperand);
+    return AC;
   }
-  cout <<"AC of x-y | x=" << X << ", y=" << Y <<
-      " WRT " << (WRTOperand==1?'x':'y') << " is " << AC << '\n';
+  printf("AC of x-y | x=%f, y=%f WRT %c is %f.\n",
+         X, Y, (WRTOperand==1?'x':'y'), AC);
+  
+  StorageTable->FP32ACItems.push_back(ACItem<float>(XName, X, YName, Y, 16, WRTOperand, AC));
 
   return AC;
 }
 
-template<typename FP>
-FP acBinaryMul(FP X, FP Y, int WRTOperand) {
-  FP AC=1;
-  cout <<"AC of x*y | x=" << X << ", y=" << Y <<
-      " WRT " << (WRTOperand==1?'x':'y') << " is " << AC << '\n';
+float fACfp32BinaryMul(string XName, float X, string YName, float Y, int WRTOperand) {
+  float AC=1.0;
+  printf("AC of x*y | x=%f, y=%f WRT %c is %f.\n",
+         X, Y, (WRTOperand==1?'x':'y'), AC);
+  if (WRTOperand != 1 && WRTOperand != 2)
+    return AC;
+  
+  StorageTable->FP32ACItems.push_back(ACItem<float>(XName, X, YName, Y, 18, WRTOperand, AC));
 
   return AC;
 }
 
-template<typename FP>
-FP acBinaryDiv(FP X, FP Y, int WRTOperand) {
-  FP AC=1;
-  cout <<"AC of x+y | x=" << X << ", y=" << Y <<
-      " WRT " << (WRTOperand==1?'x':'y') << " is " << AC << '\n';
+float fACfp32BinaryDiv(string XName, float X, string YName, float Y, int WRTOperand) {
+  float AC=1.0;
+  printf("AC of x/y | x=%f, y=%f WRT %c is %f.\n",
+         X, Y, (WRTOperand==1?'x':'y'), AC);
+  if (WRTOperand != 1 && WRTOperand != 2)
+    return AC;
+  
+  StorageTable->FP32ACItems.push_back(ACItem<float>(XName, X, YName, Y, 21, WRTOperand, AC));
 
   return AC;
 }
 
+
+// ---------------------------------------------------------------------------
+// --------------- fp64 Atomic Condition Calculating Functions ---------------
+// ---------------------------------------------------------------------------
+
+double fACfp64BinaryAdd(string XName, double X, string YName, double Y, int WRTOperand) {
+  double AC = 0;
+  if(WRTOperand == 1) {
+    AC = abs(X / (X+Y));
+  } else if(WRTOperand == 2) {
+    AC = abs(Y / (X+Y));
+  } else {
+    printf("There is no operand %d.\n", WRTOperand);
+    return AC;
+  }
+  printf("AC of x+y | x=%lf, y=%lf WRT %c is %lf.\n",
+         X, Y, (WRTOperand==1?'x':'y'), AC);
+
+  StorageTable->FP64ACItems.push_back(ACItem<double>(XName, X, YName, Y, 14, WRTOperand, AC));
+
+  return AC;
+}
+
+double fACfp64BinarySub(string XName, double X, string YName, double Y, int WRTOperand) {
+  double AC;
+  if(WRTOperand == 1) {
+    AC = abs(X / (X-Y));
+  } else if(WRTOperand == 2) {
+    AC = abs(Y / (Y-X));
+  } else {
+    printf("There is no operand %d.\n", WRTOperand);
+    return AC;
+  }
+  printf("AC of x-y | x=%lf, y=%lf WRT %c is %lf.\n",
+         X, Y, (WRTOperand==1?'x':'y'), AC);
+
+  StorageTable->FP64ACItems.push_back(ACItem<double>(XName, X, YName, Y, 16, WRTOperand, AC));
+
+  return AC;
+}
+
+double fACfp64BinaryMul(string XName, double X, string YName, double Y, int WRTOperand) {
+  double AC=1.0;
+  printf("AC of x*y | x=%lf, y=%lf WRT %c is %lf.\n",
+         X, Y, (WRTOperand==1?'x':'y'), AC);
+  if (WRTOperand != 1 && WRTOperand != 2)
+    return AC;
+
+  StorageTable->FP64ACItems.push_back(ACItem<double>(XName, X, YName, Y, 18, WRTOperand, AC));
+
+  return AC;
+}
+
+double fACfp64BinaryDiv(string XName, double X, string YName, double Y, int WRTOperand) {
+  double AC=1.0;
+  printf("AC of x/y | x=%lf, y=%lf WRT %c is %lf.\n",
+         X, Y, (WRTOperand==1?'x':'y'), AC);
+  if (WRTOperand != 1 && WRTOperand != 2)
+    return AC;
+
+  StorageTable->FP64ACItems.push_back(ACItem<double>(XName, X, YName, Y, 21, WRTOperand, AC));
+
+  return AC;
+}
 
 
 #endif // LLVM_ATOMICCONDITION_H
--- llvm/unittests/Transforms/ErrorAnalysis/AtomicConditionTest.cpp
+++ llvm/unittests/Transforms/ErrorAnalysis/AtomicConditionTest.cpp
@@ -5,34 +5,76 @@
 #include "llvm/Transforms/ErrorAnalysis/AtomicCondition/AtomicCondition.h"
 #include "gtest/gtest.h"
 
-TEST(AtomicCondition, BinaryOperationAdd) {
-  EXPECT_FLOAT_EQ(acBinaryAdd(2.0, 4.0, 1), 0.33333333);
-  EXPECT_DOUBLE_EQ(acBinaryAdd(2.0, 4.0, 1), 0.33333333333333333);
+// ---------------------------------------------------------------------------
+// --------------- fp32 Atomic Condition Tests ---------------
+// ---------------------------------------------------------------------------
 
-  EXPECT_FLOAT_EQ(acBinaryAdd(2.0, 4.0, 2), 0.66666666);
-  EXPECT_DOUBLE_EQ(acBinaryAdd(2.0, 4.0, 2), 0.66666666666666666);
+TEST(AtomicConditionFP32, FP32UnaryOperations) {
+
 }
 
-TEST(AtomicCondition, BinaryOperationSub) {
-  EXPECT_FLOAT_EQ(acBinarySub(2.0, 4.0, 1), 1.0);
-  EXPECT_DOUBLE_EQ(acBinarySub(2.0, 4.0, 1), 1.0);
+TEST(AtomicConditionFP32, FP32BinaryOperations) {
+  fACCreate(8);
 
-  EXPECT_FLOAT_EQ(acBinarySub(2.0, 4.0, 2), 2.0);
-  EXPECT_DOUBLE_EQ(acBinarySub(2.0, 4.0, 2), 2.0);
+  fACfp32BinaryDriver("AddTest1Op1", 2.0, "AddTest1Op2", 4.0, 14, 1);
+  EXPECT_EQ(StorageTable->FP32ACItems.back(), ACItem<float>("AddTest1Op1", 2.0, "AddTest1Op2", 4.0, 14, 1, 0.33333333));
+  fACfp32BinaryDriver("AddTest2Op1", 2.0, "AddTest2Op2", 4.0, 14, 2);
+  EXPECT_EQ(StorageTable->FP32ACItems.back(), ACItem<float>("AddTest2Op1", 2.0, "AddTest2Op2", 4.0, 14, 2, 0.66666666));
+
+
+  fACfp32BinaryDriver("SubTest1Op1", 2.0, "SubTest1Op2", 4.0, 16, 1);
+  EXPECT_EQ(StorageTable->FP32ACItems.back(), ACItem<float>("SubTest1Op1", 2.0, "SubTest1Op2", 4.0, 16, 1, 1.0));
+  fACfp32BinaryDriver("SubTest2Op1", 2.0, "SubTest2Op2", 4.0, 16, 2);
+  EXPECT_EQ(StorageTable->FP32ACItems.back(), ACItem<float>("SubTest2Op1", 2.0, "SubTest2Op2", 4.0, 16, 2, 2.0));
+
+  fACfp32BinaryDriver("MulTest1Op1", 2.0, "MulTest1Op2", 4.0, 18, 1);
+  EXPECT_EQ(StorageTable->FP32ACItems.back(), ACItem<float>("MulTest1Op1", 2.0, "MulTest1Op2", 4.0, 18, 1, 1.0));
+  fACfp32BinaryDriver("MulTest2Op1", 2.0, "MulTest2Op2", 4.0, 18, 2);
+  EXPECT_EQ(StorageTable->FP32ACItems.back(), ACItem<float>("MulTest2Op1", 2.0, "MulTest2Op2", 4.0, 18, 2, 1.0));
+
+  fACfp32BinaryDriver("DivTest1Op1", 2.0, "DivTest1Op2", 4.0, 21, 1);
+  EXPECT_EQ(StorageTable->FP32ACItems.back(), ACItem<float>("DivTest1Op1", 2.0, "DivTest1Op2", 4.0, 21, 1, 1.0));
+  fACfp32BinaryDriver("DivTest2Op1", 2.0, "DivTest2Op2", 4.0, 21, 2);
+  EXPECT_EQ(StorageTable->FP32ACItems.back(), ACItem<float>("DivTest2Op1", 2.0, "DivTest2Op2", 4.0, 21, 2, 1.0));
+
+
+
+  delete StorageTable;
 }
 
-TEST(AtomicCondition, BinaryOperationMul) {
-  EXPECT_FLOAT_EQ(acBinaryMul(2.0, 4.0, 1), 1.0);
-  EXPECT_DOUBLE_EQ(acBinaryMul(2.0, 4.0, 1), 1.0);
 
-  EXPECT_FLOAT_EQ(acBinaryMul(2.0, 4.0, 2), 1.0);
-  EXPECT_DOUBLE_EQ(acBinaryMul(2.0, 4.0, 2), 1.0);
+// ---------------------------------------------------------------------------
+// --------------- fp64 Atomic Condition Tests ---------------
+// ---------------------------------------------------------------------------
+
+TEST(AtomicConditionFP64, FP64UnaryOperations) {
+
 }
 
-TEST(AtomicCondition, BinaryOperationDiv) {
-  EXPECT_FLOAT_EQ(acBinaryDiv(2.0, 4.0, 1), 1.0);
-  EXPECT_DOUBLE_EQ(acBinaryDiv(2.0, 4.0, 1), 1.0);
+TEST(AtomicConditionFP64, FP64BinaryOperations) {
+  fACCreate(8);
 
-  EXPECT_FLOAT_EQ(acBinaryMul(2.0, 4.0, 2), 1.0);
-  EXPECT_DOUBLE_EQ(acBinaryMul(2.0, 4.0, 2), 1.0);
+  fACfp64BinaryDriver("AddTest1Op1", 2.0, "AddTest1Op2", 4.0, 14, 1);
+  EXPECT_EQ(StorageTable->FP64ACItems.back(), ACItem<double>("AddTest1Op1", 2.0, "AddTest1Op2", 4.0, 14, 1, 0.33333333333333333));
+  fACfp64BinaryDriver("AddTest2Op1", 2.0, "AddTest2Op2", 4.0, 14, 2);
+  EXPECT_EQ(StorageTable->FP64ACItems.back(), ACItem<double>("AddTest2Op1", 2.0, "AddTest2Op2", 4.0, 14, 2, 0.66666666666666666));
+
+  fACfp64BinaryDriver("SubTest1Op1", 2.0, "SubTest1Op2", 4.0, 16,1);
+  EXPECT_EQ(StorageTable->FP64ACItems.back(), ACItem<double>("SubTest1Op1", 2.0, "SubTest1Op2", 4.0, 16,1, 1.0));
+  fACfp64BinaryDriver("SubTest2Op1", 2.0, "SubTest2Op2", 4.0, 16,2);
+  EXPECT_EQ(StorageTable->FP64ACItems.back(), ACItem<double>("SubTest2Op1", 2.0, "SubTest2Op2", 4.0, 16,2, 2.0));
+
+  fACfp64BinaryDriver("MulTest1Op1", 2.0, "MulTest1Op2", 4.0, 18, 1);
+  EXPECT_EQ(StorageTable->FP64ACItems.back(), ACItem<double>("MulTest1Op1", 2.0, "MulTest1Op2", 4.0, 18, 1, 1.0));
+  fACfp64BinaryDriver("MulTest2Op1", 2.0, "MulTest2Op2", 4.0, 18, 2);
+  EXPECT_EQ(StorageTable->FP64ACItems.back(), ACItem<double>("MulTest2Op1", 2.0, "MulTest2Op2", 4.0, 18, 2, 1.0));
+
+  fACfp64BinaryDriver("DivTest1Op1", 2.0, "DivTest1Op2", 4.0, 21, 1);
+  EXPECT_EQ(StorageTable->FP64ACItems.back(), ACItem<double>("DivTest1Op1", 2.0, "DivTest1Op2", 4.0, 21, 1, 1.0));
+  fACfp64BinaryDriver("DivTest2Op1", 2.0, "DivTest2Op2", 4.0, 21, 2);
+  EXPECT_EQ(StorageTable->FP64ACItems.back(), ACItem<double>("DivTest2Op1", 2.0, "DivTest2Op2", 4.0, 21, 2, 1.0));
+
+  EXPECT_EQ(int(StorageTable->FP64ACItems.size()), 8);
+
+  delete StorageTable;
 }
