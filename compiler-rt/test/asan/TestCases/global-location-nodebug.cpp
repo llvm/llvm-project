b76fcc118f67314bd46d7675554be65a9e603513
Mitch Phillips <31459023+hctim@users.noreply.github.com>
2022-06-16 10:23:26 -0700

    Reland "[ASan] Use debuginfo for symbolization."
    This reverts commit 6d81060c657006c5fb175f4e9ed988d27df7604a.
    
    Hint: Looking here because your manual invocation of something in
    'check-asan' broke? You need a new symbolizer (after D123538).
    
    An upcoming patch will remove the internal metadata for global
    variables. With D123534 and D123538, clang now emits DWARF debug info
    for constant strings (the only global variable type it was missing), and
    llvm-symbolizer is now able to symbolize all global variable addresses
    (where previously it wouldn't give you the file:line information).
    
    Move ASan's runtime over from the internal metadata to DWARF.
    
    Differential Revision: https://reviews.llvm.org/D127552

---
+++ compiler-rt/test/asan/TestCases/global-location-nodebug.cpp
@@ -0,0 +1,16 @@
+/// Same as global-location.cpp, but without debuginfo. In a separate file to
+/// allow this test to also run on Windows (which can't be done for the
+/// debuginfo variant).
+
+// RUN: %clangxx_asan -O2 %S/global-location.cpp -o %t -Wl,-S
+// RUN: not %run %t g 2>&1 | FileCheck %s --check-prefix=CHECK --check-prefix=GLOB-NO-G
+// RUN: not %run %t c 2>&1 | FileCheck %s --check-prefix=CHECK --check-prefix=CLASS_STATIC-NO-G
+// RUN: not %run %t f 2>&1 | FileCheck %s --check-prefix=CHECK --check-prefix=FUNC_STATIC-NO-G
+// RUN: not %run %t l 2>&1 | FileCheck %s --check-prefix=CHECK --check-prefix=LITERAL-NO-G
+
+// CHECK: AddressSanitizer: global-buffer-overflow
+// CLASS_STATIC-NO-G: 0x{{.*}} is located 4 bytes to the right of global variable 'C::array' defined in '{{.*}}global-location.cpp' {{.*}} of size 40
+// GLOB-NO-G: 0x{{.*}} is located 4 bytes to the right of global variable 'global' defined in '{{.*}}global-location.cpp' {{.*}} of size 40
+// FUNC_STATIC-NO-G: 0x{{.*}} is located 4 bytes to the right of global variable 'array' defined in '{{.*}}global-location.cpp' {{.*}} of size 40
+// LITERAL-NO-G: 0x{{.*}} is located 0 bytes to the right of global variable {{.*}} defined in '{{.*}}global-location.cpp' {{.*}} of size 11
+// CHECK: SUMMARY: AddressSanitizer: global-buffer-overflow
