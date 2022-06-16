feca1d4e66a86603853d19864ee52720330ad224
Congzhe Cao <congzhe.cao@huawei.com>
2022-06-16 16:03:30 -0400

    [Delinearization] Refactoring of fixed-size array delinearization
    This is a follow-up patch to D122857 where we added delinearization of
    fixed-size arrays to loop cache analysis, which resulted in some duplicate
    code, i.e., "tryDelinearizeFixedSize()", in LoopCacheCost.cpp and
    DependenceAnalysis.cpp. Refactoring is done in this patch.
    
    This patch refactors out the main logic of "tryDelinearizeFixedSize()" as
    "tryDelinearizeFixedSizeImpl()" and moves it to Delinearization.cpp, such that
    clients can reuse "llvm::tryDelinearizeFixedSizeImpl()" wherever they would
    like to delinearize fixed-size arrays. Currently it has two users, i.e.,
    DependenceAnalysis.cpp and LoopCacheCost.cpp.
    
    Reviewed By: Meinersbur, #loopoptwg
    
    Differential Revision: https://reviews.llvm.org/D124745

--- llvm/include/llvm/Analysis/LoopCacheAnalysis.h
+++ llvm/include/llvm/Analysis/LoopCacheAnalysis.h
@@ -98,9 +98,9 @@ private:
   /// Attempt to delinearize the indexed reference.
   bool delinearize(const LoopInfo &LI);
 
-  bool tryDelinearizeFixedSize(ScalarEvolution *SE, Instruction *Src,
-                               const SCEV *SrcAccessFn,
-                               SmallVectorImpl<const SCEV *> &SrcSubscripts);
+  /// Attempt to delinearize \p AccessFn for fixed-size arrays.
+  bool tryDelinearizeFixedSize(const SCEV *AccessFn,
+                               SmallVectorImpl<const SCEV *> &Subscripts);
 
   /// Return true if the index reference is invariant with respect to loop \p L.
   bool isLoopInvariant(const Loop &L) const;
