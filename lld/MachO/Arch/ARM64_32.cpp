3c24fae3986aea9a920025c87d0feb3d03c6e876
Keith Smiley <keithbsmiley@gmail.com>
2022-06-16 21:35:18 -0700

    [lld-macho] Add support for objc_msgSend stubs
    Apple Clang in Xcode 14 introduced a new feature for reducing the
    overhead of objc_msgSend calls by deduplicating the setup calls for each
    individual selector. This works by clang adding undefined symbols for
    each selector called in a translation unit, such as `_objc_msgSend$foo`
    for calling the `foo` method on any `NSObject`. There are 2
    different modes for this behavior, the default directly does the setup
    for `_objc_msgSend` and calls it, and the smaller option does the
    selector setup, and then calls the standard `_objc_msgSend` stub
    function.
    
    The general overview of how this works is:
    
    - Undefined symbols with the given prefix are collected
    - The suffix of each matching undefined symbol is added as a string to
      `__objc_methname`
    - A pointer is added for every method name in the `__objc_selrefs`
      section
    - A `got` entry is emitted for `_objc_msgSend`
    - Stubs are emitting pointing to the synthesized locations
    
    Notes:
    
    - Both `__objc_methname` and `__objc_selrefs` can also exist from object
      files, so their contents are merged with our synthesized contents
    - The compiler emits method names for defined methods, but not for
      undefined symbols you call, but stubs are used for both
    - This only implements the default "fast" mode currently just to reduce
      the diff, I also doubt many folks will care to swap modes
    - This only implements this for arm64 and x86_64, we don't need to
      implement this for 32 bit iOS archs, but we should implement it for
      watchOS archs in a later diff
    
    Differential Revision: https://reviews.llvm.org/D128108

--- lld/MachO/Arch/ARM64_32.cpp
+++ lld/MachO/Arch/ARM64_32.cpp
@@ -33,6 +33,10 @@ struct ARM64_32 : ARM64Common {
   void writeStubHelperHeader(uint8_t *buf) const override;
   void writeStubHelperEntry(uint8_t *buf, const Symbol &,
                             uint64_t entryAddr) const override;
+  void writeObjCMsgSendStub(uint8_t *buf, Symbol *sym, uint64_t stubsAddr,
+                            uint64_t stubOffset, uint64_t selrefsVA,
+                            uint64_t selectorIndex, uint64_t gotAddr,
+                            uint64_t msgSendIndex) const override;
 };
 
 } // namespace
@@ -92,6 +96,14 @@ static constexpr uint32_t stubHelperEntryCode[] = {
 void ARM64_32::writeStubHelperEntry(uint8_t *buf8, const Symbol &sym,
                                     uint64_t entryVA) const {
   ::writeStubHelperEntry(buf8, stubHelperEntryCode, sym, entryVA);
+}
+
+void ARM64_32::writeObjCMsgSendStub(uint8_t *buf, Symbol *sym,
+                                    uint64_t stubsAddr, uint64_t stubOffset,
+                                    uint64_t selrefsVA, uint64_t selectorIndex,
+                                    uint64_t gotAddr,
+                                    uint64_t msgSendIndex) const {
+  fatal("TODO: implement this");
 }
 
 ARM64_32::ARM64_32() : ARM64Common(ILP32()) {
