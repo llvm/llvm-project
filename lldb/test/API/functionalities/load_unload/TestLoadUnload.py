59f664f03c873781df78aa477df6bd9f9dbc4ce0
Muhammad Omair Javaid <omair.javaid@linaro.org>
2022-06-17 11:46:27 +0400

    [LLDB] XFAIL TestLoadUnload fails on Arm/Ubuntu Jammy
    This patch marks following tests as XFAIL for Arm/Ubuntu Jammy 22.04:
    test_lldb_process_load_and_unload_commands
    test_load_unload

--- lldb/test/API/functionalities/load_unload/TestLoadUnload.py
+++ lldb/test/API/functionalities/load_unload/TestLoadUnload.py
@@ -203,6 +203,7 @@ class LoadUnloadTestCase(TestBase):
         hostoslist=["windows"],
         triple='.*-android')
     @expectedFailureAll(oslist=["windows"]) # process load not implemented
+    @expectedFailureAll(oslist=["linux"], archs=["arm"]) # Fails on ubuntu jammy
     def test_lldb_process_load_and_unload_commands(self):
         self.setSvr4Support(False)
         self.run_lldb_process_load_and_unload_commands()
@@ -296,6 +297,7 @@ class LoadUnloadTestCase(TestBase):
         self.runCmd("process continue")
 
     @expectedFailureAll(oslist=["windows"]) # breakpoint not hit
+    @expectedFailureAll(oslist=["linux"], archs=["arm"]) # Fails on ubuntu jammy
     def test_load_unload(self):
         self.setSvr4Support(False)
         self.run_load_unload()
