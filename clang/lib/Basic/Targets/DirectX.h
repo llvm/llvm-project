77f72ac15bcac8923c6bd104f303feaa1e964692
Xiang Li <python3kgae@outlook.com>
2022-06-19 15:37:00 -0700

    [HLSL] Enable half type for hlsl.
    HLSL supports half type.
    When enable-16bit-types is not set, half will be treated as float.
    When enable-16bit-types is set, half will be treated like real 16bit float type and map to llvm half type.
    Also change CXXABI to Microsoft to match dxc behavior.
    The mangle name for half is "$f16@" when half is treat as native half type and "$halff@" when treat as float.
    
    In AST, half is still half.
    The special thing is done at clang codeGen, when NativeHalfType is false, half will translated into float.
    
    Reviewed By: aaron.ballman
    
    Differential Revision: https://reviews.llvm.org/D124790

--- clang/lib/Basic/Targets/DirectX.h
+++ clang/lib/Basic/Targets/DirectX.h
@@ -57,8 +57,9 @@ public:
     NoAsmVariants = true;
     resetDataLayout("e-m:e-p:32:32-i1:32-i8:8-i16:16-i32:32-i64:64-f16:16-f32:"
                     "32-f64:64-n8:16:32:64");
+    TheCXXABI.set(TargetCXXABI::Microsoft);
   }
-
+  bool useFP16ConversionIntrinsics() const override { return false; }
   void getTargetDefines(const LangOptions &Opts,
                         MacroBuilder &Builder) const override;
 
