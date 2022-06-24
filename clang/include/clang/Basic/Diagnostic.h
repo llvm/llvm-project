946c45a4ed5d5e2f262110a27390369f0d8fc3eb
Tapasweni Pathak <tapaswenipathak@gmail.com>
2022-06-24 11:11:20 +0000

    Implement soft reset of the diagnostics engine.
    This patch implements soft reset and adds tests for soft reset success of the
    diagnostics engine. This allows us to recover from errors in clang-repl without
    resetting the pragma handlers' state.
    
    Differential revision: https://reviews.llvm.org/D126183

--- clang/include/clang/Basic/Diagnostic.h
+++ clang/include/clang/Basic/Diagnostic.h
@@ -545,6 +545,7 @@ public:
   DiagnosticsEngine &operator=(const DiagnosticsEngine &) = delete;
   ~DiagnosticsEngine();
 
+  friend void DiagnosticsTestHelper(DiagnosticsEngine &);
   LLVM_DUMP_METHOD void dump() const;
   LLVM_DUMP_METHOD void dump(StringRef DiagName) const;
 
@@ -891,9 +892,9 @@ public:
     LastDiagLevel = Other.LastDiagLevel;
   }
 
-  /// Reset the state of the diagnostic object to its initial
-  /// configuration.
-  void Reset();
+  /// Reset the state of the diagnostic object to its initial configuration.
+  /// \param[in] soft - if true, doesn't reset the diagnostic mappings and state
+  void Reset(bool soft = false);
 
   //===--------------------------------------------------------------------===//
   // DiagnosticsEngine classification and reporting interfaces.
