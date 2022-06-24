de2675428c4c07ca72d17cba7927c6f796c4576f
Adrian Prantl <aprantl@apple.com>
2022-06-24 10:47:42 -0700

    Unskip passing testcase
    (cherry picked from commit ef2acd5bea0e7ccf4022f10958939f57939956b7)

--- lldb/test/API/lang/swift/protocols/class_protocol/TestSwiftClassConstrainedProtocolArgument.py
+++ lldb/test/API/lang/swift/protocols/class_protocol/TestSwiftClassConstrainedProtocolArgument.py
@@ -8,7 +8,4 @@ from lldbsuite.test.decorators import *
 lldbinline.MakeInlineTest(
     __file__, globals(),
             decorators=[
-            swiftTest,skipUnlessDarwin,
-            skipIf(bugnumber="rdar://60396797", # should work but crashes.
-                   setting=('symbols.use-swift-clangimporter', 'false'))
-                ])
+            swiftTest,skipUnlessDarwin])
