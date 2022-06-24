1f4a92d61a8b8e72e43f97539d130493f65c6e04
Adrian Prantl <aprantl@apple.com>
2022-06-24 09:57:35 -0700

    Unskip passing test
    (cherry picked from commit 0cb1742c94db70a29fff8f6d43229f737e1b0cf2)

--- lldb/test/API/lang/swift/enum_objc/TestEnumObjC.py
+++ lldb/test/API/lang/swift/enum_objc/TestEnumObjC.py
@@ -5,9 +5,6 @@ lldbinline.MakeInlineTest(
     __file__,
     globals(),
     decorators=[
-        skipIf(
-            bugnumber="rdar://60396797",  # should work but crashes.
-            setting=('symbols.use-swift-clangimporter', 'false')),
         expectedFailureAll(oslist=["linux"], bugnumber="rdar://83444822"),
         swiftTest
     ])
