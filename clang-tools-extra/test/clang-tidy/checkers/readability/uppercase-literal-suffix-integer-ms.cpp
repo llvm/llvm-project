9081d3d8097af5e89905faf0ed568ba7e848e8df
Haojian Wu <hokein.wu@gmail.com>
2022-06-24 11:34:58 +0200

    [clang-tidy] Make the cert/uppercase-literal-suffix-integer fully hermetic.
    after the test-reorg commit (89a1d03e2b379e325daa5249411e414bbd995b5e), the
    cert/uppercase test starts to fail in our internal environment -- it accesses
    a header file from "../readability", which is not friendly to a hermetic test environment.
    
    This change makes the test fully hermetic, and does some cleanup on the
    uppercase header (I think it is better to move it the share
    Inputs/Header directory, and rename it)
    
    Differential Revision: https://reviews.llvm.org/D128511

--- clang-tools-extra/test/clang-tidy/checkers/readability/uppercase-literal-suffix-integer-ms.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/readability/uppercase-literal-suffix-integer-ms.cpp
@@ -1,9 +1,9 @@
-// RUN: %check_clang_tidy %s readability-uppercase-literal-suffix %t -- -- -target x86_64-pc-linux-gnu -I %S -fms-extensions
+// RUN: %check_clang_tidy %s readability-uppercase-literal-suffix %t -- -- -target x86_64-pc-linux-gnu -I %clang_tidy_headers -fms-extensions
 // RUN: grep -Ev "// *[A-Z-]+:" %s > %t.cpp
-// RUN: clang-tidy %t.cpp -checks='-*,readability-uppercase-literal-suffix' -fix -- -target x86_64-pc-linux-gnu -I %S -fms-extensions
-// RUN: clang-tidy %t.cpp -checks='-*,readability-uppercase-literal-suffix' -warnings-as-errors='-*,readability-uppercase-literal-suffix' -- -target x86_64-pc-linux-gnu -I %S -fms-extensions
+// RUN: clang-tidy %t.cpp -checks='-*,readability-uppercase-literal-suffix' -fix -- -target x86_64-pc-linux-gnu -I %clang_tidy_headers -fms-extensions
+// RUN: clang-tidy %t.cpp -checks='-*,readability-uppercase-literal-suffix' -warnings-as-errors='-*,readability-uppercase-literal-suffix' -- -target x86_64-pc-linux-gnu -I %clang_tidy_headers -fms-extensions
 
-#include "uppercase-literal-suffix.h"
+#include "integral_constant.h"
 
 void integer_suffix() {
   static constexpr auto v0 = __LINE__; // synthetic
