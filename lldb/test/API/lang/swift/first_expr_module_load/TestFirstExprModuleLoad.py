66f3973640db30a70c69681690ee8e13d766d705
Adrian Prantl <aprantl@apple.com>
2022-06-24 10:33:27 -0700

    Unskip passing testcase
    (cherry picked from commit f0ca8867b09a8f8d90d6bed482ab2794580bb9b3)

--- lldb/test/API/lang/swift/first_expr_module_load/TestFirstExprModuleLoad.py
+++ lldb/test/API/lang/swift/first_expr_module_load/TestFirstExprModuleLoad.py
@@ -1,7 +1,5 @@
 import lldbsuite.test.lldbinline as lldbinline
 from lldbsuite.test.decorators import *
 
-lldbinline.MakeInlineTest(__file__, globals(), decorators=[swiftTest, skipUnlessFoundation, skipIf(oslist=['windows']),
-    skipIf(bugnumber="rdar://60396797", # should work but crashes.
-            setting=('symbols.use-swift-clangimporter', 'false'))
+lldbinline.MakeInlineTest(__file__, globals(), decorators=[swiftTest, skipUnlessFoundation, skipIf(oslist=['windows'])
 ])
