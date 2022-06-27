416d6504eccd73a7a9c8543d1b59ef4bb4e6bfa5
Valentin Clement <clementval@gmail.com>
2022-06-27 15:00:07 +0200

    [flang][NFC] Add IO lowering tests
    These tests were left behind or only partially upstreamed during
    the lower code upstreaming.
    
    This patch is part of the upstreaming effort from fir-dev branch.
    
    Reviewed By: PeteSteinfeld
    
    Differential Revision: https://reviews.llvm.org/D128634
    
    Co-authored-by: Kiran Chandramohan <kiran.chandramohan@arm.com>
    Co-authored-by: Jean Perier <jperier@nvidia.com>

---
+++ flang/test/Lower/io-implied-do-fixes.f90
@@ -0,0 +1,49 @@
+! RUN: bbc -emit-fir %s -o - | FileCheck %s
+! UNSUPPORTED: system-windows
+
+! CHECK-LABEL: func @_QPido1
+! CHECK: %[[J_REF_ADDR:.*]] = fir.alloca !fir.ptr<i32> {uniq_name = "_QFido1Eiptr.addr"}
+! CHECK: %[[J_ADDR:.*]] = fir.load %[[J_REF_ADDR]] : !fir.ref<!fir.ptr<i32>>
+! CHECK: %[[J_VAL_FINAL:.*]] = fir.do_loop %[[J_VAL:.*]] = %{{.*}} to %{{.*}} step %{{.*}} -> index {
+! CHECK:   %[[J_VAL_CVT1:.*]] = fir.convert %[[J_VAL]] : (index) -> i32
+! CHECK:   fir.store %[[J_VAL_CVT1]] to %[[J_ADDR]] : !fir.ptr<i32>
+! CHECK: }
+! CHECK: %[[J_VAL_CVT2:.*]] = fir.convert %[[J_VAL_FINAL]] : (index) -> i32
+! CHECK: fir.store %[[J_VAL_CVT2]] to %[[J_ADDR]] : !fir.ptr<i32>
+subroutine ido1
+  integer, pointer :: iptr
+  integer, target :: itgt
+  iptr => itgt
+  print *, (iptr,iptr=1,10)
+end subroutine
+
+! CHECK-LABEL: func @_QPido2
+! CHECK: %[[J_REF_ADDR:.*]] = fir.alloca !fir.heap<i32> {uniq_name = "_QFido2Eiptr.addr"}
+! CHECK: %[[J_ADDR:.*]] = fir.load %[[J_REF_ADDR]] : !fir.ref<!fir.heap<i32>>
+! CHECK: %[[J_VAL_FINAL:.*]] = fir.do_loop %[[J_VAL:.*]] = %{{.*}} to %{{.*}} step %{{.*}} -> index {
+! CHECK: %[[J_VAL_CVT1:.*]] = fir.convert %[[J_VAL]] : (index) -> i32
+! CHECK: fir.store %[[J_VAL_CVT1]] to %[[J_ADDR]] : !fir.heap<i32>
+! CHECK: }
+! CHECK: %[[J_VAL_CVT2:.*]] = fir.convert %[[J_VAL_FINAL]] : (index) -> i32
+! CHECK: fir.store %[[J_VAL_CVT2]] to %[[J_ADDR]] : !fir.heap<i32>
+subroutine ido2
+  integer, allocatable :: iptr
+  allocate(iptr)
+  print *, (iptr,iptr=1,10)
+end subroutine
+
+! CHECK-LABEL: func @_QPido3
+! CHECK:  %[[J_REF_ADDR:.*]] = fir.alloca !fir.heap<i32> {uniq_name = "_QFido3Ej.addr"}
+! CHECK:  %[[J_ADDR:.*]] = fir.load %[[J_REF_ADDR]] : !fir.ref<!fir.heap<i32>>
+! CHECK:  %[[J_VAL_FINAL:.*]]:2 = fir.iterate_while (%[[J_VAL:.*]] = %{{.*}} to %{{.*}} step %{{.*}}) and ({{.*}}) -> (index, i1) {
+! CHECK:    %[[J_VAL_CVT1:.*]] = fir.convert %[[J_VAL]] : (index) -> i32
+! CHECK:    fir.store %[[J_VAL_CVT1]] to %[[J_ADDR]] : !fir.heap<i32>
+! CHECK:  }
+! CHECK:  %[[J_VAL_CVT2:.*]] = fir.convert %[[J_VAL_FINAL]]#0 : (index) -> i32
+! CHECK:  fir.store %[[J_VAL_CVT2]] to %[[J_ADDR]] : !fir.heap<i32
+subroutine ido3
+  integer, allocatable :: j
+  allocate(j)
+  write(*,*,err=404) (j,j=1,10)
+404 continue
+end subroutine
