ffdc31604a283ae92046d19fea4760458d1976db
Lian Wang <Lian.Wang@streamcomputing.com>
2022-06-29 06:28:26 +0000

    [RISCV] Add cost model for vector.reverse mask operation
    Reviewed By: craig.topper
    
    Differential Revision: https://reviews.llvm.org/D128784

--- llvm/lib/Target/RISCV/RISCVTargetTransformInfo.cpp
+++ llvm/lib/Target/RISCV/RISCVTargetTransformInfo.cpp
@@ -198,6 +198,9 @@ InstructionCost RISCVTTIImpl::getShuffleCost(TTI::ShuffleKind Kind,
       //   vid.v v9
       //   vrsub.vx v10, v9, a0
       //   vrgather.vv v9, v8, v10
+      if (Tp->getElementType()->isIntegerTy(1))
+        // Mask operation additionally required extend and truncate
+        return LT.first * 9;
       return LT.first * 6;
     }
   }
