dd01645e00ad84c09278e87a043cc5c29e8b5827
Valentin Clement <clementval@gmail.com>
2022-06-30 10:36:47 +0200

    [flang] Fix for array upper bounds with *
    Even though the array is declared with '*' upper bounds, it has an
    initial value that has a statically known shape. Use the shape from
    the type of the initializer when the declared size is '*'.
    
    This patch is part of the upstreaming effort from fir-dev branch.
    
    Reviewed By: jeanPerier
    
    Differential Revision: https://reviews.llvm.org/D128889
    
    Co-authored-by: Eric Schweitz <eschweitz@nvidia.com>

---
+++ flang/test/Lower/memory-alloc.f90
@@ -0,0 +1,19 @@
+! RUN: bbc -o - %s | FileCheck %s
+
+! CHECK-LABEL: func @_QMw0bPtest1(
+! CHECK: %[[TWO:.*]] = arith.constant 2 : index
+! CHECK: %[[HEAP:.*]] = fir.allocmem !fir.array<?x!fir.logical<4>>, %[[TWO]] {uniq_name = ".array.expr"}
+! CHECK: fir.freemem %[[HEAP]] : !fir.heap<!fir.array<?x!fir.logical<4>>>
+
+Module w0b
+  Integer,Parameter :: a(*,*) = Reshape( [ 1,2,3,4 ], [ 2,2 ])
+contains
+  Subroutine test1(i,expect)
+    Integer,Intent(In) :: i,expect(:)
+    Logical :: ok = .True.
+    If (Any(a(:,i)/=expect)) Then
+      !Print *,'FAIL 1:',a(:,i),'/=',expect
+      ok = .False.
+    End If
+  End Subroutine
+End Module
