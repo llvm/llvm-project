d5d420bb3bcc2eea926fb70fc3e6818c32e1eecc
Fangrui Song <i@maskray.me>
2022-06-29 22:08:02 -0700

    [flang][test] Remove RUN LINES?/EXPECTED OUTPUT.*/INPUT markers from test/Driver
    Follow-up to D128763.

--- flang/test/Driver/pic-flags.f90
+++ flang/test/Driver/pic-flags.f90
@@ -1,16 +1,10 @@
 ! Verify that in contrast to Clang, Flang does not default to generating position independent executables/code
 
-!-------------
-! RUN COMMANDS
-!-------------
 ! RUN: %flang -### %s --target=aarch64-linux-gnu 2>&1 | FileCheck %s --check-prefix=CHECK-NOPIE
 ! RUN: %flang -### %s --target=aarch64-linux-gnu -fno-pie 2>&1 | FileCheck %s --check-prefix=CHECK-NOPIE
 
 ! RUN: %flang -### %s --target=aarch64-linux-gnu -fpie 2>&1 | FileCheck %s --check-prefix=CHECK-PIE
 
-!----------------
-! EXPECTED OUTPUT
-!----------------
 ! CHECK-NOPIE: "-fc1"
 ! CHECk-NOPIE-NOT: "-fpic"
 ! CHECK-NOPIE: "{{.*}}ld"
