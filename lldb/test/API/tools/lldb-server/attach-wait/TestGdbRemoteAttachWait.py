2d8c1a4c2fe9c2ef45080df2aaa4ee515785b0e6
Pavel Labath <pavel@labath.sk>
2022-07-01 14:32:50 +0200

    [lldb/test] Don't use preexec_fn for launching inferiors
    As the documentation states, using this is not safe in multithreaded
    programs, and I have traced it to a rare deadlock in some of the tests.
    
    The reason this was introduced was to be able to attach to a program
    from the very first instruction, where our usual mechanism of
    synchronization -- waiting for a file to appear -- does not work.
    
    However, this is only needed for a single test
    (TestGdbRemoteAttachWait) so instead of doing this everywhere, I create
    a bespoke solution for that single test. The solution basically
    consists of outsourcing the preexec_fn code to a separate (and
    single-threaded) shim process, which enables attaching and then executes
    the real program.
    
    This pattern could be generalized in case we needed to use it for other
    tests, but I suspect that we will not be having many tests like this.
    
    This effectively reverts commit
    022faf873c8f65c5d537f68d316c9ccb6f5deaa3.

--- lldb/test/API/tools/lldb-server/attach-wait/TestGdbRemoteAttachWait.py
+++ lldb/test/API/tools/lldb-server/attach-wait/TestGdbRemoteAttachWait.py
@@ -14,10 +14,12 @@ class TestGdbRemoteAttachWait(gdbremote_testcase.GdbRemoteTestCaseBase):
     @skipIfWindows # This test is flaky on Windows
     def test_attach_with_vAttachWait(self):
         exe = '%s_%d' % (self.testMethodName, os.getpid())
+        exe_to_attach = exe
+        args = []
 
         def launch_inferior():
             inferior = self.launch_process_for_attach(
-                inferior_args=["sleep:60"],
+                inferior_args=args,
                 exe_path=self.getBuildArtifact(exe))
             self.assertIsNotNone(inferior)
             self.assertTrue(inferior.pid > 0)
@@ -26,7 +28,14 @@ class TestGdbRemoteAttachWait(gdbremote_testcase.GdbRemoteTestCaseBase):
                     inferior.pid, True))
             return inferior
 
-        self.build(dictionary={'EXE': exe})
+        self.build(dictionary={'EXE': exe, 'CXX_SOURCES': 'main.cpp'})
+        if self.getPlatform() != "windows":
+            # Use a shim to ensure that the process is ready to be attached from
+            # the get-go.
+            args = [self.getBuildArtifact(exe)]
+            exe = "shim"
+            self.build(dictionary={'EXE': exe, 'CXX_SOURCES': 'shim.cpp'})
+
         self.set_inferior_startup_attach_manually()
 
         server = self.connect_to_debug_monitor()
@@ -38,7 +47,8 @@ class TestGdbRemoteAttachWait(gdbremote_testcase.GdbRemoteTestCaseBase):
         self.do_handshake()
         self.test_sequence.add_log_lines([
             # Do the attach.
-            "read packet: $vAttachWait;{}#00".format(lldbgdbserverutils.gdbremote_hex_encode_string(exe)),
+            "read packet: $vAttachWait;{}#00".format(
+                lldbgdbserverutils.gdbremote_hex_encode_string(exe_to_attach)),
         ], True)
         # Run the stream until attachWait.
         context = self.expect_gdbremote_sequence()
