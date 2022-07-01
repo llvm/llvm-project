eb7a02ea009b6a2aa3689ee92905fb6acea905fc
Diana Picus <diana.picus@linaro.org>
2022-07-01 11:43:20 +0000

    [flang] Single entry point for GET_COMMAND_ARGUMENT
    This patch refactors the runtime support for GET_COMMAND_ARGUMENT to
    have a single entry point instead of 2. It also updates lowering
    accordingly.
    
    This makes it easier to handle dynamically optional arguments. See also
    https://reviews.llvm.org/D118777
    
    Differential Revision: https://reviews.llvm.org/D130475

--- flang/lib/Optimizer/Builder/Runtime/Command.cpp
+++ flang/lib/Optimizer/Builder/Runtime/Command.cpp
@@ -32,27 +32,19 @@ mlir::Value fir::runtime::genCommandArgumentCount(fir::FirOpBuilder &builder,
   return builder.create<fir::CallOp>(loc, argumentCountFunc).getResult(0);
 }
 
-mlir::Value fir::runtime::genArgumentValue(fir::FirOpBuilder &builder,
-                                           mlir::Location loc,
-                                           mlir::Value number,
-                                           mlir::Value value,
-                                           mlir::Value errmsg) {
-  auto argumentValueFunc =
-      fir::runtime::getRuntimeFunc<mkRTKey(ArgumentValue)>(loc, builder);
-  llvm::SmallVector<mlir::Value> args = fir::runtime::createArguments(
-      builder, loc, argumentValueFunc.getFunctionType(), number, value, errmsg);
-  return builder.create<fir::CallOp>(loc, argumentValueFunc, args).getResult(0);
-}
-
-mlir::Value fir::runtime::genArgumentLength(fir::FirOpBuilder &builder,
-                                            mlir::Location loc,
-                                            mlir::Value number) {
-  auto argumentLengthFunc =
-      fir::runtime::getRuntimeFunc<mkRTKey(ArgumentLength)>(loc, builder);
-  llvm::SmallVector<mlir::Value> args = fir::runtime::createArguments(
-      builder, loc, argumentLengthFunc.getFunctionType(), number);
-  return builder.create<fir::CallOp>(loc, argumentLengthFunc, args)
-      .getResult(0);
+mlir::Value fir::runtime::genGetCommandArgument(
+    fir::FirOpBuilder &builder, mlir::Location loc, mlir::Value number,
+    mlir::Value value, mlir::Value length, mlir::Value errmsg) {
+  auto runtimeFunc =
+      fir::runtime::getRuntimeFunc<mkRTKey(GetCommandArgument)>(loc, builder);
+  mlir::FunctionType runtimeFuncTy = runtimeFunc.getFunctionType();
+  mlir::Value sourceFile = fir::factory::locationToFilename(builder, loc);
+  mlir::Value sourceLine =
+      fir::factory::locationToLineNo(builder, loc, runtimeFuncTy.getInput(5));
+  llvm::SmallVector<mlir::Value> args =
+      fir::runtime::createArguments(builder, loc, runtimeFuncTy, number, value,
+                                    length, errmsg, sourceFile, sourceLine);
+  return builder.create<fir::CallOp>(loc, runtimeFunc, args).getResult(0);
 }
 
 mlir::Value fir::runtime::genEnvVariableValue(
