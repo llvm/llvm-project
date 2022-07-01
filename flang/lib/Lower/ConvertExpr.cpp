bdf4620b2069ba281ac1a6a0e08fc6d59bc0eb56
Slava Zakharin <szakharin@nvidia.com>
2022-07-01 14:22:29 -0700

    [flang] Changed lowering for allocatable assignment to make array-value-copy correct.
    Array-value-copy fails to generate a temporary array for case like this:
    subroutine bug(b)
      real, allocatable :: b(:)
      b = b(2:1:-1)
    end subroutine
    
    Since LHS may need to be reallocated, lowering produces the following FIR:
    %rhs_load = fir.array_load %b %slice
    
    %lhs_mem = fir.if %b_is_allocated_with_right_shape {
       fir.result %b
    } else {
       %new_storage = fir.allocmem %rhs_shape
       fir.result %new_storage
    }
    
    %lhs = fir.array_load %lhs_mem
    %loop = fir.do_loop {
     ....
    }
    fir.array_merge_store %lhs, %loop to %lhs_mem
    // deallocate old storage if reallocation occured,
    // and update b descriptor if needed.
    
    Since %b in array_load and %lhs_mem in array_merge_store are not the same SSA
    values, array-value-copy does not detect the conflict and does not produce
    a temporary array. This causes incorrect result in runtime.
    
    The suggested change in lowering is to generate this:
    %rhs_load = fir.array_load %b %slice
    %lhs_mem = fir.if %b_is_allocated_with_right_shape {
       %lhs = fir.array_load %b
       %loop = fir.do_loop {
          ....
       }
       fir.array_merge_store %lhs, %loop to %b
       fir.result %b
    } else {
       %new_storage = fir.allocmem %rhs_shape
       %lhs = fir.array_load %new_storage
       %loop = fir.do_loop {
          ....
       }
       fir.array_merge_store %lhs, %loop to %new_storage
       fir.result %new_storage
    }
    // deallocate old storage if reallocation occured,
    // and update b descriptor if needed.
    
    Note that there are actually 3 branches in FIR, so the assignment loops
    are currently produced in three copies, which is a code-size issue.
    It is possible to generate just two branches with two copies of the loops,
    but it is not addressed in this change-set.
    
    Differential Revision: https://reviews.llvm.org/D129314

--- flang/lib/Lower/ConvertExpr.cpp
+++ flang/lib/Lower/ConvertExpr.cpp
@@ -3633,6 +3633,38 @@ public:
     ael.lowerAllocatableArrayAssignment(lhs, rhs);
   }
 
+  /// Lower an assignment to allocatable array, where the LHS array
+  /// is represented with \p lhs extended value produced in different
+  /// branches created in genReallocIfNeeded(). The RHS lowering
+  /// is provided via \p rhsCC continuation.
+  void lowerAllocatableArrayAssignment(ExtValue lhs, CC rhsCC) {
+    mlir::Location loc = getLoc();
+    // Check if the initial destShape is null, which means
+    // it has not been computed from rhs (e.g. rhs is scalar).
+    bool destShapeIsEmpty = destShape.empty();
+    // Create ArrayLoad for the mutable box and save it into `destination`.
+    PushSemantics(ConstituentSemantics::ProjectedCopyInCopyOut);
+    ccStoreToDest = genarr(lhs);
+    // destShape is either non-null on entry to this function,
+    // or has been just set by lhs lowering.
+    assert(!destShape.empty() && "destShape must have been set.");
+    // Finish lowering the loop nest.
+    assert(destination && "destination must have been set");
+    ExtValue exv = lowerArrayExpression(rhsCC, destination.getType());
+    if (!explicitSpaceIsActive())
+      builder.create<fir::ArrayMergeStoreOp>(
+          loc, destination, fir::getBase(exv), destination.getMemref(),
+          destination.getSlice(), destination.getTypeparams());
+    // destShape may originally be null, if rhs did not define a shape.
+    // In this case the destShape is computed from lhs, and we may have
+    // multiple different lhs values for different branches created
+    // in genReallocIfNeeded(). We cannot reuse destShape computed
+    // in different branches, so we have to reset it,
+    // so that it is recomputed for the next branch FIR generation.
+    if (destShapeIsEmpty)
+      destShape.clear();
+  }
+
   /// Assignment to allocatable array.
   ///
   /// The semantics are reverse that of a "regular" array assignment. The rhs
@@ -3648,7 +3680,6 @@ public:
     // array.
     fir::MutableBoxValue mutableBox =
         Fortran::lower::createMutableBox(loc, converter, lhs, symMap);
-    mlir::Type resultTy = converter.genType(rhs);
     if (rhs.Rank() > 0)
       determineShapeOfDest(rhs);
     auto rhsCC = [&]() {
@@ -3679,25 +3710,16 @@ public:
       auto lbs = fir::factory::getOrigins(arrayOperands[0].shape);
       lbounds.append(lbs.begin(), lbs.end());
     }
+    auto assignToStorage = [&](fir::ExtendedValue newLhs) {
+      // The lambda will be called repeatedly by genReallocIfNeeded().
+      lowerAllocatableArrayAssignment(newLhs, rhsCC);
+    };
     fir::factory::MutableBoxReallocation realloc =
         fir::factory::genReallocIfNeeded(builder, loc, mutableBox, destShape,
-                                         lengthParams);
-    // Create ArrayLoad for the mutable box and save it into `destination`.
-    PushSemantics(ConstituentSemantics::ProjectedCopyInCopyOut);
-    ccStoreToDest = genarr(realloc.newValue);
-    // If the rhs is scalar, get shape from the allocatable ArrayLoad.
-    if (destShape.empty())
-      destShape = getShape(destination);
-    // Finish lowering the loop nest.
-    assert(destination && "destination must have been set");
-    ExtValue exv = lowerArrayExpression(rhsCC, resultTy);
+                                         lengthParams, assignToStorage);
     if (explicitSpaceIsActive()) {
       explicitSpace->finalizeContext();
-      builder.create<fir::ResultOp>(loc, fir::getBase(exv));
-    } else {
-      builder.create<fir::ArrayMergeStoreOp>(
-          loc, destination, fir::getBase(exv), destination.getMemref(),
-          destination.getSlice(), destination.getTypeparams());
+      builder.create<fir::ResultOp>(loc, fir::getBase(realloc.newValue));
     }
     fir::factory::finalizeRealloc(builder, loc, mutableBox, lbounds,
                                   takeLboundsIfRealloc, realloc);
@@ -7214,8 +7236,9 @@ private:
                              Fortran::lower::ImplicitIterSpace *impSpace)
       : converter{converter}, builder{converter.getFirOpBuilder()},
         stmtCtx{stmtCtx}, symMap{symMap},
-        explicitSpace(expSpace->isActive() ? expSpace : nullptr),
-        implicitSpace(impSpace->empty() ? nullptr : impSpace), semant{sem} {
+        explicitSpace((expSpace && expSpace->isActive()) ? expSpace : nullptr),
+        implicitSpace((impSpace && !impSpace->empty()) ? impSpace : nullptr),
+        semant{sem} {
     // Generate any mask expressions, as necessary. This is the compute step
     // that creates the effective masks. See 10.2.3.2 in particular.
     genMasks();
