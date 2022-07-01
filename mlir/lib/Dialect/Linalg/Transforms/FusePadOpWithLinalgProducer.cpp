7fc4518f4a70fd99460c941480a7fc521f97a7e7
lorenzo chelini <l.chelini@icloud.com>
2022-07-01 20:21:09 +0200

    [MLIR] Rename FusePadOpWithLinalgConsumer -> FusePadOpWithLinalgProducer (NFC)
    Follow up after D128978, where I mistakenly rename the file. The linalg op is
    fused with its producer, not the consumer.

--- mlir/lib/Dialect/Linalg/Transforms/FusePadOpWithLinalgProducer.cpp
+++ mlir/lib/Dialect/Linalg/Transforms/FusePadOpWithLinalgProducer.cpp
@@ -1,4 +1,4 @@
-//===- FusePadOpWithLinalgConsumer.cpp ---- Fuse pad with linalg producer -===//
+//===- FusePadOpWithLinalgProducer.cpp ---- Fuse pad with linalg producer -===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
