0ce40d1ffe7ffe47ea04c4f7a8046a78eca94423
Christian Sigg <csigg@google.com>
2022-07-04 08:11:30 +0200

    [mlir] Add InferIntRangeInterface to gpu.launch
    Infers block/grid dimensions/indices or ranges of such dimensions/indices.
    
    Reviewed By: krzysz00
    
    Differential Revision: https://reviews.llvm.org/D129036

--- mlir/include/mlir/Dialect/GPU/IR/GPUDialect.h
+++ mlir/include/mlir/Dialect/GPU/IR/GPUDialect.h
@@ -22,6 +22,7 @@
 #include "mlir/IR/OpDefinition.h"
 #include "mlir/IR/OpImplementation.h"
 #include "mlir/IR/SymbolTable.h"
+#include "mlir/Interfaces/InferIntRangeInterface.h"
 #include "mlir/Interfaces/InferTypeOpInterface.h"
 #include "mlir/Interfaces/SideEffectInterfaces.h"
 
