e2ad0f176054caeb28f1898766e9e7f6b08b5af6
Nikita Popov <npopov@redhat.com>
2022-07-04 16:01:12 +0200

    [BPI] Avoid ConstantExpr::get()
    Use ConstantFoldBinaryOpOperands() instead, to prepare for the case
    where not all binary operators have a constant expression form.
    
    I believe this code actually intended to set OnlyIfReduced=true,
    however ConstantExpr::get() actually accepts a Flags argument at
    that position (and OnlyIfReducedTy as the next argument), so this
    ended up creating a constant expression with some random flag
    (probably exact or nuw depending on which).

--- llvm/lib/Analysis/BranchProbabilityInfo.cpp
+++ llvm/lib/Analysis/BranchProbabilityInfo.cpp
@@ -15,6 +15,7 @@
 #include "llvm/ADT/SCCIterator.h"
 #include "llvm/ADT/STLExtras.h"
 #include "llvm/ADT/SmallVector.h"
+#include "llvm/Analysis/ConstantFolding.h"
 #include "llvm/Analysis/LoopInfo.h"
 #include "llvm/Analysis/PostDominators.h"
 #include "llvm/Analysis/TargetLibraryInfo.h"
@@ -629,9 +630,10 @@ computeUnlikelySuccessors(const BasicBlock *BB, Loop *L,
       if (!CmpLHSConst || !llvm::is_contained(successors(BB), B))
         continue;
       // First collapse InstChain
+      const DataLayout &DL = BB->getModule()->getDataLayout();
       for (Instruction *I : llvm::reverse(InstChain)) {
-        CmpLHSConst = ConstantExpr::get(I->getOpcode(), CmpLHSConst,
-                                        cast<Constant>(I->getOperand(1)), true);
+        CmpLHSConst = ConstantFoldBinaryOpOperands(
+            I->getOpcode(), CmpLHSConst, cast<Constant>(I->getOperand(1)), DL);
         if (!CmpLHSConst)
           break;
       }
