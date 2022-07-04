a126e1c4fbee1a4ec6c30cbc581bab514a7c7655
Haojian Wu <hokein.wu@gmail.com>
2022-07-04 14:15:51 +0200

    [pseudo] Use the prebuilt cxx grammar for the lit tests, NFC.
    Differential Revision: https://reviews.llvm.org/D129074

--- clang-tools-extra/pseudo/test/cxx/template-empty-type-parameter.cpp
+++ clang-tools-extra/pseudo/test/cxx/template-empty-type-parameter.cpp
@@ -1,3 +1,3 @@
-// RUN: clang-pseudo -grammar=%cxx-bnf-file -source=%s --print-forest | FileCheck %s
+// RUN: clang-pseudo -grammar=cxx -source=%s --print-forest | FileCheck %s
 template <typename> struct MatchParents;
 // CHECK: template-parameter-list~TYPENAME := tok[2]
