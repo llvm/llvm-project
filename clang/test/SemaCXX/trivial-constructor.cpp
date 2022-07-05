c113b96e22fea3dcddba76ff3beb4c0c0c4e7a08
Roy Jacobson <roi.jacobson1@gmail.com>
2022-07-06 01:37:31 +0300

    [Sema] Add deprecation warnings for some compiler provided __has_* type traits
    Some compiler provided type traits like __has_trivial_constructor have been documented
    as deprecated for quite some time.
    Still, some people apparently still use them, even though mixing them with concepts
    and with deleted functions leads to weird results. There's also disagreement about some
    edge cases between GCC (which Clang claims to follow) and MSVC.
    
    This patch adds deprecation warnings for the usage of those builtins, except for __has_trivial_destructor
    which doesn't have a GCC alternative.
    
    I made the warning on by default, so I had to silence it for some tests but it's not too many.
    
    Some (decade old) history of issues with those builtins:
    https://github.com/llvm/llvm-project/issues/18187
    https://github.com/llvm/llvm-project/issues/18559
    https://github.com/llvm/llvm-project/issues/22161
    https://github.com/llvm/llvm-project/issues/33063
    
    The abseil usage of them that triggered me to add this warning:
    https://github.com/abseil/abseil-cpp/issues/1201
    
    Weird interaction of those builtins with C++20's conditionally trivial special member functions:
    https://gcc.gnu.org/bugzilla/show_bug.cgi?id=106085
    
    Reviewed By: #clang-language-wg, aaron.ballman
    
    Differential Revision: https://reviews.llvm.org/D129170

--- clang/test/SemaCXX/trivial-constructor.cpp
+++ clang/test/SemaCXX/trivial-constructor.cpp
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -fsyntax-only -verify %s -std=c++11
+// RUN: %clang_cc1 -fsyntax-only -verify %s -std=c++11 -Wno-deprecated-builtins
 // expected-no-diagnostics
 struct T1 {
 };
--- clang/test/SemaCXX/type-traits.cpp
+++ clang/test/SemaCXX/type-traits.cpp
@@ -1,6 +1,6 @@
-// RUN: %clang_cc1 -triple x86_64-apple-darwin10 -fsyntax-only -verify -std=gnu++11 -fms-extensions -Wno-microsoft %s
-// RUN: %clang_cc1 -triple x86_64-apple-darwin10 -fsyntax-only -verify -std=gnu++14 -fms-extensions -Wno-microsoft %s
-// RUN: %clang_cc1 -triple x86_64-apple-darwin10 -fsyntax-only -verify -std=gnu++1z -fms-extensions -Wno-microsoft %s
+// RUN: %clang_cc1 -triple x86_64-apple-darwin10 -fsyntax-only -verify -std=gnu++11 -Wno-deprecated-builtins -fms-extensions -Wno-microsoft %s
+// RUN: %clang_cc1 -triple x86_64-apple-darwin10 -fsyntax-only -verify -std=gnu++14 -Wno-deprecated-builtins -fms-extensions -Wno-microsoft %s
+// RUN: %clang_cc1 -triple x86_64-apple-darwin10 -fsyntax-only -verify -std=gnu++1z -Wno-deprecated-builtins -fms-extensions -Wno-microsoft %s
 
 #define T(b) (b) ? 1 : -1
 #define F(b) (b) ? -1 : 1
