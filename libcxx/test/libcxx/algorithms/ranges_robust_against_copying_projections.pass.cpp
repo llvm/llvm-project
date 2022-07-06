126a2578bf7b9c0857c6b4344741580743800e35
Nikolas Klauser <nikolasklauser@berlin.de>
2022-07-06 14:03:00 +0200

    [libc++] Implement ranges::remove{, _if}
    Reviewed By: var-const, #libc
    
    Spies: huixie90, sstefan1, libcxx-commits, mgorny
    
    Differential Revision: https://reviews.llvm.org/D128618

--- libcxx/test/libcxx/algorithms/ranges_robust_against_copying_projections.pass.cpp
+++ libcxx/test/libcxx/algorithms/ranges_robust_against_copying_projections.pass.cpp
@@ -174,10 +174,10 @@ constexpr bool all_the_algorithms()
     //(void)std::ranges::remove_copy(a, first2, value, Proj(&copies)); assert(copies == 0);
     //(void)std::ranges::remove_copy_if(first, last, first2, UnaryTrue(), Proj(&copies)); assert(copies == 0);
     //(void)std::ranges::remove_copy_if(a, first2, UnaryTrue(), Proj(&copies)); assert(copies == 0);
-    //(void)std::ranges::remove(first, last, value, Proj(&copies)); assert(copies == 0);
-    //(void)std::ranges::remove(a, value, Proj(&copies)); assert(copies == 0);
-    //(void)std::ranges::remove_if(first, last, UnaryTrue(), Proj(&copies)); assert(copies == 0);
-    //(void)std::ranges::remove_if(a, UnaryTrue(), Proj(&copies)); assert(copies == 0);
+    (void)std::ranges::remove(first, last, value, Proj(&copies)); assert(copies == 0);
+    (void)std::ranges::remove(a, value, Proj(&copies)); assert(copies == 0);
+    (void)std::ranges::remove_if(first, last, UnaryTrue(), Proj(&copies)); assert(copies == 0);
+    (void)std::ranges::remove_if(a, UnaryTrue(), Proj(&copies)); assert(copies == 0);
     //(void)std::ranges::replace_copy(first, last, first2, value, T(), Proj(&copies)); assert(copies == 0);
     //(void)std::ranges::replace_copy(a, first2, value, T(), Proj(&copies)); assert(copies == 0);
     //(void)std::ranges::replace_copy_if(first, last, first2, UnaryTrue(), T(), Proj(&copies)); assert(copies == 0);
