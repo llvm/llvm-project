18a30f6257a8bd7274878f25fbf4a6e013e42bc4
Jun Zhang <jun@junz.org>
2022-07-07 22:14:04 +0800

    [clang-repl][NFC] Split weak symbol test to a new test
    Windows has some issues when we try to use `__attribute__((weak))` in
    JIT, so we disabled that. But it's not worth to disable the whole test
    just for this single feature. This patch split that part from the
    original test so we can keep testing stuff that normally working in
    Windows.
    
    Signed-off-by: Jun Zhang <jun@junz.org>
    
    Differential Revision: https://reviews.llvm.org/D129250

--- clang/test/Interpreter/execute-weak.cpp
+++ clang/test/Interpreter/execute-weak.cpp
@@ -3,19 +3,12 @@
 // RUN:            'auto r1 = printf("i = %d\n", i);' | FileCheck --check-prefix=CHECK-DRIVER %s
 // REQUIRES: host-supports-jit
 // UNSUPPORTED: system-aix
+// XFAIL: system-windows
 // CHECK-DRIVER: i = 10
 // RUN: cat %s | clang-repl | FileCheck %s
 extern "C" int printf(const char *, ...);
-int i = 42;
-auto r1 = printf("i = %d\n", i);
-// CHECK: i = 42
-
-struct S { float f = 1.0; S *m = nullptr;} s;
-
-auto r2 = printf("S[f=%f, m=0x%llx]\n", s.f, reinterpret_cast<unsigned long long>(s.m));
-// CHECK-NEXT: S[f=1.000000, m=0x0]
-
-inline int foo() { return 42; }
-int r3 = foo();
+int __attribute__((weak)) bar() { return 42; }
+auto r4 = printf("bar() = %d\n", bar());
+// CHECK: bar() = 42
 
 %quit
