13d50d0b08708f11c364983e373080ac946ed980
Cole Kissane <cole.kissane@gmail.com>
2022-07-08 11:46:51 -0700

    [llvm] cmake config groundwork to have ZSTD in LLVM
    - added `FindZSTD.cmake`
    - added a CMake option `LLVM_ENABLE_ZSTD` with behavior mirroring that of `LLVM_ENABLE_ZLIB`
    - likewise added have_zstd to compiler-rt/test/lit.common.cfg.py, clang-tools-extra/clangd/test/lit.cfg.py, and several lit.site.cfg.py.in files mirroring have_zlib behavior
    
    Reviewed By: leonardchan, MaskRay
    
    Differential Revision: https://reviews.llvm.org/D128465

--- compiler-rt/test/lit.common.cfg.py
+++ compiler-rt/test/lit.common.cfg.py
@@ -217,6 +217,9 @@ config.available_features.add("host-byteorder-" + sys.byteorder + "-endian")
 
 if config.have_zlib == "1":
   config.available_features.add("zlib")
+  
+if config.have_zstd == "1":
+  config.available_features.add("zstd")
 
 # Use ugly construction to explicitly prohibit "clang", "clang++" etc.
 # in RUN lines.
