6a4d79214e606157af203897952996bcea508dc3
Hubert Tong <hubert.reinterpretcast@gmail.com>
2022-07-08 18:30:48 -0400

    [AIX][clang/test] Set/propagate AIXTHREAD_STK for AIX
    Some tests perform deep recursion, which requires a larger pthread stack
    size than the relatively low default of 192 KiB for 64-bit processes on
    AIX. The `AIXTHREAD_STK` environment variable provides a non-intrusive
    way to request a larger pthread stack size for the tests. The required
    pthread stack size depends on the build configuration.
    
    A 4 MiB default is generous compared to the 512 KiB of macOS; however,
    it is known that some compilers on AIX produce code that uses
    comparatively more stack space.
    
    This patch expands the solution from D65688 to apply to all Clang LIT
    tests.
    
    This also reverts commit fcd570ec55e36768ac1123b96c31284dd5518cd5,
    "[clang][test] Mark test arm-float-abi-lto.c unsupported on AIX".
    
    The problem was caused by the test running up against the per-thread
    stack limit on AIX. This is resolved by having the tests run with
    `AIXTHREAD_STK` set for 4 MiB.
    
    Reviewed By: xingxue
    
    Differential Revision: https://reviews.llvm.org/D129165

--- clang/test/lit.cfg.py
+++ clang/test/lit.cfg.py
@@ -264,3 +264,13 @@ if 'aix' in config.target_triple:
                       '/ASTMerge/anonymous-fields', '/ASTMerge/injected-class-name-decl'):
         exclude_unsupported_files_for_aix(config.test_source_root + directory)
 
+# Some tests perform deep recursion, which requires a larger pthread stack size
+# than the relatively low default of 192 KiB for 64-bit processes on AIX. The
+# `AIXTHREAD_STK` environment variable provides a non-intrusive way to request
+# a larger pthread stack size for the tests. Various applications and runtime
+# libraries on AIX use a default pthread stack size of 4 MiB, so we will use
+# that as a default value here.
+if 'AIXTHREAD_STK' in os.environ:
+    config.environment['AIXTHREAD_STK'] = os.environ['AIXTHREAD_STK']
+elif platform.system() == 'AIX':
+    config.environment['AIXTHREAD_STK'] = '4194304'
