ff408cb20cec37285da172a3258689986ebdbe3b
Chuanqi Xu <yedeng.yd@linux.alibaba.com>
2022-07-08 11:10:49 +0800

    [C++20] [Modules] Don't complain about duplicated default template argument across modules
     See https://github.com/cplusplus/draft/pull/5204 for a detailed
     background.
    
     Simply, the test redundant-template-default-arg.cpp attached to this
     patch should be accepted instead of being complained about the
     redefinition.
    
     Reviewed By: urnathan, rsmith, ChuanqiXu
    
     Differential Revision: https://reviews.llvm.org/D118034

---
+++ clang/test/Modules/redundant-template-default-arg.cpp
@@ -0,0 +1,56 @@
+// RUN: rm -rf %t
+// RUN: mkdir %t
+// RUN: split-file %s %t
+//
+// RUN: %clang_cc1  -std=c++20 %t/foo.cppm -I%t -emit-module-interface -o %t/foo.pcm
+// RUN: %clang_cc1  -fprebuilt-module-path=%t -std=c++20 %t/use.cpp -I%t -fsyntax-only -verify
+
+//--- foo.h
+template <typename T>
+T u;
+
+template <typename T = int>
+T v;
+
+template <int T = 8>
+int v2;
+
+template <typename T>
+class my_array {};
+
+template <template <typename> typename C = my_array>
+int v3;
+
+template <typename T, int *i = nullptr>
+T v4;
+
+template <typename T, T *i = nullptr>
+T v5;
+
+inline int a = 43;
+template <typename T, int *i = &a>
+T v6;
+
+inline int b = 43;
+template <typename T, T *i = &b>
+T v7;
+
+template <int T = (3 > 2)>
+int v8;
+
+consteval int getInt() {
+  return 55;
+}
+template <int T = getInt()>
+int v9;
+
+//--- foo.cppm
+module;
+#include "foo.h"
+export module foo;
+
+
+//--- use.cpp
+// expected-no-diagnostics
+import foo;
+#include "foo.h"
