ea080fb5906a3f0352044cfc68bcec87e80b6b26
Vignesh Balasubramanian <Vignesh.Balasubrmanian@amd.com>
2022-07-12 14:02:50 +0530

    [OpenMP][OMPD] GDB plugin code to leverage libompd to provide debugging support for OpenMP programs.
    This is 5th of 6 patches started from https://reviews.llvm.org/D100181
    This plugin code, when loaded in gdb, adds a few commands like
    ompd icv, ompd bt, ompd parallel.
    These commands create an interface for GDB to read the OpenMP
    runtime through libompd.
    
    Reviewed By: @dreachem
    Differential Revision: https://reviews.llvm.org/D100185

---
+++ openmp/libompd/gdb-plugin/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, Extension, find_packages
+
+import os 
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+omp_include_dir = os.environ.get('LIBOMP_INCLUDE_DIR', dir_path)
+
+print("find_packages : ", find_packages())
+setup(
+    name='ompd',
+    version='1.0',
+    py_modules=['loadompd'],
+    setup_requires=['wheel'],
+    packages=find_packages(),
+	ext_modules=[Extension('ompd.ompdModule', [dir_path+'/ompdModule.c', dir_path+'/ompdAPITests.c'], include_dirs=[omp_include_dir])]
+)
