2240d72f15f3b7b9d9fb65450f9bf635fd310f6f
Nick Desaulniers <ndesaulniers@google.com>
2022-07-12 09:17:15 -0700

    [X86] initial -mfunction-return=thunk-extern support
    Adds support for:
    * `-mfunction-return=<value>` command line flag, and
    * `__attribute__((function_return("<value>")))` function attribute
    
    Where the supported <value>s are:
    * keep (disable)
    * thunk-extern (enable)
    
    thunk-extern enables clang to change ret instructions into jmps to an
    external symbol named __x86_return_thunk, implemented as a new
    MachineFunctionPass named "x86-return-thunks", keyed off the new IR
    attribute fn_ret_thunk_extern.
    
    The symbol __x86_return_thunk is expected to be provided by the runtime
    the compiled code is linked against and is not defined by the compiler.
    Enabling this option alone doesn't provide mitigations without
    corresponding definitions of __x86_return_thunk!
    
    This new MachineFunctionPass is very similar to "x86-lvi-ret".
    
    The <value>s "thunk" and "thunk-inline" are currently unsupported. It's
    not clear yet that they are necessary: whether the thunk pattern they
    would emit is beneficial or used anywhere.
    
    Should the <value>s "thunk" and "thunk-inline" become necessary,
    x86-return-thunks could probably be merged into x86-retpoline-thunks
    which has pre-existing machinery for emitting thunks (which could be
    used to implement the <value> "thunk").
    
    Has been found to build+boot with corresponding Linux
    kernel patches. This helps the Linux kernel mitigate RETBLEED.
    * CVE-2022-23816
    * CVE-2022-28693
    * CVE-2022-29901
    
    See also:
    * "RETBLEED: Arbitrary Speculative Code Execution with Return
    Instructions."
    * AMD SECURITY NOTICE AMD-SN-1037: AMD CPU Branch Type Confusion
    * TECHNICAL GUIDANCE FOR MITIGATING BRANCH TYPE CONFUSION REVISION 1.0
      2022-07-12
    * Return Stack Buffer Underflow / Return Stack Buffer Underflow /
      CVE-2022-29901, CVE-2022-28693 / INTEL-SA-00702
    
    SystemZ may eventually want to support "thunk-extern" and "thunk"; both
    options are used by the Linux kernel's CONFIG_EXPOLINE.
    
    This functionality has been available in GCC since the 8.1 release, and
    was backported to the 7.3 release.
    
    Many thanks for folks that provided discrete review off list due to the
    embargoed nature of this hardware vulnerability. Many Bothans died to
    bring us this information.
    
    Link: https://www.youtube.com/watch?v=IF6HbCKQHK8
    Link: https://github.com/llvm/llvm-project/issues/54404
    Link: https://gcc.gnu.org/legacy-ml/gcc-patches/2018-01/msg01197.html
    Link: https://www.intel.com/content/www/us/en/developer/articles/technical/software-security-guidance/advisory-guidance/return-stack-buffer-underflow.html
    Link: https://arstechnica.com/information-technology/2022/07/intel-and-amd-cpus-vulnerable-to-a-new-speculative-execution-attack/?comments=1
    Link: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=ce114c866860aa9eae3f50974efc68241186ba60
    Link: https://www.intel.com/content/www/us/en/security-center/advisory/intel-sa-00702.html
    Link: https://www.intel.com/content/www/us/en/security-center/advisory/intel-sa-00707.html
    
    Reviewed By: aaron.ballman, craig.topper
    
    Differential Revision: https://reviews.llvm.org/D129572

--- clang/lib/Driver/ToolChains/Clang.cpp
+++ clang/lib/Driver/ToolChains/Clang.cpp
@@ -6344,6 +6344,10 @@ void Clang::ConstructJob(Compilation &C, const JobAction &JA,
   if (IsUsingLTO)
     Args.AddLastArg(CmdArgs, options::OPT_mibt_seal);
 
+  if (Arg *A = Args.getLastArg(options::OPT_mfunction_return_EQ))
+    CmdArgs.push_back(
+        Args.MakeArgString(Twine("-mfunction-return=") + A->getValue()));
+
   // Forward -f options with positive and negative forms; we translate these by
   // hand.  Do not propagate PGO options to the GPU-side compilations as the
   // profile info is for the host-side compilation only.
