de9a7260aca314778da7f768cd4d8687defc759a
Ulrich Weigand <ulrich.weigand@de.ibm.com>
2022-07-12 09:29:07 +0200

    Read/write external resource alignment tag in little-endian
    https://reviews.llvm.org/D126446 added support for encoding
    binary blobs in MLIR assembly.  To enable cross-architecture
    compatibility, these need to be encoded in little-endian format.
    
    This patch is a first step in that direction by reading and
    writing the alignment tag that those blobs are prefixed by
    in little-endian format.  This fixes assertion failures in
    several test cases on big-endian platforms.
    
    The actual content of the blob is not yet handled here.
    
    Differential Revision: https://reviews.llvm.org/D129483

--- mlir/lib/Parser/Parser.cpp
+++ mlir/lib/Parser/Parser.cpp
@@ -24,6 +24,7 @@
 #include "llvm/ADT/ScopeExit.h"
 #include "llvm/ADT/StringSet.h"
 #include "llvm/ADT/bit.h"
+#include "llvm/Support/Endian.h"
 #include "llvm/Support/PrettyStackTrace.h"
 #include "llvm/Support/SourceMgr.h"
 #include <algorithm>
@@ -2349,7 +2350,7 @@ public:
                          "expected hex string blob for key '" + key +
                              "' to encode alignment in first 4 bytes");
     }
-    uint32_t align = 0;
+    llvm::support::ulittle32_t align;
     memcpy(&align, blobData->data(), sizeof(uint32_t));
 
     // Get the data portion of the blob.
