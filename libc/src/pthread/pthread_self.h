8dc42802f7052d2eed4f2267851b94578c0152fc
Siva Chandra Reddy <sivachandra@google.com>
2022-07-12 21:50:03 +0000

    [libc] Add implementations of pthread_equal and pthread_self.
    Reviewed By: michaelrj, lntue
    
    Differential Revision: https://reviews.llvm.org/D129729

--- libc/src/pthread/pthread_self.h
+++ libc/src/pthread/pthread_self.h
@@ -1,4 +1,4 @@
-//===--- Definitions of common thread items ---------------------*- C++ -*-===//
+//===-- Implementation header for pthread_self function ---------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,10 +6,15 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "thread.h"
+#ifndef LLVM_LIBC_SRC_THREADS_PTHREAD_SELF_H
+#define LLVM_LIBC_SRC_THREADS_PTHREAD_SELF_H
+
+#include <pthread.h>
 
 namespace __llvm_libc {
 
-thread_local Thread self;
+pthread_t pthread_self();
 
 } // namespace __llvm_libc
+
+#endif // LLVM_LIBC_SRC_THREADS_PTHREAD_SELF_H
