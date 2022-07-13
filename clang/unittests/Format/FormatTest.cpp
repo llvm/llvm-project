74affc725ec79cd51c5fcf2a47bbeb83c08681ab
Björn Schäpers <bjoern@hazardy.de>
2022-07-13 12:38:38 +0200

    [clang-format] Indent TT_CtorInitializerColon after requires clauses
    Fixes https://github.com/llvm/llvm-project/issues/56215
    
    Differential Revision: https://reviews.llvm.org/D129942

--- clang/unittests/Format/FormatTest.cpp
+++ clang/unittests/Format/FormatTest.cpp
@@ -24714,6 +24714,18 @@ TEST_F(FormatTest, RequiresClauses) {
       "struct S {};",
       Style);
 
+  Style = getLLVMStyle();
+  Style.ConstructorInitializerIndentWidth = 4;
+  Style.BreakConstructorInitializers = FormatStyle::BCIS_BeforeColon;
+  Style.PackConstructorInitializers = FormatStyle::PCIS_Never;
+  verifyFormat("constexpr Foo(Foo const &other)\n"
+               "  requires std::is_copy_constructible<T>\n"
+               "    : value{other.value} {\n"
+               "  do_magic();\n"
+               "  do_more_magic();\n"
+               "}",
+               Style);
+
   // Not a clause, but we once hit an assert.
   verifyFormat("#if 0\n"
                "#else\n"
