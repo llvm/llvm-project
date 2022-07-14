a2e01bdcc21429d92cd785053f7cc9f72f1d212a
Vitaly Buka <vitalybuka@google.com>
2022-07-14 10:57:07 -0700

    [test][CodeGen] Don't miss lifetime markers in lifetime tests
    Reviewed By: MaskRay
    
    Differential Revision: https://reviews.llvm.org/D129789

--- clang/test/CodeGen/lifetime2.c
+++ clang/test/CodeGen/lifetime2.c
@@ -1,27 +1,27 @@
-// RUN: %clang_cc1 -no-opaque-pointers -S -emit-llvm -o - -O2 -disable-llvm-passes %s | FileCheck %s -check-prefixes=CHECK,O2
-// RUN: %clang_cc1 -no-opaque-pointers -S -emit-llvm -o - -O2 -disable-lifetime-markers %s \
-// RUN:       | FileCheck %s -check-prefixes=CHECK,O0
-// RUN: %clang_cc1 -no-opaque-pointers -S -emit-llvm -o - -O0 %s | FileCheck %s -check-prefixes=CHECK,O0
+// RUN: %clang_cc1 -no-opaque-pointers -S -emit-llvm -o - -O2 -disable-llvm-passes %s      | FileCheck %s --implicit-check-not="call void @llvm.lifetime" --check-prefixes=CHECK,O2
+// RUN: %clang_cc1 -no-opaque-pointers -S -emit-llvm -o - -O2 -disable-lifetime-markers %s | FileCheck %s --implicit-check-not="call void @llvm.lifetime" --check-prefixes=CHECK
+// RUN: %clang_cc1 -no-opaque-pointers -S -emit-llvm -o - -O0 %s                           | FileCheck %s --implicit-check-not="call void @llvm.lifetime" --check-prefixes=CHECK 
 
 extern int bar(char *A, int n);
 
 // CHECK-LABEL: @foo
-// O0-NOT: @llvm.lifetime.start
 int foo (int n) {
   if (n) {
-// O2: @llvm.lifetime.start
+// O2: call void @llvm.lifetime.start.p0i8(i64 100,
     char A[100];
     return bar(A, 1);
+// O2: call void @llvm.lifetime.end.p0i8(i64 100,
   } else {
-// O2: @llvm.lifetime.start
+// O2: call void @llvm.lifetime.start.p0i8(i64 100,
     char A[100];
     return bar(A, 2);
+// O2: call void @llvm.lifetime.end.p0i8(i64 100,
   }
 }
 
 // CHECK-LABEL: @no_goto_bypass
 void no_goto_bypass(void) {
-  // O2: @llvm.lifetime.start.p0i8(i64 1
+  // O2: call void @llvm.lifetime.start.p0i8(i64 1,
   char x;
 l1:
   bar(&x, 1);
@@ -29,14 +29,11 @@ l1:
   bar(y, 5);
   goto l1;
   // Infinite loop
-  // O2-NOT: @llvm.lifetime.end.p0i8(
 }
 
 // CHECK-LABEL: @goto_bypass
 void goto_bypass(void) {
   {
-    // O2-NOT: @llvm.lifetime.start.p0i8(i64 1
-    // O2-NOT: @llvm.lifetime.end.p0i8(i64 1
     char x;
   l1:
     bar(&x, 1);
@@ -48,16 +45,16 @@ void goto_bypass(void) {
 void no_switch_bypass(int n) {
   switch (n) {
   case 1: {
-    // O2: @llvm.lifetime.start.p0i8(i64 1
-    // O2: @llvm.lifetime.end.p0i8(i64 1
+    // O2: call void @llvm.lifetime.start.p0i8(i64 1,
+    // O2: call void @llvm.lifetime.end.p0i8(i64 1,
     char x;
     bar(&x, 1);
     break;
   }
   case 2:
     n = n;
-    // O2: @llvm.lifetime.start.p0i8(i64 5
-    // O2: @llvm.lifetime.end.p0i8(i64 5
+    // O2: call void @llvm.lifetime.start.p0i8(i64 5,
+    // O2: call void @llvm.lifetime.end.p0i8(i64 5,
     char y[5];
     bar(y, 5);
     break;
@@ -69,8 +66,6 @@ void switch_bypass(int n) {
   switch (n) {
   case 1:
     n = n;
-    // O2-NOT: @llvm.lifetime.start.p0i8(i64 1
-    // O2-NOT: @llvm.lifetime.end.p0i8(i64 1
     char x;
     bar(&x, 1);
     break;
@@ -83,23 +78,18 @@ void switch_bypass(int n) {
 // CHECK-LABEL: @indirect_jump
 void indirect_jump(int n) {
   char x;
-  // O2-NOT: @llvm.lifetime
   void *T[] = {&&L};
   goto *T[n];
 L:
   bar(&x, 1);
 }
 
-// O2-LABEL: @jump_backward_over_declaration(
-// O2: %[[p:.*]] = alloca i32*
-// O2: %[[v0:.*]] = bitcast i32** %[[p]] to i8*
-// O2: call void @llvm.lifetime.start.p0i8(i64 {{.*}}, i8* %[[v0]])
-// O2-NOT: call void @llvm.lifetime.start.p0i8(
-
 extern void foo2(int p);
 
+// O2-LABEL: @jump_backward_over_declaration(
 int jump_backward_over_declaration(int a) {
   int *p = 0;
+// O2: call void @llvm.lifetime.start.p0i8(i64 8,
 label1:
   if (p) {
     foo2(*p);
@@ -112,4 +102,5 @@ label1:
     goto label1;
   }
   return -1;
+// O2: call void @llvm.lifetime.end.p0i8(i64 8,
 }
