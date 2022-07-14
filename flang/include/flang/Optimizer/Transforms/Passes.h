057a24273793de987ad382c28c1cd5d9bad108dc
Slava Zakharin <szakharin@nvidia.com>
2022-07-14 16:50:41 -0700

    [flang] Run algebraic simplification optimization pass.
    Try 2 to merge b66e4bde833ecb537176839a311be94b0d7dadb4.
    
    Flang algebraic simplification pass will run algebraic simplification
    rewrite patterns for Math/Complex/etc. dialects. It is enabled
    under opt-for-speed optimization levels (i.e. for O1/O2/O3; Os/Oz will not
    enable it).
    
    With this change the FIR/MLIR optimization pipeline becomes affected
    by the -O* optimization level switches. Until now these switches
    only affected the middle-end and back-end.
    
    Differential Revision: https://reviews.llvm.org/D130035

--- flang/include/flang/Optimizer/Transforms/Passes.h
+++ flang/include/flang/Optimizer/Transforms/Passes.h
@@ -39,6 +39,7 @@ std::unique_ptr<mlir::Pass>
 createMemoryAllocationPass(bool dynOnHeap, std::size_t maxStackSize);
 std::unique_ptr<mlir::Pass> createAnnotateConstantOperandsPass();
 std::unique_ptr<mlir::Pass> createSimplifyRegionLitePass();
+std::unique_ptr<mlir::Pass> createAlgebraicSimplificationPass();
 
 // declarative passes
 #define GEN_PASS_REGISTRATION
