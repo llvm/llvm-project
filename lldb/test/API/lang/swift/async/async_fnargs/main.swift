5c812fe385b1559c7e2bf93ed46b2c4fec6c2613
Dave Lee <davelee.com@gmail.com>
2022-07-15 16:33:37 -0700

    [lldb] Be aware of async functions in the form of closures (#4966) (#4980)
    Recognize async closures as being async, in `IsSwiftAsyncFunctionSymbol`. Like `static` functions, this requires peeling off a wrapper node to get to the `Function` node.
    
    rdar://94840584
    
    (cherry-picked from commit ed878fea253a35bfffa4b4b6830fce06e56e1c1e)

--- lldb/test/API/lang/swift/async/async_fnargs/main.swift
+++ lldb/test/API/lang/swift/async/async_fnargs/main.swift
@@ -5,15 +5,37 @@ func sayHello() async {
   print("hello")
 }
 
+func sayBasic(_ msg: String) async {
+  print("Set breakpoint here")
+  await sayHello()
+  print(msg) // Set breakpoint here
+}
+
 func sayGeneric<T>(_ msg: T) async {
   print("Set breakpoint here")
   await sayHello()
-  print(msg) // And also here.
+  print(msg) // Set breakpoint here
+}
+
+struct Struct {
+  static func sayStatic(_ msg: String) async {
+    print("Set breakpoint here")
+    await sayHello()
+    print(msg) // Set breakpoint here
+  }
 }
 
 @main struct Main {
   static func main() async {
-    await sayHello()
-    await sayGeneric("world")
+    let closure = { msg in
+      print("Set breakpoint here")
+      await sayHello()
+      print(msg) // Set breakpoint here
+    }
+
+    await sayBasic("basic world")
+    await sayGeneric("generic world")
+    await Struct.sayStatic("static world")
+    await closure("closure world")
   }
 }
