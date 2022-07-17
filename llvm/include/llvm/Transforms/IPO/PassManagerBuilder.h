70519a1fba8a9c3f8b9cf07ae8af751fa472db80
Fangrui Song <i@maskray.me>
2022-07-17 14:24:36 -0700

    [LegacyPM] Remove LTO passes from optimization pipeline
    Following recent changes removing non-core features of the legacy
    PM/optimization pipeline.

--- llvm/include/llvm/Transforms/IPO/PassManagerBuilder.h
+++ llvm/include/llvm/Transforms/IPO/PassManagerBuilder.h
@@ -164,9 +164,6 @@ public:
   bool VerifyInput;
   bool VerifyOutput;
   bool MergeFunctions;
-  bool PrepareForLTO;
-  bool PrepareForThinLTO;
-  bool PerformThinLTO;
   bool DivergentTarget;
   unsigned LicmMssaOptCap;
   unsigned LicmMssaNoAccForPromotionCap;
