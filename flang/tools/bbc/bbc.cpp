0ec3ac9b7fbd15698af7289e1214e8ff3d82ec14
Jonathon Penix <jpenix@quicinc.com>
2022-07-19 11:47:25 -0700

    [Flang] Add -fconvert option to swap endianness for unformatted files.
    To accomplish this, this patch creates an optional list of environment
    variable default values to be set by the runtime to allow directly using
    the existing runtime implementation of FORT_CONVERT for I/O conversions.

--- flang/tools/bbc/bbc.cpp
+++ flang/tools/bbc/bbc.cpp
@@ -230,7 +230,7 @@ static mlir::LogicalResult convertFortranSourceToMLIR(
   auto burnside = Fortran::lower::LoweringBridge::create(
       ctx, semanticsContext, defKinds, semanticsContext.intrinsics(),
       semanticsContext.targetCharacteristics(), parsing.allCooked(), "",
-      kindMap, loweringOptions);
+      kindMap, loweringOptions, {});
   burnside.lower(parseTree, semanticsContext);
   mlir::ModuleOp mlirModule = burnside.getModule();
   std::error_code ec;
