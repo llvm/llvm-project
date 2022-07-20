41ae78ea3a931018cf2be58fb9eaa98bfeaf2db6
Kazu Hirata <kazu@google.com>
2022-07-19 20:15:44 -0700

    Use has_value instead of hasValue (NFC)

--- llvm/lib/Transforms/IPO/Attributor.cpp
+++ llvm/lib/Transforms/IPO/Attributor.cpp
@@ -1109,7 +1109,7 @@ bool Attributor::getAssumedSimplifiedValues(
   const auto &SimplificationCBs = SimplificationCallbacks.lookup(IRP);
   for (auto &CB : SimplificationCBs) {
     Optional<Value *> CBResult = CB(IRP, AA, UsedAssumedInformation);
-    if (!CBResult.hasValue())
+    if (!CBResult.has_value())
       continue;
     Value *V = CBResult.getValue();
     if (!V)
