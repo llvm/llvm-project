46e6dd84b778dd2e30368183fec265beceecdd0f
Nikita Popov <npopov@redhat.com>
2022-07-21 14:42:08 +0200

    [MemoryBuiltins] Remove isFreeCall() function (NFC)
    Remove isFreeCall() in favor of getFreedOperand(). Replace the
    two remaining uses with a getFreedOperand() != nullptr check, as
    they only care that something is getting freed. (The usage in DSE
    is correct as such. The allocator-related checks in CFLGraph look
    rather questionable in general.)

--- llvm/include/llvm/Analysis/MemoryBuiltins.h
+++ llvm/include/llvm/Analysis/MemoryBuiltins.h
@@ -80,9 +80,6 @@ bool isReallocLikeFn(const Function *F, const TargetLibraryInfo *TLI);
 /// isLibFreeFunction - Returns true if the function is a builtin free()
 bool isLibFreeFunction(const Function *F, const LibFunc TLIFn);
 
-/// Returns true if the value is a call to a free function.
-bool isFreeCall(const Value *I, const TargetLibraryInfo *TLI);
-
 /// If this if a call to a free function, return the freed operand.
 Value *getFreedOperand(const CallBase *CB, const TargetLibraryInfo *TLI);
 
