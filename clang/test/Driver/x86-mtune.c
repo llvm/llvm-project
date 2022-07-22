7c7293a73407d7928756deca703da2b561862320
Fangrui Song <i@maskray.me>
2022-07-22 14:37:58 -0700

    [Driver][test] Clean up and improve some -mtune tests
    Note: we should test CHECK-NOT: "-tune-cpu" instead of CHECK-NOT: "-tune-cpu" "generic"

--- clang/test/Driver/x86-mtune.c
+++ clang/test/Driver/x86-mtune.c
@@ -1,43 +1,36 @@
 // Ensure we support the -mtune flag.
 
-// Default mtune should be generic.
-// RUN: %clang -target x86_64-unknown-unknown -c -### %s 2>&1 \
-// RUN:   | FileCheck %s -check-prefix=notune
-// notune: "-tune-cpu" "generic"
+// cpu: "-target-cpu" "x86-64"
 
-// RUN: %clang -target x86_64-unknown-unknown -c -### %s -mtune=generic 2>&1 \
-// RUN:   | FileCheck %s -check-prefix=generic
+// Default mtune should be generic.
+// RUN: %clang -### -c --target=x86_64 %s 2>&1 | FileCheck %s -check-prefix=generic
+
+// RUN: %clang -### -c --target=x86_64 %s -mtune=generic 2>&1 | FileCheck %s --check-prefixes=cpu,generic
 // generic: "-tune-cpu" "generic"
 
-// RUN: %clang -target x86_64-unknown-unknown -c -### %s -mtune=nocona 2>&1 \
-// RUN:   | FileCheck %s -check-prefix=nocona
+// RUN: %clang -### -c --target=x86_64 %s -mtune=nocona 2>&1 | FileCheck %s --check-prefixes=cpu,nocona
 // nocona: "-tune-cpu" "nocona"
 
 // Unlike march we allow 32-bit only cpus with mtune.
 
-// RUN: %clang -target x86_64-unknown-unknown -c -### %s -mtune=i686 2>&1 \
-// RUN:   | FileCheck %s -check-prefix=i686
+// RUN: %clang -### -c --target=x86_64 %s -mtune=i686 2>&1 | FileCheck %s --check-prefixes=cpu,i686
 // i686: "-tune-cpu" "i686"
 
-// RUN: %clang -target x86_64-unknown-unknown -c -### %s -mtune=pentium4 2>&1 \
-// RUN:   | FileCheck %s -check-prefix=pentium4
+// RUN: %clang -### -c --target=x86_64 %s -mtune=pentium4 2>&1 | FileCheck %s -check-prefix=pentium4
 // pentium4: "-tune-cpu" "pentium4"
 
-// RUN: %clang -target x86_64-unknown-unknown -c -### %s -mtune=athlon 2>&1 \
-// RUN:   | FileCheck %s -check-prefix=athlon
+// RUN: %clang -### -c --target=x86_64 %s -mtune=athlon 2>&1 | FileCheck %s -check-prefixes=cpu,athlon
 // athlon: "-tune-cpu" "athlon"
 
 // Check interaction between march and mtune.
 
 // -march should remove default mtune generic.
-// RUN: %clang -target x86_64-unknown-unknown -c -### %s -march=core2 2>&1 \
-// RUN:   | FileCheck %s -check-prefix=marchcore2
+// RUN: %clang -### -c --target=x86_64 %s -march=core2 2>&1 | FileCheck %s -check-prefix=marchcore2
 // marchcore2: "-target-cpu" "core2"
 // marchcore2-NOT: "-tune-cpu"
 
 // -march should remove default mtune generic.
-// RUN: %clang -target x86_64-unknown-unknown -c -### %s -march=core2 -mtune=nehalem 2>&1 \
-// RUN:   | FileCheck %s -check-prefix=marchmtune
+// RUN: %clang -### -c --target=x86_64 %s -march=core2 -mtune=nehalem 2>&1 | FileCheck %s -check-prefix=marchmtune
 // marchmtune: "-target-cpu" "core2"
 // mmarchmtune: "-tune-cpu" "nehalem"
 
