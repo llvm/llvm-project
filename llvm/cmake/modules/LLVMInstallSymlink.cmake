9b9688275cc5ad2854bd3f180c9c6cb5293735d3
John Ericson <John.Ericson@Obsidian.Systems>
2022-07-25 21:15:14 +0000

    Revert "[llvm][cmake] Make `install_symlink` workflow work with absolute install dirs"
    `ExtendPath` needs to be moved from internal to external CMake if it is
    to be used to `AddLLVM`. See, for example, the failure in
    https://lab.llvm.org/buildbot#builders/61/builds/29940
    
    This reverts commit 7b06e3960ace8034fd029630f8f51be6fd9c8ea7.

--- llvm/cmake/modules/LLVMInstallSymlink.cmake
+++ llvm/cmake/modules/LLVMInstallSymlink.cmake
@@ -1,26 +1,22 @@
 # We need to execute this script at installation time because the
 # DESTDIR environment variable may be unset at configuration time.
 # See PR8397.
-#
-# `outdir` must be an absolute path. This module gets a very reduced
-# `CMAKE_MODULE_PATH` so it is easier to make the caller the responsible
-# for this.
 
 include(GNUInstallDirs)
 
 function(install_symlink name target outdir)
   set(DESTDIR $ENV{DESTDIR})
-  set(outdir "${DESTDIR}${outdir}")
+  set(bindir "${DESTDIR}${CMAKE_INSTALL_PREFIX}/${outdir}")
 
   message(STATUS "Creating ${name}")
 
   execute_process(
     COMMAND "${CMAKE_COMMAND}" -E create_symlink "${target}" "${name}"
-    WORKING_DIRECTORY "${outdir}" ERROR_VARIABLE has_err)
+    WORKING_DIRECTORY "${bindir}" ERROR_VARIABLE has_err)
   if(CMAKE_HOST_WIN32 AND has_err)
     execute_process(
       COMMAND "${CMAKE_COMMAND}" -E copy "${target}" "${name}"
-      WORKING_DIRECTORY "${outdir}")
+      WORKING_DIRECTORY "${bindir}")
   endif()
 
 endfunction()
