8ad1086fa5a9c1a556fbddd49169842928e8c698
David Stuttard <david.stuttard@amd.com>
2022-07-25 11:01:28 +0100

    Revert "AMDGPU: Turn off force init 16 input SGPRS for pal"
    This reverts commit 3d985005baf715358a97cbedc12470bb6d8538ac.

--- llvm/lib/Target/AMDGPU/SIISelLowering.cpp
+++ llvm/lib/Target/AMDGPU/SIISelLowering.cpp
@@ -2133,8 +2133,7 @@ void SITargetLowering::allocateSystemSGPRs(CCState &CCInfo,
                                            SIMachineFunctionInfo &Info,
                                            CallingConv::ID CallConv,
                                            bool IsShader) const {
-  if (Subtarget->hasUserSGPRInit16Bug() && !Subtarget->isAmdPalOS()) {
-    // Note: user SGPRs are handled by the front-end for pal based shaders
+  if (Subtarget->hasUserSGPRInit16Bug()) {
     // Pad up the used user SGPRs with dead inputs.
     unsigned CurrentUserSGPRs = Info.getNumUserSGPRs();
 
@@ -2197,8 +2196,7 @@ void SITargetLowering::allocateSystemSGPRs(CCState &CCInfo,
     CCInfo.AllocateReg(PrivateSegmentWaveByteOffsetReg);
   }
 
-  assert(!Subtarget->hasUserSGPRInit16Bug() || Subtarget->isAmdPalOS() ||
-         Info.getNumPreloadedSGPRs() >= 16);
+  assert(!Subtarget->hasUserSGPRInit16Bug() || Info.getNumPreloadedSGPRs() >= 16);
 }
 
 static void reservePrivateMemoryRegs(const TargetMachine &TM,
