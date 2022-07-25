9930285ed0c153db16842e86b3db4e4bf9bdefa4
Sunho Kim <ksunhokim123@gmail.com>
2022-07-25 23:24:25 +0900

    [ORC][COFF] Properly set weak flag to COMDAT symbols.
    Properly set weak flag to COMDAT symbols so that no duplicate definition error will be generated. There is an inaccuracy in setting plain weak for largest selection type, which will be dealt with soon when largest type is properly implemented.
    
    Reviewed By: lhames
    
    Differential Revision: https://reviews.llvm.org/D129764

---
+++ llvm/test/ExecutionEngine/JITLink/X86/COFF_comdat_weak_duplicate.s
@@ -0,0 +1,29 @@
+# RUN: rm -rf %t && mkdir -p %t
+# RUN: yaml2obj %S/Inputs/COFF_comdat_weak_def.yaml -o %t/COFF_weak_1.o
+# RUN: yaml2obj %S/Inputs/COFF_comdat_weak_def.yaml -o %t/COFF_weak_2.o
+# RUN: llvm-mc -filetype=obj -triple=x86_64-windows-msvc %s -o %t/COFF_main.o
+# RUN: 
+# RUN: llvm-jitlink -noexec %t/COFF_main.o %t/COFF_weak_1.o %t/COFF_weak_2.o \
+# RUN: -slab-allocate 100Kb -slab-address 0xfff00000 -slab-page-size 4096 \
+# RUN: -show-graph -noexec 2>&1 | FileCheck %s
+#
+# Check that duplicate comdat any definitions don't generate duplicate definition error.
+#
+# CHECK: section weakfunc:
+# CHECK-EMPTY:
+# CHECK-NEXT:  block 0xfff02000 size = 0x00000001, align = 16, alignment-offset = 0
+# CHECK-NEXT:    symbols:
+# CHECK-NEXT:      0xfff02000 (block + 0x00000000): size: 0x00000001, linkage: weak, scope: default, live  -   func
+# CHECK-NEXT:    no edges
+
+	.text
+
+	.def	main;
+	.scl	2;
+	.type	32;
+	.endef
+	.globl	main
+	.p2align	4, 0x90
+main:
+    callq func
+	retq
