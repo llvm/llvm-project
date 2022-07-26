2b38f589301d7defef6099b57ecf45139010a5a7
Simon Tatham <simon.tatham@arm.com>
2022-07-26 09:20:46 +0100

    [llvm-objdump,ARM] Add PrettyPrinters for Arm and AArch64.
    Most Arm disassemblers, including GNU objdump and Arm's own `fromelf`,
    emit an instruction's raw encoding as a 32-bit words or (for Thumb)
    one or two 16-bit halfwords, in logical order rather than according to
    their storage endianness. This is generally easier to read: it matches
    the encoding diagrams in the architecture spec, it matches the value
    you'd write in a `.inst` directive, and it means that fields within
    the instruction encoding that span more than one byte (such as branch
    offsets or `SVC` immediates) can be read directly in the encoding
    without having to mentally reverse the bytes.
    
    llvm-objdump already has a system of PrettyPrinter subclasses which
    makes it easy for a target to drop in its own preferred formatting.
    This patch adds pretty-printers for all the Arm targets, so that
    llvm-objdump will display Arm instruction encodings in their preferred
    layout instead of little-endian and bytewise.
    
    Reviewed By: DavidSpickett
    
    Differential Revision: https://reviews.llvm.org/D130358

--- llvm/test/tools/llvm-objdump/ELF/AArch64/disassemble-align.s
+++ llvm/test/tools/llvm-objdump/ELF/AArch64/disassemble-align.s
@@ -3,8 +3,8 @@
 
 ## Use '|' to show where the tabs line up.
 #       CHECK:0000000000000000 <$x.0>:
-#  CHECK-NEXT:       0: 62 10 00 91  |add|x2, x3, #4{{$}}
-#  CHECK-NEXT:       4: 1f 20 03 d5  |nop
+#  CHECK-NEXT:       0: 91001062     |add|x2, x3, #4{{$}}
+#  CHECK-NEXT:       4: d503201f     |nop
 # CHECK-EMPTY:
 #  CHECK-NEXT:0000000000000008 <$d.1>:
 #  CHECK-NEXT:       8:|ff ff 00 00|.word|0x0000ffff
