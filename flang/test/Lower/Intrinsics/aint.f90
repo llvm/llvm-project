c05db41058ae045964392bad1498f8e445703a04
Peixin Qiao <qiaopeixin@huawei.com>
2022-07-26 20:21:51 +0800

    [flang] Remove fp128 support for llvm.round and llvm.trunc
    The fp128 in llvm.round and llvm.trunc is not supported in X86_64 for
    now. Revert the support. To support quad precision for llvm.round and
    llvm.trunc, it may should be supported using runtime.
    
    Reviewed By: Jean Perier
    
    Differential Revision: https://reviews.llvm.org/D130556

--- flang/test/Lower/Intrinsics/aint.f90
+++ flang/test/Lower/Intrinsics/aint.f90
@@ -39,16 +39,8 @@ subroutine aint_test_real10(a, b)
   b = aint(a)
 end subroutine
 
-! CHECK-LABEL: func.func @_QPaint_test_real16(
-! CHECK-SAME:                                 %[[VAL_0:.*]]: !fir.ref<f128> {fir.bindc_name = "a"},
-! CHECK-SAME:                                 %[[VAL_1:.*]]: !fir.ref<f128> {fir.bindc_name = "b"}) {
-! CHECK:         %[[VAL_2:.*]] = fir.load %[[VAL_0]] : !fir.ref<f128>
-! CHECK:         %[[VAL_3:.*]] = fir.call @llvm.trunc.f128(%[[VAL_2]]) : (f128) -> f128
-! CHECK:         fir.store %[[VAL_3]] to %[[VAL_1]] : !fir.ref<f128>
-! CHECK:         return
-! CHECK:       }
-
-subroutine aint_test_real16(a, b)
-  real(16) :: a, b
-  b = aint(a)
-end subroutine
+! TODO: wait until fp128 is supported well in llvm.trunc
+!subroutine aint_test_real16(a, b)
+!  real(16) :: a, b
+!  b = aint(a)
+!end subroutine
