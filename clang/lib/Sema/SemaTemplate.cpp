3e6ea8aff3d534c56e04e39424a27761facc4101
Ilya Biryukov <ibiryukov@google.com>
2022-07-27 12:11:42 +0200

    [Sema] Return primary merged decl as canonical for concepts
    Otherwise we get invalid results for ODR checks. See changed test for an
    example: despite the fact that we merge the first concept, its **uses**
    were considered different by `Profile`, leading to redefinition errors.
    
    After this change, canonical decl for a concept can come from a
    different module and may not be visible. This behavior looks suspicious,
    but does not break any tests. We might want to add a mechanism to make
    the canonical concept declaration visible if we find code that relies on
    this invariant.
    
    Additionally make sure we always merge with the canonical declaration to
    avoid chains of merged concepts being reported as redefinitions. An
    example was added to the test.
    
    Also change the order of includes in the test. Importing a moduralized
    header before its textual part causes the include guard macro to be
    exported and the corresponding `#include` becomes a no-op.
    
    Reviewed By: ChuanqiXu
    
    Differential Revision: https://reviews.llvm.org/D130585
    
    (cherry picked from commit 42f87bb62d0719848842da60d2a8180b9e4d7c52)

--- clang/lib/Sema/SemaTemplate.cpp
+++ clang/lib/Sema/SemaTemplate.cpp
@@ -8758,7 +8758,8 @@ void Sema::CheckConceptRedefinition(ConceptDecl *NewDecl,
     //        Other decls (e.g. namespaces) also have this shortcoming.
     return;
   }
-  Context.setPrimaryMergedDecl(NewDecl, OldConcept);
+  // We unwrap canonical decl late to check for module visibility.
+  Context.setPrimaryMergedDecl(NewDecl, OldConcept->getCanonicalDecl());
 }
 
 /// \brief Strips various properties off an implicit instantiation
