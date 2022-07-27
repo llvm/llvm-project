7aaf5addb85cb3fc436bec0d7c224376ada36ad4
Quinn Pham <quinn.pham@ibm.com>
2022-07-27 08:46:30 -0500

    Revert "[libLTO] Set data-sections by default in libLTO."
    This reverts commit 9418ce37a2431257715b6a5d64bc4f68947b7f2a.

--- clang/test/Driver/function-sections.c
+++ clang/test/Driver/function-sections.c
@@ -6,12 +6,6 @@
 // CHECK-NODS-NOT: -fdata-sections
 // CHECK-US-NOT: -fno-unique-section-names
 // CHECK-NOUS: -fno-unique-section-names
-// CHECK-PLUGIN-DEFAULT-NOT: "-plugin-opt=-function-sections
-// CHECK-PLUGIN-DEFAULT-NOT: "-plugin-opt=-data-sections
-// CHECK-PLUGIN-SECTIONS: "-plugin-opt=-function-sections=1"
-// CHECK-PLUGIN-SECTIONS: "-plugin-opt=-data-sections=1"
-// CHECK-PLUGIN-NO-SECTIONS: "-plugin-opt=-function-sections=0"
-// CHECK-PLUGIN-NO-SECTIONS: "-plugin-opt=-data-sections=0"
 
 // RUN: %clang -### %s -fsyntax-only 2>&1       \
 // RUN:     --target=i386-unknown-linux \
@@ -78,18 +72,3 @@
 // RUN:     --target=i386-unknown-linux \
 // RUN:     -fno-unique-section-names \
 // RUN:   | FileCheck --check-prefix=CHECK-NOUS %s
-
-
-// RUN: %clang -### %s -flto 2>&1                \
-// RUN:     --target=x86_64-unknown-linux \
-// RUN:   | FileCheck --check-prefix=CHECK-PLUGIN-DEFAULT %s
-
-// RUN: %clang -### %s -flto 2>&1                \
-// RUN:     --target=x86_64-unknown-linux \
-// RUN:     -ffunction-sections -fdata-sections \
-// RUN:   | FileCheck --check-prefix=CHECK-PLUGIN-SECTIONS %s
-
-// RUN: %clang -### %s -flto 2>&1                \
-// RUN:     --target=x86_64-unknown-linux \
-// RUN:     -fno-function-sections -fno-data-sections \
-// RUN:   | FileCheck --check-prefix=CHECK-PLUGIN-NO-SECTIONS %s
