ba56673c485da1b0b8f349f45833ae726d12c43f
Jon Chesterfield <jonathanchesterfield@gmail.com>
2022-07-28 16:21:36 +0100

    [openmp] Introduce optional plugin init/deinit functions
    Will allow plugins to migrate away from using global variables to
    manage lifetime, which will fix a segfault discovered in relation to D127432
    
    Reviewed By: jhuber6
    
    Differential Revision: https://reviews.llvm.org/D130712

--- openmp/libomptarget/include/omptargetplugin.h
+++ openmp/libomptarget/include/omptargetplugin.h
@@ -20,6 +20,12 @@
 extern "C" {
 #endif
 
+// First method called on the plugin
+int32_t __tgt_rtl_init_plugin();
+
+// Last method called on the plugin
+int32_t __tgt_rtl_deinit_plugin();
+
 // Return the number of available devices of the type supported by the
 // target RTL.
 int32_t __tgt_rtl_number_of_devices(void);
