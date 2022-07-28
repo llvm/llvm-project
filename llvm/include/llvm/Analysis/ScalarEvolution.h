8daa338297d533db4d1ae8d3770613eb25c29688
Florian Hahn <flo@fhahn.com>
2022-07-28 10:02:19 +0100

    [SCEV] Avoid repeated proveNoUnsignedWrapViaInduction calls.
    At the moment, proveNoUnsignedWrapViaInduction may be called for the
    same AddRec a large number of times via getZeroExtendExpr. This can have
    a severe compile-time impact for very loop-heavy code. One one
    particular workload, LSR takes ~51s without this patch, almost
    exlusively in proveNoUnsignedWrapViaInduction. With this patch, the time
    in LSR drops to ~0.4s.
    
    If proveNoUnsignedWrapViaInduction failed to prove NUW the first time,
    it is unlikely to succeed on subsequent tries and the cost doesn't seem
    to be justified.
    
    Besides drastically improving compile-time in some excessive cases, this
    also has a slightly positive compile-time impact on CTMark:
    
    NewPM-O3: -0.07%
    NewPM-ReleaseThinLTO: -0.08%
    NewPM-ReleaseLTO-g: -0.06
    
    https://llvm-compile-time-tracker.com/compare.php?from=b435da027d7774c24cdb8c88d09f6b771e07fb14&to=f2729e33e8284b502f6c35a43345272252f35d12&stat=instructions
    
    Reviewed By: nikic
    
    Differential Revision: https://reviews.llvm.org/D130648

--- llvm/include/llvm/Analysis/ScalarEvolution.h
+++ llvm/include/llvm/Analysis/ScalarEvolution.h
@@ -2115,6 +2115,10 @@ private:
            std::pair<const SCEV *, SmallVector<const SCEVPredicate *, 3>>>
       PredicatedSCEVRewrites;
 
+  /// Set of AddRecs for which proving NUW via an induction has already been
+  /// tried.
+  SmallPtrSet<const SCEVAddRecExpr *, 16> UnsignedWrapViaInductionTried;
+
   /// The head of a linked list of all SCEVUnknown values that have been
   /// allocated. This is used by releaseMemory to locate them all and call
   /// their destructors.
