d11c356093da3c20b8e469c3e1ca6253705377ab
Adrian Prantl <aprantl@apple.com>
2022-07-29 16:57:21 -0700

    Resuscitate TestSwiftStaticLinking.py
    rdar://31066897
    (cherry picked from commit a488eaa028f8a55058feb07eec0238ab22cdc88b)

--- lldb/test/API/lang/swift/static_linking/macOS/B.swift
+++ lldb/test/API/lang/swift/static_linking/macOS/B.swift
@@ -1,19 +1,7 @@
-// B.swift
-//
-// This source file is part of the Swift.org open source project
-//
-// Copyright (c) 2014 - 2016 Apple Inc. and the Swift project authors
-// Licensed under Apache License v2.0 with Runtime Library Exception
-//
-// See https://swift.org/LICENSE.txt for license information
-// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
-//
-// -----------------------------------------------------------------------------
-
 import Foundation
 
 @objc public class B: NSObject {
-	public func bar() -> Int {
+	@objc public func bar() -> Int {
 	  return 8  // Set breakpoint here
 	}
 }
