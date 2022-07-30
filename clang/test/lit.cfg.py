e25046801d469422aa404a4ffacbd63a7fde1cc6
Sunho Kim <ksunhokim123@gmail.com>
2022-07-31 05:42:16 +0900

    [clang-repl] Disable building when LLVM_STATIC_LINK_CXX_STDLIB is ON.
    We have seen random symbol not found "__cxa_throw" error in fuschia build bots and out-of-tree users. The understanding have been that they are built without exception support, but it turned out that these platforms have LLVM_STATIC_LINK_CXX_STDLIB ON so that they link libstdc++ to llvm statically. The reason why this is problematic for clang-repl is that by default clang-repl tries to find symbols from symbol table of executable and dynamic libraries loaded by current process. It needs to load another libstdc++, but the platform that had LLVM_STATIC_LINK_CXX_STDLIB turned on is usally those with missing or obsolate shared libstdc++ in the first place -- trying to load it again would be destined to fail eventually with a risk to introuduce mixed libstdc++ versions.
    
    A proper solution that doesn't take a workaround is statically link the same libstdc++ by clang-repl side, but this is not possible with old JIT linker runtimedyld. New just-in-time linker JITLink handles this relatively well, but it's not availalbe in majority of platforms. For now, this patch just disables the building of clang-repl when LLVM_STATIC_LINK_CXX_STDLIB is ON and removes the "__cxa_throw" check in exception unittest as well as reverting previous exception check flag patch.
    
    Reviewed By: v.g.vassilev
    
    Differential Revision: https://reviews.llvm.org/D130788

--- clang/test/lit.cfg.py
+++ clang/test/lit.cfg.py
@@ -74,7 +74,6 @@ def have_host_jit_feature_support(feature_name):
     clang_repl_exe = lit.util.which('clang-repl', config.clang_tools_dir)
 
     if not clang_repl_exe:
-        print('clang-repl not found')
         return False
 
     try:
@@ -91,9 +90,6 @@ def have_host_jit_feature_support(feature_name):
 
 if have_host_jit_feature_support('jit'):
     config.available_features.add('host-supports-jit')
-
-if have_host_jit_feature_support('exception'):
-    config.available_features.add('host-supports-exception')
 
 if config.clang_staticanalyzer:
     config.available_features.add('staticanalyzer')
