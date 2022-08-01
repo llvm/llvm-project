7eddeb9e99f36d907295a2ed75244d39e1e41e33
Zakk Chen <zakkc@google.com>
2022-08-01 17:59:53 +0000

    [RISCV][Clang] Support policy functions for vmerge, vfmerge and vcompress.
    We will switch all UndefValue to PoisonValue in follow up patches.
    
    Reviewed By: kito-cheng
    
    Differential Revision: https://reviews.llvm.org/D126745

--- clang/include/clang/Support/RISCVVIntrinsicUtils.h
+++ clang/include/clang/Support/RISCVVIntrinsicUtils.h
@@ -290,8 +290,12 @@ public:
 
 enum PolicyScheme : uint8_t {
   SchemeNone,
+  // Passthru operand is at first parameter in C builtin.
   HasPassthruOperand,
   HasPolicyOperand,
+  // Special case for vmerge, the passthru operand is second
+  // parameter in C builtin.
+  HasPassthruOperandAtIdx1,
 };
 
 // TODO refactor RVVIntrinsic class design after support all intrinsic
