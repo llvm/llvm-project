a27bbd453a8ef53f582058892b0e8fb7bb270e1e
Alex Bradbury <asb@igalia.com>
2022-08-01 10:08:17 +0100

    [NFC] Use AllocaInst's getAddressSpace helper

--- llvm/lib/IR/AsmWriter.cpp
+++ llvm/lib/IR/AsmWriter.cpp
@@ -4260,7 +4260,7 @@ void AssemblyWriter::printInstruction(const Instruction &I) {
       Out << ", align " << A->value();
     }
 
-    unsigned AddrSpace = AI->getType()->getAddressSpace();
+    unsigned AddrSpace = AI->getAddressSpace();
     if (AddrSpace != 0) {
       Out << ", addrspace(" << AddrSpace << ')';
     }
