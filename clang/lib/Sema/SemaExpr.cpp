2c3f5d71e3118d67ccd0c988bcc1f9bc0710574c
David Truby <david.truby@arm.com>
2022-08-02 11:54:05 +0000

    [clang][AArch64][SVE] Add unary +/- operators for SVE types
    This patch enables the unary promotion and negation operators on
    SVE types.
    
    Differential Revision: https://reviews.llvm.org/D130984
    
    (cherry picked from commit 286d59ef6f7118c8a0600f3d281593700528a5d3)

--- clang/lib/Sema/SemaExpr.cpp
+++ clang/lib/Sema/SemaExpr.cpp
@@ -15603,6 +15603,8 @@ ExprResult Sema::CreateBuiltinUnaryOp(SourceLocation OpLoc,
               resultType->castAs<VectorType>()->getVectorKind() !=
               VectorType::AltiVecBool))
       break;
+    else if (resultType->isVLSTBuiltinType()) // SVE vectors allow + and -
+      break;
     else if (getLangOpts().CPlusPlus && // C++ [expr.unary.op]p6
              Opc == UO_Plus &&
              resultType->isPointerType())
