760d2b462c04537d119d76d3cc37d2cb53774a05
Nikolas Klauser <nikolasklauser@berlin.de>
2022-08-02 22:13:59 -0700

    [libc++][ranges] Implement `ranges::remove_copy{, _if}`.
    Co-authored-by: Hui Xie <hui.xie1990@gmail.com>
    
    Differential Revision: https://reviews.llvm.org/D130599

--- libcxx/test/std/algorithms/ranges_result_alias_declarations.compile.pass.cpp
+++ libcxx/test/std/algorithms/ranges_result_alias_declarations.compile.pass.cpp
@@ -32,8 +32,8 @@ static_assert(std::is_same_v<in_out_result<int, long>, copy_backward_result<int,
 static_assert(std::is_same_v<in_out_result<int, long>, move_result<int, long>>);
 static_assert(std::is_same_v<in_out_result<int, long>, move_backward_result<int, long>>);
 static_assert(std::is_same_v<in_out_result<int, long>, partial_sort_copy_result<int, long>>);
-// static_assert(std::is_same_v<in_out_result<int, long>, remove_copy_result<int, long>>);
-// static_assert(std::is_same_v<in_out_result<int, long>, remove_copy_if_result<int, long>>);
+static_assert(std::is_same_v<in_out_result<int, long>, remove_copy_result<int, long>>);
+static_assert(std::is_same_v<in_out_result<int, long>, remove_copy_if_result<int, long>>);
 // static_assert(std::is_same_v<in_out_result<int, long>, replace_copy_result<int, long>>);
 // static_assert(std::is_same_v<in_out_result<int, long>, replace_copy_if_result<int, long>>);
 // static_assert(std::is_same_v<in_out_result<int, long>, reverse_copy_result<int, long>>);
