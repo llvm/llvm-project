7d417ff48ed1220b67762c84af670a37becf04c6
Kazu Hirata <kazu@google.com>
2022-08-02 22:16:55 -0700

    [mlir] Add has_value and value to OptionalParseResult
    llvm::Optional is in the process of switching to the
    std::optional-like interface with has_value/value as opposed to
    hasValue/getValue.
    
    This patch adds has_value and value to enable the same transition.
    
    Differential Revision: https://reviews.llvm.org/D130819

--- mlir/include/mlir/IR/OpDefinition.h
+++ mlir/include/mlir/IR/OpDefinition.h
@@ -44,9 +44,11 @@ public:
   OptionalParseResult(llvm::NoneType) : impl(llvm::None) {}
 
   /// Returns true if we contain a valid ParseResult value.
+  bool has_value() const { return impl.has_value(); }
   bool hasValue() const { return impl.has_value(); }
 
   /// Access the internal ParseResult value.
+  ParseResult value() const { return impl.value(); }
   ParseResult getValue() const { return impl.value(); }
   ParseResult operator*() const { return getValue(); }
 
