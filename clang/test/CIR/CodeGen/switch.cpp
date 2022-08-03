4a32a2d6425d11fb1a525df6b66083904ca5a1a3
Nathan Lanza <nathan@lanza.io>
2022-08-03 11:45:03 -0700

    [TO BE DROPPED] Remove dead switch case for now

--- clang/test/CIR/CodeGen/switch.cpp
+++ clang/test/CIR/CodeGen/switch.cpp
@@ -1,12 +1,6 @@
 // RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fclangir -emit-cir %s -o %t.cir
 // RUN: FileCheck --input-file=%t.cir %s
 
-void sw0(int a) {
-  switch (a) {}
-}
+void sw0(int a) {}
 
-// CHECK: cir.scope {
-// CHECK-NEXT:   %1 = cir.load %0 : cir.ptr <i32>, i32
-// CHECK-NEXT:   cir.switch (%1 : i32) [
-// CHECK-NEXT:   ]
-// CHECK-NEXT: }
+// CHECK: func.func
