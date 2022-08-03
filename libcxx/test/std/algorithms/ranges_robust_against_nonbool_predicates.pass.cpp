f20566daeb0eb7d560e69847b37cb9dc38cd6129
Nikolas Klauser <nikolasklauser@berlin.de>
2022-08-03 16:30:50 -0700

    [libc++][ranges] Implement `ranges::clamp`
    Differential Revision: https://reviews.llvm.org/D126193
    
    (cherry picked from commit a203acb9dd7227323d6161431225189d49917744)

--- libcxx/test/std/algorithms/ranges_robust_against_nonbool_predicates.pass.cpp
+++ libcxx/test/std/algorithms/ranges_robust_against_nonbool_predicates.pass.cpp
@@ -109,7 +109,7 @@ constexpr bool test_all() {
   test(std::ranges::includes, in, in2, binary_pred);
   test(std::ranges::is_heap, in, binary_pred);
   test(std::ranges::is_heap_until, in, binary_pred);
-  //std::ranges::clamp(2, 1, 3, binary_pred);
+  std::ranges::clamp(2, 1, 3, binary_pred);
   //test(std::ranges::is_permutation, in, in2, binary_pred);
   test(std::ranges::copy_if, in, out, unary_pred);
   test(std::ranges::remove_copy_if, in, out, unary_pred);
