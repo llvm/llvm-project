33171df9cc7f6560dea7b0b162ab51ff97417468
Leonard Chan <leonardchan@google.com>
2022-08-04 22:56:32 +0000

    Revert "[clang][Darwin] Always set the default C++ Standard Library to libc++"
    This reverts commit c5ccb78ade8136134e0ca9dde64de97f913f0f8c.
    
    We're seeing darwin-stdlib.cpp fail on our linux, mac, and windows
    builders:
    https://luci-milo.appspot.com/ui/p/fuchsia/builders/toolchain.ci/clang-linux-x64/b8806821020552676065/overview

--- clang/lib/Driver/ToolChains/Darwin.cpp
+++ clang/lib/Driver/ToolChains/Darwin.cpp
@@ -896,7 +896,12 @@ types::ID MachO::LookupTypeForExtension(StringRef Ext) const {
 bool MachO::HasNativeLLVMSupport() const { return true; }
 
 ToolChain::CXXStdlibType Darwin::GetDefaultCXXStdlibType() const {
-  // Always use libc++ by default
+  // Use libstdc++ on old targets (OSX < 10.9 and iOS < 7)
+  if ((isTargetMacOSBased() && isMacosxVersionLT(10, 9)) ||
+      (isTargetIOSBased() && isIPhoneOSVersionLT(7, 0)))
+    return ToolChain::CST_Libstdcxx;
+
+  // On all other targets, use libc++
   return ToolChain::CST_Libcxx;
 }
 
