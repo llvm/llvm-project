bb156d9430f0fccca17cd9579f6ea5a270e86422
Shilei Tian <i@tianshilei.me>
2022-08-04 14:48:07 -0400

    [OpenMP] Fix the test case issue that printf cannot be used in target region for AMDGPU

--- openmp/libomptarget/test/offloading/test_libc.cpp
+++ openmp/libomptarget/test/offloading/test_libc.cpp
@@ -6,16 +6,17 @@ extern "C" int printf(const char *, ...);
 
 // std::equal is lowered to libc function memcmp.
 void test_memcpy() {
-#pragma omp target
+  int r = 0;
+#pragma omp target map(from: r)
   {
     int x[2] = {0, 0};
     int y[2] = {0, 0};
     int z[2] = {0, 1};
     bool eq1 = std::equal(x, x + 2, y);
     bool eq2 = std::equal(x, x + 2, z);
-    bool r = eq1 && !eq2;
-    printf("memcmp: %s\n", r ? "PASS" : "FAIL");
+    r = eq1 && !eq2;
   }
+  printf("memcmp: %s\n", r ? "PASS" : "FAIL");
 }
 
 int main(int argc, char *argv[]) {
