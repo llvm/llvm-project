f2ed78db69a47a35e3f674fb780a5a9c68833628
Nathan James <n.james93@hotmail.co.uk>
2022-08-05 08:42:51 +0100

    [clangd] Change the url for clang-tidy check documentation
    In https://github.com/llvm/llvm-project/commit/eecf87cdb19ec295e9062d675d31817227eecca6, The directory structure of the documentation for clang-tidy checks was changed, however clangd wasn't updated.
    Now all the links generated will point to old dead pages.
    This updated clangd to use the new page structure.
    
    Reviewed By: sammccall, kadircet
    
    Differential Revision: https://reviews.llvm.org/D128379

--- clang-tools-extra/clangd/Diagnostics.cpp
+++ clang-tools-extra/clangd/Diagnostics.cpp
@@ -918,9 +918,19 @@ llvm::Optional<std::string> getDiagnosticDocURI(Diag::DiagSource Source,
     // information to be worth linking.
     // https://clang.llvm.org/docs/DiagnosticsReference.html
     break;
-  case Diag::ClangTidy:
-    return {("https://clang.llvm.org/extra/clang-tidy/checks/" + Name + ".html")
-                .str()};
+  case Diag::ClangTidy: {
+    StringRef Module, Check;
+    // This won't correctly get the module for clang-analyzer checks, but as we
+    // don't link in the analyzer that shouldn't be an issue.
+    // This would also need updating if anyone decides to create a module with a
+    // '-' in the name.
+    std::tie(Module, Check) = Name.split('-');
+    if (Module.empty() || Check.empty())
+      return llvm::None;
+    return ("https://clang.llvm.org/extra/clang-tidy/checks/" + Module + "/" +
+            Check + ".html")
+        .str();
+  }
   case Diag::Clangd:
     if (Name == "unused-includes")
       return {"https://clangd.llvm.org/guides/include-cleaner"};
