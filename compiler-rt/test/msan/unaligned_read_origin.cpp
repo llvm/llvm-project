187a3a8b4d30eef4b9063aa40560eac1959c1406
Kevin Athey <kda@google.com>
2022-08-05 16:50:45 -0700

    Desist from passing function location to __msan_set_alloca_origin4.
    This is done by calling __msan_set_alloca_origin and providing the location of the variable by using the call stack.
    This is prepatory work for dropping variable names when track-origins is enabled.
    
    Reviewed By: vitalybuka
    
    Differential Revision: https://reviews.llvm.org/D131205

--- compiler-rt/test/msan/unaligned_read_origin.cpp
+++ compiler-rt/test/msan/unaligned_read_origin.cpp
@@ -12,5 +12,5 @@ int main(int argc, char **argv) {
   // CHECK: WARNING: MemorySanitizer: use-of-uninitialized-value
   // CHECK: {{#0 0x.* in main .*unaligned_read_origin.cpp:}}[[@LINE-2]]
   // CHECK: Uninitialized value was created by an allocation of 'x' in the stack frame of function 'main'
-  // CHECK: {{#0 0x.* in main .*unaligned_read_origin.cpp:}}[[@LINE-7]]
+  // CHECK: {{#0 0x.* in main .*unaligned_read_origin.cpp:}}[[@LINE-6]]
 }
