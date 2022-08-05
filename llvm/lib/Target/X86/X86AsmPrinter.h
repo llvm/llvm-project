07c828509751dc7eb7546f71b50564f54ac4a198
Phoebe Wang <phoebe.wang@intel.com>
2022-08-05 01:36:33 -0700

    [X86] Move getting module flag into `runOnMachineFunction` to reduce compile-time. NFCI
    Reviewed By: nikic
    
    Differential Revision: https://reviews.llvm.org/D131245

--- llvm/lib/Target/X86/X86AsmPrinter.h
+++ llvm/lib/Target/X86/X86AsmPrinter.h
@@ -31,6 +31,7 @@ class LLVM_LIBRARY_VISIBILITY X86AsmPrinter : public AsmPrinter {
   std::unique_ptr<MCCodeEmitter> CodeEmitter;
   bool EmitFPOData = false;
   bool ShouldEmitWeakSwiftAsyncExtendedFramePointerFlags = false;
+  bool IndCSPrefix = false;
 
   // This utility class tracks the length of a stackmap instruction's 'shadow'.
   // It is used by the X86AsmPrinter to ensure that the stackmap shadow
