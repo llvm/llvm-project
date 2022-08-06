cb4e86bc72803f354cc83292b1b586d90dc1bfad
Adrian Vogelsgesang <avogelsgesang@salesforce.com>
2022-08-06 15:04:38 +0200

    [libc++] Implement `operator<=>` for `unique_ptr`
    Implements part of:
    
      - P1614R2 The Mothership has Landed
    
    Fixes LWG3426
    
    Reviewed By: #libc, Mordante
    
    Differential Revision: https://reviews.llvm.org/D130838

--- libcxx/test/std/utilities/smartptr/unique.ptr/unique.ptr.special/cmp_nullptr.pass.cpp
+++ libcxx/test/std/utilities/smartptr/unique.ptr/unique.ptr.special/cmp_nullptr.pass.cpp
@@ -19,54 +19,73 @@
 // template <class T, class D>
 //     bool operator!=(nullptr_t, const unique_ptr<T, D>& y) noexcept;
 // template <class T, class D>
-//     bool operator<(const unique_ptr<T, D>& x, nullptr_t) noexcept;
+//     bool operator<(const unique_ptr<T, D>& x, nullptr_t);
 // template <class T, class D>
-//     bool operator<(nullptr_t, const unique_ptr<T, D>& y) noexcept;
+//     bool operator<(nullptr_t, const unique_ptr<T, D>& y);
 // template <class T, class D>
-//     bool operator<=(const unique_ptr<T, D>& x, nullptr_t) noexcept;
+//     bool operator<=(const unique_ptr<T, D>& x, nullptr_t);
 // template <class T, class D>
-//     bool operator<=(nullptr_t, const unique_ptr<T, D>& y) noexcept;
+//     bool operator<=(nullptr_t, const unique_ptr<T, D>& y);
 // template <class T, class D>
-//     bool operator>(const unique_ptr<T, D>& x, nullptr_t) noexcept;
+//     bool operator>(const unique_ptr<T, D>& x, nullptr_t);
 // template <class T, class D>
-//     bool operator>(nullptr_t, const unique_ptr<T, D>& y) noexcept;
+//     bool operator>(nullptr_t, const unique_ptr<T, D>& y);
 // template <class T, class D>
-//     bool operator>=(const unique_ptr<T, D>& x, nullptr_t) noexcept;
+//     bool operator>=(const unique_ptr<T, D>& x, nullptr_t);
 // template <class T, class D>
-//     bool operator>=(nullptr_t, const unique_ptr<T, D>& y) noexcept;
+//     bool operator>=(nullptr_t, const unique_ptr<T, D>& y);
+// template<class T, class D>
+//   requires three_­way_­comparable<typename unique_ptr<T, D>::pointer>
+//   constexpr compare_three_way_result_t<typename unique_ptr<T, D>::pointer>
+//     operator<=>(const unique_ptr<T, D>& x, nullptr_t);                            // C++20
 
 #include <memory>
 #include <cassert>
 
 #include "test_macros.h"
-
-void do_nothing(int*) {}
+#include "test_comparisons.h"
 
 int main(int, char**)
 {
-    const std::unique_ptr<int> p1(new int(1));
-    assert(!(p1 == nullptr));
-    assert(!(nullptr == p1));
-    assert(!(p1 < nullptr));
-    assert( (nullptr < p1));
-    assert(!(p1 <= nullptr));
-    assert( (nullptr <= p1));
-    assert( (p1 > nullptr));
-    assert(!(nullptr > p1));
-    assert( (p1 >= nullptr));
-    assert(!(nullptr >= p1));
+  AssertEqualityAreNoexcept<std::unique_ptr<int>, nullptr_t>();
+  AssertEqualityAreNoexcept<nullptr_t, std::unique_ptr<int> >();
+  AssertComparisonsReturnBool<std::unique_ptr<int>, nullptr_t>();
+  AssertComparisonsReturnBool<nullptr_t, std::unique_ptr<int> >();
+#if TEST_STD_VER > 17
+  AssertOrderReturn<std::strong_ordering, std::unique_ptr<int>, nullptr_t>();
+  AssertOrderReturn<std::strong_ordering, nullptr_t, std::unique_ptr<int>>();
+#endif
 
-    const std::unique_ptr<int> p2;
-    assert( (p2 == nullptr));
-    assert( (nullptr == p2));
-    assert(!(p2 < nullptr));
-    assert(!(nullptr < p2));
-    assert( (p2 <= nullptr));
-    assert( (nullptr <= p2));
-    assert(!(p2 > nullptr));
-    assert(!(nullptr > p2));
-    assert( (p2 >= nullptr));
-    assert( (nullptr >= p2));
+  const std::unique_ptr<int> p1(new int(1));
+  assert(!(p1 == nullptr));
+  assert(!(nullptr == p1));
+  assert(!(p1 < nullptr));
+  assert((nullptr < p1));
+  assert(!(p1 <= nullptr));
+  assert((nullptr <= p1));
+  assert((p1 > nullptr));
+  assert(!(nullptr > p1));
+  assert((p1 >= nullptr));
+  assert(!(nullptr >= p1));
+#if TEST_STD_VER > 17
+  assert((nullptr <=> p1) == std::strong_ordering::less);
+  assert((p1 <=> nullptr) == std::strong_ordering::greater);
+#endif
+
+  const std::unique_ptr<int> p2;
+  assert((p2 == nullptr));
+  assert((nullptr == p2));
+  assert(!(p2 < nullptr));
+  assert(!(nullptr < p2));
+  assert((p2 <= nullptr));
+  assert((nullptr <= p2));
+  assert(!(p2 > nullptr));
+  assert(!(nullptr > p2));
+  assert((p2 >= nullptr));
+  assert((nullptr >= p2));
+#if TEST_STD_VER > 17
+  assert((nullptr <=> p2) == std::strong_ordering::equivalent);
+#endif
 
   return 0;
 }
