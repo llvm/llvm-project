9dbe909df18b62e9f54b41bc44e903101dff6c09
Lang Hames <lhames@gmail.com>
2022-08-07 14:15:08 -0700

    [ORC-RT] Fix missing #include <string_view>s.
    fdb6ade5dfc updated the ORC runtime to use std::string_view, rather than its
    own placeholder class (__orc_rt::string_view), but failed to add these
    includes.

--- compiler-rt/lib/orc/elfnix_platform.cpp
+++ compiler-rt/lib/orc/elfnix_platform.cpp
@@ -19,6 +19,7 @@
 #include <map>
 #include <mutex>
 #include <sstream>
+#include <string_view>
 #include <unordered_map>
 #include <vector>
 
--- compiler-rt/lib/orc/simple_packed_serialization.h
+++ compiler-rt/lib/orc/simple_packed_serialization.h
@@ -40,6 +40,7 @@
 #include "stl_extras.h"
 
 #include <string>
+#include <string_view>
 #include <tuple>
 #include <type_traits>
 #include <unordered_map>
