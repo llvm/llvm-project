35e586124659f6bb12bad685f00a7ade3751d17a
Vitaly Buka <vitalybuka@google.com>
2022-08-08 18:34:01 -0700

    [asan] Avoid few branches on memcpy hot path

--- compiler-rt/lib/asan/asan_internal.h
+++ compiler-rt/lib/asan/asan_internal.h
@@ -133,6 +133,7 @@ void InstallAtExitCheckLeaks();
 extern int asan_inited;
 // Used to avoid infinite recursion in __asan_init().
 extern bool asan_init_is_running;
+extern bool replace_intrin_cached;
 extern void (*death_callback)(void);
 // These magic values are written to shadow for better error
 // reporting.
