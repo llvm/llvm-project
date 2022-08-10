ca0c092777f690409042593f8a62a8a24038faf6
Med Ismail Bennani <medismail.bennani@gmail.com>
2022-08-09 21:44:20 -0700

    Revert "Reland "[compiler-rt][BF16] Provide __truncsfbf2 only when __bf16 is available""
    This reverts commit 45d367bc5aab32c2a4361b0dd1ea319e4a222f16 since it
    causes a build failure on macOS:
    
    https://green.lab.llvm.org/green/job/lldb-cmake/45984/

--- compiler-rt/lib/builtins/truncsfbf2.c
+++ compiler-rt/lib/builtins/truncsfbf2.c
@@ -6,11 +6,8 @@
 //
 //===----------------------------------------------------------------------===//
 
-#if defined(COMPILER_RT_HAS_BFLOAT16)
 #define SRC_SINGLE
 #define DST_BFLOAT
 #include "fp_trunc_impl.inc"
 
 COMPILER_RT_ABI dst_t __truncsfbf2(float a) { return __truncXfYf2__(a); }
-
-#endif
