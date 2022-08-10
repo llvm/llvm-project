ca1127ce65bf08ac618dd4fea0eeb2a11836b9b1
Simon Pilgrim <llvm-dev@redking.me.uk>
2022-08-10 11:27:08 +0100

    [DAG] canCreateUndefOrPoison - add freeze(truncate(x)) -> truncate(freeze(x)) support

--- llvm/lib/CodeGen/SelectionDAG/SelectionDAG.cpp
+++ llvm/lib/CodeGen/SelectionDAG/SelectionDAG.cpp
@@ -4567,6 +4567,7 @@ bool SelectionDAG::canCreateUndefOrPoison(SDValue Op, const APInt &DemandedElts,
   case ISD::PARITY:
   case ISD::SIGN_EXTEND:
   case ISD::ZERO_EXTEND:
+  case ISD::TRUNCATE:
   case ISD::BITCAST:
     return false;
 
