181c0373d2832afbb62ca50c6c59d9759d17edf7
Jonas Devlieghere <jonas@devlieghere.com>
2022-08-11 12:34:20 -0700

    [lldb] Simplify TestExec's source code
    Simplify the test's source code, remove unnecessary headers, and convert
    it from C++ to C.

--- lldb/test/API/functionalities/exec/main.cpp
+++ lldb/test/API/functionalities/exec/main.cpp
@@ -1,19 +1,10 @@
-#define _POSIX_C_SOURCE 200809L
-
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <libgen.h>
-#include <string>
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 int main(int argc, char const **argv) {
-  char *buf = strdup(argv[0]); // Set breakpoint 1 here
-  std::string directory_name(::dirname(buf));
-
-  std::string other_program = directory_name + "/secondprog";
-  argv[0] = other_program.c_str();
-  execv(argv[0], const_cast<char *const *>(argv));
+  // Set breakpoint 1 here
+  execl("secondprog", "secondprog", NULL);
   perror("execve");
   abort();
 }
