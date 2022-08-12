267344b12eccbfa8ef4604e4b91f14f73d04bb13
Jonas Devlieghere <jonas@devlieghere.com>
2022-08-12 10:06:21 -0700

    [lldb] Skip target variable tests on Darwin because of chained fixups
    When targeting macOS Ventura, ld64 will use authenticated fixups for
    x86_64 as well as arm64 (where that has always been the case). This
    results in test failures when using an Xcode 14 toolchain on an Intel
    mac running macOS Ventura:
    
      Failed Tests (3):
        lldb-api :: commands/target/basic/TestTargetCommand.py
        lldb-api :: lang/c/global_variables/TestGlobalVariables.py
        lldb-api :: lang/cpp/char8_t/TestCxxChar8_t.py
    
    Rather than trying to come up with a sophisticated decorator based off
    the deployment target, I marked them all as skipped with a comment
    explaining why.
    
    Differential revision: https://reviews.llvm.org/D131741

--- lldb/test/API/commands/target/basic/TestTargetCommand.py
+++ lldb/test/API/commands/target/basic/TestTargetCommand.py
@@ -42,7 +42,7 @@ class targetCommandTestCase(TestBase):
         self.buildAll()
         self.do_target_command()
 
-    @expectedFailureDarwin(archs=["arm64", "arm64e"]) # <rdar://problem/37773624>
+    @skipIfDarwin # Chained Fixups
     def test_target_variable_command(self):
         """Test 'target variable' command before and after starting the inferior."""
         d = {'C_SOURCES': 'globals.c', 'EXE': self.getBuildArtifact('globals')}
@@ -51,7 +51,7 @@ class targetCommandTestCase(TestBase):
 
         self.do_target_variable_command('globals')
 
-    @expectedFailureDarwin(archs=["arm64", "arm64e"]) # <rdar://problem/37773624>
+    @skipIfDarwin # Chained Fixups
     def test_target_variable_command_no_fail(self):
         """Test 'target variable' command before and after starting the inferior."""
         d = {'C_SOURCES': 'globals.c', 'EXE': self.getBuildArtifact('globals')}
