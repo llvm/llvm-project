0169d5d02752305c9f0a783cf1e7239062192b2b
Alan Zhao <ayzhao@google.com>
2022-08-12 19:39:26 -0400

    [clang][diagnostics] Don't warn about unreachable code in constexpr if
    The point of a constexpr if statement is to determine which branch to
    take at compile time, so warning on unreachable code is meaningless in
    these situations.
    
    Fixes #57123.
    
    Reviewed By: thakis
    
    Differential Revision: https://reviews.llvm.org/D131818

--- clang/lib/Analysis/ReachableCode.cpp
+++ clang/lib/Analysis/ReachableCode.cpp
@@ -299,6 +299,12 @@ static bool shouldTreatSuccessorsAsReachable(const CFGBlock *B,
     if (isa<BinaryOperator>(Term)) {
       return isConfigurationValue(Term, PP);
     }
+    // Do not treat constexpr if statement successors as unreachable in warnings
+    // since the point of these statements is to determine branches at compile
+    // time.
+    if (const auto *IS = dyn_cast<IfStmt>(Term);
+        IS != nullptr && IS->isConstexpr())
+      return true;
   }
 
   const Stmt *Cond = B->getTerminatorCondition(/* stripParens */ false);
