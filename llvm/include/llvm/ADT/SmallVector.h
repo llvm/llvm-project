91d784a0217f24891de4e55fa06678e45864e57a
wangyihan <yihan.wang@intel.com>
2022-08-15 21:11:42 +0800

    [NFC][SmallVector] Use std::conditional_t instead of std::conditional
    Signed-off-by: wangyihan <yihan.wang@intel.com>

--- llvm/include/llvm/ADT/SmallVector.h
+++ llvm/include/llvm/ADT/SmallVector.h
@@ -92,8 +92,8 @@ protected:
 
 template <class T>
 using SmallVectorSizeType =
-    typename std::conditional<sizeof(T) < 4 && sizeof(void *) >= 8, uint64_t,
-                              uint32_t>::type;
+    std::conditional_t<sizeof(T) < 4 && sizeof(void *) >= 8, uint64_t,
+                       uint32_t>;
 
 /// Figure out the offset of the first element.
 template <class T, typename = void> struct SmallVectorAlignmentAndSize {
