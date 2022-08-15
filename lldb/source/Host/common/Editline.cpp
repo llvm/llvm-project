f2370814788b5878c2f513988fc09ef65360a16c
Jonas Devlieghere <jonas@devlieghere.com>
2022-08-15 11:23:33 -0700

    [lldb] Fix warning: comparison of integers of different signs
    Fixes a warning about comparison of integers of different signs
    'wchar_t' and 'int' in Editline.cpp:
    
          return out != (int)WEOF;
                 ~~~ ^  ~~~~~~~~~

--- lldb/source/Host/common/Editline.cpp
+++ lldb/source/Host/common/Editline.cpp
@@ -1609,7 +1609,7 @@ bool Editline::CompleteCharacter(char ch, EditLineGetCharType &out) {
     switch (cvt.in(state, input.begin(), input.end(), from_next, &out, &out + 1,
                    to_next)) {
     case std::codecvt_base::ok:
-      return out != (int)WEOF;
+      return out != (EditLineGetCharType)WEOF;
 
     case std::codecvt_base::error:
     case std::codecvt_base::noconv:
