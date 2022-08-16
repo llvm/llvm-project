6e06e6ab65db7e1865b04e7886522d5ae55be3b0
Jonas Devlieghere <jonas@devlieghere.com>
2022-08-15 21:26:25 -0700

    [lldb] Adjust LogChannelSwift for upstream logging changes
    (cherry picked from commit bbee0c7159b1b0416be616e7c7050d7b3e22d9c0)

--- lldb/source/Plugins/Language/Swift/LogChannelSwift.cpp
+++ lldb/source/Plugins/Language/Swift/LogChannelSwift.cpp
@@ -7,6 +7,7 @@
 //===----------------------------------------------------------------------===//
 
 #include "LogChannelSwift.h"
+#include "lldb/Utility/Log.h"
 #include "lldb/Version/Version.h"
 
 using namespace lldb_private;
@@ -25,20 +26,39 @@ template <> Log::Channel &lldb_private::LogChannelFor<SwiftLog>() {
   return g_channel;
 }
 
+class StringLogHandler : public LogHandler {
+public:
+  StringLogHandler(std::string& str) : m_string(str) {}
+
+  void Emit(llvm::StringRef message) override {
+    std::lock_guard<std::mutex> guard(m_mutex);
+    m_string += std::string(message);
+  }
+
+  bool isA(const void *ClassID) const override { return ClassID == &ID; }
+  static bool classof(const LogHandler *obj) { return obj->isA(&ID); }
+
+private:
+  std::mutex m_mutex;
+  std::string& m_string;
+  static char ID;
+};
+
+char StringLogHandler::ID;
+
 void LogChannelSwift::Initialize() {
   Log::Register("swift", g_channel);
 
-  llvm::raw_null_ostream error_stream;
-  Log::EnableLogChannel(
-      std::make_shared<llvm::raw_string_ostream>(g_swift_log_buffer),
-      LLDB_LOG_OPTION_THREADSAFE, "swift", {"health"}, error_stream);
+  auto log_handler_sp = std::make_shared<StringLogHandler>(g_swift_log_buffer);
+  Log::EnableLogChannel(log_handler_sp, 0, "swift", {"health"}, llvm::nulls());
   if (Log *log = GetSwiftHealthLog())
     log->Printf(
         "==== LLDB swift-healthcheck log. ===\n"
         "This file contains the configuration of LLDB's embedded Swift "
         "compiler to help diagnosing module import and search path issues. "
         "The swift-healthcheck command is meant to be run *after* an error "
-        "has occurred.\n%s", lldb_private::GetVersion());
+        "has occurred.\n%s",
+        lldb_private::GetVersion());
 }
 
 void LogChannelSwift::Terminate() { Log::Unregister("swift"); }
