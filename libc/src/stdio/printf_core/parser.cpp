d769cd8cdc11bb2439bf8d0cb3f079c9e8912fa8
Guillaume Chatelet <gchatelet@google.com>
2022-08-19 21:10:12 +0000

    [reland][libc][NFC] Use STL case for bit

--- libc/src/stdio/printf_core/parser.cpp
+++ libc/src/stdio/printf_core/parser.cpp
@@ -12,7 +12,7 @@
 
 #include "src/__support/arg_list.h"
 
-#include "src/__support/CPP/Bit.h"
+#include "src/__support/CPP/bit.h"
 #include "src/__support/FPUtil/FPBits.h"
 #include "src/__support/ctype_utils.h"
 #include "src/__support/str_to_integer.h"
@@ -132,10 +132,11 @@ FormatSection Parser::get_next_section() {
     case ('G'):
       if (lm != LengthModifier::L)
         section.conv_val_raw =
-            bit_cast<uint64_t>(GET_ARG_VAL_SIMPLEST(double, conv_index));
+            cpp::bit_cast<uint64_t>(GET_ARG_VAL_SIMPLEST(double, conv_index));
       else
-        section.conv_val_raw = bit_cast<fputil::FPBits<long double>::UIntType>(
-            GET_ARG_VAL_SIMPLEST(long double, conv_index));
+        section.conv_val_raw =
+            cpp::bit_cast<fputil::FPBits<long double>::UIntType>(
+                GET_ARG_VAL_SIMPLEST(long double, conv_index));
       break;
 #endif // LLVM_LIBC_PRINTF_DISABLE_FLOAT
 #ifndef LLVM_LIBC_PRINTF_DISABLE_WRITE_INT
