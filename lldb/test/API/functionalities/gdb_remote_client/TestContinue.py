f371784985b7b7b0131f954b9486816f036d30d2
Michał Górny <mgorny@moritz.systems>
2022-08-19 10:07:37 +0200

    [lldb] [test] Remove test_step_multiprocess, it's unreliable
    Sponsored by: The FreeBSD Foundation

--- lldb/test/API/functionalities/gdb_remote_client/TestContinue.py
+++ lldb/test/API/functionalities/gdb_remote_client/TestContinue.py
@@ -66,25 +66,3 @@ class TestContinue(GDBRemoteTestBase):
         target = self.createTarget("a.yaml")
         process = self.connect(target)
         self.assertPacketLogContains(["vCont;C13:p400.401"])
-
-    # uses 'S13' instead of 's' arm (the pc dance?), testing it on one
-    # arch should be entirely sufficient
-    @skipIf(archs=no_match(["x86_64"]))
-    def test_step_multiprocess(self):
-        class MyResponder(self.BaseResponder):
-            def other(self, packet):
-                if packet == "vCont?":
-                    return "vCont;c;C;s;S"
-                if packet.startswith("vCont;C"):
-                    return "S13"
-                if packet.startswith("vCont;s"):
-                    return "W00"
-                return ""
-
-        self.server.responder = MyResponder()
-        self.runCmd("platform select remote-linux")
-        target = self.createTarget("a.yaml")
-        process = self.connect(target)
-        thread = process.GetSelectedThread()
-        thread.StepInstruction(False)
-        self.assertPacketLogContains(["vCont;s:p400.401"])
