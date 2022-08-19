175560fc21bc79c90c2cde8c0f0d8d8930220f61
Guillaume Chatelet <gchatelet@google.com>
2022-08-19 21:17:48 +0000

    Revert "[libc][NFC] Use STL case for bit"
    This reverts commit 7ba14b8611df1172d92d60bf19870e98a92e7a3a.

--- libc/src/__support/CPP/Bit.h
+++ libc/src/__support/CPP/Bit.h
@@ -9,7 +9,7 @@
 #ifndef LLVM_LIBC_SUPPORT_CPP_BIT_H
 #define LLVM_LIBC_SUPPORT_CPP_BIT_H
 
-namespace __llvm_libc::cpp {
+namespace __llvm_libc {
 
 #if defined __has_builtin
 #if __has_builtin(__builtin_bit_cast)
@@ -43,6 +43,6 @@ template <class To, class From> constexpr To bit_cast(const From &from) {
 #endif // defined(LLVM_LIBC_HAS_BUILTIN_BIT_CAST)
 }
 
-} // namespace __llvm_libc::cpp
+} // namespace __llvm_libc
 
 #endif // LLVM_LIBC_SUPPORT_CPP_BIT_H
