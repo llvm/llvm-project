f2bf924ca2037f681b1e68bdf862f356e94dab3c
Nikolas Klauser <nikolasklauser@berlin.de>
2022-08-22 02:59:09 +0200

    [libc++] Enable hash only for the correct types
    Also implement LWG3705.
    Fixes https://github.com/llvm/llvm-project/issues/55823
    
    Reviewed By: ldionne, #libc
    
    Spies: libcxx-commits
    
    Differential Revision: https://reviews.llvm.org/D132338

--- libcxx/test/std/strings/basic.string.hash/enabled_hashes.pass.cpp
+++ libcxx/test/std/strings/basic.string.hash/enabled_hashes.pass.cpp
@@ -15,9 +15,14 @@
 
 #include <string>
 
+#include "constexpr_char_traits.h"
 #include "poisoned_hash_helper.h"
-
+#include "test_allocator.h"
 #include "test_macros.h"
+
+struct MyChar {
+  char c;
+};
 
 int main(int, char**) {
   test_library_hash_specializations_available();
@@ -31,6 +36,9 @@ int main(int, char**) {
 #endif
     test_hash_enabled_for_type<std::u16string>();
     test_hash_enabled_for_type<std::u32string>();
+    test_hash_enabled_for_type<std::basic_string<char, std::char_traits<char>, test_allocator<char>>>();
+    test_hash_disabled_for_type<std::basic_string<MyChar, std::char_traits<MyChar>, std::allocator<MyChar>>>();
+    test_hash_disabled_for_type<std::basic_string<char, constexpr_char_traits<char>, std::allocator<char>>>();
   }
 
   return 0;
