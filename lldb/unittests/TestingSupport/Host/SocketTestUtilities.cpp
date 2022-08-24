59656c0492224a2da590b913959630107e0a31f4
Pavel Labath <pavel@labath.sk>
2022-08-24 15:59:46 +0200

    [lldb] Make CommunicationTest compatible with windows
    Our (TCP) socket support is in a much better state than pipes. Use that
    for testing the Communication class.
    
    Move the CreateTCPConnectedSockets function
    (SocketTestUtilities.{h,cpp}) to a place where it can be used from
    Communication tests.

--- lldb/unittests/TestingSupport/Host/SocketTestUtilities.cpp
+++ lldb/unittests/TestingSupport/Host/SocketTestUtilities.cpp
@@ -6,7 +6,7 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "SocketTestUtilities.h"
+#include "TestingSupport/Host/SocketTestUtilities.h"
 #include "lldb/Host/Config.h"
 #include "lldb/Utility/StreamString.h"
 
