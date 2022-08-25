18385cffc50a6e549dcf3306cbe8a99e10a11fff
Chris Bieneman <chris.bieneman@me.com>
2022-08-25 18:25:20 -0500

    [HLSL] Add abs library function
    This change exposes the abs library function for HLSL scalar types. Abs
    is supported for all scalar, vector and matrix types. This patch only
    adds a subset of scalar type support.
    
    Fixes #57100 (https://llvm.org/pr57100)
    
    The full documentation of the HLSL abs function is available here:
    https://docs.microsoft.com/en-us/windows/win32/direct3dhlsl/dx-graphics-hlsl-abs
    
    Reviewed By: bogner
    
    Differential Revision: https://reviews.llvm.org/D131718

--- clang/lib/Headers/hlsl/hlsl_intrinsics.h
+++ clang/lib/Headers/hlsl/hlsl_intrinsics.h
@@ -12,4 +12,13 @@
 __attribute__((clang_builtin_alias(__builtin_hlsl_wave_active_count_bits))) uint
 WaveActiveCountBits(bool bBit);
 
+__attribute__((clang_builtin_alias(__builtin_abs))) int abs(int In);
+__attribute__((clang_builtin_alias(__builtin_labs))) int64_t abs(int64_t In);
+__attribute__((clang_builtin_alias(__builtin_fabsf))) float abs(float In);
+__attribute__((clang_builtin_alias(__builtin_fabs))) double abs(double In);
+
+#ifdef __HLSL_ENABLE_16_BIT
+__attribute__((clang_builtin_alias(__builtin_fabsf16))) half abs(half In);
+#endif
+
 #endif //_HLSL_HLSL_INTRINSICS_H_
