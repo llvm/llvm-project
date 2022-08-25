1d6a4bf714e425f897d6478a78ebb00c5d8520c4
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2022-08-25 16:56:15 -0700

    [CIR][CodeGen][NFC] Add another interface for buildAlloca and track getASTAllocaAddressSpace()

--- clang/lib/CIR/CIRGenExpr.cpp
+++ clang/lib/CIR/CIRGenExpr.cpp
@@ -1101,7 +1101,7 @@ mlir::LogicalResult CIRGenFunction::buildIfOnBoolExpr(const Expr *cond,
 }
 
 mlir::Value CIRGenFunction::buildAlloca(StringRef name, InitStyle initStyle,
-                                        QualType ty, mlir::Location loc,
+                                        mlir::Type ty, mlir::Location loc,
                                         CharUnits alignment) {
   auto getAllocaInsertPositionOp =
       [&](mlir::Block **insertBlock) -> mlir::Operation * {
@@ -1117,9 +1117,7 @@ mlir::Value CIRGenFunction::buildAlloca(StringRef name, InitStyle initStyle,
     return &*lastAlloca;
   };
 
-  auto localVarTy = getCIRType(ty);
-  auto localVarPtrTy =
-      mlir::cir::PointerType::get(builder.getContext(), localVarTy);
+  auto localVarPtrTy = mlir::cir::PointerType::get(builder.getContext(), ty);
   auto alignIntAttr = CGM.getSize(alignment);
 
   mlir::Value addr;
@@ -1138,10 +1136,16 @@ mlir::Value CIRGenFunction::buildAlloca(StringRef name, InitStyle initStyle,
     }
 
     addr = builder.create<mlir::cir::AllocaOp>(loc, /*addr type*/ localVarPtrTy,
-                                               /*var type*/ localVarTy, name,
-                                               initStyle, alignIntAttr);
+                                               /*var type*/ ty, name, initStyle,
+                                               alignIntAttr);
   }
   return addr;
+}
+
+mlir::Value CIRGenFunction::buildAlloca(StringRef name, InitStyle initStyle,
+                                        QualType ty, mlir::Location loc,
+                                        CharUnits alignment) {
+  return buildAlloca(name, initStyle, getCIRType(ty), loc, alignment);
 }
 
 mlir::Value CIRGenFunction::buildLoadOfScalar(LValue lvalue,
