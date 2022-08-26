2f56be774359649649b5397b14e74ad6c43473ef
Nathan Lanza <nathan@lanza.io>
2022-08-26 15:17:17 -0700

    [CIR] Introduce NullAttr
    This is more-or-less just a typed version of UnitAttr. We previously
    used `UnitAttr` to represent the case of passing nullptr to ConstantOp,
    but the removal of the mlir::Type from mlir::Attribute broke this as
    ConstantOp's value requires a type.

--- mlir/lib/Dialect/CIR/IR/CIRDialect.cpp
+++ mlir/lib/Dialect/CIR/IR/CIRDialect.cpp
@@ -11,6 +11,7 @@
 //===----------------------------------------------------------------------===//
 
 #include "mlir/Dialect/CIR/IR/CIRDialect.h"
+#include "mlir/Dialect/CIR/IR/CIRAttrs.h"
 #include "mlir/Dialect/CIR/IR/CIRTypes.h"
 
 #include "mlir/Dialect/Func/IR/FuncOps.h"
@@ -34,6 +35,7 @@ using namespace mlir::cir;
 /// the point of registration of types and operations for the dialect.
 void cir::CIRDialect::initialize() {
   registerTypes();
+  registerAttributes();
   addOperations<
 #define GET_OP_LIST
 #include "mlir/Dialect/CIR/IR/CIROps.cpp.inc"
