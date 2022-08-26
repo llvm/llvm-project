cd3091a88f7c55c90d9b5fff372ce1cdfc71948d
Adrian Vogelsgesang <avogelsgesang@salesforce.com>
2022-08-26 06:00:43 -0700

    [LLDB] Do not dereference promise pointer in `coroutine_handle` pretty printer
    So far, the pretty printer for `std::coroutine_handle` internally
    dereferenced the contained frame pointer displayed the `promise`
    as a sub-value. As noticed in https://reviews.llvm.org/D132624
    by @labath, this can lead to an endless loop in lldb during printing
    if the coroutine frame pointers form a cycle.
    
    This commit breaks the cycle by exposing the `promise` as a pointer
    type instead of a value type. The depth to which the `frame variable`
    and the `expression` commands dereference those pointers can be
    controlled using the `--ptr-depth` argument.
    
    Differential Revision: https://reviews.llvm.org/D132815

--- lldb/source/Plugins/Language/CPlusPlus/Coroutines.h
+++ lldb/source/Plugins/Language/CPlusPlus/Coroutines.h
@@ -47,7 +47,9 @@ public:
   size_t GetIndexOfChildWithName(ConstString name) override;
 
 private:
-  lldb::ValueObjectSP m_frame_ptr_sp;
+  lldb::ValueObjectSP m_resume_ptr_sp;
+  lldb::ValueObjectSP m_destroy_ptr_sp;
+  lldb::ValueObjectSP m_promise_ptr_sp;
   std::unique_ptr<lldb_private::ClangASTImporter> m_ast_importer;
 };
 
