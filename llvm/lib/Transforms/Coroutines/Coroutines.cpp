e0acdb1c08e499a37993131023275b17c9761a0f
Chuanqi Xu <yedeng.yd@linux.alibaba.com>
2022-08-26 13:49:00 +0800

    [Coroutines] Store the index for final suspend point if there is unwind coro end
    Closing https://github.com/llvm/llvm-project/issues/57339
    
    The root cause for this issue is an pre-mature optimization to eliminate
    the index for the final suspend point since we feel like we can judge
    if a coroutine is suspended at the final suspend by if resume_fn_addr is
    null. However this is not true if the coroutine exists via an exception
    in promise.unhandled_exception(). According to
    [dcl.fct.def.coroutine]p14:
    
    > If the evaluation of the expression promise.unhandled_exception()
    > exits via an exception, the coroutine is considered suspended at the
    > final suspend point.
    
    But from the perspective of the implementation, we can't set the coro
    index to the final suspend point directly since it breaks the states.
    
    To fix the issue, we block the optimization if we find there is any
    unwind coro end, which indicates that it is possible that the coroutine
    exists via an exception from promise.unhandled_exception().
    
    Test Plan: folly

--- llvm/lib/Transforms/Coroutines/Coroutines.cpp
+++ llvm/lib/Transforms/Coroutines/Coroutines.cpp
@@ -171,6 +171,7 @@ static CoroSaveInst *createCoroSave(CoroBeginInst *CoroBegin,
 // Collect "interesting" coroutine intrinsics.
 void coro::Shape::buildFrom(Function &F) {
   bool HasFinalSuspend = false;
+  bool HasUnwindCoroEnd = false;
   size_t FinalSuspendIndex = 0;
   clear(*this);
   SmallVector<CoroFrameInst *, 8> CoroFrames;
@@ -242,6 +243,10 @@ void coro::Shape::buildFrom(Function &F) {
         if (auto *AsyncEnd = dyn_cast<CoroAsyncEndInst>(II)) {
           AsyncEnd->checkWellFormed();
         }
+
+        if (CoroEnds.back()->isUnwind())
+          HasUnwindCoroEnd = true;
+
         if (CoroEnds.back()->isFallthrough() && isa<CoroEndInst>(II)) {
           // Make sure that the fallthrough coro.end is the first element in the
           // CoroEnds vector.
@@ -290,6 +295,7 @@ void coro::Shape::buildFrom(Function &F) {
     auto SwitchId = cast<CoroIdInst>(Id);
     this->ABI = coro::ABI::Switch;
     this->SwitchLowering.HasFinalSuspend = HasFinalSuspend;
+    this->SwitchLowering.HasUnwindCoroEnd = HasUnwindCoroEnd;
     this->SwitchLowering.ResumeSwitch = nullptr;
     this->SwitchLowering.PromiseAlloca = SwitchId->getPromise();
     this->SwitchLowering.ResumeEntryBlock = nullptr;
