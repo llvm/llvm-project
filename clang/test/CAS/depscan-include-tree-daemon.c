84e48b141bd1c6fa62d69f770d0256e589a88932
Argyrios Kyrtzidis <kyrtzidis@apple.com>
2022-08-26 13:41:07 -0700

    [test/CAS/depscan-include-tree.c] Split the daemon checking part to a separate test file
    rdar://98722407

---
+++ clang/test/CAS/depscan-include-tree-daemon.c
@@ -0,0 +1,26 @@
+// REQUIRES: system-darwin, clang-cc1daemon
+// RUN: rm -rf %t
+// RUN: split-file %s %t
+
+// RUN: %clang -cc1depscan -o %t/inline.rsp -fdepscan=inline -fdepscan-include-tree -cc1-args -cc1 -triple x86_64-apple-macos11.0 \
+// RUN:     -fsyntax-only %t/t.c -I %t/includes -isysroot %S/Inputs/SDK -fcas-path %t/cas -DSOME_MACRO -dependency-file %t/inline.d -MT deps
+
+// RUN: %clang -cc1depscand -start %{clang-daemon-dir}/depscan-include-tree-daemon -fcas-path %t/cas -faction-cache-path %t/cache
+// RUN: %clang -cc1depscan -o %t/daemon.rsp -fdepscan=daemon -fdepscan-include-tree -cc1-args -cc1 -triple x86_64-apple-macos11.0 \
+// RUN:     -fsyntax-only %t/t.c -I %t/includes -isysroot %S/Inputs/SDK -fcas-path %t/cas -DSOME_MACRO -dependency-file %t/daemon.d -MT deps
+// RUN: %clang -cc1depscand -shutdown %{clang-daemon-dir}/depscan-include-tree-daemon
+
+// RUN: diff -u %t/inline.rsp %t/daemon.rsp
+// RUN: diff -u %t/inline.d %t/daemon.d
+
+//--- t.c
+#include "t.h"
+
+int test(struct S *s) {
+  return s->x;
+}
+
+//--- includes/t.h
+struct S {
+  int x;
+};
