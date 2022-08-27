da34a3e8fa2abd074fc516dabef20c97a07a2c7f
Lang Hames <lhames@gmail.com>
2022-08-26 17:06:56 -0700

    [ORC-RT] Remove __orc_rt::apply_tuple.
    This utility was a substitute for std::apply, which is now available.

--- compiler-rt/lib/orc/stl_extras.h
+++ compiler-rt/lib/orc/stl_extras.h
@@ -18,29 +18,6 @@
 
 namespace __orc_rt {
 
-namespace detail {
-
-template <typename F, typename Tuple, std::size_t... I>
-decltype(auto) apply_tuple_impl(F &&f, Tuple &&t, std::index_sequence<I...>) {
-  return std::forward<F>(f)(std::get<I>(std::forward<Tuple>(t))...);
-}
-
-} // end namespace detail
-
-/// Given an input tuple (a1, a2, ..., an), pass the arguments of the
-/// tuple variadically to f as if by calling f(a1, a2, ..., an) and
-/// return the result.
-///
-/// FIXME: Switch to std::apply once we can use c++17.
-template <typename F, typename Tuple>
-decltype(auto) apply_tuple(F &&f, Tuple &&t) {
-  using Indices = std::make_index_sequence<
-      std::tuple_size<typename std::decay<Tuple>::type>::value>;
-
-  return detail::apply_tuple_impl(std::forward<F>(f), std::forward<Tuple>(t),
-                                  Indices{});
-}
-
 /// Substitute for std::identity.
 /// Switch to std::identity once we can use c++20.
 template <class Ty> struct identity {
