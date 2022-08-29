d1ad21995f1ab46ce3c0fe981e2feb3cf6ce8d5c
Kazu Hirata <kazu@google.com>
2022-08-28 17:35:08 -0700

    [Support] Remove greatestCommonDivisor and GreatestCommonDivisor64 (NFC)
    This patch removes greatestCommonDivisor and GreatestCommonDivisor64
    as I've migrated all the uses to std::gcd.

--- llvm/include/llvm/Support/MathExtras.h
+++ llvm/include/llvm/Support/MathExtras.h
@@ -595,21 +595,6 @@ inline unsigned Log2_64_Ceil(uint64_t Value) {
   return 64 - countLeadingZeros(Value - 1);
 }
 
-/// Return the greatest common divisor of the values using Euclid's algorithm.
-template <typename T>
-inline T greatestCommonDivisor(T A, T B) {
-  while (B) {
-    T Tmp = B;
-    B = A % B;
-    A = Tmp;
-  }
-  return A;
-}
-
-inline uint64_t GreatestCommonDivisor64(uint64_t A, uint64_t B) {
-  return greatestCommonDivisor<uint64_t>(A, B);
-}
-
 /// This function takes a 64-bit integer and returns the bit equivalent double.
 inline double BitsToDouble(uint64_t Bits) {
   static_assert(sizeof(uint64_t) == sizeof(double), "Unexpected type sizes");
