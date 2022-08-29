c7c36c2af8588082ad700cd736ab88e8599ddb3f
Fangrui Song <i@maskray.me>
2022-08-28 23:13:46 -0700

    [MC][test] Test FreeBSD after D131162

--- llvm/test/MC/ELF/ifunc-gnu.s
+++ llvm/test/MC/ELF/ifunc-gnu.s
@@ -1,5 +1,5 @@
-# REQUIRES: x86-registered-target
-# RUN: llvm-mc -filetype=obj -triple=x86_64-pc-linux-gnu %s -o -| llvm-readelf -hs - | FileCheck %s
+# RUN: llvm-mc -filetype=obj -triple=x86_64-pc-linux-gnu %s | llvm-readelf -hs - | FileCheck %s --check-prefixes=CHECK,GNU
+# RUN: llvm-mc -filetype=obj -triple=x86_64-pc-freebsd %s | llvm-readelf -hs - | FileCheck %s --check-prefixes=CHECK,FREEBSD
 
 .text
 
@@ -16,7 +16,9 @@ foo_resolver:
 .set   foo,foo_resolver
 
 ## ELFOSABI_NONE is changed to ELFOSABI_GNU. Other OSABI values are unchanged.
-# CHECK:      OS/ABI: UNIX - GNU
+# GNU:        OS/ABI: UNIX - GNU
+# FREEBSD:    OS/ABI: UNIX - FreeBSD
+
 # CHECK:      FUNC    LOCAL  DEFAULT    2 foo_impl
 # CHECK-NEXT: FUNC    LOCAL  DEFAULT    2 foo_resolver
 # CHECK-NEXT: IFUNC   LOCAL  DEFAULT    2 foo
