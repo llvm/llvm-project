16f282502c50ff33108b93a7733616a0b82b00eb
Doru Bercea <doru.bercea@amd.com>
2022-08-29 21:36:07 +0000

    Revert SPMD exclusion for target regions without parallels in OpenMP Opt.
    Change-Id: Ie08a6e8ce025d2ac1c0bbc74ff875ed9f2f9c34d

--- llvm/lib/Transforms/IPO/OpenMPOpt.cpp
+++ llvm/lib/Transforms/IPO/OpenMPOpt.cpp
@@ -3346,9 +3346,6 @@ struct AAKernelInfoFunction : AAKernelInfo {
   }
 
   bool changeToSPMDMode(Attributor &A, ChangeStatus &Changed) {
-    if (!mayContainParallelRegion())
-      return false;
-
     auto &OMPInfoCache = static_cast<OMPInformationCache &>(A.getInfoCache());
 
     if (!SPMDCompatibilityTracker.isAssumed()) {
