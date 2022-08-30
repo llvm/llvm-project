c55b41d5199d2394dd6cdb8f52180d8b81d809d4
Krzysztof Drewniak <Krzysztof.Drewniak@amd.com>
2022-08-30 17:12:10 +0000

    [mlir][AMDGPU] Define amdgpu.mfma operator
    The amdgpu.mfma operator is a wrapper around the Matrix Fused Multiply
    Add (MFMA) instructions on some AMD GPUs (the CDNA-based MI-* cards).
    
    This interface allows for selecting the operation to be performed by
    specifying the dimensions of the multiplication to be performed and
    any additional attributes (such as whether to use reduced-precision
    floating-point math) that are needed to select the relevant mfma
    instruction and set its parameters.
    
    Reviewed By: ThomasRaoux, nirvedhmeshram
    
    Differential Revision: https://reviews.llvm.org/D132956

--- mlir/lib/Conversion/AMDGPUToROCDL/AMDGPUToROCDL.cpp
+++ mlir/lib/Conversion/AMDGPUToROCDL/AMDGPUToROCDL.cpp
@@ -11,8 +11,11 @@
 #include "mlir/Conversion/LLVMCommon/ConversionTarget.h"
 #include "mlir/Conversion/LLVMCommon/Pattern.h"
 #include "mlir/Dialect/AMDGPU/AMDGPUDialect.h"
+#include "mlir/Dialect/LLVMIR/LLVMDialect.h"
 #include "mlir/Dialect/LLVMIR/ROCDLDialect.h"
 #include "mlir/Pass/Pass.h"
+
+#include "llvm/ADT/STLExtras.h"
 
 namespace mlir {
 #define GEN_PASS_DEF_CONVERTAMDGPUTOROCDL
@@ -25,7 +28,7 @@ using namespace mlir::amdgpu;
 static Value createI32Constant(ConversionPatternRewriter &rewriter,
                                Location loc, int32_t value) {
   Type llvmI32 = rewriter.getI32Type();
-  return rewriter.create<LLVM::ConstantOp>(loc, llvmI32, value);
+  return rewriter.createOrFold<LLVM::ConstantOp>(loc, llvmI32, value);
 }
 
 namespace {
@@ -123,7 +126,8 @@ struct RawBufferOpLowering : public ConvertOpToLLVMPattern<GpuOp> {
     MemRefDescriptor memrefDescriptor(memref);
     Type llvmI64 = this->typeConverter->convertType(rewriter.getI64Type());
     Type llvm2xI32 = this->typeConverter->convertType(VectorType::get(2, i32));
-    Value c32I64 = rewriter.create<LLVM::ConstantOp>(loc, llvmI64, 32);
+    Value c32I64 = rewriter.create<LLVM::ConstantOp>(
+        loc, llvmI64, rewriter.getI64IntegerAttr(32));
 
     Value resource = rewriter.create<LLVM::UndefOp>(loc, llvm4xI32);
 
@@ -273,6 +277,173 @@ struct LDSBarrierOpLowering : public ConvertOpToLLVMPattern<LDSBarrierOp> {
     return success();
   }
 };
+} // namespace
+
+/// If `input` is a vector of bytes, concatentate those bytes in little-endian
+/// order to form a single integer of size 8 * [vector length]. This works
+/// around a wart in the AMDGPU intrinsics where operations that logically take
+/// vectors of bytes instead integers. Since we do not want to expose this
+/// implementation detail to MLIR, we correct for it here.
+static Value mfmaConcatIfNeeded(ConversionPatternRewriter &rewriter,
+                                Location loc, Value input) {
+  Type inputType = input.getType();
+  if (auto vectorType = inputType.dyn_cast<VectorType>()) {
+    if (!vectorType.getElementType().isInteger(8))
+      return input;
+    int64_t numBytes = vectorType.getNumElements();
+    Type destType = rewriter.getIntegerType(numBytes * 8);
+    Value result = rewriter.createOrFold<LLVM::ConstantOp>(
+        loc, destType, rewriter.getIntegerAttr(destType, 0));
+    for (int64_t i = 0; i < numBytes; ++i) {
+      Value idxConst = createI32Constant(rewriter, loc, i);
+      Value element =
+          rewriter.create<LLVM::ExtractElementOp>(loc, input, idxConst);
+      Value extended = rewriter.create<LLVM::ZExtOp>(loc, destType, element);
+      Value shiftConst = rewriter.createOrFold<LLVM::ConstantOp>(
+          loc, destType, rewriter.getIntegerAttr(destType, i * 8));
+      Value shifted = rewriter.create<LLVM::ShlOp>(loc, extended, shiftConst);
+      result = rewriter.create<LLVM::OrOp>(loc, result, shifted);
+    }
+    return result;
+  }
+  return input;
+}
+
+/// Return the `rocdl` intrinsic corresponding to a MFMA operation `mfma`
+/// if one exists. This includes checking to ensure the intrinsic is supported
+/// on the architecture you are compiling for.
+static Optional<StringRef> mfmaOpToIntrinsic(MFMAOp mfma, Chipset chipset) {
+  uint32_t m = mfma.getM(), n = mfma.getN(), k = mfma.getK(),
+           b = mfma.getBlocks();
+  Type sourceElem = mfma.getSourceA().getType();
+  if (auto sourceType = sourceElem.dyn_cast<VectorType>())
+    sourceElem = sourceType.getElementType();
+  Type destElem = mfma.getDestC().getType();
+  if (auto destType = destElem.dyn_cast<VectorType>())
+    destElem = destType.getElementType();
+
+  if (sourceElem.isF32() && destElem.isF32()) {
+    if (mfma.getReducePrecision() && chipset.minorVersion >= 0x40) {
+      if (m == 32 && n == 32 && k == 4 && b == 1)
+        return ROCDL::mfma_f32_32x32x4_xf32::getOperationName();
+      if (m == 16 && n == 16 && k == 8 && b == 1)
+        return ROCDL::mfma_f32_16x16x8_xf32::getOperationName();
+    }
+    if (m == 32 && n == 32 && k == 1 && b == 2)
+      return ROCDL::mfma_f32_32x32x1f32::getOperationName();
+    if (m == 16 && n == 16 && k == 1 && b == 4)
+      return ROCDL::mfma_f32_16x16x1f32::getOperationName();
+    if (m == 4 && n == 4 && k == 1 && b == 16)
+      return ROCDL::mfma_f32_4x4x1f32::getOperationName();
+    if (m == 32 && n == 32 && k == 2 && b == 1)
+      return ROCDL::mfma_f32_32x32x2f32::getOperationName();
+    if (m == 16 && n == 16 && k == 4 && b == 1)
+      return ROCDL::mfma_f32_16x16x4f32::getOperationName();
+  }
+
+  if (sourceElem.isF16() && destElem.isF32()) {
+    if (m == 32 && n == 32 && k == 4 && b == 2)
+      return ROCDL::mfma_f32_32x32x4f16::getOperationName();
+    if (m == 16 && n == 16 && k == 4 && b == 4)
+      return ROCDL::mfma_f32_16x16x4f16::getOperationName();
+    if (m == 4 && n == 4 && k == 4 && b == 16)
+      return ROCDL::mfma_f32_4x4x4f16::getOperationName();
+    if (m == 32 && n == 32 && k == 8 && b == 1)
+      return ROCDL::mfma_f32_32x32x8f16::getOperationName();
+    if (m == 16 && n == 16 && k == 16 && b == 1)
+      return ROCDL::mfma_f32_16x16x16f16::getOperationName();
+  }
+
+  if (sourceElem.isBF16() && destElem.isF32() && chipset.minorVersion >= 0x0a) {
+    if (m == 32 && n == 32 && k == 4 && b == 2)
+      return ROCDL::mfma_f32_32x32x4bf16_1k::getOperationName();
+    if (m == 16 && n == 16 && k == 4 && b == 4)
+      return ROCDL::mfma_f32_16x16x4bf16_1k::getOperationName();
+    if (m == 4 && n == 4 && k == 4 && b == 16)
+      return ROCDL::mfma_f32_4x4x4bf16_1k::getOperationName();
+    if (m == 32 && n == 32 && k == 8 && b == 1)
+      return ROCDL::mfma_f32_32x32x8bf16_1k::getOperationName();
+    if (m == 16 && n == 16 && k == 16 && b == 1)
+      return ROCDL::mfma_f32_16x16x16bf16_1k::getOperationName();
+  }
+
+  if (sourceElem.isBF16() && destElem.isF32()) {
+    if (m == 32 && n == 32 && k == 2 && b == 2)
+      return ROCDL::mfma_f32_32x32x2bf16::getOperationName();
+    if (m == 16 && n == 16 && k == 2 && b == 4)
+      return ROCDL::mfma_f32_16x16x2bf16::getOperationName();
+    if (m == 4 && n == 4 && k == 2 && b == 16)
+      return ROCDL::mfma_f32_4x4x2bf16::getOperationName();
+    if (m == 32 && n == 32 && k == 4 && b == 1)
+      return ROCDL::mfma_f32_32x32x4bf16::getOperationName();
+    if (m == 16 && n == 16 && k == 8 && b == 1)
+      return ROCDL::mfma_f32_16x16x8bf16::getOperationName();
+  }
+
+  if (sourceElem.isa<IntegerType>() && destElem.isInteger(32)) {
+    if (m == 32 && n == 32 && k == 4 && b == 2)
+      return ROCDL::mfma_i32_32x32x4i8::getOperationName();
+    if (m == 16 && n == 16 && k == 4 && b == 4)
+      return ROCDL::mfma_i32_16x16x4i8::getOperationName();
+    if (m == 4 && n == 4 && k == 4 && b == 16)
+      return ROCDL::mfma_i32_4x4x4i8::getOperationName();
+    if (m == 32 && n == 32 && k == 8 && b == 1)
+      return ROCDL::mfma_i32_32x32x8i8::getOperationName();
+    if (m == 16 && n == 16 && k == 16 && b == 1)
+      return ROCDL::mfma_i32_16x16x16i8::getOperationName();
+    if (m == 32 && n == 32 && k == 16 && b == 1 && chipset.minorVersion >= 0x40)
+      return ROCDL::mfma_i32_32x32x16_i8::getOperationName();
+    if (m == 16 && n == 16 && k == 32 && b == 1 && chipset.minorVersion >= 0x40)
+      return ROCDL::mfma_i32_16x16x32_i8::getOperationName();
+  }
+
+  if (sourceElem.isF64() && destElem.isF64() && chipset.minorVersion >= 0x0a) {
+    if (m == 16 && n == 16 && k == 4 && b == 1)
+      return ROCDL::mfma_f64_16x16x4f64::getOperationName();
+    if (m == 4 && n == 4 && k == 4 && b == 4)
+      return ROCDL::mfma_f64_4x4x4f64::getOperationName();
+  }
+  return None;
+}
+
+namespace {
+struct MFMAOpLowering : public ConvertOpToLLVMPattern<MFMAOp> {
+  MFMAOpLowering(LLVMTypeConverter &converter, Chipset chipset)
+      : ConvertOpToLLVMPattern<MFMAOp>(converter), chipset(chipset) {}
+
+  Chipset chipset;
+
+  LogicalResult
+  matchAndRewrite(MFMAOp op, MFMAOpAdaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+    Location loc = op.getLoc();
+    Type outType = typeConverter->convertType(op.getDestD().getType());
+
+    if (chipset.majorVersion != 9 || chipset.minorVersion < 0x08)
+      return op->emitOpError("MFMA only supported on gfx908+");
+    uint32_t getBlgpField = static_cast<uint32_t>(op.getBlgp());
+    if (op.getNegateA() || op.getNegateB() || op.getNegateC()) {
+      if (chipset.minorVersion < 0x40)
+        return op.emitOpError("negation unsupported on older than gfx840");
+      getBlgpField |=
+          op.getNegateA() | (op.getNegateB() << 1) | (op.getNegateC() << 2);
+    }
+    Optional<StringRef> maybeIntrinsic = mfmaOpToIntrinsic(op, chipset);
+    if (!maybeIntrinsic.has_value())
+      return op.emitOpError("no intrinsic matching MFMA size on given chipset");
+    OperationState loweredOp(loc, *maybeIntrinsic);
+    loweredOp.addTypes(outType);
+    loweredOp.addOperands(
+        {mfmaConcatIfNeeded(rewriter, loc, adaptor.getSourceA()),
+         mfmaConcatIfNeeded(rewriter, loc, adaptor.getSourceB()),
+         adaptor.getDestC(), createI32Constant(rewriter, loc, op.getCbsz()),
+         createI32Constant(rewriter, loc, op.getAbid()),
+         createI32Constant(rewriter, loc, getBlgpField)});
+    Operation *lowered = rewriter.create(loweredOp);
+    rewriter.replaceOp(op, lowered->getResults());
+    return success();
+  }
+};
 
 struct ConvertAMDGPUToROCDLPass
     : public impl::ConvertAMDGPUToROCDLBase<ConvertAMDGPUToROCDLPass> {
@@ -290,6 +461,7 @@ struct ConvertAMDGPUToROCDLPass
     LLVMTypeConverter converter(ctx);
     populateAMDGPUToROCDLConversionPatterns(converter, patterns, *maybeChipset);
     LLVMConversionTarget target(getContext());
+    target.addIllegalDialect<::mlir::amdgpu::AMDGPUDialect>();
     target.addLegalDialect<::mlir::LLVM::LLVMDialect>();
     target.addLegalDialect<::mlir::ROCDL::ROCDLDialect>();
     if (failed(applyPartialConversion(getOperation(), target,
@@ -306,8 +478,8 @@ void mlir::populateAMDGPUToROCDLConversionPatterns(LLVMTypeConverter &converter,
   patterns.add<
       RawBufferOpLowering<RawBufferLoadOp, ROCDL::RawBufferLoadOp>,
       RawBufferOpLowering<RawBufferStoreOp, ROCDL::RawBufferStoreOp>,
-      RawBufferOpLowering<RawBufferAtomicFaddOp, ROCDL::RawBufferAtomicFAddOp>>(
-      converter, chipset);
+      RawBufferOpLowering<RawBufferAtomicFaddOp, ROCDL::RawBufferAtomicFAddOp>,
+      MFMAOpLowering>(converter, chipset);
 }
 
 std::unique_ptr<Pass> mlir::createConvertAMDGPUToROCDLPass() {
