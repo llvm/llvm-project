17717036b3da7c98d45939e00e30be2b5e6944dd
Ying Yi <maggieyi666@gmail.com>
2022-08-31 20:16:22 +0100

    Re-add the REQUIRES line to fix a failed build on builder llvm-clang-win-x-aarch64.

--- clang/test/Driver/ps4-ps5-header-search.c
+++ clang/test/Driver/ps4-ps5-header-search.c
@@ -1,3 +1,5 @@
+// REQUIRES: x86-registered-target
+
 /// PS4 and PS5 use the same SDK layout, so use the same tree for both.
 // RUN: env SCE_ORBIS_SDK_DIR=%S/Inputs/scei-ps4_tree %clang -target x86_64-scei-ps4 -E -v %s 2>&1 | FileCheck %s --check-prefix=ENVPS4
 // RUN: env SCE_PROSPERO_SDK_DIR=%S/Inputs/scei-ps4_tree %clang -target x86_64-sie-ps5 -E -v %s 2>&1 | FileCheck %s --check-prefix=ENVPS4
