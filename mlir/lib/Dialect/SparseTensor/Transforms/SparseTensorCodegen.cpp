8c79543080311db1bcba8a4aafb716d2fee3ec3a
Aart Bik <ajcbik@google.com>
2022-08-31 13:46:24 -0700

    [mlir][sparse] sparse storage scheme type conversion
    This builds a compound type for the buffers required for the sparse storage scheme defined by the MLIR sparse tensor types. The use of a tuple allows for a simple 1:1 type conversion. A subsequent pass can expand this tuple into its component with an isolated 1:N type conversion.
    
    Reviewed By: Peiming
    
    Differential Revision: https://reviews.llvm.org/D133050

--- mlir/lib/Dialect/SparseTensor/Transforms/SparseTensorCodegen.cpp
+++ mlir/lib/Dialect/SparseTensor/Transforms/SparseTensorCodegen.cpp
@@ -33,14 +33,72 @@ namespace {
 // Helper methods.
 //===----------------------------------------------------------------------===//
 
-/// Maps each sparse tensor type to the appropriate buffer.
-static Optional<Type> convertSparseTensorTypes(Type type) {
-  if (getSparseTensorEncoding(type) != nullptr) {
-    // TODO: this is just a dummy rule to get the ball rolling....
-    RankedTensorType rTp = type.cast<RankedTensorType>();
-    return MemRefType::get({ShapedType::kDynamicSize}, rTp.getElementType());
+/// Maps a sparse tensor type to the appropriate compounded buffers.
+static Optional<Type> convertSparseTensorType(Type type) {
+  auto enc = getSparseTensorEncoding(type);
+  if (!enc)
+    return llvm::None;
+  // Construct the basic types.
+  auto context = type.getContext();
+  unsigned idxWidth = enc.getIndexBitWidth();
+  unsigned ptrWidth = enc.getPointerBitWidth();
+  RankedTensorType rType = type.cast<RankedTensorType>();
+  Type indexType = IndexType::get(context);
+  Type idxType = idxWidth ? IntegerType::get(context, idxWidth) : indexType;
+  Type ptrType = ptrWidth ? IntegerType::get(context, ptrWidth) : indexType;
+  Type eltType = rType.getElementType();
+  //
+  // Sparse tensor storage for rank-dimensional tensor is organized as a
+  // single compound type with the following fields:
+  //
+  // struct {
+  //   memref<rank x index> dimSize  ; size in each dimension
+  //   ; per-dimension d:
+  //   ;  if dense:
+  //        <nothing>
+  //   ;  if compresed:
+  //        memref<? x idx>  indices-d   ; indices for sparse dim d
+  //        memref<? x ptr>  pointers-d  ; pointers for sparse dim d
+  //   ;  if singleton:
+  //        memref<? x idx>  indices-d   ; indices for singleton dim d
+  //   memref<? x eltType> values    ; values
+  // };
+  //
+  // TODO: fill in the ? when statically known
+  //
+  // TODO: emit dimSizes when not needed (e.g. all-dense)
+  //
+  unsigned rank = rType.getShape().size();
+  SmallVector<Type, 8> fields;
+  fields.push_back(MemRefType::get({rank}, indexType));
+  for (unsigned r = 0; r < rank; r++) {
+    // Dimension level types apply in order to the reordered dimension.
+    // As a result, the compound type can be constructed directly in the given
+    // order. Clients of this type know what field is what from the sparse
+    // tensor type.
+    switch (enc.getDimLevelType()[r]) {
+    case SparseTensorEncodingAttr::DimLevelType::Dense:
+      break;
+    case SparseTensorEncodingAttr::DimLevelType::Compressed:
+    case SparseTensorEncodingAttr::DimLevelType::CompressedNu:
+    case SparseTensorEncodingAttr::DimLevelType::CompressedNo:
+    case SparseTensorEncodingAttr::DimLevelType::CompressedNuNo:
+      fields.push_back(MemRefType::get({ShapedType::kDynamicSize}, idxType));
+      fields.push_back(MemRefType::get({ShapedType::kDynamicSize}, ptrType));
+      break;
+    case SparseTensorEncodingAttr::DimLevelType::Singleton:
+    case SparseTensorEncodingAttr::DimLevelType::SingletonNu:
+    case SparseTensorEncodingAttr::DimLevelType::SingletonNo:
+    case SparseTensorEncodingAttr::DimLevelType::SingletonNuNo:
+      fields.push_back(MemRefType::get({ShapedType::kDynamicSize}, idxType));
+      break;
+    }
   }
-  return llvm::None;
+  fields.push_back(MemRefType::get({ShapedType::kDynamicSize}, eltType));
+  // Sparse tensor storage (temporarily) lives in a tuple. This allows a
+  // simple 1:1 type conversion during codegen. A subsequent pass uses
+  // a 1:N type conversion to expand the tuple into its fields.
+  return TupleType::get(context, fields);
 }
 
 //===----------------------------------------------------------------------===//
@@ -67,7 +125,7 @@ public:
 
 mlir::SparseTensorTypeToBufferConverter::SparseTensorTypeToBufferConverter() {
   addConversion([](Type type) { return type; });
-  addConversion(convertSparseTensorTypes);
+  addConversion(convertSparseTensorType);
 }
 
 //===----------------------------------------------------------------------===//
