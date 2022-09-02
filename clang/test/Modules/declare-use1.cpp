b484256f59850e702df4d4532c5f31f478879bb9
Richard Smith <richard@metafoo.co.uk>
2022-09-02 14:47:05 -0700

    Add driver test for -fmodule-name and -fmodule-map-file use without -fmodules.

--- clang/test/Modules/declare-use1.cpp
+++ clang/test/Modules/declare-use1.cpp
@@ -1,5 +1,8 @@
 // RUN: rm -rf %t
-// RUN: %clang_cc1 -fimplicit-module-maps -fmodules-cache-path=%t -fmodules-decluse -fmodule-name=XG -I %S/Inputs/declare-use %s -verify
+// RUN: %clang_cc1 -fimplicit-module-maps -fmodules-decluse -fmodule-name=XG -I %S/Inputs/declare-use %s -verify
+//
+// Check these flags get properly passed through the driver even when -fmodules is disabled.
+// RUN: %clang -fimplicit-module-maps -fmodules-decluse -fmodule-name=XG -I %S/Inputs/declare-use -c -o /dev/null -Xclang -verify %s
 
 #include "g.h"
 #include "e.h"
