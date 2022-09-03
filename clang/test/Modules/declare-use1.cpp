220c2cb0a88f0f1cc97ea657018abbffb8697a73
Richard Smith <richard@metafoo.co.uk>
2022-09-02 18:43:27 -0700

    Attempt to make AIX bot happier.

--- clang/test/Modules/declare-use1.cpp
+++ clang/test/Modules/declare-use1.cpp
@@ -2,7 +2,7 @@
 // RUN: %clang_cc1 -fimplicit-module-maps -fmodules-decluse -fmodule-name=XG -I %S/Inputs/declare-use %s -verify
 //
 // Check these flags get properly passed through the driver even when -fmodules is disabled.
-// RUN: %clang -fimplicit-module-maps -fmodules-decluse -fmodule-name=XG -I %S/Inputs/declare-use -c -o /dev/null -Xclang -verify %s
+// RUN: %clang -fimplicit-module-maps -fmodules-decluse -fmodule-name=XG -I %S/Inputs/declare-use -fsyntax-only -Xclang -verify %s
 
 #include "g.h"
 #include "e.h"
