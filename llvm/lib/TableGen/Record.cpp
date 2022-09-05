9491efe1f1864f032e047bffe16d250b88d6f777
James Y Knight <jyknight@google.com>
2022-09-05 12:10:21 -0400

    [TableGen][CodeEmitterGen] Allow local names for sub-operands in a operand list.
    These names can then be matched by name against 'bits' fields in a
    record, to populate an instruction's encoding.
    
    This does _not_ yet change DecoderEmitter to allow by-name matching of
    sub-operands. Unlike the encoder, the decoder already defaulted to not
    supporting positional matching, and backends had workarounds in place
    for the missing decoding support.
    
    Additionally, use this new capability to allow the ARM and AArch64
    backends not to require any positional operand matching.
    
    Differential Revision: https://reviews.llvm.org/D131003

--- llvm/lib/TableGen/Record.cpp
+++ llvm/lib/TableGen/Record.cpp
@@ -2238,6 +2238,7 @@ static void ProfileDagInit(FoldingSetNodeID &ID, Init *V, StringInit *VN,
 
 DagInit *DagInit::get(Init *V, StringInit *VN, ArrayRef<Init *> ArgRange,
                       ArrayRef<StringInit *> NameRange) {
+  assert(ArgRange.size() == NameRange.size());
   FoldingSetNodeID ID;
   ProfileDagInit(ID, V, VN, ArgRange, NameRange);
 
