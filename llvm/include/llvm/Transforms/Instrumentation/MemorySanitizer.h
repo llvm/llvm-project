2344d7625e9bdc54b7a5394ba0accd00dcb00ef3
Vitaly Buka <vitalybuka@google.com>
2022-09-05 21:28:02 -0700

    [msan] Convert Msan to ModulePass
    MemorySanitizerPass function pass violatied requirement 4 of function
    pass to do not insert globals. Msan nees to insert globals for origin
    tracking, and paramereters tracking.
    
    https://llvm.org/docs/WritingAnLLVMPass.html#the-functionpass-class
    
    Reviewed By: kstoimenov, fmayer
    
    Differential Revision: https://reviews.llvm.org/D133336

--- llvm/include/llvm/Transforms/Instrumentation/MemorySanitizer.h
+++ llvm/include/llvm/Transforms/Instrumentation/MemorySanitizer.h
@@ -34,24 +34,6 @@ struct MemorySanitizerOptions {
   bool EagerChecks;
 };
 
-/// A function pass for msan instrumentation.
-///
-/// Instruments functions to detect unitialized reads. This function pass
-/// inserts calls to runtime library functions. If the functions aren't declared
-/// yet, the pass inserts the declarations. Otherwise the existing globals are
-/// used.
-struct MemorySanitizerPass : public PassInfoMixin<MemorySanitizerPass> {
-  MemorySanitizerPass(MemorySanitizerOptions Options) : Options(Options) {}
-
-  PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM);
-  void printPipeline(raw_ostream &OS,
-                     function_ref<StringRef(StringRef)> MapClassName2PassName);
-  static bool isRequired() { return true; }
-
-private:
-  MemorySanitizerOptions Options;
-};
-
 /// A module pass for msan instrumentation.
 ///
 /// Instruments functions to detect unitialized reads. This function pass
@@ -62,6 +44,8 @@ struct ModuleMemorySanitizerPass : public PassInfoMixin<ModuleMemorySanitizerPas
   ModuleMemorySanitizerPass(MemorySanitizerOptions Options) : Options(Options) {}
 
   PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM);
+  void printPipeline(raw_ostream &OS,
+                     function_ref<StringRef(StringRef)> MapClassName2PassName);
   static bool isRequired() { return true; }
 
 private:
