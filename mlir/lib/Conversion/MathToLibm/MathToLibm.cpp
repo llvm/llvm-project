46b199ac49e59a316b2cb4900dc84b9d72a04b5d
jacquesguan <Jianjian.Guan@streamcomputing.com>
2022-09-06 16:06:48 +0800

    [mlir][Math] Add TruncOp.
    This patch adds TruncOp for Math, it returns the operand rounded to the nearest integer not larger in magnitude than the operand. And this patch also adds the correspond llvm intrinsic op.
    
    Reviewed By: Mogball
    
    Differential Revision: https://reviews.llvm.org/D133342

--- mlir/lib/Conversion/MathToLibm/MathToLibm.cpp
+++ mlir/lib/Conversion/MathToLibm/MathToLibm.cpp
@@ -155,19 +155,19 @@ ScalarOpToLibmCall<Op>::matchAndRewrite(Op op,
 void mlir::populateMathToLibmConversionPatterns(
     RewritePatternSet &patterns, PatternBenefit benefit,
     llvm::Optional<PatternBenefit> log1pBenefit) {
-  patterns
-      .add<VecOpToScalarOp<math::Atan2Op>, VecOpToScalarOp<math::ExpM1Op>,
-           VecOpToScalarOp<math::TanhOp>, VecOpToScalarOp<math::CosOp>,
-           VecOpToScalarOp<math::SinOp>, VecOpToScalarOp<math::ErfOp>,
-           VecOpToScalarOp<math::RoundEvenOp>, VecOpToScalarOp<math::RoundOp>,
-           VecOpToScalarOp<math::AtanOp>, VecOpToScalarOp<math::TanOp>>(
-          patterns.getContext(), benefit);
+  patterns.add<VecOpToScalarOp<math::Atan2Op>, VecOpToScalarOp<math::ExpM1Op>,
+               VecOpToScalarOp<math::TanhOp>, VecOpToScalarOp<math::CosOp>,
+               VecOpToScalarOp<math::SinOp>, VecOpToScalarOp<math::ErfOp>,
+               VecOpToScalarOp<math::RoundEvenOp>,
+               VecOpToScalarOp<math::RoundOp>, VecOpToScalarOp<math::AtanOp>,
+               VecOpToScalarOp<math::TanOp>, VecOpToScalarOp<math::TruncOp>>(
+      patterns.getContext(), benefit);
   patterns.add<PromoteOpToF32<math::Atan2Op>, PromoteOpToF32<math::ExpM1Op>,
                PromoteOpToF32<math::TanhOp>, PromoteOpToF32<math::CosOp>,
                PromoteOpToF32<math::SinOp>, PromoteOpToF32<math::ErfOp>,
                PromoteOpToF32<math::RoundEvenOp>, PromoteOpToF32<math::RoundOp>,
-               PromoteOpToF32<math::AtanOp>, PromoteOpToF32<math::TanOp>>(
-      patterns.getContext(), benefit);
+               PromoteOpToF32<math::AtanOp>, PromoteOpToF32<math::TanOp>,
+               PromoteOpToF32<math::TruncOp>>(patterns.getContext(), benefit);
   patterns.add<ScalarOpToLibmCall<math::AtanOp>>(patterns.getContext(), "atanf",
                                                  "atan", benefit);
   patterns.add<ScalarOpToLibmCall<math::Atan2Op>>(patterns.getContext(),
@@ -194,6 +194,8 @@ void mlir::populateMathToLibmConversionPatterns(
                                                   "floorf", "floor", benefit);
   patterns.add<ScalarOpToLibmCall<math::CeilOp>>(patterns.getContext(), "ceilf",
                                                  "ceil", benefit);
+  patterns.add<ScalarOpToLibmCall<math::TruncOp>>(patterns.getContext(),
+                                                  "truncf", "trunc", benefit);
 }
 
 namespace {
--- mlir/lib/Dialect/Math/IR/MathOps.cpp
+++ mlir/lib/Dialect/Math/IR/MathOps.cpp
@@ -457,6 +457,24 @@ OpFoldResult math::RoundOp::fold(ArrayRef<Attribute> operands) {
       });
 }
 
+//===----------------------------------------------------------------------===//
+// TruncOp folder
+//===----------------------------------------------------------------------===//
+
+OpFoldResult math::TruncOp::fold(ArrayRef<Attribute> operands) {
+  return constFoldUnaryOpConditional<FloatAttr>(
+      operands, [](const APFloat &a) -> Optional<APFloat> {
+        switch (a.getSizeInBits(a.getSemantics())) {
+        case 64:
+          return APFloat(trunc(a.convertToDouble()));
+        case 32:
+          return APFloat(truncf(a.convertToFloat()));
+        default:
+          return {};
+        }
+      });
+}
+
 /// Materialize an integer or floating point constant.
 Operation *math::MathDialect::materializeConstant(OpBuilder &builder,
                                                   Attribute value, Type type,
