e5a8f50ab160c56a316e79f46e787afa15c1cbcc
Joe Loser <joeloser@fastmail.com>
2022-09-06 18:01:39 -0600

    [mlir] Use std::size instead of llvm::array_lengthof
    LLVM contains a helpful function for getting the size of a C-style
    array: `llvm::array_lengthof`. This is useful prior to C++17, but not as
    helpful for C++17 or later: `std::size` already has support for C-style
    arrays.
    
    Change call sites to use `std::size` instead.
    
    Differential Revision: https://reviews.llvm.org/D133387

--- mlir/lib/Dialect/SPIRV/IR/SPIRVEnums.cpp
+++ mlir/lib/Dialect/SPIRV/IR/SPIRVEnums.cpp
@@ -19,6 +19,8 @@
 #include "llvm/ADT/StringRef.h"
 #include "llvm/ADT/StringSwitch.h"
 
+#include <iterator>
+
 using namespace mlir;
 
 // Pull in all enum utility function definitions
@@ -59,16 +61,16 @@ ArrayRef<spirv::Extension> spirv::getImpliedExtensions(spirv::Version version) {
   case Version::V_1_3: {
     // The following manual ArrayRef constructor call is to satisfy GCC 5.
     static const Extension exts[] = {V_1_3_IMPLIED_EXTS};
-    return ArrayRef<spirv::Extension>(exts, llvm::array_lengthof(exts));
+    return ArrayRef<spirv::Extension>(exts, std::size(exts));
   }
   case Version::V_1_4: {
     static const Extension exts[] = {V_1_3_IMPLIED_EXTS, V_1_4_IMPLIED_EXTS};
-    return ArrayRef<spirv::Extension>(exts, llvm::array_lengthof(exts));
+    return ArrayRef<spirv::Extension>(exts, std::size(exts));
   }
   case Version::V_1_5: {
     static const Extension exts[] = {V_1_3_IMPLIED_EXTS, V_1_4_IMPLIED_EXTS,
                                      V_1_5_IMPLIED_EXTS};
-    return ArrayRef<spirv::Extension>(exts, llvm::array_lengthof(exts));
+    return ArrayRef<spirv::Extension>(exts, std::size(exts));
   }
   }
 
