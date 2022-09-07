215c9fa4deac9ec6b4e504843830551f03b60620
Siva Chandra Reddy <sivachandra@google.com>
2022-09-07 23:04:09 +0000

    [libc] Re-enable functions from signal.h and re-enable abort.
    They were disabled because we were including linux/signal.h from our
    signal.h. Linux's signal.h is not designed to be included from user
    programs as it causes a lot of non-standard name pollution. Also, it is
    not self-contained. This change defines types and macros relevant for
    signal related syscalls within libc's headers and removes inclusion of
    Linux headers.
    
    This patch enables the funtions only for x86_64. They will be enabled
    for aarch64 also in a follow up patch after testing.
    
    Reviewed By: abrachet, lntue
    
    Differential Revision: https://reviews.llvm.org/D134567

--- libc/src/signal/linux/raise.cpp
+++ libc/src/signal/linux/raise.cpp
@@ -7,19 +7,19 @@
 //===----------------------------------------------------------------------===//
 
 #include "src/signal/raise.h"
-#include "src/signal/linux/signal.h"
+#include "src/signal/linux/signal_utils.h"
 
 #include "src/__support/common.h"
 
 namespace __llvm_libc {
 
 LLVM_LIBC_FUNCTION(int, raise, (int sig)) {
-  __llvm_libc::Sigset sigset;
-  __llvm_libc::block_all_signals(sigset);
+  ::sigset_t sigset;
+  block_all_signals(sigset);
   long pid = __llvm_libc::syscall(SYS_getpid);
   long tid = __llvm_libc::syscall(SYS_gettid);
   int ret = __llvm_libc::syscall(SYS_tgkill, pid, tid, sig);
-  __llvm_libc::restore_signals(sigset);
+  restore_signals(sigset);
   return ret;
 }
 
