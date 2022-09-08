195cb7fb2f2448c551a6352d5a06ef41ef5b7c1a
Fangrui Song <i@maskray.me>
2022-09-07 22:02:26 -0700

    [NewPM] Switch -filter-passes from ClassName to pass-name
    NewPM -filter-passes (D86360) uses ClassName instead of pass-name as used in
    `-passes`, `-print-after`, etc. D87216 has added a mechanism to map
    ClassName to pass-name. Adopt it for -filter-passes.
    
    Reviewed By: aeubanks
    
    Differential Revision: https://reviews.llvm.org/D133263

--- llvm/include/llvm/IR/PrintPasses.h
+++ llvm/include/llvm/IR/PrintPasses.h
@@ -53,6 +53,7 @@ bool forcePrintModuleIR();
 
 // Return true if -filter-passes is empty or contains the pass name.
 bool isPassInPrintList(StringRef PassName);
+bool isFilterPassesEmpty();
 
 // Returns true if we should print the function.
 bool isFunctionInPrintList(StringRef FunctionName);
