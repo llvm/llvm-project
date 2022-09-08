2f23dc9127e6c0ef392f163cbaa58afcb7b61a09
Fangrui Song <i@maskray.me>
2022-09-08 13:49:07 -0700

    [AST/Interp] Fix left shift overflow bug in D64146
    Noticed by Dmitri Gribenko

--- clang/lib/AST/Interp/Descriptor.cpp
+++ clang/lib/AST/Interp/Descriptor.cpp
@@ -269,7 +269,7 @@ InitMap::T *InitMap::data() {
 
 bool InitMap::initialize(unsigned I) {
   unsigned Bucket = I / PER_FIELD;
-  unsigned Mask = 1ull << static_cast<uint64_t>(I % PER_FIELD);
+  T Mask = T(1) << (I % PER_FIELD);
   if (!(data()[Bucket] & Mask)) {
     data()[Bucket] |= Mask;
     UninitFields -= 1;
@@ -279,8 +279,7 @@ bool InitMap::initialize(unsigned I) {
 
 bool InitMap::isInitialized(unsigned I) {
   unsigned Bucket = I / PER_FIELD;
-  unsigned Mask = 1ull << static_cast<uint64_t>(I % PER_FIELD);
-  return data()[Bucket] & Mask;
+  return data()[Bucket] & (T(1) << (I % PER_FIELD));
 }
 
 InitMap *InitMap::allocate(unsigned N) {
