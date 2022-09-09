8761604f6d67e161d64d29c5dc9cc1096150b219
Ron Lieberman <ron.lieberman@amd.com>
2022-09-09 07:01:56 -0500

    DebugCompressionType  Z -> Zlib, per upstream
    Change-Id: I5bbf1d4eeef42de2f5f379ceb27e1fbdde2b9acf

--- amd/comgr/src/comgr-compiler.cpp
+++ amd/comgr/src/comgr-compiler.cpp
@@ -244,12 +244,12 @@ bool AssemblerInvocation::createFromArgs(AssemblerInvocation &Opts,
                                      OPT_compress_debug_sections_EQ)) {
     if (A->getOption().getID() == OPT_compress_debug_sections) {
       // TODO: be more clever about the compression type auto-detection
-      Opts.CompressDebugSections = llvm::DebugCompressionType::Z;
+      Opts.CompressDebugSections = llvm::DebugCompressionType::Zlib;
     } else {
       Opts.CompressDebugSections =
           llvm::StringSwitch<llvm::DebugCompressionType>(A->getValue())
               .Case("none", llvm::DebugCompressionType::None)
-              .Case("zlib", llvm::DebugCompressionType::Z)
+              .Case("zlib", llvm::DebugCompressionType::Zlib)
               .Default(llvm::DebugCompressionType::None);
     }
   }
