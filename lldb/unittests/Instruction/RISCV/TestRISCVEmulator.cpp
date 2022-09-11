502f0cb1bf40939f6d909ed8f94714b8c04c99a5
Emmmer <yjhdandan@163.com>
2022-09-11 16:36:12 +0800

    [LLDB][RISCV] Add RVM and RVA instruction support for EmulateInstructionRISCV
    Add:
    - RVM and RVA instructions sets.
    - corresponding unittests.
    
    Further work:
    - implement RVC, RVF, RVD, and RVV extension.
    
    Reviewed By: DavidSpickett
    
    Differential Revision: https://reviews.llvm.org/D133670

--- lldb/unittests/Instruction/RISCV/TestRISCVEmulator.cpp
+++ lldb/unittests/Instruction/RISCV/TestRISCVEmulator.cpp
@@ -24,11 +24,14 @@ using namespace lldb_private;
 
 struct RISCVEmulatorTester : public EmulateInstructionRISCV, testing::Test {
   RegisterInfoPOSIX_riscv64::GPR gpr;
+  uint8_t memory[1024] = {0};
 
   RISCVEmulatorTester()
       : EmulateInstructionRISCV(ArchSpec("riscv64-unknown-linux-gnu")) {
     EmulateInstruction::SetReadRegCallback(ReadRegisterCallback);
     EmulateInstruction::SetWriteRegCallback(WriteRegisterCallback);
+    EmulateInstruction::SetReadMemCallback(ReadMemoryCallback);
+    EmulateInstruction::SetWriteMemCallback(WriteMemoryCallback);
   }
 
   static bool ReadRegisterCallback(EmulateInstruction *instruction, void *baton,
@@ -53,6 +56,25 @@ struct RISCVEmulatorTester : public EmulateInstructionRISCV, testing::Test {
       tester->gpr.gpr[reg] = reg_value.GetAsUInt64();
     return true;
   }
+
+  static size_t ReadMemoryCallback(EmulateInstruction *instruction, void *baton,
+                                   const Context &context, lldb::addr_t addr,
+                                   void *dst, size_t length) {
+    RISCVEmulatorTester *tester = (RISCVEmulatorTester *)instruction;
+    assert(addr + length < sizeof(tester->memory));
+    memcpy(dst, tester->memory + addr, length);
+    return length;
+  };
+
+  static size_t WriteMemoryCallback(EmulateInstruction *instruction,
+                                    void *baton, const Context &context,
+                                    lldb::addr_t addr, const void *dst,
+                                    size_t length) {
+    RISCVEmulatorTester *tester = (RISCVEmulatorTester *)instruction;
+    assert(addr + length < sizeof(tester->memory));
+    memcpy(tester->memory + addr, dst, length);
+    return length;
+  };
 };
 
 TEST_F(RISCVEmulatorTester, testJAL) {
@@ -64,7 +86,7 @@ TEST_F(RISCVEmulatorTester, testJAL) {
   auto x1 = gpr.gpr[1];
 
   bool success = false;
-  auto pc = ReadPC(&success);
+  auto pc = ReadPC(success);
 
   ASSERT_TRUE(success);
   ASSERT_EQ(x1, old_pc + 4);
@@ -91,7 +113,7 @@ TEST_F(RISCVEmulatorTester, testJALR) {
   auto x1 = gpr.gpr[1];
 
   bool success = false;
-  auto pc = ReadPC(&success);
+  auto pc = ReadPC(success);
 
   ASSERT_TRUE(success);
   ASSERT_EQ(x1, old_pc + 4);
@@ -144,7 +166,7 @@ void testBranch(RISCVEmulatorTester *tester, EncoderB encoder, bool branched,
   uint32_t inst = encoder(1, 2, -256);
   ASSERT_TRUE(tester->DecodeAndExecute(inst, false));
   bool success = false;
-  auto pc = tester->ReadPC(&success);
+  auto pc = tester->ReadPC(success);
   ASSERT_TRUE(success);
   ASSERT_EQ(pc, old_pc + (branched ? (-256) : 0));
 }
@@ -161,6 +183,13 @@ void CheckRD(RISCVEmulatorTester *tester, uint64_t rd, uint64_t value) {
   ASSERT_EQ(tester->gpr.gpr[rd], value);
 }
 
+template <typename T>
+void CheckMem(RISCVEmulatorTester *tester, uint64_t addr, uint64_t value) {
+  bool success = false;
+  ASSERT_EQ(tester->ReadMem<T>(*tester, addr, &success), value);
+  ASSERT_TRUE(success);
+}
+
 using RS1 = uint64_t;
 using RS2 = uint64_t;
 using PC = uint64_t;
@@ -171,21 +200,62 @@ void TestInst(RISCVEmulatorTester *tester, uint64_t inst, bool has_rs2,
 
   lldb::addr_t old_pc = 0x114514;
   tester->WritePC(old_pc);
-  auto rd = DecodeRD(inst);
-  auto rs1 = DecodeRS1(inst);
-  auto rs2 = 0;
+  uint32_t rd = DecodeRD(inst);
+  uint32_t rs1 = DecodeRS1(inst);
+  uint32_t rs2 = 0;
+
+  uint64_t rs1_val = 0x19;
+  uint64_t rs2_val = 0x81;
+
   if (rs1)
-    tester->gpr.gpr[rs1] = 0x1919;
+    tester->gpr.gpr[rs1] = rs1_val;
 
   if (has_rs2) {
     rs2 = DecodeRS2(inst);
-    if (rs2)
-      tester->gpr.gpr[rs2] = 0x8181;
+    if (rs2) {
+      if (rs1 == rs2)
+        rs2_val = rs1_val;
+      tester->gpr.gpr[rs2] = rs2_val;
+    }
   }
 
   ASSERT_TRUE(tester->DecodeAndExecute(inst, false));
-  CheckRD(tester, rd,
-          rd_val(tester->gpr.gpr[rs1], rs2 ? tester->gpr.gpr[rs2] : 0, old_pc));
+  CheckRD(tester, rd, rd_val(rs1_val, rs2 ? rs2_val : 0, old_pc));
+}
+
+template <typename T>
+void TestAtomic(RISCVEmulatorTester *tester, uint64_t inst, T rs1_val,
+                T rs2_val, T rd_expected, T mem_expected) {
+  // Atomic inst must have rs1 and rs2
+
+  uint32_t rd = DecodeRD(inst);
+  uint32_t rs1 = DecodeRS1(inst);
+  uint32_t rs2 = DecodeRS2(inst);
+
+  // addr was stored in rs1
+  uint64_t atomic_addr = 0x100;
+
+  tester->gpr.gpr[rs1] = atomic_addr;
+  tester->gpr.gpr[rs2] = rs2_val;
+
+  // Write and check rs1_val in atomic_addr
+  ASSERT_TRUE(
+      tester->WriteMem<T>(*tester, atomic_addr, RegisterValue(rs1_val)));
+  CheckMem<T>(tester, atomic_addr, rs1_val);
+
+  ASSERT_TRUE(tester->DecodeAndExecute(inst, false));
+  CheckRD(tester, rd, rd_expected);
+  CheckMem<T>(tester, atomic_addr, mem_expected);
+}
+
+TEST_F(RISCVEmulatorTester, TestAtomicSequence) {
+  this->WritePC(0x0);
+  *(uint64_t *)this->memory = 0x100427af;        // lr.w	a5,(s0)
+  *(uint64_t *)(this->memory + 4) = 0x00079663;  // bnez	a5,12
+  *(uint64_t *)(this->memory + 8) = 0x1ce426af;  // sc.w.aq	a3,a4,(s0)
+  *(uint64_t *)(this->memory + 12) = 0xfe069ae3; // bnez	a3,-12
+  ASSERT_TRUE(this->DecodeAndExecute(*(uint32_t *)this->memory, false));
+  ASSERT_EQ(this->gpr.gpr[0], uint64_t(16));
 }
 
 // GEN_BRANCH_TEST(opcode, imm1, imm2, imm3):
@@ -208,18 +278,43 @@ struct TestData {
 TEST_F(RISCVEmulatorTester, TestDecodeAndExcute) {
 
   std::vector<TestData> tests = {
+      // RV32I & RV64I Tests
       {0x00010113, "ADDI", false, [](RS1 rs1, RS2, PC) { return rs1 + 0; }},
       {0x00023517, "AUIPC", false, [](RS1, RS2, PC pc) { return pc + 143360; }},
       {0x0006079b, "ADDIW", false, [](RS1 rs1, RS2, PC) { return rs1 + 0; }},
       {0x00110837, "LUI", false, [](RS1, RS2, PC pc) { return 1114112; }},
       {0x00147513, "ANDI", false, [](RS1 rs1, RS2, PC) { return rs1 & 1; }},
-      {0x00153513, "SLTIU", false, [](RS1 rs1, RS2, PC) { return rs1 != 0; }},
-      {0x00256513, "ORI", false, [](RS1 rs1, RS2, PC) { return rs1 | 1; }},
+      {0x00153513, "SLTIU", false, [](RS1 rs1, RS2, PC) { return 0; }},
+      {0x00256513, "ORI", false, [](RS1 rs1, RS2, PC) { return rs1 | 2; }},
       {0x00451a13, "SLLI", false, [](RS1 rs1, RS2, PC) { return rs1 << 4; }},
       {0x00455693, "SRLI", false, [](RS1 rs1, RS2, PC) { return rs1 >> 4; }},
       {0x00a035b3, "SLTU", true, [](RS1 rs1, RS2 rs2, PC) { return rs2 != 0; }},
       {0x00b50633, "ADD", true, [](RS1 rs1, RS2 rs2, PC) { return rs1 + rs2; }},
       {0x40d507b3, "SUB", true, [](RS1 rs1, RS2 rs2, PC) { return rs1 - rs2; }},
+
+      // RV32M & RV64M Tests
+      {0x02f787b3, "MUL", true, [](RS1 rs1, RS2 rs2, PC) { return rs1 * rs2; }},
+      {0x2F797B3, "MULH", true, [](RS1 rs1, RS2 rs2, PC) { return 0; }},
+      {0x2F7A7B3, "MULHSU", true,
+       [](RS1 rs1, RS2 rs2, PC) { return 0; }},
+      {0x2F7B7B3, "MULHU", true,
+       [](RS1 rs1, RS2 rs2, PC) { return 0; }},
+      {0x02f747b3, "DIV", true, [](RS1 rs1, RS2 rs2, PC) { return rs1 / rs2; }},
+      {0x02f757b3, "DIVU", true,
+       [](RS1 rs1, RS2 rs2, PC) { return rs1 / rs2; }},
+      {0x02f767b3, "REM", true, [](RS1 rs1, RS2 rs2, PC) { return rs1 % rs2; }},
+      {0x02f777b3, "REMU", true,
+       [](RS1 rs1, RS2 rs2, PC) { return rs1 % rs2; }},
+      {0x02f787bb, "MULW", true,
+       [](RS1 rs1, RS2 rs2, PC) { return rs1 * rs2; }},
+      {0x02f747bb, "DIVW", true,
+       [](RS1 rs1, RS2 rs2, PC) { return rs1 / rs2; }},
+      {0x02f757bb, "DIVUW", true,
+       [](RS1 rs1, RS2 rs2, PC) { return rs1 / rs2; }},
+      {0x02f767bb, "REMW", true,
+       [](RS1 rs1, RS2 rs2, PC) { return rs1 % rs2; }},
+      {0x02f777bb, "REMUW", true,
+       [](RS1 rs1, RS2 rs2, PC) { return rs1 % rs2; }},
   };
   for (auto i : tests) {
     const InstrPattern *pattern = this->Decode(i.inst);
@@ -228,4 +323,49 @@ TEST_F(RISCVEmulatorTester, TestDecodeAndExcute) {
     ASSERT_EQ(name, i.name);
     TestInst(this, i.inst, i.has_rs2, i.rd_val);
   }
+}
+
+TEST_F(RISCVEmulatorTester, TestAMOSWAP) {
+  TestAtomic<uint32_t>(this, 0x8F7282F, 0x1, 0x2, 0x1, 0x2);
+  TestAtomic<uint64_t>(this, 0x8F7382F, 0x1, 0x2, 0x1, 0x2);
+}
+
+TEST_F(RISCVEmulatorTester, TestAMOADD) {
+  TestAtomic<uint32_t>(this, 0xF7282F, 0x1, 0x2, 0x1, 0x3);
+  TestAtomic<uint64_t>(this, 0xF7382F, 0x1, 0x2, 0x1, 0x3);
+}
+
+TEST_F(RISCVEmulatorTester, TestAMOXOR) {
+  TestAtomic<uint32_t>(this, 0x20F7282F, 0x1, 0x2, 0x1, 0x3);
+  TestAtomic<uint32_t>(this, 0x20F7382F, 0x1, 0x2, 0x1, 0x3);
+}
+
+TEST_F(RISCVEmulatorTester, TestAMOAND) {
+  TestAtomic<uint32_t>(this, 0x60F7282F, 0x1, 0x2, 0x1, 0x0);
+  TestAtomic<uint64_t>(this, 0x60F7382F, 0x1, 0x2, 0x1, 0x0);
+}
+
+TEST_F(RISCVEmulatorTester, TestAMOOR) {
+  TestAtomic<uint32_t>(this, 0x40F7282F, 0x1, 0x2, 0x1, 0x3);
+  TestAtomic<uint32_t>(this, 0x40F7382F, 0x1, 0x2, 0x1, 0x3);
+}
+
+TEST_F(RISCVEmulatorTester, TestAMOMIN) {
+  TestAtomic<uint32_t>(this, 0x80F7282F, 0x1, 0x2, 0x1, 0x1);
+  TestAtomic<uint64_t>(this, 0x80F7382F, 0x1, 0x2, 0x1, 0x1);
+}
+
+TEST_F(RISCVEmulatorTester, TestAMOMAX) {
+  TestAtomic<uint32_t>(this, 0xA0F7282F, 0x1, 0x2, 0x1, 0x2);
+  TestAtomic<uint64_t>(this, 0xA0F7382F, 0x1, 0x2, 0x1, 0x2);
+}
+
+TEST_F(RISCVEmulatorTester, TestAMOMINU) {
+  TestAtomic<uint32_t>(this, 0xC0F7282F, 0x1, 0x2, 0x1, 0x1);
+  TestAtomic<uint64_t>(this, 0xC0F7382F, 0x1, 0x2, 0x1, 0x1);
+}
+
+TEST_F(RISCVEmulatorTester, TestAMOMAXU) {
+  TestAtomic<uint32_t>(this, 0xE0F7282F, 0x1, 0x2, 0x1, 0x2);
+  TestAtomic<uint64_t>(this, 0xE0F7382F, 0x1, 0x2, 0x1, 0x2);
 }
