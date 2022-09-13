24b96602cb4b92e49980e61d18b4b26078a54acf
Arthur Eubanks <aeubanks@google.com>
2022-09-13 14:01:15 -0700

    [OptBisect] Add flag to print IR when opt-bisect kicks in
    -opt-bisect-print-ir-path=foo will dump the IR to foo when opt-bisect-limit starts skipping passes.
    
    Currently we don't print the IR if the opt-bisect-limit is higher than the total number of times opt-bisect is called.
    
    This makes getting the IR right before a bad transform easier.
    
    Reviewed By: hans
    
    Differential Revision: https://reviews.llvm.org/D133809

--- llvm/include/llvm/Passes/StandardInstrumentations.h
+++ llvm/include/llvm/Passes/StandardInstrumentations.h
@@ -75,6 +75,8 @@ private:
 };
 
 class OptBisectInstrumentation {
+  bool HasWrittenIR = false;
+
 public:
   OptBisectInstrumentation() = default;
   void registerCallbacks(PassInstrumentationCallbacks &PIC);
