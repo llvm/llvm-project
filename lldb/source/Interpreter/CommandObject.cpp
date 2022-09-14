40de9e7ebe6e1bebc36b5e73c6fa9f495370a063
Jim Ingham <jingham@apple.com>
2022-09-14 12:45:26 -0700

    Revert "Revert "Be more careful to maintain quoting information when parsing commands.""
        This reverts commit ac1a13a3ee6d0a788621918f9eb7d07ee562c741.
    
    I had incorrectly removed one set of checks in the option handling in
    Options::ParseAlias because I couldn't see what it is for.  It was a
    bit obscure, but it handled the case where you pass "-something=other --"
    as the input_line, which caused the built-in "run" alias not to return
    the right value for IsDashDashCommand, causing TestHelp.py to fail.

--- lldb/source/Interpreter/CommandObject.cpp
+++ lldb/source/Interpreter/CommandObject.cpp
@@ -727,7 +727,7 @@ bool CommandObjectParsed::Execute(const char *args_string,
   }
   if (!handled) {
     for (auto entry : llvm::enumerate(cmd_args.entries())) {
-      if (!entry.value().ref().empty() && entry.value().ref().front() == '`') {
+      if (!entry.value().ref().empty() && entry.value().GetQuoteChar() == '`') {
         cmd_args.ReplaceArgumentAtIndex(
             entry.index(),
             m_interpreter.ProcessEmbeddedScriptCommands(entry.value().c_str()));
