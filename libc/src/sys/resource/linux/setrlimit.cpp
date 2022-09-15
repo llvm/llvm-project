8a1d5fc8c9be9aa3c39f37fd062e1574147b64ce
Siva Chandra Reddy <sivachandra@google.com>
2022-09-15 22:22:59 +0000

    [libc] Add implementation of POSIX setrlimit and getrlimit functions.
    Reviewed By: lntue
    
    Differential Revision: https://reviews.llvm.org/D134016

---
+++ libc/src/sys/resource/linux/setrlimit.cpp
@@ -0,0 +1,29 @@
+//===-- Linux implementation of setrlimit ---------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "src/sys/resource/setrlimit.h"
+
+#include "src/__support/OSUtil/syscall.h" // For internal syscall function.
+#include "src/__support/common.h"
+
+#include <errno.h>
+#include <sys/resource.h> // For struct rlimit_t
+#include <sys/syscall.h>  // For syscall numbers.
+
+namespace __llvm_libc {
+
+LLVM_LIBC_FUNCTION(int, setrlimit, (int res, const struct rlimit_t *limits)) {
+  long ret = __llvm_libc::syscall(SYS_prlimit64, 0, res, limits, nullptr);
+  if (ret < 0) {
+    errno = -ret;
+    return -1;
+  }
+  return 0;
+}
+
+} // namespace __llvm_libc
