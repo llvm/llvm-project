48a9bc619dd4a105809feab868fea50938a66386
Xiang Li <python3kgae@outlook.com>
2022-09-17 00:11:44 -0700

    [NFC} update CodeGenHLSL tests to use cc1 instead of driver-mode

--- clang/test/CodeGenHLSL/half.hlsl
+++ clang/test/CodeGenHLSL/half.hlsl
@@ -1,5 +1,11 @@
-// RUN: %clang_dxc -Tlib_6_7 -fcgl  -Fo - %s | FileCheck %s --check-prefix=FLOAT
-// RUN: %clang_dxc -Tlib_6_7 -enable-16bit-types -fcgl  -Fo - %s | FileCheck %s --check-prefix=HALF
+// RUN: %clang_cc1 -std=hlsl2021 -finclude-default-header -x hlsl -triple \
+// RUN:   dxil-pc-shadermodel6.3-library %s -fnative-half-type \
+// RUN:   -emit-llvm -disable-llvm-passes -o - | FileCheck %s --check-prefix=HALF
+
+// RUN: %clang_cc1 -std=hlsl2021 -finclude-default-header -x hlsl -triple \
+// RUN:   dxil-pc-shadermodel6.3-library %s \
+// RUN:   -emit-llvm -disable-llvm-passes -o - | FileCheck %s --check-prefix=FLOAT
+
 
 // Make sure use float when not enable-16bit-types.
 // FLOAT:define {{.*}}float @"?foo@@YA$halff@$halff@0@Z"(float{{[^,]+}}, float{{[^,)]+}})
