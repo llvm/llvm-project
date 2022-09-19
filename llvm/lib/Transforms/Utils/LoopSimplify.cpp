0ec9dc2823ad1bf0a8148454572df1083ab07f5c
Max Kazantsev <mkazantsev@azul.com>
2022-09-19 14:05:42 +0700

    [SCEV][NFC] Remove unused parameter from forgetLoopDispositions
    Let's be honest about it, we don't drop loop dispositions for
    particular loops. Remove the parameter that misleadingly makes
    it apparent that we do.

--- llvm/lib/Transforms/Utils/LoopSimplify.cpp
+++ llvm/lib/Transforms/Utils/LoopSimplify.cpp
@@ -660,7 +660,7 @@ ReprocessLoop:
         // The loop disposition of all SCEV expressions that depend on any
         // hoisted values have also changed.
         if (SE)
-          SE->forgetLoopDispositions(L);
+          SE->forgetLoopDispositions();
       }
       if (!AllInvariant) continue;
 
