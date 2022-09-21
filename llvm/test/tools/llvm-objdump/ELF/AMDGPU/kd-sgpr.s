66d7c3b4c4d70ee4155cfbc9146569c74cdc79ff
Scott Linder <Scott.Linder@amd.com>
2022-09-21 18:48:42 +0000

    Revert "[NFC][AMDGPU] Refactor AMDGPUDisassembler"
    This reverts commit 08360c30ae23e4047bb2d20446a1e86fb40587a7.

--- llvm/test/tools/llvm-objdump/ELF/AMDGPU/kd-sgpr.s
+++ llvm/test/tools/llvm-objdump/ELF/AMDGPU/kd-sgpr.s
@@ -1,52 +1,26 @@
 ;; Test disassembly for GRANULATED_WAVEFRONT_SGPR_COUNT in the kernel descriptor.
 
-; RUN: split-file %s %t && cd %t
+; RUN: split-file %s %t.dir
+
+; RUN: llvm-mc %t.dir/1.s --triple=amdgcn-amd-amdhsa -mcpu=gfx908 -mattr=-xnack -filetype=obj -o %t1
+; RUN: llvm-objdump --disassemble-symbols=my_kernel_1.kd %t1 | tail -n +7 \
+; RUN: | llvm-mc --triple=amdgcn-amd-amdhsa -mcpu=gfx908 -mattr=-xnack -filetype=obj -o %t1-re-assemble
+; RUN: diff %t1 %t1-re-assemble
+
+; RUN: llvm-mc %t.dir/2.s --triple=amdgcn-amd-amdhsa -mcpu=gfx908 -mattr=-xnack -filetype=obj -o %t2
+; RUN: llvm-objdump --disassemble-symbols=my_kernel_2.kd %t2 | tail -n +7 \
+; RUN: | llvm-mc --triple=amdgcn-amd-amdhsa -mcpu=gfx908 -mattr=-xnack -filetype=obj -o %t2-re-assemble
+; RUN: diff %t2 %t2-re-assemble
+
+; RUN: llvm-mc %t.dir/3.s --triple=amdgcn-amd-amdhsa -mcpu=gfx908 -mattr=-xnack -filetype=obj -o %t3
+; RUN: llvm-objdump --disassemble-symbols=my_kernel_3.kd %t3 | tail -n +7 \
+; RUN: | llvm-mc --triple=amdgcn-amd-amdhsa -mcpu=gfx908 -mattr=-xnack -filetype=obj -o %t3-re-assemble
+; RUN: diff %t3 %t3-re-assemble
+
 
 ;--- 1.s
 ;; Only set next_free_sgpr.
-; RUN: %assemble -mcpu=gfx908 <1.s >1.o
-; RUN: %disassemble_kd 1.o | %tee_kd 1-disasm.s | FileCheck 1.s
-; RUN: %assemble -mcpu=gfx908 <1-disasm.s >1-disasm.o
-; RUN: diff 1.o 1-disasm.o
-; CHECK: .amdhsa_kernel kernel
-; CHECK-NEXT: .amdhsa_group_segment_fixed_size 0
-; CHECK-NEXT: .amdhsa_private_segment_fixed_size 0
-; CHECK-NEXT: .amdhsa_kernarg_size 0
-; CHECK-NEXT: .amdhsa_next_free_vgpr 4
-; CHECK-NEXT: .amdhsa_reserve_vcc 0
-; CHECK-NEXT: .amdhsa_reserve_flat_scratch 0
-; CHECK-NEXT: .amdhsa_reserve_xnack_mask 0
-; CHECK-NEXT: .amdhsa_next_free_sgpr 48
-; CHECK-NEXT: .amdhsa_float_round_mode_32 0
-; CHECK-NEXT: .amdhsa_float_round_mode_16_64 0
-; CHECK-NEXT: .amdhsa_float_denorm_mode_32 0
-; CHECK-NEXT: .amdhsa_float_denorm_mode_16_64 3
-; CHECK-NEXT: .amdhsa_dx10_clamp 1
-; CHECK-NEXT: .amdhsa_ieee_mode 1
-; CHECK-NEXT: .amdhsa_fp16_overflow 0
-; CHECK-NEXT: .amdhsa_system_sgpr_private_segment_wavefront_offset 0
-; CHECK-NEXT: .amdhsa_system_sgpr_workgroup_id_x 1
-; CHECK-NEXT: .amdhsa_system_sgpr_workgroup_id_y 0
-; CHECK-NEXT: .amdhsa_system_sgpr_workgroup_id_z 0
-; CHECK-NEXT: .amdhsa_system_sgpr_workgroup_info 0
-; CHECK-NEXT: .amdhsa_system_vgpr_workitem_id 0
-; CHECK-NEXT: .amdhsa_exception_fp_ieee_invalid_op 0
-; CHECK-NEXT: .amdhsa_exception_fp_denorm_src 0
-; CHECK-NEXT: .amdhsa_exception_fp_ieee_div_zero 0
-; CHECK-NEXT: .amdhsa_exception_fp_ieee_overflow 0
-; CHECK-NEXT: .amdhsa_exception_fp_ieee_underflow 0
-; CHECK-NEXT: .amdhsa_exception_fp_ieee_inexact 0
-; CHECK-NEXT: .amdhsa_exception_int_div_zero 0
-; CHECK-NEXT: .amdhsa_user_sgpr_private_segment_buffer 0
-; CHECK-NEXT: .amdhsa_user_sgpr_dispatch_ptr 0
-; CHECK-NEXT: .amdhsa_user_sgpr_queue_ptr 0
-; CHECK-NEXT: .amdhsa_user_sgpr_kernarg_segment_ptr 0
-; CHECK-NEXT: .amdhsa_user_sgpr_dispatch_id 0
-; CHECK-NEXT: .amdhsa_user_sgpr_flat_scratch_init 0
-; CHECK-NEXT: .amdhsa_user_sgpr_private_segment_size 0
-; CHECK-NEXT: .amdhsa_uses_dynamic_stack 0
-; CHECK-NEXT: .end_amdhsa_kernel
-.amdhsa_kernel kernel
+.amdhsa_kernel my_kernel_1
   .amdhsa_next_free_vgpr 0
   .amdhsa_next_free_sgpr 42
   .amdhsa_reserve_flat_scratch 0
@@ -56,49 +30,7 @@
 
 ;--- 2.s
 ;; Only set other directives.
-; RUN: %assemble -mcpu=gfx908 <2.s >2.o
-; RUN: %disassemble_kd 2.o | %tee_kd 2-disasm.s | FileCheck 2.s
-; RUN: %assemble -mcpu=gfx908 <2-disasm.s >2-disasm.o
-; RUN: diff 2.o 2-disasm.o
-; CHECK: .amdhsa_kernel kernel
-; CHECK-NEXT: .amdhsa_group_segment_fixed_size 0
-; CHECK-NEXT: .amdhsa_private_segment_fixed_size 0
-; CHECK-NEXT: .amdhsa_kernarg_size 0
-; CHECK-NEXT: .amdhsa_next_free_vgpr 4
-; CHECK-NEXT: .amdhsa_reserve_vcc 0
-; CHECK-NEXT: .amdhsa_reserve_flat_scratch 0
-; CHECK-NEXT: .amdhsa_reserve_xnack_mask 0
-; CHECK-NEXT: .amdhsa_next_free_sgpr 8
-; CHECK-NEXT: .amdhsa_float_round_mode_32 0
-; CHECK-NEXT: .amdhsa_float_round_mode_16_64 0
-; CHECK-NEXT: .amdhsa_float_denorm_mode_32 0
-; CHECK-NEXT: .amdhsa_float_denorm_mode_16_64 3
-; CHECK-NEXT: .amdhsa_dx10_clamp 1
-; CHECK-NEXT: .amdhsa_ieee_mode 1
-; CHECK-NEXT: .amdhsa_fp16_overflow 0
-; CHECK-NEXT: .amdhsa_system_sgpr_private_segment_wavefront_offset 0
-; CHECK-NEXT: .amdhsa_system_sgpr_workgroup_id_x 1
-; CHECK-NEXT: .amdhsa_system_sgpr_workgroup_id_y 0
-; CHECK-NEXT: .amdhsa_system_sgpr_workgroup_id_z 0
-; CHECK-NEXT: .amdhsa_system_sgpr_workgroup_info 0
-; CHECK-NEXT: .amdhsa_system_vgpr_workitem_id 0
-; CHECK-NEXT: .amdhsa_exception_fp_ieee_invalid_op 0
-; CHECK-NEXT: .amdhsa_exception_fp_denorm_src 0
-; CHECK-NEXT: .amdhsa_exception_fp_ieee_div_zero 0
-; CHECK-NEXT: .amdhsa_exception_fp_ieee_overflow 0
-; CHECK-NEXT: .amdhsa_exception_fp_ieee_underflow 0
-; CHECK-NEXT: .amdhsa_exception_fp_ieee_inexact 0
-; CHECK-NEXT: .amdhsa_exception_int_div_zero 0
-; CHECK-NEXT: .amdhsa_user_sgpr_private_segment_buffer 0
-; CHECK-NEXT: .amdhsa_user_sgpr_dispatch_ptr 0
-; CHECK-NEXT: .amdhsa_user_sgpr_queue_ptr 0
-; CHECK-NEXT: .amdhsa_user_sgpr_kernarg_segment_ptr 0
-; CHECK-NEXT: .amdhsa_user_sgpr_dispatch_id 0
-; CHECK-NEXT: .amdhsa_user_sgpr_flat_scratch_init 0
-; CHECK-NEXT: .amdhsa_user_sgpr_private_segment_size 0
-; CHECK-NEXT: .amdhsa_uses_dynamic_stack 0
-; CHECK-NEXT: .end_amdhsa_kernel
-.amdhsa_kernel kernel
+.amdhsa_kernel my_kernel_2
   .amdhsa_next_free_vgpr 0
   .amdhsa_next_free_sgpr 0
   .amdhsa_reserve_flat_scratch 1
@@ -108,49 +40,7 @@
 
 ;--- 3.s
 ;; Set all affecting directives.
-; RUN: %assemble -mcpu=gfx908 <3.s >3.o
-; RUN: %disassemble_kd 3.o | %tee_kd 3-disasm.s | FileCheck 3.s
-; RUN: %assemble -mcpu=gfx908 <3-disasm.s >3-disasm.o
-; RUN: diff 3.o 3-disasm.o
-; CHECK: .amdhsa_kernel kernel
-; CHECK-NEXT: .amdhsa_group_segment_fixed_size 0
-; CHECK-NEXT: .amdhsa_private_segment_fixed_size 0
-; CHECK-NEXT: .amdhsa_kernarg_size 0
-; CHECK-NEXT: .amdhsa_next_free_vgpr 4
-; CHECK-NEXT: .amdhsa_reserve_vcc 0
-; CHECK-NEXT: .amdhsa_reserve_flat_scratch 0
-; CHECK-NEXT: .amdhsa_reserve_xnack_mask 0
-; CHECK-NEXT: .amdhsa_next_free_sgpr 48
-; CHECK-NEXT: .amdhsa_float_round_mode_32 0
-; CHECK-NEXT: .amdhsa_float_round_mode_16_64 0
-; CHECK-NEXT: .amdhsa_float_denorm_mode_32 0
-; CHECK-NEXT: .amdhsa_float_denorm_mode_16_64 3
-; CHECK-NEXT: .amdhsa_dx10_clamp 1
-; CHECK-NEXT: .amdhsa_ieee_mode 1
-; CHECK-NEXT: .amdhsa_fp16_overflow 0
-; CHECK-NEXT: .amdhsa_system_sgpr_private_segment_wavefront_offset 0
-; CHECK-NEXT: .amdhsa_system_sgpr_workgroup_id_x 1
-; CHECK-NEXT: .amdhsa_system_sgpr_workgroup_id_y 0
-; CHECK-NEXT: .amdhsa_system_sgpr_workgroup_id_z 0
-; CHECK-NEXT: .amdhsa_system_sgpr_workgroup_info 0
-; CHECK-NEXT: .amdhsa_system_vgpr_workitem_id 0
-; CHECK-NEXT: .amdhsa_exception_fp_ieee_invalid_op 0
-; CHECK-NEXT: .amdhsa_exception_fp_denorm_src 0
-; CHECK-NEXT: .amdhsa_exception_fp_ieee_div_zero 0
-; CHECK-NEXT: .amdhsa_exception_fp_ieee_overflow 0
-; CHECK-NEXT: .amdhsa_exception_fp_ieee_underflow 0
-; CHECK-NEXT: .amdhsa_exception_fp_ieee_inexact 0
-; CHECK-NEXT: .amdhsa_exception_int_div_zero 0
-; CHECK-NEXT: .amdhsa_user_sgpr_private_segment_buffer 0
-; CHECK-NEXT: .amdhsa_user_sgpr_dispatch_ptr 0
-; CHECK-NEXT: .amdhsa_user_sgpr_queue_ptr 0
-; CHECK-NEXT: .amdhsa_user_sgpr_kernarg_segment_ptr 0
-; CHECK-NEXT: .amdhsa_user_sgpr_dispatch_id 0
-; CHECK-NEXT: .amdhsa_user_sgpr_flat_scratch_init 0
-; CHECK-NEXT: .amdhsa_user_sgpr_private_segment_size 0
-; CHECK-NEXT: .amdhsa_uses_dynamic_stack 0
-; CHECK-NEXT: .end_amdhsa_kernel
-.amdhsa_kernel kernel
+.amdhsa_kernel my_kernel_3
   .amdhsa_next_free_vgpr 0
   .amdhsa_next_free_sgpr 35
   .amdhsa_reserve_flat_scratch 1
