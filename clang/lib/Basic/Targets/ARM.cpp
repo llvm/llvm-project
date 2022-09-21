39ac388b0df12c273b307c8a11ba4457ac053083
Fangrui Song <i@maskray.me>
2022-09-21 11:50:15 -0700

    [ARM] Check target feature support for __builtin_arm_crc*
    `__builtin_arm_crc*` requires the target feature crc which is available on armv8
    and above. Calling the fuctions for armv7 leads to a SelectionDAG crash.
    
    ```
    % clang -c --target=armv7-unknown-linux-gnueabi -c a.c
    fatal error: error in backend: Cannot select: intrinsic %llvm.arm.crc32b
    PLEASE submit a bug report to ...
    ```
    
    Add `TARGET_BUILTIN` and define required features for these builtins to
    report an error in `CodeGenFunction::checkTargetFeatures`. The problem is quite widespread.
    I will add `TARGET_BUILTIN` for more builtins later.
    
    Fix https://github.com/llvm/llvm-project/issues/57802
    
    Differential Revision: https://reviews.llvm.org/D134127

--- clang/lib/Basic/Targets/ARM.cpp
+++ clang/lib/Basic/Targets/ARM.cpp
@@ -979,6 +979,8 @@ const Builtin::Info ARMTargetInfo::BuiltinInfo[] = {
   {#ID, TYPE, ATTRS, nullptr, LANG, nullptr},
 #define LIBBUILTIN(ID, TYPE, ATTRS, HEADER)                                    \
   {#ID, TYPE, ATTRS, HEADER, ALL_LANGUAGES, nullptr},
+#define TARGET_BUILTIN(ID, TYPE, ATTRS, FEATURE)                               \
+  {#ID, TYPE, ATTRS, nullptr, ALL_LANGUAGES, FEATURE},
 #define TARGET_HEADER_BUILTIN(ID, TYPE, ATTRS, HEADER, LANGS, FEATURE)         \
   {#ID, TYPE, ATTRS, HEADER, LANGS, FEATURE},
 #include "clang/Basic/BuiltinsARM.def"
