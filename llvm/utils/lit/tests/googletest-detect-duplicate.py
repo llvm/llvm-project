f39e57a2f879bef59cf2edecd5e6c8e1698724a8
Yuanfang Chen <yuanfang.chen@sony.com>
2022-09-22 16:42:47 -0700

    [lit] follow-up cdf7083e4c981, check stdout and stderr separately
    The error output is cached so it could be out of order with stdout in
    some bots.

--- llvm/utils/lit/tests/googletest-detect-duplicate.py
+++ llvm/utils/lit/tests/googletest-detect-duplicate.py
@@ -2,9 +2,11 @@
 # when it happens.
 
 # RUN: %{lit} -v --order=random %{inputs}/googletest-detect-duplicate \
-# RUN:                          %{inputs}/googletest-detect-duplicate 2>&1 | FileCheck %s
+# RUN:                          %{inputs}/googletest-detect-duplicate 2> %t.warn | FileCheck %s
+# RUN: FileCheck --check-prefix=CHECK-WARN < %t.warn %s
 
-# CHECK: warning: Skip adding
+# CHECK-WARN: warning: Skip adding
+
 # CHECK: -- Testing:
 # CHECK: PASS: googletest-detect-duplicate :: [[PATH:[Dd]ummy[Ss]ub[Dd]ir/]][[FILE:OneTest\.py]]/0
 # CHECK: Passed{{ *}}: 1
