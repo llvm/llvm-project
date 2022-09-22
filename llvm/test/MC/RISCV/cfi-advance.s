0be5e2912abeeffa3a6907f5961a38026153930e
LiaoChunyu <chunyu@iscas.ac.cn>
2022-09-22 11:53:24 +0800

    [RelocationResolver] Add R_RISCV_SET{16,32}
    Reviewed By: MaskRay
    
    Differential Revision: https://reviews.llvm.org/D134408

--- llvm/test/MC/RISCV/cfi-advance.s
+++ llvm/test/MC/RISCV/cfi-advance.s
@@ -4,8 +4,16 @@
 # RUN:     | FileCheck -check-prefix=CHECK-DWARFDUMP %s
 
 # CHECK: 0x26 R_RISCV_SET8 - 0x0
-# CHECK-DWARFDUMP:   DW_CFA_advance_loc1
-# CHECK-DWARFDUMP-NEXT:  DW_CFA_def_cfa_offset
+# CHECK-NEXT: 0x26 R_RISCV_SUB8 - 0x0
+# CHECK-NEXT: 0x2A R_RISCV_SET16 - 0x0
+# CHECK-NEXT: 0x2A R_RISCV_SUB16 - 0x0
+# CHECK-NEXT: 0x2F R_RISCV_SET32 - 0x0
+# CHECK-DWARFDUMP: DW_CFA_advance_loc1
+# CHECK-DWARFDUMP-NEXT: DW_CFA_def_cfa_offset
+# CHECK-DWARFDUMP-NEXT: DW_CFA_advance_loc2
+# CHECK-DWARFDUMP-NEXT: DW_CFA_def_cfa_offset
+# CHECK-DWARFDUMP-NEXT: DW_CFA_advance_loc4
+# CHECK-DWARFDUMP-NEXT: DW_CFA_def_cfa_offset
         .text
         .globl  test                            # -- Begin function test
         .p2align        1
@@ -14,6 +22,12 @@ test:
         .cfi_startproc
         nop
         .zero 100, 0x90
+        .cfi_def_cfa_offset 8
+        nop
+        .zero 255, 0x90
+        .cfi_def_cfa_offset 8
+        nop
+        .zero 65535, 0x90
         .cfi_def_cfa_offset 8
         nop
         .cfi_endproc
