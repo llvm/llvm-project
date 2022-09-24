7871f6236e3f912f3909bf3a142b4de4c2fdfcc7
WuXinlong <821408745@qq.com>
2022-09-24 18:07:44 +0800

    fix compile erroe

--- lld/ELF/Driver.cpp
+++ lld/ELF/Driver.cpp
@@ -1443,7 +1443,7 @@ static void readConfigs(opt::InputArgList &args) {
   config->whyExtract = args.getLastArgValue(OPT_why_extract);
   config->zCombreloc = getZFlag(args, "combreloc", "nocombreloc", true);
   config->zCopyreloc = getZFlag(args, "copyreloc", "nocopyreloc", true);
-  config->riscvTbljal = args.hasArg(OPT_riscv_bljal);
+  config->riscvTbljal = args.hasArg(OPT_riscv_tbljal);
   config->zForceBti = hasZOption(args, "force-bti");
   config->zForceIbt = hasZOption(args, "force-ibt");
   config->zGlobal = hasZOption(args, "global");
