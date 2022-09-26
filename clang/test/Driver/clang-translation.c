e62b3a9375d8694efe5bf3d1409bec9be0d288e9
Tim Northover <tnorthover@apple.com>
2022-09-26 13:26:36 +0100

    Unwind-tables: move back to original logic outline for kind.
    There are lots of options interacting in complex ways here, and when moving to
    `getDefaultUnwindTableLevel` I had refactored this and changed behaviour in
    some cases. So this reverts the basic structure of the logic back to the
    original, while leaving the hook in the new style.

--- clang/test/Driver/clang-translation.c
+++ clang/test/Driver/clang-translation.c
@@ -99,6 +99,17 @@
 //
 // ARM64-EXPLICIT-UWTABLE-APPLE: -funwind-tables
 
+// RUN: %clang -target arm64-apple-macosx -### -ffreestanding -fasynchronous-unwind-tables %s 2>&1 | \
+// RUN: FileCheck --check-prefix=ASYNC-UNWIND-FREESTANDING %s
+//
+// ASYNC-UNWIND-FREESTANDING: -funwind-tables=2
+
+// Quite weird behaviour, but it's a long-standing default.
+// RUN: %clang -target x86_64-apple-macosx -### -fno-unwind-tables %s 2>&1 |\
+// RUN: FileCheck --check-prefix=NOUNWIND-IGNORED %s
+//
+// NOUNWIND-IGNORED: -funwind-tables=2
+
 // RUN: %clang -target arm64-apple-ios10 -fno-exceptions -### -S %s -arch arm64 2>&1 | \
 // RUN: FileCheck -check-prefix=ARM64-APPLE-EXCEP %s
 // ARM64-APPLE-EXCEP-NOT: -funwind-tables
