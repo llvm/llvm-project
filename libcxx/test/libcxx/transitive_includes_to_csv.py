b56d51c0990454f346e51774a703284c12dae263
Louis Dionne <ldionne.2@gmail.com>
2022-09-28 16:24:20 -0400

    [libc++] Simplify transitive includes test and fix a bug
    This patch incorporates the "sanitize" step of the transitive includes
    test into the CSV generator itself. In doing so, it removes complexity
    in the test but also fixes a bug where we would filter out <__mutex>
    from the output, leading to an incorrect list of includes for the
    <shared_mutex> header.
    
    Differential Revision: https://reviews.llvm.org/D134830

--- libcxx/test/libcxx/transitive_includes_to_csv.py
+++ libcxx/test/libcxx/transitive_includes_to_csv.py
@@ -10,9 +10,9 @@
 from dataclasses import dataclass, field
 from typing import List  # Needed for python 3.8 compatibility.
 import argparse
+import pathlib
 import re
 import sys
-import pathlib
 
 
 @dataclass
@@ -22,6 +22,9 @@ class header:
 
 
 def parse_line(line: str) -> header:
+    """
+    Parse an output line from --trace-include into a `header`.
+    """
     match = re.match(r"(\.+) (.+)", line)
     if not match:
         sys.exit(f"Line {line} contains invalid data.")
@@ -31,59 +34,117 @@ def parse_line(line: str) -> header:
     return header(match.group(2), len(match.group(1)))
 
 
-# Generates the list of transitive includes of a header.
-#
-# The input contains two kinds of headers
-# * Standard headers  (algorithm, string, format, etc)
-# * Detail headers (__algorithm/copy.h, __algorithm/copy_n.h, etc)
-# The output contains the transitive includes of the Standard header being
-# processed. The detail headers are omitted from the output, but their
-# transitive includes are parsed and added, if they are a Standard header.
-#
-# This effectively generates the dependency graph of a Standard header.
+# On Windows, the path separators can either be forward slash or backslash.
+# If it is a backslash, Clang prints it escaped as two consecutive
+# backslashes, and they need to be escaped in the RE. (Use a raw string for
+# the pattern to avoid needing another level of escaping on the Python string
+# literal level.)
+LIBCXX_HEADER_REGEX = r".*c\+\+(?:/|\\\\)v[0-9]+(?:/|\\\\)(.+)"
+
+def is_libcxx_public_header(header : str) -> bool:
+    """
+    Returns whether a header is a C++ public header file.
+    """
+    # Only keep files in the c++/vN directory.
+    match = re.match(LIBCXX_HEADER_REGEX, header)
+    if not match:
+        return False
+
+    # Skip C compatibility headers.
+    if header.endswith(".h"):
+        return False
+
+    # Skip all other detail headers (headers starting with __ or in a subdirectory starting with __).
+    relative = match.group(1)
+    if relative.startswith("__") or re.search(r"(/|\\\\)__", relative):
+        return False
+
+    return True
+
+
+def is_libcxx_header(header : str) -> bool:
+    """
+    Returns whether a header is a libc++ header, excluding the C-compatibility headers.
+    """
+    # Only keep files in the c++/vN directory.
+    match = re.match(LIBCXX_HEADER_REGEX, header)
+    if not match:
+        return False
+
+    # Skip C compatibility headers (in particular, make sure not to skip libc++ detail headers).
+    relative = match.group(1)
+    if relative.endswith(".h") and not (relative.startswith("__") or re.search(r"(/|\\\\)__", relative)):
+        return False
+
+    return True
+
+
 def parse_file(file: pathlib.Path) -> List[str]:
+    """
+    Parse a file containing --trace-include output to generate a list of the top-level C++ includes
+    contained in it.
+
+    This effectively generates the dependency graph of C++ Standard Library headers of the header
+    whose --trace-include it is. In order to get the expected result of --trace-include, the
+    -fshow-skipped-includes flag also needs to be passed.
+    """
     result = list()
     with file.open(encoding="utf-8") as f:
-        level = 999
-
-        # The first line contains the Standard header being processed.
-        # The transitive includes of this Standard header should be processed.
-        header = parse_line(f.readline())
-        assert header.level == 1
-        result.append(header.name)
-
         for line in f.readlines():
             header = parse_line(line)
 
-            # Skip deeper nested headers for Standard headers.
+            # Skip non-libc++ headers
+            if not is_libcxx_header(header.name):
+                continue
+
+            # Include top-level headers in the output. There's usually exactly one,
+            # except if the compiler is passed a file with `-include`. Top-level
+            # headers are transparent, in the sense that we want to go look at
+            # transitive includes underneath.
+            if header.level == 1:
+                level = 999
+                result.append(header)
+                continue
+
+            # Skip libc++ headers included transitively.
             if header.level > level:
                 continue
 
-            # Process deeper nested headers for detail headers.
-            if header.name.startswith("__") or header.name.__contains__("/__"):
+            # Detail headers are transparent too: we attribute all includes of public libc++
+            # headers under a detail header to the last public libc++ header that included it.
+            if header.name.startswith("__") or re.search(r"(/|\\\\)__", header.name):
                 level = 999
                 continue
 
-            # Add the Standard header.
+            # Add the non-detail libc++ header to the list.
             level = header.level
-            result.append(header.name)
-
+            result.append(header)
     return result
 
 
 def create_include_graph(path: pathlib.Path) -> List[str]:
     result = list()
     for file in sorted(path.glob("header.*")):
-        includes = parse_file(file)
-        if len(includes) > 1:
-            result.append(includes)
+        headers = parse_file(file)
+
+        # Get actual filenames relative to libc++'s installation directory instead of full paths
+        relative = lambda h: re.match(LIBCXX_HEADER_REGEX, h).group(1)
+
+        top_level = relative(next(h.name for h in headers if h.level == 1)) # There should be only one top-level header
+        includes = [relative(h.name) for h in headers if h.level != 1]
+
+        # Remove duplicates in all includes.
+        includes = list(set(includes))
+
+        if len(includes) != 0:
+            result.append([top_level] + includes)
     return result
 
 
 def print_csv(graph: List[str]) -> None:
     for includes in graph:
         header = includes[0]
-        for include in sorted(set(includes[1:])):
+        for include in sorted(includes[1:]):
             if header == include:
                 sys.exit(f"Cycle detected: header {header} includes itself.")
             print(f"{header} {include}")
