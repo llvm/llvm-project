003b6033e1b254dd96ddb341f375b73ee5bed2af
Leonard Chan <leonardchan@google.com>
2022-09-29 21:53:30 +0000

    [clang] Ensure correct metadata for relative vtables
    Prior to this, metadata pertaining to the size or address point offsets
    into a relative vtable were twice the value they should be (treating
    component widths as pointer width rather than 4 bytes). This prevented
    some vtables from being devirtualized with D134320. This ensures the
    correct metadata is written so whole program devirtualization can catch
    these remaining devirt targets.
    
    Differential Revision: https://reviews.llvm.org/D134687

--- clang/lib/CodeGen/CGVTables.h
+++ clang/lib/CodeGen/CGVTables.h
@@ -102,6 +102,10 @@ public:
     return *cast<ItaniumVTableContext>(VTContext);
   }
 
+  const ItaniumVTableContext &getItaniumVTableContext() const {
+    return *cast<ItaniumVTableContext>(VTContext);
+  }
+
   MicrosoftVTableContext &getMicrosoftVTableContext() {
     return *cast<MicrosoftVTableContext>(VTContext);
   }
