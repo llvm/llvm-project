3a79f1caa9bd1e43f4e0e9187b87c978105c40b1
Fangrui Song <i@maskray.me>
2022-09-29 12:21:57 -0700

    [Driver][test] Restore %clang -cc1 in test/Driver
    This partially reverts commit 1609a5d7715c06ff52c13af8b20ee64811a8ec7b (the
    test/Driver part). We want to discourage %clang_cc1 and clang -cc1 in
    test/Driver. The clang -cc1 uses in hlsl/offload/etc are not good examples.

--- clang/test/Driver/reloc-model.c
+++ clang/test/Driver/reloc-model.c
@@ -1,4 +1,4 @@
-// RUN: not %clang_cc1 -mrelocation-model tinkywinky \
+// RUN: not %clang -cc1 -mrelocation-model tinkywinky \
 // RUN: -emit-llvm %s 2>&1 | FileCheck -check-prefix CHECK-INVALID %s
 
 // CHECK-INVALID: error: invalid value 'tinkywinky' in '-mrelocation-model tinkywinky'
