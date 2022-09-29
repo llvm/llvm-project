77ff99c10bee11a202dbe5fd1a08d8394d7828af
Nikita Popov <npopov@redhat.com>
2022-09-29 15:51:05 +0200

    [ValueTracking] Fix CannotBeOrderedLessThanZero() for fdiv (PR58046)
    When checking the RHS of fdiv, we should set the SignBitOnly flag,
    because a negative zero can become -Inf, which is ordered less
    than zero.
    
    Fixes https://github.com/llvm/llvm-project/issues/58046.
    
    Differential Revision: https://reviews.llvm.org/D134876

--- llvm/lib/Analysis/ValueTracking.cpp
+++ llvm/lib/Analysis/ValueTracking.cpp
@@ -3593,10 +3593,19 @@ static bool cannotBeOrderedLessThanZeroImpl(const Value *V,
   // Unsigned integers are always nonnegative.
   case Instruction::UIToFP:
     return true;
-  case Instruction::FMul:
   case Instruction::FDiv:
-    // X * X is always non-negative or a NaN.
     // X / X is always exactly 1.0 or a NaN.
+    if (I->getOperand(0) == I->getOperand(1) &&
+        (!SignBitOnly || cast<FPMathOperator>(I)->hasNoNaNs()))
+      return true;
+
+    // Set SignBitOnly for RHS, because X / -0.0 is -Inf (or NaN).
+    return cannotBeOrderedLessThanZeroImpl(I->getOperand(0), TLI, SignBitOnly,
+                                           Depth + 1) &&
+           cannotBeOrderedLessThanZeroImpl(I->getOperand(1), TLI,
+                                           /*SignBitOnly*/ true, Depth + 1);
+  case Instruction::FMul:
+    // X * X is always non-negative or a NaN.
     if (I->getOperand(0) == I->getOperand(1) &&
         (!SignBitOnly || cast<FPMathOperator>(I)->hasNoNaNs()))
       return true;
