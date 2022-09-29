72a86a9dc4c53010f3b493685d0798bf02bf6ebc
Dave Lee <davelee.com@gmail.com>
2022-09-29 16:23:22 -0700

    [lldb] Remove scoped timer from high firing and fast running SymbolFileDWARF::FindFunctions
    Profiles show that `SymbolFileDWARF::FindFunctions` is both high firing (many thousands of calls) and fast running (35 µs mean).
    
    Timers like this are noise and load for profiling systems, and can be removed.
    
    rdar://100326595
    
    Differential Revision: https://reviews.llvm.org/D134922

--- lldb/source/Plugins/SymbolFile/DWARF/SymbolFileDWARF.cpp
+++ lldb/source/Plugins/SymbolFile/DWARF/SymbolFileDWARF.cpp
@@ -2355,8 +2355,6 @@ void SymbolFileDWARF::FindFunctions(const Module::LookupInfo &lookup_info,
   std::lock_guard<std::recursive_mutex> guard(GetModuleMutex());
   ConstString name = lookup_info.GetLookupName();
   FunctionNameType name_type_mask = lookup_info.GetNameTypeMask();
-  LLDB_SCOPED_TIMERF("SymbolFileDWARF::FindFunctions (name = '%s')",
-                     name.AsCString());
 
   // eFunctionNameTypeAuto should be pre-resolved by a call to
   // Module::LookupInfo::LookupInfo()
