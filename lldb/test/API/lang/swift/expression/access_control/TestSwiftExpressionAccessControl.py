51d961b7695f96367ff0a159024e58392e59d147
Adrian Prantl <aprantl@apple.com>
2022-09-30 10:22:21 -0700

    Simplify testcase (NFC)
    (cherry picked from commit c0911e18e7729020816830fa4b28401ea91a8b15)
    
     Conflicts:
    	lldb/test/API/lang/swift/expression/access_control/TestExpressionAccessControl.py

---
+++ lldb/test/API/lang/swift/expression/access_control/TestSwiftExpressionAccessControl.py
@@ -0,0 +1,22 @@
+import lldb
+from lldbsuite.test.lldbtest import *
+from lldbsuite.test.decorators import *
+import lldbsuite.test.lldbutil as lldbutil
+import os
+import unittest2
+
+
+class TestSwiftExpressionAccessControl(TestBase):
+
+    def setUp(self):
+        TestBase.setUp(self)
+
+    @swiftTest
+    def test_swift_expression_access_control(self):
+        """Make sure expressions ignore access control"""
+        self.build()
+        lldbutil.run_to_source_breakpoint(
+            self, 'Set breakpoint here', lldb.SBFileSpec('main.swift'))
+        lldbutil.check_expression(self, self.frame(),
+                                  "foo.m_a", "3", use_summary=False)
+
