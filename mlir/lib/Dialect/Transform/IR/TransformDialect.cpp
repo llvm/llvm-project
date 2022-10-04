b586d56c7b8b0188355a4d7f5f8b09f8b3847757
Alex Zinenko <zinenko@google.com>
2022-10-04 15:49:30 +0000

    [mlir] clean up transform dialect definitions, NFC
    Refactor the definition of the Transform dialect to move non-trivial
    method implementations out of the .td file, and detemplatize functions
    when possible while moving their implementations to a .cpp.
    
    Reviewed By: nicolasvasilache
    
    Differential Revision: https://reviews.llvm.org/D135165

--- mlir/lib/Dialect/Transform/IR/TransformDialect.cpp
+++ mlir/lib/Dialect/Transform/IR/TransformDialect.cpp
@@ -9,6 +9,7 @@
 #include "mlir/Dialect/Transform/IR/TransformDialect.h"
 #include "mlir/Dialect/PDL/IR/PDL.h"
 #include "mlir/Dialect/PDLInterp/IR/PDLInterp.h"
+#include "mlir/Dialect/Transform/IR/TransformInterfaces.h"
 #include "mlir/Dialect/Transform/IR/TransformOps.h"
 #include "mlir/Dialect/Transform/IR/TransformTypes.h"
 #include "mlir/IR/DialectImplementation.h"
@@ -18,6 +19,22 @@ using namespace mlir;
 #include "mlir/Dialect/Transform/IR/TransformDialect.cpp.inc"
 
 #ifndef NDEBUG
+void transform::detail::checkImplementsTransformOpInterface(
+    StringRef name, MLIRContext *context) {
+  // Since the operation is being inserted into the Transform dialect and the
+  // dialect does not implement the interface fallback, only check for the op
+  // itself having the interface implementation.
+  RegisteredOperationName opName =
+      *RegisteredOperationName::lookup(name, context);
+  assert((opName.hasInterface<TransformOpInterface>() ||
+          opName.hasTrait<OpTrait::IsTerminator>()) &&
+         "non-terminator ops injected into the transform dialect must "
+         "implement TransformOpInterface");
+  assert(opName.hasInterface<MemoryEffectOpInterface>() &&
+         "ops injected into the transform dialect must implement "
+         "MemoryEffectsOpInterface");
+}
+
 void transform::detail::checkImplementsTransformTypeInterface(
     TypeID typeID, MLIRContext *context) {
   const auto &abstractType = AbstractType::lookup(typeID, context);
@@ -76,8 +93,18 @@ void transform::TransformDialect::reportDuplicateTypeRegistration(
     StringRef mnemonic) {
   std::string buffer;
   llvm::raw_string_ostream msg(buffer);
-  msg << "error: extensible dialect type '" << mnemonic
+  msg << "extensible dialect type '" << mnemonic
       << "' is already registered with a different implementation";
+  msg.flush();
+  llvm::report_fatal_error(StringRef(buffer));
+}
+
+void transform::TransformDialect::reportDuplicateOpRegistration(
+    StringRef opName) {
+  std::string buffer;
+  llvm::raw_string_ostream msg(buffer);
+  msg << "extensible dialect operation '" << opName
+      << "' is already registered with a mismatching TypeID";
   msg.flush();
   llvm::report_fatal_error(StringRef(buffer));
 }
