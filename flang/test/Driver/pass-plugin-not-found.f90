1a2d5571e0aaec8736a12feb8509290687ce1677
Mats Petersson <mats.petersson@arm.com>
2022-10-05 19:16:18 +0100

    Revert "[flang] Add -fpass-plugin option to Flang frontend"
    This reverts commit 52a3819d0d19f0cbbf7b921ddbdd73f6c23e7ed5.
    
    Reverting this as CI breaks.
    
    To reproduce, run check-flang, and it will fail with an error saying
    .../lib/Bye.so not found in pass-plugin.f90

--- flang/test/Driver/pass-plugin-not-found.f90
+++
@@ -1,8 +0,0 @@
-! Check the correct error diagnostic is reported when a pass plugin shared object isn't found
-
-! REQUIRES: plugins, shell
-
-! RUN: not %flang -fpass-plugin=X.Y %s 2>&1 | FileCheck %s --check-prefix=ERROR
-! RUN: not %flang_fc1 -emit-llvm -o /dev/null -fpass-plugin=X.Y %s 2>&1 | FileCheck %s --check-prefix=ERROR
-
-! ERROR: error: unable to load plugin 'X.Y': 'Could not load library 'X.Y': X.Y: cannot open shared object file: No such file or directory'
