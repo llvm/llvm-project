03819f88ca97d108fc792e6c4470be9b7aa57f55
eopXD <yueh.ting.chen@gmail.com>
2022-10-04 18:10:14 -0700

    [VP][RISCV] Add vp.roundtozero and its RISC-V support
    The scalar instruction of this is `llvm.trunc`. However the naming of
    ISD::VP_TRUNC is already taken by `trunc` of the LLVM IR. Naming this as
    `vp.ftrunc` would likely cause confusion with `vp.fptrunc`. So adding
    `vp.roundtozero` that will look similar to `vp.roundeven`.
    
    Reviewed By: frasercrmck
    
    Differential Revision: https://reviews.llvm.org/D135233

--- llvm/lib/CodeGen/SelectionDAG/LegalizeVectorTypes.cpp
+++ llvm/lib/CodeGen/SelectionDAG/LegalizeVectorTypes.cpp
@@ -1049,6 +1049,7 @@ void DAGTypeLegalizer::SplitVectorResult(SDNode *N, unsigned ResNo) {
   case ISD::FSIN:
   case ISD::FSQRT: case ISD::VP_SQRT:
   case ISD::FTRUNC:
+  case ISD::VP_FROUNDTOZERO:
   case ISD::SINT_TO_FP:
   case ISD::VP_SINT_TO_FP:
   case ISD::TRUNCATE:
@@ -4094,6 +4095,7 @@ void DAGTypeLegalizer::WidenVectorResult(SDNode *N, unsigned ResNo) {
   case ISD::VP_FFLOOR:
   case ISD::VP_FROUND:
   case ISD::VP_FROUNDEVEN:
+  case ISD::VP_FROUNDTOZERO:
   case ISD::FREEZE:
   case ISD::ARITH_FENCE:
   case ISD::FCANONICALIZE:
