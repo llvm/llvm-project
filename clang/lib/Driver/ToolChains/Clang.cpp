ac135f9ee574e7451088926c667d93d51a3d6940
Joseph Huber <jhuber6@vols.utk.edu>
2022-10-05 14:35:48 -0500

    [Clang] Fix using LTO with the new driver in RDC-mode
    The new driver supports LTO for RDC-mode compilations. However, this was
    not correctly handled for non-LTO compilations. HIP can handle this as
    it is fed to `lld` which will perform the LTO itself. CUDA however would
    require every work which is wholly useless in non-RDC mode so it should
    report an error.
    
    Reviewed By: yaxunl
    
    Differential Revision: https://reviews.llvm.org/D135305

--- clang/lib/Driver/ToolChains/Clang.cpp
+++ clang/lib/Driver/ToolChains/Clang.cpp
@@ -4842,7 +4842,6 @@ void Clang::ConstructJob(Compilation &C, const JobAction &JA,
       CmdArgs.push_back("-emit-llvm-uselists");
 
     if (IsUsingLTO) {
-      // Only AMDGPU supports device-side LTO.
       if (IsDeviceOffloadAction && !JA.isDeviceOffloading(Action::OFK_OpenMP) &&
           !Args.hasFlag(options::OPT_offload_new_driver,
                         options::OPT_no_offload_new_driver, false) &&
@@ -4852,6 +4851,12 @@ void Clang::ConstructJob(Compilation &C, const JobAction &JA,
                                options::OPT_foffload_lto_EQ)
                    ->getAsString(Args)
             << Triple.getTriple();
+      } else if (Triple.isNVPTX() && !IsRDCMode) {
+        D.Diag(diag::err_drv_unsupported_opt_for_language_mode)
+            << Args.getLastArg(options::OPT_foffload_lto,
+                               options::OPT_foffload_lto_EQ)
+                   ->getAsString(Args)
+            << "-fno-gpu-rdc";
       } else {
         assert(LTOMode == LTOK_Full || LTOMode == LTOK_Thin);
         CmdArgs.push_back(Args.MakeArgString(
