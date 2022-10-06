d7fa95fd9dc0fa5f634dad76ecae915dad355741
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2022-10-06 16:53:30 -0400

    [libc++] Add the C++17 <memory_resource> header (mono-patch)
    This patch is the rebase and squash of three earlier patches.
    It supersedes all three of them.
    
    - D47111: experimental monotonic_buffer_resource.
    - D47358: experimental pool resources.
    - D47360: Copy std::experimental::pmr to std::pmr.
    
    The significant difference between this patch and the-sum-of-those-three
    is that this patch does not add `std::experimental::pmr::monotonic_buffer_resource`
    and so on. This patch simply adds the C++17 standard facilities, and
    leaves the `std::experimental` namespace entirely alone.
    
    Differential Revision: https://reviews.llvm.org/D89057

---
+++ libcxx/test/std/utilities/utility/mem.res/mem.poly.allocator.class/mem.poly.allocator.mem/allocate.pass.cpp
@@ -0,0 +1,110 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++03, c++11, c++14
+// XFAIL: use_system_cxx_lib && target={{.+}}-apple-macosx10.{{9|10|11|12|13|14|15}}
+// XFAIL: use_system_cxx_lib && target={{.+}}-apple-macosx{{11.0|12.0}}
+
+// <memory_resource>
+
+// template <class T> class polymorphic_allocator
+
+// T* polymorphic_allocator<T>::allocate(size_t n)
+
+#include <memory_resource>
+#include <cassert>
+#include <exception>
+#include <limits>
+#include <memory>
+#include <type_traits>
+
+#include "test_macros.h"
+#include "test_std_memory_resource.h"
+
+template <size_t S, size_t Align>
+void testForSizeAndAlign() {
+  using T = typename std::aligned_storage<S, Align>::type;
+  TestResource R;
+  std::pmr::polymorphic_allocator<T> a(&R);
+
+  for (int N = 1; N <= 5; ++N) {
+    auto ret = a.allocate(N);
+    assert(R.checkAlloc(ret, N * sizeof(T), alignof(T)));
+
+    a.deallocate(ret, N);
+    R.reset();
+  }
+}
+
+#ifndef TEST_HAS_NO_EXCEPTIONS
+template <size_t S>
+void testAllocForSizeThrows() {
+  using T      = typename std::aligned_storage<S>::type;
+  using Alloc  = std::pmr::polymorphic_allocator<T>;
+  using Traits = std::allocator_traits<Alloc>;
+  NullResource R;
+  Alloc a(&R);
+
+  // Test that allocating exactly the max size does not throw.
+  size_t maxSize = Traits::max_size(a);
+  try {
+    a.allocate(maxSize);
+  } catch (...) {
+    assert(false);
+  }
+
+  size_t sizeTypeMax = std::numeric_limits<std::size_t>::max();
+  if (maxSize != sizeTypeMax) {
+    // Test that allocating size_t(~0) throws bad alloc.
+    try {
+      a.allocate(sizeTypeMax);
+      assert(false);
+    } catch (const std::exception&) {
+    }
+
+    // Test that allocating even one more than the max size does throw.
+    size_t overSize = maxSize + 1;
+    try {
+      a.allocate(overSize);
+      assert(false);
+    } catch (const std::exception&) {
+    }
+  }
+}
+#endif // TEST_HAS_NO_EXCEPTIONS
+
+int main(int, char**) {
+  {
+    std::pmr::polymorphic_allocator<int> a;
+    ASSERT_SAME_TYPE(decltype(a.allocate(0)), int*);
+    static_assert(!noexcept(a.allocate(0)), "");
+  }
+  {
+    constexpr std::size_t MA = alignof(std::max_align_t);
+    testForSizeAndAlign<1, 1>();
+    testForSizeAndAlign<1, 2>();
+    testForSizeAndAlign<1, MA>();
+    testForSizeAndAlign<2, 2>();
+    testForSizeAndAlign<73, alignof(void*)>();
+    testForSizeAndAlign<73, MA>();
+    testForSizeAndAlign<13, MA>();
+  }
+#ifndef TEST_HAS_NO_EXCEPTIONS
+  {
+    testAllocForSizeThrows<1>();
+    testAllocForSizeThrows<2>();
+    testAllocForSizeThrows<4>();
+    testAllocForSizeThrows<8>();
+    testAllocForSizeThrows<16>();
+    testAllocForSizeThrows<73>();
+    testAllocForSizeThrows<13>();
+  }
+#endif
+
+  return 0;
+}
