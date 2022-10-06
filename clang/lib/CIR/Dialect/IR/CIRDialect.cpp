a4399c407ec8313a37fc81ec968dd0803ddcf7a8
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2022-10-05 18:05:40 -0700

    [CIR][NFC] Constify AST references

--- clang/lib/CIR/Dialect/IR/CIRDialect.cpp
+++ clang/lib/CIR/Dialect/IR/CIRDialect.cpp
@@ -1482,7 +1482,7 @@ void ASTFunctionDeclAttr::print(::mlir::AsmPrinter &printer) const {
 
 LogicalResult ASTFunctionDeclAttr::verify(
     ::llvm::function_ref<::mlir::InFlightDiagnostic()> emitError,
-    ::clang::FunctionDecl *decl) {
+    const ::clang::FunctionDecl *decl) {
   if (!decl) {
     emitError() << "expected non-null AST declaration";
     return failure();
@@ -1503,7 +1503,7 @@ void ASTVarDeclAttr::print(::mlir::AsmPrinter &printer) const {
 
 LogicalResult ASTVarDeclAttr::verify(
     ::llvm::function_ref<::mlir::InFlightDiagnostic()> emitError,
-    ::clang::VarDecl *decl) {
+    const ::clang::VarDecl *decl) {
   if (!decl) {
     emitError() << "expected non-null AST declaration";
     return failure();
