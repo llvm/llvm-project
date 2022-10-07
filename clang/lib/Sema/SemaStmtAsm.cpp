4d77b01bd4cc0143fa06edd3977f05ed3c7cc1e4
Tom Honermann <tom.honermann@intel.com>
2022-10-07 14:13:46 -0700

    [Clang] reject bit-fields as instruction operands in Microsoft style inline asm blocks.
    MSVC allows bit-fields to be specified as instruction operands in inline asm
    blocks. Such references are resolved to the address of the allocation unit that
    the named bitfield is a part of. The result is that reads and writes of such
    operands will read or mutate nearby bit-fields stored in the same allocation
    unit. This is a surprising behavior for which MSVC issues warning C4401,
    "'<identifier>': member is bit field". Intel's icc compiler also allows such
    bit-field references, but does not issue a diagnostic.
    
    Prior to this change, Clang fails the following assertion when a bit-field is
    referenced in such instructions:
      clang/lib/CodeGen/CGValue.h:338: llvm::Value* clang::CodeGen::LValue::getPointer(clang::CodeGen::CodeGenFunction&) const: Assertion `isSimple()' failed.
    In non-assert enabled builds, Clang's behavior appears to match the behavior
    of the MSVC and icc compilers, though it is unknown if that is by design or
    happenstance.
    
    Following this change, attempts to use a bit-field as an instruction operand
    in Microsoft style asm blocks is diagnosed as an error due to the risk of
    unintentionally reading or writing nearby bit-fields.
    
    Fixes https://github.com/llvm/llvm-project/issues/57791
    
    Reviewed By: erichkeane, aaron.ballman
    
    Differential Revision: https://reviews.llvm.org/D135500

--- clang/lib/Sema/SemaStmtAsm.cpp
+++ clang/lib/Sema/SemaStmtAsm.cpp
@@ -932,13 +932,24 @@ StmtResult Sema::ActOnMSAsmStmt(SourceLocation AsmLoc, SourceLocation LBraceLoc,
   bool IsSimple = (NumOutputs != 0 || NumInputs != 0);
   setFunctionHasBranchProtectedScope();
 
+  bool InvalidOperand = false;
   for (uint64_t I = 0; I < NumOutputs + NumInputs; ++I) {
-    if (Exprs[I]->getType()->isBitIntType())
-      return StmtError(
-          Diag(Exprs[I]->getBeginLoc(), diag::err_asm_invalid_type)
-          << Exprs[I]->getType() << (I < NumOutputs)
-          << Exprs[I]->getSourceRange());
+    Expr *E = Exprs[I];
+    if (E->getType()->isBitIntType()) {
+      InvalidOperand = true;
+      Diag(E->getBeginLoc(), diag::err_asm_invalid_type)
+          << E->getType() << (I < NumOutputs)
+          << E->getSourceRange();
+    } else if (E->refersToBitField()) {
+      InvalidOperand = true;
+      FieldDecl *BitField = E->getSourceBitField();
+      Diag(E->getBeginLoc(), diag::err_ms_asm_bitfield_unsupported)
+          << E->getSourceRange();
+      Diag(BitField->getLocation(), diag::note_bitfield_decl);
+    }
   }
+  if (InvalidOperand)
+    return StmtError();
 
   MSAsmStmt *NS =
     new (Context) MSAsmStmt(Context, AsmLoc, LBraceLoc, IsSimple,
