d3fe7a561ea97b30ca46e6b9b0e7eec8983c931a
Diego Caballero <diegocaballero@google.com>
2022-10-10 21:21:03 +0000

    [mlir][Vector] Introduce 'vector.mask' operation and MaskableOpInterface
    This patch introduces the `vector.mask` operation and the MaskableOpInterface
    as described in https://discourse.llvm.org/t/rfc-vector-masking-representation-in-mlir/64964.
    The `vector.mask` operation is used to predicate the execution of operations
    implementing the MaskableOpInterface. This interface will be implemented by maskable
    operations and provides information about its masking constraints and semantics.
    
    For now, only vector transfer and reduction ops implement the MaskableOpInterface
    for illustration and testing purposes.
    
    Reviewed By: nicolasvasilache, rriddle
    
    Differential Revision: https://reviews.llvm.org/D134939

---
+++ mlir/include/mlir/Dialect/Vector/Interfaces/MaskingInterfaces.h
@@ -0,0 +1,22 @@
+//===- MaskingInterfaces.h - Masking interfaces ---------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file implements the interfaces for masking operations.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef MLIR_DIALECT_VECTOR_INTERFACES_MASKINGINTERFACES_H_
+#define MLIR_DIALECT_VECTOR_INTERFACES_MASKINGINTERFACES_H_
+
+#include "mlir/IR/BuiltinTypes.h"
+#include "mlir/IR/OpDefinition.h"
+
+/// Include the generated interface declarations.
+#include "mlir/Dialect/Vector/Interfaces/MaskingInterfaces.h.inc"
+
+#endif // MLIR_DIALECT_VECTOR_INTERFACES_MASKINGINTERFACES_H_
