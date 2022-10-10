fd91e8f5016626328093aedc8419f12af20ff554
Michael Buch <michaelbuch12@gmail.com>
2022-10-10 14:31:29 +0100

    [lldb][test] Skip TestStepAvoidsRegexp.py on Windows

--- lldb/test/API/functionalities/step-avoids-regexp/TestStepAvoidsRegexp.py
+++ lldb/test/API/functionalities/step-avoids-regexp/TestStepAvoidsRegexp.py
@@ -19,6 +19,7 @@ class StepAvoidsRegexTestCase(TestBase):
         self.dbg.HandleCommand(
                 "settings set target.process.thread.step-avoid-regexp ^ignore::")
 
+    @skipIfWindows
     def test_step_avoid_regex(self):
         """Tests stepping into a function which matches the avoid regex"""
         self.build()
@@ -36,6 +37,7 @@ class StepAvoidsRegexTestCase(TestBase):
         self.thread.StepInto()
         self.hit_correct_function("main")
 
+    @skipIfWindows
     @expectedFailureAll(bugnumber="rdar://100645742")
     def test_step_avoid_regex_abi_tagged_template(self):
         """Tests stepping into an ABI tagged function that matches the avoid regex"""
