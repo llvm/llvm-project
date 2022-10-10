3be94a11b8b268dde48d6d12e0956b6e7a7e561f
Michael Jones <michaelrj@google.com>
2022-10-10 14:37:21 -0700

    [libc] add isatty
    The isatty function uses the side effects of an ioctl call to determine
    if a specific file descriptor is a terminal. I chose TIOCGETD (get line
    discipline of terminal) because it didn't require any new structs.
    
    Reviewed By: sivachandra, lntue
    
    Differential Revision: https://reviews.llvm.org/D135618

---
+++ libc/test/src/unistd/isatty_test.cpp
@@ -0,0 +1,54 @@
+//===-- Unittests for isatty ----------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "src/fcntl/open.h"
+#include "src/unistd/close.h"
+#include "src/unistd/isatty.h"
+#include "test/ErrnoSetterMatcher.h"
+#include "utils/UnitTest/Test.h"
+
+#include <errno.h>
+
+using __llvm_libc::testing::ErrnoSetterMatcher::Fails;
+using __llvm_libc::testing::ErrnoSetterMatcher::Succeeds;
+
+TEST(LlvmLibcIsATTYTest, StdInOutTests) {
+  // If stdin is connected to a terminal, assume that all of the standard i/o
+  // fds are.
+  if (__llvm_libc::isatty(0)) {
+    EXPECT_THAT(__llvm_libc::isatty(0), Succeeds(1)); // stdin
+    EXPECT_THAT(__llvm_libc::isatty(1), Succeeds(1)); // stdout
+    EXPECT_THAT(__llvm_libc::isatty(2), Succeeds(1)); // stderr
+  } else {
+    EXPECT_THAT(__llvm_libc::isatty(0), Fails(ENOTTY, 0)); // stdin
+    EXPECT_THAT(__llvm_libc::isatty(1), Fails(ENOTTY, 0)); // stdout
+    EXPECT_THAT(__llvm_libc::isatty(2), Fails(ENOTTY, 0)); // stderr
+  }
+}
+
+TEST(LlvmLibcIsATTYTest, BadFdTest) {
+  EXPECT_THAT(__llvm_libc::isatty(-1), Fails(EBADF, 0)); // invalid fd
+}
+
+TEST(LlvmLibcIsATTYTest, DevTTYTest) {
+  constexpr const char *TTY_FILE = "/dev/tty";
+  int fd = __llvm_libc::open(TTY_FILE, O_RDONLY);
+  ASSERT_EQ(errno, 0);
+  ASSERT_GT(fd, 0);
+  EXPECT_THAT(__llvm_libc::isatty(fd), Succeeds(1));
+  ASSERT_THAT(__llvm_libc::close(fd), Succeeds(0));
+}
+
+TEST(LlvmLibcIsATTYTest, FileTest) {
+  constexpr const char *TEST_FILE = "testdata/isatty.test";
+  int fd = __llvm_libc::open(TEST_FILE, O_WRONLY | O_CREAT, S_IRWXU);
+  ASSERT_EQ(errno, 0);
+  ASSERT_GT(fd, 0);
+  EXPECT_THAT(__llvm_libc::isatty(fd), Fails(ENOTTY, 0));
+  ASSERT_THAT(__llvm_libc::close(fd), Succeeds(0));
+}
