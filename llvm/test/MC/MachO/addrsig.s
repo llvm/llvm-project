66046e6e65e5dfe0b6e2222a467d07899a43b624
Fangrui Song <i@maskray.me>
2022-10-10 21:10:35 -0700

    [MC][test] Add Mach-O .addrsig test

---
+++ llvm/test/MC/MachO/addrsig.s
@@ -0,0 +1,45 @@
+# RUN: llvm-mc -filetype=obj -triple arm64-apple-darwin %s -o %t
+# RUN: llvm-readobj -r -s %t | FileCheck %s
+
+# CHECK:      Section __llvm_addrsig {
+# CHECK-NEXT:   0x0 0 3 1 ARM64_RELOC_UNSIGNED 0 .Llocal
+# CHECK-NEXT:   0x0 0 3 1 ARM64_RELOC_UNSIGNED 0 local
+# CHECK-NEXT:   0x0 0 3 1 ARM64_RELOC_UNSIGNED 0 g3
+# CHECK-NEXT:   0x0 0 3 1 ARM64_RELOC_UNSIGNED 0 g1
+# CHECK-NEXT: }
+
+# CHECK:      Symbols [
+# CHECK:        Symbol {
+# CHECK-NEXT:     Name: ltmp0
+# CHECK:        Symbol {
+# CHECK-NEXT:     Name: local
+# CHECK:        Symbol {
+# CHECK-NEXT:     Name: .Llocal
+# CHECK:        Symbol {
+# CHECK-NEXT:     Name: ltmp1
+# CHECK:        Symbol {
+# CHECK-NEXT:     Name: g1
+# CHECK:        Symbol {
+# CHECK-NEXT:     Name: g2
+# CHECK:        Symbol {
+# CHECK-NEXT:     Name: g3
+# CHECK-NOT:    Symbol {
+# CHECK:      ]
+
+.globl g1
+
+.addrsig
+.addrsig_sym g1
+.globl g2
+.addrsig_sym g3
+.addrsig_sym local
+.addrsig_sym .Llocal
+
+local:
+  nop
+.globl g3
+
+.data
+.Llocal:
+
+.subsections_via_symbols
