657f68b1f2fd38deb63c23d8f46d12b7fd357e63
Quentin Colombet <quentin.colombet@gmail.com>
2022-10-11 20:53:25 +0000

    [NFC][mlir][MemRef] Make use of InferTypeOpInterface
    The `InferTypeOpInterface` generates builders for things it can infer
    the types.
    Thanks to that interface we can:
    - Eliminate a builder for DimOp, and
    - Describe how to infer the result types of `extract_strided_metadata`
      from its source, and get a simpler builder as a result
    
    NFC
    
    Differential Revision: https://reviews.llvm.org/D135734

--- mlir/include/mlir/Dialect/MemRef/IR/MemRef.h
+++ mlir/include/mlir/Dialect/MemRef/IR/MemRef.h
@@ -15,6 +15,7 @@
 #include "mlir/Interfaces/CastInterfaces.h"
 #include "mlir/Interfaces/ControlFlowInterfaces.h"
 #include "mlir/Interfaces/CopyOpInterface.h"
+#include "mlir/Interfaces/InferTypeOpInterface.h"
 #include "mlir/Interfaces/ShapedOpInterfaces.h"
 #include "mlir/Interfaces/SideEffectInterfaces.h"
 #include "mlir/Interfaces/ViewLikeInterface.h"
