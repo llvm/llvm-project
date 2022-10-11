7667dfbb3a070f23993e19977c3b1dc48420d15f
Nathan Lanza <nathan@lanza.io>
2022-10-10 21:58:23 -0400

    [CIR] Begin support for fp constrained rounding and except
    Begin implementing the block that propagates floating point language
    options to the CIRBuilderTy. More support to come.

--- clang/lib/CIR/CodeGen/CIRGenBuilder.h
+++ clang/lib/CIR/CodeGen/CIRGenBuilder.h
@@ -9,15 +9,41 @@
 #ifndef LLVM_CLANG_LIB_CIR_CIRGENBUILDER_H
 #define LLVM_CLANG_LIB_CIR_CIRGENBUILDER_H
 
+#include "clang/CIR/Dialect/IR/FPEnv.h"
+
 #include "mlir/IR/Builders.h"
+#include "llvm/ADT/FloatingPointMode.h"
 
 namespace cir {
 
 class CIRGenFunction;
 
 class CIRGenBuilderTy : public mlir::OpBuilder {
+  fp::ExceptionBehavior DefaultConstrainedExcept = fp::ebStrict;
+  llvm::RoundingMode DefaultConstrainedRounding = llvm::RoundingMode::Dynamic;
+
 public:
   CIRGenBuilderTy(mlir::MLIRContext &C) : mlir::OpBuilder(&C) {}
+
+  /// Set the exception handling to be used with constrained floating point
+  void setDefaultConstrainedExcept(fp::ExceptionBehavior NewExcept) {
+#ifndef NDEBUG
+    std::optional<llvm::StringRef> ExceptStr =
+        convertExceptionBehaviorToStr(NewExcept);
+    assert(ExceptStr && "Garbage strict exception behavior!");
+#endif
+    DefaultConstrainedExcept = NewExcept;
+  }
+
+  /// Set the rounding mode handling to be used with constrained floating point
+  void setDefaultConstrainedRounding(llvm::RoundingMode NewRounding) {
+#ifndef NDEBUG
+    std::optional<llvm::StringRef> RoundingStr =
+        convertRoundingModeToStr(NewRounding);
+    assert(RoundingStr && "Garbage strict rounding mode!");
+#endif
+    DefaultConstrainedRounding = NewRounding;
+  }
 };
 
 } // namespace cir
