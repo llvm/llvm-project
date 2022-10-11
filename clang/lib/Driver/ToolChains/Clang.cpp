e3155235807abb0c63f68155995f5ff085e81fa1
Daniel Rodríguez Troitiño <danielrodriguez@fb.com>
2022-10-11 16:38:39 -0700

    [clang][macho] Add support for -darwin-target-variant-sdk-version in cc1as
    D121868 provided support for -darwin-target-variant-triple, but the
    support for -darwin-target-variant-sdk-version was still missing for
    cc1as. These changes build upon the previous and provides such support.
    
    - Extracted the common code to handle -darwin-target-variant-triple and
      -darwin-target-variant-sdk-version in the Darwin toolchain to a method
      that can be used for both the cc1 and the cc1as job construction.
      cc1as does not support some of the parameters that were provided to
      cc1, so the same code cannot be used for both.
    - Invoke that new common code when constructing a cc1as invocation.
    - Parse the new -darwin-target-variant-sdk-version in the cc1as driver.
      Apply its value to the MCObjectFileInfo to generate the right values
      in the object files.
    - Includes two new tests that check that cc1as uses the provided values
      in -darwin-target-variant-sdk and that the Clang driver creates the
      jobs with the correct arguments.
    
    Differential Revision: https://reviews.llvm.org/D135729

--- clang/lib/Driver/ToolChains/Clang.cpp
+++ clang/lib/Driver/ToolChains/Clang.cpp
@@ -7901,8 +7901,6 @@ void ClangAs::ConstructJob(Compilation &C, const JobAction &JA,
 
   const llvm::Triple &Triple = getToolChain().getEffectiveTriple();
   const std::string &TripleStr = Triple.getTriple();
-  const Optional<llvm::Triple> TargetVariantTriple =
-      getToolChain().getTargetVariantTriple();
   const auto &D = getToolChain().getDriver();
 
   // Don't warn about "clang -w -c foo.s"
@@ -7920,10 +7918,8 @@ void ClangAs::ConstructJob(Compilation &C, const JobAction &JA,
   // Add the "effective" target triple.
   CmdArgs.push_back("-triple");
   CmdArgs.push_back(Args.MakeArgString(TripleStr));
-  if (TargetVariantTriple) {
-    CmdArgs.push_back("-darwin-target-variant-triple");
-    CmdArgs.push_back(Args.MakeArgString(TargetVariantTriple->getTriple()));
-  }
+
+  getToolChain().addClangCC1ASTargetOptions(Args, CmdArgs);
 
   // Set the output mode, we currently only expect to be used as a real
   // assembler.
