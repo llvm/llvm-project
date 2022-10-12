a48a43d9cb59eed7ea298a55b7cd792d705ee2fb
Peter Klausler <pklausler@nvidia.com>
2022-10-12 12:59:48 -0700

    [flang] Always diagnose incompatible function results when associating function pointers
    There are some exceptional cases where the compiler permits association of
    procedure pointers or dummy arguments with slightly incompatible procedure
    targets, but they should not override any incompatibilty of function
    result types.
    
    (Includes a second fix to resolve the original motivating test failure, in
    which a COMPLEX intrinsic function was getting its result kind divided by
    two due to an implicit C++ conversion of the kind to a "*kind" parse tree
    node, and those legacy "COMPLEX*size" type designators' values are twice
    the type kind value.)
    
    Differential Revision: https://reviews.llvm.org/D136964

--- flang/lib/Semantics/symbol.cpp
+++ flang/lib/Semantics/symbol.cpp
@@ -136,6 +136,9 @@ void EntityDetails::set_type(const DeclTypeSpec &type) {
 void AssocEntityDetails::set_rank(int rank) { rank_ = rank; }
 void EntityDetails::ReplaceType(const DeclTypeSpec &type) { type_ = &type; }
 
+ObjectEntityDetails::ObjectEntityDetails(EntityDetails &&d)
+    : EntityDetails(d) {}
+
 void ObjectEntityDetails::set_shape(const ArraySpec &shape) {
   CHECK(shape_.empty());
   for (const auto &shapeSpec : shape) {
@@ -362,9 +365,6 @@ bool Symbol::IsFromModFile() const {
   return test(Flag::ModFile) ||
       (!owner_->IsTopLevel() && owner_->symbol()->IsFromModFile());
 }
-
-ObjectEntityDetails::ObjectEntityDetails(EntityDetails &&d)
-    : EntityDetails(d) {}
 
 llvm::raw_ostream &operator<<(llvm::raw_ostream &os, const EntityDetails &x) {
   DumpBool(os, "dummy", x.isDummy());
