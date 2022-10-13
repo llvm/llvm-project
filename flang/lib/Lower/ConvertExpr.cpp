3e7fc3880378f0fe8c330b04ea01c11e43c3e45b
Valentin Clement <clementval@gmail.com>
2022-10-13 10:39:29 +0200

    [flang] Embox derived-type directly to fir.class without fir.convert
    non-polymorphic derived-type can call type-bound procedure with passed-object.
    In that case, the derived-type is emboxed in order to be passed to the call.
    Until now the emboxing was done to a fir.box followed by a fir.convert.
    This patch update the createBox function so that we can directly embox to
    a fir.class and avoid the extra fir.convert.
    
    Reviewed By: PeteSteinfeld
    
    Differential Revision: https://reviews.llvm.org/D135790

--- flang/lib/Lower/ConvertExpr.cpp
+++ flang/lib/Lower/ConvertExpr.cpp
@@ -3410,8 +3410,10 @@ public:
           // actually a variable.
           mlir::Value box =
               Fortran::evaluate::IsVariable(*expr)
-                  ? builder.createBox(loc, genBoxArg(*expr))
-                  : builder.createBox(getLoc(), genTempExtAddr(*expr));
+                  ? builder.createBox(loc, genBoxArg(*expr),
+                                      fir::isPolymorphicType(argTy))
+                  : builder.createBox(getLoc(), genTempExtAddr(*expr),
+                                      fir::isPolymorphicType(argTy));
           caller.placeInput(arg, box);
         }
       } else if (arg.passBy == PassBy::AddressAndLength) {
