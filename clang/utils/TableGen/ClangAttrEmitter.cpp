230e541b7212c75371568d8d72a121875db14a80
Volodymyr Sapsai <vsapsai@apple.com>
2022-10-13 19:22:11 -0700

    [Attributes] Improve writing `ExprArgument` value.
    Instead of dumping `Expr*` memory address, output `Expr` representation.
    
    Differential Revision: https://reviews.llvm.org/D135931
    
    (cherry picked from commit 1783253c41f1b0ac378ab7fa4b6fe5547369c6b0)

--- clang/utils/TableGen/ClangAttrEmitter.cpp
+++ clang/utils/TableGen/ClangAttrEmitter.cpp
@@ -1221,6 +1221,13 @@ namespace {
       OS << "      }\n";
     }
 
+    void writeValue(raw_ostream &OS) const override {
+      OS << "\";\n";
+      OS << "    get" << getUpperName()
+         << "()->printPretty(OS, nullptr, Policy);\n";
+      OS << "    OS << \"";
+    }
+
     void writeDump(raw_ostream &OS) const override {}
 
     void writeDumpChildren(raw_ostream &OS) const override {
