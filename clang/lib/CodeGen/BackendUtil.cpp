5c9a264478b3fd22b017065cfb15307a17871fdf
Dmitry Vyukov <dvyukov@google.com>
2022-10-17 15:13:56 +0200

    Use-after-return sanitizer binary metadata
    Currently per-function metadata consists of:
    (start-pc, size, features)
    
    This adds a new UAR feature and if it's set an additional element:
    (start-pc, size, features, stack-args-size)
    
    Reviewed By: melver
    
    Differential Revision: https://reviews.llvm.org/D136078

--- clang/lib/CodeGen/BackendUtil.cpp
+++ clang/lib/CodeGen/BackendUtil.cpp
@@ -234,6 +234,7 @@ getSanitizerBinaryMetadataOptions(const CodeGenOptions &CGOpts) {
   SanitizerBinaryMetadataOptions Opts;
   Opts.Covered = CGOpts.SanitizeBinaryMetadataCovered;
   Opts.Atomics = CGOpts.SanitizeBinaryMetadataAtomics;
+  Opts.UAR = CGOpts.SanitizeBinaryMetadataUAR;
   return Opts;
 }
 
