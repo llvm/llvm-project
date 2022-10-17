a1255dc467f7ce57a966efa76bbbb4ee91d9115a
Dmitry Vyukov <dvyukov@google.com>
2022-10-17 15:13:56 +0200

    Use-after-return sanitizer binary metadata
    Currently per-function metadata consists of:
    (start-pc, size, features)
    
    This adds a new UAR feature and if it's set an additional element:
    (start-pc, size, features, stack-args-size)
    
    Reviewed By: melver
    
    Differential Revision: https://reviews.llvm.org/D136078

--- llvm/lib/CodeGen/TargetPassConfig.cpp
+++ llvm/lib/CodeGen/TargetPassConfig.cpp
@@ -1269,6 +1269,7 @@ void TargetPassConfig::addMachinePasses() {
 
   addPass(&StackMapLivenessID);
   addPass(&LiveDebugValuesID);
+  addPass(&MachineSanitizerBinaryMetadataID);
 
   if (TM->Options.EnableMachineOutliner && getOptLevel() != CodeGenOpt::None &&
       EnableMachineOutliner != RunOutliner::NeverOutline) {
