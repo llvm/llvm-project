9cf924c7168e2eeb5f677cd5c00295c270fd4400
Zahira Ammarguellat <zahira.ammarguellat@intel.com>
2022-10-17 09:36:18 -0400

    Fix LIT test func-attr.c added by https://reviews.llvm.org/D135097.
    Differential Revision: https://reviews.llvm.org/D136084

--- clang/test/CodeGen/func-attr.c
+++ clang/test/CodeGen/func-attr.c
@@ -1,12 +1,12 @@
-// RUN: %clang -c -ffast-math -emit-llvm -S -o - %s \
-// RUN: | FileCheck %s
+// RUN: %clang -c -target x86_64 -ffast-math \
+// RUN: -emit-llvm -S -o - %s | FileCheck %s
 
-// RUN: %clang -c -funsafe-math-optimizations -emit-llvm -S -o - %s \
-// RUN: | FileCheck %s
+// RUN: %clang -c -target x86_64 -funsafe-math-optimizations \
+// RUN: -emit-llvm -S -o - %s | FileCheck %s
 
 float foo(float a, float b) {
   return a+b;
 }
 
-// CHECK: define{{.*}} float @foo(float noundef %{{.*}}, float noundef %{{.*}}) [[FAST_ATTRS:#[0-9]+]]
+// CHECK: define{{.*}} float @foo(float noundef %{{.*}}, float noundef %{{.*}}){{.*}} [[FAST_ATTRS:#[0-9]+]]
 // CHECK: attributes [[FAST_ATTRS]] = { {{.*}} "approx-func-fp-math"="true" {{.*}} "no-signed-zeros-fp-math"="true" "no-trapping-math"="true" {{.*}} "unsafe-fp-math"="true"
