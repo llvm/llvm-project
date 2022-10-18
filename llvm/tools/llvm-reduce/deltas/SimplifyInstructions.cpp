ce3c3cb2912425bb4367bfbe9a4c68a6d6f0a04a
Arthur Eubanks <aeubanks@google.com>
2022-10-17 21:14:05 -0700

    [llvm-reduce] Check if reduction fails/is redundant before invoking oracle
    So we don't over count the number of chunks and do unnecessary work reducing more chunks than exist.
    
    This lowers some random reduction I tested with locally from 250s to 232s.
    
    Reviewed By: arsenm
    
    Differential Revision: https://reviews.llvm.org/D136127

--- llvm/tools/llvm-reduce/deltas/SimplifyInstructions.cpp
+++ llvm/tools/llvm-reduce/deltas/SimplifyInstructions.cpp
@@ -28,11 +28,11 @@ static void extractInstrFromModule(Oracle &O, Module &Program) {
   for (auto &F : Program) {
     for (auto &BB : F) {
       for (auto &Inst : BB) {
-        if (O.shouldKeep())
-          continue;
 
         SimplifyQuery Q(DL, &Inst);
         if (Value *Simplified = simplifyInstruction(&Inst, Q)) {
+          if (O.shouldKeep())
+            continue;
           Inst.replaceAllUsesWith(Simplified);
           InstToDelete.push_back(&Inst);
         }
