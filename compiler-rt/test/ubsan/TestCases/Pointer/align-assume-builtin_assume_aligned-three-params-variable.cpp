db98c06f11ad1996fc0690c812df7854a0bb0aa2
Arthur Eubanks <aeubanks@google.com>
2022-10-18 08:45:15 -0700

    [ubsan][test] Make some tests have shorter file names
    We're hitting path size limits on Windows on these tests. As a
    workaround, make the file names shorter.
    
    Reviewed By: vitalybuka
    
    Differential Revision: https://reviews.llvm.org/D136113

--- compiler-rt/test/ubsan/TestCases/Pointer/align-assume-builtin_assume_aligned-three-params-variable.cpp
+++ compiler-rt/test/ubsan/TestCases/Pointer/align-assume-builtin_assume_aligned-three-params-variable.cpp
@@ -10,17 +10,16 @@
 
 #include <stdlib.h>
 
-char *__attribute__((assume_aligned(0x8000))) passthrough(char *x) {
-  return x;
-}
+volatile long offset;
 
 int main(int argc, char* argv[]) {
-  char *ptr = (char *)malloc(2);
+  char *ptr = (char *)malloc(3);
 
-  passthrough(ptr + 1);
-  // CHECK: {{.*}}alignment-assumption-{{.*}}.cpp:[[@LINE-1]]:3: runtime error: assumption of 32768 byte alignment for pointer of type 'char *' failed
-  // CHECK: {{.*}}alignment-assumption-{{.*}}.cpp:[[@LINE-9]]:22: note: alignment assumption was specified here
-  // CHECK: 0x{{.*}}: note: address is {{.*}} aligned, misalignment offset is {{.*}} byte
+  offset = 1;
+
+  __builtin_assume_aligned(ptr + 2, 0x8000, offset);
+  // CHECK: {{.*}}align-assume-{{.*}}.cpp:[[@LINE-1]]:32: runtime error: assumption of 32768 byte alignment (with offset of 1 byte) for pointer of type 'char *' failed
+  // CHECK: 0x{{.*}}: note: offset address is {{.*}} aligned, misalignment offset is {{.*}} byte
 
   free(ptr);
 
