9ab0827f70a524aa9f94995662d37675cd042f30
Haojian Wu <hokein.wu@gmail.com>
2022-10-18 22:25:46 +0200

    [include-cleaner] Add a data-structure to capture IWYU pragmas.
    PragmaIncludes captures the pragma-based header-mapping information, it is used
    in the "Location => Header" step to determine the final spelling header for a
    symbol (rather than the header directive).
    
    The structure is by design to be used inside the include-cleaner library
    and clangd.
    
    Differential Revision: https://reviews.llvm.org/D136071

--- clang-tools-extra/include-cleaner/lib/Record.cpp
+++ clang-tools-extra/include-cleaner/lib/Record.cpp
@@ -11,8 +11,114 @@
 #include "clang/AST/ASTContext.h"
 #include "clang/AST/DeclGroup.h"
 #include "clang/Basic/SourceManager.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Lex/PPCallbacks.h"
+#include "clang/Lex/Preprocessor.h"
 
 namespace clang::include_cleaner {
+
+// FIXME: this is a mirror of clang::clangd::parseIWYUPragma, move to libTooling
+// to share the code?
+static llvm::Optional<StringRef> parseIWYUPragma(const char *Text) {
+  assert(strncmp(Text, "//", 2) || strncmp(Text, "/*", 2));
+  constexpr llvm::StringLiteral IWYUPragma = " IWYU pragma: ";
+  Text += 2; // Skip the comment start, // or /*.
+  if (strncmp(Text, IWYUPragma.data(), IWYUPragma.size()))
+    return llvm::None;
+  Text += IWYUPragma.size();
+  const char *End = Text;
+  while (*End != 0 && *End != '\n')
+    ++End;
+  return StringRef(Text, End - Text);
+}
+
+class PragmaIncludes::RecordPragma : public PPCallbacks, public CommentHandler {
+public:
+  RecordPragma(const CompilerInstance &CI, PragmaIncludes *Out)
+      : SM(CI.getSourceManager()), Out(Out) {}
+
+  void FileChanged(SourceLocation Loc, FileChangeReason Reason,
+                   SrcMgr::CharacteristicKind FileType,
+                   FileID PrevFID) override {
+    InMainFile = SM.isWrittenInMainFile(Loc);
+  }
+
+  void InclusionDirective(SourceLocation HashLoc, const Token &IncludeTok,
+                          llvm::StringRef FileName, bool IsAngled,
+                          CharSourceRange /*FilenameRange*/,
+                          Optional<FileEntryRef> File,
+                          llvm::StringRef /*SearchPath*/,
+                          llvm::StringRef /*RelativePath*/,
+                          const clang::Module * /*Imported*/,
+                          SrcMgr::CharacteristicKind FileKind) override {
+    if (!InMainFile)
+      return;
+    int HashLine =
+        SM.getLineNumber(SM.getMainFileID(), SM.getFileOffset(HashLoc));
+    if (LastPragmaKeepInMainFileLine == HashLine)
+      Out->ShouldKeep.insert(HashLine);
+  }
+
+  bool HandleComment(Preprocessor &PP, SourceRange Range) override {
+    auto &SM = PP.getSourceManager();
+    auto Pragma = parseIWYUPragma(SM.getCharacterData(Range.getBegin()));
+    if (!Pragma)
+      return false;
+
+    if (Pragma->consume_front("private, include ")) {
+      // We always insert using the spelling from the pragma.
+      if (auto *FE = SM.getFileEntryForID(SM.getFileID(Range.getBegin())))
+        Out->IWYUPublic.insert(
+            {FE->getLastRef().getUniqueID(),
+             Pragma->startswith("<") || Pragma->startswith("\"")
+                 ? Pragma->str()
+                 : ("\"" + *Pragma + "\"").str()});
+      return false;
+    }
+
+    if (InMainFile) {
+      if (!Pragma->startswith("keep"))
+        return false;
+      // Given:
+      //
+      // #include "foo.h"
+      // #include "bar.h" // IWYU pragma: keep
+      //
+      // The order in which the callbacks will be triggered:
+      //
+      // 1. InclusionDirective("foo.h")
+      // 2. handleCommentInMainFile("// IWYU pragma: keep")
+      // 3. InclusionDirective("bar.h")
+      //
+      // This code stores the last location of "IWYU pragma: keep" (or export)
+      // comment in the main file, so that when next InclusionDirective is
+      // called, it will know that the next inclusion is behind the IWYU pragma.
+      LastPragmaKeepInMainFileLine = SM.getLineNumber(
+          SM.getMainFileID(), SM.getFileOffset(Range.getBegin()));
+    }
+    return false;
+  }
+
+private:
+  bool InMainFile = false;
+  const SourceManager &SM;
+  PragmaIncludes *Out;
+  // Track the last line "IWYU pragma: keep" was seen in the main file, 1-based.
+  int LastPragmaKeepInMainFileLine = -1;
+};
+
+void PragmaIncludes::record(const CompilerInstance &CI) {
+  auto Record = std::make_unique<RecordPragma>(CI, this);
+  CI.getPreprocessor().addCommentHandler(Record.get());
+  CI.getPreprocessor().addPPCallbacks(std::move(Record));
+}
+
+llvm::StringRef PragmaIncludes::getPublic(const FileEntry *F) const {
+  auto It = IWYUPublic.find(F->getUniqueID());
+  if (It == IWYUPublic.end())
+    return "";
+  return It->getSecond();
+}
 
 std::unique_ptr<ASTConsumer> RecordedAST::record() {
   class Recorder : public ASTConsumer {
