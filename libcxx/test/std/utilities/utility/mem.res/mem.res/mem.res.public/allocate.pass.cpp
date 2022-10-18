ba907c253d11b89ece38fb6af41fb4a542adcd27
Nikolas Klauser <nikolasklauser@berlin.de>
2022-10-18 21:40:49 +0200

    [libc++][PMR] Move the pmr::memory_resource destructor into the dylib
    This avoids emitting the VTable of `pmr::memory_resource` in every TU.
    
    Reviewed By: ldionne
    
    Spies: EricWF, nemanjai, libcxx-commits
    
    Differential Revision: https://reviews.llvm.org/D136196

--- libcxx/test/std/utilities/utility/mem.res/mem.res/mem.res.public/allocate.pass.cpp
+++ libcxx/test/std/utilities/utility/mem.res/mem.res/mem.res.public/allocate.pass.cpp
@@ -7,6 +7,8 @@
 //===----------------------------------------------------------------------===//
 
 // UNSUPPORTED: c++03, c++11, c++14
+// XFAIL: use_system_cxx_lib && target={{.+}}-apple-macosx10.{{9|10|11|12|13|14|15}}
+// XFAIL: use_system_cxx_lib && target={{.+}}-apple-macosx{{11.0|12.0}}
 
 // test_memory_resource requires RTTI for dynamic_cast
 // UNSUPPORTED: no-rtti
