57f296c457c7fc948797f8dcad7ccb93bc1ae2aa
Nikita Popov <npopov@redhat.com>
2022-10-19 12:57:41 +0200

    [PowerPC] Fix check for ieeelongdouble support
    Clang detects the GCC version from the libdir. However, modern
    GCC versions only include the major version in the libdir
    (something like lib/gcc/powerpc64le-linux-gnu/12/), not all
    version components. For this reason, even though the system has
    a supported libstdcxx, it will still fail the check against the
    12.1.0 version requirement.
    
    Fix this by doing the same thing we do for patch versions: Assume
    that a missing minor version is larger than any specific version.
    
    To allow this to be tested, we need to fix two additional issues:
    First, the GCC toolchain directories used for testing need to
    contain a crtbegin.o file to be properly detected. The existing
    tests actually ended up using a 0.0.0 version, rather the intended
    one. Second, we also need to satisfy the glibc version check based
    on the dynamic linker. To do so, respect the --dyld-prefix argument
    and add the necessary file to the test toolchain directory.
    
    Differential Revision: https://reviews.llvm.org/D136258

--- clang/lib/Driver/ToolChains/Gnu.cpp
+++ clang/lib/Driver/ToolChains/Gnu.cpp
@@ -1884,8 +1884,15 @@ bool Generic_GCC::GCCVersion::isOlderThan(int RHSMajor, int RHSMinor,
                                           StringRef RHSPatchSuffix) const {
   if (Major != RHSMajor)
     return Major < RHSMajor;
-  if (Minor != RHSMinor)
+  if (Minor != RHSMinor) {
+    // Note that versions without a specified minor sort higher than those with
+    // a minor.
+    if (RHSMinor == -1)
+      return true;
+    if (Minor == -1)
+      return false;
     return Minor < RHSMinor;
+  }
   if (Patch != RHSPatch) {
     // Note that versions without a specified patch sort higher than those with
     // a patch.
