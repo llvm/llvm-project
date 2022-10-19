4203439ad97d280064da44bea233f08fa426d6a7
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2022-10-19 14:17:27 -0700

    [CIR][Lifetime] Add option to control the history display limit
    This is useful while investigating the previous invalidation history
    for a given pointer.

--- clang/test/CIR/Transforms/lifetime-check-owner.cpp
+++ clang/test/CIR/Transforms/lifetime-check-owner.cpp
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fclangir -fclangir-lifetime-check="history=all;remarks=all" -clangir-verify-diagnostics -emit-cir %s -o %t.cir
+// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fclangir -fclangir-lifetime-check="history=all;remarks=all;history_limit=1" -clangir-verify-diagnostics -emit-cir %s -o %t.cir
 
 struct [[gsl::Owner(int)]] MyIntOwner {
   int val;
