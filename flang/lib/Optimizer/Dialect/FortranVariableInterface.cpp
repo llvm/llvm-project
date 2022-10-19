875fd9df76ded4a88a3a44b690f290ea98f91705
Jean Perier <jperier@nvidia.com>
2022-10-19 08:55:02 +0200

    [flang] Introduce FortranVariableOpInterface for ops creating variable
    HLFIR will rely on certain operations to create SSA memory values
    that correspond to a Fortran variable. They will hold bounds and type
    parameters information as well as metadata (like Fortran attributes).
    
    This patch adds an interface that for such operations so that Fortran
    variable can be stored, manipulated, and queried regardless of what
    created them. This is so far intended for fir.declare, hlfir.designate
    and hlfir.associate operations.
    It is added to FIR and not HLFIR because fir.declare needs it and it
    does not itself needs any HLFIR concepts.
    
    Unit tests for the interface methods will be added alongside
    fir.declare in the next patch.
    
    Differential Revision: https://reviews.llvm.org/D136151

---
+++ flang/lib/Optimizer/Dialect/FortranVariableInterface.cpp
@@ -0,0 +1,17 @@
+//===-- FortranVariableInterface.cpp.cpp ----------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// Coding style: https://mlir.llvm.org/getting_started/DeveloperGuide/
+//
+//===----------------------------------------------------------------------===//
+
+#include "flang/Optimizer/Dialect/FortranVariableInterface.h"
+
+namespace fir {
+#include "flang/Optimizer/Dialect/FortranVariableInterface.cpp.inc"
+}
