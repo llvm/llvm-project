d07dce75b87e687c5ed31bc5ffd25052895b5aea
Nathan Lanza <nathan@lanza.io>
2022-10-19 21:58:46 -0400

    [CIR] Add a new CastKid for floating conversions

--- clang/lib/CIR/Dialect/IR/CIRDialect.cpp
+++ clang/lib/CIR/Dialect/IR/CIRDialect.cpp
@@ -246,6 +246,12 @@ LogicalResult CastOp::verify() {
       return emitOpError() << "requires !cir.ptr type for source and result";
     return success();
   }
+  case cir::CastKind::floating: {
+    if (!srcType.dyn_cast<mlir::FloatType>() ||
+        !resType.dyn_cast<mlir::FloatType>())
+      return emitOpError() << "requries floating for source and result";
+    return success();
+  }
   }
 
   llvm_unreachable("Unknown CastOp kind?");
