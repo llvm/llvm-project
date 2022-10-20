09f7784fa27065968820a684eadacfe294ba1d06
rkayaith <rkayaith@gmail.com>
2022-10-20 01:04:34 -0400

    [mlir][python] Include anchor op in PassManager constructor
    This adds an extra argument for specifying the pass manager's anchor op,
    with a default of `any`. Previously the anchor was always defaulted to
    `builtin.module`.
    
    Reviewed By: mehdi_amini
    
    Differential Revision: https://reviews.llvm.org/D136406

--- mlir/lib/Bindings/Python/Pass.cpp
+++ mlir/lib/Bindings/Python/Pass.cpp
@@ -56,11 +56,14 @@ void mlir::python::populatePassManagerSubmodule(py::module &m) {
   // Mapping of the top-level PassManager
   //----------------------------------------------------------------------------
   py::class_<PyPassManager>(m, "PassManager", py::module_local())
-      .def(py::init<>([](DefaultingPyMlirContext context) {
-             MlirPassManager passManager =
-                 mlirPassManagerCreate(context->get());
+      .def(py::init<>([](const std::string &anchorOp,
+                         DefaultingPyMlirContext context) {
+             MlirPassManager passManager = mlirPassManagerCreateOnOperation(
+                 context->get(),
+                 mlirStringRefCreate(anchorOp.data(), anchorOp.size()));
              return new PyPassManager(passManager);
            }),
+           py::arg("anchor_op") = py::str("any"),
            py::arg("context") = py::none(),
            "Create a new PassManager for the current (or provided) Context.")
       .def_property_readonly(MLIR_PYTHON_CAPI_PTR_ATTR,
