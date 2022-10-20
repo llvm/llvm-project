b8f8506cd91d1c41f9888d6fa276c76b3d1259bb
Doru Bercea <doru.bercea@amd.com>
2022-10-20 18:59:26 +0000

    Fix use of abs function in C device regions.
    Change-Id: Id8ade91ee3a088655f464153729c91105efe3abb

--- clang/lib/Headers/__clang_hip_math.h
+++ clang/lib/Headers/__clang_hip_math.h
@@ -166,7 +166,6 @@ uint64_t __make_mantissa(const char *__tagp) {
 }
 
 // BEGIN FLOAT
-#if defined(__cplusplus)
 __DEVICE__
 int abs(int __x) {
   int __sgn = __x >> (sizeof(int) * CHAR_BIT - 1);
@@ -182,7 +181,6 @@ long long llabs(long long __x) {
   long long __sgn = __x >> (sizeof(long long) * CHAR_BIT - 1);
   return (__x ^ __sgn) - __sgn;
 }
-#endif
 
 __DEVICE__
 float acosf(float __x) { return __ocml_acos_f32(__x); }
