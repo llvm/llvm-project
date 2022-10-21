3adb24a9a3cdd6f8a4c571e04b33568cf63585e3
Arthur Eubanks <aeubanks@google.com>
2022-10-21 11:05:04 -0700

    [CodeView][clang] Add flag to disable emitting command line into CodeView
    In https://reviews.llvm.org/D80833, there were concerns about
    determinism emitting the commandline into CodeView. We're actually
    hitting these when running clang-cl on Linux (cross compiling) versus on
    Windows (e.g. -fmessage-length being inferred on terminals).
    
    Add -g[no-]codeview-command-line to enable/disable this feature.
    It's still on by default to preserve the current state of clang.
    
    Reviewed By: thakis, rnk
    
    Differential Revision: https://reviews.llvm.org/D136474

--- clang/test/CodeGen/debug-info-codeview-buildinfo.c
+++ clang/test/CodeGen/debug-info-codeview-buildinfo.c
@@ -1,8 +1,12 @@
 // REQUIRES: x86-registered-target
 // RUN: %clang_cl --target=i686-windows-msvc /c /Z7 /Fo%t.obj -- %s
 // RUN: llvm-pdbutil dump --types %t.obj | FileCheck %s
+// RUN: %clang_cl -gcodeview-command-line --target=i686-windows-msvc /c /Z7 /Fo%t.obj -- %s
+// RUN: llvm-pdbutil dump --types %t.obj | FileCheck %s
 // RUN: %clang_cl --target=i686-windows-msvc /c /Z7 /Fo%t.obj -fdebug-compilation-dir=. -- %s
 // RUN: llvm-pdbutil dump --types %t.obj | FileCheck %s --check-prefix RELATIVE
+// RUN: %clang_cl -gno-codeview-command-line --target=i686-windows-msvc /c /Z7 /Fo%t.obj -- %s
+// RUN: llvm-pdbutil dump --types %t.obj | FileCheck %s --check-prefix DISABLE
 
 int main(void) { return 42; }
 
@@ -14,13 +18,21 @@ int main(void) { return 42; }
 // CHECK: 0x[[TOOL:.+]] | LF_STRING_ID [size = {{.+}}] ID: <no type>, String: [[TOOLVAL:.+[\\/]clang.*]]
 // CHECK: 0x[[CMDLINE:.+]] | LF_STRING_ID [size = {{.+}}] ID: <no type>, String: "-cc1
 // CHECK: 0x{{.+}} | LF_BUILDINFO [size = {{.+}}]
-// CHECK:          0x[[PWD]]: `[[PWDVAL]]`
-// CHECK:          0x[[TOOL]]: `[[TOOLVAL]]`
-// CHECK:          0x[[FILEPATH]]: `[[FILEPATHVAL]]`
-// CHECK:          0x[[ZIPDB]]: ``
-// CHECK:          0x[[CMDLINE]]: `"-cc1
+// CHECK-NEXT:          0x[[PWD]]: `[[PWDVAL]]`
+// CHECK-NEXT:          0x[[TOOL]]: `[[TOOLVAL]]`
+// CHECK-NEXT:          0x[[FILEPATH]]: `[[FILEPATHVAL]]`
+// CHECK-NEXT:          0x[[ZIPDB]]: ``
+// CHECK-NEXT:          0x[[CMDLINE]]: `"-cc1
 
 // RELATIVE:                       Types (.debug$T)
 // RELATIVE: ============================================================
 // RELATIVE: 0x{{.+}} | LF_BUILDINFO [size = {{.+}}]
 // RELATIVE:          0x{{.+}}: `.`
+
+// DISABLE-NOT: cc1
+// DISABLE: 0x{{.+}} | LF_BUILDINFO [size = {{.+}}]
+// DISABLE-NEXT:          0x{{.+}}: `{{.*}}`
+// DISABLE-NEXT:          <no type>: ``
+// DISABLE-NEXT:          0x{{.+}}: `{{.*}}`
+// DISABLE-NEXT:          0x{{.+}}: ``
+// DISABLE-NEXT:          <no type>: ``
