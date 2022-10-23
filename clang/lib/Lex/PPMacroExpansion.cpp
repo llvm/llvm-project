c38cc5423acb368a07de905a5733f8809e0d8703
Christopher Di Bella <cjdb.ns@gmail.com>
2022-10-22 22:54:38 -0700

    adds `__reference_constructs_from_temporary`
    This is information that the compiler already has, and should be exposed
    so that the library doesn't need to reimplement the exact same
    functionality.
    
    Differential Revision: https://reviews.llvm.org/D135341

--- clang/lib/Lex/PPMacroExpansion.cpp
+++ clang/lib/Lex/PPMacroExpansion.cpp
@@ -1695,6 +1695,7 @@ void Preprocessor::ExpandBuiltinMacro(Token &Tok) {
               .Case("__array_rank", true)
               .Case("__array_extent", true)
               .Case("__reference_binds_to_temporary", true)
+              .Case("__reference_constructs_from_temporary", true)
 #define TRANSFORM_TYPE_TRAIT_DEF(_, Trait) .Case("__" #Trait, true)
 #include "clang/Basic/TransformTypeTraits.def"
               .Default(false);
