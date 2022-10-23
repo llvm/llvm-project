103bbddde66f4157b52c2b6d7532c1dd0dfcaf94
Tomas Matheson <tomas.matheson@arm.com>
2022-10-23 21:44:21 +0100

    [ARM] Move Triple::getARMCPUForArch into ARMTargetParser
    This is very backend specific so either belongs in Toolchains/ARM or in
    ARMTargetParser. Since it is used in lldb, ARMTargetParser made more sense.
    
    This is part of an effort to move information about ARM/AArch64 architecture
    versions, extensions and CPUs into their respective TargetParsers.
    
    Differential Revision: https://reviews.llvm.org/D137564

--- clang/lib/Driver/ToolChains/Arch/ARM.cpp
+++ clang/lib/Driver/ToolChains/Arch/ARM.cpp
@@ -938,7 +938,7 @@ StringRef arm::getARMCPUForMArch(StringRef Arch, const llvm::Triple &Triple) {
 
   // We need to return an empty string here on invalid MArch values as the
   // various places that call this function can't cope with a null result.
-  return Triple.getARMCPUForArch(MArch);
+  return llvm::ARM::getARMCPUForArch(Triple, MArch);
 }
 
 /// getARMTargetCPU - Get the (LLVM) name of the ARM cpu we are targeting.
@@ -971,7 +971,8 @@ llvm::ARM::ArchKind arm::getLLVMArchKindForARM(StringRef CPU, StringRef Arch,
     if (ArchKind == llvm::ARM::ArchKind::INVALID)
       // In case of generic Arch, i.e. "arm",
       // extract arch from default cpu of the Triple
-      ArchKind = llvm::ARM::parseCPUArch(Triple.getARMCPUForArch(ARMArch));
+      ArchKind =
+          llvm::ARM::parseCPUArch(llvm::ARM::getARMCPUForArch(Triple, ARMArch));
   } else {
     // FIXME: horrible hack to get around the fact that Cortex-A7 is only an
     // armv7k triple if it's actually been specified via "-arch armv7k".
