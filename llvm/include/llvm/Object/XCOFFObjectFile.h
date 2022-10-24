3d639b6eaf0afad169f6d291e2d1e60496d91bec
zhijian <zhijian@ca.ibm.com>
2022-10-24 13:31:01 -0400

    [XCOFF] llvm-readobj support decoding the loader section header field for XCOFF object file.
    Reviewers: James Henderson, Esme Yi
    
    Differential Revision: https://reviews.llvm.org/D134883

--- llvm/include/llvm/Object/XCOFFObjectFile.h
+++ llvm/include/llvm/Object/XCOFFObjectFile.h
@@ -216,8 +216,7 @@ struct LoaderSectionHeader64 {
   support::big64_t OffsetToImpid;
   support::big64_t OffsetToStrTbl;
   support::big64_t OffsetToSymTbl;
-  char Padding[16];
-  support::big32_t OffsetToRelEnt;
+  support::big64_t OffsetToRelEnt;
 };
 
 template <typename AddressType> struct ExceptionSectionEntry {
@@ -494,8 +493,6 @@ private:
 
   DataRefImpl getSectionByType(XCOFF::SectionTypeFlags SectType) const;
   uint64_t getSectionFileOffsetToRawData(DataRefImpl Sec) const;
-  Expected<uintptr_t>
-  getSectionFileOffsetToRawData(XCOFF::SectionTypeFlags SectType) const;
 
   // This returns a pointer to the start of the storage for the name field of
   // the 32-bit or 64-bit SectionHeader struct. This string is *not* necessarily
@@ -637,6 +634,9 @@ public:
 
   int32_t getSectionFlags(DataRefImpl Sec) const;
   Expected<DataRefImpl> getSectionByNum(int16_t Num) const;
+
+  Expected<uintptr_t>
+  getSectionFileOffsetToRawData(XCOFF::SectionTypeFlags SectType) const;
 
   void checkSymbolEntryPointer(uintptr_t SymbolEntPtr) const;
 
