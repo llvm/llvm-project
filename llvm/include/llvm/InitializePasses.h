a527bda520f01174178eaddccf52124ac1b2473f
Fangrui Song <i@maskray.me>
2022-10-25 10:55:29 -0700

    [LegacyPM] Remove DataFlowSanitizerLegacyPass
    Using the legacy PM for the optimization pipeline was deprecated in 13.0.0.
    Following recent changes to remove non-core features of the legacy
    PM/optimization pipeline, remove DataFlowSanitizerLegacyPass.
    
    Differential Revision: https://reviews.llvm.org/D124594

--- llvm/include/llvm/InitializePasses.h
+++ llvm/include/llvm/InitializePasses.h
@@ -117,7 +117,6 @@ void initializeDAHPass(PassRegistry&);
 void initializeDCELegacyPassPass(PassRegistry&);
 void initializeDFAJumpThreadingLegacyPassPass(PassRegistry &);
 void initializeDSELegacyPassPass(PassRegistry&);
-void initializeDataFlowSanitizerLegacyPassPass(PassRegistry &);
 void initializeDeadMachineInstructionElimPass(PassRegistry&);
 void initializeDebugifyMachineModulePass(PassRegistry &);
 void initializeDelinearizationPass(PassRegistry&);
