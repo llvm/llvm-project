2977300ae1ca0109159e2db4e0299dbac63b5c69
Katherine Rasmussen <krasmussen@lbl.gov>
2022-10-25 16:26:59 -0700

    [flang] Add atomic_fetch_and to list of intrinsics
    Add the atomic subroutine, atomic_fetch_and, to the list of
    intrinsic subroutines, add its last dummy argument to a check
    for coindexed-object, and update test.
    
    Reviewed By: jeanPerier
    
    Differential Revision: https://reviews.llvm.org/D136720

--- flang/lib/Evaluate/intrinsics.cpp
+++ flang/lib/Evaluate/intrinsics.cpp
@@ -1134,6 +1134,16 @@ static const IntrinsicInterface intrinsicSubroutine[]{
             {"stat", AnyInt, Rank::scalar, Optionality::optional,
                 common::Intent::Out}},
         {}, Rank::elemental, IntrinsicClass::atomicSubroutine},
+    {"atomic_fetch_and",
+        {{"atom", AtomicInt, Rank::atom, Optionality::required,
+             common::Intent::InOut},
+            {"value", AnyInt, Rank::scalar, Optionality::required,
+                common::Intent::In},
+            {"old", AtomicInt, Rank::scalar, Optionality::required,
+                common::Intent::Out},
+            {"stat", AnyInt, Rank::scalar, Optionality::optional,
+                common::Intent::Out}},
+        {}, Rank::elemental, IntrinsicClass::atomicSubroutine},
     {"atomic_fetch_or",
         {{"atom", AtomicInt, Rank::atom, Optionality::required,
              common::Intent::InOut},
@@ -2670,7 +2680,8 @@ static bool ApplySpecificChecks(SpecificCall &call, FoldingContext &context) {
   } else if (name == "atomic_define") {
     return CheckAtomicDefineAndRef(
         context, call.arguments[0], call.arguments[1], call.arguments[2], name);
-  } else if (name == "atomic_fetch_add" || name == "atomic_fetch_or") {
+  } else if (name == "atomic_fetch_add" || name == "atomic_fetch_and" ||
+      name == "atomic_fetch_or") {
     return CheckForCoindexedObject(context, call.arguments[3], name, "stat");
   } else if (name == "atomic_ref") {
     return CheckAtomicDefineAndRef(
