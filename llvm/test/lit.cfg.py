99e74c8536eef29da62d43afec5891af4b0de135
Paul Robinson <paul.robinson@sony.com>
2022-10-25 09:22:35 -0700

    [lit] Define keyword used by MCJIT test
    test/ExecutionEngine/MCJIT/2005-12-02-TailCallBug.ll has
    'XFAIL: mcjit-ia32' but that feature isn't defined anywhere, so
    the XFAIL can't have any effect.  This has not been a problem
    because apparently there are no 32-bit x86 bots out there.
    
    I found this as part of other work to clean up lit keywords, so
    I figured I would go ahead and fix it.  Verified by hacking my
    lit.site.cfg.py so host_triple = target_triple = "i686-pc-windows-msvc"
    and the test correctly reported XFAIL.

--- llvm/test/lit.cfg.py
+++ llvm/test/lit.cfg.py
@@ -100,6 +100,8 @@ lli_args = []
 # difficult to obtain on Windows.
 if re.search(r'cygwin|windows-gnu|windows-msvc', config.host_triple):
     lli_args = ['-mtriple=' + config.host_triple + '-elf']
+if re.search(r'^i.86', config.host_triple):
+    config.available_features.add('mcjit-ia32')
 
 llc_args = []
 
