c05461eec101124f932575227dd72b9165c20d40
David Sherwood <david.sherwood@arm.com>
2022-10-26 08:50:17 +0000

    [AArch64][SVE2] Add the SVE2.1 while predicate-as-counter instructions
    This patch adds the assembly/disassembly for the following
    predicate-as-counter instructions:
    
    whilelt: While incrementing signed scalar less than scalar
    whilele: While incrementing signed scalar less than or equal to scalar
    whilegt: While incrementing signed scalar greater than scalar
    whilege: While incrementing signed scalar greater than or equal to scalar
    whilelo: While incrementing unsigned scalar lower than scalar
    whilels: While incrementing unsigned scalar lower or same as scalar
    whilehs: While decrementing unsigned scalar higher or same as scalar
    whilehi: While decrementing unsigned scalar higher than scalar
    
    The reference can be found here:
    https://developer.arm.com/documentation/ddi0602/2022-09
    
    Differential Revision: https://reviews.llvm.org/D136750

---
+++ llvm/test/MC/AArch64/SVE2p1/whilele-diagnostics.s
@@ -0,0 +1,30 @@
+// RUN: not llvm-mc -triple=aarch64 -show-encoding -mattr=+sve2p1 2>&1 < %s | FileCheck %s
+
+// --------------------------------------------------------------------------//
+// Invalid Pattern
+
+whilele pn8.b, x0, x0, vlx1
+// CHECK: [[@LINE-1]]:{{[0-9]+}}: error: invalid operand
+// CHECK-NEXT: whilele pn8.b, x0, x0, vlx1
+// CHECK-NOT: [[@LINE-1]]:{{[0-9]+}}:
+
+whilele pn8.b, x0, x0
+// CHECK: [[@LINE-1]]:{{[0-9]+}}: error: too few operands for instruction
+// CHECK-NEXT: whilele pn8.b, x0, x0
+// CHECK-NOT: [[@LINE-1]]:{{[0-9]+}}:
+
+// --------------------------------------------------------------------------//
+// Invalid use of predicate without suffix
+
+whilele pn8, x0, x0, vlx2
+// CHECK: [[@LINE-1]]:{{[0-9]+}}: error: Invalid predicate register, expected PN in range pn8..pn15 with element suffix.
+// CHECK-NEXT: whilele pn8, x0, x0, vlx2
+// CHECK-NOT: [[@LINE-1]]:{{[0-9]+}}:
+
+// --------------------------------------------------------------------------//
+// Out of range Predicate register
+
+whilele pn7.b, x0, x0, vlx2
+// CHECK: [[@LINE-1]]:{{[0-9]+}}: error: Invalid predicate register, expected PN in range pn8..pn15 with element suffix.
+// CHECK-NEXT: whilele pn7.b, x0, x0, vlx2
+// CHECK-NOT: [[@LINE-1]]:{{[0-9]+}}:
