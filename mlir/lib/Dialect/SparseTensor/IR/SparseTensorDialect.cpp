14504cae9a203b56f863031e4a6a6593cdee3bd8
bixia1 <bixia@google.com>
2022-10-26 08:42:44 -0700

    [mlir][sparse] Extend sparse_tensor.push_back to allow push_back a value n times.
    Reviewed By: aartbik, Peiming
    
    Differential Revision: https://reviews.llvm.org/D136653

--- mlir/lib/Dialect/SparseTensor/IR/SparseTensorDialect.cpp
+++ mlir/lib/Dialect/SparseTensor/IR/SparseTensorDialect.cpp
@@ -538,6 +538,22 @@ LogicalResult InsertOp::verify() {
   return success();
 }
 
+void PushBackOp::build(OpBuilder &builder, OperationState &result,
+                       Type outBuffer, Value bufferSizes, Value inBuffer,
+                       Value value, APInt idx) {
+  build(builder, result, outBuffer, bufferSizes, inBuffer, value, idx, Value());
+}
+
+LogicalResult PushBackOp::verify() {
+  Value n = getN();
+  if (n) {
+    auto nValue = dyn_cast_or_null<arith::ConstantIndexOp>(n.getDefiningOp());
+    if (nValue && nValue.value() < 1)
+      return emitOpError("n must be not less than 1");
+  }
+  return success();
+}
+
 LogicalResult CompressOp::verify() {
   RankedTensorType ttp = getTensor().getType().cast<RankedTensorType>();
   if (ttp.getRank() != 1 + static_cast<int64_t>(getIndices().size()))
