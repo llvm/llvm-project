bf531f28f51a32e8f130ca1e6ce8d689315341d8
Matthias Springer <springerm@google.com>
2022-10-27 10:59:52 +0200

    [mlir][vector][bufferize] Implement DestinationStyleOpInterface on TransferWriteOp
    This simplifies the BufferizableOpInterface implementation of vector.transfer_write.
    
    Differential Revision: https://reviews.llvm.org/D136348

--- mlir/include/mlir/Dialect/Vector/IR/VectorOps.h
+++ mlir/include/mlir/Dialect/Vector/IR/VectorOps.h
@@ -21,6 +21,7 @@
 #include "mlir/IR/OpDefinition.h"
 #include "mlir/IR/PatternMatch.h"
 #include "mlir/Interfaces/ControlFlowInterfaces.h"
+#include "mlir/Interfaces/DestinationStyleOpInterface.h"
 #include "mlir/Interfaces/InferTypeOpInterface.h"
 #include "mlir/Interfaces/SideEffectInterfaces.h"
 #include "mlir/Interfaces/VectorInterfaces.h"
