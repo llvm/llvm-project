89c35f6b9b182eb16c5ea8ac8947cfe2c343b63a
Weining Lu <luweining@loongson.cn>
2022-10-27 20:28:56 +0800

    [Clang][LoongArch] Handle -march/-m{single,double,soft}-float/-mfpu options
    This patch adds options -march, -msingle-float, -mdouble-float,
    -msoft-float and -mfpu for LoongArch.
    
    Clang options `msingle_float` and `mdouble_float` are moved from
    `m_mips_Features_Group` to `m_Group` because now more than targets use
    them.
    
    Reference:
    https://github.com/loongson/LoongArch-Documentation/blob/main/docs/LoongArch-toolchain-conventions-EN.adoc
    
    TODO: add -mtune.
    
    Differential Revision: https://reviews.llvm.org/D136146

--- clang/test/Driver/loongarch-default-features.c
+++ clang/test/Driver/loongarch-default-features.c
@@ -1,8 +1,8 @@
 // RUN: %clang --target=loongarch32 -S -emit-llvm %s -o - | FileCheck %s --check-prefix=LA32
 // RUN: %clang --target=loongarch64 -S -emit-llvm %s -o - | FileCheck %s --check-prefix=LA64
 
-// LA32: "target-features"="+d,+f"
-// LA64: "target-features"="+d,+f"
+// LA32-NOT: "target-features"=
+// LA64: "target-features"="+64bit,+d,+f"
 
 /// Dummy function
 int foo(void) {
---
+++ clang/test/Driver/loongarch-mdouble-float.c
@@ -0,0 +1,18 @@
+// RUN: %clang --target=loongarch64 -mdouble-float -fsyntax-only %s -### 2>&1 \
+// RUN:   | FileCheck %s --check-prefix=CC1
+// RUN: %clang --target=loongarch64 -mdouble-float -S -emit-llvm %s -o - \
+// RUN:   | FileCheck %s --check-prefix=IR
+
+// CC1-NOT: "-target-feature"
+// CC1: "-target-feature" "+64bit"
+// CC1-SAME: {{^}} "-target-feature" "+f"
+// CC1-SAME: {{^}} "-target-feature" "+d"
+// CC1-NOT: "-target-feature"
+// CC1: "-target-abi" "lp64d"
+
+// IR: attributes #{{[0-9]+}} ={{.*}}"target-features"="+64bit,+d,+f"
+
+/// Dummy function
+int foo(void) {
+  return  3;
+}
