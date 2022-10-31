2a3b257a935aa49886ccd073695e75b385b3822f
Kshitij Jain <jkshtj@outlook.com>
2022-10-30 17:24:57 -0700

    [JITLink][i386] Adds absolute and pc relative relocation support for ELF/i386.
    This commit adds support for 32 bit absolute and pc relative relocations in
    ELF/i386 objects, along with simple regression tests.
    
    Reviewed By: sgraenitz, lhames
    
    Differential Revision: https://reviews.llvm.org/D135523

--- llvm/lib/ExecutionEngine/JITLink/ELFLinkGraphBuilder.h
+++ llvm/lib/ExecutionEngine/JITLink/ELFLinkGraphBuilder.h
@@ -108,24 +108,49 @@ protected:
   Error graphifySections();
   Error graphifySymbols();
 
-  /// Traverse all matching relocation records in the given section. The handler
-  /// function Func should be callable with this signature:
+  /// Traverse all matching ELFT::Rela relocation records in the given section.
+  /// The handler function Func should be callable with this signature:
   ///   Error(const typename ELFT::Rela &,
   ///         const typename ELFT::Shdr &, Section &)
   ///
-  template <typename RelocHandlerFunction>
-  Error forEachRelocation(const typename ELFT::Shdr &RelSect,
-                          RelocHandlerFunction &&Func,
-                          bool ProcessDebugSections = false);
+  template <typename RelocHandlerMethod>
+  Error forEachRelaRelocation(const typename ELFT::Shdr &RelSect,
+                              RelocHandlerMethod &&Func,
+                              bool ProcessDebugSections = false);
 
-  /// Traverse all matching relocation records in the given section. Convenience
-  /// wrapper to allow passing a member function for the handler.
+  /// Traverse all matching ELFT::Rel relocation records in the given section.
+  /// The handler function Func should be callable with this signature:
+  ///   Error(const typename ELFT::Rel &,
+  ///         const typename ELFT::Shdr &, Section &)
+  ///
+  template <typename RelocHandlerMethod>
+  Error forEachRelRelocation(const typename ELFT::Shdr &RelSect,
+                             RelocHandlerMethod &&Func,
+                             bool ProcessDebugSections = false);
+
+  /// Traverse all matching rela relocation records in the given section.
+  /// Convenience wrapper to allow passing a member function for the handler.
   ///
   template <typename ClassT, typename RelocHandlerMethod>
-  Error forEachRelocation(const typename ELFT::Shdr &RelSect, ClassT *Instance,
-                          RelocHandlerMethod &&Method,
-                          bool ProcessDebugSections = false) {
-    return forEachRelocation(
+  Error forEachRelaRelocation(const typename ELFT::Shdr &RelSect,
+                              ClassT *Instance, RelocHandlerMethod &&Method,
+                              bool ProcessDebugSections = false) {
+    return forEachRelaRelocation(
+        RelSect,
+        [Instance, Method](const auto &Rel, const auto &Target, auto &GS) {
+          return (Instance->*Method)(Rel, Target, GS);
+        },
+        ProcessDebugSections);
+  }
+
+  /// Traverse all matching rel relocation records in the given section.
+  /// Convenience wrapper to allow passing a member function for the handler.
+  ///
+  template <typename ClassT, typename RelocHandlerMethod>
+  Error forEachRelRelocation(const typename ELFT::Shdr &RelSect,
+                             ClassT *Instance, RelocHandlerMethod &&Method,
+                             bool ProcessDebugSections = false) {
+    return forEachRelRelocation(
         RelSect,
         [Instance, Method](const auto &Rel, const auto &Target, auto &GS) {
           return (Instance->*Method)(Rel, Target, GS);
@@ -487,13 +512,12 @@ template <typename ELFT> Error ELFLinkGraphBuilder<ELFT>::graphifySymbols() {
 }
 
 template <typename ELFT>
-template <typename RelocHandlerFunction>
-Error ELFLinkGraphBuilder<ELFT>::forEachRelocation(
-    const typename ELFT::Shdr &RelSect, RelocHandlerFunction &&Func,
+template <typename RelocHandlerMethod>
+Error ELFLinkGraphBuilder<ELFT>::forEachRelaRelocation(
+    const typename ELFT::Shdr &RelSect, RelocHandlerMethod &&Func,
     bool ProcessDebugSections) {
-
   // Only look into sections that store relocation entries.
-  if (RelSect.sh_type != ELF::SHT_RELA && RelSect.sh_type != ELF::SHT_REL)
+  if (RelSect.sh_type != ELF::SHT_RELA)
     return Error::success();
 
   // sh_info contains the section header index of the target (FixupSection),
@@ -527,6 +551,53 @@ Error ELFLinkGraphBuilder<ELFT>::forEachRelocation(
 
   // Let the callee process relocation entries one by one.
   for (const typename ELFT::Rela &R : *RelEntries)
+    if (Error Err = Func(R, **FixupSection, *BlockToFix))
+      return Err;
+
+  LLVM_DEBUG(dbgs() << "\n");
+  return Error::success();
+}
+
+template <typename ELFT>
+template <typename RelocHandlerMethod>
+Error ELFLinkGraphBuilder<ELFT>::forEachRelRelocation(
+    const typename ELFT::Shdr &RelSect, RelocHandlerMethod &&Func,
+    bool ProcessDebugSections) {
+  // Only look into sections that store relocation entries.
+  if (RelSect.sh_type != ELF::SHT_REL)
+    return Error::success();
+
+  // sh_info contains the section header index of the target (FixupSection),
+  // which is the section to which all relocations in RelSect apply.
+  auto FixupSection = Obj.getSection(RelSect.sh_info);
+  if (!FixupSection)
+    return FixupSection.takeError();
+
+  // Target sections have names in valid ELF object files.
+  Expected<StringRef> Name = Obj.getSectionName(**FixupSection);
+  if (!Name)
+    return Name.takeError();
+  LLVM_DEBUG(dbgs() << "  " << *Name << ":\n");
+
+  // Consider skipping these relocations.
+  if (!ProcessDebugSections && isDwarfSection(*Name)) {
+    LLVM_DEBUG(dbgs() << "    skipped (dwarf section)\n\n");
+    return Error::success();
+  }
+
+  // Lookup the link-graph node corresponding to the target section name.
+  auto *BlockToFix = getGraphBlock(RelSect.sh_info);
+  if (!BlockToFix)
+    return make_error<StringError>(
+        "Refencing a section that wasn't added to the graph: " + *Name,
+        inconvertibleErrorCode());
+
+  auto RelEntries = Obj.rels(RelSect);
+  if (!RelEntries)
+    return RelEntries.takeError();
+
+  // Let the callee process relocation entries one by one.
+  for (const typename ELFT::Rel &R : *RelEntries)
     if (Error Err = Func(R, **FixupSection, *BlockToFix))
       return Err;
 
