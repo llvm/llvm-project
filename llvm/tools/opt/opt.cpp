70dc3b811e4926fa2c88bd3b53b29c46fcba1a90
Arthur Eubanks <aeubanks@google.com>
2022-10-31 14:50:38 -0700

    [AggressiveInstCombine] Remove legacy PM pass
    As part of legacy PM optimization pipeline removal.
    
    This shouldn't be used in codegen pipelines so it should be ok to remove.
    
    Reviewed By: asbirlea
    
    Differential Revision: https://reviews.llvm.org/D137116

--- llvm/tools/opt/opt.cpp
+++ llvm/tools/opt/opt.cpp
@@ -465,7 +465,6 @@ int main(int argc, char **argv) {
   initializeAnalysis(Registry);
   initializeTransformUtils(Registry);
   initializeInstCombine(Registry);
-  initializeAggressiveInstCombine(Registry);
   initializeTarget(Registry);
   // For codegen passes, only passes that do IR to IR transformation are
   // supported.
