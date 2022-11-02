79a1d509f7e5c57dbaf6361896a7836d74b3125d
Nuri Amari <nuriamari@meta.com>
2022-11-01 17:48:00 -0700

    Add nested generic LLDB metadata tests
    Add tests to check that LLDB is able to correctly
    read the types of generic classes nested in various
    contexts from instance metadata.
    
    (cherry picked from commit 20d83574795dc73e82bcf05380236d47041cb08e)

---
+++ lldb/test/API/lang/swift/private_nested_generic_class/TestSwiftPrivateNestedGenericClass.py
@@ -0,0 +1,15 @@
+import lldb
+from lldbsuite.test.lldbtest import *
+from lldbsuite.test.decorators import *
+import lldbsuite.test.lldbutil as lldbutil
+import unittest2
+
+
+class SwiftPrivateNestedGenericClassTest(TestBase):
+
+    @swiftTest
+    def test(self):
+        """Tests that a private generic class type nested inside another generic class can be resolved correctly from the instance metadata"""
+        self.build()
+        lldbutil.run_to_source_breakpoint(self, "break here", lldb.SBFileSpec("main.swift"))
+        self.expect("v self", substrs=["A<Int>.B<String>"])
