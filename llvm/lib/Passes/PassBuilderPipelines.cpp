2bcebc23640442e95792b3d04bc994bb0d081eda
serge-sans-paille <sguelton@mozilla.com>
2022-11-02 08:52:29 +0100

    Move "auto-init" instructions to the dominator of their users
    As a result of -ftrivial-auto-var-init, clang generates instructions to
    set alloca'd memory to a given pattern, right after the allocation site.
    In some cases, this (somehow costly) operation could be delayed, leading
    to conditional execution in some cases.
    
    This is not an uncommon situation: it happens ~500 times on the cPython
    code base, and much more on the LLVM codebase. The benefit greatly
    varies on the execution path, but it should not regress on performance.
    
    This is a recommit of a6f1c74a5d0b4aeb26a288fc4ee432b7a0360a03 with
    MemorySSA update fixes.
    
    Differential Revision: https://reviews.llvm.org/D137707

--- llvm/lib/Passes/PassBuilderPipelines.cpp
+++ llvm/lib/Passes/PassBuilderPipelines.cpp
@@ -125,6 +125,7 @@
 #include "llvm/Transforms/Utils/InjectTLIMappings.h"
 #include "llvm/Transforms/Utils/LibCallsShrinkWrap.h"
 #include "llvm/Transforms/Utils/Mem2Reg.h"
+#include "llvm/Transforms/Utils/MoveAutoInit.h"
 #include "llvm/Transforms/Utils/NameAnonGlobals.h"
 #include "llvm/Transforms/Utils/RelLookupTableConverter.h"
 #include "llvm/Transforms/Utils/SimplifyCFGOptions.h"
@@ -656,6 +657,8 @@ PassBuilder::buildFunctionSimplificationPipeline(OptimizationLevel Level,
   FPM.addPass(MemCpyOptPass());
 
   FPM.addPass(DSEPass());
+  FPM.addPass(MoveAutoInitPass());
+
   FPM.addPass(createFunctionToLoopPassAdaptor(
       LICMPass(PTO.LicmMssaOptCap, PTO.LicmMssaNoAccForPromotionCap,
                /*AllowSpeculation=*/true),
@@ -1802,6 +1805,7 @@ PassBuilder::buildLTODefaultPipeline(OptimizationLevel Level,
 
   // Nuke dead stores.
   MainFPM.addPass(DSEPass());
+  MainFPM.addPass(MoveAutoInitPass());
   MainFPM.addPass(MergedLoadStoreMotionPass());
 
   LoopPassManager LPM;
