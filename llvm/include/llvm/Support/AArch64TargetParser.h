2f64bf74ae8f60a0ddfb4c5c8a3de82999fd82f4
David Sherwood <david.sherwood@arm.com>
2022-11-02 14:58:34 +0000

    [AArch64][SVE2] Add the SVE2.1 BF16 instructions
    This patch adds the new FEAT_B16B16 feature as well as the
    assembly/disassembly for all of the B16B16 instructions:
    
    bfadd:   BFloat16 floating-point add vectors
    bfsub:   BFloat16 floating-point subtract vectors
    bfmul:   BFloat16 floating-point multiply vectors
    bfclamp: BFloat16 floating-point clamp to minimum/maximum number
    bfmax:   BFloat16 floating-point maximum
    bfmaxnm: BFloat16 floating-point maximum number
    bfmin:   BFloat16 floating-point minimum
    bfminnm: BFloat16 floating-point minimum number
    bfmla:   BFloat16 floating-point fused multiply-add vectors
    bfmls:   BFloat16 floating-point fused multiply-subtract vectors
    
    The reference can be found here:
    https://developer.arm.com/documentation/ddi0602/2022-09
    
    Differential Revision: https://reviews.llvm.org/D137321

--- llvm/include/llvm/Support/AArch64TargetParser.h
+++ llvm/include/llvm/Support/AArch64TargetParser.h
@@ -75,6 +75,7 @@ enum ArchExtKind : uint64_t {
   AEK_SME2 =        1ULL << 43, // FEAT_SME2
   AEK_SVE2p1 =      1ULL << 44, // FEAT_SVE2p1
   AEK_SME2p1 =      1ULL << 45, // FEAT_SME2p1
+  AEK_B16B16 =      1ULL << 46  // FEAT_B16B16
 };
 
 enum class ArchKind {
