e7252a1732d58031c6ed1c4c6c5d4a10b7c15c0b
Paul Robinson <paul.robinson@sony.com>
2022-11-04 08:36:00 -0700

    [lit][AIX] Convert clang tests to use 'target={{.*}}-aix{{.*}}'
    Part of the project to eliminate special handling for triples in lit
    expressions.
    
    Differential Revision: https://reviews.llvm.org/D137437

--- clang/test/ClangScanDeps/modules-symlink.c
+++ clang/test/ClangScanDeps/modules-symlink.c
@@ -2,7 +2,7 @@
 // RUN: split-file %s %t
 // Unsupported on AIX because we don't support the requisite "__clangast"
 // section in XCOFF yet.
-// UNSUPPORTED: system-windows, aix
+// UNSUPPORTED: system-windows, target={{.*}}-aix{{.*}}
 
 //--- cdb_pch.json
 [
