49143f9d1471e3f01a6d32001676b29274298e22
luxufan <luxufan@iscas.ac.cn>
2022-11-06 10:44:50 +0800

    [IndVars] Forget the SCEV when the instruction has been sunk.
    In the past, the SCEV expression of the sunk instruction was not
    forgetted. This led to the incorrect block dispositions after the
    instruction be sunk.
    
    Fixes https://github.com/llvm/llvm-project/issues/58662
    
    Reviewed By: fhahn
    
    Differential Revision: https://reviews.llvm.org/D137060

--- llvm/lib/Transforms/Scalar/IndVarSimplify.cpp
+++ llvm/lib/Transforms/Scalar/IndVarSimplify.cpp
@@ -1281,7 +1281,7 @@ bool IndVarSimplify::sinkUnusedInvariants(Loop *L) {
 
     MadeAnyChanges = true;
     ToMove->moveBefore(*ExitBlock, InsertPt);
-    SE->forgetBlockAndLoopDispositions(ToMove);
+    SE->forgetValue(ToMove);
     if (Done) break;
     InsertPt = ToMove->getIterator();
   }
