80473e2883bb959f1531012d5dc0f3f8860da3c8
Ayke van Laethem <aykevanlaethem@gmail.com>
2022-11-07 18:46:38 +0100

    [AVR][Clang] Implement __AVR_HAVE_*__ macros
    These macros are defined in avr-gcc and are useful when working with
    assembly. For example, startup code needs to copy the contents of .data
    from flash to RAM, but should use elpm (instead of lpm) on devices with
    more than 64kB flash. Without __AVR_HAVE_ELPM__, there is no way to know
    whether the elpm instruction is supported.
    
    This partially fixes https://github.com/llvm/llvm-project/issues/56157.
    
    Differential Revision: https://reviews.llvm.org/D137572

--- clang/test/Preprocessor/avr-attiny104.c
+++ clang/test/Preprocessor/avr-attiny104.c
@@ -4,5 +4,6 @@
 // CHECK: #define __AVR 1
 // CHECK: #define __AVR_ARCH__ 100
 // CHECK: #define __AVR_ATtiny104__ 1
+// CHECK-NOT: #define __AVR_HAVE_MUL__ 1
 // CHECK: #define __AVR__ 1
 // CHECK: #define __ELF__ 1
