fdf22598862a072fe6136bc0484b5747e68da20d
Mircea Trofin <mtrofin@google.com>
2022-11-07 11:10:58 -0800

    [NFC] Comment in MLInlineAdvisor as to why use  std::map for FPICache

--- llvm/include/llvm/Analysis/MLInlineAdvisor.h
+++ llvm/include/llvm/Analysis/MLInlineAdvisor.h
@@ -69,6 +69,9 @@ private:
   getSkipAdviceIfUnreachableCallsite(CallBase &CB);
   void print(raw_ostream &OS) const override;
 
+  // Using std::map to benefit from its iterator / reference non-invalidating
+  // semantics, which make it easy to use `getCachedFPI` results from multiple
+  // calls without needing to copy to avoid invalidation effects.
   mutable std::map<const Function *, FunctionPropertiesInfo> FPICache;
 
   LazyCallGraph &CG;
