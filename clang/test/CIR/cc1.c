3aa386d08e43cb1114eb166441d82123f1e380a8
Nathan Lanza <nathan@lanza.io>
2022-11-06 22:26:08 -0500

    [CIR] Add a test to ensure `-S` works with -fclangir-enable

--- clang/test/CIR/cc1.c
+++ clang/test/CIR/cc1.c
@@ -2,6 +2,8 @@
 // RUN: FileCheck --input-file=%t.mlir %s -check-prefix=MLIR
 // RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fclangir -emit-llvm %s -o %t.ll
 // RUN: FileCheck --input-file=%t.ll %s -check-prefix=LLVM
+// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fclangir -S %s -o %t.s
+// RUN: FileCheck --input-file=%t.s %s -check-prefix=ASM
 // RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fclangir -emit-obj %s -o %t.o
 // RUN: llvm-objdump -d %t.o | FileCheck %s -check-prefix=OBJ
 // XFAIL: *
@@ -15,6 +17,12 @@ void foo() {}
 //      LLVM: define void @foo()
 // LLVM-NEXT:   ret void,
 // LLVM-NEXT: }
+
+//      ASM: .globl  foo
+// ASM-NEXT: .p2align
+// ASM-NEXT: .type foo,@function
+// ASM-NEXT: foo:
+//      ASM: retq
 
 // OBJ: 0: c3 retq
 
