bfa409a8cefe1d7d3f445fc3cceb9eedd059bd4f
Simon Pilgrim <llvm-dev@redking.me.uk>
2022-11-08 14:35:30 +0000

    [X86] CVTTSS2SI64rm has the same scheduler def as (V)CVTSS2SI64rm
    None of Haswell/Broadwell/Skylake/Icelake treat CVTTSS2SI64rm differently from CVTSS2SI64rm (or the AVX variants)
    
    Confirmed with Agner, uops.info and Intel AoM

--- llvm/test/tools/llvm-mca/X86/SkylakeClient/resources-sse1.s
+++ llvm/test/tools/llvm-mca/X86/SkylakeClient/resources-sse1.s
@@ -225,7 +225,7 @@ xorps       (%rax), %xmm2
 # CHECK-NEXT:  2      6     1.00                        cvttss2si	%xmm0, %ecx
 # CHECK-NEXT:  3      7     1.00                        cvttss2si	%xmm0, %rcx
 # CHECK-NEXT:  3      11    1.00    *                   cvttss2si	(%rax), %ecx
-# CHECK-NEXT:  4      12    1.00    *                   cvttss2si	(%rax), %rcx
+# CHECK-NEXT:  3      11    1.00    *                   cvttss2si	(%rax), %rcx
 # CHECK-NEXT:  1      11    3.00                        divps	%xmm0, %xmm2
 # CHECK-NEXT:  2      17    3.00    *                   divps	(%rax), %xmm2
 # CHECK-NEXT:  1      11    3.00                        divss	%xmm0, %xmm2
@@ -333,7 +333,7 @@ xorps       (%rax), %xmm2
 
 # CHECK:      Resource pressure per iteration:
 # CHECK-NEXT: [0]    [1]    [2]    [3]    [4]    [5]    [6]    [7]    [8]    [9]
-# CHECK-NEXT:  -     24.00  71.33  24.33  32.00  32.00  8.00   33.83  0.50   3.00
+# CHECK-NEXT:  -     24.00  71.33  24.33  32.00  32.00  8.00   32.83  0.50   3.00
 
 # CHECK:      Resource pressure by instruction:
 # CHECK-NEXT: [0]    [1]    [2]    [3]    [4]    [5]    [6]    [7]    [8]    [9]    Instructions:
@@ -368,7 +368,7 @@ xorps       (%rax), %xmm2
 # CHECK-NEXT:  -      -     1.50   0.50    -      -      -      -      -      -     cvttss2si	%xmm0, %ecx
 # CHECK-NEXT:  -      -     1.50   0.50    -      -      -     1.00    -      -     cvttss2si	%xmm0, %rcx
 # CHECK-NEXT:  -      -     1.50   0.50   0.50   0.50    -      -      -      -     cvttss2si	(%rax), %ecx
-# CHECK-NEXT:  -      -     1.50   0.50   0.50   0.50    -     1.00    -      -     cvttss2si	(%rax), %rcx
+# CHECK-NEXT:  -      -     1.50   0.50   0.50   0.50    -      -      -      -     cvttss2si	(%rax), %rcx
 # CHECK-NEXT:  -     3.00   1.00    -      -      -      -      -      -      -     divps	%xmm0, %xmm2
 # CHECK-NEXT:  -     3.00   1.00    -     0.50   0.50    -      -      -      -     divps	(%rax), %xmm2
 # CHECK-NEXT:  -     3.00   1.00    -      -      -      -      -      -      -     divss	%xmm0, %xmm2
