7f70c2d7e60def8584c6fb8ea3be5e8c09bcf95c
Nikita Popov <npopov@redhat.com>
2022-11-08 14:46:09 +0100

    [TableGen] Use MemoryEffects to represent intrinsic memory effects (NFCI)
    The TableGen implementation was using a homegrown implementation of
    FunctionModRefInfo. This switches it to use MemoryEffects instead.
    This makes the code simpler, and will allow exposing the full
    representational power of MemoryEffects in the future. Among other
    things, this will allow us to map IntrHasSideEffects to an
    inaccessiblemem readwrite, rather than just ignoring it entirely
    in most cases.
    
    To avoid layering issues, this moves the ModRef.h header from IR
    to Support, so that it can be included in the TableGen layer.
    
    Differential Revision: https://reviews.llvm.org/D137641

--- llvm/lib/IR/Instructions.cpp
+++ llvm/lib/IR/Instructions.cpp
@@ -30,7 +30,6 @@
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/MDBuilder.h"
 #include "llvm/IR/Metadata.h"
-#include "llvm/IR/ModRef.h"
 #include "llvm/IR/Module.h"
 #include "llvm/IR/Operator.h"
 #include "llvm/IR/Type.h"
@@ -39,6 +38,7 @@
 #include "llvm/Support/Casting.h"
 #include "llvm/Support/ErrorHandling.h"
 #include "llvm/Support/MathExtras.h"
+#include "llvm/Support/ModRef.h"
 #include "llvm/Support/TypeSize.h"
 #include <algorithm>
 #include <cassert>
