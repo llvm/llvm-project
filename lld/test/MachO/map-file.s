213dbdbef0bad835abca0753f9e59b17dc2bcde2
Jez Ng <jezng@fb.com>
2022-11-08 16:33:22 -0500

    [lld-macho] Overhaul map file code
    The previous map file code left out was modeled after LLD-ELF's
    implementation. However, ld64's map file differs quite a bit from
    LLD-ELF's. I've revamped our map file implementation so it is better
    able to emit ld64-style map files.
    
    Notable differences:
    * ld64 doesn't demangle symbols in map files, regardless of whether
      `-demangle` is passed. So we don't have to bother with
      `getSymbolStrings()`.
    * ld64 doesn't emit symbols in cstring sections; it emits just the
      literal values. Moreover, it emits these literal values regardless of
      whether they are labeled with a symbol.
    * ld64 emits map file entries for things that are not strictly symbols,
      such as unwind info, GOT entries, etc. That isn't handled in this
      diff, but this redesign makes them easy to implement.
    
    Additionally, the previous implementation sorted the symbols so as to
    emit them in address order. This was slow and unnecessary -- the symbols
    can already be traversed in address order by walking the list of
    OutputSections. This brings significant speedups. Here's the numbers
    from the chromium_framework_less_dwarf benchmark on my Mac Pro, with the
    `-map` argument added to the response file:
    
                 base            diff           difference (95% CI)
      sys_time   2.922 ± 0.059   2.950 ± 0.085  [  -0.7% ..   +2.5%]
      user_time  11.464 ± 0.191  8.290 ± 0.123  [ -28.7% ..  -26.7%]
      wall_time  11.235 ± 0.175  9.184 ± 0.169  [ -19.3% ..  -17.2%]
      samples    16              23
    
    (It's worth noting that map files are written in parallel with the
    output binary, but they often took longer to write than the binary
    itself.)
    
    Finally, I did further cleanups to the map-file.s test -- there was no
    real need to have a custom-named section. There were also alt_entry
    symbol declarations that had no corresponding definition. Either way,
    neither custom-named sections nor alt_entry symbols trigger special code
    paths in our map file implementation.
    
    Reviewed By: #lld-macho, Roger
    
    Differential Revision: https://reviews.llvm.org/D137368

--- lld/test/MachO/map-file.s
+++ lld/test/MachO/map-file.s
@@ -4,23 +4,24 @@
 # RUN: llvm-mc -filetype=obj -triple=x86_64-apple-darwin %t/test.s -o %t/test.o
 # RUN: llvm-mc -filetype=obj -triple=x86_64-apple-darwin %t/c-string-literal.s -o %t/c-string-literal.o
 
-# RUN: %lld -map %t/map %t/test.o %t/foo.o %t/c-string-literal.o --time-trace -o %t/test
+# RUN: %lld -demangle -map %t/map %t/test.o %t/foo.o %t/c-string-literal.o \
+# RUN:   --time-trace -o %t/test
 # RUN: llvm-objdump --syms --section-headers %t/test > %t/objdump
-# RUN: cat %t/objdump %t/map > %t/out
-# RUN: FileCheck %s < %t/out
+## Check that symbols in cstring sections aren't emitted
+# RUN: cat %t/objdump %t/map | FileCheck %s --implicit-check-not _hello_world
 # RUN: FileCheck %s --check-prefix=MAPFILE < %t/test.time-trace
 
 # CHECK:      Sections:
-# CHECK-NEXT: Idx  Name          Size           VMA           Type
-# CHECK-NEXT: 0    __text        {{[0-9a-f]+}}  [[#%x,TEXT:]] TEXT
-# CHECK-NEXT: 1    obj           {{[0-9a-f]+}}  [[#%x,DATA:]] TEXT
-# CHECK-NEXT: 2    __cstring     {{[0-9a-f]+}}  [[#%x,CSTR:]] DATA
-# CHECK-NEXT: 3    __common      {{[0-9a-f]+}}  [[#%x,BSS:]]  BSS
+# CHECK-NEXT: Idx  Name          Size           VMA               Type
+# CHECK-NEXT: 0    __text        {{[0-9a-f]+}}  [[#%x,TEXT:]]     TEXT
+# CHECK-NEXT: 1    __cstring     {{[0-9a-f]+}}  [[#%x,CSTR:]]     DATA
+# CHECK-NEXT: 2    __common      {{[0-9a-f]+}}  [[#%x,BSS:]]      BSS
 
 # CHECK:      SYMBOL TABLE:
 # CHECK-DAG:  [[#%x,MAIN:]]    g     F __TEXT,__text _main
 # CHECK-DAG:  [[#%x,NUMBER:]]  g     O __DATA,__common _number
-# CHECK-DAG:  [[#%x,FOO:]]     g     F __TEXT,obj _foo
+# CHECK-DAG:  [[#%x,BAR:]]     g     F __TEXT,__text _bar
+# CHECK-DAG:  [[#%x,FOO:]]     g     F __TEXT,__text __ZTIN3foo3bar4MethE
 # CHECK-DAG:  [[#%x,HIWORLD:]] g     O __TEXT,__cstring _hello_world
 # CHECK-DAG:  [[#%x,HIITSME:]] g     O __TEXT,__cstring _hello_its_me
 
@@ -35,43 +36,50 @@
 # CHECK-NEXT: # Sections:
 # CHECK-NEXT: # Address       Size              Segment  Section
 # CHECK-NEXT: 0x[[#%X,TEXT]]  0x{{[0-9A-F]+}}   __TEXT   __text
-# CHECK-NEXT: 0x[[#%X,DATA]]  0x{{[0-9A-F]+}}   __TEXT   obj
 # CHECK-NEXT: 0x[[#%X,CSTR]]  0x{{[0-9A-F]+}}   __TEXT   __cstring
 # CHECK-NEXT: 0x[[#%X,BSS]]   0x{{[0-9A-F]+}}   __DATA   __common
 
 # CHECK-NEXT: # Symbols:
-# CHECK-NEXT: # Address           Size        File   Name
-# CHECK-DAG:  0x[[#%X,MAIN]]      0x00000001  [  1]  _main
-# CHECK-DAG:  0x[[#%X,FOO]]       0x00000001  [  2]  _foo
-# CHECK-DAG:  0x[[#%X,HIWORLD]]   0x0000000E  [  3]  literal string: Hello world!\n
-# CHECK-DAG:  0x[[#%X,HIITSME]]   0x0000000F  [  3]  literal string: Hello, it's me
-# CHECK-DAG:  0x[[#%X,NUMBER]]    0x00000001  [  1]  _number
+# CHECK-NEXT: # Address                Size        File   Name
+# CHECK-DAG:  0x[[#%X,MAIN]]           0x00000001  [  1]  _main
+# CHECK-DAG:  0x[[#%X,BAR]]            0x00000001  [  1]  _bar
+# CHECK-DAG:  0x[[#%X,FOO]]            0x00000001  [  2]  __ZTIN3foo3bar4MethE
+# CHECK-DAG:  0x[[#%X,HIWORLD]]        0x0000000E  [  3]  literal string: Hello world!\n
+# CHECK-DAG:  0x[[#%X,HIITSME]]        0x0000000F  [  3]  literal string: Hello, it's me
+# CHECK-DAG:  0x[[#%X,HIITSME + 0xf]]  0x0000000E  [  3]  literal string: Hello world!\n
+# CHECK-DAG:  0x[[#%X,NUMBER]]         0x00000001  [  1]  _number
 
 # MAPFILE: "name":"Total Write map file"
 
-# RUN: %lld -dead_strip -map %t/stripped-map %t/test.o %t/foo.o %t/c-string-literal.o -o %t/stripped
+# RUN: %lld -demangle -dead_strip -map %t/stripped-map %t/test.o %t/foo.o %t/c-string-literal.o -o %t/stripped
 # RUN: FileCheck --check-prefix=STRIPPED %s < %t/stripped-map
 
 ## C-string literals should be printed as "literal string: <C string literal>"
 # STRIPPED-LABEL: Dead Stripped Symbols:
-# STRIPPED-DAG:   <<dead>> 0x00000001 [  2] _foo
-# STRIPPED-DAG:   <<dead>> 0x0000000E [  3] literal string: Hello world!\n
-# STRIPPED-DAG:   <<dead>> 0x0000000F [  3] literal string: Hello, it's me
-# STRIPPED-DAG:   <<dead>> 0x00000001 [  1] _number
+# STRIPPED-DAG:   <<dead>>	0x00000001	[  1] _bar
+# STRIPPED-DAG:   <<dead>>	0x00000001	[  1] _number
+# STRIPPED-DAG:   <<dead>>	0x00000001	[  2] __ZTIN3foo3bar4MethE
+# STRIPPED-DAG:   <<dead>>	0x0000000E	[  3] literal string: Hello world!\n
+# STRIPPED-DAG:   <<dead>>	0x0000000F	[  3] literal string: Hello, it's me
+# STRIPPED-DAG:   <<dead>>	0x0000000E	[  3] literal string: Hello world!\n
 
 # RUN: %lld --icf=all -map %t/icf-map %t/test.o %t/foo.o %t/c-string-literal.o -o %t/icf
 # RUN: FileCheck --check-prefix=ICF %s < %t/icf-map
 
+## Verify that folded symbols and cstrings have size zero. Note that ld64 prints
+## folded symbols but not folded cstrings; we print both.
+
 # ICF:     Symbols:
-# ICF-DAG: 0x[[#%X,FOO:]]  0x00000000  [  2] _foo
-# ICF-DAG: 0x[[#FOO]]      0x00000001  [  1] _bar
+# ICF-DAG: 0x[[#%X,FOO:]]     0x00000000  [  2] __ZTIN3foo3bar4MethE
+# ICF-DAG: 0x[[#FOO]]         0x00000001  [  1] _bar
+# ICF-DAG: 0x[[#%X,HIWORLD:]] 0x0000000E  [  3]  literal string: Hello world!\n
+# ICF-DAG: 0x[[#%X,HIWORLD]]  0x00000000  [  3]  literal string: Hello world!\n
 
 #--- foo.s
-## ICF will only fold sections marked as pure_instructions
-.section __TEXT,obj,regular,pure_instructions
-.globl _foo
-.alt_entry _alt_foo
-_foo:
+.globl __ZTIN3foo3bar4MethE
+## This C++ symbol makes it clear that we do not print the demangled name in
+## the map file, even if `-demangle` is passed.
+__ZTIN3foo3bar4MethE:
   nop
 
 .subsections_via_symbols
@@ -79,12 +87,10 @@ _foo:
 #--- test.s
 .comm _number, 1
 .globl _main, _bar
-.alt_entry _alt_bar
 
 _main:
   ret
 
-.section __TEXT,obj,regular,pure_instructions
 _bar:
   nop
 
@@ -100,5 +106,7 @@ _hello_world:
 
 _hello_its_me:
 .asciz "Hello, it's me"
+
+.asciz "Hello world!\n"
 
 .subsections_via_symbols
