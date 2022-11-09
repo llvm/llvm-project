8a084f68c8a1cac99d9fa0fbcf490b53ab3b6e0c
Tom Stellard <tstellar@redhat.com>
2022-11-09 11:32:26 -0800

    Revert "Move googletest to the third-party directory"
    This reverts commit 59052468c3e38cab15582cefbb5133fd4c2ffce5.
    
    It looks like this patch breaks the build when compiler-rt is passed to
    LLVM_ENABLE_PROJECTS instead of LLVM_ENABLE_RUNTIMES.

--- lldb/cmake/modules/LLDBStandalone.cmake
+++ lldb/cmake/modules/LLDBStandalone.cmake
@@ -100,8 +100,8 @@ include_directories(
 if(LLDB_INCLUDE_TESTS)
   # Build the gtest library needed for unittests, if we have LLVM sources
   # handy.
-  if (EXISTS ${LLVM_THIRD_PARTY_DIR}/unittest AND NOT TARGET llvm_gtest)
-    add_subdirectory(${LLVM_THIRD_PARTY_DIR}}/utils/unittest third-party/unittest)
+  if (EXISTS ${LLVM_MAIN_SRC_DIR}/utils/unittest AND NOT TARGET llvm_gtest)
+    add_subdirectory(${LLVM_MAIN_SRC_DIR}/utils/unittest utils/unittest)
   endif()
   # LLVMTestingSupport library is needed for Process/gdb-remote.
   if (EXISTS ${LLVM_MAIN_SRC_DIR}/lib/Testing/Support
