39998dc48e1102c38bb4d55165e7cecb3907fa8c
Konstantin Zhuravlyov <kzhuravl_dev@outlook.com>
2022-11-09 12:07:42 -0500

    Revert "Revert commit:"
    This reverts commit 86a7072fd716bdb5bf29825aafa6a913ce5e0540.
    
    Needs some changes in MIOpen
    
    Change-Id: I8f4b6ad3b09570f04c6cfc0ea30791ed90613261

--- clang-tools-extra/unittests/clang-tidy/ClangTidyOptionsTest.cpp
+++ clang-tools-extra/unittests/clang-tidy/ClangTidyOptionsTest.cpp
@@ -313,8 +313,9 @@ TEST(CheckOptionsValidation, ValidIntOptions) {
   CheckOptions["test.BoolIFalseValue"] = "0";
   CheckOptions["test.BoolTrueValue"] = "true";
   CheckOptions["test.BoolFalseValue"] = "false";
-  CheckOptions["test.BoolTrueShort"] = "Y";
-  CheckOptions["test.BoolFalseShort"] = "N";
+// FIXME: SWDEV-268185.
+//  CheckOptions["test.BoolTrueShort"] = "Y";
+//  CheckOptions["test.BoolFalseShort"] = "N";
   CheckOptions["test.BoolUnparseable"] = "Nothing";
 
   ClangTidyContext Context(std::make_unique<DefaultOptionsProvider>(
@@ -336,8 +337,9 @@ TEST(CheckOptionsValidation, ValidIntOptions) {
   CHECK_VAL(TestCheck.getIntLocal<bool>("BoolIFalseValue"), false);
   CHECK_VAL(TestCheck.getIntLocal<bool>("BoolTrueValue"), true);
   CHECK_VAL(TestCheck.getIntLocal<bool>("BoolFalseValue"), false);
-  CHECK_VAL(TestCheck.getIntLocal<bool>("BoolTrueShort"), true);
-  CHECK_VAL(TestCheck.getIntLocal<bool>("BoolFalseShort"), false);
+// FIXME: SWDEV-268185.
+//  CHECK_VAL(TestCheck.getIntLocal<bool>("BoolTrueShort"), true);
+//  CHECK_VAL(TestCheck.getIntLocal<bool>("BoolFalseShort"), false);
   EXPECT_FALSE(TestCheck.getIntLocal<bool>("BoolUnparseable"));
 
   EXPECT_THAT(
