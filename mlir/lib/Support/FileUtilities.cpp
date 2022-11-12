85c3bee399bbc53e6d6ea3a150366c5323c834c3
River Riddle <riddleriver@gmail.com>
2022-11-12 14:38:27 -0800

    [mlir] Add openInputFile overload that accepts the expected alignment
    This just forwards to the alignment parameter on `MemoryBuffer::getFileOrSTDIN`.

--- mlir/lib/Support/FileUtilities.cpp
+++ mlir/lib/Support/FileUtilities.cpp
@@ -18,9 +18,12 @@
 
 using namespace mlir;
 
-std::unique_ptr<llvm::MemoryBuffer>
-mlir::openInputFile(StringRef inputFilename, std::string *errorMessage) {
-  auto fileOrErr = llvm::MemoryBuffer::getFileOrSTDIN(inputFilename);
+static std::unique_ptr<llvm::MemoryBuffer>
+openInputFileImpl(StringRef inputFilename, std::string *errorMessage,
+                  Optional<llvm::Align> alignment) {
+  auto fileOrErr = llvm::MemoryBuffer::getFileOrSTDIN(
+      inputFilename, /*IsText=*/false, /*RequiresNullTerminator=*/true,
+      alignment);
   if (std::error_code error = fileOrErr.getError()) {
     if (errorMessage)
       *errorMessage = "cannot open input file '" + inputFilename.str() +
@@ -29,6 +32,16 @@ mlir::openInputFile(StringRef inputFilename, std::string *errorMessage) {
   }
 
   return std::move(*fileOrErr);
+}
+std::unique_ptr<llvm::MemoryBuffer>
+mlir::openInputFile(StringRef inputFilename, std::string *errorMessage) {
+  return openInputFileImpl(inputFilename, errorMessage,
+                           /*alignment=*/llvm::None);
+}
+std::unique_ptr<llvm::MemoryBuffer>
+mlir::openInputFile(llvm::StringRef inputFilename, llvm::Align alignment,
+                    std::string *errorMessage) {
+  return openInputFileImpl(inputFilename, errorMessage, alignment);
 }
 
 std::unique_ptr<llvm::ToolOutputFile>
