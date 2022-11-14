77bf0df376d7fcd305d9576511e7d0806b96971a
Fangrui Song <i@maskray.me>
2022-11-14 15:51:03 -0800

    Revert "[opt][clang] Enable using -module-summary/-flto=thin with -S/-emit-llvm"
    This reverts commit bf8381a8bce28fc69857645cc7e84a72317e693e.
    
    There is a layering violation: LLVMAnalysis depends on LLVMCore, so
    LLVMCore should not include LLVMAnalysis header
    llvm/Analysis/ModuleSummaryAnalysis.h

--- llvm/lib/Analysis/ModuleSummaryAnalysis.cpp
+++ llvm/lib/Analysis/ModuleSummaryAnalysis.cpp
@@ -880,6 +880,8 @@ ModuleSummaryIndex llvm::buildModuleSummaryIndex(
   return Index;
 }
 
+AnalysisKey ModuleSummaryIndexAnalysis::Key;
+
 ModuleSummaryIndex
 ModuleSummaryIndexAnalysis::run(Module &M, ModuleAnalysisManager &AM) {
   ProfileSummaryInfo &PSI = AM.getResult<ProfileSummaryAnalysis>(M);
