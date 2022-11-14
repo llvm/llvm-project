a7097f5e953ea77c09766ac063b62442ba2ec3d9
Muhammad Omair Javaid <omair.javaid@linaro.org>
2022-11-14 12:27:11 +0400

    Revert "[libclang] Expose completion result kind in `CXCompletionResult`"
    This reverts commit 2e911d23c92f68ccd2030b5591ab79bdb67ca849.
    It breaks clang-armv8-quick buildbot:
    https://lab.llvm.org/buildbot/#/builders/245/builds/761

--- clang/tools/c-index-test/c-index-test.c
+++ clang/tools/c-index-test/c-index-test.c
@@ -2512,10 +2512,7 @@ static void print_completion_result(CXTranslationUnit translation_unit,
                                     unsigned index,
                                     FILE *file) {
   CXCompletionResult *completion_result = completion_results->Results + index;
-  CXString ks =
-      completion_result->CursorKind == CXCursor_NotImplemented
-          ? clang_getCompletionResultKindSpelling(completion_result->ResultKind)
-          : clang_getCursorKindSpelling(completion_result->CursorKind);
+  CXString ks = clang_getCursorKindSpelling(completion_result->CursorKind);
   unsigned annotationCount;
   enum CXCursorKind ParentKind;
   CXString ParentName;
