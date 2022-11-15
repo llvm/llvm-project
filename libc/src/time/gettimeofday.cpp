66fa1374bc3aa364b97dc7ee972c1677644b1c18
Raman Tenneti <rtenneti@google.com>
2022-11-15 15:26:36 -0800

    [libc] [Obvious] Cleanup.
    + Avoid the `clockid` var and just passed `long(CLOCK_REALTIME)`
      to the syscall.
    
    Reviewed By: rtenneti
    
    Differential Revision: https://reviews.llvm.org/D138070

--- libc/src/time/gettimeofday.cpp
+++ libc/src/time/gettimeofday.cpp
@@ -20,11 +20,10 @@ LLVM_LIBC_FUNCTION(int, gettimeofday,
                    (struct timeval * tv, [[maybe_unused]] void *unused)) {
   if (tv == nullptr)
     return 0;
-  clockid_t clockid = CLOCK_REALTIME;
   struct timespec tp;
-  long ret_val =
-      __llvm_libc::syscall_impl(SYS_clock_gettime, static_cast<long>(clockid),
-                                reinterpret_cast<long>(&tp));
+  long ret_val = __llvm_libc::syscall_impl(SYS_clock_gettime,
+                                           static_cast<long>(CLOCK_REALTIME),
+                                           reinterpret_cast<long>(&tp));
   // A negative return value indicates an error with the magnitude of the
   // value being the error code.
   if (ret_val < 0) {
