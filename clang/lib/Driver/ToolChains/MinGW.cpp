7954fec0e1a3fb2bda49687aead67c8deccd4b00
Louis Dionne <ldionne.2@gmail.com>
2022-11-15 16:41:55 -0500

    [clang] Don't include C++ Standard Library headers when -nostdinc is used
    This is a follow-up to 6bcf1090f, which made the same change but only
    for GNU. It seems that we should try to provide a consistent behavior
    across all targets.
    
    This fixes an issue where clang/test/Driver/nostdincxx.cpp would start
    failing on non-GNU targets because that test was too loose in its checks.
    It would only check that 'file not found' was part of the error message,
    but didn't ensure that the file we had not found was <vector>.
    
    Differential Revision: https://reviews.llvm.org/D138062

--- clang/lib/Driver/ToolChains/MinGW.cpp
+++ clang/lib/Driver/ToolChains/MinGW.cpp
@@ -644,8 +644,8 @@ void toolchains::MinGW::addClangTargetOptions(
 
 void toolchains::MinGW::AddClangCXXStdlibIncludeArgs(
     const ArgList &DriverArgs, ArgStringList &CC1Args) const {
-  if (DriverArgs.hasArg(options::OPT_nostdlibinc) ||
-      DriverArgs.hasArg(options::OPT_nostdincxx))
+  if (DriverArgs.hasArg(options::OPT_nostdinc, options::OPT_nostdlibinc,
+                        options::OPT_nostdincxx))
     return;
 
   StringRef Slash = llvm::sys::path::get_separator();
