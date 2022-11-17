507dc9d26f7754200c472a5deefccbf00efbafa5
Dominik Adamski <dominik.adamski@amd.com>
2022-11-17 04:55:58 -0600

    [OpenMP][Classic Flang]Add Fortran modulo function
    Add definition of Fortran modulo functions to the OpenMP device math
    library.
    
    Currently classic Flang supports runtime functions on GPU which have C
    equivalent or they are directly translated into llvm intrinsics.
    Unfortunately, some Fortran runtime functions have no C equivalent
    and they need to be ported separately.
    
    Change-Id: Ia7d517ef24f9397f7922fddd320952bf1ee08cfa

--- openmp/libomptarget/libm/src/libm.c
+++ openmp/libomptarget/libm/src/libm.c
@@ -21,6 +21,54 @@
 #define __OPENMP_AMDGCN__
 #include <__clang_cuda_complex_builtins.h>
 #include <__clang_hip_math.h>
+
+#ifndef FORTRAN_NO_LONGER_NEEDS
+// Attach Fortran runtimes which are used by Classic Flang
+double __f90_dmodulov(double a, double p) {
+  double d;
+  d = fmod(a, p);
+  if (d != 0 && ((a < 0 && p > 0) || (a > 0 && p < 0)))
+    d += p;
+  return d;
+}
+
+float __f90_amodulov(float a, float p) { return __f90_dmodulov(a, p); }
+
+int32_t __f90_modulov(int32_t a, int32_t p) {
+  int32_t q, r;
+
+  q = a / p;
+  r = a - q * p;
+  if (r != 0 && (a ^ p) < 0) { /* signs differ */
+    r += p;
+  }
+  return r;
+}
+
+int64_t __f90_i8modulov_i8(int64_t a, int64_t p) {
+  int64_t q, r;
+
+  q = a / p;
+  r = a - q * p;
+  if (r != 0 && (a ^ p) < 0) { /* signs differ */
+    r += (p);
+  }
+  return r;
+}
+
+int16_t __f90_imodulov(int16_t a, int16_t p) {
+  int32_t q, r;
+
+  q = a / p;
+  r = a - q * p;
+  if (r != 0 && (a ^ p) < 0) { /* signs differ */
+    r += p;
+  }
+  return r;
+}
+
+#endif
+
 #pragma omp end declare target
 #endif
 
