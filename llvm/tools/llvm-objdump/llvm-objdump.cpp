603bab690a3216303a436cd579803f25c4186cec
Kristina Bessonova <kbessonova@accesssoftek.com>
2022-11-18 19:56:51 +0200

    [llvm-objdump] Avoid using mapping symbols as branch target labels
    The main motivation for this change is to avoid ambiguity because
    mapping symbol names may not be unique across a binary and do not allow uniquely
    identifying target address. So that mapping symbols used as branch target
    labels make llvm-objdump output less readable.
    
    Another point is that mapping symbols sometimes appear in
    non-allocatable sections, like debug info sections which make objdump
    output even more confusing.
    
    For example, a small AArch64 executable may contain plenty of `$d[.*]`
    symbols and none of them would be useful as a label for resolving
    a branch or a memory operand target address:
    
    ```
      0000000000000254 l       .note.ABI-tag	0000000000000000 $d
      00000000000008d4 l       .eh_frame            0000000000000000 $d
      0000000000000868 l       .rodata              0000000000000000 $d
      0000000000011028 l       .data                0000000000000000 $d
      0000000000010db8 l       .fini_array          0000000000000000 $d
      0000000000010db0 l       .init_array          0000000000000000 $d
      00000000000008e8 l       .eh_frame            0000000000000000 $d
      0000000000011034 l       .bss                 0000000000000000 $d
    ```
    
    Note that GNU objdump doesn't use mapping symbols as branch target
    labels for all targets that support such symbols (ARM, AArch64, CSKY).
    
    Differential Revision: https://reviews.llvm.org/D139131

--- llvm/tools/llvm-objdump/llvm-objdump.cpp
+++ llvm/tools/llvm-objdump/llvm-objdump.cpp
@@ -463,6 +463,11 @@ static bool hasMappingSymbols(const ObjectFile &Obj) {
   return isArmElf(Obj) || isAArch64Elf(Obj) || isCSKYElf(Obj) ;
 }
 
+static bool isMappingSymbol(const SymbolInfoTy &Sym) {
+  return Sym.Name.startswith("$d") || Sym.Name.startswith("$x") ||
+         Sym.Name.startswith("$a") || Sym.Name.startswith("$t");
+}
+
 static void printRelocation(formatted_raw_ostream &OS, StringRef FileName,
                             const RelocationRef &Rel, uint64_t Address,
                             bool Is64Bits) {
@@ -1877,10 +1882,17 @@ static void disassembleObject(const Target *TheTarget, ObjectFile &Obj,
                 auto It = llvm::partition_point(
                     *TargetSymbols,
                     [=](const SymbolInfoTy &O) { return O.Addr <= Target; });
-                if (It != TargetSymbols->begin()) {
-                  TargetSym = &*(It - 1);
-                  break;
+                while (It != TargetSymbols->begin()) {
+                  --It;
+                  // Skip mapping symbols to avoid possible ambiguity as they
+                  // do not allow uniquely identifying the target address.
+                  if (!hasMappingSymbols(Obj) || !isMappingSymbol(*It)) {
+                    TargetSym = &*It;
+                    break;
+                  }
                 }
+                if (TargetSym)
+                  break;
               }
 
               // Print the labels corresponding to the target if there's any.
