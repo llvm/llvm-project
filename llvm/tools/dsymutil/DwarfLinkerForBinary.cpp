fac6fa2063632e1c4f69bc5d80c4e5fb69838d34
Alexey Lapshin <a.v.lapshin@mail.ru>
2022-11-20 20:03:51 +0100

    [DWARFLinker][NFC] Change interface of DWARFLinker to specify accel table kinds explicitly.
    Currently, DWARFLinker receives kind of accel tables as predefined sets:
    
    ```
      Apple,   ///< .apple_names, .apple_namespaces, .apple_types, .apple_objc.
      Dwarf,   ///< DWARF v5 .debug_names.
      Default, ///< Dwarf for DWARF5 or later, Apple otherwise.
      Pub,     ///< .debug_pubnames, .debug_pubtypes
    ```
    
    This patch removes implicit sets of tables(Default, Dwarf) and allows to ask for several sets:
    
    ```
      Apple,     ///< .apple_names, .apple_namespaces, .apple_types, .apple_objc.
      Pub,       ///< .debug_pubnames, .debug_pubtypes
      DebugNames ///< .debug_names.
    ```
    
    It allows seamlessness adding more accel tables in the future: .gdb_index, .debug_cu_index...
    Doing things that way, DWARFLinker will be independent of consumers' requirements.
    f.e. dsymutil and llvm-dwarfutil may have different variants for Default set
    (so, instead of implementing these differencies inside DWARFLinker it could be
    implemented in the corresponding module).
    
    Differential Revision: https://reviews.llvm.org/D132371

--- llvm/tools/dsymutil/DwarfLinkerForBinary.cpp
+++ llvm/tools/dsymutil/DwarfLinkerForBinary.cpp
@@ -579,7 +579,6 @@ bool DwarfLinkerForBinary::link(const DebugMap &Map) {
   GeneralLinker.setNoODR(Options.NoODR);
   GeneralLinker.setUpdate(Options.Update);
   GeneralLinker.setNumThreads(Options.Threads);
-  GeneralLinker.setAccelTableKind(Options.TheAccelTableKind);
   GeneralLinker.setPrependPath(Options.PrependPath);
   GeneralLinker.setKeepFunctionForStatic(Options.KeepFunctionForStatic);
   if (Options.Translator)
@@ -726,6 +725,27 @@ bool DwarfLinkerForBinary::link(const DebugMap &Map) {
 
   if (Error E = GeneralLinker.setTargetDWARFVersion(MaxDWARFVersion))
     return error(toString(std::move(E)));
+
+  switch (Options.TheAccelTableKind) {
+  case DsymutilAccelTableKind::Apple:
+    GeneralLinker.addAccelTableKind(DwarfLinkerAccelTableKind::Apple);
+    break;
+  case DsymutilAccelTableKind::Dwarf:
+    GeneralLinker.addAccelTableKind(DwarfLinkerAccelTableKind::DebugNames);
+    break;
+  case DsymutilAccelTableKind::Pub:
+    GeneralLinker.addAccelTableKind(DwarfLinkerAccelTableKind::Pub);
+    break;
+  case DsymutilAccelTableKind::Default:
+    if (MaxDWARFVersion >= 5)
+      GeneralLinker.addAccelTableKind(DwarfLinkerAccelTableKind::DebugNames);
+    else
+      GeneralLinker.addAccelTableKind(DwarfLinkerAccelTableKind::Apple);
+    break;
+  case DsymutilAccelTableKind::None:
+    // Nothing to do.
+    break;
+  }
 
   // link debug info for loaded object files.
   if (Error E = GeneralLinker.link())
