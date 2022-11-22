7a1cd932f1882cb1dbc2904304a3cea214ecc7d4
Martin Storsjö <martin@martin.st>
2022-11-22 14:38:54 +0200

    [clang] [MinGW] Improve/extend the gcc/sysroot detection logic
    There are three functions that try to detect the right implicit
    sysroot and libgcc directory setup to use
    - One which looks for mingw sysroots located in
      <clangbin>/../<sysrootname>
    - One which looks for a mingw-targeting gcc executables in the PATH
    - One which looks in the <gccroot>/lib/gcc directory to find the
      right one to use, and the right specific triple used for arch
      specific directories in the gcc/libstdc++ install
    
    These have mostly tried to look for executables named
    "<arch>-w64-mingw32-gcc" or "mingw32-gcc" or subdirectories
    named "<arch>-w64-mingw32" or "mingw32".
    
    In the case of findClangRelativeSysroot, it also has looked
    for directories with the name of the actual triple. This
    was added in deff7536278d355977171726124f83aa4bb95419,
    with the intent of looking for a directory matching exactly
    the user provided literal triple - however the triple here
    is the normalized one, not the one provided by the user on
    the command line.
    
    Improve and unify this logic somewhat:
    - Always first look for things based on the literal triple
      provided by the user.
    - Secondly look for things based on the normalized triple
      (which usually ends up as e.g. x86_64-w64-windows-gnu),
      accessed via the Triple which is passed to the constructor
    - Then look for the common triple form <arch>-w64-mingw32
    
    The literal triple provided by the user is available via
    Driver::getTargetTriple(), but computeTargetTriple() may
    change e.g. the architecture of it, so we need to
    reapply the effective architecture on the literal triple
    spelling from Driver::getTargetTriple().
    
    Do this consistently for all of findGcc, findClangRelativeSysroot
    and findGccLibDir (while keeping the existing plain "mingw32"
    cases in findGcc and findGccLibDir too).
    
    Fedora 37 started shipping mingw sysroots targeting UCRT,
    in addition to the traditional msvcrt.dll, and these use
    triples in the form <arch>-w64-mingw32ucrt - see
    https://fedoraproject.org/wiki/Changes/F37MingwUCRT.
    
    Thus, in addition to the existing default tested triples,
    try looking for triples in the form <arch>-w64-mingw32ucrt,
    to automatically find the UCRT sysroots on Fedora 37.
    By explicitly setting a specific target on the Clang command
    line, the user can be more explicit with which flavour is
    to be preferred.
    
    This should fix the main issue in
    https://github.com/llvm/llvm-project/issues/59001.
    
    Differential Revision: https://reviews.llvm.org/D138692

--- clang/test/Driver/mingw.cpp
+++ clang/test/Driver/mingw.cpp
@@ -39,6 +39,10 @@
 // CHECK_MINGW_OPENSUSE_TREE: "[[BASE]]/Inputs/mingw_opensuse_tree/usr{{/|\\\\}}x86_64-w64-mingw32/sys-root/mingw/include"
 
 
+// RUN: %clang -target x86_64-pc-windows-gnu -rtlib=platform -stdlib=libstdc++ -c -### --sysroot=%S/Inputs/mingw_fedora_tree/usr %s 2>&1 | FileCheck -check-prefix=CHECK_MINGW_FEDORA_TREE %s
+// CHECK_MINGW_FEDORA_TREE: "[[BASE:[^"]+]]/Inputs/mingw_fedora_tree/usr{{/|\\\\}}x86_64-w64-mingw32ucrt/sys-root/mingw/include"
+
+
 // RUN: %clang -target i686-pc-windows-gnu -rtlib=platform -stdlib=libstdc++ -c -### --sysroot=%S/Inputs/mingw_arch_tree/usr %s 2>&1 | FileCheck -check-prefix=CHECK_MINGW_ARCH_TREE %s
 // CHECK_MINGW_ARCH_TREE: "[[BASE:[^"]+]]/Inputs/mingw_arch_tree/usr{{/|\\\\}}i686-w64-mingw32{{/|\\\\}}include{{/|\\\\}}c++{{/|\\\\}}5.1.0"
 // CHECK_MINGW_ARCH_TREE: "[[BASE]]/Inputs/mingw_arch_tree/usr{{/|\\\\}}i686-w64-mingw32{{/|\\\\}}include{{/|\\\\}}c++{{/|\\\\}}5.1.0{{/|\\\\}}i686-w64-mingw32"
