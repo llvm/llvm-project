1ad6f3fea85a6e4383e2e6ed3ad800f9a10ffcd1
Yi Kong <yikong@google.com>
2022-11-22 20:11:42 +0900

    Revert "[libc++] Remove workarounds for systems that used to require __need_XXX macros"
    This reverts commit 4e5b38de0322607d315febc59fea5a29607dd0af.
    
    The change broke multiple builders.

--- libcxx/include/stdbool.h
+++ libcxx/include/stdbool.h
@@ -6,7 +6,6 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
-
 #ifndef _LIBCPP_STDBOOL_H
 #define _LIBCPP_STDBOOL_H
 
