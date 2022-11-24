d056632fc860edf1471e5ef11ef0fc099d3d67d2
Fangrui Song <i@maskray.me>
2022-11-23 23:08:49 -0800

    [CSSPGO] Move cl::opt inside llvm:: after D100528 and D108342

--- llvm/lib/ProfileData/ProfileSummaryBuilder.cpp
+++ llvm/lib/ProfileData/ProfileSummaryBuilder.cpp
@@ -18,6 +18,7 @@
 
 using namespace llvm;
 
+namespace llvm {
 cl::opt<bool> UseContextLessSummary(
     "profile-summary-contextless", cl::Hidden,
     cl::desc("Merge context profiles before calculating thresholds."));
@@ -64,6 +65,7 @@ cl::opt<uint64_t> ProfileSummaryColdCount(
     "profile-summary-cold-count", cl::ReallyHidden,
     cl::desc("A fixed cold count that overrides the count derived from"
              " profile-summary-cutoff-cold"));
+} // namespace llvm
 
 // A set of cutoff values. Each value, when divided by ProfileSummary::Scale
 // (which is 1000000) is a desired percentile of total counts.
