fa3587d63eed78dd90663b7fe81cf9f77d403ff1
Valentin Clement <clementval@gmail.com>
2022-11-25 09:30:08 +0100

    [flang] Remove debug option
    Remove debug option added by mistake in D138587

--- flang/lib/Frontend/CompilerInvocation.cpp
+++ flang/lib/Frontend/CompilerInvocation.cpp
@@ -951,7 +951,6 @@ void CompilerInvocation::setLoweringOptions() {
 
   // Lower TRANSPOSE as a runtime call under -O0.
   loweringOpts.setOptimizeTranspose(codegenOpts.OptimizationLevel > 0);
-  loweringOpts.setPolymorphicTypeImpl(true);
 
   const LangOptions &langOptions = getLangOpts();
   Fortran::common::MathOptionsBase &mathOpts = loweringOpts.getMathOptions();
