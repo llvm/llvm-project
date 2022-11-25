ce670f34c559ed7e98fe0437eba2c2d534d3ab86
Louis Dionne <ldionne.2@gmail.com>
2022-11-25 10:25:10 -0500

    [libc++][NFC] Consistently use newline between license and include guard

--- libcxx/include/stdbool.h
+++ libcxx/include/stdbool.h
@@ -6,6 +6,7 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
+
 #ifndef _LIBCPP_STDBOOL_H
 #define _LIBCPP_STDBOOL_H
 
