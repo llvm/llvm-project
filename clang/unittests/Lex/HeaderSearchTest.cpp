1dc0a1e5d220b83c1074204bd3afd54f3bac4270
Sam McCall <sam.mccall@gmail.com>
2022-11-25 14:01:06 +0100

    Revert "[Lex] Fix suggested spelling of /usr/bin/../include/foo"
    This reverts commit 8bed59c7e7da2fea41a9167e15c15a8f58a5ede7.
    
    Breaks bots e.g. https://lab.llvm.org/buildbot/#/builders/216/builds/13282

--- clang/unittests/Lex/HeaderSearchTest.cpp
+++ clang/unittests/Lex/HeaderSearchTest.cpp
@@ -150,14 +150,6 @@ TEST_F(HeaderSearchTest, DotDotsWithAbsPath) {
             "z");
 }
 
-TEST_F(HeaderSearchTest, BothDotDots) {
-  addSearchDir("/x/../y/");
-  EXPECT_EQ(Search.suggestPathToFileForDiagnostics("/x/../y/z",
-                                                   /*WorkingDir=*/"",
-                                                   /*MainFile=*/""),
-            "z");
-}
-
 TEST_F(HeaderSearchTest, IncludeFromSameDirectory) {
   EXPECT_EQ(Search.suggestPathToFileForDiagnostics("/y/z/t.h",
                                                    /*WorkingDir=*/"",
