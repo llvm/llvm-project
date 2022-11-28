42c2dc401742266da3e0251b6c1ca491f4779963
Alexandros Lamprineas <alexandros.lamprineas@arm.com>
2022-11-28 13:01:29 +0000

    [IPSCCP] Move the IPSCCP run function under the IPO directory.
    The LLVMipo library no longer depends on the Scalar component.
    The shared functions between IPSCCP and SCCP have been moved
    under Utils, in the SCCPSolver.
    
    This is preliminary work for D126455, in order to break a cyclic
    dependency between LLVM libraries.
    
    Differential Revision: https://reviews.llvm.org/D138654

--- llvm/include/llvm/Transforms/Scalar/SCCP.h
+++ llvm/include/llvm/Transforms/Scalar/SCCP.h
@@ -40,10 +40,6 @@ public:
   PreservedAnalyses run(Function &F, FunctionAnalysisManager &AM);
 };
 
-bool runIPSCCP(Module &M, const DataLayout &DL,
-               std::function<const TargetLibraryInfo &(Function &)> GetTLI,
-               function_ref<AnalysisResultsForFn(Function &)> getAnalysis);
-
 bool runFunctionSpecialization(
     Module &M, FunctionAnalysisManager *FAM, const DataLayout &DL,
     std::function<TargetLibraryInfo &(Function &)> GetTLI,
