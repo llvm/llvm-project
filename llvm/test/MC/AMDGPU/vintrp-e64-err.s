80140c13279fd188814dbe1d481b90e8628ab587
Jay Foad <jay.foad@amd.com>
2022-11-29 13:18:24 +0000

    [AMDGPU] Add error line number checking
    Differential Revision: https://reviews.llvm.org/D138916

--- llvm/test/MC/AMDGPU/vintrp-e64-err.s
+++ llvm/test/MC/AMDGPU/vintrp-e64-err.s
@@ -3,43 +3,43 @@
 // RUN: not llvm-mc -arch=amdgcn -mcpu=gfx1010 %s 2>&1 | FileCheck %s --implicit-check-not=error:
 
 v_interp_p1_f32_e64 v5, 0.5, attr0.w
-// CHECK: error: invalid operand for instruction
+// CHECK: :[[@LINE-1]]:{{[0-9]+}}: error: invalid operand for instruction
 
 v_interp_p1_f32_e64 v5, s1, attr0.w
-// CHECK: error: invalid operand for instruction
+// CHECK: :[[@LINE-1]]:{{[0-9]+}}: error: invalid operand for instruction
 
 v_interp_p1ll_f16 v5, 0.5, attr0.w
-// CHECK: error: invalid operand for instruction
+// CHECK: :[[@LINE-1]]:{{[0-9]+}}: error: invalid operand for instruction
 
 v_interp_p1ll_f16 v5, s1, attr0.w
-// CHECK: error: invalid operand for instruction
+// CHECK: :[[@LINE-1]]:{{[0-9]+}}: error: invalid operand for instruction
 
 v_interp_p1lv_f16 v5, 0.5, attr0.w, v3
-// CHECK: error: invalid operand for instruction
+// CHECK: :[[@LINE-1]]:{{[0-9]+}}: error: invalid operand for instruction
 
 v_interp_p1lv_f16 v5, s1, attr0.w, v3
-// CHECK: error: invalid operand for instruction
+// CHECK: :[[@LINE-1]]:{{[0-9]+}}: error: invalid operand for instruction
 
 v_interp_p1lv_f16 v5, v1, attr31.w, 0.5
-// CHECK: error: invalid operand for instruction
+// CHECK: :[[@LINE-1]]:{{[0-9]+}}: error: invalid operand for instruction
 
 v_interp_p1lv_f16 v5, v1, attr31.w, s1
-// CHECK: error: invalid operand for instruction
+// CHECK: :[[@LINE-1]]:{{[0-9]+}}: error: invalid operand for instruction
 
 v_interp_p2_f16 v5, 0.5, attr0.w, v3
-// CHECK: error: invalid operand for instruction
+// CHECK: :[[@LINE-1]]:{{[0-9]+}}: error: invalid operand for instruction
 
 v_interp_p2_f16 v5, s1, attr0.w, v3
-// CHECK: error: invalid operand for instruction
+// CHECK: :[[@LINE-1]]:{{[0-9]+}}: error: invalid operand for instruction
 
 v_interp_p2_f16 v5, v1, attr1.w, 0.5
-// CHECK: error: invalid operand for instruction
+// CHECK: :[[@LINE-1]]:{{[0-9]+}}: error: invalid operand for instruction
 
 v_interp_p2_f16 v5, v1, attr1.w, s1
-// CHECK: error: invalid operand for instruction
+// CHECK: :[[@LINE-1]]:{{[0-9]+}}: error: invalid operand for instruction
 
 v_interp_p2_f32_e64 v5, 0.5, attr31.w
-// CHECK: error: invalid operand for instruction
+// CHECK: :[[@LINE-1]]:{{[0-9]+}}: error: invalid operand for instruction
 
 v_interp_p2_f32_e64 v5, s1, attr31.w
-// CHECK: error: invalid operand for instruction
+// CHECK: :[[@LINE-1]]:{{[0-9]+}}: error: invalid operand for instruction
