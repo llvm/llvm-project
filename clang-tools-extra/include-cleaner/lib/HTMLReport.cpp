d3714c2b277ac649374ada6dacf20c7a1ba9120c
Sam McCall <sam.mccall@gmail.com>
2022-11-30 16:33:50 +0100

    [include-cleaner] Move RecordedPP::RecordedIncludes -> Includes in Types.h. NFC
    Requiring everything that wants to match Includes to depend on Record is weird.
    This isn't lightweight enough that it feels perfect in Types, could be its own
    header instead. But pragmatically it doesn't add bad deps, and is widely used.
    
    Differential Revision: https://reviews.llvm.org/D139014

--- clang-tools-extra/include-cleaner/lib/HTMLReport.cpp
+++ clang-tools-extra/include-cleaner/lib/HTMLReport.cpp
@@ -135,7 +135,7 @@ class Reporter {
   const ASTContext &Ctx;
   const SourceManager &SM;
   HeaderSearch &HS;
-  const RecordedPP::RecordedIncludes &Includes;
+  const include_cleaner::Includes &Includes;
   const PragmaIncludes *PI;
   FileID MainFile;
   const FileEntry *MainFE;
@@ -220,7 +220,7 @@ class Reporter {
 
 public:
   Reporter(llvm::raw_ostream &OS, ASTContext &Ctx, HeaderSearch &HS,
-           const RecordedPP::RecordedIncludes &Includes,
+           const include_cleaner::Includes &Includes,
            const PragmaIncludes *PI, FileID MainFile)
       : OS(OS), Ctx(Ctx), SM(Ctx.getSourceManager()), HS(HS),
         Includes(Includes), PI(PI), MainFile(MainFile),
@@ -521,7 +521,7 @@ private:
 
 } // namespace
 
-void writeHTMLReport(FileID File, const RecordedPP::RecordedIncludes &Includes,
+void writeHTMLReport(FileID File, const include_cleaner::Includes &Includes,
                      llvm::ArrayRef<Decl *> Roots,
                      llvm::ArrayRef<SymbolReference> MacroRefs, ASTContext &Ctx,
                      HeaderSearch &HS, PragmaIncludes *PI,
