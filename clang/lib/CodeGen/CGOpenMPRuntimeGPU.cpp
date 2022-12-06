079a2d70b73634e6151281ca968286705e8e1ff2
Jan Sjodin <jan_sjodin@yahoo.com>
2022-12-06 14:26:50 -0500

    [OpenMP][OMPIRBuilder] Migrate code to emit target region functions from clang to OMPIRBuilder
    This patch moves some of the logic on how to emit target region functions and
    adds emitTargetRegionFunction to the OpenMPIRBuilder. Also the
    OpenMPOffloadMandatory flag is added to the config.
    
    Reviewed By: jdoerfert
    
    Differential Revision: https://reviews.llvm.org/D139634

--- clang/lib/CodeGen/CGOpenMPRuntimeGPU.cpp
+++ clang/lib/CodeGen/CGOpenMPRuntimeGPU.cpp
@@ -891,7 +891,8 @@ unsigned CGOpenMPRuntimeGPU::getDefaultLocationReserved2Flags() const {
 CGOpenMPRuntimeGPU::CGOpenMPRuntimeGPU(CodeGenModule &CGM)
     : CGOpenMPRuntime(CGM) {
   llvm::OpenMPIRBuilderConfig Config(CGM.getLangOpts().OpenMPIsDevice, true,
-                                     hasRequiresUnifiedSharedMemory());
+                                     hasRequiresUnifiedSharedMemory(),
+                                     CGM.getLangOpts().OpenMPOffloadMandatory);
   OMPBuilder.setConfig(Config);
   OffloadEntriesInfoManager.setConfig(Config);
 
