fe0286edb64f5c578969e0d2a2acee2fb88efbc2
Roman Lebedev <lebedev.ri@gmail.com>
2022-12-07 20:14:13 +0300

    [llvm-exegesis] Dry run mode
    Sometimes we only want to ensure that we can produce snippets (all the way
    through `SnippetRepetitor`!), but don't care for the execution.
    E.g. all of our tests are this way.
    
    I've built LLVM without PFM and removed my CPU from `X86PfmCounters.td`,
    and this produces the expected results in that configuration.
    
    Reviewed By: courbet
    
    Differential Revision: https://reviews.llvm.org/D139448

--- llvm/test/tools/llvm-exegesis/X86/uops/uops-FLDENVm.s
+++ llvm/test/tools/llvm-exegesis/X86/uops/uops-FLDENVm.s
@@ -1,4 +1,4 @@
-# RUN: llvm-exegesis -mode=uops -opcode-name=FLDENVm,FLDL2E -repetition-mode=duplicate | FileCheck %s
+# RUN: llvm-exegesis -mtriple=x86_64-unknown-unknown -mcpu=x86-64 -mode=uops --skip-measurements -opcode-name=FLDENVm,FLDL2E -repetition-mode=duplicate | FileCheck %s
 
 CHECK:      mode:            uops
 CHECK-NEXT: key:
