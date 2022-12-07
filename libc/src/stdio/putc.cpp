ae93a882a38171c2026b80e404d41e4715f642c4
Michael Jones <michaelrj@google.com>
2022-12-07 13:25:22 -0800

    [libc] move errno out of file internals
    Now errno is only set by the terminal entrypoints, and not the internal
    implementations. This patch is part of the larger effort to not set
    errno in libc internal code: https://github.com/llvm/llvm-project/issues/59278
    
    Reviewed By: sivachandra
    
    Differential Revision: https://reviews.llvm.org/D139576

--- libc/src/stdio/putc.cpp
+++ libc/src/stdio/putc.cpp
@@ -9,13 +9,19 @@
 #include "src/stdio/putc.h"
 #include "src/__support/File/file.h"
 
+#include <errno.h>
 #include <stdio.h>
 
 namespace __llvm_libc {
 
 LLVM_LIBC_FUNCTION(int, putc, (int c, ::FILE *stream)) {
   unsigned char uc = static_cast<unsigned char>(c);
-  size_t written = reinterpret_cast<__llvm_libc::File *>(stream)->write(&uc, 1);
+
+  auto result = reinterpret_cast<__llvm_libc::File *>(stream)->write(&uc, 1);
+  if (result.has_error())
+    errno = result.error;
+  size_t written = result.value;
+
   if (1 != written) {
     // The stream should be in an error state in this case.
     return EOF;
