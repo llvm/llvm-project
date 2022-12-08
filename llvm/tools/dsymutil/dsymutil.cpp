ab3dd9ad532d63248f731d207afecf71b2fa5b29
Alex Brachet <abrachet@google.com>
2022-12-08 07:31:11 +0000

    [llvm-driver] Rename dsynutil_main to main after removing from driver

--- llvm/tools/dsymutil/dsymutil.cpp
+++ llvm/tools/dsymutil/dsymutil.cpp
@@ -546,7 +546,7 @@ getOutputFileName(StringRef InputFile, const DsymutilOptions &Options) {
   return OutputLocation(std::string(Path.str()), ResourceDir);
 }
 
-int dsymutil_main(int argc, char **argv) {
+int main(int argc, char **argv) {
   InitLLVM X(argc, argv);
 
   // Parse arguments.
