93b1ec3652aae9e859e84010c3157046a0ad48cb
Kazu Hirata <kazu@google.com>
2022-12-10 09:28:07 -0800

    Revert "[FileCheck] Use std::optional in FileCheck.cpp (NFC)"
    This reverts commit f555ec57cc01181ce09c802194b6f572f7a3f78a.
    
    One build failure has been reported:
    
    https://lab.llvm.org/buildbot/#/builders/67/builds/9785/steps/6/logs/stdio

--- llvm/lib/FileCheck/FileCheck.cpp
+++ llvm/lib/FileCheck/FileCheck.cpp
@@ -22,7 +22,6 @@
 #include "llvm/Support/FormatVariadic.h"
 #include <cstdint>
 #include <list>
-#include <optional>
 #include <set>
 #include <tuple>
 #include <utility>
@@ -696,7 +695,7 @@ Pattern::parseCallExpr(StringRef &Expr, StringRef FuncName,
   Expr = Expr.ltrim(SpaceChars);
   assert(Expr.startswith("("));
 
-  auto OptFunc = StringSwitch<std::optional<binop_eval_t>>(FuncName)
+  auto OptFunc = StringSwitch<Optional<binop_eval_t>>(FuncName)
                      .Case("add", operator+)
                      .Case("div", operator/)
                      .Case("max", max)
