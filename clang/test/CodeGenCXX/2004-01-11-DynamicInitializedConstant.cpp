65c027ab61c080cabc0998967ffaec0b89cf9301
Nikita Popov <npopov@redhat.com>
2022-12-12 14:43:14 +0100

    Revert "[Clang][NFC] Prevent lit tests from matching substrings in current path"
    This reverts commit ba65864bf01d65ceb291899541d4e5562b90b13c.
    
    Using placeholders inside CHECK-LABEL is not legal.

--- clang/test/CodeGenCXX/2004-01-11-DynamicInitializedConstant.cpp
+++ clang/test/CodeGenCXX/2004-01-11-DynamicInitializedConstant.cpp
@@ -1,11 +1,6 @@
 // RUN: %clang_cc1 -emit-llvm -o - %s | FileCheck %s
 
-// Catch the beginning and the end of the IR. This prevents the CHECK- from
-// matching a spurious "constant" string in file paths printed later.
-
-// CHECK-LABEL: target triple
 // CHECK-NOT: constant
-// CHECK-LABEL: attributes
 extern int X;
 const int Y = X;
 const int* foo() { return &Y; }
