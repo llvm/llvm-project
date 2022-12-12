f71276c6c9c5c91626d1ef861594c7f1b97a1b23
Carlos Galvez <carlosgalvezp@gmail.com>
2022-12-12 13:36:29 +0000

    [clang-tidy][NFC] Simply match processing in misc-use-anonymous-namespace
    No need for the templated function "processMatch", since
    we can infer the type with llvm:isa.

--- clang-tools-extra/clang-tidy/misc/UseAnonymousNamespaceCheck.cpp
+++ clang-tools-extra/clang-tidy/misc/UseAnonymousNamespaceCheck.cpp
@@ -31,33 +31,26 @@ AST_MATCHER(Decl, isInAnonymousNamespace) {
 }
 } // namespace
 
-template <typename T>
-void UseAnonymousNamespaceCheck::processMatch(const T *MatchedDecl) {
-  StringRef Type = llvm::isa<VarDecl>(MatchedDecl) ? "variable" : "function";
-  diag(MatchedDecl->getLocation(),
-       "%0 %1 declared 'static', move to anonymous namespace instead")
-      << Type << MatchedDecl;
-}
-
 void UseAnonymousNamespaceCheck::registerMatchers(MatchFinder *Finder) {
   Finder->addMatcher(
       functionDecl(isStatic(),
                    unless(anyOf(isInAnonymousNamespace(), isMemberFunction())))
-          .bind("func"),
+          .bind("x"),
       this);
   Finder->addMatcher(
       varDecl(isStatic(), unless(anyOf(isInAnonymousNamespace(),
                                        isStaticLocal(), isStaticDataMember())))
-          .bind("var"),
+          .bind("x"),
       this);
 }
 
 void UseAnonymousNamespaceCheck::check(const MatchFinder::MatchResult &Result) {
-  if (const auto *MatchedDecl = Result.Nodes.getNodeAs<FunctionDecl>("func"))
-    processMatch(MatchedDecl);
-
-  if (const auto *MatchedDecl = Result.Nodes.getNodeAs<VarDecl>("var"))
-    processMatch(MatchedDecl);
+  if (const auto *MatchedDecl = Result.Nodes.getNodeAs<NamedDecl>("x")) {
+    StringRef Type = llvm::isa<VarDecl>(MatchedDecl) ? "variable" : "function";
+    diag(MatchedDecl->getLocation(),
+         "%0 %1 declared 'static', move to anonymous namespace instead")
+        << Type << MatchedDecl;
+  }
 }
 
 } // namespace misc
