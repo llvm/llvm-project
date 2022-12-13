9830a5d0c19b8d5165735e39343b7174addd94a0
Terry Wilmarth <terry.l.wilmarth@intel.com>
2022-12-13 12:54:24 -0600

    [OpenMP] Fix for smaller team sizes inside teams construct.
    When a team nested inside a teams construct is allocated, it is
    allocated to a size specified by the teams thread_limit.  In the case
    where any mechanism that might not grant the full thread_limit is in
    use, we may get a smaller team.  This possibility was not reflected in
    the code when using the th_teams_size.nth value stored on the master
    thread for the team. This value was never updated even when t_nproc on
    the team itself was different.  I added a line to update it shortly
    before the team is forked.
    
    Added a simple teams test that uses KMP_DYNAMIC_MODE=random to mimic
    allocating teams with sizes <= thread_limit. Eventually, this
    will segfault without the fix in this commit.
    
    Differential Revision: https://reviews.llvm.org/D139960

--- openmp/runtime/src/kmp_runtime.cpp
+++ openmp/runtime/src/kmp_runtime.cpp
@@ -1479,6 +1479,13 @@ __kmp_fork_in_teams(ident_t *loc, int gtid, kmp_team_t *parent_team,
   parent_team->t.t_level++;
   parent_team->t.t_def_allocator = master_th->th.th_def_allocator; // save
 
+  // If the threads allocated to the team are less than the thread limit, update
+  // the thread limit here. th_teams_size.nth is specific to this team nested
+  // in a teams construct, the team is fully created, and we're about to do
+  // the actual fork. Best to do this here so that the subsequent uses below
+  // and in the join have the correct value.
+  master_th->th.th_teams_size.nth = parent_team->t.t_nproc;
+
 #if OMPT_SUPPORT
   if (ompt_enabled.enabled) {
     ompt_lw_taskteam_t lw_taskteam;
