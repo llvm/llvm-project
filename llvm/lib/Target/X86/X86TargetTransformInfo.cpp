5023d5809d093b099d772c5bf513e6063eb8b0a3
Roman Lebedev <lebedev.ri@gmail.com>
2022-12-13 20:09:21 +0300

    [NFC][Costmodel][X86] Replication shuffle: AVX512F can promote i1 to i32.
    As the added codegen test coverage shows,
    there isn't that much difference between AVX512DQI and
    baseline AVX512F codegen, DQI added `vpmovm2d`/`vpmovd2m`,
    but with just the Foundation we can use `vpternlogd`/`vptestmd`
    to do the same.

--- llvm/lib/Target/X86/X86TargetTransformInfo.cpp
+++ llvm/lib/Target/X86/X86TargetTransformInfo.cpp
@@ -4592,11 +4592,8 @@ X86TTIImpl::getReplicationShuffleCost(Type *EltTy, int ReplicationFactor,
         PromEltTyBits = 16; // promote to i16, AVX512BW.
       break;
     }
-    if (ST->hasDQI()) {
-      PromEltTyBits = 32; // promote to i32, AVX512F.
-      break;
-    }
-    return bailout();
+    PromEltTyBits = 32; // promote to i32, AVX512F.
+    break;
   default:
     return bailout();
   }
