8eacc2d0fc5cbc52a6e063d49b0aa6e612f6d1e9
Jan Svoboda <jan_svoboda@apple.com>
2022-12-15 15:03:07 -0800

    [clang][deps] Implement `dump()` for the VFS

--- llvm/include/llvm/Support/VirtualFileSystem.h
+++ llvm/include/llvm/Support/VirtualFileSystem.h
@@ -450,7 +450,7 @@ public:
   }
 
 protected:
-  FileSystem &getUnderlyingFS() { return *FS; }
+  FileSystem &getUnderlyingFS() const { return *FS; }
 
 private:
   IntrusiveRefCntPtr<FileSystem> FS;
