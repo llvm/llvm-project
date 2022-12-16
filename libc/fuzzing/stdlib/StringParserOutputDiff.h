5ae9dd028fd13344a3cc1c5a30325556e693d759
Michael Jones <michaelrj@google.com>
2022-12-15 16:36:59 -0800

    [libc] add fuzz target for strtointeger functions
    The string to integer conversion functions are well suited to
    differential fuzzing, and this patch adds a target to enable just that.
    It also fixes a bug in the fuzzing comparison logic and changes atoi
    slightly to match the behavior described in the C standard.
    
    Reviewed By: sivachandra, lntue
    
    Differential Revision: https://reviews.llvm.org/D140178

--- libc/fuzzing/stdlib/StringParserOutputDiff.h
+++ libc/fuzzing/stdlib/StringParserOutputDiff.h
@@ -32,4 +32,28 @@ void StringParserOutputDiff(StringInputSingleOutputFunc<T> func1,
     __builtin_trap();
 }
 
+template <typename T>
+using StringToNumberFunc = T (*)(const char *, char **, int);
+
+template <typename T>
+void StringToNumberOutputDiff(StringToNumberFunc<T> func1,
+                              StringToNumberFunc<T> func2, const uint8_t *data,
+                              size_t size) {
+  if (size < sizeof(T))
+    return;
+
+  const char *x = reinterpret_cast<const char *>(data + 1);
+  int base = data[0] % 36;
+  base = base + ((base == 0) ? 0 : 1);
+
+  char *outPtr1 = nullptr;
+  char *outPtr2 = nullptr;
+
+  T result1 = func1(x, &outPtr1, base);
+  T result2 = func2(x, &outPtr2, base);
+
+  if (!(ValuesEqual(result1, result2) && (*outPtr1 == *outPtr2)))
+    __builtin_trap();
+}
+
 #endif // LLVM_LIBC_FUZZING_STDLIB_STRING_PARSER_OUTPUT_DIFF_H
