22d7c6cb117cc7db5102a0c95155954a052b7319
Siva Chandra Reddy <sivachandra@google.com>
2022-12-17 00:04:23 +0000

    [libc] Add support for standalone cross compilation of libc.
    One should be able to do a cross build of the libc now. For example, using
    clang on a x86_64 linux host, one can build for an aarch64 linux target by
    specifying -DLIBC_TARGET_TRIPLE=aarch64-linux-gnu.
    
    Follow up changes will add a baremetal config and also appropriate
    documentation about cross compiling the libc for CPU targets.
    
    Reviewed By: jhuber6
    
    Differential Revision: https://reviews.llvm.org/D140351

--- libc/cmake/modules/prepare_libc_gpu_build.cmake
+++ libc/cmake/modules/prepare_libc_gpu_build.cmake
@@ -1,3 +1,8 @@
+if(NOT LIBC_TARGET_ARCHITECTURE_IS_GPU)
+  message(FATAL_ERROR
+          "libc build: Invalid attempt to set up GPU architectures.")
+endif()
+
 # Set up the target architectures to build the GPU libc for.
 set(all_gpu_architectures "sm_35;sm_37;sm_50;sm_52;sm_53;sm_60;sm_61;sm_62;"
                           "sm_70;sm_72;sm_75;sm_80;sm_86;gfx700;gfx701;gfx801;"
@@ -13,9 +18,8 @@ endif()
 
 # Ensure the compiler is a valid clang when building the GPU target.
 set(req_ver "${LLVM_VERSION_MAJOR}.${LLVM_VERSION_MINOR}.${LLVM_VERSION_PATCH}")
-if(LIBC_TARGET_ARCHITECTURE_IS_GPU AND
-   NOT (CMAKE_CXX_COMPILER_ID MATCHES "[Cc]lang" AND
-       ${CMAKE_CXX_COMPILER_VERSION} VERSION_EQUAL "${req_ver}"))
+if(NOT (CMAKE_CXX_COMPILER_ID MATCHES "[Cc]lang" AND
+        ${CMAKE_CXX_COMPILER_VERSION} VERSION_EQUAL "${req_ver}"))
   message(FATAL_ERROR "Cannot build libc for GPU. CMake compiler "
                       "'${CMAKE_CXX_COMPILER_ID} ${CMAKE_CXX_COMPILER_VERSION}' "
                       " is not `Clang ${req_ver}.")
