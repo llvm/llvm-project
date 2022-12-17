299cefdfa1808377d0606d44a2b3cb3e2108a075
Nathan Lanza <nathanlanza@gmail.com>
2022-12-17 02:50:16 -0500

    [CIR] Account for two changes to loc ordering while rebasing

--- clang/test/CIR/CodeGen/struct.c
+++ clang/test/CIR/CodeGen/struct.c
@@ -19,7 +19,7 @@ void baz() {
 
 //      CHECK: !ty_22struct2EBar22 = !cir.struct<"struct.Bar", i32, i8>
 // CHECK-NEXT: !ty_22struct2EFoo22 = !cir.struct<"struct.Foo", i32, i8, !ty_22struct2EBar22>
-// CHECK-NEXT: module {{.*}} {
+//  CHECK-DAG: module {{.*}} {
 // CHECK-NEXT:   cir.func @baz() {
 // CHECK-NEXT:     %0 = cir.alloca !ty_22struct2EBar22, cir.ptr <!ty_22struct2EBar22>, ["b"] {alignment = 4 : i64}
 // CHECK-NEXT:     %1 = cir.alloca !ty_22struct2EFoo22, cir.ptr <!ty_22struct2EFoo22>, ["f"] {alignment = 4 : i64}
