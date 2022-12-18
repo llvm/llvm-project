llvm/llvm-project

commit 7b9183d822b682e3006795fd393340f3bb950403

[NFCI][llvm-exegesis] Extract 'Min' repetition handling from `BenchmarkRunner` into it's caller
If `BenchmarkRunner::runConfiguration()` deals with more than a single
repetitor, tasking will be less straight-forward to implement.
But i think dealing with that in it's callee is even more readable.

llvm/tools/llvm-exegesis/llvm-exegesis.cpp
@@ -406,8 +406,40 @@ void benchmarkMain() {
     Meter.emplace(Configurations.size());
   for (const BenchmarkCode &Conf : Configurations) {
     ProgressMeter<>::ProgressMeterStep MeterStep(Meter ? &*Meter : nullptr);
-    InstructionBenchmark Result = ExitOnErr(Runner->runConfiguration(
-        Conf, NumRepetitions, LoopBodySize, Repetitors, DumpObjectToDisk));
+    SmallVector<InstructionBenchmark, 2> AllResults;
+
+    for (const std::unique_ptr<const SnippetRepetitor> &Repetitor :
+         Repetitors) {
+      AllResults.emplace_back(ExitOnErr(Runner->runConfiguration(
+          Conf, NumRepetitions, LoopBodySize, *Repetitor, DumpObjectToDisk)));
+    }
+    InstructionBenchmark &Result = AllResults.front();
+
+    if (RepetitionMode == InstructionBenchmark::RepetitionModeE::AggregateMin) {
+      assert(!Result.Measurements.empty() &&
+             "We're in an 'min' repetition mode, and need to aggregate new "
+             "result to the existing result.");
+      for (const InstructionBenchmark &OtherResult :
+           ArrayRef<InstructionBenchmark>(AllResults).drop_front()) {
+        llvm::append_range(Result.AssembledSnippet,
+                           OtherResult.AssembledSnippet);
+        assert(OtherResult.Measurements.size() == Result.Measurements.size() &&
+               "Expected to have identical number of measurements.");
+        for (auto I : zip(Result.Measurements, OtherResult.Measurements)) {
+          BenchmarkMeasure &Measurement = std::get<0>(I);
+          const BenchmarkMeasure &NewMeasurement = std::get<1>(I);
+          assert(Measurement.Key == NewMeasurement.Key &&
+                 "Expected measurements to be symmetric");
+
+          Measurement.PerInstructionValue =
+              std::min(Measurement.PerInstructionValue,
+                       NewMeasurement.PerInstructionValue);
+          Measurement.PerSnippetValue = std::min(
+              Measurement.PerSnippetValue, NewMeasurement.PerSnippetValue);
+        }
+      }
+    }
+
     ExitOnFileError(BenchmarkFile, Result.writeYaml(State, BenchmarkFile));
   }
   exegesis::pfm::pfmTerminate();

llvm/tools/llvm-exegesis/lib/BenchmarkRunner.cpp
@@ -137,8 +137,7 @@ private:
 
 Expected<InstructionBenchmark> BenchmarkRunner::runConfiguration(
     const BenchmarkCode &BC, unsigned NumRepetitions, unsigned LoopBodySize,
-    ArrayRef<std::unique_ptr<const SnippetRepetitor>> Repetitors,
-    bool DumpObjectToDisk) const {
+    const SnippetRepetitor &Repetitor, bool DumpObjectToDisk) const {
   InstructionBenchmark InstrBenchmark;
   InstrBenchmark.Mode = Mode;
   InstrBenchmark.CpuName = std::string(State.getTargetMachine().getTargetCPU());
@@ -151,23 +150,7 @@ Expected<InstructionBenchmark> BenchmarkRunner::runConfiguration(
 
   InstrBenchmark.Key = BC.Key;
 
-  // If we end up having an error, and we've previously succeeded with
-  // some other Repetitor, we want to discard the previous measurements.
-  struct ClearBenchmarkOnReturn {
-    ClearBenchmarkOnReturn(InstructionBenchmark *IB) : IB(IB) {}
-    ~ClearBenchmarkOnReturn() {
-      if (Clear)
-        IB->Measurements.clear();
-    }
-    void disarm() { Clear = false; }
-
-  private:
-    InstructionBenchmark *const IB;
-    bool Clear = true;
-  };
-  ClearBenchmarkOnReturn CBOR(&InstrBenchmark);
-
-  for (const std::unique_ptr<const SnippetRepetitor> &Repetitor : Repetitors) {
+  {
     // Assemble at least kMinInstructionsForSnippet instructions by repeating
     // the snippet for debug/analysis. This is so that the user clearly
     // understands that the inside instructions are repeated.
@@ -179,8 +162,8 @@ Expected<InstructionBenchmark> BenchmarkRunner::runConfiguration(
       if (Error E = assembleToStream(
               State.getExegesisTarget(), State.createTargetMachine(),
               BC.LiveIns, BC.Key.RegisterInitialValues,
-              Repetitor->Repeat(Instructions, MinInstructionsForSnippet,
-                                LoopBodySizeForSnippet),
+              Repetitor.Repeat(Instructions, MinInstructionsForSnippet,
+                               LoopBodySizeForSnippet),
               OS)) {
         return std::move(E);
       }
@@ -192,7 +175,7 @@ Expected<InstructionBenchmark> BenchmarkRunner::runConfiguration(
 
     // Assemble NumRepetitions instructions repetitions of the snippet for
     // measurements.
-    const auto Filler = Repetitor->Repeat(
+    const auto Filler = Repetitor.Repeat(
         Instructions, InstrBenchmark.NumRepetitions, LoopBodySize);
 
     object::OwningBinary<object::ObjectFile> ObjectFile;
@@ -219,7 +202,7 @@ Expected<InstructionBenchmark> BenchmarkRunner::runConfiguration(
     if (BenchmarkSkipMeasurements) {
       InstrBenchmark.Error =
           "in --skip-measurements mode, actual measurements skipped.";
-      continue;
+      return InstrBenchmark;
     }
 
     const FunctionExecutorImpl Executor(State, std::move(ObjectFile),
@@ -239,31 +222,9 @@ Expected<InstructionBenchmark> BenchmarkRunner::runConfiguration(
       BM.PerSnippetValue *= static_cast<double>(Instructions.size()) /
                             InstrBenchmark.NumRepetitions;
     }
-    if (InstrBenchmark.Measurements.empty()) {
-      InstrBenchmark.Measurements = std::move(*NewMeasurements);
-      continue;
-    }
-
-    assert(Repetitors.size() > 1 && !InstrBenchmark.Measurements.empty() &&
-           "We're in an 'min' repetition mode, and need to aggregate new "
-           "result to the existing result.");
-    assert(InstrBenchmark.Measurements.size() == NewMeasurements->size() &&
-           "Expected to have identical number of measurements.");
-    for (auto I : zip(InstrBenchmark.Measurements, *NewMeasurements)) {
-      BenchmarkMeasure &Measurement = std::get<0>(I);
-      BenchmarkMeasure &NewMeasurement = std::get<1>(I);
-      assert(Measurement.Key == NewMeasurement.Key &&
-             "Expected measurements to be symmetric");
-
-      Measurement.PerInstructionValue = std::min(
-          Measurement.PerInstructionValue, NewMeasurement.PerInstructionValue);
-      Measurement.PerSnippetValue =
-          std::min(Measurement.PerSnippetValue, NewMeasurement.PerSnippetValue);
-    }
+    InstrBenchmark.Measurements = std::move(*NewMeasurements);
   }
 
-  // We successfully measured everything, so don't discard the results.
-  CBOR.disarm();
   return InstrBenchmark;
 }
 

llvm/tools/llvm-exegesis/lib/BenchmarkRunner.h
@@ -42,8 +42,7 @@ public:
 
   Expected<InstructionBenchmark>
   runConfiguration(const BenchmarkCode &Configuration, unsigned NumRepetitions,
-                   unsigned LoopUnrollFactor,
-                   ArrayRef<std::unique_ptr<const SnippetRepetitor>> Repetitors,
+                   unsigned LoopUnrollFactor, const SnippetRepetitor &Repetitor,
                    bool DumpObjectToDisk) const;
 
   // Scratch space to run instructions that touch memory.

commit 4be068f98a2e7526ead3a92ec1350eaa85e60841

[llvm-exegesis] Fix 'min' repetition mode in presence of missing measurements
This was a regression from 7b9183d822b682e3006795fd393340f3bb950403.
Previously we'd gracefully handle missing measurements,
but that handling got accidentally lost during the code move,
and we'd assert.

What we want to do, is to discard all measurements (from all repetitors
in a given config) if any of them failed, but do append the snippet,
and do emit the empty measurement.

llvm/tools/llvm-exegesis/llvm-exegesis.cpp
@@ -348,14 +348,21 @@ static void runBenchmarkConfigurations(
     }
     InstructionBenchmark &Result = AllResults.front();
 
+    // If any of our measurements failed, pretend they all have failed.
+    if (AllResults.size() > 1 &&
+        any_of(AllResults, [](const InstructionBenchmark &R) {
+          return R.Measurements.empty();
+        }))
+      Result.Measurements.clear();
+
     if (RepetitionMode == InstructionBenchmark::RepetitionModeE::AggregateMin) {
-      assert(!Result.Measurements.empty() &&
-             "We're in an 'min' repetition mode, and need to aggregate new "
-             "result to the existing result.");
       for (const InstructionBenchmark &OtherResult :
            ArrayRef<InstructionBenchmark>(AllResults).drop_front()) {
         llvm::append_range(Result.AssembledSnippet,
                            OtherResult.AssembledSnippet);
+        // Aggregate measurements, but only iff all measurements succeeded.
+        if (Result.Measurements.empty())
+          continue;
         assert(OtherResult.Measurements.size() == Result.Measurements.size() &&
                "Expected to have identical number of measurements.");
         for (auto I : zip(Result.Measurements, OtherResult.Measurements)) {

llvm/test/tools/llvm-exegesis/X86/latency/latency-CMOV32rr.s
@@ -1,5 +1,6 @@
 # RUN: llvm-exegesis -mtriple=x86_64-unknown-unknown -mcpu=x86-64 -mode=latency --skip-measurements -opcode-name=CMOV32rr -repetition-mode=duplicate | FileCheck %s
 # RUN: llvm-exegesis -mtriple=x86_64-unknown-unknown -mcpu=x86-64 -mode=latency --skip-measurements -opcode-name=CMOV32rr -repetition-mode=loop | FileCheck %s
+# RUN: llvm-exegesis -mtriple=x86_64-unknown-unknown -mcpu=x86-64 -mode=latency --skip-measurements -opcode-name=CMOV32rr -repetition-mode=min | FileCheck %s
 
 CHECK:      ---
 CHECK-NEXT: mode: latency