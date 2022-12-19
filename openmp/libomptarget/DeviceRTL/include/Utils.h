86d09e8f127424f3f680a15283fd4c53bf7386d9
Johannes Doerfert <johannes@jdoerfert.de>
2022-12-19 10:54:15 -0800

    [OpenMP] Rename the `_OMP` namespace in the device runtime to `ompx`
    Differential Revision: https://reviews.llvm.org/D140334

--- openmp/libomptarget/DeviceRTL/include/Utils.h
+++ openmp/libomptarget/DeviceRTL/include/Utils.h
@@ -16,7 +16,7 @@
 
 #pragma omp begin declare target device_type(nohost)
 
-namespace _OMP {
+namespace ompx {
 namespace utils {
 
 /// Return the value \p Var from thread Id \p SrcLane in the warp if the thread
@@ -89,7 +89,7 @@ __attribute__((loader_uninitialized)) static void *const UndefPtr;
 #define OMP_UNLIKELY(EXPR) __builtin_expect((bool)(EXPR), false)
 
 } // namespace utils
-} // namespace _OMP
+} // namespace ompx
 
 #pragma omp end declare target
 
