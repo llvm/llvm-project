108b08f2a91272f82d524616a337a8ce52edeed5
Matthias Springer <springerm@google.com>
2022-12-21 10:51:10 +0100

    [mlir] Add RuntimeVerifiableOpInterface and transform
    Static op verification cannot detect cases where an op is valid at compile time but may be invalid at runtime.
    
    An example of such an op is `memref::ExpandShapeOp`.
    
    Invalid at compile time: `memref.expand_shape %m [[0, 1]] : memref<11xf32> into memref<2x5xf32>`
    
    Valid at compile time (because we do not know any better): `memref.expand_shape %m [[0, 1]] : memref<?xf32> into memref<?x5xf32>`. This op may or may not be valid at runtime depending on the runtime shape of `%m`.
    
    Invalid runtime ops such as the one above are hard to debug because they can crash the program execution at a seemingly unrelated position or (even worse) compute an invalid result without crashing.
    
    This revision adds a new op interface `RuntimeVerifiableOpInterface` that can be implemented by ops that provide additional runtime verification. Such runtime verification can be computationally expensive, so it is only generated on an opt-in basis by running `-generate-runtime-verification`. A simple runtime verifier for `memref::ExpandShapeOp` is provided as an example.
    
    Differential Revision: https://reviews.llvm.org/D138576

---
+++ mlir/lib/Dialect/MemRef/Transforms/RuntimeOpVerification.cpp
@@ -0,0 +1,70 @@
+//===- RuntimeOpVerification.cpp - Op Verification ------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "mlir/Dialect/MemRef/Transforms/RuntimeOpVerification.h"
+
+#include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/Dialect/ControlFlow/IR/ControlFlow.h"
+#include "mlir/Dialect/ControlFlow/IR/ControlFlowOps.h"
+#include "mlir/Dialect/MemRef/IR/MemRef.h"
+#include "mlir/Interfaces/RuntimeVerifiableOpInterface.h"
+
+namespace mlir {
+namespace memref {
+namespace {
+struct ExpandShapeOpInterface
+    : public RuntimeVerifiableOpInterface::ExternalModel<ExpandShapeOpInterface,
+                                                         ExpandShapeOp> {
+  void generateRuntimeVerification(Operation *op, OpBuilder &builder,
+                                   Location loc) const {
+    auto expandShapeOp = cast<ExpandShapeOp>(op);
+
+    // Verify that the expanded dim sizes are a product of the collapsed dim
+    // size.
+    for (auto it : llvm::enumerate(expandShapeOp.getReassociationIndices())) {
+      Value srcDimSz =
+          builder.create<DimOp>(loc, expandShapeOp.getSrc(), it.index());
+      int64_t groupSz = 1;
+      bool foundDynamicDim = false;
+      for (int64_t resultDim : it.value()) {
+        if (expandShapeOp.getResultType().isDynamicDim(resultDim)) {
+          // Keep this assert here in case the op is extended in the future.
+          assert(!foundDynamicDim &&
+                 "more than one dynamic dim found in reassoc group");
+          foundDynamicDim = true;
+          continue;
+        }
+        groupSz *= expandShapeOp.getResultType().getDimSize(resultDim);
+      }
+      Value staticResultDimSz =
+          builder.create<arith::ConstantIndexOp>(loc, groupSz);
+      // staticResultDimSz must divide srcDimSz evenly.
+      Value mod =
+          builder.create<arith::RemSIOp>(loc, srcDimSz, staticResultDimSz);
+      Value isModZero = builder.create<arith::CmpIOp>(
+          loc, arith::CmpIPredicate::eq, mod,
+          builder.create<arith::ConstantIndexOp>(loc, 0));
+      builder.create<cf::AssertOp>(
+          loc, isModZero,
+          "static result dims in reassoc group do not divide src dim evenly");
+    }
+  }
+};
+} // namespace
+} // namespace memref
+} // namespace mlir
+
+void mlir::memref::registerRuntimeVerifiableOpInterfaceExternalModels(
+    DialectRegistry &registry) {
+  registry.addExtension(+[](MLIRContext *ctx, memref::MemRefDialect *dialect) {
+    ExpandShapeOp::attachInterface<ExpandShapeOpInterface>(*ctx);
+
+    // Load additional dialects of which ops may get created.
+    ctx->loadDialect<arith::ArithDialect, cf::ControlFlowDialect>();
+  });
+}
