74dd8c1bf80bbc415f8f6f45cb8f3f9bd83b3850
Mitch Phillips <31459023+hctim@users.noreply.github.com>
2022-12-20 17:16:17 -0800

    Revert "[AArch64][NFC] Fix aarch64 target features test."
    This reverts commit bf94eac6a3f7c5cd8941956d44c15524fa3751bd.
    
    Reason: Depends on reverted change https://reviews.llvm.org/D127812

--- clang/test/Driver/aarch64-features.c
+++ clang/test/Driver/aarch64-features.c
@@ -13,7 +13,7 @@
 // RUN: %clang -target aarch64-linux-android -rtlib=compiler-rt -mno-fmv \
 // RUN: -### -c %s 2>&1 | FileCheck -check-prefix=CHECK-FMV-OFF %s
 
-// RUN: %clang -target aarch64-linux-gnu -rtlib=libgcc \
+// RUN: %clang -target aarch64-linux-gnu \
 // RUN: -### -c %s 2>&1 | FileCheck -check-prefix=CHECK-FMV-OFF %s
 
 // RUN: %clang -target arm64-unknown-linux -rtlib=libgcc \
