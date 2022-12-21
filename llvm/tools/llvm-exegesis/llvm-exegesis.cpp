579f3714c8d2e08893d4b7cd0c5d45e87b51c098
Roman Lebedev <lebedev.ri@gmail.com>
2022-12-21 03:38:10 +0300

    [NFC][llvm-exegesis] Ensure that target options show up in --help
    Fixes https://github.com/llvm/llvm-project/issues/59377

--- llvm/tools/llvm-exegesis/llvm-exegesis.cpp
+++ llvm/tools/llvm-exegesis/llvm-exegesis.cpp
@@ -47,10 +47,6 @@
 namespace llvm {
 namespace exegesis {
 
-static cl::OptionCategory Options("llvm-exegesis options");
-static cl::OptionCategory BenchmarkOptions("llvm-exegesis benchmark options");
-static cl::OptionCategory AnalysisOptions("llvm-exegesis analysis options");
-
 static cl::opt<int> OpcodeIndex(
     "opcode-index",
     cl::desc("opcode to measure, by index, or -1 to measure all opcodes"),
