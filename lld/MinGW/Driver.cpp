07d9ab9aa5d272cad99617a1f28cdcfd73d86550
serge-sans-paille <sguelton@mozilla.com>
2022-12-23 13:25:58 +0100

    Revert "[clang] Use a StringRef instead of a raw char pointer to store builtin and call information"
    There are still remaining issues with GCC 12, see for instance
    
    https://lab.llvm.org/buildbot/#/builders/93/builds/12669
    
    This reverts commit 5ce4e92264102de21760c94db9166afe8f71fcf6.

--- lld/MinGW/Driver.cpp
+++ lld/MinGW/Driver.cpp
@@ -61,13 +61,12 @@ enum {
 };
 
 // Create prefix string literals used in Options.td
-#define PREFIX(NAME, VALUE)                                                    \
-  static constexpr std::initializer_list<llvm::StringLiteral> NAME = VALUE;
+#define PREFIX(NAME, VALUE) static const char *const NAME[] = VALUE;
 #include "Options.inc"
 #undef PREFIX
 
 // Create table mapping all options defined in Options.td
-static constexpr std::initializer_list<opt::OptTable::Info> infoTable = {
+static constexpr opt::OptTable::Info infoTable[] = {
 #define OPTION(X1, X2, ID, KIND, GROUP, ALIAS, X7, X8, X9, X10, X11, X12)      \
   {X1, X2, X10,         X11,         OPT_##ID, opt::Option::KIND##Class,       \
    X9, X8, OPT_##GROUP, OPT_##ALIAS, X7,       X12},
