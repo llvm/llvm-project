e7a93bae11563be6ce8982a50d9c29742c6a329d
Argyrios Kyrtzidis <kyrtzidis@apple.com>
2022-12-22 20:12:21 -0800

    [clang/cc1_main] Add `clang::` namespace prefix to `CachingDiagnosticsProcessor references
    This is to fix "error: reference to 'cas' is ambiguous" for a module build.
    
    (cherry picked from commit 30159ab5dd80aef4e1f6f926798aa124ab25e81f)

--- clang/tools/driver/cc1_main.cpp
+++ clang/tools/driver/cc1_main.cpp
@@ -244,7 +244,7 @@ protected:
   clang::cas::CompileJobCacheResult::Builder CachedResultBuilder;
   std::string OutputFile;
   std::string DependenciesFile;
-  std::unique_ptr<cas::CachingDiagnosticsProcessor> DiagProcessor;
+  std::unique_ptr<clang::cas::CachingDiagnosticsProcessor> DiagProcessor;
 };
 
 /// Store and retrieve compilation artifacts using \p llvm::cas::ObjectStore and
@@ -551,8 +551,8 @@ CachingOutputs::CachingOutputs(CompilerInstance &Clang,
   OutputFile = fixupRelativePath(FrontendOpts.OutputFile, FM);
   DependenciesFile =
       fixupRelativePath(Invocation.getDependencyOutputOpts().OutputFile, FM);
-  DiagProcessor =
-      std::make_unique<cas::CachingDiagnosticsProcessor>(PrefixMapper, FM);
+  DiagProcessor = std::make_unique<clang::cas::CachingDiagnosticsProcessor>(
+      PrefixMapper, FM);
 }
 
 Expected<bool> ObjectStoreCachingOutputs::tryReplayCachedResult(
