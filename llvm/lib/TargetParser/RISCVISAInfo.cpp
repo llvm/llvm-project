143cf6030138843a4a81ac3c5eacbc1abfc68ff7
Yeting Kuo <yeting.kuo@sifive.com>
2022-12-27 03:34:29 -0800

    [RISCV] Add Svpbmt extension support.
    Spec of Svpbmt: https://github.com/riscv/riscv-isa-manual/blob/master/src/supervisor.tex#L2399
    
    Reviewed By: kito-cheng
    
    Differential Revision: https://reviews.llvm.org/D140692

--- llvm/lib/TargetParser/RISCVISAInfo.cpp
+++ llvm/lib/TargetParser/RISCVISAInfo.cpp
@@ -103,6 +103,7 @@ static const RISCVSupportedExtension SupportedExtensions[] = {
     {"zicbop", RISCVExtensionVersion{1, 0}},
 
     {"svnapot", RISCVExtensionVersion{1, 0}},
+    {"svpbmt", RISCVExtensionVersion{1, 0}},
     {"svinval", RISCVExtensionVersion{1, 0}},
     {"xventanacondops", RISCVExtensionVersion{1, 0}},
     {"xtheadvdot", RISCVExtensionVersion{1, 0}},
