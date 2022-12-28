9816c1912d56668736a2bd95ce18b7fb41653b92
Freddy Ye <freddy.ye@intel.com>
2022-12-28 16:44:54 +0800

    [X86] Rename CMPCCXADD intrinsics.
    "__cmpccxadd_epi*" -> "_cmpccxadd_epi*"
    This is to align with other intrinsics to follow single leading "_" style. Gcc
    and intrinsic guide website will also apply this change.
    
    Reviewed By: LuoYuanke, skan
    
    Differential Revision: https://reviews.llvm.org/D140281

--- clang/test/CodeGen/X86/cmpccxadd-builtins-error.c
+++ clang/test/CodeGen/X86/cmpccxadd-builtins-error.c
@@ -4,9 +4,9 @@
 #include <immintrin.h>
 
 int test_cmpccxadd32(void *__A, int __B, int __C) {
-  return __cmpccxadd_epi32(__A, __B, __C, 16); // expected-error {{argument value 16 is outside the valid range [0, 15]}}
+  return _cmpccxadd_epi32(__A, __B, __C, 16); // expected-error {{argument value 16 is outside the valid range [0, 15]}}
 }
 
 long long test_cmpccxadd64(void *__A, long long __B, long long __C) {
-  return __cmpccxadd_epi64(__A, __B, __C, 16); // expected-error {{argument value 16 is outside the valid range [0, 15]}}
+  return _cmpccxadd_epi64(__A, __B, __C, 16); // expected-error {{argument value 16 is outside the valid range [0, 15]}}
 }
