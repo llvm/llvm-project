e9ebfd71fba5fc086381033a66e86ba9670411d9
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2022-12-28 16:44:23 -0300

    [CIR][CIRGen] Add boilerplate for bunch of currently unhandled builtins

--- clang/lib/CIR/CodeGen/CIRGenBuiltin.cpp
+++ clang/lib/CIR/CodeGen/CIRGenBuiltin.cpp
@@ -75,13 +75,277 @@ RValue CIRGenFunction::buildBuiltinExpr(const GlobalDecl GD, unsigned BuiltinID,
   if (FD->hasAttr<ConstAttr>() ||
       ((ConstWithoutErrnoAndExceptions || ConstWithoutExceptions) &&
        (!ConstWithoutErrnoAndExceptions || (!getLangOpts().MathErrno)))) {
-    llvm_unreachable("NYI");
+    switch (BuiltinIDIfNoAsmLabel) {
+    case Builtin::BIceil:
+    case Builtin::BIceilf:
+    case Builtin::BIceill:
+    case Builtin::BI__builtin_ceil:
+    case Builtin::BI__builtin_ceilf:
+    case Builtin::BI__builtin_ceilf16:
+    case Builtin::BI__builtin_ceill:
+    case Builtin::BI__builtin_ceilf128:
+      llvm_unreachable("NYI");
+
+    case Builtin::BIcopysign:
+    case Builtin::BIcopysignf:
+    case Builtin::BIcopysignl:
+    case Builtin::BI__builtin_copysign:
+    case Builtin::BI__builtin_copysignf:
+    case Builtin::BI__builtin_copysignf16:
+    case Builtin::BI__builtin_copysignl:
+    case Builtin::BI__builtin_copysignf128:
+      llvm_unreachable("NYI");
+
+    case Builtin::BIcos:
+    case Builtin::BIcosf:
+    case Builtin::BIcosl:
+    case Builtin::BI__builtin_cos:
+    case Builtin::BI__builtin_cosf:
+    case Builtin::BI__builtin_cosf16:
+    case Builtin::BI__builtin_cosl:
+    case Builtin::BI__builtin_cosf128:
+      llvm_unreachable("NYI");
+
+    case Builtin::BIexp:
+    case Builtin::BIexpf:
+    case Builtin::BIexpl:
+    case Builtin::BI__builtin_exp:
+    case Builtin::BI__builtin_expf:
+    case Builtin::BI__builtin_expf16:
+    case Builtin::BI__builtin_expl:
+    case Builtin::BI__builtin_expf128:
+      llvm_unreachable("NYI");
+
+    case Builtin::BIexp2:
+    case Builtin::BIexp2f:
+    case Builtin::BIexp2l:
+    case Builtin::BI__builtin_exp2:
+    case Builtin::BI__builtin_exp2f:
+    case Builtin::BI__builtin_exp2f16:
+    case Builtin::BI__builtin_exp2l:
+    case Builtin::BI__builtin_exp2f128:
+      llvm_unreachable("NYI");
+
+    case Builtin::BIfabs:
+    case Builtin::BIfabsf:
+    case Builtin::BIfabsl:
+    case Builtin::BI__builtin_fabs:
+    case Builtin::BI__builtin_fabsf:
+    case Builtin::BI__builtin_fabsf16:
+    case Builtin::BI__builtin_fabsl:
+    case Builtin::BI__builtin_fabsf128:
+      llvm_unreachable("NYI");
+
+    case Builtin::BIfloor:
+    case Builtin::BIfloorf:
+    case Builtin::BIfloorl:
+    case Builtin::BI__builtin_floor:
+    case Builtin::BI__builtin_floorf:
+    case Builtin::BI__builtin_floorf16:
+    case Builtin::BI__builtin_floorl:
+    case Builtin::BI__builtin_floorf128:
+      llvm_unreachable("NYI");
+
+    case Builtin::BIfma:
+    case Builtin::BIfmaf:
+    case Builtin::BIfmal:
+    case Builtin::BI__builtin_fma:
+    case Builtin::BI__builtin_fmaf:
+    case Builtin::BI__builtin_fmaf16:
+    case Builtin::BI__builtin_fmal:
+    case Builtin::BI__builtin_fmaf128:
+      llvm_unreachable("NYI");
+
+    case Builtin::BIfmax:
+    case Builtin::BIfmaxf:
+    case Builtin::BIfmaxl:
+    case Builtin::BI__builtin_fmax:
+    case Builtin::BI__builtin_fmaxf:
+    case Builtin::BI__builtin_fmaxf16:
+    case Builtin::BI__builtin_fmaxl:
+    case Builtin::BI__builtin_fmaxf128:
+      llvm_unreachable("NYI");
+
+    case Builtin::BIfmin:
+    case Builtin::BIfminf:
+    case Builtin::BIfminl:
+    case Builtin::BI__builtin_fmin:
+    case Builtin::BI__builtin_fminf:
+    case Builtin::BI__builtin_fminf16:
+    case Builtin::BI__builtin_fminl:
+    case Builtin::BI__builtin_fminf128:
+      llvm_unreachable("NYI");
+
+    // fmod() is a special-case. It maps to the frem instruction rather than an
+    // LLVM intrinsic.
+    case Builtin::BIfmod:
+    case Builtin::BIfmodf:
+    case Builtin::BIfmodl:
+    case Builtin::BI__builtin_fmod:
+    case Builtin::BI__builtin_fmodf:
+    case Builtin::BI__builtin_fmodf16:
+    case Builtin::BI__builtin_fmodl:
+    case Builtin::BI__builtin_fmodf128: {
+      llvm_unreachable("NYI");
+    }
+
+    case Builtin::BIlog:
+    case Builtin::BIlogf:
+    case Builtin::BIlogl:
+    case Builtin::BI__builtin_log:
+    case Builtin::BI__builtin_logf:
+    case Builtin::BI__builtin_logf16:
+    case Builtin::BI__builtin_logl:
+    case Builtin::BI__builtin_logf128:
+      llvm_unreachable("NYI");
+
+    case Builtin::BIlog10:
+    case Builtin::BIlog10f:
+    case Builtin::BIlog10l:
+    case Builtin::BI__builtin_log10:
+    case Builtin::BI__builtin_log10f:
+    case Builtin::BI__builtin_log10f16:
+    case Builtin::BI__builtin_log10l:
+    case Builtin::BI__builtin_log10f128:
+      llvm_unreachable("NYI");
+
+    case Builtin::BIlog2:
+    case Builtin::BIlog2f:
+    case Builtin::BIlog2l:
+    case Builtin::BI__builtin_log2:
+    case Builtin::BI__builtin_log2f:
+    case Builtin::BI__builtin_log2f16:
+    case Builtin::BI__builtin_log2l:
+    case Builtin::BI__builtin_log2f128:
+      llvm_unreachable("NYI");
+
+    case Builtin::BInearbyint:
+    case Builtin::BInearbyintf:
+    case Builtin::BInearbyintl:
+    case Builtin::BI__builtin_nearbyint:
+    case Builtin::BI__builtin_nearbyintf:
+    case Builtin::BI__builtin_nearbyintl:
+    case Builtin::BI__builtin_nearbyintf128:
+      llvm_unreachable("NYI");
+
+    case Builtin::BIpow:
+    case Builtin::BIpowf:
+    case Builtin::BIpowl:
+    case Builtin::BI__builtin_pow:
+    case Builtin::BI__builtin_powf:
+    case Builtin::BI__builtin_powf16:
+    case Builtin::BI__builtin_powl:
+    case Builtin::BI__builtin_powf128:
+      llvm_unreachable("NYI");
+
+    case Builtin::BIrint:
+    case Builtin::BIrintf:
+    case Builtin::BIrintl:
+    case Builtin::BI__builtin_rint:
+    case Builtin::BI__builtin_rintf:
+    case Builtin::BI__builtin_rintf16:
+    case Builtin::BI__builtin_rintl:
+    case Builtin::BI__builtin_rintf128:
+      llvm_unreachable("NYI");
+
+    case Builtin::BIround:
+    case Builtin::BIroundf:
+    case Builtin::BIroundl:
+    case Builtin::BI__builtin_round:
+    case Builtin::BI__builtin_roundf:
+    case Builtin::BI__builtin_roundf16:
+    case Builtin::BI__builtin_roundl:
+    case Builtin::BI__builtin_roundf128:
+      llvm_unreachable("NYI");
+
+    case Builtin::BIsin:
+    case Builtin::BIsinf:
+    case Builtin::BIsinl:
+    case Builtin::BI__builtin_sin:
+    case Builtin::BI__builtin_sinf:
+    case Builtin::BI__builtin_sinf16:
+    case Builtin::BI__builtin_sinl:
+    case Builtin::BI__builtin_sinf128:
+      llvm_unreachable("NYI");
+
+    case Builtin::BIsqrt:
+    case Builtin::BIsqrtf:
+    case Builtin::BIsqrtl:
+    case Builtin::BI__builtin_sqrt:
+    case Builtin::BI__builtin_sqrtf:
+    case Builtin::BI__builtin_sqrtf16:
+    case Builtin::BI__builtin_sqrtl:
+    case Builtin::BI__builtin_sqrtf128:
+      llvm_unreachable("NYI");
+
+    case Builtin::BItrunc:
+    case Builtin::BItruncf:
+    case Builtin::BItruncl:
+    case Builtin::BI__builtin_trunc:
+    case Builtin::BI__builtin_truncf:
+    case Builtin::BI__builtin_truncf16:
+    case Builtin::BI__builtin_truncl:
+    case Builtin::BI__builtin_truncf128:
+      llvm_unreachable("NYI");
+
+    case Builtin::BIlround:
+    case Builtin::BIlroundf:
+    case Builtin::BIlroundl:
+    case Builtin::BI__builtin_lround:
+    case Builtin::BI__builtin_lroundf:
+    case Builtin::BI__builtin_lroundl:
+    case Builtin::BI__builtin_lroundf128:
+      llvm_unreachable("NYI");
+
+    case Builtin::BIllround:
+    case Builtin::BIllroundf:
+    case Builtin::BIllroundl:
+    case Builtin::BI__builtin_llround:
+    case Builtin::BI__builtin_llroundf:
+    case Builtin::BI__builtin_llroundl:
+    case Builtin::BI__builtin_llroundf128:
+      llvm_unreachable("NYI");
+
+    case Builtin::BIlrint:
+    case Builtin::BIlrintf:
+    case Builtin::BIlrintl:
+    case Builtin::BI__builtin_lrint:
+    case Builtin::BI__builtin_lrintf:
+    case Builtin::BI__builtin_lrintl:
+    case Builtin::BI__builtin_lrintf128:
+      llvm_unreachable("NYI");
+
+    case Builtin::BIllrint:
+    case Builtin::BIllrintf:
+    case Builtin::BIllrintl:
+    case Builtin::BI__builtin_llrint:
+    case Builtin::BI__builtin_llrintf:
+    case Builtin::BI__builtin_llrintl:
+    case Builtin::BI__builtin_llrintf128:
+      llvm_unreachable("NYI");
+
+    default:
+      break;
+    }
   }
 
   switch (BuiltinIDIfNoAsmLabel) {
   default:
     llvm_unreachable("NYI");
     break;
+
+  // C++ std:: builtins.
+  case Builtin::BImove:
+  case Builtin::BImove_if_noexcept:
+  case Builtin::BIforward:
+  case Builtin::BIas_const:
+    llvm_unreachable("NYI");
+  case Builtin::BI__GetExceptionInfo: {
+    llvm_unreachable("NYI");
+  }
+
+  case Builtin::BI__fastfail:
+    llvm_unreachable("NYI");
 
   case Builtin::BI__builtin_coro_id:
   case Builtin::BI__builtin_coro_promise:
