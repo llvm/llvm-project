e2e1c2b26040428d04536b558ba148e08fcd09e2
William Huang <williamjhuang@google.com>
2022-12-28 21:26:16 +0000

    [llvm-profdata] Remove unnecessary file size check
    Unsure why profile reader checks profile size to be less than 4 GB. This breaks builds using a very large profile.
    The limit is not seen anywhere else, so I am not sure why is it there in the first place.
    
    Reviewed By: davidxl
    
    Differential Revision: https://reviews.llvm.org/D140741

--- llvm/lib/ProfileData/SampleProfReader.cpp
+++ llvm/lib/ProfileData/SampleProfReader.cpp
@@ -1837,10 +1837,6 @@ setupMemoryBuffer(const Twine &Filename) {
     return EC;
   auto Buffer = std::move(BufferOrErr.get());
 
-  // Check the file.
-  if (uint64_t(Buffer->getBufferSize()) > std::numeric_limits<uint32_t>::max())
-    return sampleprof_error::too_large;
-
   return std::move(Buffer);
 }
 
