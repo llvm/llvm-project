3d56da5087e8a1171234034ddb0f24bcb690241e
Ron Lieberman <Ron.Lieberman@amd.com>
2022-12-28 19:31:57 -0500

    [libompd] CMake include issue for python module DynamicLibrary.h
    plugin occasionally could not find header, varied by system
    
    Change-Id: I3c5df0822ef8274099bffdafe45941966aa11359

--- openmp/libompd/gdb-plugin/setup.py
+++ openmp/libompd/gdb-plugin/setup.py
@@ -4,6 +4,7 @@ import os
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 omp_include_dir = os.environ.get('LIBOMP_INCLUDE_DIR', dir_path)
+llvm_include_dir = os.environ.get('LLVM_MAIN_INCLUDE_DIR', dir_path)
 python_include_dir = os.environ.get('PYTHON_HEADERS', dir_path)
 
 print("find_packages : ", find_packages())
@@ -13,5 +14,5 @@ setup(
     py_modules=['loadompd'],
     setup_requires=['wheel'],
     packages=find_packages(),
-	ext_modules=[Extension('ompd.ompdModule', [dir_path+'/ompdModule.c', dir_path+'/ompdAPITests.c', dir_path+'/DLSymService.cpp'], include_dirs=[omp_include_dir])]
+	ext_modules=[Extension('ompd.ompdModule', [dir_path+'/ompdModule.c', dir_path+'/ompdAPITests.c', dir_path+'/DLSymService.cpp'], include_dirs=[omp_include_dir, llvm_include_dir])]
 )
