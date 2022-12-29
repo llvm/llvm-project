c1a4fef66a7ec1796385564aea9c3a34b7386a42
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2022-12-28 21:05:37 -0300

    [CIR][CIRGen] Add initial support for std::move
    There's likely some more higher level modeling needed, but
    this is a start for unblocking some coroutine testing.

--- clang/lib/CIR/CodeGen/CIRGenTypes.h
+++ clang/lib/CIR/CodeGen/CIRGenTypes.h
@@ -138,6 +138,12 @@ public:
   clang::ASTContext &getContext() const { return Context; }
   mlir::MLIRContext &getMLIRContext() const;
 
+  bool isRecordLayoutComplete(const clang::Type *Ty) const;
+  bool noRecordsBeingLaidOut() const { return RecordsBeingLaidOut.empty(); }
+  bool isRecordBeingLaidOut(const clang::Type *Ty) const {
+    return RecordsBeingLaidOut.count(Ty);
+  }
+
   const ABIInfo &getABIInfo() const { return TheABIInfo; }
   CIRGenCXXABI &getCXXABI() const { return TheCXXABI; }
 
@@ -240,8 +246,6 @@ public:
       clang::FunctionType::ExtInfo info,
       llvm::ArrayRef<clang::FunctionProtoType::ExtParameterInfo> paramInfos,
       RequiredArgs args);
-
-  bool noRecordsBeingLaidOut() const { return RecordsBeingLaidOut.empty(); }
 };
 } // namespace cir
 
