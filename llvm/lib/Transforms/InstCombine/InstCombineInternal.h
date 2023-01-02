cf58063a4069b9ff11a99fd49345c1d188813feb
Roman Lebedev <lebedev.ri@gmail.com>
2023-01-02 21:15:31 +0300

    [InstCombine] Canonicalize math-y conditional negation into a `select`
    https://alive2.llvm.org/ce/z/vPs-gZ
    
    This is a larger pattern than would seem necessary, with minimal being:
    * `and` https://alive2.llvm.org/ce/z/q9-MqK
    * `or`  https://alive2.llvm.org/ce/z/AUUEMZ
    * `xor` https://alive2.llvm.org/ce/z/dm3Ume
    .. so for all others, we canonicalize away from math to `select`,
    but there we canonicalize in the opposite direction.
    
    Fixes https://github.com/llvm/llvm-project/issues/59791

--- llvm/lib/Transforms/InstCombine/InstCombineInternal.h
+++ llvm/lib/Transforms/InstCombine/InstCombineInternal.h
@@ -365,6 +365,9 @@ private:
   Instruction *foldLogicOfIsFPClass(BinaryOperator &Operator, Value *LHS,
                                     Value *RHS);
 
+  Instruction *
+  canonicalizeConditionalNegationViaMathToSelect(BinaryOperator &i);
+
   Value *foldAndOrOfICmpsOfAndWithPow2(ICmpInst *LHS, ICmpInst *RHS,
                                        Instruction *CxtI, bool IsAnd,
                                        bool IsLogical = false);
