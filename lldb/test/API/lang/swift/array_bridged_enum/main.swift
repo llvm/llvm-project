65d26048e81597f6472aaf7dce8bf32a7efc4b98
Augusto Noronha <augusto2112@me.com>
2023-01-03 15:23:05 -0800

    [lldb] Prefer an exact match over ORed cases on SwiftOptionSet frmting
    rdar://88821820
    (cherry picked from commit 24b3eb6f04ffd52f1f6ac400ad93fc18b1d4c939)

--- lldb/test/API/lang/swift/array_bridged_enum/main.swift
+++ lldb/test/API/lang/swift/array_bridged_enum/main.swift
@@ -2,4 +2,4 @@ let array = [SampleEnum.zero, SampleEnum.one, SampleEnum.two, SampleEnum.three]
 print(array) //%self.expect('frame variable array[0]', substrs=['[0]', '.SampleEnumZero'])
              //%self.expect('frame variable array[1]', substrs=['[1]', '.SampleEnumOne'])
              //%self.expect('frame variable array[2]', substrs=['[2]', '.SampleEnumTwo'])
-             //FIXME: frame variable array[3] returns [.SampleEnumOne, .SampleEnumTwo]
+             //%self.expect('frame variable array[3]', substrs=['[3]', '.SampleEnumThree'])
