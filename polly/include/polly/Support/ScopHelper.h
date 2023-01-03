ccdc271a0899c2225a9cd2db1af61c1d5cf8fcad
Kazu Hirata <kazu@google.com>
2023-01-02 19:18:46 -0800

    [polly] Use std::optional instead of llvm::Optional (NFC)
    This is part of an effort to migrate from llvm::Optional to
    std::optional:
    
    https://discourse.llvm.org/t/deprecating-llvm-optional-x-hasvalue-getvalue-getvalueor/63716

--- polly/include/polly/Support/ScopHelper.h
+++ polly/include/polly/Support/ScopHelper.h
@@ -18,6 +18,7 @@
 #include "llvm/IR/IntrinsicInst.h"
 #include "llvm/IR/ValueHandle.h"
 #include "isl/isl-noexceptions.h"
+#include <optional>
 
 namespace llvm {
 class LoopInfo;
@@ -522,16 +523,16 @@ bool hasDebugCall(ScopStmt *Stmt);
 ///
 /// Then `nullptr` is set to mark the property is existing, but does not carry
 /// any value. If the property does not exist, `None` is returned.
-llvm::Optional<llvm::Metadata *> findMetadataOperand(llvm::MDNode *LoopMD,
-                                                     llvm::StringRef Name);
+std::optional<llvm::Metadata *> findMetadataOperand(llvm::MDNode *LoopMD,
+                                                    llvm::StringRef Name);
 
 /// Find a boolean property value in a LoopID. The value not being defined is
 /// interpreted as a false value.
 bool getBooleanLoopAttribute(llvm::MDNode *LoopID, llvm::StringRef Name);
 
 /// Find an integers property value in a LoopID.
-llvm::Optional<int> getOptionalIntLoopAttribute(llvm::MDNode *LoopID,
-                                                llvm::StringRef Name);
+std::optional<int> getOptionalIntLoopAttribute(llvm::MDNode *LoopID,
+                                               llvm::StringRef Name);
 
 /// Does the loop's LoopID contain a 'llvm.loop.disable_heuristics' property?
 ///
