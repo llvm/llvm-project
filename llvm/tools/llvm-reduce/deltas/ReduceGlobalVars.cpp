a50cec5bf9ede33881988e4b9c3bb9b5ee5f7178
Matt Arsenault <Matthew.Arsenault@amd.com>
2023-01-04 17:46:03 -0500

    llvm-reduce: Don't delete instructions in global variable reduction
    For some reason the global variable reduction was trying to delete
    use instructions. This broke the verifier if the user was a terminator,
    since the block now no longer has one. It doesn't make sense for this
    reduction to delete the users, so just stop doing that.

--- llvm/tools/llvm-reduce/deltas/ReduceGlobalVars.cpp
+++ llvm/tools/llvm-reduce/deltas/ReduceGlobalVars.cpp
@@ -15,49 +15,36 @@
 #include "Utils.h"
 #include "llvm/IR/Constants.h"
 #include "llvm/Transforms/Utils/ModuleUtils.h"
-#include <set>
 
 using namespace llvm;
+
+static bool shouldAlwaysKeep(const GlobalVariable &GV) {
+  return GV.getName() == "llvm.used" || GV.getName() == "llvm.compiler.used";
+}
 
 /// Removes all the GVs that aren't inside the desired Chunks.
 static void extractGVsFromModule(Oracle &O, Module &Program) {
   // Get GVs inside desired chunks
   std::vector<Constant *> InitGVsToKeep;
   for (auto &GV : Program.globals()) {
-    if ((GV.getName() == "llvm.used" || GV.getName() == "llvm.compiler.used") ||
-        O.shouldKeep())
+    if (shouldAlwaysKeep(GV) || O.shouldKeep())
       InitGVsToKeep.push_back(&GV);
   }
 
   // We create a vector first, then convert it to a set, so that we don't have
   // to pay the cost of rebalancing the set frequently if the order we insert
   // the elements doesn't match the order they should appear inside the set.
-  std::set<Constant *> GVsToKeep(InitGVsToKeep.begin(), InitGVsToKeep.end());
+  DenseSet<Constant *> GVsToKeep(InitGVsToKeep.begin(), InitGVsToKeep.end());
 
   // Delete out-of-chunk GVs and their uses
   DenseSet<Constant *> ToRemove;
-  std::vector<WeakVH> InstToRemove;
   for (auto &GV : Program.globals()) {
-    if (!GVsToKeep.count(&GV)) {
-      for (auto *U : GV.users())
-        if (auto *Inst = dyn_cast<Instruction>(U))
-          InstToRemove.push_back(Inst);
-
+    if (!GVsToKeep.count(&GV))
       ToRemove.insert(&GV);
-    }
   }
 
   removeFromUsedLists(Program,
                       [&ToRemove](Constant *C) { return ToRemove.count(C); });
-
-  // Delete (unique) Instruction uses of unwanted GVs
-  for (Value *V : InstToRemove) {
-    if (!V)
-      continue;
-    auto *Inst = cast<Instruction>(V);
-    Inst->replaceAllUsesWith(getDefaultValue(Inst->getType()));
-    Inst->eraseFromParent();
-  }
 
   for (auto *GV : ToRemove) {
     GV->replaceAllUsesWith(getDefaultValue(GV->getType()));
