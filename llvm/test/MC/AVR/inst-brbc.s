86edcc81e7a1eba2d39f996101f3ccbe78d59f0d
Ben Shi <powerman1st@163.com>
2023-01-07 15:25:38 +0800

    [AVR] Fix incorrect decoding of conditional branch instructions
    This patch fixes the inaccurate decoding of the offset operand of
    the conditional branch instructions.
    
    Reviewed By: aykevl
    
    Differential Revision: https://reviews.llvm.org/D140816

--- llvm/test/MC/AVR/inst-brbc.s
+++ llvm/test/MC/AVR/inst-brbc.s
@@ -1,12 +1,24 @@
 ; RUN: llvm-mc -triple avr -show-encoding < %s | FileCheck %s
-
+; RUN: llvm-mc -filetype=obj -triple avr < %s \
+; RUN:     | llvm-objdump -d - | FileCheck --check-prefix=INST %s
 
 foo:
 
   brbc 3, .+8
   brbc 0, .-16
+  .short 0xf759
+  .short 0xf752
+  .short 0xf74c
+  .short 0xf4c7
 
 ; CHECK: brvc .Ltmp0+8              ; encoding: [0bAAAAA011,0b111101AA]
 ; CHECK:                            ; fixup A - offset: 0, value: .Ltmp0+8, kind: fixup_7_pcrel
 ; CHECK: brcc .Ltmp1-16             ; encoding: [0bAAAAA000,0b111101AA]
 ; CHECK:                            ; fixup A - offset: 0, value: .Ltmp1-16, kind: fixup_7_pcrel
+
+; INST: brvc .+0
+; INST: brsh .+0
+; INST: brne .-42
+; INST: brpl .-44
+; INST: brge .-46
+; INST: brid .+48
