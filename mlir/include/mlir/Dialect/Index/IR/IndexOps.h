455305624884cf9237143e2ba0635fcc5ba5206a
Krzysztof Drewniak <Krzysztof.Drewniak@amd.com>
2023-01-09 17:09:17 +0000

    [mlir][Index] Implement InferIntRangeInterface
    Implement InferIntRangeInterface for all operations in the Index dialect. The
    inference implementation, unlike the one for Arith, accounts for the
    fact that Index can be either 64 or 32 bits long by evaluating both
    cases. Bounds are stored as if index were i64, but when inferring new
    bounds, we compute both f(...) and f(trunc(...)). We then compare
    trunc(f(...)) to f(trunc(...)). If they are equal in the relevant
    range components, we use the 64-bit range computation, otherwise we
    give the range ext(f(trunc(...))) union f(...).
    
    Note that this can cause surprising behavior as seen in the tests,
    where, for example, the order of min and max operations impacts the
    behavior of the inference. The inference could perhaps be made more
    precise in the future (ex. by tracking 32 and 64-bit results
    separately and having them influence each other somehow) butt, since
    my project targets an index=i32 platform and doesn't see index-valued
    values > uint32_max, I'm not too concerned about it.
    
    Depends on https://reviews.llvm.org/D141299
    
    Depends on https://reviews.llvm.org/D141296
    
    Reviewed By: Mogball
    
    Differential Revision: https://reviews.llvm.org/D140899

--- mlir/include/mlir/Dialect/Index/IR/IndexOps.h
+++ mlir/include/mlir/Dialect/Index/IR/IndexOps.h
@@ -13,6 +13,7 @@
 #include "mlir/IR/OpDefinition.h"
 #include "mlir/IR/OpImplementation.h"
 #include "mlir/Interfaces/CastInterfaces.h"
+#include "mlir/Interfaces/InferIntRangeInterface.h"
 #include "mlir/Interfaces/InferTypeOpInterface.h"
 #include "mlir/Interfaces/SideEffectInterfaces.h"
 
