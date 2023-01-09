a0d2e170f78c05f9a5d86f737f4f83e410cbd78c
Alex Brachet <abrachet@google.com>
2023-01-09 20:24:16 +0000

    Revert "[libc] Templatize str{,n}cmp"
    This reverts commit 08facdf3ca6bb744161d5ec087dbd7019c002a7e.
    
    Broke tests on arm and aarch64

--- libc/src/__support/CPP/functional.h
+++ libc/src/__support/CPP/functional.h
@@ -24,10 +24,6 @@ public:
   constexpr R operator()(Args... params) { return func(params...); }
 };
 
-template <class T> struct minus {
-  constexpr T operator()(const T &lhs, const T &rhs) const { return lhs - rhs; }
-};
-
 } // namespace cpp
 } // namespace __llvm_libc
 
