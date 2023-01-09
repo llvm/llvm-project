d43549f81c2231144297e90512ff8a57912507eb
Michael Francis <michaelfrancis@ibm.com>
2023-01-09 18:35:54 +0000

    [z/OS][pg] Throw error when using -pg on z/OS
    Throw an error when trying to compile with `-pg` on z/OS,
    as the platform does not support `gprof`.
    
    Reviewed By: cebowleratibm, MaskRay
    
    Differential Revision: https://reviews.llvm.org/D137756

--- clang/lib/Driver/ToolChains/Clang.cpp
+++ clang/lib/Driver/ToolChains/Clang.cpp
@@ -6338,6 +6338,12 @@ void Clang::ConstructJob(Compilation &C, const JobAction &JA,
           << A->getAsString(Args) << TripleStr;
     }
   }
+  if (Arg *A = Args.getLastArgNoClaim(options::OPT_pg)) {
+    if (TC.getTriple().isOSzOS()) {
+      D.Diag(diag::err_drv_unsupported_opt_for_target)
+          << A->getAsString(Args) << TripleStr;
+    }
+  }
 
   if (Args.getLastArg(options::OPT_fapple_kext) ||
       (Args.hasArg(options::OPT_mkernel) && types::isCXX(InputType)))
