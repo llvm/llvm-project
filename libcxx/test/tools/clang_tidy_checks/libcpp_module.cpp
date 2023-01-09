06385491a6d4b34b7d3e3928e0afa3ddc56e4898
Nikolas Klauser <nikolasklauser@berlin.de>
2023-01-09 03:01:26 +0100

    [libc++] Refactor clang-query checks to clang-tidy checks to get less obscure error messages
    Also remove clang-query related code, since it's unused now.
    
    Reviewed By: ldionne, Mordante, #libc
    
    Spies: libcxx-commits, arichardson
    
    Differential Revision: https://reviews.llvm.org/D141805

--- libcxx/test/tools/clang_tidy_checks/libcpp_module.cpp
+++ libcxx/test/tools/clang_tidy_checks/libcpp_module.cpp
@@ -8,6 +8,9 @@
 
 #include "clang-tidy/ClangTidyModule.h"
 #include "clang-tidy/ClangTidyModuleRegistry.h"
+
+#include "abi_tag_on_virtual.hpp"
+#include "hide_from_abi.hpp"
 #include "robust_against_adl.hpp"
 #include "qualify_declval.hpp"
 
@@ -15,6 +18,8 @@ namespace {
 class LibcxxTestModule : public clang::tidy::ClangTidyModule {
 public:
   void addCheckFactories(clang::tidy::ClangTidyCheckFactories& check_factories) override {
+    check_factories.registerCheck<libcpp::abi_tag_on_virtual>("libcpp-avoid-abi-tag-on-virtual");
+    check_factories.registerCheck<libcpp::hide_from_abi>("libcpp-hide-from-abi");
     check_factories.registerCheck<libcpp::robust_against_adl_check>("libcpp-robust-against-adl");
     check_factories.registerCheck<libcpp::qualify_declval>("libcpp-qualify-declval");
   }
