3ed58d4df65e00b0ed3679551394f794170233d0
Tim Northover <tnorthover@apple.com>
2023-01-10 11:52:12 +0000

    AArch64: allocate small fixed args properly in varargs functions.
    On Darwin, function arguments occupy their real size when passed on the stack
    (e.g. an i16 only consumes 2 bytes). This means that, even for fixed args in
    varargs calls we need to keep track of the original type being passed before
    any DAG/GISel promotions. Existing logic only applied this fix to the
    non-varargs case leading to mismatch between caller & callee in those
    situations.
    
    On Linux & Windows these arguments always occupy a 64-bit slot anyway so
    there's no special handling needed.

--- llvm/lib/Target/AArch64/AArch64ISelLowering.cpp
+++ llvm/lib/Target/AArch64/AArch64ISelLowering.cpp
@@ -6856,7 +6856,9 @@ static void analyzeCallOperands(const AArch64TargetLowering &TLI,
       } else {
         UseVarArgCC = !Outs[i].IsFixed;
       }
-    } else {
+    }
+
+    if (!UseVarArgCC) {
       // Get type of the original argument.
       EVT ActualVT =
           TLI.getValueType(DAG.getDataLayout(), CLI.Args[Outs[i].OrigArgIndex].Ty,
