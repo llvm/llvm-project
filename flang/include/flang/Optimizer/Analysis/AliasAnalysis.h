33a7f162d627f522a5dedc06ce2de4a30b2588bc
Slava Zakharin <szakharin@nvidia.com>
2023-01-10 10:24:48 -0800

    [flang] Added coarse grained alias analysis for FIR.
    These are experimental changes in Flang AA to provide
    at least some means to disambiguate memory accesses in some
    simple cases. This AA is still not used by any transformation,
    so the LIT tests are the only way to trigger it currently.
    I will further look into applying this AA within Flang
    to address some of the known performance issues in the benchmarks.
    
    Credits to @Renaud-K for the initial implementation.
    
    Differential Revision: https://reviews.llvm.org/D141410

--- flang/include/flang/Optimizer/Analysis/AliasAnalysis.h
+++ flang/include/flang/Optimizer/Analysis/AliasAnalysis.h
@@ -1,4 +1,4 @@
-//===- AliasAnalysis.h - Alias Analysis in FIR -----------------*- C++ -*-===//
+//===-- AliasAnalysis.h - Alias Analysis in FIR -----------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,10 +6,14 @@
 //
 //===----------------------------------------------------------------------===//
 
-#ifndef FIR_ANALYSIS_ALIASANALYSIS_H_
-#define FIR_ANALYSIS_ALIASANALYSIS_H_
+#ifndef FORTRAN_OPTIMIZER_ANALYSIS_ALIASANALYSIS_H
+#define FORTRAN_OPTIMIZER_ANALYSIS_ALIASANALYSIS_H
 
+#include "flang/Common/enum-class.h"
+#include "flang/Common/enum-set.h"
 #include "mlir/Analysis/AliasAnalysis.h"
+#include "mlir/IR/Value.h"
+#include "llvm/ADT/PointerUnion.h"
 
 namespace fir {
 
@@ -17,13 +21,77 @@ namespace fir {
 // AliasAnalysis
 //===----------------------------------------------------------------------===//
 class AliasAnalysis {
+  // Structures to describe the memory source of a value.
+
+  /// Kind of the memory source referenced by a value.
+  ENUM_CLASS(SourceKind,
+             /// Unique memory allocated by an operation, e.g.
+             /// by fir::AllocaOp or fir::AllocMemOp.
+             Allocate,
+             /// A global object allocated statically on the module level.
+             Global,
+             /// Memory allocated outside of a function and passed
+             /// to the function as a by-ref argument.
+             Argument,
+             /// Represents memory allocated by unknown means and
+             /// with the memory address defined by a memory reading
+             /// operation (e.g. fir::LoadOp).
+             Indirect,
+             /// Represents memory allocated by unknown means.
+             Unknown);
+
+  /// Attributes of the memory source object.
+  ENUM_CLASS(Attribute, Target, Pointer, IntentIn);
+
+  struct Source {
+    using SourceUnion = llvm::PointerUnion<mlir::SymbolRefAttr, mlir::Value>;
+    using Attributes = Fortran::common::EnumSet<Attribute, Attribute_enumSize>;
+
+    /// Source definition of a value.
+    SourceUnion u;
+    /// Kind of the memory source.
+    SourceKind kind;
+    /// Value type of the source definition.
+    mlir::Type valueType;
+    /// Attributes of the memory source object, e.g. Target.
+    Attributes attributes;
+
+    /// Print information about the memory source to `os`.
+    void print(llvm::raw_ostream &os) const;
+
+    /// Return true, if Target or Pointer attribute is set.
+    bool isTargetOrPointer() const;
+
+    /// Return true, if the memory source's `valueType` is a reference type
+    /// to an object of derived type that contains a component with POINTER
+    /// attribute.
+    bool isRecordWithPointerComponent() const;
+
+    /// Return true, if `ty` is a reference type to a boxed
+    /// POINTER object or a raw fir::PointerType.
+    static bool isPointerReference(mlir::Type ty);
+  };
+
+  friend llvm::raw_ostream &operator<<(llvm::raw_ostream &os,
+                                       const AliasAnalysis::Source &op);
+
 public:
   /// Given two values, return their aliasing behavior.
   mlir::AliasResult alias(mlir::Value lhs, mlir::Value rhs);
 
   /// Return the modify-reference behavior of `op` on `location`.
   mlir::ModRefResult getModRef(mlir::Operation *op, mlir::Value location);
+
+  /// Return the memory source of a value.
+  Source getSource(mlir::Value);
 };
+
+inline llvm::raw_ostream &operator<<(llvm::raw_ostream &os,
+                                     const AliasAnalysis::Source &op) {
+  op.print(os);
+  return os;
+}
+
 } // namespace fir
 
-#endif // FIR_ANALYSIS_ALIASANALYSIS_H_
+#endif // FORTRAN_OPTIMIZER_ANALYSIS_ALIASANALYSIS_H
