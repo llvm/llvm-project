36117cc46388d677359f1180bd536f80d0c5fe97
serge-sans-paille <sguelton@mozilla.com>
2023-01-10 11:49:15 +0100

    [llvm] Declare llvm::makeArrayRef as obsolete
    Also cleans up a few remaining references.
    
    This is the final commit for the series started by
    https://reviews.llvm.org/D140896

--- llvm/include/llvm/ADT/ArrayRef.h
+++ llvm/include/llvm/ADT/ArrayRef.h
@@ -491,10 +491,10 @@ namespace llvm {
   template <typename T, std::size_t N>
   ArrayRef(const std::array<T, N> &Vec) -> ArrayRef<T>;
 
-  /// Deduction guide to construct an ArrayRef from an ArrayRef (no-op) (const)
+  /// Deduction guide to construct an ArrayRef from an ArrayRef (const)
   template <typename T> ArrayRef(const ArrayRef<T> &Vec) -> ArrayRef<T>;
 
-  /// Deduction guide to construct an ArrayRef from an ArrayRef (no-op)
+  /// Deduction guide to construct an ArrayRef from an ArrayRef
   template <typename T> ArrayRef(ArrayRef<T> &Vec) -> ArrayRef<T>;
 
   /// Deduction guide to construct an ArrayRef from a C array.
@@ -505,59 +505,71 @@ namespace llvm {
   /// @name ArrayRef Convenience constructors
   /// @{
   /// Construct an ArrayRef from a single element.
-  template<typename T>
+  template <typename T>
+  LLVM_DEPRECATED("Use deduction guide instead", "ArrayRef")
   ArrayRef<T> makeArrayRef(const T &OneElt) {
     return OneElt;
   }
 
   /// Construct an ArrayRef from a pointer and length.
-  template<typename T>
+  template <typename T>
+  LLVM_DEPRECATED("Use deduction guide instead", "ArrayRef")
   ArrayRef<T> makeArrayRef(const T *data, size_t length) {
     return ArrayRef<T>(data, length);
   }
 
   /// Construct an ArrayRef from a range.
-  template<typename T>
+  template <typename T>
+  LLVM_DEPRECATED("Use deduction guide instead", "ArrayRef")
   ArrayRef<T> makeArrayRef(const T *begin, const T *end) {
     return ArrayRef<T>(begin, end);
   }
 
   /// Construct an ArrayRef from a SmallVector.
   template <typename T>
+  LLVM_DEPRECATED("Use deduction guide instead", "ArrayRef")
   ArrayRef<T> makeArrayRef(const SmallVectorImpl<T> &Vec) {
     return Vec;
   }
 
   /// Construct an ArrayRef from a SmallVector.
   template <typename T, unsigned N>
+  LLVM_DEPRECATED("Use deduction guide instead", "ArrayRef")
   ArrayRef<T> makeArrayRef(const SmallVector<T, N> &Vec) {
     return Vec;
   }
 
   /// Construct an ArrayRef from a std::vector.
-  template<typename T>
+  template <typename T>
+  LLVM_DEPRECATED("Use deduction guide instead", "ArrayRef")
   ArrayRef<T> makeArrayRef(const std::vector<T> &Vec) {
     return Vec;
   }
 
   /// Construct an ArrayRef from a std::array.
   template <typename T, std::size_t N>
+  LLVM_DEPRECATED("Use deduction guide instead", "ArrayRef")
   ArrayRef<T> makeArrayRef(const std::array<T, N> &Arr) {
     return Arr;
   }
 
   /// Construct an ArrayRef from an ArrayRef (no-op) (const)
-  template <typename T> ArrayRef<T> makeArrayRef(const ArrayRef<T> &Vec) {
+  template <typename T>
+  LLVM_DEPRECATED("Use deduction guide instead", "ArrayRef")
+  ArrayRef<T> makeArrayRef(const ArrayRef<T> &Vec) {
     return Vec;
   }
 
   /// Construct an ArrayRef from an ArrayRef (no-op)
-  template <typename T> ArrayRef<T> &makeArrayRef(ArrayRef<T> &Vec) {
+  template <typename T>
+  LLVM_DEPRECATED("Use deduction guide instead", "ArrayRef")
+  ArrayRef<T> &makeArrayRef(ArrayRef<T> &Vec) {
     return Vec;
   }
 
   /// Construct an ArrayRef from a C array.
-  template<typename T, size_t N>
+  template <typename T, size_t N>
+  LLVM_DEPRECATED("Use deduction guide instead", "ArrayRef")
   ArrayRef<T> makeArrayRef(const T (&Arr)[N]) {
     return ArrayRef<T>(Arr);
   }
