e828dd43027e1d2d096719e70d1e18d7bb9562c3
David Green <david.green@arm.com>
2023-01-11 15:58:38 +0000

    [ARM] Don't treat arguments as producesFalseLanesZero
    Invalid tail predicated loops could be formed by treating function
    arguments as FalseLanesZero due to getGlobalReachingDefs not returning
    any values. Make sure we check that the list of Defs is empty and if so
    treat it like a unknown value.
    
    Differential Revision: https://reviews.llvm.org/D141399

--- llvm/lib/Target/ARM/ARMLowOverheadLoops.cpp
+++ llvm/lib/Target/ARM/ARMLowOverheadLoops.cpp
@@ -916,6 +916,8 @@ static bool producesFalseLanesZero(MachineInstr &MI,
     //   false lane zeros, so we can ignore the uses.
     SmallPtrSet<MachineInstr *, 2> Defs;
     RDA.getGlobalReachingDefs(&MI, MO.getReg(), Defs);
+    if (Defs.empty())
+      return false;
     for (auto *Def : Defs) {
       if (Def == &MI || FalseLanesZero.count(Def) || IsZeroInit(Def))
         continue;
