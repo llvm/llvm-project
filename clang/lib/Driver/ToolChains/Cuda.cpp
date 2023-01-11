4921b0a285ace77b7a3d5b9502b8602134daee0d
Kadir Cetinkaya <kadircet@google.com>
2023-01-11 09:57:37 +0100

    [clang][Driver][CUDA] Get rid of unused LibPath
    LibPath discovered during InstallationDetection wasn't used anywhere.
    Moreover it actually resulted in discarding installations that don't have any
    `/lib` directory.
    
    This is causing troubles for our pipelines downstream, that want to perform
    syntax-only analysis on the sources.
    
    Differential Revision: https://reviews.llvm.org/D141467

--- clang/lib/Driver/ToolChains/Cuda.cpp
+++ clang/lib/Driver/ToolChains/Cuda.cpp
@@ -194,19 +194,6 @@ CudaInstallationDetector::CudaInstallationDetector(
     if (CheckLibDevice && !FS.exists(LibDevicePath))
       continue;
 
-    // On Linux, we have both lib and lib64 directories, and we need to choose
-    // based on our triple.  On MacOS, we have only a lib directory.
-    //
-    // It's sufficient for our purposes to be flexible: If both lib and lib64
-    // exist, we choose whichever one matches our triple.  Otherwise, if only
-    // lib exists, we use it.
-    if (HostTriple.isArch64Bit() && FS.exists(InstallPath + "/lib64"))
-      LibPath = InstallPath + "/lib64";
-    else if (FS.exists(InstallPath + "/lib"))
-      LibPath = InstallPath + "/lib";
-    else
-      continue;
-
     Version = CudaVersion::UNKNOWN;
     if (auto CudaHFile = FS.getBufferForFile(InstallPath + "/include/cuda.h"))
       Version = parseCudaHFile((*CudaHFile)->getBuffer());
