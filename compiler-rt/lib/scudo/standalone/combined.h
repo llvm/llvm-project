eaf57f02d5b3864e0ce27c337508dc35fa842eac
Luke Nicholson <lukenicholson@google.com>
2023-01-12 13:05:30 -0800

    [scudo] Fix RingBuffer initialization.
    Several map implementations require that map calls provide a page
    aligned size.
    
    Reviewed By: fmayer
    
    Differential Revision: https://reviews.llvm.org/D141630

--- compiler-rt/lib/scudo/standalone/combined.h
+++ compiler-rt/lib/scudo/standalone/combined.h
@@ -1500,7 +1500,8 @@ private:
       AllocationRingBufferSize = 1;
     MapPlatformData Data = {};
     RawRingBuffer = static_cast<char *>(
-        map(/*Addr=*/nullptr, ringBufferSizeInBytes(AllocationRingBufferSize),
+        map(/*Addr=*/nullptr,
+            roundUpTo(ringBufferSizeInBytes(AllocationRingBufferSize), getPageSizeCached()),
             "AllocatorRingBuffer", /*Flags=*/0, &Data));
     auto *RingBuffer = reinterpret_cast<AllocationRingBuffer *>(RawRingBuffer);
     RingBuffer->Size = AllocationRingBufferSize;
