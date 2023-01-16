73081c61b64324b5aa2968d04d3bb6f2511a26b8
Luo, Yuanke <yuanke.luo@intel.com>
2023-01-16 22:39:06 +0800

    [X86] Don't fold select for vXi1 on X86 target.
    Since there is no mask instruction for vXi1 with avx512f in X86 target.
    Folding select for vXi1 doesn't help to reduce instructions.
    
    Differential Revision: https://reviews.llvm.org/D141782

--- llvm/lib/Target/X86/X86ISelLowering.cpp
+++ llvm/lib/Target/X86/X86ISelLowering.cpp
@@ -34857,7 +34857,7 @@ bool X86TargetLowering::shouldFoldSelectWithIdentityConstant(unsigned Opcode,
     return false;
   if (!Subtarget.hasVLX() && !VT.is512BitVector())
     return false;
-  if (!VT.isVector())
+  if (!VT.isVector() || VT.getScalarType() == MVT::i1)
     return false;
 
   return true;
