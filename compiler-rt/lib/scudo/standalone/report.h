a1a7b7b511c86c1d470b75aea15c4d00e6c7de1a
Chia-hung Duan <chiahungduan@google.com>
2023-01-17 19:04:52 +0000

    [scudo] Call getStats when the region is exhausted
    Because of lock contention, we temporarily disabled the printing of
    regions' status when it's exhausted. Given that it's useful when the
    Region OOM happens, this CL brings it back without lock contention.
    
    Differential Revision: https://reviews.llvm.org/D141955

--- compiler-rt/lib/scudo/standalone/report.h
+++ compiler-rt/lib/scudo/standalone/report.h
@@ -32,6 +32,7 @@ void NORETURN reportSanityCheckError(const char *Field);
 void NORETURN reportAlignmentTooBig(uptr Alignment, uptr MaxAlignment);
 void NORETURN reportAllocationSizeTooBig(uptr UserSize, uptr TotalSize,
                                          uptr MaxSize);
+void NORETURN reportOutOfBatchClass();
 void NORETURN reportOutOfMemory(uptr RequestedSize);
 void NORETURN reportSoftRSSLimit(uptr RssLimitMb);
 void NORETURN reportHardRSSLimit(uptr RssLimitMb);
