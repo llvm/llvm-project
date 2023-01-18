b9b1750405348cc9b5c514526158beeb538e5287
Jason Molenda <jason@molenda.com>
2023-01-18 12:29:52 -0800

    Do qProcessInfo-hint binary loading later in Process setup
    The remote stub may give lldb hints about binaries to
    be loaded, especially in a firmware type environment, and
    relay those hints in the qProcessInfo response.  The
    binary loading was done very early in Process setup, before
    we had any threads, and this made it complicated for people
    to write dSYM python scripts which need access to a thread.
    Delay the binary loading until a bit later in the Process
    startup.
    
    Differential Revision: https://reviews.llvm.org/D141972
    rdar://104235301

--- lldb/source/Plugins/Process/gdb-remote/ProcessGDBRemote.h
+++ lldb/source/Plugins/Process/gdb-remote/ProcessGDBRemote.h
@@ -374,6 +374,7 @@ protected:
   bool UpdateThreadIDList();
 
   void DidLaunchOrAttach(ArchSpec &process_arch);
+  void LoadStubBinaries();
   void MaybeLoadExecutableModule();
 
   Status ConnectToDebugserver(llvm::StringRef host_port);
