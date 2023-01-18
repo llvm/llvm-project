6f2b9dfc71563d4fb1ee83e051e320b74d43e231
Nikita Popov <npopov@redhat.com>
2023-01-18 09:48:31 +0100

    Revert "[BitcodeReader] Allow reading pointer types from old IR"
    This reverts commit 55240679e60edf606c9eeabd0bd6989c657d9f15.
    
    The unit tests are implemented in a way that requires support for
    writing typed pointer bitcode, which is going away soon. Please
    rewrite it in a way that not have requirement, e.g. by shipping
    pre-compiled bitcode, as we do for integration tests.

--- llvm/tools/llc/llc.cpp
+++ llvm/tools/llc/llc.cpp
@@ -568,8 +568,7 @@ static int compileModule(char **argv, LLVMContext &Context) {
       if (MIR)
         M = MIR->parseIRModule(SetDataLayout);
     } else {
-      M = parseIRFile(InputFilename, Err, Context,
-                      ParserCallbacks(SetDataLayout));
+      M = parseIRFile(InputFilename, Err, Context, SetDataLayout);
     }
     if (!M) {
       Err.print(argv[0], WithColor::error(errs(), argv[0]));
--- llvm/tools/opt/opt.cpp
+++ llvm/tools/opt/opt.cpp
@@ -545,8 +545,7 @@ int main(int argc, char **argv) {
             InputFilename, Err, Context, nullptr, SetDataLayout)
             .Mod;
   else
-    M = parseIRFile(InputFilename, Err, Context,
-                    ParserCallbacks(SetDataLayout));
+    M = parseIRFile(InputFilename, Err, Context, SetDataLayout);
 
   if (!M) {
     Err.print(argv[0], errs());
