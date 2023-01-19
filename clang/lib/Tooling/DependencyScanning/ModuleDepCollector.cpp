efcd8ac7d2e65f7a9339bfaf6a2ba3fa2f8df2cd
Ben Langmuir <blangmuir@apple.com>
2023-01-19 09:31:31 -0800

    [clang][deps] Add module files for input dependencies earlier
    I originally thought we needed to add module file inputs for modular
    deps at the same time as outputs because they depend on the
    lookupModuleOutput callback, but this is not the case: they only depend
    on the callback results for other modules, which have already been
    computed by this point. So move them earlier so that they're set in the
    CompilerInvocation at the same time as other inputs. This makes the
    code easier to understand.
    
    This change is effectively NFC, though it technically changes the module
    exact value of the context hash.
    
    Differential Revision: https://reviews.llvm.org/D142392

--- clang/lib/Tooling/DependencyScanning/ModuleDepCollector.cpp
+++ clang/lib/Tooling/DependencyScanning/ModuleDepCollector.cpp
@@ -55,11 +55,6 @@ static std::vector<std::string> splitString(std::string S, char Separator) {
 
 void ModuleDepCollector::addOutputPaths(CompilerInvocation &CI,
                                         ModuleDeps &Deps) {
-  // These are technically *inputs* to the compilation, but we populate them
-  // here in order to make \c getModuleContextHash() independent of
-  // \c lookupModuleOutput().
-  addModuleFiles(CI, Deps.ClangModuleDeps);
-
   CI.getFrontendOpts().OutputFile =
       Consumer.lookupModuleOutput(Deps.ID, ModuleOutputKind::ModuleFile);
   if (!CI.getDiagnosticOpts().DiagnosticSerializationFile.empty())
@@ -155,6 +150,9 @@ ModuleDepCollector::makeInvocationForModuleBuildWithoutOutputs(
   // Report the prebuilt modules this module uses.
   for (const auto &PrebuiltModule : Deps.PrebuiltModuleDeps)
     CI.getFrontendOpts().ModuleFiles.push_back(PrebuiltModule.PCMFile);
+
+  // Add module file inputs from dependencies.
+  addModuleFiles(CI, Deps.ClangModuleDeps);
 
   // Remove any macro definitions that are explicitly ignored.
   if (!CI.getHeaderSearchOpts().ModulesIgnoreMacros.empty()) {
