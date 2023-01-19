e315b872b291ad268bf0ae6e276a07de6f29992b
Valentin Clement <clementval@gmail.com>
2023-01-19 17:33:08 +0100

    [flang] Support polymorphic source in RESHAPE intrinsic
    Result must carry the polymorphic type information
    from the source.
    
    Reviewed By: jeanPerier, PeteSteinfeld
    
    Differential Revision: https://reviews.llvm.org/D142095

--- flang/lib/Lower/IntrinsicCall.cpp
+++ flang/lib/Lower/IntrinsicCall.cpp
@@ -4222,8 +4222,9 @@ IntrinsicLibrary::genReshape(mlir::Type resultType,
 
   // Create mutable fir.box to be passed to the runtime for the result.
   mlir::Type type = builder.getVarLenSeqTy(resultType, resultRank);
-  fir::MutableBoxValue resultMutableBox =
-      fir::factory::createTempMutableBox(builder, loc, type);
+  fir::MutableBoxValue resultMutableBox = fir::factory::createTempMutableBox(
+      builder, loc, type, {},
+      fir::isPolymorphicType(source.getType()) ? source : mlir::Value{});
 
   mlir::Value resultIrBox =
       fir::factory::getMutableIRBox(builder, loc, resultMutableBox);
