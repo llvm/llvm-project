2e26df8ed64a185a792262d2ad335aea5af90214
Peiming Liu <peiming@google.com>
2023-01-20 22:51:26 +0000

    [mlir][sparse] extend loop emitter and optimize lattices with the awareness of slice based iteration
    Reviewed By: aartbik
    
    Differential Revision: https://reviews.llvm.org/D142929

--- mlir/lib/Dialect/SparseTensor/Transforms/CodegenEnv.h
+++ mlir/lib/Dialect/SparseTensor/Transforms/CodegenEnv.h
@@ -99,7 +99,6 @@ public:
     topSort.reserve(capacity);
   }
 
-  ArrayRef<LoopId> getTopSort() const { return topSort; };
   ArrayRef<LoopId> getTopSortSlice(LoopOrd n, LoopOrd m) const;
   ArrayRef<LoopId> getLoopStackUpTo(LoopOrd n) const;
   ArrayRef<LoopId> getCurrentLoopStack() const;
