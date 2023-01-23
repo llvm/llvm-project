ddbe14084da7f31d4b4b53e13d9f868d759f3673
Luca Di Sera <disera.luca@gmail.com>
2023-01-23 10:05:51 +0100

    Add clang_CXXMethod_isExplicit to libclang
    The new method is a wrapper of `CXXConstructorDecl::isExplicit` and
    `CXXConversionDecl::isExplicit`, allowing the user to recognize whether
    the declaration pointed to by a cursor was marked with the explicit
    specifier.
    
    An export for the function, together with its documentation, was added
    to "clang/include/clang-c/Index.h" with an implementation provided in
    "clang/tools/libclang/CIndex.cpp".
    
    The implementation is based on similar `clang_CXXMethod`
    implementations, returning a falsy unsigned value when the cursor is not
    a declaration, is not a declaration for a constructor or conversion
    function or is not a relevant declaration that was marked with the
    `explicit` specifier.
    
    The new symbol was added to "clang/tools/libclang/libclang.map" to be
    exported, under the LLVM16 tag.
    
    "clang/tools/c-index-test/c-index-test.c" was modified to print a
    specific tag, "(explicit)", for cursors that are recognized by
    `clang_CXXMethod_isExplicit`.
    
    Two new regression files, "explicit-constructor.cpp" and
    "explicit-conversion-function.cpp", were added to "clang/test/Index", to
    ensure that the behavior of the new function is correct for constructors
    and conversion functions, respectively.
    
    The "get-cursor.cpp", "index-file.cpp" and
    "recursive-cxx-member-calls.cpp" regression files in "clang/test/Index"
    were updated as they were affected by the new "(explicit)" tag.
    
    A binding for the new function was added to libclang's python's
    bindings, in "clang/bindings/python/clang/cindex.py", as the
    "is_explicit_method" method under `Cursor`.
    
    An accompanying test was added to
    "clang/bindings/python/tests/cindex/test_cursor.py", mimicking the
    regression tests for the C side.
    
    The current release note for Clang, "clang/docs/ReleaseNotes.rst" was
    modified to report the new addition under the "libclang" section.
    
    Reviewed By: aaron.ballman
    
    Differential Revision: https://reviews.llvm.org/D140756

--- clang/bindings/python/clang/cindex.py
+++ clang/bindings/python/clang/cindex.py
@@ -1529,6 +1529,51 @@ class Cursor(Structure):
         """
         return conf.lib.clang_CXXMethod_isMoveAssignmentOperator(self)
 
+    def is_explicit_method(self):
+        """Determines if a C++ constructor or conversion function is
+        explicit, returning 1 if such is the case and 0 otherwise.
+
+        Constructors or conversion functions are declared explicit through
+        the use of the explicit specifier.
+
+        For example, the following constructor and conversion function are
+        not explicit as they lack the explicit specifier:
+
+            class Foo {
+                Foo();
+                operator int();
+            };
+
+        While the following constructor and conversion function are
+        explicit as they are declared with the explicit specifier.
+
+            class Foo {
+                explicit Foo();
+                explicit operator int();
+            };
+
+        This method will return 0 when given a cursor pointing to one of
+        the former declarations and it will return 1 for a cursor pointing
+        to the latter declarations.
+
+        The explicit specifier allows the user to specify a
+        conditional compile-time expression whose value decides
+        whether the marked element is explicit or not.
+
+        For example:
+
+            constexpr bool foo(int i) { return 1 % 2 == 0; }
+
+            class Foo {
+                 explicit(foo(1)) Foo();
+                 explicit(foo(2)) operator int();
+            }
+
+        This method will return 0 for the constructor and 1 for
+        the conversion function.
+        """
+        return conf.lib.clang_CXXMethod_isExplicit(self)
+
     def is_mutable_field(self):
         """Returns True if the cursor refers to a C++ field that is declared
         'mutable'.
@@ -3491,6 +3536,10 @@ functionList = [
    bool),
 
   ("clang_CXXMethod_isMoveAssignmentOperator",
+   [Cursor],
+   bool),
+
+  ("clang_CXXMethod_isExplicit",
    [Cursor],
    bool),
 
