951d0652de1152f2c2344348bef15dce8dc6d093
Sanjay Patel <spatel@rotateright.com>
2023-01-23 16:55:25 -0500

    [InstCombine] remove dead pattern matching code; NFC
    Complexity canonicalization guarantees that a binop and cast
    are op0/op1 respectively. Adjusted generic test names to
    show that this pattern is still useful.

--- llvm/lib/Transforms/InstCombine/InstCombineCompares.cpp
+++ llvm/lib/Transforms/InstCombine/InstCombineCompares.cpp
@@ -4653,13 +4653,13 @@ Instruction *InstCombinerImpl::foldICmpEquality(ICmpInst &I) {
     }
   }
 
-  // Transform (zext A) == (B & (1<<X)-1) --> A == (trunc B)
-  // and       (B & (1<<X)-1) == (zext A) --> A == (trunc B)
+  // (B & (Pow2C-1)) == zext A --> A == trunc B
+  // (B & (Pow2C-1)) != zext A --> A != trunc B
+  // TODO: The one use check could be relaxed.
+  // TODO: This can be generalized for vector types.
   ConstantInt *Cst1;
-  if ((Op0->hasOneUse() && match(Op0, m_ZExt(m_Value(A))) &&
-       match(Op1, m_And(m_Value(B), m_ConstantInt(Cst1)))) ||
-      (Op1->hasOneUse() && match(Op0, m_And(m_Value(B), m_ConstantInt(Cst1))) &&
-       match(Op1, m_ZExt(m_Value(A))))) {
+  if (match(Op0, m_And(m_Value(B), m_ConstantInt(Cst1))) &&
+      match(Op1, m_OneUse(m_ZExt(m_Value(A))))) {
     APInt Pow2 = Cst1->getValue() + 1;
     if (Pow2.isPowerOf2() && isa<IntegerType>(A->getType()) &&
         Pow2.logBase2() == cast<IntegerType>(A->getType())->getBitWidth())
