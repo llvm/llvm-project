0245dcc000faf4f6dea9552a44106c8921df841e
Ben Langmuir <blangmuir@apple.com>
2023-01-24 15:34:37 -0800

    [clang][test] Remove check that fails if SOURCE_DATE_EPOCH is set globally
    The check for "no SOURCE_DATE_EPOCH" wasn't especially interesting, and
    I am not aware of a _portable_ way to unset and environment variable in
    a lit test. So remove it since it can fail if the build environment has
    SOURCE_DATE_EPOCH set globally.
    
    Differential Revision: https://reviews.llvm.org/D142511

--- clang/test/Driver/SOURCE_DATE_EPOCH.c
+++ clang/test/Driver/SOURCE_DATE_EPOCH.c
@@ -1,5 +1,2 @@
-// RUN: %clang -E %s -### 2>&1 | FileCheck %s -check-prefix=NO_EPOCH
-// NO_EPOCH-NOT: "-source-date-epoch"
-
 // RUN: env SOURCE_DATE_EPOCH=123 %clang -E %s -### 2>&1 | FileCheck %s
 // CHECK: "-source-date-epoch" "123"
