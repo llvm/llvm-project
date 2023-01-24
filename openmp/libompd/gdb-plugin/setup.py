b0cd4fab2237fb2a862f943b3940fc21422b7ddd
Ethan Stewart <ethan.stewart@amd.com>
2023-01-24 15:31:29 -0600

    [openmp][libompd] - Ensure the python module links in libclang-cpp.so
    After switching to dlsym for the LLVM_DYLIB change,
    the libompd python module needs to link in libclang-cpp.so
    to resolve missing symbols.
    
    Change-Id: I48ca1844fe4c67d1e39ed6950b8e95f4ac934486

--- openmp/libompd/gdb-plugin/setup.py
+++ openmp/libompd/gdb-plugin/setup.py
@@ -7,6 +7,11 @@ omp_include_dir = os.environ.get('LIBOMP_INCLUDE_DIR', dir_path)
 llvm_include_dir = os.environ.get('LLVM_MAIN_INCLUDE_DIR', dir_path)
 python_include_dir = os.environ.get('PYTHON_HEADERS', dir_path)
 
+# Needed for dlsym
+clang_cpp = os.environ.get('CLANG_CPP', dir_path)
+clang_cpp_dir = clang_cpp.split('libclang-cpp.so')
+install_lib_loc = os.environ.get('INSTALL_LIB_LOC', dir_path)
+
 print("find_packages : ", find_packages())
 setup(
     name='ompd',
@@ -14,5 +19,5 @@ setup(
     py_modules=['loadompd'],
     setup_requires=['wheel'],
     packages=find_packages(),
-	ext_modules=[Extension('ompd.ompdModule', [dir_path+'/ompdModule.c', dir_path+'/ompdAPITests.c', dir_path+'/DLSymService.cpp'], include_dirs=[omp_include_dir, llvm_include_dir])]
+	ext_modules=[Extension('ompd.ompdModule', [dir_path+'/ompdModule.c', dir_path+'/ompdAPITests.c', dir_path+'/DLSymService.cpp'], include_dirs=[omp_include_dir, llvm_include_dir], runtime_library_dirs=[install_lib_loc], libraries=['clang-cpp'], library_dirs=[clang_cpp_dir[0]])]
 )
