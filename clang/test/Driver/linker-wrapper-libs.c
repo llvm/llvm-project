7532e88f38a417d3713cb179dc974948c30f0336
Joseph Huber <jhuber6@vols.utk.edu>
2023-01-24 17:09:18 -0600

    [Clang] Add missing requires directives for new test
    Summary:
    Forgot to add this.

--- clang/test/Driver/linker-wrapper-libs.c
+++ clang/test/Driver/linker-wrapper-libs.c
@@ -1,3 +1,7 @@
+// REQUIRES: x86-registered-target
+// REQUIRES: nvptx-registered-target
+// REQUIRES: amdgpu-registered-target
+
 // RUN: %clang -cc1 %s -triple x86_64-unknown-linux-gnu -emit-obj -o %t.elf.o
 
 #if defined(RESOLVES)
