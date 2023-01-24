76f9a6a4e7f32b38458ed9c5c267c4f45b1d69db
Guillaume Chatelet <gchatelet@google.com>
2023-01-24 16:22:26 +0000

    Revert D142108 "[reland][libc][NFC] Detect host CPU features using try_compile instead of try_run."
    The build bots are failing.
    This reverts commit c84d74f5bfe810744de1268eb0516a6622e4aa73.

--- libc/cmake/modules/LLVMLibCCheckCpuFeatures.cmake
+++ libc/cmake/modules/LLVMLibCCheckCpuFeatures.cmake
@@ -44,32 +44,44 @@ function(_intersection output_var list1 list2)
   set(${output_var} ${tmp} PARENT_SCOPE)
 endfunction()
 
-set(AVAILABLE_CPU_FEATURES "")
-if(LIBC_CROSSBUILD)
-  # If we are doing a cross build, we will just assume that all CPU features
-  # are available.
-  set(AVAILABLE_CPU_FEATURES ${ALL_CPU_FEATURES})
-else()
-  # Try compile a C file to check if flag is supported.
-  set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+# Generates a cpp file to introspect the compiler defined flags.
+function(_generate_check_code)
   foreach(feature IN LISTS ALL_CPU_FEATURES)
-    try_compile(
-      has_feature
-      ${CMAKE_CURRENT_BINARY_DIR}/cpu_features
-      SOURCES ${LIBC_SOURCE_DIR}/cmake/modules/cpu_features/check_${feature}.cpp
-      COMPILE_DEFINITIONS -I${LIBC_SOURCE_DIR} ${LIBC_COMPILE_OPTIONS_NATIVE}
-    )
-    if(has_feature)
-      list(APPEND AVAILABLE_CPU_FEATURES ${feature})
-    endif()
+    set(DEFINITIONS
+        "${DEFINITIONS}
+#ifdef __${feature}__
+    \"${feature}\",
+#endif")
   endforeach()
-endif()
+  configure_file(
+    "${LIBC_SOURCE_DIR}/cmake/modules/cpu_features/check_cpu_features.cpp.in"
+    "cpu_features/check_cpu_features.cpp" @ONLY)
+endfunction()
+_generate_check_code()
 
-set(LIBC_CPU_FEATURES ${AVAILABLE_CPU_FEATURES} CACHE STRING "Host supported CPU features")
+set(LIBC_CPU_FEATURES "" CACHE PATH "Host supported CPU features")
 
-_intersection(cpu_features "${AVAILABLE_CPU_FEATURES}" "${LIBC_CPU_FEATURES}")
-if(NOT "${cpu_features}" STREQUAL "${LIBC_CPU_FEATURES}")
-  message(FATAL_ERROR "Unsupported CPU features: ${cpu_features}")
-else()
+if(LIBC_CROSSBUILD)
+  _intersection(cpu_features "${ALL_CPU_FEATURES}" "${LIBC_CPU_FEATURES}")
+  if(NOT "${cpu_features}" STREQUAL "${LIBC_CPU_FEATURES}")
+    message(FATAL_ERROR "Unsupported CPU features: ${cpu_features}")
+  endif()
   message(STATUS "Set CPU features: ${cpu_features}")
+  set(LIBC_CPU_FEATURES "${cpu_features}")
+else()
+  # Populates the LIBC_CPU_FEATURES list from host.
+  try_run(
+    run_result compile_result "${CMAKE_CURRENT_BINARY_DIR}/check_${feature}"
+    "${CMAKE_CURRENT_BINARY_DIR}/cpu_features/check_cpu_features.cpp"
+    COMPILE_DEFINITIONS ${LIBC_COMPILE_OPTIONS_NATIVE}
+    COMPILE_OUTPUT_VARIABLE compile_output
+    RUN_OUTPUT_VARIABLE run_output)
+  if("${run_result}" EQUAL 0)
+    message(STATUS "Set CPU features: ${run_output}")
+    set(LIBC_CPU_FEATURES "${run_output}")
+  elseif(NOT ${compile_result})
+    message(FATAL_ERROR "Failed to compile: ${compile_output}")
+  else()
+    message(FATAL_ERROR "Failed to run: ${run_output}")
+  endif()
 endif()
--- libc/src/math/generic/asinf.cpp
+++ libc/src/math/generic/asinf.cpp
@@ -13,7 +13,6 @@
 #include "src/__support/FPUtil/except_value_utils.h"
 #include "src/__support/FPUtil/multiply_add.h"
 #include "src/__support/FPUtil/sqrt.h"
-#include "src/__support/cpu_features.h"
 
 #include <errno.h>
 
