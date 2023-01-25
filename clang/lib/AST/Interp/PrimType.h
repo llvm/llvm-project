cb7f58221101cfad0ddec987465de57a20b0d11c
Timm Bäder <tbaeder@redhat.com>
2023-01-25 14:51:16 +0100

    Re-apply "[clang][Interp] Support floating-point values"
    Don't move the Repr struct into Integral this time.
    
    Differential Revision: https://reviews.llvm.org/D134859

--- clang/lib/AST/Interp/PrimType.h
+++ clang/lib/AST/Interp/PrimType.h
@@ -23,6 +23,7 @@ namespace interp {
 
 class Pointer;
 class Boolean;
+class Floating;
 
 /// Enumeration of the primitive types of the VM.
 enum PrimType : unsigned {
@@ -35,6 +36,7 @@ enum PrimType : unsigned {
   PT_Sint64,
   PT_Uint64,
   PT_Bool,
+  PT_Float,
   PT_Ptr,
 };
 
@@ -48,6 +50,7 @@ template <> struct PrimConv<PT_Sint32> { using T = Integral<32, true>; };
 template <> struct PrimConv<PT_Uint32> { using T = Integral<32, false>; };
 template <> struct PrimConv<PT_Sint64> { using T = Integral<64, true>; };
 template <> struct PrimConv<PT_Uint64> { using T = Integral<64, false>; };
+template <> struct PrimConv<PT_Float> { using T = Floating; };
 template <> struct PrimConv<PT_Bool> { using T = Boolean; };
 template <> struct PrimConv<PT_Ptr> { using T = Pointer; };
 
@@ -77,6 +80,7 @@ inline bool isPrimitiveIntegral(PrimType Type) {
   case PT_Uint32:
   case PT_Sint64:
   case PT_Uint64:
+  case PT_Float:
     return true;
   default:
     return false;
@@ -101,6 +105,7 @@ inline bool isPrimitiveIntegral(PrimType Type) {
       TYPE_SWITCH_CASE(PT_Uint32, B)                                           \
       TYPE_SWITCH_CASE(PT_Sint64, B)                                           \
       TYPE_SWITCH_CASE(PT_Uint64, B)                                           \
+      TYPE_SWITCH_CASE(PT_Float, B)                                            \
       TYPE_SWITCH_CASE(PT_Bool, B)                                             \
       TYPE_SWITCH_CASE(PT_Ptr, B)                                              \
     }                                                                          \
