bc4c0158d114721e1e1f005d9d05a4e0b790b9cf
Elizabeth Andrews <elizabeth.andrews@intel.com>
2023-01-26 13:51:56 -0800

    [SYCL] Fix test to conform to SYCL2020

--- clang/test/SemaSYCL/bf16.cpp
+++ clang/test/SemaSYCL/bf16.cpp
@@ -1,7 +1,7 @@
 // RUN: %clang_cc1 -triple spir64 -aux-triple x86_64-unknown-linux-gnu -fsycl-is-device -verify -fsyntax-only %s
 
 template <typename Name, typename Func>
-__attribute__((sycl_kernel)) void kernel(Func kernelFunc) {
+__attribute__((sycl_kernel)) void kernel(const Func &kernelFunc) {
   kernelFunc(); // expected-note {{called by 'kernel}}
 }
 
