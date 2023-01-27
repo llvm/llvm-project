7ac41e0c852a753da59f323c27f0908b849d86bf
Aaron Ballman <aaron@aaronballman.com>
2023-01-27 11:01:12 -0500

    Stop diagnosing member and array access in offsetof as an extension
    This was a mistake from f8a4c9c766cc1a34f56b8e2c7d41ae5b95c8f65f
    (https://reviews.llvm.org/D133574) caused by us accidentally tracking
    an older copy of the C DR list for DR496. The text in
    https://www.open-std.org/jtc1/sc22/wg14/www/docs/n2396.htm#dr_496 makes
    it clear that subobjects are allowed, which means member and array
    access expressions are allowed.
    
    This backs out the changes from the previous commit that relate to this
    diagnostic.

--- clang/lib/Parse/ParseExpr.cpp
+++ clang/lib/Parse/ParseExpr.cpp
@@ -2629,12 +2629,6 @@ ExprResult Parser::ParseBuiltinPrimaryExpression() {
     Comps.back().U.IdentInfo = Tok.getIdentifierInfo();
     Comps.back().LocStart = Comps.back().LocEnd = ConsumeToken();
 
-    enum class Kind { MemberAccess, ArraySubscript };
-    auto DiagExt = [&](SourceLocation Loc, Kind K) {
-      Diag(Loc, diag::ext_offsetof_member_designator)
-          << (K == Kind::ArraySubscript) << (OOK == Sema::OOK_Macro);
-    };
-
     // FIXME: This loop leaks the index expressions on error.
     while (true) {
       if (Tok.is(tok::period)) {
@@ -2648,7 +2642,6 @@ ExprResult Parser::ParseBuiltinPrimaryExpression() {
           SkipUntil(tok::r_paren, StopAtSemi);
           return ExprError();
         }
-        DiagExt(Comps.back().LocStart, Kind::MemberAccess);
         Comps.back().U.IdentInfo = Tok.getIdentifierInfo();
         Comps.back().LocEnd = ConsumeToken();
       } else if (Tok.is(tok::l_square)) {
@@ -2666,7 +2659,6 @@ ExprResult Parser::ParseBuiltinPrimaryExpression() {
           SkipUntil(tok::r_paren, StopAtSemi);
           return Res;
         }
-        DiagExt(Comps.back().LocStart, Kind::ArraySubscript);
         Comps.back().U.E = Res.get();
 
         ST.consumeClose();
