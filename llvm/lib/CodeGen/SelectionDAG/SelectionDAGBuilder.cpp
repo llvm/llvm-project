8bdb149c0a544611c06cbdfd6cd5151ca1589dcb
Matt Arsenault <Matthew.Arsenault@amd.com>
2023-01-30 18:51:01 -0400

    DAG: Remove redundant check for return alignment
    This is already what the CallBase getRetAlign does

--- llvm/lib/CodeGen/SelectionDAG/SelectionDAGBuilder.cpp
+++ llvm/lib/CodeGen/SelectionDAG/SelectionDAGBuilder.cpp
@@ -4918,8 +4918,7 @@ void SelectionDAGBuilder::visitTargetIntrinsic(const CallInst &I,
       Result = lowerRangeToAssertZExt(DAG, I, Result);
 
     MaybeAlign Alignment = I.getRetAlign();
-    if (!Alignment)
-      Alignment = F->getAttributes().getRetAlignment();
+
     // Insert `assertalign` node if there's an alignment.
     if (InsertAssertAlign && Alignment) {
       Result =
