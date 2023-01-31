2b215a2f4537641808c2d40051b211a1ecba6dc7
Guillaume Chatelet <gchatelet@google.com>
2023-01-31 14:10:25 +0000

    Revert D142708 "[NFC] Transition GlobalObject alignment from MaybeAlign to Align"
    This is breaking the build bots. e.g.,
    https://lab.llvm.org/buildbot/#/builders/121/builds/27549
    
    This reverts commit f69c1eede313433e88884d7c2e1a9fe7e21bc5ac.

--- llvm/include/llvm/IR/GlobalObject.h
+++ llvm/include/llvm/IR/GlobalObject.h
@@ -82,12 +82,6 @@ public:
     return decodeMaybeAlign(AlignmentData);
   }
 
-  /// Sets the alignment attribute of the GlobalObject.
-  void setAlignment(Align Align);
-
-  /// Sets the alignment attribute of the GlobalObject.
-  /// This method will be deprecated as the alignment property should always be
-  /// defined.
   void setAlignment(MaybeAlign Align);
 
   unsigned getGlobalObjectSubClassData() const {
--- llvm/include/llvm/LTO/LTO.h
+++ llvm/include/llvm/LTO/LTO.h
@@ -289,7 +289,7 @@ private:
                     const Config &Conf);
     struct CommonResolution {
       uint64_t Size = 0;
-      Align Align;
+      MaybeAlign Align;
       /// Record if at least one instance of the common was marked as prevailing
       bool Prevailing = false;
     };
