99f7b590b4fe71da86b1c0dcb4e5c9c8ad457997
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2023-02-01 12:18:53 -0800

    [CIR][CIRTidy] Translate between MLIR diagnostics and ClangTidyError
    - Create a new diagnostic handler that at the same time allows printing
      diagnostics but also forwarding to ClangTidyErrors related machinery.
    - Unique references to lifetime check name.
    - Add support for warnings, notes and remarks.
    - Add testing for the YAML outputs, which are automatically derived from
      ClangTidyErrors.
    - Use `ClangTidyCheck::OptionsView` to handle pass options.

--- clang-tools-extra/clang-tidy/cir-tidy/CIRASTConsumer.cpp
+++ clang-tools-extra/clang-tidy/cir-tidy/CIRASTConsumer.cpp
@@ -1,20 +1,25 @@
+//===--- clang-tidy/cir-tidy/CIRASTConsumer.cpp ---------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
 #include "CIRASTConsumer.h"
+#include "CIRChecks.h"
 
-#include "clang/CIR/Dialect/Passes.h"
-#include "mlir/IR/BuiltinOps.h"
 #include "../utils/OptionsUtils.h"
-
+#include "ClangTidyCheck.h"
+#include "mlir/IR/BuiltinOps.h"
 #include "mlir/IR/MLIRContext.h"
 #include "mlir/Pass/Pass.h"
 #include "mlir/Pass/PassManager.h"
+#include "clang/CIR/Dialect/Passes.h"
 #include <algorithm>
 
 using namespace clang;
 using namespace clang::tidy;
-
-namespace {
-const std::string lifeTimeCheck = "cir-lifetime-check";
-} // namespace
 
 namespace cir {
 namespace tidy {
@@ -49,30 +54,102 @@ void CIRASTConsumer::HandleTranslationUnit(ASTContext &C) {
   mlir::OpPrintingFlags flags;
   flags.enableDebugInfo(/*prettyForm=*/false);
 
-  SourceManager &SourceMgr = C.getSourceManager();
-  FileID MainFileID = SourceMgr.getMainFileID();
+  clang::SourceManager &clangSrcMgr = C.getSourceManager();
+  FileID MainFileID = clangSrcMgr.getMainFileID();
 
-  llvm::MemoryBufferRef MainFileBuf = SourceMgr.getBufferOrFake(MainFileID);
+  llvm::MemoryBufferRef MainFileBuf = clangSrcMgr.getBufferOrFake(MainFileID);
   std::unique_ptr<llvm::MemoryBuffer> FileBuf =
       llvm::MemoryBuffer::getMemBuffer(MainFileBuf);
 
-  llvm::SourceMgr sourceMgr;
-  sourceMgr.AddNewSourceBuffer(std::move(FileBuf), llvm::SMLoc());
+  llvm::SourceMgr llvmSrcMgr;
+  llvmSrcMgr.AddNewSourceBuffer(std::move(FileBuf), llvm::SMLoc());
 
-  mlir::SourceMgrDiagnosticHandler sourceMgrHandler(sourceMgr, mlirCtx.get());
+  class CIRTidyDiagnosticHandler : public mlir::SourceMgrDiagnosticHandler {
+    clang::tidy::ClangTidyContext &tidyCtx;
+    clang::SourceManager &clangSrcMgr;
+
+    clang::SourceLocation getClangSrcLoc(mlir::Location loc) {
+      clang::SourceLocation clangLoc;
+      FileManager &fileMgr = clangSrcMgr.getFileManager();
+
+      auto fileLoc = loc.dyn_cast<mlir::FileLineColLoc>();
+      if (!fileLoc)
+        return clangLoc;
+      // The column and line may be zero to represent unknown column and/or
+      // unknown line/column information.
+      if (fileLoc.getLine() == 0 || fileLoc.getColumn() == 0)
+        return clangLoc;
+      if (auto FE = fileMgr.getFile(fileLoc.getFilename())) {
+        return clangSrcMgr.translateFileLineCol(*FE, fileLoc.getLine(),
+                                                fileLoc.getColumn());
+      }
+      return clangLoc;
+    }
+
+    clang::DiagnosticIDs::Level
+    translateToClangDiagLevel(const mlir::DiagnosticSeverity &sev) {
+      switch (sev) {
+      case mlir::DiagnosticSeverity::Note:
+        return clang::DiagnosticIDs::Level::Note;
+      case mlir::DiagnosticSeverity::Warning:
+        return clang::DiagnosticIDs::Level::Warning;
+      case mlir::DiagnosticSeverity::Error:
+        return clang::DiagnosticIDs::Level::Error;
+      case mlir::DiagnosticSeverity::Remark:
+        return clang::DiagnosticIDs::Level::Remark;
+      }
+      llvm_unreachable("should not get here!");
+    }
+
+  public:
+    void emitClangTidyDiagnostic(mlir::Diagnostic &diag) {
+      tidyCtx.diag(cir::checks::LifetimeCheckName,
+                   getClangSrcLoc(diag.getLocation()), diag.str(),
+                   translateToClangDiagLevel(diag.getSeverity()));
+      for (const auto &note : diag.getNotes()) {
+        tidyCtx.diag(cir::checks::LifetimeCheckName,
+                     getClangSrcLoc(note.getLocation()), note.str(),
+                     translateToClangDiagLevel(note.getSeverity()));
+      }
+    }
+
+    CIRTidyDiagnosticHandler(llvm::SourceMgr &mgr, mlir::MLIRContext *ctx,
+                             clang::tidy::ClangTidyContext &tidyContext,
+                             clang::SourceManager &clangMgr,
+                             ShouldShowLocFn &&shouldShowLocFn = {})
+        : SourceMgrDiagnosticHandler(mgr, ctx, llvm::errs(),
+                                     std::move(shouldShowLocFn)),
+          tidyCtx(tidyContext), clangSrcMgr(clangMgr) {
+      setHandler([this](mlir::Diagnostic &diag) {
+        // Emit diagnostic to llvm::errs() but also populate Clang
+        emitClangTidyDiagnostic(diag);
+        emitDiagnostic(diag);
+      });
+    }
+    ~CIRTidyDiagnosticHandler() = default;
+  };
+
+  // Use a custom diagnostic handler that can allow both regular printing to
+  // stderr but also populates clang-tidy context with diagnostics (and allow
+  // for instance, diagnostics to be later converted to YAML).
+  CIRTidyDiagnosticHandler sourceMgrHandler(llvmSrcMgr, mlirCtx.get(), Context,
+                                            clangSrcMgr);
+
   mlir::PassManager pm(mlirCtx.get());
   pm.addPass(mlir::createMergeCleanupsPass());
 
   clang::tidy::ClangTidyOptions Opts = Context.getOptions();
-  static constexpr const char *remarkOptName = "cir-lifetime-check.RemarksList";
-  static constexpr const char *histOptName = "cir-lifetime-check.HistoryList";
-  auto remarks =
-      utils::options::parseStringList(Opts.CheckOptions[remarkOptName].Value);
-  auto hist =
-      utils::options::parseStringList(Opts.CheckOptions[histOptName].Value);
+  ClangTidyCheck::OptionsView OptsView(cir::checks::LifetimeCheckName,
+                                       Opts.CheckOptions, &Context);
 
-  if (Context.isCheckEnabled(lifeTimeCheck))
-    pm.addPass(mlir::createLifetimeCheckPass(remarks, hist, 1, &C));
+  auto remarks =
+      utils::options::parseStringList(OptsView.get("RemarksList", ""));
+  auto hist =
+      utils::options::parseStringList(OptsView.get("HistoryList", "all"));
+  auto hLimit = OptsView.get("HistLimit", 1U);
+
+  if (Context.isCheckEnabled(cir::checks::LifetimeCheckName))
+    pm.addPass(mlir::createLifetimeCheckPass(remarks, hist, hLimit, &C));
 
   bool Result = !mlir::failed(pm.run(mlirMod));
   if (!Result)
