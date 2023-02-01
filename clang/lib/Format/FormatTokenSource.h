e9989e1975ac3d7f0d733b243bbcbfb1d50d8b44
Manuel Klimek <klimek@google.com>
2023-02-01 13:18:44 +0000

    [clang-format] Enable FormatTokenSource to insert tokens.
    In preparation for configured macro replacements in formatting,
    add the ability to insert tokens to FormatTokenSource, and implement
    token insertion in IndexedTokenSource.
    
    Differential Revision: https://reviews.llvm.org/D143070

--- clang/lib/Format/FormatTokenSource.h
+++ clang/lib/Format/FormatTokenSource.h
@@ -1,4 +1,3 @@
-
 //===--- FormatTokenSource.h - Format C++ code ------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
@@ -8,7 +7,7 @@
 //===----------------------------------------------------------------------===//
 ///
 /// \file
-/// This file defines the \c TokenSource interface, which provides a token
+/// This file defines the \c FormatTokenSource interface, which provides a token
 /// stream as well as the ability to manipulate the token stream.
 ///
 //===----------------------------------------------------------------------===//
@@ -18,12 +17,17 @@
 
 #include "FormatToken.h"
 #include "UnwrappedLineParser.h"
+#include "llvm/ADT/DenseMap.h"
 
 #define DEBUG_TYPE "format-token-source"
 
 namespace clang {
 namespace format {
 
+// Navigate a token stream.
+//
+// Enables traversal of a token stream, resetting the position in a token
+// stream, as well as inserting new tokens.
 class FormatTokenSource {
 public:
   virtual ~FormatTokenSource() {}
@@ -33,6 +37,9 @@ public:
 
   // Returns the token preceding the token returned by the last call to
   // getNextToken() in the token stream, or nullptr if no such token exists.
+  //
+  // Must not be called directly at the position directly after insertTokens()
+  // is called.
   virtual FormatToken *getPreviousToken() = 0;
 
   // Returns the token that would be returned by the next call to
@@ -45,14 +52,31 @@ public:
   virtual bool isEOF() = 0;
 
   // Gets the current position in the token stream, to be used by setPosition().
+  //
+  // Note that the value of the position is not meaningful, and specifically
+  // should not be used to get relative token positions.
   virtual unsigned getPosition() = 0;
 
   // Resets the token stream to the state it was in when getPosition() returned
   // Position, and return the token at that position in the stream.
   virtual FormatToken *setPosition(unsigned Position) = 0;
+
+  // Insert the given tokens before the current position.
+  // Returns the first token in \c Tokens.
+  // The next returned token will be the second token in \c Tokens.
+  // Requires the last token in Tokens to be EOF; once the EOF token is reached,
+  // the next token will be the last token returned by getNextToken();
+  //
+  // For example, given the token sequence 'a1 a2':
+  // getNextToken() -> a1
+  // insertTokens('b1 b2') -> b1
+  // getNextToken() -> b2
+  // getNextToken() -> a1
+  // getNextToken() -> a2
+  virtual FormatToken *insertTokens(ArrayRef<FormatToken *> Tokens) = 0;
 };
 
-class LLVM_GSL_POINTER IndexedTokenSource : public FormatTokenSource {
+class IndexedTokenSource : public FormatTokenSource {
 public:
   IndexedTokenSource(ArrayRef<FormatToken *> Tokens)
       : Tokens(Tokens), Position(-1) {}
@@ -65,7 +89,7 @@ public:
       });
       return Tokens[Position];
     }
-    ++Position;
+    Position = successor(Position);
     LLVM_DEBUG({
       llvm::dbgs() << "Next ";
       dbgToken(Position);
@@ -74,16 +98,17 @@ public:
   }
 
   FormatToken *getPreviousToken() override {
+    assert(Position <= 0 || !Tokens[Position - 1]->is(tok::eof));
     return Position > 0 ? Tokens[Position - 1] : nullptr;
   }
 
   FormatToken *peekNextToken(bool SkipComment = false) override {
     if (isEOF())
       return Tokens[Position];
-    int Next = Position + 1;
+    int Next = successor(Position);
     if (SkipComment)
       while (Tokens[Next]->is(tok::comment))
-        ++Next;
+        Next = successor(Next);
     LLVM_DEBUG({
       llvm::dbgs() << "Peeking ";
       dbgToken(Next);
@@ -107,9 +132,40 @@ public:
     return Tokens[Position];
   }
 
+  FormatToken *insertTokens(ArrayRef<FormatToken *> New) override {
+    assert(Position != -1);
+    assert((*New.rbegin())->Tok.is(tok::eof));
+    int Next = Tokens.size();
+    Tokens.append(New.begin(), New.end());
+    LLVM_DEBUG({
+      llvm::dbgs() << "Inserting:\n";
+      for (int I = Next, E = Tokens.size(); I != E; ++I)
+        dbgToken(I, "  ");
+      llvm::dbgs() << "  Jump from: " << (Tokens.size() - 1) << " -> "
+                   << Position << "\n";
+    });
+    Jumps[Tokens.size() - 1] = Position;
+    Position = Next;
+    LLVM_DEBUG({
+      llvm::dbgs() << "At inserted token ";
+      dbgToken(Position);
+    });
+    return Tokens[Position];
+  }
+
   void reset() { Position = -1; }
 
 private:
+  int successor(int Current) const {
+    int Next = Current + 1;
+    auto it = Jumps.find(Next);
+    if (it != Jumps.end()) {
+      Next = it->second;
+      assert(Jumps.find(Next) == Jumps.end());
+    }
+    return Next;
+  }
+
   void dbgToken(int Position, llvm::StringRef Indent = "") {
     FormatToken *Tok = Tokens[Position];
     llvm::dbgs() << Indent << "[" << Position
@@ -117,8 +173,12 @@ private:
                  << ", Macro: " << !!Tok->MacroCtx << "\n";
   }
 
-  ArrayRef<FormatToken *> Tokens;
+  SmallVector<FormatToken *> Tokens;
   int Position;
+
+  // Maps from position a to position b, so that when we reach a, the token
+  // stream continues at position b instead.
+  llvm::DenseMap<int, int> Jumps;
 };
 
 class ScopedMacroState : public FormatTokenSource {
@@ -173,6 +233,10 @@ public:
     PreviousToken = nullptr;
     Token = PreviousTokenSource->setPosition(Position);
     return Token;
+  }
+
+  FormatToken *insertTokens(ArrayRef<FormatToken *> Tokens) override {
+    assert(false && "Cannot insert tokens while parsing a macro.");
   }
 
 private:
