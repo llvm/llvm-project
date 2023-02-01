a5f717f9d014ea3996dc5ced6c0cc9367b3c4262
Tom Eccles <tom.eccles@arm.com>
2023-02-01 15:14:11 +0000

    [flang][NFC] Move runtime helpers used by intrinsics to lib/Optimizer 3/6
    This will allow IntrinsicCall to be moved into lib/Optimizer later.
    
    Differential Revision: https://reviews.llvm.org/D143081

--- flang/include/flang/Lower/Runtime.h
+++ flang/include/flang/Lower/Runtime.h
@@ -63,42 +63,11 @@ void genSyncTeamStatement(AbstractConverter &, const parser::SyncTeamStmt &);
 void genUnlockStatement(AbstractConverter &, const parser::UnlockStmt &);
 void genPauseStatement(AbstractConverter &, const parser::PauseStmt &);
 
-mlir::Value genAssociated(fir::FirOpBuilder &, mlir::Location,
-                          mlir::Value pointer, mlir::Value target);
-
 void genPointerAssociate(fir::FirOpBuilder &, mlir::Location,
                          mlir::Value pointer, mlir::Value target);
 void genPointerAssociateRemapping(fir::FirOpBuilder &, mlir::Location,
                                   mlir::Value pointer, mlir::Value target,
                                   mlir::Value bounds);
-
-mlir::Value genCpuTime(fir::FirOpBuilder &, mlir::Location);
-void genDateAndTime(fir::FirOpBuilder &, mlir::Location,
-                    std::optional<fir::CharBoxValue> date,
-                    std::optional<fir::CharBoxValue> time,
-                    std::optional<fir::CharBoxValue> zone, mlir::Value values);
-
-void genRandomInit(fir::FirOpBuilder &, mlir::Location, mlir::Value repeatable,
-                   mlir::Value imageDistinct);
-void genRandomNumber(fir::FirOpBuilder &, mlir::Location, mlir::Value harvest);
-void genRandomSeed(fir::FirOpBuilder &, mlir::Location, mlir::Value size,
-                   mlir::Value put, mlir::Value get);
-
-/// generate runtime call to transfer intrinsic with no size argument
-void genTransfer(fir::FirOpBuilder &builder, mlir::Location loc,
-                 mlir::Value resultBox, mlir::Value sourceBox,
-                 mlir::Value moldBox);
-
-/// generate runtime call to transfer intrinsic with size argument
-void genTransferSize(fir::FirOpBuilder &builder, mlir::Location loc,
-                     mlir::Value resultBox, mlir::Value sourceBox,
-                     mlir::Value moldBox, mlir::Value size);
-
-/// generate system_clock runtime call/s
-/// all intrinsic arguments are optional and may appear here as mlir::Value{}
-void genSystemClock(fir::FirOpBuilder &, mlir::Location, mlir::Value count,
-                    mlir::Value rate, mlir::Value max);
-
 } // namespace lower
 } // namespace Fortran
 
