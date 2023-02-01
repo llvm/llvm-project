54302f9a80862b8f4cbcffe4c74b557ba5e5d6a5
Adrian Prantl <aprantl@apple.com>
2023-01-31 18:22:44 -0800

    Add an LLDB test for Swift macros.

---
+++ lldb/test/API/lang/swift/macro/TestSwiftMacro.py
@@ -0,0 +1,35 @@
+import lldb
+from lldbsuite.test.decorators import *
+import lldbsuite.test.lldbtest as lldbtest
+import lldbsuite.test.lldbutil as lldbutil
+import unittest2
+
+
+class TestSwiftMacro(lldbtest.TestBase):
+
+    @swiftTest
+    # At the time of writing swift/test/Macros/macro_expand.swift is also disabled.
+    @expectedFailureAll(oslist=['linux'])
+    def test(self):
+        """Test Swift macros"""
+        self.build()
+        target, process, thread, bkpt = lldbutil.run_to_source_breakpoint(
+            self, 'break here', lldb.SBFileSpec('main.swift'))
+
+        thread.StepOver()
+        thread.StepInto()
+        # This is the expanded macro source, we should be able to step into it.
+        self.expect('reg read pc', substrs=[
+            '[inlined] stringify at',
+            '13testStringify'
+        ])
+
+        # Macros are not supported in the expression evaluator.
+        self.expect('expression -- #stringify(1)', error=True,
+                    substrs=["external macro implementation",
+                              'MacroImpl.StringifyMacro',
+                              "could not be found"])
+
+        # Make sure we can set a symbolic breakpoint on a macro.
+        b = target.BreakpointCreateByName("stringify")
+        self.assertGreaterEqual(b.GetNumLocations(), 1)
