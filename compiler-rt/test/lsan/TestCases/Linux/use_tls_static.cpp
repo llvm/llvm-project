ef1e9ee3825343170d75a32658cb679920d888e3
Kirill Stoimenov <kstoimenov@google.com>
2023-02-01 22:00:13 +0000

    [LSAN] Add GetUserAddr function which returns the user visible address of an internal pointer
    For HWASAN this would be the tagged address. It is the same pointer when pointer tagging is not used. Coincidently this also fixes some test which rely on comparing pointers.
    
    Reviewed By: vitalybuka
    
    Differential Revision: https://reviews.llvm.org/D143121

--- compiler-rt/test/lsan/TestCases/Linux/use_tls_static.cpp
+++ compiler-rt/test/lsan/TestCases/Linux/use_tls_static.cpp
@@ -4,9 +4,6 @@
 // RUN: %env_lsan_opts="report_objects=1:use_stacks=0:use_registers=0:use_tls=1" %run %t 2>&1
 // RUN: %env_lsan_opts="" %run %t 2>&1
 
-// Fixme: remove once test passes with hwasan
-// UNSUPPORTED: hwasan
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "sanitizer_common/print_address.h"
@@ -21,4 +18,4 @@ int main() {
 // CHECK: Test alloc: [[ADDR:0x[0-9,a-f]+]]
 // CHECK: LeakSanitizer: detected memory leaks
 // CHECK: [[ADDR]] (1337 bytes)
-// CHECK: SUMMARY: {{(Leak|Address)}}Sanitizer:
+// CHECK: SUMMARY: {{.*}}Sanitizer:
