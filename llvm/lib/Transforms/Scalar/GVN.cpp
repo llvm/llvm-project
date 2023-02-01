3a5777f6308bcac33d7320a3ffaf368eaa98478a
Guozhi Wei <carrot@google.com>
2023-02-01 22:48:06 +0000

    Revert "[GVN] Don't count debug instructions when limit the number of checked instructions"
    This reverts commit f494b366ff8a076a72a8e1b7a6f401686d6eb0e6.

--- llvm/lib/Transforms/Scalar/GVN.cpp
+++ llvm/lib/Transforms/Scalar/GVN.cpp
@@ -1369,8 +1369,6 @@ LoadInst *GVNPass::findLoadToHoistIntoPred(BasicBlock *Pred, BasicBlock *LoadBB,
 
   unsigned int NumInsts = MaxNumInsnsPerBlock;
   for (Instruction &Inst : *SuccBB) {
-    if (Inst.isDebugOrPseudoInst())
-      continue;
     if (--NumInsts == 0)
       return nullptr;
 
