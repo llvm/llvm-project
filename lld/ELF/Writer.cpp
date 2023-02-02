cb96e84f82c2057f9372bd0051d7639fb7c0ca04
Yuhan Deng <yhdeng@stanford.edu>
2023-02-02 14:16:14 -0800

    [lld] lld/ELF/Writer.cpp: write directly to outs when output is "-"
    Co-authored-by: Keith Winstein <keithw@cs.stanford.edu>

--- lld/ELF/Writer.cpp
+++ lld/ELF/Writer.cpp
@@ -652,6 +652,13 @@ template <class ELFT> void Writer<ELFT>::run() {
     if (errorCount())
       return;
 
+    if (config->outputFile == "-") {
+      lld::outs() << StringRef((const char *)buffer->getBufferStart(),
+                               buffer->getBufferSize());
+      lld::outs().flush();
+      return;
+    }
+
     if (auto e = buffer->commit())
       fatal("failed to write output '" + buffer->getPath() +
             "': " + toString(std::move(e)));
