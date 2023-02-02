6dabe601994def41eb420e2f19a7e89c57fddff5
Adrian Vogelsgesang <avogelsgesang@salesforce.com>
2023-02-01 17:39:27 -0800

    [LLDB] Fix assertion failure by removing `CopyType` in `std::coroutine_handle` pretty printer
    The pretty printer for `std::coroutine_handle` was running into
    > Assertion failed: (target_ctx != source_ctx && "Can't import into itself")
    from ClangASTImporter.h, line 270.
    
    This commit fixes the issue by removing the `CopyType` call from the
    pretty printer. While this call was necessary in the past, it seems to
    be no longer required, at least all test cases are still passing. Maybe
    something changed in the meantime around the handling of `TypesystemClang`
    instances. I don't quite understand why `CopyType` was necessary earlier.
    
    I am not sure how to add a regression test for this, though. It seems
    the issue is already triggered by the exising `TestCoroutineHandle.py`,
    but API tests seem to ignore all violations of `lldbassert` and still
    report the test as "passed", even if assertions were triggered
    
    Differential Revision: https://reviews.llvm.org/D143127
    
    (cherry picked from commit 54d4a2550d3167d51a9d386d9823a06aca459531)

--- lldb/source/Plugins/Language/CPlusPlus/Coroutines.h
+++ lldb/source/Plugins/Language/CPlusPlus/Coroutines.h
@@ -16,8 +16,6 @@
 
 namespace lldb_private {
 
-class ClangASTImporter;
-
 namespace formatters {
 
 /// Summary provider for `std::coroutine_handle<T>` from  libc++, libstdc++ and
@@ -50,7 +48,6 @@ private:
   lldb::ValueObjectSP m_resume_ptr_sp;
   lldb::ValueObjectSP m_destroy_ptr_sp;
   lldb::ValueObjectSP m_promise_ptr_sp;
-  std::unique_ptr<lldb_private::ClangASTImporter> m_ast_importer;
 };
 
 SyntheticChildrenFrontEnd *
