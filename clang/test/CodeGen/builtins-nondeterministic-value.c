3de55e6b0e7faf374510aa469aafbf3fba7c848e
ManuelJBrito <manuel.brito@tecnico.ulisboa.pt>
2023-02-03 17:51:43 +0000

    Fix tests commited in 450a461
    Remove alignment matching in tests to avoid ABI compatibility issues

--- clang/test/CodeGen/builtins-nondeterministic-value.c
+++ clang/test/CodeGen/builtins-nondeterministic-value.c
@@ -42,19 +42,19 @@ _Bool clang_nondet_b( _Bool x) {
 
 void clang_nondet_fv( ) {
 // CHECK-LABEL: entry
-// CHECK: [[A:%.*]] = alloca <4 x float>, align 16
+// CHECK: [[A:%.*]] = alloca <4 x float>, align
 // CHECK: [[R:%.*]] = freeze <4 x float> poison
-// CHECK: store <4 x float> [[R]], ptr [[A]], align 16
+// CHECK: store <4 x float> [[R]], ptr [[A]], align
 // CHECK: ret void
   float4 x = __builtin_nondeterministic_value(x);
 }
 
 void clang_nondet_bv( ) {
-// CHECK: [[A:%.*]] = alloca i8, align 1
+// CHECK: [[A:%.*]] = alloca i8, align
 // CHECK: [[V:%.*]] = freeze <4 x i1> poison
 // CHECK: [[SV:%.*]] = shufflevector <4 x i1> [[V]], <4 x i1> poison, <8 x i32> <i32 0, i32 1, i32 2, i32 3, i32 undef, i32 undef, i32 undef, i32 undef>
 // CHECK: [[BC:%.*]] = bitcast <8 x i1> [[SV]] to i8
-// CHECK: store i8 [[BC]], ptr [[A]], align 1
+// CHECK: store i8 [[BC]], ptr [[A]], align
 // CHECK: ret void
   bool4 x = __builtin_nondeterministic_value(x);
 }
