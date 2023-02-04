ae691c381261311c4ae2697fc5b1ae2e2113ca6c
Shengchen Kan <shengchen.kan@intel.com>
2023-02-04 09:35:32 +0800

    [UpdateTestChecks][NFC] Share the code to get CHECK prefix between all scripts
    Reviewed By: MaskRay
    
    Differential Revision: https://reviews.llvm.org/D143307

--- llvm/utils/update_llc_test_checks.py
+++ llvm/utils/update_llc_test_checks.py
@@ -98,10 +98,7 @@ def main():
       llc_cmd_args = llc_cmd_args.replace('< %s', '').replace('%s', '').strip()
       if ti.path.endswith('.mir'):
         llc_cmd_args += ' -x mir'
-      check_prefixes = [item for m in common.CHECK_PREFIX_RE.finditer(filecheck_cmd)
-                               for item in m.group(1).split(',')]
-      if not check_prefixes:
-        check_prefixes = ['CHECK']
+      check_prefixes = common.get_check_prefixes(filecheck_cmd)
 
       # FIXME: We should use multiple check prefixes to common check lines. For
       # now, we just ignore all but the last.
