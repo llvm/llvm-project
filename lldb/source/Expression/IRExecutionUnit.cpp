128ac581adbb28063abed2007ce06a2217c87d09
Vasileios Porpodas <vporpodas@google.com>
2023-02-06 13:05:01 -0800

    [NFC][IR] Make Module::getAliasList() private
    This patch adds several missing AliasList modifier functions, like
    removeAlias(), eraseAlias() and insertAlias().
    There is no longer need to access the list directly so it also makes
    getAliaList() private.
    
    Differential Revision: https://reviews.llvm.org/D143958

--- lldb/source/Expression/IRExecutionUnit.cpp
+++ lldb/source/Expression/IRExecutionUnit.cpp
@@ -410,7 +410,7 @@ void IRExecutionUnit::GetRunnableInfo(Status &error, lldb::addr_t &func_addr,
     RegisterOneValue(global_var);
   }
 
-  for (llvm::GlobalAlias &global_alias : m_module->getAliasList()) {
+  for (llvm::GlobalAlias &global_alias : m_module->getAliases()) {
     RegisterOneValue(global_alias);
   }
 
