2dd221fe482c1d202ba2fecfabd09fca7357b4bd
Bjorn Pettersson <bjorn.a.pettersson@ericsson.com>
2023-02-06 11:29:17 +0100

    Remove no longer needed includes of LegacyPassManager.h
    Most of the removed includes should probably have been removed already
    when we removed TargetMachine::adjustPassManager.

--- llvm/lib/Target/RISCV/RISCVTargetMachine.cpp
+++ llvm/lib/Target/RISCV/RISCVTargetMachine.cpp
@@ -29,7 +29,6 @@
 #include "llvm/CodeGen/Passes.h"
 #include "llvm/CodeGen/TargetLoweringObjectFileImpl.h"
 #include "llvm/CodeGen/TargetPassConfig.h"
-#include "llvm/IR/LegacyPassManager.h"
 #include "llvm/InitializePasses.h"
 #include "llvm/MC/TargetRegistry.h"
 #include "llvm/Support/FormattedStream.h"
