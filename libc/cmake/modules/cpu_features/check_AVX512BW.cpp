d11e27db6a222e990595825f4f1caf4f76b510ab
Guillaume Chatelet <gchatelet@google.com>
2023-02-07 10:32:03 +0000

    [libc][NFC] Move cpu_features to macros folder

--- libc/cmake/modules/cpu_features/check_AVX512BW.cpp
+++ libc/cmake/modules/cpu_features/check_AVX512BW.cpp
@@ -1,4 +1,4 @@
-#include "src/__support/cpu_features.h"
+#include "src/__support/macros/cpu_features.h"
 
 #ifndef LIBC_TARGET_HAS_AVX512BW
 #error unsupported
--- libc/src/__support/FPUtil/multiply_add.h
+++ libc/src/__support/FPUtil/multiply_add.h
@@ -10,8 +10,8 @@
 #define LLVM_LIBC_SRC_SUPPORT_FPUTIL_MULTIPLY_ADD_H
 
 #include "src/__support/common.h"
-#include "src/__support/cpu_features.h"
 #include "src/__support/macros/architectures.h"
+#include "src/__support/macros/cpu_features.h"
 
 namespace __llvm_libc {
 namespace fputil {
