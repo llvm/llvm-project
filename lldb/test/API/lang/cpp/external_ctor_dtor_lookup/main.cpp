b296ddd91cb8e7676b3c38ca7f6d7bdbd84eb4af
Michael Buch <michaelbuch12@gmail.com>
2023-02-07 13:53:30 +0000

    [lldb][DWARFASTParserClang] Attach linkage name to ctors/dtors if missing
    **Summary**
    
    This patch addresses the case where we have a `DW_AT_external`
    subprogram for a constructor (and/or destructor) that doesn't carry
    a `DW_AT_linkage_name` attribute. The corresponding DIE(s) that
    represent the definition will have a linkage name, but if the name
    contains constructs that LLDBs fallback mechanism for guessing mangled
    names to resolve external symbols doesn't support (e.g., abi-tags)
    then we end up failing to resolve the function call.
    
    We address this by trying to find the linkage name before we create
    the constructor/destructor decl, which will get attached using
    an `AsmLabelAttr` to make symbol resolution easier.
    
    **Testing**
    
    * Added API test
    
    Differential Revision: https://reviews.llvm.org/D143652

---
+++ lldb/test/API/lang/cpp/external_ctor_dtor_lookup/main.cpp
@@ -0,0 +1,13 @@
+#include "lib.h"
+
+struct Foo {};
+
+struct Bar {
+  Wrapper<Foo> getWrapper() { return Wrapper<Foo>(); }
+  int sinkWrapper(Wrapper<Foo>) { return -1; }
+};
+
+int main() {
+  Bar b;
+  return b.sinkWrapper(b.getWrapper());
+}
