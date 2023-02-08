aff3cdc604b01d3db1674e8d1402a5e5727fa087
Nikolas Klauser <nikolasklauser@berlin.de>
2023-02-08 20:09:12 +0100

    [libc++] Optimize std::ranges::{min, max} for types that are cheap to copy
    Don't forward to `min_element` for small types that are trivially copyable, and instead use a naive loop that keeps track of the smallest element (as opposed to an iterator to the smallest element). This allows the compiler to vectorize the loop in some cases.
    
    Reviewed By: #libc, ldionne
    
    Spies: ldionne, libcxx-commits
    
    Differential Revision: https://reviews.llvm.org/D143596

--- libcxx/include/__type_traits/is_trivially_copyable.h
+++ libcxx/include/__type_traits/is_trivially_copyable.h
@@ -11,6 +11,7 @@
 
 #include <__config>
 #include <__type_traits/integral_constant.h>
+#include <cstdint>
 
 #if !defined(_LIBCPP_HAS_NO_PRAGMA_SYSTEM_HEADER)
 #  pragma GCC system_header
@@ -25,6 +26,11 @@ template <class _Tp> struct _LIBCPP_TEMPLATE_VIS is_trivially_copyable
 #if _LIBCPP_STD_VER >= 17
 template <class _Tp>
 inline constexpr bool is_trivially_copyable_v = __is_trivially_copyable(_Tp);
+#endif
+
+#if _LIBCPP_STD_VER >= 20
+template <class _Tp>
+inline constexpr bool __is_cheap_to_copy = is_trivially_copyable_v<_Tp> && sizeof(_Tp) <= sizeof(std::intmax_t);
 #endif
 
 _LIBCPP_END_NAMESPACE_STD
