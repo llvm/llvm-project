99b7f382742425a8f5b67434b7f10729d0380deb
Ramon de C Valle <rcvalle@users.noreply.github.com>
2023-02-08 19:19:13 +0000

    Add CFI integer types normalization
    This commit adds a new option (i.e.,
    `-fsanitize-cfi-icall-normalize-integers`) for normalizing integer types
    as vendor extended types for cross-language LLVM CFI/KCFI support with
    other languages that can't represent and encode C/C++ integer types.
    
    Specifically, integer types are encoded as their defined representations
    (e.g., 8-bit signed integer, 16-bit signed integer, 32-bit signed
    integer, ...) for compatibility with languages that define
    explicitly-sized integer types (e.g., i8, i16, i32, ..., in Rust).
    
    ``-fsanitize-cfi-icall-normalize-integers`` is compatible with
    ``-fsanitize-cfi-icall-generalize-pointers``.
    
    This helps with providing cross-language CFI support with the Rust
    compiler and is an alternative solution for the issue described and
    alternatives proposed in the RFC
    https://github.com/rust-lang/rfcs/pull/3296.
    
    For more information about LLVM CFI/KCFI and cross-language LLVM
    CFI/KCFI support for the Rust compiler, see the design document in the
    tracking issue https://github.com/rust-lang/rust/issues/89653.
    
    Relands 5ef0f08c4e5b2723202f19c520aa64cc8acfd981 with fixes.
    
    Reviewed By: pcc, samitolvanen
    
    Differential Revision: https://reviews.llvm.org/D139395

--- clang/lib/Driver/SanitizerArgs.cpp
+++ clang/lib/Driver/SanitizerArgs.cpp
@@ -713,6 +713,9 @@ SanitizerArgs::SanitizerArgs(const ToolChain &TC,
     CfiICallGeneralizePointers =
         Args.hasArg(options::OPT_fsanitize_cfi_icall_generalize_pointers);
 
+    CfiICallNormalizeIntegers =
+        Args.hasArg(options::OPT_fsanitize_cfi_icall_normalize_integers);
+
     if (CfiCrossDso && CfiICallGeneralizePointers && DiagnoseErrors)
       D.Diag(diag::err_drv_argument_not_allowed_with)
           << "-fsanitize-cfi-cross-dso"
@@ -1216,6 +1219,9 @@ void SanitizerArgs::addArgs(const ToolChain &TC, const llvm::opt::ArgList &Args,
 
   if (CfiICallGeneralizePointers)
     CmdArgs.push_back("-fsanitize-cfi-icall-generalize-pointers");
+
+  if (CfiICallNormalizeIntegers)
+    CmdArgs.push_back("-fsanitize-cfi-icall-experimental-normalize-integers");
 
   if (CfiCanonicalJumpTables)
     CmdArgs.push_back("-fsanitize-cfi-canonical-jump-tables");
