cae033dcf227aeecf58fca5af6fc7fde1fd2fb4f
Amara Emerson <amara@apple.com>
2023-02-08 19:52:00 -0800

    Inlining: Run the legacy AlwaysInliner before the regular inliner.
    We have several situations where it's beneficial for code size to ensure that every
    call to always-inline functions are inlined before normal inlining decisions are
    made. While the normal inliner runs in a "MandatoryOnly" mode to try to do this,
    it only does it on a per-SCC basis, rather than the whole module. Ensuring that
    all mandatory inlinings are done before any heuristic based decisions are made
    just makes sense.
    
    Despite being referred to the "legacy" AlwaysInliner pass, it's already necessary
    for -O0 because the CGSCC inliner is too expensive in compile time to run at -O0.
    
    This also fixes an exponential compile time blow up in
    https://github.com/llvm/llvm-project/issues/59126
    
    Differential Revision: https://reviews.llvm.org/D143624

--- clang/test/CodeGen/code-coverage.c
+++ clang/test/CodeGen/code-coverage.c
@@ -14,7 +14,6 @@
 // RUN: %clang_cc1 -emit-llvm-bc -o /dev/null -fdebug-pass-manager -fprofile-arcs %s 2>&1 | FileCheck --check-prefix=NEWPM %s
 // RUN: %clang_cc1 -emit-llvm-bc -o /dev/null -fdebug-pass-manager -fprofile-arcs -O3 %s 2>&1 | FileCheck --check-prefix=NEWPM-O3 %s
 
-// NEWPM-NOT: Running pass
 // NEWPM: Running pass: GCOVProfilerPass
 
 // NEWPM-O3-NOT: Running pass
