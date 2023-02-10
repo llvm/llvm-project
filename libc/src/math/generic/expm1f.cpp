737e1cd161f39171ed8d8bf7fcd9567498b4796e
Guillaume Chatelet <gchatelet@google.com>
2023-02-10 15:15:10 +0000

    [libc] Move likely/unlikely to the optimization header

--- libc/src/math/generic/expm1f.cpp
+++ libc/src/math/generic/expm1f.cpp
@@ -16,7 +16,8 @@
 #include "src/__support/FPUtil/multiply_add.h"
 #include "src/__support/FPUtil/nearest_integer.h"
 #include "src/__support/common.h"
-#include "src/__support/macros/properties/cpu_features.h"
+#include "src/__support/macros/optimization.h"            // LIBC_UNLIKELY
+#include "src/__support/macros/properties/cpu_features.h" // LIBC_TARGET_CPU_HAS_FMA
 
 #include <errno.h>
 
