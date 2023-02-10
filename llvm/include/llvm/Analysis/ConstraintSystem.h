9497680067cc5a2e7d4e0bf657b23d57c06e5e97
Florian Hahn <flo@fhahn.com>
2023-02-10 17:12:21 +0000

    [ConstraintElim] Update getLastConstraint to return to last row. (NFC)
    The current code incorrectly returned the first instead of the last row.
    This fixes the debug output.

--- llvm/include/llvm/Analysis/ConstraintSystem.h
+++ llvm/include/llvm/Analysis/ConstraintSystem.h
@@ -91,7 +91,7 @@ public:
 
   bool isConditionImplied(SmallVector<int64_t, 8> R) const;
 
-  ArrayRef<int64_t> getLastConstraint() { return Constraints[0]; }
+  ArrayRef<int64_t> getLastConstraint() { return Constraints.back(); }
   void popLastConstraint() { Constraints.pop_back(); }
   void popLastNVariables(unsigned N) {
     for (auto &C : Constraints) {
