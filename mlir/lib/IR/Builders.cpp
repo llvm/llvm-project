506880439cdd42432ffa4a8dfec08d0642824c47
Jake Hall <jakeh@graphcore.ai>
2023-02-13 14:10:20 +0000

    [mlir] Add Float8E5M2FNUZ and Float8E4M3FNUZ types to MLIR
    Float8E5M2FNUZ and Float8E4M3FNUZ have been added to APFloat in D141863.
    This change adds these types as MLIR builtin types alongside Float8E5M2
    and Float8E4M3FN (added in D133823 and D138075).
    
    Reviewed By: krzysz00
    
    Differential Revision: https://reviews.llvm.org/D143744

--- mlir/lib/IR/Builders.cpp
+++ mlir/lib/IR/Builders.cpp
@@ -41,6 +41,14 @@ FloatType Builder::getFloat8E4M3FNType() {
   return FloatType::getFloat8E4M3FN(context);
 }
 
+FloatType Builder::getFloat8E5M2FNUZType() {
+  return FloatType::getFloat8E5M2FNUZ(context);
+}
+
+FloatType Builder::getFloat8E4M3FNUZType() {
+  return FloatType::getFloat8E4M3FNUZ(context);
+}
+
 FloatType Builder::getBF16Type() { return FloatType::getBF16(context); }
 
 FloatType Builder::getF16Type() { return FloatType::getF16(context); }
