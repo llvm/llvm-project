a722cb662c67d0f0db60b83671dec8e700952b47
Tobias Hieta <tobias@hieta.se>
2023-02-13 16:58:13 +0100

    [NFC] Fix using-declspec.cpp test with non-C++17 compilers

--- clang/test/SemaCXX/using-declspec.cpp
+++ clang/test/SemaCXX/using-declspec.cpp
@@ -3,7 +3,7 @@
 // This should ignore the alignment and issue a warning about
 // align not being used
 auto func() -> __declspec(align(16)) int; // expected-warning{{attribute ignored when parsing type}}
-static_assert(alignof(decltype(func())) == alignof(int));
+static_assert(alignof(decltype(func())) == alignof(int), "error");
 
 // The following should NOT assert since alignment should
 // follow the type
@@ -16,4 +16,5 @@ int i = (__declspec(align(16))int)12; // expected-warning{{attribute ignored whe
 
 // But there is a declaration here!
 typedef __declspec(align(16)) int Foo;
-static_assert(alignof(Foo) == 16);
+static_assert(alignof(Foo) == 16, "error");
+
