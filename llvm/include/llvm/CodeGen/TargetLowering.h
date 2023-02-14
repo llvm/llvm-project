19ae3a89fd66aca1348ae03eb37decf575dda4f6
Jake Egan <jakeegan10@gmail.com>
2023-02-14 15:13:28 -0500

    Revert "[CGP] Add generic TargetLowering::shouldAlignPointerArgs() implementation"
    These commits are causing a test-suite build failure on AIX. Revert for now for time to investigate.
    https://lab.llvm.org/buildbot/#/builders/214/builds/5779/steps/9/logs/stdio
    
    This reverts commit 46105575b1317e6abae51cd12c6a021a380f4528 and dada386e1e4e1e7a1042388e98a4c7d2f4b5261c.

--- llvm/include/llvm/CodeGen/TargetLowering.h
+++ llvm/include/llvm/CodeGen/TargetLowering.h
@@ -1921,10 +1921,10 @@ public:
   /// the object whose address is being passed. If so then MinSize is set to the
   /// minimum size the object must be to be aligned and PrefAlign is set to the
   /// preferred alignment.
-  virtual bool
-  shouldUpdatePointerArgAlignment(const CallInst *CI, unsigned &MinSize,
-                                  Align &PrefAlign,
-                                  const TargetTransformInfo &TTI) const;
+  virtual bool shouldAlignPointerArgs(CallInst * /*CI*/, unsigned & /*MinSize*/,
+                                      Align & /*PrefAlign*/) const {
+    return false;
+  }
 
   //===--------------------------------------------------------------------===//
   /// \name Helpers for TargetTransformInfo implementations
