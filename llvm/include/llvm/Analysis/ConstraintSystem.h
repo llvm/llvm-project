5674e32e24ecfd7f9ae8d4cdde6f734676e3b841
Nikita Popov <npopov@redhat.com>
2023-02-15 16:36:44 +0100

    Revert "Recommit "[ConstraintElimination] Change debug output to display variable names.""
    This reverts commit a3c517d0a33b97a7e137149d1e2d3b9db37deb9b.
    
    This causes build failures:
    
        /home/npopov/repos/llvm-project/llvm/lib/Analysis/ConstraintSystem.cpp: In member function ‘llvm::SmallVector<std::__cxx11::basic_string<char> > llvm::ConstraintSystem::getVarNamesList() const’:
        /home/npopov/repos/llvm-project/llvm/lib/Analysis/ConstraintSystem.cpp:118:10: error: invalid use of incomplete type ‘class llvm::Value’
          118 |     if (V->getName().empty())
              |          ^~
        In file included from /home/npopov/repos/llvm-project/llvm/lib/Analysis/ConstraintSystem.cpp:9:
        /home/npopov/repos/llvm-project/llvm/include/llvm/Analysis/ConstraintSystem.h:21:7: note: forward declaration of ‘class llvm::Value’
           21 | class Value;
              |       ^~~~~
        /home/npopov/repos/llvm-project/llvm/lib/Analysis/ConstraintSystem.cpp:119:22: error: invalid use of incomplete type ‘class llvm::Value’
          119 |       OperandName = V->getNameOrAsOperand();
              |                      ^~
        /home/npopov/repos/llvm-project/llvm/include/llvm/Analysis/ConstraintSystem.h:21:7: note: forward declaration of ‘class llvm::Value’
           21 | class Value;
              |       ^~~~~
        /home/npopov/repos/llvm-project/llvm/lib/Analysis/ConstraintSystem.cpp:121:41: error: invalid use of incomplete type ‘class llvm::Value’
          121 |       OperandName = std::string("%") + V->getName().str();
              |                                         ^~
        /home/npopov/repos/llvm-project/llvm/include/llvm/Analysis/ConstraintSystem.h:21:7: note: forward declaration of ‘class llvm::Value’
           21 | class Value;
              |       ^~~~~

--- llvm/include/llvm/Analysis/ConstraintSystem.h
+++ llvm/include/llvm/Analysis/ConstraintSystem.h
@@ -36,17 +36,13 @@ class ConstraintSystem {
   // Eliminate constraints from the system using Fourier–Motzkin elimination.
   bool eliminateUsingFM();
 
+  /// Print the constraints in the system, using x0...xn as variable names.
+  void dump() const;
+
   /// Returns true if there may be a solution for the constraints in the system.
   bool mayHaveSolutionImpl();
 
-  /// Get list of variable names from the Value2Index map.
-  SmallVector<std::string> getVarNamesList() const;
-
 public:
-  ConstraintSystem() {}
-  ConstraintSystem(const DenseMap<Value *, unsigned> &Value2Index)
-      : Value2Index(Value2Index) {}
-
   bool addVariableRow(ArrayRef<int64_t> R) {
     assert(Constraints.empty() || R.size() == Constraints.back().size());
     // If all variable coefficients are 0, the constraint does not provide any
@@ -107,8 +103,8 @@ public:
   /// Returns the number of rows in the constraint system.
   unsigned size() const { return Constraints.size(); }
 
-  /// Print the constraints in the system.
-  void dump() const;
+  /// Print the constraints in the system, using \p Names as variable names.
+  void dump(ArrayRef<std::string> Names) const;
 };
 } // namespace llvm
 
