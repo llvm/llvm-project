60b077a451a005235705b7dea75e46c6f094754c
Dave Lee <davelee.com@gmail.com>
2023-02-15 08:23:57 -0800

    [lldb] Add expression command options in dwim-print
    Adopt `expression`'s options in `dwim-print`.
    
    This is primarily added to support the `--language`/`-l` flag.
    
    Differential Revision: https://reviews.llvm.org/D144114

--- lldb/source/Commands/CommandObjectExpression.h
+++ lldb/source/Commands/CommandObjectExpression.h
@@ -35,6 +35,12 @@ public:
 
     void OptionParsingStarting(ExecutionContext *execution_context) override;
 
+    /// Return the appropriate expression options used for evaluating the
+    /// expression in the given target.
+    EvaluateExpressionOptions GetEvaluateExpressionOptions(
+        const Target &target,
+        const OptionGroupValueObjectDisplay &display_opts);
+
     bool top_level;
     bool unwind_on_error;
     bool ignore_breakpoints;
@@ -66,10 +72,6 @@ protected:
                                 StringList &lines) override;
 
   bool DoExecute(llvm::StringRef command, CommandReturnObject &result) override;
-
-  /// Return the appropriate expression options used for evaluating the
-  /// expression in the given target.
-  EvaluateExpressionOptions GetEvalOptions(const Target &target);
 
   /// Evaluates the given expression.
   /// \param output_stream The stream to which the evaluation result will be
