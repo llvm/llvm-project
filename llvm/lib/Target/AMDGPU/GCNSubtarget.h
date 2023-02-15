0dc1d5a575f0dc96c8fa3b10ddbb0abe7745c8a9
Jay Foad <jay.foad@amd.com>
2023-02-15 16:44:41 +0000

    Implement readcyclecounter for GFX12
    Change-Id: Ic6a58c5b57cdcba8d2cbd486752898d8f7231cef

--- llvm/lib/Target/AMDGPU/GCNSubtarget.h
+++ llvm/lib/Target/AMDGPU/GCNSubtarget.h
@@ -167,6 +167,7 @@ protected:
   bool HasGetWaveIdInst = false;
   bool HasSMemTimeInst = false;
   bool HasShaderCyclesRegister = false;
+  bool HasShaderCyclesHiLoRegisters = false;
   bool HasVOP3Literal = false;
   bool HasNoDataDepHazard = false;
   bool FlatAddressSpace = false;
@@ -798,6 +799,10 @@ public:
 
   bool hasShaderCyclesRegister() const {
     return HasShaderCyclesRegister;
+  }
+
+  bool hasShaderCyclesHiLoRegisters() const {
+    return HasShaderCyclesHiLoRegisters;
   }
 
   bool hasVOP3Literal() const {
