861709107b43d40ad366e0efd225cb804be3b44d
Joseph Huber <jhuber6@vols.utk.edu>
2023-02-16 09:51:21 -0600

    [Libomptarget] Check errors when synchronizing the async queue
    Currently when we synchronize the asynchronous queue for the plugins, we
    ignore the return value. This is problematic because we will continue on
    like nothing happened if the kernel fails.
    
    Fixes https://github.com/llvm/llvm-project/issues/60814
    
    Reviewed By: jdoerfert
    
    Differential Revision: https://reviews.llvm.org/D144191

--- openmp/libomptarget/src/private.h
+++ openmp/libomptarget/src/private.h
@@ -250,9 +250,12 @@ public:
     if (AsyncInfo == &LocalAsyncInfo)
       return;
 
+    auto DoneOrErr = AsyncInfo->isDone();
+    if (!DoneOrErr)
+      FATAL_MESSAGE0(1, "Error while synchronizing the async queue\n");
     // If the are device operations still pending, return immediately without
     // deallocating the handle.
-    if (!AsyncInfo->isDone())
+    if (!*DoneOrErr)
       return;
 
     // Delete the handle and unset it from the OpenMP task data.
