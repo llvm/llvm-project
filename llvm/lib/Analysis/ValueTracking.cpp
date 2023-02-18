3bd38f66398303eb375085dc2681c94c7169f273
Noah Goldstein <goldstein.w.n@gmail.com>
2023-02-18 13:36:06 -0600

    [ValueTracking] Add cases for additional ops in `isKnownNonZero`
    Add cases for the following ops:
        - 0-X            -- https://alive2.llvm.org/ce/z/6C75Li
        - bitreverse(X)  -- https://alive2.llvm.org/ce/z/SGG1q9
        - bswap(X)       -- https://alive2.llvm.org/ce/z/p7pzwh
        - ctpop(X)       -- https://alive2.llvm.org/ce/z/c5y3BC
        - abs(X)         -- https://alive2.llvm.org/ce/z/yxXGz_
                            https://alive2.llvm.org/ce/z/rSRg4K
        - uadd_sat(X, Y) -- https://alive2.llvm.org/ce/z/Zw-y4W
                            https://alive2.llvm.org/ce/z/2NRqRz
                            https://alive2.llvm.org/ce/z/M1OpF8
    
    Reviewed By: nikic
    
    Differential Revision: https://reviews.llvm.org/D142828

--- llvm/lib/Analysis/ValueTracking.cpp
+++ llvm/lib/Analysis/ValueTracking.cpp
@@ -2607,6 +2607,12 @@ bool isKnownNonZero(const Value *V, const APInt &DemandedElts, unsigned Depth,
             Q.DL.getTypeSizeInBits(I->getType()).getFixedValue())
       return isKnownNonZero(I->getOperand(0), Depth, Q);
     break;
+  case Instruction::Sub:
+    if (auto *C = dyn_cast<Constant>(I->getOperand(0)))
+      if (C->isNullValue() &&
+          isKnownNonZero(I->getOperand(1), DemandedElts, Depth, Q))
+        return true;
+    break;
   case Instruction::Or:
     // X | Y != 0 if X != 0 or Y != 0.
     return isKnownNonZero(I->getOperand(0), DemandedElts, Depth, Q) ||
@@ -2751,8 +2757,26 @@ bool isKnownNonZero(const Value *V, const APInt &DemandedElts, unsigned Depth,
            isGuaranteedNotToBePoison(I->getOperand(0), Q.AC, Q.CxtI, Q.DT,
                                      Depth);
   case Instruction::Call:
-    if (cast<CallInst>(I)->getIntrinsicID() == Intrinsic::vscale)
-      return true;
+    if (auto *II = dyn_cast<IntrinsicInst>(I)) {
+      switch (II->getIntrinsicID()) {
+      case Intrinsic::abs:
+      case Intrinsic::bitreverse:
+      case Intrinsic::bswap:
+      case Intrinsic::ctpop:
+        if (isKnownNonZero(II->getArgOperand(0), DemandedElts, Depth, Q))
+          return true;
+        break;
+      case Intrinsic::uadd_sat:
+        if (isKnownNonZero(II->getArgOperand(0), DemandedElts, Depth, Q) ||
+            isKnownNonZero(II->getArgOperand(1), DemandedElts, Depth, Q))
+          return true;
+        break;
+      case Intrinsic::vscale:
+        return true;
+      default:
+        break;
+      }
+    }
     break;
   }
 
