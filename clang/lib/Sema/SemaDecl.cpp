890146b19206827bc48ee1ae1dc1534ff2ff18d7
Paulo Matos <pmatos@igalia.com>
2023-02-17 18:38:58 -0800

    [WebAssembly] Initial support for reference type externref in clang
    This patch introduces a new type __externref_t that denotes a WebAssembly opaque
    reference type. It also implements builtin __builtin_wasm_ref_null_extern(),
    that returns a null value of __externref_t. This lays the ground work
    for further builtins and reference types.
    
    Reviewed By: aaron.ballman
    
    Differential Revision: https://reviews.llvm.org/D122215

--- clang/lib/Sema/SemaDecl.cpp
+++ clang/lib/Sema/SemaDecl.cpp
@@ -8665,7 +8665,8 @@ void Sema::CheckVariableDeclarationType(VarDecl *NewVD) {
     return;
   }
 
-  if (!NewVD->hasLocalStorage() && T->isSizelessType()) {
+  if (!NewVD->hasLocalStorage() && T->isSizelessType() &&
+      !T->isWebAssemblyReferenceType()) {
     Diag(NewVD->getLocation(), diag::err_sizeless_nonlocal) << T;
     NewVD->setInvalidDecl();
     return;
