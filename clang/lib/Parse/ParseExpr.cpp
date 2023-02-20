77b65bac9a8893f8272d780e55686b6fdc88d16b
Alejandro Álvarez Ayllón <alejandro.alvarez@sonarsource.com>
2023-02-20 16:46:10 +0100

    [clang] Fix eager skipping on new with unknown type and no new-initializer
    i.e. in `function(new Unknown);`, the parser should skip only
    until the semicolon.

--- clang/lib/Parse/ParseExpr.cpp
+++ clang/lib/Parse/ParseExpr.cpp
@@ -3694,7 +3694,7 @@ bool Parser::ParseExpressionList(SmallVectorImpl<Expr *> &Exprs,
       SawError = true;
       if (FailImmediatelyOnInvalidExpr)
         break;
-      SkipUntil(tok::comma, tok::r_paren, StopBeforeMatch);
+      SkipUntil(tok::comma, tok::r_paren, StopAtSemi | StopBeforeMatch);
     } else {
       Exprs.push_back(Expr.get());
     }
