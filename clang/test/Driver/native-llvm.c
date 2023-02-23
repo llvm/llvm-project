b34c80b32153af15cbae376a6e3b555f11c01b86
Fangrui Song <i@maskray.me>
2023-02-23 15:06:02 -0800

    [Driver] Define BareMetal::HasNativeLLVMSupport to return true
    D33259 switched the default linker to ld.lld which supports LLVM LTO.
    We can support LTO compile/link in one command and drop the
    `unable to pass LLVM bit-code files to linker` error.
    
    Fix https://github.com/llvm/llvm-project/issues/52807

--- clang/test/Driver/native-llvm.c
+++ clang/test/Driver/native-llvm.c
@@ -3,3 +3,5 @@
 
 // RUN: %clang -### -flto -target x86_64-unknown-unknown %s 2>&1 | FileCheck %s
 // CHECK: error: {{.*}} unable to pass LLVM bit-code files to linker
+
+// RUN: %clang -### -flto --target=arm-none-eabi %s 2>&1 | FileCheck /dev/null --implicit-check-not=error:
