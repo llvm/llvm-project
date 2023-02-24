bdbeb9a238dae384d81f5e55178bc5f3d3ab41d6
Zhangyin <zhangyin2018@iscas.ac.cn>
2023-02-24 15:23:51 +0800

    [libcxx] <experimental/simd> Add ABI tags, class template simd/simd_mask implementations. Add related simd traits and tests.
    [libcxx] <experimental/simd> Add ABI tags, class template simd/simd_mask implementations.
    [libcxx] <experimental/simd> Add traits is_abi_tag[_v], is_simd[_v] and is_simd_mask[_v].
    [libcxx] <experimental/simd> Add related tests.
    
    Reviewed By: #libc, philnik
    
    Differential Revision: https://reviews.llvm.org/D144362

---
+++ libcxx/include/experimental/__simd/vec_ext.h
@@ -0,0 +1,30 @@
+// -*- C++ -*-
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef _LIBCPP_EXPERIMENTAL___SIMD_VEC_EXT_H
+#define _LIBCPP_EXPERIMENTAL___SIMD_VEC_EXT_H
+
+#include <cstddef>
+
+#if _LIBCPP_STD_VER >= 17 && defined(_LIBCPP_ENABLE_EXPERIMENTAL)
+
+_LIBCPP_BEGIN_NAMESPACE_EXPERIMENTAL
+inline namespace parallelism_v2 {
+namespace simd_abi {
+template <int _Np>
+struct __vec_ext {
+  static constexpr size_t __simd_size = _Np;
+};
+
+} // namespace simd_abi
+} // namespace parallelism_v2
+_LIBCPP_END_NAMESPACE_EXPERIMENTAL
+
+#endif // _LIBCPP_STD_VER >= 17 && defined(_LIBCPP_ENABLE_EXPERIMENTAL)
+#endif // _LIBCPP_EXPERIMENTAL___SIMD_VEC_EXT_H
---
+++ libcxx/test/std/experimental/simd/test_utils.h
@@ -0,0 +1,45 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef TEST_UTIL_H
+#define TEST_UTIL_H
+
+#include <utility>
+#include <experimental/simd>
+#include "type_algorithms.h"
+
+namespace ex = std::experimental::parallelism_v2;
+
+constexpr std::size_t max_simd_size = 32;
+
+template <template <class T, std::size_t N> class F>
+struct TestAllSimdAbiFunctor {
+  template <class T, std::size_t N>
+  using sized_abis = types::type_list<ex::simd_abi::fixed_size<N>, ex::simd_abi::deduce_t<T, N>>;
+
+  template <class T, std::size_t... Ns>
+  void instantiate_with_n(std::index_sequence<Ns...>) {
+    (types::for_each(sized_abis<T, Ns + 1>{}, F<T, Ns + 1>{}), ...);
+  }
+
+  template <class T>
+  void operator()() {
+    using abis = types::type_list<ex::simd_abi::scalar, ex::simd_abi::native<T>, ex::simd_abi::compatible<T>>;
+    types::for_each(abis{}, F<T, 1>());
+
+    instantiate_with_n<T>(std::make_index_sequence<max_simd_size - 1>{});
+  }
+};
+
+template <template <class T, std::size_t N> class Func>
+void test_all_simd_abi() {
+  using arithmetic_no_bool_types = types::concatenate_t<types::integer_types, types::floating_point_types>;
+  types::for_each(arithmetic_no_bool_types(), TestAllSimdAbiFunctor<Func>());
+}
+
+#endif // TEST_UTIL_H
