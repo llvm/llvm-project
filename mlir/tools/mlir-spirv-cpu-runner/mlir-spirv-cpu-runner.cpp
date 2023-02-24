dff04fc21c6a88a958495c02776a367829783f17
Markus Böck <markus.boeck02@gmail.com>
2023-02-24 14:11:35 +0100

    [mlir][SPIRV] Enable use of opaque-pointers in spirv-cpu-runner
    Part of https://discourse.llvm.org/t/rfc-switching-the-llvm-dialect-and-dialect-lowerings-to-opaque-pointers/68179
    
    All passes required for lowering have already been successfully converted to allowing opaque-pointers. Switching the Spir-V runner to opaque-pointers is therefore only a matter of turning it on in these conversion passes. Regression tests also did not find any issues.
    
    Differential Revision: https://reviews.llvm.org/D144725

--- mlir/tools/mlir-spirv-cpu-runner/mlir-spirv-cpu-runner.cpp
+++ mlir/tools/mlir-spirv-cpu-runner/mlir-spirv-cpu-runner.cpp
@@ -82,11 +82,18 @@ static LogicalResult runMLIRPasses(Operation *module,
   passManager.addPass(createGpuKernelOutliningPass());
   passManager.addPass(createConvertGPUToSPIRVPass(/*mapMemorySpace=*/true));
 
+  auto enableOpaquePointers = [](auto options) {
+    options.useOpaquePointers = true;
+    return options;
+  };
+
   OpPassManager &nestedPM = passManager.nest<spirv::ModuleOp>();
   nestedPM.addPass(spirv::createSPIRVLowerABIAttributesPass());
   nestedPM.addPass(spirv::createSPIRVUpdateVCEPass());
-  passManager.addPass(createLowerHostCodeToLLVMPass());
-  passManager.addPass(createConvertSPIRVToLLVMPass());
+  passManager.addPass(createLowerHostCodeToLLVMPass(
+      enableOpaquePointers(LowerHostCodeToLLVMPassOptions{})));
+  passManager.addPass(createConvertSPIRVToLLVMPass(
+      enableOpaquePointers(ConvertSPIRVToLLVMPassOptions{})));
   return passManager.run(module);
 }
 
