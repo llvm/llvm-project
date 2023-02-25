ff93c9beefd4848c6a482ab57d6667eb40344026
Alex Bradbury <asb@igalia.com>
2023-02-25 06:17:40 +0000

    [lld][RISCV] Avoid error when encountering unrecognised ISA extensions/versions in RISC-V attributes
    This patch follows on from this RFC thread
    <https://discourse.llvm.org/t/rfc-resolving-issues-related-to-extension-versioning-in-risc-v/68472/>
    and the ensuing discussion in the RISC-V LLVM sync-up call. The
    consensus agreed that the behaviour change in LLD introduced in D138550
    that results in object files including arch attributes with unrecognised
    extensions or versions of extensions is a regression and should be
    treated as such. As it stands, this logic means that LLD will error out
    if trying to link a RISC-V object file from LLVM HEAD (rv32i2p0/rv64i2p0)
    with one from current GCC (rv32i2p1/rv64i2p1 by default).
    
    There's a bigger discussion about exactly when to warn vs error and so
    on, and how to control that, and this patch doesn't attempt to address
    all those questions. It simply tries to fix the problem with a minimally
    invasive change, intended to be cherry-picked for 16.0.x (ideally
    16.0.0, but queued for 16.0.1 if we're too late in the release cycle).
    
    As you can see from the test changes, although the changed logic is
    mostly more permissive, it will reject some embedded arch strings that
    were accepted before. Because the same logic was previously used for
    parsing human-written -march as for the arch attribute (intended to be
    stored in normalised form), various short-hand formats were previously
    accepted. Maintaining support for such ill-formed attributes would
    substantially complicate the logic, and given the previous
    implementation was so much stricter in every other way, was surely a bug
    rather than a design choice.
    
    Surprisingly, the precise rules for how numbers can be embedded into
    extension names isn't fully defined (there is a PR to the ISA manual
    that is not yet merged
    <https://github.com/riscv/riscv-isa-manual/pull/718>).
    In the absence of specific criteria for rejecting extensions names that
    would be ambiguous in abbreviated form,
    `RISCVISAInfo::parseArchStringNormalized` just pulls out the version
    information from the end of each extension description.
    
    Differential Revision: https://reviews.llvm.org/D144353

---
+++ llvm/unittests/Support/RISCVISAInfoTest.cpp
@@ -0,0 +1,105 @@
+//===-- unittests/RISCVISAInfoTest.cpp ------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "llvm/Support/RISCVISAInfo.h"
+#include "llvm/Testing/Support/Error.h"
+#include "gtest/gtest.h"
+
+using namespace llvm;
+
+bool operator==(const llvm::RISCVExtensionInfo &A,
+                const llvm::RISCVExtensionInfo &B) {
+  return A.ExtName == B.ExtName && A.MajorVersion == B.MajorVersion &&
+         A.MinorVersion == B.MinorVersion;
+}
+
+TEST(ParseNormalizedArchString, RejectsUpperCase) {
+  for (StringRef Input : {"RV32", "rV64", "rv32i2P0", "rv64i2p0_A2p0"}) {
+    EXPECT_EQ(
+        toString(RISCVISAInfo::parseNormalizedArchString(Input).takeError()),
+        "string must be lowercase");
+  }
+}
+
+TEST(ParseNormalizedArchString, RejectsInvalidBaseISA) {
+  for (StringRef Input : {"rv32", "rv64", "rv32j", "rv65i"}) {
+    EXPECT_EQ(
+        toString(RISCVISAInfo::parseNormalizedArchString(Input).takeError()),
+        "arch string must begin with valid base ISA");
+  }
+}
+
+TEST(ParseNormalizedArchString, RejectsMalformedInputs) {
+  for (StringRef Input : {"rv64i2p0_", "rv32i2p0__a2p0", "rv32e2.0", "rv64e2p",
+                          "rv32i", "rv64ip1"}) {
+    EXPECT_EQ(
+        toString(RISCVISAInfo::parseNormalizedArchString(Input).takeError()),
+        "extension lacks version in expected format");
+  }
+}
+
+TEST(ParseNormalizedArchString, AcceptsValidBaseISAsAndSetsXLen) {
+  auto MaybeRV32I = RISCVISAInfo::parseNormalizedArchString("rv32i2p0");
+  ASSERT_THAT_EXPECTED(MaybeRV32I, Succeeded());
+  RISCVISAInfo &InfoRV32I = **MaybeRV32I;
+  EXPECT_EQ(InfoRV32I.getExtensions().size(), 1UL);
+  EXPECT_TRUE(InfoRV32I.getExtensions().at("i") ==
+              (RISCVExtensionInfo{"i", 2, 0}));
+  EXPECT_EQ(InfoRV32I.getXLen(), 32U);
+
+  auto MaybeRV32E = RISCVISAInfo::parseNormalizedArchString("rv32e2p0");
+  ASSERT_THAT_EXPECTED(MaybeRV32E, Succeeded());
+  RISCVISAInfo &InfoRV32E = **MaybeRV32E;
+  EXPECT_EQ(InfoRV32E.getExtensions().size(), 1UL);
+  EXPECT_TRUE(InfoRV32E.getExtensions().at("e") ==
+              (RISCVExtensionInfo{"e", 2, 0}));
+  EXPECT_EQ(InfoRV32I.getXLen(), 32U);
+
+  auto MaybeRV64I = RISCVISAInfo::parseNormalizedArchString("rv64i2p0");
+  ASSERT_THAT_EXPECTED(MaybeRV64I, Succeeded());
+  RISCVISAInfo &InfoRV64I = **MaybeRV64I;
+  EXPECT_EQ(InfoRV64I.getExtensions().size(), 1UL);
+  EXPECT_TRUE(InfoRV64I.getExtensions().at("i") ==
+              (RISCVExtensionInfo{"i", 2, 0}));
+  EXPECT_EQ(InfoRV64I.getXLen(), 64U);
+
+  auto MaybeRV64E = RISCVISAInfo::parseNormalizedArchString("rv64e2p0");
+  ASSERT_THAT_EXPECTED(MaybeRV64E, Succeeded());
+  RISCVISAInfo &InfoRV64E = **MaybeRV64E;
+  EXPECT_EQ(InfoRV64E.getExtensions().size(), 1UL);
+  EXPECT_TRUE(InfoRV64E.getExtensions().at("e") ==
+              (RISCVExtensionInfo{"e", 2, 0}));
+  EXPECT_EQ(InfoRV64I.getXLen(), 64U);
+}
+
+TEST(ParseNormalizedArchString, AcceptsArbitraryExtensionsAndVersions) {
+  auto MaybeISAInfo = RISCVISAInfo::parseNormalizedArchString(
+      "rv64i5p1_m3p2_zmadeup11p12_sfoo2p0_xbar3p0");
+  ASSERT_THAT_EXPECTED(MaybeISAInfo, Succeeded());
+  RISCVISAInfo &Info = **MaybeISAInfo;
+  EXPECT_EQ(Info.getExtensions().size(), 5UL);
+  EXPECT_TRUE(Info.getExtensions().at("i") == (RISCVExtensionInfo{"i", 5, 1}));
+  EXPECT_TRUE(Info.getExtensions().at("m") == (RISCVExtensionInfo{"m", 3, 2}));
+  EXPECT_TRUE(Info.getExtensions().at("zmadeup") ==
+              (RISCVExtensionInfo{"zmadeup", 11, 12}));
+  EXPECT_TRUE(Info.getExtensions().at("sfoo") ==
+              (RISCVExtensionInfo{"sfoo", 2, 0}));
+  EXPECT_TRUE(Info.getExtensions().at("xbar") ==
+              (RISCVExtensionInfo{"xbar", 3, 0}));
+}
+
+TEST(ParseNormalizedArchString, UpdatesFLenMinVLenMaxELen) {
+  auto MaybeISAInfo = RISCVISAInfo::parseNormalizedArchString(
+      "rv64i2p0_d2p0_zvl64b1p0_zve64d1p0");
+  ASSERT_THAT_EXPECTED(MaybeISAInfo, Succeeded());
+  RISCVISAInfo &Info = **MaybeISAInfo;
+  EXPECT_EQ(Info.getXLen(), 64U);
+  EXPECT_EQ(Info.getFLen(), 64U);
+  EXPECT_EQ(Info.getMinVLen(), 64U);
+  EXPECT_EQ(Info.getMaxELen(), 64U);
+}
