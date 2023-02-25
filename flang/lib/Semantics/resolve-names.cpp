bce8b57fc92f10511c69a8c9823bd12a3718cb3d
Peter Klausler <pklausler@nvidia.com>
2023-02-25 06:46:32 -0800

    [flang] Fix CONTIGUOUS attribute for construct entities
    Currently, the selector of a construct entity (e.g., ASSOCIATE(x=>a(1:20)))
    is inheriting the CONTIGUOUS attribute from its associated variable
    even if it has subscripts that make it noncontiguous (a(1:20:2)).
    Add construct entities to the dynamic contiguity predicate instead.
    
    Differential Revision: https://reviews.llvm.org/D145114

--- flang/lib/Semantics/resolve-names.cpp
+++ flang/lib/Semantics/resolve-names.cpp
@@ -6655,8 +6655,8 @@ void ConstructVisitor::SetTypeFromAssociation(Symbol &symbol) {
 // If current selector is a variable, set some of its attributes on symbol.
 void ConstructVisitor::SetAttrsFromAssociation(Symbol &symbol) {
   Attrs attrs{evaluate::GetAttrs(GetCurrentAssociation().selector.expr)};
-  symbol.attrs() |= attrs &
-      Attrs{Attr::TARGET, Attr::ASYNCHRONOUS, Attr::VOLATILE, Attr::CONTIGUOUS};
+  symbol.attrs() |=
+      attrs & Attrs{Attr::TARGET, Attr::ASYNCHRONOUS, Attr::VOLATILE};
   if (attrs.test(Attr::POINTER)) {
     SetImplicitAttr(symbol, Attr::TARGET);
   }
