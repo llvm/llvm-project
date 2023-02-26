fb28196a642a12599c1251b9d1587b398a479fca
Maksim Panchenko <maks@fb.com>
2023-02-25 18:23:53 -0800

    [BOLT] Fix intermittent crash with instrumentation
    When createInstrumentedIndirectCall() was invoked for tail calls, we
    attached annotation instruction twice to the new call instruction.
    First in createDirectCall(), and then again while copying over the
    metadata operands.
    
    As a result, the annotations were not properly stripped for such calls
    before the call to freeAnnotations() in LowerAnnotations pass. That lead
    to use-after-free while restoring the offsets with setOffset() call.
    
    Reviewed By: yota9
    
    Differential Revision: https://reviews.llvm.org/D144806

--- bolt/lib/Passes/Instrumentation.cpp
+++ bolt/lib/Passes/Instrumentation.cpp
@@ -215,7 +215,7 @@ void Instrumentation::instrumentIndirectTarget(BinaryBasicBlock &BB,
   BinaryContext &BC = FromFunction.getBinaryContext();
   bool IsTailCall = BC.MIB->isTailCall(*Iter);
   InstructionListType CounterInstrs = BC.MIB->createInstrumentedIndirectCall(
-      *Iter, IsTailCall,
+      std::move(*Iter),
       IsTailCall ? IndTailCallHandlerExitBBFunction->getSymbol()
                  : IndCallHandlerExitBBFunction->getSymbol(),
       IndCallSiteID, &*BC.Ctx);
