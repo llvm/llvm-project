2e7311170201e07e5e067e397aed8fa482d2fb8b
Craig Topper <craig.topper@sifive.com>
2023-02-27 09:19:27 -0800

    [Sema] Use isSVESizelessBuiltinType instead of isSizelessBuiltinType to prevent crashing on RISC-V.
    These 2 spots are protecting calls to SVE specific functions. If RISC-V
    sizeless types end up in there we trigger assertions.
    
    Use the more specific isSVESizelessBuiltinType() to avoid letting
    RISC-V vectors through.
    
    Reviewed By: asb, c-rhodes
    
    Differential Revision: https://reviews.llvm.org/D144772

--- clang/lib/Sema/SemaExpr.cpp
+++ clang/lib/Sema/SemaExpr.cpp
@@ -9871,8 +9871,8 @@ Sema::CheckAssignmentConstraints(QualType LHSType, ExprResult &RHS,
     }
 
     // Allow assignments between fixed-length and sizeless SVE vectors.
-    if ((LHSType->isSizelessBuiltinType() && RHSType->isVectorType()) ||
-        (LHSType->isVectorType() && RHSType->isSizelessBuiltinType()))
+    if ((LHSType->isSVESizelessBuiltinType() && RHSType->isVectorType()) ||
+        (LHSType->isVectorType() && RHSType->isSVESizelessBuiltinType()))
       if (Context.areCompatibleSveTypes(LHSType, RHSType) ||
           Context.areLaxCompatibleSveTypes(LHSType, RHSType)) {
         Kind = CK_BitCast;
