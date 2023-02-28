fce183b26e32eb3c5493365f4e941d6260d093ba
Dan Liew <dan@su-root.co.uk>
2023-02-27 17:05:19 -0800

    [LLDB] Expose several methods in SBWatchpoint
    This patch adds the following methods:
    
    * `GetType()`
    * `GetWatchValueKind()`
    * `GetWatchSpec()`
    * `IsWatchingReads()`
    * `IsWatchingWrites()`
    
    These mostly expose methods that `lldb_private::Watchpoint` already
    had. Tests are included that exercise these new methods.
    
    The motivation for exposing these are as follows:
    
    * `GetType()` - With this information and the address from a watchpoint
      it is now possible to construct an SBValue from an SBWatchpoint.
      Previously this wasn't possible. The included test case illustrates
      doing this.
    * `GetWatchValueKind()` - This allows the caller to determine whether the
      watchpoint is a variable watchpoint or an expression watchpoint. A new
      enum (`WatchpointValueKind`) has been introduced to represent the
      return values. Unfortunately the name `WatchpointKind` was already
      taken.
    * `GetWatchSpec()` - This allows (at least for variable watchpoints)
      to use a sensible name for SBValues created from an SBWatchpoint.
    * `IsWatchingReads()` - This allow checking if a watchpoint is
      monitoring read accesses.
    * `IsWatchingWRites()` - This allow checking if a watchpoint is
      monitoring write accesses.
    
    rdar://105606978
    
    Reviewers: jingham, mib, bulbazord, jasonmolenda, JDevlieghere
    
    Differential Revision: https://reviews.llvm.org/D144937

--- lldb/source/API/SBWatchpoint.cpp
+++ lldb/source/API/SBWatchpoint.cpp
@@ -17,6 +17,7 @@
 #include "lldb/Breakpoint/Watchpoint.h"
 #include "lldb/Breakpoint/WatchpointList.h"
 #include "lldb/Core/StreamFile.h"
+#include "lldb/Symbol/CompilerType.h"
 #include "lldb/Target/Process.h"
 #include "lldb/Target/Target.h"
 #include "lldb/Utility/Stream.h"
@@ -289,4 +290,73 @@ SBWatchpoint SBWatchpoint::GetWatchpointFromEvent(const lldb::SBEvent &event) {
     sb_watchpoint =
         Watchpoint::WatchpointEventData::GetWatchpointFromEvent(event.GetSP());
   return sb_watchpoint;
+}
+
+lldb::SBType SBWatchpoint::GetType() {
+  LLDB_INSTRUMENT_VA(this);
+
+  lldb::WatchpointSP watchpoint_sp(GetSP());
+  if (watchpoint_sp) {
+    std::lock_guard<std::recursive_mutex> guard(
+        watchpoint_sp->GetTarget().GetAPIMutex());
+    const CompilerType &type = watchpoint_sp->GetCompilerType();
+    return lldb::SBType(type);
+  }
+  return lldb::SBType();
+}
+
+WatchpointValueKind SBWatchpoint::GetWatchValueKind() {
+  LLDB_INSTRUMENT_VA(this);
+
+  lldb::WatchpointSP watchpoint_sp(GetSP());
+  if (watchpoint_sp) {
+    std::lock_guard<std::recursive_mutex> guard(
+        watchpoint_sp->GetTarget().GetAPIMutex());
+    if (watchpoint_sp->IsWatchVariable())
+      return WatchpointValueKind::eWatchPointValueKindVariable;
+    return WatchpointValueKind::eWatchPointValueKindExpression;
+  }
+  return WatchpointValueKind::eWatchPointValueKindInvalid;
+}
+
+const char *SBWatchpoint::GetWatchSpec() {
+  LLDB_INSTRUMENT_VA(this);
+
+  lldb::WatchpointSP watchpoint_sp(GetSP());
+  if (watchpoint_sp) {
+    std::lock_guard<std::recursive_mutex> guard(
+        watchpoint_sp->GetTarget().GetAPIMutex());
+    // Store the result of `GetWatchSpec()` as a ConstString
+    // so that the C string we return has a sufficiently long
+    // lifetime. Note this a memory leak but should be fairly
+    // low impact.
+    return ConstString(watchpoint_sp->GetWatchSpec()).AsCString();
+  }
+  return nullptr;
+}
+
+bool SBWatchpoint::IsWatchingReads() {
+  LLDB_INSTRUMENT_VA(this);
+  lldb::WatchpointSP watchpoint_sp(GetSP());
+  if (watchpoint_sp) {
+    std::lock_guard<std::recursive_mutex> guard(
+        watchpoint_sp->GetTarget().GetAPIMutex());
+
+    return watchpoint_sp->WatchpointRead();
+  }
+
+  return false;
+}
+
+bool SBWatchpoint::IsWatchingWrites() {
+  LLDB_INSTRUMENT_VA(this);
+  lldb::WatchpointSP watchpoint_sp(GetSP());
+  if (watchpoint_sp) {
+    std::lock_guard<std::recursive_mutex> guard(
+        watchpoint_sp->GetTarget().GetAPIMutex());
+
+    return watchpoint_sp->WatchpointWrite();
+  }
+
+  return false;
 }
