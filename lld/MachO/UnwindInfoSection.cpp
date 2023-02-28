09aaf53a05e3786eea374f3ce57574225036412d
Vy Nguyen <vyng@google.com>
2023-02-28 15:17:44 -0500

    [RFC][MC][MachO]Only emits compact-unwind format for "canonical" personality symbols. For the rest, use DWARFs.
    Details: https://github.com/rust-lang/rust/issues/102754
    
    The MachO format uses 2 bits to encode these personality funtions, with 0 reserved for "no-personality".
    This means we can only have up to 3 personality. There are already three popular personalities:  __gxx_personality_v0, __gcc_personality_v0, and __objc_personality_v0.
    As a result, any system that needs custom-personality will run into a problem.
    
    This patch implemented jyknight's proposal to simply force DWARFs for all non-canonical personality functions.
    
    Differential Revision: https://reviews.llvm.org/D144999

--- lld/MachO/UnwindInfoSection.cpp
+++ lld/MachO/UnwindInfoSection.cpp
@@ -343,7 +343,8 @@ void UnwindInfoSectionImpl::relocateCompactUnwind(
     if (!d->unwindEntry)
       return;
 
-    // If we have DWARF unwind info, create a CU entry that points to it.
+    // If we have DWARF unwind info, create a slimmed-down CU entry that points
+    // to it.
     if (d->unwindEntry->getName() == section_names::ehFrame) {
       // The unwinder will look for the DWARF entry starting at the hint,
       // assuming the hint points to a valid CFI record start. If it
@@ -360,7 +361,9 @@ void UnwindInfoSectionImpl::relocateCompactUnwind(
       cu.encoding = target->modeDwarfEncoding | dwarfOffsetHint;
       const FDE &fde = cast<ObjFile>(d->getFile())->fdes[d->unwindEntry];
       cu.functionLength = fde.funcLength;
-      cu.personality = fde.personality;
+      // Omit the DWARF personality from compact-unwind entry so that we
+      // don't need to encode it.
+      cu.personality = nullptr;
       cu.lsda = fde.lsda;
       return;
     }
