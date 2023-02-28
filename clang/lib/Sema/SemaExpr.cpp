abafc869cb656a81ac35a4002627045a6fb37ff2
Craig Topper <craig.topper@sifive.com>
2023-02-27 17:39:50 -0800

    [AArch64] Use isSVESizelessBuiltinType instead of isSizelessBuiltinType in SVE specific code.
    isSizelessBuiltinType includes RISC-V vector and WebAssembly reference
    types. This code is not applicable to those types.

--- clang/lib/Sema/SemaExpr.cpp
+++ clang/lib/Sema/SemaExpr.cpp
@@ -7937,7 +7937,7 @@ bool Sema::isValidSveBitcast(QualType srcTy, QualType destTy) {
   assert(srcTy->isVectorType() || destTy->isVectorType());
 
   auto ValidScalableConversion = [](QualType FirstType, QualType SecondType) {
-    if (!FirstType->isSizelessBuiltinType())
+    if (!FirstType->isSVESizelessBuiltinType())
       return false;
 
     const auto *VecTy = SecondType->getAs<VectorType>();
