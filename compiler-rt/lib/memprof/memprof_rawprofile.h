287177a47a396ca6cc0bef7696108cdaa0c68e5f
Snehasish Kumar <snehasishk@google.com>
2023-02-28 21:33:30 +0000

    [memprof] Record BuildIDs in the raw profile.
    This patch adds support for recording BuildIds usng the sanitizer
    ListOfModules API. We add another entry to the SegmentEntry struct and
    change the memprof raw version.
    
    Reviewed By: tejohnson
    
    Differential Revision: https://reviews.llvm.org/D145190

--- compiler-rt/lib/memprof/memprof_rawprofile.h
+++ compiler-rt/lib/memprof/memprof_rawprofile.h
@@ -2,13 +2,13 @@
 #define MEMPROF_RAWPROFILE_H_
 
 #include "memprof_mibmap.h"
-#include "sanitizer_common/sanitizer_procmaps.h"
+#include "sanitizer_common/sanitizer_common.h"
 
 namespace __memprof {
 // Serialize the in-memory representation of the memprof profile to the raw
 // binary format. The format itself is documented memprof_rawprofile.cpp.
-u64 SerializeToRawProfile(MIBMapTy &BlockCache, MemoryMappingLayoutBase &Layout,
-                          char *&Buffer);
+u64 SerializeToRawProfile(MIBMapTy &BlockCache,
+                          __sanitizer::ListOfModules &Modules, char *&Buffer);
 } // namespace __memprof
 
 #endif // MEMPROF_RAWPROFILE_H_
