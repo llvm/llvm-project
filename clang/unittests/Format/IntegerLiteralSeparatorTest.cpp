f8d10d5ac9ab4b45b388c74357fc82fb96562e66
Manuel Klimek <klimek@google.com>
2023-03-01 12:33:19 +0000

    [clang-format][NFC] Refactor formatting unit tests.
    Pull out common base class for formatting unit tests, removing duplicate
    code that accumulated over the years.
    
    Pull out macro expansion test into its own test file.

--- clang/unittests/Format/IntegerLiteralSeparatorTest.cpp
+++ clang/unittests/Format/IntegerLiteralSeparatorTest.cpp
@@ -6,66 +6,16 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "clang/Format/Format.h"
-
-#include "../Tooling/ReplacementTest.h"
-#include "FormatTestUtils.h"
+#include "FormatTestBase.h"
 
 #define DEBUG_TYPE "integer-literal-separator-test"
 
 namespace clang {
 namespace format {
+namespace test {
 namespace {
 
-// TODO:
-// Refactor the class declaration, which is copied from BracesInserterTest.cpp.
-class IntegerLiteralSeparatorTest : public ::testing::Test {
-protected:
-  std::string format(llvm::StringRef Code, const FormatStyle &Style,
-                     const std::vector<tooling::Range> &Ranges) {
-    LLVM_DEBUG(llvm::errs() << "---\n");
-    LLVM_DEBUG(llvm::errs() << Code << "\n\n");
-    auto NonEmptyRanges = Ranges;
-    if (Ranges.empty())
-      NonEmptyRanges = {1, tooling::Range(0, Code.size())};
-    FormattingAttemptStatus Status;
-    tooling::Replacements Replaces =
-        reformat(Style, Code, NonEmptyRanges, "<stdin>", &Status);
-    EXPECT_EQ(true, Status.FormatComplete) << Code << "\n\n";
-    ReplacementCount = Replaces.size();
-    auto Result = applyAllReplacements(Code, Replaces);
-    EXPECT_TRUE(static_cast<bool>(Result));
-    LLVM_DEBUG(llvm::errs() << "\n" << *Result << "\n\n");
-    return *Result;
-  }
-
-  void _verifyFormat(const char *File, int Line, llvm::StringRef Expected,
-                     llvm::StringRef Code,
-                     const FormatStyle &Style = getLLVMStyle(),
-                     const std::vector<tooling::Range> &Ranges = {}) {
-    testing::ScopedTrace t(File, Line, ::testing::Message() << Code.str());
-    EXPECT_EQ(Expected.str(), format(Expected, Style, Ranges))
-        << "Expected code is not stable";
-    EXPECT_EQ(Expected.str(), format(Code, Style, Ranges));
-    if (Style.Language == FormatStyle::LK_Cpp && Ranges.empty()) {
-      // Objective-C++ is a superset of C++, so everything checked for C++
-      // needs to be checked for Objective-C++ as well.
-      FormatStyle ObjCStyle = Style;
-      ObjCStyle.Language = FormatStyle::LK_ObjC;
-      EXPECT_EQ(Expected.str(), format(test::messUp(Code), ObjCStyle, Ranges));
-    }
-  }
-
-  void _verifyFormat(const char *File, int Line, llvm::StringRef Code,
-                     const FormatStyle &Style = getLLVMStyle(),
-                     const std::vector<tooling::Range> &Ranges = {}) {
-    _verifyFormat(File, Line, Code, Code, Style, Ranges);
-  }
-
-  int ReplacementCount;
-};
-
-#define verifyFormat(...) _verifyFormat(__FILE__, __LINE__, __VA_ARGS__)
+class IntegerLiteralSeparatorTest : public FormatTestBase {};
 
 TEST_F(IntegerLiteralSeparatorTest, SingleQuoteAsSeparator) {
   FormatStyle Style = getLLVMStyle();
@@ -176,7 +126,7 @@ TEST_F(IntegerLiteralSeparatorTest, FixRanges) {
                "k = +1'23'4;",
                Code, Style, {tooling::Range(0, 11)}); // line 1
 
-  verifyFormat(Code, Style, {tooling::Range(32, 10)}); // line 3
+  verifyFormat(Code, Code, Style, {tooling::Range(32, 10)}); // line 3
 
   verifyFormat("i = -12'34;\n"
                "// clang-format off\n"
@@ -224,5 +174,6 @@ TEST_F(IntegerLiteralSeparatorTest, FloatingPoint) {
 }
 
 } // namespace
+} // namespace test
 } // namespace format
 } // namespace clang
