4d513bd9a542272eb1269f9971f83562b73c984f
Nikita Popov <npopov@redhat.com>
2023-03-01 09:51:29 +0100

    [Orc] Remove LLVMInitializeCore() calls from examples
    Per discussion on D144970, these are no longer necessary.

--- llvm/examples/OrcV2Examples/OrcV2CBindingsMCJITLikeMemoryManager/OrcV2CBindingsMCJITLikeMemoryManager.c
+++ llvm/examples/OrcV2Examples/OrcV2CBindingsMCJITLikeMemoryManager/OrcV2CBindingsMCJITLikeMemoryManager.c
@@ -14,7 +14,6 @@
 
 #include "llvm-c/Core.h"
 #include "llvm-c/Error.h"
-#include "llvm-c/Initialization.h"
 #include "llvm-c/LLJIT.h"
 #include "llvm-c/OrcEE.h"
 #include "llvm-c/Support.h"
@@ -201,7 +200,6 @@ int main(int argc, char *argv[]) {
 
   // Parse command line arguments and initialize LLVM Core.
   LLVMParseCommandLineOptions(argc, (const char **)argv, "");
-  LLVMInitializeCore(LLVMGetGlobalPassRegistry());
 
   // Initialize native target codegen and asm printer.
   LLVMInitializeNativeTarget();
