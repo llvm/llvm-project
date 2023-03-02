23d28ba0bbc2852705c31c001f303b850e185b77
Paul Walker <paul.walker@arm.com>
2023-03-02 11:59:50 +0000

    Revert "[SCEV] Add SCEVType to represent `vscale`."
    This reverts commit 1fb19862c86b79e7550b6247949afb3a29509943.

--- llvm/include/llvm/Transforms/Utils/ScalarEvolutionExpander.h
+++ llvm/include/llvm/Transforms/Utils/ScalarEvolutionExpander.h
@@ -457,8 +457,6 @@ private:
 
   Value *visitConstant(const SCEVConstant *S) { return S->getValue(); }
 
-  Value *visitVScale(const SCEVVScale *S);
-
   Value *visitPtrToIntExpr(const SCEVPtrToIntExpr *S);
 
   Value *visitTruncateExpr(const SCEVTruncateExpr *S);
