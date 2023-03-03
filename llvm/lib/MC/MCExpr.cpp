442e69277244928e52d080de4e41d2e317f81bb9
Eli Friedman <efriedma@quicinc.com>
2023-03-02 18:09:14 -0800

    [COFF] Add MC support for emitting IMAGE_WEAK_EXTERN_ANTI_DEPENDENCY symbols
    This is mostly useful for ARM64EC, which uses such symbols extensively.
    
    One interesting quirk of ARM64EC is that we need to be able to emit weak
    symbols that point at each other (so if either symbol is defined
    elsewhere, both symbols point at the definition).  This required a few
    changes to the way we handle weak symbols on Windows.
    
    Differential Revision: https://reviews.llvm.org/D145208

--- llvm/lib/MC/MCExpr.cpp
+++ llvm/lib/MC/MCExpr.cpp
@@ -761,6 +761,9 @@ bool MCExpr::evaluateAsValue(MCValue &Res, const MCAsmLayout &Layout) const {
 }
 
 static bool canExpand(const MCSymbol &Sym, bool InSet) {
+  if (Sym.isWeakExternal())
+    return false;
+
   const MCExpr *Expr = Sym.getVariableValue();
   const auto *Inner = dyn_cast<MCSymbolRefExpr>(Expr);
   if (Inner) {
@@ -997,6 +1000,8 @@ MCFragment *MCExpr::findAssociatedFragment() const {
   case SymbolRef: {
     const MCSymbolRefExpr *SRE = cast<MCSymbolRefExpr>(this);
     const MCSymbol &Sym = SRE->getSymbol();
+    if (Sym.isWeakExternal())
+      return nullptr;
     return Sym.getFragment();
   }
 
