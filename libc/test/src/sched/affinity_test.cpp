52b9fbcf66dce2d4b94e2917d7abd3eedd955a38
Siva Chandra Reddy <sivachandra@google.com>
2023-03-03 07:09:58 +0000

    [libc][NFC] Switch dirent, fcntl, inttypes and sched to use libc_errno.
    Reviewed By: michaelrj
    
    Differential Revision: https://reviews.llvm.org/D145222

--- libc/test/src/sched/affinity_test.cpp
+++ libc/test/src/sched/affinity_test.cpp
@@ -7,17 +7,17 @@
 //===----------------------------------------------------------------------===//
 
 #include "src/__support/OSUtil/syscall.h"
+#include "src/errno/libc_errno.h"
 #include "src/sched/sched_getaffinity.h"
 #include "src/sched/sched_setaffinity.h"
 #include "test/ErrnoSetterMatcher.h"
 
-#include <errno.h>
 #include <sched.h>
 #include <sys/syscall.h>
 
 TEST(LlvmLibcSchedAffinityTest, SmokeTest) {
   cpu_set_t mask;
-  errno = 0;
+  libc_errno = 0;
   using __llvm_libc::testing::ErrnoSetterMatcher::Succeeds;
   pid_t tid = __llvm_libc::syscall_impl(SYS_gettid);
   ASSERT_GT(tid, pid_t(0));
@@ -32,13 +32,13 @@ TEST(LlvmLibcSchedAffinityTest, BadMask) {
   using __llvm_libc::testing::ErrnoSetterMatcher::Fails;
   pid_t tid = __llvm_libc::syscall_impl(SYS_gettid);
 
-  errno = 0;
+  libc_errno = 0;
   ASSERT_THAT(__llvm_libc::sched_getaffinity(tid, sizeof(cpu_set_t), nullptr),
               Fails(EFAULT));
 
-  errno = 0;
+  libc_errno = 0;
   ASSERT_THAT(__llvm_libc::sched_setaffinity(tid, sizeof(cpu_set_t), nullptr),
               Fails(EFAULT));
 
-  errno = 0;
+  libc_errno = 0;
 }
