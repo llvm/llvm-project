7dc9f39a10b0def462aff509ec113b5a17af98c6
Dave Lee <davelee.com@gmail.com>
2023-03-04 17:14:50 -0800

    [lldb] Remove unused portion of GetFunctionMethodInfo signature (NFC)
    This applies to IsClassMethod as well.

--- lldb/include/lldb/Symbol/TypeSystem.h
+++ lldb/include/lldb/Symbol/TypeSystem.h
@@ -127,9 +127,9 @@ public:
   virtual ConstString
   DeclContextGetScopeQualifiedName(void *opaque_decl_ctx) = 0;
 
-  virtual bool DeclContextIsClassMethod(
-      void *opaque_decl_ctx, lldb::LanguageType *language_ptr,
-      bool *is_instance_method_ptr, ConstString *language_object_name_ptr) = 0;
+  virtual bool
+  DeclContextIsClassMethod(void *opaque_decl_ctx,
+                           ConstString *language_object_name_ptr) = 0;
 
   virtual bool DeclContextIsContainedInLookup(void *opaque_decl_ctx,
                                               void *other_opaque_decl_ctx) = 0;
