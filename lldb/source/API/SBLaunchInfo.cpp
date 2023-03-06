601583e5a3083b87b48bc6747c12dc0027b09481
Med Ismail Bennani <medismail.bennani@gmail.com>
2023-03-06 11:53:09 -0800

    [lldb/Utility] Fix layering violation caused by ScriptedMetadata
    This patch moves `ScriptedMetadata.h` from the `Interpreter` directory to
    the `Utility` sub-directory since `ProcessInfo.h` depends on it.
    
    It also gets rid of the unused `OptionGroupPythonClassWithDict`
    constructor for `ScriptedMetadata` which would address the layering
    violation.
    
    Signed-off-by: Med Ismail Bennani <medismail.bennani@gmail.com>

--- lldb/source/API/SBLaunchInfo.cpp
+++ lldb/source/API/SBLaunchInfo.cpp
@@ -17,7 +17,7 @@
 #include "lldb/API/SBStructuredData.h"
 #include "lldb/Core/StructuredDataImpl.h"
 #include "lldb/Host/ProcessLaunchInfo.h"
-#include "lldb/Interpreter/ScriptedMetadata.h"
+#include "lldb/Utility/ScriptedMetadata.h"
 
 using namespace lldb;
 using namespace lldb_private;
