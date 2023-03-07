e4edee0b03b494e11b3eae0fe7d5cb4ec2503618
Jacob Lambert <jacob.lambert@amd.com>
2023-03-07 11:03:43 -0800

    Include more device-lib code in device-lib tests
    Instead of linking generic opencl files in the device lib tests,
    we add a new source file with specific device lib calls
    
    Change-Id: I77a6b5da267841488a1ba6ee8d16748713e89efc

--- amd/comgr/test/compile_device_libs_test.c
+++ amd/comgr/test/compile_device_libs_test.c
@@ -40,64 +40,44 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-  char *BufSource1, *BufSource2, *BufInclude;
-  size_t SizeSource1, SizeSource2, SizeInclude;
-  amd_comgr_data_t DataSource1, DataSource2, DataInclude;
+  char *BufSource;
+  size_t SizeSource;
+  amd_comgr_data_t DataSource;
   amd_comgr_data_set_t DataSetIn, DataSetPch, DataSetBc, DataSetDevLibs,
       DataSetLinked, DataSetAsm, DataSetReloc, DataSetExec;
   amd_comgr_action_info_t DataAction;
   amd_comgr_status_t Status;
-  const char *CodeGenOptions[] = {"-mllvm", "-amdgpu-early-inline-all", "-mcode-object-version=5"};
+  const char *CodeGenOptions[] = {"-mllvm", "-amdgpu-early-inline-all", "-mcode-object-version=4"};
   size_t CodeGenOptionsCount =
       sizeof(CodeGenOptions) / sizeof(CodeGenOptions[0]);
-  const char *DevLibsOptions[] = {"finite_only", "unsafe_math", "code_object_v5"};
+  const char *DevLibsOptions[] = {"unsafe_math", "code_object_v4"};
   size_t DevLibsOptionsCount =
       sizeof(DevLibsOptions) / sizeof(DevLibsOptions[0]);
 
-  SizeSource1 = setBuf(TEST_OBJ_DIR "/source1.cl", &BufSource1);
-  SizeSource2 = setBuf(TEST_OBJ_DIR "/source2.cl", &BufSource2);
-  SizeInclude = setBuf(TEST_OBJ_DIR "/include-a.h", &BufInclude);
+  SizeSource = setBuf(TEST_OBJ_DIR "/device_libs.cl", &BufSource);
 
   Status = amd_comgr_create_data_set(&DataSetIn);
   checkError(Status, "amd_comgr_create_data_set");
 
-  Status = amd_comgr_create_data(AMD_COMGR_DATA_KIND_SOURCE, &DataSource1);
+  Status = amd_comgr_create_data(AMD_COMGR_DATA_KIND_SOURCE, &DataSource);
   checkError(Status, "amd_comgr_create_data");
-  Status = amd_comgr_set_data(DataSource1, SizeSource1, BufSource1);
+  Status = amd_comgr_set_data(DataSource, SizeSource, BufSource);
   checkError(Status, "amd_comgr_set_data");
-  Status = amd_comgr_set_data_name(DataSource1, "source1.cl");
+  Status = amd_comgr_set_data_name(DataSource, "device_libs.cl");
   checkError(Status, "amd_comgr_set_data_name");
-  Status = amd_comgr_data_set_add(DataSetIn, DataSource1);
-  checkError(Status, "amd_comgr_data_set_add");
-
-  Status = amd_comgr_create_data(AMD_COMGR_DATA_KIND_SOURCE, &DataSource2);
-  checkError(Status, "amd_comgr_create_data");
-  Status = amd_comgr_set_data(DataSource2, SizeSource2, BufSource2);
-  checkError(Status, "amd_comgr_set_data");
-  Status = amd_comgr_set_data_name(DataSource2, "source2.cl");
-  checkError(Status, "amd_comgr_set_data_name");
-  Status = amd_comgr_data_set_add(DataSetIn, DataSource2);
-  checkError(Status, "amd_comgr_data_set_add");
-
-  Status = amd_comgr_create_data(AMD_COMGR_DATA_KIND_INCLUDE, &DataInclude);
-  checkError(Status, "amd_comgr_create_data");
-  Status = amd_comgr_set_data(DataInclude, SizeInclude, BufInclude);
-  checkError(Status, "amd_comgr_set_data");
-  Status = amd_comgr_set_data_name(DataInclude, "include-a.h");
-  checkError(Status, "amd_comgr_set_data_name");
-  Status = amd_comgr_data_set_add(DataSetIn, DataInclude);
+  Status = amd_comgr_data_set_add(DataSetIn, DataSource);
   checkError(Status, "amd_comgr_data_set_add");
 
   Status = amd_comgr_create_action_info(&DataAction);
   checkError(Status, "amd_comgr_create_action_info");
   Status = amd_comgr_action_info_set_language(DataAction,
-                                              AMD_COMGR_LANGUAGE_OPENCL_1_2);
+      AMD_COMGR_LANGUAGE_OPENCL_1_2);
   checkError(Status, "amd_comgr_action_info_set_language");
   Status = amd_comgr_action_info_set_isa_name(DataAction,
-                                              "amdgcn-amd-amdhsa--gfx803");
+      "amdgcn-amd-amdhsa--gfx900");
   checkError(Status, "amd_comgr_action_info_set_isa_name");
   Status = amd_comgr_action_info_set_option_list(DataAction, CodeGenOptions,
-                                                 CodeGenOptionsCount);
+      CodeGenOptionsCount);
   checkError(Status, "amd_comgr_action_info_set_option_list");
 
   Status = amd_comgr_create_data_set(&DataSetPch);
@@ -130,9 +110,9 @@ int main(int argc, char *argv[]) {
       amd_comgr_action_data_count(DataSetBc, AMD_COMGR_DATA_KIND_BC, &Count);
   checkError(Status, "amd_comgr_action_data_count");
 
-  if (Count != 2) {
+  if (Count != 1) {
     printf("AMD_COMGR_ACTION_COMPILE_SOURCE_TO_BC Failed: "
-           "produced %zu BC objects (expected 2)\n",
+           "produced %zu BC objects (expected 1)\n",
            Count);
     exit(1);
   }
@@ -152,9 +132,9 @@ int main(int argc, char *argv[]) {
                                        &Count);
   checkError(Status, "amd_comgr_action_data_count");
 
-  // Our 2 sources, plus 3 language libraries, plus 2 device libraries, plus 5
+  // Our 1 source, plus 3 language libraries, plus 2 device libraries, plus 5
   // features libraries.
-  if (Count != 12) {
+  if (Count != 11) {
     printf("AMD_COMGR_ACTION_ADD_DEVICE_LIBRARIES Failed: "
            "produced %zu BC objects (expected 12)\n",
            Count);
@@ -240,11 +220,7 @@ int main(int argc, char *argv[]) {
     exit(1);
   }
 
-  Status = amd_comgr_release_data(DataSource1);
-  checkError(Status, "amd_comgr_release_data");
-  Status = amd_comgr_release_data(DataSource2);
-  checkError(Status, "amd_comgr_release_data");
-  Status = amd_comgr_release_data(DataInclude);
+  Status = amd_comgr_release_data(DataSource);
   checkError(Status, "amd_comgr_release_data");
   Status = amd_comgr_destroy_data_set(DataSetIn);
   checkError(Status, "amd_comgr_destroy_data_set");
@@ -264,7 +240,5 @@ int main(int argc, char *argv[]) {
   checkError(Status, "amd_comgr_destroy_data_set");
   Status = amd_comgr_destroy_action_info(DataAction);
   checkError(Status, "amd_comgr_destroy_action_info");
-  free(BufSource1);
-  free(BufSource2);
-  free(BufInclude);
+  free(BufSource);
 }
--- amd/comgr/test/compile_source_with_device_libs_to_bc_test.c
+++ amd/comgr/test/compile_source_with_device_libs_to_bc_test.c
@@ -40,49 +40,30 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-  char *BufSource1, *BufSource2, *BufInclude;
-  size_t SizeSource1, SizeSource2, SizeInclude;
-  amd_comgr_data_t DataSource1, DataSource2, DataInclude;
+  char *BufSource;
+  size_t SizeSource;
+  amd_comgr_data_t DataSource;
   amd_comgr_data_set_t DataSetIn, DataSetPch, DataSetBc, DataSetLinked,
       DataSetAsm, DataSetReloc, DataSetExec;
   amd_comgr_action_info_t DataAction;
   amd_comgr_status_t Status;
-  const char *CodeGenOptions[] = {"-mllvm", "-amdgpu-early-inline-all"};
+  const char *CodeGenOptions[] = {"-mllvm", "-amdgpu-early-inline-all",
+    "-mcode-object-version=5"};
   size_t CodeGenOptionsCount =
       sizeof(CodeGenOptions) / sizeof(CodeGenOptions[0]);
 
-  SizeSource1 = setBuf(TEST_OBJ_DIR "/source1.cl", &BufSource1);
-  SizeSource2 = setBuf(TEST_OBJ_DIR "/source2.cl", &BufSource2);
-  SizeInclude = setBuf(TEST_OBJ_DIR "/include-a.h", &BufInclude);
+  SizeSource = setBuf(TEST_OBJ_DIR "/device_libs.cl", &BufSource);
 
   Status = amd_comgr_create_data_set(&DataSetIn);
   checkError(Status, "amd_comgr_create_data_set");
 
-  Status = amd_comgr_create_data(AMD_COMGR_DATA_KIND_SOURCE, &DataSource1);
+  Status = amd_comgr_create_data(AMD_COMGR_DATA_KIND_SOURCE, &DataSource);
   checkError(Status, "amd_comgr_create_data");
-  Status = amd_comgr_set_data(DataSource1, SizeSource1, BufSource1);
+  Status = amd_comgr_set_data(DataSource, SizeSource, BufSource);
   checkError(Status, "amd_comgr_set_data");
-  Status = amd_comgr_set_data_name(DataSource1, "source1.cl");
+  Status = amd_comgr_set_data_name(DataSource, "device_libs.cl");
   checkError(Status, "amd_comgr_set_data_name");
-  Status = amd_comgr_data_set_add(DataSetIn, DataSource1);
-  checkError(Status, "amd_comgr_data_set_add");
-
-  Status = amd_comgr_create_data(AMD_COMGR_DATA_KIND_SOURCE, &DataSource2);
-  checkError(Status, "amd_comgr_create_data");
-  Status = amd_comgr_set_data(DataSource2, SizeSource2, BufSource2);
-  checkError(Status, "amd_comgr_set_data");
-  Status = amd_comgr_set_data_name(DataSource2, "source2.cl");
-  checkError(Status, "amd_comgr_set_data_name");
-  Status = amd_comgr_data_set_add(DataSetIn, DataSource2);
-  checkError(Status, "amd_comgr_data_set_add");
-
-  Status = amd_comgr_create_data(AMD_COMGR_DATA_KIND_INCLUDE, &DataInclude);
-  checkError(Status, "amd_comgr_create_data");
-  Status = amd_comgr_set_data(DataInclude, SizeInclude, BufInclude);
-  checkError(Status, "amd_comgr_set_data");
-  Status = amd_comgr_set_data_name(DataInclude, "include-a.h");
-  checkError(Status, "amd_comgr_set_data_name");
-  Status = amd_comgr_data_set_add(DataSetIn, DataInclude);
+  Status = amd_comgr_data_set_add(DataSetIn, DataSource);
   checkError(Status, "amd_comgr_data_set_add");
 
   Status = amd_comgr_create_action_info(&DataAction);
@@ -91,7 +72,7 @@ int main(int argc, char *argv[]) {
                                               AMD_COMGR_LANGUAGE_OPENCL_1_2);
   checkError(Status, "amd_comgr_action_info_set_language");
   Status = amd_comgr_action_info_set_isa_name(DataAction,
-                                              "amdgcn-amd-amdhsa--gfx803");
+                                              "amdgcn-amd-amdhsa--gfx900");
   checkError(Status, "amd_comgr_action_info_set_isa_name");
 
   Status = amd_comgr_create_data_set(&DataSetPch);
@@ -129,9 +110,9 @@ int main(int argc, char *argv[]) {
       amd_comgr_action_data_count(DataSetBc, AMD_COMGR_DATA_KIND_BC, &Count);
   checkError(Status, "amd_comgr_action_data_count");
 
-  if (Count != 2) {
+  if (Count != 1) {
     printf("AMD_COMGR_ACTION_COMPILE_SOURCE_TO_BC Failed: "
-           "produced %zu BC objects (expected 2)\n",
+           "produced %zu BC objects (expected 1)\n",
            Count);
     exit(1);
   }
@@ -211,11 +192,7 @@ int main(int argc, char *argv[]) {
     exit(1);
   }
 
-  Status = amd_comgr_release_data(DataSource1);
-  checkError(Status, "amd_comgr_release_data");
-  Status = amd_comgr_release_data(DataSource2);
-  checkError(Status, "amd_comgr_release_data");
-  Status = amd_comgr_release_data(DataInclude);
+  Status = amd_comgr_release_data(DataSource);
   checkError(Status, "amd_comgr_release_data");
   Status = amd_comgr_destroy_data_set(DataSetIn);
   checkError(Status, "amd_comgr_destroy_data_set");
@@ -233,7 +210,5 @@ int main(int argc, char *argv[]) {
   checkError(Status, "amd_comgr_destroy_data_set");
   Status = amd_comgr_destroy_action_info(DataAction);
   checkError(Status, "amd_comgr_destroy_action_info");
-  free(BufSource1);
-  free(BufSource2);
-  free(BufInclude);
+  free(BufSource);
 }
