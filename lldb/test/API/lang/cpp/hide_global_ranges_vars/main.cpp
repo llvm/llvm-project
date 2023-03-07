8714d8c12b3ede06fbd34f009d903dba49c7bcec
Michael Buch <michaelbuch12@gmail.com>
2023-03-07 11:25:36 +0000

    Revert "[lldb] Ignore libcxx std::ranges global variables in frame var"
    Reverting because Xcode requires this to be handled elsewhere.
    The global variable list gets constructed using the SBAPI
    
    This reverts commit de10c1a824405833a0f49b22e7fa3f32a1393cc3.
    
    (cherry picked from commit b7e667bfd2a9bce0e3d5ca9fd6b97da553e03bd9)

--- lldb/test/API/lang/cpp/hide_global_ranges_vars/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <ranges>
-
-int main(int argc, char const *argv[]) {
-  int arr[3] = {1, 2, 3};
-  auto arr_view = std::ranges::views::all(arr);
-  auto arr_max = std::ranges::max_element(arr);
-
-  return 0;
-}
