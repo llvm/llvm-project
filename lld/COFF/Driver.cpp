af5f468228472ef2d4f58c2ac51842d4a6d91c9d
Saleem Abdulrasool <compnerd@compnerd.org>
2023-03-07 10:10:31 -0800

    MSVC: support version preference with search
    Extend the logic for the WinSDK and UCRT handling to prefer a user
    specified version of the VisualC++ tools and Windows SDK.  This allows
    us to now perform the regular search for the installation but select the
    exact version of the SDK or VC++ tools to override the latest version.
    Similar to the other flags controlling this behaviour, if the user
    specifies a value, we will not perform validation on the input and will
    attempt to prefer that, particularly in the case of VisualC++ tools
    where no fallback occurs.
    
    Reviewed by: hans
    Differential Revision: https://reviews.llvm.org/D145517

--- lld/COFF/Driver.cpp
+++ lld/COFF/Driver.cpp
@@ -559,7 +559,7 @@ void LinkerDriver::detectWinSysRoot(const opt::InputArgList &Args) {
                                      WinSysRoot, vcToolChainPath, vsLayout) &&
       (Args.hasArg(OPT_lldignoreenv) ||
        !findVCToolChainViaEnvironment(*VFS, vcToolChainPath, vsLayout)) &&
-      !findVCToolChainViaSetupConfig(*VFS, vcToolChainPath, vsLayout) &&
+      !findVCToolChainViaSetupConfig(*VFS, {}, vcToolChainPath, vsLayout) &&
       !findVCToolChainViaRegistry(vcToolChainPath, vsLayout))
     return;
 
