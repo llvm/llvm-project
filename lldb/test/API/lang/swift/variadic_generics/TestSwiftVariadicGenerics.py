a37e107aa6c07f15c9c8b26904b32b22d61c59ca
Adrian Prantl <aprantl@apple.com>
2023-03-07 17:50:44 -0800

    Add more tests for variadic generic functions

--- lldb/test/API/lang/swift/variadic_generics/TestSwiftVariadicGenerics.py
+++ lldb/test/API/lang/swift/variadic_generics/TestSwiftVariadicGenerics.py
@@ -11,9 +11,50 @@ class TestSwiftVariadicGenerics(TestBase):
     def test(self):
         self.build()
 
-        target,  _, _, _ = lldbutil.run_to_source_breakpoint(
+        target,  process, _, _ = lldbutil.run_to_source_breakpoint(
             self, "break here", lldb.SBFileSpec('a.swift'))
 
+        # f1(args: a, b)
         self.expect("frame variable",
                     substrs=["Pack{(a.A, a.B)}", "args", "i = 23", "d = 2.71"])
 
+        # f2(us: a, vs: b)
+        process.Continue()
+        self.expect("frame variable",
+                    substrs=["Pack{(a.A)}", "us", "i = 23",
+                             "Pack{(a.B)}", "vs", "d = 2.71"])
+
+        # f3(ts: a, b, more_ts: a, b)
+        process.Continue()
+        self.expect("frame variable",
+                    substrs=["Pack{(a.A, a.B)}", "ts", "i = 23", # FIXME! "d = 2.71",
+                             "Pack{(a.A, a.B)}", "more_ts", "i = 23", "d = 2.71"])
+
+        # f4(uvs: (a, b), (a, b))
+        #process.Continue()
+        #self.expect("frame variable",
+        #            substrs=[])
+
+        # f5(ts: (a, b), (42, b))
+        process.Continue()
+        self.expect("frame variable",
+                    substrs=[# FIXME: "Pack{(a.A, a.B), (Int, a,B)}",
+                             "ts", "i = 23", "d = 2.71"
+                             # FIXME: "42", "d = 2.71"
+                    ])
+
+        # f6(us: a, more_us: a, vs: b, b)
+        #process.Continue()
+        #self.expect("frame variable",
+        #            substrs=[])
+
+        # f7(us: a, vs: 1, b, more_us: a, more_vs: 2, b)
+        process.Continue()
+        self.expect("frame variable",
+                    substrs=["Pack{(a.A)}", "us", # FIXME: "i = 23"
+                             "Pack{(Int, a.B)}", "vs", "= 1", "d = 2.71",
+                             "Pack{(a.A)}", "more_us", #FIXME: "i = 23"
+                             "Pack{(Int, a.B)}", "more_vs", "= 2", "d = 2.71"
+                    ])
+                        
+        
--- lldb/test/API/lang/swift/variadic_generics/a.swift
+++ lldb/test/API/lang/swift/variadic_generics/a.swift
@@ -5,10 +5,49 @@ public struct B {
   var d: Double
 }
 
-public func variadic_function<each T>(args: repeat each T) {
-  print("break here")
-}
-
 let a = A(i: 23)
 let b = B(d: 2.71)
-variadic_function(args: a, b)
+
+public func f1<each T>(args: repeat each T) {
+  print("break here")
+}
+ 
+f1(args: a, b)
+ 
+public func f2<each U, each V>(us: repeat each U, vs: repeat each V) {
+  print("break here")
+}
+ 
+f2(us: a, vs: b)
+ 
+public func f3<each T>(ts: repeat each T, more_ts: repeat each T) {
+  print("break here")
+}
+ 
+f3(ts: a, b, more_ts: a, b)
+
+// FIXME: Crashes the compiler.
+//public func f4<each U, each V>(uvs: repeat (each U, each V)) {
+//  print("break here")
+//}
+// 
+//f4(uvs: (a, b), (a, b))
+ 
+public func f5<each T, U>(ts: repeat (each T, U)) {
+  print("break here")
+}
+ 
+f5(ts: (a, b), (42, b))
+
+// FIXME: Crashes the compiler. 
+//public func f6<each U, each V>(us: repeat each U, more_us: repeat each U, vs: repeat each V) {
+//  print("break here")
+//}
+// 
+//f6(us: a, more_us: a, vs: b, b)
+ 
+public func f7<each U, each V>(us: repeat each U, vs: repeat each V, more_us: repeat each U, more_vs: repeat each V) {
+  print("break here")
+}
+ 
+f7(us: a, vs: 1, b, more_us: a, more_vs: 2, b)
