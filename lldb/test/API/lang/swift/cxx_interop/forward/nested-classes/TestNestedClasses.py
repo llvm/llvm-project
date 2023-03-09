ca2f24a4deb73835a4d60a7b898895740ab1349e
Augusto Noronha <augusto2112@me.com>
2023-03-08 18:01:56 -0800

    [lldb] Add test for nested C++ classes displayed in Swift via interop
    rdar://100284870
    (cherry picked from commit 012c045b94daf7401a0dde542da882c63e4bd6e9)

---
+++ lldb/test/API/lang/swift/cxx_interop/forward/nested-classes/TestNestedClasses.py
@@ -0,0 +1,25 @@
+
+"""
+Test that a C++ class is visible in Swift.
+"""
+from lldbsuite.test.lldbtest import *
+from lldbsuite.test.decorators import *
+
+
+class TestNestedClasses(TestBase):
+
+    @skipIf(setting=('symbols.use-swift-clangimporter', 'false'))
+    @swiftTest
+    def test(self):
+        self.build()
+        self.runCmd('setting set target.experimental.swift-enable-cxx-interop true')
+        _, _, _, _= lldbutil.run_to_source_breakpoint(
+            self, 'Set breakpoint here', lldb.SBFileSpec('main.swift'))
+
+        self.expect('v nested', substrs=['CxxClass::NestedClass', 'b = 20'])
+        self.expect('expr nested', substrs=['CxxClass::NestedClass', 'b = 20'])
+
+        self.expect('v nestedSubclass', substrs=['CxxClass::NestedSubclass', 
+            'SuperClass = (a = 10)', 'c = 30'])
+        self.expect('expr nestedSubclass', substrs=['CxxClass::NestedSubclass', 
+            'SuperClass = (a = 10)', 'c = 30'])
