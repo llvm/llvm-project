5eb72480a5f9c0df5d0c86c55160224396adbd59
Jason Molenda <jason@molenda.com>
2023-03-08 17:59:46 -0800

    Clean up conditional, don't set load binaries twice
    Follow Alex Langford's feedback to my patch from
    https://reviews.llvm.org/D145547 , and fix a
    side issue I noticed while testing this, where
    binaries loaded via LC_NOTE metadata were loaded
    in the Target twice unnecessarily.
    
    (cherry picked from commit 8db8a4e8eddf91bfaee62f83ff10a51e7125d4e4)

--- lldb/source/Plugins/ObjectFile/Mach-O/ObjectFileMachO.cpp
+++ lldb/source/Plugins/ObjectFile/Mach-O/ObjectFileMachO.cpp
@@ -7074,6 +7074,8 @@ bool ObjectFileMachO::LoadCoreFileImages(lldb_private::Process &process) {
           &process, image.filename, image.uuid, image.load_address,
           false /* value_is_offset */, image.currently_executing,
           false /* notify */);
+      if (module_sp)
+        continue;
     }
 
     // If we have a slide, we need to find the original binary
@@ -7084,6 +7086,8 @@ bool ObjectFileMachO::LoadCoreFileImages(lldb_private::Process &process) {
           &process, image.filename, image.uuid, image.slide,
           true /* value_is_offset */, image.currently_executing,
           false /* notify */);
+      if (module_sp)
+        continue;
     }
 
     // Try to find the binary by UUID or filename on the local
