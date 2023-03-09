c622c06cdb7ef8fa1c2c8fb3eeab3989a0cf7e06
Michael Buch <michaelbuch12@gmail.com>
2023-03-09 16:10:00 +0000

    [llvm][Demangle][NFC] Sync latest libcxxabi/demangle
    Differential Revision: https://reviews.llvm.org/D145693

--- llvm/include/llvm/Demangle/ItaniumDemangle.h
+++ llvm/include/llvm/Demangle/ItaniumDemangle.h
@@ -27,18 +27,18 @@
 #include <cstring>
 #include <limits>
 #include <new>
-#include <type_traits>
 #include <utility>
 
 DEMANGLE_NAMESPACE_BEGIN
 
 template <class T, size_t N> class PODSmallVector {
-  static_assert(std::is_trivial_v<T>, "T is required to be a trivial type");
+  static_assert(std::is_pod<T>::value,
+                "T is required to be a plain old data type");
 
   T *First = nullptr;
   T *Last = nullptr;
   T *Cap = nullptr;
-  T Inline[N] = {};
+  T Inline[N] = {0};
 
   bool isInline() const { return First == Inline; }
 
