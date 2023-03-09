60b56492780836587dae3efdde00ca5859c5937e
Luke Lau <luke@igalia.com>
2023-03-09 16:18:28 +0000

    [RISCV] Enable interleaved access vectorization
    The loop vectorizer supports generating interleaved loads and stores via
    shuffle patterns for fixed length vectors.
    This enables it for RISC-V, since interleaved shuffle patterns can be
    lowered to vlseg/vsseg in https://reviews.llvm.org/D145022
    
    Reviewed By: reames
    
    Differential Revision: https://reviews.llvm.org/D145155

--- llvm/lib/Target/RISCV/RISCVTargetTransformInfo.h
+++ llvm/lib/Target/RISCV/RISCVTargetTransformInfo.h
@@ -122,6 +122,11 @@ public:
   InstructionCost getIntrinsicInstrCost(const IntrinsicCostAttributes &ICA,
                                         TTI::TargetCostKind CostKind);
 
+  InstructionCost getInterleavedMemoryOpCost(
+      unsigned Opcode, Type *VecTy, unsigned Factor, ArrayRef<unsigned> Indices,
+      Align Alignment, unsigned AddressSpace, TTI::TargetCostKind CostKind,
+      bool UseMaskForCond = false, bool UseMaskForGaps = false);
+
   InstructionCost getGatherScatterOpCost(unsigned Opcode, Type *DataTy,
                                          const Value *Ptr, bool VariableMask,
                                          Align Alignment,
@@ -279,6 +284,8 @@ public:
     // Let regular unroll to unroll the loop.
     return VF.isScalar() ? 1 : ST->getMaxInterleaveFactor();
   }
+
+  bool enableInterleavedAccessVectorization() { return true; }
 
   enum RISCVRegisterClass { GPRRC, FPRRC, VRRC };
   unsigned getNumberOfRegisters(unsigned ClassID) const {
