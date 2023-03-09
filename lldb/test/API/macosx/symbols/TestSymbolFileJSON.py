48c892bbe7742162e43451e8edaf3c7c93a3fda9
Jonas Devlieghere <jonas@devlieghere.com>
2023-03-08 21:29:16 -0800

    [lldb] Temporarily disable TestSymbolFileJSON on non Darwin platforms
    The new test is triggering a crash in LLDB on the Windows and Linux
    bots. Temporarily disable the test while I investigate.

--- lldb/test/API/macosx/symbols/TestSymbolFileJSON.py
+++ lldb/test/API/macosx/symbols/TestSymbolFileJSON.py
@@ -12,6 +12,7 @@ class TargetSymbolsFileJSON(TestBase):
         self.source = 'main.c'
 
     @no_debug_info_test
+    @skipUnlessDarwin
     def test_symbol_file_json_address(self):
         """Test that 'target symbols add' can load the symbols from a JSON file using file addresses."""
 
