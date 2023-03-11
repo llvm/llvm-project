38d25e7e58af605592ec1a63071fc8e0f56ea83f
Augusto Noronha <augusto2112@me.com>
2023-03-10 16:42:40 -0800

    [lldb] Add actor test and Swift/C++ interop actor test
    rdar://100284870

---
+++ lldb/test/API/lang/swift/variables/actor/TestSwiftActorTypes.py
@@ -0,0 +1,25 @@
+"""
+Test swift Class types
+"""
+import lldb
+from lldbsuite.test.lldbtest import *
+from lldbsuite.test.decorators import *
+import lldbsuite.test.lldbutil as lldbutil
+import os
+import unittest2
+
+
+class TestSwiftActorTypes(TestBase):
+
+    mydir = TestBase.compute_mydir(__file__)
+
+    @swiftTest
+    def test_swift_class_types(self):
+        """Test swift Actor types"""
+        self.build()
+        _, _, _, _= lldbutil.run_to_source_breakpoint(
+            self, 'Set breakpoint here', lldb.SBFileSpec('main.swift'))
+
+        self.expect('v actor', substrs=['Actor', 'str = "Hello"'])
+        self.expect('expr actor', substrs=['Actor', 'str = "Hello"'])
+
