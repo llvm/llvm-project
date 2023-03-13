477a00f4f9cda7459033c78d698be8b80c8f43eb
Ben Langmuir <blangmuir@apple.com>
2023-03-13 16:00:30 -0700

    [libclang][cas] Optionally enable include-tree in libclang dependency scanner
    Obey CLANG_CACHE_USE_INCLUDE_TREE and CLANG_CACHE_USE_CASFS_DEPSCAN. For
    now the default is still cas-fs since that supports modules and
    include-tree does not.
    
    (cherry picked from commit 72a7cc466ed327071a7858a83e4792ccd0f81a96)

--- clang/test/Index/Core/scan-deps-cas.m
+++ clang/test/Index/Core/scan-deps-cas.m
@@ -1,11 +1,22 @@
 // RUN: rm -rf %t.mcp %t
-// RUN: echo %S > %t.result
-//
+
 // RUN: c-index-test core --scan-deps %S -output-dir=%t -cas-path %t/cas \
 // RUN:  -- %clang -c -I %S/Inputs/module \
 // RUN:     -fmodules -fmodules-cache-path=%t.mcp \
-// RUN:     -o FoE.o -x objective-c %s >> %t.result
-// RUN: cat %t.result | sed 's/\\/\//g' | FileCheck %s -DOUTPUTS=%/t
+// RUN:     -o FoE.o -x objective-c %s > %t.result
+// RUN: cat %t.result | sed 's/\\/\//g' | FileCheck %s -DPREFIX=%S -DOUTPUTS=%/t
+
+// RUN: env CLANG_CACHE_USE_CASFS_DEPSCAN=1 c-index-test core --scan-deps %S -output-dir=%t -cas-path %t/cas \
+// RUN:  -- %clang -c -I %S/Inputs/module \
+// RUN:     -fmodules -fmodules-cache-path=%t.mcp \
+// RUN:     -o FoE.o -x objective-c %s > %t.casfs.result
+// RUN: cat %t.casfs.result | sed 's/\\/\//g' | FileCheck %s -DPREFIX=%S -DOUTPUTS=%/t
+
+// FIXME: enable modules when supported.
+// RUN: env CLANG_CACHE_USE_INCLUDE_TREE=1 c-index-test core --scan-deps %S -output-dir=%t -cas-path %t/cas \
+// RUN:  -- %clang -c -I %S/Inputs/module \
+// RUN:     -o FoE.o -x objective-c %s > %t.includetree.result
+// RUN: cat %t.includetree.result | sed 's/\\/\//g' | FileCheck %s -DPREFIX=%S -DOUTPUTS=%/t -check-prefix=INCLUDE_TREE
 
 // RUN: c-index-test core --scan-deps %S -output-dir=%t \
 // RUN:  -- %clang -c -I %S/Inputs/module \
@@ -15,10 +26,9 @@
 // NO_CAS-NOT: faction-cache
 // NO_CAS-NOT: fcache-compile-job
 
-@import ModA;
+#include "ModA.h"
 
-// CHECK: [[PREFIX:.*]]
-// CHECK-NEXT: modules:
+// CHECK:       modules:
 // CHECK-NEXT:   module:
 // CHECK-NEXT:     name: ModA
 // CHECK-NEXT:     context-hash: [[HASH_MOD_A:[A-Z0-9]+]]
@@ -52,3 +62,16 @@
 // CHECK-SAME:       -fcache-compile-job
 // CHECK-SAME:       -fmodule-file-cache-key [[PCM:.*ModA_.*pcm]] llvmcas://{{[[:xdigit:]]+}}
 // CHECK-SAME:       -fmodule-file={{(ModA=)?}}[[PCM]]
+
+// INCLUDE_TREE:      dependencies:
+// INCLUDE_TREE-NEXT:   command 0:
+// INCLUDE_TREE-NEXT:     context-hash: [[HASH_TU:[A-Z0-9]+]]
+// INCLUDE_TREE-NEXT:     module-deps:
+// INCLUDE_TREE-NEXT:     file-deps:
+// INCLUDE_TREE-NEXT:       [[PREFIX]]/scan-deps-cas.m
+// INCLUDE_TREE-NEXT:       [[PREFIX]]/Inputs/module/ModA.h
+// INCLUDE_TREE-NEXT:     build-args:
+// INCLUDE_TREE-SAME:       -cc1
+// INCLUDE_TREE-SAME:       -fcas-path
+// INCLUDE_TREE-SAME:       -fcas-include-tree llvmcas://{{[[:xdigit:]]+}}
+// INCLUDE_TREE-SAME:       -fcache-compile-job
