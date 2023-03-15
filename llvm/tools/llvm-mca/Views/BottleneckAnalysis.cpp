2fc3e1378f466b8d0017dc15de04d3da8c492000
Kazu Hirata <kazu@google.com>
2023-03-14 18:56:06 -0700

    [llvm] Use *{Set,Map}::contains (NFC)

--- llvm/tools/llvm-mca/Views/BottleneckAnalysis.cpp
+++ llvm/tools/llvm-mca/Views/BottleneckAnalysis.cpp
@@ -52,7 +52,7 @@ void PressureTracker::getResourceUsers(uint64_t ResourceMask,
   const MCProcResourceDesc &PRDesc = *SM.getProcResource(ProcResID);
   for (unsigned I = 0, E = PRDesc.NumUnits; I < E; ++I) {
     const User U = getResourceUser(ProcResID, I);
-    if (U.second && IPI.find(U.first) != IPI.end())
+    if (U.second && IPI.contains(U.first))
       Users.emplace_back(U);
   }
 }
