232e6623d4a440ae3c4872cdd6c0b6caa14aed48
Ben Langmuir <blangmuir@apple.com>
2023-03-16 15:57:06 -0700

    [clang][cas] Add a test for include-tree with explicit imports
    With explicit imports, we do not record the module import in the include
    tree, because the only thing that happens is to lookup the module by
    name, which works already via the command-line options that provide the
    module via the action cache.
    
    (cherry picked from commit a13b79f23e54a598003fcd9c3cf848f474c82c22)

--- clang/test/ClangScanDeps/modules-include-tree.c
+++ clang/test/ClangScanDeps/modules-include-tree.c
@@ -13,6 +13,8 @@
 // RUN: %deps-to-rsp %t/deps.json --module-name Top > %t/Top.rsp
 // RUN: %deps-to-rsp %t/deps.json --module-name Left > %t/Left.rsp
 // RUN: %deps-to-rsp %t/deps.json --module-name Right > %t/Right.rsp
+// RUN: %deps-to-rsp %t/deps.json --module-name ZAtImport > %t/ZAtImport.rsp
+// RUN: %deps-to-rsp %t/deps.json --module-name ZPragmaImport > %t/ZPragmaImport.rsp
 // RUN: %deps-to-rsp %t/deps.json --tu-index 0 > %t/tu.rsp
 
 // Extract include-tree casids
@@ -65,10 +67,15 @@
 // CHECK: [[PREFIX]]/Top.h llvmcas://{{[[:xdigit:]]+}}
 
 // CHECK-LABEL: TRANSLATION UNIT
-// CHECK: [[PREFIX]]/tu.c llvmcas://{{[[:xdigit:]]+}}
+// CHECK: [[PREFIX]]/tu.m llvmcas://{{[[:xdigit:]]+}}
 // CHECK: 1:1 <built-in> llvmcas://{{[[:xdigit:]]+}}
 // CHECK: 2:1 (Module) Left
 // CHECK: 3:1 (Module) Right
+
+// Note: the modules with explicit imports are imported via parser and are not
+// recorded in the include-tree; it's handled entirely by fmodule-map-file,
+// fmodule-file, and fmodule-file-cache-key options.
+
 // CHECK: Files:
 // CHECK: [[PREFIX]]/Left.h llvmcas://{{[[:xdigit:]]+}}
 // CHECK: [[PREFIX]]/module.modulemap llvmcas://{{[[:xdigit:]]+}}
@@ -101,8 +108,6 @@
 // CHECK:              "-fmodule-file-cache-key"
 // CHECK-NEXT:         "[[PREFIX]]/outputs/{{.*}}/Top-{{.*}}.pcm"
 // CHECK-NEXT:         "llvmcas://{{[[:xdigit:]]+}}"
-// CHECK:              "-x"
-// CHECK-NEXT:         "c"
 // CHECK:              "-fmodule-file=Top=[[PREFIX]]/outputs/{{.*}}/Top-{{.*}}.pcm"
 // CHECK:              "-fmodules"
 // CHECK:              "-fmodule-name=Left"
@@ -138,8 +143,6 @@
 // CHECK:              "-fmodule-file-cache-key
 // CHECK-NEXT:         "[[PREFIX]]/outputs/{{.*}}/Top-{{.*}}.pcm"
 // CHECK-NEXT:         "llvmcas://{{[[:xdigit:]]+}}"
-// CHECK:              "-x"
-// CHECK-NEXT:         "c"
 // CHECK:              "-fmodule-file=Top=[[PREFIX]]/outputs/{{.*}}/Top-{{.*}}.pcm"
 // CHECK:              "-fmodules"
 // CHECK:              "-fmodule-name=Right"
@@ -167,8 +170,6 @@
 // CHECK-NEXT:         "[[TOP_TREE]]"
 // CHECK:              "-fcache-compile-job"
 // CHECK:              "-emit-module"
-// CHECK:              "-x"
-// CHECK-NEXT:         "c"
 // CHECK:              "-fmodules"
 // CHECK:              "-fmodule-name=Top"
 // CHECK:              "-fno-implicit-modules"
@@ -178,6 +179,58 @@
 // CHECK-NEXT:         "[[PREFIX]]/module.modulemap"
 // CHECK-NEXT:       ]
 // CHECK:            "name": "Top"
+// CHECK:          }
+// CHECK-NEXT:     {
+// CHECK:            "cas-include-tree-id": "[[AT_IMPORT_TREE:llvmcas://[[:xdigit:]]+]]"
+// CHECK:            "clang-module-deps": []
+// CHECK:            "clang-modulemap-file": "[[PREFIX]]/module.modulemap"
+// CHECK:            "command-line": [
+// CHECK-NEXT:         "-cc1"
+// CHECK:              "-fcas-path"
+// CHECK-NEXT:         "[[PREFIX]]/cas"
+// CHECK:              "-o"
+// CHECK-NEXT:         "[[PREFIX]]/outputs/{{.*}}/ZAtImport-{{.*}}.pcm"
+// CHECK:              "-disable-free"
+// CHECK:              "-fno-pch-timestamp"
+// CHECK:              "-fcas-include-tree"
+// CHECK-NEXT:         "[[AT_IMPORT_TREE]]"
+// CHECK:              "-fcache-compile-job"
+// CHECK:              "-emit-module"
+// CHECK:              "-fmodules"
+// CHECK:              "-fmodule-name=ZAtImport"
+// CHECK:              "-fno-implicit-modules"
+// CHECK:            ]
+// CHECK:            "file-deps": [
+// CHECK-NEXT:         "[[PREFIX]]/AtImport.h"
+// CHECK-NEXT:         "[[PREFIX]]/module.modulemap"
+// CHECK-NEXT:       ]
+// CHECK:            "name": "ZAtImport"
+// CHECK:          }
+// CHECK-NEXT:     {
+// CHECK:            "cas-include-tree-id": "[[PRAGMA_IMPORT_TREE:llvmcas://[[:xdigit:]]+]]"
+// CHECK:            "clang-module-deps": []
+// CHECK:            "clang-modulemap-file": "[[PREFIX]]/module.modulemap"
+// CHECK:            "command-line": [
+// CHECK-NEXT:         "-cc1"
+// CHECK:              "-fcas-path"
+// CHECK-NEXT:         "[[PREFIX]]/cas"
+// CHECK:              "-o"
+// CHECK-NEXT:         "[[PREFIX]]/outputs/{{.*}}/ZPragmaImport-{{.*}}.pcm"
+// CHECK:              "-disable-free"
+// CHECK:              "-fno-pch-timestamp"
+// CHECK:              "-fcas-include-tree"
+// CHECK-NEXT:         "[[PRAGMA_IMPORT_TREE]]"
+// CHECK:              "-fcache-compile-job"
+// CHECK:              "-emit-module"
+// CHECK:              "-fmodules"
+// CHECK:              "-fmodule-name=ZPragmaImport"
+// CHECK:              "-fno-implicit-modules"
+// CHECK:            ]
+// CHECK:            "file-deps": [
+// CHECK-NEXT:         "[[PREFIX]]/PragmaImport.h"
+// CHECK-NEXT:         "[[PREFIX]]/module.modulemap"
+// CHECK-NEXT:       ]
+// CHECK:            "name": "ZPragmaImport"
 // CHECK:          }
 // CHECK:        ]
 // CHECK-NEXT:   "translation-units": [
@@ -191,6 +244,12 @@
 // CHECK:                  }
 // CHECK-NEXT:             {
 // CHECK:                    "module-name": "Right"
+// CHECK:                  }
+// CHECK-NEXT:             {
+// CHECK:                    "module-name": "ZAtImport"
+// CHECK:                  }
+// CHECK-NEXT:             {
+// CHECK:                    "module-name": "ZPragmaImport"
 // CHECK:                  }
 // CHECK-NEXT:           ]
 // CHECK:                "command-line": [
@@ -209,17 +268,15 @@
 // CHECK:                  "-fmodule-file-cache-key"
 // CHECK-NEXT:             "[[PREFIX]]/outputs/{{.*}}/Right-{{.*}}.pcm"
 // CHECK-NEXT:             "llvmcas://{{[[:xdigit:]]+}}"
-// CHECK:                  "-x"
-// CHECK-NEXT:             "c"
 // CHECK:                  "-fmodule-file=Left=[[PREFIX]]/outputs/{{.*}}/Left-{{.*}}.pcm"
 // CHECK:                  "-fmodule-file=Right=[[PREFIX]]/outputs/{{.*}}/Right-{{.*}}.pcm"
 // CHECK:                  "-fmodules"
 // CHECK:                  "-fno-implicit-modules"
 // CHECK:                ]
 // CHECK:                "file-deps": [
-// CHECK-NEXT:             "[[PREFIX]]/tu.c"
+// CHECK-NEXT:             "[[PREFIX]]/tu.m"
 // CHECK-NEXT:           ]
-// CHECK:                "input-file": "[[PREFIX]]/tu.c"
+// CHECK:                "input-file": "[[PREFIX]]/tu.m"
 // CHECK:              }
 // CHECK-NEXT:       ]
 // CHECK-NEXT:     }
@@ -234,12 +291,18 @@
 // RUN: rm -rf %t/outputs
 // RUN: %clang @%t/Right.rsp 2>&1 | FileCheck %s -check-prefix=CACHE_MISS
 // RUN: rm -rf %t/outputs
+// RUN: %clang @%t/ZAtImport.rsp 2>&1 | FileCheck %s -check-prefix=CACHE_MISS
+// RUN: rm -rf %t/outputs
+// RUN: %clang @%t/ZPragmaImport.rsp 2>&1 | FileCheck %s -check-prefix=CACHE_MISS
+// RUN: rm -rf %t/outputs
 // RUN: %clang @%t/tu.rsp 2>&1 | FileCheck %s -check-prefix=CACHE_MISS
 
 // Check cache hits
 // RUN: %clang @%t/Top.rsp 2>&1 | FileCheck %s -check-prefix=CACHE_HIT
 // RUN: %clang @%t/Left.rsp 2>&1 | FileCheck %s -check-prefix=CACHE_HIT
 // RUN: %clang @%t/Right.rsp 2>&1 | FileCheck %s -check-prefix=CACHE_HIT
+// RUN: %clang @%t/ZAtImport.rsp 2>&1 | FileCheck %s -check-prefix=CACHE_HIT
+// RUN: %clang @%t/ZPragmaImport.rsp 2>&1 | FileCheck %s -check-prefix=CACHE_HIT
 // RUN: %clang @%t/tu.rsp 2>&1 | FileCheck %s -check-prefix=CACHE_HIT
 
 // CACHE_MISS: compile job cache miss
@@ -247,15 +310,17 @@
 
 //--- cdb.json.template
 [{
-  "file": "DIR/tu.c",
+  "file": "DIR/tu.m",
   "directory": "DIR",
-  "command": "clang -fsyntax-only DIR/tu.c -I DIR -fmodules -fimplicit-modules -fimplicit-module-maps -fmodules-cache-path=DIR/module-cache -Rcompile-job-cache"
+  "command": "clang -fsyntax-only DIR/tu.m -I DIR -fmodules -fimplicit-modules -fimplicit-module-maps -fmodules-cache-path=DIR/module-cache -Rcompile-job-cache"
 }]
 
 //--- module.modulemap
 module Top { header "Top.h" export *}
 module Left { header "Left.h" export *}
 module Right { header "Right.h" export *}
+module ZAtImport { header "AtImport.h" }
+module ZPragmaImport { header "PragmaImport.h" }
 
 //--- Top.h
 #pragma once
@@ -272,12 +337,22 @@ void left(void);
 #include "Top.h"
 void right(void);
 
-//--- tu.c
+//--- AtImport.h
+void at_import(void);
+
+//--- PragmaImport.h
+void pragma_import(void);
+
+//--- tu.m
 #import "Left.h"
 #import <Right.h>
+@import ZAtImport;
+#pragma clang module import ZPragmaImport
 
 void tu(void) {
   top();
   left();
   right();
+  at_import();
+  pragma_import();
 }
