58d9a13b10f48c9170c8965695f17bbc507488d7
David Spickett <david.spickett@linaro.org>
2023-03-17 11:04:38 +0000

    [lldb] For native compiles, check signal numbers are correct when adding codes
    Reviewed By: arichardson, emaste
    
    Differential Revision: https://reviews.llvm.org/D146285

--- lldb/source/Plugins/Platform/FreeBSD/FreeBSDSignals.cpp
+++ lldb/source/Plugins/Platform/FreeBSD/FreeBSDSignals.cpp
@@ -11,13 +11,16 @@
 #ifdef __FreeBSD__
 #include <csignal>
 
-#define ADD_SIGCODE(signal, name, value, ...)                                  \
-  static_assert(name == value, "Value mismatch for signal code " #name);       \
-  AddSignalCode(signal, value, __VA_ARGS__)
+#define ADD_SIGCODE(signal_name, signal_value, code_name, code_value, ...)     \
+  static_assert(signal_name == signal_value,                                   \
+                "Value mismatch for signal number " #signal_name);             \
+  static_assert(code_name == code_value,                                       \
+                "Value mismatch for signal code " #code_name);                 \
+  AddSignalCode(signal_value, code_value, __VA_ARGS__)
 #else
-#define ADD_SIGCODE(signal, name, value, ...)                                  \
-  AddSignalCode(signal, value, __VA_ARGS__)
-#endif /* ifdef __FreeBSD__ */
+#define ADD_SIGCODE(signal_name, signal_value, code_name, code_value, ...)     \
+  AddSignalCode(signal_value, code_value, __VA_ARGS__)
+#endif /* ifdef __FreeBSD */
 
 using namespace lldb_private;
 
@@ -28,38 +31,38 @@ void FreeBSDSignals::Reset() {
 
   // clang-format off
   // SIGILL
-  ADD_SIGCODE(4, ILL_ILLOPC, 1, "illegal opcode");
-  ADD_SIGCODE(4, ILL_ILLOPN, 2, "illegal operand");
-  ADD_SIGCODE(4, ILL_ILLADR, 3, "illegal addressing mode");
-  ADD_SIGCODE(4, ILL_ILLTRP, 4, "illegal trap");
-  ADD_SIGCODE(4, ILL_PRVOPC, 5, "privileged opcode");
-  ADD_SIGCODE(4, ILL_PRVREG, 6, "privileged register");
-  ADD_SIGCODE(4, ILL_COPROC, 7, "coprocessor error");
-  ADD_SIGCODE(4, ILL_BADSTK, 8, "internal stack error");
+  ADD_SIGCODE(SIGILL, 4, ILL_ILLOPC, 1, "illegal opcode");
+  ADD_SIGCODE(SIGILL, 4, ILL_ILLOPN, 2, "illegal operand");
+  ADD_SIGCODE(SIGILL, 4, ILL_ILLADR, 3, "illegal addressing mode");
+  ADD_SIGCODE(SIGILL, 4, ILL_ILLTRP, 4, "illegal trap");
+  ADD_SIGCODE(SIGILL, 4, ILL_PRVOPC, 5, "privileged opcode");
+  ADD_SIGCODE(SIGILL, 4, ILL_PRVREG, 6, "privileged register");
+  ADD_SIGCODE(SIGILL, 4, ILL_COPROC, 7, "coprocessor error");
+  ADD_SIGCODE(SIGILL, 4, ILL_BADSTK, 8, "internal stack error");
 
   // SIGFPE
-  ADD_SIGCODE(8, FPE_INTOVF, 1, "integer overflow");
-  ADD_SIGCODE(8, FPE_INTDIV, 2, "integer divide by zero");
-  ADD_SIGCODE(8, FPE_FLTDIV, 3, "floating point divide by zero");
-  ADD_SIGCODE(8, FPE_FLTOVF, 4, "floating point overflow");
-  ADD_SIGCODE(8, FPE_FLTUND, 5, "floating point underflow");
-  ADD_SIGCODE(8, FPE_FLTRES, 6, "floating point inexact result");
-  ADD_SIGCODE(8, FPE_FLTINV, 7, "invalid floating point operation");
-  ADD_SIGCODE(8, FPE_FLTSUB, 8, "subscript out of range");
-  ADD_SIGCODE(8, FPE_FLTIDO, 9, "input denormal operation");
+  ADD_SIGCODE(SIGFPE, 8, FPE_INTOVF, 1, "integer overflow");
+  ADD_SIGCODE(SIGFPE, 8, FPE_INTDIV, 2, "integer divide by zero");
+  ADD_SIGCODE(SIGFPE, 8, FPE_FLTDIV, 3, "floating point divide by zero");
+  ADD_SIGCODE(SIGFPE, 8, FPE_FLTOVF, 4, "floating point overflow");
+  ADD_SIGCODE(SIGFPE, 8, FPE_FLTUND, 5, "floating point underflow");
+  ADD_SIGCODE(SIGFPE, 8, FPE_FLTRES, 6, "floating point inexact result");
+  ADD_SIGCODE(SIGFPE, 8, FPE_FLTINV, 7, "invalid floating point operation");
+  ADD_SIGCODE(SIGFPE, 8, FPE_FLTSUB, 8, "subscript out of range");
+  ADD_SIGCODE(SIGFPE, 8, FPE_FLTIDO, 9, "input denormal operation");
 
   // SIGBUS
-  ADD_SIGCODE(10, BUS_ADRALN, 1,   "invalid address alignment");
-  ADD_SIGCODE(10, BUS_ADRERR, 2,   "nonexistent physical address");
-  ADD_SIGCODE(10, BUS_OBJERR, 3,   "object-specific hardware error");
-  ADD_SIGCODE(10, BUS_OOMERR, 100, "no memory");
+  ADD_SIGCODE(SIGBUS, 10, BUS_ADRALN, 1,   "invalid address alignment");
+  ADD_SIGCODE(SIGBUS, 10, BUS_ADRERR, 2,   "nonexistent physical address");
+  ADD_SIGCODE(SIGBUS, 10, BUS_OBJERR, 3,   "object-specific hardware error");
+  ADD_SIGCODE(SIGBUS, 10, BUS_OOMERR, 100, "no memory");
 
   // SIGSEGV
-  ADD_SIGCODE(11, SEGV_MAPERR, 1,   "address not mapped to object",
+  ADD_SIGCODE(SIGSEGV, 11, SEGV_MAPERR, 1,   "address not mapped to object",
                   SignalCodePrintOption::Address);
-  ADD_SIGCODE(11, SEGV_ACCERR, 2,   "invalid permissions for mapped object",
+  ADD_SIGCODE(SIGSEGV, 11, SEGV_ACCERR, 2,   "invalid permissions for mapped object",
                   SignalCodePrintOption::Address);
-  ADD_SIGCODE(11, SEGV_PKUERR, 100, "PKU violation",
+  ADD_SIGCODE(SIGSEGV, 11, SEGV_PKUERR, 100, "PKU violation",
                   SignalCodePrintOption::Address);
 
   //        SIGNO NAME           SUPPRESS STOP   NOTIFY DESCRIPTION
