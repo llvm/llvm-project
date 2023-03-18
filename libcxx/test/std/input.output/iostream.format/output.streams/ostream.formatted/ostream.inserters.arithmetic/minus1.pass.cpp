ed61d6a46611cfb144b260e0dd0fb1b5783562d9
Louis Dionne <ldionne.2@gmail.com>
2023-03-18 13:34:29 -0400

    [libc++] Use the stdlib=<LIB> Lit feature instead of use_system_cxx_lib
    The use_system_cxx_lib Lit feature was only used for back-deployment
    testing. However, one immense hole in that setup was that we didn't
    have a proper way to test Apple's own libc++ outside of back-deployment,
    which was embodied by the fact that we needed to define _LIBCPP_DISABLE_AVAILABILITY
    when testing (see change in libcxx/utils/libcxx/test/params.py).
    
    This led to the apple-system testing configuration not checking for
    availability markup, which is obviously quite bad since the library
    we ship actually has availability markup.
    
    Using stdlib=<VENDOR>-libc++ instead to encode back-deployment restrictions
    on tests is simpler and it makes it possible to naturally support tests
    such as availability markup checking even in the tip-of-trunk Apple-libc++
    configuration.
    
    Differential Revision: https://reviews.llvm.org/D146366

--- libcxx/test/std/input.output/iostream.format/output.streams/ostream.formatted/ostream.inserters.arithmetic/minus1.pass.cpp
+++ libcxx/test/std/input.output/iostream.format/output.streams/ostream.formatted/ostream.inserters.arithmetic/minus1.pass.cpp
@@ -6,7 +6,7 @@
 //
 //===----------------------------------------------------------------------===//
 
-// XFAIL: use_system_cxx_lib && target={{.+}}-apple-macosx10.12
+// XFAIL: stdlib=apple-libc++ && target={{.+}}-apple-macosx10.12
 
 // <ostream>
 
