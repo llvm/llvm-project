f4085f480fba567db28b2a58dc05b4db5f1dbd27
Ishaan Gandhi <ishaangandhi@gmail.com>
2023-03-20 22:31:39 -0700

    [Attributor] Add convergent abstract attribute
    This patch adds the AANonConvergent abstract attribute. It removes the
    convergent attribute from functions that only call non-convergent
    functions.
    
    Reviewed By: jdoerfert
    
    Differential Revision: https://reviews.llvm.org/D143228

--- llvm/lib/Transforms/IPO/Attributor.cpp
+++ llvm/lib/Transforms/IPO/Attributor.cpp
@@ -3193,6 +3193,10 @@ void Attributor::identifyDefaultAbstractAttributes(Function &F) {
   // Every function might be "no-recurse".
   getOrCreateAAFor<AANoRecurse>(FPos);
 
+  // Every function can be "non-convergent".
+  if (F.hasFnAttribute(Attribute::Convergent))
+    getOrCreateAAFor<AANonConvergent>(FPos);
+
   // Every function might be "readnone/readonly/writeonly/...".
   getOrCreateAAFor<AAMemoryBehavior>(FPos);
 
