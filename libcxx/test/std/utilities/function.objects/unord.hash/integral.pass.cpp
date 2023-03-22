7680fd0492d1b612510a0767009256104c057bcc
Mark de Wever <koraq@xs4all.nl>
2023-03-22 17:39:27 +0100

    [libc++] Qualifies intptr_t and uintptr_t.
    This has been done using the following command
    
      find libcxx/test -type f -exec perl -pi -e 's|^([^/]+?)((?<!::)(?<!::u)u?intptr_t)|\1std::\2|' \{} \;
    
    The std module doesn't export declarations in the global namespaace.
    This is a preparation for that module.
    
    Reviewed By: #libc, ldionne
    
    Differential Revision: https://reviews.llvm.org/D146643

--- libcxx/test/std/utilities/function.objects/unord.hash/integral.pass.cpp
+++ libcxx/test/std/utilities/function.objects/unord.hash/integral.pass.cpp
@@ -90,7 +90,7 @@ int main(int, char**)
     test<std::int_least64_t>();
 
     test<intmax_t>();
-    test<intptr_t>();
+    test<std::intptr_t>();
 
     test<std::uint8_t>();
     test<std::uint16_t>();
@@ -108,7 +108,7 @@ int main(int, char**)
     test<std::uint_least64_t>();
 
     test<uintmax_t>();
-    test<uintptr_t>();
+    test<std::uintptr_t>();
 
 #ifndef TEST_HAS_NO_INT128
     test<__int128_t>();
