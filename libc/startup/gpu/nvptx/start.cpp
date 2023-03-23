ae63b1a5767b89fe5af140365f9e3ccf74feb1f0
Joseph Huber <jhuber6@vols.utk.edu>
2023-03-22 19:58:08 -0500

    [libc] Adjust NVPTX startup code
    Summary:
    The startup code needs to include the environment pointer so we add this
    to the arguments. Also we need to ensure that the `crt1.o` file is made
    with `-fgpu-rdc` set so we can actually use it without undefined
    reference errors.

--- libc/startup/gpu/nvptx/start.cpp
+++ libc/startup/gpu/nvptx/start.cpp
@@ -6,10 +6,11 @@
 //
 //===----------------------------------------------------------------------===//
 
-extern "C" __attribute__((device)) int main(int argc, char **argv);
+extern "C" __attribute__((device)) int main(int argc, char **argv, char **envp);
 
 // TODO: We shouldn't need to use the CUDA language to emit a kernel for NVPTX.
 extern "C" [[gnu::visibility("protected")]] __attribute__((global)) void
-_start(int argc, char **argv, int *ret) {
-  __atomic_fetch_or(ret, main(argc, argv), __ATOMIC_RELAXED);
+_start(int argc, char **argv, char **envp, int *ret, void *in, void *out,
+       void *buffer) {
+  __atomic_fetch_or(ret, main(argc, argv, envp), __ATOMIC_RELAXED);
 }
