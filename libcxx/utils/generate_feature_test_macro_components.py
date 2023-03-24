b30bca7209d0b1af964fbad90f74bc33e4d6e047
Louis Dionne <ldionne.2@gmail.com>
2023-03-24 14:41:26 -0400

    [libc++] Don't try to provide source_location on AppleClang 1403
    AppleClang 1403 has some bugs that prevent std::source_location from
    working properly on it. Consequently, we XFAILed the unit test for
    source_location with that compiler. However, we should also avoid
    advertising that the feature is supported on that compiler, otherwise
    our feature-test macros lie. This was noticed to break Boost.Asio
    when building with a recent libc++ and AppleClang 14.0.3.
    
    rdar://106863087
    
    Differential Revision: https://reviews.llvm.org/D146837

--- libcxx/utils/generate_feature_test_macro_components.py
+++ libcxx/utils/generate_feature_test_macro_components.py
@@ -650,8 +650,8 @@ feature_test_macros = [ add_version_header(x) for x in [
     "name": "__cpp_lib_source_location",
     "values": { "c++20": 201907 },
     "headers": ["source_location"],
-    "test_suite_guard": "__has_builtin(__builtin_source_location)",
-    "libcxx_guard": "__has_builtin(__builtin_source_location)",
+    "test_suite_guard": "__has_builtin(__builtin_source_location) && !(defined(TEST_APPLE_CLANG_VER) && TEST_APPLE_CLANG_VER <= 1403)",
+    "libcxx_guard": "__has_builtin(__builtin_source_location) && !(defined(_LIBCPP_APPLE_CLANG_VER) && _LIBCPP_APPLE_CLANG_VER <= 1403)",
   }, {
     "name": "__cpp_lib_span",
     "values": { "c++20": 202002 },
