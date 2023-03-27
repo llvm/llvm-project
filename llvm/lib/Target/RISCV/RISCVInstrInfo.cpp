941667bc8239c4aae0e75f58c140957b315f28e4
Craig Topper <craig.topper@sifive.com>
2023-03-27 12:30:37 -0700

    Recommit "[RISCV] Add .insn support for compressed formats."
    I think the failure was caused by a mistake in an earlier patch.
    
    Original commit message:
    
    We've supported .insn for non-compressed for a while. This finishes the compressed supported.
    
    Differential Revision: https://reviews.llvm.org/D146663

--- llvm/lib/Target/RISCV/RISCVInstrInfo.cpp
+++ llvm/lib/Target/RISCV/RISCVInstrInfo.cpp
@@ -1672,7 +1672,9 @@ bool RISCVInstrInfo::verifyInstruction(const MachineInstr &MI,
         CASE_OPERAND_UIMM(3)
         CASE_OPERAND_UIMM(4)
         CASE_OPERAND_UIMM(5)
+        CASE_OPERAND_UIMM(6)
         CASE_OPERAND_UIMM(7)
+        CASE_OPERAND_UIMM(8)
         CASE_OPERAND_UIMM(12)
         CASE_OPERAND_UIMM(20)
           // clang-format on
