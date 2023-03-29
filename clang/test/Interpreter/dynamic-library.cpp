3b9f61d4d5484aebe6b0bb20435d504a4fe62088
Douglas Yung <douglas.yung@sony.com>
2023-03-29 12:09:28 -0700

    Mark test added in D141824 as unsupported for PS4/PS5 as those platforms require an external linker that is not present.

--- clang/test/Interpreter/dynamic-library.cpp
+++ clang/test/Interpreter/dynamic-library.cpp
@@ -1,4 +1,5 @@
 // REQUIRES: host-supports-jit, system-linux
+// UNSUPPORTED: target={{.*-(ps4|ps5)}}
 
 // RUN: %clang -xc++ -o %T/libdynamic-library-test.so -fPIC -shared -DLIBRARY %S/Inputs/dynamic-library-test.cpp
 // RUN: cat %s | env LD_LIBRARY_PATH=%T:$LD_LIBRARY_PATH clang-repl | FileCheck %s
