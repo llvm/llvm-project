6c01917a4e682b2e0c3eee4ddfd6b78f0f76c877
Louis Dionne <ldionne.2@gmail.com>
2023-03-29 16:48:20 -0400

    [libc++] Add incomplete availability markup for std::pmr
    This fixes rdar://110330781, which asked for the feature-test macro
    for std::pmr to take into account the deployment target. It doesn't
    fix https://llvm.org/PR62212, though, because the availability markup
    itself must be disabled until some Clang bugs have been fixed.
    
    This is pretty vexing, however at least everything should work once
    those Clang bugs have been fixed. In the meantime, this patch at least
    adds the required markup (as disabled) and ensures that the feature-test
    macro for std::pmr is aware of the deployment target requirement.
    
    Differential Revision: https://reviews.llvm.org/D135813

--- libcxx/utils/libcxx/test/features.py
+++ libcxx/utils/libcxx/test/features.py
@@ -522,7 +522,7 @@ DEFAULT_FEATURES += [
     Feature(
         name="availability-pmr-missing",
         when=lambda cfg: BooleanExpression.evaluate(
-            "stdlib=apple-libc++ && target={{.+}}-apple-macosx{{(10.9|10.10|10.11|10.12|10.13|10.14|10.15|11.0)(.0)?}}",
+            "stdlib=apple-libc++ && target={{.+}}-apple-macosx{{(10.9|10.10|10.11|10.12|10.13|10.14|10.15|11.0|12.0|13.0)(.0)?}}",
             cfg.available_features,
         ),
     ),
