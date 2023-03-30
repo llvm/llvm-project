4937431c40b035ce8933e41c75747a90c46d00a5
Ben Langmuir <blangmuir@apple.com>
2023-03-30 16:15:43 -0700

    [clang][cas] Strip -coverage-data-file and -coverage-notes-file from PCH
    Companion to https://reviews.llvm.org/D147282; when caching a PCH,
    remove the -coverage-data-file and -coverage-notes-file options. These
    paths cannot currently be prefix-mapped, because they are emitted into
    the binary for the coverage runtime to use. However, they have no effect
    on PCH, so remove them.
    
    rdar://107443796
    (cherry picked from commit 0d086a5125e09158dbab9985702c6a704a786daf)
    (cherry picked from commit eb9236e51cd666588e88a4c3102a58b4aa8de42a)

--- clang/test/CAS/depscan-include-tree.c
+++ clang/test/CAS/depscan-include-tree.c
@@ -2,7 +2,8 @@
 // RUN: split-file %s %t
 
 // RUN: %clang -cc1depscan -o %t/inline.rsp -fdepscan=inline -fdepscan-include-tree -cc1-args -cc1 -triple x86_64-apple-macos11.0 \
-// RUN:     -emit-obj %t/t.c -o %t/t.o -dwarf-ext-refs -fmodule-format=obj \
+// RUN:     -emit-obj %t/t.c -o %t/t.o -dwarf-ext-refs -fmodule-format=obj  \
+// RUN:     -ftest-coverage -coverage-notes-file %t/t.gcno -coverage-data-file %t/t.gcda \
 // RUN:     -I %t/includes -isysroot %S/Inputs/SDK -fcas-path %t/cas -DSOME_MACRO -dependency-file %t/inline.d -MT deps
 
 // RUN: FileCheck %s -input-file %t/inline.rsp -DPREFIX=%t
@@ -13,6 +14,9 @@
 // CHECK: "-fcas-path" "[[PREFIX]]/cas"
 // CHECK: "-fcas-include-tree"
 // CHECK: "-isysroot"
+// CHECK: "-ftest-coverage"
+// CHECK: "-coverage-data-file" "[[PREFIX]]/t.gcda"
+// CHECK: "-coverage-notes-file" "[[PREFIX]]/t.gcno"
 // SHOULD-NOT: "-fcas-fs"
 // SHOULD-NOT: "-fcas-fs-working-directory"
 // SHOULD-NOT: "-I"
