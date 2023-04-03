9ff1581aae80d888a814e1b552ac63306af58f71
Stanislav Mekhanoshin <Stanislav.Mekhanoshin@amd.com>
2023-04-03 13:49:24 -0700

    MI-400: require VGPR alignment
    Change-Id: I658ecbea77ea61785eafa61f3331dd9e7f960964

--- llvm/lib/Target/AMDGPU/GCNSubtarget.h
+++ llvm/lib/Target/AMDGPU/GCNSubtarget.h
@@ -1140,7 +1140,7 @@ public:
   bool hasVALUMaskWriteHazard() const { return getGeneration() == GFX11; }
 
   /// Return if operations acting on VGPR tuples require even alignment.
-  bool needsAlignedVGPRs() const { return GFX90AInsts; }
+  bool needsAlignedVGPRs() const { return GFX90AInsts || GFX12_10Insts; }
 
   /// Return true if the target has the S_PACK_HL_B32_B16 instruction.
   bool hasSPackHL() const { return GFX11Insts; }
