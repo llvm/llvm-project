6bc2bb6b78d82b6094eb804ac16258fb8b3cfdbe
Quinn Dawkins <quinn@nod-labs.com>
2023-04-06 16:45:59 -0400

    [mlir][vector] Add pattern to break down vector.bitcast
    The pattern added here is intended as a last resort for targets like
    SPIR-V where there are vector size restrictions and we need to be able
    to break down large vector types. Vectorizing loads/stores for small
    bitwidths (e.g. i8) relies on bitcasting to a larger element type and
    patterns to bubble bitcast ops to where they can cancel.
    This fails for cases such as
    ```
    %1 = arith.trunci %0 : vector<2x32xi32> to vector<2x32xi8>
    vector.transfer_write %1, %destination[%c0, %c0] {in_bounds = [true, true]} : vector<2x32xi8>, memref<2x32xi8>
    ```
    where the `arith.trunci` op essentially does the job of one of the
    bitcasts, leading to a bitcast that need to be further broken down
    ```
    vector.bitcast %0 : vector<16xi8> to vector<4xi32>
    ```
    
    Differential Revision: https://reviews.llvm.org/D149065

--- mlir/include/mlir/Dialect/Vector/Transforms/VectorRewritePatterns.h
+++ mlir/include/mlir/Dialect/Vector/Transforms/VectorRewritePatterns.h
@@ -181,6 +181,22 @@ void populateVectorExtractStridedSliceToExtractInsertChainPatterns(
     std::function<bool(ExtractStridedSliceOp)> controlFn = nullptr,
     PatternBenefit benefit = 1);
 
+/// Populate `patterns` with a pattern to break down 1-D vector.bitcast ops
+/// based on the destination vector shape. Bitcasts from a lower bitwidth
+/// element type to a higher bitwidth one are extracted from the lower bitwidth
+/// based on the native destination vector shape and inserted based on the ratio
+/// of the bitwidths.
+///
+/// This acts as a last resort way to break down vector.bitcast ops to smaller
+/// vector sizes. Because this pattern composes until it is bitcasting to a
+/// single element of the higher bitwidth, the is an optional control function.
+/// If `controlFn` is not nullptr, the pattern will only apply to ops where
+/// `controlFn` returns true, otherwise applies to all bitcast ops.
+void populateBreakDownVectorBitCastOpPatterns(
+    RewritePatternSet &patterns,
+    std::function<bool(BitCastOp)> controlFn = nullptr,
+    PatternBenefit benefit = 1);
+
 /// Populate `patterns` with the following patterns.
 ///
 /// Patterns in populateVectorInsertExtractStridedSliceDecompositionPatterns();
