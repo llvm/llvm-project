DragonBuild/llvm-logos

[clang][cas] include-tree support for submodule visibility
Track the submodule of each modular header and enter the submodule via
PPCachedActions. This fixes declaration and macro header visibility.

clang/include/clang/CAS/IncludeTree.h
@@ -69,10 +69,31 @@ public:
   Expected<FileInfo> getBaseFileInfo();
 
   SrcMgr::CharacteristicKind getFileCharacteristic() const {
-    return (SrcMgr::CharacteristicKind)dataSkippingIncludes().front();
+    return (SrcMgr::CharacteristicKind)(getData().front() & ~IsSubmoduleBit);
   }
 
-  size_t getNumIncludes() const { return getNumReferences() - 1; }
+  bool isSubmodule() const { return (getData().front() & IsSubmoduleBit) != 0; }
+
+  std::optional<ObjectRef> getSubmoduleRef() const {
+    if (isSubmodule())
+      return getReference(getNumReferences() - 1);
+    return std::nullopt;
+  }
+
+  /// If \c getBaseFile() is a modular header, get its submodule name.
+  Expected<std::optional<StringRef>> getSubmoduleName() {
+    auto Ref = getSubmoduleRef();
+    if (!Ref)
+      return std::nullopt;
+    auto Node = getCAS().getProxy(*Ref);
+    if (!Node)
+      return Node.takeError();
+    return Node->getData();
+  }
+
+  size_t getNumIncludes() const {
+    return getNumReferences() - (isSubmodule() ? 2 : 1);
+  };
 
   ObjectRef getIncludeRef(size_t I) const {
     assert(I < getNumIncludes());
@@ -131,7 +152,7 @@ public:
   static Expected<IncludeTree>
   create(ObjectStore &DB, SrcMgr::CharacteristicKind FileCharacteristic,
          ObjectRef BaseFile, ArrayRef<IncludeInfo> Includes,
-         llvm::SmallBitVector Checks);
+         std::optional<ObjectRef> SubmoduleName, llvm::SmallBitVector Checks);
 
   static Expected<IncludeTree> get(ObjectStore &DB, ObjectRef Ref);
 
@@ -140,6 +161,8 @@ public:
 private:
   friend class IncludeTreeBase<IncludeTree>;
   friend class IncludeTreeRoot;
+
+  static constexpr char IsSubmoduleBit = 0x80;
 
   explicit IncludeTree(ObjectProxy Node) : IncludeTreeBase(std::move(Node)) {
     assert(isValid(*this));
@@ -154,8 +177,10 @@ private:
 
   Expected<Node> getIncludeNode(ObjectRef Ref, NodeKind Kind);
 
+  StringRef dataSkippingFlags() const { return getData().drop_front(); }
   StringRef dataSkippingIncludes() const {
-    return getData().drop_front(getNumIncludes() * (sizeof(uint32_t) + 1));
+    return dataSkippingFlags().drop_front(getNumIncludes() *
+                                          (sizeof(uint32_t) + 1));
   }
 
   static bool isValid(const ObjectProxy &Node);

clang/lib/Frontend/IncludeTreePPActions.cpp
@@ -106,6 +106,11 @@ public:
       return std::nullopt;
     };
 
+    auto reportErrorTwine = [&](const llvm::Twine &T) -> std::nullopt_t {
+      return reportError(
+          llvm::createStringError(llvm::inconvertibleErrorCode(), T));
+    };
+
     Expected<cas::IncludeTree::Node> Node =
         IncludeInfo.Tree.getIncludeNode(IncludeInfo.CurIncludeIndex++);
     if (!Node)
@@ -142,7 +147,29 @@ public:
     PP.markIncluded(*FE);
     IncludeStack.push_back(
         {std::move(EnteredTree), SM.getLocForStartOfFile(FID)});
-    return IncludeFile{FID};
+
+    Module *M = nullptr;
+    auto SubmoduleName = EnteredTree.getSubmoduleName();
+    if (!SubmoduleName)
+      return reportError(SubmoduleName.takeError());
+    if (*SubmoduleName) {
+      SmallVector<StringRef> ModuleComponents;
+      (*SubmoduleName)->split(ModuleComponents, '.');
+      M = PP.getHeaderSearchInfo().lookupModule(
+          ModuleComponents[0], IncludeLoc,
+          /*AllowSearch=*/false, /*AllowExtraModuleMapSearch=*/false);
+      if (!M)
+        return reportErrorTwine(llvm::Twine("failed to find module '") +
+                                ModuleComponents[0] + "'");
+      for (StringRef Sub : ArrayRef(ModuleComponents).drop_front()) {
+        M = M->findOrInferSubmodule(Sub);
+        if (!M)
+          return reportErrorTwine(
+              llvm::Twine("failed to find or infer submodule '") + Sub + "'");
+      }
+    }
+
+    return IncludeFile{FID, M};
   }
 
   void exitedFile(Preprocessor &PP, FileID FID) override {

llvm/llvm-project

(cherry picked from commit bd61c2c82c59f703f157d4129f05aa36b5837ab9)
(cherry picked from commit d926cee9331681166732cd90f36363b28a79486e)

clang/include/clang/CAS/IncludeTree.h
@@ -69,10 +69,31 @@ public:
   Expected<FileInfo> getBaseFileInfo();
 
   SrcMgr::CharacteristicKind getFileCharacteristic() const {
-    return (SrcMgr::CharacteristicKind)dataSkippingIncludes().front();
+    return (SrcMgr::CharacteristicKind)(getData().front() & ~IsSubmoduleBit);
   }
 
-  size_t getNumIncludes() const { return getNumReferences() - 1; }
+  bool isSubmodule() const { return (getData().front() & IsSubmoduleBit) != 0; }
+
+  std::optional<ObjectRef> getSubmoduleNameRef() const {
+    if (isSubmodule())
+      return getReference(getNumReferences() - 1);
+    return std::nullopt;
+  }
+
+  /// If \c getBaseFile() is a modular header, get its submodule name.
+  Expected<std::optional<StringRef>> getSubmoduleName() {
+    auto Ref = getSubmoduleNameRef();
+    if (!Ref)
+      return std::nullopt;
+    auto Node = getCAS().getProxy(*Ref);
+    if (!Node)
+      return Node.takeError();
+    return Node->getData();
+  }
+
+  size_t getNumIncludes() const {
+    return getNumReferences() - (isSubmodule() ? 2 : 1);
+  };
 
   ObjectRef getIncludeRef(size_t I) const {
     assert(I < getNumIncludes());
@@ -131,7 +152,7 @@ public:
   static Expected<IncludeTree>
   create(ObjectStore &DB, SrcMgr::CharacteristicKind FileCharacteristic,
          ObjectRef BaseFile, ArrayRef<IncludeInfo> Includes,
-         llvm::SmallBitVector Checks);
+         std::optional<ObjectRef> SubmoduleName, llvm::SmallBitVector Checks);
 
   static Expected<IncludeTree> get(ObjectStore &DB, ObjectRef Ref);
 
@@ -140,6 +161,8 @@ public:
 private:
   friend class IncludeTreeBase<IncludeTree>;
   friend class IncludeTreeRoot;
+
+  static constexpr char IsSubmoduleBit = 0x80;
 
   explicit IncludeTree(ObjectProxy Node) : IncludeTreeBase(std::move(Node)) {
     assert(isValid(*this));
@@ -154,8 +177,10 @@ private:
 
   Expected<Node> getIncludeNode(ObjectRef Ref, NodeKind Kind);
 
+  StringRef dataSkippingFlags() const { return getData().drop_front(); }
   StringRef dataSkippingIncludes() const {
-    return getData().drop_front(getNumIncludes() * (sizeof(uint32_t) + 1));
+    return dataSkippingFlags().drop_front(getNumIncludes() *
+                                          (sizeof(uint32_t) + 1));
   }
 
   static bool isValid(const ObjectProxy &Node);

clang/lib/Frontend/IncludeTreePPActions.cpp
@@ -106,6 +106,11 @@ public:
       return {};
     };
 
+    auto reportErrorTwine = [&](const llvm::Twine &T) -> std::monostate {
+      return reportError(
+          llvm::createStringError(llvm::inconvertibleErrorCode(), T));
+    };
+
     Expected<cas::IncludeTree::Node> Node =
         IncludeInfo.Tree.getIncludeNode(IncludeInfo.CurIncludeIndex++);
     if (!Node)
@@ -142,7 +147,29 @@ public:
     PP.markIncluded(*FE);
     IncludeStack.push_back(
         {std::move(EnteredTree), SM.getLocForStartOfFile(FID)});
-    return IncludeFile{FID};
+
+    Module *M = nullptr;
+    auto SubmoduleName = EnteredTree.getSubmoduleName();
+    if (!SubmoduleName)
+      return reportError(SubmoduleName.takeError());
+    if (*SubmoduleName) {
+      SmallVector<StringRef> ModuleComponents;
+      (*SubmoduleName)->split(ModuleComponents, '.');
+      M = PP.getHeaderSearchInfo().lookupModule(
+          ModuleComponents[0], IncludeLoc,
+          /*AllowSearch=*/false, /*AllowExtraModuleMapSearch=*/false);
+      if (!M)
+        return reportErrorTwine(llvm::Twine("failed to find module '") +
+                                ModuleComponents[0] + "'");
+      for (StringRef Sub : ArrayRef(ModuleComponents).drop_front()) {
+        M = M->findOrInferSubmodule(Sub);
+        if (!M)
+          return reportErrorTwine(
+              llvm::Twine("failed to find or infer submodule '") + Sub + "'");
+      }
+    }
+
+    return IncludeFile{FID, M};
   }
 
   void exitedFile(Preprocessor &PP, FileID FID) override {