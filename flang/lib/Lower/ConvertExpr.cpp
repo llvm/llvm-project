d453af5675aa18734485ae72fdea506ad6da4118
Valentin Clement <clementval@gmail.com>
2023-04-10 12:19:26 -0700

    [flang][NFC] Fix invalid op result access
    Fix invalid op result access. This will trigger
    assertion introduced in D147883.
    
    Reviewed By: frgossen
    
    Differential Revision: https://reviews.llvm.org/D147959

--- flang/lib/Lower/ConvertExpr.cpp
+++ flang/lib/Lower/ConvertExpr.cpp
@@ -4276,7 +4276,9 @@ private:
     // Put the implicit loop variables in row to column order to match FIR's
     // Ops. (The loops were constructed from outermost column to innermost
     // row.)
-    mlir::Value outerRes = loops[0].getResult(0);
+    mlir::Value outerRes;
+    if (loops[0].getNumResults() != 0)
+      outerRes = loops[0].getResult(0);
     return {IterationSpace(innerArg, outerRes, llvm::reverse(ivars)),
             afterLoopNest};
   }
