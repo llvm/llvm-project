c471f2955c9495a9a112417f1d1835f797eccb88
Vitaly Buka <vitalybuka@google.com>
2023-04-11 15:32:22 -0700

    [lsan] Regression test for 18f86a8d30ff

--- compiler-rt/test/lsan/TestCases/lsan_annotations.cpp
+++ compiler-rt/test/lsan/TestCases/lsan_annotations.cpp
@@ -5,11 +5,14 @@
 #include <sanitizer/lsan_interface.h>
 #include <stdlib.h>
 
-int *x, *y;
+int *x, *y, *z;
 
 int main() {
   x = new int;
   __lsan_ignore_object(x);
+
+  z = new int[1000000];  // Large enough for the secondary allocator.
+  __lsan_ignore_object(z);
 
   {
     __lsan::ScopedDisabler disabler;
