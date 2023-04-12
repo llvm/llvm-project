3c83aeee6b7d5e9ac5de1e29605ee57c2b800ca1
Vitaly Buka <vitalybuka@google.com>
2023-04-12 12:23:30 -0700

    [test][tsan] Attempt to fix darwing after D147337

--- compiler-rt/test/tsan/debugging.cpp
+++ compiler-rt/test/tsan/debugging.cpp
@@ -9,23 +9,6 @@
 
 #include "test.h"
 
-extern "C" {
-void __tsan_on_report(void *report);
-void *__tsan_get_current_report();
-int __tsan_get_report_data(void *report, const char **description, int *count,
-                           int *stack_count, int *mop_count, int *loc_count,
-                           int *mutex_count, int *thread_count,
-                           int *unique_tid_count, void **sleep_trace,
-                           unsigned long trace_size);
-int __tsan_get_report_mop(void *report, unsigned long idx, int *tid,
-                          void **addr, int *size, int *write, int *atomic,
-                          void **trace, unsigned long trace_size);
-int __tsan_get_report_thread(void *report, unsigned long idx, int *tid,
-                             uint64_t *os_id, int *running,
-                             const char **name, int *parent_tid, void **trace,
-                             unsigned long trace_size);
-}
-
 long my_global;
 
 void *Thread(void *a) {
