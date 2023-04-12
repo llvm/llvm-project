9638da200e00bd069e6dd63604e14cbafede9324
Noah Goldstein <goldstein.w.n@gmail.com>
2023-04-12 18:37:01 -0500

    [LIBC] Fix comments / name of __sched_cpu_count tests
    Test was incorrectly named/commented after the sched_{set|get}affinity
    functions.
    
    Reviewed By: michaelrj
    
    Differential Revision: https://reviews.llvm.org/D148044

--- libc/test/src/sched/cpu_count_test.cpp
+++ libc/test/src/sched/cpu_count_test.cpp
@@ -1,4 +1,4 @@
-//===-- Unittests for sched_getaffinity and sched_setaffinity -------------===//
+//===-- Unittests for __sched_cpu_count -----------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -15,7 +15,7 @@
 #include <sched.h>
 #include <sys/syscall.h>
 
-TEST(LlvmLibcSchedAffinityTest, SmokeTest) {
+TEST(LlvmLibcSchedCpuCountTest, SmokeTest) {
   cpu_set_t mask;
   libc_errno = 0;
   using __llvm_libc::testing::ErrnoSetterMatcher::Succeeds;
