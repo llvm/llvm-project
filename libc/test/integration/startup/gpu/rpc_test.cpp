0bd564a259e1647343e3137abe6b618d7ad09c18
Joseph Huber <jhuber6@vols.utk.edu>
2023-04-14 09:24:00 -0500

    [libc] Add a test to directly stimulate the RPC interface
    Currently, the RPC interface with the loader is only tested if the other
    tests fail. This test adds a direct test that runs a simple integer
    increment over the RPC handshake 10000 times.
    
    Depends on https://reviews.llvm.org/D148288
    
    Reviewed By: lntue
    
    Differential Revision: https://reviews.llvm.org/D148342

---
+++ libc/test/integration/startup/gpu/rpc_test.cpp
@@ -0,0 +1,34 @@
+//===-- Loader test to check the RPC interface with the loader ------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "src/__support/RPC/rpc_client.h"
+#include "test/IntegrationTest/test.h"
+
+using namespace __llvm_libc;
+
+static void test_add_simple() {
+  constexpr int num_additions = 10000;
+  uint64_t cnt = 0;
+  for (int i = 0; i < num_additions; ++i) {
+    rpc::Port port = rpc::client.open(rpc::TEST_INCREMENT);
+    port.send_and_recv(
+        [=](rpc::Buffer *buffer) {
+          reinterpret_cast<uint64_t *>(buffer->data)[0] = cnt;
+        },
+        [&](rpc::Buffer *buffer) {
+          cnt = reinterpret_cast<uint64_t *>(buffer->data)[0];
+        });
+    port.close();
+  }
+  ASSERT_TRUE(cnt == num_additions && "Incorrect sum");
+}
+
+TEST_MAIN(int argc, char **argv, char **envp) {
+  test_add_simple();
+  return 0;
+}
