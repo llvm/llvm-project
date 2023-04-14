1c261e360f558a914b9eafb22423f893f5dc54de
Michael Jones <michaelrj@google.com>
2023-04-14 15:36:39 -0700

    [libc] Add implementation of getchar
    added getchar and getchar_unlocked which are just wrappers getc and getc_unlocked respectively.
    
    Reviewed By: sivachandra, lntue, michaelrj
    
    Differential Revision: https://reviews.llvm.org/D147919

---
+++ libc/src/stdio/getchar.h
@@ -0,0 +1,18 @@
+//===-- Implementation header of getchar ------------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_LIBC_SRC_STDIO_GETCHAR_H
+#define LLVM_LIBC_SRC_STDIO_GETCHAR_H
+
+namespace __llvm_libc {
+
+int getchar();
+
+} // namespace __llvm_libc
+
+#endif // LLVM_LIBC_SRC_STDIO_GETCHAR_H
