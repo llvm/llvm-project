1bd1fddc8358a0eaa566cd435d6de4f3264301e3
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2023-04-13 22:57:47 -0700

    [CIR] Add #cir.const_struct attribute to handle global constant structs

--- clang/lib/CIR/Dialect/IR/CIRAttrs.cpp
+++ clang/lib/CIR/Dialect/IR/CIRAttrs.cpp
@@ -19,12 +19,19 @@
 #include "mlir/IR/BuiltinAttributeInterfaces.h"
 #include "mlir/IR/BuiltinTypes.h"
 #include "mlir/IR/DialectImplementation.h"
+#include "mlir/IR/OpImplementation.h"
 
 #include "llvm/ADT/STLExtras.h"
 #include "llvm/ADT/TypeSwitch.h"
 
 // ClangIR holds back AST references when available.
 #include "clang/AST/Decl.h"
+
+static void printConstStructMembers(mlir::AsmPrinter &p, mlir::Type type,
+                                    mlir::ArrayAttr members);
+static mlir::ParseResult parseConstStructMembers(::mlir::AsmParser &parser,
+                                                 mlir::Type &type,
+                                                 mlir::ArrayAttr &members);
 
 #define GET_ATTRDEF_CLASSES
 #include "clang/CIR/Dialect/IR/CIROpsAttributes.cpp.inc"
@@ -52,6 +59,41 @@ Attribute CIRDialect::parseAttribute(DialectAsmParser &parser,
 void CIRDialect::printAttribute(Attribute attr, DialectAsmPrinter &os) const {
   if (failed(generatedAttributePrinter(attr, os)))
     llvm_unreachable("unexpected CIR type kind");
+}
+
+static void printConstStructMembers(mlir::AsmPrinter &p, mlir::Type type,
+                                    mlir::ArrayAttr members) {
+  p << members;
+}
+
+static ParseResult parseConstStructMembers(::mlir::AsmParser &parser,
+                                           mlir::Type &type,
+                                           mlir::ArrayAttr &members) {
+  SmallVector<mlir::Attribute, 4> elts;
+  SmallVector<mlir::Type, 4> tys;
+  if (parser
+          .parseCommaSeparatedList(
+              AsmParser::Delimiter::Braces,
+              [&]() {
+                Attribute attr;
+                if (parser.parseAttribute(attr).succeeded()) {
+                  elts.push_back(attr);
+                  if (auto tyAttr = attr.dyn_cast<mlir::TypedAttr>()) {
+                    tys.push_back(tyAttr.getType());
+                    return success();
+                  }
+                  parser.emitError(parser.getCurrentLocation(),
+                                   "expected a typed attribute");
+                }
+                return failure();
+              })
+          .failed())
+    return failure();
+
+  auto *ctx = parser.getContext();
+  members = mlir::ArrayAttr::get(ctx, elts);
+  type = mlir::cir::StructType::get(ctx, tys, "", /*body=*/true);
+  return success();
 }
 
 //===----------------------------------------------------------------------===//
