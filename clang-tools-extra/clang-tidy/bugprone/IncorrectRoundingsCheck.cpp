7d707ca78eeaed82265ffa5362a34d08cebdb5d3
Piotr Zegar <me@piotrzegar.pl>
2023-04-15 08:34:41 +0000

    [clang-tidy] Add support for long double in bugprone-incorrect-roundings
    Support all floating point representations for an 0.5 floating const.
    Added missing tests.
    
    Depend on D147906.
    
    Reviewed By: carlosgalvezp
    
    Differential Revision: https://reviews.llvm.org/D147908

--- clang-tools-extra/clang-tidy/bugprone/IncorrectRoundingsCheck.cpp
+++ clang-tools-extra/clang-tidy/bugprone/IncorrectRoundingsCheck.cpp
@@ -23,12 +23,7 @@ static llvm::APFloat getHalf(const llvm::fltSemantics &Semantics) {
 
 namespace {
 AST_MATCHER(FloatingLiteral, floatHalf) {
-  const llvm::APFloat Literal = Node.getValue();
-  if ((&Node.getSemantics()) == &llvm::APFloat::IEEEsingle())
-    return Literal == getHalf(llvm::APFloat::IEEEsingle());
-  if ((&Node.getSemantics()) == &llvm::APFloat::IEEEdouble())
-    return Literal == getHalf(llvm::APFloat::IEEEdouble());
-  return false;
+  return Node.getValue() == getHalf(Node.getSemantics());
 }
 } // namespace
 
@@ -39,24 +34,15 @@ void IncorrectRoundingsCheck::registerMatchers(MatchFinder *MatchFinder) {
   // Match a floating point expression.
   auto FloatType = expr(hasType(realFloatingPointType()));
 
-  // Match a floating literal of 0.5 or a floating literal of 0.5 implicitly.
-  // cast to floating type.
-  auto FloatOrCastHalf =
-      anyOf(FloatHalf,
-            implicitCastExpr(FloatType, has(ignoringParenImpCasts(FloatHalf))));
-
-  // Match if either the LHS or RHS is a floating literal of 0.5 or a floating
-  // literal of 0.5 and the other is of type double or vice versa.
-  auto OneSideHalf = anyOf(allOf(hasLHS(FloatOrCastHalf), hasRHS(FloatType)),
-                           allOf(hasRHS(FloatOrCastHalf), hasLHS(FloatType)));
-
   // Find expressions of cast to int of the sum of a floating point expression
   // and 0.5.
   MatchFinder->addMatcher(
       traverse(TK_AsIs,
-               implicitCastExpr(hasImplicitDestinationType(isInteger()),
-                                ignoringParenCasts(binaryOperator(
-                                    hasOperatorName("+"), OneSideHalf)))
+               implicitCastExpr(
+                   hasImplicitDestinationType(isInteger()),
+                   ignoringParenCasts(binaryOperator(
+                       hasOperatorName("+"), hasOperands(FloatType, FloatType),
+                       hasEitherOperand(ignoringParenImpCasts(FloatHalf)))))
                    .bind("CastExpr")),
       this);
 }
