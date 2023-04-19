0b403fe337c9963c0e85cce597e037321ac8a8c0
Fangrui Song <i@maskray.me>
2023-04-19 13:15:33 -0700

    [Driver] Make -fsanitize=kcfi,function incompatible
    A -fsanitize=kcfi instrumented function has a special instruction/data
    before the function entry at a fixed offset.
    A -fsanitize=function instrumented function has special instruction/data
    after the function entry at a fixed offset (may change to *before* in D148665).
    
    The two instrumentations are not intended to be used together and will become
    incompatible after D148665.
    
    Reviewed By: samitolvanen
    
    Differential Revision: https://reviews.llvm.org/D148671

--- clang/lib/Driver/SanitizerArgs.cpp
+++ clang/lib/Driver/SanitizerArgs.cpp
@@ -517,7 +517,8 @@ SanitizerArgs::SanitizerArgs(const ToolChain &TC,
       std::make_pair(SanitizerKind::MemTag,
                      SanitizerKind::Address | SanitizerKind::KernelAddress |
                          SanitizerKind::HWAddress |
-                         SanitizerKind::KernelHWAddress)};
+                         SanitizerKind::KernelHWAddress),
+      std::make_pair(SanitizerKind::KCFI, SanitizerKind::Function)};
   // Enable toolchain specific default sanitizers if not explicitly disabled.
   SanitizerMask Default = TC.getDefaultSanitizers() & ~AllRemove;
 
