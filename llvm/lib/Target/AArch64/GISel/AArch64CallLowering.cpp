d45fae601067f03e8b4a5a59507ad3aaf7613ac4
NAKAMURA Takumi <geek4civic@gmail.com>
2023-04-23 21:46:09 +0900

    Move CodeGen/LowLevelType => CodeGen/LowLevelTypeUtils
    Before restoring `CodeGen/LowLevelType`, rename this to `LowLevelTypeUtils`.
    
    Differential Revision: https://reviews.llvm.org/D148768

--- llvm/lib/Target/AArch64/GISel/AArch64CallLowering.cpp
+++ llvm/lib/Target/AArch64/GISel/AArch64CallLowering.cpp
@@ -25,7 +25,7 @@
 #include "llvm/CodeGen/FunctionLoweringInfo.h"
 #include "llvm/CodeGen/GlobalISel/MachineIRBuilder.h"
 #include "llvm/CodeGen/GlobalISel/Utils.h"
-#include "llvm/CodeGen/LowLevelType.h"
+#include "llvm/CodeGen/LowLevelTypeUtils.h"
 #include "llvm/CodeGen/MachineBasicBlock.h"
 #include "llvm/CodeGen/MachineFrameInfo.h"
 #include "llvm/CodeGen/MachineFunction.h"
