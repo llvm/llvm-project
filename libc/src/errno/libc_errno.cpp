738ff7c6ce530970b478220009f1b3b347225f72
Joseph Huber <jhuber6@vols.utk.edu>
2023-04-24 18:28:54 -0500

    [libc] Ignore 'errno' on the GPU and support 'atoi'
    The 'errno' value is most likely not useful on the GPU and it prevents
    us from providing certain functions on the GPU that depend on it, like
    `atoi`. This patch makes the necessary changes to support `errno` by
    simple replacing it with a consumer class.
    
    Supporting `errno` on the GPU is possible in some aspects. The first
    approach would be to use a buffer of shared memory that has enough space
    for all threads. Another option would be to change code generation to
    support `thread_local` using `address_space(5)` memory allocated at
    kernel launch. The former could look like the following, which could be
    implemented in a later patch:
    
    ```
    template <typename T>
    using SharedBuffer = T[gpu::MAX_THREADS] [[clang::address_space(3)]];
    template <typename T> struct ErrnoSetter {
      constexpr ErrnoSetter(SharedBuffer<T> &storage) : storage(storage) {}
      SharedBuffer<T> &storage;
      void operator=(const T &val) { storage[gpu::get_thread_id()] = val; }
    };
    
    static SharedBuffer<int> thread_local_buffer [[clang::loader_uninitialized]];
    ErrnoSetter<int> __llvmlibc_internal_errno(thread_local_buffer);
    ```
    
    Reviewed By: sivachandra
    
    Differential Revision: https://reviews.llvm.org/D149107

--- libc/src/errno/libc_errno.cpp
+++ libc/src/errno/libc_errno.cpp
@@ -6,6 +6,8 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include "src/__support/macros/properties/architectures.h"
+
 namespace __llvm_libc {
 
 extern "C" {
@@ -18,9 +20,17 @@ extern "C" {
 // macro defined in LLVM libc's public errno.h header file.
 // TODO: Use a macro to distinguish full build and overlay build which can be
 //       used to exclude __llvmlibc_errno under overlay build.
+#ifdef LIBC_TARGET_ARCH_IS_GPU
+ErrnoConsumer __llvmlibc_errno;
+#else
 thread_local int __llvmlibc_errno;
+#endif // LIBC_TARGET_ARCH_IS_GPU
+#else
+#ifdef LIBC_TARGET_ARCH_IS_GPU
+ErrnoConsumer __llvmlibc_internal_errno;
 #else
 thread_local int __llvmlibc_internal_errno;
+#endif // LIBC_TARGET_ARCH_IS_GPU
 #endif
 } // extern "C"
 
