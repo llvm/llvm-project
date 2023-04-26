13f5579caeee2c75baf1249b9f64de54f7c361e2
Louis Dionne <ldionne.2@gmail.com>
2023-04-26 17:08:18 -0400

    [libc++] Make std::bind constexpr-friendly
    std::bind is supposed to be constexpr-friendly since C++20 and it was
    marked as such in our synopsis. However, the tests were not actually
    testing any of it and as it happens, std::bind was not really constexpr
    friendly. This fixes the issue and makes sure that at least some of the
    tests are running in constexpr mode.
    
    Some tests for std::bind check functions that return void, and those
    use global variables. These tests haven't been made constexpr-friendly,
    however the coverage added by this patch should be sufficient to get
    decent confidence.
    
    Differential Revision: https://reviews.llvm.org/D149295

--- libcxx/test/std/utilities/function.objects/bind/func.bind/func.bind.bind/PR23141.pass.cpp
+++ libcxx/test/std/utilities/function.objects/bind/func.bind/func.bind.bind/PR23141.pass.cpp
@@ -11,9 +11,9 @@
 // <functional>
 
 // template<CopyConstructible Fn, CopyConstructible... Types>
-//   unspecified bind(Fn, Types...);
+//   unspecified bind(Fn, Types...);    // constexpr since C++20
 // template<Returnable R, CopyConstructible Fn, CopyConstructible... Types>
-//   unspecified bind(Fn, Types...);
+//   unspecified bind(Fn, Types...);    // constexpr since C++20
 
 // https://llvm.org/PR23141
 #include <functional>
@@ -21,18 +21,23 @@
 
 #include "test_macros.h"
 
-struct Fun
-{
+struct Fun {
   template<typename T, typename U>
-  void operator()(T &&, U &&) const
-  {
+  TEST_CONSTEXPR_CXX20 void operator()(T &&, U &&) const {
     static_assert(std::is_same<U, int &>::value, "");
   }
 };
 
-int main(int, char**)
-{
-    std::bind(Fun{}, std::placeholders::_1, 42)("hello");
+TEST_CONSTEXPR_CXX20 bool test() {
+  std::bind(Fun{}, std::placeholders::_1, 42)("hello");
+  return true;
+}
+
+int main(int, char**) {
+  test();
+#if TEST_STD_VER >= 20
+  static_assert(test());
+#endif
 
   return 0;
 }
