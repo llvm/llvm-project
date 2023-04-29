d88177ed7dbf85f3e03fb935c190b0b8b11e0fe5
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2023-04-28 19:59:14 -0700

    [CIR][CIRGen] Types: allow creation of atomics

---
+++ clang/test/CIR/CodeGen/atomic.cpp
@@ -0,0 +1,10 @@
+// RUN: %clang_cc1 -std=c++17 -triple x86_64-unknown-linux-gnu -fclangir -emit-cir %s -o %t.cir
+// RUN: FileCheck --input-file=%t.cir %s
+
+typedef struct _a {
+  _Atomic(int) d;
+} at;
+
+void m() { at y; }
+
+// CHECK: !ty_22struct2E_a22 = !cir.struct<"struct._a", i32>
