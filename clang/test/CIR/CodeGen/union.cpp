faaf64cc312d6bbd90017c30cb2e40d2d5f796de
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2023-04-28 20:17:12 -0700

    [CIR][CIRGen] Unions: layout and initial lowering
    - Teach CIRRecordLowering to use mlir::DataLayout.
    - Adds logic for lowerUnion().
    - Add testcase.

---
+++ clang/test/CIR/CodeGen/union.cpp
@@ -0,0 +1,20 @@
+// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fclangir -emit-cir %s -o %t.cir
+// RUN: FileCheck --input-file=%t.cir %s
+
+typedef struct { int x; } yolo;
+typedef union { yolo y; struct { int lifecnt; }; } yolm;
+typedef union { yolo y; struct { int lifecnt; int genpad; }; } yolm2;
+
+void m() {
+  yolm q;
+  yolm2 q2;
+}
+
+// CHECK: !ty_22struct2Eanon22 = !cir.struct<"struct.anon", i32, i32, #cir.recdecl.ast>
+// CHECK: !ty_22struct2Eyolo22 = !cir.struct<"struct.yolo", i32, #cir.recdecl.ast>
+// CHECK: !ty_22union2Eyolm22 = !cir.struct<"union.yolm", !ty_22struct2Eyolo22>
+// CHECK: !ty_22union2Eyolm222 = !cir.struct<"union.yolm2", !ty_22struct2Eanon22>
+
+// CHECK:   cir.func @_Z1mv() {
+// CHECK:   cir.alloca !ty_22union2Eyolm22, cir.ptr <!ty_22union2Eyolm22>, ["q"] {alignment = 4 : i64}
+// CHECK:   cir.alloca !ty_22union2Eyolm222, cir.ptr <!ty_22union2Eyolm222>, ["q2"] {alignment = 4 : i64}
