5017ddff21028d6a450108f663c90f18d2addb99
Joseph Huber <jhuber6@vols.utk.edu>
2023-05-01 08:17:39 -0500

    [libc] Change GPU startup and loader to use multiple kernels
    The GPU has a different execution model to standard `_start`
    implementations. On the GPU, all threads are active at the start of a
    kernel. In order to correctly intitialize and call the constructors we
    want single threaded semantics. Previously, this was done using a
    makeshift global barrier with atomics. However, it should be easier to
    simply put the portions of the code that must be single threaded in
    separate kernels and then call those with only one thread. Generally,
    mixing global state between kernel launches makes optimizations more
    difficult, similarly to calling a function outside of the TU, but for
    testing it is better to be correct.
    
    Depends on D149527 D148943
    
    Reviewed By: JonChesterfield
    
    Differential Revision: https://reviews.llvm.org/D149581

--- libc/utils/gpu/loader/Loader.h
+++ libc/utils/gpu/loader/Loader.h
@@ -23,6 +23,29 @@ struct LaunchParameters {
   uint32_t num_blocks_z;
 };
 
+/// The arguments to the '_begin' kernel.
+struct begin_args_t {
+  int argc;
+  void *argv;
+  void *envp;
+  void *inbox;
+  void *outbox;
+  void *buffer;
+};
+
+/// The arguments to the '_start' kernel.
+struct start_args_t {
+  int argc;
+  void *argv;
+  void *envp;
+  void *ret;
+};
+
+/// The arguments to the '_end' kernel.
+struct end_args_t {
+  int argc;
+};
+
 /// Generic interface to load the \p image and launch execution of the _start
 /// kernel on the target device. Copies \p argc and \p argv to the device.
 /// Returns the final value of the `main` function on the device.
