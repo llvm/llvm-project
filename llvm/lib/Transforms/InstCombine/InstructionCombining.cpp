6d667d4b261e81f325756fdfd5bb43b3b3d2451d
Yingwei Zheng <dtcxzyw2333@gmail.com>
2023-05-02 00:06:26 +0800

    [InstCombine] Combine const GEP chains
    This patch reverts rGae739aefd7473517d3f08b5c8d08a66c7f469198 to address performance regressions reported by our [CI](https://github.com/dtcxzyw/llvm-ci/issues/137) after rG2ec1d0f427c7822540352c0c14d057e7bfe4f77b.
    
    For example:
    ```
    define ptr @const_gep_chain(ptr %p, i64 %a) {
        %p1 = getelementptr inbounds i8, ptr %p, i64 %a
        %p2 = getelementptr inbounds i8, ptr %p1, i64 1
        %p3 = getelementptr inbounds i8, ptr %p2, i64 2
        %p4 = getelementptr inbounds i8, ptr %p3, i64 3
        ret ptr %p4
    }
    ```
    The last three GEPs will not be folded since rG2ec1d0f427c7822540352c0c14d057e7bfe4f77b.
    
    I think it is appropriate to remove this code because there is no compile-time regression reported in our benchmarks.
    
    Reviewed By: nikic
    
    Differential Revision: https://reviews.llvm.org/D149240

--- llvm/lib/Transforms/InstCombine/InstructionCombining.cpp
+++ llvm/lib/Transforms/InstCombine/InstructionCombining.cpp
@@ -1950,13 +1950,6 @@ Instruction *InstCombinerImpl::visitGEPOfGEP(GetElementPtrInst &GEP,
   if (!shouldMergeGEPs(*cast<GEPOperator>(&GEP), *Src))
     return nullptr;
 
-  // Note that if our source is a gep chain itself then we wait for that
-  // chain to be resolved before we perform this transformation.  This
-  // avoids us creating a TON of code in some cases.
-  if (auto *SrcGEP = dyn_cast<GEPOperator>(Src->getOperand(0)))
-    if (SrcGEP->getNumOperands() == 2 && shouldMergeGEPs(*Src, *SrcGEP))
-      return nullptr;   // Wait until our source is folded to completion.
-
   // For constant GEPs, use a more general offset-based folding approach.
   Type *PtrTy = Src->getType()->getScalarType();
   if (GEP.hasAllConstantIndices() &&
