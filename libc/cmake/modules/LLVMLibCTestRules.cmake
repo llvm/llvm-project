99e41d3d908c0c56c08ec8b78197ca667e4c42cb
Joseph Huber <jhuber6@vols.utk.edu>
2023-05-03 10:12:34 -0500

    [libc] Don't use '-nolibc' on the GPU build
    We previously changed this to use `nolibc` to allow it to link in
    compiler builtins for the CPU build. However, these options are unused
    on the GPU and create a lot of noise. Furthermore, we want to use
    `nogpulib` to prevent the linking in of the vendor libraries.
    
    Reviewed By: sivachandra
    
    Differential Revision: https://reviews.llvm.org/D149753

--- libc/cmake/modules/LLVMLibCTestRules.cmake
+++ libc/cmake/modules/LLVMLibCTestRules.cmake
@@ -658,7 +658,11 @@ function(add_libc_hermetic_test test_name)
   target_compile_options(${fq_build_target_name}
       PRIVATE ${LIBC_HERMETIC_TEST_COMPILE_OPTIONS} ${HERMETIC_TEST_COMPILE_OPTIONS})
 
-  target_link_options(${fq_build_target_name} PRIVATE -nolibc -nostartfiles -nostdlib++ -static)
+  if(LIBC_TARGET_ARCHITECTURE_IS_GPU)
+    target_link_options(${fq_build_target_name} PRIVATE -nostdlib -static)
+  else()
+    target_link_options(${fq_build_target_name} PRIVATE -nolibc -nostartfiles -nostdlib++ -static)
+  endif()
   target_link_libraries(
     ${fq_build_target_name}
     PRIVATE
