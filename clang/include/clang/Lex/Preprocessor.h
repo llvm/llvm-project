e46aa7f927293989dae40817c0087cba9cf5fae7
Zhouyi Zhou <zhouzhouyi@gmail.com>
2023-05-02 19:47:22 -0700

    Fix a typo in head comment of `CurPPLexer`.
    In head comment of CurPPLexer field of class Preprocessor,
    'The current top of the stack what we're lexing from' should be
    'The current top of the stack that we're lexing from'.
    
    Differential Revision: https://reviews.llvm.org/D149709

--- clang/include/clang/Lex/Preprocessor.h
+++ clang/include/clang/Lex/Preprocessor.h
@@ -729,7 +729,7 @@ private:
   /// Only one of CurLexer, or CurTokenLexer will be non-null.
   std::unique_ptr<Lexer> CurLexer;
 
-  /// The current top of the stack what we're lexing from
+  /// The current top of the stack that we're lexing from
   /// if not expanding a macro.
   ///
   /// This is an alias for CurLexer.
