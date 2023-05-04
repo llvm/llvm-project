fd9c13e039649998f3d6602b8e9aa5da6ff54b58
Nathan Lanza <nathanlanza@gmail.com>
2023-05-04 17:17:31 -0400

    [CIR][Rebase] Fix a typed vs untyped attr issue

--- clang/test/CIR/CodeGen/globals.cpp
+++ clang/test/CIR/CodeGen/globals.cpp
@@ -1,6 +1,5 @@
 // RUN: %clang_cc1 -std=c++17 -triple x86_64-unknown-linux-gnu -fclangir -emit-cir %s -o %t.cir
 // RUN: FileCheck --input-file=%t.cir %s
-// XFAIL: *
 
 int a = 3;
 const int b = 4; // unless used wont be generated
