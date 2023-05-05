b8d2f7177c39af7be371ba7f46cb00b9c63ef8f5
Kan Wu <kanwu@google.com>
2023-05-05 16:29:50 -0700

    [MemProf] Add hot allocation type
    Add "Hot" AllocationType (in addition to existing cold, notcold).
    
    Use lifetime access density as metric to identify hot allocations.
    Treat hot as notcold for MemProfContextDisambiguation for now
    before the disambiguation for "hot" is done.
    
    Reviewed By: tejohnson
    
    Differential Revision: https://reviews.llvm.org/D149932

--- llvm/lib/AsmParser/LLParser.cpp
+++ llvm/lib/AsmParser/LLParser.cpp
@@ -9979,7 +9979,7 @@ bool LLParser::parseMemProfs(std::vector<MIBInfo> &MIBs) {
 }
 
 /// AllocType
-///   := ('none'|'notcold'|'cold')
+///   := ('none'|'notcold'|'cold'|'hot')
 bool LLParser::parseAllocType(uint8_t &AllocType) {
   switch (Lex.getKind()) {
   case lltok::kw_none:
@@ -9990,6 +9990,9 @@ bool LLParser::parseAllocType(uint8_t &AllocType) {
     break;
   case lltok::kw_cold:
     AllocType = (uint8_t)AllocationType::Cold;
+    break;
+  case lltok::kw_hot:
+    AllocType = (uint8_t)AllocationType::Hot;
     break;
   default:
     return error(Lex.getLoc(), "invalid alloc type");
