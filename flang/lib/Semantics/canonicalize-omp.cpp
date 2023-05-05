76b304c9cd6690f3e94748b7ba8290fc79e507bf
Ethan Luis McDonough <ethanluismcdonough@gmail.com>
2023-05-05 14:53:08 -0500

    [flang] OpenMP allocate directive parse tree fix
    Addresses the same issue as the following abandoned revision: D104391.
    
    Rewrite leading declarative allocations so they are nested within their respective executable allocate directive
    
    Original:
    ```
    ExecutionPartConstruct -> OpenMPDeclarativeAllocate
    ExecutionPartConstruct -> OpenMPDeclarativeAllocate
    ExecutionPartConstruct -> OpenMPExecutableAllocate
    ```
    
    After rewriting:
    ```
    ExecutionPartConstruct -> OpenMPExecutableAllocate
    | ExecutionPartConstruct -> OpenMPDeclarativeAllocate
    | ExecutionPartConstruct -> OpenMPDeclarativeAllocate
    ```
    
    Co-authored-by: Isaac Perry <isaac.perry@arm.com>
    
    Reviewed By: kiranchandramohan
    
    Differential Revision: https://reviews.llvm.org/D148409

--- flang/lib/Semantics/canonicalize-omp.cpp
+++ flang/lib/Semantics/canonicalize-omp.cpp
@@ -15,7 +15,9 @@
 //   1. move structured DoConstruct and OmpEndLoopDirective into
 //      OpenMPLoopConstruct. Compilation will not proceed in case of errors
 //      after this pass.
-//   2. TBD
+//   2. Associate declarative OMP allocation directives with their
+//      respective executable allocation directive
+//   3. TBD
 namespace Fortran::semantics {
 
 using namespace parser::literals;
@@ -46,11 +48,22 @@ public:
     } // Block list
   }
 
+  void Post(parser::ExecutionPart &body) { RewriteOmpAllocations(body); }
+
 private:
   template <typename T> T *GetConstructIf(parser::ExecutionPartConstruct &x) {
     if (auto *y{std::get_if<parser::ExecutableConstruct>(&x.u)}) {
       if (auto *z{std::get_if<common::Indirection<T>>(&y->u)}) {
         return &z->value();
+      }
+    }
+    return nullptr;
+  }
+
+  template <typename T> T *GetOmpIf(parser::ExecutionPartConstruct &x) {
+    if (auto *construct{GetConstructIf<parser::OpenMPConstruct>(x)}) {
+      if (auto *omp{std::get_if<T>(&construct->u)}) {
+        return omp;
       }
     }
     return nullptr;
@@ -104,6 +117,36 @@ private:
     messages_.Say(dir.source,
         "A DO loop must follow the %s directive"_err_en_US,
         parser::ToUpperCaseLetters(dir.source.ToString()));
+  }
+
+  void RewriteOmpAllocations(parser::ExecutionPart &body) {
+    // Rewrite leading declarative allocations so they are nested
+    // within their respective executable allocate directive
+    //
+    // Original:
+    //   ExecutionPartConstruct -> OpenMPDeclarativeAllocate
+    //   ExecutionPartConstruct -> OpenMPDeclarativeAllocate
+    //   ExecutionPartConstruct -> OpenMPExecutableAllocate
+    //
+    // After rewriting:
+    //   ExecutionPartConstruct -> OpenMPExecutableAllocate
+    //     ExecutionPartConstruct -> OpenMPDeclarativeAllocate
+    //     ExecutionPartConstruct -> OpenMPDeclarativeAllocate
+    for (auto it = body.v.rbegin(); it != body.v.rend();) {
+      if (auto *exec = GetOmpIf<parser::OpenMPExecutableAllocate>(*(it++))) {
+        parser::OpenMPDeclarativeAllocate *decl;
+        std::list<parser::OpenMPDeclarativeAllocate> subAllocates;
+        while (it != body.v.rend() &&
+            (decl = GetOmpIf<parser::OpenMPDeclarativeAllocate>(*it))) {
+          subAllocates.push_front(std::move(*decl));
+          it = decltype(it)(body.v.erase(std::next(it).base()));
+        }
+        if (!subAllocates.empty()) {
+          std::get<std::optional<std::list<parser::OpenMPDeclarativeAllocate>>>(
+              exec->t) = {std::move(subAllocates)};
+        }
+      }
+    }
   }
 
   parser::Messages &messages_;
