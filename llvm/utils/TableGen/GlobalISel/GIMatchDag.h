6abcb44790f6400fb91d3f62f1b38849be4f71bd
Matt Arsenault <Matthew.Arsenault@amd.com>
2023-05-08 13:02:38 +0100

    TableGen: Fix missing C++ mode comments

--- llvm/utils/TableGen/GlobalISel/GIMatchDag.h
+++ llvm/utils/TableGen/GlobalISel/GIMatchDag.h
@@ -1,4 +1,4 @@
-//===- GIMatchDag.h - Represent a DAG to be matched -----------------------===//
+//===- GIMatchDag.h - Represent a DAG to be matched -------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
