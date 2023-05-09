92cc30aca78703174e89880aca85e5a5ff41cc98
Théo Degioanni <theo.degioanni@nextsilicon.com>
2023-05-09 09:01:33 +0000

    Reland "[mlir][mem2reg] Expose algorithm internals."
    This patch refactors the Mem2Reg infrastructure. It decouples
    analysis from promotion, allowing for more control over the execution of
    the logic. It also adjusts the interfaces to be less coupled to mem2reg
    and more general. This will be useful for an upcoming revision
    introducing generic SROA.
    
    This commit reverts f333977eb20a and relands 91cff8a71872.
    
    The original commit was reverted accidentally due to a misinterpretation
    of a bazel build bot failure.
    
    Reviewed By: gysit
    
    Differential Revision: https://reviews.llvm.org/D149825

--- mlir/lib/Dialect/MemRef/IR/MemRefMem2Reg.cpp
+++ mlir/lib/Dialect/MemRef/IR/MemRefMem2Reg.cpp
@@ -14,7 +14,6 @@
 #include "mlir/Dialect/Arith/IR/Arith.h"
 #include "mlir/Dialect/Complex/IR/Complex.h"
 #include "mlir/Dialect/MemRef/IR/MemRef.h"
-#include "mlir/Interfaces/Mem2RegInterfaces.h"
 #include "llvm/ADT/TypeSwitch.h"
 
 using namespace mlir;
