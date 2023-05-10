273e9a545fdb07b7996fe449b9ac224bf4b501f8
luxufan <luxufan@iscas.ac.cn>
2023-05-10 14:05:57 +0800

    Revert "Revert "[ValutTracking] Use isGuaranteedNotToBePoison in impliesPoison""
    This reverts commit cc42e9d75e7c3d197dad66377dbcf5d9a1c04590.

--- llvm/lib/Analysis/ValueTracking.cpp
+++ llvm/lib/Analysis/ValueTracking.cpp
@@ -6686,7 +6686,7 @@ static bool directlyImpliesPoison(const Value *ValAssumedPoison,
 
 static bool impliesPoison(const Value *ValAssumedPoison, const Value *V,
                           unsigned Depth) {
-  if (isGuaranteedNotToBeUndefOrPoison(ValAssumedPoison))
+  if (isGuaranteedNotToBePoison(ValAssumedPoison))
     return true;
 
   if (directlyImpliesPoison(ValAssumedPoison, V, /* Depth */ 0))
