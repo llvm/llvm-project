e19387e6936c9ccc6200b32f3affea7b1020664c
Jim Ingham <jingham@apple.com>
2023-05-10 17:48:48 -0700

    We can't let GetStackFrameCount get interrupted or it will give the wrong answer. Plus, it's useful in some places to have a way to force the full stack to be created even in the face of interruption. Moreover, most of the time when you're just getting frames, you don't need to know the number of frames in the stack to start with. You just keep calling Thread::GetStackFrameAtIndex(index++) and when you get a null StackFrameSP back, you're done. That's also more amenable to interruption if you are doing some work frame by frame.
    So this patch makes GetStackFrameCount always return the full count,
    suspending interruption. I also went through all the places that use
    GetStackFrameCount to make sure that they really needed the full stack
    walk. In many cases, they did not. For instance frame select -r 10 was
    getting the number of frames just to check whether cur_frame_idx + 10
    was within the stack. It's better in that case to see if that frame
    exists first, since that doesn't force a full stack walk, and only
    deal with walking off the end of the stack if it doesn't...
    
    I also added a test for some of these behaviors.
    
    Differential Revision: https://reviews.llvm.org/D150236

---
+++ lldb/test/API/functionalities/bt-interrupt/TestInterruptBacktrace.py
@@ -0,0 +1,49 @@
+"""
+Ensure that when the interrupt is raised we still make frame 0.
+and make sure "GetNumFrames" isn't interrupted.
+"""
+
+import lldb
+import lldbsuite.test.lldbutil as lldbutil
+from lldbsuite.test.lldbtest import *
+
+
+class TestInterruptingBacktrace(TestBase):
+
+    NO_DEBUG_INFO_TESTCASE = True
+
+    def test_backtrace_interrupt(self):
+        """Use RequestInterrupt followed by stack operations
+           to ensure correct interrupt behavior for stacks."""
+        self.build()
+        self.main_source_file = lldb.SBFileSpec("main.c")
+        self.bt_interrupt_test()
+
+    def bt_interrupt_test(self):
+        (target, process, thread, bkpt) = lldbutil.run_to_source_breakpoint(self,
+                                   "Set a breakpoint here", self.main_source_file)
+
+        # Now continue, and when we stop we will have crashed.
+        process.Continue()
+        self.dbg.RequestInterrupt()
+
+        # Be sure to turn this off again:
+        def cleanup ():
+            if self.dbg.InterruptRequested():
+                self.dbg.CancelInterruptRequest()
+        self.addTearDownHook(cleanup)
+    
+        frame_0 = thread.GetFrameAtIndex(0)
+        self.assertTrue(frame_0.IsValid(), "Got a good 0th frame")
+        # The interrupt flag is up already, so any attempt to backtrace
+        # should be cut short:
+        frame_1 = thread.GetFrameAtIndex(1)
+        self.assertFalse(frame_1.IsValid(), "Prevented from getting more frames")
+        # Since GetNumFrames is a contract, we don't interrupt it:
+        num_frames = thread.GetNumFrames()
+        print(f"Number of frames: {num_frames}")
+        self.assertGreater(num_frames, 1, "Got many frames")
+        
+        self.dbg.CancelInterruptRequest()
+
+        
