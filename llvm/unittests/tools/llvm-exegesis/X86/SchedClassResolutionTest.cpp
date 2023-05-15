930421e18c51c2e8eaf7ea92fcbfc4b1eabc5975
Francesco Petrogalli <francesco.petrogalli@apple.com>
2023-05-15 15:26:32 +0200

    [unittests][llvm-exegesis] Remove build warnings [NFCI]
    Remove the warning caused by a missing field initializer.
    
    The field is `StartAtCycle` of `struct MCWriteProcResEntry`.
    
    It has been set to the default `StartAtCycle = 0`.
    
    Differential Revision: https://reviews.llvm.org/D150569

--- llvm/unittests/tools/llvm-exegesis/X86/SchedClassResolutionTest.cpp
+++ llvm/unittests/tools/llvm-exegesis/X86/SchedClassResolutionTest.cpp
@@ -65,20 +65,20 @@ protected:
 
 TEST_F(X86SchedClassResolutionTest, ComputeIdealizedProcResPressure_2P0) {
   const auto Pressure =
-      computeIdealizedProcResPressure(STI.getSchedModel(), {{P0Idx, 2}});
+      computeIdealizedProcResPressure(STI.getSchedModel(), {{P0Idx, 2, 0}});
   EXPECT_THAT(Pressure, UnorderedElementsAre(Pair(P0Idx, 2.0)));
 }
 
 TEST_F(X86SchedClassResolutionTest, ComputeIdealizedProcResPressure_2P05) {
   const auto Pressure =
-      computeIdealizedProcResPressure(STI.getSchedModel(), {{P05Idx, 2}});
+      computeIdealizedProcResPressure(STI.getSchedModel(), {{P05Idx, 2, 0}});
   EXPECT_THAT(Pressure,
               UnorderedElementsAre(Pair(P0Idx, 1.0), Pair(P5Idx, 1.0)));
 }
 
 TEST_F(X86SchedClassResolutionTest, ComputeIdealizedProcResPressure_2P05_2P0156) {
   const auto Pressure = computeIdealizedProcResPressure(
-      STI.getSchedModel(), {{P05Idx, 2}, {P0156Idx, 2}});
+      STI.getSchedModel(), {{P05Idx, 2, 0}, {P0156Idx, 2, 0}});
   EXPECT_THAT(Pressure,
               UnorderedElementsAre(Pair(P0Idx, 1.0), Pair(P1Idx, 1.0),
                                    Pair(P5Idx, 1.0), Pair(P6Idx, 1.0)));
@@ -87,7 +87,7 @@ TEST_F(X86SchedClassResolutionTest, ComputeIdealizedProcResPressure_2P05_2P0156)
 TEST_F(X86SchedClassResolutionTest,
        ComputeIdealizedProcResPressure_1P1_1P05_2P0156) {
   const auto Pressure = computeIdealizedProcResPressure(
-      STI.getSchedModel(), {{P1Idx, 1}, {P05Idx, 1}, {P0156Idx, 2}});
+      STI.getSchedModel(), {{P1Idx, 1, 0}, {P05Idx, 1, 0}, {P0156Idx, 2, 0}});
   EXPECT_THAT(Pressure,
               UnorderedElementsAre(Pair(P0Idx, 1.0), Pair(P1Idx, 1.0),
                                    Pair(P5Idx, 1.0), Pair(P6Idx, 1.0)));
