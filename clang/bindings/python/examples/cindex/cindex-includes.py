dd3c26a045c081620375a878159f536758baba6e
Tobias Hieta <tobias@hieta.se>
2023-05-17 10:56:49 +0200

    [NFC][Py Reformat] Reformat python files in clang and clang-tools-extra
    This is an ongoing series of commits that are reformatting our
    Python code.
    
    Reformatting is done with `black`.
    
    If you end up having problems merging this commit because you
    have made changes to a python file, the best way to handle that
    is to run git checkout --ours <yourfile> and then reformat it
    with black.
    
    If you run into any problems, post to discourse about it and
    we will try to help.
    
    RFC Thread below:
    
    https://discourse.llvm.org/t/rfc-document-and-standardize-python-code-style
    
    Reviewed By: MatzeB
    
    Differential Revision: https://reviews.llvm.org/D150761

--- clang/bindings/python/examples/cindex/cindex-includes.py
+++ clang/bindings/python/examples/cindex/cindex-includes.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
 
-#===- cindex-includes.py - cindex/Python Inclusion Graph -----*- python -*--===#
+# ===- cindex-includes.py - cindex/Python Inclusion Graph -----*- python -*--===#
 #
 # Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 #
-#===------------------------------------------------------------------------===#
+# ===------------------------------------------------------------------------===#
 
 """
 A simple command line tool for dumping a Graphviz description (dot) that
 describes include dependencies.
 """
+
 
 def main():
     import sys
@@ -23,7 +24,7 @@ def main():
     parser.disable_interspersed_args()
     (opts, args) = parser.parse_args()
     if len(args) == 0:
-        parser.error('invalid number arguments')
+        parser.error("invalid number arguments")
 
     # FIXME: Add an output file option
     out = sys.stdout
@@ -36,22 +37,22 @@ def main():
     # A helper function for generating the node name.
     def name(f):
         if f:
-            return "\"" + f.name + "\""
+            return '"' + f.name + '"'
 
     # Generate the include graph
     out.write("digraph G {\n")
     for i in tu.get_includes():
-        line = "  ";
+        line = "  "
         if i.is_input_file:
             # Always write the input file as a node just in case it doesn't
             # actually include anything. This would generate a 1 node graph.
             line += name(i.include)
         else:
-            line += '%s->%s' % (name(i.source), name(i.include))
-        line += "\n";
+            line += "%s->%s" % (name(i.source), name(i.include))
+        line += "\n"
         out.write(line)
     out.write("}\n")
 
-if __name__ == '__main__':
-    main()
 
+if __name__ == "__main__":
+    main()
