9fffc2c27053708ffa73407f11a609143b62800b
Fangrui Song <i@maskray.me>
2023-05-17 12:43:49 -0700

    [Driver][gcov] Derive .gcno .gcda file names from -o and -dumpdir
    Resolve a FIXME.
    When -ftest-profile, -fprofile-arcs, or --coverage is specified and the
    driver performs both compilation and linking phases, derive the .gcno &
    .gcda file names from -o and -dumpdir.
    
    `clang --coverage d/a.c d/b.c -o e/x && e/x` will now emit
    `e/x-[ab].gc{no,da}` like GCC.
    
    For -fprofile-dir=, we make the deliberate decision to not mangle the
    input filename if relative.
    
    The following script demonstrates the .gcno and .gcda filenames.
    
    ```
    PATH=/tmp/Rel/bin:$PATH                # adapt according to your build directory
    mkdir -p d e f
    echo 'int main() {}' > d/a.c
    echo > d/b.c
    
    a() { rm $@ || exit 1; }
    
    clang --coverage d/a.c d/b.c && ./a.out
    a a-[ab].gc{no,da}
    clang --coverage d/a.c d/b.c -o e/x && e/x
    a e/x-[ab].gc{no,da}
    clang --coverage d/a.c d/b.c -o e/x -dumpdir f/ && e/x
    a f/[ab].gc{no,da}
    clang --coverage -fprofile-dir=f d/a.c d/b.c -o e/x && e/x
    a e/x-[ab].gcno f/e/x-[ab].gcda
    
    clang -c --coverage d/a.c d/b.c && clang --coverage a.o b.o && ./a.out
    a [ab].gc{no,da}
    clang -c --coverage -fprofile-dir=f d/a.c d/b.c && clang --coverage a.o b.o && ./a.out
    a [ab].gcno f/[ab].gcda
    
    clang -c --coverage d/a.c -o e/xa.o && clang --coverage e/xa.o && ./a.out
    a e/xa.gc{no,da}
    clang -c --coverage d/a.c -o e/xa.o -dumpdir f/g && clang --coverage e/xa.o && ./a.out
    a f/ga.gc{no,da}
    ```
    
    The gcov code accidentally claims -c and -S, so -fsyntax-only -c/-S and
    -E -c/-S don't lead to a -Wunused-command-line-argument warning. Keep
    the unintended code for now.

--- clang/lib/Driver/ToolChains/Clang.cpp
+++ clang/lib/Driver/ToolChains/Clang.cpp
@@ -705,10 +705,10 @@ static void addDashXForInput(const ArgList &Args, const InputInfo &Input,
 }
 
 static void addPGOAndCoverageFlags(const ToolChain &TC, Compilation &C,
-                                   const Driver &D, const InputInfo &Output,
+                                   const JobAction &JA, const InputInfo &Output,
                                    const ArgList &Args, SanitizerArgs &SanArgs,
                                    ArgStringList &CmdArgs) {
-
+  const Driver &D = TC.getDriver();
   auto *PGOGenerateArg = Args.getLastArg(options::OPT_fprofile_generate,
                                          options::OPT_fprofile_generate_EQ,
                                          options::OPT_fno_profile_generate);
@@ -911,32 +911,39 @@ static void addPGOAndCoverageFlags(const ToolChain &TC, Compilation &C,
       Args.hasArg(options::OPT_coverage))
     FProfileDir = Args.getLastArg(options::OPT_fprofile_dir);
 
-  // Put the .gcno and .gcda files (if needed) next to the object file or
-  // bitcode file in the case of LTO.
-  // FIXME: There should be a simpler way to find the object file for this
-  // input, and this code probably does the wrong thing for commands that
-  // compile and link all at once.
-  if ((Args.hasArg(options::OPT_c) || Args.hasArg(options::OPT_S)) &&
-      (EmitCovNotes || EmitCovData) && Output.isFilename()) {
-    SmallString<128> OutputFilename;
-    if (Arg *FinalOutput = C.getArgs().getLastArg(options::OPT__SLASH_Fo))
-      OutputFilename = FinalOutput->getValue();
-    else if (Arg *FinalOutput = C.getArgs().getLastArg(options::OPT_o))
-      OutputFilename = FinalOutput->getValue();
-    else
-      OutputFilename = llvm::sys::path::filename(Output.getBaseInput());
-    SmallString<128> CoverageFilename = OutputFilename;
-    if (llvm::sys::path::is_relative(CoverageFilename))
-      (void)D.getVFS().makeAbsolute(CoverageFilename);
-    llvm::sys::path::replace_extension(CoverageFilename, "gcno");
+  // TODO: Don't claim -c/-S to warn about -fsyntax-only -c/-S, -E -c/-S,
+  // like we warn about -fsyntax-only -E.
+  (void)(Args.hasArg(options::OPT_c) || Args.hasArg(options::OPT_S));
 
-    CmdArgs.push_back("-coverage-notes-file");
-    CmdArgs.push_back(Args.MakeArgString(CoverageFilename));
+  // Put the .gcno and .gcda files (if needed) next to the primary output file,
+  // or fall back to a file in the current directory for `clang -c --coverage
+  // d/a.c` in the absence of -o.
+  if (EmitCovNotes || EmitCovData) {
+    SmallString<128> CoverageFilename;
+    if (Arg *DumpDir = Args.getLastArgNoClaim(options::OPT_dumpdir)) {
+      // Form ${dumpdir}${basename}.gcno. Note that dumpdir may not end with a
+      // path separator.
+      CoverageFilename = DumpDir->getValue();
+      CoverageFilename += llvm::sys::path::filename(Output.getBaseInput());
+    } else if (Arg *FinalOutput =
+                   C.getArgs().getLastArg(options::OPT__SLASH_Fo)) {
+      CoverageFilename = FinalOutput->getValue();
+    } else if (Arg *FinalOutput = C.getArgs().getLastArg(options::OPT_o)) {
+      CoverageFilename = FinalOutput->getValue();
+    } else {
+      CoverageFilename = llvm::sys::path::filename(Output.getBaseInput());
+    }
+    llvm::sys::path::replace_extension(CoverageFilename, "gcno");
+    if (EmitCovNotes) {
+      CmdArgs.push_back("-coverage-notes-file");
+      CmdArgs.push_back(Args.MakeArgString(CoverageFilename));
+    }
 
     if (EmitCovData) {
       if (FProfileDir) {
+        SmallString<128> Gcno = std::move(CoverageFilename);
         CoverageFilename = FProfileDir->getValue();
-        llvm::sys::path::append(CoverageFilename, OutputFilename);
+        llvm::sys::path::append(CoverageFilename, Gcno);
       }
       llvm::sys::path::replace_extension(CoverageFilename, "gcda");
       CmdArgs.push_back("-coverage-data-file");
@@ -5760,7 +5767,7 @@ void Clang::ConstructJob(Compilation &C, const JobAction &JA,
   // for sampling, overhead of call arc collection is way too high and there's
   // no way to collect the output.
   if (!Triple.isNVPTX() && !Triple.isAMDGCN())
-    addPGOAndCoverageFlags(TC, C, D, Output, Args, SanitizeArgs, CmdArgs);
+    addPGOAndCoverageFlags(TC, C, JA, Output, Args, SanitizeArgs, CmdArgs);
 
   Args.AddLastArg(CmdArgs, options::OPT_fclang_abi_compat_EQ);
 
