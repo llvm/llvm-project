3b3912e9b8cbcba32f1deed81a0280e3141281e3
Dhruv Chawla <dhruv263.dc@gmail.com>
2023-05-17 15:12:11 +0200

    Reapply [SelectionDAG] Handle NSW for ADD/SUB in computeKnownBits()
    This exposed a miscompile due to incorrect flag preservation in
    integer type legalization, which has been fixed in D151472.
    
    -----
    
    This patch is a continuation of D150110. It separates the cases for
    ADD and SUB into their own cases so that computeForAddSub can be
    directly called and the NSW flag passed. This allows better
    optimization when the NSW flag is enabled, and allows fixing up the
    TODO that was there previously in SimplifyDemandedBits.
    
    Differential Revision: https://reviews.llvm.org/D150769

--- llvm/lib/CodeGen/SelectionDAG/SelectionDAG.cpp
+++ llvm/lib/CodeGen/SelectionDAG/SelectionDAG.cpp
@@ -3646,6 +3646,15 @@ KnownBits SelectionDAG::computeKnownBits(SDValue Op, const APInt &DemandedElts,
     // All bits are zero except the low bit.
     Known.Zero.setBitsFrom(1);
     break;
+  case ISD::ADD:
+  case ISD::SUB: {
+    SDNodeFlags Flags = Op.getNode()->getFlags();
+    Known = computeKnownBits(Op.getOperand(0), DemandedElts, Depth + 1);
+    Known2 = computeKnownBits(Op.getOperand(1), DemandedElts, Depth + 1);
+    Known = KnownBits::computeForAddSub(Op.getOpcode() == ISD::ADD,
+                                        Flags.hasNoSignedWrap(), Known, Known2);
+    break;
+  }
   case ISD::USUBO:
   case ISD::SSUBO:
   case ISD::USUBO_CARRY:
@@ -3659,7 +3668,6 @@ KnownBits SelectionDAG::computeKnownBits(SDValue Op, const APInt &DemandedElts,
       break;
     }
     [[fallthrough]];
-  case ISD::SUB:
   case ISD::SUBC: {
     assert(Op.getResNo() == 0 &&
            "We only compute knownbits for the difference here.");
@@ -3687,7 +3695,6 @@ KnownBits SelectionDAG::computeKnownBits(SDValue Op, const APInt &DemandedElts,
       break;
     }
     [[fallthrough]];
-  case ISD::ADD:
   case ISD::ADDC:
   case ISD::ADDE: {
     assert(Op.getResNo() == 0 && "We only compute knownbits for the sum here.");
