2043eacbece0492f2dcdd335b521c444ccc254cc
Erich Keane <erich.keane@intel.com>
2023-05-18 06:23:24 -0700

    Revert "[clang][AST] Print name instead of type when diagnosing uninitialized subobject in constexpr variables"
    This reverts commit f7654f21a402478e534034e2e866386cdb2eb134.
    
    This patch causes its assertion to fire when doing AST-dump, so
    reverting so that the author has time to fix this.

--- clang/test/CXX/dcl.dcl/dcl.spec/dcl.constexpr/p4.cpp
+++ clang/test/CXX/dcl.dcl/dcl.spec/dcl.constexpr/p4.cpp
@@ -360,7 +360,7 @@ namespace PR14503 {
   // The constructor is still 'constexpr' here, but the result is not intended
   // to be a constant expression. The standard is not clear on how this should
   // work.
-  constexpr V<int> v; // expected-error {{constant expression}} expected-note {{subobject 'y' is not initialized}}
+  constexpr V<int> v; // expected-error {{constant expression}} expected-note {{subobject of type 'int' is not initialized}}
 
   constexpr int k = V<int>().x; // FIXME: ok?
 }
