67cbe1b8597272165c2529f8a14ec571abe29490
Fangrui Song <i@maskray.me>
2023-05-19 07:59:37 -0700

    Allow -fsanitize=function on all targets
    Functions instrumented with -fsanitize=function have two words before
    the function label: a signature and a RTTI proxy.
    Instrumented call sites check the signature first to skip checks
    for uninstrumented callees.
    
    The code is generic and works for all targets supporting C++ RTTI.
    Change clangDriver to allow all targets. Add tests for Armv8.5
    Branch Target Identification and `-fpatchable-function-entry=`.
    
    Reviewed By: peter.smith
    
    Differential Revision: https://reviews.llvm.org/D148573

--- compiler-rt/test/ubsan/TestCases/TypeCheck/Function/lit.local.cfg.py
+++ compiler-rt/test/ubsan/TestCases/TypeCheck/Function/lit.local.cfg.py
@@ -1,3 +1,2 @@
-# The function type checker is only supported on x86 and x86_64 for now.
-if config.target_arch not in ['x86', 'x86_64']:
+if config.host_os not in ['Darwin', 'FreeBSD', 'Linux', 'NetBSD']:
   config.unsupported = True
