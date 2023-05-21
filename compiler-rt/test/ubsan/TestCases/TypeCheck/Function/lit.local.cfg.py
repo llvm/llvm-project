0533292be3053ede6e4160c560a61f29cc149927
Fangrui Song <i@maskray.me>
2023-05-21 07:33:11 -0700

    [ubsan][test] Unsupport function.cpp in Android armv7 on sanitizer-x86_64-linux-android
    The emulated environment has a spurious failure that causes many other
    tests to XFAIL. Unsupport it for now.
    
    ```
    1: CANNOT LINK EXECUTABLE "/data/local/tmp/Output/var/lib/buildbot/sanitizer-buildbot6/sanitizer-x86_64-linux-android/build/compiler_rt_build_android_arm/test/ubsan/Standalone-arm/TestCases/TypeCheck/Function/Output/function.cpp.tmp": library "/var/lib/buildbot/sanitizer-buildbot6/sanitizer-x86_64-linux-android/build/compiler_rt_build_android_arm/test/ubsan/Standalone-arm/TestCases/TypeCheck/Function/Output/function.cpp.tmp-so.so" not found: needed by main executable
    ```

--- compiler-rt/test/ubsan/TestCases/TypeCheck/Function/lit.local.cfg.py
+++ compiler-rt/test/ubsan/TestCases/TypeCheck/Function/lit.local.cfg.py
@@ -1,2 +1,5 @@
 if config.host_os not in ['Darwin', 'FreeBSD', 'Linux', 'NetBSD']:
   config.unsupported = True
+# Work around "library ... not found: needed by main executable" in qemu.
+if config.android and config.target_arch == 'arm':
+  config.unsupported = True
