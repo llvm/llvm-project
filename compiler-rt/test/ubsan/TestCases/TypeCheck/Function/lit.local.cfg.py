3b272039a88cc9952e5bad71b6b02a62b2c22098
Fangrui Song <i@maskray.me>
2023-05-21 08:50:34 -0700

    [ubsan][test] Unsupport function.cpp in Android non-x86 on sanitizer-x86_64-linux-android after D148573
    The qemu environment cannot run an executable with a link-time shared
    object built during the test?
    ```
    Input was:
    <<<<<<
                1: CANNOT LINK EXECUTABLE "/data/local/tmp/Output/var/lib/buildbot/sanitizer-buildbot6/sanitizer-x86_64-linux-android/build/compiler_rt_build_android_aarch64/test/ubsan/Standalone-aarch64/TestCases/TypeCheck/Function/Output/function.cpp.tmp": library "/var/lib/buildbot/sanitizer-buildbot6/sanitizer-x86_64-linux-android/build/compiler_rt_build_android_aarch64/test/ubsan/Standalone-aarch64/TestCases/TypeCheck/Function/Output/function.cpp.tmp-so.so" not found: needed by main executable
    ```

--- compiler-rt/test/ubsan/TestCases/TypeCheck/Function/lit.local.cfg.py
+++ compiler-rt/test/ubsan/TestCases/TypeCheck/Function/lit.local.cfg.py
@@ -1,5 +1,5 @@
 if config.host_os not in ['Darwin', 'FreeBSD', 'Linux', 'NetBSD']:
   config.unsupported = True
 # Work around "library ... not found: needed by main executable" in qemu.
-if config.android and config.target_arch == 'arm':
+if config.android and config.target_arch not in ['x86', 'x86_64']:
   config.unsupported = True
