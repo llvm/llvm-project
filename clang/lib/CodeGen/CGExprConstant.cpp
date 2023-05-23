7f0d730c14adbd4d07d01d7aefbcc14ad6ddd867
Akira Hatanaka <ahatanaka@apple.com>
2023-05-23 16:32:19 -0700

    [CodeGen] Fix the type of the constant that is used to zero-initialize a flexible array member
    A zero-element array type was incorrectly being used when an incomplete
    array was being initialized with a non-empty initializer.
    
    This fixes an assertion failure in AddInitializerToStaticVarDecl. See
    the discussion here: https://reviews.llvm.org/D123649#4362210
    
    Differential Revision: https://reviews.llvm.org/D151172

--- clang/lib/CodeGen/CGExprConstant.cpp
+++ clang/lib/CodeGen/CGExprConstant.cpp
@@ -2189,6 +2189,11 @@ llvm::Constant *ConstantEmitter::tryEmitPrivate(const APValue &Value,
 
     llvm::ArrayType *Desired =
         cast<llvm::ArrayType>(CGM.getTypes().ConvertType(DestType));
+
+    // Fix the type of incomplete arrays if the initializer isn't empty.
+    if (DestType->isIncompleteArrayType() && !Elts.empty())
+      Desired = llvm::ArrayType::get(Desired->getElementType(), Elts.size());
+
     return EmitArrayConstant(CGM, Desired, CommonElementType, NumElements, Elts,
                              Filler);
   }
