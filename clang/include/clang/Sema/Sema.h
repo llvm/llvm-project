ca9683651e52196f590bc244d9c506ed39fe1d92
Congcong Cai <congcongcai0907@163.com>
2023-05-23 22:14:10 +0200

    [Sema] `setInvalidDecl` for error deduction declaration
    Fixed: https://github.com/llvm/llvm-project/issues/62408
    `setInvalidDecl` for invalid `CXXDeductionGuideDecl` to
    avoid crashes during semantic analysis.
    
    Reviewed By: aaron.ballman
    
    Differential Revision: https://reviews.llvm.org/D149516

--- clang/include/clang/Sema/Sema.h
+++ clang/include/clang/Sema/Sema.h
@@ -7790,7 +7790,7 @@ public:
   void CheckConversionDeclarator(Declarator &D, QualType &R,
                                  StorageClass& SC);
   Decl *ActOnConversionDeclarator(CXXConversionDecl *Conversion);
-  void CheckDeductionGuideDeclarator(Declarator &D, QualType &R,
+  bool CheckDeductionGuideDeclarator(Declarator &D, QualType &R,
                                      StorageClass &SC);
   void CheckDeductionGuideTemplate(FunctionTemplateDecl *TD);
 
