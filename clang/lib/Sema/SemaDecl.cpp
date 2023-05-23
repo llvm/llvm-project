eb5902ffc97163338bab95d2fd84a953ee76e96f
Congcong Cai <congcongcai0907@163.com>
2023-05-23 09:07:03 +0200

    [Sema] `setInvalidDecl` for error deduction declaration
    Fixed: https://github.com/llvm/llvm-project/issues/62408
    `setInvalidDecl` for invalid `CXXDeductionGuideDecl` to
    avoid crashes during semantic analysis.
    
    Reviewed By: aaron.ballman
    
    Differential Revision: https://reviews.llvm.org/D149516

--- clang/lib/Sema/SemaDecl.cpp
+++ clang/lib/Sema/SemaDecl.cpp
@@ -9199,8 +9199,8 @@ static FunctionDecl *CreateNewFunctionDecl(Sema &SemaRef, Declarator &D,
       SemaRef.Diag(TrailingRequiresClause->getBeginLoc(),
                    diag::err_trailing_requires_clause_on_deduction_guide)
           << TrailingRequiresClause->getSourceRange();
-    SemaRef.CheckDeductionGuideDeclarator(D, R, SC);
-
+    if (SemaRef.CheckDeductionGuideDeclarator(D, R, SC))
+      return nullptr;
     return CXXDeductionGuideDecl::Create(SemaRef.Context, DC, D.getBeginLoc(),
                                          ExplicitSpecifier, NameInfo, R, TInfo,
                                          D.getEndLoc());
