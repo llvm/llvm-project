c6f71531bc3341d7b817b67c397f4bb3e0c73a3e
Tai Ly <tai.ly@arm.com>
2023-05-24 14:43:02 -0700

    [TOSA] Refactor TosaMakeBroadcastable pass
    This refactors and exposes EqualizeRanks utility function
    from within TosaMakeBroadcastable pass so it may be used to
    reshape operator inputs to equal ranks.
    
    Signed-off-by: Tai Ly <tai.ly@arm.com>
    
    Differential Revision: https://reviews.llvm.org/D150283

--- mlir/lib/Dialect/Tosa/Transforms/TosaMakeBroadcastable.cpp
+++ mlir/lib/Dialect/Tosa/Transforms/TosaMakeBroadcastable.cpp
@@ -14,6 +14,7 @@
 #include "mlir/Dialect/Tensor/IR/Tensor.h"
 #include "mlir/Dialect/Tosa/IR/TosaOps.h"
 #include "mlir/Dialect/Tosa/Transforms/Passes.h"
+#include "mlir/Dialect/Tosa/Utils/ConversionUtils.h"
 #include "mlir/Dialect/Tosa/Utils/QuantUtils.h"
 #include "mlir/Pass/Pass.h"
 #include "mlir/Transforms/GreedyPatternRewriteDriver.h"
@@ -28,60 +29,17 @@ namespace tosa {
 using namespace mlir;
 using namespace mlir::tosa;
 
-/// There are two potential ways implementing broadcast:
-/// a. https://www.tensorflow.org/xla/broadcasting#formal_definition
-/// b. https://numpy.org/doc/stable/user/basics.broadcasting.html
-/// This pass implements b (numpy style) now.
-
-/// In this pass, we insert RESHAPE operators to increase the rank of the
-/// lower rank operand as a first step in the broadcasting process. The TOSA
-/// operators that support broadcast require that the rank of the operands
-/// are equal.
-
-// Examples:
-// If lower=[c], higher=[a, b, c], [c] reshaped into [1, 1, c].
-// If lower=[b, c], higher=[a, b, c], [b, c] reshaped into [1, b, c].
-// If lower=[a], higher=[a, a], [a] reshaped into [1, a].
-// If lower=[a], target=[a, b, a], [a] reshaped into [1, 1, a].
-// If lower=[], target=[a, b, c], [] reshaped into [1, 1, 1].
-
-static LogicalResult
-computeReshapeOutput(ArrayRef<int64_t> higherRankShape,
-                     ArrayRef<int64_t> lowerRankShape,
-                     SmallVectorImpl<int64_t> &reshapeOutputShape) {
-  // Initialize new shapes with [1] * higherRank.
-  int64_t higherRank = higherRankShape.size();
-  int64_t lowerRank = lowerRankShape.size();
-
-  reshapeOutputShape.assign(higherRank, 1);
-
-  int64_t higherRankDim;
-  int64_t lowerRankDim;
-
-  for (int64_t i = higherRank - 1, j = lowerRank - 1; i >= 0 && j >= 0;
-       i--, j--) {
-    higherRankDim = higherRankShape[i];
-    lowerRankDim = lowerRankShape[j];
-
-    if (lowerRankDim == 1 && higherRankDim > 1)
-      reshapeOutputShape[i] = 1;
-    else if ((lowerRankDim > 1 && higherRankDim == 1) ||
-             (lowerRankDim == higherRankDim))
-      reshapeOutputShape[i] = lowerRankDim;
-    else if (higherRankDim != lowerRankDim)
-      return failure();
-  }
-  return success();
-}
+namespace {
 
 /// Common code to create the reshape op where necessary to make the rank of the
 /// operations equal. input1 and input2 will be updated when the rank has
 /// changed. The caller is expected to use these to rewrite the original
 /// operator with the RESHAPE now in the graph.
-static LogicalResult reshapeLowerToHigher(PatternRewriter &rewriter,
-                                          Location loc,
-                                          RankedTensorType outputType,
-                                          Value &input1, Value &input2) {
+/// return failure when (1) no reshape needed, or (2) output_type is specified
+/// and it has different rank
+LogicalResult reshapeLowerToHigher(PatternRewriter &rewriter, Location loc,
+                                   RankedTensorType outputType, Value &input1,
+                                   Value &input2) {
   auto input1Ty = dyn_cast<RankedTensorType>(input1.getType());
   auto input2Ty = dyn_cast<RankedTensorType>(input2.getType());
 
@@ -96,54 +54,28 @@ static LogicalResult reshapeLowerToHigher(PatternRewriter &rewriter,
     return rewriter.notifyMatchFailure(loc,
                                        "cannot rewrite as its already correct");
 
-  Value higherTensorValue, lowerTensorValue;
-  if (input1Rank > input2Rank) {
-    higherTensorValue = input1;
-    lowerTensorValue = input2;
-  } else {
-    higherTensorValue = input2;
-    lowerTensorValue = input1;
+  Value input1_copy = input1;
+  Value input2_copy = input2;
+  if (EqualizeRanks(rewriter, loc, input1_copy, input2_copy).failed()) {
+    return rewriter.notifyMatchFailure(loc, "failed to reshape inputs");
   }
-
-  ArrayRef<int64_t> higherRankShape =
-      cast<RankedTensorType>(higherTensorValue.getType()).getShape();
-  ArrayRef<int64_t> lowerRankShape =
-      cast<RankedTensorType>(lowerTensorValue.getType()).getShape();
-
-  SmallVector<int64_t, 4> reshapeOutputShape;
-
-  if (computeReshapeOutput(higherRankShape, lowerRankShape, reshapeOutputShape)
-          .failed())
-    return rewriter.notifyMatchFailure(loc, "fail to compute a reshape type");
-
-  auto reshapeInputType = cast<RankedTensorType>(lowerTensorValue.getType());
-  auto reshapeOutputType = RankedTensorType::get(
-      ArrayRef<int64_t>(reshapeOutputShape), reshapeInputType.getElementType());
 
   // Verify the rank agrees with the output type if the output type is ranked.
   if (outputType) {
-    if (outputType.getShape().size() != reshapeOutputShape.size() ||
-        outputType.getShape().size() != higherRankShape.size())
+    if (outputType.getRank() !=
+            input1_copy.getType().cast<RankedTensorType>().getRank() ||
+        outputType.getRank() !=
+            input2_copy.getType().cast<RankedTensorType>().getRank())
       return rewriter.notifyMatchFailure(
           loc, "the reshaped type doesn't agrees with the ranked output type");
   }
 
-  auto reshapeLower = rewriter.create<tosa::ReshapeOp>(
-      loc, reshapeOutputType, lowerTensorValue,
-      rewriter.getDenseI64ArrayAttr(reshapeOutputShape));
-
-  if (input1Rank > input2Rank) {
-    input1 = higherTensorValue;
-    input2 = reshapeLower.getResult();
-  } else {
-    input1 = reshapeLower.getResult();
-    input2 = higherTensorValue;
-  }
+  input1 = input1_copy;
+  input2 = input2_copy;
 
   return success();
 }
 
-namespace {
 template <typename OpTy>
 struct ConvertTosaOp : public OpRewritePattern<OpTy> {
   using OpRewritePattern<OpTy>::OpRewritePattern;
@@ -268,8 +200,10 @@ struct ConvertTosaOp<tosa::SelectOp> : public OpRewritePattern<tosa::SelectOp> {
     int32_t result1Rank = cast<RankedTensorType>(input1.getType()).getRank();
     int32_t result2Rank = cast<RankedTensorType>(input2.getType()).getRank();
     int32_t result3Rank = cast<RankedTensorType>(input3.getType()).getRank();
+    int32_t outputRank = outputType.getRank();
 
-    if ((result1Rank != result2Rank) || (result2Rank != result3Rank))
+    if ((result1Rank != result2Rank) || (result2Rank != result3Rank) ||
+        (result1Rank != outputRank))
       return rewriter.notifyMatchFailure(
           tosaOp, "not all ranks are aligned with each other");
 
