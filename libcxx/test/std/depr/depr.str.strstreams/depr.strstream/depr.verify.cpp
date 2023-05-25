66c7388c83bb0320f2223da3e3f31363f44bb3aa
Mark de Wever <koraq@xs4all.nl>
2023-05-25 20:02:50 +0200

    [libc++] Deprecate the classes in strstream.
    These have been deprecated since their initial version in libc++. It
    seems they were never properly marked as deprecated.
    
    Discovered while working on D151223.
    
    Reviewed By: #libc, ldionne, philnik
    
    Differential Revision: https://reviews.llvm.org/D151474

--- libcxx/test/std/depr/depr.str.strstreams/depr.strstream/depr.verify.cpp
+++ libcxx/test/std/depr/depr.str.strstreams/depr.strstream/depr.verify.cpp
@@ -6,27 +6,10 @@
 //
 //===----------------------------------------------------------------------===//
 
-// ADDITIONAL_COMPILE_FLAGS: -D_LIBCPP_DISABLE_DEPRECATION_WARNINGS
-
 // <strstream>
 
-// class strstream
-
-// char* str();
+// check that strstream is marked deprecated
 
 #include <strstream>
-#include <cassert>
 
-#include "test_macros.h"
-
-int main(int, char**)
-{
-    {
-        std::strstream out;
-        out << 123 << ' ' << 4.5 << ' ' << "dog" << std::ends;
-        assert(out.str() == std::string("123 4.5 dog"));
-        out.freeze(false);
-    }
-
-  return 0;
-}
+std::strstream s; // expected-warning {{'strstream' is deprecated}}
