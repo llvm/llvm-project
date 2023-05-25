12648492998bd22d268eb1d4d476c6c3acc6c43d
Cullen Rhodes <cullen.rhodes@arm.com>
2023-05-25 09:13:15 +0000

    [mlir] Add pass to enable Armv9 Streaming SVE mode
    This patch adds a pass 'enable-arm-streaming' that enables the Armv9
    Scalable Matrix Extension (SME) Streaming SVE (SSVE) mode [1] by adding
    either of the following attributes to 'func.func' ops:
    
      * arm_streaming (default)
      * arm_locally_streaming
    
    PATCH [2 / 2] in series for RFC: https://discourse.llvm.org/t/rfc-supporting-armv9-scalable-matrix-extension-sme-streaming-sve-ssve-mode-in-mlir/70678
    
    [1] https://developer.arm.com/documentation/ddi0616/aa
    
    Reviewed By: awarzynski, dcaballe
    
    Differential Revision: https://reviews.llvm.org/D150934

---
+++ mlir/include/mlir/Dialect/ArmSME/Transforms/Passes.h
@@ -0,0 +1,43 @@
+//===- Passes.h - Pass Entrypoints ------------------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef MLIR_DIALECT_ARMSME_TRANSFORMS_PASSES_H
+#define MLIR_DIALECT_ARMSME_TRANSFORMS_PASSES_H
+
+#include "mlir/Pass/Pass.h"
+
+namespace mlir {
+
+class RewritePatternSet;
+
+namespace arm_sme {
+// Options for Armv9 Streaming SVE mode. By default, streaming-mode is part of
+// the function interface (ABI) and the caller manages PSTATE.SM on entry/exit.
+// In a locally streaming function PSTATE.SM is kept internal and the callee
+// manages it on entry/exit.
+enum class ArmStreaming { Default = 0, Locally = 1 };
+
+#define GEN_PASS_DECL
+#include "mlir/Dialect/ArmSME/Transforms/Passes.h.inc"
+
+/// Pass to enable Armv9 Streaming SVE mode.
+std::unique_ptr<Pass>
+createEnableArmStreamingPass(const ArmStreaming mode = ArmStreaming::Default);
+
+//===----------------------------------------------------------------------===//
+// Registration
+//===----------------------------------------------------------------------===//
+
+/// Generate the code for registering passes.
+#define GEN_PASS_REGISTRATION
+#include "mlir/Dialect/ArmSME/Transforms/Passes.h.inc"
+
+} // namespace arm_sme
+} // namespace mlir
+
+#endif // MLIR_DIALECT_ARMSME_TRANSFORMS_PASSES_H
