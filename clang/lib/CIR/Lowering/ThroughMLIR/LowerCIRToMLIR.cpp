42803d9fead4413181200e84635eb89c090fdc79
Hongtao Yu <hoy@fb.com>
2023-05-26 11:04:06 -0700

    [CIR] Enable -fclangir-direct-lowering by default
    Summary:
    This change turns on direct lowering from CIR to LLVM IR by default. A missing API call to register the builtin LLVM dialtect is added, otherwise an error `cannot be converted to LLVM IR: missing `LLVMTranslationDialectInterface` registration for dialect for op: builtin.module` is hit during llvm IR conversion.  A test is also tweaked.

--- clang/lib/CIR/Lowering/ThroughMLIR/LowerCIRToMLIR.cpp
+++ clang/lib/CIR/Lowering/ThroughMLIR/LowerCIRToMLIR.cpp
@@ -578,7 +578,6 @@ lowerFromCIRToMLIRToLLVMIR(mlir::ModuleOp theModule,
   mlir::registerBuiltinDialectTranslation(*mlirCtx);
   mlir::registerLLVMDialectTranslation(*mlirCtx);
 
-  LLVMContext llvmContext;
   auto llvmModule = mlir::translateModuleToLLVMIR(theModule, llvmCtx);
 
   if (!llvmModule)
