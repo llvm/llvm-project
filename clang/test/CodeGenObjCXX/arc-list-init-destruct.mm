08be7a429551cc995c939b357f709b878bfaf322
Nikita Popov <npopov@redhat.com>
2023-05-26 16:58:48 +0200

    [Clang] Convert some tests to opaque pointers (NFC)

--- clang/test/CodeGenObjCXX/arc-list-init-destruct.mm
+++ clang/test/CodeGenObjCXX/arc-list-init-destruct.mm
@@ -1,7 +1,6 @@
-// RUN: %clang_cc1 -no-opaque-pointers -triple x86_64-apple-macosx10.13.0 -std=c++1z -fobjc-arc -fobjc-exceptions -fcxx-exceptions -fexceptions -emit-llvm -o - %s | FileCheck %s
+// RUN: %clang_cc1 -triple x86_64-apple-macosx10.13.0 -std=c++1z -fobjc-arc -fobjc-exceptions -fcxx-exceptions -fexceptions -emit-llvm -o - %s | FileCheck %s
 
-// CHECK: %[[V0:.*]] = type opaque
-// CHECK: %[[STRUCT_CLASS1:.*]] = type { %[[V0]]* }
+// CHECK: %[[STRUCT_CLASS1:.*]] = type { ptr }
 
 @interface Class0;
 @end
@@ -25,8 +24,8 @@ Class0 *g;
 
 // CHECK: define {{.*}} @_Z4testv()
 // CHECK: invoke noundef zeroext i1 @_Z7getBoolv()
-// CHECK: landingpad { i8*, i32 }
-// CHECK: call void @_ZN6Class1D1Ev(%[[STRUCT_CLASS1]]* {{[^,]*}} %{{.*}})
+// CHECK: landingpad { ptr, i32 }
+// CHECK: call void @_ZN6Class1D1Ev(ptr {{[^,]*}} %{{.*}})
 // CHECK: br label
 
 // CHECK: define linkonce_odr void @_ZN6Class1D1Ev(
