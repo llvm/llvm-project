af7aea365a2ae8dc51938a614353e1586072ef83
Fangrui Song <i@maskray.me>
2023-05-26 19:01:39 -0700

    [Driver][test] Replace legacy -target with --target=

--- clang/test/Driver/gold-lto-samplepgo.c
+++ clang/test/Driver/gold-lto-samplepgo.c
@@ -1,6 +1,6 @@
 // RUN: touch %t.o
 //
-// RUN: %clang -target x86_64-unknown-linux -### %t.o -flto 2>&1 \
+// RUN: %clang --target=x86_64-unknown-linux -### %t.o -flto 2>&1 \
 // RUN:     -Wl,-plugin-opt=foo -O3 \
 // RUN:     -fprofile-sample-use=%s \
 // RUN:     | FileCheck %s
