e210f2b34bfc2291ec03dd268230dcc8834d3e9f
Haohai Wen <haohai.wen@intel.com>
2023-05-29 09:22:20 +0800

    [lld] Partially revert "Always emit symbol table when dwarf section exists in COFF"
    This reverts part of commit f2e21262bba1578582ec5787f05ec5bcc85db7a4.
    
    Fixup for NO symbol table test has been reserved.
    
    Reviewed By: wxiao3
    
    Differential Revision: https://reviews.llvm.org/D151417

--- lld/COFF/Writer.cpp
+++ lld/COFF/Writer.cpp
@@ -1266,9 +1266,7 @@ void Writer::createSymbolAndStringTable() {
   // solution where discardable sections have long names preserved and
   // non-discardable sections have their names truncated, to ensure that any
   // section which is mapped at runtime also has its name mapped at runtime.
-  bool HasDwarfSection = false;
   for (OutputSection *sec : ctx.outputSections) {
-    HasDwarfSection |= sec->name.startswith(".debug_");
     if (sec->name.size() <= COFF::NameSize)
       continue;
     if ((sec->header.Characteristics & IMAGE_SCN_MEM_DISCARDABLE) == 0)
@@ -1281,7 +1279,7 @@ void Writer::createSymbolAndStringTable() {
     sec->setStringTableOff(addEntryToStringTable(sec->name));
   }
 
-  if (ctx.config.debugDwarf || ctx.config.debugSymtab || HasDwarfSection) {
+  if (ctx.config.debugDwarf || ctx.config.debugSymtab) {
     for (ObjFile *file : ctx.objFileInstances) {
       for (Symbol *b : file->getSymbols()) {
         auto *d = dyn_cast_or_null<Defined>(b);
