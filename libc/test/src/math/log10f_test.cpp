c89601df1c83c7b1241eb61bcdccfc68094d437a
Guillaume Chatelet <gchatelet@google.com>
2023-05-31 12:40:10 +0000

    [libc] Reduce math tests runtime
    Reviewed By: lntue
    
    Differential Revision: https://reviews.llvm.org/D151798

--- libc/test/src/math/log10f_test.cpp
+++ libc/test/src/math/log10f_test.cpp
@@ -65,7 +65,7 @@ TEST(LlvmLibcLog10fTest, TrickyInputs) {
 }
 
 TEST(LlvmLibcLog10fTest, InFloatRange) {
-  constexpr uint32_t COUNT = 1000000;
+  constexpr uint32_t COUNT = 100'000;
   constexpr uint32_t STEP = UINT32_MAX / COUNT;
   for (uint32_t i = 0, v = 0; i <= COUNT; ++i, v += STEP) {
     float x = float(FPBits(v));
