b1f41685a60e5416af8f636393bffd03ac4c13f5
Alexandros Lamprineas <alexandros.lamprineas@arm.com>
2023-06-01 15:09:08 +0100

    [IPSCCP] Decouple queries for function analysis results.
    The SCCPSolver is using a structure (AnalysisResultsForFn) where it keeps
    pointers to various analyses needed by the IPSCCP pass. These analyses are
    requested all at the same time, which can become problematic in some cases.
    For example one could be retrieved via getCachedAnalysis() prior to the
    actual execution of the analysis. In more detail:
    
    The IPSCCP pass uses a DomTreeUpdater to preserve the PostDominatorTree
    in case the PostDominatorTreeAnalysis had run before IPSCCP. Starting with
    commit 1b1232047e83b the IPSCCP pass may use BlockFrequencyAnalysis for
    some functions in the module. As a result, the PostDominatorTreeAnalysis
    may not run until the BlockFrequencyAnalysis has run, since the latter
    analysis depends on the former. Currently, we setup the DomTreeUpdater
    using getCachedAnalysis to retrieve a PostDominatorTree. This happens
    before BlockFrequencyAnalysis has run, therefore the cached analysis can
    become invalid by the time we use it.
    
    Differential Revision: https://reviews.llvm.org/D151666

--- llvm/include/llvm/Transforms/Utils/SCCPSolver.h
+++ llvm/include/llvm/Transforms/Utils/SCCPSolver.h
@@ -39,14 +39,6 @@ class TargetLibraryInfo;
 class Value;
 class ValueLatticeElement;
 
-/// Helper struct for bundling up the analysis results per function for IPSCCP.
-struct AnalysisResultsForFn {
-  std::unique_ptr<PredicateInfo> PredInfo;
-  DominatorTree *DT;
-  PostDominatorTree *PDT;
-  LoopInfo *LI;
-};
-
 /// Helper struct shared between Function Specialization and SCCP Solver.
 struct ArgInfo {
   Argument *Formal; // The Formal argument being analysed.
@@ -82,7 +74,9 @@ public:
 
   ~SCCPSolver();
 
-  void addAnalysis(Function &F, AnalysisResultsForFn A);
+  void addLoopInfo(Function &F, LoopInfo &LI);
+
+  void addPredicateInfo(Function &F, DominatorTree &DT, AssumptionCache &AC);
 
   /// markBlockExecutable - This method can be used by clients to mark all of
   /// the blocks that are known to be intrinsically live in the processed unit.
@@ -92,8 +86,6 @@ public:
   const PredicateBase *getPredicateInfoFor(Instruction *I);
 
   const LoopInfo &getLoopInfo(Function &F);
-
-  DomTreeUpdater getDTU(Function &F);
 
   /// trackValueOfGlobalVariable - Clients can use this method to
   /// inform the SCCPSolver that it should track loads and stores to the
