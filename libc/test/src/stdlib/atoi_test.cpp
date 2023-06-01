722832e6d764748f8ee167c95370e043594ef995
Michael Jones <michaelrj@google.com>
2023-06-01 14:01:04 -0700

    [libc] Add strtoint32 and strtoint64 tests
    There were regressions in the testing framework due to none of the
    functioning buildbots having a 32 bit long. This allowed the 32 bit
    version of the strtointeger function to go untested. This patch adds
    tests for strtoint32 and strtoint64, which are internal testing
    functions that use constant integer sizes. It also fixes the tests to
    properly handle these situations.
    
    Reviewed By: sivachandra
    
    Differential Revision: https://reviews.llvm.org/D151935

--- libc/test/src/stdlib/atoi_test.cpp
+++ libc/test/src/stdlib/atoi_test.cpp
@@ -1,4 +1,4 @@
-//===-- Unittests for atoi -----------------------------------------------===//
+//===-- Unittests for atoi ------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
