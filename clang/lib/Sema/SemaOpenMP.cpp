19841e4dcaabe573d35eb88a130fc34d32ecd708
Joel E. Denny <jdenny.ornl@gmail.com>
2023-06-02 12:18:13 -0400

    [OpenMP] Fix transformed loop's var privacy
    Without this patch, the following example crashes Clang:
    
    ```
     #pragma omp target map(i)
     #pragma omp tile sizes(2)
     for (i = 0; i < N; ++i)
       ;
    ```
    
    This patch fixes the crash by changing `Sema::isOpenMPPrivateDecl` not
    to identify `i` as private just because it's the loop variable of a
    `tile` construct.
    
    While OpenMP TR11 and earlier do specify privacy for loop variables of
    loops *generated* from a `tile` construct, I haven't found text
    stating that the original loop variable must be private in the above
    example, so this patch leaves it shared.  Even so, it is a bit
    unexpected that value of `i` after the loop is `N - 1` instead of `N`.
    
    Reviewed By: ABataev
    
    Differential Revision: https://reviews.llvm.org/D151356

--- clang/lib/Sema/SemaOpenMP.cpp
+++ clang/lib/Sema/SemaOpenMP.cpp
@@ -2551,7 +2551,8 @@ OpenMPClauseKind Sema::isOpenMPPrivateDecl(ValueDecl *D, unsigned Level,
       }
     }
   }
-  if (isOpenMPLoopDirective(DSAStack->getCurrentDirective())) {
+  if (isOpenMPLoopDirective(DSAStack->getCurrentDirective()) &&
+      !isOpenMPLoopTransformationDirective(DSAStack->getCurrentDirective())) {
     if (DSAStack->getAssociatedLoops() > 0 && !DSAStack->isLoopStarted()) {
       DSAStack->resetPossibleLoopCounter(D);
       DSAStack->loopStart();
