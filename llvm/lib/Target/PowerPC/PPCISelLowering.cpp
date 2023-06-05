567c67ec809a57aacb4618efc3e6c557e92d87a2
Qiu Chaofan <qiucofan@cn.ibm.com>
2023-06-05 14:25:04 +0800

    [PowerPC] Require FPCVT for store fptoi combination

--- llvm/lib/Target/PowerPC/PPCISelLowering.cpp
+++ llvm/lib/Target/PowerPC/PPCISelLowering.cpp
@@ -15515,10 +15515,9 @@ SDValue PPCTargetLowering::PerformDAGCombine(SDNode *N,
 
     EVT Op1VT = N->getOperand(1).getValueType();
     unsigned Opcode = N->getOperand(1).getOpcode();
-    bool NeedsFPCVT = Opcode == ISD::FP_TO_UINT && Op1VT == MVT::i64;
 
     if ((Opcode == ISD::FP_TO_SINT || Opcode == ISD::FP_TO_UINT) &&
-        (!NeedsFPCVT || Subtarget.hasFPCVT())) {
+        Subtarget.hasFPCVT()) {
       SDValue Val= combineStoreFPToInt(N, DCI);
       if (Val)
         return Val;
