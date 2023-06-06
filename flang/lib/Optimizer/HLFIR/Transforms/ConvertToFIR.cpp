10d294a3114237a263c1c6e9017bd16eb6112a6c
Slava Zakharin <szakharin@nvidia.com>
2023-06-06 09:40:00 -0700

    [flang][hlfir] Implemented addressing an element of a polymorphic array.
    The changes convert hlfir.designate to fir.array_coor/fir.embox
    to represent a subscripted element of a polymorphic array.
    The type information is conveyed via the fir.embox's source_box.
    
    Reviewed By: tblah
    
    Differential Revision: https://reviews.llvm.org/D152200

--- flang/lib/Optimizer/HLFIR/Transforms/ConvertToFIR.cpp
+++ flang/lib/Optimizer/HLFIR/Transforms/ConvertToFIR.cpp
@@ -373,6 +373,31 @@ public:
 
 class DesignateOpConversion
     : public mlir::OpRewritePattern<hlfir::DesignateOp> {
+  // Helper method to generate the coordinate of the first element
+  // of an array section. It is also called for cases of non-section
+  // array element addressing.
+  static mlir::Value genSubscriptBeginAddr(
+      fir::FirOpBuilder &builder, mlir::Location loc,
+      hlfir::DesignateOp designate, mlir::Type baseEleTy, mlir::Value base,
+      mlir::Value shape,
+      const llvm::SmallVector<mlir::Value> &firBaseTypeParameters) {
+    assert(!designate.getIndices().empty());
+    llvm::SmallVector<mlir::Value> firstElementIndices;
+    auto indices = designate.getIndices();
+    int i = 0;
+    for (auto isTriplet : designate.getIsTripletAttr().asArrayRef()) {
+      // Coordinate of the first element are the index and triplets lower
+      // bounds
+      firstElementIndices.push_back(indices[i]);
+      i = i + (isTriplet ? 3 : 1);
+    }
+    mlir::Type arrayCoorType = fir::ReferenceType::get(baseEleTy);
+    base = builder.create<fir::ArrayCoorOp>(
+        loc, arrayCoorType, base, shape,
+        /*slice=*/mlir::Value{}, firstElementIndices, firBaseTypeParameters);
+    return base;
+  }
+
 public:
   explicit DesignateOpConversion(mlir::MLIRContext *ctx)
       : OpRewritePattern{ctx} {}
@@ -436,9 +461,20 @@ public:
 
     if (designateResultType.isa<fir::BaseBoxType>()) {
       // Generate embox or rebox.
-      if (!fir::unwrapPassByRefType(designateResultType)
-               .isa<fir::SequenceType>())
-        TODO(loc, "addressing polymorphic arrays");
+      mlir::Type eleTy = fir::unwrapPassByRefType(designateResultType);
+      bool isScalarDesignator = !eleTy.isa<fir::SequenceType>();
+      mlir::Value sourceBox;
+      if (isScalarDesignator) {
+        // The base box will be used for emboxing the scalar element.
+        sourceBox = base;
+        // Generate the coordinate of the element.
+        base = genSubscriptBeginAddr(builder, loc, designate, baseEleTy, base,
+                                     shape, firBaseTypeParameters);
+        shape = nullptr;
+        // Type information will be taken from the source box,
+        // so the type parameters are not needed.
+        firBaseTypeParameters.clear();
+      }
       llvm::SmallVector<mlir::Value> triples;
       llvm::SmallVector<mlir::Value> sliceFields;
       mlir::Type idxTy = builder.getIndexType();
@@ -462,7 +498,7 @@ public:
                 builder.create<mlir::arith::SubIOp>(loc, iIdx, lbIdx));
           }
         }
-      } else {
+      } else if (!isScalarDesignator) {
         // Otherwise, this is an array section with triplets.
         auto undef = builder.create<fir::UndefOp>(loc, idxTy);
         unsigned i = 0;
@@ -506,8 +542,9 @@ public:
         resultBox =
             builder.create<fir::ReboxOp>(loc, resultType, base, shape, slice);
       else
-        resultBox = builder.create<fir::EmboxOp>(loc, resultType, base, shape,
-                                                 slice, firBaseTypeParameters);
+        resultBox =
+            builder.create<fir::EmboxOp>(loc, resultType, base, shape, slice,
+                                         firBaseTypeParameters, sourceBox);
       rewriter.replaceOp(designate, resultBox);
       return mlir::success();
     }
@@ -525,19 +562,8 @@ public:
       // - scalar%array_comp(indices) [substring|complex_part]
       // This may be a ranked contiguous array section in which case
       // The first element address is being computed.
-      llvm::SmallVector<mlir::Value> firstElementIndices;
-      auto indices = designate.getIndices();
-      int i = 0;
-      for (auto isTriplet : designate.getIsTripletAttr().asArrayRef()) {
-        // Coordinate of the first element are the index and triplets lower
-        // bounds
-        firstElementIndices.push_back(indices[i]);
-        i = i + (isTriplet ? 3 : 1);
-      }
-      mlir::Type arrayCoorType = fir::ReferenceType::get(baseEleTy);
-      base = builder.create<fir::ArrayCoorOp>(
-          loc, arrayCoorType, base, shape,
-          /*slice=*/mlir::Value{}, firstElementIndices, firBaseTypeParameters);
+      base = genSubscriptBeginAddr(builder, loc, designate, baseEleTy, base,
+                                   shape, firBaseTypeParameters);
     }
 
     // Scalar substring (potentially on the previously built array element or
