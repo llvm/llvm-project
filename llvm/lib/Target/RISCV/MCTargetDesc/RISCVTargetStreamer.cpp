aed842d8d94547a2842e70d2eaf14034098888af
Jessica Clarke <jrtc27@jrtc27.com>
2023-06-06 14:54:43 +0100

    [RISCV] Rework .option arch target streamer interface
    The current interface requires some rather ugly tracking of state due to
    splitting up the calls for each argument. Instead, pack them all into a
    single call by passing an ArrayRef. Also clean up the dodgy whitespace
    emitted for the directive whilst here; there was a stray space between
    the tab and .option, and there was a tab rather than a space after the
    first comma for some strange reason.
    
    Reviewed By: craig.topper
    
    Differential Revision: https://reviews.llvm.org/D152193

--- llvm/lib/Target/RISCV/MCTargetDesc/RISCVTargetStreamer.cpp
+++ llvm/lib/Target/RISCV/MCTargetDesc/RISCVTargetStreamer.cpp
@@ -33,12 +33,9 @@ void RISCVTargetStreamer::emitDirectiveOptionRVC() {}
 void RISCVTargetStreamer::emitDirectiveOptionNoRVC() {}
 void RISCVTargetStreamer::emitDirectiveOptionRelax() {}
 void RISCVTargetStreamer::emitDirectiveOptionNoRelax() {}
+void RISCVTargetStreamer::emitDirectiveOptionArch(
+    ArrayRef<RISCVOptionArchArg> Args) {}
 void RISCVTargetStreamer::emitDirectiveVariantCC(MCSymbol &Symbol) {}
-void RISCVTargetStreamer::emitDirectiveOptionArchFullArch(StringRef Value,
-                                                          bool &PrefixEmitted) {
-}
-void RISCVTargetStreamer::emitDirectiveOptionArchPlusOrMinus(
-    StringRef Value, bool Enable, bool &PrefixEmitted, bool EmitComma) {}
 void RISCVTargetStreamer::emitAttribute(unsigned Attribute, unsigned Value) {}
 void RISCVTargetStreamer::finishAttributeSection() {}
 void RISCVTargetStreamer::emitTextAttribute(unsigned Attribute,
@@ -106,6 +103,26 @@ void RISCVTargetAsmStreamer::emitDirectiveOptionNoRelax() {
   OS << "\t.option\tnorelax\n";
 }
 
+void RISCVTargetAsmStreamer::emitDirectiveOptionArch(
+    ArrayRef<RISCVOptionArchArg> Args) {
+  OS << "\t.option\tarch";
+  for (const auto &Arg : Args) {
+    OS << ", ";
+    switch (Arg.Type) {
+    case RISCVOptionArchArgType::Full:
+      break;
+    case RISCVOptionArchArgType::Plus:
+      OS << "+";
+      break;
+    case RISCVOptionArchArgType::Minus:
+      OS << "-";
+      break;
+    }
+    OS << Arg.Value;
+  }
+  OS << "\n";
+}
+
 void RISCVTargetAsmStreamer::emitDirectiveVariantCC(MCSymbol &Symbol) {
   OS << "\t.variant_cc\t" << Symbol.getName() << "\n";
 }
@@ -122,34 +139,5 @@ void RISCVTargetAsmStreamer::emitTextAttribute(unsigned Attribute,
 void RISCVTargetAsmStreamer::emitIntTextAttribute(unsigned Attribute,
                                                   unsigned IntValue,
                                                   StringRef StringValue) {}
-
-static void emitDirectiveOptionArchPrefix(formatted_raw_ostream &OS,
-                                          bool &PrefixEmitted) {
-  if (!PrefixEmitted) {
-    OS << "\t .option\tarch,\t";
-    PrefixEmitted = true;
-  }
-}
-
-static void emitCommaOrNextLine(formatted_raw_ostream &OS, bool EmitComma) {
-  if (EmitComma)
-    OS << ", ";
-  else
-    OS << "\n";
-}
-
-void RISCVTargetAsmStreamer::emitDirectiveOptionArchFullArch(
-    StringRef Value, bool &PrefixEmitted) {
-  emitDirectiveOptionArchPrefix(OS, PrefixEmitted);
-  OS << Value;
-  emitCommaOrNextLine(OS, false);
-}
-
-void RISCVTargetAsmStreamer::emitDirectiveOptionArchPlusOrMinus(
-    StringRef Value, bool Enable, bool &PrefixEmitted, bool EmitComma) {
-  emitDirectiveOptionArchPrefix(OS, PrefixEmitted);
-  OS << (Enable ? "+" : "-") << Value;
-  emitCommaOrNextLine(OS, EmitComma);
-}
 
 void RISCVTargetAsmStreamer::finishAttributeSection() {}
