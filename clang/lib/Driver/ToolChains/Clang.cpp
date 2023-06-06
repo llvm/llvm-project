d2bc069520979e3b0f68110c8da5adb399869af3
Vy Nguyen <vyng@google.com>
2023-06-06 14:00:47 -0400

    Reland "D144999 [MC][MachO]Only emits compact-unwind format for "canonical" personality symbols. For the rest, use DWARFs."
    Reasons for rolling forward:
        - the crash reported from Chromium was fixed in D151824 (not related to this patch at all)
        - since D152824 was committed, it should now be safe to roll this forward.
    
    New change:
        - add an additional _ in name check
    
    This reverts commit 34c31ba8e4467a009e570fe5cf64681a31d2a599.

--- clang/lib/Driver/ToolChains/Clang.cpp
+++ clang/lib/Driver/ToolChains/Clang.cpp
@@ -2506,6 +2506,9 @@ static void CollectArgsForIntegratedAssembler(Compilation &C,
 
   Args.AddLastArg(CmdArgs, options::OPT_femit_dwarf_unwind_EQ);
 
+  Args.addOptInFlag(CmdArgs, options::OPT_femit_compact_unwind_non_canonical,
+                    options::OPT_fno_emit_compact_unwind_non_canonical);
+
   // If you add more args here, also add them to the block below that
   // starts with "// If CollectArgsForIntegratedAssembler() isn't called below".
 
