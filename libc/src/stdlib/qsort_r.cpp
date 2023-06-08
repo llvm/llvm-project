d3074f16a6f6b3447bf728d8a6d19f35f02179db
Michael Jones <michaelrj@google.com>
2023-06-08 14:04:55 -0700

    [libc] Add qsort_r
    This patch adds the reentrent qsort entrypoint, qsort_r. This is done by
    extending the qsort functionality and moving it to a shared utility
    header. For this reason the qsort_r tests focus mostly on the places
    where it differs from qsort, since they share the same sorting code.
    
    Reviewed By: sivachandra, lntue
    
    Differential Revision: https://reviews.llvm.org/D152467

--- libc/src/stdlib/qsort_r.cpp
+++ libc/src/stdlib/qsort_r.cpp
@@ -1,4 +1,4 @@
-//===-- Implementation of qsort -------------------------------------------===//
+//===-- Implementation of qsort_r -----------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,7 +6,7 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "src/stdlib/qsort.h"
+#include "src/stdlib/qsort_r.h"
 #include "src/__support/common.h"
 #include "src/stdlib/qsort_util.h"
 
@@ -14,12 +14,13 @@
 
 namespace __llvm_libc {
 
-LLVM_LIBC_FUNCTION(void, qsort,
+LLVM_LIBC_FUNCTION(void, qsort_r,
                    (void *array, size_t array_size, size_t elem_size,
-                    int (*compare)(const void *, const void *))) {
+                    int (*compare)(const void *, const void *, void *),
+                    void *arg)) {
   if (array == nullptr || array_size == 0 || elem_size == 0)
     return;
-  internal::Comparator c(compare);
+  internal::Comparator c(compare, arg);
   internal::quicksort(internal::Array(reinterpret_cast<uint8_t *>(array),
                                       array_size, elem_size, c));
 }
