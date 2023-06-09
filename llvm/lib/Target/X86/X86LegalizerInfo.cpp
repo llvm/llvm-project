79d20416f12cc967a8185212951056cecc7611ac
Thorsten Schütt <schuett@gmail.com>
2023-06-09 09:08:25 +0200

    [GlobalIsel][X86] Legalize G_FREEZE
    Reviewed By: arsenm
    
    Differential Revision: https://reviews.llvm.org/D152501

--- llvm/lib/Target/X86/X86LegalizerInfo.cpp
+++ llvm/lib/Target/X86/X86LegalizerInfo.cpp
@@ -348,6 +348,11 @@ X86LegalizerInfo::X86LegalizerInfo(const X86Subtarget &STI,
       .minScalar(0, LLT::scalar(32))
       .libcall();
 
+  getActionDefinitionsBuilder(G_FREEZE)
+    .legalFor({s8, s16, s32, s64, p0})
+    .widenScalarToNextPow2(0, /*Min=*/8)
+    .clampScalar(0, s8, sMaxScalar);
+
   setLegalizerInfo32bit();
   setLegalizerInfo64bit();
   setLegalizerInfoSSE1();
