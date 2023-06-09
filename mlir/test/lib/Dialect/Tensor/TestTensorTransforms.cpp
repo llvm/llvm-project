04e7c599a6bf3036030969aa4b5b5870f8a5f7a7
Matthias Springer <me@m-sp.org>
2023-06-09 12:30:02 +0200

    [mlir][tensor] Add option to fold only tensor.empty with a single use
    This is useful for transformations such as bufferization, which is looking for tensor.extract_slice/insert_slice pairs.
    
    Also fix the documentation of the corresponding tranform op.
    
    Differential Revision: https://reviews.llvm.org/D152455

--- mlir/test/lib/Dialect/Tensor/TestTensorTransforms.cpp
+++ mlir/test/lib/Dialect/Tensor/TestTensorTransforms.cpp
@@ -72,10 +72,6 @@ struct TestTensorTransforms
       llvm::cl::desc("Test folding of expand_shape/collapse_shape"),
       llvm::cl::init(false)};
 
-  Option<bool> testEmptyOpFolding{
-      *this, "test-empty-op-folding",
-      llvm::cl::desc("Test folding of tensor.empty"), llvm::cl::init(false)};
-
   Option<bool> testFoldIntoPackAndUnpack{
       *this, "test-fold-into-pack-and-unpack",
       llvm::cl::desc("Test folding ops into tensor.pack and tensor.unpack"),
@@ -103,12 +99,6 @@ struct TestTensorTransforms
 static void applyReassociativeReshapeFoldingPatterns(Operation *rootOp) {
   RewritePatternSet patterns(rootOp->getContext());
   tensor::populateReassociativeReshapeFoldingPatterns(patterns);
-  (void)applyPatternsAndFoldGreedily(rootOp, std::move(patterns));
-}
-
-static void applyEmptyOpFoldingPatterns(Operation *rootOp) {
-  RewritePatternSet patterns(rootOp->getContext());
-  tensor::populateFoldTensorEmptyPatterns(patterns);
   (void)applyPatternsAndFoldGreedily(rootOp, std::move(patterns));
 }
 
@@ -386,8 +376,6 @@ void TestTensorTransforms::runOnOperation() {
     applyDropRedundantInsertSliceRankExpansionPatterns(rootOp);
   if (testReassociativeReshapeFolding)
     applyReassociativeReshapeFoldingPatterns(rootOp);
-  if (testEmptyOpFolding)
-    applyEmptyOpFoldingPatterns(rootOp);
   if (testFoldIntoPackAndUnpack)
     applyFoldIntoPackAndUnpackPatterns(rootOp);
   if (testRewriteExtractSliceWithTiledCollapseShape) {
