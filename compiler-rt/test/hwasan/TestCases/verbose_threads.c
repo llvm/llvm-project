4e8fc830f659058281a77fb8768916786967c8e4
Vitaly Buka <vitalybuka@google.com>
2023-06-09 20:39:15 -0700

    [HWASAN] Fix verbose_threads
    Looks like D104248 acidentally moved the code.

--- compiler-rt/test/hwasan/TestCases/verbose_threads.c
+++ compiler-rt/test/hwasan/TestCases/verbose_threads.c
@@ -1,4 +1,4 @@
-// RUN: %clang_hwasan %s -o %t && %env_hwasan_opts=verbose_threads=1 %run %t 2>&1 | FileCheck %s
+// RUN: %clang_hwasan %s -o %t && %env_hwasan_opts=verbose_threads=1 %run %t 2>&1 | FileCheck %s --implicit-check-not=0x000000000000
 // REQUIRES: stable-runtime
 
 #include <pthread.h>
@@ -11,8 +11,8 @@
 // CHECK: Creating : T0 0x[[#%x,T0:]] stack: [0x[[#%x,SB0:]],0x[[#%x,SE0:]]) sz: [[#]] tls: [0x[[#%x,TB0:]],0x[[#%x,TE0:]])
 // CHECK: Creating : T1 0x[[#%x,T1:]] stack: [0x[[#%x,SB1:]],0x[[#%x,SE1:]]) sz: [[#]] tls: [0x[[#%x,TB1:]],0x[[#%x,TE1:]])
 // CHECK: Creating : T2 0x[[#%x,T2:]] stack: [0x[[#%x,SB2:]],0x[[#%x,SE2:]]) sz: [[#]] tls: [0x[[#%x,TB2:]],0x[[#%x,TE2:]])
-// CHECK: Destroying: T2 0x[[#T2]] stack: [0x[[#%x,SB2:]],0x[[#%x,SE2:]]) sz: [[#]] tls: [0x[[#%x,TB2:]],0x[[#%x,TE2:]])
-// CHECK: Destroying: T1 0x[[#T1]] stack: [0x[[#%x,SB1:]],0x[[#%x,SE1:]]) sz: [[#]] tls: [0x[[#%x,TB1:]],0x[[#%x,TE1:]])
+// CHECK: Destroying: T2 0x[[#T2]] stack: [0x[[#SB2]],0x[[#SE2]]) sz: [[#]] tls: [0x[[#TB2]],0x[[#TE2]])
+// CHECK: Destroying: T1 0x[[#T1]] stack: [0x[[#SB1]],0x[[#SE1]]) sz: [[#]] tls: [0x[[#TB1]],0x[[#TE1]])
 
 void *Empty(void *arg) {
   if (arg) return NULL;
