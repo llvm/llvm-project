60f491027d0c8e472ab773639eca6075047091bb
Ivan Butygin <ivan.butygin@gmail.com>
2023-06-10 22:59:24 +0200

    [mlir][math] Uplift from arith to math.fma
    Add pass to uplift from arith mulf + addf ops to math.fma if fastmath flags allow it.
    
    Differential Revision: https://reviews.llvm.org/D152633

--- mlir/include/mlir/Dialect/Math/Transforms/Passes.h
+++ mlir/include/mlir/Dialect/Math/Transforms/Passes.h
@@ -9,7 +9,17 @@
 #ifndef MLIR_DIALECT_MATH_TRANSFORMS_PASSES_H_
 #define MLIR_DIALECT_MATH_TRANSFORMS_PASSES_H_
 
+#include "mlir/Pass/Pass.h"
+
 namespace mlir {
+namespace math {
+#define GEN_PASS_DECL
+#include "mlir/Dialect/Math/Transforms/Passes.h.inc"
+#define GEN_PASS_DECL_MATHUPLIFTTOFMA
+#include "mlir/Dialect/Math/Transforms/Passes.h.inc"
+#define GEN_PASS_REGISTRATION
+#include "mlir/Dialect/Math/Transforms/Passes.h.inc"
+} // namespace math
 
 class RewritePatternSet;
 
@@ -33,6 +43,8 @@ struct MathPolynomialApproximationOptions {
 void populateMathPolynomialApproximationPatterns(
     RewritePatternSet &patterns,
     const MathPolynomialApproximationOptions &options = {});
+
+void populateUpliftToFMAPatterns(RewritePatternSet &patterns);
 
 } // namespace mlir
 
