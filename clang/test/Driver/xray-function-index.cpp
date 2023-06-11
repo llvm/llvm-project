00e56d012050925fe7ca8fc9b8d2c1e8bc721e3c
Fangrui Song <i@maskray.me>
2023-06-11 15:38:13 -0700

    [Driver] Default to -fxray-function-index
    As explained by commit cb573ef108980c775ab4e21cc40f0a7a6bc5ff5d,
    -fxray-function-index was the original default but was accidentally flipped by
    commit 032bcb906c011737100e7c094ded1fa1979e38bd. Restore the previous behavior.
    
    Originally reported by Oleksii Lozovskyi in D145848.

--- clang/test/Driver/xray-function-index.cpp
+++ clang/test/Driver/xray-function-index.cpp
@@ -1,5 +1,5 @@
-// RUN: %clang -### -c --target=x86_64 -fxray-instrument -fxray-function-index %s 2>&1 | FileCheck %s
-// RUN: %clang -### -c --target=x86_64 -fxray-instrument %s 2>&1 | FileCheck %s --check-prefix=DISABLED
+// RUN: %clang -### -c --target=x86_64 -fxray-instrument %s 2>&1 | FileCheck %s
+// RUN: %clang -### -c --target=x86_64 -fxray-instrument -fxray-function-index -fno-xray-function-index %s 2>&1 | FileCheck %s --check-prefix=DISABLED
 
-// CHECK:      "-fxray-function-index"
-// DISABLED-NOT: "-fxray-function-index"
+// CHECK-NOT:  "-fxray-function-index"
+// DISABLED:   "-fno-xray-function-index"
