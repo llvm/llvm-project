f35d0608fbd926c7ba2bf62ca6a2fa45287c0936
Vitaly Buka <vitalybuka@google.com>
2023-06-12 16:40:48 -0700

    [test][sanitizer] Add ArrayRef tests
    Tests are subset of llvm/unittests/ADT/ArrayRefTest.cpp.
    Added more members to match tests and
    make class more useful.
    
    Reviewed By: hctim
    
    Differential Revision: https://reviews.llvm.org/D152760

--- compiler-rt/lib/sanitizer_common/sanitizer_array_ref.h
+++ compiler-rt/lib/sanitizer_common/sanitizer_array_ref.h
@@ -27,11 +27,20 @@ namespace __sanitizer {
 template <typename T>
 class ArrayRef {
  public:
-  ArrayRef() {}
-  ArrayRef(const T *begin, const T *end) : begin_(begin), end_(end) {}
-
+  constexpr ArrayRef() {}
+  constexpr ArrayRef(const T *begin, const T *end) : begin_(begin), end_(end) {
+    DCHECK(empty() || begin);
+  }
+  constexpr ArrayRef(const T *data, uptr length)
+      : ArrayRef(data, data + length) {}
+  template <uptr N>
+  constexpr ArrayRef(const T (&src)[N]) : ArrayRef(src, src + N) {}
   template <typename C>
-  ArrayRef(const C &src) : ArrayRef(src.data(), src.data() + src.size()) {}
+  constexpr ArrayRef(const C &src)
+      : ArrayRef(src.data(), src.data() + src.size()) {}
+  ArrayRef(const T &one_elt) : ArrayRef(&one_elt, &one_elt + 1) {}
+
+  const T *data() const { return empty() ? nullptr : begin_; }
 
   const T *begin() const { return begin_; }
   const T *end() const { return end_; }
@@ -40,10 +49,74 @@ class ArrayRef {
 
   uptr size() const { return end_ - begin_; }
 
+  /// equals - Check for element-wise equality.
+  bool equals(ArrayRef rhs) const {
+    if (size() != rhs.size())
+      return false;
+    auto r = rhs.begin();
+    for (auto &l : *this) {
+      if (!(l == *r))
+        return false;
+      ++r;
+    }
+    return true;
+  }
+
+  /// slice(n, m) - Chop off the first N elements of the array, and keep M
+  /// elements in the array.
+  ArrayRef<T> slice(uptr N, uptr M) const {
+    DCHECK_LE(N + M, size());
+    return ArrayRef<T>(data() + N, M);
+  }
+
+  /// slice(n) - Chop off the first N elements of the array.
+  ArrayRef<T> slice(uptr N) const { return slice(N, size() - N); }
+
+  /// Drop the first \p N elements of the array.
+  ArrayRef<T> drop_front(uptr N = 1) const {
+    DCHECK_GE(size(), N);
+    return slice(N, size() - N);
+  }
+
+  /// Drop the last \p N elements of the array.
+  ArrayRef<T> drop_back(uptr N = 1) const {
+    DCHECK_GE(size(), N);
+    return slice(0, size() - N);
+  }
+
+  /// Return a copy of *this with only the first \p N elements.
+  ArrayRef<T> take_front(uptr N = 1) const {
+    if (N >= size())
+      return *this;
+    return drop_back(size() - N);
+  }
+
+  /// Return a copy of *this with only the last \p N elements.
+  ArrayRef<T> take_back(uptr N = 1) const {
+    if (N >= size())
+      return *this;
+    return drop_front(size() - N);
+  }
+
+  const T &operator[](uptr index) const {
+    DCHECK_LT(index, size());
+    return begin_[index];
+  }
+
  private:
   const T *begin_ = nullptr;
   const T *end_ = nullptr;
 };
+
+template <typename T>
+inline bool operator==(ArrayRef<T> lhs, ArrayRef<T> rhs) {
+  return lhs.equals(rhs);
+}
+
+template <typename T>
+inline bool operator!=(ArrayRef<T> lhs, ArrayRef<T> rhs) {
+  return !(lhs == rhs);
+}
 
 }  // namespace __sanitizer
 
