9d8ab73c435ca2064ac9896cea8e4ad7d6a9e4e8
Guillaume Chatelet <gchatelet@google.com>
2023-06-12 08:32:00 +0000

    Revert D148717 "[libc] Improve memcmp latency and codegen"
    This broke aarch64 debug buildbot https://lab.llvm.org/buildbot/#/builders/223/builds/21703
    This reverts commit 2118b39c47b029453e85f3ae7f33136eda0cae1c.

--- libc/src/string/memory_utils/memcmp_implementations.h
+++ libc/src/string/memory_utils/memcmp_implementations.h
@@ -13,7 +13,6 @@
 #include "src/__support/macros/optimization.h" // LIBC_UNLIKELY LIBC_LOOP_NOUNROLL
 #include "src/__support/macros/properties/architectures.h"
 #include "src/string/memory_utils/op_generic.h"
-#include "src/string/memory_utils/op_riscv.h"
 #include "src/string/memory_utils/utils.h" // CPtr MemcmpReturnType
 
 #include <stddef.h> // size_t
@@ -27,17 +26,21 @@
 namespace __llvm_libc {
 
 [[maybe_unused]] LIBC_INLINE MemcmpReturnType
-inline_memcmp_byte_per_byte(CPtr p1, CPtr p2, size_t count, size_t offset = 0) {
-  return generic::Memcmp<uint8_t>::loop_and_tail_offset(p1, p2, count, offset);
+inline_memcmp_byte_per_byte(CPtr p1, CPtr p2, size_t offset, size_t count) {
+  LIBC_LOOP_NOUNROLL
+  for (; offset < count; ++offset)
+    if (auto value = generic::Memcmp<1>::block(p1 + offset, p2 + offset))
+      return value;
+  return MemcmpReturnType::ZERO();
 }
 
 [[maybe_unused]] LIBC_INLINE MemcmpReturnType
 inline_memcmp_aligned_access_64bit(CPtr p1, CPtr p2, size_t count) {
   constexpr size_t kAlign = sizeof(uint64_t);
   if (count <= 2 * kAlign)
-    return inline_memcmp_byte_per_byte(p1, p2, count);
+    return inline_memcmp_byte_per_byte(p1, p2, 0, count);
   size_t bytes_to_p1_align = distance_to_align_up<kAlign>(p1);
-  if (auto value = inline_memcmp_byte_per_byte(p1, p2, bytes_to_p1_align))
+  if (auto value = inline_memcmp_byte_per_byte(p1, p2, 0, bytes_to_p1_align))
     return value;
   size_t offset = bytes_to_p1_align;
   size_t p2_alignment = distance_to_align_down<kAlign>(p2 + offset);
@@ -53,20 +56,21 @@ inline_memcmp_aligned_access_64bit(CPtr p1, CPtr p2, size_t count) {
       b = load64_aligned<uint8_t, uint16_t, uint16_t, uint16_t, uint8_t>(
           p2, offset);
     uint64_t a = load64_aligned<uint64_t>(p1, offset);
-    if (a != b)
-      return cmp_neq_uint64_t(Endian::to_big_endian(a),
-                              Endian::to_big_endian(b));
+    if (a != b) {
+      // TODO use cmp_neq_uint64_t from D148717 once it's submitted.
+      return Endian::to_big_endian(a) < Endian::to_big_endian(b) ? -1 : 1;
+    }
   }
-  return inline_memcmp_byte_per_byte(p1, p2, count, offset);
+  return inline_memcmp_byte_per_byte(p1, p2, offset, count);
 }
 
 [[maybe_unused]] LIBC_INLINE MemcmpReturnType
 inline_memcmp_aligned_access_32bit(CPtr p1, CPtr p2, size_t count) {
   constexpr size_t kAlign = sizeof(uint32_t);
   if (count <= 2 * kAlign)
-    return inline_memcmp_byte_per_byte(p1, p2, count);
+    return inline_memcmp_byte_per_byte(p1, p2, 0, count);
   size_t bytes_to_p1_align = distance_to_align_up<kAlign>(p1);
-  if (auto value = inline_memcmp_byte_per_byte(p1, p2, bytes_to_p1_align))
+  if (auto value = inline_memcmp_byte_per_byte(p1, p2, 0, bytes_to_p1_align))
     return value;
   size_t offset = bytes_to_p1_align;
   size_t p2_alignment = distance_to_align_down<kAlign>(p2 + offset);
@@ -79,10 +83,16 @@ inline_memcmp_aligned_access_32bit(CPtr p1, CPtr p2, size_t count) {
     else
       b = load32_aligned<uint8_t, uint16_t, uint8_t>(p2, offset);
     uint32_t a = load32_aligned<uint32_t>(p1, offset);
-    if (a != b)
-      return cmp_uint32_t(Endian::to_big_endian(a), Endian::to_big_endian(b));
+    if (a != b) {
+      // TODO use cmp_uint32_t from D148717 once it's submitted.
+      // We perform the difference as an uint64_t.
+      const int64_t diff = static_cast<int64_t>(Endian::to_big_endian(a)) -
+                           static_cast<int64_t>(Endian::to_big_endian(b));
+      // And reduce the uint64_t into an uint32_t.
+      return static_cast<int32_t>((diff >> 1) | (diff & 0xFFFF));
+    }
   }
-  return inline_memcmp_byte_per_byte(p1, p2, count, offset);
+  return inline_memcmp_byte_per_byte(p1, p2, offset, count);
 }
 
 LIBC_INLINE MemcmpReturnType inline_memcmp(CPtr p1, CPtr p2, size_t count) {
@@ -95,7 +105,7 @@ LIBC_INLINE MemcmpReturnType inline_memcmp(CPtr p1, CPtr p2, size_t count) {
 #elif defined(LIBC_TARGET_ARCH_IS_RISCV32)
   return inline_memcmp_aligned_access_32bit(p1, p2, count);
 #else
-  return inline_memcmp_byte_per_byte(p1, p2, count);
+  return inline_memcmp_byte_per_byte(p1, p2, 0, count);
 #endif
 }
 
