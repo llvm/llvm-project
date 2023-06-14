fc60bf2de11149d2c027d63e7ad5a98afa6fab80
Jonas Devlieghere <jonas@devlieghere.com>
2023-06-14 13:00:38 -0700

    [DebugInfo] Always emit `.debug_names` with DWARF 5 for Apple platforms
    On Apple platforms, we generate .apple_names, .apple_types,
    .apple_namespaces and .apple_objc Apple accelerator tables for DWARF 4
    and earlier. For DWARF 5 we should generate .debug_names, but instead we
    get no accelerator tables at all.
    
    In the backend we are correctly determining that we should be emitting
    .debug_names instead of .apple_names. However, when we get to the point
    of emitting the section, if the CU debug name table kind is not
    "default", the accelerator table emission is skipped.
    
    This patch sets the DebugNameTableKind to Apple in the frontend when
    target an Apple target. That way we know that the CU was compiled with
    the intent of emitting accelerator tables. For DWARF 4 and earlier, that
    means Apple accelerator tables. For DWARF 5 and later, that means .debug
    names.
    
    Differential revision: https://reviews.llvm.org/D118754

--- clang/lib/CodeGen/CGDebugInfo.cpp
+++ clang/lib/CodeGen/CGDebugInfo.cpp
@@ -637,17 +637,21 @@ void CGDebugInfo::CreateCompileUnit() {
       SDK = *It;
   }
 
+  llvm::DICompileUnit::DebugNameTableKind NameTableKind =
+      static_cast<llvm::DICompileUnit::DebugNameTableKind>(
+          CGOpts.DebugNameTable);
+  if (CGM.getTarget().getTriple().isNVPTX())
+    NameTableKind = llvm::DICompileUnit::DebugNameTableKind::None;
+  else if (CGM.getTarget().getTriple().getVendor() == llvm::Triple::Apple)
+    NameTableKind = llvm::DICompileUnit::DebugNameTableKind::Apple;
+
   // Create new compile unit.
   TheCU = DBuilder.createCompileUnit(
       LangTag, CUFile, CGOpts.EmitVersionIdentMetadata ? Producer : "",
       LO.Optimize || CGOpts.PrepareForLTO || CGOpts.PrepareForThinLTO,
       CGOpts.DwarfDebugFlags, RuntimeVers, CGOpts.SplitDwarfFile, EmissionKind,
       DwoId, CGOpts.SplitDwarfInlining, CGOpts.DebugInfoForProfiling,
-      CGM.getTarget().getTriple().isNVPTX()
-          ? llvm::DICompileUnit::DebugNameTableKind::None
-          : static_cast<llvm::DICompileUnit::DebugNameTableKind>(
-                CGOpts.DebugNameTable),
-      CGOpts.DebugRangesBaseAddress, remapDIPath(Sysroot), SDK);
+      NameTableKind, CGOpts.DebugRangesBaseAddress, remapDIPath(Sysroot), SDK);
 }
 
 llvm::DIType *CGDebugInfo::CreateType(const BuiltinType *BT) {
--- llvm/include/llvm/IR/DebugInfoMetadata.h
+++ llvm/include/llvm/IR/DebugInfoMetadata.h
@@ -1377,7 +1377,8 @@ public:
     Default = 0,
     GNU = 1,
     None = 2,
-    LastDebugNameTableKind = None
+    Apple = 3,
+    LastDebugNameTableKind = Apple
   };
 
   static std::optional<DebugEmissionKind> getEmissionKind(StringRef Str);
