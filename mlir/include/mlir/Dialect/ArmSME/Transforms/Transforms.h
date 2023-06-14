0e65e95a9226b10257ee44a0cd70d61eac1f031b
Cullen Rhodes <cullen.rhodes@arm.com>
2023-06-14 08:26:44 +0000

    [mlir][ArmSME] Add initial dialect with basic lowering of vector.transfer write to zero
    This patch adds support for lowering a `vector.transfer_write` of zeroes
    and type `vector<[16x16]xi8>` to the SME `zero {za}` instruction [1],
    which zeroes the entire accumulator.
    
    This contributes to supporting a path from `linalg.fill` to SME.
    
    [1] https://developer.arm.com/documentation/ddi0602/2022-06/SME-Instructions/ZERO--Zero-a-list-of-64-bit-element-ZA-tiles-
    
    Reviewed By: awarzynski, dcaballe
    
    Differential Revision: https://reviews.llvm.org/D152508

---
+++ mlir/include/mlir/Dialect/ArmSME/Transforms/Transforms.h
@@ -0,0 +1,27 @@
+//===- Transforms.h - ArmSME Dialect Transformation Entrypoints -*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef MLIR_DIALECT_ARMSME_TRANSFORMS_H
+#define MLIR_DIALECT_ARMSME_TRANSFORMS_H
+
+namespace mlir {
+
+class LLVMConversionTarget;
+class RewritePatternSet;
+
+namespace arm_sme {
+void populateVectorTransferLoweringPatterns(RewritePatternSet &patterns);
+} // namespace arm_sme
+
+/// Configure the target to support lowering ArmSME ops to ops that map to LLVM
+/// intrinsics.
+void configureArmSMELegalizeForExportTarget(LLVMConversionTarget &target);
+
+} // namespace mlir
+
+#endif // MLIR_DIALECT_ARMSME_TRANSFORMS_H
