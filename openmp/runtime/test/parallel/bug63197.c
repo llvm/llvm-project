2d3a897cca9118a6a26efcdc6f4bac7d715d8bef
Shilei Tian <i@tianshilei.me>
2023-06-14 12:23:24 -0400

    [OpenMP] Fix the issue in openmp/runtime/test/parallel/bug63197.c
    If the system has 32 threads, then the test will fail because of partial match.

--- openmp/runtime/test/parallel/bug63197.c
+++ openmp/runtime/test/parallel/bug63197.c
@@ -4,14 +4,17 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-#pragma omp parallel num_threads(3) if(0)
+#pragma omp parallel num_threads(3) if (0)
 #pragma omp single
   { printf("BBB %2d\n", omp_get_num_threads()); }
 
 #pragma omp parallel
 #pragma omp single
-  { printf("CCC %2d\n", omp_get_num_threads()); }
+  {
+    if (omp_get_num_threads() != 3)
+      printf("PASS\n");
+  }
   return 0;
 }
 
-// CHECK-NOT: CCC  3
+// CHECK: PASS
