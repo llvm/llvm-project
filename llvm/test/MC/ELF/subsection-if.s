llvm/llvm-project

commit 2d484cc33a93983befd02b0251b09c8f248f6d98

[MC] Remove unneeded special cases from AttemptToFoldSymbolOffsetDifference

llvm/lib/MC/MCExpr.cpp
@@ -619,17 +619,8 @@ static void AttemptToFoldSymbolOffsetDifference(
 
   const MCFragment *FA = SA.getFragment();
   const MCFragment *FB = SB.getFragment();
-  // If both symbols are in the same fragment, return the difference of their
-  // offsets
-  if (FA == FB && !SA.isVariable() && !SA.isUnset() && !SB.isVariable() &&
-      !SB.isUnset()) {
-    Addend += SA.getOffset() - SB.getOffset();
-    return FinalizeFolding();
-  }
-
   const MCSection &SecA = *FA->getParent();
   const MCSection &SecB = *FB->getParent();
-
   if ((&SecA != &SecB) && !Addrs)
     return;
 
@@ -654,11 +645,12 @@ static void AttemptToFoldSymbolOffsetDifference(
     // this is important when the Subtarget is changed and a new MCDataFragment
     // is created in the case of foo: instr; .arch_extension ext; instr .if . -
     // foo.
-    if (SA.isVariable() || SA.isUnset() || SB.isVariable() || SB.isUnset() ||
-        FA->getKind() != MCFragment::FT_Data ||
-        FB->getKind() != MCFragment::FT_Data ||
+    if (FA != FB && (!isa<MCDataFragment>(FA) || !isa<MCDataFragment>(FA)))
+      return;
+    if (SA.isVariable() || SB.isVariable() ||
         FA->getSubsectionNumber() != FB->getSubsectionNumber())
       return;
+
     // Try to find a constant displacement from FA to FB, add the displacement
     // between the offset in FA of SA and the offset in FB of SB.
     int64_t Displacement = SA.getOffset() - SB.getOffset();

commit 53c94ec4fe3fba2e922764cb0f6c77053e3e031d

[MC] Restore a special case to support limited A-B folding when A/B are in the same fragment being laided out
Add subsection-if.s to test what we can fold (in the same fragment) and what we cannot.

Fix https://github.com/ClangBuiltLinux/linux/issues/1876
Fixes: 2d484cc33a93983befd02b0251b09c8f248f6d98

llvm/lib/MC/MCExpr.cpp
@@ -625,6 +625,12 @@ static void AttemptToFoldSymbolOffsetDifference(
     return;
 
   if (Layout) {
+    // If both symbols are in the same fragment, return the difference of their
+    // offsets. canGetFragmentOffset(FA) may be false.
+    if (FA == FB && !SA.isVariable() && !SB.isVariable()) {
+      Addend += SA.getOffset() - SB.getOffset();
+      return FinalizeFolding();
+    }
     // One of the symbol involved is part of a fragment being laid out. Quit now
     // to avoid a self loop.
     if (!Layout->canGetFragmentOffset(FA) || !Layout->canGetFragmentOffset(FB))

llvm/test/MC/ELF/subsection-if.s
@@ -1,0 +1,26 @@
+# RUN: llvm-mc -filetype=obj -triple=x86_64 %s -o %t
+# RUN: llvm-readelf -x .text %t | FileCheck %s
+# RUN: not llvm-mc -filetype=obj -triple=x86_64 --defsym ERR=1 %s -o /dev/null 2>&1 | FileCheck %s --check-prefix=ERR
+
+# CHECK: 0x00000000 9090
+
+.subsection 1
+661:
+  nop
+662:
+.previous
+## 661 and 662 are in the same fragment being laied out.
+  .org . - (662b-661b) + (662b-661b)
+  nop
+
+.ifdef ERR
+.subsection 1
+661:
+  .p2align 2
+  nop
+662:
+.previous
+# ERR: :[[#@LINE+1]]:8: error: expected assembly-time absolute expression
+  .org . - (662b-661b) + (662b-661b)
+  nop
+.endif