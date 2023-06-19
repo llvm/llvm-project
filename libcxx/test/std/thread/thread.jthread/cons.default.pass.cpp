4c62ed02edf76f7693c5372bf00003cb4bf37ab7
Hui <hui.xie0621@gmail.com>
2023-06-19 13:32:58 +0100

    [libc++] implement std::`jthread`

---
+++ libcxx/test/std/thread/thread.jthread/cons.default.pass.cpp
@@ -0,0 +1,33 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// UNSUPPORTED: no-threads
+// UNSUPPORTED: libcpp-has-no-experimental-stop_token
+// UNSUPPORTED: c++03, c++11, c++14, c++17
+// XFAIL: availability-synchronization_library-missing
+
+// jthread() noexcept;
+
+#include <cassert>
+#include <stop_token>
+#include <thread>
+#include <type_traits>
+
+#include "test_macros.h"
+
+static_assert(std::is_nothrow_default_constructible_v<std::jthread>);
+
+int main(int, char**) {
+  {
+    std::jthread jt = {}; // implicit
+    assert(!jt.get_stop_source().stop_possible());
+    assert(jt.get_id() == std::jthread::id());
+  }
+
+  return 0;
+}
---
+++ libcxx/test/std/thread/thread.jthread/request_stop.pass.cpp
@@ -0,0 +1,45 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// UNSUPPORTED: no-threads
+// UNSUPPORTED: libcpp-has-no-experimental-stop_token
+// UNSUPPORTED: c++03, c++11, c++14, c++17
+// XFAIL: availability-synchronization_library-missing
+
+// [[nodiscard]] bool request_stop() noexcept;
+
+#include <cassert>
+#include <concepts>
+#include <stop_token>
+#include <thread>
+#include <type_traits>
+
+#include "test_macros.h"
+
+static_assert(noexcept(std::declval<std::jthread&>().request_stop()));
+
+int main(int, char**) {
+  // Represents a thread
+  {
+    std::jthread jt{[] {}};
+    auto st = jt.get_stop_token();
+    assert(!st.stop_requested());
+    std::same_as<bool> decltype(auto) result = jt.request_stop();
+    assert(result);
+    assert(st.stop_requested());
+  }
+
+  // Does not represent a thread
+  {
+    std::jthread jt{};
+    std::same_as<bool> decltype(auto) result = jt.request_stop();
+    assert(!result);
+  }
+
+  return 0;
+}
