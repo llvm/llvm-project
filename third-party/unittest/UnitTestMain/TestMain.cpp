d46ac1c1e5b2d7b8f36897b63d3ee153fb1ce100
Martin Braenne <mboehme@google.com>
2023-06-20 07:15:31 +0000

    Revert "Prevent deadlocks in death tests."
    This reverts commit 41936d4a1a396b3f5a7f1218b38ba83f19075fb6.
    
    This was causing unit tests to fail on Gentoo, see comments on
    https://reviews.llvm.org/D152696.

--- third-party/unittest/UnitTestMain/TestMain.cpp
+++ third-party/unittest/UnitTestMain/TestMain.cpp
@@ -29,10 +29,6 @@ int main(int argc, char **argv) {
                                             true /* Disable crash reporting */);
   }
 
-  // Use the "threadsafe" test style for death tests -- the "fast" test style
-  // can cause deadlocks.
-  testing::GTEST_FLAG(death_test_style) = "threadsafe";
-
   // Initialize both gmock and gtest.
   testing::InitGoogleMock(&argc, argv);
 
