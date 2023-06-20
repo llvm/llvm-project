b6979698ea5f0c13ed4af661c88c981b9525e17c
Anna Thomas <anna@azul.com>
2023-06-20 15:17:11 -0400

    Fix mlir windows buildbot after ec146cb7c0b4
    ec146cb7c0b4 added two new ReducKinds for float maximum and minimum
    (along with support in loop vectorizer).
    
    Enumerate these kinds in SLPVectorizer to fix mlir windows build bot
    errors:
    
    C:\buildbot\mlir-x64-windows-ninja\llvm-project\llvm\lib\Transforms\Vectorize\SLPVectorizer.cpp(13883): error C2220: the following warning is treated as an error
    C:\buildbot\mlir-x64-windows-ninja\llvm-project\llvm\lib\Transforms\Vectorize\SLPVectorizer.cpp(13883): warning C4062: enumerator 'llvm::RecurKind::FMinimum' in switch of enum 'llvm::RecurKind' is not handled

--- llvm/lib/Transforms/Vectorize/SLPVectorizer.cpp
+++ llvm/lib/Transforms/Vectorize/SLPVectorizer.cpp
@@ -12767,6 +12767,9 @@ class HorizontalReduction {
         return Builder.CreateSelect(Cmp, LHS, RHS, Name);
       }
       return Builder.CreateBinaryIntrinsic(Intrinsic::umin, LHS, RHS);
+    // TODO: Add support for these recurrences.  
+    case RecurKind::FMaximum:
+    case RecurKind::FMinimum:
     default:
       llvm_unreachable("Unknown reduction operation.");
     }
@@ -13811,6 +13814,9 @@ private:
       });
       break;
     }
+    // TODO: Add support for these recurrences.  
+    case RecurKind::FMaximum:
+    case RecurKind::FMinimum:
     default:
       llvm_unreachable("Expected arithmetic or min/max reduction operation");
     }
@@ -13880,6 +13886,9 @@ private:
     case RecurKind::FMulAdd:
     case RecurKind::SelectICmp:
     case RecurKind::SelectFCmp:
+    // TODO: Add support for these recurrences.  
+    case RecurKind::FMaximum:
+    case RecurKind::FMinimum:
     case RecurKind::None:
       llvm_unreachable("Unexpected reduction kind for repeated scalar.");
     }
@@ -13969,6 +13978,9 @@ private:
     case RecurKind::FMulAdd:
     case RecurKind::SelectICmp:
     case RecurKind::SelectFCmp:
+    // TODO: Add support for these recurrences.  
+    case RecurKind::FMaximum:
+    case RecurKind::FMinimum:
     case RecurKind::None:
       llvm_unreachable("Unexpected reduction kind for reused scalars.");
     }
