b410d24a194482bbf06aed8956083fabd52d6069
Job Noorman <jnoorman@igalia.com>
2023-06-22 09:32:32 +0200

    [BOLT][RISCV] Implement R_RISCV_ADD32/SUB32
    Thispatch implements the R_RISCV_ADD32 and R_RISCV_SUB32 relocations for
    RISC-V.
    
    Reviewed By: rafauler
    
    Differential Revision: https://reviews.llvm.org/D146554

---
+++ bolt/test/RISCV/reloc-jt.s
@@ -0,0 +1,28 @@
+/// NOTE: assign section addresses explicitly to make the symbol difference
+/// calculation below less fragile.
+// RUN: %clang %cflags -Wl,--section-start=.text=0x1000,--section-start=.data=0x2000 -o %t %s
+// RUN: llvm-bolt -o %t.bolt %t
+// RUN: llvm-readelf -x .data %t.bolt | FileCheck %s
+
+  .text
+
+  .globl _start
+  .p2align 1
+_start:
+.LBB0_0:
+  auipc a1, %pcrel_hi(.LJTI0_0)
+  addi a1, a1, %pcrel_lo(.LBB0_0)
+  lw a0, (a1)
+  add a0, a0, a1
+  jr a0
+.LBB0_1:
+  ret
+  .size _start, .-_start
+
+  .data
+/// .LJTI0_0 = 0x2000
+/// .LBB0_1 = 0x40000e
+// CHECK: Hex dump of section '.data':
+// CHECK-NEXT: 0x00002000 0ee03f00
+.LJTI0_0:
+  .word .LBB0_1 - .LJTI0_0
