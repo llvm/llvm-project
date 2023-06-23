e4d257654da2d8a82f4343a2a68f0be12b56c045
Peter Klausler <pklausler@nvidia.com>
2023-06-23 11:01:33 -0700

    [flang] Honor #line and related preprocessing directives
    Extend the SourceFile class to take account of #line directives
    when computing source file positions for error messages.
    Adjust the output of #line directives to -E output so that they
    reflect any #line directives that were in the input.
    
    Differential Revision: https://reviews.llvm.org/D153910

--- flang/lib/Lower/Bridge.cpp
+++ flang/lib/Lower/Bridge.cpp
@@ -716,7 +716,7 @@ public:
               loc = cooked->GetSourcePositionRange(block)) {
         // loc is a pair (begin, end); use the beginning position
         Fortran::parser::SourcePosition &filePos = loc->first;
-        llvm::SmallString<256> filePath(filePos.file.path());
+        llvm::SmallString<256> filePath(*filePos.path);
         llvm::sys::fs::make_absolute(filePath);
         llvm::sys::path::remove_dots(filePath);
         return mlir::FileLineColLoc::get(&getMLIRContext(), filePath.str(),
