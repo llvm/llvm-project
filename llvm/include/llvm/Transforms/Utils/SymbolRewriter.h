b43d9f3e75365dfd95df50ad442a29a960a3f069
Kazu Hirata <kazu@google.com>
2023-06-24 07:33:50 -0700

    [LegacyPM] Remove RewriteSymbolsLegacyPass
    Differential Revision: https://reviews.llvm.org/D153679

--- llvm/include/llvm/Transforms/Utils/SymbolRewriter.h
+++ llvm/include/llvm/Transforms/Utils/SymbolRewriter.h
@@ -114,9 +114,6 @@ private:
 
 } // end namespace SymbolRewriter
 
-ModulePass *createRewriteSymbolsPass();
-ModulePass *createRewriteSymbolsPass(SymbolRewriter::RewriteDescriptorList &);
-
 class RewriteSymbolPass : public PassInfoMixin<RewriteSymbolPass> {
 public:
   RewriteSymbolPass() { loadAndParseMapFiles(); }
