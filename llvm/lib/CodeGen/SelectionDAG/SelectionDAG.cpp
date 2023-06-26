8e3b9b61f0c2fade56eafb880bad3ad41f91145c
Alex MacLean <amaclean@nvidia.com>
2023-06-26 16:12:43 -0700

    [SelectionDAG] Add memory size for CSEMap ID calculation
    In NVPTX `ReplaceVectorLoad()`, i1 and i8 types are promoted to i16,
    followed by a truncate operation. Thus, v2i8 (or v2i1) and v2i16 will
    have the same VTList, which causes a collision in CSEMap.
    
    To differentiate the original VTList, let's add the size in generating
    an ID. Otherwise the compiler crashes in refineAlignment:
    `MMO->getSize() == getSize() && "Size mismatch!"`
    
    Reviewed By: craig.topper
    
    Differential Revision: https://reviews.llvm.org/D153712

--- llvm/lib/CodeGen/SelectionDAG/SelectionDAG.cpp
+++ llvm/lib/CodeGen/SelectionDAG/SelectionDAG.cpp
@@ -927,6 +927,7 @@ static void AddNodeIDCustom(FoldingSetNodeID &ID, const SDNode *N) {
     ID.AddInteger(MN->getRawSubclassData());
     ID.AddInteger(MN->getPointerInfo().getAddrSpace());
     ID.AddInteger(MN->getMemOperand()->getFlags());
+    ID.AddInteger(MN->getMemoryVT().getRawBits());
   }
 }
 
@@ -8066,6 +8067,7 @@ SDValue SelectionDAG::getMemIntrinsicNode(unsigned Opcode, const SDLoc &dl,
         Opcode, dl.getIROrder(), VTList, MemVT, MMO));
     ID.AddInteger(MMO->getPointerInfo().getAddrSpace());
     ID.AddInteger(MMO->getFlags());
+    ID.AddInteger(MemVT.getRawBits());
     void *IP = nullptr;
     if (SDNode *E = FindNodeOrInsertPos(ID, dl, IP)) {
       cast<MemIntrinsicSDNode>(E)->refineAlignment(MMO);
