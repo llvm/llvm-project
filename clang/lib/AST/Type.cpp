ce358bd0ba8535a33b8564bafc6131ba34865527
Nikolas Klauser <nikolasklauser@berlin.de>
2023-06-25 20:19:01 -0700

    [clang] __is_trivially_equality_comparable should return false for arrays
    When comparing two arrays, their pointers are compared instead of their elements, which means that they are not trivially equality comparable
    
    Fixes #63371
    
    Reviewed By: cor3ntin
    
    Spies: cor3ntin, cfe-commits
    
    Differential Revision: https://reviews.llvm.org/D153737

--- clang/lib/AST/Type.cpp
+++ clang/lib/AST/Type.cpp
@@ -2684,7 +2684,7 @@ bool QualType::isTriviallyEqualityComparableType(
     const ASTContext &Context) const {
   QualType CanonicalType = getCanonicalType();
   if (CanonicalType->isIncompleteType() || CanonicalType->isDependentType() ||
-      CanonicalType->isEnumeralType())
+      CanonicalType->isEnumeralType() || CanonicalType->isArrayType())
     return false;
 
   if (const auto *RD = CanonicalType->getAsCXXRecordDecl()) {
