f03e104ab06e2f0dfcf7d272954c16e820d191ef
Joel Dice <joel.dice@fermyon.com>
2023-06-26 10:25:45 -0700

    [clang][WebAssembly] Support wasm32-wasi shared libraries
    This adds support for Emscripten-style shared libraries [1] to
    non-emscripten targets, such as `wasm32-wasi`.  Previously, only static
    linking was supported, and the `-shared` and `-fPIC` flags were simply
    ignored.  Now both flags are honored.
    
    Since WASI runtimes do not necessarily include JavaScript support, we
    cannot rely on the JS-based Emscripten linker to link shared libraries.
    Instead, we link them using the Component Model proposal [2].
    
    We have prototyped shared library support in `wasi-sdk` [3] and put
    together a demo [4] which uses a patched version of `wit-component` [5]
    to link libraries using the Component Model.  We plan to submit the
    required changes upstream to the respective repositories in the next
    week or two.
    
    [1] https://github.com/WebAssembly/tool-conventions/blob/main/DynamicLinking.md
    [2] https://github.com/WebAssembly/component-model/blob/main/design/mvp/examples/SharedEverythingDynamicLinking.md
    [3] https://github.com/dicej/wasi-sdk/tree/dynamic-linking
    [4] https://github.com/dicej/component-linking-demo
    [5] https://github.com/bytecodealliance/wasm-tools/tree/main/crates/wit-component
    
    Signed-off-by: Joel Dice <joel.dice@fermyon.com>
    
    Reviewed By: sbc100
    
    Differential Revision: https://reviews.llvm.org/D153293

--- clang/test/Driver/wasm-toolchain.c
+++ clang/test/Driver/wasm-toolchain.c
@@ -33,6 +33,20 @@
 // LINK_KNOWN: "-cc1" {{.*}} "-o" "[[temp:[^"]*]]"
 // LINK_KNOWN: wasm-ld{{.*}}" "-L/foo/lib/wasm32-wasi" "crt1.o" "[[temp]]" "-lc" "{{.*[/\\]}}libclang_rt.builtins-wasm32.a" "-o" "a.out"
 
+// -shared should be passed through to `wasm-ld` and not include crt1.o with a known OS.
+
+// RUN: %clang -### -shared --target=wasm32-wasi --sysroot=/foo %s 2>&1 \
+// RUN:   | FileCheck -check-prefix=LINK_KNOWN_SHARED %s
+// LINK_KNOWN_SHARED: "-cc1" {{.*}} "-o" "[[temp:[^"]*]]"
+// LINK_KNOWN_SHARED: wasm-ld{{.*}}" "-L/foo/lib/wasm32-wasi" "-shared" "[[temp]]" "-lc" "{{.*[/\\]}}libclang_rt.builtins-wasm32.a" "-o" "a.out"
+
+// -shared should be passed through to `wasm-ld` and not include crt1.o with an unknown OS.
+
+// RUN: %clang -### -shared --target=wasm32-unknown-unknown --sysroot=/foo %s 2>&1 \
+// RUN:   | FileCheck -check-prefix=LINK_UNKNOWN_SHARED %s
+// LINK_UNKNOWN_SHARED: "-cc1" {{.*}} "-o" "[[temp:[^"]*]]"
+// LINK_UNKNOWN_SHARED: wasm-ld{{.*}}" "-shared" "[[temp]]" "-lc" "{{.*[/\\]}}libclang_rt.builtins-wasm32.a" "-o" "a.out"
+
 // A basic C link command-line with optimization with known OS.
 
 // RUN: %clang -### -O2 --target=wasm32-wasi --sysroot=/foo %s 2>&1 \
@@ -45,6 +59,18 @@
 // RUN: %clang -### --target=wasm32-wasi --sysroot=/foo %s 2>&1 \
 // RUN:   | FileCheck -check-prefix=COMPILE %s
 // COMPILE: "-cc1" {{.*}} "-internal-isystem" "/foo/include/wasm32-wasi" "-internal-isystem" "/foo/include"
+
+// -fPIC should work on a known OS
+
+// RUN: %clang -### -fPIC --target=wasm32-wasi --sysroot=/foo %s 2>&1 \
+// RUN:   | FileCheck -check-prefix=COMPILE_KNOWN_PIC %s
+// COMPILE_KNOWN_PIC: "-cc1" {{.*}} "-mrelocation-model" "pic" "-pic-level" "2" {{.*}} "-internal-isystem" "/foo/include/wasm32-wasi" "-internal-isystem" "/foo/include"
+
+// -fPIC should work on an unknown OS
+
+// RUN: %clang -### -fPIC --target=wasm32-unknown-unknown --sysroot=/foo %s 2>&1 \
+// RUN:   | FileCheck -check-prefix=COMPILE_UNKNOWN_PIC %s
+// COMPILE_UNKNOWN_PIC: "-cc1" {{.*}} "-mrelocation-model" "pic" "-pic-level" "2"
 
 // Thread-related command line tests.
 
