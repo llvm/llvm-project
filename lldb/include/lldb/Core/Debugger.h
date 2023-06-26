d49caf4afc1bc460600b316f9736e01ceeebded5
Jonas Devlieghere <jonas@devlieghere.com>
2023-06-26 09:16:23 -0700

    [lldb] Add `source cache dump` and `source cache clear` subcommand
    Add two new source subcommands: source cache dump and source cache
    clear. As the name implies the first one dumps the source cache while
    the later clears the cache.
    
    This patch was motivated by a handful of (internal) bug reports related
    to sources not being available. Right now those issues can be hard to
    diagnose. The new commands give users, as well as us as developers, more
    insight into and control over the source cache.
    
    Differential revision: https://reviews.llvm.org/D153685

--- lldb/include/lldb/Core/Debugger.h
+++ lldb/include/lldb/Core/Debugger.h
@@ -76,8 +76,6 @@ class DataRecorder;
 class Debugger : public std::enable_shared_from_this<Debugger>,
                  public UserID,
                  public Properties {
-  friend class SourceManager; // For GetSourceFileCache.
-
 public:
   /// Broadcaster event bits definitions.
   enum {
@@ -515,6 +513,10 @@ public:
   void FlushProcessOutput(Process &process, bool flush_stdout,
                           bool flush_stderr);
 
+  SourceManager::SourceFileCache &GetSourceFileCache() {
+    return m_source_file_cache;
+  }
+
 protected:
   friend class CommandInterpreter;
   friend class REPL;
@@ -598,10 +600,6 @@ protected:
 
   // Ensures two threads don't attempt to flush process output in parallel.
   std::mutex m_output_flush_mutex;
-
-  SourceManager::SourceFileCache &GetSourceFileCache() {
-    return m_source_file_cache;
-  }
 
   void InstanceInitialize();
 
