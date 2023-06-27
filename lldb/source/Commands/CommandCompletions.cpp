b709149b76277e8bea4a3d7977ccb4e818499b7b
Alex Langford <alangford@apple.com>
2023-06-27 13:48:27 -0700

    [lldb][NFCI] Target::StopHook::GetDescription should take a Stream ref instead of pointer
    We always assume that this is valid anyway, might as well take a
    reference.
    
    Differential Revision: https://reviews.llvm.org/D153917

--- lldb/source/Commands/CommandCompletions.cpp
+++ lldb/source/Commands/CommandCompletions.cpp
@@ -754,7 +754,7 @@ void CommandCompletions::StopHookIDs(CommandInterpreter &interpreter,
     // neater.
     strm.SetIndentLevel(11);
     const Target::StopHookSP stophook_sp = target_sp->GetStopHookAtIndex(idx);
-    stophook_sp->GetDescription(&strm, lldb::eDescriptionLevelInitial);
+    stophook_sp->GetDescription(strm, lldb::eDescriptionLevelInitial);
     request.TryCompleteCurrentArg(std::to_string(stophook_sp->GetID()),
                                   strm.GetString());
   }
