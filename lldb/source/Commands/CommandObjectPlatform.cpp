3254623d73fb7252385817d8057640c9d5d5ffd1
David Spickett <david.spickett@linaro.org>
2023-06-27 14:04:41 +0000

    Revert "[LLDB] Fix the use of "platform process launch" with no extra arguments"
    This reverts commit cc0fc358540517a3d205243c27bd543afeae2b02 due to a failure
    reported on MacOS.

--- lldb/source/Commands/CommandObjectPlatform.cpp
+++ lldb/source/Commands/CommandObjectPlatform.cpp
@@ -1207,12 +1207,8 @@ protected:
       if (m_options.launch_info.GetExecutableFile()) {
         Debugger &debugger = GetDebugger();
 
-        if (argc == 0) {
-          // If no arguments were given to the command, use target.run-args.
-          Args target_run_args;
-          target->GetRunArguments(target_run_args);
-          m_options.launch_info.GetArguments().AppendArguments(target_run_args);
-        }
+        if (argc == 0)
+          target->GetRunArguments(m_options.launch_info.GetArguments());
 
         ProcessSP process_sp(platform_sp->DebugProcess(
             m_options.launch_info, debugger, *target, error));
