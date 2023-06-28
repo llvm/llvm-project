82a3969d710f5fb7a2ee4c9afadb648653923fef
Serge Pavlov <sepavloff@gmail.com>
2023-06-29 02:04:31 +0700

    Revert "[Clang] Reset FP options before function instantiations"
    This reverts commit 98390ccb80569e8fbb20e6c996b4b8cff87fbec6.
    It caused issue #63542.

--- clang/test/CodeGen/fp-template.cpp
+++ clang/test/CodeGen/fp-template.cpp
@@ -15,40 +15,4 @@ float func_01(float x, float y) {
 // CHECK-SAME:  (float noundef %{{.*}}, float noundef %{{.*}}) #[[ATTR01:[0-9]+]]{{.*}} {
 // CHECK:       call float @llvm.experimental.constrained.fadd.f32
 
-
-template <typename Ty>
-Ty templ_02(Ty x, Ty y) {
-  return x + y;
-}
-
-#pragma STDC FENV_ROUND FE_UPWARD
-
-template <typename Ty>
-Ty templ_03(Ty x, Ty y) {
-  return x - y;
-}
-
-#pragma STDC FENV_ROUND FE_TONEAREST
-
-float func_02(float x, float y) {
-  return templ_02(x, y);
-}
-
-// CHECK-LABEL: define {{.*}} float @_Z8templ_02IfET_S0_S0_
-// CHECK:       %add = fadd float %0, %1
-
-float func_03(float x, float y) {
-  return templ_03(x, y);
-}
-
-// CHECK-LABEL: define {{.*}} float @_Z8templ_03IfET_S0_S0_
-// CHECK:       call float @llvm.experimental.constrained.fsub.f32({{.*}}, metadata !"round.upward", metadata !"fpexcept.ignore")
-
-
-// This pragma sets non-default rounding mode before delayed parsing occurs. It
-// is used to check that the parsing uses FP options defined by command line
-// options or by pragma before the template definition but not by this pragma.
-#pragma STDC FENV_ROUND FE_TOWARDZERO
-
-
 // CHECK: attributes #[[ATTR01]] = { {{.*}}strictfp
