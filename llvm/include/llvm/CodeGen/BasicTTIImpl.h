cc8a346e3fa362c2b1319cca9883182fbf36b6db
Juan Manuel MARTINEZ CAAMAÑO <juamarti@amd.com>
2023-06-29 09:11:54 +0200

    [InlineCost][TargetTransformInfo][AMDGPU] Consider cost of alloca instructions in the caller (1/2)
    On AMDGPU, alloca instructions have penalty that can
    be avoided when SROA is applied after inlining.
    
    This patch introduces the default implementation of
    TargetTransformInfo::getCallerAllocaCost.
    
    Reviewed By: mtrofin
    
    Differential Revision: https://reviews.llvm.org/D149740

--- llvm/include/llvm/CodeGen/BasicTTIImpl.h
+++ llvm/include/llvm/CodeGen/BasicTTIImpl.h
@@ -538,6 +538,9 @@ public:
 
   unsigned getInliningThresholdMultiplier() const { return 1; }
   unsigned adjustInliningThreshold(const CallBase *CB) { return 0; }
+  unsigned getCallerAllocaCost(const CallBase *CB, const AllocaInst *AI) const {
+    return 0;
+  }
 
   int getInlinerVectorBonusPercent() const { return 150; }
 
