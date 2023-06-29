27134aec469036e7adf1abfe95a5cd29aa19149d
Christian Trott <crtrott@sandia.gov>
2023-06-29 13:36:07 -0600

    [libc++][mdspan] Implement layout_left
    This commit implements layout_left in support of C++23 mdspan
    (https://wg21.link/p0009). layout_left is a layout mapping policy
    whose index mapping corresponds to the memory layout of Fortran arrays.
    Thus the left most index has stride-1 access, and the right most index
    is associated with the largest stride.
    
    Co-authored-by: Damien L-G <dalg24@gmail.com>
    
    Differential Revision: https://reviews.llvm.org/D153783

--- libcxx/test/std/containers/views/mdspan/layout_right/assert.ctor.layout_left.pass.cpp
+++ libcxx/test/std/containers/views/mdspan/layout_right/assert.ctor.layout_left.pass.cpp
@@ -15,11 +15,13 @@
 
 // template<class OtherExtents>
 //   constexpr explicit(!is_convertible_v<OtherExtents, extents_type>)
-//     mapping(const mapping<OtherExtents>&) noexcept;
+//     mapping(const layout_left::mapping<OtherExtents>&) noexcept;
 
-// Constraints: is_constructible_v<extents_type, OtherExtents> is true.
+// Constraints:
+//   - extents_type::rank() <= 1 is true, and
+//   - is_constructible_v<extents_type, OtherExtents> is true.
 //
-// Preconditions: other.required_span_size() is representable as a value of type index_type.
+// Preconditions: other.required_span_size() is representable as a value of type index_type
 
 #include <mdspan>
 #include <cassert>
@@ -28,32 +30,26 @@
 
 int main(int, char**) {
   constexpr size_t D = std::dynamic_extent;
-  std::extents<int, D, D> arg_exts{100, 5};
-  std::layout_right::mapping<std::extents<int, D, D>> arg(arg_exts);
+  std::extents<int, D> arg_exts{5};
+  std::layout_left::mapping<std::extents<int, D>> arg(arg_exts);
 
   // working case
   {
-    [[maybe_unused]] std::layout_right::mapping<std::extents<size_t, D, 5>> m(arg); // should work
+    [[maybe_unused]] std::layout_right::mapping<std::extents<size_t, 5>> m(arg); // should work
   }
   // mismatch of static extent
   {
-    TEST_LIBCPP_ASSERT_FAILURE(([=] { std::layout_right::mapping<std::extents<int, D, 3>> m(arg); }()),
+    TEST_LIBCPP_ASSERT_FAILURE(([=] { std::layout_right::mapping<std::extents<int, 3>> m(arg); }()),
                                "extents construction: mismatch of provided arguments with static extents.");
   }
   // non-representability of extents itself
   {
     TEST_LIBCPP_ASSERT_FAILURE(([=] { std::layout_right::mapping<std::extents<char, D>> m(
-                                 std::layout_right::mapping<std::extents<int, D>>(std::extents<int, D>(500))); }()),
+                                 std::layout_left::mapping<std::extents<int, D>>(std::extents<int, D>(500))); }()),
                                "extents ctor: arguments must be representable as index_type and nonnegative");
   }
-  // required_span_size not representable, while individual extents are
-  {
-    // check extents would be constructible
-    [[maybe_unused]] std::extents<char, D, 5> e(arg_exts);
-    // but the product is not, so we can't use it for layout_right
-    TEST_LIBCPP_ASSERT_FAILURE(
-        ([=] { std::layout_right::template mapping<std::extents<char, D, 5>> m(arg); }()),
-        "layout_right::mapping converting ctor: other.required_span_size() must be representable as index_type.");
-  }
+
+  // Can't trigger required_span_size() representability assertion, since for rank-1 the above check will trigger first,
+  // and this conversion constructor is constrained on rank <= 1.
   return 0;
 }
