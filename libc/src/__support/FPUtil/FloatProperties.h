c855e3e60b1bf52c3f4361e9f743f50bb49a45a7
Michael Jones <michaelrj@google.com>
2023-06-29 13:53:57 -0700

    [libc] fix MPFR rounding problems in fuzz test
    The accuracy for the MPFR numbers in the strtofloat fuzz test was set
    too high, causing rounding issues when rounding to a smaller final
    result.
    
    Reviewed By: lntue
    
    Differential Revision: https://reviews.llvm.org/D154150

--- libc/src/__support/FPUtil/FloatProperties.h
+++ libc/src/__support/FPUtil/FloatProperties.h
@@ -28,6 +28,8 @@ template <> struct FloatProperties<float> {
   static constexpr uint32_t BIT_WIDTH = sizeof(BitsType) * 8;
 
   static constexpr uint32_t MANTISSA_WIDTH = 23;
+  // The mantissa precision includes the implicit bit.
+  static constexpr uint32_t MANTISSA_PRECISION = MANTISSA_WIDTH + 1;
   static constexpr uint32_t EXPONENT_WIDTH = 8;
   static constexpr BitsType MANTISSA_MASK = (BitsType(1) << MANTISSA_WIDTH) - 1;
   static constexpr BitsType SIGN_MASK = BitsType(1)
@@ -53,6 +55,7 @@ template <> struct FloatProperties<double> {
   static constexpr uint32_t BIT_WIDTH = sizeof(BitsType) * 8;
 
   static constexpr uint32_t MANTISSA_WIDTH = 52;
+  static constexpr uint32_t MANTISSA_PRECISION = MANTISSA_WIDTH + 1;
   static constexpr uint32_t EXPONENT_WIDTH = 11;
   static constexpr BitsType MANTISSA_MASK = (BitsType(1) << MANTISSA_WIDTH) - 1;
   static constexpr BitsType SIGN_MASK = BitsType(1)
@@ -82,6 +85,7 @@ template <> struct FloatProperties<long double> {
 
   static constexpr uint32_t MANTISSA_WIDTH =
       FloatProperties<double>::MANTISSA_WIDTH;
+  static constexpr uint32_t MANTISSA_PRECISION = MANTISSA_WIDTH + 1;
   static constexpr uint32_t EXPONENT_WIDTH =
       FloatProperties<double>::EXPONENT_WIDTH;
   static constexpr BitsType MANTISSA_MASK =
@@ -115,6 +119,7 @@ template <> struct FloatProperties<long double> {
   static constexpr BitsType FULL_WIDTH_MASK = ((BitsType(1) << BIT_WIDTH) - 1);
 
   static constexpr uint32_t MANTISSA_WIDTH = 63;
+  static constexpr uint32_t MANTISSA_PRECISION = MANTISSA_WIDTH + 1;
   static constexpr uint32_t EXPONENT_WIDTH = 15;
   static constexpr BitsType MANTISSA_MASK = (BitsType(1) << MANTISSA_WIDTH) - 1;
 
@@ -150,6 +155,7 @@ template <> struct FloatProperties<long double> {
   static constexpr uint32_t BIT_WIDTH = sizeof(BitsType) << 3;
 
   static constexpr uint32_t MANTISSA_WIDTH = 112;
+  static constexpr uint32_t MANTISSA_PRECISION = MANTISSA_WIDTH + 1;
   static constexpr uint32_t EXPONENT_WIDTH = 15;
   static constexpr BitsType MANTISSA_MASK = (BitsType(1) << MANTISSA_WIDTH) - 1;
   static constexpr BitsType SIGN_MASK = BitsType(1)
