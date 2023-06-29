b4ff893877ffe6f80fefd705a9e16a6dcd624851
Christian Trott <crtrott@sandia.gov>
2023-06-29 13:36:07 -0600

    [libc++][mdspan] Implement layout_left
    This commit implements layout_left in support of C++23 mdspan
    (https://wg21.link/p0009). layout_left is a layout mapping policy
    whose index mapping corresponds to the memory layout of Fortran arrays.
    Thus the left most index has stride-1 access, and the right most index
    is associated with the largest stride.
    
    Co-authored-by: Damien L-G <dalg24@gmail.com>
    
    Differential Revision: https://reviews.llvm.org/D153783

--- libcxx/include/__mdspan/layout_left.h
+++ libcxx/include/__mdspan/layout_left.h
@@ -14,8 +14,8 @@
 //
 //===---------------------------------------------------------------------===//
 
-#ifndef _LIBCPP___MDSPAN_LAYOUT_RIGHT_H
-#define _LIBCPP___MDSPAN_LAYOUT_RIGHT_H
+#ifndef _LIBCPP___MDSPAN_LAYOUT_LEFT_H
+#define _LIBCPP___MDSPAN_LAYOUT_LEFT_H
 
 #include <__assert>
 #include <__config>
@@ -25,6 +25,7 @@
 #include <__type_traits/is_convertible.h>
 #include <__type_traits/is_nothrow_constructible.h>
 #include <__utility/integer_sequence.h>
+#include <array>
 #include <cinttypes>
 #include <cstddef>
 #include <limits>
@@ -41,16 +42,16 @@ _LIBCPP_BEGIN_NAMESPACE_STD
 #if _LIBCPP_STD_VER >= 23
 
 template <class _Extents>
-class layout_right::mapping {
+class layout_left::mapping {
 public:
   static_assert(__mdspan_detail::__is_extents<_Extents>::value,
-                "layout_right::mapping template argument must be a specialization of extents.");
+                "layout_left::mapping template argument must be a specialization of extents.");
 
   using extents_type = _Extents;
   using index_type   = typename extents_type::index_type;
   using size_type    = typename extents_type::size_type;
   using rank_type    = typename extents_type::rank_type;
-  using layout_type  = layout_right;
+  using layout_type  = layout_left;
 
 private:
   _LIBCPP_HIDE_FROM_ABI static constexpr bool __required_span_size_is_representable(const extents_type& __ext) {
@@ -67,15 +68,15 @@ private:
   }
 
   static_assert((extents_type::rank_dynamic() > 0) || __required_span_size_is_representable(extents_type()),
-                "layout_right::mapping product of static extents must be representable as index_type.");
+                "layout_left::mapping product of static extents must be representable as index_type.");
 
 public:
-  // [mdspan.layout.right.cons], constructors
+  // [mdspan.layout.left.cons], constructors
   _LIBCPP_HIDE_FROM_ABI constexpr mapping() noexcept               = default;
   _LIBCPP_HIDE_FROM_ABI constexpr mapping(const mapping&) noexcept = default;
   _LIBCPP_HIDE_FROM_ABI constexpr mapping(const extents_type& __ext) noexcept : __extents_(__ext) {
     _LIBCPP_ASSERT(__required_span_size_is_representable(__ext),
-                   "layout_right::mapping extents ctor: product of extents must be representable as index_type.");
+                   "layout_left::mapping extents ctor: product of extents must be representable as index_type.");
   }
 
   template <class _OtherExtents>
@@ -85,17 +86,17 @@ public:
       : __extents_(__other.extents()) {
     _LIBCPP_ASSERT(
         __mdspan_detail::__is_representable_as<index_type>(__other.required_span_size()),
-        "layout_right::mapping converting ctor: other.required_span_size() must be representable as index_type.");
+        "layout_left::mapping converting ctor: other.required_span_size() must be representable as index_type.");
   }
 
   template <class _OtherExtents>
     requires(is_constructible_v<extents_type, _OtherExtents> && _OtherExtents::rank() <= 1)
   _LIBCPP_HIDE_FROM_ABI constexpr explicit(!is_convertible_v<_OtherExtents, extents_type>)
-      mapping(const layout_left::mapping<_OtherExtents>& __other) noexcept
+      mapping(const layout_right::mapping<_OtherExtents>& __other) noexcept
       : __extents_(__other.extents()) {
     _LIBCPP_ASSERT(
         __mdspan_detail::__is_representable_as<index_type>(__other.required_span_size()),
-        "layout_right::mapping converting ctor: other.required_span_size() must be representable as index_type.");
+        "layout_left::mapping converting ctor: other.required_span_size() must be representable as index_type.");
   }
 
 // FIXME: add when we add other layouts
@@ -107,7 +108,7 @@ public:
 
   _LIBCPP_HIDE_FROM_ABI constexpr mapping& operator=(const mapping&) noexcept = default;
 
-  // [mdspan.layout.right.obs], observers
+  // [mdspan.layout.left.obs], observers
   _LIBCPP_HIDE_FROM_ABI constexpr const extents_type& extents() const noexcept { return __extents_; }
 
   _LIBCPP_HIDE_FROM_ABI constexpr index_type required_span_size() const noexcept {
@@ -122,10 +123,12 @@ public:
              (is_nothrow_constructible_v<index_type, _Indices> && ...))
   _LIBCPP_HIDE_FROM_ABI constexpr index_type operator()(_Indices... __idx) const noexcept {
     _LIBCPP_ASSERT(__mdspan_detail::__is_multidimensional_index_in(__extents_, __idx...),
-                   "layout_right::mapping: out of bounds indexing");
+                   "layout_left::mapping: out of bounds indexing");
+    array<index_type, extents_type::rank()> __idx_a{static_cast<index_type>(__idx)...};
     return [&]<size_t... _Pos>(index_sequence<_Pos...>) {
       index_type __res = 0;
-      ((__res = static_cast<index_type>(__idx) + __extents_.extent(_Pos) * __res), ...);
+      ((__res = __idx_a[extents_type::rank() - 1 - _Pos] + __extents_.extent(extents_type::rank() - 1 - _Pos) * __res),
+       ...);
       return __res;
     }(make_index_sequence<sizeof...(_Indices)>());
   }
@@ -141,7 +144,7 @@ public:
   _LIBCPP_HIDE_FROM_ABI constexpr index_type stride(rank_type __r) const noexcept
     requires(extents_type::rank() > 0)
   {
-    _LIBCPP_ASSERT(__r < extents_type::rank(), "layout_right::mapping::stride(): invalid rank index");
+    _LIBCPP_ASSERT(__r < extents_type::rank(), "layout_left::mapping::stride(): invalid rank index");
     index_type __s = 1;
     for (rank_type __i = extents_type::rank() - 1; __i > __r; __i--)
       __s *= __extents_.extent(__i);
@@ -165,4 +168,4 @@ _LIBCPP_END_NAMESPACE_STD
 
 _LIBCPP_POP_MACROS
 
-#endif // _LIBCPP___MDSPAN_LAYOUT_RIGHT_H
+#endif // _LIBCPP___MDSPAN_LAYOUT_LEFT_H
