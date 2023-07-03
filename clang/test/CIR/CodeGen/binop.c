7ffaf84e7740b5b3aaadf0ac412e5182723814c6
Vinicius Couto Espindola <vini.couto.e@gmail.com>
2023-07-03 20:54:33 -0300

    [CIR][CodeGen] Support conditional result implicit cast
    In C, whenever we logically compare two values, we may store the result
    in any variable type. In this case, the boolean result should be cast to
    whatever type the variable is. This patch adds support for this.

---
+++ clang/test/CIR/CodeGen/binop.c
@@ -0,0 +1,13 @@
+// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fclangir -emit-cir %s -o %t.cir
+// RUN: FileCheck --input-file=%t.cir %s
+
+void conditionalResultIimplicitCast(int a, int b, float f) {
+  // Should implicit cast back to int.
+  int x = a && b;
+  // CHECK: %[[#INT:]] = cir.ternary
+  // CHECK: %{{.+}} = cir.cast(bool_to_int, %[[#INT]] : !cir.bool), !s32i
+  float y = f && f;
+  // CHECK: %[[#BOOL:]] = cir.ternary
+  // CHECK: %[[#INT:]] = cir.cast(bool_to_int, %[[#BOOL]] : !cir.bool), !s32i
+  // CHECK: %{{.+}} = cir.cast(int_to_float, %[[#INT]] : !s32i), f32
+}
