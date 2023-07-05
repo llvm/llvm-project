aaec5ce32978d8f0eafdcd32c9da83cd6772d34c
max <maksim.levental@gmail.com>
2023-07-05 15:02:59 -0500

    Add SymbolRefAttr to python bindings
    Differential Revision: https://reviews.llvm.org/D154541

--- mlir/test/python/dialects/ml_program.py
+++ mlir/test/python/dialects/ml_program.py
@@ -2,7 +2,7 @@
 # This is just a smoke test that the dialect is functional.
 
 from mlir.ir import *
-from mlir.dialects import ml_program
+from mlir.dialects import ml_program, arith, builtin
 
 
 def constructAndPrintInModule(f):
@@ -26,3 +26,21 @@ def testFuncOp():
     with InsertionPoint(block):
         # CHECK: ml_program.return
         ml_program.ReturnOp([block.arguments[0]])
+
+
+# CHECK-LABEL: testGlobalStoreOp
+@constructAndPrintInModule
+def testGlobalStoreOp():
+    # CHECK: %cst = arith.constant 4.242000e+01 : f32
+    cst = arith.ConstantOp(value=42.42, result=F32Type.get())
+
+    m = builtin.ModuleOp()
+    m.sym_name = StringAttr.get("symbol1")
+    m.sym_visibility = StringAttr.get("public")
+    # CHECK: module @symbol1 attributes {sym_visibility = "public"} {
+    # CHECK:   ml_program.global public mutable @symbol2 : f32
+    # CHECK: }
+    with InsertionPoint(m.body):
+        ml_program.GlobalOp("symbol2", F32Type.get(), is_mutable=True)
+    # CHECK: ml_program.global_store @symbol1::@symbol2 = %cst : f32
+    ml_program.GlobalStoreOp(["symbol1", "symbol2"], cst)
