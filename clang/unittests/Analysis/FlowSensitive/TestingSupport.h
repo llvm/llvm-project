fc9821a877d4e1443603d67539e4369b3ec72890
Sam McCall <sam.mccall@gmail.com>
2023-07-05 11:35:06 +0200

    Reland "[dataflow] Add dedicated representation of boolean formulas"
    This reverts commit 7a72ce98224be76d9328e65eee472381f7c8e7fe.
    
    Test problems were due to unspecified order of function arg evaluation.
    
    Reland "[dataflow] Replace most BoolValue subclasses with references to Formula (and AtomicBoolValue => Atom and BoolValue => Formula where appropriate)"
    
    This properly frees the Value hierarchy from managing boolean formulas.
    
    We still distinguish AtomicBoolValue; this type is used in client code.
    However we expect to convert such uses to BoolValue (where the
    distinction is not needed) or Atom (where atomic identity is intended),
    and then fold AtomicBoolValue into FormulaBoolValue.
    
    We also distinguish TopBoolValue; this has distinct rules for
    widen/join/equivalence, and top-ness is not represented in Formula.
    It'd be nice to find a cleaner representation (e.g. the absence of a
    formula), but no immediate plans.
    
    For now, BoolValues with the same Formula are deduplicated. This doesn't
    seem desirable, as Values are mutable by their creators (properties).
    We can probably drop this for FormulaBoolValue immediately (not in this
    patch, to isolate changes). For AtomicBoolValue we first need to update
    clients to stop using value pointers for atom identity.
    
    The data structures around flow conditions are updated:
    - flow condition tokens are Atom, rather than AtomicBoolValue*
    - conditions are Formula, rather than BoolValue
    Most APIs were changed directly, some with many clients had a
    new version added and the existing one deprecated.
    
    The factories for BoolValues in Environment keep their existing
    signatures for now (e.g. makeOr(BoolValue, BoolValue) => BoolValue)
    and are not deprecated. These have very many clients and finding the
    most ergonomic API & migration path still needs some thought.
    
    Differential Revision: https://reviews.llvm.org/D153469

--- clang/unittests/Analysis/FlowSensitive/TestingSupport.h
+++ clang/unittests/Analysis/FlowSensitive/TestingSupport.h
@@ -43,6 +43,7 @@
 #include "llvm/ADT/DenseMap.h"
 #include "llvm/ADT/StringMap.h"
 #include "llvm/ADT/StringRef.h"
+#include "llvm/Support/Allocator.h"
 #include "llvm/Support/Errc.h"
 #include "llvm/Support/Error.h"
 #include "llvm/Testing/Annotations/Annotations.h"
@@ -442,55 +443,44 @@ ValueT &getValueForDecl(ASTContext &ASTCtx, const Environment &Env,
 
 /// Creates and owns constraints which are boolean values.
 class ConstraintContext {
+  unsigned NextAtom = 0;
+  llvm::BumpPtrAllocator A;
+
+  const Formula *make(Formula::Kind K,
+                      llvm::ArrayRef<const Formula *> Operands) {
+    return &Formula::create(A, K, Operands);
+  }
+
 public:
-  // Creates an atomic boolean value.
-  BoolValue *atom() {
-    Vals.push_back(std::make_unique<AtomicBoolValue>());
-    return Vals.back().get();
+  // Returns a reference to a fresh atomic variable.
+  const Formula *atom() {
+    return &Formula::create(A, Formula::AtomRef, {}, NextAtom++);
   }
 
-  // Creates an instance of the Top boolean value.
-  BoolValue *top() {
-    Vals.push_back(std::make_unique<TopBoolValue>());
-    return Vals.back().get();
+  // Creates a boolean conjunction.
+  const Formula *conj(const Formula *LHS, const Formula *RHS) {
+    return make(Formula::And, {LHS, RHS});
   }
 
-  // Creates a boolean conjunction value.
-  BoolValue *conj(BoolValue *LeftSubVal, BoolValue *RightSubVal) {
-    Vals.push_back(
-        std::make_unique<ConjunctionValue>(*LeftSubVal, *RightSubVal));
-    return Vals.back().get();
+  // Creates a boolean disjunction.
+  const Formula *disj(const Formula *LHS, const Formula *RHS) {
+    return make(Formula::Or, {LHS, RHS});
   }
 
-  // Creates a boolean disjunction value.
-  BoolValue *disj(BoolValue *LeftSubVal, BoolValue *RightSubVal) {
-    Vals.push_back(
-        std::make_unique<DisjunctionValue>(*LeftSubVal, *RightSubVal));
-    return Vals.back().get();
+  // Creates a boolean negation.
+  const Formula *neg(const Formula *Operand) {
+    return make(Formula::Not, {Operand});
   }
 
-  // Creates a boolean negation value.
-  BoolValue *neg(BoolValue *SubVal) {
-    Vals.push_back(std::make_unique<NegationValue>(*SubVal));
-    return Vals.back().get();
+  // Creates a boolean implication.
+  const Formula *impl(const Formula *LHS, const Formula *RHS) {
+    return make(Formula::Implies, {LHS, RHS});
   }
 
-  // Creates a boolean implication value.
-  BoolValue *impl(BoolValue *LeftSubVal, BoolValue *RightSubVal) {
-    Vals.push_back(
-        std::make_unique<ImplicationValue>(*LeftSubVal, *RightSubVal));
-    return Vals.back().get();
+  // Creates a boolean biconditional.
+  const Formula *iff(const Formula *LHS, const Formula *RHS) {
+    return make(Formula::Equal, {LHS, RHS});
   }
-
-  // Creates a boolean biconditional value.
-  BoolValue *iff(BoolValue *LeftSubVal, BoolValue *RightSubVal) {
-    Vals.push_back(
-        std::make_unique<BiconditionalValue>(*LeftSubVal, *RightSubVal));
-    return Vals.back().get();
-  }
-
-private:
-  std::vector<std::unique_ptr<BoolValue>> Vals;
 };
 
 } // namespace test
