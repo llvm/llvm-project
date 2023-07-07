260ab45f5a3634377f333599d70fd286bfb9791e
Vinicius Couto Espindola <vini.couto.e@gmail.com>
2023-07-07 10:43:28 -0300

    [CIR] Remove the Executables tests folder
    We should not have tests that depend on target-specific executables.
    This patch removes the 'hello.c' executable test and converts it to both
    a codegen and a lowering test.
    
    ghstack-source-id: caa8f3aa01bbc89693ac8724ad7fac1a96114f1a
    Pull Request resolved: https://github.com/llvm/clangir/pull/155

---
+++ clang/test/CIR/CodeGen/hello.c
@@ -0,0 +1,22 @@
+// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fclangir -emit-cir %s -o %t.cir
+// RUN: FileCheck --input-file=%t.cir %s
+int printf(const char *restrict, ...);
+
+int main (void) {
+    printf ("Hello, world!\n");
+    return 0;
+}
+
+// CHECK: cir.func private @printf(!cir.ptr<!s8i>, ...) -> !s32i
+// CHECK: cir.global "private" constant internal @".str" = #cir.const_array<"Hello, world!\0A\00" : !cir.array<!s8i x 15>> : !cir.array<!s8i x 15> {alignment = 1 : i64}
+// CHECK: cir.func @main() -> !s32i {
+// CHECK:   %0 = cir.alloca !s32i, cir.ptr <!s32i>, ["__retval"] {alignment = 4 : i64}
+// CHECK:   %1 = cir.get_global @printf : cir.ptr <!cir.func<!s32i (!cir.ptr<!s8i>, ...)>>
+// CHECK:   %2 = cir.get_global @".str" : cir.ptr <!cir.array<!s8i x 15>>
+// CHECK:   %3 = cir.cast(array_to_ptrdecay, %2 : !cir.ptr<!cir.array<!s8i x 15>>), !cir.ptr<!s8i>
+// CHECK:   %4 = cir.call @printf(%3) : (!cir.ptr<!s8i>) -> !s32i
+// CHECK:   %5 = cir.const(#cir.int<0> : !s32i) : !s32i
+// CHECK:   cir.store %5, %0 : !s32i, cir.ptr <!s32i>
+// CHECK:   %6 = cir.load %0 : cir.ptr <!s32i>, !s32i
+// CHECK:   cir.return %6 : !s32i
+// CHECK: }
