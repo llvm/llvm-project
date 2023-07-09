bfd94882f2648e2a5ed651bca6cfeb4fb7788b86
Guillaume Chatelet <gchatelet@google.com>
2023-07-09 21:34:50 +0000

    [libc][NFC] Move aligned access implementations to separate header
    Follow up on https://reviews.llvm.org/D154770
    
    Differential Revision: https://reviews.llvm.org/D154800

--- libc/src/string/memory_utils/memcmp_implementations.h
+++ libc/src/string/memory_utils/memcmp_implementations.h
@@ -12,6 +12,7 @@
 #include "src/__support/common.h"
 #include "src/__support/macros/optimization.h" // LIBC_UNLIKELY LIBC_LOOP_NOUNROLL
 #include "src/__support/macros/properties/architectures.h"
+#include "src/string/memory_utils/generic/aligned_access.h"
 #include "src/string/memory_utils/generic/byte_per_byte.h"
 #include "src/string/memory_utils/op_generic.h"
 #include "src/string/memory_utils/op_riscv.h"
@@ -26,60 +27,6 @@
 #endif
 
 namespace __llvm_libc {
-
-[[maybe_unused]] LIBC_INLINE MemcmpReturnType
-inline_memcmp_aligned_access_64bit(CPtr p1, CPtr p2, size_t count) {
-  constexpr size_t kAlign = sizeof(uint64_t);
-  if (count <= 2 * kAlign)
-    return inline_memcmp_byte_per_byte(p1, p2, count);
-  size_t bytes_to_p1_align = distance_to_align_up<kAlign>(p1);
-  if (auto value = inline_memcmp_byte_per_byte(p1, p2, bytes_to_p1_align))
-    return value;
-  size_t offset = bytes_to_p1_align;
-  size_t p2_alignment = distance_to_align_down<kAlign>(p2 + offset);
-  for (; offset < count - kAlign; offset += kAlign) {
-    uint64_t b;
-    if (p2_alignment == 0)
-      b = load64_aligned<uint64_t>(p2, offset);
-    else if (p2_alignment == 4)
-      b = load64_aligned<uint32_t, uint32_t>(p2, offset);
-    else if (p2_alignment == 2)
-      b = load64_aligned<uint16_t, uint16_t, uint16_t, uint16_t>(p2, offset);
-    else
-      b = load64_aligned<uint8_t, uint16_t, uint16_t, uint16_t, uint8_t>(
-          p2, offset);
-    uint64_t a = load64_aligned<uint64_t>(p1, offset);
-    if (a != b)
-      return cmp_neq_uint64_t(Endian::to_big_endian(a),
-                              Endian::to_big_endian(b));
-  }
-  return inline_memcmp_byte_per_byte(p1, p2, count, offset);
-}
-
-[[maybe_unused]] LIBC_INLINE MemcmpReturnType
-inline_memcmp_aligned_access_32bit(CPtr p1, CPtr p2, size_t count) {
-  constexpr size_t kAlign = sizeof(uint32_t);
-  if (count <= 2 * kAlign)
-    return inline_memcmp_byte_per_byte(p1, p2, count);
-  size_t bytes_to_p1_align = distance_to_align_up<kAlign>(p1);
-  if (auto value = inline_memcmp_byte_per_byte(p1, p2, bytes_to_p1_align))
-    return value;
-  size_t offset = bytes_to_p1_align;
-  size_t p2_alignment = distance_to_align_down<kAlign>(p2 + offset);
-  for (; offset < count - kAlign; offset += kAlign) {
-    uint32_t b;
-    if (p2_alignment == 0)
-      b = load32_aligned<uint32_t>(p2, offset);
-    else if (p2_alignment == 2)
-      b = load32_aligned<uint16_t, uint16_t>(p2, offset);
-    else
-      b = load32_aligned<uint8_t, uint16_t, uint8_t>(p2, offset);
-    uint32_t a = load32_aligned<uint32_t>(p1, offset);
-    if (a != b)
-      return cmp_uint32_t(Endian::to_big_endian(a), Endian::to_big_endian(b));
-  }
-  return inline_memcmp_byte_per_byte(p1, p2, count, offset);
-}
 
 LIBC_INLINE MemcmpReturnType inline_memcmp(CPtr p1, CPtr p2, size_t count) {
 #if defined(LIBC_TARGET_ARCH_IS_X86)
