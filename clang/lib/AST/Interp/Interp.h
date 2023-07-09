246ec8731bfc98b620b09aad57e9a124aedd1927
Timm Bäder <tbaeder@redhat.com>
2023-07-09 20:09:59 +0200

    [clang][Interp] Check pointers for live-ness when returning them
    We might be trying to return a pointer or reference to a local variable,
    which doesn't work.
    
    Differential Revision: https://reviews.llvm.org/D154795

--- clang/lib/AST/Interp/Interp.h
+++ clang/lib/AST/Interp/Interp.h
@@ -181,6 +181,17 @@ template <PrimType Name, class T = typename PrimConv<Name>::T>
 bool Ret(InterpState &S, CodePtr &PC, APValue &Result) {
   const T &Ret = S.Stk.pop<T>();
 
+  // Make sure returned pointers are live. We might be trying to return a
+  // pointer or reference to a local variable.
+  // Just return false, since a diagnostic has already been emitted in Sema.
+  if constexpr (std::is_same_v<T, Pointer>) {
+    // FIXME: We could be calling isLive() here, but the emitted diagnostics
+    // seem a little weird, at least if the returned expression is of
+    // pointer type.
+    if (!Ret.isLive())
+      return false;
+  }
+
   assert(S.Current->getFrameOffset() == S.Stk.size() && "Invalid frame");
   if (!S.checkingPotentialConstantExpression() || S.Current->Caller)
     S.Current->popArgs();
