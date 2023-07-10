fb9a74122ef7730ae9839c63c4b7e9a78549d8e6
Alex Gatea <alexgatea@gmail.com>
2023-07-10 15:39:35 -0400

    [CodeGen][NFCI] Avoid calls to setTargetAttributes on definitions
    Avoid duplicate calls to setTargetAttributes on global variable definitions.
    
    Differential: https://reviews.llvm.org/D153903

--- clang/lib/CodeGen/CodeGenModule.cpp
+++ clang/lib/CodeGen/CodeGenModule.cpp
@@ -4765,7 +4765,8 @@ CodeGenModule::GetOrCreateLLVMGlobal(StringRef MangledName, llvm::Type *Ty,
     }
   }
 
-  if (GV->isDeclaration()) {
+  if (D &&
+      D->isThisDeclarationADefinition(Context) == VarDecl::DeclarationOnly) {
     getTargetCodeGenInfo().setTargetAttributes(D, GV, *this);
     // External HIP managed variables needed to be recorded for transformation
     // in both device and host compilations.
