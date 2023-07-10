ee44310a48b7d14336de75e170110e2fea90842b
Jonas Devlieghere <jonas@devlieghere.com>
2023-07-09 20:32:38 -0700

    [lldb] Support Compact C Type Format (CTF)
    Add support for the Compact C Type Format (CTF) in LLDB. The format
    describes the layout and sizes of C types. It is most commonly consumed
    by dtrace.
    
    We generate CTF for the XNU kernel and want to be able to use this in
    LLDB to debug kernels for which we don't have dSYMs (anymore). CTF is a
    much more limited debug format than DWARF which allows is to be an order
    of magnitude smaller: a 1GB dSYM can be converted to a handful of
    megabytes of CTF. For XNU, the goal is not to replace DWARF, but rather
    to have CTF serve as a "better than nothing" debug info format when
    DWARF is not available.
    
    It's worth noting that the LLVM toolchain does not support emitting CTF.
    XNU uses ctfconvert to generate CTF from DWARF which is used for
    testing.
    
    Differential revision: https://reviews.llvm.org/D154862

---
+++ lldb/test/API/macosx/ctf/TestCTF.py
@@ -0,0 +1,71 @@
+import lldbsuite.test.lldbutil as lldbutil
+from lldbsuite.test.lldbtest import *
+from lldbsuite.test.decorators import *
+import shutil
+import os
+
+
+class TestCTF(TestBase):
+    NO_DEBUG_INFO_TESTCASE = True
+
+    def no_ctf_convert(self):
+        if not shutil.which("ctfconvert"):
+            return "ctfconvert not found in path"
+        return None
+
+    def no_objcopy(self):
+        if not "OBJCOPY" in os.environ:
+            return "llvm-objcopy not found in environment"
+        return None
+
+    @skipTestIfFn(no_ctf_convert)
+    @skipTestIfFn(no_objcopy)
+    @skipUnlessDarwin
+    def test(self):
+        self.build()
+
+        lldbutil.run_to_name_breakpoint(self, "printf")
+
+        symbol_file = self.getBuildArtifact("a.ctf")
+
+        self.runCmd("target symbols add {}".format(symbol_file))
+        self.expect(
+            "target variable foo",
+            substrs=[
+                "(MyStructT) foo",
+                "i = 1",
+                "foo",
+                "'c'",
+                "[0] = 'c'",
+                "[1] = 'a'",
+                "[2] = 'b'",
+                "[3] = 'c'",
+                'u = (i = 1, s = "")',
+                "f = 0x0000000000000000",
+            ],
+        )
+
+        self.expect("target variable foo.n.i", substrs=["(MyInt) foo.n.i = 1"])
+        self.expect(
+            "target variable foo.n.s", substrs=["(const char *) foo.n.s", '"foo"']
+        )
+        self.expect(
+            "target variable foo.n.c", substrs=["(volatile char) foo.n.c = 'c'"]
+        )
+        self.expect(
+            "target variable foo.n.a",
+            substrs=[
+                "(char[4]:8) foo.n.a",
+                "[0] = 'c'",
+                "[1] = 'a'",
+                "[2] = 'b'",
+                "[3] = 'c'",
+            ],
+        )
+        self.expect(
+            "target variable foo.n.u", substrs=['(MyUnionT) foo.n.u = (i = 1, s = "")']
+        )
+        self.expect(
+            "target variable foo.f",
+            substrs=["(void (*)(int)) foo.f = 0x0000000000000000"],
+        )
