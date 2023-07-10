5f038e0e20641515fe412e1df298a62eb657f554
Corentin Jabot <corentinjabot@gmail.com>
2023-07-10 09:05:01 +0200

    [Clang][NFC] Fix "initalizer" typo

--- clang/lib/Sema/SemaDecl.cpp
+++ clang/lib/Sema/SemaDecl.cpp
@@ -19601,7 +19601,7 @@ static void CheckForDuplicateEnumValues(Sema &S, ArrayRef<Decl *> Elements,
       return;
     }
 
-    // Constants with initalizers are handled in the next loop.
+    // Constants with initializers are handled in the next loop.
     if (ECD->getInitExpr())
       continue;
 
