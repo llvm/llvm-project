728a8d5a81f35f24492fcd31928ef95e597a66af
Tobias Gysi <tobias.gysi@nextsilicon.com>
2023-07-11 07:30:18 +0000

    [mlir] Add a builtin distinct attribute
    A distinct attribute associates a referenced attribute with a unique
    identifier. Every call to its create function allocates a new
    distinct attribute instance. The address of the attribute instance
    temporarily serves as its unique identifier. Similar to the names
    of SSA values, the final unique identifiers are generated during
    pretty printing.
    
    Examples:
     #distinct = distinct[0]<42.0 : f32>
     #distinct1 = distinct[1]<42.0 : f32>
     #distinct2 = distinct[2]<array<i32: 10, 42>>
    
    This mechanism is meant to generate attributes with a unique
    identifier, which can be used to mark groups of operations
    that share a common properties such as if they are aliasing.
    
    The design of the distinct attribute ensures minimal memory
    footprint per distinct attribute since it only contains a reference
    to another attribute. All distinct attributes are stored outside of
    the storage uniquer in a thread local store that is part of the
    context. It uses one bump pointer allocator per thread to ensure
    distinct attributes can be created in-parallel.
    
    Reviewed By: rriddle, Dinistro, zero9178
    
    Differential Revision: https://reviews.llvm.org/D153360

--- mlir/lib/IR/AsmPrinter.cpp
+++ mlir/lib/IR/AsmPrinter.cpp
@@ -806,6 +806,8 @@ private:
     } else if (llvm::isa<AffineMapAttr, DenseArrayAttr, FloatAttr, IntegerAttr,
                          IntegerSetAttr, UnitAttr>(attr)) {
       return;
+    } else if (auto distinctAttr = dyn_cast<DistinctAttr>(attr)) {
+      printAttribute(distinctAttr.getReferencedAttr());
     } else if (auto dictAttr = dyn_cast<DictionaryAttr>(attr)) {
       for (const NamedAttribute &nestedAttr : dictAttr.getValue()) {
         printAttribute(nestedAttr.getName());
@@ -1605,6 +1607,31 @@ StringRef SSANameState::uniqueValueName(StringRef name) {
 }
 
 //===----------------------------------------------------------------------===//
+// DistinctState
+//===----------------------------------------------------------------------===//
+
+namespace {
+/// This class manages the state for distinct attributes.
+class DistinctState {
+public:
+  /// Returns a unique identifier for the given distinct attribute.
+  uint64_t getId(DistinctAttr distinctAttr);
+
+private:
+  uint64_t distinctCounter = 0;
+  DenseMap<DistinctAttr, uint64_t> distinctAttrMap;
+};
+} // namespace
+
+uint64_t DistinctState::getId(DistinctAttr distinctAttr) {
+  auto [it, inserted] =
+      distinctAttrMap.try_emplace(distinctAttr, distinctCounter);
+  if (inserted)
+    distinctCounter++;
+  return it->getSecond();
+}
+
+//===----------------------------------------------------------------------===//
 // Resources
 //===----------------------------------------------------------------------===//
 
@@ -1715,6 +1742,9 @@ public:
   /// Get the state used for SSA names.
   SSANameState &getSSANameState() { return nameState; }
 
+  /// Get the state used for distinct attribute identifiers.
+  DistinctState &getDistinctState() { return distinctState; }
+
   /// Return the dialects within the context that implement
   /// OpAsmDialectInterface.
   DialectInterfaceCollection<OpAsmDialectInterface> &getDialectInterfaces() {
@@ -1757,6 +1787,9 @@ private:
 
   /// The state used for SSA value names.
   SSANameState nameState;
+
+  /// The state used for distinct attribute identifiers.
+  DistinctState distinctState;
 
   /// Flags that control op output.
   OpPrintingFlags printerFlags;
@@ -2105,6 +2138,11 @@ void AsmPrinter::Impl::printAttributeImpl(Attribute attr,
                        opaqueAttr.getAttrData());
   } else if (llvm::isa<UnitAttr>(attr)) {
     os << "unit";
+    return;
+  } else if (auto distinctAttr = llvm::dyn_cast<DistinctAttr>(attr)) {
+    os << "distinct[" << state.getDistinctState().getId(distinctAttr) << "]<";
+    printAttribute(distinctAttr.getReferencedAttr());
+    os << '>';
     return;
   } else if (auto dictAttr = llvm::dyn_cast<DictionaryAttr>(attr)) {
     os << '{';
