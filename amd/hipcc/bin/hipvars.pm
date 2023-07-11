ea296b35f462fdd6dd25a8ad2c12cebd08410f51
Konstantin Zhuravlyov <kzhuravl_dev@outlook.com>
2023-07-11 11:35:44 -0400

    SWDEV-409068 - Fix can_run arg

--- amd/hipcc/bin/hipvars.pm
+++ amd/hipcc/bin/hipvars.pm
@@ -124,7 +124,7 @@ if (defined $HIP_RUNTIME and $HIP_RUNTIME eq "rocclr" and !defined $HIP_ROCCLR_H
 }
 
 if (not defined $HIP_PLATFORM) {
-    if (can_run("$HIP_CLANG_PATH/clang++") or can_run("clang++")) {
+    if (can_run("\"$HIP_CLANG_PATH/clang++\"") or can_run("clang++")) {
         $HIP_PLATFORM = "amd";
     } elsif (can_run("$CUDA_PATH/bin/nvcc") or can_run("nvcc")) {
         $HIP_PLATFORM = "nvidia";
