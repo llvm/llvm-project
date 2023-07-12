c0fead30f5d491adda463e2cd39692caeaae52ed
Tobias Hieta <tobias.hieta@ubisoft.com>
2023-07-12 09:22:06 +0200

    [lld][COFF] Add -print-search-paths for debugging.
    While working on adding more implicit search paths to the
    lld COFF driver, it was helpful to have a way to print all
    the search paths, both for debugging and for testing without
    having to create very complicated test cases.
    
    This is a simple arg that just prints the search paths and exits.
    
    Related to the efforts in #63827
    
    Differential Revision: https://reviews.llvm.org/D155047

--- lld/COFF/Driver.cpp
+++ lld/COFF/Driver.cpp
@@ -1500,6 +1500,7 @@ void LinkerDriver::linkerMain(ArrayRef<const char *> argsArr) {
     config->showTiming = true;
 
   config->showSummary = args.hasArg(OPT_summary);
+  config->printSearchPaths = args.hasArg(OPT_print_search_paths);
 
   // Handle --version, which is an lld extension. This option is a bit odd
   // because it doesn't start with "/", but we deliberately chose "--" to
@@ -2051,6 +2052,17 @@ void LinkerDriver::linkerMain(ArrayRef<const char *> argsArr) {
     addWinSysRootLibSearchPaths();
   }
   config->wordsize = config->is64() ? 8 : 4;
+
+  if (config->printSearchPaths) {
+    SmallString<256> buffer;
+    raw_svector_ostream stream(buffer);
+    stream << "Library search paths:\n";
+
+    for (StringRef path : searchPaths)
+      stream << "  " << path << "\n";
+
+    message(buffer);
+  }
 
   // Process files specified as /defaultlib. These must be processed after
   // addWinSysRootLibSearchPaths(), which is why they are in a separate loop.
