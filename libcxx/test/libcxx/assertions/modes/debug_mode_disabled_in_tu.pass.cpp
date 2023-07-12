d1367ca46ee40dd76661e3f551515d77301568c0
varconst <varconsteq@gmail.com>
2023-07-12 10:12:51 -0700

    [libc++][hardening][NFC] Add macros to enable hardened mode.
    This patch only adds new configuration knobs -- the actual assertions
    will be added in follow-up patches.
    
    Differential Revision: https://reviews.llvm.org/D153902

---
+++ libcxx/test/libcxx/assertions/modes/debug_mode_disabled_in_tu.pass.cpp
@@ -0,0 +1,23 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// This test ensures that we can disable the debug mode on a per-TU basis regardless of how the library was built.
+
+// TODO(hardening): currently, explicitly enabling assertions enables all uncategorized assertions and overrides
+// disabling the debug mode.
+// UNSUPPORTED: libcpp-has-hardened-mode, libcpp-has-assertions
+// ADDITIONAL_COMPILE_FLAGS: -Wno-macro-redefined -D_LIBCPP_ENABLE_DEBUG_MODE=0
+
+#include <cassert>
+
+int main(int, char**) {
+  _LIBCPP_ASSERT_UNCATEGORIZED(true, "Should not fire");
+  _LIBCPP_ASSERT_UNCATEGORIZED(false, "Also should not fire");
+
+  return 0;
+}
