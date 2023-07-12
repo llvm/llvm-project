dc26fc40523c323c362cfc67399c57f2862b6e56
Vinicius Couto Espindola <vini.couto.e@gmail.com>
2023-07-12 10:23:33 -0300

    [CIR][CIRGen] Generate zero initializer for pointers and structs
    ghstack-source-id: 9d59b38339fc8c8d6ddce8b4bdb3f00f450c678b
    Pull Request resolved: https://github.com/llvm/clangir/pull/162

---
+++ clang/test/CIR/CodeGen/pointer.cpp
@@ -0,0 +1,6 @@
+// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fclangir -emit-cir %s -o %t.cir
+// RUN: FileCheck --input-file=%t.cir %s
+
+// Global pointer should be zero initialized by default.
+int *ptr;
+// CHECK: cir.global external @ptr = #cir.null : !cir.ptr<!s32i>
