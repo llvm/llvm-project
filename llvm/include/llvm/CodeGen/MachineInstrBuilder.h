de79233b2e14a79d6526f4e74f35f88c8fa97296
Marco Elver <elver@google.com>
2023-07-12 15:08:43 +0200

    [X86] Complete preservation of !pcsections in X86ISelLowering
    https://reviews.llvm.org/D130883 introduced MIMetadata to simplify
    metadata propagation (DebugLoc and PCSections).
    
    However, we're currently still permitting implicit conversion of
    DebugLoc to MIMetadata, to allow for a gradual transition and let the
    old code work as-is.
    
    This manifests in lost !pcsections metadata for X86-specific lowerings.
    For example, 128-bit atomics.
    
    Fix the situation for X86ISelLowering by converting all BuildMI() calls
    to use an explicitly constructed MIMetadata.
    
    Reviewed By: dvyukov
    
    Differential Revision: https://reviews.llvm.org/D154986

--- llvm/include/llvm/CodeGen/MachineInstrBuilder.h
+++ llvm/include/llvm/CodeGen/MachineInstrBuilder.h
@@ -9,7 +9,8 @@
 // This file exposes a function named BuildMI, which is useful for dramatically
 // simplifying how MachineInstr's are created.  It allows use of code like this:
 //
-//   M = BuildMI(MBB, MI, DL, TII.get(X86::ADD8rr), Dst)
+//   MIMetadata MIMD(MI);  // Propagates DebugLoc and other metadata
+//   M = BuildMI(MBB, MI, MIMD, TII.get(X86::ADD8rr), Dst)
 //           .addReg(argVal1)
 //           .addReg(argVal2);
 //
