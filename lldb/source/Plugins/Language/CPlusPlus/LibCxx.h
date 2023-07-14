a6ff60ecdd061cc21383108ac092ae4a700f9143
Dave Lee <davelee.com@gmail.com>
2023-07-14 10:25:24 -0700

    [lldb] Delete unused LibcxxOptionalSummaryProvider (NFC)
    No longer needed following refactoring in D115178.

--- lldb/source/Plugins/Language/CPlusPlus/LibCxx.h
+++ lldb/source/Plugins/Language/CPlusPlus/LibCxx.h
@@ -59,10 +59,6 @@ bool LibcxxWStringViewSummaryProvider(
     ValueObject &valobj, Stream &stream,
     const TypeSummaryOptions &options); // libc++ std::wstring_view
 
-bool LibcxxOptionalSummaryProvider(
-    ValueObject &valobj, Stream &stream,
-    const TypeSummaryOptions &options); // libc++ std::optional<>
-
 bool LibcxxSmartPointerSummaryProvider(
     ValueObject &valobj, Stream &stream,
     const TypeSummaryOptions
