9d138baeb58136d76b244ff8102f525f47108c29
Thorsten Schütt <schuett@gmail.com>
2023-07-14 10:48:25 +0200

    [GIsel][AArch64]  extend legalization of G_INSERT_VECTOR_ELT
    Fixes https://github.com/llvm/llvm-project/issues/63826
    
    Reviewed By: aemerson
    
    Differential Revision: https://reviews.llvm.org/D155274

--- llvm/lib/Target/AArch64/GISel/AArch64LegalizerInfo.cpp
+++ llvm/lib/Target/AArch64/GISel/AArch64LegalizerInfo.cpp
@@ -702,7 +702,9 @@ AArch64LegalizerInfo::AArch64LegalizerInfo(const AArch64Subtarget &ST)
       .clampMaxNumElements(1, p0, 2);
 
   getActionDefinitionsBuilder(G_INSERT_VECTOR_ELT)
-      .legalIf(typeInSet(0, {v16s8, v8s8, v8s16, v4s16, v4s32, v2s32, v2s64}));
+      .legalIf(typeInSet(0, {v16s8, v8s8, v8s16, v4s16, v4s32, v2s32, v2s64}))
+      .clampMinNumElements(0, s16, 4)
+      .clampMaxNumElements(0, s16, 8);
 
   getActionDefinitionsBuilder(G_BUILD_VECTOR)
       .legalFor({{v8s8, s8},
