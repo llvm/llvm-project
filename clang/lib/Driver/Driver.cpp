f3ac0ac3d957157cb4ce7e9a8c66499571c2bc2a
Tobias Hieta <tobias.hieta@ubisoft.com>
2023-07-14 09:46:54 +0200

    [LLD][COFF] Add LLVM toolchain library paths by default.
    We want lld-link to automatically find compiler-rt's and
    libc++ when it's in the same directory as the rest of the
    toolchain. This is because on Windows linking isn't done
    via the clang driver - but instead invoked directly.
    
    This prepends: <llvm>/lib <llvm>/lib/clang/XX/lib and
    <llvm>/lib/clang/XX/lib/windows automatically to the library
    search paths.
    
    Related to #63827
    
    Differential Revision: https://reviews.llvm.org/D151188

--- clang/lib/Driver/Driver.cpp
+++ clang/lib/Driver/Driver.cpp
@@ -184,6 +184,8 @@ std::string Driver::GetResourcesPath(StringRef BinaryPath,
     // path of the embedding binary, which for LLVM binaries will be in bin/.
     // ../lib gets us to lib/ in both cases.
     P = llvm::sys::path::parent_path(Dir);
+    // This search path is also created in the COFF driver of lld, so any
+    // changes here also needs to happen in lld/COFF/Driver.cpp
     llvm::sys::path::append(P, CLANG_INSTALL_LIBDIR_BASENAME, "clang",
                             CLANG_VERSION_MAJOR_STRING);
   }
