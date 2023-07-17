845f98471c5a7e1b3bf901f5ddf4ef5f3d18a358
Farid Zakaria <fmzakari@google.com>
2023-07-17 16:57:21 +0000

    Introduce sqlite3 and SQELF.h/cpp files
    Introduce the bare minimum for the new BinaryFormat.
    I chose llvm/lib/BinaryFormat but I see ELF files for instance also
    in the MC directory.
    
    This is just the beginning!

---
+++ llvm/include/llvm/BinaryFormat/SQELF.h
@@ -0,0 +1,17 @@
+#ifndef LLVM_BINARYFORMAT_SQELF_H
+#define LLVM_BINARYFORMAT_SQELF_H
+
+#include <sqlite3.h>
+
+namespace llvm {
+    namespace BinaryFormat {
+        class SQELF {
+
+            SQELF();
+
+        };
+    }
+}
+
+
+#endif // LLVM_BINARYFORMAT_SQELF_H
---
+++ llvm/lib/BinaryFormat/SQELF.cpp
@@ -0,0 +1,9 @@
+#include "llvm/BinaryFormat/SQELF.h"
+
+using namespace llvm;
+using namespace BinaryFormat;
+
+
+SQELF::SQELF() {
+    
+}
