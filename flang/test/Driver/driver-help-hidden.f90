fc43c4f0181bfb7e7821e8b12fbd45e5178e884b
Kiran Chandramohan <kiran.chandramohan@arm.com>
2023-07-18 10:33:57 +0000

    [Flang] Include logical default with default-integer-8
    Other compilers include the logical default also with the
    default-integer-8 setting. This patch does the same for
    flang.
    
    Reviewed By: awarzynski, sscalpone
    
    Differential Revision: https://reviews.llvm.org/D155279

--- flang/test/Driver/driver-help-hidden.f90
+++ flang/test/Driver/driver-help-hidden.f90
@@ -27,7 +27,7 @@
 ! CHECK-NEXT: -fcolor-diagnostics    Enable colors in diagnostics
 ! CHECK-NEXT: -fconvert=<value>      Set endian conversion of data for unformatted files
 ! CHECK-NEXT: -fdefault-double-8     Set the default double precision kind to an 8 byte wide type
-! CHECK-NEXT: -fdefault-integer-8    Set the default integer kind to an 8 byte wide type
+! CHECK-NEXT: -fdefault-integer-8    Set the default integer and logical kind to an 8 byte wide type
 ! CHECK-NEXT: -fdefault-real-8       Set the default real kind to an 8 byte wide type
 ! CHECK-NEXT: -ffast-math            Allow aggressive, lossy floating-point optimizations
 ! CHECK-NEXT: -ffixed-form           Process source files in fixed form
