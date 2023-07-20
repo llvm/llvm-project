6fc6a904a85bfe53d87c4a69891eb959563e40e8
Simon Pilgrim <llvm-dev@redking.me.uk>
2023-07-20 12:11:48 +0100

    [DAG] ShrinkDemandedConstant - early-out for empty DemandedBits/Elts
    Leave this to constant folding in SimplifyDemandedBits
    
    Fixes #63975

--- llvm/lib/CodeGen/SelectionDAG/TargetLowering.cpp
+++ llvm/lib/CodeGen/SelectionDAG/TargetLowering.cpp
@@ -504,6 +504,11 @@ bool TargetLowering::ShrinkDemandedConstant(SDValue Op,
   SDLoc DL(Op);
   unsigned Opcode = Op.getOpcode();
 
+  // Early-out if we've ended up calling an undemanded node, leave this to
+  // constant folding.
+  if (DemandedBits.isZero() || DemandedElts.isZero())
+    return false;
+
   // Do target-specific constant optimization.
   if (targetShrinkDemandedConstant(Op, DemandedBits, DemandedElts, TLO))
     return TLO.New.getNode();
