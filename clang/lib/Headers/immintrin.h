fc3b7874b6c95f04a249e2c9da3c5221f50c85b2
Freddy Ye <freddy.ye@intel.com>
2023-07-20 09:44:21 +0800

    [X86] Add SHA512 instructions.
    For more details about this instruction, please refer to the latest ISE document: https://www.intel.com/content/www/us/en/develop/download/intel-architecture-instruction-set-extensions-programming-reference.html
    
    Reviewed By: RKSimon, skan
    
    Differential Revision: https://reviews.llvm.org/D155146

--- clang/lib/Headers/immintrin.h
+++ clang/lib/Headers/immintrin.h
@@ -270,6 +270,11 @@
 #endif
 
 #if !(defined(_MSC_VER) || defined(__SCE__)) || __has_feature(modules) ||      \
+    defined(__SHA512__)
+#include <sha512intrin.h>
+#endif
+
+#if !(defined(_MSC_VER) || defined(__SCE__)) || __has_feature(modules) ||      \
     defined(__RDPID__)
 /// Returns the value of the IA32_TSC_AUX MSR (0xc0000103).
 ///
