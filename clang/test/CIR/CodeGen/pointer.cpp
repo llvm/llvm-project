d23d88164c1155f0625d2126bf6fbf1a624c7bc6
Vinicius Couto Espindola <vini.couto.e@gmail.com>
2023-07-23 10:58:48 -0300

    [CIR][NFC] Separate CIR to LLVM IR translation tests
    The goal is to keep tests self-contained and focused. Mixing CIR to
    LLVM IR translation with codegen tests, or with CIR to LLVM Dialect
    lowering tests, makes it harder to understand what is being tested and
    requires the same file to go through multiple tools.
    
    This patch creates a separate `clang/tests/CIR/Translation` folder
    dedicated strictly to CIR's LLVM IR translation interface. While
    `tests/Lowering` should validate the `lowerDirectlyFromCIRToLLVMIR`
    pass, `tests/Translation` should validate `CIRDialectLLVMIRTranslationInterface`.

--- clang/test/CIR/CodeGen/pointer.cpp
+++ clang/test/CIR/CodeGen/pointer.cpp
@@ -1,10 +1,6 @@
 // RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fclangir -emit-cir %s -o %t.cir
 // RUN: FileCheck --input-file=%t.cir %s
-// FIXME(cir): Move the test below to lowering and us a separate tool to lower from CIR to LLVM IR.
-// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fclangir -emit-llvm %s -o %t.ll
-// RUN: FileCheck --input-file=%t.ll --check-prefix=LLVM %s
 
 // Global pointer should be zero initialized by default.
 int *ptr;
 // CHECK: cir.global external @ptr = #cir.null : !cir.ptr<!s32i>
-// LLVM: @ptr = global ptr null
