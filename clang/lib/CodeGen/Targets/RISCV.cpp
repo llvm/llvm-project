0fa004e0724b4398f999b8bffbc37f524e41f66e
Alex Bradbury <asb@igalia.com>
2023-07-24 16:58:21 +0100

    Revert "[clang][RISCV] Fix ABI handling of empty structs with hard FP calling conventions in C++"
    This reverts commit 17a58b3ca7ec18585e9ea8ed8b39d72fe36fb6cb and the
    minor documentation fix 569e99a471f618b7fdf045d5e96f21d3e3a7f898.
    
    An issue was reported in https://reviews.llvm.org/D142327#inline-1510301
    so reverting until it can be investigated and fixed.

--- clang/lib/CodeGen/Targets/RISCV.cpp
+++ clang/lib/CodeGen/Targets/RISCV.cpp
@@ -152,13 +152,6 @@ bool RISCVABIInfo::detectFPCCEligibleStructHelper(QualType Ty, CharUnits CurOff,
   if (const ConstantArrayType *ATy = getContext().getAsConstantArrayType(Ty)) {
     uint64_t ArraySize = ATy->getSize().getZExtValue();
     QualType EltTy = ATy->getElementType();
-    // Non-zero-length arrays of empty records make the struct ineligible for
-    // the FP calling convention in C++.
-    if (const auto *RTy = EltTy->getAs<RecordType>()) {
-      if (ArraySize != 0 && isa<CXXRecordDecl>(RTy->getDecl()) &&
-          isEmptyRecord(getContext(), EltTy, true, true))
-        return false;
-    }
     CharUnits EltSize = getContext().getTypeSizeInChars(EltTy);
     for (uint64_t i = 0; i < ArraySize; ++i) {
       bool Ret = detectFPCCEligibleStructHelper(EltTy, CurOff, Field1Ty,
@@ -175,7 +168,7 @@ bool RISCVABIInfo::detectFPCCEligibleStructHelper(QualType Ty, CharUnits CurOff,
     // copy constructor are not eligible for the FP calling convention.
     if (getRecordArgABI(Ty, CGT.getCXXABI()))
       return false;
-    if (isEmptyRecord(getContext(), Ty, true, true))
+    if (isEmptyRecord(getContext(), Ty, true))
       return true;
     const RecordDecl *RD = RTy->getDecl();
     // Unions aren't eligible unless they're empty (which is caught above).
