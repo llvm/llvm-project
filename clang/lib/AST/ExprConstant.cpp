4942092592d238a3720bd8435c91ac96a1909dbf
Nick Desaulniers <ndesaulniers@google.com>
2023-07-24 13:40:14 -0700

    [clang][ConstantEmitter] have tryEmitPrivate[ForVarInit] try ConstExprEmitter fast-path first
    As suggested by @efriedma in:
    https://reviews.llvm.org/D76096#4370369
    
    This should speed up evaluating whether an expression is constant or
    not, but due to the complexity of these two different implementations,
    we may start getting different answers for edge cases for which we do
    not yet have test cases in-tree (or perhaps even performance regressions
    for some cases). As such, contributors have carte blanche to revert if
    necessary.
    
    For additional historical context about ExprConstant vs CGExprConstant,
    here's snippets from a private conversation on discord:
    
      ndesaulniers:
      why do we have clang/lib/AST/ExprConstant.cpp and
      clang/lib/CodeGen/CGExprConstant.cpp? Does clang constant fold during
      ast walking/creation AND during LLVM codegen?
      efriedma:
      originally, clang needed to handle two things: integer constant
      expressions (the "5" in "int x[5];"), and constant global initializers
      (the "5" in "int x = 5;").  pre-C++11, the two could be handled mostly
      separately; so we had the code for integer constants in AST/, and the
      code for globals in CodeGen/.  C++11 constexpr sort of destroyed that
      separation, though. so now we do both kinds of constant evaluation on
      the AST, then CGExprConstant translates the result of that evaluation
      to LLVM IR.  but we kept around some bits of the old cgexprconstant to
      avoid performance/memory usage regressions on large arrays.
    
    Reviewed By: efriedma
    
    Differential Revision: https://reviews.llvm.org/D151587

--- clang/lib/AST/ExprConstant.cpp
+++ clang/lib/AST/ExprConstant.cpp
@@ -8391,8 +8391,8 @@ bool LValueExprEvaluator::VisitMaterializeTemporaryExpr(
       E->getSubExpr()->skipRValueSubobjectAdjustments(CommaLHSs, Adjustments);
 
   // If we passed any comma operators, evaluate their LHSs.
-  for (unsigned I = 0, N = CommaLHSs.size(); I != N; ++I)
-    if (!EvaluateIgnoredValue(Info, CommaLHSs[I]))
+  for (const Expr *E : CommaLHSs)
+    if (!EvaluateIgnoredValue(Info, E))
       return false;
 
   // A materialized temporary with static storage duration can appear within the
@@ -8400,6 +8400,8 @@ bool LValueExprEvaluator::VisitMaterializeTemporaryExpr(
   // value for use outside this evaluation.
   APValue *Value;
   if (E->getStorageDuration() == SD_Static) {
+    if (Info.EvalMode == EvalInfo::EM_ConstantFold)
+      return false;
     // FIXME: What about SD_Thread?
     Value = E->getOrCreateValue(true);
     *Value = APValue();
@@ -15475,7 +15477,7 @@ bool Expr::EvaluateAsInitializer(APValue &Value, const ASTContext &Ctx,
   EStatus.Diag = &Notes;
 
   EvalInfo Info(Ctx, EStatus,
-                (IsConstantInitialization && Ctx.getLangOpts().CPlusPlus11)
+                (IsConstantInitialization && Ctx.getLangOpts().CPlusPlus)
                     ? EvalInfo::EM_ConstantExpression
                     : EvalInfo::EM_ConstantFold);
   Info.setEvaluatingDecl(VD, Value);
