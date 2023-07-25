92c06114b2ea9900a3364fb395988dfb065758f7
Weining Lu <luweining@loongson.cn>
2023-07-25 20:26:46 +0800

    [LoongArch] Support -march=native and -mtune=
    As described in [1][2], `-mtune=` is used to select the type of target
    microarchitecture, defaults to the value of `-march`. The set of
    possible values should be a superset of `-march` values. Currently
    possible values of `-march=` and `-mtune=` are `native`, `loongarch64`
    and `la464`.
    
    D136146 has supported `-march={loongarch64,la464}` and this patch adds
    support for `-march=native` and `-mtune=`.
    
    A new ProcessorModel called `loongarch64` is defined in LoongArch.td
    to support `-mtune=loongarch64`.
    
    `llvm::sys::getHostCPUName()` returns `generic` on unknown or future
    LoongArch CPUs, e.g. the not yet added `la664`, leading to
    `llvm::LoongArch::isValidArchName()` failing to parse the arch name.
    In this case, use `loongarch64` as the default arch name for 64-bit
    CPUs.
    
    And these two preprocessor macros are defined:
    - __loongarch_arch
    - __loongarch_tune
    
    [1]: https://github.com/loongson/LoongArch-Documentation/blob/2023.04.20/docs/LoongArch-toolchain-conventions-EN.adoc
    [2]: https://github.com/loongson/la-softdev-convention/blob/v0.1/la-softdev-convention.adoc
    
    Differential Revision: https://reviews.llvm.org/D155824

--- clang/lib/Basic/Targets/LoongArch.cpp
+++ clang/lib/Basic/Targets/LoongArch.cpp
@@ -15,7 +15,7 @@
 #include "clang/Basic/MacroBuilder.h"
 #include "clang/Basic/TargetBuiltins.h"
 #include "llvm/Support/raw_ostream.h"
-#include "llvm/TargetParser/TargetParser.h"
+#include "llvm/TargetParser/LoongArchTargetParser.h"
 
 using namespace clang;
 using namespace clang::targets;
@@ -198,7 +198,19 @@ void LoongArchTargetInfo::getTargetDefines(const LangOptions &Opts,
   else
     Builder.defineMacro("__loongarch_frlen", "0");
 
-  // TODO: define __loongarch_arch and __loongarch_tune.
+  // Define __loongarch_arch.
+  StringRef Arch = llvm::LoongArch::getArch();
+  if (Arch.empty())
+    Arch = llvm::LoongArch::getDefaultArch(GRLen == 64);
+  if (!Arch.empty())
+    Builder.defineMacro("__loongarch_arch", Arch);
+
+  // Define __loongarch_tune.
+  StringRef TuneCPU = llvm::LoongArch::getTuneCPU();
+  if (TuneCPU.empty())
+    TuneCPU = Arch;
+  if (!TuneCPU.empty())
+    Builder.defineMacro("__loongarch_tune", TuneCPU);
 
   StringRef ABI = getABI();
   if (ABI == "lp64d" || ABI == "lp64f" || ABI == "lp64s")
@@ -269,4 +281,13 @@ bool LoongArchTargetInfo::handleTargetFeatures(
     }
   }
   return true;
+}
+
+bool LoongArchTargetInfo::isValidTuneCPUName(StringRef Name) const {
+  return llvm::LoongArch::isValidTuneCPUName(Name);
+}
+
+void LoongArchTargetInfo::fillValidTuneCPUList(
+    SmallVectorImpl<StringRef> &Values) const {
+  llvm::LoongArch::fillValidTuneCPUList(Values);
 }
