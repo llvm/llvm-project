9222ac74b0174860769ac682a9a90f7861058c08
Matt Arsenault <Matthew.Arsenault@amd.com>
2023-07-26 17:40:51 -0400

    Use generic f64 sqrt intrinsic
    This is now correctly lowered. Conformance test saves 4 bytes and
    somehow also is -3 registers.
    
    Change-Id: Ie94c6b351366b3b0b96ef89758f24869fd9dc50f

--- amd/device-libs/ocml/src/privD.h
+++ amd/device-libs/ocml/src/privD.h
@@ -44,22 +44,4 @@
     _fsqrt_ret; \
 })
 
-#define MATH_SQRT(X) ({ \
-    double _sqrt_x = X; \
-    bool _sqrt_b = _sqrt_x < 0x1.0p-767; \
-    _sqrt_x *= _sqrt_b ? 0x1.0p+256 : 1.0; \
-    double _sqrt_y = BUILTIN_AMDGPU_RSQRT_F64(_sqrt_x); \
-    double _sqrt_s0 = _sqrt_x * _sqrt_y; \
-    double _sqrt_h0 = 0.5 * _sqrt_y; \
-    double _sqrt_r0 = BUILTIN_FMA_F64(-_sqrt_h0, _sqrt_s0, 0.5); \
-    double _sqrt_h1 = BUILTIN_FMA_F64(_sqrt_h0, _sqrt_r0, _sqrt_h0); \
-    double _sqrt_s1 = BUILTIN_FMA_F64(_sqrt_s0, _sqrt_r0, _sqrt_s0); \
-    double _sqrt_d0 = BUILTIN_FMA_F64(-_sqrt_s1, _sqrt_s1, _sqrt_x); \
-    double _sqrt_s2 = BUILTIN_FMA_F64(_sqrt_d0, _sqrt_h1, _sqrt_s1); \
-    double _sqrt_d1 = BUILTIN_FMA_F64(-_sqrt_s2, _sqrt_s2, _sqrt_x); \
-    double _sqrt_ret = BUILTIN_FMA_F64(_sqrt_d1, _sqrt_h1, _sqrt_s2); \
-    _sqrt_ret *= _sqrt_b ? 0x1.0p-128 : 1.0; \
-    _sqrt_ret = ((_sqrt_x == 0.0) | (_sqrt_x == (double)INFINITY)) ? _sqrt_x : _sqrt_ret; \
-    _sqrt_ret; \
-})
-
+#define MATH_SQRT(X) BUILTIN_SQRT_F64(X)
