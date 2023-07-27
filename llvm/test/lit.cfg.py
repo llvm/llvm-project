ef8aac00d6222f55efc7695fdec716e74df2dcd7
Zain Jaffal <z_jaffal@apple.com>
2023-07-27 13:13:55 +0100

    [RemarkUtil] Refactor llvm-remarkutil to include size-diff
    This change follows from https://reviews.llvm.org/D156416 We include
    `llvm-remark-size-diff` as a part of `llvm-remarkutil` under a
    subcommand `size-diff`.
    
    Differential Revision: https://reviews.llvm.org/D156515

--- llvm/test/lit.cfg.py
+++ llvm/test/lit.cfg.py
@@ -216,7 +216,6 @@ tools.extend(
         "llvm-rc",
         "llvm-readelf",
         "llvm-readobj",
-        "llvm-remark-size-diff",
         "llvm-rtdyld",
         "llvm-sim",
         "llvm-size",
