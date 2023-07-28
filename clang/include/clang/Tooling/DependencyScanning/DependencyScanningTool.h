a5e6201aeab19694f5f7854b79cdb97828976f3c
Steven Wu <stevenwu@apple.com>
2023-07-28 12:50:49 -0700

    Merge commit 'c75b331fc231' from llvm.org/main into next

--- clang/include/clang/Tooling/DependencyScanning/DependencyScanningTool.h
+++ clang/include/clang/Tooling/DependencyScanning/DependencyScanningTool.h
@@ -234,6 +234,10 @@ public:
     ClangModuleDeps[MD.ID] = std::move(MD);
   }
 
+  void handleDirectModuleDependency(ModuleID ID) override {
+    DirectModuleDeps.push_back(ID);
+  }
+
   void handleContextHash(std::string Hash) override {
     ContextHash = std::move(Hash);
   }
@@ -253,6 +257,7 @@ private:
   std::vector<std::string> Dependencies;
   std::vector<PrebuiltModuleDep> PrebuiltModuleDeps;
   llvm::MapVector<ModuleID, ModuleDeps> ClangModuleDeps;
+  std::vector<ModuleID> DirectModuleDeps;
   std::vector<Command> Commands;
   std::string ContextHash;
   std::optional<std::string> CASFileSystemRootID;
