5d19c34dbc0c532ef1dacf63f599a3a2150282d5
Timm Bäder <tbaeder@redhat.com>
2023-07-28 08:37:41 +0200

    [clang][Interp] Diagnose unknown parameter values
    Differential Revision: https://reviews.llvm.org/D156509

--- clang/test/AST/Interp/c.c
+++ clang/test/AST/Interp/c.c
@@ -3,8 +3,6 @@
 // RUN: %clang_cc1 -verify=ref -std=c11 %s
 // RUN: %clang_cc1 -pedantic -verify=pedantic-ref -std=c11 %s
 
-/// expected-no-diagnostics
-
 _Static_assert(1, "");
 _Static_assert(0 != 1, "");
 _Static_assert(1.0 == 1.0, ""); // pedantic-ref-warning {{not an integer constant expression}} \
@@ -26,3 +24,15 @@ const int b = 3;
 _Static_assert(b == 3, ""); // pedantic-ref-warning {{not an integer constant expression}} \
                             // pedantic-expected-warning {{not an integer constant expression}}
 
+const int c; // ref-note {{declared here}} \
+             // pedantic-ref-note {{declared here}} \
+             // expected-note {{declared here}} \
+             // pedantic-expected-note {{declared here}}
+_Static_assert(c == 0, ""); // ref-error {{not an integral constant expression}} \
+                            // ref-note {{initializer of 'c' is unknown}} \
+                            // pedantic-ref-error {{not an integral constant expression}} \
+                            // pedantic-ref-note {{initializer of 'c' is unknown}} \
+                            // expected-error {{not an integral constant expression}} \
+                            // expected-note {{initializer of 'c' is unknown}} \
+                            // pedantic-expected-error {{not an integral constant expression}} \
+                            // pedantic-expected-note {{initializer of 'c' is unknown}}
