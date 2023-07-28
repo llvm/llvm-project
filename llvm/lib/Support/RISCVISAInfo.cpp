e4777dc4b9cb371971523cc603e1b8a5c7255e7e
melonedo <funanzeng@gmail.com>
2023-07-28 19:28:20 +0800

    Revert "[RISCV] Add support for XCVbi extension in CV32E40P"
    This reverts commit bf2ad26b4ff856aab9a62ad168e6bdefeedc374f as it
    checked in merge conflict markers.

--- llvm/lib/Support/RISCVISAInfo.cpp
+++ llvm/lib/Support/RISCVISAInfo.cpp
@@ -67,7 +67,6 @@ static const RISCVSupportedExtension SupportedExtensions[] = {
 
     // vendor-defined ('X') extensions
     {"xcvalu", RISCVExtensionVersion{1, 0}},
-    {"xcvbi", RISCVExtensionVersion{1, 0}},
     {"xcvbitmanip", RISCVExtensionVersion{1, 0}},
     {"xcvmac", RISCVExtensionVersion{1, 0}},
     {"xcvsimd", RISCVExtensionVersion{1, 0}},
