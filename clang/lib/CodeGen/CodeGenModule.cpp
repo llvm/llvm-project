1f669d35fc1f93f48fda419801034248c6412205
Jonas Hahnfeld <jonas.hahnfeld@cern.ch>
2023-07-28 15:43:11 +0200

    [CodeGen] Keep track of eagerly emitted globals
    An inline virtual function must be emitted, but we need to remember
    it and emit the same definition again in the future in case later
    LLVM optimizations stripped it from the Module. The added test case
    shows the problem; before this patch, it would fail with:
        Symbols not found: [ _ZN1AD0Ev, _ZN1AD1Ev ]
    
    This reapplies commit f8dadefd4a, reverted in commit 0e17372b38, but
    disables RTTI in the test to avoid problems on Windows.
    
    Differential Revision: https://reviews.llvm.org/D156537
    
    (cherry picked from commit c861d32d7c2791bdc058d9d9fbaecc1c2f07b8c7)

--- clang/lib/CodeGen/CodeGenModule.cpp
+++ clang/lib/CodeGen/CodeGenModule.cpp
@@ -3645,6 +3645,7 @@ void CodeGenModule::EmitGlobal(GlobalDecl GD) {
   if (MustBeEmitted(Global) && MayBeEmittedEagerly(Global)) {
     // Emit the definition if it can't be deferred.
     EmitGlobalDefinition(GD);
+    addEmittedDeferredDecl(GD);
     return;
   }
 
