955fc63ab9c585cf2e7afda724ad90d1368e5173
YAMAMOTO Takashi <yamamoto@midokura.com>
2023-07-31 08:11:49 -0700

    [llvm-c] Add LLVMSetTailCallKind and LLVMGetTailCallKind
    Reviewed By: aeubanks
    
    Differential Revision: https://reviews.llvm.org/D153107

--- llvm/lib/IR/Core.cpp
+++ llvm/lib/IR/Core.cpp
@@ -2896,6 +2896,14 @@ void LLVMSetTailCall(LLVMValueRef Call, LLVMBool isTailCall) {
   unwrap<CallInst>(Call)->setTailCall(isTailCall);
 }
 
+LLVMTailCallKind LLVMGetTailCallKind(LLVMValueRef Call) {
+  return (LLVMTailCallKind)unwrap<CallInst>(Call)->getTailCallKind();
+}
+
+void LLVMSetTailCallKind(LLVMValueRef Call, LLVMTailCallKind kind) {
+  unwrap<CallInst>(Call)->setTailCallKind((CallInst::TailCallKind)kind);
+}
+
 /*--.. Operations on invoke instructions (only) ............................--*/
 
 LLVMBasicBlockRef LLVMGetNormalDest(LLVMValueRef Invoke) {
