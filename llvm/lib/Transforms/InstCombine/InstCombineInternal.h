c5592f7acdf2e505b0431f44775b4339444a4711
Nikita Popov <npopov@redhat.com>
2023-08-01 10:04:20 +0200

    [InstCombine] Fix use after free when removing unreachable code (PR64235)
    In degenerate cases, it is possible for unreachable code removal
    to remove the current instruction. However, we still return the
    instruction to report a change, resulting in a use after free.
    
    Instead, perform the change reporting in the same way as
    eraseInstFromFunction() does, by directly setting MadeIRChange
    and returning nullptr.
    
    Fixes https://github.com/llvm/llvm-project/issues/64235.

--- llvm/lib/Transforms/InstCombine/InstCombineInternal.h
+++ llvm/lib/Transforms/InstCombine/InstCombineInternal.h
@@ -672,10 +672,10 @@ public:
   bool tryToSinkInstruction(Instruction *I, BasicBlock *DestBlock);
 
   bool removeInstructionsBeforeUnreachable(Instruction &I);
-  bool handleUnreachableFrom(Instruction *I,
+  void handleUnreachableFrom(Instruction *I,
                              SmallVectorImpl<BasicBlock *> &Worklist);
-  bool handlePotentiallyDeadBlocks(SmallVectorImpl<BasicBlock *> &Worklist);
-  bool handlePotentiallyDeadSuccessors(BasicBlock *BB, BasicBlock *LiveSucc);
+  void handlePotentiallyDeadBlocks(SmallVectorImpl<BasicBlock *> &Worklist);
+  void handlePotentiallyDeadSuccessors(BasicBlock *BB, BasicBlock *LiveSucc);
   void freelyInvertAllUsersOf(Value *V, Value *IgnoredUser = nullptr);
 };
 
