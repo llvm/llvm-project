0131126340ae031dfed523dd16b984894bd4ff4e
Adrian Prantl <aprantl@apple.com>
2023-08-02 14:57:53 -0700

    Improve TestSwiftMutablePersistentVar (NFC)
    (cherry picked from commit 1f4f6bc841d7155adf3b049f35a8ad4d2d647e05)

--- lldb/test/API/lang/swift/expression/mutable_persistent_var/TestSwiftMutablePersistentVar.py
+++ lldb/test/API/lang/swift/expression/mutable_persistent_var/TestSwiftMutablePersistentVar.py
@@ -4,7 +4,8 @@ from lldbsuite.test.lldbtest import *
 import lldbsuite.test.lldbutil as lldbutil
 
 
-class TestCase(TestBase):
+class TestSwiftMutablePersistentVar(TestBase):
+    NO_DEBUG_INFO_TESTCASE = True
     @swiftTest
     def test(self):
         """Test that persistent variables are mutable."""
@@ -12,3 +13,5 @@ class TestCase(TestBase):
         lldbutil.run_to_name_breakpoint(self, "main")
         self.expect("expr var $count = 30")
         self.expect("expr $count = 41")
+        self.expect("expr $count += 1")
+        self.expect("expr $count", substrs=["42"])
