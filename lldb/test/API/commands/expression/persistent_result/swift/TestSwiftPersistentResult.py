9aafaa419a0170ea9a5966e9ec39eb907ec3bd47
Alex Langford <alangford@apple.com>
2023-08-02 15:22:36 -0700

    [lldb] Annotate swift tests with @swiftTest
    (cherry picked from commit 229e8b4a6e326b4c9ea61295fdf017bf8a08d707)

--- lldb/test/API/commands/expression/persistent_result/swift/TestSwiftPersistentResult.py
+++ lldb/test/API/commands/expression/persistent_result/swift/TestSwiftPersistentResult.py
@@ -9,29 +9,44 @@ from lldbsuite.test import lldbutil
 
 
 class TestCase(TestBase):
-    def setUp(self):
-        TestBase.setUp(self)
-        self.build()
-        lldbutil.run_to_source_breakpoint(self, "break here", lldb.SBFileSpec("main.swift"))
-
+    @swiftTest
     def test_enable_persistent_result(self):
         """Test explicitly enabling result variables persistence."""
+        self.build()
+        lldbutil.run_to_source_breakpoint(
+            self, "break here", lldb.SBFileSpec("main.swift")
+        )
         self.expect("expression --persistent-result on -- i", startstr="(Int) $R0 = 30")
         # Verify the lifetime of $R0 extends beyond the `expression` it was created in.
         self.expect("expression $R0", startstr="(Int) $R1 = 30")
 
+    @swiftTest
     def test_disable_persistent_result(self):
         """Test explicitly disabling persistent result variables."""
+        self.build()
+        lldbutil.run_to_source_breakpoint(
+            self, "break here", lldb.SBFileSpec("main.swift")
+        )
         self.expect("expression --persistent-result off -- i", startstr="(Int) 30")
         # Verify a persistent result was not silently created.
         self.expect("expression $R0", error=True)
 
+    @swiftTest
     def test_expression_persists_result(self):
         """Test `expression`'s default behavior is to persist a result variable."""
+        self.build()
+        lldbutil.run_to_source_breakpoint(
+            self, "break here", lldb.SBFileSpec("main.swift")
+        )
         self.expect("expression i", startstr="(Int) $R0 = 30")
         self.expect("expression $R0", startstr="(Int) $R1 = 30")
 
+    @swiftTest
     def test_p_does_not_persist_results(self):
         """Test `p` does not persist a result variable."""
+        self.build()
+        lldbutil.run_to_source_breakpoint(
+            self, "break here", lldb.SBFileSpec("main.swift")
+        )
         self.expect("p i", startstr="(Int) 30")
         self.expect("p $R0", error=True)
