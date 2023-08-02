241cceb9af844ef7d7a87124407a04b0a64991fe
J. Ryan Stinnett <jryans@gmail.com>
2023-08-02 18:53:21 +0100

    [Clang][Tooling] Accept preprocessed input files
    This restores the tooling library's ability to accept invocations that take a
    preprocessed file as the primary input.
    
    Regressed by https://reviews.llvm.org/D105695
    Fixes https://github.com/llvm/llvm-project/issues/63941
    
    Differential Revision: https://reviews.llvm.org/D157011

--- clang/lib/Tooling/Tooling.cpp
+++ clang/lib/Tooling/Tooling.cpp
@@ -147,6 +147,13 @@ getCC1Arguments(DiagnosticsEngine *Diagnostics,
     if (IsCC1Command(Job) && llvm::all_of(Job.getInputInfos(), IsSrcFile))
       CC1Jobs.push_back(&Job);
 
+  // If there are no jobs for source files, try checking again for a single job
+  // with any file type. This accepts a preprocessed file as input.
+  if (CC1Jobs.empty())
+    for (const driver::Command &Job : Jobs)
+      if (IsCC1Command(Job))
+        CC1Jobs.push_back(&Job);
+
   if (CC1Jobs.empty() ||
       (CC1Jobs.size() > 1 && !ignoreExtraCC1Commands(Compilation))) {
     SmallString<256> error_msg;
