13be923dfb738bc41f6a3b03b030f05903fcf46a
Rainer Orth <ro@gcc.gnu.org>
2023-08-03 20:01:13 +0200

    [Driver] Allow for sparcv8plus subdir with Solaris/SPARC GCC
    Since GCC 11, the bundled Solaris/SPARC GCC uses the `sparcv8plus`
    subdirectory for 32-bit objects, just like upstream GCC.  Before that, it
    used `32` instead from a local patch.
    
    Since `clang` doesn't know about that `sparcv8plus` subdirectory, it
    wouldn't properly use GCC 11+ installations.
    
    The new `solaris-sparc-gcc-search.test` testcase wasn't run initially (like
    the existing `crash-report-null.test`) because the `.test` suffix wasn't
    handled.
    
    Tested on `sparcv9-sun-solaris2.11`, `amd64-pc-solaris2.11`, and
    `x86_64-pc-linux-gnu`.
    
    Differential Revision: https://reviews.llvm.org/D157013
    
    (cherry picked from commit 43dfe0f08ecaf50f986512d0548bd3ac84d1813b)

--- clang/lib/Driver/ToolChains/Gnu.cpp
+++ clang/lib/Driver/ToolChains/Gnu.cpp
@@ -1874,6 +1874,12 @@ static bool findBiarchMultilibs(const Driver &D,
                         .flag("-m64", /*Disallow=*/true)
                         .flag("-mx32")
                         .makeMultilib();
+  Multilib Alt32sparc = MultilibBuilder()
+                            .gccSuffix("/sparcv8plus")
+                            .includeSuffix("/sparcv8plus")
+                            .flag("-m32")
+                            .flag("-m64", /*Disallow=*/true)
+                            .makeMultilib();
 
   // GCC toolchain for IAMCU doesn't have crtbegin.o, so look for libgcc.a.
   FilterNonExistent NonExistent(
@@ -1885,10 +1891,14 @@ static bool findBiarchMultilibs(const Driver &D,
   const bool IsX32 = TargetTriple.isX32();
   if (TargetTriple.isArch32Bit() && !NonExistent(Alt32))
     Want = WANT64;
+  if (TargetTriple.isArch32Bit() && !NonExistent(Alt32sparc))
+    Want = WANT64;
   else if (TargetTriple.isArch64Bit() && IsX32 && !NonExistent(Altx32))
     Want = WANT64;
   else if (TargetTriple.isArch64Bit() && !IsX32 && !NonExistent(Alt64))
     Want = WANT32;
+  else if (TargetTriple.isArch64Bit() && !NonExistent(Alt32sparc))
+    Want = WANT64;
   else {
     if (TargetTriple.isArch32Bit())
       Want = NeedsBiarchSuffix ? WANT64 : WANT32;
@@ -1919,6 +1929,7 @@ static bool findBiarchMultilibs(const Driver &D,
   Result.Multilibs.push_back(Alt64);
   Result.Multilibs.push_back(Alt32);
   Result.Multilibs.push_back(Altx32);
+  Result.Multilibs.push_back(Alt32sparc);
 
   Result.Multilibs.FilterOut(NonExistent);
 
@@ -1932,7 +1943,8 @@ static bool findBiarchMultilibs(const Driver &D,
 
   if (Result.SelectedMultilibs.back() == Alt64 ||
       Result.SelectedMultilibs.back() == Alt32 ||
-      Result.SelectedMultilibs.back() == Altx32)
+      Result.SelectedMultilibs.back() == Altx32 ||
+      Result.SelectedMultilibs.back() == Alt32sparc)
     Result.BiarchSibling = Default;
 
   return true;
