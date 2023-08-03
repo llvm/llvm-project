46642cc83dc575962e1a6eb557714319c65ca5b8
Joseph Huber <jhuber6@vols.utk.edu>
2023-08-03 08:48:26 -0500

    [Libomptarget] Remove debug RAII from libomptarget
    This feature was supposed to allow you to trace execution inside of
    Libomptarget. However, this never really worked properly. The printing
    was always reoganized, only worked for single  threads, and pretty much
    only told you a handful of things about a runtime library that's an
    implementation detail to all users. Despite this, it contributed about
    40% of the total filesize of the deviceRTL. This patch simply removes
    this functionalit which I think was past due.
    
    Reviewed By: tianshilei1992
    
    Differential Revision: https://reviews.llvm.org/D157001

--- openmp/libomptarget/DeviceRTL/src/Debug.cpp
+++ openmp/libomptarget/DeviceRTL/src/Debug.cpp
@@ -37,32 +37,4 @@ void __assert_fail(const char *expr, const char *msg, const char *file,
 }
 }
 
-DebugEntryRAII::DebugEntryRAII(const char *File, const unsigned Line,
-                               const char *Function) {
-  if (config::isDebugMode(config::DebugKind::FunctionTracing) &&
-      mapping::getThreadIdInBlock() == 0 &&
-      mapping::getBlockIdInKernel() == 0) {
-
-    uint16_t &Level =
-        state::getKernelEnvironment().DynamicEnv->DebugIndentionLevel;
-
-    for (int I = 0; I < Level; ++I)
-      PRINTF("%s", "  ");
-
-    PRINTF("%s:%u: Thread %u Entering %s\n", File, Line,
-           mapping::getThreadIdInBlock(), Function);
-    Level++;
-  }
-}
-
-DebugEntryRAII::~DebugEntryRAII() {
-  if (config::isDebugMode(config::DebugKind::FunctionTracing) &&
-      mapping::getThreadIdInBlock() == 0 &&
-      mapping::getBlockIdInKernel() == 0) {
-    uint16_t &Level =
-        state::getKernelEnvironment().DynamicEnv->DebugIndentionLevel;
-    Level--;
-  }
-}
-
 #pragma omp end declare target
