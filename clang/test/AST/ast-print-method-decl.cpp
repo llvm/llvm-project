f7cc89c1956556845fd09ad4ea4216dc9549b9d5
Timo Stripf <timo.stripf@emmtrix.com>
2023-08-07 22:33:45 +0000

    [clang][DeclPrinter] Fix AST print to suppress output of implicit (non-written) constructor initializers
    DeclPrinter::PrintConstructorInitializers did output non-written constructor initiaizers. In particular, implicit constructor initializers of base classes were output.
    
    Reviewed By: aaron.ballman
    
    Differential Revision: https://reviews.llvm.org/D156523

--- clang/test/AST/ast-print-method-decl.cpp
+++ clang/test/AST/ast-print-method-decl.cpp
@@ -100,3 +100,26 @@ struct DefMethodsWithoutBody {
 
   // CHECK-NEXT: };
 };
+
+
+// ---- Check that implict (non-written) constructor initializers are not output
+
+struct ImplicitCtorInit1 {
+  int a;
+};
+
+// CHECK: struct ImplicitCtorInit2 : ImplicitCtorInit1 {
+struct ImplicitCtorInit2 : ImplicitCtorInit1 {
+
+  // CHECK-NEXT: ImplicitCtorInit2(int *) {
+  ImplicitCtorInit2(int *) {
+  // CHECK-NEXT: }
+  }
+
+  // CHECK-NEXT: ImplicitCtorInit2(int **) : ImplicitCtorInit1() {
+  ImplicitCtorInit2(int **) : ImplicitCtorInit1() {
+  // CHECK-NEXT: }
+  }
+
+  // CHECK-NEXT: };
+};
