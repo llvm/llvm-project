f6267d3b98c11b50bf24946f21c6646d2d3f6fa8
Nick Desaulniers <ndesaulniers@google.com>
2023-08-07 14:51:44 -0700

    [clang][CGExprConstant] handle implicit widening/narrowing Int-to-Int casts
    Consider the following statements:
        long x = 1;
        short y = 1;
    
    With the following AST:
        |-VarDecl 0x55d289973730 <x.c:1:1, col:10> col:6 x 'long' cinit
        | `-ImplicitCastExpr 0x55d289973800 <col:10> 'long' <IntegralCast>
        |   `-IntegerLiteral 0x55d2899737e0 <col:10> 'int' 1
        `-VarDecl 0x55d289973830 <line:2:1, col:11> col:7 y 'short' cinit
          `-ImplicitCastExpr 0x55d2899738b8 <col:11> 'short' <IntegralCast>
            `-IntegerLiteral 0x55d289973898 <col:11> 'int' 1
    
    Sign or Zero extend or truncate based on the source signedness and
    destination width.
    
    Reviewed By: efriedma
    
    Differential Revision: https://reviews.llvm.org/D156466

--- clang/test/CodeGen/global-init.c
+++ clang/test/CodeGen/global-init.c
@@ -49,6 +49,11 @@ struct K {
 // CHECK: @yuv_types ={{.*}} global [4 x [6 x i8]] {{\[}}[6 x i8] c"4:0:0\00", [6 x i8] c"4:2:0\00", [6 x i8] c"4:2:2\00", [6 x i8] c"4:4:4\00"]
 char yuv_types[4][6]= {"4:0:0","4:2:0","4:2:2","4:4:4"};
 
+unsigned long long x = -1000;
+// CHECK: @x ={{.*}} global i64 -1000
+unsigned long long uint_max = 4294967295u;
+// CHECK: @uint_max ={{.*}} global i64 4294967295
+
 
 // NOTE: tentative definitions are processed at the end of the translation unit.
 
