e40577f46e03b779ba92ce16e2b6785b31111f63
Will Hawkins <hawkinsw@obs.cr>
2023-08-08 01:04:17 -0400

    WIP: [libc++][ranges] Implement `ranges::stride_view`.
    Differential Revision: https://reviews.llvm.org/D156924
    
    Signed-off-by: Will Hawkins <hawkinsw@obs.cr>

---
+++ libcxx/test/std/ranges/range.adaptors/range.stride.view/begin.pass.cpp
@@ -0,0 +1,22 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++03, c++11, c++14, c++17, c++20
+
+// ranges
+
+// std::views::stride_view
+
+constexpr bool test() { return true; }
+
+int main(int, char**) {
+  test();
+  static_assert(test());
+
+  return 0;
+}
