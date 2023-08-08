a403d75be7add73f3e34032d73c81b8e1dcba3b9
Kazu Hirata <kazu@google.com>
2023-08-08 08:37:35 -0700

    [LegacyPM] Remove LowerGuardIntrinsicLegacyPass
    Differential Revision: https://reviews.llvm.org/D157073

--- llvm/lib/Transforms/Scalar/LowerGuardIntrinsic.cpp
+++ llvm/lib/Transforms/Scalar/LowerGuardIntrinsic.cpp
@@ -27,18 +27,6 @@
 
 using namespace llvm;
 
-namespace {
-struct LowerGuardIntrinsicLegacyPass : public FunctionPass {
-  static char ID;
-  LowerGuardIntrinsicLegacyPass() : FunctionPass(ID) {
-    initializeLowerGuardIntrinsicLegacyPassPass(
-        *PassRegistry::getPassRegistry());
-  }
-
-  bool runOnFunction(Function &F) override;
-};
-}
-
 static bool lowerGuardIntrinsic(Function &F) {
   // Check if we can cheaply rule out the possibility of not having any work to
   // do.
@@ -69,19 +57,6 @@ static bool lowerGuardIntrinsic(Function &F) {
   }
 
   return true;
-}
-
-bool LowerGuardIntrinsicLegacyPass::runOnFunction(Function &F) {
-  return lowerGuardIntrinsic(F);
-}
-
-char LowerGuardIntrinsicLegacyPass::ID = 0;
-INITIALIZE_PASS(LowerGuardIntrinsicLegacyPass, "lower-guard-intrinsic",
-                "Lower the guard intrinsic to normal control flow", false,
-                false)
-
-Pass *llvm::createLowerGuardIntrinsicPass() {
-  return new LowerGuardIntrinsicLegacyPass();
 }
 
 PreservedAnalyses LowerGuardIntrinsicPass::run(Function &F,
