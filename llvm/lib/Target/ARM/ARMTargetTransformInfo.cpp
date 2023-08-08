9a207578ac4f9f4fedbee6dc47bc6e2226104fc1
Alexey Bataev <a.bataev@outlook.com>
2023-08-08 09:57:50 -0700

    [TTI]Add InsertSubvector pattern in improveShuffleKindFromMask().
    It improves shuffle instructions estimation and improves vectorization
    outcome.
    
    Differential Revision: https://reviews.llvm.org/D157425

--- llvm/lib/Target/ARM/ARMTargetTransformInfo.cpp
+++ llvm/lib/Target/ARM/ARMTargetTransformInfo.cpp
@@ -1213,7 +1213,7 @@ InstructionCost ARMTTIImpl::getShuffleCost(TTI::ShuffleKind Kind,
                                            TTI::TargetCostKind CostKind,
                                            int Index, VectorType *SubTp,
                                            ArrayRef<const Value *> Args) {
-  Kind = improveShuffleKindFromMask(Kind, Mask);
+  Kind = improveShuffleKindFromMask(Kind, Mask, Tp, Index, SubTp);
   if (ST->hasNEON()) {
     if (Kind == TTI::SK_Broadcast) {
       static const CostTblEntry NEONDupTbl[] = {
