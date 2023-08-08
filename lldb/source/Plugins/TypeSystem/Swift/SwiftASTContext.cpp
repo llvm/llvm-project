a158861fa56e267dce61f800f48975c9710e1d81
Alex Langford <alangford@apple.com>
2023-08-08 12:52:37 -0700

    [lldb] Remove mis-merged code
    These were duplicated code blocks that were likely mismerged.

--- lldb/source/Plugins/TypeSystem/Swift/SwiftASTContext.cpp
+++ lldb/source/Plugins/TypeSystem/Swift/SwiftASTContext.cpp
@@ -4163,7 +4163,6 @@ static SwiftASTContext::TypeOrDecl DeclToTypeOrDecl(swift::ASTContext *ast,
     case swift::DeclKind::Module:
     case swift::DeclKind::Missing:
     case swift::DeclKind::MissingMember:
-    case swift::DeclKind::MacroExpansion:
       break;
 
     case swift::DeclKind::InfixOperator:
@@ -8084,9 +8083,6 @@ static void DescribeFileUnit(Stream &s, swift::FileUnit *file_unit) {
         break;
       case swift::SourceFileKind::Interface:
         s.PutCString("Interface");
-        break;
-      case swift::SourceFileKind::MacroExpansion:
-        s.PutCString("MacroExpansion");
         break;
       }
     }
