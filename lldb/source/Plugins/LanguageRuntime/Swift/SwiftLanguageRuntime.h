c71d8ebbd773231e16a656d616e74bfd6dadbbcb
Alex Langford <alangford@apple.com>
2023-08-08 13:26:22 -0700

    [lldb] Add GetCxxBridgedSyntheticChildProvider to SwiftLanguageRuntime
    It looks like 2952ca389f92c6fa5b759454942b9637ff3303f9 may not have been
    cherry-picked correctly. This patch gets us building again.
    
    (cherry picked from commit 7df6518aca5e8fbfc0a5ed2def78f9784c3750c5)

--- lldb/source/Plugins/LanguageRuntime/Swift/SwiftLanguageRuntime.h
+++ lldb/source/Plugins/LanguageRuntime/Swift/SwiftLanguageRuntime.h
@@ -460,6 +460,10 @@ public:
   lldb::SyntheticChildrenSP
   GetBridgedSyntheticChildProvider(ValueObject &valobj);
 
+  /// Get the synthetic child provider that displays Swift in C++ frames.
+  lldb::SyntheticChildrenSP
+  GetCxxBridgedSyntheticChildProvider(lldb::ValueObjectSP valobj);
+
   /// Expression Callbacks.
   /// \{
   void WillStartExecutingUserExpression(bool);
