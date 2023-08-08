97afd2629161f8eaa7ddc2b47a753467ba7c36bf
Farid Zakaria <fmzakari@google.com>
2023-08-08 03:07:37 +0000

    Initial support for MCInst
    Separate out a MCInst into various parts to get ready to put
    into the sqlite database.

--- llvm/lib/MC/MCSQELFStreamer.cpp
+++ llvm/lib/MC/MCSQELFStreamer.cpp
@@ -17,6 +17,8 @@
 #include "llvm/MC/MCAssembler.h"
 #include "llvm/MC/MCCodeEmitter.h"
 #include "llvm/MC/MCContext.h"
+#include "llvm/MC/MCInstPrinter.h"
+#include "llvm/MC/MCInstrInfo.h"
 #include "llvm/MC/MCObjectWriter.h"
 #include "llvm/MC/MCSymbol.h"
 #include "llvm/MC/TargetRegistry.h"
@@ -46,8 +48,40 @@ void MCSQELFStreamer::emitZerofill(MCSection *Section, MCSymbol *Symbol,
                                    SMLoc Loc) {}
 
 void MCSQELFStreamer::emitInstToData(const MCInst &Inst,
-                                     const MCSubtargetInfo &) {
-  LLVM_DEBUG(dbgs() << "I am here!\n");
+                                     const MCSubtargetInfo &STI) {
+  MCAssembler &Assembler = getAssembler();
+  SmallVector<MCFixup, 4> Fixups;
+
+  // Fetch the raw representation of the code
+  SmallString<256> Code;
+  Assembler.getEmitter().encodeInstruction(Inst, Code, Fixups, STI);
+
+  const llvm::MCContext &Ctx = getContext();
+  const llvm::MCAsmInfo *MAI = Ctx.getAsmInfo();
+  const llvm::MCRegisterInfo *MRI = Ctx.getRegisterInfo();
+  const llvm::Triple &TheTriple = Ctx.getTargetTriple();
+
+  std::string Error;
+  const Target *Target = TargetRegistry::lookupTarget(TheTriple.str(), Error);
+  unsigned AsmVariant = 0; // typically 0 for the default assembly variant
+
+  std::unique_ptr<const MCInstrInfo> MII(Target->createMCInstrInfo());
+  std::unique_ptr<llvm::MCInstPrinter> Printer(
+      Target->createMCInstPrinter(TheTriple, AsmVariant, *MAI, *MII, *MRI));
+
+  const char *Mnemonic = Printer->getMnemonic(&Inst).first;
+  std::string InstructionStr;
+  raw_string_ostream SS(InstructionStr);
+  Printer->printInst(&Inst, 0, "", STI, SS);
+
+  // TODO(fzakaria): really bad way to get the OpStr. Find better way?
+  // idealy we should have Printer return a string rather than write to an
+  // ostream
+  StringRef OpStr =
+      StringRef(InstructionStr).split("\t").second.split("\t").second;
+
+  LLVM_DEBUG(dbgs() << Mnemonic << "\n");
+  LLVM_DEBUG(dbgs() << OpStr << "\n");
 }
 
 MCStreamer *llvm::createSQELFStreamer(MCContext &Context,
