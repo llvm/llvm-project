035aaae45cf42ed469e4cb60001307ecbeebd947
Noah Goldstein <goldstein.w.n@gmail.com>
2023-08-09 13:50:45 -0500

    [ValueTracking] Check likely to be canonicalized constant operands first in ; NFC
    Checking operands that a likely to be canonicalized constants first makes sense
    from a compile time perspective as it puts whats expected to be a cheaper check first.
    
    Reviewed By: nikic
    
    Differential Revision: https://reviews.llvm.org/D157313

--- llvm/lib/Analysis/ValueTracking.cpp
+++ llvm/lib/Analysis/ValueTracking.cpp
@@ -2061,8 +2061,8 @@ bool isKnownToBeAPowerOfTwo(const Value *V, bool OrZero, unsigned Depth,
   case Instruction::And:
     if (OrZero) {
       // A power of two and'd with anything is a power of two or zero.
-      if (isKnownToBeAPowerOfTwo(I->getOperand(0), /*OrZero*/ true, Depth, Q) ||
-          isKnownToBeAPowerOfTwo(I->getOperand(1), /*OrZero*/ true, Depth, Q))
+      if (isKnownToBeAPowerOfTwo(I->getOperand(1), /*OrZero*/ true, Depth, Q) ||
+          isKnownToBeAPowerOfTwo(I->getOperand(0), /*OrZero*/ true, Depth, Q))
         return true;
       // X & (-X) is always a power of two or zero.
       if (match(I->getOperand(0), m_Neg(m_Specific(I->getOperand(1)))) ||
