9c2395e6a7365faada62c51842f1771f22ea5844
pvanhout <pierre.vanhoutryve@amd.com>
2023-08-11 15:20:46 +0200

    [AMDGPU] Clear BreakPhiNodesCache in-between functions
    Otherwise stale pointers pollute the cache and
    when a dead PHI's memory is reused for another PHI, we can get a false positive hit in the cache.
    
    Reviewed By: arsenm
    
    Differential Revision: https://reviews.llvm.org/D157711

--- llvm/lib/Target/AMDGPU/AMDGPUCodeGenPrepare.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPUCodeGenPrepare.cpp
@@ -319,6 +319,7 @@ public:
 } // end anonymous namespace
 
 bool AMDGPUCodeGenPrepareImpl::run(Function &F) {
+  BreakPhiNodesCache.clear();
   bool MadeChange = false;
 
   Function::iterator NextBB;
