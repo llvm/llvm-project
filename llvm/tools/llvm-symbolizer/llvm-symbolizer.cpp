a1a6f30d5ee67a603bf75bc80b4082af3e7dfede
Serge Pavlov <sepavloff@gmail.com>
2023-08-12 15:52:33 +0700

    [symbolizer] Change reaction on invalid input
    If llvm-symbolizer finds a malformed command, it echoes it to the
    standard output. New versions of binutils (starting from 2.39) allow to
    specify an address by a symbols. Implementation of this feature in
    llvm-symbolizer makes the current reaction on invalid input
    inappropriate. Almost any invalid command may be treated as a symbol
    name, so the right reaction should be "symbol not found" in such case.
    
    The exception are commands that are recognized but have incorrect
    syntax, like "FILE:FILE:". The utility must produce descriptive
    diagnostic for such input and route it to the stderr.
    
    This change implements the new reaction on invalid input and is a
    prerequisite for implementation of symbol lookup in llvm-symbolizer.
    
    Differential Revision: https://reviews.llvm.org/D157210

--- llvm/tools/llvm-symbolizer/llvm-symbolizer.cpp
+++ llvm/tools/llvm-symbolizer/llvm-symbolizer.cpp
@@ -32,6 +32,7 @@
 #include "llvm/Support/COM.h"
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/Debug.h"
+#include "llvm/Support/Errc.h"
 #include "llvm/Support/FileSystem.h"
 #include "llvm/Support/InitLLVM.h"
 #include "llvm/Support/Path.h"
@@ -78,10 +79,10 @@ public:
 
 static std::string ToolName;
 
-static void printError(const ErrorInfoBase &EI, StringRef Path) {
+static void printError(const ErrorInfoBase &EI, StringRef AuxInfo) {
   WithColor::error(errs(), ToolName);
-  if (!EI.isA<FileError>())
-    errs() << "'" << Path << "': ";
+  if (!AuxInfo.empty())
+    errs() << "'" << AuxInfo << "': ";
   EI.log(errs());
   errs() << '\n';
 }
@@ -149,10 +150,14 @@ static StringRef getSpaceDelimitedWord(StringRef &Source) {
   return Result;
 }
 
-static bool parseCommand(StringRef BinaryName, bool IsAddr2Line,
-                         StringRef InputString, Command &Cmd,
-                         std::string &ModuleName, object::BuildID &BuildID,
-                         uint64_t &ModuleOffset) {
+static Error makeStringError(StringRef Msg) {
+  return make_error<StringError>(Msg, inconvertibleErrorCode());
+}
+
+static Error parseCommand(StringRef BinaryName, bool IsAddr2Line,
+                          StringRef InputString, Command &Cmd,
+                          std::string &ModuleName, object::BuildID &BuildID,
+                          uint64_t &ModuleOffset) {
   ModuleName = BinaryName;
   if (InputString.consume_front("CODE ")) {
     Cmd = Command::Code;
@@ -172,15 +177,13 @@ static bool parseCommand(StringRef BinaryName, bool IsAddr2Line,
     InputString = InputString.ltrim();
     if (InputString.consume_front("FILE:")) {
       if (HasFilePrefix || HasBuildIDPrefix)
-        // Input file specification prefix has already been seen.
-        return false;
+        return makeStringError("duplicate input file specification prefix");
       HasFilePrefix = true;
       continue;
     }
     if (InputString.consume_front("BUILDID:")) {
       if (HasBuildIDPrefix || HasFilePrefix)
-        // Input file specification prefix has already been seen.
-        return false;
+        return makeStringError("duplicate input file specification prefix");
       HasBuildIDPrefix = true;
       continue;
     }
@@ -189,17 +192,14 @@ static bool parseCommand(StringRef BinaryName, bool IsAddr2Line,
 
   if (HasBuildIDPrefix || HasFilePrefix) {
     if (!BinaryName.empty() || !BuildID.empty())
-      // Input file has already been specified on the command line.
-      return false;
+      return makeStringError("input file is already specified");
     StringRef Name = getSpaceDelimitedWord(InputString);
     if (Name.empty())
-      // Wrong name for module file.
-      return false;
+      return makeStringError("input file name is incorrect");
     if (HasBuildIDPrefix) {
       BuildID = parseBuildID(Name);
       if (BuildID.empty())
-        // Wrong format of BuildID hash.
-        return false;
+        return makeStringError("wrong format of build-id");
     } else {
       ModuleName = Name;
     }
@@ -209,7 +209,7 @@ static bool parseCommand(StringRef BinaryName, bool IsAddr2Line,
     ModuleName = getSpaceDelimitedWord(InputString);
     if (ModuleName.empty() || InputString.empty())
       // No input filename has been specified.
-      return false;
+      return makeStringError("no input filename is specified");
   }
 
   // Skip delimiters and parse module offset.
@@ -220,7 +220,14 @@ static bool parseCommand(StringRef BinaryName, bool IsAddr2Line,
   // "0x" or "0X" prefix; do the same for compatibility.
   if (IsAddr2Line)
     Offset.consume_front("0x") || Offset.consume_front("0X");
-  return !Offset.getAsInteger(IsAddr2Line ? 16 : 0, ModuleOffset);
+
+  // If the input is not a valid module offset, it is not an error, but its
+  // lookup does not make sense. Return error of different kind to distinguish
+  // from error or success.
+  if (Offset.getAsInteger(IsAddr2Line ? 16 : 0, ModuleOffset))
+    return errorCodeToError(errc::invalid_argument);
+
+  return Error::success();
 }
 
 template <typename T>
@@ -275,9 +282,16 @@ static void symbolizeInput(const opt::InputArgList &Args,
   std::string ModuleName;
   object::BuildID BuildID(IncomingBuildID.begin(), IncomingBuildID.end());
   uint64_t Offset = 0;
-  if (!parseCommand(Args.getLastArgValue(OPT_obj_EQ), IsAddr2Line,
-                    StringRef(InputString), Cmd, ModuleName, BuildID, Offset)) {
-    Printer.printInvalidCommand({ModuleName, std::nullopt}, InputString);
+  if (Error E = parseCommand(Args.getLastArgValue(OPT_obj_EQ), IsAddr2Line,
+                             StringRef(InputString), Cmd, ModuleName, BuildID,
+                             Offset)) {
+    handleAllErrors(
+        std::move(E),
+        [&](const StringError &EI) { printError(EI, InputString); },
+        [&](const ECError &EI) {
+          Request SymRequest = {ModuleName, std::nullopt};
+          Printer.print(SymRequest, DILineInfo());
+        });
     return;
   }
   bool ShouldInline = Args.hasFlag(OPT_inlines, OPT_no_inlines, !IsAddr2Line);
