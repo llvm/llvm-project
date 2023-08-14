1ef3de6b09f6b21a383fc7cf1ce1283df738015a
Daniel Kiss <daniel.kiss@arm.com>
2023-08-14 20:53:31 +0200

    Disable sanitizer's on ifunc resolvers.
    Resolvers are running before the module is initialised which leads to
    crashes due to the santizer is not yet initialised.
    
    Fixes #40287
    
    Reviewed By: hctim
    
    Differential Revision: https://reviews.llvm.org/D150262

--- clang/lib/CodeGen/CodeGenModule.cpp
+++ clang/lib/CodeGen/CodeGenModule.cpp
@@ -5832,7 +5832,9 @@ void CodeGenModule::emitIFuncDefinition(GlobalDecl GD) {
     Entry->eraseFromParent();
   } else
     GIF->setName(MangledName);
-
+  if (auto *F = dyn_cast<llvm::Function>(Resolver)) {
+    F->addFnAttr(llvm::Attribute::DisableSanitizerInstrumentation);
+  }
   SetCommonAttributes(GD, GIF);
 }
 
