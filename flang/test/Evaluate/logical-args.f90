1136fc4c5c68dd6bac0d1448c1cba25afc602679
David Truby <david@truby.dev>
2023-08-15 15:50:03 +0100

    [flang] Fix calls to interfaces with logical literal arguments
    Fixes: 64698
    
    Differential Revision: https://reviews.llvm.org/D157987

--- flang/test/Evaluate/logical-args.f90
+++ flang/test/Evaluate/logical-args.f90
@@ -2,7 +2,20 @@
 ! RUN: %flang_fc1 -fdebug-unparse %s 2>&1 | FileCheck %s
 ! RUN: %flang_fc1 -fdebug-unparse -fdefault-integer-8 %s 2>&1 | FileCheck %s --check-prefixes CHECK-8
 
+module m
+  interface foog
+     subroutine foo4(l)
+       logical(kind=4), intent(in) :: l
+     end subroutine foo4
+
+     subroutine foo8(l)
+       logical(kind=8), intent(in) :: l
+     end subroutine foo8
+  end interface foog
+end module m
+
 program main
+  use m
   integer :: x(10), y
   ! CHECK: CALL foo(.true._4)
   ! CHECK-8: CALL foo(logical(.true._4,kind=8))
@@ -10,6 +23,11 @@ program main
   ! CHECK: CALL fooa(x>y)
   ! CHECK-8: CALL fooa(logical(x>y,kind=8))
   call fooa(x > y)
+
+  ! Ensure that calls to interfaces call the correct subroutine
+  ! CHECK: CALL foo4(.true._4)
+  ! CHECK-8: CALL foo8(.true._8)
+  call foog(.true.)
 
   contains
     subroutine foo(l)
@@ -19,4 +37,5 @@ program main
     subroutine fooa(l)
       logical :: l(10)
     end subroutine fooa
+
 end program main
