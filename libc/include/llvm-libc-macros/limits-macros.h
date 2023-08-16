f0c1794a18033280850ee6a49cce3a14562fe49c
Alfred Persson Forsberg <cat@catcream.org>
2023-08-16 02:58:48 +0100

    [libc] limits.h: include compiler limits if not already included
    The LLVM-libc build itself will override include paths and prefer it's
    own limits.h over the compiler's limits.h. Because we rely on the
    compiler limits.h for numerical limits in LLVM-libc it needs to be
    include_next:ed if not already included. The other method to work
    around this is to define all numeric macros in place.
    
    Signed-off-by: Alfred Persson Forsberg <cat@catcream.org>
    
    Reviewed By: thesamesam
    
    Differential Revision: https://reviews.llvm.org/D158040

--- libc/include/llvm-libc-macros/limits-macros.h
+++ libc/include/llvm-libc-macros/limits-macros.h
@@ -1,6 +1,11 @@
 #ifndef __LLVM_LIBC_MACROS_LIMITS_MACROS_H
 #define __LLVM_LIBC_MACROS_LIMITS_MACROS_H
 
+#if !defined _GCC_LIMITS_H_ && !defined __CLANG_LIMITS_H &&                    \
+    __has_include_next(<limits.h>)
+#include_next <limits.h>
+#endif
+
 #ifdef __linux__
 #include <linux/limits.h>
 #endif
