13bbca8d694b4ef2ad03ec1805f07da8640c1a31
Alfred Persson Forsberg <cat@catcream.org>
2023-08-17 01:16:45 +0200

    [libc] [NFC] explain compiler macros in limits.h
    Differential Revision: https://reviews.llvm.org/D158128

--- libc/include/llvm-libc-macros/limits-macros.h
+++ libc/include/llvm-libc-macros/limits-macros.h
@@ -1,6 +1,12 @@
 #ifndef __LLVM_LIBC_MACROS_LIMITS_MACROS_H
 #define __LLVM_LIBC_MACROS_LIMITS_MACROS_H
 
+// Normally compiler headers will be prefered over LLVM-libc headers and
+// include_next this header, however, during LLVM-libc build itself the
+// LLVM-libc headers are prefered, and to get C numerical limits we need to
+// include compiler (freestanding) limits.h. The macro checks are here to avoid
+// including limits.h when compiler headers have already been included.
+
 #if !defined _GCC_LIMITS_H_ && !defined __CLANG_LIMITS_H &&                    \
     __has_include_next(<limits.h>)
 #include_next <limits.h>
