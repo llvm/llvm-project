e428f6a27120b4dd9342b9aaa74c317f30af6d9f
Jason Molenda <jmolenda@apple.com>
2023-08-16 16:10:30 -0700

    hi/low addr space bits can be sent in stop-rely packet
    Add support for the `low_mem_addressing_bits` and
    `high_mem_addressing_bits` keys in the stop reply packet,
    in addition to the existing `addressing_bits`.  Same
    behavior as in the qHostInfo packet.
    
    Clean up AddressableBits so we don't need to check if
    any values have been set in the object before using it
    to potentially update the Process address masks.
    
    Differential Revision: https://reviews.llvm.org/D158041
    
    (cherry picked from commit 6f4a0c762fe2c0077865e0e30e3dfd67cd6287d1)

--- lldb/source/Plugins/ObjectFile/Mach-O/ObjectFileMachO.h
+++ lldb/source/Plugins/ObjectFile/Mach-O/ObjectFileMachO.h
@@ -122,7 +122,7 @@ public:
 
   std::string GetIdentifierString() override;
 
-  bool GetAddressableBits(lldb_private::AddressableBits &address_bits) override;
+  lldb_private::AddressableBits GetAddressableBits() override;
 
   bool GetCorefileMainBinaryInfo(lldb::addr_t &value, bool &value_is_offset,
                                  lldb_private::UUID &uuid,
