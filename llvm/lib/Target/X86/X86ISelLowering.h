c2561f0ad9cd3ad07e47d7702bba77e431f540d5
Noah Goldstein <goldstein.w.n@gmail.com>
2023-08-16 01:11:01 -0500

    [DAGCombiner][X86] Guard `(X & Y) ==/!= Y` --> `(X & Y) !=/== 0` behind TLI preference
    On X86 for vec types `(X & Y) == Y` is generally preferable to
    `(X & Y) != 0`. Creating zero requires an extra instruction and on
    pre-avx512 targets there is no vector `pcmpne` so it requires two
    additional instructions to invert the `pcmpeq`.
    
    Reviewed By: RKSimon
    
    Differential Revision: https://reviews.llvm.org/D157014

--- llvm/lib/Target/X86/X86ISelLowering.h
+++ llvm/lib/Target/X86/X86ISelLowering.h
@@ -1057,6 +1057,9 @@ namespace llvm {
     bool preferSextInRegOfTruncate(EVT TruncVT, EVT VT,
                                    EVT ExtVT) const override;
 
+    bool isXAndYEqZeroPreferableToXAndYEqY(ISD::CondCode Cond,
+                                           EVT VT) const override;
+
     /// Return true if the target has native support for
     /// the specified value type and it is 'desirable' to use the type for the
     /// given node type. e.g. On x86 i16 is legal, but undesirable since i16
