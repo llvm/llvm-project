b5400bb834053ba47d31efca63f4a043da734f60
Johannes Doerfert <johannes@jdoerfert.de>
2023-08-17 13:42:06 -0700

    [Attributor][NFC] Precommit tests

---
+++ openmp/libomptarget/test/jit/type_punning.c
@@ -0,0 +1,39 @@
+// clang-format off
+//
+// RUN: %libomptarget-compileopt-generic -fopenmp-target-jit
+// RUN: env LIBOMPTARGET_JIT_PRE_OPT_IR_MODULE=%t.pre.ll     \
+// RUN:     LIBOMPTARGET_JIT_SKIP_OPT=true                   \
+// RUN:     %libomptarget-run-generic
+// RUN: %fcheck-plain-generic --input-file %t.pre.ll %s
+//
+// clang-format on
+
+// UNSUPPORTED: aarch64-unknown-linux-gnu
+// UNSUPPORTED: aarch64-unknown-linux-gnu-LTO
+// UNSUPPORTED: x86_64-pc-linux-gnu
+// UNSUPPORTED: x86_64-pc-linux-gnu-LTO
+
+// FIXME: We want that there is only the kernel function left, not two outlined
+// parallel regions.
+//
+// CHECK: define
+// CHECK: define
+// CHECK: define
+
+#include <omp.h>
+void f(long *A, int N) {
+  long i = 0;
+#pragma omp target map(A[ : N])
+  {
+#pragma omp parallel firstprivate(i)
+    A[omp_get_thread_num()] = i;
+#pragma omp parallel firstprivate(i, N)
+    A[omp_get_thread_num()] += i + N;
+  }
+}
+
+int main() {
+  long A[1];
+  f(&A[0], 1);
+  return A[0];
+}
