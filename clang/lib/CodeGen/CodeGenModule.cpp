0e17372b380467ac8339afdec992fbf887a11feb
Jonas Hahnfeld <jonas.hahnfeld@cern.ch>
2023-08-17 14:21:39 +0200

    Revert "[CodeGen] Keep track of eagerly emitted globals"
    The added test doesn't work on Windows:
    https://lab.llvm.org/buildbot/#/builders/216/builds/25769
    
    This reverts commit f8dadefd4afc8e1b7b9a1e69a0ba8df051e03328.

--- clang/lib/CodeGen/CodeGenModule.cpp
+++ clang/lib/CodeGen/CodeGenModule.cpp
@@ -3658,7 +3658,6 @@ void CodeGenModule::EmitGlobal(GlobalDecl GD) {
   if (MustBeEmitted(Global) && MayBeEmittedEagerly(Global)) {
     // Emit the definition if it can't be deferred.
     EmitGlobalDefinition(GD);
-    addEmittedDeferredDecl(GD);
     return;
   }
 
