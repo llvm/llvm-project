30c4b97aec60895a6905816670f493cdd1d7c546
Han Shen <shenhan@google.com>
2023-08-17 10:09:41 -0700

    [test] Fix unit test case errors.
    This fix includes two changes:
    - use proper triple name for arm: arm-unknown-linux-gnueabi
    - specify output to be in a temp directory
    
    Test cases fixed:
     - clang/test/Driver/fsplit-machine-functions-with-cuda-nvptx.c
     - clang/test/Driver/fsplit-machine-functions.c
     - clang/test/Driver/fsplit-machine-functions2.c
    
    Reviewed: xur

--- clang/test/Driver/fsplit-machine-functions-with-cuda-nvptx.c
+++ clang/test/Driver/fsplit-machine-functions-with-cuda-nvptx.c
@@ -5,21 +5,24 @@
 
 // Check that -fsplit-machine-functions is passed to both x86 and cuda
 // compilation and does not cause driver error.
+// RUN:   cd "$(dirname "%t")" ; \
 // RUN:   %clang -### --target=x86_64-unknown-linux-gnu -nogpulib -nogpuinc \
-// RUN:     --cuda-gpu-arch=sm_70 -x cuda -fsplit-machine-functions -S %s 2>&1 \
-// RUN:     | FileCheck %s --check-prefix=MFS1
+// RUN:     --cuda-gpu-arch=sm_70 -x cuda -fsplit-machine-functions -S %s \
+// RUN:     2>&1 | FileCheck %s --check-prefix=MFS1
 // MFS1: "-target-cpu" "x86-64"{{.*}}"-fsplit-machine-functions"
 // MFS1: "-target-cpu" "sm_70"{{.*}}"-fsplit-machine-functions"
 
 // Check that -fsplit-machine-functions is passed to cuda and it
 // causes a warning.
+// RUN:   cd "$(dirname "%t")" ; \
 // RUN:   %clang --target=x86_64-unknown-linux-gnu -nogpulib -nogpuinc \
-// RUN:     --cuda-gpu-arch=sm_70 -x cuda -fsplit-machine-functions -S %s 2>&1 \
-// RUN:     | FileCheck %s --check-prefix=MFS2
+// RUN:     --cuda-gpu-arch=sm_70 -x cuda -fsplit-machine-functions -S %s \
+// RUN:     2>&1 | FileCheck %s --check-prefix=MFS2
 // MFS2: warning: -fsplit-machine-functions is not valid for nvptx
 
 // Check that -Xarch_host -fsplit-machine-functions is passed only to
 // native compilation.
+// RUN:   cd "$(dirname "%t")" ; \
 // RUN:   %clang -### --target=x86_64-unknown-linux-gnu -nogpulib -nogpuinc \
 // RUN:     --cuda-gpu-arch=sm_70 -x cuda -Xarch_host \
 // RUN:     -fsplit-machine-functions -S %s \
@@ -28,6 +31,7 @@
 // MFS3-NOT: "-target-cpu" "sm_70"{{.*}}"-fsplit-machine-functions"
 
 // Check that -Xarch_host -fsplit-machine-functions does not cause any warning.
+// RUN:   cd "$(dirname "%t")" ; \
 // RUN:   %clang --target=x86_64-unknown-linux-gnu -nogpulib -nogpuinc \
 // RUN      --cuda-gpu-arch=sm_70 -x cuda -Xarch_host \
 // RUN      -fsplit-machine-functions -S %s || { echo \
@@ -36,6 +40,7 @@
 // MFS4-NOT: warning: -fsplit-machine-functions is not valid for
 
 // Check that -Xarch_device -fsplit-machine-functions does cause the warning.
+// RUN:   cd "$(dirname "%t")" ; \
 // RUN:   %clang --target=x86_64-unknown-linux-gnu -nogpulib -nogpuinc \
 // RUN:     --cuda-gpu-arch=sm_70 -x cuda -Xarch_device \
 // RUN:     -fsplit-machine-functions -S %s 2>&1 | \
@@ -44,6 +49,7 @@
 
 // Check that -fsplit-machine-functions -Xarch_device
 // -fno-split-machine-functions only passes MFS to x86
+// RUN:   cd "$(dirname "%t")" ; \
 // RUN:   %clang -### --target=x86_64-unknown-linux-gnu -nogpulib -nogpuinc \
 // RUN:     --cuda-gpu-arch=sm_70 -x cuda -fsplit-machine-functions \
 // RUN:     -Xarch_device -fno-split-machine-functions -S %s \
@@ -53,6 +59,7 @@
 
 // Check that -fsplit-machine-functions -Xarch_device
 // -fno-split-machine-functions has no warnings
+// RUN:   cd "$(dirname "%t")" ; \
 // RUN:   %clang --target=x86_64-unknown-linux-gnu -nogpulib -nogpuinc \
 // RUN:     --cuda-gpu-arch=sm_70 -x cuda -fsplit-machine-functions \
 // RUN:     -Xarch_device -fno-split-machine-functions -S %s \
