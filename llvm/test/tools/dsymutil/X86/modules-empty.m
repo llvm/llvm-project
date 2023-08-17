9ae57c74c8146c4eabae661f1be06c92f7b794cb
Alexey Lapshin <a.v.lapshin@mail.ru>
2023-08-17 12:45:12 +0200

    Revert "[DWARFLinkerParallel] Add limited functionality to DWARFLinkerParallel."
    This reverts commit 36b82f1cd5e72815650075bdadf27dfd8b54109a.

--- llvm/test/tools/dsymutil/X86/modules-empty.m
+++ llvm/test/tools/dsymutil/X86/modules-empty.m
@@ -12,13 +12,8 @@ EOF
 
 // RUN: rm -rf %t.dir
 // RUN: mkdir %t.dir
-// RUN: cp %p/../Inputs/modules-empty/1.o %p/../Inputs/modules-empty/Empty.pcm \
-// RUN: %t.dir
+// RUN: cp %p/../Inputs/modules-empty/1.o %p/../Inputs/modules-empty/Empty.pcm %t.dir
 // RUN: dsymutil -f -oso-prepend-path=%t.dir \
-// RUN:   -verify \
-// RUN:   -y %p/dummy-debug-map.map -o - \
-// RUN:     | llvm-dwarfdump --debug-info - | FileCheck %s
-// RUN: dsymutil --linker llvm -f -oso-prepend-path=%t.dir \
 // RUN:   -verify \
 // RUN:   -y %p/dummy-debug-map.map -o - \
 // RUN:     | llvm-dwarfdump --debug-info - | FileCheck %s
