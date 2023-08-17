a078eb589bbf4737a4df415c83d27c6f3dd03898
Saleem Abdulrasool <abdulras@google.com>
2023-08-17 07:44:32 -0700

    APINotes: add initial stub of APINotesWriter
    This adds the skeleton for serializing out the APINotes data from the
    APINotes. The writer uses a private implementation pattern to reduce
    the exposed surface to just the programmatic representation of the
    APINotes and not expose the details of the bitcode encoding. The format
    itself is not considered stable and should only be accessed through the
    APINotes Reader and Writer types.
    
    Differential Revision: https://reviews.llvm.org/D92797
    Reviewed By: martong

--- clang/lib/APINotes/APINotesFormat.h
+++ clang/lib/APINotes/APINotesFormat.h
@@ -252,4 +252,36 @@ struct StoredObjCSelector {
 } // namespace api_notes
 } // namespace clang
 
+namespace llvm {
+template <> struct DenseMapInfo<clang::api_notes::StoredObjCSelector> {
+  typedef DenseMapInfo<unsigned> UnsignedInfo;
+
+  static inline clang::api_notes::StoredObjCSelector getEmptyKey() {
+    return clang::api_notes::StoredObjCSelector{UnsignedInfo::getEmptyKey(),
+                                                {}};
+  }
+
+  static inline clang::api_notes::StoredObjCSelector getTombstoneKey() {
+    return clang::api_notes::StoredObjCSelector{UnsignedInfo::getTombstoneKey(),
+                                                {}};
+  }
+
+  static unsigned
+  getHashValue(const clang::api_notes::StoredObjCSelector &Selector) {
+    auto hash = llvm::hash_value(Selector.NumPieces);
+    hash = hash_combine(hash, Selector.Identifiers.size());
+    for (auto piece : Selector.Identifiers)
+      hash = hash_combine(hash, static_cast<unsigned>(piece));
+    // FIXME: Mix upper/lower 32-bit values together to produce
+    // unsigned rather than truncating.
+    return hash;
+  }
+
+  static bool isEqual(const clang::api_notes::StoredObjCSelector &LHS,
+                      const clang::api_notes::StoredObjCSelector &RHS) {
+    return LHS.NumPieces == RHS.NumPieces && LHS.Identifiers == RHS.Identifiers;
+  }
+};
+} // namespace llvm
+
 #endif
