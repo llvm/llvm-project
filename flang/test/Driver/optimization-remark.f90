a7ec1d05fe1d2be3dc3a1037135f5f67f889a748
Victor Kingi <victor.kingi@arm.com>
2023-08-17 11:19:12 +0000

    [Flang][Driver] Implement OPT_R_Joined options
    Add a BackendRemarkConsumer class, responsible for handling diagnostics
    received from LLVM. The diagnostics being information on middle and
    backend passes used or not used.
    
    Clang by default has all remarks ignored but manually sets the severity of
    `R_Group` to visible(`clang::diag::clang::Severity::Remark`). This patch does
    the same for Flang.
    
    Depends on D157410. That patch adds the R family of options to
    `FlangOption` and `FC1Option` in
    `clang/include/clang/Driver/Options.td`
    
    Reviewed By: awarzynski
    
    Differential Revision: https://reviews.llvm.org/D158174

---
+++ flang/test/Driver/optimization-remark.f90
@@ -0,0 +1,46 @@
+! This file tests the -Rpass family of flags (-Rpass, -Rpass-missed
+! and -Rpass-analysis)
+! loop-delete isn't enabled at O0 so we use at least O1
+
+! Check that we can override -Rpass= with -Rno-pass.
+! RUN: %flang_fc1 %s -O1 -Rpass -emit-llvm -o - 2>&1 | FileCheck %s --check-prefix=REMARKS
+! RUN: %flang_fc1 %s -O1 -Rpass -Rno-pass -emit-llvm -o - 2>&1 | FileCheck %s --check-prefix=NO-REMARKS
+
+! Check -Rno-pass, -Rno-pass-analysis, -Rno-pass-missed nothing emitted
+! RUN: %flang %s -O1 -Rno-pass 2>&1 | FileCheck %s --allow-empty --check-prefix=NO-REMARKS
+! RUN: %flang %s -O1 -Rno-pass-missed 2>&1 | FileCheck %s --allow-empty --check-prefix=NO-REMARKS
+! RUN: %flang %s -O1 -Rno-pass-analysis 2>&1 | FileCheck %s --allow-empty --check-prefix=NO-REMARKS
+
+! Check full -Rpass message is emitted
+! RUN: %flang %s -O1 -Rpass 2>&1 | FileCheck %s
+
+! Check full -Rpass-missed message is emitted
+! RUN: %flang %s -O1 -Rpass-missed 2>&1 | FileCheck %s --check-prefix=REMARKS-MISSED
+
+! Check full -Rpass-analysis message is emitted
+! RUN: %flang %s -O1 -Rpass-analysis 2>&1 | FileCheck %s --check-prefix=REMARKS-ANALYSIS
+
+! CHECK: remark: Loop deleted because it is invariant
+! REMARKS-MISSED: remark: loop not vectorized
+! REMARKS-MISSED-NOT: instruction cannot be vectorized
+! REMARKS-ANALYSIS: remark: loop not vectorized: instruction cannot be vectorized
+! REMARKS-ANALYSIS-NOT: will not be inlined into {{.*}} because its definition is unavailable
+
+! REMARKS: remark:
+! NO-REMARKS-NOT: remark:
+
+program forttest
+    implicit none
+    real, dimension(1:50) :: aR1
+    integer :: n
+
+    do n = 1,50
+        aR1(n) = n * 1.34
+        print *, "hello"
+    end do
+
+    do n = 1,50
+        aR1(n) = n * 1.34
+    end do
+
+end program forttest
