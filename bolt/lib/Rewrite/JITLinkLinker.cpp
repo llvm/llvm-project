b6b3149cbc4cdd962b7ffcb0131d6283929bb597
Rafael Auler <rafaelauler@fb.com>
2023-08-17 18:14:53 -0700

    [BOLT] Support input binaries that use R_X86_GOTPC64
    In large code model, the address of GOT is calculated by the
    static linker via R_X86_GOTPC64 reloc applied against a MOVABSQ
    instruction. In the final binary, it can be disassembled as a regular
    immediate, but because such immediate is the result of PC-relative
    pointer arithmetic, we need to parse this relocation and update this
    calculation whenever we move code, otherwise we break the code trying
    to read GOT.
    
    A test case showing how GOT is accessed was provided.
    
    Differential Revision: https://reviews.llvm.org/D158911

--- bolt/lib/Rewrite/JITLinkLinker.cpp
+++ bolt/lib/Rewrite/JITLinkLinker.cpp
@@ -141,6 +141,20 @@ struct JITLinkLinker::Context : jitlink::JITLinkContext {
             orc::ExecutorAddr(Address), JITSymbolFlags());
         continue;
       }
+
+      if (Linker.BC.isGlobalOffsetTableSym(SymName)) {
+        if (const BinaryData *I = Linker.BC.getGlobalOffsetTableSym()) {
+          uint64_t Address = I->isMoved() && !I->isJumpTable()
+                                 ? I->getOutputAddress()
+                                 : I->getAddress();
+          LLVM_DEBUG(dbgs() << "Resolved to address 0x"
+                            << Twine::utohexstr(Address) << "\n");
+          AllResults[Symbol.first] = orc::ExecutorSymbolDef(
+              orc::ExecutorAddr(Address), JITSymbolFlags());
+          continue;
+        }
+      }
+
       LLVM_DEBUG(dbgs() << "Resolved to address 0x0\n");
       AllResults[Symbol.first] =
           orc::ExecutorSymbolDef(orc::ExecutorAddr(0), JITSymbolFlags());
