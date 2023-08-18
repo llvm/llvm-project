7d456ab05f037328a70eea054d10109e54b82cbd
Valentin Clement <clementval@gmail.com>
2023-08-18 09:28:43 -0700

    [flang][openacc][NFC] Split global acc declare and function/subroutine declare
    Split the tests for declare in module specification and the declare in
    function/subroutine specificatons. Since the lowering is specific to the
    location of the declare it will make it easier to update these files.
    This also remove the duplicate parts shared by the FIR vs HLFIR tests.
    
    Reviewed By: razvanlupusoru
    
    Differential Revision: https://reviews.llvm.org/D158222

--- flang/test/Lower/OpenACC/HLFIR/acc-declare.f90
+++ flang/test/Lower/OpenACC/HLFIR/acc-declare.f90
@@ -1,74 +1,8 @@
-! This test checks lowering of OpenACC declare directive.
+! This test checks lowering of OpenACC declare directive in function and
+! subroutine specification parts.
 
 ! RUN: bbc -fopenacc -emit-hlfir %s -o - | FileCheck %s --check-prefixes=HLFIR,ALL
 ! RUN: bbc -fopenacc -emit-fir -hlfir %s -o - | FileCheck %s --check-prefixes=FIR,ALL
-
-module acc_declare_test
- integer, parameter :: n = 100000
- real, dimension(n) :: data1
- !$acc declare create(data1)
-end module
-
-! ALL-LABEL: fir.global @_QMacc_declare_testEdata1 {acc.declare = #acc.declare<dataClause = acc_create>} : !fir.array<100000xf32>
-
-! ALL-LABEL: acc.global_ctor @_QMacc_declare_testEdata1_acc_ctor {
-! ALL:         %[[GLOBAL_ADDR:.*]] = fir.address_of(@_QMacc_declare_testEdata1) {acc.declare = #acc.declare<dataClause = acc_create>} : !fir.ref<!fir.array<100000xf32>>
-! ALL:         %[[CREATE:.*]] = acc.create varPtr(%[[GLOBAL_ADDR]] : !fir.ref<!fir.array<100000xf32>>) -> !fir.ref<!fir.array<100000xf32>> {name = "data1", structured = false}
-! ALL:         acc.declare_enter dataOperands(%[[CREATE]] : !fir.ref<!fir.array<100000xf32>>)
-! ALL:         acc.terminator
-! ALL:       }
-
-! ALL-LABEL: acc.global_dtor @_QMacc_declare_testEdata1_acc_dtor {
-! ALL:         %[[GLOBAL_ADDR:.*]] = fir.address_of(@_QMacc_declare_testEdata1) {acc.declare = #acc.declare<dataClause = acc_create>} : !fir.ref<!fir.array<100000xf32>>
-! ALL:         %[[DEVICEPTR:.*]] = acc.getdeviceptr varPtr(%[[GLOBAL_ADDR]] : !fir.ref<!fir.array<100000xf32>>) -> !fir.ref<!fir.array<100000xf32>> {dataClause = #acc<data_clause acc_create>, name = "data1", structured = false}
-! ALL:         acc.declare_exit dataOperands(%[[DEVICEPTR]] : !fir.ref<!fir.array<100000xf32>>)
-! ALL:         acc.delete accPtr(%[[DEVICEPTR]] : !fir.ref<!fir.array<100000xf32>>) {dataClause = #acc<data_clause acc_create>, name = "data1", structured = false}
-! ALL:         acc.terminator
-! ALL:       }
-
-module acc_declare_copyin_test
- integer, parameter :: n = 100000
- real, dimension(n) :: data1
- !$acc declare copyin(data1)
-end module
-
-
-! ALL-LABEL: acc.global_ctor @_QMacc_declare_copyin_testEdata1_acc_ctor {
-! ALL:         %[[GLOBAL_ADDR:.*]] = fir.address_of(@_QMacc_declare_copyin_testEdata1) {acc.declare = #acc.declare<dataClause =  acc_copyin>} : !fir.ref<!fir.array<100000xf32>>
-! ALL:         %[[COPYIN:.*]] = acc.copyin varPtr(%[[GLOBAL_ADDR]] : !fir.ref<!fir.array<100000xf32>>) -> !fir.ref<!fir.array<100000xf32>> {name = "data1", structured = false}
-! ALL:         acc.declare_enter dataOperands(%[[COPYIN]] : !fir.ref<!fir.array<100000xf32>>)
-! ALL:         acc.terminator
-! ALL:       }
-
-module acc_declare_device_resident_test
- integer, parameter :: n = 5000
- integer, dimension(n) :: data1
- !$acc declare device_resident(data1)
-end module
-
-! ALL-LABEL: fir.global @_QMacc_declare_device_resident_testEdata1 {acc.declare = #acc.declare<dataClause =  acc_declare_device_resident>} : !fir.array<5000xi32>
-
-! ALL-LABEL: acc.global_ctor @_QMacc_declare_device_resident_testEdata1_acc_ctor {
-! ALL:         %[[GLOBAL_ADDR:.*]] = fir.address_of(@_QMacc_declare_device_resident_testEdata1) {acc.declare = #acc.declare<dataClause =  acc_declare_device_resident>} : !fir.ref<!fir.array<5000xi32>>
-! ALL:         %[[DEVICERESIDENT:.*]] = acc.declare_device_resident varPtr(%0 : !fir.ref<!fir.array<5000xi32>>) -> !fir.ref<!fir.array<5000xi32>> {name = "data1", structured = false}
-! ALL:         acc.declare_enter dataOperands(%[[DEVICERESIDENT]] : !fir.ref<!fir.array<5000xi32>>)
-! ALL:         acc.terminator
-! ALL:       }
-
-module acc_declare_device_link_test
- integer, parameter :: n = 5000
- integer, dimension(n) :: data1
- !$acc declare link(data1)
-end module
-
-! ALL-LABEL: fir.global @_QMacc_declare_device_link_testEdata1 {acc.declare = #acc.declare<dataClause =  acc_declare_link>} : !fir.array<5000xi32> {
-
-! ALL-LABEL: acc.global_ctor @_QMacc_declare_device_link_testEdata1_acc_ctor {
-! ALL:         %[[GLOBAL_ADDR:.*]] = fir.address_of(@_QMacc_declare_device_link_testEdata1) {acc.declare = #acc.declare<dataClause =  acc_declare_link>} : !fir.ref<!fir.array<5000xi32>>
-! ALL:         %[[LINK:.*]] = acc.declare_link varPtr(%[[GLOBAL_ADDR]] : !fir.ref<!fir.array<5000xi32>>) -> !fir.ref<!fir.array<5000xi32>> {name = "data1", structured = false}
-! ALL:         acc.declare_enter dataOperands(%[[LINK]] : !fir.ref<!fir.array<5000xi32>>)
-! ALL:         acc.terminator
-! ALL:       }
 
 module acc_declare
   contains
