dcb7bcff276664c12e1eb05e19e3e845a76b7116
Nico Weber <thakis@chromium.org>
2023-08-18 09:44:09 -0400

    clang: Fix x86-no-gather-no-scatter.cpp on macOS after 993bdb047c90e9
    On macOS, files are usually below `/Users/...` and clang-cl treats
    that as the `/U` flag followed by something instead of as a path.
    Put `--` in front of `%s` to make it treat it as a patch, like in
    all other tests.
    
    The failure without this change:
    
        x86-no-gather-no-scatter.cpp:4:14: error: NOGATHER: expected string not found in input
        // NOGATHER: "-target-feature" "+prefer-no-gather"
                     ^
        <stdin>:5:44: note: possible intended match here
        clang: warning: 'x86-no-gather-no-scatter.cpp' treated as the '/U' option [-Wslash-u-filename]
                                                   ^
    
    (cherry picked from commit 547ee1c81fceaabcb7064ed525f11f9e94083f56)

--- clang/test/Driver/x86-no-gather-no-scatter.cpp
+++ clang/test/Driver/x86-no-gather-no-scatter.cpp
@@ -1,8 +1,8 @@
 /// Tests -mno-gather and -mno-scatter
-// RUN: %clang -c -mno-gather -### %s 2>&1 | FileCheck --check-prefix=NOGATHER %s
-// RUN: %clang_cl -c /Qgather- -### %s 2>&1 | FileCheck --check-prefix=NOGATHER %s
+// RUN: %clang -target x86_64-unknown-linux-gnu -c -mno-gather -### %s 2>&1 | FileCheck --check-prefix=NOGATHER %s
+// RUN: %clang_cl --target=x86_64-windows -c /Qgather- -### -- %s 2>&1 | FileCheck --check-prefix=NOGATHER %s
 // NOGATHER: "-target-feature" "+prefer-no-gather"
 
-// RUN: %clang -c -mno-scatter -### %s 2>&1 | FileCheck --check-prefix=NOSCATTER %s
-// RUN: %clang_cl -c /Qscatter- -### %s 2>&1 | FileCheck --check-prefix=NOSCATTER %s
+// RUN: %clang -target x86_64-unknown-linux-gnu -c -mno-scatter -### %s 2>&1 | FileCheck --check-prefix=NOSCATTER %s
+// RUN: %clang_cl --target=x86_64-windows -c /Qscatter- -### -- %s 2>&1 | FileCheck --check-prefix=NOSCATTER %s
 // NOSCATTER: "-target-feature" "+prefer-no-scatter"
