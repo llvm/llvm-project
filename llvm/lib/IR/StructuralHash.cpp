f879a1b3b063c43103679ff19ae87f5413a56831
Aiden Grossman <agrossman154@yahoo.com>
2023-08-19 12:23:16 -0700

    Revert "[NFCi][MergeFunctions] Consolidate Hashing Functions"
    This reverts commit fc27ac0f55986d525364061d41f5cf61deda3e88.
    
    This patch was causing build failures on multiple buildbots on 32-bit
    architectures. Reverting now so I can deboug out-of-trunk and resubmit
    later.

--- llvm/lib/IR/StructuralHash.cpp
+++ llvm/lib/IR/StructuralHash.cpp
@@ -27,28 +27,12 @@ class StructuralHashImpl {
 public:
   StructuralHashImpl() : Hash(4) {}
 
-  // A function hash is calculated by considering only the number of arguments
-  // and whether a function is varargs, the order of basic blocks (given by the
-  // successors of each basic block in depth first order), and the order of
-  // opcodes of each instruction within each of these basic blocks. This mirrors
-  // the strategy FunctionComparator::compare() uses to compare functions by
-  // walking the BBs in depth first order and comparing each instruction in
-  // sequence. Because this hash currently does not look at the operands, it is
-  // insensitive to things such as the target of calls and the constants used in
-  // the function, which makes it useful when possibly merging functions which
-  // are the same modulo constants and call targets.
-  //
-  // Note that different users of StructuralHash will want different behavior
-  // out of it (i.e., MergeFunctions will want something different from PM
-  // expensive checks for pass modification status). When modifying this
-  // function, most changes should be gated behind an option and enabled
-  // selectively.
   void update(const Function &F) {
     // Declarations don't affect analyses.
     if (F.isDeclaration())
       return;
 
-    hash(0x6acaa36bef8325c5ULL); // Function header
+    hash(12345); // Function header
 
     hash(F.isVarArg());
     hash(F.arg_size());
@@ -56,18 +40,11 @@ public:
     SmallVector<const BasicBlock *, 8> BBs;
     SmallPtrSet<const BasicBlock *, 16> VisitedBBs;
 
-    // Walk the blocks in the same order as
-    // FunctionComparator::cmpBasicBlocks(), accumulating the hash of the
-    // function "structure." (BB and opcode sequence)
     BBs.push_back(&F.getEntryBlock());
     VisitedBBs.insert(BBs[0]);
     while (!BBs.empty()) {
       const BasicBlock *BB = BBs.pop_back_val();
-
-      // This random value acts as a block header, as otherwise the partition of
-      // opcodes into BBs wouldn't affect the hash, only the order of the
-      // opcodes
-      hash(45798);
+      hash(45798); // Block header
       for (auto &Inst : *BB)
         hash(Inst.getOpcode());
 
@@ -102,13 +79,13 @@ public:
 
 } // namespace
 
-IRHash llvm::StructuralHash(const Function &F) {
+uint64_t llvm::StructuralHash(const Function &F) {
   StructuralHashImpl H;
   H.update(F);
   return H.getHash();
 }
 
-IRHash llvm::StructuralHash(const Module &M) {
+uint64_t llvm::StructuralHash(const Module &M) {
   StructuralHashImpl H;
   H.update(M);
   return H.getHash();
