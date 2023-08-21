97defdd9a1f9ff8512babb285598fc8fdcfe6b67
Alexey Bataev <a.bataev@outlook.com>
2023-08-21 13:01:03 -0700

    [IR]Add NumSrcElts param to is..Mask static function in ShuffleVectorInst.
    Need to add NumSrcElts param to is..Mask functions in
    ShuffleVectorInstruction class for better mask analysis. Mask.size() not
    always matches the sizes of the permuted vector(s). Allows to better
    estimate the cost in SLP and fix uses of the functions in other cases.
    
    Differential Revision: https://reviews.llvm.org/D158449

--- llvm/lib/Target/RISCV/RISCVISelLowering.cpp
+++ llvm/lib/Target/RISCV/RISCVISelLowering.cpp
@@ -4337,7 +4337,8 @@ static SDValue lowerBitreverseShuffle(ShuffleVectorSDNode *SVN,
 
   assert(VT.getVectorElementType() == MVT::i1);
 
-  if (!ShuffleVectorInst::isReverseMask(SVN->getMask()) ||
+  if (!ShuffleVectorInst::isReverseMask(SVN->getMask(),
+                                        SVN->getMask().size()) ||
       !SVN->getOperand(1).isUndef())
     return SDValue();
 
