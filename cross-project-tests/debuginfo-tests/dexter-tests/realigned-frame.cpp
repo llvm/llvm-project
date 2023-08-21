8df9eff90ff8c79e3e508cce7fec1cd897487069
Stephen Tozer <Stephen.Tozer@Sony.com>
2023-08-21 18:01:06 +0100

    Revert "[Dexter] Remove builder from Dexter" & Revert "[Dexter] Fix incorrect substitution errors in clang-cl builder"
    This reverts commits 262520a3c5450fd7f149438245b2aef12736347f,
    and 0b72b71cd3c8ad824342c05bc6d9d64d87eeb81b.
    
    Failures occurred on two buildbots, the SIE buildbot:
    https://lab.llvm.org/buildbot/#/builders/216/builds/26006
    
    And the green dragon buildbot:
    https://green.lab.llvm.org/green/view/LLDB/job/lldb-cmake/59091
    
    Errors appear to be related to incorrect tool substitution in the Dexter
    test commands, and a currently unknown error with one of the general
    debuginfo tests that uses Dexter.

--- cross-project-tests/debuginfo-tests/dexter-tests/realigned-frame.cpp
+++ cross-project-tests/debuginfo-tests/dexter-tests/realigned-frame.cpp
@@ -1,7 +1,7 @@
 // REQUIRES: system-windows
 //
-// RUN: %clang_cl /Z7 /Zi %s -o %t
-// RUN: %dexter --fail-lt 1.0 -w --binary %t --debugger 'dbgeng' -- %s
+// RUN: %dexter --fail-lt 1.0 -w --builder 'clang-cl_vs2015' \
+// RUN:      --debugger 'dbgeng' --cflags '/Z7 /Zi' --ldflags '/Z7 /Zi' -- %s
 
 // From https://llvm.org/pr38857, where we had issues with stack realignment.
 
--- cross-project-tests/debuginfo-tests/dexter/feature_tests/commands/perfect/dex_finish_test/default_simple.cpp
+++ cross-project-tests/debuginfo-tests/dexter/feature_tests/commands/perfect/dex_finish_test/default_simple.cpp
@@ -4,8 +4,7 @@
 //      is stepped on.
 //      Tests using the default controller (no \DexLimitSteps).
 //
-// RUN: %dexter_regression_test_build %s -o %t
-// RUN: %dexter_regression_test_run --binary %t -- %s | FileCheck %s
+// RUN: %dexter_regression_test -- %s | FileCheck %s
 // CHECK: default_simple.cpp
 
 int main() {
