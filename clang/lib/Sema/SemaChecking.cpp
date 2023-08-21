de69d7e68f594fccb1385fa9657eeac3ee3567fd
Kazu Hirata <kazu@google.com>
2023-08-20 22:07:58 -0700

    [Sema] Modernize UsageInfo (NFC)

--- clang/lib/Sema/SemaChecking.cpp
+++ clang/lib/Sema/SemaChecking.cpp
@@ -15906,9 +15906,9 @@ class SequenceChecker : public ConstEvaluatedExprVisitor<SequenceChecker> {
     Usage Uses[UK_Count];
 
     /// Have we issued a diagnostic for this object already?
-    bool Diagnosed;
+    bool Diagnosed = false;
 
-    UsageInfo() : Diagnosed(false) {}
+    UsageInfo() = default;
   };
   using UsageInfoMap = llvm::SmallDenseMap<Object, UsageInfo, 16>;
 
