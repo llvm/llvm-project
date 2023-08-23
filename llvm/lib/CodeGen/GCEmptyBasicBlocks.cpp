80c766a0eaba4ae1ce87130f6883250066258262
Rahman Lavaee <rahmanl@google.com>
2023-08-23 17:44:16 +0000

    Add file header for GCEmptyBasicBlocks.cpp.

--- llvm/lib/CodeGen/GCEmptyBasicBlocks.cpp
+++ llvm/lib/CodeGen/GCEmptyBasicBlocks.cpp
@@ -1,3 +1,16 @@
+//===-- GCEmptyBasicBlocks.cpp ----------------------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file contains the implementation of empty blocks garbage collection
+/// pass.
+///
+//===----------------------------------------------------------------------===//
 #include "llvm/ADT/SmallVector.h"
 #include "llvm/ADT/Statistic.h"
 #include "llvm/CodeGen/MachineBasicBlock.h"
