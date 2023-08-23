03f1eb8c5cde6426c9bb3d2a78f6671487b33288
Ivan Trofimov <i.trofimow@yandex.ru>
2023-08-23 22:37:40 +0300

    + clang-format, fix libcxx.imp

--- libcxx/include/__typeinfo/typeinfo.h
+++ libcxx/include/__typeinfo/typeinfo.h
@@ -294,7 +294,7 @@ public:
 
 } // namespace std
 
-#endif   // defined(_LIBCPP_ABI_VCRUNTIME)
+#endif // defined(_LIBCPP_ABI_VCRUNTIME)
 
 #if defined(_LIBCPP_ABI_VCRUNTIME) && _HAS_EXCEPTIONS == 0
 
