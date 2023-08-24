b6ba804f7775f89f230ee1e62526a2f8225c7966
Jan Svoboda <jan_svoboda@apple.com>
2023-08-24 14:42:00 -0700

    Revert "[clang][modules] Move `UNHASHED_CONTROL_BLOCK` up in the AST file"
    This reverts commit 7d1565727dad3acb54fe76a908630843835d7bc8.

--- clang/lib/Serialization/GlobalModuleIndex.cpp
+++ clang/lib/Serialization/GlobalModuleIndex.cpp
@@ -697,12 +697,9 @@ llvm::Error GlobalModuleIndexBuilder::loadModuleFile(const FileEntry *File) {
     }
 
     // Get Signature.
-    if (State == DiagnosticOptionsBlock && Code == SIGNATURE) {
-      auto Signature = ASTFileSignature::create(Blob.begin(), Blob.end());
-      assert(Signature != ASTFileSignature::createDummy() &&
-             "Dummy AST file signature not backpatched in ASTWriter.");
-      getModuleFileInfo(File).Signature = Signature;
-    }
+    if (State == DiagnosticOptionsBlock && Code == SIGNATURE)
+      getModuleFileInfo(File).Signature = ASTFileSignature::create(
+          Record.begin(), Record.begin() + ASTFileSignature::size);
 
     // We don't care about this record.
   }
