3b4b6cbba5e04fc6bbbba4a034176aa4039a73a6
Cullen Rhodes <cullen.rhodes@arm.com>
2023-08-29 09:03:53 +0000

    [mlir][ArmSME] Add move vector to tile slice op and lowerings
    This adds a 'move_vector_to_tile_slice' op to the ArmSME dialect that
    moves a 1-D scalable vector to a slice of a 2-D tile at a given index.
    
    This is lowered to the 'llvm.aarch64.sme.write.horiz' intrinsic that
    maps to the MOVA (vector to tile, single) SME instruction [1] when
    lowering to LLVM. Like the SME load and store instructions this operates
    on ZA tile slices, which are 1D vectors of horizontally or vertically
    contiguous elements within a ZA tile.
    
    This patch extends the lowering of 'arith.constant' to SME to support
    non-zero constants using this new op.  This requires materializing a
    loop that broadcasts the constant to each tile slice with the
    'vector_to_tile_slice' op. Unlike load and store, this is done during
    conversion from Vector to ArmSME, rather than ArmSME to SCF. The latter
    would require a higher-level custom op in the ArmSME dialect like
    'tile_load' and 'tile_store' and this isn't necessary. We may also
    remove the load and store ops in the future in favour of lowering
    straight from Vector, at which point this would converge.
    
    Currently only horizontal tile slices are supported. A future patch will
    extend this mechanism to support 'vector.broadcast'.
    
    Depends on D156980 D157004
    
    [1] https://developer.arm.com/documentation/ddi0602
    
    Reviewed By: awarzynski, dcaballe
    
    Differential Revision: https://reviews.llvm.org/D157005

--- mlir/lib/Conversion/VectorToArmSME/VectorToArmSME.cpp
+++ mlir/lib/Conversion/VectorToArmSME/VectorToArmSME.cpp
@@ -93,15 +93,67 @@ struct ConstantOpToArmSMELowering : public OpRewritePattern<arith::ConstantOp> {
 
   LogicalResult matchAndRewrite(arith::ConstantOp constantOp,
                                 PatternRewriter &rewriter) const final {
-    auto vType = dyn_cast<VectorType>(constantOp.getType());
-    if (!vType || !arm_sme::isValidSMETileVectorType(vType))
+    auto tileType = dyn_cast<VectorType>(constantOp.getType());
+    if (!tileType || !arm_sme::isValidSMETileVectorType(tileType))
       return failure();
 
     auto denseAttr = dyn_cast<DenseElementsAttr>(constantOp.getValueAttr());
-    if (!denseAttr || !isSplatZero(vType.getElementType(), denseAttr))
+    if (!denseAttr || !denseAttr.isSplat())
       return failure();
 
-    rewriter.replaceOpWithNewOp<arm_sme::ZeroOp>(constantOp, vType);
+    auto tileElementType = tileType.getElementType();
+
+    // Lower 'arith.constant dense<0>' to 'arm_sme.zero' op.
+    if (isSplatZero(tileElementType, denseAttr)) {
+      rewriter.replaceOpWithNewOp<arm_sme::ZeroOp>(constantOp, tileType);
+      return success();
+    }
+
+    // Lower non-zero constants to a loop of 'arm_sme.move_vector_to_tile_slice'
+    // ops that broadcast the constant to each tile slice.
+    OpBuilder::InsertionGuard g(rewriter);
+    auto loc = constantOp.getLoc();
+
+    // Unpack 1-d vector type from 2-d vector type.
+    auto tileSliceType =
+        VectorType::get(tileType.getShape().drop_front(), tileElementType,
+                        /*scalableDims=*/{true});
+    auto denseAttr1D = DenseElementsAttr::get(
+        tileSliceType, denseAttr.getSplatValue<Attribute>());
+    auto constantOp1D = rewriter.create<arith::ConstantOp>(loc, denseAttr1D);
+
+    unsigned tileElementWidth = tileElementType.getIntOrFloatBitWidth();
+
+    // Create 'arm_sme.get_tile' op.
+    auto tileId = rewriter.create<arm_sme::GetTileID>(
+        loc, rewriter.getIntegerType(tileElementWidth));
+
+    // Create `arm_sme.cast_tile_to_vector` to cast tile ID to a vector type to
+    // use as input tile to 'arm_sme.move_vector_to_tile_slice' ops.
+    auto tile =
+        rewriter.create<arm_sme::CastTileToVector>(loc, tileType, tileId);
+
+    auto step = rewriter.create<arith::ConstantIndexOp>(loc, 1);
+    auto minTileSlices = rewriter.create<arith::ConstantIndexOp>(
+        loc, arm_sme::getSMETileSliceMinNumElts(tileElementType));
+    auto vscale =
+        rewriter.create<vector::VectorScaleOp>(loc, rewriter.getIndexType());
+    auto lowerBound = rewriter.create<arith::ConstantIndexOp>(loc, 0);
+    auto numTileSlices =
+        rewriter.create<arith::MulIOp>(loc, minTileSlices, vscale);
+    // Create a loop that broadcasts the constant to each ZA tile slice.
+    auto forOp =
+        rewriter.create<scf::ForOp>(loc, lowerBound, numTileSlices, step);
+    rewriter.setInsertionPointToStart(forOp.getBody());
+    auto tileSliceIndex = forOp.getInductionVar();
+
+    // Create 'arm_sme.move_vector_to_tile_slice' to write vector to tile slice.
+    rewriter.create<arm_sme::MoveVectorToTileSliceOp>(
+        loc, tileType, constantOp1D, tile, tileSliceIndex);
+
+    rewriter.setInsertionPointAfter(forOp);
+
+    rewriter.replaceOp(constantOp, tile);
 
     return success();
   }
