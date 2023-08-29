12a81456d21aa09609de7a72e18610d98b0b19f3
Alex Langford <alangford@apple.com>
2023-08-29 14:11:41 -0700

    [lldb] Add support for recognizing swift ast sections in object files
    In Apple's downstream fork, there is support for understanding the swift
    AST sections in various binaries. Even though the lldb on llvm.org does
    not have support for debugging swift, I think it makes sense to move
    support for recognizing swift ast sections upstream.
    
    Differential Revision: https://reviews.llvm.org/D159142

--- lldb/source/Plugins/ObjectFile/PECOFF/ObjectFilePECOFF.cpp
+++ lldb/source/Plugins/ObjectFile/PECOFF/ObjectFilePECOFF.cpp
@@ -1009,6 +1009,7 @@ SectionType ObjectFilePECOFF::GetSectionType(llvm::StringRef sect_name,
           // .eh_frame can be truncated to 8 chars.
           .Cases(".eh_frame", ".eh_fram", eSectionTypeEHFrame)
           .Case(".gosymtab", eSectionTypeGoSymtab)
+          .Case("swiftast", eSectionTypeSwiftModules)
           .Default(eSectionTypeInvalid);
   if (section_type != eSectionTypeInvalid)
     return section_type;
