47e59272544b1bce2fd4f6e31c3e017b8c7cddb8
Johannes Doerfert <johannes@jdoerfert.de>
2023-08-29 16:05:00 -0700

    [Attributor] Introduce the closed world flag
    The Attributor user can now set the closed world flag
    (`AttributorConfig.IsClosedWorldModule` or
    `-attributor-assume-closed-world`) in order to specialize call edges
    based only on available callees. That means, we assume all functions are
    known and hence all potential callees must be declared/defined in the
    module. We will use this for GPUs and LTO cases, but for now the user
    has to set it via a flag.

--- llvm/lib/Transforms/IPO/AttributorAttributes.cpp
+++ llvm/lib/Transforms/IPO/AttributorAttributes.cpp
@@ -12065,11 +12065,22 @@ struct AAIndirectCallInfoCallSite : public AAIndirectCallInfo {
   /// See AbstractAttribute::initialize(...).
   void initialize(Attributor &A) override {
     auto *MD = getCtxI()->getMetadata(LLVMContext::MD_callees);
-    if (!MD)
+    if (!MD && !A.isClosedWorldModule())
       return;
-    for (const auto &Op : MD->operands())
-      if (Function *Callee = mdconst::dyn_extract_or_null<Function>(Op))
-        PotentialCallees.insert(Callee);
+
+    if (MD) {
+      for (const auto &Op : MD->operands())
+        if (Function *Callee = mdconst::dyn_extract_or_null<Function>(Op))
+          PotentialCallees.insert(Callee);
+    } else if (A.isClosedWorldModule()) {
+      ArrayRef<Function *> IndirectlyCallableFunctions =
+          A.getInfoCache().getIndirectlyCallableFunctions(A);
+      PotentialCallees.insert(IndirectlyCallableFunctions.begin(),
+                              IndirectlyCallableFunctions.end());
+    }
+
+    if (PotentialCallees.empty())
+      indicateOptimisticFixpoint();
   }
 
   ChangeStatus updateImpl(Attributor &A) override {
