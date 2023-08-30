62b004ce886aa5efc6308a3df04e7f9efb0e6941
Christian Sigg <csigg@google.com>
2023-08-30 12:23:25 +0200

    [mlir][nfc] Allow ops to have operands/attributes named `context`.
    This is probably a bad idea, but it's only become a problem with properties and is easy to fix.
    
    Reviewed By: mehdi_amini
    
    Differential Revision: https://reviews.llvm.org/D159185

--- mlir/include/mlir/IR/OperationSupport.h
+++ mlir/include/mlir/IR/OperationSupport.h
@@ -555,7 +555,7 @@ public:
                                              StringRef name) final {
       if constexpr (hasProperties) {
         auto concreteOp = cast<ConcreteOp>(op);
-        return ConcreteOp::getInherentAttr(concreteOp.getContext(),
+        return ConcreteOp::getInherentAttr(concreteOp->getContext(),
                                            concreteOp.getProperties(), name);
       }
       // If the op does not have support for properties, we dispatch back to the
@@ -576,7 +576,7 @@ public:
     void populateInherentAttrs(Operation *op, NamedAttrList &attrs) final {
       if constexpr (hasProperties) {
         auto concreteOp = cast<ConcreteOp>(op);
-        ConcreteOp::populateInherentAttrs(concreteOp.getContext(),
+        ConcreteOp::populateInherentAttrs(concreteOp->getContext(),
                                           concreteOp.getProperties(), attrs);
       }
     }
