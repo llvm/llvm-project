20f4f295da4d3f05d88eb85a8bb83c0e8139ece8
Joseph Huber <jhuber6@vols.utk.edu>
2023-08-30 16:51:45 -0500

    [libc] Fix 'clock()' testing on the GPU
    Summary:
    We should check for the GPU architectures first, since `__linux__` can
    be set potentially during these compilations. Also the test needs to be
    a hermetic test.

--- libc/include/llvm-libc-macros/time-macros.h
+++ libc/include/llvm-libc-macros/time-macros.h
@@ -1,10 +1,10 @@
 #ifndef __LLVM_LIBC_MACROS_TIME_MACROS_H
 #define __LLVM_LIBC_MACROS_TIME_MACROS_H
 
-#ifdef __linux__
-#include "linux/time-macros.h"
-#elif defined(__AMDGPU__) || defined(__NVPTX__)
+#if defined(__AMDGPU__) || defined(__NVPTX__)
 #include "gpu/time-macros.h"
+#elif defined(__linux__)
+#include "linux/time-macros.h"
 #endif
 
 #endif // __LLVM_LIBC_MACROS_TIME_MACROS_H
