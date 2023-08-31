380c5da98efa62920e9fbf3aa9a4fa8add236fb9
Med Ismail Bennani <ismail@bennani.ma>
2023-08-31 16:30:11 +0100

    Revert "Re-land "[lldb/docs] Silence warnings when generating website""
    This reverts 3 commit:
    - f0731d5b61ba798e6d5a63a92d9228010e5a3b50.
    - 8e0a087571a31057bb98939e3ada73227bed83c7.
    - f2f5d6fb8d53bc4bd93a3d4e110134ed017b636f.
    
    This changes were introduced to silence the warnings that are printed
    when generating the lldb module documentation for the website but it
    changed the python bindings and causes test failures on the macos bot:
    
    https://green.lab.llvm.org/green/job/lldb-cmake/59438/
    
    We will have to consider other options to silence these warnings.

--- lldb/docs/conf.py
+++ lldb/docs/conf.py
@@ -170,7 +170,7 @@ html_title = "🐛 LLDB"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ["_static"]
+html_static_path = ["_static"]
 
 html_extra_path = [".htaccess"]
 
