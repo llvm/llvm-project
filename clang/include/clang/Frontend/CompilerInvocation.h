d8df928df8b946b739061dfba9be56311b7731df
Jan Svoboda <jan_svoboda@apple.com>
2023-08-31 14:22:23 -0700

    [clang] NFCI: Change returned AnalyzerOptions smart pointer to reference

--- clang/include/clang/Frontend/CompilerInvocation.h
+++ clang/include/clang/Frontend/CompilerInvocation.h
@@ -129,7 +129,8 @@ public:
     return *PreprocessorOpts;
   }
 
-  AnalyzerOptionsRef getAnalyzerOpts() const { return AnalyzerOpts; }
+  AnalyzerOptions &getAnalyzerOpts() { return *AnalyzerOpts; }
+  const AnalyzerOptions &getAnalyzerOpts() const { return *AnalyzerOpts; }
 };
 
 /// The base class of CompilerInvocation with value semantics.
