34d8e29a0c9c6bd11d8d0ba1433015d8d22f0c4a
zijunzhao <zijunzhao@google.com>
2023-08-31 20:08:32 +0000

    [libc++] Implement ranges::contains
    Differential Revision: https://reviews.llvm.org/D159232

--- libcxx/test/std/algorithms/alg.nonmodifying/alg.contains/ranges.contains.pass.cpp
+++ libcxx/test/std/algorithms/alg.nonmodifying/alg.contains/ranges.contains.pass.cpp
@@ -40,15 +40,17 @@ static_assert(!HasContainsIt<InputIteratorNotIndirectlyReadable>);
 static_assert(!HasContainsIt<InputIteratorNotInputOrOutputIterator>);
 static_assert(!HasContainsIt<cpp20_input_iterator<int*>, SentinelForNotSemiregular>);
 static_assert(!HasContainsIt<cpp20_input_iterator<int*>, InputRangeNotSentinelEqualityComparableWith>);
+static_assert(!HasContainsIt<cpp20_input_iterator<int*>, sentinel_wrapper<cpp20_input_iterator<int*>>>);
 
 static_assert(!HasContainsIt<int*, int>);
 static_assert(!HasContainsIt<int, int*>);
+static_assert(HasContainsIt<int*, int*>);
 
 template <class Range, class ValT>
-concept HasContainsR = requires(Range range) { std::ranges::contains(range, ValT{}); };
+concept HasContainsR = requires(Range&& range) { std::ranges::contains(std::forward<Range>(range), ValT{}); };
 
-static_assert(HasContainsR<std::array<int, 1>, int>);
 static_assert(!HasContainsR<int, int>);
+static_assert(HasContainsR<std::array<int, 1>, int>);
 static_assert(!HasContainsR<std::array<NotEqualityComparable, 1>, NotEqualityComparable>);
 static_assert(!HasContainsR<InputRangeNotDerivedFrom, int>);
 static_assert(!HasContainsR<InputRangeNotIndirectlyReadable, int>);
@@ -63,45 +65,81 @@ template <class Iter, class Sent = Iter>
 constexpr void test_iterators() {
   using ValueT = std::iter_value_t<Iter>;
   {  // simple tests
+    ValueT a[] = {1, 2, 3, 4, 5, 6};
+    auto whole = std::ranges::subrange(Iter(a), Sent(Iter(a + 6)));
     {
-      ValueT a[] = {1, 2, 3, 4, 5, 6};
-      std::same_as<bool> auto ret =
-        std::ranges::contains(Iter(a), Sent(Iter(a + 6)), 3);
+      [[maybe_unused]] std::same_as<bool> decltype(auto) ret =
+        std::ranges::contains(whole.begin(), whole.end(), 3);
       assert(ret);
     }
     {
-      ValueT a[] = {1, 2, 3, 4, 5, 6};
-      auto range = std::ranges::subrange(Iter(a), Sent(Iter(a + 6)));
-      std::same_as<bool> decltype(auto) ret =
-        std::ranges::contains(range, 3);
+      [[maybe_unused]] std::same_as<bool> decltype(auto) ret =
+        std::ranges::contains(whole, 3);
+      assert(ret);
+    }
+  }
+
+  { // check that a range with a single element works
+    ValueT a[] = {32};
+    auto whole = std::ranges::subrange(Iter(a), Sent(Iter(a + 1)));
+    {
+      bool ret = std::ranges::contains(whole.begin(), whole.end(), 32);
+      assert(ret);
+    }
+    {
+      bool ret = std::ranges::contains(whole, 32);
       assert(ret);
     }
   }
 
   { // check that an empty range works
+    ValueT a[] = {};
+    auto whole = std::ranges::subrange(Iter(a), Sent(Iter(a)));
     {
-      ValueT a[] = {};
-      auto ret = std::ranges::contains(Iter(a), Sent(Iter(a)), 1);
+      bool ret = std::ranges::contains(whole.begin(), whole.end(), 1);
       assert(!ret);
     }
     {
-      ValueT a[] = {};
-      auto range = std::ranges::subrange(Iter(a), Sent(Iter(a)));
-      auto ret = std::ranges::contains(range, 1);
+      bool ret = std::ranges::contains(whole, 1);
       assert(!ret);
     }
   }
 
-  { // check that no match
+  { // check that the first element matches
+    ValueT a[] = {32, 3, 2, 1, 0, 23, 21, 9, 40, 100};
+    auto whole = std::ranges::subrange(Iter(a), Sent(Iter(a + 10)));
     {
-      ValueT a[] = {13, 1, 21, 4, 5};
-      auto ret = std::ranges::contains(Iter(a), Sent(Iter(a + 5)), 10);
+      bool ret = std::ranges::contains(whole.begin(), whole.end(), 32);
+      assert(ret);
+    }
+    {
+      bool ret = std::ranges::contains(whole, 32);
+      assert(ret);
+    }
+  }
+
+  { // check that the last element matches
+    ValueT a[] = {3, 22, 1, 43, 99, 0, 56, 100, 32};
+    auto whole = std::ranges::subrange(Iter(a), Sent(Iter(a + 9)));
+    {
+      bool ret = std::ranges::contains(whole.begin(), whole.end(), 32);
+      assert(ret);
+    }
+    {
+      bool ret = std::ranges::contains(whole, 32);
+      assert(ret);
+    }
+  }
+
+  { // no match
+    ValueT a[] = {13, 1, 21, 4, 5};
+    auto whole = std::ranges::subrange(Iter(a), Sent(Iter(a + 5)));
+    {
+      bool ret = std::ranges::contains(whole.begin(), whole.end(), 10);
       assert(!ret);
     }
     {
-      ValueT a[] = {13, 1, 21, 4, 5};
-      auto range = std::ranges::subrange(Iter(a), Sent(Iter(a + 5)));
-      auto ret = std::ranges::contains(range, 10);
+      bool ret = std::ranges::contains(whole, 10);
       assert(!ret);
     }
   }
@@ -115,7 +153,7 @@ class TriviallyComparable {
   ElementT el_;
 
 public:
-  TEST_CONSTEXPR TriviallyComparable(ElementT el) : el_(el) {}
+  constexpr TriviallyComparable(ElementT el) : el_(el) {}
   bool operator==(const TriviallyComparable&) const = default;
 };
 
@@ -152,20 +190,20 @@ constexpr bool test() {
                   });
 
   {
+    int a[] = {1, 9, 0, 13, 25};
+    int projection_count = 0;
     // count invocations of the projection
     {
-      int a[] = {1, 9, 0, 13, 25};
-      int projection_count = 0;
-      auto ret = std::ranges::contains(a, a + 5, 0,
+      projection_count = 0;
+      bool ret = std::ranges::contains(a, a + 5, 0,
                                 [&](int i) { ++projection_count; return i; });
       assert(ret);
       assert(projection_count == 3);
     }
     {
-      int a[] = {1, 9, 0, 13, 25};
-      int projection_count = 0;
+      projection_count = 0;
       auto range = std::ranges::subrange(a, a + 5);
-      auto ret = std::ranges::contains(range, 0, [&](int i) { ++projection_count; return i; });
+      bool ret = std::ranges::contains(range, 0, [&](int i) { ++projection_count; return i; });
       assert(ret);
       assert(projection_count == 3);
     }
