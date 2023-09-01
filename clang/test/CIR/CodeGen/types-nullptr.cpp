f3a5a5f86451567536ce493533fb32750e2cbd4f
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2023-08-31 18:30:44 -0700

    [CIR][CIRGen] Add support for nullptr_t

---
+++ clang/test/CIR/CodeGen/types-nullptr.cpp
@@ -0,0 +1,9 @@
+// RUN: %clang_cc1 -std=c++20 -triple x86_64-unknown-linux-gnu -fclangir -emit-cir %s -o %t.cir
+// RUN: FileCheck --input-file=%t.cir %s
+
+typedef decltype(nullptr) nullptr_t;
+void f() { nullptr_t t = nullptr; }
+
+// CHECK: %0 = cir.alloca !cir.ptr<!void>, cir.ptr <!cir.ptr<!void>>
+// CHECK: %1 = cir.const(#cir.null : !cir.ptr<!void>) : !cir.ptr<!void>
+// CHECK: cir.store %1, %0 : !cir.ptr<!void>, cir.ptr <!cir.ptr<!void>>
