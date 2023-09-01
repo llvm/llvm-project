909954de6fbd76ebdc998af8ece4f53c37a7de8a
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2023-08-31 17:35:20 -0700

    [CIR][CIRGen][NFC] Add more checks for detecting proper vtable linkage
    Add an assert while we don't handle the available externally.

--- clang/lib/CIR/CodeGen/CIRGenItaniumCXXABI.cpp
+++ clang/lib/CIR/CodeGen/CIRGenItaniumCXXABI.cpp
@@ -44,6 +44,56 @@ protected:
     return cast<ItaniumMangleContext>(cir::CIRGenCXXABI::getMangleContext());
   }
 
+  bool isVTableHidden(const CXXRecordDecl *RD) const {
+    const auto &VtableLayout =
+        CGM.getItaniumVTableContext().getVTableLayout(RD);
+
+    for (const auto &VtableComponent : VtableLayout.vtable_components()) {
+      if (VtableComponent.isRTTIKind()) {
+        const CXXRecordDecl *RTTIDecl = VtableComponent.getRTTIDecl();
+        if (RTTIDecl->getVisibility() == Visibility::HiddenVisibility)
+          return true;
+      } else if (VtableComponent.isUsedFunctionPointerKind()) {
+        const CXXMethodDecl *Method = VtableComponent.getFunctionDecl();
+        if (Method->getVisibility() == Visibility::HiddenVisibility &&
+            !Method->isDefined())
+          return true;
+      }
+    }
+    return false;
+  }
+
+  bool hasAnyUnusedVirtualInlineFunction(const CXXRecordDecl *RD) const {
+    const auto &VtableLayout =
+        CGM.getItaniumVTableContext().getVTableLayout(RD);
+
+    for (const auto &VtableComponent : VtableLayout.vtable_components()) {
+      // Skip empty slot.
+      if (!VtableComponent.isUsedFunctionPointerKind())
+        continue;
+
+      const CXXMethodDecl *Method = VtableComponent.getFunctionDecl();
+      if (!Method->getCanonicalDecl()->isInlined())
+        continue;
+
+      StringRef Name = CGM.getMangledName(VtableComponent.getGlobalDecl());
+      auto *op = CGM.getGlobalValue(Name);
+      if (auto globalOp = dyn_cast_or_null<mlir::cir::GlobalOp>(op))
+        llvm_unreachable("NYI");
+
+      if (auto funcOp = dyn_cast_or_null<mlir::cir::FuncOp>(op)) {
+        // This checks if virtual inline function has already been emitted.
+        // Note that it is possible that this inline function would be emitted
+        // after trying to emit vtable speculatively. Because of this we do
+        // an extra pass after emitting all deferred vtables to find and emit
+        // these vtables opportunistically.
+        if (!funcOp || funcOp.isDeclaration())
+          return true;
+      }
+    }
+    return false;
+  }
+
 public:
   CIRGenItaniumCXXABI(CIRGenModule &CGM, bool UseARMMethodPtrABI = false,
                       bool UseARMGuardVarABI = false)
@@ -132,6 +182,7 @@ public:
                                     const CXXRecordDecl *VTableClass) override;
   bool isVirtualOffsetNeededForVTableField(CIRGenFunction &CGF,
                                            CIRGenFunction::VPtr Vptr) override;
+  bool canSpeculativelyEmitVTableAsBaseClass(const CXXRecordDecl *RD) const;
   mlir::Value getVTableAddressPointInStructor(
       CIRGenFunction &CGF, const CXXRecordDecl *VTableClass, BaseSubobject Base,
       const CXXRecordDecl *NearestVBase) override;
@@ -678,9 +729,63 @@ bool CIRGenItaniumCXXABI::isVirtualOffsetNeededForVTableField(
   return NeedsVTTParameter(CGF.CurGD);
 }
 
+bool CIRGenItaniumCXXABI::canSpeculativelyEmitVTableAsBaseClass(
+    const CXXRecordDecl *RD) const {
+  // We don't emit available_externally vtables if we are in -fapple-kext mode
+  // because kext mode does not permit devirtualization.
+  if (CGM.getLangOpts().AppleKext)
+    return false;
+
+  // If the vtable is hidden then it is not safe to emit an available_externally
+  // copy of vtable.
+  if (isVTableHidden(RD))
+    return false;
+
+  if (CGM.getCodeGenOpts().ForceEmitVTables)
+    return true;
+
+  // If we don't have any not emitted inline virtual function then we are safe
+  // to emit an available_externally copy of vtable.
+  // FIXME we can still emit a copy of the vtable if we
+  // can emit definition of the inline functions.
+  if (hasAnyUnusedVirtualInlineFunction(RD))
+    return false;
+
+  // For a class with virtual bases, we must also be able to speculatively
+  // emit the VTT, because CodeGen doesn't have separate notions of "can emit
+  // the vtable" and "can emit the VTT". For a base subobject, this means we
+  // need to be able to emit non-virtual base vtables.
+  if (RD->getNumVBases()) {
+    for (const auto &B : RD->bases()) {
+      auto *BRD = B.getType()->getAsCXXRecordDecl();
+      assert(BRD && "no class for base specifier");
+      if (B.isVirtual() || !BRD->isDynamicClass())
+        continue;
+      if (!canSpeculativelyEmitVTableAsBaseClass(BRD))
+        return false;
+    }
+  }
+
+  return true;
+}
+
 bool CIRGenItaniumCXXABI::canSpeculativelyEmitVTable(
-    [[maybe_unused]] const CXXRecordDecl *RD) const {
-  llvm_unreachable("NYI");
+    const CXXRecordDecl *RD) const {
+  if (!canSpeculativelyEmitVTableAsBaseClass(RD))
+    return false;
+
+  // For a complete-object vtable (or more specifically, for the VTT), we need
+  // to be able to speculatively emit the vtables of all dynamic virtual bases.
+  for (const auto &B : RD->vbases()) {
+    auto *BRD = B.getType()->getAsCXXRecordDecl();
+    assert(BRD && "no class for base specifier");
+    if (!BRD->isDynamicClass())
+      continue;
+    if (!canSpeculativelyEmitVTableAsBaseClass(BRD))
+      return false;
+  }
+
+  return true;
 }
 
 namespace {
