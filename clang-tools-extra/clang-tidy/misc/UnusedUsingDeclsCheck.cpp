d752aa2cd813852136f2a85898ebf2ece5d2a821
Piotr Zegar <me@piotrzegar.pl>
2023-09-03 06:00:40 +0000

    [clang-tidy][NFC] Unwind if-else-if-... in UnusedUsingDeclsCheck
    Break a if-else-if-else... chain. Use early return instead.
    
    Fixes: #46375

--- clang-tools-extra/clang-tidy/misc/UnusedUsingDeclsCheck.cpp
+++ clang-tools-extra/clang-tidy/misc/UnusedUsingDeclsCheck.cpp
@@ -126,10 +126,14 @@ void UnusedUsingDeclsCheck::check(const MatchFinder::MatchResult &Result) {
     // Also remove variants of Used.
     if (const auto *FD = dyn_cast<FunctionDecl>(Used)) {
       removeFromFoundDecls(FD->getPrimaryTemplate());
-    } else if (const auto *Specialization =
-                   dyn_cast<ClassTemplateSpecializationDecl>(Used)) {
+      return;
+    }
+    if (const auto *Specialization =
+            dyn_cast<ClassTemplateSpecializationDecl>(Used)) {
       removeFromFoundDecls(Specialization->getSpecializedTemplate());
-    } else if (const auto *ECD = dyn_cast<EnumConstantDecl>(Used)) {
+      return;
+    }
+    if (const auto *ECD = dyn_cast<EnumConstantDecl>(Used)) {
       if (const auto *ET = ECD->getType()->getAs<EnumType>())
         removeFromFoundDecls(ET->getDecl());
     }
@@ -151,10 +155,16 @@ void UnusedUsingDeclsCheck::check(const MatchFinder::MatchResult &Result) {
     if (Used->getKind() == TemplateArgument::Template) {
       if (const auto *TD = Used->getAsTemplate().getAsTemplateDecl())
         removeFromFoundDecls(TD);
-    } else if (Used->getKind() == TemplateArgument::Type) {
+      return;
+    }
+
+    if (Used->getKind() == TemplateArgument::Type) {
       if (auto *RD = Used->getAsType()->getAsCXXRecordDecl())
         removeFromFoundDecls(RD);
-    } else if (Used->getKind() == TemplateArgument::Declaration) {
+      return;
+    }
+
+    if (Used->getKind() == TemplateArgument::Declaration) {
       RemoveNamedDecl(Used->getAsDecl());
     }
     return;
