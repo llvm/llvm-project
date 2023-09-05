007f8cd3e48acba02fb8597ecb2018ac0b57a5f7
Stephen Tozer <Stephen.Tozer@Sony.com>
2023-09-05 16:04:53 +0100

    Re-reapply "[Dexter] Remove builder from Dexter"
    This patch makes a further attempt to fix the tests broken by the
    previous revision by ensuring that the command line for the modified
    Dexter tests use -std=gnu++11, in keeping with the old build script.
    
    This reverts commit 5647f2908de90fe07b0805e988cd2e91a1751928.

--- cross-project-tests/debuginfo-tests/dexter/feature_tests/commands/perfect/limit_steps/limit_steps_overlapping_ranges.cpp
+++ cross-project-tests/debuginfo-tests/dexter/feature_tests/commands/perfect/limit_steps/limit_steps_overlapping_ranges.cpp
@@ -1,7 +1,8 @@
 // Purpose:
 //      Ensure that multiple overlapping \DexLimitSteps ranges do not interfere.
 //
-// RUN: %dexter_regression_test -- %s | FileCheck %s
+// RUN: %dexter_regression_test_build %s -o %t
+// RUN: %dexter_regression_test_run --binary %t -- %s | FileCheck %s
 // CHECK: limit_steps_overlapping_ranges.cpp
 
 int main() {
