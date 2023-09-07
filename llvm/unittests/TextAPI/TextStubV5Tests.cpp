9d04a6df22bd6c63f73c795d9c16998600bee75a
Cyndy Ishida <cyndy_ishida@apple.com>
2023-09-07 10:26:31 -0700

    [TextAPI] Support more constructors for PackedVersions
    TBD files now record minimum deplyoment versions and tapi interfaces
    with apple system linker by a packed version encoding. Support mapping
    between that and `VersionTuple`s.

--- llvm/unittests/TextAPI/TextStubV5Tests.cpp
+++ llvm/unittests/TextAPI/TextStubV5Tests.cpp
@@ -197,8 +197,18 @@ TEST(TBDv5, ReadFile) {
       Target(AK_arm64, PLATFORM_MACOS, VersionTuple(11, 0, 0)),
       Target(AK_arm64, PLATFORM_MACCATALYST, VersionTuple(14, 0)),
   };
+  std::set<Target> FileTargets{File->targets().begin(), File->targets().end()};
   EXPECT_EQ(mapToPlatformSet(AllTargets), File->getPlatforms());
   EXPECT_EQ(mapToArchitectureSet(AllTargets), File->getArchitectures());
+  EXPECT_EQ(FileTargets.size(), AllTargets.size());
+  for (const auto &Targ : AllTargets) {
+    auto FileTarg = FileTargets.find(Targ);
+    EXPECT_FALSE(FileTarg == FileTargets.end());
+    EXPECT_EQ(*FileTarg, Targ);
+    PackedVersion MD = Targ.MinDeployment;
+    PackedVersion FileMD = FileTarg->MinDeployment;
+    EXPECT_EQ(MD, FileMD);
+  }
 
   EXPECT_EQ(PackedVersion(1, 2, 0), File->getCurrentVersion());
   EXPECT_EQ(PackedVersion(1, 1, 0), File->getCompatibilityVersion());
