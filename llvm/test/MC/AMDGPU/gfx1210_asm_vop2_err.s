2563bfc44c2fe17b923613ecf3973fe753582298
Stanislav Mekhanoshin <Stanislav.Mekhanoshin@amd.com>
2023-09-08 10:22:58 -0700

    MI-400: Remove V_MUL_I64 and clamp support of V_MUL_U64
    This is the recent change in the RTL.
    
    Change-Id: Ia6bc96e866067a6992381ba57f1e020aa2988045

--- llvm/test/MC/AMDGPU/gfx1210_asm_vop2_err.s
+++ llvm/test/MC/AMDGPU/gfx1210_asm_vop2_err.s
@@ -105,7 +105,7 @@ v_mul_u64 v[2:3], v[4:5], v[6:7] row_share:1
 // GFX1210-ERR-NEXT:{{^}}v_mul_u64 v[2:3], v[4:5], v[6:7] row_share:1
 // GFX1210-ERR-NEXT:{{^}}                                 ^
 
-v_mul_i64 v[2:3], v[4:5], v[6:7] row_share:1
-// GFX1210-ERR: :[[@LINE-1]]:{{[0-9]+}}: error: not a valid operand.
-// GFX1210-ERR-NEXT:{{^}}v_mul_i64 v[2:3], v[4:5], v[6:7] row_share:1
+v_mul_u64 v[4:5], v[2:3], v[8:9] clamp
+// GFX1210-ERR: :[[@LINE-1]]:{{[0-9]+}}: error: invalid operand for instruction
+// GFX1210-ERR-NEXT:{{^}}v_mul_u64 v[4:5], v[2:3], v[8:9] clamp
 // GFX1210-ERR-NEXT:{{^}}                                 ^
