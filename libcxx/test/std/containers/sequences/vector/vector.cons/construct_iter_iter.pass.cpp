c2f2ddedabfc4a24b1a603cc259cd35789cf471f
Louis Dionne <ldionne.2@gmail.com>
2023-09-08 14:34:46 -0400

    [libc++] Add regression tests for issue #46841

--- libcxx/test/std/containers/sequences/vector/vector.cons/construct_iter_iter.pass.cpp
+++ libcxx/test/std/containers/sequences/vector/vector.cons/construct_iter_iter.pass.cpp
@@ -89,6 +89,12 @@ TEST_CONSTEXPR_CXX20 void basic_test_cases() {
   test<std::vector<int, safe_allocator<int> > >(
       random_access_iterator<const int*>(a), random_access_iterator<const int*>(an));
 #endif
+
+  // Regression test for https://github.com/llvm/llvm-project/issues/46841
+  {
+    std::vector<int> v1({}, forward_iterator<const int*>{});
+    std::vector<int> v2(forward_iterator<const int*>{}, {});
+  }
 }
 
 TEST_CONSTEXPR_CXX20 void emplaceable_concept_tests() {
