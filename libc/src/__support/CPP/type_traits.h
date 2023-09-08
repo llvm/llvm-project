eebf8faf3e1eeba945c873dba5b964619976e9ae
Guillaume Chatelet <gchatelet@google.com>
2023-09-08 09:36:19 +0000

    [libc] Add is_member_pointer_v (#65631)
    Implementation from
    https://en.cppreference.com/w/cpp/types/is_member_pointer

--- libc/src/__support/CPP/type_traits.h
+++ libc/src/__support/CPP/type_traits.h
@@ -30,6 +30,7 @@
 #include "src/__support/CPP/type_traits/is_function.h"
 #include "src/__support/CPP/type_traits/is_integral.h"
 #include "src/__support/CPP/type_traits/is_lvalue_reference.h"
+#include "src/__support/CPP/type_traits/is_member_pointer.h"
 #include "src/__support/CPP/type_traits/is_null_pointer.h"
 #include "src/__support/CPP/type_traits/is_pointer.h"
 #include "src/__support/CPP/type_traits/is_reference.h"
