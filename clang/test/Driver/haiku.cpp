c184f27f55ab767e8c7bec589b24daeb4177c093
Brad Smith <brad@comstyle.com>
2023-09-09 00:13:26 -0400

    Fixes for Haiku tests on Win32

--- clang/test/Driver/haiku.cpp
+++ clang/test/Driver/haiku.cpp
@@ -1,11 +1,11 @@
 // Check the C++ header path (libstdc++)
-// RUN: %clang++ --target=x86_64-unknown-haiku -### %s 2>&1 \
+// RUN: %clangxx --target=x86_64-unknown-haiku -### %s 2>&1 \
 // RUN:   --sysroot=%S/Inputs/haiku_x86_64_tree \
 // RUN:   | FileCheck --check-prefix=CHECK-LIBSTDCXX-HEADER-PATH %s
 // CHECK-LIBSTDCXX-HEADER-PATH: "-internal-isystem" "[[SYSROOT:[^"]+]]/boot/system/develop/headers/c++"
 
 // Check the C++ header path (when using libc++)
-// RUN: %clang++ --target=x86_64-unknown-haiku --stdlib=libc++ -### %s 2>&1 \
+// RUN: %clangxx --target=x86_64-unknown-haiku --stdlib=libc++ -### %s 2>&1 \
 // RUN:   --sysroot=%S/Inputs/haiku_x86_64_tree \
 // RUN:   | FileCheck --check-prefix=CHECK-LIBCXX-HEADER-PATH %s
 // CHECK-LIBCXX-HEADER-PATH: "-internal-isystem" "[[SYSROOT:[^"]+]]/boot/system/develop/headers/c++/v1"
