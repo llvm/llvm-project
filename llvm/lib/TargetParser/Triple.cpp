59204c592daf3b3cb7d5d5a01915d557cd713ba7
Nico Weber <thakis@chromium.org>
2023-09-11 13:56:53 -0700

    Revert "[Driver] Properly report error for unsupported powerpc darwin/macos triples"
    This reverts commit d6c12798786cf49bbf97710beb6d8cfb4897febe.
    
    The change unintentionally changed lots of codegen, see
    https://github.com/llvm/llvm-project/issues/47698#issuecomment-1714548640
    
    Also revert a follow-up:
    This reverts commit 2ecccdbd51ea343b46e2a8651bc68788c2462ab0.

--- llvm/lib/TargetParser/Triple.cpp
+++ llvm/lib/TargetParser/Triple.cpp
@@ -787,8 +787,6 @@ static Triple::SubArchType parseSubArch(StringRef SubArchName) {
 }
 
 static Triple::ObjectFormatType getDefaultFormat(const Triple &T) {
-  if (T.isOSDarwin())
-    return Triple::MachO;
   switch (T.getArch()) {
   case Triple::UnknownArch:
   case Triple::aarch64:
@@ -797,7 +795,9 @@ static Triple::ObjectFormatType getDefaultFormat(const Triple &T) {
   case Triple::thumb:
   case Triple::x86:
   case Triple::x86_64:
-    if (T.isOSWindows())
+    if (T.isOSDarwin())
+      return Triple::MachO;
+    else if (T.isOSWindows())
       return Triple::COFF;
     return Triple::ELF;
 
