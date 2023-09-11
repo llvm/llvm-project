7c86dfea3ff46045662dbde4e71ccaab1ad816a0
Tom Stellard <tstellar@redhat.com>
2023-09-11 16:40:58 -0700

    Fix formatting

--- llvm/utils/git/github-automation.py
+++ llvm/utils/git/github-automation.py
@@ -82,7 +82,9 @@ class PRSubscriber:
         self.repo = github.Github(token).get_repo(repo)
         self.org = github.Github(token).get_organization(self.repo.organization.login)
         self.pr = self.repo.get_issue(pr_number).as_pull_request()
-        self._team_name = "pr-subscribers-{}".format(label_name.replace('+', 'x')).lower()
+        self._team_name = "pr-subscribers-{}".format(
+            label_name.replace('+', 'x')
+        ).lower()
 
     def run(self) -> bool:
         patch = None
