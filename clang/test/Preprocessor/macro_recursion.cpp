a28be970bf5c440bbecf7146d27e51dca42ba4cd
Kelbon Nik <kelbonage@gmail.com>
2023-09-11 16:10:20 +0400

    whitespaces in tests

--- clang/test/Preprocessor/macro_recursion.cpp
+++ clang/test/Preprocessor/macro_recursion.cpp
@@ -2,7 +2,7 @@
 
 #define merge_all_expand2(a, b) a ## b
 #define merge_all_expand(a, b) merge_all_expand2(a, b)
-#define2 concat_all(head, ...) merge_all_expand(head, __VA_OPT__(concat_all(__VA_ARGS__)))
+#define2 concat_all(head, ...)merge_all_expand(head,__VA_OPT__(concat_all(__VA_ARGS__)))
 0: concat_all(aa, bb, cc)
 1: [concat_all()]
 // CHECK: 0: aabbcc
@@ -12,17 +12,17 @@
 #undef merge_all_expand2
 #undef concat_all
 
-#define2 reverse(head, ...) __VA_OPT__(reverse(__VA_ARGS__) , ) head
+#define2 reverse(head, ...)__VA_OPT__(reverse(__VA_ARGS__),)head
 
 2: reverse(1,2,3)
 
-// CHECK: 2: 3 , 2 , 1
+// CHECK: 2: 3,2,1
 
 #undef reverse
 
-#define2 fold_left(op, head, ...) ( __VA_OPT__(fold_left(op, __VA_ARGS__) op) head )
+#define2 fold_left(op, head, ...)(__VA_OPT__(fold_left(op,__VA_ARGS__)op)head)
 
 3: fold_left(+, 1, 2, 3, 4)
-// CHECK: 3: ((((4) + 3) + 2) + 1)
+// CHECK: 3: ((((4)+3)+2)+1)
 
 #undef fold_left
