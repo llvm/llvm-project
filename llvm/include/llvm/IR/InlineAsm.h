4590de5220df92a5e31e2e0c338b4c0c699d5eea
Nick Desaulniers <ndesaulniers@google.com>
2023-09-11 11:24:22 -0700

    [InlineAsm] fix msvc warning
    Link: https://github.com/llvm/llvm-project/pull/65649#issuecomment-1714309236

--- llvm/include/llvm/IR/InlineAsm.h
+++ llvm/include/llvm/IR/InlineAsm.h
@@ -344,6 +344,7 @@ public:
       case Kind::Func:
         return "mem";
       }
+      llvm_unreachable("impossible kind");
     }
 
     /// getNumOperandRegisters - Extract the number of registers field from the
