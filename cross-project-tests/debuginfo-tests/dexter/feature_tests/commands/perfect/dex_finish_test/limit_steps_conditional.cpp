28a590e61787e10cea25734407aa415aae6bb449
Stephen Tozer <Stephen.Tozer@Sony.com>
2023-09-11 18:49:28 +0100

    [Dexter] Replace clang with clang++ in various cross project tests
    This patch attempts to fixup a set of tests in the cross-project-tests
    directory by replacing invocations of clang with clang++ for a set of c++
    files.
    
    As a small additional change, this patch removes -lstdc++ from a test that
    did not appear to require it.

--- cross-project-tests/debuginfo-tests/dexter/feature_tests/commands/perfect/dex_finish_test/limit_steps_conditional.cpp
+++ cross-project-tests/debuginfo-tests/dexter/feature_tests/commands/perfect/dex_finish_test/limit_steps_conditional.cpp
@@ -7,7 +7,7 @@
 // The dbgeng driver doesn't support \DexLimitSteps yet.
 // UNSUPPORTED: system-windows
 //
-// RUN: %dexter_regression_test_build %s -o %t
+// RUN: %dexter_regression_test_cxx_build %s -o %t
 // RUN: %dexter_regression_test_run --binary %t -- %s | FileCheck %s
 // CHECK: limit_steps_conditional.cpp
 
