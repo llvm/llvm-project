a08344118d7a07067e8f39ac5441e4b99a44a220
Cassie Jones <cassie_jones@apple.com>
2023-09-11 15:29:51 -0700

    fixup! [include-mapping] Python fixes

--- clang/tools/include-mapping/cppreference_parser.py
+++ clang/tools/include-mapping/cppreference_parser.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # ===- cppreference_parser.py -  ------------------------------*- python -*--===#
 #
 # Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
