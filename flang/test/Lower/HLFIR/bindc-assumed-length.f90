4b1846f99354c4488a3446cd508c4cc02cc73140
Jean Perier <jperier@nvidia.com>
2023-09-11 09:26:00 -0700

    Fix bug from patch on the callee side
    Lowering hit and assert with the previous patch because it is not meant
    to manipulate scalars characters as fir.box.
    
    Add the code to open the fir.box of BIND(C) characters on the callee
    side, taking care of dealing with the case where the fir.box is optional
    and cannot be addressed unconditionally.

--- flang/test/Lower/HLFIR/bindc-assumed-length.f90
+++ flang/test/Lower/HLFIR/bindc-assumed-length.f90
@@ -1,18 +1,41 @@
 ! Test that assumed length character scalars and explicit shape arrays are passed via
 ! CFI descriptor (fir.box) in BIND(C) procedures. They are passed only by address
 ! and length  in non BIND(C) procedures. See Fortran 2018 standard 18.3.6 point 2(5).
-! RUN: bbc -emit-hlfir -o - %s 2>&1 | FileCheck %s
+! RUN: bbc -hlfir -emit-fir -o - %s 2>&1 | FileCheck %s
 
-! CHECK: func.func @foo(
+module bindcchar
+contains
+! CHECK-LABEL: func.func @bindc(
 ! CHECK-SAME: %{{[^:]*}}: !fir.box<!fir.char<1,?>>
 ! CHECK-SAME: %{{[^:]*}}: !fir.box<!fir.array<100x!fir.char<1,?>>>
-subroutine foo(c1, c3) bind(c)
-  character(*) :: c1,  c3(100)
+subroutine bindc(c1, c3) bind(c)
+  character(*) ::  c1, c3(100)
+ print *, c1(1:3), c3(5)(1:3)
 end subroutine
 
-! CHECK: func.func @_QPnot_bindc(
+! CHECK-LABEL:  func.func @bindc_optional(
+! CHECK-SAME: %{{[^:]*}}: !fir.box<!fir.char<1,?>>
+! CHECK-SAME: %{{[^:]*}}: !fir.box<!fir.array<100x!fir.char<1,?>>>
+subroutine bindc_optional(c1, c3) bind(c)
+  character(*), optional ::  c1, c3(100)
+ print *, c1(1:3), c3(5)(1:3)
+end subroutine
+
+! CHECK-LABEL:  func.func @_QMbindccharPnot_bindc(
 ! CHECK-SAME: %{{[^:]*}}: !fir.boxchar<1>
 ! CHECK-SAME: %{{[^:]*}}: !fir.boxchar<1>
 subroutine not_bindc(c1, c3)
   character(*) :: c1,  c3(100)
+  call bindc(c1, c3)
+  call bindc_optional(c1, c3)
 end subroutine
+
+! CHECK-LABEL:  func.func @_QMbindccharPnot_bindc_optional(
+! CHECK-SAME: %{{[^:]*}}: !fir.boxchar<1>
+! CHECK-SAME: %{{[^:]*}}: !fir.boxchar<1>
+subroutine not_bindc_optional(c1, c3)
+  character(*), optional :: c1,  c3(100)
+  call bindc(c1, c3)
+  call bindc_optional(c1, c3)
+end subroutine
+end module
