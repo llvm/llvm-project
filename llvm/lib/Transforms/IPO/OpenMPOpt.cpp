65a5f9d13d2d60c8b9d4b0486b2aa25a19666e38
Dominik Adamski <dominik.adamski@amd.com>
2023-09-11 05:31:37 -0400

    [OpenMPIRBuilder]Add support to omp target parallel
    Added support for LLVM IR code generation which is used for handling
    omp target parallel code. The call for __kmpc_parallel_51 is generated
    and the parallel region is outlined to separate function.
    
    The proper setup of kmpc_target_init mode is not included in the commit.
    It is assumed that the SPMD mode for target init is properly set by other
    codegen functions.

--- llvm/lib/Transforms/IPO/OpenMPOpt.cpp
+++ llvm/lib/Transforms/IPO/OpenMPOpt.cpp
@@ -286,6 +286,7 @@ struct OMPInformationCache : public InformationCache {
       : InformationCache(M, AG, Allocator, CGSCC), OMPBuilder(M),
         OpenMPPostLink(OpenMPPostLink) {
 
+    OMPBuilder.Config.IsTargetDevice = isOpenMPDevice(OMPBuilder.M);
     OMPBuilder.initialize();
     initializeRuntimeFunctions(M);
     initializeInternalControlVars();
