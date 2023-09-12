c13a5dc6d0509e329c8735c26361cd3abfdde5a1
Tom Eccles <tom.eccles@arm.com>
2023-09-12 15:40:30 +0000

    [flang][FIR] add FirAliasAnalysisOpInterface
    This interface allows (HL)FIR passes to add TBAA information to fir.load
    and fir.store. If present, these TBAA tags take precedence over those
    added during CodeGen.
    
    We can't reuse mlir::LLVMIR::AliasAnalysisOpInterface because that uses
    the mlir::LLVMIR namespace so it tries to define methods for fir
    operations in the wrong namespace. But I did re-use the tbaa tag type to
    minimise boilerplate code.
    
    The new builders are to preserve the old interface without the tbaa tag.

---
+++ flang/include/flang/Optimizer/Dialect/FirAliasAnalysisOpInterface.h
@@ -0,0 +1,27 @@
+//===- FirAliasAnalysisInterface.h ------------------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file contains an interface for adding alias analysis information to
+// loads and stores
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef FORTRAN_OPTIMIZER_DIALECT_FIR_ALIAS_ANALYSIS_INTERFACE_H
+#define FORTRAN_OPTIMIZER_DIALECT_FIR_ALIAS_ANALYSIS_INTERFACE_H
+
+#include "mlir/IR/OpDefinition.h"
+#include "mlir/IR/Operation.h"
+#include "mlir/Support/LogicalResult.h"
+
+namespace fir::detail {
+mlir::LogicalResult verifyFirAliasAnalysisOpInterface(mlir::Operation *op);
+} // namespace fir::detail
+
+#include "flang/Optimizer/Dialect/FirAliasAnalaysOpInterface.h.inc"
+
+#endif // FORTRAN_OPTIMIZER_DIALECT_FIR_ALIAS_ANALYSIS_INTERFACE_H
