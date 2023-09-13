4bc9cedce41ab06e4a4a78bceaa83c40dd0ce3ab
Martin Erhart <merhart@google.com>
2023-09-13 13:38:51 +0000

    Revert "[mlir][bufferization] Improve buffer deallocation pass"
    This reverts commit 1bebb60a7565e5197d23120528f544b886b4d905.
    
    This caused problems in downstream projects. We are reverting to give
    them more time for integration.

--- mlir/include/mlir/Dialect/Bufferization/Transforms/Passes.h
+++ mlir/include/mlir/Dialect/Bufferization/Transforms/Passes.h
@@ -4,7 +4,6 @@
 #include "mlir/Pass/Pass.h"
 
 namespace mlir {
-class FunctionOpInterface;
 class ModuleOp;
 class RewritePatternSet;
 class OpBuilder;
@@ -126,8 +125,7 @@ func::FuncOp buildDeallocationLibraryFunction(OpBuilder &builder, Location loc,
                                               SymbolTable &symbolTable);
 
 /// Run buffer deallocation.
-LogicalResult deallocateBuffers(FunctionOpInterface op,
-                                bool privateFuncDynamicOwnership);
+LogicalResult deallocateBuffers(Operation *op);
 
 /// Creates a pass that moves allocations upwards to reduce the number of
 /// required copies that are inserted during the BufferDeallocation pass.
