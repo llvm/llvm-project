3070db62d3c5c5f1c3f7e9165c6486b668f3364f
Eymen Ünay <eymenunay@outlook.com>
2023-09-13 16:35:36 +0200

    [JITLink][AArch32] Implement ELF::R_ARM_CALL relocation
    - Added WritableArmRelocation and ArmRelocation Structs
    - Encode/Decode funcs for B/BL A1 and BLX A2 encodings
    - Add ARM helper functions, consistent with the existing Thumb helper functions
    - Add Test for ELF::R_ARM_CALL
    
    Reviewed By: sgraenitz
    
    Differential Revision: https://reviews.llvm.org/D157533

---
+++ llvm/test/ExecutionEngine/JITLink/AArch32/ELF_static_arm_reloc.s
@@ -0,0 +1,39 @@
+# RUN: llvm-mc -triple=armv7-linux-gnueabi -arm-add-build-attributes -filetype=obj -o %t.o %s
+# RUN: llvm-objdump -r %t.o | FileCheck --check-prefix=CHECK-TYPE %s
+# RUN: llvm-objdump --disassemble %t.o | FileCheck --check-prefix=CHECK-INSTR %s
+# RUN: llvm-jitlink -noexec -slab-address 0x76ff0000 -slab-allocate 10Kb \
+# RUN:              -slab-page-size 4096 -show-entry-es -check %s %t.o
+
+
+	.text
+	.syntax unified
+
+# CHECK-TYPE: {{[0-9a-f]+}} R_ARM_CALL call_target
+# CHECK-INSTR: 	00000000 <call_site>:
+# CHECK-INSTR: 	       0: ebfffffe     bl      0x0 <call_site>
+# CHECK-INSTR: 	00000004 <call_target>:
+# CHECK-INSTR: 	       4: e12fff1e     bx      lr
+# ARM branch offset is 8, because it accounts for an additional prefetch
+# instruction that increments PC even though it is implicit
+# jitlink-check: decode_operand(call_site, 0) = call_target - (call_site + 8)
+	.globl	call_site
+	.type	call_site,%function
+	.p2align	2
+call_site:
+	bl	call_target
+	.size	call_site,	.-call_site
+
+	.globl	call_target
+	.type	call_target,%function
+	.p2align	2
+call_target:
+	bx	lr
+	.size	call_target,	.-call_target
+
+# Empty main function for jitlink to be happy
+	.globl	main
+	.type	main,%function
+	.p2align	2
+main:
+	bx	lr
+	.size	main,	.-main
