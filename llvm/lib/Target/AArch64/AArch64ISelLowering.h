2ca4d136124d151216aac77a0403dcb5c5835bcd
Nick Desaulniers <nickdesaulniers@users.noreply.github.com>
2023-09-13 08:48:09 -0700

    [InlineAsm] wrap ConstraintCode in enum class NFC (#66003)
    Similar to
    commit 2fad6e69851e ("[InlineAsm] wrap Kind in enum class NFC")
    
    Fix the TODOs added in
    commit 93bd428742f9 ("[InlineAsm] refactor InlineAsm class NFC
    (#65649)")

--- llvm/lib/Target/AArch64/AArch64ISelLowering.h
+++ llvm/lib/Target/AArch64/AArch64ISelLowering.h
@@ -1169,9 +1169,10 @@ private:
                                     std::vector<SDValue> &Ops,
                                     SelectionDAG &DAG) const override;
 
-  unsigned getInlineAsmMemConstraint(StringRef ConstraintCode) const override {
+  InlineAsm::ConstraintCode
+  getInlineAsmMemConstraint(StringRef ConstraintCode) const override {
     if (ConstraintCode == "Q")
-      return InlineAsm::Constraint_Q;
+      return InlineAsm::ConstraintCode::Q;
     // FIXME: clang has code for 'Ump', 'Utf', 'Usa', and 'Ush' but these are
     //        followed by llvm_unreachable so we'll leave them unimplemented in
     //        the backend for now.
