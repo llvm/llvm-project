9bcac5f17cc15e1106f701980e4d63a21d5293b9
Valentin Clement <clementval@gmail.com>
2023-09-13 14:35:01 -0700

    [flang][openacc] Support labeled DO loop after acc loop directive

--- flang/test/Semantics/OpenACC/acc-loop-validity.f90
+++ flang/test/Semantics/OpenACC/acc-loop-validity.f90
@@ -4,12 +4,16 @@ program openacc_clause_validity
 
   implicit none
 
-  integer :: i
+  integer :: i, n
 
   i = 0
 
+  !ERROR: A DO loop must follow the LOOP directive
   !$acc loop
-  !ERROR: A DO loop must follow the loop construct
   i = 1
+
+  !$acc loop
+  do 100 i=0, n
+  100 continue
 
 end
