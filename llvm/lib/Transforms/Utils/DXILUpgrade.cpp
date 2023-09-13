1f3d70a95a98ee77ff31c37f3587dba52fa5f250
Justin Bogner <mail@justinbogner.com>
2023-09-13 12:35:33 -0700

    [Transforms][DXIL] Basic debug output in dxil-upgrade. NFC

--- llvm/lib/Transforms/Utils/DXILUpgrade.cpp
+++ llvm/lib/Transforms/Utils/DXILUpgrade.cpp
@@ -7,14 +7,26 @@
 //===----------------------------------------------------------------------===//
 
 #include "llvm/Transforms/Utils/DXILUpgrade.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/Metadata.h"
+#include "llvm/IR/Module.h"
+#include "llvm/Support/Debug.h"
 
 using namespace llvm;
+
+#define DEBUG_TYPE "dxil-upgrade"
 
 static bool handleValVerMetadata(Module &M) {
   NamedMDNode *ValVer = M.getNamedMetadata("dx.valver");
   if (!ValVer)
     return false;
 
+  LLVM_DEBUG({
+    MDNode *N = ValVer->getOperand(0);
+    auto X = mdconst::extract<ConstantInt>(N->getOperand(0))->getZExtValue();
+    auto Y = mdconst::extract<ConstantInt>(N->getOperand(1))->getZExtValue();
+    dbgs() << "DXIL: validation version: " << X << "." << Y << "\n";
+  });
   // We don't need the validation version internally, so we drop it.
   ValVer->dropAllReferences();
   ValVer->eraseFromParent();
