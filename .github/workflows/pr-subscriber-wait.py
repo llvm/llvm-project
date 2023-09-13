ec8bcec0ea66288d48fbf230094eabb6b0d20d9f
Tom Stellard <tstellar@redhat.com>
2023-09-13 12:43:25 -0700

    Fix code

--- .github/workflows/pr-subscriber-wait.py
+++ .github/workflows/pr-subscriber-wait.py
@@ -3,13 +3,13 @@ import os
 import sys
 import time
 
-def needs_to_wait(repo):
 
+def needs_to_wait(repo):
     workflow_name = os.environ.get("GITHUB_WORKFLOW")
     run_number = os.environ.get("GITHUB_RUN_NUMBER")
     print("Workflow Name:", workflow_name, "Run Number:", run_number)
     for status in ["in_progress", "queued"]:
-        for workflow in repo.get_workflow_runs(status = status):
+        for workflow in repo.get_workflow_runs(status=status):
             print("Looking at ", workflow.name, "#", workflow.run_number)
             if workflow.name != workflow_name:
                 continue
@@ -17,6 +17,7 @@ def needs_to_wait(repo):
                 print("Workflow {} still {} ".format(workflow.run_number, status))
                 return True
     return False
+
 
 repo_name = os.environ.get("GITHUB_REPOSITORY")
 token = os.environ.get("GITHUB_TOKEN")
