6f3c31d2014f753114000b6e1a5d4053ca22bf39
Douglas Yung <douglas.yung@sony.com>
2023-09-13 21:07:56 -0700

    Revert "Revert "Revert "[clang] Add experimental option to omit the RTTI component from the vtable when -fno-rtti is used"""
    This reverts commit e16474ba2cba8fa69b413a83852d06c93155f8c9.
    
    This change is still causing a test failure on a bot: https://lab.llvm.org/buildbot/#/builders/139/builds/49666

--- clang/lib/Driver/ToolChains/Clang.cpp
+++ clang/lib/Driver/ToolChains/Clang.cpp
@@ -5526,9 +5526,6 @@ void Clang::ConstructJob(Compilation &C, const JobAction &JA,
   Args.AddLastArg(CmdArgs, options::OPT_fexperimental_relative_cxx_abi_vtables,
                   options::OPT_fno_experimental_relative_cxx_abi_vtables);
 
-  Args.AddLastArg(CmdArgs, options::OPT_fexperimental_omit_vtable_rtti,
-                  options::OPT_fno_experimental_omit_vtable_rtti);
-
   // Handle segmented stacks.
   Args.addOptInFlag(CmdArgs, options::OPT_fsplit_stack,
                     options::OPT_fno_split_stack);
@@ -6009,9 +6006,6 @@ void Clang::ConstructJob(Compilation &C, const JobAction &JA,
 
   Args.AddLastArg(CmdArgs, options::OPT_fexperimental_relative_cxx_abi_vtables,
                   options::OPT_fno_experimental_relative_cxx_abi_vtables);
-
-  Args.AddLastArg(CmdArgs, options::OPT_fexperimental_omit_vtable_rtti,
-                  options::OPT_fno_experimental_omit_vtable_rtti);
 
   if (Arg *A = Args.getLastArg(options::OPT_ffuchsia_api_level_EQ))
     A->render(Args, CmdArgs);
