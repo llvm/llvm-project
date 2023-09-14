c990d9444350ef583c6d53e84d9c10cebbf65532
Antonio Frighetto <me@antoniofrighetto.com>
2023-09-14 09:16:01 +0200

    [clang] Include `expected-no-diagnostics` in newly-added test (NFC)

--- clang/test/SemaCXX/cxx2a-virtual-base-used.cpp
+++ clang/test/SemaCXX/cxx2a-virtual-base-used.cpp
@@ -1,4 +1,6 @@
-// RUN: %clang_cc1 -std=c++20 -verify=cxx20 -triple=x86_64-linux-gnu %s
+// RUN: %clang_cc1 -std=c++2a -verify -triple=x86_64-linux-gnu %s
+// expected-no-diagnostics
+
 // Fixes assertion triggered by https://github.com/llvm/llvm-project/issues/65982
 
 struct A { int y; };
