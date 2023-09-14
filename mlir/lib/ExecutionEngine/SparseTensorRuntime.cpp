2bd5286d2c94a243e5b75c10beb7c776ed2f6f09
Brian Kelley <bmkelle@sandia.gov>
2023-09-14 12:26:35 -0600

    Add some useful debugging stuff to CRunnerUtils
    - print out 1D strided memrefs
    - get the rank and datatype of memrefs
    - see the arguments to newSparseTensor and newSparseTensorFromReader

--- mlir/lib/ExecutionEngine/SparseTensorRuntime.cpp
+++ mlir/lib/ExecutionEngine/SparseTensorRuntime.cpp
@@ -325,6 +325,21 @@ extern "C" {
 static_assert(std::is_same<index_type, uint64_t>::value,
               "Expected index_type == uint64_t");
 
+}
+
+template<typename PrintAs, typename SMR>
+void printSMR(SMR smr)
+{
+  static_assert(SMR::Rank == 1);
+  for(size_t i = 0; i < smr.sizes[0]; i++)
+  {
+    std::cout << PrintAs(smr.data[i]) << ' ';
+  }
+  std::cout << "\n";
+}
+
+extern "C" {
+
 // TODO: this swiss-army-knife should be split up into separate functions
 // for each action, since the various actions don't agree on (1) whether
 // the first two arguments are "sizes" vs "shapes", (2) whether the "lvl"
@@ -337,6 +352,23 @@ void *_mlir_ciface_newSparseTensor( // NOLINT
     StridedMemRefType<index_type, 1> *lvl2dimRef,
     StridedMemRefType<index_type, 1> *dim2lvlRef, OverheadType ptrTp,
     OverheadType indTp, PrimaryType valTp, Action action, void *ptr) {
+  /*
+  std::cout << "Hello from _mlir_ciface_newSparseTensor!\n";
+  std::cout << "dimSizesRef: ";
+  printSMR<index_type>(*dimSizesRef);
+  std::cout << "lvlSizesRef: ";
+  printSMR<index_type>(*lvlSizesRef);
+  std::cout << "lvlTypesRef: ";
+  printSMR<int>(*lvlTypesRef);
+  std::cout << "lvl2dimRef: ";
+  printSMR<index_type>(*lvl2dimRef);
+  std::cout << "dim2lvlRef: ";
+  printSMR<index_type>(*dim2lvlRef);
+  std::cout << "indTp = " << (int) indTp << '\n';
+  std::cout << "valTp = " << (int) valTp << '\n';
+  std::cout << "action = " << (int) action << '\n';
+  std::cout << "ptr = " << ptr << '\n';
+  */
   ASSERT_NO_STRIDE(dimSizesRef);
   ASSERT_NO_STRIDE(lvlSizesRef);
   ASSERT_NO_STRIDE(lvlTypesRef);
@@ -637,6 +669,21 @@ void *_mlir_ciface_newSparseTensorFromReader(
     StridedMemRefType<index_type, 1> *lvl2dimRef,
     StridedMemRefType<index_type, 1> *dim2lvlRef, OverheadType ptrTp,
     OverheadType indTp, PrimaryType valTp) {
+  /*
+  std::cout << "Hello from _mlir_ciface_newSparseTensorFromReader!\n";
+  std::cout << "Reader (p): " << p << '\n';
+  std::cout << "lvlSizesRef: ";
+  printSMR<index_type>(*lvlSizesRef);
+  std::cout << "lvlTypesRef: ";
+  printSMR<int>(*lvlTypesRef);
+  std::cout << "lvl2dimRef: ";
+  printSMR<index_type>(*lvl2dimRef);
+  std::cout << "dim2lvlRef: ";
+  printSMR<index_type>(*dim2lvlRef);
+  std::cout << "ptrTp = " << (int) ptrTp << '\n';
+  std::cout << "indTp = " << (int) indTp << '\n';
+  std::cout << "valTp = " << (int) valTp << '\n';
+  */
   assert(p);
   SparseTensorReader &reader = *static_cast<SparseTensorReader *>(p);
   ASSERT_NO_STRIDE(lvlSizesRef);
