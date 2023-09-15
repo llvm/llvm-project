fce6f339f7cd5979910338d126fff57af00ba9b0
Alastair Houghton <ahoughton@apple.com>
2023-09-15 13:46:19 +0100

    [Clang][ObjC] Add optionality to property attribute strings.
    Add a new attribute, "?", to the property attribute string for
    properties of protocols that are declared @optional.
    
    (Previously https://reviews.llvm.org/D135273)
    
    rdar://100463524

--- clang/lib/AST/ASTContext.cpp
+++ clang/lib/AST/ASTContext.cpp
@@ -7824,6 +7824,7 @@ ASTContext::getObjCPropertyImplDeclForPropertyDecl(
 /// kPropertyWeak = 'W'              // 'weak' property
 /// kPropertyStrong = 'P'            // property GC'able
 /// kPropertyNonAtomic = 'N'         // property non-atomic
+/// kPropertyOptional = '?'          // property optional
 /// };
 /// @endcode
 std::string
@@ -7848,6 +7849,9 @@ ASTContext::getObjCEncodingForPropertyDecl(const ObjCPropertyDecl *PD,
   // GCC has some special rules regarding encoding of properties which
   // closely resembles encoding of ivars.
   getObjCEncodingForPropertyType(PD->getType(), S);
+
+  if (PD->isOptional())
+    S += ",?";
 
   if (PD->isReadOnly()) {
     S += ",R";
