1f47aa1cd03ca8b1c2c3f7e64be58f7f743b44eb
Will Hawkins <hawkinsw@obs.cr>
2023-09-15 09:54:24 -0400

    fixup! WIP: [libc++][ranges] Implement `ranges::stride_view`.
    Respond to JMazurkiewicz's helpful comments.

---
+++ libcxx/test/std/ranges/range.adaptors/range.stride.view/stride.pass.cpp
@@ -0,0 +1,32 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++03, c++11, c++14, c++17, c++20
+
+// ranges
+
+// std::views::stride_view
+
+#include "test.h"
+#include <ranges>
+#include <type_traits>
+#include <utility>
+
+constexpr bool test() {
+  static_assert(noexcept(std::declval<std::ranges::stride_view<BidirView>>().stride()));
+  static_assert(std::is_same_v<std::ranges::range_difference_t<BidirView>,
+                               decltype(std::declval<std::ranges::stride_view<BidirView>>().stride())>);
+  return true;
+}
+
+int main(int, char**) {
+  test();
+  static_assert(test());
+
+  return 0;
+}
