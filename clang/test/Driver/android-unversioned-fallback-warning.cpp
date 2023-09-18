915ebb07dfc53486eccf0dc09b6411929a463e98
Shoaib Meenai <smeenai@fb.com>
2023-09-18 16:28:04 -0700

    [driver] Address missed feedback from https://reviews.llvm.org/D158476
    I missed this before I committed.

--- clang/test/Driver/android-unversioned-fallback-warning.cpp
+++ clang/test/Driver/android-unversioned-fallback-warning.cpp
@@ -11,12 +11,12 @@
 // RUN: mkdir -p %t/resource/lib/aarch64-none-linux-android23
 
 // Using an unversioned directory for an unversioned triple isn't a warning.
-// RUN: %clang -target aarch64-none-linux-android -ccc-install-dir %t/bin \
+// RUN: %clang --target=aarch64-none-linux-android -ccc-install-dir %t/bin \
 // RUN:     -resource-dir %t/resource -### -c %s 2>&1 | \
 // RUN:   FileCheck --check-prefix=NO-WARNING %s
 // NO-WARNING-NOT: Using unversioned Android target directory
 
-// RUN: %clang -target aarch64-none-linux-android21 -ccc-install-dir %t/bin \
+// RUN: %clang --target=aarch64-none-linux-android21 -ccc-install-dir %t/bin \
 // RUN:     -resource-dir %t/resource -### -c %s 2>&1 | \
 // RUN:   FileCheck --check-prefix=ANDROID21 -DDIR=%t -DSEP=%{fs-sep} %s
 // ANDROID21-DAG: Using unversioned Android target directory [[DIR]]/bin[[SEP]]..[[SEP]]include[[SEP]]aarch64-none-linux-android
@@ -24,10 +24,10 @@
 // ANDROID21-DAG: Using unversioned Android target directory [[DIR]]/resource[[SEP]]lib[[SEP]]aarch64-none-linux-android
 
 // 23 or newer should use the versioned directory
-// RUN: %clang -target aarch64-none-linux-android23 -ccc-install-dir %t/bin \
+// RUN: %clang --target=aarch64-none-linux-android23 -ccc-install-dir %t/bin \
 // RUN:     -resource-dir %t/resource -### -c %s 2>&1 | \
 // RUN:   FileCheck --check-prefix=NO-WARNING %s
 
-// RUN: %clang -target aarch64-none-linux-android28 -ccc-install-dir %t/bin \
+// RUN: %clang --target=aarch64-none-linux-android28 -ccc-install-dir %t/bin \
 // RUN:     -resource-dir %t/resource -### -c %s 2>&1 | \
 // RUN:   FileCheck --check-prefix=NO-WARNING %s
