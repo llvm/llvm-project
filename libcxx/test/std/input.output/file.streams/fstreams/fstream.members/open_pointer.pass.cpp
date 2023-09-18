14de40c725097331c9eb04688d12a537377013fe
PragmaTwice <twice.mliu@gmail.com>
2023-09-18 16:32:02 -0400

    [libc++] Implement P2467R1: Support exclusive mode for fstreams
    This patch brings std::ios_base::noreplace from P2467R1 to libc++.
    This requires compiling the shared library in C++23 mode since otherwise
    fstream::open(...) doesn't know about the new flag.
    
    Differential Revision: https://reviews.llvm.org/D137640
    Co-authored-by: Louis Dionne <ldionne.2@gmail.com>

--- libcxx/test/std/input.output/file.streams/fstreams/fstream.members/open_pointer.pass.cpp
+++ libcxx/test/std/input.output/file.streams/fstreams/fstream.members/open_pointer.pass.cpp
@@ -13,6 +13,8 @@
 
 // void open(const char* s, ios_base::openmode mode = ios_base::in|ios_base::out);
 
+// XFAIL: LIBCXX-AIX-FIXME
+
 #include <fstream>
 #include <cassert>
 #include "test_macros.h"
@@ -51,5 +53,57 @@ int main(int, char**)
     std::remove(temp.c_str());
 #endif
 
-  return 0;
+#if TEST_STD_VER >= 23
+    // Test all the noreplace flag combinations
+    {
+        std::ios_base::openmode modes[] = {
+            std::ios_base::out | std::ios_base::noreplace,
+            std::ios_base::out | std::ios_base::trunc | std::ios_base::noreplace,
+            std::ios_base::in | std::ios_base::out | std::ios_base::trunc | std::ios_base::noreplace,
+            std::ios_base::out | std::ios_base::noreplace | std::ios_base::binary,
+            std::ios_base::out | std::ios_base::trunc | std::ios_base::noreplace | std::ios_base::binary,
+            std::ios_base::in | std::ios_base::out | std::ios_base::trunc | std::ios_base::noreplace |
+                std::ios_base::binary,
+        };
+        for (auto mode : modes) {
+          std::string tmp = get_temp_file_name(); // also creates the file
+
+          {
+            std::fstream f;
+            f.open(tmp.c_str(), mode);
+            assert(!f.is_open()); // since it already exists
+          }
+
+          {
+            std::remove(tmp.c_str());
+
+            std::fstream f;
+            f.open(tmp.c_str(), mode);
+            assert(f.is_open()); // since it doesn't exist
+          }
+        }
+
+#  ifndef TEST_HAS_NO_WIDE_CHARACTERS
+        for (auto mode : modes) {
+          std::string tmp = get_temp_file_name(); // also creates the file
+
+          {
+            std::wfstream f;
+            f.open(tmp.c_str(), mode);
+            assert(!f.is_open()); // since it already exists
+          }
+
+          {
+            std::remove(tmp.c_str());
+
+            std::wfstream f;
+            f.open(tmp.c_str(), mode);
+            assert(f.is_open()); // since it doesn't exist
+          }
+        }
+#  endif
+    }
+#endif // TEST_STD_VER >= 23
+
+    return 0;
 }
