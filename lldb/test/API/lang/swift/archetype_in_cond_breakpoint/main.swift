edbb96800beed35796804d591f2af3b3bd1931fc
Augusto Noronha <augusto2112@me.com>
2023-09-19 13:31:25 -0700

    [lldb] Mark parsing Swift expressions with generics as not cacheable
    Because a Swift expression may have different generic instantiations in
    different invocations, we cannot cache the parsing of them.
    
    (cherry picked from commit 97bb2d80e2d25d1ebcc2c19d2edf5e13b0944a84)

--- lldb/test/API/lang/swift/archetype_in_cond_breakpoint/main.swift
+++ lldb/test/API/lang/swift/archetype_in_cond_breakpoint/main.swift
@@ -1,4 +1,18 @@
+
 func f<T>(t: T) {
-  print(1) // break here
+  print(1) // break here for free function
 }
+f(t: "This is a string")
+f(t: "This is another string")
+f(t: true)
 f(t: 5)
+
+class MyClass<T> {
+  func f() {
+    print(1) // break here for class
+  }
+}
+MyClass<String>().f()
+MyClass<String>().f()
+MyClass<Bool>().f()
+MyClass<Int>().f()
