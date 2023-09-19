97bb2d80e2d25d1ebcc2c19d2edf5e13b0944a84
Augusto Noronha <augusto2112@me.com>
2023-09-19 13:31:25 -0700

    [lldb] Mark parsing Swift expressions with generics as not cacheable
    Because a Swift expression may have different generic instantiations in
    different invocations, we cannot cache the parsing of them.

--- lldb/test/API/lang/swift/archetype_in_cond_breakpoint/TestArchetypeInConditionalBreakpoint.py
+++ lldb/test/API/lang/swift/archetype_in_cond_breakpoint/TestArchetypeInConditionalBreakpoint.py
@@ -6,30 +6,45 @@ import unittest2
 
 
 class TestArchetypeInConditionalBreakpoint(TestBase):
+    @swiftTest
+    def test_stops_free_function(self):
+        self.stops("break here for free function")
 
     @swiftTest
-    def test_stops(self):
+    def test_doesnt_stop_free_function(self):
+        self.doesnt_stop("break here for free function")
+
+    @swiftTest
+    def test_stops_class(self):
+        self.stops("break here for class")
+
+    @swiftTest
+    def test_doesnt_stop_class(self):
+        self.doesnt_stop("break here for class")
+
+    def stops(self, breakpoint_string):
         """Tests that using archetypes in a conditional breakpoint's expression works correctly"""
         self.build()
         target = lldbutil.run_to_breakpoint_make_target(self)
 
         breakpoint = target.BreakpointCreateBySourceRegex(
-                "break here", lldb.SBFileSpec("main.swift"))
+            breakpoint_string, lldb.SBFileSpec("main.swift")
+        )
 
         breakpoint.SetCondition("T.self == Int.self")
         _, process, _, _ = lldbutil.run_to_breakpoint_do_run(self, target, breakpoint)
-    
+
         self.assertEqual(process.state, lldb.eStateStopped)
+        self.expect("expression T.self", substrs=["Int"])
 
-
-    @swiftTest
-    def test_doesnt_stop(self):
+    def doesnt_stop(self, breakpoint_string):
         """Tests that using archetypes in a conditional breakpoint's expression works correctly"""
         self.build()
         target = lldbutil.run_to_breakpoint_make_target(self)
 
         breakpoint = target.BreakpointCreateBySourceRegex(
-                "break here", lldb.SBFileSpec("main.swift"))
+            breakpoint_string, lldb.SBFileSpec("main.swift")
+        )
 
         breakpoint.SetCondition("T.self == Double.self")
 
@@ -41,4 +56,3 @@ class TestArchetypeInConditionalBreakpoint(TestBase):
 
         # Make sure that we didn't stop since the condition doesn't match
         self.assertEqual(process.state, lldb.eStateExited)
-
