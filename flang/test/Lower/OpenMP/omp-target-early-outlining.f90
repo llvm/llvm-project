99d1b20a7db012b38f8a93670102e6409a137f0f
Andrew Gozillon <Andrew.Gozillon@amd.com>
2023-09-19 08:00:40 -0500

    [Flang][OpenMP] Add lowering from PFT to new MapEntry and Bounds operations and tie them to relevant Target operations
    This patch builds on top of a prior patch in review which adds a new map
    and bounds operation by modifying the OpenMP PFT lowering to support
    these operations and generate them from the PFT.
    
    A significant amount of the support for the Bounds operation is borrowed
    from OpenACC's own current implementation and lowering, just ported
    over to OpenMP.
    
    The patch also adds very preliminary/initial support for lowering to
    a new Capture attribute, which is stored on the new Map Operation,
    which helps the later lowering from OpenMP -> LLVM IR by indicating
    how a map argument should be handled. This capture type will
    influence how a map argument is accessed on device and passed by
    the host (different load/store handling etc.). It is reflective of a
    similar piece of information stored in the Clang AST which performs a
    similar role.
    
    As well as some minor adjustments to how the map type (map bitshift
    which dictates to the runtime how it should handle an argument) is
    generated to further support more use-cases for future patches that
    build on this work.
    
    Finally it adds the map entry operation creation and tying it to the relevant
    target operations as well as the addition of some new tests and alteration
    of previous tests to support the new changes.
    
    Depends on D158732
    
    reviewers: kiranchandramohan, TIFitis, clementval, razvanlupusoru
    
    Differential Revision: https://reviews.llvm.org/D158734

--- flang/test/Lower/OpenMP/omp-target-early-outlining.f90
+++ flang/test/Lower/OpenMP/omp-target-early-outlining.f90
@@ -8,7 +8,8 @@
 !CHECK: func.func @_QPtarget_function
 
 !CHECK:  func.func @_QPwrite_index_omp_outline_0(%[[ARG0:.*]]: !fir.ref<i32>) attributes {omp.declare_target = #omp.declaretarget<device_type = (host), capture_clause = (to)>, omp.outline_parent_name = "_QPwrite_index"} {
-!CHECK-NEXT: omp.target  {{.*}} {
+!CHECK-NEXT: %[[map_info0:.*]] = omp.map_info var_ptr(%[[ARG0]]{{.*}}
+!CHECK-NEXT: omp.target  map_entries(%[[map_info0]]{{.*}} {
 !CHECK: %[[CONSTANT_VALUE_10:.*]] = arith.constant 10 : i32
 !CHECK: fir.store %[[CONSTANT_VALUE_10]] to %[[ARG0]] : !fir.ref<i32>
 !CHECK: omp.terminator
@@ -16,7 +17,8 @@
 !CHECK-NEXT: return
 
 !CHECK:  func.func @_QPwrite_index_omp_outline_1(%[[ARG1:.*]]: !fir.ref<i32>) attributes {omp.declare_target = #omp.declaretarget<device_type = (host), capture_clause = (to)>, omp.outline_parent_name = "_QPwrite_index"} {
-!CHECK-NEXT: omp.target  {{.*}} {
+!CHECK-NEXT: %[[map_info1:.*]] = omp.map_info var_ptr(%[[ARG1]]{{.*}}
+!CHECK-NEXT: omp.target  map_entries(%[[map_info1]]{{.*}} {
 !CHECK: %[[CONSTANT_VALUE_20:.*]] = arith.constant 20 : i32
 !CHECK: fir.store %[[CONSTANT_VALUE_20]] to %[[ARG1]] : !fir.ref<i32>
 !CHECK: omp.terminator
@@ -41,3 +43,47 @@ end subroutine WRITE_INDEX
 SUBROUTINE TARGET_FUNCTION()
 !$omp declare target
 END
+
+!CHECK: func.func @_QParray_bounds_omp_outline_0(%[[ARG0:.*]]: !fir.ref<i32>, %[[ARG1:.*]]: !fir.ref<!fir.array<10xi32>>) attributes {omp.declare_target = #omp.declaretarget<device_type = (host), capture_clause = (to)>, omp.outline_parent_name = "_QParray_bounds"} {
+!CHECK: %[[C1:.*]] = arith.constant 1 : index
+!CHECK: %[[C4:.*]] = arith.constant 4 : index
+!CHECK: %[[C1_0:.*]] = arith.constant 1 : index
+!CHECK: %[[C1_1:.*]] = arith.constant 1 : index
+!CHECK: %[[BOUNDS:.*]] = omp.bounds   lower_bound(%[[C1]] : index) upper_bound(%[[C4]] : index) stride(%[[C1_1]] : index) start_idx(%[[C1_1]] : index)
+!CHECK: %[[ENTRY:.*]] = omp.map_info var_ptr(%[[ARG1]] : !fir.ref<!fir.array<10xi32>>)   map_clauses(tofrom) capture(ByRef) bounds(%[[BOUNDS]]) -> !fir.ref<!fir.array<10xi32>> {name = "sp_write(2:5)"}
+!CHECK: omp.target   map_entries(%[[ENTRY]] : !fir.ref<!fir.array<10xi32>>) {
+!CHECK:  %c2_i32 = arith.constant 2 : i32
+!CHECK:  %2 = fir.convert %c2_i32 : (i32) -> index
+!CHECK:  %c5_i32 = arith.constant 5 : i32
+!CHECK:  %3 = fir.convert %c5_i32 : (i32) -> index
+!CHECK:  %c1_2 = arith.constant 1 : index
+!CHECK:  %4 = fir.convert %2 : (index) -> i32
+!CHECK:  %5:2 = fir.do_loop %arg2 = %2 to %3 step %c1_2 iter_args(%arg3 = %4) -> (index, i32) {
+!CHECK:    fir.store %arg3 to %[[ARG0]] : !fir.ref<i32>
+!CHECK:    %6 = fir.load %[[ARG0]] : !fir.ref<i32>
+!CHECK:    %7 = fir.load %[[ARG0]] : !fir.ref<i32>
+!CHECK:    %8 = fir.convert %7 : (i32) -> i64
+!CHECK:    %c1_i64 = arith.constant 1 : i64
+!CHECK:    %9 = arith.subi %8, %c1_i64 : i64
+!CHECK:    %10 = fir.coordinate_of %[[ARG1]], %9 : (!fir.ref<!fir.array<10xi32>>, i64) -> !fir.ref<i32>
+!CHECK:    fir.store %6 to %10 : !fir.ref<i32>
+!CHECK:    %11 = arith.addi %arg2, %c1_2 : index
+!CHECK:    %12 = fir.convert %c1_2 : (index) -> i32
+!CHECK:    %13 = fir.load %[[ARG0]] : !fir.ref<i32>
+!CHECK:    %14 = arith.addi %13, %12 : i32
+!CHECK:    fir.result %11, %14 : index, i32
+!CHECK:  }
+!CHECK: fir.store %5#1 to %[[ARG0]] : !fir.ref<i32>
+!CHECK:  omp.terminator
+!CHECK: }
+!CHECK:return
+!CHECK:}
+
+SUBROUTINE ARRAY_BOUNDS()
+        INTEGER :: sp_write(10) = (/0,0,0,0,0,0,0,0,0,0/)
+!$omp target map(tofrom:sp_write(2:5))
+        do i = 2, 5
+                sp_write(i) = i
+        end do
+!$omp end target
+end subroutine ARRAY_BOUNDS
