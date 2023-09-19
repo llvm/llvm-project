82f1eca7c180b0dfe8af36e933670f938a49bab1
DianQK <dianqk@dianqk.net>
2023-09-19 14:12:59 +0800

    [EarlyCSE] Replace APInt with uint64_t

--- llvm/lib/Transforms/Scalar/EarlyCSE.cpp
+++ llvm/lib/Transforms/Scalar/EarlyCSE.cpp
@@ -561,13 +561,13 @@ namespace {
 
 struct GEPValue {
   Instruction *Inst;
-  std::optional<APInt> ConstantOffset;
+  std::optional<uint64_t> ConstantOffset;
 
   GEPValue(Instruction *I) : Inst(I) {
     assert((isSentinel() || canHandle(I)) && "Inst can't be handled!");
   }
 
-  GEPValue(Instruction *I, std::optional<APInt> ConstantOffset)
+  GEPValue(Instruction *I, std::optional<uint64_t> ConstantOffset)
       : Inst(I), ConstantOffset(ConstantOffset) {
     assert((isSentinel() || canHandle(I)) && "Inst can't be handled!");
   }
@@ -1649,10 +1649,9 @@ bool EarlyCSE::processNode(DomTreeNode *Node) {
     // Compare GEP instructions based on offset.
     if (GEPValue::canHandle(&Inst)) {
       auto *GEP = cast<GetElementPtrInst>(&Inst);
-      std::optional<APInt> Offset =
-          APInt(SQ.DL.getIndexTypeSizeInBits(GEP->getType()), 0);
-      GEPValue GEPVal(GEP, GEP->accumulateConstantOffset(SQ.DL, Offset.value())
-                               ? Offset
+      APInt Offset = APInt(SQ.DL.getIndexTypeSizeInBits(GEP->getType()), 0);
+      GEPValue GEPVal(GEP, GEP->accumulateConstantOffset(SQ.DL, Offset)
+                               ? Offset.tryZExtValue()
                                : std::nullopt);
       if (Value *V = AvailableGEPs.lookup(GEPVal)) {
         LLVM_DEBUG(dbgs() << "EarlyCSE CSE GEP: " << Inst << "  to: " << *V
