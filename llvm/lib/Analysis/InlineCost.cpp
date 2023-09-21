82793d35ce6f65a5c9c95b52e47c44e0acfccbe6
Haohai Wen <haohai.wen@intel.com>
2023-09-21 10:30:39 +0800

    Revert "[InlineCost] Enable the cost benefit analysis for Sample PGO (#66457)"
    This reverts commit 2d95439eb1e435fbfe4b02e4f945ffb8abb096d2.

--- llvm/lib/Analysis/InlineCost.cpp
+++ llvm/lib/Analysis/InlineCost.cpp
@@ -787,7 +787,7 @@ class InlineCostCallAnalyzer final : public CallAnalyzer {
         return false;
     } else {
       // Otherwise, require instrumentation profile.
-      if (!(PSI->hasInstrumentationProfile() || PSI->hasSampleProfile()))
+      if (!PSI->hasInstrumentationProfile())
         return false;
     }
 
