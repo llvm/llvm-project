8466eb7d031cb8df9585e4cfa29d2af08b9a0c01
Yinying Li <107574043+yinying-lisa-li@users.noreply.github.com>
2023-09-22 16:03:08 +0000

    [mlir][sparse] Add more error messages and avoid crashing in new parser (#67034)
    Updates:
    1. Added more invalid encodings to test the robustness of the new syntax
    2. Changed the asserts that caused crashing into returning booleans
    3. Modified some error messages to make them clearer and handled
    failures in parsing quotes as keyword for level formats and properties.

--- mlir/lib/Dialect/SparseTensor/IR/Detail/Var.h
+++ mlir/lib/Dialect/SparseTensor/IR/Detail/Var.h
@@ -453,8 +453,8 @@ public:
   /// for the variable with the given name (i.e., either the newly created
   /// variable, or the pre-existing variable), and a bool indicating whether
   /// a new variable was created.
-  std::pair<VarInfo::ID, bool> create(StringRef name, llvm::SMLoc loc,
-                                      VarKind vk, bool verifyUsage = false);
+  std::optional<std::pair<VarInfo::ID, bool>>
+  create(StringRef name, llvm::SMLoc loc, VarKind vk, bool verifyUsage = false);
 
   /// Attempts to lookup or create a variable according to the given
   /// `Policy`.  Returns nullopt in one of two circumstances:
