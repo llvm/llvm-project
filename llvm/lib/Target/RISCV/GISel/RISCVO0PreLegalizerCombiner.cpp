8e87dc10b80f298a682bf01fab190a57e6d5af57
Craig Topper <craig.topper@sifive.com>
2023-09-22 10:13:56 -0700

    [RISCV][GISel] Add a post legalizer combiner and enable a couple comb… (#67053)
    …ines.
    
    We have an existing test that shows benefit from redundant_and and
    identity combines so use them as a starting point.

--- llvm/lib/Target/RISCV/GISel/RISCVO0PreLegalizerCombiner.cpp
+++ llvm/lib/Target/RISCV/GISel/RISCVO0PreLegalizerCombiner.cpp
@@ -139,13 +139,13 @@ bool RISCVO0PreLegalizerCombiner::runOnMachineFunction(MachineFunction &MF) {
 
 char RISCVO0PreLegalizerCombiner::ID = 0;
 INITIALIZE_PASS_BEGIN(RISCVO0PreLegalizerCombiner, DEBUG_TYPE,
-                      "Combine RISCV machine instrs before legalization", false,
+                      "Combine RISC-V machine instrs before legalization", false,
                       false)
 INITIALIZE_PASS_DEPENDENCY(TargetPassConfig)
 INITIALIZE_PASS_DEPENDENCY(GISelKnownBitsAnalysis)
 INITIALIZE_PASS_DEPENDENCY(GISelCSEAnalysisWrapperPass)
 INITIALIZE_PASS_END(RISCVO0PreLegalizerCombiner, DEBUG_TYPE,
-                    "Combine RISCV machine instrs before legalization", false,
+                    "Combine RISC-V machine instrs before legalization", false,
                     false)
 
 namespace llvm {
