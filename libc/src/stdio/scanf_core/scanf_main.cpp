a5a008ff4f5908e59ded084cc14a2237f080d681
michaelrj-google <71531609+michaelrj-google@users.noreply.github.com>
2023-09-22 12:50:02 -0700

    [libc] Refactor scanf reader to match printf (#66023)
    In a previous patch, the printf writer was rewritten to use a single
    writer class with a buffer and a callback hook. This patch refactors
    scanf's reader to match conceptually.

--- libc/src/stdio/scanf_core/scanf_main.cpp
+++ libc/src/stdio/scanf_core/scanf_main.cpp
@@ -38,11 +38,6 @@ int scanf_main(Reader *reader, const char *__restrict str,
     }
   }
 
-  if (conversions == 0 && reader->has_error()) {
-    // This is intended to be converted to EOF in the client call to avoid
-    // including stdio.h in this internal file.
-    return -1;
-  }
   return conversions;
 }
 
