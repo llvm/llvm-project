49836ba42edcdf1996c490916d596871781dc961
Alan Hu <alanh@ccs.neu.edu>
2023-09-23 12:39:38 -0400

    Simplify dune files

--- llvm/bindings/ocaml/discover.sh
+++ llvm/bindings/ocaml/discover.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-llvm_config=$1
-shift 1
-link_libs=$($llvm_config --libs $@)
-ld_flags=$($llvm_config --ldflags)
+link_libs=$($LLVM_CONFIG $LINK_MODE --libs $@)
+ld_flags=$($LLVM_CONFIG --ldflags)
 echo "(" > c_library_flags.sexp
 echo $ld_flags >> c_library_flags.sexp
 echo " " >> c_library_flags.sexp
