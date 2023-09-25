5f2da9c80db99c302de9938d5785e43a3d71fa6f
Louis Dionne <ldionne.2@gmail.com>
2023-09-25 09:46:01 -0400

    [runtimes] Bump the supported AppleClang version to AppleClang 15 (#67065)
    AppleClang 15 was released on September 18th and is now stable. Per our
    policy, we're bumping the supported AppleClang compiler to the latest
    release. This allows cleaning up the test suite, but most importantly
    unblocking various other patches that are blocked on bumping the
    compiler requirements.

--- libcxx/test/std/utilities/function.objects/func.wrap/func.wrap.func/func.wrap.func.con/ctad.static.compile.pass.cpp
+++ libcxx/test/std/utilities/function.objects/func.wrap/func.wrap.func/func.wrap.func.con/ctad.static.compile.pass.cpp
@@ -7,7 +7,7 @@
 //===----------------------------------------------------------------------===//
 
 // UNSUPPORTED: c++03, c++11, c++14, c++17, c++20
-// XFAIL: clang-15, apple-clang-14
+// XFAIL: clang-15
 
 // checks that CTAD for std::function works properly with static operator() overloads
 
