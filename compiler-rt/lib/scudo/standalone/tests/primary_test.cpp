ee316774e8d7c2a4035c92fe5ad5892f1a36e8aa
Chia-hung Duan <chiahungduan@google.com>
2023-09-25 21:31:55 +0000

    [scudo] Make local cache be agnostic to the type of node in freelist
    This change moves the `TransferBatch` and `BatchGroup` out of
    SizeClassAllocatorLocalCache. It allows us that the node in freelist
    can store more blocks instead of depending on the number of blocks
    cached.
    
    That means we will be able to store more blocks in each node of freelist
    and therefore reduce the memory used by BatchClass (with little
    performance overhead). Note that we haven't enabled that in this patch.
    This is the first step of this transition.

--- compiler-rt/lib/scudo/standalone/tests/primary_test.cpp
+++ compiler-rt/lib/scudo/standalone/tests/primary_test.cpp
@@ -207,7 +207,7 @@ struct SmallRegionsConfig {
 // For the 32-bit one, it requires actually exhausting memory, so we skip it.
 TEST(ScudoPrimaryTest, Primary64OOM) {
   using Primary = scudo::SizeClassAllocator64<SmallRegionsConfig>;
-  using TransferBatch = Primary::CacheT::TransferBatch;
+  using TransferBatch = Primary::TransferBatch;
   Primary Allocator;
   Allocator.init(/*ReleaseToOsInterval=*/-1);
   typename Primary::CacheT Cache;
@@ -233,8 +233,9 @@ TEST(ScudoPrimaryTest, Primary64OOM) {
   while (!Batches.empty()) {
     TransferBatch *B = Batches.back();
     Batches.pop_back();
-    B->copyToArray(Blocks);
-    Allocator.pushBlocks(&Cache, ClassId, Blocks, B->getCount());
+    const scudo::u16 Count = B->getCount();
+    B->moveToArray(Blocks);
+    Allocator.pushBlocks(&Cache, ClassId, Blocks, Count);
     Cache.deallocate(Primary::SizeClassMap::BatchClassId, B);
   }
   Cache.destroy(nullptr);
