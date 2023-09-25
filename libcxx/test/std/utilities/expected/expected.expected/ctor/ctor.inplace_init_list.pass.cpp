d26837cf672bc58adb0f0a86eb71ae5809caaf21
Louis Dionne <ldionne.2@gmail.com>
2023-09-25 09:46:01 -0400

    [runtimes] Bump the supported AppleClang version to AppleClang 15 (#67065)
    AppleClang 15 was released on September 18th and is now stable. Per our
    policy, we're bumping the supported AppleClang compiler to the latest
    release. This allows cleaning up the test suite, but most importantly
    unblocking various other patches that are blocked on bumping the
    compiler requirements.

--- libcxx/test/std/utilities/expected/expected.expected/ctor/ctor.inplace_init_list.pass.cpp
+++ libcxx/test/std/utilities/expected/expected.expected/ctor/ctor.inplace_init_list.pass.cpp
@@ -7,9 +7,6 @@
 
 // UNSUPPORTED: c++03, c++11, c++14, c++17, c++20
 
-// Older Clangs do not support the C++20 feature to constrain destructors
-// XFAIL: apple-clang-14
-
 // template<class U, class... Args>
 //   constexpr explicit expected(in_place_t, initializer_list<U> il, Args&&... args);
 //
