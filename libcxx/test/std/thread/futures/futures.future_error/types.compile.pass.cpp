670b24f7d6618560a62adf3dd0d78d7e375e61ce
Marek Kurdej <marek.kurdej+llvm.org@gmail.com>
2023-09-26 12:54:16 -0400

    [libc++] Make future_error constructor standard-compliant
    This patch removes the non compliant constructor of std::future_error
    and adds the standards compliant constructor in C++17 instead.
    
    Note that we can't support the constructor as an extension in all
    standard modes because it uses delegating constructors, which require
    C++11. We could in theory support the constructor as an extension in
    C++11 and C++14 only, however I believe it is acceptable not to do that
    since I expect the breakage from this patch will be minimal.
    
    If it turns out that more code than we expect is broken by this, we can
    reconsider that decision.
    
    This was found during D99515.
    
    Differential Revision: https://reviews.llvm.org/D99567
    Co-authored-by: Louis Dionne <ldionne.2@gmail.com>

--- libcxx/test/std/thread/futures/futures.future_error/types.compile.pass.cpp
+++ libcxx/test/std/thread/futures/futures.future_error/types.compile.pass.cpp
@@ -15,12 +15,4 @@
 #include <future>
 #include <type_traits>
 
-#include "test_macros.h"
-
-int main(int, char**)
-{
-    static_assert((std::is_convertible<std::future_error*,
-                                       std::logic_error*>::value), "");
-
-  return 0;
-}
+static_assert(std::is_convertible<std::future_error*, std::logic_error*>::value, "");
