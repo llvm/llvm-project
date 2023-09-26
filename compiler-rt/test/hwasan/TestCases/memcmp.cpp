0f8281fe909d8ececbbeda2c840f069daa7d89b9
Kirill Stoimenov <kstoimenov@google.com>
2023-09-26 17:35:42 +0000

    Fixed test to pass on ARM.

--- compiler-rt/test/hwasan/TestCases/memcmp.cpp
+++ compiler-rt/test/hwasan/TestCases/memcmp.cpp
@@ -3,15 +3,13 @@
 // RUN: %clangxx_hwasan -O2 %s -o %t && not %run %t 2>&1 | FileCheck %s
 // RUN: %clangxx_hwasan -O3 %s -o %t && not %run %t 2>&1 | FileCheck %s
 
-// REQUIRES: arm
+// REQUIRES: pointer-tagging
 
 #include <string.h>
 int main(int argc, char **argv) {
   char a1[] = {static_cast<char>(argc), 2, 3, 4};
   char a2[] = {1, static_cast<char>(2*argc), 3, 4};
   int res = memcmp(a1, a2, 4 + argc);  // BOOM
-  // CHECK: AddressSanitizer: stack-buffer-overflow
-  // CHECK: {{#[0-9]+ .*memcmp}}
-  // CHECK: {{#[0-9]+ .*main}}
+  // CHECK: HWAddressSanitizer: tag-mismatch on address
   return res;
 }
