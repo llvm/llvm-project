3cf5f0c2b4a3433d75fde74c89b7357eeb00165a
Kirill Stoimenov <kstoimenov@google.com>
2023-09-26 17:39:37 +0000

    Address comments.

--- compiler-rt/test/hwasan/TestCases/memcmp.cpp
+++ compiler-rt/test/hwasan/TestCases/memcmp.cpp
@@ -3,13 +3,16 @@
 // RUN: %clangxx_hwasan -O2 %s -o %t && not %run %t 2>&1 | FileCheck %s
 // RUN: %clangxx_hwasan -O3 %s -o %t && not %run %t 2>&1 | FileCheck %s
 
-// REQUIRES: pointer-tagging
-
 #include <string.h>
+#include <stdlib.h>
+#include <sanitizer/hwasan_interface.h>
+
 int main(int argc, char **argv) {
-  char a1[] = {static_cast<char>(argc), 2, 3, 4};
-  char a2[] = {1, static_cast<char>(2*argc), 3, 4};
-  int res = memcmp(a1, a2, 4 + argc);  // BOOM
+  __hwasan_enable_allocator_tagging();
+  char a[] = {static_cast<char>(argc), 2, 3, 4};
+  char *p = (char *)malloc(sizeof(a));
+  free(p);
+  memcpy(p, a, sizeof(a));
   // CHECK: HWAddressSanitizer: tag-mismatch on address
-  return res;
+  return memcmp(p, a, sizeof(a));
 }
