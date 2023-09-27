aff6ffc8760b99cc3d66dd6e251a4f90040c0ab9
Luke Lau <luke@igalia.com>
2023-09-27 18:12:29 +0100

    [RISCV] Handle .vx pseudos in hasAllNBitUsers (#67419)
    Vector pseudos with scalar operands only use the lower SEW bits (or less
    in the
    case of shifts and clips). This patch accounts for this in
    hasAllNBitUsers for
    both SDNodes in RISCVISelDAGToDAG. We also need to handle this in
    RISCVOptWInstrs otherwise we introduce slliw instructions that are less
    compressible than their original slli counterpart.

--- llvm/lib/Target/RISCV/RISCVOptWInstrs.cpp
+++ llvm/lib/Target/RISCV/RISCVOptWInstrs.cpp
@@ -107,8 +107,23 @@ static bool hasAllNBitUsers(const MachineInstr &OrigMI,
       unsigned OpIdx = UserOp.getOperandNo();
 
       switch (UserMI->getOpcode()) {
-      default:
+      default: {
+        if (const RISCVVPseudosTable::PseudoInfo *PseudoInfo =
+                RISCVVPseudosTable::getPseudoInfo(UserMI->getOpcode())) {
+          const MCInstrDesc &MCID = UserMI->getDesc();
+          if (!RISCVII::hasSEWOp(MCID.TSFlags))
+            return false;
+          assert(RISCVII::hasVLOp(MCID.TSFlags));
+          const unsigned Log2SEW =
+              UserMI->getOperand(RISCVII::getSEWOpNum(MCID)).getImm();
+          if (UserOp.getOperandNo() == RISCVII::getVLOpNum(MCID))
+            return false;
+          if (RISCVII::vectorInstUsesNBitsOfScalarOp(PseudoInfo->BaseInstr,
+                                                     Bits, Log2SEW))
+            break;
+        }
         return false;
+      }
 
       case RISCV::ADDIW:
       case RISCV::ADDW:
