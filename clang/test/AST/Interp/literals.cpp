9243b1bfdbf4c8b8f3f8c8e45b57f784311adaac
Aaron Ballman <aaron@aaronballman.com>
2023-09-27 14:26:49 -0400

    Revert "[clang-cl] Fix value of __FUNCTION__ and __FUNC__ in MSVC mode for c++. (#66120)"
    This reverts commit 265568c136f94b108790e9be73cd8071e714aad1.
    
    See https://github.com/llvm/llvm-project/issues/66114#issuecomment-1732319259

--- clang/test/AST/Interp/literals.cpp
+++ clang/test/AST/Interp/literals.cpp
@@ -1046,8 +1046,8 @@ namespace PredefinedExprs {
     static_assert(strings_match(__FUNCSIG__, "void __cdecl PredefinedExprs::foo(void)"), "");
     static_assert(strings_match(L__FUNCSIG__, L"void __cdecl PredefinedExprs::foo(void)"), "");
     static_assert(strings_match(L__FUNCTION__, L"foo"), "");
-    static_assert(strings_match(__FUNCTION__, "PredefinedExprs::foo"), "");
-    static_assert(strings_match(__func__, "PredefinedExprs::foo"), "");
+    static_assert(strings_match(__FUNCTION__, "foo"), "");
+    static_assert(strings_match(__func__, "foo"), "");
     static_assert(strings_match(__PRETTY_FUNCTION__, "void PredefinedExprs::foo()"), "");
   }
 
@@ -1058,9 +1058,9 @@ namespace PredefinedExprs {
                                 // expected-warning {{result unused}}
     return __FUNCTION__[index];
   }
-  static_assert(heh(0) == 'P', "");
-  static_assert(heh(1) == 'r', "");
-  static_assert(heh(2) == 'e', "");
+  static_assert(heh(0) == 'h', "");
+  static_assert(heh(1) == 'e', "");
+  static_assert(heh(2) == 'h', "");
 #endif
 }
 
