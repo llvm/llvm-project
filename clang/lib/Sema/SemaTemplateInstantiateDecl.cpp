1ddb91fe7c4fbb48afa56a3982a8b033e45326bc
Takuya Shimizu <shimizu2486@gmail.com>
2023-09-27 09:26:06 +0900

    [clang][SemaCXX] Diagnose tautological uses of consteval if and is_constant_evaluated
    This patch makes clang diagnose extensive cases of consteval if and is_constant_evaluated usage that are tautologically true or false.
    This introduces a new IsRuntimeEvaluated boolean flag to Sema::ExpressionEvaluationContextRecord that means the immediate appearance of if consteval or is_constant_evaluated are tautologically false(e.g. inside if !consteval {} block or non-constexpr-qualified function definition body)
    This patch also pushes new expression evaluation context when parsing the condition of if constexpr and initializer of constexpr variables so that Sema can be aware that the use of consteval if and is_consteval are tautologically true in if constexpr condition and constexpr variable initializers.
    BEFORE this patch, the warning for is_constant_evaluated was emitted from constant evaluator. This patch moves the warning logic to Sema in order to diagnose tautological use of is_constant_evaluated in the same way as consteval if.
    
    This patch separates initializer evaluation context from InitializerScopeRAII.
    This fixes a bug that was happening when user takes address of function address in initializers of non-local variables.
    
    Fixes https://github.com/llvm/llvm-project/issues/43760
    Fixes https://github.com/llvm/llvm-project/issues/51567
    
    Reviewed By: cor3ntin, ldionne
    Differential Revision: https://reviews.llvm.org/D155064

--- clang/lib/Sema/SemaTemplateInstantiateDecl.cpp
+++ clang/lib/Sema/SemaTemplateInstantiateDecl.cpp
@@ -5378,8 +5378,11 @@ void Sema::InstantiateVariableInitializer(
     Var->setImplicitlyInline();
 
   if (OldVar->getInit()) {
-    EnterExpressionEvaluationContext Evaluated(
-        *this, Sema::ExpressionEvaluationContext::PotentiallyEvaluated, Var);
+    Sema::ExpressionEvaluationContext InitEvalContext =
+        Var->isConstexpr()
+            ? Sema::ExpressionEvaluationContext::ConstantEvaluated
+            : Sema::ExpressionEvaluationContext::PotentiallyEvaluated;
+    EnterExpressionEvaluationContext Evaluated(*this, InitEvalContext, Var);
 
     // Instantiate the initializer.
     ExprResult Init;
