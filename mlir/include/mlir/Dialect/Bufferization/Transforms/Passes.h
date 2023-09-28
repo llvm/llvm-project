241ff50d6673a2a536b429fe363b9660cc67f7cc
Martin Erhart <merhart@google.com>
2023-09-28 09:14:46 +0000

    Revert "[mlir][bufferization] Don't clone on unknown ownership and verify function boundary ABI (#66626)"
    This reverts commit aa9eb47da2e501d3505de733240eb89c9a0ea2cf.
    It introduced a double free in a test case. Reverting to have some time
    for fixing this and relanding later.

--- mlir/include/mlir/Dialect/Bufferization/Transforms/Passes.h
+++ mlir/include/mlir/Dialect/Bufferization/Transforms/Passes.h
@@ -1,7 +1,6 @@
 #ifndef MLIR_DIALECT_BUFFERIZATION_TRANSFORMS_PASSES_H
 #define MLIR_DIALECT_BUFFERIZATION_TRANSFORMS_PASSES_H
 
-#include "mlir/Dialect/Bufferization/IR/BufferDeallocationOpInterface.h"
 #include "mlir/Pass/Pass.h"
 
 namespace mlir {
@@ -32,7 +31,7 @@ std::unique_ptr<Pass> createBufferDeallocationPass();
 /// Creates an instance of the OwnershipBasedBufferDeallocation pass to free all
 /// allocated buffers.
 std::unique_ptr<Pass> createOwnershipBasedBufferDeallocationPass(
-    const DeallocationOptions &options = DeallocationOptions());
+    bool privateFuncDynamicOwnership = false);
 
 /// Creates a pass that optimizes `bufferization.dealloc` operations. For
 /// example, it reduces the number of alias checks needed at runtime using
@@ -135,9 +134,8 @@ func::FuncOp buildDeallocationLibraryFunction(OpBuilder &builder, Location loc,
 LogicalResult deallocateBuffers(Operation *op);
 
 /// Run ownership basedbuffer deallocation.
-LogicalResult deallocateBuffersOwnershipBased(
-    FunctionOpInterface op,
-    const DeallocationOptions &options = DeallocationOptions());
+LogicalResult deallocateBuffersOwnershipBased(FunctionOpInterface op,
+                                              bool privateFuncDynamicOwnership);
 
 /// Creates a pass that moves allocations upwards to reduce the number of
 /// required copies that are inserted during the BufferDeallocation pass.
