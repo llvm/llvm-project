8602504272a9144698561ff33ed3acfc0cacf466
jeanPerier <jperier@nvidia.com>
2023-09-28 10:47:04 +0200

    [flang][lowering] Fix clash between string literals of different kinds (#67576)
    At least fir.global for empty string literals of different kinds. Add
    the kind to the prefix if it is not 1.

---
+++ flang/test/Lower/HLFIR/constant-character.f90
@@ -0,0 +1,17 @@
+! Test that character literals of different types do not clash.
+! RUN: bbc -emit-hlfir %s -o - | FileCheck %s
+
+subroutine repro(c1, c4)
+  character(kind=1,len=*) :: c1
+  character(kind=4,len=*) :: c4
+  print *, ""
+  print *, 4_""
+end subroutine
+!CHECK-LABEL: func.func @_QPrepro
+!CHECK:    fir.address_of(@_QQcl.) : !fir.ref<!fir.char<1,0>>
+!CHECK:    fir.call @_FortranAioOutputAscii
+!CHECK:    fir.address_of(@_QQcl4.) : !fir.ref<!fir.char<4,0>>
+!CHECK:    fir.call @_FortranAioOutputDescriptor(
+
+!CHECK-DAG:  fir.global linkonce @_QQcl. constant : !fir.char<1,0>
+!CHECK-DAG:  fir.global linkonce @_QQcl4. constant : !fir.char<4,0>
