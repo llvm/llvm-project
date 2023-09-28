9ffa883e6fca7557a6f4bc03f85bb3bd79344a81
Dhruv Chawla <44582521+dc03@users.noreply.github.com>
2023-09-28 13:44:29 +0530

    Replace UDiv/SDiv matchers by IDiv

--- llvm/lib/Analysis/ValueTracking.cpp
+++ llvm/lib/Analysis/ValueTracking.cpp
@@ -2264,8 +2264,7 @@ static bool isKnownNonNullFromDominatingCondition(const Value *V,
         return true;
     }
 
-    if ((match(U, m_UDiv(m_Value(), m_Specific(V))) ||
-         match(U, m_SDiv(m_Value(), m_Specific(V)))) &&
+    if (match(U, m_IDiv(m_Value(), m_Specific(V))) &&
         isValidAssumeForContext(cast<Instruction>(U), CtxI, DT))
       return true;
 
