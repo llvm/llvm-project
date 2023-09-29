574c94ce4f7eed06013a5e5d1460da1d148cc7cd
James Smith <james.smith9113@gmail.com>
2023-09-29 14:11:49 -0400

    [libc++] Implement ranges::iota: update module and docs info

--- libcxx/utils/generate_feature_test_macro_components.py
+++ libcxx/utils/generate_feature_test_macro_components.py
@@ -806,7 +806,6 @@ feature_test_macros = [
             "name": "__cpp_lib_ranges_iota",
             "values": {"c++23": 202202},
             "headers": ["numeric"],
-            "unimplemented": True,
         },
         {
             "name": "__cpp_lib_ranges_join_with",
