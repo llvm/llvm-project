b4e14f87d4c21cad3cb58502dc3348ba4af36822
Alex Richardson <alexrichardson@google.com>
2023-09-29 09:07:15 -0700

    [runtimes] Fix parsing of LIB{CXX,CXXABI,UNWIND}_TEST_PARAMS
    Since 78d649a417b48cb8a2ba2e755f0e7c8fb8b1bb83 the recommended way to pass
    an executor is to use the _TEST_PARAMS variable, which means we now pass
    more complicated value (including ones that may contain multiple `=`) as
    part of this variable. However, the `REGEX REPLACE` being used has greedy
    matches so everything up to the last = becomes part of the variable name
    which results in invalid syntax in the generated lit config file.
    
    This was noticed due to builder failures for those using the
    CrossWinToARMLinux.cmake cache file.
    
    Co-authored-by: Vladimir Vereschaka <vvereschaka@accesssoftek.com>

--- runtimes/cmake/Modules/HandleLitArguments.cmake
+++ runtimes/cmake/Modules/HandleLitArguments.cmake
@@ -8,3 +8,13 @@ macro(serialize_lit_string_param output_var param value)
   string(REPLACE "\"" "\\\"" _escaped_value "${value}")
   string(APPEND ${output_var} "config.${param} = \"${_escaped_value}\"\n")
 endmacro()
+
+macro(serialize_lit_params_list output_var list)
+  foreach(param IN LISTS ${list})
+    string(FIND "${param}" "=" _eq_index)
+    string(SUBSTRING "${param}" 0 ${_eq_index} name)
+    string(SUBSTRING "${param}" ${_eq_index} -1 value)
+    string(SUBSTRING "${value}" 1 -1 value) # strip the leading =
+    serialize_lit_string_param("${output_var}" "${name}" "${value}")
+  endforeach()
+endmacro()
