5180c21fca9f0f2d67b0f4273047955b1652229e
Mirko Brkušanin <Mirko.Brkusanin@amd.com>
2023-09-29 11:54:49 +0200

    [AMDGPU] Src1 of VOP3 DPP instructions can be SGPR on supported subtargets (#67461)
    In order to avoid duplicating every dpp pseudo opcode that has src1, we
    allow it for all opcodes and add manual checks on subtargets that do not
    support it.

--- llvm/lib/Target/AMDGPU/Utils/AMDGPUBaseInfo.cpp
+++ llvm/lib/Target/AMDGPU/Utils/AMDGPUBaseInfo.cpp
@@ -2085,6 +2085,10 @@ bool hasVOPD(const MCSubtargetInfo &STI) {
   return STI.hasFeature(AMDGPU::FeatureVOPD);
 }
 
+bool hasDPPSrc1SGPR(const MCSubtargetInfo &STI) {
+  return STI.hasFeature(AMDGPU::FeatureDPPSrc1SGPR);
+}
+
 unsigned hasKernargPreload(const MCSubtargetInfo &STI) {
   return STI.hasFeature(AMDGPU::FeatureKernargPreload);
 }
