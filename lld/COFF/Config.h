695680b9b6c0af95c71cf1eb1360be2b409a6df0
Martin Storsjö <martin@martin.st>
2023-10-01 00:01:31 +0300

    [LLD] [COFF] Add a separate option for allowing duplicate weak symbols
    The MinGW mode (enabled with the flag -lldmingw) does allow duplicate
    weak symbols. A test in
    compiler-rt/test/profile/Windows/coverage-weak-lld.cpp does currently
    enable the -lldmingw flag in an MSVC context, in order to deal with
    duplicate weak symbols.
    
    Add a new, separate, lld specific flag for enabling this. In MinGW
    mode, this is enabled by default, otherwise it is disabled.
    
    This allows making the MinGW mode more restrictive in adding
    libpaths from the surrounding environment; in MinGW mode, all
    libpaths are passed explicitly by the compiler driver to the
    linker, which is attempted in https://reviews.llvm.org/D144084.

--- lld/COFF/Config.h
+++ lld/COFF/Config.h
@@ -314,6 +314,7 @@ struct Configuration {
   bool stdcallFixup = false;
   bool writeCheckSum = false;
   EmitKind emit = EmitKind::Obj;
+  bool allowDuplicateWeak = false;
 };
 
 } // namespace lld::coff
