14e35787ad64b0b5d56804fdc4db20b7b2a630d2
Mikhail R. Gadelha <mikhail@igalia.com>
2023-10-02 16:34:24 -0300

    Addec wint check back + comment
    Signed-off-by: Mikhail R. Gadelha <mikhail@igalia.com>

--- libc/src/__support/wctype_utils.h
+++ libc/src/__support/wctype_utils.h
@@ -28,7 +28,9 @@ namespace internal {
 LIBC_INLINE cpp::optional<int> wctob(wint_t c) {
   // This needs to be translated to EOF at the callsite. This is to avoid
   // including stdio.h in this file.
-  if (c > 127)
+  // The standard states that wint_t may either be an alias of wchar_t or
+  // an alias of an integer type, so we need to keep the c < 0 check.
+  if (c > 127 || c < 0)
     return cpp::nullopt;
   return static_cast<int>(c);
 }
