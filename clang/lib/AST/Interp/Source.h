1dd979994ab4b0ae8eb02f734453dab174c2733e
Sam McCall <sam.mccall@gmail.com>
2023-10-02 15:57:16 +0200

    [clang]Interp] Re-add some header deps of Source.h
    This is a partial revert of cc696627657c422f8d580354a76d32fda0f0157f
    
    The full definition of Decl and Stmt are required by PointerUnion, which
    validates the number of free bits in Decl* etc based on type alignment.

--- clang/lib/AST/Interp/Source.h
+++ clang/lib/AST/Interp/Source.h
@@ -14,12 +14,12 @@
 #define LLVM_CLANG_AST_INTERP_SOURCE_H
 
 #include "PrimType.h"
+#include "clang/AST/DeclBase.h"
+#include "clang/AST/Stmt.h"
 #include "llvm/ADT/PointerUnion.h"
 #include "llvm/Support/Endian.h"
 
 namespace clang {
-class Stmt;
-class Decl;
 class Expr;
 class SourceLocation;
 class SourceRange;
