875e56948f40e97770143393d13e1925749ed922
Mark de Wever <koraq@xs4all.nl>
2023-10-03 20:34:27 +0200

    [NFC][libc++][test] ' ,c++17' -> ', c++17'.

--- libcxx/test/libcxx/diagnostics/ranges.nodiscard_extensions.compile.pass.cpp
+++ libcxx/test/libcxx/diagnostics/ranges.nodiscard_extensions.compile.pass.cpp
@@ -9,7 +9,7 @@
 // Check that ranges algorithms aren't marked [[nodiscard]] when
 // _LIBCPP_DISBALE_NODISCARD_EXT is defined
 
-// UNSUPPORTED: c++03, c++11, c++14 ,c++17
+// UNSUPPORTED: c++03, c++11, c++14, c++17
 
 // ADDITIONAL_COMPILE_FLAGS: -D_LIBCPP_DISABLE_NODISCARD_EXT
 
