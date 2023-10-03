d1c63e0292181b6705c1e464e0e61e66b7192417
Da-Viper <57949090+Da-Viper@users.noreply.github.com>
2023-10-03 20:25:49 +0100

    Update llvm/utils/git/github-automation.py
    Co-authored-by: Danny Mösch <danny.moesch@icloud.com>

--- llvm/utils/git/github-automation.py
+++ llvm/utils/git/github-automation.py
@@ -32,7 +32,7 @@ This issue may be a good introductory issue for people new to working on LLVM. I
   4) Create a `git` commit
   5) Run [`git clang-format HEAD~1`](https://clang.llvm.org/docs/ClangFormat.html#git-integration) to format your changes.
   6) Open a pull request to the [upstream repository](https://github.com/llvm/llvm-project/pulls) on GitHub.
-    6.1) Detailed instructions can be found [here](https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/proposing-changes-to-your-work-with-pull-requests/creating-a-pull-request)
+    6.1) Detailed instructions can be found [here](https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/proposing-changes-to-your-work-with-pull-requests/creating-a-pull-request).
 
 If you have any further questions about this issue, don't hesitate to ask via a comment on this Github issue.
 """
