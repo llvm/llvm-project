5e9d420f5554c18da653dfcd2766f8a90d2b496e
Douglas Yung <douglas.yung@sony.com>
2023-10-03 14:27:53 -0700

    Bump up DIAG_SIZE_DRIVER as we are very close to the limit as of 9a40858.

--- clang/include/clang/Basic/DiagnosticIDs.h
+++ clang/include/clang/Basic/DiagnosticIDs.h
@@ -31,7 +31,7 @@ namespace clang {
     // Size of each of the diagnostic categories.
     enum {
       DIAG_SIZE_COMMON        =  300,
-      DIAG_SIZE_DRIVER        =  300,
+      DIAG_SIZE_DRIVER        =  400,
       DIAG_SIZE_FRONTEND      =  150,
       DIAG_SIZE_SERIALIZATION =  120,
       DIAG_SIZE_LEX           =  400,
