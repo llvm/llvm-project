e9fce8024c4345522c227b1e47bf6cb870321403
Will Hawkins <hawkinsw@obs.cr>
2023-10-04 19:44:22 -0400

    fixup! WIP: [libc++][ranges] Implement `ranges::stride_view`.
    Address first of Konstantin's helpful comments.

--- libcxx/test/std/ranges/range.adaptors/range.stride.view/iterator/ctor.default.pass.cpp
+++ libcxx/test/std/ranges/range.adaptors/range.stride.view/iterator/ctor.default.pass.cpp
@@ -13,40 +13,40 @@
 // std::views::stride_view
 
 #include "../test.h"
+#include "__concepts/same_as.h"
+#include "__ranges/stride_view.h"
 #include <cassert>
 #include <ranges>
 #include <type_traits>
 
 constexpr bool non_simple_view_iter_ctor_test() {
-  using StrideView             = std::ranges::stride_view<NotSimpleView>;
-  using StrideViewIterNonConst = std::ranges::iterator_t<StrideView>;
-  using StrideViewIterConst    = std::ranges::iterator_t<const StrideView>;
+  using NotSimpleStrideView     = std::ranges::stride_view<NotSimpleView>;
+  using NotSimpleStrideViewIter = std::ranges::iterator_t<NotSimpleStrideView>;
 
-  StrideView sv{NotSimpleView{}, 1};
-  StrideViewIterNonConst iter = {sv, sv.base().begin(), 0};
-  // StrideViewIterNonConst constructs its new __current_ and __end_ by
-  // moving the same members from the given iterator. When that is not possible
-  // for either of those two values, it should fail.
-  static_assert(!std::is_constructible_v<StrideViewIterConst, decltype(iter)>);
-  return true;
-}
+  using SimpleStrideView     = std::ranges::stride_view<ForwardTracedMoveView>;
+  using SimpleStrideViewIter = std::ranges::iterator_t<SimpleStrideView>;
 
-constexpr bool simpleview_iter_ctor_test() {
-  using StrideView     = std::ranges::stride_view<ForwardTracedMoveView>;
-  using StrideViewIter = std::ranges::iterator_t<StrideView>;
+  NotSimpleStrideView nsv{NotSimpleView{}, 1};
+  [[maybe_unused]] NotSimpleStrideViewIter nsv_iter = nsv.begin();
 
-  StrideView sv{ForwardTracedMoveView{}, 1};
-  StrideViewIter iter = {sv, sv.base().begin(), 0};
-  // Guarantee that when the iterator is given to the constructor that
-  // it is moved there.
-  assert(iter.base().moved);
+  SimpleStrideView sv{ForwardTracedMoveView{}, 1};
+  [[maybe_unused]] SimpleStrideViewIter ssv_iter = sv.begin();
 
+  using NotSimpleStrideViewIterConst = std::ranges::iterator_t<const NotSimpleStrideView>;
+  using SimpleStrideViewIterConst    = std::ranges::iterator_t<const SimpleStrideView>;
+
+  // .begin on a stride view over a non-simple view will give us a
+  // stride_view iterator with its _Const == false. Compare that type
+  // with an iterator on a stride view over a simple view that will give
+  // us an iterator with its _Const == true. They should *not* be the same.
+  static_assert(!std::is_same_v<decltype(ssv_iter), decltype(nsv_iter)>);
+  static_assert(!std::is_same_v<NotSimpleStrideViewIterConst, decltype(nsv_iter)>);
+  static_assert(std::is_same_v<SimpleStrideViewIterConst, decltype(ssv_iter)>);
   return true;
 }
 
 int main(int, char**) {
-  simpleview_iter_ctor_test();
   non_simple_view_iter_ctor_test();
-  static_assert(simpleview_iter_ctor_test());
+  static_assert(non_simple_view_iter_ctor_test());
   return 0;
 }
