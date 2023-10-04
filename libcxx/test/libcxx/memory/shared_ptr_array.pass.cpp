23e66713a43e80a54b0b4e37791f1dfbd136ef93
Ilya Biryukov <ibiryukov@google.com>
2023-10-04 18:14:13 +0200

    [libc++] Use correct size for deallocation of arrays in shared_ptr
    Fixes #68051.
    
    Current implementation passes the number of `_AlignedStorage` objects
    when it calls to `allocate` and the number of **bytes** on deallocate.
    This only applies to allocations that allocate control block and the
    storage together, i.e. `make_shared` and `allocate_shared`.
    
    Found by ASan.

---
+++ libcxx/test/libcxx/memory/shared_ptr_array.pass.cpp
@@ -0,0 +1,25 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+
+// UNSUPPORTED: c++03, c++11, c++14, c++17
+// REQUIRES: -fsized-deallocation
+// ADDITIONAL_COMPILE_FLAGS: -fsized-deallocation
+
+// This test will fail with ASan if the implementation passes different sizes
+// to corresponding allocation and deallocation functions.
+
+#include <memory>
+
+int main() {
+    std::allocate_shared<int64_t[]>(std::allocator<int64_t>{}, 10);
+    std::make_shared<int64_t[]>(10);
+
+    std::allocate_shared<int64_t[10]>(std::allocator<int64_t>{});
+    std::make_shared<int64_t[10]>();
+}
