909a48a4d1dd04b1841d5e6953797001a560c432
Joel E. Denny <jdenny.ornl@gmail.com>
2023-10-07 15:56:28 -0400

    [lit] Clean up internal shell parse errors with ScriptFatal
    Without this patch, the functions `executeScriptInternal` and thus
    `runOnce` in `llvm/utils/lit/lit/TestRunner.py` return either the
    tuple `(out, err, exitCode, timeoutInfo)` or a `lit.Test.Result`
    object.  They return the latter only when there's a lit internal shell
    parse error in a RUN line.  In my opinion, a more straight-forward way
    to handle exceptional cases like that is to use python exceptions.
    
    For that purpose, this patch introduces `ScriptFatal`.  Thus, this
    patch changes `executeScriptInternal` to always either return the
    tuple or raise the `ScriptFatal` exception.  It updates `runOnce` and
    `libcxx/utils/libcxx/test/format.py` to catch the exception rather
    than check for the special return type.
    
    This patch also changes `runOnce` to convert the exception to a
    `Test.UNRESOLVED` result instead of `TEST.FAIL`.  The former is the
    proper result for such a malformed test, for which a rerun (given an
    `ALLOW_RETRIES:`) serves no purpose.  There are at least two benefits
    from this change.  First, `_runShTest` no longer has to specially and
    cryptically handle this case to avoid unnecessary reruns.  Second, an
    `XFAIL:` directive no longer hides such a failure [as we saw
    previously](https://reviews.llvm.org/D154987#4501125).
    
    To facilitate the `_runShTest` change, this patch inserts the internal
    shell parse error diagnostic into the format of the test's normal
    debug output rather than suppressing the latter entirely.  That change
    is also important for [D154987](https://reviews.llvm.org/D154987),
    which proposes to reuse `ScriptFatal` for python compile errors in
    PYTHON lines or in `config.prologue`.  In that case, the diagnostic
    might follow debugging output from the test's previous RUN or PYTHON
    lines, so suppressing the normal debug output would lose information.

--- llvm/utils/lit/lit/TestRunner.py
+++ llvm/utils/lit/lit/TestRunner.py
@@ -34,6 +34,17 @@ class InternalShellError(Exception):
         self.message = message
 
 
+class ScriptFatal(Exception):
+    """
+    A script had a fatal error such that there's no point in retrying.  The
+    message has not been emitted on stdout or stderr but is instead included in
+    this exception.
+    """
+
+    def __init__(self, message):
+        super().__init__(message)
+
+
 kIsWindows = platform.system() == "Windows"
 
 # Don't use close_fds on Windows.
@@ -1009,7 +1020,8 @@ def formatOutput(title, data, limit=None):
     return out
 
 
-# Normally returns out, err, exitCode, timeoutInfo.
+# Always either returns the tuple (out, err, exitCode, timeoutInfo) or raises a
+# ScriptFatal exception.
 #
 # If debug is True (the normal lit behavior), err is empty, and out contains an
 # execution trace, including stdout and stderr shown per command executed.
@@ -1043,9 +1055,9 @@ def executeScriptInternal(test, litConfig, tmpBase, commands, cwd, debug=True):
                 ShUtil.ShParser(ln, litConfig.isWindows, test.config.pipefail).parse()
             )
         except:
-            return lit.Test.Result(
-                Test.FAIL, f"shell parser error on {dbg}: {command.lstrip()}\n"
-            )
+            raise ScriptFatal(
+                f"shell parser error on {dbg}: {command.lstrip()}\n"
+            ) from None
 
     cmd = cmds[0]
     for c in cmds[1:]:
@@ -2130,7 +2142,9 @@ def parseIntegratedTestScript(test, additional_parsers=[], require_script=True):
     return script
 
 
+# Always returns a lit.Test.Result.
 def _runShTest(test, litConfig, useExternalSh, script, tmpBase):
+    # Always returns the tuple (out, err, exitCode, timeoutInfo).
     def runOnce(execdir):
         # script is modified below (for litConfig.per_test_coverage, and for
         # %dbg expansions).  runOnce can be called multiple times, but applying
@@ -2158,12 +2172,16 @@ def _runShTest(test, litConfig, useExternalSh, script, tmpBase):
                     command = buildPdbgCommand(dbg, command)
                 scriptCopy[i] = command
 
-        if useExternalSh:
-            res = executeScript(test, litConfig, tmpBase, scriptCopy, execdir)
-        else:
-            res = executeScriptInternal(test, litConfig, tmpBase, scriptCopy, execdir)
-        if isinstance(res, lit.Test.Result):
-            return res
+        try:
+            if useExternalSh:
+                res = executeScript(test, litConfig, tmpBase, scriptCopy, execdir)
+            else:
+                res = executeScriptInternal(
+                    test, litConfig, tmpBase, scriptCopy, execdir
+                )
+        except ScriptFatal as e:
+            out = f"# " + "\n# ".join(str(e).splitlines()) + "\n"
+            return out, "", 1, None, Test.UNRESOLVED
 
         out, err, exitCode, timeoutInfo = res
         if exitCode == 0:
@@ -2183,9 +2201,6 @@ def _runShTest(test, litConfig, useExternalSh, script, tmpBase):
     attempts = test.allowed_retries + 1
     for i in range(attempts):
         res = runOnce(execdir)
-        if isinstance(res, lit.Test.Result):
-            return res
-
         out, err, exitCode, timeoutInfo, status = res
         if status != Test.FAIL:
             break
