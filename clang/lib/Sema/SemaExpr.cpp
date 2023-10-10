c8f595bc6f8304c89348a840b888fcf67cba37a2
Congcong Cai <congcongcai0907@163.com>
2023-10-10 15:32:10 +0800

    [clang]Avoid diagnose invalid consteval call for invalid function decl (#68646)
    Fixes:#68542
    It‘s meaningless to diagnose further error for invalid function
    declaration.

--- clang/lib/Sema/SemaExpr.cpp
+++ clang/lib/Sema/SemaExpr.cpp
@@ -18408,6 +18408,8 @@ static void EvaluateAndDiagnoseImmediateInvocation(
 
     assert(FD && FD->isImmediateFunction() &&
            "could not find an immediate function in this expression");
+    if (FD->isInvalidDecl())
+      return;
     SemaRef.Diag(CE->getBeginLoc(), diag::err_invalid_consteval_call)
         << FD << FD->isConsteval();
     if (auto Context =
