ef1021e88597067a8327e2adc978f51d70705373
Francesco Petrogalli <francesco.petrogalli@apple.com>
2023-10-10 17:14:16 +0200

    [TableGen] New bang operator `!repr`.
    The !repr operator represents the content of a variable or of a record
    as a string.
    
    This patch is based on code originally written by Adam Nemet, and on
    the feedback received by the reviewers in
    https://reviews.llvm.org/D157492.

--- llvm/lib/TableGen/TGLexer.cpp
+++ llvm/lib/TableGen/TGLexer.cpp
@@ -605,6 +605,7 @@ tgtok::TokKind TGLexer::LexExclaim() {
           .Case("exists", tgtok::XExists)
           .Case("tolower", tgtok::XToLower)
           .Case("toupper", tgtok::XToUpper)
+          .Case("repr", tgtok::XRepr)
           .Default(tgtok::Error);
 
   return Kind != tgtok::Error ? Kind : ReturnError(Start-1, "Unknown operator");
