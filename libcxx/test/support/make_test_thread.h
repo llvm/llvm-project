10d05d9c46aaa726f951601dc2f0f1f8da4ac083
Louis Dionne <ldionne.2@gmail.com>
2023-10-11 14:39:10 -0700

    [libc++] Introduce make_test_jthread for jthread tests
    This patch introduces the support::make_test_jthread utility which
    is basically the same as support::make_test_thread but for std::jthread.
    It allows vendors to maintain a downstream way to create threads for
    use within the test suite, which is especially useful for embedded
    platforms.

--- libcxx/test/support/make_test_thread.h
+++ libcxx/test/support/make_test_thread.h
@@ -12,12 +12,33 @@
 #include <thread>
 #include <utility>
 
+#include "test_macros.h"
+
 namespace support {
+
+// These functions are used to mock the creation of threads within the test suite.
+//
+// This provides a vendor-friendly way of making the test suite work even on platforms
+// where the standard thread constructors don't work (e.g. embedded environments where
+// creating a thread requires additional information like setting attributes).
+//
+// Vendors can keep a downstream diff in this file to create threads however they
+// need on their platform, and the majority of the test suite will work out of the
+// box. Of course, tests that exercise the standard thread constructors won't work,
+// but any other test that only creates threads as a side effect of testing should
+// work if they use the utilities in this file.
 
 template <class F, class ...Args>
 std::thread make_test_thread(F&& f, Args&& ...args) {
     return std::thread(std::forward<F>(f), std::forward<Args>(args)...);
 }
+
+#if TEST_STD_VER >= 20 && !defined(_LIBCPP_HAS_NO_EXPERIMENTAL_STOP_TOKEN)
+template <class F, class ...Args>
+std::jthread make_test_jthread(F&& f, Args&& ...args) {
+    return std::jthread(std::forward<F>(f), std::forward<Args>(args)...);
+}
+#endif
 
 } // end namespace support
 
