29f37f812f80e376725ce9005e3dfdd53b18ff30
petar-avramovic <56677889+petar-avramovic@users.noreply.github.com>
2023-10-11 09:21:08 +0200

    CycleInfo: Fix splitCriticalEdge (#68584)
    There are cases when cycle that contains both Pred and Succ is not
    found: Pred is in a smaller cycle that is contained in a larger cycle
    that also contains Succ, or Pred and Succ are in disjointed innermost
    cycles but there is a larger cycle that contains both.
    Add efficient helper function that finds innermost cycle that contains
    both Pred and Succ if it exists.

--- llvm/include/llvm/ADT/GenericCycleInfo.h
+++ llvm/include/llvm/ADT/GenericCycleInfo.h
@@ -248,6 +248,12 @@ private:
   /// the subtree.
   void moveTopLevelCycleToNewParent(CycleT *NewParent, CycleT *Child);
 
+  /// Assumes that \p Cycle is the innermost cycle containing \p Block.
+  /// \p Block will be appended to \p Cycle and all of its parent cycles.
+  /// \p Block will be added to BlockMap with \p Cycle and
+  /// BlockMapTopLevel with \p Cycle's top level parent cycle.
+  void addBlockToCycle(BlockT *Block, CycleT *Cycle);
+
 public:
   GenericCycleInfo() = default;
   GenericCycleInfo(GenericCycleInfo &&) = default;
@@ -261,6 +267,7 @@ public:
   const ContextT &getSSAContext() const { return Context; }
 
   CycleT *getCycle(const BlockT *Block) const;
+  CycleT *getSmallestCommonCycle(CycleT *A, CycleT *B) const;
   unsigned getCycleDepth(const BlockT *Block) const;
   CycleT *getTopLevelParentCycle(BlockT *Block);
 
