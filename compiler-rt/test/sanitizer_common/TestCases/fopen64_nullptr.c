eb37a4ed8fd3193471b769d2f72ac29a019fa299
Wu, Yingcong <yingcong.wu@intel.com>
2023-10-10 22:26:26 -0700

    add run to test

--- compiler-rt/test/sanitizer_common/TestCases/fopen64_nullptr.c
+++ compiler-rt/test/sanitizer_common/TestCases/fopen64_nullptr.c
@@ -1,6 +1,6 @@
 // Check that fopen64(NULL, "r") is ok.
-
 // REQUIRES: linux
+// RUN: %clang -O2 %s -o %t && %run %t
 #include <stdio.h>
 FILE *fopen64(const char *filename, const char *mode);
 const char *fn = NULL;
