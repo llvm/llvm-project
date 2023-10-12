9e058aeca6e0dff6d092babaab705bf443f451b3
max <maksim.levental@gmail.com>
2023-10-11 23:32:14 -0500

    [mlir][python] remove mixins

--- mlir/python/mlir/dialects/bufferization.py
+++ mlir/python/mlir/dialects/bufferization.py
@@ -3,4 +3,47 @@
 #  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 from ._bufferization_ops_gen import *
+from ._bufferization_ops_gen import _Dialect
 from ._bufferization_enum_gen import *
+
+try:
+    from typing import Sequence, Union
+    from ..ir import *
+    from ._ods_common import get_default_loc_context, _cext as _ods_cext
+
+    from typing import Any, List, Union
+except ImportError as e:
+    raise RuntimeError("Error loading imports from extension module") from e
+
+_AllocTensorOp = AllocTensorOp
+
+
+@_ods_cext.register_operation(_Dialect, replace=True)
+class AllocTensorOp(_AllocTensorOp):
+    """Extends the bufferization.alloc_tensor op."""
+
+    def __init__(
+        self,
+        tensor_type: Type,
+        dynamic_sizes: Sequence[Value],
+        copy: Value,
+        size_hint: Value,
+        escape: BoolAttr,
+        *,
+        loc=None,
+        ip=None,
+    ):
+        """Constructs an `alloc_tensor` with static and/or dynamic sizes."""
+        context = get_default_loc_context(loc)
+        attributes = {}
+        if escape:
+            attributes["escape"] = escape
+        super(_AllocTensorOp, self).__init__(
+            self.build_generic(
+                results=[tensor_type],
+                operands=[dynamic_sizes, copy, size_hint],
+                attributes=attributes,
+                loc=loc,
+                ip=ip,
+            )
+        )
--- mlir/python/mlir/dialects/builtin.py
+++ mlir/python/mlir/dialects/builtin.py
@@ -3,3 +3,27 @@
 #  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 from ._builtin_ops_gen import *
+from ._builtin_ops_gen import _Dialect
+
+try:
+    from ..ir import *
+    from ._ods_common import _cext as _ods_cext
+except ImportError as e:
+    raise RuntimeError("Error loading imports from extension module") from e
+
+_ModuleOp = ModuleOp
+
+
+@_ods_cext.register_operation(_Dialect, replace=True)
+class ModuleOp(_ModuleOp):
+    """Specialization for the module op class."""
+
+    def __init__(self, *, loc=None, ip=None):
+        super(_ModuleOp, self).__init__(
+            self.build_generic(results=[], operands=[], loc=loc, ip=ip)
+        )
+        body = self.regions[0].blocks.append()
+
+    @property
+    def body(self):
+        return self.regions[0].blocks[0]
