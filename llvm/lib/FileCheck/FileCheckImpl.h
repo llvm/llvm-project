684c1fce2bf710e6f452891a063b355a0f618bf5
Tom Stellard <tstellar@redhat.com>
2023-10-13 05:17:21 +0000

    XXX: manual unittests fixes

--- llvm/lib/FileCheck/FileCheckImpl.h
+++ llvm/lib/FileCheck/FileCheckImpl.h
@@ -277,7 +277,7 @@ public:
 
 /// Class representing the use of a numeric variable in the AST of an
 /// expression.
-class NumericVariableUse : public ExpressionAST {
+class LLVM_CLASS_ABI NumericVariableUse : public ExpressionAST {
 private:
   /// Pointer to the class instance for the variable this use is about.
   NumericVariable *Variable;
@@ -371,7 +371,7 @@ public:
   virtual Expected<std::string> getResult() const = 0;
 };
 
-LLVM_CLASS_ABI class StringSubstitution : public Substitution {
+class LLVM_CLASS_ABI StringSubstitution : public Substitution {
 public:
   StringSubstitution(FileCheckPatternContext *Context, StringRef VarName,
                      size_t InsertIdx)
@@ -382,7 +382,7 @@ public:
   LLVM_FUNC_ABI Expected<std::string> getResult() const override;
 };
 
-LLVM_CLASS_ABI class NumericSubstitution : public Substitution {
+class LLVM_CLASS_ABI NumericSubstitution : public Substitution {
 private:
   /// Pointer to the class representing the expression whose value is to be
   /// substituted.
@@ -520,7 +520,7 @@ public:
 
 class NotFoundError : public ErrorInfo<NotFoundError> {
 public:
-  static char ID;
+  LLVM_FUNC_ABI static char ID;
 
   std::error_code convertToErrorCode() const override {
     return inconvertibleErrorCode();
@@ -644,7 +644,7 @@ public:
   FileCheckPatternContext *getContext() const { return Context; }
 
   /// \returns whether \p C is a valid first character for a variable name.
-  static bool isValidVarNameStart(char C);
+  LLVM_FUNC_ABI static bool isValidVarNameStart(char C);
 
   /// Parsing information about a variable.
   struct VariableProperties {
@@ -657,7 +657,7 @@ public:
   /// is the name of a pseudo variable, or an error holding a diagnostic
   /// against \p SM if parsing fail. If parsing was successful, also strips
   /// \p Str from the variable name.
-  static Expected<VariableProperties> parseVariable(StringRef &Str,
+  LLVM_FUNC_ABI static Expected<VariableProperties> parseVariable(StringRef &Str,
                                                     const SourceMgr &SM);
   /// Parses \p Expr for a numeric substitution block at line \p LineNumber,
   /// or before input is parsed if \p LineNumber is None. Parameter
