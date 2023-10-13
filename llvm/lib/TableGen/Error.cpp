256292212574f2647aa3a2fb7098727378e7bc47
Francesco Petrogalli <francesco.petrogalli@apple.com>
2023-10-13 12:50:18 +0200

    Enforce dumpMessage to require a StringInit to print.

--- llvm/lib/TableGen/Error.cpp
+++ llvm/lib/TableGen/Error.cpp
@@ -172,8 +172,9 @@ void CheckAssert(SMLoc Loc, Init *Condition, Init *Message) {
 
 // Dump a message to stderr.
 void dumpMessage(SMLoc Loc, Init *Message) {
-  if (auto *MessageInit = dyn_cast<StringInit>(Message))
-    PrintNote(Loc, MessageInit->getValue());
+  auto *MessageInit = dyn_cast<StringInit>(Message);
+  assert(MessageInit && "no debug message to print");
+  PrintNote(Loc, MessageInit->getValue());
 }
 
 } // end namespace llvm
