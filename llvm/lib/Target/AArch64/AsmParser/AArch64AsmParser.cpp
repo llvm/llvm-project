5ac47544b663a27cbf8002d816e77e9a60865214
CarolineConcatto <51754594+CarolineConcatto@users.noreply.github.com>
2023-10-13 14:25:42 +0100

    [AArch64][NFC] Refactor NEON, SVE and SME classes and multiclasses fo… (#68800)
    …r the assembly disassembly
    
    This NFC patch refactors the assembly/disassembly class and multiclass
    in the AArch64 backend to receive a new 2023/09 AArch64[1] ISA release.
    The encoding for the 2023 instructions re-uses encoding blocks from
    previous assembly/disassembly instructions.
    The refactoring makes the class and multiclass for assembly/disassembly
    generic so it can be used to describe the instructions for the new ISA.
    
    [1]https://developer.arm.com/documentation/ddi0602/2023-09

--- llvm/lib/Target/AArch64/AsmParser/AArch64AsmParser.cpp
+++ llvm/lib/Target/AArch64/AsmParser/AArch64AsmParser.cpp
@@ -4537,6 +4537,8 @@ ParseStatus AArch64AsmParser::tryParseZTOperand(OperandVector &Operands) {
 
   // Check if register is followed by an index
   if (parseOptionalToken(AsmToken::LBrac)) {
+    Operands.push_back(
+        AArch64Operand::CreateToken("[", getLoc(), getContext()));
     const MCExpr *ImmVal;
     if (getParser().parseExpression(ImmVal))
       return ParseStatus::NoMatch;
@@ -4549,6 +4551,8 @@ ParseStatus AArch64AsmParser::tryParseZTOperand(OperandVector &Operands) {
     Operands.push_back(AArch64Operand::CreateImm(
         MCConstantExpr::create(MCE->getValue(), getContext()), StartLoc,
         getLoc(), getContext()));
+    Operands.push_back(
+        AArch64Operand::CreateToken("]", getLoc(), getContext()));
   }
 
   return ParseStatus::Success;
