1985b95789af6204c0dcede4eb996884fd0a535e
Fangrui Song <i@maskray.me>
2023-10-12 22:58:42 -0700

    [ELF] Restore the --call-graph-profile-sort=hfsort default before #68638
    The high time complexity of cache-directed sort is a real issue and is not
    appropriate as the default, at least for now
    (https://github.com/llvm/llvm-project/pull/68638#issuecomment-1760918891).

--- lld/ELF/Driver.cpp
+++ lld/ELF/Driver.cpp
@@ -1095,7 +1095,7 @@ static void ltoValidateAllVtablesHaveTypeInfos(opt::InputArgList &args) {
 }
 
 static CGProfileSortKind getCGProfileSortKind(opt::InputArgList &args) {
-  StringRef s = args.getLastArgValue(OPT_call_graph_profile_sort, "cdsort");
+  StringRef s = args.getLastArgValue(OPT_call_graph_profile_sort, "hfsort");
   if (s == "hfsort")
     return CGProfileSortKind::Hfsort;
   if (s == "cdsort")
