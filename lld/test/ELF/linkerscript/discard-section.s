de44f08d8665e07ed0f2cfc898c7592c7a8f91cc
Fangrui Song <i@maskray.me>
2023-10-14 14:59:10 -0700

    [ELF][test] Test relocations referencing weak symbol, which is defined relative to a section discarded by /DISCARD/

--- lld/test/ELF/linkerscript/discard-section.s
+++ lld/test/ELF/linkerscript/discard-section.s
@@ -1,23 +1,36 @@
 # REQUIRES: x86
 ## Test relocations referencing symbols defined relative to sections discarded by /DISCARD/.
 
-# RUN: llvm-mc -filetype=obj -triple=x86_64 %s -o %t.o
-# RUN: echo "SECTIONS { /DISCARD/ : { *(.aaa*) } }" > %t.lds
-# RUN: ld.lld -T %t.lds %t.o -z undefs -o /dev/null 2>&1 | count 0
-# RUN: ld.lld -T %t.lds %t.o -o /dev/null 2>&1 | count 0
-# RUN: ld.lld -r -T %t.lds %t.o -o /dev/null 2>&1 | count 0
+# RUN: rm -rf %t && split-file %s %t && cd %t
+# RUN: llvm-mc -filetype=obj -triple=x86_64 a.s -o a.o
+# RUN: llvm-mc -filetype=obj -triple=x86_64 b.s -o b.o
+# RUN: ld.lld -T a.lds a.o b.o -z undefs -o /dev/null 2>&1 | count 0
+# RUN: ld.lld -T a.lds a.o b.o -o /dev/null 2>&1 | count 0
+# RUN: ld.lld -r -T a.lds a.o b.o -o /dev/null 2>&1 | count 0
 
+#--- a.s
 .globl _start
 _start:
 
 .section .aaa,"a"
-.globl global
-.weak weak
+.globl global, weakref1
+.weak weak, weakref2
 global:
 weak:
+weakref1:
+weakref2:
   .quad 0
 
-.section .zzz,"a"
+.section .bbb,"aw"
   .quad .aaa
+
+#--- b.s
+.weak weakref1, weakref2
+.section .data,"aw"
   .quad global
   .quad weak
+  .quad weakref1
+  .quad weakref2
+
+#--- a.lds
+SECTIONS { /DISCARD/ : { *(.aaa) } }
