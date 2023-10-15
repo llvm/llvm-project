4980cbfc04ee9475f01695d4fe9de7952b8ce752
Congcong Cai <congcongcai0907@163.com>
2023-10-16 07:57:23 +0800

    update test

--- clang-tools-extra/test/clang-tidy/checkers/modernize/loop-convert-basic.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/modernize/loop-convert-basic.cpp
@@ -939,15 +939,18 @@ void fundamentalTypesTest() {
   // CHECK-FIXES: for (double Double : Doubles)
 }
 
-template <unsigned int p> void test() {
-  unsigned int test[3][p];
-  // Initialize to zero
-  for (unsigned int i = 0; i < p; ++i)
-    for (unsigned int j = 0; j < 3; ++j)
-      test[j][i] = 0;
+template <unsigned  p> void _dependenceArrayTest() {
+  unsigned test[3][p];
+  for (unsigned i = 0; i < p; ++i)
+    for (unsigned j = 0; j < 3; ++j)
+      printf("%d", test[j][i]);
   // CHECK-MESSAGES: :[[@LINE-2]]:5: warning: use range-based for loop instead
   // CHECK-FIXES: (auto & j : test)
-  // CHECK-FIXES: j[i] = 0;
+  // CHECK-FIXES: printf("%d", j[i]);
+}
+void dependenceArrayTest() {
+  _dependenceArrayTest<1>();
+  _dependenceArrayTest<2>();
 }
 
 } // namespace PseudoArray
