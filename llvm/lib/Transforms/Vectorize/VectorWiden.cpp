2a6a2abad411df58b3a524d2ec32d56f414fffac
Dinar Temirbulatov <dinar.temirbulatov@arm.com>
2023-10-16 06:29:23 +0000

    Add support for testing with override TTI::considerToWiden().

--- llvm/lib/Transforms/Vectorize/VectorWiden.cpp
+++ llvm/lib/Transforms/Vectorize/VectorWiden.cpp
@@ -1,4 +1,4 @@
-///==--- VectorWiden.cpp - Combining Vector Operations to wider types ----==//
+//===--- VectorWiden.cpp - Combining Vector Operations to wider types ----===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -51,6 +51,10 @@ static cl::opt<unsigned>
                     cl::desc("Maximum distance between instructions to"
                              "consider to widen"));
 
+static cl::opt<bool> OverrideTargetConsiderToWiden(
+    "vw-override-target-consider-to-widen", cl::init(false), cl::Hidden,
+    cl::desc("Ignore any target information while considoring to widen"));
+
 namespace {
 class VectorWiden {
 public:
@@ -94,10 +98,14 @@ private:
 void VectorWiden::widenCastInst(ArrayRef<Instruction *> IL) {
   Instruction *I = IL[0];
   Instruction *I1 = IL[1];
-  VectorType *RetOrigType = cast<VectorType>(I->getType());
-  VectorType *OrigType = cast<VectorType>(I->getOperand(0)->getType());
-  VectorType *RetType = VectorType::getDoubleElementsVectorType(RetOrigType);
-  VectorType *OpType = VectorType::getDoubleElementsVectorType(OrigType);
+  auto *RetOrigType = cast<VectorType>(I->getType());
+  auto *OrigType = cast<VectorType>(I->getOperand(0)->getType());
+  auto *RetType = VectorType::getDoubleElementsVectorType(RetOrigType);
+  auto *OpType = VectorType::getDoubleElementsVectorType(OrigType);
+  unsigned Offset =
+      dyn_cast<ScalableVectorType>(OrigType)
+          ? (cast<ScalableVectorType>(OrigType))->getMinNumElements()
+          : (cast<FixedVectorType>(OrigType))->getNumElements();
   Value *WideVec = UndefValue::get(OpType);
   Builder.SetInsertPoint(I);
   Function *InsertIntr = llvm::Intrinsic::getDeclaration(
@@ -105,7 +113,7 @@ void VectorWiden::widenCastInst(ArrayRef<Instruction *> IL) {
   Value *Insert1 = Builder.CreateCall(
       InsertIntr, {WideVec, I->getOperand(0), Builder.getInt64(0)});
   Value *Insert2 = Builder.CreateCall(
-      InsertIntr, {Insert1, I1->getOperand(0), Builder.getInt64(4)});
+      InsertIntr, {Insert1, I1->getOperand(0), Builder.getInt64(Offset)});
   Value *ResCast = Builder.CreateCast(Instruction::CastOps(I->getOpcode()),
                                       Insert2, RetType);
   Function *ExtractIntr = llvm::Intrinsic::getDeclaration(
@@ -117,7 +125,7 @@ void VectorWiden::widenCastInst(ArrayRef<Instruction *> IL) {
   }
   if (!I1->users().empty()) {
     Value *Res2 =
-        Builder.CreateCall(ExtractIntr, {ResCast, Builder.getInt64(4)});
+        Builder.CreateCall(ExtractIntr, {ResCast, Builder.getInt64(Offset)});
     I1->replaceAllUsesWith(Res2);
   }
 }
@@ -131,20 +139,26 @@ void VectorWiden::widenBinaryOperator(ArrayRef<Instruction *> IL) {
   Value *YHi = I->getOperand(1);
   Value *YLo = I1->getOperand(1);
 
-  VectorType *RetOrigType = cast<VectorType>(I->getType());
-  VectorType *OrigType = cast<VectorType>(I->getOperand(0)->getType());
-  VectorType *RetType = VectorType::getDoubleElementsVectorType(RetOrigType);
-  VectorType *OpType = VectorType::getDoubleElementsVectorType(OrigType);
+  auto *RetOrigType = cast<VectorType>(I->getType());
+  auto *OrigType = cast<VectorType>(I->getOperand(0)->getType());
+  auto *RetType = VectorType::getDoubleElementsVectorType(RetOrigType);
+  auto *OpType = VectorType::getDoubleElementsVectorType(OrigType);
+  unsigned Offset =
+      dyn_cast<ScalableVectorType>(OrigType)
+          ? (cast<ScalableVectorType>(OrigType))->getMinNumElements()
+          : (cast<FixedVectorType>(OrigType))->getNumElements();
   Value *WideVec = UndefValue::get(OpType);
   Builder.SetInsertPoint(I);
   Function *InsertIntr = llvm::Intrinsic::getDeclaration(
       F.getParent(), Intrinsic::vector_insert, {OpType, OrigType});
   Value *X1 =
       Builder.CreateCall(InsertIntr, {WideVec, XLo, Builder.getInt64(0)});
-  Value *X2 = Builder.CreateCall(InsertIntr, {X1, XHi, Builder.getInt64(4)});
+  Value *X2 =
+      Builder.CreateCall(InsertIntr, {X1, XHi, Builder.getInt64(Offset)});
   Value *Y1 =
       Builder.CreateCall(InsertIntr, {WideVec, YLo, Builder.getInt64(0)});
-  Value *Y2 = Builder.CreateCall(InsertIntr, {Y1, YHi, Builder.getInt64(4)});
+  Value *Y2 =
+      Builder.CreateCall(InsertIntr, {Y1, YHi, Builder.getInt64(Offset)});
   Value *ResBinOp =
       Builder.CreateBinOp((Instruction::BinaryOps)I->getOpcode(), X2, Y2);
   ValueList VL;
@@ -155,7 +169,7 @@ void VectorWiden::widenBinaryOperator(ArrayRef<Instruction *> IL) {
       F.getParent(), Intrinsic::vector_extract, {RetOrigType, RetType});
   if (!I->users().empty()) {
     Value *Res =
-        Builder.CreateCall(ExtractIntr, {ResBinOp, Builder.getInt64(4)});
+        Builder.CreateCall(ExtractIntr, {ResBinOp, Builder.getInt64(Offset)});
     I->replaceAllUsesWith(Res);
   }
   if (!I1->users().empty()) {
@@ -167,7 +181,10 @@ void VectorWiden::widenBinaryOperator(ArrayRef<Instruction *> IL) {
 
 bool VectorWiden::canWidenNode(ArrayRef<Instruction *> IL,
                                LLVMContext &Context) {
-  if (!TTI.considerToWiden(Context, IL))
+  if (!OverrideTargetConsiderToWiden && !TTI.considerToWiden(Context, IL))
+    return false;
+
+  if (!dyn_cast<VectorType>(IL[0]->getType()))
     return false;
 
   for (int X = 0, E = IL.size(); X < E; X++) {
@@ -176,7 +193,6 @@ bool VectorWiden::canWidenNode(ArrayRef<Instruction *> IL,
         continue;
       if ((IL[X] == IL[Y]) || (IL[X]->getOpcode() != IL[Y]->getOpcode()) ||
           // Ignore if any live in a diffrent Basic Block
-          (IL[X]->getParent() != IL[Y]->getParent()) ||
           (IL[X]->getType() != IL[Y]->getType()) ||
           (IL[X]->getOperand(0)->getType() !=
            IL[Y]->getOperand(0)->getType()) ||
@@ -190,44 +206,53 @@ bool VectorWiden::canWidenNode(ArrayRef<Instruction *> IL,
     if (isDeleted(IL[X]) || !IL[X]->hasOneUse())
       return false;
     if (IL[0]->getParent() == IL[X]->user_back()->getParent() &&
-        DT.dominates(IL[X]->user_back(), IL[0]))
+        IL[X]->user_back()->comesBefore(IL[0]))
       return false;
   }
   return true;
 }
 
 bool VectorWiden::widenNode(ArrayRef<Instruction *> IL, LLVMContext &Context) {
-  LLVM_DEBUG(dbgs() << "VW: widenNode: " << *IL[0] << " " << *IL[1] << "\n");
-  assert(IL.size() == 2 && "Incorrect instructions list to widen.");
+  // Currently, this pass supports only two operations to widen to
+  // a single operation.
+  if (IL.size() != 2)
+    return false;
   if (!canWidenNode(IL, Context))
     return false;
+
+  unsigned Opcode = IL[0]->getOpcode();
+
   if (dyn_cast<CastInst>(IL[0])) {
-    VectorType *RetOrigType = cast<VectorType>(IL[0]->getType());
-    VectorType *OrigType = cast<VectorType>(IL[0]->getOperand(0)->getType());
-    InstructionCost Cost =
-        getOpCost(Instruction::FPTrunc, RetOrigType, OrigType, IL[0]);
-    VectorType *RetType = VectorType::getDoubleElementsVectorType(RetOrigType);
-    VectorType *OpType = VectorType::getDoubleElementsVectorType(OrigType);
-    InstructionCost CostNew =
-        getOpCost(Instruction::FPTrunc, RetType, OpType, IL[0]);
-    if (2 * Cost < CostNew)
-      return false;
-    LLVM_DEBUG(dbgs() << "VW: Decided to widen CastInst, safe to merge : "
-                      << *IL[0] << " with  " << *IL[1] << "\n");
+    if (!OverrideTargetConsiderToWiden) {
+      auto *OrigType = cast<VectorType>(IL[0]->getOperand(0)->getType());
+      auto *RetOrigType = cast<VectorType>(IL[0]->getType());
+      InstructionCost Cost = getOpCost(Opcode, RetOrigType, OrigType, IL[0]);
+      auto *RetType = VectorType::getDoubleElementsVectorType(RetOrigType);
+      auto *OpType = VectorType::getDoubleElementsVectorType(OrigType);
+      InstructionCost CostNew = getOpCost(Opcode, RetType, OpType, IL[0]);
+      if (2 * Cost < CostNew)
+        return false;
+    }
+    LLVM_DEBUG(
+        dbgs()
+        << "VW: Decided to widen CastInst, safe to merge node starting with "
+        << *IL[0] << "\n");
     widenCastInst(IL);
     return true;
   }
   if (dyn_cast<BinaryOperator>(IL[0])) {
-    VectorType *OrigType = cast<VectorType>(IL[0]->getOperand(0)->getType());
-    VectorType *OpType = VectorType::getDoubleElementsVectorType(OrigType);
-    InstructionCost Cost =
-        getOpCost(Instruction::Add, OrigType, OrigType, IL[0]);
-    InstructionCost CostNew =
-        getOpCost(Instruction::Add, OpType, OpType, IL[0]);
-    if (2 * Cost < CostNew)
-      return false;
-    LLVM_DEBUG(dbgs() << "VW: Decided to widen BinaryOp, safe to merge : "
-                      << *IL[0] << " with  " << *IL[1] << "\n");
+    if (!OverrideTargetConsiderToWiden) {
+      auto *OrigType = cast<VectorType>(IL[0]->getOperand(0)->getType());
+      auto *OpType = VectorType::getDoubleElementsVectorType(OrigType);
+      InstructionCost Cost = getOpCost(Opcode, OrigType, OrigType, IL[0]);
+      InstructionCost CostNew = getOpCost(Opcode, OpType, OpType, IL[0]);
+      if (2 * Cost < CostNew)
+        return false;
+    }
+    LLVM_DEBUG(
+        dbgs()
+        << "VW: Decided to widen BinaryOp, safe to merge node starting with "
+        << *IL[0] << "\n");
     widenBinaryOperator(IL);
     return true;
   }
@@ -259,11 +284,26 @@ InstructionCost VectorWiden::getOpCost(unsigned Opcode, Type *To, Type *From,
   return Cost;
 }
 
+static bool isOperationSupported(unsigned Opcode) {
+  if (Opcode == Instruction::FPToUI || Opcode == Instruction::FPToSI ||
+      Opcode == Instruction::FPExt || Opcode == Instruction::SIToFP ||
+      Opcode == Instruction::UIToFP || Opcode == Instruction::Trunc ||
+      Opcode == Instruction::FPTrunc || Opcode == Instruction::BitCast)
+    return true;
+  return false;
+}
+
 bool VectorWiden::processBB(BasicBlock &BB, LLVMContext &Context) {
-  DenseMap<unsigned, std::pair<InstrList, unsigned>> Operations;
-  unsigned Counter = 0;
+  struct Operation {
+    // Distance between the first operation in the list of operations
+    // and the last instruction in the current basic block.
+    unsigned Position;
+    InstrList Ops;
+  };
+  DenseMap<unsigned, Operation> Operations;
+  unsigned InstrDistance = 0;
   for (BasicBlock::reverse_iterator IP(BB.rbegin()); IP != BB.rend();
-       *IP++, ++Counter) {
+       *IP++, ++InstrDistance) {
     Instruction *I = &*IP;
     unsigned OpFound = 0;
 
@@ -271,41 +311,45 @@ bool VectorWiden::processBB(BasicBlock &BB, LLVMContext &Context) {
       continue;
 
     unsigned Opcode = I->getOpcode();
-    if ((dyn_cast<BinaryOperator>(I) && I->getNumOperands() == 2) ||
-        (dyn_cast<CastInst>(I) &&
-         (Opcode == Instruction::ZExt || Opcode == Instruction::SExt ||
-          Opcode == Instruction::FPToUI || Opcode == Instruction::FPToSI ||
-          Opcode == Instruction::FPExt || Opcode == Instruction::SIToFP ||
-          Opcode == Instruction::UIToFP || Opcode == Instruction::Trunc ||
-          Opcode == Instruction::FPTrunc || Opcode == Instruction::BitCast) &&
-         I->getNumOperands() == 1)) {
-      if (Operations.find(I->getOpcode()) != Operations.end()) {
-        auto *OpRec = &Operations[I->getOpcode()];
-        // If instructions are too apart then remove old instrction
-        // and reset position to this instruction.
-        if (Counter - Operations[I->getOpcode()].second > MaxInstDistance) {
-          OpRec->second = Counter;
-          OpRec->first.clear();
-          OpRec->first.push_back(I);
-        } else {
-          OpRec->first.push_back(I);
-          OpFound = I->getOpcode();
+    if (!dyn_cast<BinaryOperator>(I) && !isOperationSupported(Opcode))
+      continue;
+
+    if (Operations.contains(I->getOpcode())) {
+      Operation *OpRec = &Operations[I->getOpcode()];
+      // If instructions are too apart then remove old instrction
+      // and reset position to this instruction.
+      if (InstrDistance - OpRec->Position > MaxInstDistance) {
+        OpRec->Ops.erase(OpRec->Ops.begin());
+
+        for (InstrList::iterator It = OpRec->Ops.begin();
+             It != OpRec->Ops.end(); ++It) {
+          Instruction *Instr = *It;
+          if (std::distance(Instr, &(*BB.end())) > MaxInstDistance)
+            OpRec->Ops.erase(It);
         }
+        // If no operations left in the list set position
+        // to the current.
+        if (!OpRec->Ops.size())
+          OpRec->Position = InstrDistance;
+        OpRec->Ops.push_back(I);
       } else {
-        Operations[I->getOpcode()] = {{I}, Counter};
+        OpRec->Ops.push_back(I);
+        OpFound = I->getOpcode();
       }
+    } else {
+      Operations[I->getOpcode()] = {InstrDistance, {I}};
     }
 
-    if (OpFound && Operations.find(OpFound) != Operations.end()) {
+    if (OpFound && Operations.contains(OpFound)) {
       auto *OpRec = &Operations[OpFound];
-      for (Instruction *Op : OpRec->first)
-        LLVM_DEBUG(dbgs() << "VW Op to check : " << *Op << "\n");
-      if (!widenNode(OpRec->first, Context)) {
-        LLVM_DEBUG(dbgs() << "VW Unable to consturct the tree.\n");
-        OpRec->first.erase(OpRec->first.begin());
-        OpRec->second = Counter;
+      for (Instruction *Op : OpRec->Ops)
+        LLVM_DEBUG(dbgs() << "VW: operation to check : " << *Op << "\n");
+      if (!widenNode(OpRec->Ops, Context)) {
+        LLVM_DEBUG(dbgs() << "VW: Unable to construct the tree.\n");
+        OpRec->Ops.erase(OpRec->Ops.begin());
+        OpRec->Position = InstrDistance;
       } else {
-        for (Instruction *Instr : OpRec->first)
+        for (Instruction *Instr : OpRec->Ops)
           eraseInstruction(Instr);
         return true;
       }
@@ -318,9 +362,9 @@ bool VectorWiden::run() {
   bool Changed = false;
   LLVMContext &Context = F.getContext();
 
-  LLVM_DEBUG(dbgs() << "VW Function:" << F.getName() << "\n");
+  LLVM_DEBUG(dbgs() << "VW: Function:" << F.getName() << "\n");
   for (BasicBlock &BB : F) {
-    LLVM_DEBUG(dbgs() << "VW BB:" << BB.getName() << "\n");
+    LLVM_DEBUG(dbgs() << "VW: BB:" << BB.getName() << "\n");
 
     while (processBB(BB, Context))
       Changed = true;
