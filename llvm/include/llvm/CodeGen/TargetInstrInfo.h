f07b965a98fe8229a2e2d9c09de28d4e8a438c5f
Bill Wendling <5993918+bwendling@users.noreply.github.com>
2023-10-17 12:03:26 -0700

    [CodeGen] Avoid potential sideeffects from XOR (#67193)
    XOR may change flag values (e.g. for X86 gprs). In the case where that's
    not desirable, specify that buildClearRegister() should use MOV instead.

--- llvm/include/llvm/CodeGen/TargetInstrInfo.h
+++ llvm/include/llvm/CodeGen/TargetInstrInfo.h
@@ -2093,10 +2093,13 @@ public:
         "Target didn't implement TargetInstrInfo::insertOutlinedCall!");
   }
 
-  /// Insert an architecture-specific instruction to clear a register.
+  /// Insert an architecture-specific instruction to clear a register. If you
+  /// need to avoid sideeffects (e.g. avoid XOR on x86, which sets EFLAGS), set
+  /// \p AllowSideEffects to \p false.
   virtual void buildClearRegister(Register Reg, MachineBasicBlock &MBB,
                                   MachineBasicBlock::iterator Iter,
-                                  DebugLoc &DL) const {
+                                  DebugLoc &DL,
+                                  bool AllowSideEffects = true) const {
     llvm_unreachable(
         "Target didn't implement TargetInstrInfo::buildClearRegister!");
   }
