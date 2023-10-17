d6bea024f109f73798387d743e035c0defe8b917
刘雨培 <liuyupei951018@hotmail.com>
2023-10-17 13:23:28 +0800

    [Clang] Fix dependence handling of nttp for variable templates (#69075)
    The dependence of a template argument is not only determined by the
    argument itself, but also by the type of the template parameter:
    
    > Furthermore, a non-type
    [template-argument](https://eel.is/c++draft/temp.names#nt:template-argument)
    is dependent if the corresponding non-type
    [template-parameter](https://eel.is/c++draft/temp.param#nt:template-parameter)
    is of reference or pointer type and the
    [template-argument](https://eel.is/c++draft/temp.names#nt:template-argument)
    designates or points to a member of the current instantiation or a
    member of a dependent
    type[.](https://eel.is/c++draft/temp.dep#temp-3.sentence-1)
    
    For example:
    
    ```cpp
    struct A{};
    
    template <const A& T>
    const A JoinStringViews = T;
    
    template <int V>
    class Builder {
    public:
        static constexpr A Equal{};
        static constexpr auto Val = JoinStringViews<Equal>;
    };
    ```
    
    The constant expression `Equal` is not dependent, but because the type
    of the template parameter is a reference type and `Equal` is a member of
    the current instantiation, the template argument of
    `JoinStringViews<Equal>` is actually dependent, which makes
    `JoinStringViews<Equal>` dependent.
    
    When a template-id of a variable template is dependent,
    `CheckVarTemplateId` will return an `UnresolvedLookupExpr`, but
    `UnresolvedLookupExpr` calculates dependence by template arguments only
    (the `ConstantExpr` `Equal` here), which is not dependent. This causes
    type deduction to think that `JoinStringViews<Equal>` is `OverloadTy`
    and treat it as a function template, which is clearly wrong.
    
    This PR adds a `KnownDependent` parameter to the constructor of
    `UnresolvedLookupExpr`. After canonicalization, if `CanonicalConverted`
    contains any dependent argument, `KnownDependent` is set to `true`. This
    fixes the dependence calculation of `UnresolvedLookupExpr` for dependent
    variable templates.
    
    Fixes #65153 .

--- clang/lib/AST/ASTImporter.cpp
+++ clang/lib/AST/ASTImporter.cpp
@@ -8395,10 +8395,13 @@ ASTNodeImporter::VisitUnresolvedLookupExpr(UnresolvedLookupExpr *E) {
     if (!ToTemplateKeywordLocOrErr)
       return ToTemplateKeywordLocOrErr.takeError();
 
+    const bool KnownDependent =
+        (E->getDependence() & ExprDependence::TypeValue) ==
+        ExprDependence::TypeValue;
     return UnresolvedLookupExpr::Create(
         Importer.getToContext(), *ToNamingClassOrErr, *ToQualifierLocOrErr,
         *ToTemplateKeywordLocOrErr, ToNameInfo, E->requiresADL(), &ToTAInfo,
-        ToDecls.begin(), ToDecls.end());
+        ToDecls.begin(), ToDecls.end(), KnownDependent);
   }
 
   return UnresolvedLookupExpr::Create(
