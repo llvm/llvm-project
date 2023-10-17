620c734c75249106456b7d4a88fb06fac35900d6
Amirreza Ashouri <ar.ashouri999@gmail.com>
2023-10-17 13:08:12 +0330

    [libc++] Eliminate extra allocations from `std::move(oss).str()` (#67294)
    Add test coverage for the new behaviors, especially to verify that the
    returned string uses the correct allocator.
    Fixes https://github.com/llvm/llvm-project/issues/64644
    
    Migrated from https://reviews.llvm.org/D157776 — @philnik777  @pfusik 
    @ldionne @mordante 
     please take another look!

--- libcxx/test/support/test_allocator.h
+++ libcxx/test/support/test_allocator.h
@@ -475,4 +475,29 @@ TEST_CONSTEXPR inline bool operator!=(limited_allocator<T, N> const& LHS, limite
   return !(LHS == RHS);
 }
 
+// Track the "provenance" of this allocator instance: how many times was
+// select_on_container_copy_construction called in order to produce it?
+//
+template <class T>
+struct SocccAllocator {
+  using value_type = T;
+
+  int count_ = 0;
+  explicit SocccAllocator(int i) : count_(i) {}
+
+  template <class U>
+  SocccAllocator(const SocccAllocator<U>& a) : count_(a.count_) {}
+
+  T* allocate(std::size_t n) { return std::allocator<T>().allocate(n); }
+  void deallocate(T* p, std::size_t n) { std::allocator<T>().deallocate(p, n); }
+
+  SocccAllocator select_on_container_copy_construction() const { return SocccAllocator(count_ + 1); }
+
+  bool operator==(const SocccAllocator&) const { return true; }
+
+  using propagate_on_container_copy_assignment = std::false_type;
+  using propagate_on_container_move_assignment = std::false_type;
+  using propagate_on_container_swap            = std::false_type;
+};
+
 #endif // TEST_ALLOCATOR_H
