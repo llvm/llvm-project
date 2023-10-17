c5c7e28d712e21e2d18b834eaf7357d30c2b8628
Kai Luo <lkail@cn.ibm.com>
2023-10-17 09:57:44 +0000

    Minor

--- llvm/lib/Target/PowerPC/PPCRegisterInfo.cpp
+++ llvm/lib/Target/PowerPC/PPCRegisterInfo.cpp
@@ -655,11 +655,9 @@ bool PPCRegisterInfo::getRegAllocationHints(Register VirtReg,
         continue;
       for (MachineOperand &MO : MI.operands()) {
         if (!MO.isReg() || !MO.getReg() || !MO.getReg().isVirtual() ||
-            !MO.isDef())
+            !MO.isDef() || !VRM->hasPhys(MO.getReg()))
           continue;
         MCPhysReg PhysReg = VRM->getPhys(MO.getReg());
-        if (PhysReg == VirtRegMap::NO_PHYS_REG)
-          continue;
         llvm::copy_if(
             TRI->superregs_inclusive(PhysReg),
             std::inserter(ModifiedRegisters, ModifiedRegisters.begin()),
@@ -668,6 +666,10 @@ bool PPCRegisterInfo::getRegAllocationHints(Register VirtReg,
     }
     llvm::copy_if(llvm::make_range(Order.begin(), Order.end()),
                   std::back_inserter(Hints), [&](MCPhysReg Reg) {
+                    // if (TRI->regsOverlap(Reg, PPC::CR2) ||
+                    //     TRI->regsOverlap(Reg, PPC::CR3) ||
+                    //     TRI->regsOverlap(Reg, PPC::CR4))
+                    //   return false;
                     return llvm::all_of(TRI->superregs_inclusive(Reg),
                                         [&](MCPhysReg SR) {
                                           return !ModifiedRegisters.count(SR);
