0099e4d1ffdbbb8ab805fbf31bd1cfc17a08a440
Dave Pagan <dave.pagan@amd.com>
2023-10-18 12:43:26 -0500

    [clang][OpenMP] Fix target data if/logical expression assert fail
    Fixed assertion failure
    
      Basic Block in function 'main' does not have terminator!
      label %land.end
    
    caused by premature setting of CodeGenIP upon entry to
    emitTargetDataCalls, where subsequent evaluation of logical
    expression created new basic blocks, leaving CodeGenIP pointing to
    the wrong basic block. CodeGenIP is now set near the end of the
    function, just prior to generating a comparison of the logical
    expression result (from the if clause) which uses CodeGenIP to
    insert new IR.
    
    Fixes SWDEV-422794/AOMP issue #601
    
    Test already exists in smoke-fail/issue601_if_clause
    
    Change-Id: I792141db01b0f030705ec0742c9d9fb1255f036a

--- clang/lib/CodeGen/CGOpenMPRuntime.cpp
+++ clang/lib/CodeGen/CGOpenMPRuntime.cpp
@@ -10450,11 +10450,6 @@ void CGOpenMPRuntime::emitTargetDataCalls(
   PrePostActionTy NoPrivAction;
 
   using InsertPointTy = llvm::OpenMPIRBuilder::InsertPointTy;
-  InsertPointTy AllocaIP(CGF.AllocaInsertPt->getParent(),
-                         CGF.AllocaInsertPt->getIterator());
-  InsertPointTy CodeGenIP(CGF.Builder.GetInsertBlock(),
-                          CGF.Builder.GetInsertPoint());
-  llvm::OpenMPIRBuilder::LocationDescription OmpLoc(CodeGenIP);
 
   llvm::Value *IfCondVal = nullptr;
   if (IfCond)
@@ -10534,6 +10529,11 @@ void CGOpenMPRuntime::emitTargetDataCalls(
   // Source location for the ident struct
   llvm::Value *RTLoc = emitUpdateLocation(CGF, D.getBeginLoc());
 
+  InsertPointTy AllocaIP(CGF.AllocaInsertPt->getParent(),
+                         CGF.AllocaInsertPt->getIterator());
+  InsertPointTy CodeGenIP(CGF.Builder.GetInsertBlock(),
+                          CGF.Builder.GetInsertPoint());
+  llvm::OpenMPIRBuilder::LocationDescription OmpLoc(CodeGenIP);
   CGF.Builder.restoreIP(OMPBuilder.createTargetData(
       OmpLoc, AllocaIP, CodeGenIP, DeviceID, IfCondVal, Info, GenMapInfoCB,
       /*MapperFunc=*/nullptr, BodyCB, DeviceAddrCB, CustomMapperCB, RTLoc));
