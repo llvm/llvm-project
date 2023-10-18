0bc8cdd1259f78432ad785d1f54b18e4b0b75418
Alex Bradbury <asb@igalia.com>
2023-10-18 14:13:43 +0100

    [builtins] Convert more int to fp functions to use common implementation (#67540)
    Builds on #66903, converting the rest of the low-hanging fruit to use
    the common implementation.
    
    See https://github.com/llvm/llvm-project/pull/67540#issuecomment-1766499179 for links to Alive2 comparisons of before/after.

--- compiler-rt/lib/builtins/floatuntitf.c
+++ compiler-rt/lib/builtins/floatuntitf.c
@@ -16,6 +16,10 @@
 #include "fp_lib.h"
 #include "int_lib.h"
 
+#define SRC_U128
+#define DST_QUAD
+#include "int_to_fp_impl.inc"
+
 // Returns: convert a tu_int to a fp_t, rounding toward even.
 
 // Assumption: fp_t is a IEEE 128 bit floating point type
@@ -26,50 +30,6 @@
 // mmmm mmmm mmmm
 
 #if defined(CRT_HAS_TF_MODE)
-COMPILER_RT_ABI fp_t __floatuntitf(tu_int a) {
-  if (a == 0)
-    return 0.0;
-  const unsigned N = sizeof(tu_int) * CHAR_BIT;
-  int sd = N - __clzti2(a); // number of significant digits
-  int e = sd - 1;           // exponent
-  if (sd > TF_MANT_DIG) {
-    //  start:  0000000000000000000001xxxxxxxxxxxxxxxxxxxxxxPQxxxxxxxxxxxxxxxxxx
-    //  finish: 000000000000000000000000000000000000001xxxxxxxxxxxxxxxxxxxxxxPQR
-    //                                                12345678901234567890123456
-    //  1 = msb 1 bit
-    //  P = bit TF_MANT_DIG-1 bits to the right of 1
-    //  Q = bit TF_MANT_DIG bits to the right of 1
-    //  R = "or" of all bits to the right of Q
-    switch (sd) {
-    case TF_MANT_DIG + 1:
-      a <<= 1;
-      break;
-    case TF_MANT_DIG + 2:
-      break;
-    default:
-      a = (a >> (sd - (TF_MANT_DIG + 2))) |
-          ((a & ((tu_int)(-1) >> ((N + TF_MANT_DIG + 2) - sd))) != 0);
-    };
-    // finish:
-    a |= (a & 4) != 0; // Or P into R
-    ++a;               // round - this step may add a significant bit
-    a >>= 2;           // dump Q and R
-    // a is now rounded to TF_MANT_DIG or TF_MANT_DIG+1 bits
-    if (a & ((tu_int)1 << TF_MANT_DIG)) {
-      a >>= 1;
-      ++e;
-    }
-    // a is now rounded to TF_MANT_DIG bits
-  } else {
-    a <<= (TF_MANT_DIG - sd);
-    // a is now rounded to TF_MANT_DIG bits
-  }
-
-  long_double_bits fb;
-  fb.u.high.all = (du_int)(e + 16383) << 48             // exponent
-                  | ((a >> 64) & 0x0000ffffffffffffLL); // significand
-  fb.u.low.all = (du_int)(a);
-  return fb.f;
-}
+COMPILER_RT_ABI fp_t __floatuntitf(tu_int a) { return __floatXiYf__(a); }
 
 #endif
