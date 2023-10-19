e3d78c69a2121cc12f8f7112deab9f8b0b1ccf04
Yaxun (Sam) Liu <yaxun.liu@amd.com>
2023-10-19 12:55:41 -0400

    Fix test clang/test/Driver/cl-offload.cu
    Regression caused by e880e8aedbc17ba04c969c9426d1f2567af72e7b
    
    Due to aux-target mismatch. Add -target option to fix aux-target.
    
    https://lab.llvm.org/buildbot/#/builders/230/builds/20138

--- clang/test/Driver/cl-offload.cu
+++ clang/test/Driver/cl-offload.cu
@@ -1,9 +1,9 @@
-// RUN: %clang_cl -### --offload-arch=sm_35 -fgpu-rdc \
+// RUN: %clang_cl -### -target x86_64-pc-windows-msvc --offload-arch=sm_35 -fgpu-rdc \
 // RUN:   --cuda-path=%S/Inputs/CUDA/usr/local/cuda \
 // RUN:   /Wall -x cuda %s 2>&1 \
 // RUN:   | FileCheck %s -check-prefix=CUDA
 
-// RUN: %clang_cl -### --offload-arch=gfx1010 -fgpu-rdc --hip-link \
+// RUN: %clang_cl -### -target x86_64-pc-windows-msvc --offload-arch=gfx1010 -fgpu-rdc --hip-link \
 // RUN:   --rocm-path=%S/Inputs/rocm /Wall -x hip %s 2>&1 \
 // RUN:   | FileCheck %s -check-prefix=HIP
 
