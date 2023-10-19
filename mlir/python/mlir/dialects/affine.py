a2288a8944c310fcad1196302f16513797e1fcbc
Maksim Levental <maksim.levental@gmail.com>
2023-10-19 16:20:14 -0500

    [mlir][python] remove mixins (#68853)
    This PR replaces the mixin `OpView` extension mechanism with the
    standard inheritance mechanism.
    
    Why? Firstly, mixins are not very pythonic (inheritance is usually used
    for this), a little convoluted, and too "tight" (can only be used in the
    immediately adjacent `_ext.py`). Secondly, it (mixins) are now blocking
    are correct implementation of "value builders" (see
    [here](https://github.com/llvm/llvm-project/pull/68764)) where the
    problem becomes how to choose the correct base class that the value
    builder should call.
    
    This PR looks big/complicated but appearances are deceiving; 4 things
    were needed to make this work:
    
    1. Drop `skipDefaultBuilders` in
    `OpPythonBindingGen::emitDefaultOpBuilders`
    2. Former mixin extension classes are converted to inherit from the
    generated `OpView` instead of being "mixins"
    a. extension classes that simply were calling into an already generated
    `super().__init__` continue to do so
    b. (almost all) extension classes that were calling `self.build_generic`
    because of a lack of default builder being generated can now also just
    call `super().__init__`
    3. To handle the [lone single
    use-case](https://sourcegraph.com/search?q=context%3Aglobal+select_opview_mixin&patternType=standard&sm=1&groupBy=repo)
    of `select_opview_mixin`, namely
    [linalg](https://github.com/llvm/llvm-project/blob/main/mlir/python/mlir/dialects/_linalg_ops_ext.py#L38),
    only a small change was necessary in `opdsl/lang/emitter.py` (thanks to
    the emission/generation of default builders/`__init__`s)
    4. since the `extend_opview_class` decorator is removed, we need a way
    to register extension classes as the desired `OpView` that `op.opview`
    conjures into existence; so we do the standard thing and just enable
    replacing the existing registered `OpView` i.e.,
    `register_operation(_Dialect, replace=True)`.
    
    Note, the upgrade path for the common case is to change an extension to
    inherit from the generated builder and decorate it with
    `register_operation(_Dialect, replace=True)`. In the slightly more
    complicated case where `super().__init(self.build_generic(...))` is
    called in the extension's `__init__`, this needs to be updated to call
    `__init__` in `OpView`, i.e., the grandparent (see updated docs). 
    Note, also `<DIALECT>_ext.py` files/modules will no longer be automatically loaded.
    
    Note, the PR has 3 base commits that look funny but this was done for
    the purpose of tracking the line history of moving the
    `<DIALECT>_ops_ext.py` class into `<DIALECT>.py` and updating (commit
    labeled "fix").

--- mlir/python/mlir/dialects/affine.py
+++ mlir/python/mlir/dialects/affine.py
@@ -1,5 +1,50 @@
-#  Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.                                        
-#  See https://llvm.org/LICENSE.txt for license information.                                                            
-#  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception    
+#  Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+#  See https://llvm.org/LICENSE.txt for license information.
+#  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 from ._affine_ops_gen import *
+from ._affine_ops_gen import _Dialect
+
+try:
+    from ..ir import *
+    from ._ods_common import (
+        get_op_result_or_value as _get_op_result_or_value,
+        get_op_results_or_values as _get_op_results_or_values,
+        _cext as _ods_cext,
+    )
+except ImportError as e:
+    raise RuntimeError("Error loading imports from extension module") from e
+
+from typing import Optional, Sequence, Union
+
+
+@_ods_cext.register_operation(_Dialect, replace=True)
+class AffineStoreOp(AffineStoreOp):
+    """Specialization for the Affine store operation."""
+
+    def __init__(
+        self,
+        value: Union[Operation, OpView, Value],
+        memref: Union[Operation, OpView, Value],
+        map: AffineMap = None,
+        *,
+        map_operands=None,
+        loc=None,
+        ip=None,
+    ):
+        """Creates an affine store operation.
+
+        - `value`: the value to store into the memref.
+        - `memref`: the buffer to store into.
+        - `map`: the affine map that maps the map_operands to the index of the
+          memref.
+        - `map_operands`: the list of arguments to substitute the dimensions,
+          then symbols in the affine map, in increasing order.
+        """
+        map = map if map is not None else []
+        map_operands = map_operands if map_operands is not None else []
+        indicies = [_get_op_result_or_value(op) for op in map_operands]
+        _ods_successors = None
+        super().__init__(
+            value, memref, indicies, AffineMapAttr.get(map), loc=loc, ip=ip
+        )
