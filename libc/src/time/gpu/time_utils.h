62cc69eaa78684bbbe7f119d6411a20c6c54a57b
alfredfo <98554039+alfredfo@users.noreply.github.com>
2023-10-19 19:39:02 +0200

    [libc] Fix accidental LIBC_NAMESPACE_clock_freq (#69620)
    See-also: https://github.com/llvm/llvm-project/pull/69548

--- libc/src/time/gpu/time_utils.h
+++ libc/src/time/gpu/time_utils.h
@@ -39,8 +39,8 @@ constexpr uint64_t clock_freq = 0;
 // We provide an externally visible symbol such that the runtime can set this to
 // the correct value. If it is not set we try to default to the known values.
 extern "C" [[gnu::visibility("protected")]] uint64_t
-    [[clang::address_space(4)]] LIBC_NAMESPACE_clock_freq;
-#define GPU_CLOCKS_PER_SEC static_cast<clock_t>(LIBC_NAMESPACE_clock_freq)
+    [[clang::address_space(4)]] __llvm_libc_clock_freq;
+#define GPU_CLOCKS_PER_SEC static_cast<clock_t>(__llvm_libc_clock_freq)
 
 #elif defined(LIBC_TARGET_ARCH_IS_NVPTX)
 // NPVTX uses a single 1 GHz fixed frequency clock for all target architectures.
