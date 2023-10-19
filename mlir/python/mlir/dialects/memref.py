936d9058c4e35a45cd8e6a4690b2a0de0912d35b
Maksim Levental <maksim.levental@gmail.com>
2023-10-19 16:20:14 -0500

    [mlir][python] remove mixins (#68853)
    This PR replaces the mixin `OpView` extension mechanism with the
    standard inheritance mechanism.
    
    Why? Firstly, mixins are not very pythonic (inheritance is usually used
    for this), a little convoluted, and too "tight" (can only be used in the
    immediately adjacent `_ext.py`). Secondly, it (mixins) are now blocking
    are correct implementation of "value builders" (see
    [here](https://github.com/llvm/llvm-project/pull/68764)) where the
    problem becomes how to choose the correct base class that the value
    builder should call.
    
    This PR looks big/complicated but appearances are deceiving; 4 things
    were needed to make this work:
    
    1. Drop `skipDefaultBuilders` in
    `OpPythonBindingGen::emitDefaultOpBuilders`
    2. Former mixin extension classes are converted to inherit from the
    generated `OpView` instead of being "mixins"
    a. extension classes that simply were calling into an already generated
    `super().__init__` continue to do so
    b. (almost all) extension classes that were calling `self.build_generic`
    because of a lack of default builder being generated can now also just
    call `super().__init__`
    3. To handle the [lone single
    use-case](https://sourcegraph.com/search?q=context%3Aglobal+select_opview_mixin&patternType=standard&sm=1&groupBy=repo)
    of `select_opview_mixin`, namely
    [linalg](https://github.com/llvm/llvm-project/blob/main/mlir/python/mlir/dialects/_linalg_ops_ext.py#L38),
    only a small change was necessary in `opdsl/lang/emitter.py` (thanks to
    the emission/generation of default builders/`__init__`s)
    4. since the `extend_opview_class` decorator is removed, we need a way
    to register extension classes as the desired `OpView` that `op.opview`
    conjures into existence; so we do the standard thing and just enable
    replacing the existing registered `OpView` i.e.,
    `register_operation(_Dialect, replace=True)`.
    
    Note, the upgrade path for the common case is to change an extension to
    inherit from the generated builder and decorate it with
    `register_operation(_Dialect, replace=True)`. In the slightly more
    complicated case where `super().__init(self.build_generic(...))` is
    called in the extension's `__init__`, this needs to be updated to call
    `__init__` in `OpView`, i.e., the grandparent (see updated docs). 
    Note, also `<DIALECT>_ext.py` files/modules will no longer be automatically loaded.
    
    Note, the PR has 3 base commits that look funny but this was done for
    the purpose of tracking the line history of moving the
    `<DIALECT>_ops_ext.py` class into `<DIALECT>.py` and updating (commit
    labeled "fix").

--- mlir/python/mlir/dialects/memref.py
+++ mlir/python/mlir/dialects/memref.py
@@ -3,3 +3,41 @@
 #  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 from ._memref_ops_gen import *
+from ._memref_ops_gen import _Dialect
+
+try:
+    from ..ir import *
+    from ._ods_common import (
+        get_op_result_or_value as _get_op_result_or_value,
+        get_op_results_or_values as _get_op_results_or_values,
+        _cext as _ods_cext,
+    )
+except ImportError as e:
+    raise RuntimeError("Error loading imports from extension module") from e
+
+from typing import Optional, Sequence, Union
+
+
+@_ods_cext.register_operation(_Dialect, replace=True)
+class LoadOp(LoadOp):
+    """Specialization for the MemRef load operation."""
+
+    def __init__(
+        self,
+        memref: Union[Operation, OpView, Value],
+        indices: Optional[Union[Operation, OpView, Sequence[Value]]] = None,
+        *,
+        loc=None,
+        ip=None,
+    ):
+        """Creates a memref load operation.
+
+        Args:
+          memref: the buffer to load from.
+          indices: the list of subscripts, may be empty for zero-dimensional
+            buffers.
+          loc: user-visible location of the operation.
+          ip: insertion point.
+        """
+        indices_resolved = [] if indices is None else _get_op_results_or_values(indices)
+        super().__init__(memref, indices_resolved, loc=loc, ip=ip)
--- mlir/python/mlir/dialects/scf.py
+++ mlir/python/mlir/dialects/scf.py
@@ -2,11 +2,122 @@
 #  See https://llvm.org/LICENSE.txt for license information.
 #  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-from typing import Optional, Sequence
 
 from ._scf_ops_gen import *
+from ._scf_ops_gen import _Dialect
 from .arith import constant
-from ..ir import *
+
+try:
+    from ..ir import *
+    from ._ods_common import (
+        get_op_result_or_value as _get_op_result_or_value,
+        get_op_results_or_values as _get_op_results_or_values,
+        _cext as _ods_cext,
+    )
+except ImportError as e:
+    raise RuntimeError("Error loading imports from extension module") from e
+
+from typing import Optional, Sequence, Union
+
+
+_ForOp = ForOp
+
+
+@_ods_cext.register_operation(_Dialect, replace=True)
+class ForOp(_ForOp):
+    """Specialization for the SCF for op class."""
+
+    def __init__(
+        self,
+        lower_bound,
+        upper_bound,
+        step,
+        iter_args: Optional[Union[Operation, OpView, Sequence[Value]]] = None,
+        *,
+        loc=None,
+        ip=None,
+    ):
+        """Creates an SCF `for` operation.
+
+        - `lower_bound` is the value to use as lower bound of the loop.
+        - `upper_bound` is the value to use as upper bound of the loop.
+        - `step` is the value to use as loop step.
+        - `iter_args` is a list of additional loop-carried arguments or an operation
+          producing them as results.
+        """
+        if iter_args is None:
+            iter_args = []
+        iter_args = _get_op_results_or_values(iter_args)
+
+        results = [arg.type for arg in iter_args]
+        super(_ForOp, self).__init__(
+            self.build_generic(
+                regions=1,
+                results=results,
+                operands=[
+                    _get_op_result_or_value(o) for o in [lower_bound, upper_bound, step]
+                ]
+                + list(iter_args),
+                loc=loc,
+                ip=ip,
+            )
+        )
+        self.regions[0].blocks.append(self.operands[0].type, *results)
+
+    @property
+    def body(self):
+        """Returns the body (block) of the loop."""
+        return self.regions[0].blocks[0]
+
+    @property
+    def induction_variable(self):
+        """Returns the induction variable of the loop."""
+        return self.body.arguments[0]
+
+    @property
+    def inner_iter_args(self):
+        """Returns the loop-carried arguments usable within the loop.
+
+        To obtain the loop-carried operands, use `iter_args`.
+        """
+        return self.body.arguments[1:]
+
+
+_IfOp = IfOp
+
+
+@_ods_cext.register_operation(_Dialect, replace=True)
+class IfOp(_IfOp):
+    """Specialization for the SCF if op class."""
+
+    def __init__(self, cond, results_=[], *, hasElse=False, loc=None, ip=None):
+        """Creates an SCF `if` operation.
+
+        - `cond` is a MLIR value of 'i1' type to determine which regions of code will be executed.
+        - `hasElse` determines whether the if operation has the else branch.
+        """
+        operands = []
+        operands.append(cond)
+        results = []
+        results.extend(results_)
+        super(_IfOp, self).__init__(
+            self.build_generic(
+                regions=2, results=results, operands=operands, loc=loc, ip=ip
+            )
+        )
+        self.regions[0].blocks.append(*[])
+        if hasElse:
+            self.regions[1].blocks.append(*[])
+
+    @property
+    def then_block(self):
+        """Returns the then block of the if operation."""
+        return self.regions[0].blocks[0]
+
+    @property
+    def else_block(self):
+        """Returns the else block of the if operation."""
+        return self.regions[1].blocks[0]
 
 
 def for_(
