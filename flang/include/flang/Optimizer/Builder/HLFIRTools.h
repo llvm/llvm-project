2ef370b7716b39390736e181d2eaabd740e1d59d
jeanPerier <jperier@nvidia.com>
2023-10-20 11:11:52 +0200

    [flang][openmp] Update copyHostAssociateVar to use hlfir.assign for HLFIR (#69441)
    The code in `copyHostAssociateVar` is using `createSomeArrayAssignment`
    for arrays which is using the soon legacy expression lowering. Update
    the copy to use hlfir.assign instead.
    
    I used the temporary_lhs flag to mimic the current behavior, but maybe
    user defined assignment should be called when needed .This flag also
    prevents any finalizers to be called on the LHS if the LHS type has
    finalizers (which would occur otherwise in normal intrinsic assignment).
    Again, I am not sure what the OpenMP spec wants here.
    
    Also, I added special handling for ALLOCATABLE, the current code seems
    broken to me since it is basically copying the descriptor which would
    lead to memory leak given the TEMP was previously allocated with the
    shape of the variable in createHostAssociateVarClone. So copying the
    DATA instead seemed like the right thing to do.

--- flang/include/flang/Optimizer/Builder/HLFIRTools.h
+++ flang/include/flang/Optimizer/Builder/HLFIRTools.h
@@ -172,6 +172,11 @@ public:
     return varIface ? varIface.isAllocatable() : false;
   }
 
+  bool isPointer() const {
+    auto varIface = getIfVariableInterface();
+    return varIface ? varIface.isPointer() : false;
+  }
+
   // Get the entity as an mlir SSA value containing all the shape, type
   // parameters and dynamic shape information.
   mlir::Value getBase() const { return *this; }
