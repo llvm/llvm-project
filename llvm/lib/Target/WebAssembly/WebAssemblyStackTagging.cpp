e6b12455518e0fea331b67001058ac3972253423
trcrsired <uwgghhbcad@gmail.com>
2023-10-20 18:24:02 -0400

    Wasm add DT, LI to wasm stack tagging

--- llvm/lib/Target/WebAssembly/WebAssemblyStackTagging.cpp
+++ llvm/lib/Target/WebAssembly/WebAssemblyStackTagging.cpp
@@ -10,6 +10,9 @@
 #include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/InstIterator.h"
+#include "llvm/IR/ValueHandle.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/Transforms/Utils/Local.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Analysis/AliasAnalysis.h"
 #include "llvm/Analysis/StackSafetyAnalysis.h"
@@ -20,6 +23,28 @@ using namespace llvm;
 #define DEBUG_TYPE "wasm-stack-tagging"
 
 namespace {
+
+static cl::opt<bool> ClMergeInit(
+    "stack-tagging-merge-init", cl::Hidden, cl::init(true),
+    cl::desc("merge stack variable initializers with tagging when possible"));
+
+static cl::opt<bool>
+    ClUseStackSafety("stack-tagging-use-stack-safety", cl::Hidden,
+                     cl::init(true),
+                     cl::desc("Use Stack Safety analysis results"));
+
+static cl::opt<unsigned> ClScanLimit("stack-tagging-merge-init-scan-limit",
+                                     cl::init(40), cl::Hidden);
+
+static cl::opt<unsigned>
+    ClMergeInitSizeLimit("stack-tagging-merge-init-size-limit", cl::init(272),
+                         cl::Hidden);
+
+static cl::opt<size_t> ClMaxLifetimes(
+    "stack-tagging-max-lifetimes-for-alloca", cl::Hidden, cl::init(3),
+    cl::ReallyHidden,
+    cl::desc("How many lifetime ends to handle for a single alloca."),
+    cl::Optional);
 
 struct WebAssemblyStackTagging : public FunctionPass {
   static char ID;
@@ -41,6 +66,8 @@ private:
 #endif
 }; // end of struct Hello
 
+static const inline Align kTagGranuleSize = Align(16);
+
 }
 
 bool WebAssemblyStackTagging::runOnFunction(Function & Fn) {
@@ -52,9 +79,60 @@ bool WebAssemblyStackTagging::runOnFunction(Function & Fn) {
   for (Instruction &I : instructions(F))
     SIB.visit(I);
   memtag::StackInfo &SInfo = SIB.get();
+
+  std::unique_ptr<DominatorTree> DeleteDT;
+  DominatorTree *DT = nullptr;
+  if (auto *P = getAnalysisIfAvailable<DominatorTreeWrapperPass>())
+    DT = &P->getDomTree();
+
+  if (DT == nullptr) {
+    DeleteDT = std::make_unique<DominatorTree>(*F);
+    DT = DeleteDT.get();
+  }
+
+#if 0
+  std::unique_ptr<PostDominatorTree> DeletePDT;
+  PostDominatorTree *PDT = nullptr;
+  if (auto *P = getAnalysisIfAvailable<PostDominatorTreeWrapperPass>())
+    PDT = &P->getPostDomTree();
+
+  if (PDT == nullptr) {
+    DeletePDT = std::make_unique<PostDominatorTree>(*F);
+    PDT = DeletePDT.get();
+  }
+#endif
+
+  std::unique_ptr<LoopInfo> DeleteLI;
+  LoopInfo *LI = nullptr;
+  if (auto *LIWP = getAnalysisIfAvailable<LoopInfoWrapperPass>()) {
+    LI = &LIWP->getLoopInfo();
+  } else {
+    DeleteLI = std::make_unique<LoopInfo>(*DT);
+    LI = DeleteLI.get();
+  }
+
+
 #if 1
+  int NextTag = 0;
   for (auto &I : SInfo.AllocasToInstrument) {
-    LLVM_DEBUG(dbgs() << I << "\n");
+    memtag::AllocaInfo &Info = I.second;
+    TrackingVH<Instruction> OldAI = Info.AI;
+    memtag::alignAndPadAlloca(Info, kTagGranuleSize);
+    AllocaInst *AI = Info.AI;
+    int Tag = NextTag;
+    NextTag = (NextTag + 1) % 16;
+    IRBuilder<> IRB(Info.AI->getNextNode());
+
+    // Calls to functions that may return twice (e.g. setjmp) confuse the
+    // postdominator analysis, and will leave us to keep memory tagged after
+    // function return. Work around this by always untagging at every return
+    // statement if return_twice functions are called.
+    bool StandardLifetime =
+        SInfo.UnrecognizedLifetimes.empty() &&
+        memtag::isStandardLifetime(Info.LifetimeStart, Info.LifetimeEnd, DT, LI,
+                                   ClMaxLifetimes) &&
+        !SInfo.CallsReturnTwice;
+//    LLVM_DEBUG(dbgs() << I << "\n");
   }
 #endif
   return true;
