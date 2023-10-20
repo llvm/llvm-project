6842e48c76ed0e0b7d43265c7ae09244d8ac55ba
Brandon Wu <brandon.wu@sifive.com>
2023-10-19 19:31:49 -0700

    fixup! [RISCV] Support Xsfvqmaccdod and Xsfvqmaccqoq extensions

--- llvm/lib/Target/RISCV/Disassembler/RISCVDisassembler.cpp
+++ llvm/lib/Target/RISCV/Disassembler/RISCVDisassembler.cpp
@@ -560,10 +560,10 @@ DecodeStatus RISCVDisassembler::getInstruction(MCInst &MI, uint64_t &Size,
                           "SiFive VCIX custom opcode table");
     TRY_TO_DECODE_FEATURE(
         RISCV::FeatureVendorXSfvqmaccdod, DecoderTableXSfvqmaccdod32,
-        "SiFive Matrix Multiplication Instruction opcode table");
+        "SiFive Matrix Multiplication (2x8 and 8x2) Instruction opcode table");
     TRY_TO_DECODE_FEATURE(
         RISCV::FeatureVendorXSfvqmaccqoq, DecoderTableXSfvqmaccqoq32,
-        "SiFive Matrix Multiplication Instruction opcode table");
+        "SiFive Matrix Multiplication (4x8 and 8x4) Instruction opcode table");
     TRY_TO_DECODE_FEATURE(RISCV::FeatureVendorXSfcie, DecoderTableXSfcie32,
                           "Sifive CIE custom opcode table");
     TRY_TO_DECODE_FEATURE(RISCV::FeatureVendorXCVbitmanip,
