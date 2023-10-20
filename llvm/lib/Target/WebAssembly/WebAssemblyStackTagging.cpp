06271b8eabf79cfc1ca22028e770bb7520691218
trcrsired <uwgghhbcad@gmail.com>
2023-10-20 18:58:45 -0400

    Webassembly stack tagging remove cl::opt

--- llvm/lib/Target/WebAssembly/WebAssemblyStackTagging.cpp
+++ llvm/lib/Target/WebAssembly/WebAssemblyStackTagging.cpp
@@ -16,6 +16,7 @@
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Analysis/AliasAnalysis.h"
 #include "llvm/Analysis/StackSafetyAnalysis.h"
+#include "llvm/Analysis/PostDominators.h"
 #include "llvm/Transforms/Utils/MemoryTaggingSupport.h"
 
 using namespace llvm;
@@ -24,33 +25,15 @@ using namespace llvm;
 
 namespace {
 
-static cl::opt<bool> ClMergeInit(
-    "stack-tagging-merge-init", cl::Hidden, cl::init(true),
-    cl::desc("merge stack variable initializers with tagging when possible"));
-
-static cl::opt<bool>
-    ClUseStackSafety("stack-tagging-use-stack-safety", cl::Hidden,
-                     cl::init(true),
-                     cl::desc("Use Stack Safety analysis results"));
-
-static cl::opt<unsigned> ClScanLimit("stack-tagging-merge-init-scan-limit",
-                                     cl::init(40), cl::Hidden);
-
-static cl::opt<unsigned>
-    ClMergeInitSizeLimit("stack-tagging-merge-init-size-limit", cl::init(272),
-                         cl::Hidden);
-
-static cl::opt<size_t> ClMaxLifetimes(
-    "stack-tagging-max-lifetimes-for-alloca", cl::Hidden, cl::init(3),
-    cl::ReallyHidden,
-    cl::desc("How many lifetime ends to handle for a single alloca."),
-    cl::Optional);
-
 struct WebAssemblyStackTagging : public FunctionPass {
   static char ID;
   StackSafetyGlobalInfo const *SSI = nullptr;
   AAResults *AA = nullptr;
   WebAssemblyStackTagging() : FunctionPass(ID) {}
+
+  void tagAlloca(AllocaInst *AI, Instruction *InsertBefore, Value *Ptr,
+                 uint64_t Size);
+  void untagAlloca(AllocaInst *AI, Instruction *InsertBefore, uint64_t Size);
 
   bool runOnFunction(Function &) override;
 private:
@@ -68,6 +51,40 @@ private:
 
 static const inline Align kTagGranuleSize = Align(16);
 
+}
+
+void WebAssemblyStackTagging::tagAlloca(AllocaInst *AI, Instruction *InsertBefore,
+                                    Value *Ptr, uint64_t Size) {
+
+#if 0
+  auto SetTagZeroFunc =
+      Intrinsic::getDeclaration(F->getParent(), Intrinsic::aarch64_settag_zero);
+  auto StgpFunc =
+      Intrinsic::getDeclaration(F->getParent(), Intrinsic::aarch64_stgp);
+
+  InitializerBuilder IB(Size, DL, Ptr, SetTagFunc, SetTagZeroFunc, StgpFunc);
+  bool LittleEndian =
+      Triple(AI->getModule()->getTargetTriple()).isLittleEndian();
+  // Current implementation of initializer merging assumes little endianness.
+  if (MergeInit && !F->hasOptNone() && LittleEndian &&
+      Size < ClMergeInitSizeLimit) {
+    LLVM_DEBUG(dbgs() << "collecting initializers for " << *AI
+                      << ", size = " << Size << "\n");
+    InsertBefore = collectInitializers(InsertBefore, Ptr, Size, IB);
+  }
+
+  IRBuilder<> IRB(InsertBefore);
+  IB.generate(IRB);
+#endif
+}
+
+void WebAssemblyStackTagging::untagAlloca(AllocaInst *AI, Instruction *InsertBefore,
+                                      uint64_t Size) {
+#if 0
+  IRBuilder<> IRB(InsertBefore);
+  IRB.CreateCall(SetTagFunc, {IRB.CreatePointerCast(AI, IRB.getPtrTy()),
+                              ConstantInt::get(IRB.getInt64Ty(), Size)});
+#endif
 }
 
 bool WebAssemblyStackTagging::runOnFunction(Function & Fn) {
@@ -90,7 +107,6 @@ bool WebAssemblyStackTagging::runOnFunction(Function & Fn) {
     DT = DeleteDT.get();
   }
 
-#if 0
   std::unique_ptr<PostDominatorTree> DeletePDT;
   PostDominatorTree *PDT = nullptr;
   if (auto *P = getAnalysisIfAvailable<PostDominatorTreeWrapperPass>())
@@ -100,7 +116,6 @@ bool WebAssemblyStackTagging::runOnFunction(Function & Fn) {
     DeletePDT = std::make_unique<PostDominatorTree>(*F);
     PDT = DeletePDT.get();
   }
-#endif
 
   std::unique_ptr<LoopInfo> DeleteLI;
   LoopInfo *LI = nullptr;
@@ -130,8 +145,38 @@ bool WebAssemblyStackTagging::runOnFunction(Function & Fn) {
     bool StandardLifetime =
         SInfo.UnrecognizedLifetimes.empty() &&
         memtag::isStandardLifetime(Info.LifetimeStart, Info.LifetimeEnd, DT, LI,
-                                   ClMaxLifetimes) &&
+                                   3) &&
         !SInfo.CallsReturnTwice;
+#if 0
+    if (StandardLifetime) {
+      IntrinsicInst *Start = Info.LifetimeStart[0];
+      uint64_t Size =
+          cast<ConstantInt>(Start->getArgOperand(0))->getZExtValue();
+      Size = alignTo(Size, kTagGranuleSize);
+      tagAlloca(AI, Start->getNextNode(), Start->getArgOperand(1), Size);
+
+      auto TagEnd = [&](Instruction *Node) { untagAlloca(AI, Node, Size); };
+      if (!DT || !PDT ||
+          !memtag::forAllReachableExits(*DT, *PDT, *LI, Start, Info.LifetimeEnd,
+                                        SInfo.RetVec, TagEnd)) {
+        for (auto *End : Info.LifetimeEnd)
+          End->eraseFromParent();
+      }
+    } else {
+      uint64_t Size = *Info.AI->getAllocationSize(*DL);
+      Value *Ptr = IRB.CreatePointerCast(TagPCall, IRB.getPtrTy());
+      tagAlloca(AI, &*IRB.GetInsertPoint(), Ptr, Size);
+      for (auto *RI : SInfo.RetVec) {
+        untagAlloca(AI, RI, Size);
+      }
+      // We may have inserted tag/untag outside of any lifetime interval.
+      // Remove all lifetime intrinsics for this alloca.
+      for (auto *II : Info.LifetimeStart)
+        II->eraseFromParent();
+      for (auto *II : Info.LifetimeEnd)
+        II->eraseFromParent();
+    }
+#endif
 //    LLVM_DEBUG(dbgs() << I << "\n");
   }
 #endif
