ac83f05092cce7ecb87af09029ba7f812fce2d53
Jakub Kuderski <jakub@nod-labs.com>
2023-10-20 13:18:31 -0400

    Revert "[mlir][index][spirv] Add conversion for index to spirv (#68085)"
    This caused linker issues on a buildbot:
    https://lab.llvm.org/buildbot/#/builders/61/builds/50716.
    
    This reverts commit 3c07a21683c3c0a30aef4a47dde31cb85ad9830c.

--- mlir/lib/Conversion/SCFToSPIRV/SCFToSPIRVPass.cpp
+++ mlir/lib/Conversion/SCFToSPIRV/SCFToSPIRVPass.cpp
@@ -14,7 +14,6 @@
 
 #include "mlir/Conversion/ArithToSPIRV/ArithToSPIRV.h"
 #include "mlir/Conversion/FuncToSPIRV/FuncToSPIRV.h"
-#include "mlir/Conversion/IndexToSPIRV/IndexToSPIRV.h"
 #include "mlir/Conversion/MemRefToSPIRV/MemRefToSPIRV.h"
 #include "mlir/Conversion/SCFToSPIRV/SCFToSPIRV.h"
 #include "mlir/Dialect/SCF/IR/SCF.h"
@@ -53,7 +52,6 @@ void SCFToSPIRVPass::runOnOperation() {
   populateFuncToSPIRVPatterns(typeConverter, patterns);
   populateMemRefToSPIRVPatterns(typeConverter, patterns);
   populateBuiltinFuncToSPIRVPatterns(typeConverter, patterns);
-  index::populateIndexToSPIRVPatterns(typeConverter, patterns);
 
   if (failed(applyPartialConversion(op, *target, std::move(patterns))))
     return signalPassFailure();
