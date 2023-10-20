11f77d3f901b75e34e706f6aced3d105387fd0eb
Augusto Noronha <augusto2112@me.com>
2023-10-20 12:59:00 -0700

    [lldb][NFC] Move DWARFASTParserClang::GetTypeForDIE to DWARFASTParser
    (cherry picked from commit 040c4f4d98f3306e068521e3c218bdbc170f81f3)
    (cherry picked from commit fd6891f75c7692a6baab6b25bb70860530ac2088)

--- lldb/source/Plugins/SymbolFile/DWARF/DWARFASTParser.h
+++ lldb/source/Plugins/SymbolFile/DWARF/DWARFASTParser.h
@@ -63,6 +63,9 @@ public:
   ParseChildArrayInfo(const DWARFDIE &parent_die,
                       const lldb_private::ExecutionContext *exe_ctx = nullptr);
 
+  lldb_private::Type *
+  GetTypeForDIE(const lldb_private::plugin::dwarf::DWARFDIE &die);
+
   static lldb::AccessType GetAccessTypeFromDWARF(uint32_t dwarf_accessibility);
 };
 
