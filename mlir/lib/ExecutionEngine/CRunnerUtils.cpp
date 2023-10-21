a1fc999c96b5f75ef381339bdc1e83412a2a502a
Brad Smith <brad@comstyle.com>
2023-10-21 01:18:36 -0400

    [mlir] Avoid including <alloca.h> on DragonFly

--- mlir/lib/ExecutionEngine/CRunnerUtils.cpp
+++ mlir/lib/ExecutionEngine/CRunnerUtils.cpp
@@ -16,7 +16,8 @@
 #include "mlir/ExecutionEngine/Msan.h"
 
 #ifndef _WIN32
-#if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__)
+#if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || \
+    defined(__DragonFly__)
 #include <cstdlib>
 #else
 #include <alloca.h>
