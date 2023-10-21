ba75bd4065baf05e06308241c08ed9fca856557a
Johannes Doerfert <johannes@jdoerfert.de>
2023-10-21 14:49:30 -0700

    [OpenMP] Basic BumpAllocator for (AMD)GPUs (#69806)
    The patch contains a basic BumpAllocator for (AMD)GPUs to allow us to
    run more tests. The allocator implements `malloc`, both internally and
    externally, while we continue to default to the NVIDIA `malloc` when we
    target NVIDIA GPUs. Once we have smarter or customizable allocators we
    should consider this choice, for now, this allocator is better than
    none. It traps if it is out of memory, making it easy to debug. Heap
    size is configured via `LIBOMPTARGET_HEAP_SIZE` and defaults to 512MB.
    It allows to track allocation statistics via
    `LIBOMPTARGET_DEVICE_RTL_DEBUG=8` (together with
    `-fopenmp-target-debug=8`). Two tests were added, and one was enabled.
    
    This is the next step towards fixing
     https://github.com/llvm/llvm-project/issues/66708

--- openmp/libomptarget/plugins-nextgen/common/PluginInterface/PluginInterface.h
+++ openmp/libomptarget/plugins-nextgen/common/PluginInterface/PluginInterface.h
@@ -625,6 +625,10 @@ struct GenericDeviceTy : public DeviceAllocatorTy {
   /// this behavior by overriding the shouldSetupDeviceEnvironment function.
   Error setupDeviceEnvironment(GenericPluginTy &Plugin, DeviceImageTy &Image);
 
+  /// Setup the global device memory pool, if the plugin requires one.
+  Error setupDeviceMemoryPool(GenericPluginTy &Plugin, DeviceImageTy &Image,
+                              uint64_t PoolSize);
+
   // Setup the RPC server for this device if needed. This may not run on some
   // plugins like the CPU targets. By default, it will not be executed so it is
   // up to the target to override this using the shouldSetupRPCServer function.
@@ -831,6 +835,10 @@ private:
   /// setupDeviceEnvironment() function.
   virtual bool shouldSetupDeviceEnvironment() const { return true; }
 
+  /// Indicate whether the device should setup the global device memory pool. If
+  /// false is return the value on the device will be uninitialized.
+  virtual bool shouldSetupDeviceMemoryPool() const { return true; }
+
   /// Indicate whether or not the device should setup the RPC server. This is
   /// only necessary for unhosted targets like the GPU.
   virtual bool shouldSetupRPCServer() const { return false; }
@@ -911,6 +919,9 @@ private:
   /// Return the kernel environment object for kernel \p Name.
   Expected<KernelEnvironmentTy>
   getKernelEnvironmentForKernel(StringRef Name, DeviceImageTy &Image);
+
+  DeviceMemoryPoolTy DeviceMemoryPool = {nullptr, 0};
+  DeviceMemoryPoolTrackingTy DeviceMemoryPoolTracking = {0, 0, ~0U, 0};
 };
 
 /// Class implementing common functionalities of offload plugins. Each plugin
