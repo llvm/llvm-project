1bfbe4c4a6084f94b741ea46925c51305b27d9de
Jean Perier <jperier@nvidia.com>
2023-10-23 09:27:05 -0700

    [flang] Allow lowering of sub-expressions to be overridden
    OpenACC/OpenMP atomic lowering needs a finer control over expression
    lowering. This patch allows mapping evaluate::Expr<T> to mlir::Value
    so that any subsequent expression lowering will use these values when
    an operand is a mapped Expr<T>.
    
    This is an alternative to https://github.com/llvm/llvm-project/pull/69866
    From which I took the test.
    
    The same test as in https://github.com/llvm/llvm-project/pull/69866 are
    failing because the "non atomic part" is now out of the atomic.update
    op, which in some case causing verification failure because this is
    generated in the middle of an omp.atomic.capture. I did not try fixing
    these failures. My patch is about the lowering infrastructure and how to
    use it rather than the OpenMP semantics.
    
    Co-authored-by: Nimish Mishra <neelam.nimish@gmail.com>

--- flang/lib/Lower/Bridge.cpp
+++ flang/lib/Lower/Bridge.cpp
@@ -504,6 +504,15 @@ public:
     addSymbol(sym, exval, /*forced=*/true);
   }
 
+  void
+  overrideExprValues(const Fortran::lower::ExprToValueMap *map) override final {
+    exprValueOverrides = map;
+  }
+
+  const Fortran::lower::ExprToValueMap *getExprOverrides() override final {
+    return exprValueOverrides;
+  }
+
   bool lookupLabelSet(Fortran::lower::SymbolRef sym,
                       Fortran::lower::pft::LabelSet &labelSet) override final {
     Fortran::lower::pft::FunctionLikeUnit &owningProc =
@@ -4890,6 +4899,8 @@ private:
   /// Whether an OpenMP target region or declare target function/subroutine
   /// intended for device offloading has been detected
   bool ompDeviceCodeFound = false;
+
+  const Fortran::lower::ExprToValueMap *exprValueOverrides{nullptr};
 };
 
 } // namespace
