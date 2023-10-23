28607b257f13ce3e4992d07dc543eb13b175e7a3
ZhangYin <zhangyin2018@iscas.ac.cn>
2023-10-23 17:51:34 +0800

    [libc++] <experimental/simd> Add operator value_type() of simd reference (#68960)

--- libcxx/include/experimental/__simd/simd.h
+++ libcxx/include/experimental/__simd/simd.h
@@ -46,11 +46,12 @@ public:
 
   // generator constructor
   template <class _Generator, enable_if_t<__can_generate_v<value_type, _Generator, size()>, int> = 0>
-  explicit _LIBCPP_HIDE_FROM_ABI simd(_Generator&& __g) : __s_(_Impl::__generate(std::forward<_Generator>(__g))) {}
+  explicit _LIBCPP_HIDE_FROM_ABI simd(_Generator&& __g) noexcept
+      : __s_(_Impl::__generate(std::forward<_Generator>(__g))) {}
 
   // scalar access [simd.subscr]
-  // Add operator[] temporarily to test braodcast. Add test for it in later patch.
-  _LIBCPP_HIDE_FROM_ABI value_type operator[](size_t __i) const { return __s_.__get(__i); }
+  _LIBCPP_HIDE_FROM_ABI reference operator[](size_t __i) noexcept { return reference(__s_, __i); }
+  _LIBCPP_HIDE_FROM_ABI value_type operator[](size_t __i) const noexcept { return __s_.__get(__i); }
 };
 
 template <class _Tp>
