e45356910f826194003e9bd62a2406971b2fe3f3
Augusto Noronha <augusto2112@me.com>
2023-10-23 16:37:45 -0700

    [lldb][NFC] Remote reference to Swift in DWARFASTParser::Kind enum (#69984)

--- lldb/source/Plugins/SymbolFile/DWARF/DWARFASTParser.h
+++ lldb/source/Plugins/SymbolFile/DWARF/DWARFASTParser.h
@@ -29,7 +29,7 @@ class SymbolFileDWARF;
 
 class DWARFASTParser {
 public:
-  enum class Kind { DWARFASTParserClang, DWARFASTParserSwift };
+  enum class Kind { DWARFASTParserClang };
   DWARFASTParser(Kind kind) : m_kind(kind) {}
 
   virtual ~DWARFASTParser() = default;
