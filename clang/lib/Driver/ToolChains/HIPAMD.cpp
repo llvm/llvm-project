ad0c0a946a9e8d5fd8a293fe7ad7f58201546a20
Juan Manuel MARTINEZ CAAMAÑO <juamarti@amd.com>
2023-10-24 15:07:16 +0200

    [Clang][HIP] Do not link user defined static device libs (SDL) in clang-cc1
    Change-Id: I80ac1ecea332a8157b10b62766a83f934f6f793c

--- clang/lib/Driver/ToolChains/HIPAMD.cpp
+++ clang/lib/Driver/ToolChains/HIPAMD.cpp
@@ -243,13 +243,6 @@ HIPAMDToolChain::HIPAMDToolChain(const Driver &D, const llvm::Triple &Triple,
 void HIPAMDToolChain::addActionsFromClangTargetOptions(
     const llvm::opt::ArgList &DriverArgs, llvm::opt::ArgStringList &CC1Args,
     const JobAction &JA, Compilation &C, const InputInfoList &Inputs) const {
-  StringRef GpuArch = DriverArgs.getLastArgValue(options::OPT_mcpu_EQ);
-  if (!DriverArgs.hasFlag(options::OPT_fgpu_rdc, options::OPT_fno_gpu_rdc,
-                          false))
-    AddStaticDeviceLibsLinking(C, *getTool(JA.getKind()), JA, Inputs,
-                               DriverArgs, CC1Args, "amdgcn", GpuArch,
-                               /* bitcode SDL?*/ true,
-                               /* PostClang Link? */ false);
 }
 
 void HIPAMDToolChain::addClangTargetOptions(
