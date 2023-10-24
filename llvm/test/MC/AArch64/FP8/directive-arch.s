ea89a780178b097755f3dd40d701d47f58c49e4b
Hassnaa Hamdi <hassnaa.hamdi@arm.com>
2023-10-24 21:42:22 +0000

    [llvm][AArch64][Assembly]: Add FP8FMA  assembly and disassembly.
    This patch adds the feature flag FP8FMA and the assembly/disassembly
    for the following instructions of NEON and SVE2:
      * NEON:
        - FMLALBlane
        - FMLALTlane
        - FMLALLBBlane
        - FMLALLBTlane
        - FMLALLTBlane
        - FMLALLTTlane
        - FMLALB
        - FMLALT
        - FMLALLB
        - FMLALLBT
        - FMLALLTB
        - FMLALLTT
      * SVE2:
        - FMLALB_ZZZI
        - FMLALT_ZZZI
        - FMLALB_ZZZ
        - FMLALT_ZZZ
        - FMLALLBB_ZZZI
        - FMLALLBT_ZZZI
        - FMLALLTB_ZZZI
        - FMLALLTT_ZZZI
        - FMLALLBB_ZZZ
        - FMLALLBT_ZZZ
        - FMLALLTB_ZZZ
        - FMLALLTT_ZZZ
    
    That is according to this documentation:
    https://developer.arm.com/documentation/ddi0602/2023-09
    
    Change-Id: Ie7527ecd3be9de04a1176f41761986bb2e679fab

--- llvm/test/MC/AArch64/FP8/directive-arch.s
+++ llvm/test/MC/AArch64/FP8/directive-arch.s
@@ -3,11 +3,19 @@
 .arch armv9-a+fp8
 bf1cvtl v0.8h, v0.8b
 // CHECK: bf1cvtl v0.8h, v0.8b
-
 .arch armv9-a+nofp8
+
 .arch armv9-a+faminmax
 famax  v31.4h, v31.4h, v31.4h
 // CHECK: famax  v31.4h, v31.4h, v31.4h
-
 .arch armv9-a+nofaminmax
 
+.arch armv9-a+fp8fma
+fmlalb  v0.8h, v0.16b, v0.16b
+// CHECK: fmlalb  v0.8h, v0.16b, v0.16b
+.arch armv9-a+nofp8fma
+
+.arch armv9-a+ssve-fp8fma
+fmlalb z23.h, z13.b, z0.b[7]
+// CHECK: fmlalb z23.h, z13.b, z0.b[7]
+.arch armv9-a+nossve-fp8fma
