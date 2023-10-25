86dfaeb009c8621084f6e28f44cd2a14f6e0779d
Dinar Temirbulatov <dinar.temirbulatov@arm.com>
2023-10-25 13:14:34 +0000

    Resolving remarks.

--- llvm/lib/Transforms/Vectorize/VectorWiden.cpp
+++ llvm/lib/Transforms/Vectorize/VectorWiden.cpp
@@ -171,7 +171,9 @@ void VectorWiden::widenBinaryOperator(ArrayRef<Instruction *> IL) {
   ValueList VL;
   for (Instruction *I : IL)
     VL.push_back(I);
+
   propagateIRFlags(ResBinOp, VL);
+
   Function *ExtractIntr = llvm::Intrinsic::getDeclaration(
       F.getParent(), Intrinsic::vector_extract, {RetOrigType, RetType});
   if (!I->users().empty()) {
@@ -191,9 +193,6 @@ bool VectorWiden::canWidenNode(ArrayRef<Instruction *> IL,
   if (!OverrideTargetConsiderToWiden && !TTI.considerToWiden(Context, IL))
     return false;
 
-  if (!dyn_cast<VectorType>(IL[0]->getType()))
-    return false;
-
   for (int X = 0, E = IL.size(); X < E; X++) {
     for (int Y = 0, E = IL.size(); Y < E; Y++) {
       if (X == Y)
@@ -205,7 +204,7 @@ bool VectorWiden::canWidenNode(ArrayRef<Instruction *> IL,
            IL[Y]->getOperand(0)->getType()) ||
           // Ignore if disatance between two are too apart.
           (IL[Y]->comesBefore(IL[X]) &&
-           abs(std::distance(IL[Y]->getIterator(), IL[X]->getIterator())) >
+           std::distance(IL[Y]->getIterator(), IL[X]->getIterator()) >
                MaxInstDistance) ||
           (IL[X]->getOperand(0) == IL[Y] ||
            (IL[X]->getNumOperands() > 1 && IL[X]->getOperand(1) == IL[Y])))
@@ -261,6 +260,8 @@ bool VectorWiden::widenNode(ArrayRef<Instruction *> IL, LLVMContext &Context) {
         dbgs()
         << "VW: Decided to widen BinaryOp, safe to merge node starting with "
         << *IL[0] << "\n");
+    // We want to propagate here IR flags for the group of operations like
+    // "fast" flag for float pointer ones or "nuw" for integer instructions.
     widenBinaryOperator(IL);
     return true;
   }
@@ -271,81 +272,100 @@ InstructionCost VectorWiden::getOpCost(unsigned Opcode, Type *To, Type *From,
                                        Instruction *I) {
   InstructionCost Cost = 0;
   TTI::TargetCostKind CostKind = TTI::TCK_RecipThroughput;
-  switch (Opcode) {
-  case Instruction::FPTrunc: {
-    Cost = TTI.getCastInstrCost(Opcode, To, From, TTI::getCastContextHint(I),
-                                CostKind, I);
-    break;
-  }
-  case Instruction::Add: {
+  if (dyn_cast<BinaryOperator>(I)) {
     unsigned OpIdx = isa<UnaryOperator>(I) ? 0 : 1;
     TTI::OperandValueInfo Op1Info = TTI::getOperandInfo(I->getOperand(0));
     TTI::OperandValueInfo Op2Info = TTI::getOperandInfo(I->getOperand(OpIdx));
     SmallVector<const Value *> Operands(I->operand_values());
     Cost = TTI.getArithmeticInstrCost(I->getOpcode(), To, CostKind, Op1Info,
                                       Op2Info, Operands, I);
-    break;
-  }
-  default:
-    llvm_unreachable("Unknown instruction");
+  } else if (dyn_cast<CastInst>(I)) {
+    Cost = TTI.getCastInstrCost(Opcode, To, From, TTI::getCastContextHint(I),
+                                CostKind, I);
   }
   return Cost;
 }
 
-static bool isOperationSupported(unsigned Opcode) {
-  if (Opcode == Instruction::FPToUI || Opcode == Instruction::FPToSI ||
-      Opcode == Instruction::FPExt || Opcode == Instruction::SIToFP ||
-      Opcode == Instruction::UIToFP || Opcode == Instruction::Trunc ||
-      Opcode == Instruction::FPTrunc || Opcode == Instruction::BitCast)
+static bool isOperationSupported(Instruction *I) {
+  unsigned Opcode = I->getOpcode();
+  // Currently, we support only those operations, but later we could add more.
+  if (dyn_cast<VectorType>(I->getType()) &&
+      (I->isBinaryOp() || Opcode == Instruction::SExt ||
+       Opcode == Instruction::ZExt || Opcode == Instruction::FPToUI ||
+       Opcode == Instruction::FPToSI || Opcode == Instruction::FPExt ||
+       Opcode == Instruction::SIToFP || Opcode == Instruction::UIToFP ||
+       Opcode == Instruction::Trunc || Opcode == Instruction::FPTrunc ||
+       Opcode == Instruction::BitCast))
     return true;
   return false;
 }
 
 bool VectorWiden::processBB(BasicBlock &BB, LLVMContext &Context) {
   struct Operation {
-    // Distance between the first operation in the list of operations
-    // and the last instruction in the current basic block.
+    // Position where the first operation, in the list of operations,
+    // was discovered and the last instruction in the current basic block.
     unsigned Position;
     InstrList Ops;
   };
+  // The key is opertion opcode.
+  // The value is a list of operations with the first operation position in
+  // the basic block.
   DenseMap<unsigned, Operation> Operations;
-  unsigned InstrDistance = 0;
+  Instruction *LastInstr = BB.getTerminator();
+  unsigned CurrentPosition = 0;
   for (BasicBlock::reverse_iterator IP(BB.rbegin()); IP != BB.rend();
-       *IP++, ++InstrDistance) {
+       *IP++, ++CurrentPosition) {
     Instruction *I = &*IP;
     unsigned OpFound = 0;
 
-    if (I->isDebugOrPseudoInst() || isDeleted(I))
+    if (I->isDebugOrPseudoInst() || isDeleted(I) || !isOperationSupported(I))
       continue;
 
     unsigned Opcode = I->getOpcode();
-    if (!dyn_cast<BinaryOperator>(I) && !isOperationSupported(Opcode))
-      continue;
-
-    if (Operations.contains(I->getOpcode())) {
-      Operation *OpRec = &Operations[I->getOpcode()];
-      // If instructions are too apart then remove old instrction
-      // and reset position to this instruction.
-      if (InstrDistance - OpRec->Position > MaxInstDistance) {
-        OpRec->Ops.erase(OpRec->Ops.begin());
-
+    if (Operations.contains(Opcode)) {
+      Operation *OpRec = &Operations[Opcode];
+      // If instructions are too apart then remove old instruction
+      // and reset position to the next instruction in the list instruction.
+      if (CurrentPosition - OpRec->Position > MaxInstDistance) {
+        unsigned NumToDelete = 0;
         for (InstrList::iterator It = OpRec->Ops.begin();
              It != OpRec->Ops.end(); ++It) {
           Instruction *Instr = *It;
-          if (std::distance(Instr, &(*BB.end())) > MaxInstDistance)
-            OpRec->Ops.erase(It);
+          unsigned NewPosition =
+              std::distance(Instr->getIterator(), LastInstr->getIterator());
+          if (CurrentPosition - NewPosition > MaxInstDistance) {
+            NumToDelete++;
+          } else {
+            // Updating Position value to next remaining in range opertion.
+            OpRec->Position = NewPosition;
+            LLVM_DEBUG(dbgs() << "VW: Updating node starting with "
+                              << **(OpRec->Ops.begin())
+                              << " position to : " << NewPosition << "\n");
+            break;
+          }
         }
-        // If no operations left in the list set position
-        // to the current.
-        if (!OpRec->Ops.size())
-          OpRec->Position = InstrDistance;
-        OpRec->Ops.push_back(I);
-      } else {
-        OpRec->Ops.push_back(I);
-        OpFound = I->getOpcode();
+        for (unsigned i = 0; i < NumToDelete; ++i) {
+          LLVM_DEBUG(dbgs()
+                     << "VW: Deleting operation " << **(OpRec->Ops.begin())
+                     << " from node as out of range."
+                     << "\n");
+          OpRec->Ops.erase(OpRec->Ops.begin());
+        }
       }
+      // If no operations left in the list, set position to the current.
+      if (!OpRec->Ops.size())
+        OpRec->Position = CurrentPosition;
+      OpRec->Ops.push_back(I);
+      LLVM_DEBUG(dbgs() << "VW: Found operation " << *I
+                        << " to add to existing node starting at "
+                        << **(OpRec->Ops.begin()) << " at : " << OpRec->Position
+                        << "\n");
+      if (OpRec->Ops.size() > 1)
+        OpFound = Opcode;
     } else {
-      Operations[I->getOpcode()] = {InstrDistance, {I}};
+      LLVM_DEBUG(dbgs() << "VW: Found operation " << *I
+                        << " to form a node at : " << CurrentPosition << "\n");
+      Operations[Opcode] = {CurrentPosition, {I}};
     }
 
     if (OpFound && Operations.contains(OpFound)) {
@@ -353,9 +373,15 @@ bool VectorWiden::processBB(BasicBlock &BB, LLVMContext &Context) {
       for (Instruction *Op : OpRec->Ops)
         LLVM_DEBUG(dbgs() << "VW: operation to check : " << *Op << "\n");
       if (!widenNode(OpRec->Ops, Context)) {
-        LLVM_DEBUG(dbgs() << "VW: Unable to construct the tree.\n");
-        OpRec->Ops.erase(OpRec->Ops.begin());
-        OpRec->Position = InstrDistance;
+        LLVM_DEBUG(dbgs() << "VW: Unable use a wider vector for vector ops.\n");
+        if (OpRec->Ops.size() > 4) {
+          LLVM_DEBUG(dbgs() << "VW: Deleting operation "
+                            << **(OpRec->Ops.begin()) << " as unable to widen."
+                            << "\n");
+          OpRec->Ops.erase(OpRec->Ops.begin());
+          OpRec->Position = std::distance(
+              (*(OpRec->Ops.begin()))->getIterator(), LastInstr->getIterator());
+        }
       } else {
         for (Instruction *Instr : OpRec->Ops)
           eraseInstruction(Instr);
@@ -374,6 +400,8 @@ bool VectorWiden::run() {
   for (BasicBlock &BB : F) {
     LLVM_DEBUG(dbgs() << "VW: BB:" << BB.getName() << "\n");
 
+    // If any transformation is done, then we have to start all over again,
+    // since we generate new instructions.
     while (processBB(BB, Context))
       Changed = true;
   }
