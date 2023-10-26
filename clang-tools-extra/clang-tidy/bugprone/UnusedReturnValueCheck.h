ee2e8d19ef395cc6cd1012e2eb5c4fece07aa1ce
Piotr Zegar <piotr.zegar@nokia.com>
2023-10-26 22:58:20 +0200

    [clang-tidy] Improve bugprone-unused-return-value check (#66573)
    Improve diagnostic message to be more straight forward, fix handling of 
    casting to non-void and add new option AllowCastToVoid to control
    casting
    to void behavior.
    
    Closes #66570

--- clang-tools-extra/clang-tidy/bugprone/UnusedReturnValueCheck.h
+++ clang-tools-extra/clang-tidy/bugprone/UnusedReturnValueCheck.h
@@ -24,10 +24,14 @@ public:
   void storeOptions(ClangTidyOptions::OptionMap &Opts) override;
   void registerMatchers(ast_matchers::MatchFinder *Finder) override;
   void check(const ast_matchers::MatchFinder::MatchResult &Result) override;
+  std::optional<TraversalKind> getCheckTraversalKind() const override {
+    return TK_IgnoreUnlessSpelledInSource;
+  }
 
 private:
   std::string CheckedFunctions;
   const std::vector<StringRef> CheckedReturnTypes;
+  const bool AllowCastToVoid;
 };
 
 } // namespace clang::tidy::bugprone
