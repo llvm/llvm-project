461790881cc95675a88812b2c2d6c691c4ff4aa1
Congcong Cai <congcongcai0907@163.com>
2023-10-27 23:01:01 +0800

    move test

--- clang/test/SemaCXX/gh67687.cpp
+++
@@ -1,7 +0,0 @@
-// RUN: %clang_cc1 -verify -fsyntax-only -std=c++17 %s
-
-struct S {
-  int n;
-  int d = (4, []() { return n; }());  // expected-error {{'this' cannot be implicitly captured in this context}} \
-                                      // expected-note {{explicitly capture 'this'}}
-};
