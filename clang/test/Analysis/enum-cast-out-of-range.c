af9baa208cd1b61af0ce1212fe132cf466bd653a
Endre Fülöp <endre.fulop@sigmatechnology.se>
2023-10-27 12:29:19 +0200

    Add test cases for anonymous enums

--- clang/test/Analysis/enum-cast-out-of-range.c
+++ clang/test/Analysis/enum-cast-out-of-range.c
@@ -41,6 +41,16 @@ void unusedExpr(void) {
   unused; // expected-warning {{expression result unused}}
 }
 
+// Test typedef-ed anonymous enums
+typedef enum { // expected-note {{enum declared here}}
+    TD_0 = 0,
+} TD_t;
+
+void testTypeDefEnum(void) {
+  (void)(TD_t)(-1); // expected-warning {{not in the valid range of values for the enum}}
+                    // expected-note@-1 {{not in the valid range of values for the enum}}
+}
+
 // Test expression tracking
 void set(int* p, int v) {
   *p = v; // expected-note {{The value -1 is assigned to 'i'}}
