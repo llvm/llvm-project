8564f7982b3ae6a63a042ff9d5cf6491363b32a0
Luke Lau <luke@igalia.com>
2023-10-27 15:31:49 +0100

    Fix comment

--- llvm/lib/Target/RISCV/RISCVFoldMasks.cpp
+++ llvm/lib/Target/RISCV/RISCVFoldMasks.cpp
@@ -1,4 +1,4 @@
-//===- RISCVOptWInstrs.cpp - MI W instruction optimizations ---------------===//
+//===- RISCVFoldMasks.cpp - MI Vector Pseudo Mask Peepholes ---------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
