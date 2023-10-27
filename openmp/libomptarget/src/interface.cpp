bf5d83c3f879a649638bab2a4c75241e23bb6dab
fel-cab <fel-cab@github.com>
2023-10-27 20:58:26 +0000

    Adding kernel function name to the slice name

--- openmp/libomptarget/src/interface.cpp
+++ openmp/libomptarget/src/interface.cpp
@@ -65,7 +65,7 @@ targetData(ident_t *Loc, int64_t DeviceId, int32_t ArgNum, void **ArgsBase,
   static_assert(std::is_convertible_v<TargetAsyncInfoTy, AsyncInfoTy>,
                 "TargetAsyncInfoTy must be convertible to AsyncInfoTy.");
 
-  TIMESCOPE_WITH_DETAILS_AND_IDENT("Runtime: Data Copy",
+  TIMESCOPE_WITH_DETAILS_AND_IDENT("Runtime Data Copy:",
                                    "NumArgs=" + std::to_string(ArgNum), Loc);
 
   DP("Entering data %s region for device %" PRId64 " with %d mappings\n",
@@ -263,7 +263,7 @@ static inline int targetKernel(ident_t *Loc, int64_t DeviceId, int32_t NumTeams,
          !KernelArgs->ThreadLimit[1] && !KernelArgs->ThreadLimit[2] &&
          "OpenMP interface should not use multiple dimensions");
   TIMESCOPE_WITH_DETAILS_AND_IDENT(
-      "Runtime target exe",
+      "Runtime target exe:",
       "NumTeams=" + std::to_string(NumTeams) +
           ";NumArgs=" + std::to_string(KernelArgs->NumArgs),
       Loc);
