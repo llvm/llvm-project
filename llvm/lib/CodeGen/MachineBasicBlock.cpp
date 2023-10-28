6ad0d6cacb76a370304fb0b49f695ba2a82aa994
Rahman Lavaee <rahmanl@google.com>
2023-10-27 21:49:39 -0700

    [BasicBlockSections] Apply path cloning with -basic-block-sections. (#68860)
    https://github.com/llvm/llvm-project/commit/11f45d0569cb7946096ce3b6d653cc6c99d70e2f
    introduced the path cloning format in the basic-block-sections profile.
    
    This PR validates and applies path clonings. 
    A path cloning is valid if all of these conditions hold:
      1. All bb ids in the path are mapped to existing blocks.
    2. Each two consecutive bb ids in the path have a successor relationship
    in the CFG.
    3. The path does not include a block with indirect branches, except
    possibly as the last block.
     
    Applying a path cloning involves cloning all blocks in the path (except
    the first one) and setting up their branches.
    Once all clonings are applied, the cluster information is used to guide
    block layout in the modified function.

--- llvm/lib/CodeGen/MachineBasicBlock.cpp
+++ llvm/lib/CodeGen/MachineBasicBlock.cpp
@@ -567,7 +567,9 @@ void MachineBasicBlock::printName(raw_ostream &os, unsigned printNameFlags,
     }
     if (getBBID().has_value()) {
       os << (hasAttributes ? ", " : " (");
-      os << "bb_id " << *getBBID();
+      os << "bb_id " << getBBID()->BaseID;
+      if (getBBID()->CloneID != 0)
+        os << " " << getBBID()->CloneID;
       hasAttributes = true;
     }
     if (CallFrameSize != 0) {
@@ -886,7 +888,7 @@ void MachineBasicBlock::replaceSuccessor(MachineBasicBlock *Old,
   removeSuccessor(OldI);
 }
 
-void MachineBasicBlock::copySuccessor(MachineBasicBlock *Orig,
+void MachineBasicBlock::copySuccessor(const MachineBasicBlock *Orig,
                                       succ_iterator I) {
   if (!Orig->Probs.empty())
     addSuccessor(*I, Orig->getSuccProbability(I));
