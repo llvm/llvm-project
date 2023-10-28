f4263f7932558baed0eaaf1aef9f5151a578ccea
“Nhat <nhat7203@gmail.com>
2023-10-28 14:11:01 -0400

    add static assert for floating point type for uniform distribution

--- libcxx/include/__random/uniform_real_distribution.h
+++ libcxx/include/__random/uniform_real_distribution.h
@@ -27,6 +27,8 @@ _LIBCPP_BEGIN_NAMESPACE_STD
 template<class _RealType = double>
 class _LIBCPP_TEMPLATE_VIS uniform_real_distribution
 {
+  static_assert(std::is_floating_type<_RealType>::value, "result_type must be of type floating point");
+
 public:
     // types
     typedef _RealType result_type;
