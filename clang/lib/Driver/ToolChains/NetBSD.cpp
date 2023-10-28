b2ea95ae0ad8ff7bd46622d4f5b8158f91cf46da
Brad Smith <brad@comstyle.com>
2023-10-27 22:10:05 -0400

    [Driver][NFC] Fix a typo in the function name

--- clang/lib/Driver/ToolChains/NetBSD.cpp
+++ clang/lib/Driver/ToolChains/NetBSD.cpp
@@ -318,7 +318,7 @@ void netbsd::Linker::ConstructJob(Compilation &C, const JobAction &JA,
     // Additional linker set-up and flags for Fortran. This is required in order
     // to generate executables. As Fortran runtime depends on the C runtime,
     // these dependencies need to be listed before the C runtime below (i.e.
-    // AddRuntTimeLibs).
+    // AddRunTimeLibs).
     if (D.IsFlangMode()) {
       addFortranRuntimeLibraryPath(ToolChain, Args, CmdArgs);
       addFortranRuntimeLibs(ToolChain, CmdArgs);
