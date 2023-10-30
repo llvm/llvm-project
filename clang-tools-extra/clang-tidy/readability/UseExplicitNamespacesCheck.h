5af593d18e18a9e4afdff004fe85f1a80d7918a1
Daniel Walter <dwalter@altair.com>
2023-10-30 00:22:17 -0500

    Initial commit of changes for explicit namespace check changes

--- clang-tools-extra/clang-tidy/readability/UseExplicitNamespacesCheck.h
+++ clang-tools-extra/clang-tidy/readability/UseExplicitNamespacesCheck.h
@@ -19,10 +19,44 @@ namespace clang::tidy::readability {
 /// http://clang.llvm.org/extra/clang-tidy/checks/readability/use-explicit-namespaces.html
 class UseExplicitNamespacesCheck : public ClangTidyCheck {
 public:
-  UseExplicitNamespacesCheck(StringRef Name, ClangTidyContext *Context)
-      : ClangTidyCheck(Name, Context) {}
+  UseExplicitNamespacesCheck(StringRef Name, ClangTidyContext *Context);
   void registerMatchers(ast_matchers::MatchFinder *Finder) override;
   void check(const ast_matchers::MatchFinder::MatchResult &Result) override;
+
+  void storeOptions(ClangTidyOptions::OptionMap &Opts) override;
+  bool isLanguageVersionSupported(const LangOptions &LangOpts) const override {
+    return LangOpts.CPlusPlus;
+  }
+
+  IdentifierInfo *
+  getTypeNestedNameIdentfierRecursive(const Type *type,
+                                      std::string &nestedTypeInfo);
+  bool matchesNamespaceLimits(
+      const std::vector<const DeclContext *> &targetContextVector);
+
+  void diagOut(const SourceLocation &sourcePosition,
+               const std::string &message);
+
+  void processTransform(NestedNameSpecifierLoc nestedName,
+                        const SourceLocation &sourcePosition,
+                        const NamedDecl *target,
+                        const DeclContext *referenceContext, bool usingShadow,
+                        const std::string &context);
+
+  void processTypePiecesRecursive(NestedNameSpecifierLoc nestedName,
+                                  const TypeLoc &typeLoc,
+                                  const DeclContext *declContext,
+                                  const std::string &context);
+
+  void processTypePieces(TypeSourceInfo *typeSourceInfo,
+                         const DeclContext *declContext,
+                         const std::string &context);
+
+private:
+  const StringRef limitToPattern;
+  std::vector<std::string> limitToPatternVector;
+  bool onlyExpandUsingNamespace;
+  const unsigned diagnosticLevel;
 };
 
 } // namespace clang::tidy::readability
