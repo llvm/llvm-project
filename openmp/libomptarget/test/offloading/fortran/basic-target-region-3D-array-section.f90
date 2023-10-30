18b43c97d8cc3064f4f20b662029a01e19f9495e
Andrew Gozillon <Andrew.Gozillon@amd.com>
2023-10-30 14:51:47 -0500

    [Flang][MLIR][OpenMP] Temporarily re-add basic handling of uses in target regions to avoid gfortran test-suite regressions
    This was a regression introduced by myself in:
    
     https://github.com/llvm/llvm-project/commit/6a62707c048e16ce9bad37ed8e3520799139436b
    
    where I too hastily removed the basic handling of implicit captures
    we have currently. This will be superseded by all implicit captures
    being added to target operations map_info entries in a soon landing
    series of patches, however, that is currently not the case so we must
    continue to do some basic handling of these captures for the time
    being. This patch re-adds that behaviour to avoid regressions.
    
    Unfortunately this means some test changes as well as
    getUsedValuesDefinedAbove grabs constants used outside
    of the target region which aren't handled particularly
    well currently.

--- openmp/libomptarget/test/offloading/fortran/basic-target-region-3D-array-section.f90
+++ openmp/libomptarget/test/offloading/fortran/basic-target-region-3D-array-section.f90
@@ -14,6 +14,7 @@ program main
     integer :: inArray(3,3,3)
     integer :: outArray(3,3,3)
     integer :: i, j, k 
+    integer :: j2 = 3, k2 = 3
 
     do i = 1, 3
       do j = 1, 3
@@ -24,11 +25,16 @@ program main
        end do
     end do
 
-!$omp target map(tofrom:inArray(1:3, 1:3, 2:2), outArray(1:3, 1:3, 1:3), j, k)
-    do j = 1, 3
-      do k = 1, 3
+j = 1
+k = 1 
+!$omp target map(tofrom:inArray(1:3, 1:3, 2:2), outArray(1:3, 1:3, 1:3), j, k, j2, k2)
+    do while (j <= j2)
+      k = 1
+      do while (k <= k2)
         outArray(k, j, 2) = inArray(k, j, 2)
+        k = k + 1
       end do
+      j = j + 1
     end do
 !$omp end target
 
