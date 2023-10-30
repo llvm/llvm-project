daef3ccf107ced30f163041407d9c38768e6c4e9
Vlad Serebrennikov <serebrennikov.vladislav@gmail.com>
2023-10-30 21:15:56 +0300

    Remove unnecessary `typename` keyword

--- llvm/include/llvm/ADT/STLForwardCompat.h
+++ llvm/include/llvm/ADT/STLForwardCompat.h
@@ -60,11 +60,12 @@ auto transformOptional(std::optional<T> &&O, const Function &F)
   return std::nullopt;
 }
 
-/// Returns underlying integer value of an enum. Backport of C++23 std::to_underlying.
+/// Returns underlying integer value of an enum. Backport of C++23
+/// std::to_underlying.
 template <typename Enum>
-[[nodiscard]] constexpr typename std::underlying_type_t<Enum>
+[[nodiscard]] constexpr std::underlying_type_t<Enum>
 to_underlying(Enum E) {
-  return static_cast<typename std::underlying_type_t<Enum>>(E);
+  return static_cast<std::underlying_type_t<Enum>>(E);
 }
 
 } // namespace llvm
