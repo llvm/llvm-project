6877eb24cb98a20e047c0a5f0b1644085acba2c2
Peter Klausler <35819229+klausler@users.noreply.github.com>
2023-10-31 16:04:35 -0700

    [flang] Accept CONTIGUOUS attribute when redundant (#70853)
    As an extension, accept the redundant use of the CONTIGUOUS attribute
    when applied to scalars and to simply contiguous objects, with a
    portability warning.

--- flang/test/Semantics/bind-c13.f90
+++ flang/test/Semantics/bind-c13.f90
@@ -1,12 +1,12 @@
-! RUN: %python %S/test_errors.py %s %flang_fc1
+! RUN: %python %S/test_errors.py %s %flang_fc1 -pedantic
 ! Interoperable objects that require descriptors cannot be CONTIGUOUS
 subroutine interop(ptr,ashape,arank,eshape,asize) bind(c)
   !ERROR: An interoperable pointer must not be CONTIGUOUS
   real, pointer, contiguous :: ptr(:)
   real, contiguous :: ashape(:) ! ok
   real, contiguous :: arank(..) ! ok
-  !ERROR: CONTIGUOUS entity 'eshape' must be an array pointer, assumed-shape, or assumed-rank
+  !PORTABILITY: CONTIGUOUS entity 'eshape' should be an array pointer, assumed-shape, or assumed-rank
   real, contiguous :: eshape(10)
-  !ERROR: CONTIGUOUS entity 'asize' must be an array pointer, assumed-shape, or assumed-rank
+  !PORTABILITY: CONTIGUOUS entity 'asize' should be an array pointer, assumed-shape, or assumed-rank
   real, contiguous :: asize(*)
 end
