f37882cb63ca860c1dc0254ae5849bc875e1abe2
Aleksandr Popov <42888396+aleks-tmb@users.noreply.github.com>
2023-10-31 14:02:42 +0100

    [LoopPeeling] Fix weights updating of peeled off branches (#70094)
    In https://reviews.llvm.org/D64235 a new algorithm has been introduced
    for updating the branch weights of latch blocks and their copies.
    
    It increases the probability of going to the exit block for each next
    peel iteration, calculating weights by (F - I * E, E), where:
    - F is a weight of the edge from latch to header.
    - E is a weight of the edge from latch to exit.
    - I is a number of peeling iteration.
    
    E.g: Let's say the latch branch weights are (100,300) and the estimated
    trip count is 4. If we peel off all 4 iterations the weights of the
    copied branches will be:
    0: (100,300)
    1: (100,200)
    2: (100,100)
    3: (100,1)
    
    https://godbolt.org/z/93KnoEsT6
    
    So we make the original loop almost unreachable from the 3rd peeled copy
    according to the profile data. But that's only true if the profiling
    data is accurate.
    Underestimated trip count can lead to a performance issues with the
    register allocator, which may decide to spill intervals inside the loop
    assuming it's unreachable.
    
    Since we don't know how accurate the profiling data is, it seems better
    to set neutral 1/1 weights on the last peeled latch branch. After this
    change, the weights in the example above will look like this:
    0: (100,300)
    1: (100,200)
    2: (100,100)
    3: (100,100)
    
    Co-authored-by: Aleksandr Popov <apopov@azul.com>

--- llvm/lib/Transforms/Utils/LoopPeel.cpp
+++ llvm/lib/Transforms/Utils/LoopPeel.cpp
@@ -636,9 +636,14 @@ static void updateBranchWeights(Instruction *Term, WeightInfo &Info) {
                     MDB.createBranchWeights(Info.Weights));
   for (auto [Idx, SubWeight] : enumerate(Info.SubWeights))
     if (SubWeight != 0)
-      Info.Weights[Idx] = Info.Weights[Idx] > SubWeight
-                              ? Info.Weights[Idx] - SubWeight
-                              : 1;
+      // Don't set the probability of taking the edge from latch to loop header
+      // to less than 1:1 ratio (meaning Weight should not be lower than
+      // SubWeight), as this could significantly reduce the loop's hotness,
+      // which would be incorrect in the case of underestimating the trip count.
+      Info.Weights[Idx] =
+          Info.Weights[Idx] > SubWeight
+              ? std::max(Info.Weights[Idx] - SubWeight, SubWeight)
+              : SubWeight;
 }
 
 /// Initialize the weights for all exiting blocks.
