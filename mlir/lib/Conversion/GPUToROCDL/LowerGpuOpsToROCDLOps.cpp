4279a642fba2d107e9bef17376facf9350dae6a9
Christian Ulmann <christian.ulmann@nextsilicon.com>
2023-11-01 10:13:06 +0100

    [MLIR][GPUToROCDL] Remove typed pointer support (#70908)
    This commit removes the support for lowering GPU to ROCDL dialect with
    typed pointers. Typed pointers have been deprecated for a while now and
    it's planned to soon remove them from the LLVM dialect.
    
    Related PSA:
    https://discourse.llvm.org/t/psa-removal-of-typed-pointers-from-the-llvm-dialect/74502

--- mlir/lib/Conversion/GPUToROCDL/LowerGpuOpsToROCDLOps.cpp
+++ mlir/lib/Conversion/GPUToROCDL/LowerGpuOpsToROCDLOps.cpp
@@ -229,7 +229,6 @@ struct LowerGpuOpsToROCDLOpsPass
         ctx, DataLayout(cast<DataLayoutOpInterface>(m.getOperation())));
     if (indexBitwidth != kDeriveIndexBitwidthFromDataLayout)
       options.overrideIndexBitwidth(indexBitwidth);
-    options.useOpaquePointers = useOpaquePointers;
 
     if (useBarePtrCallConv) {
       options.useBarePtrCallConv = true;
