256d85ba3fe4f2cbf1ffe680d73c20dab423c3a6
erichkeane <ekeane@nvidia.com>
2023-11-02 06:54:58 -0700

    Fixes suggested by Bruno

--- clang/test/Preprocessor/openacc.c
+++ clang/test/Preprocessor/openacc.c
@@ -4,3 +4,10 @@
 // DEFAULT: OpenACC:1:
 // OVERRIDE: OpenACC:202211:
 OpenACC:_OPENACC:
+
+// RUN: %clang_cc1 -E -dM -fopenacc %s | FileCheck %s --check-prefix=MACRO_PRINT_DEF
+// RUN: %clang_cc1 -E -dM -fopenacc -fexperimental-openacc-macro-override 202211 %s | FileCheck %s --check-prefix=MACRO_PRINT_OVR
+// MACRO_PRINT_DEF: #define _OPENACC 1
+// MACRO_PRINT_OVR: #define _OPENACC 202211
+
+
