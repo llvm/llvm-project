94a0d58bbe9bb56505af7e90394e219cac85f3bc
Will Hawkins <hawkinsw@obs.cr>
2023-11-03 09:05:01 -0400

    fixup! WIP: [libc++][ranges] Implement `ranges::stride_view`.
    Add assertion to __iterator::operator+= for valid stride length and test.

---
+++ libcxx/test/libcxx/ranges/range.adaptors/range.stride.view/iterator/operator_plus_equal.pass.cpp
@@ -0,0 +1,30 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++03, c++11, c++14, c++17, c++20
+// UNSUPPORTED: no-exceptions
+// UNSUPPORTED: !libcpp-hardening-mode=debug
+// XFAIL: availability-verbose_abort-missing
+
+// <ranges>
+
+// Call ....
+
+#include "check_assertion.h"
+#include <ranges>
+
+void operator_plus_equal_past_end_is_illegal() {
+  int range[]   = {1, 2, 3};
+  auto striv    = std::ranges::views::stride(range, 2);
+  auto striv_it = striv.begin();
+  TEST_LIBCPP_ASSERT_FAILURE(striv_it += 3, "Advancing the iterator beyond the end is not allowed.");
+}
+int main() {
+  operator_plus_equal_past_end_is_illegal();
+  return 0;
+}
