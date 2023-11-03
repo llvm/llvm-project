ac85f3ce8148de6b068b48275fd923619e3ff359
Nikita Popov <npopov@redhat.com>
2023-11-03 14:47:27 +0100

    [IR] Remove ZExtOperator (NFC)
    Now that zext constant expressions are no longer supported,
    ZExtInst should be used instead.

--- llvm/lib/Transforms/InstCombine/InstructionCombining.cpp
+++ llvm/lib/Transforms/InstCombine/InstructionCombining.cpp
@@ -916,7 +916,7 @@ InstCombinerImpl::foldBinOpOfSelectAndCastOfSelectCondition(BinaryOperator &I) {
 
   auto NewFoldedConst = [&](bool IsTrueArm, Value *V) {
     bool IsCastOpRHS = (CastOp == RHS);
-    bool IsZExt = isa<ZExtOperator>(CastOp);
+    bool IsZExt = isa<ZExtInst>(CastOp);
     Constant *C;
 
     if (IsTrueArm) {
