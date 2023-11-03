e26b213d1556f91e2fb04d880939bd2c7f00b506
Jonas Devlieghere <jonas@devlieghere.com>
2023-11-03 14:35:34 -0700

    [lldb] Remove LocateSymbolFile
    This completes the conversion of LocateSymbolFile into a SymbolLocator
    plugin. The only remaining function is DownloadSymbolFileAsync which
    doesn't really fit into the plugin model, and therefore moves into the
    SymbolLocator class, while still relying on the plugins to do the
    underlying work.

--- lldb/include/lldb/Symbol/SymbolLocator.h
+++ lldb/include/lldb/Symbol/SymbolLocator.h
@@ -10,12 +10,20 @@
 #define LLDB_SYMBOL_SYMBOLLOCATOR_H
 
 #include "lldb/Core/PluginInterface.h"
+#include "lldb/Utility/UUID.h"
 
 namespace lldb_private {
 
 class SymbolLocator : public PluginInterface {
 public:
   SymbolLocator() = default;
+
+  /// Locate the symbol file for the given UUID on a background thread. This
+  /// function returns immediately. Under the hood it uses the debugger's
+  /// thread pool to call DownloadObjectAndSymbolFile. If a symbol file is
+  /// found, this will notify all target which contain the module with the
+  /// given UUID.
+  static void DownloadSymbolFileAsync(const UUID &uuid);
 };
 
 } // namespace lldb_private
