bdff060e187fd1d366f649373d838b8df70340f8
Heejin Ahn <aheejin@gmail.com>
2023-11-03 16:37:36 -0700

    [sanitizer] Fix pthread_exit interceptor's return type
    `pthread_exit`'s return type is void.

--- compiler-rt/lib/hwasan/hwasan_interceptors.cpp
+++ compiler-rt/lib/hwasan/hwasan_interceptors.cpp
@@ -322,9 +322,9 @@ INTERCEPTOR(int, pthread_detach, void *thread) {
   return result;
 }
 
-INTERCEPTOR(int, pthread_exit, void *retval) {
+INTERCEPTOR(void, pthread_exit, void *retval) {
   hwasanThreadArgRetval().Finish(GetThreadSelf(), retval);
-  return REAL(pthread_exit)(retval);
+  REAL(pthread_exit)(retval);
 }
 
 #    if SANITIZER_GLIBC
