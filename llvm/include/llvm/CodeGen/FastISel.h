f2f6a474ee404c40f59868e6d9a8ae858f0b7015
OCHyams <orlando.hyams@sony.com>
2023-11-03 15:58:44 +0000

    FastISel - code changes

--- llvm/include/llvm/CodeGen/FastISel.h
+++ llvm/include/llvm/CodeGen/FastISel.h
@@ -319,6 +319,8 @@ public:
   /// Reset InsertPt to the given old insert position.
   void leaveLocalValueArea(SavePoint Old);
 
+  void handleDbgInfo(const Instruction *II);
+
 protected:
   explicit FastISel(FunctionLoweringInfo &FuncInfo,
                     const TargetLibraryInfo *LibInfo,
@@ -341,6 +343,18 @@ protected:
   /// This method is called by target-independent code to do target-
   /// specific intrinsic lowering. It returns true if it was successful.
   virtual bool fastLowerIntrinsicCall(const IntrinsicInst *II);
+
+  /// This method is called by target-independent code to do target-specific
+  /// lowering of debug information. It returns false if the debug information
+  /// couldn't be lowered and was instead discarded.
+  virtual bool lowerDbgValue(const Value *V, DIExpression *Expr,
+                             DILocalVariable *Var, const DebugLoc &DL);
+
+  /// This method is called by target-independent code to do target-specific
+  /// lowering of debug information. It returns false if the debug information
+  /// couldn't be lowered and was instead discarded.
+  virtual bool lowerDbgDeclare(const Value *V, DIExpression *Expr,
+                               DILocalVariable *Var, const DebugLoc &DL);
 
   /// This method is called by target-independent code to request that an
   /// instruction with the given type and opcode be emitted.
