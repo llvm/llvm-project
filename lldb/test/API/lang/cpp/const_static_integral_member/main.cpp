5f86b49146902a37ea93470b5992206b20665128
Michael Buch <michaelbuch12@gmail.com>
2023-11-06 10:56:49 +0000

    Revert "[lldb][DWARFASTParserClang] Fetch constant value from variable defintion if available (#71004)"
    This reverts commit ef3febadf606c2fc4f1ad8d85a7ecdde16e4cbb3.
    
    This caused an LLDB test failure on Linux for `lang/cpp/symbols/TestSymbols.test_dwo`:
    
    ```
    make: Leaving directory '/home/worker/2.0.1/lldb-x86_64-debian/build/lldb-test-build.noindex/lang/cpp/symbols/TestSymbols.test_dwo'
    runCmd: expression -- D::i
    PLEASE submit a bug report to https://github.com/llvm/llvm-project/issues/ and include the crash backtrace.
    Stack dump:
    0.	HandleCommand(command = "expression -- D::i")
    1.	<user expression 0>:1:4: current parser token 'i'
    2.	<lldb wrapper prefix>:44:1: parsing function body '$__lldb_expr'
    3.	<lldb wrapper prefix>:44:1: in compound statement ('{}')
    Stack dump without symbol names (ensure you have llvm-symbolizer in your PATH or set the environment var `LLVM_SYMBOLIZER_PATH` to point to it):
    0  _lldb.cpython-39-x86_64-linux-gnu.so 0x00007fbcfcb08b87
    1  _lldb.cpython-39-x86_64-linux-gnu.so 0x00007fbcfcb067ae
    2  _lldb.cpython-39-x86_64-linux-gnu.so 0x00007fbcfcb0923f
    3  libpthread.so.0                      0x00007fbd07ab7140
    ```
    
    And a failure in `TestCallStdStringFunction.py` on Linux aarch64:
    ```
    --
    Exit Code: -11
    
    Command Output (stdout):
    --
    lldb version 18.0.0git (https://github.com/llvm/llvm-project.git revision ef3febadf606c2fc4f1ad8d85a7ecdde16e4cbb3)
      clang revision ef3febadf606c2fc4f1ad8d85a7ecdde16e4cbb3
      llvm revision ef3febadf606c2fc4f1ad8d85a7ecdde16e4cbb3
    
    --
    Command Output (stderr):
    --
    PLEASE submit a bug report to https://github.com/llvm/llvm-project/issues/ and include the crash backtrace.
    Stack dump:
    0.      HandleCommand(command = "expression str")
    1.      <lldb wrapper prefix>:45:34: current parser token ';'
    2.      <lldb wrapper prefix>:44:1: parsing function body '$__lldb_expr'
    3.      <lldb wrapper prefix>:44:1: in compound statement ('{}')
      #0 0x0000ffffb72a149c llvm::sys::PrintStackTrace(llvm::raw_ostream&, int) (/home/tcwg-buildbot/worker/lldb-aarch64-ubuntu/build/lib/python3.8/site-packages/lldb/_[lldb.cpython-38-aarch64-linux-gnu.so](http://lldb.cpython-38-aarch64-linux-gnu.so/)+0x58c749c)
      #1 0x0000ffffb729f458 llvm::sys::RunSignalHandlers() (/home/tcwg-buildbot/worker/lldb-aarch64-ubuntu/build/lib/python3.8/site-packages/lldb/_[lldb.cpython-38-aarch64-linux-gnu.so](http://lldb.cpython-38-aarch64-linux-gnu.so/)+0x58c5458)
      #2 0x0000ffffb72a1bd0 SignalHandler(int) (/home/tcwg-buildbot/worker/lldb-aarch64-ubuntu/build/lib/python3.8/site-packages/lldb/_[lldb.cpython-38-aarch64-linux-gnu.so](http://lldb.cpython-38-aarch64-linux-gnu.so/)+0x58c7bd0)
      #3 0x0000ffffbdd9e7dc (linux-vdso.so.1+0x7dc)
      #4 0x0000ffffb71799d8 lldb_private::plugin::dwarf::SymbolFileDWARF::FindGlobalVariables(lldb_private::ConstString, lldb_private::CompilerDeclContext const&, unsigned int, lldb_private::VariableList&) (/home/tcwg-buildbot/worker/lldb-aarch64-ubuntu/build/lib/python3.8/site-packages/lldb/_[lldb.cpython-38-aarch64-linux-gnu.so](http://lldb.cpython-38-aarch64-linux-gnu.so/)+0x579f9d8)
      #5 0x0000ffffb7197508 DWARFASTParserClang::FindConstantOnVariableDefinition(lldb_private::plugin::dwarf::DWARFDIE) (/home/tcwg-buildbot/worker/lldb-aarch64-ubuntu/build/lib/python3.8/site-packages/lldb/_[lldb.cpython-38-aarch64-linux-gnu.so](http://lldb.cpython-38-aarch64-linux-gnu.so/)+0x57bd508)
    ```

--- lldb/test/API/lang/cpp/const_static_integral_member/main.cpp
+++ lldb/test/API/lang/cpp/const_static_integral_member/main.cpp
@@ -89,25 +89,6 @@ struct ClassWithEnumAlias {
       ScopedEnum::scoped_enum_case1;
 };
 
-namespace ns {
-struct Foo {
-  constexpr static int mem = 10;
-
-  void bar() { return; }
-};
-} // namespace ns
-
-struct Foo {
-  constexpr static int mem = -29;
-};
-
-int func() {
-  Foo f1;
-  ns::Foo f2;
-  f2.bar();
-  return ns::Foo::mem + Foo::mem;
-}
-
 int main() {
   A a;
 
@@ -143,7 +124,6 @@ int main() {
 
   auto enum_alias_val = ClassWithEnumAlias::enum_alias;
   auto enum_alias_alias_val = ClassWithEnumAlias::enum_alias_alias;
-  auto ret = func();
 
   return 0; // break here
 }
