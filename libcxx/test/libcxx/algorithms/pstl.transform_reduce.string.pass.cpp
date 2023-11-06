144578eba8b678ff908a88b77f454c45311bbb65
AntonRydahl <rydahl2610@gmail.com>
2023-11-06 12:09:16 -0800

    Update string reduction commit

--- libcxx/test/libcxx/algorithms/pstl.transform_reduce.string.pass.cpp
+++ libcxx/test/libcxx/algorithms/pstl.transform_reduce.string.pass.cpp
@@ -9,30 +9,45 @@
 // This test verifies that you can make string reductions with parallel
 // algorithms
 
+// UNSUPPORTED: c++03, c++11, c++14
+
 #include <algorithm>
 #include <cassert>
 #include <execution>
 #include <vector>
 
+template <class Execution, class Input>
+void test_execution_policy(Execution& execution, Input& input) {
+  std::string result = std::transform_reduce(
+      execution, input.begin(), input.end(), (std::string) "", std::plus{}, [](std::string& str) { return str; });
+  for (auto v : input) {
+    assert(result.find(v) != std::string::npos && "Substring not found in reduuced string.");
+  }
+}
+
 int main(int, char**) {
   int length = 10000;
   std::vector<std::string> vec(length);
+
+  // Generating semi-random strings of length 5
+  unsigned int seed = 17;
   for (int i = 0; i < length; i++) {
     std::string str = "";
     char a;
     for (int j = 0; j < 5; j++) {
-      a = 56 + (((int)i * 5 + 3 * j) % 200);
+      // Generating random unsigned int with a linear congruential generator
+      seed = (214013*seed + 11) % 4294967295;
+      
+      // Generating ASCII character from 33 (!) to 126 (~)
+      a = ((unsigned int) 33 + (seed % 93));
       str += a;
     }
     vec[i] = str;
   }
 
-  std::string result = std::transform_reduce(
-      std::execution::par_unseq, vec.begin(), vec.end(), (std::string) "", std::plus{}, [](std::string& str) {
-        return str;
-      });
-  for (auto v : vec) {
-    assert(result.find(v) != std::string::npos && "Substring not found in deruced string.");
-  }
+  test_execution_policy(std::execution::seq, vec);
+  test_execution_policy(std::execution::unseq, vec);
+  test_execution_policy(std::execution::par, vec);
+  test_execution_policy(std::execution::par_unseq, vec);
   return 0;
 }
