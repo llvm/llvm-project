8f1c9a0999121feb05b0cb55fdb20069eb2c4ba8
Craig Topper <craig.topper@sifive.com>
2023-11-06 21:26:43 -0800

    [RISCV][GISel] Add support for G_FPTOSI/G_FPTOUI with F and D extensions.

--- llvm/lib/Target/RISCV/GISel/RISCVLegalizerInfo.cpp
+++ llvm/lib/Target/RISCV/GISel/RISCVLegalizerInfo.cpp
@@ -217,6 +217,15 @@ RISCVLegalizerInfo::RISCVLegalizerInfo(const RISCVSubtarget &ST) {
                (ST.hasStdExtD() && typeIs(0, s64)(Query));
       });
 
+  getActionDefinitionsBuilder({G_FPTOSI, G_FPTOUI})
+      .legalIf([=, &ST](const LegalityQuery &Query) -> bool {
+        return typeInSet(0, {s32, sXLen})(Query) &&
+               ((ST.hasStdExtF() && typeIs(1, s32)(Query)) ||
+                (ST.hasStdExtD() && typeIs(1, s64)(Query)));
+      })
+      .widenScalarToNextPow2(0)
+      .clampScalar(0, s32, sXLen);
+
   getLegacyLegalizerInfo().computeTables();
 }
 
