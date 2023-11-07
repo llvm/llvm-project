e0281fc280385286c3d5da7de619e793bd3b6bea
Joseph Huber <jhuber6@vols.utk.edu>
2023-11-07 17:12:31 -0600

    [OpenMP] Rework handling of global ctor/dtors in OpenMP
    Summary:
    This patch reworks how we handle global constructors in OpenMP.
    Previously, we emitted individual kernels that were all registered and
    called individually. In order to provide more generic support, this
    patch moves all handling of this to the target backend and the runtime
    plugin. This has the benefit of supporting the GNU extensions for
    constructors an destructors, removing a class of failures related to
    shared library destruction order, and allows targets other than OpenMP
    to use the same support without needing to change the frontend.
    
    This is primarily done by calling kernels that the backend emits to
    iterate a list of ctor / dtor functions. For x64, this is automatic and
    we get it for free with the standard `dlopen` handling. For AMDGPU, we
    emit `amdgcn.device.init` and `amdgcn.device.fini` functions which
    handle everything atuomatically and simply need to be called. For NVPTX,
    a patch https://github.com/llvm/llvm-project/pull/71549 provides the
    kernels to call, but the runtime needs to set up the array manually by
    pulling out all the known constructor / destructor functions.
    
    One concession that this patch requires is the change that for GPU
    targets in OpenMP offloading we will use `llvm.global_dtors` instead of
    using `atexit`. This is because `atexit` is a separate runtime function
    that does not mesh well with the handling we're trying to do here. This
    should be equivalent in all cases except for cases where we would need
    to destruct manually such as:
    
    ```
    struct S { ~S() { foo(); } };
    void foo() {
      static S s;
    }
    ```
    
    However this is broken in many other ways on the GPU, so it is not
    regressing any support, simply increasing the scope of what we can
    handle.
    
    This changes the handling of ctors / dtors. This patch now outputs a
    information message regarding the deprecation if the old format is used.
    This will be completely removed in a later release.
    
    Depends on: https://github.com/llvm/llvm-project/pull/71549
    
    Add LangOption for atexit usage
    
    Summary:
    This method isn't 1-to-1 but it's more functional than not having it.

--- clang/lib/CodeGen/ItaniumCXXABI.cpp
+++ clang/lib/CodeGen/ItaniumCXXABI.cpp
@@ -2794,6 +2794,14 @@ void ItaniumCXXABI::registerGlobalDtor(CodeGenFunction &CGF, const VarDecl &D,
   if (D.isNoDestroy(CGM.getContext()))
     return;
 
+  // OpenMP offloading supports C++ constructors and destructors but we do not
+  // always have 'atexit' available. Instead lower these to use the LLVM global
+  // destructors which we can handle directly in the runtime. Note that this is
+  // not strictly 1-to-1 with using `atexit` because we no longer tear down
+  // globals in reverse order of when they were constructed.
+  if (!CGM.getLangOpts().hasAtExit() && !D.isStaticLocal())
+    return CGF.registerGlobalDtorWithLLVM(D, dtor, addr);
+
   // emitGlobalDtorWithCXAAtExit will emit a call to either __cxa_thread_atexit
   // or __cxa_atexit depending on whether this VarDecl is a thread-local storage
   // or not. CXAAtExit controls only __cxa_atexit, so use it if it is enabled.
