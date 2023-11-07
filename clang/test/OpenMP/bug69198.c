b36dfcff9f08f728df3330edcd00ccb4ecbd2c50
Baodi Shan <lwshanbd@gmail.com>
2023-11-07 13:54:02 -0500

    [Clang][OpenMP]: add tests for PR #71552

--- clang/test/OpenMP/bug69198.c
+++ clang/test/OpenMP/bug69198.c
@@ -1,8 +1,10 @@
-// RUN: %clang_cc1 -verify -fopenmp -x c -triple x86_64-apple-darwin10 %s
-// expected-no-diagnostics
+// RUN: %clang_cc1 -verify -fsyntax-only -fopenmp -x c %s
 
-int c[-1];
+int c[-1]; // expected-error {{'c' declared as an array with a negative size}}
 
 void foo (){
   #pragma omp task depend(inout: c[:][:])
+  {
+    c[0] = 1;
+  }
 }
