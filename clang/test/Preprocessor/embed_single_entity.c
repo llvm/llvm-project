495f1d49d3e88c294be43e752ef699c267f67f8c
Aaron Ballman <aaron@aaronballman.com>
2023-11-07 11:40:52 -0500

    Fix logical think-o with the test

--- clang/test/Preprocessor/embed_single_entity.c
+++ clang/test/Preprocessor/embed_single_entity.c
@@ -1,7 +1,7 @@
-// RUN: %clang_cc1 %s -fsyntax-only -embed-dir=%S/Inputs -verify
+// RUN: %clang_cc1 %s -fsyntax-only -std=c23 -embed-dir=%S/Inputs -verify
 
 const char data =
 #embed "single_byte.txt"
 ;
-_Static_assert('a' == data[0]);
+_Static_assert('b' == data);
 // expected-no-diagnostics
