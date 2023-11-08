fa91086bd834238587f6e193cc21722debe97d7c
Rin Dobrescu <rin.dobrescu@arm.com>
2023-11-08 10:05:08 +0000

    Defer IsLoopInvariantInst check until after the cheaper register checks

--- llvm/lib/CodeGen/MachineLICM.cpp
+++ llvm/lib/CodeGen/MachineLICM.cpp
@@ -1251,9 +1251,10 @@ bool MachineLICMBase::IsProfitableToHoist(MachineInstr &MI,
 
   // If we have a COPY with other uses in the loop, hoist to allow the users to
   // also be hoisted.
-  if (MI.isCopy() && IsLoopInvariantInst(MI, CurLoop) &&
-      MI.getOperand(0).isReg() && MI.getOperand(0).getReg().isVirtual() &&
-      MI.getOperand(1).isReg() && MI.getOperand(1).getReg().isVirtual() &&
+  if (MI.isCopy() && MI.getOperand(0).isReg() &&
+      MI.getOperand(0).getReg().isVirtual() && MI.getOperand(1).isReg() &&
+      MI.getOperand(1).getReg().isVirtual() &&
+      IsLoopInvariantInst(MI, CurLoop) &&
       any_of(MRI->use_nodbg_instructions(MI.getOperand(0).getReg()),
              [&](MachineInstr &UseMI) { return CurLoop->contains(&UseMI); }))
     return true;
