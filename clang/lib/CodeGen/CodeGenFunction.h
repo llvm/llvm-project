5408a927e188ffaf88a14e377466db48fa5d891b
Pravin Jagtap <prjagtap@amd.com>
2023-11-08 15:09:10 +0530

    [AMDGPU] const-fold imm operands of amdgcn_update_dpp intrinsic (#71139)
    Operands of `__builtin_amdgcn_update_dpp` need to evaluate to constant
    to match the intrinsic requirements.
    
    Fixes: SWDEV-426822, SWDEV-431138
    ---------
    
    Authored-by: Pravin Jagtap <Pravin.Jagtap@amd.com>

--- clang/lib/CodeGen/CodeGenFunction.h
+++ clang/lib/CodeGen/CodeGenFunction.h
@@ -4327,6 +4327,8 @@ public:
   llvm::Value *EmitX86BuiltinExpr(unsigned BuiltinID, const CallExpr *E);
   llvm::Value *EmitPPCBuiltinExpr(unsigned BuiltinID, const CallExpr *E);
   llvm::Value *EmitAMDGPUBuiltinExpr(unsigned BuiltinID, const CallExpr *E);
+  llvm::Value *EmitScalarOrConstFoldImmArg(unsigned ICEArguments, unsigned Idx,
+                                           const CallExpr *E);
   llvm::Value *EmitSystemZBuiltinExpr(unsigned BuiltinID, const CallExpr *E);
   llvm::Value *EmitNVPTXBuiltinExpr(unsigned BuiltinID, const CallExpr *E);
   llvm::Value *EmitWebAssemblyBuiltinExpr(unsigned BuiltinID,
