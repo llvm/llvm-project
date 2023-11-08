b8abb10fb997250dfc1398151a70888b80482e9a
Konstantin Varlamov <varconsteq@gmail.com>
2023-11-08 09:10:00 -1000

    [libc++][hardening] Rework macros for enabling the hardening mode. (#70575)
    1. Instead of using individual "boolean" macros, have an "enum" macro
    `_LIBCPP_HARDENING_MODE`. This avoids issues with macros being
    mutually exclusive and makes overriding the hardening mode within a TU
    more straightforward.
    
    2. Rename the safe mode to debug-lite.
    
    This brings the code in line with the RFC:
    https://discourse.llvm.org/t/rfc-hardening-in-libc/73925
    
    Fixes #65101

--- libcxx/test/libcxx/assertions/modes/override_with_extensive_mode.pass.cpp
+++ libcxx/test/libcxx/assertions/modes/override_with_extensive_mode.pass.cpp
@@ -6,24 +6,25 @@
 //
 //===----------------------------------------------------------------------===//
 
-// This test ensures that assertions trigger without the user having to do anything when the hardened mode has been
-// enabled by default.
+// This test ensures that we can override any hardening mode with the extensive hardening mode on a per-TU basis.
 
-// REQUIRES: libcpp-hardening-mode=hardened
 // `check_assertion.h` is only available starting from C++11.
 // UNSUPPORTED: c++03
 // `check_assertion.h` requires Unix headers.
 // REQUIRES: has-unix-headers
+// The ability to set a custom abort message is required to compare the assertion message.
 // XFAIL: availability-verbose_abort-missing
+// ADDITIONAL_COMPILE_FLAGS: -U_LIBCPP_HARDENING_MODE -D_LIBCPP_HARDENING_MODE=_LIBCPP_HARDENING_MODE_EXTENSIVE
 
 #include <cassert>
 #include "check_assertion.h"
 
 int main(int, char**) {
-  _LIBCPP_ASSERT_VALID_ELEMENT_ACCESS(true, "Should not fire");
-  TEST_LIBCPP_ASSERT_FAILURE([] {
-    _LIBCPP_ASSERT_VALID_ELEMENT_ACCESS(false, "Should fire");
-  }(), "Should fire");
+  _LIBCPP_ASSERT_COMPATIBLE_ALLOCATOR(true, "Should not fire");
+  TEST_LIBCPP_ASSERT_FAILURE(
+      [] { _LIBCPP_ASSERT_COMPATIBLE_ALLOCATOR(false, "Extensive-mode assertions should fire"); }(),
+      "Extensive-mode assertions should fire");
+  _LIBCPP_ASSERT_INTERNAL(false, "Debug-mode assertions should not fire");
 
   return 0;
 }
