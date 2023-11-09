c4d380793c74015f2ca2b2d867dac83f728ae505
Mu, Zhailong <zhailong.mu@intel.com>
2023-11-09 10:24:14 +0800

    [mlir][SCFToSPIRV] Implement createConvertSCFToSPIRVPass function

--- mlir/lib/Conversion/SCFToSPIRV/SCFToSPIRVPass.cpp
+++ mlir/lib/Conversion/SCFToSPIRV/SCFToSPIRVPass.cpp
@@ -58,3 +58,7 @@ void SCFToSPIRVPass::runOnOperation() {
   if (failed(applyPartialConversion(op, *target, std::move(patterns))))
     return signalPassFailure();
 }
+
+std::unique_ptr<OperationPass<>> mlir::createConvertSCFToSPIRVPass() {
+  return std::make_unique<SCFToSPIRVPass>();
+}
