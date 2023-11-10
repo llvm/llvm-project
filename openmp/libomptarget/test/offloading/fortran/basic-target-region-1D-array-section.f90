dd7ada84d7c51a2cd5998b0c39c294dd6db6462c
Fabian Mora <fmora.dev@gmail.com>
2023-11-10 21:57:17 +0000

    [flang][NVPTX] Add initial support to the NVPTX target
    This patch adds initial support to the NVPTX target, enabling Flang to produce
    OpenMP offload code for NVPTX targets.

--- openmp/libomptarget/test/offloading/fortran/basic-target-region-1D-array-section.f90
+++ openmp/libomptarget/test/offloading/fortran/basic-target-region-1D-array-section.f90
@@ -1,7 +1,6 @@
 ! Basic offloading test of arrays with provided lower 
 ! and upper bounds as specified by OpenMP's sectioning
-! REQUIRES: flang, amdgcn-amd-amdhsa
-! UNSUPPORTED: nvptx64-nvidia-cuda
+! REQUIRES: flang, amdgcn-amd-amdhsa, nvptx64-nvidia-cuda
 ! UNSUPPORTED: nvptx64-nvidia-cuda-LTO
 ! UNSUPPORTED: aarch64-unknown-linux-gnu
 ! UNSUPPORTED: aarch64-unknown-linux-gnu-LTO
