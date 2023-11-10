efe0c15cf142e2d79085e97da9726e382048e29e
Tom Eccles <tom.eccles@arm.com>
2023-11-10 12:17:52 +0000

    Test using libmvec
    Hopefully this is target independent

--- flang/test/Driver/fveclib-codegen.f90
+++ flang/test/Driver/fveclib-codegen.f90
@@ -1,13 +1,15 @@
 ! test that -fveclib= is passed to the backend
 ! -target aarch64 so that ArmPL is available
-! RUN: %flang -S -target aarch64-unknown-linux-gnu -mcpu=neoverse-v1 -Ofast -fveclib=ArmPL -o - %s | FileCheck %s
+! RUN: %flang -S -Ofast -fveclib=LIBMVEC -o - %s | FileCheck %s
+! RUN: %flang -S -Ofast -fveclib=NoLibrary -o - %s | FileCheck %s --check-prefix=NOLIB
 
 subroutine sb(a, b)
   real :: a(:), b(:)
   integer :: i
   do i=1,100
 ! check that we used a vectorized call to powf()
-! CHECK: armpl_svpow_f32_x
+! CHECK: bl _ZGVbN4vv_powf
+! NOLIB: bl powf
     a(i) = a(i) ** b(i)
   end do
 end subroutine
