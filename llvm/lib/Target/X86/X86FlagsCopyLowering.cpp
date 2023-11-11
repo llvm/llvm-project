413582ba1e4e6b45e34de32ddcd1387607eb2000
Kazu Hirata <kazu@google.com>
2023-11-11 12:32:15 -0800

    [llvm] Stop including llvm/ADT/SmallSet.h (NFC)
    Identified with clangd.

--- llvm/lib/Target/X86/X86FlagsCopyLowering.cpp
+++ llvm/lib/Target/X86/X86FlagsCopyLowering.cpp
@@ -29,7 +29,6 @@
 #include "llvm/ADT/STLExtras.h"
 #include "llvm/ADT/ScopeExit.h"
 #include "llvm/ADT/SmallPtrSet.h"
-#include "llvm/ADT/SmallSet.h"
 #include "llvm/ADT/SmallVector.h"
 #include "llvm/ADT/SparseBitVector.h"
 #include "llvm/ADT/Statistic.h"
