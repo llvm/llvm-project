4bd30c5a3c89d9d74759933554c6a2f9ea3ed9b7
David Truby <david@truby.dev>
2023-11-14 16:17:59 +0000

    Add test for diagnostic on non-Windows

--- flang/test/Driver/dependent-lib.f90
+++ flang/test/Driver/dependent-lib.f90
@@ -1,7 +1,15 @@
-
-! RUN: %flang_fc1 -emit-mlir -triple aarch64-pc-windows-msvc --dependent-lib=libtest %s -o - 2>&1 | FileCheck %s
-! RUN: %flang_fc1 -emit-mlir -triple x86_64-pc-windows-msvc --dependent-lib=libtest %s -o - 2>&1 | FileCheck %s
+! DEFINE: %{triple} =
+! DEFINE: %{run} = %flang_fc1 -emit-mlir -triple %{triple} --dependent-lib=libtest %s -o - 2>&1 | FileCheck %s
+! REDEFINE: %{triple} = aarch64-pc-windows-msvc
+! RUN: %{run}
+! REDEFINE: %{triple} = x86_64-pc-windows-msvc
+! RUN: %{run}
+! REDEFINE: %{triple} = x86_64-linux-unknown-gnu
+! RUN: not %{run} --check-prefixes=CHECK-NOWIN
+! REDEFINE: %{triple} = aarch64-apple-darwin
+! RUN: not %{run} --check-prefixes=CHECK-NOWIN
 
 ! CHECK: llvm.linker_options ["/DEFAULTLIB:", "libtest"]
 program test
 end program test
+! CHECK-NOWIN: --dependent-lib is only supported on Windows
