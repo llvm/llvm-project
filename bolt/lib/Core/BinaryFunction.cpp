2c60473ae4e584f3d7d7d7cfacdb7fb826044f11
Maksim Panchenko <maks@fb.com>
2023-11-13 18:12:39 -0800

    [BOLT] Fix NOP instruction emission on x86 (#72186)
    Use MCAsmBackend::writeNopData() interface to emit NOP instructions on
    x86. There are multiple forms of NOP instruction on x86 with different
    sizes. Currently, LLVM's assembly/disassembly does not support all forms
    correctly which can lead to a breakage of input code semantics, e.g. if
    the program relies on NOP instructions for reserving a patch space.
    
    Add "--keep-nops" option to preserve NOP instructions.

--- bolt/lib/Core/BinaryFunction.cpp
+++ bolt/lib/Core/BinaryFunction.cpp
@@ -58,6 +58,7 @@ extern cl::OptionCategory BoltRelocCategory;
 
 extern cl::opt<bool> EnableBAT;
 extern cl::opt<bool> Instrument;
+extern cl::opt<bool> KeepNops;
 extern cl::opt<bool> StrictMode;
 extern cl::opt<bool> UpdateDebugSections;
 extern cl::opt<unsigned> Verbosity;
@@ -4364,6 +4365,10 @@ MCInst *BinaryFunction::getInstructionAtOffset(uint64_t Offset) {
   } else {
     llvm_unreachable("invalid CFG state to use getInstructionAtOffset()");
   }
+}
+
+bool BinaryFunction::shouldPreserveNops() const {
+  return PreserveNops || opts::KeepNops;
 }
 
 void BinaryFunction::printLoopInfo(raw_ostream &OS) const {
