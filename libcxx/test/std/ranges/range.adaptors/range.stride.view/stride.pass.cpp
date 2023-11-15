62da789ce86920ad10cc268785b3176c2ff6f48f
Will Hawkins <hawkinsw@obs.cr>
2023-11-15 10:42:54 -0500

    fixup! WIP: [libc++][ranges] Implement `ranges::stride_view`.
    Update stride() tests per @var-const feedback.

--- libcxx/test/std/ranges/range.adaptors/range.stride.view/stride.pass.cpp
+++ libcxx/test/std/ranges/range.adaptors/range.stride.view/stride.pass.cpp
@@ -8,27 +8,33 @@
 
 // UNSUPPORTED: c++03, c++11, c++14, c++17, c++20
 
-// ranges
+// constexpr range_difference_t<_View> stride() const noexcept;
 
-// std::views::stride_view
-
-#include "test.h"
 #include "test_iterators.h"
+#include "test.h"
 #include <ranges>
 #include <type_traits>
 #include <utility>
 
 constexpr bool test() {
-  using View = InputView<bidirectional_iterator<int*>>;
-  static_assert(noexcept(std::declval<std::ranges::stride_view<View>>().stride()));
-  static_assert(std::is_same_v<std::ranges::range_difference_t<View>,
-                               decltype(std::declval<std::ranges::stride_view<View>>().stride())>);
+  using View = InputView<cpp17_input_iterator<int*>>;
+  int arr[]{1, 2, 3};
+  auto arrv(View(cpp17_input_iterator(arr), cpp17_input_iterator(arr + 3)));
+  // Mark str const so that we confirm that stride is a const member function.
+  const std::ranges::stride_view<View> str(arrv, 1);
+
+  // Make sure that stride member function is noexcept.
+  static_assert(noexcept(str.stride()));
+  // Make sure that the type returned by stride matches what is expected.
+  static_assert(std::is_same_v<std::ranges::range_difference_t<View>, decltype(str.stride())>);
+  // Make sure that we get back a stride equal to the one that we gave in the ctor.
+  assert(str.stride() == 1);
+
   return true;
 }
 
 int main(int, char**) {
   test();
   static_assert(test());
-
   return 0;
 }
