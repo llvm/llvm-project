99d72368147d7c1749f58ecb9eafce6c7b3a2de9
Yingwei Zheng <dtcxzyw2333@gmail.com>
2023-11-16 02:35:47 +0800

    [InstCombine] Infer nuw flags for `C-(X+C2)` -> `(C-C2)-X` (#72373)
    This patch improves https://reviews.llvm.org/D152068 by inferring NUW
    flags for sub insts.
    It is worth noting that we don't need to check overflow for `C-C2`.
    Alive2: https://alive2.llvm.org/ce/z/uutGpS
    
    This missed optimization is discovered with the help of
    https://github.com/AliveToolkit/alive2/pull/962.

--- llvm/lib/Transforms/InstCombine/InstCombineAddSub.cpp
+++ llvm/lib/Transforms/InstCombine/InstCombineAddSub.cpp
@@ -2111,14 +2111,16 @@ Instruction *InstCombinerImpl::visitSub(BinaryOperator &I) {
 
     // C-(X+C2) --> (C-C2)-X
     if (match(Op1, m_Add(m_Value(X), m_ImmConstant(C2)))) {
-      // C-C2 never overflow, and C-(X+C2), (X+C2) has NSW
-      // => (C-C2)-X can have NSW
+      // C-C2 never overflow, and C-(X+C2), (X+C2) has NSW/NUW
+      // => (C-C2)-X can have NSW/NUW
       bool WillNotSOV = willNotOverflowSignedSub(C, C2, I);
       BinaryOperator *Res =
           BinaryOperator::CreateSub(ConstantExpr::getSub(C, C2), X);
       auto *OBO1 = cast<OverflowingBinaryOperator>(Op1);
       Res->setHasNoSignedWrap(I.hasNoSignedWrap() && OBO1->hasNoSignedWrap() &&
                               WillNotSOV);
+      Res->setHasNoUnsignedWrap(I.hasNoUnsignedWrap() &&
+                                OBO1->hasNoUnsignedWrap());
       return Res;
     }
   }
