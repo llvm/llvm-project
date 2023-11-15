0767b57320aa3433a65761132d57414fe6ffd1ea
PragmaTwice <twice@apache.org>
2023-11-15 23:49:52 +0900

    use .verify.cpp instead of .compile.fail.cpp

--- libcxx/test/std/utilities/expected/expected.void/observers/value.lwg3940.verify.cpp
+++ libcxx/test/std/utilities/expected/expected.void/observers/value.lwg3940.verify.cpp
@@ -6,6 +6,7 @@
 //===----------------------------------------------------------------------===//
 
 // UNSUPPORTED: c++03, c++11, c++14, c++17, c++20
+// ADDITIONAL_COMPILE_FLAGS: -Xclang -verify-ignore-unexpected=error
 
 // constexpr void value() &&;
 // Mandates: is_copy_constructible_v<E> is true and is_move_constructible_v<E> is true.
@@ -15,11 +16,11 @@
 #include "MoveOnly.h"
 
 void test() {
-  // MoveOnly
+  // MoveOnly type as error_type
   std::expected<void, MoveOnly> e(std::unexpect, 5);
 
-  // COMPILE FAIL: MoveOnly is not copy constructible
-  std::move(e).value();
+  std::move(e).value(); // expected-note{{in instantiation of member function 'std::expected<void, MoveOnly>::value' requested here}}
+  // expected-error-re@*:* {{static assertion failed due to requirement 'is_copy_constructible_v<MoveOnly>': error_type has to be both copy constructible and move constructible}}
 }
 
 int main(int, char**) { test(); }
