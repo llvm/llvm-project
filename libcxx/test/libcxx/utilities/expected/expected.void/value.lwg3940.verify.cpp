cc857072825b73c34f7cc7c01ff860a7b1dd7cd0
PragmaTwice <twice@apache.org>
2023-11-16 21:52:55 +0900

    rename & clean up

--- libcxx/test/libcxx/utilities/expected/expected.void/value.lwg3940.verify.cpp
+++ libcxx/test/libcxx/utilities/expected/expected.void/value.lwg3940.verify.cpp
@@ -6,7 +6,6 @@
 //===----------------------------------------------------------------------===//
 
 // UNSUPPORTED: c++03, c++11, c++14, c++17, c++20
-// ADDITIONAL_COMPILE_FLAGS: -Xclang -verify-ignore-unexpected=error
 
 // constexpr void value() &&;
 // Mandates: is_copy_constructible_v<E> is true and is_move_constructible_v<E> is true.
@@ -20,8 +19,6 @@ void test() {
   std::expected<void, MoveOnly> e(std::unexpect, 5);
 
   std::move(e)
-      .value(); // expected-note{{in instantiation of member function 'std::expected<void, MoveOnly>::value' requested here}}
-  // expected-error-re@*:* {{static assertion failed due to requirement 'is_copy_constructible_v<MoveOnly>': error_type has to be both copy constructible and move constructible}}
+      .value(); // expected-note {{in instantiation of member function 'std::expected<void, MoveOnly>::value' requested here}}
+  // expected-error@*:* {{static assertion failed due to requirement 'is_copy_constructible_v<MoveOnly>': error_type has to be both copy constructible and move constructible}}
 }
-
-int main(int, char**) { test(); }
