07730f63b9bf72dc3f62dd7cccc98f0ca627d2b8
David Li <57157229+david-xl@users.noreply.github.com>
2023-11-16 13:47:31 -0800

    Enable Custom Lowering for fabs.v8f16 on AVX (#71730)
    [X86]: Enable custom lowering for fabs.v8f16 on AVX
    
    Currently, custom lowering of fabs.v8f16 requires AVX512FP16, which is
    too restrictive. For v8f16 fabs lowering, no instructions in AVX512FP16
    are needed. Without the fix, horribly inefficient code is generated
    without AVX512FP16. Note instcombiner generates calls to intrinsics
    @llvm.fabs.v8f16 when simplifyping AND <8 x half> operations.

--- llvm/lib/Target/X86/X86ISelLowering.cpp
+++ llvm/lib/Target/X86/X86ISelLowering.cpp
@@ -1396,6 +1396,8 @@ X86TargetLowering::X86TargetLowering(const X86TargetMachine &TM,
       setOperationAction(ISD::FMINIMUM,          VT, Custom);
     }
 
+    setOperationAction(ISD::FABS, MVT::v8f16, Custom);
+
     // (fp_to_int:v8i16 (v8f32 ..)) requires the result type to be promoted
     // even though v8i16 is a legal type.
     setOperationPromotedToType(ISD::FP_TO_SINT,        MVT::v8i16, MVT::v8i32);
