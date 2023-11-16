5bfc0608073cd699e42a23b07d68b4572a14fcbd
Jacob Lambert <jacob.lambert@amd.com>
2023-11-15 22:06:46 -0800

    [CodeGen] Add conditional to module cloning in bitcode linking
    Now that we have a commandline option dictating a second link step,
    (-relink-builtin-bitcode-postop), we can condition the module
    creation when linking in bitcode modules. This aims to improve
    performance by avoiding unnecessary linking

--- clang/lib/CodeGen/CodeGenAction.cpp
+++ clang/lib/CodeGen/CodeGenAction.cpp
@@ -57,6 +57,10 @@ using namespace llvm;
 
 #define DEBUG_TYPE "codegenaction"
 
+namespace llvm {
+extern cl::opt<bool> ClRelinkBuiltinBitcodePostop;
+}
+
 namespace clang {
 class BackendConsumer;
 class ClangDiagnosticHandler final : public DiagnosticHandler {
@@ -251,32 +255,50 @@ bool BackendConsumer::LinkInModules(llvm::Module *M, bool ShouldLinkFiles) {
       }
 
     CurLinkModule = LM.Module.get();
-
-    // TODO: If CloneModule() is updated to support cloning of unmaterialized
-    // modules, we can remove this
     bool Err;
-    if (Error E = CurLinkModule->materializeAll())
-      return false;
 
     // Create a Clone to move to the linker, which preserves the original
     // linking modules, allowing them to be linked again in the future
-    // TODO: Add a ShouldCleanup option to make Cloning optional. When
-    // set, we can pass the original modules to the linker for cleanup
-    std::unique_ptr<llvm::Module> Clone = llvm::CloneModule(*LM.Module);
+    if (ClRelinkBuiltinBitcodePostop) {
+      // TODO: If CloneModule() is updated to support cloning of unmaterialized
+      // modules, we can remove this
+      if (Error E = CurLinkModule->materializeAll())
+        return false;
 
-    if (LM.Internalize) {
-      Err = Linker::linkModules(
+      std::unique_ptr<llvm::Module> Clone = llvm::CloneModule(*LM.Module);
+
+      if (LM.Internalize) {
+        Err = Linker::linkModules(
           *M, std::move(Clone), LM.LinkFlags,
           [](llvm::Module &M, const llvm::StringSet<> &GVS) {
-            internalizeModule(M, [&GVS](const llvm::GlobalValue &GV) {
-              return !GV.hasName() || (GVS.count(GV.getName()) == 0);
-            });
+          internalizeModule(M, [&GVS](const llvm::GlobalValue &GV) {
+                            return !GV.hasName() ||
+                            (GVS.count(GV.getName()) == 0);
+                            });
           });
-    } else
-      Err = Linker::linkModules(*M, std::move(Clone), LM.LinkFlags);
+      } else
+        Err = Linker::linkModules(*M, std::move(Clone), LM.LinkFlags);
 
-    if (Err)
-      return true;
+      if (Err)
+        return true;
+    }
+    // Otherwise we can link (and clean up) the original modules
+    else {
+      if (LM.Internalize) {
+        Err = Linker::linkModules(
+          *M, std::move(LM.Module), LM.LinkFlags,
+          [](llvm::Module &M, const llvm::StringSet<> &GVS) {
+          internalizeModule(M, [&GVS](const llvm::GlobalValue &GV) {
+                            return !GV.hasName() ||
+                            (GVS.count(GV.getName()) == 0);
+                            });
+          });
+      } else
+        Err = Linker::linkModules(*M, std::move(LM.Module), LM.LinkFlags);
+
+      if (Err)
+        return true;
+    }
   }
 
   return false; // success
