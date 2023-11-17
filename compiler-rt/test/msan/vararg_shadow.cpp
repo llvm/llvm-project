a05e736d288a7f2009ee9d057e78713d9adeeb5f
Vitaly Buka <vitalybuka@google.com>
2023-11-17 15:13:11 -0800

    [msan][x86] Fix shadow if vararg overflow beyond kParamTLSSize
    Caller puts argument shadow one by one into __msan_va_arg_tls, until it
    reaches kParamTLSSize. After that it still increment OverflowOffset but
    does not store the shadow.
    
    Callee needs OverflowOffset to prepare a shadow for the entire overflow
    area. It's done by creating "varargs shadow copy" for complete list of
    args, copying available shadow from __msan_va_arg_tls, and clearing the
    rest.
    
    However callee does not know if the tail of __msan_va_arg_tls was not
    able to fit an argument, and callee will copy tail shadow into "varargs
    shadow copy", and later used as a shadow for an omitted argument.
    
    So that unused tail of the __msan_va_arg_tls must be cleared if left
    unused.
    
    This allows us to enable compiler-rt/test/msan/vararg_shadow.cpp for
    x86.
    
    Reviewers: kstoimenov, thurstond
    
    Reviewed By: thurstond
    
    Pull Request: https://github.com/llvm/llvm-project/pull/72707

--- compiler-rt/test/msan/vararg_shadow.cpp
+++ compiler-rt/test/msan/vararg_shadow.cpp
@@ -3,8 +3,8 @@
 // Without -fno-sanitize-memory-param-retval we can't even pass poisoned values.
 // RUN: %clangxx_msan -fno-sanitize-memory-param-retval -fsanitize-memory-track-origins=0 -O3 %s -o %t
 
-// The most of targets fail the test.
-// XFAIL: target={{(x86|aarch64|loongarch64|mips|powerpc64).*}}
+// Nothing works yet.
+// XFAIL: target={{(aarch64|loongarch64|mips|powerpc64).*}}
 
 #include <sanitizer/msan_interface.h>
 #include <stdarg.h>
