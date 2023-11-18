b34f3c376360c07c03326acbc444f57a734f8cb4
George Koehler <kernigh@gmail.com>
2023-11-18 14:39:36 -0500

    [PowerPC] Handle CALL_RM like CALL for 32-bit ELF
    If a function call has the strictfp attribute, its opcode changes from
    CALL to CALL_RM.  If the call uses the secure PLT for 32-bit ELF, then
    it must getGlobalBaseReg() to set r30.

--- llvm/lib/Target/PowerPC/PPCISelDAGToDAG.cpp
+++ llvm/lib/Target/PowerPC/PPCISelDAGToDAG.cpp
@@ -5475,7 +5475,8 @@ void PPCDAGToDAGISel::Select(SDNode *N) {
     // generate secure plt code for TLS symbols.
     getGlobalBaseReg();
   } break;
-  case PPCISD::CALL: {
+  case PPCISD::CALL:
+  case PPCISD::CALL_RM: {
     if (PPCLowering->getPointerTy(CurDAG->getDataLayout()) != MVT::i32 ||
         !TM.isPositionIndependent() || !Subtarget->isSecurePlt() ||
         !Subtarget->isTargetELF())
@@ -5491,8 +5492,7 @@ void PPCDAGToDAGISel::Select(SDNode *N) {
       if (ES->getTargetFlags() == PPCII::MO_PLT)
         getGlobalBaseReg();
     }
-  }
-    break;
+  } break;
 
   case PPCISD::GlobalBaseReg:
     ReplaceNode(N, getGlobalBaseReg());
