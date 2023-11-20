1f48a0eb04df6ccc2d8117ebd6b68cc45484a0f3
Yaxun (Sam) Liu <yaxun.liu@amd.com>
2023-11-19 19:34:35 -0500

    [CUDA][HIP] ignore implicit host/device attr for override
    When deciding whether a previous function declaration is an
    overload or override, implicit host/device attrs should
    not be considered.
    
    This fixes the failure for the following code:
    
    `template <typename T>
    class C {
        explicit C() {};
    };
    
    template <> C<int>::C() {};
    `
    
    The issue was introduced by https://github.com/llvm/llvm-project/pull/72394
    
    sine the template specialization is treated as overload
    due to implicit host/device attrs are considered for
    overload/override differentiation.

--- clang/test/SemaCUDA/trivial-ctor-dtor.cu
+++ clang/test/SemaCUDA/trivial-ctor-dtor.cu
@@ -38,3 +38,19 @@ struct TC : TB<T> {
 };
 
 __device__ TC<int> tc; //expected-error {{dynamic initialization is not supported for __device__, __constant__, __shared__, and __managed__ variables}}
+
+// Check trivial ctor specialization
+template <typename T>
+struct C { //expected-note {{candidate constructor (the implicit copy constructor) not viable}}
+           //expected-note@-1 {{candidate constructor (the implicit move constructor) not viable}}
+    explicit C() {};
+};
+
+template <> C<int>::C() {};
+__device__ C<int> ci_d;
+C<int> ci_h;
+
+// Check non-trivial ctor specialization
+template <> C<float>::C() { static int nontrivial_ctor = 1; } //expected-note {{candidate constructor not viable: call to __host__ function from __device__ function}}
+__device__ C<float> cf_d; //expected-error {{no matching constructor for initialization of 'C<float>'}}
+C<float> cf_h;
