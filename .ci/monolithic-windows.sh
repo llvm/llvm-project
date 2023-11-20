07a4c25b2f7617f55871fd8ba6a45d5578768db4
Benjamin Maxwell <benjamin.maxwell@arm.com>
2023-11-20 10:25:25 +0000

    [CI] Add check-mlir-python to MLIR pre-merge checks

--- .ci/monolithic-windows.sh
+++ .ci/monolithic-windows.sh
@@ -48,7 +48,9 @@ cmake -S ${MONOREPO_ROOT}/llvm -B ${BUILD_DIR} \
       -D LLVM_LIT_ARGS="-v --xunit-xml-output ${BUILD_DIR}/test-results.xml" \
       -D COMPILER_RT_BUILD_ORC=OFF \
       -D CMAKE_C_COMPILER_LAUNCHER=sccache \
-      -D CMAKE_CXX_COMPILER_LAUNCHER=sccache
+      -D CMAKE_CXX_COMPILER_LAUNCHER=sccache \
+      -D MLIR_ENABLE_BINDINGS_PYTHON=ON \
+      -D Python3_EXECUTABLE="$(which python)"
 
 echo "--- ninja"
 # Targets are not escaped as they are passed as separate arguments.
