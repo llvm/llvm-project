fd455bf3bb32191e728f1961917d7eda5cdc4227
Utkarsh Saxena <usx@google.com>
2023-11-21 13:13:31 +0100

    typo fix

--- clang-tools-extra/clang-tidy/misc/CoroutineHostileRAIICheck.h
+++ clang-tools-extra/clang-tidy/misc/CoroutineHostileRAIICheck.h
@@ -43,8 +43,7 @@ private:
   // List of fully qualified types which should not persist across a suspension
   // point in a coroutine.
   std::vector<StringRef> RAIITypesList;
-  // List of fully qualified awaitable types for which are considered safe to
-  // co_await.
+  // List of fully qualified awaitable types which are considered safe to co_await.
   std::vector<StringRef> SafeAwaitablesList;
 };
 
