f8adcd49a4627926ac8009c595c6b0103589e600
Dong JianQiang <dongjianqiang2@huawei.com>
2023-11-21 16:15:52 +0800

    [Driver] Default Generic_GCC aarch64_be to -fasynchronous-unwind-tables
    This patch defaults Generic_GCC aarch64_be to use -fasynchronous-unwind-tables.

--- clang/lib/Driver/ToolChains/Gnu.cpp
+++ clang/lib/Driver/ToolChains/Gnu.cpp
@@ -2937,6 +2937,7 @@ ToolChain::UnwindTableLevel
 Generic_GCC::getDefaultUnwindTableLevel(const ArgList &Args) const {
   switch (getArch()) {
   case llvm::Triple::aarch64:
+  case llvm::Triple::aarch64_be:
   case llvm::Triple::ppc:
   case llvm::Triple::ppcle:
   case llvm::Triple::ppc64:
