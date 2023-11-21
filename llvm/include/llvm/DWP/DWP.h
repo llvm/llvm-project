b70253d22daadc31a9dbcb05e0bd6f4194f8ed27
huangjinjie <huangjinjie@bytedance.com>
2023-11-21 13:35:54 +0800

    fix clang-format

--- llvm/include/llvm/DWP/DWP.h
+++ llvm/include/llvm/DWP/DWP.h
@@ -67,7 +67,7 @@ struct CompileUnitIdentifiers {
 };
 
 Error write(MCStreamer &Out, ArrayRef<std::string> Inputs,
-            bool ContinueOnCuIndexOverflow, bool SoftStopOnCuIndexOverflow);
+            const OnCuIndexOverflow &OverflowOptValue);
 
 unsigned getContributionIndex(DWARFSectionKind Kind, uint32_t IndexVersion);
 
