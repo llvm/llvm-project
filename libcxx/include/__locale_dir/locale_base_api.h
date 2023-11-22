84874912d30d3835f9e88f7c700039546303c4e0
Pavel Kosov <kpdev42@gmail.com>
2023-11-22 16:02:59 +0300

    [libcxx] Add OHOS support
    Add support for OpenHarmony OS (OHOS)
    
    OpenHarmony is an open-source project incubated and operated by the OpenAtom Foundation.
    It targets IoT, and already used on production devices.
    
    General OpenHarmony OS discussion on discourse thread:
      "Add support for OpenHarmony OS"
      https://discourse.llvm.org/t/rfc-add-support-for-openharmony-os/66656
    
    LLVM and clang parts of OHOS support: D138202, D145227
    
    Phabricator review: D146804
    
    ~~
    
    Huawei RRI, OS Lab

--- libcxx/include/__locale_dir/locale_base_api.h
+++ libcxx/include/__locale_dir/locale_base_api.h
@@ -23,7 +23,9 @@
 #  include <__locale_dir/locale_base_api/openbsd.h>
 #elif defined(__Fuchsia__)
 #  include <__locale_dir/locale_base_api/fuchsia.h>
-#elif defined(__wasi__) || defined(_LIBCPP_HAS_MUSL_LIBC)
+#elif defined(__wasi__) ||                  \
+      defined(_LIBCPP_HAS_MUSL_LIBC) ||     \
+      defined(__OHOS_FAMILY__)
 #  include <__locale_dir/locale_base_api/musl.h>
 #elif defined(__APPLE__) || defined(__FreeBSD__)
 #  include <xlocale.h>
