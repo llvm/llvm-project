bf20ddba56da213510f101e4db0ef5b6c17b1313
Daniel Chen <cdchen@ca.ibm.com>
2023-11-22 11:51:12 -0500

    [Flang] Add partial support for lowering procedure pointer assignment. (#70461)
    **Scope of the PR:**
    1. Lowering global and local procedure pointer declaration statement
    with explicit or implicit interface. The explicit interface can be from
    an interface block, a module procedure or an internal procedure.
    2. Lowering procedure pointer assignment, where the target procedure
    could be external, module or internal procedures.
    3. Lowering reference to procedure pointers so that it works end to end.
    
    **PR notes:**
    1. The first commit of the PR does not include testing. I would like to
    collect some comments first, which may alter the output. Once I confirm
    the implementation, I will add some testing as a follow up commit to
    this PR.
    2. No special handling of the host-associated entities when an internal
    procedure is the target of a procedure pointer assignment in this PR.
    
    **Implementation notes:**
    1. The implementation is using the HLFIR path.
    2. Flang currently uses `getUntypedBoxProcType` to get the
    `fir::BoxProcType` for `ProcedureDesignator` when getting the address of
    a procedure in order to pass it as an actual argument. This PR inherits
    the same design decision for procedure pointer as the `fir::StoreOp`
    requires the same memory type.
    
    Note: this commit is actually resubmitting the original commit from
    PR #70461 that was reverted. See PR #73221.

--- flang/lib/Lower/ConvertType.cpp
+++ flang/lib/Lower/ConvertType.cpp
@@ -248,8 +248,13 @@ struct TypeBuilderImpl {
     // links, the fir type is built based on the ultimate symbol. This relies
     // on the fact volatile and asynchronous are not reflected in fir types.
     const Fortran::semantics::Symbol &ultimate = symbol.GetUltimate();
-    if (Fortran::semantics::IsProcedurePointer(ultimate))
-      TODO(loc, "procedure pointers");
+
+    if (Fortran::semantics::IsProcedurePointer(ultimate)) {
+      Fortran::evaluate::ProcedureDesignator proc(ultimate);
+      auto procTy{Fortran::lower::translateSignature(proc, converter)};
+      return fir::BoxProcType::get(context, procTy);
+    }
+
     if (const Fortran::semantics::DeclTypeSpec *type = ultimate.GetType()) {
       if (const Fortran::semantics::IntrinsicTypeSpec *tySpec =
               type->AsIntrinsic()) {
