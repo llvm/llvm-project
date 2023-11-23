c2b7674546588d9e5227f1d71cada670bf61166b
Tom Eccles <tom.eccles@arm.com>
2023-11-23 11:44:24 +0000

    Refactor argument handling

--- flang/lib/Frontend/CompilerInvocation.cpp
+++ flang/lib/Frontend/CompilerInvocation.cpp
@@ -242,24 +242,12 @@ static void parseCodeGenArgs(Fortran::frontend::CodeGenOptions &opts,
                    clang::driver::options::OPT_fno_loop_versioning, false))
     opts.LoopVersioning = 1;
 
-  bool aliasAnalysis = false;
-  bool noAliasAnalysis = false;
+  opts.AliasAnalysis = opts.OptimizationLevel > 0;
   if (auto *arg =
           args.getLastArg(clang::driver::options::OPT_falias_analysis,
-                          clang::driver::options::OPT_fno_alias_analysis)) {
-    if (arg->getOption().matches(clang::driver::options::OPT_falias_analysis))
-      aliasAnalysis = true;
-    else
-      noAliasAnalysis = true;
-  }
-  opts.AliasAnalysis = 0;
-  if (opts.OptimizationLevel > 0) {
-    if (!noAliasAnalysis)
-      opts.AliasAnalysis = 1;
-  } else {
-    if (aliasAnalysis)
-      opts.AliasAnalysis = 1;
-  }
+                          clang::driver::options::OPT_fno_alias_analysis))
+    opts.AliasAnalysis =
+        arg->getOption().matches(clang::driver::options::OPT_falias_analysis);
 
   for (auto *a : args.filtered(clang::driver::options::OPT_fpass_plugin_EQ))
     opts.LLVMPassPlugins.push_back(a->getValue());
