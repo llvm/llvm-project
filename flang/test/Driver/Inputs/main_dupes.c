1d78798a0d363b67c3cb17d0b7dfd21ece640248
Michael Klemm <michael.klemm@amd.com>
2023-11-23 18:05:10 +0100

    Make clang-format happy

--- flang/test/Driver/Inputs/main_dupes.c
+++ flang/test/Driver/Inputs/main_dupes.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-int main(int argc, char * argv[]) {
-    // Irrelevant what to do in here.
-    // Test is supposed to fail at link time.
-    printf("Hello from C [%s]\n", __FUNCTION__);
-    return 0;
+int main(int argc, char *argv[]) {
+  // Irrelevant what to do in here.
+  // Test is supposed to fail at link time.
+  printf("Hello from C [%s]\n", __FUNCTION__);
+  return 0;
 }
