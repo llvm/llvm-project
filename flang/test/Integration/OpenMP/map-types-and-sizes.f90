5fc02436f348f8616a3e4e568f415bef48d4ccb9
Akash Banerjee <akash.banerjee@amd.com>
2023-11-24 16:02:39 +0000

    [Flang] Add new Integration tests directory to Flang (#73141)
    As per the RFC:
    [https://discourse.llvm.org/t/rfc-flang-new-directory-for-adding-end-to-end-tests-for-lowering-to-llvm-ir-in-flang/74872/11](https://discourse.llvm.org/t/rfc-flang-new-directory-for-adding-end-to-end-tests-for-lowering-to-llvm-ir-in-flang/74872/11),
    this patch adds a new Integration test directory for OpenMP-
    `flang/test/Integration/OpenMP` and moves the existing OpenMP
    integration tests from `flang/test/Driver/OpenMP` to this directory.
    
    **This directory can be used to add Integration tests involving multiple
    stages of the compiler (for eg. from Fortran to LLVM IR). It should not
    contain executable tests. We should only add tests here sparingly and
    only if there is no other way to test. Repeat this message in each test
    that is added to this directory and sub-directories.**

--- flang/test/Integration/OpenMP/map-types-and-sizes.f90
+++ flang/test/Integration/OpenMP/map-types-and-sizes.f90
@@ -1,3 +1,11 @@
+!===----------------------------------------------------------------------===!
+! This directory can be used to add Integration tests involving multiple
+! stages of the compiler (for eg. from Fortran to LLVM IR). It should not
+! contain executable tests. We should only add tests here sparingly and only
+! if there is no other way to test. Repeat this message in each test that is
+! added to this directory and sub-directories.
+!===----------------------------------------------------------------------===!
+
 !RUN: %flang_fc1 -emit-llvm -fopenmp -flang-deprecated-no-hlfir %s -o - | FileCheck %s
 
 !===============================================================================
