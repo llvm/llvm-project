12d890eef5401d0611f0b07cfff616933deb9566
Guillaume Chatelet <gchatelet@google.com>
2023-11-24 10:24:03 +0000

    [libc][NFC] Move float macro into its own header / add target os detection

--- libc/src/__support/macros/properties/compiler.h
+++ libc/src/__support/macros/properties/compiler.h
@@ -21,25 +21,4 @@
 #define LIBC_COMPILER_IS_MSC
 #endif
 
-// Check compiler features
-#if defined(FLT128_MANT_DIG)
-// C23 _Float128 type is available.
-#define LIBC_COMPILER_HAS_FLOAT128
-#define LIBC_FLOAT128_IS_C23
-using float128 = _Float128;
-
-#elif defined(__SIZEOF_FLOAT128__)
-// Builtin __float128 is available.
-#define LIBC_COMPILER_HAS_FLOAT128
-#define LIBC_FLOAT128_IS_BUILTIN
-using float128 = __float128;
-
-#elif (defined(__linux__) && defined(__aarch64__))
-// long double on Linux aarch64 is 128-bit floating point.
-#define LIBC_COMPILER_HAS_FLOAT128
-#define LIBC_FLOAT128_IS_LONG_DOUBLE
-using float128 = long double;
-
-#endif
-
 #endif // LLVM_LIBC_SRC___SUPPORT_MACROS_PROPERTIES_COMPILER_H
