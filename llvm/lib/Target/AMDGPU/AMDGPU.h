4636e328dad9767d0338101a3c673850421840e7
Jay Foad <jay.foad@amd.com>
2023-11-24 10:23:06 +0000

    [AMDGPU] New AMDGPUInsertSingleUseVDST pass (#72388)
    Add support for emitting GFX11.5 s_singleuse_vdst instructions. This is
    a power saving feature whereby the compiler can annotate VALU
    instructions whose results are known to have only a single use, so the
    hardware can in some cases avoid writing the result back to VGPR RAM.
    
    To begin with the pass is disabled by default because of one missing
    feature: we need an exclusion list of opcodes that never qualify as
    single-use producers and/or consumers. A future patch will implement
    this and enable the pass by default.
    
    ---------
    
    Co-authored-by: Scott Egerton <scott.egerton@amd.com>

--- llvm/lib/Target/AMDGPU/AMDGPU.h
+++ llvm/lib/Target/AMDGPU/AMDGPU.h
@@ -335,6 +335,9 @@ extern char &SIModeRegisterID;
 void initializeAMDGPUInsertDelayAluPass(PassRegistry &);
 extern char &AMDGPUInsertDelayAluID;
 
+void initializeAMDGPUInsertSingleUseVDSTPass(PassRegistry &);
+extern char &AMDGPUInsertSingleUseVDSTID;
+
 void initializeSIInsertHardClausesPass(PassRegistry &);
 extern char &SIInsertHardClausesID;
 
