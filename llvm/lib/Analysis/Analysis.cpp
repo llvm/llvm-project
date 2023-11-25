977cf3bbc6f9067c9bf094114df819e0c25f1654
Aiden Grossman <agrossman154@yahoo.com>
2023-11-25 14:30:35 -0800

    [NewPM] Remove CFGViewerLegacyPass
    This pass isn't used anywhere upstream, has a NewPM equivalent, and has
    no test coverage. For these reasons, remove it.

--- llvm/lib/Analysis/Analysis.cpp
+++ llvm/lib/Analysis/Analysis.cpp
@@ -25,7 +25,6 @@ void llvm::initializeAnalysis(PassRegistry &Registry) {
   initializeCallGraphDOTPrinterPass(Registry);
   initializeCallGraphPrinterLegacyPassPass(Registry);
   initializeCallGraphViewerPass(Registry);
-  initializeCFGViewerLegacyPassPass(Registry);
   initializeCFGPrinterLegacyPassPass(Registry);
   initializeCFGOnlyViewerLegacyPassPass(Registry);
   initializeCFGOnlyPrinterLegacyPassPass(Registry);
