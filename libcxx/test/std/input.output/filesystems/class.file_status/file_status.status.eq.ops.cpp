9fbc1fffb9158c0b3127738fcf894cdada2780cb
Stephan T. Lavavej <stl@nuwen.net>
2023-11-26 05:59:43 -0800

    Revert "Fix massive test code typo - how was this compiling?!"
    This reverts commit a8632f986f059f4dca2a69ca7caa09d283f451b3.
    
    @philnik777 is fixing this properly with #73444.

--- libcxx/test/std/input.output/filesystems/class.file_status/file_status.status.eq.ops.cpp
+++ libcxx/test/std/input.output/filesystems/class.file_status/file_status.status.eq.ops.cpp
@@ -22,26 +22,26 @@
 
 void test() {
   {
-    std::filesystem::file_status f1;
-    std::filesystem::file_status f2;
+    std::fileystem::file_status f1;
+    std::fileystem::file_status f2;
 
     assert(testEquality(f1, f2, true));
   }
   {
-    std::filesystem::file_status f1{std::filesystem::file_type::regular, std::filesystem::perms::owner_read};
-    std::filesystem::file_status f2{std::filesystem::file_type::regular, std::filesystem::perms::owner_read};
+    std::fileystem::file_status f1{std::filesystem::file_type::regular, std::filesystem::perms::owner_read};
+    std::fileystem::file_status f2{std::filesystem::file_type::regular, std::filesystem::perms::owner_read};
 
     assert(testEquality(f1, f2, true));
   }
   {
-    std::filesystem::file_status f1{std::filesystem::file_type::regular, std::filesystem::perms::owner_read};
-    std::filesystem::file_status f2{std::filesystem::file_type::none, std::filesystem::perms::owner_read};
+    std::fileystem::file_status f1{std::filesystem::file_type::regular, std::filesystem::perms::owner_read};
+    std::fileystem::file_status f2{std::filesystem::file_type::none, std::filesystem::perms::owner_read};
 
     assert(testEquality(f1, f2, false));
   }
   {
-    std::filesystem::file_status f1{std::filesystem::file_type::regular, std::filesystem::perms::owner_read};
-    std::filesystem::file_status f2{std::filesystem::file_type::regular, std::filesystem::perms::owner_write};
+    std::fileystem::file_status f1{std::filesystem::file_type::regular, std::filesystem::perms::owner_read};
+    std::fileystem::file_status f2{std::filesystem::file_type::regular, std::filesystem::perms::owner_write};
 
     assert(testEquality(f1, f2, false));
   }
