d6c3c908df237d21646bf270e3650b1e32078a88
Aaron Ballman <aaron@aaronballman.com>
2023-11-27 11:17:06 -0500

    Remove the embed path group; it's not necessary

--- clang/lib/Driver/ToolChains/Clang.cpp
+++ clang/lib/Driver/ToolChains/Clang.cpp
@@ -1351,8 +1351,7 @@ void Clang::AddPreprocessingOptions(Compilation &C, const JobAction &JA,
 
   Args.addAllArgs(CmdArgs,
                   {options::OPT_D, options::OPT_U, options::OPT_I_Group,
-                   options::OPT_F, options::OPT_index_header_map,
-                   options::OPT_EmbedPath_Group});
+                   options::OPT_F, options::OPT_index_header_map});
 
   // Add -Wp, and -Xpreprocessor if using the preprocessor.
 
@@ -8279,9 +8278,6 @@ void ClangAs::ConstructJob(Compilation &C, const JobAction &JA,
 
   // Pass along any -I options so we get proper .include search paths.
   Args.AddAllArgs(CmdArgs, options::OPT_I_Group);
-
-  // Pass along any --embed-dir or similar options so we get proper embed paths.
-  Args.AddAllArgs(CmdArgs, options::OPT_EmbedPath_Group);
 
   // Determine the original source input.
   auto FindSource = [](const Action *S) -> const Action * {
