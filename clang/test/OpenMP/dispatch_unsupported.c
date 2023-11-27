786d368b9d4d315254df1b8631adead6f8c8e379
Christopher Di Bella <cjdb@google.com>
2023-11-27 19:58:51 +0000

    [clang] changes where a test writes its output
    Writing directly to /dev/null has permission issues on some systems, so
    we've changed this to a temporary file instead.

--- clang/test/OpenMP/dispatch_unsupported.c
+++ clang/test/OpenMP/dispatch_unsupported.c
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -emit-llvm -fopenmp -disable-llvm-passes %s -o /dev/null -verify=expected
+// RUN: %clang_cc1 -emit-llvm -fopenmp -disable-llvm-passes %s -o %t -verify=expected
 
 // expected-error@+2 {{cannot compile this OpenMP dispatch directive yet}}
 void a(){
