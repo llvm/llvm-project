ee0c547bca29787e646f7d172359ff91aef542de
Stephan T. Lavavej <stl@nuwen.net>
2023-11-26 17:11:24 -0800

    [libc++][test] Cleanup typos and unnecessary semicolons (#73435)
    I've structured this into a series of commits for even easier reviewing,
    if that helps. I could easily split this up into separate PRs if
    desired, but as this is low-risk with simple edits, I thought one PR
    would be easiest.
    
    * Drop unnecessary semicolons after function definitions.
    * Cleanup comment typos.
    * Cleanup `static_assert` typos.
    * Cleanup test code typos.
    + There should be no functional changes, assuming I've changed all
    occurrences.
    * ~~Fix massive test code typos.~~
    + This was a real problem, but needed more surgery. I reverted those
    changes here, and @philnik777 is fixing this properly with #73444.
    * clang-formatting as requested by the CI.

--- libcxx/test/std/utilities/format/format.formatter/format.context/format.context/locale.pass.cpp
+++ libcxx/test/std/utilities/format/format.formatter/format.context/format.context/locale.pass.cpp
@@ -32,7 +32,7 @@ void test() {
   std::locale fr_FR{LOCALE_fr_FR_UTF_8};
   std::basic_string<CharT> string = MAKE_STRING(CharT, "string");
   // The type of the object is an exposition only type. The temporary is needed
-  // to extend the lifetype of the object since args stores a pointer to the
+  // to extend the lifetime of the object since args stores a pointer to the
   // data in this object.
   auto format_arg_store = std::make_format_args<std::basic_format_context<OutIt, CharT>>(true, CharT('a'), 42, string);
   std::basic_format_args args = format_arg_store;
