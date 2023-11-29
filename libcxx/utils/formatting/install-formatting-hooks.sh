b50ac103f449eb570460754893122dd0c6296204
eric <eric@efcs.ca>
2023-11-29 10:30:34 -0500

    Attempt to use the new check on the bots.

--- libcxx/utils/formatting/install-formatting-hooks.sh
+++ libcxx/utils/formatting/install-formatting-hooks.sh
@@ -2,6 +2,14 @@
 
 
 # Jump up to the root directory of the repository
+FORCE=${LLVM_FORCE_FORMATTING:-0}
+
+for arg in "$@"
+do
+  if [ "$arg" = "--force" ]; then
+    FORCE=1
+  fi
+done
 
 git_repo_path="$(git rev-parse --show-toplevel)"
 cd "$git_repo_path"
@@ -9,8 +17,9 @@ cd "$git_repo_path"
 # Set up git-clang-format config so that it formats files without extensions as needed by libc++
 function check_formatting_config() {
   # Set the desired git config key and value
-  CONFIG_KEY="clangFormat.extensions"
-  CONFIG_VALUE=" c,h,m,mm,cpp,cxx,hpp,,"
+  CONFIG_KEY=$1
+  CONFIG_VALUE=$2
+  ALLOW_DIFFERENT=$3
 
   # Get the actual value of the config key
   ACTUAL_VALUE=$(git config --local --get $CONFIG_KEY)
@@ -18,11 +27,13 @@ function check_formatting_config() {
   # Check if the actual value is the same as the desired value
   if [[ "$ACTUAL_VALUE" == "$CONFIG_VALUE" ]]; then
       echo "The git config value for $CONFIG_KEY is correctly set to $CONFIG_VALUE"
+  elif [[ "$ACTION_VALUE" != "" ]] && [[ "$ALLOW_DIFFERENT" -eq 1 ]]; then
+    echo "The git config value for $CONFIG_KEY is set to $ACTUAL_VALUE, but $CONFIG_VALUE is allowed."
   else
       echo "Setting up git-clang-format config for libc++..."
       # Prompt the user to set the git config key to the desired value
       echo "Git config key $CONFIG_KEY is not set or incorrect."
-      read -p "Would you like to set it to $CONFIG_VALUE [Y/n]? " -n 1 -r
+      read -p "Would you like to set it to $CONFIG_VALUE [Y/n]? " -n $FORCE -r
       echo
       if [[ $REPLY =~ ^[Yy]$ ]]
       then
@@ -52,24 +63,26 @@ function check_clang_format() {
 function check_pre_commit_hooks() {
   # Check if libcxx-formatting.sh is present in pre-commit hook
   PRE_COMMIT_FILE=".git/hooks/pre-commit"
-  EXPECTED_COMMIT_SCRIPT=". ./libcxx/utils/formatting/libcxx-pre-commit-formatting-hook.sh"
+  EXPECTED_COMMIT_SCRIPT=". ./libcxx/utils/formatting/pre-commit-formatting-hook.sh"
   if grep -q -F "$EXPECTED_COMMIT_SCRIPT" "$PRE_COMMIT_FILE"; then
-      echo "libcxx-pre-commit-formatting-hook.sh is already installed in pre-commit hook."
+      echo "pre-commit-formatting-hook.sh is already installed in pre-commit hook."
   else
       # Offer to install it
-      read -p "libcxx-pre-commit-formatting-hook.sh is not installed. Would you like to install it [Y/n]? " -n 1 -r
+      read -p "pre-commit-formatting-hook.sh is not installed. Would you like to install it [Y/n]? " -n $FORCE -r
       echo
       if [[ $REPLY =~ ^[Yy]$ ]]
       then
-          echo "Installing libcxx-pre-commit-formatting-hook.sh..."
+          echo "Installing pre-commit-formatting-hook.sh..."
           echo "$EXPECTED_COMMIT_SCRIPT" >> "$PRE_COMMIT_FILE"
-          echo "Installed libcxx-pre-commit-formatting-hook.sh to pre-commit hook."
+          chmod +x "$PRE_COMMIT_FILE"
+          echo "Installed pre-commit-formatting-hook.sh to pre-commit hook."
       else
           echo "No changes were made to the pre-commit hook."
       fi
   fi
 }
 
-check_formatting_config
+check_formatting_config "clangFormat.extensions" "c,h,m,mm,cpp,cxx,hpp,," 0
+check_formatting_config "llvmPrecommit.formatPaths" "libcxx libcxxabi libunwind" 1
 check_clang_format
 check_pre_commit_hooks
---
+++ libcxx/utils/formatting/pre-commit-formatting-hook.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -x
+set -e
+FORCE=${LLVM_FORCE_FORMATTING:-0}
+VERBOSE=${VERBOSE:-0}
+
+for arg in "$@"
+do
+  if [ "$arg" = "--force" ]; then
+    FORCE=1
+  fi
+done
+
+FORMAT_PATHS=$(git config --get llvmPrecommit.formatPaths || echo ".")
+
+echo "FORMAT_PATHS is set to: $FORMAT_PATHS"
+
+
+# Define the staged files
+staged_files=$(git diff --cached --name-only --diff-filter=d -- $FORMAT_PATHS)
+
+# Check if any of the staged files have not been properly formatted with git-clang-format
+
+echo "Checking $file..."
+formatting_diff=$(git-clang-format --diff -- $staged_files) # quotes are used here
+if [[ "$formatting_diff" != "no modified files to format" && "$formatting_diff" != "clang-format did not modify any files" ]]; then
+    echo "$file has not been formatted with git-clang-format."
+    git-clang-format --diff -- $staged_files
+    read -p "Format the files [Y/n]? " -n $FORCE -r
+      echo
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+          git-clang-format -- $staged_files
+          git add $staged_files
+          exit 0
+    else
+          echo "No changes were made to the git config."
+          exit 1
+    fi
+fi
+
+
+# Everything checks out
+echo "All staged files have been formatted with git-clang-format."
+exit 0
