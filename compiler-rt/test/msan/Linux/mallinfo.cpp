f5f5c69068aea42905d6fa5acf2267642ab44164
Vitaly Buka <vitalybuka@google.com>
2023-11-29 11:00:17 -0800

    [msan] Intercept mallinfo2 (#73729)

--- compiler-rt/test/msan/Linux/mallinfo.cpp
+++ compiler-rt/test/msan/Linux/mallinfo.cpp
@@ -8,5 +8,8 @@
 int main(void) {
   struct mallinfo mi = mallinfo();
   assert(__msan_test_shadow(&mi, sizeof(mi)) == -1);
+
+  struct mallinfo2 mi2 = mallinfo2();
+  assert(__msan_test_shadow(&mi2, sizeof(mi2)) == -1);
   return 0;
 }
