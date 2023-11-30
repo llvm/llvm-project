250ec6be63d63f38cee1f7ee7cbf342cbab456fa
Aaron Ballman <aaron@aaronballman.com>
2023-11-30 11:18:07 -0500

    Remove a newly-added unused diagnostic group, add test coverage

--- clang/test/Preprocessor/embed_parameter_unrecognized.c
+++ clang/test/Preprocessor/embed_parameter_unrecognized.c
@@ -1,4 +1,6 @@
 // RUN: %clang_cc1 %s -std=c23 -E -verify
+// RUN: %clang_cc1 %s -std=c23 -E -Wno-unknown-directive-parameters -verify=okay
+// okay-no-diagnostics
 
 #embed __FILE__ unrecognized
 // expected-warning@-1 {{unknown embed preprocessor parameter 'unrecognized' ignored}}
