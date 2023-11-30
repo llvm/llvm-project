8de61cc72f1948f34a19eca109d9137c46aeaf00
Philip Reames <preames@rivosinc.com>
2023-11-30 12:00:12 -0800

    [LLD][RISCV] Add test case showing incorrect call relaxation

---
+++ lld/test/ELF/riscv-relax-call-mixed-rvc.s
@@ -0,0 +1,30 @@
+# REQUIRES: riscv
+# RUN: rm -rf %t && split-file %s %t && cd %t
+# RUN: llvm-mc -filetype=obj -triple=riscv64 -mattr=+c,+relax a.s -o a.o
+# RUN: llvm-mc -filetype=obj -triple=riscv64 -mattr=-c,+relax b.s -o b.o
+
+# RUN: ld.lld a.o b.o --shared -o a
+# RUN: llvm-objdump -d --no-show-raw-insn -M no-aliases a | FileCheck %s
+
+## FIXME: The use of c.j is wrong here, because this is coming from a source
+## file which does not use C.
+# CHECK-LABEL: <foo>:
+# CHECK-NEXT:    1260: c.j 0x1260 <foo>
+# CHECK-NEXT:    1262: addi zero, zero, 0
+
+# w/C
+#--- a.s
+	.text
+	.attribute	4, 16
+	.attribute	5, "rv64i2p1_m2p0_a2p1_f2p2_d2p2_c2p0_zicsr2p0_zifencei2p0"
+
+# w/o C
+#--- b.s
+	.text
+	.attribute	4, 16
+	.attribute	5, "rv64i2p1_m2p0_a2p1_f2p2_d2p2_zicsr2p0_zifencei2p0"
+	.p2align	5
+	.type	foo,@function
+foo:
+    tail    foo
+    nop
