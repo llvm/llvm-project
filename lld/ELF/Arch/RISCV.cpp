60dc91ace9ca4a867f44762defbfb2f538f24865
Philip Reames <preames@rivosinc.com>
2023-12-01 11:14:16 -0800

    Address review comments

--- lld/ELF/Arch/RISCV.cpp
+++ lld/ELF/Arch/RISCV.cpp
@@ -688,10 +688,10 @@ static bool relax(InputSection &sec) {
       remove = nextLoc - ((loc + align - 1) & -align);
       // If we can't satisfy this alignment, we've found a bad input.
       if (LLVM_UNLIKELY(static_cast<int32_t>(remove) < 0)) {
-        error(getErrorLocation((const uint8_t*)loc) +
-              "insufficient padding bytes for " + lld::toString(r.type) +
-              ": " + Twine(r.addend) + " bytes available "
-              + "for requested alignment of " + Twine(align) + " bytes");
+        errorOrWarn(getErrorLocation((const uint8_t*)loc) +
+                    "insufficient padding bytes for " + lld::toString(r.type) +
+                    ": " + Twine(r.addend) + " bytes available "
+                    "for requested alignment of " + Twine(align) + " bytes");
         remove = 0;
       }
       break;
