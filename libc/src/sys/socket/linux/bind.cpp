b839e06282d22f09fe52f28f6917c365c95c302c
Michael Jones <michaelrj@google.com>
2023-11-30 16:39:01 -0800

    [libc] Add bind function
    This patch adds the bind function to go with the socket function. It
    also cleans up a lot of socket related data structures.

--- libc/src/sys/socket/linux/bind.cpp
+++ libc/src/sys/socket/linux/bind.cpp
@@ -1,4 +1,4 @@
-//===-- Linux implementation of socket ------------------------------------===//
+//===-- Linux implementation of bind --------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,7 +6,7 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "src/sys/socket/socket.h"
+#include "src/sys/socket/bind.h"
 
 #include "src/__support/OSUtil/syscall.h" // For internal syscall function.
 #include "src/__support/common.h"
@@ -18,15 +18,17 @@
 
 namespace LIBC_NAMESPACE {
 
-LLVM_LIBC_FUNCTION(int, socket, (int domain, int type, int protocol)) {
+LLVM_LIBC_FUNCTION(int, bind,
+                   (int domain, const struct sockaddr *address,
+                    socklen_t address_len)) {
 #ifdef SYS_socket
   int ret =
-      LIBC_NAMESPACE::syscall_impl<int>(SYS_socket, domain, type, protocol);
+      LIBC_NAMESPACE::syscall_impl<int>(SYS_bind, domain, address, address_len);
 #elif defined(SYS_socketcall)
   unsigned long sockcall_args[3] = {static_cast<unsigned long>(domain),
-                                    static_cast<unsigned long>(type),
-                                    static_cast<unsigned long>(protocol)};
-  int ret = LIBC_NAMESPACE::syscall_impl<int>(SYS_socketcall, SYS_SOCKET,
+                                    reinterpret_cast<unsigned long>(address),
+                                    static_cast<unsigned long>(address_len)};
+  int ret = LIBC_NAMESPACE::syscall_impl<int>(SYS_socketcall, SYS_BIND,
                                               sockcall_args);
 #else
 #error "socket and socketcall syscalls unavailable for this platform."
