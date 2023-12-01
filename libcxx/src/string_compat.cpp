15a319ae34d11c61fd886de8d3a67f3dd04aafbb
David Tenty <daltenty@ibm.com>
2023-11-30 21:00:12 -0500

    [libcxx] retain a version of basic_string::reserve(0) that shrinks
        Old (LLVM >=6) versions of the move assignment operator in basic string used
        to (indirectly) zero the size of the string then call reserve(0) to
        deallocate the memory they were holding. Implementing p0966r1 changed this by changing the contract of reserve() to never shrink, leading to memory leaks.
    
        This PR attempts to fix this situation by:
        1. reverting the shrinking behaviour in the restricted case where we reserve(0) on a zero-sized string (i.e. it has been cleared first)
        2. applying an ABI-tag to reserve with the new behaviour, so uses going forward will always get the C++20+ standard compliant non-shrinking reserve in all cases

---
+++ libcxx/src/string_compat.cpp
@@ -0,0 +1,25 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#if !defined(_LIBCPP_ABI_DO_NOT_RETAIN_SHRINKING_RESERVE)
+
+// Instantiate a copy of the shrinking reserve implementation to maintain ABI compatibility for older versions of
+// basic_string which relied on this behavior in move assignment.
+#define _LIBCPP_ENABLE_RESERVE_SHRINKING_ABI
+#include <string>
+
+_LIBCPP_BEGIN_NAMESPACE_STD
+
+template _LIBCPP_EXPORTED_FROM_ABI void basic_string<char>::reserve(size_type);
+#   ifndef _LIBCPP_HAS_NO_WIDE_CHARACTERS
+template _LIBCPP_EXPORTED_FROM_ABI void basic_string<wchar_t >::reserve(size_type);
+#   endif
+
+_LIBCPP_END_NAMESPACE_STD
+
+#endif
---
+++ libcxx/test/libcxx/strings/basic.string/string.modifiers/reserve_with_shrink.pass.cpp
@@ -0,0 +1,44 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// <string>
+
+// Check reserve(0) with old mangling shrinks for compatibility if it exists.
+#if !defined(_LIBCPP_ABI_DO_NOT_RETAIN_SHRINKING_RESERVE)
+#define _LIBCPP_ENABLE_RESERVE_SHRINKING_ABI
+#include <string>
+#include <cassert>
+
+#include "test_macros.h"
+#include "min_allocator.h"
+
+bool test() {
+  std::string l = "Long string so that allocation definitely, for sure, absolutely happens. Probably.";
+  const char *c = l.c_str();
+
+  assert(l.__invariants());
+
+  l.clear();
+  assert(l.__invariants());
+  assert(l.size() == 0);
+
+  l.reserve(0);
+  assert(l.__invariants());
+  assert(l.size() == 0);
+  assert(c != l.c_str());
+
+  return true;
+}
+#else
+bool test() { return true; }
+#endif
+
+int main(int, char**) {
+  test();
+  return 0;
+}
