0560b3c2518b797849c15d7c9c502c46c009d2a3
Ivan Arkhipov <arkhipov.iv99@mail.ru>
2023-12-02 19:21:34 +0300

    Finding integer value

--- clang-tools-extra/clang-tidy/modernize/UseDigitSeparatorCheck.cpp
+++ clang-tools-extra/clang-tidy/modernize/UseDigitSeparatorCheck.cpp
@@ -16,18 +16,24 @@ namespace clang::tidy::modernize {
 
 void UseDigitSeparatorCheck::registerMatchers(MatchFinder *Finder) {
   // FIXME: Add matchers.
-  Finder->addMatcher(functionDecl().bind("x"), this);
+//  Finder->addMatcher(functionDecl().bind("x"), this);
+  Finder->addMatcher(integerLiteral().bind("integerLiteral"), this);
 }
 
 void UseDigitSeparatorCheck::check(const MatchFinder::MatchResult &Result) {
   // FIXME: Add callback implementation.
-  const auto *MatchedDecl = Result.Nodes.getNodeAs<FunctionDecl>("x");
-  if (!MatchedDecl->getIdentifier() || MatchedDecl->getName().startswith("awesome_"))
-    return;
-  diag(MatchedDecl->getLocation(), "function %0 is insufficiently awesome")
-      << MatchedDecl
-      << FixItHint::CreateInsertion(MatchedDecl->getLocation(), "awesome_");
-  diag(MatchedDecl->getLocation(), "insert 'awesome'", DiagnosticIDs::Note);
+//  const auto *MatchedDecl = Result.Nodes.getNodeAs<FunctionDecl>("x");
+//  if (!MatchedDecl->getIdentifier() || MatchedDecl->getName().startswith("awesome_"))
+//    return;
+//  diag(MatchedDecl->getLocation(), "function %0 is insufficiently awesome")
+//      << MatchedDecl
+//      << FixItHint::CreateInsertion(MatchedDecl->getLocation(), "awesome_");
+//  diag(MatchedDecl->getLocation(), "insert 'awesome'", DiagnosticIDs::Note);
+  const auto *MatchedInteger = Result.Nodes.getNodeAs<IntegerLiteral>("integerLiteral");
+  const auto IntegerValue = MatchedInteger->getValue();
+  diag(MatchedInteger->getLocation(), "integer warning %0") << toString(IntegerValue, 10, true)
+        << FixItHint::CreateInsertion(MatchedInteger->getLocation(), "this is integer");
+  diag(MatchedInteger->getLocation(), "integer", DiagnosticIDs::Note);
 }
 
 } // namespace clang::tidy::modernize
