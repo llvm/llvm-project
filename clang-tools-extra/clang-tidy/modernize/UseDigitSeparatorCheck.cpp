43f66ac8da7a67beb2d31ec7bfc23d10391c3c77
Ivan Arkhipov <arkhipov.iv99@mail.ru>
2023-12-03 18:07:16 +0300

    Formatting

--- clang-tools-extra/clang-tidy/modernize/UseDigitSeparatorCheck.cpp
+++ clang-tools-extra/clang-tidy/modernize/UseDigitSeparatorCheck.cpp
@@ -16,18 +16,19 @@
 using namespace clang::ast_matchers;
 
 namespace {
-  std::vector<std::basic_string<char>> splitString3Symbols(const std::basic_string<char> &String) {
-    std::vector<std::basic_string<char>> Result;
-    std::basic_string<char> ReversedString(String.rbegin(), String.rend());
+std::vector<std::basic_string<char>>
+splitString3Symbols(const std::basic_string<char> &String) {
+  std::vector<std::basic_string<char>> Result;
+  std::basic_string<char> ReversedString(String.rbegin(), String.rend());
 
-    for (size_t I = 0; I < ReversedString.size(); I += 3) {
-      Result.push_back(ReversedString.substr(I, 3));
-    }
-
-    std::reverse(Result.begin(), Result.end());
-
-    return Result;
+  for (size_t I = 0; I < ReversedString.size(); I += 3) {
+    Result.push_back(ReversedString.substr(I, 3));
   }
+
+  std::reverse(Result.begin(), Result.end());
+
+  return Result;
+}
 } // namespace
 
 namespace clang::tidy::modernize {
@@ -39,10 +40,18 @@ void UseDigitSeparatorCheck::registerMatchers(MatchFinder *Finder) {
 void UseDigitSeparatorCheck::check(const MatchFinder::MatchResult &Result) {
   const auto *MatchedInteger = Result.Nodes.getNodeAs<IntegerLiteral>("integerLiteral");
   const auto IntegerValue = MatchedInteger->getValue();
-  const auto IntegerString = splitString3Symbols(toString(IntegerValue, 10, true));
-  const auto FinalString = std::accumulate(IntegerString.begin(), IntegerString.end(), std::string(""), [](std::basic_string<char> S1, std::basic_string<char> S2) {return S1 + "\'" + S2;}).erase(0, 1);
-  diag(MatchedInteger->getLocation(), "integer warning %0") << FinalString
-        << FixItHint::CreateInsertion(MatchedInteger->getLocation(), "this is integer");
+  const auto IntegerString =
+      splitString3Symbols(toString(IntegerValue, 10, true));
+  const auto FinalString =
+      std::accumulate(IntegerString.begin(), IntegerString.end(),
+                      std::string(""),
+                      [](std::basic_string<char> S1,
+                         std::basic_string<char> S2) { return S1 + "\'" + S2; })
+          .erase(0, 1);
+  diag(MatchedInteger->getLocation(), "integer warning %0")
+      << FinalString
+      << FixItHint::CreateInsertion(MatchedInteger->getLocation(),
+                                    "this is integer");
   diag(MatchedInteger->getLocation(), "integer", DiagnosticIDs::Note);
 }
 
