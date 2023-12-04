9d4430d1e6690a67a4177f84825b58267e0db4bb
Ivan Arkhipov <arkhipov.iv99@mail.ru>
2023-12-04 15:51:03 +0300

    Support for octal number system

--- clang-tools-extra/clang-tidy/modernize/UseDigitSeparatorCheck.cpp
+++ clang-tools-extra/clang-tidy/modernize/UseDigitSeparatorCheck.cpp
@@ -59,6 +59,10 @@ void UseDigitSeparatorCheck::check(const MatchFinder::MatchResult &Result) {
     Radix = 16;
     GroupSize = 4;
     Prefix = "0x";
+  } else if (OriginalLiteralString.starts_with("0") && OriginalLiteralString != "0") {
+    Radix = 8;
+    GroupSize = 3;
+    Prefix = "0";
   } else {
       Radix = 10;
       GroupSize = 3;
