f196e0ca6156df42959591d0aed8f56b1e3ebf6d
Juergen Ributzka <juergen@ributzka.de>
2023-12-04 14:26:44 -0800

    Revert "[clang][modules] Reset codegen options. (#74006)"
    This reverts commit f2cf3963fe9ddd140c299d0921b7bbfb26f94b2a.

--- clang/include/clang/Basic/CodeGenOptions.h
+++ clang/include/clang/Basic/CodeGenOptions.h
@@ -530,10 +530,6 @@ public:
     return SanitizeBinaryMetadataCovered || SanitizeBinaryMetadataAtomics ||
            SanitizeBinaryMetadataUAR;
   }
-
-  /// Reset all of the options that are not considered when building a
-  /// module.
-  void resetNonModularOptions(StringRef ModuleFormat);
 };
 
 }  // end namespace clang
