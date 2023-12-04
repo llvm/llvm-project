7b795af3032ee2c2956ae8a8e10965448a630562
Will Hawkins <hawkinsw@obs.cr>
2023-12-04 11:45:14 -0500

    fixup! WIP: [libc++][ranges] Implement `ranges::stride_view`.
    Add test for iterator::operator- on forward iterators (and clean up other iterator::operator- test).

--- libcxx/test/std/ranges/range.adaptors/range.stride.view/iterator/operator.pass.cpp
+++ libcxx/test/std/ranges/range.adaptors/range.stride.view/iterator/operator.pass.cpp
@@ -136,31 +136,72 @@ constexpr bool operator_tests() {
     static_assert(is_relationally_comparable<StrideViewIterator>);
   }
   {
-      // Test the forward-range operator- between two iterators (i.e., no ceil).
-      // TODO
-  } {
-    // Test the non-forward-range operator- between two iterators (i.e., ceil).
+    using Base = InputView<SizedForwardIterator, SizedForwardIterator>;
+    // Test the forward-range operator- between two iterators (i.e., no ceil).
     int arr[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    auto rav_zero =
-        InputView<SizedInputIterator, SizedInputIterator>(SizedInputIterator(arr), SizedInputIterator(arr + 10));
-    auto rav_one =
-        InputView<SizedInputIterator, SizedInputIterator>(SizedInputIterator(arr + 1), SizedInputIterator(arr + 10));
+    auto rav_zero    = Base(SizedForwardIterator(arr), SizedForwardIterator(arr + 10));
+    auto rav_one     = Base(SizedForwardIterator(arr + 1), SizedForwardIterator(arr + 10));
     auto stride_zoff = std::ranges::stride_view(rav_zero, 3);
     auto stride_ooff = std::ranges::stride_view(rav_one, 3);
 
-    auto stride_zoff_base = stride_zoff.begin();
-    auto stride_ooff_base = stride_ooff.begin();
+    auto stride_zoff_begin = stride_zoff.begin();
+    auto stride_ooff_begin = stride_ooff.begin();
 
-    auto stride_zoff_one   = stride_zoff_base;
-    auto stride_zoff_four  = ++stride_zoff_base;
-    auto stride_zoff_seven = ++stride_zoff_base;
+    auto stride_zoff_one   = stride_zoff_begin;
+    auto stride_zoff_four  = ++stride_zoff_begin;
+    auto stride_zoff_seven = ++stride_zoff_begin;
 
-    auto stride_ooff_two  = stride_ooff_base;
-    auto stride_ooff_five = ++stride_ooff_base;
+    auto stride_ooff_two  = stride_ooff_begin;
+    auto stride_ooff_five = ++stride_ooff_begin;
 
-    static_assert(std::sized_sentinel_for<decltype(std::move(stride_zoff_base).base()),
-                                          decltype(std::move(stride_zoff_base).base())>);
-    static_assert(can_calculate_distance_between_non_sentinel<decltype(stride_zoff_base)>);
+    static_assert(std::sized_sentinel_for<std::ranges::iterator_t<Base>, std::ranges::iterator_t<Base>>);
+    static_assert(can_calculate_distance_between_non_sentinel<decltype(stride_zoff_begin)>);
+    static_assert(std::forward_iterator<SizedForwardIterator>);
+
+    assert(*stride_zoff_one == 1);
+    assert(*stride_zoff_four == 4);
+    assert(*stride_zoff_seven == 7);
+
+    assert(*stride_ooff_two == 2);
+    assert(*stride_ooff_five == 5);
+    // Check positive __n with exact multiple of left's stride.
+    assert(stride_zoff_four - stride_zoff_one == 1);
+    assert(stride_zoff_seven - stride_zoff_one == 2);
+
+    // Check positive __n with non-exact multiple of left's stride.
+    assert(stride_ooff_two - stride_zoff_one == 0);
+    assert(stride_ooff_five - stride_zoff_one == 1);
+
+    // Check negative __n with exact multiple of left's stride.
+    assert(stride_zoff_one - stride_zoff_four == -1);
+    assert(stride_zoff_one - stride_zoff_seven == -2);
+
+    // Check negative __n with non-exact multiple of left's stride.
+    assert(stride_zoff_one - stride_ooff_two == 0);
+    assert(stride_zoff_one - stride_ooff_five == -1);
+  }
+
+  {
+    using Base = InputView<SizedInputIterator, SizedInputIterator>;
+    // Test the non-forward-range operator- between two iterators (i.e., ceil).
+    int arr[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    auto rav_zero    = Base(SizedInputIterator(arr), SizedInputIterator(arr + 10));
+    auto rav_one     = Base(SizedInputIterator(arr + 1), SizedInputIterator(arr + 10));
+    auto stride_zoff = std::ranges::stride_view(rav_zero, 3);
+    auto stride_ooff = std::ranges::stride_view(rav_one, 3);
+
+    auto stride_zoff_begin = stride_zoff.begin();
+    auto stride_ooff_begin = stride_ooff.begin();
+
+    auto stride_zoff_one   = stride_zoff_begin;
+    auto stride_zoff_four  = ++stride_zoff_begin;
+    auto stride_zoff_seven = ++stride_zoff_begin;
+
+    auto stride_ooff_two  = stride_ooff_begin;
+    auto stride_ooff_five = ++stride_ooff_begin;
+
+    static_assert(std::sized_sentinel_for<std::ranges::iterator_t<Base>, std::ranges::iterator_t<Base>>);
+    static_assert(can_calculate_distance_between_non_sentinel<decltype(stride_zoff_begin)>);
 
     assert(*stride_zoff_one == 1);
     assert(*stride_zoff_four == 4);
