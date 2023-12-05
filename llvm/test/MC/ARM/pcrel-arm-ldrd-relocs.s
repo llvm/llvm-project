bcdfa80ff5082a32a6c0eb89b4cd818ce5288658
Eleanor Bonnici <eleanor.bonnici@arm.com>
2023-12-05 17:13:22 +0000

    [llvm][MC][ARM] Don't autoresolve fixups
    Removes logic that caused some fixups to be marked as resolved in the
    assembler without actually resolving them. Sometimes to resolve these
    fixups a relocation was needed, which was not generated, resulting in
    invalid code. Now in situations when the fixup cannot be resolved by the
    assembler, either a relocation is generated, or an error is produced.
    
    This was partially addressed previously
    (https://github.com/llvm/llvm-project/pull/72873/,
    https://github.com/llvm/llvm-project/pull/73834) specifically for LDRx
    and ADR instructions, This patch expands it LDRD and VLDR, which should
    cover all instructions affected.

---
+++ llvm/test/MC/ARM/pcrel-arm-ldrd-relocs.s
@@ -0,0 +1,30 @@
+@ RUN: llvm-mc -filetype=obj -triple=armv7 %s -o %t
+@ RUN: llvm-readelf -r %t | FileCheck %s --check-prefix=ARM
+@ RUN: llvm-objdump -d --triple=armv7 %t | FileCheck %s --check-prefix=ARM_ADDEND
+@ RUN: llvm-mc -filetype=obj -triple=armebv7 %s -o %t
+@ RUN: llvm-readelf -r %t | FileCheck %s --check-prefix=ARM
+@ RUN: llvm-objdump -d --triple=armebv7 %t | FileCheck %s --check-prefix=ARM_ADDEND
+
+    .section .text.bar, "ax"
+    .balign 4
+    .global bar
+    .type bar, %function
+
+bar:
+    ldrd r0, r1, foo1    @ arm_pcrel_10_unscaled
+    ldrd r0, r1, foo2-8  @ arm_pcrel_10_unscaled
+    bx lr
+
+    .section .data.foo, "a", %progbits
+    .balign 4
+    .global foo1
+    .global foo2
+foo1:
+    .word 0x11223344, 0x55667788
+foo2:
+    .word 0x99aabbcc, 0xddeeff00
+
+@ ARM: R_ARM_LDRS_PC_G0
+
+@ ARM_ADDEND: ldrd r0, r1, [pc, #-8]
+@ ARM_ADDEND: ldrd r0, r1, [pc, #-16]
