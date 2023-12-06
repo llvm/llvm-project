49bbf5a31975dec3f11e91f4147452dafdd39b7b
Brian Kelley <bmkelle@sandia.gov>
2023-12-06 14:54:03 -0700

    Sparse kokkos codegen pass works
    - Correctly rewrites scf.parallel to a call to a kernel function
    - Handles host-device data transfers for local memrefs
    - Handles reduction return values

--- mlir/lib/Dialect/SparseTensor/Transforms/SparseKokkosCodegen.cpp
+++ mlir/lib/Dialect/SparseTensor/Transforms/SparseKokkosCodegen.cpp
@@ -27,6 +27,15 @@ using namespace mlir::sparse_tensor;
 
 namespace {
 
+// Enum representing memref access types.
+// This is meant to be used as a bitfield (READ | WRITE == READWRITE)
+enum MRAccess
+{
+  READ = 1,
+  WRITE = 2,
+  READWRITE = 3
+};
+
 //===----------------------------------------------------------------------===//
 // Helper methods.
 //===----------------------------------------------------------------------===//
@@ -36,8 +45,7 @@ namespace {
 /// and nothing else. All memrefs are expected to be in device space
 /// (DefaultExecutionSpace::memory_space), not HostSpace.
 static func::FuncOp genKokkosFunc(OpBuilder &builder, ModuleOp module,
-                                 SmallVectorImpl<Value> &args,
-                                 scf::ParallelOp forallOp) {
+                                 scf::ParallelOp forallOp, SmallVectorImpl<Value> &args) {
   static unsigned kernelNumber = 0;
   SmallString<16> kernelName;
   ("kokkos_sparse_kernel_" + Twine(kernelNumber++)).toStringRef(kernelName);
@@ -46,9 +54,8 @@ static func::FuncOp genKokkosFunc(OpBuilder &builder, ModuleOp module,
   SmallVector<Type> argsTp;
   for (unsigned i = 0, e = args.size(); i < e; i++)
     argsTp.push_back(args[i].getType());
-  auto results = forallOp.getResults();
   SmallVector<Type> resultsTp;
-  for (auto r : results)
+  for (auto r : forallOp.getResults())
     resultsTp.push_back(r.getType());
   FunctionType type = FunctionType::get(builder.getContext(), argsTp, resultsTp);
   func::FuncOp func = builder.create<func::FuncOp>(module.getLoc(), kernelName, type);
@@ -77,11 +84,12 @@ static void genDeepCopy(OpBuilder &builder, Location loc, Value dst, Value src) 
   builder.create<emitc::CallOp>(loc, TypeRange(), "asyncDeepCopy", ArrayAttr(), ArrayAttr(), ValueRange({dst, src}));
 }
 
-/// Generates an alloc/copy pair.
-static Value genAllocCopy(OpBuilder &builder, Location loc, Value hostMem) {
+/// Generates an alloc (and possibly copy, if the buffer is read).
+static Value genAllocCopy(OpBuilder &builder, Location loc, Value hostMem, int access) {
   auto alloc = genCreateDeviceMirror(builder, loc, hostMem);
   Value devMem = alloc.getResult(0);
-  genDeepCopy(builder, loc, devMem, hostMem);
+  if(access & MRAccess::READ)
+    genDeepCopy(builder, loc, devMem, hostMem);
   return devMem;
 }
 
@@ -90,10 +98,18 @@ static Value genAllocCopy(OpBuilder &builder, Location loc, Value hostMem) {
 /// a set of properly chained asynchronous allocation/copy pairs to increase
 /// overlap before launching the kernel.
 static void genParametersIn(OpBuilder &builder, Location loc,
-                             SmallVectorImpl<Value> &buffers) {
+                             SmallVectorImpl<Value> &scalars,
+                             SmallVectorImpl<Value> &buffers,
+                             SmallVectorImpl<int> &bufferAccess,
+                             SmallVectorImpl<Value> &args) {
+  // Scalars are passed by value.
+  for (Value s : scalars)
+    args.push_back(s);
   // Buffers are need to be made visible on device.
-  for (Value b : buffers) {
-    args.push_back(genAllocCopy(builder, loc, b));
+  for (auto i : zip(buffers, bufferAccess)) {
+    Value buffer = std::get<0>(i);
+    int access = std::get<1>(i);
+    args.push_back(genAllocCopy(builder, loc, buffer, access));
   }
 }
 
@@ -101,13 +117,20 @@ static void genParametersIn(OpBuilder &builder, Location loc,
 /// on the kernel token and then deallocated. All other buffers are simply
 /// deallocated. Then we wait for all operations to complete.
 static void genParametersOut(OpBuilder &builder, Location loc,
-                             SmallVectorImpl<Value> &buffers) {
+                             SmallVectorImpl<Value> &scalars,
+                             SmallVectorImpl<Value> &buffers,
+                             SmallVectorImpl<int> &bufferAccess,
+                             SmallVectorImpl<Value> &args) {
+  unsigned base = scalars.size();
   // Go through the buffer (memref-typed) args.
-  // The first one is the output buffer, and must be copied back to host.
-  // The others are inputs and just need to be deallocated.
-  genDeepCopy(builder, loc, buffers[0], args[base]);
+  // For those that were written to by the parallel,
+  // generate a copy back to the original host buffer.
+  //
+  // For all mirror buffers, deallocate them.
   // Sequence the output tensor dealloc after it's copied to host.
   for (unsigned i = base, e = args.size(); i < e; i++) {
+    if(bufferAccess[i - base] & MRAccess::WRITE)
+      genDeepCopy(builder, loc, buffers[i - base], args[i]);
     genDestroyDeviceMirror(builder, loc, args[i], buffers[i - base]);
   }
 }
@@ -134,7 +157,6 @@ static void genKokkosCode(PatternRewriter &rewriter, func::FuncOp func,
   // all constants, and replace scalars and buffers with corresponding arguments to func.
   auto newForall = rewriter.clone(*forallOp, irMap);
   
-  //TODO: if forallOp has at least one reduction at the top level, func should return those result(s)
   rewriter.create<func::ReturnOp>(forallOp.getLoc(), newForall->getResults());
 }
 
@@ -150,12 +172,10 @@ struct KokkosForallRewriter : public OpRewritePattern<scf::ParallelOp> {
     if(forallOp->getParentOfType<scf::ParallelOp>()) {
       return failure();
     }
-    // Do not run on ParallelOps that have already been moved into a sparse kernel function
+    // Do not run on ParallelOps that are already in 
     auto enclosingFuncName = forallOp->getParentOfType<func::FuncOp>().getSymName();
     if(enclosingFuncName.starts_with("kokkos_sparse_kernel_"))
       return failure();
-    puts("Hello from KokkosForallRewriter, processing top-level ParallelOp:");
-    forallOp.dump();
     // Check the forallOp for operations that can't be executed on device
     // NOTE: Kokkos emitter will have to check for this again, since we have
     // no way to mark an scf.parallel as executing in a particular place.
@@ -175,9 +195,6 @@ struct KokkosForallRewriter : public OpRewritePattern<scf::ParallelOp> {
       // it will execute on host (but can still be parallel).
       return failure();
     }
-    ModuleOp module = forallOp->getParentOfType<ModuleOp>();
-    puts("The module before:");
-    module.dump();
     // Collect every value that is computed outside the parallel loop.
     SetVector<Value> invariants; // stable iteration!
     forallOp->walk([&](Operation *op) {
@@ -193,48 +210,79 @@ struct KokkosForallRewriter : public OpRewritePattern<scf::ParallelOp> {
           invariants.insert(val);
       }
     });
+    // Outline the outside values as proper parameters. Fail when sharing
+    // value between host and device is not straightforward.
+    SmallVector<Value> constants;
+    SmallVector<Value> scalars;
     SmallVector<Value> buffers;
     for (Value val : invariants) {
       Type tp = val.getType();
-      if (isa<MemRefType>(tp))
+      if (val.getDefiningOp<arith::ConstantOp>())
+        constants.push_back(val);
+      else if (isa<FloatType>(tp) || tp.isIntOrIndex())
+        scalars.push_back(val);
+      else if (isa<MemRefType>(tp))
         buffers.push_back(val);
+      else
+        return failure(); // don't know how to share
+    }
+    // Determine the access mode for each buffer
+    // (MRAccess::READ, ::WRITE, or ::READWRITE)
+    // TODO: this does attempt to handle memref subview, cast, flatten, etc.
+    // In these cases, the actual owner of the memory is not the memref
+    // used inside the parallel, but rather some other memref. This is what
+    // needs to be copied to/from device.
+    SmallVector<int> bufferAccess(buffers.size(), 0);
+    WalkResult walkResult = forallOp->walk([&](memref::StoreOp store) -> WalkResult {
+        Value mr = store.getMemref();
+        for(size_t i = 0; i < buffers.size(); i++) {
+          if(buffers[i] == mr) {
+            bufferAccess[i] |= MRAccess::WRITE;
+            return WalkResult::advance();
+          }
+        }
+        // The memref accessed by store should have been found in buffers.
+        // If not, it's an error.
+        return WalkResult::interrupt();
+    });
+    if(walkResult.wasInterrupted()) {
+      return forallOp.emitError("A memref.store inside an scf.parallel accessed an unknown buffer");
+    }
+    walkResult = forallOp->walk([&](memref::LoadOp load) -> WalkResult {
+        Value mr = load.getMemref();
+        for(size_t i = 0; i < buffers.size(); i++) {
+          if(buffers[i] == mr) {
+            bufferAccess[i] |= MRAccess::READ;
+            return WalkResult::advance();
+          }
+        }
+        // The memref accessed by load should have been found in buffers.
+        // If not, it's an error.
+        return WalkResult::interrupt();
+    });
+    if(walkResult.wasInterrupted()) {
+      return forallOp.emitError("A memref.load inside an scf.parallel accessed an unknown buffer");
     }
     // Pass outlined non-constant values.
     Location loc = forallOp->getLoc();
     SmallVector<Value> args;
-    genParametersIn(rewriter, loc, scalars, buffers, args);
-    puts("The module 1:");
-    module.dump();
+    genParametersIn(rewriter, loc, scalars, buffers, bufferAccess, args);
     auto saveIp = rewriter.saveInsertionPoint();
-    auto func = genKokkosFunc(rewriter, module, args, forallOp);
-    puts("The module 2:");
-    module.dump();
+    ModuleOp module = forallOp->getParentOfType<ModuleOp>();
+    auto func = genKokkosFunc(rewriter, module, forallOp, args);
     genKokkosCode(rewriter, func, forallOp, constants, scalars, buffers);
-    puts("The module 3:");
-    module.dump();
     // Move the rewriter back to the kernel launch site
     rewriter.restoreInsertionPoint(saveIp);
-    // Call the Kokkos function
+    // Replace the original forallOp with a call to the kernel function.
+    // This includes replacing all uses of forallOp's results with the results of the call.
     auto kernelCall = rewriter.create<func::CallOp>(loc, func, args);
-    auto origForallResults = forallOp.getResults();
-    auto callResults = kernelCall->getResults();
     // Finalize the outlined arguments.
-    genParametersOut(rewriter, loc, scalars, buffers, args);
-    // Replace the original forall's results with the new kernel call's results.
-    for(size_t i = 0; i < callResults.size(); i++)
-    {
-      module->replaceUsesOfWith(origForallResults[i], callResults[i]);
-    }
-    puts("The module 4:");
-    module.dump();
+    genParametersOut(rewriter, loc, scalars, buffers, bufferAccess, args);
     // Lastly, add a Kokkos::fence() to make sure the kernel
     // and following deep copy have completed
     rewriter.create<emitc::CallOp>(loc, TypeRange(),
         "Kokkos::DefaultExecutionSpace().fence", ArrayAttr(), ArrayAttr(), ValueRange());
-    rewriter.eraseOp(forallOp);
-    puts("Finished rewriting!");
-    puts("The module now:");
-    module.dump();
+    rewriter.replaceOp(forallOp, kernelCall.getResults());
     return success();
   }
 
