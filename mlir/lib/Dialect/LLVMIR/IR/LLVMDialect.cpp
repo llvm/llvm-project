78879e1fb040479f67a27b3456af371501c7b60b
Tom Eccles <tom.eccles@arm.com>
2023-12-06 17:35:06 +0000

    Hoist FMF attribute filtering out of if

--- mlir/lib/Dialect/LLVMIR/IR/LLVMDialect.cpp
+++ mlir/lib/Dialect/LLVMIR/IR/LLVMDialect.cpp
@@ -69,12 +69,13 @@ static ParseResult parseLLVMOpAttrs(OpAsmParser &parser,
 
 static void printLLVMOpAttrs(OpAsmPrinter &printer, Operation *op,
                              DictionaryAttr attrs) {
+  auto filteredAttrs = processFMFAttr(attrs.getValue());
   if (auto iface = dyn_cast<IntegerOverflowFlagsInterface>(op))
     printer.printOptionalAttrDict(
-        processFMFAttr(attrs.getValue()),
+        filteredAttrs,
         /*elidedAttrs=*/{iface.getIntegerOverflowAttrName()});
   else
-    printer.printOptionalAttrDict(processFMFAttr(attrs.getValue()));
+    printer.printOptionalAttrDict(filteredAttrs);
 }
 
 /// Verifies `symbol`'s use in `op` to ensure the symbol is a valid and
