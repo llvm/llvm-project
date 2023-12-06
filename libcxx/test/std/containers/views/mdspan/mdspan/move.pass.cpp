9a5f0cb8efb796598bdf22dffcc9563588107e10
Stephan T. Lavavej <stl@nuwen.net>
2023-12-05 23:26:26 -0800

    Drop unnecessary `typename` and `template` disambiguators for `std::layout_meow::mapping`.
    (Note that `typename OtherLayout::template mapping<E2>` is an example of a situation where they're needed.)
    
    This is a pure regex replacement from:
    `(?:typename )?std::layout_(left|right|stride)::template mapping`
    to:
    `std::layout_$1::mapping`

--- libcxx/test/std/containers/views/mdspan/mdspan/move.pass.cpp
+++ libcxx/test/std/containers/views/mdspan/mdspan/move.pass.cpp
@@ -59,7 +59,7 @@ constexpr void mixin_extents(const H& handle, const L& layout, const A& acc) {
 template <class H, class A>
 constexpr void mixin_layout(const H& handle, const A& acc) {
   // make sure we test a trivially copyable mapping
-  static_assert(std::is_trivially_move_assignable_v<typename std::layout_left::template mapping<std::extents<int>>>);
+  static_assert(std::is_trivially_move_assignable_v<std::layout_left::mapping<std::extents<int>>>);
   mixin_extents(handle, std::layout_left(), acc);
   mixin_extents(handle, std::layout_right(), acc);
   // make sure we test a not trivially copyable mapping
