fc791b61272322d72238533dccadd9564e117894
Aiden Grossman <agrossman154@yahoo.com>
2023-12-07 00:32:33 -0800

    [llvm-exegesis] Add option to specify the number of measurement repetitions (#74276)
    Currently, the llvm-exegesis LatencyBenchmarkRunner repeats the
    benchmark several times (currently 30) and then aggregates the result to
    deal with noise in the measurement process. With this patch, the number
    of repetitions to perform is made configurable rather than left as a
    static number. This allows for significantly faster execution in
    situations where someone is performing a task like experimenting with
    memory annotations where the exact cycle counts might not be useful, and
    also allows for increased precision when desired.

--- llvm/tools/llvm-exegesis/llvm-exegesis.cpp
+++ llvm/tools/llvm-exegesis/llvm-exegesis.cpp
@@ -262,6 +262,12 @@ static cl::opt<BenchmarkRunner::ExecutionModeE> ExecutionMode(
                           "allows for the use of memory annotations")),
     cl::init(BenchmarkRunner::ExecutionModeE::InProcess));
 
+static cl::opt<unsigned> BenchmarkRepeatCount(
+    "benchmark-repeat-count",
+    cl::desc("The number of times to repeat measurements on the benchmark k "
+             "before aggregating the results"),
+    cl::cat(BenchmarkOptions), cl::init(30));
+
 static ExitOnError ExitOnErr("llvm-exegesis error: ");
 
 // Helper function that logs the error(s) and exits.
@@ -485,7 +491,7 @@ void benchmarkMain() {
   const std::unique_ptr<BenchmarkRunner> Runner =
       ExitOnErr(State.getExegesisTarget().createBenchmarkRunner(
           BenchmarkMode, State, BenchmarkPhaseSelector, ExecutionMode,
-          ResultAggMode));
+          BenchmarkRepeatCount, ResultAggMode));
   if (!Runner) {
     ExitWithError("cannot create benchmark runner");
   }
