13b88265088329decd15449e3b2461a6177174b2
Johannes Doerfert <johannes@jdoerfert.de>
2023-12-06 16:04:23 -0800

    Revert " [OpenMP][NFC] Remove `DelayedBinDesc`" (#74679)
    Reverts llvm/llvm-project#74360
    
    As I wrote in the analysis of #74360:
    
    Since
    https://github.com/llvm/llvm-project/commit/bc4e0c048aa3cd940b0cea787014c7e8680e5040
    we will not add PluginAdaptors into the container of all plugin adaptors
    before the plugin is not ready. The error is thereby gone. When and old
    HSA loads other libraries they can call register_image but that will
    simply not register the image with the plugin we are currently
    initializing. That seems like reasonable behavior, thought it is good to
    keep in mind if we ever want a kernel library (@jhuber6 @mjklemm). We
    can still have a standalone kernel library though or load it late after
    all plugins are setup (which seems reasonable).
    
    I did not expect one our tests actually doing exactly what this will not
    allow anymore, at least when you use rocm <5.5.0. Need to figure out if
    we want this behavior (for rocm <5.5.0).

--- openmp/libomptarget/test/offloading/bug60119.c
+++
@@ -1,6 +0,0 @@
-// RUN: %clang-generic -fPIC -shared %S/../Inputs/empty.c -o %T/liba.so
-// RUN: %clang-generic -fPIC -shared %S/../Inputs/empty.c -o %T/libb.so
-// RUN: %clang-generic -rpath %T -L %T -l a -l b %s -o %t
-// RUN: %t
-
-int main() {}
