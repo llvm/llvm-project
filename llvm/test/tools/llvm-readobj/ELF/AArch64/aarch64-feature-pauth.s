94b5cfefef93acc8530aad168a68bf17abf09689
Daniil Kovalev <dkovalev@accesssoftek.com>
2023-12-07 05:39:49 +0300

    Use `#` instead of `//` for lit and FileCheck directives

--- llvm/test/tools/llvm-readobj/ELF/AArch64/aarch64-feature-pauth.s
+++ llvm/test/tools/llvm-readobj/ELF/AArch64/aarch64-feature-pauth.s
@@ -16,52 +16,52 @@
 # RUN: llvm-readobj --notes tag-short.o | FileCheck --check-prefix LLVM-SHORT %s
 # RUN: llvm-readobj --notes tag-long.o  | FileCheck --check-prefix LLVM-LONG %s
 
-// LLVM-SHORT:      Notes [
-// LLVM-SHORT-NEXT:   NoteSection {
-// LLVM-SHORT-NEXT:     Name: .note.AARCH64-PAUTH-ABI-tag
-// LLVM-SHORT-NEXT:     Offset: 0x40
-// LLVM-SHORT-NEXT:     Size: 0x1C
-// LLVM-SHORT-NEXT:     Note {
-// LLVM-SHORT-NEXT:       Owner: ARM
-// LLVM-SHORT-NEXT:       Data size: 0xC
-// LLVM-SHORT-NEXT:       Type: NT_ARM_TYPE_PAUTH_ABI_TAG
-// LLVM-SHORT-NEXT:       Description data (
-// LLVM-SHORT-NEXT:         0000: 2A000000 00000000 01000000
-// LLVM-SHORT-NEXT:       )
-// LLVM-SHORT-NEXT:     }
-// LLVM-SHORT-NEXT:   }
-// LLVM-SHORT-NEXT: ]
+# LLVM-SHORT:      Notes [
+# LLVM-SHORT-NEXT:   NoteSection {
+# LLVM-SHORT-NEXT:     Name: .note.AARCH64-PAUTH-ABI-tag
+# LLVM-SHORT-NEXT:     Offset: 0x40
+# LLVM-SHORT-NEXT:     Size: 0x1C
+# LLVM-SHORT-NEXT:     Note {
+# LLVM-SHORT-NEXT:       Owner: ARM
+# LLVM-SHORT-NEXT:       Data size: 0xC
+# LLVM-SHORT-NEXT:       Type: NT_ARM_TYPE_PAUTH_ABI_TAG
+# LLVM-SHORT-NEXT:       Description data (
+# LLVM-SHORT-NEXT:         0000: 2A000000 00000000 01000000
+# LLVM-SHORT-NEXT:       )
+# LLVM-SHORT-NEXT:     }
+# LLVM-SHORT-NEXT:   }
+# LLVM-SHORT-NEXT: ]
 
-// LLVM-NORMAL:      Notes [
-// LLVM-NORMAL-NEXT:   NoteSection {
-// LLVM-NORMAL-NEXT:     Name: .note.AARCH64-PAUTH-ABI-tag
-// LLVM-NORMAL-NEXT:     Offset: 0x40
-// LLVM-NORMAL-NEXT:     Size: 0x20
-// LLVM-NORMAL-NEXT:     Note {
-// LLVM-NORMAL-NEXT:       Owner: ARM
-// LLVM-NORMAL-NEXT:       Data size: 0x10
-// LLVM-NORMAL-NEXT:       Type: NT_ARM_TYPE_PAUTH_ABI_TAG
-// LLVM-NORMAL-NEXT:       Platform: 42
-// LLVM-NORMAL-NEXT:       Version: 1
-// LLVM-NORMAL-NEXT:     }
-// LLVM-NORMAL-NEXT:   }
-// LLVM-NORMAL-NEXT: ]
+# LLVM-NORMAL:      Notes [
+# LLVM-NORMAL-NEXT:   NoteSection {
+# LLVM-NORMAL-NEXT:     Name: .note.AARCH64-PAUTH-ABI-tag
+# LLVM-NORMAL-NEXT:     Offset: 0x40
+# LLVM-NORMAL-NEXT:     Size: 0x20
+# LLVM-NORMAL-NEXT:     Note {
+# LLVM-NORMAL-NEXT:       Owner: ARM
+# LLVM-NORMAL-NEXT:       Data size: 0x10
+# LLVM-NORMAL-NEXT:       Type: NT_ARM_TYPE_PAUTH_ABI_TAG
+# LLVM-NORMAL-NEXT:       Platform: 42
+# LLVM-NORMAL-NEXT:       Version: 1
+# LLVM-NORMAL-NEXT:     }
+# LLVM-NORMAL-NEXT:   }
+# LLVM-NORMAL-NEXT: ]
 
-// LLVM-LONG:      Notes [
-// LLVM-LONG-NEXT:   NoteSection {
-// LLVM-LONG-NEXT:     Name: .note.AARCH64-PAUTH-ABI-tag
-// LLVM-LONG-NEXT:     Offset: 0x40
-// LLVM-LONG-NEXT:     Size: 0x28
-// LLVM-LONG-NEXT:     Note {
-// LLVM-LONG-NEXT:       Owner: ARM
-// LLVM-LONG-NEXT:       Data size: 0x18
-// LLVM-LONG-NEXT:       Type: NT_ARM_TYPE_PAUTH_ABI_TAG
-// LLVM-LONG-NEXT:       Platform: 42
-// LLVM-LONG-NEXT:       Version: 1
-// LLVM-LONG-NEXT:       Additional info: EFCDAB8967452301
-// LLVM-LONG-NEXT:     }
-// LLVM-LONG-NEXT:   }
-// LLVM-LONG-NEXT: ]
+# LLVM-LONG:      Notes [
+# LLVM-LONG-NEXT:   NoteSection {
+# LLVM-LONG-NEXT:     Name: .note.AARCH64-PAUTH-ABI-tag
+# LLVM-LONG-NEXT:     Offset: 0x40
+# LLVM-LONG-NEXT:     Size: 0x28
+# LLVM-LONG-NEXT:     Note {
+# LLVM-LONG-NEXT:       Owner: ARM
+# LLVM-LONG-NEXT:       Data size: 0x18
+# LLVM-LONG-NEXT:       Type: NT_ARM_TYPE_PAUTH_ABI_TAG
+# LLVM-LONG-NEXT:       Platform: 42
+# LLVM-LONG-NEXT:       Version: 1
+# LLVM-LONG-NEXT:       Additional info: EFCDAB8967452301
+# LLVM-LONG-NEXT:     }
+# LLVM-LONG-NEXT:   }
+# LLVM-LONG-NEXT: ]
 
 #--- abi-tag.s
 
