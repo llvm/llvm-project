3e5a15832f845c8ce2ef161130d62da9410c4b98
Jakub Mazurkiewicz <mazkuba3@gmail.com>
2023-12-07 18:47:37 +0100

    `"test_comparisons.h"`: `constexpr` -> `TEST_CONSTEXPR`

--- libcxx/test/support/test_comparisons.h
+++ libcxx/test/support/test_comparisons.h
@@ -213,7 +213,7 @@ void AssertEqualityAreNoexcept()
 }
 
 template <class T, class U = T>
-constexpr void AssertEqualityReturnBool()
+TEST_CONSTEXPR void AssertEqualityReturnBool()
 {
     ASSERT_SAME_TYPE(decltype(std::declval<const T&>() == std::declval<const U&>()), bool);
     ASSERT_SAME_TYPE(decltype(std::declval<const T&>() != std::declval<const U&>()), bool);
