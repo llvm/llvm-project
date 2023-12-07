ea96d51e668ca9a8ae80d9d7136fb649d8b6ea5e
Weining Lu <luweining@loongson.cn>
2023-12-07 16:47:09 +0800

    Add a testcase showing the incorrect result when pcalau12i, addi.d, lu32i.d and lu52i.d are not in the same 4k-page

--- lld/test/ELF/loongarch-pc-aligned.s
+++ lld/test/ELF/loongarch-pc-aligned.s
@@ -284,6 +284,17 @@
 # EXTREME17-NEXT: lu32i.d   $t0, 0
 # EXTREME17-NEXT: lu52i.d   $t0, $t0, 0
 
+## A case that pcalau12i, lu32i.d and lu52i.d are in different pages.
+# RUN: ld.lld %t/extreme.o --section-start=.rodata=0x80000111 --section-start=.text=0xff8 -o %t/extreme18
+# RUN: llvm-objdump -d --no-show-raw-insn %t/extreme18 | FileCheck %s --check-prefix=EXTREME18
+# EXTREME18:      addi.d $t0, $zero, 273
+# EXTREME18-NEXT: pcalau12i $t1, -524288
+## FIXME: The relocation result is incorrect because lld uses seperate PCs of
+## each insn but not the PC of pcalau12i to calculate page delta. In this case
+## the correct result should be `lu32i.d $t0, 1`.
+# EXTREME18-NEXT: lu32i.d   $t0, 0
+# EXTREME18-NEXT: lu52i.d   $t0, $t0, 0
+
 #--- a.s
 .rodata
 x:
