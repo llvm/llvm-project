b74813c2a11396365e480db848f3643400be0cc3
Miheer Vaidya <m.vaidya@utah.edu>
2023-12-07 02:18:56 -0700

    🖋️ format change

--- mlir/lib/Dialect/SparseTensor/KokkosPipelines/SparseTensorKokkosPipelines.cpp
+++ mlir/lib/Dialect/SparseTensor/KokkosPipelines/SparseTensorKokkosPipelines.cpp
@@ -1,4 +1,4 @@
-//===- SparseTensorKokkosPipelines.cpp - Pipelines for sparse tensor code -------===//
+//===- SparseTensorKokkosPipelines.cpp - Pipelines for sparse tensor code -===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
