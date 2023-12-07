2a664f504045b560feb9ebb6b37132ebe0a863a6
W-50243 <wanghao636@huawei.com>
2023-12-07 19:42:29 +0800

    [Clang][Builtin] add __builtin_exit

---
+++ clang/test/CodeGen/builtin-exit-test.c
@@ -0,0 +1,9 @@
+//RUN: %clang_cc1 -no-opaque-pointers -emit-llvm -Wno-strict-prototypes -triple aarch64-target-linux-gnu %s -o - | FileCheck %s
+
+//CHECK: define dso_local void @test() #0 {
+//CHECK: call void @exit(i32 noundef 1)
+//CHECK: unreachable
+
+void test(void){
+	__builtin_exit(1);
+}
