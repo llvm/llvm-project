8953195dc1d6619fac5110328e547cce0b81be8b
Caroline Concatto <caroline.concatto@arm.com>
2023-12-08 09:58:59 +0000

    Remove test for neon_vector_type

--- clang/test/SemaCUDA/neon-attrs.cu
+++ clang/test/SemaCUDA/neon-attrs.cu
@@ -13,9 +13,6 @@
 // Regular C++ compilation on ARM with neon disabled.
 // RUN: %clang_cc1 -triple arm64-linux-gnu -target-feature -neon -x c++ -fsyntax-only -verify %s
 
-// quiet-no-diagnostics
-typedef __attribute__((neon_vector_type(4))) float float32x4_t;
-// expected-error@-1 {{'neon_vector_type' attribute is not supported on targets missing 'neon' or 'mve'}}
 typedef unsigned char poly8_t;
 typedef __attribute__((neon_polyvector_type(8))) poly8_t poly8x8_t;
 // expected-error@-1 {{'neon_polyvector_type' attribute is not supported on targets missing 'neon' or 'mve'}}
