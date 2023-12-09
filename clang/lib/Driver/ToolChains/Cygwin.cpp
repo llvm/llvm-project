51be811e6e40fbdb5db01aed7a719ae65a583756
徐持恒 Xu Chiheng <chiheng.xu@gmail.com>
2023-12-09 23:08:43 +0800

    1

--- clang/lib/Driver/ToolChains/Cygwin.cpp
+++ clang/lib/Driver/ToolChains/Cygwin.cpp
@@ -1,4 +1,4 @@
-//===--- MinGW.cpp - MinGWToolChain Implementation ------------------------===//
+//===--- Cygwin.cpp - CygwinToolChain Implementation ----------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
