51529be43e6f7d095c743353b34221bb162265ac
Kazu Hirata <kazu@google.com>
2023-12-09 14:28:45 -0800

    [ADT] Rename SmallString::{starts,ends}with to {starts,ends}_with (#74916)
    This patch renames {starts,ends}with to {starts,ends}_with for
    consistency with std::{string,string_view}::{starts,ends}_with in
    C++20.  Since there are only a handful of occurrences, this patch
    skips the deprecation phase and simply renames them.

--- clang/lib/Driver/ToolChains/Darwin.cpp
+++ clang/lib/Driver/ToolChains/Darwin.cpp
@@ -1281,7 +1281,7 @@ void MachO::AddLinkRuntimeLib(const ArgList &Args, ArgStringList &CmdArgs,
   // rpaths. This is currently true from this place, but we need to be
   // careful if this function is ever called before user's rpaths are emitted.
   if (Opts & RLO_AddRPath) {
-    assert(DarwinLibName.endswith(".dylib") && "must be a dynamic library");
+    assert(DarwinLibName.ends_with(".dylib") && "must be a dynamic library");
 
     // Add @executable_path to rpath to support having the dylib copied with
     // the executable.
