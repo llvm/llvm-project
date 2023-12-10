ed7ee83fa9b10a37295d050e1edc538b05a22504
Zingam <zingam@outlook.com>
2023-12-10 16:03:08 +0200

    Cleanup tests

--- libcxx/test/std/containers/views/views.span/span.elem/at.pass.cpp
+++ libcxx/test/std/containers/views/views.span/span.elem/at.pass.cpp
@@ -17,144 +17,35 @@
 #include <concepts>
 #include <span>
 #include <stdexcept>
+#include <utility>
 #include <vector>
 
 #include "test_macros.h"
 
-// template <typename Span>
-// constexpr bool testConstexprSpan(Span sp, std::size_t idx)
-// {
-//     LIBCPP_ASSERT(noexcept(sp[idx]));
-
-//     typename Span::reference r1 = sp[idx];
-//     typename Span::reference r2 = *(sp.data() + idx);
-
-//     return r1 == r2;
-// }
-
-// template <typename Span>
-// void testRuntimeSpan(Span sp, std::size_t idx)
-// {
-//     LIBCPP_ASSERT(noexcept(sp[idx]));
-
-//     typename Span::reference r1 = sp[idx];
-//     typename Span::reference r2 = *(sp.data() + idx);
-
-//     assert(r1 == r2);
-// }
-
-// struct A{};
-// constexpr int iArr1[] = { 0,  1,  2,  3,  4,  5,  6,  7,  8,  9};
-//           int iArr2[] = {10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
-
-// int main(int, char**)
-// {
-//     static_assert(testConstexprSpan(std::span<const int>(iArr1, 1), 0), "");
-
-//     static_assert(testConstexprSpan(std::span<const int>(iArr1, 2), 0), "");
-//     static_assert(testConstexprSpan(std::span<const int>(iArr1, 2), 1), "");
-
-//     static_assert(testConstexprSpan(std::span<const int>(iArr1, 3), 0), "");
-//     static_assert(testConstexprSpan(std::span<const int>(iArr1, 3), 1), "");
-//     static_assert(testConstexprSpan(std::span<const int>(iArr1, 3), 2), "");
-
-//     static_assert(testConstexprSpan(std::span<const int>(iArr1, 4), 0), "");
-//     static_assert(testConstexprSpan(std::span<const int>(iArr1, 4), 1), "");
-//     static_assert(testConstexprSpan(std::span<const int>(iArr1, 4), 2), "");
-//     static_assert(testConstexprSpan(std::span<const int>(iArr1, 4), 3), "");
-
-//     static_assert(testConstexprSpan(std::span<const int, 1>(iArr1, 1), 0), "");
-
-//     static_assert(testConstexprSpan(std::span<const int, 2>(iArr1, 2), 0), "");
-//     static_assert(testConstexprSpan(std::span<const int, 2>(iArr1, 2), 1), "");
-
-//     static_assert(testConstexprSpan(std::span<const int, 3>(iArr1, 3), 0), "");
-//     static_assert(testConstexprSpan(std::span<const int, 3>(iArr1, 3), 1), "");
-//     static_assert(testConstexprSpan(std::span<const int, 3>(iArr1, 3), 2), "");
-
-//     static_assert(testConstexprSpan(std::span<const int, 4>(iArr1, 4), 0), "");
-//     static_assert(testConstexprSpan(std::span<const int, 4>(iArr1, 4), 1), "");
-//     static_assert(testConstexprSpan(std::span<const int, 4>(iArr1, 4), 2), "");
-//     static_assert(testConstexprSpan(std::span<const int, 4>(iArr1, 4), 3), "");
-
-//     testRuntimeSpan(std::span<int>(iArr2, 1), 0);
-
-//     testRuntimeSpan(std::span<int>(iArr2, 2), 0);
-//     testRuntimeSpan(std::span<int>(iArr2, 2), 1);
-
-//     testRuntimeSpan(std::span<int>(iArr2, 3), 0);
-//     testRuntimeSpan(std::span<int>(iArr2, 3), 1);
-//     testRuntimeSpan(std::span<int>(iArr2, 3), 2);
-
-//     testRuntimeSpan(std::span<int>(iArr2, 4), 0);
-//     testRuntimeSpan(std::span<int>(iArr2, 4), 1);
-//     testRuntimeSpan(std::span<int>(iArr2, 4), 2);
-//     testRuntimeSpan(std::span<int>(iArr2, 4), 3);
-
-//     testRuntimeSpan(std::span<int, 1>(iArr2, 1), 0);
-
-//     testRuntimeSpan(std::span<int, 2>(iArr2, 2), 0);
-//     testRuntimeSpan(std::span<int, 2>(iArr2, 2), 1);
-
-//     testRuntimeSpan(std::span<int, 3>(iArr2, 3), 0);
-//     testRuntimeSpan(std::span<int, 3>(iArr2, 3), 1);
-//     testRuntimeSpan(std::span<int, 3>(iArr2, 3), 2);
-
-//     testRuntimeSpan(std::span<int, 4>(iArr2, 4), 0);
-//     testRuntimeSpan(std::span<int, 4>(iArr2, 4), 1);
-//     testRuntimeSpan(std::span<int, 4>(iArr2, 4), 2);
-//     testRuntimeSpan(std::span<int, 4>(iArr2, 4), 3);
-
-//     std::string s;
-//     testRuntimeSpan(std::span<std::string>   (&s, 1), 0);
-//     testRuntimeSpan(std::span<std::string, 1>(&s, 1), 0);
-
-//   return 0;
-// }
+constexpr void testSpan(auto span, int idx, int expectedValue) {
+  std::same_as<typename decltype(span)::reference> decltype(auto) elem = span.at(idx);
+  assert(elem == expectedValue);
+}
 
 constexpr bool test() {
-  //   {
-  //     typedef double T;
-  //     typedef std::array<T, 3> C;
-  //     C const c                      = {1, 2, 3.5};
-  //     typename C::const_reference r1 = c.at(0);
-  //     assert(r1 == 1);
-
-  //     typename C::const_reference r2 = c.at(2);
-  //     assert(r2 == 3.5);
-  //   }
-
-  const auto testSpan =
-      [](auto span, int idx, int expectedValue) {
-        {
-          std::same_as<decltype(span)::reference> elem = span.at(idx);
-          assert(elem == expectedValue);
-        }
-
-        {
-          std::same_as<decltype(span)::const_reference> elem = std::as_const(span).at(idx);
-          assert(elem == expectedValue);
-        }
-      }
-
   // With static extent
 
   std::array arr{0, 1, 2, 3, 4, 5, 9084};
-  std::span arrSpan{ar};
+  std::span arrSpan{arr};
 
   assert(std::dynamic_extent != arrSpan.extent);
 
   testSpan(arrSpan, 0, 0);
   testSpan(arrSpan, 1, 1);
-  testSpan(arrSpan, 5, 9084);
+  testSpan(arrSpan, 6, 9084);
 
   {
-    std::same_as<decltype(arrSpan)::reference> arrElem = arrSpan.at(1);
+    std::same_as<typename decltype(arrSpan)::reference> decltype(auto) arrElem = arrSpan.at(1);
     assert(arrElem == 1);
   }
 
   {
-    std::same_as<decltype(arrSpan)::const_reference> arrElem = std::as_const(arrSpan).at(1);
+    std::same_as<typename decltype(arrSpan)::reference> decltype(auto) arrElem = std::as_const(arrSpan).at(1);
     assert(arrElem == 1);
   }
 
@@ -162,17 +53,17 @@ constexpr bool test() {
 
   std::vector vec{0, 1, 2, 3, 4, 5};
   std::span vecSpan{vec};
-  
-  assert(std::dynamic_extent == vecSpan.extent)
+
+  assert(std::dynamic_extent == vecSpan.extent);
 
   {
-    std::same_as<decltype(vecSpan)::reference> vecElem = vecSpan.at(1);
-    assert(vec_elem == 1);
+    std::same_as<typename decltype(vecSpan)::reference> decltype(auto) vecElem = vecSpan.at(1);
+    assert(vecElem == 1);
   }
 
   {
-    std::same_as<decltype(vecSpan)::reference> vecElem = std::as_const(vecSpan).at(1);
-    assert(vec_elem == 1);
+    std::same_as<typename decltype(vecSpan)::reference> decltype(auto) vecElem = std::as_const(vecSpan).at(1);
+    assert(vecElem == 1);
   }
 
   return true;
