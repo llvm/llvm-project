ddf4c777d8444559a19c3d0571369882f0768766
XDeme <fernando.tagawa.gamail.com@gmail.com>
2023-12-10 00:25:47 -0300

    Change test back to verifyFormat

--- clang/unittests/Format/FormatTest.cpp
+++ clang/unittests/Format/FormatTest.cpp
@@ -26454,12 +26454,17 @@ TEST_F(FormatTest, BreakAfterAttributes) {
 
   Style.BreakBeforeBraces = FormatStyle::BS_Attach;
   Style.ReferenceAlignment = FormatStyle::ReferenceAlignmentStyle::RAS_Left;
-  verifyNoChange("struct Foo {\n"
+  verifyFormat("struct Foo {\n"
                  "  [[maybe_unused]]\n"
                  "  void operator+();\n"
                  "};\n"
                  "[[nodiscard]]\n"
                  "Foo& operator-(Foo&);",
+
+                 "struct Foo {\n"
+                 "[[maybe_unused]] void operator+();\n"
+                 "};\n"
+                 "[[nodiscard]] Foo &operator-(Foo&);",
                  Style);
 }
 
