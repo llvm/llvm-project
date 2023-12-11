f28e77952302ae1849c0ec7e750dafffb5a510fd
Will Hawkins <hawkinsw@obs.cr>
2023-12-11 09:40:04 -0500

    fixup! WIP: [libc++][ranges] Implement `ranges::stride_view`.
    Cleanup enable_borrowed_range tests.

--- libcxx/test/std/ranges/range.adaptors/range.stride.view/enable_borrowed_range.verify.cpp
+++ libcxx/test/std/ranges/range.adaptors/range.stride.view/enable_borrowed_range.verify.cpp
@@ -8,21 +8,21 @@
 
 // UNSUPPORTED: c++03, c++11, c++14, c++17, c++20
 
-#include <ranges>
-
-#include "test.h"
-
 // template<class T>
 // inline constexpr bool enable_borrowed_range<stride_view<T>> = false;
 
+#include <ranges>
+#include <vector>
+
 // The stride_view is not one of those range adaptors that (under any circumstances)
 // is enabled as a borrowable range by default. In other words, we will have to make
-// a positively test case explicity
+// a positive test case explicit.
 
 template <>
 inline constexpr bool
-    std::ranges::enable_borrowed_range<std::ranges::stride_view<InputView<cpp17_input_iterator<int*>>>> = true;
+    std::ranges::enable_borrowed_range<std::ranges::stride_view<std::views::all_t<std::vector<int>>>> = true;
 
-static_assert(std::ranges::borrowed_range<
-              std::ranges::stride_view<InputView<cpp17_input_iterator<int*>>>>);      // expected-no-diagnostics
-static_assert(!std::ranges::borrowed_range<InputView<bidirectional_iterator<int*>>>); // expected-no-diagnostics
+static_assert(!std::ranges::enable_borrowed_range<
+              std::ranges::stride_view<std::ranges::empty_view<int>>>); // expected-no-diagnostics
+static_assert(std::ranges::enable_borrowed_range<
+              std::ranges::stride_view<std::views::all_t<std::vector<int>>>>); // expected-no-diagnostics
