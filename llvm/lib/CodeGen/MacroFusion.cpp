f1fc128d7eaaf8f87bae63b96d88c1a4bcf288ad
wangpc <wangpengcheng.pp@bytedance.com>
2023-12-11 11:59:11 +0800

    Remove std::move

--- llvm/lib/CodeGen/MacroFusion.cpp
+++ llvm/lib/CodeGen/MacroFusion.cpp
@@ -214,13 +214,13 @@ bool MacroFusion::scheduleAdjacentImpl(ScheduleDAGInstrs &DAG, SUnit &AnchorSU) 
 std::unique_ptr<ScheduleDAGMutation>
 llvm::createMacroFusionDAGMutation(ArrayRef<MacroFusionPredTy> Predicates) {
   if (EnableMacroFusion)
-    return std::make_unique<MacroFusion>(std::move(Predicates), true);
+    return std::make_unique<MacroFusion>(Predicates, true);
   return nullptr;
 }
 
 std::unique_ptr<ScheduleDAGMutation> llvm::createBranchMacroFusionDAGMutation(
     ArrayRef<MacroFusionPredTy> Predicates) {
   if (EnableMacroFusion)
-    return std::make_unique<MacroFusion>(std::move(Predicates), false);
+    return std::make_unique<MacroFusion>(Predicates, false);
   return nullptr;
 }
