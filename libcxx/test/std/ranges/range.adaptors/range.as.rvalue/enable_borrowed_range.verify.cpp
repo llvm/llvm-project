8d748f0129bb242b7af5cf1b0ef51e6f696cb58e
Will Hawkins <hawkinsw@obs.cr>
2023-12-11 15:21:03 -0500

    fixup! [libc++][test] enable_borrowed_range compile -> verify for ranges::as_rvalue_view
    Only need one expected-no-diagnostics directive.

--- libcxx/test/std/ranges/range.adaptors/range.as.rvalue/enable_borrowed_range.verify.cpp
+++ libcxx/test/std/ranges/range.adaptors/range.as.rvalue/enable_borrowed_range.verify.cpp
@@ -7,13 +7,11 @@
 //===----------------------------------------------------------------------===//
 
 // UNSUPPORTED: c++03, c++11, c++14, c++17, c++20
+// expected-no-diagnostics
 
 #include <ranges>
 #include <vector>
 
-static_assert(std::ranges::enable_borrowed_range<
-              std::ranges::as_rvalue_view<std::ranges::empty_view<int>>>); // expected-no-diagnostics
-static_assert(std::ranges::enable_borrowed_range<
-              std::ranges::as_rvalue_view<std::views::all_t<std::vector<int>&>>>); // expected-no-diagnostics
-static_assert(!std::ranges::enable_borrowed_range<
-              std::ranges::as_rvalue_view<std::views::all_t<std::vector<int>>>>); // expected-no-diagnostics
+static_assert(std::ranges::enable_borrowed_range< std::ranges::as_rvalue_view<std::ranges::empty_view<int>>>);
+static_assert(std::ranges::enable_borrowed_range< std::ranges::as_rvalue_view<std::views::all_t<std::vector<int>&>>>);
+static_assert(!std::ranges::enable_borrowed_range< std::ranges::as_rvalue_view<std::views::all_t<std::vector<int>>>>);
