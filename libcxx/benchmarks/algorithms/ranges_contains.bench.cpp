221cd0a91d61e2ac7d19bf27d657b378af967767
Zijun Zhao <zijunzhao@google.com>
2023-12-11 23:15:54 -0800

    Update benchmark tests and identity.h

--- libcxx/benchmarks/algorithms/ranges_contains.bench.cpp
+++ libcxx/benchmarks/algorithms/ranges_contains.bench.cpp
@@ -13,37 +13,7 @@
 #include "test_iterators.h"
 #include <vector>
 
-static std::vector<char> comparable_data;
-
-template <class ElementT>
-class TriviallyComparable {
-  ElementT el_;
-
-public:
-  TEST_CONSTEXPR TriviallyComparable(ElementT el) : el_(el) {}
-  bool operator==(const TriviallyComparable&) const = default;
-};
-
-template <class IndexT>
-class Comparable {
-  IndexT index_;
-
-public:
-  Comparable(IndexT i)
-      : index_([&]() {
-          IndexT size = static_cast<IndexT>(comparable_data.size());
-          comparable_data.push_back(i);
-          return size;
-        }()) {}
-
-  bool operator==(const Comparable& other) const {
-    return comparable_data[other.index_] == comparable_data[index_];
-  }
-
-  friend bool operator==(const Comparable& lhs, long long rhs) { return comparable_data[lhs.index_] == rhs; }
-};
-
-static void bm_contains(benchmark::State& state) {
+static void bm_contains_char(benchmark::State& state) {
   std::vector<char> a(state.range(), 'a');
 
   for (auto _ : state) {
@@ -52,28 +22,28 @@ static void bm_contains(benchmark::State& state) {
     benchmark::DoNotOptimize(std::ranges::contains(a.begin(), a.end(), 'B'));
   }
 }
-BENCHMARK(bm_contains)->RangeMultiplier(16)->Range(16, 16 << 20);
+BENCHMARK(bm_contains_char)->RangeMultiplier(16)->Range(16, 16 << 20);
 
-static void bm_contains_with_trivially_comparable(benchmark::State& state) {
-  std::vector<TriviallyComparable<char>> a(state.range(), 'a');
-
-  for (auto _ : state) {
-    benchmark::DoNotOptimize(a);
-
-    benchmark::DoNotOptimize(std::ranges::contains(a.begin(), a.end(), 'B'));
-  }
-}
-BENCHMARK(bm_contains_with_trivially_comparable)->RangeMultiplier(16)->Range(16, 16 << 20);
-
-static void bm_contains_with_comparable(benchmark::State& state) {
-  std::vector<Comparable<char>> a(state.range(), 'a');
+static void bm_contains_int(benchmark::State& state) {
+  std::vector<int> a(state.range(), 1);
 
   for (auto _ : state) {
     benchmark::DoNotOptimize(a);
 
-    benchmark::DoNotOptimize(std::ranges::contains(a.begin(), a.end(), 'B'));
+    benchmark::DoNotOptimize(std::ranges::contains(a.begin(), a.end(), 2));
   }
 }
-BENCHMARK(bm_contains_with_comparable)->RangeMultiplier(16)->Range(16, 16 << 20);
+BENCHMARK(bm_contains_int)->RangeMultiplier(16)->Range(16, 16 << 20);
+
+static void bm_contains_bool(benchmark::State& state) {
+  std::vector<bool> a(state.range(), true);
+
+  for (auto _ : state) {
+    benchmark::DoNotOptimize(a);
+
+    benchmark::DoNotOptimize(std::ranges::contains(a.begin(), a.end(), false));
+  }
+}
+BENCHMARK(bm_contains_bool)->RangeMultiplier(16)->Range(16, 16 << 20);
 
 BENCHMARK_MAIN();
