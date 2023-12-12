41b9d45ce6a5edb18233dec0b780d302f1ab0180
Wei Wang <apollo.mobility@gmail.com>
2023-12-12 09:47:46 -0800

    [coroutines] Use DILocation from new storage for hoisted dbg.declare (#75104)
    Make the hoisted dbg.declare inherent the DILocation scope from the new
    storage.
    
    After hoisting, the dbg.declare is moved into the block that defines the
    new storage. This could create an inconsistency in the debug location
    scope hierarchy where the scope of hoisted dbg.declare (i.e.
    DILexicalBlock) is enclosed with the scope of the block (i.e.
    DISubprogram). This confuses LiveDebugValues pass to think that the
    hoisted dbg.declare is killed in that block and does not generate
    DBG_VALUE in other blocks. Debugger won't be able to track its value
    anymore.

--- llvm/lib/Transforms/Coroutines/CoroFrame.cpp
+++ llvm/lib/Transforms/Coroutines/CoroFrame.cpp
@@ -2891,9 +2891,11 @@ void coro::salvageDebugInfo(
   // dbg.declare does.
   if (isa<DbgDeclareInst>(DVI)) {
     std::optional<BasicBlock::iterator> InsertPt;
-    if (auto *I = dyn_cast<Instruction>(Storage))
+    if (auto *I = dyn_cast<Instruction>(Storage)) {
       InsertPt = I->getInsertionPointAfterDef();
-    else if (isa<Argument>(Storage))
+      if (I->getDebugLoc())
+        DVI->setDebugLoc(I->getDebugLoc());
+    } else if (isa<Argument>(Storage))
       InsertPt = F->getEntryBlock().begin();
     if (InsertPt)
       DVI->moveBefore(*(*InsertPt)->getParent(), *InsertPt);
