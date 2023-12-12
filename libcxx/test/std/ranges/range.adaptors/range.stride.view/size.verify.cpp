162be5fe354ca16456b96469c5623efad3e09c29
Will Hawkins <hawkinsw@obs.cr>
2023-12-11 21:11:09 -0500

    fixup! WIP: [libc++][ranges] Implement `ranges::stride_view`.
    Use only on expected-no-diagnostics per verify.

--- libcxx/test/std/ranges/range.adaptors/range.stride.view/size.verify.cpp
+++ libcxx/test/std/ranges/range.adaptors/range.stride.view/size.verify.cpp
@@ -7,6 +7,7 @@
 //===----------------------------------------------------------------------===//
 
 // UNSUPPORTED: c++03, c++11, c++14, c++17, c++20
+// expected-no-diagnostics
 
 // constexpr auto size()
 
@@ -17,28 +18,26 @@
 
 // There is no size member function on a stride view over a view that
 // is *not* a sized range
-static_assert(!std::ranges::sized_range<UnsizedBasicRange>);                           // expected-no-diagnostics
-static_assert(!std::ranges::sized_range<std::ranges::stride_view<UnsizedBasicRange>>); // expected-no-diagnosticss
+static_assert(!std::ranges::sized_range<UnsizedBasicRange>);
+static_assert(!std::ranges::sized_range<std::ranges::stride_view<UnsizedBasicRange>>);
 
 constexpr bool test() {
   {
     // Test with stride as exact multiple of number of elements in view strided over.
     constexpr auto iota_twelve = std::views::iota(0, 12);
-    static_assert(std::ranges::sized_range<decltype(iota_twelve)>); // expected-no-diagnostics
+    static_assert(std::ranges::sized_range<decltype(iota_twelve)>);
     constexpr auto stride_iota_twelve = std::views::stride(iota_twelve, 3);
-    static_assert(std::ranges::sized_range<decltype(stride_iota_twelve)>); // expected-no-diagnostics
-    static_assert(4 == stride_iota_twelve.size(),
-                  "Striding by 3 through a 12 member list has size 4."); // expected-no-diagnostics
+    static_assert(std::ranges::sized_range<decltype(stride_iota_twelve)>);
+    static_assert(4 == stride_iota_twelve.size(), "Striding by 3 through a 12 member list has size 4.");
   }
 
   {
     // Test with stride as inexact multiple of number of elements in view strided over.
     constexpr auto iota_twenty_two = std::views::iota(0, 22);
-    static_assert(std::ranges::sized_range<decltype(iota_twenty_two)>); // expected-no-diagnostics
+    static_assert(std::ranges::sized_range<decltype(iota_twenty_two)>);
     constexpr auto stride_iota_twenty_two = std::views::stride(iota_twenty_two, 3);
-    static_assert(std::ranges::sized_range<decltype(stride_iota_twenty_two)>); // expected-no-diagnostics
-    static_assert(8 == stride_iota_twenty_two.size(),
-                  "Striding by 3 through a 22 member list has size 8."); // expected-no-diagnostics
+    static_assert(std::ranges::sized_range<decltype(stride_iota_twenty_two)>);
+    static_assert(8 == stride_iota_twenty_two.size(), "Striding by 3 through a 22 member list has size 8.");
   }
   return true;
 }
