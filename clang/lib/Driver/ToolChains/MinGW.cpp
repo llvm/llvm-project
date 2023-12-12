b7383e3b85acae68c69c7db2091356cb27067ce1
徐持恒 Xu Chiheng <chiheng.xu@gmail.com>
2023-12-13 02:58:47 +0800

    1

--- clang/lib/Driver/ToolChains/MinGW.cpp
+++ clang/lib/Driver/ToolChains/MinGW.cpp
@@ -165,7 +165,13 @@ void tools::MinGW::Linker::ConstructJob(Compilation &C, const JobAction &JA,
       CmdArgs.push_back("DllMainCRTStartup");
     CmdArgs.push_back("--enable-auto-image-base");
   }
-  CmdArgs.push_back("--dynamicbase");
+  switch (TC.getArch()) {
+  case llvm::Triple::x86_64:
+    CmdArgs.push_back("--dynamicbase");
+    break;
+  case llvm::Triple::aarch64:
+    break;
+  }
 
   if (Args.hasArg(options::OPT_Z_Xlinker__no_demangle))
     CmdArgs.push_back("--no-demangle");
