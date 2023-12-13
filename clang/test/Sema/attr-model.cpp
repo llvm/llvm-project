75cc3dbafc01be6e1a1782c06652117360c36a3e
WANG Rui <wangrui@loongson.cn>
2023-12-13 16:33:50 +0800

    Address xen0n's comments

--- clang/test/Sema/attr-model.cpp
+++ clang/test/Sema/attr-model.cpp
@@ -9,24 +9,24 @@
 #error "Should support model attribute"
 #endif
 
-int a __attribute((model("tiny")));    // expected-error {{code_model 'tiny' is not supported on this target}}
-int b __attribute((model("small")));   // expected-error {{code_model 'small' is not supported on this target}}
-int c __attribute((model("normal")));  // aarch64-error {{code_model 'normal' is not supported on this target}} \
-                                       // mips64-error {{code_model 'normal' is not supported on this target}} \
-                                       // powerpc64-error {{code_model 'normal' is not supported on this target}} \
-                                       // riscv64-error {{code_model 'normal' is not supported on this target}} \
-                                       // x86_64-error {{code_model 'normal' is not supported on this target}}
-int d __attribute((model("kernel")));  // expected-error {{code_model 'kernel' is not supported on this target}}
-int e __attribute((model("medium")));  // aarch64-error {{code_model 'medium' is not supported on this target}} \
-                                       // mips64-error {{code_model 'medium' is not supported on this target}} \
-                                       // powerpc64-error {{code_model 'medium' is not supported on this target}} \
-                                       // riscv64-error {{code_model 'medium' is not supported on this target}} \
-                                       // x86_64-error {{code_model 'medium' is not supported on this target}}
-int f __attribute((model("large")));   // expected-error {{code_model 'large' is not supported on this target}}
-int g __attribute((model("extreme"))); // aarch64-error {{code_model 'extreme' is not supported on this target}} \
-                                       // mips64-error {{code_model 'extreme' is not supported on this target}} \
-                                       // powerpc64-error {{code_model 'extreme' is not supported on this target}} \
-                                       // riscv64-error {{code_model 'extreme' is not supported on this target}} \
-                                       // x86_64-error {{code_model 'extreme' is not supported on this target}}
+int a __attribute((model("tiny")));    // expected-error {{code model 'tiny' is not supported on this target}}
+int b __attribute((model("small")));   // expected-error {{code model 'small' is not supported on this target}}
+int c __attribute((model("normal")));  // aarch64-error {{code model 'normal' is not supported on this target}} \
+                                       // mips64-error {{code model 'normal' is not supported on this target}} \
+                                       // powerpc64-error {{code model 'normal' is not supported on this target}} \
+                                       // riscv64-error {{code model 'normal' is not supported on this target}} \
+                                       // x86_64-error {{code model 'normal' is not supported on this target}}
+int d __attribute((model("kernel")));  // expected-error {{code model 'kernel' is not supported on this target}}
+int e __attribute((model("medium")));  // aarch64-error {{code model 'medium' is not supported on this target}} \
+                                       // mips64-error {{code model 'medium' is not supported on this target}} \
+                                       // powerpc64-error {{code model 'medium' is not supported on this target}} \
+                                       // riscv64-error {{code model 'medium' is not supported on this target}} \
+                                       // x86_64-error {{code model 'medium' is not supported on this target}}
+int f __attribute((model("large")));   // expected-error {{code model 'large' is not supported on this target}}
+int g __attribute((model("extreme"))); // aarch64-error {{code model 'extreme' is not supported on this target}} \
+                                       // mips64-error {{code model 'extreme' is not supported on this target}} \
+                                       // powerpc64-error {{code model 'extreme' is not supported on this target}} \
+                                       // riscv64-error {{code model 'extreme' is not supported on this target}} \
+                                       // x86_64-error {{code model 'extreme' is not supported on this target}}
 void __attribute((model("extreme"))) h() {} // expected-error {{'model' attribute only applies to non-TLS global variables}}
 thread_local int i __attribute((model("extreme"))); // expected-error {{'model' attribute only applies to non-TLS global variables}}
