e0af079cdba52e845211ef9577dc4c2d96fc835b
Tom Stellard <tstellar@redhat.com>
2023-12-13 20:00:31 +0000

    Fix test formatting

--- clang/test/Driver/gcc-prefix.cpp
+++ clang/test/Driver/gcc-prefix.cpp
@@ -1,8 +1,8 @@
 // UNSUPPORTED: system-windows
-//
+
 // RUN: %clang --target=x86_64-redhat-linux-gnu \
 // RUN: --sysroot=%S/Inputs/fedora_39_tree --gcc-triple=x86_64-redhat-linux -v 2>&1 | \
 // RUN: FileCheck %s
-//
+
 // CHECK: {{^}}Selected GCC installation:
 // CHECK: fedora_39_tree/usr/lib/gcc/x86_64-redhat-linux/13{{$}}
