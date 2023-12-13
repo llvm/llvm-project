a8fd5df6ef14f7d69aa3320bd77b2b70c86022f7
Oliver Stannard <oliver.stannard@arm.com>
2023-12-13 16:24:54 +0000

    Change tests to use split-file

--- llvm/test/tools/llvm-objdump/visualize-jumps-arm.s
+++ llvm/test/tools/llvm-objdump/visualize-jumps-arm.s
@@ -1,11 +1,14 @@
-// RUN: llvm-mc < %s -triple armv8a -filetype=obj | \
+// RUN: rm -rf %t && split-file %s %t && cd %t
+
+// RUN: llvm-mc < input.s -triple armv8a -filetype=obj | \
 // RUN:   llvm-objdump --triple armv8a -d --visualize-jumps=unicode - | \
-// RUN:   diff - %p/Inputs/visualize-jumps-arm-unicode.txt
+// RUN:   diff - expected-unicode.txt
 
-// RUN: llvm-mc < %s -triple armv8a -filetype=obj | \
+// RUN: llvm-mc < input.s -triple armv8a -filetype=obj | \
 // RUN:   llvm-objdump --triple armv8a -d --visualize-jumps=ascii - | \
-// RUN:   diff - %p/Inputs/visualize-jumps-arm-ascii.txt
+// RUN:   diff - expected-ascii.txt
 
+//--- input.s
 test_func:
   // Relocated instructions don't get control-flow edges.
   bl extern_func
@@ -52,3 +55,60 @@ test_func:
   b .Llabel9
 .Llabel9:
   nop
+
+//--- expected-unicode.txt
+
+<stdin>:	file format elf32-littlearm
+
+Disassembly of section .text:
+
+00000000 <test_func>:
+       0:           ebfffffe   	bl	0x0 <test_func>         @ imm = #-0x8
+       4:           eafffffe   	b	0x4 <test_func+0x4>     @ imm = #-0x8
+       8:       ╭── eaffffff   	b	0xc <test_func+0xc>     @ imm = #-0x4
+       c:       ├─> e320f000   	nop
+      10:       ╰── eafffffd   	b	0xc <test_func+0xc>     @ imm = #-0xc
+      14:           eafffffe   	b	0x14 <test_func+0x14>   @ imm = #-0x8
+      18:       ╭── 0affffff   	beq	0x1c <test_func+0x1c>   @ imm = #-0x4
+      1c:       ╰─> e320f000   	nop
+      20:   ╭────── ea000003   	b	0x34 <test_func+0x34>   @ imm = #0xc
+      24:   │ ╭──── ea000001   	b	0x30 <test_func+0x30>   @ imm = #0x4
+      28:   │ │ ╭── eaffffff   	b	0x2c <test_func+0x2c>   @ imm = #-0x4
+      2c:   │ │ ╰─> e320f000   	nop
+      30:   │ ╰───> e320f000   	nop
+      34:   ╰─────> e320f000   	nop
+      38:       ╭── ea000000   	b	0x40 <test_func+0x40>   @ imm = #0x0
+      3c:     ╭─│── ea000000   	b	0x44 <test_func+0x44>   @ imm = #0x0
+      40:     │ ╰─> e320f000   	nop
+      44:     ╰───> e320f000   	nop
+      48:     ╭──── eaffffff   	b	0x4c <test_func+0x4c>   @ imm = #-0x4
+      4c:     ╰─│─> eaffffff   	b	0x50 <test_func+0x50>   @ imm = #-0x4
+      50:       ╰─> e320f000   	nop
+//--- expected-ascii.txt
+
+<stdin>:	file format elf32-littlearm
+
+Disassembly of section .text:
+
+00000000 <test_func>:
+       0:           ebfffffe   	bl	0x0 <test_func>         @ imm = #-0x8
+       4:           eafffffe   	b	0x4 <test_func+0x4>     @ imm = #-0x8
+       8:       /-- eaffffff   	b	0xc <test_func+0xc>     @ imm = #-0x4
+       c:       +-> e320f000   	nop
+      10:       \-- eafffffd   	b	0xc <test_func+0xc>     @ imm = #-0xc
+      14:           eafffffe   	b	0x14 <test_func+0x14>   @ imm = #-0x8
+      18:       /-- 0affffff   	beq	0x1c <test_func+0x1c>   @ imm = #-0x4
+      1c:       \-> e320f000   	nop
+      20:   /------ ea000003   	b	0x34 <test_func+0x34>   @ imm = #0xc
+      24:   | /---- ea000001   	b	0x30 <test_func+0x30>   @ imm = #0x4
+      28:   | | /-- eaffffff   	b	0x2c <test_func+0x2c>   @ imm = #-0x4
+      2c:   | | \-> e320f000   	nop
+      30:   | \---> e320f000   	nop
+      34:   \-----> e320f000   	nop
+      38:       /-- ea000000   	b	0x40 <test_func+0x40>   @ imm = #0x0
+      3c:     /-|-- ea000000   	b	0x44 <test_func+0x44>   @ imm = #0x0
+      40:     | \-> e320f000   	nop
+      44:     \---> e320f000   	nop
+      48:     /---- eaffffff   	b	0x4c <test_func+0x4c>   @ imm = #-0x4
+      4c:     \-|-> eaffffff   	b	0x50 <test_func+0x50>   @ imm = #-0x4
+      50:       \-> e320f000   	nop
