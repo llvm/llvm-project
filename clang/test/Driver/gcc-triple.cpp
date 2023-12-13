8a6d5089fc98899c4234c72c628063872273e1e8
Tom Stellard <tstellar@redhat.com>
2023-12-13 20:32:53 +0000

    Add negative test

--- clang/test/Driver/gcc-triple.cpp
+++ clang/test/Driver/gcc-triple.cpp
@@ -2,7 +2,13 @@
 
 // RUN: %clang --target=x86_64-redhat-linux-gnu \
 // RUN:   --sysroot=%S/Inputs/fedora_39_tree --gcc-triple=x86_64-redhat-linux -v 2>&1 | \
-// RUN: FileCheck %s
+// RUN:   FileCheck %s --check-prefix=TRIPLE_EXISTS
 
-// CHECK: {{^}}Selected GCC installation:
-// CHECK: fedora_39_tree/usr/lib/gcc/x86_64-redhat-linux/13{{$}}
+// TRIPLE_EXISTS: {{^}}Selected GCC installation:
+// TRIPLE_EXISTS: fedora_39_tree/usr/lib/gcc/x86_64-redhat-linux/13{{$}}
+
+// RUN: %clang --target=x86_64-redhat-linux-gnu \
+// RUN:   --sysroot=%S/Inputs/fedora_39_tree --gcc-triple=x86_64-gentoo-linux -v 2>&1 | \
+// RUN:   FileCheck %s --check-prefix=TRIPLE_DOESNT_EXIST
+//
+// TRIPLE_DOESNT_EXIST-NOT: x86_64-gentoo-linux
