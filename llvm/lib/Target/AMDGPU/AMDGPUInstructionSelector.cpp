eeebfe90f6a77f7a54a70087e3dcbff883e2b796
Petar Avramovic <Petar.Avramovic@amd.com>
2023-12-13 12:38:04 +0100

    AMDGPU/GlobalISelDivergenceLowering: select divergent i1 phis
    Implement PhiLoweringHelper for GlobalISel in DivergenceLoweringHelper.
    Use machine uniformity analysis to find divergent i1 phis and select them
    as lane mask phis in same way SILowerI1Copies select VReg_1 phis.
    Note that divergent i1 phis include phis created by LCSSA and all cases
    of uses outside of cycle are actually covered by "lowering LCSSA phis".
    GlobalISel lane masks are registers with sgpr register class and S1 LLT.
    
    TODO: General goal is that instructions created in this pass are fully
    instruction-selected so that selection of lane mask phis is not split
    across multiple passes.
    
    patch 3 from: https://github.com/llvm/llvm-project/pull/73337

--- llvm/lib/Target/AMDGPU/AMDGPUInstructionSelector.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPUInstructionSelector.cpp
@@ -210,6 +210,7 @@ bool AMDGPUInstructionSelector::selectCOPY(MachineInstr &I) const {
 bool AMDGPUInstructionSelector::selectPHI(MachineInstr &I) const {
   const Register DefReg = I.getOperand(0).getReg();
   const LLT DefTy = MRI->getType(DefReg);
+
   if (DefTy == LLT::scalar(1)) {
     if (!AllowRiskySelect) {
       LLVM_DEBUG(dbgs() << "Skipping risky boolean phi\n");
@@ -3552,8 +3553,6 @@ bool AMDGPUInstructionSelector::selectStackRestore(MachineInstr &MI) const {
 }
 
 bool AMDGPUInstructionSelector::select(MachineInstr &I) {
-  if (I.isPHI())
-    return selectPHI(I);
 
   if (!I.isPreISelOpcode()) {
     if (I.isCopy())
@@ -3696,6 +3695,8 @@ bool AMDGPUInstructionSelector::select(MachineInstr &I) {
     return selectWaveAddress(I);
   case AMDGPU::G_STACKRESTORE:
     return selectStackRestore(I);
+  case AMDGPU::G_PHI:
+    return selectPHI(I);
   default:
     return selectImpl(I, *CoverageInfo);
   }
