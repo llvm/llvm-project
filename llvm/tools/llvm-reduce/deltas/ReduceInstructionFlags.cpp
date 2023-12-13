ee80b445d2583755fe61bdeac49b7805d8b6fed7
Matt Arsenault <Matthew.Arsenault@amd.com>
2023-12-13 10:55:58 +0700

    llvm-reduce: Handle nneg flag

--- llvm/tools/llvm-reduce/deltas/ReduceInstructionFlags.cpp
+++ llvm/tools/llvm-reduce/deltas/ReduceInstructionFlags.cpp
@@ -30,6 +30,9 @@ static void reduceFlagsInModule(Oracle &O, ReducerWorkItem &WorkItem) {
       } else if (auto *PE = dyn_cast<PossiblyExactOperator>(&I)) {
         if (PE->isExact() && !O.shouldKeep())
           I.setIsExact(false);
+      } else if (auto *NNI = dyn_cast<PossiblyNonNegInst>(&I)) {
+        if (NNI->hasNonNeg() && !O.shouldKeep())
+          NNI->setNonNeg(false);
       } else if (auto *GEP = dyn_cast<GetElementPtrInst>(&I)) {
         if (GEP->isInBounds() && !O.shouldKeep())
           GEP->setIsInBounds(false);
