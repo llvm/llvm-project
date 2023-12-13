471c4ce281ae5319ac32cb4188d4ee0d45b42f5d
Tom Stellard <tstellar@redhat.com>
2023-12-13 20:01:35 +0000

    More formatting fixes for tests

--- clang/test/Driver/gcc-triple.cpp
+++ clang/test/Driver/gcc-triple.cpp
@@ -1,7 +1,7 @@
 // UNSUPPORTED: system-windows
 
 // RUN: %clang --target=x86_64-redhat-linux-gnu \
-// RUN: --sysroot=%S/Inputs/fedora_39_tree --gcc-triple=x86_64-redhat-linux -v 2>&1 | \
+// RUN:   --sysroot=%S/Inputs/fedora_39_tree --gcc-triple=x86_64-redhat-linux -v 2>&1 | \
 // RUN: FileCheck %s
 
 // CHECK: {{^}}Selected GCC installation:
