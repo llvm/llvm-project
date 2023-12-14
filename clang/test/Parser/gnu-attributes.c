e9d2bcb0d6fc4ddf0acde5deed2096c6a515ab3c
Sirui Mu <msrlancern@gmail.com>
2023-12-14 01:55:09 +0000

    [clang] Move no_stack_protector parsing tests to test/Sema

--- clang/test/Parser/gnu-attributes.c
+++
@@ -1,4 +0,0 @@
-// RUN: %clang_cc1 -fsyntax-only -verify -std=c23 %s
-// expected-no-diagnostics
-
-[[gnu::no_stack_protector]] void test1(int i) {} // ok
--- clang/test/Parser/gnu-attributes.cpp
+++
@@ -1,4 +0,0 @@
-// RUN: %clang_cc1 -fcxx-exceptions -fexceptions -fsyntax-only -verify -std=c++11 %s
-// expected-no-diagnostics
-
-[[gnu::no_stack_protector]] void test1(int i) {} // ok
