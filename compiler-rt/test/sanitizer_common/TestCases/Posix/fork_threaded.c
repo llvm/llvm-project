3cc39f7d22965edfa79aacc959549d3b9b3a5fa6
Vitaly Buka <vitalybuka@google.com>
2023-12-14 15:49:39 -0800

    [test][sanitizer] Add pthread_join in child
    Call dangerous code on main thread as well.
    And use _exit() to avoid any issues during regular process cleanup.
    
    And disable TSAN as it does not lock allocator yet.

--- compiler-rt/test/sanitizer_common/TestCases/Posix/fork_threaded.c
+++ compiler-rt/test/sanitizer_common/TestCases/Posix/fork_threaded.c
@@ -6,6 +6,9 @@
 // FIXME: It probably hangs on this platform.
 // UNSUPPORTED: ppc
 
+// FIXME: TSAN does not lock allocator.
+// UNSUPPORTED: tsan
+
 // Forking in multithread environment is unsupported. However we already have
 // some workarounds, and will add more, so this is the test.
 // The test try to check two things:
@@ -35,7 +38,8 @@ void ShouldNotDeadlock() {
   __lsan_disable();
   void *volatile p = malloc(10);
   __lsan_do_recoverable_leak_check();
-  free(p);
+  // Allocator still in broken state, `free` may report errors.
+  // free(p);
   __lsan_enable();
 }
 
@@ -80,14 +84,18 @@ int main(void) {
     perror("fork");
     return -1;
   case 0:
+    ShouldNotDeadlock();
     while (pthread_create(&thread_id, 0, &inchild, 0) != 0) {
     }
+    pthread_join(thread_id, NULL);
+    _exit(0);
     break;
   default: {
     int status;
     pid_t child = waitpid(pid, &status, /*options=*/0);
     assert(pid == child);
-    assert(WIFEXITED(status) && WEXITSTATUS(status) == 0);
+    assert(WIFEXITED(status));
+    assert(WEXITSTATUS(status) == 0);
     break;
   }
   }
