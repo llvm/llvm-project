ce1faec3ad15780f6336ac83047aac6eacd16772
Fangrui Song <i@maskray.me>
2023-12-14 19:40:09 -0800

    [Serialization] Fix -Wpessimizing-move after 9cdb825a4f1bf9e75829d03879620c6144d0b7bc

--- clang/lib/Serialization/ASTWriterDecl.cpp
+++ clang/lib/Serialization/ASTWriterDecl.cpp
@@ -2131,7 +2131,7 @@ getFunctionDeclAbbrev(serialization::DeclCode Code) {
   //
   //  Add an AbbrevOp for 'size then elements' and use it here.
   Abv->Add(BitCodeAbbrevOp(BitCodeAbbrevOp::Array));
-  return std::move(Abv);
+  return Abv;
 }
 
 template <FunctionDecl::TemplatedKind Kind>
