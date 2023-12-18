a9a1cbc1ac42b2eb66dac8a60aa9d8f54df291c3
Felipe de Azevedo Piovezan <fpiovezan@apple.com>
2023-12-18 11:26:41 -0300

    [lldb] Add actionable feedback when overwriting a command fails
    If adding a user commands fails because a command with the same name already
    exists, we only say that "force replace is not set" without telling the user
    _how_ to set it. There are two ways to do so; this commit changes the error
    message to mention both.

--- lldb/source/Interpreter/CommandInterpreter.cpp
+++ lldb/source/Interpreter/CommandInterpreter.cpp
@@ -1160,7 +1160,9 @@ Status CommandInterpreter::AddUserCommand(llvm::StringRef name,
 
   if (UserCommandExists(name)) {
     if (!can_replace) {
-      result.SetErrorString("user command exists and force replace not set");
+      result.SetErrorString(
+          "user command exists and force replace not set by --overwrite or "
+          "'settings set interpreter.require-overwrite false'");
       return result;
     }
     if (cmd_sp->IsMultiwordObject()) {
