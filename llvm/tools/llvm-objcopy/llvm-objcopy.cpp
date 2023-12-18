f73694733f7e56495131b78bbd9f2a6e0b9595c8
quic-areg <154252969+quic-areg@users.noreply.github.com>
2023-12-18 14:24:24 -0800

    [llvm-objcopy] Support SREC output format
    Adds a new output target "srec" to write SREC files from ELF inputs.
    
    https://en.wikipedia.org/wiki/SREC_(file_format)

--- llvm/tools/llvm-objcopy/llvm-objcopy.cpp
+++ llvm/tools/llvm-objcopy/llvm-objcopy.cpp
@@ -122,6 +122,7 @@ static Error executeObjcopyOnRawBinary(ConfigManager &ConfigMgr,
   case FileFormat::Binary:
   case FileFormat::IHex:
   case FileFormat::Unspecified:
+  case FileFormat::SREC:
     Expected<const ELFConfig &> ELFConfig = ConfigMgr.getELFConfig();
     if (!ELFConfig)
       return ELFConfig.takeError();
