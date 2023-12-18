35d07590efe6cedf908493d3083907e30dddc975
Nick Desaulniers <ndesaulniers@google.com>
2023-12-18 14:29:19 -0800

    add death test

---
+++ libc/test/src/compiler/stack_chk_guard_test.cpp
@@ -0,0 +1,16 @@
+//===-- Unittests for assert ----------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "llvm-libc-macros/signal-macros.h"
+#include "src/compiler/__stack_chk_fail.h"
+#include "test/UnitTest/Test.h"
+
+TEST(LlvmLibcStackChkFail, Death) {
+  EXPECT_DEATH([] { LIBC_NAMESPACE::__stack_chk_fail(); },
+               WITH_SIGNAL(SIGABRT));
+}
