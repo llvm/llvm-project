72d1edc7ac421d7b8e5c8cbf7799d19ebd353770
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2023-12-19 17:29:23 -0300

    [CIR][NFC] Fix some copy n paste and update comments

--- clang/lib/CIR/Dialect/Transforms/IdiomRecognizer.cpp
+++ clang/lib/CIR/Dialect/Transforms/IdiomRecognizer.cpp
@@ -1,4 +1,4 @@
-//===- IdiomRecognizer.cpp - pareparation work for LLVM lowering ----------===//
+//===- IdiomRecognizer.cpp - Recognize and raise C/C++ library calls ------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
