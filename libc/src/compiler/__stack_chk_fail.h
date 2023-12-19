cd80da5e765b4d9703cde9b4147428f16cbbc64a
Nick Desaulniers <nickdesaulniers@users.noreply.github.com>
2023-12-19 11:59:42 -0800

    [libc] __stack_chk_fail post submit test failures (#75962)
    Use a size smaller than the smallest supported page size so that we
    don't
    clobber over any guard pages, which may result in a segfault before
    __stack_chk_fail can be called.
    
    Also, move __stack_chk_fail outside of our namespace.

--- libc/src/compiler/__stack_chk_fail.h
+++ libc/src/compiler/__stack_chk_fail.h
@@ -9,10 +9,12 @@
 #ifndef LLVM_LIBC_SRC_COMPILER___STACK_CHK_FAIL_H
 #define LLVM_LIBC_SRC_COMPILER___STACK_CHK_FAIL_H
 
-namespace LIBC_NAMESPACE {
-
+// The compiler will emit calls implicitly to a non-namespaced version.
+// TODO: can we additionally provide a namespaced alias so that tests can
+// explicitly call the namespaced variant rather than the non-namespaced
+// definition?
+extern "C" {
 [[noreturn]] void __stack_chk_fail();
-
-} // namespace LIBC_NAMESPACE
+} // extern "C"
 
 #endif // LLVM_LIBC_SRC_COMPILER___STACK_CHK_FAIL_H
