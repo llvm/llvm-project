3fb24a8041df2858f40ed642e93a4080512e3c48
Nick Desaulniers <ndesaulniers@google.com>
2023-12-19 11:58:42 -0800

    add todo/comment

--- libc/src/compiler/__stack_chk_fail.h
+++ libc/src/compiler/__stack_chk_fail.h
@@ -9,6 +9,10 @@
 #ifndef LLVM_LIBC_SRC_COMPILER___STACK_CHK_FAIL_H
 #define LLVM_LIBC_SRC_COMPILER___STACK_CHK_FAIL_H
 
+// The compiler will emit calls implicitly to a non-namespaced version.
+// TODO: can we additionally provide a namespaced alias so that tests can
+// explicitly call the namespaced variant rather than the non-namespaced
+// definition?
 extern "C" {
 [[noreturn]] void __stack_chk_fail();
 } // extern "C"
