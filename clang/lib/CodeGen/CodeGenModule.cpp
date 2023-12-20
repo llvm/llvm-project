02bf2307c23ee7ffaee88e4b698c0d1b8d3b725e
Dimitry Andric <dimitry@andric.com>
2023-12-20 20:07:22 +0100

    Revert "[clang] Add getClangVendor() and use it in CodeGenModule.cpp (#75935)"
    This reverts commit e25a186f8c2261f07872802efe963eb8735b9dc6, due to an
    incorrectly chosen commit message.

--- clang/lib/CodeGen/CodeGenModule.cpp
+++ clang/lib/CodeGen/CodeGenModule.cpp
@@ -995,7 +995,12 @@ void CodeGenModule::Release() {
                               uint32_t(CLANG_VERSION_MINOR));
     getModule().addModuleFlag(llvm::Module::Warning, "zos_product_patchlevel",
                               uint32_t(CLANG_VERSION_PATCHLEVEL));
-    std::string ProductId = getClangVendor() + "clang";
+    std::string ProductId;
+#ifdef CLANG_VENDOR
+    ProductId = #CLANG_VENDOR;
+#else
+    ProductId = "clang";
+#endif
     getModule().addModuleFlag(llvm::Module::Error, "zos_product_id",
                               llvm::MDString::get(VMContext, ProductId));
 
