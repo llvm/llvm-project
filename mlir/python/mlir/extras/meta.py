7d8f5798b5457a28ad0b7159e1ee34915cb44e1c
max <maksim.levental@gmail.com>
2023-12-20 17:20:14 -0600

    add comment describing region_op

--- mlir/python/mlir/extras/meta.py
+++ mlir/python/mlir/extras/meta.py
@@ -11,7 +11,7 @@ from ..ir import Type, InsertionPoint
 
 def op_region_builder(op, op_region, terminator=None):
     def builder_wrapper(body_builder):
-        # add a block with block args having types ...
+        # Add a block with block args having types determined by type hints on the wrapped function.
         if len(op_region.blocks) == 0:
             sig = inspect.signature(body_builder)
             types = [p.annotation for p in sig.parameters.values()]
@@ -43,6 +43,30 @@ def op_region_builder(op, op_region, terminator=None):
 
 
 def region_op(op_constructor, terminator=None):
+    """Decorator to define an MLIR Op specified as a python function.
+
+    Requires that an `mlir.ir.InsertionPoint` and `mlir.ir.Location` are
+    active for the current thread (i.e. established in a `with` block).
+
+    Supports "naked" usage i.e., no parens if no args need to be passed to the Op constructor.
+
+    When applied as a decorator to a Python function, an entry block will
+    be constructed for the Op with types as specified **as type hints on the args of the function**.
+    The block arguments will be passed positionally to the Python function.
+
+    If a terminator is specified then the return from the decorated function will be passed
+    to the terminator as the last statement in the entry block. Note, the API for the terminator
+    is a (possibly empty) list; terminator accepting single values should be wrapped in a
+    `lambda args: term(args[0])`
+
+    The identifier (name) of the function will become:
+    1. A single value result if the Op returns a single value;
+    2. An OpResultList (as a list) if the Op returns multiple values;
+    3. The Operation if the Op returns no results.
+
+    See examples in tensor.py and transform.extras.
+    """
+
     def op_decorator(*args, **kwargs):
         op = op_constructor(*args, **kwargs)
         op_region = op.regions[0]
