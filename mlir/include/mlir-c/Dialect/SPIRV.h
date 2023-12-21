5502eebd69eb10949853ba455e7228f92c525ac2
jungpark-mlir <jungwook@jungwook-22.04>
2023-12-21 09:04:09 +0000

    style fix.

--- mlir/include/mlir-c/Dialect/SPIRV.h
+++ mlir/include/mlir-c/Dialect/SPIRV.h
@@ -1,11 +1,11 @@
-//===-- mlir-c/Dialect/SPIRV.h - C API for SPIRV dialect -------*- C -*-===//
+//===-- mlir-c/Dialect/SPIRV.h - C API for SPIRV dialect ----------*- C -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM
 // Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
-//===---------------------------------------------------------------------===//
+//===----------------------------------------------------------------------===//
 
 #ifndef MLIR_C_DIALECT_SPIRV_H
 #define MLIR_C_DIALECT_SPIRV_H
--- mlir/lib/CAPI/Dialect/SPIRV.cpp
+++ mlir/lib/CAPI/Dialect/SPIRV.cpp
@@ -1,4 +1,4 @@
-//===- SPIRV.cpp - C Interface for SPIRV dialect ------------------------===//
+//===- SPIRV.cpp - C Interface for SPIRV dialect --------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
