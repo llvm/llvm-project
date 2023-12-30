55cf94d98c38cd2b5cd8fbf76e5fd83b5a47f4af
Aiden Grossman <agrossman154@yahoo.com>
2023-12-29 16:14:15 -0800

    [AsmPrinter] Remove mbb-profile-dump flag
    Now that the work embedding PGO information in SHT_LLVM_BB_ADDR_MAP ELF
    sections has landed, there is no longer a need to keep around the
    mbb-profile-dump flag.

--- llvm/include/llvm/CodeGen/AsmPrinter.h
+++ llvm/include/llvm/CodeGen/AsmPrinter.h
@@ -236,10 +236,6 @@ private:
   /// split stack prologue.
   bool HasNoSplitStack = false;
 
-  /// Raw FDOstream for outputting machine basic block frequncies if the
-  /// --mbb-profile-dump flag is set for downstream cost modelling applications
-  std::unique_ptr<raw_fd_ostream> MBBProfileDumpFileOutput;
-
 protected:
   explicit AsmPrinter(TargetMachine &TM, std::unique_ptr<MCStreamer> Streamer);
 
