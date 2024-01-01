9a9f5d3404a30adf43ad0534e4487c84b6d359ea
Zingam <zingam@outlook.com>
2024-01-01 15:13:08 +0200

    WIP: nothing

--- libcxx/test/std/ranges/range.adaptors/range.enumerate/iterator/base.pass.cpp
+++ libcxx/test/std/ranges/range.adaptors/range.enumerate/iterator/base.pass.cpp
@@ -28,6 +28,55 @@
 #include "test_iterators.h"
 #include "../types.h"
 
+// template <class It, class ItTraits = It>
+// class MovableIterator {
+//   using Traits = std::iterator_traits<ItTraits>;
+//   It it_;
+
+//   template <class U, class T>
+//   friend class MovableIterator;
+
+// public:
+//   using iterator_category = std::input_iterator_tag;
+//   using value_type        = typename Traits::value_type;
+//   using difference_type   = typename Traits::difference_type;
+//   using pointer           = It;
+//   using reference         = typename Traits::reference;
+
+//   TEST_CONSTEXPR explicit MovableIterator(It it) : it_(it), justInitialized{true} { static_assert(false); }
+
+//   template <class U, class T>
+//   TEST_CONSTEXPR MovableIterator(const MovableIterator<U, T>& u) : it_(u.it_), wasCopyInitialized{true} {
+//     static_assert(false);
+//   }
+
+//   template <class U, class T, class = typename std::enable_if<std::is_default_constructible<U>::value>::type>
+//   TEST_CONSTEXPR_CXX14 MovableIterator(MovableIterator<U, T>&& u) : it_(u.it_), wasMoveInitialized{true} {
+//     static_assert(false);
+//     u.it_ = U();
+//   }
+
+//   TEST_CONSTEXPR reference operator*() const { return *it_; }
+
+//   TEST_CONSTEXPR_CXX14 MovableIterator& operator++() {
+//     ++it_;
+//     return *this;
+//   }
+//   TEST_CONSTEXPR_CXX14 MovableIterator operator++(int) { return MovableIterator(it_++); }
+
+//   friend TEST_CONSTEXPR bool operator==(const MovableIterator& x, const MovableIterator& y) { return x.it_ == y.it_; }
+//   friend TEST_CONSTEXPR bool operator!=(const MovableIterator& x, const MovableIterator& y) { return x.it_ != y.it_; }
+
+//   friend TEST_CONSTEXPR It base(const MovableIterator& i) { return i.it_; }
+
+//   template <class T>
+//   void operator,(T const&) = delete;
+
+//   bool justInitialized    = false;
+//   bool wasCopyInitialized = false;
+//   bool wasMoveInitialized = false;
+// };
+
 template <class Iterator>
 constexpr void testBase() {
   using Sentinel          = sentinel_wrapper<Iterator>;
@@ -57,10 +106,18 @@ constexpr void testBase() {
     EnumerateIterator it                         = view.begin();
     std::same_as<Iterator> decltype(auto) result = std::move(it).base();
     assert(base(result) == array.begin());
+
+    // // Test move
+    // if constexpr (std::same_as<Iterator, MovableIterator<int*>>) {
+    //   assert(result.justInitialized);
+    //   assert(!result.wasCopyInitialized);
+    //   assert(!result.wasMoveInitialized);
+    // }
   }
 }
 
 constexpr bool test() {
+  // testBase<MovableIterator<int*>>();
   testBase<cpp17_input_iterator<int*>>();
   testBase<cpp20_input_iterator<int*>>();
   testBase<forward_iterator<int*>>();
