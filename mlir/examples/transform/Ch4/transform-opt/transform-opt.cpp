b39af880ac31c6661e2d29bdd8051582bbee62a4
Alex Zinenko <zinenko@google.com>
2024-01-02 14:46:22 +0000

    [mlir] add a chapter on matchers to the transform dialect tutorial
    These operations has been available for a while, but were not described
    in the tutorial. Add a new chapter on using and defining match
    operations.

--- mlir/examples/transform/Ch4/transform-opt/transform-opt.cpp
+++ mlir/examples/transform/Ch4/transform-opt/transform-opt.cpp
@@ -6,12 +6,13 @@
 //
 //===----------------------------------------------------------------------===//
 //
-// This is the top-level file for the Transform dialect tutorial chapter 3.
+// This is the top-level file for the Transform dialect tutorial chapter 4.
 //
 //===----------------------------------------------------------------------===//
 
 #include "MyExtension.h"
 
+#include "mlir/Dialect/Transform/Transforms/Passes.h"
 #include "mlir/IR/DialectRegistry.h"
 #include "mlir/IR/MLIRContext.h"
 #include "mlir/InitAllDialects.h"
@@ -19,14 +20,6 @@
 #include "mlir/Tools/mlir-opt/MlirOptMain.h"
 #include "mlir/Transforms/Passes.h"
 #include <cstdlib>
-
-// Forward declarations of test passes that used in this chapter for
-// illustrative purposes. Test passes are not directly exposed for use in
-// binaries other than mlir-opt, which is too big to serve as an example.
-namespace mlir::test {
-void registerTestTransformDialectEraseSchedulePass();
-void registerTestTransformDialectInterpreterPass();
-} // namespace mlir::test
 
 namespace test {
 void registerTestTransformDialectExtension(mlir::DialectRegistry &);
@@ -44,19 +37,18 @@ int main(int argc, char **argv) {
   mlir::registerCanonicalizerPass();
   mlir::registerCSEPass();
   mlir::registerSymbolDCEPass();
+  mlir::transform::registerInterpreterPass();
 
   // Register the test passes.
 #ifdef MLIR_INCLUDE_TESTS
-  mlir::test::registerTestTransformDialectEraseSchedulePass();
-  mlir::test::registerTestTransformDialectInterpreterPass();
   test::registerTestTransformDialectExtension(registry);
 #else
-  llvm::errs() << "warning: MLIR built without test passes, interpreter "
+  llvm::errs() << "warning: MLIR built without test extension, interpreter "
                   "testing will not be available\n";
 #endif // MLIR_INCLUDE_TESTS
 
   // Delegate to the MLIR utility for parsing and pass management.
-  return mlir::MlirOptMain(argc, argv, "transform-opt-ch3", registry)
+  return mlir::MlirOptMain(argc, argv, "transform-opt-ch4", registry)
                  .succeeded()
              ? EXIT_SUCCESS
              : EXIT_FAILURE;
--- mlir/test/lit.cfg.py
+++ mlir/test/lit.cfg.py
@@ -156,6 +156,7 @@ tools.extend(
         ToolSubst("toyc-ch7", unresolved="ignore"),
         ToolSubst('transform-opt-ch2', unresolved='ignore'),
         ToolSubst('transform-opt-ch3', unresolved='ignore'),
+        ToolSubst('transform-opt-ch4', unresolved='ignore'),
         ToolSubst("%mlir_lib_dir", config.mlir_lib_dir, unresolved="ignore"),
         ToolSubst("%mlir_src_dir", config.mlir_src_root, unresolved="ignore"),
     ]
