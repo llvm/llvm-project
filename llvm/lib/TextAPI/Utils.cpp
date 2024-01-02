a2ce0eca8977b5e3e9f0ca41683574e4ec4eb112
Cyndy Ishida <cyndy_ishida@apple.com>
2024-01-02 10:47:33 -0800

    [readtapi] Add support for stubify-ing directories
    When given a directory input `llvm-readtapi` traverses through the directory
    to find dylibs or tbd files to operate on. TBD files will be created with
    the same base file name as the dylib. Symlinks should be
    created if the input is one.
    
    This also introduces options to delete input files which
    are defined as library files that existed before `readtapi -stubify` was
    invoked. Also the ability to delete private libraries where private
    libraries are in a predefined file system locations on darwin based
    platforms.

--- llvm/lib/TextAPI/Utils.cpp
+++ llvm/lib/TextAPI/Utils.cpp
@@ -11,6 +11,9 @@
 //===----------------------------------------------------------------------===//
 
 #include "llvm/TextAPI/Utils.h"
+#if !defined(_MSC_VER) && !defined(__MINGW32__)
+#include <unistd.h>
+#endif
 
 using namespace llvm;
 using namespace llvm::MachO;
@@ -37,4 +40,118 @@ void llvm::MachO::replace_extension(SmallVectorImpl<char> &Path,
 
   // Append extension.
   Path.append(Ext.begin(), Ext.end());
+}
+
+std::error_code llvm::MachO::shouldSkipSymLink(const Twine &Path,
+                                               bool &Result) {
+  Result = false;
+  SmallString<PATH_MAX> Storage;
+  auto P = Path.toNullTerminatedStringRef(Storage);
+  sys::fs::file_status stat1;
+  auto ec = sys::fs::status(P.data(), stat1);
+  if (ec == std::errc::too_many_symbolic_link_levels) {
+    Result = true;
+    return {};
+  }
+
+  if (ec)
+    return ec;
+
+  StringRef Parent = sys::path::parent_path(P);
+  while (!Parent.empty()) {
+    sys::fs::file_status stat2;
+    if (auto ec = sys::fs::status(Parent, stat2))
+      return ec;
+
+    if (sys::fs::equivalent(stat1, stat2)) {
+      Result = true;
+      return {};
+    }
+
+    Parent = sys::path::parent_path(Parent);
+  }
+  return {};
+}
+
+std::error_code
+llvm::MachO::make_relative(StringRef From, StringRef To,
+                           SmallVectorImpl<char> &RelativePath) {
+  SmallString<PATH_MAX> Src = From;
+  SmallString<PATH_MAX> Dst = To;
+  if (auto ec = sys::fs::make_absolute(Src))
+    return ec;
+
+  if (auto ec = sys::fs::make_absolute(Dst))
+    return ec;
+
+  SmallString<PATH_MAX> Result;
+  Src = sys::path::parent_path(From);
+  auto it1 = sys::path::begin(Src), it2 = sys::path::begin(Dst),
+       ie1 = sys::path::end(Src), ie2 = sys::path::end(Dst);
+  // Ignore the common part.
+  for (; it1 != ie1 && it2 != ie2; ++it1, ++it2) {
+    if (*it1 != *it2)
+      break;
+  }
+
+  for (; it1 != ie1; ++it1)
+    sys::path::append(Result, "../");
+
+  for (; it2 != ie2; ++it2)
+    sys::path::append(Result, *it2);
+
+  if (Result.empty())
+    Result = ".";
+
+  RelativePath.swap(Result);
+
+  return {};
+}
+
+bool llvm::MachO::isPrivateLibrary(StringRef Path, bool IsSymLink) {
+  // Remove the iOSSupport and DriverKit prefix to identify public locations.
+  Path.consume_front(MACCATALYST_PREFIX_PATH);
+  Path.consume_front(DRIVERKIT_PREFIX_PATH);
+  // Also /Library/Apple prefix for ROSP.
+  Path.consume_front("/Library/Apple");
+
+  if (Path.starts_with("/usr/local/lib"))
+    return true;
+
+  if (Path.starts_with("/System/Library/PrivateFrameworks"))
+    return true;
+
+  // Everything in /usr/lib/swift (including sub-directories) are considered
+  // public.
+  if (Path.consume_front("/usr/lib/swift/"))
+    return false;
+
+  // Only libraries directly in /usr/lib are public. All other libraries in
+  // sub-directories are private.
+  if (Path.consume_front("/usr/lib/"))
+    return Path.contains('/');
+
+  // "/System/Library/Frameworks/" is a public location.
+  if (Path.starts_with("/System/Library/Frameworks/")) {
+    StringRef Name, Rest;
+    std::tie(Name, Rest) =
+        Path.drop_front(sizeof("/System/Library/Frameworks")).split('.');
+
+    // Allow symlinks to top-level frameworks.
+    if (IsSymLink && Rest == "framework")
+      return false;
+
+    // Only top level framework are public.
+    // /System/Library/Frameworks/Foo.framework/Foo ==> true
+    // /System/Library/Frameworks/Foo.framework/Versions/A/Foo ==> true
+    // /System/Library/Frameworks/Foo.framework/Resources/libBar.dylib ==> false
+    // /System/Library/Frameworks/Foo.framework/Frameworks/Bar.framework/Bar
+    // ==> false
+    // /System/Library/Frameworks/Foo.framework/Frameworks/Xfoo.framework/XFoo
+    // ==> false
+    return !(Rest.starts_with("framework/") &&
+             (Rest.ends_with(Name) || Rest.ends_with((Name + ".tbd").str()) ||
+              (IsSymLink && Rest.ends_with("Current"))));
+  }
+  return false;
 }
