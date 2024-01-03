cb28f744635c3cc8bce40e5f9374c57606f8c774
Daniel Walter <dwalter@altair.com>
2024-01-03 13:16:20 -0600

    cleanup test case

--- clang-tools-extra/test/clang-tidy/checkers/readability/use-explicit-namespaces.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/readability/use-explicit-namespaces.cpp
@@ -1,22 +1,15 @@
 // RUN: %check_clang_tidy %s readability-use-explicit-namespaces %t
 
-namespace foo
-{
-void doSomething()
-{
-}
-}
+namespace foo {
+void doSomething() {}
+} // namespace foo
 
-void test1()
-{
-        foo::doSomething();
-}
+void test1() { foo::doSomething(); }
 
 using namespace foo;
 
-void test2()
-{
-	// CHECK-MESSAGES: :[[@LINE+1]]:2: warning: Missing namespace qualifiers foo::
-        doSomething();
+void test2() {
+  doSomething();
+  // CHECK-MESSAGES: :[[@LINE-1]]:3: warning: Missing namespace qualifiers foo::
+  // CHECK-FIXES:  foo::
 }
-
