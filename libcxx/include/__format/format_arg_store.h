bbd14a34180e1976047a801810e3812274f36b96
Bogdan Graur <bgraur@google.com>
2024-01-03 15:52:34 +0000

    [NFC] Renames a template parameter to avoid clashes with userspace names.

--- libcxx/include/__format/format_arg_store.h
+++ libcxx/include/__format/format_arg_store.h
@@ -228,15 +228,15 @@ _LIBCPP_HIDE_FROM_ABI void __store_basic_format_arg(basic_format_arg<_Context>* 
   ([&] { *__data++ = __format::__create_format_arg<_Context>(__args); }(), ...);
 }
 
-template <class _Context, size_t N>
+template <class _Context, size_t _Nargs>
 struct __packed_format_arg_store {
-  __basic_format_arg_value<_Context> __values_[N];
+  __basic_format_arg_value<_Context> __values_[_Nargs];
   uint64_t __types_ = 0;
 };
 
-template <class _Context, size_t N>
+template <class _Context, size_t _Nargs>
 struct __unpacked_format_arg_store {
-  basic_format_arg<_Context> __args_[N];
+  basic_format_arg<_Context> __args_[_Nargs];
 };
 
 } // namespace __format
