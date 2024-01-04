edb6c66e97c806cc301715cbe71a26506d87336b
Yury Gribov <tetra2005@gmail.com>
2024-01-04 23:37:10 +0300

    [CIR][Lowering] Support lowering of cir.const with GlobalViewAttr (gh-352) (#363)
    The error manifested in code like
    ```
    int a[16];
    int *const p = a;
    
    void foo() {
      p[0];
    }
    ```
    It's one the most frequent errors in current llvm-test-suite.
    
    I've added the test to globals.cir which is currently XFAILed, I think
    @gitoleg will fix it soon.
    
    Co-authored-by: Bruno Cardoso Lopes <bcardosolopes@users.noreply.github.com>

--- clang/lib/CIR/Lowering/DirectToLLVM/LowerToLLVM.cpp
+++ clang/lib/CIR/Lowering/DirectToLLVM/LowerToLLVM.cpp
@@ -1056,6 +1056,12 @@ public:
           return mlir::success();
         }
       }
+      // Lower GlobalViewAttr to llvm.mlir.addressof
+      if (auto gv = op.getValue().dyn_cast<mlir::cir::GlobalViewAttr>()) {
+        auto newOp = lowerCirAttrAsValue(op, gv, rewriter, getTypeConverter());
+        rewriter.replaceOp(op, newOp);
+        return mlir::success();
+      }
       attr = op.getValue();
     }
     // TODO(cir): constant arrays are currently just pushed into the stack using
