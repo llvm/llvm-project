503527cc8306152b8efd513342f13cd39976cb85
Jakub Kuderski <jakub@nod-labs.com>
2024-01-04 10:52:56 -0500

    Use emitOpError

--- mlir/lib/IR/Operation.cpp
+++ mlir/lib/IR/Operation.cpp
@@ -618,9 +618,9 @@ static void checkFoldResultTypes(Operation *op,
   for (auto [ofr, opResult] : llvm::zip_equal(results, op->getResults())) {
     if (auto value = dyn_cast<Value>(ofr)) {
       if (value.getType() != opResult.getType()) {
-        llvm::errs() << "Folder produced a value of incorrect type for: " << *op
-                     << "\nOriginal type: '" << value.getType()
-                     << "'\nNew type: '" << opResult.getType() << "'\n";
+        op->emitOpError() << "folder produced a value of incorrect type: "
+                          << opResult.getType()
+                          << ", expected: " << value.getType();
         assert(false && "incorrect fold result type");
       }
     }
