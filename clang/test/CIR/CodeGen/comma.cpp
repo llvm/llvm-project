77f897d163a05b6d0bc11c7a7e9973cd384ee74c
Sirui Mu <msrlancern@gmail.com>
2024-01-05 04:41:34 +0800

    [CIR][CIRGen] Lvalues and comma expression (#376)
    Currently, codegen of lvalue comma expression would crash:
    
    ```cpp
    int &foo1();
    int &foo2();
    
    void c1() {
        int &x = (foo1(), foo2());  // CRASH
    }
    ```
    
    This simple patch fixes this issue.

--- clang/test/CIR/CodeGen/comma.cpp
+++ clang/test/CIR/CodeGen/comma.cpp
@@ -15,3 +15,16 @@ int c0() {
 // CHECK: %[[#]] = cir.binop(add, %[[#LOADED_B]], %[[#]]) : !s32i
 // CHECK: %[[#LOADED_A:]] = cir.load %[[#A]] : cir.ptr <!s32i>, !s32i
 // CHECK: cir.store %[[#LOADED_A]], %[[#RET]] : !s32i, cir.ptr <!s32i>
+
+int &foo1();
+int &foo2();
+
+void c1() {
+    int &x = (foo1(), foo2());
+}
+
+// CHECK: cir.func @_Z2c1v()
+// CHECK: %0 = cir.alloca !cir.ptr<!s32i>, cir.ptr <!cir.ptr<!s32i>>
+// CHECK: %1 = cir.call @_Z4foo1v() : () -> !cir.ptr<!s32i>
+// CHECK: %2 = cir.call @_Z4foo2v() : () -> !cir.ptr<!s32i>
+// CHECK: cir.store %2, %0 : !cir.ptr<!s32i>, cir.ptr <!cir.ptr<!s32i>>
