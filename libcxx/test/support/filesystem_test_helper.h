202fb858344d102bd5199cd749bb15195dbce558
Wu, Yingcong <yingcong.wu@intel.com>
2024-01-05 00:48:34 -0800

    try to directly create file in /tmp when filepath is too long

--- libcxx/test/support/filesystem_test_helper.h
+++ libcxx/test/support/filesystem_test_helper.h
@@ -18,6 +18,7 @@
 #include <chrono>
 #include <cstdint>
 #include <cstdio> // for printf
+#include <ctime>
 #include <string>
 #include <system_error>
 #include <type_traits>
@@ -324,10 +325,22 @@ struct scoped_test_env
 
         ::sockaddr_un address;
         address.sun_family = AF_UNIX;
+
+        // If file.size() is too big, try to create a file directly inside
+        // /tmp to make sure file path is short enough.
+        if (file.size() <= sizeof(address.sun_path) && utils::exists("/tmp")) {
+            fs::path const tmp = "/tmp";
+            std::size_t i      = std::hash<std::string>()(std::to_string(std::time(nullptr)));
+            fs::path p         = tmp / ("libcxx-socket-" + file + "-" + std::to_string(i));
+            while (utils::exists(p.string())) {
+              p = tmp / ("libcxx-socket-" + file + "-" + std::to_string(++i));
+            }
+            file = p.string();
+        }
         assert(file.size() <= sizeof(address.sun_path));
         ::strncpy(address.sun_path, file.c_str(), sizeof(address.sun_path));
         int fd = ::socket(AF_UNIX, SOCK_STREAM, 0);
-        ::bind(fd, reinterpret_cast<::sockaddr*>(&address), sizeof(address));
+        assert(::bind(fd, reinterpret_cast<::sockaddr*>(&address), sizeof(address)) == 0);
         return file;
     }
 #endif
