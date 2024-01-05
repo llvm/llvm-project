027f320c11ea753ec337403d69662ab2d13bcd14
Mark de Wever <koraq@xs4all.nl>
2024-01-05 19:43:10 +0100

    "Reapply "[Sema] Fix crash on invalid code with parenthesized aggrega… (#76833)
    …te initialization" (#76272)""
    
    With updates the libc++ tests.
    
    This reverts commit c59d1c2 and relands
    442ff93 and
    62095ce.
    
    Original commit was reverted because of failing libc++ tests, see #76232
    for the discussion.
    
    The errors in the tests are spurious in the first place (coming from
    initialization of invalid classes), so update the tests to match new
    behavior that does not show those errors.
    
    The original patch was written by @ilya-biryukov 
    
    To fix the CI two libc++ tests are temporary disabled for clang-18.

--- libcxx/test/libcxx/utilities/expected/expected.void/transform_error.mandates.verify.cpp
+++ libcxx/test/libcxx/utilities/expected/expected.void/transform_error.mandates.verify.cpp
@@ -6,6 +6,15 @@
 //
 //===----------------------------------------------------------------------===//
 
+// https://github.com/llvm/llvm-project/pull/76232 breaks this libc++ test.
+// The fix would be to update this file. The issue is that the CI uses 2
+// versions of Clang-18
+// - An older nightly build as the main compiler
+// - A freshly bootstrap build
+// This means the test can't be used until the nightly build is updated.
+// TODO(mordante) Reenable clang-18.
+// UNSUPPORTED: clang-18
+
 // UNSUPPORTED: c++03, c++11, c++14, c++17, c++20
 
 // Test the mandates
