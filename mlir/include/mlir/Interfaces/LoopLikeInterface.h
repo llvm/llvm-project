baaf648f077da5e73466c2c7b8e2beb34d8d1eff
MaheshRavishankar <mahesh.ravishankar@gmail.com>
2024-01-04 18:50:09 -0800

    [mlir][TilingInterface] Use `LoopLikeOpInterface` in tiling using SCF to unify tiling with `scf.for` and `scf.forall`.
    Using `LoopLikeOpInterface` as the basis for the implementation
    unifies all the tiling logic for both `scf.for` and `scf.forall`. The
    only difference is the actual loop generation.
    Instead of many entry points for each loop type, the loop type is now
    passed as part of the options passed to the tiling method.
    
    This is a breaking change with the following changes
    
    1) The `scf::tileUsingSCFForOp` is renamed to `scf::tileUsingSCF`
    2) The `scf::tileUsingSCFForallOp` is deprecated. The same
       functionality is obtained by using `scf::tileUsingSCF` and setting
       the loop type in `scf::SCFTilingOptions` passed into this method to
       `scf::SCFTilingOptions::LoopType::ForallOp` (using the
       `setLoopType` method).
    3) The `scf::tileConsumerAndFusedProducerGreedilyUsingSCFForOp` is
       renamed to `scf::tileConsumerAndFuseProducerUsingSCF`. The use of
       the `controlFn` in `scf::SCFTileAndFuseOptions` allows implementing
       any strategy with the default callback implemeting the greedy fusion.
    4) The `scf::SCFTilingResult` and `scf::SCFTileAndFuseResult` now use
       `SmallVector<LoopLikeOpInterface>`.
    5) To make `scf::ForallOp` implement the parts of
       `LoopLikeOpInterface` needed, the `getOutputBlockArguments()`
       method is replaced with `getRegionIterArgs()`
    
    This change also introduces a new interface method for
    `LoopLikeOpInterface`, that allows loop constructs to handle tiled
    yields.
    
    These changes now bring the tiling and fusion capabilities using
    `scf.forall` on par with what was already supported

--- mlir/include/mlir/Interfaces/LoopLikeInterface.h
+++ mlir/include/mlir/Interfaces/LoopLikeInterface.h
@@ -25,6 +25,28 @@ class RewriterBase;
 using NewYieldValuesFn = std::function<SmallVector<Value>(
     OpBuilder &b, Location loc, ArrayRef<BlockArgument> newBbArgs)>;
 
+/// A function that allows returning additional yielded values during
+/// `yieldTiledValuesAndReplace`.
+/// - `ivs` induction variable for the loop.
+/// - `newBbArgs` basic block arguments corresponding to newly added iter_args.
+/// - `tiledValues` the tiled values to return. Must be of same size as
+///   `newbbArgs`, each element of this array is inserted into the corresponding
+///   element in `newbbArgs`.
+/// - `resultOffsets` is of the same size as `tiledValues` and represents
+///   the offsets to use when inserting corresponding element from `tiledValues`
+///   into the element from `newBbArgs`.
+/// - `resultSizes` is of the same size as `tiledValues` and represents
+///   the size of the corresponding element from `tiledValues` inserted into
+///   the element from `newBbArgs`.
+/// - `resultStrides` is of the same size as `tiledValues` and represents
+///   the strides to use when inserting corresponding element from `tiledValues`
+///   into the element from `newBbArgs`.
+using YieldTiledValuesFn = std::function<LogicalResult(
+    RewriterBase &rewriter, Location loc, ValueRange ivs, ValueRange newBbArgs,
+    SmallVector<Value> &tiledValues,
+    SmallVector<SmallVector<OpFoldResult>> &resultOffsets,
+    SmallVector<SmallVector<OpFoldResult>> &resultSizes)>;
+
 namespace detail {
 /// Verify invariants of the LoopLikeOpInterface.
 LogicalResult verifyLoopLikeOpInterface(Operation *op);
