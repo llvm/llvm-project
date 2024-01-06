0f696121345989c04d6c3948d126d95ad0f00a4c
Will Hawkins <hawkinsw@obs.cr>
2024-01-06 03:13:44 -0500

    fixup! WIP: [libc++][ranges] Implement `ranges::stride_view`.
    Another round of types.h cleanup.

--- libcxx/test/std/ranges/range.adaptors/range.stride.view/types.h
+++ libcxx/test/std/ranges/range.adaptors/range.stride.view/types.h
@@ -13,10 +13,19 @@
 #include <ranges>
 #include <type_traits>
 
+#include "__iterator/concepts.h"
 #include "test_iterators.h"
 #include "test_range.h"
 
-template <class Derived, typename Iter = int*, bool Sized = false>
+// Concepts
+
+template <typename Iter>
+concept IterDifferable = requires(Iter& t) { t - t; };
+
+// Iterators
+
+template <class Derived, std::input_iterator Iter = int*, bool IsSized = false>
+  requires((!IsSized) || (IsSized && IterDifferable<Iter>))
 struct InputIterBase {
   using iterator_concept = std::input_iterator_tag;
   using value_type       = typename std::iterator_traits<Iter>::value_type;
@@ -43,13 +52,15 @@ struct InputIterBase {
   }
   friend constexpr bool operator==(const Derived& left, const Derived& right) { return left.value_ == right.value_; }
   friend constexpr difference_type operator-(const Derived& left, const Derived& right)
-    requires Sized
+    requires IsSized
   {
     return left.value_ - right.value_;
   }
 };
 
-struct UnsizedBasicRangeIterator : InputIterBase<UnsizedBasicRangeIterator> {};
+struct UnsizedInputIterator : InputIterBase<UnsizedInputIterator> {};
+static_assert(std::input_iterator<UnsizedInputIterator>);
+static_assert(!std::sized_sentinel_for<UnsizedInputIterator, UnsizedInputIterator>);
 
 struct SizedInputIterator : InputIterBase<SizedInputIterator, int*, true> {
   using InputIterBase::InputIterBase;
@@ -57,41 +68,24 @@ struct SizedInputIterator : InputIterBase<SizedInputIterator, int*, true> {
 static_assert(std::input_iterator<SizedInputIterator>);
 static_assert(std::sized_sentinel_for<SizedInputIterator, SizedInputIterator>);
 
-// Don't move/hold the iterator itself, copy/hold the base
-// of that iterator and reconstruct the iterator on demand.
-// May result in aliasing (if, e.g., Iterator is an iterator
-// over int *).
-template <class Iterator, class Sentinel>
-struct ViewOverNonCopyableIterator : std::ranges::view_base {
-  constexpr explicit ViewOverNonCopyableIterator(Iterator it, Sentinel sent) : it_(base(it)), sent_(base(sent)) {}
-
-  ViewOverNonCopyableIterator(ViewOverNonCopyableIterator&&)            = default;
-  ViewOverNonCopyableIterator& operator=(ViewOverNonCopyableIterator&&) = default;
-
-  constexpr Iterator begin() const { return Iterator(it_); }
-  constexpr Sentinel end() const { return Sentinel(sent_); }
-
-private:
-  decltype(base(std::declval<Iterator>())) it_;
-  decltype(base(std::declval<Sentinel>())) sent_;
-};
-
 // Put IterMoveIterSwapTestRangeIterator in a namespace to test ADL of CPOs iter_swap and iter_move
 // (see iter_swap.pass.cpp and iter_move.pass.cpp).
 namespace adl {
-template <typename T = int*, bool IsSwappable = true, bool IsNoExcept = true>
+template <std::input_iterator Iter = int*, bool IsIterSwappable = true, bool IsNoExceptIterMoveable = true>
 struct IterMoveIterSwapTestRangeIterator
-    : InputIterBase<IterMoveIterSwapTestRangeIterator<T, IsSwappable, IsNoExcept>, T, false> {
+    : InputIterBase<IterMoveIterSwapTestRangeIterator<Iter, IsIterSwappable, IsNoExceptIterMoveable>, Iter, false> {
   int* counter_{nullptr};
 
-  using InputIterBase<IterMoveIterSwapTestRangeIterator<T, IsSwappable, IsNoExcept>, T, false>::InputIterBase;
+  using InputIterBase<IterMoveIterSwapTestRangeIterator<Iter, IsIterSwappable, IsNoExceptIterMoveable>, Iter, false>::
+      InputIterBase;
 
-  constexpr IterMoveIterSwapTestRangeIterator(T value, int* counter)
-      : InputIterBase<IterMoveIterSwapTestRangeIterator<T, IsSwappable, IsNoExcept>, T, false>(value),
+  constexpr IterMoveIterSwapTestRangeIterator(Iter value, int* counter)
+      : InputIterBase<IterMoveIterSwapTestRangeIterator<Iter, IsIterSwappable, IsNoExceptIterMoveable>, Iter, false>(
+            value),
         counter_(counter) {}
 
   friend constexpr void iter_swap(IterMoveIterSwapTestRangeIterator t, IterMoveIterSwapTestRangeIterator u) noexcept
-    requires IsSwappable && IsNoExcept
+    requires IsIterSwappable && IsNoExceptIterMoveable
   {
     (*t.counter_)++;
     (*u.counter_)++;
@@ -99,7 +93,7 @@ struct IterMoveIterSwapTestRangeIterator
   }
 
   friend constexpr void iter_swap(IterMoveIterSwapTestRangeIterator t, IterMoveIterSwapTestRangeIterator u)
-    requires IsSwappable && (!IsNoExcept)
+    requires IsIterSwappable && (!IsNoExceptIterMoveable)
   {
     (*t.counter_)++;
     (*u.counter_)++;
@@ -107,13 +101,13 @@ struct IterMoveIterSwapTestRangeIterator
   }
 
   friend constexpr auto iter_move(const IterMoveIterSwapTestRangeIterator& t)
-    requires(!IsNoExcept)
+    requires(!IsNoExceptIterMoveable)
   {
     (*t.counter_)++;
     return *t.value_;
   }
   friend constexpr auto iter_move(const IterMoveIterSwapTestRangeIterator& t) noexcept
-    requires IsNoExcept
+    requires IsNoExceptIterMoveable
   {
     (*t.counter_)++;
     return *t.value_;
@@ -121,25 +115,45 @@ struct IterMoveIterSwapTestRangeIterator
 };
 } // namespace adl
 
-template <typename T = int*, bool IsSwappable = true, bool IsNoExcept = true>
+template <typename Iter = int*, bool IsSwappable = true, bool IsNoExcept = true>
 struct IterMoveIterSwapTestRange : std::ranges::view_base {
-  adl::IterMoveIterSwapTestRangeIterator<T, IsSwappable, IsNoExcept> begin_;
-  adl::IterMoveIterSwapTestRangeIterator<T, IsSwappable, IsNoExcept> end_;
-  constexpr IterMoveIterSwapTestRange(const T& begin, const T& end, int* counter)
-      : begin_(adl::IterMoveIterSwapTestRangeIterator<T, IsSwappable, IsNoExcept>(begin, counter)),
-        end_(adl::IterMoveIterSwapTestRangeIterator<T, IsSwappable, IsNoExcept>(end, counter)) {}
-  constexpr adl::IterMoveIterSwapTestRangeIterator<T, IsSwappable, IsNoExcept> begin() const { return begin_; }
-  constexpr adl::IterMoveIterSwapTestRangeIterator<T, IsSwappable, IsNoExcept> end() const { return end_; }
+  adl::IterMoveIterSwapTestRangeIterator<Iter, IsSwappable, IsNoExcept> begin_;
+  adl::IterMoveIterSwapTestRangeIterator<Iter, IsSwappable, IsNoExcept> end_;
+  constexpr IterMoveIterSwapTestRange(const Iter& begin, const Iter& end, int* counter)
+      : begin_(adl::IterMoveIterSwapTestRangeIterator<Iter, IsSwappable, IsNoExcept>(begin, counter)),
+        end_(adl::IterMoveIterSwapTestRangeIterator<Iter, IsSwappable, IsNoExcept>(end, counter)) {}
+  constexpr adl::IterMoveIterSwapTestRangeIterator<Iter, IsSwappable, IsNoExcept> begin() const { return begin_; }
+  constexpr adl::IterMoveIterSwapTestRangeIterator<Iter, IsSwappable, IsNoExcept> end() const { return end_; }
 };
 
 // Views
 
-template <std::input_iterator T, std::sentinel_for<T> S = sentinel_wrapper<T>, bool IsCopyable = true>
-struct MaybeCopyableAlwaysMoveableView : std::ranges::view_base {
-  T begin_;
-  T end_;
+template <std::input_iterator Iter, std::sentinel_for<Iter> Sent = sentinel_wrapper<Iter>, bool IsSized = false>
+  requires((!IsSized) || (IsSized && IterDifferable<Iter>))
+struct BasicTestView : std::ranges::view_base {
+  Iter begin_{};
+  Iter end_{};
 
-  constexpr explicit MaybeCopyableAlwaysMoveableView(T b, T e) : begin_(b), end_(e) {}
+  constexpr BasicTestView(Iter b, Iter e) : begin_(b), end_(e) {}
+
+  constexpr Iter begin() { return begin_; }
+  constexpr Iter begin() const { return begin_; }
+  constexpr Sent end() { return Sent{end_}; }
+  constexpr Sent end() const { return Sent{end_}; }
+
+  constexpr auto size() const
+    requires IsSized
+  {
+    return begin_ - end_;
+  }
+};
+
+template <std::input_iterator Iter, std::sentinel_for<Iter> Sent = sentinel_wrapper<Iter>, bool IsCopyable = true>
+struct MaybeCopyableAlwaysMoveableView : std::ranges::view_base {
+  Iter begin_;
+  Iter end_;
+
+  constexpr explicit MaybeCopyableAlwaysMoveableView(Iter b, Iter e) : begin_(b), end_(e) {}
 
   constexpr MaybeCopyableAlwaysMoveableView(MaybeCopyableAlwaysMoveableView&& other)      = default;
   constexpr MaybeCopyableAlwaysMoveableView& operator=(MaybeCopyableAlwaysMoveableView&&) = default;
@@ -158,40 +172,40 @@ struct MaybeCopyableAlwaysMoveableView : std::ranges::view_base {
     requires IsCopyable
   = default;
 
-  constexpr T begin() const { return begin_; }
-  constexpr sentinel_wrapper<T> end() const { return sentinel_wrapper<T>{end_}; }
+  constexpr Iter begin() const { return begin_; }
+  constexpr Sent end() const { return Sent{end_}; }
 };
 static_assert(std::ranges::view<MaybeCopyableAlwaysMoveableView<cpp17_input_iterator<int*>>>);
+static_assert(std::ranges::view<MaybeCopyableAlwaysMoveableView<cpp17_input_iterator<int*>,
+                                                                sentinel_wrapper<cpp17_input_iterator<int*>>,
+                                                                false>>);
 
 static_assert(std::copyable<MaybeCopyableAlwaysMoveableView<cpp17_input_iterator<int*>>>);
-static_assert(!std::copyable<MaybeCopyableAlwaysMoveableView<cpp17_input_iterator<int*>,
-                                                             sentinel_wrapper<cpp17_input_iterator<int*>>,
-                                                             false>>);
+template <std::input_iterator Iter, std::sentinel_for<Iter> Sent = sentinel_wrapper<Iter>>
+using CopyableView = MaybeCopyableAlwaysMoveableView<Iter, Sent>;
+static_assert(std::copyable<CopyableView<cpp17_input_iterator<int*>>>);
 
-template <std::input_iterator T, std::sentinel_for<T> S = sentinel_wrapper<T>>
-using CopyableView = MaybeCopyableAlwaysMoveableView<T, S>;
+template <std::input_iterator Iter, std::sentinel_for<Iter> Sent = sentinel_wrapper<Iter>>
+using MoveOnlyView = MaybeCopyableAlwaysMoveableView<Iter, Sent, false>;
+static_assert(!std::copyable<MoveOnlyView<cpp17_input_iterator<int*>>>);
 
-template <std::input_iterator T, std::sentinel_for<T> S = sentinel_wrapper<T>>
-using MoveOnlyView = MaybeCopyableAlwaysMoveableView<T, S, false>;
+// Don't move/hold the iterator itself, copy/hold the base
+// of that iterator and reconstruct the iterator on demand.
+// May result in aliasing (if, e.g., Iterator is an iterator
+// over int *).
+template <class Iter, std::sentinel_for<Iter> Sent = sentinel_wrapper<Iter>>
+struct ViewOverNonCopyableIterator : std::ranges::view_base {
+  constexpr explicit ViewOverNonCopyableIterator(Iter it, Sent sent) : it_(base(it)), sent_(base(sent)) {}
 
-template <std::input_iterator T, std::sentinel_for<T> S = sentinel_wrapper<T>, bool IsSized = false>
-struct BasicTestView : std::ranges::view_base {
-  T begin_{};
-  T end_{};
+  ViewOverNonCopyableIterator(ViewOverNonCopyableIterator&&)            = default;
+  ViewOverNonCopyableIterator& operator=(ViewOverNonCopyableIterator&&) = default;
 
-  constexpr BasicTestView(T b, T e) : begin_(b), end_(e) {}
+  constexpr Iter begin() const { return Iter(it_); }
+  constexpr Sent end() const { return Sent(sent_); }
 
-  constexpr T begin() { return begin_; }
-  constexpr T begin() const { return begin_; }
-  constexpr S end() { return S{end_}; }
-  constexpr S end() const { return S{end_}; }
-
-  constexpr auto size() const
-    requires IsSized
-  {
-    return begin_ - end_;
-  }
+private:
+  decltype(base(std::declval<Iter>())) it_;
+  decltype(base(std::declval<Sent>())) sent_;
 };
-static_assert(std::ranges::sized_range<BasicTestView<SizedInputIterator, sentinel_wrapper<SizedInputIterator>, true>>);
 
 #endif // TEST_STD_RANGES_RANGE_ADAPTORS_RANGE_STRIDE_TYPES_H
