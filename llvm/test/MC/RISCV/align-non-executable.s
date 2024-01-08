02e670d7336e6bc8051f35fc184b053434e7a27a
Jinyang He <hejinyang@loongson.cn>
2024-01-08 10:25:52 +0800

    Address @MaskRay's comments.

--- llvm/test/MC/RISCV/align-non-executable.s
+++ llvm/test/MC/RISCV/align-non-executable.s
@@ -1,3 +1,8 @@
+## Check the data diff (separated by aligment directives) directives which in
+## a section which has instructions but is not executable should generate relocs
+## because it can not be calculated out in AttemptToFoldSymbolOffsetDifference.
+## https://github.com/llvm/llvm-project/pull/76552
+
 # RUN: llvm-mc --filetype=obj --triple=riscv64 --mattr=+relax %s \
 # RUN:     | llvm-readobj -r - | FileCheck --check-prefixes=CHECK,RELAX %s
 # RUN: llvm-mc --filetype=obj --triple=riscv64 --mattr=-relax %s \
@@ -9,9 +14,6 @@
 .p2align 3
 .L2:
 .dword .L2 - .L1
-.word .L2 - .L1
-.half .L2 - .L1
-.byte .L2 - .L1
 
 # CHECK:       Relocations [
 # CHECK-NEXT:    Section ({{.*}}) .rela.dummy {
@@ -19,11 +21,5 @@
 # RELAX-NEXT:      0x0 R_RISCV_RELAX - 0x0
 # CHECK-NEXT:      0x8 R_RISCV_ADD64 .L2 0x0
 # CHECK-NEXT:      0x8 R_RISCV_SUB64 .L1 0x0
-# CHECK-NEXT:      0x10 R_RISCV_ADD32 .L2 0x0
-# CHECK-NEXT:      0x10 R_RISCV_SUB32 .L1 0x0
-# CHECK-NEXT:      0x14 R_RISCV_ADD16 .L2 0x0
-# CHECK-NEXT:      0x14 R_RISCV_SUB16 .L1 0x0
-# CHECK-NEXT:      0x16 R_RISCV_ADD8 .L2 0x0
-# CHECK-NEXT:      0x16 R_RISCV_SUB8 .L1 0x0
 # CHECK-NEXT:    }
 # CHECK-NEXT:  ]
