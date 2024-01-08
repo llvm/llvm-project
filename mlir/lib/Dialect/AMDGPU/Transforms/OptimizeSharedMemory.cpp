c072744c97c328360afbb96eaabd6033665dc965
erman-gurses <erman@nod-labs.com>
2024-01-07 22:21:29 -0800

    Add a fix for bad line break

--- mlir/lib/Dialect/AMDGPU/Transforms/OptimizeSharedMemory.cpp
+++ mlir/lib/Dialect/AMDGPU/Transforms/OptimizeSharedMemory.cpp
@@ -1,5 +1,4 @@
-//===- OptimizeSharedMemory.cpp - MLIR AMDGPU pass implementation
-//----------===//
+//===- OptimizeSharedMemory.cpp - MLIR AMDGPU pass implementation ---------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
