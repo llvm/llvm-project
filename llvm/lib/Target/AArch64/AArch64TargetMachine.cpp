869c1f5bc0603f19b2d8602cebdd77a8ee778b3e
David Sherwood <david.sherwood@arm.com>
2024-01-08 18:47:17 +0000

    Rebase and disable by default
    * Fixed an issue after rebase with change in
    registerPassBuilderCallbacks definition.
    * Disabled the new pass by default - we can enable this in
    a later patch.

--- llvm/lib/Target/AArch64/AArch64TargetMachine.cpp
+++ llvm/lib/Target/AArch64/AArch64TargetMachine.cpp
@@ -540,7 +540,8 @@ public:
 
 } // end anonymous namespace
 
-void AArch64TargetMachine::registerPassBuilderCallbacks(PassBuilder &PB) {
+void AArch64TargetMachine::registerPassBuilderCallbacks(
+    PassBuilder &PB, bool PopulateClassToPassNames) {
   PB.registerLateLoopOptimizationsEPCallback(
       [=](LoopPassManager &LPM, OptimizationLevel Level) {
         LPM.addPass(AArch64LoopIdiomTransformPass());
