a3c71f77ab2f25bf94f137c69b997d61b0e9dea8
Yinying Li <yinyingli@google.com>
2024-01-09 19:56:42 +0000

    mref check

--- mlir/lib/ExecutionEngine/CRunnerUtils.cpp
+++ mlir/lib/ExecutionEngine/CRunnerUtils.cpp
@@ -179,10 +179,13 @@ extern "C" void rtdrand(void *g) {
 
 extern "C" void _mlir_ciface_shuffle(StridedMemRefType<uint64_t, 1> *mref,
                                      void *g) {
+  assert(mref);
+  assert(mref->strides[0] == 1); // consecutive
   std::mt19937 *generator = static_cast<std::mt19937 *>(g);
   uint64_t s = mref->sizes[0];
-  std::iota(mref->data, mref->data + s, 0);
-  std::shuffle(mref->data, mref->data + s, *generator);
+  uint64_t *data = mref->data + mref->offset;
+  std::iota(data, data + s, 0);
+  std::shuffle(data, data + s, *generator);
 }
 
 #define IMPL_STDSORT(VNAME, V)                                                 \
