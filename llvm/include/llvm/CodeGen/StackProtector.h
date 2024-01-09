62f4452f87ec9e19533b58e97b6012a02355f0a5
PaperChalice <liujunchang97@outlook.com>
2024-01-09 19:43:54 +0800

    [CodeGen] Fix friend declaration in SSPLayoutAnalysis

--- llvm/include/llvm/CodeGen/StackProtector.h
+++ llvm/include/llvm/CodeGen/StackProtector.h
@@ -66,7 +66,7 @@ public:
 };
 
 class SSPLayoutAnalysis : public AnalysisInfoMixin<SSPLayoutAnalysis> {
-  friend struct AnalysisInfoMixin<SSPLayoutAnalysis>;
+  friend AnalysisInfoMixin<SSPLayoutAnalysis>;
   using SSPLayoutMap = SSPLayoutInfo::SSPLayoutMap;
 
   static AnalysisKey Key;
