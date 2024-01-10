59ec23c9cdd4d4557c0b471331683d577add31d6
Vlad Serebrennikov <serebrennikov.vladislav@gmail.com>
2024-01-10 22:06:01 +0300

    Eliminate the need for readers to guess how the file is named

--- clang/test/CXX/drs/dr18xx.cpp
+++ clang/test/CXX/drs/dr18xx.cpp
@@ -164,7 +164,7 @@ void A<double, U>::C<V>::f4() {
 }
 } // namespace dr1804
 
-// dr1807 is in separate file
+// dr1807 is in dr1807.cpp
 
 namespace dr1812 { // dr1812: no
                    // NB: dup 1710
