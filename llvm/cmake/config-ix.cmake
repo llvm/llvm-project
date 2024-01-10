9b70189e2a248c9b6cbdaacb8b68eb503a02e7ed
Abhina Sreeskantharajan <Abhina.Sreeskantharajan@ibm.com>
2024-01-10 09:52:43 -0500

    add LLVM_ENABLE_ICU and LLVM_ENABLE_ICONV option

--- llvm/cmake/config-ix.cmake
+++ llvm/cmake/config-ix.cmake
@@ -295,19 +295,23 @@ if(LLVM_HAS_LOGF128)
 endif()
 
 #Check for icu.
-find_package(ICU COMPONENTS uc i18n)
-if(ICU_FOUND)
-  set(HAVE_ICU 1)
-else()
-  set(HAVE_ICU 0)
+if(LLVM_ENABLE_ICU)
+  find_package(ICU COMPONENTS uc i18n)
+  if(ICU_FOUND)
+    set(HAVE_ICU 1)
+  else()
+    set(HAVE_ICU 0)
+  endif()
 endif()
 
 # Check for iconv.
-find_package(Iconv)
-if(Iconv_FOUND)
-  set(HAVE_ICONV 1)
-else()
-  set(HAVE_ICONV 0)
+if(LLVM_ENABLE_ICONV)
+  find_package(Iconv)
+  if(Iconv_FOUND)
+    set(HAVE_ICONV 1)
+  else()
+    set(HAVE_ICONV 0)
+  endif()
 endif()
 
 # function checks
