9443c4311cc01e24bbb04ef912059f06b527d60c
Will Hawkins <hawkinsw@obs.cr>
2024-01-10 22:26:22 -0500

    fixup! WIP: [libc++][ranges] Implement `ranges::stride_view`.
    Cleanup libcxx assertion tests.

--- libcxx/test/libcxx/ranges/range.adaptors/range.stride.view/ctor.assert.pass.cpp
+++ libcxx/test/libcxx/ranges/range.adaptors/range.stride.view/ctor.assert.pass.cpp
@@ -7,7 +7,6 @@
 //===----------------------------------------------------------------------===//
 
 // UNSUPPORTED: c++03, c++11, c++14, c++17, c++20
-// UNSUPPORTED: no-exceptions
 // UNSUPPORTED: !libcpp-hardening-mode=debug
 // XFAIL: availability-verbose_abort-missing
 
--- libcxx/test/libcxx/ranges/range.adaptors/range.stride.view/iterator/operator_plus_equal.pass.cpp
+++ libcxx/test/libcxx/ranges/range.adaptors/range.stride.view/iterator/operator_plus_equal.pass.cpp
@@ -7,19 +7,20 @@
 //===----------------------------------------------------------------------===//
 
 // UNSUPPORTED: c++03, c++11, c++14, c++17, c++20
-// UNSUPPORTED: no-exceptions
 // UNSUPPORTED: !libcpp-hardening-mode=debug
 // XFAIL: availability-verbose_abort-missing
+
+// constexpr __iterator& operator+=(difference_type __n)
 
 #include <ranges>
 
 #include "check_assertion.h"
 
 int main(int, char**) {
-  int range[]   = {1, 2, 3};
-  auto striv    = std::ranges::views::stride(range, 2);
-  auto striv_it = striv.begin();
-  TEST_LIBCPP_ASSERT_FAILURE(striv_it += 3, "Advancing the iterator beyond the end is not allowed.");
+  int range[] = {1, 2, 3};
+  auto view   = std::ranges::views::stride(range, 2);
+  auto it     = view.begin();
+  TEST_LIBCPP_ASSERT_FAILURE(it += 3, "Advancing the iterator beyond the end is not allowed.");
 
   return 0;
 }
