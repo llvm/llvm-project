5f164766204173402412f6857983beeaf1a71b6f
carlobertolli <carlo.bertolli@amd.com>
2024-01-12 14:42:49 -0600

    [OpenMP] Fix two usm tests for amdgpus. (#77851)
    Some are missing setting of HSA_XNACK=1 environment variable, used to
    enable unified memory support on amdgpu's when it's not been set at
    kernel boot time. Some others needed to be marked as supporting
    unified_shared_memory in the lit test harness.

--- openmp/libomptarget/test/unified_shared_memory/close_enter_exit.c
+++ openmp/libomptarget/test/unified_shared_memory/close_enter_exit.c
@@ -1,11 +1,11 @@
-// RUN: %libomptarget-compile-run-and-check-generic
+// RUN: %libomptarget-compile-generic
+// RUN: env HSA_XNACK=1 \
+// RUN: %libomptarget-run-generic | %fcheck-generic
 
 // REQUIRES: unified_shared_memory
 // UNSUPPORTED: clang-6, clang-7, clang-8, clang-9
 
-// Fails on amdgpu with error: GPU Memory Error
 // Fails on nvptx with error: an illegal memory access was encountered
-// XFAIL: amdgcn-amd-amdhsa
 // XFAIL: nvptx64-nvidia-cuda
 // XFAIL: nvptx64-nvidia-cuda-LTO
 
