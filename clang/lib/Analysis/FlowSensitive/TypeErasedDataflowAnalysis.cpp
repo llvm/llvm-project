fbac3b0db5ff1d409f90219aeb6460ec5d14f19a
Jie Fu <jiefu@tencent.com>
2024-01-12 17:17:39 +0800

    Revert "[clang][dataflow] Remove unused private field 'StmtToEnv' (NFC)"
    Revert it after 1aacdfe473276ad631db773310fe167ec93fb764

--- clang/lib/Analysis/FlowSensitive/TypeErasedDataflowAnalysis.cpp
+++ clang/lib/Analysis/FlowSensitive/TypeErasedDataflowAnalysis.cpp
@@ -77,7 +77,7 @@ class TerminatorVisitor
 public:
   TerminatorVisitor(const StmtToEnvMap &StmtToEnv, Environment &Env,
                     int BlockSuccIdx)
-      : Env(Env), BlockSuccIdx(BlockSuccIdx) {}
+      : StmtToEnv(StmtToEnv), Env(Env), BlockSuccIdx(BlockSuccIdx) {}
 
   TerminatorVisitorRetTy VisitIfStmt(const IfStmt *S) {
     auto *Cond = S->getCond();
@@ -152,6 +152,7 @@ private:
     return {&Cond, ConditionValue};
   }
 
+  const StmtToEnvMap &StmtToEnv;
   Environment &Env;
   int BlockSuccIdx;
 };
