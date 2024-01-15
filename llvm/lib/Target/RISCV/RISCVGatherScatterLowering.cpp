630e7ee93d51decf00e6041fc8e1af1a88165ad0
Luke Lau <luke@igalia.com>
2024-01-15 14:09:52 +0700

    [RISCV] Don't check haveNoCommonBitsSet in RISCVGatherScatterLowering
    If an or instruction has no common bits set in its operands, InstCombine will
    set the disjoint flag. This means we shouldn't need to compute it ourselves
    anymore in RISCVGatherScatterLowering, and can just rely on said flag being
    set.
    Originally split out from #77800

--- llvm/lib/Target/RISCV/RISCVGatherScatterLowering.cpp
+++ llvm/lib/Target/RISCV/RISCVGatherScatterLowering.cpp
@@ -250,8 +250,7 @@ bool RISCVGatherScatterLowering::matchStridedRecurrence(Value *Index, Loop *L,
     return false;
   case Instruction::Or:
     // We need to be able to treat Or as Add.
-    if (!haveNoCommonBitsSet(BO->getOperand(0), BO->getOperand(1), *DL) &&
-        !cast<PossiblyDisjointInst>(BO)->isDisjoint())
+    if (!cast<PossiblyDisjointInst>(BO)->isDisjoint())
       return false;
     break;
   case Instruction::Add:
