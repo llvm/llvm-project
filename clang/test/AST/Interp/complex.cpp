85d4ffb15f466b8b2aa5d3e522ce4e449de662ca
Timm Bäder <tbaeder@redhat.com>
2024-01-15 09:52:26 +0100

    Enable commented-out tests

--- clang/test/AST/Interp/complex.cpp
+++ clang/test/AST/Interp/complex.cpp
@@ -45,9 +45,9 @@ static_assert(__imag(13) == 0, "");
 constexpr int ignoredCast() {
   I2;
   (int)I2;
-  /* (float)I2; FIXME*/
+  (float)I2;
   D1;
-  /* (int)D1; FIXME*/
+  (int)D1;
   (double)D1;
   return 0;
 }
