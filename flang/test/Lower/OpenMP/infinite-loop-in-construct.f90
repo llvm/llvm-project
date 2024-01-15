a1b59bfe0783fb1ebf90344a1261b4efe073aafe
Krzysztof Parzyszek <Krzysztof.Parzyszek@amd.com>
2024-01-15 13:00:03 -0600

    Add more detailed checks to testcase

--- flang/test/Lower/OpenMP/infinite-loop-in-construct.f90
+++ flang/test/Lower/OpenMP/infinite-loop-in-construct.f90
@@ -3,8 +3,15 @@
 ! Check that this test can be lowered successfully.
 ! See https://github.com/llvm/llvm-project/issues/74348
 
-! CHECK-LABEL: func.func @_QPsb
-! CHECK: omp.parallel
+! CHECK-LABEL:  func.func @_QPsb
+! CHECK:          omp.parallel
+! CHECK:            cf.cond_br %{{[0-9]+}}, ^bb1, ^bb2
+! CHECK-NEXT:     ^bb1:  // pred: ^bb0
+! CHECK:            cf.br ^bb2
+! CHECK-NEXT:     ^bb2:  // 3 preds: ^bb0, ^bb1, ^bb2
+! CHECK-NEXT:       cf.br ^bb2
+! CHECK-NEXT:     }
+
 subroutine sb(ninter, numnod)
   integer :: ninter, numnod
   integer, dimension(:), allocatable :: indx_nm
