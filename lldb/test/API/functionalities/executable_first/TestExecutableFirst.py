bd4f49d12d5ceecd559139fef0b4f2bfaa00164a
Jim Ingham <jingham@apple.com>
2024-01-16 15:17:45 -0800

    Bow to the lord of darkness.

--- lldb/test/API/functionalities/executable_first/TestExecutableFirst.py
+++ lldb/test/API/functionalities/executable_first/TestExecutableFirst.py
@@ -22,11 +22,11 @@ class TestExecutableIsFirst(TestBase):
         target = self.dbg.CreateTarget(None)
         module = target.AddModule(lib, None, None)
         self.assertTrue(module.IsValid(), "Added the module for the library")
-        
+
         module = target.AddModule(exe, None, None)
         self.assertTrue(module.IsValid(), "Added the executable module")
 
-       # This is the executable module so it should be the first in the list:
+        # This is the executable module so it should be the first in the list:
         first_module = target.GetModuleAtIndex(0)
         print("This is the first test, this one succeeds")
         self.assertEqual(module, first_module, "This executable is the first module")
@@ -34,12 +34,16 @@ class TestExecutableIsFirst(TestBase):
         # The executable property is an SBFileSpec to the executable.  Make sure
         # that is also right:
         executable_module = target.executable
-        self.assertEqual(first_module.file, executable_module, "Python property is also our module")
+        self.assertEqual(
+            first_module.file, executable_module, "Python property is also our module"
+        )
 
     def test_executable_is_first_during_run(self):
         self.build()
-        (target, process, thread, bkpt) = lldbutil.run_to_source_breakpoint(self, "break after function call", lldb.SBFileSpec("main.cpp"))
-        
+        (target, process, thread, bkpt) = lldbutil.run_to_source_breakpoint(
+            self, "break after function call", lldb.SBFileSpec("main.cpp")
+        )
+
         first_module = target.GetModuleAtIndex(0)
         self.assertTrue(first_module.IsValid(), "We have at least one module")
         executable_module = target.executable
