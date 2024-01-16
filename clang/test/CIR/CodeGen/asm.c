fa60989e3c01bdcdb4d128bb2110e4e357f4497a
gitoleg <forown@yandex.ru>
2024-01-16 21:44:28 +0300

    [CIR][CIRGen] Add constraints to inline assembly (#351)
    The next step for inline assembly. Sorry, maybe it looks too big on the
    first glance. And it's kind of hard to extract something well-grained
    from the code and introduce it as a separate PR, but I try.
    
    Actually there is nothing really interesting done here, and the next
    will (I hope :) ) simplify your review process.
    
    1) In this PR we introduce operand's constraints and the task is to
    collect them (and maybe transform a little)
    2) There are two big functions copy-pasted from the traditional
    `Codegen` and I doubt they need to be reviewed.
    3) We still don't do anything CIR-specific. Basically, we just work with
    strings in the same way like traditional `Codegen` does.
    4) We just iterate over the input and output operands and collect the
    constraints
    5) We still follow to the traditional `CodeGen` and don't do anything
    new, except a separate function that collects constraints infos in the
    very beginning of the `buildStmt`.
    
    Also, I renamed `AsmDialect` to `AsmFlavor` as you asked in #326

--- clang/test/CIR/CodeGen/asm.c
+++ clang/test/CIR/CodeGen/asm.c
@@ -1,12 +1,32 @@
 // RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fclangir -emit-cir %s -o %t.cir
 // RUN: FileCheck --input-file=%t.cir %s
 
-//CHECK: cir.asm(x86_att, {""})
+//CHECK: cir.asm(x86_att, {"" ""})
 void empty1() {
   __asm__ volatile("" : : : );
 }
 
-//CHECK: cir.asm(x86_att, {"xyz"})
+//CHECK: cir.asm(x86_att, {"xyz" ""})
 void empty2() {
   __asm__ volatile("xyz" : : : );
+}
+
+//CHECK: cir.asm(x86_att, {"" "=*m,m"})
+void t1(int x) {
+  __asm__ volatile("" : "+m"(x));
+}
+
+//CHECK: cir.asm(x86_att, {"" "m"})
+void t2(int x) {
+  __asm__ volatile("" : : "m"(x));
+}
+
+//CHECK: cir.asm(x86_att, {"" "=*m"})
+void t3(int x) {
+  __asm__ volatile("" : "=m"(x));
+}
+
+//CHECK: cir.asm(x86_att, {"" "=&r,=&r,1"})
+void t4(int x) {
+  __asm__ volatile("" : "=&r"(x), "+&r"(x));
 }
