59d1ed627e5eb2da32d9c077e3cffa4c7e393c77
gitoleg <forown@yandex.ru>
2024-01-16 21:44:28 +0300

    [CIR][CIRGen] Add constraints to inline assembly (#351)
    The next step for inline assembly. Sorry, maybe it looks too big on the
    first glance. And it's kind of hard to extract something well-grained
    from the code and introduce it as a separate PR, but I try.
    
    Actually there is nothing really interesting done here, and the next
    will (I hope :) ) simplify your review process.
    
    1) In this PR we introduce operand's constraints and the task is to
    collect them (and maybe transform a little)
    2) There are two big functions copy-pasted from the traditional
    `Codegen` and I doubt they need to be reviewed.
    3) We still don't do anything CIR-specific. Basically, we just work with
    strings in the same way like traditional `Codegen` does.
    4) We just iterate over the input and output operands and collect the
    constraints
    5) We still follow to the traditional `CodeGen` and don't do anything
    new, except a separate function that collects constraints infos in the
    very beginning of the `buildStmt`.
    
    Also, I renamed `AsmDialect` to `AsmFlavor` as you asked in #326

--- clang/lib/CIR/CodeGen/CIRAsm.cpp
+++ clang/lib/CIR/CodeGen/CIRAsm.cpp
@@ -8,26 +8,236 @@ using namespace cir;
 using namespace clang;
 using namespace mlir::cir;
 
-static AsmDialect inferDialect(const CIRGenModule &cgm, const AsmStmt &S) {
-  AsmDialect GnuAsmDialect =
+static AsmFlavor inferFlavor(const CIRGenModule &cgm, const AsmStmt &S) {
+  AsmFlavor GnuAsmFlavor =
       cgm.getCodeGenOpts().getInlineAsmDialect() == CodeGenOptions::IAD_ATT
-          ? AsmDialect::x86_att
-          : AsmDialect::x86_intel;
+          ? AsmFlavor::x86_att
+          : AsmFlavor::x86_intel;
 
-  return isa<MSAsmStmt>(&S) ? AsmDialect::x86_intel : GnuAsmDialect;
+  return isa<MSAsmStmt>(&S) ? AsmFlavor::x86_intel : GnuAsmFlavor;
+}
+
+// FIXME(cir): This should be a common helper between CIRGen
+// and traditional CodeGen
+static std::string SimplifyConstraint(
+    const char *Constraint, const TargetInfo &Target,
+    SmallVectorImpl<TargetInfo::ConstraintInfo> *OutCons = nullptr) {
+  std::string Result;
+
+  while (*Constraint) {
+    switch (*Constraint) {
+    default:
+      Result += Target.convertConstraint(Constraint);
+      break;
+    // Ignore these
+    case '*':
+    case '?':
+    case '!':
+    case '=': // Will see this and the following in mult-alt constraints.
+    case '+':
+      break;
+    case '#': // Ignore the rest of the constraint alternative.
+      while (Constraint[1] && Constraint[1] != ',')
+        Constraint++;
+      break;
+    case '&':
+    case '%':
+      Result += *Constraint;
+      while (Constraint[1] && Constraint[1] == *Constraint)
+        Constraint++;
+      break;
+    case ',':
+      Result += "|";
+      break;
+    case 'g':
+      Result += "imr";
+      break;
+    case '[': {
+      assert(OutCons &&
+             "Must pass output names to constraints with a symbolic name");
+      unsigned Index;
+      bool result = Target.resolveSymbolicName(Constraint, *OutCons, Index);
+      assert(result && "Could not resolve symbolic name");
+      (void)result;
+      Result += llvm::utostr(Index);
+      break;
+    }
+    }
+
+    Constraint++;
+  }
+
+  return Result;
+}
+
+// FIXME(cir): This should be a common helper between CIRGen
+// and traditional CodeGen
+/// Look at AsmExpr and if it is a variable declared
+/// as using a particular register add that as a constraint that will be used
+/// in this asm stmt.
+static std::string
+AddVariableConstraints(const std::string &Constraint, const Expr &AsmExpr,
+                       const TargetInfo &Target, CIRGenModule &CGM,
+                       const AsmStmt &Stmt, const bool EarlyClobber,
+                       std::string *GCCReg = nullptr) {
+  const DeclRefExpr *AsmDeclRef = dyn_cast<DeclRefExpr>(&AsmExpr);
+  if (!AsmDeclRef)
+    return Constraint;
+  const ValueDecl &Value = *AsmDeclRef->getDecl();
+  const VarDecl *Variable = dyn_cast<VarDecl>(&Value);
+  if (!Variable)
+    return Constraint;
+  if (Variable->getStorageClass() != SC_Register)
+    return Constraint;
+  AsmLabelAttr *Attr = Variable->getAttr<AsmLabelAttr>();
+  if (!Attr)
+    return Constraint;
+  StringRef Register = Attr->getLabel();
+  assert(Target.isValidGCCRegisterName(Register));
+  // We're using validateOutputConstraint here because we only care if
+  // this is a register constraint.
+  TargetInfo::ConstraintInfo Info(Constraint, "");
+  if (Target.validateOutputConstraint(Info) && !Info.allowsRegister()) {
+    CGM.ErrorUnsupported(&Stmt, "__asm__");
+    return Constraint;
+  }
+  // Canonicalize the register here before returning it.
+  Register = Target.getNormalizedGCCRegisterName(Register);
+  if (GCCReg != nullptr)
+    *GCCReg = Register.str();
+  return (EarlyClobber ? "&{" : "{") + Register.str() + "}";
+}
+
+using constraintInfos = SmallVector<TargetInfo::ConstraintInfo, 4>;
+
+static void collectInOutConstrainsInfos(const CIRGenFunction &cgf,
+                                        const AsmStmt &S, constraintInfos &out,
+                                        constraintInfos &in) {
+
+  for (unsigned i = 0, e = S.getNumOutputs(); i != e; i++) {
+    StringRef Name;
+    if (const GCCAsmStmt *GAS = dyn_cast<GCCAsmStmt>(&S))
+      Name = GAS->getOutputName(i);
+    TargetInfo::ConstraintInfo Info(S.getOutputConstraint(i), Name);
+    bool IsValid = cgf.getTarget().validateOutputConstraint(Info);
+    (void)IsValid;
+    assert(IsValid && "Failed to parse output constraint");
+    out.push_back(Info);
+  }
+
+  for (unsigned i = 0, e = S.getNumInputs(); i != e; i++) {
+    StringRef Name;
+    if (const GCCAsmStmt *GAS = dyn_cast<GCCAsmStmt>(&S))
+      Name = GAS->getInputName(i);
+    TargetInfo::ConstraintInfo Info(S.getInputConstraint(i), Name);
+    bool IsValid = cgf.getTarget().validateInputConstraint(out, Info);
+    assert(IsValid && "Failed to parse input constraint");
+    (void)IsValid;
+    in.push_back(Info);
+  }
 }
 
 mlir::LogicalResult CIRGenFunction::buildAsmStmt(const AsmStmt &S) {
   // Assemble the final asm string.
   std::string AsmString = S.generateAsmString(getContext());
 
+  // Get all the output and input constraints together.
+  constraintInfos OutputConstraintInfos;
+  constraintInfos InputConstraintInfos;
+  collectInOutConstrainsInfos(*this, S, OutputConstraintInfos,
+                              InputConstraintInfos);
+
   std::string Constraints;
   std::vector<mlir::Type> ResultRegTypes;
   std::vector<mlir::Value> Args;
 
-  assert(!S.getNumOutputs() && "asm output operands are NYI");
-  assert(!S.getNumInputs() && "asm intput operands are NYI");
+  // Keep track of input constraints.
+  std::string InOutConstraints;
+
+  // Keep track of out constraints for tied input operand.
+  std::vector<std::string> OutputConstraints;
+
   assert(!S.getNumClobbers() && "asm clobbers operands are NYI");
+
+  for (unsigned i = 0, e = S.getNumOutputs(); i != e; i++) {
+    TargetInfo::ConstraintInfo &Info = OutputConstraintInfos[i];
+
+    // Simplify the output constraint.
+    std::string OutputConstraint(S.getOutputConstraint(i));
+    OutputConstraint = SimplifyConstraint(OutputConstraint.c_str() + 1,
+                                          getTarget(), &OutputConstraintInfos);
+
+    const Expr *OutExpr = S.getOutputExpr(i);
+    OutExpr = OutExpr->IgnoreParenNoopCasts(getContext());
+
+    std::string GCCReg;
+    OutputConstraint =
+        AddVariableConstraints(OutputConstraint, *OutExpr, getTarget(), CGM, S,
+                               Info.earlyClobber(), &GCCReg);
+
+    OutputConstraints.push_back(OutputConstraint);
+
+    if (!Constraints.empty())
+      Constraints += ',';
+
+    // If this is a register output, then make the inline a sm return it
+    // by-value.  If this is a memory result, return the value by-reference.
+    QualType QTy = OutExpr->getType();
+    const bool IsScalarOrAggregate =
+        hasScalarEvaluationKind(QTy) || hasAggregateEvaluationKind(QTy);
+    if (!Info.allowsMemory() && IsScalarOrAggregate) {
+      Constraints += "=" + OutputConstraint;
+    } else {
+      Constraints += "=*";
+      Constraints += OutputConstraint;
+    }
+
+    if (Info.isReadWrite()) {
+      InOutConstraints += ',';
+
+      // Only tie earlyclobber physregs.
+      if (Info.allowsRegister() && (GCCReg.empty() || Info.earlyClobber()))
+        InOutConstraints += llvm::utostr(i);
+      else
+        InOutConstraints += OutputConstraint;
+    }
+  } // iterate over output operands
+
+  for (unsigned i = 0, e = S.getNumInputs(); i != e; i++) {
+    const Expr *InputExpr = S.getInputExpr(i);
+
+    TargetInfo::ConstraintInfo &Info = InputConstraintInfos[i];
+
+    if (!Constraints.empty())
+      Constraints += ',';
+
+    // Simplify the input constraint.
+    std::string InputConstraint(S.getInputConstraint(i));
+    InputConstraint = SimplifyConstraint(InputConstraint.c_str(), getTarget(),
+                                         &OutputConstraintInfos);
+
+    InputConstraint = AddVariableConstraints(
+        InputConstraint, *InputExpr->IgnoreParenNoopCasts(getContext()),
+        getTarget(), CGM, S, false /* No EarlyClobber */);
+
+    std::string ReplaceConstraint(InputConstraint);
+
+    // If this input argument is tied to a larger output result, extend the
+    // input to be the same size as the output.  The LLVM backend wants to see
+    // the input and output of a matching constraint be the same size.  Note
+    // that GCC does not define what the top bits are here.  We use zext because
+    // that is usually cheaper, but LLVM IR should really get an anyext someday.
+    if (Info.hasTiedOperand()) {
+      unsigned Output = Info.getTiedOperand();
+
+      // Deal with the tied operands' constraint code in adjustInlineAsmType.
+      ReplaceConstraint = OutputConstraints[Output];
+    }
+
+    Constraints += InputConstraint;
+  } // iterate over input operands
+
+  Constraints += InOutConstraints;
 
   mlir::Type ResultType;
 
@@ -39,10 +249,10 @@ mlir::LogicalResult CIRGenFunction::buildAsmStmt(const AsmStmt &S) {
         builder.getCompleteStructTy(ResultRegTypes, sname, false, nullptr);
   }
 
-  AsmDialect AsmDialect = inferDialect(CGM, S);
+  AsmFlavor AsmFlavor = inferFlavor(CGM, S);
 
   builder.create<mlir::cir::InlineAsmOp>(getLoc(S.getAsmLoc()), ResultType,
-                                         AsmString, AsmDialect);
+                                         AsmString, Constraints, AsmFlavor);
 
   return mlir::success();
 }
