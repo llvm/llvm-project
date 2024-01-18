fadaafbf791d5fe78f6ac9ee3494b128339781ba
Zingam <zingam@outlook.com>
2024-01-18 09:47:40 +0200

    [libc++][memory] P2868R1 - Removing deprecated typedef `std::allocator::is_always_equal`
    Implements:
    - https://wg21.link/P2868R1
    - https://wg21.link/LWG3170

---
+++ libcxx/test/std/utilities/memory/default.allocator/allocator_types.deprecated_in_cxx23.verify.cpp
@@ -0,0 +1,44 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// REQUIRES: c++23
+
+// <memory>
+
+// template <class T>
+// class allocator
+// {
+// ...
+//     typedef true_type is_always_equal; // Deprecated in C++23, removed in C++26
+// ...
+// };
+
+#include <memory>
+
+template <typename T>
+void check() {
+  typedef std::allocator<char>::is_always_equal IAE; // expected-warning {{'is_always_equal' is deprecated}}
+}
+
+void test() {
+  check<char>();
+  check<char const>();
+  check<void>();
+}
+
+// void f() {
+//   {
+//     typedef std::allocator<char>::is_always_equal IAE; // _expected-warning {{'is_always_equal' is deprecated}}
+//   }
+//   {
+//     typedef std::allocator<char const>::is_always_equal IAE; // _expected-warning {{'is_always_equal' is deprecated}}
+//   }
+//   {
+//     typedef std::allocator<void>::is_always_equal IAE; // _expected-warning {{'is_always_equal' is deprecated}}
+//   }
+// }
