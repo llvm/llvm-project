c17c95c77cadd72aeb529d723b092c2b21ca79eb
Utkarsh Saxena <usx@google.com>
2024-01-18 11:56:55 +0100

    [coroutines][coro_lifetimebound] Detect lifetime issues with lambda captures (#77066)
    ### Problem
    
    ```cpp
    co_task<int> coro() {
        int a = 1;
        auto lamb = [a]() -> co_task<int> {
            co_return a; // 'a' in the lambda object dies after the iniital_suspend in the lambda coroutine.
        }();
        co_return co_await lamb;
    }
    ```
    [use-after-free](https://godbolt.org/z/GWPEovWWc)
    
    Lambda captures (even by value) are prone to use-after-free once the
    lambda object dies. In the above example, the lambda object appears only
    as a temporary in the call expression. It dies after the first
    suspension (`initial_suspend`) in the lambda.
    On resumption in `co_await lamb`, the lambda accesses `a` which is part
    of the already-dead lambda object.
    
    ---
    
    ### Solution
    
    This problem can be formulated by saying that the `this` parameter of
    the lambda call operator is a lifetimebound parameter. The lambda object
    argument should therefore live atleast as long as the return object.
    That said, this requirement does not hold if the lambda does not have a
    capture list. In principle, the coroutine frame still has a reference to
    a dead lambda object, but it is easy to see that the object would not be
    used in the lambda-coroutine body due to no capture list.
    
    It is safe to use this pattern inside a`co_await` expression due to the
    lifetime extension of temporaries. Example:
    
    ```cpp
    co_task<int> coro() {
        int a = 1;
        int res = co_await [a]() -> co_task<int> { co_return a; }();
        co_return res;
    }
    ```
    ---
    ### Background
    
    This came up in the discussion with seastar folks on
    [RFC](https://discourse.llvm.org/t/rfc-lifetime-bound-check-for-parameters-of-coroutines/74253/19?u=usx95).
    This is a fairly common pattern in continuation-style-passing (CSP)
    async programming involving futures and continuations. Document ["Lambda
    coroutine
    fiasco"](https://github.com/scylladb/seastar/blob/master/doc/lambda-coroutine-fiasco.md)
    by Seastar captures the problem.
    This pattern makes the migration from CSP-style async programming to
    coroutines very bugprone.
    
    
    Fixes https://github.com/llvm/llvm-project/issues/76995
    
    ---------
    
    Co-authored-by: Chuanqi Xu <yedeng.yd@linux.alibaba.com>

--- clang/lib/Sema/SemaDecl.cpp
+++ clang/lib/Sema/SemaDecl.cpp
@@ -15912,13 +15912,26 @@ static void diagnoseImplicitlyRetainedSelf(Sema &S) {
           << FixItHint::CreateInsertion(P.first, "self->");
 }
 
+static bool methodHasName(const FunctionDecl *FD, StringRef Name) {
+  return isa<CXXMethodDecl>(FD) && FD->param_empty() &&
+         FD->getDeclName().isIdentifier() && FD->getName().equals(Name);
+}
+
+bool Sema::CanBeGetReturnObject(const FunctionDecl *FD) {
+  return methodHasName(FD, "get_return_object");
+}
+
+bool Sema::CanBeGetReturnTypeOnAllocFailure(const FunctionDecl *FD) {
+  return FD->isStatic() &&
+         methodHasName(FD, "get_return_object_on_allocation_failure");
+}
+
 void Sema::CheckCoroutineWrapper(FunctionDecl *FD) {
   RecordDecl *RD = FD->getReturnType()->getAsRecordDecl();
   if (!RD || !RD->getUnderlyingDecl()->hasAttr<CoroReturnTypeAttr>())
     return;
-  // Allow `get_return_object()`.
-  if (FD->getDeclName().isIdentifier() &&
-      FD->getName().equals("get_return_object") && FD->param_empty())
+  // Allow some_promise_type::get_return_object().
+  if (CanBeGetReturnObject(FD) || CanBeGetReturnTypeOnAllocFailure(FD))
     return;
   if (!FD->hasAttr<CoroWrapperAttr>())
     Diag(FD->getLocation(), diag::err_coroutine_return_type) << RD;
