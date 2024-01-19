88bfc0bd6e39d3172e0697b4707b462c7b1762c1
Jeremy Morse <jeremy.morse@sony.com>
2024-01-19 15:49:31 +0000

    [DebugInfo][RemoveDIs] Adjust AMDGPU passes to work with DPValues
    This patch tweaks two AMDGPU passes to use iterators rather than
    instruction pointers for expressing an insertion point. This is needed to
    accurately support DPValues, the non-instruction storage object for
    debug-info.
    
    Two tests were sensitive to this change (variable assignments were being
    put in the wrong place), and I've added extra run-lines with the "try new
    debug-info..." flag. These get tested on our public buildbot to ensure
    they continue to work accurately.

--- llvm/lib/Target/AMDGPU/AMDGPULowerKernelArguments.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPULowerKernelArguments.cpp
@@ -106,7 +106,7 @@ static bool lowerKernelArguments(Function &F, const TargetMachine &TM) {
   LLVMContext &Ctx = F.getParent()->getContext();
   const DataLayout &DL = F.getParent()->getDataLayout();
   BasicBlock &EntryBlock = *F.begin();
-  IRBuilder<> Builder(&*getInsertPt(EntryBlock));
+  IRBuilder<> Builder(&EntryBlock, getInsertPt(EntryBlock));
 
   const Align KernArgBaseAlign(16); // FIXME: Increase if necessary
   const uint64_t BaseOffset = ST.getExplicitKernelArgOffset();
