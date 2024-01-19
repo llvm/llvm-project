c77cd418648fe1dc0362d21cef4747288c62f766
Simeon Krastnikov <simeon.krastnikov@imgtec.com>
2024-01-19 16:56:39 +0000

    fixup! [RISCV] Fix missing scaling by LMUL in cost model

--- llvm/lib/Target/RISCV/RISCVTargetTransformInfo.cpp
+++ llvm/lib/Target/RISCV/RISCVTargetTransformInfo.cpp
@@ -784,9 +784,8 @@ RISCVTTIImpl::getIntrinsicInstrCost(const IntrinsicCostAttributes &ICA,
   }
   // TODO: add more intrinsic
   case Intrinsic::experimental_stepvector: {
-    unsigned Cost = 1; // vid
     auto LT = getTypeLegalizationCost(RetTy);
-    return (Cost + (LT.first - 1)) * TLI->getLMULCost(LT.second);
+    return LT.first * TLI->getLMULCost(LT.second);
   }
   case Intrinsic::vp_rint: {
     // RISC-V target uses at least 5 instructions to lower rounding intrinsics.
