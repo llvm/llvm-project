f670112a591c83498e348f3ff2f73f02baf8e303
Aiden Grossman <agrossman154@yahoo.com>
2024-01-19 02:00:33 -0800

    [llvm-exegesis] Add support for validation counters (#76653)
    This patch adds support for validation counters. Validation counters can
    be used to measure events that occur during snippet execution like cache
    misses to ensure that certain assumed invariants about the benchmark
    actually hold. Validation counters are setup within a perf event group,
    so are turned on and off at exactly the same time as the "group leader"
    counter that measures the desired value.

--- llvm/tools/llvm-exegesis/lib/BenchmarkResult.h
+++ llvm/tools/llvm-exegesis/lib/BenchmarkResult.h
@@ -32,6 +32,8 @@ class Error;
 
 namespace exegesis {
 
+enum ValidationEvent { InstructionRetired };
+
 enum class BenchmarkPhaseSelectorE {
   PrepareSnippet,
   PrepareAndAssembleSnippet,
@@ -77,8 +79,10 @@ struct BenchmarkKey {
 
 struct BenchmarkMeasure {
   // A helper to create an unscaled BenchmarkMeasure.
-  static BenchmarkMeasure Create(std::string Key, double Value) {
-    return {Key, Value, Value};
+  static BenchmarkMeasure
+  Create(std::string Key, double Value,
+         std::map<ValidationEvent, int64_t> ValCounters) {
+    return {Key, Value, Value, ValCounters};
   }
   std::string Key;
   // This is the per-instruction value, i.e. measured quantity scaled per
@@ -87,6 +91,8 @@ struct BenchmarkMeasure {
   // This is the per-snippet value, i.e. measured quantity for one repetition of
   // the whole snippet.
   double PerSnippetValue;
+  // These are the validation counter values.
+  std::map<ValidationEvent, int64_t> ValidationCounters;
 };
 
 // The result of an instruction benchmark.
