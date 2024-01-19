9f1b87e02ea35881a651eec55080b320302d3fa7
Hui <hui.xie0621@gmail.com>
2024-01-19 19:05:02 +0000

    comments

--- libcxx/test/std/thread/thread.condition/thread.condition.condvarany/helpers.h
+++ libcxx/test/std/thread/thread.condition/thread.condition.condvarany/helpers.h
@@ -16,6 +16,18 @@
 
 #if TEST_STD_VER >= 17
 
+// wait_for and wait_until function can exit via
+// - predicate is true
+// - timeout
+// - stop_requested
+// The return value only tells if the predicate is true
+// when the function exits, but it does not tell whether
+// it is timeout or stop_requested.
+//
+// ElapsedTimeCheck would fail the test if a test takes
+// longer than a duration. This is useful because we can
+// ensure that the wait_{for, until} function does not
+// wait until the timeout
 struct ElapsedTimeCheck {
   ElapsedTimeCheck(std::chrono::steady_clock::duration timeoutDuration)
       : timeout_(std::chrono::steady_clock::now() + timeoutDuration) {}
