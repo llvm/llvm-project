6cc472d5a1214e847b7f3a5dce28d6bed86121ed
Bolshakov <bolsh.andrey@yandex.ru>
2024-01-21 16:19:51 +0300

    [c++20] P1907R1: Support for generalized non-type template arguments of scalar type.
    Previously committed as 9e08e51a20d0d2b1c5724bb17e969d036fced4cd, and
    reverted because a dependency commit was reverted, then committed again
    as 4b574008aef5a7235c1f894ab065fe300d26e786 and reverted again because
    "dependency commit" 5a391d38ac6c561ba908334d427f26124ed9132e was
    reverted. But it doesn't seem that 5a391d38ac6c was a real dependency
    for this.
    
    This commit incorporates 4b574008aef5a7235c1f894ab065fe300d26e786 and
    18e093faf726d15f210ab4917142beec51848258 by Richard Smith, with some
    minor fixes, most notably:
    
    - `UncommonValue` renamed to `StructuralValue`
    
    - `VK_PRValue` instead of `VK_RValue` as default kind in lvalue and
    member pointer handling branch
    in `BuildExpressionFromNonTypeTemplateArgumentValue`;
    
    - handling of `StructuralValue` in `IsTypeDeclaredInsideVisitor`;
    
    - filling in `SugaredConverted` along with `CanonicalConverted`
    parameter in `Sema::CheckTemplateArgument`;
    
    - minor cleanup
    in `TemplateInstantiator::transformNonTypeTemplateParmRef`;
    
    - `TemplateArgument` constructors refactored;
    
    - `ODRHash` calculation for `UncommonValue`;
    
    - USR generation for `UncommonValue`;
    
    - more correct MS compatibility mangling algorithm (tested on MSVC
    ver. 19.35; toolset ver. 143);
    
    - IR emitting fixed on using a subobject as a template argument when
    the corresponding template parameter is used in an lvalue context;
    
    - `noundef` attribute and opaque pointers in `template-arguments` test;
    
    - analysis for C++17 mode is turned off for templates
    in `warn-bool-conversion` test;
    in C++17 and C++20 mode, array reference used as a template argument
    of pointer type produces template argument of UncommonValue type, and
    `BuildExpressionFromNonTypeTemplateArgumentValue` makes
    `OpaqueValueExpr` for it, and `DiagnoseAlwaysNonNullPointer` cannot see
    through it; despite of "These cases should not warn" comment, I'm not
    sure about correct behavior; I'd expect a suggestion to replace `if` by
    `if constexpr`;
    
    - `temp.arg.nontype/p1.cpp` and `dr18xx.cpp` tests fixed.

--- clang/lib/Sema/TreeTransform.h
+++ clang/lib/Sema/TreeTransform.h
@@ -3908,6 +3908,7 @@ public:
     case TemplateArgument::Null:
     case TemplateArgument::Integral:
     case TemplateArgument::Declaration:
+    case TemplateArgument::StructuralValue:
     case TemplateArgument::Pack:
     case TemplateArgument::TemplateExpansion:
     case TemplateArgument::NullPtr:
@@ -4577,7 +4578,8 @@ bool TreeTransform<Derived>::TransformTemplateArgument(
 
   case TemplateArgument::Integral:
   case TemplateArgument::NullPtr:
-  case TemplateArgument::Declaration: {
+  case TemplateArgument::Declaration:
+  case TemplateArgument::StructuralValue: {
     // Transform a resolved template argument straight to a resolved template
     // argument. We get here when substituting into an already-substituted
     // template type argument during concept satisfaction checking.
@@ -4604,9 +4606,15 @@ bool TreeTransform<Derived>::TransformTemplateArgument(
     else if (Arg.getKind() == TemplateArgument::NullPtr)
       Output = TemplateArgumentLoc(TemplateArgument(NewT, /*IsNullPtr=*/true),
                                    TemplateArgumentLocInfo());
-    else
+    else if (Arg.getKind() == TemplateArgument::Declaration)
       Output = TemplateArgumentLoc(TemplateArgument(NewD, NewT),
                                    TemplateArgumentLocInfo());
+    else if (Arg.getKind() == TemplateArgument::StructuralValue)
+      Output = TemplateArgumentLoc(
+          TemplateArgument(getSema().Context, NewT, Arg.getAsStructuralValue()),
+          TemplateArgumentLocInfo());
+    else
+      llvm_unreachable("unexpected template argument kind");
 
     return false;
   }
