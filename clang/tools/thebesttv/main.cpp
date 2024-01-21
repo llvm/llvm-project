df6f998402deb90fb3a891a72f7f4a0e182a4f57
thebesttv <taojuntian@outlook.com>
2024-01-21 14:17:25 +0800

    Update FindVarVisitor and other things
    改得太多了，懒得想了……

--- clang/tools/thebesttv/main.cpp
+++ clang/tools/thebesttv/main.cpp
@@ -9,6 +9,8 @@
 #include "clang/Tooling/Tooling.h"
 #include "llvm/Support/CommandLine.h"
 
+#include <queue>
+
 using namespace clang;
 using namespace clang::tooling;
 using namespace llvm;
@@ -51,7 +53,7 @@ static cl::extrahelp MoreHelp("\nMore help text...\n");
 //                          Opts.ASTDumpFormat);
 // }
 
-void requireTrue(bool condition, std::string message) {
+void requireTrue(bool condition, std::string message = "") {
     if (!condition) {
         llvm::errs() << "requireTrue failed: " << message << "\n";
         exit(1);
@@ -121,35 +123,34 @@ class VarVisitor : public RecursiveASTVisitor<VarVisitor>,
     }
 };
 
+struct VarLocation {
+    std::string file; // absolute path
+    int line;
+    int column;
+
+    VarLocation(std::string file, int line, int column)
+        : file(file), line(line), column(column) {}
+
+    VarLocation(const FullSourceLoc &fullLoc) {
+        requireTrue(fullLoc.hasManager(), "no source manager!");
+        requireTrue(fullLoc.isValid(), "invalid location!");
+
+        file = fullLoc.getFileEntry()->tryGetRealPathName();
+        line = fullLoc.getLineNumber();
+        column = fullLoc.getColumnNumber();
+        requireTrue(!file.empty(), "empty file path!");
+    }
+
+    bool operator==(const VarLocation &other) const {
+        return file == other.file && line == other.line &&
+               column == other.column;
+    }
+};
+
 /**
  * Visit all DeclRefExprs and print their parents.
  */
 class FindVarVisitor : public RecursiveASTVisitor<FindVarVisitor> {
-  public:
-    struct VarLocation {
-        std::string file; // absolute path
-        int line;
-        int column;
-
-        VarLocation(std::string file, int line, int column)
-            : file(file), line(line), column(column) {}
-
-        VarLocation(const FullSourceLoc &fullLoc) {
-            requireTrue(fullLoc.hasManager(), "no source manager!");
-            requireTrue(fullLoc.isValid(), "invalid location!");
-
-            file = fullLoc.getFileEntry()->tryGetRealPathName();
-            line = fullLoc.getLineNumber();
-            column = fullLoc.getColumnNumber();
-            requireTrue(!file.empty(), "empty file path!");
-        }
-
-        bool operator==(const VarLocation &other) const {
-            return file == other.file && line == other.line &&
-                   column == other.column;
-        }
-    };
-
   private:
     ASTContext *Context;
     VarLocation targetLoc;
@@ -158,39 +159,210 @@ class FindVarVisitor : public RecursiveASTVisitor<FindVarVisitor> {
     explicit FindVarVisitor(ASTContext *Context, VarLocation targetLoc)
         : Context(Context), targetLoc(targetLoc) {}
 
-    void visitParents(const Stmt &base) {
-        const Stmt *s = &base;
-        llvm::errs() << "    parents:\n";
-        while (true) {
-            const auto &parents = Context->getParents(*s);
-            requireTrue(parents.size() == 1, "parent size is not 1");
+    template <typename NodeT> void visitParentsRecursively(const NodeT &base) {
+        const DynTypedNodeList &parents = Context->getParents(base);
+        requireTrue(parents.size() == 1, "parent size is not 1");
 
-            const Stmt *parent = parents.begin()->get<Stmt>();
-            requireTrue(parent != nullptr, "parent is null");
-
-            llvm::errs() << "      " << parent->getStmtClassName() << "\n";
-            if (isa<CompoundStmt>(parent)) {
-                break;
-            }
-
-            s = parent;
+        if (const Stmt *parent = parents.begin()->get<Stmt>()) {
+            llvm::errs() << "    " << parent->getStmtClassName() << "\n";
+            if (isa<CompoundStmt>(parent))
+                return;
+            visitParentsRecursively(*parent);
+        } else if (const Decl *parent = parents.begin()->get<Decl>()) {
+            llvm::errs() << "    " << parent->getDeclKindName() << "\n";
+            visitParentsRecursively(*parent);
+        } else {
+            llvm::errs() << "    unknown parent\n";
+            exit(1);
         }
     }
 
-    bool VisitStmt(Stmt *s) {
-        // DeclRefExpr
-        if (DeclRefExpr *dre = dyn_cast<DeclRefExpr>(s)) {
-            FullSourceLoc FullLocation = Context->getFullLoc(s->getBeginLoc());
-            VarLocation varLoc(FullLocation);
-            if (varLoc == targetLoc) {
-                const auto &var = dre->getDecl()->getQualifiedNameAsString();
-                llvm::errs() << "Found DeclRefExpr: " << var << "\n";
-                llvm::errs() << "  at " << varLoc.file << ":" << varLoc.line
-                             << ":" << varLoc.column << "\n";
-                visitParents(*s);
+    bool findMatch(const Stmt *S, const NamedDecl *decl,
+                   const SourceLocation &loc) {
+        FullSourceLoc FullLocation = Context->getFullLoc(loc);
+        VarLocation varLoc(FullLocation);
+        bool match = varLoc == targetLoc;
+        if (match) {
+            const auto &var = decl->getQualifiedNameAsString();
+            llvm::errs() << "Found VarDecl: " << var << "\n";
+            llvm::errs() << "  at " << varLoc.file << ":" << varLoc.line << ":"
+                         << varLoc.column << "\n";
+            llvm::errs() << "  parents:\n";
+            visitParentsRecursively(*S);
+        }
+
+        return match;
+    }
+
+    bool VisitDeclStmt(DeclStmt *ds) {
+        for (Decl *d : ds->decls()) {
+            if (VarDecl *vd = dyn_cast<VarDecl>(d)) {
+                findMatch(ds, vd, vd->getLocation());
             }
         }
         return true;
+    }
+
+    bool VisitDeclRefExpr(DeclRefExpr *dre) {
+        findMatch(dre, dre->getDecl(), dre->getBeginLoc());
+        return true;
+    }
+};
+
+struct Graph {
+    int n; // 0-indexed
+    std::vector<std::vector<int>> G;
+    std::vector<int> d;
+
+    const int INF = 0x3f3f3f3f;
+
+    struct Node {
+        int u, d;
+        bool operator<(const Node &b) const { return d > b.d; }
+    };
+
+    Graph(int n) : n(n) {
+        G.resize(n);
+        d.resize(n);
+    }
+
+    void addEdge(int u, int v) { G[u].push_back(v); }
+
+    void dij(int s) {
+        std::fill(d.begin(), d.end(), INF);
+        d[s] = 0;
+
+        std::priority_queue<Node> q;
+        q.push({s, 0});
+
+        while (!q.empty()) {
+            Node p = q.top();
+            q.pop();
+            int u = p.u;
+            if (p.d != d[u])
+                continue;
+            for (int v : G[u]) {
+                if (d[v] > d[u] + 1) {
+                    d[v] = d[u] + 1;
+                    q.push({v, d[v]});
+                }
+            }
+        }
+    }
+};
+
+struct BlockGraph {
+    const ASTContext &Context;
+    const CFG *cfg;
+    Graph g;
+
+    std::map<const Stmt *, int> blockIdOfStmt;
+
+    BlockGraph(const ASTContext &Context, const CFG *cfg)
+        : Context(Context), cfg(cfg), g(cfg->size()) {
+        for (auto BI = cfg->begin(); BI != cfg->end(); ++BI) {
+            const CFGBlock &B = **BI;
+
+            llvm::errs() << "Block " << B.getBlockID() << ":\n";
+
+            // map stmts to block ids
+            for (auto EI = B.begin(); EI != B.end(); ++EI) {
+                const CFGElement &E = *EI;
+                if (std::optional<CFGStmt> CS = E.getAs<CFGStmt>()) {
+                    const Stmt &S = *CS->getStmt();
+                    blockIdOfStmt[&S] = B.getBlockID();
+
+                    llvm::errs() << "  " << S.getStmtClassName() << " ("
+                                 << S.getID(Context) << ")\n";
+                }
+            }
+
+            llvm::errs() << "  successors:";
+            // add edges
+            for (auto SI = B.succ_begin(); SI != B.succ_end(); ++SI) {
+                const CFGBlock *Succ = *SI;
+                g.addEdge(B.getBlockID(), Succ->getBlockID());
+
+                llvm::errs() << " " << Succ->getBlockID();
+            }
+            llvm::errs() << "\n";
+        }
+    }
+};
+
+struct FunctionInfo {
+    const FunctionDecl *D;
+    std::string name;
+    std::string file;
+    int line;
+    int column;
+
+    const CFG *cfg;
+    BlockGraph *bg;
+
+    static FunctionInfo *fromDecl(FunctionDecl *D) {
+        // ensure that the function has a body
+        if (!D->hasBody())
+            return nullptr;
+
+        // get location
+        FullSourceLoc FullLocation =
+            D->getASTContext().getFullLoc(D->getBeginLoc());
+        if (FullLocation.isInvalid() || !FullLocation.hasManager())
+            return nullptr;
+
+        std::string name = D->getQualifiedNameAsString();
+        int line = FullLocation.getSpellingLineNumber();
+        int column = FullLocation.getSpellingColumnNumber();
+        StringRef file = FullLocation.getFileEntry()->tryGetRealPathName();
+        requireTrue(!file.empty());
+
+        // build CFG
+        const std::unique_ptr<CFG> cfg = CFG::buildCFG(
+            D, D->getBody(), &D->getASTContext(), CFG::BuildOptions());
+
+        // build graph for each CFGBlock
+        BlockGraph *bg = new BlockGraph(D->getASTContext(), cfg.get());
+
+        FunctionInfo *fi = new FunctionInfo();
+        fi->D = D;
+        fi->name = name;
+        fi->file = file;
+        fi->line = line;
+        fi->column = column;
+        fi->cfg = cfg.get();
+        fi->bg = bg;
+        return fi;
+    }
+};
+
+class FindPathVisitor : public RecursiveASTVisitor<FindPathVisitor> {
+  private:
+    ASTContext *Context;
+    std::set<const FunctionDecl *> functionDecls;
+    std::map<const FunctionDecl *, const FunctionInfo *> infoOfFunction;
+
+  public:
+    explicit FindPathVisitor(ASTContext *Context) : Context(Context) {}
+
+    bool VisitFunctionDecl(FunctionDecl *D) {
+
+        FunctionInfo *fi = FunctionInfo::fromDecl(D);
+        if (fi == nullptr)
+            return true;
+
+        functionDecls.insert(D);
+        infoOfFunction[D] = fi;
+
+        return true;
+    }
+
+    void collect(/**/) {
+        for (const FunctionDecl *D : functionDecls) {
+            llvm::errs() << "------ FunctionDecl: "
+                         << D->getQualifiedNameAsString() << "\n";
+            const FunctionInfo *fi = infoOfFunction[D];
+        }
     }
 };
 
@@ -299,14 +471,35 @@ class FindNamedClassConsumer : public clang::ASTConsumer {
         TUD->dump();
 
         // call different visitors
-        llvm::errs() << "\n--- FunctionDeclVisitor ---\n";
-        FunctionDeclVisitor(&Context).TraverseDecl(TUD);
+        // llvm::errs() << "\n--- FunctionDeclVisitor ---\n";
+        // FunctionDeclVisitor(&Context).TraverseDecl(TUD);
 
         llvm::errs() << "\n--- FindVarVisitor ---\n";
-        FindVarVisitor::VarLocation targetLoc = {
-            "/home/thebesttv/vul/llvm-project/graph-generation/test4.cpp", 23,
-            5};
+        VarLocation targetLoc(
+            "/home/thebesttv/vul/llvm-project/graph-generation/test4.cpp", 2,
+            7);
         FindVarVisitor(&Context, targetLoc).TraverseDecl(TUD);
+
+        targetLoc.line = 2;
+        targetLoc.column = 14;
+        FindVarVisitor(&Context, targetLoc).TraverseDecl(TUD);
+
+        targetLoc.line = 23;
+        targetLoc.column = 7;
+        FindVarVisitor(&Context, targetLoc).TraverseDecl(TUD);
+
+        targetLoc.line = 23;
+        targetLoc.column = 11;
+        FindVarVisitor(&Context, targetLoc).TraverseDecl(TUD);
+
+        targetLoc.line = 23;
+        targetLoc.column = 15;
+        FindVarVisitor(&Context, targetLoc).TraverseDecl(TUD);
+
+        llvm::errs() << "\n--- FindPathVisitor ---\n";
+        FindPathVisitor fpv(&Context);
+        fpv.TraverseDecl(TUD);
+        fpv.collect();
     }
 };
 
--- graph-generation/test4.cpp
+++ graph-generation/test4.cpp
@@ -1,26 +1,26 @@
 int loop(int n) {
-    int s = 0, x = 1;
-    for (int i = 0; i < n; i++) {
-        s = s + i;
-        if (i < 0) {
-            x *= i;
-            break;
-        } else if (i > 10) {
-            continue;
-        } else if (i > 20) {
-            x *= 2 * i;
-        } else {
-            x = x / 2 * i;
-        }
-        s += i;
+  int s = 0, x = 1;
+  for (int i = 0; i < n; i++) {
+    s = s + i;
+    if (i < 0) {
+      x *= i;
+      break;
+    } else if (i > 10) {
+      continue;
+    } else if (i > 20) {
+      x *= 2 * i;
+    } else {
+      x = x / 2 * i;
     }
-    s += x;
-    return s;
+    s += i;
+  }
+  s += x;
+  return s;
 }
 
 void foo() {
-    int x = 0;
-    x = x + 10;
-    int ret = -1;
-    ret = loop(x);
+  int x = 0;
+  int y = x + x;
+  int ret = -1;
+  ret = loop(x);
 }
