ec3682aa0214289e7226212e9831d0068f7e11bf
Qiongsi Wu <qwu@ibm.com>
2024-01-22 13:39:48 -0500

    Revise test so that we generate profile files instead of using an existing one. Test is turned off on Windows

--- compiler-rt/test/profile/instrprof-api.c
+++ compiler-rt/test/profile/instrprof-api.c
@@ -1,11 +1,21 @@
+// UNSUPPORTED: target={{.*windows.*}}
+// __llvm_orderfile_dump() is not supported on Windows.
+
 // Testing profile generate.
 // RUN: %clang_profgen %s -S -emit-llvm -o - | FileCheck %s --check-prefix=PROFGEN
 // RUN: %clang_pgogen %s -S -emit-llvm -o - | FileCheck %s --check-prefix=PROFGEN
 
-// Testing profile use.
-// RUN: %clang_pgouse=%S/Inputs/instrprof-api.c.profdata %s -S -emit-llvm -o - \
-// RUN:   | FileCheck %s --check-prefix=PROFUSE
-
+// Testing profile use. Generate some profile file first.
+// RUN: rm -rf rawprof.profraw
+// RUN: %clang_profgen -o %t1 %s
+// RUN: %run %t1
+// RUN: llvm-profdata merge -o %t1.profdata rawprof.profraw
+// RUN: %clang_profuse=%t1.profdata %s -S -emit-llvm -o - | FileCheck %s --check-prefix=PROFUSE
+// RUN: rm -rf rawprof.profraw
+// RUN: %clang_pgogen -o %t2 %s
+// RUN: %run %t2
+// RUN: llvm-profdata merge -o %t2.profdata rawprof.profraw
+// RUN: %clang_pgouse=%t2.profdata %s -S -emit-llvm -o - | FileCheck %s --check-prefix=PROFUSE
 #include "profile/instr_prof_interface.h"
 
 __attribute__((noinline)) int bar() { return 4; }
