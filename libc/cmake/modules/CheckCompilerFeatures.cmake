029bfd6329aee0b60761998986dadba38e1de6f3
lntue <35648136+lntue@users.noreply.github.com>
2024-01-22 19:27:27 -0500

    [libc] Replace -nostdlib++ flag when building with gcc and add placement new operator to HermeticTestUtils.cpp. (#78906)
    `-nostdlib++` is a clang-only flag. Replacing it with `-nostdlib` when
    building with gcc.

--- libc/cmake/modules/CheckCompilerFeatures.cmake
+++ libc/cmake/modules/CheckCompilerFeatures.cmake
@@ -62,3 +62,6 @@ message(STATUS "Compiler features available: ${AVAILABLE_COMPILER_FEATURES}")
 
 # clang-8+, gcc-12+
 check_cxx_compiler_flag("-ftrivial-auto-var-init=pattern" LIBC_CC_SUPPORTS_PATTERN_INIT)
+
+# clang-6+, gcc-13+
+check_cxx_compiler_flag("-nostdlib++" LIBC_CC_SUPPORTS_NOSTDLIBPP)
