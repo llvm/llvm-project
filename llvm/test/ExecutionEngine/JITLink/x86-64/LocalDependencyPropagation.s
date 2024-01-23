f6aa995bbfd430fd417b3e8afdea3ea4afbdf32d
Lang Hames <lhames@gmail.com>
2024-01-23 12:42:12 -0800

    [ORC] Merge MaterializationResponsibility notifyEmitted and addDependencies.
    Removes the MaterializationResponsibility::addDependencies and
    addDependenciesForAll methods, and transfers dependency registration to the
    notifyEmitted operation. The new dependency registration allows dependencies to
    be specified for arbitrary subsets of the MaterializationResponsibility's
    symbols (rather than just single symbols or all symbols) via an array of
    SymbolDependenceGroups (pairs of symbol sets and corresponding dependencies for
    that set).
    
    This patch aims to both improve emission performance and simplify dependence
    tracking. By eliminating some states (e.g. symbols having registered
    dependencies but not yet being resolved or emitted) we make some errors
    impossible by construction, and reduce the number of error cases that we need
    to check. NonOwningSymbolStringPtrs are used for dependence tracking under the
    session lock, which should reduce ref-counting operations, and intra-emit
    dependencies are resolved outside the session lock, which should provide better
    performance when JITing concurrently (since some dependence tracking can happen
    in parallel).
    
    The Orc C API is updated to account for this change, with the
    LLVMOrcMaterializationResponsibilityNotifyEmitted API being modified and the
    LLVMOrcMaterializationResponsibilityAddDependencies and
    LLVMOrcMaterializationResponsibilityAddDependenciesForAll operations being
    removed.

--- llvm/test/ExecutionEngine/JITLink/x86-64/LocalDependencyPropagation.s
+++ llvm/test/ExecutionEngine/JITLink/x86-64/LocalDependencyPropagation.s
@@ -7,8 +7,13 @@
 # symbols: _baz depends on _foo indirectly via the local symbol _bar. We expect
 # _baz to depend on _foo, and _foo on _external_func.
 
-# CHECK-DAG: In main adding dependencies for _foo: { (main, { _external_func }) }
-# CHECK-DAG: In main adding dependencies for _baz: { (main, { _foo }) }
+# CHECK: In main emitting {{.*}}_foo{{.*}}
+# CHECK-NEXT: Initial dependencies:
+# CHECK-DAG: Symbols: { _foo }, Dependencies: { (main, { _external_func }) }
+# CHECK-DAG: Symbols: { _baz }, Dependencies: { (main, { _foo }) }
+# CHECK: Simplified dependencies:
+# CHECK-DAG: Symbols: { _foo }, Dependencies: { (main, { _external_func }) }
+# CHECK-DAG: Symbols: { _baz }, Dependencies: { (main, { _external_func }) }
 
         .section	__TEXT,__text,regular,pure_instructions
 
