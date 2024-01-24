d09738ee4480bd45301dcc0a497b4eed9d68dbd3
Fangrui Song <i@maskray.me>
2024-01-23 21:44:19 -0800

    add REQUIRES since clang/test/CodeGen/RISCV doesn't require it
    Created using spr 1.3.4

--- clang/test/CodeGen/RISCV/tls-dialect.c
+++ clang/test/CodeGen/RISCV/tls-dialect.c
@@ -1,3 +1,4 @@
+// REQUIRES: riscv-registered-target
 /// cc1 -enable-tlsdesc (due to -mtls-dialect=desc) enables TLSDESC.
 // RUN: %clang_cc1 -triple riscv64 -S -mrelocation-model pic -pic-level 1 -enable-tlsdesc %s -o - | FileCheck %s --check-prefix=DESC
 // RUN: %clang_cc1 -triple riscv64 -S -mrelocation-model pic -pic-level 1 %s -o - | FileCheck %s --check-prefix=NODESC
