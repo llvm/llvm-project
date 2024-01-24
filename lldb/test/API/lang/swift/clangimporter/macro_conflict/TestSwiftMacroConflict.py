7f2402bc2a87be428962235aaffbd697d627cd0f
Kavon Farvardin <kfarvardin@apple.com>
2024-01-24 11:27:12 -0800

    disable test
    (cherry picked from commit ff5893b2e64757474fcd1875a9861834febdf81d)

--- lldb/test/API/lang/swift/clangimporter/macro_conflict/TestSwiftMacroConflict.py
+++ lldb/test/API/lang/swift/clangimporter/macro_conflict/TestSwiftMacroConflict.py
@@ -25,6 +25,7 @@ class TestSwiftMacroConflict(TestBase):
     # Don't run ClangImporter tests if Clangimporter is disabled.
     @skipIf(setting=('symbols.use-swift-clangimporter', 'false'))
     @skipIf(setting=('symbols.swift-precise-compiler-invocation', 'true'))
+    @skipIf(bugnumber="rdar://121539666")
     @skipUnlessDarwin
     @swiftTest
     def test(self):
@@ -60,6 +61,7 @@ class TestSwiftMacroConflict(TestBase):
 
     # Don't run ClangImporter tests if Clangimporter is disabled.
     @skipIf(setting=('symbols.use-swift-clangimporter', 'false'))
+    @skipIf(bugnumber="rdar://121539666")
     @skipUnlessDarwin
     @swiftTest
     def test_with_dwarfimporter(self):
