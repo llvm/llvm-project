650952c151a094aa19a8a1621f21d28851e30f51
Jeremy Morse <jeremy.morse@sony.com>
2024-01-24 16:27:20 +0000

    [DebugInfo][RemoveDIs] Don't pointlessly scan funcs for debug-info
    The utility functions this patch modifies are part of cleanly transitioning
    from a context where we use dbg.value intrinsics to one where we use
    DPValue objects to record debug-info, and back again. However, this is a
    waste of time in non-debug builds (i.e. no -g on the command line). We
    still have to call the function on all blocks though to set the
    IsNewDbgInfoFormat flag.
    
    To reduce the overhead of this, test whether there's any debug-info in the
    function by checking whether the function has a DISubprogram, and pass a
    flag down to the utility functions indicating whether they can skip the
    scan.

--- llvm/lib/IR/BasicBlock.cpp
+++ llvm/lib/IR/BasicBlock.cpp
@@ -60,12 +60,16 @@ DPMarker *BasicBlock::createMarker(InstListType::iterator It) {
   return DPM;
 }
 
-void BasicBlock::convertToNewDbgValues() {
+void BasicBlock::convertToNewDbgValues(bool HasNoDebugInfo) {
   // Is the command line option set?
   if (!UseNewDbgInfoFormat)
     return;
 
   IsNewDbgInfoFormat = true;
+
+  // If the caller knows there's no debug-info in this function, do nothing.
+  if (HasNoDebugInfo)
+    return;
 
   // Iterate over all instructions in the instruction list, collecting dbg.value
   // instructions and converting them to DPValues. Once we find a "real"
@@ -93,9 +97,13 @@ void BasicBlock::convertToNewDbgValues() {
   }
 }
 
-void BasicBlock::convertFromNewDbgValues() {
+void BasicBlock::convertFromNewDbgValues(bool HasNoDebugInfo) {
   invalidateOrders();
   IsNewDbgInfoFormat = false;
+
+  // If the caller knows there's no debug-info in this function, do nothing.
+  if (HasNoDebugInfo)
+    return;
 
   // Iterate over the block, finding instructions annotated with DPMarkers.
   // Convert any attached DPValues to dbg.values and insert ahead of the
