2dbfba570f2d061bbf5ab90351ddde1aa5d325b2
Kelbon Nik <kelbonage@gmail.com>
2024-01-24 20:19:20 +0400

    ?

--- libcxx/test/libcxx/diagnostics/format.lifetimebound_extensions.verify.cpp
+++ libcxx/test/libcxx/diagnostics/format.lifetimebound_extensions.verify.cpp
@@ -1,19 +1,14 @@
 // Check that format functions are marked [[clang::lifetimebound]] as a conforming extension
 
 // UNSUPPORTED: c++03, c++11, c++14, c++17
-
 #include <format>
-
 #include "test_macros.h"
-
 int j;
-
 auto test_format() {
   int i = 0;
   return std::make_format_args(
       j, i); // expected-warning {{address of stack memory associated with local variable 'i' returned}}
 }
-
 #ifndef TEST_HAS_NO_WIDE_CHARACTERS
 auto test_wformat() {
   int i = 0;
