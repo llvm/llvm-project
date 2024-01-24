605a997e3bcc9be0a44d00dfbbaff7ba937741c9
Adrian Prantl <aprantl@apple.com>
2024-01-24 12:42:59 -0800

    Remove obsolete calls to MemoryPressureDetected

--- lldb/test/API/lang/swift/clangimporter/macro_conflict/TestSwiftMacroConflict.py
+++ lldb/test/API/lang/swift/clangimporter/macro_conflict/TestSwiftMacroConflict.py
@@ -54,7 +54,6 @@ class TestSwiftMacroConflict(TestBase):
                         bar_value.GetError().Success())
 
         self.assertTrue(os.path.isdir(mod_cache), "module cache exists")
-        lldb.SBDebugger.MemoryPressureDetected()
 
     # Don't run ClangImporter tests if Clangimporter is disabled.
     @skipIf(setting=('symbols.use-swift-clangimporter', 'false'))
@@ -83,4 +82,3 @@ class TestSwiftMacroConflict(TestBase):
         process.Continue()
         self.expect("v foo", substrs=["42"])
         self.assertTrue(os.path.isdir(mod_cache), "module cache exists")
-        lldb.SBDebugger.MemoryPressureDetected()
