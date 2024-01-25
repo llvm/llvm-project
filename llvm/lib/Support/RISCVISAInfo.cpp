1a14c446dd800b1d79fed1735c48e392d06e495d
Wang Pengcheng <wangpengcheng.pp@bytedance.com>
2024-01-25 17:03:25 +0800

    [RISCV][MC] Add experimental support of Zaamo and Zalrsc
    `A` extension has been split into two parts: Zaamo (Atomic Memory
    Operations) and Zalrsc (Load-Reserved/Store-Conditional). See also
    https://github.com/riscv/riscv-zaamo-zalrsc.
    
    This patch adds the MC support.
    
    Reviewers: dtcxzyw, topperc, kito-cheng
    
    Reviewed By: topperc
    
    Pull Request: https://github.com/llvm/llvm-project/pull/78970

--- llvm/lib/Support/RISCVISAInfo.cpp
+++ llvm/lib/Support/RISCVISAInfo.cpp
@@ -190,8 +190,11 @@ static const RISCVSupportedExtension SupportedExtensions[] = {
 };
 
 // NOTE: This table should be sorted alphabetically by extension name.
+// clang-format off
 static const RISCVSupportedExtension SupportedExperimentalExtensions[] = {
+    {"zaamo", {0, 2}},
     {"zacas", {1, 0}},
+    {"zalrsc", {0, 2}},
 
     {"zcmop", {0, 2}},
 
@@ -209,6 +212,7 @@ static const RISCVSupportedExtension SupportedExperimentalExtensions[] = {
     {"zvfbfmin", {1, 0}},
     {"zvfbfwma", {1, 0}},
 };
+// clang-format on
 
 static void verifyTables() {
 #ifndef NDEBUG
