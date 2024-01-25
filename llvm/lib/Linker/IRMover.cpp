f36f4b71648e77f43c59abc9c575548e108c5b34
Jeremy Morse <jeremy.morse@sony.com>
2024-01-25 14:17:35 +0000

    Revert "[DebugInfo][RemoveDIs] Convert debug-info modes when loading bitcode (#78967)"
    This reverts commit be5d6f7875d955fa9a677595b5f3b717e0f74fc5.
    
    Numerous builders exploded from this X_X, for example
    
      https://lab.llvm.org/buildbot/#/builders/46/builds/62657

--- llvm/lib/Linker/IRMover.cpp
+++ llvm/lib/Linker/IRMover.cpp
@@ -694,7 +694,6 @@ Function *IRLinker::copyFunctionProto(const Function *SF) {
                              SF->getAddressSpace(), SF->getName(), &DstM);
   F->copyAttributesFrom(SF);
   F->setAttributes(mapAttributeTypes(F->getContext(), F->getAttributes()));
-  F->IsNewDbgInfoFormat = SF->IsNewDbgInfoFormat;
   return F;
 }
 
