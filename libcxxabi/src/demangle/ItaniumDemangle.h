ab10e59821a164a8d431c2af9034ec7cb67aae0f
Nico Weber <thakis@chromium.org>
2024-01-25 15:21:35 -0500

    clang-format

--- libcxxabi/src/demangle/ItaniumDemangle.h
+++ libcxxabi/src/demangle/ItaniumDemangle.h
@@ -39,7 +39,8 @@
 DEMANGLE_NAMESPACE_BEGIN
 
 template <class T, size_t N> class PODSmallVector {
-  static_assert(std::is_trivial<T>::value, "T is required to be a trivial type");
+  static_assert(std::is_trivial<T>::value,
+                "T is required to be a trivial type");
   T *First = nullptr;
   T *Last = nullptr;
   T *Cap = nullptr;
