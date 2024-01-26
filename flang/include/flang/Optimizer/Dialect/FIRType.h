a49f630cf6f12f3ca2d5814a581986140ee6e474
jeanPerier <jperier@nvidia.com>
2024-01-26 16:01:51 +0100

    [flang] Lower passing non assumed-rank/size to assumed-ranks (#79145)
    Start implementing assumed-rank support as described in
    https://github.com/llvm/llvm-project/blob/main/flang/docs/AssumedRank.md
    
    This commit holds the minimal support for lowering calls to procedure
    with assumed-rank arguments where the procedure implementation is done
    in C.
    
    The case for passing assumed-size to assumed-rank is left TODO since it
    will be done a change in assumed-size lowering that is better done in
    another patch.
    
    Care is taken to set the lower bounds to zero when passing non allocatable no pointer as descriptor
    to a BIND(C) procedure as required per 18.5.3 point 3. This was not done before while the requirements also applies to non assumed-rank descriptors. This change  required special attention with IGNORE_TKR(t) to avoid emitting invalid fir.rebox operations (the actual argument type must be used in this case as the output type). 
    
    Implementation of Fortran procedure with assumed-rank arguments is still
    TODO.

--- flang/include/flang/Optimizer/Dialect/FIRType.h
+++ flang/include/flang/Optimizer/Dialect/FIRType.h
@@ -46,6 +46,13 @@ public:
   /// Unwrap element type from fir.heap, fir.ptr and fir.array.
   mlir::Type unwrapInnerType() const;
 
+  /// Is this the box for an assumed rank?
+  bool isAssumedRank() const;
+
+  /// Return the same type, except for the shape, that is taken the shape
+  /// of shapeMold.
+  BaseBoxType getBoxTypeWithNewShape(mlir::Type shapeMold) const;
+
   /// Methods for support type inquiry through isa, cast, and dyn_cast.
   static bool classof(mlir::Type type);
 };
@@ -427,6 +434,13 @@ inline mlir::Type updateTypeForUnlimitedPolymorphic(mlir::Type ty) {
     return mlir::NoneType::get(ty.getContext());
   return ty;
 }
+
+/// Replace the element type of \p type by \p newElementType, preserving
+/// all other layers of the type (fir.ref/ptr/heap/array/box/class).
+/// If \p turnBoxIntoClass and the input is a fir.box, it will be turned into
+/// a fir.class in the result.
+mlir::Type changeElementType(mlir::Type type, mlir::Type newElementType,
+                             bool turnBoxIntoClass);
 
 /// Is `t` an address to fir.box or class type?
 inline bool isBoxAddress(mlir::Type t) {
