6efbe3a3e455fc230bc5d228d265ac9b818f2a7d
Hui <hui.xie0621@gmail.com>
2024-01-26 15:12:33 +0000

    [libc++][ranges] implement `std::ranges::zip_transform_view`

---
+++ libcxx/test/std/ranges/range.adaptors/range.zip.transform/cpo.pass.cpp
@@ -0,0 +1,101 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++03, c++11, c++14, c++17, c++20
+
+// std::views::zip_transform
+
+#include <ranges>
+
+#include <array>
+#include <algorithm>
+#include <cassert>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+
+struct NotMoveConstructible {
+  NotMoveConstructible()                       = default;
+  NotMoveConstructible(NotMoveConstructible&&) = delete;
+  int operator()() const { return 5; }
+};
+
+struct NotInvocable {};
+
+template <class... Args>
+struct Invocable {
+  int operator()(Args...) const { return 5; }
+};
+
+struct ReturnNotObject {
+  void operator()() const {}
+};
+
+static_assert(!std::is_invocable_v<decltype((std::views::zip_transform))>);
+static_assert(!std::is_invocable_v<decltype((std::views::zip_transform)), NotMoveConstructible>);
+static_assert(!std::is_invocable_v<decltype((std::views::zip_transform)), NotInvocable>);
+static_assert(std::is_invocable_v<decltype((std::views::zip_transform)), Invocable<>>);
+static_assert(!std::is_invocable_v<decltype((std::views::zip_transform)), ReturnNotObject>);
+
+static_assert(std::is_invocable_v<decltype((std::views::zip_transform)), //
+                                  Invocable<int>,                        //
+                                  std::ranges::iota_view<int, int>>);
+static_assert(!std::is_invocable_v<decltype((std::views::zip_transform)), //
+                                   Invocable<>,                           //
+                                   std::ranges::iota_view<int, int>>);
+static_assert(!std::is_invocable_v<decltype((std::views::zip_transform)),
+                                   Invocable<int>,
+                                   std::ranges::iota_view<int, int>,
+                                   std::ranges::iota_view<int, int>>);
+static_assert(std::is_invocable_v<decltype((std::views::zip_transform)),
+                                  Invocable<int, int>,
+                                  std::ranges::iota_view<int, int>,
+                                  std::ranges::iota_view<int, int>>);
+
+constexpr bool test() {
+  {
+    // zip_transform function with no ranges
+    auto v = std::views::zip_transform(Invocable<>{});
+    assert(std::ranges::empty(v));
+    static_assert(std::is_same_v<decltype(v), std::ranges::empty_view<int>>);
+  }
+
+  {
+    // zip_transform views
+    int buffer1[] = {1, 2, 3, 4, 5, 6, 7, 8};
+    int buffer2[] = {9, 10, 11, 12};
+    auto view1    = std::views::all(buffer1);
+    auto view2    = std::views::all(buffer2);
+    std::same_as<std::ranges::zip_transform_view<std::plus<>, decltype(view1), decltype(view2)>> decltype(auto) v =
+        std::views::zip_transform(std::plus{}, buffer1, buffer2);
+    assert(std::ranges::size(v) == 4);
+    auto expected = {10, 12, 14, 16};
+    assert(std::ranges::equal(v, expected));
+    static_assert(std::is_same_v<std::ranges::range_reference_t<decltype(v)>, int>);
+  }
+
+  {
+    // zip_transform a viewable range
+    std::array a{1, 2, 3};
+    auto id = [](auto& x) -> decltype(auto) { return (x); };
+    std::same_as<
+        std::ranges::zip_transform_view<decltype(id), std::ranges::ref_view<std::array<int, 3>>>> decltype(auto) v =
+        std::views::zip_transform(id, a);
+    assert(&v[0] == &a[0]);
+    static_assert(std::is_same_v<std::ranges::range_reference_t<decltype(v)>, int&>);
+  }
+
+  return true;
+}
+
+int main(int, char**) {
+  test();
+  static_assert(test());
+
+  return 0;
+}
---
+++ libcxx/test/std/ranges/range.adaptors/range.zip.transform/size.pass.cpp
@@ -0,0 +1,92 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++03, c++11, c++14, c++17, c++20
+
+// constexpr auto size() requires sized_range<InnerView>
+// constexpr auto size() const requires sized_range<const InnerView>
+
+#include <ranges>
+
+#include <cassert>
+#include <tuple>
+#include <utility>
+
+#include "test_iterators.h"
+#include "types.h"
+
+int buffer[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+struct SizedView : std::ranges::view_base {
+  std::size_t size_ = 0;
+  constexpr SizedView(std::size_t s) : size_(s) {}
+  constexpr auto begin() const { return buffer; }
+  constexpr auto end() const { return buffer + size_; }
+};
+
+struct SizedNonConst : std::ranges::view_base {
+  using iterator    = forward_iterator<int*>;
+  std::size_t size_ = 0;
+  constexpr SizedNonConst(std::size_t s) : size_(s) {}
+  constexpr auto begin() const { return iterator{buffer}; }
+  constexpr auto end() const { return iterator{buffer + size_}; }
+  constexpr std::size_t size() { return size_; }
+};
+
+struct ConstNonConstDifferentSize : std::ranges::view_base {
+  constexpr auto begin() const { return buffer; }
+  constexpr auto end() const { return buffer + 8; }
+
+  constexpr auto size() { return 5; }
+  constexpr auto size() const { return 6; }
+};
+
+constexpr bool test() {
+  {
+    // single range
+    std::ranges::zip_transform_view v(MakeTuple{}, SizedView(8));
+    assert(v.size() == 8);
+    assert(std::as_const(v).size() == 8);
+  }
+
+  {
+    // multiple ranges
+    std::ranges::zip_transform_view v(MakeTuple{}, SizedView(2), SizedView(3));
+    assert(v.size() == 2);
+    assert(std::as_const(v).size() == 2);
+  }
+
+  {
+    // const-view non-sized range
+    std::ranges::zip_transform_view v(MakeTuple{}, SizedNonConst(2), SizedView(3));
+    assert(v.size() == 2);
+    static_assert(std::ranges::sized_range<decltype(v)>);
+    static_assert(!std::ranges::sized_range<decltype(std::as_const(v))>);
+  }
+
+  {
+    // const/non-const has different sizes
+    std::ranges::zip_transform_view v(MakeTuple{}, ConstNonConstDifferentSize{});
+    assert(v.size() == 5);
+    assert(std::as_const(v).size() == 6);
+  }
+
+  {
+    // underlying range not sized
+    std::ranges::zip_transform_view v(MakeTuple{}, InputCommonView{buffer});
+    static_assert(!std::ranges::sized_range<decltype(v)>);
+    static_assert(!std::ranges::sized_range<decltype(std::as_const(v))>);
+  }
+  return true;
+}
+
+int main(int, char**) {
+  test();
+  static_assert(test());
+
+  return 0;
+}
