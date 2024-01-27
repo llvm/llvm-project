710e0706ccf3c6ee8e7fe58e83459a0127927cfa
Florian Hahn <flo@fhahn.com>
2024-01-27 13:30:52 +0000

    [VPlan] Implement cloning of VPlans. (#73158)
    This patch implements cloning for VPlans and recipes. Cloning is used in
    the epilogue vectorization path, to clone the VPlan for the main vector
    loop. This means we won't re-use a VPlan when executing the VPlan for
    the epilogue vector loop, which in turn will enable us to perform
    optimizations based on UF & VF.

--- llvm/lib/Transforms/Vectorize/LoopVectorize.cpp
+++ llvm/lib/Transforms/Vectorize/LoopVectorize.cpp
@@ -10141,7 +10141,7 @@ bool LoopVectorizePass::processLoop(Loop *L) {
         EpilogueVectorizerMainLoop MainILV(L, PSE, LI, DT, TLI, TTI, AC, ORE,
                                            EPI, &LVL, &CM, BFI, PSI, Checks);
 
-        VPlan &BestMainPlan = LVP.getBestPlanFor(EPI.MainLoopVF);
+        VPlan &BestMainPlan = *LVP.getBestPlanFor(EPI.MainLoopVF).duplicate();
         const auto &[ExpandedSCEVs, ReductionResumeValues] = LVP.executePlan(
             EPI.MainLoopVF, EPI.MainLoopUF, BestMainPlan, MainILV, DT, true);
         ++LoopsVectorized;
