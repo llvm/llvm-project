e7f695603e75a40e5a9a9d3aea50eec437e6a07a
Nick Desaulniers <ndesaulniers@google.com>
2024-01-29 09:18:07 -0800

    fix formatting

--- libc/test/src/stdbit/stdc_leading_zeros_ull_test.cpp
+++ libc/test/src/stdbit/stdc_leading_zeros_ull_test.cpp
@@ -1,5 +1,4 @@
-//===-- Unittests for stdc_leading_zeros_ull
-//-------------------------------===//
+//===-- Unittests for stdc_leading_zeros_ull ------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
