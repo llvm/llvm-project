418938eaa966b346ff49bcf1fb1f2ba7e25db412
Zaara Syeda <syzaara@cpap8104.rtp.raleigh.ibm.com>
2024-01-29 16:10:18 -0500

    Format code

--- llvm/lib/CodeGen/TargetLoweringObjectFileImpl.cpp
+++ llvm/lib/CodeGen/TargetLoweringObjectFileImpl.cpp
@@ -2424,7 +2424,8 @@ MCSection *TargetLoweringObjectFileXCOFF::SelectSectionForGlobal(
     if (GVar->hasAttribute("toc-data")) {
       SmallString<128> Name;
       getNameWithPrefix(Name, GO, TM);
-      XCOFF::SymbolType symType = GO->hasCommonLinkage() ? XCOFF::XTY_CM : XCOFF::XTY_SD;
+      XCOFF::SymbolType symType =
+          GO->hasCommonLinkage() ? XCOFF::XTY_CM : XCOFF::XTY_SD;
       return getContext().getXCOFFSection(
           Name, Kind, XCOFF::CsectProperties(XCOFF::XMC_TD, symType),
           /* MultiSymbolsAllowed*/ true);
