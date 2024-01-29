dff1c5bae77f10fcab4fc15abb0f5c8a168c1f42
Wang, Xin10 <xin10.wang@intel.com>
2024-01-28 18:16:20 -0800

    update test

--- llvm/test/MC/X86/apx/imul-reloc.s
+++ llvm/test/MC/X86/apx/imul-reloc.s
@@ -2,17 +2,17 @@
 
 // CHECK:      Relocations [
 // CHECK-NEXT:   Section ({{[0-9]+}}) .rela.text {
-{evex}  subw $foo, %ax, %ax                 // CHECK-NEXT:     R_X86_64_16
-{nf}    subw $foo, %ax, %ax                 // CHECK-NEXT:     R_X86_64_16
-{evex}  subw $foo, 123(%r8,%rax,4), %ax     // CHECK-NEXT:     R_X86_64_16
-{nf}    subw $foo, 123(%r8,%rax,4), %ax     // CHECK-NEXT:     R_X86_64_16
-{evex}  subl $foo, %eax, %eax               // CHECK-NEXT:     R_X86_64_32
-{nf}    subl $foo, %eax, %eax               // CHECK-NEXT:     R_X86_64_32
-{evex}  subl $foo, 123(%r8,%rax,4), %eax    // CHECK-NEXT:     R_X86_64_32
-{nf}    subl $foo, 123(%r8,%rax,4), %eax    // CHECK-NEXT:     R_X86_64_32
-{evex}  subq $foo, %rax, %rax               // CHECK-NEXT:     R_X86_64_32S
-{nf}    subq $foo, %rax, %rax               // CHECK-NEXT:     R_X86_64_32S
-{evex}  subq $foo, 123(%r8,%rax,4), %rax    // CHECK-NEXT:     R_X86_64_32S
-{nf}    subq $foo, 123(%r8,%rax,4), %rax    // CHECK-NEXT:     R_X86_64_32S
+{evex}  imulw $foo, %ax, %ax                 // CHECK-NEXT:     R_X86_64_16
+{nf}    imulw $foo, %ax, %ax                 // CHECK-NEXT:     R_X86_64_16
+{evex}  imulw $foo, 123(%r8,%rax,4), %ax     // CHECK-NEXT:     R_X86_64_16
+{nf}    imulw $foo, 123(%r8,%rax,4), %ax     // CHECK-NEXT:     R_X86_64_16
+{evex}  imull $foo, %eax, %eax               // CHECK-NEXT:     R_X86_64_32
+{nf}    imull $foo, %eax, %eax               // CHECK-NEXT:     R_X86_64_32
+{evex}  imull $foo, 123(%r8,%rax,4), %eax    // CHECK-NEXT:     R_X86_64_32
+{nf}    imull $foo, 123(%r8,%rax,4), %eax    // CHECK-NEXT:     R_X86_64_32
+{evex}  imulq $foo, %rax, %rax               // CHECK-NEXT:     R_X86_64_32S
+{nf}    imulq $foo, %rax, %rax               // CHECK-NEXT:     R_X86_64_32S
+{evex}  imulq $foo, 123(%r8,%rax,4), %rax    // CHECK-NEXT:     R_X86_64_32S
+{nf}    imulq $foo, 123(%r8,%rax,4), %rax    // CHECK-NEXT:     R_X86_64_32S
 // CHECK-NEXT:   }
 // CHECK-NEXT: ]
