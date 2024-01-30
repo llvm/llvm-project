0fa3f98c765abe984fec7dd003dc6de753d49bfa
Eduard Zingerman <eddyz87@gmail.com>
2024-01-30 22:20:43 +0200

    [BPF] Merge global variables with address space to .arena.N section
    Make it so that all globals within same address space reside in
    section with name ".arena.N", where N is number of the address space.
    
    E.g. for the following C program:
    
    ```c
    __as const char a[2] = {1,2};
    __as char b[2] = {3,4};
    __as char c[2];
    ...
    ```
    
    Generate the following layout:
    
    ```
    $ clang -O2 --target=bpf t.c -c -o - \
      | llvm-readelf --sections --symbols -
    ...
    Section Headers:
      [Nr] Name              Type            Address          Off    Size   ES Flg Lk Inf Al
      ...
      [ 4] .arena.272        PROGBITS        0000000000000000 0000e8 000018 00  WA  0   0  4
      ...
    
    Symbol table '.symtab' contains 8 entries:
       Num:    Value          Size Type    Bind   Vis       Ndx Name
         ...
         3: 0000000000000000     8 OBJECT  GLOBAL DEFAULT     4 a
         4: 0000000000000008     8 OBJECT  GLOBAL DEFAULT     4 b
         5: 0000000000000010     8 OBJECT  GLOBAL DEFAULT     4 c
         ...                                                 ^^^
                                                      Note section index
    ```

--- llvm/lib/Target/BPF/BPFCheckAndAdjustIR.cpp
+++ llvm/lib/Target/BPF/BPFCheckAndAdjustIR.cpp
@@ -516,6 +516,18 @@ bool BPFCheckAndAdjustIR::insertASpaceCasts(Module &M) {
     }
     Changed |= !CastsCache.empty();
   }
+  // Merge all globals within same address space into single
+  // .arena.<addr space no> section
+  for (GlobalVariable &G : M.globals()) {
+    if (G.getAddressSpace() == 0 || G.hasSection())
+      continue;
+    SmallString<16> SecName;
+    raw_svector_ostream OS(SecName);
+    OS << ".arena." << G.getAddressSpace();
+    G.setSection(SecName);
+    // Prevent having separate section for constants
+    G.setConstant(false);
+  }
   return Changed;
 }
 
