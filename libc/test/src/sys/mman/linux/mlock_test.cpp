039b3788680cc4ab8287ff2009aca3e83a02ab5a
Schrodinger ZHU Yifan <yifanzhu@rochester.edu>
2024-01-30 13:57:45 -0500

    adopt invalid flag tests Co-authored-by: Sirui Mu <msrlancern@gmail.com>

--- libc/test/src/sys/mman/linux/mlock_test.cpp
+++ libc/test/src/sys/mman/linux/mlock_test.cpp
@@ -118,6 +118,24 @@ TEST(LlvmLibcMlockTest, MLock2) {
 }
 #endif
 
+TEST(LlvmLibcMlockTest, InvalidFlag) {
+  size_t alloc_size = 128; // page size
+  libc_errno = 0;
+  void *addr = LIBC_NAMESPACE::mmap(nullptr, alloc_size, PROT_READ,
+                                    MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+  EXPECT_EQ(0, libc_errno);
+  EXPECT_NE(addr, MAP_FAILED);
+
+  // Invalid mlock2 flags.
+  EXPECT_THAT(LIBC_NAMESPACE::mlock2(addr, alloc_size, 1234), Fails(EINVAL));
+
+  // Invalid mlockall flags.
+  EXPECT_THAT(LIBC_NAMESPACE::mlockall(1234), Fails(EINVAL));
+  EXPECT_THAT(LIBC_NAMESPACE::mlockall(MCL_ONFAULT), Fails(EINVAL));
+
+  LIBC_NAMESPACE::munmap(addr, alloc_size);
+}
+
 TEST(LlvmLibcMlockTest, MLockAll) {
   {
     PageHolder holder;
