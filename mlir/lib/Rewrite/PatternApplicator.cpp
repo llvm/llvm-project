3ecf2c4fa377d9d4edad1b8e219766b66002fa1a
Robert Konicar <xkonicar@fi.muni.cz>
2024-01-30 16:12:03 +0100

    [mlir] Fix debug output for passes that modify top-level operation.
    Make it so that when the top-level (root) operation itself is being
    modified, it is also used as the root for debug output in
    PatternApplicator.

--- mlir/lib/Rewrite/PatternApplicator.cpp
+++ mlir/lib/Rewrite/PatternApplicator.cpp
@@ -40,7 +40,9 @@ static void logImpossibleToMatch(const Pattern &pattern) {
 
 /// Log IR after pattern application.
 static Operation *getDumpRootOp(Operation *op) {
-  return op->getParentWithTrait<mlir::OpTrait::IsIsolatedFromAbove>();
+  return op->hasTrait<mlir::OpTrait::IsIsolatedFromAbove>()
+             ? op
+             : op->getParentWithTrait<mlir::OpTrait::IsIsolatedFromAbove>();
 }
 static void logSucessfulPatternApplication(Operation *op) {
   llvm::dbgs() << "// *** IR Dump After Pattern Application ***\n";
