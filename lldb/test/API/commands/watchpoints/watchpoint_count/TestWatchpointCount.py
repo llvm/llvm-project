cc4af03a839293ff908b52bb3a79b6ed562fe198
Jason Molenda <jason@molenda.com>
2024-01-31 12:22:38 -0800

    Revert "Add extra printing to TestWatchpointCount.py to debug CI fail"
    This reverts commit dad50fefa24e717927a4d3315ae70fe507d624e0.

--- lldb/test/API/commands/watchpoints/watchpoint_count/TestWatchpointCount.py
+++ lldb/test/API/commands/watchpoints/watchpoint_count/TestWatchpointCount.py
@@ -29,12 +29,6 @@ class TestWatchpointCount(TestBase):
         second_watch = second_var.Watch(True, False, True, error)
         if not error.Success():
             self.fail("Failed to make watchpoint for x2: %s" % (error.GetCString()))
-        # LWP_TODO: Adding temporary prints to debug a test
-        # failure on the x86-64 Debian bot.
-        self.runCmd("p &x1")
-        self.runCmd("p &x2")
-        self.runCmd("watchpoint list")
-
         process.Continue()
 
         stop_reason = thread.GetStopReason()
@@ -45,11 +39,6 @@ class TestWatchpointCount(TestBase):
         self.assertEqual(stop_reason_descr, "watchpoint 1")
 
         process.Continue()
-        # LWP_TODO: Adding temporary prints to debug a test
-        # failure on the x86-64 Debian bot.
-        self.runCmd("frame select 0")
-        self.runCmd("disassemble")
-
         stop_reason = thread.GetStopReason()
         self.assertStopReason(
             stop_reason, lldb.eStopReasonWatchpoint, "watchpoint for x2 not hit"
