64912cb60bbbb6a87ce5962083352f5e8df519ed
Nick Desaulniers <ndesaulniers@google.com>
2024-01-31 15:51:41 -0800

    fix copy pasta

--- libc/src/stdbit/stdc_leading_ones_us.h
+++ libc/src/stdbit/stdc_leading_ones_us.h
@@ -1,4 +1,4 @@
-//===-- Implementation header for stdc_leading_ones_uc ----------*- C++ -*-===//
+//===-- Implementation header for stdc_leading_ones_us ----------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
