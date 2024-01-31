3d108c3e30aa7b63dbfbaff76f7309e5b9e0d4f3
Piotr Zegar <me@piotrzegar.pl>
2024-01-31 17:57:55 +0000

    Formating

--- clang-tools-extra/clang-tidy/modernize/AvoidCArraysCheck.cpp
+++ clang-tools-extra/clang-tidy/modernize/AvoidCArraysCheck.cpp
@@ -53,8 +53,10 @@ void AvoidCArraysCheck::registerMatchers(MatchFinder *Finder) {
       anything();
   if (AllowStringArrays)
     IgnoreStringArrayIfNeededMatcher =
-        unless(typeLoc(loc(hasCanonicalType(incompleteArrayType(hasElementType(isAnyCharacter())))),
-                       hasParent(varDecl(hasInitializer(stringLiteral()), unless(parmVarDecl())))));
+        unless(typeLoc(loc(hasCanonicalType(incompleteArrayType(
+                           hasElementType(isAnyCharacter())))),
+                       hasParent(varDecl(hasInitializer(stringLiteral()),
+                                         unless(parmVarDecl())))));
 
   Finder->addMatcher(
       typeLoc(hasValidBeginLoc(), hasType(arrayType()),
