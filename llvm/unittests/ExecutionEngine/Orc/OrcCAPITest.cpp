2739549b217cfd018ace2ed9c7fc5c8ee97fd882
Lang Hames <lhames@gmail.com>
2024-01-31 19:11:22 +0000

    Remove designated initializers to appease MSVC

--- llvm/unittests/ExecutionEngine/Orc/OrcCAPITest.cpp
+++ llvm/unittests/ExecutionEngine/Orc/OrcCAPITest.cpp
@@ -692,9 +692,9 @@ void Materialize(void *Ctx, LLVMOrcMaterializationResponsibilityRef MR) {
   LLVMOrcRetainSymbolStringPoolEntry(TargetSym.Name);
   LLVMOrcCDependenceMapPair Dependency = {JD, {&DependencySymbol, 1}};
   LLVMOrcCSymbolDependenceGroup DependenceSet = {
-      .Symbols = {.Symbols = &TargetSym.Name, .Length = 1},
-      .Dependencies = &Dependency,
-      .NumDependencies = 1};
+      /*.Symbols = */ { /*.Symbols = */ &TargetSym.Name, /* .Length */ = 1},
+      /* .Dependencies = */ &Dependency,
+      /* .NumDependencies = */ 1};
 
   LLVMOrcMaterializationResponsibilityNotifyEmitted(MR, &DependenceSet, 1);
   LLVMOrcDisposeMaterializationResponsibility(MR);
