ddab122b7e2c18948d1daf229f9a34481695508b
Emilia Kond <emilia@rymiel.space>
2024-02-01 08:00:45 +0200

    [clang-format] Allow decltype in requires clause (#78847)
    If clang-format is not sure whether a `requires` keyword starts a
    requires clause or a requires expression, it looks ahead to see if any
    token disqualifies it from being a requires clause. Among these tokens
    was `decltype`, since it fell through the switch.
    
    This patch allows decltype to exist in a require clause.
    
    I'm not 100% sure this change won't have repercussions, but that just
    means we need more test coverage!
    
    Fixes https://github.com/llvm/llvm-project/issues/78645

--- clang/lib/Format/UnwrappedLineParser.cpp
+++ clang/lib/Format/UnwrappedLineParser.cpp
@@ -3446,11 +3446,6 @@ bool clang::format::UnwrappedLineParser::parseRequires() {
         return false;
       }
       break;
-    case tok::r_paren:
-    case tok::pipepipe:
-      FormatTok = Tokens->setPosition(StoredPosition);
-      parseRequiresClause(RequiresToken);
-      return true;
     case tok::eof:
       // Break out of the loop.
       Lookahead = 50;
@@ -3458,6 +3453,7 @@ bool clang::format::UnwrappedLineParser::parseRequires() {
     case tok::coloncolon:
       LastWasColonColon = true;
       break;
+    case tok::kw_decltype:
     case tok::identifier:
       if (FoundType && !LastWasColonColon && OpenAngles == 0) {
         FormatTok = Tokens->setPosition(StoredPosition);
