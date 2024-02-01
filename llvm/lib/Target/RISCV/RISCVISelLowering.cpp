0a934f36df24d37c2b8cdcd96e6a08cc01611dac
Fangrui Song <i@maskray.me>
2024-02-01 10:18:42 -0800

    [RISCV] Support constraint "s" (#80201)
    GCC has supported a generic constraint "s" for a long time (since at
    least 1992), which references a symbol or label with an optional
    constant offset. "i" is a superset that also supports a constant
    integer.
    
    GCC's RISC-V port also supports a machine-specific constraint "S",
    which cannot be used with a preemptible symbol. (We don't bother to
    check preemptibility.) In PIC code, an external symbol is preemptible by
    default, making "S" less useful if you want to create an artificial
    reference for linker garbage collection, or define sections to hold
    symbol addresses:
    
    ```
    void fun();
    // error: impossible constraint in ‘asm’ for riscv64-linux-gnu-gcc -fpie/-fpic
    void foo() { asm(".reloc ., BFD_RELOC_NONE, %0" :: "S"(fun)); }
    // good even if -fpie/-fpic
    void foo() { asm(".reloc ., BFD_RELOC_NONE, %0" :: "s"(fun)); }
    ```
    
    This patch adds support for "s". Modify https://reviews.llvm.org/D105254
    ("S") to handle multi-depth GEPs (https://reviews.llvm.org/D61560).

--- llvm/lib/Target/RISCV/RISCVISelLowering.cpp
+++ llvm/lib/Target/RISCV/RISCVISelLowering.cpp
@@ -19188,6 +19188,7 @@ RISCVTargetLowering::getConstraintType(StringRef Constraint) const {
       return C_Immediate;
     case 'A':
       return C_Memory;
+    case 's':
     case 'S': // A symbolic address
       return C_Other;
     }
@@ -19449,13 +19450,7 @@ void RISCVTargetLowering::LowerAsmOperandForConstraint(
       }
       return;
     case 'S':
-      if (const auto *GA = dyn_cast<GlobalAddressSDNode>(Op)) {
-        Ops.push_back(DAG.getTargetGlobalAddress(GA->getGlobal(), SDLoc(Op),
-                                                 GA->getValueType(0)));
-      } else if (const auto *BA = dyn_cast<BlockAddressSDNode>(Op)) {
-        Ops.push_back(DAG.getTargetBlockAddress(BA->getBlockAddress(),
-                                                BA->getValueType(0)));
-      }
+      TargetLowering::LowerAsmOperandForConstraint(Op, "s", Ops, DAG);
       return;
     default:
       break;
