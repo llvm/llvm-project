656fc921a10696b1f65ae5fb458cc8db3b25e004
Yitzhak Mandelbaum <ymand@users.noreply.github.com>
2024-02-02 08:41:46 -0500

    Update clang/include/clang/Analysis/FlowSensitive/DataflowEnvironment.h
    Co-authored-by: Dmitri Gribenko <gribozavr@gmail.com>

--- clang/include/clang/Analysis/FlowSensitive/DataflowEnvironment.h
+++ clang/include/clang/Analysis/FlowSensitive/DataflowEnvironment.h
@@ -122,8 +122,9 @@ public:
     virtual void join(QualType Type, const Value &Val1, const Environment &Env1,
                       const Value &Val2, const Environment &Env2,
                       Value &JoinedVal, Environment &JoinedEnv) {
-      assert(merge(Type, Val1, Env1, Val2, Env2, JoinedVal, JoinedEnv) &&
-             "dropping merged value is unsupported");
+      bool ShouldKeep = merge(Type, Val1, Env1, Val2, Env2, JoinedVal, JoinedEnv);
+      assert(ShouldKeep && "dropping merged value is unsupported");
+      (void)ShouldKeep;
     }
 
     /// This function may widen the current value -- replace it with an
