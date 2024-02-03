48f6b155e6b87dd03e48463832e6cf2dbc62481b
Matt Arsenault <Matthew.Arsenault@amd.com>
2024-02-03 21:25:33 +0530

    AMDGPU: Add v_smfmac_f32_32x32x32x64_fp8_bf8 for gfx950
    Change-Id: I597369fb9c3c9760c370b7433e7668a857e93cbf

--- llvm/lib/Target/AMDGPU/AMDGPURegisterBankInfo.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPURegisterBankInfo.cpp
@@ -4802,7 +4802,8 @@ AMDGPURegisterBankInfo::getInstrMapping(const MachineInstr &MI) const {
     case Intrinsic::amdgcn_smfmac_f32_16x16x128_fp8_bf8:
     case Intrinsic::amdgcn_smfmac_f32_16x16x128_fp8_fp8:
     case Intrinsic::amdgcn_smfmac_f32_32x32x64_bf8_bf8:
-    case Intrinsic::amdgcn_smfmac_f32_32x32x64_bf8_fp8: {
+    case Intrinsic::amdgcn_smfmac_f32_32x32x64_bf8_fp8:
+    case Intrinsic::amdgcn_smfmac_f32_32x32x64_fp8_bf8: {
       // vdst, srcA, srcB, srcC, idx
       OpdsMapping[0] = getAGPROpMapping(MI.getOperand(0).getReg(), MRI, *TRI);
       OpdsMapping[2] = getVGPROpMapping(MI.getOperand(2).getReg(), MRI, *TRI);
