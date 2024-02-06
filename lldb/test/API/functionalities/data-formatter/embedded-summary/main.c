81ae1b5a2907cdf8b9ea52c9c585302de1770dd4
Dave Lee <davelee.com@gmail.com>
2024-02-06 15:14:55 -0800

    [lldb] Fix embedded type summary regex handling (#8121)
    Fixes a logic error when determining if a type is a regex. Look for a leading `^` in 
    the type name, not the summary string.
    
    Adds a test to verify regex handling.
    
    Fixes a log message to help debugging.

--- lldb/test/API/functionalities/data-formatter/embedded-summary/main.c
+++ lldb/test/API/functionalities/data-formatter/embedded-summary/main.c
@@ -5,7 +5,8 @@ struct Player {
   int number;
 };
 
-__attribute__((used, section("__DATA_CONST,__lldbsummaries"))) unsigned char
+__attribute__((aligned(1), used,
+               section("__DATA_CONST,__lldbsummaries"))) unsigned char
     _Player_type_summary[] = "\x01"     // version
                              "\x25"     // record size
                              "\x07"     // type name size
@@ -13,10 +14,28 @@ __attribute__((used, section("__DATA_CONST,__lldbsummaries"))) unsigned char
                              "\x1c"     // summary string size
                              "${var.name} (${var.number})"; // summary string
 
+struct Layer {
+  char *name;
+  int number;
+};
+
+// Near copy of the record for `Player`, using a regex type name (`^Layer`).
+__attribute__((aligned(1), used,
+               section("__DATA_CONST,__lldbsummaries"))) unsigned char
+    _Layer_type_summary[] = "\x01"     // version
+                            "\x25"     // record size
+                            "\x07"     // type name size
+                            "^Layer\0" // type name
+                            "\x1c"     // summary string size
+                            "${var.name} (${var.number})"; // summary string
+
 int main() {
   struct Player player;
   player.name = "Dirk";
   player.number = 41;
+  struct Layer layer;
+  layer.name = "crust";
+  layer.number = 3;
   puts("break here");
   return 0;
 }
