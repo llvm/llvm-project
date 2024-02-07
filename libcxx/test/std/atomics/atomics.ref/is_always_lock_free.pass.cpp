ee7cb8080bded33f7d4aeae0de54379a43dafd13
Damien L-G <dalg24@gmail.com>
2024-02-07 08:56:31 -0500

    [libc++][atomic_ref] Check more types for is_always_lock_free

--- libcxx/test/std/atomics/atomics.ref/is_always_lock_free.pass.cpp
+++ libcxx/test/std/atomics/atomics.ref/is_always_lock_free.pass.cpp
@@ -28,6 +28,13 @@ void check_always_lock_free(std::atomic_ref<T> const a) {
   ASSERT_NOEXCEPT(a.is_lock_free());
 }
 
+#define CHECK_ALWAYS_LOCK_FREE(T)                                                                                      \
+  do {                                                                                                                 \
+    typedef T type;                                                                                                    \
+    type obj{};                                                                                                        \
+    check_always_lock_free(std::atomic_ref<type>(obj));                                                                \
+  } while (0)
+
 void test() {
   int i = 0;
   check_always_lock_free(std::atomic_ref<int>(i));
@@ -38,9 +45,23 @@ void test() {
   int* p = &i;
   check_always_lock_free(std::atomic_ref<int*>(p));
 
-  struct X {
-  } x;
-  check_always_lock_free(std::atomic_ref<X>(x));
+  CHECK_ALWAYS_LOCK_FREE(struct Empty{});
+  CHECK_ALWAYS_LOCK_FREE(struct OneInt { int i; });
+  CHECK_ALWAYS_LOCK_FREE(struct IntArr2 { int i[2]; });
+  CHECK_ALWAYS_LOCK_FREE(struct FloatArr3 { float i[3]; });
+  CHECK_ALWAYS_LOCK_FREE(struct LLIArr2 { long long int i[2]; });
+  CHECK_ALWAYS_LOCK_FREE(struct LLIArr4 { long long int i[4]; });
+  CHECK_ALWAYS_LOCK_FREE(struct LLIArr8 { long long int i[8]; });
+  CHECK_ALWAYS_LOCK_FREE(struct LLIArr16 { long long int i[16]; });
+  CHECK_ALWAYS_LOCK_FREE(struct Padding {
+    char c; /* padding */
+    long long int i;
+  });
+  CHECK_ALWAYS_LOCK_FREE(union IntFloat {
+    int i;
+    float f;
+  });
+  CHECK_ALWAYS_LOCK_FREE(enum class CharEnumClass : char{foo});
 }
 
 int main(int, char**) {
