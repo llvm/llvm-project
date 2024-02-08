0785502986026d2d2a57dd1293bae7972c6701ef
Wu, Yingcong <yingcong.wu@intel.com>
2024-02-08 01:17:47 -0800

    format

--- llvm/lib/Transforms/Instrumentation/AddressSanitizer.cpp
+++ llvm/lib/Transforms/Instrumentation/AddressSanitizer.cpp
@@ -1957,7 +1957,7 @@ bool ModuleAddressSanitizer::shouldInstrumentGlobal(GlobalVariable *G) const {
     // On COFF, don't instrument non-ODR linkages.
     if (G->isInterposable())
       return false;
-    // If the global has AvailableExternally linkage, then it is not in this 
+    // If the global has AvailableExternally linkage, then it is not in this
     // module, which means it does not need to be instrumented.
     if (G->hasAvailableExternallyLinkage())
       return false;
