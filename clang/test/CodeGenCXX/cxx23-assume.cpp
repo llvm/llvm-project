c7515d9a9116c3bc75e972ed618290d3f96226b9
Sirraide <aeternalmail@gmail.com>
2024-02-08 06:10:10 +0100

    [Clang] Actually include CHECK directives in comments

--- clang/test/CodeGenCXX/cxx23-assume.cpp
+++ clang/test/CodeGenCXX/cxx23-assume.cpp
@@ -2,6 +2,11 @@
 
 bool f();
 
+template <typename T>
+void f2() {
+  [[assume(sizeof(T) == sizeof(int))]];
+}
+
 // CHECK: @_Z1gii(i32 noundef [[X:%.*]], i32 noundef [[Y:%.*]])
 // CHECK-NEXT: entry:
 // CHECK-NEXT: [[X_ADDR:%.*]] = alloca i32
@@ -15,14 +20,28 @@ void g(int x, int y) {
   // CHECK-NEXT: call void @llvm.assume(i1 true)
   [[assume((1, 2))]];
 
-  // [[X1:%.*]] = load i32, ptr [[X_ADDR]]
-  // [[CMP1:%.*]] = icmp ne i32 [[X1]], 27
-  // call void @llvm.assume(i1 [[CMP1]])
+  // CHECK-NEXT: [[X1:%.*]] = load i32, ptr [[X_ADDR]]
+  // CHECK-NEXT: [[CMP1:%.*]] = icmp ne i32 [[X1]], 27
+  // CHECK-NEXT: call void @llvm.assume(i1 [[CMP1]])
   [[assume(x != 27)]];
 
-  // [[X2:%.*]] = load i32, ptr [[X_ADDR]]
-  // [[Y2:%.*]] = load i32, ptr [[Y_ADDR]]
-  // [[CMP2:%.*]] = icmp eq i32 [[X2]], [[Y2]]
-  // call void @llvm.assume(i1 [[CMP2]])
+  // CHECK-NEXT: [[X2:%.*]] = load i32, ptr [[X_ADDR]]
+  // CHECK-NEXT: [[Y2:%.*]] = load i32, ptr [[Y_ADDR]]
+  // CHECK-NEXT: [[CMP2:%.*]] = icmp eq i32 [[X2]], [[Y2]]
+  // CHECK-NEXT: call void @llvm.assume(i1 [[CMP2]])
   [[assume(x == y)]];
+
+  // CHECK-NEXT: call void @_Z2f2IiEvv()
+  f2<int>();
+
+  // CHECK-NEXT: call void @_Z2f2IdEvv()
+  f2<double>();
 }
+
+// CHECK: void @_Z2f2IiEvv()
+// CHECK-NEXT: entry:
+// CHECK-NEXT: call void @llvm.assume(i1 true)
+
+// CHECK: void @_Z2f2IdEvv()
+// CHECK-NEXT: entry:
+// CHECK-NEXT: call void @llvm.assume(i1 false)
