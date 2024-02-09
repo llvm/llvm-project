c4e5d9c747b021201c5cdb7d3073f6ec358418e5
Ryosuke Niwa <rniwa@webkit.org>
2024-02-09 15:27:18 -0800

    Use mock-types.h instead of embedding the defintion of Ref in the test.

--- clang/test/Analysis/Checkers/WebKit/implicit-cast-to-base-class-with-deref-in-superclass.cpp
+++ clang/test/Analysis/Checkers/WebKit/implicit-cast-to-base-class-with-deref-in-superclass.cpp
@@ -1,28 +1,7 @@
 // RUN: %clang_analyze_cc1 -analyzer-checker=alpha.webkit.UncountedCallArgsChecker -verify %s
 // expected-no-diagnostics
 
-template<typename T>
-class Ref {
-public:
-    ~Ref()
-    {
-        if (auto* ptr = m_ptr)
-            ptr->deref();
-        m_ptr = nullptr;
-    }
-
-    Ref(T& object)
-        : m_ptr(&object)
-    {
-        object.ref();
-    }
-
-    operator T&() const { return *m_ptr; }
-    bool operator!() const { return !*m_ptr; }
-
-private:
-    T* m_ptr;
-};
+#include "mock-types.h"
 
 class Base {
 public:
