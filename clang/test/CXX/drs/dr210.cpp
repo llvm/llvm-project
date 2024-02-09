964b9df77280330bb27074829f0ccfae22f62b98
Vlad Serebrennikov <serebrennikov.vladislav@gmail.com>
2024-02-09 19:50:44 +0300

    Incorporate Erich's suggestion into CWG210 test
    Also add a couple of static asserts, and remove unnecessary function

--- clang/test/CXX/drs/dr210.cpp
+++ clang/test/CXX/drs/dr210.cpp
@@ -6,28 +6,36 @@
 // RUN: %clang_cc1 -std=c++23 %s -triple x86_64-linux-gnu -emit-llvm -disable-llvm-passes -o - -fexceptions -fcxx-exceptions -pedantic-errors | llvm-cxxfilt -n | FileCheck %s --check-prefixes CHECK
 // RUN: %clang_cc1 -std=c++2c %s -triple x86_64-linux-gnu -emit-llvm -disable-llvm-passes -o - -fexceptions -fcxx-exceptions -pedantic-errors | llvm-cxxfilt -n | FileCheck %s --check-prefixes CHECK
 
+#if __cplusplus == 199711L
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wvariadic-macros"
+#define static_assert(...) __extension__ _Static_assert(__VA_ARGS__)
+#pragma clang diagnostic pop
+#endif
+
 namespace dr210 { // dr210: 2.7
 struct B {
+  long i;
   B();
   virtual ~B();
 };
 
+static_assert(sizeof(B) == 16, "");
+
 struct D : B {
-  int i;
+  long j;
   D();
 };
+
+static_assert(sizeof(D) == 24, "");
 
 void toss(const B* b) {
   throw *b;
 }
 
-void f() {
-  const D d;
-  toss(&d);
-}
-
 // CHECK-LABEL: define {{.*}} void @dr210::toss(dr210::B const*)
-// CHECK:         call ptr @__cxa_allocate_exception(i64 8)
+// CHECK:         %[[EXCEPTION:.*]] = call ptr @__cxa_allocate_exception(i64 16)
+// CHECK:         call void @__cxa_throw(ptr %[[EXCEPTION]], ptr @typeinfo for dr210::B, ptr @dr210::B::~B())
 // CHECK-LABEL: }
 
 } // namespace dr210
