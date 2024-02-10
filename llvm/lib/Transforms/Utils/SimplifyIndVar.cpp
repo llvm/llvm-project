9faa859c168bec4606fb6ae476717baf447dc54b
Florian Hahn <flo@fhahn.com>
2024-02-10 12:42:14 +0000

    !fixup remove stray whitespace

--- llvm/lib/Transforms/Utils/SimplifyIndVar.cpp
+++ llvm/lib/Transforms/Utils/SimplifyIndVar.cpp
@@ -1987,7 +1987,7 @@ PHINode *WidenIV::createWideIV(SCEVExpander &Rewriter) {
           cast<Instruction>(OrigPhi->getIncomingValueForBlock(LatchBlock));
 
       WideInc->setDebugLoc(OrigInc->getDebugLoc());
-      // We are replacing a narrow IV increment with a wider IV increment . If
+      // We are replacing a narrow IV increment with a wider IV increment. If
       // the original (narrow) increment did not wrap, the wider increment one
       // should not wrap either. Set the flags to be the union of both wide
       // increment and original increment; this ensures we preserve flags SCEV
