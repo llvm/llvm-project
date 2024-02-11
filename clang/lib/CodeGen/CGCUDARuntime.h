f1e9c3c70f97029f3ca3baf9419054f7a90011bb
Vlad Serebrennikov <serebrennikov.vladislav@gmail.com>
2024-02-11 12:07:27 +0300

    [clang][NFC] Annotate `CGCUDARuntime.h` with `preferred_type`
    This helps debuggers to display values in bit-fields in a more helpful way.

--- clang/lib/CodeGen/CGCUDARuntime.h
+++ clang/lib/CodeGen/CGCUDARuntime.h
@@ -54,10 +54,15 @@ public:
     };
 
   private:
+    LLVM_PREFERRED_TYPE(DeviceVarKind)
     unsigned Kind : 2;
+    LLVM_PREFERRED_TYPE(bool)
     unsigned Extern : 1;
+    LLVM_PREFERRED_TYPE(bool)
     unsigned Constant : 1;   // Constant variable.
+    LLVM_PREFERRED_TYPE(bool)
     unsigned Managed : 1;    // Managed variable.
+    LLVM_PREFERRED_TYPE(bool)
     unsigned Normalized : 1; // Normalized texture.
     int SurfTexType;         // Type of surface/texutre.
 
