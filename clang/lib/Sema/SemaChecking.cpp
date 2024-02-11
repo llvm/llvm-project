e760882c38ec9f3821606358d0ae4589526de610
Vlad Serebrennikov <serebrennikov.vladislav@gmail.com>
2024-02-11 15:06:15 +0300

    [clang][NFC] Annotate `SemaChecking.cpp` with `preferred_type`
    This helps debuggers to display values in bit-fields in a more helpful way.

--- clang/lib/Sema/SemaChecking.cpp
+++ clang/lib/Sema/SemaChecking.cpp
@@ -16652,6 +16652,7 @@ class SequenceChecker : public ConstEvaluatedExprVisitor<SequenceChecker> {
     struct Value {
       explicit Value(unsigned Parent) : Parent(Parent), Merged(false) {}
       unsigned Parent : 31;
+      LLVM_PREFERRED_TYPE(bool)
       unsigned Merged : 1;
     };
     SmallVector<Value, 8> Values;
