e2d187400f123d6b1ea33cce967cb0715d7f1c74
mingmingl <mingmingl@google.com>
2024-02-12 07:59:14 -0800

    simplify ir in unit test

--- llvm/unittests/Transforms/Utils/CallPromotionUtilsTest.cpp
+++ llvm/unittests/Transforms/Utils/CallPromotionUtilsTest.cpp
@@ -412,19 +412,15 @@ target datalayout = "e-m:e-p270:32:32-p271:32:32-p272:64:64-i64:64-f80:128-n8:16
 target triple = "x86_64-unknown-linux-gnu"
 
 @_ZTV5Base1 = constant { [4 x ptr] } { [4 x ptr] [ptr null, ptr null, ptr @_ZN5Base15func0Ev, ptr @_ZN5Base15func1Ev] }, !type !0
-@_ZTV8Derived1 = constant { [4 x ptr], [3 x ptr] } { [4 x ptr] [ptr inttoptr (i64 -8 to ptr), ptr null, ptr @_ZN5Base15func0Ev, ptr @_ZN5Base15func1Ev], [3 x ptr] [ptr null, ptr null, ptr @_ZN5Base25func2Ev] }, !type !1, !type !2, !type !3
-@_ZTV5Base2 = constant { [3 x ptr] } { [3 x ptr] [ptr null, ptr null, ptr @_ZN5Base25func2Ev] }, !type !2
-@_ZTV8Derived2 = constant { [3 x ptr], [3 x ptr], [4 x ptr] } { [3 x ptr] [ptr null, ptr null, ptr @_ZN5Base35func3Ev], [3 x ptr] [ptr inttoptr (i64 -8 to ptr), ptr null, ptr @_ZN5Base25func2Ev], [4 x ptr] [ptr inttoptr (i64 -16 to ptr), ptr null, ptr @_ZN5Base15func0Ev, ptr @_ZN5Base15func1Ev] }, !type !4, !type !5, !type !6, !type !7
-@_ZTV5Base3 = constant { [3 x ptr] } { [3 x ptr] [ptr null, ptr null, ptr @_ZN5Base35func3Ev] }, !type !6
+@_ZTV8Derived1 = constant { [4 x ptr], [3 x ptr] } { [4 x ptr] [ptr inttoptr (i64 -8 to ptr), ptr null, ptr @_ZN5Base15func0Ev, ptr @_ZN5Base15func1Ev], [3 x ptr] [ptr null, ptr null, ptr @_ZN5Base25func2Ev] }, !type !0, !type !1, !type !2
+@_ZTV8Derived2 = constant { [3 x ptr], [3 x ptr], [4 x ptr] } { [3 x ptr] [ptr null, ptr null, ptr @_ZN5Base35func3Ev], [3 x ptr] [ptr inttoptr (i64 -8 to ptr), ptr null, ptr @_ZN5Base25func2Ev], [4 x ptr] [ptr inttoptr (i64 -16 to ptr), ptr null, ptr @_ZN5Base15func0Ev, ptr @_ZN5Base15func1Ev] }, !type !3, !type !4, !type !5, !type !6
 
 define i32 @testfunc(ptr %d) {
 entry:
-  %vtable = load ptr, ptr %d, !prof !8
-  %0 = tail call i1 @llvm.type.test(ptr %vtable, metadata !"_ZTS5Base1")
-  tail call void @llvm.assume(i1 %0)
+  %vtable = load ptr, ptr %d, !prof !7
   %vfn = getelementptr inbounds ptr, ptr %vtable, i64 1
-  %1 = load ptr, ptr %vfn
-  %call = tail call i32 %1(ptr %d), !prof !9
+  %0 = load ptr, ptr %vfn
+  %call = tail call i32 %0(ptr %d), !prof !8
   ret i32 %call
 }
 
@@ -433,23 +429,19 @@ entry:
   ret i32 2
 }
 
-
-declare i1 @llvm.type.test(ptr, metadata)
-declare void @llvm.assume(i1)
 declare i32 @_ZN5Base25func2Ev(ptr)
 declare i32 @_ZN5Base15func0Ev(ptr)
 declare void @_ZN5Base35func3Ev(ptr)
 
 !0 = !{i64 16, !"_ZTS5Base1"}
-!1 = !{i64 16, !"_ZTS5Base1"}
-!2 = !{i64 48, !"_ZTS5Base2"}
-!3 = !{i64 16, !"_ZTS8Derived1"}
-!4 = !{i64 64, !"_ZTS5Base1"}
-!5 = !{i64 40, !"_ZTS5Base2"}
-!6 = !{i64 16, !"_ZTS5Base3"}
-!7 = !{i64 16, !"_ZTS8Derived2"}
-!8 = !{!"VP", i32 2, i64 1600, i64 -9064381665493407289, i64 800, i64 5035968517245772950, i64 500, i64 3215870116411581797, i64 300}
-!9 = !{!"VP", i32 0, i64 1600, i64 6804820478065511155, i64 1600})IR");
+!1 = !{i64 48, !"_ZTS5Base2"}
+!2 = !{i64 16, !"_ZTS8Derived1"}
+!3 = !{i64 64, !"_ZTS5Base1"}
+!4 = !{i64 40, !"_ZTS5Base2"}
+!5 = !{i64 16, !"_ZTS5Base3"}
+!6 = !{i64 16, !"_ZTS8Derived2"}
+!7 = !{!"VP", i32 2, i64 1600, i64 -9064381665493407289, i64 800, i64 5035968517245772950, i64 500, i64 3215870116411581797, i64 300}
+!8 = !{!"VP", i32 0, i64 1600, i64 6804820478065511155, i64 1600})IR");
 
   Function *F = M->getFunction("testfunc");
   ASSERT_TRUE(F);
@@ -462,10 +454,6 @@ declare void @_ZN5Base35func3Ev(ptr)
   GetElementPtrInst *GEP =
       dyn_cast<GetElementPtrInst>(FuncPtr->getPointerOperand());
   ASSERT_TRUE(GEP);
-
-  LoadInst *VPtr = dyn_cast<LoadInst>(&*F->front().begin());
-
-  Function *Callee = M->getFunction("_ZN5Base15func1Ev");
 
   // Create the constant and the branch weights
   SmallVector<Constant *, 3> VTableAddressPoints;
@@ -481,12 +469,16 @@ declare void @_ZN5Base35func3Ev(ptr)
 
   size_t OrigEntryBBSize = F->front().size();
 
+  LoadInst *VPtr = dyn_cast<LoadInst>(&*F->front().begin());
+
+  Function *Callee = M->getFunction("_ZN5Base15func1Ev");
   // Tests that promoted direct call is returned.
   CallBase &DirectCB = promoteCallWithVTableCmp(
       *CI, VPtr, Callee, VTableAddressPoints, BranchWeights);
   EXPECT_EQ(DirectCB.getCalledOperand(), Callee);
 
-  // Tests that GEP and FuncPtr sink to the basic block of indirect call.
+  // GEP and FuncPtr remains in the original block. `promoteCallWithVTableCmp`
+  // doesn't sink them to the basic block of indirect fallback.
   BasicBlock *EntryBB = &F->front();
   EXPECT_EQ(EntryBB, GEP->getParent());
   EXPECT_EQ(EntryBB, FuncPtr->getParent());
