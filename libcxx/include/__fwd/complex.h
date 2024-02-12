d04435fde0d7c9b9ed89c38b559daf7eac8cd80d
Hristo Hristov <hghristov.rmm@gmail.com>
2024-02-12 12:18:14 +0200

    Moved `get` to `complex.h`

--- libcxx/include/__fwd/complex.h
+++ libcxx/include/__fwd/complex.h
@@ -10,6 +10,7 @@
 #define _LIBCPP___FWD_COMPLEX_H
 
 #include <__config>
+#include <cstddef>
 
 #if !defined(_LIBCPP_HAS_NO_PRAGMA_SYSTEM_HEADER)
 #  pragma GCC system_header
@@ -19,6 +20,22 @@ _LIBCPP_BEGIN_NAMESPACE_STD
 
 template <class _Tp>
 class _LIBCPP_TEMPLATE_VIS complex;
+
+#if _LIBCPP_STD_VER >= 26
+
+template <size_t _Ip, class _Tp>
+_LIBCPP_HIDE_FROM_ABI constexpr _Tp& get(complex<_Tp>&) noexcept;
+
+template <size_t _Ip, class _Tp>
+_LIBCPP_HIDE_FROM_ABI constexpr _Tp&& get(complex<_Tp>&&) noexcept;
+
+template <size_t _Ip, class _Tp>
+_LIBCPP_HIDE_FROM_ABI constexpr const _Tp& get(const complex<_Tp>&) noexcept;
+
+template <size_t _Ip, class _Tp>
+_LIBCPP_HIDE_FROM_ABI constexpr const _Tp&& get(const complex<_Tp>&&) noexcept;
+
+#endif // _LIBCPP_STD_VER >= 26
 
 _LIBCPP_END_NAMESPACE_STD
 
