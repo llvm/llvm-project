b3146c8f0a685a5f0684c04c5b1bdbc3b748a6cf
Ryosuke Niwa <rniwa@webkit.org>
2024-02-12 15:00:12 -0800

    [analyzer] Add the support for calling Ref::ptr accessor. (#80919)
    This accessor returns a pointer from Ref type and is therefore safe.

--- clang/lib/StaticAnalyzer/Checkers/WebKit/PtrTypesSemantics.cpp
+++ clang/lib/StaticAnalyzer/Checkers/WebKit/PtrTypesSemantics.cpp
@@ -154,6 +154,7 @@ std::optional<bool> isGetterOfRefCounted(const CXXMethodDecl* M)
 
     if (((className == "Ref" || className == "RefPtr") &&
          methodName == "get") ||
+        (className == "Ref" && methodName == "ptr") ||
         ((className == "String" || className == "AtomString" ||
           className == "AtomStringImpl" || className == "UniqueString" ||
           className == "UniqueStringImpl" || className == "Identifier") &&
