57b0979ea9fe37428ff3911d6c27f4c5a7ce9691
Kirill Yansitov <36601354+YazZz1k@users.noreply.github.com>
2024-02-12 22:12:59 +0300

    [CIR][CIRGen] Fix in replacing of no_proto func (#460)
    When replacing the no-proto functions with it's real definition, codegen
    assumes that only `cir.call` operation may use the replaced function.
    Such behaviour leads to compilation error because of the
    `cir.get_global` op can also use the function to get pointer to
    function.
    This PR adds handle the case with `cir.get_global` operation and fixes
    the issue.

---
+++ clang/test/CIR/CodeGen/no-proto-fun-ptr.c
@@ -0,0 +1,17 @@
+// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fclangir -emit-cir %s -o -  | FileCheck %s
+
+void empty();
+
+void check_noproto_ptr() {
+  void (*fun)(void) = empty;
+}
+
+// CHECK:  cir.func no_proto @check_noproto_ptr()
+// CHECK:    [[ALLOC:%.*]] = cir.alloca !cir.ptr<!cir.func<!void ()>>, cir.ptr <!cir.ptr<!cir.func<!void ()>>>, ["fun", init] {alignment = 8 : i64}
+// CHECK:    [[GGO:%.*]] = cir.get_global @empty : cir.ptr <!cir.func<!void ()>>
+// CHECK:    [[CAST:%.*]] = cir.cast(bitcast, [[GGO]] : !cir.ptr<!cir.func<!void ()>>), !cir.ptr<!cir.func<!void ()>>
+// CHECK:    cir.store [[CAST]], [[ALLOC]] : !cir.ptr<!cir.func<!void ()>>, cir.ptr <!cir.ptr<!cir.func<!void ()>>>
+// CHECK:    cir.return
+
+void empty(void) {}
+
