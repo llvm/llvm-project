f4fb72075b8b3242656c10618eb5baa6a4204907
Younan Zhang <zyn7109@gmail.com>
2024-02-12 15:47:59 +0800

    [Clang][Sema] Don't consider top-level cv-qualifiers in template partial orderings
    This fixes a regression since
    https://github.com/llvm/llvm-project/commit/340eac01f7dad6c24cee35dd35f2484098dd6b1a,
    from which we compared function parameter types with cv-qualifiers
    taken into account. However, as per [dcl.fct]/p5:
    
    > After producing the list of parameter types, any top-level cv-qualifiers modifying
    > a parameter type are deleted when forming the function type.
    
    Thus I think we should use hasSameUnqualifiedType for type comparison.
    
    This fixes https://github.com/llvm/llvm-project/issues/75404.

--- clang/test/CXX/over/over.match/over.match.best/p1-2a.cpp
+++ clang/test/CXX/over/over.match/over.match.best/p1-2a.cpp
@@ -98,12 +98,12 @@ namespace non_template
   static_assert(is_same_v<decltype(bar<int>()), void>); // expected-error {{call to 'bar' is ambiguous}}
 
   template<typename T>
-  constexpr int goo(int a) requires AtLeast2<int> && true { // expected-note {{candidate function}}
+  constexpr int goo(T a) requires AtLeast2<T> && true {
     return 1;
   }
 
   template<typename T>
-  constexpr int goo(const int b) requires AtLeast2<int> { // expected-note {{candidate function}}
+  constexpr int goo(const T b) requires AtLeast2<T> {
     return 2;
   }
 
@@ -122,7 +122,6 @@ namespace non_template
     return 2;
   }
 
-  // By temp.func.order-6.2.2, this is ambiguous because parameter a and b have different types.
-  static_assert(goo<int>(1) == 1); // expected-error {{call to 'goo' is ambiguous}}
+  static_assert(goo<int>(1) == 1);
   static_assert(doo<int>(2) == 1);
 }
