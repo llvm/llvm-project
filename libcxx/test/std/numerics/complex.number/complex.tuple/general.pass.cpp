bb066436926b19cc3fec42b65c1ddf92c07043ce
Hristo Hristov <hghristov.rmm@gmail.com>
2024-02-13 12:53:32 +0200

    WIP

--- libcxx/test/std/numerics/complex.number/complex.tuple/general.pass.cpp
+++ libcxx/test/std/numerics/complex.number/complex.tuple/general.pass.cpp
@@ -30,7 +30,9 @@ constexpr void test() {
     std::complex<T> c{T{27}, T{28}};
 
     auto& [r, i]{c};
+    static_assert(std::same_as<T&, decltype(r)>);
     assert(r == T{27});
+    static_assert(std::same_as<T&, decltype(i)>);
     assert(i == T{28});
   }
   //  &&
@@ -38,7 +40,9 @@ constexpr void test() {
     std::complex<T> c{T{27}, T{28}};
 
     auto&& [r, i]{std::move(c)};
+    static_assert(std::same_as<T&&, decltype(r)>);
     assert(r == T{27});
+    static_assert(std::same_as<T&&, decltype(i)>);
     assert(i == T{28});
   }
   // const &
@@ -46,7 +50,9 @@ constexpr void test() {
     const std::complex<T> c{T{27}, T{28}};
 
     const auto& [r, i]{c};
+    static_assert(std::same_as<const T&, decltype(r)>);
     assert(r == T{27});
+    static_assert(std::same_as<const T&, decltype(i)>);
     assert(i == T{28});
   }
   //  const &&
@@ -54,24 +60,26 @@ constexpr void test() {
     const std::complex<T> c{T{27}, T{28}};
 
     const auto&& [r, i]{std::move(c)};
+    static_assert(std::same_as<const T&&, decltype(r)>);
     assert(r == T{27});
+    static_assert(std::same_as<const T&&, decltype(i)>);
     assert(i == T{28});
   }
 
   // Ranges
 
   {
-    std::complex<T> arr[]{{27, 28}, {82, 94}};
+    std::complex<T> arr[]{{T{27}, T{28}}, {T{82}, T{94}}};
 
     std::same_as<std::vector<T>> decltype(auto) reals{arr | std::views::elements<0> | std::ranges::to<std::vector>()};
     assert(reals.size() == 2);
-    assert(reals[0] == 27);
-    assert(reals[1] == 82);
+    assert(reals[0] == T{27});
+    assert(reals[1] == T{82});
 
     std::same_as<std::vector<T>> decltype(auto) imags{arr | std::views::elements<1> | std::ranges::to<std::vector>()};
     assert(reals.size() == 2);
-    assert(imags[0] == 28);
-    assert(imags[1] == 94);
+    assert(imags[0] == T{28});
+    assert(imags[1] == T{94});
   }
 }
 
