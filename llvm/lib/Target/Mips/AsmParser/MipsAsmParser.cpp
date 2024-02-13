a760ac793b9915163fed1b275424b7830ba73758
Fangrui Song <i@maskray.me>
2024-02-13 00:36:00 -0800

    Update MipsAsmParser.cpp

--- llvm/lib/Target/Mips/AsmParser/MipsAsmParser.cpp
+++ llvm/lib/Target/Mips/AsmParser/MipsAsmParser.cpp
@@ -1,4 +1,4 @@
-///===-- MipsAsmParser.cpp - Parse Mips assembly to MCInst instructions ----===//
+//===-- MipsAsmParser.cpp - Parse Mips assembly to MCInst instructions ----===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
