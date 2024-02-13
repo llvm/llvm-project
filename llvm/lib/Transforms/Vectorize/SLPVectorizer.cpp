cabbe058b6bec5ca4099a8ab892bf2ec2f5d84a9
Florian Hahn <flo@fhahn.com>
2024-02-13 18:04:23 +0000

    !fixup remove escape hatch for non-power-of-2 vectors from processBV.

--- llvm/lib/Transforms/Vectorize/SLPVectorizer.cpp
+++ llvm/lib/Transforms/Vectorize/SLPVectorizer.cpp
@@ -7185,7 +7185,8 @@ class BoUpSLP::ShuffleCostEstimator : public BaseShuffleAnalysis {
         for (unsigned I = 0, End = VL.size(); I < End; I += VF) {
           if (VectorizedLoads.contains(VL[I]))
             continue;
-          GatherCost += getBuildVectorCost(VL.slice(I, VF), Root);
+          GatherCost += getBuildVectorCost(
+              VL.slice(I, std::min(VL.size() - I, size_t(VF))), Root);
         }
         // Exclude potentially vectorized loads from list of gathered
         // scalars.
@@ -10745,15 +10746,6 @@ ResTy BoUpSLP::processBuildVector(const TreeEntry *E, Args &...Params) {
   assert(E->State == TreeEntry::NeedToGather && "Expected gather node.");
   unsigned VF = E->getVectorFactor();
   BVTy ShuffleBuilder(Params...);
-  // FIXME: Only full gathering is implemented for non-power-of-2 nodes at the
-  // moment.
-  if (E->isNonPowOf2Vec()) {
-    Value *BV = ShuffleBuilder.gather(E->Scalars);
-    SmallVector<int> Mask(VF, PoisonMaskElem);
-    std::iota(Mask.begin(), Mask.end(), 0);
-    ShuffleBuilder.add(BV, Mask);
-    return ShuffleBuilder.finalize(std::nullopt);
-  }
   bool NeedFreeze = false;
   SmallVector<int> ReuseShuffleIndicies(E->ReuseShuffleIndices.begin(),
                                         E->ReuseShuffleIndices.end());
