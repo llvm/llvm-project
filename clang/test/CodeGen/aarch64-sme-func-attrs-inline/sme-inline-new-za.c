3283c2e2e9264f5d8fbad79be86104897bd84d1a
Samuel Tebbs <samuel.tebbs@arm.com>
2024-02-14 16:43:26 +0000

    fixup: merge tests

--- clang/test/CodeGen/aarch64-sme-func-attrs-inline/sme-inline-new-za.c
+++
@@ -1,6 +0,0 @@
-// RUN: %clang --target=aarch64-none-linux-gnu -march=armv9-a+sme -S -Xclang -verify %s
-
-__attribute__((always_inline)) __arm_new("za")
-void inline_new_za(void)  { }
-// expected-error@+1 {{always_inline function 'inline_new_za' has new za state}}
-void inline_caller() { inline_new_za(); }
--- clang/test/CodeGen/aarch64-sme-func-attrs-inline/sme-inline-streaming-compatible-caller.c
+++
@@ -1,7 +0,0 @@
-// RUN: %clang --target=aarch64-none-linux-gnu -march=armv9-a+sme -S -Xclang -verify %s
-
-// Conflicting attributes when using always_inline
-__attribute__((always_inline))
-void inlined_fn_streaming(void) __arm_streaming {}
-// expected-error@+1 {{always_inline function 'inlined_fn_streaming' and its caller 'inlined_fn_caller' have mismatching streaming attributes}}
-void inlined_fn_caller(void) __arm_streaming_compatible { inlined_fn_streaming(); }
