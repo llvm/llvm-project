5136b867e6fa25ca78477f3e03eb6b00fa0f530a
akshaykumars614 <akshaykumars614@gmail.com>
2024-02-15 17:24:16 -0500

    updated the testcase

--- clang/test/Lexer/raw-string-dlim-newline.cpp
+++
@@ -1,5 +0,0 @@
-// RUN: %clang_cc1 -E -fsyntax-only -verify %s
-
-// expected-error@+2{{invalid newline character in raw string delimiter; use PREFIX( )PREFIX to delimit raw string}}
-// expected-error@+1{{expected expression}}
-char const* str1 = R"";
