d928d2ffcbde8b40c58aa31b1dca8ebfa3a3df86
Hiroshi Yamauchi <56735936+hjyamauchi@users.noreply.github.com>
2024-02-15 15:54:03 -0800

    Fix an assert failure with a funclet in a swifttailcc function. (#78806)
    The failure happens in the livedebugvalues pass.

--- llvm/lib/Target/AArch64/AArch64Subtarget.h
+++ llvm/lib/Target/AArch64/AArch64Subtarget.h
@@ -363,6 +363,7 @@ public:
     case CallingConv::C:
     case CallingConv::Fast:
     case CallingConv::Swift:
+    case CallingConv::SwiftTail:
       return isTargetWindows();
     case CallingConv::Win64:
       return true;
