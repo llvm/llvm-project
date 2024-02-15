cbfde25913db1af43ea05a3f86f38cedce936b86
Jan Korous <jkorous@apple.com>
2024-02-14 17:55:05 -0800

    [-Wunsafe-buffer-usage][NFC] Rebase the PR

--- clang/test/SemaCXX/warn-unsafe-buffer-usage-array.cpp
+++ clang/test/SemaCXX/warn-unsafe-buffer-usage-array.cpp
@@ -35,6 +35,6 @@ void constant_idx_safe0(unsigned idx) {
 
 void constant_idx_unsafe(unsigned idx) {
   int buffer[10];       // expected-warning{{'buffer' is an unsafe buffer that does not perform bounds checks}}
-                        // expected-note@-1{{change type of 'buffer' to 'std::array' to harden it}}
+                        // expected-note@-1{{change type of 'buffer' to 'std::array' to label it for hardening}}
   buffer[10] = 0;       // expected-note{{used in buffer access here}}
 }
