6e9ab11148f237ded61f015c48d47fb7b05c4906
Ryosuke Niwa <rniwa@webkit.org>
2024-02-14 18:46:30 -0800

    [analyzer] UncountedCallArgsChecker: Detect & ignore trivial function calls. (#81808)
    This PR introduces the concept of a "trivial function" which applies to
    a function that only calls other trivial functions and contain literals
    and expressions that don't result in heap mutations (specifically it
    does not call deref). This is implemented using ConstStmtVisitor and
    checking each statement and expression's trivialness.
    
    This PR also introduces the concept of a "ingleton function", which is a
    static member function or a free standing function which ends with the
    suffix "singleton". Such a function's return value is understood to be
    safe to call any function with.

--- clang/lib/StaticAnalyzer/Checkers/WebKit/UncountedCallArgsChecker.cpp
+++ clang/lib/StaticAnalyzer/Checkers/WebKit/UncountedCallArgsChecker.cpp
@@ -32,6 +32,8 @@ class UncountedCallArgsChecker
             "WebKit coding guidelines"};
   mutable BugReporter *BR;
 
+  TrivialFunctionAnalysis TFA;
+
 public:
 
   void checkASTDecl(const TranslationUnitDecl *TUD, AnalysisManager &MGR,
@@ -134,6 +136,11 @@ public:
   }
 
   bool shouldSkipCall(const CallExpr *CE) const {
+    const auto *Callee = CE->getDirectCallee();
+
+    if (Callee && TFA.isTrivial(Callee))
+      return true;
+
     if (CE->getNumArgs() == 0)
       return false;
 
@@ -155,7 +162,6 @@ public:
         return false;
     }
 
-    const auto *Callee = CE->getDirectCallee();
     if (!Callee)
       return false;
 
