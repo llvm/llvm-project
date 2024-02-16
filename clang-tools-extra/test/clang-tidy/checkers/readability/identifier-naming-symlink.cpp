ddafb4672e1a481d4a9556ebe31ca9a07e1f3569
Dmitry Polukhin <dmitry.polukhin@gmail.com>
2024-02-16 03:51:07 -0800

    [clang-tidy][readability-identifier-naming] Resolve symlinks for checking style for file
    Summary:
    Some build systems create symlinks in a temporary build directory for headers in the source tree for isolation purposes. These symlinks prevent `readability-identifier-naming` detecting issues and applying fixes. Without this fix clang-tidy is checking .clang-tidy config file in a temporary directory instead of source source location.
    
    If you think this change may have undesired results, I can add an option to activate it only when users enable symlink resolution explicitly.
    
    Test Plan: check-clang-tools

---
+++ clang-tools-extra/test/clang-tidy/checkers/readability/identifier-naming-symlink.cpp
@@ -0,0 +1,15 @@
+// Specify `-std=c++20` to run test only once becuase test expects changes
+// in the header file so it fails if runs multiple times with different
+// `-std` flags as check_clang_tidy doesn by default.
+//
+// RUN: rm -rf %T/symlink
+// RUN: cp -r %S/Inputs/identifier-naming/symlink %T/symlink
+// RUN: %check_clang_tidy -std=c++20 %s readability-identifier-naming %t -- --header-filter="test.h" --config-file=%S/Inputs/identifier-naming/symlink/include/.clang-tidy -- -I %T/symlink/build
+
+#include "test.h"
+
+int foo() {
+    return global_const;
+    // CHECK-MESSAGES: warning: invalid case style for global constant 'global_const' [readability-identifier-naming]
+    // CHECK-FIXES: return kGlobalConst;
+}
