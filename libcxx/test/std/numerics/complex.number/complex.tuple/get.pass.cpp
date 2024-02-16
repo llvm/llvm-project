7626a1d7c646a5271c29b5432c8e99a68dc4e642
Hristo Hristov <hghristov.rmm@gmail.com>
2024-02-16 12:18:56 +0200

    Addressed review comments

--- libcxx/test/std/numerics/complex.number/complex.tuple/get.pass.cpp
+++ libcxx/test/std/numerics/complex.number/complex.tuple/get.pass.cpp
@@ -78,6 +78,41 @@ constexpr void test() {
     static_assert(noexcept(get<1>(c)));
     assert(i == T{28});
   }
+  // Test `get()` allows using `complex` with structured bindings
+  {
+    std::complex<T> c{T{27}, T{28}};
+
+    auto [r, i]{c};
+    static_assert(std::same_as<T, decltype(r)>);
+    assert(r == T{27});
+    static_assert(std::same_as<T, decltype(i)>);
+    assert(i == T{28});
+  }
+  {
+    std::complex<T> c{T{27}, T{28}};
+
+    auto& [r, i]{c};
+    static_assert(std::same_as<T, decltype(r)>);
+    assert(r == T{27});
+    static_assert(std::same_as<T, decltype(i)>);
+    assert(i == T{28});
+  }
+  // Test `get()` allows using `complex` with ranges
+  {
+    std::complex<T> arr[]{{T{27}, T{28}}, {T{82}, T{94}}};
+
+    std::same_as<std::vector<T>> decltype(auto) reals{
+        arr | std::views::elements<0> | std::ranges::to<std::vector<T>>()};
+    assert(reals.size() == 2);
+    assert(reals[0] == T{27});
+    assert(reals[1] == T{82});
+
+    std::same_as<std::vector<T>> decltype(auto) imags{
+        arr | std::views::elements<1> | std::ranges::to<std::vector<T>>()};
+    assert(reals.size() == 2);
+    assert(imags[0] == T{28});
+    assert(imags[1] == T{94});
+  }
 }
 
 constexpr bool test() {
