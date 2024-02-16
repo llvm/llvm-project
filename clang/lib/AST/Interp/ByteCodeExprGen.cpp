720b793d40185e09722df272ed79c270ac9f94fd
Timm Bäder <tbaeder@redhat.com>
2024-02-16 12:47:31 +0100

    [clang][Interp] Lazily visit constant locals in C++
    While we _do_ get them registered via visitInitializer(), they
    are still local, so gone on the next call to e.g. evaluateAsRValue().
    
    Visit them lazily, similarly like we do in C.

--- clang/lib/AST/Interp/ByteCodeExprGen.cpp
+++ clang/lib/AST/Interp/ByteCodeExprGen.cpp
@@ -3237,9 +3237,20 @@ bool ByteCodeExprGen<Emitter>::VisitDeclRefExpr(const DeclRefExpr *E) {
   // Try to lazily visit (or emit dummy pointers for) declarations
   // we haven't seen yet.
   if (Ctx.getLangOpts().CPlusPlus) {
-    if (const auto *VD = dyn_cast<VarDecl>(D); VD && VD->isStaticLocal()) {
-      if (std::optional<unsigned> I = P.getOrCreateDummy(D))
-        return this->emitGetPtrGlobal(*I, E);
+    if (const auto *VD = dyn_cast<VarDecl>(D)) {
+      // Dummy for static locals
+      if (VD->isStaticLocal()) {
+        if (std::optional<unsigned> I = P.getOrCreateDummy(D))
+          return this->emitGetPtrGlobal(*I, E);
+        return false;
+      }
+      // Visit local const variables like normal.
+      if (VD->isLocalVarDecl() && VD->getType().isConstQualified()) {
+        if (!this->visitVarDecl(VD))
+          return false;
+        // Retry.
+        return this->VisitDeclRefExpr(E);
+      }
     }
   } else {
     if (const auto *VD = dyn_cast<VarDecl>(D);
