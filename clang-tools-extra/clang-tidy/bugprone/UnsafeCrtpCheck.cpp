b930eee4efff6481d8ec07f785ca6791feeff8ee
isuckatcs <65320245+isuckatcs@users.noreply.github.com>
2024-02-18 02:18:18 +0100

    fix friend detection

--- clang-tools-extra/clang-tidy/bugprone/UnsafeCrtpCheck.cpp
+++ clang-tools-extra/clang-tidy/bugprone/UnsafeCrtpCheck.cpp
@@ -34,15 +34,39 @@ bool hasPrivateConstructor(const CXXRecordDecl *RD) {
   return false;
 }
 
-bool isDerivedBefriended(const CXXRecordDecl *CRTP,
-                         const CXXRecordDecl *Derived) {
+bool isDerivedParameterBefriended(const CXXRecordDecl *CRTP,
+                                  const NamedDecl *Derived) {
   for (auto &&Friend : CRTP->friends()) {
-    if (Friend->getFriendType()->getType()->getAsCXXRecordDecl() == Derived)
+    const auto *TTPT =
+        dyn_cast<TemplateTypeParmType>(Friend->getFriendType()->getType());
+
+    if (TTPT && TTPT->getDecl() == Derived)
       return true;
   }
 
   return false;
 }
+
+std::optional<const NamedDecl *>
+getDerivedParameter(const ClassTemplateSpecializationDecl *CRTP,
+                    const CXXRecordDecl *Derived) {
+  size_t Idx = 0;
+  bool Found = false;
+  for (auto &&TemplateArg : CRTP->getTemplateArgs().asArray()) {
+    if (TemplateArg.getKind() == TemplateArgument::Type &&
+        TemplateArg.getAsType()->getAsCXXRecordDecl() == Derived) {
+      Found = true;
+      break;
+    }
+    ++Idx;
+  }
+
+  if (!Found)
+    return std::nullopt;
+
+  return CRTP->getSpecializedTemplate()->getTemplateParameters()->getParam(Idx);
+}
+
 } // namespace
 
 void UnsafeCrtpCheck::registerMatchers(MatchFinder *Finder) {
@@ -61,42 +85,33 @@ void UnsafeCrtpCheck::check(const MatchFinder::MatchResult &Result) {
       Result.Nodes.getNodeAs<ClassTemplateSpecializationDecl>("crtp");
   const auto *MatchedDerived = Result.Nodes.getNodeAs<CXXRecordDecl>("derived");
 
-  if (!MatchedCRTP->hasUserDeclaredConstructor()) {
-    diag(MatchedCRTP->getLocation(),
-         "the implicit default constructor of the CRTP is publicly accessible")
-        << MatchedCRTP
-        << FixItHint::CreateInsertion(
-               MatchedCRTP->getBraceRange().getBegin().getLocWithOffset(1),
-               "private: " + MatchedCRTP->getNameAsString() + "() = default;");
+  const auto *CRTPTemplate =
+      MatchedCRTP->getSpecializedTemplate()->getTemplatedDecl();
 
-    diag(MatchedCRTP->getLocation(), "consider making it private",
+  if (!CRTPTemplate->hasUserDeclaredConstructor()) {
+    diag(CRTPTemplate->getLocation(),
+         "the implicit default constructor of the CRTP is publicly accessible")
+        << CRTPTemplate
+        << FixItHint::CreateInsertion(
+               CRTPTemplate->getBraceRange().getBegin().getLocWithOffset(1),
+               "private: " + CRTPTemplate->getNameAsString() + "() = default;");
+
+    diag(CRTPTemplate->getLocation(), "consider making it private",
          DiagnosticIDs::Note);
   }
 
-  // FIXME: Extract this.
-  size_t idx = 0;
-  for (auto &&TemplateArg : MatchedCRTP->getTemplateArgs().asArray()) {
-    if (TemplateArg.getKind() == TemplateArgument::Type &&
-        TemplateArg.getAsType()->getAsCXXRecordDecl() == MatchedDerived) {
-      break;
-    }
-    ++idx;
-  }
+  const auto *DerivedTemplateParameter =
+      *getDerivedParameter(MatchedCRTP, MatchedDerived);
 
-  if (hasPrivateConstructor(MatchedCRTP) &&
-      !isDerivedBefriended(MatchedCRTP, MatchedDerived)) {
-    diag(MatchedCRTP->getLocation(),
+  if (hasPrivateConstructor(CRTPTemplate) &&
+      !isDerivedParameterBefriended(CRTPTemplate, DerivedTemplateParameter)) {
+    diag(CRTPTemplate->getLocation(),
          "the CRTP cannot be constructed from the derived class")
-        << MatchedCRTP
+        << CRTPTemplate
         << FixItHint::CreateInsertion(
-               MatchedCRTP->getBraceRange().getEnd().getLocWithOffset(-1),
-               "friend " +
-                   MatchedCRTP->getSpecializedTemplate()
-                       ->getTemplateParameters()
-                       ->asArray()[idx]
-                       ->getNameAsString() +
-                   ';');
-    diag(MatchedCRTP->getLocation(),
+               CRTPTemplate->getBraceRange().getEnd().getLocWithOffset(-1),
+               "friend " + DerivedTemplateParameter->getNameAsString() + ';');
+    diag(CRTPTemplate->getLocation(),
          "consider declaring the derived class as friend", DiagnosticIDs::Note);
   }
 
