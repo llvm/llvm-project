8650c0b19087c963576ecc8869bb1cf88d6e3ae1
Chuanqi Xu <yedeng.yd@linux.alibaba.com>
2024-02-18 15:08:02 +0800

    [C++20] [Modules] Don't import non-inline function bodies even if it is marked as always_inline
    Close https://github.com/llvm/llvm-project/issues/80949
    
    Previously, I thought the always-inline function can be an exception to
    enable optimizations as much as possible. However, it looks like it
    breaks the ABI requirement we discussed later. So it looks better to not
    import non-inline function bodies at all even if the function bodies are
    marked as always_inline.
    
    It doesn't produce regressions in some degree since the always_inline
    still works in the same TU.

--- clang/lib/CodeGen/CodeGenModule.cpp
+++ clang/lib/CodeGen/CodeGenModule.cpp
@@ -3985,8 +3985,7 @@ bool CodeGenModule::shouldEmitFunction(GlobalDecl GD) {
   // behavior may break ABI compatibility of the current unit.
   if (const Module *M = F->getOwningModule();
       M && M->getTopLevelModule()->isNamedModule() &&
-      getContext().getCurrentNamedModule() != M->getTopLevelModule() &&
-      !F->hasAttr<AlwaysInlineAttr>())
+      getContext().getCurrentNamedModule() != M->getTopLevelModule())
     return false;
 
   if (F->hasAttr<NoInlineAttr>())
