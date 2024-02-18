28737890db53bd2cef7a6fad4b260e20e325f899
David Tellenbach <dtellenbach@apple.com>
2024-02-18 14:28:58 -0800

    Fix typo in test

--- clang/test/CoverageMapping/conditional-operator.c
+++ clang/test/CoverageMapping/conditional-operator.c
@@ -11,14 +11,14 @@ int binary_conditional(int x) {
   return y;
 }
 
-// CHECK-LABEL:       tenary_conditional:
+// CHECK-LABEL:       ternary_conditional:
 // CHECK-NEXT:          File 0, [[@LINE+6]]:31 -> {{[0-9]+}}:2 = #0
 // CHECK-NEXT:          File 0, [[@LINE+6]]:7 -> [[@LINE+6]]:8 = #0
 // CHECK-NEXT:          Branch,File 0, [[@LINE+5]]:7 -> [[@LINE+5]]:8 = #1, (#0 - #1)
 // CHECK-NEXT:          Gap,File 0, [[@LINE+4]]:10 -> [[@LINE+4]]:11 = #1
 // CHECK-NEXT:          File 0, [[@LINE+3]]:11 -> [[@LINE+3]]:12 = #1
 // CHECK-NEXT:          File 0, [[@LINE+2]]:15 -> [[@LINE+2]]:16 = (#0 - #1)
-int tenary_conditional(int x) {
+int ternary_conditional(int x) {
   x = x ? x : 4;
   int y = x;
   return y;
