73702af15fa291d4951aaa6ad6aa43326fa6727a
Abhinav271828 <abhinav.m@research.iiit.ac.in>
2024-02-19 23:19:08 +0530

    Use EXPECT_NE

--- mlir/unittests/Analysis/Presburger/MatrixTest.cpp
+++ mlir/unittests/Analysis/Presburger/MatrixTest.cpp
@@ -377,7 +377,7 @@ TEST(MatrixTest, gramSchmidt) {
 
   // The integers involved are too big to construct the actual matrix.
   // but we can check that the result is linearly independent.
-  ASSERT_FALSE(mat.determinant(nullptr) == 0);
+  EXPECT_NE(mat.determinant(nullptr), 0);
 
   for (unsigned i = 0; i < 4u; ++i)
     for (unsigned j = i + 1; j < 4u; ++j)
