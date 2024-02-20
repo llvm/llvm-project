1d9cb0aca8985aa1636780b3ff9a863962cc2d57
Justin Stitt <justinstitt@google.com>
2024-02-20 23:30:47 +0000

    update tests to use common prefix

--- clang/test/CodeGen/integer-overflow.c
+++ clang/test/CodeGen/integer-overflow.c
@@ -1,8 +1,8 @@
 // RUN: %clang_cc1 -triple x86_64-apple-darwin %s -emit-llvm -o - | FileCheck %s --check-prefix=DEFAULT
 // RUN: %clang_cc1 -triple x86_64-apple-darwin %s -emit-llvm -o - -fwrapv | FileCheck %s --check-prefix=WRAPV
 // RUN: %clang_cc1 -triple x86_64-apple-darwin %s -emit-llvm -o - -ftrapv | FileCheck %s --check-prefix=TRAPV
-// RUN: %clang_cc1 -triple x86_64-apple-darwin %s -emit-llvm -o - -fsanitize=signed-integer-overflow | FileCheck %s --check-prefix=CATCH_UB
-// RUN: %clang_cc1 -triple x86_64-apple-darwin %s -emit-llvm -o - -fsanitize=signed-integer-overflow -fwrapv | FileCheck %s --check-prefix=CATCH_WRAP
+// RUN: %clang_cc1 -triple x86_64-apple-darwin %s -emit-llvm -o - -fsanitize=signed-integer-overflow | FileCheck %s --check-prefixes=CATCH_UB,CATCH_UB_POINTER
+// RUN: %clang_cc1 -triple x86_64-apple-darwin %s -emit-llvm -o - -fsanitize=signed-integer-overflow -fwrapv | FileCheck %s --check-prefixes=CATCH_UB,NOCATCH_UB_POINTER
 // RUN: %clang_cc1 -triple x86_64-apple-darwin %s -emit-llvm -o - -ftrapv -ftrapv-handler foo | FileCheck %s --check-prefix=TRAPV_HANDLER
 
 
@@ -17,7 +17,6 @@ void test1(void) {
   // WRAPV: add i32
   // TRAPV: llvm.sadd.with.overflow.i32
   // CATCH_UB: llvm.sadd.with.overflow.i32
-  // CATCH_WRAP: llvm.sadd.with.overflow.i32
   // TRAPV_HANDLER: foo(
   f11G = a + b;
   
@@ -25,7 +24,6 @@ void test1(void) {
   // WRAPV: sub i32
   // TRAPV: llvm.ssub.with.overflow.i32
   // CATCH_UB: llvm.ssub.with.overflow.i32
-  // CATCH_WRAP: llvm.ssub.with.overflow.i32
   // TRAPV_HANDLER: foo(
   f11G = a - b;
   
@@ -33,7 +31,6 @@ void test1(void) {
   // WRAPV: mul i32
   // TRAPV: llvm.smul.with.overflow.i32
   // CATCH_UB: llvm.smul.with.overflow.i32
-  // CATCH_WRAP: llvm.smul.with.overflow.i32
   // TRAPV_HANDLER: foo(
   f11G = a * b;
 
@@ -41,7 +38,6 @@ void test1(void) {
   // WRAPV: sub i32 0, 
   // TRAPV: llvm.ssub.with.overflow.i32(i32 0
   // CATCH_UB: llvm.ssub.with.overflow.i32(i32 0
-  // CATCH_WRAP: llvm.ssub.with.overflow.i32(i32 0
   // TRAPV_HANDLER: foo(
   f11G = -a;
   
@@ -51,7 +47,6 @@ void test1(void) {
   // WRAPV: add i32 {{.*}}, 1
   // TRAPV: llvm.sadd.with.overflow.i32({{.*}}, i32 1)
   // CATCH_UB: llvm.sadd.with.overflow.i32({{.*}}, i32 1)
-  // CATCH_WRAP: llvm.sadd.with.overflow.i32({{.*}}, i32 1)
   // TRAPV_HANDLER: foo(
   ++a;
   
@@ -59,7 +54,6 @@ void test1(void) {
   // WRAPV: add i32 {{.*}}, -1
   // TRAPV: llvm.ssub.with.overflow.i32({{.*}}, i32 1)
   // CATCH_UB: llvm.ssub.with.overflow.i32({{.*}}, i32 1)
-  // CATCH_WRAP: llvm.ssub.with.overflow.i32({{.*}}, i32 1)
   // TRAPV_HANDLER: foo(
   --a;
   
@@ -69,7 +63,8 @@ void test1(void) {
   // DEFAULT: getelementptr inbounds i32, ptr
   // WRAPV: getelementptr i32, ptr
   // TRAPV: getelementptr inbounds i32, ptr
-  // CATCH_UB: getelementptr inbounds i32, ptr
+  // CATCH_UB_POINTER: getelementptr inbounds i32, ptr
+  // NOCATCH_UB_POINTER: getelementptr i32, ptr
 
   // PR9350: char pre-increment never overflows.
   extern volatile signed char PR9350_char_inc;
@@ -77,7 +72,6 @@ void test1(void) {
   // WRAPV: add i8 {{.*}}, 1
   // TRAPV: add i8 {{.*}}, 1
   // CATCH_UB: add i8 {{.*}}, 1
-  // CATCH_WRAP: add i8 {{.*}}, 1
   ++PR9350_char_inc;
 
   // PR9350: char pre-decrement never overflows.
@@ -86,7 +80,6 @@ void test1(void) {
   // WRAPV: add i8 {{.*}}, -1
   // TRAPV: add i8 {{.*}}, -1
   // CATCH_UB: add i8 {{.*}}, -1
-  // CATCH_WRAP: add i8 {{.*}}, -1
   --PR9350_char_dec;
 
   // PR9350: short pre-increment never overflows.
@@ -95,7 +88,6 @@ void test1(void) {
   // WRAPV: add i16 {{.*}}, 1
   // TRAPV: add i16 {{.*}}, 1
   // CATCH_UB: add i16 {{.*}}, 1
-  // CATCH_WRAP: add i16 {{.*}}, 1
   ++PR9350_short_inc;
 
   // PR9350: short pre-decrement never overflows.
@@ -104,7 +96,6 @@ void test1(void) {
   // WRAPV: add i16 {{.*}}, -1
   // TRAPV: add i16 {{.*}}, -1
   // CATCH_UB: add i16 {{.*}}, -1
-  // CATCH_WRAP: add i16 {{.*}}, -1
   --PR9350_short_dec;
 
   // PR24256: don't instrument __builtin_frame_address.
@@ -113,5 +104,4 @@ void test1(void) {
   // WRAPV:    call ptr @llvm.frameaddress.p0(i32 0)
   // TRAPV:    call ptr @llvm.frameaddress.p0(i32 0)
   // CATCH_UB: call ptr @llvm.frameaddress.p0(i32 0)
-  // CATCH_WRAP: call ptr @llvm.frameaddress.p0(i32 0)
 }
