761d8a0aeeef340948181bc92ac0f290aaccb051
Vandana2896 <129426835+Vandana2896@users.noreply.github.com>
2024-02-20 14:50:55 -0800

    Update NVPTXAsmPrinter.cpp
    Clang formatting reverted.

--- llvm/lib/Target/NVPTX/NVPTXAsmPrinter.cpp
+++ llvm/lib/Target/NVPTX/NVPTXAsmPrinter.cpp
@@ -1595,8 +1595,7 @@ void NVPTXAsmPrinter::emitFunctionParamList(const Function *F, raw_ostream &O) {
           O << "\t.param .u" << PTySizeInBits << " ";
 
           int addrSpace = PTy->getAddressSpace();
-          if (static_cast<NVPTXTargetMachine &>(TM).getDrvInterface() ==
-              NVPTX::CUDA) {
+          if (static_cast<NVPTXTargetMachine &>(TM).getDrvInterface() == NVPTX::CUDA) {
             assert(addrSpace == 0 && "Invalid address space");
             O << ".ptr .global ";
           } else {
