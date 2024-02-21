ab17d55b13c89132167e6deed2110a9e00979658
Diego Caballero <diegocaballero@google.com>
2024-02-20 17:39:58 -0800

    [mlir][Linalg] Add folders for `linalg.transpose` (#81709)
    This PR adds folders for linalg transpose ops with only one dimension or
    an identity permutation. The folding removes the `linalg.transpose` and
    just propagates the input tensor.

--- mlir/lib/Dialect/Linalg/IR/LinalgOps.cpp
+++ mlir/lib/Dialect/Linalg/IR/LinalgOps.cpp
@@ -1786,6 +1786,22 @@ void TransposeOp::getEffects(
                         getDpsInits());
 }
 
+LogicalResult TransposeOp::fold(FoldAdaptor adaptor,
+                                SmallVectorImpl<OpFoldResult> &result) {
+  // Single dimension transpose.
+  if (getPermutation().size() == 0) {
+    result.push_back(getInput());
+    return success();
+  }
+  // Identity permutation.
+  if (isIdentityPermutation(getPermutation())) {
+    result.push_back(getInput());
+    return success();
+  }
+
+  return failure();
+}
+
 //===----------------------------------------------------------------------===//
 // BroadcastOp
 //===----------------------------------------------------------------------===//
