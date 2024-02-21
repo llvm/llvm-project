d1ed2167eb19cf78f10a754623616d724d09118c
Joseph Huber <huberjn@outlook.com>
2024-02-20 18:12:27 -0600

    [NVPTX] Enable the _Float16 type for NVPTX compilation (#82436)
    Summary:
    The PTX target supports the f16 type natively and we alreaqdy have a few
    LLVM backend tests that support the LLVM-IR. We should be able to enable
    this for generic use. This is done prior the f16 math functions being
    written in the GPU libc case.

--- clang/test/SemaCUDA/float16.cu
+++ clang/test/SemaCUDA/float16.cu
@@ -1,4 +1,5 @@
 // RUN: %clang_cc1 -fsyntax-only -triple x86_64 -aux-triple amdgcn -verify %s
+// RUN: %clang_cc1 -fsyntax-only -triple x86_64 -aux-triple nvptx64 -verify %s
 // expected-no-diagnostics
 #include "Inputs/cuda.h"
 
