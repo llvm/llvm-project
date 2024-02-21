2c525a8042bb24e6bc61a968fd28a81b24d31094
Philip Reames <preames@rivosinc.com>
2024-02-20 16:47:08 -0800

    Fix a bug with insufficiently aligned extracts

--- llvm/lib/Target/RISCV/RISCVTargetTransformInfo.cpp
+++ llvm/lib/Target/RISCV/RISCVTargetTransformInfo.cpp
@@ -436,15 +436,19 @@ InstructionCost RISCVTTIImpl::getShuffleCost(TTI::ShuffleKind Kind,
     if (Index == 0)
       return TTI::TCC_Free;
 
-    // If we're extracting a subvector at a sub-register boundary - which
-    // unfortunately we need exact vlen to identity - this is a subregister
-    // extract at worst and thus won't require a vslidedown.
+    // If we're extracting a subvector of at most m1 size at a sub-register
+    // boundary - which unfortunately we need exact vlen to identity - this is
+    // a subregister extract at worst and thus won't require a vslidedown.
+    // TODO: Extend for aligned m2, m4 subvector extracts
+    // TODO: Extend for misalgined (but contained) extracts
+    // TODO: Extend for scalable subvector types
     if (std::pair<InstructionCost, MVT> SubLT = getTypeLegalizationCost(SubTp);
         SubLT.second.isValid() && SubLT.second.isFixedLengthVector()) {
       const unsigned MinVLen = ST->getRealMinVLen();
       const unsigned MaxVLen = ST->getRealMaxVLen();
       if (MinVLen == MaxVLen &&
-          SubLT.second.getScalarSizeInBits() * Index % MinVLen == 0)
+          SubLT.second.getScalarSizeInBits() * Index % MinVLen == 0 &&
+          SubLT.second.getSizeInBits() <= MinVLen)
         return TTI::TCC_Free;
     }
 
