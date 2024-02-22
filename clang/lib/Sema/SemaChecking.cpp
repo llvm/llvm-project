5b8e5604c297aa8fd09bf641d12d0a663e0ea801
zhijian lin <zhijian@ca.ibm.com>
2024-02-22 08:46:08 -0500

    [AIX] Lower intrinsic __builtin_cpu_is into AIX platform-specific code. (#80069)
    On AIX OS, __builtin_cpu_is() references the runtime external variable
    _system_configuration from /usr/include/sys/systemcfg.h.
    
    ref issue:  https://github.com/llvm/llvm-project/issues/80042

--- clang/lib/Sema/SemaChecking.cpp
+++ clang/lib/Sema/SemaChecking.cpp
@@ -2165,7 +2165,10 @@ static bool SemaBuiltinCpu(Sema &S, const TargetInfo &TI, CallExpr *TheCall,
     return S.Diag(TheCall->getBeginLoc(), diag::err_builtin_target_unsupported)
            << SourceRange(TheCall->getBeginLoc(), TheCall->getEndLoc());
   if (!IsCPUSupports && !TheTI->supportsCpuIs())
-    return S.Diag(TheCall->getBeginLoc(), diag::err_builtin_target_unsupported)
+    return S.Diag(TheCall->getBeginLoc(),
+                  TI.getTriple().isOSAIX()
+                      ? diag::err_builtin_aix_os_unsupported
+                      : diag::err_builtin_target_unsupported)
            << SourceRange(TheCall->getBeginLoc(), TheCall->getEndLoc());
 
   Expr *Arg = TheCall->getArg(0)->IgnoreParenImpCasts();
