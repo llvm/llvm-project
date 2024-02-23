22734e15d8f2c437e8543f19632299d2e09b31f3
Sander de Smalen <sander.desmalen@arm.com>
2024-02-23 11:31:24 +0000

    [Clang][AArch64] Fix 'svzero_za' intrinsic to take no arguments. (#82648)
    We previously defined svzero_za as:
    
      void svzero_za();
    
    rather than:
    
      void svzero_za(void);
    
    Which meant that Clang accepted arguments. Compiling for example
    `svzero_za(<non-constant integer>)` ended up with incorrect IR and a
    compiler crash because it couldn't select an instruction for it.

---
+++ clang/test/Sema/aarch64-sme-intrinsics/acle_sme_zero.c
@@ -0,0 +1,7 @@
+// RUN: %clang_cc1 -triple aarch64-none-linux-gnu -target-feature +sme -target-feature +sve -fsyntax-only -verify %s
+
+void test_svzero_args(uint64_t m) {
+  svzero_za(0); // expected-error {{too many arguments to function call, expected 0, have 1}}
+  svzero_za(m); // expected-error {{too many arguments to function call, expected 0, have 1}}
+  svzero_mask_za(m); // expected-error {{argument to 'svzero_mask_za' must be a constant integer}}
+}
