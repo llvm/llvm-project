f6dbecb30a6e57ae2ed42c255714ec3333aa178a
MaxEW707 <82551778+MaxEW707@users.noreply.github.com>
2024-02-24 22:17:24 -0500

    Fix 'yvals_core.h` line endings and comment style

--- clang/lib/Headers/yvals_core.h
+++ clang/lib/Headers/yvals_core.h
@@ -1,13 +1,12 @@
-/* ===-------- vadefs.h ---------------------------------------------------===
- *
- * Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
- * See https://llvm.org/LICENSE.txt for license information.
- * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
- *
- *===-----------------------------------------------------------------------===
- */
+//===----- yvals_core.h - Internal MSVC STL core header -------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
 
-/* Only include this if we are aiming for MSVC compatibility. */
+// Only include this if we are aiming for MSVC compatibility.
 #ifndef _MSC_VER
 #include_next <yvals_core.h>
 #else
