00d1d53a1c52850042e1d09250833355a140ea4c
Samira Bazuzi <bazuzi@google.com>
2024-02-26 10:00:48 -0500

    [clang][dataflow] Skip array types when handling InitListExprs.
    Crashes resulted from single-element InitListExprs for arrays with
    elements of a record type after #80970.

--- clang/lib/Analysis/FlowSensitive/Transfer.cpp
+++ clang/lib/Analysis/FlowSensitive/Transfer.cpp
@@ -671,8 +671,8 @@ public:
     }
 
     if (!Type->isStructureOrClassType()) {
-      // Until array initialization is implemented, we skip arrays and don't need
-      // to care about cases where `getNumInits() > 1`.
+      // Until array initialization is implemented, we skip arrays and don't
+      // need to care about cases where `getNumInits() > 1`.
       if (!Type->isArrayType() && S->getNumInits() == 1)
         propagateValueOrStorageLocation(*S->getInit(0), *S, Env);
       return;
