58a7941d511678b4780390306c843012528ba58a
Farzon Lotfi <1802579+farzonl@users.noreply.github.com>
2024-02-26 11:08:59 -0500

    [HLSL] Implementation of dot intrinsic (#81190)
    This change implements https://github.com/llvm/llvm-project/issues/70073
    
    HLSL has a dot intrinsic defined here:
    
    https://learn.microsoft.com/en-us/windows/win32/direct3dhlsl/dx-graphics-hlsl-dot
    
    The intrinsic itself is defined as a HLSL_LANG LangBuiltin in
    Builtins.td.
    This is used to associate all the dot product typdef defined
    hlsl_intrinsics.h
    with a single intrinsic check in CGBuiltin.cpp & SemaChecking.cpp.
    
    In IntrinsicsDirectX.td we define the llvmIR for the dot product.
    A few goals were in mind for this IR. First it should operate on only
    vectors. Second the return type should be the vector element type. Third
    the second parameter vector should be of the same size as the first
    parameter. Finally `a dot b` should be the same as `b dot a`.
    
    In CGBuiltin.cpp hlsl has built on top of existing clang intrinsics via
    EmitBuiltinExpr. Dot
    product though is language specific intrinsic and so is guarded behind
    getLangOpts().HLSL.
    The call chain looks like this: EmitBuiltinExpr -> EmitHLSLBuiltinExp
    
    EmitHLSLBuiltinExp dot product intrinsics makes a destinction
    between vectors and scalars. This is because HLSL supports dot product
    on scalars which simplifies down to multiply.
    
    Sema.h & SemaChecking.cpp saw the addition of
    CheckHLSLBuiltinFunctionCall, a language specific semantic validation
    that can be expanded for other hlsl specific intrinsics.
    
    Fixes #70073

--- clang/include/clang/Sema/Sema.h
+++ clang/include/clang/Sema/Sema.h
@@ -14063,6 +14063,7 @@ private:
   bool CheckPPCBuiltinFunctionCall(const TargetInfo &TI, unsigned BuiltinID,
                                    CallExpr *TheCall);
   bool CheckAMDGCNBuiltinFunctionCall(unsigned BuiltinID, CallExpr *TheCall);
+  bool CheckHLSLBuiltinFunctionCall(unsigned BuiltinID, CallExpr *TheCall);
   bool CheckRISCVLMUL(CallExpr *TheCall, unsigned ArgNum);
   bool CheckRISCVBuiltinFunctionCall(const TargetInfo &TI, unsigned BuiltinID,
                                      CallExpr *TheCall);
@@ -14128,6 +14129,8 @@ private:
 
   bool CheckPPCMMAType(QualType Type, SourceLocation TypeLoc);
 
+  bool SemaBuiltinVectorMath(CallExpr *TheCall, QualType &Res);
+  bool SemaBuiltinVectorToScalarMath(CallExpr *TheCall);
   bool SemaBuiltinElementwiseMath(CallExpr *TheCall);
   bool SemaBuiltinElementwiseTernaryMath(CallExpr *TheCall);
   bool PrepareBuiltinElementwiseMathOneArgCall(CallExpr *TheCall);
