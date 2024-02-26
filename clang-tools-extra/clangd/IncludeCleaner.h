e0cb66c8313c8ff1a934dc222165040de656b71e
Nathan Ridge <zeratul976@hotmail.com>
2024-02-25 19:24:31 -0500

    [clangd] Make all calls to format::getStyle() go through getFormatStyleForFile()

--- clang-tools-extra/clangd/IncludeCleaner.h
+++ clang-tools-extra/clangd/IncludeCleaner.h
@@ -59,6 +59,7 @@ using HeaderFilter = llvm::ArrayRef<std::function<bool(llvm::StringRef)>>;
 std::vector<Diag>
 issueIncludeCleanerDiagnostics(ParsedAST &AST, llvm::StringRef Code,
                                const IncludeCleanerFindings &Findings,
+                               const ThreadsafeFS &TFS,
                                HeaderFilter IgnoreHeader = {});
 
 /// Affects whether standard library includes should be considered for
