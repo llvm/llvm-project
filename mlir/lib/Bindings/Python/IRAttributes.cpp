6a5a923ec1a046d3002ada500299f800758016c4
Rob Suderman <rob.suderman@gmail.com>
2024-02-26 11:09:29 -0800

    git-clang-format

--- mlir/lib/Bindings/Python/IRAttributes.cpp
+++ mlir/lib/Bindings/Python/IRAttributes.cpp
@@ -695,7 +695,7 @@ public:
         assert(view.itemsize == 2 && "mismatched array itemsize");
         bulkLoadElementType = mlirF16TypeGet(context);
       } else if (format == "?") {
-          bulkLoadElementType = mlirIntegerTypeGet(context, 1);
+        bulkLoadElementType = mlirIntegerTypeGet(context, 1);
       } else if (isSignedIntegerFormat(format)) {
         if (view.itemsize == 4) {
           // i32
