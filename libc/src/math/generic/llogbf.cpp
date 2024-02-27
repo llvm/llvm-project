aa95aa69b96f51a3c19a24a0387ebc1fb7e6e52c
lntue <35648136+lntue@users.noreply.github.com>
2024-02-27 12:23:19 -0500

    [libc][math][c23] Add C23 math functions ilogbf128, logbf128, and llogb(f|l|f128). (#82144)

--- libc/src/math/generic/llogbf.cpp
+++ libc/src/math/generic/llogbf.cpp
@@ -1,4 +1,4 @@
-//===-- Implementation of ilogbf function ---------------------------------===//
+//===-- Implementation of llogbf function ---------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,12 +6,12 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "src/math/ilogbf.h"
+#include "src/math/llogbf.h"
 #include "src/__support/FPUtil/ManipulationFunctions.h"
 #include "src/__support/common.h"
 
 namespace LIBC_NAMESPACE {
 
-LLVM_LIBC_FUNCTION(int, ilogbf, (float x)) { return fputil::intlogb<int>(x); }
+LLVM_LIBC_FUNCTION(long, llogbf, (float x)) { return fputil::intlogb<long>(x); }
 
 } // namespace LIBC_NAMESPACE
