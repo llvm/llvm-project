770694539bde0f1b706c70d87dd70777c94f178f
Joseph Huber <huberjn@outlook.com>
2024-02-27 11:57:47 -0600

    [libc] Work around incorrect fmin/fmax results for +/-x (#83158)
    Summary:
    The IEEE 754 standard as of the 2019 revision states that for fmin -0.0
    is always less than 0.0 and for fmax 0.0 is always greater than 0.0.
    These are currently not respected by the builtin value and thus cause
    the tests to fail. This patch works around it in the implementation for
    now by explicitly modifying the sign bit.

--- libc/src/math/nvptx/fminf.cpp
+++ libc/src/math/nvptx/fminf.cpp
@@ -7,11 +7,18 @@
 //===----------------------------------------------------------------------===//
 
 #include "src/math/fminf.h"
+
+#include "src/__support/CPP/bit.h"
 #include "src/__support/common.h"
+#include "src/__support/macros/optimization.h"
 
 namespace LIBC_NAMESPACE {
 
 LLVM_LIBC_FUNCTION(float, fminf, (float x, float y)) {
+  // FIXME: The builtin function does not correctly handle the +/-0.0 case.
+  if (LIBC_UNLIKELY(x == y))
+    return cpp::bit_cast<float>(cpp::bit_cast<uint32_t>(x) |
+                                cpp::bit_cast<uint32_t>(y));
   return __builtin_fminf(x, y);
 }
 
