024749a3816ae18d8821ffb14022631d571f7ff0
Fangrui Song <i@maskray.me>
2024-02-27 16:32:53 -0800

    [builtins] Disable COMPILER_RT_CRT_USE_EH_FRAME_REGISTRY by default (#83201)
    Most of GCC's Linux targets have a link spec
    `%{!static|static-pie:--eh-frame-hdr}` that doesn't pass --eh-frame-hdr
    for `-static` links. `-static` links are supposed to utilize
    `__register_frame_info` (called by `crtbeginT.o`, not by crtbegin.o or
    crtbeginS.o) as a replacement.
    
    compiler-rt crtbegin (not used with GCC) has some ehframe code, which is
    not utilized because Clang driver unconditionally passes --eh-frame-hdr
    for Linux targets, even for -static. In addition, LLVM libunwind
    implements `__register_frame_info` as an empty stub.
    
    Furthermore, in a non-static link, the `__register_frame_info`
    references can cause an undesired weak dynamic symbol.
    
    For now, just disable the config by default.

--- compiler-rt/test/builtins/Unit/ctor_dtor.c
+++ compiler-rt/test/builtins/Unit/ctor_dtor.c
@@ -9,22 +9,12 @@
 
 // Ensure the various startup functions are called in the proper order.
 
-// CHECK: __register_frame_info()
 /// ctor() is here if ld.so/libc supports DT_INIT/DT_FINI
 // CHECK:      main()
 /// dtor() is here if ld.so/libc supports DT_INIT/DT_FINI
-// CHECK:      __deregister_frame_info()
 
 struct object;
 static int counter;
-
-void __register_frame_info(const void *fi, struct object *obj) {
-  printf("__register_frame_info()\n");
-}
-
-void __deregister_frame_info(const void *fi) {
-  printf("__deregister_frame_info()\n");
-}
 
 void __attribute__((constructor)) ctor() {
   printf("ctor()\n");
