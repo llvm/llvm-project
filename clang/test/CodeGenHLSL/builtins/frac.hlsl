cb9768bbf2ccf9eb67953dc468ab3f703db52b4d
Farzon Lotfi <farzon@farzon.org>
2024-02-28 16:46:04 -0500

    run clang format on tests

--- clang/test/CodeGenHLSL/builtins/frac.hlsl
+++ clang/test/CodeGenHLSL/builtins/frac.hlsl
@@ -12,58 +12,42 @@
 // NO_HALF: define noundef float @"?test_frac_half@@YA$halff@$halff@@Z"(
 // NO_HALF: %dx.frac = call float @llvm.dx.frac.f32(
 // NO_HALF: ret float %dx.frac
-half test_frac_half ( half p0 ) {
-  return frac ( p0 );
-}
+half test_frac_half(half p0) { return frac(p0); }
 // NATIVE_HALF: define noundef <2 x half> @
 // NATIVE_HALF: %dx.frac = call <2 x half> @llvm.dx.frac.v2f16
 // NATIVE_HALF: ret <2 x half> %dx.frac
 // NO_HALF: define noundef <2 x float> @
 // NO_HALF: %dx.frac = call <2 x float> @llvm.dx.frac.v2f32(
 // NO_HALF: ret <2 x float> %dx.frac
-half2 test_frac_half2 ( half2 p0 ) {
-  return frac ( p0 );
-}
+half2 test_frac_half2(half2 p0) { return frac(p0); }
 // NATIVE_HALF: define noundef <3 x half> @
 // NATIVE_HALF: %dx.frac = call <3 x half> @llvm.dx.frac.v3f16
 // NATIVE_HALF: ret <3 x half> %dx.frac
 // NO_HALF: define noundef <3 x float> @
 // NO_HALF: %dx.frac = call <3 x float> @llvm.dx.frac.v3f32(
 // NO_HALF: ret <3 x float> %dx.frac
-half3 test_frac_half3 ( half3 p0 ) {
-  return frac ( p0 );
-}
+half3 test_frac_half3(half3 p0) { return frac(p0); }
 // NATIVE_HALF: define noundef <4 x half> @
 // NATIVE_HALF: %dx.frac = call <4 x half> @llvm.dx.frac.v4f16
 // NATIVE_HALF: ret <4 x half> %dx.frac
 // NO_HALF: define noundef <4 x float> @
 // NO_HALF: %dx.frac = call <4 x float> @llvm.dx.frac.v4f32(
 // NO_HALF: ret <4 x float> %dx.frac
-half4 test_frac_half4 ( half4 p0 ) {
-  return frac ( p0 );
-}
+half4 test_frac_half4(half4 p0) { return frac(p0); }
 
 // CHECK: define noundef float @
 // CHECK: %dx.frac = call float @llvm.dx.frac.f32(
 // CHECK: ret float %dx.frac
-float test_frac_float ( float p0 ) {
-  return frac ( p0 );
-}
+float test_frac_float(float p0) { return frac(p0); }
 // CHECK: define noundef <2 x float> @
 // CHECK: %dx.frac = call <2 x float> @llvm.dx.frac.v2f32
 // CHECK: ret <2 x float> %dx.frac
-float2 test_frac_float2 ( float2 p0 ) {
-  return frac ( p0 );
-}
+float2 test_frac_float2(float2 p0) { return frac(p0); }
 // CHECK: define noundef <3 x float> @
 // CHECK: %dx.frac = call <3 x float> @llvm.dx.frac.v3f32
 // CHECK: ret <3 x float> %dx.frac
-float3 test_frac_float3 ( float3 p0 ) {
-  return frac ( p0 );
-}
+float3 test_frac_float3(float3 p0) { return frac(p0); }
 // CHECK: define noundef <4 x float> @
 // CHECK: %dx.frac = call <4 x float> @llvm.dx.frac.v4f32
 // CHECK: ret <4 x float> %dx.frac
-float4 test_frac_float4 ( float4 p0 ) {
-  return frac ( p0 );
-}
+float4 test_frac_float4(float4 p0) { return frac(p0); }
