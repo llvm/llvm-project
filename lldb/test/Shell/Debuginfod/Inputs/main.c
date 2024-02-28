f66df9bf5c5a59f7b880602727c5f67843ec75b0
Kevin Frei <freik@meta.com>
2024-02-28 15:08:05 -0800

    Working on API test migration

--- lldb/test/Shell/Debuginfod/Inputs/main.c
+++ lldb/test/Shell/Debuginfod/Inputs/main.c
@@ -1,16 +1,10 @@
 // A script to (re)create the .yaml files is in 'make-inputs'. If you make changes
 // you'll need to update the .note.gnu.buildid values in the tests, as the cache names
 
-int func(int argc, const char **argv) { return (argc + 1) * (argv[argc][0] + 2); }
+int func(int argc, const char **argv) {
+  return (argc + 1) * (argv[argc][0] + 2);
+}
 
-__attribute__((force_align_arg_pointer)) void _start(void) {
-
-  /* main body of program: call main(), etc */
-
-  const char *argv[] = {""};
-  func(0, argv);
-
-  /* exit system call */
-  asm("mov $60,%rax; mov $0,%rdi; syscall");
-  __builtin_unreachable(); // tell the compiler to make sure side effects are done before the asm statement
+int main(int argc, const char *argv[]) {
+  return func(0, argv);
 }
