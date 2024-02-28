8eab7547d92086e16c26bb2785f399d392589d59
Nick Desaulniers <ndesaulniers@google.com>
2024-02-28 13:09:48 -0800

    add nolint

--- libc/src/assert/assert.h
+++ libc/src/assert/assert.h
@@ -1,3 +1,4 @@
+// NOLINT(llvm-header-guard)
 //===-- Internal header for assert ------------------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
