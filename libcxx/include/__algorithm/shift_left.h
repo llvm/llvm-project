bd692a49cfe4d07a354b6ab8d99ccc44c3ead0a8
Xiaoyang Liu <siujoeng.lau@gmail.com>
2024-02-27 23:47:20 -0800

    [libc++][ranges] implement ranges::shift_left

--- libcxx/include/__algorithm/shift_left.h
+++ libcxx/include/__algorithm/shift_left.h
@@ -24,30 +24,40 @@ _LIBCPP_BEGIN_NAMESPACE_STD
 
 #if _LIBCPP_STD_VER >= 20
 
+template <class _AlgPolicy, class _Iter, class _Sent>
+inline _LIBCPP_HIDE_FROM_ABI constexpr pair<_Iter, _Iter>
+__shift_left(_Iter __first, _Sent __last, typename iterator_traits<_Iter>::difference_type __n) {
+  _Iter __end = _IterOps<_AlgPolicy>::next(__first, __last);
+
+  if (__n == 0) {
+    return {std::move(__first), std::move(__end)};
+  }
+
+  _Iter __m = __first;
+  if constexpr (__has_random_access_iterator_category<_Iter>::value) {
+    if (__n >= __end - __first) {
+      return {std::move(__first), std::move(__first)};
+    }
+    __m += __n;
+  } else {
+    for (; __n > 0; --__n) {
+      if (__m == __end) {
+        return {std::move(__first), std::move(__first)};
+      }
+      ++__m;
+    }
+  }
+
+  _Iter __result = std::__move<_AlgPolicy>(__m, __end, __first).second;
+  return {std::move(__first), std::move(__result)};
+}
+
 template <class _ForwardIterator>
 inline _LIBCPP_HIDE_FROM_ABI constexpr _ForwardIterator
 shift_left(_ForwardIterator __first,
            _ForwardIterator __last,
            typename iterator_traits<_ForwardIterator>::difference_type __n) {
-  if (__n == 0) {
-    return __last;
-  }
-
-  _ForwardIterator __m = __first;
-  if constexpr (__has_random_access_iterator_category<_ForwardIterator>::value) {
-    if (__n >= __last - __first) {
-      return __first;
-    }
-    __m += __n;
-  } else {
-    for (; __n > 0; --__n) {
-      if (__m == __last) {
-        return __first;
-      }
-      ++__m;
-    }
-  }
-  return std::move(__m, __last, __first);
+  return std::__shift_left<_ClassicAlgPolicy>(std::move(__first), std::move(__last), __n).second;
 }
 
 #endif // _LIBCPP_STD_VER >= 20
