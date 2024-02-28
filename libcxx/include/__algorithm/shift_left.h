a0506d9de25e6e392dbb64f00b573b1c176594dc
Xiaoyang Liu <siujoeng.lau@gmail.com>
2024-02-28 12:33:50 -0800

    [libc++][ranges] add missing headers to 'shift_left.h'

--- libcxx/include/__algorithm/shift_left.h
+++ libcxx/include/__algorithm/shift_left.h
@@ -13,6 +13,7 @@
 #include <__algorithm/move.h>
 #include <__config>
 #include <__iterator/iterator_traits.h>
+#include <__utility/pair.h>
 
 #if !defined(_LIBCPP_HAS_NO_PRAGMA_SYSTEM_HEADER)
 #  pragma GCC system_header
