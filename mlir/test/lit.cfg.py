dd03068780bfca6ae13a09214e7ecf48e2d48285
Oleksandr "Alex" Zinenko <zinenko@google.com>
2024-02-28 09:48:15 +0100

    [mlir] add an example of using transform dialect standalone (#82623)
    Transform dialect interpreter is designed to be usable outside of the
    pass pipeline, as the main program transformation driver, e.g., for
    languages with explicit schedules. Provide an example of such usage with
    a couple of tests.

--- mlir/test/lit.cfg.py
+++ mlir/test/lit.cfg.py
@@ -161,6 +161,7 @@ tools.extend(
         ToolSubst("transform-opt-ch2", unresolved="ignore"),
         ToolSubst("transform-opt-ch3", unresolved="ignore"),
         ToolSubst("transform-opt-ch4", unresolved="ignore"),
+        ToolSubst("mlir-transform-opt", unresolved="ignore"),
         ToolSubst("%mlir_lib_dir", config.mlir_lib_dir, unresolved="ignore"),
         ToolSubst("%mlir_src_dir", config.mlir_src_root, unresolved="ignore"),
     ]
