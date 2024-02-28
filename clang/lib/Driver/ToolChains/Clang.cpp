d17ca2f6dd88ad5920c372b64b61ceef1b9e6e89
Daniel Hoekwater <hoekwater@google.com>
2024-02-28 01:34:48 +0000

    [Driver] Allow -fbasic-block-address-map for AArch64 ELF (#82662)
    Emitting the basic block address map with
    `-fbasic-block-sections=labels` is allowed for AArch64 ELF since
    7eaf94fefa1250fc8a46982cea8ce99abacae11f. Allow doing so with
    `-fbasic-block-address-map`.

--- clang/lib/Driver/ToolChains/Clang.cpp
+++ clang/lib/Driver/ToolChains/Clang.cpp
@@ -5958,7 +5958,7 @@ void Clang::ConstructJob(Compilation &C, const JobAction &JA,
 
   if (Arg *A = Args.getLastArg(options::OPT_fbasic_block_address_map,
                                options::OPT_fno_basic_block_address_map)) {
-    if (Triple.isX86() && Triple.isOSBinFormatELF()) {
+    if ((Triple.isX86() || Triple.isAArch64()) && Triple.isOSBinFormatELF()) {
       if (A->getOption().matches(options::OPT_fbasic_block_address_map))
         A->render(Args, CmdArgs);
     } else {
