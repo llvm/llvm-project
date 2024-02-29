a25b740cc2ec9c27df40fb6c2e41d31b6e521265
Hristo Hristov <hghristov.rmm@gmail.com>
2024-02-29 11:05:55 +0200

    Tests: `spanbuf` constructor tests

--- libcxx/test/std/input.output/span.streams/spanbuf/spanbuf.cons/mode.pass.cpp
+++ libcxx/test/std/input.output/span.streams/spanbuf/spanbuf.cons/mode.pass.cpp
@@ -15,36 +15,68 @@
 //     : public basic_streambuf<charT, traits> {
 
 //     // [spanbuf.cons], constructors
-//     basic_spanbuf();
+//
+//     explicit basic_spanbuf(ios_base::openmode which);
 
 #include <cassert>
+#include <concepts>
+#include <span>
 #include <spanstream>
 
 #include "constexpr_char_traits.h"
 #include "nasty_string.h"
+#include "test_convertible.h"
 #include "test_macros.h"
+
+#include "../../types.h"
 
 void test_sfinae_with_nasty_char() {
   using SpBuf = std::basic_spanbuf<nasty_char, nasty_char_traits>;
 
-  static_assert(std::default_initializable<SpBuf>);
+  static_assert(std::constructible_from<SpBuf, std::ios_base::openmode>);
+  static_assert(!test_convertible<SpBuf, std::ios_base::openmode>());
 }
 
 template <typename CharT, typename TraitsT = std::char_traits<CharT>>
 void test_sfinae() {
   using SpBuf = std::basic_spanbuf<CharT, TraitsT>;
 
-  static_assert(std::default_initializable<SpBuf>);
+  // `Mode`
+  static_assert(std::constructible_from<SpBuf, std::ios_base::openmode>);
+  static_assert(!test_convertible<SpBuf, std::ios_base::openmode>());
+
+  // Non-mode
+  static_assert(!std::constructible_from<SpBuf, const NonMode>);
+  static_assert(!test_convertible<SpBuf, const NonMode>());
 }
 
 template <typename CharT, typename TraitsT = std::char_traits<CharT>>
 void test() {
-  using SpanBuf = std::basic_spanbuf<CharT, TraitsT>;
+  using SpBuf = std::basic_spanbuf<CharT, TraitsT>;
 
-  SpanBuf spBuf;
-  assert(spBuf.span().data() == nullptr);
-  assert(spBuf.span().empty());
-  assert(spBuf.span().size() == 0);
+  static_assert(std::default_initializable<SpBuf>);
+
+  // Mode: `in`
+  {
+    SpBuf spBuf(std::ios_base::in);
+    assert(spBuf.span().data() == nullptr);
+    assert(spBuf.span().empty());
+    assert(spBuf.span().size() == 0);
+  }
+  // Mode: `out`
+  {
+    SpBuf spBuf(std::ios_base::out);
+    assert(spBuf.span().data() == nullptr);
+    assert(spBuf.span().empty());
+    assert(spBuf.span().size() == 0);
+  }
+  // Mode: multiple
+  {
+    SpBuf spBuf(std::ios_base::out | std::ios_base::in | std::ios_base::binary);
+    assert(spBuf.span().data() == nullptr);
+    assert(spBuf.span().empty());
+    assert(spBuf.span().size() == 0);
+  }
 }
 
 int main(int, char**) {
@@ -54,6 +86,8 @@ int main(int, char**) {
   test<char>();
   test<char, constexpr_char_traits<char>>();
 #ifndef TEST_HAS_NO_WIDE_CHARACTERS
+  test_sfinae<wchar_t>();
+  test_sfinae<wchar_t, constexpr_char_traits<wchar_t>>();
   test<wchar_t>();
   test<wchar_t, constexpr_char_traits<wchar_t>>();
 #endif
