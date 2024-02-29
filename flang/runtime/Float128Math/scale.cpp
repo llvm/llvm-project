c3d60c278e24f265c37568bbec929471477cea8b
Slava Zakharin <szakharin@nvidia.com>
2024-02-29 09:05:43 -0800

    [flang][runtime] Moved support for some REAL(16) intrinsics to Float128Math. (#83383)
    This adds support for 128-bit float versions of SCALE, NEAREST, MOD,
    MODULO, SET_EXPONENT, EXPONENT, FRACTION, SPACING and RRSPACING.

--- flang/runtime/Float128Math/scale.cpp
+++ flang/runtime/Float128Math/scale.cpp
@@ -1,4 +1,4 @@
-//===-- runtime/Float128Math/asinh.cpp ------------------------------------===//
+//===-- runtime/Float128Math/scale.cpp ------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -7,14 +7,20 @@
 //===----------------------------------------------------------------------===//
 
 #include "math-entries.h"
+#include "numeric-template-specs.h"
+#include <limits>
 
 namespace Fortran::runtime {
 extern "C" {
 
 #if LDBL_MANT_DIG == 113 || HAS_FLOAT128
-CppTypeFor<TypeCategory::Real, 16> RTDEF(AsinhF128)(
-    CppTypeFor<TypeCategory::Real, 16> x) {
-  return Asinh<true>::invoke(x);
+F128Type RTDEF(Scale16)(F128Type x, std::int64_t p) {
+  auto ip{static_cast<int>(p)};
+  if (ip != p) {
+    ip = p < 0 ? std::numeric_limits<int>::min()
+               : std::numeric_limits<int>::max();
+  }
+  return LDEXPTy<F128Type>::compute(x, ip);
 }
 #endif
 
