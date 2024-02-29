4c1726418b63407b16010d0bfbeac46b5aff2b2a
Guillaume Chatelet <gchatelet@google.com>
2024-02-29 10:28:53 +0100

    [libc][NFC] rename `float.h` macro file to `types.h` (#83190)

--- libc/src/math/ldexpf128.h
+++ libc/src/math/ldexpf128.h
@@ -9,7 +9,7 @@
 #ifndef LLVM_LIBC_SRC_MATH_LDEXPF128_H
 #define LLVM_LIBC_SRC_MATH_LDEXPF128_H
 
-#include "src/__support/macros/properties/float.h"
+#include "src/__support/macros/properties/types.h"
 
 namespace LIBC_NAMESPACE {
 
