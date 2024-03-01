98ca9a958046b50cac4affa40bdd5a7fdf30828d
Kolya Panchenko <kolya.panchenko@sifive.com>
2024-03-01 05:48:14 -0800

    removed isLegalMaskedCompressStore from RISCVTTIImpl

--- llvm/lib/Target/RISCV/RISCVTargetTransformInfo.h
+++ llvm/lib/Target/RISCV/RISCVTargetTransformInfo.h
@@ -261,8 +261,6 @@ public:
     return TLI->isLegalStridedLoadStore(DataTypeVT, Alignment);
   }
 
-  bool isLegalMaskedCompressStore(Type *DataTy, Align Alignment);
-
   bool isVScaleKnownToBeAPowerOfTwo() const {
     return TLI->isVScaleKnownToBeAPowerOfTwo();
   }
