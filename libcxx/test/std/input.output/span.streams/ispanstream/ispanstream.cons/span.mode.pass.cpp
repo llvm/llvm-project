9e816e2ad2039c68cab3c41f2facc413100ce55d
Hristo Hristov <hghristov.rmm@gmail.com>
2024-03-01 09:49:44 +0200

    Tests: `ispanstream` constructors - WIP

---
+++ libcxx/test/std/input.output/span.streams/ispanstream/ispanstream.cons/span.mode.pass.cpp
@@ -0,0 +1,136 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++03, c++11, c++14, c++17, c++20
+
+// <spanstream>
+
+//   template<class charT, class traits = char_traits<charT>>
+//   class basic_ispanstream
+//     : public basic_streambuf<charT, traits> {
+
+//     // [spanbuf.cons], constructors
+//
+//     explicit basic_ispanstream(std::span<charT> s,
+//                                ios_base::openmode which = ios_base::in);
+
+#include <cassert>
+#include <concepts>
+#include <span>
+#include <spanstream>
+#include <utility>
+
+#include "constexpr_char_traits.h"
+#include "nasty_string.h"
+#include "test_convertible.h"
+#include "test_macros.h"
+
+#include "../../macros.h"
+#include "../../types.h"
+
+void test_sfinae_with_nasty_char() {
+  using SpStream = std::basic_ispanstream<nasty_char, nasty_char_traits>;
+
+  // Mode
+  static_assert(std::constructible_from<SpStream, const std::span<nasty_char>, std::ios_base::openmode>);
+  static_assert(!test_convertible<SpStream, std::ios_base::openmode>());
+
+  // Non-mode
+  static_assert(!std::constructible_from<SpStream, const std::span<nasty_char>, const NonMode>);
+  static_assert(!test_convertible<SpStream, const std::span<nasty_char>, const NonMode>());
+}
+
+template <typename CharT, typename TraitsT = std::char_traits<CharT>>
+void test_sfinae() {
+  using SpStream = std::basic_ispanstream<CharT, TraitsT>;
+
+  // Mode
+  static_assert(std::constructible_from<SpStream, const std::span<CharT>, std::ios_base::openmode>);
+  static_assert(!test_convertible<SpStream, const std::span<CharT>, std::ios_base::openmode>());
+
+  // Non-mode
+  static_assert(!std::constructible_from<SpStream, const std::span<CharT>, const NonMode>);
+  static_assert(!test_convertible<SpStream, const std::span<CharT>, const NonMode>());
+}
+
+template <typename CharT, typename TraitsT = std::char_traits<CharT>>
+void test() {
+  using SpStream = std::basic_ispanstream<CharT, TraitsT>;
+
+  CharT arr[4];
+  std::span<CharT> sp{arr};
+
+  // Mode: default
+  {
+    SpStream sps(sp);
+    assert(sps.span().data() == arr);
+    assert(!sps.span().empty());
+    assert(sps.span().size() == 4);
+  }
+  {
+    SpStream sps(std::as_const(sp));
+    assert(sps.span().data() == arr);
+    assert(!sps.span().empty());
+    assert(sps.span().size() == 4);
+  }
+  // Mode: `ios_base::in`
+  {
+    SpStream sps(sp, std::ios_base::in);
+    assert(sps.span().data() == arr);
+    assert(!sps.span().empty());
+    assert(sps.span().size() == 4);
+  }
+  {
+    SpStream sps(std::as_const(sp), std::ios_base::in);
+    assert(sps.span().data() == arr);
+    assert(!sps.span().empty());
+    assert(sps.span().size() == 4);
+  }
+  // Mode `ios_base::out`
+  {
+    SpStream sps(sp, std::ios_base::out);
+    assert(sps.span().data() == arr);
+    assert(sps.span().empty());
+    assert(sps.span().size() == 0);
+  }
+  {
+    SpStream sps(std::as_const(sp), std::ios_base::out);
+    assert(sps.span().data() == arr);
+    assert(sps.span().empty());
+    assert(sps.span().size() == 0);
+  }
+  // Mode: multiple
+  {
+    SpStream sps(sp, std::ios_base::in | std::ios_base::out | std::ios_base::binary);
+    assert(sps.span().data() == arr);
+    assert(sps.span().empty());
+    assert(sps.span().size() == 0);
+  }
+  {
+    SpStream sps(std::as_const(sp), std::ios_base::in | std::ios_base::out | std::ios_base::binary);
+    assert(sps.span().data() == arr);
+    assert(sps.span().empty());
+    assert(sps.span().size() == 0);
+  }
+}
+
+int main(int, char**) {
+  test_sfinae_with_nasty_char();
+  test_sfinae<char>();
+  test_sfinae<char, constexpr_char_traits<char>>();
+  test<char>();
+  test<char, constexpr_char_traits<char>>();
+#ifndef TEST_HAS_NO_WIDE_CHARACTERS
+  test_sfinae<wchar_t>();
+  test_sfinae<wchar_t, constexpr_char_traits<wchar_t>>();
+  test<wchar_t>();
+  test<wchar_t, constexpr_char_traits<wchar_t>>();
+#endif
+
+  return 0;
+}
