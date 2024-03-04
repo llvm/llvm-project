2fe81edef6f0b35abffbbc59b649b30ea9c15a62
Jeremy Morse <jeremy.morse@sony.com>
2024-03-04 14:51:56 +0000

    [NFC][RemoveDIs] Insert instruction using iterators in Transforms/
    As part of the RemoveDIs project we need LLVM to insert instructions using
    iterators wherever possible, so that the iterators can carry a bit of
    debug-info. This commit implements some of that by updating the contents of
    llvm/lib/Transforms/Utils to always use iterator-versions of instruction
    constructors.
    
    There are two general flavours of update:
     * Almost all call-sites just call getIterator on an instruction
     * Several make use of an existing iterator (scenarios where the code is
       actually significant for debug-info)
    The underlying logic is that any call to getFirstInsertionPt or similar
    APIs that identify the start of a block need to have that iterator passed
    directly to the insertion function, without being converted to a bare
    Instruction pointer along the way.
    
    Noteworthy changes:
     * FindInsertedValue now takes an optional iterator rather than an
       instruction pointer, as we need to always insert with iterators,
     * I've added a few iterator-taking versions of some value-tracking and
       DomTree methods -- they just unwrap the iterator. These are purely
       convenience methods to avoid extra syntax in some passes.
     * A few calls to getNextNode become std::next instead (to keep in the
       theme of using iterators for positions),
     * SeparateConstOffsetFromGEP has it's insertion-position field changed.
       Noteworthy because it's not a purely localised spelling change.
    
    All this should be NFC.

--- llvm/lib/Transforms/InstCombine/InstCombineCalls.cpp
+++ llvm/lib/Transforms/InstCombine/InstCombineCalls.cpp
@@ -3140,28 +3140,30 @@ Instruction *InstCombinerImpl::visitCallInst(CallInst &CI) {
       if (match(BO0, m_VecReverse(m_Value(X)))) {
         // rev(binop rev(X), rev(Y)) --> binop X, Y
         if (match(BO1, m_VecReverse(m_Value(Y))))
-          return replaceInstUsesWith(CI,
-                                     BinaryOperator::CreateWithCopiedFlags(
-                                         OldBinOp->getOpcode(), X, Y, OldBinOp,
-                                         OldBinOp->getName(), II));
+          return replaceInstUsesWith(CI, BinaryOperator::CreateWithCopiedFlags(
+                                             OldBinOp->getOpcode(), X, Y,
+                                             OldBinOp, OldBinOp->getName(),
+                                             II->getIterator()));
         // rev(binop rev(X), BO1Splat) --> binop X, BO1Splat
         if (isSplatValue(BO1))
-          return replaceInstUsesWith(CI,
-                                     BinaryOperator::CreateWithCopiedFlags(
-                                         OldBinOp->getOpcode(), X, BO1,
-                                         OldBinOp, OldBinOp->getName(), II));
+          return replaceInstUsesWith(CI, BinaryOperator::CreateWithCopiedFlags(
+                                             OldBinOp->getOpcode(), X, BO1,
+                                             OldBinOp, OldBinOp->getName(),
+                                             II->getIterator()));
       }
       // rev(binop BO0Splat, rev(Y)) --> binop BO0Splat, Y
       if (match(BO1, m_VecReverse(m_Value(Y))) && isSplatValue(BO0))
-        return replaceInstUsesWith(CI, BinaryOperator::CreateWithCopiedFlags(
-                                           OldBinOp->getOpcode(), BO0, Y,
-                                           OldBinOp, OldBinOp->getName(), II));
+        return replaceInstUsesWith(CI,
+                                   BinaryOperator::CreateWithCopiedFlags(
+                                       OldBinOp->getOpcode(), BO0, Y, OldBinOp,
+                                       OldBinOp->getName(), II->getIterator()));
     }
     // rev(unop rev(X)) --> unop X
     if (match(Vec, m_OneUse(m_UnOp(m_VecReverse(m_Value(X)))))) {
       auto *OldUnOp = cast<UnaryOperator>(Vec);
       auto *NewUnOp = UnaryOperator::CreateWithCopiedFlags(
-          OldUnOp->getOpcode(), X, OldUnOp, OldUnOp->getName(), II);
+          OldUnOp->getOpcode(), X, OldUnOp, OldUnOp->getName(),
+          II->getIterator());
       return replaceInstUsesWith(CI, NewUnOp);
     }
     break;
