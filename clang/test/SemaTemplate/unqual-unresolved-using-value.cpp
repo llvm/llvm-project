ce1e5a979e7ee3c1b3e1bbc5254524c39815c8e1
Krystian Stasiowski <sdkrystian@gmail.com>
2024-03-05 08:38:58 -0500

    [FOLD] update test

--- clang/test/SemaTemplate/unqual-unresolved-using-value.cpp
+++ clang/test/SemaTemplate/unqual-unresolved-using-value.cpp
@@ -4,13 +4,16 @@ template<typename T>
 struct A : T {
   using T::f;
   using T::g;
+  using T::h;
 
   void f();
   void g();
 
-  void h() {
+  void i() {
     f<int>();
     g<int>(); // expected-error{{no member named 'g' in 'A<B>'}}
+    h<int>(); // expected-error{{expected '(' for function-style cast or type construction}}
+              // expected-error@-1{{expected expression}}
   }
 };
 
@@ -19,6 +22,9 @@ struct B {
   void f();
 
   void g();
+
+  template<typename T>
+  void h();
 };
 
-template struct A<B>; // expected-note{{in instantiation of member function 'A<B>::h' requested here}}
+template struct A<B>; // expected-note{{in instantiation of member function 'A<B>::i' requested here}}
