04af88d5dc417a6f9cc46434ed873d8107a63894
Shengchen Kan <shengchen.kan@intel.com>
2024-03-05 13:31:43 +0800

    check line info in error test

--- llvm/test/MC/X86/apx/ccmp-intel-error.s
+++ llvm/test/MC/X86/apx/ccmp-intel-error.s
@@ -1,26 +1,21 @@
 # RUN: not llvm-mc -triple x86_64 -show-encoding -x86-asm-syntax=intel -output-asm-variant=1 %s 2>&1 | FileCheck --strict-whitespace %s
 
-# CHECK: error: Expected { at this point
+# CHECK: [[#@LINE+2]]:7: error: Expected { at this point
 # CHECK: ccmpe 1 rbx, rax
-# CHECK:       ^
 ccmpe 1 rbx, rax
 
-# CHECK: error: Expected } or , at this point
+# CHECK: [[#@LINE+2]]:14: error: Expected } or , at this point
 # CHECK: ccmpe {sf,cf rbx, rax
-# CHECK:              ^
 ccmpe {sf,cf rbx, rax
 
-# CHECK: error: Invalid conditional flags
+# CHECK: [[#@LINE+2]]:8: error: Invalid conditional flags
 # CHECK: ccmpe {pf} rbx, rax
-# CHECK:        ^
 ccmpe {pf} rbx, rax
 
-# CHECK: error: Duplicated conditional flag
+# CHECK: [[#@LINE+2]]:15: error: Duplicated conditional flag
 # CHECK: ccmpeq {of,zf,of} rbx, rax
-# CHECK:               ^
 ccmpeq {of,zf,of} rbx, rax
 
-# CHECK: error: Expected } at this point
+# CHECK: [[#@LINE+2]]:20: error: Expected } at this point
 # CHECK: ccmpeq {of,sf,zf,cf,of} rbx, rax
-# CHECK:                    ^
 ccmpeq {of,sf,zf,cf,of} rbx, rax
