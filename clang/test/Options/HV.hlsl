87ae56a7d129e0f04197757d50189a3921c75161
Joshua Batista <jbatista@microsoft.com>
2024-03-05 10:55:52 -0800

    address Justin

--- clang/test/Options/HV.hlsl
+++ clang/test/Options/HV.hlsl
@@ -1,7 +1,7 @@
 // RUN: %clang_dxc -T lib_6_4 -HV 2016 %s 2>&1 -###   | FileCheck -check-prefix=2016 %s
 // RUN: %clang_dxc -T lib_6_4 -HV 2017 %s 2>&1 -###   | FileCheck -check-prefix=2017 %s
-// RUN: %clang_dxc -T lib_6_4 -HV 2018 %s 2>&1 -###   | FileCheck -check-prefix=2018 %s
-// RUN: %clang_dxc -T lib_6_4 -HV 2021 %s 2>&1 -###   | FileCheck -check-prefix=2021 %s
+// RUN: %clang_dxc -T lib_6_4 /HV 2018 %s 2>&1 -###   | FileCheck -check-prefix=2018 %s
+// RUN: %clang_dxc -T lib_6_4 /HV 2021 %s 2>&1 -###   | FileCheck -check-prefix=2021 %s
 
 // 2016: "-std=hlsl2016"
 // 2017: "-std=hlsl2017"
