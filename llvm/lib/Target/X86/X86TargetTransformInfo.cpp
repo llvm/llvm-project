1547c01df745407758212e6e496989fa4ed93948
Simon Pilgrim <llvm-dev@redking.me.uk>
2024-03-05 17:42:30 +0000

    [CostModel][X86] SK_InsertSubvector - if the legalized base/subvector types match then the insert is now just a copy

--- llvm/lib/Target/X86/X86TargetTransformInfo.cpp
+++ llvm/lib/Target/X86/X86TargetTransformInfo.cpp
@@ -1550,6 +1550,14 @@ InstructionCost X86TTIImpl::getShuffleCost(TTI::ShuffleKind Kind,
   if (Kind == TTI::SK_InsertSubvector && LT.second.isVector()) {
     int NumElts = LT.second.getVectorNumElements();
     std::pair<InstructionCost, MVT> SubLT = getTypeLegalizationCost(SubTp);
+
+    // If we're inserting into the same legalized type then its not an
+    // insert anymore, its a copy.
+    if (LT.second == SubLT.second &&
+        BaseTp->getPrimitiveSizeInBits() >= LT.second.getSizeInBits() &&
+        SubTp->getPrimitiveSizeInBits() >= SubLT.second.getSizeInBits())
+      return TTI::TCC_Free;
+
     if (SubLT.second.isVector()) {
       int NumSubElts = SubLT.second.getVectorNumElements();
       if ((Index % NumSubElts) == 0 && (NumElts % NumSubElts) == 0)
