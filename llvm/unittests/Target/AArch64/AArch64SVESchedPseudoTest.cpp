3a4a3facbe82e0b968b852fbfd720ed35709944b
David Green <david.green@arm.com>
2024-03-06 15:23:02 +0000

    [AArch64] Ensure Neoverse V1 scheduling model includes all SVE pseudos.
    With the many pseudos used in SVE codegen it can be too easy to miss
    instructions. This enables the existing test we have for checking the
    scheduling info of the pseudos matches the real instructions, and adjusts the
    scheduling info in the NeoverseV1 model to make sure all are handled. In the
    cases I could I opted to use the same info as in the NeoverseV2 model, to keep
    the differences smaller.

--- llvm/unittests/Target/AArch64/AArch64SVESchedPseudoTest.cpp
+++ llvm/unittests/Target/AArch64/AArch64SVESchedPseudoTest.cpp
@@ -107,6 +107,10 @@ TEST(AArch64SVESchedPseudoTesta510, IsCorrect) {
   runSVEPseudoTestForCPU("cortex-a510");
 }
 
+TEST(AArch64SVESchedPseudoTestv1, IsCorrect) {
+  runSVEPseudoTestForCPU("neoverse-v1");
+}
+
 TEST(AArch64SVESchedPseudoTestv2, IsCorrect) {
   runSVEPseudoTestForCPU("neoverse-v2");
 }
