e9e1892062a09f54e71d0cae2d0716074d2300f1
Ulrich Weigand <ulrich.weigand@de.ibm.com>
2024-03-06 20:49:12 +0100

    [libomptarget] Support BE ELF files in plugins-nextgen (#83976)
    Code in plugins-nextgen reading ELF files is currently hard-coded to
    assume a 64-bit little-endian ELF format. Unfortunately, this assumption
    is even embedded in the interface between GlobalHandler and Utils/ELF
    routines, which use ELF64LE types.
    
    To fix this, I've refactored the interface to push all ELF specific
    types into Utils/ELF. Specifically, this patch removes both the
    getSymbol and getSymbolAddress routines and replaces them with a
    single findSymbolInImage, which gets a StringRef identifying the
    raw object file image as input, and returns a StringRef covering
    the data addressed by the symbol (address and size) if found, or
    std::nullopt otherwise.
    
    This allows properly templating over multiple ELF format variants inside
    Utils/ELF; specifically, this patch adds support for 64-bit big-endian
    ELF files in addition to 64-bit little-endian files.

--- openmp/libomptarget/plugins-nextgen/common/src/GlobalHandler.cpp
+++ openmp/libomptarget/plugins-nextgen/common/src/GlobalHandler.cpp
@@ -25,16 +25,6 @@ using namespace omp;
 using namespace target;
 using namespace plugin;
 
-Expected<ELF64LEObjectFile>
-GenericGlobalHandlerTy::getELFObjectFile(DeviceImageTy &Image) {
-  assert(utils::elf::isELF(Image.getMemoryBuffer().getBuffer()) &&
-         "Input is not an ELF file");
-
-  Expected<ELF64LEObjectFile> ElfOrErr =
-      ELF64LEObjectFile::create(Image.getMemoryBuffer());
-  return ElfOrErr;
-}
-
 Error GenericGlobalHandlerTy::moveGlobalBetweenDeviceAndHost(
     GenericDeviceTy &Device, DeviceImageTy &Image, const GlobalTy &HostGlobal,
     bool Device2Host) {
@@ -81,55 +71,37 @@ Error GenericGlobalHandlerTy::moveGlobalBetweenDeviceAndHost(
 bool GenericGlobalHandlerTy::isSymbolInImage(GenericDeviceTy &Device,
                                              DeviceImageTy &Image,
                                              StringRef SymName) {
-  // Get the ELF object file for the image. Notice the ELF object may already
-  // be created in previous calls, so we can reuse it. If this is unsuccessful
-  // just return false as we couldn't find it.
-  auto ELFObjOrErr = getELFObjectFile(Image);
-  if (!ELFObjOrErr) {
-    consumeError(ELFObjOrErr.takeError());
-    return false;
-  }
 
   // Search the ELF symbol using the symbol name.
-  auto SymOrErr = utils::elf::getSymbol(*ELFObjOrErr, SymName);
+  auto SymOrErr = utils::elf::findSymbolInImage(
+      Image.getMemoryBuffer().getBuffer(), SymName);
   if (!SymOrErr) {
     consumeError(SymOrErr.takeError());
     return false;
   }
 
-  return *SymOrErr;
+  return SymOrErr->has_value();
 }
 
 Error GenericGlobalHandlerTy::getGlobalMetadataFromImage(
     GenericDeviceTy &Device, DeviceImageTy &Image, GlobalTy &ImageGlobal) {
 
-  // Get the ELF object file for the image. Notice the ELF object may already
-  // be created in previous calls, so we can reuse it.
-  auto ELFObj = getELFObjectFile(Image);
-  if (!ELFObj)
-    return ELFObj.takeError();
-
   // Search the ELF symbol using the symbol name.
-  auto SymOrErr = utils::elf::getSymbol(*ELFObj, ImageGlobal.getName());
+  auto SymOrErr = utils::elf::findSymbolInImage(
+      Image.getMemoryBuffer().getBuffer(), ImageGlobal.getName());
   if (!SymOrErr)
     return Plugin::error("Failed ELF lookup of global '%s': %s",
                          ImageGlobal.getName().data(),
                          toString(SymOrErr.takeError()).data());
 
-  if (!*SymOrErr)
+  if (!SymOrErr->has_value())
     return Plugin::error("Failed to find global symbol '%s' in the ELF image",
                          ImageGlobal.getName().data());
 
-  auto AddrOrErr = utils::elf::getSymbolAddress(*ELFObj, **SymOrErr);
-  // Get the section to which the symbol belongs.
-  if (!AddrOrErr)
-    return Plugin::error("Failed to get ELF symbol from global '%s': %s",
-                         ImageGlobal.getName().data(),
-                         toString(AddrOrErr.takeError()).data());
-
   // Setup the global symbol's address and size.
-  ImageGlobal.setPtr(const_cast<void *>(*AddrOrErr));
-  ImageGlobal.setSize((*SymOrErr)->st_size);
+  auto Symbol = **SymOrErr;
+  ImageGlobal.setPtr(static_cast<void *>(const_cast<char *>(Symbol.data())));
+  ImageGlobal.setSize(Symbol.size());
 
   return Plugin::success();
 }
