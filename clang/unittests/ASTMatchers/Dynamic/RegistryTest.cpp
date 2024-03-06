9e375924265534aa29b842ecd6f6ae8759e7668a
Balazs Benics <benicsbalazs@gmail.com>
2024-03-06 15:27:04 +0100

    Shorten abs paths, and use `//===-` leading

--- clang/unittests/ASTMatchers/Dynamic/RegistryTest.cpp
+++ clang/unittests/ASTMatchers/Dynamic/RegistryTest.cpp
@@ -1,4 +1,4 @@
-//===- unittest/ASTMatchers/Dynamic/RegistryTest.cpp - Registry unit tests ===//
+//===- RegistryTest.cpp - Registry unit tests -----------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
