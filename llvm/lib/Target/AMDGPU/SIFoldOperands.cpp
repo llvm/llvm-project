1af318e8bacc13a5118303f6f444a8983e0ce9f0
Martin Wehking <martin.wehking@codeplay.com>
2024-03-06 16:06:37 +0000

    Replace non-null check with assert

--- llvm/lib/Target/AMDGPU/SIFoldOperands.cpp
+++ llvm/lib/Target/AMDGPU/SIFoldOperands.cpp
@@ -1054,7 +1054,8 @@ void SIFoldOperands::foldOperand(
       // Don't fold if OpToFold doesn't hold an aligned register.
       const TargetRegisterClass *RC =
           TRI->getRegClassForReg(*MRI, OpToFold.getReg());
-      if (RC && TRI->hasVectorRegisters(RC) && OpToFold.getSubReg()) {
+      assert(RC);
+      if (TRI->hasVectorRegisters(RC) && OpToFold.getSubReg()) {
         unsigned SubReg = OpToFold.getSubReg();
         if (const TargetRegisterClass *SubRC =
                 TRI->getSubRegisterClass(RC, SubReg))
