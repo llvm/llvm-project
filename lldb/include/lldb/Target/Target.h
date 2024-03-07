d5c4f828915554213b869bd8b226aad485a52a03
Dave Lee <davelee.com@gmail.com>
2024-03-06 20:41:46 -0800

    [lldb] Extract getter function for experimental target properties (NFC) (#83504)
    In Swift's downstream lldb, there are a number of experimental properties. This change 
    extracts a getter function containing the common logic for getting a boolean valued 
    experimental property.
    
    This also deletes `SetInjectLocalVariables` which isn't used anywhere.

--- lldb/include/lldb/Target/Target.h
+++ lldb/include/lldb/Target/Target.h
@@ -244,8 +244,6 @@ public:
 
   bool GetInjectLocalVariables(ExecutionContext *exe_ctx) const;
 
-  void SetInjectLocalVariables(ExecutionContext *exe_ctx, bool b);
-
   void SetRequireHardwareBreakpoints(bool b);
 
   bool GetRequireHardwareBreakpoints() const;
@@ -259,6 +257,10 @@ public:
   bool GetDebugUtilityExpression() const;
 
 private:
+  std::optional<bool>
+  GetExperimentalPropertyValue(size_t prop_idx,
+                               ExecutionContext *exe_ctx = nullptr) const;
+
   // Callbacks for m_launch_info.
   void Arg0ValueChangedCallback();
   void RunArgsValueChangedCallback();
