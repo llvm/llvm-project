77a0947fc625ffc8859da7b0aaca2634916b701c
Kojo Acquah <kooljblack@google.com>
2024-03-08 22:34:05 +0000

    further cleanup

--- mlir/lib/Dialect/ArmNeon/Transforms/LowerContractionToSMMLAPattern.cpp
+++ mlir/lib/Dialect/ArmNeon/Transforms/LowerContractionToSMMLAPattern.cpp
@@ -36,8 +36,8 @@ static Type matchContainerType(Type element, Type container) {
   return element;
 }
 
-/// Lowering from vector::contractOp directly to the arm neon
-/// intrinsic.
+/// Lowering from a single vector::contractOp directly to the arm neon smmla
+/// intrinsic. The shapes of the contract and intrinsic must match.
 class LowerContractionToSMMLAPattern
     : public OpRewritePattern<vector::ContractionOp> {
 public:
@@ -49,7 +49,7 @@ public:
     Value rhs = op.getRhs();
     Value res = op.getAcc();
 
-    // Check index maps represent M N K and aren't transposed.
+    // Check index maps that represent M N K in contract.
     auto indexingMaps = op.getIndexingMapsArray();
     if (llvm::any_of(indexingMaps, [](mlir::AffineMap affineMap) {
           return affineMap.isPermutation() || affineMap.getNumDims() != 3 ||
@@ -58,7 +58,7 @@ public:
       return failure();
     }
 
-    // Check iterator types for contract
+    // Check iterator types for contract.
     auto iteratorTypes = op.getIteratorTypesArray();
     if (iteratorTypes.size() != 3 ||
         iteratorTypes[0] != vector::IteratorType::parallel ||
@@ -67,10 +67,7 @@ public:
       return failure();
     }
 
-    // Check the tile size by mapping the dimensions of the contract
-    //  -- Tile size: [2, 2, 8]
-    // Infer tile sizes from operands. Check required tile size
-    // Note: RHS is not transposed
+    // Check the tile size by mapping the dimensions of the contract.
     mlir::VectorType lhsType = op.getLhsType();
     mlir::VectorType rhsType = op.getRhsType();
     auto dimM = lhsType.getDimSize(0);
@@ -80,7 +77,7 @@ public:
       return failure();
     }
 
-    // Check two extsi inputs Rhs Lhs
+    // Check two extsi inputs Rhs Lhs for contract.
     arith::ExtSIOp origLhsExtOp =
         dyn_cast_or_null<arith::ExtSIOp>(lhs.getDefiningOp());
     arith::ExtSIOp origRhsExtOp =
@@ -89,28 +86,26 @@ public:
       return failure();
     }
 
-    arith::ExtSIOp extsiLhs;
-    arith::ExtSIOp extsiRhs;
     // Match any iX to i32 for X<8 then turn into an i8 output. Feed into
     // following neon instruction. Check inputs for extsi are <=i8
+    Value extsiLhs;
+    Value extsiRhs;
     if (auto lhsExtInType =
             origLhsExtOp.getIn().getType().dyn_cast<mlir::VectorType>()) {
       if (lhsExtInType.getElementTypeBitWidth() <= 8) {
-        // Target lhs type with i8. This is likely redundant
         Type targetLhsExtTy =
             matchContainerType(rewriter.getI8Type(), lhsExtInType);
-        extsiLhs = rewriter.create<arith::ExtSIOp>(loc, targetLhsExtTy,
-                                                   origLhsExtOp.getIn());
+        extsiLhs = rewriter.createOrFold<arith::ExtSIOp>(loc, targetLhsExtTy,
+                                                         origLhsExtOp.getIn());
       }
     }
     if (auto rhsExtInType =
             origRhsExtOp.getIn().getType().dyn_cast<mlir::VectorType>()) {
       if (rhsExtInType.getElementTypeBitWidth() <= 8) {
-        // Target rhs type with i8
         Type targetRhsExtTy =
             matchContainerType(rewriter.getI8Type(), rhsExtInType);
-        extsiRhs = rewriter.create<arith::ExtSIOp>(loc, targetRhsExtTy,
-                                                   origRhsExtOp.getIn());
+        extsiRhs = rewriter.createOrFold<arith::ExtSIOp>(loc, targetRhsExtTy,
+                                                         origRhsExtOp.getIn());
       }
     }
 
@@ -123,15 +118,15 @@ public:
         {16}, extsiLhs.getType().cast<ShapedType>().getElementType());
     auto collapsedOutputType =
         VectorType::get({4}, res.getType().cast<ShapedType>().getElementType());
-    auto collapsedLhs = rewriter.create<vector::ShapeCastOp>(
+    auto collapsedLhs = rewriter.createOrFold<vector::ShapeCastOp>(
         extsiLhs.getLoc(), collapsedInputType, extsiLhs);
-    auto collapsedRhs = rewriter.create<vector::ShapeCastOp>(
+    auto collapsedRhs = rewriter.createOrFold<vector::ShapeCastOp>(
         extsiRhs.getLoc(), collapsedInputType, extsiRhs);
-    auto collapsedRes = rewriter.create<vector::ShapeCastOp>(
+    auto collapsedRes = rewriter.createOrFold<vector::ShapeCastOp>(
         res.getLoc(), collapsedOutputType, res);
 
     // Replace the contract with a neon op
-    auto smmlaOp = rewriter.create<arm_neon::SmmlaOp>(
+    auto smmlaOp = rewriter.createOrFold<arm_neon::SmmlaOp>(
         op.getLoc(), collapsedRes.getType(), collapsedRes, collapsedLhs,
         collapsedRhs);
 
