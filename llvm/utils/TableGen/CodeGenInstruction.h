ef7a9a5e259c8f71b7b02a3246c512959c6c62a6
Jason Eckhardt <jeckhardt@nvidia.com>
2024-03-09 11:53:08 -0600

    [TableGen] More efficiency improvements for encode/decode emission.
    DecoderEmitter and CodeEmitterGen perform repeated linear walks over the
    entire instruction list. This patch eliminates two more such walks.
    
    The eliminated traversals visit every instruction merely to determine whether
    the target has variable length encodings. For a target with variable length
    encodings, the original any_of will terminate quickly. But all targets other
    than M68k use fixed length encodings and thus any_of must visit the entire
    instruction list.

--- llvm/utils/TableGen/CodeGenInstruction.h
+++ llvm/utils/TableGen/CodeGenInstruction.h
@@ -17,6 +17,7 @@
 #include "llvm/ADT/StringMap.h"
 #include "llvm/ADT/StringRef.h"
 #include "llvm/CodeGenTypes/MachineValueType.h"
+#include "llvm/TableGen/Record.h"
 #include <cassert>
 #include <string>
 #include <utility>
@@ -331,6 +332,12 @@ public:
   /// Check if the operand is required to be an immediate.
   bool isInOperandImmArg(unsigned i) const {
     return isOperandImpl("InOperandList", i, "IsImmediate");
+  }
+
+  /// Return true if the instruction uses a variable length encoding.
+  bool isVariableLengthEncoding() const {
+    const RecordVal *RV = TheDef->getValue("Inst");
+    return RV && isa<DagInit>(RV->getValue());
   }
 
 private:
