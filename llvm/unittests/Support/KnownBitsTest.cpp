6fb4b4a904520858a84e1ebef33578ded142df02
Sh0g0-1758 <shouryagoel10000@gmail.com>
2024-03-10 02:23:26 +0530

    Ran clang Formatter

--- llvm/unittests/Support/KnownBitsTest.cpp
+++ llvm/unittests/Support/KnownBitsTest.cpp
@@ -537,14 +537,12 @@ TEST(KnownBitsTest, BinaryExhaustive) {
       [](const KnownBits &Known1, const KnownBits &Known2) {
         return KnownBits::mulhs(Known1, Known2);
       },
-      &APIntOps::mulhs,
-      checkCorrectnessOnlyBinary);
+      &APIntOps::mulhs, checkCorrectnessOnlyBinary);
   testBinaryOpExhaustive(
       [](const KnownBits &Known1, const KnownBits &Known2) {
         return KnownBits::mulhu(Known1, Known2);
       },
-      &APIntOps::mulhu,
-      checkCorrectnessOnlyBinary);
+      &APIntOps::mulhu, checkCorrectnessOnlyBinary);
 }
 
 TEST(KnownBitsTest, UnaryExhaustive) {
