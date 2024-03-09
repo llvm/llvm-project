0875bc6976abe8c2dafe52d6c78c1ab1aa7840f9
Yaxun (Sam) Liu <yaxun.liu@amd.com>
2024-03-09 09:45:48 -0500

    [HIP] add --offload-compression-level= option (#83605)
    Added --offload-compression-level= option to clang and
    -compression-level=
    option to clang-offload-bundler for controlling compression level.
    
    Added support of long distance matching (LDM) for llvm::zstd which is
    off
    by default. Enable it for clang-offload-bundler by default since it
    improves compression rate in general.
    
    Change default compression level to 3 for zstd for clang-offload-bundler
    since it works well for bundle entry size from 1KB to 32MB, which should
    cover most of the clang-offload-bundler usage. Users can still specify
    compression level by -compression-level= option if necessary.

--- clang/lib/Driver/ToolChains/CommonArgs.cpp
+++ clang/lib/Driver/ToolChains/CommonArgs.cpp
@@ -2863,3 +2863,15 @@ void tools::addOutlineAtomicsArgs(const Driver &D, const ToolChain &TC,
     CmdArgs.push_back("+outline-atomics");
   }
 }
+
+void tools::addOffloadCompressArgs(const llvm::opt::ArgList &TCArgs,
+                                   llvm::opt::ArgStringList &CmdArgs) {
+  if (TCArgs.hasFlag(options::OPT_offload_compress,
+                     options::OPT_no_offload_compress, false))
+    CmdArgs.push_back("-compress");
+  if (TCArgs.hasArg(options::OPT_v))
+    CmdArgs.push_back("-verbose");
+  if (auto *Arg = TCArgs.getLastArg(options::OPT_offload_compression_level_EQ))
+    CmdArgs.push_back(
+        TCArgs.MakeArgString(Twine("-compression-level=") + Arg->getValue()));
+}
