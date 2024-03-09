db910c4076c045750e293f0e5ea5d34bc394b9a0
Sh0g0-1758 <shouryagoel10000@gmail.com>
2024-03-10 03:08:15 +0530

    Replaced Lamda Function

--- mlir/lib/Dialect/Arith/IR/ArithOps.cpp
+++ mlir/lib/Dialect/Arith/IR/ArithOps.cpp
@@ -433,9 +433,8 @@ arith::MulSIExtendedOp::fold(FoldAdaptor adaptor,
           [](const APInt &a, const APInt &b) { return a * b; })) {
     // Invoke the constant fold helper again to calculate the 'high' result.
     Attribute highAttr = constFoldBinaryOp<IntegerAttr>(
-        adaptor.getOperands(), [](const APInt &a, const APInt &b) {
-          return llvm::APIntOps::mulhs(a, b);
-        });
+        adaptor.getOperands(), &llvm::APIntOps::mulhs(a, b)
+        );
     assert(highAttr && "Unexpected constant-folding failure");
 
     results.push_back(lowAttr);
@@ -488,9 +487,7 @@ arith::MulUIExtendedOp::fold(FoldAdaptor adaptor,
           [](const APInt &a, const APInt &b) { return a * b; })) {
     // Invoke the constant fold helper again to calculate the 'high' result.
     Attribute highAttr = constFoldBinaryOp<IntegerAttr>(
-        adaptor.getOperands(), [](const APInt &a, const APInt &b) {
-          return llvm::APIntOps::mulhu(a, b);
-        });
+        adaptor.getOperands(), &llvm::APIntOps::mulhu(a, b));
     assert(highAttr && "Unexpected constant-folding failure");
 
     results.push_back(lowAttr);
