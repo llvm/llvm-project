b5926a959f5fe1f9a18aaf465087cee13c6bc0a1
fpasserby <125797601+fpasserby@users.noreply.github.com>
2024-03-11 03:00:00 +0100

    [coroutine] Implement llvm.coro.await.suspend intrinsic (#79712)
    Implement `llvm.coro.await.suspend` intrinsics, to deal with performance
    regression after prohibiting `.await_suspend` inlining, as suggested in
    #64945.
    Actually, there are three new intrinsics, which directly correspond to
    each of three forms of `await_suspend`:
    ```
    void llvm.coro.await.suspend.void(ptr %awaiter, ptr %frame, ptr @wrapperFunction)
    i1 llvm.coro.await.suspend.bool(ptr %awaiter, ptr %frame, ptr @wrapperFunction)
    ptr llvm.coro.await.suspend.handle(ptr %awaiter, ptr %frame, ptr @wrapperFunction)
    ```
    There are three different versions instead of one, because in `bool`
    case it's result is used for resuming via a branch, and in
    `coroutine_handle` case exceptions from `await_suspend` are handled in
    the coroutine, and exceptions from the subsequent `.resume()` are
    propagated to the caller.
    
    Await-suspend block is simplified down to intrinsic calls only, for
    example for symmetric transfer:
    ```
    %id = call token @llvm.coro.save(ptr null)
    %handle = call ptr @llvm.coro.await.suspend.handle(ptr %awaiter, ptr %frame, ptr @wrapperFunction)
    call void @llvm.coro.resume(%handle)
    %result = call i8 @llvm.coro.suspend(token %id, i1 false)
    switch i8 %result, ...
    ```
    All await-suspend logic is moved out into a wrapper function, generated
    for each suspension point.
    The signature of the function is `<type> wrapperFunction(ptr %awaiter,
    ptr %frame)` where `<type>` is one of `void` `i1` or `ptr`, depending on
    the return type of `await_suspend`.
    Intrinsic calls are lowered during `CoroSplit` pass, right after the
    split.
    
    Because I'm new to LLVM, I'm not sure if the helper function generation,
    calls to them and lowering are implemented in the right way, especially
    with regard to various metadata and attributes, i. e. for TBAA. All
    things that seemed questionable are marked with `FIXME` comments.
    
    There is another detail: in case of symmetric transfer raw pointer to
    the frame of coroutine, that should be resumed, is returned from the
    helper function and a direct call to `@llvm.coro.resume` is generated.
    C++ standard demands, that `.resume()` method is evaluated. Not sure how
    important is this, because code has been generated in the same way
    before, sans helper function.

--- clang/test/CodeGenCoroutines/coro-symmetric-transfer-01.cpp
+++ clang/test/CodeGenCoroutines/coro-symmetric-transfer-01.cpp
@@ -50,10 +50,5 @@ detached_task foo() {
 // check that the lifetime of the coroutine handle used to obtain the address is contained within single basic block, and hence does not live across suspension points.
 // CHECK-LABEL: final.suspend:
 // CHECK:         %{{.+}} = call token @llvm.coro.save(ptr null)
-// CHECK:         call void @llvm.lifetime.start.p0(i64 8, ptr %[[HDL:.+]])
-// CHECK:         %[[CALL:.+]] = call ptr @_ZN13detached_task12promise_type13final_awaiter13await_suspendESt16coroutine_handleIS0_E(
-// CHECK:         %[[HDL_CAST2:.+]] = getelementptr inbounds %"struct.std::coroutine_handle.0", ptr %[[HDL]], i32 0, i32 0
-// CHECK:         store ptr %[[CALL]], ptr %[[HDL_CAST2]], align 8
-// CHECK:         %[[HDL_TRANSFER:.+]] = call noundef ptr @_ZNKSt16coroutine_handleIvE7addressEv(ptr noundef {{.*}}%[[HDL]])
-// CHECK:         call void @llvm.lifetime.end.p0(i64 8, ptr %[[HDL]])
+// CHECK:         %[[HDL_TRANSFER:.+]] = call ptr @llvm.coro.await.suspend.handle
 // CHECK:         call void @llvm.coro.resume(ptr %[[HDL_TRANSFER]])
