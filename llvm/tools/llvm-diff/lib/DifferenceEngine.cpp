431bdd42855c957eb0efefc4a03ac4adacd4c5c8
YanWQ-monad <YanWQmonad@gmail.com>
2024-03-11 23:59:47 +0800

    Diff alloca allocated type and alignment

--- llvm/tools/llvm-diff/lib/DifferenceEngine.cpp
+++ llvm/tools/llvm-diff/lib/DifferenceEngine.cpp
@@ -527,6 +527,20 @@ class FunctionDifferenceEngine {
           Difference = true;
         }
       return Difference;
+    } else if (isa<AllocaInst>(L)) {
+      const AllocaInst *LI = cast<AllocaInst>(L);
+      const AllocaInst *RI = cast<AllocaInst>(R);
+
+      if (LI->getAllocatedType() != RI->getAllocatedType()) {
+        if (Complain)
+          Engine.log("alloca allocated type differ");
+        return true;
+      }
+      if (LI->getAlign() != RI->getAlign()) {
+        if (Complain)
+          Engine.log("alloca alignment differ");
+        return true;
+      }
     } else if (isa<UnreachableInst>(L)) {
       return false;
     }
