56b27d16c1015214e82677834c1ec55dc4491691
Chuanqi Xu <yedeng.yd@linux.alibaba.com>
2024-03-11 11:14:40 +0800

    [C++20] [Moduls] Avoid computing odr hash for functions from comparing constraint expression
    Previously we disabled to compute ODR hash for declarations from the
    global module fragment. However, we missed the case that the functions
    lives in the concept requiments (see the attached the test files for
    example). And the mismatch causes the potential crashment.
    
    Due to we will set the function body as lazy after we deserialize it and
    we will only take its body when needed. However, we don't allow to take
    the body during deserializing. So it is actually potentially problematic
    if we set the body as lazy first and computing the hash value of the
    function, which requires to deserialize its body. So we will meet a
    crash here.
    
    This patch tries to solve the issue by not taking the body of the
    function from GMF. Note that we can't skip comparing the constraint
    expression from the GMF directly since it is an key part of the
    function selecting and it may be the reason why we can't return 0
    directly for `FunctionDecl::getODRHash()` from the GMF.

--- clang/lib/AST/Decl.cpp
+++ clang/lib/AST/Decl.cpp
@@ -4496,7 +4496,7 @@ unsigned FunctionDecl::getODRHash() {
   }
 
   class ODRHash Hash;
-  Hash.AddFunctionDecl(this);
+  Hash.AddFunctionDecl(this, /*SkipBody=*/shouldSkipCheckingODR());
   setHasODRHash(true);
   ODRHash = Hash.CalculateHash();
   return ODRHash;
