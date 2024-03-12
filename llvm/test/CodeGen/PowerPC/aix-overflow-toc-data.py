565ac5735e9bdd2d116f16b1d9c28ffbb85399fd
Zaara Syeda <syzaara@cpap8104.rtp.raleigh.ibm.com>
2024-03-12 12:01:42 -0400

    Address review comments

--- llvm/test/CodeGen/PowerPC/aix-overflow-toc-data.py
+++ llvm/test/CodeGen/PowerPC/aix-overflow-toc-data.py
@@ -9,11 +9,11 @@
 
 # RUN: llc -mtriple powerpc-ibm-aix-xcoff -code-model=small -mcpu=pwr4 -mattr=-altivec -O0 \
 # RUN:     -filetype=obj -o %t.o < %t.ll
-# RUN: llvm-objdump --no-print-imm-hex -D -r --symbol-description %t.o | FileCheck --check-prefix=DIS32 %s
+# RUN: llvm-objdump --no-print-imm-hex -D -r --symbol-description %t.o | FileCheck -D#NFA=2 --check-prefix=DIS32 %s
 
 # RUN: llc -mtriple powerpc64-ibm-aix-xcoff -code-model=small -mcpu=pwr4 -mattr=-altivec -O0 \
 # RUN:     -filetype=obj -o %t.o < %t.ll
-# RUN: llvm-objdump --no-print-imm-hex -D -r --symbol-description %t.o | FileCheck --check-prefix=DIS64 %s
+# RUN: llvm-objdump --no-print-imm-hex -D -r --symbol-description %t.o | FileCheck -D#NFA=2 --check-prefix=DIS64 %s
 
 numentries = 8195
 for x in range(0, numentries):
@@ -45,15 +45,15 @@ print('attributes #0 = { "toc-data" }')
 # ASM64:  la 4, a8193[TD](2)
 
 # DIS32:    fffc: 38 82 7f fc   addi 4, 2, 32764
-# DIS32:      0000fffe:  R_TOC  (idx: 16391) a8191[TD]
+# DIS32:      0000fffe:  R_TOC  (idx: [[#NFA+16391]]) a8191[TD]
 # DIS32:   10004: 38 82 80 00   addi 4, 2, -32768
-# DIS32:      00010006:  R_TOC  (idx: 16393) a8192[TD]
+# DIS32:      00010006:  R_TOC  (idx: [[#NFA+16393]]) a8192[TD]
 # DIS32:   1000c: 38 82 80 04   addi 4, 2, -32764
-# DIS32:      0001000e:  R_TOC  (idx: 16395) a8193[TD]
+# DIS32:      0001000e:  R_TOC  (idx: [[#NFA+16395]]) a8193[TD]
 
 # DIS64:    fffc: 38 82 7f fc   addi 4, 2, 32764
-# DIS64:      0000fffe:  R_TOC  (idx: 16391) a8191[TD]
+# DIS64:      0000fffe:  R_TOC  (idx: [[#NFA+16391]]) a8191[TD]
 # DIS64:   10004: 38 82 80 00   addi 4, 2, -32768
-# DIS64:      00010006:  R_TOC  (idx: 16393) a8192[TD]
+# DIS64:      00010006:  R_TOC  (idx: [[#NFA+16393]]) a8192[TD]
 # DIS64:   1000c: 38 82 80 04   addi 4, 2, -32764
-# DIS64:      0001000e:  R_TOC  (idx: 16395) a8193[TD]
+# DIS64:      0001000e:  R_TOC  (idx: [[#NFA+16395]]) a8193[TD]
