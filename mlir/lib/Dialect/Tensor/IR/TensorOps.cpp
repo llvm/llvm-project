10cf634bb310c5eafe37746f9fdad87d8ed0488d
Sayan Saha <sayan.jubiee@gmail.com>
2024-03-11 22:37:33 -0400

    [MLIR] Fix incorrect memref::DimOp canonicalization, add tensor::DimOp canonicalization (#84225)
    The current canonicalization of `memref.dim` operating on the result of
    `memref.reshape` into `memref.load` is incorrect as it doesn't check
    whether the `index` operand of `memref.dim` dominates the source
    `memref.reshape` op. It always introduces `memref.load` right after
    `memref.reshape` to ensure the `memref` is not mutated before the
    `memref.load` call. As a result, the following error is observed:
    
    ```
    $> mlir-opt --canonicalize input.mlir
    
    func.func @reshape_dim(%arg0: memref<*xf32>, %arg1: memref<?xindex>, %arg2: index) -> index {
        %c4 = arith.constant 4 : index
        %reshape = memref.reshape %arg0(%arg1) : (memref<*xf32>, memref<?xindex>) -> memref<*xf32>
        %0 = arith.muli %arg2, %c4 : index
        %dim = memref.dim %reshape, %0 : memref<*xf32>
        return %dim : index
      }
    ```
    
    results in:
    
    ```
    dominator.mlir:22:12: error: operand #1 does not dominate this use
        %dim = memref.dim %reshape, %0 : memref<*xf32>
               ^
    dominator.mlir:22:12: note: see current operation: %1 = "memref.load"(%arg1, %2) <{nontemporal = false}> : (memref<?xindex>, index) -> index
    dominator.mlir:21:10: note: operand defined here (op in the same block)
        %0 = arith.muli %arg2, %c4 : index
    ```
    
    Properly fixing this issue requires a dominator analysis which is
    expensive to run within a canonicalization pattern. So, this patch fixes
    the canonicalization pattern by being more strict/conservative about the
    legality condition in which we perform this canonicalization.
    The more general pattern is also added to `tensor.dim`. Since tensors are
    immutable we don't need to worry about where to introduce the
    `tensor.extract` call after canonicalization.

--- mlir/lib/Dialect/Tensor/IR/TensorOps.cpp
+++ mlir/lib/Dialect/Tensor/IR/TensorOps.cpp
@@ -824,11 +824,37 @@ struct DimOfDestStyleOp : public OpRewritePattern<DimOp> {
     return success();
   }
 };
+
+/// Fold dim of a tensor reshape operation to a extract into the reshape's shape
+/// operand.
+struct DimOfReshapeOp : public OpRewritePattern<DimOp> {
+  using OpRewritePattern<DimOp>::OpRewritePattern;
+
+  LogicalResult matchAndRewrite(DimOp dim,
+                                PatternRewriter &rewriter) const override {
+    auto reshape = dim.getSource().getDefiningOp<ReshapeOp>();
+
+    if (!reshape)
+      return failure();
+
+    // Since tensors are immutable we don't need to worry about where to place
+    // the extract call
+    rewriter.setInsertionPointAfter(dim);
+    Location loc = dim.getLoc();
+    Value extract =
+        rewriter.create<ExtractOp>(loc, reshape.getShape(), dim.getIndex());
+    if (extract.getType() != dim.getType())
+      extract =
+          rewriter.create<arith::IndexCastOp>(loc, dim.getType(), extract);
+    rewriter.replaceOp(dim, extract);
+    return success();
+  }
+};
 } // namespace
 
 void DimOp::getCanonicalizationPatterns(RewritePatternSet &results,
                                         MLIRContext *context) {
-  results.add<DimOfCastOp, DimOfDestStyleOp>(context);
+  results.add<DimOfCastOp, DimOfDestStyleOp, DimOfReshapeOp>(context);
 }
 
 //===----------------------------------------------------------------------===//
