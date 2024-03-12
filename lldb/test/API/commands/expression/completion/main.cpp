2961ca8068b2a90999e694ce40f53da80684e5b7
Michael Buch <michaelbuch12@gmail.com>
2024-03-12 10:21:20 +0000

    fixup! move type into header

--- lldb/test/API/commands/expression/completion/main.cpp
+++ lldb/test/API/commands/expression/completion/main.cpp
@@ -1,10 +1,4 @@
-class MyVec {
-    int __mem;
-    int _Mem;
-public:
-    void __func() {}
-    void _Func() {}
-};
+#include <reserved.h>
 
 namespace LongNamespaceName { class NestedClass { long m; }; }
 
---
+++ lldb/test/API/commands/expression/completion/sys/reserved.h
@@ -0,0 +1,7 @@
+class MyVec {
+    int __mem;
+    int _Mem;
+public:
+    void __func() {}
+    void _Func() {}
+};
