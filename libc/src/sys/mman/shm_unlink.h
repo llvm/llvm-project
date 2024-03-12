628c04b2969dbbc81df0bf113f1dace4fd35b30d
Schrodinger ZHU Yifan <yifanzhu@rochester.edu>
2024-03-12 15:12:31 -0400

    fix header

--- libc/src/sys/mman/shm_unlink.h
+++ libc/src/sys/mman/shm_unlink.h
@@ -1,5 +1,4 @@
-//===-- Implementation header for shm_unlink function -------------*- C++
-//-*-===//
+//===-- Implementation header for shm_unlink function ------------*- C++-*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
