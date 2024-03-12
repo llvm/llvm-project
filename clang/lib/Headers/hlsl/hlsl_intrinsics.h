2aeabdb9dc8483a38cb07685374cd60de44cdb16
Nathan Gauër <brioche@google.com>
2024-03-12 18:18:54 +0100

    add convergent attribute

--- clang/lib/Headers/hlsl/hlsl_intrinsics.h
+++ clang/lib/Headers/hlsl/hlsl_intrinsics.h
@@ -1389,11 +1389,13 @@ float4 trunc(float4);
 /// true, across all active lanes in the current wave.
 _HLSL_AVAILABILITY(shadermodel, 6.0)
 _HLSL_BUILTIN_ALIAS(__builtin_hlsl_wave_active_count_bits)
+__attribute__((convergent))
 uint WaveActiveCountBits(bool Val);
 
 /// \brief Returns the index of the current lane within the current wave.
 _HLSL_AVAILABILITY(shadermodel, 6.0)
 _HLSL_BUILTIN_ALIAS(__builtin_hlsl_wave_get_lane_index)
+__attribute__((convergent))
 uint WaveGetLaneIndex();
 
 } // namespace hlsl
