c103f2f88bf5f5fbc8c971a5fd905591900bce39
thebesttv <taojuntian@outlook.com>
2024-03-13 14:32:26 +0800

    Use all cores

--- build-release.sh
+++ build-release.sh
@@ -6,8 +6,9 @@ mkdir build-release && cd build-release
 cmake -G Ninja \
       -DLLVM_ENABLE_PROJECTS='clang' \
       -DCMAKE_BUILD_TYPE=Release \
-      -DLLVM_PARALLEL_COMPILE_JOBS=12 \
       ../llvm
 
 # 打开对 typeid 的支持（感觉没用）
       # -DLLVM_ENABLE_RTTI=true \
+
+#      -DLLVM_PARALLEL_COMPILE_JOBS=12 \
