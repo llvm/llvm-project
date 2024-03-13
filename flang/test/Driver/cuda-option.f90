da2fce036141095ab8bddf4524ef1685e7f85f1f
Valentin Clement (バレンタイン クレメン) <clementval@gmail.com>
2024-03-13 09:14:40 -0700

    [flang][cuda] Enable cuda with -x cuda option (#84944)
    Flang driver was already able to enable the CUDA language feature base
    on the file extension but there was no command line option. This PR adds
    one.

---
+++ flang/test/Driver/cuda-option.f90
@@ -0,0 +1,15 @@
+! Test -fcuda option
+! RUN: %flang -fc1 -cpp -x cuda -fdebug-unparse %s -o - | FileCheck %s
+! RUN: not %flang -fc1 -cpp %s -o - 2>&1 | FileCheck %s --check-prefix=ERROR
+program main
+#if _CUDA
+  integer :: var = _CUDA
+#endif
+  integer, device :: dvar
+end program
+
+! CHECK-LABEL: PROGRAM main
+! CHECK: INTEGER :: var = 1
+! CHECK: INTEGER, DEVICE :: dvar
+
+! ERROR: cuda-option.f90:8:19: error: expected end of statement
