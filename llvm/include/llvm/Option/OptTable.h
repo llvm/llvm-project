4d9029525b046c15987a46be9d1f1d8452ea6da7
David Spickett <david.spickett@linaro.org>
2024-03-13 09:57:28 +0000

    Simplify emission, move into one function

--- llvm/include/llvm/Option/OptTable.h
+++ llvm/include/llvm/Option/OptTable.h
@@ -63,8 +63,8 @@ public:
     // visibilities. If no help text is found in this list for the visibility of
     // the program, HelpText is used instead. This cannot use std::vector
     // because OptTable is used in constexpr contexts. Increase the array sizes
-    // here if you need more entries and adjust the constants in the
-    // OptParserEmitter TableGen backend.
+    // here if you need more entries and adjust the constants in
+    // OptParserEmitter::EmitHelpTextsForVariants.
     std::array<std::pair<std::array<unsigned int, 2>, const char *>, 1>
         HelpTextsForVariants;
     const char *MetaVar;
--- llvm/utils/TableGen/OptParserEmitter.cpp
+++ llvm/utils/TableGen/OptParserEmitter.cpp
@@ -191,6 +191,60 @@ static MarshallingInfo createMarshallingInfo(const Record &R) {
   return Ret;
 }
 
+static void EmitHelpTextsForVariants(
+    raw_ostream &OS, std::vector<std::pair<std::vector<std::string>, StringRef>>
+                         HelpTextsForVariants) {
+  // OptTable must be constexpr so it uses std::arrays with these capacities.
+  const unsigned MaxVisibilityHelp = 1;
+  const unsigned MaxVisibilityPerHelp = 2;
+
+  // This function must initialise any unused elements of those arrays.
+  for (auto [Visibilities, _] : HelpTextsForVariants)
+    while (Visibilities.size() < MaxVisibilityPerHelp)
+      Visibilities.push_back("0");
+
+  while (HelpTextsForVariants.size() < MaxVisibilityHelp)
+    HelpTextsForVariants.push_back(
+        {std::vector<std::string>(MaxVisibilityPerHelp, "0"), ""});
+
+  OS << ", (std::array<std::pair<std::array<unsigned, " << MaxVisibilityPerHelp
+     << ">, const char*>, " << MaxVisibilityHelp << ">{{ ";
+
+  assert(HelpTextsForVariants.size() <= MaxVisibilityHelp &&
+         "Too many help text variants to store in "
+         "OptTable::HelpTextsForVariants");
+
+  for (auto VisibilityHelp = HelpTextsForVariants.cbegin();
+       VisibilityHelp != HelpTextsForVariants.cend(); ++VisibilityHelp) {
+    auto [Visibilities, Help] = *VisibilityHelp;
+
+    assert(Visibilities.size() <= MaxVisibilityPerHelp &&
+           "Too many visibilities to store in an "
+           "OptTable::HelpTextsForVariants entry");
+    OS << "std::make_pair(std::array<unsigned, " << MaxVisibilityPerHelp
+       << ">{{";
+
+    for (auto Visibility = Visibilities.cbegin();
+         Visibility != Visibilities.cend(); ++Visibility) {
+      OS << *Visibility;
+      if (std::next(Visibility) != Visibilities.cend())
+        OS << ", ";
+    }
+
+    OS << "}}, ";
+
+    if (Help.size())
+      OS << "\"" << Help << "\"";
+    else
+      OS << "nullptr";
+    OS << ")";
+
+    if (std::next(VisibilityHelp) != HelpTextsForVariants.cend())
+      OS << ", ";
+  }
+  OS << " }})";
+}
+
 /// OptParserEmitter - This tablegen backend takes an input .td file
 /// describing a list of options and emits a data structure for parsing and
 /// working with those options when given an input command line.
@@ -275,9 +329,6 @@ static void EmitOptParser(RecordKeeper &Records, raw_ostream &OS) {
   }
   OS << "#endif\n";
 
-  const unsigned MaxVisibilityHelp = 1;
-  const unsigned MaxVisibilityPerHelp = 2;
-
   OS << "/////////\n";
   OS << "// Groups\n\n";
   OS << "#ifdef OPTION\n";
@@ -316,24 +367,7 @@ static void EmitOptParser(RecordKeeper &Records, raw_ostream &OS) {
       OS << ", nullptr";
 
     // Not using Visibility specific text for group help.
-    OS << ", (std::array<std::pair<std::array<unsigned, "
-       << MaxVisibilityPerHelp << ">, const char*>, " << MaxVisibilityHelp
-       << ">{{";
-    for (unsigned HelpEmitted = 0; HelpEmitted < MaxVisibilityHelp;
-         ++HelpEmitted) {
-      OS << "std::make_pair(std::array<unsigned, " << MaxVisibilityPerHelp
-         << ">{{";
-      for (unsigned VisibilityEmitted = 0;
-           VisibilityEmitted < MaxVisibilityPerHelp; ++VisibilityEmitted) {
-        OS << "0";
-        if ((VisibilityEmitted + 1) != MaxVisibilityPerHelp)
-          OS << ", ";
-      }
-      OS << "}}, nullptr)";
-      if ((HelpEmitted + 1) != MaxVisibilityHelp)
-        OS << ", ";
-    }
-    OS << "}})";
+    EmitHelpTextsForVariants(OS, {});
 
     // The option meta-variable name (unused).
     OS << ", nullptr";
@@ -433,70 +467,21 @@ static void EmitOptParser(RecordKeeper &Records, raw_ostream &OS) {
     } else
       OS << ", nullptr";
 
-    OS << ", (std::array<std::pair<std::array<unsigned, "
-       << MaxVisibilityPerHelp << ">, const char*>, " << MaxVisibilityHelp
-       << ">{{ ";
-
-    unsigned NumVisibilityHelpEmitted = 0;
-
-    std::vector<Record *> VisibilitiesHelp =
-        R.getValueAsListOfDefs("HelpTextsForVariants");
-    assert(VisibilitiesHelp.size() <= MaxVisibilityHelp &&
-           "Too many help text variants to store in "
-           "OptTable::HelpTextsForVariants");
-
-    for (std::vector<Record *>::const_iterator VisibilityHelp =
-             VisibilitiesHelp.begin();
-         VisibilityHelp != VisibilitiesHelp.end();
-         ++VisibilityHelp, ++NumVisibilityHelpEmitted) {
+    std::vector<std::pair<std::vector<std::string>, StringRef>>
+        HelpTextsForVariants;
+    for (Record *VisibilityHelp :
+         R.getValueAsListOfDefs("HelpTextsForVariants")) {
       ArrayRef<Init *> Visibilities =
-          (*VisibilityHelp)->getValueAsListInit("Visibilities")->getValues();
-      assert(Visibilities.size() <= MaxVisibilityPerHelp &&
-             "Too many visibilities to store in an "
-             "OptTable::HelpTextsForVariants entry");
+          VisibilityHelp->getValueAsListInit("Visibilities")->getValues();
 
-      OS << "std::make_pair(std::array<unsigned, " << MaxVisibilityPerHelp
-         << ">{{";
-      unsigned NumVisibilitiesEmitted = 0;
-      for (auto Visibility = Visibilities.begin();
-           Visibility != Visibilities.end();
-           ++Visibility, ++NumVisibilitiesEmitted) {
-        OS << (*Visibility)->getAsUnquotedString();
-        if (std::next(Visibility) != Visibilities.end())
-          OS << ", ";
-      }
-      // Init unused elements.
-      for (; NumVisibilitiesEmitted < MaxVisibilityPerHelp;
-           ++NumVisibilitiesEmitted) {
-        OS << "0";
-        if ((NumVisibilitiesEmitted + 1) != MaxVisibilityPerHelp)
-          OS << ", ";
-      }
-      OS << "}}, ";
+      std::vector<std::string> VisibilityNames;
+      for (Init *Visibility : Visibilities)
+        VisibilityNames.push_back(Visibility->getAsUnquotedString());
 
-      StringRef Text = (*VisibilityHelp)->getValueAsString("Text");
-      OS << "\"" << Text << "\")";
-
-      if (std::next(VisibilityHelp) != VisibilitiesHelp.end())
-        OS << ", ";
+      HelpTextsForVariants.push_back(std::make_pair(
+          VisibilityNames, VisibilityHelp->getValueAsString("Text")));
     }
-    // Init any unused elements.
-    for (; NumVisibilityHelpEmitted < MaxVisibilityHelp;
-         ++NumVisibilityHelpEmitted) {
-      OS << "std::make_pair(std::array<unsigned, " << MaxVisibilityPerHelp
-         << ">{{";
-      for (unsigned NumVisibilitiesEmitted = 0;
-           NumVisibilitiesEmitted != MaxVisibilityPerHelp;
-           ++NumVisibilitiesEmitted) {
-        OS << "0";
-        if ((NumVisibilitiesEmitted + 1) != MaxVisibilityPerHelp)
-          OS << ", ";
-      }
-      OS << "}}, nullptr)";
-      if ((NumVisibilityHelpEmitted + 1) != MaxVisibilityHelp)
-        OS << ", ";
-    }
-    OS << " }})";
+    EmitHelpTextsForVariants(OS, HelpTextsForVariants);
 
     // The option meta-variable name.
     OS << ", ";
