a1fd3063a11ce33c3f0a0a42d9bcc940012ac527
Freddy Ye <freddy.ye@intel.com>
2024-03-14 13:06:47 +0800

    address comments.

--- llvm/test/MC/X86/apx/ccmp-reloc.s
+++ llvm/test/MC/X86/apx/ccmp-reloc.s
@@ -8,7 +8,7 @@ ccmpbw	{dfv=of}	$foo, %bx               // CHECK-NEXT:     R_X86_64_16
 ccmpbw	{dfv=of}	$foo, 123(%r8,%rax,4)   // CHECK-NEXT:     R_X86_64_16
 ccmpbl	{dfv=of}	$foo, %ebx              // CHECK-NEXT:     R_X86_64_32
 ccmpbl	{dfv=of}	$foo, 123(%r8,%rax,4)   // CHECK-NEXT:     R_X86_64_32
-ccmpbq	{dfv=of}	$foo, %rbx              // CHECK-NEXT:     R_X86_64_32
-ccmpbq	{dfv=of}	$foo, 123(%r8,%rax,4)   // CHECK-NEXT:     R_X86_64_32
+ccmpbq	{dfv=of}	$foo, %rbx              // CHECK-NEXT:     R_X86_64_32S
+ccmpbq	{dfv=of}	$foo, 123(%r8,%rax,4)   // CHECK-NEXT:     R_X86_64_32S
 // CHECK-NEXT:   }
 // CHECK-NEXT: ]
