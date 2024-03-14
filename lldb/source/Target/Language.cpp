9362d76cc4b6b5b6d2cf043ea3cba92da873940c
Felipe de Azevedo Piovezan <fpiovezan@apple.com>
2024-03-14 09:40:00 -0700

    [lldb] Allow languages to filter breakpoints set by line (#83908)
    Some languages may create artificial functions that have no real user
    code, even though there is line table information for them. One such
    case is with coroutine code that receives the CoroSplitter
    transformation in LLVM IR. That code transformation creates many
    different Functions, cloning one Instruction into many Instructions in
    many different Functions and copying the associated debug locations.
    
    It would be difficult to make that pass delete debug locations of cloned
    instructions in a language agnostic way (is it even possible?), but LLDB
    can ignore certain locations by querying its Language APIs and having it
    decide based on, for example, mangling information.

--- lldb/source/Target/Language.cpp
+++ lldb/source/Target/Language.cpp
@@ -13,6 +13,7 @@
 #include "lldb/Target/Language.h"
 
 #include "lldb/Core/PluginManager.h"
+#include "lldb/Interpreter/OptionValueProperties.h"
 #include "lldb/Symbol/SymbolFile.h"
 #include "lldb/Symbol/TypeList.h"
 #include "lldb/Target/Target.h"
@@ -26,6 +27,35 @@ using namespace lldb_private::formatters;
 
 typedef std::unique_ptr<Language> LanguageUP;
 typedef std::map<lldb::LanguageType, LanguageUP> LanguagesMap;
+
+#define LLDB_PROPERTIES_language
+#include "TargetProperties.inc"
+
+enum {
+#define LLDB_PROPERTIES_language
+#include "TargetPropertiesEnum.inc"
+};
+
+LanguageProperties &Language::GetGlobalLanguageProperties() {
+  static LanguageProperties g_settings;
+  return g_settings;
+}
+
+llvm::StringRef LanguageProperties::GetSettingName() {
+  static constexpr llvm::StringLiteral g_setting_name("language");
+  return g_setting_name;
+}
+
+LanguageProperties::LanguageProperties() {
+  m_collection_sp = std::make_shared<OptionValueProperties>(GetSettingName());
+  m_collection_sp->Initialize(g_language_properties);
+}
+
+bool LanguageProperties::GetEnableFilterForLineBreakpoints() const {
+  const uint32_t idx = ePropertyEnableFilterForLineBreakpoints;
+  return GetPropertyAtIndexAs<bool>(
+      idx, g_language_properties[idx].default_uint_value != 0);
+}
 
 static LanguagesMap &GetLanguagesMap() {
   static LanguagesMap *g_map = nullptr;
