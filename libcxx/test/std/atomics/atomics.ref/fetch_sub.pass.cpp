3cc65a58347c829544a44aeb6805259448e1731f
Damien L-G <dalg24@gmail.com>
2024-03-14 08:29:02 -0400

    [libc++][atomic_ref] forgot fetch_sub

--- libcxx/test/std/atomics/atomics.ref/fetch_sub.pass.cpp
+++ libcxx/test/std/atomics/atomics.ref/fetch_sub.pass.cpp
@@ -16,6 +16,7 @@
 #include <concepts>
 #include <type_traits>
 
+#include "atomic_helpers.h"
 #include "test_macros.h"
 
 template <typename T>
@@ -24,62 +25,66 @@ concept has_fetch_sub = requires {
   std::declval<T const>().fetch_sub(std::declval<T>(), std::declval<std::memory_order>());
 };
 
-static_assert(!has_fetch_sub<std::atomic_ref<bool>>);
-struct X {
-  int i;
-  X(int ii) noexcept : i(ii) {}
-  bool operator==(X o) const { return i == o.i; }
+template <typename T>
+struct TestDoesNotHaveFetchSub {
+  void operator()() const { static_assert(!has_fetch_sub<std::atomic_ref<T>>); }
 };
-static_assert(!has_fetch_sub<std::atomic_ref<X>>);
 
 template <typename T>
-void test_arithmetic() {
-  T x(T(7));
-  std::atomic_ref<T> const a(x);
+struct TestFetchSub {
+  void operator()() const {
+    if constexpr (std::is_arithmetic_v<T>) {
+      T x(T(7));
+      std::atomic_ref<T> const a(x);
 
-  {
-    std::same_as<T> auto y = a.fetch_sub(T(4));
-    assert(y == T(7));
-    assert(x == T(3));
-    ASSERT_NOEXCEPT(a.fetch_sub(T(0)));
+      {
+        std::same_as<T> auto y = a.fetch_sub(T(4));
+        assert(y == T(7));
+        assert(x == T(3));
+        ASSERT_NOEXCEPT(a.fetch_sub(T(0)));
+      }
+
+      {
+        std::same_as<T> auto y = a.fetch_sub(T(2), std::memory_order_relaxed);
+        assert(y == T(3));
+        assert(x == T(1));
+        ASSERT_NOEXCEPT(a.fetch_sub(T(0), std::memory_order_relaxed));
+      }
+    } else if constexpr (std::is_pointer_v<T>) {
+      using U = std::remove_pointer_t<T>;
+      U t[9]  = {};
+      T p{&t[7]};
+      std::atomic_ref<T> const a(p);
+
+      {
+        std::same_as<T> auto y = a.fetch_sub(4);
+        assert(y == &t[7]);
+        assert(a == &t[3]);
+        ASSERT_NOEXCEPT(a.fetch_sub(0));
+      }
+
+      {
+        std::same_as<T> auto y = a.fetch_sub(2, std::memory_order_relaxed);
+        assert(y == &t[3]);
+        assert(a == &t[1]);
+        ASSERT_NOEXCEPT(a.fetch_sub(0, std::memory_order_relaxed));
+      }
+    } else {
+      static_assert(std::is_void_v<T>);
+    }
   }
-
-  {
-    std::same_as<T> auto y = a.fetch_sub(T(2), std::memory_order_relaxed);
-    assert(y == T(3));
-    assert(x == T(1));
-    ASSERT_NOEXCEPT(a.fetch_sub(T(0), std::memory_order_relaxed));
-  }
-}
-
-template <typename T>
-void test_pointer() {
-  using U = std::remove_pointer_t<T>;
-  U t[9]  = {};
-  T p{&t[7]};
-  std::atomic_ref<T> const a(p);
-
-  {
-    std::same_as<T> auto y = a.fetch_sub(4);
-    assert(y == &t[7]);
-    assert(a == &t[3]);
-    ASSERT_NOEXCEPT(a.fetch_sub(0));
-  }
-
-  {
-    std::same_as<T> auto y = a.fetch_sub(2, std::memory_order_relaxed);
-    assert(y == &t[3]);
-    assert(a == &t[1]);
-    ASSERT_NOEXCEPT(a.fetch_sub(0, std::memory_order_relaxed));
-  }
-}
+};
 
 void test() {
-  test_arithmetic<int>();
-  test_arithmetic<float>();
+  TestEachIntegralType<TestFetchSub>()();
 
-  test_pointer<int*>();
-  test_pointer<const int*>();
+  TestEachFloatingPointType<TestFetchSub>()();
+
+  TestEachPointerType<TestFetchSub>()();
+
+  TestDoesNotHaveFetchSub<bool>()();
+  TestDoesNotHaveFetchSub<UserAtomicType>()();
+  TestDoesNotHaveFetchSub<LargeUserAtomicType>()();
 }
 
 int main(int, char**) {
