73bc935c86f7a07dcd0f1aa0387d9f469a7ce98a
Dave Lee <davelee.com@gmail.com>
2024-03-15 16:09:24 -0700

    [lldb] Fix dwim-print to not delete non-result persistent variables (#85152)
    `EvaluateExpression` does not always create a new persistent result. If the expression 
    is a bare persistent variable, then a new persistent result is not created. This means 
    the caller can't assume a new persistent result is created for each evaluation. 
    However, `dwim-print` was doing exactly that: assuming a new persistent result for each 
    evaluation. This resulted in a bug:
    
    ```
    (lldb) p int $j = 23
    (lldb) p $j
    (lldb) p $j
    ```
    
    The first `p $j` would not create a persistent result, and so `dwim-print` would 
    inadvertently delete `$j`. The second `p $j` would fail.
    
    The fix is to try `expr` as a persistent variable, after trying `expr` as a frame 
    variable. For persistent variables, this avoids calling `EvaluateExpression`.
    
    Resolves https://github.com/llvm/llvm-project/issues/84806
    
    rdar://124688427

--- lldb/test/API/commands/dwim-print/TestDWIMPrint.py
+++ lldb/test/API/commands/dwim-print/TestDWIMPrint.py
@@ -146,3 +146,15 @@ class TestCase(TestBase):
             self, "// break here", lldb.SBFileSpec("main.c")
         )
         self.expect("dwim-print (void)15", matching=False, patterns=["(?i)error"])
+
+    def test_preserves_persistent_variables(self):
+        """Test dwim-print does not delete persistent variables."""
+        self.build()
+        lldbutil.run_to_source_breakpoint(
+            self, "// break here", lldb.SBFileSpec("main.c")
+        )
+        self.expect("dwim-print int $i = 15")
+        # Run the same expression twice and verify success. This ensures the
+        # first command does not delete the persistent variable.
+        for _ in range(2):
+            self.expect("dwim-print $i", startstr="(int) 15")
