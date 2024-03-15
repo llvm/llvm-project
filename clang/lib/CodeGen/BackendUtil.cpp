a602c25589104917ccf4d38f410f69141deeabcd
justinfargnoli <justinfargnoli@gmail.com>
2024-03-14 20:12:54 -0700

    Stash clang changes

--- clang/lib/CodeGen/BackendUtil.cpp
+++ clang/lib/CodeGen/BackendUtil.cpp
@@ -87,6 +87,7 @@
 #include "llvm/Transforms/Scalar/GVN.h"
 #include "llvm/Transforms/Scalar/JumpThreading.h"
 #include "llvm/Transforms/Utils/Debugify.h"
+#include "llvm/Transforms/Utils/IRNormalizer.h"
 #include "llvm/Transforms/Utils/ModuleUtils.h"
 #include <memory>
 #include <optional>
@@ -1083,6 +1084,8 @@ void EmitAssemblyHelper::RunOptimizationPipeline(
   // Link against bitcodes supplied via the -mlink-builtin-bitcode option
   if (CodeGenOpts.LinkBitcodePostopt)
     MPM.addPass(LinkInModulesPass(BC));
+
+  MPM.addPass(createModuleToFunctionPassAdaptor(IRNormalizerPass()));
 
   // Add a verifier pass if requested. We don't have to do this if the action
   // requires code generation because there will already be a verifier pass in
