2e0f3ac936766e8128c24baf5a67470060db7375
Farzon Lotfi <1802579+farzonl@users.noreply.github.com>
2024-03-15 20:57:08 -0400

    [HLSL] implement `clamp` intrinsic (#85424)
    closes #70071
    - `CGBuiltin.cpp` - Add the unsigned\generic clamp intrinsic emitter.
    - `IntrinsicsDirectX.td` - add the `dx.clamp` & `dx.uclamp` intrinsics
    - `DXILIntrinsicExpansion.cpp` - add the `clamp` instruction expansion
    while maintaining vector form.
    - `SemaChecking.cpp` -  Add `clamp`  builtin Sema Checks.
    - `Builtins.td` - add a `clamp` builtin
    - `hlsl_intrinsics.h` - add the `clamp` api
    
    Why `clamp` as instruction expansion  for DXIL?
    1. SPIR-V has a GLSL `clamp` extension via:
    -
    [FClamp](https://registry.khronos.org/SPIR-V/specs/1.0/GLSL.std.450.html#FClamp)
    -
    [UClamp](https://registry.khronos.org/SPIR-V/specs/1.0/GLSL.std.450.html#UClamp)
    -
    [SClamp](https://registry.khronos.org/SPIR-V/specs/1.0/GLSL.std.450.html#SClamp)
    2. Further Clamp lowers to `min(max( x, min_range ), max_range)` which
    we have float, signed, & unsigned dixilOps.

--- clang/lib/Sema/SemaChecking.cpp
+++ clang/lib/Sema/SemaChecking.cpp
@@ -5499,6 +5499,17 @@ bool Sema::CheckHLSLBuiltinFunctionCall(unsigned BuiltinID, CallExpr *TheCall) {
       return true;
     break;
   }
+  case Builtin::BI__builtin_hlsl_elementwise_clamp: {
+    if (checkArgCount(*this, TheCall, 3))
+      return true;
+    if (CheckVectorElementCallArgs(this, TheCall))
+      return true;
+    if (SemaBuiltinElementwiseTernaryMath(
+            TheCall, /*CheckForFloatArgs*/
+            TheCall->getArg(0)->getType()->hasFloatingRepresentation()))
+      return true;
+    break;
+  }
   case Builtin::BI__builtin_hlsl_dot: {
     if (checkArgCount(*this, TheCall, 2))
       return true;
@@ -5547,7 +5558,9 @@ bool Sema::CheckHLSLBuiltinFunctionCall(unsigned BuiltinID, CallExpr *TheCall) {
       return true;
     if (CheckVectorElementCallArgs(this, TheCall))
       return true;
-    if (SemaBuiltinElementwiseTernaryMath(TheCall, /*CheckForFloatArgs*/ false))
+    if (SemaBuiltinElementwiseTernaryMath(
+            TheCall, /*CheckForFloatArgs*/
+            TheCall->getArg(0)->getType()->hasFloatingRepresentation()))
       return true;
   }
   }
