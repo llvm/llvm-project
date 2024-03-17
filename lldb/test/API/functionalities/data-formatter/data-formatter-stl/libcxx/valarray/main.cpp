bf1d779fe432449147fff315db89d5b2b01a48fa
Mark de Wever <koraq@xs4all.nl>
2024-03-17 14:56:35 +0100

    [lldb][libc++] Adds slice_array data formatters. (#85544)
    Co-authored-by: Michael Buch <michaelbuch12@gmail.com>

--- lldb/test/API/functionalities/data-formatter/data-formatter-stl/libcxx/valarray/main.cpp
+++ lldb/test/API/functionalities/data-formatter/data-formatter-stl/libcxx/valarray/main.cpp
@@ -13,5 +13,8 @@ int main() {
 
   std::valarray<double> va_double({1.0, 0.5, 0.25, 0.125});
 
+  std::valarray<int> va({0, 1, 2, 3, 4, 5, 6, 7, 8, 9});
+  std::slice_array<int> sa = va[std::slice(1, 4, 2)];
+
   std::cout << "break here\n";
 }
