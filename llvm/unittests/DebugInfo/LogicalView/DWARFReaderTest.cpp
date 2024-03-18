2d9959e792c8363807afd7708d76c37fcb848fc3
Carlos Alberto Enciso <47597242+CarlosAlbertoEnciso@users.noreply.github.com>
2024-03-18 05:08:42 +0000

    [llvm-debuginfo-analyzer][NFC] Rename LVElfReader.cpp[h] (#85530)
    As part of the WebAssembly support work review
      https://github.com/llvm/llvm-project/pull/82588
    
    It was decided to rename:
    
      Files: LVElfReader.cpp[h] -> LVDWARFReader.cpp[h]
             ELFReaderTest.cpp  -> DWARFReaderTest.cpp
    
      Class: LVELFReader        -> LVDWARFReader
    
    The name LVDWARFReader would match the another reader LVCodeViewReader
    as they will reflect the type of
    debug information format that they are parsing.

--- llvm/unittests/DebugInfo/LogicalView/DWARFReaderTest.cpp
+++ llvm/unittests/DebugInfo/LogicalView/DWARFReaderTest.cpp
@@ -1,4 +1,4 @@
-//===- llvm/unittest/DebugInfo/LogicalView/ELFReaderTest.cpp --------------===//
+//===- llvm/unittest/DebugInfo/LogicalView/DWARFReaderTest.cpp ------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -323,7 +323,7 @@ void compareElements(SmallString<128> &InputsDir) {
   checkElementComparison(Reference.get(), Target.get());
 }
 
-TEST(LogicalViewTest, ELFReader) {
+TEST(LogicalViewTest, DWARFReader) {
   // Initialize targets and assembly printers/parsers.
   llvm::InitializeAllTargetInfos();
   llvm::InitializeAllTargetMCs();
