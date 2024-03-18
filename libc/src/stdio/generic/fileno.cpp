9b289311091c741674d270009f9557e9d9ac4760
Sh0g0-1758 <shouryagoel10000@gmail.com>
2024-03-18 16:08:15 +0530

    implement fileno

---
+++ libc/src/stdio/generic/fileno.cpp
@@ -0,0 +1,23 @@
+//===-- Implementation of fileno
+//-------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "src/stdio/fileno.h"
+#include "src/__support/File/file.h"
+
+#include <stdio.h>
+
+namespace LIBC_NAMESPACE {
+
+LLVM_LIBC_FUNCTION(int, fileno, (::FILE * stream)) {
+  int result =
+      reinterpret_cast<LIBC_NAMESPACE::File *>(stream)->get_fileno(stream);
+  return result;
+}
+
+} // namespace LIBC_NAMESPACE
