dac232485a3c91b778290379171714f45f34e63e
Owen Pan <owenpiano@gmail.com>
2024-03-17 21:37:32 -0700

    [clang-format][NFC] Minor changes to a unit test in 8e5de66af3da
    See https://github.com/llvm/llvm-project/pull/85470#discussion_r1527297517

--- clang/unittests/Format/FormatTest.cpp
+++ clang/unittests/Format/FormatTest.cpp
@@ -11475,10 +11475,10 @@ TEST_F(FormatTest, UnderstandsNewAndDelete) {
                "void new (link p);\n"
                "void delete (link p);");
 
-  verifyFormat("{ p->delete(); }\n"
-               "{ p->new(); }",
-               "{ p->delete (); }\n"
-               "{ p->new (); }");
+  verifyFormat("p->new();\n"
+               "p->delete();",
+               "p->new ();\n"
+               "p->delete ();");
 
   FormatStyle AfterPlacementOperator = getLLVMStyle();
   AfterPlacementOperator.SpaceBeforeParens = FormatStyle::SBPO_Custom;
