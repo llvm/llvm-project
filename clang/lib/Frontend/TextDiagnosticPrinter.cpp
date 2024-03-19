d46837885ebf1beb9fc2ce9f58f9d6adef37cf89
yronglin <yronglin777@gmail.com>
2024-03-19 22:25:41 +0800

    Fix test failure
    Signed-off-by: yronglin <yronglin777@gmail.com>

--- clang/lib/Frontend/TextDiagnosticPrinter.cpp
+++ clang/lib/Frontend/TextDiagnosticPrinter.cpp
@@ -108,6 +108,7 @@ static void printDiagnosticOptions(raw_ostream &OS,
 
 void TextDiagnosticPrinter::HandleDiagnostic(DiagnosticsEngine::Level Level,
                                              const Diagnostic &Info) {
+  abort();
   // Default implementation (Warnings/errors count).
   DiagnosticConsumer::HandleDiagnostic(Level, Info);
 
