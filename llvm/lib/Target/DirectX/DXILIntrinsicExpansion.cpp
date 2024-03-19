8e499ef696b1c5fd8b262d3104a90129825e3270
Farzon Lotfi <1802579+farzonl@users.noreply.github.com>
2024-03-19 12:03:43 -0400

    [DXIL] implement dot intrinsic lowering for integers (#85662)
    this implements part 1 of 2 for #83626
    - `CGBuiltin.cpp` - modified to have seperate cases for signed and
    unsigned integers.
    - `SemaChecking.cpp` - modified to prevent the generation of a double
    dot product intrinsic if the builtin were to be called directly.
    - `IntrinsicsDirectX.td` creation of the signed and unsigned dot
    intrinsics needed for instruction expansion.
    - `DXILIntrinsicExpansion.cpp` - handle instruction expansion cases for
    integer dot product.

--- llvm/lib/Target/DirectX/DXILIntrinsicExpansion.cpp
+++ llvm/lib/Target/DirectX/DXILIntrinsicExpansion.cpp
@@ -39,9 +39,42 @@ static bool isIntrinsicExpansion(Function &F) {
   case Intrinsic::dx_uclamp:
   case Intrinsic::dx_lerp:
   case Intrinsic::dx_rcp:
+  case Intrinsic::dx_sdot:
+  case Intrinsic::dx_udot:
     return true;
   }
   return false;
+}
+
+static bool expandIntegerDot(CallInst *Orig, Intrinsic::ID DotIntrinsic) {
+  assert(DotIntrinsic == Intrinsic::dx_sdot ||
+         DotIntrinsic == Intrinsic::dx_udot);
+  Intrinsic::ID MadIntrinsic = DotIntrinsic == Intrinsic::dx_sdot
+                                   ? Intrinsic::dx_imad
+                                   : Intrinsic::dx_umad;
+  Value *A = Orig->getOperand(0);
+  Value *B = Orig->getOperand(1);
+  Type *ATy = A->getType();
+  Type *BTy = B->getType();
+  assert(ATy->isVectorTy() && BTy->isVectorTy());
+
+  IRBuilder<> Builder(Orig->getParent());
+  Builder.SetInsertPoint(Orig);
+
+  auto *AVec = dyn_cast<FixedVectorType>(A->getType());
+  Value *Elt0 = Builder.CreateExtractElement(A, (uint64_t)0);
+  Value *Elt1 = Builder.CreateExtractElement(B, (uint64_t)0);
+  Value *Result = Builder.CreateMul(Elt0, Elt1);
+  for (unsigned I = 1; I < AVec->getNumElements(); I++) {
+    Elt0 = Builder.CreateExtractElement(A, I);
+    Elt1 = Builder.CreateExtractElement(B, I);
+    Result = Builder.CreateIntrinsic(Result->getType(), MadIntrinsic,
+                                     ArrayRef<Value *>{Elt0, Elt1, Result},
+                                     nullptr, "dx.mad");
+  }
+  Orig->replaceAllUsesWith(Result);
+  Orig->eraseFromParent();
+  return true;
 }
 
 static bool expandExpIntrinsic(CallInst *Orig) {
@@ -191,6 +224,9 @@ static bool expandIntrinsic(Function &F, CallInst *Orig) {
     return expandLerpIntrinsic(Orig);
   case Intrinsic::dx_rcp:
     return expandRcpIntrinsic(Orig);
+  case Intrinsic::dx_sdot:
+  case Intrinsic::dx_udot:
+    return expandIntegerDot(Orig, F.getIntrinsicID());
   }
   return false;
 }
