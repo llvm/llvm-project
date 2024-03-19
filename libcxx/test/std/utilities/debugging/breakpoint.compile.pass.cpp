cadf3002d64f8cbd4faaafd7e9220f0b0298af44
Hristo Hristov <hghristov.rmm@gmail.com>
2024-03-19 09:38:40 +0200

    Added tests

---
+++ libcxx/test/std/utilities/debugging/breakpoint.compile.pass.cpp
@@ -0,0 +1,20 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++03, c++11, c++14, c++17, c++20, c++23
+// XFAIL: LIBCXX-AIX-FIXME, LIBCXX-PICOLIBC-FIXME
+
+// <debugging>
+
+// void breakpoint() noexcept;
+
+#include <debugging>
+
+// Test without debugger.
+
+static_assert(noexcept(std::breakpoint()));
