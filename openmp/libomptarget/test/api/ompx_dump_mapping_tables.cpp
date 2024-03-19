5730b7bfa3881a3c3dc75dee9b1381c07a37d7ee
Nicole Aschenbrenner <nicole.aschenbrenner@amd.com>
2024-03-19 04:29:47 -0400

    [OpenMP] Fix ompx_dump_mapping_tables lit test
    Fixes ompx_dump_mapping_tables test by only using one device after breaking built bots.

--- openmp/libomptarget/test/api/ompx_dump_mapping_tables.cpp
+++ openmp/libomptarget/test/api/ompx_dump_mapping_tables.cpp
@@ -6,33 +6,28 @@
 #define N 10
 
 int main() {
-  int *a = new int[N]; // mapped and released from device 0
-  int *b = new int[N]; // mapped to device 2
+  int *a = new __int32_t[N];     // mapped and released from device 0
+  int *b = new __int32_t[2 * N]; // mapped to device 0
 
   // clang-format off
   // CHECK: Mapping tables after target enter data:
   // CHECK-NEXT: omptarget device 0 info: OpenMP Host-Device pointer mappings after block
   // CHECK-NEXT: omptarget device 0 info: Host Ptr Target Ptr Size (B) DynRefCount HoldRefCount Declaration
-  // CHECK-NEXT: omptarget device 0 info: {{(0x[0-9a-f]{16})}} {{(0x[0-9a-f]{16})}}
-  // CHECK-NEXT: omptarget device 1 info: OpenMP Host-Device pointer mappings table empty
-  // CHECK-NEXT: omptarget device 2 info: OpenMP Host-Device pointer mappings after block
-  // CHECK-NEXT: omptarget device 2 info: Host Ptr Target Ptr Size (B) DynRefCount HoldRefCount Declaration
-  // CHECK-NEXT: omptarget device 2 info: {{(0x[0-9a-f]{16})}} {{(0x[0-9a-f]{16})}}
-  // clang-format on
+  // CHECK-NEXT: omptarget device 0 info: {{(0x[0-9a-f]{16})}} {{(0x[0-9a-f]{16})}} 40
+  // CHECK-NEXT: omptarget device 0 info: {{(0x[0-9a-f]{16})}} {{(0x[0-9a-f]{16})}} 80
 #pragma omp target enter data device(0) map(to : a[ : N])
-#pragma omp target enter data device(2) map(to : b[ : N])
+#pragma omp target enter data device(0) map(to : b[ : 2*N])
+  // clang-format on
   printf("Mapping tables after target enter data:\n");
   ompx_dump_mapping_tables();
 
   // clang-format off
   // CHECK: Mapping tables after target exit data for a:
-  // CHECK-NEXT: omptarget device 0 info: OpenMP Host-Device pointer mappings table empty
-  // CHECK-NEXT: omptarget device 1 info: OpenMP Host-Device pointer mappings table empty
-  // CHECK-NEXT: omptarget device 2 info: OpenMP Host-Device pointer mappings after block
-  // CHECK-NEXT: omptarget device 2 info: Host Ptr Target Ptr Size (B) DynRefCount HoldRefCount Declaration
-  // CHECK-NEXT: omptarget device 2 info: {{(0x[0-9a-f]{16})}} {{(0x[0-9a-f]{16})}}
-  // clang-format on
+  // CHECK-NEXT: omptarget device 0 info: OpenMP Host-Device pointer mappings after block
+  // CHECK-NEXT: omptarget device 0 info: Host Ptr Target Ptr Size (B) DynRefCount HoldRefCount Declaration
+  // CHECK-NEXT: omptarget device 0 info: {{(0x[0-9a-f]{16})}} {{(0x[0-9a-f]{16})}} 80
 #pragma omp target exit data device(0) map(release : a[ : N])
+  // clang-format on
   printf("\nMapping tables after target exit data for a:\n");
   ompx_dump_mapping_tables();
 
