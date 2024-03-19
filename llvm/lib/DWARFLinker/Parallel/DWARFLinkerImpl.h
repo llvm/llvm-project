d2b1ad7485e2219e2aaa47bdcdc17a7fd462d85c
Andres Villegas <andresvi@google.com>
2024-03-19 10:58:31 -0700

    Revert "[dsymutil] Remove support for obfuscated bitcode" (#85826)
    Reverts llvm/llvm-project#85713 Since it is breaking Linux x64 builds.

--- llvm/lib/DWARFLinker/Parallel/DWARFLinkerImpl.h
+++ llvm/lib/DWARFLinker/Parallel/DWARFLinkerImpl.h
@@ -26,7 +26,8 @@ namespace parallel {
 class DWARFLinkerImpl : public DWARFLinker {
 public:
   DWARFLinkerImpl(MessageHandlerTy ErrorHandler,
-                  MessageHandlerTy WarningHandler);
+                  MessageHandlerTy WarningHandler,
+                  TranslatorFuncTy StringsTranslator);
 
   /// Add object file to be linked. Pre-load compile unit die. Call
   /// \p OnCUDieLoaded for each compile unit die. If specified \p File
