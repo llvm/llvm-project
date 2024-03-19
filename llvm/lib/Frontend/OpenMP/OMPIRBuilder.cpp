7ef433f62c199c414bffdcac1c8ee3159b29c5f5
Jeremy Morse <jeremy.morse@sony.com>
2024-03-19 14:30:41 +0000

    [NFC][RemoveDIs] Switch ConstantExpr::getAsInstruction to not insert (#84737)
    Because the RemoveDIs work is putting a debug-info bit into
    BasicBlock::iterator and iterators are needed for insertion, the
    getAsInstruction method declaration would need to use a fully defined
    instruction-iterator, which leads to a complicated
    header-inclusion-order problem. Much simpler to instead just not insert,
    and make it the callers problem to insert.
    
    This is proportionate because there are only four call-sites to
    getAsInstruction -- it would suck if we did this everywhere.
    
    ---------
    
    Merged by: Stephen Tozer <stephen.tozer@sony.com>

--- llvm/lib/Frontend/OpenMP/OMPIRBuilder.cpp
+++ llvm/lib/Frontend/OpenMP/OMPIRBuilder.cpp
@@ -5073,10 +5073,15 @@ FunctionCallee OpenMPIRBuilder::createDispatchFiniFunction(unsigned IVSize,
 
 static void replaceConstatExprUsesInFuncWithInstr(ConstantExpr *ConstExpr,
                                                   Function *Func) {
-  for (User *User : make_early_inc_range(ConstExpr->users()))
-    if (auto *Instr = dyn_cast<Instruction>(User))
-      if (Instr->getFunction() == Func)
-        Instr->replaceUsesOfWith(ConstExpr, ConstExpr->getAsInstruction(Instr));
+  for (User *User : make_early_inc_range(ConstExpr->users())) {
+    if (auto *Instr = dyn_cast<Instruction>(User)) {
+      if (Instr->getFunction() == Func) {
+        Instruction *ConstInst = ConstExpr->getAsInstruction();
+        ConstInst->insertBefore(*Instr->getParent(), Instr->getIterator());
+        Instr->replaceUsesOfWith(ConstExpr, ConstInst);
+      }
+    }
+  }
 }
 
 static void replaceConstantValueUsesInFuncWithInstr(llvm::Value *Input,
