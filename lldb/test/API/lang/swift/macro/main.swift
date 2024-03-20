134c1bbe12c999c96fea7b010a719129a10b6eea
Adrian Prantl <aprantl@apple.com>
2024-03-20 14:18:47 -0700

    Remove redundant compiler flag from test
    (cherry picked from commit dbbf0f10feffa917f4f04a87d10d8ae1f38a9063)

--- lldb/test/API/lang/swift/macro/main.swift
+++ lldb/test/API/lang/swift/macro/main.swift
@@ -2,8 +2,8 @@ import Macro
 
 func testStringify(a: Int, b: Int) {
   print("break here")
-  let s = #stringify(a + b)
+  let s = #stringify(a / b)
   print(s.1)
 }
 
-testStringify(a: 23, b: 42)
+testStringify(a: 23, b: 0)
