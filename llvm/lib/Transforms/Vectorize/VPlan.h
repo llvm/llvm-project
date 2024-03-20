e67b9ac87308b68c1c5208f09f4f09495a69915d
Benjamin Kramer <benny.kra@googlemail.com>
2024-03-20 15:12:33 +0100

    Revert "[VPlan] Replace disjoint or with add instead of dropping disjoint. (#83821)"
    This reverts commit c2c1e6ee4ce0df3d000ba880fa6cf58441da6462. It creates
    a use after free.
    
    ==8342==ERROR: AddressSanitizer: heap-use-after-free on address 0x50f000001760 at pc 0x55b9fb84a8fb bp 0x7ffc18468a10 sp 0x7ffc18468a08
    READ of size 1 at 0x50f000001760 thread T0
     #0 0x55b9fb84a8fa in dropPoisonGeneratingFlags llvm/lib/Transforms/Vectorize/VPlan.h:1040:13
     #1 0x55b9fb84a8fa in llvm::VPlanTransforms::dropPoisonGeneratingRecipes(llvm::VPlan&, llvm::function_ref<bool (llvm::BasicBlock*)>)::$_0::operator()(llvm::VPRecipeBase*) const llvm/lib/Transforms/Vectorize/VPlanTransforms.cpp:1236:23
     #2 0x55b9fb84a196 in llvm::VPlanTransforms::dropPoisonGeneratingRecipes(llvm::VPlan&, llvm::function_ref<bool (llvm::BasicBlock*)>) llvm/lib/Transforms/Vectorize/VPlanTransforms.cpp
    
    Can be reproduced with asan on
    Transforms/LoopVectorize/AArch64/sve-interleaved-masked-accesses.ll
    Transforms/LoopVectorize/X86/pr81872.ll
    Transforms/LoopVectorize/X86/x86-interleaved-accesses-masked-group.ll

--- llvm/lib/Transforms/Vectorize/VPlan.h
+++ llvm/lib/Transforms/Vectorize/VPlan.h
@@ -1127,12 +1127,6 @@ public:
     return WrapFlags.HasNSW;
   }
 
-  bool isDisjoint() const {
-    assert(OpType == OperationType::DisjointOp &&
-           "recipe cannot have a disjoing flag");
-    return DisjointFlags.IsDisjoint;
-  }
-
 #if !defined(NDEBUG) || defined(LLVM_ENABLE_DUMP)
   void printFlags(raw_ostream &O) const;
 #endif
