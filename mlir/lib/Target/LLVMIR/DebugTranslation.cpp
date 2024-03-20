e95aa7234d38b23a2236dff60475f9687b71dc24
Christian Ulmann <christianulmann@gmail.com>
2024-03-20 16:08:38 +0100

    [MLIR][LLVM] Add extraData field to the DIDerivedType attribute (#85935)
    This commit extends the DIDerivedTypeAttr with the `extraData` field.
    For now, the type of it is limited to be a `DINodeAttr`, as extending
    the debug metadata handling to support arbitrary metadata nodes does not
    seem to be necessary so far.

--- mlir/lib/Target/LLVMIR/DebugTranslation.cpp
+++ mlir/lib/Target/LLVMIR/DebugTranslation.cpp
@@ -163,7 +163,7 @@ llvm::DIDerivedType *DebugTranslation::translateImpl(DIDerivedTypeAttr attr) {
       /*Scope=*/nullptr, translate(attr.getBaseType()), attr.getSizeInBits(),
       attr.getAlignInBits(), attr.getOffsetInBits(),
       /*DWARFAddressSpace=*/std::nullopt, /*PtrAuthData=*/std::nullopt,
-      /*Flags=*/llvm::DINode::FlagZero);
+      /*Flags=*/llvm::DINode::FlagZero, translate(attr.getExtraData()));
 }
 
 llvm::DIFile *DebugTranslation::translateImpl(DIFileAttr attr) {
