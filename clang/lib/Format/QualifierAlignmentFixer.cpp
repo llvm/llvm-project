cb1521d96d5e918f26733425922f5fa319446d8d
Owen Pan <owenpiano@gmail.com>
2024-03-19 20:58:49 -0700

    Revert "[clang-format][NFC] Delete 100+ redundant #include lines in .cpp files"
    This reverts commit b92d6dd704d789240685a336ad8b25a9f381b4cc. See
    github.com/llvm/llvm-project/commit/b92d6dd704d7#commitcomment-139992444
    
    We should use a tool like Visual Studio to clean up the headers.

--- clang/lib/Format/QualifierAlignmentFixer.cpp
+++ clang/lib/Format/QualifierAlignmentFixer.cpp
@@ -13,6 +13,12 @@
 //===----------------------------------------------------------------------===//
 
 #include "QualifierAlignmentFixer.h"
+#include "FormatToken.h"
+#include "llvm/Support/Debug.h"
+#include "llvm/Support/Regex.h"
+
+#include <algorithm>
+#include <optional>
 
 #define DEBUG_TYPE "format-qualifier-alignment-fixer"
 
