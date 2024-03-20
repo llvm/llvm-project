c6f08acecd03f5c54ebe62630a6c6761aae91cb4
Jonas Paulsson <paulson1@linux.ibm.com>
2024-03-20 11:46:19 -0400

    Revert "Move assertion for AdjustsStack from PEI to MachineVerifier. (#85698)"
    This reverts commit 05bde30585710a51592eee0a6cf6df8184d09c92.
    
    Reverting due to verifier complaints with expensive checks on build-bot.

--- llvm/lib/CodeGen/PrologEpilogInserter.cpp
+++ llvm/lib/CodeGen/PrologEpilogInserter.cpp
@@ -372,6 +372,8 @@ void PEI::calculateCallFrameInfo(MachineFunction &MF) {
   MFI.computeMaxCallFrameSize(MF, &FrameSDOps);
   assert(MFI.getMaxCallFrameSize() <= MaxCFSIn &&
          "Recomputing MaxCFS gave a larger value.");
+  assert((FrameSDOps.empty() || MF.getFrameInfo().adjustsStack()) &&
+         "AdjustsStack not set in presence of a frame pseudo instruction.");
 
   if (TFI->canSimplifyCallFramePseudos(MF)) {
     // If call frames are not being included as part of the stack frame, and
