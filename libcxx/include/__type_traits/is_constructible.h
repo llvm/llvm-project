7227ec98ad9a5e8e973d0c831e6f8bc0f7029f9f
Nikolas Klauser <nikolasklauser@berlin.de>
2024-03-20 11:23:48 +0100

    [libc++][NFC] Remove uses of add_{const,cv,volatile} (#85635)
    These traits can be expressed without having to instantiate any classes,
    reducing compile times slightly.

--- libcxx/include/__type_traits/is_constructible.h
+++ libcxx/include/__type_traits/is_constructible.h
@@ -10,7 +10,6 @@
 #define _LIBCPP___TYPE_IS_CONSTRUCTIBLE_H
 
 #include <__config>
-#include <__type_traits/add_const.h>
 #include <__type_traits/add_lvalue_reference.h>
 #include <__type_traits/add_rvalue_reference.h>
 #include <__type_traits/integral_constant.h>
@@ -31,8 +30,7 @@ inline constexpr bool is_constructible_v = __is_constructible(_Tp, _Args...);
 
 template <class _Tp>
 struct _LIBCPP_TEMPLATE_VIS is_copy_constructible
-    : public integral_constant<bool, __is_constructible(_Tp, __add_lvalue_reference_t<typename add_const<_Tp>::type>)> {
-};
+    : public integral_constant<bool, __is_constructible(_Tp, __add_lvalue_reference_t<const _Tp>)> {};
 
 #if _LIBCPP_STD_VER >= 17
 template <class _Tp>
