7cb964cf203d1253510c343254788ee16978c51b
Mariusz Borsa <m_borsa@apple.com>
2024-03-20 16:15:47 -0700

    [Sanitizers][Darwin] Bump up DEFAULT_SANITIZER_MIN_OSX_VERSION
    The greendragon was recently moved and now it runs on somewhat newer macOS
    version - which breaks some sanitizers tests
    
    rdar://125052915
    (cherry picked from commit 2347384476d680f5c7e5ba37d5834b2a1c8282f2)

--- compiler-rt/cmake/config-ix.cmake
+++ compiler-rt/cmake/config-ix.cmake
@@ -446,7 +446,7 @@ if(APPLE)
 
   # Note: In order to target x86_64h on OS X the minimum deployment target must
   # be 10.8 or higher.
-  set(DEFAULT_SANITIZER_MIN_OSX_VERSION 10.10)
+  set(DEFAULT_SANITIZER_MIN_OSX_VERSION 10.13)
   set(DARWIN_osx_MIN_VER_FLAG "-mmacosx-version-min")
   if(NOT SANITIZER_MIN_OSX_VERSION)
     string(REGEX MATCH "${DARWIN_osx_MIN_VER_FLAG}=([.0-9]+)"
