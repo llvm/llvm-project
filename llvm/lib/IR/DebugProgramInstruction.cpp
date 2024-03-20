4baec5dd416dab28477ab0b9ec2d7a5853ca2d8d
Stephen Tozer <stephen.tozer@sony.com>
2024-03-20 15:59:51 +0000

    Fix file header comment

--- llvm/lib/IR/DebugProgramInstruction.cpp
+++ llvm/lib/IR/DebugProgramInstruction.cpp
@@ -1,5 +1,4 @@
-//=====-- DebugProgramInstruction.cpp - Implement DbgRecords/DbgMarkers
-//--=====//
+//=====-- DebugProgramInstruction.cpp - Implement DbgRecords/DbgMarkers --====//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
