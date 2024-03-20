62d3aa29550d40a80d26d2e7dfe1cc3e4fad7ee1
Nikolas Klauser <nikolasklauser@berlin.de>
2024-03-20 11:23:48 +0100

    [libc++][NFC] Remove uses of add_{const,cv,volatile} (#85635)
    These traits can be expressed without having to instantiate any classes,
    reducing compile times slightly.

--- libcxx/include/__type_traits/is_trivially_assignable.h
+++ libcxx/include/__type_traits/is_trivially_assignable.h
@@ -31,9 +31,9 @@ inline constexpr bool is_trivially_assignable_v = __is_trivially_assignable(_Tp,
 
 template <class _Tp>
 struct _LIBCPP_TEMPLATE_VIS is_trivially_copy_assignable
-    : public integral_constant<bool,
-                               __is_trivially_assignable(__add_lvalue_reference_t<_Tp>,
-                                                         __add_lvalue_reference_t<typename add_const<_Tp>::type>)> {};
+    : public integral_constant<
+          bool,
+          __is_trivially_assignable(__add_lvalue_reference_t<_Tp>, __add_lvalue_reference_t<const _Tp>)> {};
 
 #if _LIBCPP_STD_VER >= 17
 template <class _Tp>
