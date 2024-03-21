5d1fba7496647593ec92f833a5599266e77ddeb3
Sh0g0-1758 <shouryagoel10000@gmail.com>
2024-03-21 14:02:09 +0530

    Format header

--- libc/src/math/canonicalizel.h
+++ libc/src/math/canonicalizel.h
@@ -1,11 +1,10 @@
-//===-- Implementation header for canonicalizel ------------------------*- C++
-//-*-===//
+//===-- Implementation header for canonicalizel ------------------*- C++-*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
-//===-----------------------------------------------------------------------------===//
+//===----------------------------------------------------------------------===//
 
 #ifndef LLVM_LIBC_SRC_MATH_CANONICALIZEL_H
 #define LLVM_LIBC_SRC_MATH_CANONICALIZEL_H
