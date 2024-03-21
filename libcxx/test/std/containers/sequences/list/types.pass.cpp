8ba5582b8941cbfad81b74bfab0df4f7db87d606
Ilya Biryukov <ibiryukov@google.com>
2024-03-21 10:42:33 +0100

    fixup! [libc++] Remove macros for keeping std::allocator members and void specialization after C++20
    Add spaces between consequtive right angle brackets.

--- libcxx/test/std/containers/sequences/list/types.pass.cpp
+++ libcxx/test/std/containers/sequences/list/types.pass.cpp
@@ -35,10 +35,10 @@ int main(int, char**)
     typedef std::list<int> C;
     static_assert((std::is_same<C::value_type, int>::value), "");
     static_assert((std::is_same<C::allocator_type, std::allocator<int> >::value), "");
-    static_assert((std::is_same<C::reference, std::allocator_traits<std::allocator<int>>::value_type&>::value), "");
-    static_assert((std::is_same<C::const_reference, const std::allocator_traits<std::allocator<int>>::value_type&>::value), "");
-    static_assert((std::is_same<C::pointer, std::allocator_traits<std::allocator<int>>::pointer>::value), "");
-    static_assert((std::is_same<C::const_pointer, std::allocator_traits<std::allocator<int>>::const_pointer>::value), "");
+    static_assert((std::is_same<C::reference, std::allocator_traits<std::allocator<int> >::value_type&>::value), "");
+    static_assert((std::is_same<C::const_reference, const std::allocator_traits<std::allocator<int> >::value_type&>::value), "");
+    static_assert((std::is_same<C::pointer, std::allocator_traits<std::allocator<int> >::pointer>::value), "");
+    static_assert((std::is_same<C::const_pointer, std::allocator_traits<std::allocator<int> >::const_pointer>::value), "");
 
     static_assert((std::is_signed<typename C::difference_type>::value), "");
     static_assert((std::is_unsigned<typename C::size_type>::value), "");
