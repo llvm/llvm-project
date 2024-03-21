c1da42c5c5154fdc1fe8324dccd5368568d5304e
Yeoul Na <yeoul_na@apple.com>
2024-03-22 05:45:31 +0900

    Fix test.attr-counted-by-debug-info.c to also cover 32-bit long

--- clang/test/CodeGen/attr-counted-by-debug-info.c
+++ clang/test/CodeGen/attr-counted-by-debug-info.c
@@ -13,6 +13,6 @@ struct {
 } agent_send_response_port_num;
 
 // CHECK: !DICompositeType(tag: DW_TAG_array_type, baseType: ![[BT:.*]], elements: ![[ELEMENTS:.*]])
-// CHECK: ![[BT]] = !DIBasicType(name: "long", size: 64, encoding: DW_ATE_signed)
+// CHECK: ![[BT]] = !DIBasicType(name: "long", size: {{.*}}, encoding: DW_ATE_signed)
 // CHECK: ![[ELEMENTS]] = !{![[COUNT:.*]]}
 // CHECK: ![[COUNT]] = !DISubrange(count: -1)
