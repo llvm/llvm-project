cdbec7baf1bc31b59526442c9d4d5f53aac746eb
Nick Desaulniers <nickdesaulniers@users.noreply.github.com>
2024-03-22 08:24:08 -0700

    [libc] fix up the use of angle includes in include/ (#86027)
    Performed en-masse via:
    
        $ grep -rn "#include <ll" libc/include -l | \
          xargs perl -pi -e 's/#include <ll(.*)>/#include "ll$1"/'
        $ grep -rn "#include <__" libc/include -l | \
          xargs perl -pi -e 's/#include <__(.*)>/#include "__$1"/'
    
    Link: #83463
    Link: #83210

--- libc/include/llvm-libc-types/pthread_mutex_t.h
+++ libc/include/llvm-libc-types/pthread_mutex_t.h
@@ -9,7 +9,7 @@
 #ifndef LLVM_LIBC_TYPES_PTHREAD_MUTEX_T_H
 #define LLVM_LIBC_TYPES_PTHREAD_MUTEX_T_H
 
-#include <llvm-libc-types/__mutex_type.h>
+#include "llvm-libc-types/__mutex_type.h"
 
 typedef __mutex_type pthread_mutex_t;
 
