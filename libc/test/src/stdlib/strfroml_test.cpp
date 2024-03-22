21bdaac8d9d03ee56df09b3dbc9f122699487ee8
Vinayak Dev <104419489+vinayakdsci@users.noreply.github.com>
2024-03-23 00:09:53 +0530

    [libc] Implement strfromd() and strfroml() (#86113)
    Follow up to #85438.
    
    Implements the functions `strfromd()` and `strfroml()` introduced in
    C23, and unifies the testing framework for `strfrom*()` functions.

--- libc/test/src/stdlib/strfroml_test.cpp
+++ libc/test/src/stdlib/strfroml_test.cpp
@@ -1,4 +1,4 @@
-//===-- Unittests for strfromf --------------------------------------------===//
+//===-- Unittests for strfroml --------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -7,7 +7,7 @@
 //===----------------------------------------------------------------------===//
 
 #include "StrfromTest.h"
-#include "src/stdlib/strfromf.h"
+#include "src/stdlib/strfroml.h"
 #include "test/UnitTest/Test.h"
 
-STRFROM_TEST(float, StrFromf, LIBC_NAMESPACE::strfromf)
+STRFROM_TEST(long double, Strfroml, LIBC_NAMESPACE::strfroml)
