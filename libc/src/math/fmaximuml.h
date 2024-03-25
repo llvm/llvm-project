aa184385cde0b373a6087c51127649160698f42b
Job Henandez Lara <hj93@protonmail.com>
2024-03-25 12:19:10 -0700

    [libc][math][c23] add c23 floating point fmaximum and fminimum functions. (#86016)
    Fixes https://github.com/llvm/llvm-project/issues/85496.
    
    ---------
    
    Co-authored-by: Job Hernandez <h93@protonmail.com>

---
+++ libc/src/math/fmaximuml.h
@@ -0,0 +1,19 @@
+//===-- Implementation header for fmaximuml -------------------------*- C++
+//-*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_LIBC_SRC_MATH_FMAXIMUML_H
+#define LLVM_LIBC_SRC_MATH_FMAXIMUML_H
+
+namespace LIBC_NAMESPACE {
+
+long double fmaximuml(long double x, long double y);
+
+} // namespace LIBC_NAMESPACE
+
+#endif // LLVM_LIBC_SRC_MATH_FMAXIMUML_H
