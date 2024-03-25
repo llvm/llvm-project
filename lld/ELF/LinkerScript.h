d7054f2ed1e724036cdb76e200f8ac02a991fc9a
Parth Arora <quic_partaror@quicinc.com>
2024-03-25 16:11:21 -0700

    [ELF] Fix unnecessary inclusion of unreferenced provide symbols
    Previously, linker was unnecessarily including a PROVIDE symbol which
    was referenced by another unused PROVIDE symbol. For example, if a
    linker script contained the below code and 'not_used_sym' provide symbol
    is not included, then linker was still unnecessarily including 'foo' PROVIDE
    symbol because it was referenced by 'not_used_sym'. This commit fixes
    this behavior.
    
    PROVIDE(not_used_sym = foo)
    PROVIDE(foo = 0x1000)
    
    This commit fixes this behavior by using dfs-like algorithm to find
    all the symbols referenced in provide expressions of included provide
    symbols.
    
    This commit also fixes the issue of unused section not being garbage-collected
    if a symbol of the section is referenced by an unused PROVIDE symbol.
    
    Closes #74771
    Closes #84730
    
    Co-authored-by: Fangrui Song <i@maskray.me>

--- lld/ELF/LinkerScript.h
+++ lld/ELF/LinkerScript.h
@@ -16,6 +16,7 @@
 #include "llvm/ADT/ArrayRef.h"
 #include "llvm/ADT/DenseMap.h"
 #include "llvm/ADT/MapVector.h"
+#include "llvm/ADT/SmallVector.h"
 #include "llvm/ADT/StringRef.h"
 #include "llvm/Support/Compiler.h"
 #include <cstddef>
@@ -348,6 +349,18 @@ public:
   // Check backward location counter assignment and memory region/LMA overflows.
   void checkFinalScriptConditions() const;
 
+  // Add symbols that are referenced in the linker script to the symbol table.
+  // Symbols referenced in a PROVIDE command are only added to the symbol table
+  // if the PROVIDE command actually provides the symbol.
+  // It also adds the symbols referenced by the used PROVIDE symbols to the
+  // linker script referenced symbols list.
+  void addScriptReferencedSymbolsToSymTable();
+
+  // Returns true if the PROVIDE symbol should be added to the link.
+  // A PROVIDE symbol is added to the link only if it satisfies an
+  // undefined reference.
+  static bool shouldAddProvideSym(StringRef symName);
+
   // SECTIONS command list.
   SmallVector<SectionCommand *, 0> sectionCommands;
 
@@ -379,6 +392,14 @@ public:
 
   // Sections that will be warned/errored by --orphan-handling.
   SmallVector<const InputSectionBase *, 0> orphanSections;
+
+  // Stores the mapping: PROVIDE symbol -> symbols referred in the PROVIDE
+  // expression. For example, if the PROVIDE command is:
+  //
+  // PROVIDE(v = a + b + c);
+  //
+  // then provideMap should contain the mapping: 'v' -> ['a', 'b', 'c']
+  llvm::MapVector<StringRef, SmallVector<StringRef, 0>> provideMap;
 };
 
 LLVM_LIBRARY_VISIBILITY extern std::unique_ptr<LinkerScript> script;
