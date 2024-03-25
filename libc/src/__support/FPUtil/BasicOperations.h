dde2428e6ea5e710784d960170a5813d907ed68d
Job Hernandez <h93@protonmail.com>
2024-03-25 10:02:19 -0700

    format code

--- libc/src/__support/FPUtil/BasicOperations.h
+++ libc/src/__support/FPUtil/BasicOperations.h
@@ -90,17 +90,17 @@ LIBC_INLINE T fmaximum_num(T x, T y) {
   FPBits<T> bitx(x), bity(y);
 
   if (bitx.is_nan()) {
-    if (bitx.is_signaling_nan()) 
+    if (bitx.is_signaling_nan())
       fputil::raise_except_if_required(FE_INVALID);
     if (bity.is_nan())
-	return FPBits<T>::quiet_nan().get_val();
-     return y;
-}
+      return FPBits<T>::quiet_nan().get_val();
+    return y;
+  }
   if (bity.is_nan()) {
-    if (bity.is_signaling_nan()) 
+    if (bity.is_signaling_nan())
       fputil::raise_except_if_required(FE_INVALID);
     return x;
-}
+  }
   if (bitx.is_quiet_nan())
     return y;
   if (bity.is_quiet_nan())
@@ -114,17 +114,17 @@ template <typename T, cpp::enable_if_t<cpp::is_floating_point_v<T>, int> = 0>
 LIBC_INLINE T fminimum_num(T x, T y) {
   const FPBits<T> bitx(x), bity(y);
   if (bitx.is_nan()) {
-    if (bitx.is_signaling_nan()) 
+    if (bitx.is_signaling_nan())
       fputil::raise_except_if_required(FE_INVALID);
     if (bity.is_nan())
-	return FPBits<T>::quiet_nan().get_val();
-     return y;
-}
+      return FPBits<T>::quiet_nan().get_val();
+    return y;
+  }
   if (bity.is_nan()) {
-    if (bity.is_signaling_nan()) 
+    if (bity.is_signaling_nan())
       fputil::raise_except_if_required(FE_INVALID);
     return x;
-}
+  }
   if (bitx.is_quiet_nan())
     return y;
   if (bity.is_quiet_nan())
