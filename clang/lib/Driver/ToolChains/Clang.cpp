6dcf86e0d8a19fad4cdeeb64ee34391f8fbcb7b5
Daniil Kovalev <dkovalev@accesssoftek.com>
2024-03-26 18:41:40 +0300

    Fix code formatting

--- clang/lib/Driver/ToolChains/Clang.cpp
+++ clang/lib/Driver/ToolChains/Clang.cpp
@@ -1789,11 +1789,13 @@ void Clang::AddAArch64TargetArgs(const ArgList &Args,
   Args.addOptInFlag(CmdArgs, options::OPT_fptrauth_auth_traps,
                     options::OPT_fno_ptrauth_auth_traps);
 
-  Args.addOptInFlag(CmdArgs, options::OPT_fptrauth_vtable_pointer_address_discrimination,
-                    options::OPT_fno_ptrauth_vtable_pointer_address_discrimination);
+  Args.addOptInFlag(
+      CmdArgs, options::OPT_fptrauth_vtable_pointer_address_discrimination,
+      options::OPT_fno_ptrauth_vtable_pointer_address_discrimination);
 
-  Args.addOptInFlag(CmdArgs, options::OPT_fptrauth_vtable_pointer_type_discrimination,
-                    options::OPT_fno_ptrauth_vtable_pointer_type_discrimination);
+  Args.addOptInFlag(
+      CmdArgs, options::OPT_fptrauth_vtable_pointer_type_discrimination,
+      options::OPT_fno_ptrauth_vtable_pointer_type_discrimination);
 
   Args.addOptInFlag(CmdArgs, options::OPT_fptrauth_init_fini,
                     options::OPT_fno_ptrauth_init_fini);
