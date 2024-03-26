1a1586c0d2e8bf4e897021a1a67e67b0299e3d4b
Cyndy Ishida <cyndy_ishida@apple.com>
2024-03-26 11:55:37 -0400

    [InstallAPI] Add missing license header to file, NFC

--- clang/lib/InstallAPI/DylibVerifier.cpp
+++ clang/lib/InstallAPI/DylibVerifier.cpp
@@ -1,3 +1,11 @@
+//===- DylibVerifier.cpp ----------------------------------------*- C++--*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
 #include "clang/InstallAPI/DylibVerifier.h"
 #include "clang/InstallAPI/FrontendRecords.h"
 #include "clang/InstallAPI/InstallAPIDiagnostic.h"
