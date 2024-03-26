e25de13be58ddc1be4c9de8a10fc3a79715a4d3c
smanna12 <soumi.manna@intel.com>
2024-03-26 11:48:37 -0500

    [NFC][Clang] Fix potential dereferencing of nullptr (#85944)
    This patch replaces getAs<> with castAs<> to resolve potential static
    analyzer bugs for
    
    1. Dereferencing a pointer issue with nullptr FPT when calling
    ResolveExceptionSpec() in
        checkEscapingByref(clang::VarDecl *, clang::Sema &).
    
    3. Dereferencing a pointer issue with nullptr ElementTy->getAs() when
    calling getElementType() in
    clang::Sema::SemaBuiltinFPClassification(clang::CallExpr *, unsigned
    int).
    
    4. Dereferencing a pointer issue with nullptr ConvType->getAs() when
    calling getKeyword() in
    clang::Sema::ActOnConversionDeclarator(clang::CXXConversionDecl *).

--- clang/lib/Sema/Sema.cpp
+++ clang/lib/Sema/Sema.cpp
@@ -2207,7 +2207,7 @@ static void checkEscapingByref(VarDecl *VD, Sema &S) {
   // block copy/destroy functions. Resolve it here.
   if (const CXXRecordDecl *RD = T->getAsCXXRecordDecl())
     if (CXXDestructorDecl *DD = RD->getDestructor()) {
-      auto *FPT = DD->getType()->getAs<FunctionProtoType>();
+      auto *FPT = DD->getType()->castAs<FunctionProtoType>();
       S.ResolveExceptionSpec(Loc, FPT);
     }
 }
