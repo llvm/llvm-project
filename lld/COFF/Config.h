e605f5ecd69b2357b318b39a0eacf71f8508b080
Jacek Caban <jacek@codeweavers.com>
2024-03-27 11:37:02 +0100

    [LLD][COFF] Add support for EXPORTAS import name type. (#86541)
    #78772 added similar support for .def file parser and import library
    writer. This PR adds missing bits in LLD to propagate EXPORTAS name and
    allow it in `/export` parser. This is syntax is used by MSVC for ARM64EC
    `__declspec(dllexport)` handling.

--- lld/COFF/Config.h
+++ lld/COFF/Config.h
@@ -54,6 +54,7 @@ enum class EmitKind { Obj, LLVM, ASM };
 struct Export {
   StringRef name;       // N in /export:N or /export:E=N
   StringRef extName;    // E in /export:E=N
+  StringRef exportAs;   // E in /export:N,EXPORTAS,E
   StringRef aliasTarget; // GNU specific: N in "alias == N"
   Symbol *sym = nullptr;
   uint16_t ordinal = 0;
@@ -73,10 +74,9 @@ struct Export {
   StringRef exportName; // Name in DLL
 
   bool operator==(const Export &e) const {
-    return (name == e.name && extName == e.extName &&
-            aliasTarget == e.aliasTarget &&
-            ordinal == e.ordinal && noname == e.noname &&
-            data == e.data && isPrivate == e.isPrivate);
+    return (name == e.name && extName == e.extName && exportAs == e.exportAs &&
+            aliasTarget == e.aliasTarget && ordinal == e.ordinal &&
+            noname == e.noname && data == e.data && isPrivate == e.isPrivate);
   }
 };
 
