05a882b276e6e6f3c8502c9e1102d575f4c49018
efferre79 <rossi.f@inwind.it>
2024-03-28 11:11:21 +0100

    [libclang/python] export libclang version to the bindings

--- clang/bindings/python/clang/cindex.py
+++ clang/bindings/python/clang/cindex.py
@@ -3846,6 +3846,7 @@ functionList = [
     ("clang_getCanonicalCursor", [Cursor], Cursor, Cursor.from_cursor_result),
     ("clang_getCanonicalType", [Type], Type, Type.from_result),
     ("clang_getChildDiagnostics", [Diagnostic], c_object_p),
+    ("clang_getClangVersion", [], _CXString, _CXString.from_result),
     ("clang_getCompletionAvailability", [c_void_p], c_int),
     ("clang_getCompletionBriefComment", [c_void_p], _CXString, _CXString.from_result),
     ("clang_getCompletionChunkCompletionString", [c_void_p, c_int], c_object_p),
@@ -4178,6 +4179,9 @@ class Config:
             return False
 
         return True
+ 
+    def get_version(self):
+        return self.lib.clang_getClangVersion()
 
 
 def register_enumerations():
