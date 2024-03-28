2e5660aa22388eeaaeae63076fcbd6370e250ac9
Petr Hosek <phosek@google.com>
2024-03-27 23:59:24 -0700

    [libc] Move baremetal write_to_stderr implementation to io.cpp (#86890)
    This is required to avoid multiple definitions error.

--- libc/src/__support/OSUtil/baremetal/io.cpp
+++ libc/src/__support/OSUtil/baremetal/io.cpp
@@ -6,15 +6,17 @@
 //
 //===----------------------------------------------------------------------===//
 
-#ifndef LLVM_LIBC_SRC___SUPPORT_OSUTIL_BAREMETAL_IO_H
-#define LLVM_LIBC_SRC___SUPPORT_OSUTIL_BAREMETAL_IO_H
+#include "io.h"
 
 #include "src/__support/CPP/string_view.h"
 
+// This is intended to be provided by the vendor.
+extern "C" void __llvm_libc_log_write(const char *msg, size_t len);
+
 namespace LIBC_NAMESPACE {
 
-void write_to_stderr(cpp::string_view msg);
+void write_to_stderr(cpp::string_view msg) {
+  __llvm_libc_log_write(msg.data(), msg.size());
+}
 
 } // namespace LIBC_NAMESPACE
-
-#endif // LLVM_LIBC_SRC___SUPPORT_OSUTIL_BAREMETAL_IO_H
