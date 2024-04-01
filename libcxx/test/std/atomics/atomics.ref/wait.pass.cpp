ef5fb5a070e5b9ec2585491b1752cfbb9afd53ef
Damien L-G <dalg24@gmail.com>
2024-03-31 21:40:20 -0400

    [libc++][atomic_ref] fix redeclaration of x shadow warning

--- libcxx/test/std/atomics/atomics.ref/wait.pass.cpp
+++ libcxx/test/std/atomics/atomics.ref/wait.pass.cpp
@@ -21,30 +21,32 @@
 template <typename T>
 struct TestWait {
   void operator()() const {
-    T x(T(1));
-    std::atomic_ref<T> const a(x);
+    {
+      T x(T(1));
+      std::atomic_ref<T> const a(x);
 
-    assert(a.load() == T(1));
-    a.wait(T(0));
-    std::thread t1 = support::make_test_thread([&]() {
-      a.store(T(3));
-      a.notify_one();
-    });
-    a.wait(T(1));
-    assert(a.load() == T(3));
-    t1.join();
-    ASSERT_NOEXCEPT(a.wait(T(0)));
+      assert(a.load() == T(1));
+      a.wait(T(0));
+      std::thread t1 = support::make_test_thread([&]() {
+        a.store(T(3));
+        a.notify_one();
+      });
+      a.wait(T(1));
+      assert(a.load() == T(3));
+      t1.join();
+      ASSERT_NOEXCEPT(a.wait(T(0)));
 
-    assert(a.load() == T(3));
-    a.wait(T(0), std::memory_order_seq_cst);
-    std::thread t2 = support::make_test_thread([&]() {
-      a.store(T(5));
-      a.notify_one();
-    });
-    a.wait(T(3), std::memory_order_seq_cst);
-    assert(a.load() == T(5));
-    t2.join();
-    ASSERT_NOEXCEPT(a.wait(T(0), std::memory_order_seq_cst));
+      assert(a.load() == T(3));
+      a.wait(T(0), std::memory_order_seq_cst);
+      std::thread t2 = support::make_test_thread([&]() {
+        a.store(T(5));
+        a.notify_one();
+      });
+      a.wait(T(3), std::memory_order_seq_cst);
+      assert(a.load() == T(5));
+      t2.join();
+      ASSERT_NOEXCEPT(a.wait(T(0), std::memory_order_seq_cst));
+    }
 
     // memory_order::acquire
     {
