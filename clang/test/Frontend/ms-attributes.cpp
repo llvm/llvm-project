82ef047d9aae3d1d58bc5127cb44601e3f7a3e69
Elliot <35050275+apache-hb@users.noreply.github.com>
2024-04-01 14:54:36 -0400

    fix failing test

--- clang/test/Frontend/ms-attributes.cpp
+++ clang/test/Frontend/ms-attributes.cpp
@@ -1,6 +1,6 @@
-// RUN: %clang -fplugin=%llvmshlibdir/MicrosoftAttributes%pluginext -fms-extensions -E %s | FileCheck %s --check-prefix=MS
-// REQUIRE: plugins, examples
+// RUN: %clang -fplugin=%llvmshlibdir/MicrosoftAttributes%pluginext -fsyntax-only -fms-extensions -E %s | FileCheck %s
+// REQUIRES: plugins, examples
 // expected-no-diagnostics
 [ms_example]
 class C {};
-// CHECK-NEXT: AnnotateAttr{{.*}} "ms_example"
+// CHECK: AnnotateAttr{{.*}} "ms_example"
