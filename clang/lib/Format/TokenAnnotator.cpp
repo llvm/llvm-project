fecfb1eba4f43fe942693fcd7eeee2c25d208a10
Owen Pan <owenpiano@gmail.com>
2024-04-02 14:48:14 -0700

    [clang-format] Fix a regression in annotating TrailingReturnArrow (#86624)
    Fixes #86559.

--- clang/lib/Format/TokenAnnotator.cpp
+++ clang/lib/Format/TokenAnnotator.cpp
@@ -3889,6 +3889,8 @@ void TokenAnnotator::calculateFormattingInformation(AnnotatedLine &Line) const {
       }
     } else if (ClosingParen) {
       for (auto *Tok = ClosingParen->Next; Tok; Tok = Tok->Next) {
+        if (Tok->is(TT_CtorInitializerColon))
+          break;
         if (Tok->is(tok::arrow)) {
           Tok->setType(TT_TrailingReturnArrow);
           break;
