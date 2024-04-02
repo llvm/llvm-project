c89b9694c8dc05b1416d2076a067316ed19f0049
Nathan Chancellor <nathan@kernel.org>
2024-04-02 14:59:57 -0700

    fixup! [clang] Support __typeof_unqual__ in all C modes
    Include more coverage for new test
    
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- clang/test/SemaCXX/typeof_unqual.cpp
+++ clang/test/SemaCXX/typeof_unqual.cpp
@@ -1,5 +1,5 @@
 // RUN: %clang_cc1 -fsyntax-only -verify %s
-// expected-no-diagnostics
 
-__typeof_unqual__(int) u = 12;
+typeof_unqual(int) u = 12; // expected-error {{expected function body after function declarator}}
 __typeof_unqual(int) _u = 12;
+__typeof_unqual__(int) __u = 12;
