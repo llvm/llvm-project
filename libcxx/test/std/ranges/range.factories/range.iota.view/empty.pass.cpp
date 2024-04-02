02c4e7184ef60ce8511697d0ea152be65c80a3e9
Hristo Hristov <hghristov.rmm@gmail.com>
2024-04-02 16:37:29 +0300

    Addressed comments

--- libcxx/test/std/ranges/range.factories/range.iota.view/empty.pass.cpp
+++ libcxx/test/std/ranges/range.factories/range.iota.view/empty.pass.cpp
@@ -19,12 +19,69 @@
 #include "types.h"
 
 template <typename R>
-concept HasFreeEmpty = requires(R r) { std::ranges::empty(r); };
-
-template <typename R>
-concept HasMemberEmpty = requires(R r) {
+concept HasEmpty = requires(R r) {
+  std::ranges::empty(r);
   { r.empty() } -> std::same_as<bool>;
 };
+
+constexpr void test_empty_iota_sfinae() {
+  std::vector<int> ev;
+
+  // Both parameters are non-const
+  {
+    auto iv = std::views::iota(std::ranges::begin(ev), std::ranges::end(ev));
+
+    static_assert(HasEmpty<decltype(iv)>);
+  }
+  // Left parameter is const
+  {
+    auto iv = std::views::iota(std::ranges::begin(std::as_const(ev)), std::ranges::end(ev));
+
+    static_assert(HasEmpty<decltype(iv)>);
+  }
+  // Right parameter is const
+  {
+    auto iv = std::views::iota(std::ranges::begin(ev), std::ranges::end(std::as_const(ev)));
+
+    static_assert(HasEmpty<decltype(iv)>);
+  }
+  // Both parameters are const
+  {
+    auto iv = std::views::iota(std::ranges::begin(std::as_const(ev)), std::ranges::end(std::as_const(ev)));
+
+    static_assert(HasEmpty<decltype(iv)>);
+  }
+}
+
+constexpr void test_nonempty_iota_sfinae() {
+  // Default ctr
+  {
+    std::ranges::iota_view<Int42<DefaultTo42>> iv;
+
+    static_assert(HasEmpty<decltype(iv)>);
+  }
+  // Value pass
+  {
+    std::ranges::iota_view<SomeInt> iv(SomeInt(94));
+
+    static_assert(HasEmpty<decltype(iv)>);
+  }
+
+  {
+    std::vector<char> v;
+    auto it = std::back_inserter(v);
+    auto iv = std::views::iota(it);
+
+    static_assert(HasEmpty<decltype(iv)>);
+  }
+  {
+    std::vector<char> v{'b', 'a', 'b', 'a', 'z', 'm', 't'};
+    auto it = std::back_inserter(v);
+    auto iv = std::views::iota(it);
+
+    static_assert(HasEmpty<decltype(iv)>);
+  }
+}
 
 constexpr void test_empty_iota() {
   std::vector<int> ev;
@@ -33,17 +90,11 @@ constexpr void test_empty_iota() {
   {
     auto iv = std::views::iota(std::ranges::begin(ev), std::ranges::end(ev));
 
-    static_assert(HasFreeEmpty<decltype(iv)>);
-    static_assert(HasMemberEmpty<decltype(iv)>);
-
     assert(iv.empty());
   }
   // Left parameter is const
   {
     auto iv = std::views::iota(std::ranges::begin(std::as_const(ev)), std::ranges::end(ev));
-
-    static_assert(HasFreeEmpty<decltype(iv)>);
-    static_assert(HasMemberEmpty<decltype(iv)>);
 
     assert(iv.empty());
   }
@@ -51,29 +102,11 @@ constexpr void test_empty_iota() {
   {
     auto iv = std::views::iota(std::ranges::begin(ev), std::ranges::end(std::as_const(ev)));
 
-    static_assert(HasFreeEmpty<decltype(iv)>);
-    static_assert(HasMemberEmpty<decltype(iv)>);
-
     assert(iv.empty());
   }
   // Both parameters are const
   {
     auto iv = std::views::iota(std::ranges::begin(std::as_const(ev)), std::ranges::end(std::as_const(ev)));
-
-    static_assert(HasFreeEmpty<decltype(iv)>);
-    static_assert(HasMemberEmpty<decltype(iv)>);
-
-    assert(iv.empty());
-  }
-
-  std::vector<char> v{'b', 'a', 'b', 'a', 'z', 'm', 't'};
-  auto fv = v | std::views::filter([](auto val) { return val == '0'; });
-
-  {
-    auto iv = std::views::iota(std::ranges::begin(fv), std::ranges::end(fv));
-
-    static_assert(HasFreeEmpty<decltype(iv)>);
-    static_assert(HasMemberEmpty<decltype(iv)>);
 
     assert(iv.empty());
   }
@@ -84,17 +117,11 @@ constexpr void test_nonempty_iota() {
   {
     std::ranges::iota_view<Int42<DefaultTo42>> iv;
 
-    static_assert(HasFreeEmpty<decltype(iv)>);
-    static_assert(HasMemberEmpty<decltype(iv)>);
-
     assert(!iv.empty());
   }
   // Value pass
   {
     std::ranges::iota_view<SomeInt> iv(SomeInt(94));
-
-    static_assert(HasFreeEmpty<decltype(iv)>);
-    static_assert(HasMemberEmpty<decltype(iv)>);
 
     assert(!iv.empty());
   }
@@ -104,18 +131,12 @@ constexpr void test_nonempty_iota() {
     auto it = std::back_inserter(v);
     auto iv = std::views::iota(it);
 
-    static_assert(HasFreeEmpty<decltype(iv)>);
-    static_assert(HasMemberEmpty<decltype(iv)>);
-
     assert(!iv.empty());
   }
   {
     std::vector<char> v{'b', 'a', 'b', 'a', 'z', 'm', 't'};
     auto it = std::back_inserter(v);
     auto iv = std::views::iota(it);
-
-    static_assert(HasFreeEmpty<decltype(iv)>);
-    static_assert(HasMemberEmpty<decltype(iv)>);
 
     assert(!iv.empty());
   }
