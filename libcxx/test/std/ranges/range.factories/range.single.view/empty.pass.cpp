9b6d7befd953a6e13dd8bec227af6aa88778b932
Hristo Hristov <hghristov.rmm@gmail.com>
2024-04-02 20:15:19 +0300

    Minor tweak

--- libcxx/test/std/ranges/range.factories/range.single.view/empty.pass.cpp
+++ libcxx/test/std/ranges/range.factories/range.single.view/empty.pass.cpp
@@ -25,13 +25,27 @@ struct BigType {
 template <typename T>
 constexpr void test_empty(T value) {
   using SingleView = std::ranges::single_view<T>;
-  SingleView sv{value};
 
-  std::same_as<bool> decltype(auto) result = SingleView::empty();
-  assert(result == false);
-  static_assert(noexcept(SingleView::empty()));
-  static_assert(noexcept(std::ranges::empty(sv)));
-  static_assert(noexcept(std::ranges::empty(std::as_const(sv))));
+  {
+    std::same_as<bool> decltype(auto) result = SingleView::empty();
+    assert(result == false);
+    static_assert(noexcept(SingleView::empty()));
+  }
+
+  {
+    SingleView sv{value};
+
+    std::same_as<bool> decltype(auto) result = std::ranges::empty(sv);
+    assert(result == false);
+    static_assert(noexcept(std::ranges::empty(sv)));
+  }
+  {
+    const SingleView sv{value};
+
+    std::same_as<bool> decltype(auto) result = std::ranges::empty(sv);
+    assert(result == false);
+    static_assert(noexcept(std::ranges::empty(std::as_const(sv))));
+  }
 }
 
 constexpr bool test() {
