975bf492281a17e961ba7e5fb88b1636cf86bcbb
Owen Pan <owenpiano@gmail.com>
2024-04-02 22:03:48 -0700

    Use ArrayRef<tooling::Range> instead.

--- clang/lib/Format/Format.cpp
+++ clang/lib/Format/Format.cpp
@@ -3578,7 +3578,7 @@ cleanupAroundReplacements(StringRef Code, const tooling::Replacements &Replaces,
   // We need to use lambda function here since there are two versions of
   // `cleanup`.
   auto Cleanup = [](const FormatStyle &Style, StringRef Code,
-                    const std::vector<tooling::Range> &Ranges,
+                    ArrayRef<tooling::Range> Ranges,
                     StringRef FileName) -> tooling::Replacements {
     return cleanup(Style, Code, Ranges, FileName);
   };
