57af3d2e1da231f632f29e6a1d99b864d981cde8
Jon Roelofs <jonathan_roelofs@apple.com>
2024-04-04 11:38:20 -0700

    capitalization

--- llvm/unittests/ADT/Interleave.cpp
+++ llvm/unittests/ADT/Interleave.cpp
@@ -1,4 +1,4 @@
-//===- unittests/ADT/Interleave.cpp - interleave unit tests ----------------===//
+//===- unittests/ADT/Interleave.cpp - Interleave unit tests ----------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
