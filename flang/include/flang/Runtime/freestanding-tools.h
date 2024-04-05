e91d8a9ef3d45ee38a7e6e42bbe476db7b49371d
Slava Zakharin <szakharin@nvidia.com>
2024-04-05 15:10:04 -0700

    [NFC][flang][runtime] Moved freestanding-tools.h to use it in FortranDecimal. (#87827)
    I will add `toupper` implementation into it in the next PR.

--- flang/include/flang/Runtime/freestanding-tools.h
+++ flang/include/flang/Runtime/freestanding-tools.h
@@ -1,4 +1,4 @@
-//===-- runtime/freestanding-tools.h ----------------------------*- C++ -*-===//
+//===-- include/flang/Runtime/freestanding-tools.h --------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
