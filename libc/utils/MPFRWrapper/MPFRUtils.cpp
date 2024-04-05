4c8692d5bda7b4478c46377c295ead5aab235637
Vinayak Dev <vinayakdev.sci@gmail.com>
2024-04-05 10:01:50 +0530

    Add version guard for mpfr_roundeven

--- libc/utils/MPFRWrapper/MPFRUtils.cpp
+++ libc/utils/MPFRWrapper/MPFRUtils.cpp
@@ -352,9 +352,16 @@ public:
   }
 
   MPFRNumber roundeven() const {
+#if MPFR_VERSION_MAJOR > 4 ||                                                  \
+    (MPFR_VERSION_MAJOR == 4 && MPFR_VERSION_MINOR >= 0)
     MPFRNumber result(*this);
     mpfr_roundeven(result.value, value);
     return result;
+#else
+    MPFRNumber result(*this);
+    mpfr_rint(result.value, value, MPFR_RNDN);
+    return result;
+#endif
   }
 
   bool round_to_long(long &result) const {
