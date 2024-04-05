30e0fcda25f31ed8e2879b4bfe9fd3eaa4282268
Cyndy Ishida <cyndy_ishida@apple.com>
2024-04-05 13:58:24 -0700

    [InstallAPI] Capture & compare load commands that may differ per arch slice (#87674)
    * Capture reexported libraries, allowable clients, rpaths, shared cache
    eligibility.
    * Add support for select Xarch options.
    * Add diagnostics related to capturing these options.
    * Add support for verifying these attributes against what is encoded in
    the dylib.

--- clang/include/clang/InstallAPI/DylibVerifier.h
+++ clang/include/clang/InstallAPI/DylibVerifier.h
@@ -24,6 +24,9 @@ enum class VerificationMode {
   Pedantic,
 };
 
+using LibAttrs = llvm::StringMap<ArchitectureSet>;
+using ReexportedInterfaces = llvm::SmallVector<llvm::MachO::InterfaceFile, 8>;
+
 /// Service responsible to tracking state of verification across the
 /// lifetime of InstallAPI.
 /// As declarations are collected during AST traversal, they are
@@ -63,11 +66,12 @@ public:
 
   DylibVerifier() = default;
 
-  DylibVerifier(llvm::MachO::Records &&Dylib, DiagnosticsEngine *Diag,
-                VerificationMode Mode, bool Demangle, StringRef DSYMPath)
-      : Dylib(std::move(Dylib)), Mode(Mode), Demangle(Demangle),
-        DSYMPath(DSYMPath), Exports(std::make_unique<SymbolSet>()),
-        Ctx(VerifierContext{Diag}) {}
+  DylibVerifier(llvm::MachO::Records &&Dylib, ReexportedInterfaces &&Reexports,
+                DiagnosticsEngine *Diag, VerificationMode Mode, bool Demangle,
+                StringRef DSYMPath)
+      : Dylib(std::move(Dylib)), Reexports(std::move(Reexports)), Mode(Mode),
+        Demangle(Demangle), DSYMPath(DSYMPath),
+        Exports(std::make_unique<SymbolSet>()), Ctx(VerifierContext{Diag}) {}
 
   Result verify(GlobalRecord *R, const FrontendAttrs *FA);
   Result verify(ObjCInterfaceRecord *R, const FrontendAttrs *FA);
@@ -76,6 +80,14 @@ public:
 
   // Scan through dylib slices and report any remaining missing exports.
   Result verifyRemainingSymbols();
+
+  /// Compare and report the attributes represented as
+  /// load commands in the dylib to the attributes provided via options.
+  bool verifyBinaryAttrs(const ArrayRef<Target> ProvidedTargets,
+                         const BinaryAttrs &ProvidedBA,
+                         const LibAttrs &ProvidedReexports,
+                         const LibAttrs &ProvidedClients,
+                         const LibAttrs &ProvidedRPaths, const FileType &FT);
 
   /// Initialize target for verification.
   void setTarget(const Target &T);
@@ -104,6 +116,10 @@ private:
   // expected to result in a symbol mismatch.
   bool shouldIgnoreObsolete(const Record *R, SymbolContext &SymCtx,
                             const Record *DR);
+
+  /// Check if declaration is exported from a reexported library. These
+  /// symbols should be omitted from the text-api file.
+  bool shouldIgnoreReexport(const Record *R, SymbolContext &SymCtx) const;
 
   /// Compare the visibility declarations to the linkage of symbol found in
   /// dylib.
@@ -153,6 +169,9 @@ private:
 
   // Symbols in dylib.
   llvm::MachO::Records Dylib;
+
+  // Reexported interfaces apart of the library.
+  ReexportedInterfaces Reexports;
 
   // Controls what class of violations to report.
   VerificationMode Mode = VerificationMode::Invalid;
