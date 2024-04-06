8fa32e24a14bba8b20e39d31a8226550d65a106d
Job Hernandez <hj93@protonmail.com>
2024-04-06 14:49:01 -0700

    run the formatter

--- libc/utils/MPFRWrapper/MPFRUtils.cpp
+++ libc/utils/MPFRWrapper/MPFRUtils.cpp
@@ -14,8 +14,8 @@
 #include "src/__support/FPUtil/fpbits_str.h"
 #include "test/UnitTest/FPMatcher.h"
 
-#include "hdr/math_macros.h"
 #include "hdr/fenv_macros.h"
+#include "hdr/math_macros.h"
 #include <memory>
 #include <stdint.h>
 
