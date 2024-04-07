2f1a09450bca0bdfb7e8c1013a12fa279b8b76c2
Xiaoyang Liu <siujoeng.lau@gmail.com>
2024-04-07 15:44:52 -0700

    [libc++] P3029R1: Better mdspan's CTAD

--- libcxx/include/__mdspan/mdspan.h
+++ libcxx/include/__mdspan/mdspan.h
@@ -269,8 +269,8 @@ private:
 #  if _LIBCPP_STD_VER >= 26
 template <class _ElementType, class... _OtherIndexTypes>
   requires((is_convertible_v<_OtherIndexTypes, size_t> && ...) && (sizeof...(_OtherIndexTypes) > 0))
-explicit mdspan(_ElementType*, _OtherIndexTypes...)
-    -> mdspan<_ElementType, extents<size_t, __maybe_static_ext<_OtherIndexTypes>...>>;
+explicit mdspan(_ElementType*,
+                _OtherIndexTypes...) -> mdspan<_ElementType, extents<size_t, __maybe_static_ext<_OtherIndexTypes>...>>;
 #  else
 template <class _ElementType, class... _OtherIndexTypes>
   requires((is_convertible_v<_OtherIndexTypes, size_t> && ...) && (sizeof...(_OtherIndexTypes) > 0))
