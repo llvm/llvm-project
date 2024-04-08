51cadbcfaf85f5ef2b6b7d09c9243400635defe9
Andrzej Warzyński <andrzej.warzynski@arm.com>
2024-04-08 21:22:00 +0100

    [mlir][arith] Refine the verifier for arith.constant (#87999)
    Disallows initialization of scalable vectors with an attribute of
    arbitrary values, e.g.:
    ```mlir
      %c = arith.constant dense<[0, 1]> : vector<[2] x i32>
    ```
    
    Initialization using vector splats remains allowed (i.e. when all the
    init values are identical):
    ```mlir
      %c = arith.constant dense<[1, 1]> : vector<[2] x i32>
    ```
    
    Note: This is a re-upload of #86178

--- mlir/lib/Dialect/Arith/IR/ArithOps.cpp
+++ mlir/lib/Dialect/Arith/IR/ArithOps.cpp
@@ -213,6 +213,15 @@ LogicalResult arith::ConstantOp::verify() {
     return emitOpError(
         "value must be an integer, float, or elements attribute");
   }
+
+  // Note, we could relax this for vectors with 1 scalable dim, e.g.:
+  //  * arith.constant dense<[[3, 3], [1, 1]]> : vector<2 x [2] x i32>
+  // However, this would most likely require updating the lowerings to LLVM.
+  auto vecType = dyn_cast<VectorType>(type);
+  if (vecType && vecType.isScalable() && !isa<SplatElementsAttr>(getValue()))
+    return emitOpError(
+        "intializing scalable vectors with elements attribute is not supported"
+        " unless it's a vector splat");
   return success();
 }
 
