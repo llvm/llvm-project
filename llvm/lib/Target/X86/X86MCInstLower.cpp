c837970dd7e97a7ea8eac149ff19357d75c59083
Alexandre Ganea <37383324+aganea@users.noreply.github.com>
2024-04-08 20:02:19 -0400

    [Codegen][X86] Fix /HOTPATCH with clang-cl and inline asm (#87639)
    This fixes an edge case where functions starting with inline assembly
    would assert while trying to lower that inline asm instruction.
    
    After this PR, for now we always add a no-op (xchgw in this case) without
    considering the size of the next inline asm instruction. We might want
    to revisit this in the future.
    
    This fixes Unreal Engine 5.3.2 compilation with clang-cl and /HOTPATCH.
    
    Should close https://github.com/llvm/llvm-project/issues/56234
    
    (cherry picked from commit ec1af63dde58c735fe60d6f2aafdb10fa93f410d)

--- llvm/lib/Target/X86/X86MCInstLower.cpp
+++ llvm/lib/Target/X86/X86MCInstLower.cpp
@@ -959,8 +959,10 @@ void X86AsmPrinter::LowerPATCHABLE_OP(const MachineInstr &MI,
   SmallString<256> Code;
   unsigned MinSize = MI.getOperand(0).getImm();
 
-  if (NextMI != MI.getParent()->end()) {
+  if (NextMI != MI.getParent()->end() && !NextMI->isInlineAsm()) {
     // Lower the next MachineInstr to find its byte size.
+    // If the next instruction is inline assembly, we skip lowering it for now,
+    // and assume we should always generate NOPs.
     MCInst MCI;
     MCIL.Lower(&*NextMI, MCI);
 
