bc80aec6322eb5d58112cb7a2db523e37edb8b17
Nick Desaulniers <ndesaulniers@google.com>
2024-04-09 14:29:11 -0700

    damned linter

--- libc/src/setjmp/x86_64/setjmp.cpp
+++ libc/src/setjmp/x86_64/setjmp.cpp
@@ -17,8 +17,7 @@
 
 namespace LIBC_NAMESPACE_DECL {
 
-__attribute__((naked))
-LLVM_LIBC_FUNCTION(int, setjmp, (__jmp_buf * buf)) {
+__attribute__((naked)) LLVM_LIBC_FUNCTION(int, setjmp, (__jmp_buf * buf)) {
   asm("mov %%rbx, %c[rbx](%%rdi)\n\t"
       "mov %%rbp, %c[rbp](%%rdi)\n\t"
       "mov %%r12, %c[r12](%%rdi)\n\t"
