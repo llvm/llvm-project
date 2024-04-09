e0646d2098f567ca0cd5642c8694d3d8980134c7
Krystian Stasiowski <sdkrystian@gmail.com>
2024-04-09 09:03:25 -0400

    [FOLD] apply review changes

--- clang/test/CXX/expr/expr.prim/expr.prim.id/expr.prim.id.qual/p3.cpp
+++ clang/test/CXX/expr/expr.prim/expr.prim.id/expr.prim.id.qual/p3.cpp
@@ -1,5 +1,4 @@
 // RUN: %clang_cc1 -verify %s
-// RUN: %clang_cc1 -std=c++2c -verify %s
 
 template<typename T>
 struct A {
@@ -28,18 +27,3 @@ namespace N {
 
 template<typename T>
 void N::E<T>::f() { } // expected-warning {{a declarative nested name specifier cannot name an alias template}}
-
-#if __cplusplus > 202302L
-template<typename... Ts>
-struct A {
-  // FIXME: The nested-name-specifier in the following friend declarations are declarative,
-  // but we don't treat them as such (yet).
-  friend void Ts...[0]::f();
-  template<typename U>
-  friend void Ts...[0]::g();
-
-  friend struct Ts...[0]::B;
-  template<typename U>
-  friend struct Ts...[0]::C; // expected-warning{{is not supported; ignoring this friend declaration}}
-};
-#endif
