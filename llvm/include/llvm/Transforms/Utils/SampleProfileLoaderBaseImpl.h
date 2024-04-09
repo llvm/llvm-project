637addb2acdad14864552161e4f6a65ef6bf86f8
wlei <wlei@fb.com>
2024-04-08 22:17:09 -0700

    Update variable name and comment

--- llvm/include/llvm/Transforms/Utils/SampleProfileLoaderBaseImpl.h
+++ llvm/include/llvm/Transforms/Utils/SampleProfileLoaderBaseImpl.h
@@ -129,24 +129,23 @@ public:
 
   bool profileIsValid(const Function &F, const FunctionSamples &Samples) const {
     const auto *Desc = getDesc(F);
-    bool MayBeDerefined = !F.isDefinitionExact();
+    bool NonExactDefinition = !F.isDefinitionExact();
     // Always check the function attribute to determine checksum mismatch for
-    // functions whose definition is not exact(MayBeDerefined) even if their
-    // desc are available. This is because the desc is computed based on the
-    // original internal definition. When a derefined case occurs, the
-    // definition of function may be replaced by a differently optimized variant
-    // of the same source level function at link time and this could result in
-    // different checksums. So we should use the state from the new definition,
-    // which is saved in its attribute.
+    // functions whose definition is not exact even if their desc are available.
+    // This is because the desc is computed based on the original internal
+    // definition, however, the definition may be replaced by a different
+    // optimized variant of the same source level function at link time and this
+    // could result in different checksums. So we should use the state from the
+    // new definition, which is saved in its attribute.
     assert(
-        (LTOPhase != ThinOrFullLTOPhase::ThinLTOPostLink || MayBeDerefined ||
-         !Desc ||
+        (LTOPhase != ThinOrFullLTOPhase::ThinLTOPostLink ||
+         NonExactDefinition || !Desc ||
          profileIsHashMismatched(*Desc, Samples) ==
              F.hasFnAttribute("profile-checksum-mismatch")) &&
         "In post-link, profile checksum matching state doesn't match the exact "
         "definition function's 'profile-checksum-mismatch' attribute.");
     (void)LTOPhase;
-    if (MayBeDerefined || !Desc)
+    if (NonExactDefinition || !Desc)
       return !F.hasFnAttribute("profile-checksum-mismatch");
 
     return Desc && !profileIsHashMismatched(*Desc, Samples);
