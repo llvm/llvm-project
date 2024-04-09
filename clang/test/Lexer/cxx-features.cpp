1e13ed6d538a02e22285c52a411b104aa49e52d5
Erich Keane <ekeane@nvidia.com>
2024-04-09 06:59:31 -0700

    Update __cpp_concepts macro (#87998)
    After discussion with a few others, and seeing the state of our concepts
    support, I believe it is worth trying to see if we can update this for
    Clang19. The forcing function is that libstdc++'s `<expected>` header is
    guarded by this macro, so we need to update it to support that.

--- clang/test/Lexer/cxx-features.cpp
+++ clang/test/Lexer/cxx-features.cpp
@@ -85,7 +85,7 @@
 #error "wrong value for __cpp_char8_t"
 #endif
 
-#if check(concepts, 0, 0, 0, 0, 201907, 201907, 201907)
+#if check(concepts, 0, 0, 0, 0, 202002, 202002, 202002)
 #error "wrong value for __cpp_concepts"
 #endif
 
