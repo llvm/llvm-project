10fd94f58b73ddef384b979d4dc0edf4848e6fa9
David Pagan <dave.pagan@amd.com>
2024-04-10 13:09:17 -0700

    [OpenMP][CodeGen] Improved codegen for combined loop directives (#87278)
    IR for 'target teams loop' is now dependent on suitability of associated
    loop-nest.
    
    If a loop-nest:
    
    - does not contain a function call, or
    - the -fopenmp-assume-no-nested-parallelism has been specified,
    - or the call is to an OpenMP API AND
    - does not contain nested loop bind(parallel) directives
    
    then it can be emitted as 'target teams distribute parallel for', which
    is the current default. Otherwise, it is emitted as 'target teams
    distribute'.
    
    Added debug output indicating how 'target teams loop' was emitted. Flag
    is -mllvm -debug-only=target-teams-loop-codegen
    
    Added LIT tests explicitly verifying 'target teams loop' emitted as a
    parallel loop and a distribute loop.
    
    Updated other 'loop' related tests as needed to reflect change in IR.
    - These updates account for most of the changed files and
    additions/deletions.

--- clang/lib/Serialization/ASTWriterStmt.cpp
+++ clang/lib/Serialization/ASTWriterStmt.cpp
@@ -2821,6 +2821,7 @@ void ASTStmtWriter::VisitOMPTeamsGenericLoopDirective(
 void ASTStmtWriter::VisitOMPTargetTeamsGenericLoopDirective(
     OMPTargetTeamsGenericLoopDirective *D) {
   VisitOMPLoopDirective(D);
+  Record.writeBool(D->canBeParallelFor());
   Code = serialization::STMT_OMP_TARGET_TEAMS_GENERIC_LOOP_DIRECTIVE;
 }
 
