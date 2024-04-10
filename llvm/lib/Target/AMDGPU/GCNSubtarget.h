86842e1f724fba5abae50ce438553895e69b8141
Jun Wang <jwang86@yahoo.com>
2024-04-10 10:47:04 -0700

    [AMDGPU] New clang option for emitting a waitcnt instruction after each memory instruction (#79236)
    This patch introduces a new command-line option for clang, namely,
    amdgpu-precise-mem-op (or precise-memory in the backend). When this option is specified, a waitcnt
    instruction is generated after each memory load/store instruction. The
    counter values are always 0, but which counters are involved depends on
    the memory instruction.
    
    ---------
    
    Co-authored-by: Jun Wang <jun.wang7@amd.com>

--- llvm/lib/Target/AMDGPU/GCNSubtarget.h
+++ llvm/lib/Target/AMDGPU/GCNSubtarget.h
@@ -87,6 +87,7 @@ protected:
   bool EnableTgSplit = false;
   bool EnableCuMode = false;
   bool TrapHandler = false;
+  bool EnablePreciseMemory = false;
 
   // Used as options.
   bool EnableLoadStoreOpt = false;
@@ -598,6 +599,8 @@ public:
   bool isCuModeEnabled() const {
     return EnableCuMode;
   }
+
+  bool isPreciseMemoryEnabled() const { return EnablePreciseMemory; }
 
   bool hasFlatAddressSpace() const {
     return FlatAddressSpace;
