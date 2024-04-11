ce16ee0eec3dadf7d7e3b617ce6c3fd461e690ee
Matthias Braun <matze@braunis.de>
2024-04-11 16:06:12 -0700

    GlobalOpt: Handle threadlocal.address intrinsic

--- llvm/lib/Transforms/Utils/GlobalStatus.cpp
+++ llvm/lib/Transforms/Utils/GlobalStatus.cpp
@@ -172,9 +172,20 @@ static bool analyzeGlobalAux(const Value *V, GlobalStatus &GS,
           return true;
         GS.StoredType = GlobalStatus::Stored;
       } else if (const auto *CB = dyn_cast<CallBase>(I)) {
-        if (!CB->isCallee(&U))
-          return true;
-        GS.IsLoaded = true;
+        bool KnownIntrinsic = false;
+        if (const IntrinsicInst *II = dyn_cast<IntrinsicInst>(I)) {
+          if (II->getIntrinsicID() == Intrinsic::threadlocal_address &&
+              II->getArgOperand(0) == V) {
+            if (analyzeGlobalAux(I, GS, VisitedUsers))
+              return true;
+            KnownIntrinsic = true;
+          }
+        }
+        if (!KnownIntrinsic) {
+          if (!CB->isCallee(&U))
+            return true;
+          GS.IsLoaded = true;
+        }
       } else {
         return true; // Any other non-load instruction might take address!
       }
