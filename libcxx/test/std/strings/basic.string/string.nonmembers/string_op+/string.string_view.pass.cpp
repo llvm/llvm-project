af56ab902866a6971fad6034b483ad55e02ccdef
Hristo Hristov <hghristov.rmm@gmail.com>
2024-04-11 21:01:34 +0300

    Implemented tests

--- libcxx/test/std/strings/basic.string/string.nonmembers/string_op+/string.string_view.pass.cpp
+++ libcxx/test/std/strings/basic.string/string.nonmembers/string_op+/string.string_view.pass.cpp
@@ -30,6 +30,7 @@
 //               basic_string<charT, traits, Allocator>&& rhs);                                    // Since C++26
 
 #include <cassert>
+#include <concepts>
 #include <string>
 #include <utility>
 
@@ -37,6 +38,7 @@
 #include "constexpr_char_traits.h"
 #include "make_string.h"
 #include "min_allocator.h"
+#include "test_allocator.h"
 #include "test_macros.h"
 
 #define CS(S) MAKE_CSTRING(CharT, S)
@@ -44,25 +46,114 @@
 #define SV(S) std::basic_string_view<CharT, TraitsT>(MAKE_CSTRING(CharT, S), MKSTR_LEN(CharT, S))
 
 template <typename CharT, typename TraitsT, typename AllocT>
-constexpr void test() {
+constexpr void test(const CharT* x, const CharT* y, const CharT* expected) {
   AllocT allocator;
-  std::basic_string<CharT, TraitsT, AllocT> st{ST("Hello", allocator)};
-  std::basic_string_view<CharT, TraitsT> sv{SV("World")};
 
-  assert(st + sv == ST("HelloWorld", allocator));
-  assert(st + sv != ST("Hello World", allocator));
+  // string& + string_view
+  {
+    std::basic_string<CharT, TraitsT, AllocT> st{x, allocator};
+    std::basic_string_view<CharT, TraitsT> sv{y};
+
+    std::same_as<std::basic_string<CharT, TraitsT, AllocT>> decltype(auto) result = st + sv;
+    assert(result == expected);
+    assert(result.get_allocator() == allocator);
+    LIBCPP_ASSERT(is_string_asan_correct(st + sv));
+  }
+  // const string& + string_view
+  {
+    const std::basic_string<CharT, TraitsT, AllocT> st{x, allocator};
+    std::basic_string_view<CharT, TraitsT> sv{y};
+
+    std::same_as<std::basic_string<CharT, TraitsT, AllocT>> decltype(auto) result = st + sv;
+    assert(result == expected);
+    assert(result.get_allocator() == allocator);
+    LIBCPP_ASSERT(is_string_asan_correct(st + sv));
+  }
+  // string&& + string_view
+  {
+    std::basic_string<CharT, TraitsT, AllocT> st{x, allocator};
+    std::basic_string_view<CharT, TraitsT> sv{y};
+
+    std::same_as<std::basic_string<CharT, TraitsT, AllocT>> decltype(auto) result = std::move(st) + sv;
+    assert(result == expected);
+    assert(result.get_allocator() == allocator);
+    LIBCPP_ASSERT(is_string_asan_correct(st + sv));
+  }
+  // string_view + string&
+  {
+    std::basic_string_view<CharT, TraitsT> sv{x};
+    std::basic_string<CharT, TraitsT, AllocT> st{y, allocator};
+
+    std::same_as<std::basic_string<CharT, TraitsT, AllocT>> decltype(auto) result = sv + st;
+    assert(result == expected);
+    assert(result.get_allocator() == allocator);
+    LIBCPP_ASSERT(is_string_asan_correct(sv + st));
+  }
+  // string_view + const string&
+  {
+    std::basic_string_view<CharT, TraitsT> sv{x};
+    const std::basic_string<CharT, TraitsT, AllocT> st{y, allocator};
+
+    std::same_as<std::basic_string<CharT, TraitsT, AllocT>> decltype(auto) result = sv + st;
+    assert(result == expected);
+    assert(result.get_allocator() == allocator);
+    LIBCPP_ASSERT(is_string_asan_correct(sv + st));
+  }
+  // string_view + string&&
+  {
+    std::basic_string_view<CharT, TraitsT> sv{x};
+    std::basic_string<CharT, TraitsT, AllocT> st{y, allocator};
+
+    std::same_as<std::basic_string<CharT, TraitsT, AllocT>> decltype(auto) result = sv + std::move(st);
+    assert(result == expected);
+    assert(result.get_allocator() == allocator);
+    LIBCPP_ASSERT(is_string_asan_correct(sv + st));
+  }
+}
+
+template <typename CharT, typename TraitsT = std::char_traits<CharT>, typename AllocT = std::allocator<CharT>>
+constexpr void test() {
+  // Concatinate with an empty string
+  test<CharT, TraitsT, AllocT>(CS(""), CS(""), CS(""));
+  test<CharT, TraitsT, AllocT>(CS(""), CS("short"), CS("short"));
+  test<CharT, TraitsT, AllocT>(CS(""), CS("not so short"), CS("not so short"));
+  test<CharT, TraitsT, AllocT>(CS(""), CS("this is a much longer string"), CS("this is a much longer string"));
+
+  test<CharT, TraitsT, AllocT>(CS(""), CS(""), CS(""));
+  test<CharT, TraitsT, AllocT>(CS("short"), CS(""), CS("short"));
+  test<CharT, TraitsT, AllocT>(CS("not so short"), CS(""), CS("not so short"));
+  test<CharT, TraitsT, AllocT>(CS("this is a much longer string"), CS(""), CS("this is a much longer string"));
+
+  // Non empty
+  test<CharT, TraitsT, AllocT>(CS("B"), CS("D"), CS("BD"));
+  test<CharT, TraitsT, AllocT>(CS("zmt94"), CS("+hkt82"), CS("zmt94+hkt82"));
+  test<CharT, TraitsT, AllocT>(CS("not so short"), CS("+is not bad"), CS("not so short+is not bad"));
+  test<CharT, TraitsT, AllocT>(
+      CS("this is a much longer string"),
+      CS("+which is so much better"),
+      CS("this is a much longer string+which is so much better"));
 }
 
 constexpr bool test() {
+  test<char, std::char_traits<char>>();
   test<char, std::char_traits<char>, min_allocator<char>>();
-  test<char, constexpr_char_traits<char>, min_allocator<char>>();
   test<char, std::char_traits<char>, safe_allocator<char>>();
+  // test<char, std::char_traits<char>, test_allocator<char>>();
+
+  test<char, constexpr_char_traits<char>>();
+  test<char, constexpr_char_traits<char>, min_allocator<char>>();
   test<char, constexpr_char_traits<char>, safe_allocator<char>>();
+  // test<char, constexpr_char_traits<char>, test_allocator<char>>();
 #ifndef TEST_HAS_NO_WIDE_CHARACTERS
-  test<wchar_t, std::char_traits<wchar_t>, min_allocator<wchar_t>>();
-  test<wchar_t, constexpr_char_traits<wchar_t>, min_allocator<wchar_t>>();
+  test<wchar_t, std::char_traits<wchar_t>>();
+  // test<wchar_t, std::char_traits<wchar_t>, min_allocator<wchar_t>>();
   test<wchar_t, std::char_traits<wchar_t>, safe_allocator<wchar_t>>();
+  // test<wchar_t, std::char_traits<wchar_t>, test_allocator<wchar_t>>();
+
+  test<wchar_t, constexpr_char_traits<wchar_t>>();
+  // test<wchar_t, constexpr_char_traits<wchar_t>, min_allocator<wchar_t>>();
   test<wchar_t, constexpr_char_traits<wchar_t>, safe_allocator<wchar_t>>();
+  // test<wchar_t, constexpr_char_traits<wchar_t>, test_allocator<wchar_t>>();
 #endif
 
   return true;
