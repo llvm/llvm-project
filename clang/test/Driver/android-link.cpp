acaf36c06b8cb35d05a2193d71aa17f738f25cd5
Kalesh Singh <kaleshsingh@google.com>
2024-04-11 11:34:32 -0700

    Add max-page-size test for x86_64/i386

--- clang/test/Driver/android-link.cpp
+++ clang/test/Driver/android-link.cpp
@@ -1,7 +1,7 @@
-// Check that we add relevant linker flags for Android ARM/AArch64.
+// Check that we add relevant linker flags for Android ARM/AArch64/i386/x86_64.
 
 // RUN: %clang -### -target arm-linux-androideabi %s 2>&1 | \
-// RUN:   FileCheck --check-prefix=MAX-PAGE-SIZE %s
+// RUN:   FileCheck --check-prefix=MAX-PAGE-SIZE-4KB %s
 
 // RUN: %clang -target aarch64-none-linux-android \
 // RUN:   -### -v %s 2> %t
@@ -17,10 +17,20 @@
 //
 // RUN: %clang -target aarch64-none-linux-android \
 // RUN:   -### -v %s 2> %t
-// RUN: FileCheck -check-prefix=MAX-PAGE-SIZE-AARCH64 < %t %s
+// RUN: FileCheck -check-prefix=MAX-PAGE-SIZE-16KB < %t %s
+//
+// RUN: %clang -### -target i386-none-linux-android %s 2>&1 | \
+// RUN:   FileCheck --check-prefix=NO-MAX-PAGE-SIZE-16KB %s
+//
+// RUN: %clang -### -target x86_64-none-linux-gnu %s 2>&1 | \
+// RUN:   FileCheck --check-prefix=NO-MAX-PAGE-SIZE-16KB %s
+//
+// RUN: %clang -### -target x86_64-none-linux-android %s 2>&1 | \
+// RUN:   FileCheck --check-prefix=MAX-PAGE-SIZE-16KB %s
 //
 // GENERIC-ARM: --fix-cortex-a53-843419
 // CORTEX-A53: --fix-cortex-a53-843419
 // CORTEX-A57-NOT: --fix-cortex-a53-843419
-// MAX-PAGE-SIZE: "-z" "max-page-size=4096"
-// MAX-PAGE-SIZE-AARCH64: "-z" "max-page-size=16384"
+// MAX-PAGE-SIZE-4KB: "-z" "max-page-size=4096"
+// MAX-PAGE-SIZE-16KB: "-z" "max-page-size=16384"
+// NO-MAX-PAGE-SIZE-16KB-NOT: "-z" "max-page-size=16384"
