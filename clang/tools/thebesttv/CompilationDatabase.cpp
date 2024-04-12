38b10f5d9e5c31b7e2837d20f62808315f67f289
thebesttv <taojuntian@outlook.com>
2024-04-12 14:50:17 +0800

    Add function for dumping compilation database

--- clang/tools/thebesttv/CompilationDatabase.cpp
+++ clang/tools/thebesttv/CompilationDatabase.cpp
@@ -20,3 +20,16 @@ getCompilationDatabase(fs::path buildPath) {
                 cb->getAllCompileCommands().size(), cb->getAllFiles().size());
     return cb;
 }
+
+void dumpCompileCommand(const CompileCommand &cmd) {
+    logger.info("CompileCommand for: {}", cmd.Filename);
+    logger.warn("  dir: {}", cmd.Directory);
+    logger.warn("  cmd: {}", fmt::join(cmd.CommandLine, " "));
+}
+
+void dumpCompilationDatabase(const CompilationDatabase &cb) {
+    auto commands = cb.getAllCompileCommands();
+    for (auto cmd : commands) {
+        dumpCompileCommand(cmd);
+    }
+}
--- clang/tools/thebesttv/CompilationDatabase.h
+++ clang/tools/thebesttv/CompilationDatabase.h
@@ -3,3 +3,6 @@
 #include "utils.h"
 
 std::unique_ptr<CompilationDatabase> getCompilationDatabase(fs::path buildPath);
+
+void dumpCompileCommand(const CompileCommand &cmd);
+void dumpCompilationDatabase(const CompilationDatabase &cb);
