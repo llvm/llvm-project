a67dd1a14e5510b7aba9e75582c271d59065bbf8
chandan singh <chandankds@gmail.com>
2024-04-12 11:43:06 +0530

    Update omp__kmpc_fork_call_if.c
    fixed formatting

--- openmp/runtime/test/misc_bugs/omp__kmpc_fork_call_if.c
+++ openmp/runtime/test/misc_bugs/omp__kmpc_fork_call_if.c
@@ -3,22 +3,20 @@
 #include <stdio.h>
 #include <omp.h>
 
-
 // Microtask function for parallel region
-void microtask(int* global_tid, int* bound_tid) {
-// CHECK: PASS
-    if (omp_in_parallel()) {
-        printf("FAIL\n");
-    } else {
-        printf("PASS\n");
-    }
+void microtask(int *global_tid, int *bound_tid) {
+  // CHECK: PASS
+  if (omp_in_parallel()) {
+    printf("FAIL\n");
+  } else {
+    printf("PASS\n");
+  }
 }
 
 int main() {
-    // Condition for parallelization (false in this case)
-    int cond = 0;
-
-    // Call __kmpc_fork_call_if
-    __kmpc_fork_call_if(NULL, 0, microtask, cond, NULL);
-    return 0;
+  // Condition for parallelization (false in this case)
+  int cond = 0;
+  // Call __kmpc_fork_call_if
+  __kmpc_fork_call_if(NULL, 0, microtask, cond, NULL);
+  return 0;
 }
