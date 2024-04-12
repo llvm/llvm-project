6e40a2f9b0ebc55a3cb97bf763ff8b6d5b8db030
Erich Keane <ekeane@nvidia.com>
2024-04-12 14:13:31 -0700

    [OpenACC] Implement 'if' clause for Compute Constructs (#88411)
    Like with the 'default' clause, this is being applied to only Compute
    Constructs for now. The 'if' clause takes a condition expression which
    is used as a runtime value.
    
    This is not a particularly complex semantic implementation, as there
    isn't much to this clause, other than its interactions with 'self',
      which will be managed in the patch to implement that.

--- clang/lib/Serialization/ASTReader.cpp
+++ clang/lib/Serialization/ASTReader.cpp
@@ -11787,6 +11787,12 @@ OpenACCClause *ASTRecordReader::readOpenACCClause() {
     return OpenACCDefaultClause::Create(getContext(), DCK, BeginLoc, LParenLoc,
                                         EndLoc);
   }
+  case OpenACCClauseKind::If: {
+    SourceLocation LParenLoc = readSourceLocation();
+    Expr *CondExpr = readSubExpr();
+    return OpenACCIfClause::Create(getContext(), BeginLoc, LParenLoc, CondExpr,
+                                   EndLoc);
+  }
   case OpenACCClauseKind::Finalize:
   case OpenACCClauseKind::IfPresent:
   case OpenACCClauseKind::Seq:
@@ -11795,7 +11801,6 @@ OpenACCClause *ASTRecordReader::readOpenACCClause() {
   case OpenACCClauseKind::Worker:
   case OpenACCClauseKind::Vector:
   case OpenACCClauseKind::NoHost:
-  case OpenACCClauseKind::If:
   case OpenACCClauseKind::Self:
   case OpenACCClauseKind::Copy:
   case OpenACCClauseKind::UseDevice:
