8c5b0c18ba6f77ac5101db5645b36f41259c3645
Krzysztof Parzyszek <Krzysztof.Parzyszek@amd.com>
2024-04-12 14:52:24 -0500

    [LLVM][OpenMP] Add "nowait" clause as valid for "workshare" (#88426)
    Add the "nowait" clause to the list of allowed clauses for the "workshare"
    directive. This will make it consistent with other directives (which are
    shared between C/C++ and Fortran).
    
    The parser will still reject "nowait" on "!$omp workshare", so this has no
    effect on accepting/rejecting Fortran source code.

--- flang/test/Semantics/OpenMP/clause-validity01.f90
+++ flang/test/Semantics/OpenMP/clause-validity01.f90
@@ -342,6 +342,9 @@ use omp_lib
   a = 1.0
   !ERROR: COPYPRIVATE clause is not allowed on the END WORKSHARE directive
   !$omp end workshare nowait copyprivate(a)
+  !$omp workshare nowait
+  !ERROR: NOWAIT clause is not allowed on the WORKSHARE directive, use it on OMP END WORKSHARE directive
+  !$omp end workshare
   !$omp end parallel
 
 ! 2.8.1 simd-clause -> safelen-clause |
