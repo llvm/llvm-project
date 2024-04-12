8fd00e2a3224c04028791ba14b99199a4a93be93
Louis Dionne <ldionne.2@gmail.com>
2024-04-12 13:58:47 -0400

    Make tests portable

--- libcxx/test/libcxx/utilities/is_valid_range.pass.cpp
+++ libcxx/test/libcxx/utilities/is_valid_range.pass.cpp
@@ -11,42 +11,49 @@
 
 #include "test_macros.h"
 
-template <class T>
+template <class T, class TQualified>
 TEST_CONSTEXPR_CXX14 void check_type() {
   {
-    T i = 0;
-    T j = 0;
-    assert(std::__is_valid_range(&i, &i));
-    assert(std::__is_valid_range(&i, &i + 1));
+    // We need to ensure that the addresses of i and j are ordered as &i < &j for
+    // the test below to work portably, so we define them in a struct.
+    struct {
+      T i = 0;
+      T j = 0;
+    } storage;
+    assert(std::__is_valid_range(static_cast<TQualified*>(&storage.i), static_cast<TQualified*>(&storage.i)));
+    assert(std::__is_valid_range(static_cast<TQualified*>(&storage.i), static_cast<TQualified*>(&storage.i + 1)));
 
-    assert(!std::__is_valid_range(&i, &j));
-    assert(!std::__is_valid_range(&i + 1, &i));
+    assert(!std::__is_valid_range(static_cast<TQualified*>(&storage.j), static_cast<TQualified*>(&storage.i)));
+    assert(!std::__is_valid_range(static_cast<TQualified*>(&storage.i + 1), static_cast<TQualified*>(&storage.i)));
+
+    // We detect this as being a valid range even though it is not really valid.
+    assert(std::__is_valid_range(static_cast<TQualified*>(&storage.i), static_cast<TQualified*>(&storage.j)));
   }
 
   {
     T arr[3] = {1, 2, 3};
-    assert(std::__is_valid_range(&arr[0], &arr[0]));
-    assert(std::__is_valid_range(&arr[0], &arr[1]));
-    assert(std::__is_valid_range(&arr[0], &arr[2]));
+    assert(std::__is_valid_range(static_cast<TQualified*>(&arr[0]), static_cast<TQualified*>(&arr[0])));
+    assert(std::__is_valid_range(static_cast<TQualified*>(&arr[0]), static_cast<TQualified*>(&arr[1])));
+    assert(std::__is_valid_range(static_cast<TQualified*>(&arr[0]), static_cast<TQualified*>(&arr[2])));
 
-    assert(!std::__is_valid_range(&arr[1], &arr[0]));
-    assert(!std::__is_valid_range(&arr[2], &arr[0]));
+    assert(!std::__is_valid_range(static_cast<TQualified*>(&arr[1]), static_cast<TQualified*>(&arr[0])));
+    assert(!std::__is_valid_range(static_cast<TQualified*>(&arr[2]), static_cast<TQualified*>(&arr[0])));
   }
 
 #if TEST_STD_VER >= 20
   {
     T* arr = new int[4]{1, 2, 3, 4};
-    assert(std::__is_valid_range(arr, arr + 4));
+    assert(std::__is_valid_range(static_cast<TQualified*>(arr), static_cast<TQualified*>(arr + 4)));
     delete[] arr;
   }
 #endif
 }
 
 TEST_CONSTEXPR_CXX14 bool test() {
-  check_type<int>();
-  check_type<const int>();
-  check_type<volatile int>();
-  check_type<const volatile int>();
+  check_type<int, int>();
+  check_type<int, int const>();
+  check_type<int, int volatile>();
+  check_type<int, int const volatile>();
 
   return true;
 }
