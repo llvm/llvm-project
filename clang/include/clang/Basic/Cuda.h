fdf4c68d3eff5b1cea0710484bca1482c40923c7
Vlad Serebrennikov <serebrennikov.vladislav@gmail.com>
2024-04-14 17:51:15 +0400

    [clang] Fix name conflict with `sys/mac.h` on AIX (#88644)
    Fixes clang-ppc64-aix bot failure after #88559
    (0a6f6df5b0c3d0f2a42f013bf5cafb9b5020dcac)
    https://lab.llvm.org/buildbot/#/builders/214/builds/11887
    
    ---------
    
    Co-authored-by: Joseph Huber <huberjn@outlook.com>

--- clang/include/clang/Basic/Cuda.h
+++ clang/include/clang/Basic/Cuda.h
@@ -50,6 +50,10 @@ const char *CudaVersionToString(CudaVersion V);
 // Input is "Major.Minor"
 CudaVersion CudaStringToVersion(const llvm::Twine &S);
 
+// We have a name conflict with sys/mac.h on AIX
+#ifdef SM_32
+#undef SM_32
+#endif
 enum class CudaArch {
   UNUSED,
   UNKNOWN,
