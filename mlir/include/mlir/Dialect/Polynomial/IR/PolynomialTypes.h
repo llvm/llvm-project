6453d78954d59aa3b09b8ddcfca5e040feb96019
Jeremy Kun <jkun@google.com>
2024-04-15 09:12:00 -0700

    Add a polynomial dialect shell, attributes, and types (#72081)
    RFC:
    https://discourse.llvm.org/t/rfc-a-poly-dialect-for-polynomial-arithmetic/73891
    
    This PR implements the minimal work needed to represent the polynomial
    type such that it can be tested with `lit`.
    
    In this PR:
    
    - Dialect shell
    - `Polynomial` data structure needed for folding
    - Polynomial attributes (`PolynomialAttr` and `RingAttr` which store a polynomial)
    - `polynomial.polynomial` type
    - Basic lit tests
    
    ---------
    
    Co-authored-by: Jeremy Kun <j2kun@users.noreply.github.com>

---
+++ mlir/include/mlir/Dialect/Polynomial/IR/PolynomialTypes.h
@@ -0,0 +1,17 @@
+//===- PolynomialTypes.h - Types for the Polynomial dialect -----*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+#ifndef MLIR_INCLUDE_MLIR_DIALECT_POLYNOMIAL_IR_POLYNOMIALTYPES_H_
+#define MLIR_INCLUDE_MLIR_DIALECT_POLYNOMIAL_IR_POLYNOMIALTYPES_H_
+
+#include "mlir/Dialect/Polynomial/IR/PolynomialAttributes.h"
+#include "mlir/Dialect/Polynomial/IR/PolynomialDialect.h"
+
+#define GET_TYPEDEF_CLASSES
+#include "mlir/Dialect/Polynomial/IR/PolynomialTypes.h.inc"
+
+#endif // MLIR_INCLUDE_MLIR_DIALECT_POLYNOMIAL_IR_POLYNOMIALTYPES_H_
