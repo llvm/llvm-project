56c2b4f70735a6ef3b80cb8461a88ea51f2d02d7
mahtohappy <Happy.Kumar@Windriver.com>
2024-04-16 17:48:45 +0530

    [Clang][Sema] placement new initializes typedef array with correct size (#83124)
    When in-place new-ing a local variable of an array of trivial type, the
    generated code calls 'memset' with the correct size of the array,
    earlier it was generating size (squared of the typedef array + size).
    
    The cause: `typedef TYPE TArray[8]; TArray x;` The type of declarator is
    Tarray[8] and in `SemaExprCXX.cpp::BuildCXXNew` we check if it's of
    typedef and of constant size then we get the original type and it works
    fine for non-dependent cases.
    But in case of template we do `TreeTransform.h:TransformCXXNEWExpr` and
    there we again check the allocated type which is TArray[8] and it stays
    that way, so ArraySize=(Tarray[8] type, alloc Tarray[8*type]) so the
    squared size allocation.
    
    ArraySize gets calculated earlier in `TreeTransform.h` so that
    `if(!ArraySize)` condition was failing.
    fix: I changed that condition to `if(ArraySize)`. 
    
    
    Fixes #41441

---
+++ clang/test/SemaCXX/instantiate-new-placement-size.cpp
@@ -0,0 +1,20 @@
+// RUN: %clang -S -fno-discard-value-names -emit-llvm -o - %s | FileCheck %s
+// Issue no: 41441
+#include <new>
+
+// CHECK: call void @llvm.memset.p0.i64(ptr align 1 %x, i8 0, i64 8, i1 false)
+// CHECK: call void @llvm.memset.p0.i64(ptr align 16 %x, i8 0, i64 32, i1 false)
+template <typename TYPE>
+void f()
+{
+    typedef TYPE TArray[8];
+
+    TArray x;
+    new(&x) TArray();
+}
+
+int main()
+{
+    f<char>();
+    f<int>();
+}
