b73df40a5e4be7bc4cb0f593dad701a6f8af99f1
Louis Dionne <ldionne.2@gmail.com>
2024-04-16 14:34:02 -0400

    [libc++][pstl] Promote CPU backends to top-level backends
    This patch removes the two-level backend dispatching mechanism we
    had in the PSTL. Instead of selecting both a PSTL backend and a PSTL
    CPU backend, we now only select a top-level PSTL backend. This greatly
    simplifies the PSTL configuration layer.
    
    While this patch technically removes some flexibility from the PSTL
    configuration mechanism because CPU backends are not considered
    separately, it opens the door to a much more powerful configuration
    mechanism based on chained backends in a follow-up patch.
    
    This is a step towards overhauling the PSTL dispatching mechanism.

--- libcxx/include/__algorithm/pstl_rotate_copy.h
+++ libcxx/include/__algorithm/pstl_rotate_copy.h
@@ -9,10 +9,10 @@
 #ifndef _LIBCPP___ALGORITHM_PSTL_ROTATE_COPY_H
 #define _LIBCPP___ALGORITHM_PSTL_ROTATE_COPY_H
 
-#include <__algorithm/pstl_backend.h>
 #include <__algorithm/pstl_copy.h>
 #include <__algorithm/pstl_frontend_dispatch.h>
 #include <__iterator/cpp17_iterator_concepts.h>
+#include <__pstl/configuration.h>
 #include <__type_traits/is_execution_policy.h>
 #include <optional>
 
