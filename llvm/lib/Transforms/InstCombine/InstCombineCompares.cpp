3920bcf44b5f44d23a98fa044181c63b0ca9943b
Vlad Mishel <43666597+vmishelcs@users.noreply.github.com>
2024-04-15 22:05:29 -0700

    [InstCombine] Implement `fcmp (fadd x, 0.0), y` => `fcmp x, y` optimization (#88476)
    This PR addresses issue #88168. It implements an optimization for
    the case of
    
    ```
    define i1 @fcmp_fadd_zero_ugt(float %x, float %y) {
      %add = fadd float %x, 0.000000e+00
      %cmp = fcmp ugt float %add, %y
      ret i1 %cmp
    }
    ```
    `=>`
    ```
    define i1 @fcmp_fadd_zero_ugt(float %x, float %y) {
      %cmp = fcmp ugt float %x, %y
      ret i1 %cmp
    }
    ```
    and all other types of `fcmp` instructions (`uge`, `ogt`, etc).
    
    Proofs: 
    `fadd x, 0.0` https://alive2.llvm.org/ce/z/7FzNnM
    `fsub x, 0.0` https://alive2.llvm.org/ce/z/puUxLK

--- llvm/lib/Transforms/InstCombine/InstCombineCompares.cpp
+++ llvm/lib/Transforms/InstCombine/InstCombineCompares.cpp
@@ -8097,6 +8097,14 @@ Instruction *InstCombinerImpl::visitFCmpInst(FCmpInst &I) {
         return new FCmpInst(I.getSwappedPredicate(), X, NegC, "", &I);
   }
 
+  // fcmp (fadd X, 0.0), Y --> fcmp X, Y
+  if (match(Op0, m_FAdd(m_Value(X), m_AnyZeroFP())))
+    return new FCmpInst(Pred, X, Op1, "", &I);
+
+  // fcmp X, (fadd Y, 0.0) --> fcmp X, Y
+  if (match(Op1, m_FAdd(m_Value(Y), m_AnyZeroFP())))
+    return new FCmpInst(Pred, Op0, Y, "", &I);
+
   if (match(Op0, m_FPExt(m_Value(X)))) {
     // fcmp (fpext X), (fpext Y) -> fcmp X, Y
     if (match(Op1, m_FPExt(m_Value(Y))) && X->getType() == Y->getType())
