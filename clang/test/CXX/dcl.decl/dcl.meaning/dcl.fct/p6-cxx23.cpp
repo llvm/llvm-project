586649aef02281367b6f26278051febe653f8036
Krystian Stasiowski <sdkrystian@gmail.com>
2024-04-17 10:00:19 -0400

    [FOLD] fix test

--- clang/test/CXX/dcl.decl/dcl.meaning/dcl.fct/p6-cxx23.cpp
+++ clang/test/CXX/dcl.decl/dcl.meaning/dcl.fct/p6-cxx23.cpp
@@ -3,6 +3,5 @@
 auto x0 = requires (this int) { true; }; // expected-error {{a requires expression cannot have an explicit object parameter}}
 auto x1 = requires (int, this int) { true; }; // expected-error {{a requires expression cannot have an explicit object parameter}}
 
-template<this auto>
-void f(); // expected-error {{expected template parameter}}
-          // expected-error@-1 {{no function template matches function template specialization 'f'}}
+template<this auto> // expected-error {{expected template parameter}}
+void f(); // expected-error {{no function template matches function template specialization 'f'}}
