8f935fbcd7ee8a572e0a5242d1b6ad5c5a70975d
Krzysztof Parzyszek <Krzysztof.Parzyszek@amd.com>
2024-04-17 11:46:48 -0500

    Finish the renaming

--- llvm/unittests/Frontend/OpenMPCompositionTest.cpp
+++ llvm/unittests/Frontend/OpenMPCompositionTest.cpp
@@ -1,4 +1,4 @@
-//===- llvm/unittests/Frontend/OpenMPComposeTest.cpp ----------------------===//
+//===- llvm/unittests/Frontend/OpenMPCompositionTest.cpp ------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
