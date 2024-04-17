a315828cff7ed2303c879046a62df5fa589d25e3
Nick Desaulniers <nickdesaulniers@users.noreply.github.com>
2024-04-17 10:34:23 -0700

    [libc] set cmake dependencies for condattr test (#89103)
    The entrypoints are not yet exposed on non-x86. Express this dependency
    to
    unbreak post submit.
    
    Fixes #88987

--- libc/src/pthread/pthread_condattr_destroy.cpp
+++ libc/src/pthread/pthread_condattr_destroy.cpp
@@ -14,7 +14,8 @@
 
 namespace LIBC_NAMESPACE {
 
-LLVM_LIBC_FUNCTION(int, pthread_condattr_destroy, (pthread_condattr_t * attr)) {
+LLVM_LIBC_FUNCTION(int, pthread_condattr_destroy,
+                   (pthread_condattr_t * attr [[gnu::unused]])) {
   // Initializing a pthread_condattr_t acquires no resources, so this is a
   // no-op.
   return 0;
