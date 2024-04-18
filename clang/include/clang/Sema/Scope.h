a22574fd00ea15ac45579d1e406a395fa36ae2fb
Qizhi Hu <836744285@qq.com>
2024-04-18 08:52:25 +0800

    [Clang][Sema] set declaration invalid earlier to prevent crash in calculating record layout (#87173)
    Try to fix https://github.com/llvm/llvm-project/issues/75221
    This crash caused by calculating record layout which contains a field
    declaration with dependent type. Make it invalid before it is a complete
    definition to prevent this crash. Define a new scope type to record this
    type alias and set the record declaration invalid when it is defined in
    a type alias template.
    
    Co-authored-by: huqizhi <836744285@qq.com>

--- clang/include/clang/Sema/Scope.h
+++ clang/include/clang/Sema/Scope.h
@@ -156,6 +156,9 @@ public:
     /// This is the scope of an OpenACC Compute Construct, which restricts
     /// jumping into/out of it.
     OpenACCComputeConstructScope = 0x10000000,
+
+    /// This is a scope of type alias declaration.
+    TypeAliasScope = 0x20000000,
   };
 
 private:
@@ -579,6 +582,9 @@ public:
   /// Determine whether this scope is a controlling scope in a
   /// if/switch/while/for statement.
   bool isControlScope() const { return getFlags() & Scope::ControlScope; }
+
+  /// Determine whether this scope is a type alias scope.
+  bool isTypeAliasScope() const { return getFlags() & Scope::TypeAliasScope; }
 
   /// Returns if rhs has a higher scope depth than this.
   ///
