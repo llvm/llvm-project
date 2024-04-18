bc54b15ca93422537ee9efcfdf4da11e0cfa71f8
Rose <gfunni234@gmail.com>
2024-04-18 13:55:53 -0400

    [compiler-rt]: Update BlocksRuntime to current open-source version
    This updates the BlocksRuntime used in compiler-rt to match the current implementation.

--- compiler-rt/lib/BlocksRuntime/Block.h
+++ compiler-rt/lib/BlocksRuntime/Block.h
@@ -1,56 +1,70 @@
-/*
- * Block.h
- *
- * Copyright 2008-2010 Apple, Inc. Permission is hereby granted, free of charge,
- * to any person obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without restriction,
- * including without limitation the rights to use, copy, modify, merge, publish,
- * distribute, sublicense, and/or sell copies of the Software, and to permit
- * persons to whom the Software is furnished to do so, subject to the following
- * conditions:
- * 
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- *
- */
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2015 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
 
-#ifndef _BLOCK_H_
-#define _BLOCK_H_
+
+#ifndef _Block_H_
+#define _Block_H_
+
+#if defined(_WIN32)
+#    if defined(BlocksRuntime_STATIC)
+#        define BLOCK_ABI
+#    else
+#        if defined(BlocksRuntime_EXPORTS)
+#            define BLOCK_ABI __declspec(dllexport)
+#        else
+#            define BLOCK_ABI __declspec(dllimport)
+#        endif
+#    endif
+#else
+#    define BLOCK_ABI __attribute__((__visibility__("default")))
+#endif
 
 #if !defined(BLOCK_EXPORT)
 #   if defined(__cplusplus)
-#       define BLOCK_EXPORT extern "C"
+#       define BLOCK_EXPORT extern "C" BLOCK_ABI
 #   else
-#       define BLOCK_EXPORT extern
+#       define BLOCK_EXPORT extern BLOCK_ABI
 #   endif
 #endif
 
-#if defined(__cplusplus)
+#if __cplusplus
 extern "C" {
 #endif
 
-/* Create a heap based copy of a Block or simply add a reference to an existing one.
- * This must be paired with Block_release to recover memory, even when running
- * under Objective-C Garbage Collection.
- */
+// Create a heap based copy of a Block or simply add a reference to an existing one.
+// This must be paired with Block_release to recover memory, even when running
+// under Objective-C Garbage Collection.
 BLOCK_EXPORT void *_Block_copy(const void *aBlock);
 
-/* Lose the reference, and if heap based and last reference, recover the memory. */
+// Lose the reference, and if heap based and last reference, recover the memory
 BLOCK_EXPORT void _Block_release(const void *aBlock);
 
-#if defined(__cplusplus)
+// Used by the compiler. Do not call this function yourself.
+BLOCK_EXPORT void _Block_object_assign(void *, const void *, const int);
+
+// Used by the compiler. Do not call this function yourself.
+BLOCK_EXPORT void _Block_object_dispose(const void *, const int);
+
+// Used by the compiler. Do not use these variables yourself.
+#if defined(_WIN32)
+extern void * _NSConcreteGlobalBlock[32];
+extern void * _NSConcreteStackBlock[32];
+#else
+BLOCK_EXPORT void * _NSConcreteGlobalBlock[32];
+BLOCK_EXPORT void * _NSConcreteStackBlock[32];
+#endif
+
+#if __cplusplus
 }
 #endif
 
-/* Type correct macros. */
+// Type correct macros
 
 #define Block_copy(...) ((__typeof(__VA_ARGS__))_Block_copy((const void *)(__VA_ARGS__)))
 #define Block_release(...) _Block_release((const void *)(__VA_ARGS__))
