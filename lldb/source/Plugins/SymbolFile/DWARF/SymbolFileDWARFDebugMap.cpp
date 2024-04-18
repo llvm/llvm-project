a13fc0de7e5412d29add4bcb57ab55614e92108a
Jim Ingham <jingham@apple.com>
2024-04-18 16:14:10 -0700

    Bow

--- lldb/source/Plugins/SymbolFile/DWARF/SymbolFileDWARFDebugMap.cpp
+++ lldb/source/Plugins/SymbolFile/DWARF/SymbolFileDWARFDebugMap.cpp
@@ -849,15 +849,15 @@ SymbolFileDWARFDebugMap::ResolveSymbolContext(const Address &exe_so_addr,
             Address oso_so_addr;
             if (oso_module->ResolveFileAddress(oso_file_addr, oso_so_addr)) {
               if (SymbolFile *sym_file = oso_module->GetSymbolFile()) {
-                resolved_flags |= sym_file->ResolveSymbolContext(oso_so_addr, 
-                    resolve_scope, sc);
+                resolved_flags |= sym_file->ResolveSymbolContext(
+                    oso_so_addr, resolve_scope, sc);
               } else {
                 ObjectFile *obj_file = GetObjectFile();
                 LLDB_LOG(GetLog(DWARFLog::DebugMap),
-                         "Failed to get symfile for OSO: {0} in module: {1}", 
-                         oso_module->GetFileSpec(), 
-                         obj_file ? obj_file->GetFileSpec() : 
-                             FileSpec("unknown"));
+                         "Failed to get symfile for OSO: {0} in module: {1}",
+                         oso_module->GetFileSpec(),
+                         obj_file ? obj_file->GetFileSpec()
+                                  : FileSpec("unknown"));
               }
             }
           }
