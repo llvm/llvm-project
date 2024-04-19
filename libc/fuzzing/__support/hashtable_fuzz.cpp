ea094120be6387893bc53f4ce173b1697e9bf22d
Schrodinger ZHU Yifan <yifanzhu@rochester.edu>
2024-04-19 17:56:50 -0400

    add missing license headers

--- libc/fuzzing/__support/hashtable_fuzz.cpp
+++ libc/fuzzing/__support/hashtable_fuzz.cpp
@@ -1,3 +1,14 @@
+//===-- hashtable_fuzz.cpp ------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+///
+/// Fuzzing test for llvm-libc hashtable implementations.
+///
+//===----------------------------------------------------------------------===//
 #include "src/__support/CPP/new.h"
 #include "src/__support/CPP/optional.h"
 #include "src/__support/HashTable/table.h"
