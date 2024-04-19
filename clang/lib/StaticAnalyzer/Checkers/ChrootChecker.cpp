22cadf0cde369cd87edd1e8727089e8179007d15
NagyDonat <donat.nagy@ericsson.com>
2024-04-19 14:22:51 +0200

    [analyzer] Use explicit call description mode (easy cases) (#88879)
    This commit explicitly specifies the matching mode (C library function,
    any non-method function, or C++ method) for the `CallDescription`s
    constructed in various checkers where this transition was easy and
    straightforward.
    
    This change won't cause major functional changes, but isn't NFC because
    it ensures that e.g. call descriptions for a non-method function won't
    accidentally match a method that has the same name.
    
    Separate commits will perform (or have already performed) this change in
    other checkers. My goal is to ensure that the call description mode is
    always explicitly specified and eliminate (or strongly restrict) the
    vague "may be either a method or a simple function" mode that's the
    current default.

--- clang/lib/StaticAnalyzer/Checkers/ChrootChecker.cpp
+++ clang/lib/StaticAnalyzer/Checkers/ChrootChecker.cpp
@@ -43,7 +43,8 @@ class ChrootChecker : public Checker<eval::Call, check::PreCall> {
   // This bug refers to possibly break out of a chroot() jail.
   const BugType BT_BreakJail{this, "Break out of jail"};
 
-  const CallDescription Chroot{{"chroot"}, 1}, Chdir{{"chdir"}, 1};
+  const CallDescription Chroot{CDM::CLibrary, {"chroot"}, 1},
+      Chdir{CDM::CLibrary, {"chdir"}, 1};
 
 public:
   ChrootChecker() {}
