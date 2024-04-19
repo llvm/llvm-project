0603a9afe4da1aae989b03213da4a8b899d12cc7
Fabio D'Urso <fdurso@google.com>
2024-04-19 21:21:30 +0200

    Removed unnecessary assignment to the default value

--- compiler-rt/lib/scudo/standalone/allocator_config.h
+++ compiler-rt/lib/scudo/standalone/allocator_config.h
@@ -150,7 +150,6 @@ struct FuchsiaConfig {
 #else
     static const uptr RegionSizeLog = 30U;
     static const uptr GroupSizeLog = 21U;
-    static const bool EnableContiguousRegions = true;
 #endif
     typedef u32 CompactPtrT;
     static const bool EnableRandomOffset = true;
