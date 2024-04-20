131393f6d720bcdbd067b77b36d7257b04098212
Shubham Sandeep Rastogi <srastogi22@apple.com>
2024-04-19 22:37:37 -0700

    Add REQUIRES to 32-bit relocation tests in MCCAS

--- llvm/test/CAS/test-reloc-mccas-32bit-arm.s
+++ llvm/test/CAS/test-reloc-mccas-32bit-arm.s
@@ -1,5 +1,5 @@
 # This test tests to make sure mccas can handle scattered relocations properly on 32 bit arm
-
+# REQUIRES: arm-registered-target
 # RUN: rm -rf %t && mkdir -p %t
 # RUN: llvm-mc --cas=%t/cas --cas-backend --mccas-verify -triple=armv7-apple-darwin10  -filetype=obj -o %t/reloc.o %s
 
--- llvm/test/CAS/test-reloc-mccas-32bit.s
+++ llvm/test/CAS/test-reloc-mccas-32bit.s
@@ -1,5 +1,5 @@
 # This test tests to make sure mccas can handle scattered relocations properly on 32 bit x86
-
+# REQUIRES: x86-registered-target
 # RUN: rm -rf %t && mkdir -p %t
 # RUN: llvm-mc --cas=%t/cas --cas-backend --mccas-verify  -triple=i386-apple-macosx10.4 -filetype=obj -o %t/reloc.o %s
         movl    y+4, %ecx
