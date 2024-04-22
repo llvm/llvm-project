8ebba44565b9f01e04009fbdcbc5b3fabe642e2e
Ivan Kosarev <ivan.kosarev@amd.com>
2024-04-22 11:39:41 +0100

    [AMDGPU][MC][GFX13] Support VOPC instructions.
    All the encodings are identical to those of MI400. Compared to
    GFX12, the only difference is that MI400 and GFX13 additionally
    support 64-bit literals.
    
    Change-Id: Icaee2e932dc40ba7092275a7603995fb54b94d03

--- llvm/test/MC/AMDGPU/gfx1210_asm_valu_lit64.s
+++ llvm/test/MC/AMDGPU/gfx1210_asm_valu_lit64.s
@@ -1,4 +1,5 @@
 // RUN: llvm-mc -arch=amdgcn -show-encoding -mcpu=gfx1210 %s | FileCheck --check-prefix=GFX1210 %s
+// RUN: llvm-mc -arch=amdgcn -show-encoding -mcpu=gfx1300 %s | FileCheck --check-prefix=GFX1210 %s
 
 v_ceil_f64 v[254:255], 0x10abcdef12345678
 // GFX1210: v_ceil_f64_e32 v[254:255], 0x10abcdef12345678 ; encoding: [0xfe,0x30,0xfc,0x7f,0x78,0x56,0x34,0x12,0xef,0xcd,0xab,0x10]
