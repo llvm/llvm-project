3214b729e75b6e0486c727fc38c2b201cd7f059c
Théo Degioanni <theo.degioanni.llvm.deluge062@simplelogin.fr>
2024-04-22 22:03:56 +0200

    [nfc][llvm] Fix a typo in MathExtras.h testing (#89653)
    I made a small typo when writing a test for MathExtras.h, sorry!

--- llvm/unittests/Support/MathExtrasTest.cpp
+++ llvm/unittests/Support/MathExtrasTest.cpp
@@ -41,9 +41,9 @@ TEST(MathExtras, onesMask) {
 TEST(MathExtras, isIntN) {
   EXPECT_TRUE(isIntN(16, 32767));
   EXPECT_FALSE(isIntN(16, 32768));
-  EXPECT_TRUE(isUIntN(0, 0));
-  EXPECT_FALSE(isUIntN(0, 1));
-  EXPECT_FALSE(isUIntN(0, -1));
+  EXPECT_TRUE(isIntN(0, 0));
+  EXPECT_FALSE(isIntN(0, 1));
+  EXPECT_FALSE(isIntN(0, -1));
 }
 
 TEST(MathExtras, isUIntN) {
