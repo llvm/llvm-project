334d02196efe007d5ebda16d411210ddec6fb920
Roland McGrath <mcgrathr@google.com>
2024-04-22 15:18:02 -0700

    [libc] Clean up alternate test framework support (#89659)
    This replaces the old macros LIBC_COPT_TEST_USE_FUCHSIA and
    LIBC_COPT_TEST_USE_PIGWEED with LIBC_COPT_TEST_ZXTEST and
    LIBC_COPT_TEST_GTEST, respectively.  These are really not about
    whether the code is in the Fuchsia build or in the Pigweed build,
    but just about what test framework is being used.  The gtest
    framework can be used in many contexts, and the zxtest framework
    is not always what's used in the Fuchsia build.
    
    The test/UnitTest/Test.h wrapper header now provides the macro
    LIBC_TEST_HAS_MATCHERS() for use in `#if` conditionals on use of
    gmock-style matchers, replacing `#if` conditionals that test the
    framework selection macros directly.

--- libc/test/UnitTest/ZxTest.h
+++ libc/test/UnitTest/ZxTest.h
@@ -1,13 +1,13 @@
-//===-- Header for setting up the Fuchsia tests -----------------*- C++ -*-===//
+//===-- Header for using Fuchsia's zxtest framework ------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
-//===----------------------------------------------------------------------===//
+//===---------------------------------------------------------------------===//
 
-#ifndef LLVM_LIBC_UTILS_UNITTEST_FUCHSIATEST_H
-#define LLVM_LIBC_UTILS_UNITTEST_FUCHSIATEST_H
+#ifndef LLVM_LIBC_UTILS_UNITTEST_ZXTEST_H
+#define LLVM_LIBC_UTILS_UNITTEST_ZXTEST_H
 
 #include <zxtest/zxtest.h>
 
@@ -29,7 +29,12 @@
 #endif
 
 namespace LIBC_NAMESPACE::testing {
-using Test = ::zxtest::Test;
-}
 
-#endif // LLVM_LIBC_UTILS_UNITTEST_FUCHSIATEST_H
+using Test = ::zxtest::Test;
+
+} // namespace LIBC_NAMESPACE::testing
+
+// zxtest does not have gmock-style matchers.
+#define LIBC_TEST_HAS_MATCHERS() (0)
+
+#endif // LLVM_LIBC_UTILS_UNITTEST_ZXTEST_H
