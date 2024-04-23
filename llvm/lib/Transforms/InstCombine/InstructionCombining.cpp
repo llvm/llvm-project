bc2f6c09102da318f07889f5f926242d2eadf1f2
Kai Nacke <kai.peter.nacke@ibm.com>
2024-04-23 10:39:03 -0400

    [SystemZ][z/OS] Make z/OS personality function known (#89679)
    This change adds the z/OS personality function to the list of known EH
    personality functions. It enables removing of the EH data/labels if the
    personality function is not invoked.

--- llvm/lib/Transforms/InstCombine/InstructionCombining.cpp
+++ llvm/lib/Transforms/InstCombine/InstructionCombining.cpp
@@ -3972,6 +3972,7 @@ static bool isCatchAll(EHPersonality Personality, Constant *TypeInfo) {
   case EHPersonality::CoreCLR:
   case EHPersonality::Wasm_CXX:
   case EHPersonality::XL_CXX:
+  case EHPersonality::ZOS_CXX:
     return TypeInfo->isNullValue();
   }
   llvm_unreachable("invalid enum");
