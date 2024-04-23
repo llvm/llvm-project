7d32ab52d78aac088d03cc138477326d9f7e3398
Abid Qadeer <haqadeer@amd.com>
2024-04-23 19:11:43 +0100

    [flang] Remove hardcoded bits from AddDebugInfo. (#89231)
    This PR adds following options to the AddDebugInfo pass.
    
    1. IsOptimized flag.
    2. Level of debug info to generate.
    3. Name of the source file
    
    This enables us to remove the hard coded values from the code. It also
    allows us to test the pass with different options. The tests have been
    modified to take advantage of that.
    
    The calling convention flag and producer name have also been improved.

--- flang/include/flang/Optimizer/Transforms/Passes.h
+++ flang/include/flang/Optimizer/Transforms/Passes.h
@@ -47,6 +47,7 @@ namespace fir {
 #define GEN_PASS_DECL_ALGEBRAICSIMPLIFICATION
 #define GEN_PASS_DECL_POLYMORPHICOPCONVERSION
 #define GEN_PASS_DECL_OPENACCDATAOPERANDCONVERSION
+#define GEN_PASS_DECL_ADDDEBUGINFO
 #include "flang/Optimizer/Transforms/Passes.h.inc"
 
 std::unique_ptr<mlir::Pass> createAffineDemotionPass();
@@ -64,7 +65,8 @@ std::unique_ptr<mlir::Pass> createMemoryAllocationPass();
 std::unique_ptr<mlir::Pass> createStackArraysPass();
 std::unique_ptr<mlir::Pass> createAliasTagsPass();
 std::unique_ptr<mlir::Pass> createSimplifyIntrinsicsPass();
-std::unique_ptr<mlir::Pass> createAddDebugInfoPass();
+std::unique_ptr<mlir::Pass>
+createAddDebugInfoPass(fir::AddDebugInfoOptions options = {});
 std::unique_ptr<mlir::Pass> createLoopVersioningPass();
 
 std::unique_ptr<mlir::Pass>
