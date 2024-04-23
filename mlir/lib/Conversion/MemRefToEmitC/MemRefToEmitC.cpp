86ba077a67a38dd634fe9dd09ad5f07e5bf25386
Matthias Gehre <matthias.gehre@amd.com>
2024-04-23 09:28:04 +0200

    typo

--- mlir/lib/Conversion/MemRefToEmitC/MemRefToEmitC.cpp
+++ mlir/lib/Conversion/MemRefToEmitC/MemRefToEmitC.cpp
@@ -81,7 +81,7 @@ struct ConvertGlobal final : public OpConversionPattern<memref::GlobalOp> {
           op.getLoc(),
           "only public and private visibility is currently supported");
     }
-    // We are explicit in specifier the linkage because the default linkage
+    // We are explicit in specifing the linkage because the default linkage
     // for constants is different in C and C++.
     bool staticSpecifier = visibility == SymbolTable::Visibility::Private;
     bool externSpecifier = !staticSpecifier;
