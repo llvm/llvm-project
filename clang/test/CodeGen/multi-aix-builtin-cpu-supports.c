3ecb1153b4f380e697ed5584a81a81fad9d91e3a
zhijian <zhijian@ca.ibm.com>
2024-04-23 16:33:05 -0400

    address lei's comment

--- clang/test/CodeGen/multi-aix-builtin-cpu-supports.c
+++ clang/test/CodeGen/multi-aix-builtin-cpu-supports.c
@@ -2,10 +2,12 @@
 
 int main() { 
   int ret = 0; 
-  ret += __builtin_cpu_supports("vsx");
-  ret += __builtin_cpu_supports("htm");
-  ret += __builtin_cpu_supports("cellbe");
-  ret += __builtin_cpu_supports("power4");
+  ret += __builtin_cpu_supports("vsx");     // Test reading `vsx` information from the system variable `_system_configuration`.
+  ret += __builtin_cpu_supports("htm");     // Test getting `htm` information from the function call `getsystemcfg`
+  ret += __builtin_cpu_supports("cellbe");  // The test always returns false for the feature 'cellbe.
+  ret += __builtin_cpu_supports("power4");  // The test always returns false for the feature `power4`.
+  ret += __builtin_cpu_supports("fpu");     // The test always returns true for the feature `fpu`.
+  ret += __builtin_cpu_supports("mma");     // Test getting `mma` information from the function call `getsystemcfg`.
   return ret;
 }
 
@@ -37,7 +39,16 @@ int main() {
 // CHECK-NEXT:   %add4 = add nsw i32 %7, 1
 // CHECK-NEXT:   store i32 %add4, ptr %ret, align 4
 // CHECK-NEXT:   %8 = load i32, ptr %ret, align 4
-// CHECK-NEXT:   ret i32 %8
+// CHECK-NEXT:   %add5 = add nsw i32 %8, 1
+// CHECK-NEXT:   store i32 %add5, ptr %ret, align 4
+// CHECK-NEXT:   %9 = call i64 @getsystemcfg(i32 62)
+// CHECK-NEXT:   %10 = icmp ugt i64 %9, 0
+// CHECK-NEXT:   %conv6 = zext i1 %10 to i32
+// CHECK-NEXT:   %11 = load i32, ptr %ret, align 4
+// CHECK-NEXT:   %add7 = add nsw i32 %11, %conv6
+// CHECK-NEXT:   store i32 %add7, ptr %ret, align 4
+// CHECK-NEXT:   %12 = load i32, ptr %ret, align 4
+// CHECK-NEXT:   ret i32 %12
 // CHECK-NEXT: }
 // CHECK-EMPTY: 
 // CHECK-NEXT: declare i64 @getsystemcfg(i32)
