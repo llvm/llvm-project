e1406cf692ad24abb6d592535c415d0f9f33a54a
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2024-04-23 18:20:52 -0700

    [CIR][CIRGen] Atomics: initial compare and exchange support

--- clang/test/CIR/CodeGen/atomic-xchg-field.c
+++ clang/test/CIR/CodeGen/atomic-xchg-field.c
@@ -23,13 +23,13 @@ void field_access(wPtr item) {
 }
 
 // CHECK: ![[W:.*]] = !cir.struct<struct "w"
-// CHECK: cir.func @field_access
+// CHECK-LABEL: @field_access
 // CHECK-NEXT: %[[WADDR:.*]] = cir.alloca !cir.ptr<![[W]]>, {{.*}} {alignment = 8 : i64}
 // CHECK: %[[FIELD:.*]] = cir.load %[[WADDR]]
 // CHECK: %[[MEMBER:.*]] = cir.get_member %[[FIELD]][1] {name = "ref"}
 // CHECK: cir.atomic.xchg(%[[MEMBER]] : !cir.ptr<!cir.ptr<!void>>, {{.*}} : !u64i, seq_cst)
 
-// LLVM: define void @field_access
+// LLVM-LABEL: @field_access
 // LLVM: = alloca ptr, i64 1, align 8
 // LLVM: %[[VAL_ADDR:.*]] = alloca ptr, i64 1, align 8
 // LLVM: %[[RES_ADDR:.*]] = alloca ptr, i64 1, align 8
@@ -41,3 +41,28 @@ void field_access(wPtr item) {
 // LLVM: store i64 %[[RES]], ptr %4, align 8
 // LLVM: load ptr, ptr %[[RES_ADDR]], align 8
 // LLVM: ret void
+
+void structAtomicExchange(unsigned referenceCount, wPtr item) {
+  __atomic_compare_exchange_n((&item->_base.a), (&referenceCount), (referenceCount + 1), 1 , 5, 5);
+}
+
+// CHECK-LABEL: @structAtomicExchange
+// CHECK:  cir.atomic.cmp_xchg({{.*}} : !cir.ptr<!u32i>, {{.*}} : <!u32i>, {{.*}} : !cir.ptr<!u32i>, success = seq_cst, failure = seq_cst) weak : !cir.bool
+
+// LLVM-LABEL: @structAtomicExchange
+// LLVM:   load i32
+// LLVM:   add i32
+// LLVM:   store i32
+// LLVM:   %[[EXP:.*]] = load i32
+// LLVM:   %[[DES:.*]] = load i32
+// LLVM:   %[[RES:.*]] = cmpxchg weak ptr %9, i32 %[[EXP]], i32 %[[DES]] seq_cst seq_cst
+// LLVM:   %[[OLD:.*]] = extractvalue { i32, i1 } %[[RES]], 0
+// LLVM:   %[[CMP:.*]] = extractvalue { i32, i1 } %[[RES]], 1
+// LLVM:   br i1 %[[CMP]], label %[[CONTINUE:.*]], label %[[STORE_OLD:.*]],
+// LLVM: [[CONTINUE]]:
+// LLVM:   zext i1 %[[CMP]] to i8
+// LLVM:   ret void
+
+// LLVM: [[STORE_OLD]]:
+// LLVM:   store i32 %[[OLD]], ptr
+// LLVM:   br label %[[CONTINUE]]
