9d18805ac5395158a2a80fd2efd5edac46c70d28
Vitaly Buka <vitalybuka@google.com>
2024-04-23 23:52:42 -0700

    [libcxx] Remove empty ~__no_destroy
    Primary motivation is that after #84651 msan will
    complain if fields accessed after ~__no_destroy.
    
    Previously msan assumed that __obj_ will have own
    destructor, were we will poison the field, but it never
    happened before.
    
    After #84651 msan will complain on any field access
    after `~__no_destroy`.
    
    As is Msan does validate fields destruction order for
    classes with trivial destructor.
    
    Additionally empty destructor will register __cxa_atexit with -O0.
    https://gcc.godbolt.org/z/hce587b65
    
    We can not remove the destructor with union where
    _Tp can have non-trivial destructor.
    
    But we can remove destructor if we use in-place
    new instead of union.
    https://gcc.godbolt.org/z/Yqxx57eEd - empty even with -O0.
    
    Reviewers: #reviewers-libcxx
    
    Pull Request: https://github.com/llvm/llvm-project/pull/89882

---
+++ libcxx/test/libcxx/utilities/no_destroy.pass.cpp
@@ -0,0 +1,28 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include <__utility/no_destroy.h>
+#include <cassert>
+
+#include "test_macros.h"
+
+struct DestroyLast {
+  ~DestroyLast() { assert(*ptr == 5); }
+
+  int* ptr;
+} last;
+
+static std::__no_destroy<int> nd_int(5);
+
+void test() { last.ptr = &nd_int.__get(); }
+
+int main(int, char**) {
+  test();
+
+  return 0;
+}
