8c5f1d0f92d77bffec88759c19133a0bac130f32
yronglin <yronglin777@gmail.com>
2024-04-24 23:36:10 +0800

    [Clang] Implement P2748R5 "Disallow Binding a Returned Glvalue to a Temporary"
    Signed-off-by: yronglin <yronglin777@gmail.com>

---
+++ clang/test/CXX/stmt.stmt/stmt.return/p6.cpp
@@ -0,0 +1,18 @@
+// RUN: %clang_cc1 -std=c++26 -fsyntax-only -verify %s
+
+auto&& f1() {
+  return 42; // expected-error{{returning reference to local temporary object}}
+}
+const double& f2() {
+  static int x = 42;
+  return x; // expected-error{{returning reference to local temporary object}}
+}
+auto&& id(auto&& r) {
+  return static_cast<decltype(r)&&>(r);
+}
+auto&& f3() {
+  return id(42);        // OK, but probably a bug
+}
+
+static_assert(__is_convertible(int, const int &));
+static_assert(__is_nothrow_convertible(int, const int &));
