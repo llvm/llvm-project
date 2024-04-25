a9002d10a33b9052b0f6921d4b4789b25c1d3298
Timm Bäder <tbaeder@redhat.com>
2024-04-25 07:49:04 +0200

    [clang][Interp][NFC] Move collectBaseOffset() to Context
    We will need this outside of ByteCodeExprGen later.

--- clang/lib/AST/Interp/Context.h
+++ clang/lib/AST/Interp/Context.h
@@ -104,6 +104,9 @@ public:
   /// Returns the program. This is only needed for unittests.
   Program &getProgram() const { return *P.get(); }
 
+  unsigned collectBaseOffset(const RecordDecl *BaseDecl,
+                             const RecordDecl *DerivedDecl) const;
+
 private:
   /// Runs a function.
   bool Run(State &Parent, const Function *Func, APValue &Result);
