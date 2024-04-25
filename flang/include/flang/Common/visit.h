9232d3390b0a347739e304a96ac53c033285babc
Alexander Shaposhnikov <ashaposhnikov@google.com>
2024-04-25 21:13:33 +0000

    Reapply "[Flang] Add fallthrough annotations in visit.h (#90014)"
    Use [[fallthrough]] directly (similarly to
    https://reviews.llvm.org/D131346).
    
    This recommits eb05a2e89dccec734625aa336b553197b75f2340.

--- flang/include/flang/Common/visit.h
+++ flang/include/flang/Common/visit.h
@@ -40,11 +40,17 @@ inline RT_API_ATTRS RESULT Log2VisitHelper(
       return visitor(std::get<(LOW + N)>(std::forward<VARIANT>(u))...); \
     }
       VISIT_CASE_N(1)
+      [[fallthrough]];
       VISIT_CASE_N(2)
+      [[fallthrough]];
       VISIT_CASE_N(3)
+      [[fallthrough]];
       VISIT_CASE_N(4)
+      [[fallthrough]];
       VISIT_CASE_N(5)
+      [[fallthrough]];
       VISIT_CASE_N(6)
+      [[fallthrough]];
       VISIT_CASE_N(7)
 #undef VISIT_CASE_N
     }
