94c2093fdace575dea4a5741a73f254f2c094629
Mehdi Amini <joker.eph@gmail.com>
2024-04-25 19:20:19 -0700

    Revert 4 last "[mlir-lsp]" commits: pre-merge bot is broken
    This reverts commits:
    - f3f6f22dfcced21116710a477c78e0739c942139.
    - 37e13d4924841bd84edb8c67c667d6d2a6c2bc63.
    - ba1b52e6e764ad637822f0b63d144eca6e5d5627.
    - 84bc21f9101730af611a0175d89ae2056d5f41a7.

--- mlir/include/mlir/Tools/lsp-server-support/Transport.h
+++ mlir/include/mlir/Tools/lsp-server-support/Transport.h
@@ -147,15 +147,9 @@ public:
                     void (ThisT::*handler)(const Param &)) {
     notificationHandlers[method] = [method, handler,
                                     thisPtr](llvm::json::Value rawParams) {
-      llvm::Expected<Param> param =
-          parse<Param>(rawParams, method, "notification");
-      if (!param) {
-        return llvm::consumeError(
-            llvm::handleErrors(param.takeError(), [](const LSPError &lspError) {
-              Logger::error("JSON parsing error: {0}",
-                            lspError.message.c_str());
-            }));
-      }
+      llvm::Expected<Param> param = parse<Param>(rawParams, method, "request");
+      if (!param)
+        return llvm::consumeError(param.takeError());
       (thisPtr->*handler)(*param);
     };
   }
