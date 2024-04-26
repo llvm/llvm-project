e9da7cd8fc5441fbb85bee94a846ae875edd9d3f
erichkeane <ekeane@nvidia.com>
2024-04-26 06:17:05 -0700

    [NFC][OpenACC] Remove stale FIXME comment in a test
    A recent patch changed the array sections to work differently for
    OpenACC, including for the diagnostic, so the FIXME is now irrelevant.

--- clang/test/ParserOpenACC/parse-cache-construct.cpp
+++ clang/test/ParserOpenACC/parse-cache-construct.cpp
@@ -72,8 +72,6 @@ void use() {
     #pragma acc cache(Arrs.MemArr[3].array[1:4])
   }
   for (int i = 0; i < 10; ++i) {
-    // FIXME: Once we have a new array-section type to represent OpenACC as
-    // well, change this error message.
     // expected-error@+2{{OpenACC sub-array is not allowed here}}
     // expected-warning@+1{{OpenACC construct 'cache' not yet implemented, pragma ignored}}
     #pragma acc cache(Arrs.MemArr[3:4].array[1:4])
