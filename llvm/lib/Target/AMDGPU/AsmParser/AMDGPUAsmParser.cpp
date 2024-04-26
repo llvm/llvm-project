5c5410507360d90eed0417a488a75914d5ba4f9f
Stanislav Mekhanoshin <Stanislav.Mekhanoshin@amd.com>
2024-04-26 10:26:48 -0700

    MI-400: Use NamedIntOperand for byte_sel and scale_sel
    Change-Id: I4e9994e1a7817efb01582a39af8fa3ad8b11b2b3

--- llvm/lib/Target/AMDGPU/AsmParser/AMDGPUAsmParser.cpp
+++ llvm/lib/Target/AMDGPU/AsmParser/AMDGPUAsmParser.cpp
@@ -1995,12 +1995,6 @@ public:
   ParseStatus parseEndpgm(OperandVector &Operands);
 
   ParseStatus parseVOPD(OperandVector &Operands);
-
-  ParseStatus parseScaleSel(OperandVector &Operands);
-  AMDGPUOperand::Ptr defaultScaleSel() const;
-
-  ParseStatus parseByteSel(OperandVector &Operands);
-  AMDGPUOperand::Ptr defaultByteSel() const;
 };
 
 } // end anonymous namespace
@@ -10263,35 +10257,6 @@ bool AMDGPUOperand::isWaitVMVSrc() const {
 
 bool AMDGPUOperand::isWaitEXP() const {
   return isImmTy(ImmTyWaitEXP) && isUInt<3>(getImm());
-}
-
-//===----------------------------------------------------------------------===//
-// ScaleSel
-//===----------------------------------------------------------------------===//
-
-ParseStatus AMDGPUAsmParser::parseScaleSel(OperandVector &Operands) {
-  ParseStatus Res =
-      parseIntWithPrefix("scale_sel", Operands, AMDGPUOperand::ImmTyScaleSel);
-  return Res;
-}
-
-AMDGPUOperand::Ptr AMDGPUAsmParser::defaultScaleSel() const {
-  return AMDGPUOperand::CreateImm(this, 0, SMLoc(), AMDGPUOperand::ImmTyScaleSel);
-}
-
-//===----------------------------------------------------------------------===//
-// ByteSel
-//===----------------------------------------------------------------------===//
-
-ParseStatus AMDGPUAsmParser::parseByteSel(OperandVector &Operands) {
-  ParseStatus Res =
-      parseIntWithPrefix("byte_sel", Operands, AMDGPUOperand::ImmTyByteSel);
-  return Res;
-}
-
-AMDGPUOperand::Ptr AMDGPUAsmParser::defaultByteSel() const {
-  return AMDGPUOperand::CreateImm(this, 0, SMLoc(),
-                                  AMDGPUOperand::ImmTyByteSel);
 }
 
 //===----------------------------------------------------------------------===//
