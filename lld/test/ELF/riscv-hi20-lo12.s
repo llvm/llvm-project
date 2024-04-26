1750e2000f39605d754f8bf967d60019f8ec37d2
Craig Topper <craig.topper@sifive.com>
2024-04-26 10:19:22 -0700

    fixup! Fix mistakes in lld test updates.

--- lld/test/ELF/riscv-hi20-lo12.s
+++ lld/test/ELF/riscv-hi20-lo12.s
@@ -7,23 +7,23 @@
 # RUN: ld.lld %t.rv64.o --defsym foo=0 --defsym bar=42 -o %t.rv64
 # RUN: llvm-objdump -d %t.rv32 | FileCheck %s
 # RUN: llvm-objdump -d %t.rv64 | FileCheck %s
-# CHECK:      03705000     lui     a0, 0x0
-# CHECK-NEXT: 01305050     mv      a0, a0
-# CHECK-NEXT: 02320a50     sw      a0, 0x0(a0)
-# CHECK-NEXT: 0b705000     lui     a1, 0x0
-# CHECK-NEXT: 09385a52     addi    a1, a1, 0x2a
-# CHECK-NEXT: 023a5b52     sw      a1, 0x2a(a1)
+# CHECK:      00000537     lui     a0, 0x0
+# CHECK-NEXT: 00050513     mv      a0, a0
+# CHECK-NEXT: 00a52023     sw      a0, 0x0(a0)
+# CHECK-NEXT: 000005b7     lui     a1, 0x0
+# CHECK-NEXT: 02a58593     addi    a1, a1, 0x2a
+# CHECK-NEXT: 02b5a523     sw      a1, 0x2a(a1)
 
 # RUN: ld.lld %t.rv32.o --defsym foo=0x7ffff7ff --defsym bar=0x7ffff800 -o %t.rv32.limits
 # RUN: ld.lld %t.rv64.o --defsym foo=0x7ffff7ff --defsym bar=0xffffffff7ffff800 -o %t.rv64.limits
 # RUN: llvm-objdump -d %t.rv32.limits | FileCheck --check-prefix=LIMITS %s
 # RUN: llvm-objdump -d %t.rv64.limits | FileCheck --check-prefix=LIMITS %s
-# LIMITS:      73fff57f     lui     a0, 0x7ffff
-# LIMITS-NEXT: 71f5053f     addi    a0, a0, 0x7ff
-# LIMITS-NEXT: 7aa52f3e     sw      a0, 0x7ff(a0)
-# LIMITS-NEXT: 8b000570     lui     a1, 0x80000
-# LIMITS-NEXT: 89058530     addi    a1, a1, -0x800
-# LIMITS-NEXT: 82b5a030     sw      a1, -0x800(a1)
+# LIMITS:      7ffff537     lui     a0, 0x7ffff
+# LIMITS-NEXT: 7ff50513     addi    a0, a0, 0x7ff
+# LIMITS-NEXT: 7ea52fa3     sw      a0, 0x7ff(a0)
+# LIMITS-NEXT: 800005b7     lui     a1, 0x80000
+# LIMITS-NEXT: 80058593     addi    a1, a1, -0x800
+# LIMITS-NEXT: 80b5a023     sw      a1, -0x800(a1)
 
 # RUN: not ld.lld %t.rv64.o --defsym foo=0x7ffff800 --defsym bar=0xffffffff7ffff7ff -o /dev/null 2>&1 | FileCheck --check-prefix ERROR %s
 # ERROR: relocation R_RISCV_HI20 out of range: 524288 is not in [-524288, 524287]; references 'foo'
