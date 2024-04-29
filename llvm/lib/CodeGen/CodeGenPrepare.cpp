6350acdb134d2e494c029ef2f83ccbc1a1462ba3
Yingwei Zheng <dtcxzyw2333@gmail.com>
2024-04-29 15:51:49 +0800

    [CGP] Drop poison-generating flags after hoisting (#90382)
    See the following case:
    ```
    define i8 @src1(i8 %x) {
    entry:
      %cmp = icmp eq i8 %x, -1
      br i1 %cmp, label %exit, label %if.then
    
    if.then:
      %inc = add nuw nsw i8 %x, 1
      br label %exit
    
    exit:
      %retval = phi i8 [ %inc, %if.then ], [ -1, %entry ]
      ret i8 %retval
    }
    
    define i8 @tgt1(i8 %x) {
    entry:
      %inc = add nuw nsw i8 %x, 1
      %0 = icmp eq i8 %inc, 0
      br i1 %0, label %exit, label %if.then
    
    if.then:                                          ; preds = %entry
      br label %exit
    
    exit:                                             ; preds = %if.then, %entry
      %retval = phi i8 [ %inc, %if.then ], [ -1, %entry ]
      ret i8 %retval
    }
    ```
    `optimizeBranch` converts `icmp eq X, -1` into cmp to zero on RISC-V and
    hoists the add into the entry block. Poison-generating flags should be
    dropped as they don't still hold.
    
    Proof: https://alive2.llvm.org/ce/z/sP7mvK
    Fixes https://github.com/llvm/llvm-project/issues/90380
    
    (cherry picked from commit ab12bba0aad800c1805eca2ea937da958c1854c8)

--- llvm/lib/CodeGen/CodeGenPrepare.cpp
+++ llvm/lib/CodeGen/CodeGenPrepare.cpp
@@ -8154,6 +8154,7 @@ static bool optimizeBranch(BranchInst *Branch, const TargetLowering &TLI,
       IRBuilder<> Builder(Branch);
       if (UI->getParent() != Branch->getParent())
         UI->moveBefore(Branch);
+      UI->dropPoisonGeneratingFlags();
       Value *NewCmp = Builder.CreateCmp(ICmpInst::ICMP_EQ, UI,
                                         ConstantInt::get(UI->getType(), 0));
       LLVM_DEBUG(dbgs() << "Converting " << *Cmp << "\n");
@@ -8167,6 +8168,7 @@ static bool optimizeBranch(BranchInst *Branch, const TargetLowering &TLI,
       IRBuilder<> Builder(Branch);
       if (UI->getParent() != Branch->getParent())
         UI->moveBefore(Branch);
+      UI->dropPoisonGeneratingFlags();
       Value *NewCmp = Builder.CreateCmp(Cmp->getPredicate(), UI,
                                         ConstantInt::get(UI->getType(), 0));
       LLVM_DEBUG(dbgs() << "Converting " << *Cmp << "\n");
