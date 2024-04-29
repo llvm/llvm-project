b284c940a0dc072130f3ede1f2fcd0473e326445
Renato Golin <rengolin@systemcall.eu>
2024-04-29 15:42:35 +0100

    [MLIR][Linalg] Left over Linalg named ops from previous PR (#90405)
    Adding `erf` as unary and `powf` as binary.
    
    Same as `max(arg, 0.0)` for `ReLU`, `powf(arg, const)` can be either a
    generic (with broadcast) or a pair (`linalg.broadcast + linalg.powf`)
    and then lowered "correctly". Either way, the lower dialects need to
    know what kind of broadcast anyway, so no materialization of the
    constant tensors should remain.
    
    I want to flush the easy ones before we start working on type cast &
    softmax.

--- mlir/lib/Dialect/Linalg/IR/LinalgOps.cpp
+++ mlir/lib/Dialect/Linalg/IR/LinalgOps.cpp
@@ -411,6 +411,8 @@ public:
       return builder.create<arith::MulFOp>(arg.getLoc(), arg, arg);
     case UnaryFn::tanh:
       return builder.create<math::TanhOp>(arg.getLoc(), arg);
+    case UnaryFn::erf:
+      return builder.create<math::ErfOp>(arg.getLoc(), arg);
     }
     llvm_unreachable("unsupported unary function");
   }
@@ -483,6 +485,9 @@ public:
       if (allFloatingPoint)
         return builder.create<arith::MinimumFOp>(arg0.getLoc(), arg0, arg1);
       return builder.create<arith::MinUIOp>(arg0.getLoc(), arg0, arg1);
+    case BinaryFn::powf:
+      assert(allFloatingPoint);
+      return builder.create<math::PowFOp>(arg0.getLoc(), arg0, arg1);
     }
     llvm_unreachable("unsupported binary function");
   }
