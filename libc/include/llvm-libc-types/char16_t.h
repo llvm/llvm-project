97ad4a7ba5bc3918e1100aaab0a796fc254a1f1f
Fabian Keßler <fabian_kessler@gmx.de>
2024-04-29 23:14:15 +0200

    Applied feedback  - headers fixed  - included `stdint-macros.h` instead of `stdint.h`  - Updated dependencies of `char16_t` and `char32_t`  - Added uchar support for linux-riscv  - Added uchar & wchar support for linux-arm & linux-aarch64  - Added UCharAPI type to linux/api.td

--- libc/include/llvm-libc-types/char16_t.h
+++ libc/include/llvm-libc-types/char16_t.h
@@ -1,4 +1,4 @@
-//===-- Definition of clock_t type ----------------------------------------===//
+//===-- Definition of char16_t type ---------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,12 +6,12 @@
 //
 //===----------------------------------------------------------------------===//
 
-#ifndef LLVM_LIBC_TYPES_CHAR8_T_H
-#define LLVM_LIBC_TYPES_CHAR8_T_H
+#ifndef LLVM_LIBC_TYPES_CHAR16_T_H
+#define LLVM_LIBC_TYPES_CHAR16_T_H
 
 #if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L
-#include <stdint.h>
+#include "../llvm-libc-macros/stdint-macros.h"
 typedef uint_least16_t char16_t;
 #endif
 
-#endif // LLVM_LIBC_TYPES_CHAR8_T_H
+#endif // LLVM_LIBC_TYPES_CHAR16_T_H
--- libc/include/llvm-libc-types/char32_t.h
+++ libc/include/llvm-libc-types/char32_t.h
@@ -1,4 +1,4 @@
-//===-- Definition of clock_t type ----------------------------------------===//
+//===-- Definition of char32_t type ---------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,12 +6,12 @@
 //
 //===----------------------------------------------------------------------===//
 
-#ifndef LLVM_LIBC_TYPES_CHAR8_T_H
-#define LLVM_LIBC_TYPES_CHAR8_T_H
+#ifndef LLVM_LIBC_TYPES_CHAR32_T_H
+#define LLVM_LIBC_TYPES_CHAR32_T_H
 
 #if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L
-#include <stdint.h>
+#include "../llvm-libc-macros/stdint-macros.h"
 typedef uint_least32_t char32_t;
 #endif
 
-#endif // LLVM_LIBC_TYPES_CHAR8_T_H
+#endif // LLVM_LIBC_TYPES_CHAR32_T_H
