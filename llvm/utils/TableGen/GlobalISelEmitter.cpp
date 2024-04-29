5e6ea10ed521d595d1ed9f437a68bf73ea0fbf80
Kai Nacke <kai.peter.nacke@ibm.com>
2024-04-29 12:06:26 -0400

    [TableGen][GISel] Handle frameindex/tframeindex (#90475)
    Support patterns like
    
      Pat<(p0 frameindex:$fi), (ADD tframeindex:$fi, 0)>;
    
    in the GlobalISel emitter in TableGen. Currently, using such a pattern
    results in an error message.

--- llvm/utils/TableGen/GlobalISelEmitter.cpp
+++ llvm/utils/TableGen/GlobalISelEmitter.cpp
@@ -834,6 +834,11 @@ Expected<InstructionMatcher &> GlobalISelEmitter::createAndImportSelDAGMatcher(
       return InsnMatcher;
     }
 
+    if (SrcGIOrNull->TheDef->getName() == "G_FRAME_INDEX") {
+      InsnMatcher.addOperand(OpIdx++, Src.getName(), TempOpIdx);
+      return InsnMatcher;
+    }
+
     // Special case because the operand order is changed from setcc. The
     // predicate operand needs to be swapped from the last operand to the first
     // source.
@@ -1221,6 +1226,10 @@ Expected<action_iterator> GlobalISelEmitter::importExplicitUseRenderer(
     // FIXME: The target should be able to choose sign-extended when appropriate
     //        (e.g. on Mips).
     if (DstChild.getOperator()->getName() == "timm") {
+      DstMIBuilder.addRenderer<CopyRenderer>(DstChild.getName());
+      return InsertPt;
+    }
+    if (DstChild.getOperator()->getName() == "tframeindex") {
       DstMIBuilder.addRenderer<CopyRenderer>(DstChild.getName());
       return InsertPt;
     } else if (DstChild.getOperator()->getName() == "imm") {
