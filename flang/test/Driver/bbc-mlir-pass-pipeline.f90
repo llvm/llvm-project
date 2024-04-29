43b414621408f8149fb0d6328a156e83ac87d7b6
Kareem Ergawy <kareem.ergawy@amd.com>
2024-04-29 05:46:37 +0200

    [flang][OpenMP][LLVMIR] Support CFG and LLVM IR conversion for `omp.p… (#90164)
    …rivate`
    
    Adds support for CFG conversion and conversion to LLVM IR for
    `omp.private` ops. This bridges a gap between FIR and LLVM to provide
    more support for lowering `omp.private` ops for things like
    allocatables.

--- flang/test/Driver/bbc-mlir-pass-pipeline.f90
+++ flang/test/Driver/bbc-mlir-pass-pipeline.f90
@@ -17,13 +17,15 @@ end program
 ! CHECK-NEXT:   (S) 0 num-cse'd - Number of operations CSE'd
 ! CHECK-NEXT:   (S) 0 num-dce'd - Number of operations DCE'd
 
-! CHECK-NEXT: Pipeline Collection : ['fir.global', 'func.func', 'omp.declare_reduction']
+! CHECK-NEXT: Pipeline Collection : ['fir.global', 'func.func', 'omp.declare_reduction', 'omp.private']
 ! CHECK-NEXT: 'fir.global' Pipeline
 ! CHECK-NEXT:   CharacterConversion
 ! CHECK-NEXT: 'func.func' Pipeline
 ! CHECK-NEXT:   ArrayValueCopy
 ! CHECK-NEXT:   CharacterConversion
 ! CHECK-NEXT: 'omp.declare_reduction' Pipeline
+! CHECK-NEXT:   CharacterConversion
+! CHECK-NEXT: 'omp.private' Pipeline
 ! CHECK-NEXT:   CharacterConversion
 
 ! CHECK-NEXT: Canonicalizer
@@ -43,13 +45,15 @@ end program
 ! CHECK-NEXT:   (S) 0 num-cse'd - Number of operations CSE'd
 ! CHECK-NEXT:   (S) 0 num-dce'd - Number of operations DCE'd
 
-! CHECK-NEXT: Pipeline Collection : ['fir.global', 'func.func', 'omp.declare_reduction']
+! CHECK-NEXT: Pipeline Collection : ['fir.global', 'func.func', 'omp.declare_reduction', 'omp.private']
 ! CHECK-NEXT: 'fir.global' Pipeline
 ! CHECK-NEXT:   CFGConversion
 ! CHECK-NEXT: 'func.func' Pipeline
 ! CHECK-NEXT:   PolymorphicOpConversion
 ! CHECK-NEXT:   CFGConversion
 ! CHECK-NEXT: 'omp.declare_reduction' Pipeline
+! CHECK-NEXT:   CFGConversion
+! CHECK-NEXT: 'omp.private' Pipeline
 ! CHECK-NEXT:   CFGConversion
 
 ! CHECK-NEXT: SCFToControlFlow
