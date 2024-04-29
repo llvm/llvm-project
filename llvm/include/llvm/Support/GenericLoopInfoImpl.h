141bce57d81f6cbd66060cdcf22e31da0b116277
Ben Barham <ben_barham@apple.com>
2024-04-28 18:55:09 -0700

    [Support] Do not use `llvm::size` in `getLoopPreheader`
    There's no reason to require random access on downstream clients here.
    
    (cherry picked from commit 355f86e7741422bff73fb568013456a4cbf5e76b)

--- llvm/include/llvm/Support/GenericLoopInfoImpl.h
+++ llvm/include/llvm/Support/GenericLoopInfoImpl.h
@@ -208,7 +208,10 @@ BlockT *LoopBase<BlockT, LoopT>::getLoopPreheader() const {
     return nullptr;
 
   // Make sure there is only one exit out of the preheader.
-  if (llvm::size(llvm::children<BlockT *>(Out)) != 1)
+  typedef GraphTraits<BlockT *> BlockTraits;
+  typename BlockTraits::ChildIteratorType SI = BlockTraits::child_begin(Out);
+  ++SI;
+  if (SI != BlockTraits::child_end(Out))
     return nullptr; // Multiple exits from the block, must not be a preheader.
 
   // The predecessor has exactly one successor, so it is a preheader.
