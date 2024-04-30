78a75f56e6624adc89b4b701ab6c3a617a9f8d90
Craig Topper <craig.topper@sifive.com>
2024-04-29 22:11:30 -0700

    fixup! Use Passthru.isUndef()

--- llvm/lib/Target/RISCV/RISCVISelLowering.cpp
+++ llvm/lib/Target/RISCV/RISCVISelLowering.cpp
@@ -16791,9 +16791,8 @@ SDValue RISCVTargetLowering::PerformDAGCombine(SDNode *N,
     SDValue Scalar = N->getOperand(1);
     SDValue VL = N->getOperand(2);
 
-    if (Scalar.getOpcode() == RISCVISD::VMV_X_S &&
-        Scalar.getOperand(0).getValueType() == N->getValueType(0) &&
-        N->getOperand(0).isUndef())
+    if (Scalar.getOpcode() == RISCVISD::VMV_X_S && Passthru.isUndef() &&
+        Scalar.getOperand(0).getValueType() == N->getValueType(0))
       return Scalar.getOperand(0);
 
     // Use M1 or smaller to avoid over constraining register allocation
