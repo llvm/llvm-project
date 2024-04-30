c51dbb4692e1e3718881f2639c95a215a8820079
Wei Wang <apollo.mobility@gmail.com>
2024-04-30 14:31:31 -0700

    [Coroutines][Test] Specify target triple in coro-elide-thinlto  (#90549)
    Resolve test failure on non-x86 linux host

--- clang/test/CodeGenCoroutines/coro-elide-thinlto.cpp
+++ clang/test/CodeGenCoroutines/coro-elide-thinlto.cpp
@@ -5,7 +5,7 @@
 // RUN: %clang --target=x86_64-linux -std=c++20 -O2 -flto=thin -I %S -c %t/coro-elide-callee.cpp -o %t/coro-elide-callee.o
 // RUN: %clang --target=x86_64-linux -std=c++20 -O2 -flto=thin -I %S -c %t/coro-elide-caller.cpp -o %t/coro-elide-caller.o
 // RUN: llvm-lto -thinlto %t/coro-elide-callee.o %t/coro-elide-caller.o -o %t/summary
-// RUN: %clang_cc1 -O2 -x ir %t/coro-elide-caller.o -fthinlto-index=%t/summary.thinlto.bc -emit-llvm -o - | FileCheck %s
+// RUN: %clang_cc1 -triple x86_64-unknown-linux -O2 -x ir %t/coro-elide-caller.o -fthinlto-index=%t/summary.thinlto.bc -emit-llvm -o - | FileCheck %s
 
 //--- coro-elide-task.h
 #pragma once
