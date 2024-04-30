184fbd4d73e86da2ce9fea98f534d3d259c18744
Schrodinger ZHU Yifan <yifanzhu@rochester.edu>
2024-04-30 17:55:33 -0400

    fix

--- libc/fuzzing/__support/hashtable_fuzz.cpp
+++ libc/fuzzing/__support/hashtable_fuzz.cpp
@@ -11,160 +11,179 @@
 //===----------------------------------------------------------------------===//
 #include "src/__support/CPP/new.h"
 #include "src/__support/CPP/optional.h"
+#include "src/__support/CPP/string.h"
+#include "src/__support/CPP/utility/forward.h"
 #include "src/__support/HashTable/table.h"
-#include "src/string/memcpy.h"
-#include <search.h>
 #include <stdint.h>
 namespace LIBC_NAMESPACE {
 
-enum class Action { Find, Insert, CrossCheck };
-static uint8_t *global_buffer = nullptr;
-static size_t remaining = 0;
+template <typename T> class UniquePtr {
+  T *ptr;
 
-static cpp::optional<uint8_t> next_u8() {
-  if (remaining == 0)
-    return cpp::nullopt;
-  uint8_t result = *global_buffer;
-  global_buffer++;
-  remaining--;
-  return result;
-}
-
-static cpp::optional<uint64_t> next_uint64() {
-  uint64_t result;
-  if (remaining < sizeof(result))
-    return cpp::nullopt;
-  memcpy(&result, global_buffer, sizeof(result));
-  global_buffer += sizeof(result);
-  remaining -= sizeof(result);
-  return result;
-}
-
-static cpp::optional<Action> next_action() {
-  if (cpp::optional<uint8_t> action = next_u8()) {
-    switch (*action % 3) {
-    case 0:
-      return Action::Find;
-    case 1:
-      return Action::Insert;
-    case 2:
-      return Action::CrossCheck;
-    }
+public:
+  UniquePtr(T *ptr) : ptr(ptr) {}
+  ~UniquePtr() { delete ptr; }
+  UniquePtr(UniquePtr &&other) : ptr(other.ptr) { other.ptr = nullptr; }
+  UniquePtr &operator=(UniquePtr &&other) {
+    delete ptr;
+    ptr = other.ptr;
+    other.ptr = nullptr;
+    return *this;
   }
-  return cpp::nullopt;
-}
-
-static cpp::optional<char *> next_cstr() {
-  char *result = reinterpret_cast<char *>(global_buffer);
-  if (cpp::optional<uint8_t> len = next_u8()) {
-    uint64_t length;
-    for (length = 0; length < *len; length++) {
-      if (length >= remaining)
-        return cpp::nullopt;
-      if (*global_buffer == '\0')
-        break;
-    }
-    if (length >= remaining)
-      return cpp::nullopt;
-    global_buffer[length] = '\0';
-    global_buffer += length + 1;
-    remaining -= length + 1;
-    return result;
+  T *operator->() { return ptr; }
+  template <typename... U> static UniquePtr create(U &&...x) {
+    AllocChecker ac;
+    T *ptr = new (ac) T(cpp::forward<U>(x)...);
+    if (!ac)
+      return {nullptr};
+    return UniquePtr(ptr);
   }
-  return cpp::nullopt;
-}
-
-#define get_value(op)                                                          \
-  __extension__({                                                              \
-    auto val = op();                                                           \
-    if (!val)                                                                  \
-      return 0;                                                                \
-    *val;                                                                      \
-  })
-
-template <typename Fn> struct CleanUpHook {
-  cpp::optional<Fn> fn;
-  ~CleanUpHook() {
-    if (fn)
-      (*fn)();
-  }
-  CleanUpHook(Fn fn) : fn(cpp::move(fn)) {}
-  CleanUpHook(const CleanUpHook &) = delete;
-  CleanUpHook(CleanUpHook &&other) : fn(cpp::move(other.fn)) {
-    other.fn = cpp::nullopt;
-  }
+  operator bool() { return ptr != nullptr; }
+  T *get() { return ptr; }
 };
 
-#define register_cleanup(ID, ...)                                              \
-  auto cleanup_hook##ID = __extension__({                                      \
-    auto a = __VA_ARGS__;                                                      \
-    CleanUpHook<decltype(a)>(cpp::move(a));                                    \
-  });
+// a tagged union
+struct Action {
+  enum class Tag { Find, Insert, CrossCheck } tag;
+  cpp::string key;
+  UniquePtr<Action> next;
+  Action(Tag tag, cpp::string key, UniquePtr<Action> next)
+      : tag(tag), key(cpp::move(key)), next(cpp::move(next)) {}
+};
 
-static void trap_with_message(const char *msg) { __builtin_trap(); }
+static struct {
+  UniquePtr<Action> actions = nullptr;
+  size_t remaining;
+  const char *buffer;
+
+  template <typename T> cpp::optional<T> next() {
+    static_assert(cpp::is_integral<T>::value, "T must be an integral type");
+    union {
+      T result;
+      char data[sizeof(T)];
+    };
+    if (remaining < sizeof(result))
+      return cpp::nullopt;
+    for (size_t i = 0; i < sizeof(result); i++)
+      data[i] = buffer[i];
+    buffer += sizeof(result);
+    remaining -= sizeof(result);
+    return result;
+  }
+
+  cpp::optional<cpp::string> next_string() {
+    if (cpp::optional<uint16_t> len = next<uint16_t>()) {
+      uint64_t length;
+      for (length = 0; length < *len && length < remaining; length++)
+        if (buffer[length] == '\0')
+          break;
+      cpp::string result(buffer, length);
+      result += '\0';
+      buffer += length;
+      remaining -= length;
+      return result;
+    }
+    return cpp::nullopt;
+  }
+  Action *next_action() {
+    if (cpp::optional<uint8_t> action = next<uint8_t>()) {
+      switch (*action % 3) {
+      case 0: {
+        if (cpp::optional<cpp::string> key = next_string())
+          actions = UniquePtr<Action>::create(
+              Action::Tag::Find, cpp::move(*key), cpp::move(actions));
+        else
+          return nullptr;
+        break;
+      }
+      case 1: {
+        if (cpp::optional<cpp::string> key = next_string())
+          actions = UniquePtr<Action>::create(
+              Action::Tag::Insert, cpp::move(*key), cpp::move(actions));
+        else
+          return nullptr;
+        break;
+      }
+      case 2: {
+        actions = UniquePtr<Action>::create(Action::Tag::CrossCheck, "",
+                                            cpp::move(actions));
+        break;
+      }
+      }
+      return actions.get();
+    }
+    return nullptr;
+  }
+} global_status;
+
+class HashTable {
+  internal::HashTable *table;
+
+public:
+  HashTable(uint64_t size, uint64_t seed)
+      : table(internal::HashTable::allocate(size, seed)) {}
+  HashTable(internal::HashTable *table) : table(table) {}
+  ~HashTable() { internal::HashTable::deallocate(table); }
+  HashTable(HashTable &&other) : table(other.table) { other.table = nullptr; }
+  bool is_valid() const { return table != nullptr; }
+  ENTRY *find(const char *key) { return table->find(key); }
+  ENTRY *insert(const ENTRY &entry) {
+    return internal::HashTable::insert(this->table, entry);
+  }
+  using iterator = internal::HashTable::iterator;
+  iterator begin() const { return table->begin(); }
+  iterator end() const { return table->end(); }
+};
+
+HashTable next_hashtable() {
+  if (cpp::optional<uint16_t> size = global_status.next<uint16_t>())
+    if (cpp::optional<uint64_t> seed = global_status.next<uint64_t>())
+      return HashTable(*size, *seed);
+
+  return HashTable(0, 0);
+}
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
-  AllocChecker ac;
-  global_buffer = static_cast<uint8_t *>(::operator new(size, ac));
-  register_cleanup(0, [global_buffer = global_buffer, size] {
-    ::operator delete(global_buffer, size);
-  });
-  if (!ac)
+  char key[] = "key";
+  global_status.buffer = reinterpret_cast<const char *>(data);
+  global_status.remaining = size;
+  HashTable table_a = next_hashtable();
+  HashTable table_b = next_hashtable();
+  if (!table_a.is_valid() || !table_b.is_valid())
     return 0;
-  memcpy(global_buffer, data, size);
 
-  remaining = size;
-  uint64_t size_a = get_value(next_u8);
-  uint64_t size_b = get_value(next_u8);
-  uint64_t rand_a = get_value(next_uint64);
-  uint64_t rand_b = get_value(next_uint64);
-  internal::HashTable *table_a = internal::HashTable::allocate(size_a, rand_a);
-  register_cleanup(1, [&table_a] {
-    if (table_a)
-      internal::HashTable::deallocate(table_a);
-  });
-  internal::HashTable *table_b = internal::HashTable::allocate(size_b, rand_b);
-  register_cleanup(2, [&table_b] {
-    if (table_b)
-      internal::HashTable::deallocate(table_b);
-  });
-  if (!table_a || !table_b)
-    return 0;
   for (;;) {
-    Action action = get_value(next_action);
-    switch (action) {
-    case Action::Find: {
-      const char *key = get_value(next_cstr);
-      if (static_cast<bool>(table_a->find(key)) !=
-          static_cast<bool>(table_b->find(key)))
-        trap_with_message(key);
+    Action *action = global_status.next_action();
+    if (!action)
+      return 0;
+    switch (action->tag) {
+    case Action::Tag::Find: {
+      if (table_a.find(action->key.c_str()) !=
+          table_b.find(action->key.c_str()))
+        __builtin_trap();
       break;
     }
-    case Action::Insert: {
-      char *key = get_value(next_cstr);
-      ENTRY *a = internal::HashTable::insert(table_a, ENTRY{key, key});
-      ENTRY *b = internal::HashTable::insert(table_b, ENTRY{key, key});
+    case Action::Tag::Insert: {
+      ENTRY *a = table_a.insert(ENTRY{key, key});
+      ENTRY *b = table_b.insert(ENTRY{key, key});
       if (a->data != b->data)
         __builtin_trap();
       break;
     }
-    case Action::CrossCheck: {
-      for (ENTRY a : *table_a) {
-        if (const ENTRY *b = table_b->find(a.key)) {
-          if (a.data != b->data)
-            __builtin_trap();
-        }
-      }
-      for (ENTRY b : *table_b) {
-        if (const ENTRY *a = table_a->find(b.key)) {
-          if (a->data != b.data)
-            __builtin_trap();
-        }
-      }
+    case Action::Tag::CrossCheck: {
+      for (ENTRY a : table_a)
+        if (const ENTRY *b = table_b.find(a.key); a.data != b->data)
+          __builtin_trap();
+
+      for (ENTRY b : table_b)
+        if (const ENTRY *a = table_a.find(b.key); a->data != b.data)
+          __builtin_trap();
+
       break;
     }
     }
   }
+  return 0;
 }
 
 } // namespace LIBC_NAMESPACE
