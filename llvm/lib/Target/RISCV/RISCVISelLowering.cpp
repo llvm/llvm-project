fae348ed55de2736d86525518b82fc1df0b6d959
Craig Topper <craig.topper@sifive.com>
2024-04-29 23:35:30 -0700

    [RISCV] Add DAG combine for (vmv_s_x_vl (undef) (vmv_x_s X). (#90524)
    We can use the original vector as long as the type of X matches the
    result type of the vmv_s_x_vl.

--- llvm/lib/Target/RISCV/RISCVISelLowering.cpp
+++ llvm/lib/Target/RISCV/RISCVISelLowering.cpp
@@ -16791,6 +16791,10 @@ SDValue RISCVTargetLowering::PerformDAGCombine(SDNode *N,
     SDValue Scalar = N->getOperand(1);
     SDValue VL = N->getOperand(2);
 
+    if (Scalar.getOpcode() == RISCVISD::VMV_X_S && Passthru.isUndef() &&
+        Scalar.getOperand(0).getValueType() == N->getValueType(0))
+      return Scalar.getOperand(0);
+
     // Use M1 or smaller to avoid over constraining register allocation
     const MVT M1VT = getLMUL1VT(VT);
     if (M1VT.bitsLT(VT)) {
