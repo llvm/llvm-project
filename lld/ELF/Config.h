2dcddeda96b170e19406c0b1919856face73814c
Fangrui Song <i@maskray.me>
2024-05-01 11:40:46 -0700

    [ELF] Adjust --compress-sections to support compression level
    zstd excels at scaling from low-ratio-very-fast to
    high-ratio-pretty-slow. Some users prioritize speed and prefer disk read
    speed, while others focus on achieving the highest compression ratio
    possible, similar to traditional high-ratio codecs like LZMA.
    
    Add an optional `level` to `--compress-sections` (#84855) to cater to
    these diverse needs. While we initially aimed for a one-size-fits-all
    approach, this no longer seems to work.
    (https://richg42.blogspot.com/2015/11/the-lossless-decompression-pareto.html)
    
    When --compress-debug-sections is used together, make
    --compress-sections take precedence since --compress-sections is usually
    more specific.
    
    Remove the level distinction between -O/-O1 and -O2 for
    --compress-debug-sections=zlib for a more consistent user experience.
    
    Pull Request: https://github.com/llvm/llvm-project/pull/90567

--- lld/ELF/Config.h
+++ lld/ELF/Config.h
@@ -224,7 +224,8 @@ struct Config {
   bool checkSections;
   bool checkDynamicRelocs;
   std::optional<llvm::DebugCompressionType> compressDebugSections;
-  llvm::SmallVector<std::pair<llvm::GlobPattern, llvm::DebugCompressionType>, 0>
+  llvm::SmallVector<
+      std::tuple<llvm::GlobPattern, llvm::DebugCompressionType, unsigned>, 0>
       compressSections;
   bool cref;
   llvm::SmallVector<std::pair<llvm::GlobPattern, uint64_t>, 0>
