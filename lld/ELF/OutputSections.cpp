94f73d7c70302a19932b9d6ba23dc2186cdbcc5e
Fangrui Song <i@maskray.me>
2024-05-01 11:40:46 -0700

    [ELF] Adjust --compress-sections to support compression level
    zstd excels at scaling from low-ratio-very-fast to
    high-ratio-pretty-slow. Some users prioritize speed and prefer disk read
    speed, while others focus on achieving the highest compression ratio
    possible, similar to traditional high-ratio codecs like LZMA.
    
    Add an optional `level` to `--compress-sections` (#84855) to cater to
    these diverse needs. While we initially aimed for a one-size-fits-all
    approach, this no longer seems to work.
    (https://richg42.blogspot.com/2015/11/the-lossless-decompression-pareto.html)
    
    When --compress-debug-sections is used together, make
    --compress-sections take precedence since --compress-sections is usually
    more specific.
    
    Remove the level distinction between -O/-O1 and -O2 for
    --compress-debug-sections=zlib for a more consistent user experience.
    
    Pull Request: https://github.com/llvm/llvm-project/pull/90567

--- lld/ELF/OutputSections.cpp
+++ lld/ELF/OutputSections.cpp
@@ -339,12 +339,13 @@ template <class ELFT> void OutputSection::maybeCompress() {
   (void)sizeof(Elf_Chdr);
 
   DebugCompressionType ctype = DebugCompressionType::None;
-  for (auto &[glob, t] : config->compressSections)
-    if (glob.match(name))
-      ctype = t;
+  unsigned level = 0; // default compression level
   if (!(flags & SHF_ALLOC) && config->compressDebugSections &&
       name.starts_with(".debug_") && size)
     ctype = *config->compressDebugSections;
+  for (auto &[glob, t, l] : config->compressSections)
+    if (glob.match(name))
+      std::tie(ctype, level) = {t, l};
   if (ctype == DebugCompressionType::None)
     return;
   if (flags & SHF_ALLOC) {
@@ -376,13 +377,14 @@ template <class ELFT> void OutputSection::maybeCompress() {
   auto shardsOut = std::make_unique<SmallVector<uint8_t, 0>[]>(numShards);
 
 #if LLVM_ENABLE_ZSTD
-  // Use ZSTD's streaming compression API which permits parallel workers working
-  // on the stream. See http://facebook.github.io/zstd/zstd_manual.html
-  // "Streaming compression - HowTo".
+  // Use ZSTD's streaming compression API. See
+  // http://facebook.github.io/zstd/zstd_manual.html "Streaming compression -
+  // HowTo".
   if (ctype == DebugCompressionType::Zstd) {
     parallelFor(0, numShards, [&](size_t i) {
       SmallVector<uint8_t, 0> out;
       ZSTD_CCtx *cctx = ZSTD_createCCtx();
+      ZSTD_CCtx_setParameter(cctx, ZSTD_c_compressionLevel, level);
       ZSTD_inBuffer zib = {shardsIn[i].data(), shardsIn[i].size(), 0};
       ZSTD_outBuffer zob = {nullptr, 0, 0};
       size_t size;
@@ -410,12 +412,10 @@ template <class ELFT> void OutputSection::maybeCompress() {
 
 #if LLVM_ENABLE_ZLIB
   // We chose 1 (Z_BEST_SPEED) as the default compression level because it is
-  // the fastest. If -O2 is given, we use level 6 to compress debug info more by
-  // ~15%. We found that level 7 to 9 doesn't make much difference (~1% more
-  // compression) while they take significant amount of time (~2x), so level 6
-  // seems enough.
+  // fast and provides decent compression ratios.
   if (ctype == DebugCompressionType::Zlib) {
-    const int level = config->optimize >= 2 ? 6 : Z_BEST_SPEED;
+    if (!level)
+      level = Z_BEST_SPEED;
 
     // Compress shards and compute Alder-32 checksums. Use Z_SYNC_FLUSH for all
     // shards but the last to flush the output to a byte boundary to be
