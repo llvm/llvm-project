cf1b4a9df82969b71518a2be7da5408645c5d554
Björn Pettersson <bjorn.a.pettersson@ericsson.com>
2024-05-02 22:49:39 +0200

    [clang][CodeGen] Fix in codegen for __builtin_popcountg/ctzg/clzg (#90845)
    Make sure that the result from the popcnt/ctlz/cttz intrinsics is
    unsigned casted to int, rather than casted as a signed value, when
    expanding the __builtin_popcountg/__builtin_ctzg/__builtin_clzg
    builtins.
    
    An example would be
      unsigned _BitInt(1) x = ...;
      int y = __builtin_popcountg(x);
    which previously was incorrectly expanded to
      %1 = call i1 @llvm.ctpop.i1(i1 %0)
      %cast = sext i1 %1 to i32
    
    Since the input type is generic for those "g" versions of the builtins
    the intrinsic call may return a value for which the sign bit is set
    (that could typically for BitInt of size 1 and 2). So we need to emit a
    zext rather than a sext to avoid negative results.

--- clang/lib/CodeGen/CGBuiltin.cpp
+++ clang/lib/CodeGen/CGBuiltin.cpp
@@ -3239,8 +3239,8 @@ RValue CodeGenFunction::EmitBuiltinExpr(const GlobalDecl GD, unsigned BuiltinID,
         Builder.getInt1(HasFallback || getTarget().isCLZForZeroUndef());
     Value *Result = Builder.CreateCall(F, {ArgValue, ZeroUndef});
     if (Result->getType() != ResultType)
-      Result = Builder.CreateIntCast(Result, ResultType, /*isSigned*/true,
-                                     "cast");
+      Result =
+          Builder.CreateIntCast(Result, ResultType, /*isSigned*/ false, "cast");
     if (!HasFallback)
       return RValue::get(Result);
 
@@ -3271,8 +3271,8 @@ RValue CodeGenFunction::EmitBuiltinExpr(const GlobalDecl GD, unsigned BuiltinID,
         Builder.getInt1(HasFallback || getTarget().isCLZForZeroUndef());
     Value *Result = Builder.CreateCall(F, {ArgValue, ZeroUndef});
     if (Result->getType() != ResultType)
-      Result = Builder.CreateIntCast(Result, ResultType, /*isSigned*/true,
-                                     "cast");
+      Result =
+          Builder.CreateIntCast(Result, ResultType, /*isSigned*/ false, "cast");
     if (!HasFallback)
       return RValue::get(Result);
 
@@ -3351,8 +3351,8 @@ RValue CodeGenFunction::EmitBuiltinExpr(const GlobalDecl GD, unsigned BuiltinID,
     llvm::Type *ResultType = ConvertType(E->getType());
     Value *Result = Builder.CreateCall(F, ArgValue);
     if (Result->getType() != ResultType)
-      Result = Builder.CreateIntCast(Result, ResultType, /*isSigned*/true,
-                                     "cast");
+      Result =
+          Builder.CreateIntCast(Result, ResultType, /*isSigned*/ false, "cast");
     return RValue::get(Result);
   }
   case Builtin::BI__builtin_unpredictable: {
