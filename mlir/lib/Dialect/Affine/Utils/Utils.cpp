0e00155d0e6a7a57255438dc98558478f3b58320
Kai Sasaki <lewuathe@gmail.com>
2024-05-02 16:37:12 +0900

    [mlir][affine] Fix the crash due to the simultaneous replacement store

--- mlir/lib/Dialect/Affine/Utils/Utils.cpp
+++ mlir/lib/Dialect/Affine/Utils/Utils.cpp
@@ -866,8 +866,7 @@ static void forwardStoreToLoad(AffineReadOpInterface loadOp,
     // 3. The store must reach the load. Access function equivalence only
     // guarantees this for accesses in the same block. The load could be in a
     // nested block that is unreachable.
-    if (storeOp->getBlock() != loadOp->getBlock() &&
-        !mustReachAtInnermost(srcAccess, destAccess))
+    if (!mustReachAtInnermost(srcAccess, destAccess))
       continue;
 
     // 4. Ensure there is no intermediate operation which could replace the
