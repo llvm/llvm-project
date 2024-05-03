919e8c9a7b7de704735648decb1c1ed69cb68b2a
Damien L-G <dalg24@gmail.com>
2024-05-03 13:06:35 -0400

    [libc++][atomic_ref] Mark notify_{all,one}/wait test as unsupported with no threads

--- libcxx/test/std/atomics/atomics.ref/notify_one.pass.cpp
+++ libcxx/test/std/atomics/atomics.ref/notify_one.pass.cpp
@@ -6,6 +6,7 @@
 //===----------------------------------------------------------------------===//
 
 // UNSUPPORTED: c++03, c++11, c++14, c++17
+// UNSUPPORTED: no-threads
 
 // void notify_one() const noexcept;
 
