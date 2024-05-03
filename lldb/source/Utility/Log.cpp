d1adf630a9981f275f24e4d0c2c613a90ff38290
Jonas Devlieghere <jonas@devlieghere.com>
2024-05-03 10:11:40 -0700

    [lldb] Add TeeLogHandler to log to 2 handlers
    Add a T-style log handler that multiplexes messages to two log handlers.
    The goal is to use this in combination with the SystemLogHandler to log
    messages both to the user requested file as well as the system log. The
    latter is part of a sysdiagnose on Darwin which is commonly attached to
    bug reports.

--- lldb/source/Utility/Log.cpp
+++ lldb/source/Utility/Log.cpp
@@ -39,6 +39,7 @@ char LogHandler::ID;
 char StreamLogHandler::ID;
 char CallbackLogHandler::ID;
 char RotatingLogHandler::ID;
+char TeeLogHandler::ID;
 
 llvm::ManagedStatic<Log::ChannelMap> Log::g_channel_map;
 
@@ -437,4 +438,16 @@ void RotatingLogHandler::Dump(llvm::raw_ostream &stream) const {
     stream << m_messages[idx];
   }
   stream.flush();
+}
+
+TeeLogHandler::TeeLogHandler(std::shared_ptr<LogHandler> first_log_handler,
+                             std::shared_ptr<LogHandler> second_log_handler)
+    : m_first_log_handler(first_log_handler),
+      m_second_log_handler(second_log_handler) {}
+
+void TeeLogHandler::Emit(llvm::StringRef message) {
+  if (m_first_log_handler)
+    m_first_log_handler->Emit(message);
+  if (m_second_log_handler)
+    m_second_log_handler->Emit(message);
 }
