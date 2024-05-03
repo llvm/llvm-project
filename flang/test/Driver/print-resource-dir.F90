99f61034d5c5e66919670723ef881b46ed006321
Michael Klemm <michael.klemm@amd.com>
2024-05-03 09:35:25 +0200

    Simplify test by using DEFINE to avoid repitition

--- flang/test/Driver/print-resource-dir.F90
+++ flang/test/Driver/print-resource-dir.F90
@@ -1,3 +1,4 @@
-! RUN: %flang -print-resource-dir -resource-dir=%S/Inputs/resource_dir \
-! RUN:  | FileCheck -check-prefix=PRINT-RESOURCE-DIR -DFILE=%S/Inputs/resource_dir %s
+! DEFINE: %{resource_dir} = %S/Inputs/resource_dir
+! RUN: %flang -print-resource-dir -resource-dir=%{resource_dir}.. \
+! RUN:  | FileCheck -check-prefix=PRINT-RESOURCE-DIR -DFILE=%{resource_dir} %s
 ! PRINT-RESOURCE-DIR: [[FILE]]
