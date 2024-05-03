d579fe76ae01cbe78305f09790ee7ae231ce51e9
Abid Qadeer <haqadeer@amd.com>
2024-05-03 15:23:04 +0100

    Address review comment.
    I have added CHECK lines as was suggested in the review.

--- flang/test/Integration/debug-loc-1.f90
+++ flang/test/Integration/debug-loc-1.f90
@@ -1,4 +1,4 @@
-!RUN: %flang_fc1 -emit-llvm -debug-info-kind=line-tables-only -fopenmp %s -o -
+!RUN: %flang_fc1 -emit-llvm -debug-info-kind=line-tables-only -fopenmp %s -o - | FileCheck %s
 
 ! Test that this file builds without an error.
 
@@ -9,6 +9,7 @@ implicit none
  integer :: i
  real, save :: var
 
+! CHECK: DILocation(line: [[@LINE+1]], {{.*}})
 !$omp parallel do
 do i=1,100
   var = var + 0.1
@@ -21,6 +22,8 @@ subroutine test2
 
 real, save :: tp
 !$omp threadprivate (tp)
+! CHECK: DILocation(line: [[@LINE+1]], {{.*}})
+  tp = tp + 1
 
 end subroutine test2
 
