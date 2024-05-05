0d501f38f348cf046d40c9baee12f0c5145b6d8c
Fangrui Song <i@maskray.me>
2024-05-04 17:15:51 -0700

    [test] %clang_cc1 -emit-llvm: remove redundant -S
    Also replace aarch64-none-linux-gnu (none can indicate an OS as well) with aarch64

--- clang/test/CodeGen/math-builtins-long.c
+++ clang/test/CodeGen/math-builtins-long.c
@@ -1,6 +1,6 @@
-// RUN: %clang_cc1 -triple x86_64-unknown-unknown -w -S -o - -emit-llvm %s \
+// RUN: %clang_cc1 -triple x86_64-unknown-unknown -w -o - -emit-llvm %s \
 // RUN:   -fmath-errno | FileCheck %s -check-prefix=F80
-// RUN: %clang_cc1 -triple ppc64le-unknown-unknown -w -S -o - -emit-llvm %s \
+// RUN: %clang_cc1 -triple ppc64le-unknown-unknown -w -o - -emit-llvm %s \
 // RUN:   -fmath-errno | FileCheck %s -check-prefix=PPC
 // RUN: %clang_cc1 -triple x86_64-unknown-unknown -mlong-double-128 -w -S \
 // RUN:   -o - -emit-llvm %s -fmath-errno | FileCheck %s -check-prefix=X86F128
