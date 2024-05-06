f88d5ae36bfc35654aabc7c7b0b5521efcb6987d
Elvis Wang <elvis.wang@sifive.com>
2024-05-05 18:28:24 -0700

    [RISCV][CostModel] Remove cost of cmp inst in cmp+select with SFB.
    With ShortFowrardBranchOpt(SFB) or ConditionalMoveFusion, scalar
    ICmp and scalar Select instructinos will lower to SELECT_CC
    and lower to PseudoCCMOVGPR which will generate a conditional
    branch instr and a move instr.
    The cost of scalar (ICmp + Select) = (0 + Select instr cost).

--- llvm/lib/Target/RISCV/RISCVTargetTransformInfo.cpp
+++ llvm/lib/Target/RISCV/RISCVTargetTransformInfo.cpp
@@ -1443,6 +1443,17 @@ InstructionCost RISCVTTIImpl::getCmpSelInstrCost(unsigned Opcode, Type *ValTy,
     }
   }
 
+  // With ShortForwardBranchOpt or ConditionalMoveFusion, scalar icmp + select
+  // instructions will lower to SELECT_CC and lower to PseudoCCMOVGPR which will
+  // generate a conditional branch + mv. The cost of scalar (icmp + select) will
+  // be (0 + select instr cost).
+  if (ST->hasConditionalMoveFusion() && I && isa<ICmpInst>(I) &&
+      !ValTy->isVectorTy() && I->hasOneUser() &&
+      isa<SelectInst>(I->user_back()) &&
+      !I->user_back()->getType()->isVectorTy() &&
+      I->user_back()->getOperand(0) == I)
+    return 0;
+
   // TODO: Add cost for scalar type.
 
   return BaseT::getCmpSelInstrCost(Opcode, ValTy, CondTy, VecPred, CostKind, I);
