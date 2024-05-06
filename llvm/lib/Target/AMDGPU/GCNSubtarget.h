55b4cfe8ae919f34bdaa2efebbf8c74d10104ead
Stanislav Mekhanoshin <Stanislav.Mekhanoshin@amd.com>
2024-05-06 12:22:50 -0700

    MI-400: MC support for V_FMA_MIX*_BF16 instructions
    Change-Id: I32b76d20ff56dc4d8f257a262c4dd6676d6c3beb

--- llvm/lib/Target/AMDGPU/GCNSubtarget.h
+++ llvm/lib/Target/AMDGPU/GCNSubtarget.h
@@ -120,6 +120,7 @@ protected:
   bool HasSMemRealTime = false;
   bool HasIntClamp = false;
   bool HasFmaMixInsts = false;
+  bool HasFmaMixBF16Insts = false;
   bool HasMovrel = false;
   bool HasVGPRIndexMode = false;
   bool HasScalarDwordx3Loads = false;
@@ -433,6 +434,10 @@ public:
 
   bool hasFmaMixInsts() const {
     return HasFmaMixInsts;
+  }
+
+  bool hasFmaMixBF16Insts() const {
+    return HasFmaMixBF16Insts;
   }
 
   bool hasCARRY() const {
