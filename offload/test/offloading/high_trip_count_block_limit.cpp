90e183386d7afe1b3466457caa73c9cf387729dc
Tim Gymnich <tgymnich@icloud.com>
2024-05-06 16:09:52 +0200

    disable clang format and add check for default behaviour

--- offload/test/offloading/high_trip_count_block_limit.cpp
+++ offload/test/offloading/high_trip_count_block_limit.cpp
@@ -1,9 +1,14 @@
-// RUN: %libomptarget-compilexx-generic && env
-// LIBOMPTARGET_REUSE_BLOCKS_FOR_HIGH_TRIP_COUNT=False %libomptarget-run-generic
-// 2>&1 | %fcheck-generic UNSUPPORTED: aarch64-unknown-linux-gnu UNSUPPORTED:
-// aarch64-unknown-linux-gnu-LTO UNSUPPORTED: x86_64-pc-linux-gnu UNSUPPORTED:
-// x86_64-pc-linux-gnu-LTO UNSUPPORTED: s390x-ibm-linux-gnu UNSUPPORTED:
-// s390x-ibm-linux-gnu-LTO
+// clang-format off
+// RUN: %libomptarget-compilexx-generic && env LIBOMPTARGET_REUSE_BLOCKS_FOR_HIGH_TRIP_COUNT=False %libomptarget-run-generic 2>&1 | %fcheck-generic
+// RUN: %libomptarget-compilexx-generic && env LIBOMPTARGET_REUSE_BLOCKS_FOR_HIGH_TRIP_COUNT=False %libomptarget-run-generic 2>&1 | %fcheck-generic --check-prefix=DEFAULT
+//
+// UNSUPPORTED: aarch64-unknown-linux-gnu 
+// UNSUPPORTED: aarch64-unknown-linux-gnu-LTO 
+// UNSUPPORTED: x86_64-pc-linux-gnu 
+// UNSUPPORTED: x86_64-pc-linux-gnu-LTO 
+// UNSUPPORTED: s390x-ibm-linux-gnu 
+// UNSUPPORTED: s390x-ibm-linux-gnu-LTO
+// clang-format on
 
 /*
   Check if there is a thread for each loop iteration
@@ -20,10 +25,11 @@ int main() {
     num_threads[j] = omp_get_num_threads() * omp_get_num_teams();
   }
 
-  // CHECK: PASS
   if (num_threads[0] == N)
+    // CHECK: PASS
     printf("PASS\n");
   else
+    // DEFAULT: FAIL
     printf("FAIL: num_threads: %d\n != N: %d", num_threads[0], N);
   return 0;
 }
