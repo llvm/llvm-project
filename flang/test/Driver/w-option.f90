facbbc32905a1b81776187397e1f5ab378b66134
Kiran Chandramohan <kiran.chandramohan@arm.com>
2024-05-07 19:44:47 +0000

    Address review comments and rebase

--- flang/test/Driver/w-option.f90
+++ flang/test/Driver/w-option.f90
@@ -1,13 +1,21 @@
-! RUN: %flang -c %s 2>&1 | FileCheck %s
-! RUN: %flang -c -w %s 2>&1 | FileCheck --allow-empty %s -check-prefix=CHECK-W
-! RUN: %flang -c -pedantic %s 2>&1 | FileCheck %s -check-prefixes=CHECK,CHECK-PORT
-! RUN: %flang -c -pedantic -w %s 2>&1 | FileCheck --allow-empty %s -check-prefixes=CHECK-W,CHECK-PORT-W
-! RUN: %flang -c -w -pedantic %s 2>&1 | FileCheck  --allow-empty %s -check-prefixes=CHECK-W,CHECK-PORT-W
-! CHECK: warning: Label '40' is in a construct that should not be used as a branch target here
-! CHECK: warning: Label '50' is in a construct that should not be used as a branch target here
-! CHECK-W-NOT: warning
-! CHECK-PORT: portability: Statement function 'sf1' should not contain an array constructor
-! CHECK-PORT-W-NOT: portability
+! Test the default setting. Emit warnings only.
+! RUN: %flang -c %s 2>&1 | FileCheck %s -check-prefix=DEFAULT
+
+! Test that the warnings are not generated with `-w` option.
+! RUN: %flang -c -w %s 2>&1 | FileCheck --allow-empty %s -check-prefix=WARNING
+
+! Test that warnings are portability messages are generated.
+! RUN: %flang -c -pedantic %s 2>&1 | FileCheck %s -check-prefixes=DEFAULT,PORTABILITY
+
+! Test that warnings and portability messages are not generated.
+! TODO: Support the last flag wins behaviour.
+! RUN: %flang -c -pedantic -w %s 2>&1 | FileCheck --allow-empty %s -check-prefixes=WARNING,PORTABILITY-WARNING
+! RUN: %flang -c -w -pedantic %s 2>&1 | FileCheck --allow-empty %s -check-prefixes=WARNING,PORTABILITY-WARNING
+! DEFAULT: warning: Label '40' is in a construct that should not be used as a branch target here
+! DEFAULT: warning: Label '50' is in a construct that should not be used as a branch target here
+! WARNING-NOT: warning
+! PORTABILITY: portability: Statement function 'sf1' should not contain an array constructor
+! PORTABILITY-WARNING-NOT: portability
 
 subroutine sub01(n)
   integer n
