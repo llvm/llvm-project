682ce7f7a555d526c717c12defabd7f6c499c5bd
Krzysztof Parzyszek <Krzysztof.Parzyszek@amd.com>
2024-05-07 08:54:02 -0500

    Add comment to `buildConstructQueue`

--- flang/lib/Lower/OpenMP/Decomposer.h
+++ flang/lib/Lower/OpenMP/Decomposer.h
@@ -35,6 +35,11 @@ using ConstructQueue = List<UnitConstruct>;
 LLVM_DUMP_METHOD llvm::raw_ostream &operator<<(llvm::raw_ostream &os,
                                                const UnitConstruct &uc);
 
+// Given a potentially compound construct with a list of clauses that
+// apply to it, break it up into individual sub-constructs each with
+// the subset of applicable clauses (plus implicit clauses, if any).
+// From that create a work queue where each work item corresponds to
+// the sub-construct with its clauses.
 ConstructQueue buildConstructQueue(mlir::ModuleOp modOp,
                                    semantics::SemanticsContext &semaCtx,
                                    lower::pft::Evaluation &eval,
