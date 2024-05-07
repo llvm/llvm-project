e34245c58464a3145641427277d0df6adcf533ee
Peter Waller <peter.waller@arm.com>
2024-05-07 13:06:00 +0100

    [llvm-mca] Make bad-input.s even more CPU specific
    Note: This patch is distinct from the previous one titled
      "[llvm-mca] Move bad-input.s test to be target specific"
    
    This is a followup to #90474 and commit
    afc10fc9b7ce3d23d9012f5a1496e849fe873ba2
    
    Context: Builders failing because they're unable to run the failure
    test.
    
    This still doesn't work in various circumstances, it seems MCA doesn't
    want to run on a wide variety of hosts in various configurations, so
    stick to the tried and tested method and pass -mtriple and -mcpu.

--- llvm/test/tools/llvm-mca/X86/bad-input.s
+++ llvm/test/tools/llvm-mca/X86/bad-input.s
@@ -1,8 +1,10 @@
-# RUN: not llvm-mca %s -o /dev/null 2>&1 | FileCheck --check-prefixes=CHECK-ALL,CHECK %s
-# RUN: not llvm-mca -skip-unsupported-instructions=none %s -o /dev/null 2>&1 | FileCheck --check-prefixes=CHECK-ALL,CHECK %s
-# RUN: not llvm-mca -skip-unsupported-instructions=lack-sched %s -o /dev/null 2>&1 | FileCheck --check-prefixes=CHECK-ALL,CHECK %s
-# RUN: not llvm-mca -skip-unsupported-instructions=parse-failure %s -o /dev/null 2>&1 | FileCheck --check-prefixes=CHECK-ALL,CHECK-SKIP %s
-# RUN: not llvm-mca -skip-unsupported-instructions=any %s -o /dev/null 2>&1 | FileCheck --check-prefixes=CHECK-ALL,CHECK-SKIP %s
+# This test is generic but not all builders have an llvm-mca which can run natively.
+
+# RUN: not llvm-mca -mtriple=x86_64 -mcpu=x86-64 %s -o /dev/null 2>&1 | FileCheck --check-prefixes=CHECK-ALL,CHECK %s
+# RUN: not llvm-mca -mtriple=x86_64 -mcpu=x86-64 -skip-unsupported-instructions=none %s -o /dev/null 2>&1 | FileCheck --check-prefixes=CHECK-ALL,CHECK %s
+# RUN: not llvm-mca -mtriple=x86_64 -mcpu=x86-64 -skip-unsupported-instructions=lack-sched %s -o /dev/null 2>&1 | FileCheck --check-prefixes=CHECK-ALL,CHECK %s
+# RUN: not llvm-mca -mtriple=x86_64 -mcpu=x86-64 -skip-unsupported-instructions=parse-failure %s -o /dev/null 2>&1 | FileCheck --check-prefixes=CHECK-ALL,CHECK-SKIP %s
+# RUN: not llvm-mca -mtriple=x86_64 -mcpu=x86-64 -skip-unsupported-instructions=any %s -o /dev/null 2>&1 | FileCheck --check-prefixes=CHECK-ALL,CHECK-SKIP %s
 
 # Test checks that MCA does not produce a total cycles estimate if it encounters parse errors.
 
