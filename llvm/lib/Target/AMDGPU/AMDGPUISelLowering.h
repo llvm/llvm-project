02799f53b32e6f656e8b39105ba8a56e15815de2
Matt Arsenault <Matthew.Arsenault@amd.com>
2024-05-07 21:43:22 +0200

    AMDGPU: Don't bitcast float typed atomic store in IR (#90116)
    Implement the promotion in the DAG.
    
    Depends #90113

--- llvm/lib/Target/AMDGPU/AMDGPUISelLowering.h
+++ llvm/lib/Target/AMDGPU/AMDGPUISelLowering.h
@@ -236,6 +236,10 @@ public:
     return AtomicExpansionKind::None;
   }
 
+  AtomicExpansionKind shouldCastAtomicStoreInIR(StoreInst *SI) const override {
+    return AtomicExpansionKind::None;
+  }
+
   AtomicExpansionKind shouldCastAtomicRMWIInIR(AtomicRMWInst *) const override {
     return AtomicExpansionKind::None;
   }
