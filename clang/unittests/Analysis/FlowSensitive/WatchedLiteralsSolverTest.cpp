7e91b880e83065cb6ea9e14d73f7f7e705e88fad
Martin Braenne <mboehme@google.com>
2024-05-08 10:30:43 +0000

    fixup! [clang][dataflow] Make `SolverTest` a type-parameterized test.

--- clang/unittests/Analysis/FlowSensitive/WatchedLiteralsSolverTest.cpp
+++ clang/unittests/Analysis/FlowSensitive/WatchedLiteralsSolverTest.cpp
@@ -1,4 +1,4 @@
-//===- unittests/Analysis/FlowSensitive/SolverTest.cpp --------------------===//
+//===- unittests/Analysis/FlowSensitive/WatchedLiteralsSolverTest.cpp -----===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
