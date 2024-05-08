32b68f274244cc866d7edf5a83fffcb4ba0bec19
Xiaoyang Liu <siujoeng.lau@gmail.com>
2024-05-08 13:22:10 -0400

    [libc++][ranges] implement 'ranges::elements_of'

--- libcxx/include/__ranges/elements_of.h
+++ libcxx/include/__ranges/elements_of.h
@@ -31,7 +31,11 @@ namespace ranges {
 template <range _Range, class _Allocator = allocator<byte>>
 struct elements_of {
   _LIBCPP_NO_UNIQUE_ADDRESS _Range range;
-  _LIBCPP_NO_UNIQUE_ADDRESS _Allocator allocator = _Allocator();
+  _LIBCPP_NO_UNIQUE_ADDRESS _Allocator allocator;
+
+  // This explicit constructor is required because AppleClang 15 hasn't implement P0960R3
+  explicit elements_of(_Range&& __range, _Allocator __allocator = _Allocator())
+      : range(std::move(__range)), allocator(std::move(__allocator)) {}
 };
 
 template <class _Range, class _Allocator = allocator<byte>>
