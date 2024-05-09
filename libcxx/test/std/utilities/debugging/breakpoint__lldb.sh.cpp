b9d83ef2e37b83f352ba2d8fce32dfdc316dc0f3
Hristo Hristov <hghristov.rmm@gmail.com>
2024-05-09 15:54:10 +0300

    Added `breakpoint()` and `breakpoint_if_debugging()` tests

---
+++ libcxx/test/std/utilities/debugging/breakpoint__lldb.sh.cpp
@@ -0,0 +1,34 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++03, c++11, c++14, c++17, c++20, c++23
+// UNSUPPORTED: libcpp-has-no-incomplete-debugging
+// XFAIL: LIBCXX-PICOLIBC-FIXME
+
+// RUN: %{cxx} %{flags} %s -o %t.exe %{compile_flags} -g %{link_flags}
+// RUN: "%{lldb}" %t.exe -o "command source %S/breakpoint__lldb.cmd" \
+// RUN:   | grep -qFf %S/breakpoint__lldb.grep
+
+// <debugging>
+
+// void breakpoint() noexcept;
+
+#include <debugging>
+
+void test() {
+  static_assert(noexcept(std::breakpoint()));
+
+  std::breakpoint();
+}
+
+int main(int, char**) {
+  test();
+
+  return 0;
+}
+
---
+++ libcxx/test/std/utilities/debugging/breakpoint_if_debugging__gdb.sh.cpp
@@ -0,0 +1,33 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++03, c++11, c++14, c++17, c++20, c++23
+// UNSUPPORTED: libcpp-has-no-incomplete-debugging
+// XFAIL: LIBCXX-PICOLIBC-FIXME
+
+// RUN: %{cxx} %{flags} %s -o %t.exe %{compile_flags} -g %{link_flags}
+// RUN: "%{gdb}" %t.exe -ex "source %S/breakpoint_if_debugging__gdb.cmd" \
+// RUN:   | grep -qFf %S/breakpoint_if_debugging__gdb.grep
+
+// <debugging>
+
+// void breakpoint() noexcept;
+
+#include <debugging>
+
+void test() {
+  static_assert(noexcept(std::breakpoint_if_debugging()));
+
+  std::breakpoint_if_debugging();
+}
+
+int main(int, char**) {
+  test();
+
+  return 0;
+}
