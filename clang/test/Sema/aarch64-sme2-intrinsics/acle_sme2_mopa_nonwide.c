f27c39460f31e6405a1ca6ac692d89fa057b5488
Momchil Velikov <momchil.velikov@arm.com>
2024-05-09 17:08:13 +0100

    [fixup] Fix test RUN lines

--- clang/test/Sema/aarch64-sme2-intrinsics/acle_sme2_mopa_nonwide.c
+++ clang/test/Sema/aarch64-sme2-intrinsics/acle_sme2_mopa_nonwide.c
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -triple aarch64-none-linux-gnu -target-feature +sme -fsyntax-only -verify -emit-llvm %s
+// RUN: %clang_cc1 -triple aarch64-none-linux-gnu -target-feature +sme -verify -emit-llvm %s
 
 // REQUIRES: aarch64-registered-target
 
