4349c6fb5da82c995c4b5323b78440edbc197f7e
agozillon <Andrew.Gozillon@amd.com>
2024-05-10 14:42:45 -0500

    [Offload] Fix new fortran OpenMP offload test failures via addition of requires

--- offload/test/offloading/fortran/target-map-nested-dtype-complex-member.f90
+++ offload/test/offloading/fortran/target-map-nested-dtype-complex-member.f90
@@ -1,6 +1,7 @@
 ! Offloading test checking interaction of an
 ! nested derived type member map of a complex
 ! number member
+! REQUIRES: flang, amdgcn-amd-amdhsa
 ! UNSUPPORTED: nvptx64-nvidia-cuda
 ! UNSUPPORTED: nvptx64-nvidia-cuda-LTO
 ! UNSUPPORTED: aarch64-unknown-linux-gnu
