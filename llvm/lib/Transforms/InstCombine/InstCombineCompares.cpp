60d8a18ae0caed5a3327b87932ade531cb3cba87
SahilPatidar <patidarsahil2001@gmail.com>
2024-05-11 11:11:10 +0530

    fixes comment

--- llvm/lib/Transforms/InstCombine/InstCombineCompares.cpp
+++ llvm/lib/Transforms/InstCombine/InstCombineCompares.cpp
@@ -8086,7 +8086,7 @@ Instruction *InstCombinerImpl::visitFCmpInst(FCmpInst &I) {
       case FCmpInst::FCMP_OEQ:
       case FCmpInst::FCMP_OGE:
       case FCmpInst::FCMP_OLE:
-        // fsub x, y --> isnnan(x, y) && isninf(x, y)
+        // Skip optimization: fsub x, y guaranteed isInf(x, y).
         if (!LHSI->hasNoNaNs() && !LHSI->hasNoInfs() &&
             !isKnownNeverInfinity(LHSI->getOperand(1), /*Depth=*/0,
                                   getSimplifyQuery().getWithInstruction(&I)) &&
