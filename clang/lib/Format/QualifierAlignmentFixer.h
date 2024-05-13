5feb2091b6a45775f94a512071a4a8a16d314f2e
Owen Pan <owenpiano@gmail.com>
2024-05-12 17:23:19 -0700

    Revert "[clang-format] Fix buildbot failures"
    This reverts commit 294f372da771d6f49fdb06450e91280f93314a78, which caused a
    buildbot failure:
    https://lab.llvm.org/buildbot/#/builders/5/builds/43322

--- clang/lib/Format/QualifierAlignmentFixer.h
+++ clang/lib/Format/QualifierAlignmentFixer.h
@@ -72,11 +72,11 @@ public:
 
   // Is the Token a simple or qualifier type
   static bool isQualifierOrType(const FormatToken *Tok,
-                                const LangOptions *LangOpts = nullptr);
+                                const LangOptions &LangOpts);
   static bool
   isConfiguredQualifierOrType(const FormatToken *Tok,
                               const std::vector<tok::TokenKind> &Qualifiers,
-                              const LangOptions *LangOpts = nullptr);
+                              const LangOptions &LangOpts);
 
   // Is the Token likely a Macro
   static bool isPossibleMacro(const FormatToken *Tok);
