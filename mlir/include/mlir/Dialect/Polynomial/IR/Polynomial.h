65cbc36c39a6e4c242c00e1bbe553937b6424631
Jeremy Kun <j2kun@users.noreply.github.com>
2024-05-13 13:39:03 -0700

    clang-format

--- mlir/include/mlir/Dialect/Polynomial/IR/Polynomial.h
+++ mlir/include/mlir/Dialect/Polynomial/IR/Polynomial.h
@@ -109,7 +109,7 @@ class PolynomialBase {
 public:
   PolynomialBase() = delete;
 
-  explicit PolynomialBase(ArrayRef<Monomial> terms) : terms(terms){};
+  explicit PolynomialBase(ArrayRef<Monomial> terms) : terms(terms) {};
 
   explicit operator bool() const { return !terms.empty(); }
   bool operator==(const PolynomialBase &other) const {
