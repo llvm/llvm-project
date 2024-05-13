68b541906c5238b9165702c5623a00c877b53cbf
Balazs Benics <benicsbalazs@gmail.com>
2024-05-13 09:17:13 +0200

    Track the LHS of assignments for deref bugs
    This adds notes for the "definition" lines for the dereferenced
    variables that are raised for assignment expressions.

--- clang/test/Analysis/gh-issue-89185.c
+++ clang/test/Analysis/gh-issue-89185.c
@@ -1,13 +1,13 @@
-// RUN: %clang_analyze_cc1 -analyzer-checker=core,debug.ExprInspection -verify %s
+// RUN: %clang_analyze_cc1 -analyzer-checker=core,debug.ExprInspection -analyzer-output text -verify %s 
 
-void clang_analyzer_dump(char);
-void clang_analyzer_dump_ptr(char*);
+void clang_analyzer_warnIfReached(void);
 
 // https://github.com/llvm/llvm-project/issues/89185
 void binding_to_label_loc() {
-  char *b = &&MyLabel;
+  char *b = &&MyLabel; // expected-note {{'b' initialized here}}
 MyLabel:
-  *b = 0; // expected-warning {{Dereference of the address of a label}}
-  clang_analyzer_dump_ptr(b);
-  clang_analyzer_dump(*b);
+  *b = 0;
+  // expected-warning@-1 {{Dereference of the address of a label}}
+  // expected-note@-2    {{Dereference of the address of a label}}
+  clang_analyzer_warnIfReached(); // no-warning: Unreachable due to fatal error.
 }
