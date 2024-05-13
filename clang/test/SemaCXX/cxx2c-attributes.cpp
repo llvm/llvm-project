da3c68333a544a1354c8870798d394fa20047f82
Corentin Jabot <corentinjabot@gmail.com>
2024-05-13 20:00:49 +0200

    Fix tests

--- clang/test/SemaCXX/cxx2c-attributes.cpp
+++ clang/test/SemaCXX/cxx2c-attributes.cpp
@@ -1,8 +1,9 @@
-// RUN: %clang_cc1 -x c++ -std=c++11 -triple x86_64-pc-linux -fsyntax-only
-// RUN: %clang_cc1 -x c++ -std=c++11 -triple x86_64-windows-msvc -fsyntax-only
+// RUN: %clang_cc1 %s -x c++ -std=c++11 -triple x86_64-pc-linux -fsyntax-only -verify -Wno-c++17-extensions
+// RUN: %clang_cc1 %s -x c++ -std=c++11 -triple x86_64-windows-msvc -fsyntax-only -verify=msvc -Wno-c++17-extensions
+// expected-no-diagnostics
 
 // Check we return non-zero values for supported attributes as per
-// wg21.link/p2552r3.pdf
+// wg21.link/P2552
 static_assert(__has_cpp_attribute(assume));
 
 // The standard does not prescribe a behavior for [[carries_dependency]]
@@ -15,9 +16,5 @@ static_assert(__has_cpp_attribute(maybe_unused));
 static_assert(__has_cpp_attribute(nodiscard));
 static_assert(__has_cpp_attribute(noreturn));
 
-#ifdef _MSC_VER
 // We do not support [[no_unique_address]] in MSVC emulation mode
-static_assert(__has_cpp_attribute(no_unique_address) == 0);
-#else
-static_assert(__has_cpp_attribute(no_unique_address));
-#endif
+static_assert(__has_cpp_attribute(no_unique_address)); // msvc-error {{static assertion failed}}
