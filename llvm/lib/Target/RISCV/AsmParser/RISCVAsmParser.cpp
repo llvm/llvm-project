6398ae305ab2230060de0abde7661710aaebd0da
Palmer Dabbelt <palmer@dabbelt.com>
2024-05-14 10:11:20 -0700

    [RISCV] Only allow up to e64 in vsetvli (#92010)
    These larger SEWs aren't in the ratified V spec. Thanks to dzaima and
    sorear on IRC for pointing this one out.
    
    Signed-off-by: Palmer Dabbelt <palmer@rivosinc.com>

--- llvm/lib/Target/RISCV/AsmParser/RISCVAsmParser.cpp
+++ llvm/lib/Target/RISCV/AsmParser/RISCVAsmParser.cpp
@@ -2225,7 +2225,7 @@ bool RISCVAsmParser::generateVTypeError(SMLoc ErrorLoc) {
   return Error(
       ErrorLoc,
       "operand must be "
-      "e[8|16|32|64|128|256|512|1024],m[1|2|4|8|f2|f4|f8],[ta|tu],[ma|mu]");
+      "e[8|16|32|64],m[1|2|4|8|f2|f4|f8],[ta|tu],[ma|mu]");
 }
 
 ParseStatus RISCVAsmParser::parseMaskReg(OperandVector &Operands) {
