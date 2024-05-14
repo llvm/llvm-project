504bff6995bdf21ea1903995938c8ebe7f61497f
Peter Klausler <pklausler@nvidia.com>
2024-05-14 10:59:12 -0700

    [flang] Parse REDUCE clauses in !$CUF KERNEL DO
    A !$CUF KERNEL DO directive is allowed to have advisory REDUCE
    clauses similar to those in OpenACC and DO CONCURRENT.  Parse
    and represent them.  Semantic validation will follow.

--- flang/lib/Semantics/resolve-names.cpp
+++ flang/lib/Semantics/resolve-names.cpp
@@ -8941,7 +8941,7 @@ bool ResolveNamesVisitor::Pre(const parser::ProgramUnit &x) {
   FinishSpecificationParts(root);
   ResolveExecutionParts(root);
   FinishExecutionParts(root);
-  ResolveAccParts(context(), x);
+  ResolveAccParts(context(), x, /*topScope=*/nullptr);
   ResolveOmpParts(context(), x);
   return false;
 }
