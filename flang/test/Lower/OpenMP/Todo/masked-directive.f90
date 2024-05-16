893c1ea59623e04d831cfd48edc68244c3228598
Anchu Rajendran <asudhaku@amd.com>
2024-05-16 01:02:38 -0500

    R7: Fixing the typo

--- flang/test/Lower/OpenMP/Todo/masked-directive.f90
+++ flang/test/Lower/OpenMP/Todo/masked-directive.f90
@@ -1,11 +1,9 @@
-
 ! This test checks lowering of OpenMP masked Directive.
 
 ! RUN: %not_todo_cmd bbc -emit-fir -fopenmp -o - %s 2>&1 | FileCheck %s
 ! RUN: %not_todo_cmd %flang_fc1 -emit-fir -fopenmp -o - %s 2>&1 | FileCheck %s
 
-! CHECK: not yet implemented: Unhandled Directive masked
-
+! CHECK: not yet implemented: Unhandled directive masked
 subroutine test_masked()
   integer :: c = 1
   !$omp masked
