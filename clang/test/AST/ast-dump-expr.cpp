21077adfefd5a255735cb7f6f0bb690a1e880e62
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2024-05-16 09:27:25 +0200

    Move the AST dump test to the conventional file

--- clang/test/AST/ast-dump-expr.cpp
+++ clang/test/AST/ast-dump-expr.cpp
@@ -583,3 +583,8 @@ void NonADLCall3() {
   f(x);
 }
 } // namespace test_adl_call_three
+
+struct Sock {};
+void leakNewFn() { new struct Sock; }
+// CHECK: CXXNewExpr {{.*}} <col:20, col:31> 'struct Sock *'
+
--- clang/test/Misc/ast-source-ranges.cpp
+++
@@ -1,5 +0,0 @@
-// RUN: %clang_cc1 -ast-dump %s  2>&1 | FileCheck %s
-
-struct Sock {};
-void leakNewFn() { new struct Sock; }
-// CHECK: CXXNewExpr {{.*}} <col:20, col:31> 'struct Sock *'
