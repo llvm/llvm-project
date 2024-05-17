751e22cc0a277cd3f3c9c331f69ac441f8763fde
changkhothuychung <nhat7203@gmail.com>
2024-05-16 23:26:58 -0400

    some changes

--- libc/src/unistd/pathconf_utils.h
+++ libc/src/unistd/pathconf_utils.h
@@ -1,5 +1,4 @@
-//===-- Implementation header for pathconf_utils ----------------------*- C++
-//-*-===//
+//===-- Implementation header for pathconf_utils ----------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
