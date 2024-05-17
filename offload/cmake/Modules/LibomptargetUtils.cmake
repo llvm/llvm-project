fb7fdb33df23ee96ba9e1c45a735499ba7693910
Joseph Huber <huberjn@outlook.com>
2024-05-17 09:05:03 -0500

    [Offload][NFC] Remove header license in CMake files (#92544)
    Summary:
    No other project has these in the CMake itself, and they're wildly
    inconsistent even within the project. These don't really add anything so
    I think they should be removed.

--- offload/cmake/Modules/LibomptargetUtils.cmake
+++ offload/cmake/Modules/LibomptargetUtils.cmake
@@ -1,13 +1,3 @@
-#
-#//===----------------------------------------------------------------------===//
-#//
-#// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-#// See https://llvm.org/LICENSE.txt for license information.
-#// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-#//
-#//===----------------------------------------------------------------------===//
-#
-
 # void libomptarget_say(string message_to_user);
 # - prints out message_to_user
 macro(libomptarget_say message_to_user)
