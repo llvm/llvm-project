447ff7ddea68df64890c077bb30382bfabaf3d30
Fangrui Song <i@maskray.me>
2024-05-17 23:49:01 -0700

    [MCAsmParser] Improve .rept/.irp tests

--- llvm/test/MC/AsmParser/macro-rept.s
+++ llvm/test/MC/AsmParser/macro-rept.s
@@ -1,5 +1,7 @@
-# RUN: llvm-mc -triple x86_64 %s | FileCheck %s
+# RUN: rm -rf %t && split-file %s %t && cd %t
+# RUN: llvm-mc -triple=x86_64 a.s | FileCheck %s
 
+#--- a.s
 .rept 2
     .long 1
 .endr
@@ -23,3 +25,21 @@
 # CHECK-NEXT: .long 0
 # CHECK-NEXT: .long 0
 # CHECK-NEXT: .long 42
+
+# RUN: not llvm-mc -triple=x86_64 err1.s 2>&1 | FileCheck %s --check-prefix=ERR1
+# ERR1: .s:1:6: error: unmatched '.endr' directive
+#--- err1.s
+.endr
+
+# RUN: not llvm-mc -triple=x86_64 err2.s 2>&1 | FileCheck %s --check-prefix=ERR2
+# ERR2: .s:1:1: error: no matching '.endr' in definition
+#--- err2.s
+.rept 3
+.long
+
+# RUN: not llvm-mc -triple=x86_64 err3.s 2>&1 | FileCheck %s --check-prefix=ERR3
+# ERR3: .s:3:7: error: expected newline
+#--- err3.s
+.rept 1
+.long 0
+.endr ab
