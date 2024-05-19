7fc524fe080a69e79bd1ce8925e680350b7e9d44
Vitaly Buka <vitalybuka@google.com>
2024-05-19 02:43:55 -0700

    [ctx_profile] Pass lib path into test
    Fixes build after cfe9deb1353021a1c1fe4731ec3e90f702dbd43d on
    https://lab.llvm.org/buildbot/#/builders/37/builds/34828

--- compiler-rt/test/ctx_profile/lit.cfg.py
+++ compiler-rt/test/ctx_profile/lit.cfg.py
@@ -33,3 +33,10 @@ config.suffixes = [".c", ".cpp", ".test"]
 config.substitutions.append(
     ("%clangxx ", " ".join([config.clang] + config.cxx_mode_flags) + " -ldl -lpthread ")
 )
+
+config.substitutions.append(
+    (
+        "%ctxprofilelib",
+        "-L%s -lclang_rt.ctx_profile%s" % (config.compiler_rt_libdir, config.target_suffix)
+    )
+)
