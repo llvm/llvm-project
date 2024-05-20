e6b99223c4d74c08741400eb3e2c0c651a10c360
Vlad Serebrennikov <serebrennikov.vladislav@gmail.com>
2024-05-20 20:17:14 +0300

    Fix `SemaRISCV.cpp` file header again

--- clang/lib/Sema/SemaRISCV.cpp
+++ clang/lib/Sema/SemaRISCV.cpp
@@ -1,4 +1,4 @@
-//==------- SemaRISCV.cpp ------- RISC-V target-specific routines ----------==//
+//===------ SemaRISCV.cpp ------- RISC-V target-specific routines ---------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
