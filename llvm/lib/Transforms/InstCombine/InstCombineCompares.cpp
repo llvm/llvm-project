a18a17ff36240e43f2f0a4c519b9b0f9e9da9201
SahilPatidar <patidarsahil2001@gmail.com>
2024-05-20 09:23:38 +0530

    Fixes clang-format

--- llvm/lib/Transforms/InstCombine/InstCombineCompares.cpp
+++ llvm/lib/Transforms/InstCombine/InstCombineCompares.cpp
@@ -8088,11 +8088,12 @@ Instruction *InstCombinerImpl::visitFCmpInst(FCmpInst &I) {
       case FCmpInst::FCMP_OLE:
         // Skip optimization: fsub x, y unless guaranteed !isinf(x) ||
         // !isinf(y).
-        if (!LHSI->hasOneUse() || (!LHSI->hasNoNaNs() && !LHSI->hasNoInfs() &&
-            !isKnownNeverInfinity(LHSI->getOperand(1), /*Depth=*/0,
-                                  getSimplifyQuery().getWithInstruction(&I)) &&
-            !isKnownNeverInfinity(LHSI->getOperand(0), /*Depth=*/0,
-                                  getSimplifyQuery().getWithInstruction(&I))))
+        if (!LHSI->hasOneUse() ||
+            (!LHSI->hasNoNaNs() && !LHSI->hasNoInfs() &&
+             !isKnownNeverInfinity(LHSI->getOperand(1), /*Depth=*/0,
+                                   getSimplifyQuery().getWithInstruction(&I)) &&
+             !isKnownNeverInfinity(LHSI->getOperand(0), /*Depth=*/0,
+                                   getSimplifyQuery().getWithInstruction(&I))))
           break;
 
         [[fallthrough]];
