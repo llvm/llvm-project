d4ed4ab52ab708d3e3009a8a0122a41a49d662c3
Vlad Serebrennikov <serebrennikov.vladislav@gmail.com>
2024-05-20 06:19:01 +0300

    Remove OpenMP tests A-C

--- flang/test/SemanticsChecked/OpenMP/compiler-directive.f90
+++
@@ -1,8 +0,0 @@
-! RUN: %python %S/../test_errors.py %s %flang -fopenmp
-! CompilerDirective with openmp tests
-
-!ERROR: !DIR$ IGNORE_TKR directive must appear in a subroutine or function
-!dir$ ignore_tkr
-
-program main
-end program main
--- flang/test/SemanticsChecked/OpenMP/critical-empty.f90
+++
@@ -1,6 +0,0 @@
-! RUN: %python %S/../test_errors.py %s %flang_fc1 -fopenmp 
-! Test that there are no errors for an empty critical construct
-
-!$omp critical
-!$omp end critical
-end
