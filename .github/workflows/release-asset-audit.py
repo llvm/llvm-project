ab0cea7d95f6313bebf57a2984d1593800ba149c
Tom Stellard <tstellar@redhat.com>
2024-05-20 09:25:07 -0700

    workflows: Add a job for auditing release assets
    This checks to ensure that uploads are only made by 'approved'
    uploaders, which is just everyone who has uploaded a release asset
    in the past.
    
    We could do more, but this is just a simple implementation so we
    can put something in place and see how it works.
    
    For more discussion see:
    https://discourse.llvm.org/t/rfc-improve-binary-security/78121

---
+++ .github/workflows/release-asset-audit.py
@@ -0,0 +1,40 @@
+import github
+import sys
+
+token = sys.argv[1]
+
+gh = github.Github(login_or_token=token)
+repo = gh.get_repo('llvm/llvm-project')
+
+uploaders = set([
+    'DimitryAndric',
+    'stefanp-ibm',
+    'lei137',
+    'omjavaid',
+    'nicolerabjohn',
+    'amy-kwan',
+    'mandlebug',
+    'zmodem',
+    'androm3da',
+    'tru',
+    'rovka',
+    'rorth',
+    'quinnlp',
+    'kamaub',
+    'abrisco',
+    'jakeegan',
+    'maryammo',
+    'tstellar',
+    'github-actions[bot]'
+])
+
+for release in repo.get_releases():
+    print("Release:", release.title)
+    for asset in release.get_assets():
+        created_at = asset.created_at
+        updated_at = "" if asset.created_at == asset.updated_at else asset.updated_at
+        print(f'{asset.name} : {asset.uploader.login} [{created_at} {updated_at}] ( {asset.download_count} )')
+        if asset.uploader.login not in uploaders:
+          print("Invalid uploader")
+          sys.exit(1)
+
