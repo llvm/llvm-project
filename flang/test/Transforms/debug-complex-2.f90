9725619e4d3523a304d431a8b248a04a7e33bed8
Abid Qadeer <haqadeer@amd.com>
2024-05-20 11:32:51 +0100

    Move LLVM IR tests to integration folder. (NFC)

--- flang/test/Transforms/debug-complex-2.f90
+++ flang/test/Transforms/debug-complex-2.f90
@@ -1,6 +1,5 @@
 ! RUN: %flang_fc1 -emit-fir -debug-info-kind=standalone -mmlir --mlir-print-debuginfo %s -o - | \
 ! RUN: fir-opt --cg-rewrite="preserve-declare=true" --mlir-print-debuginfo | fir-opt --add-debug-info --mlir-print-debuginfo | FileCheck %s
-! RUN: %flang_fc1 -emit-llvm -debug-info-kind=standalone %s -o - | FileCheck  --check-prefix=LLVM %s
 
 program mn
   complex(kind=4) :: c4
@@ -27,13 +26,3 @@ end program
 ! CHECK-DAG: #llvm.di_local_variable<{{.*}}name = "a"{{.*}}type = #[[CMPX4]]>
 ! CHECK-DAG: #llvm.di_local_variable<{{.*}}name = "b"{{.*}}type = #[[CMPX8]]>
 ! CHECK-DAG: #llvm.di_local_variable<{{.*}}name = "c"{{.*}}type = #[[CMPX16]]>
-
-! LLVM-DAG: ![[C4:.*]] = !DIBasicType(name: "complex", size: 64, encoding: DW_ATE_complex_float)
-! LLVM-DAG: ![[C8:.*]] = !DIBasicType(name: "complex", size: 128, encoding: DW_ATE_complex_float)
-! LLVM-DAG: ![[C16:.*]] = !DIBasicType(name: "complex", size: 256, encoding: DW_ATE_complex_float)
-! LLVM-DAG: !DILocalVariable(name: "c4"{{.*}}type: ![[C4]])
-! LLVM-DAG: !DILocalVariable(name: "c8"{{.*}}type: ![[C8]])
-! LLVM-DAG: !DILocalVariable(name: "r"{{.*}}type: ![[C16]])
-! LLVM-DAG: !DILocalVariable(name: "a"{{.*}}type: ![[C4]])
-! LLVM-DAG: !DILocalVariable(name: "b"{{.*}}type: ![[C8]])
-! LLVM-DAG: !DILocalVariable(name: "c"{{.*}}type: ![[C16]])
