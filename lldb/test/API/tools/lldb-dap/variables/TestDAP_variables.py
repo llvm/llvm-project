b49a9b5945c0621db9b8c2f9e24dadc00bbca973
Pavel Labath <pavel@labath.sk>
2024-05-21 10:37:41 +0200

    [lldb-dap] Don't send expanded descriptions for "hover" expressions (#92726)
    VSCode will automatically ask for the children (in structured form) so
    there's no point in sending the textual representation. This can make
    displaying hover popups for complex variables with complicated data
    formatters much faster. See discussion on #77026 for context.

--- lldb/test/API/tools/lldb-dap/variables/TestDAP_variables.py
+++ lldb/test/API/tools/lldb-dap/variables/TestDAP_variables.py
@@ -502,29 +502,12 @@ class TestDAP_variables(lldbdap_testcase.DAPTestCaseBase):
                 },
                 "hover": {
                     "equals": {"type": "PointType"},
-                    "equals": {
-                        "result": """(PointType) pt = {
-  x = 11
-  y = 22
-  buffer = {
-    [0] = 0
-    [1] = 1
-    [2] = 2
-    [3] = 3
-    [4] = 4
-    [5] = 5
-    [6] = 6
-    [7] = 7
-    [8] = 8
-    [9] = 9
-    [10] = 10
-    [11] = 11
-    [12] = 12
-    [13] = 13
-    [14] = 14
-    [15] = 15
-  }
-}"""
+                    "startswith": {
+                        "result": (
+                            "{x:11, y:22, buffer:{...}}"
+                            if enableAutoVariableSummaries
+                            else "PointType @ 0x"
+                        )
                     },
                     "missing": ["indexedVariables"],
                     "hasVariablesReference": True,
