0b9155faf2dc063f81091c8e78d14dd4446c2db5
Paul Kirth <paulkirth@google.com>
2024-05-21 09:58:46 -0700

    Fix fallthrough and update comment
    Created using spr 1.3.4

--- lld/ELF/InputFiles.cpp
+++ lld/ELF/InputFiles.cpp
@@ -833,14 +833,15 @@ void ObjFile<ELFT>::initializeSections(bool ignoreComdats,
           createInputSection(i, sec, check(obj.getSectionName(sec, shstrtab)));
       break;
     case SHT_LLVM_LTO:
-      // When doing a relocatable link with FatLTO objects, if we're not using
-      // the bitcode, discard it, since it will be concatenated together when
-      // handling orphan sections, and which will be an invalid bitcode object.
+      // Discard .llvm.lto in a relocatable link that does not use the bitcode.
+      // The concatenated output does not properly reflect the linking
+      // semantics. In addition, since we do not use the bitcode wrapper format,
+      // the concatenated raw bitcode would be invalid.
       if (config->relocatable && !config->fatLTOObjects) {
         sections[i] = &InputSection::discarded;
         break;
       }
-      LLVM_FALLTHROUGH;
+      [[fallthrough]];
     default:
       this->sections[i] =
           createInputSection(i, sec, check(obj.getSectionName(sec, shstrtab)));
