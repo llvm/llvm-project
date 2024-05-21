96107099ef213e59692abbf090b5ecf85abe11ad
Tom Stellard <tstellar@redhat.com>
2024-05-20 17:30:09 -0700

    Move everything into a main function

--- .github/workflows/release-asset-audit.py
+++ .github/workflows/release-asset-audit.py
@@ -1,43 +1,49 @@
 import github
 import sys
 
-token = sys.argv[1]
+def main():
 
-gh = github.Github(login_or_token=token)
-repo = gh.get_repo("llvm/llvm-project")
+    token = sys.argv[1]
 
-uploaders = set(
-    [
-        "DimitryAndric",
-        "stefanp-ibm",
-        "lei137",
-        "omjavaid",
-        "nicolerabjohn",
-        "amy-kwan",
-        "mandlebug",
-        "zmodem",
-        "androm3da",
-        "tru",
-        "rovka",
-        "rorth",
-        "quinnlp",
-        "kamaub",
-        "abrisco",
-        "jakeegan",
-        "maryammo",
-        "tstellar",
-        "github-actions[bot]",
-    ]
-)
+    gh = github.Github(login_or_token=token)
+    repo = gh.get_repo("llvm/llvm-project")
 
-for release in repo.get_releases():
-    print("Release:", release.title)
-    for asset in release.get_assets():
-        created_at = asset.created_at
-        updated_at = "" if asset.created_at == asset.updated_at else asset.updated_at
-        print(
-            f"{asset.name} : {asset.uploader.login} [{created_at} {updated_at}] ( {asset.download_count} )"
-        )
-        if asset.uploader.login not in uploaders:
-            print("Invalid uploader")
-            sys.exit(1)
+    uploaders = set(
+        [
+            "DimitryAndric",
+            "stefanp-ibm",
+            "lei137",
+            "omjavaid",
+            "nicolerabjohn",
+            "amy-kwan",
+            "mandlebug",
+            "zmodem",
+            "androm3da",
+            "tru",
+            "rovka",
+            "rorth",
+            "quinnlp",
+            "kamaub",
+            "abrisco",
+            "jakeegan",
+            "maryammo",
+            "tstellar",
+            "github-actions[bot]",
+        ]
+    )
+
+    for release in repo.get_releases():
+        print("Release:", release.title)
+        for asset in release.get_assets():
+            created_at = asset.created_at
+            updated_at = "" if asset.created_at == asset.updated_at else asset.updated_at
+            print(
+                f"{asset.name} : {asset.uploader.login} [{created_at} {updated_at}] ( {asset.download_count} )"
+            )
+            if asset.uploader.login not in uploaders:
+                print("Invalid uploader")
+                sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
