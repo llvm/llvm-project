dae2618f08010ae53033068be97296e86879aa91
Timm Bäder <tbaeder@redhat.com>
2024-05-22 14:35:43 +0200

    [clang][Interp] Test non-active union access

--- clang/test/AST/Interp/unions.cpp
+++ clang/test/AST/Interp/unions.cpp
@@ -1,8 +1,6 @@
 // RUN: %clang_cc1 -fexperimental-new-constant-interpreter -verify=expected,both %s
 // RUN: %clang_cc1 -verify=ref,both %s
 
-// both-no-diagnostics
-
 union U {
   int a;
   int b;
@@ -10,5 +8,5 @@ union U {
 
 constexpr U a = {12};
 static_assert(a.a == 12, "");
-
-
+static_assert(a.b == 0, ""); // both-error {{not an integral constant expression}} \
+                             // both-note {{read of member 'b' of union with active member 'a'}}
