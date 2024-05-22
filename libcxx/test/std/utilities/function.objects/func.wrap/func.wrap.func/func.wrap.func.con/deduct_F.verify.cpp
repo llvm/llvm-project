cc44886b7421e6fdd8a5159a37d0fa784fefdc46
Hui <hui.xie0621@gmail.com>
2024-05-22 16:03:13 +0100

    [libc++][test] Close LWG3238 and add tests

--- libcxx/test/std/utilities/function.objects/func.wrap/func.wrap.func/func.wrap.func.con/deduct_F.verify.cpp
+++ libcxx/test/std/utilities/function.objects/func.wrap/func.wrap.func/func.wrap.func.con/deduct_F.verify.cpp
@@ -18,6 +18,7 @@
 // Make sure we stick to the specification.
 
 #include <functional>
+#include <type_traits>
 
 struct R { };
 struct f0 { R operator()() && { return {}; } };
@@ -28,3 +29,19 @@ void f() {
     std::function g = f1{}; // expected-error{{no viable constructor or deduction guide for deduction of template arguments of 'function'}}
     std::function h = nullptr; // expected-error{{no viable constructor or deduction guide for deduction of template arguments of 'function'}}
 }
+
+// LWG 3238. Insufficiently-defined behavior of std::function deduction guides
+// https://cplusplus.github.io/LWG/issue3238
+template <class T, class = void>
+struct IsFunctionDeducible : std::false_type {};
+
+template <class T>
+struct IsFunctionDeducible<T, std::void_t<decltype(std::function(std::declval<T>()))>> : std::true_type {};
+
+struct Deducible {
+    int operator()() const;
+};
+
+static_assert(IsFunctionDeducible<Deducible>::value);
+static_assert(!IsFunctionDeducible<f0>::value);
+static_assert(!IsFunctionDeducible<f1>::value);
