c47d09d3001c1088e56b080e3f31bf90e0d90bf9
Hui <hui.xie0621@gmail.com>
2024-05-22 15:33:55 +0100

    [libc++][test] Close LWG3382 and add tests

---
+++ libcxx/test/std/utilities/utility/pairs/pairs.pair/lwg3382.compile.pass.cpp
@@ -0,0 +1,23 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++03, c++11, c++14, c++17
+
+#include <utility>
+
+#include "test_macros.h"
+
+template <auto>
+struct Test {};
+
+void test() {
+  // LWG 3382. NTTP for pair and array
+  // https://cplusplus.github.io/LWG/issue3382
+  constexpr std::pair<int, long> a{};
+  [[maybe_unused]] Test<a> test1{};
+}
