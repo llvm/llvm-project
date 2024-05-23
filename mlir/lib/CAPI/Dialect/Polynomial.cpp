3437b4cce709f8542603463d7ae99d9b762aa0de
Maksim Levental <maksim.levental@gmail.com>
2024-05-22 21:00:14 -0500

    Update mlir/lib/CAPI/Dialect/Polynomial.cpp
    Co-authored-by: Jeremy Kun <kun.jeremy@gmail.com>

--- mlir/lib/CAPI/Dialect/Polynomial.cpp
+++ mlir/lib/CAPI/Dialect/Polynomial.cpp
@@ -1,5 +1,4 @@
-//===- Polynomial.cpp - C Interface for Polynomial dialect
-//--------------------------===//
+//===- Polynomial.cpp - C Interface for Polynomial dialect ----------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
