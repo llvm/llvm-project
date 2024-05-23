8954d10b6a291a03aeed0bce70852f05bdd7bfa7
Timm Bäder <tbaeder@redhat.com>
2024-05-23 13:10:13 +0200

    [clang][Interp] Don't diagnose ObjCIvarDecls as invalid reads

---
+++ clang/test/AST/Interp/objc.mm
@@ -0,0 +1,8 @@
+// RUN: %clang_cc1 -fexperimental-new-constant-interpreter -verify=expected,both %s
+// RUN: %clang_cc1 -verify=ref,both %s
+
+@interface A {
+  int a;
+  static_assert(a, ""); // both-error {{static assertion expression is not an integral constant expression}}
+}
+@end
