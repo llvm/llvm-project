756fe01263e0db7c039b60c2a44fd38da254978b
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2024-05-23 11:42:24 -0700

    [CIR][CIRGen][NFC] Update ReturnValueSlot according to upstream

--- clang/lib/CIR/CodeGen/CIRGenCall.h
+++ clang/lib/CIR/CodeGen/CIRGenCall.h
@@ -267,8 +267,11 @@ class ReturnValueSlot {
   Address Addr = Address::invalid();
 
   // Return value slot flags
+  LLVM_PREFERRED_TYPE(bool)
   unsigned IsVolatile : 1;
+  LLVM_PREFERRED_TYPE(bool)
   unsigned IsUnused : 1;
+  LLVM_PREFERRED_TYPE(bool)
   unsigned IsExternallyDestructed : 1;
 
 public:
@@ -284,6 +287,7 @@ public:
   Address getValue() const { return Addr; }
   bool isUnused() const { return IsUnused; }
   bool isExternallyDestructed() const { return IsExternallyDestructed; }
+  Address getAddress() const { return Addr; }
 };
 
 enum class FnInfoOpts {
