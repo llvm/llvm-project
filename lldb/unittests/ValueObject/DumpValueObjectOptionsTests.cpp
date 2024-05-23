cf967ad89924534ecd84cd31c984134d87a5eee8
David Spickett <david.spickett@linaro.org>
2024-05-23 09:41:24 +0000

    non bitfield enum tests and refactoring

--- lldb/unittests/ValueObject/DumpValueObjectOptionsTests.cpp
+++ lldb/unittests/ValueObject/DumpValueObjectOptionsTests.cpp
@@ -70,6 +70,42 @@ public:
     m_type_system = m_holder->GetAST();
   }
 
+  CompilerType
+  MakeEnumType(const std::vector<std::pair<const char *, int>> enumerators) {
+    CompilerType uint_type = m_type_system->GetBuiltinTypeForEncodingAndBitSize(
+        lldb::eEncodingUint, 32);
+    CompilerType enum_type = m_type_system->CreateEnumerationType(
+        "TestEnum", m_type_system->GetTranslationUnitDecl(),
+        OptionalClangModuleID(), Declaration(), uint_type, false);
+
+    m_type_system->StartTagDeclarationDefinition(enum_type);
+    Declaration decl;
+    for (auto [name, value] : enumerators)
+      m_type_system->AddEnumerationValueToEnumerationType(enum_type, decl, name,
+                                                          value, 32);
+    m_type_system->CompleteTagDeclarationDefinition(enum_type);
+
+    return enum_type;
+  }
+
+  void TestDumpValueObject(
+      CompilerType enum_type,
+      const std::vector<
+          std::tuple<uint32_t, DumpValueObjectOptions, const char *>> &tests) {
+    StreamString strm;
+    ConstString var_name("test_var");
+    ByteOrder endian = endian::InlHostByteOrder();
+    ExecutionContextScope *exe_scope = m_exe_ctx.GetBestExecutionContextScope();
+    for (auto [value, options, expected] : tests) {
+      DataExtractor data_extractor{&value, sizeof(value), endian, 4};
+      ValueObjectConstResult::Create(exe_scope, enum_type, var_name,
+                                     data_extractor)
+          ->Dump(strm, options);
+      ASSERT_STREQ(strm.GetString().str().c_str(), expected);
+      strm.Clear();
+    }
+  }
+
   ExecutionContext m_exe_ctx;
   TypeSystemClang *m_type_system;
 
@@ -87,50 +123,54 @@ private:
 };
 
 TEST_F(ValueObjectMockProcessTest, Enum) {
-  CompilerType uint_type = m_type_system->GetBuiltinTypeForEncodingAndBitSize(
-      lldb::eEncodingUint, 32);
-  CompilerType enum_type = m_type_system->CreateEnumerationType(
-      "test_enum", m_type_system->GetTranslationUnitDecl(),
-      OptionalClangModuleID(), Declaration(), uint_type, false);
+  // This is not a bitfield-like enum, so values are printed as decimal by
+  // default. Also we only show the enumerator name if the value is an
+  // exact match.
+  TestDumpValueObject(
+      MakeEnumType({{"test_2", 2}, {"test_3", 3}}),
+      {{0, {}, "(TestEnum) test_var = 0\n"},
+       {1, {}, "(TestEnum) test_var = 1\n"},
+       {2, {}, "(TestEnum) test_var = test_2\n"},
+       {3, {}, "(TestEnum) test_var = test_3\n"},
+       {4, {}, "(TestEnum) test_var = 4\n"},
+       {5, {}, "(TestEnum) test_var = 5\n"},
+       {1, DumpValueObjectOptions().SetHideRootName(true), "(TestEnum) 1\n"},
+       {1, DumpValueObjectOptions().SetHideRootType(true), "test_var = 1\n"},
+       {1, DumpValueObjectOptions().SetHideRootName(true).SetHideRootType(true),
+        "1\n"},
+       {1, DumpValueObjectOptions().SetHideName(true), "(TestEnum) 1\n"},
+       {1, DumpValueObjectOptions().SetHideValue(true),
+        "(TestEnum) test_var =\n"},
+       {1, DumpValueObjectOptions().SetHideName(true).SetHideValue(true),
+        "(TestEnum) \n"}});
+}
 
-  m_type_system->StartTagDeclarationDefinition(enum_type);
-  Declaration decl;
-  // Each value sets one bit in the enum, to make this a "bitfield like enum".
-  m_type_system->AddEnumerationValueToEnumerationType(enum_type, decl, "test_2",
-                                                      2, 32);
-  m_type_system->AddEnumerationValueToEnumerationType(enum_type, decl, "test_4",
-                                                      4, 32);
-  m_type_system->CompleteTagDeclarationDefinition(enum_type);
-
-  std::vector<std::tuple<uint32_t, DumpValueObjectOptions, const char *>> enums{
-      {0, {}, "(test_enum) test_var =\n"},
-      {1, {}, "(test_enum) test_var = 0x1\n"},
-      {2, {}, "(test_enum) test_var = test_2\n"},
-      {4, {}, "(test_enum) test_var = test_4\n"},
-      {6, {}, "(test_enum) test_var = test_2 | test_4\n"},
-      {7, {}, "(test_enum) test_var = test_2 | test_4 | 0x1\n"},
-      {8, {}, "(test_enum) test_var = 0x8\n"},
-      {1, DumpValueObjectOptions().SetHideRootName(true), "(test_enum) 0x1\n"},
-      {1, DumpValueObjectOptions().SetHideRootType(true), "test_var = 0x1\n"},
-      {1, DumpValueObjectOptions().SetHideRootName(true).SetHideRootType(true),
-       "0x1\n"},
-      {1, DumpValueObjectOptions().SetHideName(true), "(test_enum) 0x1\n"},
-      {1, DumpValueObjectOptions().SetHideValue(true),
-       "(test_enum) test_var =\n"},
-      {1, DumpValueObjectOptions().SetHideName(true).SetHideValue(true),
-       "(test_enum) \n"},
-  };
-
-  StreamString strm;
-  ExecutionContextScope *exe_scope = m_exe_ctx.GetBestExecutionContextScope();
-  ConstString var_name("test_var");
-  ByteOrder endian = endian::InlHostByteOrder();
-  for (auto [value, options, expected] : enums) {
-    DataExtractor data_extractor{&value, sizeof(value), endian, 4};
-    ValueObjectConstResult::Create(exe_scope, enum_type, var_name,
-                                   data_extractor)
-        ->Dump(strm, options);
-    ASSERT_STREQ(strm.GetString().str().c_str(), expected);
-    strm.Clear();
-  }
+TEST_F(ValueObjectMockProcessTest, BitFieldLikeEnum) {
+  // These enumerators set individual bits in the value, as if it were a flag
+  // set. lldb treats this as a "bitfield like enum". This means we show values
+  // as hex, a value of 0 shows nothing, and values with no exact enumerator are
+  // shown as combinations of the other values.
+  TestDumpValueObject(
+      MakeEnumType({{"test_2", 2}, {"test_4", 4}}),
+      {
+          {0, {}, "(TestEnum) test_var =\n"},
+          {1, {}, "(TestEnum) test_var = 0x1\n"},
+          {2, {}, "(TestEnum) test_var = test_2\n"},
+          {4, {}, "(TestEnum) test_var = test_4\n"},
+          {6, {}, "(TestEnum) test_var = test_2 | test_4\n"},
+          {7, {}, "(TestEnum) test_var = test_2 | test_4 | 0x1\n"},
+          {8, {}, "(TestEnum) test_var = 0x8\n"},
+          {1, DumpValueObjectOptions().SetHideRootName(true),
+           "(TestEnum) 0x1\n"},
+          {1, DumpValueObjectOptions().SetHideRootType(true),
+           "test_var = 0x1\n"},
+          {1,
+           DumpValueObjectOptions().SetHideRootName(true).SetHideRootType(true),
+           "0x1\n"},
+          {1, DumpValueObjectOptions().SetHideName(true), "(TestEnum) 0x1\n"},
+          {1, DumpValueObjectOptions().SetHideValue(true),
+           "(TestEnum) test_var =\n"},
+          {1, DumpValueObjectOptions().SetHideName(true).SetHideValue(true),
+           "(TestEnum) \n"},
+      });
 }
