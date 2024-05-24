69088d64e6e691476e93252192ef45a1600c1ae0
Kiran Chandramohan <kiran.chandramohan@arm.com>
2024-05-24 14:31:09 +0100

    [Flang][OpenMP] Reenable and fix final few tests 6/6 (#93295)
    Add do02.f90 and taskloop03.f90 that were removed in
    https://github.com/llvm/llvm-project/pull/92739
    Replace shell script tests with python.

--- flang/test/Semantics/OpenMP/sections03.f90
+++ flang/test/Semantics/OpenMP/sections03.f90
@@ -1,6 +1,4 @@
-! UNSUPPORTED: system-windows
-! Marking as unsupported due to suspected long runtime on Windows
-! RUN: %python %S/../test_errors.py %s %flang -fopenmp
+! RUN: %python %S/../test_errors.py %s %flang_fc1 %openmp_flags
 !XFAIL: *
 ! OpenMP version 5.0.0
 ! 2.8.1 sections construct
