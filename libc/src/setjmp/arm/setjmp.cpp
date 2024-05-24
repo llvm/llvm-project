51464ea52da8c76234008df8192b3d1b82dc9dbf
Nick Desaulniers <ndesaulniers@google.com>
2024-05-24 14:33:17 -0700

    fix up setjmp for thumb1

--- libc/src/setjmp/arm/setjmp.cpp
+++ libc/src/setjmp/arm/setjmp.cpp
@@ -11,6 +11,34 @@
 
 namespace LIBC_NAMESPACE {
 
+#if defined(__thumb__) && __ARM_ARCH_ISA_THUMB == 1
+
+[[gnu::naked]]
+LLVM_LIBC_FUNCTION(int, setjmp, (__jmp_buf * buf)) {
+  asm(R"(
+      # Store r4, r5, r6, and r7 into buf.
+      stmia r0!, {r4-r7}
+
+      # Store r8, r9, r10, and r11 into buf. Thumb(1) doesn't support the high
+      # registers > r7 in stmia, so move them into lower GPRs first.
+      mov r4, r8
+      mov r5, r9
+      mov r6, r10
+      mov r7, r11
+      stmia r0!, {r4-r7}
+
+      # Store sp into buf. Thumb(1) doesn't support sp in str, move to GPR
+      # first.
+      mov r4, sp
+      str r4, [r0]
+
+      # Return 0.
+      movs r0, #0
+      bx lr)");
+}
+
+#else // Thumb2 or ARM
+
 [[gnu::naked]]
 LLVM_LIBC_FUNCTION(int, setjmp, (__jmp_buf * buf)) {
   asm(R"(
@@ -19,5 +47,7 @@ LLVM_LIBC_FUNCTION(int, setjmp, (__jmp_buf * buf)) {
       mov r0, #0
       bx lr)");
 }
+
+#endif
 
 } // namespace LIBC_NAMESPACE
