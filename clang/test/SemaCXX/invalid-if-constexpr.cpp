e4f190be4f59dec78c8a184d9603ac8856da3900
Youngsuk Kim <youngsuk.kim@hpe.com>
2024-05-26 10:28:02 -0500

    Add dedicated test

--- clang/test/SemaCXX/invalid-if-constexpr.cpp
+++ clang/test/SemaCXX/invalid-if-constexpr.cpp
@@ -5,7 +5,6 @@ void similar() { // expected-note {{'similar' declared here}}
   if constexpr (similer<>) {} // expected-error {{use of undeclared identifier 'similer'; did you mean 'similar'?}}
 }
 void a() { if constexpr (__adl_swap<>) {}} // expected-error{{use of undeclared identifier '__adl_swap'; did you mean '__sync_swap'?}} \
-                                           // not-expected-note {{'__sync_swap' declared here}}
 
 int AA() { return true;} // expected-note {{'AA' declared here}}
 
