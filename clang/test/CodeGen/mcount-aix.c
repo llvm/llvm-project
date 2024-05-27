1abdb8602d3ef60a339227087b473274e1739626
Egor Pasko <pasko@chromium.org>
2024-05-27 14:32:50 +0200

    Run clang/test/CodeGen/... instrumentation tests only with -disable-llvm-passes

--- clang/test/CodeGen/mcount-aix.c
+++ clang/test/CodeGen/mcount-aix.c
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -pg -triple powerpc-ibm-aix7.2.0.0 -emit-llvm %s -o - | FileCheck %s
+// RUN: %clang_cc1 -disable-llvm-passes -pg -triple powerpc-ibm-aix7.2.0.0 -emit-llvm %s -o - | FileCheck %s
 // RUN: %clang_cc1 -disable-llvm-passes -pg -triple powerpc64-ibm-aix7.2.0.0 -emit-llvm %s -o - | FileCheck %s
 
 void foo() {
