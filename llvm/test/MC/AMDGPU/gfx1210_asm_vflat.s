2e20b59193e7ff011840be7ae8bdbf876e3a8204
Stanislav Mekhanoshin <Stanislav.Mekhanoshin@amd.com>
2024-05-28 14:27:54 -0700

    MI-400: Change scale_offset encoding
    The bit has been moved at some point.
    
    Change-Id: I792bc663eec29cef7e0c319aa21a48d270c55aaa

--- llvm/test/MC/AMDGPU/gfx1210_asm_vflat.s
+++ llvm/test/MC/AMDGPU/gfx1210_asm_vflat.s
@@ -62,49 +62,49 @@ scratch_load_b32 v5, v2, off nv
 // GFX12-ERR-NEXT:{{^}}                             ^
 
 global_load_b32 v5, v1, s[2:3] offset:32 scale_offset
-// GFX1210: global_load_b32 v5, v1, s[2:3] offset:32 scale_offset ; encoding: [0x02,0x00,0x05,0xee,0x05,0x00,0x01,0x00,0x01,0x20,0x00,0x00]
+// GFX1210: global_load_b32 v5, v1, s[2:3] offset:32 scale_offset ; encoding: [0x02,0x00,0x05,0xee,0x05,0x00,0x00,0x80,0x01,0x20,0x00,0x00]
 // GFX12-ERR: :[[@LINE-2]]:{{[0-9]+}}: error: scale_offset is not supported on this GPU
 // GFX12-ERR-NEXT:{{^}}global_load_b32 v5, v1, s[2:3] offset:32 scale_offset
 // GFX12-ERR-NEXT:{{^}}                                         ^
 
 global_store_b32 v5, v1, s[2:3] offset:32 scale_offset
-// GFX1210: global_store_b32 v5, v1, s[2:3] offset:32 scale_offset ; encoding: [0x02,0x80,0x06,0xee,0x00,0x00,0x81,0x00,0x05,0x20,0x00,0x00]
+// GFX1210: global_store_b32 v5, v1, s[2:3] offset:32 scale_offset ; encoding: [0x02,0x80,0x06,0xee,0x00,0x00,0x80,0x80,0x05,0x20,0x00,0x00]
 // GFX12-ERR: :[[@LINE-2]]:{{[0-9]+}}: error: scale_offset is not supported on this GPU
 // GFX12-ERR-NEXT:{{^}}global_store_b32 v5, v1, s[2:3] offset:32 scale_offset
 // GFX12-ERR-NEXT:{{^}}                                          ^
 
 global_atomic_add_u32 v2, v5, s[2:3] scale_offset
-// GFX1210: global_atomic_add_u32 v2, v5, s[2:3] scale_offset ; encoding: [0x02,0x40,0x0d,0xee,0x00,0x00,0x81,0x02,0x02,0x00,0x00,0x00]
+// GFX1210: global_atomic_add_u32 v2, v5, s[2:3] scale_offset ; encoding: [0x02,0x40,0x0d,0xee,0x00,0x00,0x80,0x82,0x02,0x00,0x00,0x00]
 // GFX12-ERR: :[[@LINE-2]]:{{[0-9]+}}: error: scale_offset is not supported on this GPU
 // GFX12-ERR-NEXT:{{^}}global_atomic_add_u32 v2, v5, s[2:3] scale_offset
 // GFX12-ERR-NEXT:{{^}}                                     ^
 
 scratch_load_b32 v5, v2, off scale_offset
-// GFX1210: scratch_load_b32 v5, v2, off scale_offset ; encoding: [0x7c,0x00,0x05,0xed,0x05,0x00,0x03,0x00,0x02,0x00,0x00,0x00]
+// GFX1210: scratch_load_b32 v5, v2, off scale_offset ; encoding: [0x7c,0x00,0x05,0xed,0x05,0x00,0x02,0x80,0x02,0x00,0x00,0x00]
 // GFX12-ERR: :[[@LINE-2]]:{{[0-9]+}}: error: scale_offset is not supported on this GPU
 // GFX12-ERR-NEXT:{{^}}scratch_load_b32 v5, v2, off scale_offset
 // GFX12-ERR-NEXT:{{^}}                             ^
 
 scratch_load_b32 v5, v2, off offset:32 scale_offset
-// GFX1210: scratch_load_b32 v5, v2, off offset:32 scale_offset ; encoding: [0x7c,0x00,0x05,0xed,0x05,0x00,0x03,0x00,0x02,0x20,0x00,0x00]
+// GFX1210: scratch_load_b32 v5, v2, off offset:32 scale_offset ; encoding: [0x7c,0x00,0x05,0xed,0x05,0x00,0x02,0x80,0x02,0x20,0x00,0x00]
 // GFX12-ERR: :[[@LINE-2]]:{{[0-9]+}}: error: scale_offset is not supported on this GPU
 // GFX12-ERR-NEXT:{{^}}scratch_load_b32 v5, v2, off offset:32 scale_offset
 // GFX12-ERR-NEXT:{{^}}                                       ^
 
 scratch_load_b32 v5, v2, s1 offset:32 scale_offset
-// GFX1210: scratch_load_b32 v5, v2, s1 offset:32 scale_offset ; encoding: [0x01,0x00,0x05,0xed,0x05,0x00,0x03,0x00,0x02,0x20,0x00,0x00]
+// GFX1210: scratch_load_b32 v5, v2, s1 offset:32 scale_offset ; encoding: [0x01,0x00,0x05,0xed,0x05,0x00,0x02,0x80,0x02,0x20,0x00,0x00]
 // GFX12-ERR: :[[@LINE-2]]:{{[0-9]+}}: error: scale_offset is not supported on this GPU
 // GFX12-ERR-NEXT:{{^}}scratch_load_b32 v5, v2, s1 offset:32 scale_offset
 // GFX12-ERR-NEXT:{{^}}                                      ^
 
 scratch_store_b32 v2, v5, off scale_offset
-// GFX1210: scratch_store_b32 v2, v5, off scale_offset ; encoding: [0x7c,0x80,0x06,0xed,0x00,0x00,0x83,0x02,0x02,0x00,0x00,0x00]
+// GFX1210: scratch_store_b32 v2, v5, off scale_offset ; encoding: [0x7c,0x80,0x06,0xed,0x00,0x00,0x82,0x82,0x02,0x00,0x00,0x00]
 // GFX12-ERR: :[[@LINE-2]]:{{[0-9]+}}: error: scale_offset is not supported on this GPU
 // GFX12-ERR-NEXT:{{^}}scratch_store_b32 v2, v5, off scale_offset
 // GFX12-ERR-NEXT:{{^}}                              ^
 
 scratch_store_b32 v2, v5, s1 scale_offset
-// GFX1210: scratch_store_b32 v2, v5, s1 scale_offset ; encoding: [0x01,0x80,0x06,0xed,0x00,0x00,0x83,0x02,0x02,0x00,0x00,0x00]
+// GFX1210: scratch_store_b32 v2, v5, s1 scale_offset ; encoding: [0x01,0x80,0x06,0xed,0x00,0x00,0x82,0x82,0x02,0x00,0x00,0x00]
 // GFX12-ERR: :[[@LINE-2]]:{{[0-9]+}}: error: scale_offset is not supported on this GPU
 // GFX12-ERR-NEXT:{{^}}scratch_store_b32 v2, v5, s1 scale_offset
 // GFX12-ERR-NEXT:{{^}}                             ^
