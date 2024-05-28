1da3d4dc3c74f1240e7d921c97b0c7eea2a53e33
Endre Fülöp <endre.fulop@sigmatechnology.com>
2024-05-29 00:38:07 +0200

    [clang][analyzer] Move unix.BlockInCriticalSection out of alpha
    After recent improvements, and testing on open source projects, the
    checker is ready to move out of the alpha package.

--- clang/test/Analysis/block-in-critical-section.c
+++ clang/test/Analysis/block-in-critical-section.c
@@ -1,4 +1,4 @@
-// RUN: %clang_analyze_cc1 -analyzer-checker=core,alpha.unix.BlockInCriticalSection -verify %s
+// RUN: %clang_analyze_cc1 -analyzer-checker=core,unix.BlockInCriticalSection -verify %s
 // expected-no-diagnostics
 
 // This should not crash
