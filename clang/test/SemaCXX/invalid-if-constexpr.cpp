ff17cdfe6de71f54eb4f68375cd26318761fc979
Youngsuk Kim <joseph942010@gmail.com>
2024-05-28 10:59:30 -0400

    Remove unneeded backtick character from test
    Co-authored-by: S. B. Tam <cpplearner@outlook.com>

--- clang/test/SemaCXX/invalid-if-constexpr.cpp
+++ clang/test/SemaCXX/invalid-if-constexpr.cpp
@@ -4,7 +4,7 @@ namespace GH61885 {
 void similar() { // expected-note {{'similar' declared here}}
   if constexpr (similer<>) {} // expected-error {{use of undeclared identifier 'similer'; did you mean 'similar'?}}
 }
-void a() { if constexpr (__adl_swap<>) {}} // expected-error{{use of undeclared identifier '__adl_swap'; did you mean '__sync_swap'?}} \
+void a() { if constexpr (__adl_swap<>) {}} // expected-error{{use of undeclared identifier '__adl_swap'; did you mean '__sync_swap'?}}
 
 int AA() { return true;} // expected-note {{'AA' declared here}}
 
