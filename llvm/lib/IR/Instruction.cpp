64399ddd94053354567a04812d65639e42b2ebcd
Jay Foad <jay.foad@amd.com>
2024-05-29 16:09:49 +0100

    [InstCombine] Do not require nsz for reassociating fmul
    The sign of the result of fmul is always the xor of the signs of the
    operands. Reassociation should not affect that.

--- llvm/lib/IR/Instruction.cpp
+++ llvm/lib/IR/Instruction.cpp
@@ -1204,6 +1204,7 @@ bool Instruction::isAssociative() const {
 
   switch (Opcode) {
   case FMul:
+    return cast<FPMathOperator>(this)->hasAllowReassoc();
   case FAdd:
     return cast<FPMathOperator>(this)->hasAllowReassoc() &&
            cast<FPMathOperator>(this)->hasNoSignedZeros();
