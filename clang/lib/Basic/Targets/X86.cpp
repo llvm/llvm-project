bc58fff8f57ff1f1fce5e1213cc139624e522878
Shengchen Kan <shengchen.kan@intel.com>
2024-05-29 17:30:14 +0800

    [X86][Driver] Enable feature ccmp,nf for -mapxf
    This is follow-up for #78901 after validation.

--- clang/lib/Basic/Targets/X86.cpp
+++ clang/lib/Basic/Targets/X86.cpp
@@ -961,7 +961,7 @@ void X86TargetInfo::getTargetDefines(const LangOptions &Opts,
   if (HasCF)
     Builder.defineMacro("__CF__");
   // Condition here is aligned with the feature set of mapxf in Options.td
-  if (HasEGPR && HasPush2Pop2 && HasPPX && HasNDD)
+  if (HasEGPR && HasPush2Pop2 && HasPPX && HasNDD && HasCCMP && HasNF)
     Builder.defineMacro("__APX_F__");
 
   // Each case falls through to the previous one here.
