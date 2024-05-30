e1c3e16d24b5cc097ff08e9283f53319acd3f245
Akira Hatanaka <ahatanak@gmail.com>
2024-05-30 16:52:37 -0700

    [clang] Fix a crash when a variable is captured by a block nested inside a lambda (#93749)
    `Eval->Value.get` returns a null pointer when the variable doesn't have
    an initializer. Use `cast_if_present` instead of `cast`.
    
    This fixes https://github.com/llvm/llvm-project/issues/93625.
    
    rdar://128482541

--- clang/lib/AST/Decl.cpp
+++ clang/lib/AST/Decl.cpp
@@ -2408,9 +2408,11 @@ Expr *VarDecl::getInit() {
     return cast<Expr>(S);
 
   auto *Eval = getEvaluatedStmt();
-  return cast<Expr>(Eval->Value.isOffset()
-                        ? Eval->Value.get(getASTContext().getExternalSource())
-                        : Eval->Value.get(nullptr));
+
+  return cast_if_present<Expr>(
+      Eval->Value.isOffset()
+          ? Eval->Value.get(getASTContext().getExternalSource())
+          : Eval->Value.get(nullptr));
 }
 
 Stmt **VarDecl::getInitAddress() {
