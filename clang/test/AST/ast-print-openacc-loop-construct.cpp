f2d57ac996835efd3f7b4c3a3c69db43f5783fcf
erichkeane <ekeane@nvidia.com>
2024-05-30 11:10:19 -0700

    [OpenACC] Implement 'device_type' sema for 'loop' construct
    This clause is effectively identical to how this works on compute
    clauses, however the list of clauses allowed after it are slightly
    different.  This enables the clause for the 'loop', and ensures we're
    permitting the correct list.

--- clang/test/AST/ast-print-openacc-loop-construct.cpp
+++ clang/test/AST/ast-print-openacc-loop-construct.cpp
@@ -1,9 +1,35 @@
 // RUN: %clang_cc1 -fopenacc -Wno-openacc-deprecated-clause-alias -ast-print %s -o - | FileCheck %s
 
+struct SomeStruct{};
+
 void foo() {
 // CHECK: #pragma acc loop
 // CHECK-NEXT: for (;;)
 // CHECK-NEXT: ;
 #pragma acc loop
+  for(;;);
+
+// CHECK: #pragma acc loop device_type(SomeStruct)
+// CHECK-NEXT: for (;;)
+// CHECK-NEXT: ;
+#pragma acc loop device_type(SomeStruct)
+  for(;;);
+
+// CHECK: #pragma acc loop device_type(int)
+// CHECK-NEXT: for (;;)
+// CHECK-NEXT: ;
+#pragma acc loop device_type(int)
+  for(;;);
+
+// CHECK: #pragma acc loop dtype(bool)
+// CHECK-NEXT: for (;;)
+// CHECK-NEXT: ;
+#pragma acc loop dtype(bool)
+  for(;;);
+
+// CHECK: #pragma acc loop dtype(AnotherIdent)
+// CHECK-NEXT: for (;;)
+// CHECK-NEXT: ;
+#pragma acc loop dtype(AnotherIdent)
   for(;;);
 }
