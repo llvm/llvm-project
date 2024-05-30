25021ebeca17e7268c36c85d0dd0172ff41925c5
Pranav Bhandarkar <pranav.bhandarkar@amd.com>
2024-05-30 13:08:29 -0500

    add an end-to-end offloading test for target depend

---
+++ offload/test/offloading/fortran/target-depend.f90
@@ -0,0 +1,40 @@
+! Offloading test checking interaction of fixed size
+! arrays with enter, exit and target
+! REQUIRES: flang, amdgcn-amd-amdhsa
+! UNSUPPORTED: nvptx64-nvidia-cuda
+! UNSUPPORTED: nvptx64-nvidia-cuda-LTO
+! UNSUPPORTED: aarch64-unknown-linux-gnu
+! UNSUPPORTED: aarch64-unknown-linux-gnu-LTO
+! UNSUPPORTED: x86_64-pc-linux-gnu
+! UNSUPPORTED: x86_64-pc-linux-gnu-LTO
+
+! RUN: %libomptarget-compile-fortran-run-and-check-generic
+program main
+  integer :: a = 0
+  call foo(5, a)
+  print*, "======= FORTRAN Test passed! ======="
+  print*, "foo(5) returned ", a, ", expected 6\n"
+  !       stop 0
+end program main
+subroutine foo(N, r)
+  integer, intent(in) :: N
+  integer, intent(out) :: r
+  integer :: z
+
+  z = 1
+  !$omp task depend(out: z) shared(z)
+  ! print*, "N is ", N
+  ! print*, "z is ", z
+  z = N
+!  print*, "z is ", z
+  !$omp end task
+
+  !$omp target map(tofrom: z) depend(in: z)
+  z = z + 1
+  !$omp end target
+
+  r = z
+end subroutine foo
+
+!CHECK: ======= FORTRAN Test passed! =======
+!CHECK: foo(5) returned 6 , expected 6
