fe014fd12e662d7c73f4eb45dded1877111aec83
Rose Zhang <rosezhang@google.com>
2024-05-30 20:40:25 +0000

    fixed  noreturn and no_stack_protector brackets and other formatting and includes issues

--- libc/src/__support/OSUtil/linux/exit.cpp
+++ libc/src/__support/OSUtil/linux/exit.cpp
@@ -15,10 +15,9 @@ namespace LIBC_NAMESPACE::internal {
 // mark as no_stack_protector for x86 since TLS can be torn down before calling
 // exit so that the stack protector canary cannot be loaded.
 #ifdef LIBC_TARGET_ARCH_IS_X86
-__attribute__((no_stack_protector))
+[[clang::no_stack_protector]]
 #endif
-__attribute__((noreturn)) void
-exit(int status) {
+[[noreturn]] void exit(int status) {
   for (;;) {
     LIBC_NAMESPACE::syscall_impl<long>(SYS_exit_group, status);
     LIBC_NAMESPACE::syscall_impl<long>(SYS_exit, status);
