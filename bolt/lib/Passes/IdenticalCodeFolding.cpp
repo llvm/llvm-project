2f070c180ee067755a0c5097eee5b1edaaf45773
shaw young <58664393+shawbyoung@users.noreply.github.com>
2024-05-31 09:50:47 -0700

    Update tabs in  IdenticalCodeFolding.cpp

--- bolt/lib/Passes/IdenticalCodeFolding.cpp
+++ bolt/lib/Passes/IdenticalCodeFolding.cpp
@@ -79,7 +79,7 @@ static bool equalJumpTables(const JumpTable &JumpTableA,
     }
 
     assert(TargetA && TargetB && "cannot locate target block(s)");
-      
+
     if (TargetA->getLayoutIndex() != TargetB->getLayoutIndex())
       return false;
   }
@@ -183,7 +183,7 @@ static bool isIdenticalWith(const BinaryFunction &A, const BinaryFunction &B,
   auto BBI = OrderB.begin();
   for (const BinaryBasicBlock *BB : OrderA) {
     const BinaryBasicBlock *OtherBB = *BBI;
-      
+
     if (BB->getLayoutIndex() != OtherBB->getLayoutIndex())
       return false;
 
