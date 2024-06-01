18c746282e4a1155217ae9adb44d4090ee729909
OverMighty <its.overmighty@gmail.com>
2024-06-01 16:42:56 +0200

    [libc][math][c23] Fix file headers for ceilf16 and floorf16

--- libc/src/math/floorf16.h
+++ libc/src/math/floorf16.h
@@ -1,4 +1,4 @@
-//===-- floorf16 ------------------------------------------------*- C++ -*-===//
+//===-- Implementation header for floorf16 ----------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
--- libc/src/math/generic/ceilf16.cpp
+++ libc/src/math/generic/ceilf16.cpp
@@ -1,4 +1,4 @@
-//===-- ceilf16 function --------------------------------------------------===//
+//===-- Implementation of ceilf16 function --------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
