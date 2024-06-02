d6ce1100cc397ba37537c10e32cb9eb15b3765be
changkhothuychung <nhat7203@gmail.com>
2024-06-02 15:43:45 -0400

    more fix

--- libc/test/src/sys/ioctl/linux/ioctl_test.cpp
+++ libc/test/src/sys/ioctl/linux/ioctl_test.cpp
@@ -32,7 +32,6 @@ TEST(LlvmLibcIoctlTest, InvalidFileDescriptor) {
 TEST(LlvmLibcIoctlTest, ValidFileDescriptor) {
   constexpr const char *TEST_FILE = "testdata/ioctl.test";
   int fd = open(TEST_FILE, O_CREAT | O_WRONLY, S_IRWXU);
-  ASSERT_FALSE(fd == -1);
   int data;
   int res = LIBC_NAMESPACE::ioctl(fd, FS_IOC_GETFLAGS, &data);
   EXPECT_THAT(res, Succeeds());
