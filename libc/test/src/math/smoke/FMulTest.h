3bf640d06c04698ce5b7f98d6e1add86d76d50a8
Job Hernandez <hj93@protonmail.com>
2024-06-02 12:28:54 -0700

    fix tests

--- libc/test/src/math/smoke/FMulTest.h
+++ libc/test/src/math/smoke/FMulTest.h
@@ -21,8 +21,9 @@ public:
 
   void testMul(FMulFunc func) {
     EXPECT_FP_EQ(T(15.0), func(3.0, 5.0));
-    EXPECT_FP_EQ(T(7.34684e-40), func(0x1.0p1, 0x1.0p-131));
-    EXPECT_FP_EQ(T(5.87747e-39), func(0x1.0p2, 0x1.0p-129));
+    EXPECT_FP_EQ(T(0x1.0p-130), func(0x1.0p1, 0x1.0p-131));
+    EXPECT_FP_EQ(T(0x1.0p-127), func(0x1.0p2, 0x1.0p-129));
+    
   }
 };
 
