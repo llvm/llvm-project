26b715b31b4cc2d7d89c744217fc5cec4c89022f
cxy <chenxunyu1993@gmail.com>
2024-06-03 12:42:47 +0000

    refactor(*): Include more precise side effects
    This patch adds more precise side effects to the current ops with memory
    effects, allowing us to determine which OpOperand/OpResult/BlockArgument the
    operation reads or writes, rather than just recording the reading and writing
    of values. This allows for convenient use of precise side effects to achieve
    analysis and optimization.
    
    Related discussions: https://discourse.llvm.org/t/rfc-add-operandindex-to-sideeffect-instance/79243

--- mlir/lib/Dialect/Vector/IR/VectorOps.cpp
+++ mlir/lib/Dialect/Vector/IR/VectorOps.cpp
@@ -4123,7 +4123,7 @@ void TransferReadOp::getEffects(
     SmallVectorImpl<SideEffects::EffectInstance<MemoryEffects::Effect>>
         &effects) {
   if (llvm::isa<MemRefType>(getShapedType()))
-    effects.emplace_back(MemoryEffects::Read::get(), getSource(),
+    effects.emplace_back(MemoryEffects::Read::get(), &getSourceMutable(),
                          SideEffects::DefaultResource::get());
 }
 
@@ -4497,7 +4497,7 @@ void TransferWriteOp::getEffects(
     SmallVectorImpl<SideEffects::EffectInstance<MemoryEffects::Effect>>
         &effects) {
   if (llvm::isa<MemRefType>(getShapedType()))
-    effects.emplace_back(MemoryEffects::Write::get(), getSource(),
+    effects.emplace_back(MemoryEffects::Write::get(), &getSourceMutable(),
                          SideEffects::DefaultResource::get());
 }
 
