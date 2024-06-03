961f6f8798b4bddde5ef83547b941ca50e95b8b1
Zhang Yan <yan3.zhang@intel.com>
2024-06-03 10:26:18 +0800

    do not use pattern

--- mlir/lib/Dialect/Arith/Transforms/EliminateExplicitRounding.cpp
+++ mlir/lib/Dialect/Arith/Transforms/EliminateExplicitRounding.cpp
@@ -29,46 +29,33 @@ using namespace mlir;
 
 namespace {
 
-struct EliminateExplicitRoundingRewritePattern final
-    : OpRewritePattern<arith::ExtFOp> {
-  using OpRewritePattern::OpRewritePattern;
-
-  LogicalResult matchAndRewrite(arith::ExtFOp extFOp,
-                                PatternRewriter &rewriter) const final {
-    // Check whether match `truncF->extF` pair.
-    auto truncFOp = extFOp.getOperand().getDefiningOp<arith::TruncFOp>();
-    if (!truncFOp)
-      return failure();
-
-    // Check whether the rounding pair's input and output data type are the
-    // same. Currently only consider to eliminate rounding pairs for (bf16 / f16
-    // <-> f32).
-    Value input = truncFOp.getOperand();
-    Type inTy = getElementTypeOrSelf(input.getType());
-    Type outTy = getElementTypeOrSelf(extFOp.getType());
-    Type shortTy = getElementTypeOrSelf(truncFOp.getType());
-    if (isa<Float32Type>(inTy) && isa<Float32Type>(outTy) &&
-        (isa<Float16Type, BFloat16Type>(shortTy))) {
-      rewriter.replaceOp(extFOp, {input});
-      return success();
-    }
-
-    return failure();
-  }
-};
-
 struct EliminateExplicitRounding final
     : arith::impl::ArithEliminateExplicitRoundingBase<
           EliminateExplicitRounding> {
   using ArithEliminateExplicitRoundingBase::ArithEliminateExplicitRoundingBase;
   void runOnOperation() override {
-    RewritePatternSet patterns(&getContext());
-    patterns.insert<EliminateExplicitRoundingRewritePattern>(&getContext());
-    FrozenRewritePatternSet patternSet(std::move(patterns));
-    SmallVector<Operation *> ops;
-    getOperation()->walk([&](arith::ExtFOp op) { ops.push_back(op); });
-    if (failed(applyOpPatternsAndFold(ops, patternSet)))
-      signalPassFailure();
+    getOperation()->walk([&](arith::ExtFOp extFOp) {
+      // Check whether match `truncF->extF` pair.
+      auto truncFOp = extFOp.getOperand().getDefiningOp<arith::TruncFOp>();
+      if (truncFOp) {
+        // Check whether the rounding pair's input and output data type are the
+        // same. Currently only consider to eliminate rounding pairs for (bf16 /
+        // f16
+        // <-> f32).
+        Value input = truncFOp.getOperand();
+        Type inTy = getElementTypeOrSelf(input.getType());
+        Type outTy = getElementTypeOrSelf(extFOp.getType());
+        Type shortTy = getElementTypeOrSelf(truncFOp.getType());
+        if (isa<Float32Type>(inTy) && isa<Float32Type>(outTy) &&
+            (isa<Float16Type, BFloat16Type>(shortTy))) {
+          for (auto &use :
+               llvm::make_early_inc_range(extFOp.getResult().getUses())) {
+            use.set(input);
+          }
+          extFOp.erase();
+        }
+      }
+    });
   }
 };
 
