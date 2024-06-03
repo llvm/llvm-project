0e56adeacaf9501acb098bcb136c1127ad9d738b
hdoc <github@hdoc.io>
2024-06-03 14:52:22 -0700

    Fix comment string

--- clang/include/clang/AST/CommentParser.h
+++ clang/include/clang/AST/CommentParser.h
@@ -100,7 +100,7 @@ public:
   ArrayRef<Comment::Argument>
   parseCommandArgs(TextTokenRetokenizer &Retokenizer, unsigned NumArgs);
 
-  /// Parse arguments for \\throws command supported args are in form of class
+  /// Parse arguments for \throws command supported args are in form of class
   /// or template.
   ArrayRef<Comment::Argument>
   parseThrowCommandArgs(TextTokenRetokenizer &Retokenizer, unsigned NumArgs);
