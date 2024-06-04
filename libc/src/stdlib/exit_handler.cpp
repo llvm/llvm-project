faaf2ee5d86cd3a5c9f3befd365e833d3170de37
Aaryan Shukla <aaryanshukla@google.com>
2024-06-04 17:32:51 +0000

    [libc] adding at_quick_exit function

--- libc/src/stdlib/exit_handler.cpp
+++ libc/src/stdlib/exit_handler.cpp
@@ -1,4 +1,4 @@
-//===-- Implementation of exit_handler-------------------------------------===//
+//===--- Implementation of exit_handler------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
