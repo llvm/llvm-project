af58a8ad715923421b76a36c8940d57b1701be9f
Christos Perivolaropoulos <cperivol@google.com>
2024-06-04 15:32:12 +0000

    [MLIR][NVVM][GPU] Do not expect nvidia `fatbinary` to be present
    By default use only ptxas to compile NVVM IR. If the user requests
    fatbin, only then use `fatbinary` and complain if it's missing.

--- mlir/lib/Dialect/GPU/IR/GPUDialect.cpp
+++ mlir/lib/Dialect/GPU/IR/GPUDialect.cpp
@@ -2263,7 +2263,7 @@ CompilationTarget TargetOptions::getCompilationTarget() const {
 }
 
 CompilationTarget TargetOptions::getDefaultCompilationTarget() {
-  return CompilationTarget::Fatbin;
+  return CompilationTarget::Binary;
 }
 
 std::pair<llvm::BumpPtrAllocator, SmallVector<const char *>>
