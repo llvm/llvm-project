a7cd5477abd57be6df86c8abf3ff25eb72758fb5
Yingwei Zheng <dtcxzyw2333@gmail.com>
2024-06-04 21:55:00 +0800

    [InstCombine] Remove misleading comments. NFC.

--- llvm/lib/Transforms/InstCombine/InstCombineSelect.cpp
+++ llvm/lib/Transforms/InstCombine/InstCombineSelect.cpp
@@ -4024,8 +4024,6 @@ Instruction *InstCombinerImpl::visitSelectInst(SelectInst &SI) {
   }
 
   // select Cond, !X, X -> xor Cond, X
-  // Note: We don't fold select Cond, Y, X -> X (iff X->Y & !X->!Y) here as
-  // it indicates that these two patterns should be canonicalized.
   if (CondVal->getType() == SI.getType() && isKnownInversion(FalseVal, TrueVal))
     return BinaryOperator::CreateXor(CondVal, FalseVal);
 
