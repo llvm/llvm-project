2eca2701d2fc88871f62f432e47f4753e297612f
Rintaro Ishizaki <rishizaki@apple.com>
2024-06-05 21:59:40 +0000

    [lldb] Update for _CompilerSwiftSyntax libraries
    swift-syntax libraries for compilers are now in 'lib/swift/host/compiler'
    
    (cherry picked from commit 6f572b83ad2cfd87f6f34d2fb0c66f07fe7e1ee0)

--- lldb/cmake/modules/AddLLDB.cmake
+++ lldb/cmake/modules/AddLLDB.cmake
@@ -240,9 +240,9 @@ function(add_properties_for_swift_modules target reldir)
     if (SWIFT_BUILD_SWIFT_SYNTAX)
       if (CMAKE_SYSTEM_NAME MATCHES "Darwin")
         set_property(TARGET ${target}
-          APPEND PROPERTY BUILD_RPATH "@loader_path/${build_reldir}lib/swift/host")
+          APPEND PROPERTY BUILD_RPATH "@loader_path/${build_reldir}lib/swift/host/compiler")
         set_property(TARGET ${target}
-          APPEND PROPERTY INSTALL_RPATH "@loader_path/${reldir}lib/swift/host")
+          APPEND PROPERTY INSTALL_RPATH "@loader_path/${reldir}lib/swift/host/compiler")
         if(SWIFT_ALLOW_LINKING_SWIFT_CONTENT_IN_DARWIN_TOOLCHAIN)
           get_filename_component(TOOLCHAIN_BIN_DIR ${CMAKE_Swift_COMPILER} DIRECTORY)
           get_filename_component(TOOLCHAIN_LIB_DIR "${TOOLCHAIN_BIN_DIR}/../lib/swift/macosx" ABSOLUTE)
@@ -250,9 +250,9 @@ function(add_properties_for_swift_modules target reldir)
         endif()
       elseif (CMAKE_SYSTEM_NAME MATCHES "Linux|Android|OpenBSD|FreeBSD")
         set_property(TARGET ${target}
-          APPEND PROPERTY BUILD_RPATH "$ORIGIN/${build_reldir}lib/swift/host")
+          APPEND PROPERTY BUILD_RPATH "$ORIGIN/${build_reldir}lib/swift/host/compiler")
         set_property(TARGET ${target}
-          APPEND PROPERTY INSTALL_RPATH "$ORIGIN/${reldir}lib/swift/host")
+          APPEND PROPERTY INSTALL_RPATH "$ORIGIN/${reldir}lib/swift/host/compiler")
       endif()
     endif()
   endif()
