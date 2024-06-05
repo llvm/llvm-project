39c8c9aa1cc45fd4cfd16fe841f65a4d1cb08cf4
Miro Bucko <mbucko@meta.com>
2024-06-05 09:03:38 -0700

    Fix flaky TestDAP_console test.
    Test Plan:
    llvm-lit llvm-project/lldb/test/API/tools/lldb-dap/console/TestDAP_console.py

--- lldb/packages/Python/lldbsuite/test/tools/lldb-dap/dap_server.py
+++ lldb/packages/Python/lldbsuite/test/tools/lldb-dap/dap_server.py
@@ -171,13 +171,15 @@ class DebugCommunication(object):
         self.output_condition.release()
         return output
 
-    def collect_output(self, category, duration, clear=True):
-        end_time = time.time() + duration
+    def collect_output(self, category, timeout_secs, pattern, clear=True):
+        end_time = time.time() + timeout_secs
         collected_output = ""
         while end_time > time.time():
             output = self.get_output(category, timeout=0.25, clear=clear)
             if output:
                 collected_output += output
+                if pattern is not None and pattern in output:
+                    break
         return collected_output if collected_output else None
 
     def enqueue_recv_packet(self, packet):
