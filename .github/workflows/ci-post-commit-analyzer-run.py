24d616f99234bcaceab33f5d0bb486c02e4f2216
Tom Stellard <tstellar@redhat.com>
2024-06-06 21:49:59 +0000

    Fix python formatting

--- .github/workflows/ci-post-commit-analyzer-run.py
+++ .github/workflows/ci-post-commit-analyzer-run.py
@@ -5,14 +5,20 @@ import re
 import subprocess
 import sys
 
+
 def run_analyzer(data):
-    os.chdir(data['directory'])
-    command = data['command'] + f' --analyze --analyzer-output html -o analyzer-results -Xclang -analyzer-config -Xclang max-nodes=75000'
+    os.chdir(data["directory"])
+    command = (
+        data["command"] 
+        + f" --analyze --analyzer-output html -o analyzer-results -Xclang -analyzer-config -Xclang max-nodes=75000"
+    )
     print(command)
     subprocess.run(command, shell=True, check=True)
 
+
 def pool_error(e):
     print("Error analyzing file:", e)
+
 
 def main():
     db_path = sys.argv[1]
@@ -22,6 +28,7 @@ def main():
         pool.map_async(run_analyzer, [k for k in database], error_callback=pool_error)
         pool.close()
         pool.join()
+
 
 if __name__ == "__main__":
     main()
