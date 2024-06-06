94dddc22f2de5e520e24324f572afebc1797129f
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2024-06-06 15:21:38 -0700

    Revert "[CIR][Pipeline] Support -fclangir-analysis-only (#638)"
    This reverts commit f4d538f4245e3d18187dee48e86a214391bc54cd.
    
    It's causing a circular dependency in shared lib builds. See https://github.com/llvm/clangir/issues/655

--- clang/include/clang/Frontend/FrontendOptions.h
+++ clang/include/clang/Frontend/FrontendOptions.h
@@ -447,10 +447,6 @@ public:
   // Enable Clang IR call conv lowering pass.
   unsigned ClangIREnableCallConvLowering : 1;
 
-  // Enable Clang IR analysis only pipeline that uses tranditional code gen
-  // pipeline.
-  unsigned ClangIRAnalysisOnly : 1;
-
   CodeCompleteOptions CodeCompleteOpts;
 
   /// Specifies the output format of the AST.
@@ -645,7 +641,7 @@ public:
         ClangIRDisablePasses(false), ClangIRDisableCIRVerifier(false),
         ClangIRDisableEmitCXXDefault(false), ClangIRLifetimeCheck(false),
         ClangIRIdiomRecognizer(false), ClangIRLibOpt(false),
-        ClangIRAnalysisOnly(false), TimeTraceGranularity(500) {}
+        TimeTraceGranularity(500) {}
 
   /// getInputKindForExtension - Return the appropriate input kind for a file
   /// extension. For example, "c" would return Language::C.
--- clang/test/CIR/analysis-only.cpp
+++
@@ -1,2 +0,0 @@
-// RUN: %clang %s -fclangir-analysis-only -### -c %s 2>&1 | FileCheck %s
-// CHECK: "-fclangir-analysis-only"
