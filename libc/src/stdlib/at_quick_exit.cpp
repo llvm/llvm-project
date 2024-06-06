8fb694541567719ab18fadcd4c2b2832d253418e
aaryanshukla <53713108+aaryanshukla@users.noreply.github.com>
2024-06-06 17:21:44 +0000

    [libc] at_quick_exit function implemented (#94317)
    - added at_quick_exit function 
    - used helper file exit_handler which reuses code from atexit
    - atexit now calls helper functions from exit_handler
    - test cases and dependencies are added
    
    ---------
    
    Co-authored-by: Aaryan Shukla <aaryanshukla@google.com>

--- libc/src/stdlib/at_quick_exit.cpp
+++ libc/src/stdlib/at_quick_exit.cpp
@@ -1,4 +1,4 @@
-//===-- Implementation of quick_exit --------------------------------------===//
+//===-- Implementation of at_quick_exit -----------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,19 +6,17 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "src/stdlib/quick_exit.h"
-#include "src/__support/OSUtil/exit.h"
+#include "src/stdlib/at_quick_exit.h"
+#include "hdr/types/atexithandler_t.h"
 #include "src/__support/common.h"
 #include "src/stdlib/exit_handler.h"
 
-// extern "C" void __cxa_finalize(void *);
 namespace LIBC_NAMESPACE {
 
-extern ExitCallbackList at_quick_exit_callbacks;
-
-[[noreturn]] LLVM_LIBC_FUNCTION(void, quick_exit, (int status)) {
-  call_exit_callbacks(at_quick_exit_callbacks);
-  internal::exit(status);
+LLVM_LIBC_FUNCTION(int, at_quick_exit, (__atexithandler_t callback)) {
+  return add_atexit_unit(
+      at_quick_exit_callbacks,
+      {&stdc_at_exit_func, reinterpret_cast<void *>(callback)});
 }
 
 } // namespace LIBC_NAMESPACE
