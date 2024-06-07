c41adb2e70b263a9847e0e69254be295191ed635
Job Hernandez <hj93@protonmail.com>
2024-06-07 15:00:50 -0700

    refactor

--- libc/src/math/generic/fmul.cpp
+++ libc/src/math/generic/fmul.cpp
@@ -7,19 +7,16 @@
 //===----------------------------------------------------------------------===//
 
 #include "src/math/fmul.h"
-#include "src/__support/CPP/bit.h"
 #include "src/__support/FPUtil/BasicOperations.h"
 #include "src/__support/FPUtil/FPBits.h"
 #include "src/__support/FPUtil/rounding_mode.h"
-#include "src/__support/common.h"
 #include "src/__support/uint128.h"
+#include "src/__support/common.h"
+#include "src/__support/CPP/bit.h"
 
 namespace LIBC_NAMESPACE {
-
-namespace Fmul {
-
-float fmul(double x, double y) {
-
+  
+LLVM_LIBC_FUNCTION(float, fmul, (double x, double y)) {
   auto x_bits = fputil::FPBits<double>(x);
   uint64_t x_u = x_bits.uintval();
 
@@ -28,24 +25,19 @@ float fmul(double x, double y) {
 
   auto output_sign = (x_bits.sign() != y_bits.sign()) ? Sign::NEG : Sign::POS;
 
-  if (LIBC_UNLIKELY(x_bits.is_inf_or_nan() || y_bits.is_inf_or_nan() ||
-                    x_bits.is_zero() || y_bits.is_zero())) {
-    if (x_bits.is_nan())
-      return static_cast<float>(x);
-    if (y_bits.is_nan())
-      return static_cast<float>(y);
-    if (x_bits.is_inf())
-      return y_bits.is_zero()
-                 ? fputil::FPBits<float>::quiet_nan().get_val()
-                 : fputil::FPBits<float>::inf(output_sign).get_val();
-    if (y_bits.is_inf())
-      return x_bits.is_zero()
-                 ? fputil::FPBits<float>::quiet_nan().get_val()
-                 : fputil::FPBits<float>::inf(output_sign).get_val();
+    if (LIBC_UNLIKELY(x_bits.is_inf_or_nan() || y_bits.is_inf_or_nan() || x_bits.is_zero() || y_bits.is_zero())) {
+      if (x_bits.is_nan())
+	return static_cast<float>(x);
+      if (y_bits.is_nan())
+	return static_cast<float>(y);
+      if (x_bits.is_inf())
+	return y_bits.is_zero() ? fputil::FPBits<float>::quiet_nan().get_val() : fputil::FPBits<float>::inf(output_sign).get_val();
+      if (y_bits.is_inf())
+	return x_bits.is_zero() ? fputil::FPBits<float>::quiet_nan().get_val() : fputil::FPBits<float>::inf(output_sign).get_val();
     // Now either x or y is zero, and the other one is finite.
-    return fputil::FPBits<float>::zero(output_sign).get_val();
+      return fputil::FPBits<float>::zero(output_sign).get_val();
   }
-
+ 
   uint64_t mx, my;
 
   // Get mantissa and append the hidden bit if needed.
@@ -63,15 +55,13 @@ float fmul(double x, double y) {
   mx <<= nx;
   my <<= ny;
 
-  // Adjust exponent accordingly: If x or y are normal, we will only need to
-  // shift by (exponent length + sign bit = 11 bits. If x or y are denormal, we
-  // will need to shift more than 11 bits.
+  // Adjust exponent accordingly: If x or y are normal, we will only need to shift by (exponent length + sign bit = 11 bits. If x or y are denormal, we will need to shift more than 11 bits.
   ex -= (nx - 11);
   ey -= (ny - 11);
-
+  
   UInt128 product = static_cast<UInt128>(mx) * static_cast<UInt128>(my);
   int32_t dm1;
-  uint64_t highs, lows, b;
+  uint64_t  highs, lows, b;
   uint64_t g, hight, lowt, m;
   bool c;
 
@@ -85,13 +75,12 @@ float fmul(double x, double y) {
   dm1 = ex + ey + cint + fputil::FPBits<float>::EXP_BIAS;
 
   uint32_t sr = static_cast<uint32_t>((x_u ^ y_u) & 0x8000000000000000);
-  Sign prod_sign = (sr == 1) ? Sign::NEG : Sign::POS;
   int round_mode = fputil::quick_get_round();
   if (dm1 >= 255) {
     if ((round_mode == FE_TOWARDZERO) ||
-        (round_mode == FE_UPWARD && prod_sign.is_neg()) ||
-        (round_mode == FE_DOWNWARD && prod_sign.is_pos())) {
-      return fputil::FPBits<float>::max_normal(prod_sign).get_val();
+        (round_mode == FE_UPWARD && output_sign.is_neg()) ||
+        (round_mode == FE_DOWNWARD && output_sign.is_pos())) {
+      return fputil::FPBits<float>::max_normal(output_sign).get_val();
     }
     return fputil::FPBits<float>::inf().get_val();
   } else if (dm1 <= 0) {
@@ -113,29 +102,23 @@ float fmul(double x, double y) {
     b = g && ((hight && lowt) || ((m & 1) != 0));
   } else if (round_mode == FE_TOWARDZERO) {
     b = 0;
-  } else if ((sr == 1 && round_mode == FE_DOWNWARD) ||
-             (sr == 0 && round_mode == FE_UPWARD)) {
+  } else if ((output_sign.is_neg() && round_mode == FE_DOWNWARD) ||
+             (output_sign.is_pos() && round_mode == FE_UPWARD)) {
     b = (g == 0 && (hight && lowt) == 0) ? 0 : 1;
   } else {
     b = 0;
   }
 
+  
   uint32_t exp16 = sr | (dm1 << 23);
 
   constexpr uint32_t FLOAT32_MANTISSA_MASK = 0b00000000011111111111111111111111;
   uint32_t m2 = static_cast<uint32_t>(m) & FLOAT32_MANTISSA_MASK;
 
   uint32_t result =
-      (static_cast<uint32_t>(exp16) + m2) + static_cast<uint32_t>(b);
+        (static_cast<uint32_t>(exp16) + m2) + static_cast<uint32_t>(b);
 
-  float result32 = cpp::bit_cast<float>(result);
-
-  return result32;
-}
-} // namespace Fmul
-
-LLVM_LIBC_FUNCTION(float, fmul, (double x, double y)) {
-  return Fmul::fmul(x, y);
+  return cpp::bit_cast<float>(result);
 }
 
 } // namespace LIBC_NAMESPACE
