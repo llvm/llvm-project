16ecc5e617b9ab7bf7dcf0fb853de8d6a8442a1f
Kazu Hirata <kazu@google.com>
2024-06-08 12:25:01 -0700

    [memprof] Add InstrProfWriter::addMemProfData
    This patch adds InstrProfWriter::addMemProfData, which adds the
    complete MemProf profile (frames, call stacks, and records) to the
    writer context.
    
    Without this function, functions like loadInput in llvm-profdata.cpp
    and InstrProfWriter::mergeRecordsFromWriter must add one item (frame,
    call stack, or record) at a time.  The new function std::moves the
    entire MemProf profile to the writer context if the destination is
    empty, which is the common use case.  Otherwise, we fall back to
    adding one item at a time behind the scene.
    
    Here are a couple of reasons why we should add this function:
    
    - We've had a bug where we forgot to add one of the three data
      structures (frames, call stacks, and records) to the writer context,
      resulting in a nearly empty indexed profile.  We should always
      package the three data structures together, especially on API
      boundaries.
    
    - We expose a little too much of the MemProf detail to
      InstrProfWriter.  I'd like to gradually transform
      InstrProfReader/Writer to entities managing buffers (sequences of
      bytes), with actual serialization/deserialization left to external
      classes.  We already do some of this in InstrProfReader, where
      InstrProfReader "contracts out" to IndexedMemProfReader to handle
      MemProf details.
    
    I am not changing loadInput or InstrProfWriter::mergeRecordsFromWriter
    for now because MemProfReader uses DenseMap for frames and call
    stacks, whereas MemProfData uses MapVector.  I'll resolve these
    mismatches in subsequent patches.

--- llvm/lib/ProfileData/InstrProfWriter.cpp
+++ llvm/lib/ProfileData/InstrProfWriter.cpp
@@ -350,6 +350,31 @@ bool InstrProfWriter::addMemProfCallStack(
   return true;
 }
 
+bool InstrProfWriter::addMemProfData(memprof::IndexedMemProfData Incoming,
+                                     function_ref<void(Error)> Warn) {
+  if (MemProfData.Frames.empty())
+    MemProfData.Frames = std::move(Incoming.Frames);
+  else
+    for (const auto &[Id, F] : Incoming.Frames)
+      if (addMemProfFrame(Id, F, Warn))
+        return false;
+
+  if (MemProfData.CallStacks.empty())
+    MemProfData.CallStacks = std::move(Incoming.CallStacks);
+  else
+    for (const auto &[CSId, CS] : Incoming.CallStacks)
+      if (addMemProfCallStack(CSId, CS, Warn))
+        return false;
+
+  if (MemProfData.Records.empty())
+    MemProfData.Records = std::move(Incoming.Records);
+  else
+    for (const auto &[GUID, Record] : Incoming.Records)
+      addMemProfRecord(GUID, Record);
+
+  return true;
+}
+
 void InstrProfWriter::addBinaryIds(ArrayRef<llvm::object::BuildID> BIs) {
   llvm::append_range(BinaryIds, BIs);
 }
