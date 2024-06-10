b6ef071dd4af3c19e881f9bb396e467f98da5fa2
Ethan Luis McDonough <ethanluismcdonough@gmail.com>
2024-06-10 15:01:19 -0500

    Address requested changes

--- llvm/lib/Transforms/IPO/AttributorAttributes.cpp
+++ llvm/lib/Transforms/IPO/AttributorAttributes.cpp
@@ -12031,7 +12031,7 @@ struct AAGlobalValueInfoFloating : public AAGlobalValueInfo {
                 SmallVectorImpl<const Value *> &Worklist) {
     Instruction *UInst = dyn_cast<Instruction>(U.getUser());
     if (!UInst) {
-      Follow = !Uses.contains(&U);
+      Follow = true;
       return true;
     }
 
