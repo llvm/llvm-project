809adec88edb2cdca5c97d612865c9789233d3c3
Paul Kirth <paulkirth@google.com>
2024-06-10 16:43:14 -0700

    Revert "[llvm][IR] Extend BranchWeightMetadata to track provenance of weights…"
    This reverts commit c5978f1eb5eeca8610b9dfce1fcbf1f473911cd8.

--- llvm/lib/Transforms/Scalar/JumpThreading.cpp
+++ llvm/lib/Transforms/Scalar/JumpThreading.cpp
@@ -231,7 +231,7 @@ static void updatePredecessorProfileMetadata(PHINode *PN, BasicBlock *BB) {
       Weights[0] = BP.getCompl().getNumerator();
       Weights[1] = BP.getNumerator();
     }
-    setBranchWeights(*PredBr, Weights, hasBranchWeightOrigin(*PredBr));
+    setBranchWeights(*PredBr, Weights);
   }
 }
 
@@ -2618,7 +2618,7 @@ void JumpThreadingPass::updateBlockFreqAndEdgeWeight(BasicBlock *PredBB,
       Weights.push_back(Prob.getNumerator());
 
     auto TI = BB->getTerminator();
-    setBranchWeights(*TI, Weights, hasBranchWeightOrigin(*TI));
+    setBranchWeights(*TI, Weights);
   }
 }
 
