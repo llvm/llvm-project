5eba555bf6aaf042a9b3c7783bcce3efd9c04985
Zbigniew Sarbinowski <zibi@ca.ibm.com>
2024-06-10 13:32:52 +0000

    Make __find_first_set endianness aware

--- libcxx/include/__algorithm/mismatch.h
+++ libcxx/include/__algorithm/mismatch.h
@@ -22,7 +22,6 @@
 #include <__type_traits/is_constant_evaluated.h>
 #include <__type_traits/is_equality_comparable.h>
 #include <__type_traits/is_integral.h>
-#include <__utility/integer_sequence.h>
 #include <__utility/move.h>
 #include <__utility/pair.h>
 #include <__utility/unreachable.h>
@@ -56,32 +55,7 @@ __mismatch(_Iter1 __first1, _Sent1 __last1, _Iter2 __first2, _Pred& __pred, _Pro
 }
 
 #if _LIBCPP_VECTORIZE_ALGORITHMS
-template <class _ValueType, size_t _Np>
-_LIBCPP_NODISCARD _LIBCPP_HIDE_FROM_ABI __simd_vector<long long, _Np>
-__reverse_vector(__simd_vector<long long, _Np> __cmp_res) {
-  return [&]<size_t... _Indices>(index_sequence<_Indices...>) {
-    return __builtin_shufflevector(__cmp_res, __cmp_res, (_Np - _Indices - 1)...);
-  }(make_index_sequence<_Np>{});
-}
-template <class _ValueType, size_t _Np>
-_LIBCPP_NODISCARD _LIBCPP_HIDE_FROM_ABI __simd_vector<long, _Np> __reverse_vector(__simd_vector<long, _Np> __cmp_res) {
-  return [&]<size_t... _Indices>(index_sequence<_Indices...>) {
-    return __builtin_shufflevector(__cmp_res, __cmp_res, (_Np - _Indices - 1)...);
-  }(make_index_sequence<_Np>{});
-}
-template <class _ValueType, size_t _Np>
-_LIBCPP_NODISCARD _LIBCPP_HIDE_FROM_ABI __simd_vector<int, _Np> __reverse_vector(__simd_vector<int, _Np> __cmp_res) {
-  return [&]<size_t... _Indices>(index_sequence<_Indices...>) {
-    return __builtin_shufflevector(__cmp_res, __cmp_res, (_Np - _Indices - 1)...);
-  }(make_index_sequence<_Np>{});
-}
-template <class _ValueType, size_t _Np>
-_LIBCPP_NODISCARD _LIBCPP_HIDE_FROM_ABI __simd_vector<_ValueType, _Np>
-__reverse_vector(__simd_vector<_ValueType, _Np> __cmp_res) {
-  return [&]<size_t... _Indices>(index_sequence<_Indices...>) {
-    return __builtin_shufflevector(__cmp_res, __cmp_res, (_Np - _Indices - 1)...);
-  }(make_index_sequence<_Np>{});
-}
+
 template <class _Iter>
 _LIBCPP_NODISCARD _LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX20 pair<_Iter, _Iter>
 __mismatch_vectorized(_Iter __first1, _Iter __last1, _Iter __first2) {
@@ -103,11 +77,7 @@ __mismatch_vectorized(_Iter __first1, _Iter __last1, _Iter __first2) {
       }
 
       for (size_t __i = 0; __i != __unroll_count; ++__i) {
-        auto __cmp_res = __lhs[__i] == __rhs[__i];
-#  if defined(_LIBCPP_BIG_ENDIAN)
-        __cmp_res = std::__reverse_vector<__value_type>(__cmp_res);
-#  endif
-        if (!std::__all_of(__cmp_res)) {
+        if (auto __cmp_res = __lhs[__i] == __rhs[__i]; !std::__all_of(__cmp_res)) {
           auto __offset = __i * __vec_size + std::__find_first_not_set(__cmp_res);
           return {__first1 + __offset, __first2 + __offset};
         }
@@ -119,11 +89,8 @@ __mismatch_vectorized(_Iter __first1, _Iter __last1, _Iter __first2) {
 
     // check the remaining 0-3 vectors
     while (static_cast<size_t>(__last1 - __first1) >= __vec_size) {
-      auto __cmp_res = std::__load_vector<__vec>(__first1) == std::__load_vector<__vec>(__first2);
-#  if defined(_LIBCPP_BIG_ENDIAN)
-      __cmp_res = std::__reverse_vector<__value_type>(__cmp_res);
-#  endif
-      if (!std::__all_of(__cmp_res)) {
+      if (auto __cmp_res = std::__load_vector<__vec>(__first1) == std::__load_vector<__vec>(__first2);
+          !std::__all_of(__cmp_res)) {
         auto __offset = std::__find_first_not_set(__cmp_res);
         return {__first1 + __offset, __first2 + __offset};
       }
@@ -139,11 +106,8 @@ __mismatch_vectorized(_Iter __first1, _Iter __last1, _Iter __first2) {
     if (static_cast<size_t>(__first1 - __orig_first1) >= __vec_size) {
       __first1 = __last1 - __vec_size;
       __first2 = __last2 - __vec_size;
-      auto __cmp_res = std::__load_vector<__vec>(__first1) == std::__load_vector<__vec>(__first2);
-#  if defined(_LIBCPP_BIG_ENDIAN)
-      __cmp_res = std::__reverse_vector<__value_type>(__cmp_res);
-#  endif
-      auto __offset = std::__find_first_not_set(__cmp_res);
+      auto __offset =
+          std::__find_first_not_set(std::__load_vector<__vec>(__first1) == std::__load_vector<__vec>(__first2));
       return {__first1 + __offset, __first2 + __offset};
     } // else loop over the elements individually
   }
