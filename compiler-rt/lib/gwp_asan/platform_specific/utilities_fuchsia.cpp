e320875facdeb2d0d8193873cd6d0db8505f9981
Mitch Phillips <mitchp@google.com>
2024-06-10 10:39:52 +0200

    Log errno (or fuchsia equivalent) on map failures
    A feature requested by Android, we should log errno (or the
    corresponding fuchsia error status) as part of the message when
    mmap/mprotect/etc. fails.
    
    Fuchsia friends - can you please validate this patch?

--- compiler-rt/lib/gwp_asan/platform_specific/utilities_fuchsia.cpp
+++ compiler-rt/lib/gwp_asan/platform_specific/utilities_fuchsia.cpp
@@ -8,12 +8,23 @@
 
 #include "gwp_asan/utilities.h"
 
+#include <alloca.h>
+#include <inttypes.h>
 #include <string.h>
 #include <zircon/sanitizer.h>
 
 namespace gwp_asan {
 void die(const char *Message) {
   __sanitizer_log_write(Message, strlen(Message));
+  __builtin_trap();
+}
+
+void dieWithErrorCode(const char *Message, int64_t ErrorCode) {
+  size_t buffer_size = strlen(Message) + 48;
+  char *buffer = static_cast<char *>(alloca(strlen(Message) + 48));
+  snprintf(buffer, buffer_size, "%s (Error Code: %" PRId64 ")", Message,
+           ErrorCode);
+  __sanitizer_log_write(buffer, strlen(buffer));
   __builtin_trap();
 }
 } // namespace gwp_asan
