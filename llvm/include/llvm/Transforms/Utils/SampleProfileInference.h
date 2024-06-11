cc52e147b1b211e9df15a0c5dd94b8c54b383a82
shawbyoung <shawbyoung@gmail.com>
2024-06-11 11:07:54 -0700

    [𝘀𝗽𝗿] changes to main this commit is based on
    Created using spr 1.3.4
    
    [skip ci]

--- llvm/include/llvm/Transforms/Utils/SampleProfileInference.h
+++ llvm/include/llvm/Transforms/Utils/SampleProfileInference.h
@@ -28,6 +28,7 @@ struct FlowBlock {
   uint64_t Weight{0};
   bool HasUnknownWeight{true};
   bool IsUnlikely{false};
+  bool IsExit{false};
   uint64_t Flow{0};
   std::vector<FlowJump *> SuccJumps;
   std::vector<FlowJump *> PredJumps;
@@ -57,6 +58,7 @@ struct FlowFunction {
   std::vector<FlowJump> Jumps;
   /// The index of the entry block.
   uint64_t Entry{0};
+  uint64_t Sink{UINT64_MAX};
 };
 
 /// Various thresholds and options controlling the behavior of the profile
