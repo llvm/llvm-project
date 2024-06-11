0e3d60906d390990f029baa226b76bc55927f27a
Congcong Cai <congcongcai0907@163.com>
2024-06-11 11:24:40 +0800

    Update clang-tools-extra/test/clang-tidy/checkers/misc/include-cleaner.cpp

--- clang-tools-extra/test/clang-tidy/checkers/misc/include-cleaner.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/misc/include-cleaner.cpp
@@ -16,4 +16,10 @@ std::string HelloString;
 int FooBarResult = foobar();
 // CHECK-MESSAGES: :[[@LINE-1]]:20: warning: no header providing "foobar" is directly included [misc-include-cleaner]
 
+namespace valid {
+
+namespace gh93335 {
 void log2() {}
+} // namespace gh93335
+
+} // namespace valid
