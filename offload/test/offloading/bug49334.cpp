50bf48345673ae01438e610d3c4d2b41bdaad101
Ethan Luis McDonough <ethanluismcdonough@gmail.com>
2024-06-11 19:44:10 -0500

    Modify C offload tests

--- offload/test/offloading/bug49334.cpp
+++ offload/test/offloading/bug49334.cpp
@@ -5,12 +5,7 @@
 // RUN: %libomptarget-compileoptxx-generic -O3 -ffast-math && \
 // RUN: %libomptarget-run-generic
 
-// UNSUPPORTED: x86_64-pc-linux-gnu
-// UNSUPPORTED: x86_64-pc-linux-gnu-LTO
-// UNSUPPORTED: aarch64-unknown-linux-gnu
-// UNSUPPORTED: aarch64-unknown-linux-gnu-LTO
-// UNSUPPORTED: s390x-ibm-linux-gnu
-// UNSUPPORTED: s390x-ibm-linux-gnu-LTO
+// REQUIRES: gpu
 // UNSUPPORTED: amdgcn-amd-amdhsa
 // UNSUPPORTED: nvptx64-nvidia-cuda
 // UNSUPPORTED: nvptx64-nvidia-cuda-LTO
