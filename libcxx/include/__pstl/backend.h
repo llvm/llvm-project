827ce898a1e07a47c432c63bccfecac9b4ed4978
Louis Dionne <ldionne.2@gmail.com>
2024-06-12 12:24:34 -0400

    [libc++] Overhaul the PSTL dispatching mechanism (#88131)
    The experimental PSTL's current dispatching mechanism was designed with
    flexibility in mind. However, while reviewing the in-progress OpenMP
    backend, I realized that the dispatching mechanism based on ADL and
    default definitions in the frontend had several downsides. To name a
    few:
    
    1. The dispatching of an algorithm to the back-end and its default
       implementation is bundled together via `_LIBCPP_PSTL_CUSTOMIZATION_POINT`.
       This makes the dispatching really confusing and leads to annoyances
       such as variable shadowing and weird lambda captures in the front-end.
    2. The distinction between back-end functions and front-end algorithms
       is not as clear as it could be, which led us to call one where we meant
       the other in a few cases. This is bad due to the exception requirements
       of the PSTL: calling a front-end algorithm inside the implementation of
       a back-end is incorrect for exception-safety.
    3. There are two levels of back-end dispatching in the PSTL, which treat
       CPU backends as a special case. This was confusing and not as flexible
       as we'd like. For example, there was no straightforward way to dispatch
       all uses of `unseq` to a specific back-end from the OpenMP backend,
       or for CPU backends to fall back on each other.
    
    This patch rewrites the backend dispatching mechanism to solve these
    problems, but doesn't touch any of the actual implementation of
    algorithms. Specifically, this rewrite has the following
    characteristics:
    
    - There is a single level of backend dispatching, however partial backends can
      be stacked to provide a full implementation of the PSTL. The two-level dispatching
      that was used for CPU-based backends is handled by providing CPU-based basis 
      operations as simple helpers that can easily be reused when defining any PSTL 
      backend.
    
    - The default definitions for algorithms are separated from their dispatching logic.
    
    - The front-end is thus simplified a whole lot and made very consistent
      for all algorithms, which makes it easier to audit the front-end for
      things like exception-correctness, appropriate forwarding, etc.
    
    Fixes #70718

--- libcxx/include/__pstl/backend.h
+++ libcxx/include/__pstl/backend.h
@@ -6,22 +6,30 @@
 //
 //===----------------------------------------------------------------------===//
 
-#ifndef _LIBCPP___PSTL_CONFIGURATION_H
-#define _LIBCPP___PSTL_CONFIGURATION_H
+#ifndef _LIBCPP___PSTL_BACKEND_H
+#define _LIBCPP___PSTL_BACKEND_H
 
 #include <__config>
-#include <__pstl/configuration_fwd.h>
+#include <__pstl/backend_fwd.h>
 
 #if !defined(_LIBCPP_HAS_NO_PRAGMA_SYSTEM_HEADER)
 #  pragma GCC system_header
 #endif
 
+_LIBCPP_PUSH_MACROS
+#include <__undef_macros>
+
 #if defined(_LIBCPP_PSTL_BACKEND_SERIAL)
+#  include <__pstl/backends/default.h>
 #  include <__pstl/backends/serial.h>
 #elif defined(_LIBCPP_PSTL_BACKEND_STD_THREAD)
+#  include <__pstl/backends/default.h>
 #  include <__pstl/backends/std_thread.h>
 #elif defined(_LIBCPP_PSTL_BACKEND_LIBDISPATCH)
+#  include <__pstl/backends/default.h>
 #  include <__pstl/backends/libdispatch.h>
 #endif
 
-#endif // _LIBCPP___PSTL_CONFIGURATION_H
+_LIBCPP_POP_MACROS
+
+#endif // _LIBCPP___PSTL_BACKEND_H
