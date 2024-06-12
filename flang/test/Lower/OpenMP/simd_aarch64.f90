7473c5b686f8ab8850218a7d3132fa43fa7d5de2
harishch4 <harishcse44@gmail.com>
2024-06-12 19:37:45 +0530

    Update flang/test/Lower/OpenMP/simd_aarch64.f90
    Co-authored-by: Tom Eccles <t@freedommail.info>

--- flang/test/Lower/OpenMP/simd_aarch64.f90
+++ flang/test/Lower/OpenMP/simd_aarch64.f90
@@ -1,5 +1,5 @@
 ! Tests for 2.9.3.1 Simd and target dependent defult alignment for AArch64
-! The default alignment for AARch is 0 so we do not emit aligned clause
+! The default alignment for AARCH64 is 0 so we do not emit aligned clause
 ! REQUIRES: aarch64-registered-target
 ! RUN: %flang_fc1 -triple aarch64-unknown-linux-gnu -emit-hlfir -fopenmp %s -o - | FileCheck  %s
 subroutine simdloop_aligned_cptr(A)
