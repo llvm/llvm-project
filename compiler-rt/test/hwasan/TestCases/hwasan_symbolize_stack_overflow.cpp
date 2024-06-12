c4e41d1f899520cd6ca031dd0ecb90a1306b801e
Florian Mayer <fmayer@google.com>
2024-06-12 13:51:08 -0700

    more test
    Created using spr 1.3.4

--- compiler-rt/test/hwasan/TestCases/hwasan_symbolize_stack_overflow.cpp
+++ compiler-rt/test/hwasan/TestCases/hwasan_symbolize_stack_overflow.cpp
@@ -5,6 +5,7 @@
 // RUN: %env_hwasan_opts=symbolize=0 not %run %t -17 2>&1 | hwasan_symbolize --symbols $(dirname %t) --index | FileCheck %s --check-prefixes=CHECK,BEFORE17
 // RUN: %env_hwasan_opts=symbolize=0 not %run %t 1016 2>&1 | hwasan_symbolize --symbols $(dirname %t) --index | FileCheck %s --check-prefixes=CHECK,AFTER1000
 // RUN: %env_hwasan_opts=symbolize=0 not %run %t -1000 2>&1 | hwasan_symbolize --symbols $(dirname %t) --index | FileCheck %s --check-prefixes=CHECK,BEFORE1000
+// RUN: %env_hwasan_opts=symbolize=0 not %run %t 1000000 2>&1 | hwasan_symbolize --symbols $(dirname %t) --index | FileCheck %s --check-prefixes=TOOFAR
 
 // REQUIRES: pointer-tagging
 
@@ -16,6 +17,7 @@ static volatile char sink;
 int main(int argc, char **argv) {
   volatile char x[16];
   sink = x[atoi(argv[1])];
+  // TOOFAR-NOT: Potentially referenced stack object:
   // CHECK: Potentially referenced stack object:
   // AFTER0:   0 bytes after a variable "x" in stack frame of function "main"
   // AFTER1:   1 bytes after a variable "x" in stack frame of function "main"
@@ -23,6 +25,6 @@ int main(int argc, char **argv) {
   // BEFORE17:   17 bytes before a variable "x" in stack frame of function "main"
   // BEFORE1000:   1000 bytes before a variable "x" in stack frame of function "main"
   // AFTER1000:   1000 bytes after a variable "x" in stack frame of function "main"
-  // CHECK:   at {{.*}}hwasan_symbolize_stack_overflow.cpp:[[@LINE-9]]
+  // CHECK:   at {{.*}}hwasan_symbolize_stack_overflow.cpp:[[@LINE-10]]
   return 0;
 }
