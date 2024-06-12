bd8dde98f287a493c2689cff4e0520323d4105a2
Louis Dionne <ldionne.2@gmail.com>
2024-06-12 12:24:34 -0400

    [libc++] Overhaul the PSTL dispatching mechanism (#88131)
    The experimental PSTL's current dispatching mechanism was designed with
    flexibility in mind. However, while reviewing the in-progress OpenMP
    backend, I realized that the dispatching mechanism based on ADL and
    default definitions in the frontend had several downsides. To name a
    few:
    
    1. The dispatching of an algorithm to the back-end and its default
       implementation is bundled together via `_LIBCPP_PSTL_CUSTOMIZATION_POINT`.
       This makes the dispatching really confusing and leads to annoyances
       such as variable shadowing and weird lambda captures in the front-end.
    2. The distinction between back-end functions and front-end algorithms
       is not as clear as it could be, which led us to call one where we meant
       the other in a few cases. This is bad due to the exception requirements
       of the PSTL: calling a front-end algorithm inside the implementation of
       a back-end is incorrect for exception-safety.
    3. There are two levels of back-end dispatching in the PSTL, which treat
       CPU backends as a special case. This was confusing and not as flexible
       as we'd like. For example, there was no straightforward way to dispatch
       all uses of `unseq` to a specific back-end from the OpenMP backend,
       or for CPU backends to fall back on each other.
    
    This patch rewrites the backend dispatching mechanism to solve these
    problems, but doesn't touch any of the actual implementation of
    algorithms. Specifically, this rewrite has the following
    characteristics:
    
    - There is a single level of backend dispatching, however partial backends can
      be stacked to provide a full implementation of the PSTL. The two-level dispatching
      that was used for CPU-based backends is handled by providing CPU-based basis 
      operations as simple helpers that can easily be reused when defining any PSTL 
      backend.
    
    - The default definitions for algorithms are separated from their dispatching logic.
    
    - The front-end is thus simplified a whole lot and made very consistent
      for all algorithms, which makes it easier to audit the front-end for
      things like exception-correctness, appropriate forwarding, etc.
    
    Fixes #70718

--- libcxx/include/__pstl/cpu_algos/for_each.h
+++ libcxx/include/__pstl/cpu_algos/for_each.h
@@ -10,9 +10,10 @@
 #define _LIBCPP___PSTL_CPU_ALGOS_FOR_EACH_H
 
 #include <__algorithm/for_each.h>
+#include <__assert>
 #include <__config>
 #include <__iterator/concepts.h>
-#include <__pstl/configuration_fwd.h>
+#include <__pstl/backend_fwd.h>
 #include <__pstl/cpu_algos/cpu_traits.h>
 #include <__type_traits/is_execution_policy.h>
 #include <__utility/empty.h>
@@ -35,26 +36,30 @@ _LIBCPP_HIDE_FROM_ABI _Iterator __simd_walk(_Iterator __first, _DifferenceType _
   return __first + __n;
 }
 
-template <class _ExecutionPolicy, class _ForwardIterator, class _Functor>
-_LIBCPP_HIDE_FROM_ABI optional<__empty>
-__pstl_for_each(__cpu_backend_tag, _ForwardIterator __first, _ForwardIterator __last, _Functor __func) {
-  if constexpr (__is_parallel_execution_policy_v<_ExecutionPolicy> &&
-                __has_random_access_iterator_category_or_concept<_ForwardIterator>::value) {
-    return __pstl::__cpu_traits<__cpu_backend_tag>::__for_each(
-        __first, __last, [__func](_ForwardIterator __brick_first, _ForwardIterator __brick_last) {
-          [[maybe_unused]] auto __res = std::__pstl_for_each<__remove_parallel_policy_t<_ExecutionPolicy>>(
-              __cpu_backend_tag{}, __brick_first, __brick_last, __func);
-          _LIBCPP_ASSERT_INTERNAL(__res, "unseq/seq should never try to allocate!");
-        });
-  } else if constexpr (__is_unsequenced_execution_policy_v<_ExecutionPolicy> &&
-                       __has_random_access_iterator_category_or_concept<_ForwardIterator>::value) {
-    std::__simd_walk(__first, __last - __first, __func);
-    return __empty{};
-  } else {
-    std::for_each(__first, __last, __func);
-    return __empty{};
+template <class _Backend, class _RawExecutionPolicy>
+struct __cpu_parallel_for_each {
+  template <class _Policy, class _ForwardIterator, class _Function>
+  _LIBCPP_HIDE_FROM_ABI optional<__empty>
+  operator()(_Policy&& __policy, _ForwardIterator __first, _ForwardIterator __last, _Function __func) const noexcept {
+    if constexpr (__is_parallel_execution_policy_v<_RawExecutionPolicy> &&
+                  __has_random_access_iterator_category_or_concept<_ForwardIterator>::value) {
+      return __pstl::__cpu_traits<_Backend>::__for_each(
+          __first, __last, [&__policy, __func](_ForwardIterator __brick_first, _ForwardIterator __brick_last) {
+            using _ForEachUnseq = __pstl::__for_each<_Backend, __remove_parallel_policy_t<_RawExecutionPolicy>>;
+            [[maybe_unused]] auto __res =
+                _ForEachUnseq()(std::__remove_parallel_policy(__policy), __brick_first, __brick_last, __func);
+            _LIBCPP_ASSERT_INTERNAL(__res, "unseq/seq should never try to allocate!");
+          });
+    } else if constexpr (__is_unsequenced_execution_policy_v<_RawExecutionPolicy> &&
+                         __has_random_access_iterator_category_or_concept<_ForwardIterator>::value) {
+      std::__simd_walk(__first, __last - __first, __func);
+      return __empty{};
+    } else {
+      std::for_each(__first, __last, __func);
+      return __empty{};
+    }
   }
-}
+};
 
 _LIBCPP_END_NAMESPACE_STD
 
