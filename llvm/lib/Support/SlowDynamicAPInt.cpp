013368569fa9e0de898142b811fe4cc667d899e2
Ramkumar Ramachandra <ramkumar.ramachandra@codasip.com>
2024-06-12 09:00:09 +0100

    DynamicAPInt: fix spelling in header comments

--- llvm/lib/Support/SlowDynamicAPInt.cpp
+++ llvm/lib/Support/SlowDynamicAPInt.cpp
@@ -1,4 +1,4 @@
-//===- SlowDynamicAPInt.cpp - SlowDyanmicAPInt Implementation -------------===//
+//===- SlowDynamicAPInt.cpp - SlowDynamicAPInt Implementation -------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
