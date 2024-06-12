87b384bfd3536519f641e3ed3e2a6774140f7704
pradeepku <pradeepku@nvidia.com>
2024-06-12 12:59:02 +0000

    [MLIR][GPU] Add gpu.cluster_dim_blocks and gpu.cluster_block_id Ops
    This commit adds support for `gpu.cluster_dim_blocks` and `gpu.cluster_block_id`
    Ops to represent number of blocks per cluster and block id inside a cluster respectively.
    Also, fixed the description of `gpu.cluster_dim` Op and updated the `cga_cluster.mlir`
    test file to use `gpu.cluster_dim_blocks`

--- mlir/lib/Dialect/GPU/IR/InferIntRangeInterfaceImpls.cpp
+++ mlir/lib/Dialect/GPU/IR/InferIntRangeInterfaceImpls.cpp
@@ -86,11 +86,22 @@ static std::optional<uint64_t> getKnownLaunchDim(Op op, LaunchDims type) {
 
 void ClusterDimOp::inferResultRanges(ArrayRef<ConstantIntRanges>,
                                      SetIntRangeFn setResultRange) {
+  setResultRange(getResult(), getIndexRange(1, kMaxDim));
+}
+
+void ClusterDimBlocksOp::inferResultRanges(ArrayRef<ConstantIntRanges>,
+                                           SetIntRangeFn setResultRange) {
   setResultRange(getResult(), getIndexRange(1, kMaxClusterDim));
 }
 
 void ClusterIdOp::inferResultRanges(ArrayRef<ConstantIntRanges>,
                                     SetIntRangeFn setResultRange) {
+  uint64_t max = kMaxClusterDim;
+  setResultRange(getResult(), getIndexRange(0, max - 1ULL));
+}
+
+void ClusterBlockIdOp::inferResultRanges(ArrayRef<ConstantIntRanges>,
+                                         SetIntRangeFn setResultRange) {
   uint64_t max = kMaxClusterDim;
   setResultRange(getResult(), getIndexRange(0, max - 1ULL));
 }
