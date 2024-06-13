f1d06c736fe5839723d463e5b6df99ab7094d7c3
Fangrui Song <i@maskray.me>
2024-06-13 11:49:21 -0700

    Revert "Reland "[AArch64] Decouple feature dependency expansion. (#94279)" (#95231)"
    This reverts commit 70510733af33c70ff7877eaf30d7718b9358a725.
    
    The following code is now incorrectly rejected.
    
    ```
    % cat neon.c
    #include <arm_neon.h>
    __attribute__((target("arch=armv8-a")))
    uint64x2_t foo(uint64x2_t a, uint64x2_t b) {
      return veorq_u64(a, b);
    }
    % newclang --target=aarch64-linux-gnu -c neon.c
    neon.c:5:10: error: always_inline function 'veorq_u64' requires target feature 'outline-atomics', but would be inlined into function 'foo' that is compiled without support for 'outline-atomics'
        5 |   return veorq_u64(a, b);
          |          ^
    1 error generated.
    ```
    
    "+outline-atomics" seems misleading here.

--- clang/include/clang/AST/ASTContext.h
+++ clang/include/clang/AST/ASTContext.h
@@ -3210,6 +3210,9 @@ public:
   /// valid feature names.
   ParsedTargetAttr filterFunctionTargetAttrs(const TargetAttr *TD) const;
 
+  std::vector<std::string>
+  filterFunctionTargetVersionAttrs(const TargetVersionAttr *TV) const;
+
   void getFunctionFeatureMap(llvm::StringMap<bool> &FeatureMap,
                              const FunctionDecl *) const;
   void getFunctionFeatureMap(llvm::StringMap<bool> &FeatureMap,
