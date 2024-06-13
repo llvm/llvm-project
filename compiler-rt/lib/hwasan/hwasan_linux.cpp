aee602b65988478470cfccbecb6c4dd8d6bc6717
Florian Mayer <fmayer@google.com>
2024-06-13 10:07:49 -0700

    Revert "[hwasan] Add fixed_shadow_base flag" (#95435)
    Reverts llvm/llvm-project#73980
    
    This broke static hwasan binaries in Android, for some reason the
    fixed_shadow_base branch gets taken

--- compiler-rt/lib/hwasan/hwasan_linux.cpp
+++ compiler-rt/lib/hwasan/hwasan_linux.cpp
@@ -106,12 +106,8 @@ static uptr GetHighMemEnd() {
 }
 
 static void InitializeShadowBaseAddress(uptr shadow_size_bytes) {
-  if (flags()->fixed_shadow_base != (uptr)-1) {
-    __hwasan_shadow_memory_dynamic_address = flags()->fixed_shadow_base;
-  } else {
-    __hwasan_shadow_memory_dynamic_address =
-        FindDynamicShadowStart(shadow_size_bytes);
-  }
+  __hwasan_shadow_memory_dynamic_address =
+      FindDynamicShadowStart(shadow_size_bytes);
 }
 
 static void MaybeDieIfNoTaggingAbi(const char *message) {
