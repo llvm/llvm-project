6784bf764207d267b781b4f515a2fafdcb345509
Ivy Zhang <yan3.zhang@intel.com>
2024-06-13 09:27:44 +0800

    [MLIR][Arith] add fastMathAttr on arith::extf and arith::truncf (#93443)
    Add an `fastMathAttr` on `arith::extf` and `arith::truncf`. If these two
    ops are inserted by some promotion passes (like legalize-to-f32 /
    emulate-unsupported-floats), they will be labeled as
    `FastMathFlags::contract`, denoting that they can be then `eliminated by
    canonicalizer`.
    
    The `elimination` can help improve performance, while may introduce some
    numerical differences.

--- mlir/lib/Dialect/Arith/IR/ArithOps.cpp
+++ mlir/lib/Dialect/Arith/IR/ArithOps.cpp
@@ -1390,6 +1390,20 @@ LogicalResult arith::ExtSIOp::verify() {
 /// Fold extension of float constants when there is no information loss due the
 /// difference in fp semantics.
 OpFoldResult arith::ExtFOp::fold(FoldAdaptor adaptor) {
+  if (auto truncFOp = getOperand().getDefiningOp<TruncFOp>()) {
+    if (truncFOp.getOperand().getType() == getType()) {
+      arith::FastMathFlags truncFMF = truncFOp.getFastmath();
+      bool isTruncContract =
+          bitEnumContainsAll(truncFMF, arith::FastMathFlags::contract);
+      arith::FastMathFlags extFMF = getFastmath();
+      bool isExtContract =
+          bitEnumContainsAll(extFMF, arith::FastMathFlags::contract);
+      if (isTruncContract && isExtContract) {
+        return truncFOp.getOperand();
+      }
+    }
+  }
+
   auto resElemType = cast<FloatType>(getElementTypeOrSelf(getType()));
   const llvm::fltSemantics &targetSemantics = resElemType.getFloatSemantics();
   return constFoldCastOp<FloatAttr, FloatAttr>(
