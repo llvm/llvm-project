63d0a8c1e2b0eef0fcbd898503479f01195d0a96
cmtice <cmtice@google.com>
2024-06-13 09:14:17 -0700

    [LLDB] Add more helper functions to ValueObject class. (#87197)
    Create additional helper functions for the ValueObject class, for:
      - returning the value as an APSInt or APFloat
      - additional type casting options
      - additional ways to create ValueObjects from various types of data
      - dereferencing a ValueObject
    
    These helper functions are needed for implementing the Data Inspection
    Language, described in
    https://discourse.llvm.org/t/rfc-data-inspection-language/69893

--- lldb/source/API/SBValue.cpp
+++ lldb/source/API/SBValue.cpp
@@ -1281,26 +1281,8 @@ lldb::addr_t SBValue::GetLoadAddress() {
   lldb::addr_t value = LLDB_INVALID_ADDRESS;
   ValueLocker locker;
   lldb::ValueObjectSP value_sp(GetSP(locker));
-  if (value_sp) {
-    TargetSP target_sp(value_sp->GetTargetSP());
-    if (target_sp) {
-      const bool scalar_is_load_address = true;
-      AddressType addr_type;
-      value = value_sp->GetAddressOf(scalar_is_load_address, &addr_type);
-      if (addr_type == eAddressTypeFile) {
-        ModuleSP module_sp(value_sp->GetModule());
-        if (!module_sp)
-          value = LLDB_INVALID_ADDRESS;
-        else {
-          Address addr;
-          module_sp->ResolveFileAddress(value, addr);
-          value = addr.GetLoadAddress(target_sp.get());
-        }
-      } else if (addr_type == eAddressTypeHost ||
-                 addr_type == eAddressTypeInvalid)
-        value = LLDB_INVALID_ADDRESS;
-    }
-  }
+  if (value_sp)
+    return value_sp->GetLoadAddress();
 
   return value;
 }
