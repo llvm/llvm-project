443e23eed24d9533566f189ef25154263756a36d
Tom Stellard <tstellar@redhat.com>
2024-06-14 12:20:26 -0700

    Bump version to 18.1.8 (#95458)

--- llvm/utils/lit/lit/__init__.py
+++ llvm/utils/lit/lit/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = "Daniel Dunbar"
 __email__ = "daniel@minormatter.com"
-__versioninfo__ = (18, 1, 7)
+__versioninfo__ = (18, 1, 8)
 __version__ = ".".join(str(v) for v in __versioninfo__) + "dev"
 
 __all__ = []
