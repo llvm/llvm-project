a817018094393eeecafc81b2ac61eae2a62a8cad
Nikita Popov <npopov@redhat.com>
2024-06-14 13:06:27 +0200

    [AMDGPULowerBufferFatPointers] Expand const exprs ufsing fat pointers
    Expand all constant expressions that use fat pointers upfront, so
    that the rewriting logic only has to deal with instructions and
    not the constant expression variants as well.
    
    My primary motivation is to remove the creation of illegal constant
    expressions (mul and shl) from this pass, but this also cuts down
    quite a bit on the amount of duplicate logic.

--- llvm/include/llvm/IR/ReplaceConstant.h
+++ llvm/include/llvm/IR/ReplaceConstant.h
@@ -30,9 +30,13 @@ class Function;
 /// RemoveDeadConstants by default will remove all dead constants as
 /// the final step of the function after replacement, when passed
 /// false it will skip this final step.
+///
+/// If \p IncludeSelf is enabled, also convert the passed constants themselves
+/// to instructions, rather than only their users.
 bool convertUsersOfConstantsToInstructions(ArrayRef<Constant *> Consts,
                                            Function *RestrictToFunc = nullptr,
-                                           bool RemoveDeadConstants = true);
+                                           bool RemoveDeadConstants = true,
+                                           bool IncludeSelf = false);
 
 } // end namespace llvm
 
