8d166a8121d34ff30559fcf261b7c9ef659ac464
Durgadoss R <durgadossr@nvidia.com>
2024-06-14 14:17:37 +0530

    [APFloat] Add APFloat support for FP4 data type (#95392)
    This patch adds APFloat type support for the E2M1
    FP4 datatype. The definitions for this format are
    detailed in section 5.3.3 of the OCP specification,
    which can be accessed here:
    
    https://www.opencompute.org/documents/ocp-microscaling-formats-mx-v1-0-spec-final-pdf
    
    Signed-off-by: Durgadoss R <durgadossr@nvidia.com>

--- clang/lib/AST/MicrosoftMangle.cpp
+++ clang/lib/AST/MicrosoftMangle.cpp
@@ -901,6 +901,7 @@ void MicrosoftCXXNameMangler::mangleFloat(llvm::APFloat Number) {
   case APFloat::S_FloatTF32:
   case APFloat::S_Float6E3M2FN:
   case APFloat::S_Float6E2M3FN:
+  case APFloat::S_Float4E2M1FN:
     llvm_unreachable("Tried to mangle unexpected APFloat semantics");
   }
 
