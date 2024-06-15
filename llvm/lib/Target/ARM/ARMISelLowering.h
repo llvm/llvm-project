772e3a4c10131f1fc7fab81dc28c2ce73b671d73
Sergei Barannikov <barannikov88@gmail.com>
2024-06-15 12:08:32 +0300

    [ARM] Remove duplicate custom SDag node (NFCI) (#93419)
    ARMISD::SUBS is a duplicate of ARMISD::SUBC.
    The node was introduced in 5745b6ac. This patch replaces SUBS with SUBC
    and reverts changes in *.td files.

--- llvm/lib/Target/ARM/ARMISelLowering.h
+++ llvm/lib/Target/ARM/ARMISelLowering.h
@@ -95,7 +95,6 @@ class VectorType;
     FMSTAT,   // ARM fmstat instruction.
 
     CMOV, // ARM conditional move instructions.
-    SUBS, // Flag-setting subtraction.
 
     SSAT, // Signed saturation
     USAT, // Unsigned saturation
@@ -244,7 +243,7 @@ class VectorType;
     VADDLVAps, // Same as VADDLVp[su] but with a v4i1 predicate mask
     VADDLVApu,
     VMLAVs, // sign- or zero-extend the elements of two vectors to i32, multiply
-    VMLAVu, //   them and add the results together, returning an i32 of their sum
+    VMLAVu, //   them and add the results together, returning an i32 of the sum
     VMLAVps, // Same as VMLAV[su] with a v4i1 predicate mask
     VMLAVpu,
     VMLALVs,  // Same as VMLAV but with i64, returning the low and
