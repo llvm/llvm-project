bd2a43f858b318f67b5ab8495910e519a4203898
huangjinjie <huangjinjie@bytedance.com>
2024-06-17 11:26:13 +0800

    modify test

--- llvm/test/tools/llvm-dwp/Inputs/empty_warning/non_split.s
+++ llvm/test/tools/llvm-dwp/Inputs/empty_warning/non_split.s
@@ -4,7 +4,7 @@
 
 # RUN: rm -rf %t
 # RUN: mkdir -p %t
-# RUN: llvm-mc --triple=x86_64-unknown-linux --filetype=obj -dwarf-version=4 %p/../Inputs/empty_warning/non_split.s -o %t/non_split.o
+# RUN: llvm-mc --triple=x86_64-unknown-linux --filetype=obj -dwarf-version=4 %s -o %t/non_split.o
 # RUN: llvm-dwp -e %t/non_split.o -o %t/non_split.dwp 2>&1 | FileCheck %s --check-prefix=CHECK-WARNING
 # CHECK-WARNING: warning: no dwo files found
 
