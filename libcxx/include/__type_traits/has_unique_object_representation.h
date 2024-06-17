e51099a2fc31d7f9c67355522a453bd84a9d9c5c
Louis Dionne <ldionne.2@gmail.com>
2024-06-17 11:18:41 -0400

    Apply review comment

--- libcxx/include/__type_traits/has_unique_object_representation.h
+++ libcxx/include/__type_traits/has_unique_object_representation.h
@@ -23,12 +23,12 @@ _LIBCPP_BEGIN_NAMESPACE_STD
 
 template <class _Tp>
 struct _LIBCPP_TEMPLATE_VIS has_unique_object_representations
-    // TODO: We work around a Clang bug in __has_unique_object_representations by instantiating the
-    //       builtin on the non-array type first and discarding that. This is issue #95311.
-    //       This workaround can be removed once the bug has been fixed in all supported Clangs.
-    : public integral_constant<bool,
-                               ((void)__has_unique_object_representations(remove_all_extents_t<_Tp>),
-                                __has_unique_object_representations(_Tp))> {};
+    // TODO: We work around a Clang and GCC bug in __has_unique_object_representations by using remove_all_extents
+    //       even though it should not be necessary. This was reported to the compilers:
+    //         - Clang: https://github.com/llvm/llvm-project/issues/95311
+    //         - GCC: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=115476
+    //       remove_all_extents_t can be removed once all the compilers we support have fixed this bug.
+    : public integral_constant<bool, __has_unique_object_representations(remove_all_extents_t<_Tp>)> {};
 
 template <class _Tp>
 inline constexpr bool has_unique_object_representations_v = __has_unique_object_representations(_Tp);
