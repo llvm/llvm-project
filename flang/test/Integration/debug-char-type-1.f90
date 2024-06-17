84bc2420008d6581f2cc7acbc8902909d172d75d
Abid Qadeer <haqadeer@amd.com>
2024-06-17 17:31:46 +0100

    [flang][debug] Handle allocatable strings.
    The allocatable strings also use DIStringType but provide dwarf
    expression to find the location and length of the string. With this
    change in place, the debugging of the allocatable strings looks like
    this:
    
      character(len=:), allocatable :: first
      character(len=:), allocatable :: second
      character(len=:), allocatable :: third
    
      first = 'Mount'
      second = 'Everest'
      third = first // " " // second
      print *, third
    
    (gdb) p third
    $1 = ""
    (gdb) n
    18        print *, third
    (gdb) p third
    $2 = 'Mount Everest'
    (gdb) ptype third
    type = character (13)

--- flang/test/Integration/debug-char-type-1.f90
+++ flang/test/Integration/debug-char-type-1.f90
@@ -2,6 +2,7 @@
 
 module helper
   character(len=40) :: str
+  character(len=:), allocatable :: str2
 end module helper
 
 program test
@@ -11,10 +12,13 @@ program test
   first = '3.14 = π'
   second = 'Fortran'
   str = 'Hello World!'
+  str2 = 'A quick brown fox jumps over a lazy dog'
 end program test
 
 ! CHECK-DAG: !DIGlobalVariable(name: "str"{{.*}}type: ![[TY40:[0-9]+]]{{.*}})
 ! CHECK-DAG: ![[TY40]] = !DIStringType(size: 320, encoding: DW_ATE_ASCII)
+! CHECK-DAG: !DIGlobalVariable(name: "str2"{{.*}}type: ![[TY:[0-9]+]]{{.*}})
+! CHECK-DAG: ![[TY]] = !DIStringType(stringLengthExpression: !DIExpression(DW_OP_push_object_address, DW_OP_plus_uconst, 8), stringLocationExpression: !DIExpression(DW_OP_push_object_address, DW_OP_deref), encoding: DW_ATE_ASCII)
 ! CHECK-DAG: !DILocalVariable(name: "first"{{.*}}type: ![[TY8:[0-9]+]])
 ! CHECK-DAG: ![[TY8]] = !DIStringType(size: 256, encoding: DW_ATE_UCS)
 ! CHECK-DAG: !DILocalVariable(name: "second"{{.*}}type: ![[TY10:[0-9]+]])
