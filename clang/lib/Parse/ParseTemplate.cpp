5ae501f81a7659865cc9aa709e8d1ffd8f068eda
Veeloxfire <58116051+Veeloxfire@users.noreply.github.com>
2024-06-17 12:22:33 +0100

    [clang][Parser] Reformat

--- clang/lib/Parse/ParseTemplate.cpp
+++ clang/lib/Parse/ParseTemplate.cpp
@@ -788,14 +788,14 @@ NamedDecl *Parser::ParseTemplateTemplateParameter(unsigned Depth,
   assert(Tok.is(tok::kw_template) && "Expected 'template' keyword");
 
   if (Token ahead = GetLookAheadToken(1); ahead.isNot(tok::less)) {
-    // Maybe they intended `typename` instead of `template` (given thats more common)
-    // Error early, to add a fixit hint
+    // Maybe they intended `typename` instead of `template` (given thats more
+    // common) Error early, to add a fixit hint
 
     Diag(ahead.getLocation(), diag::err_expected_less_after) << "template";
-    
+
     Diag(Tok.getLocation(), diag::note_meant_to_use_typename)
-      << FixItHint::CreateReplacement(CharSourceRange::getTokenRange(Tok.getLocation()),
-                                      "typename");
+        << FixItHint::CreateReplacement(
+               CharSourceRange::getTokenRange(Tok.getLocation()), "typename");
 
     Tok.setKind(tok::kw_typename);
     return ParseTypeParameter(Depth, Position);
