14664b5f6c34e178873ef1fc633e8534f426467c
Daniil Kovalev <dkovalev@accesssoftek.com>
2024-06-17 07:22:45 +0300

    [PAC][AArch64] Lower ptrauth constants in code
    Define the following pseudos for lowering ptrauth constants in code:
    
    - non-`extern_weak`:
      - no GOT load needed: `MOVaddrPAC` - similar to `MOVaddr`, with added PAC;
      - GOT load needed: `LOADgotPAC` - similar to `LOADgot`, with added PAC;
    - `extern_weak`: `LOADauthptrstatic` - similar to `LOADgot`, but use a special
      stub slot named `sym$auth_ptr$key$disc` filled by dynamic linker during
      relocation resolving instead of a GOT slot.
    
    Co-authored-by: Ahmed Bougacha <ahmed@bougacha.org>

--- llvm/lib/CodeGen/MachineVerifier.cpp
+++ llvm/lib/CodeGen/MachineVerifier.cpp
@@ -2066,6 +2066,12 @@ void MachineVerifier::verifyPreISelGenericInstruction(const MachineInstr *MI) {
       report("Dst operand 0 must be a pointer", MI);
     break;
   }
+  case TargetOpcode::G_PTRAUTH_GLOBAL_VALUE: {
+    const MachineOperand &AddrOp = MI->getOperand(1);
+    if (!AddrOp.isReg() || !MRI->getType(AddrOp.getReg()).isPointer())
+      report("addr operand must be a pointer", &AddrOp, 1);
+    break;
+  }
   default:
     break;
   }
