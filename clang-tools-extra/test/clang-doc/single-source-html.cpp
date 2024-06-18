2910383a3a0fabd36f01e62e4c14f29ff58efffa
PeterChou1 <peter.chou@mail.utoronto.ca>
2024-06-18 16:10:26 -0400

    [clang][clang-doc] remove unnecessary commands

--- clang-tools-extra/test/clang-doc/single-source-html.cpp
+++ clang-tools-extra/test/clang-doc/single-source-html.cpp
@@ -1,7 +1,2 @@
-// RUN: rm -rf %t
-// RUN: mkdir %t
-// RUN: echo "" > %t/compile_flags.txt
-// RUN: cp "%s" "%t/test.cpp"
 // RUN: clang-doc --format=html --executor=standalone %s -output=%t/docs | FileCheck %s
-
 // CHECK: Using default asset: {{.*}}..\share\clang
