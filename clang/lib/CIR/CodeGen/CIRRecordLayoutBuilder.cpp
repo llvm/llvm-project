fdde871b5a7d8d361261525e7c9e8a38ece919ee
ghehg <166402688+ghehg@users.noreply.github.com>
2024-06-17 23:27:55 -0400

    [CIR][CIRDataLayout]moving CIRDataLayout to MLIRCIR (#693)
    fix build failure 
    undefined reference to
    `cir::CIRDataLayout::CIRDataLayout(mlir::ModuleOp)'
    by breaking circular dependency caused by the fact CIRDataLayout was in
    CIR Codegen

--- clang/lib/CIR/CodeGen/CIRRecordLayoutBuilder.cpp
+++ clang/lib/CIR/CodeGen/CIRRecordLayoutBuilder.cpp
@@ -722,29 +722,3 @@ CIRGenBitFieldInfo CIRGenBitFieldInfo::MakeInfo(CIRGenTypes &Types,
                                                 CharUnits StorageOffset) {
   llvm_unreachable("NYI");
 }
-
-CIRDataLayout::CIRDataLayout(mlir::ModuleOp modOp) : layout{modOp} {
-  auto dlSpec = modOp->getAttr(mlir::DLTIDialect::kDataLayoutAttrName)
-                    .dyn_cast<mlir::DataLayoutSpecAttr>();
-  assert(dlSpec && "expected dl_spec in the module");
-  auto entries = dlSpec.getEntries();
-
-  for (auto entry : entries) {
-    auto entryKey = entry.getKey();
-    auto strKey = entryKey.dyn_cast<mlir::StringAttr>();
-    if (!strKey)
-      continue;
-    auto entryName = strKey.strref();
-    if (entryName == mlir::DLTIDialect::kDataLayoutEndiannessKey) {
-      auto value = entry.getValue().dyn_cast<mlir::StringAttr>();
-      assert(value && "expected string attribute");
-      auto endian = value.getValue();
-      if (endian == mlir::DLTIDialect::kDataLayoutEndiannessBig)
-        bigEndian = true;
-      else if (endian == mlir::DLTIDialect::kDataLayoutEndiannessLittle)
-        bigEndian = false;
-      else
-        llvm_unreachable("unknown endianess");
-    }
-  }
-}
