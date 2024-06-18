43fd4c49bd8d54b9058620f0a885c7a5672fd602
Krzysztof Drewniak <Krzysztof.Drewniak@amd.com>
2024-06-17 21:47:38 -0700

    [mlir][GPU] Improve handling of GPU bounds (#95166)
    This change reworks how range information for GPU dispatch IDs (block
    IDs, thread IDs, and so on) is handled.
    
    1. `known_block_size` and `known_grid_size` become inherent attributes
    of GPU functions. This makes them less clunky to work with. As a
    consequence, the `gpu.func` lowering patterns now only look at the
    inherent attributes when setting target-specific attributes on the
    `llvm.func` that they lower to.
    2. At the same time, `gpu.known_block_size` and `gpu.known_grid_size`
    are made official dialect-level discardable attributes which can be
    placed on arbitrary functions. This allows for progressive lowerings
    (without this, a lowering for `gpu.thread_id` couldn't know about the
    bounds if it had already been moved from a `gpu.func` to an `llvm.func`)
    and allows for range information to be provided even when
    `gpu.*_{id,dim}` are being used outside of a `gpu.func` context.
    3. All of these index operations have gained an optional `upper_bound`
    attribute, allowing for an alternate mode of operation where the bounds
    are specified locally and not inherited from the operation's context.
    These also allow handling of cases where the precise launch sizes aren't
    known, but can be bounded more precisely than the maximum of what any
    platform's API allows. (I'd like to thank @benvanik for pointing out
    that this could be useful.)
    
    When inferring bounds (either for range inference or for setting `range`
    during lowering) these sources of information are consulted in order of
    specificity (`upper_bound` > inherent attribute > discardable attribute,
    except that dimension sizes check for `known_*_bounds` to see if they
    can be constant-folded before checking their `upper_bound`).
    
    This patch also updates the documentation about the bounds and inference
    behavior to clarify what these attributes do when set and the
    consequences of setting them up incorrectly.
    
    ---------
    
    Co-authored-by: Mehdi Amini <joker.eph@gmail.com>

--- mlir/lib/Conversion/GPUCommon/GPUOpsLowering.cpp
+++ mlir/lib/Conversion/GPUCommon/GPUOpsLowering.cpp
@@ -74,7 +74,9 @@ GPUFuncOpLowering::matchAndRewrite(gpu::GPUFuncOp gpuFuncOp, OpAdaptor adaptor,
         attr.getName() ==
             gpu::GPUFuncOp::getNumWorkgroupAttributionsAttrName() ||
         attr.getName() == gpuFuncOp.getWorkgroupAttribAttrsAttrName() ||
-        attr.getName() == gpuFuncOp.getPrivateAttribAttrsAttrName())
+        attr.getName() == gpuFuncOp.getPrivateAttribAttrsAttrName() ||
+        attr.getName() == gpuFuncOp.getKnownBlockSizeAttrName() ||
+        attr.getName() == gpuFuncOp.getKnownGridSizeAttrName())
       continue;
     if (attr.getName() == gpuFuncOp.getArgAttrsAttrName()) {
       argAttrs = gpuFuncOp.getArgAttrsAttr();
@@ -82,27 +84,28 @@ GPUFuncOpLowering::matchAndRewrite(gpu::GPUFuncOp gpuFuncOp, OpAdaptor adaptor,
     }
     attributes.push_back(attr);
   }
+
+  DenseI32ArrayAttr knownBlockSize = gpuFuncOp.getKnownBlockSizeAttr();
+  DenseI32ArrayAttr knownGridSize = gpuFuncOp.getKnownGridSizeAttr();
+  // Ensure we don't lose information if the function is lowered before its
+  // surrounding context.
+  auto *gpuDialect = cast<gpu::GPUDialect>(gpuFuncOp->getDialect());
+  if (knownBlockSize)
+    attributes.emplace_back(gpuDialect->getKnownBlockSizeAttrHelper().getName(),
+                            knownBlockSize);
+  if (knownGridSize)
+    attributes.emplace_back(gpuDialect->getKnownGridSizeAttrHelper().getName(),
+                            knownGridSize);
+
   // Add a dialect specific kernel attribute in addition to GPU kernel
   // attribute. The former is necessary for further translation while the
   // latter is expected by gpu.launch_func.
   if (gpuFuncOp.isKernel()) {
     attributes.emplace_back(kernelAttributeName, rewriter.getUnitAttr());
-
-    // Set the block size attribute if it is present.
-    if (kernelBlockSizeAttributeName.has_value()) {
-      std::optional<int32_t> dimX =
-          gpuFuncOp.getKnownBlockSize(gpu::Dimension::x);
-      std::optional<int32_t> dimY =
-          gpuFuncOp.getKnownBlockSize(gpu::Dimension::y);
-      std::optional<int32_t> dimZ =
-          gpuFuncOp.getKnownBlockSize(gpu::Dimension::z);
-      if (dimX.has_value() || dimY.has_value() || dimZ.has_value()) {
-        // If any of the dimensions are missing, fill them in with 1.
-        attributes.emplace_back(
-            kernelBlockSizeAttributeName.value(),
-            rewriter.getDenseI32ArrayAttr(
-                {dimX.value_or(1), dimY.value_or(1), dimZ.value_or(1)}));
-      }
+    // Set the dialect-specific block size attribute if there is one.
+    if (kernelBlockSizeAttributeName.has_value() && knownBlockSize) {
+      attributes.emplace_back(kernelBlockSizeAttributeName.value(),
+                              knownBlockSize);
     }
   }
   auto llvmFuncOp = rewriter.create<LLVM::LLVMFuncOp>(
