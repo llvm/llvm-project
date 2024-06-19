185e0f8266c2dec9a161328c6c14490fe3cffa69
Wang Pengcheng <wangpengcheng.pp@bytedance.com>
2024-06-19 18:45:50 +0800

    Support buttomup/topdown/bidirectional and fix some failures
    Created using spr 1.3.6-beta.1

--- llvm/lib/Target/RISCV/RISCVMachineScheduler.cpp
+++ llvm/lib/Target/RISCV/RISCVMachineScheduler.cpp
@@ -27,34 +27,68 @@ static cl::opt<bool> EnableScheduleSameVType(
     cl::desc("Enable scheduling RVV instructions with same vtype first"));
 
 SUnit *RISCVPreRAMachineSchedStrategy::pickNode(bool &IsTopNode) {
-  if (EnableScheduleSameVType) {
-    for (SUnit *SU : Bot.Available) {
+  auto FindPotentialRVVInstructionInQueue =
+      [&](SchedBoundary &Boundary, ReadyQueue Q, bool ShouldBeTop) -> SUnit * {
+    for (SUnit *SU : Q) {
+      if (SU->isScheduled)
+        continue;
+
       MachineInstr *MI = SU->getInstr();
       const MCInstrDesc &Desc = MI->getDesc();
       if (RISCVII::hasSEWOp(Desc.TSFlags)) {
         unsigned CurVSEW = MI->getOperand(RISCVII::getSEWOpNum(Desc)).getImm();
         RISCVII::VLMUL CurVLMUL = RISCVII::getLMul(Desc.TSFlags);
+        // FIXME: We should consider vl and policy here.
         if (CurVSEW == PrevVSEW && CurVLMUL == PrevVLMUL) {
-          Bot.removeReady(SU);
-          IsTopNode = true;
+          IsTopNode = ShouldBeTop;
+          // Boundary.removeReady(SU);
+          if (SU->isTopReady())
+            Top.removeReady(SU);
+          if (SU->isBottomReady())
+            Bot.removeReady(SU);
+          LLVM_DEBUG(dbgs() << "Scheduling SU(" << SU->NodeNum << ") "
+                            << *SU->getInstr());
           return SU;
         }
       }
     }
-    for (SUnit *SU : Bot.Pending) {
-      MachineInstr *MI = SU->getInstr();
-      const MCInstrDesc &Desc = MI->getDesc();
-      if (RISCVII::hasSEWOp(Desc.TSFlags)) {
-        unsigned CurVSEW = MI->getOperand(RISCVII::getSEWOpNum(Desc)).getImm();
-        RISCVII::VLMUL CurVLMUL = RISCVII::getLMul(Desc.TSFlags);
-        if (CurVSEW == PrevVSEW && CurVLMUL == PrevVLMUL) {
-          Bot.removeReady(SU);
-          IsTopNode = false;
-          return SU;
-        }
-      }
+    return nullptr;
+  };
+
+  auto FindPotentialRVVInstruction = [&](SchedBoundary &Boundary,
+                                         bool ShouldBeTop) -> SUnit * {
+    if (SUnit *Available = FindPotentialRVVInstructionInQueue(
+            Boundary, Boundary.Available, ShouldBeTop))
+      return Available;
+    if (SUnit *Pending = FindPotentialRVVInstructionInQueue(
+            Boundary, Boundary.Pending, ShouldBeTop))
+      return Pending;
+    return nullptr;
+  };
+
+  if (EnableScheduleSameVType) {
+    if (RegionPolicy.OnlyBottomUp) {
+      if (SUnit *SU = FindPotentialRVVInstruction(Bot, false))
+        return SU;
+    } else if (RegionPolicy.OnlyTopDown) {
+      if (SUnit *SU = FindPotentialRVVInstruction(Top, true))
+        return SU;
+    } else {
+      if (SUnit *SU =
+              FindPotentialRVVInstructionInQueue(Bot, Bot.Available, false))
+        return SU;
+      if (SUnit *SU =
+              FindPotentialRVVInstructionInQueue(Top, Top.Available, true))
+        return SU;
+      if (SUnit *SU =
+              FindPotentialRVVInstructionInQueue(Bot, Bot.Pending, false))
+        return SU;
+      if (SUnit *SU =
+              FindPotentialRVVInstructionInQueue(Top, Top.Pending, true))
+        return SU;
     }
   }
+
   return GenericScheduler::pickNode(IsTopNode);
 }
 
@@ -73,11 +107,12 @@ void RISCVPreRAMachineSchedStrategy::schedNode(SUnit *SU, bool IsTopNode) {
   if (RISCVII::hasSEWOp(Desc.TSFlags)) {
     PrevVSEW = MI->getOperand(RISCVII::getSEWOpNum(Desc)).getImm();
     PrevVLMUL = RISCVII::getLMul(Desc.TSFlags);
+    LLVM_DEBUG(dbgs() << "Previous scheduled Unit: ";
+               dbgs() << "SU(" << SU->NodeNum << ") - ";
+               SU->getInstr()->dump(););
+    LLVM_DEBUG(dbgs() << "Previous VSEW : " << (1 << PrevVSEW) << "\n";
+               auto LMUL = RISCVVType::decodeVLMUL(PrevVLMUL);
+               dbgs() << "Previous VLMUL: m" << (LMUL.second ? "f" : "")
+                      << LMUL.first << "\n";);
   }
-  LLVM_DEBUG(dbgs() << "Previous scheduled Unit: ";
-             dbgs() << "SU(" << SU->NodeNum << ") - "; SU->getInstr()->dump(););
-  LLVM_DEBUG(dbgs() << "Previous VSEW : " << (1 << PrevVSEW) << "\n";
-             auto LMUL = RISCVVType::decodeVLMUL(PrevVLMUL);
-             dbgs() << "Previous VLMUL: m" << (LMUL.second ? "f" : "")
-                    << LMUL.first << "\n";);
 }
