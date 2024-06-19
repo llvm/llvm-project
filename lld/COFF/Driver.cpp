97d649f855c2923abcbbefb4471425f33368d9c7
Martin Storsjö <martin@martin.st>
2024-06-19 14:13:50 +0300

    [LLD] [COFF] Don't crash on an empty -entry: argument
    We can't pass an empty string to addUndefined().
    
    This fixes the crash that was encountered in
    https://github.com/llvm/llvm-project/issues/93309 (turning the
    crash into a properly handled error; making it do the right
    thing is handled in https://github.com/llvm/llvm-project/pull/96055).

--- lld/COFF/Driver.cpp
+++ lld/COFF/Driver.cpp
@@ -412,6 +412,8 @@ void LinkerDriver::parseDirectives(InputFile *file) {
         enqueuePath(*path, false, false);
       break;
     case OPT_entry:
+      if (!arg->getValue()[0])
+        fatal("missing entry point symbol name");
       ctx.config.entry = addUndefined(mangle(arg->getValue()));
       break;
     case OPT_failifmismatch:
@@ -2248,6 +2250,8 @@ void LinkerDriver::linkerMain(ArrayRef<const char *> argsArr) {
   {
     llvm::TimeTraceScope timeScope("Entry point");
     if (auto *arg = args.getLastArg(OPT_entry)) {
+      if (!arg->getValue()[0])
+        fatal("missing entry point symbol name");
       config->entry = addUndefined(mangle(arg->getValue()));
     } else if (!config->entry && !config->noEntry) {
       if (args.hasArg(OPT_dll)) {
