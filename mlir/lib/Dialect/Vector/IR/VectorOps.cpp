1ba850c1801b94bcb903bbd6c4d715a0d4b6c959
Cullen Rhodes <cullen.rhodes@arm.com>
2024-06-19 15:29:25 +0000

    [mlir][vector] Add vector.step operation
    This patch adds a new vector.step operation to the Vector dialect. It
    produces a linear sequence of index values from 0 to N, where N is the
    number of elements in the result vector, and can be used to create
    vectors of indices.
    
    It supports both fixed-width and scalable vectors. For fixed the
    canonical representation is `arith.constant dense<[0, .., N]>`. A
    scalable step cannot be represented as a constant and is lowered to the
    `llvm.experimental.stepvector` intrinsic [1].
    
    [1] https://llvm.org/docs/LangRef.html#llvm-experimental-stepvector-intrinsic

--- mlir/lib/Dialect/Vector/IR/VectorOps.cpp
+++ mlir/lib/Dialect/Vector/IR/VectorOps.cpp
@@ -6317,6 +6317,20 @@ OpFoldResult SplatOp::fold(FoldAdaptor adaptor) {
 }
 
 //===----------------------------------------------------------------------===//
+// StepOp
+//===----------------------------------------------------------------------===//
+
+OpFoldResult StepOp::fold(FoldAdaptor adaptor) {
+  auto resultType = cast<VectorType>(getType());
+  if (resultType.isScalable())
+    return nullptr;
+  SmallVector<APInt> indices;
+  for (unsigned i = 0; i < resultType.getNumElements(); i++)
+    indices.push_back(APInt(/*width=*/64, i));
+  return DenseElementsAttr::get(resultType, indices);
+}
+
+//===----------------------------------------------------------------------===//
 // WarpExecuteOnLane0Op
 //===----------------------------------------------------------------------===//
 
