c484ded6a017f4400a6099af66706d0f35b9de2b
Simon Pilgrim <llvm-dev@redking.me.uk>
2024-06-20 12:46:14 +0100

    Use densemap::detail::mix(Val) instead

--- llvm/include/llvm/ADT/DenseMapInfo.h
+++ llvm/include/llvm/ADT/DenseMapInfo.h
@@ -142,7 +142,7 @@ template<> struct DenseMapInfo<unsigned long> {
     if constexpr (sizeof(Val) == 4)
       return DenseMapInfo<unsigned>::getHashValue(Val);
     else
-      return detail::combineHashValue(Val >> (4 * sizeof(Val)), Val);
+      return densemap::detail::mix(Val);
   }
 
   static bool isEqual(const unsigned long& LHS, const unsigned long& RHS) {
