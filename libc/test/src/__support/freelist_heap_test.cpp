05537510eb78191b08349cebc4e1d2f86b0e3f35
PiJoules <6019989+PiJoules@users.noreply.github.com>
2024-06-20 13:13:52 -0700

    [libc] Move freelist + block to __support (#96231)

--- libc/test/src/__support/freelist_heap_test.cpp
+++ libc/test/src/__support/freelist_heap_test.cpp
@@ -7,7 +7,7 @@
 //===----------------------------------------------------------------------===//
 
 #include "src/__support/CPP/span.h"
-#include "src/stdlib/freelist_heap.h"
+#include "src/__support/freelist_heap.h"
 #include "src/string/memcmp.h"
 #include "src/string/memcpy.h"
 #include "test/UnitTest/Test.h"
