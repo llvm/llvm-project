cb3b05c5d6f9d25d5dbdabc70053039f00165511
jameshu15869 <jhudson15869@gmail.com>
2024-06-20 22:48:11 -0400

    clean up

--- libc/benchmarks/gpu/timing/nvptx/timing.h
+++ libc/benchmarks/gpu/timing/nvptx/timing.h
@@ -25,9 +25,9 @@ namespace LIBC_NAMESPACE {
   volatile uint32_t x = 1;
   uint32_t y = x;
   uint64_t start = gpu::processor_clock();
-  asm volatile("" ::"r"(y), "llr"(start));
+  asm("" ::"r"(y), "llr"(start));
   uint32_t result = y;
-  asm volatile("or.b32 %[v_reg], %[v_reg], 0;" ::[v_reg] "r"(result) :);
+  asm("or.b32 %[v_reg], %[v_reg], 0;" ::[v_reg] "r"(result) :);
   uint64_t stop = gpu::processor_clock();
   volatile auto storage = result;
   return stop - start;
@@ -42,7 +42,7 @@ template <typename F, typename T>
   // not constant propagate it and remove the profiling region.
   volatile T storage = t;
   T arg = storage;
-  asm volatile("" ::"r"(arg));
+  asm("" ::"r"(arg));
 
   // Get the current timestamp from the clock.
   gpu::memory_fence();
@@ -50,20 +50,20 @@ template <typename F, typename T>
 
   // This forces the compiler to load the input argument and run the clock cycle
   // counter before the profiling region.
-  asm volatile("" ::"r"(arg), "llr"(start));
+  asm("" ::"r"(arg), "llr"(start));
 
   // Run the function under test and return its value.
   auto result = f(arg);
 
   // This inline assembly performs a no-op which forces the result to both be
   // used and prevents us from exiting this region before it's complete.
-  asm volatile("or.b32 %[v_reg], %[v_reg], 0;" ::[v_reg] "r"(result) :);
+  asm("or.b32 %[v_reg], %[v_reg], 0;" ::[v_reg] "r"(result) :);
 
   // Obtain the current timestamp after running the calculation and force
   // ordering.
   uint64_t stop = gpu::processor_clock();
   gpu::memory_fence();
-  asm volatile("" ::"r"(stop));
+  asm("" ::"r"(stop));
   volatile T output = result;
 
   // Return the time elapsed.
@@ -76,20 +76,20 @@ static LIBC_INLINE uint64_t latency(F f, T1 t1, T2 t2) {
   volatile T2 storage2 = t2;
   T1 arg = storage;
   T2 arg2 = storage2;
-  asm volatile("" ::"r"(arg), "r"(arg2));
+  asm("" ::"r"(arg), "r"(arg2));
 
   gpu::memory_fence();
   uint64_t start = gpu::processor_clock();
 
-  asm volatile("" ::"r"(arg), "r"(arg2), "llr"(start));
+  asm("" ::"r"(arg), "r"(arg2), "llr"(start));
 
   auto result = f(arg, arg2);
 
-  asm volatile("or.b32 %[v_reg], %[v_reg], 0;" ::[v_reg] "r"(result) :);
+  asm("or.b32 %[v_reg], %[v_reg], 0;" ::[v_reg] "r"(result) :);
 
   uint64_t stop = gpu::processor_clock();
   gpu::memory_fence();
-  asm volatile("" ::"r"(stop));
+  asm("" ::"r"(stop));
   volatile auto output = result;
 
   return stop - start;
