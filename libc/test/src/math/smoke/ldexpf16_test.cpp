c37b731e758b7bb5f3cb1b2cced280d4e462e95f
OverMighty <its.overmighty@gmail.com>
2024-06-21 15:01:47 +0200

    [libc][math][c23] Add {ldexp,scalbn,scalbln}f16 C23 math functions (#94797)
    Part of #93566.

--- libc/test/src/math/smoke/ldexpf16_test.cpp
+++ libc/test/src/math/smoke/ldexpf16_test.cpp
@@ -1,4 +1,4 @@
-//===-- Unittests for scalbnl ---------------------------------------------===//
+//===-- Unittests for ldexpf16 --------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,8 +6,8 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "ScalbnTest.h"
+#include "LdExpTest.h"
 
-#include "src/math/scalbnl.h"
+#include "src/math/ldexpf16.h"
 
-LIST_SCALBN_TESTS(long double, int, LIBC_NAMESPACE::scalbnl)
+LIST_LDEXP_TESTS(float16, LIBC_NAMESPACE::ldexpf16);
