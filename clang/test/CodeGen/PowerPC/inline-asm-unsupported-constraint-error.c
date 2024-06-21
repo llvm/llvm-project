6c3ff4ab36bb565d53dc6bac2577fd0f8c033f6b
Kamau Bridgeman <kamau.bridgeman.ibm@gmail.com>
2024-06-21 13:02:50 -0500

    Addressing review comment
    Adding test case run lines for aix 32 and 64 bit targets.

--- clang/test/CodeGen/PowerPC/inline-asm-unsupported-constraint-error.c
+++ clang/test/CodeGen/PowerPC/inline-asm-unsupported-constraint-error.c
@@ -1,8 +1,10 @@
-// RUN: %clang_cc1 -emit-llvm -triple powerpc64le-unknown-unknown -verify %s
+// RUN: %clang_cc1 -emit-llvm -triple powerpc64le-linux-gnu -verify %s
+// RUN: %clang_cc1 -emit-llvm -triple powerpc64-ibm-aix-xcoff -verify %s
+// RUN: %clang_cc1 -emit-llvm -triple powerpc-ibm-aix-xcoff -verify %s
 // This test case exist to test marking the 'a' inline assembly constraint as
 // unsupported because powerpc previously marked it as supported.
 int foo(int arg){
-  asm goto ("bc %0,%1,%l[TEST_LABEL]" : : "a"(&&TEST_LABEL) : : TEST_LABEL); //expected-error {{invalid input constraint 'a' in asm}}
+  asm goto ("bc 12,2,%l[TEST_LABEL]" : : "a"(&&TEST_LABEL) : : TEST_LABEL); //expected-error {{invalid input constraint 'a' in asm}}
   return 0;
 TEST_LABEL: return arg + 1;
 }
