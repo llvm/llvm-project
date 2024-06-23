d389e96ae07775239cc26ecec46cec7e29ac775c
Nicolai Hähnle <nicolai.haehnle@amd.com>
2024-06-23 18:56:43 +0200

    AMDGPU: Add plumbing for private segment size argument
    The actual size of scratch/private is determined at dispatch time, so
    add more plumbing to request it. Will be used in subsequent change.

--- llvm/lib/Target/AMDGPU/GCNSubtarget.h
+++ llvm/lib/Target/AMDGPU/GCNSubtarget.h
@@ -1567,6 +1567,8 @@ public:
 
   bool hasFlatScratchInit() const { return FlatScratchInit; }
 
+  bool hasPrivateSegmentSize() const { return PrivateSegmentSize; }
+
   unsigned getNumKernargPreloadSGPRs() const { return NumKernargPreloadSGPRs; }
 
   unsigned getNumUsedUserSGPRs() const { return NumUsedUserSGPRs; }
@@ -1630,6 +1632,8 @@ private:
   bool DispatchID = false;
 
   bool FlatScratchInit = false;
+
+  bool PrivateSegmentSize = false;
 
   unsigned NumKernargPreloadSGPRs = 0;
 
