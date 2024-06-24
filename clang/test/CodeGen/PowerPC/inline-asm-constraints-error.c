f682c2960f518db872f33ff27addea9d62132e45
Benjamin Kramer <benny.kra@googlemail.com>
2024-06-24 16:34:06 +0200

    [PPC][InlineASM] Don't write to source directory in test

--- clang/test/CodeGen/PowerPC/inline-asm-constraints-error.c
+++ clang/test/CodeGen/PowerPC/inline-asm-constraints-error.c
@@ -1,5 +1,5 @@
-// RUN: %clang_cc1 -emit-llvm -triple powerpc64-ibm-aix-xcoff -verify %s
-// RUN: %clang_cc1 -emit-llvm -triple powerpc-ibm-aix-xcoff -verify %s
+// RUN: %clang_cc1 -emit-llvm-only -triple powerpc64-ibm-aix-xcoff -verify %s
+// RUN: %clang_cc1 -emit-llvm-only -triple powerpc-ibm-aix-xcoff -verify %s
 // This test case exist to test marking the 'a' inline assembly constraint as
 // unsupported because powerpc previously marked it as supported.
 int foo(int arg){
