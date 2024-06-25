4d5a639e8fe3a5cd90525f8bca2c637aaa9ae6a9
Vitaly Buka <vitalybuka@google.com>
2024-06-24 23:47:04 -0700

    [𝘀𝗽𝗿] changes introduced through rebase
    Created using spr 1.3.4
    
    [skip ci]

--- compiler-rt/lib/asan/asan_interceptors.cpp
+++ compiler-rt/lib/asan/asan_interceptors.cpp
@@ -334,7 +334,7 @@ INTERCEPTOR(int, pthread_timedjoin_np, void *thread, void **ret,
 #    endif
 
 DEFINE_INTERNAL_PTHREAD_FUNCTIONS
-#  endif  // ASAN_INTERCEPT_PTHREAD_CREATE
+#endif  // ASAN_INTERCEPT_PTHREAD_CREATE
 
 #if ASAN_INTERCEPT_SWAPCONTEXT
 static void ClearShadowMemoryForContextStack(uptr stack, uptr ssize) {
