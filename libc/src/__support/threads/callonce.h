c6bb43daf5cb4a2032e6bc21a39305372a6cbeff
Schrodinger ZHU Yifan <i@zhuyi.fan>
2024-06-25 21:54:40 -0700

    address CRs

--- libc/src/__support/threads/callonce.h
+++ libc/src/__support/threads/callonce.h
@@ -9,37 +9,31 @@
 #ifndef LLVM_LIBC_SRC___SUPPORT_THREADS_CALLONCE_H
 #define LLVM_LIBC_SRC___SUPPORT_THREADS_CALLONCE_H
 
-#include "src/__support/macros/optimization.h"
+#include "src/__support/macros/optimization.h" // For LIBC_LIKELY
 
+// Plaform specific routines, provides:
+// - OnceFlag definition
+// - callonce_impl::callonce_fastpath for fast path check
+// - callonce_impl::callonce_slowpath for slow path execution
 #ifdef __linux__
-#include "src/__support/threads/linux/futex_utils.h"
+#include "src/__support/threads/linux/callonce.h"
 #else
 #error "callonce is not supported on this platform"
 #endif
 
 namespace LIBC_NAMESPACE {
 
-#ifdef __linux__
-using CallOnceFlag = Futex;
-#endif
+// Common definitions
 using CallOnceCallback = void(void);
 namespace callonce_impl {
-static constexpr FutexWordType NOT_CALLED = 0x0;
-static constexpr FutexWordType START = 0x11;
-static constexpr FutexWordType WAITING = 0x22;
-static constexpr FutexWordType FINISH = 0x33;
 int callonce_slowpath(CallOnceFlag *flag, CallOnceCallback *callback);
 } // namespace callonce_impl
 
 LIBC_INLINE int callonce(CallOnceFlag *flag, CallOnceCallback *callback) {
-  using namespace callonce_impl;
-  // Avoid cmpxchg operation if the function has already been called.
-  // The destination operand of cmpxchg may receive a write cycle without
-  // regard to the result of the comparison
-  if (LIBC_LIKELY(flag->load(cpp::MemoryOrder::RELAXED) == FINISH))
+  if (LIBC_LIKELY(callonce_impl::callonce_fastpath(flag)))
     return 0;
 
-  return callonce_slowpath(flag, callback);
+  return callonce_impl::callonce_slowpath(flag, callback);
 }
 } // namespace LIBC_NAMESPACE
 
