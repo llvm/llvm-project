b815e710025aa0f00f563741fbfaf3be26387b9d
Shengchen Kan <shengchen.kan@intel.com>
2024-06-26 11:14:12 +0800

    Revert "[X86] Fix description for pass X86FixupSetCC.cpp, NFCI"
    This reverts commit 174d3537c203ec81a6810bad554a5a71cc331e24.
    
    It introduced new typo "IMul" by accident.

--- llvm/lib/Target/X86/X86FixupSetCC.cpp
+++ llvm/lib/Target/X86/X86FixupSetCC.cpp
@@ -1,4 +1,4 @@
-//===- X86FixupIMulSetCC.cpp - fix zero-extension of setcc patterns -------===//
+//===---- X86FixupSetCC.cpp - optimize usage of LEA instructions ----------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
