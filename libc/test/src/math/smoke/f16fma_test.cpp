c9fc212af0d87fb40eb39162339455318e5f2e17
OverMighty <its.overmighty@gmail.com>
2024-06-27 20:44:19 +0200

    [libc][math][c23] Add f16fma{,l,f128} C23 math function (#96711)
    Part of #93566.

--- libc/test/src/math/smoke/f16fma_test.cpp
+++ libc/test/src/math/smoke/f16fma_test.cpp
@@ -1,4 +1,4 @@
-//===-- Unittests for fmaf ------------------------------------------------===//
+//===-- Unittests for f16fma ----------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -8,6 +8,6 @@
 
 #include "FmaTest.h"
 
-#include "src/math/fmaf.h"
+#include "src/math/f16fma.h"
 
-LIST_FMA_TESTS(float, LIBC_NAMESPACE::fmaf)
+LIST_NARROWING_FMA_TESTS(float16, double, LIBC_NAMESPACE::f16fma)
