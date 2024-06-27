7744e7c40fdc73d439f0f1e2280927fc7b8ed09c
Joshua Batista <jbatista@microsoft.com>
2024-06-27 10:43:41 -0700

    the c in FileCheck needs capitalization for linux builds

--- clang/test/ParserHLSL/bitfields.hlsl
+++ clang/test/ParserHLSL/bitfields.hlsl
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -triple dxil-pc-shadermodel6.0-compute -ast-dump -x hlsl -o - %s | Filecheck %s
+// RUN: %clang_cc1 -triple dxil-pc-shadermodel6.0-compute -ast-dump -x hlsl -o - %s | FileCheck %s
 
 
 struct MyBitFields {
