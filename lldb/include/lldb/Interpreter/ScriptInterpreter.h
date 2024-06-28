a7d325290397a5115912aea29558c2a51b5d9395
Nico Weber <thakis@chromium.org>
2024-06-28 13:25:11 +0200

    Revert "[lldb/Interpreter] Discard ScriptedThreadPlan::GetStopDescription return value (#96985)"
    This reverts commit 191f676ff91fa37da440e3765c7d03053ee6d4ee.
    Very likely causes build problems on Windows and with LLVM_NO_DEAD_STRIP=ON,
    see https://github.com/llvm/llvm-project/pull/96985#pullrequestreview-2147599208

--- lldb/include/lldb/Interpreter/ScriptInterpreter.h
+++ lldb/include/lldb/Interpreter/ScriptInterpreter.h
@@ -575,7 +575,7 @@ public:
 
   Event *GetOpaqueTypeFromSBEvent(const lldb::SBEvent &event) const;
 
-  lldb::StreamSP GetOpaqueTypeFromSBStream(const lldb::SBStream &stream) const;
+  Stream *GetOpaqueTypeFromSBStream(const lldb::SBStream &stream) const;
 
   lldb::BreakpointSP
   GetOpaqueTypeFromSBBreakpoint(const lldb::SBBreakpoint &breakpoint) const;
