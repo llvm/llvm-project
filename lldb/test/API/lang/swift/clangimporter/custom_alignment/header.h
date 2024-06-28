0d4b6a9f99e3d1e041e36abac06ce395fc90987f
Augusto Noronha <anoronha@apple.com>
2024-06-28 13:04:34 -0700

    [lldb] Add test with custom alignment on a clang type
    rdar://127785973
    (cherry picked from commit 935e266854041650151823268dea991f6ed3eaf6)

---
+++ lldb/test/API/lang/swift/clangimporter/custom_alignment/header.h
@@ -0,0 +1,14 @@
+#include "stdlib.h"
+#include <stdint.h>
+
+#pragma pack(push, 4)
+
+struct Struct {
+ int64_t field_64_1;
+ int32_t field_32_1;
+ uint32_t field_32_2;
+ int64_t field_64_2;
+}; 
+
+#pragma pack(pop)
+
