1c924c866cc2de5e9e1d84ce0b185e9dacefd4ec
Michael Buch <michaelbuch12@gmail.com>
2024-06-28 15:29:54 +0100

    [lldb][test] Add tests for alignof on class with overlapping bases
    Follow-up to https://github.com/llvm/llvm-project/pull/96932
    
    Adds XFAILed test where LLDB incorrectly infers the
    alignment of a derived class whose bases are overlapping
    due to [[no_unique_address]]. Specifically, the `InferAlignment`
    code-path of the `ItaniumRecordLayoutBuilder` assumes that overlapping
    base offsets imply a packed structure and thus sets alignment
    to 1. See discussion in https://github.com/llvm/llvm-project/pull/93809.
    
    Also adds test where LLDB correctly infers an alignment
    of `1` when a packed base class is overlapping with other bases.
    
    Lastly, there were a couple of `alignof` inconsistencies which
    I encapsulated in an XFAIL-ed `packed-alignof.cpp`.

--- lldb/test/Shell/SymbolFile/DWARF/packed.cpp
+++ lldb/test/Shell/SymbolFile/DWARF/packed.cpp
@@ -4,6 +4,8 @@
 // RUN:   -o "expr sizeof(packed)" \
 // RUN:   -o "expr alignof(packed_and_aligned)" \
 // RUN:   -o "expr sizeof(packed_and_aligned)" \
+// RUN:   -o "expr alignof(derived)" \
+// RUN:   -o "expr sizeof(derived)" \
 // RUN:   -o exit | FileCheck %s
 
 // CHECK:      (lldb) expr alignof(packed)
@@ -15,6 +17,11 @@
 // CHECK-NEXT: ${{.*}} = 16
 // CHECK:      (lldb) expr sizeof(packed_and_aligned)
 // CHECK-NEXT: ${{.*}} = 16
+
+// CHECK:      (lldb) expr alignof(derived)
+// CHECK-NEXT: ${{.*}} = 1
+// CHECK:      (lldb) expr sizeof(derived)
+// CHECK-NEXT: ${{.*}} = 13
 
 struct __attribute__((packed)) packed {
   int x;
@@ -31,5 +38,12 @@ struct __attribute__((packed, aligned(16))) packed_and_aligned {
 } g_packed_and_aligned_struct;
 static_assert(alignof(packed_and_aligned) == 16);
 static_assert(sizeof(packed_and_aligned) == 16);
+
+struct __attribute__((packed)) packed_base { int x; };
+static_assert(alignof(packed_base) == 1);
+
+struct derived : packed, packed_base {} g_derived;
+static_assert(alignof(derived) == 1);
+static_assert(sizeof(derived) == 13);
 
 int main() {}
