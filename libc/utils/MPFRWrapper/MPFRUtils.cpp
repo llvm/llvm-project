ad9e459a8f1d3a49153e48484106b34c4fa7c52e
OverMighty <its.overmighty@gmail.com>
2024-06-28 14:53:25 +0200

    [libc][math][c23] Add f16div C23 math function

--- libc/utils/MPFRWrapper/MPFRUtils.cpp
+++ libc/utils/MPFRWrapper/MPFRUtils.cpp
@@ -935,6 +935,8 @@ template void explain_binary_operation_one_output_error(
 template void
 explain_binary_operation_one_output_error(Operation, const BinaryInput<float> &,
                                           float16, double, RoundingMode);
+template void explain_binary_operation_one_output_error(
+    Operation, const BinaryInput<double> &, float16, double, RoundingMode);
 #endif
 
 template <typename InputType, typename OutputType>
@@ -1102,6 +1104,10 @@ template bool compare_binary_operation_one_output(Operation,
                                                   RoundingMode);
 template bool compare_binary_operation_one_output(Operation,
                                                   const BinaryInput<float> &,
+                                                  float16, double,
+                                                  RoundingMode);
+template bool compare_binary_operation_one_output(Operation,
+                                                  const BinaryInput<double> &,
                                                   float16, double,
                                                   RoundingMode);
 #endif
