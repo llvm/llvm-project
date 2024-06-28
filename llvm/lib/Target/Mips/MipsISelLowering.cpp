fc26068d432952343b201ed117ad8193179cbcab
Nikita Popov <npopov@redhat.com>
2024-06-28 14:30:47 +0200

    [IR] Don't include Module.h in Analysis.h (NFC) (#97023)
    Replace it with a forward declaration instead. Analysis.h is pulled in
    by all passes, but not all passes need to access the module.

--- llvm/lib/Target/Mips/MipsISelLowering.cpp
+++ llvm/lib/Target/Mips/MipsISelLowering.cpp
@@ -55,6 +55,7 @@
 #include "llvm/IR/DerivedTypes.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/GlobalValue.h"
+#include "llvm/IR/Module.h"
 #include "llvm/IR/Type.h"
 #include "llvm/IR/Value.h"
 #include "llvm/MC/MCContext.h"
