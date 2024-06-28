626eef5ecf92e98cbfccfa6134e0a760e7592813
Fangrui Song <i@maskray.me>
2024-06-27 22:51:30 -0700

    [MC] Optimize getCurrentSectionOnly using CurFrag and make it non-nullable
    Follow-up to e48c4011ca80385573f1b92793c75dc98abb228f ("[MC] Cache current fragment in MCStreamer").
    
    Prerequisite: a few commits that removed nullable getCurrentSectionOnly calls.

--- llvm/lib/MC/MCELFStreamer.cpp
+++ llvm/lib/MC/MCELFStreamer.cpp
@@ -102,7 +102,7 @@ void MCELFStreamer::emitAssemblerFlag(MCAssemblerFlag Flag) {
 // needs to be aligned to at least the bundle size.
 static void setSectionAlignmentForBundling(const MCAssembler &Assembler,
                                            MCSection *Section) {
-  if (Section && Assembler.isBundlingEnabled() && Section->hasInstructions())
+  if (Assembler.isBundlingEnabled() && Section->hasInstructions())
     Section->ensureMinAlignment(Align(Assembler.getBundleAlignSize()));
 }
 
