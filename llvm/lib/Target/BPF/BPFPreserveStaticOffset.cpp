ba6d7cfa7953d02ec73668042ff871dca782573d
Nikita Popov <llvm@npopov.com>
2024-06-29 15:05:04 +0200

    [IRBuilder] Don't include Module.h (NFC) (#97159)
    This used to be necessary to fetch the DataLayout, but isn't anymore.

--- llvm/lib/Target/BPF/BPFPreserveStaticOffset.cpp
+++ llvm/lib/Target/BPF/BPFPreserveStaticOffset.cpp
@@ -116,6 +116,7 @@
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/Intrinsics.h"
 #include "llvm/IR/IntrinsicsBPF.h"
+#include "llvm/IR/Module.h"
 #include "llvm/Support/Debug.h"
 #include "llvm/Support/ErrorHandling.h"
 
