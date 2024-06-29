1940d73af11f9ef33a533b8daf11d04e4891b295
hhuebner <hendrik.huebner18@gmail.com>
2024-06-29 20:50:58 +0200

    Fix formatting

--- libc/src/math/generic/sinpif.cpp
+++ libc/src/math/generic/sinpif.cpp
@@ -14,7 +14,7 @@
 #include "src/__support/FPUtil/PolyEval.h"
 #include "src/__support/FPUtil/multiply_add.h"
 #include "src/__support/common.h"
-#include "src/__support/macros/optimization.h"            // LIBC_UNLIKELY
+#include "src/__support/macros/optimization.h" // LIBC_UNLIKELY
 
 namespace LIBC_NAMESPACE {
 
