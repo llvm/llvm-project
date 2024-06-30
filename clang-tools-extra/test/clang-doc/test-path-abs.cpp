40a52e547a3d80f80ffcff4aeb34e93d1cc72e87
PeterChou1 <peter.chou@mail.utoronto.ca>
2024-06-30 03:22:50 -0400

    [clang-doc] fix missing  - in output

--- clang-tools-extra/test/clang-doc/test-path-abs.cpp
+++ clang-tools-extra/test/clang-doc/test-path-abs.cpp
@@ -2,6 +2,6 @@
 // RUN: mkdir %t
 // RUN: echo "CHECK: var RootPath = \"%t\";" > %t/check.txt
 // RUN: cp "%s" "%t/test.cpp"
-// RUN: clang-doc --format=html --executor=standalone -p %t %t/test.cpp -output=%t/docs
+// RUN: clang-doc --format=html --executor=standalone -p %t %t/test.cpp --output=%t/docs
 // RUN: FileCheck %t/check.txt -input-file=%t/docs/index_json.js
 // RUN: rm -rf %t
