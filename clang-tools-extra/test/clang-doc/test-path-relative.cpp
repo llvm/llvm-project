1c7675aa300f9e4bcf6f05091ade335691c6925b
PeterChou1 <peter.chou@mail.utoronto.ca>
2024-06-30 03:27:59 -0400

    [clang-doc] fix platform

--- clang-tools-extra/test/clang-doc/test-path-relative.cpp
+++ clang-tools-extra/test/clang-doc/test-path-relative.cpp
@@ -1,7 +1,7 @@
 // RUN: rm -rf %t
 // RUN: mkdir %t
+// RUN: echo "CHECK: var RootPath = \"%/t/../docs\";" > %t/check.txt
 // RUN: cp "%s" "%t/test.cpp"
-// RUN: echo "CHECK: var RootPath = \"%t/../docs\";" > %t/check.txt
 // RUN: clang-doc --format=html --executor=standalone -p %t %t/test.cpp --output=../docs
 // RUN: FileCheck %t/check.txt -input-file=%t/../docs/index_json.js
 // RUN: rm -rf %t
