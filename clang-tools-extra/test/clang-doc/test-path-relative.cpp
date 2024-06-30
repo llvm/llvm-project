f7863964e01f79006c37a49c87061620ab0d787f
PeterChou1 <peter.chou@mail.utoronto.ca>
2024-06-30 03:16:20 -0400

    [clang-doc] fix tests

--- clang-tools-extra/test/clang-doc/test-path-relative.cpp
+++ clang-tools-extra/test/clang-doc/test-path-relative.cpp
@@ -1,8 +1,7 @@
 // RUN: rm -rf %t
 // RUN: mkdir %t
 // RUN: cp "%s" "%t/test.cpp"
-// RUN: clang-doc --doxygen --executor=standalone -p %t %t/test.cpp -output=../docs
-// RUN: FileCheck %s -input-file=%t/docs/index_json.js
+// RUN: echo "CHECK: var RootPath = \"%t/../docs\";" > %t/check.txt
+// RUN: clang-doc --format=html --executor=standalone -p %t %t/test.cpp -output=../docs
+// RUN: FileCheck %t/check.txt -input-file=%t/../docs/index_json.js
 // RUN: rm -rf %t
-
-// CHECK: var RootPath = "{{.*}}../docs";
