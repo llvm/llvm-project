725417dfb2cace41fc25d0ed5271ace746030799
Joseph Huber <huberjn@outlook.com>
2024-07-01 13:55:07 -0500

    Update clang/lib/Basic/Targets/NVPTX.h

--- clang/lib/Basic/Targets/NVPTX.h
+++ clang/lib/Basic/Targets/NVPTX.h
@@ -116,7 +116,7 @@ public:
   }
 
   BuiltinVaListKind getBuiltinVaListKind() const override {
-    return TargetInfo::VoidPtrBuiltinVaList;
+    return TargetInfo::CharPtrBuiltinVaList;
   }
 
   bool isValidCPUName(StringRef Name) const override {
