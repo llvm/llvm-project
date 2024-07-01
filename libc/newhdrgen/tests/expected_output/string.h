62b6d624aaa7897bea05448548ea6039f8e6d06f
Aaryan Shukla <aaryanshukla@google.com>
2024-07-01 23:21:39 +0000

    clang formatter on string.h

--- libc/newhdrgen/tests/expected_output/string.h
+++ libc/newhdrgen/tests/expected_output/string.h
@@ -17,51 +17,51 @@
 
 __BEGIN_C_DECLS
 
-void * memccpy(void *__restrict, const void *__restrict, int, size_t);
+void *memccpy(void *__restrict, const void *__restrict, int, size_t);
 
-void * memchr(const void *, int, size_t);
+void *memchr(const void *, int, size_t);
 
 int memcmp(const void *, const void *, size_t);
 
-void * memcpy(void *__restrict, const void *__restrict, size_t);
+void *memcpy(void *__restrict, const void *__restrict, size_t);
 
-void * memmem(const void *, size_t, const void *, size_t);
+void *memmem(const void *, size_t, const void *, size_t);
 
-void * memmove(void *, const void *, size_t);
+void *memmove(void *, const void *, size_t);
 
-void * mempcpy(void *__restrict, const void *__restrict, size_t);
+void *mempcpy(void *__restrict, const void *__restrict, size_t);
 
-void * memrchr(const void *, int, size_t);
+void *memrchr(const void *, int, size_t);
 
-void * memset(void *, int, size_t);
+void *memset(void *, int, size_t);
 
-void * memset_explicit(void *, int, size_t);
+void *memset_explicit(void *, int, size_t);
 
-char * stpcpy(char *__restrict, const char *__restrict);
+char *stpcpy(char *__restrict, const char *__restrict);
 
-char * stpncpy(char *__restrict, const char *__restrict, size_t);
+char *stpncpy(char *__restrict, const char *__restrict, size_t);
 
-char * strcasestr(const char *, const char *);
+char *strcasestr(const char *, const char *);
 
-char * strcat(char *__restrict, const char *__restrict);
+char *strcat(char *__restrict, const char *__restrict);
 
-char * strchr(const char *, int);
+char *strchr(const char *, int);
 
-char * strchrnul(const char *, int);
+char *strchrnul(const char *, int);
 
 int strcmp(const char *, const char *);
 
 int strcoll(const char *, const char *);
 
-char * strcpy(char *__restrict, const char *__restrict);
+char *strcpy(char *__restrict, const char *__restrict);
 
 size_t strcspn(const char *, const char *);
 
-char * strdup(const char *);
+char *strdup(const char *);
 
-char * strerror(int);
+char *strerror(int);
 
-char * strerror_r(int, char *, size_t);
+char *strerror_r(int, char *, size_t);
 
 size_t strlcat(const char *__restrict, const char *__restrict, size_t);
 
@@ -69,31 +69,31 @@ size_t strlcpy(const char *__restrict, const char *__restrict, size_t);
 
 size_t strlen(const char *);
 
-char * strncat(char *, const char *, size_t);
+char *strncat(char *, const char *, size_t);
 
 int strncmp(const char *, const char *, size_t);
 
-char * strncpy(char *__restrict, const char *__restrict, size_t);
+char *strncpy(char *__restrict, const char *__restrict, size_t);
 
-char * strndup(const char *, size_t);
+char *strndup(const char *, size_t);
 
 size_t strnlen(const char *, size_t);
 
-char * strpbrk(const char *, const char *);
+char *strpbrk(const char *, const char *);
 
-char * strrchr(const char *, int);
+char *strrchr(const char *, int);
 
-char * strsep(char * *__restrict, const char *__restrict);
+char *strsep(char **__restrict, const char *__restrict);
 
-char * strsignal(int);
+char *strsignal(int);
 
 size_t strspn(const char *, const char *);
 
-char * strstr(const char *, const char *);
+char *strstr(const char *, const char *);
 
-char * strtok(char *__restrict, const char *__restrict);
+char *strtok(char *__restrict, const char *__restrict);
 
-char * strtok_r(char *__restrict, const char *__restrict, char * *__restrict);
+char *strtok_r(char *__restrict, const char *__restrict, char **__restrict);
 
 size_t strxfrm(char *__restrict, const char *__restrict, size_t);
 
