5c287efee77930ab15fe5579bed00b2750bbb254
Haojian Wu <hokein.wu@gmail.com>
2024-07-01 17:43:07 +0200

    [Clang] Extend lifetime bound analysis to support assignments for the built-in pointer type (#96475)
    The lifetime bound warning in Clang currently only considers
    initializations. This patch extends the warning to include assignments.
    
    - **Support for assignments of built-in pointer types**: this is done is
    by reusing the existing statement-local implementation. Clang now warns
    if the pointer is assigned to a temporary object that being destoryed at
    the end of the full assignment expression.
    
    With this patch, we will detect more cases under the on-by-default
    diagnostic `-Wdangling`. I have added a new category for this specific
    diagnostic so that people can temporarily disable it if their codebase
    is not yet clean.
    
    This is the first step to address #63310, focusing only on pointer
    types. Support for C++ assignment operators will come in a follow-up
    patch.
    
    Fixes #54492

--- clang/lib/Sema/SemaExpr.cpp
+++ clang/lib/Sema/SemaExpr.cpp
@@ -10,6 +10,7 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include "CheckExprLifetime.h"
 #include "TreeTransform.h"
 #include "UsedDeclVisitor.h"
 #include "clang/AST/ASTConsumer.h"
@@ -13777,6 +13778,9 @@ QualType Sema::CheckAssignmentOperands(Expr *LHSExpr, ExprResult &RHS,
     return QualType();
 
   CheckForNullPointerDereference(*this, LHSExpr);
+
+  AssignedEntity AE{LHSExpr};
+  checkExprLifetime(*this, AE, RHS.get());
 
   if (getLangOpts().CPlusPlus20 && LHSType.isVolatileQualified()) {
     if (CompoundType.isNull()) {
