f2844358ab84ff2eb6f898a2f3d0850498367033
Haojian Wu <hokein.wu@gmail.com>
2024-07-01 17:43:07 +0200

    [Clang] Extend lifetime bound analysis to support assignments for the built-in pointer type (#96475)
    The lifetime bound warning in Clang currently only considers
    initializations. This patch extends the warning to include assignments.
    
    - **Support for assignments of built-in pointer types**: this is done is
    by reusing the existing statement-local implementation. Clang now warns
    if the pointer is assigned to a temporary object that being destoryed at
    the end of the full assignment expression.
    
    With this patch, we will detect more cases under the on-by-default
    diagnostic `-Wdangling`. I have added a new category for this specific
    diagnostic so that people can temporarily disable it if their codebase
    is not yet clean.
    
    This is the first step to address #63310, focusing only on pointer
    types. Support for C++ assignment operators will come in a follow-up
    patch.
    
    Fixes #54492

--- clang/test/SemaCXX/warn-dangling-local.cpp
+++ clang/test/SemaCXX/warn-dangling-local.cpp
@@ -4,8 +4,10 @@ using T = int[];
 
 void f() {
   int *p = &(int&)(int&&)0; // expected-warning {{temporary whose address is used as value of local variable 'p' will be destroyed at the end of the full-expression}}
+  p =  &(int&)(int&&)0; // expected-warning {{object backing the pointer p will be destroyed at the end of the full-expression}}
 
   int *q = (int *const &)T{1, 2, 3}; // expected-warning {{temporary whose address is used as value of local variable 'q' will be destroyed at the end of the full-expression}}
+  q = (int *const &)T{1, 2, 3}; // expected-warning {{object backing the pointer q will be destroyed at the end of the full-expression}}
 
   // FIXME: We don't warn here because the 'int*' temporary is not const, but
   // it also can't have actually changed since it was created, so we could
