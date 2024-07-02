3e9557c0c3f1c704c51586891fca9a5d3dd577ab
Izaak Schroeder <izaak.schroeder@gmail.com>
2024-07-02 14:01:47 -0700

    fixup! more locale funcs

---
+++ libc/src/string/strxfrm_l.cpp
@@ -0,0 +1,23 @@
+//===-- Implementation of strxfrm_l ---------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "src/string/strxfrm_l.h"
+#include "src/string/strxfrm.h"
+
+#include "src/__support/common.h"
+
+namespace LIBC_NAMESPACE {
+
+// TODO: Add support for locales.
+LLVM_LIBC_FUNCTION(size_t, strxfrm_l,
+                   (char *__restrict dest, const char *__restrict src,
+                    size_t n, locale_t)) {
+  return strxfrm(dest, src, n);
+}
+
+} // namespace LIBC_NAMESPACE
