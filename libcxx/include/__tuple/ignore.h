bd82810eb04cbdfaebeed6ad254831de1b687885
Hristo Hristov <hghristov.rmm@gmail.com>
2024-07-02 10:40:15 +0300

    [libc++][tuple][utility] P2968R2 Make `std::ignore` a first-class object
    Implements as DR11:  https://wg21.link/P2968R2
    
    References:
    - https://eel.is/c++draft/tuple.general
    - https://eel.is/c++draft/tuple.syn
    - https://eel.is/c++draft/tuple.creation
    - https://github.com/cplusplus/draft/milestone/31
    - https://github.com/cplusplus/papers/issues/1640
    - https://cplusplus.github.io/LWG/issue2933
    - https://cplusplus.github.io/LWG/issue3978

---
+++ libcxx/include/__tuple/ignore.h
@@ -0,0 +1,32 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef _LIBCPP___TUPLE_IGNORE_H
+#define _LIBCPP___TUPLE_IGNORE_H
+
+#include <__config>
+
+#if !defined(_LIBCPP_HAS_NO_PRAGMA_SYSTEM_HEADER)
+#  pragma GCC system_header
+#endif
+
+#ifndef _LIBCPP_CXX03_LANG
+
+_LIBCPP_BEGIN_NAMESPACE_STD
+
+struct __ignore_type {
+  _LIBCPP_HIDE_FROM_ABI constexpr const __ignore_type& operator=(const auto&) const noexcept { return *this; }
+};
+
+inline constexpr __ignore_type ignore;
+
+_LIBCPP_END_NAMESPACE_STD
+
+#endif // _LIBCPP_CXX03_LANG
+
+#endif // _LIBCPP___TUPLE_IGNORE_H
--- libcxx/test/std/utilities/tuple/tuple.general/ignore.pass.cpp
+++ libcxx/test/std/utilities/tuple/tuple.general/ignore.pass.cpp
@@ -6,11 +6,11 @@
 //
 //===----------------------------------------------------------------------===//
 
+// UNSUPPORTED: c++03
+
 // <tuple>
 
-// constexpr unspecified ignore;
-
-// UNSUPPORTED: c++03
+// inline constexpr ignore-type ignore;
 
 #include <cassert>
 #include <tuple>
@@ -18,39 +18,34 @@
 
 #include "test_macros.h"
 
-constexpr bool test_ignore_constexpr()
-{
-#if TEST_STD_VER > 11
-    { // Test that std::ignore provides constexpr converting assignment.
-        auto& res = (std::ignore = 42);
-        assert(&res == &std::ignore);
-    }
-    { // Test that std::ignore provides constexpr copy/move constructors
-        auto copy = std::ignore;
-        auto moved = std::move(copy);
-        ((void)moved);
-    }
-    { // Test that std::ignore provides constexpr copy/move assignment
-        auto copy = std::ignore;
-        copy = std::ignore;
-        auto moved = std::ignore;
-        moved = std::move(copy);
-    }
-#endif
-    return true;
+constexpr bool test() {
+  {
+    constexpr auto& ignore_v = std::ignore;
+    ((void)ignore_v);
+  }
+  { // Test that std::ignore provides constexpr converting assignment.
+    auto& res = (std::ignore = 42);
+    assert(&res == &std::ignore);
+  }
+  { // Test that std::ignore provides constexpr copy/move constructors
+    auto copy  = (std::ignore = 42);
+    auto moved = std::move(copy);
+    ((void)moved);
+  }
+  { // Test that std::ignore provides constexpr copy/move assignment
+    auto copy  = (std::ignore = 82);
+    copy       = std::ignore;
+    auto moved = (std::ignore = 94);
+    moved      = std::move(copy);
+  }
+
+  return true;
 }
 
 int main(int, char**) {
-    {
-        constexpr auto& ignore_v = std::ignore;
-        ((void)ignore_v);
-    }
-    {
-        static_assert(test_ignore_constexpr(), "");
-    }
-    {
-        LIBCPP_STATIC_ASSERT(std::is_trivial<decltype(std::ignore)>::value, "");
-    }
+  test();
+  static_assert(test(), "");
+  LIBCPP_STATIC_ASSERT(std::is_trivial<decltype(std::ignore)>::value, "");
 
   return 0;
 }
