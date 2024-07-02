ee64234937cff16be97efccfa66d02ccaf25082e
Davide Italiano <davidino@fb.com>
2024-07-02 08:41:24 -0700

    Revert "[MC] Remove assert to work around BOLT"
    This reverts commit 09413742a754a32552d4132b406afb1d880d0c7e.
    The patch was not reviewed, and this assertion guards against incorrect
    values for symbols if the linker decides to modify the layout as it
    happens on some platforms. getSymbolOffset() is used for code size
    estimation, among others.
    
    Reverting for now, until we get proper code review and undersatnding of
    the implications.

--- llvm/lib/MC/MCAssembler.cpp
+++ llvm/lib/MC/MCAssembler.cpp
@@ -538,6 +538,7 @@ bool MCAssembler::getSymbolOffset(const MCSymbol &S, uint64_t &Val) const {
 }
 
 uint64_t MCAssembler::getSymbolOffset(const MCSymbol &S) const {
+  assert(HasLayout);
   uint64_t Val;
   getSymbolOffsetImpl(*this, S, true, Val);
   return Val;
