e6f0e544226f386b6da0e80bf5bcbfc339a12cc8
Shubham Sandeep Rastogi <srastogi22@apple.com>
2024-07-03 10:41:34 -0700

    Fix build error with automerging 057f28be3e11
    rdar://131017120
    ([Automerger: llvm.org/main -> next] failed to build merge with
    057f28be3e11 ([MC] Remove unused MCAsmLayout declarations and includes))

--- llvm/include/llvm/MC/MCMachObjectWriter.h
+++ llvm/include/llvm/MC/MCMachObjectWriter.h
@@ -12,6 +12,7 @@
 #include "llvm/ADT/DenseMap.h"
 #include "llvm/ADT/StringRef.h"
 #include "llvm/BinaryFormat/MachO.h"
+#include "llvm/MC/MCAsmLayout.h"
 #include "llvm/MC/MCExpr.h"
 #include "llvm/MC/MCObjectWriter.h"
 #include "llvm/MC/MCSection.h"
