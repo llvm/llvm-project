7f9c172662717be33e8d340b5eb744c51c6137b6
Sam <srcarroll314@gmail.com>
2024-07-03 11:54:39 -0500

    Fix bug in fusion refactor and add test

--- mlir/lib/Interfaces/LoopLikeInterface.cpp
+++ mlir/lib/Interfaces/LoopLikeInterface.cpp
@@ -138,6 +138,7 @@ LoopLikeOpInterface mlir::createFused(LoopLikeOpInterface target,
   if (failed(maybeFusedLoop))
     llvm_unreachable("failed to replace loop");
   LoopLikeOpInterface fusedLoop = *maybeFusedLoop;
+  rewriter.moveOpBefore(fusedLoop, source);
 
   // Map control operands.
   IRMapping mapping;
@@ -145,7 +146,8 @@ LoopLikeOpInterface mlir::createFused(LoopLikeOpInterface target,
       fusedLoop.getLoopInductionVars();
   if (fusedInductionVar) {
     if (!targetInductionVar || !sourceInductionVar)
-      llvm_unreachable("expected target and source loops to have induction vars");
+      llvm_unreachable(
+          "expected target and source loops to have induction vars");
     mapping.map(*targetInductionVar, *fusedInductionVar);
     mapping.map(*sourceInductionVar, *fusedInductionVar);
   }
