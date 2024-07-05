f7dd9d8ff798cc083bc7a5b41a65499e730c814b
Song, Yunfei <yunfei.song@intel.com>
2024-07-05 02:12:59 -0700

    extend fuse producer to multi-level extractSliceOp

--- mlir/include/mlir/Dialect/SCF/Transforms/TileUsingInterface.h
+++ mlir/include/mlir/Dialect/SCF/Transforms/TileUsingInterface.h
@@ -157,10 +157,14 @@ struct SCFFuseProducerOfSliceResult {
   Value tiledAndFusedProducer; // Tile and fused producer value.
   SmallVector<Operation *> tiledOps;
 };
+
 std::optional<SCFFuseProducerOfSliceResult>
 tileAndFuseProducerOfSlice(RewriterBase &rewriter,
                            tensor::ExtractSliceOp candidateSliceOp,
                            MutableArrayRef<LoopLikeOpInterface> loops);
+
+std::optional<SCFFuseProducerOfSliceResult>
+tileAndFuseProducerOfSlice(RewriterBase &rewriter, Operation *candidateSliceOp);
 
 /// Reconstruct the fused producer from within the tiled-and-fused code. Based
 /// on the slice of the producer computed in place it is possible that within
