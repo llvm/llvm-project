009082aa4b20462d46c885d5abc9320a887f1932
Fangrui Song <i@maskray.me>
2024-07-04 23:34:54 -0700

    [MC] Move MCAssembler::DataRegions to MachObjectWriter
    and make some cleanup.

--- llvm/lib/MC/MCAssembler.cpp
+++ llvm/lib/MC/MCAssembler.cpp
@@ -97,7 +97,6 @@ void MCAssembler::reset() {
   IncrementalLinkerCompatible = false;
   Sections.clear();
   Symbols.clear();
-  DataRegions.clear();
   LinkerOptions.clear();
   FileNames.clear();
   ThumbFuncs.clear();
