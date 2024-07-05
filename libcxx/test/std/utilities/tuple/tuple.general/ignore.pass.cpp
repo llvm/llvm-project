99900ba54551053177daefb8def4e1b74f321915
Hristo Hristov <hghristov.rmm@gmail.com>
2024-07-05 14:36:40 +0300

    Updated tests

--- libcxx/test/std/utilities/tuple/tuple.general/ignore.pass.cpp
+++ libcxx/test/std/utilities/tuple/tuple.general/ignore.pass.cpp
@@ -13,18 +13,42 @@
 // inline constexpr ignore-type ignore;
 
 #include <cassert>
+#include <cstdint>
 #include <tuple>
 #include <type_traits>
 
 #include "test_macros.h"
 
+static_assert(std::is_trivial<decltype(std::ignore)>::value, "");
+
+#if TEST_STD_VER >= 17
+[[nodiscard]] constexpr int test_nodiscard() { return 8294; }
+#endif
+
+constexpr bool test() {
+#if TEST_STD_VER >= 17
+  { std::ignore = test_nodiscard(); }
+#endif
+
+  return true;
+}
+
 int main(int, char**) {
-  static_assert(std::is_trivial<decltype(std::ignore)>::value, "");
+  test();
+  static_assert(test(), "");
 
   { [[maybe_unused]] constexpr auto& ignore_v = std::ignore; }
   { // Test that std::ignore provides constexpr converting assignment.
     constexpr auto& res = (std::ignore = 42);
     static_assert(noexcept(res = (std::ignore = 42)), "Must be noexcept");
+    assert(&res == &std::ignore);
+  }
+  { // Test bit-field binding.
+    struct S {
+      unsigned int bf : 3;
+    };
+    constexpr S s{0b010};
+    constexpr auto& res = (std::ignore = s.bf);
     assert(&res == &std::ignore);
   }
   { // Test that std::ignore provides constexpr copy/move constructors
