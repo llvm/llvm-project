c0b952b1c893d66c1c84e1cc8e6c8fb1990a60a1
Nikolas Klauser <nikolasklauser@berlin.de>
2024-07-06 16:52:45 +0200

    [libc++] Remove annotations for GCC 13 and update the documentation (#97744)
    GCC 14 has been released a while ago. We've updated the CI to use GCC 14
    now. This removes any old annotations in the tests and updates the
    documentation to reflect the updated version requirements.

--- libcxx/test/libcxx/gdb/gdb_pretty_printer_test.sh.cpp
+++ libcxx/test/libcxx/gdb/gdb_pretty_printer_test.sh.cpp
@@ -14,9 +14,6 @@
 // TODO: Investigate these failures which break the CI.
 // UNSUPPORTED: clang-17, clang-18, clang-19
 
-// TODO: Investigate this failure on GCC 13 (in Ubuntu Jammy)
-// UNSUPPORTED: gcc-13
-
 // The Android libc++ tests are run on a non-Android host, connected to an
 // Android device over adb. gdb needs special support to make this work (e.g.
 // gdbclient.py, ndk-gdb.py, gdbserver), and the Android organization doesn't
