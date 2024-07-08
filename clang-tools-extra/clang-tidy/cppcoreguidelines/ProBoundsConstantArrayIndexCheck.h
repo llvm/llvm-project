05ec838f3307f3f97863da641ece68a885d082e9
Paul Heidekrüger <paul.heidekrueger@tum.de>
2024-07-08 12:21:27 +0200

    Use TK_IgnoreUnlessSpelledInSource as traversal kind
    Co-authored-by: Manuel Pietsch <manuelpietsch@outlook.de>

--- clang-tools-extra/clang-tidy/cppcoreguidelines/ProBoundsConstantArrayIndexCheck.h
+++ clang-tools-extra/clang-tidy/cppcoreguidelines/ProBoundsConstantArrayIndexCheck.h
@@ -32,6 +32,10 @@ public:
                            Preprocessor *ModuleExpanderPP) override;
   void registerMatchers(ast_matchers::MatchFinder *Finder) override;
   void storeOptions(ClangTidyOptions::OptionMap &Opts) override;
+  std::optional<TraversalKind> getCheckTraversalKind() const override {
+    return TK_IgnoreUnlessSpelledInSource;
+  }
+
   void check(const ast_matchers::MatchFinder::MatchResult &Result) override;
 };
 
