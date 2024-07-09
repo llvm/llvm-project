9cd683a6c596ab03e02a909b683352966537ee9f
Mital Ashok <mital@mitalashok.co.uk>
2024-07-09 18:05:33 +0100

    Remove test for <stdatomic.h> in C23/n2653.c
    Still fails with -nostdsysteminc -ffreestanding (seems to be pulling in other system headers still)

--- clang/test/C/C23/n2653.c
+++ clang/test/C/C23/n2653.c
@@ -1,16 +1,9 @@
-// RUN: %clang_cc1 -ffreestanding -verify=c23 -std=c23 %s
-// RUN: %clang_cc1 -ffreestanding -verify=c17 -std=c17 %s
+// RUN: %clang_cc1 -verify=c23 -std=c23 %s
+// RUN: %clang_cc1 -verify=c17 -std=c17 %s
 
 // c23-no-diagnostics
 
-#include <stdatomic.h>
-
 #define __enable_constant_folding(x) (__builtin_constant_p(x) ? (x) : (x))
-
-#ifndef ATOMIC_CHAR8_T_LOCK_FREE
-#error missing
-#endif
-// c17-error@-2 {{missing}}
 
 _Static_assert(_Generic(u8"", unsigned char*: 1, char*: 0), "");
 // c17-error@-1 {{static assertion failed}}
