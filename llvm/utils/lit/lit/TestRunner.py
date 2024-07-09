b68584571de3370ba7655b62047908b28a81d56c
Jay Foad <jay.foad@gmail.com>
2024-07-09 17:17:57 +0100

    Revert "[lit] Implement builtin umask (#94621)"
    This reverts commit 9f6dd1f761a5121e9a69e5d1f67c2438c065b499.

--- llvm/utils/lit/lit/TestRunner.py
+++ llvm/utils/lit/lit/TestRunner.py
@@ -87,10 +87,9 @@ class ShellEnvironment(object):
     we maintain a dir stack for pushd/popd.
     """
 
-    def __init__(self, cwd, env, umask=-1):
+    def __init__(self, cwd, env):
         self.cwd = cwd
         self.env = dict(env)
-        self.umask = umask
         self.dirStack = []
 
     def change_dir(self, newdir):
@@ -566,20 +565,6 @@ def executeBuiltinRm(cmd, cmd_shenv):
     return ShellCommandResult(cmd, "", stderr.getvalue(), exitCode, False)
 
 
-def executeBuiltinUmask(cmd, shenv):
-    """executeBuiltinUmask - Change the current umask."""
-    if os.name != "posix":
-        raise InternalShellError(cmd, "'umask' not supported on this system")
-    if len(cmd.args) != 2:
-        raise InternalShellError(cmd, "'umask' supports only one argument")
-    try:
-        # Update the umask in the parent environment.
-        shenv.umask = int(cmd.args[1], 8)
-    except ValueError as err:
-        raise InternalShellError(cmd, "Error: 'umask': %s" % str(err))
-    return ShellCommandResult(cmd, "", "", 0, False)
-
-
 def executeBuiltinColon(cmd, cmd_shenv):
     """executeBuiltinColon - Discard arguments and exit with status 0."""
     return ShellCommandResult(cmd, "", "", 0, False)
@@ -734,7 +719,6 @@ def _executeShCmd(cmd, shenv, results, timeoutHelper):
         "popd": executeBuiltinPopd,
         "pushd": executeBuiltinPushd,
         "rm": executeBuiltinRm,
-        "umask": executeBuiltinUmask,
         ":": executeBuiltinColon,
     }
     # To avoid deadlock, we use a single stderr stream for piped
@@ -756,7 +740,7 @@ def _executeShCmd(cmd, shenv, results, timeoutHelper):
                 #   env FOO=1 llc < %s | env BAR=2 llvm-mc | FileCheck %s
                 #   env FOO=1 %{another_env_plus_cmd} | FileCheck %s
                 if cmd_shenv is shenv:
-                    cmd_shenv = ShellEnvironment(shenv.cwd, shenv.env, shenv.umask)
+                    cmd_shenv = ShellEnvironment(shenv.cwd, shenv.env)
                 args = updateEnv(cmd_shenv, args)
                 if not args:
                     raise InternalShellError(j, "Error: 'env' requires a" " subcommand")
@@ -900,7 +884,6 @@ def _executeShCmd(cmd, shenv, results, timeoutHelper):
                     close_fds=kUseCloseFDs,
                     universal_newlines=True,
                     errors="replace",
-                    umask=cmd_shenv.umask,
                 )
             )
             proc_not_counts.append(not_count)
