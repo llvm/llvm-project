05d1e3333b69e0dda3e290981fc1f11d7f63477b
Stanislav Mekhanoshin <rampitec@users.noreply.github.com>
2024-07-09 14:25:58 -0700

    [AMDGPU] Report error in clang if wave32 is requested where unsupported (#97633)

--- llvm/include/llvm/TargetParser/TargetParser.h
+++ llvm/include/llvm/TargetParser/TargetParser.h
@@ -157,6 +157,12 @@ enum ArchFeatureKind : uint32_t {
   FEATURE_WGP = 1 << 9,
 };
 
+enum FeatureError : uint32_t {
+  NO_ERROR = 0,
+  INVALID_FEATURE_COMBINATION,
+  UNSUPPORTED_TARGET_FEATURE
+};
+
 StringRef getArchFamilyNameAMDGCN(GPUKind AK);
 
 StringRef getArchNameAMDGCN(GPUKind AK);
@@ -177,8 +183,9 @@ void fillAMDGPUFeatureMap(StringRef GPU, const Triple &T,
                           StringMap<bool> &Features);
 
 /// Inserts wave size feature for given GPU into features map
-bool insertWaveSizeFeature(StringRef GPU, const Triple &T,
-                           StringMap<bool> &Features, std::string &ErrorMsg);
+std::pair<FeatureError, StringRef>
+insertWaveSizeFeature(StringRef GPU, const Triple &T,
+                      StringMap<bool> &Features);
 
 } // namespace AMDGPU
 } // namespace llvm
