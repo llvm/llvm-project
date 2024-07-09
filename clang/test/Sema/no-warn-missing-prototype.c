1143a06a4b4b7f435c19325bd3b2253cde02372c
MaxEW707 <max.enrico.winkler@gmail.com>
2024-07-08 19:19:07 -0700

    formatting

--- clang/test/Sema/no-warn-missing-prototype.c
+++ clang/test/Sema/no-warn-missing-prototype.c
@@ -3,31 +3,27 @@
 // RUN: %clang_cc1 -fms-compatibility -fsyntax-only -x c++ -ffreestanding -triple=x86_64-pc-win32 -verify -DMS %s
 // expected-no-diagnostics
 int main() {
-    return 0;
+  return 0;
 }
 
 int efi_main() {
-    return 0;
+  return 0;
 }
 
 #ifdef MS
-int wmain(int, wchar_t *[], wchar_t *[])
-{
-    return 0;
+int wmain(int, wchar_t *[], wchar_t *[]) {
+  return 0;
 }
 
-int wWinMain(void*, void*, wchar_t*, int)
-{
-    return 0;
+int wWinMain(void*, void*, wchar_t*, int) {
+  return 0;
 }
 
-int WinMain(void*, void*, char*, int)
-{
-    return 0;
+int WinMain(void*, void*, char*, int) {
+  return 0;
 }
 
-bool DllMain(void*, unsigned, void*)
-{
-    return true;
+bool DllMain(void*, unsigned, void* {
+  return true;
 }
 #endif
