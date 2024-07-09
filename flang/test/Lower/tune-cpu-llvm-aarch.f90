48e468a8689057cd5077fd6e62c217ef37ad6a8d
Alexis Perry-Holby <aperry@lanl.gov>
2024-07-09 15:44:51 -0600

    mtune tests - remove unnecessary target guards, combine test files

--- flang/test/Lower/tune-cpu-llvm-aarch.f90
+++
@@ -1,7 +0,0 @@
-! REQUIRES: aarch64-registered-target
-! RUN: %flang -mtune=neoverse-n1 -S -emit-llvm %s -o - | FileCheck %s
-
-!CHECK: attributes #{{[0-9]+}} = {
-!CHECK-SAME: "tune-cpu"="neoverse-n1"
-subroutine a
-end subroutine a
