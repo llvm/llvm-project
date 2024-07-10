28b646792249bad0a01d9724cc538df5d41736da
T-Gruber <tobi.gruber@gmx.de>
2024-07-10 10:37:17 +0200

    Adapted format of file header

--- clang/unittests/StaticAnalyzer/ExprEngineVisitTest.cpp
+++ clang/unittests/StaticAnalyzer/ExprEngineVisitTest.cpp
@@ -1,10 +1,11 @@
-//===- ExprEngineVisitTest.cpp -----------------------------------===//
+//===- ExprEngineVisitTest.cpp --------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
+
 
 #include "CheckerRegistration.h"
 #include "clang/AST/Stmt.h"
