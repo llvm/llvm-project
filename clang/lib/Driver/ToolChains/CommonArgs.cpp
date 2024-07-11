d74228c36b6985fbe5382eda07c24c5124125485
Rainer Orth <ro@gcc.gnu.org>
2024-07-11 14:04:45 +0200

    [clang][Driver] Fix safestack -u ordering (#98468)
    When re-enabling safestack testing on Solaris after the unexplained
    b0260c5b1052f8e3ff1ec77dc42a11f42da762cc, all tests `FAIL`ed to link:
    
    ```
    Undefined			first referenced
     symbol  			    in file
    __safestack_unsafe_stack_ptr        buffer-copy-vla.o
    __safestack_init                    (command line)
    ld: fatal: symbol referencing errors
    ```
    
    The problem is that `-u __safestack_init` was passed to the linker after
    the corresponding version of `libclang_rt.safestack-*.a`. Since the
    Solaris linker (like Unix linkers for decades) respects the command line
    argument order (unlike e.g. GNU ld which uses GNU getopt), this cannot
    work. Fixed by moving the `-u` arg further to the front. Two affected
    testcases were fixed accordingly.
    
    Tested on `amd64-pc-solaris2.11`, `sparcv9-sun-solaris2.11`,
    `x86_64-pc-linux-gnu`, and `sparc64-unknown-linux-gnu`.

--- clang/lib/Driver/ToolChains/CommonArgs.cpp
+++ clang/lib/Driver/ToolChains/CommonArgs.cpp
@@ -1534,6 +1534,12 @@ bool tools::addSanitizerRuntimes(const ToolChain &TC, const ArgList &Args,
                              RequiredSymbols);
   }
 
+  // -u options must be added before the runtime libs that resolve them.
+  for (auto S : RequiredSymbols) {
+    CmdArgs.push_back("-u");
+    CmdArgs.push_back(Args.MakeArgString(S));
+  }
+
   // Inject libfuzzer dependencies.
   if (SanArgs.needsFuzzer() && SanArgs.linkRuntimes() &&
       !Args.hasArg(options::OPT_shared)) {
@@ -1565,10 +1571,6 @@ bool tools::addSanitizerRuntimes(const ToolChain &TC, const ArgList &Args,
   for (auto RT : NonWholeStaticRuntimes) {
     addSanitizerRuntime(TC, Args, CmdArgs, RT, false, false);
     AddExportDynamic |= !addSanitizerDynamicList(TC, Args, CmdArgs, RT);
-  }
-  for (auto S : RequiredSymbols) {
-    CmdArgs.push_back("-u");
-    CmdArgs.push_back(Args.MakeArgString(S));
   }
   // If there is a static runtime with no dynamic list, force all the symbols
   // to be dynamic to be sure we export sanitizer interface functions.
