20105bc3d91dce5214ea1bd906deccc6a93ee79c
Chuanqi Xu <yedeng.yd@linux.alibaba.com>
2024-07-12 13:53:20 +0800

    [C++20] [Modules] Allow export redeclarations within language linkage
    Close https://github.com/llvm/llvm-project/issues/98583
    
    Currently, clang will reject the following code:
    
    ```
    export module mod;
    extern "C++" void func();
    export extern "C++" {
        void func();
    }
    ```
    
    while both MSVC and GCC accept it. Although clang's behavior matches the
    current wording, from the discussion, the consensus is that we should
    accept the above example from the intention. Since the intention to not
    allow export redeclaration which is not exported is to make the linkage
    clear. But it doesn't matter with the declarations within global module.

--- clang/lib/Sema/SemaDecl.cpp
+++ clang/lib/Sema/SemaDecl.cpp
@@ -1676,6 +1676,15 @@ bool Sema::CheckRedeclarationExported(NamedDecl *New, NamedDecl *Old) {
   if (IsOldExported)
     return false;
 
+  // If the Old declaration are not attached to named modules
+  // and the New declaration are attached to global module.
+  // It should be fine to allow the export since it doesn't change
+  // the linkage of declarations. See
+  // https://github.com/llvm/llvm-project/issues/98583 for details.
+  if (!Old->isInNamedModule() && New->getOwningModule() &&
+      New->getOwningModule()->isImplicitGlobalModule())
+    return false;
+
   assert(IsNewExported);
 
   auto Lk = Old->getFormalLinkage();
