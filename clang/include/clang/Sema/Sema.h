9d1017204a253782a82d1b7d16c59eea3f811a11
Aaron Ballman <aaron@aaronballman.com>
2024-07-12 06:58:43 -0400

    Revert "[clang] Catch missing format attributes" (#98617)
    Reverts llvm/llvm-project#70024
    
    It broke several post-commit bots:
    https://lab.llvm.org/buildbot/#/builders/193/builds/896
    https://lab.llvm.org/buildbot/#/builders/23/builds/925
    https://lab.llvm.org/buildbot/#/builders/13/builds/686
    and others

--- clang/include/clang/Sema/Sema.h
+++ clang/include/clang/Sema/Sema.h
@@ -4594,10 +4594,6 @@ public:
 
   enum class RetainOwnershipKind { NS, CF, OS };
 
-  void DiagnoseMissingFormatAttributes(Stmt *Body, const FunctionDecl *FDecl);
-  std::vector<FormatAttr *>
-  GetMissingFormatAttributes(Stmt *Body, const FunctionDecl *FDecl);
-
   UuidAttr *mergeUuidAttr(Decl *D, const AttributeCommonInfo &CI,
                           StringRef UuidAsWritten, MSGuidDecl *GuidDecl);
 
