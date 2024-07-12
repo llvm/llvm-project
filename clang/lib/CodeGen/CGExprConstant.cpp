5c4b58b2bb281529b7d87c32fda5fe21b3451948
Podchishchaeva, Mariya <mariya.podchishchaeva@intel.com>
2024-07-12 00:36:38 -0700

    Fix format

--- clang/lib/CodeGen/CGExprConstant.cpp
+++ clang/lib/CodeGen/CGExprConstant.cpp
@@ -615,9 +615,9 @@ bool ConstStructBuilder::AppendBitField(const FieldDecl *Field,
 
   llvm::ConstantInt *CI = dyn_cast<llvm::ConstantInt>(C);
   if (!CI) {
-    // Constants for long _BitInt types are sometimes split into individual bytes.
-    // Try to fold these back into an integer constant. If that doesn't work
-    // out, then we are trying to initialize a bitfield with a non-trivial
+    // Constants for long _BitInt types are sometimes split into individual
+    // bytes. Try to fold these back into an integer constant. If that doesn't
+    // work out, then we are trying to initialize a bitfield with a non-trivial
     // constant, this must require run-time code.
     llvm::Type *LoadType =
         CGM.getTypes().convertTypeForLoadStore(Field->getType(), C->getType());
