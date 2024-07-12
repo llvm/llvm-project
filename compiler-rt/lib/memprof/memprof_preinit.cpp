56d1dc456f8e6c88385b5fa73791942c8b4cdf7c
Fangrui Song <i@maskray.me>
2024-07-11 20:46:36 -0700

    [𝘀𝗽𝗿] initial version
    Created using spr 1.3.5-bogner

--- compiler-rt/lib/memprof/memprof_preinit.cpp
+++ compiler-rt/lib/memprof/memprof_preinit.cpp
@@ -18,6 +18,6 @@ using namespace __memprof;
 // The symbol is called __local_memprof_preinit, because it's not intended to
 // be exported. This code linked into the main executable when -fmemory-profile
 // is in the link flags. It can only use exported interface functions.
-__attribute__((section(".preinit_array"),
-               used)) void (*__local_memprof_preinit)(void) = __memprof_preinit;
+__attribute__((section(".preinit_array"), used)) static auto preinit =
+    __memprof_preinit;
 #endif
