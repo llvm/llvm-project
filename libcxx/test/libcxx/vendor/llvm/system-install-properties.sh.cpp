49d341992be12b11e0dc7d5511ba53f76ec4471d
Louis Dionne <ldionne.2@gmail.com>
2024-07-12 10:30:04 -0400

    Try fixing issues on Linux

--- libcxx/test/libcxx/vendor/llvm/system-install-properties.sh.cpp
+++ libcxx/test/libcxx/vendor/llvm/system-install-properties.sh.cpp
@@ -8,8 +8,13 @@
 
 // REQUIRES: stdlib=libc++ && !stdlib=apple-libc++
 
+// DEFINE: %{shared-suffix}=%if target={{.+}}-apple-{{.+}} %{ dylib %} %else %{ so %}
+
 // This file checks various properties of the installation of libc++ when built under
 // a vanilla upstream configuration.
+
+// TODO: temporary to debug
+// RUN: ls %{lib-dir}
 
 // Make sure we install the libc++ headers in the right location.
 //
@@ -17,10 +22,10 @@
 
 // Make sure we install libc++.1.dylib and libc++experimental.a in the right location.
 //
-// RUN: stat "%{lib-dir}/libc++.1.dylib"
+// RUN: stat "%{lib-dir}/libc++.1.%{shared-suffix}"
 // RUN: stat "%{lib-dir}/libc++experimental.a"
 
 // Make sure we install a symlink from libc++.dylib to libc++.1.dylib.
 //
-// RUN: stat "%{lib-dir}/libc++.dylib"
-// RUN: readlink "%{lib-dir}/libc++.dylib" | grep "libc++.1.dylib"
+// RUN: stat "%{lib-dir}/libc++.%{shared-suffix}"
+// RUN: readlink "%{lib-dir}/libc++.%{shared-suffix}" | grep "libc++.1.%{shared-suffix}"
