c25ae907874d8b5890617a831091d5b699604467
akielaries <akiel@akiel.org>
2024-07-13 09:04:57 -0700

    [libc][math] removing `FPTest` from CMake build and fixing 1st line comment

--- libc/test/include/isfinite_test.c
+++ libc/test/include/isfinite_test.c
@@ -1,12 +1,10 @@
-//===-- Unittests for isfinite macro
-//-----------------------------------------===//
+//===-- Unittests for isfinite macro --------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDSList-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
-
 #include "include/llvm-libc-macros/math-function-macros.h"
 
 #include <assert.h>
