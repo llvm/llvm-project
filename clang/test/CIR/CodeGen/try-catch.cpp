6f45aa4b88ad93613183d57ed8aa4fb3178cdcda
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2024-07-12 19:54:23 -0700

    [CIR][CIRGen] cir.try: handle trivial ones and fix crash
    OG codegen does not generate any exception related content when there are not
    calls happening inside the try block. For now we mimic OG and do the same,
    until we see a concrete use case that would have used emitting this code.

--- clang/test/CIR/CodeGen/try-catch.cpp
+++ clang/test/CIR/CodeGen/try-catch.cpp
@@ -85,3 +85,27 @@ unsigned long long tc3() {
 
   return z;
 }
+
+// CIR: cir.func @_Z3tc4v()
+unsigned long long tc4() {
+  int x = 50, y = 3;
+  unsigned long long z;
+
+  // CIR-NOT: cir.try
+  try {
+    int a = 4;
+    a++;
+
+    // CIR: cir.scope {
+    // CIR: cir.alloca !s32i, !cir.ptr<!s32i>, ["a", init]
+    // CIR-NOT: cir.alloca !cir.ptr<!cir.eh.info>
+    // CIR: cir.const #cir.int<4> : !s32i
+    // CIR: cir.unary(inc,
+    // CIR: cir.store %11, %8 : !s32i, !cir.ptr<!s32i>
+  } catch (int idx) {
+    z = 98;
+    idx++;
+  }
+
+  return z;
+}
