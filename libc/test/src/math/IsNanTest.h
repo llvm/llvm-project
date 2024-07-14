e4bb13d7c700a8d63ca32d3788d4c3527b9a8928
Petr Hosek <phosek@google.com>
2024-07-13 21:06:11 -0700

    Use `testing::FPTest<T>`

--- libc/test/src/math/IsNanTest.h
+++ libc/test/src/math/IsNanTest.h
@@ -6,48 +6,27 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "test/UnitTest/FEnvSafeTest.h"
 #include "test/UnitTest/FPMatcher.h"
 #include "test/UnitTest/Test.h"
 
 template <typename T>
-class IsNanTest : public LIBC_NAMESPACE::testing::FEnvSafeTest {
+class IsNanTest : public LIBC_NAMESPACE::testing::FPTest<T> {
 
   DECLARE_SPECIAL_CONSTANTS(T)
-
-  static constexpr T one = static_cast<T>(1.0);
-  static constexpr T neg_one = static_cast<T>(-1.0);
 
 public:
   typedef int (*IsNanFunc)(T);
 
   void testSpecialNumbers(IsNanFunc func) {
     EXPECT_EQ(func(zero), 0);
-    EXPECT_EQ(func(one), 0);
-    EXPECT_EQ(func(inf), 0);
-    EXPECT_EQ(func(aNaN), 1);
-
     EXPECT_EQ(func(neg_zero), 0);
-    EXPECT_EQ(func(neg_one), 0);
+    EXPECT_EQ(func(inf), 0);
     EXPECT_EQ(func(neg_inf), 0);
-    EXPECT_EQ(func(neg_aNaN), 1);
-  }
-
-  void testSpecialCases(IsNanFunc func) {
-    EXPECT_EQ(func(one / zero), 0);
-    EXPECT_EQ(func(one / inf), 0);
-    EXPECT_EQ(func(one / neg_inf), 0);
-    EXPECT_EQ(func(inf / neg_inf), 1);
-
-    EXPECT_EQ(func(inf * neg_inf), 0);
-    EXPECT_EQ(func(inf * zero), 1);
-    EXPECT_EQ(func(neg_inf * zero), 1);
-
-    EXPECT_EQ(func(inf + neg_inf), 1);
+    EXPECT_EQ(func(aNaN), 1);
+    EXPECT_EQ(func(sNaN), 1);
   }
 };
 
 #define LIST_ISNAN_TESTS(T, func)                                              \
   using LlvmLibcIsNanTest = IsNanTest<T>;                                      \
-  TEST_F(LlvmLibcIsNanTest, SpecialNumbers) { testSpecialNumbers(&func); }     \
-  TEST_F(LlvmLibcIsNanTest, SpecialCases) { testSpecialCases(&func); }
+  TEST_F(LlvmLibcIsNanTest, SpecialNumbers) { testSpecialNumbers(&func); }
