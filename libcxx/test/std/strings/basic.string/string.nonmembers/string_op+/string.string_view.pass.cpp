d214ef1055b366633ddb1945c9466d2603424d69
Hristo Hristov <hghristov.rmm@gmail.com>
2024-07-14 17:15:36 +0300

    Added ConvertibleToStringView test

--- libcxx/test/std/strings/basic.string/string.nonmembers/string_op+/string.string_view.pass.cpp
+++ libcxx/test/std/strings/basic.string/string.nonmembers/string_op+/string.string_view.pass.cpp
@@ -41,70 +41,62 @@
 #include "test_allocator.h"
 #include "test_macros.h"
 
-template <typename CharT, class Traits = std::char_traits<CharT>>
-class ConstConvertibleStringView {
+template <typename CharT, class TraitsT = std::char_traits<CharT>>
+class ConvertibleToStringView {
 public:
-  constexpr explicit ConstConvertibleStringView(const CharT* cs) : cs_{cs} {}
+  constexpr explicit ConvertibleToStringView(const CharT* cs) : cs_{cs} {}
+  // template <typename AllocT>
+  // constexpr explicit ConvertibleToStringView(std::basic_string<CharT, TraitsT, AllocT> str) : cs_{str.c_str()} {}
 
-  constexpr operator std::basic_string_view<CharT, Traits>() = delete;
-  constexpr operator std::basic_string_view<CharT, Traits>() const { return std::basic_string_view<CharT, Traits>(cs_); }
+  constexpr operator std::basic_string_view<CharT, TraitsT>() { return std::basic_string_view<CharT, TraitsT>(cs_); }
+  constexpr operator std::basic_string_view<CharT, TraitsT>() const {
+    return std::basic_string_view<CharT, TraitsT>(cs_);
+  }
 
 private:
   const CharT* cs_;
 };
 
-static_assert(!std::constructible_from<std::basic_string_view<char>, ConstConvertibleStringView<char>>);
-static_assert(!std::convertible_to<ConstConvertibleStringView<char>, std::basic_string_view<char>>);
+// static_assert(!std::constructible_from<std::basic_string_view<char>, ConvertibleToStringView<char>>);
+// static_assert(!std::convertible_to<ConvertibleToStringView<char>, std::basic_string_view<char>>);
 
-static_assert(std::constructible_from<std::basic_string_view<char>, const ConstConvertibleStringView<char>>);
-static_assert(std::convertible_to<const ConstConvertibleStringView<char>, std::basic_string_view<char>>);
+static_assert(std::constructible_from<std::basic_string_view<char>, const ConvertibleToStringView<char>>);
+static_assert(std::convertible_to<const ConvertibleToStringView<char>, std::basic_string_view<char>>);
 
-#ifndef TEST_HAS_NO_WIDE_CHARACTERS
-static_assert(!std::constructible_from<std::basic_string_view<wchar_t>, ConstConvertibleStringView<wchar_t>>);
-static_assert(!std::convertible_to<ConstConvertibleStringView<wchar_t>, std::basic_string_view<wchar_t>>);
+// #ifndef TEST_HAS_NO_WIDE_CHARACTERS
+// static_assert(!std::constructible_from<std::basic_string_view<wchar_t>, ConvertibleToStringView<wchar_t>>);
+// static_assert(!std::convertible_to<ConvertibleToStringView<wchar_t>, std::basic_string_view<wchar_t>>);
 
-static_assert(std::constructible_from<std::basic_string_view<wchar_t>, const ConstConvertibleStringView<wchar_t>>);
-static_assert(std::convertible_to<const ConstConvertibleStringView<wchar_t>, std::basic_string_view<wchar_t>>);
-#endif
+// static_assert(std::constructible_from<std::basic_string_view<wchar_t>, const ConvertibleToStringView<wchar_t>>);
+// static_assert(std::convertible_to<const ConvertibleToStringView<wchar_t>, std::basic_string_view<wchar_t>>);
+// #endif
 
-template <typename CharT, class Traits = std::char_traits<CharT>>
-class NonConstConvertibleStringView {
-public:
-  constexpr explicit NonConstConvertibleStringView(const CharT* cs) : cs_{cs} {}
+static_assert(std::constructible_from<std::basic_string_view<char>, ConvertibleToStringView<char>>);
+static_assert(std::convertible_to<ConvertibleToStringView<char>, std::basic_string_view<char>>);
 
-  constexpr operator std::basic_string_view<CharT, Traits>() { return std::basic_string_view<CharT, Traits>(cs_); }
-  constexpr operator std::basic_string_view<CharT, Traits>() const = delete;
+// static_assert(!std::constructible_from<std::basic_string_view<char>, const ConvertibleToStringView<char>>);
+// static_assert(!std::convertible_to<const ConvertibleToStringView<char>, std::basic_string_view<char>>);
 
-private:
-  const CharT* cs_;
-};
+// #ifndef TEST_HAS_NO_WIDE_CHARACTERS
+// static_assert(std::constructible_from<std::basic_string_view<wchar_t>, ConvertibleToStringView<wchar_t>>);
+// static_assert(std::convertible_to<ConvertibleToStringView<wchar_t>, std::basic_string_view<wchar_t>>);
 
-static_assert(std::constructible_from<std::basic_string_view<char>, NonConstConvertibleStringView<char>>);
-static_assert(std::convertible_to<NonConstConvertibleStringView<char>, std::basic_string_view<char>>);
-
-static_assert(!std::constructible_from<std::basic_string_view<char>, const NonConstConvertibleStringView<char>>);
-static_assert(!std::convertible_to<const NonConstConvertibleStringView<char>, std::basic_string_view<char>>);
-
-#ifndef TEST_HAS_NO_WIDE_CHARACTERS
-static_assert(std::constructible_from<std::basic_string_view<wchar_t>, NonConstConvertibleStringView<wchar_t>>);
-static_assert(std::convertible_to<NonConstConvertibleStringView<wchar_t>, std::basic_string_view<wchar_t>>);
-
-static_assert(!std::constructible_from<std::basic_string_view<wchar_t>, const NonConstConvertibleStringView<wchar_t>>);
-static_assert(!std::convertible_to<const NonConstConvertibleStringView<wchar_t>, std::basic_string_view<wchar_t>>);
-#endif
+// static_assert(!std::constructible_from<std::basic_string_view<wchar_t>, const ConvertibleToStringView<wchar_t>>);
+// static_assert(!std::convertible_to<const ConvertibleToStringView<wchar_t>, std::basic_string_view<wchar_t>>);
+// #endif
 
 #define CS(S) MAKE_CSTRING(CharT, S)
-#define ST(S, a) std::basic_string<CharT, TraitsT, AllocT>(MAKE_CSTRING(CharT, S), MKSTR_LEN(CharT, S), a)
-#define SV(S) std::basic_string_view<CharT, TraitsT>(MAKE_CSTRING(CharT, S), MKSTR_LEN(CharT, S))
+// #define ST(S, a) std::basic_string<CharT, TraitsT, AllocT>(MAKE_CSTRING(CharT, S), MKSTR_LEN(CharT, S), a)
+// #define SV(S) std::basic_string_view<CharT, TraitsT>(MAKE_CSTRING(CharT, S), MKSTR_LEN(CharT, S))
 
-template <typename CharT, typename TraitsT, typename AllocT>
+template <template <typename, typename> typename StringViewT, typename CharT, typename TraitsT, typename AllocT>
 constexpr void test(const CharT* x, const CharT* y, const CharT* expected) {
   AllocT allocator;
 
   // string& + string_view
   {
     std::basic_string<CharT, TraitsT, AllocT> st{x, allocator};
-    std::basic_string_view<CharT, TraitsT> sv{y};
+    StringViewT<CharT, TraitsT> sv{y};
 
     std::same_as<std::basic_string<CharT, TraitsT, AllocT>> decltype(auto) result = st + sv;
     assert(result == expected);
@@ -114,7 +106,7 @@ constexpr void test(const CharT* x, const CharT* y, const CharT* expected) {
   // const string& + string_view
   {
     const std::basic_string<CharT, TraitsT, AllocT> st{x, allocator};
-    std::basic_string_view<CharT, TraitsT> sv{y};
+    StringViewT<CharT, TraitsT> sv{y};
 
     std::same_as<std::basic_string<CharT, TraitsT, AllocT>> decltype(auto) result = st + sv;
     assert(result == expected);
@@ -124,7 +116,7 @@ constexpr void test(const CharT* x, const CharT* y, const CharT* expected) {
   // string&& + string_view
   {
     std::basic_string<CharT, TraitsT, AllocT> st{x, allocator};
-    std::basic_string_view<CharT, TraitsT> sv{y};
+    StringViewT<CharT, TraitsT> sv{y};
 
     std::same_as<std::basic_string<CharT, TraitsT, AllocT>> decltype(auto) result = std::move(st) + sv;
     assert(result == expected);
@@ -133,7 +125,7 @@ constexpr void test(const CharT* x, const CharT* y, const CharT* expected) {
   }
   // string_view + string&
   {
-    std::basic_string_view<CharT, TraitsT> sv{x};
+    StringViewT<CharT, TraitsT> sv{x};
     std::basic_string<CharT, TraitsT, AllocT> st{y, allocator};
 
     std::same_as<std::basic_string<CharT, TraitsT, AllocT>> decltype(auto) result = sv + st;
@@ -143,7 +135,7 @@ constexpr void test(const CharT* x, const CharT* y, const CharT* expected) {
   }
   // string_view + const string&
   {
-    std::basic_string_view<CharT, TraitsT> sv{x};
+    StringViewT<CharT, TraitsT> sv{x};
     const std::basic_string<CharT, TraitsT, AllocT> st{y, allocator};
 
     std::same_as<std::basic_string<CharT, TraitsT, AllocT>> decltype(auto) result = sv + st;
@@ -153,154 +145,105 @@ constexpr void test(const CharT* x, const CharT* y, const CharT* expected) {
   }
   // string_view + string&&
   {
-    // Create a `basic_string` to workaround clang bug:
+    // TODO: Create a `basic_string` to workaround clang bug:
     // https://github.com/llvm/llvm-project/issues/92382
     // Comparison between pointers to a string literal and some other object results in constant evaluation failure.
-    std::basic_string<CharT, TraitsT, AllocT> st_{x, allocator};
-    std::basic_string_view<CharT, TraitsT> sv{st_};
-    std::basic_string<CharT, TraitsT, AllocT> st{y, allocator};
+    if constexpr (std::same_as<StringViewT<CharT, TraitsT>, std::basic_string_view<CharT, TraitsT>>) {
+      std::basic_string<CharT, TraitsT, AllocT> st_{x, allocator};
+      StringViewT<CharT, TraitsT> sv{st_};
+      // StringViewT<CharT, TraitsT> sv = [&] {
+      //   if constexpr (std::same_as<StringViewT<CharT, TraitsT>, std::basic_string_view<CharT, TraitsT>>) {
+      //     std::basic_string<CharT, TraitsT, AllocT> st{x, allocator};
+      //     return std::basic_string_view<CharT, TraitsT>{st};
+      //   } else {
+      //     return StringViewT<CharT, TraitsT>{x};
+      //   }
+      // }();
+      std::basic_string<CharT, TraitsT, AllocT> st{y, allocator};
 
-    std::same_as<std::basic_string<CharT, TraitsT, AllocT>> decltype(auto) result = sv + std::move(st);
-    assert(result == expected);
-    assert(result.get_allocator() == allocator);
-    LIBCPP_ASSERT(is_string_asan_correct(sv + std::move(st)));
-  }
-
-  // string& + nonconst_convertible_to_string_view
-  {
-    std::basic_string<CharT, TraitsT, AllocT> st{x, allocator};
-    NonConstConvertibleStringView<CharT, TraitsT> sv{y};
-
-    std::same_as<std::basic_string<CharT, TraitsT, AllocT>> decltype(auto) result = st + sv;
-    assert(result == expected);
-    assert(result.get_allocator() == allocator);
-    LIBCPP_ASSERT(is_string_asan_correct(st + sv));
+      std::same_as<std::basic_string<CharT, TraitsT, AllocT>> decltype(auto) result = sv + std::move(st);
+      assert(result == expected);
+      assert(result.get_allocator() == allocator);
+      LIBCPP_ASSERT(is_string_asan_correct(sv + std::move(st)));
+    }
   }
 }
 
-template <typename CharT, typename TraitsT, typename AllocT = std::allocator<CharT>>
+template <template <typename, typename> typename StringViewT,
+          typename CharT,
+          typename TraitsT,
+          typename AllocT = std::allocator<CharT>>
 constexpr void test() {
   // Concatenate with an empty `string`/`string_view`
-  test<CharT, TraitsT, AllocT>(CS(""), CS(""), CS(""));
-  test<CharT, TraitsT, AllocT>(CS(""), CS("short"), CS("short"));
-  test<CharT, TraitsT, AllocT>(CS(""), CS("not so short"), CS("not so short"));
-  test<CharT, TraitsT, AllocT>(CS(""), CS("this is a much longer string"), CS("this is a much longer string"));
+  test<StringViewT, CharT, TraitsT, AllocT>(CS(""), CS(""), CS(""));
+  test<StringViewT, CharT, TraitsT, AllocT>(CS(""), CS("short"), CS("short"));
+  test<StringViewT, CharT, TraitsT, AllocT>(CS(""), CS("not so short"), CS("not so short"));
+  test<StringViewT, CharT, TraitsT, AllocT>(
+      CS(""), CS("this is a much longer string"), CS("this is a much longer string"));
 
-  test<CharT, TraitsT, AllocT>(CS(""), CS(""), CS(""));
-  test<CharT, TraitsT, AllocT>(CS("short"), CS(""), CS("short"));
-  test<CharT, TraitsT, AllocT>(CS("not so short"), CS(""), CS("not so short"));
-  test<CharT, TraitsT, AllocT>(CS("this is a much longer string"), CS(""), CS("this is a much longer string"));
+  test<StringViewT, CharT, TraitsT, AllocT>(CS(""), CS(""), CS(""));
+  test<StringViewT, CharT, TraitsT, AllocT>(CS("short"), CS(""), CS("short"));
+  test<StringViewT, CharT, TraitsT, AllocT>(CS("not so short"), CS(""), CS("not so short"));
+  test<StringViewT, CharT, TraitsT, AllocT>(
+      CS("this is a much longer string"), CS(""), CS("this is a much longer string"));
 
   // Non empty
-  test<CharT, TraitsT, AllocT>(CS("B"), CS("D"), CS("BD"));
-  test<CharT, TraitsT, AllocT>(CS("zmt94"), CS("+hkt82"), CS("zmt94+hkt82"));
-  test<CharT, TraitsT, AllocT>(CS("not so short"), CS("+is not bad"), CS("not so short+is not bad"));
-  test<CharT, TraitsT, AllocT>(
+  test<StringViewT, CharT, TraitsT, AllocT>(CS("B"), CS("D"), CS("BD"));
+  test<StringViewT, CharT, TraitsT, AllocT>(CS("zmt94"), CS("+hkt82"), CS("zmt94+hkt82"));
+  test<StringViewT, CharT, TraitsT, AllocT>(CS("not so short"), CS("+is not bad"), CS("not so short+is not bad"));
+  test<StringViewT, CharT, TraitsT, AllocT>(
       CS("this is a much longer string"),
       CS("+which is so much better"),
       CS("this is a much longer string+which is so much better"));
 }
 
-template <typename CharT>
+template <template <typename, typename> typename StringViewT, typename CharT>
 constexpr bool test() {
-  test<CharT, std::char_traits<CharT>>();
-  test<CharT, std::char_traits<CharT>, min_allocator<CharT>>();
-  test<CharT, std::char_traits<CharT>, safe_allocator<CharT>>();
-  test<CharT, std::char_traits<CharT>, test_allocator<CharT>>();
+  test<StringViewT, CharT, std::char_traits<CharT>>();
+  test<StringViewT, CharT, std::char_traits<CharT>, min_allocator<CharT>>();
+  test<StringViewT, CharT, std::char_traits<CharT>, safe_allocator<CharT>>();
+  test<StringViewT, CharT, std::char_traits<CharT>, test_allocator<CharT>>();
 
-  test<CharT, constexpr_char_traits<CharT>>();
-  test<CharT, constexpr_char_traits<CharT>, min_allocator<CharT>>();
-  test<CharT, constexpr_char_traits<CharT>, safe_allocator<CharT>>();
-  test<CharT, constexpr_char_traits<CharT>, test_allocator<CharT>>();
+  test<StringViewT, CharT, constexpr_char_traits<CharT>>();
+  test<StringViewT, CharT, constexpr_char_traits<CharT>, min_allocator<CharT>>();
+  test<StringViewT, CharT, constexpr_char_traits<CharT>, safe_allocator<CharT>>();
+  test<StringViewT, CharT, constexpr_char_traits<CharT>, test_allocator<CharT>>();
 
   return true;
 }
 
-template<typename CharT>
-constexpr bool test_const_convertible() {
-
-  // string& + const_convertible_to_string_view
-  {
-    std::basic_string<CharT, TraitsT, AllocT> st{x, allocator};
-    ConstConvertibleStringView<CharT, TraitsT> sv{y};
-
-    std::same_as<std::basic_string<CharT, TraitsT, AllocT>> decltype(auto) result = st + std::as_const(sv);
-    assert(result == expected);
-    assert(result.get_allocator() == allocator);
-    LIBCPP_ASSERT(is_string_asan_correct(st + std::as_const(sv)));
-  }
-    // const string& + const_convertible_to_string_view
-  {
-    const std::basic_string<CharT, TraitsT, AllocT> st{x, allocator};
-    ConstConvertibleStringView<CharT, TraitsT> sv{y};
-
-    std::same_as<std::basic_string<CharT, TraitsT, AllocT>> decltype(auto) result = st + std::as_const(sv);
-    assert(result == expected);
-    assert(result.get_allocator() == allocator);
-    LIBCPP_ASSERT(is_string_asan_correct(st + std::as_const(sv)));
-  }
-  // string&& + const_convertible_to_string_view
-  {
-    std::basic_string<CharT, TraitsT, AllocT> st{x, allocator};
-    ConstConvertibleStringView<CharT, TraitsT> sv{y};
-
-    std::same_as<std::basic_string<CharT, TraitsT, AllocT>> decltype(auto) result = std::move(st) + std::as_const(sv);
-    assert(result == expected);
-    assert(result.get_allocator() == allocator);
-    LIBCPP_ASSERT(is_string_asan_correct(std::move(st) + std::as_const(sv)));
-  }
-  // const_convertible_to_string_view + string&
-  {
-    ConstConvertibleStringView<CharT, TraitsT> sv{x};
-    std::basic_string<CharT, TraitsT, AllocT> st{y, allocator};
-
-    std::same_as<std::basic_string<CharT, TraitsT, AllocT>> decltype(auto) result = std::as_const(sv) + st;
-    assert(result == expected);
-    assert(result.get_allocator() == allocator);
-    LIBCPP_ASSERT(is_string_asan_correct(std::as_const(sv) + st));
-  }
-  // const_convertible_to_string_view + const string&
-  {
-    ConstConvertibleStringView<CharT, TraitsT> sv{x};
-    const std::basic_string<CharT, TraitsT, AllocT> st{y, allocator};
-
-    std::same_as<std::basic_string<CharT, TraitsT, AllocT>> decltype(auto) result = std::as_const(sv) + st;
-    assert(result == expected);
-    assert(result.get_allocator() == allocator);
-    LIBCPP_ASSERT(is_string_asan_correct(std::as_const(sv) + st));
-  }
-  // const_convertible_to_string_view + string&&
-  {
-    // // Create a `basic_string` to workaround clang bug:
-    // // https://github.com/llvm/llvm-project/issues/92382
-    // // Comparison between pointers to a string literal and some other object results in constant evaluation failure.
-    std::basic_string<CharT, TraitsT, AllocT> st_{x, allocator};
-    ConstConvertibleStringView<CharT, TraitsT> sv{x};
-    std::basic_string<CharT, TraitsT, AllocT> st{y, allocator};
-
-    std::same_as<std::basic_string<CharT, TraitsT, AllocT>> decltype(auto) result = std::as_const(sv) + std::move(st);
-    assert(result == expected);
-    assert(result.get_allocator() == allocator);
-    LIBCPP_ASSERT(is_string_asan_correct(std::as_const(sv) + std::move(st)));
-  }
-
-}
-
 int main(int, char**) {
-  test<char>();
-  static_assert(test<char>());
-// #ifndef TEST_HAS_NO_WIDE_CHARACTERS
-//   test<wchar_t>();
-//   static_assert(test<wchar_t>());
-// #endif
-// #ifndef TEST_HAS_NO_CHAR8_T
-//   test<char8_t>();
-//   static_assert(test<char8_t>());
-// #endif
-  // test<char16_t>();
-  // static_assert(test<char16_t>());
-//   test<char32_t>();
-//   static_assert(test<char32_t>());
+  // std::basic_string_view
+  test<std::basic_string_view, char>();
+  static_assert(test<std::basic_string_view, char>());
+#ifndef TEST_HAS_NO_WIDE_CHARACTERS
+  test<std::basic_string_view, wchar_t>();
+  static_assert(test<std::basic_string_view, wchar_t>());
+#endif
+#ifndef TEST_HAS_NO_CHAR8_T
+  test<std::basic_string_view, char8_t>();
+  static_assert(test<std::basic_string_view, char8_t>());
+#endif
+  test<std::basic_string_view, char16_t>();
+  static_assert(test<std::basic_string_view, char16_t>());
+  test<std::basic_string_view, char32_t>();
+  static_assert(test<std::basic_string_view, char32_t>());
+
+  // ConvertibleToStringView
+  test<ConvertibleToStringView, char>();
+  static_assert(test<ConvertibleToStringView, char>());
+  #ifndef TEST_HAS_NO_WIDE_CHARACTERS
+    test<ConvertibleToStringView, wchar_t>();
+    static_assert(test<ConvertibleToStringView, wchar_t>());
+  #endif
+  #ifndef TEST_HAS_NO_CHAR8_T
+    test<ConvertibleToStringView, char8_t>();
+    static_assert(test<ConvertibleToStringView, char8_t>());
+  #endif
+    test<ConvertibleToStringView, char16_t>();
+    static_assert(test<ConvertibleToStringView, char16_t>());
+    test<ConvertibleToStringView, char32_t>();
+    static_assert(test<ConvertibleToStringView, char32_t>());
 
   return 0;
 }
