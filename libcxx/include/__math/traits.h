9df4a55da66dea7c875530fc11c25e648426cfb5
Robin Caloudis <robin.caloudis@gmx.de>
2024-07-15 00:10:17 +0200

    Provide long double overload

--- libcxx/include/__math/traits.h
+++ libcxx/include/__math/traits.h
@@ -64,6 +64,10 @@ _LIBCPP_NODISCARD inline _LIBCPP_CONSTEXPR_SINCE_CXX23 _LIBCPP_HIDE_FROM_ABI _LI
 isfinite(double __x) _NOEXCEPT {
   return __builtin_isfinite(__x);
 }
+
+_LIBCPP_NODISCARD inline _LIBCPP_CONSTEXPR_SINCE_CXX23 _LIBCPP_HIDE_FROM_ABI bool isfinite(long double __x) _NOEXCEPT {
+  return __builtin_isfinite(__x);
+}
 #endif
 // isinf
 
