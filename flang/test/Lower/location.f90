fa36cb770a56234fc9c5909e410d9b9bba991b0b
Valentin Clement <clementval@gmail.com>
2024-07-15 09:08:17 -0700

    Fix test

--- flang/test/Lower/location.f90
+++ flang/test/Lower/location.f90
@@ -6,6 +6,8 @@ include 'location0.inc'
 end 
 
 ! CHECK-LABEL: func.func @_QQmain() attributes {fir.bindc_name = "test"} {
-! CHECK: fir.call @_FortranAioOutputAscii(%{{.*}}, %{{.*}}, %{{.*}}) fastmath<contract> : (!fir.ref<i8>, !fir.ref<i8>, i64) -> i1 loc(fused<#fir<loc_kind_array[ base,  inclusion,  inclusion]>>["{{.*}}location1.inc":1:8, "{{.*}}location0.inc":1:1, "{{.*}}location.f90":4:1])
+! CHECK: fir.call @_FortranAioOutputAscii(%2, %5, %6) fastmath<contract> : (!fir.ref<i8>, !fir.ref<i8>, i64) -> i1 loc(fused<#fir<loc_kind_array[ base,  inclusion,  inclusion]>>["{{.*}}flang/test/Lower/location1.inc":1:10, "{{.*}}flang/test/Lower/location0.inc":1:1, "{{.*}}flang/test/Lower/location.f90":4:1])
 ! CHECK: return loc("{{.*}}flang/test/Lower/location.f90":6:1)
 ! CHECK: } loc("{{.*}}flang/test/Lower/location.f90":3:1)
+
+
