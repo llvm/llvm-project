607a43381a39b4a6e18a247e7a1ab9ca87690bd5
Dmitry Chestnykh <dm.chestnykh@gmail.com>
2024-07-15 09:35:40 +0300

    [Clang][Driver] Refactor test
    - Rename to warn-fsyntax-only.c
    - Add `-fsyntax-only -E` testcase
    - Remove `-c -S` case because we really don't need it

--- clang/test/Driver/warn-fsyntax-only.c
+++ clang/test/Driver/warn-fsyntax-only.c
@@ -1,9 +1,8 @@
+// RUN: %clang -fsyntax-only -E %s 2>&1 | FileCheck %s --check-prefix=CHECK-PP
 // RUN: %clang -fsyntax-only -S %s 2>&1 | FileCheck %s --check-prefix=CHECK-ASM
 // RUN: %clang -fsyntax-only -c %s 2>&1 | FileCheck %s --check-prefix=CHECK-OBJ
-// RUN: %clang -fsyntax-only -S -c %s 2>&1 | FileCheck %s --check-prefix=CHECK-BOTH
 
+// CHECK-PP:  warning: argument unused during compilation: '-fsyntax-only' [-Wunused-command-line-argument]
 // CHECK-ASM: warning: argument unused during compilation: '-S' [-Wunused-command-line-argument]
 // CHECK-OBJ: warning: argument unused during compilation: '-c' [-Wunused-command-line-argument]
 
-// CHECK-BOTH: warning: argument unused during compilation: '-S' [-Wunused-command-line-argument]
-// CHECK-NEXT: warning: argument unused during compilation: '-c' [-Wunused-command-line-argument]
