ab267cc31f01dcc088c4839a4fadd3eca92b902f
Jeremy Morse <jeremy.morse@sony.com>
2024-07-16 14:23:43 +0100

    Switch back to using SmallPtrSets in IDF calculator

--- llvm/include/llvm/Support/GenericIteratedDominanceFrontier.h
+++ llvm/include/llvm/Support/GenericIteratedDominanceFrontier.h
@@ -144,8 +144,8 @@ void IDFCalculatorBase<NodeTy, IsPostDom>::calculate(
   DT.updateDFSNumbers();
 
   SmallVector<DomTreeNodeBase<NodeTy> *, 32> Worklist;
-  SmallDenseSet<DomTreeNodeBase<NodeTy> *, 16> VisitedPQ;
-  SmallDenseSet<DomTreeNodeBase<NodeTy> *, 16> VisitedWorklist;
+  SmallPtrSet<DomTreeNodeBase<NodeTy> *, 16> VisitedPQ;
+  SmallPtrSet<DomTreeNodeBase<NodeTy> *, 16> VisitedWorklist;
   if (useLiveIn) {
     VisitedPQ.reserve(LiveInBlocks->size());
     VisitedWorklist.reserve(LiveInBlocks->size());
