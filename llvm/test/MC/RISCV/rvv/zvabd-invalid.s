1b1af0a4711a9ceaf1932139ecd7a402d3a4be29
Wang Pengcheng <wangpengcheng.pp@bytedance.com>
2024-07-16 16:08:16 +0800

    [RISCV][MC] Support Zvabd instructions
    Support of these instructions are added:
    
    - Vector Single-Width Signed/Unsigned Integer Absolute Difference
    - Vector Widening Signed/Unsigned Integer Absolute Difference and
      Accumulate
    
    Doc: https://riscv.atlassian.net/wiki/spaces/VXXX/pages/166690866/Fast-Track+Proposal+for+integer+vector+absolute+difference+instructions

---
+++ llvm/test/MC/RISCV/rvv/zvabd-invalid.s
@@ -0,0 +1,10 @@
+# RUN: not llvm-mc -triple=riscv64 --mattr=+zve64x --mattr=+experimental-zvabd %s 2>&1 \
+# RUN:        | FileCheck %s --check-prefix=CHECK-ERROR
+
+vwabdacc.vv v9, v9, v8
+# CHECK-ERROR: [[@LINE-1]]:13: error: the destination vector register group cannot overlap the source vector register group
+# CHECK-ERROR-LABEL: vwabdacc.vv v9, v9, v8
+
+vwabdaccu.vv v9, v9, v8
+# CHECK-ERROR: [[@LINE-1]]:14: error: the destination vector register group cannot overlap the source vector register group
+# CHECK-ERROR-LABEL: vwabdaccu.vv v9, v9, v8
