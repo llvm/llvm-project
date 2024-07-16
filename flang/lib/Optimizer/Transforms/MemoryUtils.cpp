3a17f15ba43692ccbe2173413525071fce13b67a
Jean Perier <jperier@nvidia.com>
2024-07-16 08:07:21 -0700

    handle block cycles properly

--- flang/lib/Optimizer/Transforms/MemoryUtils.cpp
+++ flang/lib/Optimizer/Transforms/MemoryUtils.cpp
@@ -14,6 +14,28 @@
 #include "llvm/ADT/STLExtras.h"
 
 namespace {
+/// Helper class to detect if an alloca is inside an mlir::Block that can be
+/// reached again before its deallocation points via block successors. This
+/// analysis is only valid if the deallocation points are inside (or nested
+/// inside) the same region as alloca because it does not consider region CFG
+/// (for instance, the block inside a fir.do_loop is obviously inside a loop,
+/// but is not a loop formed by blocks). The dominance of the alloca on its
+/// deallocation points implies this pre-condition (although it is more
+/// restrictive).
+class BlockCycleDetector {
+public:
+  bool allocaIsInCycle(fir::AllocaOp alloca,
+                       llvm::ArrayRef<mlir::Operation *> deallocationPoints);
+
+private:
+  // Cache for blocks owning alloca that have been analyzed. In many Fortran
+  // programs, allocas are usually made in the same blocks with no block cycles.
+  // So getting a fast "no" is beneficial.
+  llvm::DenseMap<mlir::Block *, /*isInCycle*/ bool> analyzed;
+};
+} // namespace
+
+namespace {
 class AllocaReplaceImpl {
 public:
   AllocaReplaceImpl(fir::AllocaRewriterCallBack allocaRewriter,
@@ -42,8 +64,54 @@ private:
   fir::AllocaRewriterCallBack allocaRewriter;
   fir::DeallocCallBack deallocGenerator;
   mlir::DominanceInfo dominanceInfo;
+  BlockCycleDetector blockCycleDetector;
 };
 } // namespace
+
+static bool
+allocaIsInCycleImpl(mlir::Block *allocaBlock,
+                    llvm::ArrayRef<mlir::Operation *> deallocationPoints) {
+  llvm::DenseSet<mlir::Block *> seen;
+  // Insert the deallocation point blocks as "seen" so that the block
+  // traversal will stop at them.
+  for (mlir::Operation *deallocPoint : deallocationPoints)
+    seen.insert(deallocPoint->getBlock());
+  if (seen.contains(allocaBlock))
+    return false;
+  // Traverse the block successor graph starting by the alloca block.
+  llvm::SmallVector<mlir::Block *> successors{allocaBlock};
+  while (!successors.empty())
+    for (mlir::Block *next : successors.pop_back_val()->getSuccessors()) {
+      if (next == allocaBlock)
+        return true;
+      if (auto pair = seen.insert(next); pair.second)
+        successors.push_back(next);
+    }
+  // The traversal did not reach the alloca block again.
+  return false;
+}
+bool BlockCycleDetector::allocaIsInCycle(
+    fir::AllocaOp alloca,
+    llvm::ArrayRef<mlir::Operation *> deallocationPoints) {
+  mlir::Block *allocaBlock = alloca->getBlock();
+  auto analyzedPair = analyzed.try_emplace(allocaBlock, /*isInCycle=*/false);
+  bool alreadyAnalyzed = !analyzedPair.second;
+  bool &isInCycle = analyzedPair.first->second;
+  // Fast exit if block was already analyzed and no cycle was found.
+  if (alreadyAnalyzed && !isInCycle)
+    return false;
+  // If the analysis was not done generically for this block, run it and
+  // save the result.
+  if (!alreadyAnalyzed)
+    isInCycle = allocaIsInCycleImpl(allocaBlock, /*deallocationPoints*/ {});
+  if (!isInCycle)
+    return false;
+  // If the generic analysis found a block loop, see if the deallocation
+  // point would be reached before reaching the block again. Do not
+  // cache that analysis that is specific to the deallocation points
+  // found for this alloca.
+  return allocaIsInCycleImpl(allocaBlock, deallocationPoints);
+}
 
 static bool terminatorYieldsMemory(mlir::Operation &terminator) {
   return llvm::any_of(terminator.getResults(), [](mlir::OpResult res) {
@@ -102,11 +170,12 @@ mlir::Region *AllocaReplaceImpl::findDeallocationPointsAndOwner(
   // Step 3: detect block based loops between the allocation and deallocation
   // points, and add a deallocation point on the back edge to avoid memory
   // leaks.
-  // To avoid complex CFG analysis, the detection uses the fact an alloca
-  // inside a loop does not dominate its deallocation points after the loop. So
-  // dominance can be used as a simple detection method with false positives
-  // (there will be false positives for alloca inside block based if-then-else
-  // for instance).
+  // The detection avoids doing region CFG analysis by assuming that there may
+  // be cycles if deallocation points are not dominated by the alloca.
+  // This leaves the cases where the deallocation points are in the same region
+  // as the alloca (or nested inside it). In which cases there may be a back
+  // edge between the alloca and the deallocation point via block successors. An
+  // analysis is run to detect those cases.
   // When a loop is detected, the easiest solution to deallocate on the back
   // edge is to store the allocated memory address in a variable (that dominates
   // the loops) and to deallocate the address in that variable if it is set
@@ -115,7 +184,8 @@ mlir::Region *AllocaReplaceImpl::findDeallocationPointsAndOwner(
   // Hence, the alloca is added as a deallocation point when there is no
   // dominance. Note that bringing lifetime markers above will reduce the
   // false positives.
-  if (!allocDominatesDealloc(alloca, deallocationPoints))
+  if (!allocDominatesDealloc(alloca, deallocationPoints) ||
+      blockCycleDetector.allocaIsInCycle(alloca, deallocationPoints))
     deallocationPoints.push_back(alloca.getOperation());
   return owningRegion;
 }
