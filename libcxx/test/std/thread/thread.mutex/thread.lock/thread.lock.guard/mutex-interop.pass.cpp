09c8e0c6eafd724b8311f97c2fabf29e9b1eac0c
Louis Dionne <ldionne.2@gmail.com>
2024-07-16 16:00:55 -0400

    Fix other ref issue

--- libcxx/test/std/thread/thread.mutex/thread.lock/thread.lock.guard/mutex-interop.pass.cpp
+++ libcxx/test/std/thread/thread.mutex/thread.lock/thread.lock.guard/mutex-interop.pass.cpp
@@ -14,6 +14,7 @@
 #include <cassert>
 #include <mutex>
 #include <type_traits>
+#include <functional>
 
 #include "make_test_thread.h"
 #include "test_macros.h"
@@ -23,18 +24,21 @@ void do_try_lock(std::mutex& m) { assert(m.try_lock() == false); }
 int main(int, char**) {
   {
     std::mutex m;
-    std::lock_guard<std::mutex> lg(m);
-    std::thread t = support::make_test_thread(do_try_lock, m);
-    t.join();
-  }
+    {
+      std::lock_guard<std::mutex> lg(m);
+      std::thread t = support::make_test_thread(do_try_lock, std::ref(m));
+      t.join();
+    }
 
-  // This should work because the lock_guard unlocked the mutex when it was destroyed above.
-  m.lock();
-  m.unlock();
+    // This should work because the lock_guard unlocked the mutex when it was destroyed above.
+    m.lock();
+    m.unlock();
+  }
 
   // Test CTAD
 #if TEST_STD_VER >= 17
   {
+    std::mutex m;
     std::lock_guard lg(m);
     static_assert(std::is_same<decltype(lg), std::lock_guard<std::mutex>>::value, "");
   }
