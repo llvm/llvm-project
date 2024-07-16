8d56623d29163c49442851f5c9a3af1adf2360c8
Robin Caloudis <robin.caloudis@gmx.de>
2024-07-16 20:41:02 +0200

    Extract template and leave comment

--- libcxx/test/std/numerics/c.math/isfinite.pass.cpp
+++ libcxx/test/std/numerics/c.math/isfinite.pass.cpp
@@ -62,30 +62,21 @@ struct TestInt {
   }
 };
 
-struct ConvertibleFloat {
-  int value;
-  ConvertibleFloat(int v) : value(v) {}
-  operator float() const { return static_cast<float>(value); }
-};
-
-struct ConvertibleDouble {
-  int value;
-  ConvertibleDouble(int v) : value(v) {}
-  operator double() const { return static_cast<double>(value); }
-};
-
-struct ConvertibleLongDouble {
-  int value;
-  ConvertibleLongDouble(int v) : value(v) {}
-  operator long double() const { return static_cast<long double>(value); }
+template <typename T>
+struct ConvertibleTo {
+  operator T() const { return T(); }
 };
 
 int main(int, char**) {
   types::for_each(types::floating_point_types(), TestFloat());
   types::for_each(types::integral_types(), TestInt());
-  assert(std::isfinite(ConvertibleFloat(0)));
-  assert(std::isfinite(ConvertibleDouble(0)));
-  assert(std::isfinite(ConvertibleLongDouble(0)));
+
+  // Make sure we can call `std::isfinite` with convertible types
+  {
+    assert(std::isfinite(ConvertibleTo<float>()));
+    assert(std::isfinite(ConvertibleTo<double>()));
+    assert(std::isfinite(ConvertibleTo<long double>()));
+  }
 
   return 0;
 }
