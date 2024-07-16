38116359101ccdadbc169f4c897b08eb253bceac
cor3ntin <corentinjabot@gmail.com>
2024-07-16 11:00:27 +0200

    Revert "[Clang][C++26] Implement "Ordering of constraints involving fold expressions" (#99007)
    Reverts llvm/llvm-project#98160
    
    Breaks CI on some architectures

--- clang/include/clang/Sema/Sema.h
+++ clang/include/clang/Sema/Sema.h
@@ -14078,11 +14078,6 @@ public:
       const DeclarationNameInfo &NameInfo,
       SmallVectorImpl<UnexpandedParameterPack> &Unexpanded);
 
-  /// Collect the set of unexpanded parameter packs within the given
-  /// expression.
-  static void collectUnexpandedParameterPacks(
-      Expr *E, SmallVectorImpl<UnexpandedParameterPack> &Unexpanded);
-
   /// Invoked when parsing a template argument followed by an
   /// ellipsis, which creates a pack expansion.
   ///
