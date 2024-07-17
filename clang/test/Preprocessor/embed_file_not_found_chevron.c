178c66c9c4c68485931cf2d60063dd97f7858309
Podchishchaeva, Mariya <mariya.podchishchaeva@intel.com>
2024-07-17 07:39:02 -0700

    [clang] Fix static analyzer concerns in #embed code
    1. Dead code int LookupEmbedFile. The loop always exited on the first
       iteration. This was also causing a bug of not checking all
       directories provided by --embed-dir
    
    2. Use of uninitialized variable CurTok LexEmbedParameters. It was used
       to initialize the field which seems to be unused. Removed unused
       field, this way CurTok should be initialized by Lex method.

--- clang/test/Preprocessor/embed_file_not_found_chevron.c
+++ clang/test/Preprocessor/embed_file_not_found_chevron.c
@@ -1,4 +1,5 @@
-// RUN: %clang_cc1 -std=c23 %s -E -verify
+// RUN: %clang_cc1 -std=c23 %s -E -verify --embed-dir=%S --embed-dir=%S/Inputs
 
+#embed <jk.txt>
 #embed <nfejfNejAKFe>
 // expected-error@-1 {{'nfejfNejAKFe' file not found}}
