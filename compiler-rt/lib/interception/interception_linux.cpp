916d9323ada65f55538df5db70c68ecf87916ca4
Rainer Orth <ro@gcc.gnu.org>
2024-07-17 10:08:44 +0200

    [safestack] Various Solaris fixes
    Even with the `-u __safestack_init` link order fixed on Solaris, there
    are still several safestack test issues left:
    
    - While 540fd42c755f20f7b79c6c79493ec36d8cb9b3d3 enabled safestack on
      Solaris in the driver unconditionally, it ignored that Solaris also
      exists on SPARC and forgot to enable SPARC support for the runtime lib.
      This patch fixes that.
    
    - The tests fail to link with undefined references to
      `__sanitizer_internal_memset` etc in `safestack.cpp.o` and
      `interception_linux.cpp.o`.  These are from indirectly including
      `sanitizer_redefine_builtins.h`.  Instead of using the implementations
      from `sanitizer_common` as was done in [[safestack] Various Solaris
      fixes](https://github.com/llvm/llvm-project/pull/98469), this patch
      disables the interception as discussed in [Revert "[safestack] Various
      Solaris fixes"](https://github.com/llvm/llvm-project/pull/98541).  A
      similar issue affects 32-bit Linux/sparc where compiling `safestack.cpp`
      with `-ftrivial-auto-var-init=pattern` causes the compiler to generate
      calls to `memset` to initialize a `pthread_attr_t` which is larger than
      can be handled inline.  Both cases are avoided by defining
      `SANITIZER_COMMON_NO_REDEFINE_BUILTINS` in the affected sources.
    
    - The `pthread*.c` tests `FAIL` with
    
      ``` safestack CHECK failed:
      /vol/llvm/src/llvm-project/local/compiler-rt/lib/safestack/safestack.cpp:227 size
      ```
    
      The problem is that `pthread_attr_init` initializes the `stacksize`
      attribute to 0, signifying the default.  Unless explicitly overridded, it
      stays that way.  I think this is allowed by XPG7.  Since safestack cannot
      deal with this, I set `size` to the defaults documented in
      `pthread_create(3C)`.  Unfortunately, there's no macro for those values
      outside of private `libc` headers.
    
    - The Solaris `syscall` interface isn't stable.  This is not just a
      theoretical concern, but the syscalls have changed incompatibly several
      times in the past.  Therefore this patch switches the implementations of
      `TgKill` (where `SYS_lwp_kill` doesn't exist on Solaris 11.4 anyway),
      `Mmap`, `Munmap`, and `Mprotect` to the same `_REAL*` solution already
      used in `sanitizer_solaris.cpp`.
    
    With those changes, safestack compiles and all tests `PASS`, so the
    tests are re-enabled for good.
    
    Tested on `amd64-pc-solaris2.11`, `sparcv9-sun-solaris2.11`,
    `x86_64-pc-linux-gnu`, and `sparc64-unknown-linux-gnu`.

--- compiler-rt/lib/interception/interception_linux.cpp
+++ compiler-rt/lib/interception/interception_linux.cpp
@@ -11,6 +11,8 @@
 // Linux-specific interception methods.
 //===----------------------------------------------------------------------===//
 
+#define SANITIZER_COMMON_NO_REDEFINE_BUILTINS
+
 #include "interception.h"
 
 #if SANITIZER_LINUX || SANITIZER_FREEBSD || SANITIZER_NETBSD || \
--- compiler-rt/lib/safestack/safestack.cpp
+++ compiler-rt/lib/safestack/safestack.cpp
@@ -13,6 +13,8 @@
 //
 //===----------------------------------------------------------------------===//
 
+#define SANITIZER_COMMON_NO_REDEFINE_BUILTINS
+
 #include "safestack_platform.h"
 #include "safestack_util.h"
 
@@ -223,6 +225,17 @@ INTERCEPTOR(int, pthread_create, pthread_t *thread,
     pthread_attr_getguardsize(&tmpattr, &guard);
     pthread_attr_destroy(&tmpattr);
   }
+
+#if SANITIZER_SOLARIS
+  // Solaris pthread_attr_init initializes stacksize to 0 (the default), so
+  // hardcode the actual values as documented in pthread_create(3C).
+  if (size == 0)
+#  if defined(_LP64)
+    size = 2 * 1024 * 1024;
+#  else
+    size = 1024 * 1024;
+#  endif
+#endif
 
   SFS_CHECK(size);
   size = RoundUpTo(size, kStackAlign);
