d2d9684809440641bbc5f7b4dddc83beb86515ed
Michael Kruse <llvm-project@meinersbur.de>
2024-07-18 10:35:32 +0200

    [Clang][OpenMP] Add reverse directive (#92916)
    Add the reverse directive which will be introduced in the upcoming
    OpenMP 6.0 specification. A preview has been published in [Technical
    Report 12](https://www.openmp.org/wp-content/uploads/openmp-TR12.pdf).
    
    ---------
    
    Co-authored-by: Alexey Bataev <a.bataev@outlook.com>

--- clang/tools/libclang/CIndex.cpp
+++ clang/tools/libclang/CIndex.cpp
@@ -2182,6 +2182,7 @@ public:
   VisitOMPLoopTransformationDirective(const OMPLoopTransformationDirective *D);
   void VisitOMPTileDirective(const OMPTileDirective *D);
   void VisitOMPUnrollDirective(const OMPUnrollDirective *D);
+  void VisitOMPReverseDirective(const OMPReverseDirective *D);
   void VisitOMPForDirective(const OMPForDirective *D);
   void VisitOMPForSimdDirective(const OMPForSimdDirective *D);
   void VisitOMPSectionsDirective(const OMPSectionsDirective *D);
@@ -3225,6 +3226,10 @@ void EnqueueVisitor::VisitOMPTileDirective(const OMPTileDirective *D) {
 }
 
 void EnqueueVisitor::VisitOMPUnrollDirective(const OMPUnrollDirective *D) {
+  VisitOMPLoopTransformationDirective(D);
+}
+
+void EnqueueVisitor::VisitOMPReverseDirective(const OMPReverseDirective *D) {
   VisitOMPLoopTransformationDirective(D);
 }
 
@@ -6097,6 +6102,8 @@ CXString clang_getCursorKindSpelling(enum CXCursorKind Kind) {
     return cxstring::createRef("OMPTileDirective");
   case CXCursor_OMPUnrollDirective:
     return cxstring::createRef("OMPUnrollDirective");
+  case CXCursor_OMPReverseDirective:
+    return cxstring::createRef("OMPReverseDirective");
   case CXCursor_OMPForDirective:
     return cxstring::createRef("OMPForDirective");
   case CXCursor_OMPForSimdDirective:
