6586477b31af00ad1e1571fe23eadcb4be1bcaf9
OverMighty <its.overmighty@gmail.com>
2024-07-18 19:50:49 +0200

    [libc][math][c23] Add {f,d}mul{l,f128} and f16mul{,f,l,f128} C23 math functions (#98972)
    Part of #93566.
                    
    Fixes #94833.

--- libc/test/src/math/smoke/f16mull_test.cpp
+++ libc/test/src/math/smoke/f16mull_test.cpp
@@ -1,4 +1,4 @@
-//===-- Unittests for fmul ------------------------------------------------===//
+//===-- Unittests for f16mull ---------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -8,6 +8,6 @@
 
 #include "MulTest.h"
 
-#include "src/math/fmul.h"
+#include "src/math/f16mull.h"
 
-LIST_MUL_TESTS(float, double, LIBC_NAMESPACE::fmul)
+LIST_MUL_TESTS(float16, long double, LIBC_NAMESPACE::f16mull)
