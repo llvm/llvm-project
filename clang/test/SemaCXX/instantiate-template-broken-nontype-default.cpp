465bbc2a3d09fec4f75ed9eb98bab3c6a9c7f4fd
Alejandro Álvarez Ayllón <alejandro.alvarez@sonarsource.com>
2024-07-18 09:58:56 +0200

    Update test

--- clang/test/SemaCXX/instantiate-template-broken-nontype-default.cpp
+++ clang/test/SemaCXX/instantiate-template-broken-nontype-default.cpp
@@ -3,8 +3,10 @@
 constexpr Missing a = 0; // expected-error {{unknown type name 'Missing'}}
 
 template < typename T, Missing b = a> // expected-error {{unknown type name 'Missing'}}
-class Klass { // expected-note {{candidate template ignored: could not match 'Klass<T, b>' against 'int'}}
-  Klass(T);   // expected-note {{candidate template ignored: substitution failure [with T = int, b = <recovery-expr>()]}}
+class Klass { // expected-note {{candidate template ignored: could not match 'Klass<T, b>' against 'int'}} \
+                 expected-note {{implicit deduction guide declared as 'template <typename T, int b = <recovery-expr>()> Klass(Klass<T, b>) -> Klass<T, b>'}}
+  Klass(T);   // expected-note {{candidate template ignored: substitution failure [with T = int, b = <recovery-expr>()]}} \
+                 expected-note {{implicit deduction guide declared as 'template <typename T, int b = <recovery-expr>()> Klass(T) -> Klass<T, b>'}}
 };
 
 Klass foo{5}; // no-crash \
