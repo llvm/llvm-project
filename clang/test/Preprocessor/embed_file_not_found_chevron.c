9c6914f33b91fe3a449680c5726b7b886687fa5f
Podchishchaeva, Mariya <mariya.podchishchaeva@intel.com>
2024-07-18 04:04:13 -0700

    Rename the test

--- clang/test/Preprocessor/embed_file_not_found_chevron.c
+++ clang/test/Preprocessor/embed_file_not_found_chevron.c
@@ -1,5 +1,4 @@
-// RUN: %clang_cc1 -std=c23 %s -E -verify --embed-dir=%S --embed-dir=%S/Inputs
+// RUN: %clang_cc1 -std=c23 %s -E -verify
 
-#embed <jk.txt>
 #embed <nfejfNejAKFe>
 // expected-error@-1 {{'nfejfNejAKFe' file not found}}
