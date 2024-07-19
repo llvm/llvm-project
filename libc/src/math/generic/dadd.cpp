baaf05c414fbb151f5f1d3065425f200722bc514
Aaryan Shukla <aaryanshukla@google.com>
2024-07-19 17:55:11 +0000

    testing

---
+++ libc/src/math/generic/dadd.cpp
@@ -0,0 +1,20 @@
+//===-- Implementation of dadd function ---------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "src/math/dadd.h"
+#include "src/__support/FPUtil/generic/add_sub.h"
+#include "src/__support/common.h"
+#include "src/__support/macros/config.h"
+
+namespace LIBC_NAMESPACE_DECL {
+
+LLVM_LIBC_FUNCTION(double, f16add, (float x, float y)) {
+  return fputil::generic::add<double>(x, y);
+}
+
+} // namespace LIBC_NAMESPACE_DECL
