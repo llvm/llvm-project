4c226d60d740f9e8516fdc7b3dd2c4cda07dac8f
Job Hernandez <hj93@protonmail.com>
2024-07-20 14:16:59 -0700

    add template for explain and compare

--- libc/utils/MPFRWrapper/MPFRUtils.cpp
+++ libc/utils/MPFRWrapper/MPFRUtils.cpp
@@ -887,6 +887,9 @@ template void explain_unary_operation_single_output_error(Operation op,
                                                           long double,
                                                           long double, double,
                                                           RoundingMode);
+template void explain_unary_operation_single_output_error(Operation op, double, float, double, RoundingMode);
+template void explain_unary_operation_single_output_error(Operation op, long double, float, double, RoundingMode);
+  
 #ifdef LIBC_TYPES_HAS_FLOAT16
 template void explain_unary_operation_single_output_error(Operation op, float16,
                                                           float16, double,
@@ -1105,6 +1108,11 @@ template bool compare_unary_operation_single_output(Operation, double, double,
                                                     double, RoundingMode);
 template bool compare_unary_operation_single_output(Operation, long double,
                                                     long double, double,
+                                                    RoundingMode);
+template bool compare_unary_operation_single_output(Operation, double, float,
+                                                    double, RoundingMode);
+template bool compare_unary_operation_single_output(Operation, long double,
+                                                    float, double,
                                                     RoundingMode);
 #ifdef LIBC_TYPES_HAS_FLOAT16
 template bool compare_unary_operation_single_output(Operation, float16, float16,
