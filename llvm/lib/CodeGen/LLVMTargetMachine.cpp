3f73ca43d950399485ec4a3e567a97adc710de1d
Fangrui Song <i@maskray.me>
2024-07-20 10:43:52 -0700

    [MC] Remove unnecessary DWARFMustBeAtTheEnd check
    36a15cb975334403216e6145d4abece3026af17a introduced the
    DWARFMustBeAtTheEnd check to ensure DWARF sections were placed after all
    text sections to help avoid out-of-range branches for Darwin ARM. The
    commit removed a Darwin ARM hack from
    20e5f5ed7930efdf2bd34bf099f24ac88798c5ea (2009), likely due to a
    no-longer-relevant assembler limitation.
    
    However, this check is no longer relevant due to the following:
    
    * Our CodeGen approach reliably places DWARF sections at the end.
    * Darwin AArch32 is less relevant today.
    
    Removing this check also addresses a minor clang cc1as crash that could
    occur when text sections were placed after DWARF sections
    (e9ad54b3ee905ea3a77c35ca7d6e843b2c552e0b (2015)).

--- llvm/lib/CodeGen/LLVMTargetMachine.cpp
+++ llvm/lib/CodeGen/LLVMTargetMachine.cpp
@@ -208,9 +208,8 @@ Expected<std::unique_ptr<MCStreamer>> LLVMTargetMachine::createMCStreamer(
         T, Context, std::unique_ptr<MCAsmBackend>(MAB),
         DwoOut ? MAB->createDwoObjectWriter(Out, *DwoOut)
                : MAB->createObjectWriter(Out),
-        std::unique_ptr<MCCodeEmitter>(MCE), STI, Options.MCOptions.MCRelaxAll,
-        Options.MCOptions.MCIncrementalLinkerCompatible,
-        /*DWARFMustBeAtTheEnd*/ true));
+        std::unique_ptr<MCCodeEmitter>(MCE), STI, /*ignore=*/false, false,
+        false));
     break;
   }
   case CodeGenFileType::Null:
@@ -284,9 +283,7 @@ bool LLVMTargetMachine::addPassesToEmitMC(PassManagerBase &PM, MCContext *&Ctx,
   const Triple &T = getTargetTriple();
   std::unique_ptr<MCStreamer> AsmStreamer(getTarget().createMCObjectStreamer(
       T, *Ctx, std::move(MAB), MAB->createObjectWriter(Out), std::move(MCE),
-      STI, Options.MCOptions.MCRelaxAll,
-      Options.MCOptions.MCIncrementalLinkerCompatible,
-      /*DWARFMustBeAtTheEnd*/ true));
+      STI, /*ignore=*/false, false, false));
 
   // Create the AsmPrinter, which takes ownership of AsmStreamer if successful.
   FunctionPass *Printer =
