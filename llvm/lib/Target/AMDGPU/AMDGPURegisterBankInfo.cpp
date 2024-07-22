ec7f8e11133ff6cd0370398ee32d684a6f857fcc
Jessica Del <50999226+OutOfCache@users.noreply.github.com>
2024-07-22 18:04:49 +0200

    [AMDGPU] Add intrinsic for raw atomic buffer loads (#97707)
    Upstream the intrinsics `llvm.amdgcn.raw.atomic.buffer.load`
    and `llvm.amdgcn.raw.atomic.ptr.buffer.load`.
    
    These additional intrinsics mark atomic buffer loads
    as atomic to LLVM by removing the `IntrReadMem`
    attribute. Otherwise, it could hoist these
    intrinsics out of loops in cases where LLVM marks
    them as invariant. That can cause issues such as
    infinite loops.
    
    Continuation of https://reviews.llvm.org/D138786
    with the additional use in the fat buffer lowering,
    more test cases and the additional ptr versions 
    of these intrinsics.
    
    ---------
    
    Co-authored-by: rtayl <>
    Co-authored-by: Jay Foad <jay.foad@amd.com>
    Co-authored-by: Mariusz Sikora <mariusz.sikora@amd.com>

--- llvm/lib/Target/AMDGPU/AMDGPURegisterBankInfo.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPURegisterBankInfo.cpp
@@ -4985,6 +4985,8 @@ AMDGPURegisterBankInfo::getInstrMapping(const MachineInstr &MI) const {
     }
     case Intrinsic::amdgcn_raw_buffer_load:
     case Intrinsic::amdgcn_raw_ptr_buffer_load:
+    case Intrinsic::amdgcn_raw_atomic_buffer_load:
+    case Intrinsic::amdgcn_raw_ptr_atomic_buffer_load:
     case Intrinsic::amdgcn_raw_tbuffer_load:
     case Intrinsic::amdgcn_raw_ptr_tbuffer_load: {
       // FIXME: Should make intrinsic ID the last operand of the instruction,
