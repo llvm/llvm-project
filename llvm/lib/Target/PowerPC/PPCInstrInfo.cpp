f6a4fc6261d185f8a878d8b23470b8421fc09146
Alison Zhang <alisonzhang@ibm.com>
2024-07-22 13:46:48 -0500

    Remove P11 opcodes

--- llvm/lib/Target/PowerPC/PPCInstrInfo.cpp
+++ llvm/lib/Target/PowerPC/PPCInstrInfo.cpp
@@ -3486,7 +3486,7 @@ unsigned PPCInstrInfo::getSpillTarget() const {
   // registers. MMA implies paired vectors, so we can just check that.
   bool IsP10Variant = Subtarget.isISA3_1() || Subtarget.pairedVectorMemops();
   // P11 uses the P10 target.
-  return Subtarget.isISAFuture()   ? 4
+  return Subtarget.isISAFuture()   ? 3
          : IsP10Variant            ? 2
          : Subtarget.hasP9Vector() ? 1
                                    : 0;
