38f10a7ea83836fda155624cd03e2e09b505d9e6
Daniel Bertalan <dani@danielbertalan.dev>
2024-07-22 10:56:17 +0200

    address code style issues in benchmark

--- llvm/benchmarks/xxhash.cpp
+++ llvm/benchmarks/xxhash.cpp
@@ -1,6 +1,8 @@
 #include "llvm/Support/xxhash.h"
 #include "benchmark/benchmark.h"
 
+#include <memory>
+
 static uint32_t xorshift(uint32_t State) {
   State ^= State << 13;
   State ^= State >> 17;
@@ -9,21 +11,17 @@ static uint32_t xorshift(uint32_t State) {
 }
 
 static void BM_xxh3_64bits(benchmark::State &State) {
-  uint32_t *Data = new uint32_t[State.range(0) / 4];
+  std::unique_ptr<uint32_t[]> Data(new uint32_t[State.range(0) / 4]);
 
   uint32_t Prev = 0xcafebabe;
-  for (int64_t I = 0; I < State.range(0) / 4; I++) {
+  for (int64_t I = 0; I < State.range(0) / 4; I++)
     Data[I] = Prev = xorshift(Prev);
-  }
 
   llvm::ArrayRef DataRef =
-      llvm::ArrayRef(reinterpret_cast<uint8_t *>(Data), State.range(0));
+      llvm::ArrayRef(reinterpret_cast<uint8_t *>(Data.get()), State.range(0));
 
-  for (auto _ : State) {
+  for (auto _ : State)
     llvm::xxh3_64bits(DataRef);
-  }
-
-  delete[] Data;
 }
 
 BENCHMARK(BM_xxh3_64bits)->Arg(32)->Arg(512)->Arg(64 * 1024)->Arg(1024 * 1024);
