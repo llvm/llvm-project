ede49c7485fa49ab0b73707ed02b152afc95dfcd
Philip Reames <preames@rivosinc.com>
2024-07-23 09:16:20 -0700

    Move feature_bits.c into cpu_model
    Patch by @BeMG

--- compiler-rt/lib/builtins/cpu_model/riscv.c
+++ compiler-rt/lib/builtins/cpu_model/riscv.c
@@ -1,4 +1,4 @@
-//=== feature_bits.c - Update RISC-V Feature Bits Structure -*- C -*-=========//
+//=== cpu_model/riscv.c - Update RISC-V Feature Bits Structure -*- C -*-======//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
