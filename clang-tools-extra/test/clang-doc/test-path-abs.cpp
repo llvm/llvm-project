02a1c5afed9d87850f93c2a65a31d587a67e89e5
PeterChou1 <peter.chou@mail.utoronto.ca>
2024-07-24 18:47:26 -0400

    [clang-doc] address pr comments

--- clang-tools-extra/test/clang-doc/test-path-abs.cpp
+++ clang-tools-extra/test/clang-doc/test-path-abs.cpp
@@ -1,6 +1,6 @@
 // RUN: rm -rf %t && mkdir %t
 // RUN: echo "CHECK: var RootPath = \"%/t/docs\";" > %t/check.txt
 // RUN: cp "%s" "%t/test.cpp"
-// RUN: clang-doc --format=html --executor=standalone -p %t %t/test.cpp --output=%t/docs
-// RUN: FileCheck %t/check.txt -input-file=%t/docs/index_json.js
+// RUN: clang-doc --format=html --executor=standalone -p %s --output=%t
+// RUN: FileCheck %s -input-file=%t/index_json.js
 // RUN: rm -rf %t
