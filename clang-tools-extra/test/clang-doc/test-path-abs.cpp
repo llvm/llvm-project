17326765ebcbbf562ea0a4768e316ae06622c98c
PeterChou1 <peter.chou@mail.utoronto.ca>
2024-07-24 19:24:29 -0400

    [clang-doc] rework tests

--- clang-tools-extra/test/clang-doc/test-path-abs.cpp
+++ clang-tools-extra/test/clang-doc/test-path-abs.cpp
@@ -1,6 +1,7 @@
 // RUN: rm -rf %t && mkdir %t
-// RUN: echo "CHECK: var RootPath = \"%/t/docs\";" > %t/check.txt
 // RUN: cp "%s" "%t/test.cpp"
 // RUN: clang-doc --format=html --executor=standalone -p %s --output=%t
-// RUN: FileCheck %s -input-file=%t/index_json.js
+// RUN: FileCheck %s -input-file=%t/index_json.js  -check-prefix=JSON-INDEX
 // RUN: rm -rf %t
+
+// JSON-INDEX: var RootPath = "{{.*}}";
