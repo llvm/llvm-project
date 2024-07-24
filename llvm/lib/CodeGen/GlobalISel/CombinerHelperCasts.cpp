30012aa04778bef9d28b41cfe7b5ef37ad4ccaec
Thorsten Schütt <schuett@gmail.com>
2024-07-24 15:50:38 +0200

    ping ci

--- llvm/lib/CodeGen/GlobalISel/CombinerHelperCasts.cpp
+++ llvm/lib/CodeGen/GlobalISel/CombinerHelperCasts.cpp
@@ -149,7 +149,7 @@ bool CombinerHelper::matchTruncateOfExt(const MachineInstr &Root,
   }
 
   if (SrcTy.getScalarSizeInBits() > DstTy.getScalarSizeInBits()) {
-    // The source is larger than the destination. We need to truncate.
+    // If the  source is larger than the destination, we need to truncate.
 
     if (!isLegalOrBeforeLegalizer({TargetOpcode::G_TRUNC, {DstTy, SrcTy}}))
       return false;
