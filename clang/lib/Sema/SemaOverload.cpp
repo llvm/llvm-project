2b417ccb9dba0e3f62a4fe46f85882247b03f701
MaxEW707 <max.enrico.winkler@gmail.com>
2024-07-23 20:40:06 -0700

    Reorder logic around `AllowMSLValueReferenceBinding`

--- clang/lib/Sema/SemaOverload.cpp
+++ clang/lib/Sema/SemaOverload.cpp
@@ -5014,13 +5014,8 @@ Sema::CompareReferenceRelationship(SourceLocation Loc,
 }
 
 bool Sema::AllowMSLValueReferenceBinding(Qualifiers Quals, QualType QT) {
-  if (Quals.hasVolatile())
-    return false;
-  if (Quals.hasConst())
-    return true;
-  if (QT->isBuiltinType() || QT->isArrayType())
-    return false;
-  return true;
+  return getLangOpts().MSVCReferenceBinding &&
+         !(Quals.hasVolatile() || QT->isBuiltinType() || QT->isArrayType());
 }
 
 /// Look for a user-defined conversion to a value reference-compatible
@@ -5255,19 +5250,10 @@ TryReferenceInit(Sema &S, Expr *Init, QualType DeclType,
   //     -- Otherwise, the reference shall be an lvalue reference to a
   //        non-volatile const type (i.e., cv1 shall be const), or the reference
   //        shall be an rvalue reference.
-  if (!isRValRef) {
-    const bool CanBindLValueRef =
-        !S.getLangOpts().MSVCReferenceBinding
-            ? (T1.isConstQualified() && !T1.isVolatileQualified())
-            : S.AllowMSLValueReferenceBinding(T1.getQualifiers(), T1);
-    if (!CanBindLValueRef) {
-      if (InitCategory.isRValue() && RefRelationship != Sema::Ref_Incompatible)
-        ICS.setBad(BadConversionSequence::lvalue_ref_to_rvalue, Init, DeclType);
-      return ICS;
-    }
-  }
-
-  if (!isRValRef && !CanBindLValueRef) {
+  if (!isRValRef && (!T1.isConstQualified() || T1.isVolatileQualified()) &&
+      !S.AllowMSLValueReferenceBinding(T1.getQualifiers(), T1)) {
+    if (InitCategory.isRValue() && RefRelationship != Sema::Ref_Incompatible)
+      ICS.setBad(BadConversionSequence::lvalue_ref_to_rvalue, Init, DeclType);
     return ICS;
   }
 
