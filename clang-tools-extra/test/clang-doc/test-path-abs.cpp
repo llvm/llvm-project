c2d9e15b944727195b3a5abc9c3f7bcb24842a02
PeterChou1 <peter.chou@mail.utoronto.ca>
2024-07-24 19:52:55 -0400

    [clang-doc] fix broken tests

--- clang-tools-extra/test/clang-doc/test-path-abs.cpp
+++ clang-tools-extra/test/clang-doc/test-path-abs.cpp
@@ -1,7 +1,7 @@
 // RUN: rm -rf %t && mkdir %t
 // RUN: cp "%s" "%t/test.cpp"
-// RUN: clang-doc --format=html --executor=standalone -p %s --output=%t
+// RUN: clang-doc --format=html --executor=standalone -p %t %t/test.cpp --output=%t
 // RUN: FileCheck %s -input-file=%t/index_json.js  -check-prefix=JSON-INDEX
 // RUN: rm -rf %t
 
-// JSON-INDEX: var RootPath = "{{.*}}";
+// JSON-INDEX: var RootPath = "{{.*}}test-path-abs.cpp";
