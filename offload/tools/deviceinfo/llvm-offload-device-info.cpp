5929b7ed26d25c51ce0fda60fcc9b8ef31a9d998
Johannes Doerfert <johannes@jdoerfert.de>
2024-07-24 09:35:09 -0700

    [Offload] Repair and rename `llvm-omp-device-info` (to `-offload-`) (#100309)
    The `llvm-omp-device-info` tool is very handy, but broke due to the lazy
    evaluation of devices. This repairs the functionality and adds a test.
    The tool is also renamed into `llvm-offload-device-info` as `-omp-` is
    going away.

--- offload/tools/deviceinfo/llvm-offload-device-info.cpp
+++ offload/tools/deviceinfo/llvm-offload-device-info.cpp
@@ -1,4 +1,4 @@
-//===- llvm-omp-device-info.cpp - Obtain device info as seen from OpenMP --===//
+//===- llvm-offload-device-info.cpp - Device info as seen by LLVM/Offload -===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,8 +6,8 @@
 //
 //===----------------------------------------------------------------------===//
 //
-// This is a command line utility that, by using Libomptarget, and the device
-// plugins, list devices information as seen from the OpenMP Runtime.
+// This is a command line utility that, by using LLVM/Offload, and the device
+// plugins, list devices information as seen by the runtime.
 //
 //===----------------------------------------------------------------------===//
 
@@ -19,8 +19,9 @@ int main(int argc, char **argv) {
   __tgt_register_lib(&EmptyDesc);
   __tgt_init_all_rtls();
 
+  printf("Found %d devices:\n", omp_get_num_devices());
   for (int Dev = 0; Dev < omp_get_num_devices(); Dev++) {
-    printf("Device (%d):\n", Dev);
+    printf("  Device %d:\n", Dev);
     if (!__tgt_print_device_info(Dev))
       printf("    print_device_info not implemented\n");
     printf("\n");
