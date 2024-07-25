a0ec85177c7f14e3963675a3072241567be6decc
Shilei Tian <shilei.tian@amd.com>
2024-07-25 15:49:58 -0400

    AMDGPU: Add support for V_CVT_PK_F16_F32 instruction for gfx950
    Co-authored-by: Shilei Tian <shilei.tian@amd.com>

--- llvm/lib/Target/AMDGPU/AMDGPUSubtarget.h
+++ llvm/lib/Target/AMDGPU/AMDGPUSubtarget.h
@@ -55,6 +55,7 @@ protected:
   bool HasFP4ConversionScaleInsts = false;
   bool HasFP6BF6ConversionScaleInsts = false;
   bool HasF16BF16ToFP6BF6ConversionScaleInsts = false;
+  bool HasCvtPkF16F32Inst = false;
   bool HasF32ToF16BF16ConversionSRInsts = false;
   bool EnableRealTrue16Insts = false;
   bool HasBF16ConversionInsts = false;
@@ -190,6 +191,8 @@ public:
   bool hasFP6BF6ConversionScaleInsts() const { return HasFP6BF6ConversionScaleInsts; }
 
   bool hasF16BF16ToFP6BF6ConversionScaleInsts() const { return HasF16BF16ToFP6BF6ConversionScaleInsts; }
+
+  bool hasCvtPkF16F32Inst() const { return HasCvtPkF16F32Inst; }
 
   bool hasF32ToF16BF16ConversionSRInsts() const {
     return HasF32ToF16BF16ConversionSRInsts;
