aac1dd8182d62b908cdb8e897cbadd05f370797b
Mitch Phillips <mitchp@google.com>
2024-07-26 11:17:40 +0200

    Some include and function prefix changes for Fuchsia

--- compiler-rt/lib/gwp_asan/platform_specific/utilities_fuchsia.cpp
+++ compiler-rt/lib/gwp_asan/platform_specific/utilities_fuchsia.cpp
@@ -9,10 +9,10 @@
 #include "gwp_asan/utilities.h"
 
 #include <alloca.h>
-#include <inttypes.h>
 #include <stdio.h>
 #include <string.h>
 #include <zircon/sanitizer.h>
+#include <zircon/status.h>
 
 namespace gwp_asan {
 void die(const char *Message) {
@@ -22,7 +22,7 @@ void die(const char *Message) {
 
 void dieWithErrorCode(const char *Message, int64_t ErrorCode) {
   const char *error_str =
-      zx_status_get_string(static_cast<zx_status_t>(ErrorCode));
+      _zx_status_get_string(static_cast<zx_status_t>(ErrorCode));
   size_t buffer_size = strlen(Message) + 32 + strlen(error_str);
   char *buffer = static_cast<char *>(alloca(buffer_size));
   snprintf(buffer, buffer_size, "%s (Error Code: %s)", Message, error_str);
