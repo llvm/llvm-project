296549260dbc6c3c0d56e7aae7022655291f9fc6
Michael Maitland <michaeltmaitland@gmail.com>
2024-07-26 07:10:04 -0700

    [DAG] Add SDPatternMatch for VScale nodes

--- llvm/include/llvm/CodeGen/SDPatternMatch.h
+++ llvm/include/llvm/CodeGen/SDPatternMatch.h
@@ -722,6 +722,10 @@ inline Or<UnaryOpc_match<Opnd>, Opnd> m_TruncOrSelf(const Opnd &Op) {
   return Or<UnaryOpc_match<Opnd>, Opnd>(m_Trunc(Op), Op);
 }
 
+template <typename Opnd> inline UnaryOpc_match<Opnd> m_VScale(const Opnd &Op) {
+  return UnaryOpc_match<Opnd>(ISD::VSCALE, Op);
+}
+
 // === Constants ===
 struct ConstantInt_match {
   APInt *BindVal;
