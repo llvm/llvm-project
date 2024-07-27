57c8ca05677e000c6d5aaccf9a168db281bab873
Fangrui Song <i@maskray.me>
2024-07-27 14:46:41 -0700

    [ELF] Track line number precisely
    `getLineNumber` is both imprecise (when `INCLUDE` is used) and
    inefficient (see https://reviews.llvm.org/D104137). Track line number
    precisely now that we have `struct Buffer` abstraction from #100493.

--- lld/ELF/ScriptLexer.h
+++ lld/ELF/ScriptLexer.h
@@ -23,6 +23,7 @@ protected:
     // The remaining content to parse and the filename.
     StringRef s, filename;
     const char *begin = nullptr;
+    size_t lineNumber = 1;
     Buffer() = default;
     Buffer(MemoryBufferRef mb)
         : s(mb.getBuffer()), filename(mb.getBufferIdentifier()),
@@ -44,6 +45,7 @@ protected:
   // curTok holds the cached return value of peek() and is invalid when the
   // expression state changes.
   StringRef curTok;
+  size_t prevTokLine = 1;
   // The inExpr state when curTok is cached.
   bool curTokState = false;
   bool eof = false;
@@ -67,12 +69,8 @@ public:
   std::vector<MemoryBufferRef> mbs;
   bool inExpr = false;
 
-  size_t lastLineNumber = 0;
-  size_t lastLineNumberOffset = 0;
-
 private:
   StringRef getLine();
-  size_t getLineNumber();
   size_t getColumnNumber();
 };
 
