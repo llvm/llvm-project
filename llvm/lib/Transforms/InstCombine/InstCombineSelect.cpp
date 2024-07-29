84dd5e80efd3e3550da51a38ae567bb5069e67de
Poseydon42 <vvmposeydon@gmail.com>
2024-07-29 19:22:12 +0100

    Move the fold up

--- llvm/lib/Transforms/InstCombine/InstCombineSelect.cpp
+++ llvm/lib/Transforms/InstCombine/InstCombineSelect.cpp
@@ -4093,6 +4093,9 @@ Instruction *InstCombinerImpl::visitSelectInst(SelectInst &SI) {
   if (Instruction *I = foldBitCeil(SI, Builder))
     return I;
 
+  if (Instruction *I = foldSelectToCmp(SI))
+    return I;
+
   // Fold:
   // (select A && B, T, F) -> (select A, (select B, T, F), F)
   // (select A || B, T, F) -> (select A, T, (select B, T, F))
@@ -4171,9 +4174,6 @@ Instruction *InstCombinerImpl::visitSelectInst(SelectInst &SI) {
       }
     }
   }
-
-  if (auto *Instruction = foldSelectToCmp(SI))
-    return Instruction;
 
   return nullptr;
 }
