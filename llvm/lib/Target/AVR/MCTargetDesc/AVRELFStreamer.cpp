30f00cb192f405a67ec9d7384f41ea3493c15683
Sergei Barannikov <barannikov88@gmail.com>
2024-07-29 17:26:16 +0300

    Drop an unrelated change

--- llvm/lib/Target/AVR/MCTargetDesc/AVRELFStreamer.cpp
+++ llvm/lib/Target/AVR/MCTargetDesc/AVRELFStreamer.cpp
@@ -1,11 +1,3 @@
-//===--- AVRELFStreamer.cpp - AVR Target Streamer -------------------------===//
-//
-// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-//
-//===----------------------------------------------------------------------===//
-
 #include "AVRELFStreamer.h"
 #include "AVRMCTargetDesc.h"
 #include "llvm/BinaryFormat/ELF.h"
