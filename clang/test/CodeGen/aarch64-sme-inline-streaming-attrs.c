5715bd7acda7ab93aed8f9781a31738ff91c4920
Sander de Smalen <sander.desmalen@arm.com>
2024-07-29 09:29:30 +0100

    [Clang] Demote always_inline error to warning for mismatching SME attrs (#100740)
    PR #77936 introduced a diagnostic to avoid calls being inlined into
    functions with a different streaming mode, because inlining those
    functions may result in different runtime behaviour. This was necessary
    because LLVM doesn't check whether inlining is possible and thus blindly
    inlines the function without checking feasibility.
    
    In practice however, this introduces an artificial restriction that the
    user may not be able to work around. Calling an `always_inline` function
    from some header file that is out of the control of the user would
    result in an error that the user cannot remedy.
    
    Therefore, this patch demotes the error into a warning (for calls from
    streaming[-compatible] -> non-streaming), but the proper fix would be to
    fix the AlwaysInliner in LLVM to avoid inlining when it has analyzed the
    callee and has determined that inlining is not possible.
    
    Calling an always_inline function for calls from non-streaming ->
    streaming will remain an error, because there is little pre-existing
    code for SME, so it is expected that the header file can be modified by
    the user (e.g. by using `__arm_streaming_compatible` if the code is
    claimed to be compatible).

--- clang/test/CodeGen/aarch64-sme-inline-streaming-attrs.c
+++ clang/test/CodeGen/aarch64-sme-inline-streaming-attrs.c
@@ -20,7 +20,7 @@ void caller(void) {
 
 #ifdef TEST_COMPATIBLE
 void caller_compatible(void) __arm_streaming_compatible {
-    inlined_fn(); // expected-error {{always_inline function 'inlined_fn' and its caller 'caller_compatible' have mismatching streaming attributes}}
+    inlined_fn(); // expected-warning {{always_inline function 'inlined_fn' and its caller 'caller_compatible' have mismatching streaming attributes, inlining may change runtime behaviour}}
     inlined_fn_streaming_compatible();
     inlined_fn_streaming(); // expected-error {{always_inline function 'inlined_fn_streaming' and its caller 'caller_compatible' have mismatching streaming attributes}}
     inlined_fn_local(); // expected-error {{always_inline function 'inlined_fn_local' and its caller 'caller_compatible' have mismatching streaming attributes}}
@@ -29,7 +29,7 @@ void caller_compatible(void) __arm_streaming_compatible {
 
 #ifdef TEST_STREAMING
 void caller_streaming(void) __arm_streaming {
-    inlined_fn(); // expected-error {{always_inline function 'inlined_fn' and its caller 'caller_streaming' have mismatching streaming attributes}}
+    inlined_fn(); // expected-warning {{always_inline function 'inlined_fn' and its caller 'caller_streaming' have mismatching streaming attributes, inlining may change runtime behaviour}}
     inlined_fn_streaming_compatible();
     inlined_fn_streaming();
     inlined_fn_local();
@@ -39,7 +39,7 @@ void caller_streaming(void) __arm_streaming {
 #ifdef TEST_LOCALLY
 __arm_locally_streaming
 void caller_local(void) {
-    inlined_fn(); // expected-error {{always_inline function 'inlined_fn' and its caller 'caller_local' have mismatching streaming attributes}}
+    inlined_fn(); // expected-warning {{always_inline function 'inlined_fn' and its caller 'caller_local' have mismatching streaming attributes, inlining may change runtime behaviour}}
     inlined_fn_streaming_compatible();
     inlined_fn_streaming();
     inlined_fn_local();
