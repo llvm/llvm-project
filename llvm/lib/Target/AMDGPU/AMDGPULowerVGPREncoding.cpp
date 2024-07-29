c9ed7654d1274535244744a0b4a8c3105db36dd6
Joe Nash <joseph.nash@amd.com>
2024-07-29 13:01:57 -0400

    [AMDGPU][GFX13] Lower idx0-3
    Read the index operand in V_LOAD/STORE_IDX and set_vgpr_frames based on
    it.
    Still only one idx per instruction is supported.
    
    Change-Id: I9341de6d060aa4ae5a2953e86e9a5e23c90e82ec

--- llvm/lib/Target/AMDGPU/AMDGPULowerVGPREncoding.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPULowerVGPREncoding.cpp
@@ -193,10 +193,6 @@ void AMDGPULowerVGPREncoding::updateModeForIDX(ModeTy &Mode,
 
 bool AMDGPULowerVGPREncoding::lowerIDX(ModeTy &NewMode, MachineInstr &MI) {
   unsigned Opc = MI.getOpcode();
-  // TODO-GFX13: We can look at the actual operands to determine the index
-  // register, but for now IsLoad is enough.
-  assert(MI.getOperand(AMDGPU::getNamedOperandIdx(Opc, AMDGPU::OpName::idx))
-             .getReg() == AMDGPU::IDX1);
   // The RC in MachineInstrDesc for V_LOAD/STORE_IDX can contain many
   // possible register sizes, we need to use the register info instead.
   const auto *TRI = ST->getRegisterInfo();
@@ -207,13 +203,17 @@ bool AMDGPULowerVGPREncoding::lowerIDX(ModeTy &NewMode, MachineInstr &MI) {
   auto Size = TRI->getRegSizeInBits(*RC);
   assert(Size == 32 &&
          "TODO-GFX13 Support lowering non-32-bit sizes for V_LOAD/STORE_IDX");
+  Register IdxReg =
+      MI.getOperand(AMDGPU::getNamedOperandIdx(Opc, AMDGPU::OpName::idx))
+          .getReg();
+  unsigned IdxRegVal = IdxReg - AMDGPU::IDX0;
   bool IsLoad = Opc == AMDGPU::V_LOAD_IDX;
   // src0, src1, src2, dst
   ModeTy UsedIdxRegs;
   if (IsLoad)
-    UsedIdxRegs[0] = 1;
+    UsedIdxRegs[0] = IdxRegVal;
   else
-    UsedIdxRegs[3] = 1;
+    UsedIdxRegs[3] = IdxRegVal;
   updateModeForIDX(NewMode, UsedIdxRegs);
 
   // Synthesize the offset VGPR
