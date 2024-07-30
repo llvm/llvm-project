71a23f543040bd15f7649a2cb44f1a94d4ddcc02
Alexis Engelke <engelke@in.tum.de>
2024-07-30 15:46:38 +0000

    Move block number functions to block-related functions

--- llvm/include/llvm/IR/Function.h
+++ llvm/include/llvm/IR/Function.h
@@ -82,31 +82,6 @@ private:
   /// Epoch of block numbers. (Could be shrinked to uint8_t if required.)
   unsigned BlockNumEpoch = 0;
 
-public:
-  /// Return a value larger than the largest block number. Intended to allocate
-  /// a vector that is sufficiently large to hold all blocks indexed by their
-  /// number.
-  unsigned getMaxBlockNumber() const { return NextBlockNum; }
-
-  /// Renumber basic blocks into a dense value range starting from 0. Be aware
-  /// that other data structures and analyses (e.g., DominatorTree) may depend
-  /// on the value numbers and need to be updated or invalidated.
-  void renumberBlocks();
-
-  /// Return the "epoch" of current block numbers. This will return a different
-  /// value after every renumbering. The intention is: if something (e.g., an
-  /// analysis) uses block numbers, it also stores the number epoch and then
-  /// can assert later on that the epoch didn't change (indicating that the
-  /// numbering is still valid). If the epoch changed, blocks might have been
-  /// assigned new numbers and previous uses of the numbers needs to be
-  /// invalidated. This is solely intended as a debugging feature.
-  unsigned getBlockNumberEpoch() const { return BlockNumEpoch; }
-
-private:
-  /// Assert that all blocks have unique numbers within 0..NextBlockNum. This
-  /// has O(n) runtime complexity.
-  void validateBlockNumbers() const;
-
   mutable Argument *Arguments = nullptr;  ///< The formal arguments
   size_t NumArgs;
   std::unique_ptr<ValueSymbolTable>
@@ -842,6 +817,34 @@ public:
     return SymTab.get();
   }
 
+  //===--------------------------------------------------------------------===//
+  // Block number functions
+
+  /// Return a value larger than the largest block number. Intended to allocate
+  /// a vector that is sufficiently large to hold all blocks indexed by their
+  /// number.
+  unsigned getMaxBlockNumber() const { return NextBlockNum; }
+
+  /// Renumber basic blocks into a dense value range starting from 0. Be aware
+  /// that other data structures and analyses (e.g., DominatorTree) may depend
+  /// on the value numbers and need to be updated or invalidated.
+  void renumberBlocks();
+
+  /// Return the "epoch" of current block numbers. This will return a different
+  /// value after every renumbering. The intention is: if something (e.g., an
+  /// analysis) uses block numbers, it also stores the number epoch and then
+  /// can assert later on that the epoch didn't change (indicating that the
+  /// numbering is still valid). If the epoch changed, blocks might have been
+  /// assigned new numbers and previous uses of the numbers needs to be
+  /// invalidated. This is solely intended as a debugging feature.
+  unsigned getBlockNumberEpoch() const { return BlockNumEpoch; }
+
+private:
+  /// Assert that all blocks have unique numbers within 0..NextBlockNum. This
+  /// has O(n) runtime complexity.
+  void validateBlockNumbers() const;
+
+public:
   //===--------------------------------------------------------------------===//
   // BasicBlock iterator forwarding functions
   //
