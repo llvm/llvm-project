539a6b500c9c7a808e68db32bf7965985202197f
Peter Klausler <35819229+klausler@users.noreply.github.com>
2024-07-30 09:42:15 -0700

    [flang] Detect use-before-decl errors on type parameters (#99947)
    Ensure that type parameters are declared as such before being referenced
    within the derived type definition. (Previously, such references would
    resolve to symbols in the enclosing scope.)
    
    This change causes the symbols for the type parameters to be created
    when the TYPE statement is processed in name resolution. They are
    TypeParamDetails symbols with no KIND/LEN attribute set, and they shadow
    any symbols of the same name in the enclosing scope.
    
    When the type parameter declarations are processed, the KIND/LEN
    attributes are set. Any earlier reference to a type parameter with no
    KIND/LEN attribute elicits an error.
    
    Some members of TypeParamDetails have been retyped &/or renamed.

--- flang/lib/Semantics/tools.cpp
+++ flang/lib/Semantics/tools.cpp
@@ -1135,12 +1135,12 @@ std::optional<parser::MessageFormattedText> CheckAccessibleSymbol(
   return std::nullopt;
 }
 
-std::list<SourceName> OrderParameterNames(const Symbol &typeSymbol) {
-  std::list<SourceName> result;
+SymbolVector OrderParameterNames(const Symbol &typeSymbol) {
+  SymbolVector result;
   if (const DerivedTypeSpec * spec{typeSymbol.GetParentTypeSpec()}) {
     result = OrderParameterNames(spec->typeSymbol());
   }
-  const auto &paramNames{typeSymbol.get<DerivedTypeDetails>().paramNames()};
+  const auto &paramNames{typeSymbol.get<DerivedTypeDetails>().paramNameOrder()};
   result.insert(result.end(), paramNames.begin(), paramNames.end());
   return result;
 }
@@ -1150,7 +1150,7 @@ SymbolVector OrderParameterDeclarations(const Symbol &typeSymbol) {
   if (const DerivedTypeSpec * spec{typeSymbol.GetParentTypeSpec()}) {
     result = OrderParameterDeclarations(spec->typeSymbol());
   }
-  const auto &paramDecls{typeSymbol.get<DerivedTypeDetails>().paramDecls()};
+  const auto &paramDecls{typeSymbol.get<DerivedTypeDetails>().paramDeclOrder()};
   result.insert(result.end(), paramDecls.begin(), paramDecls.end());
   return result;
 }
