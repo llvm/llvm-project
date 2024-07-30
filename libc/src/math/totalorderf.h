60da1920634558b0926272c4160b89f0a2ea7638
Job Hernandez <hj93@protonmail.com>
2024-07-30 09:37:28 -0700

    address review

--- libc/src/math/totalorderf.h
+++ libc/src/math/totalorderf.h
@@ -1,5 +1,4 @@
-//===-- Implementation header for totalorderf --------------------*- C++
-//-*-===//
+//===-- Implementation header for totalorderf -------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
