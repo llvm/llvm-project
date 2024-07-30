8510748f3270f2e7e9e96cc5e6080b6072fdb9d9
Vinicius Couto Espindola <34522047+sitio-couto@users.noreply.github.com>
2024-07-30 07:13:40 -0300

    [CIR][ABI] Add AArch64 singed integer and floating point CC lowering (#757)

--- clang/test/CIR/Transforms/Target/aarch64/aarch64-call-conv-lowering-pass.cpp
+++ clang/test/CIR/Transforms/Target/aarch64/aarch64-call-conv-lowering-pass.cpp
@@ -40,3 +40,46 @@ unsigned long long ULongLong(unsigned long long l) {
   // CHECK: cir.call @_Z9ULongLongy(%2) : (!u64i) -> !u64i
   return ULongLong(l);
 }
+
+
+/// Test call conv lowering for trivial signed cases. ///
+
+// CHECK: cir.func @_Z4Chara(%arg0: !s8i loc({{.+}})) -> !s8i
+char Char(signed char c) {
+  // CHECK: cir.call @_Z4Chara(%{{.+}}) : (!s8i) -> !s8i
+  return Char(c);
+}
+// CHECK: cir.func @_Z5Shorts(%arg0: !s16i loc({{.+}})) -> !s16i
+short Short(short s) {
+  // CHECK: cir.call @_Z5Shorts(%{{.+}}) : (!s16i) -> !s16i
+  return Short(s);
+}
+// CHECK: cir.func @_Z3Inti(%arg0: !s32i loc({{.+}})) -> !s32i
+int Int(int i) {
+  // CHECK: cir.call @_Z3Inti(%{{.+}}) : (!s32i) -> !s32i
+  return Int(i);
+}
+// CHECK: cir.func @_Z4Longl(%arg0: !s64i loc({{.+}})) -> !s64i
+long Long(long l) {
+  // CHECK: cir.call @_Z4Longl(%{{.+}}) : (!s64i) -> !s64i
+  return Long(l);
+}
+// CHECK: cir.func @_Z8LongLongx(%arg0: !s64i loc({{.+}})) -> !s64i
+long long LongLong(long long l) {
+  // CHECK: cir.call @_Z8LongLongx(%{{.+}}) : (!s64i) -> !s64i
+  return LongLong(l);
+}
+
+
+/// Test call conv lowering for floating point. ///
+
+// CHECK: cir.func @_Z5Floatf(%arg0: !cir.float loc({{.+}})) -> !cir.float
+float Float(float f) {
+  // cir.call @_Z5Floatf(%{{.+}}) : (!cir.float) -> !cir.float
+  return Float(f);
+}
+// CHECK: cir.func @_Z6Doubled(%arg0: !cir.double loc({{.+}})) -> !cir.double
+double Double(double d) {
+  // cir.call @_Z6Doubled(%{{.+}}) : (!cir.double) -> !cir.double
+  return Double(d);
+}
