8ce5541703ec7e1c6d499f5512e3b44c96e556a1
Advenam Tacet <advenam.tacet@gmail.com>
2024-07-31 02:12:36 +0200

    fixing formatting

--- compiler-rt/test/asan/TestCases/contiguous_container_crash.cpp
+++ compiler-rt/test/asan/TestCases/contiguous_container_crash.cpp
@@ -38,8 +38,8 @@ void BadBounds() {
 
 void UnalignedBadBounds() {
   char t[100];
-// CHECK-UNALIGNED-BAD-BOUNDS-NOT: beg is not aligned by
-// CHECK-UNALIGNED-BAD-BOUNDS: ERROR: AddressSanitizer: bad parameters to __sanitizer_annotate_contiguous_container
+  // CHECK-UNALIGNED-BAD-BOUNDS-NOT: beg is not aligned by
+  // CHECK-UNALIGNED-BAD-BOUNDS: ERROR: AddressSanitizer: bad parameters to __sanitizer_annotate_contiguous_container
   __sanitizer_annotate_contiguous_container(&t[1], &t[0] + 100, &t[0] + 101,
                                             &t[0] + 50);
 }
