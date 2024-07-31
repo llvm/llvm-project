b82183b0b50e4ea6ba942b1ae63c41c4bb4fa695
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2024-07-31 16:47:34 -0700

    [CIR][NFC] Fix few unused vars and tablegen format

--- clang/lib/CIR/Lowering/DirectToLLVM/LowerToLLVM.cpp
+++ clang/lib/CIR/Lowering/DirectToLLVM/LowerToLLVM.cpp
@@ -3603,7 +3603,6 @@ public:
             op->getParentOfType<mlir::LLVM::LLVMFuncOp>());
         auto catchFn = rewriter.create<mlir::LLVM::LLVMFuncOp>(
             op.getLoc(), cxaBeginCatch, catchFnTy);
-        sourceSymbol = catchFn;
       }
       rewriter.replaceOpWithNewOp<mlir::LLVM::CallOp>(
           op, mlir::TypeRange{llvmPtrTy}, cxaBeginCatch,
@@ -3623,7 +3622,6 @@ public:
             op->getParentOfType<mlir::LLVM::LLVMFuncOp>());
         auto catchFn = rewriter.create<mlir::LLVM::LLVMFuncOp>(
             op.getLoc(), cxaEndCatch, catchFnTy);
-        sourceSymbol = catchFn;
       }
       rewriter.create<mlir::LLVM::CallOp>(op.getLoc(), mlir::TypeRange{},
                                           cxaEndCatch, mlir::ValueRange{});
