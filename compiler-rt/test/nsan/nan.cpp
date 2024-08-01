e6017b53bf27fbbd9ca55a60ff50af78c8986eae
cseslowpoke <cseslowpoke@gmail.com>
2024-08-02 01:40:49 +0800

    update formatting

--- compiler-rt/test/nsan/nan.cpp
+++ compiler-rt/test/nsan/nan.cpp
@@ -1,18 +1,15 @@
 // RUN: %clangxx_nsan -O0 -g %s -o %t
 // RUN: NSAN_OPTIONS=check_nan=true %run %t 2>&1 | FileCheck %s
 
-
-#include<cmath>
-#include<cstdio>
+#include <cmath>
+#include <cstdio>
 
 // This function returns a NaN value for triggering the NaN detection.
-__attribute__((noinline))  
-float ReturnNaN() {
+__attribute__((noinline)) float ReturnNaN() {
   float ret = 0.0 / 0.0;
   return ret;
   // CHECK: WARNING: NumericalStabilitySanitizer: NaN detected
 }
-
 
 int main() {
   float val = ReturnNaN();
