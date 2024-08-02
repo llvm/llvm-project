b89fa14f78e734bc2496821f5425ef7460e4c59c
Eduard Zingerman <eddyz87@gmail.com>
2024-08-02 12:12:22 -0700

    test fix: use [[names]] for attributes in bpf-attr-bpf-fastcall-1
    Also change attribute names to be less confusing.

--- clang/test/CodeGen/bpf-attr-bpf-fastcall-1.c
+++ clang/test/CodeGen/bpf-attr-bpf-fastcall-1.c
@@ -12,13 +12,13 @@ void foo(void) {
 }
 
 // CHECK: @ptr = global ptr null
-// CHECK: define {{.*}} @foo()
+// CHECK: define {{.*}} void @foo()
 // CHECK: entry:
-// CHECK:   call void @test() #[[test_attr:[0-9]+]]
+// CHECK:   call void @test() #[[call_attr:[0-9]+]]
 // CHECK:   %[[ptr:.*]] = load ptr, ptr @ptr, align 8
-// CHECK:   call void %[[ptr]]() #[[test_attr]]
+// CHECK:   call void %[[ptr]]() #[[call_attr]]
 // CHECK:   ret void
 
-// CHECK: declare void @test() #[[ptr_attr:[0-9]+]]
-// CHECK: attributes #1 = { {{.*}}"bpf_fastcall"{{.*}} }
-// CHECK: attributes #[[test_attr]] = { {{.*}}"bpf_fastcall"{{.*}} }
+// CHECK: declare void @test() #[[func_attr:[0-9]+]]
+// CHECK: attributes #[[func_attr]] = { {{.*}}"bpf_fastcall"{{.*}} }
+// CHECK: attributes #[[call_attr]] = { "bpf_fastcall" }
