9c2fb031a520c6addd0128b0078a36d55d1d7451
Connie Zhu <connieyzhu@google.com>
2024-08-02 17:28:09 +0000

    [llvm-lit] Combined test files to separate by option for cat
    Tests for the same cat options were initially in different files, but
    now these tests are condensed into 3 files that contain all tests for
    each cat option, which are cat, cat -v, and cat -e. Also made some minor
    syntax changes and added clarifying comments.

--- llvm/utils/lit/tests/shtest-cat.py
+++ llvm/utils/lit/tests/shtest-cat.py
@@ -1,11 +1,11 @@
-# Check the cat command
+## Test the cat command
 
 # RUN: not %{lit} -a -v %{inputs}/shtest-cat \
 # RUN: | FileCheck -match-full-lines %s
 #
 # END.
 
-# CHECK: FAIL: shtest-cat :: cat-e-allchars.txt ({{[^)]*}})
+# CHECK: FAIL: shtest-cat :: cat-e.txt ({{[^)]*}})
 # CHECK: cat -e {{.+}}/allchars | FileCheck {{.*}}
 # CHECK: # executed command: cat -e {{.*}}
 # CHECK: # | Unsupported: 'cat':  option -e not recognized
@@ -13,20 +13,15 @@
 # CHECK: # executed command: FileCheck {{.*}}
 # CHECK: # error: command failed with exit status: {{.*}}
 
-# CHECK: FAIL: shtest-cat :: cat-e-newline.txt ({{[^)]*}})
-# CHECK: cat -e {{.+}}/newline | FileCheck {{.*}}
-# CHECK: # executed command: cat -e {{.*}}
-# CHECK: # | Unsupported: 'cat':  option -e not recognized
-# CHECK: # error: command failed with exit status: {{.*}}
-# CHECK: # executed command: FileCheck {{.*}}
-# CHECK: # error: command failed with exit status: {{.*}}
-
-# CHECK: PASS: shtest-cat :: cat-newline.txt ({{[^)]*}})
-# CHECK: cat {{.+}}/newline | FileCheck {{.*}}
-# CHECK: # executed command: cat {{.*}}
-# CHECK: # executed command: FileCheck {{.*}}
-
-# CHECK: PASS: shtest-cat :: cat-v-allchars.txt ({{[^)]*}})
+# CHECK: PASS: shtest-cat :: cat-v.txt ({{[^)]*}})
 # CHECK: cat -v {{.+}}/allchars | FileCheck {{.*}}
 # CHECK: # executed command: cat -v {{.*}}
+# CHECK: # executed command: FileCheck {{.*}}
+# CHECK: cat -v {{.+}}/newline | FileCheck {{.*}}
+# CHECK: # executed command: cat -v {{.*}}
+# CHECK: # executed command: FileCheck {{.*}}
+
+# CHECK: PASS: shtest-cat :: cat.txt ({{[^)]*}})
+# CHECK: cat {{.+}}/newline | FileCheck {{.*}}
+# CHECK: # executed command: cat {{.*}}
 # CHECK: # executed command: FileCheck {{.*}}
