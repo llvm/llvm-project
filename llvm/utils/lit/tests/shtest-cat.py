08082d1c3248cebb050c2032701abb6711bb625b
Connie Zhu <connieyzhu@google.com>
2024-08-02 21:29:32 +0000

    [llvm-lit] Changed CHECK directives for more check precision
    Made changes to CHECK directives to check output more precisely.

--- llvm/utils/lit/tests/shtest-cat.py
+++ llvm/utils/lit/tests/shtest-cat.py
@@ -1,13 +1,13 @@
 ## Test the cat command
 
 # RUN: not %{lit} -a -v %{inputs}/shtest-cat \
-# RUN: | FileCheck -match-full-lines %s --dump-input-context=1000
+# RUN: | FileCheck -match-full-lines %s
 #
 # END.
 
 # CHECK: FAIL: shtest-cat :: cat-e.txt ({{[^)]*}})
 # CHECK: cat -e {{.+}}/allchars | FileCheck {{.*}}
-# CHECK: # executed command: cat -e {{.*}}
+# CHECK: # executed command: cat -e {{.+}}/allchars
 # CHECK: # | Unsupported: 'cat':  option -e not recognized
 # CHECK: # error: command failed with exit status: {{.*}}
 # CHECK: # executed command: FileCheck {{.*}}
@@ -15,17 +15,17 @@
 
 # CHECK: PASS: shtest-cat :: cat-v.txt ({{[^)]*}})
 # CHECK: cat -v {{.+}}/allchars | FileCheck {{.*}}
-# CHECK: # executed command: cat -v {{.*}}
-# CHECK: # executed command: FileCheck {{.*}}
+# CHECK-NEXT: # executed command: cat -v {{.+}}/allchars
+# CHECK-NEXT: # executed command: FileCheck {{.*}}
 # CHECK: cat -v {{.+}}/newline | FileCheck {{.*}}
-# CHECK: # executed command: cat -v {{.*}}
-# CHECK: # executed command: FileCheck {{.*}}
+# CHECK-NEXT: # executed command: cat -v {{.+}}/newline 
+# CHECK-NEXT: # executed command: FileCheck {{.*}}
 
 # CHECK: PASS: shtest-cat :: cat.txt ({{[^)]*}})
 # CHECK: cat {{.+}}/newline | FileCheck {{.*}}
-# CHECK: # executed command: cat {{.*}}
-# CHECK: # executed command: FileCheck {{.*}}
-# CHECK: cat {{.+}}/allchars > {{.*}}
-# CHECK: # executed command: cat {{.*}}
-# CHECK: diff {{.+}}/allchars {{.*}}
-# CHECK: # executed command: diff {{.*}}
+# CHECK-NEXT: # executed command: cat {{.+}}/newline
+# CHECK-NEXT: # executed command: FileCheck {{.*}}
+# CHECK: cat {{.+}}/allchars > {{.+}}
+# CHECK-NEXT: # executed command: cat {{.+}}/allchars
+# CHECK: diff {{.+}}/allchars {{.+}}
+# CHECK-NEXT: # executed command: diff {{.+}}/allchars {{.+}}
