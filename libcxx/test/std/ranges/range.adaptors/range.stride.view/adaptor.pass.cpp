41ab53a0c890129d753cda8109c03264cf444cd2
Will Hawkins <hawkinsw@obs.cr>
2024-08-03 01:47:13 -0400

    fixup! WIP: [libc++][ranges] Implement `ranges::stride_view`.
    Format.
    
    Signed-off-by: Will Hawkins <hawkinsw@obs.cr>

--- libcxx/test/std/ranges/range.adaptors/range.stride.view/adaptor.pass.cpp
+++ libcxx/test/std/ranges/range.adaptors/range.stride.view/adaptor.pass.cpp
@@ -134,7 +134,9 @@ constexpr bool test() {
   }
 
   // A final sanity check.
-  { static_assert(std::same_as<decltype(std::views::stride), decltype(std::ranges::views::stride)>); }
+  {
+    static_assert(std::same_as<decltype(std::views::stride), decltype(std::ranges::views::stride)>);
+  }
 
   return true;
 }
