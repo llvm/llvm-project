3e9cdc6c4b609715bed5681792ab70f004fdcf0e
OverMighty <its.overmighty@gmail.com>
2024-08-03 20:21:25 +0200

    Restore yaml_to_classes.py and set executable bit on it

--- libc/newhdrgen/yaml_to_classes.py
+++ libc/newhdrgen/yaml_to_classes.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+#
 # ===- Generate headers for libc functions  -------------------*- python -*--==#
 #
 # Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
