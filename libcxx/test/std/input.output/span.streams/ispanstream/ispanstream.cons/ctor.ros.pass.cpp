d75e22120d598d9a7b1c9a6d14236353ce7a3834
Hristo Hristov <hghristov.rmm@gmail.com>
2024-08-05 02:34:37 +0300

    Updated libcxx/test/std/input.output/span.streams/ispanstream/ispanstream.cons/ctor.ros.pass.cpp

--- libcxx/test/std/input.output/span.streams/ispanstream/ispanstream.cons/ctor.ros.pass.cpp
+++ libcxx/test/std/input.output/span.streams/ispanstream/ispanstream.cons/ctor.ros.pass.cpp
@@ -29,13 +29,6 @@
 
 #include "../../helper_types.h"
 
-#include <string_view>
-
-using namespace std::string_view_literals;
-
-template <class CharT>
-constexpr auto input_view = "1 2 3 4 5"sv;
-
 template <typename CharT, typename TraitsT = std::char_traits<CharT>>
 void test_sfinae() {
   using SpStream = std::basic_ispanstream<CharT, TraitsT>;
@@ -61,64 +54,54 @@ template <typename CharT, typename TraitsT = std::char_traits<CharT>>
 void test() {
   using SpStream = std::basic_ispanstream<CharT, TraitsT>;
 
-  // TODO:
-  CharT arr[4];
-  ReadOnlySpan<CharT, 4> ros{arr};
+  CharT arr1[4];
+  ReadOnlySpan<CharT, 4> ros{arr1};
   assert(ros.size() == 4);
 
   {
     SpStream spSt(ros);
-    assert(spSt.span().data() == arr);
+    assert(spSt.span().data() == arr1);
     assert(!spSt.span().empty());
     assert(spSt.span().size() == 4);
   }
 
-  CharT arr1[6];
-  ReadOnlySpan<CharT, 6> ros2{arr1};
+  CharT arr2[6];
+  ReadOnlySpan<CharT, 6> ros2{arr2};
 
   {
     SpStream spSt(ros2);
-    assert(spSt.span().data() != arr);
-    assert(spSt.span().data() == arr1);
+    assert(spSt.span().data() != arr1);
+    assert(spSt.span().data() == arr2);
     assert(!spSt.span().empty());
     assert(spSt.span().size() == 6);
-  }
-
-  {
-    auto r = input_view<char>;
-    SpStream spSt{r};
-    assert(spSt.span().data() != arr);
-    assert(spSt.span().data() != arr1);
-    assert(!spSt.span().empty());
-    assert(spSt.span().size() == 9);
   }
 }
 
 int main(int, char**) {
-// #ifndef TEST_HAS_NO_NASTY_STRING
-//   test_sfinae<nasty_char, nasty_char_traits>();
-// #endif
-//   test_sfinae<char>();
-//   test_sfinae<char, constexpr_char_traits<char>>();
-// #ifndef TEST_HAS_NO_NASTY_STRING
-//   test<nasty_char, nasty_char_traits>();
-// #endif
+#ifndef TEST_HAS_NO_NASTY_STRING
+  test_sfinae<nasty_char, nasty_char_traits>();
+#endif
+  test_sfinae<char>();
+  test_sfinae<char, constexpr_char_traits<char>>();
+#ifndef TEST_HAS_NO_NASTY_STRING
+  test<nasty_char, nasty_char_traits>();
+#endif
 
   test<char>();
-//   test<char, constexpr_char_traits<char>>();
-// #ifndef TEST_HAS_NO_WIDE_CHARACTERS
-//   test<wchar_t>();
-//   test<wchar_t, constexpr_char_traits<wchar_t>>();
-// #endif
+  test<char, constexpr_char_traits<char>>();
+#ifndef TEST_HAS_NO_WIDE_CHARACTERS
+  test<wchar_t>();
+  test<wchar_t, constexpr_char_traits<wchar_t>>();
+#endif
 
-// #ifndef TEST_HAS_NO_CHAR8_T
-//   test<char8_t>();
-//   test<char8_t, constexpr_char_traits<char8_t>>();
-// #endif
-//   test<char16_t>();
-//   test<char16_t, constexpr_char_traits<char16_t>>();
-//   test<char32_t>();
-//   test<char32_t, constexpr_char_traits<char32_t>>();
+#ifndef TEST_HAS_NO_CHAR8_T
+  test<char8_t>();
+  test<char8_t, constexpr_char_traits<char8_t>>();
+#endif
+  test<char16_t>();
+  test<char16_t, constexpr_char_traits<char16_t>>();
+  test<char32_t>();
+  test<char32_t, constexpr_char_traits<char32_t>>();
 
   return 0;
 }
