fb767b926b579e5de999f8937eda0c5e2ef7c87d
Hristo Hristov <hghristov.rmm@gmail.com>
2024-08-05 13:16:52 +0300

    Removed constexpr_char_traits from tests

--- libcxx/test/std/input.output/span.streams/ispanstream/inherited.stream.ops.pass.cpp
+++ libcxx/test/std/input.output/span.streams/ispanstream/inherited.stream.ops.pass.cpp
@@ -16,8 +16,6 @@
 
 //   Test stream operations inherited from `basic_istream` and `basic_ostream`
 
-#include <print>
-
 #include <cassert>
 #include <span>
 #include <spanstream>
@@ -76,27 +74,11 @@ void test() {
     int i3;
     spSt >> i3;
 
-    // TODO: Subsequent reads with constexpr_char_traits return empty string:
-    // # .---command stderr------------
-    // # | -----> zmt
-    // # | -----> 94
-    // # | -----> hkt
-    // # | -----> 82
-    // # | -----> pir
-    // # | -----> zmt
-    // # | -----> 94
-    // # | ----->
-    // # | -----> 82
     assert(str1 == CS("zmt"));
-    std::println(stderr, "-----> {}", str1);
     assert(i1 == 94);
-    std::println(stderr, "-----> {}", i1);
-    std::println(stderr, "-----> {}", str2);
-    // assert(str2 == CS("hkt"));
+    assert(str2 == CS("hkt"));
     assert(i2 == 82);
-    std::println(stderr, "-----> {}", i2);
     assert(str3 == CS("pir"));
-    std::println(stderr, "-----> {}", str3);
     assert(i3 == 43);
 
     assert(spSt);
@@ -245,10 +227,8 @@ void test() {
 
 int main(int, char**) {
   test<char>();
-  test<char, constexpr_char_traits<char>>();
 #ifndef TEST_HAS_NO_WIDE_CHARACTERS
-  // test<wchar_t>();
-  // test<wchar_t, constexpr_char_traits<wchar_t>>();
+  test<wchar_t>();
 #endif
 
   return 0;
