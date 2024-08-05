7fd7b5fe4c93a8f2957867742304c7737be10aa3
Daniel Thornburgh <dthorn@google.com>
2024-08-05 13:06:45 -0700

    [LLD] Add CLASS syntax to SECTIONS (#95323)
    This allows the input section matching algorithm to be separated from
    output section descriptions. This allows a group of sections to be
    assigned to multiple output sections, providing an explicit version of
    --enable-non-contiguous-regions's spilling that doesn't require altering
    global linker script matching behavior with a flag. It also makes the
    linker script language more expressive even if spilling is not intended,
    since input section matching can be done in a different order than
    sections are placed in an output section.
    
    The implementation reuses the backend mechanism provided by
    --enable-non-contiguous-regions, so it has roughly similar semantics and
    limitations. In particular, sections cannot be spilled into or out of
    INSERT, OVERWRITE_SECTIONS, or /DISCARD/. The former two aren't
    intrinsic, so it may be possible to relax those restrictions later.

--- lld/ELF/InputSection.cpp
+++ lld/ELF/InputSection.cpp
@@ -194,6 +194,8 @@ uint64_t SectionBase::getOffset(uint64_t offset) const {
     // For output sections we treat offset -1 as the end of the section.
     return offset == uint64_t(-1) ? os->size : offset;
   }
+  case Class:
+    llvm_unreachable("section classes do not have offsets");
   case Regular:
   case Synthetic:
   case Spill:
