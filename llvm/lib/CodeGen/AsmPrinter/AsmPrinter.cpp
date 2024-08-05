450ade1d9523386c192f2ec05391e187018c9d7f
Nabeel Omer <Nabeel.Omer@sony.com>
2024-08-05 12:29:19 +0100

    Fix formatting

--- llvm/lib/CodeGen/AsmPrinter/AsmPrinter.cpp
+++ llvm/lib/CodeGen/AsmPrinter/AsmPrinter.cpp
@@ -2777,8 +2777,8 @@ void AsmPrinter::emitJumpTableInfo() {
     StringRef GroupName = F.hasComdat() ? F.getComdat()->getName() : "";
 
     MCSection *JumpTableSizesSection = OutContext.getELFSection(
-        ".debug_llvm_jump_table_sizes", ELF::SHT_LLVM_JT_SIZES, Flags, 0, GroupName,
-        F.hasComdat(), MCSection::NonUniqueID, LinkedToSym);
+        ".debug_llvm_jump_table_sizes", ELF::SHT_LLVM_JT_SIZES, Flags, 0,
+        GroupName, F.hasComdat(), MCSection::NonUniqueID, LinkedToSym);
 
     OutStreamer->switchSection(JumpTableSizesSection);
 
