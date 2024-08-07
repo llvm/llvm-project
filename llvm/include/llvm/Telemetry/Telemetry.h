6e43f67c9f0412fbf0f4a16f2be68daa40d4b6f4
Vy Nguyen <vyng@google.com>
2024-08-07 13:21:32 -0400

    add header

--- llvm/include/llvm/Telemetry/Telemetry.h
+++ llvm/include/llvm/Telemetry/Telemetry.h
@@ -1,3 +1,15 @@
+//===- llvm/Telemetry/Telemetry.h - Telemetry -------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// Defines the commom Telemetry framework.
+//
+//===----------------------------------------------------------------------===//
+
 #ifndef LVM_TELEMETRY_TELEMETRY_H
 #define LVM_TELEMETRY_TELEMETRY_H
 
