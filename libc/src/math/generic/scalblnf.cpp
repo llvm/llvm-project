d67e1fae32c67f1906f581cbacd022884a58a3e0
aaryanshukla <53713108+aaryanshukla@users.noreply.github.com>
2024-08-07 09:45:13 -0700

    Update libc/src/math/generic/scalblnf.cpp
    Co-authored-by: OverMighty <its.overmighty@gmail.com>

--- libc/src/math/generic/scalblnf.cpp
+++ libc/src/math/generic/scalblnf.cpp
@@ -11,7 +11,6 @@
 #include "src/__support/FPUtil/ManipulationFunctions.h"
 #include "src/__support/common.h"
 #include "src/__support/macros/config.h"
-#include "src/__support/macros/properties/types.h"
 
 #if FLT_RADIX != 2
 #error "FLT_RADIX != 2 is not supported."
