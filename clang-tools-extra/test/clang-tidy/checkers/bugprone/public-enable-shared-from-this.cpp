acf73fd6f8c9288e33416d0659bfb7bc9edf3a62
Michelle C Djunaidi <michellechrisalyn@gmail.com>
2024-08-07 17:58:42 +0800

    Fix test to be compatible with llvm-lit, add docs

--- clang-tools-extra/test/clang-tidy/checkers/bugprone/public-enable-shared-from-this.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/bugprone/public-enable-shared-from-this.cpp
@@ -1,56 +1,45 @@
-// RUN: %check_clang_tidy %s bugprone-public-enable-shared-from-this %t -- -- -I %S/Inputs/
-#include <memory>
+// RUN: %check_clang_tidy %s bugprone-public-enable-shared-from-this %t -- --
 
-class BadExample : std::enable_shared_from_this<BadExample> {
-// CHECK-MESSAGES: :[[@LINE-1]]:7: warning: class BadExample is not public even though it's derived from std::enable_shared_from_this [bugprone-public-enable-shared-from-this]
-// CHECK-FIXES: :[[@LINE-2]]:19 public 
-        public:
-        BadExample* foo() { return shared_from_this().get(); }
-        void bar() { return; }
-};
+namespace std {
 
-void using_not_public() {
-        auto bad_example = std::make_shared<BadExample>();
-        auto* b_ex = bad_example->foo();
-        b_ex->bar();
-}
+    template <typename T> class enable_shared_from_this {};
 
-class GoodExample : public std::enable_shared_from_this<GoodExample> {
-        public:
-        GoodExample* foo() { return shared_from_this().get(); }
-        void bar() { return; }
-};
+    class BadExample : enable_shared_from_this<BadExample> {};
+    // CHECK-MESSAGES: :[[@LINE-1]]:7: warning: class BadExample is not public even though it's derived from std::enable_shared_from_this [bugprone-public-enable-shared-from-this]
+    // CHECK-FIXES: public enable_shared_from_this<BadExample>
 
-void using_public() {
-        auto good_example = std::make_shared<GoodExample>();
-        auto* g_ex = good_example->foo();
-        g_ex->bar();
-}
+    class Bad2Example : std::enable_shared_from_this<Bad2Example> {};
+    // CHECK-MESSAGES: :[[@LINE-1]]:7: warning: class Bad2Example is not public even though it's derived from std::enable_shared_from_this [bugprone-public-enable-shared-from-this]
+    // CHECK-FIXES: public std::enable_shared_from_this<Bad2Example>
 
-struct BaseClass {
+    class GoodExample : public enable_shared_from_this<GoodExample> {
+    };
 
-    void print() {
-        (void) State;
-        (void) Requester;
-    }
-    bool State;
-    int Requester;
-};
+    struct BaseClass {
 
-class InheritPrivateBaseClass : BaseClass {
-    public:
-        void additionalFunction() {
-            (void) ID;
+        void print() {
+            (void) State;
+            (void) Requester;
         }
-    private: 
-        int ID;
-};
+        bool State;
+        int Requester;
+    };
 
-class InheritPublicBaseClass : public BaseClass {
-    public:
-        void additionalFunction() {
-            (void) ID;
-        }
-    private: 
-        int ID;
-};
+    class InheritPrivateBaseClass : BaseClass {
+        public:
+            void additionalFunction() {
+                (void) ID;
+            }
+        private: 
+            int ID;
+    };
+
+    class InheritPublicBaseClass : public BaseClass {
+        public:
+            void additionalFunction() {
+                (void) ID;
+            }
+        private: 
+            int ID;
+    };
+}
