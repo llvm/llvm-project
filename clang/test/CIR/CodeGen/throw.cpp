bb4b7151ac19998495117215924c516c05451943
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2024-08-08 16:30:21 -0700

    [CIR][CIRGen][NFC] Tide up cir.alloc.exception

--- clang/test/CIR/CodeGen/throw.cpp
+++ clang/test/CIR/CodeGen/throw.cpp
@@ -8,7 +8,7 @@ double d(int a, int b) {
 }
 
 //      CHECK: cir.if %10 {
-// CHECK-NEXT:   %11 = cir.alloc_exception(!cir.ptr<!s8i>) -> <!cir.ptr<!s8i>>
+// CHECK-NEXT:   %11 = cir.alloc.exception 8
 // CHECK-NEXT:   %12 = cir.get_global @".str" : !cir.ptr<!cir.array<!s8i x 28>>
 // CHECK-NEXT:   %13 = cir.cast(array_to_ptrdecay, %12 : !cir.ptr<!cir.array<!s8i x 28>>), !cir.ptr<!s8i>
 // CHECK-NEXT:   cir.store %13, %11 : !cir.ptr<!s8i>, !cir.ptr<!cir.ptr<!s8i>>
