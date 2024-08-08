f14464c0734776cd79a2fbd93cec94e815276009
yronglin <yronglin777@gmail.com>
2024-08-09 01:22:29 +0800

    [Clang][Interp] Fix display of syntactically-invalid note for member function calls
    Signed-off-by: yronglin <yronglin777@gmail.com>

--- clang/test/AST/Interp/lambda.cpp
+++ clang/test/AST/Interp/lambda.cpp
@@ -46,7 +46,7 @@ constexpr int div(int a, int b) {
     return a / b; // both-note {{division by zero}}
   };
 
-  return f(); // expected-note {{in call to '&f->operator()()'}} \
+  return f(); // expected-note {{in call to 'f.operator()()'}} \
               // ref-note {{in call to 'f.operator()()'}}
 }
 static_assert(div(8, 2) == 4);
