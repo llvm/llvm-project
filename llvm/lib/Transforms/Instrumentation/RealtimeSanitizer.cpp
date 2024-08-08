06ce2d22f3d5e8c91029857542cf75ed5550ff40
Chris Apple <cja-private@pm.me>
2024-08-08 16:09:04 -0700

    Add file header for RealtimeSanitizer.cpp

--- llvm/lib/Transforms/Instrumentation/RealtimeSanitizer.cpp
+++ llvm/lib/Transforms/Instrumentation/RealtimeSanitizer.cpp
@@ -1,3 +1,18 @@
+//===- RealtimeSanitizer.cpp - RealtimeSanitizer instrumentation *- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file is a part of the RealtimeSanitizer, an LLVM transformation for
+// detecting and reporting realtime safety violations.
+//
+// See also: llvm-project/compiler-rt/lib/rtsan/
+//
+//===----------------------------------------------------------------------===//
+
 #include "llvm/IR/Analysis.h"
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/Module.h"
