0e124537aabea0d18e030226a3ca4c6f494c1322
David Green <david.green@arm.com>
2024-08-09 11:48:37 +0100

    [AArch64] Sink operands to fmuladd. (#102297)
    A fmuladd can be treated as a fma when sinking operands to the
    intrinsic, similar to D126234.
    
    Addresses a small part of #102195

--- llvm/lib/Target/AArch64/AArch64ISelLowering.cpp
+++ llvm/lib/Target/AArch64/AArch64ISelLowering.cpp
@@ -16174,6 +16174,7 @@ bool AArch64TargetLowering::shouldSinkOperands(
       [[fallthrough]];
 
     case Intrinsic::fma:
+    case Intrinsic::fmuladd:
       if (isa<VectorType>(I->getType()) &&
           cast<VectorType>(I->getType())->getElementType()->isHalfTy() &&
           !Subtarget->hasFullFP16())
