0a2a319ea6c8dc59fd3f0e284766361b8deca51c
Chris Apple <cja-private@pm.me>
2024-08-11 15:58:49 -0700

    [rtsan] Make sure rtsan gets initialized on mac (#100188)
    Intermittently on my mac I was getting the same nullptr crash in dlsym.
    
    We need to make sure rtsan gets initialized on mac between when the
    binary starts running, and the first intercepted function is called.
    Until that point we should use the DlsymAllocator.

--- compiler-rt/lib/rtsan/rtsan.cpp
+++ compiler-rt/lib/rtsan/rtsan.cpp
@@ -12,23 +12,42 @@
 #include <rtsan/rtsan_context.h>
 #include <rtsan/rtsan_interceptors.h>
 
-using namespace __rtsan;
+#include "sanitizer_common/sanitizer_atomic.h"
+#include "sanitizer_common/sanitizer_mutex.h"
 
-bool __rtsan::rtsan_initialized;
-bool __rtsan::rtsan_init_is_running;
+using namespace __rtsan;
+using namespace __sanitizer;
+
+static StaticSpinMutex rtsan_inited_mutex;
+static atomic_uint8_t rtsan_initialized = {0};
+
+static void SetInitialized() {
+  atomic_store(&rtsan_initialized, 1, memory_order_release);
+}
 
 extern "C" {
 
 SANITIZER_INTERFACE_ATTRIBUTE void __rtsan_init() {
-  CHECK(!rtsan_init_is_running);
-  if (rtsan_initialized)
-    return;
-  rtsan_init_is_running = true;
-
+  CHECK(!__rtsan_is_initialized());
   InitializeInterceptors();
+  SetInitialized();
+}
 
-  rtsan_init_is_running = false;
-  rtsan_initialized = true;
+SANITIZER_INTERFACE_ATTRIBUTE void __rtsan_ensure_initialized() {
+  if (LIKELY(__rtsan_is_initialized()))
+    return;
+
+  SpinMutexLock lock(&rtsan_inited_mutex);
+
+  // Someone may have initialized us while we were waiting for the lock
+  if (__rtsan_is_initialized())
+    return;
+
+  __rtsan_init();
+}
+
+SANITIZER_INTERFACE_ATTRIBUTE bool __rtsan_is_initialized() {
+  return atomic_load(&rtsan_initialized, memory_order_acquire) == 1;
 }
 
 SANITIZER_INTERFACE_ATTRIBUTE void __rtsan_realtime_enter() {
