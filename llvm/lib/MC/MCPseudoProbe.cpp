9746055b0a1ae1e7c6aff50fc217dc216605c277
Amir Ayupov <aaupov@fb.com>
2024-08-10 23:48:40 -0700

    [MC][profgen][NFC] Expand auto for MCDecodedPseudoProbe
    Expand autos in select places in preparation to #102789.
    
    Reviewers: dcci, maksfb, WenleiHe, rafaelauler, ayermolo, wlei-llvm
    
    Reviewed By: WenleiHe, wlei-llvm
    
    Pull Request: https://github.com/llvm/llvm-project/pull/102788

--- llvm/lib/MC/MCPseudoProbe.cpp
+++ llvm/lib/MC/MCPseudoProbe.cpp
@@ -652,7 +652,7 @@ void MCPseudoProbeDecoder::printProbeForAddress(raw_ostream &OS,
                                                 uint64_t Address) {
   auto It = Address2ProbesMap.find(Address);
   if (It != Address2ProbesMap.end()) {
-    for (auto &Probe : It->second) {
+    for (const MCDecodedPseudoProbe &Probe : It->second) {
       OS << " [Probe]:\t";
       Probe.print(OS, GUID2FuncDescMap, true);
     }
@@ -679,7 +679,7 @@ MCPseudoProbeDecoder::getCallProbeForAddr(uint64_t Address) const {
   const auto &Probes = It->second;
 
   const MCDecodedPseudoProbe *CallProbe = nullptr;
-  for (const auto &Probe : Probes) {
+  for (const MCDecodedPseudoProbe &Probe : Probes) {
     if (Probe.isCall()) {
       // Disabling the assert and returning first call probe seen so far.
       // Subsequent call probes, if any, are ignored. Due to the the way
