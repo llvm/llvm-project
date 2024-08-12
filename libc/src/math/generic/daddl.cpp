13825f2a9f6cffd1da4bb4d95d41735b0e57b358
Joseph Huber <huberjn@outlook.com>
2024-08-12 13:16:38 -0500

    [libc] Undo accidental changes to `dsubl` that were leftoever

--- libc/src/math/generic/daddl.cpp
+++ libc/src/math/generic/daddl.cpp
@@ -10,16 +10,11 @@
 #include "src/__support/FPUtil/generic/add_sub.h"
 #include "src/__support/common.h"
 #include "src/__support/macros/config.h"
-#include "src/__support/macros/properties/types.h"
 
 namespace LIBC_NAMESPACE_DECL {
 
 LLVM_LIBC_FUNCTION(double, daddl, (long double x, long double y)) {
-#ifdef LIBC_TYPES_LONG_DOUBLE_IS_FLOAT64
-  return static_cast<double>(x) + static_cast<double>(y);
-#else
   return fputil::generic::add<double>(x, y);
-#endif
 }
 
 } // namespace LIBC_NAMESPACE_DECL
