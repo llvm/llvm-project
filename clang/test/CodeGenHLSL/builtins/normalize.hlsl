d0bea332d689db1b26dac9ce5c05289f1ffe2338
Joshua Batista <jbatista@microsoft.com>
2024-08-12 22:32:27 -0700

    fix whitespace

--- clang/test/CodeGenHLSL/builtins/normalize.hlsl
+++ clang/test/CodeGenHLSL/builtins/normalize.hlsl
@@ -13,7 +13,7 @@
 // NO_HALF: ret float
 half test_normalize_half(half p0)
 {
-	return normalize(p0);
+    return normalize(p0);
 }
 // NATIVE_HALF: define noundef <2 x half> @
 // NATIVE_HALF: %hlsl.normalize = call <2 x half> @llvm.dx.normalize.v2f16
@@ -22,7 +22,7 @@ half test_normalize_half(half p0)
 // NO_HALF: ret <2 x float> %hlsl.normalize
 half2 test_normalize_half2(half2 p0)
 {
-	return normalize(p0);
+    return normalize(p0);
 }
 // NATIVE_HALF: define noundef <3 x half> @
 // NATIVE_HALF: %hlsl.normalize = call <3 x half> @llvm.dx.normalize.v3f16
@@ -31,7 +31,7 @@ half2 test_normalize_half2(half2 p0)
 // NO_HALF: ret <3 x float> %hlsl.normalize
 half3 test_normalize_half3(half3 p0)
 {
-	return normalize(p0);
+    return normalize(p0);
 }
 // NATIVE_HALF: define noundef <4 x half> @
 // NATIVE_HALF: %hlsl.normalize = call <4 x half> @llvm.dx.normalize.v4f16
@@ -40,7 +40,7 @@ half3 test_normalize_half3(half3 p0)
 // NO_HALF: ret <4 x float> %hlsl.normalize
 half4 test_normalize_half4(half4 p0)
 {
-	return normalize(p0);
+    return normalize(p0);
 }
 
 // CHECK: define noundef float @
@@ -48,26 +48,26 @@ half4 test_normalize_half4(half4 p0)
 // CHECK: ret float
 float test_normalize_float(float p0)
 {
-	return normalize(p0);
+    return normalize(p0);
 }
 // CHECK: define noundef <2 x float> @
 // CHECK: %hlsl.normalize = call <2 x float> @llvm.dx.normalize.v2f32(
 // CHECK: ret <2 x float> %hlsl.normalize
 float2 test_normalize_float2(float2 p0)
 {
-	return normalize(p0);
+    return normalize(p0);
 }
 // CHECK: define noundef <3 x float> @
 // CHECK: %hlsl.normalize = call <3 x float> @llvm.dx.normalize.v3f32(
 // CHECK: ret <3 x float> %hlsl.normalize
 float3 test_normalize_float3(float3 p0)
 {
-	return normalize(p0);
+    return normalize(p0);
 }
 // CHECK: define noundef <4 x float> @
 // CHECK: %hlsl.normalize = call <4 x float> @llvm.dx.normalize.v4f32(
 // CHECK: ret <4 x float> %hlsl.normalize
 float4 test_length_float4(float4 p0)
 {
-	return normalize(p0);
+    return normalize(p0);
 }
