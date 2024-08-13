bfd43c4b79366f0ab989f516a9200965fac5f66a
Igor Kudrin <ikudrin@accesssoftek.com>
2024-08-13 12:54:28 -0700

    AsteriskWildcard -> Asterisk, update the comment

--- lld/ELF/SymbolTable.cpp
+++ lld/ELF/SymbolTable.cpp
@@ -311,33 +311,32 @@ void SymbolTable::scanVersionScript() {
 
   // Then, assign versions to "*". In GNU linkers they have lower priority than
   // other wildcards.
-  bool globalAsteriskWildcardFound = false;
-  bool localAsteriskWildcardFound = false;
-  bool asteriskWildcardReported = false;
-  auto assignAsteriskWildcard = [&](SymbolVersion &pat, VersionDefinition *ver,
-                                    bool isLocal) {
+  bool globalAsteriskFound = false;
+  bool localAsteriskFound = false;
+  bool asteriskReported = false;
+  auto assignAsterisk = [&](SymbolVersion &pat, VersionDefinition *ver,
+                            bool isLocal) {
     // Avoid issuing a warning if both '--retain-symbol-file' and a version
     // script with `global: *` are used.
     //
     // '--retain-symbol-file' adds a "*" pattern to
     // 'config->versionDefinitions[VER_NDX_LOCAL].nonLocalPatterns', see
-    // 'readConfigs()' in 'Driver.cpp'. Note that it is '.nonLocalPatterns', not
-    // '.localPatterns', which may seem counterintuitive, but still works as
-    // expected. Here we can exploit that and skip analyzing the pattern added
-    // for this option.
-    if (!asteriskWildcardReported && (isLocal || ver->id > VER_NDX_LOCAL)) {
-      if ((isLocal && globalAsteriskWildcardFound) ||
-          (!isLocal && localAsteriskWildcardFound)) {
+    // 'readConfigs()' in 'Driver.cpp'. Note that it is not '.localPatterns',
+    // and may seem counterintuitive, but still works as expected. Here we can
+    // exploit that and skip analyzing the pattern added for this option.
+    if (!asteriskReported && (isLocal || ver->id > VER_NDX_LOCAL)) {
+      if ((isLocal && globalAsteriskFound) ||
+          (!isLocal && localAsteriskFound)) {
         warn("wildcard pattern '*' is used for both 'local' and 'global' "
              "scopes in version script");
-        asteriskWildcardReported = true;
-      } else if (!isLocal && globalAsteriskWildcardFound) {
+        asteriskReported = true;
+      } else if (!isLocal && globalAsteriskFound) {
         warn("wildcard pattern '*' is used for multiple version definitions in "
              "version script");
-        asteriskWildcardReported = true;
+        asteriskReported = true;
       } else {
-        localAsteriskWildcardFound = isLocal;
-        globalAsteriskWildcardFound = !isLocal;
+        localAsteriskFound = isLocal;
+        globalAsteriskFound = !isLocal;
       }
     }
     assignWildcard(pat, isLocal ? VER_NDX_LOCAL : ver->id, ver->name);
@@ -345,10 +344,10 @@ void SymbolTable::scanVersionScript() {
   for (VersionDefinition &v : llvm::reverse(config->versionDefinitions)) {
     for (SymbolVersion &pat : v.nonLocalPatterns)
       if (pat.hasWildcard && pat.name == "*")
-        assignAsteriskWildcard(pat, &v, false);
+        assignAsterisk(pat, &v, false);
     for (SymbolVersion &pat : v.localPatterns)
       if (pat.hasWildcard && pat.name == "*")
-        assignAsteriskWildcard(pat, &v, true);
+        assignAsterisk(pat, &v, true);
   }
 
   // Symbol themselves might know their versions because symbols
