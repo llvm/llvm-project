5d489b80d78a97a48cebee258c65faabf4f71ac5
MichelleCDjunaidi <87893361+MichelleCDjunaidi@users.noreply.github.com>
2024-08-14 18:08:52 +0800

    Fix check code formatting to follow LLVM style

--- clang-tools-extra/clang-tidy/bugprone/IncorrectEnableSharedFromThisCheck.cpp
+++ clang-tools-extra/clang-tidy/bugprone/IncorrectEnableSharedFromThisCheck.cpp
@@ -7,8 +7,8 @@
 //===----------------------------------------------------------------------===//
 
 #include "IncorrectEnableSharedFromThisCheck.h"
-#include "clang/AST/DeclCXX.h"
 #include "clang/AST/ASTContext.h"
+#include "clang/AST/DeclCXX.h"
 #include "clang/AST/RecursiveASTVisitor.h"
 #include "clang/ASTMatchers/ASTMatchFinder.h"
 #include "clang/Basic/Specifiers.h"
@@ -18,21 +18,21 @@ using namespace clang::ast_matchers;
 
 namespace clang::tidy::bugprone {
 
-void IncorrectEnableSharedFromThisCheck::registerMatchers(
-    MatchFinder *Finder) {
+void IncorrectEnableSharedFromThisCheck::registerMatchers(MatchFinder *Finder) {
   Finder->addMatcher(translationUnitDecl(), this);
 }
 
 void IncorrectEnableSharedFromThisCheck::check(
-  const MatchFinder::MatchResult &Result) {
+    const MatchFinder::MatchResult &Result) {
 
   class Visitor : public RecursiveASTVisitor<Visitor> {
     IncorrectEnableSharedFromThisCheck &Check;
-    llvm::SmallPtrSet<const CXXRecordDecl*, 16> EnableSharedClassSet;
+    llvm::SmallPtrSet<const CXXRecordDecl *, 16> EnableSharedClassSet;
 
   public:
-    explicit Visitor(IncorrectEnableSharedFromThisCheck &Check) : Check(Check), EnableSharedClassSet(EnableSharedClassSet) {}
-  
+    explicit Visitor(IncorrectEnableSharedFromThisCheck &Check)
+        : Check(Check), EnableSharedClassSet(EnableSharedClassSet) {}
+
     bool VisitCXXRecordDecl(CXXRecordDecl *RDecl) {
       for (const auto &Base : RDecl->bases()) {
         VisitCXXBaseSpecifier(Base, RDecl);
@@ -44,29 +44,32 @@ void IncorrectEnableSharedFromThisCheck::check(
           EnableSharedClassSet.insert(RDecl->getCanonicalDecl());
         }
       }
-      return true;     
+      return true;
     }
 
-    bool VisitCXXBaseSpecifier(const CXXBaseSpecifier &Base, CXXRecordDecl *RDecl) {
+    bool VisitCXXBaseSpecifier(const CXXBaseSpecifier &Base,
+                               CXXRecordDecl *RDecl) {
       const CXXRecordDecl *BaseType = Base.getType()->getAsCXXRecordDecl();
       const clang::AccessSpecifier AccessSpec = Base.getAccessSpecifier();
 
-      if ( BaseType && BaseType->getQualifiedNameAsString() ==
-                          "enable_shared_from_this") {
+      if (BaseType &&
+          BaseType->getQualifiedNameAsString() == "enable_shared_from_this") {
         if (AccessSpec == clang::AS_public) {
           const SourceLocation InsertLocation = Base.getBaseTypeLoc();
           const FixItHint Hint = FixItHint::CreateInsertion(InsertLocation, "std::");
           Check.diag(RDecl->getLocation(),
-              "Should be std::enable_shared_from_this", DiagnosticIDs::Warning)
+                     "Should be std::enable_shared_from_this",
+                     DiagnosticIDs::Warning)
               << Hint;
-          
+
         } else {
           const SourceRange ReplacementRange = Base.getSourceRange();
           const std::string ReplacementString =
               "public std::" + Base.getType().getAsString();
           const FixItHint Hint =
               FixItHint::CreateReplacement(ReplacementRange, ReplacementString);
-          Check.diag(RDecl->getLocation(),
+          Check.diag(
+              RDecl->getLocation(),
               "Should be std::enable_shared_from_this and "
               "inheritance from std::enable_shared_from_this should be public "
               "inheritance, otherwise the internal weak_ptr won't be "
@@ -74,9 +77,8 @@ void IncorrectEnableSharedFromThisCheck::check(
               DiagnosticIDs::Warning)
               << Hint;
         }
-      }
-      else if ( BaseType && BaseType->getQualifiedNameAsString() ==
-                          "std::enable_shared_from_this") {
+      } else if (BaseType && BaseType->getQualifiedNameAsString() ==
+                                 "std::enable_shared_from_this") {
         if (AccessSpec != clang::AS_public) {
           const SourceRange ReplacementRange = Base.getSourceRange();
           const std::string ReplacementString =
@@ -86,12 +88,12 @@ void IncorrectEnableSharedFromThisCheck::check(
           Check.diag(
               RDecl->getLocation(),
               "inheritance from std::enable_shared_from_this should be public "
-              "inheritance, otherwise the internal weak_ptr won't be initialized",
+              "inheritance, otherwise the internal weak_ptr won't be "
+              "initialized",
               DiagnosticIDs::Warning)
               << Hint;
         }
-      } 
-      else if (EnableSharedClassSet.contains(BaseType->getCanonicalDecl())) {
+      } else if (EnableSharedClassSet.contains(BaseType->getCanonicalDecl())) {
         if (AccessSpec != clang::AS_public) {
           const SourceRange ReplacementRange = Base.getSourceRange();
           const std::string ReplacementString =
@@ -101,17 +103,17 @@ void IncorrectEnableSharedFromThisCheck::check(
           Check.diag(
               RDecl->getLocation(),
               "inheritance from std::enable_shared_from_this should be public "
-              "inheritance, otherwise the internal weak_ptr won't be initialized",
+              "inheritance, otherwise the internal weak_ptr won't be "
+              "initialized",
               DiagnosticIDs::Warning)
               << Hint;
-        }            
+        }
       }
       return true;
     }
   };
- 
+
   Visitor(*this).TraverseAST(*Result.Context);
-  
 }
 
 } // namespace clang::tidy::bugprone
