c278c1f1f5db5304a75a72327a3575bfb6c3b83d
Bill Wendling <morbo@google.com>
2024-08-15 14:25:09 -0700

    Add _Generic checks.

--- clang/test/Sema/builtin-get-counted-by.c
+++ clang/test/Sema/builtin-get-counted-by.c
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -fsyntax-only -verify %s
+// RUN: %clang_cc1 -std=c99 -fsyntax-only -verify %s
 
 struct fam_struct {
   int x;
@@ -25,4 +25,43 @@ void test1(int size) {
 
   __builtin_get_counted_by();                         // expected-error {{too few arguments to function call, expected 1, have 0}}
   __builtin_get_counted_by(p->array, p->x, p->count); // expected-error {{too many arguments to function call, expected 1, have 3}}
+}
+
+struct char_count {
+  char count;
+  int array[] __attribute__((counted_by(count)));
+} *cp;
+
+struct short_count {
+  short count;
+  int array[] __attribute__((counted_by(count)));
+} *sp;
+
+struct int_count {
+  int count;
+  int array[] __attribute__((counted_by(count)));
+} *ip;
+
+struct unsigned_count {
+  unsigned count;
+  int array[] __attribute__((counted_by(count)));
+} *up;
+
+struct long_count {
+  long count;
+  int array[] __attribute__((counted_by(count)));
+} *lp;
+
+struct unsigned_long_count {
+  unsigned long count;
+  int array[] __attribute__((counted_by(count)));
+} *ulp;
+
+void test2(void) {
+  _Static_assert(_Generic(__builtin_get_counted_by(cp->array), char * : 1, default : 0) == 1, "wrong return type");
+  _Static_assert(_Generic(__builtin_get_counted_by(sp->array), short * : 1, default : 0) == 1, "wrong return type");
+  _Static_assert(_Generic(__builtin_get_counted_by(ip->array), int * : 1, default : 0) == 1, "wrong return type");
+  _Static_assert(_Generic(__builtin_get_counted_by(up->array), unsigned int * : 1, default : 0) == 1, "wrong return type");
+  _Static_assert(_Generic(__builtin_get_counted_by(lp->array), long * : 1, default : 0) == 1, "wrong return type");
+  _Static_assert(_Generic(__builtin_get_counted_by(ulp->array), unsigned long * : 1, default : 0) == 1, "wrong return type");
 }
