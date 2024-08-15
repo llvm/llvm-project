4bf461678b6eb12da78c0ebcb793b120a2918843
Eduard Zingerman <eddyz87@gmail.com>
2024-08-15 12:28:05 -0700

    adjust Sema test case to show that bpf_fastcall is not a type attr
    As discussed in the pull request thread, it is not an error to assign
    pointer with bpf_fastcall attribute to a pointer w/o such attribute
    and vice-versa.
    (Hence, no need to track bpf_fastcall as a part of the type).

--- clang/test/Sema/bpf-attr-bpf-fastcall.c
+++ clang/test/Sema/bpf-attr-bpf-fastcall.c
@@ -5,3 +5,10 @@ __attribute__((bpf_fastcall)) int var; // expected-warning {{'bpf_fastcall' attr
 
 __attribute__((bpf_fastcall)) void func();
 __attribute__((bpf_fastcall(1))) void func_invalid(); // expected-error {{'bpf_fastcall' attribute takes no arguments}}
+
+void (*ptr1)(void) __attribute__((bpf_fastcall));
+void (*ptr2)(void);
+void foo(void) {
+  ptr2 = ptr1; // not an error
+  ptr1 = ptr2; // not an error
+}
