45dea561c6e90b78b0ead0607e1795498571229b
Fangrui Song <i@maskray.me>
2024-08-15 17:04:41 -0700

    [Driver] Improve error message for -Wa,-x=unknown

--- clang/test/Driver/relax.c
+++ clang/test/Driver/relax.c
@@ -4,7 +4,7 @@
 // CHECK: "-mrelax-relocations=no"
 
 // RUN: not %clang -### --target=x86_64 -c -Wa,-mrelax-relocations=x %s 2>&1 | FileCheck %s --check-prefix=ERR
-// ERR: error: unsupported argument '-mrelax-relocations=x' to option '-Wa,'
+// ERR: error: unsupported argument 'x' to option '-Wa,-mrelax-relocations='
 
 // RUN: not %clang -### --target=aarch64 -c -Wa,-mrelax-relocations=no %s 2>&1 | FileCheck %s --check-prefix=ERR2
 // ERR2: error: unsupported argument '-mrelax-relocations=no' to option '-Wa,'
