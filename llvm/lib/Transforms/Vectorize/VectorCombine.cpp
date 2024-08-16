a2889ee08b488fdf27cc9549bd93884b240ec3e5
Igor Kirillov <igor.kirillov@arm.com>
2024-08-16 15:06:00 +0000

    [VectorCombine] Add type shrinking and zext propagation for fixed-width vector types
    Check that binop(zext(value), other) is possible and profitable to transform
    into: zext(binop(value, trunc(other))).
    When CPU architecture has illegal scalar type iX, but vector type <N * iX> is
    legal, scalar expressions before vectorisation may be extended to a legal
    type iY. This extension could result in underutilization of vector lanes,
    as more lanes could be used at one instruction with the lower type.
    Vectorisers may not always recognize opportunities for type shrinking, and
    this patch aims to address that limitation.

--- llvm/lib/Transforms/Vectorize/VectorCombine.cpp
+++ llvm/lib/Transforms/Vectorize/VectorCombine.cpp
@@ -119,6 +119,7 @@ private:
   bool foldShuffleFromReductions(Instruction &I);
   bool foldCastFromReductions(Instruction &I);
   bool foldSelectShuffle(Instruction &I, bool FromReduction = false);
+  bool shrinkType(Instruction &I);
 
   void replaceValue(Value &Old, Value &New) {
     Old.replaceAllUsesWith(&New);
@@ -2493,6 +2494,106 @@ bool VectorCombine::foldSelectShuffle(Instruction &I, bool FromReduction) {
   return true;
 }
 
+/// Check if instruction depends on ZExt and this ZExt can be moved after the
+/// instruction. Move ZExt if it is profitable
+bool VectorCombine::shrinkType(llvm::Instruction &I) {
+  Value *ZExted, *OtherOperand;
+  if (match(&I, m_c_BinOp(m_ZExt(m_Value(ZExted)), m_Value(OtherOperand)))) {
+    if (I.getOpcode() != Instruction::And && I.getOpcode() != Instruction::Or &&
+        I.getOpcode() != Instruction::Xor && I.getOpcode() != Instruction::LShr)
+      return false;
+
+    // In case of LShr extraction, ZExtOperand should be applied to the first
+    // operand
+    if (I.getOpcode() == Instruction::LShr && I.getOperand(1) != OtherOperand)
+      return false;
+
+    Instruction *ZExtOperand = cast<Instruction>(
+        I.getOperand(I.getOperand(0) == OtherOperand ? 1 : 0));
+
+    auto *BigTy = cast<FixedVectorType>(I.getType());
+    auto *SmallTy = cast<FixedVectorType>(ZExted->getType());
+    auto BW = SmallTy->getElementType()->getPrimitiveSizeInBits();
+
+    // Check that the expression overall uses at most the same number of bits as
+    // ZExted
+    auto KB = computeKnownBits(&I, *DL);
+    auto IBW = KB.getBitWidth() - KB.Zero.countLeadingOnes();
+    if (IBW > BW)
+      return false;
+
+    bool HasUNZExtableUser = false;
+
+    // Calculate costs of leaving current IR as it is and moving ZExt operation
+    // later, along with adding truncates if needed
+    InstructionCost ZExtCost = TTI.getCastInstrCost(
+        Instruction::ZExt, BigTy, SmallTy,
+        TargetTransformInfo::CastContextHint::None, TTI::TCK_RecipThroughput);
+    InstructionCost CurrentCost = ZExtCost;
+    InstructionCost ShrinkCost = 0;
+
+    for (User *U : ZExtOperand->users()) {
+      auto *UI = cast<Instruction>(U);
+      if (UI == &I) {
+        CurrentCost += TTI.getArithmeticInstrCost(UI->getOpcode(), BigTy);
+        ShrinkCost += TTI.getArithmeticInstrCost(UI->getOpcode(), SmallTy);
+        ShrinkCost += ZExtCost;
+        continue;
+      }
+
+      if (!Instruction::isBinaryOp(UI->getOpcode())) {
+        HasUNZExtableUser = true;
+        continue;
+      }
+
+      // Check if we can propagate ZExt through its other users
+      auto KB = computeKnownBits(UI, *DL);
+      auto UBW = KB.getBitWidth() - KB.Zero.countLeadingOnes();
+      if (UBW <= BW) {
+        CurrentCost += TTI.getArithmeticInstrCost(UI->getOpcode(), BigTy);
+        ShrinkCost += TTI.getArithmeticInstrCost(UI->getOpcode(), SmallTy);
+        ShrinkCost += ZExtCost;
+      } else {
+        HasUNZExtableUser = true;
+      }
+    }
+
+    // ZExt can't remove, add extra cost
+    if (HasUNZExtableUser)
+      ShrinkCost += ZExtCost;
+
+    // If the other instruction operand is not a constant, we'll need to
+    // generate a truncate instruction. So we have to adjust cost
+    if (!isa<Constant>(OtherOperand))
+      ShrinkCost += TTI.getCastInstrCost(
+          Instruction::Trunc, SmallTy, BigTy,
+          TargetTransformInfo::CastContextHint::None, TTI::TCK_RecipThroughput);
+
+    // If the cost of shrinking types and leaving the IR is the same, we'll lean
+    // towards modifying the IR because shrinking opens opportunities for other
+    // shrinking optimisations.
+    if (ShrinkCost > CurrentCost)
+      return false;
+
+    auto *Op0 = ZExted;
+    if (auto *OI = dyn_cast<Instruction>(OtherOperand))
+      Builder.SetInsertPoint(OI->getNextNode());
+    auto *Op1 = Builder.CreateTrunc(OtherOperand, SmallTy);
+    Builder.SetInsertPoint(&I);
+    // Keep the order of operands the same
+    if (I.getOperand(0) == OtherOperand)
+      std::swap(Op0, Op1);
+    auto *NewBinOp =
+        Builder.CreateBinOp((Instruction::BinaryOps)I.getOpcode(), Op0, Op1);
+    cast<Instruction>(NewBinOp)->copyIRFlags(&I);
+    cast<Instruction>(NewBinOp)->copyMetadata(I);
+    auto *NewZExtr = Builder.CreateZExt(NewBinOp, BigTy);
+    replaceValue(I, *NewZExtr);
+    return true;
+  }
+  return false;
+}
+
 /// This is the entry point for all transforms. Pass manager differences are
 /// handled in the callers of this function.
 bool VectorCombine::run() {
@@ -2559,6 +2660,9 @@ bool VectorCombine::run() {
         break;
       case Instruction::BitCast:
         MadeChange |= foldBitcastShuffle(I);
+        break;
+      default:
+        MadeChange |= shrinkType(I);
         break;
       }
     } else {
