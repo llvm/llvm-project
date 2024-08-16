8b59615b3709f58823a9deb91b5fbd4460ec182e
Fangrui Song <i@maskray.me>
2024-08-15 18:01:10 -0700

    [Driver] Reject -Wa,-mrelax-relocations= for non-ELF

--- clang/test/Driver/relax.c
+++ clang/test/Driver/relax.c
@@ -8,3 +8,6 @@
 
 // RUN: not %clang -### --target=aarch64 -c -Wa,-mrelax-relocations=no %s 2>&1 | FileCheck %s --check-prefix=ERR2
 // ERR2: error: unsupported argument '-mrelax-relocations=no' to option '-Wa,'
+
+// RUN: not %clang -### --target=x86_64-apple-darwin -c -Wa,-mrelax-relocations=no %s 2>&1 | FileCheck %s --check-prefix=ERR3
+// ERR3: error: unsupported option '-Wa,-mrelax-relocations=' for target 'x86_64-apple-darwin'
