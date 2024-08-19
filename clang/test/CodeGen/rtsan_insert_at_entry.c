e85d100dcd4fae646ba6b6b11c0a63b7e71fcafa
Chris Apple <cja-private@pm.me>
2024-08-19 11:26:34 -0700

    [PR] Maskray - fix grammar, merge entry and exit test, change to aarch-unknown, ditch unnecessary driver test

--- clang/test/CodeGen/rtsan_insert_at_entry.c
+++
@@ -1,9 +0,0 @@
-// RUN: %clang_cc1 -triple aarch64-none-linux-gnu -fsanitize=realtime -emit-llvm -o - %s | FileCheck %s
-
-// The first instruction after the function is entred should be a call to
-// enable the realtime sanitizer stack
-
-int foo(int *a) [[clang::nonblocking]] { return *a; }
-// CHECK-LABEL: define{{.*}}@foo
-// CHECK-NEXT: entry:
-// CHECK-NEXT: call{{.*}}__rtsan_realtime_enter
--- clang/test/CodeGen/rtsan_insert_at_exit.c
+++
@@ -1,9 +0,0 @@
-// RUN: %clang_cc1 -triple aarch64-none-linux-gnu -fsanitize=realtime -emit-llvm -o - %s | FileCheck %s
-
-// __rtsan_realtime_exit should be inserted at all function returns
-
-int bar(int* x) [[clang::nonblocking]] {
-  return *x;
-}
-// CHECK-LABEL: call{{.*}}__rtsan_realtime_exit
-// CHECK-NEXT: ret
