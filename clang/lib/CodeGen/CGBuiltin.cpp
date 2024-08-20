b7f2a0db609e227d38dcb78db85d321517f1e99c
Stanislav Mekhanoshin <Stanislav.Mekhanoshin@amd.com>
2024-08-20 11:49:15 -0700

    MI-400: Remove v_wmma_[scale_]bf16_16x16x128_f8f6f4
    These are defeatured and nop longer supported by SP3.
    
    Change-Id: I0eaa6fdfc12ab5d63a7b791ef2b9743f38bafcb7

--- clang/lib/CodeGen/CGBuiltin.cpp
+++ clang/lib/CodeGen/CGBuiltin.cpp
@@ -19297,9 +19297,7 @@ Value *CodeGenFunction::EmitAMDGPUBuiltinExpr(unsigned BuiltinID,
   case AMDGPU::BI__builtin_amdgcn_wmma_i32_16x16x64_iu8:
   case AMDGPU::BI__builtin_amdgcn_wmma_i32_16x16x128_iu4:
   case AMDGPU::BI__builtin_amdgcn_wmma_f32_16x16x128_f8f6f4:
-  case AMDGPU::BI__builtin_amdgcn_wmma_bf16_16x16x128_f8f6f4:
   case AMDGPU::BI__builtin_amdgcn_wmma_scale_f32_16x16x128_f8f6f4:
-  case AMDGPU::BI__builtin_amdgcn_wmma_scale_bf16_16x16x128_f8f6f4:
   case AMDGPU::BI__builtin_amdgcn_swmmac_f32_16x16x64_f16:
   case AMDGPU::BI__builtin_amdgcn_swmmac_f32_16x16x64_bf16:
   case AMDGPU::BI__builtin_amdgcn_swmmac_f16_16x16x64_f16:
@@ -19547,17 +19545,9 @@ Value *CodeGenFunction::EmitAMDGPUBuiltinExpr(unsigned BuiltinID,
       ArgsForMatchingMatrixTypes = {5, 1};
       BuiltinWMMAOp = Intrinsic::amdgcn_wmma_f32_16x16x128_f8f6f4;
       break;
-    case AMDGPU::BI__builtin_amdgcn_wmma_bf16_16x16x128_f8f6f4:
-      ArgsForMatchingMatrixTypes = {5, 1};
-      BuiltinWMMAOp = Intrinsic::amdgcn_wmma_bf16_16x16x128_f8f6f4;
-      break;
     case AMDGPU::BI__builtin_amdgcn_wmma_scale_f32_16x16x128_f8f6f4:
       ArgsForMatchingMatrixTypes = {5, 1};
       BuiltinWMMAOp = Intrinsic::amdgcn_wmma_scale_f32_16x16x128_f8f6f4;
-      break;
-    case AMDGPU::BI__builtin_amdgcn_wmma_scale_bf16_16x16x128_f8f6f4:
-      ArgsForMatchingMatrixTypes = {5, 1};
-      BuiltinWMMAOp = Intrinsic::amdgcn_wmma_scale_bf16_16x16x128_f8f6f4;
       break;
     case AMDGPU::BI__builtin_amdgcn_swmmac_f32_16x16x64_f16:
       ArgsForMatchingMatrixTypes = {4, 1, 3, 5};
