a4ee7ebd69db218a1084e44fee35832a169f7c7e
Daniel Cederman <cederman@gaisler.com>
2024-08-20 09:46:53 +0200

    [Sparc] Use --target= instead of -target

--- clang/test/Driver/sparc-fix.c
+++ clang/test/Driver/sparc-fix.c
@@ -1,11 +1,11 @@
-// RUN: %clang -target sparc -mfix-gr712rc -### %s 2> %t
+// RUN: %clang --target=sparc -mfix-gr712rc -### %s 2> %t
 // RUN: FileCheck --check-prefix=CHECK-FIX-GR712RC < %t %s
 // CHECK-FIX-GR712RC: "-target-feature" "+fix-tn0009"
 // CHECK-FIX-GR712RC: "-target-feature" "+fix-tn0011"
 // CHECK-FIX-GR712RC: "-target-feature" "+fix-tn0012"
 // CHECK-FIX-GR712RC: "-target-feature" "+fix-tn0013"
 
-// RUN: %clang -target sparc -mfix-ut700 -### %s 2> %t
+// RUN: %clang --target=sparc -mfix-ut700 -### %s 2> %t
 // RUN: FileCheck --check-prefix=CHECK-FIX-UT700 < %t %s
 // CHECK-FIX-UT700: "-target-feature" "+fix-tn0009"
 // CHECK-FIX-UT700: "-target-feature" "+fix-tn0010"
