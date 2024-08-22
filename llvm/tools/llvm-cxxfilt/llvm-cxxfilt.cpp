30f23192f63a60358ac4cfae6d26228295cfa982
Michael Buch <michaelbuch12@gmail.com>
2024-08-22 15:17:27 +0100

    [llvm-cxxfilt][macOS] Don't strip underscores on macOS by default
    Currently, `llvm-cxxfilt` will strip the leading underscore of
    its input on macOS. Historically MachO symbols were prefixed
    with an extra underscore and this is why this default exists.
    However, since this default was introduced the `ItaniumDemangler`
    supports all of the following mangling prefixes:
    `_Z`, `__Z`, `___Z`, `____Z`. So really `llvm-cxxfilt` can
    simply forward the mangled name to the demangler and let the
    library decide whether it's a valid encoding.
    
    Compiling C++ on macOS nowadays will generate symbols with `_Z`
    and `___Z` prefixes. So users trying to demangle these symbols
    will have to know that they need to add the `-n` prefix. This
    routinely catches people off-guard.
    
    This patch removes the `-n` default for macOS and allows calling
    into the `ItaniumDemangler` with all the `_Z` prefixes that the
    demangler supports (1-4 underscores).
    
    rdar://132714940

--- llvm/tools/llvm-cxxfilt/llvm-cxxfilt.cpp
+++ llvm/tools/llvm-cxxfilt/llvm-cxxfilt.cpp
@@ -54,7 +54,7 @@ public:
 } // namespace
 
 static bool ParseParams;
-static bool StripUnderscore;
+static bool StripUnderscore = false;
 static bool Types;
 
 static StringRef ToolName;
@@ -165,13 +165,9 @@ int llvm_cxxfilt_main(int argc, char **argv, const llvm::ToolContext &) {
     return 0;
   }
 
-  // The default value depends on the default triple. Mach-O has symbols
-  // prefixed with "_", so strip by default.
   if (opt::Arg *A =
           Args.getLastArg(OPT_strip_underscore, OPT_no_strip_underscore))
     StripUnderscore = A->getOption().matches(OPT_strip_underscore);
-  else
-    StripUnderscore = Triple(sys::getProcessTriple()).isOSBinFormatMachO();
 
   ParseParams = !Args.hasArg(OPT_no_params);
 
