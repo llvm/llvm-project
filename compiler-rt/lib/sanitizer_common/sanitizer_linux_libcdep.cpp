d1fb2fb5047c50d3636230b52b298a674150a681
Wu, Yingcong <yingcong.wu@intel.com>
2024-08-21 21:39:13 -0700

    add null check

--- compiler-rt/lib/sanitizer_common/sanitizer_linux_libcdep.cpp
+++ compiler-rt/lib/sanitizer_common/sanitizer_linux_libcdep.cpp
@@ -235,6 +235,7 @@ void InitTlsSize() {
 #    if defined(__aarch64__) || defined(__x86_64__) || \
         defined(__powerpc64__) || defined(__loongarch__)
   void *get_tls_static_info = dlsym(RTLD_NEXT, "_dl_get_tls_static_info");
+  CHECK_NE(get_tls_static_info, nullptr);
   size_t tls_align;
   ((void (*)(size_t *, size_t *))get_tls_static_info)(&g_tls_size, &tls_align);
 #    endif
