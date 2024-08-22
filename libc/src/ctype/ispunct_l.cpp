856dadb33c38f4e3be592f11c3d67e7337e288c7
Joseph Huber <huberjn@outlook.com>
2024-08-22 13:34:48 -0500

    [libc] Add `ctype.h` locale variants (#102711)
    Summary:
    This patch adds all the libc ctype variants. These ignore the locale
    ingormation completely, so they're pretty much just stubs. Because these
    use locale information, which is system scope, we do not enable building
    them outisde of full build mode.

--- libc/src/ctype/ispunct_l.cpp
+++ libc/src/ctype/ispunct_l.cpp
@@ -1,4 +1,4 @@
-//===-- Implementation of isspace------------------------------------------===//
+//===-- Implementation of ispunct -----------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,16 +6,17 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "src/ctype/isspace.h"
-#include "src/__support/ctype_utils.h"
+#include "src/ctype/ispunct_l.h"
 
 #include "src/__support/common.h"
+#include "src/__support/ctype_utils.h"
 #include "src/__support/macros/config.h"
 
 namespace LIBC_NAMESPACE_DECL {
 
-LLVM_LIBC_FUNCTION(int, isspace, (int c)) {
-  return static_cast<int>(internal::isspace(static_cast<unsigned>(c)));
+LLVM_LIBC_FUNCTION(int, ispunct_l, (int c, locale_t)) {
+  const unsigned ch = static_cast<unsigned>(c);
+  return static_cast<int>(!internal::isalnum(ch) && internal::isgraph(ch));
 }
 
 } // namespace LIBC_NAMESPACE_DECL
