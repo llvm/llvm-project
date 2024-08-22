5f84cc47cde13732ed61eccc0b17f6c420a64ab7
Alan Rosenthal <alanrosenthal@google.com>
2024-08-22 21:42:04 +0000

    [clang-tidy] Update TODO comment check
    The doc for google-readability-todo reference:
    https://google.github.io/styleguide/cppguide.html#TODO_Comments
    Which lists two styles of TODO comments.
    
    Previously, only `TODO(John): comment` were supported.
    After this PR, `TODO: bug 123 - comment` are also supported.

--- clang-tools-extra/test/clang-tidy/checkers/google/readability-todo.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/google/readability-todo.cpp
@@ -24,3 +24,7 @@
 // TODO(b/12345): find the holy grail
 // TODO (b/12345): allow spaces before parentheses
 // TODO(asdf) allow missing semicolon
+// TODO: bug 12345678 - Remove this after the 2047q4 compatibility window expires.
+// TODO: example.com/my-design-doc - Manually fix up this code the next time it's touched.
+// TODO(bug 12345678): Update this list after the Foo service is turned down.
+// TODO(John): Use a "\*" here for concatenation operator.
