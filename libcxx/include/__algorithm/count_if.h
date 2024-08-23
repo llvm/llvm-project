7e47590501af2ddef9b889b0d0a83051d9c63937
adeel10x <adeel.ahmad@10xengineers.ai>
2024-08-24 02:04:23 +0500

    [libcxx] Added segmented iterator for count_if

--- libcxx/include/__algorithm/count_if.h
+++ libcxx/include/__algorithm/count_if.h
@@ -10,8 +10,10 @@
 #ifndef _LIBCPP___ALGORITHM_COUNT_IF_H
 #define _LIBCPP___ALGORITHM_COUNT_IF_H
 
+#include <__algorithm/for_each.h>
 #include <__config>
 #include <__iterator/iterator_traits.h>
+#include <__iterator/segmented_iterator.h>
 
 #if !defined(_LIBCPP_HAS_NO_PRAGMA_SYSTEM_HEADER)
 #  pragma GCC system_header
@@ -19,15 +21,38 @@
 
 _LIBCPP_BEGIN_NAMESPACE_STD
 
-template <class _InputIterator, class _Predicate>
+template <class _InputIterator,
+          class _Predicate,
+          __enable_if_t<!__is_segmented_iterator<_InputIterator>::value, int> = 0>
 _LIBCPP_NODISCARD inline _LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX20
-typename iterator_traits<_InputIterator>::difference_type
-count_if(_InputIterator __first, _InputIterator __last, _Predicate __pred) {
+    typename iterator_traits<_InputIterator>::difference_type
+    __count_if(_InputIterator __first, _InputIterator __last, _Predicate __pred) {
   typename iterator_traits<_InputIterator>::difference_type __r(0);
   for (; __first != __last; ++__first)
     if (__pred(*__first))
       ++__r;
   return __r;
+}
+
+template <class _SegmentedIterator,
+          class _Predicate,
+          __enable_if_t<__is_segmented_iterator<_SegmentedIterator>::value, int> = 0>
+_LIBCPP_NODISCARD inline _LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX20
+    typename iterator_traits<_SegmentedIterator>::difference_type
+    __count_if(_SegmentedIterator __first, _SegmentedIterator __last, _Predicate __pred) {
+  typename iterator_traits<_SegmentedIterator>::difference_type __r(0);
+  std::for_each(__first, __last, [&__r, __pred](auto& __val) mutable {
+    if (__pred(__val))
+      ++__r;
+  });
+  return __r;
+}
+
+template <class _InputIterator, class _Predicate>
+_LIBCPP_NODISCARD inline _LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX20
+    typename iterator_traits<_InputIterator>::difference_type
+    count_if(_InputIterator __first, _InputIterator __last, _Predicate __pred) {
+  return __count_if(__first, __last, __pred);
 }
 
 _LIBCPP_END_NAMESPACE_STD
