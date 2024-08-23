33e25c57e46d7ece3ab3554aa2b89ba20a74aca8
Felipe de Azevedo Piovezan <fpiovezan@apple.com>
2024-08-23 13:10:00 -0700

    [GDBRemote] Handle 'heap' memory region info type
    This should cause the memory region info "is stack" field to be set to "no".
    
    (cherry picked from commit 0730cd7f48dd4b80fba0275fed9beadcf6193987)

--- lldb/source/Plugins/Process/gdb-remote/GDBRemoteCommunicationClient.cpp
+++ lldb/source/Plugins/Process/gdb-remote/GDBRemoteCommunicationClient.cpp
@@ -1632,6 +1632,8 @@ Status GDBRemoteCommunicationClient::GetMemoryRegionInfo(
           for (llvm::StringRef entry : llvm::split(value, ',')) {
             if (entry == "stack")
               region_info.SetIsStackMemory(MemoryRegionInfo::eYes);
+            if (entry == "heap")
+              region_info.SetIsStackMemory(MemoryRegionInfo::eNo);
           }
         } else if (name == "error") {
           StringExtractorGDBRemote error_extractor(value);
