cbc7dbbb80fe2c3049cb414535ce31adcad8d8e1
Justin Bogner <mail@justinbogner.com>
2024-08-23 14:02:45 -0700

    [SPIRV] Fix return type mismatch for createSPIRVEmitNonSemanticDIPass
    The declaration in SPIRV.h had this returning a `MachineFunctionPass
    *`, but the implementation returned a `FunctionPass *`. This showed up
    as a build error on windows, but it was clearly a mistake regardless.
    
    I also updated the pass to include SPIRV.h rather than using its own
    declarations for pass initialization, as this results in better errors
    for this kind of typo.

--- llvm/lib/Target/SPIRV/SPIRVEmitNonSemanticDI.cpp
+++ llvm/lib/Target/SPIRV/SPIRVEmitNonSemanticDI.cpp
@@ -1,3 +1,4 @@
+#include "SPIRV.h"
 #include "MCTargetDesc/SPIRVBaseInfo.h"
 #include "MCTargetDesc/SPIRVMCTargetDesc.h"
 #include "SPIRVGlobalRegistry.h"
@@ -33,12 +34,6 @@ private:
   bool IsGlobalDIEmitted = false;
   bool emitGlobalDI(MachineFunction &MF);
 };
-
-void initializeSPIRVEmitNonSemanticDIPass(PassRegistry &);
-
-FunctionPass *createSPIRVEmitNonSemanticDIPass(SPIRVTargetMachine *TM) {
-  return new SPIRVEmitNonSemanticDI(TM);
-}
 } // namespace llvm
 
 using namespace llvm;
@@ -47,6 +42,11 @@ INITIALIZE_PASS(SPIRVEmitNonSemanticDI, DEBUG_TYPE,
                 "SPIRV NonSemantic.Shader.DebugInfo.100 emitter", false, false)
 
 char SPIRVEmitNonSemanticDI::ID = 0;
+
+MachineFunctionPass *
+llvm::createSPIRVEmitNonSemanticDIPass(SPIRVTargetMachine *TM) {
+  return new SPIRVEmitNonSemanticDI(TM);
+}
 
 SPIRVEmitNonSemanticDI::SPIRVEmitNonSemanticDI(SPIRVTargetMachine *TM)
     : MachineFunctionPass(ID), TM(TM) {
