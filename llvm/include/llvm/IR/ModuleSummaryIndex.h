3ee6c856bbea4b3e4119ff7d0b508ad60496b083
Kazu Hirata <kazu@google.com>
2024-08-23 17:14:59 -0700

    Update a comment.

--- llvm/include/llvm/IR/ModuleSummaryIndex.h
+++ llvm/include/llvm/IR/ModuleSummaryIndex.h
@@ -1291,7 +1291,8 @@ using ModulePathStringTableTy = StringMap<ModuleHash>;
 /// a particular module, and provide efficient access to their summary.
 using GVSummaryMapTy = DenseMap<GlobalValue::GUID, GlobalValueSummary *>;
 
-/// Map of a module name to its summary.
+/// Map of a module name to the GUIDs and summaries we will import from that
+/// module.
 using ModuleToSummariesForIndexTy = std::map<std::string, GVSummaryMapTy>;
 
 /// A set of global value summary pointers.
