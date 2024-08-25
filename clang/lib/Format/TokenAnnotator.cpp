1b1ddb767e430a29c35c8b12760b7aa21f508d15
Owen Pan <owenpiano@gmail.com>
2024-08-24 19:12:15 -0700

    [clang-format] Fix a misannotation of redundant r_paren as CastRParen (#105921)
    Fixes #105880.
    
    (cherry picked from commit 6bc225e0630f28e83290a43c3d9b25b057fc815a)

--- clang/lib/Format/TokenAnnotator.cpp
+++ clang/lib/Format/TokenAnnotator.cpp
@@ -2874,6 +2874,8 @@ private:
     // Search for unexpected tokens.
     for (auto *Prev = BeforeRParen; Prev != LParen; Prev = Prev->Previous) {
       if (Prev->is(tok::r_paren)) {
+        if (Prev->is(TT_CastRParen))
+          return false;
         Prev = Prev->MatchingParen;
         if (!Prev)
           return false;
