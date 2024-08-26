llvm/llvm-project

commit 6c7bb61d318ddf19bc846b67faa187439b452e48

AMDGPU: Refactoring of AMDGPULowerVGPREncoding
Instead of maintaining bits of the old mode, compute a fully fresh mode
and a mask of which bits we actually care about. This makes GFX13
indexing-related changes a bit less awkward. Some other structural
changes from GFX13, like the computeMode method, are back-ported with
this change as well.

It does change the generated code slightly, because we now default to 0
for the unused fields, which I find a bit easier to justify, and it's
probably a wash in terms of code size.

Also exploit some additional code size opportunities by fixing up the
most recent s_set_vgpr_msbs instruction if possible, and fix a bug
related to control flow.

Change-Id: I2270bfdc5a821fdb148048f47d264282f9e37777

llvm/lib/Target/AMDGPU/AMDGPULowerVGPREncoding.cpp
@@ -55,6 +55,7 @@ class AMDGPULowerVGPREncoding : public MachineFunctionPass {
   static constexpr unsigned OpNum = 4;
   static constexpr unsigned BitsPerField = 2;
   static constexpr unsigned NumFields = 4;
+  static constexpr unsigned FieldMask = (1 << BitsPerField) - 1;
   using ModeType = PackedVector<unsigned, BitsPerField,
                                 std::bitset<BitsPerField * NumFields>>;
 
@@ -64,6 +65,12 @@ class AMDGPULowerVGPREncoding : public MachineFunctionPass {
     ModeTy() : ModeType(0) {}
 
     operator int64_t() const { return raw_bits().to_ulong(); }
+
+    static ModeTy fullMask() {
+      ModeTy M;
+      M.raw_bits().flip();
+      return M;
+    }
   };
 
 public:
@@ -82,8 +89,18 @@ private:
   const SIInstrInfo *TII;
   const SIRegisterInfo *TRI;
 
+  /// Most recent s_set_* instruction.
+  MachineInstr *MostRecentModeSet;
+
+  /// Whether the current mode is known.
+  bool CurrentModeKnown;
+
   /// Current mode bits.
-  ModeTy Mode;
+  ModeTy CurrentMode;
+
+  /// Current mask of mode bits that instructions since MostRecentModeSet care
+  /// about.
+  ModeTy CurrentMask;
 
   /// Number of current hard clause instructions.
   unsigned ClauseLen;
@@ -95,13 +112,13 @@ private:
   unsigned ClauseBreaks;
 
   /// Last hard clause instruction.
-  MachineBasicBlock::instr_iterator Clause;
+  MachineInstr *Clause;
 
   /// Insert mode change before \p I. \returns true if mode was changed.
-  bool setMode(ModeTy NewMode, MachineBasicBlock::instr_iterator I);
+  bool setMode(ModeTy NewMode, ModeTy Mask, MachineInstr *I);
 
   /// Reset mode to default.
-  void resetMode(MachineBasicBlock::instr_iterator I) { setMode(ModeTy(), I); }
+  void resetMode(MachineInstr *I) { setMode(ModeTy(), ModeTy::fullMask(), I); }
 
   /// If \p MO references VGPRs, return the MSBs. Otherwise, return nullopt.
   std::optional<unsigned> getMSBs(const MachineOperand &MO) const;
@@ -109,30 +126,46 @@ private:
   /// Handle single \p MI. \return true if changed.
   bool runOnMachineInstr(MachineInstr &MI);
 
-  /// Handle single \p MI given \p Ops operands bit mapping.
-  /// \return true if changed. Optionally takes second array \p Ops2.
+  /// Compute the mode and mode mask for a single \p MI given \p Ops operands
+  /// bit mapping. Optionally takes second array \p Ops2 for VOPD.
   /// If provided and an operand from \p Ops is not a VGPR, then \p Ops2
   /// is checked.
-  bool runOnMachineInstr(MachineInstr &MI, const unsigned Ops[OpNum],
-                         const unsigned *Ops2 = nullptr);
+  void computeMode(ModeTy &NewMode, ModeTy &Mask, MachineInstr &MI,
+                   const unsigned Ops[OpNum], const unsigned *Ops2 = nullptr);
 
   /// Check if an instruction \p I is within a clause and returns a suitable
   /// iterator to insert mode change. It may also modify the S_CLAUSE
   /// instruction to extend it or drop the clause if it cannot be adjusted.
-  MachineBasicBlock::instr_iterator
-  handleClause(MachineBasicBlock::instr_iterator I);
+  MachineInstr *handleClause(MachineInstr *I);
 };
 
-bool AMDGPULowerVGPREncoding::setMode(ModeTy NewMode,
-                                      MachineBasicBlock::instr_iterator I) {
-  if (NewMode == Mode)
-    return false;
+bool AMDGPULowerVGPREncoding::setMode(ModeTy NewMode, ModeTy Mask,
+                                      MachineInstr *I) {
+  assert((NewMode.raw_bits() & ~Mask.raw_bits()).none());
+
+  if (CurrentModeKnown) {
+    auto Delta = NewMode.raw_bits() ^ CurrentMode.raw_bits();
+
+    if ((Delta & Mask.raw_bits()).none())
+      return false;
+
+    if (MostRecentModeSet && (Delta & CurrentMask.raw_bits()).none()) {
+      CurrentMode |= NewMode;
+      CurrentMask |= Mask;
+
+      MostRecentModeSet->getOperand(0).setImm(CurrentMode);
+      return true;
+    }
+  }
 
   I = handleClause(I);
-  BuildMI(*I->getParent(), I, nullptr, TII->get(AMDGPU::S_SET_VGPR_MSB))
-      .addImm(NewMode);
+  MostRecentModeSet =
+      BuildMI(*I->getParent(), I, {}, TII->get(AMDGPU::S_SET_VGPR_MSB))
+          .addImm(NewMode);
 
-  Mode = NewMode;
+  CurrentMode = NewMode;
+  CurrentMask = Mask;
+  CurrentModeKnown = true;
   return true;
 }
 
@@ -150,10 +183,12 @@ AMDGPULowerVGPREncoding::getMSBs(const MachineOperand &MO) const {
   return Idx >> 8;
 }
 
-bool AMDGPULowerVGPREncoding::runOnMachineInstr(MachineInstr &MI,
-                                                const unsigned Ops[OpNum],
-                                                const unsigned *Ops2) {
-  ModeTy NewMode = Mode;
+void AMDGPULowerVGPREncoding::computeMode(ModeTy &NewMode, ModeTy &Mask,
+                                          MachineInstr &MI,
+                                          const unsigned Ops[OpNum],
+                                          const unsigned *Ops2) {
+  NewMode = {};
+  Mask = {};
 
   for (unsigned I = 0; I < OpNum; ++I) {
     MachineOperand *Op = TII->getNamedOperand(MI, Ops[I]);
@@ -180,7 +215,6 @@ bool AMDGPULowerVGPREncoding::runOnMachineInstr(MachineInstr &MI,
         MSBits = getMSBs(*Op);
     }
 
-    // Keep unused bits from the old mask to minimize switches.
     if (!MSBits.has_value())
       continue;
 
@@ -194,31 +228,30 @@ bool AMDGPULowerVGPREncoding::runOnMachineInstr(MachineInstr &MI,
       continue;
 
     NewMode[I] = MSBits.value();
+    Mask[I] = FieldMask;
   }
-
-  return setMode(NewMode, MI.getIterator());
 }
 
 bool AMDGPULowerVGPREncoding::runOnMachineInstr(MachineInstr &MI) {
   auto Ops = AMDGPU::getVGPRLoweringOperandTables(MI.getDesc());
-
-  if (Ops.first)
-    return runOnMachineInstr(MI, Ops.first, Ops.second);
-
+  if (Ops.first) {
+    ModeTy NewMode, Mask;
+    computeMode(NewMode, Mask, MI, Ops.first, Ops.second);
+    return setMode(NewMode, Mask, &MI);
+  }
   assert(!TII->hasVGPRUses(MI) || MI.isMetaInstruction() || MI.isPseudo());
 
   return false;
 }
 
-MachineBasicBlock::instr_iterator
-AMDGPULowerVGPREncoding::handleClause(MachineBasicBlock::instr_iterator I) {
+MachineInstr *AMDGPULowerVGPREncoding::handleClause(MachineInstr *I) {
   if (!ClauseRemaining)
     return I;
 
   // A clause cannot start with a special instruction, place it right before
   // the clause.
   if (ClauseRemaining == ClauseLen) {
-    I = std::prev(Clause);
+    I = Clause->getPrevNode();
     assert(I->isBundle());
     return I;
   }
@@ -253,24 +286,29 @@ bool AMDGPULowerVGPREncoding::runOnMachineFunction(MachineFunction &MF) {
 
   bool Changed = false;
   ClauseLen = ClauseRemaining = 0;
-  Mode.reset();
+  CurrentMode.reset();
+  CurrentMask.reset();
+  CurrentModeKnown = true;
   for (auto &MBB : MF) {
+    MostRecentModeSet = nullptr;
+
     for (auto &MI : llvm::make_early_inc_range(MBB.instrs())) {
       if (MI.isMetaInstruction())
         continue;
 
       if (MI.isTerminator() || MI.isCall()) {
         if (MI.getOpcode() == AMDGPU::S_ENDPGM ||
-            MI.getOpcode() == AMDGPU::S_ENDPGM_SAVED)
-          Mode.reset();
-        else
-          resetMode(MI.getIterator());
+            MI.getOpcode() == AMDGPU::S_ENDPGM_SAVED) {
+          CurrentMode.reset();
+          CurrentModeKnown = true;
+        } else
+          resetMode(&MI);
         continue;
       }
 
       if (MI.isInlineAsm()) {
         if (TII->hasVGPRUses(MI))
-          resetMode(MI.getIterator());
+          resetMode(&MI);
         continue;
       }
 
@@ -279,7 +317,7 @@ bool AMDGPULowerVGPREncoding::runOnMachineFunction(MachineFunction &MF) {
         ClauseLen = MI.getOperand(0).getImm();
         ClauseBreaks = (ClauseLen >> 8) & 15;
         ClauseLen = ClauseRemaining = (ClauseLen & 63) + 1;
-        Clause = MI.getIterator();
+        Clause = &MI;
         continue;
       }
 
@@ -287,6 +325,15 @@ bool AMDGPULowerVGPREncoding::runOnMachineFunction(MachineFunction &MF) {
 
       if (ClauseRemaining)
         --ClauseRemaining;
+    }
+
+    // If we're falling through to a block that has at least one other
+    // predecessor, we no longer know the mode.
+    MachineBasicBlock *Next = MBB.getNextNode();
+    if (Next && Next->pred_size() >= 2 &&
+        llvm::is_contained(Next->predecessors(), &MBB)) {
+      if (CurrentMode.raw_bits().any())
+        CurrentModeKnown = false;
     }
   }
 

llvm/test/CodeGen/AMDGPU/vgpr-lowering-gfx1210.mir
@@ -64,8 +64,8 @@ body:             |
     $vgpr254_vgpr255_vgpr256_vgpr257 = V_MQSAD_U32_U8_e64 $vgpr258_vgpr259, $vgpr0, undef $vgpr500_vgpr501_vgpr502_vgpr503, 0, implicit $exec
 
     ; DPP/tied operand
-    ; GCN-NEXT: s_set_vgpr_msb 0x55
-    ; ASM-SAME:                                         ;  msbs: dst=1 src0=1 src1=1 src2=1
+    ; GCN-NEXT: s_set_vgpr_msb 0x45
+    ; ASM-SAME:                                         ;  msbs: dst=1 src0=1 src1=1 src2=0
     ; GCN-NEXT: s_delay_alu instid0(VALU_DEP_1)
     ; GCN-NEXT: v_add_nc_u16_e64_dpp v0 /*v256*/, v1 /*v257*/, v2 /*v258*/ quad_perm:[1,0,0,0] row_mask:0xf bank_mask:0xf bound_ctrl:1
     $vgpr256 = V_ADD_NC_U16_e64_dpp $vgpr256, 0, $vgpr257, 0, undef $vgpr258, 0, 0, 1, 15, 15, 1, implicit $exec
@@ -213,10 +213,8 @@ body:             |
     ; GCN-NEXT: v_fma_f32 v0 /*v768*/, v1 /*v769*/, v2 /*v770*/, v3 /*v771*/
     $vgpr768 = V_FMA_F32_e64 0, undef $vgpr769, 0, undef $vgpr770, 0, undef $vgpr771, 0, 0, implicit $exec, implicit $mode
 
-    ; Partial reset, bits for src1 and src2 are preserved.
-
-    ; GCN-NEXT: s_set_vgpr_msb 0x7e
-    ; ASM-SAME:                                         ;  msbs: dst=1 src0=2 src1=3 src2=3
+    ; GCN-NEXT: s_set_vgpr_msb 0x42
+    ; ASM-SAME:                                         ;  msbs: dst=1 src0=2 src1=0 src2=0
     ; GCN-NEXT: v_mov_b32_e32 v0 /*v256*/, v0 /*v512*/
     $vgpr256 = V_MOV_B32_e32 undef $vgpr512, implicit $exec
 
@@ -419,35 +417,32 @@ body:             |
     ; GCN-NEXT: v_fma_f32 v3 /*v259*/, v4 /*v260*/, v5 /*v261*/, v6 /*v262*/
     $vgpr259 = V_FMA_F32_e64 0, undef $vgpr260, 0, undef $vgpr261, 0, undef $vgpr262, 0, 0, implicit $exec, implicit $mode
 
-    ; GCN-NEXT: s_set_vgpr_msb 16
+    ; GCN-NEXT: s_set_vgpr_msb 0
     ; GCN-NEXT: v_add_nc_u32_e32 v0, v1, v2
     $vgpr0 = V_ADD_U32_e32 undef $vgpr1, undef $vgpr2, implicit $exec
 
     ; GCN-NEXT: v_mov_b32_e32 v0, v1
     $vgpr0 = V_MOV_B32_e32 undef $vgpr1, implicit $exec
 
-    ; GCN-NEXT: s_set_vgpr_msb 0x50
+    ; GCN-NEXT: s_set_vgpr_msb 64
     ; GCN-NEXT: v_mov_b32_e32 v3 /*v259*/, v1
     $vgpr259 = V_MOV_B32_e32 undef $vgpr1, implicit $exec
 
     ; GCN-NEXT: v_add_nc_u32_e32 v0 /*v256*/, v1, v2
     $vgpr256 = V_ADD_U32_e32 undef $vgpr1, undef $vgpr2, implicit $exec
 
-    ; Preserve bit for sgpr src2
-    ; GCN-NEXT: s_set_vgpr_msb 16
+    ; GCN-NEXT: s_set_vgpr_msb 0
     ; GCN-NEXT: v_fma_f32 v3, v4, v5, s2
     $vgpr3 = V_FMA_F32_e64 0, undef $vgpr4, 0, undef $vgpr5, 0, undef $sgpr2, 0, 0, implicit $exec, implicit $mode
 
-    ; Preserve bit for immediate src2
-    ; GCN-NEXT: s_set_vgpr_msb 17
+    ; GCN-NEXT: s_set_vgpr_msb 1
     ; GCN-NEXT: v_fma_f32 v3, v4 /*v260*/, v5, 1
     $vgpr3 = V_FMA_F32_e64 0, undef $vgpr260, 0, undef $vgpr5, 0, 1, 0, 0, implicit $exec, implicit $mode
 
-    ; GCN-NEXT: s_set_vgpr_msb 16
+    ; GCN-NEXT: s_set_vgpr_msb 4
     ; GCN-NEXT: v_mov_b32_e32 v0, v1
     $vgpr0 = V_MOV_B32_e32 undef $vgpr1, implicit $exec
 
-    ; GCN-NEXT: s_set_vgpr_msb 20
     ; GCN-NEXT: v_add_nc_u32_e32 v2, v1, v3 /*v259*/
     $vgpr2 = V_ADD_U32_e32 undef $vgpr1, undef $vgpr259, implicit $exec
 
@@ -574,9 +569,7 @@ body:             |
 
   bb.1:
     ; ASM: .LBB{{[0-9]+}}_1:
-    ;
-    ; BUG: Missing s_set_vgpr_msb here!
-    ;
+    ; GCN-NEXT: s_set_vgpr_msb 64
     ; GCN-NEXT: v_mov_b32_e32 v1 /*v257*/, v1
     ; GCN-NEXT: s_set_vgpr_msb 0
     ; GCN-NEXT: s_cbranch_scc0

llvm/include/llvm/ADT/PackedVector.h
@@ -143,6 +143,7 @@ public:
   }
 
   const BitVectorTy &raw_bits() const { return Bits; }
+  BitVectorTy &raw_bits() { return Bits; }
 };
 
 // Leave BitNum=0 undefined.

commit d93c4fb0d9adc007362ae4b781bd774fcf3f0255

AMDGPU/MI400: Fix an AMDGPULowerVGPREncoding regression
Fix the tracking of which mode bits we care about.

Fixes: 6c7bb61d318d ("AMDGPU: Refactoring of AMDGPULowerVGPREncoding")
Change-Id: Ieb3fa7d8d69a192ee69bf849925cd50c41ddcc3f

llvm/lib/Target/AMDGPU/AMDGPULowerVGPREncoding.cpp
@@ -146,8 +146,10 @@ bool AMDGPULowerVGPREncoding::setMode(ModeTy NewMode, ModeTy Mask,
   if (CurrentModeKnown) {
     auto Delta = NewMode.raw_bits() ^ CurrentMode.raw_bits();
 
-    if ((Delta & Mask.raw_bits()).none())
+    if ((Delta & Mask.raw_bits()).none()) {
+      CurrentMask |= Mask;
       return false;
+    }
 
     if (MostRecentModeSet && (Delta & CurrentMask.raw_bits()).none()) {
       CurrentMode |= NewMode;

llvm/test/CodeGen/AMDGPU/vgpr-lowering-gfx1210.mir
@@ -446,10 +446,10 @@ body:             |
     ; GCN-NEXT: v_add_nc_u32_e32 v2, v1, v3 /*v259*/
     $vgpr2 = V_ADD_U32_e32 undef $vgpr1, undef $vgpr259, implicit $exec
 
-    ; TODO-MI400: The following is incorrect, missing an intermediate update
-    ; GCN-NEXT: s_set_vgpr_msb 5
+    ; GCN-NEXT: s_set_vgpr_msb 1
     ; GCN-NEXT: v_mov_b32_e32 v0, v0 /*v256*/
     ; GCN-NEXT: v_add_nc_u32_e32 v1, v1 /*v257*/, v1
+    ; GCN-NEXT: s_set_vgpr_msb 5
     ; GCN-NEXT: v_add_nc_u32_e32 v2, v2 /*v258*/, v2 /*v258*/
     $vgpr0 = V_MOV_B32_e32 undef $vgpr256, implicit $exec
     $vgpr1 = V_ADD_U32_e32 undef $vgpr257, undef $vgpr1, implicit $exec