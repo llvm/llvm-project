761248f8c0c8413a0bbbe622ae7912c19a9b8a7b
Harini <harinidonthula@google.com>
2024-08-26 15:14:06 +0000

    Simplify suppression file creation by using a single echo command
    Simplified the suppression file creation in the test script by combining multiple RUN lines into a single echo command using the -e option. This change reduces redundancy, improves readability, and makes the script more efficient by creating the suppression file with all necessary entries in one step.

--- compiler-rt/test/ubsan/TestCases/TypeCheck/vptr.cpp
+++ compiler-rt/test/ubsan/TestCases/TypeCheck/vptr.cpp
@@ -23,9 +23,7 @@
 // RUN: %env_ubsan_opts=halt_on_error=1 not %run %t nN 2>&1 | FileCheck %s --check-prefix=CHECK-NULL-MEMFUN --strict-whitespace
 // RUN: %env_ubsan_opts=print_stacktrace=1 %run %t dT 2>&1 | FileCheck %s --check-prefix=CHECK-DYNAMIC --allow-unused-prefixes --check-prefix=CHECK-%os-DYNAMIC --strict-whitespace
 
-// RUN: echo "vptr_check:S" > %t.supp
-// RUN: echo "vptr_check:T" >> %t.supp
-// RUN: echo "vptr_check:U" >> %t.supp
+// RUN: echo -e "vptr_check:S\nvptr_check:T\nvptr_check:U" > %t.supp
 // RUN: %env_ubsan_opts=halt_on_error=1:suppressions='"%t.supp"' %run %t mS
 // RUN: %env_ubsan_opts=halt_on_error=1:suppressions='"%t.supp"' %run %t fS
 // RUN: %env_ubsan_opts=halt_on_error=1:suppressions='"%t.supp"' %run %t cS
