7e44b452010bc978811aae57d362571146f68afa
Whisperity <whisperity@gmail.com>
2024-08-27 15:19:40 +0200

    test: Implemented the tests with proper format and `// CHECK` lines

--- clang-tools-extra/test/clang-tidy/checkers/bugprone/suspicious-pointer-arithmetics-using-sizeof.c
+++ clang-tools-extra/test/clang-tidy/checkers/bugprone/suspicious-pointer-arithmetics-using-sizeof.c
@@ -1,15 +1,33 @@
 // RUN: %check_clang_tidy %s bugprone-suspicious-pointer-arithmetics-using-sizeof %t
 
+typedef __typeof__(sizeof(void*)) size_t;
+#define offsetof(type, member) __builtin_offsetof(type, member)
+extern void *memset(void *Dst, int Ch, size_t Count);
+extern void sink(const void *P);
+
 enum { BufferSize = 1024 };
 
-void bad1(void) {
+void bad1a(void) {
   int Buffer[BufferSize];
 
   int *P = &Buffer[0];
   int *Q = P;
   while (Q < P + sizeof(Buffer)) {
     // CHECK-MESSAGES: :[[@LINE-1]]:16: warning: pointer arithmetic using a number scaled by 'sizeof()'; this distance will be scaled again by the '+' operator [bugprone-suspicious-pointer-arithemetics-using-sizeof]
-    // CHECK-MESSAGES: :[[@LINE-2]]:18: note: 'sizeof(int)' == {{[0-9]+}}
+    // CHECK-MESSAGES: :[[@LINE-2]]:16: note: '+' scales with 'sizeof(Buffer)' == {{[0-9]+}}
+    *Q++ = 0;
+  }
+}
+
+void bad1b(void) {
+  typedef int Integer;
+  Integer Buffer[BufferSize];
+
+  Integer *P = &Buffer[0];
+  Integer *Q = P;
+  while (Q < P + sizeof(Buffer)) {
+    // CHECK-MESSAGES: :[[@LINE-1]]:16: warning: pointer arithmetic using a number scaled by 'sizeof()'; this distance will be scaled again by the '+' operator
+    // CHECK-MESSAGES: :[[@LINE-2]]:16: note: '+' scales with 'sizeof(Buffer)' == {{[0-9]+}}
     *Q++ = 0;
   }
 }
@@ -24,196 +42,272 @@ void good1(void) {
   }
 }
 
+void bad2(void) {
+  int Buffer[BufferSize];
+  int *P = Buffer;
 
-// ----------------------------------------------------------------------------
+  while (P < Buffer + sizeof(Buffer)) {
+    // CHECK-MESSAGES: :[[@LINE-1]]:16: warning: pointer arithmetic using a number scaled by 'sizeof()'; this value will be scaled again by the '+' operator
+    // CHECK-MESSAGES: :[[@LINE-2]]:16: note: '+' scales with 'sizeof(int)' == {{[0-9]+}}
+    *P++ = 0;
+  }
+}
 
+void good2(void) {
+  int Buffer[BufferSize];
+  int *P = Buffer;
 
-struct mystruct {
-  long a;
-  long b;
-  long c;
+  while (P < Buffer + BufferSize) {
+    *P++ = 0;
+  }
+}
+
+struct S {
+  long A, B, C;
 };
-void noncompliant_f1(void);
-void compliant_f1(void);
-void noncompliant_f3(struct mystruct *msptr);
-void compliant_f3(struct mystruct *msptr);
-extern void sink(const char *);
 
-enum { bufsize = 1024 };
+void bad3a(struct S *S) {
+  const size_t Offset = offsetof(struct S, B);
+  struct S *P = S;
 
-void noncompliant_f1a(void) {
-  typedef int my_int_t;
-  my_int_t buffer[bufsize];
+  // This is not captureable by Tidy because the size/offset expression is
+  // not a direct child of the pointer arithmetics.
+  memset(P + Offset, 0, sizeof(struct S) - Offset);
+}
 
-  my_int_t *bptr = &buffer[0];
-  my_int_t *ptr  = bptr;
-  while ( ptr < bptr + sizeof(buffer) ) { // noncompliant
-    *ptr++ = 0;	// compliant
+void good3a(struct S *S) {
+  const size_t Offset = offsetof(struct S, B);
+  char *P = (char*)S;
+
+  // This is not captureable by Tidy because the size/offset expression is
+  // not a direct child of the pointer arithmetics.
+  memset(P + Offset, 0, sizeof(struct S) - Offset);
+}
+
+void bad3b(struct S *S) {
+  memset(S + offsetof(struct S, B), 0,
+         sizeof(struct S) - offsetof(struct S, B));
+  // NCHECK-MESSAGES: :[[@LINE-1]]:12: warning: pointer arithmetic using a number scaled by 'offsetof()'; this value will be scaled again by the '+' operator
+  // NCHECK-MESSAGES: :[[@LINE-2]]:12: note: '+' scales with 'sizeof(int)' == {{[0-9]+}}
+}
+
+void good3b(struct S *S) {
+  char *P = (char*)S;
+  memset(P + offsetof(struct S, B), 0,
+         sizeof(struct S) - offsetof(struct S, B));
+}
+
+void bad3c(void) {
+  struct S Buffer[BufferSize];
+
+  struct S *P = &Buffer[0];
+  struct S *Q = P;
+  while (Q < P + sizeof(Buffer)) {
+    // CHECK-MESSAGES: :[[@LINE-1]]:16: warning: pointer arithmetic using a number scaled by 'sizeof()'; this value will be scaled again by the '+' operator
+    // CHECK-MESSAGES: :[[@LINE-2]]:16: note: '+' scales with 'sizeof(struct S)' == {{[0-9]+}}
+    sink(Q++);
   }
 }
 
-void noncompliant_f2(void) {
-  int buffer[bufsize];
-  int *ptr = buffer;
+void bad4(void) {
+  int Buffer[BufferSize];
 
-  while ( ptr < buffer + sizeof(buffer) ) { // noncompliant
-    *ptr++ = 0;	// compliant
+  int *P = &Buffer[0];
+  int *Q = P;
+  while (Q < P + BufferSize) {
+    *Q = 0;
+    Q += sizeof(*Q);
+    // CHECK-MESSAGES: :[[@LINE-1]]:7: warning: pointer arithmetic using a number scaled by 'sizeof()'; this value will be scaled again by the '+' operator
+    // CHECK-MESSAGES: :[[@LINE-2]]:7: note: '+' scales with 'sizeof(int)' == {{[0-9]+}}
   }
 }
 
-void compliant_f2(void) {
-  int buffer[bufsize];
-  int *ptr  = buffer;
+void silenced4(void) {
+  char Buffer[BufferSize];
 
-  while ( ptr < buffer + bufsize ) { // compliant
-    *ptr++ = 0;	// compliant
+  char *P = &Buffer[0];
+  char *Q = P;
+  while (Q < P + BufferSize) {
+    *Q = 0;
+    Q += sizeof(*Q);
   }
 }
 
-void memset2(void*, int, unsigned int);
+void good4(void) {
+  char Buffer[BufferSize];
 
-void noncompliant_f3(struct mystruct *msptr) {
-  const unsigned int skip = sizeof(long); // why offsetof is declared?
-  struct mystruct *ptr = msptr;
-
-  memset2(ptr + skip, // noncompliant, impossible with tidy
-                     0, sizeof(struct mystruct) - skip);
-}
-
-void compliant_f3(struct mystruct *msptr) {
-  const unsigned int skip = sizeof(long);
-  char *ptr = (char*)msptr;
-
-  memset2(ptr + skip, // compliant
-                     0, sizeof(struct mystruct) - skip);
-}
-
-void noncompliant_f4(void) {
-  int buffer[bufsize];
-
-  int *bptr = &buffer[0];
-  int *ptr  = bptr;
-  while ( ptr < bptr + bufsize ) { // compliant
-    *ptr = 0;
-    ptr += sizeof(*ptr); // noncompliant
-  }
-}
-void noncompliant_f4w(void) { /* accidentally good */
-  char buffer[bufsize];
-
-  char *bptr = &buffer[0];
-  char *ptr  = bptr;
-  while ( ptr < bptr + bufsize ) { // compliant
-    *ptr = 0;
-    ptr += sizeof(*ptr);  // silenced
-  }
-}
-void compliant_f4w(void) {
-  char buffer[bufsize];
-
-  char *bptr = &buffer[0];
-  char *ptr  = bptr;
-  while ( ptr < bptr + bufsize ) { // compliant
-    *ptr = 0;
-    ptr += 1;  // compliant
+  char *P = &Buffer[0];
+  char *Q = P;
+  while (Q < P + BufferSize) {
+    *Q = 0;
+    Q += 1;
   }
 }
 
-void noncompliant_f5(void) {
-  int buffer[bufsize];
+void good5aa(void) {
+  int Buffer[BufferSize];
 
-  int *bptr = &buffer[0];
-  int *ptr  = bptr;
-  while ( ptr < bptr + bufsize ) { // compliant
-    *ptr = 0;
-    ptr = ptr + sizeof(*ptr); // noncompliant
-  }
-}
-void noncompliant_f5w(void) {
-  char buffer[bufsize];
-
-  char*bptr = &buffer[0];
-  char *ptr  = bptr;
-  while ( ptr < bptr + bufsize ) { // compliant
-    *ptr = 0;
-    ptr = ptr + sizeof(*ptr); // silenced
-  }
-}
-void noncompliant_f5c(void) {
-  char buffer[bufsize];
-
-  char*bptr = &buffer[0];
-  const char *ptr  = bptr;
-  while ( ptr < bptr + bufsize ) { // compliant
-    sink(ptr);
-    ptr = ptr + sizeof(*ptr); // silenced
-  }
-}
-void compliant_f5c(void) {
-  char buffer[bufsize];
-
-  char *bptr = &buffer[0];
-  const char *ptr  = bptr;
-  while ( ptr < bptr + bufsize ) { // compliant
-    sink(ptr);
-    ptr = ptr + 1;  // compliant
+  int *P = &Buffer[0];
+  int *Q = P;
+  while (Q < P + BufferSize) {
+    *Q = 0;
+    Q += ( sizeof(Buffer)/ sizeof(Buffer[0]) );
   }
 }
 
-void noncompliant_f6(void) {
-  int buffer[bufsize];
+void good5ab(void) {
+  int Buffer[BufferSize];
 
-  int *bptr = &buffer[0];
-  int *ptr  = bptr + bufsize; // compliant
-  while ( ptr >= bptr ) {
-    *ptr = 0;
-    ptr = ptr - sizeof(*ptr); // noncompliant
-  }
-}
-void noncompliant_f6w(void) {
-  char buffer[bufsize];
-
-  char *bptr = &buffer[0];
-  char *ptr  = bptr + bufsize; // compliant
-  while ( ptr >= bptr ) {
-    *ptr = 0;
-    ptr = ptr - sizeof(*ptr); // silenced
-  }
-}
-void compliant_f6(void) {
-  int buffer[bufsize];
-
-  int *bptr = &buffer[0];
-  int *ptr  = bptr + bufsize; // compliant
-  while ( ptr >= bptr ) {
-    *ptr = 0;
-    ptr = ptr - 1; // compliant
+  int *P = &Buffer[0];
+  int *Q = P;
+  while (Q < P + BufferSize) {
+    *Q = 0;
+    Q = Q + ( sizeof(Buffer)/ sizeof(Buffer[0]) );
   }
 }
 
-void compliant_f7(void) {
-  int buffer[bufsize];
+void good5ba(void) {
+  int Buffer[BufferSize];
 
-  int *bptr = &buffer[0];
-  int *ptr  = bptr + bufsize; // compliant
-  int i = ptr - bptr; // compliant
-  while ( i >= 0 ) {
-    ptr[i] = 0;
-    i = i - 1; // compliant
+  int *P = &Buffer[0];
+  int *Q = P;
+  while (Q < P + BufferSize) {
+    *Q = 0;
+    Q -= ( sizeof(Buffer)/ sizeof(Buffer[0]) );
   }
 }
 
-void compliant_f8(void) {
-  int buffer[bufsize];
+void good5bb(void) {
+  int Buffer[BufferSize];
 
-  int *bptr = &buffer[0];
-  int *ptr  = bptr + bufsize; // compliant
-  int i = sizeof(*ptr) - sizeof(*bptr); // compliant
+  int *P = &Buffer[0];
+  int *Q = P;
+  while (Q < P + BufferSize) {
+    *Q = 0;
+    Q = Q - ( sizeof(Buffer)/ sizeof(Buffer[0]) );
+  }
 }
-void compliant_f9(void) {
-  int buffer[bufsize];
 
-  int *bptr = &buffer[0];
-  int *ptr  = bptr + bufsize; // compliant
-  int i = sizeof(ptr) - sizeof(*bptr); // compliant
+void bad6(void) {
+  int Buffer[BufferSize];
+
+  int *P = &Buffer[0];
+  int *Q = P;
+  while (Q < P + BufferSize) {
+    *Q = 0;
+    Q = Q + sizeof(*Q);
+    // CHECK-MESSAGES: :[[@LINE-1]]:11: warning: pointer arithmetic using a number scaled by 'sizeof()'; this value will be scaled again by the '+' operator
+    // CHECK-MESSAGES: :[[@LINE-2]]:11: note: '+' scales with 'sizeof(int)' == {{[0-9]+}}
+  }
+}
+
+void silenced6(void) {
+  char Buffer[BufferSize];
+
+  char *P = &Buffer[0];
+  char *Q = P;
+  while (Q < P + BufferSize) {
+    *Q = 0;
+    Q = Q + sizeof(*Q);
+  }
+}
+
+void good6(void) {
+  char Buffer[BufferSize];
+
+  char *P = &Buffer[0];
+  char *Q = P;
+  while (Q < P + BufferSize) {
+    *Q = 0;
+    Q = Q + 1;
+  }
+}
+
+void silenced7(void) {
+  char Buffer[BufferSize];
+
+  char *P = &Buffer[0];
+  const char *Q = P;
+  while (Q < P + BufferSize) {
+    sink(Q);
+    Q = Q + sizeof(*Q);
+  }
+}
+
+void good7(void) {
+  char Buffer[BufferSize];
+
+  char *P = &Buffer[0];
+  const char *Q = P;
+  while (Q < P + BufferSize) {
+    sink(Q);
+    Q = Q + 1;
+  }
+}
+
+void bad8(void) {
+  int Buffer[BufferSize];
+
+  int *P = &Buffer[0];
+  int *Q = P;
+  while (Q >= P) {
+    *Q = 0;
+    Q = Q - sizeof(*Q);
+    // CHECK-MESSAGES: :[[@LINE-1]]:11: warning: pointer arithmetic using a number scaled by 'sizeof()'; this value will be scaled again by the '-' operator
+    // CHECK-MESSAGES: :[[@LINE-2]]:11: note: '-' scales with 'sizeof(int)' == {{[0-9]+}}
+  }
+}
+
+void silenced8(void) {
+  char Buffer[BufferSize];
+
+  char *P = &Buffer[0];
+  char *Q = P;
+  while (Q >= P) {
+    *Q = 0;
+    Q = Q - sizeof(*Q);
+  }
+}
+
+void good8(void) {
+  char Buffer[BufferSize];
+
+  char *P = &Buffer[0];
+  char *Q = P;
+  while (Q >= P) {
+    *Q = 0;
+    Q = Q - 1;
+  }
+}
+
+void good9(void) {
+  int Buffer[BufferSize];
+
+  int *P = &Buffer[0];
+  int *Q = P + BufferSize;
+  int N = Q - P;
+  while (N >= 0) {
+    Q[N] = 0;
+    N = N - 1;
+  }
+}
+
+void good10(void) {
+  int Buffer[BufferSize];
+
+  int *P = &Buffer[0];
+  int *Q = Buffer + BufferSize;
+  int I = sizeof(*P) - sizeof(*Q);
+
+  sink(&I);
+}
+
+void good11(void) {
+  int Buffer[BufferSize];
+
+  int *P = &Buffer[0];
+  int *Q = Buffer + BufferSize;
+  int I = sizeof(Q) - sizeof(*P);
+
+  sink(&I);
 }
