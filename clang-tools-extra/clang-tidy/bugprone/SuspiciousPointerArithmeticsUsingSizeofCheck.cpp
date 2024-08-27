91288437f6f1543f351846a09e77c48175a2d74a
Whisperity <whisperity@gmail.com>
2024-08-27 15:24:30 +0200

    style: [NFC] `clang-format` the check's main file

--- clang-tools-extra/clang-tidy/bugprone/SuspiciousPointerArithmeticsUsingSizeofCheck.cpp
+++ clang-tools-extra/clang-tidy/bugprone/SuspiciousPointerArithmeticsUsingSizeofCheck.cpp
@@ -17,46 +17,56 @@ using namespace clang::ast_matchers;
 
 namespace clang::tidy::bugprone {
 
-//static const char *bin_op_bind = "ptr-sizeof-expression";	
+// static const char *bin_op_bind = "ptr-sizeof-expression";
 static constexpr llvm::StringLiteral BinOp{"bin-op"};
 static constexpr llvm::StringLiteral PointedType{"pointed-type"};
-static const auto IgnoredType = qualType(anyOf(asString("char"),asString("unsigned char"),asString("signed char"),asString("int8_t"),asString("uint8_t"),asString("std::byte"),asString("const char"),asString("const unsigned char"),asString("const signed char"),asString("const int8_t"),asString("const uint8_t"),asString("const std::byte")));
-static const auto InterestingPointer = pointerType(unless(pointee(IgnoredType)));
+static const auto IgnoredType = qualType(
+    anyOf(asString("char"), asString("unsigned char"), asString("signed char"),
+          asString("int8_t"), asString("uint8_t"), asString("std::byte"),
+          asString("const char"), asString("const unsigned char"),
+          asString("const signed char"), asString("const int8_t"),
+          asString("const uint8_t"), asString("const std::byte")));
+static const auto InterestingPointer =
+    pointerType(unless(pointee(IgnoredType)));
 
-SuspiciousPointerArithmeticsUsingSizeofCheck::SuspiciousPointerArithmeticsUsingSizeofCheck(
-    StringRef Name, ClangTidyContext *Context)
-    : ClangTidyCheck(Name, Context) {
-}
+SuspiciousPointerArithmeticsUsingSizeofCheck::
+    SuspiciousPointerArithmeticsUsingSizeofCheck(StringRef Name,
+                                                 ClangTidyContext *Context)
+    : ClangTidyCheck(Name, Context) {}
 
-void SuspiciousPointerArithmeticsUsingSizeofCheck::registerMatchers(MatchFinder *Finder) {
-    Finder->addMatcher(
-	     expr(anyOf(
-/*                    binaryOperator(hasAnyOperatorName("+","-"),
-                      hasEitherOperand(hasType(pointerType())),
-		      hasEitherOperand(sizeOfExpr(expr())),
-		      unless(allOf(hasLHS(hasType(pointerType())),
-				   hasRHS(hasType(pointerType()))))
-		      ).bind(bin_op_bind),
-		    binaryOperator(hasAnyOperatorName("+=","-="),
-	              hasLHS(hasType(pointerType())),
-		      hasRHS(sizeOfExpr(expr()))
-		      ).bind(bin_op_bind)
+void SuspiciousPointerArithmeticsUsingSizeofCheck::registerMatchers(
+    MatchFinder *Finder) {
+  Finder->addMatcher(
+      expr(anyOf(
+          /*                    binaryOperator(hasAnyOperatorName("+","-"),
+                                hasEitherOperand(hasType(pointerType())),
+                                hasEitherOperand(sizeOfExpr(expr())),
+                                unless(allOf(hasLHS(hasType(pointerType())),
+                                             hasRHS(hasType(pointerType()))))
+                                ).bind(bin_op_bind),
+                              binaryOperator(hasAnyOperatorName("+=","-="),
+                                hasLHS(hasType(pointerType())),
+                                hasRHS(sizeOfExpr(expr()))
+                                ).bind(bin_op_bind)
 
-		    binaryOperator(hasAnyOperatorName("+=","-=","+","-" ),
-	              hasLHS(hasType(InterestingPointer)),
-		      hasRHS(sizeOfExpr(expr()))).bind(BinOp),
-		    binaryOperator(hasAnyOperatorName("+","-" ),
-	              hasRHS(hasType(InterestingPointer)),
-		      hasLHS(sizeOfExpr(expr()))).bind(BinOp)
-*/		    
-		    binaryOperator(hasAnyOperatorName("+=","-=","+","-" ),
-	              hasLHS(hasType(pointerType(pointee(qualType().bind(PointedType))))),
-		      hasRHS(sizeOfExpr(expr()))).bind(BinOp),
-		    binaryOperator(hasAnyOperatorName("+","-" ),
-	              hasRHS(hasType(pointerType(pointee(qualType().bind(PointedType))))),
-		      hasLHS(sizeOfExpr(expr()))).bind(BinOp)
-            )),
-        this);
+                              binaryOperator(hasAnyOperatorName("+=","-=","+","-"
+             ), hasLHS(hasType(InterestingPointer)),
+                                hasRHS(sizeOfExpr(expr()))).bind(BinOp),
+                              binaryOperator(hasAnyOperatorName("+","-" ),
+                                hasRHS(hasType(InterestingPointer)),
+                                hasLHS(sizeOfExpr(expr()))).bind(BinOp)
+          */
+          binaryOperator(hasAnyOperatorName("+=", "-=", "+", "-"),
+                         hasLHS(hasType(pointerType(
+                             pointee(qualType().bind(PointedType))))),
+                         hasRHS(sizeOfExpr(expr())))
+              .bind(BinOp),
+          binaryOperator(hasAnyOperatorName("+", "-"),
+                         hasRHS(hasType(pointerType(
+                             pointee(qualType().bind(PointedType))))),
+                         hasLHS(sizeOfExpr(expr())))
+              .bind(BinOp))),
+      this);
 }
 
 static CharUnits getSizeOfType(const ASTContext &Ctx, const Type *Ty) {
@@ -66,20 +76,21 @@ static CharUnits getSizeOfType(const ASTContext &Ctx, const Type *Ty) {
   return Ctx.getTypeSizeInChars(Ty);
 }
 
-void SuspiciousPointerArithmeticsUsingSizeofCheck::check(const MatchFinder::MatchResult &Result) {
-    const ASTContext &Ctx = *Result.Context;
-    const auto *Matched = Result.Nodes.getNodeAs<BinaryOperator>(BinOp);
-    const auto *SuspiciousQualTypePtr =
-        Result.Nodes.getNodeAs<QualType>(PointedType);
-    const auto *SuspiciousTypePtr = SuspiciousQualTypePtr->getTypePtr();
+void SuspiciousPointerArithmeticsUsingSizeofCheck::check(
+    const MatchFinder::MatchResult &Result) {
+  const ASTContext &Ctx = *Result.Context;
+  const auto *Matched = Result.Nodes.getNodeAs<BinaryOperator>(BinOp);
+  const auto *SuspiciousQualTypePtr =
+      Result.Nodes.getNodeAs<QualType>(PointedType);
+  const auto *SuspiciousTypePtr = SuspiciousQualTypePtr->getTypePtr();
 
-    std::size_t sz = getSizeOfType(Ctx,SuspiciousTypePtr).getQuantity();
-    if ( sz > 1 )
-    {
-        diag(Matched->getExprLoc(),"Suspicious pointer arithmetics using sizeof() operator: sizeof(%0) is %1") << SuspiciousQualTypePtr->getAsString(Ctx.getPrintingPolicy())
-	                       << sz
-		               << Matched->getSourceRange();
-    }
+  std::size_t sz = getSizeOfType(Ctx, SuspiciousTypePtr).getQuantity();
+  if (sz > 1) {
+    diag(Matched->getExprLoc(), "Suspicious pointer arithmetics using sizeof() "
+                                "operator: sizeof(%0) is %1")
+        << SuspiciousQualTypePtr->getAsString(Ctx.getPrintingPolicy()) << sz
+        << Matched->getSourceRange();
+  }
 }
 
 } // namespace clang::tidy::bugprone
