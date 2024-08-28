6dc5002e2a0e3552aeadf3489c035304f4864b36
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2024-08-28 08:56:59 +0200

    Document false negatives on multiple inheritance

--- clang/test/Analysis/block-in-critical-section-inheritance.cpp
+++ clang/test/Analysis/block-in-critical-section-inheritance.cpp
@@ -54,3 +54,77 @@ void two_mutexes_no_false_negative(TwoMutexes &tm) {
   // expected-note@-2 {{Call to blocking function 'sleep' inside of critical section}}
   tm.m1.unlock();
 }
+
+struct MyMutexBase1 : std::mutex {
+  void lock1() { lock(); }
+  void unlock1() { unlock(); }
+};
+struct MyMutexBase2 : std::mutex {
+  void lock2() { lock(); }
+  void unlock2() { unlock(); }
+};
+struct MyMutex : MyMutexBase1, MyMutexBase2 {};
+// MyMutex has two distinct std::mutex as base classes
+
+void custom_mutex_tp(MyMutexBase1 &mb) {
+  mb.lock();
+  // expected-note@-1 {{Entering critical section here}}
+  sleep(10);
+  // expected-warning@-1 {{Call to blocking function 'sleep' inside of critical section}}
+  // expected-note@-2 {{Call to blocking function 'sleep' inside of critical section}}
+  mb.unlock();
+}
+
+void custom_mutex_tn(MyMutexBase1 &mb) {
+  mb.lock();
+  mb.unlock();
+  sleep(10);
+}
+
+void custom_mutex_cast_tp(MyMutexBase1 &mb) {
+  static_cast<std::mutex&>(mb).lock();
+  // expected-note@-1 {{Entering critical section here}}
+  sleep(10);
+  // expected-warning@-1 {{Call to blocking function 'sleep' inside of critical section}}
+  // expected-note@-2 {{Call to blocking function 'sleep' inside of critical section}}
+  static_cast<std::mutex&>(mb).unlock();
+}
+
+void custom_mutex_cast_tn(MyMutexBase1 &mb) {
+  static_cast<std::mutex&>(mb).lock();
+  static_cast<std::mutex&>(mb).unlock();
+  sleep(10);
+}
+
+void two_custom_mutex_bases_fn(MyMutex &m) {
+  m.lock1();
+  m.unlock2();
+  // The critical section is associated with `m` ignoring the fact that m holds
+  // two mutexes. hense unlock2 is considered to unlock the same mutex as lock1
+  // locks
+  sleep(10); // False negative
+  m.unlock1();
+}
+
+void two_custom_mutex_bases_tn(MyMutex &m) {
+  m.lock1();
+  m.unlock1();
+  sleep(10);
+}
+
+void two_custom_mutex_bases_casts_fn(MyMutex &m) {
+  static_cast<MyMutexBase1&>(m).lock();
+  static_cast<MyMutexBase2&>(m).unlock();
+  // The critical section is associated with `m` ignoring the fact that m holds
+  // two mutexes. hense MyMutexBase2::unlock is considered to unlock the same
+  // mutex as MyMutexBase1::lock locks
+  sleep(10); // False negative
+  static_cast<MyMutexBase1&>(m).unlock();
+}
+
+void two_custom_mutex_bases_casts_tn(MyMutex &m) {
+  static_cast<MyMutexBase1&>(m).lock();
+  static_cast<MyMutexBase1&>(m).unlock();
+  sleep(10);
+}
+
