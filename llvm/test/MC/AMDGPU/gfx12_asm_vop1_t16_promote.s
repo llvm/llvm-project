d7b8922d1180d2530d52a39a0204d7475d81f457
guochen2 <guochen2@amd.com>
2024-08-29 15:47:16 -0400

    address comment, add gfx12 asm/dasm test

--- llvm/test/MC/AMDGPU/gfx12_asm_vop1_t16_promote.s
+++ llvm/test/MC/AMDGPU/gfx12_asm_vop1_t16_promote.s
@@ -1,4 +1,4 @@
-// RUN: llvm-mc -triple=amdgcn -mcpu=gfx1200 -show-encoding %s | FileCheck --check-prefix=GFX12 --implicit-check-not=_e32 %s
+// RUN: llvm-mc -triple=amdgcn -mcpu=gfx1200 -mattr=+real-true16 -show-encoding %s | FileCheck --check-prefix=GFX12 --implicit-check-not=_e32 %s
 
 v_ceil_f16 v128, 0xfe0b
 // GFX12: v_ceil_f16_e64
@@ -96,51 +96,6 @@ v_cos_f16 v255, vcc_lo
 v_cos_f16 v5, v199
 // GFX12: v_cos_f16_e64
 
-v_cvt_f16_f32 v128, 0xaf123456
-// GFX12: v_cvt_f16_f32_e64
-
-v_cvt_f16_f32 v255, -1
-// GFX12: v_cvt_f16_f32_e64
-
-v_cvt_f16_f32 v255, 0.5
-// GFX12: v_cvt_f16_f32_e64
-
-v_cvt_f16_f32 v255, exec_hi
-// GFX12: v_cvt_f16_f32_e64
-
-v_cvt_f16_f32 v255, exec_lo
-// GFX12: v_cvt_f16_f32_e64
-
-v_cvt_f16_f32 v255, m0
-// GFX12: v_cvt_f16_f32_e64
-
-v_cvt_f16_f32 v255, null
-// GFX12: v_cvt_f16_f32_e64
-
-v_cvt_f16_f32 v255, s1
-// GFX12: v_cvt_f16_f32_e64
-
-v_cvt_f16_f32 v255, s105
-// GFX12: v_cvt_f16_f32_e64
-
-v_cvt_f16_f32 v255, src_scc
-// GFX12: v_cvt_f16_f32_e64
-
-v_cvt_f16_f32 v255, ttmp15
-// GFX12: v_cvt_f16_f32_e64
-
-v_cvt_f16_f32 v255, v1
-// GFX12: v_cvt_f16_f32_e64
-
-v_cvt_f16_f32 v255, v255
-// GFX12: v_cvt_f16_f32_e64
-
-v_cvt_f16_f32 v255, vcc_hi
-// GFX12: v_cvt_f16_f32_e64
-
-v_cvt_f16_f32 v255, vcc_lo
-// GFX12: v_cvt_f16_f32_e64
-
 v_cvt_f16_i16 v128, 0xfe0b
 // GFX12: v_cvt_f16_i16_e64
 
@@ -237,7 +192,7 @@ v_cvt_f16_u16 v255, vcc_lo
 v_cvt_f16_u16 v5, v199
 // GFX12: v_cvt_f16_u16_e64
 
-v_cvt_f32_f16 v5, v199
+v_cvt_f32_f16 v5, v199.l
 // GFX12: v_cvt_f32_f16_e64
 
 v_cvt_i16_f16 v128, 0xfe0b
@@ -1107,7 +1062,7 @@ v_cvt_f16_u16 v255, v127 quad_perm:[3,2,1,0]
 v_cvt_f16_u16 v5, v199 quad_perm:[3,2,1,0]
 // GFX12: v_cvt_f16_u16_e64
 
-v_cvt_f32_f16 v5, v199 quad_perm:[3,2,1,0]
+v_cvt_f32_f16 v5, v199.l quad_perm:[3,2,1,0]
 // GFX12: v_cvt_f32_f16_e64
 
 v_cvt_i16_f16 v255, v1 quad_perm:[3,2,1,0]
@@ -1314,7 +1269,7 @@ v_cvt_f16_u16 v255, v127 dpp8:[7,6,5,4,3,2,1,0]
 v_cvt_f16_u16 v5, v199 dpp8:[7,6,5,4,3,2,1,0]
 // GFX12: v_cvt_f16_u16_e64
 
-v_cvt_f32_f16 v5, v199 dpp8:[7,6,5,4,3,2,1,0]
+v_cvt_f32_f16 v5, v199.l dpp8:[7,6,5,4,3,2,1,0]
 // GFX12: v_cvt_f32_f16_e64
 
 v_cvt_i16_f16 v255, v1 dpp8:[7,6,5,4,3,2,1,0]
@@ -1478,3 +1433,88 @@ v_trunc_f16 v255, v127 dpp8:[7,6,5,4,3,2,1,0]
 
 v_trunc_f16 v5, v199 dpp8:[7,6,5,4,3,2,1,0]
 // GFX12: v_trunc_f16_e64
+
+v_cvt_f32_f16 v5, v199.h
+// GFX12: v_cvt_f32_f16_e64
+
+v_cvt_f32_f16 v5, v199.h quad_perm:[3,2,1,0]
+// GFX12: v_cvt_f32_f16_e64
+
+v_cvt_f32_f16 v5, v199.h dpp8:[7,6,5,4,3,2,1,0]
+// GFX12: v_cvt_f32_f16_e64
+
+v_cvt_f16_f32 v255.l, v1
+// GFX12: v_cvt_f16_f32_e64
+
+v_cvt_f16_f32 v255.l, v255
+// GFX12: v_cvt_f16_f32_e64
+
+v_cvt_f16_f32 v255.l, v1 quad_perm:[3,2,1,0]
+// GFX12: v_cvt_f16_f32_e64
+
+v_cvt_f16_f32 v255.l, v255 quad_perm:[3,2,1,0]
+// GFX12: v_cvt_f16_f32_e64
+
+v_cvt_f16_f32 v255.l, v1 dpp8:[7,6,5,4,3,2,1,0]
+// GFX12: v_cvt_f16_f32_e64
+
+v_cvt_f16_f32 v255.l, v255 dpp8:[7,6,5,4,3,2,1,0]
+// GFX12: v_cvt_f16_f32_e64
+
+v_cvt_f16_f32 v128.h, 0xaf123456
+// GFX12: v_cvt_f16_f32_e64
+
+v_cvt_f16_f32 v255.h, -1
+// GFX12: v_cvt_f16_f32_e64
+
+v_cvt_f16_f32 v255.h, 0.5
+// GFX12: v_cvt_f16_f32_e64
+
+v_cvt_f16_f32 v255.h, exec_hi
+// GFX12: v_cvt_f16_f32_e64
+
+v_cvt_f16_f32 v255.h, exec_lo
+// GFX12: v_cvt_f16_f32_e64
+
+v_cvt_f16_f32 v255.h, m0
+// GFX12: v_cvt_f16_f32_e64
+
+v_cvt_f16_f32 v255.h, null
+// GFX12: v_cvt_f16_f32_e64
+
+v_cvt_f16_f32 v255.h, s1
+// GFX12: v_cvt_f16_f32_e64
+
+v_cvt_f16_f32 v255.h, s105
+// GFX12: v_cvt_f16_f32_e64
+
+v_cvt_f16_f32 v255.h, src_scc
+// GFX12: v_cvt_f16_f32_e64
+
+v_cvt_f16_f32 v255.h, ttmp15
+// GFX12: v_cvt_f16_f32_e64
+
+v_cvt_f16_f32 v255.h, v1
+// GFX12: v_cvt_f16_f32_e64
+
+v_cvt_f16_f32 v255.h, v255
+// GFX12: v_cvt_f16_f32_e64
+
+v_cvt_f16_f32 v255.h, vcc_hi
+// GFX12: v_cvt_f16_f32_e64
+
+v_cvt_f16_f32 v255.h, vcc_lo
+// GFX12: v_cvt_f16_f32_e64
+
+v_cvt_f16_f32 v255.h, v1 quad_perm:[3,2,1,0]
+// GFX12: v_cvt_f16_f32_e64
+
+v_cvt_f16_f32 v255.h, v255 quad_perm:[3,2,1,0]
+// GFX12: v_cvt_f16_f32_e64
+
+v_cvt_f16_f32 v255.h, v1 dpp8:[7,6,5,4,3,2,1,0]
+// GFX12: v_cvt_f16_f32_e64
+
+v_cvt_f16_f32 v255.h, v255 dpp8:[7,6,5,4,3,2,1,0]
+// GFX12: v_cvt_f16_f32_e64
+
