7c7201c86b0724e4858ee059226ab03b5783e751
Christudasan Devadasan <Christudasan.Devadasan@amd.com>
2024-08-30 00:26:22 +0530

    comment padded for 80 chars.

--- llvm/include/llvm/CodeGen/MachineCSE.h
+++ llvm/include/llvm/CodeGen/MachineCSE.h
@@ -1,4 +1,4 @@
-//===- llvm/CodeGen/MachineCSE.h -----------------*- C++ -*-===//
+//===- llvm/CodeGen/MachineCSE.h --------------------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
