5ae5da0f8567f678450386656fb766d3b1ebf8a8
Chuanqi Xu <yedeng.yd@linux.alibaba.com>
2024-08-30 15:15:22 +0800

    [NFC] [clangd] [Modules] Change the argument type of IsModuleFileUpToDate to reference
    It is better to use references instead of pointers as the argument type
    of IsModuleFileUpToDate. Since the PrerequisiteModules is always
    expected to exist.

--- clang-tools-extra/clangd/ModulesBuilder.cpp
+++ clang-tools-extra/clangd/ModulesBuilder.cpp
@@ -129,13 +129,12 @@ struct ModuleFile {
 
 bool IsModuleFileUpToDate(
     PathRef ModuleFilePath,
-    const PrerequisiteModules *RequisiteModules) {
+    const PrerequisiteModules &RequisiteModules) {
 IntrusiveRefCntPtr<DiagnosticsEngine> Diags =
       CompilerInstance::createDiagnostics(new DiagnosticOptions());
 
   auto HSOpts = std::make_shared<HeaderSearchOptions>();
-  if (RequisiteModules)
-    RequisiteModules->adjustHeaderSearchOptions(*HSOpts);
+  RequisiteModules.adjustHeaderSearchOptions(*HSOpts);
   HSOpts->ForceCheckCXX20ModulesInputFiles = true;
   HSOpts->ValidateASTInputFilesContent = true;
 
@@ -168,8 +167,8 @@ IntrusiveRefCntPtr<DiagnosticsEngine> Diags =
 
 bool IsModuleFilesUpToDate(
     llvm::SmallVector<PathRef> ModuleFilePaths,
-    const PrerequisiteModules *RequisiteModules = nullptr) {
-  return llvm::all_of(ModuleFilePaths, [RequisiteModules](auto ModuleFilePath) {
+    const PrerequisiteModules &RequisiteModules) {
+  return llvm::all_of(ModuleFilePaths, [&RequisiteModules](auto ModuleFilePath) {
     return IsModuleFileUpToDate(ModuleFilePath, RequisiteModules);
   });
 }
@@ -348,7 +347,7 @@ bool StandalonePrerequisiteModules::canReuse(
   SmallVector<StringRef> BMIPaths;
   for (auto &MF : RequiredModules)
     BMIPaths.push_back(MF.ModuleFilePath);
-  return IsModuleFilesUpToDate(BMIPaths, this);
+  return IsModuleFilesUpToDate(BMIPaths, *this);
 }
 
 } // namespace clangd
