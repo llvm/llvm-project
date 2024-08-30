2aee2511c4035846cd25a230dac5675f17964158
adeel10x <adeel.ahmad@10xengineers.ai>
2024-08-31 03:47:03 +0500

    [libcxx] Removed separate function templates for input and segmented iterators. Used a single function template that calls std::for_each.

--- libcxx/include/__algorithm/count_if.h
+++ libcxx/include/__algorithm/count_if.h
@@ -21,41 +21,18 @@
 
 _LIBCPP_BEGIN_NAMESPACE_STD
 
-template <class _InputIterator,
-          class _Predicate,
-          __enable_if_t<!__is_segmented_iterator<_InputIterator>::value, int> = 0>
+template <class _InputIterator, class _Predicate>
 _LIBCPP_NODISCARD inline _LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX20
     typename iterator_traits<_InputIterator>::difference_type
-    __count_if(_InputIterator __first, _InputIterator __last, _Predicate __pred) {
+    count_if(_InputIterator __first, _InputIterator __last, _Predicate __pred) {
   typename iterator_traits<_InputIterator>::difference_type __r(0);
-  for (; __first != __last; ++__first)
-    if (__pred(*__first))
-      ++__r;
-  return __r;
-}
-
-template <class _SegmentedIterator,
-          class _Predicate,
-          __enable_if_t<__is_segmented_iterator<_SegmentedIterator>::value, int> = 0>
-_LIBCPP_NODISCARD inline _LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX20
-    typename iterator_traits<_SegmentedIterator>::difference_type
-    __count_if(_SegmentedIterator __first, _SegmentedIterator __last, _Predicate __pred) {
-  typename iterator_traits<_SegmentedIterator>::difference_type __r(0);
-  std::for_each(__first, __last, [&__r, __pred](auto& __val) mutable {
+  std::for_each(__first, __last, [&__r, &__pred](auto& __val) mutable {
     if (__pred(__val))
       ++__r;
   });
   return __r;
 }
 
-template <class _InputIterator, class _Predicate>
-_LIBCPP_NODISCARD inline _LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX20
-    typename iterator_traits<_InputIterator>::difference_type
-    count_if(_InputIterator __first, _InputIterator __last, _Predicate __pred) {
-  return __count_if(__first, __last, __pred);
-}
-
 _LIBCPP_END_NAMESPACE_STD
 
 #endif // _LIBCPP___ALGORITHM_COUNT_IF_H
-
