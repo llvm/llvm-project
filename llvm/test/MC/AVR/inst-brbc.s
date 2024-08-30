830b7ebac09ebef91671f0863986aee1a1d60e5e
Patryk Wychowaniec <pwychowaniec@pm.me>
2024-08-30 15:25:54 +0200

    [AVR] Fix parsing & emitting relative jumps (#106722)
    Ever since 6859685a87ad093d60c8bed60b116143c0a684c7 (or, precisely,
    84428dafc0941e3a31303fa1b286835ab2b8e234) relative jumps emitted by the
    AVR codegen are off by two bytes - this pull request fixes it.
    
    ## Abstract
    
    As compared to absolute jumps, relative jumps - such as rjmp, rcall or
    brsh - have an implied `pc+2` behavior; that is, `jmp 100` is `pc =
    100`, but `rjmp 100` gets understood as `pc = pc + 100 + 2`.
    
    This is not reflected in the AVR codegen:
    
    https://github.com/llvm/llvm-project/blob/f95026dbf66e353128a3a3d7b55f3e52d5985535/llvm/lib/Target/AVR/MCTargetDesc/AVRAsmBackend.cpp#L89
    
    ... which always emits relative jumps that are two bytes too far - or
    rather it _would_ emit such jumps if not for this check:
    
    https://github.com/llvm/llvm-project/blob/f95026dbf66e353128a3a3d7b55f3e52d5985535/llvm/lib/Target/AVR/MCTargetDesc/AVRAsmBackend.cpp#L517
    
    ... which causes most of the relative jumps to be actually resolved
    late, by the linker, which applies the offsetting logic on its own,
    hiding the issue within LLVM.
    
    [Some time
    ago](https://github.com/llvm/llvm-project/commit/697a162fa63df328ec9ca334636c5e85390b2bf0)
    we've had a similar "jumps are off" problem that got solved by touching
    `shouldForceRelocation()`, but I think that has worked only by accident.
    It's exploited the fact that absolute vs relative jumps in the parsed
    assembly can be distinguished through a "side channel" check relying on
    the existence of labels (i.e. absolute jumps happen to named labels, but
    relative jumps are anonymous, so to say). This was an alright idea back
    then, but it got broken by 6859685a87ad093d60c8bed60b116143c0a684c7.
    
    I propose a different approach:
    - when emitting relative jumps, offset them by `-2` (well, `-1`,
    strictly speaking, because those instructions rely on right-shifted
    offset),
    - when parsing relative jumps, treat `.` as `+2` and read `rjmp .+1234`
    as `rjmp (1234 + 2)`.
    
    This approach seems to be sound and now we generate the same assembly as
    avr-gcc, which can be confirmed with:
    
    ```cpp
    // avr-gcc test.c -O3 && avr-objdump -d a.out
    
    int main() {
        asm(
    "      foo:\n\t"
    "        rjmp  .+2\n\t"
    "        rjmp  .-2\n\t"
    "        rjmp  foo\n\t"
    "        rjmp  .+8\n\t"
    "        rjmp  end\n\t"
    "        rjmp  .+0\n\t"
    "      end:\n\t"
    "        rjmp .-4\n\t"
    "        rjmp .-6\n\t"
    "      x:\n\t"
    "        rjmp x\n\t"
    "        .short 0xc00f\n\t"
    );
    }
    ```
    
    avr-gcc is also how I got the opcodes for all new tests like `inst-brbc.s`, so we should be good.
    
    (cherry picked from commit 86a60e7f1e8f361f84ccb6e656e848dd4fbaa713)

--- llvm/test/MC/AVR/inst-brbc.s
+++ llvm/test/MC/AVR/inst-brbc.s
@@ -3,7 +3,6 @@
 ; RUN:     | llvm-objdump -d - | FileCheck --check-prefix=INST %s
 
 foo:
-
   brbc 3, .+8
   brbc 0, .-16
   .short 0xf759
@@ -11,14 +10,16 @@ foo:
   .short 0xf74c
   .short 0xf4c7
 
-; CHECK: brvc .Ltmp0+8              ; encoding: [0bAAAAA011,0b111101AA]
-; CHECK:                            ; fixup A - offset: 0, value: .Ltmp0+8, kind: fixup_7_pcrel
-; CHECK: brcc .Ltmp1-16             ; encoding: [0bAAAAA000,0b111101AA]
-; CHECK:                            ; fixup A - offset: 0, value: .Ltmp1-16, kind: fixup_7_pcrel
+; CHECK: brvc (.Ltmp0+8)+2   ; encoding: [0bAAAAA011,0b111101AA]
+; CHECK-NEXT:                ; fixup A - offset: 0, value: (.Ltmp0+8)+2, kind: fixup_7_pcrel
+;
+; CHECK: brcc (.Ltmp1-16)+2  ; encoding: [0bAAAAA000,0b111101AA]
+; CHECK-NEXT:                ; fixup A - offset: 0, value: (.Ltmp1-16)+2, kind: fixup_7_pcrel
 
-; INST: 23 f4   brvc .+8
-; INST: c0 f7   brsh .-16
-; INST: 59 f7   brne .-42
-; INST: 52 f7   brpl .-44
-; INST: 4c f7   brge .-46
-; INST: c7 f4   brid .+48
+; INST-LABEL: <foo>:
+; INST-NEXT: 23 f4   brvc .+8
+; INST-NEXT: c0 f7   brsh .-16
+; INST-NEXT: 59 f7   brne .-42
+; INST-NEXT: 52 f7   brpl .-44
+; INST-NEXT: 4c f7   brge .-46
+; INST-NEXT: c7 f4   brid .+48
