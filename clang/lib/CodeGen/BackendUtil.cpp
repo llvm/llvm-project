5c0d61e318a77434487fcec8361d8110fb06e59d
Kazu Hirata <kazu@google.com>
2024-09-01 08:36:06 -0700

    [LTO] Reduce memory usage for import lists (#106772)
    This patch reduces the memory usage for import lists by employing
    memory-efficient data structures.
    
    With this patch, an import list for a given destination module is
    basically DenseSet<uint32_t> with each element indexing into the
    deduplication table containing tuples of:
    
      {SourceModule, GUID, Definition/Declaration}
    
    In one of our large applications, the peak memory usage goes down by
    9.2% from 6.120GB to 5.555GB during the LTO indexing step.
    
    This patch addresses several sources of space inefficiency associated
    with std::unordered_map:
    
    - std::unordered_map<GUID, ImportKind> takes up 16 bytes because of
      padding even though ImportKind only carries one bit of information.
    
    - std::unordered_map uses pointers to elements, both in the hash table
      proper and for collision chains.
    
    - We allocate an instance of std::unordered_map for each
      {Destination Module, Source Module} pair for which we have at least
      one import.  Most import lists have less than 10 imports, so the
      metadata like the size of std::unordered_map and the pointer to the
      hash table costs a lot relative to the actual contents.

--- clang/lib/CodeGen/BackendUtil.cpp
+++ clang/lib/CodeGen/BackendUtil.cpp
@@ -1206,7 +1206,8 @@ static void runThinLTOBackend(
   // We can simply import the values mentioned in the combined index, since
   // we should only invoke this using the individual indexes written out
   // via a WriteIndexesThinBackend.
-  FunctionImporter::ImportMapTy ImportList;
+  FunctionImporter::ImportIDTable ImportIDs;
+  FunctionImporter::ImportMapTy ImportList(ImportIDs);
   if (!lto::initImportList(*M, *CombinedIndex, ImportList))
     return;
 
