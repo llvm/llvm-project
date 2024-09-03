f94fb03bea31f35aa970f45fe797442988a2956c
Krystian Stasiowski <sdkrystian@gmail.com>
2024-09-03 09:31:16 -0400

    [FOLD] more tests

--- clang/test/CXX/temp/temp.constr/temp.constr.decl/p4.cpp
+++ clang/test/CXX/temp/temp.constr/temp.constr.decl/p4.cpp
@@ -2,12 +2,18 @@
 // expected-no-diagnostics
 
 template<typename T>
+concept D = true;
+
+template<typename T>
 struct A {
   template<typename U, bool V>
   void f() requires V;
 
   template<>
   void f<short, true>();
+
+  template<D U>
+  void g();
 
   template<typename U, bool V> requires V
   struct B;
@@ -18,6 +24,12 @@ struct A {
   template<>
   struct B<short, true>;
 
+  template<D U>
+  struct C;
+
+  template<D U>
+  struct C<U*>;
+
   template<typename U, bool V> requires V
   static int x;
 
@@ -26,11 +38,21 @@ struct A {
 
   template<>
   int x<short, true>;
+
+  template<D U>
+  static int y;
+
+  template<D U>
+  static int y<U*>;
 };
 
 template<typename T>
 template<typename U, bool V>
 void A<T>::f() requires V { }
+
+template<typename T>
+template<D U>
+void A<T>::g() { }
 
 template<typename T>
 template<typename U, bool V> requires V
@@ -45,6 +67,14 @@ template<typename U, bool V> requires V
 struct A<T>::B<U&, V> { };
 
 template<typename T>
+template<D U>
+struct A<T>::C { };
+
+template<typename T>
+template<D U>
+struct A<T>::C<U*> { };
+
+template<typename T>
 template<typename U, bool V> requires V
 int A<T>::x = 0;
 
@@ -55,6 +85,14 @@ int A<T>::x<U*, V> = 0;
 template<typename T>
 template<typename U, bool V> requires V
 int A<T>::x<U&, V> = 0;
+
+template<typename T>
+template<D U>
+int A<T>::y = 0;
+
+template<typename T>
+template<D U>
+int A<T>::y<U*> = 0;
 
 template<>
 template<typename U, bool V>
@@ -67,6 +105,10 @@ void A<short>::f<int, true>();
 template<>
 template<>
 void A<void>::f<int, true>();
+
+template<>
+template<D U>
+void A<short>::g();
 
 template<>
 template<typename U, bool V> requires V
@@ -89,6 +131,18 @@ template<typename U, bool V> requires V
 struct A<int>::B<U&, V>;
 
 template<>
+template<D U>
+struct A<int>::C;
+
+template<>
+template<D U>
+struct A<int>::C<U*>;
+
+template<>
+template<D U>
+struct A<int>::C<U&>;
+
+template<>
 template<typename U, bool V> requires V
 int A<long>::x;
 
@@ -107,3 +161,15 @@ int A<long>::x<U*, V>;
 template<>
 template<typename U, bool V> requires V
 int A<long>::x<U&, V>;
+
+template<>
+template<D U>
+int A<long>::y;
+
+template<>
+template<D U>
+int A<long>::y<U*>;
+
+template<>
+template<D U>
+int A<long>::y<U&>;
