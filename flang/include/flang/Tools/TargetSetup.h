697bc748f97736b294dd85b8f78530d023557b72
Renaud Kauffmann <rkauffmann@nvidia.com>
2024-09-04 08:59:55 -0700

    Allow disabling of types from the command line (#107126)
    Adding hidden options to disable types through the
    `TargetCharacteristics`. I am seeing issues when I do this
    programmatically and would like, for anyone, to have the ability to
    reproduce them for development and testing purposes.
    
    I am planning to file a couple of issues following this patch.

--- flang/include/flang/Tools/TargetSetup.h
+++ flang/include/flang/Tools/TargetSetup.h
@@ -10,6 +10,7 @@
 #define FORTRAN_TOOLS_TARGET_SETUP_H
 
 #include "flang/Evaluate/target.h"
+#include "flang/Frontend/TargetOptions.h"
 #include "llvm/Target/TargetMachine.h"
 
 namespace Fortran::tools {
@@ -17,6 +18,7 @@ namespace Fortran::tools {
 [[maybe_unused]] inline static void setUpTargetCharacteristics(
     Fortran::evaluate::TargetCharacteristics &targetCharacteristics,
     const llvm::TargetMachine &targetMachine,
+    const Fortran::frontend::TargetOptions &targetOptions,
     const std::string &compilerVersion, const std::string &compilerOptions) {
 
   const llvm::Triple &targetTriple{targetMachine.getTargetTriple()};
@@ -24,6 +26,12 @@ namespace Fortran::tools {
   if (targetTriple.getArch() != llvm::Triple::ArchType::x86_64)
     targetCharacteristics.DisableType(
         Fortran::common::TypeCategory::Real, /*kind=*/10);
+
+  for (auto realKind : targetOptions.disabledRealKinds)
+    targetCharacteristics.DisableType(common::TypeCategory::Real, realKind);
+
+  for (auto intKind : targetOptions.disabledIntegerKinds)
+    targetCharacteristics.DisableType(common::TypeCategory::Integer, intKind);
 
   targetCharacteristics.set_compilerOptionsString(compilerOptions)
       .set_compilerVersionString(compilerVersion);
