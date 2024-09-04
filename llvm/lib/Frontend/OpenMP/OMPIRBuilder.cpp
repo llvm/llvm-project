b71e38418dfc12d9d6404bed52b81dc975e7cfc7
Akash Banerjee <akash.banerjee@amd.com>
2024-09-04 12:36:03 +0100

    [OpenMP]Update use_device_clause lowering (#101707)
    This patch updates the use_device_ptr and use_device_addr clauses to use
    the mapInfoOps for lowering. This allows all the types that are handle
    by the map clauses such as derived types to also be supported by the
    use_device_clauses.
    
    This is patch 2/2 in a series of patches.

--- llvm/lib/Frontend/OpenMP/OMPIRBuilder.cpp
+++ llvm/lib/Frontend/OpenMP/OMPIRBuilder.cpp
@@ -6371,6 +6371,7 @@ OpenMPIRBuilder::InsertPointTy OpenMPIRBuilder::createTargetData(
   if (!updateToLocation(Loc))
     return InsertPointTy();
 
+  Builder.restoreIP(CodeGenIP);
   // Disable TargetData CodeGen on Device pass.
   if (Config.IsTargetDevice.value_or(false)) {
     if (BodyGenCB)
@@ -6378,7 +6379,6 @@ OpenMPIRBuilder::InsertPointTy OpenMPIRBuilder::createTargetData(
     return Builder.saveIP();
   }
 
-  Builder.restoreIP(CodeGenIP);
   bool IsStandAlone = !BodyGenCB;
   MapInfosTy *MapInfo;
   // Generate the code for the opening of the data environment. Capture all the
