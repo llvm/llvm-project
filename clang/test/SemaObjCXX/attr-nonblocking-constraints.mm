54feb05e7e697de5a98089693d1a6b3cabb0c37f
Doug Wyatt <dwyatt@apple.com>
2024-09-04 17:33:42 -0700

    Fix test broken by rewording of warnings.

--- clang/test/SemaObjCXX/attr-nonblocking-constraints.mm
+++ clang/test/SemaObjCXX/attr-nonblocking-constraints.mm
@@ -8,19 +8,19 @@
 @end
 
 void nb1(OCClass *oc) [[clang::nonblocking]] {
-	[oc method]; // expected-warning {{'nonblocking' function must not access ObjC methods or properties}}
+	[oc method]; // expected-warning {{function with 'nonblocking' attribute must not access ObjC methods or properties}}
 }
 void nb2(OCClass *oc) {
 	[oc method]; // expected-note {{function cannot be inferred 'nonblocking' because it accesses an ObjC method or property}}
 }
 void nb3(OCClass *oc) [[clang::nonblocking]] {
-	nb2(oc); // expected-warning {{'nonblocking' function must not call non-'nonblocking' function 'nb2'}}
+	nb2(oc); // expected-warning {{function with 'nonblocking' attribute must not call non-'nonblocking' function 'nb2'}}
 }
 
 void nb4() [[clang::nonblocking]] {
 	@try {
-		@throw @"foo"; // expected-warning {{'nonblocking' function must not throw or catch exceptions}}
+		@throw @"foo"; // expected-warning {{function with 'nonblocking' attribute must not throw or catch exceptions}}
 	}
-	@catch (...) { // expected-warning {{'nonblocking' function must not throw or catch exceptions}}
+	@catch (...) { // expected-warning {{function with 'nonblocking' attribute must not throw or catch exceptions}}
 	}
 }
