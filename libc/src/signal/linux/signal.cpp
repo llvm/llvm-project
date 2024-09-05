e8857837c71f438048ae36b0856749c7d07cb3e4
wldfngrs <wldfngrs@gmail.com>
2024-09-05 06:43:16 +0100

    add proxy headers for __sighandler_t type

--- libc/src/signal/linux/signal.cpp
+++ libc/src/signal/linux/signal.cpp
@@ -8,13 +8,15 @@
 
 #include "src/signal/signal.h"
 #include "hdr/signal_macros.h"
+#include "include/llvm-libc-types/__sighandler_t.h"
 #include "src/__support/common.h"
 #include "src/__support/macros/config.h"
 #include "src/signal/sigaction.h"
 
 namespace LIBC_NAMESPACE_DECL {
 
-LLVM_LIBC_FUNCTION(sighandler_t, signal, (int signum, sighandler_t handler)) {
+LLVM_LIBC_FUNCTION(__sighandler_t, signal,
+                   (int signum, __sighandler_t handler)) {
   struct sigaction action, old;
   action.sa_handler = handler;
   action.sa_flags = SA_RESTART;
