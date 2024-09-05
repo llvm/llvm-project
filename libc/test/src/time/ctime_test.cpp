f41a8ed581ac5a56fdb5e5360d99498850c77de6
Зишан Мирза <149377404+zimirza@users.noreply.github.com>
2024-09-05 20:19:06 +0200

    [libc][c11] implement ctime
    fix: tests

--- libc/test/src/time/ctime_test.cpp
+++ libc/test/src/time/ctime_test.cpp
@@ -12,10 +12,7 @@
 #include "test/UnitTest/Test.h"
 #include "test/src/time/TmHelper.h"
 
-static inline char *call_ctime(struct time_t *t, int year, int month, int mday,
-                               int hour, int min, int sec, int wday, int yday) {
-  LIBC_NAMESPACE::tmhelper::testing::initialize_tm_data(
-      localtime(t), year, month, mday, hour, min, sec, wday, yday);
+static inline char *call_ctime(struct time_t *t) {
   return LIBC_NAMESPACE::ctime(t);
 }
 
@@ -26,189 +23,14 @@ TEST(LlvmLibcCtime, Nullptr) {
   ASSERT_STREQ(nullptr, result);
 }
 
-// Weekdays are in the range 0 to 6. Test passing invalid value in wday.
-TEST(LlvmLibcCtime, InvalidWday) {
-  struct time_t t;
-
-  // Test with wday = -1.
-  call_ctime(&t,
-             1970, // year
-             1,    // month
-             1,    // day
-             0,    // hr
-             0,    // min
-             0,    // sec
-             -1,   // wday
-             0);   // yday
-  ASSERT_ERRNO_EQ(EINVAL);
-
-  // Test with wday = 7.
-  call_ctime(&t,
-             1970, // year
-             1,    // month
-             1,    // day
-             0,    // hr
-             0,    // min
-             0,    // sec
-             7,    // wday
-             0);   // yday
-  ASSERT_ERRNO_EQ(EINVAL);
-}
-
-// Months are from January to December. Test passing invalid value in month.
-TEST(LlvmLibcCtime, InvalidMonth) {
-  struct time_t t;
-
-  // Test with month = 0.
-  call_ctime(&t,
-             1970, // year
-             0,    // month
-             1,    // day
-             0,    // hr
-             0,    // min
-             0,    // sec
-             4,    // wday
-             0);   // yday
-  ASSERT_ERRNO_EQ(EINVAL);
-
-  // Test with month = 13.
-  call_ctime(&t,
-             1970, // year
-             13,   // month
-             1,    // day
-             0,    // hr
-             0,    // min
-             0,    // sec
-             4,    // wday
-             0);   // yday
-  ASSERT_ERRNO_EQ(EINVAL);
-}
-
-TEST(LlvmLibcCtime, ValidWeekdays) {
-  struct time_t t;
-  char *result;
-  // 1970-01-01 00:00:00.
-  result = call_ctime(&t,
-                      1970, // year
-                      1,    // month
-                      1,    // day
-                      0,    // hr
-                      0,    // min
-                      0,    // sec
-                      4,    // wday
-                      0);   // yday
+TEST(LlvmLibcCtime, ValidUnixTimestamp0) {
+  struct time_t t = 0;
+  char* result = call_ctime(&t);
   ASSERT_STREQ("Thu Jan  1 00:00:00 1970\n", result);
-
-  // 1970-01-03 00:00:00.
-  result = call_ctime(&t,
-                      1970, // year
-                      1,    // month
-                      3,    // day
-                      0,    // hr
-                      0,    // min
-                      0,    // sec
-                      6,    // wday
-                      0);   // yday
-  ASSERT_STREQ("Sat Jan  3 00:00:00 1970\n", result);
-
-  // 1970-01-04 00:00:00.
-  result = call_ctime(&t,
-                      1970, // year
-                      1,    // month
-                      4,    // day
-                      0,    // hr
-                      0,    // min
-                      0,    // sec
-                      0,    // wday
-                      0);   // yday
-  ASSERT_STREQ("Sun Jan  4 00:00:00 1970\n", result);
 }
 
-TEST(LlvmLibcCtime, ValidMonths) {
-  struct time_t t;
-  char *result;
-  // 1970-01-01 00:00:00.
-  result = call_ctime(&t,
-                      1970, // year
-                      1,    // month
-                      1,    // day
-                      0,    // hr
-                      0,    // min
-                      0,    // sec
-                      4,    // wday
-                      0);   // yday
-  ASSERT_STREQ("Thu Jan  1 00:00:00 1970\n", result);
-
-  // 1970-02-01 00:00:00.
-  result = call_ctime(&t,
-                      1970, // year
-                      2,    // month
-                      1,    // day
-                      0,    // hr
-                      0,    // min
-                      0,    // sec
-                      0,    // wday
-                      0);   // yday
-  ASSERT_STREQ("Sun Feb  1 00:00:00 1970\n", result);
-
-  // 1970-12-31 23:59:59.
-  result = call_ctime(&t,
-                      1970, // year
-                      12,   // month
-                      31,   // day
-                      23,   // hr
-                      59,   // min
-                      59,   // sec
-                      4,    // wday
-                      0);   // yday
-  ASSERT_STREQ("Thu Dec 31 23:59:59 1970\n", result);
-}
-
-TEST(LlvmLibcCtime, EndOf32BitEpochYear) {
-  struct time_t t;
-  char *result;
-  // Test for maximum value of a signed 32-bit integer.
-  // Test implementation can encode time for Tue 19 January 2038 03:14:07 UTC.
-  result = call_ctime(&t,
-                      2038, // year
-                      1,    // month
-                      19,   // day
-                      3,    // hr
-                      14,   // min
-                      7,    // sec
-                      2,    // wday
-                      7);   // yday
-  ASSERT_STREQ("Tue Jan 19 03:14:07 2038\n", result);
-}
-
-TEST(LlvmLibcCtime, Max64BitYear) {
-  if (sizeof(time_t) == 4)
-    return;
-  // Mon Jan 1 12:50:50 2170 (200 years from 1970),
-  struct time_t t;
-  char *result;
-  result = call_ctime(&t,
-                      2170, // year
-                      1,    // month
-                      1,    // day
-                      12,   // hr
-                      50,   // min
-                      50,   // sec
-                      1,    // wday
-                      50);  // yday
-  ASSERT_STREQ("Mon Jan  1 12:50:50 2170\n", result);
-
-  // Test for Tue Jan 1 12:50:50 in 2,147,483,647th year.
-  // This test would cause buffer overflow and thus ctime returns nullptr.
-  result = call_ctime(&t,
-                      2147483647, // year
-                      1,          // month
-                      1,          // day
-                      12,         // hr
-                      50,         // min
-                      50,         // sec
-                      2,          // wday
-                      50);        // yday
-  ASSERT_ERRNO_EQ(EOVERFLOW);
-  ASSERT_STREQ(nullptr, result);
+TEST(LlvmLibcCtime, ValidUnixTimestamp32Int) {
+  struct time_t t = 2147483647;
+  char* result = call_ctime(&t);
+  ASSERT_STREQ("Tue Jan  19 03:14:07 2038\n", result);
 }
