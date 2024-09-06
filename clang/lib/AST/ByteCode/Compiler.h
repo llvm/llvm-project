3157288171a3316b0eaed3f49a4ba55b90fe4cba
yronglin <yronglin777@gmail.com>
2024-09-06 20:27:11 +0800

    [clang][bytecode] Implement comparsion operators for vector type (#107258)
    Implement ==, !=, <, <=, >, >= comparsion operators for vector type.
    
    ---------
    
    Signed-off-by: yronglin <yronglin777@gmail.com>

--- clang/lib/AST/ByteCode/Compiler.h
+++ clang/lib/AST/ByteCode/Compiler.h
@@ -130,6 +130,7 @@ public:
   bool VisitLogicalBinOp(const BinaryOperator *E);
   bool VisitPointerArithBinOp(const BinaryOperator *E);
   bool VisitComplexBinOp(const BinaryOperator *E);
+  bool VisitVectorBinOp(const BinaryOperator *E);
   bool VisitCXXDefaultArgExpr(const CXXDefaultArgExpr *E);
   bool VisitCallExpr(const CallExpr *E);
   bool VisitBuiltinCallExpr(const CallExpr *E);
@@ -363,7 +364,6 @@ private:
   bool emitComplexBoolCast(const Expr *E);
   bool emitComplexComparison(const Expr *LHS, const Expr *RHS,
                              const BinaryOperator *E);
-
   bool emitRecordDestruction(const Record *R);
   bool emitDestruction(const Descriptor *Desc);
   unsigned collectBaseOffset(const QualType BaseType,
