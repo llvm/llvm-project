2e51762d175f1005206dd53b237d419e13332753
Зишан Мирза <149377404+zimirza@users.noreply.github.com>
2024-09-06 18:07:55 +0200

    add comment for tests

--- libc/test/src/time/ctime_r_test.cpp
+++ libc/test/src/time/ctime_r_test.cpp
@@ -14,6 +14,7 @@
 
 using LIBC_NAMESPACE::time_utils::TimeConstants;
 
+// make check-libc LIBC_TEST_TARGET=call_ctime_r VERBOSE=1
 static inline char *call_ctime_r(time_t *t, char *buffer) {
   return LIBC_NAMESPACE::ctime_r(t, buffer);
 }
