1e6f25c517d8d1adeeaf59f826141efdcad8f05a
Kerry McLaughlin <kerry.mclaughlin@arm.com>
2024-09-06 10:13:33 +0000

    [Clang] Emit error for duplicate mangled names within a lambda
    When functions are passed as arguments to a lambda, it's possible for
    the mangled names of these functions to be the same despite the prototypes
    being different. For example:
    
      int non_streaming_fn(int);
      int streaming_fn(int) __arm_streaming;
    
      auto lambda_fn = [](const auto &f) { return f(); };
      return lambda_fn(non_streaming_fn) + lambda_fn(streaming_fn);
    
    Only one function will be generated for the lambda above and the __arm_streaming
    attribute from streaming_fn will be incorrectly applied to non_streaming_fn.
    With this change, an error will be emitted when a duplicate mangled name is
    found but the function prototypes do not match.

---
+++ clang/test/CodeGenCXX/aarch64-sme-lambda-attributes.cpp
@@ -0,0 +1,28 @@
+// REQUIRES: aarch64-registered-target
+
+// RUN: %clang_cc1 -triple aarch64 -target-feature +sme -target-feature +sme2 -emit-llvm -o - %s -verify -DTEST1
+// RUN: %clang_cc1 -triple aarch64 -target-feature +sme -target-feature +sme2 -emit-llvm -o - %s -verify -DTEST2
+
+int normal_fn(int);
+int streaming_fn(int) __arm_streaming;
+int streaming_compatible_fn(int) __arm_streaming_compatible;
+
+#ifdef TEST1
+
+// expected-error@+2 {{definition with same mangled name '_ZZ32function_params_normal_streamingvENK3$_0clIFiiEEEDaRT_' as another definition}}
+int function_params_normal_streaming() {
+  auto a = [](auto &fn) { return fn(42); };
+  return a(normal_fn) + a(streaming_fn);
+}
+
+#endif
+
+#ifdef TEST2
+
+// expected-error@+2 {{definition with same mangled name '_ZZ36function_params_streaming_compatiblevENK3$_0clIFiiEEEDaRT_' as another definition}}
+int function_params_streaming_compatible() {
+  auto a = [](auto &fn) { return fn(42); };
+  return a(streaming_fn) + a(streaming_compatible_fn);
+}
+
+#endif
