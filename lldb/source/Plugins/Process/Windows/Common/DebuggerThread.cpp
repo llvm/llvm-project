11084c5c49f8bb7825f81adc5b7140b3506fe253
Adrian Prantl <aprantl@apple.com>
2024-09-05 18:00:46 -0700

    [lldb] Convert DebuggerThread.cpp to new Status API (NFC)

--- lldb/source/Plugins/Process/Windows/Common/DebuggerThread.cpp
+++ lldb/source/Plugins/Process/Windows/Common/DebuggerThread.cpp
@@ -75,7 +75,7 @@ Status DebuggerThread::DebugAttach(lldb::pid_t pid,
         return DebuggerThreadAttachRoutine(pid, attach_info);
       });
   if (!secondary_thread) {
-    result = Status(secondary_thread.takeError());
+    result = Status::FromError(secondary_thread.takeError());
     LLDB_LOG(log, "couldn't attach to process '{0}'. {1}", pid, result);
   }
 
