a41ce109077e95d6e65c91f3427ad44b2c455e6c
Nathan Gauër <brioche@google.com>
2024-09-09 16:28:49 +0200

    fix tests relying on spirv-as
    Signed-off-by: Nathan Gauër <brioche@google.com>

--- llvm/test/CodeGen/SPIRV/structurizer/cf.switch.opswitch.literal.hlsl
+++ llvm/test/CodeGen/SPIRV/structurizer/cf.switch.opswitch.literal.hlsl
@@ -1,4 +1,4 @@
-// RUN: %clang --driver-mode=dxc -T cs_6_0 -fspv-target-env=vulkan1.3 %s -spirv | spirv-as --preserve-numeric-ids - -o - | spirv-val
+// RUN: %if spirv-tools %{ %clang --driver-mode=dxc -T cs_6_0 -fspv-target-env=vulkan1.3 %s -spirv | spirv-as --preserve-numeric-ids - -o - | spirv-val %}
 
 [numthreads(1, 1, 1)]
 void main() {
