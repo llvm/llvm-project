d71e947022f3bcbdbce36fa8c145501054b9cc52
Anchu Rajendran <asudhaku@amd.com>
2024-09-09 13:25:58 -0500

    R6: Reworked to add a few more semantic checks and moved checks to check-omp-structure

--- flang/lib/Semantics/resolve-directives.cpp
+++ flang/lib/Semantics/resolve-directives.cpp
@@ -1590,7 +1590,6 @@ bool OmpAttributeVisitor::Pre(
     const parser::OpenMPSimpleStandaloneConstruct &x) {
   const auto &standaloneDir{
       std::get<parser::OmpSimpleStandaloneDirective>(x.t)};
-  const auto &parentContext{GetContextIf()};
   switch (standaloneDir.v) {
   case llvm::omp::Directive::OMPD_barrier:
   case llvm::omp::Directive::OMPD_ordered:
@@ -1604,19 +1603,6 @@ bool OmpAttributeVisitor::Pre(
     break;
   default:
     break;
-  }
-  if (standaloneDir.v == llvm::omp::Directive::OMPD_scan) {
-    if (std::get<parser::OmpClauseList>(x.t).v.size() != 1) {
-      context_.Say(standaloneDir.source,
-          "Exactly one of `exclusive` or `inclusive` clause is expected"_err_en_US);
-    }
-    if (!parentContext ||
-        !llvm::omp::scanParentAllowedSet.test(parentContext->directive)) {
-      context_.Say(standaloneDir.source,
-          "Orphaned `omp scan` directives are prohibited; perhaps you forgot "
-          "to enclose the directive in to a worksharing loop, a worksharing "
-          "loop simd or a simd directive."_err_en_US);
-    }
   }
   ClearDataSharingAttributeObjects();
   return true;
