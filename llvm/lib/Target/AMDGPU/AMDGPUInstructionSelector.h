0228b0b41e0069cc850eaa37f868a38792100bd0
Vitaly Buka <vitalybuka@google.com>
2024-09-10 09:51:43 -0700

    Revert "[amdgpu] Add llvm.amdgcn.init.whole.wave intrinsic" (#108054)
    Breaks bots, see #105822.
    
    Reverts llvm/llvm-project#105822

--- llvm/lib/Target/AMDGPU/AMDGPUInstructionSelector.h
+++ llvm/lib/Target/AMDGPU/AMDGPUInstructionSelector.h
@@ -120,7 +120,6 @@ private:
   bool selectDSOrderedIntrinsic(MachineInstr &MI, Intrinsic::ID IID) const;
   bool selectDSGWSIntrinsic(MachineInstr &MI, Intrinsic::ID IID) const;
   bool selectDSAppendConsume(MachineInstr &MI, bool IsAppend) const;
-  bool selectInitWholeWave(MachineInstr &MI) const;
   bool selectSBarrier(MachineInstr &MI) const;
   bool selectDSBvhStackIntrinsic(MachineInstr &MI) const;
 
