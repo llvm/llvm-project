61bdaf75ddbd5940af5f23363311ffcacb0540d7
Dhruv-Srivastava <dhruv.srivastava@ibm.com>
2024-09-10 05:43:37 -0500

    Modified specific to AIX

--- lldb/include/lldb/Host/aix/Ptrace.h
+++ lldb/include/lldb/Host/aix/Ptrace.h
@@ -8,29 +8,25 @@
 
 // This file defines ptrace functions & structures
 
-#ifndef liblldb_Host_linux_Ptrace_h_
-#define liblldb_Host_linux_Ptrace_h_
+#ifndef liblldb_Host_aix_Ptrace_h_
+#define liblldb_Host_aix_Ptrace_h_
 
 #include <sys/ptrace.h>
-
-#ifndef __GLIBC__
-typedef int __ptrace_request;
-#endif
 
 #define DEBUG_PTRACE_MAXBYTES 20
 
 // Support ptrace extensions even when compiled without required kernel support
 #ifndef PTRACE_GETREGS
-#define PTRACE_GETREGS 12
+#define PTRACE_GETREGS (PT_COMMAND_MAX + 1)
 #endif
 #ifndef PTRACE_SETREGS
-#define PTRACE_SETREGS 13
+#define PTRACE_SETREGS (PT_COMMAND_MAX + 2)
 #endif
 #ifndef PTRACE_GETFPREGS
-#define PTRACE_GETFPREGS 14
+#define PTRACE_GETFPREGS (PT_COMMAND_MAX + 3)
 #endif
 #ifndef PTRACE_SETFPREGS
-#define PTRACE_SETFPREGS 15
+#define PTRACE_SETFPREGS (PT_COMMAND_MAX + 4)
 #endif
 #ifndef PTRACE_GETREGSET
 #define PTRACE_GETREGSET 0x4204
@@ -38,23 +34,11 @@ typedef int __ptrace_request;
 #ifndef PTRACE_SETREGSET
 #define PTRACE_SETREGSET 0x4205
 #endif
-#ifndef PTRACE_GET_THREAD_AREA
-#define PTRACE_GET_THREAD_AREA 25
+#ifndef PTRACE_GETVRREGS
+#define PTRACE_GETVRREGS (PT_COMMAND_MAX + 5)
 #endif
-#ifndef PTRACE_ARCH_PRCTL
-#define PTRACE_ARCH_PRCTL 30
-#endif
-#ifndef ARCH_GET_FS
-#define ARCH_SET_GS 0x1001
-#define ARCH_SET_FS 0x1002
-#define ARCH_GET_FS 0x1003
-#define ARCH_GET_GS 0x1004
-#endif
-#ifndef PTRACE_PEEKMTETAGS
-#define PTRACE_PEEKMTETAGS 33
-#endif
-#ifndef PTRACE_POKEMTETAGS
-#define PTRACE_POKEMTETAGS 34
+#ifndef PTRACE_GETVSRREGS
+#define PTRACE_GETVSRREGS (PT_COMMAND_MAX + 6)
 #endif
 
-#endif // liblldb_Host_linux_Ptrace_h_
+#endif // liblldb_Host_aix_Ptrace_h_
