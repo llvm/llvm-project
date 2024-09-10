ad71899164499513266d43ec2cc25d6edc711a6b
Dmitry Polukhin <34227995+dmpolukhin@users.noreply.github.com>
2024-09-10 18:15:50 +0300

    [RFC][C++20][Modules] Fix crash when function and lambda inside loaded from different modules (#104512)
    Summary:
    Because AST loading code is lazy and happens in unpredictable order it
    could happen that function and lambda inside function can be loaded from
    different modules. In this case, captured DeclRefExpr won’t match the
    corresponding VarDecl inside function. In AST it looks like this:
    ```
    FunctionDecl 0x555564f4aff0 <Conv.h:33:1, line:41:1> line:33:35 imported in ./thrift_cpp2_base.h hidden tryTo 'Expected<Tgt, const char *> ()' inline
    |-also in ./folly-conv.h
    `-CompoundStmt 0x555564f7cfc8 <col:43, line:41:1>
      |-DeclStmt 0x555564f7ced8 <line:34:3, col:17>
      | `-VarDecl 0x555564f7cef8 <col:3, col:16> col:7 imported in ./thrift_cpp2_base.h hidden referenced result 'Tgt' cinit
      |   `-IntegerLiteral 0x555564f7d080 <col:16> 'int' 0
      |-CallExpr 0x555564f7cea8 <line:39:3, col:76> '<dependent type>'
      | |-UnresolvedLookupExpr 0x555564f7bea0 <col:3, col:19> '<overloaded function type>' lvalue (no ADL) = 'then_' 0x555564f7bef0
      | |-CXXTemporaryObjectExpr 0x555564f7bcb0 <col:25, col:45> 'Expected<bool, int>':'folly::Expected<bool, int>' 'void () noexcept' zeroing
      | `-LambdaExpr 0x555564f7bc88 <col:48, col:75> '(lambda at Conv.h:39:48)'
      |   |-CXXRecordDecl 0x555564f76b88 <col:48> col:48 imported in ./folly-conv.h hidden implicit <undeserialized declarations> class definition
      |   | |-also in ./thrift_cpp2_base.h
      |   | `-DefinitionData lambda empty standard_layout trivially_copyable literal can_const_default_init
      |   |   |-DefaultConstructor defaulted_is_constexpr
      |   |   |-CopyConstructor simple trivial has_const_param needs_implicit implicit_has_const_param
      |   |   |-MoveConstructor exists simple trivial needs_implicit
      |   |   |-CopyAssignment trivial has_const_param needs_implicit implicit_has_const_param
      |   |   |-MoveAssignment
      |   |   `-Destructor simple irrelevant trivial constexpr needs_implicit
      |   `-CompoundStmt 0x555564f7d1a8 <col:58, col:75>
      |     `-ReturnStmt 0x555564f7d198 <col:60, col:67>
      |       `-DeclRefExpr 0x555564f7d0a0 <col:67> 'Tgt' lvalue Var 0x555564f7d0c8 'result' 'Tgt' refers_to_enclosing_variable_or_capture
      `-ReturnStmt 0x555564f7bc78 <line:40:3, col:11>
        `-InitListExpr 0x555564f7bc38 <col:10, col:11> 'void'
    ```
    This diff changes AST deserialization to load lambdas inside canonical
    function declaration earlier right after the function to make sure that
    their canonical decl is loaded from the same module.
    
    Test Plan: check-clang

--- clang/lib/Serialization/ASTReader.cpp
+++ clang/lib/Serialization/ASTReader.cpp
@@ -9782,7 +9782,8 @@ void ASTReader::finishPendingActions() {
       !PendingDeducedVarTypes.empty() || !PendingIncompleteDeclChains.empty() ||
       !PendingDeclChains.empty() || !PendingMacroIDs.empty() ||
       !PendingDeclContextInfos.empty() || !PendingUpdateRecords.empty() ||
-      !PendingObjCExtensionIvarRedeclarations.empty()) {
+      !PendingObjCExtensionIvarRedeclarations.empty() ||
+      !PendingLambdas.empty()) {
     // If any identifiers with corresponding top-level declarations have
     // been loaded, load those declarations now.
     using TopLevelDeclsMap =
@@ -9927,6 +9928,11 @@ void ASTReader::finishPendingActions() {
       }
       PendingObjCExtensionIvarRedeclarations.pop_back();
     }
+
+    // Load any pendiong lambdas.
+    for (auto ID : PendingLambdas)
+      GetDecl(ID);
+    PendingLambdas.clear();
   }
 
   // At this point, all update records for loaded decls are in place, so any
