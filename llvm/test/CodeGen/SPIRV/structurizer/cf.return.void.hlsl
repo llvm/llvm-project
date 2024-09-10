39f4d9b42315443dd39965a469def27ca9625567
Nathan Gauër <brioche@google.com>
2024-09-10 17:39:04 +0200

    convert tests to filecheck

--- llvm/test/CodeGen/SPIRV/structurizer/cf.return.void.hlsl
+++
@@ -1,9 +0,0 @@
-// RUN: %if spirv-tools %{ %clang --driver-mode=dxc -T cs_6_0 -fspv-target-env=vulkan1.3 %s -spirv | spirv-as --preserve-numeric-ids - -o - | spirv-val %}
-
-void A() {
-}
-
-[numthreads(1, 1, 1)]
-void main() {
-  return A();
-}
