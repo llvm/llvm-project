4570984e7fe7409cec10d2305fb43c0b52806683
Saleem Abdulrasool <abdulras@thebrowser.company>
2024-09-11 11:58:10 -0700

    builtins: honour `_M_ARM64` as `__aarch64__`
    When clang is used as `clang-cl`, we use MSVC style macros. The spelling
    of `__aarch64__` is converted to `_M_ARM64`. Account for this
    alternative spelling in the conditional check. While in the area, add a
    tertiary spelling of `__arm64__` to ensure that we catch more of the
    variants.

--- compiler-rt/lib/builtins/cpu_model/aarch64.c
+++ compiler-rt/lib/builtins/cpu_model/aarch64.c
@@ -14,7 +14,7 @@
 
 #include "aarch64.h"
 
-#if !defined(__aarch64__)
+#if !defined(__aarch64__) && !defined(__arm64__) && !defined(_M_ARM64)
 #error This file is intended only for aarch64-based targets
 #endif
 
