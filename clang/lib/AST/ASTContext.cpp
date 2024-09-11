1df68534d086e20572f2371239826d6b3514e58b
Nathan Ridge <zeratul976@hotmail.com>
2024-09-10 22:34:55 -0400

    [AST] Ensure getRawCommentsForAnyRedecl() does not miss any redecl with a comment
    The previous implementation had a bug where, if it was called on a
    Decl later in the redecl chain than `LastCheckedDecl`, it could
    incorrectly skip and overlook a Decl with a comment.
    
    The patch addresses this by only using `LastCheckedDecl` if the input
    Decl `D` is on the path from the first (canonical) Decl to
    `LastCheckedDecl`.
    
    An alternative that was considered was to start the iteration from
    the (canonical) Decl, however this ran into problems with the
    modelling of explicit template specializations in the AST where
    the canonical Decl can be unusual. With the current solution, if no
    Decls were checked yet, we prefer to check the input Decl over the
    canonical one.

--- clang/lib/AST/ASTContext.cpp
+++ clang/lib/AST/ASTContext.cpp
@@ -440,12 +440,26 @@ const RawComment *ASTContext::getRawCommentForAnyRedecl(
   }
 
   // Any redeclarations of D that we haven't checked for comments yet?
-  // We can't use DenseMap::iterator directly since it'd get invalid.
-  auto LastCheckedRedecl = [this, CanonicalD]() -> const Decl * {
-    return CommentlessRedeclChains.lookup(CanonicalD);
+  const Decl *LastCheckedRedecl = [&]() {
+    const Decl *LastChecked = CommentlessRedeclChains.lookup(CanonicalD);
+    bool CanUseCommentlessCache = false;
+    if (LastChecked) {
+      for (auto *Redecl : CanonicalD->redecls()) {
+        if (Redecl == D) {
+          CanUseCommentlessCache = true;
+          break;
+        }
+        if (Redecl == LastChecked)
+          break;
+      }
+    }
+    // FIXME: This could be improved so that even if CanUseCommentlessCache
+    // is false, once we've traversed past CanonicalD we still skip ahead
+    // LastChecked.
+    return CanUseCommentlessCache ? LastChecked : nullptr;
   }();
 
-  for (const auto Redecl : D->redecls()) {
+  for (const Decl *Redecl : D->redecls()) {
     assert(Redecl);
     // Skip all redeclarations that have been checked previously.
     if (LastCheckedRedecl) {
