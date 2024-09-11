6707864cb883a5ed2f018e2a6e1d70225314e7b3
Paul Robinson <paul.robinson@sony.com>
2024-09-11 14:40:46 -0700

    [DebugInfo] Correct the line attribution for IF branches
    An 'if' statement introduces a scope, but in some cases the
    conditional branch to the then/else blocks had a debug-info
    attribution that did not include the scope. This led to some
    inefficiency in the DWARF line table.

--- clang/lib/CodeGen/CGStmt.cpp
+++ clang/lib/CodeGen/CGStmt.cpp
@@ -815,6 +815,7 @@ void CodeGenFunction::EmitIfStmt(const IfStmt &S) {
   // C99 6.8.4.1: The first substatement is executed if the expression compares
   // unequal to 0.  The condition must be a scalar type.
   LexicalScope ConditionScope(*this, S.getCond()->getSourceRange());
+  ApplyDebugLocation DL(*this, S.getCond());
 
   if (S.getInit())
     EmitStmt(S.getInit());
