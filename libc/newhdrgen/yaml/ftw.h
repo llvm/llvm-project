33312f1c705395c6b6b65f594291332365a4a7bb
Jeff Bailey <jeffbailey@google.com>
2024-09-11 09:21:08 +0000

    Initial check-in of infrastructure to support ftw/nftw

---
+++ libc/newhdrgen/yaml/ftw.h
@@ -0,0 +1,15 @@
+header: fcntl.h
+macros: []
+types: []
+enums: []
+objects: []
+functions:
+  - name: ftw
+    standards:
+      - POSIX
+    return_type: int
+    arguments:
+      - type: const char *
+      - type: int
+      - type: int
+
---
+++ libc/src/ftw/ftw.cpp
@@ -0,0 +1,1 @@
+#error "I got here!"
