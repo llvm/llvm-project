bc9626810e2eefde2dc93d723b4797e026d5a620
Jeff Bailey <jeffbailey@google.com>
2024-09-12 10:27:43 +0000

    Move raman's code into the empty files.
    This currently doesn't build - it's just in the right places to work on it more.

--- libc/src/ftw/ftw.cpp
+++ libc/src/ftw/ftw.cpp
@@ -1,1 +1,193 @@
-#error "I got here!"
+//
+// https://man7.org/linux/man-pages/man3/ftw.3.html
+//
+
+/*
+ * -----
+ * Build
+ * -----
+ * cd ..../google3/experimental/users/rtenneti/llvm_libc/
+ * make
+ *
+ * ------------
+ * How to Test:
+ * ------------
+ * make test
+ */
+
+#include "nftw.h"
+
+#include <dirent.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <pthread.h>
+#include <stddef.h>  // size_t
+#include <stdint.h>  // int8_t, int16_t and int32_t
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/un.h>
+#include <unistd.h>
+
+#include <cstdio>
+#include <cstring>
+#include <filesystem>  // NOLINT
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <optional>
+#include <string_view>
+
+namespace {
+class Func {
+ public:
+  virtual int call(const char*, const struct stat*, int, struct FTW*) = 0;
+};
+
+using nftwFn = int (*)(const char* filePath, const struct stat* statBuf,
+                       int tFlag, struct FTW* ftwbuf);
+
+using ftwFn = int (*)(const char* filePath, const struct stat* statBuf,
+                      int tFlag);
+
+class NftwFunc : public Func {
+ public:
+  NftwFunc(nftwFn fn) : fn(fn) {}
+  virtual int call(const char* dirPath, const struct stat* statBuf, int tFlag,
+                   struct FTW* ftwBuf) override {
+    return fn(dirPath, statBuf, tFlag, ftwBuf);
+  }
+
+ private:
+  const nftwFn fn;
+};
+
+class FtwFunc : public Func {
+ public:
+  FtwFunc(ftwFn fn) : fn(fn) {}
+  virtual int call(const char* dirPath, const struct stat* statBuf, int tFlag,
+                   struct FTW*) override {
+    return fn(dirPath, statBuf, tFlag);
+  }
+
+ private:
+  const ftwFn fn;
+};
+
+int doMergedFtw(const std::string& dirPath, Func& fn, int fdLimit, int flags,
+                int level) {
+  // fdLimit specifies the maximum number of directories that ftw()
+  // will hold open simultaneously. When a directory is opened, fdLimit is
+  // decreased and if it becomes 0 or less, we won't open any more directories.
+  if (fdLimit <= 0) {
+    return 0;
+  }
+
+  // Determine the type of path that is passed.
+  int typeFlag;
+  struct stat statBuf;
+  if (flags & FTW_PHYS) {
+    if (lstat(dirPath.c_str(), &statBuf) < 0) return -1;
+  } else {
+    if (stat(dirPath.c_str(), &statBuf) < 0) {
+      if (!lstat(dirPath.c_str(), &statBuf)) {
+        typeFlag = FTW_SLN; /* Symbolic link pointing to a nonexistent file. */
+      } else if (errno != EACCES) {
+        /* stat failed with an errror that is not Permission denied */
+        return -1;
+      } else {
+        /* The probable cause for the failure is that the caller had read
+         * permission on  the parent directory, so that the filename fpath could
+         * be seen, but did not have execute permission on the directory.
+         */
+        typeFlag = FTW_NS;
+      }
+    }
+  }
+
+  if (S_ISDIR(statBuf.st_mode)) {
+    if (flags & FTW_DEPTH) {
+      typeFlag = FTW_DP; /* Directory, all subdirs have been visited. */
+    } else {
+      typeFlag = FTW_D; /* Directory. */
+    }
+  } else if (S_ISLNK(statBuf.st_mode)) {
+    if (flags & FTW_PHYS) {
+      typeFlag = FTW_SL; /* Symbolic link.  */
+    } else {
+      typeFlag = FTW_SLN; /* Symbolic link pointing to a nonexistent file. */
+    }
+  } else {
+    typeFlag = FTW_F; /* Regular file.  */
+  }
+
+  struct FTW ftwBuf;
+  // Find the base by finding the last slash.
+  std::size_t slash_found = dirPath.rfind("/");
+  if (slash_found != std::string::npos) {
+    ftwBuf.base = slash_found + 1;
+  }
+
+  ftwBuf.level = level;
+
+  // If the dirPath is a file, call the function on it and return.
+  if ((typeFlag == FTW_SL) || (typeFlag == FTW_F)) {
+    int returnValue = fn.call(dirPath.c_str(), &statBuf, typeFlag, &ftwBuf);
+    if (returnValue) {
+      return returnValue;
+    }
+    return 0;
+  }
+
+  // If FTW_DEPTH is not set, nftw() shall report any directory before reporting
+  // the files in that directory.
+  if (!(flags & FTW_DEPTH)) {
+    // Call the function on the directory.
+    int directory_fd = open(dirPath.c_str(), O_RDONLY);
+    if (directory_fd < 0 && errno == EACCES) {
+      typeFlag = FTW_DNR; /* Directory can't be read. */
+    }
+    close(directory_fd);
+
+    int returnValue = fn.call(dirPath.c_str(), &statBuf, typeFlag, &ftwBuf);
+    if (returnValue) {
+      return returnValue;
+    }
+  }
+
+  for (std::error_code ec; auto const& dir_entry :
+                           std::filesystem::directory_iterator(dirPath, ec)) {
+    if (ec) continue;
+    int returnValue =
+        doMergedFtw(dir_entry.path(), fn, fdLimit - 1, flags, ftwBuf.level + 1);
+    if (returnValue) {
+      return returnValue;
+    }
+  }
+
+  // If FTW_DEPTH is set, nftw() shall report all files in a directory before
+  // reporting the directory itself.
+  if (flags & FTW_DEPTH) {
+    // Call the function on the directory.
+    return fn.call(dirPath.c_str(), &statBuf, typeFlag, &ftwBuf);
+  }
+  return 0;
+}
+} // namespace
+
+int llvm_libc_nftw(const std::string& dirPath,
+                   int (*fn)(const char* filePath, const struct stat* statBuf,
+                             int tFlag, struct FTW* ftwbuf),
+                   int fdLimit, int flags) {
+  NftwFunc wrappedFn{fn};
+  return doMergedFtw(dirPath, wrappedFn, fdLimit, flags, 0);
+}
+
+int llvm_libc_ftw(const std::string &dirPath,
+                  int (*fn)(const char *filePath, const struct stat *statBuf,
+                            int tFlag),
+                  int fdLimit) {
+  return llvm_libc_nftw(dirPath, (int (*)())fn, fdLimit, FTW_PHYS, 0);
+}
--- libc/test/src/ftw/ftw_test.cpp
+++ libc/test/src/ftw/ftw_test.cpp
@@ -1,1 +1,236 @@
-#error "I got here!"
+// Test code starts here
+
+//nftw test code:
+
+namespace fs = std::filesystem;
+
+class TemporaryDirectory {
+ public:
+  TemporaryDirectory() {
+    fs::path temp_path = fs::temp_directory_path();
+    std::string temp_path_prefix = temp_path.string() + "/tmpdir.XXXXXX";
+    // Use data() to get a writable string. mkdtemp doesn't write beyond the
+    // allocated data.
+    char* dir_name = mkdtemp(temp_path_prefix.data());
+    _path = dir_name;
+    fs::current_path(_path);
+  }
+
+  ~TemporaryDirectory() {
+    fs::current_path(_path);
+    fs::remove_all(_path);
+  }
+  const std::string GetDirectoryPath() { return _path.c_str(); }
+
+ private:
+  fs::path _path;
+};
+
+static void setupTestData() {
+  fs::create_directories("sandbox");
+  fs::create_directory("sandbox/owner_all_group_read_others_read_dir");
+  fs::permissions("sandbox/owner_all_group_read_others_read_dir",
+                  fs::perms::owner_all | fs::perms::group_read |
+                      fs::perms::group_exec | fs::perms::others_read |
+                      fs::perms::others_exec,
+                  fs::perm_options::add);
+  fs::create_directory(
+      "sandbox/owner_all_group_read_others_read_dir/"
+      "owner_read_group_read_others_read_dir");
+  fs::permissions(
+      "sandbox/owner_all_group_read_others_read_dir/"
+      "owner_read_group_read_others_read_dir",
+      fs::perms::owner_read | fs::perms::owner_exec | fs::perms::group_read |
+          fs::perms::group_exec | fs::perms::others_read |
+          fs::perms::others_exec,
+      fs::perm_options::add);
+  fs::create_directory("sandbox/no_perm_dir");
+  fs::permissions("sandbox/no_perm_dir", fs::perms::none,
+                  fs::perm_options::add);
+
+  fs::create_symlink("invalid_target", "sandbox/sym1");
+  fs::create_directory_symlink("owner_all_group_read_others_read_dir",
+                               "sandbox/sym2");
+
+  std::ofstream ofs("sandbox/file");  // create regular file
+}
+
+static bool isReadable(const fs::path& p) {
+  std::error_code ec;  // For noexcept overload usage.
+  auto perms = fs::status(p, ec).permissions();
+  if ((perms & fs::perms::owner_read) != fs::perms::none &&
+      (perms & fs::perms::group_read) != fs::perms::none &&
+      (perms & fs::perms::others_read) != fs::perms::none) {
+    return true;
+  }
+  return false;
+}
+
+static int checkNftw(const char* arg_fpath, const struct stat* statBuf,
+                     int typeFlag, struct FTW* ftwbuf) {
+  displayInfo(arg_fpath, statBuf, typeFlag, ftwbuf);
+  if (arg_fpath == NULL) {
+    std::cout << " fpath is null\n";
+    return -1;
+  }
+  std::string fpath = arg_fpath;
+  if (statBuf == NULL) {
+    std::cout << " stat is null " << fpath << "\n";
+    return -1;
+  }
+
+  const fs::path path = fpath;
+
+  // status says we don't know the status of this file.
+  if (typeFlag == FTW_NS || typeFlag == FTW_SLN) {
+    struct stat sb;
+    // Verify we can't stat the path.
+    if (-1 != stat(arg_fpath, &sb)) {
+      std::cout << "status doesn't match for " << arg_fpath << "\n";
+      return -1;
+    }
+    return 0;
+  }
+
+  // If it is directory
+  if (S_ISDIR(statBuf->st_mode)) {
+    if (!fs::is_directory(fs::status(path))) {
+      std::cout << "Is not directory> " << fpath << "\n";
+      return -1;
+    }
+    if (isReadable(fpath)) {
+      // It is readable, verify typeFlag is correct.
+      if (typeFlag != FTW_D && typeFlag != FTW_DP) {
+        std::cout << "typeFlag != FTW_D && typeFlag != FTW_DP " << fpath
+                  << "\n";
+        return -1;
+      }
+      return 0;
+    }
+    // It is not readable, and verify typeFlag is correct.
+    if (typeFlag != FTW_DNR && typeFlag != FTW_D) {
+      std::cout << "typeFlag != FTW_DNR && typeFlag != FTW_D " << fpath << "\n";
+      return -1;
+    }
+    return 0;
+  }
+
+  // If it symlink, verify the filestatus and verify typeFlag is correct.
+  if (S_ISLNK(statBuf->st_mode)) {
+    if (!fs::is_symlink(fs::status(path))) {
+      std::cout << "Is not symlink" << fpath << "\n";
+      return -1;
+    }
+    if (FTW_SL != typeFlag) {
+      std::cout << " FTW_SL != typeFlag " << fpath << "\n";
+      return -1;
+    }
+    return 0;
+  }
+
+  if (!fs::is_regular_file(fs::status(path))) {
+    std::cout << " is not a regular file " << fpath << "\n";
+    return -1;
+  }
+  if (FTW_F != typeFlag) {
+    std::cout << " FTW_SL != typeFlag " << fpath << "\n";
+    return -1;
+  }
+  return 0; /* To tell llvm_libc_nftw() to continue */
+}
+
+static void testNftw() {
+  std::cout << std::endl << "Calling testNftw: " << std::endl;
+  TemporaryDirectory tmpDir = TemporaryDirectory();
+  setupTestData();
+  int flags = 0;
+  llvm_libc_nftw(tmpDir.GetDirectoryPath(), checkNftw, 128, flags);
+  std::cout << "All testNftw tests have passed: " << std::endl;
+}
+
+int main(int argc, char* argv[]) {
+  std::cout << "ftw called with args: " << argv[1] << std::endl;
+
+  int flags = 0;
+
+  if (argc > 2) {
+    if (strchr(argv[2], 'p') != NULL) flags |= FTW_PHYS;
+    if (strchr(argv[2], 'd') != NULL) flags |= FTW_DEPTH;
+  } else {
+    flags |= FTW_DEPTH;
+  }
+
+  std::cout << "Calling nftw: " << std::endl;
+  if (nftw((argc < 2) ? "." : argv[1], displayInfo, 20, flags) == -1) {
+    perror("nftw");
+    exit(EXIT_FAILURE);
+  }
+
+  std::cout << "Calling llvm_libc_nftw: " << std::endl;
+  std::string_view dirPath(argv[1]);
+  if (llvm_libc_nftw((argc < 2) ? "." : argv[1], displayInfo, 20, flags) ==
+      -1) {
+    perror("llvm_libc_nftw");
+    exit(EXIT_FAILURE);
+  }
+
+  // Unit tests for FTW.
+  testNftw();
+
+  exit(EXIT_SUCCESS);
+}
+
+// ftw test code:
+
+
+static int display_info(const char *filePath, const struct stat *sb, int tflag,
+                        struct FTW *ftwbuf) {
+  printf("%-3s %2d ",
+         (tflag == FTW_D)     ? "d"
+         : (tflag == FTW_DNR) ? "dnr"
+         : (tflag == FTW_DP)  ? "dp"
+         : (tflag == FTW_F)   ? "f"
+         : (tflag == FTW_NS)  ? "ns"
+         : (tflag == FTW_SL)  ? "sl"
+         : (tflag == FTW_SLN) ? "sln"
+                              : "???",
+         ftwbuf->level);
+
+  if (tflag == FTW_NS)
+    printf("-------");
+  else
+    printf("%7jd", (intmax_t)sb->st_size);
+
+  printf("   %-40s %d %s\n", filePath, ftwbuf->base, filePath + ftwbuf->base);
+
+  return 0; /* To tell llvm_libc_nftw() to continue */
+}
+
+int main(int argc, char *argv[]) {
+  std::cout << "ftw called with args: " << argv[1] << std::endl;
+
+  int flags = 0;
+
+  if (argc > 2) {
+    if (strchr(argv[2], 'p') != NULL) flags |= FTW_PHYS;
+    if (strchr(argv[2], 'd') != NULL) flags |= FTW_DEPTH;
+  } else {
+    flags |= FTW_DEPTH;
+  }
+
+  std::cout << "Calling nftw: " << std::endl;
+  if (ftw((argc < 2) ? "." : argv[1], display_info, 20) == -1) {
+    perror("nftw");
+    exit(EXIT_FAILURE);
+  }
+
+  std::cout << "Calling llvm_libc_nftw: " << std::endl;
+  std::string_view dirPath(argv[1]);
+  if (llvm_libc_ftw((argc < 2) ? "." : argv[1], display_info, 20) == -1) {
+    perror("llvm_libc_nftw");
+    exit(EXIT_FAILURE);
+  }
+
+  exit(EXIT_SUCCESS);
+}
+
