699568402ae66744cd9fc0d53906ac1e5bc94d6d
Doru Bercea <doru.bercea@amd.com>
2024-09-12 11:04:10 -0400

    [NFC] Remove unsafeAtomicAdd from runtime
    Change-Id: I66802f13ff61fcdd07fb2b4886e14687face5113

--- offload/DeviceRTL/src/Synchronization.cpp
+++ offload/DeviceRTL/src/Synchronization.cpp
@@ -150,8 +150,6 @@ uint64_t atomicAdd(uint64_t *Address, uint64_t Val,
   return __atomic_fetch_add(Address, Val, Ordering);
 }
 
-float unsafeAtomicAdd(float *addr, float value);
-
 #if defined(__gfx941__)
 #define ATOMIC_CAS_LOOP_MIN(TY) void atomicCASLoopMin(TY *addr, TY val);
 
@@ -377,28 +375,6 @@ void setLock(omp_lock_t *Lock) {
   // test_lock will now return true for any thread in the warp
 }
 
-#if defined(__gfx90a__) && __has_builtin(__builtin_amdgcn_is_shared) &&        \
-    __has_builtin(__builtin_amdgcn_is_private) &&                              \
-    __has_builtin(__builtin_amdgcn_ds_atomic_fadd_f32) &&                      \
-    __has_builtin(__builtin_amdgcn_global_atomic_fadd_f32)
-// This function is called for gfx90a only and single precision
-// floating point type
-float unsafeAtomicAdd(float *addr, float value) {
-  if (__builtin_amdgcn_is_shared(
-          (const __attribute__((address_space(0))) void *)addr))
-    return __builtin_amdgcn_ds_atomic_fadd_f32(
-        (const __attribute__((address_space(3))) float *)addr, value);
-  else if (__builtin_amdgcn_is_private(
-               (const __attribute__((address_space(0))) void *)addr)) {
-    float temp = *addr;
-    *addr = temp + value;
-    return temp;
-  }
-  return __builtin_amdgcn_global_atomic_fadd_f32(
-      (const __attribute__((address_space(1))) float *)addr, value);
-}
-#endif // if defined(gfx90a) &&
-
 void unsetCriticalLock(omp_lock_t *Lock) {
   (void)atomicExchange((uint32_t *)Lock, UNSET, atomic::acq_rel);
 }
@@ -556,8 +532,6 @@ void setLock(omp_lock_t *Lock) {
     }
   } // wait for 0 to be the read value
 }
-
-float unsafeAtomicAdd(float *addr, float value) { return 0.0; }
 
 #pragma omp end declare variant
 ///}
@@ -769,10 +743,6 @@ void omp_set_lock(omp_lock_t *Lock) { impl::setLock(Lock); }
 void omp_unset_lock(omp_lock_t *Lock) { impl::unsetLock(Lock); }
 
 int omp_test_lock(omp_lock_t *Lock) { return impl::testLock(Lock); }
-
-float __kmpc_unsafeAtomicAdd(float *addr, float value) {
-  return impl::unsafeAtomicAdd(addr, value);
-}
 
 #if defined(__gfx941__)
 #define KMPC_ATOMIC_CAS_LOOP_MIN(TY)                                           \
