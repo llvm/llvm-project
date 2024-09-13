3f264dd831f220c0421636c220e188ddb02d90e4
Nikolas Klauser <nikolasklauser@berlin.de>
2024-09-13 11:34:20 +0200

    Reapply "[clang] Extend diagnose_if to accept more detailed warning information (#70976)" (#108453)
    This reverts commit 6e559ac508a9a1e1a753762e4c75a7dd1459c4c8.
    
    Update the use of `getWarningOptionForDiag` in flang to use the
    DiagnosticIDs.

--- clang/lib/Sema/Sema.cpp
+++ clang/lib/Sema/Sema.cpp
@@ -1683,7 +1683,7 @@ void Sema::EmitCurrentDiagnostic(unsigned DiagID) {
   // that is different from the last template instantiation where
   // we emitted an error, print a template instantiation
   // backtrace.
-  if (!DiagnosticIDs::isBuiltinNote(DiagID))
+  if (!Diags.getDiagnosticIDs()->isNote(DiagID))
     PrintContextStack();
 }
 
@@ -1697,7 +1697,8 @@ bool Sema::hasUncompilableErrorOccurred() const {
   if (Loc == DeviceDeferredDiags.end())
     return false;
   for (auto PDAt : Loc->second) {
-    if (DiagnosticIDs::isDefaultMappingAsError(PDAt.second.getDiagID()))
+    if (Diags.getDiagnosticIDs()->isDefaultMappingAsError(
+            PDAt.second.getDiagID()))
       return true;
   }
   return false;
