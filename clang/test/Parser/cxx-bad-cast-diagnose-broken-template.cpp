d5f47ff46b9af1483e2984a25ce4f65015a5c84e
Alejandro Álvarez Ayllón <alejandro.alvarez@sonarsource.com>
2024-09-13 11:41:58 +0200

    Sync test with PR

--- clang/test/Parser/cxx-bad-cast-diagnose-broken-template.cpp
+++ clang/test/Parser/cxx-bad-cast-diagnose-broken-template.cpp
@@ -1,23 +1,18 @@
 // RUN: %clang_cc1 -fcxx-exceptions -fexceptions -fsyntax-only -verify %s
 
-template< typename>
-struct IsConstCharArray
-{
-  static const bool value = false;
-};
+template<typename>
+struct StringTrait {};
 
 template< int N >
-struct IsConstCharArray< const char[ N ] >
-{
+struct StringTrait< const char[ N ] > {
   typedef char CharType;
-  static const bool value = true;
-  static const missing_int_t length = N - 1; // expected-error {{unknown type name 'missing_int_t'}}
+  static const MissingIntT length = N - 1; // expected-error {{unknown type name 'MissingIntT'}}
 };
 
 class String {
 public:
   template <typename T>
-  String(T& str, typename IsConstCharArray<T>::CharType = 0);
+  String(T& str, typename StringTrait<T>::CharType = 0);
 };
 
 
