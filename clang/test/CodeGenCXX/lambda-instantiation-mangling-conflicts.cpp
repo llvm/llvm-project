4b6d327fe6b6a552fe16e27f7b8b15ce5d2055da
Kerry McLaughlin <kerry.mclaughlin@arm.com>
2024-09-16 16:22:41 +0000

    - Renamed aarch64-sme-lambda-attributes.cpp to lambda-instantiation-mangling-conflicts.cpp - Replaced ifdefs in test with use of split-file

--- clang/test/CodeGenCXX/lambda-instantiation-mangling-conflicts.cpp
+++ clang/test/CodeGenCXX/lambda-instantiation-mangling-conflicts.cpp
@@ -1,13 +1,15 @@
 // REQUIRES: aarch64-registered-target
 
-// RUN: %clang_cc1 -triple aarch64 -emit-llvm -o - %s -verify -DTEST1
-// RUN: %clang_cc1 -triple aarch64 -target-feature +sme -target-feature +sme2 -emit-llvm -o - %s -verify -DTEST2
+// RUN: rm -rf %t
+// RUN: split-file %s %t
+
+// RUN: %clang_cc1 -triple aarch64 -target-feature +sme -target-feature +sme2 -emit-llvm -o - %t/a.cpp -verify
+// RUN: %clang_cc1 -triple aarch64 -target-feature +sme -target-feature +sme2 -emit-llvm -o - %t/b.cpp -verify
+
+;--- a.cpp
 
 int normal_fn(int);
-int streaming_fn(int) __arm_streaming;
 int vector_pcs_fn(int) __attribute__((aarch64_vector_pcs));
-
-#ifdef TEST1
 
 // expected-error@+4 {{definition with same mangled name '_ZZ23function_pcs_attributesvENK3$_0clIFiiEEEDaRT_' as another definition}}
 // expected-note@+3 {{previous definition is here}}
@@ -17,9 +19,10 @@ int function_pcs_attributes() {
   return a(normal_fn) + a(vector_pcs_fn);
 }
 
-#endif
+;--- b.cpp
 
-#ifdef TEST2
+int normal_fn(int);
+int streaming_fn(int) __arm_streaming;
 
 // expected-error@+3 {{definition with same mangled name '_ZZ32function_params_normal_streamingvENK3$_0clIFiiEEEDaRT_' as another definition}}
 // expected-note@+2 {{previous definition is here}}
@@ -27,5 +30,3 @@ int function_params_normal_streaming() {
   auto a = [](auto &fn) { return fn(42); };
   return a(normal_fn) + a(streaming_fn);
 }
-
-#endif
