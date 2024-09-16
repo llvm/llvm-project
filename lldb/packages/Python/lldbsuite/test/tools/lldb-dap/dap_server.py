093bb34fa5dc22a33d414922505b6c8798adf1ce
Adrian Vogelsgesang <avogelsgesang@salesforce.com>
2024-09-16 22:56:20 +0200

    [lldb-dap] Support inspecting memory (#104317)
    Add support for the `readMemory` request which allows VS-Code to
    inspect memory. Also, add `memoryReference` to variables and `evaluate`
    responses, such that the binary view can be opened from the variables
    view and from the "watch" pane.
    
    (cherry picked from commit 3acb1eac5eb6ef4e60dd64b7845615e076cc6a3e)

--- lldb/packages/Python/lldbsuite/test/tools/lldb-dap/dap_server.py
+++ lldb/packages/Python/lldbsuite/test/tools/lldb-dap/dap_server.py
@@ -691,6 +691,19 @@ class DebugCommunication(object):
         for inst in instructions:
             self.disassembled_instructions[inst["address"]] = inst
 
+    def request_readMemory(self, memoryReference, offset, count):
+        args_dict = {
+            "memoryReference": memoryReference,
+            "offset": offset,
+            "count": count,
+        }
+        command_dict = {
+            "command": "readMemory",
+            "type": "request",
+            "arguments": args_dict,
+        }
+        return self.send_recv(command_dict)
+
     def request_evaluate(self, expression, frameIndex=0, threadId=None, context=None):
         stackFrame = self.get_stackFrame(frameIndex=frameIndex, threadId=threadId)
         if stackFrame is None:
