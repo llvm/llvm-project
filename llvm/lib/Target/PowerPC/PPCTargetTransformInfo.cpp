a6bf81b4d15a84b8d9e06ff2ff2d7a1e7f235fc2
Roland Froese <froese@ca.ibm.com>
2024-09-17 22:05:17 +0000

    move decl

--- llvm/lib/Target/PowerPC/PPCTargetTransformInfo.cpp
+++ llvm/lib/Target/PowerPC/PPCTargetTransformInfo.cpp
@@ -802,12 +802,13 @@ InstructionCost PPCTTIImpl::getMemoryOpCost(unsigned Opcode, Type *Src,
   // explicitly check this case. There are also corresponding store
   // instructions.
   unsigned MemBytes = Src->getPrimitiveSizeInBits();
-  Align AlignBytes = Alignment ? *Alignment : Align(1);
   unsigned SrcBytes = LT.second.getStoreSize();
   if (ST->hasVSX() && IsAltivecType) {
     if (MemBytes == 64 || (ST->hasP8Vector() && MemBytes == 32))
       return 1;
+
     // Use lfiwax/xxspltw
+    Align AlignBytes = Alignment ? *Alignment : Align(1);
     if (Opcode == Instruction::Load && MemBytes == 32 && AlignBytes < SrcBytes)
       return 2;
   }
