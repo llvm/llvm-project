33880520ab83c51e526dddf057f049493bcbb4c2
Sarah Spall <spall@planetbauer.com>
2024-09-17 21:47:46 +0000

    address latest PR comments

--- clang/test/SemaHLSL/BuiltIns/countbits-errors.hlsl
+++ clang/test/SemaHLSL/BuiltIns/countbits-errors.hlsl
@@ -1,17 +1,11 @@
 // RUN: %clang_cc1 -finclude-default-header
 // -triple dxil-pc-shadermodel6.6-library %s -fnative-half-type -emit-llvm-only
-// -disable-llvm-passes -verify
+// -disable-llvm-passes -verify -verify-ignore-unexpected
 
 
 double test_int_builtin(double p0) {
   return countbits(p0);
   // expected-error@-1 {{call to 'countbits' is ambiguous}}
-  // expected-note@hlsl/hlsl_intrinsics.h:* {{candidate function}}
-  // expected-note@hlsl/hlsl_intrinsics.h:* {{candidate function}}
-  // expected-note@hlsl/hlsl_intrinsics.h:* {{candidate function}}
-  // expected-note@hlsl/hlsl_intrinsics.h:* {{candidate function}}
-  // expected-note@hlsl/hlsl_intrinsics.h:* {{candidate function}}
-  // expected-note@hlsl/hlsl_intrinsics.h:* {{candidate function}}
 }
 
 double2 test_int_builtin_2(double2 p0) {
