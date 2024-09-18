f6a6060a4cedaaa18e58770edcbce3a9549d3245
Mike Hommey <mh@glandium.org>
2024-09-18 18:03:43 +0900

    [LLD][COFF] Do as many passes of resolveRemainingUndefines as necessary for undefined lazy symbols

--- lld/COFF/Driver.cpp
+++ lld/COFF/Driver.cpp
@@ -2601,7 +2601,9 @@ void LinkerDriver::linkerMain(ArrayRef<const char *> argsArr) {
     createECExportThunks();
 
   // Resolve remaining undefined symbols and warn about imported locals.
-  ctx.symtab.resolveRemainingUndefines();
+  while (ctx.symtab.resolveRemainingUndefines())
+    run();
+
   if (errorCount())
     return;
 
