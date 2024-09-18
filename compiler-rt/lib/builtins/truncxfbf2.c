532b958631e59c23987799bec681ac02aa612c57
Phoebe Wang <phoebe.wang@intel.com>
2024-09-18 21:23:10 +0800

    [X86][BF16] Add libcall for F80 -> BF16 (#109116)
    This fixes #108936, but the calling convention doesn't match with GCC. I
    doubt we have such a lib function for now, so leave the calling
    convention as is.

---
+++ compiler-rt/lib/builtins/truncxfbf2.c
@@ -0,0 +1,13 @@
+//===-- lib/truncxfbf2.c - long double -> bfloat conversion -------*- C -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#define SRC_80
+#define DST_BFLOAT
+#include "fp_trunc_impl.inc"
+
+COMPILER_RT_ABI dst_t __truncxfbf2(long double a) { return __truncXfYf2__(a); }
