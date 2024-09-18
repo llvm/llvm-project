1c017087e3f07c88cc191bf92c8ab301cc53f6f9
Chibuoyim (Wilson) Ogbonna <ogbonnachibuoyim12@gmail.com>
2024-09-19 00:42:41 +0300

    [CIR][Lowering] Fix lowering for multi dimensional array (#851)
    This PR fixes the lowering for multi dimensional arrays.
    
    Consider the following code snippet `test.c`: 
    ```
    void foo() {
      char arr[4][1] = {"a", "b", "c", "d"};
    }
    ```
    
    When ran with `bin/clang test.c -Xclang -fclangir -Xclang -emit-llvm -S
    -o -`, It produces the following error:
    ```
    ~/clangir/llvm/include/llvm/Support/Casting.h:566: decltype(auto) llvm::cast(const From&) [with To = mlir::ArrayAttr; From = mlir::Attribute]: Assertion `isa<To>(Val) && "cast<Ty>() argument of incompatible type!"' failed.
    ```
    
    The bug can be traced back to `LoweringHelpers.cpp`. It considers the
    values in the array as integer types, and this causes an error in this
    case.
    
    This PR updates `convertToDenseElementsAttrImpl` when the array contains
    string attributes. I have also added one more similar test. Note that in
    the tests I used a **literal match** to avoid matching as regex, so
    `!dbg` is useful.

--- clang/test/CIR/Lowering/array-init.c
+++ clang/test/CIR/Lowering/array-init.c
@@ -1,6 +1,12 @@
 // RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fclangir -emit-llvm %s -o %t.ll
 // RUN: FileCheck --input-file=%t.ll %s -check-prefix=LLVM
 
+// LLVM: charInit1.ar = internal global [4 x [4 x i8]] {{.*}}4 x i8] c"aa\00\00", [4 x i8] c"aa\00\00", [4 x i8] c"aa\00\00", [4 x i8] c"aa\00\00"], align 16
+char charInit1() {
+  static char ar[][4] = {"aa", "aa", "aa", "aa"};
+  return ar[0][0];
+}
+
 // LLVM: define dso_local void @zeroInit
 // LLVM: [[RES:%.*]] = alloca [3 x i32], i64 1
 // LLVM: store [3 x i32] zeroinitializer, ptr [[RES]]
@@ -8,3 +14,14 @@ void zeroInit() {
   int a[3] = {0, 0, 0};
 }
 
+// LLVM: %1 = alloca [4 x [1 x i8]], i64 1, align 1
+// LLVM: store [4 x [1 x i8]] {{.*}}1 x i8] c"a", [1 x i8] c"b", [1 x i8] c"c", [1 x i8] c"d"], ptr %1, align 1
+void charInit2() {
+  char arr[4][1] = {"a", "b", "c", "d"};
+}
+
+// LLVM: %1 = alloca [4 x [2 x i8]], i64 1, align 1
+// LLVM: store [4 x [2 x i8]] {{.*}}2 x i8] c"ab", [2 x i8] c"cd", [2 x i8] c"ef", [2 x i8] c"gh"], ptr %1, align 1
+void charInit3() {
+  char arr[4][2] = {"ab", "cd", "ef", "gh"};
+}
