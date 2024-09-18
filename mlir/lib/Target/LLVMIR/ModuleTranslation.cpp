ec3d8946d044030aa778aa4a7d2340e22194ab83
lemoncmd <lemoncmd@gmail.com>
2024-09-18 18:40:58 +0900

    add partition arguments and implement translation

--- mlir/lib/Target/LLVMIR/ModuleTranslation.cpp
+++ mlir/lib/Target/LLVMIR/ModuleTranslation.cpp
@@ -1050,6 +1050,9 @@ LogicalResult ModuleTranslation::convertGlobals() {
     if (op.getSection().has_value())
       var->setSection(*op.getSection());
 
+    if (op.getPartition().has_value())
+      var->setPartition(*op.getPartition());
+
     addRuntimePreemptionSpecifier(op.getDsoLocal(), var);
 
     std::optional<uint64_t> alignment = op.getAlignment();
@@ -1346,6 +1349,9 @@ LogicalResult ModuleTranslation::convertOneFunction(LLVMFuncOp func) {
 
   if (std::optional<StringRef> section = func.getSection())
     llvmFunc->setSection(*section);
+
+  if (std::optional<StringRef> partition = func.getPartition())
+    llvmFunc->setPartition(*partition);
 
   if (func.getArmStreaming())
     llvmFunc->addFnAttr("aarch64_pstate_sm_enabled");
