e4f27872464087073a7056312b04a4e0d6d8fa1b
Mainak Sil <mainaksil0@gmail.com>
2024-09-18 20:46:40 +0530

    Update AllLibrariesFit.cpp

--- clang/unittests/CodeGen/AllLibrariesFit.cpp
+++ clang/unittests/CodeGen/AllLibrariesFit.cpp
@@ -3,8 +3,8 @@
 #include "gtest/gtest.h"
 
 TEST(VecLibBitfieldTest, AllLibrariesFit) {
-    // We expect that all vector libraries fit in the bitfield size
-    EXPECT_LE(static_cast<size_t>(llvm::driver::VectorLibrary::MaxLibrary), 
-              (1 << VECLIB_BIT_COUNT))
-        << "VecLib bitfield size is too small!";
+  // We expect that all vector libraries fit in the bitfield size
+  EXPECT_LE(static_cast<size_t>(llvm::driver::VectorLibrary::MaxLibrary),
+            (1 << VECLIB_BIT_COUNT))
+      << "VecLib bitfield size is too small!";
 }
