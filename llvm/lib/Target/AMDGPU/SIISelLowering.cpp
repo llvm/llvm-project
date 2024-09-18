d3dd174a77a57da9151133d1b7bb9fa1bfe4d1f8
Jay Foad <jay.foad@amd.com>
2024-09-18 16:44:58 +0100

    [AMDGPU] Remove miscellaneous unused code. NFC.

--- llvm/lib/Target/AMDGPU/SIISelLowering.cpp
+++ llvm/lib/Target/AMDGPU/SIISelLowering.cpp
@@ -10013,8 +10013,6 @@ SDValue SITargetLowering::bufferRsrcPtrToVector(SDValue MaybePointer,
   if (!MaybePointer.getValueType().isScalarInteger())
     return MaybePointer;
 
-  SDLoc DL(MaybePointer);
-
   SDValue Rsrc = DAG.getBitcast(MVT::v4i32, MaybePointer);
   return Rsrc;
 }
