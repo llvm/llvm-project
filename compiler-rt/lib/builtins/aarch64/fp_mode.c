230ae23141aabb8d61fc698de739e7de08640c6a
Saleem Abdulrasool <abdulras@thebrowser.company>
2024-09-19 14:16:35 -0700

    builtins: adjust code for ARM64 on MSVC harder
    This addresses additional compilation issues that were highlighted by
    trying to build the builtins for Windows ARM64 on the Swift rebranch.

--- compiler-rt/lib/builtins/aarch64/fp_mode.c
+++ compiler-rt/lib/builtins/aarch64/fp_mode.c
@@ -24,6 +24,7 @@
 // For soft float targets, allow changing rounding mode by overriding the weak
 // __aarch64_fe_default_rmode symbol.
 #if defined(_MSC_VER)
+extern CRT_FE_ROUND_MODE __default_rmode;
 CRT_FE_ROUND_MODE __default_rmode = CRT_FE_TONEAREST;
 #pragma comment(linker, "/alternatename:__aarch64_fe_default_rmode=__default_rmode")
 #else
