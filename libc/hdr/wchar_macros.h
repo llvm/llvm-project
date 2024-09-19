fbf14e20e7ff60accc57e93c564ab2283a6e3b88
Tue Ly <lntue.h@gmail.com>
2024-09-19 21:50:21 +0000

    Add a common wchar_overlay.h header.

--- libc/hdr/wchar_macros.h
+++ libc/hdr/wchar_macros.h
@@ -15,34 +15,7 @@
 
 #else // Overlay mode
 
-// glibc <wchar.h header might provide extern inline definitions for few
-// functions, causing external alias errors.  They are guarded by
-// `__USE_EXTERN_INLINES` macro.  We temporarily disable `__USE_EXTERN_INLINES`
-// macro by defining `__NO_INLINE__` before including <wchar.h>.
-// And the same with `__USE_FORTIFY_LEVEL`, which will be temporarily disabled
-// with `_FORTIFY_SOURCE`.
-
-#ifdef _FORTIFY_SOURCE
-#define LIBC_OLD_FORTIFY_SOURCE _FORTIFY_SOURCE
-#undef _FORTIFY_SOURCE
-#endif
-
-#ifndef __NO_INLINE__
-#define __NO_INLINE__ 1
-#define LIBC_SET_NO_INLINE
-#endif
-
-#include <wchar.h>
-
-#ifdef LIBC_OLD_FORTIFY_SOURCE
-#define _FORTIFY_SOURCE LIBC_OLD_FORTIFY_SOURCE
-#undef LIBC_OLD_FORTIFY_SOURCE
-#endif
-
-#ifdef LIBC_SET_NO_INLINE
-#undef __NO_INLINE__
-#undef LIBC_SET_NO_INLINE
-#endif
+#include "hdr/wchar_overlay.h"
 
 #endif // LLVM_LIBC_FULL_BUILD
 
