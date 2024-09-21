192deb4adc9f7e77167a02c060eef8c91932b912
Owen Pan <owenpiano@gmail.com>
2024-09-20 20:28:31 -0700

    [clang-format] Correctly annotate */& in if condition with braced init
    Fixes #109371.

--- clang/lib/Format/TokenAnnotator.cpp
+++ clang/lib/Format/TokenAnnotator.cpp
@@ -1165,19 +1165,26 @@ private:
 
     ScopedContextCreator ContextCreator(*this, tok::l_brace, 1);
     Contexts.back().ColonIsDictLiteral = true;
-    if (OpeningBrace.is(BK_BracedInit))
+
+    const auto *Prev = OpeningBrace.getPreviousNonComment();
+
+    if (OpeningBrace.is(BK_BracedInit)) {
       Contexts.back().IsExpression = true;
-    if (Style.isJavaScript() && OpeningBrace.Previous &&
-        OpeningBrace.Previous->is(TT_JsTypeColon)) {
+      if (Prev) {
+        for (auto *Tok = Prev->Previous; Tok && Tok->isPointerOrReference();
+             Tok = Tok->Previous) {
+          Tok->setFinalizedType(TT_PointerOrReference);
+        }
+      }
+    }
+
+    if (Style.isJavaScript() && Prev && Prev->is(TT_JsTypeColon))
       Contexts.back().IsExpression = false;
-    }
-    if (Style.isVerilog() &&
-        (!OpeningBrace.getPreviousNonComment() ||
-         OpeningBrace.getPreviousNonComment()->isNot(Keywords.kw_apostrophe))) {
-      Contexts.back().VerilogMayBeConcatenation = true;
-    }
+
     if (Style.isTableGen())
       Contexts.back().ColonIsDictLiteral = false;
+    else if (Style.isVerilog() && !(Prev && Prev->is(Keywords.kw_apostrophe)))
+      Contexts.back().VerilogMayBeConcatenation = true;
 
     unsigned CommaCount = 0;
     while (CurrentToken) {
