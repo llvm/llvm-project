fd8ced9668f127aa22a143a6992daf76ed579f62
Sh0g0-1758 <shouryagoel10000@gmail.com>
2024-09-22 01:32:47 +0530

    nit

--- libc/test/include/IsNormalTest.h
+++ libc/test/include/IsNormalTest.h
@@ -1,4 +1,4 @@
-//===-- Utility class to test the isnormal macro  -------------*- C++ -*-===//
+//===-- Utility class to test the isnormal macro  ---------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
