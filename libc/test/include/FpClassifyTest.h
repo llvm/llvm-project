ed468cb9ce87d12bb4972e58c1d1892306a71927
Sh0g0-1758 <shouryagoel10000@gmail.com>
2024-09-21 19:43:28 +0530

    nit

--- libc/test/include/FpClassifyTest.h
+++ libc/test/include/FpClassifyTest.h
@@ -1,5 +1,4 @@
-//===-- Utility class to test the fpclassify macro  -----------------*- C++
-//-*-===//
+//===-- Utility class to test the fpclassify macro  -------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
