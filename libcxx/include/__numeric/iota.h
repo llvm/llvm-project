2439dafc79626a31f1afe649110371d5cc92aa63
Paul <paulxicao7@gmail.com>
2024-09-21 21:57:30 +0200

    constrain template arguments via proper concepts

--- libcxx/include/__numeric/iota.h
+++ libcxx/include/__numeric/iota.h
@@ -11,7 +11,9 @@
 #define _LIBCPP___NUMERIC_IOTA_H
 
 #include <__config>
+#include <__iterator/concepts.h>
 #include <__ranges/access.h>
+#include <__ranges/concepts.h>
 #include <__utility/pair.h>
 
 #if !defined(_LIBCPP_HAS_NO_PRAGMA_SYSTEM_HEADER)
@@ -29,7 +31,8 @@ iota(_ForwardIterator __first, _ForwardIterator __last, _Tp __value) {
 
 #if _LIBCPP_STD_VER >= 23
 namespace ranges {
-template <class O, class S, class T>
+template <input_or_output_iterator O, sentinel_for<O> S, weakly_incrementable T>
+  requires indirectly_writable<O, const T&>
 constexpr pair<O, T> iota(O first, S last, T value) {
   while (first != last) {
     *first = value;
@@ -39,7 +42,7 @@ constexpr pair<O, T> iota(O first, S last, T value) {
   return {std::move(first), std::move(value)};
 }
 
-template <class O, class R, class T>
+template < input_or_output_iterator O, weakly_incrementable T, output_range<const T&> R >
 constexpr pair<O, T> iota(R&& r, T value) {
   return std::ranges::iota(std::ranges::begin(r), std::ranges::end(r), std::move(value));
 }
