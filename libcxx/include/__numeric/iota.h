a90a3405ac7e2f46370ef84465c95a5e9365da54
Paul <paulxicao7@gmail.com>
2024-09-21 05:42:25 +0200

    first (straw-man) implementation for std::ranges::iota

--- libcxx/include/__numeric/iota.h
+++ libcxx/include/__numeric/iota.h
@@ -11,6 +11,8 @@
 #define _LIBCPP___NUMERIC_IOTA_H
 
 #include <__config>
+#include <__ranges/access.h>
+#include <__utility/pair.h>
 
 #if !defined(_LIBCPP_HAS_NO_PRAGMA_SYSTEM_HEADER)
 #  pragma GCC system_header
@@ -24,6 +26,25 @@ iota(_ForwardIterator __first, _ForwardIterator __last, _Tp __value) {
   for (; __first != __last; ++__first, (void)++__value)
     *__first = __value;
 }
+
+#if _LIBCPP_STD_VER >= 23
+namespace ranges {
+template <class O, class S, class T>
+constexpr pair<O, T> iota(O first, S last, T value) {
+  while (first != last) {
+    *first = value;
+    ++first;
+    ++value;
+  }
+  return {std::move(first), std::move(value)};
+}
+
+template <class O, class R, class T>
+constexpr pair<O, T> iota(R&& r, T value) {
+  return std::ranges::iota(std::ranges::begin(r), std::ranges::end(r), std::move(value));
+}
+} // namespace ranges
+#endif // _LIBCPP_STD_VER >= 23
 
 _LIBCPP_END_NAMESPACE_STD
 
