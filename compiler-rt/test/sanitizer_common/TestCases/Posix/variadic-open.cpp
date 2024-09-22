a1bea6cba5b64cdd4d2bf382e1ad1487e81aa94b
Vitaly Buka <vitalybuka@google.com>
2024-09-22 13:47:02 -0700

    [NFC][sanitizer] Undo "RUN:" part of 39c940bbae693346abc45dcd692775a00f1062ac
    It was not compartible with mobile tests.

--- compiler-rt/test/sanitizer_common/TestCases/Posix/variadic-open.cpp
+++ compiler-rt/test/sanitizer_common/TestCases/Posix/variadic-open.cpp
@@ -1,6 +1,4 @@
-// RUN: rm -rf %t.tmp
-// RUN: mkdir -p %t.tmp
-// RUN: %clangxx -O1 %s -o %t && %run %t %t.tmp/1 %t.tmp
+// RUN: %clangxx -O1 %s -o %t && %run %t %t.tmp %T
 
 #include <assert.h>
 #include <fcntl.h>
@@ -9,10 +7,9 @@
 #include <unistd.h>
 
 void test(const char *path, int flags) {
-  assert(path);
   int fd = open(path, flags, 0600);
   if (fd == -1)
-    perror ("open");
+    perror(path);
   assert(fd != -1);
   struct stat info;
   int result = fstat(fd, &info);
@@ -23,9 +20,12 @@ void test(const char *path, int flags) {
 
 int main(int argc, char *argv[]) {
   assert(argc == 3);
+  assert(argv[1]);
+  unlink(argv[1]);
   test(argv[1], O_RDWR | O_CREAT);
 
 #ifdef O_TMPFILE
+assert(argv[2]);
   test(argv[2], O_RDWR | O_TMPFILE);
 #endif
 }
