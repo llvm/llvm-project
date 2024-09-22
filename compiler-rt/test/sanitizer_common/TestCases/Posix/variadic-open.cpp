39c940bbae693346abc45dcd692775a00f1062ac
Vitaly Buka <vitalybuka@google.com>
2024-09-22 12:03:52 -0700

    [NFC][sanitizer] Extract test from `main` function
    And extract cleanup into RUN: command

--- compiler-rt/test/sanitizer_common/TestCases/Posix/variadic-open.cpp
+++ compiler-rt/test/sanitizer_common/TestCases/Posix/variadic-open.cpp
@@ -1,18 +1,24 @@
-// RUN: %clangxx -O1 %s -o %t && %run %t %t.tmp
+// RUN: rm -rf %t.tmp
+// RUN: mkdir -p %t.tmp
+// RUN: %clangxx -O1 %s -o %t && %run %t %t.tmp/1
 
 #include <assert.h>
 #include <fcntl.h>
-#include <unistd.h>
 #include <sys/stat.h>
+#include <unistd.h>
 
-int main(int argc, char *argv[]) {
-  assert(argv[1]);
-  unlink(argv[1]);
-  int fd = open(argv[1], O_RDWR | O_CREAT, 0600);
+void test(const char *path, int flags) {
+  assert(path);
+  int fd = open(path, flags, 0600);
   assert(fd != -1);
   struct stat info;
   int result = fstat(fd, &info);
   assert((info.st_mode & ~S_IFMT) == 0600);
   assert(result == 0);
   close(fd);
+}
+
+int main(int argc, char *argv[]) {
+  assert(argc == 2);
+  test(argv[1], O_RDWR | O_CREAT);
 }
