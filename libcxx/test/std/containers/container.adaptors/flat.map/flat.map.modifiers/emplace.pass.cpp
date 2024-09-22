0ab32753becc84647d41b9f2d73c8265a64f71d6
Hui <hui.xie0621@gmail.com>
2024-09-22 21:20:03 +0100

    exception tests

--- libcxx/test/std/containers/container.adaptors/flat.map/flat.map.modifiers/emplace.pass.cpp
+++ libcxx/test/std/containers/container.adaptors/flat.map/flat.map.modifiers/emplace.pass.cpp
@@ -18,6 +18,7 @@
 #include <deque>
 #include <tuple>
 
+#include "../helpers.h"
 #include "test_macros.h"
 #include "../../../Emplaceable.h"
 #include "DefaultOnly.h"
@@ -118,6 +119,135 @@ int main(int, char**) {
     assert(m.begin()->first == 2);
     assert(m.begin()->second == 3.5);
   }
+#ifndef TEST_HAS_NO_EXCEPTIONS
+  {
+    // Throw on emplace the key, and underlying has strong exception guarantee
+    using M = std::flat_map<CopyableThrowOnMove, int>;
+    std::vector<CopyableThrowOnMove> a;
+    a.reserve(4);
+    a.emplace_back(1);
+    a.emplace_back(2);
+    a.emplace_back(3);
+    a.emplace_back(4);
+
+    std::vector<int> b                    = {5, 6, 7, 8};
+    [[maybe_unused]] auto expected_keys   = a;
+    [[maybe_unused]] auto expected_values = b;
+    M m(std::sorted_unique, std::move(a), std::move(b));
+
+    try {
+      m.emplace(std::piecewise_construct, std::tuple(0), std::tuple(0));
+      assert(false);
+    } catch (int) {
+      assert(m.keys().size() == m.values().size());
+      assert(is_sorted_and_unique(m.keys()));
+      // In libc++, the flat_map is unchanged
+      LIBCPP_ASSERT(m.size() == 4);
+      LIBCPP_ASSERT(m.keys() == expected_keys);
+      LIBCPP_ASSERT(m.values() == expected_values);
+    }
+  }
+  {
+    // Throw on emplace the key, and underlying has no strong exception guarantee
+    using M = std::flat_map<MoveOnlyThrowOnMove, int>;
+    std::vector<MoveOnlyThrowOnMove> a;
+    a.reserve(4);
+    a.emplace_back(1);
+    a.emplace_back(2);
+    a.emplace_back(3);
+    a.emplace_back(4);
+
+    std::vector<int> b = {5, 6, 7, 8};
+    M m(std::sorted_unique, std::move(a), std::move(b));
+    try {
+      m.emplace(std::piecewise_construct, std::tuple(0), std::tuple(0));
+      assert(false);
+    } catch (int) {
+      assert(m.keys().size() == m.values().size());
+      assert(is_sorted_and_unique(m.keys()));
+      // In libc++, the flat_map is cleared
+      LIBCPP_ASSERT(m.size() == 0);
+    }
+  }
+  {
+    // Throw on emplace the value, and underlying has strong exception guarantee
+    using M            = std::flat_map<int, CopyableThrowOnMove>;
+    std::vector<int> a = {1, 2, 3, 4};
+    std::vector<CopyableThrowOnMove> b;
+    b.reserve(4);
+    b.emplace_back(1);
+    b.emplace_back(2);
+    b.emplace_back(3);
+    b.emplace_back(4);
+
+    [[maybe_unused]] auto expected_keys   = a;
+    [[maybe_unused]] auto expected_values = b;
+    M m(std::sorted_unique, std::move(a), std::move(b));
+
+    try {
+      m.emplace(std::piecewise_construct, std::tuple(0), std::tuple(0));
+      assert(false);
+    } catch (int) {
+      assert(m.keys().size() == m.values().size());
+      assert(is_sorted_and_unique(m.keys()));
+      // In libc++, the emplaced key is erased and the flat_map is unchanged
+      LIBCPP_ASSERT(m.size() == 4);
+      LIBCPP_ASSERT(m.keys() == expected_keys);
+      LIBCPP_ASSERT(m.values() == expected_values);
+    }
+  }
+  {
+    // Throw on emplace the value, and underlying has no strong exception guarantee
+    using M            = std::flat_map<int, MoveOnlyThrowOnMove>;
+    std::vector<int> a = {1, 2, 3, 4};
+    std::vector<MoveOnlyThrowOnMove> b;
+    b.reserve(4);
+    b.emplace_back(1);
+    b.emplace_back(2);
+    b.emplace_back(3);
+    b.emplace_back(4);
+
+    M m(std::sorted_unique, std::move(a), std::move(b));
+
+    try {
+      m.emplace(std::piecewise_construct, std::tuple(0), std::tuple(0));
+      assert(false);
+    } catch (int) {
+      assert(m.keys().size() == m.values().size());
+      assert(is_sorted_and_unique(m.keys()));
+      // In libc++, the flat_map is unchanged
+      LIBCPP_ASSERT(m.size() == 0);
+    }
+  }
+  {
+    using M = std::flat_map<ThrowOnSecondMove, CopyableThrowOnMove>;
+    std::vector<ThrowOnSecondMove> a;
+    a.reserve(4);
+    a.emplace_back(1);
+    a.emplace_back(2);
+    a.emplace_back(3);
+    a.emplace_back(4);
+
+    std::vector<CopyableThrowOnMove> b;
+    b.reserve(4);
+    b.emplace_back(1);
+    b.emplace_back(2);
+    b.emplace_back(3);
+    b.emplace_back(4);
+
+    M m(std::sorted_unique, std::move(a), std::move(b));
+    try {
+      m.emplace(std::piecewise_construct, std::tuple(0), std::tuple(0));
+      assert(false);
+    } catch (int) {
+      assert(m.keys().size() == m.values().size());
+      assert(is_sorted_and_unique(m.keys()));
+      // In libc++, we try to erase the key after value emplacement failure.
+      // and after erasure failure, we clear the flat_map
+      LIBCPP_ASSERT(m.size() == 0);
+    }
+  }
+#endif
 
   return 0;
 }
