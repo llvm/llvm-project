be2796fcee6ee0bd2b29455ea40762cfbe77c10e
Vitaly Buka <vitalybuka@google.com>
2024-09-22 11:59:14 -0700

    [NFC][sanitizer] Don't use FileCheck in test
    `assert` is enough.

--- compiler-rt/test/sanitizer_common/TestCases/Posix/variadic-open.cpp
+++ compiler-rt/test/sanitizer_common/TestCases/Posix/variadic-open.cpp
@@ -1,25 +1,18 @@
-// RUN: %clangxx -O1 %s -o %t && %run %t %t.tmp 2>&1 | FileCheck %s
+// RUN: %clangxx -O1 %s -o %t && %run %t %t.tmp
 
-#include <stdio.h>
 #include <assert.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/stat.h>
 
 int main(int argc, char *argv[]) {
-  fprintf(stderr, "Hello world.\n");
   assert(argv[1]);
   unlink(argv[1]);
   int fd = open(argv[1], O_RDWR | O_CREAT, 0600);
   assert(fd != -1);
   struct stat info;
   int result = fstat(fd, &info);
-  fprintf(stderr, "permissions = 0%o\n", info.st_mode & ~S_IFMT);
+  assert((info.st_mode & ~S_IFMT) == 0600);
   assert(result == 0);
   close(fd);
-  fprintf(stderr, "Done.\n");
 }
-
-// CHECK: Hello world.
-// CHECK: permissions = 0600
-// CHECK: Done.
