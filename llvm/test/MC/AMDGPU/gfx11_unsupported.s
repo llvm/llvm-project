5927c6745c2a60c368d7bcb789a1f561d62fa039
Jun Wang <jwang86@yahoo.com>
2024-09-23 10:38:27 -0700

    [AMDGPU][MC] Instructions not to be supported in GFX940 (#109225)
    Buffer_store_lds_dword, buffer_wbinvl1, and buffer_wbinvl1_vol are
    obsolete in GFX940 and should not be supported.

--- llvm/test/MC/AMDGPU/gfx11_unsupported.s
+++ llvm/test/MC/AMDGPU/gfx11_unsupported.s
@@ -34,6 +34,9 @@ buffer_invl2
 buffer_store_lds_dword s[4:7], -1 offset:4095 lds
 // CHECK: :[[@LINE-1]]:{{[0-9]+}}: error: instruction not supported on this GPU
 
+buffer_wbinvl1
+// CHECK: :[[@LINE-1]]:{{[0-9]+}}: error: instruction not supported on this GPU
+
 buffer_wbinvl1_vol
 // CHECK: :[[@LINE-1]]:{{[0-9]+}}: error: instruction not supported on this GPU
 
