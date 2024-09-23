b3131b77a6e23873e7eb4b78153077b8fcb82eb8
Fangrui Song <i@maskray.me>
2024-09-23 10:33:43 -0700

    [ELF] Move elf::symtab into Ctx
    Remove the global variable `symtab` and add a member variable
    (`std::unique_ptr<SymbolTable>`) to `Ctx` instead.
    
    This is one step toward eliminating global states.
    
    Pull Request: https://github.com/llvm/llvm-project/pull/109612

--- lld/ELF/InputSection.cpp
+++ lld/ELF/InputSection.cpp
@@ -1129,7 +1129,7 @@ static void switchMorestackCallsToMorestackNonSplit(
   // If the target adjusted a function's prologue, all calls to
   // __morestack inside that function should be switched to
   // __morestack_non_split.
-  Symbol *moreStackNonSplit = symtab.find("__morestack_non_split");
+  Symbol *moreStackNonSplit = ctx.symtab->find("__morestack_non_split");
   if (!moreStackNonSplit) {
     error("mixing split-stack objects requires a definition of "
           "__morestack_non_split");
