8e4611e5dbeda288432ba53a61bfe1452cedfa93
Valentin Clement <clementval@gmail.com>
2024-09-24 14:21:31 -0700

    [flang][cuda] Add entry point for alloc/free and simple copy

--- flang/runtime/CUDA/memory.cpp
+++ flang/runtime/CUDA/memory.cpp
@@ -8,17 +8,46 @@
 
 #include "flang/Runtime/CUDA/memory.h"
 #include "../terminator.h"
+#include "flang/Runtime/CUDA/common.h"
 
 #include "cuda_runtime.h"
 
 namespace Fortran::runtime::cuda {
 extern "C" {
 
+void *RTDEF(CUFMemAlloc)(
+    std::size_t bytes, const char *sourceFile, int sourceLine) {
+  void *ptr;
+  if (bytes != 0)
+    CUDA_REPORT_IF_ERROR(cudaMalloc((void **)&ptr, bytes));
+  return ptr;
+}
+
+void RTDEF(CUFMemFree)(void *ptr, const char *sourceFile, int sourceLine) {
+  CUDA_REPORT_IF_ERROR(cudaFree(ptr));
+}
+
 void RTDEF(CUFMemsetDescriptor)(const Descriptor &desc, void *value,
     const char *sourceFile, int sourceLine) {
   Terminator terminator{sourceFile, sourceLine};
   terminator.Crash("not yet implemented: CUDA data transfer from a scalar "
                    "value to a descriptor");
+}
+
+void RTDEF(CUFDataTransferPtrPtr)(void *dst, void *src, std::size_t bytes,
+    unsigned mode, const char *sourceFile, int sourceLine) {
+  cudaMemcpyKind kind;
+  if (mode == kHostToDevice) {
+    kind = cudaMemcpyHostToDevice;
+  } else if (mode == kDeviceToHost) {
+    kind = cudaMemcpyDeviceToHost;
+  } else if (mode == kDeviceToDevice) {
+    kind = cudaMemcpyDeviceToDevice;
+  } else {
+    Terminator terminator{sourceFile, sourceLine};
+    terminator.Crash("host to host copy not supported");
+  }
+  CUDA_REPORT_IF_ERROR(cudaMemcpy(dst, src, bytes, kind));
 }
 
 void RTDEF(CUFDataTransferDescPtr)(const Descriptor &desc, void *addr,
