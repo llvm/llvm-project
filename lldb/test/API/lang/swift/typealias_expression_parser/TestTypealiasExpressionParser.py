e61214ccf75f2374bccf7da7387d0c20a2e7aa2b
Felipe de Azevedo Piovezan <fpiovezan@apple.com>
2024-09-24 14:13:31 -0700

    [lldb] Skip tests to unblock rebranch testing

--- lldb/test/API/lang/swift/typealias_expression_parser/TestTypealiasExpressionParser.py
+++ lldb/test/API/lang/swift/typealias_expression_parser/TestTypealiasExpressionParser.py
@@ -2,4 +2,4 @@ import lldbsuite.test.lldbinline as lldbinline
 from lldbsuite.test.decorators import *
 
 # FIXME! The Swift driver insists on passing -experimental-skip-non-inlinable-function-bodies-without-types to -emit-module.
-lldbinline.MakeInlineTest(__file__, globals(), decorators=[swiftTest,expectedFailureAll(bugnumber="rdar://120928396")])
+lldbinline.MakeInlineTest(__file__, globals(), decorators=[swiftTest])
