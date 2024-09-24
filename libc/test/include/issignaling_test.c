036fcf562a9092014374cf28a3b604201ab58d4b
Sh0g0-1758 <shouryagoel10000@gmail.com>
2024-09-24 14:53:11 +0530

    fix c test

--- libc/test/include/issignaling_test.c
+++ libc/test/include/issignaling_test.c
@@ -9,24 +9,24 @@
 
 #include <assert.h>
 
+// check if macro is defined
+#ifndef issignaling
+// TODO: enable the error when issignaling macro is fixed for older compiler
+// #error "issignaling macro is not defined"
+#else
+int main(void) {
 // TODO: enable the test unconditionally when issignaling macro is fixed for
 //       older compiler
 #if (defined(__clang__) && __clang_major__ >= 18) ||                           \
     (defined(__GNUC__) && __GNUC__ >= 13)
-
-// check if macro is defined
-#ifndef issignaling
-#error "issignaling macro is not defined"
-#else
-int main(void) {
   assert(issignaling(__builtin_nans("")) == 1);
   assert(issignaling(__builtin_nansf("")) == 1);
   assert(issignaling(__builtin_nansl("")) == 1);
   assert(issignaling(1.819f) == 0);
   assert(issignaling(-1.726) == 0);
   assert(issignaling(1.426L) == 0);
+#endif
   return 0;
 }
-#endif
 
 #endif
