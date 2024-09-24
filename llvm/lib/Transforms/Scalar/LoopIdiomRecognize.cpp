2dc6fdc5f71c25c2895e73fddf3aa73901cc7de9
Nikita Popov <npopov@redhat.com>
2024-09-24 17:44:09 +0200

    [CmpInstAnalysis] Return decomposed bit test as struct (NFC)
    decomposeBitTestICmp() currently returns the result via two out
    parameters plus an in-place modification of Pred. This changes it
    to return an optional struct instead.
    
    The motivation here is twofold. First, I'd like to extend this code
    to handle cases where the comparison is against a value other than
    zero, which would mean yet another out parameter. Second, while
    doing that I was badly bitten by the in-place modification, so I'd
    like to get rid of it.

--- llvm/lib/Transforms/Scalar/LoopIdiomRecognize.cpp
+++ llvm/lib/Transforms/Scalar/LoopIdiomRecognize.cpp
@@ -2465,10 +2465,16 @@ static bool detectShiftUntilBitTestIdiom(Loop *CurLoop, Value *&BaseX,
   };
   auto MatchDecomposableConstantBitMask = [&]() {
     APInt Mask;
-    return llvm::decomposeBitTestICmp(CmpLHS, CmpRHS, Pred, CurrX, Mask) &&
-           ICmpInst::isEquality(Pred) && Mask.isPowerOf2() &&
-           (BitMask = ConstantInt::get(CurrX->getType(), Mask)) &&
-           (BitPos = ConstantInt::get(CurrX->getType(), Mask.logBase2()));
+    auto Res = llvm::decomposeBitTestICmp(CmpLHS, CmpRHS, Pred);
+    if (Res && Res->Mask.isPowerOf2()) {
+      assert(ICmpInst::isEquality(Res->Pred));
+      Pred = Res->Pred;
+      CurrX = Res->X;
+      BitMask = ConstantInt::get(CurrX->getType(), Res->Mask);
+      BitPos = ConstantInt::get(CurrX->getType(), Res->Mask.logBase2());
+      return true;
+    }
+    return false;
   };
 
   if (!MatchVariableBitMask() && !MatchConstantBitMask() &&
