3a36160efe807231c047b3438e2ba3f9f286ba7a
agozillon <Andrew.Gozillon@amd.com>
2024-09-25 11:51:05 -0500

    [Flang][Driver][Offload] Add new test case and attempt to appease the CI gods

--- flang/test/Driver/xoffload-linker.f90
+++ flang/test/Driver/xoffload-linker.f90
@@ -4,4 +4,10 @@
 ! RUN:   %flang -### --target=x86_64-unknown-linux-gnu -fopenmp --offload-arch=gfx90a \
 ! RUN:      -Xoffload-linker a %s 2>&1 | FileCheck %s --check-prefix=CHECK-XLINKER
 
-! CHECK-XLINKER: -device-linker=a{{.*}}--
+! CHECK-XLINKER: {{.*}}-device-linker=a{{.*}}
+
+! RUN:   %flang -### --target=x86_64-unknown-linux-gnu -fopenmp --offload-arch=gfx90a \
+! RUN:      -Xoffload-linker a -Xoffload-linker-amdgcn-amd-amdhsa b \
+! RUN:      %s 2>&1 | FileCheck %s --check-prefix=CHECK-XLINKER-AMDGCN
+
+! CHECK-XLINKER-AMDGCN: {{.*}}-device-linker=a{{.*}}-device-linker=amdgcn-amd-amdhsa=b{{.*}}
