eaeb963b6f78c62d87820197dda12801caf548a7
Adrian Prantl <aprantl@apple.com>
2024-09-25 10:32:28 -0700

    Unskip tests

--- lldb/test/API/lang/swift/clangimporter/anonymous-clang-types/TestSwiftAnonymousClangTypes.py
+++ lldb/test/API/lang/swift/clangimporter/anonymous-clang-types/TestSwiftAnonymousClangTypes.py
@@ -3,8 +3,6 @@ from lldbsuite.test.decorators import *
 import lldbsuite.test.lldbtest as lldbtest
 import lldbsuite.test.lldbutil as lldbutil
 
-
-@skipIf(oslist=['linux'])
 class TestSwiftAnonymousClangTypes(lldbtest.TestBase):
     @swiftTest
     def test(self):
