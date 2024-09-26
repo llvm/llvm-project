9b9ea26989d1733e2ed112b33842945fc16bfa0a
agozillon <Andrew.Gozillon@amd.com>
2024-09-25 23:59:19 -0500

    Attempt at fixing CI error #2

--- flang/test/Driver/xoffload-linker.f90
+++ flang/test/Driver/xoffload-linker.f90
@@ -1,13 +1,12 @@
 ! Test the -Xoffload-linker flag that forwards link commands to the clang-linker-wrapper used
 ! to help link offloading device libraries
 
-! RUN:   %flang -### --target=x86_64-unknown-linux-gnu -fopenmp --offload-arch=gfx90a \
-! RUN:      -Xoffload-linker a %s 2>&1 | FileCheck %s --check-prefix=CHECK-XLINKER
+! RUN: %flang -### --target=x86_64-unknown-linux-gnu -fopenmp --offload-arch=gfx90a -Xoffload-linker a %s 2>&1 | FileCheck %s --check-prefix=CHECK-XLINKER
 
-! CHECK-XLINKER: {{.*}}-device-linker=a{{.*}}
+! CHECK-XLINKER: {{.*}}--device-linker=a{{.*}}
 
-! RUN:   %flang -### --target=x86_64-unknown-linux-gnu -fopenmp --offload-arch=gfx90a \
-! RUN:      -Xoffload-linker a -Xoffload-linker-amdgcn-amd-amdhsa b \
-! RUN:      %s 2>&1 | FileCheck %s --check-prefix=CHECK-XLINKER-AMDGCN
+! RUN: %flang -### --target=x86_64-unknown-linux-gnu -fopenmp --offload-arch=gfx90a -Xoffload-linker a -Xoffload-linker-amdgcn-amd-amdhsa b %s 2>&1 | FileCheck %s --check-prefix=CHECK-XLINKER-AMDGCN
 
-! CHECK-XLINKER-AMDGCN: {{.*}}-device-linker=a{{.*}}-device-linker=amdgcn-amd-amdhsa=b{{.*}}
+! CHECK-XLINKER-AMDGCN: {{.*}}"--device-linker=a"{{.*}}"--device-linker=amdgcn-amd-amdhsa=b"{{.*}}
+
+end program
