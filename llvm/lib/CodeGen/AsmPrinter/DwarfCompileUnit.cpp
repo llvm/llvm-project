16b422b3b78b5e8c97c9cffe79615140beb6b230
Sinan Lin <sinan.lin@linux.alibaba.com>
2024-09-26 20:36:21 +0800

    [DWARF] Do not emit DWARF5 symbols in the case of DWARF2/3 + non-lldb
    Modify other legacy dwarf versions to align with the dwarf4 handling
    approach when determining whether to generate DWARF5 or GNU extensions.

--- llvm/lib/CodeGen/AsmPrinter/DwarfCompileUnit.cpp
+++ llvm/lib/CodeGen/AsmPrinter/DwarfCompileUnit.cpp
@@ -1209,7 +1209,7 @@ void DwarfCompileUnit::constructAbstractSubprogramScopeDIE(
 }
 
 bool DwarfCompileUnit::useGNUAnalogForDwarf5Feature() const {
-  return DD->getDwarfVersion() == 4 && !DD->tuneForLLDB();
+  return DD->getDwarfVersion() <= 4 && !DD->tuneForLLDB();
 }
 
 dwarf::Tag DwarfCompileUnit::getDwarf5OrGNUTag(dwarf::Tag Tag) const {
