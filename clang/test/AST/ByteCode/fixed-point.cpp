81029c5ddbedb6a12e545a0180c81e8052f27e2d
Timm Baeder <tbaeder@redhat.com>
2024-09-27 11:32:43 +0200

    [clang][bytecode] Start implementing fixed point types (#110216)
    Add the primitive type and implement to-bool casts.

---
+++ clang/test/AST/ByteCode/fixed-point.cpp
@@ -0,0 +1,9 @@
+// RUN: %clang_cc1 %s -fsyntax-only -ffixed-point -verify=expected,both -fexperimental-new-constant-interpreter
+// RUN: %clang_cc1 %s -fsyntax-only -ffixed-point -verify=ref,both
+
+static_assert((bool)1.0k);
+static_assert(!((bool)0.0k));
+static_assert((bool)0.0k); // both-error {{static assertion failed}}
+
+static_assert(1.0k == 1.0k);
+static_assert(1.0k != 1.0k); // both-error {{failed due to requirement '1.0k != 1.0k'}}
