f84921590fd3ce32ae133612f7c3fc16adf22218
Keith Smiley <keithbsmiley@gmail.com>
2024-09-28 09:11:30 -0700

    Move to release.cmake

--- clang/cmake/caches/Release.cmake
+++ clang/cmake/caches/Release.cmake
@@ -109,3 +109,6 @@ set_final_stage_var(LLVM_ENABLE_PROJECTS "${LLVM_RELEASE_ENABLE_PROJECTS}" STRIN
 set_final_stage_var(CPACK_GENERATOR "TXZ" STRING)
 set_final_stage_var(CPACK_ARCHIVE_THREADS "0" STRING)
 
+if(CMAKE_SYSTEM_NAME MATCHES "Darwin")
+  set_final_stage_var(LLVM_USE_STATIC_ZSTD "ON" BOOL)
+endif()
