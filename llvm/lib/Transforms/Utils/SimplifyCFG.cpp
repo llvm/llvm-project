4ee5b91966882806bcf17bcfe208a06f65ffa349
Simone Campanoni <campanoni@google.com>
2024-09-30 13:50:12 +0000

    [SimplifyCFG] Run the git-clang-format

--- llvm/lib/Transforms/Utils/SimplifyCFG.cpp
+++ llvm/lib/Transforms/Utils/SimplifyCFG.cpp
@@ -406,7 +406,7 @@ static InstructionCost computeSpeculationCost(const User *I,
 
 /// If we have a merge point of an "if condition" as accepted above,
 /// return true if the specified value dominates the block.  We don't handle
-/// the true generality of domination here, just a special case which works 
+/// the true generality of domination here, just a special case which works
 /// well enough for us.
 ///
 /// If AggressiveInsts is non-null, and if V does not dominate BB, we check to
