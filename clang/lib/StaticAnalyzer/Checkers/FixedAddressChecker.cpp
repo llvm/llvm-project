36d99fc59b675737ce952087b7a71ec6e4b579a4
Balázs Kéri <balazs.keri@ericsson.com>
2024-09-30 16:51:35 +0200

    [clang][analyzer] Check initialization and argument passing in FixedAddressChecker

--- clang/lib/StaticAnalyzer/Checkers/FixedAddressChecker.cpp
+++ clang/lib/StaticAnalyzer/Checkers/FixedAddressChecker.cpp
@@ -8,6 +8,8 @@
 //
 // This files defines FixedAddressChecker, a builtin checker that checks for
 // assignment of a fixed address to a pointer.
+// Using a fixed address is not portable because that address will probably
+// not be valid in all environments or platforms.
 // This check corresponds to CWE-587.
 //
 //===----------------------------------------------------------------------===//
@@ -23,38 +25,35 @@ using namespace ento;
 
 namespace {
 class FixedAddressChecker
-  : public Checker< check::PreStmt<BinaryOperator> > {
+    : public Checker<check::PreStmt<BinaryOperator>, check::PreStmt<DeclStmt>,
+                     check::PreStmt<CallExpr>> {
   const BugType BT{this, "Use fixed address"};
+
+  void checkUseOfFixedAddress(QualType DstType, const Expr *SrcExpr,
+                              CheckerContext &C) const;
 
 public:
   void checkPreStmt(const BinaryOperator *B, CheckerContext &C) const;
+  void checkPreStmt(const DeclStmt *D, CheckerContext &C) const;
+  void checkPreStmt(const CallExpr *CE, CheckerContext &C) const;
 };
 }
 
-void FixedAddressChecker::checkPreStmt(const BinaryOperator *B,
-                                       CheckerContext &C) const {
-  // Using a fixed address is not portable because that address will probably
-  // not be valid in all environments or platforms.
-
-  if (B->getOpcode() != BO_Assign)
+void FixedAddressChecker::checkUseOfFixedAddress(QualType DstType,
+                                                 const Expr *SrcExpr,
+                                                 CheckerContext &C) const {
+  if (!DstType->isPointerType())
     return;
 
-  QualType T = B->getType();
-  if (!T->isPointerType())
+  if (SrcExpr->IgnoreParenCasts()->getType()->isPointerType())
     return;
 
-  // Omit warning if the RHS has already pointer type. Without this passing
-  // around one fixed value in several pointer variables would produce several
-  // redundant warnings.
-  if (B->getRHS()->IgnoreParenCasts()->getType()->isPointerType())
-    return;
-
-  SVal RV = C.getSVal(B->getRHS());
+  SVal RV = C.getSVal(SrcExpr);
 
   if (!RV.isConstant() || RV.isZeroConstant())
     return;
 
-  if (C.getSourceManager().isInSystemMacro(B->getRHS()->getBeginLoc()))
+  if (C.getSourceManager().isInSystemMacro(SrcExpr->getBeginLoc()))
     return;
 
   if (ExplodedNode *N = C.generateNonFatalErrorNode()) {
@@ -63,9 +62,37 @@ void FixedAddressChecker::checkPreStmt(const BinaryOperator *B,
         "Using a fixed address is not portable because that address will "
         "probably not be valid in all environments or platforms.";
     auto R = std::make_unique<PathSensitiveBugReport>(BT, Msg, N);
-    R->addRange(B->getRHS()->getSourceRange());
+    R->addRange(SrcExpr->getSourceRange());
     C.emitReport(std::move(R));
   }
+}
+
+void FixedAddressChecker::checkPreStmt(const BinaryOperator *B,
+                                       CheckerContext &C) const {
+  if (B->getOpcode() != BO_Assign)
+    return;
+
+  checkUseOfFixedAddress(B->getType(), B->getRHS(), C);
+}
+
+void FixedAddressChecker::checkPreStmt(const DeclStmt *D,
+                                       CheckerContext &C) const {
+  for (const auto *D1 : D->decls()) {
+    if (const auto *VD1 = dyn_cast<VarDecl>(D1); VD1 && VD1->hasInit())
+      checkUseOfFixedAddress(VD1->getType(), VD1->getInit(), C);
+  }
+}
+
+void FixedAddressChecker::checkPreStmt(const CallExpr *CE,
+                                       CheckerContext &C) const {
+  const FunctionDecl *Callee = CE->getDirectCallee();
+  if (!Callee)
+    return;
+  if (CE->getNumArgs() != Callee->getNumParams())
+    return;
+
+  for (auto [Arg, Param] : zip_equal(CE->arguments(), Callee->parameters()))
+    checkUseOfFixedAddress(Param->getType(), Arg, C);
 }
 
 void ento::registerFixedAddressChecker(CheckerManager &mgr) {
