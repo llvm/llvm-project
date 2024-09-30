3665111e2f0aed479f6c43ac4cf55ab9aa21dbbc
Sh0g0-1758 <shouryagoel10000@gmail.com>
2024-10-01 00:52:00 +0530

    Implemented Issignaling

--- libc/src/__support/FPUtil/BasicOperations.h
+++ libc/src/__support/FPUtil/BasicOperations.h
@@ -248,6 +248,16 @@ LIBC_INLINE T fdim(T x, T y) {
 }
 
 template <typename T, cpp::enable_if_t<cpp::is_floating_point_v<T>, int> = 0>
+LIBC_INLINE int issignaling(const T &x) {
+  FPBits<T> sx(x);
+  if (LIBC_UNLIKELY(sx.is_signaling_nan())) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+template <typename T, cpp::enable_if_t<cpp::is_floating_point_v<T>, int> = 0>
 LIBC_INLINE int canonicalize(T &cx, const T &x) {
   FPBits<T> sx(x);
   if constexpr (get_fp_type<T>() == FPType::X86_Binary80) {
