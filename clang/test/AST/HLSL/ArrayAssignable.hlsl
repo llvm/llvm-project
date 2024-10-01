728e394742ebbaade8f7e96cd24a3e2040a63c9f
Sarah Spall <spall@planetbauer.com>
2024-10-01 00:27:33 +0000

    address pr comments

--- clang/test/AST/HLSL/ArrayAssignable.hlsl
+++ clang/test/AST/HLSL/ArrayAssignable.hlsl
@@ -1,20 +1,9 @@
 // RUN: %clang_cc1 -triple dxil-pc-shadermodel6.0-library -x hlsl -ast-dump %s | FileCheck %s
 
 // CHECK-LABEL: arr_assign1
-// CHECK: CompoundStmt 0x{{[0-9a-f]+}} {{.*}}
-// CHECK: DeclStmt 0x{{[0-9a-f]+}} {{.*}}
-// CHECK: VarDecl [[A:0x[0-9a-f]+]] {{.*}} col:7 used Arr 'int[2]' cinit
-// CHECK: InitListExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2]'
-// CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 0
-// CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 1
-// CHECK: DeclStmt 0x{{[0-9a-f]+}} {{.*}}
-// CHECK: VarDecl [[B:0x[0-9a-f]+]] {{.*}} col:7 used Arr2 'int[2]' cinit
-// CHECK: InitListExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2]'
-// CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 0
-// CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 0
 // CHECK: BinaryOperator 0x{{[0-9a-f]+}} {{.*}} 'int[2]' lvalue '='
-// CHECK: DeclRefExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2]' lvalue Var [[A]] 'Arr' 'int[2]'
-// CHECK: DeclRefExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2]' lvalue Var [[B]] 'Arr2' 'int[2]'
+// CHECK: DeclRefExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2]' lvalue Var 0x{{[0-9a-f]+}} 'Arr' 'int[2]'
+// CHECK: DeclRefExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2]' lvalue Var 0x{{[0-9a-f]+}} 'Arr2' 'int[2]'
 void arr_assign1() {
   int Arr[2] = {0, 1};
   int Arr2[2] = {0, 0};
@@ -22,27 +11,11 @@ void arr_assign1() {
 }
 
 // CHECK-LABEL: arr_assign2
-// CHECK: CompoundStmt 0x{{[0-9a-f]+}} {{.*}}
-// CHECK: DeclStmt 0x{{[0-9a-f]+}} {{.*}}
-// CHECK: VarDecl [[A:0x[0-9a-f]+]] {{.*}} col:7 used Arr 'int[2]' cinit
-// CHECK: InitListExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2]'
-// CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 0
-// CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 1
-// CHECK: DeclStmt 0x{{[0-9a-f]+}} {{.*}}
-// CHECK: VarDecl [[B:0x[0-9a-f]+]] {{.*}} col:7 used Arr2 'int[2]' cinit
-// CHECK: InitListExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2]'
-// CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 0
-// CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 0
-// CHECK: DeclStmt 0x{{[0-9a-f]+}} {{.*}}
-// CHECK: VarDecl [[C:0x[0-9a-f]+]] {{.*}} col:7 used Arr3 'int[2]' cinit
-// CHECK: InitListExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2]'
-// CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 2
-// CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 2
 // CHECK: BinaryOperator 0x{{[0-9a-f]+}} {{.*}} 'int[2]' lvalue '='
-// CHECK: DeclRefExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2]' lvalue Var [[A]] 'Arr' 'int[2]'
+// CHECK: DeclRefExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2]' lvalue Var 0x{{[0-9a-f]+}} 'Arr' 'int[2]'
 // CHECK: BinaryOperator 0x{{[0-9a-f]+}} {{.*}} 'int[2]' lvalue '='
-// CHECK: DeclRefExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2]' lvalue Var [[B]] 'Arr2' 'int[2]'
-// CHECK: DeclRefExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2]' lvalue Var [[C]] 'Arr3' 'int[2]'
+// CHECK: DeclRefExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2]' lvalue Var 0x{{[0-9a-f]+}} 'Arr2' 'int[2]'
+// CHECK: DeclRefExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2]' lvalue Var 0x{{[0-9a-f]+}} 'Arr3' 'int[2]'
 void arr_assign2() {
   int Arr[2] = {0, 1};
   int Arr2[2] = {0, 0};
@@ -51,28 +24,9 @@ void arr_assign2() {
 }
 
 // CHECK-LABEL: arr_assign3
-// CHECK: CompoundStmt 0x{{[0-9a-f]+}} {{.*}}
-// CHECK: DeclStmt 0x{{[0-9a-f]+}} {{.*}}
-// CHECK: VarDecl [[A:0x[0-9a-f]+]] {{.*}} col:7 used Arr 'int[2][2]' cinit
-// CHECK: InitListExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2][2]'
-// CHECK: InitListExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2]'
-// CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 0
-// CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 1
-// CHECK: InitListExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2]'
-// CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 2
-// CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 3
-// CHECK: DeclStmt 0x{{[0-9a-f]+}} {{.*}}
-// CHECK: VarDecl [[B:0x[0-9a-f]+]] {{.*}} col:7 used Arr2 'int[2][2]' cinit
-// CHECK: InitListExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2][2]'
-// CHECK: InitListExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2]'
-// CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 0
-// CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 0
-// CHECK: InitListExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2]'
-// CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 1
-// CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 1
 // CHECK: BinaryOperator 0x{{[0-9a-f]+}} {{.*}} 'int[2][2]' lvalue '='
-// CHECK: DeclRefExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2][2]' lvalue Var [[A]] 'Arr' 'int[2][2]'
-// CHECK: DeclRefExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2][2]' lvalue Var [[B]] 'Arr2' 'int[2][2]'
+// CHECK: DeclRefExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2][2]' lvalue Var 0x{{[0-9a-f]+}} 'Arr' 'int[2][2]'
+// CHECK: DeclRefExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2][2]' lvalue Var 0x{{[0-9a-f]+}} 'Arr2' 'int[2][2]'
 void arr_assign3() {
   int Arr[2][2] = {{0, 1}, {2, 3}};
   int Arr2[2][2] = {{0, 0}, {1, 1}};
@@ -80,24 +34,13 @@ void arr_assign3() {
 }
 
 // CHECK-LABEL: arr_assign4
-// CHECK: CompoundStmt 0x{{[0-9a-f]+}} {{.*}}
-// CHECK: DeclStmt 0x{{[0-9a-f]+}} {{.*}}
-// CHECK: VarDecl [[A:0x[0-9a-f]+]] {{.*}} col:7 used Arr 'int[2]' cinit
-// CHECK: InitListExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2]'
-// CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 0
-// CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 1
-// CHECK: DeclStmt 0x{{[0-9a-f]+}} {{.*}}
-// CHECK: VarDecl [[B:0x[0-9a-f]+]] {{.*}} col:7 used Arr2 'int[2]' cinit
-// CHECK: InitListExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2]'
-// CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 0
-// CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 0
 // CHECK: BinaryOperator 0x{{[0-9a-f]+}} {{.*}} 'int' lvalue '='
 // CHECK: ArraySubscriptExpr 0x{{[0-9a-f]+}} {{.*}} 'int' lvalue
 // CHECK: ImplicitCastExpr 0x{{[0-9a-f]+}} {{.*}} 'int *' <ArrayToPointerDecay>
 // CHECK: ParenExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2]' lvalue
 // CHECK: BinaryOperator 0x{{[0-9a-f]+}} {{.*}} 'int[2]' lvalue '='
-// CHECK: DeclRefExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2]' lvalue Var [[A]] 'Arr' 'int[2]'
-// CHECK: DeclRefExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2]' lvalue Var [[B]] 'Arr2' 'int[2]'
+// CHECK: DeclRefExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2]' lvalue Var 0x{{[0-9a-f]+}} 'Arr' 'int[2]'
+// CHECK: DeclRefExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2]' lvalue Var 0x{{[0-9a-f]+}} 'Arr2' 'int[2]'
 // CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 0
 // CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 6
 void arr_assign4() {
@@ -107,31 +50,15 @@ void arr_assign4() {
 }
 
 // CHECK-LABEL: arr_assign5
-// CHECK: CompoundStmt 0x{{[0-9a-f]+}} {{.*}}
-// CHECK: DeclStmt 0x{{[0-9a-f]+}} {{.*}}
-// CHECK: VarDecl [[A:0x[0-9a-f]+]] {{.*}} col:7 used Arr 'int[2]' cinit
-// CHECK: InitListExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2]'
-// CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 0
-// CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 1
-// CHECK: DeclStmt 0x{{[0-9a-f]+}} {{.*}}
-// CHECK: VarDecl [[B:0x[0-9a-f]+]] {{.*}} col:7 used Arr2 'int[2]' cinit
-// CHECK: InitListExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2]'
-// CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 0
-// CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 0
-// CHECK: DeclStmt 0x{{[0-9a-f]+}} {{.*}}
-// CHECK: VarDecl [[C:0x[0-9a-f]+]] {{.*}} col:7 used Arr3 'int[2]' cinit
-// CHECK: InitListExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2]'
-// CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 3
-// CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 4
 // CHECK: BinaryOperator 0x{{[0-9a-f]+}} {{.*}} 'int' lvalue '='
 // CHECK: ArraySubscriptExpr 0x{{[0-9a-f]+}} {{.*}} 'int' lvalue
 // CHECK: ImplicitCastExpr 0x{{[0-9a-f]+}} {{.*}} 'int *' <ArrayToPointerDecay>
 // CHECK: ParenExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2]' lvalue
 // CHECK: BinaryOperator 0x{{[0-9a-f]+}} {{.*}} 'int[2]' lvalue '='
-// CHECK: DeclRefExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2]' lvalue Var [[A]] 'Arr' 'int[2]'
+// CHECK: DeclRefExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2]' lvalue Var 0x{{[0-9a-f]+}} 'Arr' 'int[2]'
 // CHECK: BinaryOperator 0x{{[0-9a-f]+}} {{.*}} 'int[2]' lvalue '='
-// CHECK: DeclRefExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2]' lvalue Var [[B]] 'Arr2' 'int[2]'
-// CHECK: DeclRefExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2]' lvalue Var [[C]] 'Arr3' 'int[2]'
+// CHECK: DeclRefExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2]' lvalue Var 0x{{[0-9a-f]+}} 'Arr2' 'int[2]'
+// CHECK: DeclRefExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2]' lvalue Var 0x{{[0-9a-f]+}} 'Arr3' 'int[2]'
 // CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 0
 // CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 6
 void arr_assign5() {
@@ -142,25 +69,6 @@ void arr_assign5() {
 }
 
 // CHECK-LABEL: arr_assign6
-// CHECK: CompoundStmt 0x{{[0-9a-f]+}} {{.*}}
-// CHECK: DeclStmt 0x{{[0-9a-f]+}} {{.*}}
-// CHECK: VarDecl [[A:0x[0-9a-f]+]] {{.*}} col:7 used Arr 'int[2][2]' cinit
-// CHECK: InitListExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2][2]'
-// CHECK: InitListExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2]'
-// CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 0
-// CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 1
-// CHECK: InitListExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2]'
-// CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 2
-// CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 3
-// CHECK: DeclStmt 0x{{[0-9a-f]+}} {{.*}}
-// CHECK: VarDecl [[B:0x[0-9a-f]+]] {{.*}} col:7 used Arr2 'int[2][2]' cinit
-// CHECK: InitListExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2][2]'
-// CHECK: InitListExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2]'
-// CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 0
-// CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 0
-// CHECK: InitListExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2]'
-// CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 1
-// CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 1
 // CHECK: BinaryOperator 0x{{[0-9a-f]+}} {{.*}} 'int' lvalue '='
 // CHECK: ArraySubscriptExpr 0x{{[0-9a-f]+}} {{.*}} 'int' lvalue
 // CHECK: ImplicitCastExpr 0x{{[0-9a-f]+}} {{.*}} 'int *' <ArrayToPointerDecay>
@@ -168,8 +76,8 @@ void arr_assign5() {
 // CHECK: ImplicitCastExpr 0x{{[0-9a-f]+}} {{.*}} 'int (*)[2]' <ArrayToPointerDecay>
 // CHECK: ParenExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2][2]' lvalue
 // CHECK: BinaryOperator 0x{{[0-9a-f]+}} {{.*}} 'int[2][2]' lvalue '='
-// CHECK: DeclRefExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2][2]' lvalue Var [[A]] 'Arr' 'int[2][2]'
-// CHECK: DeclRefExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2][2]' lvalue Var [[B]] 'Arr2' 'int[2][2]'
+// CHECK: DeclRefExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2][2]' lvalue Var 0x{{[0-9a-f]+}} 'Arr' 'int[2][2]'
+// CHECK: DeclRefExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2][2]' lvalue Var 0x{{[0-9a-f]+}} 'Arr2' 'int[2][2]'
 // CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 0
 // CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 0
 // CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 6
@@ -180,32 +88,13 @@ void arr_assign6() {
 }
 
 // CHECK-LABEL: arr_assign7
-// CHECK: CompoundStmt 0x{{[0-9a-f]+}} {{.*}}
-// CHECK: DeclStmt 0x{{[0-9a-f]+}} {{.*}}
-// CHECK: VarDecl [[A:0x[0-9a-f]+]] {{.*}} col:7 used Arr 'int[2][2]' cinit
-// CHECK: InitListExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2][2]'
-// CHECK: InitListExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2]'
-// CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 0
-// CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 1
-// CHECK: InitListExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2]'
-// CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 2
-// CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 3
-// CHECK: DeclStmt 0x{{[0-9a-f]+}} {{.*}}
-// CHECK: VarDecl [[B:0x[0-9a-f]+]] {{.*}} col:7 used Arr2 'int[2][2]' cinit
-// CHECK: InitListExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2][2]'
-// CHECK: InitListExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2]'
-// CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 0
-// CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 0
-// CHECK: InitListExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2]'
-// CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 1
-// CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 1
 // CHECK: BinaryOperator 0x{{[0-9a-f]+}} {{.*}} 'int[2]' lvalue '='
 // CHECK: ArraySubscriptExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2]' lvalue
 // CHECK: ImplicitCastExpr 0x{{[0-9a-f]+}} {{.*}} 'int (*)[2]' <ArrayToPointerDecay>
 // CHECK: ParenExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2][2]' lvalue
 // CHECK: BinaryOperator 0x{{[0-9a-f]+}} {{.*}} 'int[2][2]' lvalue '='
-// CHECK: DeclRefExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2][2]' lvalue Var [[A]] 'Arr' 'int[2][2]'
-// CHECK: DeclRefExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2][2]' lvalue Var [[B]] 'Arr2' 'int[2][2]'
+// CHECK: DeclRefExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2][2]' lvalue Var 0x{{[0-9a-f]+}} 'Arr' 'int[2][2]'
+// CHECK: DeclRefExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2][2]' lvalue Var 0x{{[0-9a-f]+}} 'Arr2' 'int[2][2]'
 // CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 0
 // CHECK: InitListExpr 0x{{[0-9a-f]+}} {{.*}} 'int[2]'
 // CHECK: IntegerLiteral 0x{{[0-9a-f]+}} {{.*}} 'int' 6
