477486f7a14628de7072bc3b532d1834c5440759
B I Mohammed Abbas <bimohammadabbas@gmail.com>
2024-10-01 11:25:45 +0530

    Add testcase for extendhfxf2

--- compiler-rt/lib/builtins/extendhfxf2.c
+++ compiler-rt/lib/builtins/extendhfxf2.c
@@ -1,4 +1,4 @@
-//===-- lib/extendhfxf2.c - half -> long double conversion -------------*- C -*-===//
+//===-- lib/extendhfxf2.c - half -> long double conversion --------*- C -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
