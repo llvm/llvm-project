ed9d3f7092f82a9b70a3484283e9998340656a0d
Younan Zhang <zyn7109@gmail.com>
2024-10-01 08:57:39 +0800

    Address format issue

--- clang/lib/Sema/SemaTemplateInstantiate.cpp
+++ clang/lib/Sema/SemaTemplateInstantiate.cpp
@@ -1753,7 +1753,8 @@ namespace {
         for (const TemplateArgument &TA : Iter->Args)
           assert(TA.getKind() != TemplateArgument::Pack || TA.pack_size() == 1);
 #endif
-      Sema::ArgumentPackSubstitutionIndexRAII SubstIndex(SemaRef, /*NewSubstitutionIndex=*/0);
+      Sema::ArgumentPackSubstitutionIndexRAII SubstIndex(
+          SemaRef, /*NewSubstitutionIndex=*/0);
       Decl *NewPack = TransformDecl(PackLoc, Pack);
       if (!NewPack)
         return ExprError();
