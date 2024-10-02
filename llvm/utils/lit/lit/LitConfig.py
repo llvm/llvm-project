0cdbeb00beb0f24c2f7bee002d5151f02f5d218f
Henrik G. Olsson <hnrklssn@gmail.com>
2024-10-01 17:14:50 -0700

    Revert "[Utils] Add new --update-tests flag to llvm-lit" (#110772)
    Reverts llvm/llvm-project#108425

--- llvm/utils/lit/lit/LitConfig.py
+++ llvm/utils/lit/lit/LitConfig.py
@@ -38,7 +38,6 @@ class LitConfig(object):
         parallelism_groups={},
         per_test_coverage=False,
         gtest_sharding=True,
-        update_tests=False,
     ):
         # The name of the test runner.
         self.progname = progname
@@ -90,8 +89,6 @@ class LitConfig(object):
         self.parallelism_groups = parallelism_groups
         self.per_test_coverage = per_test_coverage
         self.gtest_sharding = bool(gtest_sharding)
-        self.update_tests = update_tests
-        self.test_updaters = []
 
     @property
     def maxIndividualTestTime(self):
