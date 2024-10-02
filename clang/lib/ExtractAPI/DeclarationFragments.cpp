8fb46ffb7213d3d708ab27a264644be548b80333
Daniel Grumberg <dgrumberg@apple.com>
2024-10-02 13:14:15 +0100

    [clang][ExtractAPI] Generate subheading for typedef'd anonymous types (#110689) (#9368)
    When an anonymous type has a typedef we normally use the typedef's name
    in places where we expect a named identifier in the symbol graph. This
    extends this logic to apply to subheadings.
    
    rdar://136690614

--- clang/lib/ExtractAPI/DeclarationFragments.cpp
+++ clang/lib/ExtractAPI/DeclarationFragments.cpp
@@ -1622,6 +1622,9 @@ DeclarationFragmentsBuilder::getSubHeading(const NamedDecl *Decl) {
              cast<CXXMethodDecl>(Decl)->isOverloadedOperator()) {
     Fragments.append(Decl->getNameAsString(),
                      DeclarationFragments::FragmentKind::Identifier);
+  } else if (isa<TagDecl>(Decl) &&
+             cast<TagDecl>(Decl)->getTypedefNameForAnonDecl()) {
+    return getSubHeading(cast<TagDecl>(Decl)->getTypedefNameForAnonDecl());
   } else if (Decl->getIdentifier()) {
     Fragments.append(Decl->getName(),
                      DeclarationFragments::FragmentKind::Identifier);
