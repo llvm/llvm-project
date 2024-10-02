f91cf985d10a07af617b6098f50d023108d060a4
Keith Packard <keithp@keithp.com>
2024-10-02 12:37:30 -0700

    [RISCV][ISelLowering] Use getModule() instead of getParent()->getParent()
    This is a simple clean-up motivated by review of changes adding TLS
    support for the stack canary. Using getModule() is a shorter and
    clearer expression of the desired operation.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- llvm/lib/Target/RISCV/RISCVISelLowering.cpp
+++ llvm/lib/Target/RISCV/RISCVISelLowering.cpp
@@ -21304,7 +21304,7 @@ bool RISCVTargetLowering::preferScalarizeSplat(SDNode *N) const {
 }
 
 static Value *useTpOffset(IRBuilderBase &IRB, unsigned Offset) {
-  Module *M = IRB.GetInsertBlock()->getParent()->getParent();
+  Module *M = IRB.GetInsertBlock()->getModule();
   Function *ThreadPointerFunc =
       Intrinsic::getDeclaration(M, Intrinsic::thread_pointer);
   return IRB.CreateConstGEP1_32(IRB.getInt8Ty(),
