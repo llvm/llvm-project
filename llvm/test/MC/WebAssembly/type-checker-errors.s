98122cdb6fb76fee9c4d6ee15c29b6e6dee7d235
Heejin Ahn <aheejin@gmail.com>
2024-10-02 01:19:47 +0000

    [WebAssembly] Support type checker for new EH
    This adds supports for the new EH instructions (`try_table` and
    `throw_ref`) to the type checker.
    
    One thing I'd like to improve on is the locations in the errors for
    `catch_***` clauses. Currently they just point to the starting column of
    `try_table` instruction itself. But to figure out where catch clauses
    start you need to traverse `OperandVector` and check
    `WebAssemblyOperand::isCatchList` on them to see which one is the catch
    list operand, but `WebAssemblyOperand` class is in AsmParser and
    AsmTypeCheck does not have access to it:
    https://github.com/llvm/llvm-project/blob/cdfdc857cbab0418b7e5116fd4255eb5566588bd/llvm/lib/Target/WebAssembly/AsmParser/WebAssemblyAsmParser.cpp#L43-L204
    And even if AsmTypeCheck has access to it, currently it treats the list
    of catch clauses as a single `WebAssemblyOperand` so there is no way to
    get the starting location of each `catch_***` clause in the current
    structure.
    
    This also renames `valTypeToStackType` to `valTypesToStackTypes`, given
    that it takes two type lists.

--- llvm/test/MC/WebAssembly/type-checker-errors.s
+++ llvm/test/MC/WebAssembly/type-checker-errors.s
@@ -944,3 +944,26 @@ block_param_and_return:
 
 # CHECK: :[[@LINE+1]]:3: error: type mismatch, expected [] but got [f32]
   end_function
+
+  .tagtype  __cpp_exception i32
+
+eh_test:
+  .functype eh_test () -> ()
+  block i32
+    block i32
+      block i32
+        block
+# CHECK: :[[@LINE+4]]:11: error: try_table: catch index 0: type mismatch, catch tag type is [i32], but destination's return type is []
+# CHECK: :[[@LINE+3]]:11: error: try_table: catch index 1: type mismatch, catch tag type is [i32, exnref], but destination's return type is [i32]
+# CHECK: :[[@LINE+2]]:11: error: try_table: catch index 2: type mismatch, catch tag type is [], but destination's return type is [i32]
+# CHECK: :[[@LINE+1]]:11: error: try_table: catch index 3: type mismatch, catch tag type is [exnref], but destination's return type is [i32]
+          try_table i32 (catch __cpp_exception 0) (catch_ref __cpp_exception 1) (catch_all 2) (catch_all_ref 3)
+# CHECK: :[[@LINE+1]]:11: error: type mismatch, expected [i32] but got []
+          end_try_table
+# CHECK: :[[@LINE+1]]:9: error: type mismatch, expected [] but got [i32]
+        end_block
+      end_block
+    end_block
+  end_block
+  drop
+  end_function
