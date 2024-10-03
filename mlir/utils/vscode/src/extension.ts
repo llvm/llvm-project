d3cf13a1dfae66f7a2730e033d473288f091eea4
Kuba Jalowiec <jakub.jalowiec@intel.com>
2024-10-03 20:38:58 +0000

    [mlir-vscode] Added LIT test discovery functionality

--- mlir/utils/vscode/src/extension.ts
+++ mlir/utils/vscode/src/extension.ts
@@ -3,7 +3,7 @@ import * as vscode from 'vscode';
 import {registerMLIRExtensions} from './MLIR/mlir';
 import {MLIRContext} from './mlirContext';
 import {registerPDLLExtensions} from './PDLL/pdll';
-
+import { LitTestProvider } from './LIT/lit';
 /**
  *  This method is called when the extension is activated. The extension is
  *  activated the very first time a command is executed.
@@ -15,6 +15,9 @@ export function activate(context: vscode.ExtensionContext) {
   const mlirContext = new MLIRContext();
   context.subscriptions.push(mlirContext);
 
+  const litTests = new LitTestProvider(context);  // Instantiate the LitTestProvider
+  context.subscriptions.push(litTests);  // Push it to context.subscriptions to handle cleanup
+
   // Initialize the commands of the extension.
   context.subscriptions.push(
       vscode.commands.registerCommand('mlir.restart', async () => {
@@ -22,6 +25,9 @@ export function activate(context: vscode.ExtensionContext) {
         mlirContext.dispose();
         await mlirContext.activate(outputChannel);
       }));
+  context.subscriptions.push(vscode.commands.registerCommand('lit.reconfigure', () => {
+    litTests.reconfigureLitSettings();
+  }));
   registerMLIRExtensions(context, mlirContext);
   registerPDLLExtensions(context, mlirContext);
 
