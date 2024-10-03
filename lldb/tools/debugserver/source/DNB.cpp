af1d05509f58f5670c524814059fc3c765acad6c
Jason Molenda <jmolenda@apple.com>
2024-10-03 15:00:36 -0700

    Update another error exit to DNBLogError the error message and then return attach failed.

--- lldb/tools/debugserver/source/DNB.cpp
+++ lldb/tools/debugserver/source/DNB.cpp
@@ -475,9 +475,9 @@ nub_process_t DNBProcessAttach(nub_process_t attach_pid,
         extern int communication_fd;
 
         if (communication_fd == -1) {
-          fprintf(stderr, "Trying to attach to a translated process with the "
-                          "native debugserver, exiting...\n");
-          exit(1);
+          DNBLogError("Trying to attach to a translated process with the "
+                      "native debugserver, exiting...\n");
+          return INVALID_NUB_PROCESS_ARCH;
         }
 
         struct stat st;
