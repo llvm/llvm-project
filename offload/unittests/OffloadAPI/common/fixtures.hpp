133fbc172bf42f9617096c8ccdcb9321da8ac991
Callum Fare <callum@codeplay.com>
2024-10-04 16:16:01 +0100

    Rework Offload API errors

--- offload/unittests/OffloadAPI/common/fixtures.hpp
+++ offload/unittests/OffloadAPI/common/fixtures.hpp
@@ -15,7 +15,16 @@
 #pragma once
 
 #ifndef ASSERT_SUCCESS
-#define ASSERT_SUCCESS(ACTUAL) ASSERT_EQ(OFFLOAD_RESULT_SUCCESS, ACTUAL)
+#define ASSERT_SUCCESS(ACTUAL) ASSERT_EQ(OFFLOAD_SUCCESS, ACTUAL)
+#endif
+
+// TODO: rework this so the EXPECTED/ACTUAL results are readable
+#ifndef ASSERT_ERROR
+#define ASSERT_ERROR(EXPECTED, ACTUAL)                                         \
+  do {                                                                         \
+    offload_result_t Res = ACTUAL;                                             \
+    ASSERT_TRUE(Res && (Res->code == EXPECTED));                               \
+  } while (0)
 #endif
 
 #define RETURN_ON_FATAL_FAILURE(...)                                           \
