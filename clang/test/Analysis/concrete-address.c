51a2e4470e935f35d06961f0d284afaeae31356b
Balázs Kéri <balazs.keri@ericsson.com>
2024-10-04 10:30:33 +0200

    updated failing tests

--- clang/test/Analysis/concrete-address.c
+++ clang/test/Analysis/concrete-address.c
@@ -1,7 +1,7 @@
 // RUN: %clang_analyze_cc1 -analyzer-checker=core,alpha.core -verify %s
-// expected-no-diagnostics
 
 void foo(void) {
-  int *p = (int*) 0x10000; // Should not crash here.
+  // Should not crash at next line.
+  int *p = (int*) 0x10000; // expected-warning{{Using a fixed address is not portable}}
   *p = 3;
 }
