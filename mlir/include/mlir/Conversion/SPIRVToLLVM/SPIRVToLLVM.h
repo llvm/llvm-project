139041775f9b5323287fca769de231fa30bb17e2
Matthias Springer <me@m-sp.org>
2024-10-05 21:32:40 +0200

    [mlir][NFC] Mark type converter in `populate...` functions as `const` (#111250)
    This commit marks the type converter in `populate...` functions as
    `const`. This is useful for debugging.
    
    Patterns already take a `const` type converter. However, some
    `populate...` functions do not only add new patterns, but also add
    additional type conversion rules. That makes it difficult to find the
    place where a type conversion was added in the code base. With this
    change, all `populate...` functions that only populate pattern now have
    a `const` type converter. Programmers can then conclude from the
    function signature that these functions do not register any new type
    conversion rules.
    
    Also some minor cleanups around the 1:N dialect conversion
    infrastructure, which did not always pass the type converter as a
    `const` object internally.

--- mlir/include/mlir/Conversion/SPIRVToLLVM/SPIRVToLLVM.h
+++ mlir/include/mlir/Conversion/SPIRVToLLVM/SPIRVToLLVM.h
@@ -25,13 +25,16 @@ class ModuleOp;
 template <typename SPIRVOp>
 class SPIRVToLLVMConversion : public OpConversionPattern<SPIRVOp> {
 public:
-  SPIRVToLLVMConversion(MLIRContext *context, LLVMTypeConverter &typeConverter,
+  SPIRVToLLVMConversion(MLIRContext *context,
+                        const LLVMTypeConverter &typeConverter,
                         PatternBenefit benefit = 1)
-      : OpConversionPattern<SPIRVOp>(typeConverter, context, benefit),
-        typeConverter(typeConverter) {}
+      : OpConversionPattern<SPIRVOp>(typeConverter, context, benefit) {}
 
 protected:
-  LLVMTypeConverter &typeConverter;
+  const LLVMTypeConverter *getTypeConverter() const {
+    return static_cast<const LLVMTypeConverter *>(
+        ConversionPattern::getTypeConverter());
+  }
 };
 
 /// Encodes global variable's descriptor set and binding into its name if they
@@ -46,18 +49,18 @@ void populateSPIRVToLLVMTypeConversion(
 
 /// Populates the given list with patterns that convert from SPIR-V to LLVM.
 void populateSPIRVToLLVMConversionPatterns(
-    LLVMTypeConverter &typeConverter, RewritePatternSet &patterns,
+    const LLVMTypeConverter &typeConverter, RewritePatternSet &patterns,
     spirv::ClientAPI clientAPIForAddressSpaceMapping =
         spirv::ClientAPI::Unknown);
 
 /// Populates the given list with patterns for function conversion from SPIR-V
 /// to LLVM.
 void populateSPIRVToLLVMFunctionConversionPatterns(
-    LLVMTypeConverter &typeConverter, RewritePatternSet &patterns);
+    const LLVMTypeConverter &typeConverter, RewritePatternSet &patterns);
 
 /// Populates the given patterns for module conversion from SPIR-V to LLVM.
 void populateSPIRVToLLVMModuleConversionPatterns(
-    LLVMTypeConverter &typeConverter, RewritePatternSet &patterns);
+    const LLVMTypeConverter &typeConverter, RewritePatternSet &patterns);
 
 } // namespace mlir
 
