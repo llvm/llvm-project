7c813f065cc0c32095d35a1dbad7e28ec8b4a998
Зишан Мирза <zmirza@tutanota.de>
2024-10-06 19:42:14 +0200

    added tests for localtime_r

--- libc/test/src/time/localtime_r_test.cpp
+++ libc/test/src/time/localtime_r_test.cpp
@@ -15,8 +15,19 @@ using LIBC_NAMESPACE::time_utils::TimeConstants;
 
 TEST(LlvmLibcLocaltimeR, ValidUnixTimestamp0) {
   struct tm input;
-  const time_t t_ptr = 0;
+  time_t t_ptr = 0;
   struct tm *result = LIBC_NAMESPACE::localtime_r(&t_ptr, &input);
+
+  ASSERT_EQ(70, input.tm_year);
+  ASSERT_EQ(0, input.tm_mon);
+  ASSERT_EQ(1, input.tm_mday);
+  ASSERT_EQ(2, input.tm_hour);
+  ASSERT_EQ(0, input.tm_min);
+  ASSERT_EQ(0, input.tm_sec);
+  ASSERT_EQ(4, input.tm_wday);
+  ASSERT_EQ(0, input.tm_yday);
+  ASSERT_EQ(0, input.tm_isdst);
+
   ASSERT_EQ(70, result->tm_year);
   ASSERT_EQ(0, result->tm_mon);
   ASSERT_EQ(1, result->tm_mday);
@@ -40,6 +51,17 @@ TEST(LlvmLibcLocaltimeR, ValidUnixTimestamp32Int) {
                                 .tm_yday = 0,
                                 .tm_isdst = 0};
   struct tm *result = LIBC_NAMESPACE::localtime_r(&t_ptr, &input);
+
+  ASSERT_EQ(138, input.tm_year);
+  ASSERT_EQ(0, input.tm_mon);
+  ASSERT_EQ(19, input.tm_mday);
+  ASSERT_EQ(5, input.tm_hour);
+  ASSERT_EQ(14, input.tm_min);
+  ASSERT_EQ(7, input.tm_sec);
+  ASSERT_EQ(2, input.tm_wday);
+  ASSERT_EQ(18, input.tm_yday);
+  ASSERT_EQ(0, input.tm_isdst);
+
   ASSERT_EQ(138, result->tm_year);
   ASSERT_EQ(0, result->tm_mon);
   ASSERT_EQ(19, result->tm_mday);
@@ -63,6 +85,17 @@ TEST(LlvmLibcLocaltimeR, ValidUnixTimestamp32IntDst) {
                                 .tm_yday = 0,
                                 .tm_isdst = 0};
   struct tm *result = LIBC_NAMESPACE::localtime_r(&t_ptr, &input);
+
+  ASSERT_EQ(121, input.tm_year);
+  ASSERT_EQ(6, input.tm_mon);
+  ASSERT_EQ(25, input.tm_mday);
+  ASSERT_EQ(17, input.tm_hour);
+  ASSERT_EQ(4, input.tm_min);
+  ASSERT_EQ(25, input.tm_sec);
+  ASSERT_EQ(0, input.tm_wday);
+  ASSERT_EQ(205, input.tm_yday);
+  ASSERT_EQ(1, input.tm_isdst);
+
   ASSERT_EQ(121, result->tm_year);
   ASSERT_EQ(6, result->tm_mon);
   ASSERT_EQ(25, result->tm_mday);
