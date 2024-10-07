6010fb2a19b88ca692bb029ce7e975fb54704ef7
PeterChou1 <peter.chou@mail.utoronto.ca>
2024-10-07 15:07:41 -0400

    [clang-doc] add more templates

--- clang-tools-extra/clang-doc/HTMLMustacheGenerator.cpp
+++ clang-tools-extra/clang-doc/HTMLMustacheGenerator.cpp
@@ -7,6 +7,7 @@
 //===----------------------------------------------------------------------===//
 #include "Generators.h"
 #include "Representation.h"
+#include "FileHelpersClangDoc.h"
 #include "llvm/Support/MemoryBuffer.h"
 #include "llvm/Support/Mustache.h"
 
@@ -48,9 +49,8 @@ public:
   Error registerPartialFile(StringRef Name, StringRef FileName) {
     ErrorOr<std::unique_ptr<MemoryBuffer>> BufferOrError =
         MemoryBuffer::getFile(FileName);
-    if (auto EC = BufferOrError.getError()) {
+    if (auto EC = BufferOrError.getError())
       return llvm::createFileError("cannot open file", EC);
-    }
     std::unique_ptr<llvm::MemoryBuffer> Buffer = std::move(BufferOrError.get());
     llvm::StringRef FileContent = Buffer->getBuffer();
     registerPartial(Name, FileContent);
@@ -65,24 +65,54 @@ static std::unique_ptr<MustacheTemplateFile> NamespaceTemplate = nullptr;
 static std::unique_ptr<MustacheTemplateFile> RecordTemplate = nullptr;
 
 
-llvm::Error 
-setupTemplateFiles(const clang::doc::ClangDocContext &CDCtx) {
-  auto TemplateFilePath = CDCtx.MustacheTemplates.lookup("template");
-  auto Template = MustacheTemplateFile::createMustacheFile(TemplateFilePath);
-  if (auto EC = Template.getError()) {
-    return llvm::createFileError("cannot open file", EC);
+llvm::Error setupTemplate(
+                   std::unique_ptr<MustacheTemplateFile> &Template,
+                   StringRef TemplatePath,
+                   std::vector<std::pair<StringRef, StringRef>> Partials) {
+  auto T = MustacheTemplateFile::createMustacheFile(TemplatePath);
+  if (auto EC = T.getError())
+     return llvm::createFileError("cannot open file", EC);
+  Template = std::move(T.get());
+  for (const auto &P : Partials) {
+    auto Err = Template->registerPartialFile(P.first, P.second);
+    if (Err)
+      return Err;
   }
-  NamespaceTemplate = std::move(Template.get());
   return llvm::Error::success();
 }
+
+llvm::Error 
+setupTemplateFiles(const clang::doc::ClangDocContext &CDCtx) {
+  auto NamespaceFilePath = CDCtx.MustacheTemplates.lookup("namespace-template");
+  auto ClassFilePath = CDCtx.MustacheTemplates.lookup("class-template");
+  auto CommentFilePath = CDCtx.MustacheTemplates.lookup("comments-template");
+  auto FunctionFilePath = CDCtx.MustacheTemplates.lookup("function-template");
+  auto EnumFilePath = CDCtx.MustacheTemplates.lookup("enum-template");
+  std::vector<std::pair<StringRef, StringRef>> Partials = {
+    {"Comments", CommentFilePath},
+    {"FunctionPartial", FunctionFilePath},
+    {"EnumPartial", EnumFilePath}
+  };
+  
+  auto Err = setupTemplate(NamespaceTemplate, NamespaceFilePath, Partials);
+  if (Err)
+    return Err;
+  
+  Err = setupTemplate(RecordTemplate, ClassFilePath, Partials);
+  
+  if (Err)
+    return Err;
+  
+  return llvm::Error::success();
+}
+
 
 llvm::Error 
 MustacheHTMLGenerator::generateDocs(llvm::StringRef RootDir, 
                                     llvm::StringMap<std::unique_ptr<doc::Info>> Infos, 
                                     const clang::doc::ClangDocContext &CDCtx) {
-  if (auto Err = setupTemplateFiles(CDCtx)) {
+  if (auto Err = setupTemplateFiles(CDCtx))
     return Err;
-  }
   // Track which directories we already tried to create.
   llvm::StringSet<> CreatedDirs;
   // Collect all output by file name and create the necessary directories.
@@ -95,10 +125,9 @@ MustacheHTMLGenerator::generateDocs(llvm::StringRef RootDir,
     llvm::sys::path::append(Path, Info->getRelativeFilePath(""));
     if (!CreatedDirs.contains(Path)) {
       if (std::error_code Err = llvm::sys::fs::create_directories(Path);
-          Err != std::error_code()) {
+          Err != std::error_code())
         return llvm::createStringError(Err, "Failed to create directory '%s'.",
                                        Path.c_str());
-      }
       CreatedDirs.insert(Path);
     }
 
@@ -110,14 +139,13 @@ MustacheHTMLGenerator::generateDocs(llvm::StringRef RootDir,
     std::error_code FileErr;
     llvm::raw_fd_ostream InfoOS(Group.getKey(), FileErr,
                                 llvm::sys::fs::OF_None);
-    if (FileErr) {
+    if (FileErr)
       return llvm::createStringError(FileErr, "Error opening file '%s'",
                                      Group.getKey().str().c_str());
-    }
+    
     for (const auto &Info : Group.getValue()) {
-      if (llvm::Error Err = generateDocForInfo(Info, InfoOS, CDCtx)) {
+      if (llvm::Error Err = generateDocForInfo(Info, InfoOS, CDCtx))
         return Err;
-      }
     }
   }
   return llvm::Error::success();
@@ -126,12 +154,15 @@ MustacheHTMLGenerator::generateDocs(llvm::StringRef RootDir,
 Value extractValue(const Location &L, 
                    std::optional<StringRef> RepositoryUrl = std::nullopt) {
   Object Obj = Object();
+  Obj.insert({"LineNumber", L.LineNumber});
+  Obj.insert({"Filename", L.Filename});
+  
   if (!L.IsFileInRootDir || !RepositoryUrl) {
-    Obj.insert({"LineNumber", L.LineNumber});
-    Obj.insert({"Filename", L.Filename});
+    return Obj;
   }
   SmallString<128> FileURL(*RepositoryUrl);
   llvm::sys::path::append(FileURL, llvm::sys::path::Style::posix, L.Filename);
+  FileURL += "#" + std::to_string(L.LineNumber);
   Obj.insert({"FileURL", FileURL});
   
   return Obj;
@@ -144,6 +175,8 @@ Value extractValue(const Reference &I, StringRef CurrentDirectory) {
   Object Obj = Object();
   Obj.insert({"Link", Path});
   Obj.insert({"Name", I.Name});
+  Obj.insert({"QualName", I.QualName});
+  Obj.insert({"ID", llvm::toHex(llvm::toStringRef(I.USR))});
   return Obj;
 }
 
@@ -209,12 +242,13 @@ Value extractValue(const FunctionInfo &I, StringRef ParentInfoDir,
       ArrDesc.getAsArray()->emplace_back(extractValue(Child));
     Obj.insert({"FunctionComments", ArrDesc});
   }
-  if (I.DefLoc) {
-    if (!CDCtx.RepositoryUrl)
-      Obj.insert({"Location", extractValue(*I.DefLoc)});
+  if (I.DefLoc.has_value()) {
+    Location L = *I.DefLoc;
+    if (CDCtx.RepositoryUrl.has_value())
+      Obj.insert({"Location", extractValue(L,
+                                           StringRef{*CDCtx.RepositoryUrl})});
     else
-      Obj.insert({"Location", extractValue(*I.DefLoc, 
-                                           StringRef{*CDCtx.RepositoryUrl})});  
+      Obj.insert({"Location", extractValue(L)});  
   }
   return Obj;
 }
@@ -229,7 +263,6 @@ Value extractValue(const EnumInfo &I, const ClangDocContext &CDCtx) {
   Obj.insert({"EnumName", EnumType});
   Obj.insert({"HasComment", HasComment});
   Obj.insert({"ID", llvm::toHex(llvm::toStringRef(I.USR))});
-  
   Value Arr = Array();
   for (const EnumValueInfo& M: I.Members) {
     Value EnumValue = Object();
@@ -256,15 +289,73 @@ Value extractValue(const EnumInfo &I, const ClangDocContext &CDCtx) {
     Obj.insert({"EnumComments", ArrDesc});
   }
   
-  if (I.DefLoc) {
-    if (!CDCtx.RepositoryUrl)
-      Obj.insert({"Location", extractValue(*I.DefLoc)});
+  if (I.DefLoc.has_value()) {
+    Location L = *I.DefLoc;
+    if (CDCtx.RepositoryUrl.has_value())
+      Obj.insert({"Location", extractValue(L,
+                                           StringRef{*CDCtx.RepositoryUrl})});
     else
-      Obj.insert({"Location", extractValue(*I.DefLoc, 
-                                           StringRef{*CDCtx.RepositoryUrl})});  
+      Obj.insert({"Location", extractValue(L)});  
   }
   
   return Obj;
+}
+
+void extractScopeChildren(const ScopeChildren &S, Object &Obj, 
+                          StringRef ParentInfoDir,
+                          const ClangDocContext &CDCtx) {
+  Value ArrNamespace = Array();
+  for (const Reference& Child : S.Namespaces)
+    ArrNamespace.getAsArray()->emplace_back(extractValue(Child, ParentInfoDir));
+  
+  if (!ArrNamespace.getAsArray()->empty())
+    Obj.insert({"Namespace", Object{{"Links", ArrNamespace}}});
+  
+  Value ArrRecord = Array();
+  for (const Reference& Child : S.Records)
+    ArrRecord.getAsArray()->emplace_back(extractValue(Child, ParentInfoDir));
+  
+  if (!ArrRecord.getAsArray()->empty())
+    Obj.insert({"Record", Object{{"Links", ArrRecord}}});
+  
+  Value ArrFunction = Array();
+  Value PublicFunction = Array();
+  Value ProtectedFunction = Array();
+  Value PrivateFunction = Array();
+  
+  for (const FunctionInfo& Child : S.Functions) {
+    Value F = extractValue(Child, ParentInfoDir, CDCtx);
+    AccessSpecifier Access = Child.Access;
+    if (Access == AccessSpecifier::AS_public)
+      PublicFunction.getAsArray()->emplace_back(F);
+    else if (Access == AccessSpecifier::AS_protected)
+      ProtectedFunction.getAsArray()->emplace_back(F);
+    else
+      ArrFunction.getAsArray()->emplace_back(F);
+  }  
+  if (!ArrFunction.getAsArray()->empty())
+    Obj.insert({"Function", Object{{"Obj", ArrFunction}}});
+  
+  if (!PublicFunction.getAsArray()->empty())
+    Obj.insert({"PublicFunction", Object{{"Obj", PublicFunction}}});
+  
+  if (!ProtectedFunction.getAsArray()->empty())
+    Obj.insert({"ProtectedFunction", Object{{"Obj", ProtectedFunction}}});
+  
+  
+  Value ArrEnum = Array();
+  for (const EnumInfo& Child : S.Enums)
+    ArrEnum.getAsArray()->emplace_back(extractValue(Child, CDCtx));
+  
+  if (!ArrEnum.getAsArray()->empty())
+    Obj.insert({"Enums", Object{{"Obj", ArrEnum }}});
+  
+  Value ArrTypedefs = Array();
+  for (const TypedefInfo& Child : S.Typedefs) 
+    ArrTypedefs.getAsArray()->emplace_back(extractValue(Child));
+  
+  if (!ArrTypedefs.getAsArray()->empty())
+    Obj.insert({"Typedefs", Object{{"Obj", ArrTypedefs }}});
 }
 
 Value extractValue(const NamespaceInfo &I, const ClangDocContext &CDCtx) {
@@ -285,46 +376,88 @@ Value extractValue(const NamespaceInfo &I, const ClangDocContext &CDCtx) {
       ArrDesc.getAsArray()->emplace_back(extractValue(Child));
     NamespaceValue.insert({"NamespaceComments", ArrDesc });
   }
-
-  Value ArrNamespace = Array();
-  for (const Reference& Child : I.Children.Namespaces)
-    ArrNamespace.getAsArray()->emplace_back(extractValue(Child, BasePath));
-  
-  if (!ArrNamespace.getAsArray()->empty())
-    NamespaceValue.insert({"Namespace", Object{{"Links", ArrNamespace}}});
-  
-  Value ArrRecord = Array();
-  for (const Reference& Child : I.Children.Records)
-    ArrRecord.getAsArray()->emplace_back(extractValue(Child, BasePath));
-  
-  if (!ArrRecord.getAsArray()->empty())
-    NamespaceValue.insert({"Record", Object{{"Links", ArrRecord}}});
-  
-  Value ArrFunction = Array();
-  for (const FunctionInfo& Child : I.Children.Functions)
-    ArrFunction.getAsArray()->emplace_back(extractValue(Child, BasePath,
-                                                        CDCtx));
-  if (!ArrFunction.getAsArray()->empty())
-    NamespaceValue.insert({"Function", Object{{"Obj", ArrFunction}}});
-  
-  Value ArrEnum = Array();
-  for (const EnumInfo& Child : I.Children.Enums)
-    ArrEnum.getAsArray()->emplace_back(extractValue(Child, CDCtx));
-  
-  if (!ArrEnum.getAsArray()->empty())
-    NamespaceValue.insert({"Enums", Object{{"Obj", ArrEnum }}});
-  
-  Value ArrTypedefs = Array();
-  for (const TypedefInfo& Child : I.Children.Typedefs) 
-    ArrTypedefs.getAsArray()->emplace_back(extractValue(Child));
-  
-  if (!ArrTypedefs.getAsArray()->empty())
-    NamespaceValue.insert({"Typedefs", Object{{"Obj", ArrTypedefs }}});
-  
+  extractScopeChildren(I.Children, NamespaceValue, BasePath, CDCtx);
   return NamespaceValue;
 }
 
+Value extractValue(const RecordInfo &I, const ClangDocContext &CDCtx) {
+  Object RecordValue = Object();
+  
+  if (!I.Description.empty()) {
+    Value ArrDesc = Array();
+    for (const CommentInfo& Child : I.Description) 
+      ArrDesc.getAsArray()->emplace_back(extractValue(Child));
+    RecordValue.insert({"RecordComments", ArrDesc });
+  }
+  RecordValue.insert({"Name", I.Name});
+  RecordValue.insert({"FullName", I.FullName});
+  RecordValue.insert({"RecordType", getTagType(I.TagType)});
+  
+  if (I.DefLoc.has_value()) {
+    Location L = *I.DefLoc;
+    if (CDCtx.RepositoryUrl.has_value())
+      RecordValue.insert({"Location", extractValue(L,
+                                           StringRef{*CDCtx.RepositoryUrl})});
+    else
+      RecordValue.insert({"Location", extractValue(L)});  
+  }
+  
+  StringRef BasePath = I.getRelativeFilePath("");
+  extractScopeChildren(I.Children, RecordValue, BasePath, CDCtx);
+  Value PublicMembers = Array();
+  Value ProtectedMembers = Array();
+  Value PrivateMembers = Array();
+  for (const MemberTypeInfo &Member : I.Members ) {
+    Value MemberValue = Object();
+    MemberValue.getAsObject()->insert({"Name", Member.Name});
+    MemberValue.getAsObject()->insert({"Type", Member.Type.Name});
+    if (!Member.Description.empty()) {
+      Value ArrDesc = Array();
+      for (const CommentInfo& Child : Member.Description) 
+            ArrDesc.getAsArray()->emplace_back(extractValue(Child));
+      MemberValue.getAsObject()->insert({"MemberComments", ArrDesc });
+    }
+    
+    if (Member.Access == AccessSpecifier::AS_public)
+      PublicMembers.getAsArray()->emplace_back(MemberValue);
+    else if (Member.Access == AccessSpecifier::AS_protected)
+      ProtectedMembers.getAsArray()->emplace_back(MemberValue);
+    else if (Member.Access == AccessSpecifier::AS_private)
+      PrivateMembers.getAsArray()->emplace_back(MemberValue);
+  }
+  if (!PublicMembers.getAsArray()->empty())
+    RecordValue.insert({"PublicMembers", Object{{"Obj", PublicMembers}}});
+  if (!ProtectedMembers.getAsArray()->empty())
+    RecordValue.insert({"ProtectedMembers", Object{{"Obj", ProtectedMembers}}});
+  if (!PrivateMembers.getAsArray()->empty())
+    RecordValue.insert({"PrivateMembers", Object{{"Obj", PrivateMembers}}});
+  
+  return RecordValue;
+}
 
+void setupTemplateValue(const ClangDocContext &CDCtx, Value &V, Info *I) {
+  V.getAsObject()->insert({"ProjectName", CDCtx.ProjectName});
+  Value StylesheetArr = Array();
+  auto InfoPath = I->getRelativeFilePath("");
+  SmallString<128> RelativePath = computeRelativePath("", InfoPath);
+  for (const auto &FilePath  : CDCtx.UserStylesheets) {
+    SmallString<128> StylesheetPath = RelativePath;
+    llvm::sys::path::append(StylesheetPath,
+                            llvm::sys::path::filename(FilePath));
+    llvm::sys::path::native(StylesheetPath, llvm::sys::path::Style::posix);
+    StylesheetArr.getAsArray()->emplace_back(StylesheetPath);
+  }
+  V.getAsObject()->insert({"Stylesheets", StylesheetArr});
+  
+  Value ScriptArr = Array();
+  for (auto Script : CDCtx.JsScripts) {
+    SmallString<128> JsPath = RelativePath;
+    llvm::sys::path::append(JsPath, llvm::sys::path::filename(Script));
+    ScriptArr.getAsArray()->emplace_back(JsPath);
+  }
+  V.getAsObject()->insert({"Scripts", ScriptArr});
+}
+ 
 
 llvm::Error
 MustacheHTMLGenerator::generateDocForInfo(Info *I, llvm::raw_ostream &OS,
@@ -332,13 +465,18 @@ MustacheHTMLGenerator::generateDocForInfo(Info *I, llvm::raw_ostream &OS,
   switch (I->IT) {
   case InfoType::IT_namespace: {
     Value V = extractValue(*static_cast<clang::doc::NamespaceInfo *>(I), CDCtx);
-    llvm::raw_ostream &OS = llvm::outs();
-    llvm::json::OStream J(OS, /*IndentSize=*/2);
-    J.value(V);
+    setupTemplateValue(CDCtx, V, I);
+    OS << NamespaceTemplate->render(V);
     break;
   }
-  case InfoType::IT_record:
+  case InfoType::IT_record: {
+    Value V = extractValue(*static_cast<clang::doc::RecordInfo *>(I), CDCtx);
+    setupTemplateValue(CDCtx, V, I);
+    // Serialize the JSON value to the output stream in a readable format.
+    llvm::outs() << llvm::formatv("{0:2}", V);
+    OS << RecordTemplate->render(V);
     break;
+  }  
   case InfoType::IT_enum:
     llvm::outs() << "IT_enum\n";
     break;
@@ -356,6 +494,17 @@ MustacheHTMLGenerator::generateDocForInfo(Info *I, llvm::raw_ostream &OS,
 }
 
 llvm::Error MustacheHTMLGenerator::createResources(ClangDocContext &CDCtx) {
+  llvm::Error Err = llvm::Error::success();
+  for (const auto &FilePath : CDCtx.UserStylesheets) {
+    Err = copyFile(FilePath, CDCtx.OutDirectory);
+    if (Err)
+      return Err;
+  }
+  for (const auto &FilePath : CDCtx.JsScripts) {
+    Err = copyFile(FilePath, CDCtx.OutDirectory);
+    if (Err)
+      return Err;
+  }
   return llvm::Error::success();
 }
 
