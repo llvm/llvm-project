4515c64a8b7978bc9c26ddb9d697490b5a472b65
Krystian Stasiowski <sdkrystian@gmail.com>
2024-10-07 11:45:27 -0400

    [FOLD] add tests for partial specializations

--- clang/test/CXX/temp/temp.spec/temp.expl.spec/p7.cpp
+++ clang/test/CXX/temp/temp.spec/temp.expl.spec/p7.cpp
@@ -10,7 +10,13 @@ namespace Undefined {
     static const int x; // expected-note {{declared here}}
 
     template<typename U>
+    static const int x<U*>; // expected-note {{declared here}}
+
+    template<typename U>
     struct B; // expected-note {{template is declared here}}
+
+    template<typename U>
+    struct B<U*>; // expected-note {{template is declared here}}
   };
 
   template<>
@@ -22,6 +28,16 @@ namespace Undefined {
   template<>
   template<typename U>
   constexpr int A<short>::x = A<long>::x<U>;
+
+  template<>
+  template<typename U>
+  constexpr int A<short>::x<U*> = A<long>::x<U*>;
+
+  template<>
+  template<typename U>
+  struct A<short>::B<U*> {
+    static constexpr int y = A<long>::B<U*>::y;
+  };
 
   template<>
   template<typename U>
@@ -41,19 +57,34 @@ namespace Undefined {
 
   template<>
   template<typename U>
+  constexpr int A<long>::x<U*> = 2;
+
+  template<>
+  template<typename U>
   struct A<long>::B {
     static constexpr int y = 1;
+  };
+
+  template<>
+  template<typename U>
+  struct A<long>::B<U*> {
+    static constexpr int y = 2;
   };
 
   static_assert(A<int>::f<int>() == 0); // expected-error {{static assertion expression is not an integral constant expression}}
                                         // expected-note@-1 {{undefined function 'f<int>' cannot be used in a constant expression}}
   static_assert(A<int>::x<int> == 0); // expected-error {{static assertion expression is not an integral constant expression}}
                                       // expected-note@-1 {{initializer of 'x<int>' is unknown}}
+  static_assert(A<int>::x<int*> == 0); // expected-error {{static assertion expression is not an integral constant expression}}
+                                       // expected-note@-1 {{initializer of 'x<int *>' is unknown}}
   static_assert(A<int>::B<int>::y == 0); // expected-error {{implicit instantiation of undefined template 'Undefined::A<int>::B<int>'}}
+  static_assert(A<int>::B<int*>::y == 0); // expected-error {{implicit instantiation of undefined template 'Undefined::A<int>::B<int *>'}}
 
   static_assert(A<short>::f<int>() == 1);
   static_assert(A<short>::x<int> == 1);
+  static_assert(A<short>::x<int*> == 2);
   static_assert(A<short>::B<int>::y == 1);
+  static_assert(A<short>::B<int*>::y == 2);
 } // namespace Undefined
 
 namespace Defined {
@@ -62,13 +93,21 @@ namespace Defined {
     template<typename U>
     static constexpr int f() {
       return 0;
-    }
+    };
 
     template<typename U>
-    static constexpr int x = 0;
+    static const int x = 0;
+
+    template<typename U>
+    static const int x<U*> = 0;
 
     template<typename U>
     struct B {
+      static constexpr int y = 0;
+    };
+
+    template<typename U>
+    struct B<U*> {
       static constexpr int y = 0;
     };
   };
@@ -85,6 +124,16 @@ namespace Defined {
 
   template<>
   template<typename U>
+  constexpr int A<short>::x<U*> = A<long>::x<U*>;
+
+  template<>
+  template<typename U>
+  struct A<short>::B<U*> {
+    static constexpr int y = A<long>::B<U*>::y;
+  };
+
+  template<>
+  template<typename U>
   struct A<short>::B {
     static constexpr int y = A<long>::B<U>::y;
   };
@@ -101,15 +150,29 @@ namespace Defined {
 
   template<>
   template<typename U>
+  constexpr int A<long>::x<U*> = 2;
+
+  template<>
+  template<typename U>
   struct A<long>::B {
     static constexpr int y = 1;
   };
 
+  template<>
+  template<typename U>
+  struct A<long>::B<U*> {
+    static constexpr int y = 2;
+  };
+
   static_assert(A<int>::f<int>() == 0);
   static_assert(A<int>::x<int> == 0);
+  static_assert(A<int>::x<int*> == 0);
   static_assert(A<int>::B<int>::y == 0);
+  static_assert(A<int>::B<int*>::y == 0);
 
   static_assert(A<short>::f<int>() == 1);
   static_assert(A<short>::x<int> == 1);
+  static_assert(A<short>::x<int*> == 2);
   static_assert(A<short>::B<int>::y == 1);
+  static_assert(A<short>::B<int*>::y == 2);
 } // namespace Defined
