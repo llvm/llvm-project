18e537adc6d78d660c8e6bd67f342892ae1f9892
Charlie Barto <barto.charlie@gmail.com>
2024-10-08 14:00:47 -0700

    fixup! add a comment clarifying arch support in ill.cpp

--- compiler-rt/test/asan/TestCases/ill.cpp
+++ compiler-rt/test/asan/TestCases/ill.cpp
@@ -9,7 +9,7 @@
 #endif
 
 // note: test is limited to i386 only ("asan-32-bits") when using "real" MSVC
-//  see the  REQUIRES: clause above
+//  see the requires clause above
 #if defined(_MSC_VER) && !defined(__clang__)
 #  define __builtin_trap() __asm ud2;
 #endif
