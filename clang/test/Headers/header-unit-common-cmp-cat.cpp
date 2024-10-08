6cb15c6a9a2ec5fccfef354922869bcf8f67f6fc
Dmitry Polukhin <dmitry.polukhin@gmail.com>
2024-10-08 05:13:43 -0700

    Make test closer to real issue in gcc libstc++

--- clang/test/Headers/header-unit-common-cmp-cat.cpp
+++ clang/test/Headers/header-unit-common-cmp-cat.cpp
@@ -1,11 +1,14 @@
 // RUN: rm -fR %t
 // RUN: split-file %s %t
 // RUN: cd %t
-// RUN: %clang_cc1 -verify -std=c++20 -fskip-odr-check-in-gmf -emit-header-unit -xc++-user-header bz0.h
-// RUN: %clang_cc1 -verify -std=c++20 -fskip-odr-check-in-gmf -emit-header-unit -xc++-user-header bz1.h
-// RUN: %clang_cc1 -verify -std=c++20 -fskip-odr-check-in-gmf -emit-header-unit -xc++-user-header -fmodule-file=bz0.pcm -fmodule-file=bz1.pcm bz.cpp
+// RUN: %clang_cc1 -verify -std=c++20 -fskip-odr-check-in-gmf -I. -emit-header-unit -xc++-user-header bz1.h
+// RUN: %clang_cc1 -verify -std=c++20 -fskip-odr-check-in-gmf -I. -emit-header-unit -xc++-user-header bz2.h
+// RUN: %clang_cc1 -verify -std=c++20 -fskip-odr-check-in-gmf -I. -emit-header-unit -xc++-user-header -fmodule-file=bz1.pcm -fmodule-file=bz2.pcm bz.cpp
 
 //--- compare
+namespace std {
+namespace __detail {
+
 template<typename _Tp>
 inline constexpr unsigned __cmp_cat_id = 1;
 
@@ -14,19 +17,24 @@ constexpr auto __common_cmp_cat() {
   (__cmp_cat_id<_Ts> | ...);
 }
 
+} // namespace __detail
+} // namespace std
+
 //--- bz0.h
 template <class T>
 int operator|(T, T);
 
-#include "compare"
+//--- bz1.h
+#include "bz0.h"
+#include <compare>
 // expected-no-diagnostics
 
-//--- bz1.h
-#include "compare"
+//--- bz2.h
+#include <compare>
 // expected-no-diagnostics
 
 //--- bz.cpp
-#include "compare"
+#include <compare>
 
-import "bz0.h"; // expected-warning {{the implementation of header units is in an experimental phase}}
 import "bz1.h"; // expected-warning {{the implementation of header units is in an experimental phase}}
+import "bz2.h"; // expected-warning {{the implementation of header units is in an experimental phase}}
