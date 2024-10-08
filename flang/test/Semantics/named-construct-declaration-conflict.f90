347934248036662e78bba6b29fb3631b9f13ddf9
Krzysztof Parzyszek <Krzysztof.Parzyszek@amd.com>
2024-10-08 11:53:50 -0500

    rename

--- flang/test/Semantics/named-construct-declaration-conflict.f90
+++ flang/test/Semantics/named-construct-declaration-conflict.f90
@@ -1,10 +1,10 @@
 !RUN: %python %S/test_errors.py %s %flang_fc1
 
 subroutine foo()
-  integer :: nxloop
-!ERROR: 'nxloop' is already declared in this scoping unit
-  nxloop: do i = 1, 100
-  enddo nxloop
+  integer :: xyz
+!ERROR: 'xyz' is already declared in this scoping unit
+  xyz: do i = 1, 100
+  enddo xyz
 end subroutine
 
 
