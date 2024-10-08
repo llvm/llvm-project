38f0015478196b4f6e8dee6dce32ef147d5e049e
Зишан Мирза <zmirza@tutanota.de>
2024-10-08 15:03:49 +0200

    timezone implementation

---
+++ libc/src/time/timezone.h
@@ -0,0 +1,33 @@
+//===-- Implementation of timezone functions ------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_LIBC_SRC_TIME_TIMEZONE_H
+#define LLVM_LIBC_SRC_TIME_TIMEZONE_H
+
+#include <stddef.h> // For size_t.
+
+#include "src/__support/CPP/limits.h"
+#include "src/__support/common.h"
+#include "src/__support/macros/config.h"
+#include "src/__support/CPP/string_view.h"
+#include "src/errno/libc_errno.h"
+#include "src/time/mktime.h"
+
+#include <stdint.h>
+
+namespace LIBC_NAMESPACE_DECL {
+namespace timezone {
+
+#define TZ_HEADER "TZif"
+
+extern int get_timezone_offset(char *timezone);
+
+} // namespace timezone
+} // namespace LIBC_NAMESPACE_DECL
+
+#endif // LLVM_LIBC_SRC_TIME_TIMEZONE_H
--- libc/test/src/time/localtime_test.cpp
+++ libc/test/src/time/localtime_test.cpp
@@ -6,12 +6,29 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "src/errno/libc_errno.h"
 #include "src/time/localtime.h"
 #include "test/UnitTest/Test.h"
-#include "test/src/time/TmHelper.h"
+
+#include <string.h>
+
+extern char **environ;
+
+void set_env_var(const char* env) {
+  int i = 0;
+  if (environ[i] != NULL) {
+    i++;
+  }
+
+  environ[i] = (char*)malloc(strlen(env)+1);
+  if (environ[i] != nullptr) {
+    memcpy(environ[i], env, strlen(env)+1);
+    environ[i+1] = nullptr;
+  }
+}
 
 TEST(LlvmLibcLocaltime, ValidUnixTimestamp0) {
+  set_env_var("TZ=Europe/Berlin");
+
   const time_t t_ptr = 0;
   struct tm *result = LIBC_NAMESPACE::localtime(&t_ptr);
   ASSERT_EQ(70, result->tm_year);
@@ -26,6 +43,8 @@ TEST(LlvmLibcLocaltime, ValidUnixTimestamp0) {
 }
 
 TEST(LlvmLibcLocaltime, ValidUnixTimestamp32Int) {
+  set_env_var("TZ=Europe/Berlin");
+
   time_t t_ptr = 2147483647;
   struct tm *result = LIBC_NAMESPACE::localtime(&t_ptr);
   ASSERT_EQ(138, result->tm_year);
@@ -40,12 +59,110 @@ TEST(LlvmLibcLocaltime, ValidUnixTimestamp32Int) {
 }
 
 TEST(LlvmLibcLocaltime, ValidUnixTimestamp32IntDst) {
+  set_env_var("TZ=Europe/Berlin");
+
   time_t t_ptr = 1627225465;
   struct tm *result = LIBC_NAMESPACE::localtime(&t_ptr);
   ASSERT_EQ(121, result->tm_year);
   ASSERT_EQ(6, result->tm_mon);
   ASSERT_EQ(25, result->tm_mday);
   ASSERT_EQ(17, result->tm_hour);
+  ASSERT_EQ(4, result->tm_min);
+  ASSERT_EQ(25, result->tm_sec);
+  ASSERT_EQ(0, result->tm_wday);
+  ASSERT_EQ(205, result->tm_yday);
+  ASSERT_EQ(1, result->tm_isdst);
+}
+
+TEST(LlvmLibcLocaltime, ValidUnixTimestampTzEnvironmentVariableUsaPst) {
+  set_env_var("TZ=America/San_Francisco");
+
+  time_t t_ptr = 1627225465;
+  struct tm *result = LIBC_NAMESPACE::localtime(&t_ptr);
+  ASSERT_EQ(121, result->tm_year);
+  ASSERT_EQ(6, result->tm_mon);
+  ASSERT_EQ(25, result->tm_mday);
+  ASSERT_EQ(8, result->tm_hour);
+  ASSERT_EQ(4, result->tm_min);
+  ASSERT_EQ(25, result->tm_sec);
+  ASSERT_EQ(0, result->tm_wday);
+  ASSERT_EQ(205, result->tm_yday);
+  ASSERT_EQ(1, result->tm_isdst);
+}
+
+TEST(LlvmLibcLocaltime, ValidUnixTimestampTzEnvironmentVariableUsaEst) {
+  set_env_var("TZ=America/New_York");
+
+  time_t t_ptr = 1627225465;
+  struct tm *result = LIBC_NAMESPACE::localtime(&t_ptr);
+  ASSERT_EQ(121, result->tm_year);
+  ASSERT_EQ(6, result->tm_mon);
+  ASSERT_EQ(25, result->tm_mday);
+  ASSERT_EQ(11, result->tm_hour);
+  ASSERT_EQ(4, result->tm_min);
+  ASSERT_EQ(25, result->tm_sec);
+  ASSERT_EQ(0, result->tm_wday);
+  ASSERT_EQ(205, result->tm_yday);
+  ASSERT_EQ(1, result->tm_isdst);
+}
+
+TEST(LlvmLibcLocaltime, ValidUnixTimestampTzEnvironmentVariableUTC) {
+  set_env_var("TZ=UTC");
+
+  time_t t_ptr = 1627225465;
+  struct tm *result = LIBC_NAMESPACE::localtime(&t_ptr);
+  ASSERT_EQ(121, result->tm_year);
+  ASSERT_EQ(6, result->tm_mon);
+  ASSERT_EQ(25, result->tm_mday);
+  ASSERT_EQ(15, result->tm_hour);
+  ASSERT_EQ(4, result->tm_min);
+  ASSERT_EQ(25, result->tm_sec);
+  ASSERT_EQ(0, result->tm_wday);
+  ASSERT_EQ(205, result->tm_yday);
+  ASSERT_EQ(1, result->tm_isdst);
+}
+
+TEST(LlvmLibcLocaltime, ValidUnixTimestampTzEnvironmentVariableGMT) {
+  set_env_var("TZ=GMT");
+
+  time_t t_ptr = 1627225465;
+  struct tm *result = LIBC_NAMESPACE::localtime(&t_ptr);
+  ASSERT_EQ(121, result->tm_year);
+  ASSERT_EQ(6, result->tm_mon);
+  ASSERT_EQ(25, result->tm_mday);
+  ASSERT_EQ(15, result->tm_hour);
+  ASSERT_EQ(4, result->tm_min);
+  ASSERT_EQ(25, result->tm_sec);
+  ASSERT_EQ(0, result->tm_wday);
+  ASSERT_EQ(205, result->tm_yday);
+  ASSERT_EQ(1, result->tm_isdst);
+}
+
+TEST(LlvmLibcLocaltime, ValidUnixTimestampTzEnvironmentVariableEuropeBerlin) {
+  set_env_var("TZ=Europe/Berlin");
+
+  time_t t_ptr = 1627225465;
+  struct tm *result = LIBC_NAMESPACE::localtime(&t_ptr);
+  ASSERT_EQ(121, result->tm_year);
+  ASSERT_EQ(6, result->tm_mon);
+  ASSERT_EQ(25, result->tm_mday);
+  ASSERT_EQ(17, result->tm_hour);
+  ASSERT_EQ(4, result->tm_min);
+  ASSERT_EQ(25, result->tm_sec);
+  ASSERT_EQ(0, result->tm_wday);
+  ASSERT_EQ(205, result->tm_yday);
+  ASSERT_EQ(1, result->tm_isdst);
+}
+
+TEST(LlvmLibcLocaltime, ValidUnixTimestampTzEnvironmentVariableRussiaMoscow) {
+  set_env_var("TZ=Europe/Moscow");
+
+  time_t t_ptr = 1627225465;
+  struct tm *result = LIBC_NAMESPACE::localtime(&t_ptr);
+  ASSERT_EQ(121, result->tm_year);
+  ASSERT_EQ(6, result->tm_mon);
+  ASSERT_EQ(25, result->tm_mday);
+  ASSERT_EQ(18, result->tm_hour);
   ASSERT_EQ(4, result->tm_min);
   ASSERT_EQ(25, result->tm_sec);
   ASSERT_EQ(0, result->tm_wday);
