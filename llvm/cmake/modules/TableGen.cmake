llvm/llvm-project

commit 95d4506dda79d49e55fdd0e4da7bf81487167aa1

add_tablegen: Quick fix to reflect LLVM_TABLEGEN to llvm-min-tblgen
`sanitizer-x86_64-linux-android` uses LLVM_TABLEGEN.

llvm/cmake/modules/TableGen.cmake
@@ -154,6 +154,13 @@ macro(add_tablegen target project)
     endif()
   endif()
 
+  # FIXME: Quick fix to reflect LLVM_TABLEGEN to llvm-min-tblgen
+  if("${target}" STREQUAL "llvm-min-tblgen"
+      AND NOT "${LLVM_TABLEGEN}" STREQUAL ""
+      AND NOT "${LLVM_TABLEGEN}" STREQUAL "llvm-tblgen")
+    set(${project}_TABLEGEN_DEFAULT "${LLVM_TABLEGEN}")
+  endif()
+
   if(ADD_TABLEGEN_EXPORT)
     set(${project}_TABLEGEN "${${project}_TABLEGEN_DEFAULT}" CACHE
       STRING "Native TableGen executable. Saves building one when cross-compiling.")

commit 9666fa74570fb7a7ea8eeeccf2fc95f518b1e04b

[CMake][Windows] Fix Debug config build when using MSBuild
Commit 95d4506dda caused a failure in a debug-build with MSBuild.  That
commit conditionally makes an assignment to a cmake variable, but
suppresses the assignment when LLVM_TABLEGEN is precicely "llvm-tblgen".
This commit updates that change, so that the assignment is suppressed
when LLVM_TABLEGEN logically is "llvm-tblgen" (i.e., it also suppresses
it for "llvm-tblgen.exe", or for a full pathname that ends with
llvm-tblgen" or "llvm-tblgen.exe").

llvm/cmake/modules/TableGen.cmake
@@ -191,9 +191,13 @@ macro(add_tablegen target project)
 
   # FIXME: Quick fix to reflect LLVM_TABLEGEN to llvm-min-tblgen
   if("${target}" STREQUAL "llvm-min-tblgen"
-      AND NOT "${LLVM_TABLEGEN}" STREQUAL ""
-      AND NOT "${LLVM_TABLEGEN}" STREQUAL "llvm-tblgen")
-    set(${project}_TABLEGEN_DEFAULT "${LLVM_TABLEGEN}")
+      AND NOT "${LLVM_TABLEGEN}" STREQUAL "")
+    # Extract base filename from full path.
+    get_filename_component(RAW_LLVM_TABLEGEN ${LLVM_TABLEGEN} NAME_WE)
+    if(NOT "${RAW_LLVM_TABLEGEN}" STREQUAL ""
+        AND NOT "${RAW_LLVM_TABLEGEN}" STREQUAL "llvm-tblgen")
+      set(${project}_TABLEGEN_DEFAULT "${LLVM_TABLEGEN}")
+    endif()
   endif()
 
   if(ADD_TABLEGEN_EXPORT)