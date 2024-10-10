8442dd9e7ff95d11efbf75b6b0d2c65a0c3a03c0
Stephen Tozer <stephen.tozer@sony.com>
2024-10-10 14:16:08 +0100

    Use os.path.join for getting __init__.py

--- cross-project-tests/debuginfo-tests/dexter/dex/utils/Imports.py
+++ cross-project-tests/debuginfo-tests/dexter/dex/utils/Imports.py
@@ -1,9 +1,12 @@
 import importlib
+import os
 import sys
 
 
 def load_module(name, path):
-    spec = importlib.util.spec_from_file_location(name, f"{path}/{name}/__init__.py")
+    spec = importlib.util.spec_from_file_location(
+        name, os.path.join(path, name, "__init__.py")
+    )
     module = importlib.util.module_from_spec(spec)
     sys.modules[name] = module
     spec.loader.exec_module(module)
