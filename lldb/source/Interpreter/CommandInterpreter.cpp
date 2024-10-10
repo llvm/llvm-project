4f297566b3150097de26c6a23a987d2bd5fc19c5
Robert O'Callahan <robert@ocallahan.org>
2024-10-11 09:01:47 +1300

    [lldb] Implement basic support for reverse-continue (#99736)
    This commit only adds support for the
    `SBProcess::ReverseContinue()` API. A user-accessible command for this
    will follow in a later commit.
    
    This feature depends on a gdbserver implementation (e.g. `rr`) providing
    support for the `bc` and `bs` packets. `lldb-server` does not support
    those packets, and there is no plan to change that. So, for testing
    purposes, `lldbreverse.py` wraps `lldb-server` with a Python
    implementation of *very limited* record-and-replay functionality for use
    by *tests only*.
    
    The majority of this PR is test infrastructure (about 700 of the 950
    lines added).

--- lldb/source/Interpreter/CommandInterpreter.cpp
+++ lldb/source/Interpreter/CommandInterpreter.cpp
@@ -2553,7 +2553,8 @@ bool CommandInterpreter::DidProcessStopAbnormally() const {
     const StopReason reason = stop_info->GetStopReason();
     if (reason == eStopReasonException ||
         reason == eStopReasonInstrumentation ||
-        reason == eStopReasonProcessorTrace || reason == eStopReasonInterrupt)
+        reason == eStopReasonProcessorTrace || reason == eStopReasonInterrupt ||
+        reason == eStopReasonHistoryBoundary)
       return true;
 
     if (reason == eStopReasonSignal) {
