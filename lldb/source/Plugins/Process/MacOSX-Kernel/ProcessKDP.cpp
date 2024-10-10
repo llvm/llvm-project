3bef742559f1556569423ec63c70b97dff1d426e
Jason Molenda <jmolenda@apple.com>
2024-10-10 16:22:24 -0700

    Revert "[lldb] Implement basic support for reverse-continue (#99736)"
    Reverting this again; I added a commit which added @skipIfDarwin
    markers to the TestReverseContinueBreakpoints.py and
    TestReverseContinueNotSupported.py API tests, which use lldb-server
    in gdbserver mode which does not work on Darwin.  But the aarch64 ubuntu
    bot reported a failure on TestReverseContinueBreakpoints.py,
    https://lab.llvm.org/buildbot/#/builders/59/builds/6397
    
      File "/home/tcwg-buildbot/worker/lldb-aarch64-ubuntu/llvm-project/lldb/test/API/functionalities/reverse-execution/TestReverseContinueBreakpoints.py", line 63, in test_reverse_continue_skip_breakpoint
        self.reverse_continue_skip_breakpoint_internal(async_mode=False)
      File "/home/tcwg-buildbot/worker/lldb-aarch64-ubuntu/llvm-project/lldb/test/API/functionalities/reverse-execution/TestReverseContinueBreakpoints.py", line 81, in reverse_continue_skip_breakpoint_internal
        self.expect(
      File "/home/tcwg-buildbot/worker/lldb-aarch64-ubuntu/llvm-project/lldb/packages/Python/lldbsuite/test/lldbtest.py", line 2372, in expect
        self.runCmd(
      File "/home/tcwg-buildbot/worker/lldb-aarch64-ubuntu/llvm-project/lldb/packages/Python/lldbsuite/test/lldbtest.py", line 1002, in runCmd
        self.assertTrue(self.res.Succeeded(), msg + output)
    AssertionError: False is not true : Process should be stopped due to history boundary
    Error output:
    error: Process must be launched.
    
    This reverts commit 4f297566b3150097de26c6a23a987d2bd5fc19c5.

--- lldb/source/Plugins/Process/MacOSX-Kernel/ProcessKDP.cpp
+++ lldb/source/Plugins/Process/MacOSX-Kernel/ProcessKDP.cpp
@@ -402,16 +402,9 @@ lldb_private::DynamicLoader *ProcessKDP::GetDynamicLoader() {
 
 Status ProcessKDP::WillResume() { return Status(); }
 
-Status ProcessKDP::DoResume(RunDirection direction) {
+Status ProcessKDP::DoResume() {
   Status error;
   Log *log = GetLog(KDPLog::Process);
-
-  if (direction == RunDirection::eRunReverse) {
-    error.SetErrorStringWithFormatv(
-        "error: {0} does not support reverse execution of processes", GetPluginName());
-    return error;
-  }
-
   // Only start the async thread if we try to do any process control
   if (!m_async_thread.IsJoinable())
     StartAsyncThread();
