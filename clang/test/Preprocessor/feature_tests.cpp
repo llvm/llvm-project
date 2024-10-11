9c8d2378a16359463b65012febbbab387c8c16af
YunQiang Su <yunqiang@isrc.iscas.ac.cn>
2024-10-11 21:39:18 +0800

    fix typo in clang/test/Preprocessor/feature_tests.cpp

--- clang/test/Preprocessor/feature_tests.cpp
+++ clang/test/Preprocessor/feature_tests.cpp
@@ -60,7 +60,7 @@
 #if  !__has_constexpr_builtin(__builtin_fmax) || \
      !__has_constexpr_builtin(__builtin_fmin) || \
      !__has_constexpr_builtin(__builtin_fmaximum_num) || \
-     !__has_constexpr_builtin(__builtin_fmaximum_num)
+     !__has_constexpr_builtin(__builtin_fminimum_num)
 #error Clang should have these constexpr builtins
 #endif
 
