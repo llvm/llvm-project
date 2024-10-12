d87c968c703636dfe8f0ddb8d7f49df6e35ad7e6
Hui <hui.xie0621@gmail.com>
2024-10-12 13:49:37 +0100

    address comment

--- libcxx/include/__type_traits/container_traits.h
+++ libcxx/include/__type_traits/container_traits.h
@@ -18,11 +18,18 @@
 
 _LIBCPP_BEGIN_NAMESPACE_STD
 
-// __container_traits is a general purpose struct contains traits of containers' different operations.
+// // __container_traits is a general purpose utility containing traits describing various containers operations.
 // It currently only has one trait: `__emplacement_has_strong_exception_safety_guarantee`, but it's
 // intended to be extended in the future.
-// If a container does not support an operation. For example, `std::array` does not support `insert`
-// or `emplace`, the trait of that operation will return false.
+//
+// These traits should only be used for optimization or QoI purposes. In particular, since this is a libc++ internal
+// mechanism, no user-defined containers should be expected to specialize these traits (in fact it would be illegal for
+// them to do so). Hence, when using these traits to implement something, make sure that a container that fails to
+// specialize these traits does not result in non-conforming code.
+//
+// When a trait is nonsensical for a type, this class still provides a fallback value for that trait.
+// For example, `std::array` does not support `insert` or `emplace`, so
+// `__emplacement_has_strong_exception_safety_guarantee` is false for such types.
 template <class _Container>
 struct __container_traits {
   // A trait that tells whether a single element insertion/emplacement via member function
