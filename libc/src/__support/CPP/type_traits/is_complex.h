729aea1549bb8f1f781893e2a8b032ff807ae43b
Sh0g0-1758 <shouryagoel10000@gmail.com>
2024-10-12 14:28:24 +0530

    nit

--- libc/src/__support/CPP/type_traits/is_complex.h
+++ libc/src/__support/CPP/type_traits/is_complex.h
@@ -1,4 +1,4 @@
-//===-- is_complex type_traits ---------------------------*- C++ -*-===//
+//===-- is_complex type_traits ----------------------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
