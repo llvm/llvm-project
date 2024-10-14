ffcbd23dbcb02ccccc0f5d0ffc9baf9eb2a58cde
wldfngrs <wldfngrs@gmail.com>
2024-10-14 11:17:09 +0100

    clang-format fix

--- libc/test/src/math/sinpif16_test.cpp
+++ libc/test/src/math/sinpif16_test.cpp
@@ -1,4 +1,5 @@
-//===-- Exhaustive test for sinpif16 ---------------------------------------===//
+//===-- Exhaustive test for sinpif16
+//---------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
