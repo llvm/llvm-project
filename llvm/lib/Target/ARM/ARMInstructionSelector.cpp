7bdcd90a6faf9caadbc8ba0fc5a03aadc6fe3daf
Serge Pavlov <sepavloff@gmail.com>
2024-10-14 10:20:34 +0700

    Commit suggestion
    Co-authored-by: Matt Arsenault <arsenm2@gmail.com>

--- llvm/lib/Target/ARM/ARMInstructionSelector.cpp
+++ llvm/lib/Target/ARM/ARMInstructionSelector.cpp
@@ -1109,7 +1109,7 @@ bool ARMInstructionSelector::select(MachineInstr &I) {
       MachineInstr *Ptr = MRI.getVRegDef(PtrReg);
       if (Ptr->getOpcode() == TargetOpcode::G_CONSTANT_POOL) {
         const MachineOperand &Index = Ptr->getOperand(1);
-        unsigned Opcode = (Subtarget->isThumb()) ? ARM::tLDRpci : ARM::LDRcp;
+        unsigned Opcode = Subtarget->isThumb() ? ARM::tLDRpci : ARM::LDRcp;
 
         auto Instr = BuildMI(MBB, I, I.getDebugLoc(), TII.get(Opcode))
                          .addDef(Reg)
