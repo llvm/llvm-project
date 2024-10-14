9e0b509b3d8cf4a07f2846a7354e7c70014e9288
Daniel Kiss <daniel.kiss@arm.com>
2024-10-14 21:15:08 +0200

    drop codegen dependency on santizer patch

--- clang/test/CodeGen/ifunc-win.c
+++ clang/test/CodeGen/ifunc-win.c
@@ -3,7 +3,6 @@
 // RUN: %clang_cc1 -triple aarch64-pc-windows-msvc -fsanitize=thread -O2 -emit-llvm -o - %s | FileCheck %s --check-prefix=SAN
 // RUN: %clang_cc1 -triple aarch64-pc-windows-msvc -fsanitize=address -O2 -emit-llvm -o - %s | FileCheck %s --check-prefix=SAN
 
-/// The ifunc is emitted before its resolver.
 int foo(int) __attribute__ ((ifunc("foo_ifunc")));
 
 static int f1(int i) {
@@ -42,10 +41,10 @@ void* goo_ifunc(void) {
 void *hoo_ifunc(void) { return 0; }
 extern void hoo(int) __attribute__ ((ifunc("hoo_ifunc")));
 
-/// No ifunc on Windows, lowerred to global pointers.
+/// ifunc on Windows is lowered to global pointers and an indirect call.
 // CHECK: @global = dso_local global i32 0, align 4
 // CHECK: {{.*}} = internal{{.*}}global{{.*}}poison, align 8
-/// Register the contructor for initialisation.
+/// Register the constructor for initialisation.
 // CHECK: @llvm.global_ctors = appending global [1 x { i32, ptr, ptr }] [{ i32, ptr, ptr } { i32 10, ptr @{{.*}}, ptr null }]
 
 // CHECK-LABEL: @bar()
@@ -56,14 +55,11 @@ extern void hoo(int) __attribute__ ((ifunc("hoo_ifunc")));
 // CHECK %0 = load ptr, ptr getelementptr inbounds ([3 x ptr], ptr @0, i32 0, i32 1), align 8
 // CHECK call void %0()
 
-// CHECK: define internal void @{{.*}}() #[[#CONSTRUCTOR:]] {
-// CHECK-DAG: attributes #[[#CONSTRUCTOR]] = {{{.*}} disable_sanitizer_instrumentation {{.*}}
+// CHECK: define internal void @{{.*}}()
 
-// SAN: define {{(dso_local )?}}noalias {{(noundef )?}}ptr @goo_ifunc() {{(local_unnamed_addr )?}}#[[#GOO_IFUNC:]] {
+// SAN: define {{(dso_local )?}}noalias {{(noundef )?}}ptr @goo_ifunc() {{(local_unnamed_addr )?}}
 
-// SAN: define {{(dso_local )?}}noalias {{(noundef )?}}ptr @hoo_ifunc() {{(local_unnamed_addr )?}}#[[#GOO_IFUNC]] {
+// SAN: define {{(dso_local )?}}noalias {{(noundef )?}}ptr @hoo_ifunc() {{(local_unnamed_addr )?}}
 
-// SAN: define internal {{(fastcc )?}}{{(noundef )?}}nonnull ptr @foo_ifunc() {{(unnamed_addr )?}}#[[#FOO_IFUNC:]] {
+// SAN: define internal {{(fastcc )?}}{{(noundef )?}}nonnull ptr @foo_ifunc() {{(unnamed_addr )?}}
 
-// SAN-DAG: attributes #[[#GOO_IFUNC]] = {{{.*}} disable_sanitizer_instrumentation {{.*}}
-// SAN-DAG: attributes #[[#FOO_IFUNC]] = {{{.*}} disable_sanitizer_instrumentation {{.*}}
