7cce6585707caf831f6c8de4b3096d468e3f00ee
Mel Chen <mel.chen@sifive.com>
2024-10-14 07:48:32 -0700

    Update VPlanVerify

--- llvm/lib/Transforms/Vectorize/VPlanVerifier.cpp
+++ llvm/lib/Transforms/Vectorize/VPlanVerifier.cpp
@@ -178,6 +178,10 @@ bool VPlanVerifier::verifyEVLRecipe(const VPInstruction &EVL) const {
                  return VerifyEVLUse(*I, 3);
                }
              })
+             .Case<VPWidenIntrinsicRecipe>(
+                 [&](const VPWidenIntrinsicRecipe *I) {
+                   return VerifyEVLUse(*I, I->getNumOperands() - 1);
+                 })
              .Default([&](const VPUser *U) {
                errs() << "EVL has unexpected user\n";
                return false;
