462853dda7f48d2312c7d6d6535105104315c812
Noah Goldstein <goldstein.w.n@gmail.com>
2024-10-14 16:48:51 -0500

    Use paramHasAttr

--- llvm/lib/Transforms/Utils/InlineFunction.cpp
+++ llvm/lib/Transforms/Utils/InlineFunction.cpp
@@ -1398,7 +1398,7 @@ static void AddParamAndFnBasicAttributes(const CallBase &CB,
         if (!Arg)
           continue;
 
-        if (NewInnerCB->getParamAttr(I, Attribute::ByVal).isValid())
+        if (NewInnerCB->paramHasAttr(I, Attribute::ByVal))
           // It's unsound to propagate memory attributes to byval arguments.
           // Even if CalledFunction doesn't e.g. write to the argument,
           // the call to NewInnerCB may write to its by-value copy.
