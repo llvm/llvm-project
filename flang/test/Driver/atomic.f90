89e80f8b6ed8447ba05774520fc188deeecca74e
David Truby <david.truby@arm.com>
2024-10-15 14:18:51 +0100

    Specify target and linker for tests

--- flang/test/Driver/atomic.f90
+++ flang/test/Driver/atomic.f90
@@ -1,5 +1,5 @@
-!RUN: %flang -fopenmp -rtlib=libgcc -### %s 2>&1 | FileCheck --check-prefixes=GCC %s
-!RUN: %flang -fopenmp -rtlib=compiler-rt -### %s 2>&1 | FileCheck --check-prefixes=CRT %s
+!RUN: %flang --target=aarch64-unknown-linux-gnu -fuse-ld=ld -fopenmp -rtlib=libgcc -### %s 2>&1 | FileCheck --check-prefixes=GCC %s
+!RUN: %flang --target=aarch64-unknown-linux-gnu -fuse-ld=ld -fopenmp -rtlib=compiler-rt -### %s 2>&1 | FileCheck --check-prefixes=CRT %s
 
 !GCC: -latomic
 !CRT-NOT: -latomic
