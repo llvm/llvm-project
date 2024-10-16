b1b5d57e5bf3e726654d80d2614c47e8bc91b45d
Andrew Jenner <Andrew.Jenner@amd.com>
2024-10-16 03:43:13 -0400

    Fix typo and merge issue.
    Change-Id: I68025fb4e6e239144c9205a543bd49004017b16f

--- compiler-rt/lib/sanitizer_common/sanitizer_symbolizer_amdgpu.h
+++ compiler-rt/lib/sanitizer_common/sanitizer_symbolizer_amdgpu.h
@@ -1,4 +1,4 @@
-//===-- sanitizer_symbolizer_fuchsia.h -----------------------------------===//
+//===-- sanitizer_symbolizer_amdgpu.h -------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
