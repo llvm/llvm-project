705d0e8bd1c37758ab0a6ebe1c4de6a3fdb1562c
Piyou Chen <piyou.chen@sifive.com>
2024-10-15 21:53:42 -0700

    Use getAsInteger to check whether is negative number

--- clang/lib/Sema/SemaDeclAttr.cpp
+++ clang/lib/Sema/SemaDeclAttr.cpp
@@ -3072,8 +3072,8 @@ bool Sema::checkTargetVersionAttr(SourceLocation LiteralLoc, Decl *D,
         if (HasPriority)
           DuplicateAttr = true;
         HasPriority = true;
-        int Digit;
-        if (AttrStr.getAsInteger(0, Digit) || Digit < 0)
+        unsigned Digit;
+        if (AttrStr.getAsInteger(0, Digit))
           return Diag(LiteralLoc, diag::warn_unsupported_target_attribute)
                  << Unsupported << None << AttrStr << TargetVersion;
       } else {
@@ -3226,8 +3226,8 @@ bool Sema::checkTargetClonesAttrString(
           HasDefault = true;
         } else if (AttrStr.consume_front("priority=")) {
           IsPriority = true;
-          int Digit;
-          if (AttrStr.getAsInteger(0, Digit) || Digit < 0)
+          unsigned Digit;
+          if (AttrStr.getAsInteger(0, Digit))
             return Diag(CurLoc, diag::warn_unsupported_target_attribute)
                    << Unsupported << None << Str << TargetClones;
         } else {
