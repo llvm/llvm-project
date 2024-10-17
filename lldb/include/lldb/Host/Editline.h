8c7f80f77505b7ff275d67a49f4f2dd07d604403
Jonas Devlieghere <jonas@devlieghere.com>
2024-10-17 08:19:58 -0700

    [lldb] Disable warning about codecvt_utf8 deprecation (NFC) (#112446)
    Disable -Wdeprecated-declarations for codecvt_utf8 in Editline. This is
    in preparation for #112276 which narrows the scope of
    -Wno-deprecated-declarations for building LLDB.

--- lldb/include/lldb/Host/Editline.h
+++ lldb/include/lldb/Host/Editline.h
@@ -57,6 +57,23 @@
 
 #include "llvm/ADT/FunctionExtras.h"
 
+#if defined(__clang__) && defined(__has_warning)
+#if __has_warning("-Wdeprecated-declarations")
+#define LLDB_DEPRECATED_WARNING_DISABLE                                        \
+  _Pragma("clang diagnostic push")                                             \
+      _Pragma("clang diagnostic ignored \"-Wdeprecated-declarations\"")
+#define LLDB_DEPRECATED_WARNING_RESTORE _Pragma("clang diagnostic pop")
+#endif
+#elif defined(__GNUC__) && __GNUC__ > 6
+#define LLDB_DEPRECATED_WARNING_DISABLE                                        \
+  _Pragma("GCC diagnostic push")                                               \
+      _Pragma("GCC diagnostic ignored \"-Wdeprecated-declarations\"")
+#define LLDB_DEPRECATED_WARNING_RESTORE _Pragma("GCC diagnostic pop")
+#else
+#define LLDB_DEPRECATED_WARNING_DISABLE
+#define LLDB_DEPRECATED_WARNING_RESTORE
+#endif
+
 namespace lldb_private {
 namespace line_editor {
 
@@ -367,7 +384,9 @@ private:
   void SetGetCharacterFunction(EditlineGetCharCallbackType callbackFn);
 
 #if LLDB_EDITLINE_USE_WCHAR
+  LLDB_DEPRECATED_WARNING_DISABLE
   std::wstring_convert<std::codecvt_utf8<wchar_t>> m_utf8conv;
+  LLDB_DEPRECATED_WARNING_RESTORE
 #endif
   ::EditLine *m_editline = nullptr;
   EditlineHistorySP m_history_sp;
