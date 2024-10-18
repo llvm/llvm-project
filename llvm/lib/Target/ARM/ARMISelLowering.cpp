44b020a3818a01b77415ce12629b020b641af2ea
Keith Packard <keithp@keithp.com>
2024-10-17 19:06:47 -0700

    [PowerPC][ISelLowering] Support -mstack-protector-guard=tls (#110928)
    Add support for using a thread-local variable with a specified offset
    for holding the stack guard canary value. This supports both 32- and 64-
    bit PowerPC targets.
    
    This mirrors changes from #108942 but targeting PowerPC instead of
    RISCV. Because both of these PRs modify the same driver functions, this
    series is stack on top of the RISC-V one.
    
    ---------
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- llvm/lib/Target/ARM/ARMISelLowering.cpp
+++ llvm/lib/Target/ARM/ARMISelLowering.cpp
@@ -21305,7 +21305,7 @@ bool ARMTargetLowering::shouldInsertFencesForAtomic(
   return InsertFencesForAtomic;
 }
 
-bool ARMTargetLowering::useLoadStackGuardNode() const {
+bool ARMTargetLowering::useLoadStackGuardNode(const Module &M) const {
   // ROPI/RWPI are not supported currently.
   return !Subtarget->isROPI() && !Subtarget->isRWPI();
 }
