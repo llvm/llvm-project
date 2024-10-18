bf93bb460dde909e5a0b13dc2b72ed17c4cb0989
wldfngrs <wldfngrs@gmail.com>
2024-10-19 00:53:45 +0100

    clang-format

--- libc/src/math/generic/cospif16.cpp
+++ libc/src/math/generic/cospif16.cpp
@@ -67,7 +67,7 @@ LLVM_LIBC_FUNCTION(float16, cospif16, (float16 x)) {
   // Once k and y are computed, we then deduce the answer by the sine of sum
   // formula:
   //   cos(x * pi) = cos((k + y) * pi/32)
-  //           = cos(k * pi/32) * cos(y * pi/32) 
+  //           = cos(k * pi/32) * cos(y * pi/32)
   //           + sin(y * pi/32) * sin(k * pi/32)
   // The values of sin(k * pi/32) and cos (k * pi/32) for k = 0...63 are
   // precomputed and stored using a vector of 64 single precision floats. sin(y
