dfb9e6827747e03885fa561ec2f7d4a157daaba7
abhishek-kaushik22 <abhishek.kaushik@intel.com>
2024-10-19 23:36:53 +0530

    Update CodeGenDAGPatterns.cpp

--- llvm/utils/TableGen/Common/CodeGenDAGPatterns.cpp
+++ llvm/utils/TableGen/Common/CodeGenDAGPatterns.cpp
@@ -3175,7 +3175,7 @@ void TreePattern::dump() const { print(errs()); }
 CodeGenDAGPatterns::CodeGenDAGPatterns(const RecordKeeper &R,
                                        PatternRewriterFn PatternRewriter)
     : Records(R), Target(R), Intrinsics(R),
-      LegalVTS(Target.getLegalValueTypes()), 
+      LegalVTS(Target.getLegalValueTypes()),
       PatternRewriter(std::move(PatternRewriter)) {
   ParseNodeInfo();
   ParseNodeTransforms();
