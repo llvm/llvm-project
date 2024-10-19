697397e749fd9e05981cfeda4deb5aedc766cf91
wldfngrs <wldfngrs@gmail.com>
2024-10-19 08:06:43 +0100

    added sincosf16_utils header, fixed formatting in existing pr

--- libc/src/math/cospif16.h
+++ libc/src/math/cospif16.h
@@ -1,10 +1,10 @@
-//===-- Implementation header for cospif16 ---------------------*- C++ -*-===//
+//===-- Implementation header for cospif16 ----------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
-// ===--------------------------------------------------------------------===//
+//===----------------------------------------------------------------------===//
 
 #ifndef LLVM_LIBC_SRC_MATH_COSPIF16_H
 #define LLVM_LIBC_SRC_MATH_COSPIF16_H
