bcbc0c2ad7d5d2444943da37f66a4704b2910a83
Fawdlstty <fawdlstty@users.noreply.github.com>
2024-10-21 05:17:15 +0800

    [TLI] Add basic support for scalbnxx (#112936)
    This patch adds basic support for `scalbln, scalblnf, scalblnl, scalbn,
    scalbnf, scalbnl`. Constant folding support will be submitted in a
    subsequent patch.
    
    Related issue: <#112631>

--- llvm/lib/Transforms/Utils/BuildLibCalls.cpp
+++ llvm/lib/Transforms/Utils/BuildLibCalls.cpp
@@ -1249,6 +1249,12 @@ bool llvm::inferNonMandatoryLibFuncAttrs(Function &F,
   case LibFunc_round:
   case LibFunc_roundf:
   case LibFunc_roundl:
+  case LibFunc_scalbln:
+  case LibFunc_scalblnf:
+  case LibFunc_scalblnl:
+  case LibFunc_scalbn:
+  case LibFunc_scalbnf:
+  case LibFunc_scalbnl:
   case LibFunc_sin:
   case LibFunc_sincospif_stret:
   case LibFunc_sinf:
