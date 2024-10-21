88dfe2be1c66e374d7074be1e76630a8a707f6fd
dong-miao <601183878@qq.com>
2024-10-21 16:52:47 +0800

    Update priv-invalid.s

--- llvm/test/MC/RISCV/priv-invalid.s
+++ llvm/test/MC/RISCV/priv-invalid.s
@@ -2,8 +2,6 @@
 
 mret 0x10 # CHECK: :[[@LINE]]:6: error: invalid operand for instruction
 
-mnret 0x10 # CHECK: :[[@LINE]]:6: error: invalid operand for instruction
-
 sfence.vma zero, a1, a2 # CHECK: :[[@LINE]]:22: error: invalid operand for instruction
 
 sfence.vma a0, 0x10 # CHECK: :[[@LINE]]:16: error: invalid operand for instruction
