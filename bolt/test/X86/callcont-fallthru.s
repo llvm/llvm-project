e8ec9c936c31ae0ec0a56a224860045c0a2885a1
Amir Ayupov <aaupov@fb.com>
2024-10-21 14:32:11 -0700

    Add test for getFallthroughsInTrace
    Created using spr 1.3.4

--- bolt/test/X86/callcont-fallthru.s
+++ bolt/test/X86/callcont-fallthru.s
@@ -4,9 +4,15 @@
 # RUN: llvm-mc -filetype=obj -triple x86_64-unknown-unknown %s -o %t.o
 # RUN: %clang %cflags %t.o -o %t.exe -Wl,-q -nostdlib
 # RUN: link_fdata %s %t.exe %t.pa PREAGG
+# RUN: link_fdata %s %t.exe %t.pa2 PREAGG2
 # RUN: llvm-strip --strip-unneeded %t.exe
 # RUN: llvm-bolt %t.exe --pa -p %t.pa -o %t.out \
 # RUN:   --print-cfg --print-only=main | FileCheck %s
+
+## Check that getFallthroughsInTrace correctly handles a trace starting at plt
+## call continuation
+# RUN: llvm-bolt %t.exe --pa -p %t.pa2 -o %t.out2 \
+# RUN:   --print-cfg --print-only=main | FileCheck %s --check-prefix=CHECK2
 
   .globl foo
   .type foo, %function
@@ -32,6 +38,9 @@ main:
 # CHECK:      callq puts@PLT
 # CHECK-NEXT: count: 2
 
+# CHECK2:      callq puts@PLT
+# CHECK2-NEXT: count: 0
+
 Ltmp1:
 	movq	-0x10(%rbp), %rax
 	movq	0x8(%rax), %rdi
@@ -40,6 +49,8 @@ Ltmp1:
 Ltmp4:
 	cmpl	$0x0, -0x14(%rbp)
 	je	Ltmp0
+# CHECK2:      je .Ltmp0
+# CHECK2-NEXT: count: 3
 
 	movl	$0xa, -0x18(%rbp)
 	callq	foo
@@ -47,8 +58,13 @@ Ltmp4:
 # CHECK:      callq foo
 # CHECK-NEXT: count: 1
 
+# PREAGG2: F #Ltmp1# #Ltmp3_br# 3
+# CHECK2:      callq foo
+# CHECK2-NEXT: count: 3
+
 Ltmp3:
 	cmpl	$0x0, -0x18(%rbp)
+Ltmp3_br:
 	jmp	Ltmp2
 
 Ltmp2:
