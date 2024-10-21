4e7d685377f29df02bb1db70a6ebb6b7835c96d3
Piyou Chen <piyou.chen@sifive.com>
2024-10-21 16:10:22 +0800

    [RISCV][FMV] Remove support for negative priority (#112161)
    Ensure that target_version and target_clones do not accept negative
    numbers for the priority feature.
    
    Base on discussion on
    https://github.com/riscv-non-isa/riscv-c-api-doc/pull/85.

--- clang/lib/CodeGen/CodeGenFunction.cpp
+++ clang/lib/CodeGen/CodeGenFunction.cpp
@@ -2904,19 +2904,18 @@ void CodeGenFunction::EmitMultiVersionResolver(
   }
 }
 
-static int getPriorityFromAttrString(StringRef AttrStr) {
+static unsigned getPriorityFromAttrString(StringRef AttrStr) {
   SmallVector<StringRef, 8> Attrs;
 
   AttrStr.split(Attrs, ';');
 
   // Default Priority is zero.
-  int Priority = 0;
+  unsigned Priority = 0;
   for (auto Attr : Attrs) {
     if (Attr.consume_front("priority=")) {
-      int Result;
-      if (!Attr.getAsInteger(0, Result)) {
+      unsigned Result;
+      if (!Attr.getAsInteger(0, Result))
         Priority = Result;
-      }
     }
   }
 
