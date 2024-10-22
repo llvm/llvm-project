e873b9e610631f2fec3e2cf35ca0b8dcaa9bbddf
YunQiang Su <yunqiang@isrc.iscas.ac.cn>
2024-10-22 09:56:06 +0800

    fix testcases

--- clang/test/CodeGen/fmaxnum_fminnum_use_nsz.c
+++ clang/test/CodeGen/fmaxnum_fminnum_use_nsz.c
@@ -8,26 +8,50 @@ double fmax (double, double);
 long double fmaxl (long double, long double);
 
 // CHECK: call nsz float @llvm.minnum.f32
-float fmin1(float a, float b) {
+float fmin32(float a, float b) {
         return fminf(a, b);
 }
+// CHECK: call nsz float @llvm.minnum.f32
+float fmin32b(float a, float b) {
+        return __builtin_fminf(a, b);
+}
 // CHECK: call nsz double @llvm.minnum.f64
-float fmin2(double a, double b) {
+float fmin64(double a, double b) {
         return fmin(a, b);
 }
+// CHECK: call nsz double @llvm.minnum.f64
+float fmin64b(double a, double b) {
+        return __builtin_fmin(a, b);
+}
 // CHECK: call nsz x86_fp80 @llvm.minnum.f80
-float fmin3(long double a, long double b) {
+float fmin80(long double a, long double b) {
         return fminl(a, b);
 }
+// CHECK: call nsz x86_fp80 @llvm.minnum.f80
+float fmin80b(long double a, long double b) {
+        return __builtin_fminl(a, b);
+}
 // CHECK: call nsz float @llvm.maxnum.f32
-float fmax1(float a, float b) {
+float fmax32(float a, float b) {
         return fmaxf(a, b);
 }
+// CHECK: call nsz float @llvm.maxnum.f32
+float fmax32b(float a, float b) {
+        return __builtin_fmaxf(a, b);
+}
 // CHECK: call nsz double @llvm.maxnum.f64
-float fmax2(double a, double b) {
+float fmax64(double a, double b) {
         return fmax(a, b);
 }
+// CHECK: call nsz double @llvm.maxnum.f64
+float fmax64b(double a, double b) {
+        return __builtin_fmax(a, b);
+}
 // CHECK: call nsz x86_fp80 @llvm.maxnum.f80
-float fmax3(long double a, long double b) {
+float fmax80(long double a, long double b) {
         return fmaxl(a, b);
+}
+// CHECK: call nsz x86_fp80 @llvm.maxnum.f80
+float fmax80b(long double a, long double b) {
+        return __builtin_fmaxl(a, b);
 }
