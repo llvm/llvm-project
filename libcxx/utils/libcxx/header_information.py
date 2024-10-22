cf9e2e2322f06bc064402dbb7d1746c9e25daee1
Louis Dionne <ldionne.2@gmail.com>
2024-10-22 12:37:22 -0400

    Add missing cstdalign to c header files

--- libcxx/utils/libcxx/header_information.py
+++ libcxx/utils/libcxx/header_information.py
@@ -66,6 +66,7 @@ class Header:
             "cmath",
             "csetjmp",
             "csignal",
+            "cstdalign",
             "cstdarg",
             "cstdbool",
             "cstddef",
