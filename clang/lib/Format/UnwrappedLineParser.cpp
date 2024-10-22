cbd1136f7ff7aab46c850594b4d9199256d9edfb
Emilia Kond <emilia@rymiel.space>
2024-10-22 13:36:28 +0300

    [clang-format] Make bitwise and imply requires clause (#110942)
    This patch adjusts the requires clause/expression parser to imply a
    requires clause if it is preceded by a bitwise and operator `&`, and
    assume it is a reference qualifier. The justification is that bitwise
    operations should not be used for requires expressions.
    
    This is a band-aid fix. The real problems lie in the lookahead heuristic
    in the same method. It may be worth it to rewrite that whole heuristic
    to track more state in the future, instead of just blindly marching
    forward across multiple unrelated definitions, since right now, the
    definition following the one with the requires clause can influence
    whether the heuristic chooses clause or expression.
    
    Fixes https://github.com/llvm/llvm-project/issues/110485

--- clang/lib/Format/UnwrappedLineParser.cpp
+++ clang/lib/Format/UnwrappedLineParser.cpp
@@ -3484,10 +3484,10 @@ bool UnwrappedLineParser::parseRequires() {
   case tok::r_paren:
   case tok::kw_noexcept:
   case tok::kw_const:
+  case tok::amp:
     // This is a requires clause.
     parseRequiresClause(RequiresToken);
     return true;
-  case tok::amp:
   case tok::ampamp: {
     // This can be either:
     // if (... && requires (T t) ...)
