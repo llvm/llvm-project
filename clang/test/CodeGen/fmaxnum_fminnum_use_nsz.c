86f20b2c34abbcd4757eb7011d3a4cc985a8bcf0
YunQiang Su <yunqiang@isrc.iscas.ac.cn>
2024-10-22 10:36:46 +0800

    add missing builtin test

--- clang/test/CodeGen/fmaxnum_fminnum_use_nsz.c
+++ clang/test/CodeGen/fmaxnum_fminnum_use_nsz.c
@@ -10,33 +10,63 @@ long double fmaxl (long double, long double);
 
 // CHECK: call nsz float @llvm.minnum.f32
 // CHECK-STRICT: call nsz float @llvm.experimental.constrained.minnum.f32{{.*}} #2
-float fmin1(float a, float b) {
+float fmin32(float a, float b) {
         return fminf(a, b);
+}
+// CHECK: call nsz float @llvm.minnum.f32
+// CHECK-STRICT: call nsz float @llvm.experimental.constrained.minnum.f32{{.*}} #2
+float fmin32b(float a, float b) {
+        return __builtin_fminf(a, b);
 }
 // CHECK: call nsz double @llvm.minnum.f64
 // CHECK-STRICT: call nsz double @llvm.experimental.constrained.minnum.f64{{.*}} #2
-float fmin2(double a, double b) {
+float fmin64(double a, double b) {
         return fmin(a, b);
+}
+// CHECK: call nsz double @llvm.minnum.f64
+// CHECK-STRICT: call nsz double @llvm.experimental.constrained.minnum.f64{{.*}} #2
+float fmin64b(double a, double b) {
+        return __builtin_fmin(a, b);
 }
 // CHECK: call nsz x86_fp80 @llvm.minnum.f80
 // CHECK-STRICT: call nsz x86_fp80 @llvm.experimental.constrained.minnum.f80{{.*}} #2
-float fmin3(long double a, long double b) {
+float fmin80(long double a, long double b) {
         return fminl(a, b);
+}
+// CHECK: call nsz x86_fp80 @llvm.minnum.f80
+// CHECK-STRICT: call nsz x86_fp80 @llvm.experimental.constrained.minnum.f80{{.*}} #2
+float fmin80b(long double a, long double b) {
+        return __builtin_fminl(a, b);
 }
 // CHECK: call nsz float @llvm.maxnum.f32
 // CHECK-STRICT: call nsz float @llvm.experimental.constrained.maxnum.f32{{.*}} #2
-float fmax1(float a, float b) {
+float fmax32(float a, float b) {
         return fmaxf(a, b);
+}
+// CHECK: call nsz float @llvm.maxnum.f32
+// CHECK-STRICT: call nsz float @llvm.experimental.constrained.maxnum.f32{{.*}} #2
+float fmax32b(float a, float b) {
+        return __builtin_fmaxf(a, b);
 }
 // CHECK: call nsz double @llvm.maxnum.f64
 // CHECK-STRICT: call nsz double @llvm.experimental.constrained.maxnum.f64{{.*}} #2
-float fmax2(double a, double b) {
+float fmax64(double a, double b) {
         return fmax(a, b);
+}
+// CHECK: call nsz double @llvm.maxnum.f64
+// CHECK-STRICT: call nsz double @llvm.experimental.constrained.maxnum.f64{{.*}} #2
+float fmax64b(double a, double b) {
+        return __builtin_fmax(a, b);
 }
 // CHECK: call nsz x86_fp80 @llvm.maxnum.f80
 // CHECK-STRICT: call nsz x86_fp80 @llvm.experimental.constrained.maxnum.f80{{.*}} #2
 float fmax3(long double a, long double b) {
         return fmaxl(a, b);
+}
+// CHECK: call nsz x86_fp80 @llvm.maxnum.f80
+// CHECK-STRICT: call nsz x86_fp80 @llvm.experimental.constrained.maxnum.f80{{.*}} #2
+float fmax80b(long double a, long double b) {
+        return __builtin_fmaxl(a, b);
 }
 
 //CHECK-STRICT: attributes #2 = { strictfp }
