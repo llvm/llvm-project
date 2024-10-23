1225c503ae9f8d5eaa70b6deaf4bb4c6e276f424
Lang Hames <lhames@gmail.com>
2024-10-24 08:23:41 +1100

    [ORC-RT] Initial check-in for a new, top-level ORC runtime project.
    Includes CMake files and a placeholder header, library, and tool.
    
    See discussion at
    https://discourse.llvm.org/t/rfc-move-orc-executor-support-into-top-level-project/81049

---
+++ orc-rt/lib/prelink/orc-rt-prelink.cpp
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+extern "C" void orc_rt(void) {
+  printf("hello, world!\n");
+}
---
+++ orc-rt/tools/orc-executor/orc-executor.cpp
@@ -0,0 +1,6 @@
+#include "orc-rt-c/orc-rt.h"
+
+int main(int argc, char *argv[]) {
+  orc_rt();
+  return 0;
+}
