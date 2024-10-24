5c99093aede560d08101b940354653c8e2a32db8
Krzysztof Parzyszek <Krzysztof.Parzyszek@amd.com>
2024-10-24 17:04:38 -0500

    only call ResolveName for unresolved common block names

--- flang/lib/Semantics/resolve-directives.cpp
+++ flang/lib/Semantics/resolve-directives.cpp
@@ -442,7 +442,8 @@ public:
     // Iterate over elements of x, and resolve any common blocks that
     // are still unresolved.
     for (const parser::OmpObject &obj : x.v) {
-      if (auto *name{std::get_if<parser::Name>(&obj.u)}) {
+      auto *name{std::get_if<parser::Name>(&obj.u)};
+      if (name && !name->symbol) {
         Resolve(*name, currScope().MakeCommonBlock(name->source));
       }
     }
