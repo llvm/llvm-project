80d18f9e6b05dc6adf5477a9bafe2e18f9740480
Haojian Wu <hokein.wu@gmail.com>
2024-10-25 21:25:08 +0200

    Fix a typo

--- llvm/include/llvm/Support/Compiler.h
+++ llvm/include/llvm/Support/Compiler.h
@@ -416,7 +416,7 @@
 #if LLVM_HAS_CPP_ATTRIBUTE(clang::lifetimebound)
 #define LLVM_LIFETIME_BOUND [[clang::lifetimebound]]
 #else
-#define LLVM_LLVM_LIFETIME_BOUND
+#define LLVM_LIFETIME_BOUND
 #endif
 
 #if LLVM_HAS_CPP_ATTRIBUTE(nodiscard) >= 201907L
