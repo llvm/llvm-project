77283b19c4149b49f38cc8aa032c03c04426f7a3
Jan Svoboda <jan_svoboda@apple.com>
2024-10-25 12:40:59 -0700

    [clang][serialization] Bump `NUM_PREDEF_TYPE_IDS`
    This fixes a build error caused by 4ac0e7e400fe2a66d1fd5d5d1fa1c899dfb16716.

--- clang/include/clang/Serialization/ASTBitCodes.h
+++ clang/include/clang/Serialization/ASTBitCodes.h
@@ -1149,7 +1149,7 @@ enum PredefinedTypeIDs {
 ///
 /// Type IDs for non-predefined types will start at
 /// NUM_PREDEF_TYPE_IDs.
-const unsigned NUM_PREDEF_TYPE_IDS = 512;
+const unsigned NUM_PREDEF_TYPE_IDS = 513;
 
 // Ensure we do not overrun the predefined types we reserved
 // in the enum PredefinedTypeIDs above.
