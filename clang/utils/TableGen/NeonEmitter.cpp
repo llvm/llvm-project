f36ade05da5ff07588c592c18c09857dbb652610
CarolineConcatto <caroline.concatto@arm.com>
2024-10-25 13:59:46 +0100

    [CLANG][AArch64] Add the  modal 8 bit floating-point scalar type (#97277)
    ARM ACLE PR#323[1] adds new modal types for 8-bit floating point
    intrinsic.
    
    From the PR#323:
    ```
    ACLE defines the `__mfp8` type, which can be used for the E5M2 and E4M3
    8-bit floating-point formats. It is a storage and interchange only type
    with no arithmetic operations other than intrinsic calls.
    ````
    
    The type should be an opaque type and its format in undefined in Clang.
    Only defined in the backend by a status/format register, for AArch64 the
    FPMR.
    
    This patch is an attempt to the add the mfloat8_t scalar type. It has a
    parser and codegen for the new scalar type.
    
    The patch it is lowering to and 8bit unsigned as it has no format. But
    maybe we should add another opaque type.
    
    [1]  https://github.com/ARM-software/acle/pull/323

--- clang/utils/TableGen/NeonEmitter.cpp
+++ clang/utils/TableGen/NeonEmitter.cpp
@@ -2588,6 +2588,7 @@ void NeonEmitter::runVectorTypes(raw_ostream &OS) {
   OS << "typedef __fp16 float16_t;\n";
 
   OS << "#if defined(__aarch64__) || defined(__arm64ec__)\n";
+  OS << "typedef __MFloat8_t __mfp8;\n";
   OS << "typedef __MFloat8x8_t mfloat8x8_t;\n";
   OS << "typedef __MFloat8x16_t mfloat8x16_t;\n";
   OS << "typedef double float64_t;\n";
