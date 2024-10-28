d8672b2f6127b2679dab61d53c58a439084cbc2e
Jan Svoboda <jan_svoboda@apple.com>
2024-10-28 11:24:27 -0700

    [clang][modules] Preserve the module map that allowed inferring (#113389)
    With inferred modules, the dependency scanner takes care to replace the
    fake "__inferred_module.map" path with the file that allowed the module
    to be inferred. However, this only worked when such a module was
    imported directly in the TU. Whenever such module got loaded
    transitively, the scanner would fail to perform the replacement. This is
    caused by the fact that PCM files are lossy and drop this information.
    
    This patch makes sure that PCMs include this file for each submodule (in
    the `SUBMODULE_DEFINITION` record), fixes one existing test with an
    incorrect assertion, and does a little drive-by refactoring of
    `ModuleMap`.

--- clang/lib/Serialization/ASTReader.cpp
+++ clang/lib/Serialization/ASTReader.cpp
@@ -5813,6 +5813,7 @@ llvm::Error ASTReader::ReadSubmoduleBlock(ModuleFile &F,
       SubmoduleID Parent = getGlobalSubmoduleID(F, Record[Idx++]);
       Module::ModuleKind Kind = (Module::ModuleKind)Record[Idx++];
       SourceLocation DefinitionLoc = ReadSourceLocation(F, Record[Idx++]);
+      FileID InferredAllowedBy = ReadFileID(F, Record, Idx);
       bool IsFramework = Record[Idx++];
       bool IsExplicit = Record[Idx++];
       bool IsSystem = Record[Idx++];
@@ -5833,8 +5834,6 @@ llvm::Error ASTReader::ReadSubmoduleBlock(ModuleFile &F,
       CurrentModule =
           ModMap.findOrCreateModule(Name, ParentModule, IsFramework, IsExplicit)
               .first;
-
-      // FIXME: Call ModMap.setInferredModuleAllowedBy()
 
       SubmoduleID GlobalIndex = GlobalID - NUM_PREDEF_SUBMODULE_IDS;
       if (GlobalIndex >= SubmodulesLoaded.size() ||
@@ -5866,6 +5865,8 @@ llvm::Error ASTReader::ReadSubmoduleBlock(ModuleFile &F,
       CurrentModule->DefinitionLoc = DefinitionLoc;
       CurrentModule->Signature = F.Signature;
       CurrentModule->IsFromModuleFile = true;
+      if (InferredAllowedBy.isValid())
+        ModMap.setInferredModuleAllowedBy(CurrentModule, InferredAllowedBy);
       CurrentModule->IsSystem = IsSystem || CurrentModule->IsSystem;
       CurrentModule->IsExternC = IsExternC;
       CurrentModule->InferSubmodules = InferSubmodules;
