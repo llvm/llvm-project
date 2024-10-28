8dfda36a89ca47036c34e670897d6dc936e636ec
Job Henandez Lara <jobhdezlara93@gmail.com>
2024-10-28 13:29:16 -0700

    [libc] remove #include <fcntl.h> and add proxy or type (#113836)

--- libc/hdr/types/mode_t.h
+++ libc/hdr/types/mode_t.h
@@ -1,4 +1,4 @@
-//===-- Definition of macros from fcntl/fcntl.h ---------------------------===//
+//===-- Definition of macros from mode_t.h --------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,12 +6,12 @@
 //
 //===----------------------------------------------------------------------===//
 
-#ifndef LLVM_LIBC_HDR_FCNTL_MACROS_H
-#define LLVM_LIBC_HDR_FCNTL_MACROS_H
+#ifndef LLVM_LIBC_HDR_MODE_T_H
+#define LLVM_LIBC_HDR_MODE_T_H
 
 #ifdef LIBC_FULL_BUILD
 
-#include "include/llvm-libc-macros/fcntl-macros.h"
+#include "include/llvm-libc-types/mode_t.h"
 
 #else // Overlay mode
 
@@ -19,4 +19,4 @@
 
 #endif // LLVM_LIBC_FULL_BUILD
 
-#endif // LLVM_LIBC_HDR_FCNTL_MACROS_H
+#endif // LLVM_LIBC_HDR_MODE_T_H
