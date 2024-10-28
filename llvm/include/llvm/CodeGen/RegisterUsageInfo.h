3608f47620db08475bce5ec7b28d26a84f1b0cb0
Akshat Oke <Akshat.Oke@amd.com>
2024-10-28 09:17:16 +0000

    fix incorrect changes

--- llvm/include/llvm/CodeGen/RegisterUsageInfo.h
+++ llvm/include/llvm/CodeGen/RegisterUsageInfo.h
@@ -51,6 +51,9 @@ public:
 
   void print(raw_ostream &OS, const Module *M = nullptr) const;
 
+  bool invalidate(Module &M, const PreservedAnalyses &PA,
+                  ModuleAnalysisManager::Invalidator &Inv);
+
 private:
   /// A Dense map from Function * to RegMask.
   /// In RegMask 0 means register used (clobbered) by function.
