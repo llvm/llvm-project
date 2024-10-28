ec66b31b8ab7e3a46d2012a0b1475a13af899cc0
Aiden Grossman <aidengrossman@google.com>
2024-10-28 07:03:46 +0000

    bump CI

--- cmake/Modules/CMakePolicy.cmake
+++ cmake/Modules/CMakePolicy.cmake
@@ -3,7 +3,7 @@
 # CMP0116: Ninja generators transform `DEPFILE`s from `add_custom_command()`
 # New in CMake 3.20. https://cmake.org/cmake/help/latest/policy/CMP0116.html
 if(POLICY CMP0116)
-  cmake_policy(SET CMP0116 NEW)
+  cmake_policy(SET CMP0116 OLD)
 endif()
 
 # MSVC debug information format flags are selected via
