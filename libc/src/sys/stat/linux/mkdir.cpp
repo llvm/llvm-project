3dfbc28225609ff3b7bcc211d33a93a805aa1123
Job Henandez Lara <jobhdezlara93@gmail.com>
2024-10-28 14:49:36 -0700

    [libc] Fix the remaining fcntl.h proxy header includes. (#113961)

--- libc/src/sys/stat/linux/mkdir.cpp
+++ libc/src/sys/stat/linux/mkdir.cpp
@@ -11,6 +11,7 @@
 #include "src/__support/OSUtil/syscall.h" // For internal syscall function.
 #include "src/__support/common.h"
 
+#include "hdr/fcntl_macros.h"
 #include "hdr/types/mode_t.h"
 #include "src/__support/macros/config.h"
 #include "src/errno/libc_errno.h"
