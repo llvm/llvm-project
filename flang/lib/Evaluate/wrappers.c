d5219a9792ca177f8b4bca79a9ab718c6076ec64
Kelvin Li <kli@ca.ibm.com>
2024-10-29 14:23:26 -0400

    Add copyright note and fix format

--- flang/lib/Evaluate/wrappers.c
+++ flang/lib/Evaluate/wrappers.c
@@ -1,15 +1,21 @@
+//===-- lib/Evaluate/wrappers.c -------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
 #include <complex.h>
 
-void csqrtf_wrapper(const float x[2], float res[2])
-{
+void csqrtf_wrapper(const float x[2], float res[2]) {
   float complex c = x[0] + I * x[1];
   float complex r = csqrtf(c);
   res[0] = crealf(r);
   res[1] = cimagf(r);
 }
 
-void csqrt_wrapper(const double x[2], double res[2])
-{
+void csqrt_wrapper(const double x[2], double res[2]) {
   double complex c = x[0] + I * x[1];
   double complex r = csqrt(c);
   res[0] = creal(r);
