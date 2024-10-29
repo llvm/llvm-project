0c2eceec5948ceeb58f34435513944b41ec01a61
Dave Lee <davelee.com@gmail.com>
2024-10-29 13:55:29 -0700

    [lldb] Update Measurement data formatter (#9127) (#9481)
    rdar://121940851
    (cherry picked from commit c45216d9b9daa9341eb50919cf6d13e3eda19c63)

--- lldb/test/API/lang/swift/foundation_value_types/measurement/main.swift
+++ lldb/test/API/lang/swift/foundation_value_types/measurement/main.swift
@@ -13,8 +13,7 @@ import Foundation
 
 func main() {
   var measurement = Measurement(value: 1.25, unit: Unit(symbol: "m"))
-  print("done!") //% self.expect("frame variable measurement", substrs=['1.25 m'])
-   //% self.expect("expression -d run -- measurement", substrs=['1.25 m'])
+  print("break here")
 }
 
 main()
