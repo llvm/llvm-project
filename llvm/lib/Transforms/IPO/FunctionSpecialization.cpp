317bcff4e13f38dc1a46ee08835fd7178af7b29c
Hari Limaye <hari.limaye@arm.com>
2024-10-29 12:11:52 +0000

    [FuncSpec] Improve handling of Comparison Instructions
    When visiting comparison instructions during computation of a
    specializations's bonus, make use of information from the lattice value
    of the other operand in the case where we have not found this to have a
    specific constant value.

--- llvm/lib/Transforms/IPO/FunctionSpecialization.cpp
+++ llvm/lib/Transforms/IPO/FunctionSpecialization.cpp
@@ -468,16 +468,25 @@ Constant *InstCostVisitor::visitCastInst(CastInst &I) {
 Constant *InstCostVisitor::visitCmpInst(CmpInst &I) {
   assert(LastVisited != KnownConstants.end() && "Invalid iterator!");
 
+  Constant *Const = LastVisited->second;
   bool Swap = I.getOperand(1) == LastVisited->first;
   Value *V = Swap ? I.getOperand(0) : I.getOperand(1);
   Constant *Other = findConstantFor(V, KnownConstants);
-  if (!Other)
-    return nullptr;
 
-  Constant *Const = LastVisited->second;
-  return Swap ?
-        ConstantFoldCompareInstOperands(I.getPredicate(), Other, Const, DL)
-      : ConstantFoldCompareInstOperands(I.getPredicate(), Const, Other, DL);
+  if (Other)
+    return Swap ? ConstantFoldCompareInstOperands(I.getPredicate(), Other,
+                                                  Const, DL)
+                : ConstantFoldCompareInstOperands(I.getPredicate(), Const,
+                                                  Other, DL);
+
+  // If we haven't found Other to be a specific constant value, we may still be
+  // able constant fold the comparison using information from the lattice value.
+  ValueLatticeElement ConstLV = ValueLatticeElement();
+  ConstLV.markConstant(Const);
+  const ValueLatticeElement &OtherLV = Solver.getLatticeValueFor(V);
+  auto &V1State = Swap ? OtherLV : ConstLV;
+  auto &V2State = Swap ? ConstLV : OtherLV;
+  return V1State.getCompare(I.getPredicate(), I.getType(), V2State, DL);
 }
 
 Constant *InstCostVisitor::visitUnaryOperator(UnaryOperator &I) {
