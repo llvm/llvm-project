4455b931adac5de9f086dac120729c45dadb4001
Akshat Oke <Akshat.Oke@amd.com>
2024-10-29 07:14:30 +0000

    [CodeGen][NewPM] Port RegUsageInfoPropagation pass to NPM

--- llvm/include/llvm/Passes/CodeGenPassBuilder.h
+++ llvm/include/llvm/Passes/CodeGenPassBuilder.h
@@ -55,6 +55,7 @@
 #include "llvm/CodeGen/PreISelIntrinsicLowering.h"
 #include "llvm/CodeGen/RegAllocFast.h"
 #include "llvm/CodeGen/RegUsageInfoCollector.h"
+#include "llvm/CodeGen/RegUsageInfoPropagate.h"
 #include "llvm/CodeGen/RegisterUsageInfo.h"
 #include "llvm/CodeGen/ReplaceWithVeclib.h"
 #include "llvm/CodeGen/SafeStack.h"
