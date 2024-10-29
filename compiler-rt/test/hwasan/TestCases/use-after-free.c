821f7570ce7add2c4618861e53451d474437b300
Thurston Dang <thurston@google.com>
2024-10-29 20:14:14 +0000

    [hwasan] Fix forward '[hwasan] Flush stderr/stdout in tests (#114083)'
    4146c39b6068c0a2ab81a64f75639b959840928b broke the build because subsequent checks depend on the line numbers
    
    https://lab.llvm.org/buildbot/#/builders/174/builds/7534/steps/6/logs/FAIL__HWAddressSanitizer-x86_64__use-after-free_c

--- compiler-rt/test/hwasan/TestCases/use-after-free.c
+++ compiler-rt/test/hwasan/TestCases/use-after-free.c
@@ -32,11 +32,11 @@ int main() {
   //
   // CHECK: freed by thread {{.*}} here:
   // CHECK: #0 {{.*}} in {{.*}}free{{.*}} {{.*}}hwasan_allocation_functions.cpp
-  // CHECK: #1 {{.*}} in main {{.*}}use-after-free.c:[[@LINE-19]]
+  // CHECK: #1 {{.*}} in main {{.*}}use-after-free.c:[[@LINE-20]]
 
   // CHECK: previously allocated by thread {{.*}} here:
   // CHECK: #0 {{.*}} in {{.*}}malloc{{.*}} {{.*}}hwasan_allocation_functions.cpp
-  // CHECK: #1 {{.*}} in main {{.*}}use-after-free.c:[[@LINE-24]]
+  // CHECK: #1 {{.*}} in main {{.*}}use-after-free.c:[[@LINE-25]]
   // CHECK: Memory tags around the buggy address (one tag corresponds to 16 bytes):
   // CHECK: =>{{.*}}[[MEM_TAG]]
   // CHECK: SUMMARY: HWAddressSanitizer: tag-mismatch {{.*}} in main
