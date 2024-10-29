4e843a9ea3f9f9013ffd3a81cca9101a1abc47b7
Wanyi <kusmour@gmail.com>
2024-10-29 14:22:51 -0400

    [lldb] Fix write only file action to truncate the file (#112657)
    When `FileAction` opens file with write access, it doesn't clear the
    file nor append to the end of the file if it already exists. Instead, it
    writes from cursor index 0.
    
    For example, by using the settings `target.output-path` and
    `target.error-path`, lldb will redirect process stdout/stderr to files.
    It then calls this function to write to the files which the above
    symptoms appear.
    
    ## Test
    - Added unit test checking the file flags
    - Added 2 api tests checking
      - File content overwritten if the file path already exists
    - Stdout and stderr redirection to the same file doesn't change its
    behavior

--- lldb/unittests/Host/FileActionTest.cpp
+++ lldb/unittests/Host/FileActionTest.cpp
@@ -6,6 +6,8 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include <fcntl.h>
+
 #include "lldb/Host/FileAction.h"
 #include "gtest/gtest.h"
 
@@ -16,4 +18,27 @@ TEST(FileActionTest, Open) {
   Action.Open(47, FileSpec("/tmp"), /*read*/ true, /*write*/ false);
   EXPECT_EQ(Action.GetAction(), FileAction::eFileActionOpen);
   EXPECT_EQ(Action.GetFileSpec(), FileSpec("/tmp"));
+}
+
+TEST(FileActionTest, OpenReadWrite) {
+  FileAction Action;
+  Action.Open(48, FileSpec("/tmp_0"), /*read*/ true, /*write*/ true);
+  EXPECT_TRUE(Action.GetActionArgument() & (O_NOCTTY | O_CREAT | O_RDWR));
+  EXPECT_FALSE(Action.GetActionArgument() & O_RDONLY);
+  EXPECT_FALSE(Action.GetActionArgument() & O_WRONLY);
+}
+
+TEST(FileActionTest, OpenReadOnly) {
+  FileAction Action;
+  Action.Open(49, FileSpec("/tmp_1"), /*read*/ true, /*write*/ false);
+  EXPECT_TRUE(Action.GetActionArgument() & (O_NOCTTY | O_RDONLY));
+  EXPECT_FALSE(Action.GetActionArgument() & O_WRONLY);
+}
+
+TEST(FileActionTest, OpenWriteOnly) {
+  FileAction Action;
+  Action.Open(50, FileSpec("/tmp_2"), /*read*/ false, /*write*/ true);
+  EXPECT_TRUE(Action.GetActionArgument() &
+              (O_NOCTTY | O_CREAT | O_WRONLY | O_TRUNC));
+  EXPECT_FALSE(Action.GetActionArgument() & O_RDONLY);
 }
