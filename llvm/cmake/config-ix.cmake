b6a84d3740be1b1b9767f2f51080b02e3746a5ab
Udit Agarwal <udit.agarwal@intel.com>
2024-10-29 16:11:38 -0700

    Fail if static zstd libs are not found

--- llvm/cmake/config-ix.cmake
+++ llvm/cmake/config-ix.cmake
@@ -161,6 +161,10 @@ endif()
 # If LLVM_USE_STATIC_ZSTD is specified, make sure we enable zstd only if static
 # libraries are found.
 if(LLVM_USE_STATIC_ZSTD AND NOT TARGET zstd::libzstd_static)
+  # Fail if LLVM_ENABLE_ZSTD is FORCE_ON.
+  if(LLVM_ENABLE_ZSTD STREQUAL FORCE_ON)
+      message(FATAL_ERROR "Failed to find static zstd libraries, but LLVM_USE_STATIC_ZSTD=ON and LLVM_ENABLE_ZSTD=FORCE_ON.")
+  endif()
 set(LLVM_ENABLE_ZSTD OFF)
 else()
 set(LLVM_ENABLE_ZSTD ${zstd_FOUND})
