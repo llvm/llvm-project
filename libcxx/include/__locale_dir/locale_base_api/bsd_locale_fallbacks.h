7dfae2ca2b99949f374264906923d230306c27e5
Louis Dionne <ldionne.2@gmail.com>
2024-10-30 12:49:12 -0400

    Add debug

--- libcxx/include/__locale_dir/locale_base_api/bsd_locale_fallbacks.h
+++ libcxx/include/__locale_dir/locale_base_api/bsd_locale_fallbacks.h
@@ -81,6 +81,7 @@ inline _LIBCPP_HIDE_FROM_ABI size_t __libcpp_mbrlen_l(const char* __s, size_t __
 #endif // _LIBCPP_HAS_NO_WIDE_CHARACTERS
 
 inline _LIBCPP_HIDE_FROM_ABI lconv* __libcpp_localeconv_l(locale_t __l) {
+  fprintf(stderr, "Entering __libcpp_localeconv_l() in bsd_locale_fallbacks.h\n");
   __locale_guard __current(__l);
   return localeconv();
 }
