bf8575d5b162e1e096e3c75d04e62409b3eed883
c8ef <c8ef@outlook.com>
2024-10-31 00:00:04 +0800

    Update ItaniumDemangle.h

--- libcxxabi/src/demangle/ItaniumDemangle.h
+++ libcxxabi/src/demangle/ItaniumDemangle.h
@@ -1,5 +1,5 @@
-//===--- ItaniumDemangle.h -----------*- mode:c++;eval:(read-only-mode) -*-===//
-//       Do not edit! See README.txt.
+//===------------------------- ItaniumDemangle.h ----------------*- C++ -*-===//
+//
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
