8c421d13ffaf6af529b3151203bdecc3c27a6ff7
Iñaki Amatria Barral <inaki.amatria@appentra.com>
2024-10-31 16:52:19 +0100

    [flang] Prevent errors from being suppressed
    `ModFileReader::Say()` flags all messages as errors, but Flang was
    mistakenly suppressing two errors when the `-w` flag was used, as they
    were incorrectly conditioned to warning suppression. This fix ensures
    that errors are reported regardless of the `-w` flag.
    
    This commit also replaces two uses of `_warn_en_US` with `_err_en_US` to
    prevent potential confusion in the future.

--- flang/lib/Common/Fortran-features.cpp
+++ flang/lib/Common/Fortran-features.cpp
@@ -66,7 +66,6 @@ LanguageFeatureControl::LanguageFeatureControl() {
   warnUsage_.set(UsageWarning::ZeroDoStep);
   warnUsage_.set(UsageWarning::UnusedForallIndex);
   warnUsage_.set(UsageWarning::OpenMPUsage);
-  warnUsage_.set(UsageWarning::ModuleFile);
   warnUsage_.set(UsageWarning::DataLength);
   warnUsage_.set(UsageWarning::IgnoredDirective);
   warnUsage_.set(UsageWarning::HomonymousSpecific);
