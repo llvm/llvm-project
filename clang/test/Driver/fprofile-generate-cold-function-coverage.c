2b5520ca2dd33e2daaa8782eb63dc119c2235167
wlei <wlei@fb.com>
2024-10-31 14:34:35 -0700

    fix test failure

--- clang/test/Driver/fprofile-generate-cold-function-coverage.c
+++ clang/test/Driver/fprofile-generate-cold-function-coverage.c
@@ -1,7 +1,7 @@
 // RUN: %clang -### -c -fprofile-generate-cold-function-coverage %s 2>&1 | FileCheck %s
 // CHECK: "--instrument-cold-function-only-path=default_%m.profraw" 
-// CHECK: "--pgo-function-entry-coverage"
 // CHECK: "--pgo-instrument-cold-function-only"
+// CHECK: "--pgo-function-entry-coverage"
 // CHECK-NOT:  "-fprofile-instrument"
 // CHECK-NOT:  "-fprofile-instrument-path=
 
