c72a751dabff4260dcc309e48008941d51b31d21
Phoebe Wang <phoebe.wang@intel.com>
2024-11-01 16:45:03 +0800

    [X86][AMX] Support AMX-TRANSPOSE (#113532)
    Ref.: https://cdrdv2.intel.com/v1/dl/getContent/671368

--- clang/lib/Headers/immintrin.h
+++ clang/lib/Headers/immintrin.h
@@ -652,6 +652,10 @@ _storebe_i64(void * __P, long long __D) {
 #include <amxfp8intrin.h>
 #endif
 
+#if !defined(__SCE__) || __has_feature(modules) || defined(__AMX_TRANSPOSE__)
+#include <amxtransposeintrin.h>
+#endif
+
 #if !defined(__SCE__) || __has_feature(modules) ||                             \
     defined(__AVX512VP2INTERSECT__)
 #include <avx512vp2intersectintrin.h>
