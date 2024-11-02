01a9941075c223ac71013165d7a9945aec4194f1
Will Hawkins <hawkinsw@obs.cr>
2024-11-02 02:23:47 -0400

    fixup! WIP: [libc++][ranges] Implement `ranges::stride_view`.
    Fix verifier error triggered by bad auto format.

--- libcxx/test/std/ranges/range.adaptors/range.stride.view/iterator/plus_equal.pass.cpp
+++ libcxx/test/std/ranges/range.adaptors/range.stride.view/iterator/plus_equal.pass.cpp
@@ -10,16 +10,16 @@
 
 // constexpr __iterator& operator+=(difference_type __n)
 
-#include <iterator>
 #include <ranges>
+#include <utility>
 
 #include "../types.h"
-#include "__iterator/concepts.h"
-#include "__ranges/concepts.h"
 #include "test_iterators.h"
 
 template <class T>
-concept CanPlusEqual = std::is_same_v<T&, decltype(std::declval<T>() += 1)> && requires(T& t) { t += 1; };
+concept CanPlus =
+    std::is_same_v<T&, decltype(std::declval<T>() += std::declval<typename T::__iterator::difference_type>())> &&
+    requires(T& t, T::__iterator::difference_type& u) { t += u; };
 
 // Make sure that we cannot use += on a stride view iterator
 // over an input view.(sized sentinel)
@@ -27,7 +27,7 @@ using InputView = BasicTestView<cpp17_input_iterator<int*>, sized_sentinel<cpp17
 using StrideViewOverInputViewIterator = std::ranges::iterator_t<std::ranges::stride_view<InputView>>;
 
 static_assert(std::ranges::input_range<InputView>);
-static_assert(!CanPlusEqual<StrideViewOverInputViewIterator>);
+static_assert(!CanPlus<StrideViewOverInputViewIterator>);
 
 // Make sure that we cannot use += on a stride view iterator
 // over a forward view.(sized sentinel)
@@ -35,7 +35,7 @@ using ForwardView                       = BasicTestView<forward_iterator<int*>, 
 using StrideViewOverForwardViewIterator = std::ranges::iterator_t<std::ranges::stride_view<ForwardView>>;
 
 static_assert(std::ranges::forward_range<ForwardView>);
-static_assert(!CanPlusEqual<StrideViewOverForwardViewIterator>);
+static_assert(!CanPlus<StrideViewOverForwardViewIterator>);
 
 // Make sure that we cannot use += on a stride view iterator
 // over a bidirectional view. (sized sentinel)
@@ -43,7 +43,7 @@ using BidirectionalView = BasicTestView<bidirectional_iterator<int*>, sized_sent
 using StrideViewOverBidirectionalViewIterator = std::ranges::iterator_t<std::ranges::stride_view<BidirectionalView>>;
 
 static_assert(std::ranges::bidirectional_range<BidirectionalView>);
-static_assert(!CanPlusEqual<StrideViewOverBidirectionalViewIterator>);
+static_assert(!CanPlus<StrideViewOverBidirectionalViewIterator>);
 
 // Make sure that we can use += on a stride view iterator
 // over a random access view. (non sized sentinel)
@@ -52,11 +52,20 @@ using RandomAccessView                       = BasicTestView<RandomAccessIterato
 using StrideViewOverRandomAccessViewIterator = std::ranges::iterator_t<std::ranges::stride_view<RandomAccessView<>>>;
 
 static_assert(std::ranges::random_access_range<RandomAccessView<>>);
-static_assert(CanPlusEqual<StrideViewOverRandomAccessViewIterator>);
+static_assert(CanPlus<StrideViewOverRandomAccessViewIterator>);
 
-template <std::random_access_iterator Iter, typename Difference>
-constexpr bool test_random_access_operator_plus_equal(Iter begin, Iter end, Difference distance) {
-  // Test the forward-range operator+= between an iterator and its distance type.
+constexpr bool test_random_access_operator_plus_equal() {
+  using Iter = std::vector<int>::iterator;
+  using Diff = Iter::difference_type;
+  std::vector<int> vec{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+  // Test the operator+ between an iterator and its difference type. Pay attention solely to
+  // stride views over random-access ranges because operator+ is not applicable to others.
+
+  auto begin    = vec.begin();
+  auto end      = vec.end();
+  Diff distance = 4;
+
+  // Test the forward-range operator+= between an iterator and its difference type.
   // Do not use the RandomAccessView defined in types.h to give the test user more power
   // to customize an iterator and a default sentinel.
   using Base = RandomAccessView<Iter>;
@@ -88,19 +97,12 @@ constexpr bool test_random_access_operator_plus_equal(Iter begin, Iter end, Diff
 }
 
 consteval bool do_static_tests() {
-  std::vector<int> vec{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
-  assert(test_random_access_operator_plus_equal(vec.begin(), vec.end(), 4));
+  assert(test_random_access_operator_plus_equal());
   return true;
 }
 
 int main(int, char**) {
-  constexpr int arr[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
-  static_assert(test_random_access_operator_plus_equal(arr, arr + 11, 4));
-  test_random_access_operator_plus_equal(arr, arr + 11, 4);
-
   static_assert(do_static_tests());
-
-  std::vector<int> vec{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
-  assert(test_random_access_operator_plus_equal(vec.begin(), vec.end(), 4));
+  assert(do_static_tests());
   return 0;
 }
