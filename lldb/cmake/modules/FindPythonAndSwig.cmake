fb436219e527f4de3cc4ce35eb3b884b824924a3
David Spickett <david.spickett@linaro.org>
2024-11-04 14:41:19 +0000

    [lldb] Recommend Python 3.8 as the minimum Python version for LLDB
    See https://discourse.llvm.org/t/rfc-lets-document-and-enforce-a-minimum-python-version-for-lldb/82731
    for discussions.
    
    This matches LLVM's requirement to run tests. For LLDB 20 there will
    be a CMake warning telling builders that from LLDB 21 this will be
    a hard requirement. From LLDB 21, it will be an error to try to build
    with anything <= 3.8.
    
    So there are no code changes in this commit. Once the llvm 20 branch
    is created we can remove some < 3.8 support code.
    
    As always, if you disable Python support you will not get any new
    warnings or errors from this change.

--- lldb/cmake/modules/FindPythonAndSwig.cmake
+++ lldb/cmake/modules/FindPythonAndSwig.cmake
@@ -64,3 +64,10 @@ else()
                                       Python3_EXECUTABLE
                                       LLDB_ENABLE_SWIG)
 endif()
+
+set(LLDB_RECOMMENDED_PYTHON "3.8")
+if(Python3_LIBRARIES AND Python3_INCLUDE_DIRS AND Python3_EXECUTABLE AND LLDB_ENABLE_SWIG
+   AND "${Python3_VERSION}" VERSION_LESS "${LLDB_RECOMMENDED_PYTHON}")
+  message(WARNING "Using Python ${Python3_VERSION}. ${LLDB_RECOMMENDED_PYTHON} "
+                  "is recommended and will be required from LLDB 21.")
+endif()
