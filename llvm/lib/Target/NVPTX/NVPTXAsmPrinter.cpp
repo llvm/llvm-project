aaade688a43ebd768835d090968f466d74a1dba4
Lewis Crawford <lcrawford@nvidia.com>
2024-11-04 21:43:50 +0000

    Fix clang-format issue in header comment

--- llvm/lib/Target/NVPTX/NVPTXAsmPrinter.cpp
+++ llvm/lib/Target/NVPTX/NVPTXAsmPrinter.cpp
@@ -1,4 +1,4 @@
-///===-- NVPTXAsmPrinter.cpp - NVPTX LLVM assembly writer ------------------===//
+//===-- NVPTXAsmPrinter.cpp - NVPTX LLVM assembly writer ------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
