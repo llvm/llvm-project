8f2b91a62957a54d1a28100abab13d274f57cbc7
Pirog, Mikolaj Maciej <mikolaj.maciej.pirog@intel.com>
2024-11-05 14:53:13 +0100

    Pgu test

--- clang/test/CodeGen/debug-dir-win-pe-pgu-string.c
+++ clang/test/CodeGen/debug-dir-win-pe-pgu-string.c
@@ -1,15 +1,15 @@
 // This test checks if Windows PE file contains a "PGU" string to indicate that
 // it was compiled using profiling data.
 
-// REQUIRES: system-windows
+// RUN: llvm-profdata merge -output=%code.profdata %S/Inputs/thinlto_expect1.proftext
+// RUN: %clang --target=x86_64-pc-windows -fprofile-use=%code.profdata -c %s -o %t.obj
+// RUN: llvm-objdump -h %t.obj | FileCheck --check-prefix=CHECK_PGU %s
 
-// RUN: %clang --target=x86_64-pc-windows-msvc -fprofile-instr-generate="%profdata" -fuse-ld=lld %s -o %t.exe
-// RUN: %t.exe
-// RUN: llvm-profdata merge -output=%code.profdata %profdata
-// RUN: %clang --target=x86_64-pc-windows-msvc -fprofile-use=%code.profdata -fuse-ld=lld %s -o %t.exe
-// RUN: llvm-readobj --coff-debug-directory %t.exe | FileCheck %s
+// RUN: %clang --target=aarch64-windows -fprofile-use=%code.profdata -c %s -o %t.obj
+// RUN: llvm-objdump -h %t.obj | FileCheck --check-prefix=CHECK_PGU %s
 
-// CHECK: {{.*}}ugp{{.*}}
+
+// CHECK_PGU: {{.*}}.pgu{{.*}}
 
 int main(void) {
 
