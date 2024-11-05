bda4e1c4b20819cc88be6940383dd00a7f122505
Kazu Hirata <kazu@google.com>
2024-11-05 09:38:53 -0800

    [WebAssembly] Use heterogenous lookups with std::set (NFC) (#114930)

--- llvm/lib/Target/WebAssembly/WebAssemblyLowerEmscriptenEHSjLj.cpp
+++ llvm/lib/Target/WebAssembly/WebAssemblyLowerEmscriptenEHSjLj.cpp
@@ -318,7 +318,7 @@ class WebAssemblyLowerEmscriptenEHSjLj final : public ModulePass {
   // Map of <function signature string, invoke_ wrappers>
   StringMap<Function *> InvokeWrappers;
   // Set of allowed function names for exception handling
-  std::set<std::string> EHAllowlistSet;
+  std::set<std::string, std::less<>> EHAllowlistSet;
   // Functions that contains calls to setjmp
   SmallPtrSet<Function *, 8> SetjmpUsers;
 
@@ -349,8 +349,8 @@ class WebAssemblyLowerEmscriptenEHSjLj final : public ModulePass {
 
   bool areAllExceptionsAllowed() const { return EHAllowlistSet.empty(); }
   bool supportsException(const Function *F) const {
-    return EnableEmEH && (areAllExceptionsAllowed() ||
-                          EHAllowlistSet.count(std::string(F->getName())));
+    return EnableEmEH &&
+           (areAllExceptionsAllowed() || EHAllowlistSet.count(F->getName()));
   }
   void replaceLongjmpWith(Function *LongjmpF, Function *NewF);
 
