a5bd283fcdbc87dbddd809713898cff222ae4dd4
Pirog, Mikolaj Maciej <mikolaj.maciej.pirog@intel.com>
2024-11-05 15:00:43 +0100

    Consolidate tests to one

--- clang/test/CodeGen/debug-dir-win-pe-magic-sections.c
+++ clang/test/CodeGen/debug-dir-win-pe-magic-sections.c
@@ -1,3 +1,13 @@
+// This test checks if COFF file compiled with
+// -fprofile-generate has magic section ".pgi" to indicate so.
+
+// RUN: %clang --target=x86_64-pc-windows -fprofile-generate %s -c -o %t_x86
+// RUN: llvm-objdump -h %t_x86 | FileCheck --check-prefix=CHECK_PGI %s
+// RUN: %clang --target=aarch64-pc-windows -fprofile-generate %s -c -o %t_aarch
+// RUN: llvm-objdump -h %t_aarch | FileCheck --check-prefix=CHECK_PGI %s
+
+// CHECK_PGI: {{.*}}.pgi{{.*}}
+
 // This test checks if COFF file contains a magic ".pgu" section to indicate that
 // it was compiled using profiling data.
 
@@ -14,4 +24,3 @@ int main(void) {
 
 	return 0;
 }
-	
