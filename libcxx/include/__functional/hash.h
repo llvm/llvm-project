5acc4a3dc0e2145d2bfef47f1543bb291c2b866a
Nikolas Klauser <nikolasklauser@berlin.de>
2024-11-06 10:38:19 +0100

    [libc++] Remove <stddef.h> includes from the granularized <cstddef> headers (#114788)
    We can define some of these aliases without having to include the system
    <stddef.h> and there doesn't seem to be much of a reason we shouldn't do
    it this way.

--- libcxx/include/__functional/hash.h
+++ libcxx/include/__functional/hash.h
@@ -10,6 +10,7 @@
 #define _LIBCPP___FUNCTIONAL_HASH_H
 
 #include <__config>
+#include <__cstddef/nullptr_t.h>
 #include <__functional/unary_function.h>
 #include <__fwd/functional.h>
 #include <__type_traits/conjunction.h>
