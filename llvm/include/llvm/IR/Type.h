bd51d7fcfbfae25293f87c19bc748015a72e39e4
Youngsuk Kim <youngsuk.kim@hpe.com>
2024-11-06 11:50:34 -0500

    [llvm] Deprecate Type::getPointerTo() (#113331)
    `llvm::Type::getPointerTo()` is no longer needed with opaque pointers in LLVM.
    It may rather confuse new contributors to think that LLVM has typed pointers.

--- llvm/include/llvm/IR/Type.h
+++ llvm/include/llvm/IR/Type.h
@@ -472,8 +472,9 @@ public:
   static Type *getWasm_FuncrefTy(LLVMContext &C);
 
   /// Return a pointer to the current type. This is equivalent to
-  /// PointerType::get(Foo, AddrSpace).
+  /// PointerType::get(Ctx, AddrSpace).
   /// TODO: Remove this after opaque pointer transition is complete.
+  LLVM_DEPRECATED("Use PointerType::get instead", "PointerType::get")
   PointerType *getPointerTo(unsigned AddrSpace = 0) const;
 
 private:
