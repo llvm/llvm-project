e79f82754d642a5c6c2adde5958712483e4ebe27
Felipe de Azevedo Piovezan <fpiovezan@apple.com>
2024-11-06 13:28:21 -0800

    Skip tests known to fail on 15.0
    These tests are passing on 15.1: rdar://139217313
    
    (cherry picked from commit fe2ec153f3cef0bac99254fcb4bc67602a11c65c)

--- lldb/test/API/lang/swift/nsarray_code_running_formatter/TestSwiftNSArrayCodeRunningFormatter.py
+++ lldb/test/API/lang/swift/nsarray_code_running_formatter/TestSwiftNSArrayCodeRunningFormatter.py
@@ -12,5 +12,8 @@
 import lldbsuite.test.lldbinline as lldbinline
 from lldbsuite.test.decorators import *
 
-lldbinline.MakeInlineTest(__file__, globals(),
-                          decorators=[swiftTest,skipUnlessDarwin])
+lldbinline.MakeInlineTest(
+    __file__,
+    globals(),
+    decorators=[swiftTest, skipUnlessDarwin, skipIf(macos_version=["=", "15.0"])],
+)
