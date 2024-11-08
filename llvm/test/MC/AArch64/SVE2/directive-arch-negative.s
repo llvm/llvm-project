a76455d1c88c6e6df88038dff99c2afeaaeee6c0
SpencerAbson <Spencer.Abson@arm.com>
2024-11-08 15:07:05 +0000

    [AArch64] Reduce +sve2-aes to an alias of +sve-aes+sve2 (#114293)
    This patch introduces the amended feature flag for
    [FEAT_SVE_AES](https://developer.arm.com/documentation/109697/2024_09/Feature-descriptions/The-Armv9-0-architecture-extension?lang=en#md457-the-armv90-architecture-extension__feat_FEAT_SVE_AES),
    '**sve-aes**'. The existing flag associated with this feature,
    'sve2-aes' must be retained as an alias of 'sve-aes' and 'sve2' for
    backwards compatibility.
    
    The
    [ACLE](https://github.com/ARM-software/acle/blob/main/main/acle.md#aes-extension)
    documents `__ARM_FEATURE_SVE2_AES`, which was previously defined to 1
    when
    
    > there is hardware support for the SVE2 AES (FEAT_SVE_AES) instructions
    and if the associated ACLE intrinsics are available.
    
    The front-end has been amended such that it is compatible with +sve2-aes
    and +sve2+sve-aes.

--- llvm/test/MC/AArch64/SVE2/directive-arch-negative.s
+++ llvm/test/MC/AArch64/SVE2/directive-arch-negative.s
@@ -6,10 +6,10 @@ tbx z0.b, z1.b, z2.b
 // CHECK: error: instruction requires: sve2 or sme
 // CHECK-NEXT: tbx z0.b, z1.b, z2.b
 
-.arch armv9-a+sve2-aes
-.arch armv9-a+nosve2-aes
+.arch armv9-a+sve-aes
+.arch armv9-a+nosve-aes
 aesd z23.b, z23.b, z13.b
-// CHECK: error: instruction requires: sve2-aes
+// CHECK: error: instruction requires: sve-aes
 // CHECK-NEXT: aesd z23.b, z23.b, z13.b
 
 .arch armv9-a+sve2-sm4
