06c1182a17495077d6762cfc585d8e5dbc428673
John Harrison <harjohn@google.com>
2024-11-08 09:11:01 -0800

    Correcting the args to FilterAndGetValueForKey to take a char*.

--- lldb/tools/lldb-dap/JSONUtils.cpp
+++ lldb/tools/lldb-dap/JSONUtils.cpp
@@ -1480,11 +1480,11 @@ CreateRunInTerminalReverseRequest(const llvm::json::Object &launch_request,
 // Keep all the top level items from the statistics dump, except for the
 // "modules" array. It can be huge and cause delay
 // Array and dictionary value will return as <key, JSON string> pairs
-void FilterAndGetValueForKey(const lldb::SBStructuredData data,
-                             llvm::StringRef key, llvm::json::Object &out) {
-  lldb::SBStructuredData value = data.GetValueForKey(key.data());
+void FilterAndGetValueForKey(const lldb::SBStructuredData data, char *key,
+                             llvm::json::Object &out) {
+  lldb::SBStructuredData value = data.GetValueForKey(key);
   std::string key_utf8 = llvm::json::fixUTF8(key);
-  if (key == "modules")
+  if (llvm::StringRef(key) == "modules")
     return;
   switch (value.GetType()) {
   case lldb::eStructuredDataTypeFloat:
