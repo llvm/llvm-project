9c4caf2ad8b13c721d9b64c96b86da4384edeb35
Carlos Gálvez <carlos.galvez@zenseact.com>
2024-11-09 20:46:23 +0000

    Fix review comments

--- clang-tools-extra/test/clang-tidy/checkers/bugprone/throw-keyword-missing.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/bugprone/throw-keyword-missing.cpp
@@ -139,13 +139,14 @@ CtorInitializerListTest::CtorInitializerListTest(float) try : exc(RegularExcepti
   RegularException();
 }
 
-// https://github.com/llvm/llvm-project/issues/115055
+namespace GH115055 {
 class CtorInitializerListTest2 {
  public:
-  CtorInitializerListTest2(){}
+  CtorInitializerListTest2() {}
  private:
   RegularException exc{};
 };
+}  // namespace GH115055
 
 RegularException funcReturningExceptionTest(int i) {
   return RegularException();
