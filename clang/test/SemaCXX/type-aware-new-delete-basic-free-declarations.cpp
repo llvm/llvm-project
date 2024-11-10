80154c2b3441f33d609b52463114b7ee5f662798
Oliver Hunt <oliver@apple.com>
2024-11-09 19:19:37 -0800

    Missed a -fexperimental change and adding flag names to diagnostics

--- clang/test/SemaCXX/type-aware-new-delete-basic-free-declarations.cpp
+++ clang/test/SemaCXX/type-aware-new-delete-basic-free-declarations.cpp
@@ -22,14 +22,14 @@ template <typename T> void *operator new(std::type_identity<TemplateTestType<T>>
 template <typename T, typename U> void *operator new(std::type_identity<T>, size_t, TemplateTestType<U>&); // #7
 template <template <typename> class T> void *operator new(std::type_identity<T<int>>, size_t); // #8
 #if defined(NO_TAA)
-//expected-error@#1 {{type aware allocation operators are disabled}}
-//expected-error@#2 {{type aware allocation operators are disabled}}
-//expected-error@#3 {{type aware allocation operators are disabled}}
-//expected-error@#4 {{type aware allocation operators are disabled}}
-//expected-error@#5 {{type aware allocation operators are disabled}}
-//expected-error@#6 {{type aware allocation operators are disabled}}
-//expected-error@#7 {{type aware allocation operators are disabled}}
-//expected-error@#8 {{type aware allocation operators are disabled}}
+//expected-error@#1 {{type aware allocation operators are disabled, enable with '-fexperimental-cxx-type-aware-allocators'}}
+//expected-error@#2 {{type aware allocation operators are disabled, enable with '-fexperimental-cxx-type-aware-allocators'}}
+//expected-error@#3 {{type aware allocation operators are disabled, enable with '-fexperimental-cxx-type-aware-allocators'}}
+//expected-error@#4 {{type aware allocation operators are disabled, enable with '-fexperimental-cxx-type-aware-allocators'}}
+//expected-error@#5 {{type aware allocation operators are disabled, enable with '-fexperimental-cxx-type-aware-allocators'}}
+//expected-error@#6 {{type aware allocation operators are disabled, enable with '-fexperimental-cxx-type-aware-allocators'}}
+//expected-error@#7 {{type aware allocation operators are disabled, enable with '-fexperimental-cxx-type-aware-allocators'}}
+//expected-error@#8 {{type aware allocation operators are disabled, enable with '-fexperimental-cxx-type-aware-allocators'}}
 #endif
 
 void operator delete(std::type_identity<int>, void *); // #9
@@ -37,10 +37,10 @@ void operator delete(std::type_identity<int>, void *, std::align_val_t); // #10
 void operator delete(std::type_identity<int>, void *, size_t); // #11
 void operator delete(std::type_identity<int>, void *, size_t, std::align_val_t); // #12
 #if defined(NO_TAA)
-//expected-error@#9 {{type aware allocation operators are disabled}}
-//expected-error@#10 {{type aware allocation operators are disabled}}
-//expected-error@#11 {{type aware allocation operators are disabled}}
-//expected-error@#12 {{type aware allocation operators are disabled}}
+//expected-error@#9 {{type aware allocation operators are disabled, enable with '-fexperimental-cxx-type-aware-allocators'}}
+//expected-error@#10 {{type aware allocation operators are disabled, enable with '-fexperimental-cxx-type-aware-allocators'}}
+//expected-error@#11 {{type aware allocation operators are disabled, enable with '-fexperimental-cxx-type-aware-allocators'}}
+//expected-error@#12 {{type aware allocation operators are disabled, enable with '-fexperimental-cxx-type-aware-allocators'}}
 #endif
 
 template <typename T> void operator delete(std::type_identity<T>, void *); // #13
@@ -48,35 +48,35 @@ template <typename T> void operator delete(std::type_identity<T>, void *, std::a
 template <typename T> void operator delete(std::type_identity<T>, void *, size_t); // #15
 template <typename T> void operator delete(std::type_identity<T>, void *, size_t, std::align_val_t); // #16
 #if defined(NO_TAA)
-//expected-error@#13 {{type aware allocation operators are disabled}}
-//expected-error@#14 {{type aware allocation operators are disabled}}
-//expected-error@#15 {{type aware allocation operators are disabled}}
-//expected-error@#16 {{type aware allocation operators are disabled}}
+//expected-error@#13 {{type aware allocation operators are disabled, enable with '-fexperimental-cxx-type-aware-allocators'}}
+//expected-error@#14 {{type aware allocation operators are disabled, enable with '-fexperimental-cxx-type-aware-allocators'}}
+//expected-error@#15 {{type aware allocation operators are disabled, enable with '-fexperimental-cxx-type-aware-allocators'}}
+//expected-error@#16 {{type aware allocation operators are disabled, enable with '-fexperimental-cxx-type-aware-allocators'}}
 #endif
 
 template <typename T> void operator delete(std::type_identity<TemplateTestType<T>>, void *); // #17
 template <template <typename> class T> void operator delete(std::type_identity<T<int>>, void *); // #18
 #if defined(NO_TAA)
-//expected-error@#17 {{type aware allocation operators are disabled}}
-//expected-error@#18 {{type aware allocation operators are disabled}}
+//expected-error@#17 {{type aware allocation operators are disabled, enable with '-fexperimental-cxx-type-aware-allocators'}}
+//expected-error@#18 {{type aware allocation operators are disabled, enable with '-fexperimental-cxx-type-aware-allocators'}}
 #endif
 
 typedef std::type_identity<float> TypeIdentityAlias1;
 void *operator new(TypeIdentityAlias1, size_t); // #19
 #if defined(NO_TAA)
-//expected-error@#19 {{type aware allocation operators are disabled}}
+//expected-error@#19 {{type aware allocation operators are disabled, enable with '-fexperimental-cxx-type-aware-allocators'}}
 #endif
 
 using TypeIdentityAlias2 = std::type_identity<double>;
 void *operator new(TypeIdentityAlias2, size_t); // #20
 #if defined(NO_TAA)
-//expected-error@#20 {{type aware allocation operators are disabled}}
+//expected-error@#20 {{type aware allocation operators are disabled, enable with '-fexperimental-cxx-type-aware-allocators'}}
 #endif
 
 template <typename T> using TypeIdentityAlias3 = std::type_identity<T>;
 template <typename T> void *operator new(TypeIdentityAlias3<T>, size_t); // #21
 #if defined(NO_TAA)
-//expected-error@#21 {{type aware allocation operators are disabled}}
+//expected-error@#21 {{type aware allocation operators are disabled, enable with '-fexperimental-cxx-type-aware-allocators'}}
 #endif
 
 
--- clang/test/SemaCXX/type-aware-new-delete-basic-in-class-declarations.cpp
+++ clang/test/SemaCXX/type-aware-new-delete-basic-in-class-declarations.cpp
@@ -15,8 +15,8 @@ struct S {
   void *operator new(std::type_identity<S>, size_t); // #1
   void operator delete(std::type_identity<S>, void *); // #2
 #if defined(NO_TAA)
-  //expected-error@#1 {{type aware allocation operators are disabled}}
-  //expected-error@#2 {{type aware allocation operators are disabled}}
+  //expected-error@#1 {{type aware allocation operators are disabled, enable with '-fexperimental-cxx-type-aware-allocators'}}
+  //expected-error@#2 {{type aware allocation operators are disabled, enable with '-fexperimental-cxx-type-aware-allocators'}}
 #endif
   void operator delete(S *, std::destroying_delete_t);
 };
@@ -25,8 +25,8 @@ template <typename T> struct S2 {
   void *operator new(std::type_identity<S2<T>>, size_t); // #3
   void operator delete(std::type_identity<S2<T>>, void *); // #4
 #if defined(NO_TAA)
-  //expected-error@#3 {{type aware allocation operators are disabled}}
-  //expected-error@#4 {{type aware allocation operators are disabled}}
+  //expected-error@#3 {{type aware allocation operators are disabled, enable with '-fexperimental-cxx-type-aware-allocators'}}
+  //expected-error@#4 {{type aware allocation operators are disabled, enable with '-fexperimental-cxx-type-aware-allocators'}}
 #endif
   void operator delete(S2 *, std::destroying_delete_t);
 };
@@ -35,8 +35,8 @@ struct S3 {
   template <typename T> void *operator new(std::type_identity<T>, size_t); // #5
   template <typename T> void operator delete(std::type_identity<T>, void *); // #6
 #if defined(NO_TAA)
-  //expected-error@#5 {{type aware allocation operators are disabled}}
-  //expected-error@#6 {{type aware allocation operators are disabled}}
+  //expected-error@#5 {{type aware allocation operators are disabled, enable with '-fexperimental-cxx-type-aware-allocators'}}
+  //expected-error@#6 {{type aware allocation operators are disabled, enable with '-fexperimental-cxx-type-aware-allocators'}}
 #endif
   void operator delete(S3 *, std::destroying_delete_t);
 };
@@ -46,18 +46,18 @@ struct S4 {
   template <typename T> void operator delete(std::type_identity<T>, void *); // #8
   template <typename T> void operator delete(std::type_identity<T>, S4 *, std::destroying_delete_t); // #9
 #if defined(NO_TAA)
-  //expected-error@#7 {{type aware allocation operators are disabled}}
-  //expected-error@#8 {{type aware allocation operators are disabled}}
-  //expected-error@#9 {{type aware allocation operators are disabled}}
+  //expected-error@#7 {{type aware allocation operators are disabled, enable with '-fexperimental-cxx-type-aware-allocators'}}
+  //expected-error@#8 {{type aware allocation operators are disabled, enable with '-fexperimental-cxx-type-aware-allocators'}}
+  //expected-error@#9 {{type aware allocation operators are disabled, enable with '-fexperimental-cxx-type-aware-allocators'}}
 #elif defined(NO_TADD)
-  // expected-error@#9 {{type aware destroying delete is not permitted}}
+  // expected-error@#9 {{type aware destroying delete is not permitted, enable with '-fexperimental-cxx-type-aware-destroying-delete'}}
 #endif
 };
 
 struct S5 {
   template <typename T> void operator delete(std::type_identity<T>, T *); // #10
 #if defined(NO_TAA)
-  // expected-error@#10 {{type aware allocation operators are disabled}}
+  // expected-error@#10 {{type aware allocation operators are disabled, enable with '-fexperimental-cxx-type-aware-allocators'}}
 #else
   // expected-error@#10 {{'operator delete' cannot take a dependent type as first parameter; use 'void *'}}
 #endif
