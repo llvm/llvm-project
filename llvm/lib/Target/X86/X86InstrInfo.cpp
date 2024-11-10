6e6084103d8464fafac0c79e07b649c870a9ccaf
Phoebe Wang <phoebe.wang@intel.com>
2024-11-10 12:46:21 +0800

    Fix mistakes in #113532 (#115631)
    Found during review #115151

--- llvm/lib/Target/X86/X86InstrInfo.cpp
+++ llvm/lib/Target/X86/X86InstrInfo.cpp
@@ -4737,6 +4737,7 @@ static bool isAMXOpcode(unsigned Opc) {
   case X86::TILELOADD_EVEX:
   case X86::TILESTORED_EVEX:
   case X86::PTILEPAIRLOAD:
+  case X86::PTILEPAIRSTORE:
     return true;
   }
 }
