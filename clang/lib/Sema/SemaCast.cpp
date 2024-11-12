0c6e54b63dfee5aba53492045f94239086634479
Oleksandr T <oleksandr.tarasiuk@outlook.com>
2024-11-12 23:14:47 +0200

    fix formatting

--- clang/lib/Sema/SemaCast.cpp
+++ clang/lib/Sema/SemaCast.cpp
@@ -3283,8 +3283,8 @@ void CastOperation::CheckBuiltinBitCast() {
   CharUnits SourceSize = Self.Context.getTypeSizeInChars(SrcType);
   if (DestSize != SourceSize) {
     Self.Diag(OpRange.getBegin(), diag::err_bit_cast_type_size_mismatch)
-      << SrcType << (int)SourceSize.getQuantity()
-      << DestType << (int)DestSize.getQuantity();
+        << SrcType << (int)SourceSize.getQuantity() << DestType
+        << (int)DestSize.getQuantity();
     SrcExpr = ExprError();
     return;
   }
