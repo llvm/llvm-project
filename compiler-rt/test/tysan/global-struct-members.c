9a2c70c7e6c8e78116068b729d129f152bc398b6
gbMattN <matthew.nagy@sony.com>
2024-11-12 16:52:27 +0000

    [style] Tidied up comments, formatting, and polished test

--- compiler-rt/test/tysan/global-struct-members.c
+++ compiler-rt/test/tysan/global-struct-members.c
@@ -1,5 +1,5 @@
 // RUN: %clang_tysan -O0 %s -o %t && %run %t >%t.out 2>&1
-// RUN: FileCheck %s < %t.out
+// RUN: FileCheck %s --implicit-check-not ERROR < %t.out
 
 #include <stdio.h>
 
@@ -15,7 +15,6 @@ int main() {
   x.c = 3;
 
   printf("%d %d %d\n", x.a, x.b, x.c);
-  // CHECK-NOT: ERROR: TypeSanitizer: type-aliasing-violation
 
   for (size_t i = 0; i < 2; i++) {
     xArray[i].a = 1;
@@ -26,6 +25,6 @@ int main() {
   struct X *xPtr = (struct X *)&(xArray[0].c);
   xPtr->a = 1;
   // CHECK: ERROR: TypeSanitizer: type-aliasing-violation
-  // CHECK: WRITE of size 4 at {{.*}} with type int (in X at offset 0) accesses an existing object of type int (in X at offset 8)
-  // CHECK: {{#0 0x.* in main .*struct-members.c:}}[[@LINE-3]]
+  // CHECK-NEXT: WRITE of size 4 at {{.*}} with type int (in X at offset 0) accesses an existing object of type int (in X at offset 8)
+  // CHECK-NEXT: #0 0x{{.*}} in main {{.*}}struct-members.c:[[@LINE-3]]
 }
