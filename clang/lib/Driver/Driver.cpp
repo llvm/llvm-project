86e5163d2403cebb179815ea8ee012f13d273efd
Tarun Prabhu <tarunprabhu@gmail.com>
2024-11-12 15:48:07 -0700

    Revert "[clang][flang] Support -time in both clang and flang"
    This reverts commit f5396748c7da3d9f278fcd42e2a10a3214920d82.

--- clang/lib/Driver/Driver.cpp
+++ clang/lib/Driver/Driver.cpp
@@ -1315,9 +1315,6 @@ Compilation *Driver::BuildCompilation(ArrayRef<const char *> ArgList) {
   // Ignore -pipe.
   Args.ClaimAllArgs(options::OPT_pipe);
 
-  // Ignore -time.
-  Args.ClaimAllArgs(options::OPT_time);
-
   // Extract -ccc args.
   //
   // FIXME: We need to figure out where this behavior should live. Most of it
