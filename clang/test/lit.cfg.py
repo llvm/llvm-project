fe543785c330a9da7c1392d3ab9f4784f03c1a8a
Elvina Yakubova <eyakubova@nvidia.com>
2024-11-12 08:45:46 -0800

    Make stricter rule for mcpu native test

--- clang/test/lit.cfg.py
+++ clang/test/lit.cfg.py
@@ -211,8 +211,8 @@ config.substitutions.append(("%host_cc", config.host_cc))
 config.substitutions.append(("%host_cxx", config.host_cxx))
 
 # Determine whether the test target is compatible with execution on the host.
-if config.host_arch in config.target_triple:
-    config.available_features.add("host-target-compatible")
+if "aarch64" in config.host_arch:
+    config.available_features.add("aarch64-host")
 
 # Plugins (loadable modules)
 if config.has_plugins and config.llvm_plugin_ext:
