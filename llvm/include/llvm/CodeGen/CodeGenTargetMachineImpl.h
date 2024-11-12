4182d0957107d009128f5d1dfa1a0559efb46663
matinraayai <30674652+matinraayai@users.noreply.github.com>
2024-11-11 19:40:56 -0500

    Moved the LLVMTargetMachine::reset() to CodeGenTargetMachineImpl.

--- llvm/include/llvm/CodeGen/CodeGenTargetMachineImpl.h
+++ llvm/include/llvm/CodeGen/CodeGenTargetMachineImpl.h
@@ -29,6 +29,9 @@ protected: // Can only create subclasses.
 
   void initAsmInfo();
 
+  /// Reset internal state.
+  virtual void reset() {};
+
 public:
   /// Get a TargetTransformInfo implementation for the target.
   ///
