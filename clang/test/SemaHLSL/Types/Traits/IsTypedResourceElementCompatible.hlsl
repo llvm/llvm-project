90a73b81e06e923fbbe7631a586a3e8ebecee545
Joshua Batista <jbatista@microsoft.com>
2024-11-13 14:32:50 -0800

    add more test cases

--- clang/test/SemaHLSL/Types/Traits/IsTypedResourceElementCompatible.hlsl
+++ clang/test/SemaHLSL/Types/Traits/IsTypedResourceElementCompatible.hlsl
@@ -13,8 +13,21 @@ struct s {
     int x;
 };
 
+struct Empty {};
+
+template<typename T> struct TemplatedBuffer {
+    T a;
+};
+
+template<typename T> struct TemplatedVector {
+    vector<T, 4> v;
+};
+
 // structs not allowed
 _Static_assert(!__builtin_hlsl_is_typed_resource_element_compatible(s), "");
+_Static_assert(!__builtin_hlsl_is_typed_resource_element_compatible(Empty), "");
+_Static_assert(!__builtin_hlsl_is_typed_resource_element_compatible(TemplatedBuffer<int>), "");
+_Static_assert(!__builtin_hlsl_is_typed_resource_element_compatible(TemplatedVector<int>), "");
 
 // arrays not allowed
 _Static_assert(!__builtin_hlsl_is_typed_resource_element_compatible(half[4]), "");
@@ -22,6 +35,9 @@ _Static_assert(!__builtin_hlsl_is_typed_resource_element_compatible(half[4]), ""
 typedef vector<int, 8> int8;
 // too many elements
 _Static_assert(!__builtin_hlsl_is_typed_resource_element_compatible(int8), "");
+
+typedef int MyInt;
+_Static_assert(__builtin_hlsl_is_typed_resource_element_compatible(MyInt), "");
 
 // size exceeds 16 bytes, and exceeds element count limit after splitting 64 bit types into 32 bit types
 _Static_assert(!__builtin_hlsl_is_typed_resource_element_compatible(double3), "");
