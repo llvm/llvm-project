e428a45e9638d0d310bd21e46acf4175b09815c9
Alex Richardson <alexrichardson@google.com>
2024-11-12 16:24:08 -0800

    [𝘀𝗽𝗿] initial version
    Created using spr 1.3.6-beta.1

--- clang/lib/Driver/ToolChains/Clang.cpp
+++ clang/lib/Driver/ToolChains/Clang.cpp
@@ -1347,6 +1347,7 @@ static bool isSignedCharDefault(const llvm::Triple &Triple) {
   case llvm::Triple::riscv64:
   case llvm::Triple::systemz:
   case llvm::Triple::xcore:
+  case llvm::Triple::xtensa:
     return false;
   }
 }
