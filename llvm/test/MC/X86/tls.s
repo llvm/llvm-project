e3f67f69be29881b709b44c0220d9ab00a5ad01f
Feng Zou <feng.zou@intel.com>
2024-11-13 10:43:33 +0800

    [X86][MC] Add R_X86_64_CODE_4_GOTTPOFF
    For
    
      mov name@GOTTPOFF(%rip), %reg
      add name@GOTTPOFF(%rip), %reg
    
    add
    
      `R_X86_64_CODE_4_GOTTPOFF` = 44
    
    if the instruction starts at 4 bytes before the relocation offset. It's similar
    to R_X86_64_GOTTPOFF.
    
    Linker can treat `R_X86_64_CODE_4_GOTTPOFF` as `R_X86_64_GOTTPOFF` or
    convert the instructions above to
    
      mov $name@tpoff, %reg
      add $name@tpoff, %reg
    
    if the first byte of the instruction at the relocation `offset - 4` is `0xd5`
    (namely, encoded w/REX2 prefix) when possible.

---
+++ llvm/test/MC/X86/tls.s
@@ -0,0 +1,40 @@
+// RUN: llvm-mc -filetype=obj -triple=x86_64 %s -o %t
+// RUN: llvm-objdump -dr --no-print-imm-hex %t | FileCheck %s
+
+// CHECK:      <_start>:
+// CHECK-NEXT:   movq (%rip), %r16
+// CHECK-NEXT:   R_X86_64_CODE_4_GOTTPOFF    tls0-0x4
+// CHECK-NEXT:   movq (%rip), %r20
+// CHECK-NEXT:   R_X86_64_CODE_4_GOTTPOFF    tls0-0x4
+// CHECK-NEXT:   movq (%rip), %r16
+// CHECK-NEXT:   R_X86_64_CODE_4_GOTTPOFF    tls1-0x4
+// CHECK-NEXT:   addq (%rip), %r16
+// CHECK-NEXT:   R_X86_64_CODE_4_GOTTPOFF    tls0-0x4
+// CHECK-NEXT:   addq (%rip), %r28
+// CHECK-NEXT:   R_X86_64_CODE_4_GOTTPOFF    tls0-0x4
+// CHECK-NEXT:   addq (%rip), %r16
+// CHECK-NEXT:   R_X86_64_CODE_4_GOTTPOFF    tls1-0x4
+
+.type tls0,@object
+.section .tbss,"awT",@nobits
+.globl tls0
+.align 4
+tls0:
+.long 0
+.size tls0, 4
+.type  tls1,@object
+.globl tls1
+.align 4
+tls1:
+.long 0
+.size tls1, 4
+.section .text
+.globl _start
+_start:
+    # EGPR
+    movq tls0@GOTTPOFF(%rip), %r16
+    movq tls0@GOTTPOFF(%rip), %r20
+    movq tls1@GOTTPOFF(%rip), %r16
+    addq tls0@GOTTPOFF(%rip), %r16
+    addq tls0@GOTTPOFF(%rip), %r28
+    addq tls1@GOTTPOFF(%rip), %r16
