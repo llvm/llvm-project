862f42eedf21cc28f4bc692ab846c87b28b5960b
Sam Elliott <quic_aelliott@quicinc.com>
2024-11-14 12:31:31 +0000

    [TargetLowering] Use Correct VT for Multi-out Asm (#116024)
    This was overlooked in 7d940432c46be83b8fcb5dbefee439585fa820cd - when
    inline assembly has multiple outputs, they are returned as members of a
    struct, and the `getAsmOperandType` needs to be called for each member
    of struct. The difference between this and the single-output case is
    that in the latter, there isn't a struct wrapping the outputs.
    
    I noticed this when trying to use the same mechanism in the RISC-V
    backend.
    
    Committing two tests:
    - One that shows a crash before this change, which is fixed by this
    change.
    - One (commented out) that shows a different crash with tied
    inputs/outputs. This is commented as it is not fixed by this change and
    needs more work in target-independent inline asm handling code.

--- llvm/lib/CodeGen/SelectionDAG/TargetLowering.cpp
+++ llvm/lib/CodeGen/SelectionDAG/TargetLowering.cpp
@@ -5753,7 +5753,8 @@ TargetLowering::ParseConstraints(const DataLayout &DL,
       assert(!Call.getType()->isVoidTy() && "Bad inline asm!");
       if (auto *STy = dyn_cast<StructType>(Call.getType())) {
         OpInfo.ConstraintVT =
-            getSimpleValueType(DL, STy->getElementType(ResNo));
+            getAsmOperandValueType(DL, STy->getElementType(ResNo))
+                .getSimpleVT();
       } else {
         assert(ResNo == 0 && "Asm only has one result!");
         OpInfo.ConstraintVT =
