dbe2b1f22e8e6031293af5e41480ff58aadab6d1
Spencer Abson <Spencer.Abson@arm.com>
2024-11-14 15:56:25 +0000

    [NFC] fix typo

--- llvm/test/MC/AArch64/SVE2p2/directive-cpu.s
+++ llvm/test/MC/AArch64/SVE2p2/directive-cpu.s
@@ -1,6 +1,6 @@
 // RUN: llvm-mc -triple aarch64 -filetype asm -o - %s 2>&1 | FileCheck %s
 
-// SVE2p2 should implu SVE2p1
+// SVE2p2 should imply SVE2p1
 .cpu generic+sve2p2
 sclamp z0.s, z1.s, z2.s
 // CHECK: sclamp z0.s, z1.s, z2.s
