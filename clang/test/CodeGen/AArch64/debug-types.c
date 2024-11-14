06268230d6a9e4e691c85e0eba79d515a897b6b5
Caroline Concatto <caroline.concatto@arm.com>
2024-11-14 10:33:29 +0000

    Remove Neon header from scalar tests

--- clang/test/CodeGen/AArch64/debug-types.c
+++ clang/test/CodeGen/AArch64/debug-types.c
@@ -1,9 +1,7 @@
-// RUN:  %clang_cc1 -triple aarch64-none-linux-gnu -target-feature +neon  -target-feature +fp8 \
+// RUN:  %clang_cc1 -triple aarch64-none-linux-gnu -target-feature -fp8 \
 // RUN:  -emit-llvm -o - %s -debug-info-kind=limited 2>&1 | FileCheck %s
 
 // REQUIRES: aarch64-registered-target
-
-#include<arm_neon.h>
 
 void test_locals(void) {
   // CHECK-DAG: !DIDerivedType(tag: DW_TAG_typedef, name: "__mfp8", {{.*}}, baseType: ![[ELTTYU8:[0-9]+]]
