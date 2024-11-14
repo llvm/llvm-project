659b628a12e05610ff82421dd358292c53940e93
Piyou Chen <piyou.chen@sifive.com>
2024-11-13 22:28:30 -0800

    in this target -> on this target

--- clang/test/Sema/attr-target-version-unsupported.c
+++ clang/test/Sema/attr-target-version-unsupported.c
@@ -1,4 +1,4 @@
 // RUN: %clang_cc1 -triple x86_64-unknown-unknown  -fsyntax-only -verify %s
 
-//expected-error@+1 {{target_version attribute is not supported in this target}}
+//expected-error@+1 {{target_version attribute is not supported on this target}}
 int __attribute__((target_version("aes"))) foo(void) { return 3; }
