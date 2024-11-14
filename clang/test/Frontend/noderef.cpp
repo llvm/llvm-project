c46fabd4d9bd1dd8061c5500693a31cde2382b8c
Aaron Ballman <aaron@aaronballman.com>
2024-11-14 09:18:24 -0500

    No longer assert when using noderef on an _Atomic type
    Fixes #116124

--- clang/test/Frontend/noderef.cpp
+++ clang/test/Frontend/noderef.cpp
@@ -183,3 +183,9 @@ int *const_cast_check(NODEREF const int *x) {
 const int *const_cast_check(NODEREF int *x) {
   return const_cast<const int *>(x); // expected-warning{{casting to dereferenceable pointer removes 'noderef' attribute}}
 }
+
+namespace GH116124 {
+// This declaration would previously cause a failed assertion.
+int *_Atomic a __attribute__((noderef)); // expected-note {{declared here}}
+int x = *a;                              // expected-warning{{dereferencing a; was declared with a 'noderef' type}}
+}
