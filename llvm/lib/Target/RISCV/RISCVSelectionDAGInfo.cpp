b445f91a3763ca6507f87831dbe533fd9a19434a
Sergei Barannikov <barannikov88@gmail.com>
2024-11-16 01:02:20 +0300

    RISCV

--- llvm/lib/Target/RISCV/RISCVSelectionDAGInfo.cpp
+++ llvm/lib/Target/RISCV/RISCVSelectionDAGInfo.cpp
@@ -7,18 +7,39 @@
 //===----------------------------------------------------------------------===//
 
 #include "RISCVSelectionDAGInfo.h"
-#include "RISCVISelLowering.h"
+
+#define GET_SDNODE_DESC
+#include "RISCVGenSDNodeInfo.inc"
 
 using namespace llvm;
 
+RISCVSelectionDAGInfo::RISCVSelectionDAGInfo()
+    : SelectionDAGGenTargetInfo(RISCVGenSDNodeInfo) {}
+
 RISCVSelectionDAGInfo::~RISCVSelectionDAGInfo() = default;
 
-bool RISCVSelectionDAGInfo::isTargetMemoryOpcode(unsigned Opcode) const {
-  return Opcode >= RISCVISD::FIRST_MEMORY_OPCODE &&
-         Opcode <= RISCVISD::LAST_MEMORY_OPCODE;
+const char *RISCVSelectionDAGInfo::getTargetNodeName(unsigned Opcode) const {
+#define NODE_NAME_CASE(NODE)                                                   \
+  case RISCVISD::NODE:                                                         \
+    return "RISCVISD::" #NODE;
+
+  // These nodes don't have corresponding entries in *.td files yet.
+  switch (static_cast<RISCVISD::NodeType>(Opcode)) {
+    NODE_NAME_CASE(BuildGPRPair)
+    NODE_NAME_CASE(SplitGPRPair)
+    NODE_NAME_CASE(SPLAT_VECTOR_SPLIT_I64_VL)
+    NODE_NAME_CASE(TUPLE_INSERT)
+    NODE_NAME_CASE(TUPLE_EXTRACT)
+  }
+#undef NODE_NAME_CASE
+
+  return SelectionDAGGenTargetInfo::getTargetNodeName(Opcode);
 }
 
-bool RISCVSelectionDAGInfo::isTargetStrictFPOpcode(unsigned Opcode) const {
-  return Opcode >= RISCVISD::FIRST_STRICTFP_OPCODE &&
-         Opcode <= RISCVISD::LAST_STRICTFP_OPCODE;
+void RISCVSelectionDAGInfo::verifyTargetNode(const SelectionDAG &DAG,
+                                             const SDNode *N) const {
+  // invalid number of results; expected 2, got 1
+  if (N->getOpcode() == RISCVISD::PROBED_ALLOCA)
+    return;
+  SelectionDAGGenTargetInfo::verifyTargetNode(DAG, N);
 }
