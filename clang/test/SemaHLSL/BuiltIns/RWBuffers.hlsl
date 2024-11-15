c2fc27b2c8385f5ccb8cc21db2cf5681fd427d02
Joshua Batista <jbatista@microsoft.com>
2024-11-14 17:00:58 -0800

    [HLSL] Add implicit resource element type concepts to AST (#112600)
    This PR is step one on the journey to implement resource element type
    validation via C++20 concepts. The PR sets up the infrastructure for
    injecting implicit concept decls / concept specialization expressions
    into the AST, which will then be evaluated after template arguments are
    instantiated. This is not meant to be a complete implementation of the
    desired validation for HLSL,
    there are a couple of missing elements:
    1. We need the __builtin_hlsl_is_typed_resource_element_compatible
    builtin to be implemented.
    2. We need other constraints, like is_intangible
    3. We need to put the first 2 points together, and construct a finalized
    constraint expression, which should differ between typed and raw buffers
    
    This is just an initial PR that puts some of the core infrastructure in
    place.

--- clang/test/SemaHLSL/BuiltIns/RWBuffers.hlsl
+++ clang/test/SemaHLSL/BuiltIns/RWBuffers.hlsl
@@ -5,12 +5,23 @@ typedef vector<float, 3> float3;
 RWBuffer<float3> Buffer;
 
 // expected-error@+2 {{class template 'RWBuffer' requires template arguments}}
-// expected-note@*:* {{template declaration from hidden source: template <class element_type> class RWBuffer}}
+// expected-note@*:* {{template declaration from hidden source: template <typename element_type> requires __is_typed_resource_element_compatible<element_type> class RWBuffer {}}}
 RWBuffer BufferErr1;
 
 // expected-error@+2 {{too few template arguments for class template 'RWBuffer'}}
-// expected-note@*:* {{template declaration from hidden source: template <class element_type> class RWBuffer}}
+// expected-note@*:* {{template declaration from hidden source: template <typename element_type> requires __is_typed_resource_element_compatible<element_type> class RWBuffer {}}}
 RWBuffer<> BufferErr2;
+
+struct threeDoubles {
+  double a;
+  double b;
+  double c;
+};
+
+// expected-error@+3 {{constraints not satisfied for class template 'RWBuffer'}}
+// expected-note@*:* {{because 'threeDoubles' does not satisfy '__is_typed_resource_element_compatible'}}
+// expected-note@*:* {{because 'sizeof(threeDoubles) <= 16UL' (24 <= 16) evaluated to false}}
+RWBuffer<threeDoubles> BufferErr3;
 
 [numthreads(1,1,1)]
 void main() {
