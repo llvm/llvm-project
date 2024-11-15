07558736d552dfea4eb7c53217c632559224e5c3
Anchu Rajendran S <asudhaku@amd.com>
2024-11-15 09:10:36 -0800

    [Flang][OpenMP][Sema] Adding parsing  and semantic support for scan directive. (#102792)

--- flang/lib/Parser/unparse.cpp
+++ flang/lib/Parser/unparse.cpp
@@ -2393,6 +2393,9 @@ public:
     case llvm::omp::Directive::OMPD_barrier:
       Word("BARRIER ");
       break;
+    case llvm::omp::Directive::OMPD_scan:
+      Word("SCAN ");
+      break;
     case llvm::omp::Directive::OMPD_taskwait:
       Word("TASKWAIT ");
       break;
