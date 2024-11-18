1183350b41f2ef35d38fd8203baa94ade1bc7528
Kazu Hirata <kazu@google.com>
2024-11-18 14:09:21 -0800

    [memprof] Add another constructor to IndexedAllocationInfo (NFC) (#116684)
    This patch adds another constructor to IndexedAllocationInfo that is
    identical to the existing constructor except that the new one leaves
    the CallStack field empty.
    
    I'm planning to remove MemProf format Version 1.  Then we will migrate
    the users of the existing constructor to the new one as nobody will be
    using the CallStack field anymore.
    
    Adding the new constructor now allows us to migrate a few existing
    users of the old constructor even before we remove the CallStack
    field.  In turn, that simplifies the patch to actually remove the
    field.

--- llvm/unittests/ProfileData/InstrProfTest.cpp
+++ llvm/unittests/ProfileData/InstrProfTest.cpp
@@ -415,8 +415,7 @@ makeRecordV2(std::initializer_list<::llvm::memprof::CallStackId> AllocFrames,
   for (const auto &CSId : AllocFrames)
     // We don't populate IndexedAllocationInfo::CallStack because we use it only
     // in Version1.
-    MR.AllocSites.emplace_back(::llvm::SmallVector<memprof::FrameId>(), CSId,
-                               Block, Schema);
+    MR.AllocSites.emplace_back(CSId, Block, Schema);
   for (const auto &CSId : CallSiteFrames)
     MR.CallSiteIds.push_back(CSId);
   return MR;
