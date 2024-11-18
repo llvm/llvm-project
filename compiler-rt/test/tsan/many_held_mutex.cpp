61954f9b2526a5e0155c5a13d69fd52072215a84
gbMattN <matthew.nagy@sony.com>
2024-11-18 10:32:32 +0000

    Fixed the test and its formatting

--- compiler-rt/test/tsan/many_held_mutex.cpp
+++ compiler-rt/test/tsan/many_held_mutex.cpp
@@ -1,22 +1,22 @@
-// RUN: %clangxx_tsan %s -fsanitize=thread -o %t && %run %t 2>&1 | Filecheck %s
+// RUN: %clangxx_tsan %s -fsanitize=thread -o %t && %run %t 2>&1 | FileCheck %s
 
 #include <mutex>
 #include <stdio.h>
 
-int main(){
-    const unsigned short NUM_OF_MTX = 128;
-    std::mutex mutexes[NUM_OF_MTX];
+int main() {
+  const unsigned short NUM_OF_MTX = 128;
+  std::mutex mutexes[NUM_OF_MTX];
 
-    for(int i = 0; i < NUM_OF_MTX; i++){
-        mutexes[i].lock();
-    }
-    for(int i = 0; i < NUM_OF_MTX; i++){
-        mutexes[i].unlock();
-    }
+  for (int i = 0; i < NUM_OF_MTX; i++) {
+    mutexes[i].lock();
+  }
+  for (int i = 0; i < NUM_OF_MTX; i++) {
+    mutexes[i].unlock();
+  }
 
-    printf("Success\n");
+  printf("Success\n");
 
-    return 0;
+  return 0;
 }
 
 // CHECK: Success
