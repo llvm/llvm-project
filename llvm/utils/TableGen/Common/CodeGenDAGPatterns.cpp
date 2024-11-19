3d12f45e50b68ac908ef05571e5cc52f4b966d94
Yingwei Zheng <dtcxzyw2333@gmail.com>
2024-11-19 21:24:40 +0800

    [SDAG][ISel][TableGen][LoongArch] Report error for trivial bitcasts when there are predicate calls (#116075)
    On loongarch64 with lsx extension, we select `VBITREV_W` for `v4i32 (xor
    X, (shl splat(1), Y))`:
    
    https://github.com/llvm/llvm-project/blob/8e6630391699116641cf390a10476295b7d4b95c/llvm/lib/Target/LoongArch/LoongArchLSXInstrInfo.td#L1583-L1584
    
    And `vsplat_imm_eq_1` is defined as:
    
    https://github.com/llvm/llvm-project/blob/8e6630391699116641cf390a10476295b7d4b95c/llvm/lib/Target/LoongArch/LoongArchLSXInstrInfo.td#L77-L87
    
    For the `(bitconvert (v4i32 (build_vector)))` case, the pattern is
    expected to be:
    ```
    PATTERN: (xor:{ *:[v4i32] } v4i32:{ *:[v4i32] }:$vj, (shl:{ *:[v4i32] } (bitconvert:{ *:[v4i32] } (build_vector:{ *:[v4i32] }))<<P:Predicate_vsplat_imm_eq_1>>, v4i32:{ *:[v4i32] }:$vk))
    RESULT:  (VBITREV_W:{ *:[v4i32] } v4i32:{ *:[v4i32] }:$vj, v4i32:{ *:[v4i32] }:$vk)
    ```
    
    However, `simplifyTree` drops the `bitconvert` node and its predicates:
    
    https://github.com/llvm/llvm-project/blob/8e6630391699116641cf390a10476295b7d4b95c/llvm/utils/TableGen/Common/CodeGenDAGPatterns.cpp#L3036-L3062
    
    Then llvm will match `vsplat_imm_eq_1` for any v4i32 splats and cause a
    miscompilation:
    ```
    PATTERN: (xor:{ *:[v4i32] } v4i32:{ *:[v4i32] }:$vj, (shl:{ *:[v4i32] } (build_vector:{ *:[v4i32] }), v4i32:{ *:[v4i32] }:$vk))
    RESULT:  (VBITREV_W:{ *:[v4i32] } v4i32:{ *:[v4i32] }:$vj, v4i32:{ *:[v4i32] }:$vk)
    ```
    
    This patch adds additional checks for predicates associated with the
    trivial bitconvert node. Unused patterns in the LoongArch target are
    also removed.
    
    Fixes https://github.com/llvm/llvm-project/issues/116008.
    
    (cherry picked from commit c727b48287cc96888f9e262f23d53cf635cf3b3d)

--- llvm/utils/TableGen/Common/CodeGenDAGPatterns.cpp
+++ llvm/utils/TableGen/Common/CodeGenDAGPatterns.cpp
@@ -3042,6 +3042,14 @@ static bool SimplifyTree(TreePatternNodePtr &N) {
       !N->getExtType(0).empty() &&
       N->getExtType(0) == N->getChild(0).getExtType(0) &&
       N->getName().empty()) {
+    if (!N->getPredicateCalls().empty()) {
+      std::string Str;
+      raw_string_ostream OS(Str);
+      OS << *N
+         << "\n trivial bitconvert node should not have predicate calls\n";
+      PrintFatalError(Str);
+      return false;
+    }
     N = N->getChildShared(0);
     SimplifyTree(N);
     return true;
