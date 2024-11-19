2796789caf5921dfe9c8d3549b9bf521ce74da7f
Mehdi Amini <joker.eph@gmail.com>
2024-11-19 15:28:17 +0100

    Revert "[MLIR] Add callback functions for ModuleToObject" (#116811)
    Reverts llvm/llvm-project#116007
    
    Bot is broken.

--- mlir/lib/Target/LLVM/NVVM/Target.cpp
+++ mlir/lib/Target/LLVM/NVVM/Target.cpp
@@ -572,9 +572,6 @@ NVPTXSerializer::moduleToObject(llvm::Module &llvmModule) {
     getOperation().emitError() << "Failed translating the module to ISA.";
     return std::nullopt;
   }
-  if (isaCallback)
-    isaCallback(serializedISA.value());
-
 #define DEBUG_TYPE "serialize-to-isa"
   LLVM_DEBUG({
     llvm::dbgs() << "PTX for module: " << getOperation().getNameAttr() << "\n";
