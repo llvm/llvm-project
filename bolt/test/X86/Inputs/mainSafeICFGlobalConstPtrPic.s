39c79029a60b07646bcd764b469057a05f3a659e
Alexander Yermolovich <ayermolo@meta.com>
2024-11-19 17:59:05 -0800

    updated tests

--- bolt/test/X86/Inputs/mainSafeICFGlobalConstPtrPic.s
+++ bolt/test/X86/Inputs/mainSafeICFGlobalConstPtrPic.s
@@ -3,14 +3,6 @@
 # extern int FooVar;
 # extern int BarVar;
 # [[clang::noinline]]
-# MY_CONST int fooSub(int a, int b) {
-#   return a - b;
-# }
-# [[clang::noinline]]
-# MY_CONST int barSub(int a, int b) {
-#   return a - b;
-# }
-# [[clang::noinline]]
 # MY_CONST int fooMul(int a, int b) {
 #   return a * b;
 # }
@@ -42,58 +34,13 @@
 # MY_CONST int (*MY_CONST funcGlobalBarMul)(int, int) = barMul;
 # int main(int argc, char **argv) {
 #   int temp = helper1(funcGlobalBarAdd, FooVar, BarVar) +
-#              helper2(fooMul, funcGlobalBarMul, FooVar, BarVar) + fooSub(FooVar, BarVar) +
-#              barSub(FooVar, BarVar) + fooAdd(FooVar, BarVar);
+#              helper2(fooMul, funcGlobalBarMul, FooVar, BarVar) +
+#              fooAdd(FooVar, BarVar);
 #   MY_PRINTF("val: %d", temp);
 #   return temp;
 # }
-
 	.text
 	.file	"main.cpp"
-	.globl	_Z6fooSubii                     # -- Begin function _Z6fooSubii
-	.p2align	4, 0x90
-	.type	_Z6fooSubii,@function
-_Z6fooSubii:                            # @_Z6fooSubii
-	.cfi_startproc
-# %bb.0:
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset %rbp, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register %rbp
-	movl	%edi, -4(%rbp)
-	movl	%esi, -8(%rbp)
-	movl	-4(%rbp), %eax
-	subl	-8(%rbp), %eax
-	popq	%rbp
-	.cfi_def_cfa %rsp, 8
-	retq
-.Lfunc_end0:
-	.size	_Z6fooSubii, .Lfunc_end0-_Z6fooSubii
-	.cfi_endproc
-                                        # -- End function
-	.globl	_Z6barSubii                     # -- Begin function _Z6barSubii
-	.p2align	4, 0x90
-	.type	_Z6barSubii,@function
-_Z6barSubii:                            # @_Z6barSubii
-	.cfi_startproc
-# %bb.0:
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset %rbp, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register %rbp
-	movl	%edi, -4(%rbp)
-	movl	%esi, -8(%rbp)
-	movl	-4(%rbp), %eax
-	subl	-8(%rbp), %eax
-	popq	%rbp
-	.cfi_def_cfa %rsp, 8
-	retq
-.Lfunc_end1:
-	.size	_Z6barSubii, .Lfunc_end1-_Z6barSubii
-	.cfi_endproc
-                                        # -- End function
 	.globl	_Z6fooMulii                     # -- Begin function _Z6fooMulii
 	.p2align	4, 0x90
 	.type	_Z6fooMulii,@function
@@ -112,8 +59,8 @@ _Z6fooMulii:                            # @_Z6fooMulii
 	popq	%rbp
 	.cfi_def_cfa %rsp, 8
 	retq
-.Lfunc_end2:
-	.size	_Z6fooMulii, .Lfunc_end2-_Z6fooMulii
+.Lfunc_end0:
+	.size	_Z6fooMulii, .Lfunc_end0-_Z6fooMulii
 	.cfi_endproc
                                         # -- End function
 	.globl	_Z6barMulii                     # -- Begin function _Z6barMulii
@@ -134,8 +81,8 @@ _Z6barMulii:                            # @_Z6barMulii
 	popq	%rbp
 	.cfi_def_cfa %rsp, 8
 	retq
-.Lfunc_end3:
-	.size	_Z6barMulii, .Lfunc_end3-_Z6barMulii
+.Lfunc_end1:
+	.size	_Z6barMulii, .Lfunc_end1-_Z6barMulii
 	.cfi_endproc
                                         # -- End function
 	.globl	_Z6fooAddii                     # -- Begin function _Z6fooAddii
@@ -156,8 +103,8 @@ _Z6fooAddii:                            # @_Z6fooAddii
 	popq	%rbp
 	.cfi_def_cfa %rsp, 8
 	retq
-.Lfunc_end4:
-	.size	_Z6fooAddii, .Lfunc_end4-_Z6fooAddii
+.Lfunc_end2:
+	.size	_Z6fooAddii, .Lfunc_end2-_Z6fooAddii
 	.cfi_endproc
                                         # -- End function
 	.globl	_Z6barAddii                     # -- Begin function _Z6barAddii
@@ -178,8 +125,8 @@ _Z6barAddii:                            # @_Z6barAddii
 	popq	%rbp
 	.cfi_def_cfa %rsp, 8
 	retq
-.Lfunc_end5:
-	.size	_Z6barAddii, .Lfunc_end5-_Z6barAddii
+.Lfunc_end3:
+	.size	_Z6barAddii, .Lfunc_end3-_Z6barAddii
 	.cfi_endproc
                                         # -- End function
 	.globl	_Z7helper1PFKiiiEii             # -- Begin function _Z7helper1PFKiiiEii
@@ -199,25 +146,25 @@ _Z7helper1PFKiiiEii:                    # @_Z7helper1PFKiiiEii
 	movl	%edx, -24(%rbp)
 	leaq	_Z6barAddii(%rip), %rax
 	cmpq	%rax, -16(%rbp)
-	jne	.LBB6_2
+	jne	.LBB4_2
 # %bb.1:
 	movl	$1, -4(%rbp)
-	jmp	.LBB6_3
-.LBB6_2:
+	jmp	.LBB4_3
+.LBB4_2:
 	movq	-16(%rbp), %rax
 	movl	-20(%rbp), %edi
 	movl	-24(%rbp), %esi
 	callq	*%rax
 	subl	$4, %eax
 	movl	%eax, -4(%rbp)
-.LBB6_3:
+.LBB4_3:
 	movl	-4(%rbp), %eax
 	addq	$32, %rsp
 	popq	%rbp
 	.cfi_def_cfa %rsp, 8
 	retq
-.Lfunc_end6:
-	.size	_Z7helper1PFKiiiEii, .Lfunc_end6-_Z7helper1PFKiiiEii
+.Lfunc_end4:
+	.size	_Z7helper1PFKiiiEii, .Lfunc_end4-_Z7helper1PFKiiiEii
 	.cfi_endproc
                                         # -- End function
 	.globl	_Z7helper2PFKiiiES1_ii          # -- Begin function _Z7helper2PFKiiiES1_ii
@@ -238,11 +185,11 @@ _Z7helper2PFKiiiES1_ii:                 # @_Z7helper2PFKiiiES1_ii
 	movl	%ecx, -32(%rbp)
 	movq	-16(%rbp), %rax
 	cmpq	-24(%rbp), %rax
-	jne	.LBB7_2
+	jne	.LBB5_2
 # %bb.1:
 	movl	$2, -4(%rbp)
-	jmp	.LBB7_3
-.LBB7_2:
+	jmp	.LBB5_3
+.LBB5_2:
 	movq	-16(%rbp), %rax
 	movl	-28(%rbp), %edi
 	movl	-32(%rbp), %esi
@@ -256,14 +203,14 @@ _Z7helper2PFKiiiES1_ii:                 # @_Z7helper2PFKiiiES1_ii
 	movl	-36(%rbp), %eax                 # 4-byte Reload
 	addl	%ecx, %eax
 	movl	%eax, -4(%rbp)
-.LBB7_3:
+.LBB5_3:
 	movl	-4(%rbp), %eax
 	addq	$48, %rsp
 	popq	%rbp
 	.cfi_def_cfa %rsp, 8
 	retq
-.Lfunc_end7:
-	.size	_Z7helper2PFKiiiES1_ii, .Lfunc_end7-_Z7helper2PFKiiiES1_ii
+.Lfunc_end5:
+	.size	_Z7helper2PFKiiiES1_ii, .Lfunc_end5-_Z7helper2PFKiiiES1_ii
 	.cfi_endproc
                                         # -- End function
 	.globl	main                            # -- Begin function main
@@ -277,7 +224,7 @@ main:                                   # @main
 	.cfi_offset %rbp, -16
 	movq	%rsp, %rbp
 	.cfi_def_cfa_register %rbp
-	subq	$48, %rsp
+	subq	$32, %rsp
 	movl	$0, -4(%rbp)
 	movl	%edi, -8(%rbp)
 	movq	%rsi, -16(%rbp)
@@ -287,7 +234,7 @@ main:                                   # @main
 	movl	(%rax), %edx
 	leaq	_Z6barAddii(%rip), %rdi
 	callq	_Z7helper1PFKiiiEii
-	movl	%eax, -36(%rbp)                 # 4-byte Spill
+	movl	%eax, -28(%rbp)                 # 4-byte Spill
 	movq	FooVar@GOTPCREL(%rip), %rax
 	movl	(%rax), %edx
 	movq	BarVar@GOTPCREL(%rip), %rax
@@ -295,24 +242,6 @@ main:                                   # @main
 	leaq	_Z6fooMulii(%rip), %rdi
 	leaq	_Z6barMulii(%rip), %rsi
 	callq	_Z7helper2PFKiiiES1_ii
-	movl	%eax, %ecx
-	movl	-36(%rbp), %eax                 # 4-byte Reload
-	addl	%ecx, %eax
-	movl	%eax, -32(%rbp)                 # 4-byte Spill
-	movq	FooVar@GOTPCREL(%rip), %rax
-	movl	(%rax), %edi
-	movq	BarVar@GOTPCREL(%rip), %rax
-	movl	(%rax), %esi
-	callq	_Z6fooSubii
-	movl	%eax, %ecx
-	movl	-32(%rbp), %eax                 # 4-byte Reload
-	addl	%ecx, %eax
-	movl	%eax, -28(%rbp)                 # 4-byte Spill
-	movq	FooVar@GOTPCREL(%rip), %rax
-	movl	(%rax), %edi
-	movq	BarVar@GOTPCREL(%rip), %rax
-	movl	(%rax), %esi
-	callq	_Z6barSubii
 	movl	%eax, %ecx
 	movl	-28(%rbp), %eax                 # 4-byte Reload
 	addl	%ecx, %eax
@@ -327,19 +256,17 @@ main:                                   # @main
 	addl	%ecx, %eax
 	movl	%eax, -20(%rbp)
 	movl	-20(%rbp), %eax
-	addq	$48, %rsp
+	addq	$32, %rsp
 	popq	%rbp
 	.cfi_def_cfa %rsp, 8
 	retq
-.Lfunc_end8:
-	.size	main, .Lfunc_end8-main
+.Lfunc_end6:
+	.size	main, .Lfunc_end6-main
 	.cfi_endproc
                                         # -- End function
 	.ident	"clang version 20.0.0git"
 	.section	".note.GNU-stack","",@progbits
 	.addrsig
-	.addrsig_sym _Z6fooSubii
-	.addrsig_sym _Z6barSubii
 	.addrsig_sym _Z6fooMulii
 	.addrsig_sym _Z6barMulii
 	.addrsig_sym _Z6fooAddii
