43d2a61daf7e28f93f3cec8ad617612bf7fa86b7
Peng Xie <helianthus547@gmail.com>
2024-11-20 21:28:36 +0800

    [libc++] Update lwg-3143
    This patch update the lwg 3143 issue latest status.
    Modify test to differentiate between standard behavior and libc++ specific behavior.

--- libcxx/test/std/utilities/utility/mem.res/mem.res.monotonic.buffer/mem.res.monotonic.buffer.mem/allocate_mem.pass.cpp
+++ libcxx/test/std/utilities/utility/mem.res/mem.res.monotonic.buffer/mem.res.monotonic.buffer.mem/allocate_mem.pass.cpp
@@ -20,30 +20,35 @@
 #include "count_new.h"
 #include "test_macros.h"
 
-void test_geometric_progression() {
-  // mem.res.monotonic.buffer 1.3
-  // Each additional buffer is larger than the previous one, following a
-  // geometric progression.
-
+void test_allocate_mem() {
   globalMemCounter.reset();
   std::pmr::monotonic_buffer_resource mono1(100, std::pmr::new_delete_resource());
   std::pmr::memory_resource& r1 = mono1;
 
   assert(globalMemCounter.checkNewCalledEq(0));
   std::size_t next_buffer_size = 100;
-  void* ret               = r1.allocate(10, 1);
+  void* ret                    = r1.allocate(10, 1);
   assert(ret != nullptr);
   assert(globalMemCounter.checkNewCalledEq(1));
   assert(globalMemCounter.last_new_size >= next_buffer_size);
-  next_buffer_size = globalMemCounter.last_new_size + 1;
+  next_buffer_size = globalMemCounter.last_new_size;
 
-  int new_called = 1;
+  int new_called = globalMemCounter.new_called;
   while (new_called < 5) {
     ret = r1.allocate(10, 1);
+    assert(ret != nullptr);
     if (globalMemCounter.new_called > new_called) {
       assert(globalMemCounter.new_called == new_called + 1);
-      assert(globalMemCounter.last_new_size >= next_buffer_size);
-      next_buffer_size = globalMemCounter.last_new_size + 1;
+
+#ifndef _LIBCPP_VERSION
+      assert(globalMemCounter.last_new_size >= 100);
+#else
+      // a libc++ specific test.
+      constexpr auto foot_size{4 * sizeof(void*)};
+      next_buffer_size = next_buffer_size * 2 - foot_size;
+      assert(globalMemCounter.last_new_size == next_buffer_size);
+#endif // _LIBCPP_VERSION
+
       new_called += 1;
     }
   }
@@ -51,7 +56,7 @@ void test_geometric_progression() {
 
 int main(int, char**) {
 #if TEST_SUPPORTS_LIBRARY_INTERNAL_ALLOCATIONS && !defined(DISABLE_NEW_COUNT)
-  test_geometric_progression();
+  test_allocate_mem();
 #endif
 
   return 0;
