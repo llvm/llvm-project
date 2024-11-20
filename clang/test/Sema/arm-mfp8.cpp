74464306f7e5072dc74efb0d38e6e362f262661a
CarolineConcatto <caroline.concatto@arm.com>
2024-11-20 16:49:18 +0000

    Update arm-mfp8.cpp

--- clang/test/Sema/arm-mfp8.cpp
+++ clang/test/Sema/arm-mfp8.cpp
@@ -44,6 +44,7 @@ void test_vector_sve(svmfloat8_t a, svuint8_t c) {
   a / c;  // sve-error {{cannot convert between vector type 'svuint8_t' (aka '__SVUint8_t') and vector type 'svmfloat8_t' (aka '__SVMfloat8_t') as implicit conversion would cause truncation}}
 }
 
+
 #include <arm_neon.h>
 
 void test_vector(mfloat8x8_t a, mfloat8x16_t b, uint8x8_t c) {
