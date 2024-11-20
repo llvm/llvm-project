66598fe70590b85302429e36234a35ab6b8c4f52
Benjamin Maxwell <benjamin.maxwell@arm.com>
2024-11-20 10:51:33 +0000

    Revert "Generalize fold a little"
    This reverts commit c58cd9c03b7bbf96caa50a850b8fa76e4a7c2b1c.

--- llvm/lib/Target/AArch64/AArch64ISelLowering.cpp
+++ llvm/lib/Target/AArch64/AArch64ISelLowering.cpp
@@ -24735,7 +24735,10 @@ static SDValue skipElementSizePreservingCast(SDValue Op, EVT VT) {
 static SDValue performZIP1Combine(SDNode *N, SelectionDAG &DAG) {
   SDLoc DL(N);
   EVT VT = N->getValueType(0);
-  EVT EltVT = VT.getVectorElementType();
+
+  // zip1(insert_vector_elt(undef, extract_vector_elt(vec, 0), 0),
+  //      insert_vector_elt(undef, extract_vector_elt(vec, 1), 0))
+  // -> vec
   SDValue Op0 = skipElementSizePreservingCast(N->getOperand(0), VT);
   SDValue Op1 = skipElementSizePreservingCast(N->getOperand(1), VT);
   if (Op0 && Op1 && Op0->getOpcode() == ISD::INSERT_VECTOR_ELT &&
@@ -24748,51 +24751,17 @@ static SDValue performZIP1Combine(SDNode *N, SelectionDAG &DAG) {
       SDValue Op01 = Op0->getOperand(1);
       SDValue Op11 = Op1->getOperand(1);
       if (Op01->getOpcode() == ISD::EXTRACT_VECTOR_ELT &&
-          Op11->getOpcode() == ISD::EXTRACT_VECTOR_ELT) {
+          Op11->getOpcode() == ISD::EXTRACT_VECTOR_ELT &&
+          Op01->getConstantOperandVal(1) == 0 &&
+          Op11->getConstantOperandVal(1) == 1) {
         SDValue Op010 = skipElementSizePreservingCast(Op01->getOperand(0), VT);
         SDValue Op110 = skipElementSizePreservingCast(Op11->getOperand(0), VT);
-        unsigned StartExtractIdx = Op01->getConstantOperandVal(1);
-        if (Op010 && Op010 == Op110 &&
-            Op11->getConstantOperandVal(1) == StartExtractIdx + 1 &&
-            StartExtractIdx % 2 == 0) {
-          //       t0: nxv16i8 = ...
-          //     t1: i32 = extract_vector_elt t0, Constant:i64<n>
-          //     t2: i32 = extract_vector_elt t0, Constant:i64<n + 1>
-          //   t3: nxv16i8 = insert_vector_elt(undef, t1, 0)
-          //   t4: nxv16i8 = insert_vector_elt(undef, t2, 0)
-          // t5: nxv16i8 = zip1(t3, t4)
-          //
-          // ->
-          //         t0: nxv16i8 = ...
-          //       t1: nxv8i16 = bitcast t0
-          //     t2: i32 = extract_vector_elt t1, Constant:i64<n / 2>
-          //   t3: nxv8i16 = insert_vector_elt(undef, t2, 0)
-          // t4: nxv16i8 = bitcast t3
-          //
-          // Where n % 2 == 0
-          SDValue Result;
-          if (StartExtractIdx == 0)
-            Result = Op010;
-          else if (EltVT.getSizeInBits() < 64) {
-            unsigned LargeEltBits = EltVT.getSizeInBits() * 2;
-            EVT LargeEltVT = MVT::getVectorVT(
-                MVT::getIntegerVT(LargeEltBits),
-                VT.getVectorElementCount().divideCoefficientBy(2));
-            EVT ExtractVT = MVT::getIntegerVT(std::max(LargeEltBits, 32U));
-            SDValue Extract =
-                DAG.getNode(ISD::EXTRACT_VECTOR_ELT, DL, ExtractVT,
-                            DAG.getBitcast(LargeEltVT, Op010),
-                            DAG.getVectorIdxConstant(StartExtractIdx / 2, DL));
-            Result = DAG.getNode(ISD::INSERT_VECTOR_ELT, DL, LargeEltVT,
-                                 DAG.getUNDEF(LargeEltVT), Extract,
-                                 DAG.getVectorIdxConstant(0, DL));
-          }
-          if (Result)
-            return DAG.getBitcast(VT, Result);
-        }
+        if (Op010 && Op010 == Op110)
+          return DAG.getBitcast(VT, Op010);
       }
     }
   }
+
   return SDValue();
 }
 
