ca659d447cfa1f13c41bbf0a2d26b8919b5f185b
Alex Voicu <alexandru.voicu@amd.com>
2024-11-20 20:02:33 +0000

    Make test robust.

--- clang/test/CodeGen/embed-bitcode-marker-with-nonzero-as.c
+++ clang/test/CodeGen/embed-bitcode-marker-with-nonzero-as.c
@@ -2,7 +2,7 @@
 // RUN:   | FileCheck %s --check-prefix=CHECK
 
 // CHECK: @llvm.embedded.module = private addrspace(1) constant [0 x i8] zeroinitializer, section ".llvmbc", align 1
-// CHECK-NEXT: @llvm.cmdline = private addrspace(1) constant [387 x i8] c"{{.*}}", section ".llvmcmd", align 1
+// CHECK-NEXT: @llvm.cmdline = private addrspace(1) constant [{{[0-9]+}} x i8] c"{{.*}}", section ".llvmcmd", align 1
 // CHECK-NEXT: @llvm.compiler.used = appending addrspace(1) global [5 x ptr addrspace(4)] [ptr addrspace(4) addrspacecast (ptr addrspace(1) @foo.managed to ptr addrspace(4)), ptr addrspace(4) addrspacecast (ptr addrspace(1) @foo to ptr addrspace(4)), ptr addrspace(4) addrspacecast (ptr addrspace(1) @__hip_cuid_ to ptr addrspace(4)), ptr addrspace(4) addrspacecast (ptr addrspace(1) @llvm.embedded.module to ptr addrspace(4)), ptr addrspace(4) addrspacecast (ptr addrspace(1) @llvm.cmdline to ptr addrspace(4))], section "llvm.metadata"
 
 __attribute__((managed)) int foo = 42;
