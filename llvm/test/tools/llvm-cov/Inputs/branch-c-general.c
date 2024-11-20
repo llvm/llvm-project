b2f7fdf6e1e80c191fde07f40de50fe26c4c6eff
NAKAMURA Takumi <geek4civic@gmail.com>
2024-11-21 00:08:44 +0900

    Update single tests

--- llvm/test/tools/llvm-cov/Inputs/branch-c-general.c
+++ llvm/test/tools/llvm-cov/Inputs/branch-c-general.c
@@ -16,12 +16,12 @@ void simple_loops() {           // CHECK: @LINE|{{.*}}simple_loops()
 
 void conditionals() {           // CHECK: @LINE|{{.*}}conditionals()
   for (int i = 0; i < 100; ++i) {//BRCOV: Branch ([[@LINE]]:19): [True: [[C100]], False: 1]
-    if (i % 2) {                // BRCOV: Branch ([[@LINE]]:9): [True: [[C50:50|1]], False: [[C50]]]
-      if (i) {}                 // BRCOV: Branch ([[@LINE]]:11): [True: [[C50]], False: 0]
-    } else if (i % 3) {         // BRCOV: Branch ([[@LINE]]:16): [True: [[C33:33|1]], False: [[C17:17|1]]]
-      if (i) {}                 // BRCOV: Branch ([[@LINE]]:11): [True: [[C33]], False: 0]
+    if (i % 2) {                // CHECK: Branch ([[@LINE]]:9): [True: [[C50:50|1]], False: [[C50]]]
+      if (i) {}                 // CHECK: Branch ([[@LINE]]:11): [True: [[C50]], False: 0]
+    } else if (i % 3) {         // CHECK: Branch ([[@LINE]]:16): [True: [[C33:33|1]], False: [[C17:17|1]]]
+      if (i) {}                 // CHECK: Branch ([[@LINE]]:11): [True: [[C33]], False: 0]
     } else {
-      if (i) {}                 // BRCOV: Branch ([[@LINE]]:11): [True: [[C16:16|1]], False: 1]
+      if (i) {}                 // CHECK: Branch ([[@LINE]]:11): [True: [[C16:16|1]], False: 1]
     }
                                 // BRCOV: Branch ([[@LINE+1]]:9): [True: [[C100]], Folded]
     if (1 && i) {}              // BRCOV: Branch ([[@LINE]]:14): [True: [[C99:99|1]], False: 1]
@@ -33,26 +33,26 @@ void conditionals() {           // CHECK: @LINE|{{.*}}conditionals()
 void early_exits() {            // CHECK: @LINE|{{.*}}early_exits()
   int i = 0;
 
-  if (i) {}                     // BRCOV: Branch ([[@LINE]]:7): [True: 0, False: 1]
+  if (i) {}                     // CHECK: Branch ([[@LINE]]:7): [True: 0, False: 1]
 
   while (i < 100) {             // BRCOV: Branch ([[@LINE]]:10): [True: [[C51:51|1]], False: 0]
     i++;
-    if (i > 50)                 // BRCOV: Branch ([[@LINE]]:9): [True: 1, False: [[C50]]]
+    if (i > 50)                 // CHECK: Branch ([[@LINE]]:9): [True: 1, False: [[C50]]]
       break;
-    if (i % 2)                  // BRCOV: Branch ([[@LINE]]:9): [True: [[C25:25|1]], False: [[C25]]]
+    if (i % 2)                  // CHECK: Branch ([[@LINE]]:9): [True: [[C25:25|1]], False: [[C25]]]
       continue;
   }
 
-  if (i) {}                     // BRCOV: Branch ([[@LINE]]:7): [True: 1, False: 0]
+  if (i) {}                     // CHECK: Branch ([[@LINE]]:7): [True: 1, False: 0]
 
   do {
-    if (i > 75)                 // BRCOV: Branch ([[@LINE]]:9): [True: 1, False: [[C25]]]
+    if (i > 75)                 // CHECK: Branch ([[@LINE]]:9): [True: 1, False: [[C25]]]
       return;
     else
       i++;
   } while (i < 100);            // BRCOV: Branch ([[@LINE]]:12): [True: [[C25]], False: 0]
 
-  if (i) {}                     // BRCOV: Branch ([[@LINE]]:7): [True: 0, False: 0]
+  if (i) {}                     // CHECK: Branch ([[@LINE]]:7): [True: 0, False: 0]
 
 }
 
@@ -62,17 +62,17 @@ void jumps() {                  // CHECK: @LINE|{{.*}}jumps()
   for (i = 0; i < 2; ++i) {     // BRCOV: Branch ([[@LINE]]:15): [True: 1, False: 0]
     goto outofloop;
     // Never reached -> no weights
-    if (i) {}                   // BRCOV: Branch ([[@LINE]]:9): [True: 0, False: 0]
+    if (i) {}                   // CHECK: Branch ([[@LINE]]:9): [True: 0, False: 0]
   }
 
 outofloop:
-  if (i) {}                     // BRCOV: Branch ([[@LINE]]:7): [True: 0, False: 1]
+  if (i) {}                     // CHECK: Branch ([[@LINE]]:7): [True: 0, False: 1]
 
   goto loop1;
 
   while (i) {                   // BRCOV: Branch ([[@LINE]]:10): [True: 0, False: 1]
   loop1:
-    if (i) {}                   // BRCOV: Branch ([[@LINE]]:9): [True: 0, False: 1]
+    if (i) {}                   // CHECK: Branch ([[@LINE]]:9): [True: 0, False: 1]
   }
 
   goto loop2;
@@ -80,7 +80,7 @@ first:
 second:
 third:
   i++;
-  if (i < 3)                    // BRCOV: Branch ([[@LINE]]:7): [True: [[C2:2|1]], False: 1]
+  if (i < 3)                    // CHECK: Branch ([[@LINE]]:7): [True: [[C2:2|1]], False: 1]
     goto loop2;
 
   while (i < 3) {               // BRCOV: Branch ([[@LINE]]:10): [True: 0, False: 1]
@@ -98,9 +98,9 @@ third:
   for (i = 0; i < 10; ++i) {    // BRCOV: Branch ([[@LINE]]:15): [True: [[C10:10|1]], False: 1]
     goto withinloop;
                                 // never reached -> no weights
-    if (i) {}                   // BRCOV: Branch ([[@LINE]]:9): [True: 0, False: 0]
+    if (i) {}                   // CHECK: Branch ([[@LINE]]:9): [True: 0, False: 0]
   withinloop:
-    if (i) {}                   // BRCOV: Branch ([[@LINE]]:9): [True: [[C9:9|1]], False: 1]
+    if (i) {}                   // CHECK: Branch ([[@LINE]]:9): [True: [[C9:9|1]], False: 1]
   }
 
 }
@@ -117,30 +117,30 @@ void switches() {               // CHECK: @LINE|{{.*}}switches()
   for (int i = 0, len = sizeof(weights) / sizeof(weights[0]); i < len; ++i) {
     switch (i[weights]) {
     case 1:                     // BRCOV: Branch ([[@LINE]]:5): [True: 1, Folded]
-      if (i) {}                 // BRCOV: Branch ([[@LINE]]:11): [True: 0, False: 1]
+      if (i) {}                 // CHECK: Branch ([[@LINE]]:11): [True: 0, False: 1]
       // fallthrough
     case 2:                     // BRCOV: Branch ([[@LINE]]:5): [True: [[C2]], Folded]
-      if (i) {}                 // BRCOV: Branch ([[@LINE]]:11): [True: [[C2]], False: 1]
+      if (i) {}                 // CHECK: Branch ([[@LINE]]:11): [True: [[C2]], False: 1]
       break;
     case 3:                     // BRCOV: Branch ([[@LINE]]:5): [True: [[C3:3|1]], Folded]
-      if (i) {}                 // BRCOV: Branch ([[@LINE]]:11): [True: [[C3]], False: 0]
+      if (i) {}                 // CHECK: Branch ([[@LINE]]:11): [True: [[C3:3|1]], False: 0]
       continue;
     case 4:                     // BRCOV: Branch ([[@LINE]]:5): [True: [[C4:4|1]], Folded]
-      if (i) {}                 // BRCOV: Branch ([[@LINE]]:11): [True: [[C4]], False: 0]
+      if (i) {}                 // CHECK: Branch ([[@LINE]]:11): [True: [[C4:4|1]], False: 0]
       switch (i) {
       case 6 ... 9:             // BRCOV: Branch ([[@LINE]]:7): [True: [[C4]], Folded]
-        if (i) {}               // BRCOV: Branch ([[@LINE]]:13): [True: [[C4]], False: 0]
+        if (i) {}               // CHECK: Branch ([[@LINE]]:13): [True: [[C4]], False: 0]
         continue;
       }
 
     default:                    // BRCOV: Branch ([[@LINE]]:5): [True: [[C5:5|1]], Folded]
-      if (i == len - 1)         // BRCOV: Branch ([[@LINE]]:11): [True: 1, False: [[C4]]]
+      if (i == len - 1)         // CHECK: Branch ([[@LINE]]:11): [True: 1, False: [[C4]]]
         return;
     }
   }
 
   // Never reached -> no weights
-  if (weights[0]) {}            // BRCOV: Branch ([[@LINE]]:7): [True: 0, False: 0]
+  if (weights[0]) {}            // CHECK: Branch ([[@LINE]]:7): [True: 0, False: 0]
 
 }
 
@@ -148,29 +148,29 @@ void big_switch() {             // CHECK: @LINE|{{.*}}big_switch()
   for (int i = 0; i < 32; ++i) {// BRCOV: Branch ([[@LINE]]:19): [True: [[C32:32|1]], False: 1]
     switch (1 << i) {
     case (1 << 0):              // BRCOV: Branch ([[@LINE]]:5): [True: 1, Folded]
-      if (i) {}                 // BRCOV: Branch ([[@LINE]]:11): [True: 0, False: 1]
+      if (i) {}                 // CHECK: Branch ([[@LINE]]:11): [True: 0, False: 1]
       // fallthrough
     case (1 << 1):              // BRCOV: Branch ([[@LINE]]:5): [True: 1, Folded]
-      if (i) {}                 // BRCOV: Branch ([[@LINE]]:11): [True: 1, False: 1]
+      if (i) {}                 // CHECK: Branch ([[@LINE]]:11): [True: 1, False: 1]
       break;
     case (1 << 2) ... (1 << 12):// BRCOV: Branch ([[@LINE]]:5): [True: [[C11:11|1]], Folded]
-      if (i) {}                 // BRCOV: Branch ([[@LINE]]:11): [True: [[C11]], False: 0]
+      if (i) {}                 // CHECK: Branch ([[@LINE]]:11): [True: [[C11:11|1]], False: 0]
       break;
       // The branch for the large case range above appears after the case body.
 
     case (1 << 13):             // BRCOV: Branch ([[@LINE]]:5): [True: 1, Folded]
-      if (i) {}                 // BRCOV: Branch ([[@LINE]]:11): [True: 1, False: 0]
+      if (i) {}                 // CHECK: Branch ([[@LINE]]:11): [True: 1, False: 0]
       break;
     case (1 << 14) ... (1 << 28)://BRCOV: Branch ([[@LINE]]:5): [True: [[C15]], Folded]
-      if (i) {}                 // BRCOV: Branch ([[@LINE]]:11): [True: [[C15]], False: 0]
+      if (i) {}                 // CHECK: Branch ([[@LINE]]:11): [True: [[C15:15|1]], False: 0]
       break;
     // The branch for the large case range above appears after the case body.
 
     case (1 << 29) ... ((1 << 29) + 1):
-      if (i) {}                 // BRCOV: Branch ([[@LINE]]:11): [True: 1, False: 0]
+      if (i) {}                 // CHECK: Branch ([[@LINE]]:11): [True: 1, False: 0]
       break;
     default:                    // BRCOV: Branch ([[@LINE]]:5): [True: [[C2]], Folded]
-      if (i) {}                 // BRCOV: Branch ([[@LINE]]:11): [True: [[C2]], False: 0]
+      if (i) {}                 // CHECK: Branch ([[@LINE]]:11): [True: [[C2]], False: 0]
       break;
     }
   }
