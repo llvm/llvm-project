65696108aafd6c975ae6e08f76bfc0058ee39b2c
Ahmed Bougacha <ahmed@bougacha.org>
2024-11-20 13:07:40 -0800

    [clang] Update ptrauth vtable tests folowing RTTI IRGen change. NFC.
    6bb63002fca changes the order in which RTTI globals are created.

--- clang/test/CodeGenCXX/ptrauth-rtti-layout-wrapper-globals.cpp
+++ clang/test/CodeGenCXX/ptrauth-rtti-layout-wrapper-globals.cpp
@@ -3,9 +3,9 @@
 
 struct A { int a; };
 
+// CHECK: @_ZTI1A = linkonce_odr hidden constant { ptr, ptr } { ptr @_ZTVN10__cxxabiv117__class_type_infoE.ptrauth, ptr inttoptr (i64 add (i64 ptrtoint (ptr @_ZTS1A to i64), i64 -9223372036854775808) to ptr) }
 // CHECK: @_ZTVN10__cxxabiv117__class_type_infoE = external global [0 x ptr]
 // CHECK: @_ZTVN10__cxxabiv117__class_type_infoE.ptrauth = private constant { ptr, i32, i64, i64 } { ptr getelementptr inbounds (ptr, ptr @_ZTVN10__cxxabiv117__class_type_infoE, i64 2), i32 2, i64 0, i64 0 }, section "llvm.ptrauth", align 8
 // CHECK: @_ZTS1A = linkonce_odr hidden constant [3 x i8] c"1A\00"
-// CHECK: @_ZTI1A = linkonce_odr hidden constant { ptr, ptr } { ptr @_ZTVN10__cxxabiv117__class_type_infoE.ptrauth, ptr inttoptr (i64 add (i64 ptrtoint (ptr @_ZTS1A to i64), i64 -9223372036854775808) to ptr) }
 
 auto ATI = typeid(A);
