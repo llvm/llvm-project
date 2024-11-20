4c6592260ff0b3fae7473c4be546a4cb10dde3e4
Petr Hosek <phosek@google.com>
2024-11-20 01:07:45 -0800

    Avoid introducing new __support file for simplicity

--- libc/src/time/baremetal/timespec_get.cpp
+++ libc/src/time/baremetal/timespec_get.cpp
@@ -8,17 +8,18 @@
 
 #include "src/time/timespec_get.h"
 #include "hdr/time_macros.h"
-#include "src/__support/OSUtil/baremetal/time.h"
 #include "src/__support/common.h"
 #include "src/__support/macros/config.h"
 
 namespace LIBC_NAMESPACE_DECL {
 
+extern "C" bool __llvm_libc_timespec_get_utc(struct timespec *ts);
+
 LLVM_LIBC_FUNCTION(int, timespec_get, (struct timespec * ts, int base)) {
   if (base != TIME_UTC)
     return 0;
 
-  if (!internal::timespec_get_utc(ts))
+  if (!__llvm_libc_timespec_get_utc(ts))
     return 0;
   return base;
 }
