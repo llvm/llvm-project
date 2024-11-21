480feee107c9994d0a9dec241947ed13ef4b6f16
Daniel Kiss <daniel.kiss@arm.com>
2024-11-21 20:45:30 +0100

    s/CMAKE_HOST_SYSTEM_NAME STREQUAL/CMAKE_SYSTEM_NAME
    Change-Id: I998dae17e49db94e1b46aac1b35b68439e0861cb

--- llvm/cmake/modules/GetHostTriple.cmake
+++ llvm/cmake/modules/GetHostTriple.cmake
@@ -2,7 +2,7 @@
 # Invokes config.guess
 
 function( get_host_triple var )
-  if(MSVC OR (CMAKE_HOST_SYSTEM_NAME STREQUAL Windows AND CMAKE_C_COMPILER_ID MATCHES "Clang"))
+  if( MSVC OR (CMAKE_SYSTEM_NAME STREQUAL Windows AND CMAKE_C_COMPILER_ID MATCHES "Clang"))
     if( CMAKE_C_COMPILER_ARCHITECTURE_ID MATCHES "ARM64.*" )
       set( value "aarch64-pc-windows-msvc" )
     elseif( CMAKE_C_COMPILER_ARCHITECTURE_ID MATCHES "ARM.*" )
