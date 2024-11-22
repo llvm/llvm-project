7f41d63caebd22ce5b7ff9139c8bfa0272401afe
lorenzo chelini <lchelini@nvidia.com>
2024-11-22 20:54:35 +0100

    fix bug linalg

--- mlir/lib/Dialect/Linalg/IR/LinalgOps.cpp
+++ mlir/lib/Dialect/Linalg/IR/LinalgOps.cpp
@@ -1498,17 +1498,16 @@ static Operation *findPayloadOp(Block *body, bool initFirst = false) {
 
 void printShortForm(OpAsmPrinter &p, Operation *payloadOp) {
   SmallVector<StringRef> elidedAttrs;
-  std::string attrToElide;
   p << " { " << payloadOp->getName().getStringRef();
   for (const auto &attr : payloadOp->getAttrs()) {
     if (auto fastAttr = dyn_cast<arith::FastMathFlagsAttr>(attr.getValue())) {
       if (fastAttr.getValue() == arith::FastMathFlags::none) {
-        elidedAttrs.push_back(attr.getName().str());
+        elidedAttrs.push_back(attr.getName());
       }
     }
     if (auto denormAttr = dyn_cast<arith::DenormalModeAttr>(attr.getValue())) {
       if (denormAttr.getValue() == arith::DenormalMode::ieee) {
-        elidedAttrs.push_back(attr.getName().str());
+        elidedAttrs.push_back(attr.getName());
       }
     }
   }
