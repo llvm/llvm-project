14c47bd75927c8d5476818f9477ee41c94b48a06
Louis Dionne <ldionne.2@gmail.com>
2024-11-23 18:16:56 -0500

    [libc++] Extract destroy algorithms into a single header
    This is useful to start using those algorithms from the upcoming
    relocation utilities.

--- libcxx/include/__pstl/backends/libdispatch.h
+++ libcxx/include/__pstl/backends/libdispatch.h
@@ -22,6 +22,7 @@
 #include <__iterator/move_iterator.h>
 #include <__memory/allocator.h>
 #include <__memory/construct_at.h>
+#include <__memory/destroy.h>
 #include <__memory/unique_ptr.h>
 #include <__numeric/reduce.h>
 #include <__pstl/backend_fwd.h>
