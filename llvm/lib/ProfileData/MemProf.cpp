963c623337274b2822f1317aac7df0aa16ab9db2
Kazu Hirata <kazu@google.com>
2024-11-22 21:58:01 -0800

    [memprof] Remove verifyIndexedMemProfRecord and verifyFunctionProfileData (#117412)
    This patch removes two functions to verify the consistency between:
    
    - IndexedAllocationInfo::CallStack
    - IndexedAllocationInfo::CSId
    
    Now that MemProf format Version 1 has been removed,
    IndexedAllocationInfo::CallStack doesn't participate in either
    serialization or deserialization, so we don't care about the
    consistency between the two fields in IndexAllocationInfo.
    
    Subsequent patches will remove uses of the old field and eventually
    remove the field.

--- llvm/lib/ProfileData/MemProf.cpp
+++ llvm/lib/ProfileData/MemProf.cpp
@@ -537,22 +537,5 @@ template llvm::DenseMap<LinearFrameId, FrameStat>
 computeFrameHistogram<LinearFrameId>(
     llvm::MapVector<CallStackId, llvm::SmallVector<LinearFrameId>>
         &MemProfCallStackData);
-
-void verifyIndexedMemProfRecord(const IndexedMemProfRecord &Record) {
-  for (const auto &AS : Record.AllocSites) {
-    assert(AS.CSId == hashCallStack(AS.CallStack));
-    (void)AS;
-  }
-}
-
-void verifyFunctionProfileData(
-    const llvm::MapVector<GlobalValue::GUID, IndexedMemProfRecord>
-        &FunctionProfileData) {
-  for (const auto &[GUID, Record] : FunctionProfileData) {
-    (void)GUID;
-    verifyIndexedMemProfRecord(Record);
-  }
-}
-
 } // namespace memprof
 } // namespace llvm
