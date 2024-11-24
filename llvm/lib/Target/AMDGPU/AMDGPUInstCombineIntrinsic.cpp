dcfe7be50909a01893c105b252e12f92dd4fd2b7
Alex Voicu <alexandru.voicu@amd.com>
2024-11-24 23:13:59 +0000

    Use `isWaveSizeKnown` instead of gnarly hack.

--- llvm/lib/Target/AMDGPU/AMDGPUInstCombineIntrinsic.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPUInstCombineIntrinsic.cpp
@@ -1025,13 +1025,10 @@ GCNTTIImpl::instCombineIntrinsic(InstCombiner &IC, IntrinsicInst &II) const {
     break;
   }
   case Intrinsic::amdgcn_wavefrontsize: {
-    // TODO: this is a workaround for the pseudo-generic target one gets with no
-    // specified mcpu, which spoofs its wave size to 64; it should be removed.
-    if ((ST->getCPU().empty() || ST->getCPU().starts_with("generic")) &&
-        !ST->getFeatureString().contains("+wavefrontsize"))
-      break;
-    return IC.replaceInstUsesWith(
-        II, ConstantInt::get(II.getType(), ST->getWavefrontSize()));
+    if (ST->isWaveSizeKnown())
+      return IC.replaceInstUsesWith(
+          II, ConstantInt::get(II.getType(), ST->getWavefrontSize()));
+    break;
   }
   case Intrinsic::amdgcn_wqm_vote: {
     // wqm_vote is identity when the argument is constant.
