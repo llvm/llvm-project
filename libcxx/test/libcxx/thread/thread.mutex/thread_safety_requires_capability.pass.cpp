7dd44215fdb10c420e64efef1098e4259f446afc
Nikolas Klauser <nikolasklauser@berlin.de>
2024-11-24 16:44:16 +0100

    [libc++] Add the thread safety annotations unconditionally

--- libcxx/test/libcxx/thread/thread.mutex/thread_safety_requires_capability.pass.cpp
+++ libcxx/test/libcxx/thread/thread.mutex/thread_safety_requires_capability.pass.cpp
@@ -11,11 +11,10 @@
 // XFAIL: msvc
 
 // UNSUPPORTED: no-threads
-// REQUIRES: thread-safety
 
 // <mutex>
 
-// ADDITIONAL_COMPILE_FLAGS: -D_LIBCPP_ENABLE_THREAD_SAFETY_ANNOTATIONS
+// ADDITIONAL_COMPILE_FLAGS: -Wthread-safety
 
 #include <mutex>
 
