1670e0a0efe54595d82a4941ee1eba96b46631b9
Vitaly Buka <vitalybuka@google.com>
2024-11-23 23:21:28 -0800

    Fix linking c++

--- compiler-rt/test/tsan/many_held_mutex.cpp
+++ compiler-rt/test/tsan/many_held_mutex.cpp
@@ -1,22 +1,22 @@
-// RUN: %clangxx_tsan %s -fsanitize=thread -o %t
+// RUN: %clangxx_tsan -O1 %s %link_libcxx_tsan -fsanitize=thread -o %t
 // RUN: %run %t 128
 // RUN: not %run %t 129
 
 #include <mutex>
+#include <vector>
 #include <string>
 
 int main(int argc, char *argv[]) {
-  int num_of_mtx = std::stoi(argv[1]);
+  int num_of_mtx = std::atoi(argv[1]);
 
-  std::mutex *mutexes = new std::mutex[num_of_mtx];
+  std::vector<std::mutex> mutexes(num_of_mtx);
 
-  for (int i = 0; i < num_of_mtx; i++) {
-    mutexes[i].lock();
+  for (auto& mu : mutexes) {
+    mu.lock();
   }
-  for (int i = 0; i < num_of_mtx; i++) {
-    mutexes[i].unlock();
+  for (auto& mu : mutexes) {
+    mu.unlock();
   }
 
-  delete[] mutexes;
   return 0;
 }
