c8c43222d6d7e63484cc34ec8b18de7c548c0866
Jakub Kuderski <jakub@nod-labs.com>
2024-11-23 22:55:11 -0500

    Fix formatting

--- llvm/unittests/ADT/SmallVectorExtrasTest.cpp
+++ llvm/unittests/ADT/SmallVectorExtrasTest.cpp
@@ -1,4 +1,4 @@
-//===- llvm/unittest/ADT/SmallVectorExtrasTest.cpp ------------------------------===//
+//===- llvm/unittest/ADT/SmallVectorExtrasTest.cpp ------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
