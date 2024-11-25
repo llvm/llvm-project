d27d71617e74fd65eb3600219ad9d2ad6e0a0433
David Green <david.green@arm.com>
2024-11-25 09:55:46 +0000

    [AArch64][GlobalISel] Scalarize i128 vector sadd_sat/uadd_sat/etc.
    As with other operations we scalarize any vectors with larger types to let the
    scalare legalization kick in.

--- llvm/lib/Target/AArch64/GISel/AArch64LegalizerInfo.cpp
+++ llvm/lib/Target/AArch64/GISel/AArch64LegalizerInfo.cpp
@@ -1303,6 +1303,7 @@ AArch64LegalizerInfo::AArch64LegalizerInfo(const AArch64Subtarget &ST)
       .clampNumElements(0, v4s16, v8s16)
       .clampNumElements(0, v2s32, v4s32)
       .clampMaxNumElements(0, s64, 2)
+      .scalarizeIf(scalarOrEltWiderThan(0, 64), 0)
       .moreElementsToNextPow2(0)
       .lower();
 
