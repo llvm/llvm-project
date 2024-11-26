29366fc97ddf34c2f85ce59ac2e4cc2a689bae49
Simon Camphausen <simon.camphausen@iml.fraunhofer.de>
2024-11-26 10:57:45 +0000

    Drop unused includes and fix header format

--- mlir/lib/Conversion/ConvertToEmitC/ConvertToEmitC.cpp
+++ mlir/lib/Conversion/ConvertToEmitC/ConvertToEmitC.cpp
@@ -7,22 +7,13 @@
 //===----------------------------------------------------------------------===//
 
 #include "mlir/Conversion/ConvertToEmitC/ConvertToEmitC.h"
+
 #include "mlir/Conversion/ArithToEmitC/ArithToEmitC.h"
 #include "mlir/Conversion/FuncToEmitC/FuncToEmitC.h"
 #include "mlir/Conversion/MemRefToEmitC/MemRefToEmitC.h"
 #include "mlir/Conversion/SCFToEmitC/SCFToEmitC.h"
-#include "mlir/Dialect/Arith/IR/Arith.h"
-#include "mlir/Dialect/EmitC/IR/EmitC.h"
-#include "mlir/Dialect/Func/IR/FuncOps.h"
-#include "mlir/Dialect/MemRef/IR/MemRef.h"
-#include "mlir/Dialect/SCF/IR/SCF.h"
-#include "mlir/IR/PatternMatch.h"
-#include "mlir/Pass/Pass.h"
 #include "mlir/Rewrite/FrozenRewritePatternSet.h"
 #include "mlir/Transforms/DialectConversion.h"
-#include "mlir/Transforms/GreedyPatternRewriteDriver.h"
-
-#define DEBUG_TYPE "convert-to-emitc"
 
 using namespace mlir;
 
