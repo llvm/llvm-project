4ba15cd7e64f8f4a2a5d804a1f986aafff9f6d17
Miro Bucko <mbucko@meta.com>
2024-11-26 12:07:57 -0800

    Add 'FindFirstSymbolWithNameAndType()' to ModuleList.

--- lldb/include/lldb/Core/ModuleList.h
+++ lldb/include/lldb/Core/ModuleList.h
@@ -353,6 +353,15 @@ public:
 
   lldb::ModuleSP FindFirstModule(const ModuleSpec &module_spec) const;
 
+  /// Find the first symbol that matches \a name and \a symbol_type.
+  /// \param[in] name
+  ///     The name of the symbol we are looking for.
+  /// \param[in] symbol_type
+  ///     The type of symbol we are looking for.
+  const Symbol *
+  FindFirstSymbolWithNameAndType(ConstString name,
+                                 lldb::SymbolType symbol_type) const;
+
   void FindSymbolsWithNameAndType(ConstString name,
                                   lldb::SymbolType symbol_type,
                                   SymbolContextList &sc_list) const;
