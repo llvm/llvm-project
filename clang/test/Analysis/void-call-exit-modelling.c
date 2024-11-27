eb1b47c55bb44fcaa4fb2f64c483905cde6cc846
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2024-11-27 11:26:47 +0100

    Test top-level function effect. Thanks, Balázs for this example

--- clang/test/Analysis/void-call-exit-modelling.c
+++ clang/test/Analysis/void-call-exit-modelling.c
@@ -15,3 +15,12 @@ void inf_loop_break_callee() {
 void inf_loop_break_caller() {
   inf_loop_break_callee(); // expected-note{{Calling 'inf_loop_break_callee'}}
 }
+
+void inf_loop_break_top() {
+  void* data = malloc(10); // expected-note{{Memory is allocated}}
+  while (1) { // expected-note{{Loop condition is true}}
+    (void)data;
+    break; // No note that we jump to the line above from this break
+  } // expected-note@-1{{Execution jumps to the end of the function}}
+} // expected-warning{{Potential leak of memory pointed to by 'data'}}
+// expected-note@-1  {{Potential leak of memory pointed to by 'data'}}
