98d249cdf37cf991f41b2c4090bab22cc6dab2c7
antangelo <contact@antangelo.com>
2024-11-26 20:02:59 -0500

    Change diagnostic warning wording

--- clang/test/Parser/cxx2c-oxford-variadic-comma.cpp
+++ clang/test/Parser/cxx2c-oxford-variadic-comma.cpp
@@ -5,32 +5,32 @@ void a(...);
 void b(auto...);
 void c(auto, ...);
 
-void d(auto......); // expected-warning {{variadic parameters that are not preceded by a comma are deprecated}} \
+void d(auto......); // expected-warning {{declaration of a variadic function without a comma before '...' is deprecated}} \
                     // expected-warning {{'...' in this location creates a C-style varargs function}} \
                     // expected-note {{preceding '...' declares a function parameter pack}} \
                     // expected-note {{insert ',' before '...' to silence this warning}}
 void e(auto..., ...);
 
-void f(auto x...); // expected-warning {{variadic parameters that are not preceded by a comma are deprecated}}
+void f(auto x...); // expected-warning {{declaration of a variadic function without a comma before '...' is deprecated}}
 void g(auto x, ...);
 
-void h(auto... x...); // expected-warning {{variadic parameters that are not preceded by a comma are deprecated}} \
+void h(auto... x...); // expected-warning {{declaration of a variadic function without a comma before '...' is deprecated}} \
                       // expected-warning {{'...' in this location creates a C-style varargs function}} \
                       // expected-note {{preceding '...' declares a function parameter pack}} \
                       // expected-note {{insert ',' before '...' to silence this warning}}
 void i(auto... x, ...);
 
-template<class ...T>
-void j(T... t...); // expected-warning {{variadic parameters that are not preceded by a comma are deprecated}} \
-                   // expected-warning {{'...' in this location creates a C-style varargs function}} \
-                   // expected-note {{preceding '...' declares a function parameter pack}} \
-                   // expected-note {{insert ',' before '...' to silence this warning}}
-template<class ...T>
-void k(T... t, ...);
+template<class ...Ts>
+void j(Ts... t...) {}; // expected-warning {{declaration of a variadic function without a comma before '...' is deprecated}} \
+                       // expected-warning {{'...' in this location creates a C-style varargs function}} \
+                       // expected-note {{preceding '...' declares a function parameter pack}} \
+                       // expected-note {{insert ',' before '...' to silence this warning}}
+template<class ...Ts>
+void k(Ts... t, ...) {}
 
-void l(int...); // expected-warning {{variadic parameters that are not preceded by a comma are deprecated}}
+void l(int...); // expected-warning {{declaration of a variadic function without a comma before '...' is deprecated}}
 void m(int, ...);
 
-void n(int x...); // expected-warning {{variadic parameters that are not preceded by a comma are deprecated}}
+void n(int x...); // expected-warning {{declaration of a variadic function without a comma before '...' is deprecated}}
 void o(int x, ...);
 
