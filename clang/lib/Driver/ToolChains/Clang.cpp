0901017098d5f6d8161d609dbe6846ae746a7ff4
Stephen Tozer <stephen.tozer@sony.com>
2024-11-27 18:14:54 +0000

    Make extend lifetimes an = option

--- clang/lib/Driver/ToolChains/Clang.cpp
+++ clang/lib/Driver/ToolChains/Clang.cpp
@@ -7654,10 +7654,7 @@ void Clang::ConstructJob(Compilation &C, const JobAction &JA,
   if (Args.hasArg(options::OPT_fretain_comments_from_system_headers))
     CmdArgs.push_back("-fretain-comments-from-system-headers");
 
-  Args.addOptInFlag(CmdArgs, options::OPT_fextend_this_ptr,
-                    options::OPT_fno_extend_this_ptr);
-  Args.addOptInFlag(CmdArgs, options::OPT_fextend_lifetimes,
-                    options::OPT_fno_extend_lifetimes);
+  Args.AddLastArg(CmdArgs, options::OPT_fextend_lifetimes_EQ);
 
   // Forward -fcomment-block-commands to -cc1.
   Args.AddAllArgs(CmdArgs, options::OPT_fcomment_block_commands);
