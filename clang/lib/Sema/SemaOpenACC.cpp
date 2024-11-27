4f8071ab6dff829ed42c096996d2cf417702501c
erichkeane <ekeane@nvidia.com>
2024-11-27 07:02:43 -0800

    [OpenACC] enable 'deviceptr' for combined constructs.
    This is another clause whose implementation is identical for combined
    constructs as with compute constructs, so this adds tests and enables
    it.

--- clang/lib/Sema/SemaOpenACC.cpp
+++ clang/lib/Sema/SemaOpenACC.cpp
@@ -961,10 +961,11 @@ OpenACCClause *SemaOpenACCClauseVisitor::VisitAttachClause(
 
 OpenACCClause *SemaOpenACCClauseVisitor::VisitDevicePtrClause(
     SemaOpenACC::OpenACCParsedClause &Clause) {
-  // Restrictions only properly implemented on 'compute' constructs, and
-  // 'compute' constructs are the only construct that can do anything with
-  // this yet, so skip/treat as unimplemented in this case.
-  if (!isOpenACCComputeDirectiveKind(Clause.getDirectiveKind()))
+  // Restrictions only properly implemented on 'compute'/'combined' constructs,
+  // and 'compute'/'combined' constructs are the only construct that can do
+  // anything with this yet, so skip/treat as unimplemented in this case.
+  if (!isOpenACCComputeDirectiveKind(Clause.getDirectiveKind()) &&
+      !isOpenACCCombinedDirectiveKind(Clause.getDirectiveKind()))
     return isNotImplemented();
 
   // ActOnVar ensured that everything is a valid variable reference, but we
