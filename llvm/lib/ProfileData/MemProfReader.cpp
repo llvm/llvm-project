03561517017ee1821b4f35906741135931cb89e5
Kazu Hirata <kazu@google.com>
2024-11-27 11:10:53 -0800

    [memprof] Remove inline call stacks (#117833)
    Now that MemProf format version 1 has been removed, nobody uses:
    
    - IndexedAllocationInfo::CallStack
    - IndexedMemProfRecord::CallSites
    
    This patch removed the dead struct fields.
    
    You might notice that IndexedMemProfRecord::{clear,merge} do not
    mention CallSiteIds at all.  I think it's an oversight.  clear doesn't
    matter at the moment because we call it during serialization to reduce
    memory footprint.  merge is simply not as well tested as it should be.
    I'll follow up with a separate patch to address these issues.

--- llvm/lib/ProfileData/MemProfReader.cpp
+++ llvm/lib/ProfileData/MemProfReader.cpp
@@ -599,7 +599,7 @@ Error RawMemProfReader::mapRawProfileToRecords() {
     for (size_t I = 0; /*Break out using the condition below*/; I++) {
       const Frame &F = idToFrame(Callstack[I]);
       IndexedMemProfRecord &Record = MemProfData.Records[F.Function];
-      Record.AllocSites.emplace_back(Callstack, CSId, MIB);
+      Record.AllocSites.emplace_back(CSId, MIB);
 
       if (!F.IsInlineFrame)
         break;
@@ -614,7 +614,6 @@ Error RawMemProfReader::mapRawProfileToRecords() {
     for (LocationPtr Loc : Locs) {
       CallStackId CSId = hashCallStack(*Loc);
       MemProfData.CallStacks.insert({CSId, *Loc});
-      Record.CallSites.push_back(*Loc);
       Record.CallSiteIds.push_back(CSId);
     }
   }
