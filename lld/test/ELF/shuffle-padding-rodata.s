667e7bad3979d6e527f63e6ee3e9803073e0c6ed
Peter Collingbourne <peter@pcc.me.uk>
2024-11-27 16:07:21 -0800

    Add tests
    Created using spr 1.3.6-beta.1

--- lld/test/ELF/shuffle-padding-rodata.s
+++ lld/test/ELF/shuffle-padding-rodata.s
@@ -4,26 +4,33 @@
 # RUN: ld.lld %t.o -o %t.out
 # RUN: llvm-readelf -x .rodata %t.out | FileCheck %s
 # CHECK: Hex dump of section '.rodata':
-# CHECK-NEXT: 0x00201120 010203
+# CHECK-NEXT: 0x00200120 010203
 
 ## --shuffle-padding= inserts segment offset padding and pre-section padding.
 # RUN: ld.lld --shuffle-padding=1 %t.o -o %t.out
 # RUN: llvm-readelf -x .rodata %t.out | FileCheck --check-prefix=PAD1 %s
 # PAD1: Hex dump of section '.rodata':
-# PAD1-NEXT: 0x00201548 0102cc03
+# PAD1: 0x00200540 00000000 00010203
+
+## --shuffle-padding=  does not affect .rodata flags.
+# RUN: llvm-readelf -S %t.out | FileCheck --check-prefix=HEADER %s
+# HEADER: .rodata           PROGBITS        0000000000200120 000120 000428 00   A  0   0  1
 
 ## Size of segment offset padding and location of pre-section padding is
 ## dependent on the seed.
 # RUN: ld.lld --shuffle-padding=2 %t.o -o %t.out
 # RUN: llvm-readelf -x .rodata %t.out | FileCheck --check-prefix=PAD2 %s
 # PAD2: Hex dump of section '.rodata':
-# PAD2-NEXT: 0x00201dc8 cc010203
+# PAD2: 0x00200dc0 00000000 00000000 01000203
 
-.section .rodata.a,"ax"
+.section .rodata.a,"a",@progbits
+a:
 .byte 1
 
-.section .rodata.b,"ax"
+.section .rodata.b,"a",@progbits
+b:
 .byte 2
 
-.section .rodata.c,"ax"
+.section .rodata.c,"a",@progbits
+c:
 .byte 3
