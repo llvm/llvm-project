94e40c83dbeb2ef5384fe3177372dfd7e208552d
Caroline Tice <cmtice@google.com>
2024-12-01 12:29:27 -0800

    Add test case.

---
+++ lldb/test/API/commands/frame/var/anon-struct/main.cpp
@@ -0,0 +1,20 @@
+int main(int argc, char** argv)
+{
+  struct A {
+    struct {
+      int x = 1;
+    };
+    int y = 2;
+  } a;
+
+  struct B {
+    // Anonymous struct inherits another struct.
+    struct : public A {
+      int z = 3;
+    };
+    int w = 4;
+    A a;
+  } b;
+
+  return 0; // Set a breakpoint here
+}
