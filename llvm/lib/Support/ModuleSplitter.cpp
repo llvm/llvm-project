220c5788fd11bb4d0f69e5aaa04d854132dec65e
Weiwei Chen <weiwei.chen@modular.com>
2024-11-30 21:19:00 -0500

    Add ModuleSplitter.h

--- llvm/lib/Support/ModuleSplitter.cpp
+++ llvm/lib/Support/ModuleSplitter.cpp
@@ -8,7 +8,8 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "llvm/Support/ModRef.h"
+#include "llvm/Support/ModuleSplitter.h"
 #include "llvm/ADT/STLExtras.h"
+
 
 using namespace llvm;
