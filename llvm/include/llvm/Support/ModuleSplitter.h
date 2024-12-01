9bc7b85383642995231afc4030457d25bc5f27b5
Weiwei Chen <weiwei.chen@modular.com>
2024-11-30 21:18:37 -0500

    Add files.

---
+++ llvm/include/llvm/Support/ModuleSplitter.h
@@ -0,0 +1,19 @@
+//===- ModuleSplitter.h - Module Splitter Functions -------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+/// \file
+///
+///
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_SUPPORT_MODULESPLITTER_H
+#define LLVM_SUPPORT_MODULESPLITTER_H
+namespace llvm {
+
+} // namespace llvm
+
+#endif
