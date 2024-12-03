7907292daa781aaba2be531a8599998f5fd3f645
fengfeng <153487255+fengfeng09@users.noreply.github.com>
2024-12-03 09:21:03 +0800

    [DAG] Apply Disjoint flag. (#118045)
    or disjoint (or disjoint (x, c0), c1)
    -->
    or disjont x, or (c0, c1)
    Alive2: https://alive2.llvm.org/ce/z/3wPth5
    
    ---------
    
    Signed-off-by: feng.feng <feng.feng@iluvatar.com>

--- llvm/lib/CodeGen/SelectionDAG/DAGCombiner.cpp
+++ llvm/lib/CodeGen/SelectionDAG/DAGCombiner.cpp
@@ -1220,8 +1220,11 @@ SDValue DAGCombiner::reassociateOpsCommutative(unsigned Opc, const SDLoc &DL,
 
     if (DAG.isConstantIntBuildVectorOrConstantInt(N1)) {
       // Reassociate: (op (op x, c1), c2) -> (op x, (op c1, c2))
-      if (SDValue OpNode = DAG.FoldConstantArithmetic(Opc, DL, VT, {N01, N1}))
+      if (SDValue OpNode = DAG.FoldConstantArithmetic(Opc, DL, VT, {N01, N1})) {
+        NewFlags.setDisjoint(Flags.hasDisjoint() &&
+                             N0->getFlags().hasDisjoint());
         return DAG.getNode(Opc, DL, VT, N00, OpNode, NewFlags);
+      }
       return SDValue();
     }
     if (TLI.isReassocProfitable(DAG, N0, N1)) {
