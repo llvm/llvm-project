984beaa4b26f97a925d8c3f3bc97a9e9b61511ec
Jonas Devlieghere <jonas@devlieghere.com>
2024-12-03 12:59:13 -0800

    [lldb-dap] Support finding the lldb-dap binary
    Support finding the lldb-dap binary with `xcrun` on Darwin or in PATH on
    all other platforms.
    
    Unfortunately, this PR is larger than I would like because it removes
    the `lldbDapOptions`.  I believe these options are not necessary, and as
    previously implemented, they caused a spurious warning with this change.
    The problem was that the options were created before the custom factory.
    By moving the creation logic into the factory, we make sure it's only
    called after the factory has been registered. The upside is that this
    simplifies the code and removes a level of indirection.

--- lldb/tools/lldb-dap/src-ts/debug-adapter-factory.ts
+++ lldb/tools/lldb-dap/src-ts/debug-adapter-factory.ts
@@ -1,4 +1,7 @@
+import * as path from "path";
+import * as util from "util";
 import * as vscode from "vscode";
+
 import { LLDBDapOptions } from "./types";
 
 /**
@@ -8,15 +11,7 @@ import { LLDBDapOptions } from "./types";
 export class LLDBDapDescriptorFactory
   implements vscode.DebugAdapterDescriptorFactory
 {
-  private lldbDapOptions: LLDBDapOptions;
-
-  constructor(lldbDapOptions: LLDBDapOptions) {
-    this.lldbDapOptions = lldbDapOptions;
-  }
-
-  static async isValidDebugAdapterPath(
-    pathUri: vscode.Uri,
-  ): Promise<Boolean> {
+  static async isValidFile(pathUri: vscode.Uri): Promise<Boolean> {
     try {
       const fileStats = await vscode.workspace.fs.stat(pathUri);
       if (!(fileStats.type & vscode.FileType.File)) {
@@ -28,6 +23,70 @@ export class LLDBDapDescriptorFactory
     return true;
   }
 
+  static async findDAPExecutable(): Promise<string | undefined> {
+    let executable = "lldb-dap";
+    if (process.platform === "win32") {
+      executable = "lldb-dap.exe";
+    }
+
+    // Prefer lldb-dap from Xcode on Darwin.
+    if (process.platform === "darwin") {
+      try {
+        const exec = util.promisify(require("child_process").execFile);
+        let { stdout, stderr } = await exec("/usr/bin/xcrun", [
+          "-find",
+          executable,
+        ]);
+        if (stdout) {
+          return stdout.toString().trimEnd();
+        }
+      } catch (error) {}
+    }
+
+    // Find lldb-dap in the user's path.
+    let env_path =
+      process.env["PATH"] ||
+      (process.platform === "win32" ? process.env["Path"] : null);
+    if (!env_path) {
+      return undefined;
+    }
+
+    const paths = env_path.split(path.delimiter);
+    for (const p of paths) {
+      const exe_path = path.join(p, executable);
+      if (
+        await LLDBDapDescriptorFactory.isValidFile(vscode.Uri.file(exe_path))
+      ) {
+        return exe_path;
+      }
+    }
+
+    return undefined;
+  }
+
+  static async getDAPExecutable(
+    session: vscode.DebugSession,
+  ): Promise<string | undefined> {
+    const config = vscode.workspace.getConfiguration(
+      "lldb-dap",
+      session.workspaceFolder,
+    );
+
+    // Prefer the explicitly specified path in the extension's configuration.
+    const configPath = config.get<string>("executable-path");
+    if (configPath && configPath.length !== 0) {
+      return configPath;
+    }
+
+    // Try finding the lldb-dap binary.
+    const foundPath = await LLDBDapDescriptorFactory.findDAPExecutable();
+    if (foundPath) {
+      return foundPath;
+    }
+
+    return undefined;
+  }
+
   async createDebugAdapterDescriptor(
     session: vscode.DebugSession,
     executable: vscode.DebugAdapterExecutable | undefined,
@@ -36,14 +95,42 @@ export class LLDBDapDescriptorFactory
       "lldb-dap",
       session.workspaceFolder,
     );
-    const customPath = config.get<string>("executable-path");
-    const path: string = customPath || executable!!.command;
 
-    const fileUri = vscode.Uri.file(path);
-    if (!(await LLDBDapDescriptorFactory.isValidDebugAdapterPath(fileUri))) {
-      LLDBDapDescriptorFactory.showLLDBDapNotFoundMessage(fileUri.path);
+    const log_path = config.get<string>("log-path");
+    let env: { [key: string]: string } = {};
+    if (log_path) {
+      env["LLDBDAP_LOG"] = log_path;
     }
-    return this.lldbDapOptions.createDapExecutableCommand(session, executable);
+    const configEnvironment =
+      config.get<{ [key: string]: string }>("environment") || {};
+    const dapPath = await LLDBDapDescriptorFactory.getDAPExecutable(session);
+    const dbgOptions = {
+      env: {
+        ...executable?.options?.env,
+        ...configEnvironment,
+        ...env,
+      },
+    };
+    if (dapPath) {
+      const fileUri = vscode.Uri.file(dapPath);
+      if (!(await LLDBDapDescriptorFactory.isValidFile(fileUri))) {
+        LLDBDapDescriptorFactory.showLLDBDapNotFoundMessage(fileUri.path);
+        return undefined;
+      }
+      return new vscode.DebugAdapterExecutable(dapPath, [], dbgOptions);
+    } else if (executable) {
+      const fileUri = vscode.Uri.file(executable.command);
+      if (!(await LLDBDapDescriptorFactory.isValidFile(fileUri))) {
+        LLDBDapDescriptorFactory.showLLDBDapNotFoundMessage(fileUri.path);
+        return undefined;
+      }
+      return new vscode.DebugAdapterExecutable(
+        executable.command,
+        executable.args,
+        dbgOptions,
+      );
+    }
+    return undefined;
   }
 
   /**
--- lldb/tools/lldb-dap/src-ts/extension.ts
+++ lldb/tools/lldb-dap/src-ts/extension.ts
@@ -1,74 +1,22 @@
+import * as path from "path";
+import * as util from "util";
 import * as vscode from "vscode";
-import { LLDBDapOptions } from "./types";
-import { DisposableContext } from "./disposable-context";
+
 import { LLDBDapDescriptorFactory } from "./debug-adapter-factory";
-
-/**
- * This creates the configurations for this project if used as a standalone
- * extension.
- */
-function createDefaultLLDBDapOptions(): LLDBDapOptions {
-  return {
-    debuggerType: "lldb-dap",
-    async createDapExecutableCommand(
-      session: vscode.DebugSession,
-      packageJSONExecutable: vscode.DebugAdapterExecutable | undefined,
-    ): Promise<vscode.DebugAdapterExecutable | undefined> {
-      const config = vscode.workspace.getConfiguration(
-        "lldb-dap",
-        session.workspaceFolder,
-      );
-      const path = config.get<string>("executable-path");
-      const log_path = config.get<string>("log-path");
-
-      let env: { [key: string]: string } = {};
-      if (log_path) {
-        env["LLDBDAP_LOG"] = log_path;
-      }
-      const configEnvironment = config.get<{ [key: string]: string }>("environment") || {};
-      if (path) {
-        const dbgOptions = {
-          env: {
-            ...configEnvironment,
-            ...env,
-          }
-        };
-        return new vscode.DebugAdapterExecutable(path, [], dbgOptions);
-      } else if (packageJSONExecutable) {
-        return new vscode.DebugAdapterExecutable(
-          packageJSONExecutable.command,
-          packageJSONExecutable.args,
-          {
-            ...packageJSONExecutable.options,
-            env: {
-              ...packageJSONExecutable.options?.env,
-              ...configEnvironment,
-              ...env,
-            },
-          },
-        );
-      } else {
-        return undefined;
-      }
-    },
-  };
-}
+import { DisposableContext } from "./disposable-context";
+import { LLDBDapOptions } from "./types";
 
 /**
  * This class represents the extension and manages its life cycle. Other extensions
  * using it as as library should use this class as the main entry point.
  */
 export class LLDBDapExtension extends DisposableContext {
-  private lldbDapOptions: LLDBDapOptions;
-
-  constructor(lldbDapOptions: LLDBDapOptions) {
+  constructor() {
     super();
-    this.lldbDapOptions = lldbDapOptions;
-
     this.pushSubscription(
       vscode.debug.registerDebugAdapterDescriptorFactory(
-        this.lldbDapOptions.debuggerType,
-        new LLDBDapDescriptorFactory(this.lldbDapOptions),
+        "lldb-dap",
+        new LLDBDapDescriptorFactory(),
       ),
     );
 
@@ -81,9 +29,7 @@ export class LLDBDapExtension extends DisposableContext {
 
           if (dapPath) {
             const fileUri = vscode.Uri.file(dapPath);
-            if (
-              await LLDBDapDescriptorFactory.isValidDebugAdapterPath(fileUri)
-            ) {
+            if (await LLDBDapDescriptorFactory.isValidFile(fileUri)) {
               return;
             }
           }
@@ -98,7 +44,5 @@ export class LLDBDapExtension extends DisposableContext {
  * This is the entry point when initialized by VS Code.
  */
 export function activate(context: vscode.ExtensionContext) {
-  context.subscriptions.push(
-    new LLDBDapExtension(createDefaultLLDBDapOptions()),
-  );
+  context.subscriptions.push(new LLDBDapExtension());
 }
