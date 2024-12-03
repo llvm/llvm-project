2526d5b1689389da9b194b5ec2878cfb2f4aca93
Pavel Labath <pavel@labath.sk>
2024-12-03 10:27:31 +0100

    Revert "[lldb] Use the function block as a source for function ranges (#117996)"
    This reverts commit ba14dac481564000339ba22ab867617590184f4c. I guess
    "has no conflicts" doesn't mean "it will build".

--- lldb/source/Plugins/SymbolFile/NativePDB/SymbolFileNativePDB.cpp
+++ lldb/source/Plugins/SymbolFile/NativePDB/SymbolFileNativePDB.cpp
@@ -394,12 +394,18 @@ Block *SymbolFileNativePDB::CreateBlock(PdbCompilandSymId block_id) {
 
   switch (sym.kind()) {
   case S_GPROC32:
-  case S_LPROC32:
+  case S_LPROC32: {
     // This is a function.  It must be global.  Creating the Function entry
     // for it automatically creates a block for it.
-    if (FunctionSP func = GetOrCreateFunction(block_id, *comp_unit))
-      return &func->GetBlock(false);
+    FunctionSP func = GetOrCreateFunction(block_id, *comp_unit);
+    if (func) {
+      Block &block = func->GetBlock(false);
+      if (block.GetNumRanges() == 0)
+        block.AddRange(Block::Range(0, func->GetAddressRange().GetByteSize()));
+      return &block;
+    }
     break;
+  }
   case S_BLOCK32: {
     // This is a block.  Its parent is either a function or another block.  In
     // either case, its parent can be viewed as a block (e.g. a function
