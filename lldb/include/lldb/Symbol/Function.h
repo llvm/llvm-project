aebc0ab60fef93828c9b6e951b71cedb899a4f54
Pavel Labath <pavel@labath.sk>
2024-12-03 10:14:33 +0100

    [lldb] Expose discontinuous functions through SBFunction::GetRanges (#117532)
    SBFunction::GetEndAddress doesn't really make sense for discontinuous
    functions, so I'm declaring it deprecated. GetStartAddress sort of makes
    sense, if one uses it to find the functions entry point, so I'm keeping
    that undeprecated.
    
    I've made the test a Shell tests because these make it easier to create
    discontinuous functions regardless of the host os and architecture. They
    do make testing the python API harder, but I think I've managed to come
    up with something not entirely unreasonable.

--- lldb/include/lldb/Symbol/Function.h
+++ lldb/include/lldb/Symbol/Function.h
@@ -444,7 +444,10 @@ public:
 
   Function *CalculateSymbolContextFunction() override;
 
+  /// DEPRECATED: Use GetAddressRanges instead.
   const AddressRange &GetAddressRange() { return m_range; }
+
+  const AddressRanges &GetAddressRanges() const { return m_ranges; }
 
   lldb::LanguageType GetLanguage() const;
   /// Find the file and line number of the source location of the start of the
