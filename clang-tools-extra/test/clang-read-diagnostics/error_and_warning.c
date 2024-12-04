a0d57dafa483f1f11e718085e8940a09e08d177f
Yuxuan Chen <ych@meta.com>
2024-12-04 11:06:31 -0800

    require clang to be available for the test

--- clang-tools-extra/test/clang-read-diagnostics/error_and_warning.c
+++ clang-tools-extra/test/clang-read-diagnostics/error_and_warning.c
@@ -1,3 +1,4 @@
+// REQUIRES: clang
 // RUN: not %clang %s -serialize-diagnostics %s.diag
 // RUN: clang-read-diagnostics %s.diag 2>&1 | FileCheck %s
 // RUN: rm %s.diag
@@ -5,10 +6,10 @@
 #include <stdio.h>
 
 int forgot_return() {
-  // CHECK: error_and_warning.c:9:1: non-void function does not return a value [category='Semantic Issue', flag=return-type]
+  // CHECK: error_and_warning.c:10:1: non-void function does not return a value [category='Semantic Issue', flag=return-type]
 }
 
 int main() {
-  // CHECK: error_and_warning.c:13:25: expected ';' after expression [category='Parse Issue']
+  // CHECK: error_and_warning.c:14:25: expected ';' after expression [category='Parse Issue']
   printf("Hello world!")
 }
