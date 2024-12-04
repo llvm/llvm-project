85c252ca328b0f99d61d66b240d38afb07ef6c2e
Timm Bäder <tbaeder@redhat.com>
2024-12-04 11:43:43 +0100

    Revert "[clang][bytecode] Handle bitcasts involving bitfields (#116843)"
    This reverts commit 4b5e7fa4de54e00df007ae5e2675393fd046aa59.
    
    This breaks builders:
    https://lab.llvm.org/buildbot/#/builders/154/builds/8464
    
    I guess some more testing on 32 bit hosts is needed.

--- clang/lib/AST/ByteCode/Integral.h
+++ clang/lib/AST/ByteCode/Integral.h
@@ -183,7 +183,6 @@ public:
   }
 
   Integral truncate(unsigned TruncBits) const {
-    assert(TruncBits >= 1);
     if (TruncBits >= Bits)
       return *this;
     const ReprT BitMask = (ReprT(1) << ReprT(TruncBits)) - 1;
