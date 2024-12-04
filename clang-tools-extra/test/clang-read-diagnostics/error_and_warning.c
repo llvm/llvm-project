8af94169a2e575f862b7c2958f4260e1cb9a2504
Yuxuan Chen <ych@meta.com>
2024-12-04 15:53:19 -0800

    use %t for temporary files

--- clang-tools-extra/test/clang-read-diagnostics/error_and_warning.c
+++ clang-tools-extra/test/clang-read-diagnostics/error_and_warning.c
@@ -1,15 +1,14 @@
 // REQUIRES: clang
-// RUN: not %clang %s -serialize-diagnostics %s.diag
-// RUN: clang-read-diagnostics %s.diag 2>&1 | FileCheck %s
-// RUN: rm %s.diag
+// RUN: not %clang %s -serialize-diagnostics %t
+// RUN: clang-read-diagnostics %t 2>&1 | FileCheck %s
 
 #include <stdio.h>
 
 int forgot_return() {
-  // CHECK: error_and_warning.c:10:1: non-void function does not return a value [category='Semantic Issue', flag=return-type]
+  // CHECK: error_and_warning.c:9:1: non-void function does not return a value [category='Semantic Issue', flag=return-type]
 }
 
 int main() {
-  // CHECK: error_and_warning.c:14:25: expected ';' after expression [category='Parse Issue']
+  // CHECK: error_and_warning.c:13:25: expected ';' after expression [category='Parse Issue']
   printf("Hello world!")
 }
