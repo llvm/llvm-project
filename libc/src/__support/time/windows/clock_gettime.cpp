5b5ed2e23192aa19728c8f10e910a4798789711e
Schrodinger ZHU Yifan <i@zhuyi.fan>
2024-12-04 11:11:49 -0800

    add two more clock

--- libc/src/__support/time/windows/clock_gettime.cpp
+++ libc/src/__support/time/windows/clock_gettime.cpp
@@ -40,6 +40,7 @@ static long long get_ticks_per_second() {
 
 ErrorOr<int> clock_gettime(clockid_t clockid, timespec *ts) {
   using namespace time_units;
+  constexpr unsigned long long HNS_PER_SEC = 1_s_ns / 100ULL;
   constexpr long long SEC_LIMIT =
       cpp::numeric_limits<decltype(ts->tv_sec)>::max();
   ErrorOr<int> ret = 0;
@@ -81,7 +82,6 @@ ErrorOr<int> clock_gettime(clockid_t clockid, timespec *ts) {
     time.HighPart = file_time.dwHighDateTime;
 
     // adjust to POSIX epoch (from Jan 1, 1601 to Jan 1, 1970)
-    constexpr unsigned long long HNS_PER_SEC = 1_s_ns / 100ULL;
     constexpr unsigned long long POSIX_TIME_SHIFT =
         (11644473600ULL * HNS_PER_SEC);
     if (LIBC_UNLIKELY(POSIX_TIME_SHIFT > time.QuadPart)) {
@@ -97,6 +97,54 @@ ErrorOr<int> clock_gettime(clockid_t clockid, timespec *ts) {
     }
     ts->tv_sec = static_cast<decltype(ts->tv_sec)>(tv_sec);
     ts->tv_nsec = static_cast<decltype(ts->tv_nsec)>(tv_nsec);
+    break;
+  }
+  case CLOCK_PROCESS_CPUTIME_ID:
+  case CLOCK_THREAD_CPUTIME_ID: {
+    [[clang::uninitialized]] FILETIME creation_time;
+    [[clang::uninitialized]] FILETIME exit_time;
+    [[clang::uninitialized]] FILETIME kernel_time;
+    [[clang::uninitialized]] FILETIME user_time;
+    bool success;
+    if (clockid == CLOCK_PROCESS_CPUTIME_ID) {
+      // https://learn.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-getprocesstimes
+      success = ::GetProcessTimes(::GetCurrentProcess(), &creation_time,
+                                  &exit_time, &kernel_time, &user_time);
+    } else {
+      // https://learn.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-getthreadtimes
+      success = ::GetThreadTimes(::GetCurrentThread(), &creation_time,
+                                 &exit_time, &kernel_time, &user_time);
+    }
+    if (!success) {
+      ret = cpp::unexpected(EINVAL);
+      break;
+    }
+    // https://learn.microsoft.com/en-us/windows/win32/api/minwinbase/ns-minwinbase-filetime
+    // It is not recommended that you add and subtract values from the FILETIME
+    // structure to obtain relative times. Instead, you should copy the low- and
+    // high-order parts of the file time to a ULARGE_INTEGER structure, perform
+    // 64-bit arithmetic on the QuadPart member, and copy the LowPart and
+    // HighPart members into the FILETIME structure.
+    auto kernel_time_hns = cpp::bit_cast<ULARGE_INTEGER>(kernel_time);
+    auto user_time_hns = cpp::bit_cast<ULARGE_INTEGER>(user_time);
+    auto total_time_hns = kernel_time_hns.QuadPart + user_time_hns.QuadPart;
+
+    if (LIBC_UNLIKELY(tv_sec > SEC_LIMIT)) {
+      ret = cpp::unexpected(EOVERFLOW);
+      break;
+    }
+
+    unsigned long long tv_sec = total_time_hns / HNS_PER_SEC;
+    unsigned long long tv_nsec = (total_time_hns % HNS_PER_SEC) * 100ULL;
+
+    if (LIBC_UNLIKELY(tv_sec > SEC_LIMIT)) {
+      ret = cpp::unexpected(EOVERFLOW);
+      break;
+    }
+
+    ts->tv_sec = static_cast<decltype(ts->tv_sec)>(tv_sec);
+    ts->tv_nsec = static_cast<decltype(ts->tv_nsec)>(tv_nsec);
+
     break;
   }
   }
