37696d18d0d12cbb8c97cccd1ee239d59ce632c7
abhishek-kaushik22 <abhishek.kaushik@intel.com>
2024-12-04 16:28:54 +0530

    Only check for type f80

--- llvm/lib/Target/X86/X86ISelLowering.cpp
+++ llvm/lib/Target/X86/X86ISelLowering.cpp
@@ -24228,11 +24228,7 @@ SDValue X86TargetLowering::LowerSETCC(SDValue Op, SelectionDAG &DAG) const {
   if (Subtarget.hasAVX10_2()) {
     if (CC == ISD::SETOEQ || CC == ISD::SETUNE) {
       auto NewCC = (CC == ISD::SETOEQ) ? X86::COND_E : (X86::COND_NE);
-      auto isValidType = [&](MVT Type) {
-        return Type == MVT::f16 || Type == MVT::f32 || Type == MVT::f64;
-      };
-      if (isValidType(Op0.getSimpleValueType()) &&
-          isValidType(Op1.getSimpleValueType()))
+      if (Op0.getSimpleValueType() != MVT::f80)
         return getSETCC(
             NewCC, DAG.getNode(X86ISD::UCOMX, dl, MVT::i32, Op0, Op1), dl, DAG);
     }
