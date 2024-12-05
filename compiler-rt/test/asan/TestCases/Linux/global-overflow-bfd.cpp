e103c2ac9efe3b6c41f2d450086e98179c49802e
Vitaly Buka <vitalybuka@google.com>
2024-12-05 11:05:11 -0800

    [𝘀𝗽𝗿] initial version
    Created using spr 1.3.4

--- compiler-rt/test/asan/TestCases/Linux/global-overflow-bfd.cpp
+++ compiler-rt/test/asan/TestCases/Linux/global-overflow-bfd.cpp
@@ -2,6 +2,8 @@
 // false negatives with the BFD linker.
 // RUN: %clangxx_asan -fuse-ld=bfd -Wl,-gc-sections -ffunction-sections -fdata-sections -O0 %s -o %t && not %run %t 2>&1 | FileCheck %s
 
+// UNSUPPORTED: android
+
 #include <string.h>
 int main(int argc, char **argv) {
   static char XXX[10];
