b71784c1a011af21dc9addd33884cf9f9f8401f3
Michael Kruse <llvm-project@meinersbur.de>
2024-12-05 14:39:33 +0100

    Post-merge fixes

--- flang-rt/lib/flang_rt/io-error.h
+++ flang-rt/lib/flang_rt/io-error.h
@@ -15,7 +15,6 @@
 #ifndef FLANG_RT_IO_ERROR_H_
 #define FLANG_RT_IO_ERROR_H_
 
-#include "flang-rt/iostat-funcs.h"
 #include "flang-rt/memory.h"
 #include "terminator.h"
 #include "flang/Runtime/iostat.h"
--- flang/include/flang/Runtime/iostat.h
+++ flang/include/flang/Runtime/iostat.h
@@ -1,10 +1,4 @@
-<<<<<<<< HEAD:flang-rt/include/flang-rt/iostat-funcs.h
-//===-- include/flang-rt/iostat-funcs.h -------------------------*- C++ -*-===//
-|||||||| c4faf0574a3c:flang/include/flang/Runtime/iostat-funcs.h
-//===-- include/flang/Runtime/iostat-funcs.h --------------------*- C++ -*-===//
-========
 //===-- include/flang/Runtime/iostat.h --------------------------*- C++ -*-===//
->>>>>>>> 64bade38fd6a3e5c8c081bae92ce02f58cab799f:flang/include/flang/Runtime/iostat.h
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -15,16 +9,8 @@
 // Defines the values returned by the runtime for IOSTAT= specifiers
 // on I/O statements.
 
-<<<<<<<< HEAD:flang-rt/include/flang-rt/iostat-funcs.h
-#ifndef FLANG_RT_IOSTAT_FUNCS_H_
-#define FLANG_RT_IOSTAT_FUNCS_H_
-|||||||| c4faf0574a3c:flang/include/flang/Runtime/iostat-funcs.h
-#ifndef FORTRAN_RUNTIME_IOSTAT_FUNCS_H_
-#define FORTRAN_RUNTIME_IOSTAT_FUNCS_H_
-========
 #ifndef FORTRAN_RUNTIME_IOSTAT_H_
 #define FORTRAN_RUNTIME_IOSTAT_H_
->>>>>>>> 64bade38fd6a3e5c8c081bae92ce02f58cab799f:flang/include/flang/Runtime/iostat.h
 
 #include "flang/Common/api-attrs.h"
 #include "flang/Runtime/iostat-consts.h"
