23a56b4f1f55173d2e5d029f5f5cd35043e125f8
Adam Nemet <anemet@apple.com>
2024-12-05 21:49:43 -0800

    Fix build issues after resolving merge conflict

--- clang/include/clang/AST/ExternalASTSource.h
+++ clang/include/clang/AST/ExternalASTSource.h
@@ -157,11 +157,6 @@ public:
     return false;
   }
 
-  virtual bool FindExternalVisibleMethodsByName(const DeclContext *DC,
-                                                DeclarationName Name) {
-    return false;
-  }
-
   /// Load all the external specializations for the Decl \param D if \param
   /// OnlyPartial is false. Otherwise, load all the external **partial**
   /// specializations for the \param D.
