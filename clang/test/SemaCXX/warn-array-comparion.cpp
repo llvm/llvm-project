5280439682bc428fcea8cf6ca0c17146ae1ef9c8
AmrDeveloper <amr96@programmer.net>
2024-12-07 14:12:47 +0100

    Fix typo in diagnostic message

--- clang/test/SemaCXX/warn-array-comparion.cpp
+++ clang/test/SemaCXX/warn-array-comparion.cpp
@@ -10,14 +10,14 @@ typedef struct {
 
 bool object_equal(const Object &obj1, const Object &obj2) {
   if (obj1.str != obj2.str) // not-cxx20-warning {{comparison between two arrays compare their addresses}} cxx20-warning {{comparison between two arrays is deprecated}}
-    return false;           // cxx26-error@-1 {{comparison between two arrays is ll-formed in C++26}}
+    return false;           // cxx26-error@-1 {{comparison between two arrays is ill-formed in C++26}}
   if (obj1.id != obj2.id) // not-cxx20-warning {{comparison between two arrays compare their addresses}} cxx20-warning {{comparison between two arrays is deprecated}}
-    return false;         // cxx26-error@-1 {{comparison between two arrays is ll-formed in C++26}}
+    return false;         // cxx26-error@-1 {{comparison between two arrays is ill-formed in C++26}}
   return true;
 }
 
 
 void foo(int (&array1)[2], int (&array2)[2]) {
   if (array1 == array2) { } // not-cxx20-warning {{comparison between two arrays compare their addresses}} cxx20-warning {{comparison between two arrays is deprecated}}
-                            // cxx26-error@-1 {{comparison between two arrays is ll-formed in C++26}}
+                            // cxx26-error@-1 {{comparison between two arrays is ill-formed in C++26}}
 }
