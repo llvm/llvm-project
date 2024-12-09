7bca5d534c56e5b2c08a8886e5dcadc2b6d87193
Daniel Thornburgh <dthorn@google.com>
2024-12-09 11:51:12 -0800

    Parse whylive arg

--- lld/ELF/Config.h
+++ lld/ELF/Config.h
@@ -223,6 +223,7 @@ struct Config {
   llvm::StringRef thinLTOCacheDir;
   llvm::StringRef thinLTOIndexOnlyArg;
   llvm::StringRef whyExtract;
+  llvm::SmallVector<llvm::GlobPattern, 0> whyLive;
   llvm::StringRef cmseInputLib;
   llvm::StringRef cmseOutputLib;
   StringRef zBtiReport = "none";
