746e03c4c5d049e067cef25854137c130c750551
Necip Fazil Yildiran <necip@google.com>
2024-12-10 14:54:55 -0800

    [𝘀𝗽𝗿] changes introduced through rebase
    Created using spr 1.3.6-beta.1
    
    [skip ci]

--- llvm/include/llvm/CodeGen/MachineFunction.h
+++ llvm/include/llvm/CodeGen/MachineFunction.h
@@ -496,7 +496,7 @@ private:
   GISelChangeObserver *Observer = nullptr;
 
   using CallSiteInfoMap = DenseMap<const MachineInstr *, CallSiteInfo>;
-  /// Map a call instruction to call site arguments forwarding and type id.
+  /// Map a call instruction to call site arguments forwarding info.
   CallSiteInfoMap CallSitesInfo;
 
   /// A helper function that returns call site info for a give call
@@ -1358,7 +1358,7 @@ public:
     });
   }
 
-  /// Start tracking the arguments passed to the call \p CallI and call type.
+  /// Start tracking the arguments passed to the call \p CallI.
   void addCallSiteInfo(const MachineInstr *CallI, CallSiteInfo &&CallInfo) {
     assert(CallI->isCandidateForCallSiteEntry());
     bool Inserted =
