llvm/llvm-project

commit 42ddfc2fc058652364aacd872968530212e37485

[ELF] Merge parseSymbolVersion and computeIspreemptible
ICF needs isPreemptible, which can be combined with parseSymbolVersion.

lld/ELF/Driver.cpp
@@ -2987,6 +2987,8 @@ template <class ELFT> void LinkerDriver::link(opt::InputArgList &args) {
   if (!ctx.arg.relocatable) {
     llvm::TimeTraceScope timeScope("Process symbol versions");
     ctx.symtab->scanVersionScript();
+
+    parseVersionAndComputeIsPreemptible(ctx);
   }
 
   // Skip the normal linked output if some LTO options are specified.

lld/ELF/ICF.cpp
@@ -461,13 +461,6 @@ static void combineRelocHashes(unsigned cnt, InputSection *isec,
 
 // The main function of ICF.
 template <class ELFT> void ICF<ELFT>::run() {
-  // Compute isPreemptible early. We may add more symbols later, so this loop
-  // cannot be merged with the later computeIsPreemptible() pass which is used
-  // by scanRelocations().
-  if (ctx.arg.hasDynSymTab)
-    for (Symbol *sym : ctx.symtab->getSymbols())
-      sym->isPreemptible = computeIsPreemptible(ctx, *sym);
-
   // Two text sections may have identical content and relocations but different
   // LSDA, e.g. the two functions may have catch blocks of different types. If a
   // text section is referenced by a .eh_frame FDE with LSDA, it is not

lld/ELF/SymbolTable.cpp
@@ -350,13 +350,6 @@ void SymbolTable::scanVersionScript() {
         assignAsterisk(pat, &v, true);
   }
 
-  // Symbol themselves might know their versions because symbols
-  // can contain versions in the form of <name>@<version>.
-  // Let them parse and update their names to exclude version suffix.
-  for (Symbol *sym : symVector)
-    if (sym->hasVersionSuffix)
-      sym->parseSymbolVersion(ctx);
-
   // isPreemptible is false at this point. To correctly compute the binding of a
   // Defined (which is used by includeInDynsym(ctx)), we need to know if it is
   // VER_NDX_LOCAL or not. Compute symbol versions before handling

lld/ELF/Symbols.cpp
@@ -11,6 +11,7 @@
 #include "InputFiles.h"
 #include "InputSection.h"
 #include "OutputSections.h"
+#include "SymbolTable.h"
 #include "SyntheticSections.h"
 #include "Target.h"
 #include "Writer.h"
@@ -345,7 +346,7 @@ bool elf::computeIsPreemptible(Ctx &ctx, const Symbol &sym) {
 
   // Only symbols with default visibility that appear in dynsym can be
   // preempted. Symbols with protected visibility cannot be preempted.
-  if (!sym.includeInDynsym(ctx) || sym.visibility() != STV_DEFAULT)
+  if (sym.visibility() != STV_DEFAULT)
     return false;
 
   // At this point copy relocations have not been created yet, so any
@@ -368,6 +369,20 @@ bool elf::computeIsPreemptible(Ctx &ctx, const Symbol &sym) {
        sym.binding != STB_WEAK))
     return sym.inDynamicList;
   return true;
+}
+
+void elf::parseVersionAndComputeIsPreemptible(Ctx &ctx) {
+  // Symbol themselves might know their versions because symbols
+  // can contain versions in the form of <name>@<version>.
+  // Let them parse and update their names to exclude version suffix.
+  bool hasDynSymTab = ctx.arg.hasDynSymTab;
+  for (Symbol *sym : ctx.symtab->getSymbols()) {
+    if (sym->hasVersionSuffix)
+      sym->parseSymbolVersion(ctx);
+    if (hasDynSymTab)
+      sym->isPreemptible =
+          sym->includeInDynsym(ctx) && computeIsPreemptible(ctx, *sym);
+  }
 }
 
 // Merge symbol properties.

lld/ELF/Symbols.h
@@ -527,6 +527,7 @@ void reportDuplicate(Ctx &, const Symbol &sym, const InputFile *newFile,
                      InputSectionBase *errSec, uint64_t errOffset);
 void maybeWarnUnorderableSymbol(Ctx &, const Symbol *sym);
 bool computeIsPreemptible(Ctx &, const Symbol &sym);
+void parseVersionAndComputeIsPreemptible(Ctx &);
 
 } // namespace lld::elf
 

lld/ELF/Writer.cpp
@@ -297,8 +297,11 @@ static void demoteSymbolsAndComputeIsPreemptible(Ctx &ctx) {
       }
     }
 
-    if (ctx.arg.hasDynSymTab)
-      sym->isPreemptible = computeIsPreemptible(ctx, *sym);
+    if (ctx.arg.hasDynSymTab) {
+      sym->exportDynamic = sym->includeInDynsym(ctx);
+      sym->isPreemptible =
+          sym->exportDynamic && computeIsPreemptible(ctx, *sym);
+    }
   }
 }
 
@@ -1888,7 +1891,7 @@ template <class ELFT> void Writer<ELFT>::finalizeSections() {
       if (ctx.in.symTab)
         ctx.in.symTab->addSymbol(sym);
 
-      if (sym->includeInDynsym(ctx)) {
+      if (sym->exportDynamic) {
         ctx.partitions[sym->partition - 1].dynSymTab->addSymbol(sym);
         if (auto *file = dyn_cast<SharedFile>(sym->file))
           if (file->isNeeded && !sym->isUndefined())

commit 64b4ba363dcb68d9d50f38bf77a99edc944dbce0

[ELF] Test --export-dynamic-symbol on linker-synthesized symbols and hidden symbols
Test linker-synthesized symbols in addReservedSymbols and in
finalizeSections. `isExported` is set in the middle.

The hidden symbol test could have caught an assertion failure introduced
by 42ddfc2fc058652364aacd872968530212e37485 and fixed by
e08b83dca3754f4104d140ac9683cdc8130de99c.

lld/test/ELF/export-dynamic-symbol.s
@@ -10,15 +10,16 @@
 # so that we have time to investigate the issue without losing postcommit CI.
 # UNSUPPORTED: system-windows
 
-# RUN: llvm-mc -filetype=obj -triple=x86_64 %s -o %t.o
+# RUN: rm -rf %t && split-file %s %t && cd %t
+# RUN: llvm-mc -filetype=obj -triple=x86_64 a.s -o %t.o
 
 ## For an executable, --export-dynamic-symbol exports a symbol if it is non-local and defined.
-# RUN: ld.lld -pie --export-dynamic-symbol foo --export-dynamic-symbol qux %t.o -o %t
-# RUN: llvm-nm -D -p %t | FileCheck %s
+# RUN: ld.lld -pie --export-dynamic-symbol foo --export-dynamic-symbol qux %t.o -o out
+# RUN: llvm-nm -D -p out | FileCheck %s
 # RUN: echo '{ foo; };' > %t1.list
 # RUN: echo '{ foo; qux; };' > %t2.list
-# RUN: ld.lld -pie --export-dynamic-symbol-list=%t2.list %t.o -o %t
-# RUN: llvm-nm -D -p %t | FileCheck %s
+# RUN: ld.lld -pie --export-dynamic-symbol-list=%t2.list %t.o -o out
+# RUN: llvm-nm -D -p out | FileCheck %s
 
 ## --export-dynamic exports all non-local defined symbols.
 ## --export-dynamic-symbol is shadowed.
@@ -53,6 +54,20 @@
 # RUN: ld.lld -shared -Bsymbolic --export-dynamic-symbol-list %t1.list %t.o -o %t.preempt
 # RUN: llvm-objdump -d %t.preempt | FileCheck --check-prefix=PLT1 %s
 
+## Hidden symbols cannot be exported by --export-dynamic-symbol family options.
+# RUN: llvm-mc -filetype=obj -triple=x86_64 hidden.s -o hidden.o
+# RUN: ld.lld -pie %t.o hidden.o --dynamic-list hidden.list -o out.hidden
+# RUN: llvm-readelf -s out.hidden | FileCheck %s --check-prefix=HIDDEN
+
+# HIDDEN:      '.dynsym' contains 2 entries:
+# HIDDEN:      NOTYPE GLOBAL DEFAULT [[#]] _end
+# HIDDEN:      '.symtab' contains 6 entries:
+# HIDDEN:      FUNC    LOCAL  HIDDEN  [[#]] foo
+# HIDDEN-NEXT: NOTYPE  LOCAL  HIDDEN  [[#]] _DYNAMIC
+# HIDDEN-NEXT: NOTYPE  GLOBAL DEFAULT [[#]] _start
+# HIDDEN-NEXT: FUNC    GLOBAL DEFAULT [[#]] qux
+# HIDDEN-NEXT: NOTYPE  GLOBAL DEFAULT [[#]] _end
+
 ## 'nomatch' does not match any symbol. Don't warn.
 # RUN: ld.lld --fatal-warnings -shared -Bsymbolic-functions --export-dynamic-symbol nomatch %t.o -o %t.nopreempt2
 # RUN: llvm-objdump -d %t.nopreempt2 | FileCheck --check-prefix=NOPLT %s
@@ -80,6 +95,7 @@
 # NOPLT-NOT: <foo@plt>
 # NOPLT-NOT: <qux@plt>
 
+#--- a.s
 .global _start, foo, qux
 .type foo, @function
 .type qux, @function
@@ -88,3 +104,13 @@ _start:
   call qux
 foo:
 qux:
+
+#--- hidden.s
+.hidden foo
+
+.data
+.quad _DYNAMIC
+.quad _end
+
+#--- hidden.list
+{foo;_end;_DYNAMIC;};