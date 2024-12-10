ca88a0e6f22c191a6f9a4e6547b993da8e11bb73
Shourya Goel <shouryagoel10000@gmail.com>
2024-12-10 11:38:40 +0530

    [libc][complex] fix guard for cfloat128 and float128 (#119330)

--- libc/src/__support/complex_type.h
+++ libc/src/__support/complex_type.h
@@ -36,12 +36,11 @@ template <> struct make_complex<float16> {
   using type = cfloat16;
 };
 #endif
-#if defined(LIBC_TYPES_HAS_CFLOAT128)
-#if !(LIBC_TYPES_LONG_DOUBLE_IS_FLOAT128)
+#if defined(LIBC_TYPES_HAS_CFLOAT128) &&                                       \
+    !defined(LIBC_TYPES_LONG_DOUBLE_IS_FLOAT128)
 template <> struct make_complex<float128> {
   using type = cfloat128;
 };
-#endif
 #endif
 
 template <typename T> using make_complex_t = typename make_complex<T>::type;
@@ -63,12 +62,11 @@ template <> struct make_real<cfloat16> {
   using type = float16;
 };
 #endif
-#if defined(LIBC_TYPES_HAS_CFLOAT128)
-#if !(LIBC_TYPES_CFLOAT128_IS_COMPLEX_LONG_DOUBLE)
+#if defined(LIBC_TYPES_HAS_CFLOAT128) &&                                       \
+    !defined(LIBC_TYPES_CFLOAT128_IS_COMPLEX_LONG_DOUBLE)
 template <> struct make_real<cfloat128> {
   using type = float128;
 };
-#endif
 #endif
 
 template <typename T> using make_real_t = typename make_real<T>::type;
