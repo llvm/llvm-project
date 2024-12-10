1af49ff14e550f79bc3137608e1742468df3ce8c
Tristan Ross <tristan.ross@midstall.com>
2024-12-09 18:36:14 -0800

    [libc] Add unistd overlay

--- libc/hdr/types/uid_t.h
+++ libc/hdr/types/uid_t.h
@@ -1,4 +1,4 @@
-//===-- Definition of macros from unistd.h --------------------------------===//
+//===-- Proxy for uid_t ---------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,17 +6,17 @@
 //
 //===----------------------------------------------------------------------===//
 
-#ifndef LLVM_LIBC_HDR_UNISTD_MACROS_H
-#define LLVM_LIBC_HDR_UNISTD_MACROS_H
+#ifndef LLVM_LIBC_HDR_TYPES_UID_T_H
+#define LLVM_LIBC_HDR_TYPES_UID_T_H
 
 #ifdef LIBC_FULL_BUILD
 
-#include "include/llvm-libc-macros/unistd-macros.h"
+#include "include/llvm-libc-types/uid_t.h"
 
 #else // Overlay mode
 
-#include "unistd_overlay.h"
+#include <sys/types.h>
 
 #endif // LLVM_LIBC_FULL_BUILD
 
-#endif // LLVM_LIBC_HDR_UNISTD_MACROS_H
+#endif // LLVM_LIBC_HDR_TYPES_UID_T_H
