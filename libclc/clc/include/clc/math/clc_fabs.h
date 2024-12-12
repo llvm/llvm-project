c825dc0766cd38c4950259cad18098e9d9950e74
Fraser Cormack <fraser@codeplay.com>
2024-12-12 12:36:44 +0000

    move away from using intrinsics

--- libclc/clc/include/clc/math/clc_fabs.h
+++ libclc/clc/include/clc/math/clc_fabs.h
@@ -1,12 +1,12 @@
 #ifndef __CLC_MATH_CLC_FABS_H__
 #define __CLC_MATH_CLC_FABS_H__
 
-// Map the function to an LLVM intrinsic
+#define __CLC_BODY <clc/math/unary_decl.inc>
 #define __CLC_FUNCTION __clc_fabs
-#define __CLC_INTRINSIC "llvm.fabs"
-#include <clc/math/unary_intrin.inc>
 
-#undef __CLC_INTRINSIC
+#include <clc/math/gentype.inc>
+
+#undef __CLC_BODY
 #undef __CLC_FUNCTION
 
 #endif // __CLC_MATH_CLC_FABS_H__
--- libclc/clc/include/clc/math/clc_floor.h
+++ libclc/clc/include/clc/math/clc_floor.h
@@ -1,12 +1,12 @@
 #ifndef __CLC_MATH_CLC_FLOOR_H__
 #define __CLC_MATH_CLC_FLOOR_H__
 
-// Map the function to an LLVM intrinsic
+#define __CLC_BODY <clc/math/unary_decl.inc>
 #define __CLC_FUNCTION __clc_floor
-#define __CLC_INTRINSIC "llvm.floor"
-#include <clc/math/unary_intrin.inc>
 
-#undef __CLC_INTRINSIC
+#include <clc/math/gentype.inc>
+
+#undef __CLC_BODY
 #undef __CLC_FUNCTION
 
 #endif // __CLC_MATH_CLC_FLOOR_H__
