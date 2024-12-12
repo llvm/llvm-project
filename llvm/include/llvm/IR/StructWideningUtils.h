7b2339741deb3765615b01ebf64ebe3d2e1ec475
Benjamin Maxwell <benjamin.maxwell@arm.com>
2024-12-12 17:24:49 +0000

    Update comment

--- llvm/include/llvm/IR/StructWideningUtils.h
+++ llvm/include/llvm/IR/StructWideningUtils.h
@@ -1,5 +1,4 @@
-//===---- StructWideningUtils.h - Utils for widening scalar to vector calls
-//--==//
+//===--- StructWideningUtils.h - Utils for widening/narrowing struct types ===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
