14b4b9aba794beec8c758d8f9d55756de0e38bd7
Aiden Grossman <aidengrossman@google.com>
2024-12-12 02:18:56 +0000

    test commit

--- llvm/tools/llvm-exegesis/llvm-exegesis.cpp
+++ llvm/tools/llvm-exegesis/llvm-exegesis.cpp
@@ -5,7 +5,6 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
-///
 /// \file
 /// Measures execution properties (latencies/uops) of an instruction.
 ///
