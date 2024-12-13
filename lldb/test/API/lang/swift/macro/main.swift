aee6aae468603734d7c9815f41398ab9687a71da
Jim Ingham <jingham@apple.com>
2024-12-13 12:39:41 -0800

    Revert "Revert "Merge pull request #9493 from jimingham/swift-call-site-breakpoints""
    This reverts commit c93fb6d699b398048459e2b203ccec25ed9e6ee9.
    
    (cherry picked from commit 30dbbe2bb085cb0e23a8923a420099a2b04bab06)

--- lldb/test/API/lang/swift/macro/main.swift
+++ lldb/test/API/lang/swift/macro/main.swift
@@ -1,9 +1,12 @@
 import Macro
-
+// This test depends on the layout of the lines in this file.
 func testStringify(a: Int, b: Int) {
   print("break here")
   let s = #stringify(a / b)
+  #no_return(a)
+  #no_return(b)
+  #no_return(a / b)
   print(s.1)
 }
 
-testStringify(a: 23, b: 0)
+testStringify(a: 23, b: 1)
