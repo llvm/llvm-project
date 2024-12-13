54361b984076b7b42ab073d798271b0df506c6ad
Kirill Stoimenov <kstoimenov@google.com>
2024-12-13 19:45:22 +0000

    Updated the test.

--- compiler-rt/test/ubsan_minimal/TestCases/override-callback.c
+++ compiler-rt/test/ubsan_minimal/TestCases/override-callback.c
@@ -5,12 +5,11 @@
 
 static int Result;
 
-void __ubsan_report_error(const char *msg, uintptr_t caller,
-                          const char *decorated_msg) {
-  fprintf(stderr, "CUSTOM_CALLBACK: %s\n", msg);
+void __ubsan_report_error(const char *msg, uintptr_t caller, int abort) {
+  fprintf(stderr, "CUSTOM_CALLBACK: %s %d\n", msg, abort);
 }
 
 int main(int argc, const char** argv) {
   int32_t t0 = (~((uint32_t)0));
-// CHECK: CUSTOM_CALLBACK: implicit-conversion
+// CHECK: CUSTOM_CALLBACK: implicit-conversion 0
 }
