303f43ee2ff1d15f0afa6357e6171cebf42b0133
Kazu Hirata <kazu@google.com>
2024-12-13 08:03:19 -0800

    [DataFlow] Migrate away from PointerUnion::{is,get} (NFC)
    Note that PointerUnion::{is,get} have been soft deprecated in
    PointerUnion.h:
    
      // FIXME: Replace the uses of is(), get() and dyn_cast() with
      //        isa<T>, cast<T> and the llvm::dyn_cast<T>
    
    I'm not touching PointerUnion::dyn_cast for now because it's a bit
    complicated; we could blindly migrate it to dyn_cast_if_present, but
    we should probably use dyn_cast when the operand is known to be
    non-null.

--- mlir/lib/Analysis/DataFlow/IntegerRangeAnalysis.cpp
+++ mlir/lib/Analysis/DataFlow/IntegerRangeAnalysis.cpp
@@ -43,7 +43,7 @@ void IntegerValueRangeLattice::onUpdate(DataFlowSolver *solver) const {
   // If the integer range can be narrowed to a constant, update the constant
   // value of the SSA value.
   std::optional<APInt> constant = getValue().getValue().getConstantValue();
-  auto value = anchor.get<Value>();
+  auto value = cast<Value>(anchor);
   auto *cv = solver->getOrCreateState<Lattice<ConstantValue>>(value);
   if (!constant)
     return solver->propagateIfChanged(
@@ -155,9 +155,9 @@ void IntegerRangeAnalysis::visitNonControlFlowArguments(
                                   Type boundType, bool getUpper) {
     unsigned int width = ConstantIntRanges::getStorageBitwidth(boundType);
     if (loopBound.has_value()) {
-      if (loopBound->is<Attribute>()) {
+      if (isa<Attribute>(*loopBound)) {
         if (auto bound =
-                dyn_cast_or_null<IntegerAttr>(loopBound->get<Attribute>()))
+                dyn_cast_or_null<IntegerAttr>(cast<Attribute>(*loopBound)))
           return bound.getValue();
       } else if (auto value = llvm::dyn_cast_if_present<Value>(*loopBound)) {
         const IntegerValueRangeLattice *lattice =
