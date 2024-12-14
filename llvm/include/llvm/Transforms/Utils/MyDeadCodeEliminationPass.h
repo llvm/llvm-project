a61f476669c59178cf5f2604a68f6718a7dcbc15
Rashmi Salankar <rashmi.salankar@gmail.com>
2024-12-15 03:38:58 +0530

    Basic implementation of Potential Dead Code intructions done.

--- llvm/include/llvm/Transforms/Utils/MyDeadCodeEliminationPass.h
+++ llvm/include/llvm/Transforms/Utils/MyDeadCodeEliminationPass.h
@@ -1,14 +1,20 @@
-#ifndef LLVM_TRANSFORMS_MYDEADCODEELIMINATIONPASS_H
-#define LLVM_TRANSFORMS_MYDEADCODEELIMINATIONPASS_H
+#ifndef LLVM_TRANSFORMS_UTILS_MYDEADCODEELIMINATIONPASS_H
+#define LLVM_TRANSFORMS_UTILS_MYDEADCODEELIMINATIONPASS_H
 
 #include "llvm/IR/PassManager.h"
+#include "llvm/IR/Function.h"
 
 namespace llvm {
 
 class MyDeadCodeEliminationPass : public PassInfoMixin<MyDeadCodeEliminationPass> {
 public:
   PreservedAnalyses run(Function &F, FunctionAnalysisManager &AM);
+
+private:
+  void analyzeInstructions(Function &F);
 };
 
 } // namespace llvm
-#endif // LLVM_TRANSFORMS_MYDEADCODEELIMINATIONPASS_H
+
+#endif // LLVM_TRANSFORMS_UTILS_MYDEADCODEELIMINATIONPASS_H
+
--- llvm/lib/Transforms/Utils/MyDeadCodeEliminationPass.cpp
+++ llvm/lib/Transforms/Utils/MyDeadCodeEliminationPass.cpp
@@ -1,10 +1,37 @@
 #include "llvm/Transforms/Utils/MyDeadCodeEliminationPass.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Instruction.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/Support/raw_ostream.h"
 
 using namespace llvm;
 
-PreservedAnalyses MyDeadCodeEliminationPass::run(Function &F,
-                                      FunctionAnalysisManager &AM) {
+PreservedAnalyses MyDeadCodeEliminationPass::run(Function &F, FunctionAnalysisManager &AM) {
   errs() << "I'm here in my Pass" << "\n";
-  return PreservedAnalyses::all();
-/* The PreservedAnalyses return value says that all analyses (e.g. dominator tree) are still valid after this pass since we didn’t modify any functions */
+
+  // Call the helper function to analyze instructions
+  analyzeInstructions(F);
+
+  return PreservedAnalyses::all(); // Preserve analyses since the code isn't modified.
 }
+
+void MyDeadCodeEliminationPass::analyzeInstructions(Function &F) {
+  for (BasicBlock &BB : F) {
+    for (Instruction &I : BB) {
+      errs() << "Instruction: " << I << "\n";
+
+      // Check if the instruction is "dead" (not used anywhere)
+      if (I.use_empty()) {
+        errs() << "Potential dead instruction: " << I << "\n";
+      }
+
+      // Count instruction types
+      if (isa<LoadInst>(&I)) {
+        errs() << "Load Instruction\n";
+      } else if (isa<StoreInst>(&I)) {
+        errs() << "Store Instruction\n";
+      }
+    }
+  }
+}
+
