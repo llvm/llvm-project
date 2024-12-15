64664374f53a788938e72e6160fa8f843d6541a3
Youngsuk Kim <youngsuk.kim@hpe.com>
2024-12-15 17:24:06 -0600

    Add FileCheck checks

--- clang/test/OpenMP/amdgpu_threadprivate.cpp
+++ clang/test/OpenMP/amdgpu_threadprivate.cpp
@@ -1,11 +1,17 @@
-// REQUIRES: asserts
-
 // RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -target-cpu x86-64 -disable-llvm-passes -fopenmp-targets=amdgcn-amd-amdhsa -x c++ -emit-llvm-bc %s -o %t-x86-host.bc
-// RUN: %clang_cc1 -triple amdgcn-amd-amdhsa -aux-triple x86_64-unknown-linux-gnu -target-cpu gfx906 -fopenmp -nogpulib -fopenmp-is-target-device -fopenmp-host-ir-file-path %t-x86-host.bc -x c++ %s
+// RUN: %clang_cc1 -triple amdgcn-amd-amdhsa -aux-triple x86_64-unknown-linux-gnu -target-cpu gfx906 -fopenmp -nogpulib -fopenmp-is-target-device -fopenmp-host-ir-file-path %t-x86-host.bc -x c++ -emit-llvm %s -o - | FileCheck %s
 
 // Don't crash with assertions build.
+
+// CHECK:       @MyGlobVar = external thread_local addrspace(1) global i32, align 4
+// CHECK:       define weak_odr hidden noundef ptr @_ZTW9MyGlobVar() #0 comdat {
+// CHECK-NEXT:    %1 = call align 4 ptr addrspace(1) @llvm.threadlocal.address.p1(ptr addrspace(1) align 4 @MyGlobVar)
+// CHECK-NEXT:    %2 = addrspacecast ptr addrspace(1) %1 to ptr
+// CHECK-NEXT:    ret ptr %2
+// CHECK-NEXT:  }
 int MyGlobVar;
 #pragma omp threadprivate(MyGlobVar)
 int main() {
   MyGlobVar = 1;
 }
+
