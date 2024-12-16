ee942ceeeca4a678c6ec9fad448055acff3ac352
Jakub Mazurkiewicz <mazkuba3@gmail.com>
2024-12-16 21:23:18 +0100

    Fix formatting

--- libcxx/test/std/ranges/range.adaptors/range.join.with/types.h
+++ libcxx/test/std/ranges/range.adaptors/range.join.with/types.h
@@ -37,7 +37,8 @@ struct ViewProperties {
 template <std::ranges::input_range Data,
           ViewProperties Prop,
           template <class...> class It,
-          template <class...> class ConstIt = It>
+          template <class...>
+          class ConstIt = It>
 class BasicView : public std::ranges::view_base {
   Data data_;
 
