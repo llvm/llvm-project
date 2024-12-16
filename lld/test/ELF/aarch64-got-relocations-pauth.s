03f9115ae4f3ea45edce60d2aec020bf76d4c2ff
Daniil Kovalev <dkovalev@accesssoftek.com>
2024-12-16 10:39:23 +0300

    Fix check prefixes

--- lld/test/ELF/aarch64-got-relocations-pauth.s
+++ lld/test/ELF/aarch64-got-relocations-pauth.s
@@ -9,62 +9,62 @@
 # RUN: llvm-mc -filetype=obj -triple=aarch64 ok.s -o ok.o
 
 # RUN: ld.lld ok.o a.so -pie -o ok1
-# RUN: llvm-readelf -r -S -x .got ok1 | FileCheck %s --check-prefix=EXTERNAL
+# RUN: llvm-readelf -r -S -x .got ok1 | FileCheck %s --check-prefix=OK1
 
 # RUN: ld.lld ok.o a.o -pie -o ok2
-# RUN: llvm-readelf -r -S -x .got -s ok2 | FileCheck %s --check-prefix=LOCAL
+# RUN: llvm-readelf -r -S -x .got -s ok2 | FileCheck %s --check-prefix=OK2
 
-# EXTERNAL:      Offset            Info             Type                    Symbol's Value   Symbol's Name + Addend
-# EXTERNAL-NEXT: 0000000000020380  0000000100000412 R_AARCH64_AUTH_GLOB_DAT 0000000000000000 bar + 0
-# EXTERNAL-NEXT: 0000000000020388  0000000200000412 R_AARCH64_AUTH_GLOB_DAT 0000000000000000 zed + 0
+# OK1:      Offset            Info             Type                    Symbol's Value   Symbol's Name + Addend
+# OK1-NEXT: 0000000000020380  0000000100000412 R_AARCH64_AUTH_GLOB_DAT 0000000000000000 bar + 0
+# OK1-NEXT: 0000000000020388  0000000200000412 R_AARCH64_AUTH_GLOB_DAT 0000000000000000 zed + 0
 
 ## Symbol's values for bar and zed are equal since they contain no content (see Inputs/shared.s)
-# LOCAL:         Offset            Info             Type                    Symbol's Value   Symbol's Name + Addend
-# LOCAL-NEXT:    0000000000020320  0000000000000411 R_AARCH64_AUTH_RELATIVE 10260
-# LOCAL-NEXT:    0000000000020328  0000000000000411 R_AARCH64_AUTH_RELATIVE 10260
+# OK2:         Offset            Info             Type                    Symbol's Value   Symbol's Name + Addend
+# OK2-NEXT:    0000000000020320  0000000000000411 R_AARCH64_AUTH_RELATIVE 10260
+# OK2-NEXT:    0000000000020328  0000000000000411 R_AARCH64_AUTH_RELATIVE 10260
 
-# EXTERNAL:      Hex dump of section '.got':
-# EXTERNAL-NEXT: 0x00020380 00000000 00000080 00000000 000000a0
+# OK1:      Hex dump of section '.got':
+# OK1-NEXT: 0x00020380 00000000 00000080 00000000 000000a0
 ##                                         ^^
 ##                                         0b10000000 bit 63 address diversity = true, bits 61..60 key = IA
 ##                                                           ^^
 ##                                                           0b10100000 bit 63 address diversity = true, bits 61..60 key = DA
 
-# LOCAL: Symbol table '.symtab' contains {{.*}} entries:
-# LOCAL:    Num:    Value          Size Type    Bind   Vis       Ndx Name
-# LOCAL:         0000000000010260     0 FUNC    GLOBAL DEFAULT     6 bar
-# LOCAL:         0000000000010260     0 NOTYPE  GLOBAL DEFAULT     6 zed
+# OK2: Symbol table '.symtab' contains {{.*}} entries:
+# OK2:    Num:    Value          Size Type    Bind   Vis       Ndx Name
+# OK2:         0000000000010260     0 FUNC    GLOBAL DEFAULT     6 bar
+# OK2:         0000000000010260     0 NOTYPE  GLOBAL DEFAULT     6 zed
 
-# LOCAL:         Hex dump of section '.got':
-# LOCAL-NEXT:    0x00020320 00000000 00000080 00000000 000000a0
+# OK2:         Hex dump of section '.got':
+# OK2-NEXT:    0x00020320 00000000 00000080 00000000 000000a0
 ##                                         ^^
 ##                                         0b10000000 bit 63 address diversity = true, bits 61..60 key = IA
 ##                                                           ^^
 ##                                                           0b10100000 bit 63 address diversity = true, bits 61..60 key = DA
 
-# RUN: llvm-objdump -d ok1 | FileCheck %s --check-prefix=EXTERNAL-ASM
+# RUN: llvm-objdump -d ok1 | FileCheck %s --check-prefix=OK1-ASM
 
-# EXTERNAL-ASM:      <_start>:
-# EXTERNAL-ASM-NEXT: adrp x0, 0x20000
-# EXTERNAL-ASM-NEXT: ldr  x0, [x0, #0x380]
-# EXTERNAL-ASM-NEXT: adrp x1, 0x20000
-# EXTERNAL-ASM-NEXT: add  x1, x1, #0x380
-# EXTERNAL-ASM-NEXT: adrp x0, 0x20000
-# EXTERNAL-ASM-NEXT: ldr  x0, [x0, #0x388]
-# EXTERNAL-ASM-NEXT: adrp x1, 0x20000
-# EXTERNAL-ASM-NEXT: add  x1, x1, #0x388
+# OK1-ASM:      <_start>:
+# OK1-ASM-NEXT: adrp x0, 0x20000
+# OK1-ASM-NEXT: ldr  x0, [x0, #0x380]
+# OK1-ASM-NEXT: adrp x1, 0x20000
+# OK1-ASM-NEXT: add  x1, x1, #0x380
+# OK1-ASM-NEXT: adrp x0, 0x20000
+# OK1-ASM-NEXT: ldr  x0, [x0, #0x388]
+# OK1-ASM-NEXT: adrp x1, 0x20000
+# OK1-ASM-NEXT: add  x1, x1, #0x388
 
-# RUN: llvm-objdump -d ok2 | FileCheck %s --check-prefix=LOCAL-ASM
+# RUN: llvm-objdump -d ok2 | FileCheck %s --check-prefix=OK2-ASM
 
-# LOCAL-ASM:         <_start>:
-# LOCAL-ASM-NEXT:    adrp x0, 0x20000
-# LOCAL-ASM-NEXT:    ldr  x0, [x0, #0x320]
-# LOCAL-ASM-NEXT:    adrp x1, 0x20000
-# LOCAL-ASM-NEXT:    add  x1, x1, #0x320
-# LOCAL-ASM-NEXT:    adrp x0, 0x20000
-# LOCAL-ASM-NEXT:    ldr  x0, [x0, #0x328]
-# LOCAL-ASM-NEXT:    adrp x1, 0x20000
-# LOCAL-ASM-NEXT:    add  x1, x1, #0x328
+# OK2-ASM:         <_start>:
+# OK2-ASM-NEXT:    adrp x0, 0x20000
+# OK2-ASM-NEXT:    ldr  x0, [x0, #0x320]
+# OK2-ASM-NEXT:    adrp x1, 0x20000
+# OK2-ASM-NEXT:    add  x1, x1, #0x320
+# OK2-ASM-NEXT:    adrp x0, 0x20000
+# OK2-ASM-NEXT:    ldr  x0, [x0, #0x328]
+# OK2-ASM-NEXT:    adrp x1, 0x20000
+# OK2-ASM-NEXT:    add  x1, x1, #0x328
 
 .globl _start
 _start:
