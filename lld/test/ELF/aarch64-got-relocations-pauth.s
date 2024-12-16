bc3a2e451efb5e959bc8b3f4ff2a4d9e57a7ba84
Daniil Kovalev <dkovalev@accesssoftek.com>
2024-12-16 10:40:42 +0300

    Fix check prefixes

--- lld/test/ELF/aarch64-got-relocations-pauth.s
+++ lld/test/ELF/aarch64-got-relocations-pauth.s
@@ -81,50 +81,50 @@ _start:
 # RUN: llvm-mc -filetype=obj -triple=aarch64 ok-tiny.s -o ok-tiny.o
 
 # RUN: ld.lld ok-tiny.o a.so -pie -o tiny1
-# RUN: llvm-readelf -r -S -x .got tiny1 | FileCheck %s --check-prefix=EXTERNAL-TINY
+# RUN: llvm-readelf -r -S -x .got tiny1 | FileCheck %s --check-prefix=TINY1
 
 # RUN: ld.lld ok-tiny.o a.o -pie -o tiny2
-# RUN: llvm-readelf -r -S -x .got -s tiny2 | FileCheck %s --check-prefix=LOCAL-TINY
+# RUN: llvm-readelf -r -S -x .got -s tiny2 | FileCheck %s --check-prefix=TINY2
 
-# EXTERNAL-TINY:      Offset            Info             Type                    Symbol's Value   Symbol's Name + Addend
-# EXTERNAL-TINY-NEXT: 0000000000020368  0000000100000412 R_AARCH64_AUTH_GLOB_DAT 0000000000000000 bar + 0
-# EXTERNAL-TINY-NEXT: 0000000000020370  0000000200000412 R_AARCH64_AUTH_GLOB_DAT 0000000000000000 zed + 0
+# TINY1:      Offset            Info             Type                    Symbol's Value   Symbol's Name + Addend
+# TINY1-NEXT: 0000000000020368  0000000100000412 R_AARCH64_AUTH_GLOB_DAT 0000000000000000 bar + 0
+# TINY1-NEXT: 0000000000020370  0000000200000412 R_AARCH64_AUTH_GLOB_DAT 0000000000000000 zed + 0
 
 ## Symbol's values for bar and zed are equal since they contain no content (see Inputs/shared.s)
-# LOCAL-TINY:         Offset            Info             Type                    Symbol's Value   Symbol's Name + Addend
-# LOCAL-TINY-NEXT:    0000000000020308  0000000000000411 R_AARCH64_AUTH_RELATIVE 10248
-# LOCAL-TINY-NEXT:    0000000000020310  0000000000000411 R_AARCH64_AUTH_RELATIVE 10248
+# TINY2:         Offset            Info             Type                    Symbol's Value   Symbol's Name + Addend
+# TINY2-NEXT:    0000000000020308  0000000000000411 R_AARCH64_AUTH_RELATIVE 10248
+# TINY2-NEXT:    0000000000020310  0000000000000411 R_AARCH64_AUTH_RELATIVE 10248
 
-# EXTERNAL-TINY:      Hex dump of section '.got':
-# EXTERNAL-TINY-NEXT: 0x00020368 00000000 00000080 00000000 000000a0
+# TINY1:      Hex dump of section '.got':
+# TINY1-NEXT: 0x00020368 00000000 00000080 00000000 000000a0
 ##                                              ^^
 ##                                              0b10000000 bit 63 address diversity = true, bits 61..60 key = IA
 ##                                                                ^^
 ##                                                                0b10100000 bit 63 address diversity = true, bits 61..60 key = DA
 
-# LOCAL-TINY: Symbol table '.symtab' contains {{.*}} entries:
-# LOCAL-TINY:    Num:    Value          Size Type    Bind   Vis       Ndx Name
-# LOCAL-TINY:         0000000000010248     0 FUNC    GLOBAL DEFAULT     6 bar
-# LOCAL-TINY:         0000000000010248     0 NOTYPE  GLOBAL DEFAULT     6 zed
+# TINY2: Symbol table '.symtab' contains {{.*}} entries:
+# TINY2:    Num:    Value          Size Type    Bind   Vis       Ndx Name
+# TINY2:         0000000000010248     0 FUNC    GLOBAL DEFAULT     6 bar
+# TINY2:         0000000000010248     0 NOTYPE  GLOBAL DEFAULT     6 zed
 
-# LOCAL-TINY:         Hex dump of section '.got':
-# LOCAL-TINY-NEXT:    0x00020308 00000000 00000080 00000000 000000a0
+# TINY2:         Hex dump of section '.got':
+# TINY2-NEXT:    0x00020308 00000000 00000080 00000000 000000a0
 ##                                              ^^
 ##                                              0b10000000 bit 63 address diversity = true, bits 61..60 key = IA
 ##                                                                ^^
 ##                                                                0b10100000 bit 63 address diversity = true, bits 61..60 key = DA
 
-# RUN: llvm-objdump -d tiny1 | FileCheck %s --check-prefix=EXTERNAL-TINY-ASM
+# RUN: llvm-objdump -d tiny1 | FileCheck %s --check-prefix=TINY1-ASM
 
-# EXTERNAL-TINY-ASM:      <_start>:
-# EXTERNAL-TINY-ASM-NEXT: adr x0, 0x20368
-# EXTERNAL-TINY-ASM-NEXT: ldr x1, 0x20370
+# TINY1-ASM:      <_start>:
+# TINY1-ASM-NEXT: adr x0, 0x20368
+# TINY1-ASM-NEXT: ldr x1, 0x20370
 
-# RUN: llvm-objdump -d tiny2 | FileCheck %s --check-prefix=LOCAL-TINY-ASM
+# RUN: llvm-objdump -d tiny2 | FileCheck %s --check-prefix=TINY2-ASM
 
-# LOCAL-TINY-ASM:         <_start>:
-# LOCAL-TINY-ASM-NEXT:    adr x0, 0x20308
-# LOCAL-TINY-ASM-NEXT:    ldr x1, 0x20310
+# TINY2-ASM:         <_start>:
+# TINY2-ASM-NEXT:    adr x0, 0x20308
+# TINY2-ASM-NEXT:    ldr x1, 0x20310
 
 .globl _start
 _start:
