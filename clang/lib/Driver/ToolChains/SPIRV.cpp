0557f8932f2c2276b99abc45935f7cec20c68799
Sarnie, Nick <nick.sarnie@intel.com>
2024-12-17 07:52:27 -0800

    remove spir-v fix
    Signed-off-by: Sarnie, Nick <nick.sarnie@intel.com>

--- clang/lib/Driver/ToolChains/SPIRV.cpp
+++ clang/lib/Driver/ToolChains/SPIRV.cpp
@@ -29,10 +29,8 @@ void SPIRV::constructTranslateCommand(Compilation &C, const Tool &T,
   if (Input.getType() == types::TY_PP_Asm)
     CmdArgs.push_back("-to-binary");
 
-  // The text output from spirv-dis is not in the format expected
-  // by llvm-spirv, so use the text output from llvm-spirv.
   if (Output.getType() == types::TY_PP_Asm)
-    CmdArgs.push_back("--spirv-text");
+    CmdArgs.push_back("--spirv-tools-dis");
 
   CmdArgs.append({"-o", Output.getFilename()});
 
