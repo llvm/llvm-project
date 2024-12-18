6483ce91c26e66383474f203c7f46b9cc4473991
Nathan Chancellor <nathan@kernel.org>
2024-12-18 14:24:56 -0700

    fixup! [Sema] Fix tautological bounds check warning with -fwrapv
    -verify=expected is the same thing as -verify
    
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- clang/test/Sema/tautological-pointer-comparison.c
+++ clang/test/Sema/tautological-pointer-comparison.c
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -fsyntax-only -verify=expected %s
+// RUN: %clang_cc1 -fsyntax-only -verify %s
 // RUN: %clang_cc1 -fsyntax-only -fwrapv -verify=fwrapv %s
 
 // fwrapv-no-diagnostics
