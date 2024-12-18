4434099c2b5faf74ca0c71c984b9a13373820afd
Rashmi Salankar <rashmi.salankar@gmail.com>
2024-12-19 01:59:21 +0530

    Added more features. Running but needs to be corrected and enhanced

--- llvm/include/llvm/Transforms/Utils/MyDeadCodeEliminationPass.h
+++ llvm/include/llvm/Transforms/Utils/MyDeadCodeEliminationPass.h
@@ -2,23 +2,23 @@
 #define LLVM_TRANSFORMS_UTILS_MYDEADCODEELIMINATIONPASS_H
 
 #include "llvm/IR/PassManager.h"
-#include "llvm/IR/Function.h"
-#include "llvm/IR/Instruction.h"
+#include "llvm/Analysis/LoopInfo.h"
 #include <unordered_set>
+#include <string>
 
 namespace llvm {
 
 class MyDeadCodeEliminationPass : public PassInfoMixin<MyDeadCodeEliminationPass> {
 public:
-  // Main run method for the pass
-  PreservedAnalyses run(Function &F, FunctionAnalysisManager &AM);
+    PreservedAnalyses run(Function &F, FunctionAnalysisManager &AM);
 
 private:
-  // Helper function for iterative analysis
-  void analyzeInstructionsIteratively(Function &F);
-
-  // Helper function to check if an instruction is dead
-  bool isInstructionDead(Instruction *Inst, const std::unordered_set<Instruction *> &potentialDeadInstructions);
+    void analyzeInstructionsIteratively(Function &F, FunctionAnalysisManager &AM);
+    bool isInstructionDead(Instruction *Inst, const std::unordered_set<Instruction *> &potentialDeadInstructions);
+    void printBasicBlockFeatures(const Instruction &I, const BasicBlock &BB);
+    void printInstructionFeatures(const Instruction &I, const Function &F, const LoopInfo &LI);
+    void printUsageFeatures(const Instruction &I, const std::unordered_set<Instruction *> &potentialDeadInstructions);
+    std::string getInstructionPosition(const Instruction &I, const BasicBlock &BB);
 };
 
 } // namespace llvm
--- llvm/lib/Transforms/Utils/MyDeadCodeEliminationPass.cpp
+++ llvm/lib/Transforms/Utils/MyDeadCodeEliminationPass.cpp
@@ -3,57 +3,121 @@
 #include "llvm/IR/Instruction.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/Module.h"
+#include "llvm/Analysis/LoopInfo.h"
 #include <unordered_set>
 
 using namespace llvm;
 
 PreservedAnalyses MyDeadCodeEliminationPass::run(Function &F, FunctionAnalysisManager &AM) {
-  errs() << "I'm here in my Pass" << "\n";
+    errs() << "Starting MyDeadCodeEliminationPass\n";
 
-  // Call the helper function to iteratively analyze instructions
-  analyzeInstructionsIteratively(F);
+    analyzeInstructionsIteratively(F, AM);
 
-  return PreservedAnalyses::all(); // Preserve analyses since the code isn't modified.
+    return PreservedAnalyses::all();
 }
 
-void MyDeadCodeEliminationPass::analyzeInstructionsIteratively(Function &F) {
-  std::unordered_set<Instruction *> potentialDeadInstructions; // To track potential dead instructions
-  bool foundNewDead; // Flag to track if we find new dead instructions in an iteration
+void MyDeadCodeEliminationPass::analyzeInstructionsIteratively(Function &F, FunctionAnalysisManager &AM) {
+    auto &LI = AM.getResult<LoopAnalysis>(F);
+    std::unordered_set<Instruction *> potentialDeadInstructions;
+    bool foundNewDead;
 
-  do {
-    foundNewDead = false; // Reset the flag at the start of each iteration
+    do {
+        foundNewDead = false;
 
-    for (BasicBlock &BB : F) {
-      for (Instruction &I : BB) {
-        // Skip if already identified as dead
-        if (potentialDeadInstructions.count(&I)) {
-          continue;
+        for (BasicBlock &BB : F) {
+            for (Instruction &I : BB) {
+                if (potentialDeadInstructions.count(&I)) {
+                    continue;
+                }
+
+                // Check if the instruction is "likely dead"
+                if (isInstructionDead(&I, potentialDeadInstructions)) {
+                    errs() << "Likely Dead Instruction: " << I << "\n";
+                    potentialDeadInstructions.insert(&I);
+                    foundNewDead = true;
+                }
+
+                // Print all features for this instruction
+                errs() << "------------------------------------------\n";
+                errs() << "Instruction: " << I << "\n";
+                printBasicBlockFeatures(I, BB);
+                printInstructionFeatures(I, F, LI);
+                printUsageFeatures(I, potentialDeadInstructions);
+                errs() << "------------------------------------------\n";
+            }
         }
-
-        // Check if the instruction is "dead" (not used anywhere or used only by dead instructions)
-        if (isInstructionDead(&I, potentialDeadInstructions)) {
-          errs() << "Potential dead instruction: " << I << "\n";
-          potentialDeadInstructions.insert(&I);
-          foundNewDead = true; // Mark that we found a new dead instruction
-        }
-      }
-    }
-  } while (foundNewDead); // Continue until no new dead instructions are found
+    } while (foundNewDead);
 }
 
 bool MyDeadCodeEliminationPass::isInstructionDead(Instruction *Inst, const std::unordered_set<Instruction *> &potentialDeadInstructions) {
-  // Check if the instruction's result is not used, or all users are in the potentialDeadInstructions set
-  if (Inst->use_empty()) {
-    return true; // No users, definitely dead
-  }
-
-  for (const Use &U : Inst->uses()) {
-    auto *User = dyn_cast<Instruction>(U.getUser());
-    if (!User || potentialDeadInstructions.find(User) == potentialDeadInstructions.end()) {
-      return false; // Found a user that is not "dead"
+    if (Inst->use_empty()) {
+        return true;
     }
-  }
 
-  return true; // All users are "dead"
+    for (const Use &U : Inst->uses()) {
+        auto *User = dyn_cast<Instruction>(U.getUser());
+        if (!User || potentialDeadInstructions.find(User) == potentialDeadInstructions.end()) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+void MyDeadCodeEliminationPass::printBasicBlockFeatures(const Instruction &I, const BasicBlock &BB) {
+    errs() << "Parent Basic Block: " << BB.getName() << "\n";
+    errs() << "  Number of Predecessors: " << std::distance(pred_begin(&BB), pred_end(&BB)) << "\n";
+    errs() << "  Number of Successors: " << std::distance(succ_begin(&BB), succ_end(&BB)) << "\n";
+    errs() << "  Position in Basic Block: " << getInstructionPosition(I, BB) << "\n";
+    errs() << "  Total Instructions in Basic Block: " << BB.size() << "\n";
+}
+
+void MyDeadCodeEliminationPass::printInstructionFeatures(const Instruction &I, const Function &F, const LoopInfo &LI) {
+    errs() << "Instruction Type: " << I.getOpcodeName() << "\n";
+    errs() << "Operand Count: " << I.getNumOperands() << "\n";
+    errs() << "Is Terminator: " << (I.isTerminator() ? "Yes" : "No") << "\n";
+    errs() << "Is Memory Related: " << (isa<LoadInst>(&I) || isa<StoreInst>(&I) ? "Yes" : "No") << "\n";
+
+    if (LI.getLoopFor(I.getParent())) {
+        errs() << "Part of a Loop: Yes\n";
+    } else {
+        errs() << "Part of a Loop: No\n";
+    }
+}
+
+
+void MyDeadCodeEliminationPass::printUsageFeatures(const Instruction &I, const std::unordered_set<Instruction *> &potentialDeadInstructions) {
+    errs() << "Uses: ";
+    for (const Use &U : I.uses()) {
+        const auto *User = dyn_cast<Instruction>(U.getUser());
+        if (User) {
+            errs() << User->getOpcodeName() << " ";
+            // Fix: Cast to match the type stored in the set
+            if (potentialDeadInstructions.count(const_cast<Instruction *>(User))) {
+                errs() << "(likely dead) ";
+            }
+        }
+    }
+    errs() << "\n";
+    errs() << "Number of Users: " << I.getNumUses() << "\n";
+}
+
+
+std::string MyDeadCodeEliminationPass::getInstructionPosition(const Instruction &I, const BasicBlock &BB) {
+    size_t Position = 1;
+    for (const Instruction &Inst : BB) {
+        if (&Inst == &I) {
+            break;
+        }
+        ++Position;
+    }
+    if (Position == 1) {
+        return "First";
+    } else if (&I == &BB.back()) {
+        return "Last";
+    } else {
+        return "Intermediate (" + std::to_string(Position) + ")";
+    }
 }
 
