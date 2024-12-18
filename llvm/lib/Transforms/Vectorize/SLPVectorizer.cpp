ec57f14b928093b929e8dfac0d595ca9cb960035
Han-Kuan Chen <hankuan.chen@sifive.com>
2024-12-18 02:07:39 -0800

    [SLP][REVEC] SubVectorsMask should be transformed into vector form.

--- llvm/lib/Transforms/Vectorize/SLPVectorizer.cpp
+++ llvm/lib/Transforms/Vectorize/SLPVectorizer.cpp
@@ -14261,11 +14261,15 @@ public:
     IsFinalized = true;
     unsigned ScalarTyNumElements = getNumElements(ScalarTy);
     SmallVector<int> NewExtMask(ExtMask);
+    SmallVector<int> NewSubVectorsMask(SubVectorsMask);
     if (ScalarTyNumElements != 1) {
       assert(SLPReVec && "FixedVectorType is not expected.");
       transformScalarShuffleIndiciesToVector(ScalarTyNumElements, CommonMask);
       transformScalarShuffleIndiciesToVector(ScalarTyNumElements, NewExtMask);
       ExtMask = NewExtMask;
+      transformScalarShuffleIndiciesToVector(ScalarTyNumElements,
+                                             NewSubVectorsMask);
+      SubVectorsMask = NewSubVectorsMask;
     }
     if (Action) {
       Value *Vec = InVectors.front();
