561eb319e58d93a2e653267ba7a2347aeb71d77c
Louis Dionne <ldionne.2@gmail.com>
2024-12-19 12:16:56 -0500

    Remove CUDA on Mac test since that isn't supported anymore

--- clang/test/Preprocessor/cuda-macos-includes.cu
+++
@@ -1,6 +0,0 @@
-// Check that when we do CUDA host and device compiles on MacOS, we check for
-// includes in /System/Library/Frameworks and /Library/Frameworks.
-
-// RUN: %clang -isysroot /var/empty -target unknown-nvidia-cuda -v -fsyntax-only -x cuda %s -### 2>&1 | FileCheck %s
-// CHECK-DAG: "-internal-iframework" "/var/empty/System/Library/Frameworks"
-// CHECK-DAG: "-internal-iframework" "/var/empty/Library/Frameworks"
