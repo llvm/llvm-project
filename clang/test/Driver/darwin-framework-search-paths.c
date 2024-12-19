330d2e012ab5964683d52105182cffa50dcb1a08
Louis Dionne <ldionne.2@gmail.com>
2024-12-19 12:21:02 -0500

    Use macosx15.1 target instead of darwin13.0

--- clang/test/Driver/darwin-framework-search-paths.c
+++ clang/test/Driver/darwin-framework-search-paths.c
@@ -9,7 +9,7 @@
 // RUN: mkdir -p %t/MacOSX15.1.sdk/System/Library/SubFrameworks
 // RUN: mkdir -p %t/MacOSX15.1.sdk/usr/include
 
-// RUN: %clang -xc %s -target arm64-apple-darwin13.0 -isysroot %t/MacOSX15.1.sdk -E -v 2>&1 | FileCheck --check-prefix=CHECK-C %s
+// RUN: %clang -xc %s -target arm64-apple-macosx15.1 -isysroot %t/MacOSX15.1.sdk -E -v 2>&1 | FileCheck --check-prefix=CHECK-C %s
 //
 // CHECK-C:    -isysroot [[PATH:[^ ]*/MacOSX15.1.sdk]]
 // CHECK-C:    #include <...> search starts here:
@@ -17,7 +17,7 @@
 // CHECK-C:    [[PATH]]/System/Library/Frameworks (framework directory)
 // CHECK-C:    [[PATH]]/System/Library/SubFrameworks (framework directory)
 
-// RUN: %clang -xc++ %s -target arm64-apple-darwin13.0 -isysroot %t/MacOSX15.1.sdk -E -v 2>&1 | FileCheck --check-prefix=CHECK-CXX %s
+// RUN: %clang -xc++ %s -target arm64-apple-macosx15.1 -isysroot %t/MacOSX15.1.sdk -E -v 2>&1 | FileCheck --check-prefix=CHECK-CXX %s
 //
 // CHECK-CXX:    -isysroot [[PATH:[^ ]*/MacOSX15.1.sdk]]
 // CHECK-CXX:    #include <...> search starts here:
