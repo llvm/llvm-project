52ca0891445343c864b833d5c6b494dfac679963
Michael Buch <michaelbuch12@gmail.com>
2024-12-19 12:50:27 +0000

    fixup! python format

--- lldb/test/API/lang/cpp/typedef-to-outer-fwd/TestTypedefToOuterFwd.py
+++ lldb/test/API/lang/cpp/typedef-to-outer-fwd/TestTypedefToOuterFwd.py
@@ -5,19 +5,20 @@ from lldbsuite.test import lldbutil
 
 
 class TestCaseTypedefToOuterFwd(TestBase):
-    '''
+    """
     We are stopped in main.o, which only sees a forward declaration
     of FooImpl. We then try to get the FooImpl::Ref typedef (whose
     definition is in lib.o). Make sure we correctly resolve this
     typedef.
-    '''
+    """
+
     def test(self):
         self.build()
         (_, _, thread, _) = lldbutil.run_to_source_breakpoint(
             self, "return", lldb.SBFileSpec("main.cpp")
         )
 
-        foo = thread.frames[0].FindVariable('foo')
+        foo = thread.frames[0].FindVariable("foo")
         self.assertSuccess(foo.GetError(), "Found foo")
 
         foo_type = foo.GetType()
@@ -26,7 +27,7 @@ class TestCaseTypedefToOuterFwd(TestBase):
         impl = foo_type.GetPointeeType()
         self.assertTrue(impl)
 
-        ref = impl.FindDirectNestedType('Ref')
+        ref = impl.FindDirectNestedType("Ref")
         self.assertTrue(ref)
 
         self.assertEqual(ref.GetCanonicalType(), foo_type)
