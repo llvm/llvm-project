a0e3b603f10559e027889164734d4313da2acb0b
erichkeane <ekeane@nvidia.com>
2024-12-19 06:11:36 -0800

    [OpenACC] Implement 'init' and 'shutdown' constructs
    These two constructs are very simple and similar, and only support 3
    different clauses, two of which are already implemented.  This patch
    adds AST nodes for both constructs, and leaves the device_num clause
    unimplemented, but enables the other two.

--- clang/lib/AST/TextNodeDumper.cpp
+++ clang/lib/AST/TextNodeDumper.cpp
@@ -2963,6 +2963,13 @@ void TextNodeDumper::VisitOpenACCHostDataConstruct(
 void TextNodeDumper::VisitOpenACCWaitConstruct(const OpenACCWaitConstruct *S) {
   OS << " " << S->getDirectiveKind();
 }
+void TextNodeDumper::VisitOpenACCInitConstruct(const OpenACCInitConstruct *S) {
+  OS << " " << S->getDirectiveKind();
+}
+void TextNodeDumper::VisitOpenACCShutdownConstruct(
+    const OpenACCShutdownConstruct *S) {
+  OS << " " << S->getDirectiveKind();
+}
 
 void TextNodeDumper::VisitEmbedExpr(const EmbedExpr *S) {
   AddChild("begin", [=] { OS << S->getStartingElementPos(); });
