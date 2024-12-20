48b974d50a6b5d91f094663e446b94910a22f097
Nico Weber <thakis@chromium.org>
2024-12-20 14:25:20 -0500

    [clang] Expose -f(no-)wrapv as clang-cl option
    Also move the -fno-wrapv option definition next to the -fwrapv one
    while here.

--- clang/test/Driver/cl-options.c
+++ clang/test/Driver/cl-options.c
@@ -738,6 +738,8 @@
 // RUN:     -fimplicit-modules \
 // RUN:     -fno-implicit-modules \
 // RUN:     -ftrivial-auto-var-init=zero \
+// RUN:     -fwrapv \
+// RUN:     -fno-wrapv \
 // RUN:     --version \
 // RUN:     -Werror /Zs -- %s 2>&1
 
