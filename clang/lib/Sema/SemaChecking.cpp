ca3d8572cac3a1145c2c6a6539dd183f40677d73
Dmitry Chestnykh <dm.chestnykh@gmail.com>
2024-12-23 19:46:03 +0300

    [Clang][Sema] Remove unneeded braces

--- clang/lib/Sema/SemaChecking.cpp
+++ clang/lib/Sema/SemaChecking.cpp
@@ -6592,35 +6592,32 @@ void CheckFormatHandler::HandleNonStandardConversionSpecifier(
 void CheckFormatHandler::HandlePosition(const char *startPos,
                                         unsigned posLen) {
   if (!S.getDiagnostics().isIgnored(
-          diag::warn_format_non_standard_positional_arg, SourceLocation())) {
+          diag::warn_format_non_standard_positional_arg, SourceLocation()))
     EmitFormatDiagnostic(S.PDiag(diag::warn_format_non_standard_positional_arg),
                          getLocationOfByte(startPos),
                          /*IsStringLocation*/ true,
                          getSpecifierRange(startPos, posLen));
-  }
 }
 
 void CheckFormatHandler::HandleInvalidPosition(
     const char *startSpecifier, unsigned specifierLen,
     analyze_format_string::PositionContext p) {
   if (!S.getDiagnostics().isIgnored(
-          diag::warn_format_invalid_positional_specifier, SourceLocation())) {
+          diag::warn_format_invalid_positional_specifier, SourceLocation()))
     EmitFormatDiagnostic(
         S.PDiag(diag::warn_format_invalid_positional_specifier) << (unsigned)p,
         getLocationOfByte(startSpecifier), /*IsStringLocation*/ true,
         getSpecifierRange(startSpecifier, specifierLen));
-  }
 }
 
 void CheckFormatHandler::HandleZeroPosition(const char *startPos,
                                             unsigned posLen) {
   if (!S.getDiagnostics().isIgnored(diag::warn_format_zero_positional_specifier,
-                                    SourceLocation())) {
+                                    SourceLocation()))
     EmitFormatDiagnostic(S.PDiag(diag::warn_format_zero_positional_specifier),
                          getLocationOfByte(startPos),
                          /*IsStringLocation*/ true,
                          getSpecifierRange(startPos, posLen));
-  }
 }
 
 void CheckFormatHandler::HandleNullChar(const char *nullCharacter) {
