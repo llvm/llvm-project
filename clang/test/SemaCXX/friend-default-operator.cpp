5d31475ab3bfd8349087b625dff2d009adef8c39
GrumpyPigSkin <oliver61@live.co.uk>
2024-12-26 13:28:29 +0000

    removed redundant ;

--- clang/test/SemaCXX/friend-default-operator.cpp
+++ clang/test/SemaCXX/friend-default-operator.cpp
@@ -1,7 +1,7 @@
 // RUN: %clang_cc1 -std=c++20 -fsyntax-only -verify %s
 // expected-no-diagnostics
 
-// ; Ensure the following out of line friend declaration doesn't cause the compiler to crash.
+// Ensure the following out of line friend declaration doesn't cause the compiler to crash.
 
 class A {
   friend bool operator==(const A&, const A&);
