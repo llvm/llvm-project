003c050fe9782bdbdad570534c8b7f497d6de365
Зишан Мирза <zmirza@tutanota.de>
2024-12-26 09:55:26 +0500

    fix: add timezone header file to tests

--- libc/test/src/time/asctime_test.cpp
+++ libc/test/src/time/asctime_test.cpp
@@ -10,6 +10,7 @@
 #include "src/errno/libc_errno.h"
 #include "src/time/asctime.h"
 #include "src/time/time_utils.h"
+#include "src/time/timezone.h"
 #include "test/UnitTest/Test.h"
 #include "test/src/time/TmHelper.h"
 
