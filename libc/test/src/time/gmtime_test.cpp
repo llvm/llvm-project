b5e82ecf2358d3edee9a618b178f9e3cc8e4caad
Зишан Мирза <zmirza@tutanota.de>
2024-12-26 10:57:10 +0500

    add timezone header file to gmtime tests

--- libc/test/src/time/gmtime_test.cpp
+++ libc/test/src/time/gmtime_test.cpp
@@ -9,6 +9,7 @@
 #include "hdr/types/struct_tm.h"
 #include "src/__support/CPP/limits.h" // INT_MAX, INT_MIN
 #include "src/errno/libc_errno.h"
+#include "src/time/localtime.h"
 #include "src/time/gmtime.h"
 #include "src/time/time_constants.h"
 #include "test/UnitTest/ErrnoSetterMatcher.h"
