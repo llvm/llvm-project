66004df523c576d950919c86d0bbee81b40d11fc
Congcong Cai <congcongcai0907@163.com>
2024-12-28 07:57:48 +0800

    Update clang-tools-extra/test/clang-tidy/infrastructure/deprecation-global-option.cpp

--- clang-tools-extra/test/clang-tidy/infrastructure/deprecation-global-option.cpp
+++ clang-tools-extra/test/clang-tidy/infrastructure/deprecation-global-option.cpp
@@ -1,3 +1,3 @@
 // RUN: clang-tidy %s --config="{CheckOptions:{StrictMode: true}}" -checks="-*,modernize-use-std-format" | FileCheck %s 
 
-// CHECK: warning: deprecation global option 'StrictMode', please use 'modernize-use-std-format.StrictMode'. [clang-tidy-config]
+// CHECK: warning: global option 'StrictMode' is deprecated, please use 'modernize-use-std-format.StrictMode' instead. [clang-tidy-config]
