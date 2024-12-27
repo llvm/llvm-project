d37cba9babada00b2686949af7e2eefd8e829974
Sh0g0-1758 <shouryagoel10000@gmail.com>
2024-12-27 14:26:30 +0530

    header file for mpc

---
+++ libc/utils/MPCWrapper/MPCUtils.h
@@ -0,0 +1,286 @@
+//===-- MPCUtils.h ----------------------------------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_LIBC_UTILS_MPCWRAPPER_MPCUTILS_H
+#define LLVM_LIBC_UTILS_MPCWRAPPER_MPCUTILS_H
+
+#include "src/__support/CPP/type_traits.h"
+#include "src/__support/CPP/type_traits/is_complex.h"
+#include "src/__support/macros/properties/complex_types.h"
+#include "src/__support/macros/properties/types.h"
+#include "src/__support/macros/config.h"
+#include "test/UnitTest/RoundingModeUtils.h"
+#include "test/UnitTest/Test.h"
+
+#include <stdint.h>
+
+namespace LIBC_NAMESPACE_DECL {
+namespace testing {
+namespace mpc {
+
+enum class Operation {
+  // Operations which take a single complex floating point number as input
+  // and produce a single floating point number as output which has the same
+  // floating point type as the real/imaginary part of the input.
+  BeginUnaryOperationsSingleOutputDifferentOutputType,
+  Carg,
+  Cabs,
+  EndUnaryOperationsSingleOutputDifferentOutputType,
+
+  // Operations which take a single complex floating point number as input
+  // and produce a single complex floating point number of the same kind
+  // as output.
+  BeginUnaryOperationsSingleOutputSameOutputType,
+  Csqrt,
+  Clog,
+  Cexp,
+  Csinh,
+  Ccosh,
+  Ctanh,
+  Casinh,
+  Cacosh,
+  Catanh,
+  Csin,
+  Ccos,
+  Ctan,
+  Casin,
+  Cacos,
+  Catan,
+  EndUnaryOperationsSingleOutputSameOutputType,
+
+  // Operations which take two complex floating point numbers as input
+  // and produce a single complex floating point number of the same kind
+  // as output.
+  BeginBinaryOperationsSingleOutput,
+  Cpow,
+  EndBinaryOperationsSingleOutput,
+};
+
+using LIBC_NAMESPACE::fputil::testing::ForceRoundingMode;
+using LIBC_NAMESPACE::fputil::testing::RoundingMode;
+
+template <typename T> struct BinaryInput {
+  static_assert(
+      LIBC_NAMESPACE::cpp::is_complex_v<T>,
+      "Template parameter of BinaryInput must be a complex floating point type.");
+
+  using Type = T;
+  T x, y;
+};
+
+namespace internal {
+
+template <typename InputType, typename OutputType>
+bool compare_unary_operation_single_output(Operation op, InputType input,
+                                           OutputType libc_output,
+                                           double ulp_tolerance,
+                                           RoundingMode rounding);
+
+template <typename InputType, typename OutputType>
+bool compare_binary_operation_one_output(Operation op,
+                                         const BinaryInput<InputType> &input,
+                                         OutputType libc_output,
+                                         double ulp_tolerance,
+                                         RoundingMode rounding);
+
+template <typename InputType, typename OutputType>
+void explain_unary_operation_single_output_error(Operation op, InputType input,
+                                                 OutputType match_value,
+                                                 double ulp_tolerance,
+                                                 RoundingMode rounding);
+
+template <typename InputType, typename OutputType>
+void explain_binary_operation_one_output_error(
+    Operation op, const BinaryInput<InputType> &input, OutputType match_value,
+    double ulp_tolerance, RoundingMode rounding);
+
+template <Operation op, typename InputType, typename OutputType>
+class MPCMatcher : public testing::Matcher<OutputType> {
+private:
+  InputType input;
+  OutputType match_value;
+  double ulp_tolerance;
+  RoundingMode rounding;
+public:
+  MPCMatcher(InputType testInput, double ulp_tolerance, RoundingMode rounding)
+      : input(testInput), ulp_tolerance(ulp_tolerance), rounding(rounding) {}
+
+  bool match(OutputType libcResult) {
+    match_value = libcResult;
+    return match(input, match_value);
+  }
+private:
+  template <typename InType, typename OutType>
+  bool match(InType in, OutType out) {
+    return compare_unary_operation_single_output(op, in, out, ulp_tolerance,
+                                                 rounding);
+  }
+
+  template <typename T, typename U>
+  bool match(const BinaryInput<T> &in, U out) {
+    return compare_binary_operation_one_output(op, in, out, ulp_tolerance,
+                                               rounding);
+  }
+
+  template <typename InType, typename OutType>
+  void explain_error(InType in, OutType out) {
+    explain_unary_operation_single_output_error(op, in, out, ulp_tolerance,
+                                                rounding);
+  }
+
+  template <typename T, typename U>
+  void explain_error(const BinaryInput<T> &in, U out) {
+    explain_binary_operation_one_output_error(op, in, out, ulp_tolerance,
+                                              rounding);
+  }
+};
+
+} // namespace internal
+
+template <typename T> struct get_real;
+
+template <> struct get_real<_Complex float> {
+  using type = float;
+};
+template <> struct get_real<_Complex double> {
+  using type = double;
+};
+template <> struct get_real<_Complex long double> {
+  using type = long double;
+};
+
+#if defined(LIBC_TYPES_HAS_CFLOAT16)
+template <> struct get_real<cfloat16> {
+  using type = float16;
+};
+#endif
+#ifdef LIBC_TYPES_CFLOAT128_IS_NOT_COMPLEX_LONG_DOUBLE
+template <> struct get_real<cfloat128> {
+  using type = float128;
+};
+#endif
+
+template <typename T> using get_real_t = typename get_real<T>::type;
+
+// Return true if the input and ouput types for the operation op are valid
+// types.
+template <Operation op, typename InputType, typename OutputType>
+constexpr bool is_valid_operation() {
+    return (Operation::BeginBinaryOperationsSingleOutput < op &&
+        op < Operation::EndBinaryOperationsSingleOutput &&
+        cpp::is_complex_type_same<InputType, OutputType> &&
+        cpp::is_complex_v<InputType>) || 
+        (Operation::BeginUnaryOperationsSingleOutputSameOutputType < op &&
+        op < Operation::EndUnaryOperationsSingleOutputSameOutputType &&
+        cpp::is_complex_type_same<InputType, OutputType> &&
+        cpp::is_complex_v<InputType>) ||
+        (Operation::BeginUnaryOperationsSingleOutputDifferentOutputType < op &&
+        op < Operation::EndUnaryOperationsSingleOutputDifferentOutputType &&
+        cpp::is_same_v<get_real_t<InputType>, OutputType> &&
+        cpp::is_complex_v<InputType>);
+}
+
+template <Operation op, typename InputType, typename OutputType>
+cpp::enable_if_t<
+    is_valid_operation<op, InputType, OutputType>(),
+    internal::MPCMatcher<op, InputType, OutputType>>
+get_mpc_matcher(InputType input, [[maybe_unused]] OutputType output,
+                 double ulp_tolerance, RoundingMode rounding) {
+  return internal::MPCMatcher<op, InputType, OutputType>(
+      input, ulp_tolerance, rounding);
+}
+
+template <typename T> T round(T x, RoundingMode mode);
+template <typename T> bool round_to_long(T x, long &result);
+template <typename T> bool round_to_long(T x, RoundingMode mode, long &result);
+
+} // namespace mpc
+} // namespace testing
+} // namespace LIBC_NAMESPACE_DECL
+
+#define EXPECT_MPC_MATCH_DEFAULT(op, input, match_value, ulp_tolerance)        \
+  EXPECT_THAT(match_value,                                                     \
+              LIBC_NAMESPACE::testing::mpc::get_mpc_matcher<op>(               \
+                  input, match_value, ulp_tolerance,                           \
+                  LIBC_NAMESPACE::fputil::testing::RoundingMode::Nearest))
+
+#define EXPECT_MPC_MATCH_ROUNDING(op, input, match_value, ulp_tolerance,       \
+                                   rounding)                                   \
+  EXPECT_THAT(match_value,                                                     \
+              LIBC_NAMESPACE::testing::mpc::get_mpc_matcher<op>(               \
+                  input, match_value, ulp_tolerance, rounding))
+
+#define EXPECT_MPC_MATCH_ALL_ROUNDING(op, input, match_value, ulp_tolerance)   \
+  {                                                                            \
+    namespace MPCRND = LIBC_NAMESPACE::fputil::testing;                        \
+    MPCRND::ForceRoundingMode __r1(MPCRND::RoundingMode::Nearest);             \
+    if (__r1.success) {                                                        \
+      EXPECT_MPC_MATCH_ROUNDING(op, input, match_value, ulp_tolerance,                 \
+                        MPCRND::RoundingMode::Nearest);                          \
+    }                                                                          \
+    MPCRND::ForceRoundingMode __r2(MPCRND::RoundingMode::Upward);                  \
+    if (__r2.success) {                                                        \
+      EXPECT_MPC_MATCH_ROUNDING(op, input, match_value, ulp_tolerance,                 \
+                        MPCRND::RoundingMode::Upward);                           \
+    }                                                                          \
+    MPCRND::ForceRoundingMode __r3(MPCRND::RoundingMode::Downward);                \
+    if (__r3.success) {                                                        \
+      EXPECT_MPC_MATCH_ROUNDING(op, input, match_value, ulp_tolerance,                 \
+                        MPCRND::RoundingMode::Downward);                         \
+    }                                                                          \
+    MPCRND::ForceRoundingMode __r4(MPCRND::RoundingMode::TowardZero);              \
+    if (__r4.success) {                                                        \
+      EXPECT_MPC_MATCH_ROUNDING(op, input, match_value, ulp_tolerance,                 \
+                        MPCRND::RoundingMode::TowardZero);                       \
+    }                                                                          \
+  }
+
+#define TEST_MPC_MATCH_ROUNDING(op, input, match_value, ulp_tolerance,         \
+                                 rounding)                                     \
+  LIBC_NAMESPACE::testing::mpc::get_mpc_matcher<op>(input, match_value,        \
+                                                      ulp_tolerance, rounding) \
+      .match(match_value)
+
+#define ASSERT_MPC_MATCH_DEFAULT(op, input, match_value, ulp_tolerance)        \
+  ASSERT_THAT(match_value,                                                     \
+              LIBC_NAMESPACE::testing::mpc::get_mpc_matcher<op>(               \
+                  input, match_value, ulp_tolerance,                           \
+                  LIBC_NAMESPACE::fputil::testing::RoundingMode::Nearest))
+
+#define ASSERT_MPC_MATCH_ROUNDING(op, input, match_value, ulp_tolerance,       \
+                                   rounding)                                   \
+  ASSERT_THAT(match_value,                                                     \
+              LIBC_NAMESPACE::testing::mpc::get_mpc_matcher<op>(               \
+                  input, match_value, ulp_tolerance, rounding))
+
+#define ASSERT_MPC_MATCH_ALL_ROUNDING(op, input, match_value, ulp_tolerance)  \
+  {                                                                            \
+    namespace MPCRND = LIBC_NAMESPACE::fputil::testing;                            \
+    MPCRND::ForceRoundingMode __r1(MPCRND::RoundingMode::Nearest);                 \
+    if (__r1.success) {                                                        \
+      ASSERT_MPC_MATCH_ROUNDING(op, input, match_value, ulp_tolerance,                 \
+                        MPCRND::RoundingMode::Nearest);                          \
+    }                                                                          \
+    MPCRND::ForceRoundingMode __r2(MPCRND::RoundingMode::Upward);                  \
+    if (__r2.success) {                                                        \
+      ASSERT_MPC_MATCH_ROUNDING(op, input, match_value, ulp_tolerance,                 \
+                        MPCRND::RoundingMode::Upward);                           \
+    }                                                                          \
+    MPCRND::ForceRoundingMode __r3(MPCRND::RoundingMode::Downward);                \
+    if (__r3.success) {                                                        \
+      ASSERT_MPC_MATCH_ROUNDING(op, input, match_value, ulp_tolerance,                 \
+                        MPCRND::RoundingMode::Downward);                         \
+    }                                                                          \
+    MPCRND::ForceRoundingMode __r4(MPCRND::RoundingMode::TowardZero);              \
+    if (__r4.success) {                                                        \
+      ASSERT_MPC_MATCH_ROUNDING(op, input, match_value, ulp_tolerance,                 \
+                        MPCRND::RoundingMode::TowardZero);                       \
+    }                                                                          \
+  }
+
+#endif // LLVM_LIBC_UTILS_MPCWRAPPER_MPCUTILS_H
---
+++ libc/utils/MPCWrapper/mpc_test.cpp
@@ -0,0 +1,34 @@
+#include "mpc.h"
+
+int main() {
+    // just a pair of two arbitary precision floating point numbers
+    mpc_t x,z;
+    // different rounding modes for real and complex part
+    mpc_rnd_t y;
+
+    // MPC function begins with mpc_ and store the result in the first argument. 
+    // Rounding modes in MPC are of the form MPC_RNDxy where x and y are one of :
+    //     1. N(to nearest)
+    //     2. Z(to zero)
+    //     3. U(towards +infinity)
+    //     4. D(towards -infinity)
+    //     5. A(away from zero)
+    // where the first letter is for the real part and the second letter is for the
+    // imaginary part. 
+    // MPC functions have a return value which can be used with MPC_INEX_RE and
+    // MPC_INEX_IM to tell whether the rounded value is less, equal or greater than
+    // the exact value.
+    // Results on machines with different word sizes should not wary. 
+
+    // for multiplication. 
+    mpc_mul(x, x, x, MPC_RNDNN);
+
+    // for initialization
+    mpc_init2(x, 256); // Real and Imaginary have same precision
+    mpc_init3(x, 256, 256); // Real and Imaginary can have different precision
+    mpc_clear(x); // free the space occupied by mpc_t x
+    mpc_set_prec(x, 64); // reset the precision of x to 64 bits and set x to (NaN, NaN) (Previous value of x is lost)
+    // mpc_get_prec returns the precision if precision of real and imaginary part of x are same, else it returns 0. 
+    // mpc_get_prec2 returns the precision of the real and imaginary part of x.
+    mpc_set(x, z, MPC_RNDNN); // set x to z with rounding mode MPC_RNDNN
+}
