c6ef0ad2288fc08dd1767892f32322b521401a51
Amara Emerson <amara@apple.com>
2024-12-27 17:36:49 -0800

    [𝘀𝗽𝗿] changes introduced through rebase
    Created using spr 1.3.5
    
    [skip ci]

--- llvm/lib/Target/AArch64/GISel/AArch64LegalizerInfo.cpp
+++ llvm/lib/Target/AArch64/GISel/AArch64LegalizerInfo.cpp
@@ -1430,8 +1430,7 @@ bool AArch64LegalizerInfo::legalizeBitcast(MachineInstr &MI,
       SrcTy.getElementType() != LLT::scalar(1))
     return false;
 
-  auto Load = Helper.createStackStoreLoad(SrcReg, DstTy);
-  Helper.MIRBuilder.buildCopy(DstReg, Load.getReg(0));
+  Helper.createStackStoreLoad(DstReg, SrcReg);
   MI.eraseFromParent();
   return true;
 }
