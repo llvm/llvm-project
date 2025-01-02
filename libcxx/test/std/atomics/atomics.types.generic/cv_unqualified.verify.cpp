28a01077b726d0ac36c71e9dc9416ce867e267b0
Damien L-G <dalg24@gmail.com>
2025-01-02 08:06:18 -0500

    Add volatile T test for std::atomic

--- libcxx/test/std/atomics/atomics.types.generic/cv_unqualified.verify.cpp
+++ libcxx/test/std/atomics/atomics.types.generic/cv_unqualified.verify.cpp
@@ -15,10 +15,9 @@
 // is cv-qualified, however Clang will sometimes emit additional
 // errors while trying to instantiate the rest of std::atomic<T>.
 // We silence those to make the test more robust.
-// ADDITIONAL_COMPILE_FLAGS: -Xclang -verify-ignore-unexpected=error
+// ADDITIONAL_COMPILE_FLAGS: -Xclang -verify-ignore-unexpected=error -Wno-deprecated-volatile
 
 #include <atomic>
 
-void f() {
-  std::atomic<const int> a; // expected-error@*:* {{std::atomic<T> requires that 'T' be a cv-unqualified type}}
-}
+std::atomic<const int> aci;      // expected-error@*:* {{std::atomic<T> requires that 'T' be a cv-unqualified type}}
+std::atomic<volatile float> avf; // expected-error@*:* {{std::atomic<T> requires that 'T' be a cv-unqualified type}}
