49c3bf1f716bca9a64dd974fa55e6b61ed622c57
Alexey Samsonov <vonosmas@gmail.com>
2025-01-04 01:03:54 -0800

    [libc] Remove some unused includes from headers under src/math/generic.
    These were indicated by Clang include-cleaner.

--- libc/src/math/generic/explogxf.h
+++ libc/src/math/generic/explogxf.h
@@ -12,7 +12,6 @@
 #include "common_constants.h"
 #include "src/__support/CPP/bit.h"
 #include "src/__support/CPP/optional.h"
-#include "src/__support/FPUtil/FEnvImpl.h"
 #include "src/__support/FPUtil/FPBits.h"
 #include "src/__support/FPUtil/PolyEval.h"
 #include "src/__support/FPUtil/nearest_integer.h"
