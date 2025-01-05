3816c4c4a42275f31d2d36a821f28497467a4fef
JCGoran <jcgoran@protonmail.com>
2025-01-05 19:55:53 +0100

    Add missing header

--- libcxx/include/__algorithm/ranges_fold.h
+++ libcxx/include/__algorithm/ranges_fold.h
@@ -28,6 +28,7 @@
 #include <__type_traits/invoke.h>
 #include <__utility/forward.h>
 #include <__utility/move.h>
+#include <optional>
 
 #if !defined(_LIBCPP_HAS_NO_PRAGMA_SYSTEM_HEADER)
 #  pragma GCC system_header
