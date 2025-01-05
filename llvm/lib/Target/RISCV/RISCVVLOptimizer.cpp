3d05abdfdb560aea0a7aaf3b8ce88399cfadddb5
Michael Maitland <michaeltmaitland@gmail.com>
2025-01-05 10:08:48 -0800

    fixup! VLM and VSM use EEW=1.

--- llvm/lib/Target/RISCV/RISCVVLOptimizer.cpp
+++ llvm/lib/Target/RISCV/RISCVVLOptimizer.cpp
@@ -257,10 +257,11 @@ static OperandInfo getOperandInfo(const MachineOperand &MO,
   // Vector Unit-Stride Instructions
   // Vector Strided Instructions
   /// Dest EEW encoded in the instruction and EMUL=(EEW/SEW)*LMUL
-  case RISCV::VLE8_V:
-  case RISCV::VSE8_V:
   case RISCV::VLM_V:
   case RISCV::VSM_V:
+    return OperandInfo(RISCVVType::getEMULEqualsEEWDivSEWTimesLMUL(0, MI), 0);
+  case RISCV::VLE8_V:
+  case RISCV::VSE8_V:
   case RISCV::VLSE8_V:
   case RISCV::VSSE8_V:
   case RISCV::VLE8FF_V:
