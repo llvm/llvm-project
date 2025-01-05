96eced624e0f120155256033fdcb8342e7e58d6e
Younan Zhang <zyn7109@gmail.com>
2025-01-05 10:50:52 +0800

    [Clang] Implement CWG2369 "Ordering between constraints and substitution" (#102857)
    This patch partially implements CWG2369 for non-lambda-constrained
    functions.
    
    Lambdas are left intact at this point because we need extra work to
    correctly instantiate captures before the function instantiation.
    
    As a premise of CWG2369, this patch also implements CWG2770 to ensure
    the function parameters are instantiated on demand.
    
    Closes https://github.com/llvm/llvm-project/issues/54440

--- clang/include/clang/Sema/Template.h
+++ clang/include/clang/Sema/Template.h
@@ -522,6 +522,12 @@ enum class TemplateSubstitutionKind : char {
     llvm::PointerUnion<Decl *, DeclArgumentPack *> *
     findInstantiationOf(const Decl *D);
 
+    /// Similar to \p findInstantiationOf(), but it wouldn't assert if the
+    /// instantiation was not found within the current instantiation scope. This
+    /// is helpful for on-demand declaration instantiation.
+    llvm::PointerUnion<Decl *, DeclArgumentPack *> *
+    findInstantiationUnsafe(const Decl *D);
+
     void InstantiatedLocal(const Decl *D, Decl *Inst);
     void InstantiatedLocalPackArg(const Decl *D, VarDecl *Inst);
     void MakeInstantiatedLocalArgPack(const Decl *D);
