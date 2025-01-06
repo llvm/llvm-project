b51a082e1afd707f8cf1473e4c49374e7e951d2d
Akshat Oke <Akshat.Oke@amd.com>
2025-01-06 13:50:31 +0530

    Revert "[Support] Recycler: Enforce minimum allocation size" (#121735)
    Reverts llvm/llvm-project#121425

--- llvm/include/llvm/Support/Recycler.h
+++ llvm/include/llvm/Support/Recycler.h
@@ -85,8 +85,6 @@ public:
                   "Recycler allocation alignment is less than object align!");
     static_assert(sizeof(SubClass) <= Size,
                   "Recycler allocation size is less than object size!");
-    static_assert(Size >= sizeof(FreeNode) &&
-                  "Recycler size must be at least sizeof(FreeNode)");
     return FreeList ? reinterpret_cast<SubClass *>(pop_val())
                     : static_cast<SubClass *>(Allocator.Allocate(Size, Align));
   }
