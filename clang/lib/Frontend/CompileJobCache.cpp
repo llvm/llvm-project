ecf09a4cd414cbdb7f3924bd41386e877a726a2d
Steven Wu <stevenwu@apple.com>
2025-01-06 09:37:26 -0800

    [clang][CAS] Fix assertion failures from libclang replay APIs
    Fix the libclang cache replay function after
    bdd10d9d249bd1c2a45e3de56a5accd97e953458. This restore the old behavior
    while fixing the assertion failure when accessing the file manager
    before it being initialized.

--- clang/lib/Frontend/CompileJobCache.cpp
+++ clang/lib/Frontend/CompileJobCache.cpp
@@ -616,7 +616,7 @@ Expected<std::optional<int>> CompileJobCache::replayCachedResult(
   Clang.setInvocation(std::move(Invok));
   llvm::raw_svector_ostream DiagOS(DiagText);
   Clang.createDiagnostics(
-      Clang.getVirtualFileSystem(),
+      *llvm::vfs::getRealFileSystem(),
       new TextDiagnosticPrinter(DiagOS, &Clang.getDiagnosticOpts()));
   Clang.setVerboseOutputStream(DiagOS);
 
