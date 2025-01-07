c804f8e6dec62dbcc3258e6a5a7a8aa162062009
Roland McGrath <mcgrathr@google.com>
2025-01-07 12:13:24 -0800

    [libc] Keep headers compatible with -std=c89 mode (#121981)
    C89 doesn't have the `restrict` keyword.  When in `-std=c89`
    mode, GNU-compatible compilers require the `__restrict` spelling.

--- libc/include/__llvm-libc-common.h
+++ libc/include/__llvm-libc-common.h
@@ -50,7 +50,14 @@
 #define __END_C_DECLS
 
 #undef __restrict
-#define __restrict restrict // C99 and above support the restrict keyword.
+#if __STDC_VERSION__ >= 199901L
+// C99 and above support the restrict keyword.
+#define __restrict restrict
+#elif !defined(__GNUC__)
+// GNU-compatible compilers accept the __ spelling in all modes.
+// Otherwise, omit the qualifier for pure C89 compatibility.
+#define __restrict
+#endif
 
 #undef _Noreturn
 #if __STDC_VERSION__ >= 201112L
