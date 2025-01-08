94d4003e9e9820bcfd40543e15d1d77389303860
Sarah Spall <spall@planetbauer.com>
2025-01-08 21:03:39 +0000

    fix failing tests after rebase

--- clang/test/CodeGenHLSL/builtins/StructuredBuffers-methods-lib.hlsl
+++ clang/test/CodeGenHLSL/builtins/StructuredBuffers-methods-lib.hlsl
@@ -53,7 +53,7 @@ export float TestLoad() {
     return RWSB1.Load(1) + SB1.Load(2);
 }
 
-// CHECK: define noundef float @_Z8TestLoadv()
+// CHECK: define noundef nofpclass(nan inf) float @_Z8TestLoadv()
 // CHECK: %[[PTR1:.*]] = call ptr @llvm.dx.resource.getpointer.p0.tdx.RawBuffer_f32_1_0t(target("dx.RawBuffer", float, 1, 0) %{{[0-9]+}}, i32 %{{[0-9]+}})
 // CHECK: %[[VALUE1:.*]] = load float, ptr %[[PTR1]]
 // CHECK: %[[PTR2:.*]] = call ptr @llvm.dx.resource.getpointer.p0.tdx.RawBuffer_f32_0_0t(target("dx.RawBuffer", float, 0, 0) %{{[0-9]+}}, i32 %{{[0-9]+}})
