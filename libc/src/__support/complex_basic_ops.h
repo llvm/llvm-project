cf88bba66d0dc5473a5901f52fda1e37c6170c29
Sh0g0-1758 <shouryagoel10000@gmail.com>
2025-01-10 22:45:10 +0530

    nit

--- libc/src/__support/complex_basic_ops.h
+++ libc/src/__support/complex_basic_ops.h
@@ -25,13 +25,11 @@ template <typename T> LIBC_INLINE constexpr T project(T c) {
   using real_t = make_real_t<T>;
   Complex<real_t> c_c = cpp::bit_cast<Complex<real_t>>(c);
   if (fputil::FPBits<real_t>(c_c.real).is_inf() ||
-      fputil::FPBits<real_t>(c_c.imag).is_inf()) {
+      fputil::FPBits<real_t>(c_c.imag).is_inf())
     return cpp::bit_cast<T>(
         Complex<real_t>{(fputil::FPBits<real_t>::inf(Sign::POS).get_val()),
                         static_cast<real_t>(c_c.imag > 0 ? 0.0 : -0.0)});
-  } else {
-    return c;
-  }
+  return c;
 }
 
 } // namespace LIBC_NAMESPACE_DECL
