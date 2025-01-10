b7ea0c599daced2655c1364fb1b710dc7e2dec10
Matt Arsenault <Matthew.Arsenault@amd.com>
2025-01-10 14:57:24 +0700

    AMDGPU: Implement isExtractVecEltCheap
    Once again we have excessive TLI hooks with bad defaults. Permit this
    for 32-bit element vectors, which are just use-different-register.
    We should permit 16-bit vectors as cheap with legal packed instructions,
    but I see some mixed improvements and regressions that need investigation.

--- llvm/lib/Target/AMDGPU/SIISelLowering.h
+++ llvm/lib/Target/AMDGPU/SIISelLowering.h
@@ -365,6 +365,7 @@ public:
 
   bool isExtractSubvectorCheap(EVT ResVT, EVT SrcVT,
                                unsigned Index) const override;
+  bool isExtractVecEltCheap(EVT VT, unsigned Index) const override;
 
   bool isTypeDesirableForOp(unsigned Op, EVT VT) const override;
 
