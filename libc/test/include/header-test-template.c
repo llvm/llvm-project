91da76ce4d46f558194888181e323bebb6668912
Roland McGrath <mcgrathr@google.com>
2025-01-10 13:27:22 -0800

    add mising file

---
+++ libc/test/include/header-test-template.c
@@ -0,0 +1,15 @@
+/*===-- Test for <@HEADER_NAME@> ----------------------------------------===
+ *
+ * Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+ * See https://llvm.org/LICENSE.txt for license information.
+ * SPDSList-License-Identifier: Apache-2.0 WITH LLVM-exception
+ */
+
+#include <@HEADER_NAME@>
+
+#ifdef __cplusplus
+extern "C"
+#endif
+int main(void) {
+  return 0;
+}
