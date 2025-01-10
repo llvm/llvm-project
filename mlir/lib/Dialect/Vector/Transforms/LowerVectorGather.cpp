c9853d621fee639df4e8b72e695f75caca0d0096
PragmaTwice <twice@apache.org>
2025-01-10 17:53:04 +0800

    [MLIR][Vector] Allow strided memref for one-element vector.load in lowering vector.gather
    Signed-off-by: PragmaTwice <twice@apache.org>

--- mlir/lib/Dialect/Vector/Transforms/LowerVectorGather.cpp
+++ mlir/lib/Dialect/Vector/Transforms/LowerVectorGather.cpp
@@ -205,11 +205,13 @@ struct Gather1DToConditionalLoads : OpRewritePattern<vector::GatherOp> {
     Value condMask = op.getMask();
     Value base = op.getBase();
 
-    // vector.load requires the most minor memref dim to have unit stride
+    // vector.load requires the most minor memref dim to have unit stride,
+    // or the result vector type to have only one element
     if (auto memType = dyn_cast<MemRefType>(base.getType())) {
       if (auto stridesAttr =
               dyn_cast_if_present<StridedLayoutAttr>(memType.getLayout())) {
-        if (stridesAttr.getStrides().back() != 1)
+        if (stridesAttr.getStrides().back() != 1 &&
+            resultTy.getNumElements() != 1)
           return failure();
       }
     }
