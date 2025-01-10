732398188a890e79197c61ed9c338310246f9a49
Sam Tebbs <samuel.tebbs@arm.com>
2025-01-10 11:37:37 +0000

    Rework lowering location

--- llvm/lib/CodeGen/SelectionDAG/SelectionDAGDumper.cpp
+++ llvm/lib/CodeGen/SelectionDAG/SelectionDAGDumper.cpp
@@ -585,6 +585,8 @@ std::string SDNode::getOperationName(const SelectionDAG *G) const {
     return "partial_reduce_umla";
   case ISD::PARTIAL_REDUCE_SMLA:
     return "partial_reduce_smla";
+  case ISD::EXPERIMENTAL_ALIAS_LANE_MASK:
+    return "alias_mask";
 
     // Vector Predication
 #define BEGIN_REGISTER_VP_SDNODE(SDID, LEGALARG, NAME, ...)                    \
