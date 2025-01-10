bf3e9617c3f9baa59cef47f9db212e6a5d595cb5
Tom Stellard <tstellar@redhat.com>
2025-01-10 22:19:34 +0000

    Stage2-instrumented

--- clang/cmake/caches/llvm-toolchain/stage2-instrumented.cmake
+++ clang/cmake/caches/llvm-toolchain/stage2-instrumented.cmake
@@ -3,8 +3,9 @@
 
 
 set(CLANG_ENABLE_BOOTSTRAP ON CACHE BOOL "")
-set(CLANG_BOOTSTRAP_CMAKE_ARGS -C ${CMAKE_CURRENT_LIST_DIR}/stage2.cmake CACHE BOOL "")
-set(CLANG_BOOTSTRAP_TARGETS clang check-all distribution install-distribution clang-bolt CACHE BOOL "")
+set(LLVM_BUILD_INSTRUMENTED IR CACHE STRING "")
+set(CLANG_BOOTSTRAP_CMAKE_ARGS -C ${CMAKE_CURRENT_LIST_DIR}/stage2.cmake CACHE STRING "")
+set(CLANG_BOOTSTRAP_TARGETS clang check-all distribution install-distribution clang-bolt CACHE STRING "")
 set(CLANG_BOLT OFF CACHE STRING "")
 
 include(${CMAKE_CURRENT_LIST_DIR}/stage2.cmake)
