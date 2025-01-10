3c26cba0a65a2deb679eabdfc52633474f23e8e7
Tom Stellard <tstellar@redhat.com>
2025-01-10 21:19:41 +0000

    Stage 1

--- clang/cmake/caches/llvm-toolchain/stage1.cmake
+++ clang/cmake/caches/llvm-toolchain/stage1.cmake
@@ -5,8 +5,8 @@
 
 set(CMAKE_BUILD_TYPE Release CACHE STRING "")
 set(LLVM_TARGETS_TO_BUILD Native CACHE STRING "")
-set(LLVM_ENABLE_RUNTIMES compiler-rt libcxx libcxxabi libunwind CACHE STRING "" FORCE)
-set(LLVM_ENABLE_PROJECTS "clang;lld" CACHE STRING "" FORCE)
+set(LLVM_ENABLE_RUNTIMES compiler-rt libcxx libcxxabi libunwind CACHE STRING "")
+set(LLVM_ENABLE_PROJECTS clang lld CACHE STRING "")
 
 set(CLANG_ENABLE_BOOTSTRAP ON CACHE BOOL "")
 set(CLANG_BOOTSTRAP_CMAKE_ARGS -C ${CMAKE_CURRENT_LIST_DIR}/stage2-instrumented.cmake CACHE BOOL "")
