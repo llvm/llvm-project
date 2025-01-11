e995c453e27c5a6a9d80b9fb8bef75746a70e801
Fangrui Song <i@maskray.me>
2025-01-10 19:25:18 -0800

    -ftime-report: reorganize timers
    The code generation time is unclear in the -ftime-report output:
    
    * The two clang timers "Code Generation Time" and "LLVM IR Generation
      Time" are in the default group "Miscellaneous Ungrouped Timers".
    * There is also a "Clang front-end time" group, which actually includes
      code generation time.
    
    ```
    ===-------------------------------------------------------------------------===
                             Miscellaneous Ungrouped Timers
    ===-------------------------------------------------------------------------===
    
       ---User Time---   --System Time--   --User+System--   ---Wall Time---  --- Name ---
       0.0611 (  1.7%)   0.0099 (  4.4%)   0.0710 (  1.9%)   0.0713 (  1.9%)  LLVM IR Generation Time
       3.5140 ( 98.3%)   0.2165 ( 95.6%)   3.7306 ( 98.1%)   3.7342 ( 98.1%)  Code Generation Time
       3.5751 (100.0%)   0.2265 (100.0%)   3.8016 (100.0%)   3.8055 (100.0%)  Total
    ...
    ===-------------------------------------------------------------------------===
                              Clang front-end time report
    ===-------------------------------------------------------------------------===
      Total Execution Time: 3.9108 seconds (3.9146 wall clock)
    
       ---User Time---   --System Time--   --User+System--   ---Wall Time---  --- Name ---
       3.6802 (100.0%)   0.2306 (100.0%)   3.9108 (100.0%)   3.9146 (100.0%)  Clang front-end timer
       3.6802 (100.0%)   0.2306 (100.0%)   3.9108 (100.0%)   3.9146 (100.0%)  Total
    ```
    
    This patch
    
    * renames "Clang front-end time report" (FrontendAction time) to "Clang
      time report",
    * renames "Clang front-end" to "Front end",
    * moves "LLVM IR Generation" into the group,
    * replaces "Code Generation time" with "Optimizer" (middle end) and
      "Machine code generation" (back end).
    
    ```
    % clang -c sqlite3.i -w -ftime-report -mllvm -sort-timers=0
    ...
    ===-------------------------------------------------------------------------===
                                   Clang time report
    ===-------------------------------------------------------------------------===
      Total Execution Time: 1.5922 seconds (1.5972 wall clock)
    
       ---User Time---   --System Time--   --User+System--   ---Wall Time---  --- Name ---
       0.5107 ( 35.9%)   0.0105 (  6.2%)   0.5211 ( 32.7%)   0.5222 ( 32.7%)  Front end
       0.2464 ( 17.3%)   0.0340 ( 20.0%)   0.2804 ( 17.6%)   0.2814 ( 17.6%)  LLVM IR generation
       0.6240 ( 43.9%)   0.1235 ( 72.7%)   0.7475 ( 47.0%)   0.7503 ( 47.0%)  Machine code generation
       0.0413 (  2.9%)   0.0018 (  1.0%)   0.0431 (  2.7%)   0.0433 (  2.7%)  Optimizer
       1.4224 (100.0%)   0.1698 (100.0%)   1.5922 (100.0%)   1.5972 (100.0%)  Total
    ```
    
    Pull Request: https://github.com/llvm/llvm-project/pull/122225

--- clang/include/clang/Frontend/CompilerInstance.h
+++ clang/include/clang/Frontend/CompilerInstance.h
@@ -118,7 +118,7 @@ class CompilerInstance : public ModuleLoader {
   std::unique_ptr<Sema> TheSema;
 
   /// The frontend timer group.
-  std::unique_ptr<llvm::TimerGroup> FrontendTimerGroup;
+  std::unique_ptr<llvm::TimerGroup> timerGroup;
 
   /// The frontend timer.
   std::unique_ptr<llvm::Timer> FrontendTimer;
@@ -629,6 +629,8 @@ public:
   /// @}
   /// @name Frontend timer
   /// @{
+
+  llvm::TimerGroup &getTimerGroup() const { return *timerGroup; }
 
   bool hasFrontendTimer() const { return (bool)FrontendTimer; }
 
