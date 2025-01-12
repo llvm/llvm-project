78c7f2805613ac5a0e4c7d6cfcd5d4336901eab2
Sh0g0-1758 <shouryagoel10000@gmail.com>
2025-01-12 15:15:31 +0530

    nit

--- libc/utils/MPCWrapper/MPCUtils.cpp
+++ libc/utils/MPCWrapper/MPCUtils.cpp
@@ -17,7 +17,7 @@
 #include "src/__support/FPUtil/fpbits_str.h"
 #include "src/__support/macros/config.h"
 #include "src/__support/macros/properties/types.h"
-#include "utils/MPFRWrapper/MPFRUtils.h"
+#include "utils/MPFRWrapper/MPFRUtils.cpp"
 
 #include <stdint.h>
 
