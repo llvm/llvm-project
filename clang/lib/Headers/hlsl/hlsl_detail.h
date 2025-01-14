561daa94826def9f02ea35112fbff3cd75bf0123
Deric Cheung <cheung.deric@gmail.com>
2025-01-14 09:11:54 -0800

    [HLSL] Implement D3DCOLORtoUBYTE4 intrinsic (#122202)
    Fixes #99092.
    
    1. Defines the function `D3DCOLORtoUBYTE4` in
    `clang/lib/Headers/hlsl/hlsl_intrinsics.h`.
    2. Implements the function `D3DCOLORtoUBYTE4` as `d3d_color_to_ubyte4`
    in `clang/lib/Headers/hlsl/hlsl_detail.h`
    3. Adds a HLSL codegen test to
    `clang/test/CodeGenHLSL/builtins/D3DCOLORtoUBYTE4.hlsl`
    4. Adds sema tests to
    `clang/test/SemaHLSL/BuiltIns/D3DCOLORtoUBYTE4-errors.hlsl`

--- clang/lib/Headers/hlsl/hlsl_detail.h
+++ clang/lib/Headers/hlsl/hlsl_detail.h
@@ -41,6 +41,17 @@ constexpr enable_if_t<sizeof(U) == sizeof(T), U> bit_cast(T F) {
   return __builtin_bit_cast(U, F);
 }
 
+constexpr vector<uint, 4> d3d_color_to_ubyte4_impl(vector<float, 4> V) {
+  // Use the same scaling factor used by FXC, and DXC for DXIL
+  // (i.e., 255.001953)
+  // https://github.com/microsoft/DirectXShaderCompiler/blob/070d0d5a2beacef9eeb51037a9b04665716fd6f3/lib/HLSL/HLOperationLower.cpp#L666C1-L697C2
+  // The DXC implementation refers to a comment on the following stackoverflow
+  // discussion to justify the scaling factor: "Built-in rounding, necessary
+  // because of truncation. 0.001953 * 256 = 0.5"
+  // https://stackoverflow.com/questions/52103720/why-does-d3dcolortoubyte4-multiplies-components-by-255-001953f
+  return V.zyxw * 255.001953f;
+}
+
 template <typename T>
 constexpr enable_if_t<is_same<float, T>::value || is_same<half, T>::value, T>
 length_impl(T X) {
