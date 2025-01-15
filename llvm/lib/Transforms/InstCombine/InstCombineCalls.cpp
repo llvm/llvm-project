2dfa8b7ec68703dc9862964963c736c341f5be97
Andreas Jonson <andjo403@hotmail.com>
2025-01-15 07:54:42 +0100

    fixup! [InstCombine] Handle trunc to i1 in align assume.

--- llvm/lib/Transforms/InstCombine/InstCombineCalls.cpp
+++ llvm/lib/Transforms/InstCombine/InstCombineCalls.cpp
@@ -3255,11 +3255,11 @@ Instruction *InstCombinerImpl::visitCallInst(CallInst &CI) {
     // call void @llvm.assume(i1 true) [ "align"(i32* [[A]], i64  Constant + 1)]
     uint64_t AlignMask = 1;
     if (EnableKnowledgeRetention &&
-        (match(IIOperand,
+        (match(IIOperand, m_Not(m_Trunc(m_Value(A)))) ||
+         match(IIOperand,
                m_SpecificICmp(ICmpInst::ICMP_EQ,
                               m_And(m_Value(A), m_ConstantInt(AlignMask)),
-                              m_Zero())) ||
-         match(IIOperand, m_Not(m_Trunc(m_Value(A)))))) {
+                              m_Zero())))) {
       if (isPowerOf2_64(AlignMask + 1)) {
         uint64_t Offset = 0;
         match(A, m_Add(m_Value(A), m_ConstantInt(Offset)));
