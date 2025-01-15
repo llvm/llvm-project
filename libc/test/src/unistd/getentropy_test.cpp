4b3037aab5266cc4037b0c35d59975f4f9130051
Schrodinger ZHU Yifan <i@zhuyi.fan>
2025-01-15 09:59:13 +0000

    [libc] address CRs

--- libc/test/src/unistd/getentropy_test.cpp
+++ libc/test/src/unistd/getentropy_test.cpp
@@ -6,7 +6,7 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "src/errno/libc_errno.h"
+#include "hdr/errno_macros.h"
 #include "src/unistd/getentropy.h"
 #include "test/UnitTest/ErrnoSetterMatcher.h"
 #include "test/UnitTest/Test.h"
@@ -16,16 +16,11 @@ using namespace LIBC_NAMESPACE::testing::ErrnoSetterMatcher;
 TEST(LlvmLibcUnistdGetEntropyTest, LengthTooLong) {
   char buf[1024];
   ASSERT_THAT(LIBC_NAMESPACE::getentropy(buf, 257), Fails(EIO));
-  ASSERT_THAT(LIBC_NAMESPACE::getentropy(buf, 1024), Fails(EIO));
 }
 
 TEST(LlvmLibcUnistdGetEntropyTest, SmokeTest) {
   char buf[256];
   ASSERT_THAT(LIBC_NAMESPACE::getentropy(buf, 256), Succeeds());
-  ASSERT_THAT(LIBC_NAMESPACE::getentropy(buf, 0), Succeeds());
-  ASSERT_THAT(LIBC_NAMESPACE::getentropy(buf, 1), Succeeds());
-  ASSERT_THAT(LIBC_NAMESPACE::getentropy(buf, 16), Succeeds());
-  ASSERT_THAT(LIBC_NAMESPACE::getentropy(buf, 17), Succeeds());
 }
 
 TEST(LlvmLibcUnistdGetEntropyTest, OtherError) {
