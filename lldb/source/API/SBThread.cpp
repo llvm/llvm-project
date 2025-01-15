74d436310319d17cb43ba598836b70a92c827111
Pavel Labath <pavel@labath.sk>
2025-01-15 12:41:23 +0100

    [lldb] Fix SBThread::StepOverUntil for discontinuous functions
    I think the only issue here was that we would erroneously consider
    functions which are "in the middle" of the function were stepping to as
    a part of the function, and would try to step into them (likely stepping
    out of the function instead) instead of giving up early.

--- lldb/source/API/SBThread.cpp
+++ lldb/source/API/SBThread.cpp
@@ -842,7 +842,7 @@ SBError SBThread::StepOverUntil(lldb::SBFrame &sb_frame,
     // appropriate error message.
 
     bool all_in_function = true;
-    AddressRange fun_range = frame_sc.function->GetAddressRange();
+    AddressRanges fun_ranges = frame_sc.function->GetAddressRanges();
 
     std::vector<addr_t> step_over_until_addrs;
     const bool abort_other_plans = false;
@@ -859,7 +859,9 @@ SBError SBThread::StepOverUntil(lldb::SBFrame &sb_frame,
       addr_t step_addr =
           sc.line_entry.range.GetBaseAddress().GetLoadAddress(target);
       if (step_addr != LLDB_INVALID_ADDRESS) {
-        if (fun_range.ContainsLoadAddress(step_addr, target))
+        if (llvm::any_of(fun_ranges, [&](const AddressRange &r) {
+              return r.ContainsLoadAddress(step_addr, target);
+            }))
           step_over_until_addrs.push_back(step_addr);
         else
           all_in_function = false;
