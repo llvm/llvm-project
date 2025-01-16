5c98e1351abd85ac43a20297882adae9c575c374
Schrodinger ZHU Yifan <i@zhuyi.fan>
2025-01-16 04:08:22 +0000

    adjust build

--- libc/src/stdlib/snmalloc/malloc.cpp
+++ libc/src/stdlib/snmalloc/malloc.cpp
@@ -1,3 +1,11 @@
+//===-- Implementation of malloc ------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
 #include "src/stdlib/malloc.h"
 #include "snmalloc/snmalloc.h"
 #include "src/__support/common.h"
