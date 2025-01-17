7bd2ece853e1721368016f2776968deeeff9e323
Eric Astor <epastor@google.com>
2025-01-17 22:41:13 +0000

    Updated to match error convention

--- llvm/lib/MC/MCParser/COFFMasmParser.cpp
+++ llvm/lib/MC/MCParser/COFFMasmParser.cpp
@@ -442,8 +442,7 @@ bool COFFMasmParser::parseDirectiveOption(StringRef Directive, SMLoc Loc) {
 ///      label "endproc"
 bool COFFMasmParser::parseDirectiveProc(StringRef Directive, SMLoc Loc) {
   if (!getStreamer().getCurrentFragment()) {
-    return Error(getTok().getLoc(), "expected section directive before '" +
-                                        Directive + "' directive");
+    return Error(getTok().getLoc(), "expected section directive");
   }
 
   StringRef Label;
