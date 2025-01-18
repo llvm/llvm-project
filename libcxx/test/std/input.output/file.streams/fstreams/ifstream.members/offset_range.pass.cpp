3d17650f5016f96d8e3472e07de802ef39e1de58
Vladimir Vereschaka <vvereschaka@accesssoftek.com>
2025-01-18 14:10:59 -0800

    Mark the test as UNSUPPORTED for the armv7-unknown-linux-gnueabihf target.

--- libcxx/test/std/input.output/file.streams/fstreams/ifstream.members/offset_range.pass.cpp
+++ libcxx/test/std/input.output/file.streams/fstreams/ifstream.members/offset_range.pass.cpp
@@ -31,7 +31,7 @@
 // By default, off_t is typically a 32-bit integer on ARMv7 Linux systems,
 // meaning it can represent file sizes up to 2GB (2^31 bytes) only.
 //
-// XFAIL: target=armv7{{.*}}-{{.+}}-linux{{.*}}
+// UNSUPPORTED: target=armv7-unknown-linux-gnueabihf
 
 #include <fstream>
 #include <iostream>
