5c351d7ceb424ad462a6128cdb455b23dc1a1a14
Jason Rice <ricejasonf@gmail.com>
2025-01-18 06:47:44 -0800

    [Clang][P1061] Fix error messages in tests

--- clang/test/Parser/cxx2c-binding-pack.cpp
+++ clang/test/Parser/cxx2c-binding-pack.cpp
@@ -4,8 +4,8 @@ template <unsigned N>
 void decompose_array() {
   int arr[4] = {1, 2, 3, 5};
   auto [x, ... // #1
-    rest, ...more_rest] = arr; // expected-error{{multiple ellipses in structured binding declaration}}
-                               // expected-note@#1{{previous ellipsis specified here}}
+    rest, ...more_rest] = arr; // expected-error{{multiple packs in structured binding declaration}}
+                               // expected-note@#1{{previous binding pack specified here}}
                                //
   auto [y...] = arr; // expected-error{{'...' must immediately precede declared identifier}}
 }
