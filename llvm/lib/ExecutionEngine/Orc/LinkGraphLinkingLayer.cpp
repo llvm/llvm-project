b7c48b9a211bbf17dd95587a30fea8aab26e7d5c
Lang Hames <lhames@gmail.com>
2025-01-20 13:59:30 +1100

    Fix typo in comment

--- llvm/lib/ExecutionEngine/Orc/LinkGraphLinkingLayer.cpp
+++ llvm/lib/ExecutionEngine/Orc/LinkGraphLinkingLayer.cpp
@@ -1,4 +1,4 @@
-//===----- LinkGraphLinkingLayer.cpp - JITLink backed ORC ObjectLayer -----===//
+//===------ LinkGraphLinkingLayer.cpp - Link LinkGraphs with JITLink ------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
