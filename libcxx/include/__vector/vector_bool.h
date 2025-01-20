acc1a87c51223bd099eb006a6b7d3b141615d3d4
Peng Liu <winner245@hotmail.com>
2025-01-20 14:45:49 -0500

    Add explicit precondition check

--- libcxx/include/__vector/vector_bool.h
+++ libcxx/include/__vector/vector_bool.h
@@ -552,28 +552,29 @@ vector<bool, _Allocator>::__recommend(size_type __new_size) const {
 }
 
 //  Default constructs __n objects starting at __end_
-//  Precondition:  __n > 0
 //  Precondition:  size() + __n <= capacity()
 //  Postcondition:  size() == size() + __n
 template <class _Allocator>
 inline _LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX20 void
 vector<bool, _Allocator>::__construct_at_end(size_type __n, bool __x) {
-  _LIBCPP_ASSERT_INTERNAL(__n > 0, "This function expects __n > 0");
-  iterator __old_end = end();
+  _LIBCPP_ASSERT_INTERNAL(
+      capacity() >= size() + __n, "vector<bool>::__construct_at_end called with insufficient capacity");
+  std::fill_n(end(), __n, __x);
   this->__size_ += __n;
-  this->__begin_[(this->__size_ - 1) / __bits_per_word] = __storage_type(0);
-  std::fill_n(__old_end, __n, __x);
+  if (end().__ctz_ != 0) // has uninitialized trailing bits in the last word
+    std::fill_n(end(), __bits_per_word - end().__ctz_, 0);
 }
 
 template <class _Allocator>
 template <class _InputIterator, class _Sentinel>
 _LIBCPP_CONSTEXPR_SINCE_CXX20 void
 vector<bool, _Allocator>::__construct_at_end(_InputIterator __first, _Sentinel __last, size_type __n) {
-  _LIBCPP_ASSERT_INTERNAL(__n > 0, "This function expects __n > 0");
-  iterator __old_end = end();
+  _LIBCPP_ASSERT_INTERNAL(
+      capacity() >= size() + __n, "vector<bool>::__construct_at_end called with insufficient capacity");
+  std::__copy(std::move(__first), std::move(__last), end());
   this->__size_ += __n;
-  this->__begin_[(this->__size_ - 1) / __bits_per_word] = __storage_type(0);
-  std::__copy(std::move(__first), std::move(__last), __old_end);
+  if (end().__ctz_ != 0) // has uninitialized trailing bits in the last word
+    std::fill_n(end(), __bits_per_word - end().__ctz_, 0);
 }
 
 template <class _Allocator>
