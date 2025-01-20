c2a955c5196918f53397d81bc0e77bd11f8418cb
Simon Pilgrim <llvm-dev@redking.me.uk>
2025-01-20 16:25:26 +0000

    Update re-association comment for vXi1 cases.

--- llvm/lib/Transforms/Scalar/Reassociate.cpp
+++ llvm/lib/Transforms/Scalar/Reassociate.cpp
@@ -2174,12 +2174,13 @@ void ReassociatePass::OptimizeInst(Instruction *I) {
   if (isa<FPMathOperator>(I) && !hasFPAssociativeFlags(I))
     return;
 
-  // Do not reassociate boolean (i1) expressions.  We want to preserve the
+  // Do not reassociate boolean (i1/vXi1) expressions.  We want to preserve the
   // original order of evaluation for short-circuited comparisons that
   // SimplifyCFG has folded to AND/OR expressions.  If the expression
   // is not further optimized, it is likely to be transformed back to a
   // short-circuited form for code gen, and the source order may have been
-  // optimized for the most likely conditions.
+  // optimized for the most likely conditions. For vector boolean expressions,
+  // we should optimizing for ILP and not serializing the logical operations.
   if (I->getType()->isIntOrIntVectorTy(1))
     return;
 
