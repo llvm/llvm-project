0ea6058474c49fac0c07ccfd4e181b10e13e69c4
Dominik Steenken <dominik.steenken@gmail.com>
2025-01-21 10:38:02 +0100

    [SystemZ] Add is[LoadFrom|StoreTo]StackSlotPostFE to SystemZBackend As part of an effort to enable instr-ref-based debug value tracking, this commit implements `SystemZInstrInfo::isLoadFromStackSlotPostFE`, as well as `SystemZInstrInfo::isStoreToStackSlotPostFE`. The implementation relies upon the presence of MachineMemoryOperands on the relevant `MachineInstr`s in order to access the `FrameIndex` post frame index elimination.
    Since these new functions are only meant to be called after frame-index
    elimination, they assert against the present of a frame index on the
    base register operand of the instruction.
    
    Outside of the utility of these functions to enable instr-ref-based debug value
    tracking, they also changes the behavior of the AsmPrinter, since it will now
    be able to properly detect non-folded spills and reloads, so this changes a
    number of tests that were checking specifically for folded reloads.
    
    Note that there are some tests that still check for `vst` and `vl` as folded
    spills/reloads even though they should be straight reloads. This will be
    addressed in a future commit.

--- llvm/lib/Target/SystemZ/SystemZInstrInfo.h
+++ llvm/lib/Target/SystemZ/SystemZInstrInfo.h
@@ -232,6 +232,10 @@ public:
                                int &FrameIndex) const override;
   Register isStoreToStackSlot(const MachineInstr &MI,
                               int &FrameIndex) const override;
+  Register isLoadFromStackSlotPostFE(const MachineInstr &MI,
+                                     int &FrameIndex) const override;
+  Register isStoreToStackSlotPostFE(const MachineInstr &MI,
+                                    int &FrameIndex) const override;
   bool isStackSlotCopy(const MachineInstr &MI, int &DestFrameIndex,
                        int &SrcFrameIndex) const override;
   bool analyzeBranch(MachineBasicBlock &MBB, MachineBasicBlock *&TBB,
