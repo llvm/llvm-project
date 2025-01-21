baeedd71d2463448aa74e9bc6178946a74f1028e
Jan Patrick Lehr <JanPatrick.Lehr@amd.com>
2025-01-21 20:00:48 +0100

    [Flang][OpenMP] Fix run line w/ use mod_lib (#123762)
    After the crude workaround in
    https://github.com/llvm/llvm-project/pull/123666, @mjklemm suggested
    this change instead to address the issue.
    Local testing did not show errors, so I'm inclined to land it and see
    what the buildbots think.

--- flang/test/Lower/OpenMP/Todo/allocate-clause-allocator.f90
+++ flang/test/Lower/OpenMP/Todo/allocate-clause-allocator.f90
@@ -1,11 +1,10 @@
 ! REQUIRES: openmp_runtime
-! RUN: %not_todo_cmd %flang_fc1 -emit-llvm -fopenmp -fopenmp-version=51 -o - %s 2>&1 | FileCheck %s
+! RUN: %not_todo_cmd %flang_fc1 -emit-llvm %openmp_flags -fopenmp-version=51 -o - %s 2>&1 | FileCheck %s
 
 ! CHECK: not yet implemented: Unhandled clause allocate in omp.parallel
 ! CHECK: LLVM Translation failed for operation: omp.parallel
 program p
-  !use omp_lib
-  integer(8),parameter::omp_default_mem_alloc=1_8
+  use omp_lib
   integer :: x
   integer :: a
   integer :: i
