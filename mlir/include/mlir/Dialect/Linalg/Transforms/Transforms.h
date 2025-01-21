a5ef32cf1b214e1cc056dd5d82cfff15a883a388
Hyunsung Lee <ita9naiwa@gmail.com>
2025-01-21 20:34:02 +0900

    Update Transforms.h

--- mlir/include/mlir/Dialect/Linalg/Transforms/Transforms.h
+++ mlir/include/mlir/Dialect/Linalg/Transforms/Transforms.h
@@ -701,7 +701,7 @@ FailureOr<GenericOp> interchangeGenericOp(RewriterBase &rewriter,
 
 /// Create a GenericOp from the given named operation `linalgOp` and replace
 /// the given `linalgOp`.
-/// Return failure if `namedOp` is a GenericOp or misses a region builder.
+/// Return failure if `linalgOp` is a GenericOp or misses a region builder.
 FailureOr<GenericOp> generalizeNamedOp(RewriterBase &rewriter,
                                        LinalgOp linalgOp);
 
