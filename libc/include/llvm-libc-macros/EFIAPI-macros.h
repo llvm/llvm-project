d395d1be94905dda08abca21e933dac35b331cc3
Tristan Ross <tristan.ross@midstall.com>
2025-01-21 18:54:23 -0800

    [libc] add copyright to EFIAPI macro

--- libc/include/llvm-libc-macros/EFIAPI-macros.h
+++ libc/include/llvm-libc-macros/EFIAPI-macros.h
@@ -1,3 +1,11 @@
+//===-- Definition of EFIAPI macro ------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===------------------------------------------------------------------===//
+
 #ifndef LLVM_LIBC_MACROS_EFIAPI_MACROS_H
 #define LLVM_LIBC_MACROS_EFIAPI_MACROS_H
 
