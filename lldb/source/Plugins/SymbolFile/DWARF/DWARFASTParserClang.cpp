7d06c3cc944a6c64c5b9348207b848e2ef32e22a
Michael Buch <michaelbuch12@gmail.com>
2025-01-23 11:20:14 +0000

    Revert "[lldb][DWARFASTParserClang] Make C++ method parsing aware of explicit object parameters" (#124100)
    Reverts llvm/llvm-project#124096
    
    Broke linux CI:
    ```
    Note: This is test shard 7 of 42.
    [==========] Running 1 test from 1 test suite.
    [----------] Global test environment set-up.
    [----------] 1 test from DWARFASTParserClangTests
    [ RUN      ] DWARFASTParserClangTests.TestParseSubroutine_ExplicitObjectParameter
    Expected<T> must be checked before access or destruction.
    Expected<T> value was in success state. (Note: Expected<T> values in success mode must still be checked prior to being destroyed).
    Stack dump without symbol names (ensure you have llvm-symbolizer in your PATH or set the environment var `LLVM_SYMBOLIZER_PATH` to point to it):
    0  SymbolFileDWARFTests 0x0000560271ee5ba7
    1  SymbolFileDWARFTests 0x0000560271ee3a2c
    2  SymbolFileDWARFTests 0x0000560271ee63ea
    3  libc.so.6            0x00007f3e54e5b050
    4  libc.so.6            0x00007f3e54ea9e2c
    5  libc.so.6            0x00007f3e54e5afb2 gsignal + 18
    6  libc.so.6            0x00007f3e54e45472 abort + 211
    7  SymbolFileDWARFTests 0x0000560271e79d51
    8  SymbolFileDWARFTests 0x0000560271e724f7
    9  SymbolFileDWARFTests 0x0000560271f39e2c
    10 SymbolFileDWARFTests 0x0000560271f3b368
    11 SymbolFileDWARFTests 0x0000560271f3c053
    12 SymbolFileDWARFTests 0x0000560271f4cf67
    13 SymbolFileDWARFTests 0x0000560271f4c18a
    14 SymbolFileDWARFTests 0x0000560271f2561c
    15 libc.so.6            0x00007f3e54e4624a
    16 libc.so.6            0x00007f3e54e46305 __libc_start_main + 133
    17 SymbolFileDWARFTests 0x0000560271e65161
    ```

--- lldb/source/Plugins/SymbolFile/DWARF/DWARFASTParserClang.cpp
+++ lldb/source/Plugins/SymbolFile/DWARF/DWARFASTParserClang.cpp
@@ -173,9 +173,7 @@ GetCXXObjectParameter(const DWARFDIE &subprogram,
   if (!DeclKindIsCXXClass(containing_decl_ctx.getDeclKind()))
     return {};
 
-  if (DWARFDIE object_parameter =
-          subprogram.GetAttributeValueAsReferenceDIE(DW_AT_object_pointer))
-    return object_parameter;
+  // FIXME: if subprogram has a explicit DW_AT_object_pointer, use it.
 
   // If no DW_AT_object_pointer was specified, assume the implicit object
   // parameter is the first parameter to the function, is called "this" and is
@@ -217,6 +215,11 @@ static unsigned GetCXXMethodCVQuals(const DWARFDIE &subprogram,
     return 0;
 
   uint32_t encoding_mask = this_type->GetEncodingMask();
+
+  // FIXME: explicit object parameters need not to be pointers
+  if (!(encoding_mask & (1u << Type::eEncodingIsPointerUID)))
+    return 0;
+
   unsigned cv_quals = 0;
   if (encoding_mask & (1u << Type::eEncodingIsConstUID))
     cv_quals |= clang::Qualifiers::Const;
