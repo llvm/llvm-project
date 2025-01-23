fe8a43b1cefad72b154a68c1ca666b37f4d7e238
Nikita Popov <npopov@redhat.com>
2025-01-23 15:14:15 +0100

    [X86] Fix ABI for passing after i128
    If we're passing an i128 value and we no longer have enough argument
    registers (only r9 unallocated), the value gets passed via the stack.
    However, r9 is still allocated as a shadow register, which means that
    a following i64 argument will not use it. This doesn't match the
    x86-64 psABI.
    
    Fix this by making i128 arguments as requiring consecutive registers,
    and then adding a custom CC lowering that will allocate both parts of
    the i128 at the same time, either to register or to stack, without
    reserving a shadow register.
    
    Fixes https://github.com/llvm/llvm-project/issues/123935.

--- llvm/lib/Target/X86/X86CallingConv.cpp
+++ llvm/lib/Target/X86/X86CallingConv.cpp
@@ -340,5 +340,41 @@ static bool CC_X86_64_Pointer(unsigned &ValNo, MVT &ValVT, MVT &LocVT,
   return false;
 }
 
+/// Special handling for i128: Either allocate the value to two consecutive
+/// i64 registers, or to the stack. Do not partially allocate in registers,
+/// and do not reserve any registers when allocating to the stack.
+static bool CC_X86_64_I128(unsigned &ValNo, MVT &ValVT, MVT &LocVT,
+                           CCValAssign::LocInfo &LocInfo,
+                           ISD::ArgFlagsTy &ArgFlags, CCState &State) {
+  assert(ValVT == MVT::i64 && "Should have i64 parts");
+  SmallVectorImpl<CCValAssign> &PendingMembers = State.getPendingLocs();
+  PendingMembers.push_back(
+      CCValAssign::getPending(ValNo, ValVT, LocVT, LocInfo));
+
+  if (!ArgFlags.isInConsecutiveRegsLast())
+    return true;
+
+  unsigned NumRegs = PendingMembers.size();
+  assert(NumRegs == 2 && "Should have two parts");
+
+  MCPhysReg Regs[] = {X86::RDI, X86::RSI, X86::RDX, X86::RCX, X86::R8, X86::R9};
+  ArrayRef<MCPhysReg> Allocated = State.AllocateRegBlock(Regs, NumRegs);
+  if (!Allocated.empty()) {
+    for (const auto &[Pending, Reg] : zip(PendingMembers, Allocated)) {
+      Pending.convertToReg(Reg);
+      State.addLoc(Pending);
+    }
+  } else {
+    int64_t Offset = State.AllocateStack(8, Align(16));
+    for (auto &Pending : PendingMembers) {
+      Pending.convertToMem(Offset);
+      State.addLoc(Pending);
+      Offset += 8;
+    }
+  }
+  PendingMembers.clear();
+  return true;
+}
+
 // Provides entry points of CC_X86 and RetCC_X86.
 #include "X86GenCallingConv.inc"
