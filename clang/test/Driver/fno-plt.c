e0b459cf8fa0b2cec966f506d579a7dd658ec744
Fangrui Song <i@maskray.me>
2025-01-23 08:59:30 -0800

    update test
    Created using spr 1.3.5-bogner

--- clang/test/Driver/fno-plt.c
+++ clang/test/Driver/fno-plt.c
@@ -1,9 +1,10 @@
-// RUN: %clang -### -c --target=aarch64 -fno-plt -Werror %s 2>&1 | FileCheck %s
-// RUN: %clang -### -c --target=x86_64 -fno-plt -Werror %s 2>&1 | FileCheck %s
-// RUN: %clang -### -c --target=aarch64 -fno-plt -fplt -Werror %s 2>&1 | FileCheck %s --check-prefix=NO
-// RUN: %clang -### -c --target=powerpc64 -fno-plt %s 2>&1 | FileCheck %s --check-prefixes=WARN,NO
-// RUN: %clang -### -c --target=aarch64-windows -fno-plt %s 2>&1 | FileCheck %s --check-prefixes=WARN,NO
+// RUN: %clang -### -c --target=aarch64 -fno-plt -Werror %s 2>&1 | FileCheck %s --check-prefix=NOPLT
+// RUN: %clang -### -c --target=x86_64 -fno-plt -Werror %s 2>&1 | FileCheck %s --check-prefix=NOPLT
+
+// RUN: %clang -### -c --target=aarch64 -fno-plt -fplt -Werror %s 2>&1 | FileCheck %s --check-prefix=DEFAULT
+// RUN: %clang -### -c --target=powerpc64 -fno-plt %s 2>&1 | FileCheck %s --check-prefixes=WARN,DEFAULT
+// RUN: %clang -### -c --target=aarch64-windows -fno-plt %s 2>&1 | FileCheck %s --check-prefixes=WARN,DEFAULT
 
 // WARN: warning: argument unused during compilation: '-fno-plt' [-Wunused-command-line-argument]
-// CHECK: "-fno-plt"
-// NO-NOT: "-fno-plt"
+// NOPLT: "-fno-plt"
+// DEFAULT-NOT: "-fno-plt"
