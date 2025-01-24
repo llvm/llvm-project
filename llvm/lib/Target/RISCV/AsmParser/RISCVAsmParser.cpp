a5af348bd8ab3779211e3447f4094fa778bcd958
Min Hsu <min.hsu@sifive.com>
2025-01-24 09:50:20 -0800

    [RISCV][MC] Create an AsmOperand for carry-in vmask
    Previously we used a fixed assembly string as well as encoding for
    the carry-in vector mask, since it will always be there. However, this
    makes both AsmParser and disassembler to either create a garbage
    MCOperand for the mask or fail to add one as a whole.
    This wouldn't be a problem for majority of the cases but tools like
    llvm-mca who relies on MCInst will fail to account for the register
    dependency on these mask operands.

--- llvm/lib/Target/RISCV/AsmParser/RISCVAsmParser.cpp
+++ llvm/lib/Target/RISCV/AsmParser/RISCVAsmParser.cpp
@@ -1671,6 +1671,10 @@ bool RISCVAsmParser::matchAndEmitInstruction(SMLoc IDLoc, unsigned &Opcode,
     SMLoc ErrorLoc = ((RISCVOperand &)*Operands[ErrorInfo]).getStartLoc();
     return Error(ErrorLoc, "operand must be v0.t");
   }
+  case Match_InvalidVMaskCarryInRegister: {
+    SMLoc ErrorLoc = ((RISCVOperand &)*Operands[ErrorInfo]).getStartLoc();
+    return Error(ErrorLoc, "operand must be v0");
+  }
   case Match_InvalidSImm5Plus1: {
     return generateImmOutOfRangeError(Operands, ErrorInfo, -(1 << 4) + 1,
                                       (1 << 4),
