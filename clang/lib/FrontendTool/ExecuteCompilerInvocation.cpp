llvm/clangir

[CIR] Implement cc1 support for a CIR pipeline
This patch implements trivial cc1 support that'll let you pass
`-fenable-clangir` to run clang with the clangir pipeline inserted. This
also adds `-emit-cir` which will output a cir file and `-emit-cir-only`
which will run the pipeline stopping after CIRGen.

$ clang -cc1 test.c -fenable-clangir -emit-cir-only        (no output)
$ clang -cc1 test.c -fenable-clangir -emit-cir             (outputs test.cir)
$ clang -cc1 test.c -fenable-clangir -emit-cir -o here.cir (outputs here.cir)

The following invocation:

$ clang -cc1 test.c -fenable-clangir

will crash. But it will eventually work as stanard clang does honoring
your standard permutations of `-emit-llvm`, `-S`, `-c`,
`-emit-llvm-only` etc.

clang/lib/FrontendTool/ExecuteCompilerInvocation.cpp
@@ -12,6 +12,7 @@
 //===----------------------------------------------------------------------===//
 
 #include "clang/ARCMigrate/ARCMTActions.h"
+#include "clang/CIRFrontendAction/CIRGenAction.h"
 #include "clang/CodeGen/CodeGenAction.h"
 #include "clang/Config/config.h"
 #include "clang/Driver/Options.h"
@@ -32,6 +33,7 @@
 #include "llvm/Support/DynamicLibrary.h"
 #include "llvm/Support/ErrorHandling.h"
 using namespace clang;
+using namespace cir;
 using namespace llvm::opt;
 
 namespace clang {
@@ -41,6 +43,19 @@ CreateFrontendBaseAction(CompilerInstance &CI) {
   using namespace clang::frontend;
   StringRef Action("unknown");
   (void)Action;
+
+  auto CIR = CI.getFrontendOpts().UseClangIRPipeline;
+  auto Act = CI.getFrontendOpts().ProgramAction;
+
+  auto EmitsCIR = Act == EmitCIR || Act == EmitCIROnly;
+  auto IsImplementedCIROutput = EmitsCIR || Act == EmitLLVM;
+
+  if (UseCIR && !IsImplementedCIROutput)
+    llvm::report_fatal_error("-fclangir currently only works with -emit-cir, "
+                             "-emit-cir-only and -emit-llvm");
+  if (!UseCIR && EmitsCIR)
+    llvm::report_fatal_error(
+        "-emit-cir and -emit-cir-only only valid when using -fenable");
 
   switch (CI.getFrontendOpts().ProgramAction) {
   case ASTDeclList:            return std::make_unique<ASTDeclListAction>();
@@ -53,8 +68,8 @@ CreateFrontendBaseAction(CompilerInstance &CI) {
   case DumpTokens:             return std::make_unique<DumpTokensAction>();
   case EmitAssembly:           return std::make_unique<EmitAssemblyAction>();
   case EmitBC:                 return std::make_unique<EmitBCAction>();
-  case EmitCIR:
-    llvm_unreachable("CIR suppport not built into clang");
+  case EmitCIR:                return std::make_unique<EmitCIRAction>();
+  case EmitCIROnly:            return std::make_unique<EmitCIROnlyAction>();
   case EmitHTML:               return std::make_unique<HTMLPrintAction>();
   case EmitLLVM:               return std::make_unique<EmitLLVMAction>();
   case EmitLLVMOnly:           return std::make_unique<EmitLLVMOnlyAction>();

llvm/llvm-project

clang/lib/FrontendTool/ExecuteCompilerInvocation.cpp
@@ -12,6 +12,7 @@
 //===----------------------------------------------------------------------===//
 
 #include "clang/ARCMigrate/ARCMTActions.h"
+#include "clang/CIRFrontendAction/CIRGenAction.h"
 #include "clang/CodeGen/CodeGenAction.h"
 #include "clang/Config/config.h"
 #include "clang/Driver/Options.h"
@@ -32,6 +33,7 @@
 #include "llvm/Support/DynamicLibrary.h"
 #include "llvm/Support/ErrorHandling.h"
 using namespace clang;
+using namespace cir;
 using namespace llvm::opt;
 
 namespace clang {
@@ -41,6 +43,19 @@ CreateFrontendBaseAction(CompilerInstance &CI) {
   using namespace clang::frontend;
   StringRef Action("unknown");
   (void)Action;
+
+  auto UseCIR = CI.getFrontendOpts().UseClangIRPipeline;
+  auto Act = CI.getFrontendOpts().ProgramAction;
+
+  auto EmitsCIR = Act == EmitCIR || Act == EmitCIROnly;
+  auto IsImplementedCIROutput = EmitsCIR || Act == EmitLLVM;
+
+  if (UseCIR && !IsImplementedCIROutput)
+    llvm::report_fatal_error("-fclangir currently only works with -emit-cir, "
+                             "-emit-cir-only and -emit-llvm");
+  if (!UseCIR && EmitsCIR)
+    llvm::report_fatal_error(
+        "-emit-cir and -emit-cir-only only valid when using -fenable");
 
   switch (CI.getFrontendOpts().ProgramAction) {
   case ASTDeclList:            return std::make_unique<ASTDeclListAction>();
@@ -53,6 +68,8 @@ CreateFrontendBaseAction(CompilerInstance &CI) {
   case DumpTokens:             return std::make_unique<DumpTokensAction>();
   case EmitAssembly:           return std::make_unique<EmitAssemblyAction>();
   case EmitBC:                 return std::make_unique<EmitBCAction>();
+  case EmitCIR:                return std::make_unique<EmitCIRAction>();
+  case EmitCIROnly:            return std::make_unique<EmitCIROnlyAction>();
   case EmitHTML:               return std::make_unique<HTMLPrintAction>();
   case EmitLLVM:               return std::make_unique<EmitLLVMAction>();
   case EmitLLVMOnly:           return std::make_unique<EmitLLVMOnlyAction>();