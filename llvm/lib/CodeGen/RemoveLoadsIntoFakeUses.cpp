0d4a75b411c26531aa84d37ed39a0e4f5eebc784
Stephen Tozer <stephen.tozer@sony.com>
2025-01-24 18:14:46 +0000

    Fix check for instr-ref enabled, update test to check disabled case

--- llvm/lib/CodeGen/RemoveLoadsIntoFakeUses.cpp
+++ llvm/lib/CodeGen/RemoveLoadsIntoFakeUses.cpp
@@ -79,7 +79,7 @@ INITIALIZE_PASS_END(RemoveLoadsIntoFakeUses, DEBUG_TYPE,
 bool RemoveLoadsIntoFakeUses::runOnMachineFunction(MachineFunction &MF) {
   // Skip this pass if we would use VarLoc-based LDV, as there may be DBG_VALUE
   // instructions of the restored values that would become invalid.
-  if (debuginfoShouldUseDebugInstrRef(MF.getTarget().getTargetTriple()))
+  if (!MF.shouldUseDebugInstrRef())
     return false;
   // Only run this for functions that have fake uses.
   if (!MF.hasFakeUses() || skipFunction(MF.getFunction()))
