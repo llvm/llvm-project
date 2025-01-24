83f95f03bdd2ba4d34f12df175090a3c0fbce80d
PeterChou1 <peter.chou@mail.utoronto.ca>
2025-01-23 21:18:42 -0500

    typo

--- llvm/unittests/Support/MustacheTest.cpp
+++ llvm/unittests/Support/MustacheTest.cpp
@@ -1043,7 +1043,7 @@ TEST(MustacheLambdas, Sections) {
   Value D = Object{};
   auto T = Template("<{{#lambda}}{{x}}{{/lambda}}>");
   SectionLambda L = [](StringRef Text) -> llvm::json::Value {
-git    if (Text == "{{x}}") {
+    if (Text == "{{x}}") {
       return "yes";
     }
     return "no";
