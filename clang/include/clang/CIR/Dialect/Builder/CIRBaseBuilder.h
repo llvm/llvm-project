3d29fedc85cd8450be0bccf96f9cc0e5321c7fc4
Nathan Lanza <nathanlanza@gmail.com>
2025-01-23 21:58:06 -0800

    Revert "[CIR] Upstream initial attribute support (#121069)"
    This reverts commit 8e329593313bb792592529ee825a52683108df99.

--- clang/include/clang/CIR/Dialect/Builder/CIRBaseBuilder.h
+++ clang/include/clang/CIR/Dialect/Builder/CIRBaseBuilder.h
@@ -9,11 +9,7 @@
 #ifndef LLVM_CLANG_CIR_DIALECT_BUILDER_CIRBASEBUILDER_H
 #define LLVM_CLANG_CIR_DIALECT_BUILDER_CIRBASEBUILDER_H
 
-#include "clang/CIR/Dialect/IR/CIRAttrs.h"
-
 #include "mlir/IR/Builders.h"
-#include "mlir/IR/BuiltinTypes.h"
-#include "mlir/IR/Types.h"
 
 namespace cir {
 
@@ -29,13 +25,6 @@ public:
 
   cir::PointerType getVoidPtrTy() {
     return getPointerTo(cir::VoidType::get(getContext()));
-  }
-
-  mlir::TypedAttr getConstPtrAttr(mlir::Type type, int64_t value) {
-    auto valueAttr = mlir::IntegerAttr::get(
-        mlir::IntegerType::get(type.getContext(), 64), value);
-    return cir::ConstPtrAttr::get(
-        getContext(), mlir::cast<cir::PointerType>(type), valueAttr);
   }
 };
 
