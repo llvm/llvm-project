4bf553e5c55376b42869de01440aadcf8acc2d8c
Kazu Hirata <kazu@google.com>
2025-01-25 12:41:18 -0800

    Address comments.

--- clang/include/clang/AST/DeclBase.h
+++ clang/include/clang/AST/DeclBase.h
@@ -1391,9 +1391,7 @@ public:
   const_iterator end() const { return iterator(); }
 
   bool empty() const { return Result.isNull();  }
-  bool isSingleResult() const {
-    return dyn_cast_if_present<NamedDecl *>(Result);
-  }
+  bool isSingleResult() const { return isa_and_present<NamedDecl *>(Result); }
   reference front() const { return *begin(); }
 
   // Find the first declaration of the given type in the list. Note that this
