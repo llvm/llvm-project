5bfe144e8da1842b403d1d7f734da62b23b6990d
Nathan Lanza <nathanlanza@gmail.com>
2025-01-26 10:25:42 -0800

    [CIR] Add empty CIRBaseBuilder file

---
+++ clang/include/clang/CIR/Dialect/Builder/CIRBaseBuilder.h
@@ -0,0 +1,7 @@
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_CLANG_CIR_DIALECT_BUILDER_CIRBASEBUILDER_H
+#define LLVM_CLANG_CIR_DIALECT_BUILDER_CIRBASEBUILDER_H
+#include "mlir/IR/Builders.h"
+
+#endif
