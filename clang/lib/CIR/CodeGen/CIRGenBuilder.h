llvm/clangir

[CIR] Implement cir.int type and attribute
Replaces the usage of the building integer types by a dialect-specific
integer with arbitrary size and signedness. Since building integer
attributes requires builtin integer types, a cir.int attribute was also
created.

clang/lib/CIR/CodeGen/CIRGenBuilder.h
@@ -13,10 +13,15 @@
 #include "CIRGenTypeCache.h"
 #include "UnimplementedFeatureGuarding.h"
 
+#include "clang/CIR/Dialect/IR/CIRAttrs.h"
 #include "clang/CIR/Dialect/IR/CIRDialect.h"
+#include "clang/CIR/Dialect/IR/CIRTypes.h"
 #include "clang/CIR/Dialect/IR/FPEnv.h"
 
+#include "mlir/IR/Attributes.h"
 #include "mlir/IR/Builders.h"
+#include "mlir/IR/BuiltinAttributes.h"
+#include "mlir/IR/BuiltinTypes.h"
 #include "llvm/ADT/FloatingPointMode.h"
 
 namespace cir {
@@ -145,6 +150,17 @@ public:
   mlir::Type getInt8Ty() { return typeCache.Int8Ty; }
   mlir::Type getInt32Ty() { return typeCache.Int32Ty; }
   mlir::Type getInt64Ty() { return typeCache.Int64Ty; }
+
+  mlir::Type getSInt8Ty() { return typeCache.SInt8Ty; }
+  mlir::Type getSInt16Ty() { return typeCache.SInt16Ty; }
+  mlir::Type getSInt32Ty() { return typeCache.SInt32Ty; }
+  mlir::Type getSInt64Ty() { return typeCache.SInt64Ty; }
+
+  mlir::Type getUInt8Ty() { return typeCache.UInt8Ty; }
+  mlir::Type getUInt16Ty() { return typeCache.UInt16Ty; }
+  mlir::Type getUInt32Ty() { return typeCache.UInt32Ty; }
+  mlir::Type getUInt64Ty() { return typeCache.UInt64Ty; }
+
   mlir::cir::BoolType getBoolTy() {
     return ::mlir::cir::BoolType::get(getContext());
   }
@@ -173,6 +189,11 @@ public:
   // Constant creation helpers
   // -------------------------
   //
+  mlir::cir::ConstantOp getSInt32(uint32_t C, mlir::Location loc) {
+    auto SInt32Ty = getSInt32Ty();
+    return create<mlir::cir::ConstantOp>(loc, SInt32Ty,
+                                         mlir::cir::IntAttr::get(SInt32Ty, C));
+  }
   mlir::cir::ConstantOp getInt32(uint32_t C, mlir::Location loc) {
     auto int32Ty = getInt32Ty();
     return create<mlir::cir::ConstantOp>(loc, int32Ty,
@@ -197,9 +218,16 @@ public:
   mlir::cir::ConstantOp getNullValue(mlir::Type ty, mlir::Location loc) {
     if (ty.isa<mlir::cir::PointerType>())
       return getNullPtr(ty, loc);
-    assert(ty.isa<mlir::IntegerType>() && "NYI");
-    return create<mlir::cir::ConstantOp>(loc, ty,
-                                         mlir::IntegerAttr::get(ty, 0));
+
+    mlir::TypedAttr attr;
+    if (ty.isa<mlir::IntegerType>())
+      attr = mlir::IntegerAttr::get(ty, 0);
+    else if (ty.isa<mlir::cir::IntType>())
+      attr = mlir::cir::IntAttr::get(ty, 0);
+    else
+      llvm_unreachable("NYI");
+
+    return create<mlir::cir::ConstantOp>(loc, ty, attr);
   }
 
   mlir::cir::ConstantOp getZero(mlir::Location loc, mlir::Type ty) {

clang/lib/CIR/Dialect/IR/CIRTypes.cpp
@@ -16,9 +16,11 @@
 #include "mlir/IR/Attributes.h"
 #include "mlir/IR/BuiltinTypes.h"
 #include "mlir/IR/DialectImplementation.h"
+#include "mlir/Support/LogicalResult.h"
 
 #include "llvm/ADT/STLExtras.h"
 #include "llvm/ADT/TypeSwitch.h"
+#include "llvm/Support/ErrorHandling.h"
 
 #define GET_TYPEDEF_CLASSES
 #include "clang/CIR/Dialect/IR/CIROpsTypes.cpp.inc"
@@ -301,6 +303,91 @@ void StructType::computeSizeAndAlignment(
   size = structSize;
   align = structAlignment.value();
   padded = isPadded;
+}
+
+//===----------------------------------------------------------------------===//
+// IntType Definitions
+//===----------------------------------------------------------------------===//
+
+Type IntType::parse(mlir::AsmParser &parser) {
+  auto *context = parser.getBuilder().getContext();
+  auto loc = parser.getCurrentLocation();
+  bool isSigned;
+  unsigned width;
+
+  if (parser.parseLess())
+    return {};
+
+  // Fetch integer sign.
+  llvm::StringRef sign;
+  if (parser.parseKeyword(&sign))
+    return {};
+  if (sign.equals("s"))
+    isSigned = true;
+  else if (sign.equals("u"))
+    isSigned = false;
+  else {
+    parser.emitError(loc, "expected 's' or 'u'");
+    return {};
+  }
+
+  if (parser.parseComma())
+    return {};
+
+  // Fetch integer size.
+  if (parser.parseInteger(width))
+    return {};
+  if (width % 8 != 0) {
+    parser.emitError(loc, "expected integer width to be a multiple of 8");
+    return {};
+  }
+  if (width < 8 || width > 64) {
+    parser.emitError(loc, "expected integer width to be from 8 up to 64");
+    return {};
+  }
+
+  if (parser.parseGreater())
+    return {};
+
+  return IntType::get(context, width, isSigned);
+}
+
+void IntType::print(mlir::AsmPrinter &printer) const {
+  auto sign = isSigned() ? 's' : 'u';
+  printer << '<' << sign << ", " << getWidth() << '>';
+}
+
+llvm::TypeSize
+IntType::getTypeSizeInBits(const mlir::DataLayout &dataLayout,
+                           mlir::DataLayoutEntryListRef params) const {
+  return llvm::TypeSize::getFixed(getWidth());
+}
+
+uint64_t IntType::getABIAlignment(const mlir::DataLayout &dataLayout,
+                                  mlir::DataLayoutEntryListRef params) const {
+  return (uint64_t)(getWidth() / 8);
+}
+
+uint64_t
+IntType::getPreferredAlignment(const ::mlir::DataLayout &dataLayout,
+                               ::mlir::DataLayoutEntryListRef params) const {
+  return (uint64_t)(getWidth() / 8);
+}
+
+mlir::LogicalResult
+IntType::verify(llvm::function_ref<mlir::InFlightDiagnostic()> emitError,
+                unsigned width, bool isSigned) {
+
+  if (width < 8 || width > 64) {
+    emitError() << "IntType only supports widths from 8 up to 64";
+    return mlir::failure();
+  }
+  if (width % 8 != 0) {
+    emitError() << "IntType width is not a multiple of 8";
+    return mlir::failure();
+  }
+
+  return mlir::success();
 }
 
 //===----------------------------------------------------------------------===//

llvm/llvm-project

clang/lib/CIR/CodeGen/CIRGenBuilder.h
@@ -13,10 +13,15 @@
 #include "CIRGenTypeCache.h"
 #include "UnimplementedFeatureGuarding.h"
 
+#include "clang/CIR/Dialect/IR/CIRAttrs.h"
 #include "clang/CIR/Dialect/IR/CIRDialect.h"
+#include "clang/CIR/Dialect/IR/CIRTypes.h"
 #include "clang/CIR/Dialect/IR/FPEnv.h"
 
+#include "mlir/IR/Attributes.h"
 #include "mlir/IR/Builders.h"
+#include "mlir/IR/BuiltinAttributes.h"
+#include "mlir/IR/BuiltinTypes.h"
 #include "llvm/ADT/FloatingPointMode.h"
 
 namespace cir {
@@ -145,6 +150,17 @@ public:
   mlir::Type getInt8Ty() { return typeCache.Int8Ty; }
   mlir::Type getInt32Ty() { return typeCache.Int32Ty; }
   mlir::Type getInt64Ty() { return typeCache.Int64Ty; }
+
+  mlir::Type getSInt8Ty() { return typeCache.SInt8Ty; }
+  mlir::Type getSInt16Ty() { return typeCache.SInt16Ty; }
+  mlir::Type getSInt32Ty() { return typeCache.SInt32Ty; }
+  mlir::Type getSInt64Ty() { return typeCache.SInt64Ty; }
+
+  mlir::Type getUInt8Ty() { return typeCache.UInt8Ty; }
+  mlir::Type getUInt16Ty() { return typeCache.UInt16Ty; }
+  mlir::Type getUInt32Ty() { return typeCache.UInt32Ty; }
+  mlir::Type getUInt64Ty() { return typeCache.UInt64Ty; }
+
   mlir::cir::BoolType getBoolTy() {
     return ::mlir::cir::BoolType::get(getContext());
   }
@@ -173,6 +189,11 @@ public:
   // Constant creation helpers
   // -------------------------
   //
+  mlir::cir::ConstantOp getSInt32(int32_t C, mlir::Location loc) {
+    auto SInt32Ty = getSInt32Ty();
+    return create<mlir::cir::ConstantOp>(loc, SInt32Ty,
+                                         mlir::cir::IntAttr::get(SInt32Ty, C));
+  }
   mlir::cir::ConstantOp getInt32(uint32_t C, mlir::Location loc) {
     auto int32Ty = getInt32Ty();
     return create<mlir::cir::ConstantOp>(loc, int32Ty,
@@ -197,9 +218,16 @@ public:
   mlir::cir::ConstantOp getNullValue(mlir::Type ty, mlir::Location loc) {
     if (ty.isa<mlir::cir::PointerType>())
       return getNullPtr(ty, loc);
-    assert(ty.isa<mlir::IntegerType>() && "NYI");
-    return create<mlir::cir::ConstantOp>(loc, ty,
-                                         mlir::IntegerAttr::get(ty, 0));
+
+    mlir::TypedAttr attr;
+    if (ty.isa<mlir::IntegerType>())
+      attr = mlir::IntegerAttr::get(ty, 0);
+    else if (ty.isa<mlir::cir::IntType>())
+      attr = mlir::cir::IntAttr::get(ty, 0);
+    else
+      llvm_unreachable("NYI");
+
+    return create<mlir::cir::ConstantOp>(loc, ty, attr);
   }
 
   mlir::cir::ConstantOp getZero(mlir::Location loc, mlir::Type ty) {

clang/lib/CIR/Dialect/IR/CIRTypes.cpp
@@ -16,9 +16,11 @@
 #include "mlir/IR/Attributes.h"
 #include "mlir/IR/BuiltinTypes.h"
 #include "mlir/IR/DialectImplementation.h"
+#include "mlir/Support/LogicalResult.h"
 
 #include "llvm/ADT/STLExtras.h"
 #include "llvm/ADT/TypeSwitch.h"
+#include "llvm/Support/ErrorHandling.h"
 
 #define GET_TYPEDEF_CLASSES
 #include "clang/CIR/Dialect/IR/CIROpsTypes.cpp.inc"
@@ -301,6 +303,91 @@ void StructType::computeSizeAndAlignment(
   size = structSize;
   align = structAlignment.value();
   padded = isPadded;
+}
+
+//===----------------------------------------------------------------------===//
+// IntType Definitions
+//===----------------------------------------------------------------------===//
+
+Type IntType::parse(mlir::AsmParser &parser) {
+  auto *context = parser.getBuilder().getContext();
+  auto loc = parser.getCurrentLocation();
+  bool isSigned;
+  unsigned width;
+
+  if (parser.parseLess())
+    return {};
+
+  // Fetch integer sign.
+  llvm::StringRef sign;
+  if (parser.parseKeyword(&sign))
+    return {};
+  if (sign.equals_insensitive("s"))
+    isSigned = true;
+  else if (sign.equals_insensitive("u"))
+    isSigned = false;
+  else {
+    parser.emitError(loc, "expected 's' or 'u'");
+    return {};
+  }
+
+  if (parser.parseComma())
+    return {};
+
+  // Fetch integer size.
+  if (parser.parseInteger(width))
+    return {};
+  if (width % 8 != 0) {
+    parser.emitError(loc, "expected integer width to be a multiple of 8");
+    return {};
+  }
+  if (width < 8 || width > 64) {
+    parser.emitError(loc, "expected integer width to be from 8 up to 64");
+    return {};
+  }
+
+  if (parser.parseGreater())
+    return {};
+
+  return IntType::get(context, width, isSigned);
+}
+
+void IntType::print(mlir::AsmPrinter &printer) const {
+  auto sign = isSigned() ? 's' : 'u';
+  printer << '<' << sign << ", " << getWidth() << '>';
+}
+
+llvm::TypeSize
+IntType::getTypeSizeInBits(const mlir::DataLayout &dataLayout,
+                           mlir::DataLayoutEntryListRef params) const {
+  return llvm::TypeSize::getFixed(getWidth());
+}
+
+uint64_t IntType::getABIAlignment(const mlir::DataLayout &dataLayout,
+                                  mlir::DataLayoutEntryListRef params) const {
+  return (uint64_t)(getWidth() / 8);
+}
+
+uint64_t
+IntType::getPreferredAlignment(const ::mlir::DataLayout &dataLayout,
+                               ::mlir::DataLayoutEntryListRef params) const {
+  return (uint64_t)(getWidth() / 8);
+}
+
+mlir::LogicalResult
+IntType::verify(llvm::function_ref<mlir::InFlightDiagnostic()> emitError,
+                unsigned width, bool isSigned) {
+
+  if (width < 8 || width > 64) {
+    emitError() << "IntType only supports widths from 8 up to 64";
+    return mlir::failure();
+  }
+  if (width % 8 != 0) {
+    emitError() << "IntType width is not a multiple of 8";
+    return mlir::failure();
+  }
+
+  return mlir::success();
 }
 
 //===----------------------------------------------------------------------===//