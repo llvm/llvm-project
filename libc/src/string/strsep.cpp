e71222a2d191ed6a779af8b972b015111d249fb9
AlyElashram <alyahelashram@gmail.com>
2025-01-26 23:37:26 +0200

    Trap on both pointers in strsep.cpp

--- libc/src/string/strsep.cpp
+++ libc/src/string/strsep.cpp
@@ -16,9 +16,10 @@ namespace LIBC_NAMESPACE_DECL {
 
 LLVM_LIBC_FUNCTION(char *, strsep,
                    (char **__restrict stringp, const char *__restrict delim)) {
+  LIBC_CRASH_ON_NULLPTR(stringp);
+  LIBC_CRASH_ON_NULLPTR(delim);
   if (!*stringp)
     return nullptr;
-  LIBC_CRASH_ON_NULLPTR(delim);
   return internal::string_token<false>(*stringp, delim, stringp);
 }
 
