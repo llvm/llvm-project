5e2ee55f800726910ad6e56a192554375f61bfb8
Caroline Tice <cmtice@google.com>
2025-01-25 16:56:30 -0800

    Many changes, to address all the review	comments:
    - Remove "DIL" prefix from DILTokenKind	and DILToken.
    - Change the token kind	from an	enum class to an enum inside the Token
      class.
    - Use CamelCase	for all	the method names.
    - Replace Token::SetValues method with assignments.
    - Use a	StringRef, not std::string,  to hold the input string in the lexer.
    - Update the lexer to lex all the tokens at one	time. Added two	new methods
      for this: LexAll and GetNextToken.
    - Made some of the Lexer methods private.
    - Replaces StringMap with StringSwitch for fast keyword lookups.
    - Updated GetTokenName to directly return StringRefs; removed default case from
      switch statement.
    - Cleaned up code format in IsLetter	& IsDigit.
    - Updated IsWord too return an iterator	range containing the word (if any).
    - Updated Lex function (now called by LexAll) to return	an llvm::Expected
      token; removed look_ahead checks; changed the	operator lexing	to use
      a vector of operators	(as suggested).
    - Cleaned up LookAhead method, now that	we know	all tokens have	already	been
      lexed.
    - Added	helper function	to unittests, to help check a sequence of tokens.
    - Generally cleaned up the tests to deal with all the code changes.

--- lldb/source/ValueObject/DILLexer.cpp
+++ lldb/source/ValueObject/DILLexer.cpp
@@ -12,107 +12,99 @@
 //===----------------------------------------------------------------------===//
 
 #include "lldb/ValueObject/DILLexer.h"
-#include "llvm/ADT/StringMap.h"
+#include "lldb/Utility/Status.h"
+#include "llvm/ADT/StringSwitch.h"
 
 namespace lldb_private {
 
 namespace dil {
 
-// For fast keyword lookup. More keywords will be added later.
-const llvm::StringMap<dil::TokenKind> Keywords = {
-    {"namespace", dil::TokenKind::kw_namespace},
-};
-
-const std::string DILToken::getTokenName(dil::TokenKind kind) {
+llvm::StringRef Token::GetTokenName(Kind kind) {
   switch (kind) {
-  case dil::TokenKind::coloncolon:
+  case Kind::coloncolon:
     return "coloncolon";
-  case dil::TokenKind::eof:
+  case Kind::eof:
     return "eof";
-  case dil::TokenKind::identifier:
+  case Kind::identifier:
     return "identifier";
-  case dil::TokenKind::kw_namespace:
+  case Kind::invalid:
+    return "invalid";
+  case Kind::kw_namespace:
     return "namespace";
-  case dil::TokenKind::l_paren:
+  case Kind::l_paren:
     return "l_paren";
-  case dil::TokenKind::r_paren:
+  case Kind::none:
+    return "none";
+  case Kind::r_paren:
     return "r_paren";
-  case dil::TokenKind::unknown:
+  case Kind::unknown:
     return "unknown";
-  default:
-    return "token_name";
   }
 }
 
-static bool Is_Letter(char c) {
-  if (('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z'))
-    return true;
-  return false;
+static bool IsLetter(char c) {
+  return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z');
 }
 
-static bool Is_Digit(char c) { return ('0' <= c && c <= '9'); }
+static bool IsDigit(char c) { return '0' <= c && c <= '9'; }
 
 // A word starts with a letter, underscore, or dollar sign, followed by
 // letters ('a'..'z','A'..'Z'), digits ('0'..'9'), and/or  underscores.
-bool DILLexer::Is_Word(std::string::iterator start, uint32_t &length) {
-  bool done = false;
+llvm::iterator_range<llvm::StringRef::iterator> DILLexer::IsWord() {
+  llvm::StringRef::iterator start = m_cur_pos;
   bool dollar_start = false;
 
   // Must not start with a digit.
-  if (m_cur_pos == m_expr.end() || Is_Digit(*m_cur_pos))
-    return false;
+  if (m_cur_pos == m_expr.end() || IsDigit(*m_cur_pos))
+    return llvm::make_range(m_cur_pos, m_cur_pos);
 
   // First character *may* be a '$', for a register name or convenience
   // variable.
   if (*m_cur_pos == '$') {
     dollar_start = true;
     ++m_cur_pos;
-    length++;
   }
 
   // Contains only letters, digits or underscores
-  for (; m_cur_pos != m_expr.end() && !done; ++m_cur_pos) {
+  for (; m_cur_pos != m_expr.end(); ++m_cur_pos) {
     char c = *m_cur_pos;
-    if (!Is_Letter(c) && !Is_Digit(c) && c != '_') {
-      done = true;
+    if (!IsLetter(c) && !IsDigit(c) && c != '_')
       break;
-    } else
-      length++;
   }
 
-  if (dollar_start && length > 1) // Must have something besides just '$'
-    return true;
+  // If first char is '$', make sure there's at least one mare char, or it's
+  // invalid.
+  if (dollar_start && (m_cur_pos - start <= 1)) {
+    m_cur_pos = start;
+    return llvm::make_range(start, start); // Empty range
+  }
 
-  if (!dollar_start && length > 0)
-    return true;
-
-  // Not a valid word, so re-set the lexing position.
-  m_cur_pos = start;
-  return false;
+  return llvm::make_range(start, m_cur_pos);
 }
 
-void DILLexer::UpdateLexedTokens(DILToken &result, dil::TokenKind tok_kind,
+void DILLexer::UpdateLexedTokens(Token &result, Token::Kind tok_kind,
                                  std::string tok_str, uint32_t tok_pos) {
-  DILToken new_token;
-  result.setValues(tok_kind, tok_str, tok_pos);
-  new_token = result;
+  Token new_token(tok_kind, tok_str, tok_pos);
+  result = new_token;
   m_lexed_tokens.push_back(std::move(new_token));
 }
 
-bool DILLexer::Lex(DILToken &result, bool look_ahead) {
-  bool retval = true;
-
-  if (!look_ahead) {
-    // We're being asked for the 'next' token, and not a part of a LookAhead.
-    // Check to see if we've already lexed it and pushed it onto our tokens
-    // vector; if so, return the next token from the vector, rather than doing
-    // more lexing.
-    if ((m_tokens_idx != UINT_MAX) &&
-        (m_tokens_idx < m_lexed_tokens.size() - 1)) {
-      result = m_lexed_tokens[m_tokens_idx + 1];
-      return retval;
+llvm::Expected<bool> DILLexer::LexAll() {
+  bool done = false;
+  while (!done) {
+    auto tok_or_err = Lex();
+    if (!tok_or_err)
+      return tok_or_err.takeError();
+    Token token = *tok_or_err;
+    if (token.GetKind() == Token::eof) {
+      done = true;
     }
   }
+  return true;
+}
+
+llvm::Expected<Token> DILLexer::Lex() {
+  Token result;
 
   // Skip over whitespace (spaces).
   while (m_cur_pos != m_expr.end() && *m_cur_pos == ' ')
@@ -120,84 +112,76 @@ bool DILLexer::Lex(DILToken &result, bool look_ahead) {
 
   // Check to see if we've reached the end of our input string.
   if (m_cur_pos == m_expr.end()) {
-    UpdateLexedTokens(result, dil::TokenKind::eof, "", m_expr.length());
-    return retval;
+    UpdateLexedTokens(result, Token::eof, "", (uint32_t)m_expr.size());
+    return result;
   }
 
   uint32_t position = m_cur_pos - m_expr.begin();
-  ;
-  std::string::iterator start = m_cur_pos;
-  uint32_t length = 0;
-  if (Is_Word(start, length)) {
-    dil::TokenKind kind;
-    std::string word = m_expr.substr(position, length);
-    auto iter = Keywords.find(word);
-    if (iter != Keywords.end())
-      kind = iter->second;
-    else
-      kind = dil::TokenKind::identifier;
-
-    UpdateLexedTokens(result, kind, word, position);
-    return true;
+  llvm::StringRef::iterator start = m_cur_pos;
+  llvm::iterator_range<llvm::StringRef::iterator> word_range = IsWord();
+  if (!word_range.empty()) {
+    uint32_t length = word_range.end() - word_range.begin();
+    llvm::StringRef word(m_expr.substr(position, length));
+    // We will be adding more keywords here in the future...
+    Token::Kind kind = llvm::StringSwitch<Token::Kind>(word)
+                           .Case("namespace", Token::kw_namespace)
+                           .Default(Token::identifier);
+    UpdateLexedTokens(result, kind, word.str(), position);
+    return result;
   }
 
-  switch (*m_cur_pos) {
-  case '(':
-    m_cur_pos++;
-    UpdateLexedTokens(result, dil::TokenKind::l_paren, "(", position);
-    return true;
-  case ')':
-    m_cur_pos++;
-    UpdateLexedTokens(result, dil::TokenKind::r_paren, ")", position);
-    return true;
-  case ':':
-    if (position + 1 < m_expr.size() && m_expr[position + 1] == ':') {
-      m_cur_pos += 2;
-      UpdateLexedTokens(result, dil::TokenKind::coloncolon, "::", position);
-      return true;
+  m_cur_pos = start;
+  llvm::StringRef remainder(m_expr.substr(position, m_expr.end() - m_cur_pos));
+  std::vector<std::pair<Token::Kind, const char *>> operators = {
+      {Token::l_paren, "("},
+      {Token::r_paren, ")"},
+      {Token::coloncolon, "::"},
+  };
+  for (auto [kind, str] : operators) {
+    if (remainder.consume_front(str)) {
+      m_cur_pos += strlen(str);
+      UpdateLexedTokens(result, kind, str, position);
+      return result;
     }
-    break;
-  default:
-    break;
   }
-  // Empty Token
-  result.setValues(dil::TokenKind::none, "", m_expr.length());
-  return false;
+
+  // Unrecognized character(s) in string; unable to lex it.
+  Status error("Unable to lex input string");
+  return error.ToError();
 }
 
-const DILToken &DILLexer::LookAhead(uint32_t N) {
-  uint32_t extra_lexed_tokens = m_lexed_tokens.size() - m_tokens_idx - 1;
-
-  if (N + 1 < extra_lexed_tokens)
+const Token &DILLexer::LookAhead(uint32_t N) {
+  if (m_tokens_idx + N + 1 < m_lexed_tokens.size())
     return m_lexed_tokens[m_tokens_idx + N + 1];
 
-  uint32_t remaining_tokens =
-      (m_tokens_idx + N + 1) - m_lexed_tokens.size() + 1;
-
-  bool done = false;
-  bool look_ahead = true;
-  while (!done && remaining_tokens > 0) {
-    DILToken tok;
-    Lex(tok, look_ahead);
-    if (tok.getKind() == dil::TokenKind::eof)
-      done = true;
-    remaining_tokens--;
-  };
-
-  if (remaining_tokens > 0) {
-    m_invalid_token.setValues(dil::TokenKind::invalid, "", 0);
-    return m_invalid_token;
-  }
-
-  return m_lexed_tokens[m_tokens_idx + N + 1];
+  return m_invalid_token;
 }
 
-const DILToken &DILLexer::AcceptLookAhead(uint32_t N) {
+const Token &DILLexer::AcceptLookAhead(uint32_t N) {
   if (m_tokens_idx + N + 1 > m_lexed_tokens.size())
     return m_invalid_token;
 
   m_tokens_idx += N + 1;
   return m_lexed_tokens[m_tokens_idx];
+}
+
+const Token &DILLexer::GetNextToken() {
+  if (m_tokens_idx == UINT_MAX)
+    m_tokens_idx = 0;
+  else
+    m_tokens_idx++;
+
+  // Return the next token in the vector of lexed tokens.
+  if (m_tokens_idx < m_lexed_tokens.size())
+    return m_lexed_tokens[m_tokens_idx];
+
+  // We're already at/beyond the end of our lexed tokens. If the last token
+  // is an eof token, return it.
+  if (m_lexed_tokens[m_lexed_tokens.size() - 1].GetKind() == Token::eof)
+    return m_lexed_tokens[m_lexed_tokens.size() - 1];
+
+  // Return the invalid token.
+  return m_invalid_token;
 }
 
 } // namespace dil
--- lldb/unittests/ValueObject/DILLexerTests.cpp
+++ lldb/unittests/ValueObject/DILLexerTests.cpp
@@ -13,131 +13,145 @@
 
 using llvm::StringRef;
 
+bool VerifyExpectedTokens(
+    lldb_private::dil::DILLexer &lexer,
+    std::vector<std::pair<lldb_private::dil::Token::Kind, std::string>>
+        exp_tokens,
+    uint32_t start_pos) {
+  if (lexer.NumLexedTokens() - start_pos < exp_tokens.size())
+    return false;
+
+  if (start_pos > 0)
+    lexer.ResetTokenIdx(start_pos -
+                        1); // GetNextToken increments the idx first.
+  for (const auto &pair : exp_tokens) {
+    lldb_private::dil::Token token = lexer.GetNextToken();
+    if (token.GetKind() != pair.first || token.GetSpelling() != pair.second)
+      return false;
+  }
+
+  return true;
+}
+
 TEST(DILLexerTests, SimpleTest) {
-  StringRef dil_input_expr("simple_var");
+  StringRef input_expr("simple_var");
   uint32_t tok_len = 10;
-  lldb_private::dil::DILLexer dil_lexer(dil_input_expr);
-  lldb_private::dil::DILToken dil_token;
-  dil_token.setKind(lldb_private::dil::TokenKind::unknown);
-  EXPECT_EQ(dil_token.getKind(), lldb_private::dil::TokenKind::unknown);
-  dil_lexer.Lex(dil_token);
-  EXPECT_EQ(dil_token.getKind(), lldb_private::dil::TokenKind::identifier);
-  EXPECT_EQ(dil_token.getSpelling(), "simple_var");
-  EXPECT_EQ(dil_token.getLength(), tok_len);
-  dil_lexer.Lex(dil_token);
-  EXPECT_EQ(dil_token.getKind(), lldb_private::dil::TokenKind::eof);
+  lldb_private::dil::DILLexer lexer(input_expr);
+  lldb_private::dil::Token token;
+  token.SetKind(lldb_private::dil::Token::unknown);
+  EXPECT_EQ(token.GetKind(), lldb_private::dil::Token::unknown);
+  auto success = lexer.LexAll();
+
+  if (!success) {
+    EXPECT_TRUE(false);
+  }
+  token = lexer.GetNextToken();
+  EXPECT_EQ(token.GetKind(), lldb_private::dil::Token::identifier);
+  EXPECT_EQ(token.GetSpelling(), "simple_var");
+  EXPECT_EQ(token.GetLength(), tok_len);
+  token = lexer.GetNextToken();
+  ;
+  EXPECT_EQ(token.GetKind(), lldb_private::dil::Token::eof);
 }
 
 TEST(DILLexerTests, TokenKindTest) {
-  StringRef dil_input_expr("namespace");
-  lldb_private::dil::DILLexer dil_lexer(dil_input_expr);
-  lldb_private::dil::DILToken dil_token;
-  dil_token.setKind(lldb_private::dil::TokenKind::unknown);
+  StringRef input_expr("namespace");
+  lldb_private::dil::DILLexer lexer(input_expr);
+  lldb_private::dil::Token token;
+  token.SetKind(lldb_private::dil::Token::unknown);
 
-  dil_lexer.Lex(dil_token);
-  EXPECT_EQ(dil_lexer.GetCurrentTokenIdx(), UINT_MAX);
-  dil_lexer.ResetTokenIdx(0);
+  auto success = lexer.LexAll();
+  if (!success) {
+    EXPECT_TRUE(false);
+  }
+  token = lexer.GetNextToken();
 
-  EXPECT_EQ(dil_token.getKind(), lldb_private::dil::TokenKind::kw_namespace);
-  EXPECT_TRUE(dil_token.isNot(lldb_private::dil::TokenKind::identifier));
-  EXPECT_FALSE(dil_token.is(lldb_private::dil::TokenKind::l_paren));
-  EXPECT_TRUE(dil_token.isOneOf(lldb_private::dil::TokenKind::eof,
-                                lldb_private::dil::TokenKind::kw_namespace));
-  EXPECT_FALSE(dil_token.isOneOf(lldb_private::dil::TokenKind::l_paren,
-                                 lldb_private::dil::TokenKind::r_paren,
-                                 lldb_private::dil::TokenKind::coloncolon,
-                                 lldb_private::dil::TokenKind::eof));
+  EXPECT_EQ(token.GetKind(), lldb_private::dil::Token::kw_namespace);
+  EXPECT_TRUE(token.IsNot(lldb_private::dil::Token::identifier));
+  EXPECT_FALSE(token.Is(lldb_private::dil::Token::l_paren));
+  EXPECT_TRUE(token.IsOneOf(lldb_private::dil::Token::eof,
+                            lldb_private::dil::Token::kw_namespace));
+  EXPECT_FALSE(token.IsOneOf(
+      lldb_private::dil::Token::l_paren, lldb_private::dil::Token::r_paren,
+      lldb_private::dil::Token::coloncolon, lldb_private::dil::Token::eof));
 
-  dil_token.setKind(lldb_private::dil::TokenKind::identifier);
-  EXPECT_EQ(dil_token.getKind(), lldb_private::dil::TokenKind::identifier);
+  token.SetKind(lldb_private::dil::Token::identifier);
+  EXPECT_EQ(token.GetKind(), lldb_private::dil::Token::identifier);
 }
 
 TEST(DILLexerTests, LookAheadTest) {
-  StringRef dil_input_expr("(anonymous namespace)::some_var");
-  lldb_private::dil::DILLexer dil_lexer(dil_input_expr);
-  lldb_private::dil::DILToken dil_token;
-  dil_token.setKind(lldb_private::dil::TokenKind::unknown);
+  StringRef input_expr("(anonymous namespace)::some_var");
+  lldb_private::dil::DILLexer lexer(input_expr);
+  lldb_private::dil::Token token;
+  token.SetKind(lldb_private::dil::Token::unknown);
   uint32_t expect_loc = 23;
 
-  dil_lexer.Lex(dil_token);
-  EXPECT_EQ(dil_lexer.GetCurrentTokenIdx(), UINT_MAX);
-  dil_lexer.ResetTokenIdx(0);
+  auto success = lexer.LexAll();
+  if (!success) {
+    EXPECT_TRUE(false);
+  }
+  token = lexer.GetNextToken();
 
   // Current token is '('; check the next 4 tokens, to make
   // sure they are the identifier 'anonymous', the namespace keyword,
   // ')' and '::', in that order.
-  EXPECT_EQ(dil_token.getKind(), lldb_private::dil::TokenKind::l_paren);
-  EXPECT_EQ(dil_lexer.LookAhead(0).getKind(),
-            lldb_private::dil::TokenKind::identifier);
-  EXPECT_EQ(dil_lexer.LookAhead(0).getSpelling(), "anonymous");
-  EXPECT_EQ(dil_lexer.LookAhead(1).getKind(),
-            lldb_private::dil::TokenKind::kw_namespace);
-  EXPECT_EQ(dil_lexer.LookAhead(2).getKind(),
-            lldb_private::dil::TokenKind::r_paren);
-  EXPECT_EQ(dil_lexer.LookAhead(3).getKind(),
-            lldb_private::dil::TokenKind::coloncolon);
-  // Verify we've advanced our position counter (lexing location) in the
-  // input 23 characters (the length of '(anonymous namespace)::'.
-  EXPECT_EQ(dil_lexer.GetLocation(), expect_loc);
+  EXPECT_EQ(token.GetKind(), lldb_private::dil::Token::l_paren);
+  EXPECT_EQ(lexer.LookAhead(0).GetKind(), lldb_private::dil::Token::identifier);
+  EXPECT_EQ(lexer.LookAhead(0).GetSpelling(), "anonymous");
+  EXPECT_EQ(lexer.LookAhead(1).GetKind(),
+            lldb_private::dil::Token::kw_namespace);
+  EXPECT_EQ(lexer.LookAhead(2).GetKind(), lldb_private::dil::Token::r_paren);
+  EXPECT_EQ(lexer.LookAhead(3).GetKind(), lldb_private::dil::Token::coloncolon);
 
   // Our current index should still be 0, as we only looked ahead; we are still
   // officially on the '('.
-  EXPECT_EQ(dil_lexer.GetCurrentTokenIdx(), 0);
+  EXPECT_EQ(lexer.GetCurrentTokenIdx(), (uint32_t)0);
 
   // Accept the 'lookahead', so our current token is '::', which has the index
   // 4 in our vector of tokens (which starts at zero).
-  dil_token = dil_lexer.AcceptLookAhead(3);
-  EXPECT_EQ(dil_token.getKind(), lldb_private::dil::TokenKind::coloncolon);
-  EXPECT_EQ(dil_lexer.GetCurrentTokenIdx(), 4);
+  token = lexer.AcceptLookAhead(3);
+  EXPECT_EQ(token.GetKind(), lldb_private::dil::Token::coloncolon);
+  EXPECT_EQ(lexer.GetCurrentTokenIdx(), (uint32_t)4);
 
-  // Lex the final variable name in the input string
-  dil_lexer.Lex(dil_token);
-  dil_lexer.IncrementTokenIdx();
-  EXPECT_EQ(dil_token.getKind(), lldb_private::dil::TokenKind::identifier);
-  EXPECT_EQ(dil_token.getSpelling(), "some_var");
-  EXPECT_EQ(dil_lexer.GetCurrentTokenIdx(), 5);
-
-  dil_lexer.Lex(dil_token);
-  EXPECT_EQ(dil_token.getKind(), lldb_private::dil::TokenKind::eof);
+  token = lexer.GetNextToken();
+  EXPECT_EQ(token.GetKind(), lldb_private::dil::Token::identifier);
+  EXPECT_EQ(token.GetSpelling(), "some_var");
+  EXPECT_EQ(lexer.GetCurrentTokenIdx(), (uint32_t)5);
+  // Verify we've advanced our position counter (lexing location) in the
+  // input 23 characters (the length of '(anonymous namespace)::'.
+  EXPECT_EQ(token.GetLocation(), expect_loc);
+  token = lexer.GetNextToken();
+  EXPECT_EQ(token.GetKind(), lldb_private::dil::Token::eof);
 }
 
 TEST(DILLexerTests, MultiTokenLexTest) {
-  StringRef dil_input_expr("This string has several identifiers");
-  lldb_private::dil::DILLexer dil_lexer(dil_input_expr);
-  lldb_private::dil::DILToken dil_token;
-  dil_token.setKind(lldb_private::dil::TokenKind::unknown);
+  StringRef input_expr("This string has (several ) ::identifiers");
+  lldb_private::dil::DILLexer lexer(input_expr);
+  lldb_private::dil::Token token;
+  token.SetKind(lldb_private::dil::Token::unknown);
 
-  dil_lexer.Lex(dil_token);
-  EXPECT_EQ(dil_lexer.GetCurrentTokenIdx(), UINT_MAX);
-  dil_lexer.ResetTokenIdx(0);
+  auto success = lexer.LexAll();
+  if (!success) {
+    EXPECT_TRUE(false);
+  }
 
-  EXPECT_EQ(dil_token.getSpelling(), "This");
-  EXPECT_EQ(dil_token.getKind(), lldb_private::dil::TokenKind::identifier);
-  dil_lexer.Lex(dil_token);
-  dil_lexer.IncrementTokenIdx();
+  std::vector<std::pair<lldb_private::dil::Token::Kind, std::string>>
+      expected_tokens = {
+          {lldb_private::dil::Token::identifier, "This"},
+          {lldb_private::dil::Token::identifier, "string"},
+          {lldb_private::dil::Token::identifier, "has"},
+          {lldb_private::dil::Token::l_paren, "("},
+          {lldb_private::dil::Token::identifier, "several"},
+          {lldb_private::dil::Token::r_paren, ")"},
+          {lldb_private::dil::Token::coloncolon, "::"},
+          {lldb_private::dil::Token::identifier, "identifiers"},
+      };
 
-  EXPECT_EQ(dil_token.getSpelling(), "string");
-  EXPECT_EQ(dil_token.getKind(), lldb_private::dil::TokenKind::identifier);
-  dil_lexer.Lex(dil_token);
-  dil_lexer.IncrementTokenIdx();
+  EXPECT_TRUE(VerifyExpectedTokens(lexer, expected_tokens, 0));
 
-  EXPECT_EQ(dil_token.getSpelling(), "has");
-  EXPECT_EQ(dil_token.getKind(), lldb_private::dil::TokenKind::identifier);
-  dil_lexer.Lex(dil_token);
-  dil_lexer.IncrementTokenIdx();
-
-  EXPECT_EQ(dil_token.getSpelling(), "several");
-  EXPECT_EQ(dil_token.getKind(), lldb_private::dil::TokenKind::identifier);
-  dil_lexer.Lex(dil_token);
-  dil_lexer.IncrementTokenIdx();
-
-  EXPECT_EQ(dil_token.getSpelling(), "identifiers");
-  EXPECT_EQ(dil_token.getKind(), lldb_private::dil::TokenKind::identifier);
-  dil_lexer.Lex(dil_token);
-  dil_lexer.IncrementTokenIdx();
-
-  EXPECT_EQ(dil_token.getSpelling(), "");
-  EXPECT_EQ(dil_token.getKind(), lldb_private::dil::TokenKind::eof);
+  token = lexer.GetNextToken();
+  EXPECT_EQ(token.GetSpelling(), "");
+  EXPECT_EQ(token.GetKind(), lldb_private::dil::Token::eof);
 }
 
 TEST(DILLexerTests, IdentifiersTest) {
@@ -166,28 +180,39 @@ TEST(DILLexerTests, IdentifiersTest) {
   };
 
   // Verify that all of the valid identifiers come out as identifier tokens.
-  for (auto str : valid_identifiers) {
-    StringRef dil_input_expr(str);
-    lldb_private::dil::DILLexer dil_lexer(dil_input_expr);
-    lldb_private::dil::DILToken dil_token;
-    dil_token.setKind(lldb_private::dil::TokenKind::unknown);
+  for (auto &str : valid_identifiers) {
+    SCOPED_TRACE(str);
+    lldb_private::dil::DILLexer lexer(str);
+    lldb_private::dil::Token token;
+    token.SetKind(lldb_private::dil::Token::unknown);
 
-    dil_lexer.Lex(dil_token);
-    EXPECT_EQ(dil_token.getKind(), lldb_private::dil::TokenKind::identifier);
+    auto success = lexer.LexAll();
+    if (!success) {
+      EXPECT_TRUE(false);
+    }
+    token = lexer.GetNextToken();
+    EXPECT_EQ(token.GetKind(), lldb_private::dil::Token::identifier);
   }
 
   // Verify that none of the invalid identifiers come out as identifier tokens.
-  for (auto str : invalid_identifiers) {
-    StringRef dil_input_expr(str);
-    lldb_private::dil::DILLexer dil_lexer(dil_input_expr);
-    lldb_private::dil::DILToken dil_token;
-    dil_token.setKind(lldb_private::dil::TokenKind::unknown);
+  for (auto &str : invalid_identifiers) {
+    SCOPED_TRACE(str);
+    lldb_private::dil::DILLexer lexer(str);
+    lldb_private::dil::Token token;
+    token.SetKind(lldb_private::dil::Token::unknown);
 
-    dil_lexer.Lex(dil_token);
-    EXPECT_TRUE(dil_token.isNot(lldb_private::dil::TokenKind::identifier));
-    EXPECT_TRUE(dil_token.isOneOf(lldb_private::dil::TokenKind::unknown,
-                                  lldb_private::dil::TokenKind::none,
-                                  lldb_private::dil::TokenKind::eof,
-                                  lldb_private::dil::TokenKind::kw_namespace));
+    auto success = lexer.LexAll();
+    // In this case, it's ok for Lex() to return an error.
+    if (!success) {
+      llvm::consumeError(success.takeError());
+    } else {
+      // We didn't get an error; make sure we did not get an identifier token.
+      token = lexer.GetNextToken();
+      EXPECT_TRUE(token.IsNot(lldb_private::dil::Token::identifier));
+      EXPECT_TRUE(token.IsOneOf(lldb_private::dil::Token::unknown,
+                                lldb_private::dil::Token::none,
+                                lldb_private::dil::Token::eof,
+                                lldb_private::dil::Token::kw_namespace));
+    }
   }
 }
