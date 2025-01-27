5bb5b23b5ff5447cef9c0e28326f1967db7c502a
Christian Clauss <cclauss@me.com>
2025-01-27 23:55:11 +0100

    ruff format

--- polly/utils/pyscop/jscop2iscc.py
+++ polly/utils/pyscop/jscop2iscc.py
@@ -4,67 +4,66 @@ from __future__ import print_function
 import argparse, isl, os
 import json
 
+
 def printDomain(scop):
+    domain = isl.USet("{}")
 
-  domain = isl.USet('{}')
+    for statement in scop["statements"]:
+        domain = domain.union(isl.USet(statement["domain"]))
 
-  for statement in scop['statements']:
-    domain = domain.union(isl.USet(statement['domain']))
+    print("D :=", end=" ")
+    print(str(domain) + ";")
 
-  print("D :=", end=" ")
-  print(str(domain) + ";")
 
 def printAccesses(scop):
+    read = isl.UMap("{}")
 
-  read = isl.UMap('{}')
+    for statement in scop["statements"]:
+        for access in statement["accesses"]:
+            if access["kind"] == "read":
+                read = read.union(isl.UMap(access["relation"]))
 
-  for statement in scop['statements']:
-    for access in statement['accesses']:
-      if access['kind'] == 'read':
-        read = read.union(isl.UMap(access['relation']))
+    print("R :=", end=" ")
+    print(str(read) + ";")
 
-  print("R :=", end=" ")
-  print(str(read) + ";")
+    write = isl.UMap("{}")
 
-  write = isl.UMap('{}')
+    for statement in scop["statements"]:
+        for access in statement["accesses"]:
+            if access["kind"] == "write":
+                write = write.union(isl.UMap(access["relation"]))
 
-  for statement in scop['statements']:
-    for access in statement['accesses']:
-      if access['kind'] == 'write':
-        write = write.union(isl.UMap(access['relation']))
+    print("W :=", end=" ")
+    print(str(write) + ";")
 
-  print("W :=", end=" ")
-  print(str(write) + ";")
 
 def printSchedule(scop):
+    schedule = isl.UMap("{}")
 
-  schedule = isl.UMap('{}')
+    for statement in scop["statements"]:
+        schedule = schedule.union(isl.UMap(statement["schedule"]))
 
-  for statement in scop['statements']:
-    schedule = schedule.union(isl.UMap(statement['schedule']))
+    print("S :=", end=" ")
+    print(str(schedule) + ";")
 
-  print("S :=", end=" ")
-  print(str(schedule) + ";")
 
 def __main__():
-  description = 'Translate JSCoP into iscc input'
-  parser = argparse.ArgumentParser(description)
-  parser.add_argument('inputFile', metavar='N', type=file,
-                      help='The JSCoP file')
+    description = "Translate JSCoP into iscc input"
+    parser = argparse.ArgumentParser(description)
+    parser.add_argument("inputFile", metavar="N", type=file, help="The JSCoP file")
 
-  args = parser.parse_args()
-  inputFile = args.inputFile
-  scop = json.load(inputFile)
+    args = parser.parse_args()
+    inputFile = args.inputFile
+    scop = json.load(inputFile)
 
-  printDomain(scop)
-  printAccesses(scop)
-  printSchedule(scop)
+    printDomain(scop)
+    printAccesses(scop)
+    printSchedule(scop)
 
-  print('R := R * D;')
-  print('W := W * D;')
-  print('Dep := (last W before R under S)[0];')
-  print('schedule D respecting Dep minimizing Dep;')
+    print("R := R * D;")
+    print("W := W * D;")
+    print("Dep := (last W before R under S)[0];")
+    print("schedule D respecting Dep minimizing Dep;")
 
 
 __main__()
-
