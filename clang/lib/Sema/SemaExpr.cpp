c70af0633883f00d783fd3ea2d3f029afbabe0e8
cor3ntin <corentinjabot@gmail.com>
2025-01-27 23:51:19 +0100

    Revert "[Clang] call HandleImmediateInvocation before checking for immediate escacalating expressions" (#124646)
    Reverts llvm/llvm-project#124414
    
    Turns out to be an important compile time regression, I'll come up with
    a less disruptive approach

--- clang/lib/Sema/SemaExpr.cpp
+++ clang/lib/Sema/SemaExpr.cpp
@@ -17881,9 +17881,6 @@ void Sema::PopExpressionEvaluationContext() {
   WarnOnPendingNoDerefs(Rec);
   HandleImmediateInvocations(*this, Rec);
 
-  if (auto *FD = dyn_cast<FunctionDecl>(CurContext); FD && getCurFunction())
-    CheckImmediateEscalatingFunctionDefinition(FD, getCurFunction());
-
   // Warn on any volatile-qualified simple-assignments that are not discarded-
   // value expressions nor unevaluated operands (those cases get removed from
   // this list by CheckUnusedVolatileAssignment).
