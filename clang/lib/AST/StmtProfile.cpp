2572213c51081f6f181194416f73ee7b06ec0375
Younan Zhang <zyn7109@gmail.com>
2025-01-27 23:04:57 +0800

    [Clang] Remove unnecessary Decl transform & profiles for SizeOfPackExpr (#124533)
    We used to always transform the pattern declaration for SizeOfPackExpr
    to ensure the constraint expression's profile produced the desired
    result. However, this approach failed to handle pack expansions when the
    pack referred to function parameters. In such cases, the function
    parameters were formerly expanded to 1 to avoid building Subst* nodes
    (see e6974daa7). That workaround caused us to transform a pack without a
    proper ArgumentPackSubstitutionIndex, leading to crashes when
    transforming the pattern.
    
    It turns out that profiling the pattern for partially substituted
    SizeOfPackExprs is unnecessary because their transformed forms are also
    profiled within the partial arguments.
    
    Fixes https://github.com/llvm/llvm-project/issues/124161

--- clang/lib/AST/StmtProfile.cpp
+++ clang/lib/AST/StmtProfile.cpp
@@ -2270,13 +2270,13 @@ void StmtProfiler::VisitPackExpansionExpr(const PackExpansionExpr *S) {
 
 void StmtProfiler::VisitSizeOfPackExpr(const SizeOfPackExpr *S) {
   VisitExpr(S);
-  VisitDecl(S->getPack());
   if (S->isPartiallySubstituted()) {
     auto Args = S->getPartialArguments();
     ID.AddInteger(Args.size());
     for (const auto &TA : Args)
       VisitTemplateArgument(TA);
   } else {
+    VisitDecl(S->getPack());
     ID.AddInteger(0);
   }
 }
