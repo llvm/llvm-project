dc8be6635f881f355ec58d8124158c8ef9380b33
Nikita Popov <npopov@redhat.com>
2025-01-27 09:20:23 +0100

    Add quotes so -fwrapv can't match an -fwrapv-pointer prefix

--- clang/test/Driver/clang_wrapv_opts.c
+++ clang/test/Driver/clang_wrapv_opts.c
@@ -1,20 +1,20 @@
 // RUN: %clang -### -S -fwrapv -fno-wrapv -fwrapv %s 2>&1 | FileCheck -check-prefix=CHECK1 %s
-// CHECK1: -fwrapv
+// CHECK1: "-fwrapv"
 //
 // RUN: %clang -### -S -fwrapv-pointer -fno-wrapv-pointer -fwrapv-pointer %s 2>&1 | FileCheck -check-prefix=CHECK1-POINTER %s
-// CHECK1-POINTER: -fwrapv-pointer
+// CHECK1-POINTER: "-fwrapv-pointer"
 //
 // RUN: %clang -### -S -fstrict-overflow -fno-strict-overflow %s 2>&1 | FileCheck -check-prefix=CHECK2 %s
-// CHECK2: -fwrapv{{.*}}-fwrapv-pointer
+// CHECK2: "-fwrapv"{{.*}}"-fwrapv-pointer"
 //
 // RUN: %clang -### -S -fwrapv -fstrict-overflow %s 2>&1 | FileCheck -check-prefix=CHECK3 %s --implicit-check-not="-fwrapv-pointer"
-// CHECK3: -fwrapv
+// CHECK3: "-fwrapv"
 //
 // RUN: %clang -### -S -fwrapv-pointer -fstrict-overflow %s 2>&1 | FileCheck -check-prefix=CHECK3-POINTER %s --implicit-check-not="-fwrapv"
-// CHECK3-POINTER: -fwrapv-pointer
+// CHECK3-POINTER: "-fwrapv-pointer"
 //
 // RUN: %clang -### -S -fno-wrapv -fno-strict-overflow %s 2>&1 | FileCheck -check-prefix=CHECK4 %s --implicit-check-not="-fwrapv"
-// CHECK4: -fwrapv-pointer
+// CHECK4: "-fwrapv-pointer"
 //
 // RUN: %clang -### -S -fno-wrapv-pointer -fno-strict-overflow %s 2>&1 | FileCheck -check-prefix=CHECK4-POINTER %s --implicit-check-not="-fwrapv-pointer"
-// CHECK4-POINTER: -fwrapv
+// CHECK4-POINTER: "-fwrapv"
