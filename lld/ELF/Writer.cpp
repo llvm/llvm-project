4c91089ba78f137cfec20ae5b34dde9f9ec8501b
Jorge Gorbe Moya <jgorbe@google.com>
2025-01-27 15:15:24 -0800

    Revert "[ELF] Remove redundant isExported computation"
    This reverts commit 1a4d6de1b532149b10522eae5dabce39e5f7c687.
    It causes problems with `-static-pie`. See repro at
    https://github.com/llvm/llvm-project/commit/1a4d6de1b532149b10522eae5dabce39e5f7c687#commitcomment-151827775

--- lld/ELF/Writer.cpp
+++ lld/ELF/Writer.cpp
@@ -149,7 +149,6 @@ static Defined *addOptionalRegular(Ctx &ctx, StringRef name, SectionBase *sec,
   if (!s || s->isDefined() || s->isCommon())
     return nullptr;
 
-  ctx.synthesizedSymbols.push_back(s);
   s->resolve(ctx, Defined{ctx, ctx.internalFile, StringRef(), STB_GLOBAL,
                           stOther, STT_NOTYPE, val,
                           /*size=*/0, sec});
@@ -283,7 +282,6 @@ static void demoteDefined(Defined &sym, DenseMap<SectionBase *, size_t> &map) {
 static void demoteSymbolsAndComputeIsPreemptible(Ctx &ctx) {
   llvm::TimeTraceScope timeScope("Demote symbols");
   DenseMap<InputFile *, DenseMap<SectionBase *, size_t>> sectionIndexMap;
-  bool hasDynSymTab = ctx.arg.hasDynSymTab;
   for (Symbol *sym : ctx.symtab->getSymbols()) {
     if (auto *d = dyn_cast<Defined>(sym)) {
       if (d->section && !d->section->isLive())
@@ -296,12 +294,11 @@ static void demoteSymbolsAndComputeIsPreemptible(Ctx &ctx) {
                   sym->type)
             .overwrite(*sym);
         sym->versionId = VER_NDX_GLOBAL;
-        if (sym->includeInDynsym(ctx))
-          sym->isExported = true;
       }
     }
 
-    if (hasDynSymTab)
+    sym->isExported = sym->includeInDynsym(ctx);
+    if (ctx.arg.hasDynSymTab)
       sym->isPreemptible = sym->isExported && computeIsPreemptible(ctx, *sym);
   }
 }
@@ -1838,11 +1835,6 @@ template <class ELFT> void Writer<ELFT>::finalizeSections() {
         finalizeSynthetic(ctx, part.ehFrame.get());
     }
   }
-
-  // If the previous code block defines any non-hidden symbols (e.g.
-  // __global_pointer$), they may be exported.
-  for (Symbol *sym : ctx.synthesizedSymbols)
-    sym->isExported = sym->includeInDynsym(ctx);
 
   demoteSymbolsAndComputeIsPreemptible(ctx);
 
