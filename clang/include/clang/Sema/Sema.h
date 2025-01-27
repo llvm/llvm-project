fd787533e6efbeae8448defce1a833e33c6c528a
cor3ntin <corentinjabot@gmail.com>
2025-01-27 22:21:48 +0100

    [Clang] call HandleImmediateInvocation before checking for immediate escacalating expressions (#124414)
    HandleImmediateInvocation can call MarkExpressionAsImmediateEscalating
    and should always be called before
    CheckImmediateEscalatingFunctionDefinition.
    
    However, we were not doing that in `ActFunctionBody`.
    
    We simply move CheckImmediateEscalatingFunctionDefinition to
    PopExpressionEvaluationContext.
    
    Fixes #119046

--- clang/include/clang/Sema/Sema.h
+++ clang/include/clang/Sema/Sema.h
@@ -13139,10 +13139,10 @@ public:
     ~SynthesizedFunctionScope() {
       if (PushedCodeSynthesisContext)
         S.popCodeSynthesisContext();
-      if (auto *FD = dyn_cast<FunctionDecl>(S.CurContext)) {
+
+      if (auto *FD = dyn_cast<FunctionDecl>(S.CurContext))
         FD->setWillHaveBody(false);
-        S.CheckImmediateEscalatingFunctionDefinition(FD, S.getCurFunction());
-      }
+
       S.PopExpressionEvaluationContext();
       S.PopFunctionScopeInfo();
     }
