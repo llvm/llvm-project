e9e90be46e929973314b56d164cefadbade64474
Stephen Tozer <stephen.tozer@sony.com>
2025-01-28 18:25:32 +0000

    [Clang] Cleanup docs and comments relating to -fextend-variable-liveness (#124767)
    This patch contains a number of changes relating to the above flag;
    primarily it updates comment references to the old flag names,
    "-fextend-lifetimes" and "-fextend-this-ptr" to refer to the new names,
    "-fextend-variable-liveness[={all,this}]". These changes are all NFC.
    
    This patch also removes the explicit -fextend-this-ptr-liveness flag
    alias, and shortens the help-text for the main flag; these are both
    changes that were meant to be applied in the initial PR (#110000), but
    due to some user-error on my part they were not included in the merged
    commit.

--- clang/test/Driver/extend-variable-liveness.c
+++ clang/test/Driver/extend-variable-liveness.c
@@ -4,7 +4,6 @@
 // RUN: %clang -### -c %s 2>&1 | FileCheck %s --check-prefixes=CHECK,DEFAULT
 // RUN: %clang -fextend-variable-liveness=none -### -c %s 2>&1 | FileCheck %s --check-prefixes=CHECK,NONE
 // RUN: %clang -fextend-variable-liveness=this -### -c %s 2>&1 | FileCheck %s --check-prefixes=CHECK,THIS
-// RUN: %clang -fextend-this-ptr-liveness -### -c %s 2>&1 | FileCheck %s --check-prefixes=CHECK,THIS
 // RUN: %clang -fextend-variable-liveness=all -### -c %s 2>&1 | FileCheck %s --check-prefixes=CHECK,ALL
 // RUN: %clang -fextend-variable-liveness -### -c %s 2>&1 | FileCheck %s --check-prefixes=CHECK,ALL
 
