a3ad6bbc302b6a218db537f1ff8395378db47110
Venkata Ramanaiah Nalamothu <quic_vnalamot@quicinc.com>
2025-01-28 21:03:12 +0530

    [RISC-V] Fix incorrect epilogue_begin setting in debug line table (#120623)
    The DwarfDebug.cpp implementation expects the epilogue instructions to
    have source location of last non-debug instruction after which the epilogue
    instructions are inserted. The epilogue_begin is set on location of the first
    FrameDestroy instruction with source line information that has been seen in
    the epilogue basic block.
    
    In the trunk, the risc-v backend sets the epilogue_begin after the epilogue has
    actually begun i.e. after callee saved register reloads and the source line
    information is not set on those reload instructions. This is leading to #120553
    where, while debugging, breaking on or single stepping to the epilogue_begin
    location will make accessing the variables from wrong place as the FP has been
    restored to the parent frame's FP.
    
    To fix that, this patch sets FrameSetup/FrameDestroy flags on the callee saved
    register spill/reload instructions which is actually correct. Then the
    RISCVInstrInfo::loadRegFromStackSlot uses FrameDestroy flag to identify a
    reload of the callee saved register in the epilogue and copies the source
    line information from insert position instruction to that reload instruction.
    
    Requires PR #120622
    
    Fixes #120553

--- llvm/lib/Target/RISCV/RISCVInstrInfo.cpp
+++ llvm/lib/Target/RISCV/RISCVInstrInfo.cpp
@@ -650,7 +650,8 @@ void RISCVInstrInfo::storeRegToStackSlot(MachineBasicBlock &MBB,
     BuildMI(MBB, I, DebugLoc(), get(Opcode))
         .addReg(SrcReg, getKillRegState(IsKill))
         .addFrameIndex(FI)
-        .addMemOperand(MMO);
+        .addMemOperand(MMO)
+        .setMIFlag(Flags);
   } else {
     MachineMemOperand *MMO = MF->getMachineMemOperand(
         MachinePointerInfo::getFixedStack(*MF, FI), MachineMemOperand::MOStore,
@@ -660,7 +661,8 @@ void RISCVInstrInfo::storeRegToStackSlot(MachineBasicBlock &MBB,
         .addReg(SrcReg, getKillRegState(IsKill))
         .addFrameIndex(FI)
         .addImm(0)
-        .addMemOperand(MMO);
+        .addMemOperand(MMO)
+        .setMIFlag(Flags);
   }
 }
 
@@ -670,6 +672,8 @@ void RISCVInstrInfo::loadRegFromStackSlot(
     Register VReg, MachineInstr::MIFlag Flags) const {
   MachineFunction *MF = MBB.getParent();
   MachineFrameInfo &MFI = MF->getFrameInfo();
+  DebugLoc DL =
+      Flags & MachineInstr::FrameDestroy ? MBB.findDebugLoc(I) : DebugLoc();
 
   unsigned Opcode;
   bool IsScalableVector = true;
@@ -734,18 +738,20 @@ void RISCVInstrInfo::loadRegFromStackSlot(
         LocationSize::beforeOrAfterPointer(), MFI.getObjectAlign(FI));
 
     MFI.setStackID(FI, TargetStackID::ScalableVector);
-    BuildMI(MBB, I, DebugLoc(), get(Opcode), DstReg)
+    BuildMI(MBB, I, DL, get(Opcode), DstReg)
         .addFrameIndex(FI)
-        .addMemOperand(MMO);
+        .addMemOperand(MMO)
+        .setMIFlag(Flags);
   } else {
     MachineMemOperand *MMO = MF->getMachineMemOperand(
         MachinePointerInfo::getFixedStack(*MF, FI), MachineMemOperand::MOLoad,
         MFI.getObjectSize(FI), MFI.getObjectAlign(FI));
 
-    BuildMI(MBB, I, DebugLoc(), get(Opcode), DstReg)
+    BuildMI(MBB, I, DL, get(Opcode), DstReg)
         .addFrameIndex(FI)
         .addImm(0)
-        .addMemOperand(MMO);
+        .addMemOperand(MMO)
+        .setMIFlag(Flags);
   }
 }
 
