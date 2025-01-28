ae0c69879de80f3728941a16f7e27e5073d204d5
Pengying Xu <xpy66swsry@gmail.com>
2025-01-28 15:47:33 +0800

    rewrite BPSectionOrder in ELF following #124482

--- lld/ELF/BPSectionOrderer.cpp
+++ lld/ELF/BPSectionOrderer.cpp
@@ -1,4 +1,4 @@
-//===- BPSectionOrderer.cpp------------------------------------------------===//
+//===- BPSectionOrderer.cpp -----------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -7,36 +7,82 @@
 //===----------------------------------------------------------------------===//
 
 #include "BPSectionOrderer.h"
+#include "InputFiles.h"
+#include "InputSection.h"
+#include "SymbolTable.h"
+#include "Symbols.h"
+#include "lld/Common/BPSectionOrdererBase.inc"
+#include "llvm/Support/Endian.h"
+
+#define DEBUG_TYPE "bp-section-orderer"
 
 using namespace llvm;
 using namespace lld::elf;
 
-void BPSectionELF::getSectionHashes(
-    llvm::SmallVectorImpl<uint64_t> &hashes,
-    const llvm::DenseMap<const void *, uint64_t> &sectionToIdx) const {
-  constexpr unsigned windowSize = 4;
-
-  // Calculate content hashes: k-mers and the last k-1 bytes.
-  ArrayRef<uint8_t> data = isec->content();
-  if (data.size() >= windowSize)
-    for (size_t i = 0; i <= data.size() - windowSize; ++i)
-      hashes.push_back(llvm::support::endian::read32le(data.data() + i));
-  for (uint8_t byte : data.take_back(windowSize - 1))
-    hashes.push_back(byte);
-
-  llvm::sort(hashes);
-  hashes.erase(std::unique(hashes.begin(), hashes.end()), hashes.end());
+namespace {
+struct BPOrdererELF;
 }
+template <> struct lld::BPOrdererTraits<struct BPOrdererELF> {
+  using Section = elf::InputSectionBase;
+  using Symbol = elf::Symbol;
+};
+namespace {
+struct BPOrdererELF : lld::BPOrderer<BPOrdererELF> {
+  static uint64_t getSize(const Section &sec) { return sec.getSize(); }
+  static bool isCodeSection(const Section &sec) {
+    return sec.flags & llvm::ELF::SHF_EXECINSTR;
+  }
+  static SmallVector<Symbol *, 0> getSymbols(const Section &sec) {
+    SmallVector<Symbol *, 0> symbols;
+    for (auto *sym : sec.file->getSymbols())
+      if (auto *d = llvm::dyn_cast_or_null<Defined>(sym))
+        if (d->size > 0 && d->section == &sec)
+          symbols.emplace_back(d);
+    return symbols;
+  }
 
-llvm::DenseMap<const lld::elf::InputSectionBase *, int>
-lld::elf::runBalancedPartitioning(Ctx &ctx, llvm::StringRef profilePath,
-                                  bool forFunctionCompression,
-                                  bool forDataCompression,
-                                  bool compressionSortStartupFunctions,
-                                  bool verbose) {
-  // Collect sections from symbols and wrap as BPSectionELF instances.
-  // Deduplicates sections referenced by multiple symbols.
-  SmallVector<std::unique_ptr<BPSectionBase>> sections;
+  std::optional<StringRef> static getResolvedLinkageName(llvm::StringRef name) {
+    return name;
+  }
+
+  static void
+  getSectionHashes(const Section &sec, llvm::SmallVectorImpl<uint64_t> &hashes,
+                   const llvm::DenseMap<const void *, uint64_t> &sectionToIdx) {
+    constexpr unsigned windowSize = 4;
+
+    // Calculate content hashes: k-mers and the last k-1 bytes.
+    ArrayRef<uint8_t> data = sec.content();
+    if (data.size() >= windowSize)
+      for (size_t i = 0; i <= data.size() - windowSize; ++i)
+        hashes.push_back(llvm::support::endian::read32le(data.data() + i));
+    for (uint8_t byte : data.take_back(windowSize - 1))
+      hashes.push_back(byte);
+
+    llvm::sort(hashes);
+    hashes.erase(std::unique(hashes.begin(), hashes.end()), hashes.end());
+  }
+
+  static llvm::StringRef getSymName(const Symbol &sym) { return sym.getName(); }
+  static uint64_t getSymValue(const Symbol &sym) {
+    if (auto *d = dyn_cast<Defined>(&sym))
+      return d->value;
+    return 0;
+  }
+  static uint64_t getSymSize(const Symbol &sym) {
+    if (auto *d = dyn_cast<Defined>(&sym))
+      return d->size;
+    return 0;
+  }
+};
+} // namespace
+
+DenseMap<const InputSectionBase *, int> lld::elf::runBalancedPartitioning(
+    Ctx &ctx, StringRef profilePath, bool forFunctionCompression,
+    bool forDataCompression, bool compressionSortStartupFunctions,
+    bool verbose) {
+  // Collect candidate sections and associated symbols.
+  SmallVector<InputSectionBase *> sections;
+  DenseMap<CachedHashStringRef, DenseSet<unsigned>> rootSymbolToSectionIdxs;
   DenseSet<const InputSectionBase *> seenSections;
 
   auto addSection = [&](Symbol &sym) {
@@ -44,8 +90,15 @@ lld::elf::runBalancedPartitioning(Ctx &ctx, llvm::StringRef profilePath,
       return;
     if (auto *d = dyn_cast<Defined>(&sym))
       if (auto *sec = dyn_cast_or_null<InputSectionBase>(d->section))
-        if (seenSections.insert(sec).second)
-          sections.emplace_back(std::make_unique<BPSectionELF>(sec));
+        if (seenSections.insert(sec).second) {
+          size_t idx = sections.size();
+          sections.emplace_back(sec);
+          auto rootName = getRootSymbol(sym.getName());
+          rootSymbolToSectionIdxs[CachedHashStringRef(rootName)].insert(idx);
+          if (auto linkageName = BPOrdererELF::getResolvedLinkageName(rootName))
+            rootSymbolToSectionIdxs[CachedHashStringRef(*linkageName)].insert(
+                idx);
+        }
   };
 
   for (Symbol *sym : ctx.symtab->getSymbols())
@@ -55,16 +108,8 @@ lld::elf::runBalancedPartitioning(Ctx &ctx, llvm::StringRef profilePath,
     for (Symbol *sym : file->getLocalSymbols())
       addSection(*sym);
 
-  auto reorderedSections = BPSectionBase::reorderSectionsByBalancedPartitioning(
-      profilePath, forFunctionCompression, forDataCompression,
-      compressionSortStartupFunctions, verbose, sections);
-
-  DenseMap<const InputSectionBase *, int> result;
-  for (const auto [sec, priority] : reorderedSections) {
-    auto *elfSection = cast<BPSectionELF>(sec);
-    result.try_emplace(
-        static_cast<const InputSectionBase *>(elfSection->getSection()),
-        static_cast<int>(priority));
-  }
-  return result;
+  return BPOrdererELF::computeOrder(profilePath, forFunctionCompression,
+                                    forDataCompression,
+                                    compressionSortStartupFunctions, verbose,
+                                    sections, rootSymbolToSectionIdxs);
 }
--- lld/ELF/BPSectionOrderer.h
+++ lld/ELF/BPSectionOrderer.h
@@ -14,96 +14,23 @@
 #ifndef LLD_ELF_BPSECTION_ORDERER_H
 #define LLD_ELF_BPSECTION_ORDERER_H
 
-#include "InputFiles.h"
-#include "InputSection.h"
-#include "SymbolTable.h"
-#include "lld/Common/BPSectionOrdererBase.h"
 #include "llvm/ADT/DenseMap.h"
-#include "llvm/ADT/STLExtras.h"
 #include "llvm/ADT/StringRef.h"
-#include "llvm/BinaryFormat/ELF.h"
 
 namespace lld::elf {
-
-class InputSection;
-
-class BPSymbolELF : public BPSymbol {
-  const Symbol *sym;
-
-public:
-  explicit BPSymbolELF(const Symbol *s) : sym(s) {}
-
-  llvm::StringRef getName() const override { return sym->getName(); }
-
-  const Defined *asDefined() const { return llvm::dyn_cast<Defined>(sym); }
-
-  std::optional<uint64_t> getValue() const override {
-    if (auto *d = asDefined())
-      return d->value;
-    return {};
-  }
-
-  std::optional<uint64_t> getSize() const override {
-    if (auto *d = asDefined())
-      return d->size;
-    return {};
-  }
-
-  InputSectionBase *getInputSection() const {
-    if (auto *d = asDefined())
-      return llvm::dyn_cast_or_null<InputSectionBase>(d->section);
-    return nullptr;
-  }
-
-  const Symbol *getSymbol() const { return sym; }
-};
-
-class BPSectionELF : public BPSectionBase {
-  const InputSectionBase *isec;
-
-public:
-  explicit BPSectionELF(const InputSectionBase *sec) : isec(sec) {}
-
-  const void *getSection() const override { return isec; }
-
-  uint64_t getSize() const override { return isec->getSize(); }
-
-  bool isCodeSection() const override {
-    return isec->flags & llvm::ELF::SHF_EXECINSTR;
-  }
-
-  SmallVector<std::unique_ptr<BPSymbol>> getSymbols() const override {
-    SmallVector<std::unique_ptr<BPSymbol>> symbols;
-    for (Symbol *sym : isec->file->getSymbols())
-      if (auto *d = dyn_cast<Defined>(sym))
-        if (d->size > 0 && d->section == isec)
-          symbols.emplace_back(std::make_unique<BPSymbolELF>(sym));
-
-    return symbols;
-  }
-
-  std::optional<StringRef>
-  getResolvedLinkageName(llvm::StringRef name) const override {
-    return {};
-  }
-
-  void getSectionHashes(llvm::SmallVectorImpl<uint64_t> &hashes,
-                        const llvm::DenseMap<const void *, uint64_t>
-                            &sectionToIdx) const override;
-
-  static bool classof(const BPSectionBase *s) { return true; }
-};
+struct Ctx;
+class InputSectionBase;
 
 /// Run Balanced Partitioning to find the optimal function and data order to
 /// improve startup time and compressed size.
 ///
-/// It is important that -ffunction-sections and -fdata-sections are used to
-/// ensure functions and data are in their own sections and thus can be
-/// reordered.
+/// It is important that .subsections_via_symbols is used to ensure functions
+/// and data are in their own sections and thus can be reordered.
 llvm::DenseMap<const InputSectionBase *, int>
 runBalancedPartitioning(Ctx &ctx, llvm::StringRef profilePath,
                         bool forFunctionCompression, bool forDataCompression,
                         bool compressionSortStartupFunctions, bool verbose);
+
 } // namespace lld::elf
 
 #endif
