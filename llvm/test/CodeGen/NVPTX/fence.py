01f69b4e43ed5426a0cc3e633b9eec5a1e6ac647
Akshay Deodhar <adeodhar@nvidia.com>
2025-01-29 21:14:21 +0000

    have checks for different archs together

--- llvm/test/CodeGen/NVPTX/fence.py
+++ llvm/test/CodeGen/NVPTX/fence.py
@@ -14,40 +14,43 @@ define void @fence_${ordering}_${ptx_scope}() {
 )
 
 run_statement = Template(
-    """
-; RUN: llc < %s -march=nvptx64 -mcpu=sm_${sm} -mattr=+ptx${ptx} | FileCheck %s --check-prefix=SM${sm}
-; RUN: %if ptxas %{ llc < %s -march=nvptx -mcpu=sm_${sm} -mattr=+ptx${ptx} | %ptxas-verify %}
-"""
+    """; RUN: llc < %s -march=nvptx64 -mcpu=sm_${sm} -mattr=+ptx${ptx} | FileCheck %s --check-prefix=SM${sm}
+; RUN: %if ptxas %{ llc < %s -march=nvptx -mcpu=sm_${sm} -mattr=+ptx${ptx} | %ptxas-verify %}"""
 )
 
 # (sm, ptx)
 TESTS = [(30, 50), (70, 60), (90, 87)]
 
-LLVM_SCOPES = ["", "block", "cluster", "device"]
+LLVM_SCOPES_NO_CLUSTER = ["", "block", "device"]
 
 SCOPE_LLVM_TO_PTX = {"": "sys", "block": "cta", "cluster": "cluster", "device": "gpu"}
 
 ORDERINGS = ["acquire", "release", "acq_rel", "seq_cst"]
 
 if __name__ == "__main__":
-    for sm, ptx in TESTS:
-        with open("fence-sm{}.ll".format(sm), "w") as fp:
-            print(run_statement.substitute(run="RUN", sm=sm, ptx=ptx), file=fp)
-            for ordering, llvm_scope in product(ORDERINGS, LLVM_SCOPES):
-                ptx_scope = SCOPE_LLVM_TO_PTX[llvm_scope]
-                if llvm_scope == "cluster" and (sm < 90 or ptx < 78):
-                    print(
-                        "; .cluster scope unsupported on SM = {} PTX = {}".format(
-                            sm, ptx
-                        ),
-                        file=fp,
-                    )
-                else:
-                    print(
-                        fence_func.substitute(
-                            llvm_scope=llvm_scope,
-                            ptx_scope=ptx_scope,
-                            ordering=ordering,
-                        ),
-                        file=fp,
-                    )
+    # non-cluster orderings are supported on SM30, SM70 and SM90
+    with open("fence-nocluster.ll", "w") as fp:
+        for sm, ptx in TESTS:
+            print(run_statement.substitute(sm=sm, ptx=ptx), file=fp)
+        for ordering, llvm_scope in product(ORDERINGS, LLVM_SCOPES_NO_CLUSTER):
+            print(
+                fence_func.substitute(
+                    llvm_scope=llvm_scope,
+                    ptx_scope=SCOPE_LLVM_TO_PTX[llvm_scope],
+                    ordering=ordering,
+                ),
+                file=fp,
+            )
+
+    # cluster ordering only supported on SM90
+    with open("fence-cluster.ll", "w") as fp:
+        print(run_statement.substitute(sm=90, ptx=87), file=fp)
+        for ordering in ORDERINGS:
+            print(
+                fence_func.substitute(
+                    llvm_scope="cluster",
+                    ptx_scope=SCOPE_LLVM_TO_PTX["cluster"],
+                    ordering=ordering,
+                ),
+                file=fp,
+            )
