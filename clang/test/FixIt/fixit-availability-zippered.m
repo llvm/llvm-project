d63603839788d63e6d25922e48a0c2961fee7ece
Ian Anderson <iana@apple.com>
2025-01-28 17:33:57 -0800

    [test][Clang][Sema] Fix the zippered availability test
    We never fixed the internal-only availability tests after <rdar://134475028>. Do that now.
    
    rdar://142437215

--- clang/test/FixIt/fixit-availability-zippered.m
+++ clang/test/FixIt/fixit-availability-zippered.m
@@ -1,6 +1,5 @@
 // RUN: %clang_cc1 -ftarget-variant-availability-checks -fsyntax-only -Wunguarded-availability -fdiagnostics-parseable-fixits -triple x86_64-apple-macos10.14 -darwin-target-variant-triple x86_64-apple-ios12-macabi %s 2>&1 | FileCheck %s
 
-// XFAIL: *
 __attribute__((availability(macOS, introduced=10.15))) __attribute__((availability(ios, introduced=13)))
 int function(void);
 
@@ -10,7 +9,10 @@ int use() {
 // CHECK-NEXT: fix-it:{{.*}}:{[[@LINE-2]]:14-[[@LINE-2]]:14}:"\n  } else {\n      // Fallback on earlier versions\n  }"
 }
 
-#define API_AVAILABLE(...) __attribute__((availability(macOS, introduced=10.15))) __attribute__((availability(ios, introduced=13))) // dummy macro
+#define API_AVAILABLE(x, y) __attribute__((availability(__API_AVAILABLE_PLATFORM_##x))) __attribute__((availability(__API_AVAILABLE_PLATFORM_##y)))
+#define __API_AVAILABLE_PLATFORM_macos(x) macos,introduced=x
+#define __API_AVAILABLE_PLATFORM_ios(x) ios,introduced=x
+#define __API_AVAILABLE_PLATFORM_macCatalyst(x) macCatalyst,introduced=x
 
 API_AVAILABLE(macos(10.15), ios(13))
 @interface NewClass
@@ -18,6 +20,6 @@ API_AVAILABLE(macos(10.15), ios(13))
 
 @interface OldButOfferFixit
 @property(copy) NewClass *prop;
-// CHECK: fix-it:{{.*}}:{[[@LINE-2]]:1-[[@LINE-2]]:1}:"API_AVAILABLE(macos(10.15), maccatalyst(13))\n"
+// CHECK: fix-it:{{.*}}:{[[@LINE-2]]:1-[[@LINE-2]]:1}:"API_AVAILABLE(macos(10.15), macCatalyst(13))\n"
 
 @end
