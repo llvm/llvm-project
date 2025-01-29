llvm/monorepo-prototype

[libclc] Move several integer functions to CLC library
This commit moves over the OpenCL clz, hadd, mad24, mad_hi, mul24,
mul_hi, popcount, rhadd, and upsample builtins to the CLC library. There
are no changes to any target's CLC libraries.

The OpenCL mad_hi builtin wasn't previously publicly available from the
CLC libraries, as it was hash-defined to mul_hi in the header files.
That issue has been fixed, and mad_hi is now exposed.

The custom AMD implementation/workaround for popcount has been removed
as it was only valid for clang < 7.

There are still two integer functions which haven't been moved over. The
OpenCL add_sat, sub_sat, and mad_sat builtins require saturating
conversion builtins which haven't yet been ported.

libclc/generic/include/clc/integer/hadd.h
@@ -1,2 +1,6 @@
-#define __CLC_BODY <clc/integer/hadd.inc>
+#define FUNCTION hadd
+#define __CLC_BODY "binary_decl.h"
+
 #include <clc/integer/gentype.inc>
+
+#undef FUNCTION

llvm/llvm-project

libclc/generic/include/clc/integer/hadd.h
@@ -1,2 +1,7 @@
-#define __CLC_BODY <clc/integer/hadd.inc>
+#define __CLC_FUNCTION hadd
+#define __CLC_BODY <clc/shared/binary_decl.inc>
+
 #include <clc/integer/gentype.inc>
+
+#undef __CLC_BODY
+#undef __CLC_FUNCTION