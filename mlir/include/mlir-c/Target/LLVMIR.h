345512cbda296d262a28459afc7f83640b06b6a8
Maksim Levental <maksim.levental@gmail.com>
2025-01-30 14:09:14 -0500

    Revert "[mlir][llvmir] expose Type(To/From)LLVMIRTranslator C API  (#124864)"
    This reverts commit 7ae964c55b0a2b8dccf076ced9f1ffa8d5487eca.
    
    Revert "[mlir] Fix warnings"
    
    This reverts commit 0e43b9547d56762d29de9a99c998e7939417a1f3.

--- mlir/include/mlir-c/Target/LLVMIR.h
+++ mlir/include/mlir-c/Target/LLVMIR.h
@@ -16,7 +16,6 @@
 
 #include "mlir-c/IR.h"
 #include "mlir-c/Support.h"
-#include "llvm-c/Core.h"
 #include "llvm-c/Support.h"
 
 #ifdef __cplusplus
@@ -32,48 +31,6 @@ extern "C" {
 /// owned by the caller.
 MLIR_CAPI_EXPORTED LLVMModuleRef
 mlirTranslateModuleToLLVMIR(MlirOperation module, LLVMContextRef context);
-
-struct MlirTypeFromLLVMIRTranslator {
-  void *ptr;
-};
-
-typedef struct MlirTypeFromLLVMIRTranslator MlirTypeFromLLVMIRTranslator;
-
-/// Create an LLVM::TypeFromLLVMIRTranslator and transfer ownership to the
-/// caller.
-MLIR_CAPI_EXPORTED MlirTypeFromLLVMIRTranslator
-mlirTypeFromLLVMIRTranslatorCreate(MlirContext ctx);
-
-/// Takes an LLVM::TypeFromLLVMIRTranslator owned by the caller and destroys it.
-/// It is the responsibility of the user to only pass an
-/// LLVM::TypeFromLLVMIRTranslator class.
-MLIR_CAPI_EXPORTED void
-mlirTypeFromLLVMIRTranslatorDestroy(MlirTypeFromLLVMIRTranslator translator);
-
-/// Translates the given LLVM IR type to the MLIR LLVM dialect.
-MLIR_CAPI_EXPORTED MlirType mlirTypeFromLLVMIRTranslatorTranslateType(
-    MlirTypeFromLLVMIRTranslator translator, LLVMTypeRef llvmType);
-
-struct MlirTypeToLLVMIRTranslator {
-  void *ptr;
-};
-
-typedef struct MlirTypeToLLVMIRTranslator MlirTypeToLLVMIRTranslator;
-
-/// Create an LLVM::TypeToLLVMIRTranslator and transfer ownership to the
-/// caller.
-MLIR_CAPI_EXPORTED MlirTypeToLLVMIRTranslator
-mlirTypeToLLVMIRTranslatorCreate(LLVMContextRef ctx);
-
-/// Takes an LLVM::TypeToLLVMIRTranslator owned by the caller and destroys it.
-/// It is the responsibility of the user to only pass an
-/// LLVM::TypeToLLVMIRTranslator class.
-MLIR_CAPI_EXPORTED void
-mlirTypeToLLVMIRTranslatorDestroy(MlirTypeToLLVMIRTranslator translator);
-
-/// Translates the given MLIR LLVM dialect to the LLVM IR type.
-MLIR_CAPI_EXPORTED LLVMTypeRef mlirTypeToLLVMIRTranslatorTranslateType(
-    MlirTypeToLLVMIRTranslator translator, MlirType mlirType);
 
 #ifdef __cplusplus
 }
