b5fdc65fd39d946046f029547258a4087a95d477
Daniel Paoliello <danpao@microsoft.com>
2025-01-30 09:51:43 -0800

    [clang][llvm][aarch64][win] Add a clang flag and module attribute for import call optimization, and remove LLVM flag (#122831)
    Switches import call optimization from being enabled by an LLVM flag to
    instead using a module attribute, and creates a new Clang flag that will
    set that attribute. This addresses the concern raised in the original
    PR:
    <https://github.com/llvm/llvm-project/pull/121516#discussion_r1911763991>
    
    This change also only creates the Called Global info if the module
    attribute is present, addressing this concern:
    <https://github.com/llvm/llvm-project/pull/122762#pullrequestreview-2547595934>

--- llvm/lib/Target/AArch64/AArch64ISelLowering.cpp
+++ llvm/lib/Target/AArch64/AArch64ISelLowering.cpp
@@ -9573,7 +9573,8 @@ AArch64TargetLowering::LowerCall(CallLoweringInfo &CLI,
 
     DAG.addNoMergeSiteInfo(Ret.getNode(), CLI.NoMerge);
     DAG.addCallSiteInfo(Ret.getNode(), std::move(CSInfo));
-    if (CalledGlobal)
+    if (CalledGlobal &&
+        MF.getFunction().getParent()->getModuleFlag("import-call-optimization"))
       DAG.addCalledGlobal(Ret.getNode(), CalledGlobal, OpFlags);
     return Ret;
   }
@@ -9586,7 +9587,8 @@ AArch64TargetLowering::LowerCall(CallLoweringInfo &CLI,
   DAG.addNoMergeSiteInfo(Chain.getNode(), CLI.NoMerge);
   InGlue = Chain.getValue(1);
   DAG.addCallSiteInfo(Chain.getNode(), std::move(CSInfo));
-  if (CalledGlobal)
+  if (CalledGlobal &&
+      MF.getFunction().getParent()->getModuleFlag("import-call-optimization"))
     DAG.addCalledGlobal(Chain.getNode(), CalledGlobal, OpFlags);
 
   uint64_t CalleePopBytes =
