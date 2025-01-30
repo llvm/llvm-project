01640190dfb54888cf4e9eae831fde218046e14b
vikhegde <vikram.hegde@amd.com>
2025-01-30 12:30:40 +0530

    update GCNNSAReassign.h

--- llvm/lib/Target/AMDGPU/GCNNSAReassign.h
+++ llvm/lib/Target/AMDGPU/GCNNSAReassign.h
@@ -1,4 +1,4 @@
-//===- GCNNSAReassign.h ---------------------------------------*- C++- *-===//
+//===- GCNNSAReassign.h -----------------------------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
