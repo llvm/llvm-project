a860748c9d563f84bc5fdaad6528e10e08404767
Nick Desaulniers <ndesaulniers@google.com>
2025-01-30 13:46:45 -0800

    SYS_ppoll fallback

--- libc/src/poll/linux/poll.cpp
+++ libc/src/poll/linux/poll.cpp
@@ -10,17 +10,35 @@
 
 #include "hdr/types/nfds_t.h"
 #include "hdr/types/struct_pollfd.h"
+#include "hdr/types/struct_timespec.h"
 #include "src/__support/OSUtil/syscall.h" // syscall_impl
 #include "src/__support/common.h"
 #include "src/__support/macros/config.h"
 #include "src/errno/libc_errno.h"
 
-#include <sys/syscall.h> // SYS_poll
+#include <sys/syscall.h> // SYS_poll, SYS_ppoll
 
 namespace LIBC_NAMESPACE_DECL {
 
 LLVM_LIBC_FUNCTION(int, poll, (struct pollfd * fds, nfds_t nfds, int timeout)) {
+
+#ifdef SYS_poll
   int ret = LIBC_NAMESPACE::syscall_impl<int>(SYS_poll, fds, nfds, timeout);
+#elif defined(SYS_ppoll)
+  struct timespec ts, *tsp;
+  if (timeout >= 0) {
+    ts.tv_sec = timeout / 1000;
+    ts.tv_nsec = (timeout % 1000) * 1000000;
+    tsp = &ts;
+  } else {
+    tsp = nullptr;
+  }
+  int ret = LIBC_NAMESPACE::syscall_impl<int>(SYS_ppoll, fds, nfds, tsp,
+                                              nullptr, 0);
+#else
+#error "SYS_ppoll_time64?"
+#endif
+
   if (ret < 0) {
     libc_errno = -ret;
     return -1;
