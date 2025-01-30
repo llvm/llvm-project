72f822d83f32d06175cd9817fd60a3250f1c0b6d
MaheshRavishankar <mravisha@amd.com>
2025-01-30 16:49:46 -0600

    [mlir][Linalg] NFC: Expose a method to deduplicate operands/remove dead results of `linalg.generic` op.
    This functionality was wrapped within a pattern. Expose this as a
    separate transformations function that can be used outside of pattern
    rewrite mechanism.
    
    Signed-off-by: MaheshRavishankar <mahesh.ravishankar@gmail.com>

--- mlir/include/mlir/Dialect/Linalg/Transforms/Transforms.h
+++ mlir/include/mlir/Dialect/Linalg/Transforms/Transforms.h
@@ -1785,8 +1785,16 @@ void populateDataLayoutPropagationPatterns(
     RewritePatternSet &patterns,
     const ControlPropagationFn &controlPackUnPackPropagation);
 
+/// Method to deduplicate operands and remove dead results of `linalg.generic`
+/// operations. This is effectively DCE for a linalg.generic op. If there is
+/// deduplication of operands orremoval of results, replaces the `genericOp`
+/// with a new op and returns it. Returns the same operation if there is no
+/// deduplication/removal.
+FailureOr<linalg::GenericOp> deduplicateOperandsAndRemoveDeadResults(
+    RewriterBase &rewriter, linalg::GenericOp genericOp, bool removeOutputs);
+
 /// Pattern to remove dead operands and results of `linalg.generic` operations.
-/// This is effectively DCE for a linalg op.
+/// This is a pattern wrapper for `deduplicateOperandsAndRemoveDeadResults`.
 void populateEraseUnusedOperandsAndResultsPatterns(RewritePatternSet &patterns);
 
 /// Patterns to promote inputs to outputs and remove unused inputs of
