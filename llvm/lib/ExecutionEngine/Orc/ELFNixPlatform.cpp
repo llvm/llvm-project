681592ea9316ab7d0e902152ab0cceb8492588ce
Lang Hames <lhames@gmail.com>
2025-01-30 16:03:00 +1100

    [ORC] Fix file comment formatting. NFC.
    (cherry picked from commit 7fb233f549dee0346332562de050ef2ab3654329)

--- llvm/lib/ExecutionEngine/Orc/ELFNixPlatform.cpp
+++ llvm/lib/ExecutionEngine/Orc/ELFNixPlatform.cpp
@@ -1,5 +1,4 @@
-//===------ ELFNixPlatform.cpp - Utilities for executing ELFNix in Orc
-//-----===//
+//===----- ELFNixPlatform.cpp - Utilities for executing ELFNix in Orc -----===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
