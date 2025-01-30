e36627178a02170a55564efbd545f0b6ec2781cf
Muhammad Bassiouni <bassiouni.git@gmail.com>
2025-01-30 15:13:28 +0200

    chore(license): add license to recently created files

--- clang/include/clang/Sema/Common.h
+++ clang/include/clang/Sema/Common.h
@@ -1,3 +1,14 @@
+//===--- Common.h ----- Semantic Analysis common header file --------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+// This file declares common functions used in SPIRV and HLSL semantic
+// analysis constructs.
+//===----------------------------------------------------------------------===//
+
 #ifndef LLVM_CLANG_SEMA_COMMON_H
 #define LLVM_CLANG_SEMA_COMMON_H
 
--- clang/lib/Sema/Common.cpp
+++ clang/lib/Sema/Common.cpp
@@ -1,3 +1,14 @@
+//===--- Common.cpp --- Semantic Analysis common implementation file ------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+// This implements common functions used in SPIRV and HLSL semantic
+// analysis constructs.
+//===----------------------------------------------------------------------===//
+
 #include "clang/Sema/Common.h"
 
 bool clang::CheckArgTypeIsCorrect(
