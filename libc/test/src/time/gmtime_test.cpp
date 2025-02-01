4c198adffdbf5a475beb49855646d85839542b9a
Zishan Mirza <zmirza@posteo.ch>
2025-02-01 13:58:44 +0100

    undo header include for tests

--- libc/test/src/time/gmtime_test.cpp
+++ libc/test/src/time/gmtime_test.cpp
@@ -6,6 +6,8 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include "hdr/types/struct_tm.h"
+#include "src/__support/CPP/limits.h" // INT_MAX, INT_MIN
 #include "src/errno/libc_errno.h"
 #include "src/time/gmtime.h"
 #include "src/time/time_constants.h"
