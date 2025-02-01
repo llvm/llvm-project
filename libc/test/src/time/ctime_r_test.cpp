efa0ee18c6e6d64751f20a9dadf8bed38d4d2b29
Zishan Mirza <zmirza@posteo.ch>
2025-02-01 14:19:34 +0100

    fixed header include for ctime_r tests

--- libc/test/src/time/ctime_r_test.cpp
+++ libc/test/src/time/ctime_r_test.cpp
@@ -9,7 +9,6 @@
 #include "src/errno/libc_errno.h"
 #include "src/time/ctime_r.h"
 #include "src/time/time_constants.h"
-#include "src/time/time_utils.h"
 #include "test/UnitTest/Test.h"
 #include "test/src/time/TmHelper.h"
 
