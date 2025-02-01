c6bd24637d41a8c02ef2f0e6976311e1fb7a6bf6
Zishan Mirza <zmirza@posteo.ch>
2025-02-01 14:18:42 +0100

    undo

--- libc/src/time/ctime_r.cpp
+++ libc/src/time/ctime_r.cpp
@@ -6,14 +6,12 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "ctime_r.h"
 #include "src/time/ctime_r.h"
 #include "src/__support/CPP/limits.h"
 #include "src/__support/common.h"
 #include "src/__support/macros/config.h"
 #include "src/time/time_constants.h"
 #include "src/time/time_utils.h"
-#include "time_utils.h"
 
 namespace LIBC_NAMESPACE_DECL {
 
