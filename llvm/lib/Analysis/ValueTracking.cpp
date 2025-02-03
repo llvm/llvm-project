ae59b5977d2c29b0be3b7f8bc3cc2aca693e4506
Yingwei Zheng <dtcxzyw2333@gmail.com>
2025-02-03 08:43:02 +0800

    [ValueTracking] Address review comments. NFC.

--- llvm/lib/Analysis/ValueTracking.cpp
+++ llvm/lib/Analysis/ValueTracking.cpp
@@ -776,7 +776,7 @@ static void computeKnownBitsFromICmpCond(const Value *V, ICmpInst *Cmp,
   if (match(LHS, m_Trunc(m_Specific(V)))) {
     KnownBits DstKnown(LHS->getType()->getScalarSizeInBits());
     computeKnownBitsFromCmp(LHS, Pred, LHS, RHS, DstKnown, SQ);
-    if (match(LHS, m_NUWTrunc(m_Value())))
+    if (cast<TruncInst>(LHS)->hasNoUnsignedWrap())
       Known = Known.unionWith(DstKnown.zext(Known.getBitWidth()));
     else
       Known = Known.unionWith(DstKnown.anyext(Known.getBitWidth()));
