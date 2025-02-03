bda908fcca7b9eb2feaf7e5a709f92428f9bab06
Nick Desaulniers <ndesaulniers@google.com>
2025-02-03 09:15:50 -0800

    add missing POSIX mandated macros

--- libc/include/llvm-libc-macros/linux/sys-wait-macros.h
+++ libc/include/llvm-libc-macros/linux/sys-wait-macros.h
@@ -12,9 +12,13 @@
 #include <linux/wait.h>
 
 // Wait status info macros
-#define __WEXITSTATUS(status) (((status)&0xff00) >> 8)
-#define __WTERMSIG(status) ((status)&0x7f)
+#define __WEXITSTATUS(status) (((status) & 0xff00) >> 8)
+#define __WTERMSIG(status) ((status) & 0x7f)
 #define __WIFEXITED(status) (__WTERMSIG(status) == 0)
+#define __WIFSIGNALED(status) ((__WTERMSIG(status) + 1) >= 2)
+#define __WIFSTOPPED(status) (__WTERMSIG(status) == 0x7f)
+#define __WIFCONTINUED(status) ((status) == __W_CONTINUED)
+#define __WCOREDUMP(status) ((status) & __WCOREFLAG)
 
 // Macros for constructing status values.
 #define __W_EXITCODE(ret, sig) ((ret) << 8 | (sig))
@@ -22,9 +26,14 @@
 #define __W_CONTINUED 0xffff
 #define __WCOREFLAG 0x80
 
+#define WCOREDUMP(status) ((status) & __WCOREFLAG)
 #define WEXITSTATUS(status) __WEXITSTATUS(status)
-#define WTERMSIG(status) __WTERMSIG(status)
+#define WIFCONTINUED(status) __WIFCONTINUED(status)
 #define WIFEXITED(status) __WIFEXITED(status)
+#define WIFSIGNALED(status) __WIFSIGNALED(status)
+#define WIFSTOPPED(status) __WIFSTOPPED(status)
+#define WSTOPSIG(status) WEXITSTATUS(status)
+#define WTERMSIG(status) __WTERMSIG(status)
 
 #define WCOREFLAG __WCOREFLAG
 #define W_EXITCODE(ret, sig) __W_EXITCODE(ret, sig)
