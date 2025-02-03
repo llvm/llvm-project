cbba35d6769de4658e5a6df95c021d333ca5ad1e
Craig Topper <craig.topper@sifive.com>
2025-02-03 09:35:04 -0800

    fixup! add comment to test

--- llvm/utils/TableGen/DAGISelMatcherOpt.cpp
+++ llvm/utils/TableGen/DAGISelMatcherOpt.cpp
@@ -135,25 +135,6 @@ static void ContractNodes(std::unique_ptr<Matcher> &MatcherPtr,
       // variants.
     }
 
-  // If we have a CheckType/CheckChildType/Record node followed by a
-  // CheckOpcode, invert the two nodes.  We prefer to do structural checks
-  // before type checks, as this opens opportunities for factoring on targets
-  // like X86 where many operations are valid on multiple types.
-  if ((isa<CheckTypeMatcher>(N) || isa<CheckChildTypeMatcher>(N) ||
-       isa<RecordMatcher>(N)) &&
-      isa<CheckOpcodeMatcher>(N->getNext())) {
-    // Unlink the two nodes from the list.
-    Matcher *CheckType = MatcherPtr.release();
-    Matcher *CheckOpcode = CheckType->takeNext();
-    Matcher *Tail = CheckOpcode->takeNext();
-
-    // Relink them.
-    MatcherPtr.reset(CheckOpcode);
-    CheckOpcode->setNext(CheckType);
-    CheckType->setNext(Tail);
-    return ContractNodes(MatcherPtr, CGP);
-  }
-
   ContractNodes(N->getNextPtr(), CGP);
 
   // If we have a MoveParent followed by a MoveChild, we convert it to
