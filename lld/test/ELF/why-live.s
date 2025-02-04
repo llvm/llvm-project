8cca3799bb204acf774397127ba87df70fbc77d8
Daniel Thornburgh <dthorn@google.com>
2025-02-03 16:15:32 -0800

    Complete the first test

--- lld/test/ELF/why-live.s
+++ lld/test/ELF/why-live.s
@@ -3,7 +3,9 @@
 # RUN: llvm-mc -n -filetype=obj -triple=x86_64 %s -o %t.o
 # RUN: ld.lld %t.o -o /dev/null --gc-sections --why-live=a | FileCheck %s
 
-# CHECK: blah
+# CHECK: live symbol: a
+# CHECK: >>> alive because of {{.*}}.o:(._start)
+# CHECK: >>> alive because of _start
 
 .globl _start
 .section ._start,"ax",@progbits
