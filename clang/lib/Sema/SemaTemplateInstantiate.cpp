57f36d2190f2ad6bb3536181c2b3db30b679f388
Kazu Hirata <kazu@google.com>
2025-02-04 09:09:01 -0800

    [Sema] Migrate away from PointerUnion::dyn_cast (NFC) (#125630)
    Note that PointerUnion::dyn_cast has been soft deprecated in
    PointerUnion.h:
    
      // FIXME: Replace the uses of is(), get() and dyn_cast() with
      //        isa<T>, cast<T> and the llvm::dyn_cast<T>
    
    Literal migration would result in dyn_cast_if_present (see the
    definition of PointerUnion::dyn_cast), but this patch uses dyn_cast
    because we expect *Found to be nonnull.  Note that if *Found were
    null, cast<VarDecl>(TransformedDecl) would trigger an assertion error.

--- clang/lib/Sema/SemaTemplateInstantiate.cpp
+++ clang/lib/Sema/SemaTemplateInstantiate.cpp
@@ -2438,7 +2438,7 @@ TemplateInstantiator::TransformFunctionParmPackRefExpr(DeclRefExpr *E,
   assert(Found && "no instantiation for parameter pack");
 
   Decl *TransformedDecl;
-  if (DeclArgumentPack *Pack = Found->dyn_cast<DeclArgumentPack *>()) {
+  if (DeclArgumentPack *Pack = dyn_cast<DeclArgumentPack *>(*Found)) {
     // If this is a reference to a function parameter pack which we can
     // substitute but can't yet expand, build a FunctionParmPackExpr for it.
     if (getSema().ArgumentPackSubstitutionIndex == -1) {
