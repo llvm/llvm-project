0ca085cb22c9873b870060b9a3a3f641f5a9dcb9
Petr Hosek <phosek@google.com>
2025-02-05 08:27:14 +0000

    Avoid applying compile options to the linker
    Compile options aren't the same as link options and on some platforms
    like Windows where the linker is invoked directly they're distinctly
    different.

--- libc/cmake/modules/LLVMLibCTestRules.cmake
+++ libc/cmake/modules/LLVMLibCTestRules.cmake
@@ -272,7 +272,6 @@ function(create_libc_unittest fq_target_name)
   target_include_directories(${fq_build_target_name} SYSTEM PRIVATE ${LIBC_INCLUDE_DIR})
   target_include_directories(${fq_build_target_name} PRIVATE ${LIBC_SOURCE_DIR})
   target_compile_options(${fq_build_target_name} PRIVATE ${compile_options})
-  target_link_options(${fq_build_target_name} PRIVATE ${compile_options})
 
   if(NOT LIBC_UNITTEST_CXX_STANDARD)
     set(LIBC_UNITTEST_CXX_STANDARD ${CMAKE_CXX_STANDARD})
