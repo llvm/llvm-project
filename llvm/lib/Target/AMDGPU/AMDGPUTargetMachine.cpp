0e43f4296e1cffa8b75d2fcd0a35150c252de31d
Scott Linder <Scott.Linder@amd.com>
2025-02-05 22:56:55 +0000

    Fixes for SWDEV-508901
    Cherry-pick of remaining stack from
    https://github.com/llvm/llvm-project/pull/126148 plus a downstream test.
    
    This is a combination of 2 commits.
    
    [AMDGPU] Remove dead function metadata after amdgpu-lower-kernel-arguments
    
    The verifier ensures function !dbg metadata is unique across the module,
    so ensure the old nameless function we leave behind doesn't violate
    this invariant.
    
    Removing the function via e.g. eraseFromParent seems like a better
    option, but doesn't seem to be legal from a FunctionPass.
    
    [AMDGPU] Push amdgpu-preload-kern-arg-prolog after livedebugvalues
    
    This is effectively a workaround for a bug in livedebugvalues, but seems
    to potentially be a general improvement, as BB sections seems like it
    could ruin the special 256-byte prelude scheme that
    amdgpu-preload-kern-arg-prolog requires anyway. Moving it even later
    doesn't seem to have any material impact, and just adds livedebugvalues
    to the list of things which no longer have to deal with pseudo
    multiple-entry functions.

--- llvm/lib/Target/AMDGPU/AMDGPUTargetMachine.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPUTargetMachine.cpp
@@ -1189,6 +1189,7 @@ public:
   void addPostRegAlloc() override;
   void addPreSched2() override;
   void addPreEmitPass() override;
+  void addPostBBSections() override;
 };
 
 } // end anonymous namespace
@@ -1731,6 +1732,11 @@ void GCNPassConfig::addPreEmitPass() {
     addPass(&AMDGPUInsertDelayAluID);
 
   addPass(&BranchRelaxationPassID);
+}
+
+void GCNPassConfig::addPostBBSections() {
+  // We run this later to avoid passes like livedebugvalues and BBSections
+  // having to deal with the apparent multi-entry functions we may generate.
   addPass(createAMDGPUPreloadKernArgPrologLegacyPass());
 }
 
