26e2dd5585fb9891ec7b62f46ac0c694d801af28
Fabian Ritter <fabian.ritter@amd.com>
2025-02-05 04:19:00 -0500

    [AMDGPU] Replace gfx940 and gfx941 with gfx942 in offload and libclc
    gfx940 and gfx941 are no longer supported. This is one of a series of
    PRs to remove them from the code base.
    
    For SWDEV-512631 and SWDEV-512633

--- flang/cmake/modules/AddFlangOffloadRuntime.cmake
+++ flang/cmake/modules/AddFlangOffloadRuntime.cmake
@@ -98,7 +98,7 @@ macro(enable_omp_offload_compilation files)
 
       set(all_amdgpu_architectures
         "gfx700;gfx701;gfx801;gfx803;gfx900;gfx902;gfx906"
-        "gfx908;gfx90a;gfx90c;gfx940;gfx1010;gfx1030"
+        "gfx908;gfx90a;gfx90c;gfx942;gfx1010;gfx1030"
         "gfx1031;gfx1032;gfx1033;gfx1034;gfx1035;gfx1036"
         "gfx1100;gfx1101;gfx1102;gfx1103;gfx1150;gfx1151"
         "gfx1152;gfx1153"
