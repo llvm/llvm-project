48e900c6a8bd24c7cbee057eb0d96b9f0b2b4f84
Adrian Vogelsgesang <avogelsgesang@salesforce.com>
2025-02-05 10:11:38 +0000

    [lldb-dap] Support vscode launch URLs
    This commit adds support for starting debug sessions through special
    `vscode://llvm-vs-code-extensions.lldb-dap/launch/config?config={launch-config}`
    URIs. This allows tighter integration with custom scripts. One potential
    use case is providing similar functionality to `xcdebug`, see #125777
    for some discussion on that use case.
    
    The functionality was inspired by @vadimcn's CodeLLDB extension, which
    [provides similar functionality](https://github.com/vadimcn/codelldb/blob/master/MANUAL.md#debugging-externally-launched-code).

---
+++ lldb/tools/lldb-dap/src-ts/uri-launch-handler.ts
@@ -0,0 +1,37 @@
+import * as vscode from "vscode";
+
+export class LaunchUriHandler implements vscode.UriHandler {
+    async handleUri(uri: vscode.Uri) {
+        try {
+            const params = new URLSearchParams(uri.query);
+            if (uri.path == '/launch/config') {
+                const configJson = params.get("config");
+                if (configJson === null) {
+                    throw new Error("Missing `config` URI parameter");
+                }
+                // Build the debug config
+                let debugConfig: vscode.DebugConfiguration = {
+                    type: 'lldb-dap',
+                    request: 'launch',
+                    name: '',
+                };
+                Object.assign(debugConfig, JSON.parse(configJson));
+                debugConfig.name = debugConfig.name || debugConfig.program || "Adhoc Launch";
+                // Force the type to `lldb-dap`. We don't want to allow launching any other
+                // Debug Adapters using this URI scheme.
+                if (debugConfig.type != "lldb-dap") {
+                    throw new Error(`Unsupported debugger type: ${debugConfig.type}`);
+                }
+                await vscode.debug.startDebugging(undefined, debugConfig);
+            } else {
+                throw new Error(`Unsupported Uri path: ${uri.path}`);
+            }
+        } catch (err) {
+            if (err instanceof Error) {
+                await vscode.window.showErrorMessage(`Failed to handle lldb-dap URI request: ${err.message}`);
+            } else {
+                await vscode.window.showErrorMessage(`Failed to handle lldb-dap URI request`);
+            }
+        }
+    }
+}
