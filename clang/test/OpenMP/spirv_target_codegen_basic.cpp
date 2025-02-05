626b15ba7d3dbfbe58df7b13d6aded852ca58d77
Sarnie, Nick <nick.sarnie@intel.com>
2025-02-05 11:55:28 -0800

    extra space
    Signed-off-by: Sarnie, Nick <nick.sarnie@intel.com>

--- clang/test/OpenMP/spirv_target_codegen_basic.cpp
+++ clang/test/OpenMP/spirv_target_codegen_basic.cpp
@@ -1,5 +1,5 @@
 // RUN: %clang_cc1 -verify -fopenmp -x c++ -triple x86_64-unknown-linux -fopenmp-targets=spirv64-intel -emit-llvm-bc %s -o %t-host.bc
-// RUN: %clang_cc1 -verify -fopenmp -x c++ -triple spirv64-intel -fopenmp-targets=spirv64-intel  -emit-llvm %s -fopenmp-is-target-device -fopenmp-host-ir-file-path %t-host.bc -o - | FileCheck %s
+// RUN: %clang_cc1 -verify -fopenmp -x c++ -triple spirv64-intel -fopenmp-targets=spirv64-intel -emit-llvm %s -fopenmp-is-target-device -fopenmp-host-ir-file-path %t-host.bc -o - | FileCheck %s
 
 // expected-no-diagnostics
 
