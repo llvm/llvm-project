0c61b095266f77f4feaaa6e0e28ae941843eef07
c8ef <c8ef@outlook.com>
2025-02-06 09:28:20 +0800

    [libc] implement `sys/uio/readv` (#124718)
    Closes #124694.
    
    This patch adds the `sys/uio/readv` function.
    ref:
    https://pubs.opengroup.org/onlinepubs/009696699/functions/readv.html

--- libc/src/sys/uio/linux/readv.cpp
+++ libc/src/sys/uio/linux/readv.cpp
@@ -1,11 +1,11 @@
-//===-- Implementation file for writev ------------------------------------===//
+//===-- Implementation file for readv -------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
-#include "src/sys/uio/writev.h"
+#include "src/sys/uio/readv.h"
 #include "hdr/types/ssize_t.h"
 #include "hdr/types/struct_iovec.h"
 #include "src/__support/OSUtil/syscall.h"
@@ -15,14 +15,14 @@
 
 namespace LIBC_NAMESPACE_DECL {
 
-LLVM_LIBC_FUNCTION(ssize_t, writev, (int fd, const iovec *iov, int iovcnt)) {
-  long ret = LIBC_NAMESPACE::syscall_impl<long>(SYS_writev, fd, iov, iovcnt);
+LLVM_LIBC_FUNCTION(ssize_t, readv, (int fd, const iovec *iov, int iovcnt)) {
+  long ret = LIBC_NAMESPACE::syscall_impl<long>(SYS_readv, fd, iov, iovcnt);
   // On failure, return -1 and set errno.
   if (ret < 0) {
     libc_errno = static_cast<int>(-ret);
     return -1;
   }
-  // On success, return number of bytes written.
+  // On success, return number of bytes read.
   return static_cast<ssize_t>(ret);
 }
 
--- libc/test/src/sys/uio/readv_test.cpp
+++ libc/test/src/sys/uio/readv_test.cpp
@@ -1,4 +1,4 @@
-//===-- Unittests for writev ----------------------------------------------===//
+//===-- Unittests for readv -----------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -8,26 +8,35 @@
 
 #include "hdr/types/struct_iovec.h"
 #include "src/fcntl/open.h"
-#include "src/sys/uio/writev.h"
+#include "src/sys/uio/readv.h"
 #include "src/unistd/close.h"
 #include "src/unistd/unlink.h"
+#include "src/unistd/write.h"
 #include "test/UnitTest/ErrnoSetterMatcher.h"
 #include "test/UnitTest/Test.h"
 
 using namespace LIBC_NAMESPACE::testing::ErrnoSetterMatcher;
 
-TEST(LlvmLibcSysUioWritevTest, SmokeTest) {
-  const char *filename = "./LlvmLibcSysUioWritevTest";
+TEST(LlvmLibcSysUioReadvTest, SmokeTest) {
+  const char *filename = "./LlvmLibcSysUioReadvTest";
   int fd = LIBC_NAMESPACE::open(filename, O_WRONLY | O_CREAT, 0644);
   ASSERT_THAT(fd, returns(GT(0)).with_errno(EQ(0)));
-  const char *data = "Hello, World!\n";
+  const char data[] = "Hello, World!\n";
+  ASSERT_THAT(LIBC_NAMESPACE::write(fd, data, sizeof(data)),
+              returns(EQ(sizeof(data))).with_errno(EQ(0)));
+  ASSERT_THAT(LIBC_NAMESPACE::close(fd), Succeeds());
+
+  fd = LIBC_NAMESPACE::open(filename, O_RDONLY);
+  ASSERT_THAT(fd, returns(GT(0)).with_errno(EQ(0)));
+  char buf0[2];
+  char buf1[3];
   struct iovec iov[2];
-  iov[0].iov_base = const_cast<char *>(data);
-  iov[0].iov_len = 7;
-  iov[1].iov_base = const_cast<char *>(data + 7);
-  iov[1].iov_len = 8;
-  ASSERT_THAT(LIBC_NAMESPACE::writev(fd, iov, 2),
-              returns(EQ(15)).with_errno(EQ(0)));
+  iov[0].iov_base = buf0;
+  iov[0].iov_len = 1;
+  iov[1].iov_base = buf1;
+  iov[1].iov_len = 2;
+  ASSERT_THAT(LIBC_NAMESPACE::readv(fd, iov, 2),
+              returns(EQ(3)).with_errno(EQ(0)));
   ASSERT_THAT(LIBC_NAMESPACE::close(fd), Succeeds());
   ASSERT_THAT(LIBC_NAMESPACE::unlink(filename),
               returns(EQ(0)).with_errno(EQ(0)));
