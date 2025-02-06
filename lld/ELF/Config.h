ddc3e8dbb5a5d312dcc6d082062b50368bc62194
Fangrui Song <i@maskray.me>
2025-02-05 21:16:00 -0800

    [ELF] Refine isExported/isPreemptible condition
    Reland 994cea3f0a2d0caf4d66321ad5a06ab330144d89 after bolt tests no
    longer rely on -pie --unresolved-symbols=ignore-all with no input DSO
    generating PLT entries.
    
    ---
    
    Commit f10441ad003236ef3b9e5415a571d2be0c0ce5ce , while dropping a
    special case for isUndefWeak and --no-dynamic-linking, made
    --export-dynamic ineffective when -pie is used without any input DSO.
    
    This change restores --export-dynamic and unifies -pie and -pie
    --no-dynamic-linker when there is no input DSO.
    
    * -pie with no input DSO suppresses undefined symbols in .dynsym.
      Previously this only appied to -pie --no-dynamic-linker.
    * As a side effect, -pie with no input DSO suppresses PLT.

--- lld/ELF/Config.h
+++ lld/ELF/Config.h
@@ -298,7 +298,6 @@ struct Config {
   bool gdbIndex;
   bool gnuHash = false;
   bool gnuUnique;
-  bool hasDynSymTab;
   bool ignoreDataAddressEquality;
   bool ignoreFunctionAddressEquality;
   bool ltoCSProfileGenerate;
@@ -312,7 +311,6 @@ struct Config {
   bool mipsN32Abi = false;
   bool mmapOutputFile;
   bool nmagic;
-  bool noDynamicLinker = false;
   bool noinhibitExec;
   bool nostdlib;
   bool oFormatBinary;
