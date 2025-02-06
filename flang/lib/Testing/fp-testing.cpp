26b2db41db0ff424afdf81aaffe8fb1f921fb32a
Michael Kruse <llvm-project@meinersbur.de>
2025-02-06 21:45:51 +0100

    [Flang] Promote FortranEvaluateTesting library (#124417)
    The non-GTest library will be shared by unittests of Flang and Flang-RT.
    Promote it as a regular library for use by both projects.
    
    In the long term, we may want to convert these to regular GTest checks
    to avoid having multiple testing frameworks.

--- flang/lib/Testing/fp-testing.cpp
+++ flang/lib/Testing/fp-testing.cpp
@@ -1,4 +1,12 @@
-#include "fp-testing.h"
+//===-- lib/Testing/fp-testing.cpp ------------------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "flang/Testing/fp-testing.h"
 #include "llvm/Support/Errno.h"
 #include <cstdio>
 #include <cstdlib>
--- flang/lib/Testing/testing.cpp
+++ flang/lib/Testing/testing.cpp
@@ -1,4 +1,12 @@
-#include "testing.h"
+//===-- lib/Testing/testing.cpp ---------------------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "flang/Testing/testing.h"
 #include "llvm/Support/raw_ostream.h"
 #include <cstdarg>
 #include <cstdio>
