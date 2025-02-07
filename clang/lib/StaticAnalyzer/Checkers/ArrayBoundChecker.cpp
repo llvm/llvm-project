860f9ab089a9f634069ccb799ff01ed6c4ba7d7b
Donát Nagy <donat.nagy@ericsson.com>
2025-02-07 10:32:26 +0100

    Remove '*- C++ -*' from the file header
    Co-authored-by: Balazs Benics <benicsbalazs@gmail.com>

--- clang/lib/StaticAnalyzer/Checkers/ArrayBoundChecker.cpp
+++ clang/lib/StaticAnalyzer/Checkers/ArrayBoundChecker.cpp
@@ -1,4 +1,4 @@
-//== ArrayBoundChecker.cpp --------------------------------------*- C++ -*--==//
+//== ArrayBoundChecker.cpp -------------------------------------------------==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
