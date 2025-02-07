4b148d509166e5997945ad771e30b3df5cf49fff
Brandon Wu <brandon.wu@sifive.com>
2025-02-06 22:29:02 -0800

    fixup! [RISCV][Sema] Add feature check for target attribute to VSETVL intrinsics

--- clang/lib/Sema/SemaRISCV.cpp
+++ clang/lib/Sema/SemaRISCV.cpp
@@ -623,8 +623,7 @@ bool SemaRISCV::CheckBuiltinFunctionCall(const TargetInfo &TI,
   }
   }
 
-  auto checkVsetvl = [&](unsigned SEWOffset,
-                                        unsigned LMULOffset) -> bool {
+  auto CheckVSetVL = [&](unsigned SEWOffset, unsigned LMULOffset) -> bool {
     const FunctionDecl *FD = SemaRef.getCurFunctionDecl();
     llvm::StringMap<bool> FunctionFeatureMap;
     Context.getFunctionFeatureMap(FunctionFeatureMap, FD);
@@ -635,14 +634,12 @@ bool SemaRISCV::CheckBuiltinFunctionCall(const TargetInfo &TI,
       return true;
     int SEWValue = SEWResult.getSExtValue();
     int LMULValue = LMULResult.getSExtValue();
-    if (((SEWValue == 0 && LMULValue == 5) ||  // e8mf8
-         (SEWValue == 1 && LMULValue == 6) ||  // e16mf4
-         (SEWValue == 2 && LMULValue == 7) ||  // e32mf2
-         (SEWValue == 3 && LMULValue == 0) ||  // e64m1
-         (SEWValue == 3 && LMULValue == 1) ||  // e64m2
-         (SEWValue == 3 && LMULValue == 2) ||  // e64m4
-         (SEWValue == 3 && LMULValue == 3)) && // e64m8
-        (!TI.hasFeature("zve64x") && !FunctionFeatureMap.lookup("zve64x")))
+    if (((SEWValue == 0 && LMULValue == 5) || // e8mf8
+         (SEWValue == 1 && LMULValue == 6) || // e16mf4
+         (SEWValue == 2 && LMULValue == 7) || // e32mf2
+         SEWValue == 3) &&                    // e64
+        !TI.hasFeature("zve64x") &&
+        !FunctionFeatureMap.lookup("zve64x"))
       return Diag(TheCall->getBeginLoc(),
                   diag::err_riscv_builtin_requires_extension)
              << /* IsExtension */ true << TheCall->getSourceRange() << "zve64x";
@@ -651,9 +648,9 @@ bool SemaRISCV::CheckBuiltinFunctionCall(const TargetInfo &TI,
   };
   switch (BuiltinID) {
   case RISCVVector::BI__builtin_rvv_vsetvli:
-    return checkVsetvl(1, 2);
+    return CheckVSetVL(1, 2);
   case RISCVVector::BI__builtin_rvv_vsetvlimax:
-    return checkVsetvl(0, 1);
+    return CheckVSetVL(0, 1);
   case RISCVVector::BI__builtin_rvv_vget_v: {
     ASTContext::BuiltinVectorTypeInfo ResVecInfo =
         Context.getBuiltinVectorTypeInfo(cast<BuiltinType>(
