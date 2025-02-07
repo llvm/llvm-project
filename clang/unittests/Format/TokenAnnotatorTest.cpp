5216591f56b5ded5efb95a95cc9bfd4ebff26f29
rmarker <rmarker@outlook.com>
2025-02-07 19:08:24 +1030

    Fix test case.

--- clang/unittests/Format/TokenAnnotatorTest.cpp
+++ clang/unittests/Format/TokenAnnotatorTest.cpp
@@ -874,8 +874,8 @@ TEST_F(TokenAnnotatorTest, UnderstandsCasts) {
   EXPECT_TOKEN(Tokens[14], tok::r_paren, TT_CastRParen);
   EXPECT_TOKEN(Tokens[15], tok::amp, TT_UnaryOperator);
 
-  Tokens = annotate("func((foo(bar::*)(void))&a);");
-  ASSERT_EQ(Tokens.size(), 18u) << Tokens;
+  Tokens = annotate("func((foo(bar::*)(void))&bar::a);");
+  ASSERT_EQ(Tokens.size(), 20u) << Tokens;
   EXPECT_TOKEN(Tokens[12], tok::r_paren, TT_CastRParen);
   EXPECT_TOKEN(Tokens[13], tok::amp, TT_UnaryOperator);
 
