c2e3e48912714f91981876faca4bd446b9669ce5
Michael Kruse <llvm-project@meinersbur.de>
2025-02-08 01:06:27 +0100

    [Flang] Optionally do not compile the runtime in-tree (#122336)
    Introduce the CMake switch FLANG_INCLUDE_RUNTIME. When set to off, do
    not add build instructions for the runtime.
    
    This is required for Flang-RT (#110217) and the current runtime CMake
    code to co-exist. When using `LLVM_ENABLE_RUNTIME=flang-rt`, the in-tree
    build instructions are in conflict and must be disabled.

--- flang/test/Runtime/no-cpp-dep.c
+++ flang/test/Runtime/no-cpp-dep.c
@@ -3,7 +3,7 @@ This test makes sure that flang's runtime does not depend on the C++ runtime
 library. It tries to link this simple file against libFortranRuntime.a with
 a C compiler.
 
-REQUIRES: c-compiler
+REQUIRES: c-compiler, flang-rt
 
 RUN: %if system-aix %{ export OBJECT_MODE=64 %}
 RUN: %cc -std=c99 %s -I%include %libruntime -lm  \
