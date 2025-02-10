8f541bcfba02016a78a6636afa96d1bfc231ecfd
Vy Nguyen <vyng@google.com>
2025-02-10 13:59:52 -0500

    [lldb][telemetry] Implement LLDB Telemetry (part 1) (#119716)
    Details:
    - This is a subset of PR/98528.( Pavel's suggestion was to split up the
    patch to make reviewing easier)
    - This contains only the concrete implementation of the framework to be
    used but no usages yet.
    - I plan to send a few follow-up patches:
    + part2 : includes changes in the plugin-manager to set up the plugin
    stuff (ie., how to create a default vs vendor impl)
      + part3 (all of the following can be done in parallel):
    * part 3_a: define DebuggerTelemetryInfo and related methods to collect
    data about debugger startup/exit
    * part 3_b: define TargetTelemetryInfo and related methods to collect
    data about debug target(s)
    * part 3_c: define CommandTelemetryInfo and related methods to collect
    data about debug-commands
    * part 3_d: define ClientTelemtryInfo and related methods to collect
    data about lldb-dap/any other client
    
    ---------
    
    Co-authored-by: Pavel Labath <pavel@labath.sk>
    Co-authored-by: Jonas Devlieghere <jonas@devlieghere.com>

---
+++ lldb/source/Core/Telemetry.cpp
@@ -0,0 +1,69 @@
+//===-- Telemetry.cpp -----------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+#include "lldb/Core/Telemetry.h"
+#include "lldb/Core/Debugger.h"
+#include "lldb/Utility/LLDBLog.h"
+#include "lldb/Utility/UUID.h"
+#include "lldb/lldb-enumerations.h"
+#include "lldb/lldb-forward.h"
+#include "llvm/ADT/StringRef.h"
+#include "llvm/Support/Error.h"
+#include "llvm/Support/RandomNumberGenerator.h"
+#include "llvm/Telemetry/Telemetry.h"
+#include <chrono>
+#include <cstdlib>
+#include <memory>
+#include <string>
+#include <utility>
+
+namespace lldb_private {
+namespace telemetry {
+
+using ::llvm::Error;
+using ::llvm::telemetry::Destination;
+using ::llvm::telemetry::Serializer;
+using ::llvm::telemetry::TelemetryInfo;
+
+static uint64_t ToNanosec(const SteadyTimePoint Point) {
+  return std::chrono::nanoseconds(Point.time_since_epoch()).count();
+}
+
+void LLDBBaseTelemetryInfo::serialize(Serializer &serializer) const {
+  serializer.write("entry_kind", getKind());
+  serializer.write("session_id", SessionId);
+  serializer.write("start_time", ToNanosec(start_time));
+  if (end_time.has_value())
+    serializer.write("end_time", ToNanosec(end_time.value()));
+}
+
+static std::string MakeUUID(lldb_private::Debugger *debugger) {
+  uint8_t random_bytes[16];
+  if (auto ec = llvm::getRandomBytes(random_bytes, 16)) {
+    LLDB_LOG(GetLog(LLDBLog::Object),
+             "Failed to generate random bytes for UUID: {0}", ec.message());
+    // fallback to using timestamp + debugger ID.
+    return llvm::formatv(
+        "{0}_{1}", std::chrono::steady_clock::now().time_since_epoch().count(),
+        debugger->GetID());
+  }
+  return lldb_private::UUID(random_bytes).GetAsString();
+}
+
+TelemetryManager::TelemetryManager(
+    std::unique_ptr<llvm::telemetry::Config> config)
+    : m_config(std::move(config)) {}
+
+llvm::Error TelemetryManager::preDispatch(TelemetryInfo *entry) {
+  // Do nothing for now.
+  // In up-coming patch, this would be where the manager
+  // attach the session_uuid to the entry.
+  return Error::success();
+}
+
+} // namespace telemetry
+} // namespace lldb_private
