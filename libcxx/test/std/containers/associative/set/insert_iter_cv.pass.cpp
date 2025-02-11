af761051c6f2858ed20a1dfc9ce897360ac0c20c
Louis Dionne <ldionne.2@gmail.com>
2025-02-11 06:17:39 +0100

    [libc++][NFC] Run the container tests through clang-format (#126499)
    We've been improving these the tests for vector quite a bit and we are
    probably not done improving our container tests. Formatting everything
    at once will make subsequent reviews easier.

--- libcxx/test/std/containers/associative/set/insert_iter_cv.pass.cpp
+++ libcxx/test/std/containers/associative/set/insert_iter_cv.pass.cpp
@@ -18,57 +18,56 @@
 #include "test_macros.h"
 #include "min_allocator.h"
 
-int main(int, char**)
-{
-    {
-        typedef std::set<int> M;
-        typedef M::iterator R;
-        M m;
-        R r = m.insert(m.cend(), M::value_type(2));
-        assert(r == m.begin());
-        assert(m.size() == 1);
-        assert(*r == 2);
+int main(int, char**) {
+  {
+    typedef std::set<int> M;
+    typedef M::iterator R;
+    M m;
+    R r = m.insert(m.cend(), M::value_type(2));
+    assert(r == m.begin());
+    assert(m.size() == 1);
+    assert(*r == 2);
 
-        r = m.insert(m.cend(), M::value_type(1));
-        assert(r == m.begin());
-        assert(m.size() == 2);
-        assert(*r == 1);
+    r = m.insert(m.cend(), M::value_type(1));
+    assert(r == m.begin());
+    assert(m.size() == 2);
+    assert(*r == 1);
 
-        r = m.insert(m.cend(), M::value_type(3));
-        assert(r == std::prev(m.end()));
-        assert(m.size() == 3);
-        assert(*r == 3);
+    r = m.insert(m.cend(), M::value_type(3));
+    assert(r == std::prev(m.end()));
+    assert(m.size() == 3);
+    assert(*r == 3);
 
-        r = m.insert(m.cend(), M::value_type(3));
-        assert(r == std::prev(m.end()));
-        assert(m.size() == 3);
-        assert(*r == 3);
-    }
+    r = m.insert(m.cend(), M::value_type(3));
+    assert(r == std::prev(m.end()));
+    assert(m.size() == 3);
+    assert(*r == 3);
+  }
 #if TEST_STD_VER >= 11
-    {
-        typedef std::set<int, std::less<int>, min_allocator<int>> M;
-        typedef M::iterator R;
-        M m;
-        R r = m.insert(m.cend(), M::value_type(2));
-        assert(r == m.begin());
-        assert(m.size() == 1);
-        assert(*r == 2);
+  {
+    typedef std::set<int, std::less<int>, min_allocator<int>> M;
+    typedef M::iterator R;
+    M m;
+    R r = m.insert(m.cend(), M::value_type(2));
+    assert(r == m.begin());
+    assert(m.size() == 1);
+    assert(*r == 2);
 
-        r = m.insert(m.cend(), M::value_type(1));
-        assert(r == m.begin());
-        assert(m.size() == 2);
-        assert(*r == 1);
+    r = m.insert(m.cend(), M::value_type(1));
+    assert(r == m.begin());
+    assert(m.size() == 2);
+    assert(*r == 1);
 
-        r = m.insert(m.cend(), M::value_type(3));
-        assert(r == std::prev(m.end()));
-        assert(m.size() == 3);
-        assert(*r == 3);
+    r = m.insert(m.cend(), M::value_type(3));
+    assert(r == std::prev(m.end()));
+    assert(m.size() == 3);
+    assert(*r == 3);
 
-        r = m.insert(m.cend(), M::value_type(3));
-        assert(r == std::prev(m.end()));
-        assert(m.size() == 3);
-        assert(*r == 3);
-    }
+    r = m.insert(m.cend(), M::value_type(3));
+    assert(r == std::prev(m.end()));
+    assert(m.size() == 3);
+    assert(*r == 3);
+  }
 #endif
 
   return 0;
