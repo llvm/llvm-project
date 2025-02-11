5d3d50e512c3a3b4d4e5cb89bdaad2bb8cfd599b
Louis Dionne <ldionne.2@gmail.com>
2025-02-11 06:17:39 +0100

    [libc++][NFC] Run the container tests through clang-format (#126499)
    We've been improving these the tests for vector quite a bit and we are
    probably not done improving our container tests. Formatting everything
    at once will make subsequent reviews easier.

--- libcxx/test/std/containers/unord/unord.multiset/erase_range.pass.cpp
+++ libcxx/test/std/containers/unord/unord.multiset/erase_range.pass.cpp
@@ -21,76 +21,58 @@
 #include "test_macros.h"
 #include "min_allocator.h"
 
-int main(int, char**)
-{
-    {
-        typedef std::unordered_multiset<int> C;
-        typedef int P;
-        P a[] =
-        {
-            P(1),
-            P(2),
-            P(3),
-            P(4),
-            P(1),
-            P(2)
-        };
-        C c(a, a + sizeof(a)/sizeof(a[0]));
-        C::const_iterator i = c.find(2);
-        C::const_iterator j = std::next(i, 2);
-        C::iterator k = c.erase(i, i);
-        assert(k == i);
-        assert(c.size() == 6);
-        assert(c.count(1) == 2);
-        assert(c.count(2) == 2);
-        assert(c.count(3) == 1);
-        assert(c.count(4) == 1);
+int main(int, char**) {
+  {
+    typedef std::unordered_multiset<int> C;
+    typedef int P;
+    P a[] = {P(1), P(2), P(3), P(4), P(1), P(2)};
+    C c(a, a + sizeof(a) / sizeof(a[0]));
+    C::const_iterator i = c.find(2);
+    C::const_iterator j = std::next(i, 2);
+    C::iterator k       = c.erase(i, i);
+    assert(k == i);
+    assert(c.size() == 6);
+    assert(c.count(1) == 2);
+    assert(c.count(2) == 2);
+    assert(c.count(3) == 1);
+    assert(c.count(4) == 1);
 
-        k = c.erase(i, j);
-        assert(c.size() == 4);
-        assert(c.count(1) == 2);
-        assert(c.count(3) == 1);
-        assert(c.count(4) == 1);
+    k = c.erase(i, j);
+    assert(c.size() == 4);
+    assert(c.count(1) == 2);
+    assert(c.count(3) == 1);
+    assert(c.count(4) == 1);
 
-        k = c.erase(c.cbegin(), c.cend());
-        assert(c.size() == 0);
-        assert(k == c.end());
-    }
+    k = c.erase(c.cbegin(), c.cend());
+    assert(c.size() == 0);
+    assert(k == c.end());
+  }
 #if TEST_STD_VER >= 11
-    {
-        typedef std::unordered_multiset<int, std::hash<int>,
-                                      std::equal_to<int>, min_allocator<int>> C;
-        typedef int P;
-        P a[] =
-        {
-            P(1),
-            P(2),
-            P(3),
-            P(4),
-            P(1),
-            P(2)
-        };
-        C c(a, a + sizeof(a)/sizeof(a[0]));
-        C::const_iterator i = c.find(2);
-        C::const_iterator j = std::next(i, 2);
-        C::iterator k = c.erase(i, i);
-        assert(k == i);
-        assert(c.size() == 6);
-        assert(c.count(1) == 2);
-        assert(c.count(2) == 2);
-        assert(c.count(3) == 1);
-        assert(c.count(4) == 1);
+  {
+    typedef std::unordered_multiset<int, std::hash<int>, std::equal_to<int>, min_allocator<int>> C;
+    typedef int P;
+    P a[] = {P(1), P(2), P(3), P(4), P(1), P(2)};
+    C c(a, a + sizeof(a) / sizeof(a[0]));
+    C::const_iterator i = c.find(2);
+    C::const_iterator j = std::next(i, 2);
+    C::iterator k       = c.erase(i, i);
+    assert(k == i);
+    assert(c.size() == 6);
+    assert(c.count(1) == 2);
+    assert(c.count(2) == 2);
+    assert(c.count(3) == 1);
+    assert(c.count(4) == 1);
 
-        k = c.erase(i, j);
-        assert(c.size() == 4);
-        assert(c.count(1) == 2);
-        assert(c.count(3) == 1);
-        assert(c.count(4) == 1);
+    k = c.erase(i, j);
+    assert(c.size() == 4);
+    assert(c.count(1) == 2);
+    assert(c.count(3) == 1);
+    assert(c.count(4) == 1);
 
-        k = c.erase(c.cbegin(), c.cend());
-        assert(c.size() == 0);
-        assert(k == c.end());
-    }
+    k = c.erase(c.cbegin(), c.cend());
+    assert(c.size() == 0);
+    assert(k == c.end());
+  }
 #endif
 
   return 0;
