711cad3361365ed2ce0cd157879f22c73c11e13d
Farzon Lotfi <farzonlotfi@microsoft.com>
2025-02-11 14:28:22 -0500

    switch from checking name to checking for FunctionDecl

--- clang/test/SemaHLSL/use-cxx-alt-operator-names.hlsl
+++ clang/test/SemaHLSL/use-cxx-alt-operator-names.hlsl
@@ -1,34 +1,34 @@
 // RUN: %clang_cc1 -triple dxil-pc-shadermodel6.3-library %s -ast-dump | FileCheck %s
 
-// CHECK-LABEL: and
+// CHECK: -FunctionDecl {{.*}} and 'void ()'
 void and() {}
 
-// CHECK-LABEL: and_eq
+// CHECK: -FunctionDecl {{.*}} and_eq 'void ()'
 void and_eq() {}
 
-// CHECK-LABEL: bitand
+// CHECK: -FunctionDecl {{.*}} bitand 'void ()'
 void bitand() {}
 
-// CHECK-LABEL: bitor
+// CHECK: -FunctionDecl {{.*}} bitor 'void ()'
 void bitor() {}
 
-// CHECK-LABEL: compl
+// CHECK: -FunctionDecl {{.*}} compl 'void ()'
 void compl() {}
 
-// CHECK-LABEL: not
+// CHECK: -FunctionDecl {{.*}} not 'void ()'
 void not() {}
 
-// CHECK-LABEL: not_eq
+// CHECK: -FunctionDecl {{.*}} not_eq 'void ()'
 void not_eq() {}
 
-// CHECK-LABEL: or
+// CHECK: -FunctionDecl {{.*}} or 'void ()'
 void or() {}
 
-// CHECK-LABEL: or_eq
+// CHECK: -FunctionDecl {{.*}} or_eq 'void ()'
 void or_eq() {}
 
-// CHECK-LABEL: xor
+// CHECK: -FunctionDecl {{.*}} xor 'void ()'
 void xor() {}
 
-// CHECK-LABEL: xor_eq
+// CHECK: -FunctionDecl {{.*}} xor_eq 'void ()'
 void xor_eq() {}
