a0a8fc7c8ea3c50f0762881ada6424c81a24082e
Louis Dionne <ldionne.2@gmail.com>
2025-02-11 06:17:39 +0100

    [libc++][NFC] Run the container tests through clang-format (#126499)
    We've been improving these the tests for vector quite a bit and we are
    probably not done improving our container tests. Formatting everything
    at once will make subsequent reviews easier.

--- libcxx/test/std/containers/unord/unord.map/unord.map.modifiers/emplace_hint.pass.cpp
+++ libcxx/test/std/containers/unord/unord.map/unord.map.modifiers/emplace_hint.pass.cpp
@@ -17,7 +17,6 @@
 // template <class... Args>
 //     iterator emplace_hint(const_iterator p, Args&&... args);
 
-
 #include <unordered_map>
 #include <cassert>
 
@@ -25,53 +24,52 @@
 #include "../../../Emplaceable.h"
 #include "min_allocator.h"
 
-int main(int, char**)
-{
-    {
-        typedef std::unordered_map<int, Emplaceable> C;
-        typedef C::iterator R;
-        C c;
-        C::const_iterator e = c.end();
-        R r = c.emplace_hint(e, std::piecewise_construct, std::forward_as_tuple(3),
-                                                          std::forward_as_tuple());
-        assert(c.size() == 1);
-        assert(r->first == 3);
-        assert(r->second == Emplaceable());
+int main(int, char**) {
+  {
+    typedef std::unordered_map<int, Emplaceable> C;
+    typedef C::iterator R;
+    C c;
+    C::const_iterator e = c.end();
+    R r = c.emplace_hint(e, std::piecewise_construct, std::forward_as_tuple(3), std::forward_as_tuple());
+    assert(c.size() == 1);
+    assert(r->first == 3);
+    assert(r->second == Emplaceable());
 
-        r = c.emplace_hint(c.end(), std::pair<const int, Emplaceable>(4, Emplaceable(5, 6)));
-        assert(c.size() == 2);
-        assert(r->first == 4);
-        assert(r->second == Emplaceable(5, 6));
+    r = c.emplace_hint(c.end(), std::pair<const int, Emplaceable>(4, Emplaceable(5, 6)));
+    assert(c.size() == 2);
+    assert(r->first == 4);
+    assert(r->second == Emplaceable(5, 6));
 
-        r = c.emplace_hint(c.end(), std::piecewise_construct, std::forward_as_tuple(5),
-                                                       std::forward_as_tuple(6, 7));
-        assert(c.size() == 3);
-        assert(r->first == 5);
-        assert(r->second == Emplaceable(6, 7));
-    }
-    {
-        typedef std::unordered_map<int, Emplaceable, std::hash<int>, std::equal_to<int>,
-                            min_allocator<std::pair<const int, Emplaceable>>> C;
-        typedef C::iterator R;
-        C c;
-        C::const_iterator e = c.end();
-        R r = c.emplace_hint(e, std::piecewise_construct, std::forward_as_tuple(3),
-                                                          std::forward_as_tuple());
-        assert(c.size() == 1);
-        assert(r->first == 3);
-        assert(r->second == Emplaceable());
+    r = c.emplace_hint(c.end(), std::piecewise_construct, std::forward_as_tuple(5), std::forward_as_tuple(6, 7));
+    assert(c.size() == 3);
+    assert(r->first == 5);
+    assert(r->second == Emplaceable(6, 7));
+  }
+  {
+    typedef std::unordered_map<int,
+                               Emplaceable,
+                               std::hash<int>,
+                               std::equal_to<int>,
+                               min_allocator<std::pair<const int, Emplaceable>>>
+        C;
+    typedef C::iterator R;
+    C c;
+    C::const_iterator e = c.end();
+    R r = c.emplace_hint(e, std::piecewise_construct, std::forward_as_tuple(3), std::forward_as_tuple());
+    assert(c.size() == 1);
+    assert(r->first == 3);
+    assert(r->second == Emplaceable());
 
-        r = c.emplace_hint(c.end(), std::pair<const int, Emplaceable>(4, Emplaceable(5, 6)));
-        assert(c.size() == 2);
-        assert(r->first == 4);
-        assert(r->second == Emplaceable(5, 6));
+    r = c.emplace_hint(c.end(), std::pair<const int, Emplaceable>(4, Emplaceable(5, 6)));
+    assert(c.size() == 2);
+    assert(r->first == 4);
+    assert(r->second == Emplaceable(5, 6));
 
-        r = c.emplace_hint(c.end(), std::piecewise_construct, std::forward_as_tuple(5),
-                                                       std::forward_as_tuple(6, 7));
-        assert(c.size() == 3);
-        assert(r->first == 5);
-        assert(r->second == Emplaceable(6, 7));
-    }
+    r = c.emplace_hint(c.end(), std::piecewise_construct, std::forward_as_tuple(5), std::forward_as_tuple(6, 7));
+    assert(c.size() == 3);
+    assert(r->first == 5);
+    assert(r->second == Emplaceable(6, 7));
+  }
 
   return 0;
 }
