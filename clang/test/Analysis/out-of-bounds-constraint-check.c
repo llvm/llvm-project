16423d59b1bdc3e1e829717beab6d5d74c369e65
Donát Nagy <donat.nagy@ericsson.com>
2025-02-12 13:41:24 +0100

    Don't match the type from clang_analyzer_value calls

--- clang/test/Analysis/out-of-bounds-constraint-check.c
+++ clang/test/Analysis/out-of-bounds-constraint-check.c
@@ -13,62 +13,62 @@ extern int FiveInts[5];
 
 void int_plus_one(int len) {
   (void)FiveInts[len + 1]; // no-warning
-  clang_analyzer_value(len); // expected-warning {{32s:{ [-1, 3] }}}
+  clang_analyzer_value(len); // expected-warning {{{ [-1, 3] }}}
 }
 
 void int_neutral(int len) {
   (void)FiveInts[len]; // no-warning
-  clang_analyzer_value(len); // expected-warning {{32s:{ [0, 4] }}}
+  clang_analyzer_value(len); // expected-warning {{{ [0, 4] }}}
 }
 
 void int_minus_one(int len) {
   (void)FiveInts[len - 1]; // no-warning
-  clang_analyzer_value(len); // expected-warning {{32s:{ [1, 5] }}}
+  clang_analyzer_value(len); // expected-warning {{{ [1, 5] }}}
 }
 
 void unsigned_plus_one(unsigned len) {
   (void)FiveInts[len + 1]; // no-warning
-  clang_analyzer_value(len); // expected-warning {{32u:{ [0, 3] }}}
+  clang_analyzer_value(len); // expected-warning {{{ [0, 3] }}}
 }
 
 void unsigned_neutral(unsigned len) {
   (void)FiveInts[len]; // no-warning
-  clang_analyzer_value(len); // expected-warning {{32u:{ [0, 4] }}}
+  clang_analyzer_value(len); // expected-warning {{{ [0, 4] }}}
 }
 
 void unsigned_minus_one(unsigned len) {
   (void)FiveInts[len - 1]; // no-warning
-  clang_analyzer_value(len); // expected-warning {{32u:{ [1, 5] }}}
+  clang_analyzer_value(len); // expected-warning {{{ [1, 5] }}}
 }
 
 void ll_plus_one(long long len) {
   (void)FiveInts[len + 1]; // no-warning
-  clang_analyzer_value(len); // expected-warning {{64s:{ [-1, 3] }}}
+  clang_analyzer_value(len); // expected-warning {{{ [-1, 3] }}}
 }
 
 void ll_neutral(long long len) {
   (void)FiveInts[len]; // no-warning
-  clang_analyzer_value(len); // expected-warning {{64s:{ [0, 4] }}}
+  clang_analyzer_value(len); // expected-warning {{{ [0, 4] }}}
 }
 
 void ll_minus_one(long long len) {
   (void)FiveInts[len - 1]; // no-warning
-  clang_analyzer_value(len); // expected-warning {{64s:{ [1, 5] }}}
+  clang_analyzer_value(len); // expected-warning {{{ [1, 5] }}}
 }
 
 void ull_plus_one(unsigned long long len) {
   (void)FiveInts[len + 1]; // no-warning
-  clang_analyzer_value(len); // expected-warning {{64u:{ [0, 3] }}}
+  clang_analyzer_value(len); // expected-warning {{{ [0, 3] }}}
 }
 
 void ull_neutral(unsigned long long len) {
   (void)FiveInts[len]; // no-warning
-  clang_analyzer_value(len); // expected-warning {{64u:{ [0, 4] }}}
+  clang_analyzer_value(len); // expected-warning {{{ [0, 4] }}}
 }
 
 void ull_minus_one(unsigned long long len) {
   (void)FiveInts[len - 1]; // no-warning
-  clang_analyzer_value(len); // expected-warning {{64u:{ [1, 5] }}}
+  clang_analyzer_value(len); // expected-warning {{{ [1, 5] }}}
 }
 
 // Also try the same with a dynamically allocated memory block, because in the
@@ -81,83 +81,83 @@ void free(void *);
 void dyn_int_plus_one(int len) {
   char *p = malloc(5);
   p[len + 1] = 1; // no-warning
-  clang_analyzer_value(len); // expected-warning {{32s:{ [-1, 3] }}}
+  clang_analyzer_value(len); // expected-warning {{{ [-1, 3] }}}
   free(p);
 }
 
 void dyn_int_neutral(int len) {
   char *p = malloc(5);
   p[len] = 1; // no-warning
-  clang_analyzer_value(len); // expected-warning {{32s:{ [0, 4] }}}
+  clang_analyzer_value(len); // expected-warning {{{ [0, 4] }}}
   free(p);
 }
 
 void dyn_int_minus_one(int len) {
   char *p = malloc(5);
   p[len - 1] = 1; // no-warning
-  clang_analyzer_value(len); // expected-warning {{32s:{ [1, 5] }}}
+  clang_analyzer_value(len); // expected-warning {{{ [1, 5] }}}
   free(p);
 }
 
 void dyn_unsigned_plus_one(unsigned len) {
   char *p = malloc(5);
   p[len + 1] = 1; // no-warning
-  clang_analyzer_value(len); // expected-warning {{32u:{ [0, 3] }}}
+  clang_analyzer_value(len); // expected-warning {{{ [0, 3] }}}
   free(p);
 }
 
 void dyn_unsigned_neutral(unsigned len) {
   char *p = malloc(5);
   p[len] = 1; // no-warning
-  clang_analyzer_value(len); // expected-warning {{32u:{ [0, 4] }}}
+  clang_analyzer_value(len); // expected-warning {{{ [0, 4] }}}
   free(p);
 }
 
 void dyn_unsigned_minus_one(unsigned len) {
   char *p = malloc(5);
   p[len - 1] = 1; // no-warning
-  clang_analyzer_value(len); // expected-warning {{32u:{ [1, 5] }}}
+  clang_analyzer_value(len); // expected-warning {{{ [1, 5] }}}
   free(p);
 }
 
 void dyn_ll_plus_one(long long len) {
   char *p = malloc(5);
   p[len + 1] = 1; // no-warning
-  clang_analyzer_value(len); // expected-warning {{64s:{ [-1, 3] }}}
+  clang_analyzer_value(len); // expected-warning {{{ [-1, 3] }}}
   free(p);
 }
 
 void dyn_ll_neutral(long long len) {
   char *p = malloc(5);
   p[len] = 1; // no-warning
-  clang_analyzer_value(len); // expected-warning {{64s:{ [0, 4] }}}
+  clang_analyzer_value(len); // expected-warning {{{ [0, 4] }}}
   free(p);
 }
 
 void dyn_ll_minus_one(long long len) {
   char *p = malloc(5);
   p[len - 1] = 1; // no-warning
-  clang_analyzer_value(len); // expected-warning {{64s:{ [1, 5] }}}
+  clang_analyzer_value(len); // expected-warning {{{ [1, 5] }}}
   free(p);
 }
 
 void dyn_ull_plus_one(unsigned long long len) {
   char *p = malloc(5);
   p[len + 1] = 1; // no-warning
-  clang_analyzer_value(len); // expected-warning {{64u:{ [0, 3] }}}
+  clang_analyzer_value(len); // expected-warning {{{ [0, 3] }}}
   free(p);
 }
 
 void dyn_ull_neutral(unsigned long long len) {
   char *p = malloc(5);
   p[len] = 1; // no-warning
-  clang_analyzer_value(len); // expected-warning {{64u:{ [0, 4] }}}
+  clang_analyzer_value(len); // expected-warning {{{ [0, 4] }}}
   free(p);
 }
 
 void dyn_ull_minus_one(unsigned long long len) {
   char *p = malloc(5);
   p[len - 1] = 1; // no-warning
-  clang_analyzer_value(len); // expected-warning {{64u:{ [1, 5] }}}
+  clang_analyzer_value(len); // expected-warning {{{ [1, 5] }}}
   free(p);
 }
