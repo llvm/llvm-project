52862060ca0fdb60b19b41b54ab36bb7a699bbde
Florian Mayer <fmayer@google.com>
2025-02-12 13:49:47 -0800

    fmt
    Created using spr 1.3.4

--- compiler-rt/lib/sanitizer_common/sanitizer_allocator_local_cache.h
+++ compiler-rt/lib/sanitizer_common/sanitizer_allocator_local_cache.h
@@ -169,7 +169,8 @@ struct SizeClassAllocator32LocalCache {
     // By not doing pointer arithmetic, we avoid the OOB if c->count = 0.
     // We just prefetch the previous member of the PerClass struct, which
     // doesn't do harm.
-    PREFETCH(reinterpret_cast<uptr>(c->batch) + sizeof(c->batch[0])* (c->count - 1));
+    PREFETCH(reinterpret_cast<uptr>(c->batch) +
+             sizeof(c->batch[0]) * (c->count - 1));
     stats_.Add(AllocatorStatAllocated, c->class_size);
     return res;
   }
