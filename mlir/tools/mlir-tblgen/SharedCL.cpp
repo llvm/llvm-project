b06ab32bb5af45cfe5340af60789a988a3f9ebdb
Stef <stef@modular.com>
2025-02-12 00:06:48 +0000

    clang-formt

--- mlir/tools/mlir-tblgen/SharedCL.cpp
+++ mlir/tools/mlir-tblgen/SharedCL.cpp
@@ -15,7 +15,7 @@ namespace cl = llvm::cl;
 static llvm::cl::OptionCategory clShared("Options for all -gen-*");
 
 cl::opt<bool> mlir::tblgen::clUseFallbackTypeIDs = cl::opt<bool>(
-  "use-fallback-type-ids",
-  cl::desc(
-      "Don't generate static TypeID decls; fall back to string comparison."),
-  cl::init(false), cl::cat(clShared));
+    "use-fallback-type-ids",
+    cl::desc(
+        "Don't generate static TypeID decls; fall back to string comparison."),
+    cl::init(false), cl::cat(clShared));
