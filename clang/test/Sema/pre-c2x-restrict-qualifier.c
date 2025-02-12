88b2b07ec02e942afaa0526895e397c3978ca365
Oleksandr T <oleksandr.tarasiuk@outlook.com>
2025-02-12 22:35:59 +0200

    cleanup duplicate error messages

--- clang/test/Sema/pre-c2x-restrict-qualifier.c
+++ clang/test/Sema/pre-c2x-restrict-qualifier.c
@@ -1,6 +1,6 @@
 // RUN: %clang_cc1 -std=c23 -fsyntax-only -verify %s
-// RUN: %clang_cc1 -std=c17 -fsyntax-only -pedantic -verify=pedantic %s
-// RUN: %clang_cc1 -std=c2x -fsyntax-only -Wpre-c2x-compat -verify=c2x-compat %s
+// RUN: %clang_cc1 -std=c17 -fsyntax-only -pedantic -verify=pedantic,expected %s
+// RUN: %clang_cc1 -std=c2x -fsyntax-only -Wpre-c2x-compat -verify=c2x-compat,expected %s
 
 typedef int (*T1)[2];
 restrict T1 t1;
@@ -15,11 +15,7 @@ restrict T3 t3;         // pedantic-warning {{'restrict' qualifier on an array o
 
 typedef int (*t4)();    // pedantic-warning {{a function declaration without a prototype is deprecated in all versions of C}}
 typedef t4 t5[2];
-typedef t5 restrict t6; // expected-error {{pointer to function type 'int (void)' may not be 'restrict' qualified}} \
-                        // pedantic-error {{pointer to function type 'int ()' may not be 'restrict' qualified}} \
-                        // c2x-compat-error {{pointer to function type 'int (void)' may not be 'restrict' qualified}}
+typedef t5 restrict t6; // // expected-error-re {{pointer to function type 'int {{\((void)?\)}}' may not be 'restrict' qualified}}
 
 typedef int t7[2];
-typedef t7 restrict t8; // expected-error {{restrict requires a pointer or reference ('t7' (aka 'int[2]') is invalid)}} \
-                        // pedantic-error {{restrict requires a pointer or reference ('t7' (aka 'int[2]') is invalid)}} \
-                        // c2x-compat-error {{restrict requires a pointer or reference ('t7' (aka 'int[2]') is invalid}}
+typedef t7 restrict t8; // expected-error {{restrict requires a pointer or reference ('t7' (aka 'int[2]') is invalid)}}
