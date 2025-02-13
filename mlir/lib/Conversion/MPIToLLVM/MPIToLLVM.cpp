ede4372738a7ab1bc59989be0fc8d8814a37faa0
Schlimbach, Frank <frank.schlimbach@intel.com>
2025-02-13 16:27:29 +0100

    provide empty register/populate functions even when MPIToLLVM is disabled

--- mlir/lib/Conversion/MPIToLLVM/MPIToLLVM.cpp
+++ mlir/lib/Conversion/MPIToLLVM/MPIToLLVM.cpp
@@ -6,6 +6,9 @@
 //
 //===----------------------------------------------------------------------===//
 
+// skip if no MPI C header was found
+#ifdef FOUND_MPI_C_HEADER
+
 // This must go first (MPI gets confused otherwise)
 #include "MPIImplTraits.h"
 
@@ -330,3 +333,15 @@ void mlir::mpi::registerConvertMPIToLLVMInterface(DialectRegistry &registry) {
     dialect->addInterfaces<FuncToLLVMDialectInterface>();
   });
 }
+
+#else // FOUND_MPI_C_HEADER
+
+#include "mlir/Conversion/MPIToLLVM/MPIToLLVM.h"
+using namespace mlir;
+
+void mlir::mpi::populateMPIToLLVMConversionPatterns(
+    LLVMTypeConverter &converter, RewritePatternSet &patterns) {}
+
+void mlir::mpi::registerConvertMPIToLLVMInterface(DialectRegistry &registry) {}
+
+#endif // FOUND_MPI_C_HEADER
