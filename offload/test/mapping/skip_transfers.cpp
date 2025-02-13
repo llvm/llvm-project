050c5e3333e430d4833b86feb18bb6fbf28ceb93
pradt2 <12902844+pradt2@users.noreply.github.com>
2025-02-13 15:33:37 -0800

    [Offload]: Skip copying of unused kernel-mapped data

--- offload/test/mapping/skip_transfers.cpp
+++ offload/test/mapping/skip_transfers.cpp
@@ -1,6 +1,6 @@
 // clang-format off
 // RUN: %libomptarget-compilexx-generic
-// RUN: env LIBOMPTARGET_INFO=128 %libomptarget-run-generic 2>&1 \
+// RUN: env LIBOMPTARGET_INFO=160 %libomptarget-run-generic 2>&1 \
 // RUN: | %fcheck-generic
 
 // REQUIRES: gpu
@@ -9,13 +9,13 @@
 int main() {
     float DataStack = 0;
 
-    // CHECK-NOT: omptarget device 0 info: from(unknown)[4] is not used and will not be copied
+    // CHECK: omptarget device 0 info: Copying data from device to host, TgtPtr=0x{{.*}}, HstPtr=0x{{.*}}, Size=4, Name=unknown
     #pragma omp target map(from: DataStack)
     {
         DataStack = 1;
     }
 
-    // CHECK-NOT: omptarget device 0 info: to(unknown)[4] is not used and will not be copied
+    // CHECK: omptarget device 0 info: Copying data from host to device, HstPtr=0x{{.*}}, TgtPtr=0x{{.*}}, Size=4, Name=unknown
     #pragma omp target map(always to: DataStack)
     ;
 
@@ -26,13 +26,15 @@ int main() {
     int Size = 16;
     double *Data = new double[Size];
 
-    // CHECK-NOT: omptarget device 0 info: tofrom(unknown)[8] is not used and will not be copied
+    // CHECK: omptarget device 0 info: Copying data from host to device, HstPtr=0x{{.*}}, TgtPtr=0x{{.*}}, Size=8, Name=unknown
+    // CHECK: omptarget device 0 info: Copying data from device to host, TgtPtr=0x{{.*}}, HstPtr=0x{{.*}}, Size=8, Name=unknown
     #pragma omp target map(tofrom: Data[0:1])
     {
         Data[0] = 1;
     }
 
-    // CHECK-NOT: omptarget device 0 info: tofrom(unknown)[16] is not used and will not be copied
+    // CHECK: omptarget device 0 info: Copying data from host to device, HstPtr=0x{{.*}}, TgtPtr=0x{{.*}}, Size=16, Name=unknown
+    // CHECK: omptarget device 0 info: Copying data from device to host, TgtPtr=0x{{.*}}, HstPtr=0x{{.*}}, Size=16, Name=unknown
     #pragma omp target map(always tofrom: Data[0:2])
     ;
 
@@ -48,7 +50,7 @@ int main() {
     #pragma omp target map(tofrom: Data[0:4])
     ;
 
-    // CHECK-NOT: omptarget device 0 info: to(unknown)[40] is not used and will not be copied
+    // CHECK: omptarget device 0 info: Copying data from host to device, HstPtr=0x{{.*}}, TgtPtr=0x{{.*}}, Size=40, Name=unknown
     #pragma omp target map(to: Data[0:5])
     {
         #pragma omp teams
@@ -59,7 +61,8 @@ int main() {
       double *Data;
     } Wrapper { .Data = Data };
 
-    // CHECK-NOT: omptarget device 0 info: tofrom(unknown)[48] is not used and will not be copied
+    // CHECK: omptarget device 0 info: Copying data from host to device, HstPtr=0x{{.*}}, TgtPtr=0x{{.*}}, Size=48, Name=unknown
+    // CHECK: omptarget device 0 info: Copying data from device to host, TgtPtr=0x{{.*}}, HstPtr=0x{{.*}}, Size=48, Name=unknown
     #pragma omp target map(tofrom: Wrapper.Data[0:6])
     {
         Wrapper.Data[0] = 1;
