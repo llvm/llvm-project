d838220c707e242e6402a836418fee48a6e33078
Roland McGrath <mcgrathr@google.com>
2025-02-13 16:07:06 -0800

    [libc] Make hdrgen support macro_header YAML field. (#123265)
    A macro can specify macro_header instead of macro_value to
    indicate that an llvm-libc-macros/ header file is supposed to
    define this macro.  This is used for dlfcn.h, which previously
    bogusly redefined the RTLD_* macros to empty.

--- libc/utils/hdrgen/header.py
+++ libc/utils/hdrgen/header.py
@@ -6,6 +6,8 @@
 #
 # ==-------------------------------------------------------------------------==#
 
+from pathlib import PurePath
+
 
 class HeaderFile:
     def __init__(self, name):
@@ -32,11 +34,26 @@ class HeaderFile:
     def add_function(self, function):
         self.functions.append(function)
 
+    def includes(self):
+        return sorted(
+            {
+                PurePath("llvm-libc-macros") / macro.header
+                for macro in self.macros
+                if macro.header is not None
+            }
+        )
+
     def public_api(self):
-        content = [""]
+        header_dir = PurePath(self.name).parent
+        content = [
+            f'#include "{file.relative_to(header_dir)}"' for file in self.includes()
+        ] + [""]
 
         for macro in self.macros:
-            content.append(f"{macro}\n")
+            # When there is nothing to define, the Macro object converts to str
+            # as an empty string.  Don't emit a blank line for those cases.
+            if str(macro):
+                content.append(f"{macro}\n")
 
         for type_ in self.types:
             content.append(f"{type_}")
@@ -76,11 +93,9 @@ class HeaderFile:
             content.append(f"#endif // {current_guard}")
             content.append("")
 
-        for object in self.objects:
-            content.append(str(object))
+        content.extend(str(object) for object in self.objects)
         if self.objects:
-            content.append("\n__END_C_DECLS")
-        else:
-            content.append("__END_C_DECLS")
+            content.append("")
+        content.append("__END_C_DECLS")
 
         return "\n".join(content)
