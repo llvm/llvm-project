bc3ce69cfa55766f24c7751e33c57874eaf76395
yangxiaobing <yangxiaobing@jwzg.com>
2025-02-14 14:29:16 +0800

    Add license header and UNSUPPORTED

--- libcxx/test/benchmarks/containers/associative/hash_table_find.bench.cpp
+++ libcxx/test/benchmarks/containers/associative/hash_table_find.bench.cpp
@@ -1,3 +1,13 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++03
+
 #include <unordered_set>
 #include <string>
 #include <random>
