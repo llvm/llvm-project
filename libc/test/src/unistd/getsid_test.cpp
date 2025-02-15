a9878c1c88d9631c097599bf61301a9c78a71cb9
ZakyHermawan <zaky.hermawan9615@gmail.com>
2025-02-16 02:20:41 +0700

    test: fix no member named 'libc_errno' on getsid_test
    Signed-off-by: ZakyHermawan <zaky.hermawan9615@gmail.com>

--- libc/test/src/unistd/getsid_test.cpp
+++ libc/test/src/unistd/getsid_test.cpp
@@ -6,6 +6,7 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include "src/errno/libc_errno.h"
 #include "src/unistd/getsid.h"
 #include "test/UnitTest/Test.h"
 
