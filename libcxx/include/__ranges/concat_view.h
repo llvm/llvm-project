012364f908c1caf89a0e21724b62eae9ede89e96
changkhothuychung <nhat7203@gmail.com>
2025-02-16 15:27:49 -0500

    code formatting

--- libcxx/include/__ranges/concat_view.h
+++ libcxx/include/__ranges/concat_view.h
@@ -622,7 +622,8 @@ struct __fn {
     return views::all(std::forward<_Views>(__views)...);
   }
 
-  template <class... _Views> requires(sizeof...(_Views) > 1) && (view<_Views> && ...) && __concatable<_Views...>
+  template <class... _Views> 
+    requires(sizeof...(_Views) > 1) && (view<_Views> && ...) && __concatable<_Views...>
   [[nodiscard]] _LIBCPP_HIDE_FROM_ABI constexpr auto operator()(_Views... __views) const
       noexcept(noexcept(concat_view(std::forward<_Views>(__views)...))) {
     return concat_view(std::forward<_Views>(__views)...);
