9b0da99ddc105f0f70c5628425e54aaff9e754b6
Tom Stellard <tstellar@redhat.com>
2025-02-15 17:45:02 -0800

    Fix formatting

--- flang/runtime/io-api-minimal.cpp
+++ flang/runtime/io-api-minimal.cpp
@@ -151,11 +151,11 @@ bool IODEF(OutputLogical)(Cookie cookie, bool truth) {
 // on the version provided by libc++.
 
 #if !defined(_LIBCPP_VERBOSE_ABORT_NOEXCEPT)
-  #define _LIBCPP_VERBOSE_ABORT_NOEXCEPT
+#define _LIBCPP_VERBOSE_ABORT_NOEXCEPT
 #endif
 
-
-void std::__libcpp_verbose_abort(char const *format, ...) _LIBCPP_VERBOSE_ABORT_NOEXCEPT {
+void std::__libcpp_verbose_abort(
+    char const *format, ...) _LIBCPP_VERBOSE_ABORT_NOEXCEPT {
   va_list list;
   va_start(list, format);
   std::vfprintf(stderr, format, list);
