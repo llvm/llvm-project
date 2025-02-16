c59185194c4eaa3142036117ff2c15398c6a1a28
Balazs Benics <benicsbalazs@gmail.com>
2025-02-16 19:03:08 +0100

    Cleanup header simulator

--- clang/test/Analysis/Inputs/system-header-simulator-cxx-std-locks.h
+++ clang/test/Analysis/Inputs/system-header-simulator-cxx-std-locks.h
@@ -1,17 +1,13 @@
-// This is a fake system header with divide-by-zero bugs introduced in
-// c++ std library functions. We use these bugs to test hard-coded
-// suppression of diagnostics within standard library functions that are known
-// to produce false positives.
-
 #pragma clang system_header
+
 namespace std {
 struct mutex {
-  void lock() {}
-  void unlock() {}
+  void lock();
+  void unlock();
 };
 
 template <typename T> struct lock_guard {
-  lock_guard<T>(std::mutex) {}
-  ~lock_guard<T>() {}
+  lock_guard(std::mutex &);
+  ~lock_guard();
 };
 } // namespace std
