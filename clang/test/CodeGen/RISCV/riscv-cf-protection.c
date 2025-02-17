87067be167bb5b35322fe341b6c0b05fdaf9f834
Ming-Yi Lai <ming-yi.lai@mediatek.com>
2025-02-17 12:02:08 +0800

    fixup: Move tests of macros from CodeGen dir to Preprocessor dir

--- clang/test/CodeGen/RISCV/riscv-cf-protection.c
+++ clang/test/CodeGen/RISCV/riscv-cf-protection.c
@@ -1,27 +1,12 @@
 // RUN: %clang --target=riscv32 -menable-experimental-extensions \
 // RUN: -march=rv32i_zicfilp1p0 -fcf-protection=branch \
-// RUN: -mcf-branch-label-scheme=unlabeled -E -dM %s -o - | FileCheck \
-// RUN: --check-prefixes=LPAD-MACRO,UNLABELED-MACRO %s
-
-// RUN: %clang --target=riscv32 -menable-experimental-extensions \
-// RUN: -march=rv32i_zicfilp1p0 -fcf-protection=branch \
 // RUN: -mcf-branch-label-scheme=unlabeled -S -emit-llvm %s -o - | FileCheck \
 // RUN: --check-prefixes=BRANCH-PROT-FLAG,UNLABELED-FLAG %s
 
 // RUN: %clang --target=riscv32 -menable-experimental-extensions \
 // RUN: -march=rv32i_zicfilp1p0 -fcf-protection=branch \
-// RUN: -mcf-branch-label-scheme=func-sig -E -dM %s -o - | FileCheck \
-// RUN: --check-prefixes=LPAD-MACRO,FUNC-SIG-MACRO %s
-
-// RUN: %clang --target=riscv32 -menable-experimental-extensions \
-// RUN: -march=rv32i_zicfilp1p0 -fcf-protection=branch \
 // RUN: -mcf-branch-label-scheme=func-sig -S -emit-llvm %s -o - | FileCheck \
 // RUN: --check-prefixes=BRANCH-PROT-FLAG,FUNC-SIG-FLAG %s
-
-// RUN: %clang --target=riscv32 -menable-experimental-extensions \
-// RUN: -march=rv32i_zicfilp1p0 -mcf-branch-label-scheme=unlabeled -E -dM %s \
-// RUN: -o - 2>&1 | FileCheck \
-// RUN: --check-prefixes=NO-MACRO,UNLABELED-SCHEME-UNUSED %s
 
 // RUN: %clang --target=riscv32 -menable-experimental-extensions \
 // RUN: -march=rv32i_zicfilp1p0 -mcf-branch-label-scheme=unlabeled -S \
@@ -29,42 +14,21 @@
 // RUN: --check-prefixes=NO-FLAG,UNLABELED-SCHEME-UNUSED %s
 
 // RUN: %clang --target=riscv32 -menable-experimental-extensions \
-// RUN: -march=rv32i_zicfilp1p0 -mcf-branch-label-scheme=func-sig -E -dM %s \
-// RUN: -o - 2>&1 | FileCheck \
-// RUN: --check-prefixes=NO-MACRO,FUNC-SIG-SCHEME-UNUSED %s
-
-// RUN: %clang --target=riscv32 -menable-experimental-extensions \
 // RUN: -march=rv32i_zicfilp1p0 -mcf-branch-label-scheme=func-sig -S \
 // RUN: -emit-llvm %s -o - 2>&1 | FileCheck \
 // RUN: --check-prefixes=NO-FLAG,FUNC-SIG-SCHEME-UNUSED %s
-
-// RUN: %clang --target=riscv32 -fcf-protection=branch \
-// RUN: -mcf-branch-label-scheme=unlabeled -E -dM -emit-llvm %s -o - | \
-// RUN: FileCheck --check-prefixes=LPAD-MACRO,UNLABELED-MACRO %s
 
 // RUN: %clang --target=riscv32 -fcf-protection=branch \
 // RUN: -mcf-branch-label-scheme=unlabeled -S -emit-llvm %s -o - | FileCheck \
 // RUN: --check-prefixes=BRANCH-PROT-FLAG,UNLABELED-FLAG %s
 
 // RUN: %clang --target=riscv32 -fcf-protection=branch \
-// RUN: -mcf-branch-label-scheme=func-sig -E -dM %s -o - | FileCheck \
-// RUN: --check-prefixes=LPAD-MACRO,FUNC-SIG-MACRO %s
-
-// RUN: %clang --target=riscv32 -fcf-protection=branch \
 // RUN: -mcf-branch-label-scheme=func-sig -S -emit-llvm %s -o - | FileCheck \
 // RUN: --check-prefixes=BRANCH-PROT-FLAG,FUNC-SIG-FLAG %s
-
-// RUN: %clang --target=riscv32 -mcf-branch-label-scheme=unlabeled -E -dM %s \
-// RUN: -o - 2>&1 | FileCheck \
-// RUN: --check-prefixes=NO-MACRO,UNLABELED-SCHEME-UNUSED %s
 
 // RUN: %clang --target=riscv32 -mcf-branch-label-scheme=unlabeled -S \
 // RUN: -emit-llvm %s -o - 2>&1 | FileCheck \
 // RUN: --check-prefixes=NO-FLAG,UNLABELED-SCHEME-UNUSED %s
-
-// RUN: %clang --target=riscv32 -mcf-branch-label-scheme=func-sig -E -dM %s \
-// RUN: -o - 2>&1 | FileCheck \
-// RUN: --check-prefixes=NO-MACRO,FUNC-SIG-SCHEME-UNUSED %s
 
 // RUN: %clang --target=riscv32 -mcf-branch-label-scheme=func-sig -S \
 // RUN: -emit-llvm %s -o - 2>&1 | FileCheck \
@@ -72,28 +36,13 @@
 
 // RUN: %clang --target=riscv64 -menable-experimental-extensions \
 // RUN: -march=rv64i_zicfilp1p0 -fcf-protection=branch \
-// RUN: -mcf-branch-label-scheme=unlabeled -E -dM %s -o - | FileCheck \
-// RUN: --check-prefixes=LPAD-MACRO,UNLABELED-MACRO %s
-
-// RUN: %clang --target=riscv64 -menable-experimental-extensions \
-// RUN: -march=rv64i_zicfilp1p0 -fcf-protection=branch \
 // RUN: -mcf-branch-label-scheme=unlabeled -S -emit-llvm %s -o - | FileCheck \
 // RUN: --check-prefixes=BRANCH-PROT-FLAG,UNLABELED-FLAG %s
 
 // RUN: %clang --target=riscv64 -menable-experimental-extensions \
 // RUN: -march=rv64i_zicfilp1p0 -fcf-protection=branch \
-// RUN: -mcf-branch-label-scheme=func-sig -E -dM %s -o - | FileCheck \
-// RUN: --check-prefixes=LPAD-MACRO,FUNC-SIG-MACRO %s
-
-// RUN: %clang --target=riscv64 -menable-experimental-extensions \
-// RUN: -march=rv64i_zicfilp1p0 -fcf-protection=branch \
 // RUN: -mcf-branch-label-scheme=func-sig -S -emit-llvm %s -o - | FileCheck \
 // RUN: --check-prefixes=BRANCH-PROT-FLAG,FUNC-SIG-FLAG %s
-
-// RUN: %clang --target=riscv64 -menable-experimental-extensions \
-// RUN: -march=rv64i_zicfilp1p0 -mcf-branch-label-scheme=unlabeled -E -dM %s \
-// RUN: -o - 2>&1 | FileCheck \
-// RUN: --check-prefixes=NO-MACRO,UNLABELED-SCHEME-UNUSED %s
 
 // RUN: %clang --target=riscv64 -menable-experimental-extensions \
 // RUN: -march=rv64i_zicfilp1p0 -mcf-branch-label-scheme=unlabeled -S \
@@ -101,42 +50,21 @@
 // RUN: --check-prefixes=NO-FLAG,UNLABELED-SCHEME-UNUSED %s
 
 // RUN: %clang --target=riscv64 -menable-experimental-extensions \
-// RUN: -march=rv64i_zicfilp1p0 -mcf-branch-label-scheme=func-sig -E -dM %s \
-// RUN: -o - 2>&1 | FileCheck \
-// RUN: --check-prefixes=NO-MACRO,FUNC-SIG-SCHEME-UNUSED %s
-
-// RUN: %clang --target=riscv64 -menable-experimental-extensions \
 // RUN: -march=rv64i_zicfilp1p0 -mcf-branch-label-scheme=func-sig -S \
 // RUN: -emit-llvm %s -o - 2>&1 | FileCheck \
 // RUN: --check-prefixes=NO-FLAG,FUNC-SIG-SCHEME-UNUSED %s
-
-// RUN: %clang --target=riscv64 -fcf-protection=branch \
-// RUN: -mcf-branch-label-scheme=unlabeled -E -dM %s -o - | FileCheck \
-// RUN: --check-prefixes=LPAD-MACRO,UNLABELED-MACRO %s
 
 // RUN: %clang --target=riscv64 -fcf-protection=branch \
 // RUN: -mcf-branch-label-scheme=unlabeled -S -emit-llvm %s -o - | FileCheck \
 // RUN: --check-prefixes=BRANCH-PROT-FLAG,UNLABELED-FLAG %s
 
 // RUN: %clang --target=riscv64 -fcf-protection=branch \
-// RUN: -mcf-branch-label-scheme=func-sig -E -dM %s -o - | FileCheck \
-// RUN: --check-prefixes=LPAD-MACRO,FUNC-SIG-MACRO %s
-
-// RUN: %clang --target=riscv64 -fcf-protection=branch \
 // RUN: -mcf-branch-label-scheme=func-sig -S -emit-llvm %s -o - | FileCheck \
 // RUN: --check-prefixes=BRANCH-PROT-FLAG,FUNC-SIG-FLAG %s
-
-// RUN: %clang --target=riscv64 -mcf-branch-label-scheme=unlabeled -E -dM %s \
-// RUN: -o - 2>&1 | FileCheck \
-// RUN: --check-prefixes=NO-MACRO,UNLABELED-SCHEME-UNUSED %s
 
 // RUN: %clang --target=riscv64 -mcf-branch-label-scheme=unlabeled -S \
 // RUN: -emit-llvm %s -o - 2>&1 | FileCheck \
 // RUN: --check-prefixes=NO-FLAG,UNLABELED-SCHEME-UNUSED %s
-
-// RUN: %clang --target=riscv64 -mcf-branch-label-scheme=func-sig -E -dM %s \
-// RUN: -o - 2>&1 | FileCheck \
-// RUN: --check-prefixes=NO-MACRO,FUNC-SIG-SCHEME-UNUSED %s
 
 // RUN: %clang --target=riscv64 -mcf-branch-label-scheme=func-sig -S \
 // RUN: -emit-llvm %s -o - 2>&1 | FileCheck \
@@ -154,13 +82,6 @@
 // UNLABELED-SCHEME-UNUSED-SAME: '-mcf-branch-label-scheme=unlabeled'
 // FUNC-SIG-SCHEME-UNUSED: warning: argument unused during compilation:
 // FUNC-SIG-SCHEME-UNUSED-SAME: '-mcf-branch-label-scheme=func-sig'
-
-// LPAD-MACRO: __riscv_landing_pad 1{{$}}
-// UNLABELED-MACRO: __riscv_landing_pad_unlabeled 1{{$}}
-// FUNC-SIG-MACRO: __riscv_landing_pad_func_sig 1{{$}}
-// NO-MACRO-NOT: __riscv_landing_pad
-// NO-MACRO-NOT: __riscv_landing_pad_unlabeled
-// NO-MACRO-NOT: __riscv_landing_pad_func_sig
 
 // BRANCH-PROT-FLAG-DAG: [[P_FLAG:![0-9]+]] = !{i32 8, !"cf-protection-branch", i32 1}
 // UNLABELED-FLAG-DAG: [[S_FLAG:![0-9]+]] = !{i32 1, !"cf-branch-label-scheme", !"unlabeled"}
