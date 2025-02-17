f53dd54f2324517a334ae8e423570ced9f1f7856
Ricardo Jesus <rjj@nvidia.com>
2025-02-17 11:32:47 +0000

    [TableGen] Add support for per-write cycle tunables (#125870)
    This patch adds support for describing per-write resource cycle counts
    for ReadAdvance records via a new optional field called `tunables`.
        
    This makes it possible to declare ReadAdvance records such as:
        
          def : ReadAdvance<Read_C, 1, [Write_A, Write_B], [2]>;
        
    The above will effectively declare two entries in the ReadAdvance
    table for Read_C, one for Write_A with a cycle count of 1+2, and one for
    Write_B with a cycle count of 1+0 (omitted values are assumed 0).
        
    The field `tunables` provides a list of deltas relative to the base
    `cycle` count of the ReadAdvance. Since the field is optional and
    defaults to a list of 0's, this change doesn't affect current targets.

--- llvm/utils/TableGen/SubtargetEmitter.cpp
+++ llvm/utils/TableGen/SubtargetEmitter.cpp
@@ -1308,23 +1308,27 @@ void SubtargetEmitter::genSchedClassTables(const CodeGenProcModel &ProcModel,
       }
       ConstRecVec ValidWrites =
           ReadAdvance->getValueAsListOfDefs("ValidWrites");
-      IdxVec WriteIDs;
+      std::vector<int64_t> CycleTunables =
+          ReadAdvance->getValueAsListOfInts("CycleTunables");
+      std::vector<std::pair<unsigned, int>> WriteIDs;
+      assert(CycleTunables.size() <= ValidWrites.size() && "Bad ReadAdvance");
+      CycleTunables.resize(ValidWrites.size(), 0);
       if (ValidWrites.empty())
-        WriteIDs.push_back(0);
+        WriteIDs.emplace_back(0, 0);
       else {
-        for (const Record *VW : ValidWrites) {
+        for (const auto [VW, CT] : zip_equal(ValidWrites, CycleTunables)) {
           unsigned WriteID = SchedModels.getSchedRWIdx(VW, /*IsRead=*/false);
           assert(WriteID != 0 &&
                  "Expected a valid SchedRW in the list of ValidWrites");
-          WriteIDs.push_back(WriteID);
+          WriteIDs.emplace_back(WriteID, CT);
         }
       }
       llvm::sort(WriteIDs);
-      for (unsigned W : WriteIDs) {
+      for (const auto &[W, T] : WriteIDs) {
         MCReadAdvanceEntry RAEntry;
         RAEntry.UseIdx = UseIdx;
         RAEntry.WriteResourceID = W;
-        RAEntry.Cycles = ReadAdvance->getValueAsInt("Cycles");
+        RAEntry.Cycles = ReadAdvance->getValueAsInt("Cycles") + T;
         ReadAdvanceEntries.push_back(RAEntry);
       }
     }
