9269dc9b068232c3e875f9186dbbb6307f6de072
Ming-Yi Lai <ming-yi.lai@mediatek.com>
2025-02-17 12:21:03 +0800

    fixup: Add tests for default branch label scheme

--- clang/test/Preprocessor/riscv-cf-protection.c
+++ clang/test/Preprocessor/riscv-cf-protection.c
@@ -1,4 +1,8 @@
 // RUN: %clang --target=riscv32 -menable-experimental-extensions \
+// RUN: -march=rv32i_zicfilp1p0 -fcf-protection=branch -E -dM %s -o - | \
+// RUN: FileCheck --check-prefixes=LPAD-MACRO,FUNC-SIG-MACRO %s
+
+// RUN: %clang --target=riscv32 -menable-experimental-extensions \
 // RUN: -march=rv32i_zicfilp1p0 -fcf-protection=branch \
 // RUN: -mcf-branch-label-scheme=unlabeled -E -dM %s -o - | FileCheck \
 // RUN: --check-prefixes=LPAD-MACRO,UNLABELED-MACRO %s
@@ -7,6 +11,10 @@
 // RUN: -march=rv32i_zicfilp1p0 -fcf-protection=branch \
 // RUN: -mcf-branch-label-scheme=func-sig -E -dM %s -o - | FileCheck \
 // RUN: --check-prefixes=LPAD-MACRO,FUNC-SIG-MACRO %s
+
+// RUN: %clang --target=riscv32 -menable-experimental-extensions \
+// RUN: -march=rv32i_zicfilp1p0 -E -dM %s -o - 2>&1 | \
+// RUN: FileCheck --check-prefixes=NO-MACRO %s
 
 // RUN: %clang --target=riscv32 -menable-experimental-extensions \
 // RUN: -march=rv32i_zicfilp1p0 -mcf-branch-label-scheme=unlabeled -E -dM %s \
@@ -16,6 +24,9 @@
 // RUN: -march=rv32i_zicfilp1p0 -mcf-branch-label-scheme=func-sig -E -dM %s \
 // RUN: -o - 2>&1 | FileCheck --check-prefixes=NO-MACRO %s
 
+// RUN: %clang --target=riscv32 -fcf-protection=branch -E -dM -emit-llvm %s \
+// RUN: -o - | FileCheck --check-prefixes=LPAD-MACRO,FUNC-SIG-MACRO %s
+
 // RUN: %clang --target=riscv32 -fcf-protection=branch \
 // RUN: -mcf-branch-label-scheme=unlabeled -E -dM -emit-llvm %s -o - | \
 // RUN: FileCheck --check-prefixes=LPAD-MACRO,UNLABELED-MACRO %s
@@ -24,11 +35,18 @@
 // RUN: -mcf-branch-label-scheme=func-sig -E -dM %s -o - | FileCheck \
 // RUN: --check-prefixes=LPAD-MACRO,FUNC-SIG-MACRO %s
 
+// RUN: %clang --target=riscv32 -E -dM %s -o - 2>&1 | \
+// RUN: FileCheck --check-prefixes=NO-MACRO %s
+
 // RUN: %clang --target=riscv32 -mcf-branch-label-scheme=unlabeled -E -dM %s \
 // RUN: -o - 2>&1 | FileCheck --check-prefixes=NO-MACRO %s
 
 // RUN: %clang --target=riscv32 -mcf-branch-label-scheme=func-sig -E -dM %s \
 // RUN: -o - 2>&1 | FileCheck --check-prefixes=NO-MACRO %s
+
+// RUN: %clang --target=riscv64 -menable-experimental-extensions \
+// RUN: -march=rv64i_zicfilp1p0 -fcf-protection=branch -E -dM %s -o - | \
+// RUN: FileCheck --check-prefixes=LPAD-MACRO,FUNC-SIG-MACRO %s
 
 // RUN: %clang --target=riscv64 -menable-experimental-extensions \
 // RUN: -march=rv64i_zicfilp1p0 -fcf-protection=branch \
@@ -39,6 +57,10 @@
 // RUN: -march=rv64i_zicfilp1p0 -fcf-protection=branch \
 // RUN: -mcf-branch-label-scheme=func-sig -E -dM %s -o - | FileCheck \
 // RUN: --check-prefixes=LPAD-MACRO,FUNC-SIG-MACRO %s
+
+// RUN: %clang --target=riscv64 -menable-experimental-extensions \
+// RUN: -march=rv64i_zicfilp1p0 -E -dM %s -o - 2>&1 | \
+// RUN: FileCheck --check-prefixes=NO-MACRO %s
 
 // RUN: %clang --target=riscv64 -menable-experimental-extensions \
 // RUN: -march=rv64i_zicfilp1p0 -mcf-branch-label-scheme=unlabeled -E -dM %s \
@@ -48,6 +70,9 @@
 // RUN: -march=rv64i_zicfilp1p0 -mcf-branch-label-scheme=func-sig -E -dM %s \
 // RUN: -o - 2>&1 | FileCheck --check-prefixes=NO-MACRO %s
 
+// RUN: %clang --target=riscv64 -fcf-protection=branch -E -dM %s -o - | \
+// RUN: FileCheck --check-prefixes=LPAD-MACRO,FUNC-SIG-MACRO %s
+
 // RUN: %clang --target=riscv64 -fcf-protection=branch \
 // RUN: -mcf-branch-label-scheme=unlabeled -E -dM %s -o - | FileCheck \
 // RUN: --check-prefixes=LPAD-MACRO,UNLABELED-MACRO %s
@@ -55,6 +80,9 @@
 // RUN: %clang --target=riscv64 -fcf-protection=branch \
 // RUN: -mcf-branch-label-scheme=func-sig -E -dM %s -o - | FileCheck \
 // RUN: --check-prefixes=LPAD-MACRO,FUNC-SIG-MACRO %s
+
+// RUN: %clang --target=riscv64 -E -dM %s -o - 2>&1 | \
+// RUN: FileCheck --check-prefixes=NO-MACRO %s
 
 // RUN: %clang --target=riscv64 -mcf-branch-label-scheme=unlabeled -E -dM %s \
 // RUN: -o - 2>&1 | FileCheck --check-prefixes=NO-MACRO %s
