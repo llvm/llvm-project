1a61846294298a6800be225f8fe6c8ebacc0a9d1
tianleli <tianle.l.liu@intel.com>
2025-02-17 15:50:51 +0800

    Add flto condition for the option.

--- clang/lib/Driver/ToolChains/MSVC.cpp
+++ clang/lib/Driver/ToolChains/MSVC.cpp
@@ -232,9 +232,11 @@ void visualstudio::Linker::ConstructJob(Compilation &C, const JobAction &JA,
     }
   }
 
-  if (Arg *A = Args.getLastArg(options::OPT_fprofile_sample_use_EQ))
-    CmdArgs.push_back(Args.MakeArgString(std::string("-lto-sample-profile:") +
-                                         A->getValue()));
+  if (C.getDriver().isUsingLTO()) {
+    if (Arg *A = Args.getLastArg(options::OPT_fprofile_sample_use_EQ))
+      CmdArgs.push_back(Args.MakeArgString(std::string("-lto-sample-profile:") +
+                                           A->getValue()));
+  }
   Args.AddAllArgValues(CmdArgs, options::OPT__SLASH_link);
 
   // Control Flow Guard checks
