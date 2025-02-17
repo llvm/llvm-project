8091299d4c9cbdd46cde0be7ccd766c8c7a70312
Vy Nguyen <vyng@google.com>
2025-02-16 23:13:46 -0500

    Reapply "Make llvm::telemetry::Manager::preDispatch protected.  (#127114)"
    This reverts commit 66465c3b0ab1b32403ad5a1c3114174d87830f54.
    
    New change: added missing return statement.

--- llvm/lib/Telemetry/Telemetry.cpp
+++ llvm/lib/Telemetry/Telemetry.cpp
@@ -1,3 +1,16 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file provides the basic framework for Telemetry.
+/// Refer to its documentation at llvm/docs/Telemetry.rst for more details.
+//===---------------------------------------------------------------------===//
+
 #include "llvm/Telemetry/Telemetry.h"
 
 namespace llvm {
@@ -21,6 +34,8 @@ Error Manager::dispatch(TelemetryInfo *Entry) {
 void Manager::addDestination(std::unique_ptr<Destination> Dest) {
   Destinations.push_back(std::move(Dest));
 }
+
+Error Manager::preDispatch(TelemetryInfo *Entry) { return Error::success(); }
 
 } // namespace telemetry
 } // namespace llvm
