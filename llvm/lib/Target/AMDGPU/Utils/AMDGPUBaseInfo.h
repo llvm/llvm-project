c0d96144e448c21a10ae1ff6b5fe1decc03bbfce
Nikita Popov <npopov@redhat.com>
2025-02-18 17:14:56 +0100

    Revert "[AMDGPU][True16][CodeGen] FLAT_load using D16 pseudo instruction (#114500)"
    This reverts commit 8e397abbbe3cb04e2b3b31340a7d87756992bbcc.
    
    Fails to build with:
    
    llvm/lib/Target/AMDGPU/AMDGPUMCInstLower.cpp:126:37: error: no member named 'OPERAND_LAST' in 'llvm::AMDGPU::OpName'
      126 |   uint16_t OpName = AMDGPU::OpName::OPERAND_LAST;

--- llvm/lib/Target/AMDGPU/Utils/AMDGPUBaseInfo.h
+++ llvm/lib/Target/AMDGPU/Utils/AMDGPUBaseInfo.h
@@ -113,12 +113,6 @@ struct CvtScaleF32_F32F16ToF8F4_Info {
   unsigned Opcode;
 };
 
-struct True16D16Info {
-  unsigned T16Op;
-  unsigned HiOp;
-  unsigned LoOp;
-};
-
 #define GET_MIMGBaseOpcode_DECL
 #define GET_MIMGDim_DECL
 #define GET_MIMGEncoding_DECL
@@ -129,7 +123,6 @@ struct True16D16Info {
 #define GET_MAIInstInfoTable_DECL
 #define GET_isMFMA_F8F6F4Table_DECL
 #define GET_isCvtScaleF32_F32F16ToF8F4Table_DECL
-#define GET_True16D16Table_DECL
 #include "AMDGPUGenSearchableTables.inc"
 
 namespace IsaInfo {
