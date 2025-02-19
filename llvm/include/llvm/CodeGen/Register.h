dd64bc5e9dd686081a478498f52097c97c49edce
Craig Topper <craig.topper@sifive.com>
2025-02-19 10:19:40 -0800

    [MC] Remove MCRegister::isStackSlot. (#127755)
    Stack slots should only be stored in Register. The only caller was
    Register::isStackSlot so just inline it there.

--- llvm/include/llvm/CodeGen/Register.h
+++ llvm/include/llvm/CodeGen/Register.h
@@ -42,11 +42,12 @@ public:
   ///
   /// FIXME: remove in favor of member.
   static constexpr bool isStackSlot(unsigned Reg) {
-    return MCRegister::isStackSlot(Reg);
+    return MCRegister::FirstStackSlot <= Reg &&
+           Reg < MCRegister::VirtualRegFlag;
   }
 
   /// Return true if this is a stack slot.
-  constexpr bool isStack() const { return MCRegister::isStackSlot(Reg); }
+  constexpr bool isStack() const { return isStackSlot(Reg); }
 
   /// Compute the frame index from a register value representing a stack slot.
   static int stackSlot2Index(Register Reg) {
