118cbfdc53bf8111c7c2cfd9489ed7ccc65b438a
Dmitry Nechitaev <nechda6@gmail.com>
2025-02-20 00:01:48 +0300

    Add test

---
+++ llvm/test/tools/llvm-objcopy/MachO/Inputs/macho_sections.s
@@ -0,0 +1,30 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.build_version macos, 11, 0
+	.globl	_main                           ; -- Begin function main
+	.p2align	2
+_main:                                  ; @main
+	.cfi_startproc
+; %bb.0:
+	sub	sp, sp, #32
+	stp	x29, x30, [sp, #16]             ; 16-byte Folded Spill
+	add	x29, sp, #16
+	.cfi_def_cfa w29, 16
+	.cfi_offset w30, -8
+	.cfi_offset w29, -16
+	mov	w8, #0                          ; =0x0
+	str	w8, [sp, #8]                    ; 4-byte Folded Spill
+	stur	wzr, [x29, #-4]
+	adrp	x0, __ZL7storage@PAGE
+	add	x0, x0, __ZL7storage@PAGEOFF
+	bl	__Z3fooPKc
+	ldr	w0, [sp, #8]                    ; 4-byte Folded Reload
+	ldp	x29, x30, [sp, #16]             ; 16-byte Folded Reload
+	add	sp, sp, #32
+	ret
+	.cfi_endproc
+                                        ; -- End function
+	.section	__DATA,__storage
+__ZL7storage:                           ; @_ZL7storage
+	.space	1383                        ; 0x567
+
+.subsections_via_symbols
