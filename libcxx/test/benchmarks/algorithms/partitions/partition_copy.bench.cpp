ac5953888b7539ace6f465cc899119c9f59202bc
Louis Dionne <ldionne.2@gmail.com>
2025-02-19 16:57:08 -0500

    Refactor benchmarks per other PRs

--- libcxx/test/benchmarks/algorithms/partitions/partition_copy.bench.cpp
+++ libcxx/test/benchmarks/algorithms/partitions/partition_copy.bench.cpp
@@ -9,6 +9,7 @@
 // UNSUPPORTED: c++03, c++11, c++14, c++17
 
 #include <algorithm>
+#include <cstddef>
 #include <deque>
 #include <iterator>
 #include <list>
@@ -25,47 +26,46 @@ auto compute_median(auto first, auto last) {
   return *middle;
 }
 
-template <class Container, class Operation>
-void bm(std::string operation_name, Operation partition_copy) {
-  auto bench = [partition_copy](auto& st) {
-    auto const size = st.range(0);
-    using ValueType = typename Container::value_type;
-    Container c;
-    std::generate_n(std::back_inserter(c), size, [] { return Generate<ValueType>::random(); });
-
-    std::vector<ValueType> yes(size), no(size);
-    ValueType median = compute_median(c.begin(), c.end());
-    auto pred        = [median](auto const& element) { return element < median; };
-
-    for ([[maybe_unused]] auto _ : st) {
-      auto result = partition_copy(c.begin(), c.end(), yes.begin(), no.begin(), pred);
-      benchmark::DoNotOptimize(yes);
-      benchmark::DoNotOptimize(no);
-      benchmark::DoNotOptimize(result);
-      benchmark::DoNotOptimize(c);
-      benchmark::ClobberMemory();
-    }
-  };
-  benchmark::RegisterBenchmark(operation_name, bench)->Arg(32)->Arg(1024)->Arg(8192);
-}
-
 int main(int argc, char** argv) {
   auto std_partition_copy = [](auto first, auto last, auto out_yes, auto out_no, auto pred) {
     return std::partition_copy(first, last, out_yes, out_no, pred);
   };
-  auto ranges_partition_copy = [](auto first, auto last, auto out_yes, auto out_no, auto pred) {
-    return std::ranges::partition_copy(first, last, out_yes, out_no, pred);
+
+  auto bm = []<class Container>(std::string name, auto partition_copy) {
+    benchmark::RegisterBenchmark(
+        name,
+        [partition_copy](auto& st) {
+          std::size_t const size = st.range(0);
+          using ValueType        = typename Container::value_type;
+          Container c;
+          std::generate_n(std::back_inserter(c), size, [] { return Generate<ValueType>::random(); });
+
+          std::vector<ValueType> yes(size), no(size);
+          ValueType median = compute_median(c.begin(), c.end());
+          auto pred        = [median](auto const& element) { return element < median; };
+
+          for ([[maybe_unused]] auto _ : st) {
+            benchmark::DoNotOptimize(c);
+            benchmark::DoNotOptimize(yes);
+            benchmark::DoNotOptimize(no);
+            auto result = partition_copy(c.begin(), c.end(), yes.begin(), no.begin(), pred);
+            benchmark::DoNotOptimize(result);
+          }
+        })
+        ->Arg(32)
+        ->Arg(1024)
+        ->Arg(8192);
   };
 
   // std::partition_copy
-  bm<std::vector<int>>("std::partition_copy(vector<int>)", std_partition_copy);
-  bm<std::deque<int>>("std::partition_copy(deque<int>)", std_partition_copy);
-  bm<std::list<int>>("std::partition_copy(list<int>)", std_partition_copy);
+  bm.operator()<std::vector<int>>("std::partition_copy(vector<int>)", std_partition_copy);
+  bm.operator()<std::deque<int>>("std::partition_copy(deque<int>)", std_partition_copy);
+  bm.operator()<std::list<int>>("std::partition_copy(list<int>)", std_partition_copy);
 
   // ranges::partition_copy
-  bm<std::vector<int>>("ranges::partition_copy(vector<int>)", ranges_partition_copy);
-  bm<std::deque<int>>("ranges::partition_copy(deque<int>)", ranges_partition_copy);
-  bm<std::list<int>>("ranges::partition_copy(list<int>)", ranges_partition_copy);
+  bm.operator()<std::vector<int>>("rng::partition_copy(vector<int>)", std::ranges::partition_copy);
+  bm.operator()<std::deque<int>>("rng::partition_copy(deque<int>)", std::ranges::partition_copy);
+  bm.operator()<std::list<int>>("rng::partition_copy(list<int>)", std::ranges::partition_copy);
 
   benchmark::Initialize(&argc, argv);
   benchmark::RunSpecifiedBenchmarks();
