cd657462ec40663896fa60fdabf565625188958f
kbrav <kevin@halys.dev>
2025-02-19 19:51:19 -0500

    run clang-format

--- clang/lib/Sema/SemaDecl.cpp
+++ clang/lib/Sema/SemaDecl.cpp
@@ -14058,9 +14058,9 @@ void Sema::ActOnUninitializedDecl(Decl *RealDecl) {
 
     // C++1z [dcl.dcl]p1 grammar implies that an initializer is mandatory.
     if (isa<DecompositionDecl>(RealDecl)) {
-      Preprocessor  &PP = getPreprocessor();
+      Preprocessor &PP = getPreprocessor();
       SourceManager &SM = Context.getSourceManager();
-      LangOptions    LO = Context.getLangOpts();
+      LangOptions LO = Context.getLangOpts();
 
       // Lexer previously checked for '=' and didn't find it
       // Highlight the token found in its place in the error message
@@ -14068,7 +14068,7 @@ void Sema::ActOnUninitializedDecl(Decl *RealDecl) {
       Lexer::getRawToken(PP.getLastCachedTokenLocation(), Tok, SM, LO);
 
       Diag(Tok.getLocation(), diag::err_decomp_decl_requires_init)
-        << Var << Lexer::getSpelling(Tok, SM, LO);
+          << Var << Lexer::getSpelling(Tok, SM, LO);
       Var->setInvalidDecl();
       return;
     }
