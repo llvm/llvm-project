f8bdbed5b3f12c5e1c38a29cd71df286a9e2725c
David Olsen <dolsen@nvidia.com>
2025-02-19 19:58:12 -0800

    [CIR] Upstream simple function bodies (#127674)
    Enable ClangIR generation for very simple functions. The functions have
    to return `void` or an integral type, contain only compound statements
    or `return` statements, and `return` statement expressions can only be
    integral literals of the correct type. The functions can have
    parameters, but those are currently ignored because there is no way to
    access them.
    
    This change intentionally focuses on breadth (introducing scopes,
    statements, and expressions) rather than depth, because it enables
    people to work on upstreaming in parallel without interference.
    
    The new ClangIR ops in this change are `ReturnOp`, `YieldOp`, `ScopeOp`,
    and `TrapOp`. These operations are complete (except for the
    `ParentOneOf` property) and shouldn't require further upstreaming
    changes. Significant additions were made to `FuncOp`, adding a type and
    a region, but that operation is still far from complete.
    
    The classes `ScalarExprEmitter` and `CIRGenFunction`, along with the
    `emit*` functions in `CIRGenFunction` that generate ClangIR for
    statements, are new in this change. All of these are very incomplete and
    will be filled out in later upstreaming patches.
    
    Existing test `hello.c` is removed and replaced by the new test
    `func-simple.cpp`. This tests all forms of functions that are currently
    supported.

---
+++ clang/lib/CIR/CodeGen/CIRGenExprScalar.cpp
@@ -0,0 +1,70 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// Emit Expr nodes with scalar CIR types as CIR code.
+//
+//===----------------------------------------------------------------------===//
+
+#include "CIRGenFunction.h"
+
+#include "clang/AST/Expr.h"
+#include "clang/AST/StmtVisitor.h"
+
+#include "mlir/IR/Value.h"
+
+#include <cassert>
+
+using namespace clang;
+using namespace clang::CIRGen;
+
+namespace {
+
+class ScalarExprEmitter : public StmtVisitor<ScalarExprEmitter, mlir::Value> {
+  CIRGenFunction &cgf;
+  CIRGenBuilderTy &builder;
+  bool ignoreResultAssign;
+
+public:
+  ScalarExprEmitter(CIRGenFunction &cgf, CIRGenBuilderTy &builder,
+                    bool ira = false)
+      : cgf(cgf), builder(builder), ignoreResultAssign(ira) {}
+
+  //===--------------------------------------------------------------------===//
+  //                            Visitor Methods
+  //===--------------------------------------------------------------------===//
+
+  mlir::Value Visit(Expr *e) {
+    return StmtVisitor<ScalarExprEmitter, mlir::Value>::Visit(e);
+  }
+
+  mlir::Value VisitStmt(Stmt *s) {
+    llvm_unreachable("Statement passed to ScalarExprEmitter");
+  }
+
+  mlir::Value VisitExpr(Expr *e) {
+    cgf.getCIRGenModule().errorNYI(
+        e->getSourceRange(), "scalar expression kind: ", e->getStmtClassName());
+    return {};
+  }
+
+  mlir::Value VisitIntegerLiteral(const IntegerLiteral *e) {
+    mlir::Type type = cgf.convertType(e->getType());
+    return builder.create<cir::ConstantOp>(
+        cgf.getLoc(e->getExprLoc()), type,
+        builder.getAttr<cir::IntAttr>(type, e->getValue()));
+  }
+};
+} // namespace
+
+/// Emit the computation of the specified expression of scalar type.
+mlir::Value CIRGenFunction::emitScalarExpr(const Expr *e) {
+  assert(e && hasScalarEvaluationKind(e->getType()) &&
+         "Invalid scalar expression to emit");
+
+  return ScalarExprEmitter(*this, builder).Visit(const_cast<Expr *>(e));
+}
