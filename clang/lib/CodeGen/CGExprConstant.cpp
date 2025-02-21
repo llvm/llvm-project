269f93fa37dc054c1670c8d2d6b60ca48cf07e93
Yingwei Zheng <dtcxzyw2333@gmail.com>
2025-02-21 12:15:29 +0800

    [Clang][CodeGen] Address review comments. NFC.

--- clang/lib/CodeGen/CGExprConstant.cpp
+++ clang/lib/CodeGen/CGExprConstant.cpp
@@ -1883,10 +1883,10 @@ llvm::Constant *ConstantEmitter::tryEmitPrivateForVarInit(const VarDecl &D) {
 
   // Try to emit the initializer.  Note that this can allow some things that
   // are not allowed by tryEmitPrivateForMemory alone.
-  if (APValue *value = D.evaluateValue()) {
-    if (!value->allowConstexprUnknown())
-      return tryEmitPrivateForMemory(*value, destType);
-  }
+  // Bail out on constexpr-unknown values since they are invalid in CodeGen.
+  if (APValue *value = D.evaluateValue();
+      value && !value->allowConstexprUnknown())
+    return tryEmitPrivateForMemory(*value, destType);
 
   return nullptr;
 }
