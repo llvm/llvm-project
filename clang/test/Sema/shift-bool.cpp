0ff841ab6d2f47aee4dca6752a433d4c22a56988
Oleksandr T <oleksandr.tarasiuk@outlook.com>
2025-02-21 14:49:23 +0200

    add additional test cases

--- clang/test/Sema/shift-bool.cpp
+++ clang/test/Sema/shift-bool.cpp
@@ -4,9 +4,21 @@ void t() {
   int x = 10;
   bool y = true;
 
-  bool a = y >> x;     // expected-warning {{right shifting a `bool` implicitly converts it to 'int'}}
-  bool b = false >> x; // expected-warning {{right shifting a `bool` implicitly converts it to 'int'}}
-  bool c = y >> 5;     // expected-warning {{right shifting a `bool` implicitly converts it to 'int'}}
-  bool d = y >> -1;    // expected-warning {{right shifting a `bool` implicitly converts it to 'int'}}
-  bool e = y >> 0;     // expected-warning {{right shifting a `bool` implicitly converts it to 'int'}}
+  bool a = y << x;
+  bool b = y >> x; // expected-warning {{right shifting a `bool` implicitly converts it to 'int'}}
+
+  bool c = false << x;
+  bool d = false >> x; // expected-warning {{right shifting a `bool` implicitly converts it to 'int'}}
+
+  bool e = y << 1;
+  bool f = y >> 1; // expected-warning {{right shifting a `bool` implicitly converts it to 'int'}}
+
+  bool g = y << -1; // expected-warning {{shift count is negative}}
+  bool h = y >> -1; // expected-warning {{right shifting a `bool` implicitly converts it to 'int'}}
+
+  bool i = y << 0;
+  bool j = y >> 0; // expected-warning {{right shifting a `bool` implicitly converts it to 'int'}}
+
+  if ((y << 1) != 0) { }
+  if ((y >> 1) != 0) { } // expected-warning {{right shifting a `bool` implicitly converts it to 'int'}}
 }
