caebb7b7043cee73c33e1167b449cd4e9acf539e
Lakshay Kumar <lakshayk@nvidia.com>
2025-02-20 21:40:27 -0800

    Deleted Indvidual testfiles

--- llvm/test/tools/llvm-exegesis/AArch64/reg_based/reg_ppr.s
+++
@@ -1,8 +0,0 @@
-# RUN: llvm-exegesis -mcpu=neoverse-v2 -mode=latency -opcode-name=FADDV_VPZ_D | FileCheck %s
-# REQUIRES: aarch64-registered-target
-
-# Check that warning of not initializing registers is not printed
-# CHECK-NOT: setRegTo is not implemented, results will be unreliable
-
-# Check that we add ret (bx lr) instr to snippet
-# CHECK: assembled_snippet: {{.*}}C0035FD6
