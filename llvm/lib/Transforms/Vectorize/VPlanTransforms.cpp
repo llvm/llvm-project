09528bea36ee1557fd41deef8cc50e44889c8704
Hari Limaye <hari.limaye@arm.com>
2025-02-21 11:54:30 +0000

    Address Review Comments
    - Use cast rather than dyn_cast
    - Remove unrolling test
    - Be explicit about APInt type.
    - Add comment

--- llvm/lib/Transforms/Vectorize/VPlanTransforms.cpp
+++ llvm/lib/Transforms/Vectorize/VPlanTransforms.cpp
@@ -1028,10 +1028,10 @@ static bool optimizeVectorInductionWidthForTCAndVFUF(VPlan &Plan,
 
   // Calculate the widest type required for known TC, VF and UF.
   auto ComputeBitWidth = [](APInt TC, uint64_t Align) {
-    auto AlignedTC =
+    APInt AlignedTC =
         Align * APIntOps::RoundingUDiv(TC, APInt(TC.getBitWidth(), Align),
                                        APInt::Rounding::UP);
-    auto MaxVal = AlignedTC - 1;
+    APInt MaxVal = AlignedTC - 1;
     return std::max<unsigned>(PowerOf2Ceil(MaxVal.getActiveBits()), 8);
   };
   unsigned NewBitWidth =
@@ -1045,6 +1045,10 @@ static bool optimizeVectorInductionWidthForTCAndVFUF(VPlan &Plan,
   VPBasicBlock *HeaderVPBB = Plan.getVectorLoopRegion()->getEntryBasicBlock();
   for (VPRecipeBase &Phi : HeaderVPBB->phis()) {
     auto *WideIV = dyn_cast<VPWidenIntOrFpInductionRecipe>(&Phi);
+
+    // Currently only handle canonical IVs as it is trivial to replace the start
+    // and stop values, and we only perform the optimisation when the IV is only
+    // used by the comparison controlling loop control-flow.
     if (!WideIV || !WideIV->isCanonical() ||
         WideIV->hasMoreThanOneUniqueUser() ||
         NewIVTy == WideIV->getScalarType())
@@ -1068,7 +1072,7 @@ static bool optimizeVectorInductionWidthForTCAndVFUF(VPlan &Plan,
     auto *NewBTC = new VPWidenCastRecipe(
         Instruction::Trunc, Plan.getOrCreateBackedgeTakenCount(), NewIVTy);
     Plan.getVectorPreheader()->appendRecipe(NewBTC);
-    auto *Cmp = dyn_cast<VPInstruction>(*WideIV->user_begin());
+    auto *Cmp = cast<VPInstruction>(*WideIV->user_begin());
     Cmp->setOperand(1, NewBTC);
 
     MadeChange = true;
