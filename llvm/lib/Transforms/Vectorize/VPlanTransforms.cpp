92e45cdd100c2ba196114e6edfa1c9217b8d6640
Florian Hahn <flo@fhahn.com>
2025-02-22 19:15:32 +0000

    [VPlan] Move predication to VPlanTransform (NFC) (WIP).
    This patch moves the logic to predicate and linearize a VPlan to a
    dedicated VPlan transform.
    
    The main logic to perform predication is ready to review, although
    there are few things to note that should be improved, either directly in
    the PR or in the future:
     * Edge and block masks are cached in VPRecipeBuilder, so they can be
       accessed during recipe construction. A better alternative may be to
       add mask operands to all VPInstructions that need them and use that
       during recipe construction
     * The mask caching in a map also means that this map needs updating
       each time a new recipe replaces a VPInstruction; this would also be
       handled by adding mask operands.
    
    Currently this is still WIP due to early-exit loop handling not working
    due to the exit conditions not being available in the initial VPlans.
    This will be fixed with https://github.com/llvm/llvm-project/pull/128419
    and follow-ups
    
    All tests except early-exit loops are passing

--- llvm/lib/Transforms/Vectorize/VPlanTransforms.cpp
+++ llvm/lib/Transforms/Vectorize/VPlanTransforms.cpp
@@ -2062,8 +2062,9 @@ void VPlanTransforms::handleUncountableEarlyExit(
         !OrigLoop->contains(TrueSucc) ? TrueSucc : FalseSucc);
   }
 
-  VPValue *EarlyExitNotTakenCond = RecipeBuilder.getBlockInMask(
-      OrigLoop->contains(TrueSucc) ? TrueSucc : FalseSucc);
+  VPValue *EarlyExitNotTakenCond =
+      RecipeBuilder.getBlockInMask(cast<VPBasicBlock>(
+          Plan.getVectorLoopRegion()->getEntry()->getSingleSuccessor()));
   auto *EarlyExitTakenCond = Builder.createNot(EarlyExitNotTakenCond);
   IsEarlyExitTaken =
       Builder.createNaryOp(VPInstruction::AnyOf, {EarlyExitTakenCond});
