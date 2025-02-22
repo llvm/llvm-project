52007035fa08e6e2434d27bb6d2be2e4eb7d9f36
Mariusz Borsa <m_borsa@apple.com>
2025-02-21 16:25:38 -0800

    Skip more LLDB Swift ASAN libsanitizer tests
    Disable TestMemoryHistory.py, TestReportData.py temporarily until blocking problem resolved
    
    rdar://142836595

--- lldb/test/API/functionalities/asan/TestMemoryHistory.py
+++ lldb/test/API/functionalities/asan/TestMemoryHistory.py
@@ -19,6 +19,7 @@ class AsanTestCase(TestBase):
         self.asan_tests()
 
     @skipIf(oslist=no_match(["macosx"]))
+    @skipIfDarwin #  rdar://142836595
     def test_libsanitizers_asan(self):
         try:
             self.build(make_targets=["libsanitizers"])
