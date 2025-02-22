3f05e477328c3fbcfd8583ff38c4b40ff39ef03e
Matt Arsenault <Matthew.Arsenault@amd.com>
2025-02-22 10:02:04 +0700

    Reapply "RegAlloc: Fix verifier error after failed allocation (#119690)"
    This reverts commit 0c50054820799578be8f62b6fd2cc3fbc751c01e.
    
    Reapply with more fixes to avoid expensive_checks failures. Make sure to
    call splitSeparateComponents after shrinkToUses, and update the VirtRegMap
    with the split registers. Also set undef on all physical register aliases to
    the assigned register.

--- llvm/lib/CodeGen/RegAllocBase.h
+++ llvm/lib/CodeGen/RegAllocBase.h
@@ -37,6 +37,7 @@
 #define LLVM_LIB_CODEGEN_REGALLOCBASE_H
 
 #include "llvm/ADT/SmallPtrSet.h"
+#include "llvm/ADT/SmallSet.h"
 #include "llvm/CodeGen/MachineRegisterInfo.h"
 #include "llvm/CodeGen/RegAllocCommon.h"
 #include "llvm/CodeGen/RegisterClassInfo.h"
@@ -81,6 +82,7 @@ protected:
   /// always available for the remat of all the siblings of the original reg.
   SmallPtrSet<MachineInstr *, 32> DeadRemats;
 
+  SmallSet<Register, 2> FailedVRegs;
   RegAllocBase(const RegAllocFilterFunc F = nullptr)
       : shouldAllocateRegisterImpl(F) {}
 
@@ -103,6 +105,10 @@ protected:
   // Include spiller post optimization and removing dead defs left because of
   // rematerialization.
   virtual void postOptimization();
+
+  /// Perform cleanups on registers that failed to allocate. This hacks on the
+  /// liveness in order to avoid spurious verifier errors in later passes.
+  void cleanupFailedVRegs();
 
   // Get a temporary reference to a Spiller instance.
   virtual Spiller &spiller() = 0;
