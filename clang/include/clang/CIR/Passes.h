945b7c527647be1f40947736f2fdf108478763ae
Andy Kaylor <akaylor@nvidia.com>
2025-02-21 17:54:42 -0800

    Fix file headers

--- clang/include/clang/CIR/Passes.h
+++ clang/include/clang/CIR/Passes.h
@@ -1,3 +1,15 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file exposes the entry points to create compiler passes for ClangIR.
+//
+//===----------------------------------------------------------------------===//
+
 #ifndef CLANG_CIR_PASSES_H
 #define CLANG_CIR_PASSES_H
 
