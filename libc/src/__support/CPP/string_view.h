293e112ff9937e30983803b5d21276d0b57a968d
Vinay Deshmukh <32487576+vinay-deshmukh@users.noreply.github.com>
2025-02-23 00:11:36 -0500

    use cpp over macro

--- libc/src/__support/CPP/string_view.h
+++ libc/src/__support/CPP/string_view.h
@@ -9,6 +9,7 @@
 #ifndef LLVM_LIBC_SRC___SUPPORT_CPP_STRING_VIEW_H
 #define LLVM_LIBC_SRC___SUPPORT_CPP_STRING_VIEW_H
 
+#include "limits.h"
 #include "src/__support/common.h"
 #include "src/__support/macros/config.h"
 
@@ -61,7 +62,7 @@ public:
 
   // special value equal to the maximum value representable by the type
   // size_type.
-  LIBC_INLINE_VAR static constexpr size_t npos = SIZE_MAX;
+  LIBC_INLINE_VAR static constexpr size_t npos = cpp::numeric_limits<size_t>::max();
 
   LIBC_INLINE constexpr string_view() : Data(nullptr), Len(0) {}
 
