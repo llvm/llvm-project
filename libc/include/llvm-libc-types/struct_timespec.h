9228a3b437d93d68c85ccec69e637f8fb5e30eaa
krishna2803 <kpandey81930@gmail.com>
2025-02-23 18:36:48 +0530

    revert: "fix: attempt fix for `struct_timespec`"
    This reverts commit de1eeadc09707fb1848c20cfde13412b2650d93a.
    
    Signed-off-by: krishna2803 <kpandey81930@gmail.com>

--- libc/include/llvm-libc-types/struct_timespec.h
+++ libc/include/llvm-libc-types/struct_timespec.h
@@ -11,13 +11,10 @@
 
 #include "time_t.h"
 
-#ifndef _STRUCT_TIMESPEC
-#define _STRUCT_TIMESPEC 1
 struct timespec {
   time_t tv_sec; /* Seconds.  */
   /* TODO: BIG_ENDIAN may require padding. */
   long tv_nsec; /* Nanoseconds.  */
 };
-#endif // _STRUCT_TIMESPEC
 
 #endif // LLVM_LIBC_TYPES_STRUCT_TIMESPEC_H
