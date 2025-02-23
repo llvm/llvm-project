06fede0c6bce33379560dc7719a114e23f5a0b7a
Chris Copeland <chris@chrisnc.net>
2025-02-23 00:01:26 -0800

    [ARM] Emit a warning when the hard-float ABI is enabled but can't be used.
    Compiling for an eabihf target with a CPU lacking floating-point
    registers will silently use the soft-float ABI instead, even though the
    Arm attributes section still has Tag_ABI_VFP_args: VFP registers, which
    leads to silent ABI mismatches at link time.
    
    Fixes #110383.

--- llvm/lib/Target/ARM/ARMISelLowering.cpp
+++ llvm/lib/Target/ARM/ARMISelLowering.cpp
@@ -791,7 +791,9 @@ ARMTargetLowering::ARMTargetLowering(const TargetMachine &TM,
       setAllExpand(MVT::f32);
     if (!Subtarget->hasFP64())
       setAllExpand(MVT::f64);
-  }
+  } else if (TM.Options.FloatABIType == FloatABI::Hard)
+    errs() << "The hard-float ABI can't be used for a target that "
+              "doesn't support floating-point (ignoring float-abi)\n";
 
   if (Subtarget->hasFullFP16()) {
     addRegisterClass(MVT::f16, &ARM::HPRRegClass);
