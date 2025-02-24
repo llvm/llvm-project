230aade9955299a8bf4e7327156ab6c9d950389e
Lakshay Kumar <lakshayk@nvidia.com>
2025-02-23 20:50:00 -0800

    Modified: requirement(aarch64) check only required once for a test

--- llvm/test/tools/llvm-exegesis/AArch64/setReg_init_check.s
+++ llvm/test/tools/llvm-exegesis/AArch64/setReg_init_check.s
@@ -1,14 +1,14 @@
+# REQUIRES: aarch64-registered-target
+
 # ppr register class initialization testcase 
 # ideally we should use PTRUE_{B/H?S/D} instead of FADDV_VPZ_D for isolated testcase; but exegesis does not support PTRUE_{B/H?S/D} yet;
 # RUN: llvm-exegesis -mcpu=neoverse-v2 -mode=latency -opcode-name=FADDV_VPZ_D 2>&1 | FileCheck %s --check-prefix=PPR
-# REQUIRES: aarch64-registered-target
 # PPR-NOT: setRegTo is not implemented, results will be unreliable
 # PPR: assembled_snippet: {{.*}}C0035FD6
 
 # zpr register class initialization testcase 
 # ideally we should use DUPM_ZI instead of FADDV_VPZ_S for isolated testcase; but exegesis does not support DUPM_ZI yet;
 # RUN: llvm-exegesis -mcpu=neoverse-v2 -mode=latency -opcode-name=FADDV_VPZ_S 2>&1 | FileCheck %s --check-prefix=ZPR
-# REQUIRES: aarch64-registered-target
 # ZPR-NOT: setRegTo is not implemented, results will be unreliable
 # ZPR: assembled_snippet: {{.*}}C0035FD6
 
@@ -19,6 +19,5 @@
 
 # fpr128 register class initialization testcase
 # RUN: llvm-exegesis -mcpu=neoverse-v2 -mode=latency -opcode-name=ADDVv16i8v 2>&1 | FileCheck %s --check-prefix=FPR128
-# REQUIRES: aarch64-registered-target
 # FPR128-NOT: setRegTo is not implemented, results will be unreliable
 # FPR128: assembled_snippet: {{.*}}C0035FD6
