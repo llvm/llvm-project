62d9df3c2c7a7e6f44e64b6d3e0f58cb9b19a359
Matt Arsenault <Matthew.Arsenault@amd.com>
2025-02-24 18:07:59 +0700

    InstCombine: Remove a check for pointer bitcasts

--- llvm/lib/Transforms/InstCombine/InstructionCombining.cpp
+++ llvm/lib/Transforms/InstCombine/InstructionCombining.cpp
@@ -3661,8 +3661,7 @@ Instruction *InstCombinerImpl::visitUnconditionalBranchInst(BranchInst &BI) {
 
   auto GetLastSinkableStore = [](BasicBlock::iterator BBI) {
     auto IsNoopInstrForStoreMerging = [](BasicBlock::iterator BBI) {
-      return BBI->isDebugOrPseudoInst() ||
-             (isa<BitCastInst>(BBI) && BBI->getType()->isPointerTy());
+      return BBI->isDebugOrPseudoInst();
     };
 
     BasicBlock::iterator FirstInstr = BBI->getParent()->begin();
