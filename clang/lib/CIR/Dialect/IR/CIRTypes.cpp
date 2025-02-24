bbadebdf7444ed453721f064e4e30ac2917c5fe9
David Olsen <dolsen@nvidia.com>
2025-02-24 15:50:19 -0800

    [CIR] Upstream type `bool`
    Support the type `bool` and the literals `true` and `false`.
    Add the type `cir::BoolType` and the attribute `cir::BoolAttr` to
    ClangIR.  Add code in all the necessary places in ClangIR CodeGen
    to handle and to recognize the type and the attribute.
    
    Add test cases to existing tests func-simple.cpp and
    global-var-simple.cpp.

--- clang/lib/CIR/Dialect/IR/CIRTypes.cpp
+++ clang/lib/CIR/Dialect/IR/CIRTypes.cpp
@@ -382,6 +382,34 @@ llvm::ArrayRef<mlir::Type> FuncType::getReturnTypes() const {
 bool FuncType::isVoid() const { return mlir::isa<VoidType>(getReturnType()); }
 
 //===----------------------------------------------------------------------===//
+// BoolType
+//===----------------------------------------------------------------------===//
+
+Type BoolType::parse(mlir::AsmParser &parser) {
+  return get(parser.getContext());
+}
+
+void BoolType::print(mlir::AsmPrinter &printer) const {}
+
+llvm::TypeSize
+BoolType::getTypeSizeInBits(const ::mlir::DataLayout &dataLayout,
+                            ::mlir::DataLayoutEntryListRef params) const {
+  return llvm::TypeSize::getFixed(8);
+}
+
+uint64_t
+BoolType::getABIAlignment(const ::mlir::DataLayout &dataLayout,
+                          ::mlir::DataLayoutEntryListRef params) const {
+  return 1;
+}
+
+uint64_t
+BoolType::getPreferredAlignment(const ::mlir::DataLayout &dataLayout,
+                                ::mlir::DataLayoutEntryListRef params) const {
+  return 1;
+}
+
+//===----------------------------------------------------------------------===//
 // PointerType Definitions
 //===----------------------------------------------------------------------===//
 
