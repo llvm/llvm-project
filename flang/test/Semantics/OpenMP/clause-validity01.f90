656864e62a088d432e25b8b62a3ed2a1ef74c7c9
Thirumalai-Shaktivel <thirumalaishaktivel@gmail.com>
2025-02-24 07:32:33 +0000

    Fix failures

--- flang/test/Semantics/OpenMP/clause-validity01.f90
+++ flang/test/Semantics/OpenMP/clause-validity01.f90
@@ -330,10 +330,13 @@ use omp_lib
   !$omp parallel
   b = 1
   !ERROR: LASTPRIVATE clause is not allowed on the SINGLE directive
+  !ERROR: NOWAIT clause must not be used with COPYPRIVATE clause on the SINGLE directive
   !$omp single private(a) lastprivate(c) nowait
   a = 3.14
   !ERROR: Clause NOWAIT is not allowed if clause COPYPRIVATE appears on the END SINGLE directive
   !ERROR: COPYPRIVATE variable 'a' may not appear on a PRIVATE or FIRSTPRIVATE clause on a SINGLE construct
+  !ERROR: At most one NOWAIT clause can appear on the SINGLE directive
+  !ERROR: At most one NOWAIT clause can appear on the SINGLE directive
   !ERROR: At most one NOWAIT clause can appear on the END SINGLE directive
   !$omp end single copyprivate(a) nowait nowait
   c = 2
