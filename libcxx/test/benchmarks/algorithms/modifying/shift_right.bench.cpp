13789b4ebc1b991e9f65cbe644740221f60962cd
Louis Dionne <ldionne.2@gmail.com>
2025-02-25 11:13:37 -0500

    Address comments on shift_{left,right} benchmarks

--- libcxx/test/benchmarks/algorithms/modifying/shift_right.bench.cpp
+++ libcxx/test/benchmarks/algorithms/modifying/shift_right.bench.cpp
@@ -14,6 +14,7 @@
 #include <iterator>
 #include <list>
 #include <string>
+#include <utility>
 #include <vector>
 
 #include "benchmark/benchmark.h"
@@ -22,7 +23,7 @@
 int main(int argc, char** argv) {
   auto std_shift_right = [](auto first, auto last, auto n) { return std::shift_right(first, last, n); };
 
-  // std::shift_right(normal container)
+  // Benchmark std::shift_right where we shift exactly one element, which is the worst case.
   {
     auto bm = []<class Container>(std::string name, auto shift_right) {
       benchmark::RegisterBenchmark(
@@ -32,12 +33,19 @@ int main(int argc, char** argv) {
             using ValueType        = typename Container::value_type;
             Container c;
             std::generate_n(std::back_inserter(c), size, [] { return Generate<ValueType>::random(); });
+            std::size_t n = 1;
 
-            auto const n = 9 * (size / 10); // shift all but 10% of the range
+            // To avoid ending up with a fully moved-from range, restore the element that gets
+            // overwritten by the shift after performing the shift.
+            auto first_element = c.begin();
+            auto last_element  = std::next(c.begin(), size - 1);
 
             for ([[maybe_unused]] auto _ : st) {
               benchmark::DoNotOptimize(c);
-              auto result = shift_right(c.begin(), c.end(), n);
+              benchmark::DoNotOptimize(n);
+              ValueType tmp  = *last_element;
+              auto result    = shift_right(c.begin(), c.end(), n);
+              *first_element = std::move(tmp);
               benchmark::DoNotOptimize(result);
             }
           })
