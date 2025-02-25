febc806d9ccd72e99f05fc1f3d6098e756084aa4
Nikolas Klauser <nikolasklauser@berlin.de>
2025-02-25 19:52:57 +0100

    [libc++] Add warning groups to diagnose_if when available

--- libcxx/test/libcxx/containers/associative/non_const_comparator.verify.cpp
+++ libcxx/test/libcxx/containers/associative/non_const_comparator.verify.cpp
@@ -27,22 +27,20 @@ void f() {
   static_assert(!std::__is_invocable_v<BadCompare const&, int const&, int const&>, "");
   static_assert(std::__is_invocable_v<BadCompare&, int const&, int const&>, "");
 
-  // expected-warning@set:* 2 {{the specified comparator type does not provide a viable const call operator}}
-  // expected-warning@map:* 2 {{the specified comparator type does not provide a viable const call operator}}
   {
     using C = std::set<int, BadCompare>;
-    C s;
+    C s; // expected-warning {{the specified comparator type does not provide a viable const call operator}}
   }
   {
     using C = std::multiset<long, BadCompare>;
-    C s;
+    C s; // expected-warning {{the specified comparator type does not provide a viable const call operator}}
   }
   {
     using C = std::map<int, int, BadCompare>;
-    C s;
+    C s; // expected-warning {{the specified comparator type does not provide a viable const call operator}}
   }
   {
     using C = std::multimap<long, int, BadCompare>;
-    C s;
+    C s; // expected-warning {{the specified comparator type does not provide a viable const call operator}}
   }
 }
