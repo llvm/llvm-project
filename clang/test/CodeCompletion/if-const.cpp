3d89bc08d089aaea717351071185cca736e995da
Letu Ren <fantasquex@gmail.com>
2025-02-25 14:12:28 +0800

    Add if not test

--- clang/test/CodeCompletion/if-const.cpp
+++ clang/test/CodeCompletion/if-const.cpp
@@ -1,6 +1,13 @@
+template <bool Flag>
 void test() {
-  if c
-  // RUN: %clang_cc1 -fsyntax-only -std=c++17 -code-completion-at=%s:2:7 %s -o - | FileCheck -check-prefix=CHECK-CXX17 %s
-  // RUN: %clang_cc1 -fsyntax-only -std=c++23 -code-completion-at=%s:2:7 %s -o - | FileCheck -check-prefix=CHECK-CXX23 %s
+  if constexpr (Flag) {
+    return;
+  }
+  // RUN: %clang_cc1 -fsyntax-only -std=c++17 -code-completion-at=%s:3:7 %s -o - | FileCheck -check-prefix=CHECK-CXX17 %s
+  // RUN: %clang_cc1 -fsyntax-only -std=c++23 -code-completion-at=%s:3:7 %s -o - | FileCheck -check-prefix=CHECK-CXX23 %s
   // CHECK-CXX17: constexpr
   // CHECK-CXX23: consteval
+  if !c
+  // RUN: %clang_cc1 -fsyntax-only -std=c++23 -code-completion-at=%s:10:8 %s -o - | FileCheck -check-prefix=CHECK-CXX23-NOT %s
+  // CHECK-CXX23-NOT: consteval
+}
