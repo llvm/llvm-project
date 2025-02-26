bb079b28ec303a2b55dbc2d8efb04126b2677f74
Jacob Lalonde <jalalonde@fb.com>
2025-02-26 12:00:06 -0800

    Refactor tests to be concise and add progresse end, add additional check to sbprogress description check

--- lldb/test/API/tools/lldb-dap/progress/TestDAP_Progress.py
+++ lldb/test/API/tools/lldb-dap/progress/TestDAP_Progress.py
@@ -12,6 +12,41 @@ import lldbdap_testcase
 
 
 class TestDAP_progress(lldbdap_testcase.DAPTestCaseBase):
+    def verify_progress_events(
+        self,
+        expected_title,
+        expected_message=None,
+        expected_not_in_message=None,
+        only_verify_first_update=False,
+    ):
+        self.dap_server.wait_for_event("progressEnd", 15)
+        self.assertTrue(len(self.dap_server.progress_events) > 0)
+        start_found = False
+        update_found = False
+        end_found = False
+        for event in self.dap_server.progress_events:
+            event_type = event["event"]
+            if "progressStart" in event_type:
+                title = event["body"]["title"]
+                self.assertIn(expected_title, title)
+                start_found = True
+            if "progressUpdate" in event_type:
+                message = event["body"]["message"]
+                print(f"Progress update: {message}")
+                if only_verify_first_update and update_found:
+                    continue
+                if expected_message is not None:
+                    self.assertIn(expected_message, message)
+                if expected_not_in_message is not None:
+                    self.assertNotIn(expected_not_in_message, message)
+                update_found = True
+            if "progressEnd" in event_type:
+                end_found = True
+
+        self.assertTrue(start_found)
+        self.assertTrue(update_found)
+        self.assertTrue(end_found)
+
     @skipIfWindows
     def test_output(self):
         program = self.getBuildArtifact("a.out")
@@ -30,28 +65,10 @@ class TestDAP_progress(lldbdap_testcase.DAPTestCaseBase):
             "`test-progress --total 3 --seconds 1", context="repl"
         )
 
-        self.dap_server.wait_for_event("progressEnd", 15)
-        # Expect at least a start, an update, and end event
-        # However because the underlying Progress instance is an RAII object and we can't guaruntee
-        # it's deterministic destruction in the python API, we verify just start and update
-        # otherwise this test could be flakey.
-        self.assertTrue(len(self.dap_server.progress_events) > 0)
-        start_found = False
-        update_found = False
-        for event in self.dap_server.progress_events:
-            event_type = event["event"]
-            if "progressStart" in event_type:
-                title = event["body"]["title"]
-                self.assertIn("Progress tester", title)
-                start_found = True
-            if "progressUpdate" in event_type:
-                message = event["body"]["message"]
-                print(f"Progress update: {message}")
-                self.assertNotIn("Progres tester", message)
-                update_found = True
-
-        self.assertTrue(start_found)
-        self.assertTrue(update_found)
+        self.verify_progress_events(
+            expected_title="Progress tester",
+            expected_not_in_message="Progress tester",
+        )
 
     @skipIfWindows
     def test_output_nodetails(self):
@@ -71,27 +88,10 @@ class TestDAP_progress(lldbdap_testcase.DAPTestCaseBase):
             "`test-progress --total 3 --seconds 1 --no-details", context="repl"
         )
 
-        self.dap_server.wait_for_event("progressEnd", 15)
-        # Expect at least a start, an update, and end event
-        # However because the underlying Progress instance is an RAII object and we can't guaruntee
-        # it's deterministic destruction in the python API, we verify just start and update
-        # otherwise this test could be flakey.
-        self.assertTrue(len(self.dap_server.progress_events) > 0)
-        start_found = False
-        update_found = False
-        for event in self.dap_server.progress_events:
-            event_type = event["event"]
-            if "progressStart" in event_type:
-                title = event["body"]["title"]
-                self.assertIn("Progress tester", title)
-                start_found = True
-            if "progressUpdate" in event_type:
-                message = event["body"]["message"]
-                self.assertEqual("Initial Detail", message)
-                update_found = True
-
-        self.assertTrue(start_found)
-        self.assertTrue(update_found)
+        self.verify_progress_events(
+            expected_title="Progress tester",
+            expected_message="Initial Detail",
+        )
 
     @skipIfWindows
     def test_output_indeterminate(self):
@@ -109,30 +109,11 @@ class TestDAP_progress(lldbdap_testcase.DAPTestCaseBase):
         )
         self.dap_server.request_evaluate("`test-progress --seconds 1", context="repl")
 
-        self.dap_server.wait_for_event("progressEnd", 15)
-        # Expect at least a start, an update, and end event
-        # However because the underlying Progress instance is an RAII object and we can't guaruntee
-        # it's deterministic destruction in the python API, we verify just start and update
-        # otherwise this test could be flakey.
-        self.assertTrue(len(self.dap_server.progress_events) > 0)
-        start_found = False
-        update_found = False
-        for event in self.dap_server.progress_events:
-            event_type = event["event"]
-            if "progressStart" in event_type:
-                title = event["body"]["title"]
-                self.assertIn("Progress tester", title)
-                start_found = True
-            if "progressUpdate" in event_type:
-                message = event["body"]["message"]
-                print(f"Progress update: {message}")
-                # Check on the first update we set the initial detail.
-                if not update_found:
-                    self.assertEqual("Step 1", message)
-                update_found = True
-
-        self.assertTrue(start_found)
-        self.assertTrue(update_found)
+        self.verify_progress_events(
+            expected_title="Progress tester",
+            expected_message="Step 1",
+            only_verify_first_update=True,
+        )
 
     @skipIfWindows
     def test_output_nodetails_indeterminate(self):
@@ -152,26 +133,8 @@ class TestDAP_progress(lldbdap_testcase.DAPTestCaseBase):
             "`test-progress --seconds 1 --no-details", context="repl"
         )
 
-        self.dap_server.wait_for_event("progressEnd", 15)
-        # Expect at least a start, an update, and end event
-        # However because the underlying Progress instance is an RAII object and we can't guaruntee
-        # it's deterministic destruction in the python API, we verify just start and update
-        # otherwise this test could be flakey.
-        self.assertTrue(len(self.dap_server.progress_events) > 0)
-        start_found = False
-        update_found = False
-        for event in self.dap_server.progress_events:
-            event_type = event["event"]
-            if "progressStart" in event_type:
-                title = event["body"]["title"]
-                self.assertIn("Progress tester", title)
-                start_found = True
-            if "progressUpdate" in event_type:
-                message = event["body"]["message"]
-                # Check on the first update we set the initial detail.
-                if not update_found:
-                    self.assertEqual("Initial Indeterminate Detail", message)
-                update_found = True
-
-        self.assertTrue(start_found)
-        self.assertTrue(update_found)
+        self.verify_progress_events(
+            expected_title="Progress tester",
+            expected_message="Initial Indeterminate Detail",
+            only_verify_first_update=True,
+        )
