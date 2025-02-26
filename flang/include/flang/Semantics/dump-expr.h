7913072ec360c3e492d0c26f03ce0edf3dd74ff3
Krzysztof Parzyszek <Krzysztof.Parzyszek@amd.com>
2025-02-26 08:06:03 -0600

    Fix file name in comment

--- flang/include/flang/Semantics/dump-expr.h
+++ flang/include/flang/Semantics/dump-expr.h
@@ -1,4 +1,4 @@
-//===-- Semantics/DumpEvaluateExpr.h ----------------------------*- C++ -*-===//
+//===-- Semantics/dump-expr.h -----------------------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
