f44e697beb1dfa6a028a8015f3d9587968f3600c
Sarah Spall <sarahspall@microsoft.com>
2025-02-26 15:58:44 -0800

    add warning flag for vector out of bounds warning. fix test

--- clang/test/SemaHLSL/Language/VectorOutOfRange-errors.hlsl
+++ clang/test/SemaHLSL/Language/VectorOutOfRange-errors.hlsl
@@ -3,17 +3,17 @@
 export void fn1() {
   int2 A = {1,2};
   int X = A[-1];
-  // expected-error@-1 {{vector element index '-1' is out of bounds}}
+  // expected-error@-1 {{vector element index -1 is out of bounds}}
 }
 
 export void fn2() {
   int4 A = {1,2,3,4};
   int X = A[4];
-  // expected-error@-1 {{vector element index '4' is out of bounds}}
+  // expected-error@-1 {{vector element index 4 is out of bounds}}
 }
 
 export void fn3() {
   bool2 A = {true,true};
   bool X = A[-1];
-  // expected-error@-1 {{vector element index '-1' is out of bounds}}
+  // expected-error@-1 {{vector element index -1 is out of bounds}}
 }
