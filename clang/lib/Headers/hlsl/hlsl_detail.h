fe73beac17b99b0f7836ffdb7050e04595d96dba
kmpeng <hi.kaitlin@gmail.com>
2025-02-26 15:57:59 -0800

    finished implementation, working on fmod.hlsl tests

--- clang/lib/Headers/hlsl/hlsl_detail.h
+++ clang/lib/Headers/hlsl/hlsl_detail.h
@@ -97,6 +97,12 @@ constexpr vector<T, L> reflect_vec_impl(vector<T, L> I, vector<T, L> N) {
 #endif
 }
 
+template <typename T>
+constexpr enable_if_t<is_same<float, T>::value || is_same<half, T>::value, T>
+fmod_impl(T X, T Y) {
+  return __builtin_elementwise_fmod(X, Y);
+}
+
 template <typename T, int N>
 constexpr enable_if_t<is_same<float, T>::value || is_same<half, T>::value, T>
 fmod_vec_impl(vector<T, N> X, vector<T, N> Y) {
@@ -104,10 +110,10 @@ fmod_vec_impl(vector<T, N> X, vector<T, N> Y) {
   return __builtin_elementwise_fmod(X, Y);
 #else 
   vector<T, N> div = X / Y;
-  vector<T, N> result = __builtin_hlsl_elementwise_frac(__builtin_elementwise_abs(div)) * Y; 
-  vector<bool, N> condition = (div >= -div);
-  vector<T, N> realResult = __builtin_hlsl_select(condition, result, -result);
-  return realResult;
+  vector<bool, N> ge = div >= -div;
+  vector<T, N> frc = __builtin_hlsl_elementwise_frac(__builtin_elementwise_abs(div)); 
+  vector<T, N> realFrc = __builtin_hlsl_select(ge, frc, -frc);
+  return realFrc * Y;
 #endif
 }
 
--- clang/test/CodeGenHLSL/builtins/fmod.hlsl
+++ clang/test/CodeGenHLSL/builtins/fmod.hlsl
@@ -36,42 +36,42 @@
 
 
 // CHECK: define [[FNATTRS]] [[TYPE]] @
-// CHECK: %fmod = frem reassoc nnan ninf nsz arcp afn [[TYPE]]
-// CHECK: ret [[TYPE]] %fmod
+// CHECK: call reassoc nnan ninf nsz arcp afn [[FNATTRS]] [[TYPE]] @{{.*}}([[TYPE]] noundef nofpclass(nan inf) %{{.*}}, [[TYPE]] noundef nofpclass(nan inf) %{{.*}}) #{{.*}}
+// CHECK: ret [[TYPE]] %call
 half test_fmod_half(half p0, half p1) { return fmod(p0, p1); }
 
 // CHECK: define [[FNATTRS]] <2 x [[TYPE]]> @
-// CHECK: %fmod = frem reassoc nnan ninf nsz arcp afn <2 x [[TYPE]]>
-// CHECK: ret <2 x [[TYPE]]> %fmod
+// CHECK: call reassoc nnan ninf nsz arcp afn [[FNATTRS]] [[TYPE]] @{{.*}}(<2 x [[TYPE]]> noundef nofpclass(nan inf) %{{.*}}, <2 x [[TYPE]]> noundef nofpclass(nan inf) %{{.*}}) #{{.*}}
+// CHECK: ret <2 x [[TYPE]]> %splat.splat
 half2 test_fmod_half2(half2 p0, half2 p1) { return fmod(p0, p1); }
 
 // CHECK: define [[FNATTRS]] <3 x [[TYPE]]> @
-// CHECK: %fmod = frem reassoc nnan ninf nsz arcp afn <3 x [[TYPE]]>
-// CHECK: ret <3 x [[TYPE]]> %fmod
+// CHECK: call reassoc nnan ninf nsz arcp afn [[FNATTRS]] [[TYPE]] @{{.*}}(<3 x [[TYPE]]> noundef nofpclass(nan inf) %{{.*}}, <3 x [[TYPE]]> noundef nofpclass(nan inf) %{{.*}} #{{.*}}
+// CHECK: ret <3 x [[TYPE]]> %splat.splat
 half3 test_fmod_half3(half3 p0, half3 p1) { return fmod(p0, p1); }
 
 // CHECK: define [[FNATTRS]] <4 x [[TYPE]]> @
-// CHECK: %fmod = frem reassoc nnan ninf nsz arcp afn <4 x [[TYPE]]>
-// CHECK: ret <4 x [[TYPE]]> %fmod
+// CHECK: call reassoc nnan ninf nsz arcp afn [[FNATTRS]] [[TYPE]] @{{.*}}(<4 x [[TYPE]]> noundef nofpclass(nan inf) %{{.*}}, <4 x [[TYPE]]> noundef nofpclass(nan inf) %{{.*}}) #{{.*}}
+// CHECK: ret <4 x [[TYPE]]> %splat.splat
 half4 test_fmod_half4(half4 p0, half4 p1) { return fmod(p0, p1); }
 
 // CHECK: define [[FNATTRS]] float @
-// CHECK: %fmod = frem reassoc nnan ninf nsz arcp afn float
-// CHECK: ret float %fmod
+// CHECK: call reassoc nnan ninf nsz arcp afn [[FNATTRS]] float @{{.*}}(float noundef nofpclass(nan inf) %{{.*}}, float noundef nofpclass(nan inf) %{{.*}}) #{{.*}}
+// CHECK: ret float %call
 float test_fmod_float(float p0, float p1) { return fmod(p0, p1); }
 
 // CHECK: define [[FNATTRS]] <2 x float> @
-// CHECK: %fmod = frem reassoc nnan ninf nsz arcp afn <2 x float>
-// CHECK: ret <2 x float> %fmod
+// CHECK: call reassoc nnan ninf nsz arcp afn [[FNATTRS]] float @{{.*}}(<2 x float> noundef nofpclass(nan inf) %{{.*}}, <2 x float> noundef nofpclass(nan inf) %{{.*}}) #{{.*}}
+// CHECK: ret <2 x float> %splat.splat
 float2 test_fmod_float2(float2 p0, float2 p1) { return fmod(p0, p1); }
 
 // CHECK: define [[FNATTRS]] <3 x float> @
-// CHECK: %fmod = frem reassoc nnan ninf nsz arcp afn <3 x float>
-// CHECK: ret <3 x float> %fmod
+// CHECK: call reassoc nnan ninf nsz arcp afn [[FNATTRS]] float @{{.*}}(<3 x float> noundef nofpclass(nan inf) %{{.*}}, <3 x float> noundef nofpclass(nan inf) %{{.*}}) #{{.*}}
+// CHECK: ret <3 x float> %splat.splat
 float3 test_fmod_float3(float3 p0, float3 p1) { return fmod(p0, p1); }
 
 // CHECK: define [[FNATTRS]] <4 x float> @
-// CHECK: %fmod = frem reassoc nnan ninf nsz arcp afn <4 x float>
-// CHECK: ret <4 x float> %fmod
+// CHECK: call reassoc nnan ninf nsz arcp afn [[FNATTRS]] float @{{.*}}(<4 x float> noundef nofpclass(nan inf) %{{.*}}, <4 x float> noundef nofpclass(nan inf) %{{.*}}) #{{.*}}
+// CHECK: ret <4 x float> %splat.splat
 float4 test_fmod_float4(float4 p0, float4 p1) { return fmod(p0, p1); }
 
