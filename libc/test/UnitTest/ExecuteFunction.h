ccf6e9721bce4d129986108fd4e41faa23c2106e
Vinay Deshmukh <32487576+vinay-deshmukh@users.noreply.github.com>
2025-02-25 22:28:57 -0500

    clang-format

--- libc/test/UnitTest/ExecuteFunction.h
+++ libc/test/UnitTest/ExecuteFunction.h
@@ -41,9 +41,8 @@ struct ProcessStatus {
   int get_fatal_signal();
 };
 
-ProcessStatus
-invoke_in_subprocess(FunctionCaller *func,
-                     int timeout_ms = ProcessStatus::TIMEOUT);
+ProcessStatus invoke_in_subprocess(FunctionCaller *func,
+                                   int timeout_ms = ProcessStatus::TIMEOUT);
 
 const char *signal_as_string(int signum);
 
