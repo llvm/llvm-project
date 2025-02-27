f356376d66660f7973d8e16dc939f50e3cce9e91
kmpeng <hi.kaitlin@gmail.com>
2025-02-27 13:40:52 -0800

    tweaked implementation to exclude vec1 & restrict to vec4, created new fmod-directx.hlsl test file, removed directx tests from fmod.hlsl

--- clang/lib/Headers/hlsl/hlsl_detail.h
+++ clang/lib/Headers/hlsl/hlsl_detail.h
@@ -48,20 +48,25 @@ template <typename T> struct is_arithmetic {
 template <typename T>
 constexpr enable_if_t<is_same<float, T>::value || is_same<half, T>::value, T>
 fmod_impl(T X, T Y) {
+#if !defined(__DirectX__)
   return __builtin_elementwise_fmod(X, Y);
+#else 
+  T div = X / Y;
+  bool ge = div >= -div;
+  T frc = __builtin_hlsl_elementwise_frac(__builtin_elementwise_abs(div)); 
+  return __builtin_hlsl_select(ge, frc, -frc) * Y;
+#endif
 }
 
 template <typename T, int N>
-constexpr enable_if_t<is_same<float, T>::value || is_same<half, T>::value, T>
-fmod_vec_impl(vector<T, N> X, vector<T, N> Y) {
+constexpr vector<T, N> fmod_vec_impl(vector<T, N> X, vector<T, N> Y) {
 #if !defined(__DirectX__)
   return __builtin_elementwise_fmod(X, Y);
 #else 
   vector<T, N> div = X / Y;
   vector<bool, N> ge = div >= -div;
   vector<T, N> frc = __builtin_hlsl_elementwise_frac(__builtin_elementwise_abs(div)); 
-  vector<T, N> realFrc = __builtin_hlsl_select(ge, frc, -frc);
-  return realFrc * Y;
+  return __builtin_hlsl_select(ge, frc, -frc) * Y;
 #endif
 }
 
