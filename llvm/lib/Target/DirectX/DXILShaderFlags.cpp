7edbef530bb52702f65702efad9416f8ba35c71b
Icohedron <cheung.deric@gmail.com>
2025-02-27 17:53:46 +0000

    Apply clang-format

--- llvm/lib/Target/DirectX/DXILShaderFlags.cpp
+++ llvm/lib/Target/DirectX/DXILShaderFlags.cpp
@@ -100,11 +100,13 @@ void ModuleShaderFlags::updateFunctionFlags(ComputedShaderFlags &CSF,
   }
 
   if (!CSF.Int64Ops)
-    CSF.Int64Ops = I.getType()->isIntegerTy() && I.getType()->getScalarSizeInBits() == 64;
+    CSF.Int64Ops =
+        I.getType()->isIntegerTy() && I.getType()->getScalarSizeInBits() == 64;
 
   if (!CSF.Int64Ops) {
     for (const Value *Op : I.operands()) {
-      if (Op->getType()->isIntegerTy() && Op->getType()->getScalarSizeInBits() == 64) {
+      if (Op->getType()->isIntegerTy() &&
+          Op->getType()->getScalarSizeInBits() == 64) {
         CSF.Int64Ops = true;
         break;
       }
