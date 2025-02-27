f3bbcd9d693ae69e23081c3b40040d006b3d40de
changkhothuychung <nhat7203@gmail.com>
2025-02-27 02:03:15 -0500

    valueless test

--- libcxx/test/std/ranges/range.adaptors/range.concat/iterator/ctor.pass.cpp
+++ libcxx/test/std/ranges/range.adaptors/range.concat/iterator/ctor.pass.cpp
@@ -11,6 +11,8 @@
 #include <ranges>
 
 #include "test_macros.h"
+#include "test_iterators.h"
+#include "check_assertion.h"
 #include "../types.h"
 
 int globalBuff[8] = {0, 1, 2, 3, 4, 5, 6, 7};
@@ -64,6 +66,18 @@ struct IterNoDefaultInitView : std::ranges::view_base {
   int* end();
 };
 
+struct ThrowOnCopyView : std::ranges::view_base {
+  int start_;
+  int* ptr_;
+  constexpr explicit ThrowOnCopyView(int* ptr = globalBuff, int start = 0) : start_(start), ptr_(ptr) {}
+  constexpr ThrowOnCopyView(ThrowOnCopyView&&) = default;
+  constexpr ThrowOnCopyView(const ThrowOnCopyView&) { throw 42; };
+  constexpr ThrowOnCopyView& operator=(ThrowOnCopyView&&) = default;
+  constexpr ThrowOnCopyView& operator=(const ThrowOnCopyView&) { throw 42; };
+  constexpr int* begin() const { return ptr_ + start_; }
+  constexpr int* end() const { return ptr_ + 8; }
+};
+
 constexpr bool test() {
   std::ranges::concat_view<MoveOnlyView> concatView;
   auto iter = std::move(concatView).begin();
@@ -74,6 +88,23 @@ constexpr bool test() {
 
   static_assert(std::default_initializable<std::ranges::iterator_t<std::ranges::concat_view<MoveOnlyView>>>);
   static_assert(!std::default_initializable<std::ranges::iterator_t<std::ranges::concat_view<IterNoDefaultInitView>>>);
+
+  {
+    //valueless by exception test
+    std::ranges::concat_view<ThrowOnCopyView> concatView;
+    std::ranges::iterator_t<std::ranges::concat_view<ThrowOnCopyView>> it1;
+    std::ranges::iterator_t<std::ranges::concat_view<ThrowOnCopyView>> it2;
+    try {
+      it1 = concatView.begin();
+    } catch (...) {
+      TEST_LIBCPP_ASSERT_FAILURE(
+          [&] {
+            it2 = it1;
+            (void)it2;
+          }(),
+          "valueless by exception");
+    }
+  }
 
   return true;
 }
