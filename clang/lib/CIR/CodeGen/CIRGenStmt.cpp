9ba88e246a9523775006eb0b3f5c5a3b58cf5205
Andy Kaylor <akaylor@nvidia.com>
2025-02-27 14:22:26 -0800

    [CIR] Upstream basic alloca and load support (#128792)
    This change implements basic support in ClangIR for local variables
    using the cir.alloca and cir.load operations.

--- clang/lib/CIR/CodeGen/CIRGenStmt.cpp
+++ clang/lib/CIR/CodeGen/CIRGenStmt.cpp
@@ -68,6 +68,8 @@ mlir::LogicalResult CIRGenFunction::emitSimpleStmt(const Stmt *s,
   default:
     // Only compound and return statements are supported right now.
     return mlir::failure();
+  case Stmt::DeclStmtClass:
+    return emitDeclStmt(cast<DeclStmt>(*s));
   case Stmt::CompoundStmtClass:
     if (useCurrentScope)
       emitCompoundStmtWithoutScope(cast<CompoundStmt>(*s));
@@ -77,6 +79,15 @@ mlir::LogicalResult CIRGenFunction::emitSimpleStmt(const Stmt *s,
   case Stmt::ReturnStmtClass:
     return emitReturnStmt(cast<ReturnStmt>(*s));
   }
+
+  return mlir::success();
+}
+
+mlir::LogicalResult CIRGenFunction::emitDeclStmt(const DeclStmt &s) {
+  assert(builder.getInsertionBlock() && "expected valid insertion point");
+
+  for (const Decl *I : s.decls())
+    emitDecl(*I);
 
   return mlir::success();
 }
