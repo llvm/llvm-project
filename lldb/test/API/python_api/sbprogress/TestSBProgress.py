6e9ce7e7e55924e93231799e03c4ea3cc45c1810
Jacob Lalonde <jalalonde@fb.com>
2025-02-26 16:35:55 -0800

    Further asserts based on Greg's feedback

--- lldb/test/API/python_api/sbprogress/TestSBProgress.py
+++ lldb/test/API/python_api/sbprogress/TestSBProgress.py
@@ -42,11 +42,24 @@ class SBProgressTestCase(TestBase):
         broadcaster = self.dbg.GetBroadcaster()
         broadcaster.AddListener(listener, lldb.eBroadcastBitExternalProgress)
         event = lldb.SBEvent()
-
+        # Sample JSON we're expecting:
+        # { id = 2, title = "Test SBProgress", details = "Test progress", type = update, progress = 1 of 3}
+        # details remains the same as specified in the constructor of the progress
+        # until we update it in the increment function, so we check for the Null and empty string case
+        # that details hasn't changed, but progress x of 3 has.
         progress.Increment(1, None)
         self.assertTrue(listener.GetNextEvent(event))
+        stream = lldb.SBStream()
+        event.GetDescription(stream)
+        self.assertIn("Test progress", stream.GetData())
+        self.assertIn("1 of 3", stream.GetData())
+
         progress.Increment(1, "")
         self.assertTrue(listener.GetNextEvent(event))
+        event.GetDescription(stream)
+        self.assertIn("Test progress", stream.GetData())
+        self.assertIn("2 of 3", stream.GetData())
+
         progress.Increment(1, "Step 3")
         self.assertTrue(listener.GetNextEvent(event))
         stream = lldb.SBStream()
