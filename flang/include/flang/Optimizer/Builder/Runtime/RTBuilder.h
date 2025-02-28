b4e28fd5c4f664bc03e02197e13f7b2ab832bf32
R <rqou@berkeley.edu>
2025-02-28 05:42:53 +0000

    horrible unusable long hack

--- flang/include/flang/Optimizer/Builder/Runtime/RTBuilder.h
+++ flang/include/flang/Optimizer/Builder/Runtime/RTBuilder.h
@@ -260,7 +260,7 @@ constexpr TypeBuilderFunc getModel<void **>() {
 template <>
 constexpr TypeBuilderFunc getModel<long>() {
   return [](mlir::MLIRContext *context) -> mlir::Type {
-    return mlir::IntegerType::get(context, 8 * sizeof(long));
+    return mlir::IntegerType::get(context, 8 * 4);
   };
 }
 template <>
@@ -309,7 +309,7 @@ constexpr TypeBuilderFunc getModel<const long long *>() {
 template <>
 constexpr TypeBuilderFunc getModel<unsigned long>() {
   return [](mlir::MLIRContext *context) -> mlir::Type {
-    return mlir::IntegerType::get(context, 8 * sizeof(unsigned long));
+    return mlir::IntegerType::get(context, 8 * 4);
   };
 }
 template <>
