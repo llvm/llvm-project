d6f19cdc0bda8e6d9ee89569fecc0b4a06b1fcc9
Alex Bradbury <asb@igalia.com>
2025-02-28 13:03:43 +0000

    [PreISelIntrinsicLowering] Zext/trunc count parameter as necessary for memset_pattern16 emission
    This patch cleans up the handling of the count parameter in general,
    though was initially motivated by a compiler crash upon a memset.pattern
    with a narrow count causing a compiler crash due to different types for
    CreateMul when converting the count to the number of bytes.
    
    The logic used to name globals means there is some minor renaming churn
    in the output to
    test/Transforms/PreISelIntrinsicLowering/X86/memset-pattern.ll
    irrelevant to the newly added tests (that would crash before).

--- llvm/lib/CodeGen/PreISelIntrinsicLowering.cpp
+++ llvm/lib/CodeGen/PreISelIntrinsicLowering.cpp
@@ -394,11 +394,12 @@ bool PreISelIntrinsicLowering::expandMemIntrinsicUses(Function &F) const {
       Module *M = Memset->getModule();
       const DataLayout &DL = Memset->getDataLayout();
 
+      Type *DestPtrTy = Memset->getRawDest()->getType();
+      Type *IntIdxTy = DL.getIndexType(DestPtrTy);
       StringRef FuncName = "memset_pattern16";
-      FunctionCallee MSP = getOrInsertLibFunc(
-          M, TLI, LibFunc_memset_pattern16, Builder.getVoidTy(),
-          Memset->getRawDest()->getType(), Builder.getPtrTy(),
-          Memset->getLength()->getType());
+      FunctionCallee MSP = getOrInsertLibFunc(M, TLI, LibFunc_memset_pattern16,
+                                              Builder.getVoidTy(), DestPtrTy,
+                                              Builder.getPtrTy(), IntIdxTy);
       inferNonMandatoryLibFuncAttrs(M, FuncName, TLI);
 
       // Otherwise we should form a memset_pattern16.  PatternValue is known
@@ -415,9 +416,10 @@ bool PreISelIntrinsicLowering::expandMemIntrinsicUses(Function &F) const {
       GV->setAlignment(Align(16));
       Value *PatternPtr = GV;
       Value *NumBytes = Builder.CreateMul(
-          Builder.getInt64(DL.getTypeSizeInBits(Memset->getValue()->getType()) /
-                           8),
-          Memset->getLength());
+          ConstantInt::get(IntIdxTy,
+                           DL.getTypeSizeInBits(Memset->getValue()->getType()) /
+                               8),
+          Builder.CreateZExtOrTrunc(Memset->getLength(), IntIdxTy));
       CallInst *MemsetPattern16Call =
           Builder.CreateCall(MSP, {Memset->getRawDest(), PatternPtr, NumBytes});
       MemsetPattern16Call->setAAMetadata(Memset->getAAMetadata());
