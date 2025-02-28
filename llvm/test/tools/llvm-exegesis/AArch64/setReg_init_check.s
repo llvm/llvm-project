433b62e908295633b0387ec026331bc05d4cc36b
Lakshay Kumar <lakshayk@nvidia.com>
2025-02-28 02:24:17 -0800

    Modified: Testcases to strictly check correct order of instruction in disassembly, except str

--- llvm/test/tools/llvm-exegesis/AArch64/setReg_init_check.s
+++ llvm/test/tools/llvm-exegesis/AArch64/setReg_init_check.s
@@ -1,31 +1,45 @@
-# REQUIRES: aarch64-registered-target
+; REQUIRES: aarch64-registered-target
 
-# ppr register class initialization testcase 
-# ideally we should use PTRUE_{B/H?S/D} instead of FADDV_VPZ_D for isolated testcase; but exegesis does not support PTRUE_{B/H?S/D} yet;
-# RUN: llvm-exegesis -mcpu=neoverse-v2 -mode=latency --dump-object-to-disk=%d --opcode-name=FADDV_VPZ_D 2>&1 | FileCheck %s --check-prefix=PPR
-# RUN: llvm-objdump -d %d | FileCheck %s --check-prefix=PPR_ASM
-# PPR-NOT: setRegTo is not implemented, results will be unreliable
-# PPR: assembled_snippet: {{.*}}C0035FD6
-# PPR_ASM: ptrue p{{[0-9]+}}
+# PPR REGISTER CLASS INITIALIZATION TESTCASE 
+# IDEALLY WE SHOULD USE PTRUE_{B/H?S/D} INSTEAD OF FADDV_VPZ_D FOR ISOLATED TESTCASE; BUT EXEGESIS DOES NOT SUPPORT PTRUE_{B/H?S/D} YET;
+; RUN: llvm-exegesis -mcpu=neoverse-v2 -mode=latency --dump-object-to-disk=%d --opcode-name=FADDV_VPZ_D 2>&1 | FileCheck %s --check-prefix=PPR
+; RUN: llvm-objdump -d %d > %t.s
+; RUN: FileCheck %s --check-prefix=PPR_ASM < %t.s
+; PPR-NOT: setRegTo is not implemented, results will be unreliable
+; PPR: assembled_snippet: {{.*}}C0035FD6
+; PPR_ASM: {{<foo>:}}
+; PPR_ASM: ptrue p{{[0-9]+}}.b
+; PPR_ASM-NEXT: dupm z{{[0-9]+}}.s, #0x1
+; PPR_ASM-NEXT: faddv d{{[0-9]+}}, p{{[0-9]+}}, z{{[0-9]+}}
 
-# zpr register class initialization testcase 
-# ideally we should use DUPM_ZI instead of FADDV_VPZ_S for isolated testcase; but exegesis does not support DUPM_ZI yet;
-# RUN: llvm-exegesis -mcpu=neoverse-v2 -mode=latency --dump-object-to-disk=%d --opcode-name=FADDV_VPZ_S 2>&1 | FileCheck %s --check-prefix=ZPR
-# RUN: llvm-objdump -d %d | FileCheck %s --check-prefix=ZPR_ASM
-# ZPR-NOT: setRegTo is not implemented, results will be unreliable
-# ZPR: assembled_snippet: {{.*}}C0035FD6
-# ZPR_ASM: dupm z{{[0-9]+}}.s, #0x1
+# ZPR REGISTER CLASS INITIALIZATION TESTCASE 
+# IDEALLY WE SHOULD USE PTRUE_{B/H?S/D} INSTEAD OF FADDV_VPZ_D FOR ISOLATED TESTCASE; BUT EXEGESIS DOES NOT SUPPORT PTRUE_{B/H?S/D} YET;
+; RUN: llvm-exegesis -mcpu=neoverse-v2 -mode=latency --dump-object-to-disk=%d --opcode-name=FADDV_VPZ_D 2>&1 | FileCheck %s --check-prefix=ZPR
+; RUN: llvm-objdump -d %d > %t.s
+; RUN: FileCheck %s --check-prefix=ZPR_ASM < %t.s
+; ZPR-NOT: setRegTo is not implemented, results will be unreliable
+; ZPR: assembled_snippet: {{.*}}C0035FD6
+; ZPR_ASM: {{<foo>:}}
+; ZPR_ASM: ptrue p{{[0-9]+}}.b
+; ZPR_ASM-NEXT: dupm z{{[0-9]+}}.s, #0x1
+; ZPR_ASM-NEXT: faddv d{{[0-9]+}}, p{{[0-9]+}}, z{{[0-9]+}}
 
-# fpr64 register class initialization testcase
-# RUN: llvm-exegesis -mcpu=neoverse-v2 -mode=latency --dump-object-to-disk=%d --opcode-name=ADDVv4i16v 2>&1 | FileCheck %s --check-prefix=FPR64
-# RUN: llvm-objdump -d %d | FileCheck %s --check-prefix=FPR64-ASM
-# FPR64-NOT: setRegTo is not implemented, results will be unreliable
-# FPR64: assembled_snippet: {{.*}}C0035FD6
-# FPR64-ASM: fmov d{{[0-9]+}}, #2.0
+# FPR128 REGISTER CLASS INITIALIZATION TESTCASE
+; RUN: llvm-exegesis -mcpu=neoverse-v2 -mode=latency --dump-object-to-disk=%d --opcode-name=ADDVv16i8v 2>&1 | FileCheck %s --check-prefix=FPR128
+; RUN: llvm-objdump -d %d > %t.s
+; RUN: FileCheck %s --check-prefix=FPR128-ASM < %t.s
+; FPR128-NOT: setRegTo is not implemented, results will be unreliable
+; FPR128: assembled_snippet: {{.*}}C0035FD6
+; FPR128-ASM: {{<foo>:}}
+; FPR128-ASM: movi v{{[0-9]+}}.2d, #0000000000000000
+; FPR128-ASM-NEXT: addv b{{[0-9]+}}, v{{[0-9]+}}.16b
 
-# fpr128 register class initialization testcase
-# RUN: llvm-exegesis -mcpu=neoverse-v2 -mode=latency --dump-object-to-disk=%d --opcode-name=ADDVv16i8v 2>&1 | FileCheck %s --check-prefix=FPR128
-# RUN: llvm-objdump -d %d | FileCheck %s --check-prefix=FPR128-ASM
-# FPR128-NOT: setRegTo is not implemented, results will be unreliable
-# FPR128: assembled_snippet: {{.*}}C0035FD6
-# FPR128-ASM: movi v{{[0-9]+}}.2d, {{#0}}
+# FPR64 REGISTER CLASS INITIALIZATION TESTCASE
+; RUN: llvm-exegesis -mcpu=neoverse-v2 -mode=latency --dump-object-to-disk=%d --opcode-name=ADDVv4i16v 2>&1 | FileCheck %s --check-prefix=FPR64
+; RUN: llvm-objdump -d %d > %t.s
+; RUN: FileCheck %s --check-prefix=FPR64-ASM < %t.s
+; FPR64-NOT: setRegTo is not implemented, results will be unreliable
+; FPR64: assembled_snippet: {{.*}}C0035FD6
+; FPR64-ASM: {{<foo>:}}
+; FPR64-ASM: fmov d{{[0-9]+}}, {{#2.0+|#2\.000000000000000000e\+00}}
+; FPR64-ASM-NEXT: addv h{{[0-9]+}}, v{{[0-9]+}}.4h
