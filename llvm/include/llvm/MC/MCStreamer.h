31bf16a7a2e1f5e783af9055fa2a1d815c090da2
Fangrui Song <i@maskray.me>
2025-03-02 13:14:10 -0800

    [MC] Add MCTargetStreamer::getContext to simplify code

--- llvm/include/llvm/MC/MCStreamer.h
+++ llvm/include/llvm/MC/MCStreamer.h
@@ -100,6 +100,7 @@ public:
   virtual ~MCTargetStreamer();
 
   MCStreamer &getStreamer() { return Streamer; }
+  MCContext &getContext();
 
   // Allow a target to add behavior to the EmitLabel of MCStreamer.
   virtual void emitLabel(MCSymbol *Symbol);
@@ -1156,6 +1157,10 @@ public:
                                         const MCSymbol *Label,
                                         unsigned PointerSize) {}
 };
+
+inline MCContext &MCTargetStreamer::getContext() {
+  return Streamer.getContext();
+}
 
 /// Create a dummy machine code streamer, which does nothing. This is useful for
 /// timing the assembler front end.
