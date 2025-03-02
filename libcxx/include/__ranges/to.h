883ca4f849a410db9b6c0b23dc729c92d08a48d5
A. Jiang <de34@live.cn>
2025-03-03 00:37:20 +0800

    Workaround for lacking implementation of P0588R1

--- libcxx/include/__ranges/to.h
+++ libcxx/include/__ranges/to.h
@@ -71,20 +71,18 @@ concept __constructible_from_iter_pair =
     derived_from<typename iterator_traits<iterator_t<_Range>>::iterator_category, input_iterator_tag> &&
     constructible_from<_Container, iterator_t<_Range>, sentinel_t<_Range>, _Args...>;
 
-template <class _Container>
-constexpr auto __container_append(_Container& __c) {
-  return [&__c]<class _Ref>(_Ref&& __ref) {
-    if constexpr (requires { __c.emplace_back(std::declval<_Ref>()); })
-      __c.emplace_back(std::forward<_Ref>(__ref));
-    else if constexpr (requires { __c.push_back(std::declval<_Ref>()); })
-      __c.push_back(std::forward<_Ref>(__ref));
-    else if constexpr (requires { __c.emplace(__c.end(), std::declval<_Ref>()); })
-      __c.emplace(__c.end(), std::forward<_Ref>(__ref));
-    else {
-      static_assert(requires { __c.insert(__c.end(), std::declval<_Ref>()); });
-      __c.insert(__c.end(), std::forward<_Ref>(__ref));
-    }
-  };
+template <class _Ref, class _Container>
+[[nodiscard]] _LIBCPP_HIDE_FROM_ABI constexpr auto __container_append(_Container& __c) {
+  if constexpr (requires { __c.emplace_back(std::declval<_Ref>()); })
+    return [&__c](_Ref&& __ref) { __c.emplace_back(std::forward<_Ref>(__ref)); };
+  else if constexpr (requires { __c.push_back(std::declval<_Ref>()); })
+    return [&__c](_Ref&& __ref) { __c.push_back(std::forward<_Ref>(__ref)); };
+  else if constexpr (requires { __c.emplace(__c.end(), std::declval<_Ref>()); })
+    return [&__c](_Ref&& __ref) { __c.emplace(__c.end(), std::forward<_Ref>(__ref)); };
+  else {
+    static_assert(requires { __c.insert(__c.end(), std::declval<_Ref>()); });
+    return [&__c](_Ref&& __ref) { __c.insert(__c.end(), std::forward<_Ref>(__ref)); };
+  }
 }
 
 template <class>
@@ -126,7 +124,7 @@ template <class _Container, input_range _Range, class... _Args>
         __result.reserve(static_cast<range_size_t<_Container>>(ranges::size(__range)));
       }
 
-      ranges::for_each(__range, ranges::__container_append(__result));
+      ranges::for_each(__range, ranges::__container_append<ranges::range_reference_t<_Range>>(__result));
       return __result;
     } else {
       static_assert(__always_false<_Container>, "ranges::to: unable to convert to the given container type.");
