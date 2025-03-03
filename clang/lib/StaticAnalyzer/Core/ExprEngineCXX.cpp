1717c42f7fca7b65458d188aedd76463ff8e5801
tobias.gruber <tobias.gruber@concentrio.io>
2025-03-03 08:21:27 +0100

    Add comment to argument in function call

--- clang/lib/StaticAnalyzer/Core/ExprEngineCXX.cpp
+++ clang/lib/StaticAnalyzer/Core/ExprEngineCXX.cpp
@@ -79,7 +79,7 @@ void ExprEngine::performTrivialCopy(NodeBuilder &Bldr, ExplodedNode *Pred,
     assert(V.isUnknownOrUndef());
 
   ExplodedNodeSet Tmp;
-  evalLocation(Tmp, CallExpr, VExpr, Pred, Pred->getState(), V, true);
+  evalLocation(Tmp, CallExpr, VExpr, Pred, Pred->getState(), V, /*isLoad=*/true);
   for (ExplodedNode *N : Tmp)
     evalBind(Dst, CallExpr, N, ThisVal, V, true);
 
