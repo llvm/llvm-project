017be757ca436d3ff9ebf958378c8e4c85d93f5d
Vy Nguyen <vyng@google.com>
2025-03-04 15:46:49 -0500

    Update lldb/unittests/Core/TelemetryTest.cpp
    Co-authored-by: Jonas Devlieghere <jonas@devlieghere.com>

--- lldb/unittests/Core/TelemetryTest.cpp
+++ lldb/unittests/Core/TelemetryTest.cpp
@@ -52,7 +52,7 @@ class FakePlugin : public telemetry::TelemetryManager {
 public:
   FakePlugin()
       : telemetry::TelemetryManager(
-            std::make_unique<telemetry::LLDBConfig>(true, true)) {}
+            std::make_unique<telemetry::LLDBConfig>(/*enable_telemetry=*/true, /*detailed_command_telemetry=*/true)) {}
 
   // TelemetryManager interface
   llvm::Error preDispatch(llvm::telemetry::TelemetryInfo *entry) override {
