6972d6e19a4df936661cd2daac5ba23bf72131e4
Ron Lieberman <ron.lieberman@amd.com>
2025-03-05 16:04:50 -0600

    resolve lit test conflict

--- clang/test/CodeGenHLSL/builtins/RWBuffer-constructor-opt.hlsl
+++ clang/test/CodeGenHLSL/builtins/RWBuffer-constructor-opt.hlsl
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-// RUN: %clang_cc1 -triple dxil-pc-shadermodel6.3-library -x hlsl -emit-llvm -O3 -o - %s | FileCheck %s --check-prefixes=CHECK,CHECK-DXIL
-// RUN: %clang_cc1 -triple spirv-vulkan-compute -x hlsl -emit-llvm -O3 -o - %s | FileCheck %s --check-prefixes=CHECK,CHECK-SPIRV
-// CHECK-SPIRV: %"class.hlsl::RWBuffer" = type { target("spirv.Image", float, 5, 2, 0, 0, 2, 0) }
-// CHECK-DXIL:  %"class.hlsl::RWBuffer" = type { target("dx.TypedBuffer", float, 1, 0, 0) }
-=======
 // RUN: %clang_cc1 -triple dxil-pc-shadermodel6.3-library -x hlsl -emit-llvm -O3 -o - %s | FileCheck %s
 // RUN: %clang_cc1 -triple spirv-vulkan-compute -x hlsl -emit-llvm -O3 -o - %s | FileCheck %s
 
 // All referenced to an unused resource should be removed by optimizations.
->>>>>>> 46236f4c3dbe11e14fe7ac1f4b903637efedfecf
 RWBuffer<float> Buf : register(u5, space3);
 
 [shader("compute")]
