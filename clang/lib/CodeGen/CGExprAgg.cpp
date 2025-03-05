c62373c846263048b7c21abd239d3323af1aca58
Matt Arsenault <Matthew.Arsenault@amd.com>
2025-03-05 13:13:09 +0700

    clang: Do not implicitly addrspacecast in EmitAggExprToLValue
    This fixes breaking the ABI for calls to the kernel enqueue
    implementation functions in OpenCL. The ABI passes these by stack
    byref or byval, but this was incorrectly casting the stack slot
    and passing to the use function.
    
    I think nearly all uses of CreateMemTemp should not be inserting
    this cast, but that will require a larger change with many more test
    updates.

--- clang/lib/CodeGen/CGExprAgg.cpp
+++ clang/lib/CodeGen/CGExprAgg.cpp
@@ -2219,7 +2219,7 @@ void CodeGenFunction::EmitAggExpr(const Expr *E, AggValueSlot Slot) {
 
 LValue CodeGenFunction::EmitAggExprToLValue(const Expr *E) {
   assert(hasAggregateEvaluationKind(E->getType()) && "Invalid argument!");
-  Address Temp = CreateMemTemp(E->getType());
+  Address Temp = CreateMemTempWithoutCast(E->getType());
   LValue LV = MakeAddrLValue(Temp, E->getType());
   EmitAggExpr(E, AggValueSlot::forLValue(LV, AggValueSlot::IsNotDestructed,
                                          AggValueSlot::DoesNotNeedGCBarriers,
