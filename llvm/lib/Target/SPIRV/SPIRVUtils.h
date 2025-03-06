9e183f2aa7e901fd08635e21283e87af1bea2c24
Sidorov, Dmitry <dmitry.sidorov@intel.com>
2025-03-06 04:06:22 -0800

    remove comment
    Signed-off-by: Sidorov, Dmitry <dmitry.sidorov@intel.com>

--- llvm/lib/Target/SPIRV/SPIRVUtils.h
+++ llvm/lib/Target/SPIRV/SPIRVUtils.h
@@ -148,9 +148,6 @@ void buildOpDecorate(Register Reg, MachineInstr &I, const SPIRVInstrInfo &TII,
 void buildOpSpirvDecorations(Register Reg, MachineIRBuilder &MIRBuilder,
                              const MDNode *GVarMD);
 
-// void buildMemAliasingOpDecorate(Register Reg, MachineIRBuilder &MIRBuilder,
-//                                 uint32_t Dec, const MDNode *GVarMD);
-
 // Return a valid position for the OpVariable instruction inside a function,
 // i.e., at the beginning of the first block of the function.
 MachineBasicBlock::iterator getOpVariableMBBIt(MachineInstr &I);
