e01f8a9056963a3da92435bc8f5f061cda04c7ec
Vinay Deshmukh <32487576+vinay-deshmukh@users.noreply.github.com>
2025-03-05 20:38:03 -0500

    fix cmake

--- libc/cmake/modules/LLVMLibCTestRules.cmake
+++ libc/cmake/modules/LLVMLibCTestRules.cmake
@@ -37,6 +37,7 @@ function(_get_common_test_compile_options output_var c_test flags)
       # list(APPEND compile_options "-Werror")
     endif()
     list(APPEND compile_options "-Wconversion")
+    list(APPEND compile_options "-Wsign-conversion")
     list(APPEND compile_options "-Wimplicit-fallthrough")
     list(APPEND compile_options "-Wwrite-strings")
     # Silence this warning because _Complex is a part of C99.
