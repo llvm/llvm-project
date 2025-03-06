23023d7705d646dadebb1872894c6716e2b3833c
Ajay Raj <ajayrajsaini333@gmail.com>
2025-03-06 16:32:30 +0000

    Added license header

--- libc/include/llvm-libc-macros/sysexits-macros.h
+++ libc/include/llvm-libc-macros/sysexits-macros.h
@@ -1,3 +1,11 @@
+//===-- Macros defined in sysexits.h header file --------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
 #ifndef SYSEXITS_MACROS_H
 #define SYSEXITS_MACROS_H
 
