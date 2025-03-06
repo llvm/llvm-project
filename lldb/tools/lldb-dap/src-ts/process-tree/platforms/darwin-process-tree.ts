85a19e4406e3b6e05e1f90a61803f810f7d1f363
Matthew Bastien <matthew_bastien@apple.com>
2025-03-06 16:35:16 -0500

    minor fixes to Darwin process tree

--- lldb/tools/lldb-dap/src-ts/process-tree/platforms/darwin-process-tree.ts
+++ lldb/tools/lldb-dap/src-ts/process-tree/platforms/darwin-process-tree.ts
@@ -1,15 +1,11 @@
 import { LinuxProcessTree } from "./linux-process-tree";
 
-function fill(prefix: string, suffix: string, length: number): string {
-  return prefix + suffix.repeat(length - prefix.length);
-}
-
 export class DarwinProcessTree extends LinuxProcessTree {
   protected override getCommandArguments(): string[] {
     return [
-      "-xo",
+      "-axo",
       // The length of comm must be large enough or data will be truncated.
-      `pid=PID,lstart=START,comm=${fill("COMMAND", "-", 256)},command=ARGUMENTS`,
+      `pid=PID,lstart=START,comm=${"COMMAND".padEnd(256, "-")},args=ARGUMENTS`,
     ];
   }
 }
