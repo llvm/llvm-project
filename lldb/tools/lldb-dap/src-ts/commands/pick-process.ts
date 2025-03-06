b2c03823b86c397f229c0e111e8a0924dc0199fb
Matthew Bastien <matthew_bastien@apple.com>
2025-03-06 15:50:35 -0500

    allow filtering processes by program

--- lldb/tools/lldb-dap/src-ts/commands/pick-process.ts
+++ lldb/tools/lldb-dap/src-ts/commands/pick-process.ts
@@ -3,7 +3,7 @@ import * as vscode from "vscode";
 import { createProcessTree } from "../process-tree";
 
 interface ProcessQuickPick extends vscode.QuickPickItem {
-  processId: number;
+  processId?: number;
 }
 
 /**
@@ -13,30 +13,45 @@ interface ProcessQuickPick extends vscode.QuickPickItem {
  * string substitution infrastructure. The value will eventually be converted
  * to a number by the debug configuration provider.
  *
+ * @param configuration The related debug configuration, if any
  * @returns The pid of the process as a string or undefined if cancelled.
  */
-export async function pickProcess(): Promise<string | undefined> {
+export async function pickProcess(
+  configuration?: vscode.DebugConfiguration,
+): Promise<string | undefined> {
   const processTree = createProcessTree();
   const selectedProcess = await vscode.window.showQuickPick<ProcessQuickPick>(
     processTree.listAllProcesses().then((processes): ProcessQuickPick[] => {
-      return processes
-        .sort((a, b) => b.start - a.start) // Sort by start date in descending order
-        .map((proc) => {
-          return {
-            processId: proc.id,
-            label: path.basename(proc.command),
-            description: proc.id.toString(),
-            detail: proc.arguments,
-          } satisfies ProcessQuickPick;
-        });
+      // Sort by start date in descending order
+      processes.sort((a, b) => b.start - a.start);
+      // Filter by program if requested
+      if (typeof configuration?.program === "string") {
+        processes = processes.filter(
+          (proc) => proc.command === configuration.program,
+        );
+        // Show a better message if all processes were filtered out
+        if (processes.length === 0) {
+          return [
+            {
+              label: "No processes matched the debug configuration's program",
+            },
+          ];
+        }
+      }
+      // Convert to a QuickPickItem
+      return processes.map((proc) => {
+        return {
+          processId: proc.id,
+          label: path.basename(proc.command),
+          description: proc.id.toString(),
+          detail: proc.arguments,
+        } satisfies ProcessQuickPick;
+      });
     }),
     {
       placeHolder: "Select a process to attach the debugger to",
       matchOnDetail: true,
     },
   );
-  if (!selectedProcess) {
-    return;
-  }
-  return selectedProcess.processId.toString();
+  return selectedProcess?.processId?.toString();
 }
