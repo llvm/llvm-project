e9de91e989de2b61ded7f471b48453eddf77ca29
Mariusz Sikora <mariusz.sikora@amd.com>
2025-03-07 11:10:21 +0100

    [AMDGPU] Add safe-smem-prefetch SubtargetFeature off by default (#130050)
    S_PREFETCH_* instructions may cause host to terminate process in case of
    the invalid address.
    
    Co-authored-by: Stanislav Mekhanoshin <Stanislav.Mekhanoshin@amd.com>

--- llvm/lib/Target/AMDGPU/GCNSubtarget.h
+++ llvm/lib/Target/AMDGPU/GCNSubtarget.h
@@ -233,6 +233,7 @@ protected:
   bool HasVMEMtoScalarWriteHazard = false;
   bool HasSMEMtoVectorWriteHazard = false;
   bool HasInstFwdPrefetchBug = false;
+  bool HasSafeSmemPrefetch = false;
   bool HasVcmpxExecWARHazard = false;
   bool HasLdsBranchVmemWARHazard = false;
   bool HasNSAtoVMEMBug = false;
@@ -962,6 +963,8 @@ public:
   }
 
   bool hasPrefetch() const { return GFX12Insts; }
+
+  bool hasSafeSmemPrefetch() const { return HasSafeSmemPrefetch; }
 
   // Has s_cmpk_* instructions.
   bool hasSCmpK() const { return getGeneration() < GFX12; }
