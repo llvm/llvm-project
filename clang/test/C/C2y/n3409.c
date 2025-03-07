c6a0b3f4c486a4210c1d20daa015809adf70995b
Aaron Ballman <aaron@aaronballman.com>
2025-03-07 13:08:20 -0500

    Update based on review feedback; fix tests

--- clang/test/C/C2y/n3409.c
+++ clang/test/C/C2y/n3409.c
@@ -19,14 +19,18 @@ void foo() {
   (void)(void)1;
   // FIXME: same with this.
   x;
-  _Generic(x, void: 1);      /* pre-c2y-warning {{use of an incomplete type in a '_Generic' association is incompatible with C standards before C2y; 'void' is an incomplete type}}
-                                ext-warning {{ISO C requires a complete type in a '_Generic' association; 'void' is an incomplete type}}
+  _Generic(x, void: 1);      /* pre-c2y-warning {{use of incomplete type 'void' in a '_Generic' association is incompatible with C standards before C2y}}
+                                ext-warning {{incomplete type 'void' in a '_Generic' association is a C2y extension}}
                               */
-  _Generic(x, typeof(x): 1); /* pre-c2y-warning {{use of an incomplete type in a '_Generic' association is incompatible with C standards before C2y; 'typeof (x)' (aka 'void') is an incomplete type}}
-                                ext-warning {{ISO C requires a complete type in a '_Generic' association; 'typeof (x)' (aka 'void') is an incomplete type}}
+  _Generic(x, typeof(x): 1); /* pre-c2y-warning {{use of incomplete type 'typeof (x)' (aka 'void') in a '_Generic' association is incompatible with C standards before C2y}}
+                                ext-warning {{incomplete type 'typeof (x)' (aka 'void') in a '_Generic' association is a C2y extension}}
                               */
   (void)_Generic(void, default : 1); /* pre-c2y-warning {{passing a type argument as the first operand to '_Generic' is incompatible with C standards before C2y}}
                                         ext-warning {{passing a type argument as the first operand to '_Generic' is a C2y extension}}
                                       */
-}
 
+  // This is not sufficiently important of an extension to warrant a "not
+  // compatible with standards before C2y" warning, but it is an extension in
+  // C23 and earlier.
+  return x; // ext-warning {{void function 'foo' should not return void expression}}
+}
