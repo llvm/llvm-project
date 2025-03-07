bb0ba002a3825339bb8c5f4343e333b06493dd2f
Joseph Huber <huberjn@outlook.com>
2025-03-07 08:42:37 -0600

    Update tests

--- clang/test/Sema/vector-ast.cpp
+++ clang/test/Sema/vector-ast.cpp
@@ -2,12 +2,14 @@
 
 // expected-no-diagnostics
 
-// CHECK: VarDecl {{.*}} x 'int __attribute__((ext_vector_type(4)))'
+//      CHECK: ExtVectorType {{.*}} 'int __attribute__((ext_vector_type(4)))' 4
+// CHECK-NEXT: BuiltinType {{.*}} 'int'
 int x __attribute__((ext_vector_type(4)));
+using ExtVecType = decltype(x);
 
 // CHECK: FunctionDecl {{.*}} 'int () __attribute__((ext_vector_type(4)))'
 int __attribute__((ext_vector_type(4))) foo() { return x; }
-// CHECK:  CompoundStmt
+//      CHECK:  CompoundStmt
 // CHECK-NEXT: ReturnStmt
 // CHECK-NEXT: ImplicitCastExpr {{.*}} 'int __attribute__((ext_vector_type(4)))' <LValueToRValue>
 // CHECK-NEXT: DeclRefExpr {{.*}} 'int __attribute__((ext_vector_type(4)))' lvalue Var {{.*}} 'x' 'int __attribute__((ext_vector_type(4)))'
