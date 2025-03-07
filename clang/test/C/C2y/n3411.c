de2a448cf5f800bbf3fa0d5d9063b95c64521e19
Aaron Ballman <aaron@aaronballman.com>
2025-03-07 07:24:45 -0500

    Add a test case based on review feedback

--- clang/test/C/C2y/n3411.c
+++ clang/test/C/C2y/n3411.c
@@ -1,6 +1,7 @@
-// RUN: %clang_cc1 -verify=c2y -std=c2y -Wall -pedantic %s
+// RUN: %clang_cc1 -verify=good -std=c2y -Wall -pedantic %s
 // RUN: %clang_cc1 -verify -Wnewline-eof -std=c2y -Wall -pedantic %s
 // RUN: %clang_cc1 -verify -std=c23 -Wall -pedantic %s
+// RUN: %clang_cc1 -verify=good -std=c23 %s
 
 /* WG14 N3411: Yes
  * Slay Some Earthly Demons XII
@@ -8,7 +9,7 @@
  * Allow a non-empty source file to end without a final newline character. Note
  * that this file intentionally does not end with a trailing newline.
  */
-// c2y-no-diagnostics
+// good-no-diagnostics
 
 int x; // Ensure the file contains at least one declaration.
 // expected-warning {{no newline at end of file}}
