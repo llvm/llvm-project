86faa8549036b80164259252c84358c4224d5ca8
kmpeng <kaitlinpeng@microsoft.com>
2025-03-07 15:16:25 -0800

    Added SPVCHECK prefix & changed function definition checks in fmod.hlsl tests, formatted files with clang-format

--- clang/test/CodeGenHLSL/builtins/fmod.hlsl
+++ clang/test/CodeGenHLSL/builtins/fmod.hlsl
@@ -4,16 +4,16 @@
 //
 // RUN: %clang_cc1 -finclude-default-header -x hlsl -triple \
 // RUN:   dxil-pc-shadermodel6.3-library %s -fnative-half-type \
-// RUN:   -emit-llvm -o - | FileCheck %s -DFNATTRS="noundef nofpclass(nan inf)" \
-// RUN:   -DTYPE=half --check-prefixes=DXCHECK,DXNATIVE_HALF
+// RUN:   -emit-llvm -o - | FileCheck %s \
+// RUN:   -DTYPE=half --check-prefixes=CHECK,DXCHECK,DXNATIVE_HALF
 
 //
 // ---------- No Native Half support test -----------
 //
 // RUN: %clang_cc1 -finclude-default-header -x hlsl -triple \
 // RUN:   dxil-pc-shadermodel6.3-library %s -emit-llvm \
-// RUN:   -o - | FileCheck %s -DFNATTRS="noundef nofpclass(nan inf)" \
-// RUN:   -DTYPE=float --check-prefixes=DXCHECK,DXNO_HALF
+// RUN:   -o - | FileCheck %s \
+// RUN:   -DTYPE=float --check-prefixes=CHECK,DXCHECK,DXNO_HALF
 
 
 // Spirv target:
@@ -23,7 +23,7 @@
 // RUN: %clang_cc1 -finclude-default-header -x hlsl -triple \
 // RUN:   spirv-unknown-vulkan-compute %s -fnative-half-type \
 // RUN:   -emit-llvm -o - | FileCheck %s \
-// RUN:   -DFNATTRS="spir_func noundef nofpclass(nan inf)" -DTYPE=half
+// RUN:   -DTYPE=half --check-prefixes=CHECK,SPVCHECK
 
 //
 // ---------- No Native Half support test -----------
@@ -31,11 +31,11 @@
 // RUN: %clang_cc1 -finclude-default-header -x hlsl -triple \
 // RUN:   spirv-unknown-vulkan-compute %s -emit-llvm \
 // RUN:   -o - | FileCheck %s \
-// RUN:   -DFNATTRS="spir_func noundef nofpclass(nan inf)" -DTYPE=float
+// RUN:   -DTYPE=float --check-prefixes=CHECK,SPVCHECK
 
 
 
-// DXCHECK: define [[FNATTRS]] [[TYPE]] @
+// CHECK-LABEL: test_fmod_half
 // DXCHECK: %div1.i = fdiv reassoc nnan ninf nsz arcp afn [[TYPE]]
 // DXCHECK: %fneg.i = fneg reassoc nnan ninf nsz arcp afn [[TYPE]]
 // DXCHECK: %cmp.i = fcmp reassoc nnan ninf nsz arcp afn oge [[TYPE]]
@@ -47,12 +47,11 @@
 // DXCHECK: %hlsl.select.i = select reassoc nnan ninf nsz arcp afn i1
 // DXCHECK: %mul.i = fmul reassoc nnan ninf nsz arcp afn [[TYPE]]
 // DXCHECK: ret [[TYPE]] %mul.i
-// CHECK: define [[FNATTRS]] [[TYPE]] @
-// CHECK: %fmod.i = frem reassoc nnan ninf nsz arcp afn [[TYPE]]
-// CHECK: ret [[TYPE]] %fmod.i
+// SPVCHECK: %fmod.i = frem reassoc nnan ninf nsz arcp afn [[TYPE]]
+// SPVCHECK: ret [[TYPE]] %fmod.i
 half test_fmod_half(half p0, half p1) { return fmod(p0, p1); }
 
-// DXCHECK: define [[FNATTRS]] <2 x [[TYPE]]> @
+// CHECK-LABEL: test_fmod_half2
 // DXCHECK: %div1.i = fdiv reassoc nnan ninf nsz arcp afn <2 x [[TYPE]]>
 // DXCHECK: %fneg.i = fneg reassoc nnan ninf nsz arcp afn <2 x [[TYPE]]>
 // DXCHECK: %cmp.i = fcmp reassoc nnan ninf nsz arcp afn oge <2 x [[TYPE]]>
@@ -64,12 +63,11 @@ half test_fmod_half(half p0, half p1) { return fmod(p0, p1); }
 // DXCHECK: %hlsl.select.i = select reassoc nnan ninf nsz arcp afn <2 x i1>
 // DXCHECK: %mul.i = fmul reassoc nnan ninf nsz arcp afn <2 x [[TYPE]]>
 // DXCHECK: ret <2 x [[TYPE]]> %mul.i
-// CHECK: define [[FNATTRS]] <2 x [[TYPE]]> @
-// CHECK: %fmod.i = frem reassoc nnan ninf nsz arcp afn <2 x [[TYPE]]>
-// CHECK: ret <2 x [[TYPE]]> %fmod.i
+// SPVCHECK: %fmod.i = frem reassoc nnan ninf nsz arcp afn <2 x [[TYPE]]>
+// SPVCHECK: ret <2 x [[TYPE]]> %fmod.i
 half2 test_fmod_half2(half2 p0, half2 p1) { return fmod(p0, p1); }
 
-// DXCHECK: define [[FNATTRS]] <3 x [[TYPE]]> @
+// CHECK-LABEL: test_fmod_half3
 // DXCHECK: %div1.i = fdiv reassoc nnan ninf nsz arcp afn <3 x [[TYPE]]>
 // DXCHECK: %fneg.i = fneg reassoc nnan ninf nsz arcp afn <3 x [[TYPE]]>
 // DXCHECK: %cmp.i = fcmp reassoc nnan ninf nsz arcp afn oge <3 x [[TYPE]]>
@@ -81,12 +79,11 @@ half2 test_fmod_half2(half2 p0, half2 p1) { return fmod(p0, p1); }
 // DXCHECK: %hlsl.select.i = select reassoc nnan ninf nsz arcp afn <3 x i1>
 // DXCHECK: %mul.i = fmul reassoc nnan ninf nsz arcp afn <3 x [[TYPE]]>
 // DXCHECK: ret <3 x [[TYPE]]> %mul.i
-// CHECK: define [[FNATTRS]] <3 x [[TYPE]]> @
-// CHECK: %fmod.i = frem reassoc nnan ninf nsz arcp afn <3 x [[TYPE]]>
-// CHECK: ret <3 x [[TYPE]]> %fmod.i
+// SPVCHECK: %fmod.i = frem reassoc nnan ninf nsz arcp afn <3 x [[TYPE]]>
+// SPVCHECK: ret <3 x [[TYPE]]> %fmod.i
 half3 test_fmod_half3(half3 p0, half3 p1) { return fmod(p0, p1); }
 
-// DXCHECK: define [[FNATTRS]] <4 x [[TYPE]]> @
+// CHECK-LABEL: test_fmod_half4
 // DXCHECK: %div1.i = fdiv reassoc nnan ninf nsz arcp afn <4 x [[TYPE]]>
 // DXCHECK: %fneg.i = fneg reassoc nnan ninf nsz arcp afn <4 x [[TYPE]]>
 // DXCHECK: %cmp.i = fcmp reassoc nnan ninf nsz arcp afn oge <4 x [[TYPE]]>
@@ -98,12 +95,11 @@ half3 test_fmod_half3(half3 p0, half3 p1) { return fmod(p0, p1); }
 // DXCHECK: %hlsl.select.i = select reassoc nnan ninf nsz arcp afn <4 x i1>
 // DXCHECK: %mul.i = fmul reassoc nnan ninf nsz arcp afn <4 x [[TYPE]]>
 // DXCHECK: ret <4 x [[TYPE]]> %mul.i
-// CHECK: define [[FNATTRS]] <4 x [[TYPE]]> @
-// CHECK: %fmod.i = frem reassoc nnan ninf nsz arcp afn <4 x [[TYPE]]>
-// CHECK: ret <4 x [[TYPE]]> %fmod.i
+// SPVCHECK: %fmod.i = frem reassoc nnan ninf nsz arcp afn <4 x [[TYPE]]>
+// SPVCHECK: ret <4 x [[TYPE]]> %fmod.i
 half4 test_fmod_half4(half4 p0, half4 p1) { return fmod(p0, p1); }
 
-// DXCHECK: define [[FNATTRS]] float @
+// CHECK-LABEL: test_fmod_float
 // DXCHECK: %div1.i = fdiv reassoc nnan ninf nsz arcp afn float
 // DXCHECK: %fneg.i = fneg reassoc nnan ninf nsz arcp afn float
 // DXCHECK: %cmp.i = fcmp reassoc nnan ninf nsz arcp afn oge float
@@ -113,12 +109,11 @@ half4 test_fmod_half4(half4 p0, half4 p1) { return fmod(p0, p1); }
 // DXCHECK: %hlsl.select.i = select reassoc nnan ninf nsz arcp afn i1 
 // DXCHECK: %mul.i = fmul reassoc nnan ninf nsz arcp afn float 
 // DXCHECK: ret float %mul.i
-// CHECK: define [[FNATTRS]] float @
-// CHECK: %fmod.i = frem reassoc nnan ninf nsz arcp afn float
-// CHECK: ret float %fmod.i
+// SPVCHECK: %fmod.i = frem reassoc nnan ninf nsz arcp afn float
+// SPVCHECK: ret float %fmod.i
 float test_fmod_float(float p0, float p1) { return fmod(p0, p1); }
 
-// DXCHECK: define [[FNATTRS]] <2 x float> @
+// CHECK-LABEL: test_fmod_float2
 // DXCHECK: %div1.i = fdiv reassoc nnan ninf nsz arcp afn <2 x float>
 // DXCHECK: %fneg.i = fneg reassoc nnan ninf nsz arcp afn <2 x float>
 // DXCHECK: %cmp.i = fcmp reassoc nnan ninf nsz arcp afn oge <2 x float>
@@ -128,12 +123,11 @@ float test_fmod_float(float p0, float p1) { return fmod(p0, p1); }
 // DXCHECK: %hlsl.select.i = select reassoc nnan ninf nsz arcp afn <2 x i1>
 // DXCHECK: %mul.i = fmul reassoc nnan ninf nsz arcp afn <2 x float>
 // DXCHECK: ret <2 x float> %mul.i
-// CHECK: define [[FNATTRS]] <2 x float> @
-// CHECK: %fmod.i = frem reassoc nnan ninf nsz arcp afn <2 x float>
-// CHECK: ret <2 x float> %fmod.i
+// SPVCHECK: %fmod.i = frem reassoc nnan ninf nsz arcp afn <2 x float>
+// SPVCHECK: ret <2 x float> %fmod.i
 float2 test_fmod_float2(float2 p0, float2 p1) { return fmod(p0, p1); }
 
-// DXCHECK: define [[FNATTRS]] <3 x float> @
+// CHECK-LABEL: test_fmod_float3
 // DXCHECK: %div1.i = fdiv reassoc nnan ninf nsz arcp afn <3 x float>
 // DXCHECK: %fneg.i = fneg reassoc nnan ninf nsz arcp afn <3 x float>
 // DXCHECK: %cmp.i = fcmp reassoc nnan ninf nsz arcp afn oge <3 x float>
@@ -143,12 +137,11 @@ float2 test_fmod_float2(float2 p0, float2 p1) { return fmod(p0, p1); }
 // DXCHECK: %hlsl.select.i = select reassoc nnan ninf nsz arcp afn <3 x i1>
 // DXCHECK: %mul.i = fmul reassoc nnan ninf nsz arcp afn <3 x float>
 // DXCHECK: ret <3 x float> %mul.i
-// CHECK: define [[FNATTRS]] <3 x float> @
-// CHECK: %fmod.i = frem reassoc nnan ninf nsz arcp afn <3 x float>
-// CHECK: ret <3 x float> %fmod.i
+// SPVCHECK: %fmod.i = frem reassoc nnan ninf nsz arcp afn <3 x float>
+// SPVCHECK: ret <3 x float> %fmod.i
 float3 test_fmod_float3(float3 p0, float3 p1) { return fmod(p0, p1); }
 
-// DXCHECK: define [[FNATTRS]] <4 x float> @
+// CHECK-LABEL: test_fmod_float4
 // DXCHECK: %div1.i = fdiv reassoc nnan ninf nsz arcp afn <4 x float>
 // DXCHECK: %fneg.i = fneg reassoc nnan ninf nsz arcp afn <4 x float>
 // DXCHECK: %cmp.i = fcmp reassoc nnan ninf nsz arcp afn oge <4 x float>
@@ -158,8 +151,7 @@ float3 test_fmod_float3(float3 p0, float3 p1) { return fmod(p0, p1); }
 // DXCHECK: %hlsl.select.i = select reassoc nnan ninf nsz arcp afn <4 x i1>
 // DXCHECK: %mul.i = fmul reassoc nnan ninf nsz arcp afn <4 x float>
 // DXCHECK: ret <4 x float> %mul.i
-// CHECK: define [[FNATTRS]] <4 x float> @
-// CHECK: %fmod.i = frem reassoc nnan ninf nsz arcp afn <4 x float>
-// CHECK: ret <4 x float> %fmod.i
+// SPVCHECK: %fmod.i = frem reassoc nnan ninf nsz arcp afn <4 x float>
+// SPVCHECK: ret <4 x float> %fmod.i
 float4 test_fmod_float4(float4 p0, float4 p1) { return fmod(p0, p1); }
 
