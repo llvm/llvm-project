482717706be6162559abf86e0080fc22d9a85554
Jonas Paulsson <paulson1@linux.ibm.com>
2025-03-08 10:27:13 -0600

    Updated per review + fix.

--- compiler-rt/lib/builtins/extendhfdf2.c
+++ compiler-rt/lib/builtins/extendhfdf2.c
@@ -12,16 +12,6 @@
 
 // Use a forwarding definition and noinline to implement a poor man's alias,
 // as there isn't a good cross-platform way of defining one.
-COMPILER_RT_ABI NOINLINE float __extendhfdf2(src_t a) {
+COMPILER_RT_ABI NOINLINE dst_t __extendhfdf2(src_t a) {
   return __extendXfYf2__(a);
 }
-
-COMPILER_RT_ABI float __gnu_h2d_ieee(src_t a) { return __extendhfdf2(a); }
-
-#if defined(__ARM_EABI__)
-#if defined(COMPILER_RT_ARMHF_TARGET)
-AEABI_RTABI float __aeabi_h2d(src_t a) { return __extendhfdf2(a); }
-#else
-COMPILER_RT_ALIAS(__extendhfdf2, __aeabi_h2d)
-#endif
-#endif
