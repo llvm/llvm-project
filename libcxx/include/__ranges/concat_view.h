8c9d071ac34fd7b4f9468e07cd7a13374f34323b
Nhat Nguyen <nhat7203@gmail.com>
2025-03-08 13:05:26 -0500

    Update libcxx/include/__ranges/concat_view.h
    Co-authored-by: A. Jiang <de34@live.cn>

--- libcxx/include/__ranges/concat_view.h
+++ libcxx/include/__ranges/concat_view.h
@@ -432,7 +432,7 @@ public:
     requires __concat_is_random_access<_Const, _Views...>
   {
     _LIBCPP_ASSERT_VALID_ELEMENT_ACCESS(
-        !__it_.valueless_by_exception(), "Trying to convert from a valueless iterator of concat_view.");
+        !__it_.valueless_by_exception(), "Trying to increment a valueless iterator of concat_view.");
     size_t __active_index = __it_.index();
     if (__n > 0) {
       __variant_detail::__visitation::__variant::__visit_value(
