69340e1a031935f1de333e5ff3c719d4d0a54992
Julien Villette <julien.villette@sipearl.com>
2025-03-10 15:48:01 +0100

    [MCA] Fix formatting

--- llvm/tools/llvm-mca/llvm-mca.cpp
+++ llvm/tools/llvm-mca/llvm-mca.cpp
@@ -259,10 +259,11 @@ static cl::opt<bool> ShowBarriers(
     cl::desc("Print memory barrier information in the instruction info view"),
     cl::cat(ViewOptions), cl::init(false));
 
-static cl::opt<bool> ShowSchedulingInfo(
-    "scheduling-info",
-    cl::desc("Print the instruction scheduling information in the instruction info view"),
-    cl::cat(ViewOptions), cl::init(false));
+static cl::opt<bool>
+    ShowSchedulingInfo("scheduling-info",
+                       cl::desc("Print the instruction scheduling information "
+                                "in the instruction info view"),
+                       cl::cat(ViewOptions), cl::init(false));
 
 static cl::opt<bool> DisableCustomBehaviour(
     "disable-cb",
@@ -683,13 +684,13 @@ int main(int argc, char **argv) {
 
       // Create the views for this pipeline, execute, and emit a report.
       if (PrintInstructionInfoView) {
-	Printer.addView(std::make_unique<mca::InstructionInfoView>(
-              *STI, *MCII, CE, ShowEncoding, Insts, *IP, LoweredSequence,
-              ShowBarriers, ShowSchedulingInfo, *IM, InstToInstruments));
+        Printer.addView(std::make_unique<mca::InstructionInfoView>(
+            *STI, *MCII, CE, ShowEncoding, Insts, *IP, LoweredSequence,
+            ShowBarriers, ShowSchedulingInfo, *IM, InstToInstruments));
       }
 
       Printer.addView(
-	   std::make_unique<mca::ResourcePressureView>(*STI, *IP, Insts));
+          std::make_unique<mca::ResourcePressureView>(*STI, *IP, Insts));
 
       if (!runPipeline(*P))
         return 1;
