b6d3023a2958501284ab936dc45c69740d8609e0
Dhruv-Srivastava <dhruv.srivastava@ibm.com>
2025-03-10 08:45:35 -0500

    Addressed comments

--- lldb/source/Host/aix/Host.cpp
+++ lldb/source/Host/aix/Host.cpp
@@ -7,46 +7,11 @@
 //===----------------------------------------------------------------------===//
 
 #include "lldb/Host/Host.h"
-#include "lldb/Utility/ProcessInfo.h"
 #include "lldb/Utility/Status.h"
-#include <dirent.h>
 
 using namespace llvm;
 using namespace lldb;
 using namespace lldb_private;
-
-namespace {
-enum class ProcessState {
-  Unknown,
-  Dead,
-  DiskSleep,
-  Idle,
-  Paging,
-  Parked,
-  Running,
-  Sleeping,
-  TracedOrStopped,
-  Zombie,
-};
-}
-
-namespace lldb_private {
-class ProcessLaunchInfo;
-}
-
-static bool GetStatusInfo(::pid_t Pid, ProcessInstanceInfo &ProcessInfo,
-                          ProcessState &State, ::pid_t &TracerPid,
-                          ::pid_t &Tgid) {
-  return false;
-}
-
-static void GetProcessArgs(::pid_t pid, ProcessInstanceInfo &process_info) {}
-
-static bool GetProcessAndStatInfo(::pid_t pid,
-                                  ProcessInstanceInfo &process_info,
-                                  ProcessState &State, ::pid_t &tracerpid) {
-  return false;
-}
 
 uint32_t Host::FindProcessesImpl(const ProcessInstanceInfoMatch &match_info,
                                  ProcessInstanceInfoList &process_infos) {
@@ -54,12 +19,8 @@ uint32_t Host::FindProcessesImpl(const ProcessInstanceInfoMatch &match_info,
 }
 
 bool Host::GetProcessInfo(lldb::pid_t pid, ProcessInstanceInfo &process_info) {
-  ::pid_t tracerpid;
-  ProcessState State;
-  return GetProcessAndStatInfo(pid, process_info, State, tracerpid);
+  return false;
 }
-
-Environment Host::GetEnvironment() { return Environment(environ); }
 
 Status Host::ShellExpandArguments(ProcessLaunchInfo &launch_info) {
   return Status("unimplemented");
