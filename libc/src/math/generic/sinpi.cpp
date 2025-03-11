ef20b757cb1f172da6a13675f0ac91cbdae82686
Job Hernandez <jobricciab@gmail.com>
2025-03-11 14:01:29 -0700

    improve styling

--- libc/src/math/generic/sinpi.cpp
+++ libc/src/math/generic/sinpi.cpp
@@ -13,6 +13,7 @@
 #include "src/__support/FPUtil/FPBits.h"
 #include "src/__support/FPUtil/PolyEval.h"
 #include "src/__support/FPUtil/multiply_add.h"
+#include "src/__support/FPUtil/BasicOperations.h"
 #include "src/__support/common.h"
 #include "src/__support/macros/config.h"
 #include "src/__support/macros/optimization.h" // LIBC_UNLIKELY
@@ -49,33 +50,18 @@ LLVM_LIBC_FUNCTION(double, sinpi, (double x)) {
   FPBits xbits(x);
 
   uint64_t x_u = xbits.uintval();
-  uint64_t x_abs = x_u & 0x7fff'ffffU;
-
-  if (LIBC_UNLIKELY(x_abs >= 0x7c00)) {
-    // If value is equal to infinity
-    if (x_abs == 0x7c00) {
-      fputil::set_errno_if_required(EDOM);
-      fputil::raise_except_if_required(FE_INVALID);
-    }
-    return x + FPBits::quiet_nan().get_val();
-  } else {
-    return FPBits::zero(xbits.sign()).get_val();
+  double x_abs = fputil::abs(x);
+  double p = 0x1p52; // precision = 52; 2^p
+  
+  if (x_abs >= p) {
+    if (xbits.is_nan())
+      return x;
+    if  (x.bits.is_inf()) {
+    fputil::set_errno_if_required(EDOM);
+    fputil::raise_except_if_required(FE_INVALID);
+    return FPBits::quiet_nan().get_val();
   }
-
-  if (LIBC_UNLIKELY(x_abs <= 0x3d80'0000U)) {
-    if (LIBC_UNLIKELY(x_abs < 0x33CD'01D7U)) {
-      if (LIBC_UNLIKELY(x_abs == 0U)) {
-	return x;
-    }
-      double  xdpi = xd * 0x1.921fb5444d18p1;
-      return xdpi;
-      
-    }
-    double xsq = xd * xd;
-    // todo: generate a new polynomial using double precision
-    double result = fputil::polyeval(xsq,0x1.921fb54442d183f07b2385653d8p1, -0x1.4abbce625bd95cdc955aeed9abcp2, 0x1.466bc6769ddfdb085486c0ff3ep1, -0x1.32d2c4a48bfd71fa9cdf60a0e4p-1,  0x1.502cbd2c72e3168ff209bc7656cp-4);
-
-  return (xd * result);
+    return FPBits::zero(xbits.sign()).get_val();
   }
 
   double sin_k, cos_k, sin_y, cosm1_y;
@@ -86,4 +72,4 @@ LLVM_LIBC_FUNCTION(double, sinpi, (double x)) {
 
   return fputil::multiply_add(sin_y, cos_k, fputil::multiply_add(cosm1_y, sin_k, sin_k)));
 }
-}
+
