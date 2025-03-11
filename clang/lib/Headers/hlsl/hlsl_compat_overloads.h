47c90c4be48c0c54dbfa803cb15fada50061dfa5
Sarah Spall <sarahspall@microsoft.com>
2025-03-11 15:13:18 -0700

    add vector size checks to clamp templates

--- clang/lib/Headers/hlsl/hlsl_compat_overloads.h
+++ clang/lib/Headers/hlsl/hlsl_compat_overloads.h
@@ -22,27 +22,27 @@ namespace hlsl {
 //===----------------------------------------------------------------------===//
 
 template <typename T, typename R, typename U, uint N>
-constexpr __detail::enable_if_t<__detail::is_arithmetic<U>::Value, vector<T, N>>
+constexpr __detail::enable_if_t<__detail::is_arithmetic<U>::Value && (N > 1 && N <= 4), vector<T, N>>
 clamp(vector<T, N> p0, vector<R, N> p1, U p2) {
   return __builtin_hlsl_elementwise_clamp(p0, (vector<T, N>)p1,
                                           (vector<T, N>)p2);
 }
 template <typename T, typename R, typename U, uint N>
-constexpr __detail::enable_if_t<__detail::is_arithmetic<U>::Value, vector<T, N>>
+constexpr __detail::enable_if_t<__detail::is_arithmetic<U>::Value && (N > 1 && N <= 4), vector<T, N>>
 clamp(vector<T, N> p0, U p1, vector<R, N> p2) {
   return __builtin_hlsl_elementwise_clamp(p0, (vector<T, N>)p1,
                                           (vector<T, N>)p2);
 }
 template <typename T, typename U, typename V, uint N>
 constexpr __detail::enable_if_t<__detail::is_arithmetic<U>::Value &&
-                                    __detail::is_arithmetic<V>::Value,
+  __detail::is_arithmetic<V>::Value && (N > 1 && N <= 4),
                                 vector<T, N>>
 clamp(vector<T, N> p0, U p1, V p2) {
   return __builtin_hlsl_elementwise_clamp(p0, (vector<T, N>)p1,
                                           (vector<T, N>)p2);
 }
 template <typename T, typename R, typename S, uint N>
-constexpr vector<T, N> clamp(vector<T, N> p0, vector<R, N> p1,
+constexpr __detail::enable_if_t<(N > 1 && N <= 4), vector<T, N>> clamp(vector<T, N> p0, vector<R, N> p1,
                              vector<S, N> p2) {
   return __builtin_hlsl_elementwise_clamp(p0, (vector<T, N>)p1,
                                           (vector<T, N>)p2);
