1e64f0990653e939d1e30d87d71907df973ec89d
Akshat Oke <Akshat.Oke@amd.com>
2025-03-12 06:03:07 +0000

    format and sort registry

--- llvm/lib/Target/AMDGPU/GCNCreateVOPD.cpp
+++ llvm/lib/Target/AMDGPU/GCNCreateVOPD.cpp
@@ -49,108 +49,105 @@ private:
     MachineInstr *SecondMI;
   };
 
-  public:
-    const GCNSubtarget *ST = nullptr;
+public:
+  const GCNSubtarget *ST = nullptr;
 
-    bool doReplace(const SIInstrInfo *SII, VOPDCombineInfo &CI) {
-      auto *FirstMI = CI.FirstMI;
-      auto *SecondMI = CI.SecondMI;
-      unsigned Opc1 = FirstMI->getOpcode();
-      unsigned Opc2 = SecondMI->getOpcode();
-      unsigned EncodingFamily =
-          AMDGPU::getVOPDEncodingFamily(SII->getSubtarget());
-      int NewOpcode =
-          AMDGPU::getVOPDFull(AMDGPU::getVOPDOpcode(Opc1),
-                              AMDGPU::getVOPDOpcode(Opc2), EncodingFamily);
-      assert(NewOpcode != -1 &&
-             "Should have previously determined this as a possible VOPD\n");
+  bool doReplace(const SIInstrInfo *SII, VOPDCombineInfo &CI) {
+    auto *FirstMI = CI.FirstMI;
+    auto *SecondMI = CI.SecondMI;
+    unsigned Opc1 = FirstMI->getOpcode();
+    unsigned Opc2 = SecondMI->getOpcode();
+    unsigned EncodingFamily =
+        AMDGPU::getVOPDEncodingFamily(SII->getSubtarget());
+    int NewOpcode =
+        AMDGPU::getVOPDFull(AMDGPU::getVOPDOpcode(Opc1),
+                            AMDGPU::getVOPDOpcode(Opc2), EncodingFamily);
+    assert(NewOpcode != -1 &&
+           "Should have previously determined this as a possible VOPD\n");
 
-      auto VOPDInst =
-          BuildMI(*FirstMI->getParent(), FirstMI, FirstMI->getDebugLoc(),
-                  SII->get(NewOpcode))
-              .setMIFlags(FirstMI->getFlags() | SecondMI->getFlags());
+    auto VOPDInst = BuildMI(*FirstMI->getParent(), FirstMI,
+                            FirstMI->getDebugLoc(), SII->get(NewOpcode))
+                        .setMIFlags(FirstMI->getFlags() | SecondMI->getFlags());
 
-      namespace VOPD = AMDGPU::VOPD;
-      MachineInstr *MI[] = {FirstMI, SecondMI};
-      auto InstInfo =
-          AMDGPU::getVOPDInstInfo(FirstMI->getDesc(), SecondMI->getDesc());
+    namespace VOPD = AMDGPU::VOPD;
+    MachineInstr *MI[] = {FirstMI, SecondMI};
+    auto InstInfo =
+        AMDGPU::getVOPDInstInfo(FirstMI->getDesc(), SecondMI->getDesc());
 
-      for (auto CompIdx : VOPD::COMPONENTS) {
-        auto MCOprIdx = InstInfo[CompIdx].getIndexOfDstInMCOperands();
+    for (auto CompIdx : VOPD::COMPONENTS) {
+      auto MCOprIdx = InstInfo[CompIdx].getIndexOfDstInMCOperands();
+      VOPDInst.add(MI[CompIdx]->getOperand(MCOprIdx));
+    }
+
+    for (auto CompIdx : VOPD::COMPONENTS) {
+      auto CompSrcOprNum = InstInfo[CompIdx].getCompSrcOperandsNum();
+      for (unsigned CompSrcIdx = 0; CompSrcIdx < CompSrcOprNum; ++CompSrcIdx) {
+        auto MCOprIdx = InstInfo[CompIdx].getIndexOfSrcInMCOperands(CompSrcIdx);
         VOPDInst.add(MI[CompIdx]->getOperand(MCOprIdx));
       }
-
-      for (auto CompIdx : VOPD::COMPONENTS) {
-        auto CompSrcOprNum = InstInfo[CompIdx].getCompSrcOperandsNum();
-        for (unsigned CompSrcIdx = 0; CompSrcIdx < CompSrcOprNum;
-             ++CompSrcIdx) {
-          auto MCOprIdx =
-              InstInfo[CompIdx].getIndexOfSrcInMCOperands(CompSrcIdx);
-          VOPDInst.add(MI[CompIdx]->getOperand(MCOprIdx));
-        }
-      }
-
-      SII->fixImplicitOperands(*VOPDInst);
-      for (auto CompIdx : VOPD::COMPONENTS)
-        VOPDInst.copyImplicitOps(*MI[CompIdx]);
-
-      LLVM_DEBUG(dbgs() << "VOPD Fused: " << *VOPDInst << " from\tX: "
-                        << *CI.FirstMI << "\tY: " << *CI.SecondMI << "\n");
-
-      for (auto CompIdx : VOPD::COMPONENTS)
-        MI[CompIdx]->eraseFromParent();
-
-      ++NumVOPDCreated;
-      return true;
     }
 
-    bool run(MachineFunction &MF) {
-      ST = &MF.getSubtarget<GCNSubtarget>();
-      if (!AMDGPU::hasVOPD(*ST) || !ST->isWave32())
-        return false;
-      LLVM_DEBUG(dbgs() << "CreateVOPD Pass:\n");
+    SII->fixImplicitOperands(*VOPDInst);
+    for (auto CompIdx : VOPD::COMPONENTS)
+      VOPDInst.copyImplicitOps(*MI[CompIdx]);
 
-      const SIInstrInfo *SII = ST->getInstrInfo();
-      bool Changed = false;
+    LLVM_DEBUG(dbgs() << "VOPD Fused: " << *VOPDInst << " from\tX: "
+                      << *CI.FirstMI << "\tY: " << *CI.SecondMI << "\n");
 
-      SmallVector<VOPDCombineInfo> ReplaceCandidates;
+    for (auto CompIdx : VOPD::COMPONENTS)
+      MI[CompIdx]->eraseFromParent();
 
-      for (auto &MBB : MF) {
-        auto MII = MBB.begin(), E = MBB.end();
-        while (MII != E) {
-          auto *FirstMI = &*MII;
-          MII = next_nodbg(MII, MBB.end());
-          if (MII == MBB.end())
-            break;
-          if (FirstMI->isDebugInstr())
-            continue;
-          auto *SecondMI = &*MII;
-          unsigned Opc = FirstMI->getOpcode();
-          unsigned Opc2 = SecondMI->getOpcode();
-          llvm::AMDGPU::CanBeVOPD FirstCanBeVOPD = AMDGPU::getCanBeVOPD(Opc);
-          llvm::AMDGPU::CanBeVOPD SecondCanBeVOPD = AMDGPU::getCanBeVOPD(Opc2);
-          VOPDCombineInfo CI;
+    ++NumVOPDCreated;
+    return true;
+  }
 
-          if (FirstCanBeVOPD.X && SecondCanBeVOPD.Y)
-            CI = VOPDCombineInfo(FirstMI, SecondMI);
-          else if (FirstCanBeVOPD.Y && SecondCanBeVOPD.X)
-            CI = VOPDCombineInfo(SecondMI, FirstMI);
-          else
-            continue;
-          // checkVOPDRegConstraints cares about program order, but doReplace
-          // cares about X-Y order in the constituted VOPD
-          if (llvm::checkVOPDRegConstraints(*SII, *FirstMI, *SecondMI)) {
-            ReplaceCandidates.push_back(CI);
-            ++MII;
-          }
+  bool run(MachineFunction &MF) {
+    ST = &MF.getSubtarget<GCNSubtarget>();
+    if (!AMDGPU::hasVOPD(*ST) || !ST->isWave32())
+      return false;
+    LLVM_DEBUG(dbgs() << "CreateVOPD Pass:\n");
+
+    const SIInstrInfo *SII = ST->getInstrInfo();
+    bool Changed = false;
+
+    SmallVector<VOPDCombineInfo> ReplaceCandidates;
+
+    for (auto &MBB : MF) {
+      auto MII = MBB.begin(), E = MBB.end();
+      while (MII != E) {
+        auto *FirstMI = &*MII;
+        MII = next_nodbg(MII, MBB.end());
+        if (MII == MBB.end())
+          break;
+        if (FirstMI->isDebugInstr())
+          continue;
+        auto *SecondMI = &*MII;
+        unsigned Opc = FirstMI->getOpcode();
+        unsigned Opc2 = SecondMI->getOpcode();
+        llvm::AMDGPU::CanBeVOPD FirstCanBeVOPD = AMDGPU::getCanBeVOPD(Opc);
+        llvm::AMDGPU::CanBeVOPD SecondCanBeVOPD = AMDGPU::getCanBeVOPD(Opc2);
+        VOPDCombineInfo CI;
+
+        if (FirstCanBeVOPD.X && SecondCanBeVOPD.Y)
+          CI = VOPDCombineInfo(FirstMI, SecondMI);
+        else if (FirstCanBeVOPD.Y && SecondCanBeVOPD.X)
+          CI = VOPDCombineInfo(SecondMI, FirstMI);
+        else
+          continue;
+        // checkVOPDRegConstraints cares about program order, but doReplace
+        // cares about X-Y order in the constituted VOPD
+        if (llvm::checkVOPDRegConstraints(*SII, *FirstMI, *SecondMI)) {
+          ReplaceCandidates.push_back(CI);
+          ++MII;
         }
       }
-      for (auto &CI : ReplaceCandidates) {
-        Changed |= doReplace(SII, CI);
-      }
-
-      return Changed;
     }
+    for (auto &CI : ReplaceCandidates) {
+      Changed |= doReplace(SII, CI);
+    }
+
+    return Changed;
+  }
 };
 
 class GCNCreateVOPDLegacy : public MachineFunctionPass {
