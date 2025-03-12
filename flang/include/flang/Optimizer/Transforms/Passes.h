72133135e6ae2068018dbb22aa0df4b9e09512c1
Slava Zakharin <szakharin@nvidia.com>
2025-03-12 16:56:21 -0700

    [flang] Expand new FIR box operations into runtime calls.

--- flang/include/flang/Optimizer/Transforms/Passes.h
+++ flang/include/flang/Optimizer/Transforms/Passes.h
@@ -62,6 +62,7 @@ namespace fir {
 #define GEN_PASS_DECL_COMPILERGENERATEDNAMESCONVERSION
 #define GEN_PASS_DECL_SETRUNTIMECALLATTRIBUTES
 #define GEN_PASS_DECL_GENRUNTIMECALLSFORTEST
+#define GEN_PASS_DECL_SIMPLIFYFIROPERATIONS
 
 #include "flang/Optimizer/Transforms/Passes.h.inc"
 
@@ -85,6 +86,9 @@ createVScaleAttrPass(std::pair<unsigned, unsigned> vscaleAttr);
 void populateCfgConversionRewrites(mlir::RewritePatternSet &patterns,
                                    bool forceLoopToExecuteOnce = false,
                                    bool setNSW = true);
+
+void populateSimplifyFIROperationsPatterns(mlir::RewritePatternSet &patterns,
+                                           bool preferInlineImplementation);
 
 // declarative passes
 #define GEN_PASS_REGISTRATION
