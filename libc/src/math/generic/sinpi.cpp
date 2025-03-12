4b9623b26d9268567d130e47dfcfc816e84e4182
Job Hernandez <lara2993@proton.me>
2025-03-11 22:56:10 -0700

    add new test

--- libc/src/math/generic/sinpi.cpp
+++ libc/src/math/generic/sinpi.cpp
@@ -22,7 +22,28 @@
 #include "src/math/fmul.h"
 
 namespace LIBC_NAMESPACE_DECL {
- 
+
+
+static LIBC_INLINE void sincospi_poly_eval(double k, double y, double &sin_k,
+                                          double &cos_k, double &sin_y,
+
+
+					  double &cosm1_y) {
+  // Q3 = fpminimax(sin(x*pi), 7, [|64...|], [-0.0078125, 0.0078125]);
+  sin_k =
+    k * fputil::polyeval(k, 0x1.59b6a771a45cbab8p-94, 0x1.921fb54442d1846ap1, -0x1.8633470ba8bd806cp-76, -0x1.4abbce625be56346p2, 0x1.d3e01dfd72e97a92p-61, 0x1.466bc67713dbbfp1, -0x1.14c2648595e2ad4p-47, -0x1.32d1cc20b89301fcp-1);
+
+  sin_y =
+   y *  fputil::polyeval(y, 0x1.59b6a771a45cbab8p-94, 0x1.921fb54442d1846ap1, -0x1.8633470ba8bd806cp-76, -0x1.4abbce625be56346p2, 0x1.d3e01dfd72e97a92p-61, 0x1.466bc67713dbbfp1, -0x1.14c2648595e2ad4p-47, -0x1.32d1cc20b89301fcp-1);
+
+  // Q1 = fpminimax(cos(x * pi), 7, [|64...|], [-0.0078125, 0.0078125]);
+  cos_k =
+    k * fputil::polyeval(k, 0x1p0, 0x1.a5b22c564ee1d862p-84, -0x1.3bd3cc9be45d30e6p2, -0x1.5c2328fefbe60d3ep-66, 0x1.03c1f080a6907a6p2, 0x1.569a4d5c5018eecap-51, -0x1.55d1f72455a9848ap0, -0x1.6b18e5f7fc6c39a6p-38);
+
+  cosm1_y =
+    y * fputil::polyeval(k, 0x1p0, 0x1.a5b22c564ee1d862p-84, -0x1.3bd3cc9be45d30e6p2, -0x1.5c2328fefbe60d3ep-66, 0x1.03c1f080a6907a6p2, 0x1.569a4d5c5018eecap-51, -0x1.55d1f72455a9848ap0, -0x1.6b18e5f7fc6c39a6p-38);
+}
+
 LLVM_LIBC_FUNCTION(double, sinpi, (double x)) {
   using FPBits = typename fputil::FPBits<double>;
   FPBits xbits(x);
@@ -46,16 +67,16 @@ LLVM_LIBC_FUNCTION(double, sinpi, (double x)) {
   }
   double n = pow(2, -52);
   double k = fputil::nearest_integer(x * n);
-  FPBits kbits(x);
-  uint64_t ku = kbits.uintval();
   double y = x - k;
   double sin_k, cos_k, sin_y, cosm1_y;
-  sincosf_poly_eval(ku, y,  sin_k, cos_k, sin_y, cosm1_y);
+
+  sincospi_poly_eval(k, y,  sin_k, cos_k, sin_y, cosm1_y);
+
 
   if (LIBC_UNLIKELY(sin_y == 0 && sin_k == 0))
     return FPBits::zero(xbits.sign()).get_val();
 
-  return fputil::multiply_add(sin_y, cos_k, fputil::multiply_add(cosm1_y, sin_k, sin_k));
+  return fputil::cast<double>(fputil::multiply_add(sin_y, cos_k, fputil::multiply_add(cosm1_y, sin_k, sin_k)));
 }
 }
 
