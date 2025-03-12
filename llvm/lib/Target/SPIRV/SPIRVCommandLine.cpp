bb188b2fae97482c399f3c7286d3913cdf3ea733
EbinJose2002 <ebin.jose@multicorewareinc.com>
2025-03-12 16:05:40 +0530

    FEAT :  - Added support for extension SPV_INTEL_runtime_aligned  - Added function parameter attribute RuntimeAlignedINTEL and capability RuntimeAlignedAttributeINTEL

--- llvm/lib/Target/SPIRV/SPIRVCommandLine.cpp
+++ llvm/lib/Target/SPIRV/SPIRVCommandLine.cpp
@@ -90,7 +90,9 @@ static const std::map<std::string, SPIRV::Extension::Extension, std::less<>>
         {"SPV_KHR_non_semantic_info",
          SPIRV::Extension::Extension::SPV_KHR_non_semantic_info},
         {"SPV_INTEL_long_composites",
-         SPIRV::Extension::Extension::SPV_INTEL_long_composites}};
+         SPIRV::Extension::Extension::SPV_INTEL_long_composites},
+        {"SPV_INTEL_runtime_aligned",
+         SPIRV::Extension::Extension::SPV_INTEL_runtime_aligned}};
 
 bool SPIRVExtensionsParser::parse(cl::Option &O, llvm::StringRef ArgName,
                                   llvm::StringRef ArgValue,
