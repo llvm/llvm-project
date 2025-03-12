44b68cd5ad06d38b4264ebe13efd81061f1c232d
Donát Nagy <donat.nagy@ericsson.com>
2025-03-12 18:28:43 +0100

    Update `CheckerBase::getTagDescription`
    Expand the FIXME note and make the implementation visibly crash-proof.
    (Before this commit dumping the `ExplodedGraph` could cause a crash in
    the unlikely situation when `core.DivideZero` is disabled but
    `optin.taint.TaintedDiv` is enabled.)
    
    I will clean up this method soon, but perhaps in a separate PR.

--- clang/include/clang/StaticAnalyzer/Core/Checker.h
+++ clang/include/clang/StaticAnalyzer/Core/Checker.h
@@ -516,8 +516,23 @@ public:
   }
 
   StringRef getTagDescription() const override {
-    // FIXME: This assumes that the default part is always registered.
-    return getName(DefaultPart);
+    // This method inherited from `ProgramPointTag` has two unrelated roles:
+    // (1) The analyzer option handling logic uses this method to query the
+    //     name of a checker. 
+    // (2) When the `ExplodedGraph` is dumped in DOT format for debugging,
+    //     this is called to attach a description to the nodes. (This happens
+    //     for all subclasses of `ProgramPointTag`, not just checkers.)
+    // FIXME: Application (1) should be aware of multiple parts within the same
+    // checker class instance, so it should directly use `getName` instead of
+    // this inherited interface which cannot support a `CheckerPartIdx`.
+    // FIXME: Ideally application (2) should return a string that describes the
+    // whole checker class, not just one of it parts. However, this is only for
+    // debugging, so returning the name of one part is probably good enough.
+    for (const auto &OptName : RegisteredNames) 
+      if (OptName)
+        return *OptName;
+
+    return "Unregistered checker";
   }
 
   /// Debug state dump callback, see CheckerManager::runCheckersForPrintState.
