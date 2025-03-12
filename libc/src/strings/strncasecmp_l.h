0b208dbcd9280b3f818d7836c421972cb37dd9c3
Connector Switch <c8ef@outlook.com>
2025-03-12 08:10:20 +0800

    Update libc/src/strings/strncasecmp_l.h
    Co-authored-by: Michael Jones <michaelrj@google.com>

--- libc/src/strings/strncasecmp_l.h
+++ libc/src/strings/strncasecmp_l.h
@@ -1,4 +1,4 @@
-//===-- Implementation header for strcasecmp_l ------------------*- C++ -*-===//
+//===-- Implementation header for strncasecmp_l -----------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
