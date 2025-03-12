12d9237d7a0650328f6167b1951195cd851f6234
Megan <megankuo@asu.edu>
2025-03-12 01:24:35 -0700

    [Sema] Fix test diagnostics for copy-ctor-template

--- clang/test/SemaCXX/copy-ctor-template.cpp
+++ clang/test/SemaCXX/copy-ctor-template.cpp
@@ -2,13 +2,20 @@
 
 template<class T, class V>
 struct A{
-    A();
-    A(A&);
+    A(); // expected-note{{candidate constructor not viable: requires 0 arguments, but 1 was provided}}
+    A(A&); // expected-note{{candidate constructor not viable: expects an lvalue for 1st argument}} 
     A(A<V, T>); // expected-error{{copy constructor must pass its first argument by reference}}
 };
 
 void f() {
-    A<int, int> a = A<int, int>(); // expected-note{{in instantiation of template class 'A<int, int>'}}
+    A<int, int> a = A<int, int>(); // expected-note{{in instantiation of template class 'A<int, int>'}} 
+    A<int, double> a1 = A<double, int>(); // No error (not a copy constructor)
+}
+
+// Test rvalue-to-lvalue conversion in copy constructor
+A<int, int> &&a(void);
+void g() {
+    A<int, int> a2 = a(); // expected-error{{no matching constructor}}
 }
 
 template<class T, class V>
@@ -17,6 +24,6 @@ struct B{
     template<class U> B(U); // No error (templated constructor)
 };
 
-void g() {
+void h() {
     B<int, int> b = B<int, int>(); // should use implicit copy constructor
 }
