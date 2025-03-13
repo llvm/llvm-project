07e736dfcf08809db9d78c57b877936c5fa149b6
Pavel Labath <pavel@labath.sk>
2025-03-13 13:33:47 +0100

    name arguments

--- lldb/source/Core/Disassembler.cpp
+++ lldb/source/Core/Disassembler.cpp
@@ -552,13 +552,23 @@ void Disassembler::PrintInstructions(Debugger &debugger, const ArchSpec &arch,
 
 bool Disassembler::Disassemble(Debugger &debugger, const ArchSpec &arch,
                                StackFrame &frame, Stream &strm) {
+  constexpr const char *plugin_name = nullptr;
+  constexpr const char *flavor = nullptr;
+  constexpr const char *cpu = nullptr;
+  constexpr const char *features = nullptr;
+  constexpr bool mixed_source_and_assembly = false;
+  constexpr uint32_t num_mixed_context_lines = 0;
+  constexpr uint32_t options = 0;
+
   SymbolContext sc(
       frame.GetSymbolContext(eSymbolContextFunction | eSymbolContextSymbol));
   if (sc.function) {
     if (DisassemblerSP disasm_sp = DisassembleRange(
-            arch, nullptr, nullptr, nullptr, nullptr, *frame.CalculateTarget(),
+            arch, plugin_name, flavor, cpu, features, *frame.CalculateTarget(),
             sc.function->GetAddressRanges())) {
-      disasm_sp->PrintInstructions(debugger, arch, frame, false, 0, 0, strm);
+      disasm_sp->PrintInstructions(debugger, arch, frame,
+                                   mixed_source_and_assembly,
+                                   num_mixed_context_lines, options, strm);
       return true;
     }
     return false;
@@ -579,8 +589,9 @@ bool Disassembler::Disassemble(Debugger &debugger, const ArchSpec &arch,
   if (limit.value == 0)
     limit.value = DEFAULT_DISASM_BYTE_SIZE;
 
-  return Disassemble(debugger, arch, nullptr, nullptr, nullptr, nullptr, frame,
-                     range.GetBaseAddress(), limit, false, 0, 0, strm);
+  return Disassemble(debugger, arch, plugin_name, flavor, cpu, features, frame,
+                     range.GetBaseAddress(), limit, mixed_source_and_assembly,
+                     num_mixed_context_lines, options, strm);
 }
 
 Instruction::Instruction(const Address &address, AddressClass addr_class)
