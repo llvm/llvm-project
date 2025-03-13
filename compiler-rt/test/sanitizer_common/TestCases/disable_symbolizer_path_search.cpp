9b6cbf80c5d5010b1b1d59d9a156dc0b0a0bc0c1
Midhunesh <midhunesh.p@ibm.com>
2025-03-13 22:21:02 +0530

    added test

---
+++ compiler-rt/test/sanitizer_common/TestCases/disable_symbolizer_path_search.cpp
@@ -0,0 +1,26 @@
+// REQUIRES: static-libs
+// REQUIRES: disable_symbolizer_path_search
+
+// RUN: %clangxx %s -o %t
+// RUN: %run %t 2>&1 | FileCheck %s
+
+// REQUIRES: shell
+
+// Mobile device will not have symbolizer in provided path.
+// UNSUPPORTED: ios, android
+
+#include <sanitizer/common_interface_defs.h>
+#include <stdio.h>
+
+static void Symbolize() {
+  char buffer[100];
+  __sanitizer_symbolize_pc(__builtin_return_address(0), "%p %F %L", buffer,
+                           sizeof(buffer));
+  printf("%s\n", buffer);
+}
+
+int main() {
+  Symbolize();
+}
+
+// CHECK: Symbolizer path search is disabled in the runtime build configuration"
