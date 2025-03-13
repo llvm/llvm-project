ea49f29e0696b2631f7425ca12e46b53aadf5045
Frederik Harwath <fharwath@amd.com>
2025-03-13 07:42:30 -0400

    Rename ExpandLargeFpConvert.{cpp,h}

--- llvm/lib/CodeGen/ExpandFp.cpp
+++ llvm/lib/CodeGen/ExpandFp.cpp
@@ -37,7 +37,7 @@ using namespace llvm;
 static cl::opt<unsigned>
     ExpandFpConvertBits("expand-fp-convert-bits", cl::Hidden,
                      cl::init(llvm::IntegerType::MAX_INT_BITS),
-                     cl::desc("fp  convert instructions on integers with "
+                     cl::desc("fp convert instructions on integers with "
                               "more than <N> bits are expanded."));
 
 namespace {
