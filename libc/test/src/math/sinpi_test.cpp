212827659738ac791ebb6f2279c539f48c4cf71b
Job Hernandez <jobricciab@gmail.com>
2025-03-14 10:45:02 -0700

    make tests pass

--- libc/test/src/math/sinpi_test.cpp
+++ libc/test/src/math/sinpi_test.cpp
@@ -18,27 +18,14 @@ using namespace std;
 namespace mpfr = LIBC_NAMESPACE::testing::mpfr;
 
 static constexpr uint64_t POS_START = 0x0000U;
-//static constexpr uint64_t POS_STOP =  0x7c00U;
-
-#include <iostream>
-/*
-double inputs[] = {
-  0x1p0, 0x1p1, 0x1.8p1, 0x1p2, 0x1.4p2, 0x1.8p2, 
-  0x1.cp2, 0x1p3, 0x1.2p3, 0x1.4p3, 0x1.6p3, 0x1.8p3, 
-  0x1.ap3, 0x1.cp3, 0x1.ep3, 0x1p4, 0x1.1p4, 0x1.2p4, 
-  0x1.3p4, 0x1.4p4, 0x1.5p4, 0x1.6p4, 0x1.7p4, 0x1.8p4, 
- 0x1.9p4, 0x1.ap4
-};
-*/
 
 TEST_F(LlvmLibcSinpiTest, PositiveRange) {
-  for (uint64_t v = POS_START; v <= 3; ++v) {
+  for (uint64_t v = POS_START; v <= 30; ++v) {
     double x = FPBits(v).get_val();
-    std::cout << "sin(" << x << " * pi) =" << "\n" <<  LIBC_NAMESPACE::sinpi(x) << std::endl;
     EXPECT_MPFR_MATCH_ALL_ROUNDING(mpfr::Operation::Sinpi, x,
 				   LIBC_NAMESPACE::sinpi(x), 0.5);
     
-    //EXPECT_MPFR_MATCH_ALL_ROUNDING(mpfr::Operation::Sinpi, -v,
-    //LIBC_NAMESPACE::sinpi(-v), 0.5);
+    EXPECT_MPFR_MATCH_ALL_ROUNDING(mpfr::Operation::Sinpi, -x,
+				   LIBC_NAMESPACE::sinpi(-x), 0.5);
   }
 }
