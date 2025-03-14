dc7b16d04ef4f3864dac0e1800a2861d9def4090
Job Hernandez <lara2993@proton.me>
2025-03-14 10:45:53 -0700

    format code

--- libc/src/math/generic/sinpi.cpp
+++ libc/src/math/generic/sinpi.cpp
@@ -14,25 +14,25 @@
 #include "src/__support/FPUtil/multiply_add.h"
 #include "src/__support/common.h"
 #include "src/__support/macros/config.h"
-//#include "src/__support/macros/optimization.h" // LIBC_UNLIKELY
+// #include "src/__support/macros/optimization.h" // LIBC_UNLIKELY
 #include "src/__support/FPUtil/double_double.h"
-#include "src/math/pow.h"
-#include "src/__support/FPUtil/nearest_integer.h"
 #include "src/__support/FPUtil/generic/mul.h"
-//#include "src/math/generic/range_reduction_double_common.h"
+#include "src/__support/FPUtil/nearest_integer.h"
+#include "src/math/pow.h"
+// #include "src/math/generic/range_reduction_double_common.h"
 #include "range_reduction_double_nofma.h"
 #include "src/__support/FPUtil/multiply_add.h"
 #include <iostream>
 namespace LIBC_NAMESPACE_DECL {
-  
+
 LLVM_LIBC_FUNCTION(double, sinpi, (double x)) {
-  
+
   // Range reduction:
   // Given x = (k + y) * 1/128
   // find k and y such that
   //   k = round(x * 128)
   //   y = x * 128 - k
-  
+
   // x = (k + y) * 1/128 and
   // sin(x * pi) = sin((k +y)*pi/128)
   //             = sin(k * pi/128) * cos(y * pi/128) +
@@ -40,19 +40,19 @@ LLVM_LIBC_FUNCTION(double, sinpi, (double x)) {
 
   using FPBits = typename fputil::FPBits<double>;
   using DoubleDouble = fputil::DoubleDouble;
-  
+
   double k = fputil::nearest_integer(x * 128);
   FPBits kbits(k);
   FPBits xbits(x);
   uint64_t k_bits = kbits.uintval();
 
   double fff = 5.0;
-  [[maybe_unused]] Float128 ggg = range_reduction_small_f128(fff); 
+  [[maybe_unused]] Float128 ggg = range_reduction_small_f128(fff);
 
   double y = (x * 128) - k;
-  double pi = 3.14/128;
+  double pi = 3.14 / 128;
   DoubleDouble yy = fputil::exact_mult(y, pi);
- 
+
   uint64_t abs_u = xbits.uintval();
 
   uint64_t x_abs = abs_u & 0x7fff;
@@ -70,7 +70,7 @@ LLVM_LIBC_FUNCTION(double, sinpi, (double x)) {
     }
     return FPBits::zero(xbits.sign()).get_val();
   }
-  
+
   DoubleDouble sin_y, cos_y;
 
   [[maybe_unused]] double err = generic::sincos_eval(yy, sin_y, cos_y);
@@ -84,9 +84,7 @@ LLVM_LIBC_FUNCTION(double, sinpi, (double x)) {
   if (LIBC_UNLIKELY(sin_yy == 0 && sin_kk == 0))
     return FPBits::zero(xbits.sign()).get_val();
 
-  return static_cast<double>(fputil::multiply_add(sin_yy, cos_kk, fputil::multiply_add(cos_yy, sin_kk, sin_kk)));
+  return static_cast<double>(fputil::multiply_add(
+      sin_yy, cos_kk, fputil::multiply_add(cos_yy, sin_kk, sin_kk)));
 }
-}
-						   
-
-
+} // namespace LIBC_NAMESPACE_DECL
