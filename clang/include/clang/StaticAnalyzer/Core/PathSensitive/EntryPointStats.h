991340d9f7ec04a8f9370493385f5ac74d4954a5
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2025-03-14 17:20:55 +0100

    [NFC] Fix header comment widths

--- clang/include/clang/StaticAnalyzer/Core/PathSensitive/EntryPointStats.h
+++ clang/include/clang/StaticAnalyzer/Core/PathSensitive/EntryPointStats.h
@@ -1,10 +1,10 @@
-// EntryPointStats.h - Tracking statistics per entry point --*- C++ -*-//
+// EntryPointStats.h - Tracking statistics per entry point ------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
-//===---------------------------------------------------------------===//
+//===----------------------------------------------------------------------===//
 
 #ifndef CLANG_INCLUDE_CLANG_STATICANALYZER_CORE_PATHSENSITIVE_ENTRYPOINTSTATS_H
 #define CLANG_INCLUDE_CLANG_STATICANALYZER_CORE_PATHSENSITIVE_ENTRYPOINTSTATS_H
