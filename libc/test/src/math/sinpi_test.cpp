431eba0ef6e65131899867d5d7eac4ec3ffb0b5e
Job Hernandez <lara2993@proton.me>
2025-03-14 13:52:34 -0700

    add new test

--- libc/test/src/math/sinpi_test.cpp
+++ libc/test/src/math/sinpi_test.cpp
@@ -17,26 +17,68 @@ using LlvmLibcSinpiTest = LIBC_NAMESPACE::testing::FPTest<double>;
 using namespace std;
 namespace mpfr = LIBC_NAMESPACE::testing::mpfr;
 
-static constexpr uint16_t POS_START = 0x0000000000000000U;
-static constexpr uint16_t POS_STOP = 0x7FF0000000000000U;
+using LIBC_NAMESPACE::testing::tlog;
 
-static constexpr uint16_t NEG_START = 0x8000000000000000U;
-static constexpr uint16_t NEG_STOP = 0xFFF0000000000000;
+TEST_F(LlvmLibcSinpiTest, InDoubleRange) {
+  constexpr uint64_t COUNT = 1'234'51;
+  uint64_t START = LIBC_NAMESPACE::fputil::FPBits<double>(0x1.0p-50).uintval();
+  uint64_t STOP = LIBC_NAMESPACE::fputil::FPBits<double>(0x1.0p200).uintval();
+  uint64_t STEP = (STOP - START) / COUNT;
 
-TEST_F(LlvmLibcSinpiTest, PositiveRange) {
-  for (uint64_t v = POS_START; v <= POS_STOP; ++v) {
-    double x = FPBits(v).get_val();
+  auto test = [&](mpfr::RoundingMode rounding_mode) {
+    mpfr::ForceRoundingMode __r(rounding_mode);
+    if (!__r.success)
+      return;
 
-    EXPECT_MPFR_MATCH_ALL_ROUNDING(mpfr::Operation::Sinpi, x,
-                                   LIBC_NAMESPACE::sinpi(x), 0.5);
-  }
-}
+    uint64_t fails = 0;
+    uint64_t count = 0;
+    uint64_t cc = 0;
+    double mx, mr = 0.0;
+    double tol = 0.5;
 
-TEST_F(LlvmLibcSinpiTest, NegativeRange) {
-  for (uint64_t v = NEG_START; v <= NEG_STOP; ++v) {
-    double x = FPBits(v).get_val();
+    for (uint64_t i = 0, v = START; i <= COUNT; ++i, v += STEP) {
+      double x = FPBits(v).get_val();
+      if (FPBits(v).is_nan() || FPBits(v).is_inf())
+        continue;
+      LIBC_NAMESPACE::libc_errno = 0;
+      double result = LIBC_NAMESPACE::sinpi(x);
+      ++cc;
+      if (FPBits(result).is_nan() || FPBits(result).is_inf())
+        continue;
 
-    EXPECT_MPFR_MATCH_ALL_ROUNDING(mpfr::Operation::Sinpi, x,
-                                   LIBC_NAMESPACE::sinpi(x), 0.5);
-  }
+      ++count;
+
+      if (!TEST_MPFR_MATCH_ROUNDING_SILENTLY(mpfr::Operation::Sin, x, result,
+                                             0.5, rounding_mode)) {
+        ++fails;
+        while (!TEST_MPFR_MATCH_ROUNDING_SILENTLY(mpfr::Operation::Sin, x,
+                                                  result, tol, rounding_mode)) {
+          mx = x;
+          mr = result;
+
+          if (tol > 1000.0)
+            break;
+
+          tol *= 2.0;
+        }
+      }
+    }
+    if (fails) {
+      tlog << " Sin failed: " << fails << "/" << count << "/" << cc
+           << " tests.\n";
+      tlog << "   Max ULPs is at most: " << static_cast<uint64_t>(tol) << ".\n";
+      EXPECT_MPFR_MATCH(mpfr::Operation::Sinpi, mx, mr, 0.5, rounding_mode);
+    }
+  };
+  tlog << " Test Rounding To Nearest...\n";
+  test(mpfr::RoundingMode::Nearest);
+
+  tlog << " Test Rounding Downward...\n";
+  test(mpfr::RoundingMode::Downward);
+
+  tlog << " Test Rounding Upward...\n";
+  test(mpfr::RoundingMode::Upward);
+
+  tlog << " Test Rounding Toward Zero...\n";
+  test(mpfr::RoundingMode::TowardZero);
 }
