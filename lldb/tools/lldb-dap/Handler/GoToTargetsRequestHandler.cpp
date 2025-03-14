2d9b7a8b2e5de576fe9cf6ae4cad68e28780b54e
Ebuka Ezike <yerimyah1@gmail.com>
2025-03-14 18:18:55 +0000

    [lldb][lldb-dap] use breakpoints to verify goto location.

--- lldb/tools/lldb-dap/Handler/GoToTargetsRequestHandler.cpp
+++ lldb/tools/lldb-dap/Handler/GoToTargetsRequestHandler.cpp
@@ -10,9 +10,50 @@
 
 #include "JSONUtils.h"
 
+#include <lldb/API/SBBreakpointLocation.h>
+#include <lldb/API/SBListener.h>
 #include <lldb/API/SBStream.h>
 
 namespace lldb_dap {
+
+static llvm::SmallVector<lldb::SBLineEntry>
+GetLineValidEntry(DAP &dap, const lldb::SBFileSpec &file_spec, uint32_t line) {
+  // disable breakpoint listeners so they do not send events to the DAP client.
+  lldb::SBListener listener = dap.debugger.GetListener();
+  lldb::SBBroadcaster broadcaster = dap.target.GetBroadcaster();
+  constexpr auto event_mask = lldb::SBTarget::eBroadcastBitBreakpointChanged;
+  listener.StopListeningForEvents(broadcaster, event_mask);
+
+  // create a breakpoint to resolve the line if it is on an empty line.
+  lldb::SBBreakpoint goto_bp =
+      dap.target.BreakpointCreateByLocation(file_spec, line);
+  if (!goto_bp.IsValid())
+    return {};
+
+  llvm::SmallVector<lldb::SBLineEntry> entry_locations{};
+  const size_t resolved_count = goto_bp.GetNumResolvedLocations();
+  for (size_t idx = 0; idx < resolved_count; ++idx) {
+    lldb::SBBreakpointLocation location = goto_bp.GetLocationAtIndex(idx);
+    if (!location.IsValid())
+      continue;
+
+    lldb::SBAddress addr = location.GetAddress();
+    if (!addr.IsValid())
+      continue;
+
+    lldb::SBLineEntry line_entry = addr.GetLineEntry();
+    if (!line_entry.IsValid())
+      continue;
+
+    entry_locations.push_back(line_entry);
+  }
+
+  // clean up;
+  dap.target.BreakpointDelete(goto_bp.GetID());
+  listener.StartListeningForEvents(broadcaster, event_mask);
+
+  return entry_locations;
+}
 
 //  "GotoTargetsRequest": {
 //    "allOf": [ { "$ref": "#/definitions/Request" }, {
@@ -83,37 +124,40 @@ void GoToTargetsRequestHandler::operator()(
     const llvm::json::Object &request) const {
   llvm::json::Object response;
   FillResponse(request, response);
-  const auto *arguments = request.getObject("arguments");
-  const auto *source = arguments->getObject("source");
+
+  const llvm::json::Object *arguments = request.getObject("arguments");
+  const llvm::json::Object *source = arguments->getObject("source");
   const std::string path = GetString(source, "path").str();
-
-  const auto goto_line = GetInteger<uint64_t>(arguments, "line").value_or(0u);
-  const auto goto_column =
-      GetInteger<uint64_t>(arguments, "column").value_or(0u);
-
-  lldb::SBLineEntry line_entry{};
   const lldb::SBFileSpec file_spec(path.c_str(), true);
-  line_entry.SetFileSpec(file_spec);
-  line_entry.SetLine(goto_line);
-  line_entry.SetColumn(goto_column);
+  const uint64_t goto_line =
+      GetInteger<uint64_t>(arguments, "line").value_or(1U);
 
-  const auto target_id = dap.goto_id_map.InsertLineEntry(line_entry);
-  llvm::json::Array response_targets;
-  const auto target_line = line_entry.GetLine();
-  const auto target_column = line_entry.GetColumn();
-  auto target = llvm::json::Object();
-  target.try_emplace("id", target_id);
-
-  lldb::SBStream stream;
-  line_entry.GetDescription(stream);
-  target.try_emplace("label",
-                     llvm::StringRef(stream.GetData(), stream.GetSize()));
-  target.try_emplace("column", target_column);
-  target.try_emplace("line", target_line);
-
-  response_targets.push_back(std::move(target));
   llvm::json::Object body;
-  body.try_emplace("targets", std::move(response_targets));
+
+  llvm::SmallVector<lldb::SBLineEntry> goto_locations =
+      GetLineValidEntry(dap, file_spec, goto_line);
+  if (goto_locations.empty()) {
+    response["success"] = false;
+    response["message"] = "Invalid jump location";
+  } else {
+    llvm::json::Array response_targets;
+    for (lldb::SBLineEntry &line_entry : goto_locations) {
+      const uint64_t target_id = dap.goto_id_map.InsertLineEntry(line_entry);
+      const uint32_t target_line = line_entry.GetLine();
+      auto target = llvm::json::Object();
+      target.try_emplace("id", target_id);
+
+      lldb::SBStream stream;
+      line_entry.GetDescription(stream);
+      target.try_emplace("label",
+                         llvm::StringRef(stream.GetData(), stream.GetSize()));
+      target.try_emplace("line", target_line);
+      response_targets.push_back(std::move(target));
+    }
+
+    body.try_emplace("targets", std::move(response_targets));
+  }
+
   response.try_emplace("body", std::move(body));
   dap.SendJSON(llvm::json::Value(std::move(response)));
 }
