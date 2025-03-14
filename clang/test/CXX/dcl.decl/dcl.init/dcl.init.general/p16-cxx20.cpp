4182c29059746848b136970a9f154751c5ba02fa
offsetof <offsetof@mailo.com>
2025-03-14 11:41:32 +0000

    [clang] Avoid doing C++20 aggregate init during copy-initialization

--- clang/test/CXX/dcl.decl/dcl.init/dcl.init.general/p16-cxx20.cpp
+++ clang/test/CXX/dcl.decl/dcl.init/dcl.init.general/p16-cxx20.cpp
@@ -1,18 +1,34 @@
 // RUN: %clang_cc1 -fsyntax-only -verify -std=c++20 %s
 
-// If the initializer is (), the object is value-initialized.
-
-// expected-no-diagnostics
 namespace GH69890 {
-struct A {
-    constexpr A() {}
-    int x;
-};
+    // If the initializer is (), the object is value-initialized.
+    struct A {
+        constexpr A() {}
+        int x;
+    };
 
-struct B : A {
-    int y;
-};
+    struct B : A {
+        int y;
+    };
 
-static_assert(B().x == 0);
-static_assert(B().y == 0);
-}
+    static_assert(B().x == 0);
+    static_assert(B().y == 0);
+} // namespace GH69890
+
+namespace P0960R3 {
+    struct A { // expected-note 9 {{candidate constructor}}
+        int i;
+        operator int() volatile;
+    };
+
+    volatile A va;
+    A a = va; // expected-error {{no matching constructor for initialization of 'A'}}
+
+    A f() {
+        return va; // expected-error {{no matching constructor for initialization of 'A'}}
+    }
+
+    int g(A); // expected-note {{passing argument to parameter here}}
+    int g(auto&&);
+    int i = g(va); // expected-error {{no matching constructor for initialization of 'A'}}
+} // namespace P0960R3
