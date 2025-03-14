4721cc84066808cadecb0a45c05f738f20bab5ad
Job Hernandez <jobricciab@gmail.com>
2025-03-14 13:32:34 -0700

    fix bugs

--- libc/test/src/math/sinpi_test.cpp
+++ libc/test/src/math/sinpi_test.cpp
@@ -17,30 +17,29 @@ using LlvmLibcSinpiTest = LIBC_NAMESPACE::testing::FPTest<double>;
 using namespace std;
 namespace mpfr = LIBC_NAMESPACE::testing::mpfr;
 
-static constexpr uint64_t POS_START = 0x0000U;
-static constexpr uint16_t POS_STOP = 0x0000000000000000;
+static constexpr uint16_t POS_START = 0x0000000000000000U;
+static constexpr uint16_t POS_STOP = 0x7FF0000000000000U;
 
-static constexpr uint16_t NEG_START = 0x8000000000000000;
+
+static constexpr uint16_t NEG_START = 0x8000000000000000U;
 static constexpr uint16_t NEG_STOP = 0xFFF0000000000000;
+
 
 TEST_F(LlvmLibcSinpiTest, PositiveRange) {
   for (uint64_t v = POS_START; v <= POS_STOP; ++v) {
     double x = FPBits(v).get_val();
+    
     EXPECT_MPFR_MATCH_ALL_ROUNDING(mpfr::Operation::Sinpi, x,
                                    LIBC_NAMESPACE::sinpi(x), 0.5);
-
-    // EXPECT_MPFR_MATCH_ALL_ROUNDING(mpfr::Operation::Sinpi, -x,
-    // LIBC_NAMESPACE::sinpi(-x), 0.5);
   }
 }
 
 TEST_F(LlvmLibcSinpiTest, NegativeRange) {
   for (uint64_t v = NEG_START; v <= NEG_STOP; ++v) {
     double x = FPBits(v).get_val();
-    // EXPECT_MPFR_MATCH_ALL_ROUNDING(mpfr::Operation::Sinpi, x,
-    // LIBC_NAMESPACE::sinpi(x), 0.5);
-
+    
     EXPECT_MPFR_MATCH_ALL_ROUNDING(mpfr::Operation::Sinpi, x,
                                    LIBC_NAMESPACE::sinpi(x), 0.5);
   }
 }
+
