5ce65f156a732a399c5f8c45407e68668c9c8fd8
Mikhail Zakharov <mikhail.zakharov@jetbrains.com>
2025-03-14 18:38:52 +0100

    [lldb] Reformat test code and add additional test for modifying member of the struct allocated in the cache

--- lldb/test/Shell/Expr/TestExprWithSideEffect.cpp
+++ lldb/test/Shell/Expr/TestExprWithSideEffect.cpp
@@ -12,30 +12,19 @@
 // RUN:   -o "frame variable x" \
 // RUN:   -o "continue" \
 // RUN:   -o "frame variable x" \
-// RUN:   -o "expr int $y = 11" \
-// RUN:   -o "expr $y" \
-// RUN:   -o "expr $y = 100" \
-// RUN:   -o "expr $y" \
-// RUN:   -o "continue" \
-// RUN:   -o "expr $y" \
 // RUN:   -o exit | FileCheck %s -dump-input=fail
 
-class X
-{
+class X {
   int i = 0;
+
 public:
-  void inc()
-  {
-    ++i;
-  }
+  void inc() { ++i; }
 };
 
-int main()
-{
+int main() {
   X x;
   x.inc();
 
-  __builtin_debugtrap();
   __builtin_debugtrap();
   __builtin_debugtrap();
   __builtin_debugtrap();
@@ -53,8 +42,6 @@ int main()
 // CHECK-LABEL: frame variable x
 // CHECK: (X) x = (i = 2)
 
-
-
 // CHECK-LABEL: expr x.i = 10
 // CHECK: (int) $0 = 10
 
@@ -64,19 +51,3 @@ int main()
 // CHECK-LABEL: continue
 // CHECK-LABEL: frame variable x
 // CHECK: (X) x = (i = 10)
-
-
-
-// CHECK-LABEL: expr int $y = 11
-// CHECK-LABEL: expr $y
-// CHECK: (int) $y = 11
-
-// CHECK-LABEL: expr $y = 100
-// CHECK: (int) $1 = 100
-
-// CHECK-LABEL: expr $y
-// CHECK: (int) $y = 100
-
-// CHECK-LABEL: continue
-// CHECK-LABEL: expr $y
-// CHECK: (int) $y = 100
