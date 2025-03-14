ede5e8c9b836302a5e3bd4e2d5f154d775184d1d
Sabianin, Maksim <maksim.sabianin@intel.com>
2025-03-14 07:24:46 -0700

    [offload][SYCL] Add SYCL Module splitting.
    This patch adds SYCL Module splitting - the necessary step in the SYCL
    compilation pipeline. Only 2 splitting modes are being added in this
    patch: by kernel and by source.
    
    The previous attempt was at #119713. In this patch there is no
    dependency in `TransformUtils` on "IPO" and on "Printing Passes". In
    this patch a module splitting is self-contained and it doesn't introduce
    linking issues.

--- llvm/tools/llvm-split/llvm-split.cpp
+++ llvm/tools/llvm-split/llvm-split.cpp
@@ -11,14 +11,19 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include "llvm/ADT/SmallString.h"
+#include "llvm/ADT/SmallVector.h"
 #include "llvm/ADT/StringExtras.h"
 #include "llvm/Bitcode/BitcodeWriter.h"
 #include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/PassInstrumentation.h"
+#include "llvm/IR/PassManager.h"
 #include "llvm/IR/Verifier.h"
 #include "llvm/IRReader/IRReader.h"
 #include "llvm/MC/TargetRegistry.h"
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/FileSystem.h"
+#include "llvm/Support/FormatVariadic.h"
 #include "llvm/Support/InitLLVM.h"
 #include "llvm/Support/SourceMgr.h"
 #include "llvm/Support/TargetSelect.h"
@@ -27,6 +32,9 @@
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Target/TargetMachine.h"
 #include "llvm/TargetParser/Triple.h"
+#include "llvm/Transforms/IPO/GlobalDCE.h"
+#include "llvm/Transforms/Utils/SYCLSplitModule.h"
+#include "llvm/Transforms/Utils/SYCLUtils.h"
 #include "llvm/Transforms/Utils/SplitModule.h"
 
 using namespace llvm;
@@ -69,6 +77,108 @@ static cl::opt<std::string>
 static cl::opt<std::string>
     MCPU("mcpu", cl::desc("Target CPU, ignored if --mtriple is not used"),
          cl::value_desc("cpu"), cl::cat(SplitCategory));
+
+static cl::opt<IRSplitMode> SYCLSplitMode(
+    "sycl-split",
+    cl::desc("SYCL Split Mode. If present, SYCL splitting algorithm is used "
+             "with the specified mode."),
+    cl::Optional, cl::init(IRSplitMode::IRSM_NONE),
+    cl::values(clEnumValN(IRSplitMode::IRSM_PER_TU, "source",
+                          "1 ouptput module per translation unit"),
+               clEnumValN(IRSplitMode::IRSM_PER_KERNEL, "kernel",
+                          "1 output module per kernel")),
+    cl::cat(SplitCategory));
+
+static cl::opt<bool> OutputAssembly{
+    "S", cl::desc("Write output as LLVM assembly"), cl::cat(SplitCategory)};
+
+void writeStringToFile(StringRef Content, StringRef Path) {
+  std::error_code EC;
+  raw_fd_ostream OS(Path, EC);
+  if (EC) {
+    errs() << formatv("error opening file: {0}, error: {1}\n", Path,
+                      EC.message());
+    exit(1);
+  }
+
+  OS << Content << "\n";
+}
+
+void writeModuleToFile(const Module &M, StringRef Path, bool OutputAssembly) {
+  int FD = -1;
+  if (std::error_code EC = sys::fs::openFileForWrite(Path, FD)) {
+    errs() << formatv("error opening file: {0}, error: {1}", Path, EC.message())
+           << '\n';
+    exit(1);
+  }
+
+  raw_fd_ostream OS(FD, /*ShouldClose*/ true);
+  if (OutputAssembly)
+    M.print(OS, /*AssemblyAnnotationWriter*/ nullptr);
+  else
+    WriteBitcodeToFile(M, OS);
+}
+
+void writeSplitModulesAsTable(ArrayRef<ModuleAndSYCLMetadata> Modules,
+                              StringRef Path) {
+  SmallVector<SmallString<64>> Columns;
+  Columns.emplace_back("Code");
+  Columns.emplace_back("Symbols");
+
+  SYCLStringTable Table;
+  Table.emplace_back(std::move(Columns));
+  for (const auto &[I, SM] : enumerate(Modules)) {
+    SmallString<128> SymbolsFile;
+    (Twine(Path) + "_" + Twine(I) + ".sym").toVector(SymbolsFile);
+    writeStringToFile(SM.Symbols, SymbolsFile);
+    SmallVector<SmallString<64>> Row;
+    Row.emplace_back(SM.ModuleFilePath);
+    Row.emplace_back(SymbolsFile);
+    Table.emplace_back(std::move(Row));
+  }
+
+  std::error_code EC;
+  raw_fd_ostream OS((Path + ".table").str(), EC);
+  if (EC) {
+    errs() << formatv("error opening file: {0}\n", Path);
+    exit(1);
+  }
+
+  writeSYCLStringTable(Table, OS);
+}
+
+void cleanupModule(Module &M) {
+  ModuleAnalysisManager MAM;
+  MAM.registerPass([&] { return PassInstrumentationAnalysis(); });
+  ModulePassManager MPM;
+  MPM.addPass(GlobalDCEPass()); // Delete unreachable globals.
+  MPM.run(M, MAM);
+}
+
+Error runSYCLSplitModule(std::unique_ptr<Module> M) {
+  SmallVector<ModuleAndSYCLMetadata> SplitModules;
+  auto PostSYCLSplitCallback = [&](std::unique_ptr<Module> MPart,
+                                   std::string Symbols) {
+    if (verifyModule(*MPart)) {
+      errs() << "Broken Module!\n";
+      exit(1);
+    }
+
+    // TODO: DCE is a crucial pass in a SYCL post-link pipeline.
+    //       At the moment, LIT checking can't be perfomed without DCE.
+    cleanupModule(*MPart);
+    size_t ID = SplitModules.size();
+    StringRef ModuleSuffix = OutputAssembly ? ".ll" : ".bc";
+    std::string ModulePath =
+        (Twine(OutputFilename) + "_" + Twine(ID) + ModuleSuffix).str();
+    writeModuleToFile(*MPart, ModulePath, OutputAssembly);
+    SplitModules.emplace_back(std::move(ModulePath), std::move(Symbols));
+  };
+
+  SYCLSplitModule(std::move(M), SYCLSplitMode, PostSYCLSplitCallback);
+  writeSplitModulesAsTable(SplitModules, OutputFilename);
+  return Error::success();
+}
 
 int main(int argc, char **argv) {
   InitLLVM X(argc, argv);
@@ -122,6 +232,17 @@ int main(int argc, char **argv) {
     // Declare success.
     Out->keep();
   };
+
+  if (SYCLSplitMode != IRSplitMode::IRSM_NONE) {
+    auto E = runSYCLSplitModule(std::move(M));
+    if (E) {
+      errs() << E << "\n";
+      Err.print(argv[0], errs());
+      return 1;
+    }
+
+    return 0;
+  }
 
   if (TM) {
     if (PreserveLocals) {
