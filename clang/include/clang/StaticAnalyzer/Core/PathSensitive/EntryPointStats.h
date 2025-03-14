84e35714a6572a54f50f95b6f38ca12c72b25208
Arseniy Zaostrovnykh <arseniy.zaostrovnykh@sonarsource.com>
2025-03-14 15:10:35 +0100

    Update clang/include/clang/StaticAnalyzer/Core/PathSensitive/EntryPointStats.h
    Co-authored-by: Donát Nagy <donat.nagy@ericsson.com>

--- clang/include/clang/StaticAnalyzer/Core/PathSensitive/EntryPointStats.h
+++ clang/include/clang/StaticAnalyzer/Core/PathSensitive/EntryPointStats.h
@@ -1,4 +1,4 @@
-// EntryPointStats.h - Tracking statistics per  entry point -*- C++ -*-//
+// EntryPointStats.h - Tracking statistics per entry point --*- C++ -*-//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
