e642ed8b4ac70264454d7e0a8bec787c4f2b435a
Fangrui Song <i@maskray.me>
2025-03-14 21:38:02 -0700

    [AVR] Remove initializeVariantKinds
    AVR doesn't use @reloc or ARM-style sym(reloc) supported by
    AsmParser.cpp. Instead, AVRAsmParser parses the operator (e.g.
    pm_lo8(...))

--- llvm/lib/Target/AVR/MCTargetDesc/AVRMCAsmInfo.cpp
+++ llvm/lib/Target/AVR/MCTargetDesc/AVRMCAsmInfo.cpp
@@ -11,18 +11,10 @@
 //===----------------------------------------------------------------------===//
 
 #include "AVRMCAsmInfo.h"
-#include "AVRMCExpr.h"
 #include "llvm/MC/MCExpr.h"
 #include "llvm/TargetParser/Triple.h"
 
 using namespace llvm;
-
-const MCAsmInfo::VariantKindDesc variantKindDescs[] = {
-    {AVRMCExpr::VK_DIFF16, "diff16"}, {AVRMCExpr::VK_DIFF32, "diff32"},
-    {AVRMCExpr::VK_DIFF8, "diff8"},   {AVRMCExpr::VK_HI8, "hi8"},
-    {AVRMCExpr::VK_HH8, "hlo8"},      {AVRMCExpr::VK_LO8, "lo8"},
-    {AVRMCExpr::VK_PM, "pm"},
-};
 
 AVRMCAsmInfo::AVRMCAsmInfo(const Triple &TT, const MCTargetOptions &Options) {
   CodePointerSize = 2;
@@ -33,5 +25,4 @@ AVRMCAsmInfo::AVRMCAsmInfo(const Triple &TT, const MCTargetOptions &Options) {
   PrivateLabelPrefix = ".L";
   UsesELFSectionDirectiveForBSS = true;
   SupportsDebugInformation = true;
-  initializeVariantKinds(variantKindDescs);
 }
