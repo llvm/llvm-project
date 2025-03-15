e37f2a69c7958c50676af8783b91757104b580e5
A. Jiang <de34@live.cn>
2025-03-15 09:41:30 +0800

    [libc++] Make `flat_(multi)map`'s iterators require `operator<=>`
    Flat container adaptors require the iterators of underlying containers
    to be random access, and it is required that random access container
    iterators must support three-way comparison
    ([container.reqmts]/39 - /41).
    
    As a result, we can and perhaps should reject containers with unexpected
    iterator types, as they're invalid since C++20.

---
+++ libcxx/test/std/containers/container.adaptors/flat.multimap/flat.multimap.iterators/iterator_comparison.verify.cpp
@@ -0,0 +1,81 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// REQUIRES: std-at-least-c++23
+
+// <flat_map>
+
+// For underlying iterators i and j, i <=> j must be well-formed and return std::strong_ordering.
+
+#include <flat_map>
+#include <functional>
+#include <iterator>
+#include <type_traits>
+
+#include "MinSequenceContainer.h"
+#include "test_iterators.h"
+
+template <class It>
+class bad_3way_random_access_iterator {
+  template <class U>
+  friend class bad_3way_random_access_iterator;
+
+public:
+  using iterator_category = std::random_access_iterator_tag;
+  using value_type        = typename std::iterator_traits<It>::value_type;
+  using difference_type   = typename std::iterator_traits<It>::difference_type;
+  using pointer           = It;
+  using reference         = typename std::iterator_traits<It>::reference;
+
+  bad_3way_random_access_iterator();
+  explicit bad_3way_random_access_iterator(It);
+
+  template <class U>
+  bad_3way_random_access_iterator(const bad_3way_random_access_iterator<U>&);
+
+  template <class U, class = typename std::enable_if<std::is_default_constructible<U>::value>::type>
+  bad_3way_random_access_iterator(bad_3way_random_access_iterator<U>&&);
+
+  reference operator*() const;
+  reference operator[](difference_type) const;
+
+  bad_3way_random_access_iterator& operator++();
+  bad_3way_random_access_iterator& operator--();
+  bad_3way_random_access_iterator operator++(int);
+  bad_3way_random_access_iterator operator--(int);
+
+  bad_3way_random_access_iterator& operator+=(difference_type);
+  bad_3way_random_access_iterator& operator-=(difference_type);
+  friend bad_3way_random_access_iterator operator+(bad_3way_random_access_iterator, difference_type);
+  friend bad_3way_random_access_iterator operator+(difference_type, bad_3way_random_access_iterator);
+  friend bad_3way_random_access_iterator operator-(bad_3way_random_access_iterator, difference_type);
+  friend difference_type operator-(bad_3way_random_access_iterator, bad_3way_random_access_iterator);
+
+  friend bool operator==(const bad_3way_random_access_iterator&, const bad_3way_random_access_iterator&);
+  friend std::weak_ordering operator<=>(const bad_3way_random_access_iterator&, const bad_3way_random_access_iterator&);
+};
+
+void test() {
+  {
+    using KeyCont = MinSequenceContainer<int, random_access_iterator<int*>, random_access_iterator<const int*>>;
+    using FMap    = std::flat_multimap<int, int, std::less<int>, KeyCont, MinSequenceContainer<int>>;
+    FMap m;
+    // expected-error@*:* {{static assertion failed: random accesss iterator not supporting three-way comparison is invalid for container}}
+    // expected-error@*:* {{invalid operands to binary expression}}
+    (void)(m.begin() <=> m.begin());
+  }
+  {
+    using KeyCont =
+        MinSequenceContainer<int, bad_3way_random_access_iterator<int*>, bad_3way_random_access_iterator<const int*>>;
+    using FMap = std::flat_multimap<int, int, std::less<int>, KeyCont, MinSequenceContainer<int>>;
+    FMap m;
+    // expected-error-re@*:* {{static assertion failed due to requirement 'is_same_v<std::weak_ordering, std::strong_ordering>'{{.*}}three-way comparison between random accesss container iterators must return std::strong_ordering}}
+    // expected-error-re@*:* {{no viable conversion from returned value of type{{.*}}}}
+    (void)(m.begin() <=> m.begin());
+  }
+}
