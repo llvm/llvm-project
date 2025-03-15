0e8ddf05df26cacc4e44945cc358e0bcde6cc7f7
Dave Lee <davelee.com@gmail.com>
2025-03-14 19:10:34 -0700

    Test for no expansion when summary formatter is present

--- lldb/test/API/commands/dwim-print/objc/TestDWIMPrintObjC.py
+++ lldb/test/API/commands/dwim-print/objc/TestDWIMPrintObjC.py
@@ -17,3 +17,11 @@ class TestCase(TestBase):
         self.expect(
             "dwim-print parent.child", patterns=[r'_name = 0x[0-9a-f]+ @"Seven"']
         )
+
+    @skipUnlessDarwin
+    def test_with_summary(self):
+        self.build()
+        lldbutil.run_to_source_breakpoint(self, "break here", lldb.SBFileSpec("main.m"))
+        self.runCmd("type summary add -s 'Parent of ${var._child._name}' 'Parent *'")
+        self.expect("dwim-print parent", matching=False, substrs=["_child = 0x"])
+        self.expect("dwim-print parent", substrs=['Parent of @"Seven"'])
