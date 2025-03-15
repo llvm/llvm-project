e3f0ce3ef803a5689fe0710e50052af019474dc0
cor3ntin <corentinjabot@gmail.com>
2025-03-15 22:27:08 +0100

    [Clang] Do not emit nodiscard warnings for the base expr of static member access (#131450)
    For an expression `nodiscard_function().static_member(), the nodiscard
    warnings added by #120223, are not useful or actionable, and are
    disruptive to some library implementations; we just remove them.
    
    Fixes #131410
    
    (cherry picked from commit 9a1e39062b2ab445f1f4424ecdc5ffb46e8cb9e0)

--- clang/lib/Sema/SemaExprMember.cpp
+++ clang/lib/Sema/SemaExprMember.cpp
@@ -1136,7 +1136,6 @@ Sema::BuildMemberReferenceExpr(Expr *BaseExpr, QualType BaseExprType,
     if (Converted.isInvalid())
       return true;
     BaseExpr = Converted.get();
-    DiagnoseDiscardedExprMarkedNodiscard(BaseExpr);
     return false;
   };
   auto ConvertBaseExprToGLValue = [&] {
