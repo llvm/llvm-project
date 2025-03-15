6a8d5f22ffc5113de9868e2b4227d699b14d0421
Florian Hahn <flo@fhahn.com>
2025-03-15 21:32:48 +0000

    [VPlan] Don't access canonical IV in VPWidenPointerInduction::execute.
    This updates VPWidenPointerInductionRecipe::execute to not use the
    canonical IV to determine the insert point. Instead, it relies on the
    current recipe position. In cases where this is not sufficient, set the
    insert point to the first non-phi instruction, to ensure phis are
    created together.

--- llvm/lib/Transforms/Vectorize/VPlanRecipes.cpp
+++ llvm/lib/Transforms/Vectorize/VPlanRecipes.cpp
@@ -3332,15 +3332,10 @@ void VPWidenPointerInductionRecipe::execute(VPTransformState &State) {
   BasicBlock *VectorPH = State.CFG.getPreheaderBBFor(this);
   PHINode *NewPointerPhi = nullptr;
   if (CurrentPart == 0) {
-    auto *IVR = getParent()
-                    ->getPlan()
-                    ->getVectorLoopRegion()
-                    ->getEntryBasicBlock()
-                    ->front()
-                    .getVPSingleValue();
-    PHINode *CanonicalIV = cast<PHINode>(State.get(IVR, /*IsScalar*/ true));
-    NewPointerPhi = PHINode::Create(ScStValueType, 2, "pointer.phi",
-                                    CanonicalIV->getIterator());
+    IRBuilder<>::InsertPointGuard Guard(State.Builder);
+    if (State.Builder.GetInsertPoint() != State.Builder.GetInsertBlock()->getFirstNonPHIIt())
+      State.Builder.SetInsertPoint(State.Builder.GetInsertBlock()->getFirstNonPHIIt());
+    NewPointerPhi = State.Builder.CreatePHI(ScStValueType, 2, "pointer.phi");
     NewPointerPhi->addIncoming(ScalarStartValue, VectorPH);
     NewPointerPhi->setDebugLoc(getDebugLoc());
   } else {
