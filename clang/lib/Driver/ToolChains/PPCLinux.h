bc55497ee1f122d04b13ce4ff17662be3d0b37d2
Daniel Chen <cdchen@ca.ibm.com>
2025-03-16 11:05:53 -0400

    Evern though flang-rt only supports LLVM_ENABLE_PER_TARGET_RUNTIME_DIR=ON, LoP should still be able to handle both to be consistent with clang-rt.

--- clang/lib/Driver/ToolChains/PPCLinux.h
+++ clang/lib/Driver/ToolChains/PPCLinux.h
@@ -24,10 +24,6 @@ public:
   AddClangSystemIncludeArgs(const llvm::opt::ArgList &DriverArgs,
                             llvm::opt::ArgStringList &CC1Args) const override;
 
-  std::string getCompilerRT(const llvm::opt::ArgList &Args, StringRef Component,
-                            FileType Type = ToolChain::FT_Static,
-                            bool IsFortran = false) const override;
-
   void addFortranRuntimeLibs(const llvm::opt::ArgList &Args,
                              llvm::opt::ArgStringList &CmdArgs) const override;
 
