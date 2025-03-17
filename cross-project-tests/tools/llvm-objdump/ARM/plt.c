30a96bf4c9077210482c73183a3cda81a65a6631
Vladislav Dzhidzhoev <vdzhidzhoev@accesssoftek.com>
2025-03-17 13:56:38 +0100

    Addressed @MaskRay comments

--- cross-project-tests/tools/llvm-objdump/ARM/plt.c
+++ cross-project-tests/tools/llvm-objdump/ARM/plt.c
@@ -1,13 +1,13 @@
 // Test PLT section parsing on arm.
 
-// RUN: %clang -target armv6a-none-linux-gnueabi -fuse-ld=ld.lld \
+// RUN: %clang --target=armv6a-none-linux-gnueabi -fuse-ld=lld \
 // RUN:   -nostdlib -nostdinc -shared %s -o %t1
 // RUN: llvm-objdump --no-show-raw-insn --no-print-imm-hex \
 // RUN:   -d %t1 | FileCheck %s --check-prefixes=CHECK,LE
 
 // Test PLT section parsing on armeb.
 
-// RUN: %clang -target armv6aeb-none-linux-gnueabi -fuse-ld=ld.lld \
+// RUN: %clang --target=armv6aeb-none-linux-gnueabi -fuse-ld=lld \
 // RUN:   -nostdlib -nostdinc -shared %s -o %t2
 // RUN: llvm-objdump --no-show-raw-insn --no-print-imm-hex \
 // RUN:   -d %t2 | FileCheck %s --check-prefixes=CHECK,BE
@@ -15,7 +15,7 @@
 
 // Test PLT section parsing on armeb with be8.
 
-// RUN: %clang -target armv7aeb-none-linux-gnueabi -fuse-ld=ld.lld \
+// RUN: %clang --target=armv7aeb-none-linux-gnueabi -fuse-ld=lld \
 // RUN:   -nostdlib -nostdinc -shared %s -o %t3
 // RUN: llvm-objdump --no-show-raw-insn --no-print-imm-hex \
 // RUN:   -d %t3 | FileCheck %s --check-prefixes=CHECK,BE
@@ -31,24 +31,24 @@
 // CHECK-NEXT:    <_start>:
 // CHECK-NEXT:      push	{r11, lr}
 // CHECK-NEXT:      mov	r11, sp
-// CHECK-NEXT:      bl	0x10240 <func1@plt>
-// CHECK-NEXT:      bl	0x10250 <func2@plt>
-// CHECK-NEXT:      bl	0x10260 <func3@plt>
+// CHECK-NEXT:      bl	{{.*}} <func1@plt>
+// CHECK-NEXT:      bl	{{.*}} <func2@plt>
+// CHECK-NEXT:      bl	{{.*}} <func3@plt>
 
 // CHECK: Disassembly of section .plt:
-// CHECK:      00010240 <func1@plt>:
+// CHECK:      <func1@plt>:
 // CHECK-NEXT:      add	r12, pc, #0, #12
 // CHECK-NEXT:      add	r12, r12, #32, #20
 // CHECK-NEXT:      ldr	pc, [r12, #132]!
 // CHECK-NEXT:      .word	0xd4d4d4d4
 // CHECK-EMPTY:
-// CHECK-NEXT: 00010250 <func2@plt>:
+// CHECK-NEXT: <func2@plt>:
 // CHECK-NEXT:      add	r12, pc, #0, #12
 // CHECK-NEXT:      add	r12, r12, #32, #20
 // CHECK-NEXT:      ldr	pc, [r12, #120]!
 // CHECK-NEXT:      .word	0xd4d4d4d4
 // CHECK-EMPTY:
-// CHECK-NEXT: 00010260 <func3@plt>:
+// CHECK-NEXT: <func3@plt>:
 // CHECK-NEXT:      add	r12, pc, #0, #12
 // CHECK-NEXT:      add	r12, r12, #32, #20
 // CHECK-NEXT:      ldr	pc, [r12, #108]!
@@ -56,7 +56,7 @@
 
 // Test PLT section parsing on thumb.
 
-// RUN: %clang -target thumbv8.1m.main-none-linux-eabi \
+// RUN: %clang --target=thumbv8.1m.main-none-linux-eabi \
 // RUN:   -nostdlib -nostdinc -c %s -o %t4.o
 // RUN: ld.lld --shared %t4.o -o %t4
 // RUN: llvm-objdump --no-show-raw-insn --no-print-imm-hex \
@@ -64,7 +64,7 @@
 
 // Test PLT section parsing on thumbeb.
 
-// RUN: %clang -target thumbebv8.1m.main-none-linux-eabi \
+// RUN: %clang --target=thumbebv8.1m.main-none-linux-eabi \
 // RUN:   -nostdlib -nostdinc -c %s -o %t5.o
 // RUN: ld.lld --shared %t5.o -o %t5
 // RUN: llvm-objdump --no-show-raw-insn --no-print-imm-hex \
@@ -73,7 +73,7 @@
 
 // Test PLT section parsing on thumbeb with be8.
 
-// RUN: %clang -target thumbebv8.1m.main-none-linux-eabi \
+// RUN: %clang --target=thumbebv8.1m.main-none-linux-eabi \
 // RUN:   -nostdlib -nostdinc -c %s -o %t6.o
 // RUN: ld.lld --shared --be8 %t6.o -o %t6
 // RUN: llvm-objdump --no-show-raw-insn --no-print-imm-hex \
@@ -85,32 +85,32 @@
 // THUMB-NEXT: <_start>:
 // THUMB-NEXT:      push	{r7, lr}
 // THUMB-NEXT:      mov r7, sp
-// THUMB-NEXT:      bl	0x10270 <func1@plt>
-// THUMB-NEXT:      bl	0x10280 <func2@plt>
-// THUMB-NEXT:      bl	0x10290 <func3@plt>
+// THUMB-NEXT:      bl	{{.*}} <func1@plt>
+// THUMB-NEXT:      bl	{{.*}} <func2@plt>
+// THUMB-NEXT:      bl	{{.*}} <func3@plt>
 
 // THUMB: Disassembly of section .plt:
 // THUMB-EMPTY:
-// THUMB:      00010270 <func1@plt>:
+// THUMB:      <func1@plt>:
 // THUMB-NEXT:      movw	r12, #136
 // THUMB-NEXT:      movt	r12, #2
 // THUMB-NEXT:      add	r12, pc
 // THUMB-NEXT:      ldr.w	pc, [r12]
-// THUMB-NEXT:      b	0x1027a
+// THUMB-NEXT:      b	0x
 // THUMB-EMPTY:
-// THUMB-NEXT: 00010280 <func2@plt>:
+// THUMB-NEXT: <func2@plt>:
 // THUMB-NEXT:      movw	r12, #124
 // THUMB-NEXT:      movt	r12, #2
 // THUMB-NEXT:      add	r12, pc
 // THUMB-NEXT:      ldr.w	pc, [r12]
-// THUMB-NEXT:      b	0x1028a
+// THUMB-NEXT:      b	0x
 // THUMB-EMPTY:
-// THUMB-NEXT: 00010290 <func3@plt>:
+// THUMB-NEXT: <func3@plt>:
 // THUMB-NEXT:      movw	r12, #112
 // THUMB-NEXT:      movt	r12, #2
 // THUMB-NEXT:      add	r12, pc
 // THUMB-NEXT:      ldr.w	pc, [r12]
-// THUMB-NEXT:      b	0x1029a
+// THUMB-NEXT:      b	0x
 
 // Test PLT section with long entries parsing on arm.
 
@@ -120,7 +120,7 @@
 // RUN:       .got.plt 0x9000000 : { *(.got.plt) } \
 // RUN:       }" > %t.long.script
 
-// RUN: %clang -target armv6a-none-linux-gnueabi -fuse-ld=ld.lld \
+// RUN: %clang --target=armv6a-none-linux-gnueabi -fuse-ld=lld \
 // RUN:   -Xlinker --script=%t.long.script -nostdlib -nostdinc \
 // RUN:   -shared %s -o %t7
 // RUN: llvm-objdump --no-show-raw-insn --no-print-imm-hex \
@@ -128,7 +128,7 @@
 
 // Test PLT section with long entries parsing on armeb.
 
-// RUN: %clang -target armv6aeb-none-linux-gnueabi -fuse-ld=ld.lld \
+// RUN: %clang --target=armv6aeb-none-linux-gnueabi -fuse-ld=lld \
 // RUN:   -Xlinker --script=%t.long.script -nostdlib -nostdinc \
 // RUN:   -shared %s -o %t8
 // RUN: llvm-objdump --no-show-raw-insn --no-print-imm-hex \
@@ -137,7 +137,7 @@
 
 // Test PLT section with long entries parsing on armeb with be8.
 
-// RUN: %clang -target armv7aeb-none-linux-gnueabi -fuse-ld=ld.lld \
+// RUN: %clang --target=armv7aeb-none-linux-gnueabi -fuse-ld=lld \
 // RUN:   -Xlinker --script=%t.long.script -nostdlib -nostdinc \
 // RUN:   -shared %s -o %t9
 // RUN: llvm-objdump --no-show-raw-insn --no-print-imm-hex \
@@ -180,7 +180,7 @@
 // RUN:       .got.plt 0x8002020 : { *(.got.plt) } \
 // RUN:       }" > %t.mix.script
 
-// RUN: %clang -target armv6a-none-linux-gnueabi -fuse-ld=ld.lld \
+// RUN: %clang --target=armv6a-none-linux-gnueabi -fuse-ld=lld \
 // RUN:   -Xlinker --script=%t.mix.script -nostdlib -nostdinc \
 // RUN:   -shared %s -o %t10
 // RUN: llvm-objdump --no-show-raw-insn --no-print-imm-hex \
@@ -188,7 +188,7 @@
 
 // Test PLT section with mixed long and short entries parsing on armeb.
 
-// RUN: %clang -target armv6aeb-none-linux-gnueabi -fuse-ld=ld.lld \
+// RUN: %clang --target=armv6aeb-none-linux-gnueabi -fuse-ld=lld \
 // RUN:   -Xlinker --script=%t.mix.script -nostdlib -nostdinc \
 // RUN:   -shared %s -o %t11
 // RUN: llvm-objdump --no-show-raw-insn --no-print-imm-hex \
@@ -197,7 +197,7 @@
 
 // Test PLT section with mixed long and short entries parsing on armeb with be8.
 
-// RUN: %clang -target armv7aeb-none-linux-gnueabi -fuse-ld=ld.lld \
+// RUN: %clang --target=armv7aeb-none-linux-gnueabi -fuse-ld=lld \
 // RUN:   -Xlinker --script=%t.mix.script -nostdlib -nostdinc \
 // RUN:   -shared %s -o %t12
 // RUN: llvm-objdump --no-show-raw-insn --no-print-imm-hex \
