e3f9548f1ccdf1bb85d0c540c7bdea6094019278
Harsh Chandel <quic_hchandel@quicinc.com>
2025-03-17 17:29:09 +0530

    [RISCV] Add Qualcomm uC Xqcilb (Long Branch) extension This extension adds two long branch instructions.
    The current spec can be found at:
    https://github.com/quic/riscv-unified-db/releases/tag/Xqci-0.7.0
    
    This patch adds assembler only support.
    
    Change-Id: I262d59edc8c7266800badc82d6496619ce2aeabf

--- llvm/lib/Target/RISCV/Disassembler/RISCVDisassembler.cpp
+++ llvm/lib/Target/RISCV/Disassembler/RISCVDisassembler.cpp
@@ -651,9 +651,9 @@ static constexpr FeatureBitset XqciFeatureGroup = {
     RISCV::FeatureVendorXqcibm,  RISCV::FeatureVendorXqcicli,
     RISCV::FeatureVendorXqcicm,  RISCV::FeatureVendorXqcics,
     RISCV::FeatureVendorXqcicsr, RISCV::FeatureVendorXqciint,
-    RISCV::FeatureVendorXqcili,  RISCV::FeatureVendorXqcilia,
-    RISCV::FeatureVendorXqcilo,  RISCV::FeatureVendorXqcilsm,
-    RISCV::FeatureVendorXqcisls,
+    RISCV::FeatureVendorXqcili,  RISCV::FeatureVendorXqcilb,
+    RISCV::FeatureVendorXqcilia, RISCV::FeatureVendorXqcilo,
+    RISCV::FeatureVendorXqcilsm, RISCV::FeatureVendorXqcisls,
 };
 
 static constexpr FeatureBitset XSfVectorGroup = {
