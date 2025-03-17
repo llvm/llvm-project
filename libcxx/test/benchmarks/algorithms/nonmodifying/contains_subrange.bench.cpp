252941e71c2957074ae121e17ff6070145c20ce8
Louis Dionne <ldionne.2@gmail.com>
2025-03-17 15:12:59 -0400

    Fix benchmarks for contains_subrange and find_end

--- libcxx/test/benchmarks/algorithms/nonmodifying/contains_subrange.bench.cpp
+++ libcxx/test/benchmarks/algorithms/nonmodifying/contains_subrange.bench.cpp
@@ -9,6 +9,7 @@
 // UNSUPPORTED: c++03, c++11, c++14, c++17, c++20
 
 #include <algorithm>
+#include <cassert>
 #include <cstddef>
 #include <deque>
 #include <iterator>
@@ -19,7 +20,7 @@
 #include "../../GenerateInput.h"
 
 int main(int argc, char** argv) {
-  // Benchmark ranges::contains_subrange where we never find our target, which is the
+  // Benchmark ranges::contains_subrange where we never find the needle, which is the
   // worst case.
   {
     auto bm = []<class Container>(std::string name) {
@@ -30,13 +31,15 @@ int main(int argc, char** argv) {
             using ValueType        = typename Container::value_type;
             ValueType x            = Generate<ValueType>::random();
             ValueType y            = random_different_from({x});
-            Container c(size, x);
-            Container subrange(size / 10, y); // subrange of length 10% of the full range, but we'll never find it
+            Container haystack(size, x);
+            std::size_t n = size / 10; // needle size is 10% of the haystack, but we'll never find it
+            assert(n > 0);
+            Container needle(n, y);
 
             for (auto _ : st) {
-              benchmark::DoNotOptimize(c);
-              benchmark::DoNotOptimize(subrange);
-              auto result = std::ranges::contains_subrange(c, subrange);
+              benchmark::DoNotOptimize(haystack);
+              benchmark::DoNotOptimize(needle);
+              auto result = std::ranges::contains_subrange(haystack, needle);
               benchmark::DoNotOptimize(result);
             }
           })
@@ -49,6 +52,107 @@ int main(int argc, char** argv) {
     bm.operator()<std::vector<int>>("rng::contains_subrange(vector<int>) (process all)");
     bm.operator()<std::deque<int>>("rng::contains_subrange(deque<int>) (process all)");
     bm.operator()<std::list<int>>("rng::contains_subrange(list<int>) (process all)");
+  }
+
+  // Benchmark ranges::contains_subrange where we intersperse "near matches" inside the haystack.
+  {
+    auto bm = []<class Container>(std::string name) {
+      benchmark::RegisterBenchmark(
+          name,
+          [](auto& st) {
+            std::size_t const size = st.range(0);
+            using ValueType        = typename Container::value_type;
+            ValueType x            = Generate<ValueType>::random();
+            ValueType y            = random_different_from({x});
+            Container haystack(size, x);
+            std::size_t n = size / 10; // needle size is 10% of the haystack, but we'll never find it
+            assert(n > 0);
+            Container needle(n, y);
+
+            // intersperse near-matches inside the haystack
+            {
+              auto first = haystack.begin();
+              for (int i = 0; i != 10; ++i) {
+                first = std::copy_n(needle.begin(), n - 1, first);
+                ++first; // this causes the subsequence not to match because it has length n-1
+              }
+            }
+
+            for ([[maybe_unused]] auto _ : st) {
+              benchmark::DoNotOptimize(haystack);
+              benchmark::DoNotOptimize(needle);
+              auto result = std::ranges::contains_subrange(haystack, needle);
+              benchmark::DoNotOptimize(result);
+            }
+          })
+          ->Arg(1000) // non power-of-two
+          ->Arg(1024)
+          ->Arg(8192);
+    };
+    bm.operator()<std::vector<int>>("rng::contains_subrange(vector<int>) (near matches)");
+    bm.operator()<std::deque<int>>("rng::contains_subrange(deque<int>) (near matches)");
+    bm.operator()<std::list<int>>("rng::contains_subrange(list<int>) (near matches)");
+  }
+
+  // Special case: the two ranges are the same length (and they are equal, which is the worst case).
+  {
+    auto bm = []<class Container>(std::string name) {
+      benchmark::RegisterBenchmark(
+          name,
+          [](auto& st) {
+            std::size_t const size = st.range(0);
+            using ValueType        = typename Container::value_type;
+            ValueType x            = Generate<ValueType>::random();
+            Container haystack(size, x);
+            Container needle(size, x);
+
+            for (auto _ : st) {
+              benchmark::DoNotOptimize(haystack);
+              benchmark::DoNotOptimize(needle);
+              auto result = std::ranges::contains_subrange(haystack, needle);
+              benchmark::DoNotOptimize(result);
+            }
+          })
+          ->Arg(16)
+          ->Arg(32)
+          ->Arg(50) // non power-of-two
+          ->Arg(8192)
+          ->Arg(1 << 20);
+    };
+    bm.operator()<std::vector<int>>("rng::contains_subrange(vector<int>) (same length)");
+    bm.operator()<std::deque<int>>("rng::contains_subrange(deque<int>) (same length)");
+    bm.operator()<std::list<int>>("rng::contains_subrange(list<int>) (same length)");
+  }
+
+  // Special case: the needle contains a single element (which we never find, i.e. the worst case).
+  {
+    auto bm = []<class Container>(std::string name) {
+      benchmark::RegisterBenchmark(
+          name,
+          [](auto& st) {
+            std::size_t const size = st.range(0);
+            using ValueType        = typename Container::value_type;
+            ValueType x            = Generate<ValueType>::random();
+            ValueType y            = random_different_from({x});
+            Container haystack(size, x);
+            Container needle(1, y);
+
+            for (auto _ : st) {
+              benchmark::DoNotOptimize(haystack);
+              benchmark::DoNotOptimize(needle);
+              auto result = std::ranges::contains_subrange(haystack, needle);
+              benchmark::DoNotOptimize(result);
+            }
+          })
+          ->Arg(16)
+          ->Arg(32)
+          ->Arg(50) // non power-of-two
+          ->Arg(8192)
+          ->Arg(1 << 20);
+    };
+    bm.operator()<std::vector<int>>("rng::contains_subrange(vector<int>) (single element)");
+    bm.operator()<std::deque<int>>("rng::contains_subrange(deque<int>) (single element)");
+    bm.operator()<std::list<int>>("rng::contains_subrange(list<int>) (single element)");
   }
 
   benchmark::Initialize(&argc, argv);
--- libcxx/test/benchmarks/algorithms/nonmodifying/find_end.bench.cpp
+++ libcxx/test/benchmarks/algorithms/nonmodifying/find_end.bench.cpp
@@ -9,6 +9,7 @@
 // UNSUPPORTED: c++03, c++11, c++14, c++17
 
 #include <algorithm>
+#include <cassert>
 #include <cstddef>
 #include <deque>
 #include <forward_list>
@@ -38,8 +39,34 @@ int main(int argc, char** argv) {
     });
   };
 
-  // Benchmark {std,ranges}::find_end where the subsequence is found
-  // 25% into the sequence
+  auto register_benchmarks = [&](auto bm, std::string comment) {
+    // {std,ranges}::find_end(it1, it1, it2, it2)
+    bm.template operator()<std::vector<int>>("std::find_end(vector<int>) (" + comment + ")", std_find_end);
+    bm.template operator()<std::deque<int>>("std::find_end(deque<int>) (" + comment + ")", std_find_end);
+    bm.template operator()<std::list<int>>("std::find_end(list<int>) (" + comment + ")", std_find_end);
+    bm.template operator()<std::forward_list<int>>("std::find_end(forward_list<int>) (" + comment + ")", std_find_end);
+    bm.template operator()<std::vector<int>>("rng::find_end(vector<int>) (" + comment + ")", std::ranges::find_end);
+    bm.template operator()<std::deque<int>>("rng::find_end(deque<int>) (" + comment + ")", std::ranges::find_end);
+    bm.template operator()<std::list<int>>("rng::find_end(list<int>) (" + comment + ")", std::ranges::find_end);
+    bm.template operator()<std::forward_list<int>>(
+        "rng::find_end(forward_list<int>) (" + comment + ")", std::ranges::find_end);
+
+    // {std,ranges}::find_end(it1, it1, it2, it2, pred)
+    bm.template operator()<std::vector<int>>("std::find_end(vector<int>, pred) (" + comment + ")", std_find_end_pred);
+    bm.template operator()<std::deque<int>>("std::find_end(deque<int>, pred) (" + comment + ")", std_find_end_pred);
+    bm.template operator()<std::list<int>>("std::find_end(list<int>, pred) (" + comment + ")", std_find_end_pred);
+    bm.template operator()<std::forward_list<int>>(
+        "std::find_end(forward_list<int>, pred) (" + comment + ")", std_find_end_pred);
+    bm.template operator()<std::vector<int>>(
+        "rng::find_end(vector<int>, pred) (" + comment + ")", ranges_find_end_pred);
+    bm.template operator()<std::deque<int>>("rng::find_end(deque<int>, pred) (" + comment + ")", ranges_find_end_pred);
+    bm.template operator()<std::list<int>>("rng::find_end(list<int>, pred) (" + comment + ")", ranges_find_end_pred);
+    bm.template operator()<std::forward_list<int>>(
+        "rng::find_end(forward_list<int>, pred) (" + comment + ")", ranges_find_end_pred);
+  };
+
+  // Benchmark {std,ranges}::find_end where we never find the needle, which is the
+  // worst case.
   {
     auto bm = []<class Container>(std::string name, auto find_end) {
       benchmark::RegisterBenchmark(
@@ -49,16 +76,15 @@ int main(int argc, char** argv) {
             using ValueType        = typename Container::value_type;
             ValueType x            = Generate<ValueType>::random();
             ValueType y            = random_different_from({x});
-            Container c(size, x);
-            Container subrange(size / 10, y); // subrange of length 10% of the full range
+            Container haystack(size, x);
+            std::size_t n = size / 10; // needle size is 10% of the haystack, but we'll never find it
+            assert(n > 0);
+            Container needle(n, y);
 
-            // put the element we're searching for at 25% of the sequence
-            std::ranges::copy(subrange, std::next(c.begin(), size / 4));
-
-            for ([[maybe_unused]] auto _ : st) {
-              benchmark::DoNotOptimize(c);
-              benchmark::DoNotOptimize(subrange);
-              auto result = find_end(c.begin(), c.end(), subrange.begin(), subrange.end());
+            for (auto _ : st) {
+              benchmark::DoNotOptimize(haystack);
+              benchmark::DoNotOptimize(needle);
+              auto result = find_end(haystack.begin(), haystack.end(), needle.begin(), needle.end());
               benchmark::DoNotOptimize(result);
             }
           })
@@ -67,29 +93,10 @@ int main(int argc, char** argv) {
           ->Arg(8192)
           ->Arg(1 << 20);
     };
-    // {std,ranges}::find_end(it1, it1, it2, it2)
-    bm.operator()<std::vector<int>>("std::find_end(vector<int>) (bail 25%)", std_find_end);
-    bm.operator()<std::deque<int>>("std::find_end(deque<int>) (bail 25%)", std_find_end);
-    bm.operator()<std::list<int>>("std::find_end(list<int>) (bail 25%)", std_find_end);
-    bm.operator()<std::forward_list<int>>("std::find_end(forward_list<int>) (bail 25%)", std_find_end);
-    bm.operator()<std::vector<int>>("rng::find_end(vector<int>) (bail 25%)", std::ranges::find_end);
-    bm.operator()<std::deque<int>>("rng::find_end(deque<int>) (bail 25%)", std::ranges::find_end);
-    bm.operator()<std::list<int>>("rng::find_end(list<int>) (bail 25%)", std::ranges::find_end);
-    bm.operator()<std::forward_list<int>>("rng::find_end(forward_list<int>) (bail 25%)", std::ranges::find_end);
-
-    // {std,ranges}::find_end(it1, it1, it2, it2, pred)
-    bm.operator()<std::vector<int>>("std::find_end(vector<int>, pred) (bail 25%)", std_find_end_pred);
-    bm.operator()<std::deque<int>>("std::find_end(deque<int>, pred) (bail 25%)", std_find_end_pred);
-    bm.operator()<std::list<int>>("std::find_end(list<int>, pred) (bail 25%)", std_find_end_pred);
-    bm.operator()<std::forward_list<int>>("std::find_end(forward_list<int>, pred) (bail 25%)", std_find_end_pred);
-    bm.operator()<std::vector<int>>("rng::find_end(vector<int>, pred) (bail 25%)", ranges_find_end_pred);
-    bm.operator()<std::deque<int>>("rng::find_end(deque<int>, pred) (bail 25%)", ranges_find_end_pred);
-    bm.operator()<std::list<int>>("rng::find_end(list<int>, pred) (bail 25%)", ranges_find_end_pred);
-    bm.operator()<std::forward_list<int>>("rng::find_end(forward_list<int>, pred) (bail 25%)", ranges_find_end_pred);
+    register_benchmarks(bm, "process all");
   }
 
-  // Benchmark {std,ranges}::find_end where the subsequence is found
-  // 90% into the sequence (i.e. near the end)
+  // Benchmark {std,ranges}::find_end where we intersperse "near matches" inside the haystack.
   {
     auto bm = []<class Container>(std::string name, auto find_end) {
       benchmark::RegisterBenchmark(
@@ -99,43 +106,117 @@ int main(int argc, char** argv) {
             using ValueType        = typename Container::value_type;
             ValueType x            = Generate<ValueType>::random();
             ValueType y            = random_different_from({x});
-            Container c(size, x);
-            Container subrange(size / 10, y); // subrange of length 10% of the full range
+            Container haystack(size, x);
+            std::size_t n = size / 10; // needle size is 10% of the haystack
+            assert(n > 0);
+            Container needle(n, y);
 
-            // put the element we're searching for at 90% of the sequence
-            std::ranges::copy(subrange, std::next(c.begin(), (9 * size) / 10));
+            // intersperse near-matches inside the haystack
+            {
+              auto first = haystack.begin();
+              for (int i = 0; i != 10; ++i) {
+                first = std::copy_n(needle.begin(), n - 1, first);
+                ++first; // this causes the subsequence not to match because it has length n-1
+              }
+            }
 
             for ([[maybe_unused]] auto _ : st) {
-              benchmark::DoNotOptimize(c);
-              benchmark::DoNotOptimize(subrange);
-              auto result = find_end(c.begin(), c.end(), subrange.begin(), subrange.end());
+              benchmark::DoNotOptimize(haystack);
+              benchmark::DoNotOptimize(needle);
+              auto result = find_end(haystack.begin(), haystack.end(), needle.begin(), needle.end());
               benchmark::DoNotOptimize(result);
             }
           })
           ->Arg(1000) // non power-of-two
           ->Arg(1024)
-          ->Arg(8192)
-          ->Arg(1 << 20);
+          ->Arg(8192);
     };
-    // {std,ranges}::find_end(it1, it1, it2, it2)
-    bm.operator()<std::vector<int>>("std::find_end(vector<int>) (bail 90%)", std_find_end);
-    bm.operator()<std::deque<int>>("std::find_end(deque<int>) (bail 90%)", std_find_end);
-    bm.operator()<std::list<int>>("std::find_end(list<int>) (bail 90%)", std_find_end);
-    bm.operator()<std::forward_list<int>>("std::find_end(forward_list<int>) (bail 90%)", std_find_end);
-    bm.operator()<std::vector<int>>("rng::find_end(vector<int>) (bail 90%)", std::ranges::find_end);
-    bm.operator()<std::deque<int>>("rng::find_end(deque<int>) (bail 90%)", std::ranges::find_end);
-    bm.operator()<std::list<int>>("rng::find_end(list<int>) (bail 90%)", std::ranges::find_end);
-    bm.operator()<std::forward_list<int>>("rng::find_end(forward_list<int>) (bail 90%)", std::ranges::find_end);
+    register_benchmarks(bm, "near matches");
+  }
 
-    // {std,ranges}::find_end(it1, it1, it2, it2, pred)
-    bm.operator()<std::vector<int>>("std::find_end(vector<int>, pred) (bail 90%)", std_find_end_pred);
-    bm.operator()<std::deque<int>>("std::find_end(deque<int>, pred) (bail 90%)", std_find_end_pred);
-    bm.operator()<std::list<int>>("std::find_end(list<int>, pred) (bail 90%)", std_find_end_pred);
-    bm.operator()<std::forward_list<int>>("std::find_end(forward_list<int>, pred) (bail 90%)", std_find_end_pred);
-    bm.operator()<std::vector<int>>("rng::find_end(vector<int>, pred) (bail 90%)", ranges_find_end_pred);
-    bm.operator()<std::deque<int>>("rng::find_end(deque<int>, pred) (bail 90%)", ranges_find_end_pred);
-    bm.operator()<std::list<int>>("rng::find_end(list<int>, pred) (bail 90%)", ranges_find_end_pred);
-    bm.operator()<std::forward_list<int>>("rng::find_end(forward_list<int>, pred) (bail 90%)", ranges_find_end_pred);
+  // Special case: the two ranges are the same length (and they are equal, which is the worst case).
+  {
+    auto bm = []<class Container>(std::string name, auto find_end) {
+      benchmark::RegisterBenchmark(
+          name,
+          [find_end](auto& st) {
+            std::size_t const size = st.range(0);
+            using ValueType        = typename Container::value_type;
+            ValueType x            = Generate<ValueType>::random();
+            Container haystack(size, x);
+            Container needle(size, x);
+
+            for (auto _ : st) {
+              benchmark::DoNotOptimize(haystack);
+              benchmark::DoNotOptimize(needle);
+              auto result = find_end(haystack.begin(), haystack.end(), needle.begin(), needle.end());
+              benchmark::DoNotOptimize(result);
+            }
+          })
+          ->Arg(1000) // non power-of-two
+          ->Arg(1024)
+          ->Arg(8192);
+    };
+    register_benchmarks(bm, "same length");
+  }
+
+  // Special case: the needle contains a single element (which we never find, i.e. the worst case).
+  {
+    auto bm = []<class Container>(std::string name, auto find_end) {
+      benchmark::RegisterBenchmark(
+          name,
+          [find_end](auto& st) {
+            std::size_t const size = st.range(0);
+            using ValueType        = typename Container::value_type;
+            ValueType x            = Generate<ValueType>::random();
+            ValueType y            = random_different_from({x});
+            Container haystack(size, x);
+            Container needle(1, y);
+
+            for (auto _ : st) {
+              benchmark::DoNotOptimize(haystack);
+              benchmark::DoNotOptimize(needle);
+              auto result = find_end(haystack.begin(), haystack.end(), needle.begin(), needle.end());
+              benchmark::DoNotOptimize(result);
+            }
+          })
+          ->Arg(1000) // non power-of-two
+          ->Arg(1024)
+          ->Arg(8192);
+    };
+    register_benchmarks(bm, "single element");
+  }
+
+  // Special case: we have a match close to the end of the haystack (ideal case if we start searching from the end).
+  {
+    auto bm = []<class Container>(std::string name, auto find_end) {
+      benchmark::RegisterBenchmark(
+          name,
+          [find_end](auto& st) {
+            std::size_t const size = st.range(0);
+            using ValueType        = typename Container::value_type;
+            ValueType x            = Generate<ValueType>::random();
+            ValueType y            = random_different_from({x});
+            Container haystack(size, x);
+            std::size_t n = size / 10; // needle size is 10% of the haystack
+            assert(n > 0);
+            Container needle(n, y);
+
+            // put the needle at 90% of the haystack
+            std::ranges::copy(needle, std::next(haystack.begin(), (9 * size) / 10));
+
+            for (auto _ : st) {
+              benchmark::DoNotOptimize(haystack);
+              benchmark::DoNotOptimize(needle);
+              auto result = find_end(haystack.begin(), haystack.end(), needle.begin(), needle.end());
+              benchmark::DoNotOptimize(result);
+            }
+          })
+          ->Arg(1000) // non power-of-two
+          ->Arg(1024)
+          ->Arg(8192);
+    };
+    register_benchmarks(bm, "match near end");
   }
 
   benchmark::Initialize(&argc, argv);
