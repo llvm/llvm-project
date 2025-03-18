bb14ae0e2f9c5608be270a3e7de8e9263cafebad
Felipe de Azevedo Piovezan <fpiovezan@apple.com>
2025-03-18 06:59:46 -0300

    [lldb][swift] Add function to extract funclet numbers -- fixes
    This was originally submitted to llvm.org [1], and then cherry-picked into
    the stable branch. However, when cherry-picking to stable, swift-only
    tests had to be updated [1]. These changes were never pushed to next.
    
    [1]: 0990d2ecd5e13fbf67a2b332d5e603c580a96eca

--- lldb/test/API/lang/swift/async_breakpoints/TestSwiftAsyncBreakpoints.py
+++ lldb/test/API/lang/swift/async_breakpoints/TestSwiftAsyncBreakpoints.py
@@ -18,20 +18,16 @@ class TestSwiftAsyncBreakpoints(lldbtest.TestBase):
         )
         breakpoint2 = target.BreakpointCreateBySourceRegex("Breakpoint2", filespec)
         breakpoint3 = target.BreakpointCreateBySourceRegex("Breakpoint3", filespec)
-        self.assertEquals(breakpoint1.GetNumLocations(), 2)
+        self.assertEquals(breakpoint1.GetNumLocations(), 1)
         self.assertEquals(breakpoint2.GetNumLocations(), 1)
-        self.assertEquals(breakpoint3.GetNumLocations(), 2)
+        self.assertEquals(breakpoint3.GetNumLocations(), 1)
 
         location11 = breakpoint1.GetLocationAtIndex(0)
-        location12 = breakpoint1.GetLocationAtIndex(1)
         self.assertEquals(location11.GetHitCount(), 1)
-        self.assertEquals(location12.GetHitCount(), 0)
 
         self.assertEquals(thread.GetStopDescription(128), "breakpoint 1.1")
         process.Continue()
-        self.assertEquals(thread.GetStopDescription(128), "breakpoint 1.2")
 
-        thread.StepOver()
         self.assertEquals(thread.GetStopDescription(128), "breakpoint 2.1")
         self.expect("expr timestamp1", substrs=["42"])
 
@@ -39,16 +35,8 @@ class TestSwiftAsyncBreakpoints(lldbtest.TestBase):
         self.assertIn("breakpoint 3.1", thread.GetStopDescription(128))
         self.expect("expr timestamp1", substrs=["42"])
 
-        process.Continue()
-        self.assertIn("breakpoint 3.2", thread.GetStopDescription(128))
-        self.expect("expr timestamp1", substrs=["42"])
-
-        thread.StepOver()
-        self.expect("expr timestamp1", substrs=["42"])
-        self.expect("expr timestamp2", substrs=["43"])
-
         self.runCmd("settings set language.enable-filter-for-line-breakpoints false")
         breakpoint1_no_filter = target.BreakpointCreateBySourceRegex(
             "Breakpoint1", filespec
         )
-        self.assertEquals(breakpoint1_no_filter.GetNumLocations(), 3)
+        self.assertEquals(breakpoint1_no_filter.GetNumLocations(), 2)
