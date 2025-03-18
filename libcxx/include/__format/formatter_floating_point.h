d1156fcb56891fb1a426c3e8331a51d47f98a1b8
Alexander Kornienko <alexfh@google.com>
2025-03-18 02:05:51 +0100

    Revert "[libc++] Optimize num_put integral functions" (#131613)
    Reverts llvm/llvm-project#120859
    
    This change breaks formatting of `0` with `std::showbase` + `std::hex`
    or `std::oct`, as well as `+0` with `std::showpos`. I believe the new
    behavior is violating the standard. See
    https://github.com/llvm/llvm-project/pull/120859#issuecomment-2723970242
    and later comments for details and explanation.

--- libcxx/include/__format/formatter_floating_point.h
+++ libcxx/include/__format/formatter_floating_point.h
@@ -19,7 +19,6 @@
 #include <__assert>
 #include <__charconv/chars_format.h>
 #include <__charconv/to_chars_floating_point.h>
-#include <__charconv/to_chars_integral.h>
 #include <__charconv/to_chars_result.h>
 #include <__concepts/arithmetic.h>
 #include <__concepts/same_as.h>
