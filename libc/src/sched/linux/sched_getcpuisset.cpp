522c6697db9d9d8072dc3ecb190ebe71a853a145
krishna2803 <kpandey81930@gmail.com>
2025-03-19 03:31:31 +0530

    chore: add nullptr gaurds

--- libc/src/sched/linux/sched_getcpuisset.cpp
+++ libc/src/sched/linux/sched_getcpuisset.cpp
@@ -8,8 +8,9 @@
 
 #include "src/sched/sched_getcpuisset.h"
 
-#include "src/__support/common.h"        // LLVM_LIBC_FUNCTION
-#include "src/__support/macros/config.h" // LIBC_NAMESPACE_DECL
+#include "src/__support/common.h"            // LLVM_LIBC_FUNCTION
+#include "src/__support/macros/config.h"     // LIBC_NAMESPACE_DECL
+#include "src/__support/macros/null_check.h" // LIBC_CRASH_ON_NULLPTR
 
 #include "hdr/sched_macros.h" // NCPUBITS
 #include "hdr/types/cpu_set_t.h"
@@ -19,6 +20,8 @@ namespace LIBC_NAMESPACE_DECL {
 
 LLVM_LIBC_FUNCTION(int, __sched_getcpuisset,
                    (int cpu, const size_t cpuset_size, cpu_set_t *set)) {
+  LIBC_CRASH_ON_NULLPTR(set);
+
   if (static_cast<size_t>(cpu) / 8 < cpuset_size) {
     const size_t element_index = static_cast<size_t>(cpu) / NCPUBITS;
     const size_t bit_position = static_cast<size_t>(cpu) % NCPUBITS;
--- libc/src/sched/linux/sched_setcpuzero.cpp
+++ libc/src/sched/linux/sched_setcpuzero.cpp
@@ -8,8 +8,9 @@
 
 #include "src/sched/sched_setcpuzero.h"
 
-#include "src/__support/common.h"        // LLVM_LIBC_FUNCTION
-#include "src/__support/macros/config.h" // LIBC_NAMESPACE_DECL
+#include "src/__support/common.h"            // LLVM_LIBC_FUNCTION
+#include "src/__support/macros/config.h"     // LIBC_NAMESPACE_DECL
+#include "src/__support/macros/null_check.h" // LIBC_CRASH_ON_NULLPTR
 
 #include "hdr/types/cpu_set_t.h"
 #include "hdr/types/size_t.h"
@@ -18,6 +19,7 @@ namespace LIBC_NAMESPACE_DECL {
 
 LLVM_LIBC_FUNCTION(void, __sched_setcpuzero,
                    (const size_t cpuset_size, cpu_set_t *set)) {
+  LIBC_CRASH_ON_NULLPTR(set);
   __builtin_memset(set, 0, cpuset_size);
 }
 
