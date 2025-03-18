67e763e7c17d179ac41ebe4d8d5a4b3c6b6bf5f6
John Holdsworth <github@johnholdsworth.com>
2025-03-18 17:31:40 +0100

    [lld][MachO] Support for -interposable

--- lld/MachO/Driver.cpp
+++ lld/MachO/Driver.cpp
@@ -1676,6 +1676,7 @@ bool link(ArrayRef<const char *> argsArr, llvm::raw_ostream &stdoutOS,
 
   // Must be set before any InputSections and Symbols are created.
   config->deadStrip = args.hasArg(OPT_dead_strip);
+  config->interposable = args.hasArg(OPT_interposable);
 
   config->systemLibraryRoots = getSystemLibraryRoots(args);
   if (const char *path = getReproduceOption(args)) {
