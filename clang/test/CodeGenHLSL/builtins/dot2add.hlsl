3e265ffc7ba3486027e63105acf2064699e58807
Sumit Agarwal <sumitagarwal@microsoft.com>
2025-03-18 17:33:30 -0700

    WIP

---
+++ clang/test/CodeGenHLSL/builtins/dot2add.hlsl
@@ -0,0 +1,15 @@
+// RUN: %clang_cc1 -finclude-default-header -triple \
+// RUN:   dxil-pc-shadermodel6.3-compute %s -emit-llvm -disable-llvm-passes -o - | \
+// RUN:   FileCheck %s -DTARGET=dx
+// RUN: %clang_cc1 -finclude-default-header -triple \
+// RUN:   spirv-pc-vulkan-compute %s -emit-llvm -disable-llvm-passes -o - | \
+// RUN:   FileCheck %s -DTARGET=spv
+
+// Test basic lowering to runtime function call.
+
+// CHECK-LABEL: test
+float test(half2 p1, half2 p2, float p3) {
+  return dot2add(p1, p2, p3);
+}
+
+// CHECK: declare [[TY]] @llvm.[[TARGET]].dot4add.i8packed([[TY]], [[TY]], [[TY]])
