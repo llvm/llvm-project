08792d33933092bf25c1c059fe2fd402030ca5c5
meltq <alissxlace@proton.me>
2025-03-19 15:19:37 +0530

    Add hypotf16() function

---
+++ libc/test/src/math/hypotf16_test.cpp
@@ -0,0 +1,27 @@
+//===-- Unittests for hypotf16 --------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "HypotTest.h"
+#include "hypotf_hard_to_round.h"
+
+#include "src/math/hypotf16.h"
+
+using LlvmLibcHypotf16Test = HypotTestTemplate<float16>;
+
+// TEST_F(LlvmLibcHypotf16Test, SubnormalRange) {
+//   test_subnormal_range(&LIBC_NAMESPACE::hypotf16);
+// }
+//
+// TEST_F(LlvmLibcHypotf16Test, NormalRange) {
+//   test_normal_range(&LIBC_NAMESPACE::hypotf16);
+// }
+//
+// TEST_F(LlvmLibcHypotf16Test, TrickyInputs) {
+//   test_input_list(&LIBC_NAMESPACE::hypotf16, N_HARD_TO_ROUND,
+//                   HYPOTF_HARD_TO_ROUND);
+// }
