a4496d28d0f33f7416f90c23ef1892b9423dfe4a
Krzysztof Drewniak <Krzysztof.Drewniak@amd.com>
2025-03-19 17:12:12 +0000

    Fix Python type annotations

--- mlir/python/mlir/extras/types.py
+++ mlir/python/mlir/extras/types.py
@@ -3,10 +3,11 @@
 #  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 from functools import partial
-from typing import Optional, List
+from typing import Optional, List, Union
 
 from ..ir import (
     Attribute,
+    AffineMapAttr,
     BF16Type,
     ComplexType,
     ContiguousLayoutAttr,
@@ -153,7 +154,9 @@ def memref(
     *shape,
     element_type: Type = None,
     memory_space: Optional[int] = None,
-    layout: Optional[ContiguousLayoutAttr | StridedLayoutAttr] = None,
+    layout: Optional[
+        Union[ContiguousLayoutAttr, StridedLayoutAttr, AffineMapAttr]
+    ] = None,
 ):
     if memory_space is not None:
         memory_space = Attribute.parse(str(memory_space))
