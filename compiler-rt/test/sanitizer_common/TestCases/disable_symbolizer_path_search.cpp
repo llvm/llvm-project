f89721da8a6813d0a9103b6efe4c61efb27ba411
Midhunesh <midhunesh.p@ibm.com>
2025-03-19 07:34:33 +0530

    test update

--- compiler-rt/test/sanitizer_common/TestCases/disable_symbolizer_path_search.cpp
+++ compiler-rt/test/sanitizer_common/TestCases/disable_symbolizer_path_search.cpp
@@ -4,9 +4,7 @@
 // RUN: echo $ASAN_SYMBOLIZER_PATH
 // RUN: export ASAN_SYMBOLIZER_PATH= TSAN_SYMBOLIZER_PATH= MSAN_SYMBOLIZER_PATH= UBSAN_SYMBOLIZER_PATH= 
 // RUN: echo $ASAN_SYMBOLIZER_PATH
-// RUN: env -u ASAN_SYMBOLIZER_PATH
-// RUN: echo $ASAN_SYMBOLIZER_PATH
-// RUN: %env_tool_opts=verbosity=3 %run %t 2>&1 | FileCheck %s
+// RUN: %env_tool_opts=verbosity=3 -u ASAN_SYMBOLIZER_PATH -u TSAN_SYMBOLIZER_PATH -u MSAN_SYMBOLIZER_PATH -u UBSAN_SYMBOLIZER_PATH %run %t 2>&1 | FileCheck %s
 
 // CHECK: Symbolizer path search is disabled in the runtime build configuration
 
