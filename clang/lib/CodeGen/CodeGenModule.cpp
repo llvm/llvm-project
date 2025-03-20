0c4101d47003fa7e4260522b05eb0d8edbc7b958
Nikita Popov <npopov@redhat.com>
2025-03-20 19:35:27 +0100

    [CodeGen] Fix unused variable warning (NFC)

--- clang/lib/CodeGen/CodeGenModule.cpp
+++ clang/lib/CodeGen/CodeGenModule.cpp
@@ -2935,7 +2935,7 @@ void CodeGenModule::SetFunctionAttributes(GlobalDecl GD, llvm::Function *F,
                                           bool IsIncompleteFunction,
                                           bool IsThunk) {
 
-  if (llvm::Intrinsic::ID IID = F->getIntrinsicID()) {
+  if (F->getIntrinsicID() != llvm::Intrinsic::not_intrinsic) {
     // If this is an intrinsic function, the attributes will have been set
     // when the function was created.
     return;
