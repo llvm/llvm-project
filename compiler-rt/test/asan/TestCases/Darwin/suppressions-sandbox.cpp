5cadefc1f5e554d9f766a19443938fd3cd0d5446
Paddy McDonald <paddy_mcdonald@apple.com>
2025-03-20 12:16:53 -0700

    correct merge error

--- compiler-rt/test/asan/TestCases/Darwin/suppressions-sandbox.cpp
+++ compiler-rt/test/asan/TestCases/Darwin/suppressions-sandbox.cpp
@@ -7,7 +7,7 @@
 // RUN: not %run %t 2>&1 | FileCheck --check-prefix=CHECK-CRASH %s
 
 // Check that suppressing a function name works within a no-fork sandbox
-// RUN: echo "interceptor_via_fun:CFStringCreateWithBytes" > %t.supp
+// RUN: echo "interceptor_via_fun:createCFString" > %t.supp
 // RUN: %env_asan_opts=suppressions='"%t.supp"' \
 // RUN:   sandbox-exec -p '(version 1)(allow default)(deny process-fork)' \
 // RUN:   %run %t 2>&1 | FileCheck --check-prefix=CHECK-IGNORE %s
