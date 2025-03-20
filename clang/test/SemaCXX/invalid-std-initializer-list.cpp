553ced1e367c0ec6399e71e7a3a3685a550f3431
offsetof <offsetof@mailo.com>
2025-03-20 20:54:45 +0000

    [clang] Fix crash on invalid `std::initializer_list<T>` template-id
    In `Sema::BuildStdInitializerList`, check that the synthesized
    template-id `std::initializer_list<T>` is valid (which might not be the
    case if the template has associated constraints or subsequent parameters
    with default arguments) before forming the type.

---
+++ clang/test/SemaCXX/invalid-std-initializer-list.cpp
@@ -0,0 +1,14 @@
+// RUN: %clang_cc1 %s -verify -std=c++20
+
+namespace std {
+
+template<class T, class = T::x> // expected-error 2 {{type 'int' cannot be used prior to '::' because it has no members}}
+class initializer_list;
+
+}
+
+auto x = {1}; // expected-note {{in instantiation of default argument for 'initializer_list<int>' required here}}
+
+void f() {
+	for(int x : {1, 2}); // expected-note {{in instantiation of default argument for 'initializer_list<int>' required here}}
+}
