bc3d715d2b6d3c7d443927ce0d48594b669b31ec
Vera Mitchell <vgm@apple.com>
2025-03-20 10:36:58 -0600

    don't collect bases for types without definitions
    rdar://123430367

--- clang/test/Index/extract-api-cursor-cpp.cpp
+++ clang/test/Index/extract-api-cursor-cpp.cpp
@@ -13,6 +13,11 @@ class MyClass {
     my_vec myVec;
 };
 
+struct OuterStruct {
+    struct InnerStruct;
+    int outer_field;
+};
+
 // RUN: c-index-test -single-symbol-sgf-at=%s:13:7 local %s | FileCheck --check-prefix=CHECK-VEC-TYPE %s
 // CHECK-VEC-TYPE: "parentContexts":[{"kind":"c++.typealias","name":"my_vec","usr":"c:@my_vec"}]
 // CHECK-VEC-TYPE: "declarationFragments":[{"kind":"keyword","spelling":"typedef"},{"kind":"text","spelling":" "},{"kind":"typeIdentifier","preciseIdentifier":"c:@ST>1#T@basic_vector","spelling":"basic_vector"},{"kind":"text","spelling":"<"},{"kind":"typeIdentifier","preciseIdentifier":"c:I","spelling":"int"},{"kind":"text","spelling":"> "},{"kind":"identifier","spelling":"my_vec"},{"kind":"text","spelling":";"}]
@@ -27,3 +32,10 @@ class MyClass {
 // CHECK-MYVEC: "kind":{"displayName":"Instance Property","identifier":"c++.property"}
 // CHECK-MYVEC: "title":"myVec"
 // CHECK-MYVEC: "pathComponents":["MyClass","myVec"]
+
+// RUN: c-index-test -single-symbol-sgf-at=%s:17:17 local %s | FileCheck --check-prefix=CHECK-INNER %s
+// CHECK-INNER: "parentContexts":[{"kind":"c++.struct","name":"OuterStruct","usr":"c:@S@OuterStruct"},{"kind":"c++.struct","name":"InnerStruct","usr":"c:@S@OuterStruct@S@InnerStruct"}]
+// CHECK-INNER: "identifier":{"interfaceLanguage":"c++","precise":"c:@S@OuterStruct@S@InnerStruct"}
+// CHECK-INNER: "kind":{"displayName":"Structure","identifier":"c++.struct"}
+// CHECK-INNER: "title":"InnerStruct"
+// CHECK-INNER: "pathComponents":["OuterStruct","InnerStruct"]
