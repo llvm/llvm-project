4b72ede9e1b89ffc44be3ec352d0a82c7abe7fd4
Jannick Kremer <jannick.kremer@mailbox.org>
2025-03-20 12:11:20 +0900

    Use existing test input files instead of in-memory files

--- clang/bindings/python/tests/cindex/test_file.py
+++ clang/bindings/python/tests/cindex/test_file.py
@@ -1,12 +1,13 @@
 import os
 
-from clang.cindex import Config, File, Index
+from clang.cindex import Config, File, Index, TranslationUnit
 
 if "CLANG_LIBRARY_PATH" in os.environ:
     Config.set_library_path(os.environ["CLANG_LIBRARY_PATH"])
 
 import unittest
 
+kInputsDir = os.path.join(os.path.dirname(__file__), "INPUTS")
 
 class TestFile(unittest.TestCase):
     def test_file(self):
@@ -18,16 +19,14 @@ class TestFile(unittest.TestCase):
         self.assertEqual(repr(file), "<File: t.c>")
 
     def test_file_eq(self):
-        index = Index.create()
-        tu = index.parse(
-            "t.c",
-            unsaved_files=[
-                ("t.c", "int a = 729;"),
-                ("s.c", "int a = 729;"),
-            ],
-        )
-        file1 = File.from_name(tu, "t.c")
-        file2 = File.from_name(tu, "s.c")
+        path = os.path.join(kInputsDir, "hello.cpp")
+        header_path = os.path.join(kInputsDir, "header3.h")
+        tu = TranslationUnit.from_source(path)
+        file1 = File.from_name(tu, path)
+        file2 = File.from_name(tu, header_path)
+        file2_2 = File.from_name(tu, header_path)
+
         self.assertEqual(file1, file1)
+        self.assertEqual(file2, file2_2)
         self.assertNotEqual(file1, file2)
         self.assertNotEqual(file1, "t.c")
