fdb40eac5e8059e5b7d1b85afa4d8c3641a58c0e
Poonam Vilas Metkar <poonammetkar@microsoft.com>
2025-03-20 14:52:23 -0700

    Address review comments

--- clang/test/CodeGenHLSL/builtins/asint16.hlsl
+++ clang/test/CodeGenHLSL/builtins/asint16.hlsl
@@ -1,6 +1,6 @@
 // RUN: %clang_cc1 -finclude-default-header -x hlsl -triple dxil-pc-shadermodel6.2-library %s -fnative-half-type -emit-llvm -O1 -o - | FileCheck %s
 
-// CHECK: define {{.*}}test_ints{{.*}}(i16 {{.*}} [[VAL:%.*]]){{.*}}
+// CHECK-LABEL: define {{.*}}test_ints{{.*}}(i16 {{.*}} [[VAL:%.*]]){{.*}}
 // CHECK-NOT: bitcast
 // CHECK: ret i16 [[VAL]]
 int16_t test_int(int16_t p0)
@@ -8,41 +8,46 @@ int16_t test_int(int16_t p0)
     return asint16(p0);
 }
 
-//CHECK: define {{.*}}test_uint{{.*}}(i16 {{.*}} [[VAL:%.*]]){{.*}}
+//CHECK-LABEL: define {{.*}}test_uint
+//CHECK-SAME: {{.*}}(i16 {{.*}} [[VAL:%.*]]){{.*}}
 //CHECK-NOT: bitcast
-//CHECK: ret i16 [[VAL]]
+//CHECK-NEXT: ret i16 [[VAL]]
 int16_t test_uint(uint16_t p0)
 {
     return asint16(p0);
 }
 
-//CHECK: define {{.*}}test_half{{.*}}(half {{.*}} [[VAL:%.*]]){{.*}}
+//CHECK-LABEL: define {{.*}}test_half
+//CHECK-SAME: {{.*}}(half {{.*}} [[VAL:%.*]]){{.*}}
 //CHECK: [[RES:%.*]] = bitcast half [[VAL]] to i16
-//CHECK : ret i16 [[RES]]
+//CHECK-NEXT : ret i16 [[RES]]
 int16_t test_half(half p0)
 {
     return asint16(p0);
 }
 
-//CHECK: define {{.*}}test_vector_int{{.*}}(<4 x i16> {{.*}} [[VAL:%.*]]){{.*}}
+//CHECK-LABEL: define {{.*}}test_vector_int
+//CHECK-SAME: {{.*}}(<4 x i16> {{.*}} [[VAL:%.*]]){{.*}}
 //CHECK-NOT: bitcast
-//CHECK: ret <4 x i16> [[VAL]]
+//CHECK-NEXT: ret <4 x i16> [[VAL]]
 int16_t4 test_vector_int(int16_t4 p0)
 {
     return asint16(p0);
 }
 
-//CHECK: define {{.*}}test_vector_uint{{.*}}(<4 x i16> {{.*}} [[VAL:%.*]]){{.*}}
+//CHECK-LABEL: define {{.*}}test_vector_uint
+//CHECK-SAME: {{.*}}(<4 x i16> {{.*}} [[VAL:%.*]]){{.*}}
 //CHECK-NOT: bitcast
-//CHECK: ret <4 x i16> [[VAL]]
+//CHECK-NEXT: ret <4 x i16> [[VAL]]
 int16_t4 test_vector_uint(uint16_t4 p0)
 {
     return asint16(p0);
 }
 
-//CHECK: define {{.*}}fn{{.*}}(<4 x half> {{.*}} [[VAL:%.*]]){{.*}}
+//CHECK-LABEL: define {{.*}}fn
+//CHECK-SAME: {{.*}}(<4 x half> {{.*}} [[VAL:%.*]]){{.*}}
 //CHECK: [[RES:%.*]] = bitcast <4 x half> [[VAL]] to <4 x i16>
-//CHECK: ret <4 x i16> [[RES]]
+//CHECK-NEXT: ret <4 x i16> [[RES]]
 int16_t4 fn(half4 p1)
 {
     return asint16(p1);
