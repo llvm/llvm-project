889126709951d77f65aeb1ed843494bfd0222898
Thurston Dang <thurston@google.com>
2025-03-20 17:44:04 +0000

    [asan][NFCI] Rename asan_(malloc_)?linux.cpp to ...unix.cpp
    asan_(malloc_)?linux.cpp are misleadingly named because they cover
    many non-Linux OSes, such as BSDs, Fuchsia and Solaris. This is a
    footgun where changes may be made to these files without remembering it
    is not Linux-specific (e.g., I broke the Solaris build - see https://github.com/llvm/llvm-project/pull/131975#issuecomment-2741097471).
    
    This patch mitigates the issue by renaming the file from ...linux to ...unix, which should hopefully give pause to anyone (me) when making Linux-specific changes.

--- compiler-rt/lib/asan/asan_unix.cpp
+++ compiler-rt/lib/asan/asan_unix.cpp
@@ -1,4 +1,4 @@
-//===-- asan_linux.cpp ----------------------------------------------------===//
+//===-- asan_unix.cpp -----------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
