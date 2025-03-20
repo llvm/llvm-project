9599ba2646edf73c4efef0f3fe02b07577374276
Sumit Agarwal <sumitagarwal@microsoft.com>
2025-03-20 13:22:33 -0700

    End to end working code for dot2add

--- clang/test/CodeGenHLSL/builtins/dot2add.hlsl
+++ clang/test/CodeGenHLSL/builtins/dot2add.hlsl
@@ -1,14 +1,17 @@
 // RUN: %clang_cc1 -finclude-default-header -triple \
 // RUN:   dxil-pc-shadermodel6.3-compute %s -emit-llvm -disable-llvm-passes -o - | \
-// RUN:   FileCheck %s -DTARGET=dx
+// RUN:   FileCheck %s --check-prefixes=CHECK,CHECK-DXIL
+// RUN: %clang_cc1 -finclude-default-header -triple \
+// RUN:   spirv-pc-vulkan-compute %s -emit-llvm -disable-llvm-passes -o - | \
+// RUN:   FileCheck %s --check-prefixes=CHECK,CHECK-SPIRV
 
 // Test basic lowering to runtime function call.
 
-// CHECK-LABEL: test
 float test(half2 p1, half2 p2, float p3) {
-  // CHECK-DXIL:  %hlsl.dot2add = call reassoc nnan ninf nsz arcp afn float @llvm.dx.dot2add.v2f32(<2 x float> %0, <2 x float> %1, float %2)
-  // CHECK: ret float %hlsl.dot2add
+  // CHECK-SPIRV:  %[[MUL:.*]] = call {{.*}} float @llvm.spv.fdot.v2f32(<2 x float> %1, <2 x float> %2)
+  // CHECK-SPIRV:  %[[C:.*]] = load float, ptr %c.addr, align 4
+  // CHECK-SPIRV:  %[[RES:.*]] = fadd {{.*}} float %[[MUL]], %[[C]]
+  // CHECK-DXIL:  %[[RES:.*]] = call {{.*}} float @llvm.dx.dot2add.v2f32(<2 x float> %0, <2 x float> %1, float %2)
+  // CHECK:  ret float %[[RES]]
   return dot2add(p1, p2, p3);
 }
-
-// CHECK: declare [[TY]] @llvm.[[TARGET]].dot4add.i8packed([[TY]], [[TY]], [[TY]])
