161d4d31ba8f2ead27475d681cc8a1812e2c1271
Qi Zhao <zhaoqi01@loongson.cn>
2025-03-21 11:32:31 +0800

    [LoongArch] Fix the type of tls-le symbols

--- llvm/lib/Target/LoongArch/MCTargetDesc/LoongArchMCExpr.cpp
+++ llvm/lib/Target/LoongArch/MCTargetDesc/LoongArchMCExpr.cpp
@@ -275,6 +275,7 @@ void LoongArchMCExpr::fixELFSymbolsInTLSFixups(MCAssembler &Asm) const {
   case VK_LoongArch_TLS_GD_HI20:
   case VK_LoongArch_TLS_DESC_PC_HI20:
   case VK_LoongArch_TLS_DESC_HI20:
+  case VK_LoongArch_TLS_LE_HI20_R:
   case VK_LoongArch_TLS_LD_PCREL20_S2:
   case VK_LoongArch_TLS_GD_PCREL20_S2:
   case VK_LoongArch_TLS_DESC_PCREL20_S2:
