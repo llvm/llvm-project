f8f20b8994524e9ba823fa5750e41e1fd80e531a
Poonam Vilas Metkar <poonammetkar@microsoft.com>
2025-03-20 17:10:28 -0700

    Fix codegen test failure

--- clang/test/CodeGenHLSL/builtins/asint16.hlsl
+++ clang/test/CodeGenHLSL/builtins/asint16.hlsl
@@ -1,8 +1,10 @@
 // RUN: %clang_cc1 -finclude-default-header -x hlsl -triple dxil-pc-shadermodel6.2-library %s -fnative-half-type -emit-llvm -O1 -o - | FileCheck %s
 
-// CHECK-LABEL: define {{.*}}test_ints{{.*}}(i16 {{.*}} [[VAL:%.*]]){{.*}}
-// CHECK-NOT: bitcast
-// CHECK-NEXT: ret i16 [[VAL]]
+//CHECK-LABEL: define {{.*}}test_ints
+//CHECK-SAME: {{.*}}(i16 {{.*}} [[VAL:%.*]]){{.*}}
+//CHECK-NOT: bitcast
+//CHECK: entry:
+//CHECK-NEXT: ret i16 [[VAL]]
 int16_t test_int(int16_t p0)
 {
     return asint16(p0);
@@ -10,7 +12,8 @@ int16_t test_int(int16_t p0)
 
 //CHECK-LABEL: define {{.*}}test_uint
 //CHECK-SAME: {{.*}}(i16 {{.*}} [[VAL:%.*]]){{.*}}
-//CHECK-NOT: bitcast
+//CHECK-NOT:bitcast
+//CHECK: entry:
 //CHECK-NEXT: ret i16 [[VAL]]
 int16_t test_uint(uint16_t p0)
 {
@@ -29,6 +32,7 @@ int16_t test_half(half p0)
 //CHECK-LABEL: define {{.*}}test_vector_int
 //CHECK-SAME: {{.*}}(<4 x i16> {{.*}} [[VAL:%.*]]){{.*}}
 //CHECK-NOT: bitcast
+//CHECK: entry:
 //CHECK-NEXT: ret <4 x i16> [[VAL]]
 int16_t4 test_vector_int(int16_t4 p0)
 {
@@ -38,6 +42,7 @@ int16_t4 test_vector_int(int16_t4 p0)
 //CHECK-LABEL: define {{.*}}test_vector_uint
 //CHECK-SAME: {{.*}}(<4 x i16> {{.*}} [[VAL:%.*]]){{.*}}
 //CHECK-NOT: bitcast
+//CHECK-NEXT: entry:
 //CHECK-NEXT: ret <4 x i16> [[VAL]]
 int16_t4 test_vector_uint(uint16_t4 p0)
 {
