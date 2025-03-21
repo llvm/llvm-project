62238e44bebc7ec6767e5af2d7b5a9caffb5345a
V Donaldson <vdonaldson@nvidia.com>
2025-03-21 07:30:15 -0700

    windows build fix

--- flang-rt/lib/runtime/exceptions.cpp
+++ flang-rt/lib/runtime/exceptions.cpp
@@ -101,18 +101,21 @@ uint32_t RTNAME(fetestexcept)(uint32_t excepts) {
 #endif
 }
 void RTNAME(fedisableexcept)(uint32_t excepts) {
+  extern int fedisableexcept(int);
   fedisableexcept(excepts);
 #if defined(_MM_EXCEPT_DENORM)
   _mm_setcsr(_mm_getcsr() | ((excepts & _MM_EXCEPT_MASK) << 7));
 #endif
 }
 void RTNAME(feenableexcept)(uint32_t excepts) {
+  extern int feenableexcept(int);
   feenableexcept(excepts);
 #if defined(_MM_EXCEPT_DENORM)
   _mm_setcsr(_mm_getcsr() & ~((excepts & _MM_EXCEPT_MASK) << 7));
 #endif
 }
 uint32_t RTNAME(fegetexcept)() {
+  extern int fegetexcept(void);
 #if defined(_MM_EXCEPT_DENORM)
   return (63 - ((_mm_getcsr() >> 7) & _MM_EXCEPT_MASK)) | fegetexcept();
 #else
