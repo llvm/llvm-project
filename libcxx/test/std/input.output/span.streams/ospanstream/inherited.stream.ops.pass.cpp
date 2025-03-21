4b337943d5358b2f018b5150549cd5047241a1f7
Hristo Hristov <hghristov.rmm@gmail.com>
2025-03-21 09:39:44 +0200

    WIP - `ospanstream`

--- libcxx/test/std/input.output/span.streams/ospanstream/inherited.stream.ops.pass.cpp
+++ libcxx/test/std/input.output/span.streams/ospanstream/inherited.stream.ops.pass.cpp
@@ -21,9 +21,6 @@
 #include <spanstream>
 #include <string>
 #include <string_view>
-#include <vector>
-
-#include <print>
 
 #include "constexpr_char_traits.h"
 #include "test_macros.h"
@@ -43,11 +40,10 @@ void test() {
   // Create a std::span test value
   CharT arr[arrSize]{};
   initialize_array_from_string_view(arr, sv);
-  std::vector<CharT> vec(arr, arr + arrSize);
+  std::span<CharT> sp{arr};
 
   // `std::span` + Mode: default (`out`)
   {
-    std::span<CharT> sp{arr};
     SpStream spSt(sp);
 
     assert(spSt);
@@ -56,71 +52,56 @@ void test() {
     assert(spSt.good());
     assert(spSt.span().size() == 0);
 
+    // Check underlying buffer
+    assert(arr[0] == CH('z'));
+    assert(arr[1] == CH('m'));
+    assert(arr[2] == CH('t'));
+
+    spSt << SV("snt");
+
+    assert(spSt.span().size() == 3);
+
+    assert(spSt.span()[0] == CH('s'));
+    assert(spSt.span()[1] == CH('n'));
+    assert(spSt.span()[2] == CH('t'));
+    // Check underlying buffer
+    assert(arr[0] == CH('s'));
+    assert(arr[1] == CH('n'));
+    assert(arr[2] == CH('t'));
+
+    assert(spSt);
+    assert(!spSt.bad());
+    assert(!spSt.fail());
+    assert(spSt.good());
+
+    spSt << 71;
+
+    assert(spSt.span().size() == 5);
+
+    assert(spSt.span()[3] == CH('7'));
+    assert(spSt.span()[4] == CH('1'));
+    // Check underlying buffer
+    assert(arr[3] == CH('7'));
+    assert(arr[4] == CH('1'));
+
+    assert(spSt);
+    assert(!spSt.bad());
+    assert(!spSt.fail());
+    assert(spSt.good());
+
     spSt.clear();
 
     assert(spSt);
     assert(!spSt.bad());
     assert(!spSt.fail());
     assert(spSt.good());
-    assert(spSt.span().size() == 0);
-  }
-  {
-    std::span<CharT> sp{arr};
-    SpStream spSt(sp);
-
-    assert(spSt);
-    assert(!spSt.bad());
-    assert(!spSt.fail());
-    assert(spSt.good());
-    assert(spSt.span().size() == 0);
-
-    spSt << 10;
-
-    assert(spSt.span().size() == 2);
-
-    spSt.clear();
-
-    // assert(spSt);
-    // assert(!spSt.bad());
-    // assert(!spSt.fail());
-    // assert(spSt.good());
-    // assert(spSt.span().size() == 0);
-
-    spSt << SV("gh");
-    assert(spSt.span().size() == 4);
-    std::println(stderr, "{}", spSt.span()[0]);
-    std::println(stderr, "{}", arr[0]);
-    assert(spSt.span()[0] == '1');
-    assert(arr[0] == '1');
-    assert(spSt.span()[1] == '0');
-    assert(arr[1] == '0');
-    assert(spSt.span()[2] == 'g');
-    assert(arr[2] == 'g');
-
-    CharT output_buffer[30];
-    std::basic_ospanstream<CharT, TraitsT> os{std::span<CharT>{output_buffer}};
-
-    assert(os.good());
-    assert(!os.fail());
-    assert(!os.bad());
-    os << 10 << 20 << 30;
-    os << SV("GH");
-    assert(os.good());
-    assert(!os.fail());
-    assert(!os.bad());
-    std::println(stderr, "{}", os.span()[0]);
-    std::println(stderr, "{}", output_buffer[0]);
-    std::println(stderr, "{}", os.span().size());
-    std::println(stderr, "{}", os.span()[6]);
-    std::println(stderr, "{}", output_buffer[7]);
-    assert(false);
   }
 }
 
 int main(int, char**) {
   test<char>();
 #ifndef TEST_HAS_NO_WIDE_CHARACTERS
-  // test<wchar_t>();
+  test<wchar_t>();
 #endif
 
   return 0;
