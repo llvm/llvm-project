931115f140912b05850f1e5bfaa3a35b1b0ebf98
hulxv <hulxxv@gmail.com>
2025-03-22 16:41:47 +0200

    refactor: use `cpp::countr_zero` instead of `__builtin_ctzg`

--- libc/src/stdlib/memalignment.cpp
+++ libc/src/stdlib/memalignment.cpp
@@ -1,15 +1,15 @@
 #include "src/stdlib/memalignment.h"
 #include "src/__support/macros/config.h"
-
+#include "src/__support/CPP/bit.h"
 namespace LIBC_NAMESPACE_DECL {
 
 LLVM_LIBC_FUNCTION(size_t, memalignment, (const void *p)) {
-  if (p == NULL)
+  if (p == nullptr)
     return 0;
 
   uintptr_t addr = reinterpret_cast<uintptr_t>(p);
 
-  return 1 << __builtin_ctzg(addr);
+  return 1 << cpp::countr_zero(addr);
 }
 
 } // namespace LIBC_NAMESPACE_DECL
