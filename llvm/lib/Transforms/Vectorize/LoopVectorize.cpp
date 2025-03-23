2be6684ad84b1cd77f706d333e805e47ab4b6972
Kazu Hirata <kazu@google.com>
2025-03-23 08:03:54 -0700

    [Vectorize] Avoid repeated hash lookups (NFC)

--- llvm/lib/Transforms/Vectorize/LoopVectorize.cpp
+++ llvm/lib/Transforms/Vectorize/LoopVectorize.cpp
@@ -5815,12 +5815,13 @@ LoopVectorizationCostModel::getReductionPatternCost(Instruction *I,
 
   // Test if the found instruction is a reduction, and if not return an invalid
   // cost specifying the parent to use the original cost modelling.
-  if (!InLoopReductionImmediateChains.count(RetI))
+  auto RetIIt = InLoopReductionImmediateChains.find(RetI);
+  if (RetIIt == InLoopReductionImmediateChains.end())
     return std::nullopt;
 
   // Find the reduction this chain is a part of and calculate the basic cost of
   // the reduction on its own.
-  Instruction *LastChain = InLoopReductionImmediateChains.at(RetI);
+  Instruction *LastChain = RetIIt->second;
   Instruction *ReductionPhi = LastChain;
   while (!isa<PHINode>(ReductionPhi))
     ReductionPhi = InLoopReductionImmediateChains.at(ReductionPhi);
