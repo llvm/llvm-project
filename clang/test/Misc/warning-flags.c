c1f916c5cb19c1a816737dbb5cf21e5ac1636de3
Shilei Tian <i@tianshilei.me>
2025-03-24 15:11:55 -0400

    [AMDGPU] Remove outdated COV6 warning

--- clang/test/Misc/warning-flags.c
+++ clang/test/Misc/warning-flags.c
@@ -40,7 +40,6 @@ CHECK-NEXT:   warn_collection_expr_type
 CHECK-NEXT:   warn_conflicting_variadic
 CHECK-NEXT:   warn_delete_array_type
 CHECK-NEXT:   warn_double_const_requires_fp64
-CHECK-NEXT:   warn_drv_amdgpu_cov6
 CHECK-NEXT:   warn_drv_assuming_mfloat_abi_is
 CHECK-NEXT:   warn_drv_clang_unsupported
 CHECK-NEXT:   warn_drv_pch_not_first_include
