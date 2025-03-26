e8c38bdb661bc18818c42fdd2fc53a83c1cdd47e
Joseph Huber <huberjn@outlook.com>
2025-03-26 12:20:19 -0500

    Update libc/src/stdlib/memalignment.cpp

--- libc/src/stdlib/memalignment.cpp
+++ libc/src/stdlib/memalignment.cpp
@@ -9,7 +9,7 @@ LLVM_LIBC_FUNCTION(size_t, memalignment, (const void *p)) {
 
   uintptr_t addr = reinterpret_cast<uintptr_t>(p);
 
-  return 1 << cpp::countr_zero(addr);
+  return size_t(1) << cpp::countr_zero(addr);
 }
 
 } // namespace LIBC_NAMESPACE_DECL
