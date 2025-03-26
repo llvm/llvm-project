c35e38e5f78e1bac7d0ee802da1d15233da530e0
Vy Nguyen <vyng@google.com>
2025-03-26 10:06:53 -0400

    disable client telemetry for SWIG

--- lldb/source/API/SBDebugger.cpp
+++ lldb/source/API/SBDebugger.cpp
@@ -967,6 +967,10 @@ SBTarget SBDebugger::GetDummyTarget() {
 
 void SBDebugger::DispatchClientTelemetry(const lldb::SBStructuredData &entry) {
   LLDB_INSTRUMENT_VA(this);
+  // Disable client-telemetry for SWIG.
+  // This prevent arbitrary python client (pretty printers, whatnot) from sending
+  // telemetry without vendors knowing.
+#ifndef SWIG
   if (m_opaque_sp) {
     m_opaque_sp->DispatchClientTelemetry(*entry.m_impl_up);
   } else {
@@ -974,6 +978,7 @@ void SBDebugger::DispatchClientTelemetry(const lldb::SBStructuredData &entry) {
     LLDB_LOGF(log,
               "Could not send telemetry from SBDebugger - debugger was null.");
   }
+#endif
 }
 
 bool SBDebugger::DeleteTarget(lldb::SBTarget &target) {
