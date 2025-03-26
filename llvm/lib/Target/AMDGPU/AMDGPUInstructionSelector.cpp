d907b1794cbea994bc739a56443e23b4f51b265f
Juan Manuel Martinez Caamaño <juamarti@amd.com>
2025-03-26 16:50:08 +0100

    [AMDGPU] Use a target feature to enable __builtin_amdgcn_global_load_lds on gfx9 and gfx10

--- llvm/lib/Target/AMDGPU/AMDGPUInstructionSelector.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPUInstructionSelector.cpp
@@ -3368,7 +3368,7 @@ bool AMDGPUInstructionSelector::selectG_INSERT_VECTOR_ELT(
 }
 
 bool AMDGPUInstructionSelector::selectBufferLoadLds(MachineInstr &MI) const {
-  if (!Subtarget->hasLDSBufferLoad())
+  if (!Subtarget->hasMemToLDSLoad())
     return false;
   unsigned Opc;
   unsigned Size = MI.getOperand(3).getImm();
@@ -3505,6 +3505,9 @@ static Register matchZeroExtendFromS32(MachineRegisterInfo &MRI, Register Reg) {
 }
 
 bool AMDGPUInstructionSelector::selectGlobalLoadLds(MachineInstr &MI) const{
+  if (!Subtarget->hasMemToLDSLoad())
+    return false;
+
   unsigned Opc;
   unsigned Size = MI.getOperand(3).getImm();
 
