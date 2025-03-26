25c5bad2f26e96a44b84765bedd82ee9bcffd22c
Min-Yih Hsu <min.hsu@sifive.com>
2025-03-26 12:07:02 -0700

    [RISCV] Check the legality of source vector types in matchSplatAsGather (#133028)
    When we're trying to lower `extractelement + splat` with
    vrgather.vi/.vx, we should also check the legality of source vector type
    from `extractelement`, as the entire transformation assumes legal types.
    
    Fixes #133020

--- llvm/lib/Target/RISCV/RISCVISelLowering.cpp
+++ llvm/lib/Target/RISCV/RISCVISelLowering.cpp
@@ -3566,7 +3566,8 @@ static SDValue matchSplatAsGather(SDValue SplatVal, MVT VT, const SDLoc &DL,
   // FIXME: Support i1 vectors, maybe by promoting to i8?
   MVT EltTy = VT.getVectorElementType();
   MVT SrcVT = Src.getSimpleValueType();
-  if (EltTy == MVT::i1 || EltTy != SrcVT.getVectorElementType())
+  if (EltTy == MVT::i1 || EltTy != SrcVT.getVectorElementType() ||
+      !DAG.getTargetLoweringInfo().isTypeLegal(SrcVT))
     return SDValue();
   SDValue Idx = SplatVal.getOperand(1);
   // The index must be a legal type.
