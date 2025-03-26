0fa223c35f6f2bfcb1bc69f8bf2e7e17e2146efe
Shashwathi N <nshashwa@pe31.hpc.amslabs.hpecorp.net>
2025-03-26 13:40:40 -0500

    Added testcase in mcmodel.c file

--- clang/test/Driver/diag-mcmodel-tiny-x86.c
+++
@@ -1,2 +0,0 @@
-// RUN: not %clang --target=x86_64 -c -mcmodel=tiny %s 2>&1 | FileCheck %s
-// CHECK: error: unsupported argument 'tiny' to option '-mcmodel=' for target '{{.*}}'
--- clang/test/Driver/mcmodel.c
+++ clang/test/Driver/mcmodel.c
@@ -4,6 +4,7 @@
 // RUN: %clang --target=x86_64 -### -S -mcmodel=kernel %s 2>&1 | FileCheck --check-prefix=KERNEL %s
 // RUN: %clang --target=x86_64 -### -c -mcmodel=medium %s 2>&1 | FileCheck --check-prefix=MEDIUM %s
 // RUN: %clang --target=x86_64 -### -S -mcmodel=large %s 2>&1 | FileCheck --check-prefix=LARGE %s
+// RUN: not %clang --target=x86_64 -c -mcmodel=tiny %s 2>&1 | FileCheck %s
 // RUN: not %clang -### -c --target=powerpc-linux-gnu -mcmodel=medium %s 2>&1 | FileCheck --check-prefix=ERR-MEDIUM %s
 // RUN: %clang --target=powerpc-unknown-aix -### -S -mcmodel=small %s 2>&1 | FileCheck --check-prefix=SMALL %s
 // RUN: %clang --target=powerpc-unknown-aix -### -S -mcmodel=large %s 2>&1 | FileCheck --check-prefix=LARGE %s
@@ -44,3 +45,5 @@
 // ERR-AARCH64_32: error: unsupported argument 'small' to option '-mcmodel=' for target 'aarch64_32-unknown-linux'
 
 // ERR-LOONGARCH64-PLT-EXTREME: error: invalid argument '-mcmodel=extreme' not allowed with '-fplt'
+
+// CHECK: error: unsupported argument 'tiny' to option '-mcmodel=' for target '{{.*}}'
