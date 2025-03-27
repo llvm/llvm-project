46d9f096ceee7021696c6fa688d67e57d30ca356
hulxv <hulxxv@gmail.com>
2025-03-28 01:08:14 +0200

    [libc] add header to `memalignment.cpp`

--- libc/src/stdlib/memalignment.cpp
+++ libc/src/stdlib/memalignment.cpp
@@ -1,6 +1,16 @@
+//===-- Implementation for memalignment -------------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
 #include "src/stdlib/memalignment.h"
-#include "src/__support/macros/config.h"
 #include "src/__support/CPP/bit.h"
+#include "src/__support/common.h"
+#include "src/__support/macros/config.h"
+
 namespace LIBC_NAMESPACE_DECL {
 
 LLVM_LIBC_FUNCTION(size_t, memalignment, (const void *p)) {
