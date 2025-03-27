250579535b0678b757fbd37d6dbe38caebbcf10f
Mikhail R. Gadelha <mikhail@igalia.com>
2025-03-27 15:40:50 -0300

    Update header
    Signed-off-by: Mikhail R. Gadelha <mikhail@igalia.com>

--- llvm/lib/Target/RISCV/RISCVLateOpt.cpp
+++ llvm/lib/Target/RISCV/RISCVLateOpt.cpp
@@ -1,4 +1,4 @@
-//===-- RISCVLateBranchOpt.cpp - Late stage optimization ------------------------===//
+//===-- RISCVLateBranchOpt.cpp - Late Stage Branch Optimization -----------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
