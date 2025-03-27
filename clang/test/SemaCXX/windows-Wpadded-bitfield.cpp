6c79da3341ee0cc6892d575568a29450cab8847d
Théo De Magalhaes <theo.de-magalhaes@epita.fr>
2025-03-27 18:28:06 +0100

    test(clang-cl): more bitfield padding tests

--- clang/test/SemaCXX/windows-Wpadded-bitfield.cpp
+++ clang/test/SemaCXX/windows-Wpadded-bitfield.cpp
@@ -6,6 +6,27 @@ struct __attribute__((ms_struct)) BitfieldStruct { // expected-warning {{padding
   char i;
 };
 
+struct __attribute__((ms_struct)) SevenBitfieldStruct { // expected-warning {{padding size of 'SevenBitfieldStruct' with 3 bytes to alignment boundary}}
+  char c : 7;
+  int : 0; // expected-warning {{padding struct 'SevenBitfieldStruct' with 25 bits to align anonymous bit-field}}
+  char i;
+};
+
+struct __attribute__((ms_struct)) SameUnitSizeBitfield {
+  char c : 7;
+  char : 1; // Same unit size attributes fall in the same unit + they fill the unit -> no padding
+  char i;
+};
+
+struct __attribute__((ms_struct)) DifferentUnitSizeBitfield { // expected-warning {{padding size of 'DifferentUnitSizeBitfield' with 3 bytes to alignment boundary}}
+  char c : 7;
+  int : 1; // expected-warning {{padding struct 'DifferentUnitSizeBitfield' with 25 bits to align anonymous bit-field}}
+  char i; // expected-warning {{padding struct 'DifferentUnitSizeBitfield' with 31 bits to align 'i'}}
+};
+
 int main() {
   BitfieldStruct b;
+  SevenBitfieldStruct s;
+  SameUnitSizeBitfield su;
+  DifferentUnitSizeBitfield du;
 }
