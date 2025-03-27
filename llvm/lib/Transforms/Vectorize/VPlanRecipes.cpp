f15d348dfd94bdfcf49fff9dc14e8fded2fc3308
Elvis Wang <elvis.wang@sifive.com>
2025-03-26 18:34:25 -0700

    [VPlan] Introduces explicit broadcast for live-in constants.
    This patch focus on explicit show the broadcast for the live-in
    constants. This can help the VPlan-based cost model the broadcast cost
    and track the register pressure of the broadcast value in the future.
    
    Live-in constants usually only has single user so
    insert the `broadcast` before the user to reduce the live range of the
    broadcast value and prevent generated vector IR changes.

--- llvm/lib/Transforms/Vectorize/VPlanRecipes.cpp
+++ llvm/lib/Transforms/Vectorize/VPlanRecipes.cpp
@@ -938,6 +938,7 @@ bool VPInstruction::onlyFirstLaneUsed(const VPValue *Op) const {
   default:
     return false;
   case Instruction::ExtractElement:
+  case Instruction::ExtractValue:
     return Op == getOperand(1);
   case Instruction::PHI:
     return true;
@@ -959,8 +960,9 @@ bool VPInstruction::onlyFirstLaneUsed(const VPValue *Op) const {
   case VPInstruction::PtrAdd:
     return Op == getOperand(0) || vputils::onlyFirstLaneUsed(this);
   case VPInstruction::ComputeAnyOfResult:
-  case VPInstruction::ComputeFindLastIVResult:
     return Op == getOperand(1);
+  case VPInstruction::ComputeFindLastIVResult:
+    return Op == getOperand(1) || Op == getOperand(2);
   };
   llvm_unreachable("switch should return");
 }
