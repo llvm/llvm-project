3392ff361a2005e79ae42f5199625255df0a3690
Florian Mayer <fmayer@google.com>
2025-03-28 16:50:17 -0700

    better errors
    Created using spr 1.3.4

--- compiler-rt/test/sanitizer_common/TestCases/Linux/getservent_r.cpp
+++ compiler-rt/test/sanitizer_common/TestCases/Linux/getservent_r.cpp
@@ -4,26 +4,37 @@
 
 #include <arpa/inet.h>
 #include <assert.h>
+#include <errno.h>
 #include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void CheckResult(int ret) {
+  if (ret != 0) {
+    fprintf(stderr, "%s\n", strerror(errno));
+    abort();
+  }
+}
 
 int main(void) {
   struct servent result_buf;
   struct servent *result;
   char buf[1024];
-  assert(getservent_r(&result_buf, buf, sizeof(buf), &result) == 0);
+  CheckResult(getservent_r(&result_buf, buf, sizeof(buf), &result));
   assert(result != nullptr);
 
   // If these fail, check /etc/services if "ssh" exists. I picked this because
   // it should exist everywhere, if it doesn't, I am sorry. Disable the test
   // then please.
-  assert(getservbyname_r("ssh", nullptr, &result_buf, buf, sizeof(buf),
-                         &result) == 0);
+  CheckResult(
+      getservbyname_r("ssh", nullptr, &result_buf, buf, sizeof(buf), &result));
   assert(result != nullptr);
-  assert(getservbyport_r(htons(22), nullptr, &result_buf, buf, sizeof(buf),
-                         &result) == 0);
+  CheckResult(getservbyport_r(htons(22), nullptr, &result_buf, buf, sizeof(buf),
+                              &result));
   assert(result != nullptr);
 
-  assert(getservbyname_r("invalidhadfuiasdhi", nullptr, &result_buf, buf,
-                         sizeof(buf), &result) == 0);
+  CheckResult(getservbyname_r("invalidhadfuiasdhi", nullptr, &result_buf, buf,
+                              sizeof(buf), &result));
   assert(result == nullptr);
 }
