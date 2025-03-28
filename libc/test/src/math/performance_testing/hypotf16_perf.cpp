22dc48fce3fb732c97a3fcdff2ebea3166bf0719
meltq <alissxlace@proton.me>
2025-03-28 09:56:53 +0530

    formatting changes

--- libc/test/src/math/performance_testing/hypotf16_perf.cpp
+++ libc/test/src/math/performance_testing/hypotf16_perf.cpp
@@ -8,7 +8,9 @@
 
 #include "BinaryOpSingleOutputPerf.h"
 
-#include "src/math/hypotf16.h"
 #include "src/__support/FPUtil/Hypot.h"
+#include "src/math/hypotf16.h"
 
-BINARY_OP_SINGLE_OUTPUT_PERF(float16, float16, LIBC_NAMESPACE::hypotf16, LIBC_NAMESPACE::fputil::hypot<float16>, "hypotf16_perf.log");
+BINARY_OP_SINGLE_OUTPUT_PERF(float16, float16, LIBC_NAMESPACE::hypotf16,
+                             LIBC_NAMESPACE::fputil::hypot<float16>,
+                             "hypotf16_perf.log");
