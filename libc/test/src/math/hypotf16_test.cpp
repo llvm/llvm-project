24c5d78233cd191387a7cb3008ea5f7fa4271273
meltq <alissxlace@proton.me>
2025-03-28 14:41:41 +0530

    Removed hard to round cases

--- libc/test/src/math/hypotf16_test.cpp
+++ libc/test/src/math/hypotf16_test.cpp
@@ -7,7 +7,6 @@
 //===----------------------------------------------------------------------===//
 
 #include "HypotTest.h"
-#include "hypotf16_hard_to_round.h"
 
 #include "src/math/hypotf16.h"
 
@@ -19,9 +18,4 @@ TEST_F(LlvmLibcHypotf16Test, SubnormalRange) {
 
 TEST_F(LlvmLibcHypotf16Test, NormalRange) {
   test_normal_range(&LIBC_NAMESPACE::hypotf16);
-}
-
-TEST_F(LlvmLibcHypotf16Test, TrickyInputs) {
-  test_input_list(&LIBC_NAMESPACE::hypotf16, N_HARD_TO_ROUND,
-                  HYPOTF16_HARD_TO_ROUND);
 }
