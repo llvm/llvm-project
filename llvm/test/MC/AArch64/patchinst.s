9f59807421f67ca153dc1ca41f7fb945845f7eec
Peter Collingbourne <peter@pcc.me.uk>
2025-03-28 14:16:17 -0700

    MC: Introduce R_AARCH64_PATCHINST relocation type.
    The R_AARCH64_PATCHINST relocation type is to support deactivation
    symbols. For more information, see the RFC:
    https://discourse.llvm.org/t/rfc-deactivation-symbols/85556
    
    Part of the AArch64 psABI extension:
    https://github.com/ARM-software/abi-aa/issues/340

---
+++ llvm/test/MC/AArch64/patchinst.s
@@ -0,0 +1,7 @@
+// RUN: llvm-mc -triple aarch64-elf -filetype=obj %s -o - | llvm-objdump -r - | FileCheck %s
+
+// Test that PATCHINST appears after JUMP26.
+// CHECK:      R_AARCH64_JUMP26
+// CHECK-NEXT: R_AARCH64_PATCHINST
+.reloc ., R_AARCH64_PATCHINST, ds
+b f1
