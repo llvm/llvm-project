a7f41b01f711f9360f03223f01ee5a5a17e8aeac
Harrison Hao <tsworld1314@gmail.com>
2025-03-29 06:10:45 +0000

    [libc][math][c23] Update for comments.

--- libc/src/math/generic/atanhf16.cpp
+++ libc/src/math/generic/atanhf16.cpp
@@ -1,4 +1,4 @@
-//===-- Implementation of atanh(x) function -------------------------------===//
+//===-- Half-precision atanh(x) function ----------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -10,6 +10,7 @@
 #include "explogxf.h"
 #include "hdr/errno_macros.h"
 #include "hdr/fenv_macros.h"
+#include "src/__support/FPUtil/FEnvImpl.h"
 #include "src/__support/FPUtil/FPBits.h"
 #include "src/__support/FPUtil/PolyEval.h"
 #include "src/__support/FPUtil/cast.h"
@@ -21,26 +22,27 @@
 namespace LIBC_NAMESPACE_DECL {
 
 LLVM_LIBC_FUNCTION(float16, atanhf16, (float16 x)) {
-  using FPBits = typename fputil::FPBits<float16>;
+  using FPBits = fputil::FPBits<float16>;
 
   FPBits xbits(x);
   Sign sign = xbits.sign();
   uint16_t x_abs = xbits.abs().uintval();
 
+  // |x| >= 1
   if (LIBC_UNLIKELY(x_abs >= 0x3c00U)) {
-    if (xbits.is_nan()) {
+    if (xbits.is_nan())
       return x;
-    }
+
     // |x| == 1.0
     if (x_abs == 0x3c00U) {
       fputil::set_errno_if_required(ERANGE);
       fputil::raise_except_if_required(FE_DIVBYZERO);
       return FPBits::inf(sign).get_val();
-    } else {
-      fputil::set_errno_if_required(EDOM);
-      fputil::raise_except_if_required(FE_INVALID);
-      return FPBits::quiet_nan().get_val();
     }
+    // |x| > 1.0
+    fputil::set_errno_if_required(EDOM);
+    fputil::raise_except_if_required(FE_INVALID);
+    return FPBits::quiet_nan().get_val();
   }
 
   // For |x| less than approximately 0.10
@@ -52,35 +54,25 @@ LLVM_LIBC_FUNCTION(float16, atanhf16, (float16 x)) {
     //    atanh(x) ≈ x + (1/3)*x^3
     if (LIBC_UNLIKELY(x_abs < 0x0100U)) {
       return static_cast<float16>(
-          LIBC_UNLIKELY(x_abs == 0) ? x : (x + 0x1.555556p-2 * x * x * x));
+          LIBC_UNLIKELY(x_abs == 0) ? x : (x + 0x1.555556p-2f * x * x * x));
     }
 
     // For 0x0100U <= |x| <= 0x2e66U:
     //   Let t = x^2.
     //   Define P(t) ≈ (1/3)*t + (1/5)*t^2 + (1/7)*t^3 + (1/9)*t^4 + (1/11)*t^5.
-    // The coefficients below were derived using Sollya:
-    //   > display = hexadecimal;
-    //   > round(1/3, SG, RN);
-    //   > round(1/5, SG, RN);
-    //   > round(1/7, SG, RN);
-    //   > round(1/9, SG, RN);
-    //   > round(1/11, SG, RN);
-    // This yields:
-    //   0x1.555556p-2
-    //   0x1.99999ap-3
-    //   0x1.24924ap-3
-    //   0x1.c71c72p-4
-    //   0x1.745d18p-4f
+    // Coefficients (from Sollya, RN, hexadecimal):
+    //  1/3 = 0x1.555556p-2, 1/5 = 0x1.99999ap-3, 1/7 = 0x1.24924ap-3,
+    //  1/9 = 0x1.c71c72p-4, 1/11 = 0x1.745d18p-4
     // Thus, atanh(x) ≈ x * (1 + P(x^2)).
     float xf = x;
     float x2 = xf * xf;
     float pe = fputil::polyeval(x2, 0.0f, 0x1.555556p-2f, 0x1.99999ap-3f,
                                 0x1.24924ap-3f, 0x1.c71c72p-4f, 0x1.745d18p-4f);
-    return static_cast<float16>(fputil::multiply_add(xf, pe, xf));
+    return fputil::cast<float16>(fputil::multiply_add(xf, pe, xf));
   }
 
   float xf = x;
-  return static_cast<float16>(0.5 * log_eval((xf + 1.0) / (xf - 1.0)));
+  return fputil::cast<float16>(0.5 * log_eval((xf + 1.0) / (xf - 1.0)));
 }
 
 } // namespace LIBC_NAMESPACE_DECL
