a6aca867fd4a353c2a8797f9981177e64f19e7d2
Harrison Hao <tsworld1314@gmail.com>
2025-03-29 06:10:45 +0000

    [libc][math][c23] Update for comments.

--- libc/test/src/math/atanhf16_test.cpp
+++ libc/test/src/math/atanhf16_test.cpp
@@ -7,7 +7,6 @@
 //===----------------------------------------------------------------------===//
 
 #include "src/__support/FPUtil/FPBits.h"
-#include "src/errno/libc_errno.h"
 #include "src/math/atanhf16.h"
 #include "test/UnitTest/FPMatcher.h"
 #include "test/UnitTest/Test.h"
@@ -17,13 +16,13 @@
 using LlvmLibcAtanhf16Test = LIBC_NAMESPACE::testing::FPTest<float16>;
 namespace mpfr = LIBC_NAMESPACE::testing::mpfr;
 
-// Range for positive numbers: [0, 1)
+// Range for positive numbers: [0, +Inf]
 static constexpr uint16_t POS_START = 0x0000U;
-static constexpr uint16_t POS_STOP = 0x3C00;
+static constexpr uint16_t POS_STOP = 0x7C00U;
 
-// Range for negative numbers: (-1, 0]
-static constexpr uint16_t NEG_START = 0xBBFFU;
-static constexpr uint16_t NEG_STOP = 0x8000U;
+// Range for negative numbers: [-Inf, 0]
+static constexpr uint16_t NEG_START = 0x8000U;
+static constexpr uint16_t NEG_STOP = 0xFC00U;
 
 TEST_F(LlvmLibcAtanhf16Test, PositiveRange) {
   for (uint16_t v = POS_START; v < POS_STOP; ++v) {
@@ -34,7 +33,7 @@ TEST_F(LlvmLibcAtanhf16Test, PositiveRange) {
 }
 
 TEST_F(LlvmLibcAtanhf16Test, NegativeRange) {
-  for (uint16_t v = NEG_START; v >= NEG_STOP; --v) {
+  for (uint16_t v = NEG_START; v <= NEG_STOP; ++v) {
     float16 x = FPBits(v).get_val();
     EXPECT_MPFR_MATCH_ALL_ROUNDING(mpfr::Operation::Atanh, x,
                                    LIBC_NAMESPACE::atanhf16(x), 0.5);
