35de81c4dd9c64158add7e9441e738b4459ba809
Florian Hahn <flo@fhahn.com>
2025-03-30 22:20:34 +0100

    [VPlan] Handle scalar casts and blend in isUniformAfterVectorization.
    Currently should be NFC, but will be used by
    https://github.com/llvm/llvm-project/pull/117506.

--- llvm/lib/Transforms/Vectorize/VPlanUtils.h
+++ llvm/lib/Transforms/Vectorize/VPlanUtils.h
@@ -45,7 +45,8 @@ inline bool isUniformAfterVectorization(const VPValue *VPV) {
     return true;
   if (auto *Rep = dyn_cast<VPReplicateRecipe>(VPV))
     return Rep->isUniform();
-  if (isa<VPWidenGEPRecipe, VPDerivedIVRecipe>(VPV))
+  if (isa<VPWidenGEPRecipe, VPDerivedIVRecipe, VPScalarCastRecipe,
+          VPBlendRecipe>(VPV))
     return all_of(VPV->getDefiningRecipe()->operands(),
                   isUniformAfterVectorization);
   if (auto *VPI = dyn_cast<VPInstruction>(VPV))
