00bcdcc0f0d7a0c196df69ec16e073b4863d21be
Adrian Prantl <aprantl@apple.com>
2025-03-31 15:52:58 -0700

    [lldb] Convert inline test to regular test (NFC)
    (cherry picked from commit bb00f779a1117b52dde22243461ee05dc82d5611)

--- lldb/test/API/lang/swift/enum_objc/main.swift
+++ lldb/test/API/lang/swift/enum_objc/main.swift
@@ -1,17 +1,10 @@
 import Enum 
 
-func test()
-{
-    let ascending = getReturn(-1)
-    let descending = getReturn(1)
-    let same = getReturn(0)
-    return //%self.expect('frame var -d run-target -- ascending', substrs=['OrderedAscending'])
-           //%self.expect('frame var -d run-target -- descending', substrs=['OrderedDescending'])
-           //%self.expect('frame var -d run-target -- same', substrs=['OrderedSame'])
-           //%self.expect('expr -d run-target -- ascending', substrs=['OrderedAscending'])
-           //%self.expect('expr -d run-target -- descending', substrs=['OrderedDescending'])
-           //%self.expect('expr -d run-target -- same', substrs=['OrderedSame'])
+func test() {
+  let ascending = getReturn(-1)
+  let descending = getReturn(1)
+  let same = getReturn(0)
+  print("break here")
 }
 
-_ = test()
-print("this is needed to load the stdlib")
+test()
