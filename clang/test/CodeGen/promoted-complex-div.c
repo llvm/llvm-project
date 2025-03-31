15396dc494ee1ea3c18b1221a3aaaf1532d7010d
Zahira Ammarguellat <zahira.ammarguellat@intel.com>
2025-03-31 08:06:43 -0700

    Addressed Josh's review comments.

--- clang/test/CodeGen/promoted-complex-div.c
+++ clang/test/CodeGen/promoted-complex-div.c
@@ -2,13 +2,13 @@
 // RUN -verify -complex-range=promoted -o - | FileCheck %s
 
 // RUN %clang_cc1 %s -O0 -emit-llvm -triple x86_64-unknown-unknown \
-// RUN -verify=nopromotion -complex-range=promoted -target-feature -x87 -o - | FileCheck %s --check-prefix=X87
+// RUN -verify=nopromotion -complex-range=promoted -target-feature -x87 -o - | FileCheck %s --check-prefix=NOX87
 
 // RUN: %clang_cc1 %s -O0 -emit-llvm -triple x86_64-unknown-windows \
-// RUN: -verify=nopromotion -complex-range=promoted -o - | FileCheck %s --check-prefix=X87
+// RUN: -verify=nopromotion -complex-range=promoted -o - | FileCheck %s --check-prefix=NOX87
 
 // RUN: %clang_cc1 %s -O0 -emit-llvm -triple x86_64-unknown-windows \
-// RUN: -verify=nopromotion -complex-range=promoted -target-feature -x87 -o - | FileCheck %s --check-prefix=X87
+// RUN: -verify=nopromotion -complex-range=promoted -target-feature -x87 -o - | FileCheck %s --check-prefix=NOX87
 
 
 
@@ -23,12 +23,12 @@ _Complex float divd(_Complex float a, _Complex float b) {
   // CHECK: fptrunc double {{.*}} to float
   // CHECK: fptrunc double {{.*}} to float
 
-  // X87: fpext float {{.*}} to double
-  // X87: fpext float {{.*}} to double
-  // X87: fdiv double
-  // X87: fdiv double
-  // X87: fptrunc double {{.*}} to float
-  // X87: fptrunc double {{.*}} to float
+  // NOX87: fpext float {{.*}} to double
+  // NOX87: fpext float {{.*}} to double
+  // NOX87: fdiv double
+  // NOX87: fdiv double
+  // NOX87: fptrunc double {{.*}} to float
+  // NOX87: fptrunc double {{.*}} to float
 
   return a / b;
 }
@@ -42,8 +42,8 @@ _Complex double divf(_Complex double a, _Complex double b) {
   // CHECK: fptrunc x86_fp80
   // CHECK: fptrunc x86_fp80
 
-  // X87: fdiv double
-  // X87: fdiv double
+  // NOX87: fdiv double
+  // NOX87: fdiv double
 
   return a / b; // nopromotion-warning{{excess precision is requested but the target does not support excess precision which may result in observable differences in complex division behavior}}
 }
