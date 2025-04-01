fb714455f4123a40342c472b16dc940d55cd9bdb
Orlando Cazalet-Hyams <orlando.hyams@sony.com>
2025-04-01 15:40:58 +0100

    [KeyInstr][Clang] Agg init atom
    This patch is part of a stack that teaches Clang to generate Key Instructions
    metadata for C and C++.
    
    The Key Instructions project is introduced, including a "quick summary" section
    at the top which adds context for this PR, here:
    https://discourse.llvm.org/t/rfc-improving-is-stmt-placement-for-better-interactive-debugging/82668
    
    The feature is only functional in LLVM if LLVM is built with CMake flag
    LLVM_EXPERIMENTAL_KEY_INSTRUCTIONs. Eventually that flag will be removed.
    
    The Clang-side work is demoed here:
    https://github.com/llvm/llvm-project/pull/130943

--- clang/lib/CodeGen/CGDecl.cpp
+++ clang/lib/CodeGen/CGDecl.cpp
@@ -1270,6 +1270,8 @@ void CodeGenFunction::emitStoresForConstant(const VarDecl &D, Address Loc,
                            createUnnamedGlobalForMemcpyFrom(
                                CGM, D, Builder, constant, Loc.getAlignment()),
                            SizeVal, isVolatile);
+  addInstToCurrentSourceAtom(I, nullptr);
+
   if (IsAutoInit)
     I->addAnnotationMetadata("auto-init");
 }
