29d5428307acc1493f2bb2819b627688b4917c07
Orlando Cazalet-Hyams <orlando.hyams@sony.com>
2025-04-01 14:50:41 +0100

    [KeyInstr][Clang] Scalar init atom
    This patch is part of a stack that teaches Clang to generate Key Instructions
    metadata for C and C++.
    
    The Key Instructions project is introduced, including a "quick summary" section
    at the top which adds context for this PR, here:
    https://discourse.llvm.org/t/rfc-improving-is-stmt-placement-for-better-interactive-debugging/82668
    
    The feature is only functional in LLVM if LLVM is built with CMake flag
    LLVM_EXPERIMENTAL_KEY_INSTRUCTIONs. Eventually that flag will be removed.
    
    The Clang-side work is demoed here:
    https://github.com/llvm/llvm-project/pull/130943

--- clang/lib/CodeGen/CGExpr.cpp
+++ clang/lib/CodeGen/CGExpr.cpp
@@ -2180,6 +2180,8 @@ void CodeGenFunction::EmitStoreOfScalar(llvm::Value *Value, Address Addr,
   }
 
   llvm::StoreInst *Store = Builder.CreateStore(Value, Addr, Volatile);
+  addInstToCurrentSourceAtom(Store, Value);
+
   if (isNontemporal) {
     llvm::MDNode *Node =
         llvm::MDNode::get(Store->getContext(),
