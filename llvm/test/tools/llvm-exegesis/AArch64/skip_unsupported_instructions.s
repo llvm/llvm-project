783f86de978576646369a73d6c01576832659041
lakshayk-nv <lakshayk@nvidia.com>
2025-04-01 09:24:23 -0700

    [llvm-exegesis][AArch64] Updated the testcase for AUT instruction class.

--- llvm/test/tools/llvm-exegesis/AArch64/skip_unsupported_instructions.s
+++ llvm/test/tools/llvm-exegesis/AArch64/skip_unsupported_instructions.s
@@ -2,7 +2,7 @@
 
 # Check for skipping of illegal instruction errors (AUT and LDGM)
 # RUN: llvm-exegesis -mcpu=neoverse-v2 -mode=latency --opcode-name=AUTIA --benchmark-phase=assemble-measured-code 2>&1 | FileCheck %s --check-prefix=CHECK-AUTIA
-# CHECK-AUTIA: AUTIA: Unsupported opcode: isPointerAuth
+# CHECK-AUTIA-NOT: snippet crashed while running: Illegal instruction
 
 # RUN: llvm-exegesis -mcpu=neoverse-v2 -mode=latency --opcode-name=LDGM --benchmark-phase=assemble-measured-code 2>&1 | FileCheck %s --check-prefix=CHECK-LDGM
 # CHECK-LDGM: LDGM: Unsupported opcode: load tag multiple 
