e87dd398714821a81d8ee319d6b9f9bf7bf9f901
Juan Manuel Martinez Caamaño <juamarti@amd.com>
2025-04-02 17:25:18 +0200

    [Review] static constexpr and mask

--- llvm/lib/Target/AMDGPU/AMDGPUInstCombineIntrinsic.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPUInstCombineIntrinsic.cpp
@@ -745,11 +745,11 @@ GCNTTIImpl::instCombineIntrinsic(InstCombiner &IC, IntrinsicInst &II) const {
 
     // Tabulated 0.0625 * (sext (CArg & 0xf)).
     constexpr size_t ResValsSize = 16;
-    const float ResVals[ResValsSize] = {
+    static constexpr float ResVals[ResValsSize] = {
         0.0,  0.0625,  0.125,  0.1875,  0.25,  0.3125,  0.375,  0.4375,
         -0.5, -0.4375, -0.375, -0.3125, -0.25, -0.1875, -0.125, -0.0625};
     Constant *Res =
-        ConstantFP::get(Ty, ResVals[CArg->getZExtValue() % ResValsSize]);
+        ConstantFP::get(Ty, ResVals[CArg->getZExtValue() & (ResValsSize - 1)]);
     return IC.replaceInstUsesWith(II, Res);
   }
   case Intrinsic::amdgcn_ubfe:
