77793f4d8f599a7720c6bf42f5bab17d557dcb0e
Schrodinger ZHU Yifan <yifanzhu@rochester.edu>
2025-04-02 09:43:28 -0400

    comment

--- libc/src/stdlib/exit.cpp
+++ libc/src/stdlib/exit.cpp
@@ -24,7 +24,8 @@ extern "C" [[gnu::weak]] void __cxa_thread_finalize();
 
 // TODO: use recursive mutex to protect this routine.
 [[noreturn]] LLVM_LIBC_FUNCTION(void, exit, (int status)) {
-  __cxa_thread_finalize();
+  if (__cxa_thread_finalize)
+    __cxa_thread_finalize();
   __cxa_finalize(nullptr);
   internal::exit(status);
 }
