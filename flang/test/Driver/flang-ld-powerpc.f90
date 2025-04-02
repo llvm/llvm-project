a999a3e731a43e8c76c93af81247ae8c22470805
Daniel Chen <cdchen@ca.ibm.com>
2025-04-02 11:58:58 -0400

    To address the review comments fixing LIT test.

--- flang/test/Driver/flang-ld-powerpc.f90
+++ flang/test/Driver/flang-ld-powerpc.f90
@@ -12,15 +12,15 @@
 
 ! AIX64-LD-PER-TARGET-NOT: warning:
 ! AIX64-LD-PER-TARGET:     "-fc1" "-triple" "powerpc64-ibm-aix"
-! AIX64-LD-PER-TARGET:     "-resource-dir" "[[RESOURCE_DIR:[^"]+]]"
+! AIX64-LD-PER-TARGET-SAME:     "-resource-dir" "[[RESOURCE_DIR:[^"]+]]"
 ! AIX64-LD-PER-TARGET:     "{{.*}}ld{{(.exe)?}}"
 ! AIX64-LD-PER-TARGET-NOT: "-bnso"
-! AIX64-LD-PER-TARGET:     "-b64"
-! AIX64-LD-PER-TARGET:     "-bpT:0x100000000" "-bpD:0x110000000"
-! AIX64-LD-PER-TARGET:     "-lc"
-! AIX64-LD-PER-TARGET:     "[[RESOURCE_DIR]]{{/|\\\\}}lib{{/|\\\\}}powerpc64-ibm-aix{{/|\\\\}}libflang_rt.runtime.a"
-! AIX64-LD-PER-TARGET:     "-lm"
-! AIX64-LD-PER-TARGET:     "-lpthread"
+! AIX64-LD-PER-TARGET-SAME:     "-b64"
+! AIX64-LD-PER-TARGET-SAME:     "-bpT:0x100000000" "-bpD:0x110000000"
+! AIX64-LD-PER-TARGET-SAME:     "-lc"
+! AIX64-LD-PER-TARGET-SAME:     "[[RESOURCE_DIR]]{{/|\\\\}}lib{{/|\\\\}}powerpc64-ibm-aix{{/|\\\\}}libflang_rt.runtime.a"
+! AIX64-LD-PER-TARGET-SAME:     "-lm"
+! AIX64-LD-PER-TARGET-SAME:     "-lpthread"
 
 ! Check powerpc64le-unknown-linux-gnu 64-bit linking to static flang-rt
 ! RUN: %flang %s -### 2>&1 \
@@ -30,9 +30,9 @@
 
 ! LOP64-LD-PER-TARGET-NOT: warning:
 ! LOP64-LD-PER-TARGET:     "-fc1" "-triple" "powerpc64le-unknown-linux-gnu"
-! LOP64-LD-PER-TARGET:     "-resource-dir" "[[RESOURCE_DIR:[^"]+]]"
+! LOP64-LD-PER-TARGET-SAME:     "-resource-dir" "[[RESOURCE_DIR:[^"]+]]"
 ! LOP64-LD-PER-TARGET:     "{{.*}}ld{{(.exe)?}}"
 ! LOP64-LD-PER-TARGET-NOT: "-bnso"
-! LOP64-LD-PER-TARGET:     "[[RESOURCE_DIR]]{{/|\\\\}}lib{{/|\\\\}}powerpc64le-unknown-linux-gnu{{/|\\\\}}libflang_rt.runtime.a"
-! LOP64-LD-PER-TARGET:     "-lm"
-! LOP64-LD-PER-TARGET:     "-lc"
+! LOP64-LD-PER-TARGET-SAME:     "[[RESOURCE_DIR]]{{/|\\\\}}lib{{/|\\\\}}powerpc64le-unknown-linux-gnu{{/|\\\\}}libflang_rt.runtime.a"
+! LOP64-LD-PER-TARGET-SAME:     "-lm"
+! LOP64-LD-PER-TARGET-SAME:     "-lc"
