560bf4251017f72a2e861e6044c87e9a2666d041
Matt Arsenault <Matthew.Arsenault@amd.com>
2025-04-03 17:30:01 +0700

    IR: Remove uselist for constantdata
    This is a resurrected version of the patch attached to this RFC:
    
    https://discourse.llvm.org/t/rfc-constantdata-should-not-have-use-lists/42606
    
    In this adaptation, there are a few differences. In the original patch, the Use's
    use list was replaced with an unsigned* to the reference count in the value. This
    version leaves them as null and leaves the ref counting only in Value.
    
    Remove use-lists from instances of ConstantData (which are shared
    across modules and have no operands).
    
    To continue supporting most of the use-list API, store a ref-count in
    place of the use-list; this is for API like Value::use_empty and
    Value::hasNUses.  Operations that actually need the use-list -- like
    Value::use_begin -- will assert.
    
    This change has three benefits:
    
     1. The compiler output cannot in any way depend on the use-list order
        of instances of ConstantData.
    
     2. There's no use-list traffic when adding and removing simple
        constants from operand lists (although there is ref-count traffic;
        YMMV).
    
     3. It's cheaper to serialize use-lists (since we're no longer
        serializing the use-list order of things like i32 0).
    
    The downside is that you can't look at all the users of ConstantData,
    but traversals of users of i32 0 are already ill-advised.
    
    Possible follow-ups:
      - Track if an instance of a ConstantVector/ConstantArray/etc. is known
        to have all ConstantData arguments, and drop the use-lists to
        ref-counts in those cases.  Callers need to check Value::hasUseList
        before iterating through the use-list.
      - Remove even the ref-counts.  I'm not sure they have any benefit
        besides minimizing the scope of this commit, and maintaining the
        counts is not free.
    
    Fixes #58629

--- llvm/lib/Target/SPIRV/SPIRVEmitIntrinsics.cpp
+++ llvm/lib/Target/SPIRV/SPIRVEmitIntrinsics.cpp
@@ -1108,7 +1108,7 @@ void SPIRVEmitIntrinsics::deduceOperandElementType(
   IRBuilder<> B(Ctx);
   for (auto &OpIt : Ops) {
     Value *Op = OpIt.first;
-    if (Op->use_empty())
+    if (!Op->hasUseList() || Op->use_empty())
       continue;
     if (AskOps && !AskOps->contains(Op))
       continue;
@@ -1474,34 +1474,36 @@ void SPIRVEmitIntrinsics::replacePointerOperandWithPtrCast(
   // Do not emit new spv_ptrcast if equivalent one already exists or when
   // spv_assign_ptr_type already targets this pointer with the same element
   // type.
-  for (auto User : Pointer->users()) {
-    auto *II = dyn_cast<IntrinsicInst>(User);
-    if (!II ||
-        (II->getIntrinsicID() != Intrinsic::spv_assign_ptr_type &&
-         II->getIntrinsicID() != Intrinsic::spv_ptrcast) ||
-        II->getOperand(0) != Pointer)
-      continue;
+  if (Pointer->hasUseList()) {
+    for (auto User : Pointer->users()) {
+      auto *II = dyn_cast<IntrinsicInst>(User);
+      if (!II ||
+          (II->getIntrinsicID() != Intrinsic::spv_assign_ptr_type &&
+           II->getIntrinsicID() != Intrinsic::spv_ptrcast) ||
+          II->getOperand(0) != Pointer)
+        continue;
 
-    // There is some spv_ptrcast/spv_assign_ptr_type already targeting this
-    // pointer.
-    FirstPtrCastOrAssignPtrType = false;
-    if (II->getOperand(1) != VMD ||
-        dyn_cast<ConstantInt>(II->getOperand(2))->getSExtValue() !=
-            AddressSpace)
-      continue;
+      // There is some spv_ptrcast/spv_assign_ptr_type already targeting this
+      // pointer.
+      FirstPtrCastOrAssignPtrType = false;
+      if (II->getOperand(1) != VMD ||
+          dyn_cast<ConstantInt>(II->getOperand(2))->getSExtValue() !=
+              AddressSpace)
+        continue;
 
-    // The spv_ptrcast/spv_assign_ptr_type targeting this pointer is of the same
-    // element type and address space.
-    if (II->getIntrinsicID() != Intrinsic::spv_ptrcast)
+      // The spv_ptrcast/spv_assign_ptr_type targeting this pointer is of the
+      // same element type and address space.
+      if (II->getIntrinsicID() != Intrinsic::spv_ptrcast)
+        return;
+
+      // This must be a spv_ptrcast, do not emit new if this one has the same BB
+      // as I. Otherwise, search for other spv_ptrcast/spv_assign_ptr_type.
+      if (II->getParent() != I->getParent())
+        continue;
+
+      I->setOperand(OperandToReplace, II);
       return;
-
-    // This must be a spv_ptrcast, do not emit new if this one has the same BB
-    // as I. Otherwise, search for other spv_ptrcast/spv_assign_ptr_type.
-    if (II->getParent() != I->getParent())
-      continue;
-
-    I->setOperand(OperandToReplace, II);
-    return;
+    }
   }
 
   if (isa<Instruction>(Pointer) || isa<Argument>(Pointer)) {
@@ -1633,6 +1635,9 @@ void SPIRVEmitIntrinsics::insertPtrCastOrAssignTypeInstr(Instruction *I,
   for (unsigned OpIdx = 0; OpIdx < CI->arg_size(); OpIdx++) {
     Value *ArgOperand = CI->getArgOperand(OpIdx);
     if (!isPointerTy(ArgOperand->getType()))
+      continue;
+
+    if (!ArgOperand->hasUseList())
       continue;
 
     // Constants (nulls/undefs) are handled in insertAssignPtrTypeIntrs()
@@ -2490,10 +2495,13 @@ bool SPIRVEmitIntrinsics::postprocessTypes(Module &M) {
         }
       }
     }
-    for (User *U : Op->users()) {
-      Instruction *Inst = dyn_cast<Instruction>(U);
-      if (Inst && !isa<IntrinsicInst>(Inst))
-        ToProcess[Inst].insert(Op);
+
+    if (Op->hasUseList()) {
+      for (User *U : Op->users()) {
+        Instruction *Inst = dyn_cast<Instruction>(U);
+        if (Inst && !isa<IntrinsicInst>(Inst))
+          ToProcess[Inst].insert(Op);
+      }
     }
   }
   if (TodoTypeSz == 0)
