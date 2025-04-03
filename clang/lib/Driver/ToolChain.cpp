24597d0c27cb60772e098a3ae124eec67a4e0695
Simi Pallipurath <simi.pallipurath@arm.com>
2025-04-03 10:47:39 +0100

    fixup! Remove braces on single statement if else to align with LLVM coding standard.

--- clang/lib/Driver/ToolChain.cpp
+++ clang/lib/Driver/ToolChain.cpp
@@ -310,11 +310,10 @@ static void getARMMultilibFlags(const Driver &D,
     Result.push_back(BranchProtectionArg->getAsString(Args));
   }
 
-  if (FeatureSet.contains("+strict-align")) {
+  if (FeatureSet.contains("+strict-align"))
     Result.push_back("-mno-unaligned-access");
-  } else {
+  else
     Result.push_back("-munaligned-access");
-  }
 
   if (Arg *Endian = Args.getLastArg(options::OPT_mbig_endian,
                                     options::OPT_mlittle_endian)) {
