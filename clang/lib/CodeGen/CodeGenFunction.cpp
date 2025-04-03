2b11986f9cc30fe206375324eddd000fcb571a5d
Orlando Cazalet-Hyams <orlando.hyams@sony.com>
2025-04-03 18:05:07 +0100

    [KeyInstr][Clang] If stmt atom
    This patch is part of a stack that teaches Clang to generate Key Instructions
    metadata for C and C++.
    
    The Key Instructions project is introduced, including a "quick summary" section
    at the top which adds context for this PR, here:
    https://discourse.llvm.org/t/rfc-improving-is-stmt-placement-for-better-interactive-debugging/82668
    
    The feature is only functional in LLVM if LLVM is built with CMake flag
    LLVM_EXPERIMENTAL_KEY_INSTRUCTIONs. Eventually that flag will be removed.
    
    The Clang-side work is demoed here:
    https://github.com/llvm/llvm-project/pull/130943

--- clang/lib/CodeGen/CodeGenFunction.cpp
+++ clang/lib/CodeGen/CodeGenFunction.cpp
@@ -2118,6 +2118,8 @@ void CodeGenFunction::EmitBranchOnBoolExpr(
 
   llvm::Instruction *BrInst = Builder.CreateCondBr(CondV, TrueBlock, FalseBlock,
                                                    Weights, Unpredictable);
+  addInstToNewSourceAtom(BrInst, CondV);
+
   switch (HLSLControlFlowAttr) {
   case HLSLControlFlowHintAttr::Microsoft_branch:
   case HLSLControlFlowHintAttr::Microsoft_flatten: {
@@ -3337,4 +3339,12 @@ void CodeGenFunction::addInstToSpecificSourceAtom(
     llvm::Instruction *KeyInstruction, llvm::Value *Backup, uint64_t Atom) {
   if (CGDebugInfo *DI = getDebugInfo())
     DI->addInstToSpecificSourceAtom(KeyInstruction, Backup, Atom);
+}
+
+void CodeGenFunction::addInstToNewSourceAtom(llvm::Instruction *KeyInstruction,
+                                             llvm::Value *Backup) {
+  if (CGDebugInfo *DI = getDebugInfo()) {
+    ApplyAtomGroup Grp(getDebugInfo());
+    DI->addInstToCurrentSourceAtom(KeyInstruction, Backup);
+  }
 }
