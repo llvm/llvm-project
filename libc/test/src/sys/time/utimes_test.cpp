1d3b5ebf54765eaae49aff87b1ad7801cc61ed87
Michael Jones <michaelrj@google.com>
2025-04-03 16:53:55 -0700

    [libc] Make utimes_test more stable (#134321)
    The test for utimes added in #134167 might fail if the file for one test
    hasn't been cleaned up by the OS before the second test starts. This
    patch makes the tests use different files.

--- libc/test/src/sys/time/utimes_test.cpp
+++ libc/test/src/sys/time/utimes_test.cpp
@@ -17,13 +17,12 @@
 #include "test/UnitTest/ErrnoSetterMatcher.h"
 #include "test/UnitTest/Test.h"
 
-constexpr const char *FILE_PATH = "utimes.test";
-
 // SUCCESS: Takes a file and successfully updates
 // its last access and modified times.
 TEST(LlvmLibcUtimesTest, ChangeTimesSpecific) {
   using LIBC_NAMESPACE::testing::ErrnoSetterMatcher::Succeeds;
 
+  constexpr const char *FILE_PATH = "utimes_pass.test";
   auto TEST_FILE = libc_make_test_file_path(FILE_PATH);
   int fd = LIBC_NAMESPACE::open(TEST_FILE, O_WRONLY | O_CREAT);
   ASSERT_GT(fd, 0);
@@ -62,6 +61,7 @@ TEST(LlvmLibcUtimesTest, InvalidMicroseconds) {
   using LIBC_NAMESPACE::testing::ErrnoSetterMatcher::Fails;
   using LIBC_NAMESPACE::testing::ErrnoSetterMatcher::Succeeds;
 
+  constexpr const char *FILE_PATH = "utimes_fail.test";
   auto TEST_FILE = libc_make_test_file_path(FILE_PATH);
   int fd = LIBC_NAMESPACE::open(TEST_FILE, O_WRONLY | O_CREAT);
   ASSERT_GT(fd, 0);
