8cf46b04f09aec36fb3eca8c040e865cbd57ef0e
Fraser Cormack <fraser@codeplay.com>
2025-04-03 12:01:39 +0100

    remove vector/scalar clc decls

--- libclc/clc/include/clc/math/clc_fmax.h
+++ libclc/clc/include/clc/math/clc_fmax.h
@@ -10,7 +10,7 @@
 #define __CLC_MATH_CLC_FMAX_H__
 
 #define __CLC_FUNCTION __clc_fmax
-#define __CLC_BODY <clc/shared/binary_decl_with_scalar_second_arg.inc>
+#define __CLC_BODY <clc/shared/binary_decl.inc>
 
 #include <clc/math/gentype.inc>
 
