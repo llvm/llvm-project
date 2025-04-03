73e8d67a200beaa554a72cdd50e4d1a5a55caf69
Finn Plummer <canadienfinn@gmail.com>
2025-04-03 09:40:50 -0700

    Revert "[HLSL][RootSignature] Define and integrate `HLSLRootSignatureAttr`" (#134273)
    Reverts llvm/llvm-project#134124
    
    The build is failing again to a linking error:
    [here](https://github.com/llvm/llvm-project/pull/134124#issuecomment-2776370486).
    Again the error was not present locally or any of the pre-merge builds
    and must have been transitively linked in these build environments...

--- clang/test/SemaHLSL/RootSignature-err.hlsl
+++
@@ -1,9 +0,0 @@
-// RUN: %clang_cc1 -triple dxil-pc-shadermodel6.3-library -x hlsl -o - %s -verify
-
-// Attr test
-
-[RootSignature()] // expected-error {{'RootSignature' attribute takes one argument}}
-void bad_root_signature_0() {}
-
-[RootSignature("Arg1", "Arg2")] // expected-error {{'RootSignature' attribute takes one argument}}
-void bad_root_signature_1() {}
