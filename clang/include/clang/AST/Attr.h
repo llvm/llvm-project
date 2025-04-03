ae6728d3dd3943a291314604ced5b9a4b8b4f754
Finn Plummer <canadienfinn@gmail.com>
2025-04-03 09:38:32 -0700

    Revert "[HLSL][RootSignature] Define and integrate `HLSLRootSignatureAttr` (#…"
    This reverts commit 65fa57bdcc9d745dd8c222426e79618fb7cf1c91.

--- clang/include/clang/AST/Attr.h
+++ clang/include/clang/AST/Attr.h
@@ -26,7 +26,6 @@
 #include "clang/Basic/SourceLocation.h"
 #include "clang/Support/Compiler.h"
 #include "llvm/Frontend/HLSL/HLSLResource.h"
-#include "llvm/Frontend/HLSL/HLSLRootSignature.h"
 #include "llvm/Support/CodeGen.h"
 #include "llvm/Support/ErrorHandling.h"
 #include "llvm/Support/VersionTuple.h"
