43c0008640633d7871591b200f15dbcc98785994
ergawy <kareem.ergawy@amd.com>
2025-04-03 23:15:45 -0500

    add -c flag

--- flang/test/Driver/do_concurrent_to_omp_cli.f90
+++ flang/test/Driver/do_concurrent_to_omp_cli.f90
@@ -1,4 +1,4 @@
-! UNSUPPORTED: target=powerpc{{.*}}, system-windows
+! UNSUPPORTED: system-windows
 
 ! RUN: %flang --help | FileCheck %s --check-prefix=FLANG
 
@@ -10,7 +10,7 @@
 ! BBC:      -fdo-concurrent-to-openmp=<string>
 ! BBC-SAME:   Try to map `do concurrent` loops to OpenMP [none|host|device] 
 
-! RUN: %flang -fdo-concurrent-to-openmp=host %s 2>&1 \
+! RUN: %flang -c -fdo-concurrent-to-openmp=host %s 2>&1 \
 ! RUN: | FileCheck %s --check-prefix=OPT
 
 ! OPT: warning: OpenMP is required for lowering `do concurrent` loops to OpenMP.
