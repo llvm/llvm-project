36b55019e63caf46f2219aa91bc6a05bf0da06a9
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2025-04-04 11:03:31 -0700

    Emit error when taking the address of unrecheable blocks

--- mlir/lib/Target/LLVMIR/ModuleImport.cpp
+++ mlir/lib/Target/LLVMIR/ModuleImport.cpp
@@ -2440,7 +2440,7 @@ LogicalResult ModuleImport::processFunction(llvm::Function *func) {
     // Skip unreachable blocks.
     if (!reachable.contains(&basicBlock)) {
       if (basicBlock.hasAddressTaken())
-        emitWarning(funcOp.getLoc())
+        emitError(funcOp.getLoc())
             << "unreachable block '" << basicBlock.getName()
             << "' with address taken";
       continue;
