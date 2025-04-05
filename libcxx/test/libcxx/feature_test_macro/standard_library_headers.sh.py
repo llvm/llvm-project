da82709c62cac89e7ac08244dc3d20a866598018
Mark de Wever <koraq@xs4all.nl>
2025-04-05 12:15:10 +0200

    [libc++] Implements the new FTM header test generator.
    This generator has almost identical output to the existing script.
    Notable differences are
    - conditionally include not yet implemented headers
    - removes the synopsis
    - uses 2 spaces indent in `# if`
    
    There are a few more test macros added that triggered bugs in existing
    FTM.

---
+++ libcxx/test/libcxx/feature_test_macro/standard_library_headers.sh.py
@@ -0,0 +1,33 @@
+# ===----------------------------------------------------------------------===##
+#
+# Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+#
+# ===----------------------------------------------------------------------===##
+
+# RUN: %{python} %s %{libcxx-dir}/utils %{libcxx-dir}/test/libcxx/feature_test_macro/test_data.json
+
+import sys
+
+sys.path.append(sys.argv[1])
+from generate_feature_test_macro_components import FeatureTestMacros
+
+
+def test(output, expected):
+    assert output == expected, f"expected\n{expected}\n\noutput\n{output}"
+
+
+ftm = FeatureTestMacros(sys.argv[2])
+test(
+    sorted(ftm.standard_library_headers),
+    [
+        "algorithm",
+        "any",
+        "barrier",
+        "charconv",
+        "format",
+        "numeric",
+        "variant",
+    ],
+)
--- libcxx/test/libcxx/feature_test_macro/version_header.sh.py
+++ libcxx/test/libcxx/feature_test_macro/version_header.sh.py
@@ -45,9 +45,11 @@ class Test(unittest.TestCase):
 
 #if _LIBCPP_STD_VER >= 17
 #  define __cpp_lib_any 201606L
+#  define __cpp_lib_clamp 201603L
 #  define __cpp_lib_parallel_algorithm 201603L
+// define __cpp_lib_to_chars 201611L
 #  define __cpp_lib_variant 202102L
-// define __cpp_lib_missing_FTM_in_older_standard 2017L
+// define __cpp_lib_zz_missing_FTM_in_older_standard 2017L
 #endif // _LIBCPP_STD_VER >= 17
 
 #if _LIBCPP_STD_VER >= 20
@@ -56,7 +58,7 @@ class Test(unittest.TestCase):
 #  endif
 // define __cpp_lib_format 202110L
 // define __cpp_lib_variant 202106L
-// define __cpp_lib_missing_FTM_in_older_standard 2020L
+// define __cpp_lib_zz_missing_FTM_in_older_standard 2020L
 #endif // _LIBCPP_STD_VER >= 20
 
 #if _LIBCPP_STD_VER >= 23
@@ -70,7 +72,7 @@ class Test(unittest.TestCase):
 #  endif
 // define __cpp_lib_format 202311L
 // define __cpp_lib_variant 202306L
-// define __cpp_lib_missing_FTM_in_older_standard 2026L
+// define __cpp_lib_zz_missing_FTM_in_older_standard 2026L
 #endif // _LIBCPP_STD_VER >= 26
 
 #endif // _LIBCPP_VERSIONH
