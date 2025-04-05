05161a1637eee204ad275f75c4356a2559730e5a
Peng Liu <winner245@hotmail.com>
2025-04-05 16:45:17 -0400

    Refactor to simplify logic of for_each_n_segment.h

--- libcxx/test/benchmarks/algorithms/nonmodifying/for_each_n.bench.cpp
+++ libcxx/test/benchmarks/algorithms/nonmodifying/for_each_n.bench.cpp
@@ -51,44 +51,9 @@ int main(int argc, char** argv) {
     bm.operator()<std::vector<int>>("std::for_each_n(vector<int>)", std_for_each_n);
     bm.operator()<std::deque<int>>("std::for_each_n(deque<int>)", std_for_each_n);
     bm.operator()<std::list<int>>("std::for_each_n(list<int>)", std_for_each_n);
-  }
-
-  // std::for_each_n for join_view
-  {
-    auto bm = []<class Container>(std::string name, auto for_each_n) {
-      using C1       = typename Container::value_type;
-      using ElemType = typename C1::value_type;
-      benchmark::RegisterBenchmark(
-          name,
-          [for_each_n](auto& st) {
-            std::size_t const size     = st.range(0);
-            std::size_t const seg_size = 256;
-            std::size_t const segments = (size + seg_size - 1) / seg_size;
-            Container c(segments);
-            for (std::size_t i = 0, n = size; i < segments; ++i, n -= seg_size) {
-              c[i].resize(std::min(seg_size, n), ElemType(1));
-            }
-
-            auto view  = c | std::views::join;
-            auto first = view.begin();
-
-            for ([[maybe_unused]] auto _ : st) {
-              benchmark::DoNotOptimize(c);
-              auto result = for_each_n(first, size, [](ElemType& x) { x = std::clamp<ElemType>(x, 10, 100); });
-              benchmark::DoNotOptimize(result);
-            }
-          })
-          ->Arg(8)
-          ->Arg(32)
-          ->Arg(50) // non power-of-two
-          ->Arg(1024)
-          ->Arg(4096)
-          ->Arg(8192)
-          ->Arg(1 << 14)
-          ->Arg(1 << 16)
-          ->Arg(1 << 18);
-    };
-    bm.operator()<std::vector<std::vector<int>>>("std::for_each_n(join_view(vector<vector<int>>))", std_for_each_n);
+    bm.operator()<std::vector<int>>("rng::for_each_n(vector<int>)", std::ranges::for_each_n);
+    bm.operator()<std::deque<int>>("rng::for_each_n(deque<int>)", std::ranges::for_each_n);
+    bm.operator()<std::list<int>>("rng::for_each_n(list<int>)", std::ranges::for_each_n);
   }
 
   // {std,ranges}::for_each_n for join_view
