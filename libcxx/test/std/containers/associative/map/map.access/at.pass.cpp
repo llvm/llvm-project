e941811243219e2eb3637a0c5dc3ba078b9d63f4
Vinay Deshmukh <32487576+vinay-deshmukh@users.noreply.github.com>
2025-04-06 10:18:29 -0400

    note: cast from 'void *' is not allowed in a constant expression because the pointed object type 'std::__tree_node<std::__value_type<int, double>, min_pointer<void>>' is not similar to the target type 'std::__tree_node_base<min_pointer<void>>'

--- libcxx/test/std/containers/associative/map/map.access/at.pass.cpp
+++ libcxx/test/std/containers/associative/map/map.access/at.pass.cpp
@@ -75,13 +75,15 @@ TEST_CONSTEXPR_CXX26 bool test() {
     assert(m.at(3) == 3.5);
     assert(m.at(4) == 4.5);
     assert(m.at(5) == 5.5);
-#ifndef TEST_HAS_NO_EXCEPTIONS
+// throwing is not allowed in constexpr
+#if TEST_STD_VER < 26
     try {
       TEST_IGNORE_NODISCARD m.at(6);
       assert(false);
     } catch (std::out_of_range&) {
     }
 #endif
+
     assert(m.at(7) == 7.5);
     assert(m.at(8) == 8.5);
     assert(m.size() == 7);
