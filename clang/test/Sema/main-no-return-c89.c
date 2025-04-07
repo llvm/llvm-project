45821fd65f744df37daf3727ba624f8c171edff4
Oleksandr Tarasiuk <oleksandr.tarasiuk@outlook.com>
2025-04-07 22:45:12 +0300

    add additional tests

--- clang/test/Sema/main-no-return-c89.c
+++
@@ -1,6 +0,0 @@
-/* RUN: %clang_cc1 -std=c89 -fsyntax-only -verify -pedantic -Wno-strict-prototypes %s
- */
-
-int main() {
-
-} /* expected-warning {{non-void 'main' function does not return a value}} */
