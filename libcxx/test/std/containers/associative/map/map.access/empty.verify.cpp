a38112b9596a2630e2b9ab1af71e0f8d75624e53
Vinay Deshmukh <32487576+vinay-deshmukh@users.noreply.github.com>
2025-04-07 18:47:21 -0400

    empty.verify.cpp passes

--- libcxx/test/std/containers/associative/map/map.access/empty.verify.cpp
+++ libcxx/test/std/containers/associative/map/map.access/empty.verify.cpp
@@ -10,21 +10,13 @@
 
 // class map
 
-// bool empty() const noexcept;// constexpr since C++26
+// bool empty() const noexcept;
 
 // UNSUPPORTED: c++03, c++11, c++14, c++17
 
 #include <map>
 
-bool test() {
+void f() {
   std::map<int, int> c;
   c.empty(); // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
-  return true;
-}
-
-int main() {
-  assert(test());
-#if TEST_STD_VER >= 26
-  static_assert(test());
-#endif
 }
