3ca7fa32e3a33cfe8e4e133bc7eb63c076272b5b
Eugene Epshteyn <eepshteyn@nvidia.com>
2025-04-08 14:13:57 -0400

    Compilation and bug fixes
    	modified:   flang-rt/lib/runtime/extensions.cpp
    	modified:   flang/include/flang/Runtime/extensions.h

--- flang-rt/lib/runtime/extensions.cpp
+++ flang-rt/lib/runtime/extensions.cpp
@@ -284,7 +284,7 @@ int FORTRAN_PROCEDURE_NAME(hostnm)(char* hn, int length) {
 
 #else
 
-  if (gethostname(hn, sizeof(length)) < 0) {
+  if (gethostname(hn, length) < 0) {
     status = errno;
   }
 
@@ -293,8 +293,8 @@ int FORTRAN_PROCEDURE_NAME(hostnm)(char* hn, int length) {
   if (status == 0) {
     // Find zero terminator and fill the string from the
     // zero terminator to the end with spaces
-    const char* str_end = hn + length;
-    const char* str_zero = std::find(hn, str_end, '\0');
+    char* str_end = hn + length;
+    char* str_zero = std::find(hn, str_end, '\0');
     std::fill(str_zero, str_end, ' ');
   }
 
