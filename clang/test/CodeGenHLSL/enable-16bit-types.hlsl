35a080411e4ce0618665db31de7bf887b8d1abb6
Icohedron <cheung.deric@gmail.com>
2025-04-08 23:02:16 +0000

    Replace clang_cc1 with clang_dxc in enable-16bit-types test

--- clang/test/CodeGenHLSL/enable-16bit-types.hlsl
+++ clang/test/CodeGenHLSL/enable-16bit-types.hlsl
@@ -1,7 +1,5 @@
-// RUN: %clang_cc1 -fnative-half-type -finclude-default-header -triple dxil-pc-shadermodel6.3-library -emit-llvm -disable-llvm-passes -o - %s | FileCheck %s --check-prefix=FLAG
-// RUN: %clang_cc1 -finclude-default-header -triple dxil-pc-shadermodel6.3-library -emit-llvm -disable-llvm-passes -o - %s | FileCheck %s --check-prefix=NOFLAG
-
-// NOTE: -enable-16bit-types is a DXCFlag that aliases -fnative-half-type
+// RUN: %clang_dxc -enable-16bit-types -T lib_6_3 -HV 202x %s | FileCheck %s --check-prefix=FLAG
+// RUN: %clang_dxc -T lib_6_3 -HV 202x %s | FileCheck %s --check-prefix=NOFLAG
 
 // FLAG-DAG: ![[NLP:.*]] = !{i32 1, !"dx.nativelowprec", i32 1}
 // FLAG-DAG: !llvm.module.flags = !{{{.*}}![[NLP]]{{.*}}}
