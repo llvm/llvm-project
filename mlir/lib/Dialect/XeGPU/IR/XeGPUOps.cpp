f464662ab164c11cbe6630ddf6acd9f100c83c6e
Chao Chen <chao.chen@intel.com>
2025-04-08 21:52:23 +0000

    update doc

--- mlir/lib/Dialect/XeGPU/IR/XeGPUOps.cpp
+++ mlir/lib/Dialect/XeGPU/IR/XeGPUOps.cpp
@@ -105,6 +105,12 @@ isArgShapesValid(TensorDescType tdescTy, VectorType valueTy,
                    << " for tensor descriptor " << tdescTy;
 }
 
+// Checks if the given shape is evenly distributed based on the layout
+// and data factors provided by the LayoutAttr. The function ensures that
+// each dimension of the shape can be evenly divided by the corresponding
+// data factor, and the resulting quotient can be evenly divided by the
+// layout factor. Returns `true` if the shape is evenly distributed,
+// otherwise `false`.
 static bool isEvenDistributed(llvm::ArrayRef<int64_t> shape,
                               xegpu::LayoutAttr attr) {
   assert(attr && "Layout attribute is missing.");
@@ -121,7 +127,6 @@ static bool isEvenDistributed(llvm::ArrayRef<int64_t> shape,
   }
   for (auto [dimSize, dataFactor, layoutFactor] :
        llvm::zip_equal(shape, data, layout)) {
-    // check dimSize % (dataFactor * layoutFactor) != 0
     if (dimSize % dataFactor != 0 || (dimSize / dataFactor) % layoutFactor != 0)
       return false;
   }
