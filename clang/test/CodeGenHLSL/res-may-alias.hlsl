98a4848f6b5bbc6195a6fe639c5c7fb9547b08fc
Icohedron <cheung.deric@gmail.com>
2025-04-09 21:34:53 +0000

    Add '-Xclang -emit-llvm' to avoid running the DirectX backend

--- clang/test/CodeGenHLSL/res-may-alias.hlsl
+++ clang/test/CodeGenHLSL/res-may-alias.hlsl
@@ -1,5 +1,5 @@
-// RUN: %clang_dxc -res-may-alias -T lib_6_3 -HV 202x -Vd %s | FileCheck %s --check-prefix=FLAG
-// RUN: %clang_dxc -T lib_6_3 -HV 202x -Vd %s | FileCheck %s --check-prefix=NOFLAG
+// RUN: %clang_dxc -res-may-alias -T lib_6_3 -HV 202x -Vd -Xclang -emit-llvm %s | FileCheck %s --check-prefix=FLAG
+// RUN: %clang_dxc -T lib_6_3 -HV 202x -Vd -Xclang -emit-llvm %s | FileCheck %s --check-prefix=NOFLAG
 
 // FLAG-DAG: ![[RMA:.*]] = !{i32 1, !"dx.resmayalias", i32 1}
 // FLAG-DAG: !llvm.module.flags = !{{{.*}}![[RMA]]{{.*}}}
