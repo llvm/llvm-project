db9c5a69a95a8abde662776341ba1e581a0a2445
PeterChou1 <peter.chou@mail.utoronto.ca>
2025-04-09 17:13:59 -0400

    address comment

--- clang-tools-extra/clang-doc/support/File.h
+++ clang-tools-extra/clang-doc/support/File.h
@@ -1,12 +1,12 @@
-//===-- File.h --- File Helpers -------------------*- C++-*-===//
+//===----------------------------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
-#ifndef LLVM_CLANG_TOOLS_EXTRA_CLANG_DOC_FILEHELPER_H
-#define LLVM_CLANG_TOOLS_EXTRA_CLANG_DOC_FILEHELPER_H
+#ifndef LLVM_CLANG_TOOLS_EXTRA_CLANG_DOC_FILE_H
+#define LLVM_CLANG_TOOLS_EXTRA_CLANG_DOC_FILE_H
 
 #include "llvm/ADT/StringExtras.h"
 #include "llvm/Support/Error.h"
@@ -22,4 +22,4 @@ llvm::SmallString<128> computeRelativePath(llvm::StringRef Destination,
 } // namespace doc
 } // namespace clang
 
-#endif // LLVM_CLANG_TOOLS_EXTRA_CLANG_DOC_FILEHELPER_H
+#endif // LLVM_CLANG_TOOLS_EXTRA_CLANG_DOC_FILE_H
