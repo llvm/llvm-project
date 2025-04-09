9e3fff0bc13d74195cb47f9e3a7d5271dd9f958b
Vinay Deshmukh <32487576+vinay-deshmukh@users.noreply.github.com>
2025-04-08 22:42:52 -0400

    pass map.modifiers/insert_cv.pass.cpp

--- libcxx/test/std/containers/associative/map/map.modifiers/insert_cv.pass.cpp
+++ libcxx/test/std/containers/associative/map/map.modifiers/insert_cv.pass.cpp
@@ -19,7 +19,7 @@
 #include "min_allocator.h"
 
 template <class Container>
-void do_insert_cv_test() {
+TEST_CONSTEXPR_CXX26 bool do_insert_cv_test() {
   typedef Container M;
   typedef std::pair<typename M::iterator, bool> R;
   typedef typename M::value_type VT;
@@ -56,6 +56,8 @@ void do_insert_cv_test() {
   assert(m.size() == 3);
   assert(r.first->first == 3);
   assert(r.first->second == 3.5);
+
+  return true;
 }
 
 TEST_CONSTEXPR_CXX26 bool test() {
