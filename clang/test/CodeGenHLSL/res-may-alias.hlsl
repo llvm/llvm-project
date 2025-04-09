e341d39e631948c1bd3a14b76e37e0c36c6605d2
Icohedron <cheung.deric@gmail.com>
2025-04-09 18:16:42 +0000

    Use clang_dxc instead of clang_cc1

--- clang/test/CodeGenHLSL/res-may-alias.hlsl
+++ clang/test/CodeGenHLSL/res-may-alias.hlsl
@@ -1,5 +1,5 @@
-// RUN: %clang_cc1 -res-may-alias -finclude-default-header -triple dxil-pc-shadermodel6.3-library -emit-llvm -disable-llvm-passes -o - %s | FileCheck %s --check-prefix=FLAG
-// RUN: %clang_cc1 -finclude-default-header -triple dxil-pc-shadermodel6.3-library -emit-llvm -disable-llvm-passes -o - %s | FileCheck %s --check-prefix=NOFLAG
+// RUN: %clang_dxc -res-may-alias -T lib_6_3 -HV 202x %s | FileCheck %s --check-prefix=FLAG
+// RUN: %clang_dxc -T lib_6_3 -HV 202x %s | FileCheck %s --check-prefix=NOFLAG
 
 // FLAG-DAG: ![[RMA:.*]] = !{i32 1, !"dx.resmayalias", i32 1}
 // FLAG-DAG: !llvm.module.flags = !{{{.*}}![[RMA]]{{.*}}}
