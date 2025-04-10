3fb67c9b96db3e190b90f6b822da2d45a81feead
erichkeane <ekeane@nvidia.com>
2025-04-10 10:13:09 -0700

    [OpenACC][CIR] implement basic 'set' lowering with device_type clause
    The 'set' lowering is pretty trivial.  'device_type' is a little more
    restricted since both the MLIR-Dialect and language limit it to only 1
    value (as confirmed by standards-discussion).
    
    This patch implements 'set', with 'device_type', since 'set' requires at
    least 1 clause, and  this is the least difficult to implement at the
    moment.

--- clang/lib/CIR/CodeGen/CIRGenStmtOpenACC.cpp
+++ clang/lib/CIR/CodeGen/CIRGenStmtOpenACC.cpp
@@ -92,6 +92,7 @@ public:
 
     switch (dirKind) {
     case OpenACCDirectiveKind::Init:
+    case OpenACCDirectiveKind::Set:
     case OpenACCDirectiveKind::Shutdown: {
       // Device type has a list that is either a 'star' (emitted as 'star'),
       // or an identifer list, all of which get added for attributes.
@@ -133,6 +134,11 @@ public:
 
         op.setDeviceTypesAttr(
             mlir::ArrayAttr::get(builder.getContext(), deviceTypes));
+      } else if constexpr (isOneOfTypes<Op, SetOp>) {
+        assert(attrData.deviceTypeArchs.size() <= 1 &&
+               "Set can only have a single architecture");
+        if (!attrData.deviceTypeArchs.empty())
+          op.setDeviceType(attrData.deviceTypeArchs[0]);
       } else {
         cgm.errorNYI(dirLoc, "OpenACC 'device_type' clause lowering for ",
                      dirKind);
@@ -232,6 +238,13 @@ CIRGenFunction::emitOpenACCInitConstruct(const OpenACCInitConstruct &s) {
                                s.clauses());
 }
 
+mlir::LogicalResult
+CIRGenFunction::emitOpenACCSetConstruct(const OpenACCSetConstruct &s) {
+  mlir::Location start = getLoc(s.getSourceRange().getEnd());
+  return emitOpenACCOp<SetOp>(start, s.getDirectiveKind(), s.getDirectiveLoc(),
+                              s.clauses());
+}
+
 mlir::LogicalResult CIRGenFunction::emitOpenACCShutdownConstruct(
     const OpenACCShutdownConstruct &s) {
   mlir::Location start = getLoc(s.getSourceRange().getEnd());
@@ -267,11 +280,6 @@ mlir::LogicalResult CIRGenFunction::emitOpenACCHostDataConstruct(
 mlir::LogicalResult
 CIRGenFunction::emitOpenACCWaitConstruct(const OpenACCWaitConstruct &s) {
   getCIRGenModule().errorNYI(s.getSourceRange(), "OpenACC Wait Construct");
-  return mlir::failure();
-}
-mlir::LogicalResult
-CIRGenFunction::emitOpenACCSetConstruct(const OpenACCSetConstruct &s) {
-  getCIRGenModule().errorNYI(s.getSourceRange(), "OpenACC Set Construct");
   return mlir::failure();
 }
 mlir::LogicalResult
