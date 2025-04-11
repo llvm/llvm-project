645b890fd8778a44a11a6c026b0d08fe58c76212
Mark de Wever <koraq@xs4all.nl>
2025-04-11 17:05:30 +0200

    [libc++] A quick-fix for the Apple CI. (#135202)
    The Apple runners automatically pick up newer XCode versions breaking
    the CI. This disables the test to get the CI green which allows us to
    investigate the issue properly later on.

--- libcxx/test/std/localization/locale.categories/facet.numpunct/locale.numpunct.byname/thousands_sep.pass.cpp
+++ libcxx/test/std/localization/locale.categories/facet.numpunct/locale.numpunct.byname/thousands_sep.pass.cpp
@@ -6,6 +6,9 @@
 //
 //===----------------------------------------------------------------------===//
 
+// TODO(mordante) Investigate
+// UNSUPPORTED: apple-clang
+
 // NetBSD does not support LC_NUMERIC at the moment
 // XFAIL: netbsd
 
