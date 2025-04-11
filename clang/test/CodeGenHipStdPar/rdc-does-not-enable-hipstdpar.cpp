845807ee2cc885963b7583df96f93959292c95e8
Alex Voicu <alexandru.voicu@amd.com>
2025-04-12 00:00:01 +0100

    Update test.

--- clang/test/CodeGenHipStdPar/rdc-does-not-enable-hipstdpar.cpp
+++ clang/test/CodeGenHipStdPar/rdc-does-not-enable-hipstdpar.cpp
@@ -4,8 +4,8 @@
 
 // RUN: %clang -### --hipstdpar --offload-arch=gfx906 %s -nogpulib -nogpuinc \
 // RUN:   2>&1 | FileCheck -check-prefix=NORDC %s
-// NORDC: {{".*clang.*".* "-triple" "amdgcn-amd-amdhsa".* "-mllvm" "-amdgpu-enable-hipstdpar".*}}
+// NORDC: {{.*}}"-mllvm" "-amdgpu-enable-hipstdpar"
 
 // RUN: %clang -### --hipstdpar --offload-arch=gfx906 %s -nogpulib -nogpuinc -fgpu-rdc \
 // RUN:   2>&1 | FileCheck -check-prefix=RDC %s
-// RDC-NOT: {{"-mllvm" "-amdgpu-enable-hipstdpar".*}}
+// RDC-NOT: {{.*}}"-mllvm" "-amdgpu-enable-hipstdpar"
