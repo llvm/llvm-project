2ace506037a291ef18e6cc4225811b0d785be527
Carlos Alberto Enciso <Carlos.Enciso@sony.com>
2025-04-11 21:52:56 +0100

    [llvm-debuginfo-analyzer] Add support for LLVM IR format.
    Add support for the LLVM IR format and be able to generate
    logical views.
    
    Both textual representation (.ll) and bitcode (.bc) format
    are supported.
    
    Note: This patch requires:
    
      Add DebugSSAUpdater class to track debug value liveness
      https://github.com/llvm/llvm-project/pull/135349

--- llvm/lib/DebugInfo/LogicalView/Core/LVReader.cpp
+++ llvm/lib/DebugInfo/LogicalView/Core/LVReader.cpp
@@ -22,6 +22,30 @@ using namespace llvm::logicalview;
 
 #define DEBUG_TYPE "Reader"
 
+// Traverse all the logical elements and print its basic information.
+void printCollectedElements(LVScope *Root) {
+  std::function<void(LVScope * Parent)> TraverseScope = [&](LVScope *Parent) {
+    // Print the elements.
+    auto Print = [&](const auto &Set) {
+      if (Set)
+        for (const auto &Entry : *Set)
+          Entry->print(dbgs());
+    };
+
+    Print(Parent->getChildren());
+    Print(Parent->getLines());
+    Print(Parent->getRanges());
+    if (const LVScopes *Scopes = Parent->getScopes())
+      for (LVScope *Scope : *Scopes)
+        TraverseScope(Scope);
+  };
+
+  // Start traversing the scopes root.
+  dbgs() << "\n** Collected logical elements **\n";
+  Root->print(dbgs());
+  TraverseScope(Root);
+}
+
 // Detect elements that are inserted more than once at different scopes,
 // causing a crash on the reader destruction, as the element is already
 // deleted from other scope. Helper for CodeView reader.
@@ -60,6 +84,9 @@ bool checkIntegrityScopesTree(LVScope *Root) {
     Traverse(Parent->getTypes());
     Traverse(Parent->getLines());
   };
+
+  // Print collected elements.
+  printCollectedElements(Root);
 
   // Start traversing the scopes root and print any duplicates.
   TraverseScope(Root);
