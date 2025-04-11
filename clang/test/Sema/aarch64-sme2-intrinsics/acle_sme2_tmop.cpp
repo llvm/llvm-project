51462fa9deb4547e1528ba49cf4caa8a8a04314b
Jonathan Thackray <jonathan.thackray@arm.com>
2025-04-11 21:51:28 +0000

    fixup! Fix Marian's code review comments

--- clang/test/Sema/aarch64-sme2-intrinsics/acle_sme2_tmop.cpp
+++ clang/test/Sema/aarch64-sme2-intrinsics/acle_sme2_tmop.cpp
@@ -1,28 +1,19 @@
-// RUN: %clang_cc1 -triple aarch64 -target-feature +sme -target-feature +sme2 -target-feature +bf16 -target-feature +sme-f16f16 -target-feature +sme-b16b16 -verify -emit-llvm -o - %s
+// RUN: %clang_cc1 -triple aarch64 \
+// RUN:   -target-feature +sme -target-feature +sme2 -verify -emit-llvm -o - %s
 
 // REQUIRES: aarch64-registered-target
 
 #include <arm_sme.h>
 
-void test_features() __arm_streaming __arm_inout("za") {
-    svuint8x2_t zn_u8;
-    svint8x2_t zn_s8;
-    svuint8_t zm_u8;
-    svint8_t zm_s8;
-    svuint16x2_t zn_u16;
-    svint16x2_t zn_s16;
-    svuint16_t zm_u16;
-    svint16_t zm_s16;
-    svbfloat16x2_t zn_bf16;
-    svfloat16x2_t zn_f16;
-    svbfloat16_t zm_bf16;
-    svfloat16_t zm_f16;
-    svfloat32x2_t zn_f32;
-    svfloat32_t zm_f32;
-    svmfloat8x2_t zn_f8;
-    svmfloat8_t zm_f8;
-    fpm_t fpm = 0;
-    svuint8_t zk;
+void test_features(svuint8x2_t zn_u8,      svuint8_t zm_u8,
+                   svint8x2_t zn_s8,       svint8_t zm_s8,
+                   svint16x2_t zn_s16,     svint16_t zm_s16,
+                   svuint16x2_t zn_u16,    svuint16_t zm_u16,
+                   svfloat16x2_t zn_f16,   svfloat16_t zm_f16,
+                   svbfloat16x2_t zn_bf16, svbfloat16_t zm_bf16,
+                   svfloat32x2_t zn_f32,   svfloat32_t zm_f32,
+                   svmfloat8x2_t zn_f8,    svmfloat8_t zm_f8,
+                   svuint8_t zk,           fpm_t fpm) __arm_streaming __arm_inout("za") {
 
 // expected-error@+1 {{'svtmopa_lane_za32_s8_s8' needs target feature sme,sme2,sme-tmop}}
     svtmopa_lane_za32_s8_s8(0, zn_s8, zm_s8, zk, 0);
@@ -44,7 +35,7 @@ void test_features() __arm_streaming __arm_inout("za") {
     svtmopa_lane_za32_bf16_bf16(0, zn_bf16, zm_bf16, zk, 0);
 // expected-error@+1 {{'svtmopa_lane_za16_f16_f16' needs target feature sme,sme2,sme-tmop,sme-f16f16}}
     svtmopa_lane_za16_f16_f16(0, zn_f16, zm_f16, zk, 0);
-// expected-error@+1 {{'svtmopa_lane_za16_bf16_bf16' needs target feature sme,sme2,sme-tmop,sme-f16f16}}
+// expected-error@+1 {{'svtmopa_lane_za16_bf16_bf16' needs target feature sme,sme2,sme-tmop,sme-b16b16}}
     svtmopa_lane_za16_bf16_bf16(0, zn_bf16, zm_bf16, zk, 0);
 // expected-error@+1 {{'svtmopa_lane_za16_mf8_mf8_fpm' needs target feature sme,sme2,sme-tmop,sme-f8f16}}
     svtmopa_lane_za16_mf8_mf8_fpm(0, zn_f8, zm_f8, zk, 0, fpm);
@@ -52,25 +43,15 @@ void test_features() __arm_streaming __arm_inout("za") {
     svtmopa_lane_za32_mf8_mf8_fpm(0, zn_f8, zm_f8, zk, 0, fpm);
 }
 
-void test_imm() __arm_streaming __arm_inout("za") {
-    svuint8x2_t zn_u8;
-    svint8x2_t zn_s8;
-    svuint8_t zm_u8;
-    svint8_t zm_s8;
-    svuint16x2_t zn_u16;
-    svint16x2_t zn_s16;
-    svuint16_t zm_u16;
-    svint16_t zm_s16;
-    svbfloat16x2_t zn_bf16;
-    svfloat16x2_t zn_f16;
-    svbfloat16_t zm_bf16;
-    svfloat16_t zm_f16;
-    svfloat32x2_t zn_f32;
-    svfloat32_t zm_f32;
-    svmfloat8x2_t zn_f8;
-    svmfloat8_t zm_f8;
-    fpm_t fpm;
-    svuint8_t zk;
+void test_imm(svuint8x2_t zn_u8,      svuint8_t zm_u8,
+              svint8x2_t zn_s8,       svint8_t zm_s8,
+              svint16x2_t zn_s16,     svint16_t zm_s16,
+              svuint16x2_t zn_u16,    svuint16_t zm_u16,
+              svfloat16x2_t zn_f16,   svfloat16_t zm_f16,
+              svbfloat16x2_t zn_bf16, svbfloat16_t zm_bf16,
+              svfloat32x2_t zn_f32,   svfloat32_t zm_f32,
+              svmfloat8x2_t zn_f8,    svmfloat8_t zm_f8,
+              svuint8_t zk,           fpm_t fpm) __arm_streaming __arm_inout("za") {
 
 // expected-error@+1 {{argument value 4 is outside the valid range [0, 3]}}
     svtmopa_lane_za32_s8_s8(0, zn_s8, zm_s8, zk, 4);
