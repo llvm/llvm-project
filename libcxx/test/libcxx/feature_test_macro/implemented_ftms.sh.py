05a014b2ffd43a1e6e29e436ec38217cf421138e
Mark de Wever <koraq@xs4all.nl>
2025-04-11 20:27:29 +0200

    [libc++] Adds is_implemented function for new ftm generator. (#134538)
    At the moment the ftm macro for __cpp_lib_to_chars will have the
    following values:
    
    standard_ftms: {
        "c++17": "201611L",
        "c++20": "201611L",
        "c++23": "201611L",
        "c++26": "201611L",
    }
    
    implemented_ftms: {
        "c++17": None,
    }
    
    This is an issue with the test whether the FTM is implemented it does:
      self.implemented_ftms[ftm][std] == self.standard_ftms[ftm][std]
    This will fail in C++20 since implemented_ftms[ftm] does not have the
    key c++20. This adds a new helper function and removes the None entries
    when a FTM is not implemented.
    
    ---------
    
    Co-authored-by: Louis Dionne <ldionne.2@gmail.com>

--- libcxx/test/libcxx/feature_test_macro/implemented_ftms.sh.py
+++ libcxx/test/libcxx/feature_test_macro/implemented_ftms.sh.py
@@ -38,11 +38,7 @@ class Test(unittest.TestCase):
                 "c++23": "201907L",
                 "c++26": "299900L",
             },
-            "__cpp_lib_format": {
-                "c++20": None,
-                "c++23": None,
-                "c++26": None,
-            },
+            "__cpp_lib_format": {},
             "__cpp_lib_parallel_algorithm": {
                 "c++17": "201603L",
                 "c++20": "201603L",
@@ -55,11 +51,7 @@ class Test(unittest.TestCase):
                 "c++23": "202102L",
                 "c++26": "202102L",
             },
-            "__cpp_lib_missing_FTM_in_older_standard": {
-                "c++17": None,
-                "c++20": None,
-                "c++26": None,
-            },
+            "__cpp_lib_missing_FTM_in_older_standard": {},
         }
 
         self.assertEqual(self.ftm.implemented_ftms, expected)
