cc4553879bd452dd983b2d7c262342be4748ac5d
Arun Thangamani <arun.thangamani@intel.com>
2025-04-10 19:56:58 -0700

    fixed couple of clang format

--- mlir/include/mlir/Dialect/X86Vector/X86VectorDialect.h
+++ mlir/include/mlir/Dialect/X86Vector/X86VectorDialect.h
@@ -14,6 +14,7 @@
 #define MLIR_DIALECT_X86VECTOR_X86VECTORDIALECT_H_
 
 #include "mlir/Bytecode/BytecodeOpInterface.h"
+#include "mlir/Dialect/LLVMIR/LLVMDialect.h"
 #include "mlir/IR/BuiltinTypes.h"
 #include "mlir/IR/Dialect.h"
 #include "mlir/IR/OpDefinition.h"
@@ -21,7 +22,6 @@
 #include "mlir/IR/PatternMatch.h"
 #include "mlir/Interfaces/InferTypeOpInterface.h"
 #include "mlir/Interfaces/SideEffectInterfaces.h"
-#include "mlir/Dialect/LLVMIR/LLVMDialect.h"
 
 /// Include the generated interface declarations.
 #include "mlir/Dialect/X86Vector/X86VectorInterfaces.h.inc"
