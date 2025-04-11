9f88c67d226864bd7df99a3e55d4a4790a8d69fa
Ivan Trofimov <i.trofimow@yandex.ru>
2025-04-11 18:23:30 +0300

    fix std::make_exception_ptr interaction with ObjC

--- libcxx/include/__exception/exception_ptr.h
+++ libcxx/include/__exception/exception_ptr.h
@@ -92,7 +92,7 @@ public:
 template <class _Ep>
 _LIBCPP_HIDE_FROM_ABI exception_ptr make_exception_ptr(_Ep __e) _NOEXCEPT {
 #  if _LIBCPP_HAS_EXCEPTIONS
-#    if _LIBCPP_AVAILABILITY_HAS_INIT_PRIMARY_EXCEPTION && __cplusplus >= 201103L
+#    if !defined(__OBJC__) && _LIBCPP_AVAILABILITY_HAS_INIT_PRIMARY_EXCEPTION && __cplusplus >= 201103L
   using _Ep2 = __decay_t<_Ep>;
 
   void* __ex = __cxxabiv1::__cxa_allocate_exception(sizeof(_Ep));
