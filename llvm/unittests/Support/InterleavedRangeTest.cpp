1b876f53de25833f08f24266c43910ff82aabd32
Jakub Kuderski <jakub@nod-labs.com>
2025-04-13 14:22:03 -0400

    Fix format

--- llvm/unittests/Support/InterleavedRangeTest.cpp
+++ llvm/unittests/Support/InterleavedRangeTest.cpp
@@ -6,8 +6,8 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "llvm/ADT/SmallVector.h"
 #include "llvm/Support/InterleavedRange.h"
+#include "llvm/ADT/SmallVector.h"
 #include "llvm/Support/raw_ostream.h"
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
