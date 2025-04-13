5817db4bad39405aab182ea49e0a7628d052732f
Fangrui Song <i@maskray.me>
2025-04-13 15:55:11 -0700

    ARMAsmBackend: Use fixupNeedsRelaxationAdvanced. NFC
    This prepares for the upcoming change to simplify relocation recording
    in MCAssembler.
    
    While both MCAssembler::fixupNeedsRelaxation and
    MCAssembler::handleFixup call evaluateFixup and use
    shouldForceRelocation, the shouldForceRelocation logic is not supposed
    to be needed by MCAssembler::fixupNeedsRelaxation.
    
    The ARM special cases for interworking branches
    (https://reviews.llvm.org/D33436 and https://reviews.llvm.org/D33898)
    break the assumption. Switch to fixupNeedsRelaxationAdvanced and
    explicitly test the conditions.

--- llvm/lib/Target/ARM/MCTargetDesc/ARMAsmBackend.h
+++ llvm/lib/Target/ARM/MCTargetDesc/ARMAsmBackend.h
@@ -54,8 +54,10 @@ public:
   const char *reasonForFixupRelaxation(const MCFixup &Fixup,
                                        uint64_t Value) const;
 
-  bool fixupNeedsRelaxation(const MCFixup &Fixup,
-                            uint64_t Value) const override;
+  bool fixupNeedsRelaxationAdvanced(const MCAssembler &,
+                                    const MCRelaxableFragment &,
+                                    const MCFixup &, const MCValue &, uint64_t,
+                                    bool, bool) const override;
 
   void relaxInstruction(MCInst &Inst,
                         const MCSubtargetInfo &STI) const override;
