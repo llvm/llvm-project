e902b90d6ffcca98a0d6437697ab109a1843df57
Yingwei Zheng <dtcxzyw2333@gmail.com>
2025-04-14 17:03:39 +0800

    [IR][ADT] Remove `APFloat/Type::isIEEE` (#130496)
    Address comment
    https://github.com/llvm/llvm-project/pull/130477#issuecomment-2708801892.

--- llvm/include/llvm/SandboxIR/Type.h
+++ llvm/include/llvm/SandboxIR/Type.h
@@ -120,8 +120,8 @@ public:
   bool isPPC_FP128Ty() const { return LLVMTy->isPPC_FP128Ty(); }
 
   /// Return true if this is a well-behaved IEEE-like type, which has a IEEE
-  /// compatible layout as defined by APFloat::isIEEE(), and does not have
-  /// non-IEEE values, such as x86_fp80's unnormal values.
+  /// compatible layout, and does not have non-IEEE values, such as x86_fp80's
+  /// unnormal values.
   bool isIEEELikeFPTy() const { return LLVMTy->isIEEELikeFPTy(); }
 
   /// Return true if this is one of the floating-point types
@@ -261,10 +261,6 @@ public:
   /// floating-point types. If the FP type does not have a stable mantissa (e.g.
   /// ppc long double), this method returns -1.
   int getFPMantissaWidth() const { return LLVMTy->getFPMantissaWidth(); }
-
-  /// Return whether the type is IEEE compatible, as defined by the eponymous
-  /// method in APFloat.
-  bool isIEEE() const { return LLVMTy->isIEEE(); }
 
   /// If this is a vector type, return the element type, otherwise return
   /// 'this'.
