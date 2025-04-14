a18cc4c7cb51f94182b6018c7c73acde1b8ebddb
Tian, Shilei <Shilei.Tian@amd.com>
2025-04-14 15:02:17 -0400

    [DeviceLibs][CMake] Policy `CMP0053` is no longer supported by CMake 4+ (#1519)
    The part of the code needs to be refined; otherwise we will have
    multiple breaks
    after users upgrade to CMake 4 on Windows.

--- amd/device-libs/cmake/OCL.cmake
+++ amd/device-libs/cmake/OCL.cmake
@@ -9,7 +9,10 @@
 # command-lines, but the only way to do this as part of the dependency graph is
 # configure_file and we are included from multiple places. To get around this
 # we `file(WRITE)` a file with an @variable reference and `configure_file` it.
-cmake_policy(SET CMP0053 OLD)
+# FIXME: CMP0053 is removed in CMake 4; refine code relying on this policy.
+if(${CMAKE_VERSION} VERSION_LESS "4.0.0")
+  cmake_policy(SET CMP0053 OLD)
+endif()
 
 if(${CMAKE_VERSION} VERSION_GREATER_EQUAL "3.20.0")
   # The policy change was for handling of relative paths for
