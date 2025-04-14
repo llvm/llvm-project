9e612f80495e805267bccd99fcff93ba97a6f168
Steven Perron <stevenperron@google.com>
2025-04-14 14:34:28 -0400

    Fix address space dependent test.

--- clang/test/SemaTemplate/address_space-dependent.cpp
+++ clang/test/SemaTemplate/address_space-dependent.cpp
@@ -43,7 +43,7 @@ void neg() {
 
 template <long int I>
 void tooBig() {
-  __attribute__((address_space(I))) int *bounds; // expected-error {{address space is larger than the maximum supported (8388584)}}
+  __attribute__((address_space(I))) int *bounds; // expected-error {{address space is larger than the maximum supported (8388583)}}
 }
 
 template <long int I>
