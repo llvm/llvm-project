db41ec99bf304d4498a5631ba175a0484a429a51
Akshat Oke <Akshat.Oke@amd.com>
2025-04-14 10:19:42 +0530

    [CodeGen][NPM] Port BranchRelaxation to NPM (#130067)
    This completes the PreEmitPasses.

--- llvm/lib/Passes/PassBuilder.cpp
+++ llvm/lib/Passes/PassBuilder.cpp
@@ -83,6 +83,7 @@
 #include "llvm/CodeGen/AtomicExpand.h"
 #include "llvm/CodeGen/BasicBlockSectionsProfileReader.h"
 #include "llvm/CodeGen/BranchFoldingPass.h"
+#include "llvm/CodeGen/BranchRelaxation.h"
 #include "llvm/CodeGen/CallBrPrepare.h"
 #include "llvm/CodeGen/CodeGenPrepare.h"
 #include "llvm/CodeGen/ComplexDeinterleavingPass.h"
