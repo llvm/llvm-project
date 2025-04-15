4b25eec38aa68ad2380dda6251114cebb1ed63c1
Aaron Ballman <aaron@aaronballman.com>
2025-04-15 07:18:34 -0400

    Update based on review feedback

--- clang/test/Sema/warn-cast-function-type-win.c
+++ clang/test/Sema/warn-cast-function-type-win.c
@@ -23,11 +23,14 @@ void test(void) {
   // return type to test1_type, but FARPROC1 has no prototype and so checking
   // is disabled for further compatibility issues. In C++ mode, all functions
   // have a prototype and so the check happens.
-  test1_type t1 = (test1_type)GetProcAddress1(); // linux-cpp-warning {{cast from 'FARPROC1' (aka 'int (*)()') to 'test1_type' (aka 'int (*)(int)') converts to incompatible function type}} \
-                                                    strict-warning {{cast from 'FARPROC1' (aka 'int (*)()') to 'test1_type' (aka 'int (*)(int)') converts to incompatible function type}}
+  test1_type t1 = (test1_type)GetProcAddress1();
+  // linux-cpp-warning@-1 {{cast from 'FARPROC1' (aka 'int (*)()') to 'test1_type' (aka 'int (*)(int)') converts to incompatible function type}}
+  // strict-warning@-2 {{cast from 'FARPROC1' (aka 'int (*)()') to 'test1_type' (aka 'int (*)(int)') converts to incompatible function type}}
+  
   // This case is diagnosed in both C and C++ modes on Linux because the return
   // type of FARPROC2 does not match the return type of test2_type.
-  test2_type t2 = (test2_type)GetProcAddress2(); // linux-warning {{cast from 'FARPROC2' (aka 'unsigned long long (*)()') to 'test2_type' (aka 'float (*)()') converts to incompatible function type}} \
-                                                    strict-warning {{cast from 'FARPROC2' (aka 'unsigned long long (*)()') to 'test2_type' (aka 'float (*)()') converts to incompatible function type}}
+  test2_type t2 = (test2_type)GetProcAddress2();
+  // linux-warning@-1 {{cast from 'FARPROC2' (aka 'unsigned long long (*)()') to 'test2_type' (aka 'float (*)()') converts to incompatible function type}}
+  // strict-warning@-2 {{cast from 'FARPROC2' (aka 'unsigned long long (*)()') to 'test2_type' (aka 'float (*)()') converts to incompatible function type}}
 }
 
