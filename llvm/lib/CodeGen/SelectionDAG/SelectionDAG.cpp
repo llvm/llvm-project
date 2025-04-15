21fee3b386652f5a6b3134d77161b86f3443a4bd
Simon Pilgrim <llvm-dev@redking.me.uk>
2025-04-15 08:33:42 +0100

    [DAG] isSplatValue - only treat binop splats shared undef elements as undef (#135597)
    #134602 demonstrated an issue where an AND node always had at least one demanded UNDEF element in either operand, and incorrectly reported this an all-undef result - despite the other element being 0 (so would correctly fold to 0).
    
    This fix only assumes a binops splats element is undefined if both operands are undef.
    
    Fixes #134602

--- llvm/lib/CodeGen/SelectionDAG/SelectionDAG.cpp
+++ llvm/lib/CodeGen/SelectionDAG/SelectionDAG.cpp
@@ -3002,9 +3002,12 @@ bool SelectionDAG::isSplatValue(SDValue V, const APInt &DemandedElts,
     APInt UndefLHS, UndefRHS;
     SDValue LHS = V.getOperand(0);
     SDValue RHS = V.getOperand(1);
+    // Only propagate common undef elts for both operands, otherwise we might
+    // fail to handle binop-specific undef handling.
+    // e.g. (and undef, 0) -> 0 etc.
     if (isSplatValue(LHS, DemandedElts, UndefLHS, Depth + 1) &&
         isSplatValue(RHS, DemandedElts, UndefRHS, Depth + 1)) {
-      UndefElts = UndefLHS | UndefRHS;
+      UndefElts = UndefLHS & UndefRHS;
       return true;
     }
     return false;
