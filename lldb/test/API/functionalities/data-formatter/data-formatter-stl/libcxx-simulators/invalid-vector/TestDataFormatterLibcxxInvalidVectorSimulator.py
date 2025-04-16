b194916ed677a6cbb3a604507b92e472d521a31a
Michael Buch <michaelbuch12@gmail.com>
2025-04-16 17:57:51 +0200

    [lldb][DataFormatter] Surface CalculateNumChildren errors in std::vector summary (#135944)
    When the data-formatters happen to break (e.g., due to layout changes in
    libc++), there's no clear indicator of them failing from a user's
    perspective. E.g., for `std::vector`s we would just show:
    ```
    (std::vector<int>) v = size=0 {}
    ```
    which is highly misleading, especially if `v.size()` returns a non-zero
    size.
    
    This patch surfaces the various errors that could occur when calculating
    the number of children of a vector.
    
    rdar://146964266

---
+++ lldb/test/API/functionalities/data-formatter/data-formatter-stl/libcxx-simulators/invalid-vector/TestDataFormatterLibcxxInvalidVectorSimulator.py
@@ -0,0 +1,39 @@
+"""
+Test we can understand various layouts of the libc++'s std::string
+"""
+
+
+import lldb
+from lldbsuite.test.decorators import *
+from lldbsuite.test.lldbtest import *
+from lldbsuite.test import lldbutil
+import functools
+
+
+class LibcxxInvalidVectorDataFormatterSimulatorTestCase(TestBase):
+    NO_DEBUG_INFO_TESTCASE = True
+
+    def test(self):
+        self.build()
+        lldbutil.run_to_source_breakpoint(self, "return 0", lldb.SBFileSpec("main.cpp"))
+
+        self.expect(
+            "frame variable v1",
+            substrs=["size=error: Invalid value for end of vector."],
+        )
+        self.expect(
+            "frame variable v2",
+            substrs=["size=error: Invalid value for start of vector."],
+        )
+        self.expect(
+            "frame variable v3",
+            substrs=["size=error: Start of vector data begins after end pointer."],
+        )
+        self.expect(
+            "frame variable v4",
+            substrs=["size=error: Failed to determine start/end of vector data."],
+        )
+        self.expect(
+            "frame variable v5",
+            substrs=["size=error: Size not multiple of element size."],
+        )
