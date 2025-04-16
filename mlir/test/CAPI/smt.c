1e38023fdb3b45dfce076d7e01bc1cf1161a5c69
Maksim Levental <maksim.levental@gmail.com>
2025-04-16 18:17:09 -0400

    [mlir][SMT] add python bindings (#135674)
    This PR adds "rich" python bindings to SMT dialect.

--- mlir/test/CAPI/smt.c
+++ mlir/test/CAPI/smt.c
@@ -34,7 +34,8 @@ void testExportSMTLIB(MlirContext ctx) {
   MlirModule module =
       mlirModuleCreateParse(ctx, mlirStringRefCreateFromCString(testSMT));
 
-  MlirLogicalResult result = mlirExportSMTLIB(module, dumpCallback, NULL);
+  MlirLogicalResult result =
+      mlirTranslateModuleToSMTLIB(module, dumpCallback, NULL, false, false);
   (void)result;
   assert(mlirLogicalResultIsSuccess(result));
 
@@ -44,13 +45,13 @@ void testExportSMTLIB(MlirContext ctx) {
 }
 
 void testSMTType(MlirContext ctx) {
-  MlirType boolType = smtTypeGetBool(ctx);
-  MlirType intType = smtTypeGetInt(ctx);
-  MlirType arrayType = smtTypeGetArray(ctx, intType, boolType);
-  MlirType bvType = smtTypeGetBitVector(ctx, 32);
+  MlirType boolType = mlirSMTTypeGetBool(ctx);
+  MlirType intType = mlirSMTTypeGetInt(ctx);
+  MlirType arrayType = mlirSMTTypeGetArray(ctx, intType, boolType);
+  MlirType bvType = mlirSMTTypeGetBitVector(ctx, 32);
   MlirType funcType =
-      smtTypeGetSMTFunc(ctx, 2, (MlirType[]){intType, boolType}, boolType);
-  MlirType sortType = smtTypeGetSort(
+      mlirSMTTypeGetSMTFunc(ctx, 2, (MlirType[]){intType, boolType}, boolType);
+  MlirType sortType = mlirSMTTypeGetSort(
       ctx, mlirIdentifierGet(ctx, mlirStringRefCreateFromCString("sort")), 0,
       NULL);
 
@@ -68,107 +69,107 @@ void testSMTType(MlirContext ctx) {
   mlirTypeDump(sortType);
 
   // CHECK: bool_is_any_non_func_smt_value_type
-  fprintf(stderr, smtTypeIsAnyNonFuncSMTValueType(boolType)
+  fprintf(stderr, mlirSMTTypeIsAnyNonFuncSMTValueType(boolType)
                       ? "bool_is_any_non_func_smt_value_type\n"
                       : "bool_is_func_smt_value_type\n");
   // CHECK: int_is_any_non_func_smt_value_type
-  fprintf(stderr, smtTypeIsAnyNonFuncSMTValueType(intType)
+  fprintf(stderr, mlirSMTTypeIsAnyNonFuncSMTValueType(intType)
                       ? "int_is_any_non_func_smt_value_type\n"
                       : "int_is_func_smt_value_type\n");
   // CHECK: array_is_any_non_func_smt_value_type
-  fprintf(stderr, smtTypeIsAnyNonFuncSMTValueType(arrayType)
+  fprintf(stderr, mlirSMTTypeIsAnyNonFuncSMTValueType(arrayType)
                       ? "array_is_any_non_func_smt_value_type\n"
                       : "array_is_func_smt_value_type\n");
   // CHECK: bit_vector_is_any_non_func_smt_value_type
-  fprintf(stderr, smtTypeIsAnyNonFuncSMTValueType(bvType)
+  fprintf(stderr, mlirSMTTypeIsAnyNonFuncSMTValueType(bvType)
                       ? "bit_vector_is_any_non_func_smt_value_type\n"
                       : "bit_vector_is_func_smt_value_type\n");
   // CHECK: sort_is_any_non_func_smt_value_type
-  fprintf(stderr, smtTypeIsAnyNonFuncSMTValueType(sortType)
+  fprintf(stderr, mlirSMTTypeIsAnyNonFuncSMTValueType(sortType)
                       ? "sort_is_any_non_func_smt_value_type\n"
                       : "sort_is_func_smt_value_type\n");
   // CHECK: smt_func_is_func_smt_value_type
-  fprintf(stderr, smtTypeIsAnyNonFuncSMTValueType(funcType)
+  fprintf(stderr, mlirSMTTypeIsAnyNonFuncSMTValueType(funcType)
                       ? "smt_func_is_any_non_func_smt_value_type\n"
                       : "smt_func_is_func_smt_value_type\n");
 
   // CHECK: bool_is_any_smt_value_type
-  fprintf(stderr, smtTypeIsAnySMTValueType(boolType)
+  fprintf(stderr, mlirSMTTypeIsAnySMTValueType(boolType)
                       ? "bool_is_any_smt_value_type\n"
                       : "bool_is_not_any_smt_value_type\n");
   // CHECK: int_is_any_smt_value_type
-  fprintf(stderr, smtTypeIsAnySMTValueType(intType)
+  fprintf(stderr, mlirSMTTypeIsAnySMTValueType(intType)
                       ? "int_is_any_smt_value_type\n"
                       : "int_is_not_any_smt_value_type\n");
   // CHECK: array_is_any_smt_value_type
-  fprintf(stderr, smtTypeIsAnySMTValueType(arrayType)
+  fprintf(stderr, mlirSMTTypeIsAnySMTValueType(arrayType)
                       ? "array_is_any_smt_value_type\n"
                       : "array_is_not_any_smt_value_type\n");
   // CHECK: array_is_any_smt_value_type
-  fprintf(stderr, smtTypeIsAnySMTValueType(bvType)
+  fprintf(stderr, mlirSMTTypeIsAnySMTValueType(bvType)
                       ? "array_is_any_smt_value_type\n"
                       : "array_is_not_any_smt_value_type\n");
   // CHECK: smt_func_is_any_smt_value_type
-  fprintf(stderr, smtTypeIsAnySMTValueType(funcType)
+  fprintf(stderr, mlirSMTTypeIsAnySMTValueType(funcType)
                       ? "smt_func_is_any_smt_value_type\n"
                       : "smt_func_is_not_any_smt_value_type\n");
   // CHECK: sort_is_any_smt_value_type
-  fprintf(stderr, smtTypeIsAnySMTValueType(sortType)
+  fprintf(stderr, mlirSMTTypeIsAnySMTValueType(sortType)
                       ? "sort_is_any_smt_value_type\n"
                       : "sort_is_not_any_smt_value_type\n");
 
   // CHECK: int_type_is_not_a_bool
-  fprintf(stderr, smtTypeIsABool(intType) ? "int_type_is_a_bool\n"
-                                          : "int_type_is_not_a_bool\n");
+  fprintf(stderr, mlirSMTTypeIsABool(intType) ? "int_type_is_a_bool\n"
+                                              : "int_type_is_not_a_bool\n");
   // CHECK: bool_type_is_not_a_int
-  fprintf(stderr, smtTypeIsAInt(boolType) ? "bool_type_is_a_int\n"
-                                          : "bool_type_is_not_a_int\n");
+  fprintf(stderr, mlirSMTTypeIsAInt(boolType) ? "bool_type_is_a_int\n"
+                                              : "bool_type_is_not_a_int\n");
   // CHECK: bv_type_is_not_a_array
-  fprintf(stderr, smtTypeIsAArray(bvType) ? "bv_type_is_a_array\n"
-                                          : "bv_type_is_not_a_array\n");
+  fprintf(stderr, mlirSMTTypeIsAArray(bvType) ? "bv_type_is_a_array\n"
+                                              : "bv_type_is_not_a_array\n");
   // CHECK: array_type_is_not_a_bit_vector
-  fprintf(stderr, smtTypeIsABitVector(arrayType)
+  fprintf(stderr, mlirSMTTypeIsABitVector(arrayType)
                       ? "array_type_is_a_bit_vector\n"
                       : "array_type_is_not_a_bit_vector\n");
   // CHECK: sort_type_is_not_a_smt_func
-  fprintf(stderr, smtTypeIsASMTFunc(sortType)
+  fprintf(stderr, mlirSMTTypeIsASMTFunc(sortType)
                       ? "sort_type_is_a_smt_func\n"
                       : "sort_type_is_not_a_smt_func\n");
   // CHECK: func_type_is_not_a_sort
-  fprintf(stderr, smtTypeIsASort(funcType) ? "func_type_is_a_sort\n"
-                                           : "func_type_is_not_a_sort\n");
+  fprintf(stderr, mlirSMTTypeIsASort(funcType) ? "func_type_is_a_sort\n"
+                                               : "func_type_is_not_a_sort\n");
 }
 
 void testSMTAttribute(MlirContext ctx) {
   // CHECK: slt_is_BVCmpPredicate
-  fprintf(stderr,
-          smtAttrCheckBVCmpPredicate(ctx, mlirStringRefCreateFromCString("slt"))
-              ? "slt_is_BVCmpPredicate\n"
-              : "slt_is_not_BVCmpPredicate\n");
+  fprintf(stderr, mlirSMTAttrCheckBVCmpPredicate(
+                      ctx, mlirStringRefCreateFromCString("slt"))
+                      ? "slt_is_BVCmpPredicate\n"
+                      : "slt_is_not_BVCmpPredicate\n");
   // CHECK: lt_is_not_BVCmpPredicate
-  fprintf(stderr,
-          smtAttrCheckBVCmpPredicate(ctx, mlirStringRefCreateFromCString("lt"))
-              ? "lt_is_BVCmpPredicate\n"
-              : "lt_is_not_BVCmpPredicate\n");
+  fprintf(stderr, mlirSMTAttrCheckBVCmpPredicate(
+                      ctx, mlirStringRefCreateFromCString("lt"))
+                      ? "lt_is_BVCmpPredicate\n"
+                      : "lt_is_not_BVCmpPredicate\n");
   // CHECK: slt_is_not_IntPredicate
-  fprintf(stderr,
-          smtAttrCheckIntPredicate(ctx, mlirStringRefCreateFromCString("slt"))
-              ? "slt_is_IntPredicate\n"
-              : "slt_is_not_IntPredicate\n");
+  fprintf(stderr, mlirSMTAttrCheckIntPredicate(
+                      ctx, mlirStringRefCreateFromCString("slt"))
+                      ? "slt_is_IntPredicate\n"
+                      : "slt_is_not_IntPredicate\n");
   // CHECK: lt_is_IntPredicate
-  fprintf(stderr,
-          smtAttrCheckIntPredicate(ctx, mlirStringRefCreateFromCString("lt"))
-              ? "lt_is_IntPredicate\n"
-              : "lt_is_not_IntPredicate\n");
+  fprintf(stderr, mlirSMTAttrCheckIntPredicate(
+                      ctx, mlirStringRefCreateFromCString("lt"))
+                      ? "lt_is_IntPredicate\n"
+                      : "lt_is_not_IntPredicate\n");
 
   // CHECK: #smt.bv<5> : !smt.bv<32>
-  mlirAttributeDump(smtAttrGetBitVector(ctx, 5, 32));
+  mlirAttributeDump(mlirSMTAttrGetBitVector(ctx, 5, 32));
   // CHECK: 0 : i64
   mlirAttributeDump(
-      smtAttrGetBVCmpPredicate(ctx, mlirStringRefCreateFromCString("slt")));
+      mlirSMTAttrGetBVCmpPredicate(ctx, mlirStringRefCreateFromCString("slt")));
   // CHECK: 0 : i64
   mlirAttributeDump(
-      smtAttrGetIntPredicate(ctx, mlirStringRefCreateFromCString("lt")));
+      mlirSMTAttrGetIntPredicate(ctx, mlirStringRefCreateFromCString("lt")));
 }
 
 int main(void) {
