f2c50573773471d35bffb4214117d21c7075d737
ImoutoCon1999 <ImoutoCon1999@outlook.com>
2025-04-16 19:33:45 +0800

    add banner

--- libcxx/test/libcxx/ranges/range.utility/range.utility.conv/to.verify.cpp
+++ libcxx/test/libcxx/ranges/range.utility/range.utility.conv/to.verify.cpp
@@ -1,3 +1,15 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++03, c++11, c++14, c++17, c++20
+
+
+// Test that the "mandates" requirements on the given class are checked using `static_assert`.
 #include <ranges>
 
 void test() {
