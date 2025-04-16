ec390f06db525e1eddb53ac60bd154e3cb23ebd7
YunQiang Su <yunqiang@isrc.iscas.ac.cn>
2025-04-16 18:10:21 +0800

    CodeGen: Add ISD::AssertNoFPClass
    It is used to mark a value that we are sure that it is not some fcType.
    The examples include:
      * An arguments of a function is marked with nofpclass
      * Output value of an intrinsic can be sure to not be some type
    So that the following operation can make some assumptions.

--- llvm/lib/CodeGen/SelectionDAG/SelectionDAGISel.cpp
+++ llvm/lib/CodeGen/SelectionDAG/SelectionDAGISel.cpp
@@ -3264,6 +3264,7 @@ void SelectionDAGISel::SelectCodeCommon(SDNode *NodeToMatch,
     return;
   case ISD::AssertSext:
   case ISD::AssertZext:
+  case ISD::AssertNoFPClass:
   case ISD::AssertAlign:
     ReplaceUses(SDValue(NodeToMatch, 0), NodeToMatch->getOperand(0));
     CurDAG->RemoveDeadNode(NodeToMatch);
