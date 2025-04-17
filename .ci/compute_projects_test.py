c5d73a2105f9a9261bc08f02019796061f871d19
Aiden Grossman <aidengrossman@google.com>
2025-04-17 05:53:31 +0000

    Revert "[CI] monolithic-linux improvements (#135499)"
    This reverts commit a399c6926a8701083c767cbb041e22ff92e9d717.
    
    This is causing some premerge workflow failures.
    
    Examople:
    https://buildkite.com/llvm-project/github-pull-requests/builds/169129#01963d1d-dc75-4b4c-9952-fb60efbf91b4

--- .ci/compute_projects_test.py
+++ .ci/compute_projects_test.py
@@ -188,19 +188,6 @@ class TestComputeProjects(unittest.TestCase):
         self.assertEqual(env_variables["runtimes_to_build"], "")
         self.assertEqual(env_variables["runtimes_check_targets"], "")
 
-     def test_ci(self):
-        env_variables = compute_projects.get_env_variables(
-            [".ci/compute_projects.py"], "Linux"
-        )
-        self.assertEqual(env_variables["projects_to_build"],
-                         "clang;lld;llvm;lldb")
-        self.assertEqual(env_variables["project_check_targets"], "check-clang
-                         check-lld check-llvm check-lldb")
-        self.assertEqual(env_variables["runtimes_to_build"],
-                         "libcxx;libcxxabi;libunwind")
-        self.assertEqual(env_variables["runtimes_check_targets"], "check-cxx
-                         check-cxxabi check-unwind")
-
 
 if __name__ == "__main__":
     unittest.main()
