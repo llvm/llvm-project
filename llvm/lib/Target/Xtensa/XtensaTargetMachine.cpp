b1177ed39d1f0e7f0274540bf4a001ae6bbabbcb
Andrei Safronov <safronov@espressif.com>
2025-04-17 08:45:04 +0300

    [Xtensa] Implement Xtensa S32C1I Option and atomics lowering.
    Implement Xtensa S32C1I Option and use s32c1i instruction to implement atomics
    operations.

--- llvm/lib/Target/Xtensa/XtensaTargetMachine.cpp
+++ llvm/lib/Target/Xtensa/XtensaTargetMachine.cpp
@@ -104,6 +104,7 @@ public:
   }
 
   bool addInstSelector() override;
+  void addIRPasses() override;
   void addPreEmitPass() override;
 };
 } // end anonymous namespace
@@ -111,6 +112,11 @@ public:
 bool XtensaPassConfig::addInstSelector() {
   addPass(createXtensaISelDag(getXtensaTargetMachine(), getOptLevel()));
   return false;
+}
+
+void XtensaPassConfig::addIRPasses() {
+  addPass(createAtomicExpandLegacyPass());
+  TargetPassConfig::addIRPasses();
 }
 
 void XtensaPassConfig::addPreEmitPass() { addPass(&BranchRelaxationPassID); }
