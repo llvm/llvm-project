e915153f1f2ed01e467fcf1463016d5482776ac6
Gábor Tóthvári <gabor.tothvari@ericsson.com>
2025-04-17 13:37:07 +0200

    Properly stub the pthread.h header in the tests.

--- clang-tools-extra/test/clang-tidy/checkers/bugprone/tagged-union-member-count.m
+++ clang-tools-extra/test/clang-tidy/checkers/bugprone/tagged-union-member-count.m
@@ -1,4 +1,5 @@
-// RUN: %check_clang_tidy %s bugprone-tagged-union-member-count %t
+// RUN: %check_clang_tidy %s bugprone-tagged-union-member-count %t -- -- \
+// RUN: -isystem %S/Inputs/tagged-union-member-count/system
 
 typedef enum Tags3 {
   tags3_1,
@@ -150,7 +151,7 @@ DECLARE_TAGGED_UNION_STRUCT(Tags3, Union4, TaggedUnionStructFromMacro);
 
 // Typedefed unions from system header files should be ignored when
 // we are trying to pinpoint the union part in a user-defined tagged union.
-#include "pthread.h"
+#include <pthread.h>
 
 // This should not be analyzed as a user-defined tagged union,
 // even though pthread_mutex_t may be declared as a typedefed union.
