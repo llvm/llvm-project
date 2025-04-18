64de8528dbd89cdafe32d754f37cf3da4fb5403e
Finn Plummer <canadienfinn@gmail.com>
2025-04-17 20:50:51 -0700

    [HLSL][RootSignature] Implement initial parsing of the descriptor table clause params (#133800)
    - Defines `ParseDescriptorTableClauseParams` to establish the pattern of
    how we will parse parameters in root signatures. Namely, to use
    recursive descent parsing in a way that follows closely to the EBNF
    notation definition in the root signature spec.
    
    - Implements parsing of two param types: `UInt32` and `Register` to
    demonstrate the parsing implementation and allow for unit testing
    
    - Changes the calling convention to use `std::optional` return values
    instead of boolean error returns and parameters by reference
    
    Part two of implementing:
    https://github.com/llvm/llvm-project/issues/126569
    
    ---------
    
    Co-authored-by: Finn Plummer <finnplummer@microsoft.com>

--- clang/include/clang/Parse/ParseHLSLRootSignature.h
+++ clang/include/clang/Parse/ParseHLSLRootSignature.h
@@ -40,26 +40,31 @@ public:
 private:
   DiagnosticsEngine &getDiags() { return PP.getDiagnostics(); }
 
-  // All private Parse.* methods follow a similar pattern:
+  // All private parse.* methods follow a similar pattern:
   //   - Each method will start with an assert to denote what the CurToken is
   // expected to be and will parse from that token forward
   //
   //   - Therefore, it is the callers responsibility to ensure that you are
   // at the correct CurToken. This should be done with the pattern of:
   //
-  //  if (TryConsumeExpectedToken(RootSignatureToken::Kind))
-  //    if (Parse.*())
-  //      return true;
+  //  if (tryConsumeExpectedToken(RootSignatureToken::Kind)) {
+  //    auto ParsedObject = parse.*();
+  //    if (!ParsedObject.has_value())
+  //      return std::nullopt;
+  //    ...
+  // }
   //
   // or,
   //
-  //  if (ConsumeExpectedToken(RootSignatureToken::Kind, ...))
-  //    return true;
-  //  if (Parse.*())
-  //    return true;
+  //  if (consumeExpectedToken(RootSignatureToken::Kind, ...))
+  //    return std::nullopt;
+  //  auto ParsedObject = parse.*();
+  //  if (!ParsedObject.has_value())
+  //    return std::nullopt;
+  //  ...
   //
-  //   - All methods return true if a parsing error is encountered. It is the
-  // callers responsibility to propogate this error up, or deal with it
+  //   - All methods return std::nullopt if a parsing error is encountered. It
+  // is the callers responsibility to propogate this error up, or deal with it
   // otherwise
   //
   //   - An error will be raised if the proceeding tokens are not what is
@@ -68,6 +73,23 @@ private:
   /// Root Element parse methods:
   bool parseDescriptorTable();
   bool parseDescriptorTableClause();
+
+  /// Parameter arguments (eg. `bReg`, `space`, ...) can be specified in any
+  /// order and only exactly once. `ParsedClauseParams` denotes the current
+  /// state of parsed params
+  struct ParsedClauseParams {
+    std::optional<llvm::hlsl::rootsig::Register> Register;
+    std::optional<uint32_t> Space;
+  };
+  std::optional<ParsedClauseParams>
+  parseDescriptorTableClauseParams(RootSignatureToken::Kind RegType);
+
+  std::optional<uint32_t> parseUIntParam();
+  std::optional<llvm::hlsl::rootsig::Register> parseRegister();
+
+  /// Use NumericLiteralParser to convert CurToken.NumSpelling into a unsigned
+  /// 32-bit integer
+  std::optional<uint32_t> handleUIntLiteral();
 
   /// Invoke the Lexer to consume a token and update CurToken with the result
   void consumeNextToken() { CurToken = Lexer.consumeToken(); }
