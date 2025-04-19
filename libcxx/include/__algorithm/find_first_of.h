91305de32d41305e6c5110860cc8254ab3ea8b9a
aliraeisdanaei <mrrookie2@gmail.com>
2025-04-19 19:26:39 -0400

    correct the naming of the iterators (stupid mistake)

--- libcxx/include/__algorithm/find_first_of.h
+++ libcxx/include/__algorithm/find_first_of.h
@@ -26,8 +26,8 @@ _LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX14 _ForwardIterator1 __find_fir
     _ForwardIterator2 __first2,
     _ForwardIterator2 __last2,
     _BinaryPredicate&& __pred) {
-  return std::find_if(first1, last1, [&](const auto& x) {
-    return std::any_of(first2, last2, [&](const auto& y) { return __pred(x, y); });
+  return std::find_if(__first1, __last1, [&](const auto& x) {
+    return std::any_of(__first2, __last2, [&](const auto& y) { return __pred(x, y); });
   });
 }
 
