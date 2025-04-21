6c2ce361a18ae19d6453a0ac8d717ade0d31091c
Owen Anderson <resistor@mac.com>
2025-04-22 10:23:27 +1200

    Reapply 6d1aae5 "FunctionDecl::getFunctionTypeLoc: ignore function type attributes (#118420)" (#136484)
    Avoid using PreservesMost in the testcase as it is not supported on all
    targets.
    
    Original PR #118290.
    
    Co-authored-by: Robert Dazi <14996868+v01dXYZ@users.noreply.github.com>
    Co-authored-by: v01dxyz <v01dxyz@v01d.xyz>

--- clang/lib/AST/Decl.cpp
+++ clang/lib/AST/Decl.cpp
@@ -3910,8 +3910,25 @@ bool FunctionDecl::doesDeclarationForceExternallyVisibleDefinition() const {
 
 FunctionTypeLoc FunctionDecl::getFunctionTypeLoc() const {
   const TypeSourceInfo *TSI = getTypeSourceInfo();
-  return TSI ? TSI->getTypeLoc().IgnoreParens().getAs<FunctionTypeLoc>()
-             : FunctionTypeLoc();
+
+  if (!TSI)
+    return FunctionTypeLoc();
+
+  TypeLoc TL = TSI->getTypeLoc();
+  FunctionTypeLoc FTL;
+
+  while (!(FTL = TL.getAs<FunctionTypeLoc>())) {
+    if (const auto PTL = TL.getAs<ParenTypeLoc>())
+      TL = PTL.getInnerLoc();
+    else if (const auto ATL = TL.getAs<AttributedTypeLoc>())
+      TL = ATL.getEquivalentTypeLoc();
+    else if (const auto MQTL = TL.getAs<MacroQualifiedTypeLoc>())
+      TL = MQTL.getInnerLoc();
+    else
+      break;
+  }
+
+  return FTL;
 }
 
 SourceRange FunctionDecl::getReturnTypeSourceRange() const {
