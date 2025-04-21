c8639a51d8a035644b72211e85f092e0eecd648e
Ben Shi <bennshi@tencent.com>
2025-04-21 15:32:19 +0800

    [AVR][NFC] Supplement a test for inline assembly constraint 'I'

--- clang/lib/Basic/Targets/AVR.h
+++ clang/lib/Basic/Targets/AVR.h
@@ -124,6 +124,8 @@ public:
       Info.setAllowsRegister();
       return true;
     case 'I': // 6-bit positive integer constant
+      // Due to issue https://github.com/llvm/llvm-project/issues/51513, we
+      // allow value 64 in the frontend and let it be dinied in the backend.
       Info.setRequiresImmediate(0, 64);
       return true;
     case 'J': // 6-bit negative integer constant
