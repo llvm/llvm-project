0cef3d4868445e301773187c37096f7a96fafb0c
Koakuma <koachan@protonmail.com>
2025-04-23 04:33:06 +0700

    Apply suggestions

--- compiler-rt/lib/builtins/clzdi2.c
+++ compiler-rt/lib/builtins/clzdi2.c
@@ -17,9 +17,9 @@
 #if ((defined(__sparc__) && defined(__arch64__)) || defined(__mips64) ||       \
      (defined(__riscv) && __SIZEOF_POINTER__ >= 8))
 // On 64-bit architectures with neither a native clz instruction nor a native
-// ctz instruction, resolves __builtin_clz resolves to __clzdi2 rather than
-// __clzsi2, leading to infinite recursion.
-// This is because on those platforms, libgcc doesn't ship with __clzsi2.
+// ctz instruction, `__builtin_clz` resolves to `__clzdi2` rather than
+// __clzsi2 as libgcc does not ship with `__clzsi2`, leading to infinite
+// recursion.
 #define __builtin_clz(a) __clzsi2(a)
 extern int __clzsi2(si_int);
 #endif
