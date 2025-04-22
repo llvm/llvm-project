e78694e52dfdcb6e2b085173952064a806d1d1a8
Sarnie, Nick <nick.sarnie@intel.com>
2025-04-22 12:57:36 -0700

    [clang][SPIR-V] Addrspace of opencl_global should always be 1
    Signed-off-by: Sarnie, Nick <nick.sarnie@intel.com>

--- clang/lib/Basic/Targets/SPIR.h
+++ clang/lib/Basic/Targets/SPIR.h
@@ -58,8 +58,8 @@ static const unsigned SPIRDefIsPrivMap[] = {
 // Used by both the SPIR and SPIR-V targets.
 static const unsigned SPIRDefIsGenMap[] = {
     4, // Default
-    // OpenCL address space values for this map are dummy and they can't be used
-    0, // opencl_global
+    // Some OpenCL address space values for this map are dummy and they can't be used
+    1, // opencl_global
     0, // opencl_local
     0, // opencl_constant
     0, // opencl_private
