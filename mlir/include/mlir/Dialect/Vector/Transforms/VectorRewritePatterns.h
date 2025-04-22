ed42443d93c30a4eec8fdb653bf44c7cb904700f
Ivan Butygin <ivan.butygin@gmail.com>
2025-04-22 16:18:54 +0200

    [mlir][vector] Sink vector.extract/splat into load/store ops (#134389)
    ```
    vector.load %arg0[%arg1] : memref<?xf32>, vector<4xf32>
    vector.extract %0[1] : f32 from vector<4xf32>
    ```
    Gets converted to:
    ```
    %c1 = arith.constant 1 : index
    %0 = arith.addi %arg1, %c1 overflow<nsw> : index
    %1 = memref.load %arg0[%0] : memref<?xf32>
    ```
    
    ```
    %0 = vector.splat %arg2 : vector<1xf32>
    vector.store %0, %arg0[%arg1] : memref<?xf32>, vector<1xf32>
    ```
    Gets converted to:
    ```
    memref.store %arg2, %arg0[%arg1] : memref<?xf32>
    ```

--- mlir/include/mlir/Dialect/Vector/Transforms/VectorRewritePatterns.h
+++ mlir/include/mlir/Dialect/Vector/Transforms/VectorRewritePatterns.h
@@ -161,6 +161,20 @@ void populateVectorTransferCollapseInnerMostContiguousDimsPatterns(
 void populateSinkVectorOpsPatterns(RewritePatternSet &patterns,
                                    PatternBenefit benefit = 1);
 
+/// Patterns that remove redundant Vector Ops by merging them with load/store
+/// ops
+/// ```
+/// vector.load %arg0[%arg1] : memref<?xf32>, vector<4xf32>
+/// vector.extract %0[1] : f32 from vector<4xf32>
+/// ```
+/// Gets converted to:
+/// ```
+/// %c1 = arith.constant 1 : index
+/// %0 = arith.addi %arg1, %c1 overflow<nsw> : index
+/// %1 = memref.load %arg0[%0] : memref<?xf32>
+void populateSinkVectorMemOpsPatterns(RewritePatternSet &patterns,
+                                      PatternBenefit benefit = 1);
+
 /// Patterns that fold chained vector reductions. These patterns assume that
 /// elementwise operations (e.g., `arith.addf` with vector operands) are
 /// cheaper than vector reduction.
