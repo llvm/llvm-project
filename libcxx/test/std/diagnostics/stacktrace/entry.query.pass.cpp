a1f04ace34e08db660d57f7fc98036e66518c190
Steve O'Brien <steve@obrien.cc>
2025-04-21 22:35:16 -0400

    Move stacktrace srcs out of experimental

--- libcxx/test/std/diagnostics/stacktrace/entry.query.pass.cpp
+++ libcxx/test/std/diagnostics/stacktrace/entry.query.pass.cpp
@@ -9,7 +9,7 @@
 // UNSUPPORTED: c++03, c++11, c++14, c++17, c++20
 // ADDITIONAL_COMPILE_FLAGS: -O0 -g -gsplit-dwarf
 
-#include <experimental/stacktrace>
+#include <stacktrace>
 
 #include <cassert>
 #include <iostream>
