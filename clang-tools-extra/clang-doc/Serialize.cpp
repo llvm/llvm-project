11d5c4dcccf19b5c01742a2a402889da89783630
Paul Kirth <paulkirth@google.com>
2025-04-22 12:23:45 -0700

    [clang-doc][NFC] Use isa<> over dyn_cast (#136445)

--- clang-tools-extra/clang-doc/Serialize.cpp
+++ clang-tools-extra/clang-doc/Serialize.cpp
@@ -246,9 +246,9 @@ static TypeInfo getTypeInfoForType(const QualType &T,
     return TypeInfo(Reference(SymbolID(), T.getAsString(Policy)));
 
   InfoType IT;
-  if (dyn_cast<EnumDecl>(TD)) {
+  if (isa<EnumDecl>(TD)) {
     IT = InfoType::IT_enum;
-  } else if (dyn_cast<RecordDecl>(TD)) {
+  } else if (isa<RecordDecl>(TD)) {
     IT = InfoType::IT_record;
   } else {
     IT = InfoType::IT_default;
