450c366c8dc6b0d351f34b66b132c707476f224c
Stanley Winata <stanley.winata@amd.com>
2025-04-23 08:09:02 -0700

    Revert "[MLIR][TableGen] Error on APInt parameter without custom comparator (#135970)"
    This reverts commit 4bcc414af3782c333f3d535c0e0a92e6120868f5.

--- mlir/include/mlir/TableGen/AttrOrTypeDef.h
+++ mlir/include/mlir/TableGen/AttrOrTypeDef.h
@@ -68,11 +68,7 @@ public:
   /// If specified, get the custom allocator code for this parameter.
   std::optional<StringRef> getAllocator() const;
 
-  /// Return true if user defined comparator is specified.
-  bool hasCustomComparator() const;
-
-  /// Get the custom comparator code for this parameter or fallback to the
-  /// default.
+  /// If specified, get the custom comparator code for this parameter.
   StringRef getComparator() const;
 
   /// Get the C++ type of this parameter.
