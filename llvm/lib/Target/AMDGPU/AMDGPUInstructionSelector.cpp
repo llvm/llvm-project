llvm/llvm-project

commit fbc8087f4f1da2c1d622b7bc8263935aa5eeef2d

[AMDGPU] RTS changes for the Unbiased Ray Origin (Tmin) support (td) (#1275)

llvm/lib/Target/AMDGPU/AMDGPUInstructionSelector.cpp
@@ -4301,6 +4301,11 @@ Register AMDGPUInstructionSelector::copyToVGPRIfSrcFolded(
   return Src;
 }
 
+InstructionSelector::ComplexRendererFns
+AMDGPUInstructionSelector::selectIgnore(MachineOperand &Root) const {
+  return {{}};
+}
+
 ///
 /// This will select either an SGPR or VGPR operand and will save us from
 /// having to write an extra tablegen pattern.

llvm/include/llvm/IR/IntrinsicsAMDGPU.td
@@ -3964,12 +3964,12 @@ def int_amdgcn_rts_ray_restore: Intrinsic<
 
 // llvm.amdgcn.rts.trace.ray <ray_init_data> <ray_init_flag> <ray_extent> <ray_origin> <ray_dir> <texture_descr>
 def int_amdgcn_rts_trace_ray : Intrinsic<[],
-    [llvm_i32_ty , llvm_v3i32_ty, llvm_float_ty, llvm_v3f32_ty, llvm_v3f32_ty, llvm_v4i32_ty],
+    [llvm_i32_ty, llvm_v3i32_ty, llvm_float_ty, llvm_anyfloat_ty, llvm_anyfloat_ty, llvm_v4i32_ty],
     [IntrHasSideEffects, IntrNoCallback, IntrWillReturn, IntrNoFree], "", [SDNPMemOperand]>, AMDGPURsrcIntrinsic<5>;
 
 // <vdata> llvm.amdgcn.rts.trace.ray <ray_init_data> <ray_init_flag> <ray_extent> <ray_origin> <ray_dir> <texture_descr>
 def int_amdgcn_rts_trace_ray_nonblock : Intrinsic<[llvm_i32_ty],
-    [llvm_i32_ty , llvm_v3i32_ty, llvm_float_ty, llvm_v3f32_ty, llvm_v3f32_ty, llvm_v4i32_ty],
+    [llvm_i32_ty, llvm_v3i32_ty, llvm_float_ty, llvm_anyfloat_ty, llvm_anyfloat_ty, llvm_v4i32_ty],
     [IntrHasSideEffects, IntrNoCallback, IntrWillReturn, IntrNoFree], "", [SDNPMemOperand]>, AMDGPURsrcIntrinsic<5>;
 
 // <vdata> llvm.amdgcn_rts_read_vertex <vaddr0> <vaddr1> <vaddr2>

llvm/lib/Target/AMDGPU/AMDGPUGISel.td
@@ -14,6 +14,11 @@ include "AMDGPU.td"
 include "AMDGPUCombine.td"
 
 def sd_vsrc0 : ComplexPattern<i32, 1, "">;
+
+def gi_ignore :
+    GIComplexOperandMatcher<s32, "selectIgnore">,
+    GIComplexPatternEquiv<Ignore>;
+
 def gi_vsrc0 :
     GIComplexOperandMatcher<s32, "selectVSRC0">,
     GIComplexPatternEquiv<sd_vsrc0>;

llvm/lib/Target/AMDGPU/AMDGPUISelDAGToDAG.cpp
@@ -4744,6 +4744,8 @@ bool AMDGPUDAGToDAGISel::SelectBITOP3(SDValue In, SDValue &Src0, SDValue &Src1,
   return true;
 }
 
+bool AMDGPUDAGToDAGISel::SelectIgnore(SDValue In) const { return true; }
+
 SDValue AMDGPUDAGToDAGISel::getHi16Elt(SDValue In) const {
   if (In.isUndef())
     return CurDAG->getUNDEF(MVT::i32);

llvm/lib/Target/AMDGPU/AMDGPUISelDAGToDAG.h
@@ -308,6 +308,8 @@ private:
   void SelectWAVE_ADDRESS(SDNode *N);
   void SelectSTACKRESTORE(SDNode *N);
 
+  bool SelectIgnore(SDValue In) const;
+
 protected:
   // Include the pieces autogenerated from the target description.
 #include "AMDGPUGenDAGISel.inc"

llvm/lib/Target/AMDGPU/AMDGPUInstructionSelector.h
@@ -169,6 +169,9 @@ private:
                                  bool ForceVGPR = false) const;
 
   InstructionSelector::ComplexRendererFns
+  selectIgnore(MachineOperand &Root) const;
+
+  InstructionSelector::ComplexRendererFns
   selectVCSRC(MachineOperand &Root) const;
 
   InstructionSelector::ComplexRendererFns

llvm/lib/Target/AMDGPU/MIMGInstructions.td
@@ -1798,7 +1798,10 @@ multiclass MIMG_MSAA_Load <mimgopc op, string asm> {
 }
 
 class VIMAGE_RTS_Helper<bit HasVdst, bit IsTraceRay, bit isPacket, bit isPrimOrCoords> {
-  list<RegisterClass> addr_types = !if(IsTraceRay, [VGPR_32, VReg_96, VGPR_32, VReg_96, VReg_96], !if(isPacket, [VGPR_32, VGPR_32], [VGPR_32, VGPR_32, VGPR_32]));
+  list<RegisterClass> addr_types =
+    !cond(IsTraceRay : [VGPR_32, VReg_96, VReg_128, VReg_128],
+          isPacket :   [VGPR_32, VGPR_32],
+          true     :   [VGPR_32, VGPR_32, VGPR_32]);
   int num_addrs = !size(addr_types);
   int VAddrDwords = !if(IsTraceRay, 11, !if(isPacket, 2, 3));
   int VDataDwords = !if(HasVdst, !if(IsTraceRay, 1, !if(isPacket, 1, !if(isPrimOrCoords, 3, 9))), 0);
@@ -1806,7 +1809,12 @@ class VIMAGE_RTS_Helper<bit HasVdst, bit IsTraceRay, bit isPacket, bit isPrimOrC
 
 class VIMAGE_RTS<int op, string opcode, int num_addrs, dag RsrcIns, GFXGen gfxGen, MIMGEncoding encoding,
                  list<RegisterClass> addr_types, bit HasVdst, bit IsTraceRay, bit isPacket, bit isPrimOrCoords>
-    : VIMAGE<op, !if(HasVdst, !if(IsTraceRay, (outs VGPR_32:$vdata), !if(isPacket, (outs VGPR_32:$vdata), !if(isPrimOrCoords, (outs VReg_96:$vdata), (outs VReg_288:$vdata)))), (outs)),
+    : VIMAGE<op,
+             !cond(!not(HasVdst)  : (outs),
+                   IsTraceRay     : (outs VGPR_32:$vdata),
+                   isPacket       : (outs VGPR_32:$vdata),
+                   isPrimOrCoords : (outs VReg_96:$vdata),
+                   true           : (outs VReg_288:$vdata)),
              num_addrs, gfxGen.AssemblerPredicate, encoding, gfxGen.DecoderNamespace, addr_types>{
   let InOperandList = !con(nsah.AddrIns, RsrcIns, (ins R128A16:$r128));
   let AsmString = opcode#!if(HasVdst, " $vdata, ", " ")#nsah.AddrAsm#!if(IsTraceRay, ", $rsrc", ", null")#"$r128";
@@ -2018,13 +2026,33 @@ let SubtargetPredicate = isGFX12Plus in {
 
 class RtsTraceRayPat<VIMAGE_RTS inst, SDPatternOperator node, ValueType vdata_vt = isVoid> : GCNPat<
   !if(!ne(vdata_vt, isVoid),
-      (vdata_vt:$vdata (node i32:$vaddr0, v3i32:$vaddr1, f32:$vaddr2, v3f32:$vaddr3, v3f32:$vaddr4, v4i32:$rsrc)),
-      (node i32:$vaddr0, v3i32:$vaddr1, f32:$vaddr2, v3f32:$vaddr3, v3f32:$vaddr4, v4i32:$rsrc)),
-  (inst $vaddr0, $vaddr1, $vaddr2, $vaddr3, $vaddr4, $rsrc, (i1 1))
+      (vdata_vt:$vdata (node i32:$ray_initial_data, v3i32:$ray_initial_flags, (Ignore), v4f32:$ray_origin, v4f32:$ray_direction, v4i32:$rsrc)),
+      (node i32:$ray_initial_data, v3i32:$ray_initial_flags, (Ignore), v4f32:$ray_origin, v4f32:$ray_direction, v4i32:$rsrc)),
+  (inst $ray_initial_data, $ray_initial_flags, $ray_origin, $ray_direction, $rsrc, (i1 1))
+>;
+
+// Convert v3f32 ray_origin and ray_direction to v4f32
+class RtsTraceRayPatV3F32<VIMAGE_RTS inst, SDPatternOperator node, ValueType vdata_vt = isVoid> : GCNPat<
+  !if(!ne(vdata_vt, isVoid),
+      (vdata_vt:$vdata (node i32:$ray_initial_data, v3i32:$ray_initial_flags, f32:$ray_extent, v3f32:$ray_origin, v3f32:$ray_direction, v4i32:$rsrc)),
+      (node i32:$ray_initial_data, v3i32:$ray_initial_flags, f32:$ray_extent, v3f32:$ray_origin, v3f32:$ray_direction, v4i32:$rsrc)),
+  (inst $ray_initial_data, $ray_initial_flags,
+        (REG_SEQUENCE VReg_128, (f32 (EXTRACT_SUBREG VReg_96:$ray_origin, sub0)), sub0,
+                                (f32 (EXTRACT_SUBREG VReg_96:$ray_origin, sub1)), sub1,
+                                (f32 (EXTRACT_SUBREG VReg_96:$ray_origin, sub2)), sub2,
+                                (f32 (V_MOV_B32_e32 (i32 0))), sub3), // tmin
+        (REG_SEQUENCE VReg_128, (f32 (EXTRACT_SUBREG VReg_96:$ray_direction, sub0)), sub0,
+                                (f32 (EXTRACT_SUBREG VReg_96:$ray_direction, sub1)), sub1,
+                                (f32 (EXTRACT_SUBREG VReg_96:$ray_direction, sub2)), sub2,
+                                $ray_extent, sub3), // tmax
+        $rsrc, (i1 1))
 >;
 
 def : RtsTraceRayPat <RTS_TRACE_RAY_gfx13, int_amdgcn_rts_trace_ray>;
 def : RtsTraceRayPat <RTS_TRACE_RAY_NONBLOCK_gfx13, int_amdgcn_rts_trace_ray_nonblock, i32>;
+
+def : RtsTraceRayPatV3F32 <RTS_TRACE_RAY_gfx13, int_amdgcn_rts_trace_ray>;
+def : RtsTraceRayPatV3F32 <RTS_TRACE_RAY_NONBLOCK_gfx13, int_amdgcn_rts_trace_ray_nonblock, i32>;
 
 def : GCNPat<
   (v9f32:$vdata (int_amdgcn_rts_read_vertex i64:$vaddr, i32:$vaddr2)),

llvm/lib/Target/AMDGPU/SIInstrInfo.td
@@ -1695,6 +1695,9 @@ def VOP3PMadMixBF16Mods : ComplexPattern<untyped, 2, "SelectVOP3PMadMixBF16Mods"
 def VINTERPMods  : ComplexPattern<untyped, 2, "SelectVINTERPMods">;
 def VINTERPModsHi  : ComplexPattern<untyped, 2, "SelectVINTERPModsHi">;
 
+// TODO-GFX13 : Remove this when old version of rts.trace.ray instrinsic is no longer generated
+def Ignore : ComplexPattern<untyped, 0, "SelectIgnore">;
+
 //===----------------------------------------------------------------------===//
 // SI assembler operands
 //===----------------------------------------------------------------------===//

commit 171aa2c3b8a9bc135fa45a495a7bbe52ff4d8b33

AMDGPU/GFX13: Fix build error (#1784)
Not sure why this wasn't caught earlier. It might be sensitive to the
compiler version? For me, there is an error because the std::in_place_t
constructor overload is selected, which implies use of an explicit
constructor.

Fixes: fbc8087f4f1da2c1d ("[AMDGPU] RTS changes for the Unbiased Ray
Origin (Tmin) support (td)")

llvm/lib/Target/AMDGPU/AMDGPUInstructionSelector.cpp
@@ -4303,7 +4303,10 @@ Register AMDGPUInstructionSelector::copyToVGPRIfSrcFolded(
 
 InstructionSelector::ComplexRendererFns
 AMDGPUInstructionSelector::selectIgnore(MachineOperand &Root) const {
-  return {{}};
+  // Don't render anything.
+  ComplexRendererFns Renderers;
+  Renderers.emplace();
+  return Renderers;
 }
 
 ///