f1b35e8ff093acaa3e41c5c9cba23b2b5346c19e
Mallikarjuna Gouda <mgouda@mips.com>
2025-04-23 01:40:16 -0700

    Enable MSA feature by default for MIPS cpus i6400 and i6500
    MIPS cpus support MSA by default. So enable this feature
    for cpus i6400 and i6500.
    
    Also test msa feature in mips-cpus.c driver test case

--- clang/test/Driver/mips-cpus.c
+++ clang/test/Driver/mips-cpus.c
@@ -1,9 +1,9 @@
 // Check target CPUs are correctly passed.
 
-// RUN: %clang --target=mips64 -### -c %s 2>&1 -mcpu=i6400 -mmsa | FileCheck -check-prefix=MCPU-I6400 %s
+// RUN: %clang --target=mips64 -### -c %s 2>&1 -mcpu=i6400 | FileCheck -check-prefix=MCPU-I6400 %s
 // MCPU-I6400: "-target-cpu" "i6400"
-// MCPU-I6400-SAME: "-target-feature" "+msa"
+// MCPU-I6400: "-target-feature" "+msa" "-target-feature" "-noabicalls"
 
-// RUN: %clang --target=mips64 -### -c %s 2>&1 -mcpu=i6500 -mmsa | FileCheck -check-prefix=MCPU-I6500 %s
+// RUN: %clang --target=mips64 -### -c %s 2>&1 -mcpu=i6500 | FileCheck -check-prefix=MCPU-I6500 %s
 // MCPU-I6500: "-target-cpu" "i6500"
-// MCPU-I6500-SAME: "-target-feature" "+msa"
+// MCPU-I6500: "-target-feature" "+msa" "-target-feature" "-noabicalls"
