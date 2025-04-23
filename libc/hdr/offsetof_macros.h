8fd8ba1f22e5692ebc0ab6c8f45273ca348565a5
Schrodinger ZHU Yifan <yifanzhu@rochester.edu>
2025-04-23 13:03:07 -0400

    Update libc/hdr/offsetof_macros.h
    Co-authored-by: Michael Jones <michaelrj@google.com>

--- libc/hdr/offsetof_macros.h
+++ libc/hdr/offsetof_macros.h
@@ -9,7 +9,15 @@
 #ifndef LLVM_LIBC_HDR_OFFSETOF_MACROS_H
 #define LLVM_LIBC_HDR_OFFSETOF_MACROS_H
 
+#ifdef LIBC_FULL_BUILD
+
+#include "include/llvm-libc-macros/offsetof-macros.h"
+
+#else // Overlay mode
+
 #define __need_offsetof
 #include <stddef.h>
+
+#endif // LLVM_LIBC_FULL_BUILD
 
 #endif // LLVM_LIBC_HDR_OFFSETOF_MACROS_H
