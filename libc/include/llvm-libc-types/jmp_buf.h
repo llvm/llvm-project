688cae639d4fcf942f1e050462dd6052723c878b
Schrodinger ZHU Yifan <yifanzhu@rochester.edu>
2025-04-23 14:19:47 -0400

    [libc] implement sigsetjmp/siglongjmp for x86-64 (#136072)

--- libc/include/llvm-libc-types/jmp_buf.h
+++ libc/include/llvm-libc-types/jmp_buf.h
@@ -9,6 +9,8 @@
 #ifndef LLVM_LIBC_TYPES_JMP_BUF_H
 #define LLVM_LIBC_TYPES_JMP_BUF_H
 
+#include "sigset_t.h"
+
 typedef struct {
 #ifdef __x86_64__
   __UINT64_TYPE__ rbx;
@@ -50,8 +52,21 @@ typedef struct {
 #else
 #error "__jmp_buf not available for your target architecture."
 #endif
+  // TODO: implement sigjmp_buf related functions for other architectures
+  // Issue: https://github.com/llvm/llvm-project/issues/136358
+#if defined(__i386__) || defined(__x86_64__)
+  // return address
+  void *sig_retaddr;
+  // extra register buffer to avoid indefinite stack growth in sigsetjmp
+  void *sig_extra;
+  // signal masks
+  sigset_t sigmask;
+#endif
 } __jmp_buf;
 
 typedef __jmp_buf jmp_buf[1];
 
+#if defined(__i386__) || defined(__x86_64__)
+typedef __jmp_buf sigjmp_buf[1];
+#endif
 #endif // LLVM_LIBC_TYPES_JMP_BUF_H
