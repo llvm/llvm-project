0de2f64e652a1b8c1e051635c98fb2b69c6b2c62
Alex Rønne Petersen <alex@alexrp.com>
2025-04-23 11:40:36 +0200

    [clang] XFAIL the `Xclangas.s` test on AIX. (#136744)
    Clang on AIX does not use the integrated assembler.
    
    https://github.com/llvm/llvm-project/pull/100714#issuecomment-2822056054

--- clang/test/Driver/Xclangas.s
+++ clang/test/Driver/Xclangas.s
@@ -2,3 +2,4 @@
 // RUN: %clang -### -Werror -Xclangas -target-feature -Xclangas=+v5t %s 2>&1 | FileCheck %s
 // CHECK: -cc1as
 // CHECK-SAME: "-target-feature" "+v5t"
+// XFAIL: target={{.*}}-aix{{.*}}
