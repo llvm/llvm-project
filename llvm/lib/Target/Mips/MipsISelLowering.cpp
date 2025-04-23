11d66917411a3522c59ba9694352c4018646b4d2
Ying Huang <ying.huang@oss.cipunited.com>
2025-04-22 22:29:52 -0400

    [Mips] Support "$sp" named register
    Fix #47656.

--- llvm/lib/Target/Mips/MipsISelLowering.cpp
+++ llvm/lib/Target/Mips/MipsISelLowering.cpp
@@ -4898,6 +4898,7 @@ MipsTargetLowering::getRegisterByName(const char *RegName, LLT VT,
     Register Reg = StringSwitch<Register>(RegName)
                        .Case("$28", Mips::GP_64)
                        .Case("sp", Mips::SP_64)
+                       .Case("$sp", Mips::SP_64)
                        .Default(Register());
     if (Reg)
       return Reg;
@@ -4905,6 +4906,7 @@ MipsTargetLowering::getRegisterByName(const char *RegName, LLT VT,
     Register Reg = StringSwitch<Register>(RegName)
                        .Case("$28", Mips::GP)
                        .Case("sp", Mips::SP)
+                       .Case("$sp", Mips::SP)
                        .Default(Register());
     if (Reg)
       return Reg;
