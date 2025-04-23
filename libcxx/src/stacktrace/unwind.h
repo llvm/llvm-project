9373a45152d11f2eea32e944e06101850397d9fe
Steve O'Brien <steve@obrien.cc>
2025-04-23 09:50:44 -0400

    simplify layout of stacktrace files: condense, collapse

--- libcxx/src/stacktrace/unwind.h
+++ libcxx/src/stacktrace/unwind.h
@@ -6,15 +6,31 @@
 //
 //===----------------------------------------------------------------------===//
 
+#ifndef _LIBCPP_STACKTRACE_UNWIND_H
+#define _LIBCPP_STACKTRACE_UNWIND_H
+
 #include <__config>
 #include <__config_site>
+#include <cstddef>
+#include <cstdlib>
 
-#include <__stacktrace/alloc.h>
+#include "stacktrace/config.h"
+#include "stacktrace/context.h"
+#include "stacktrace/utils.h"
+#include <__stacktrace/basic_stacktrace.h>
+#include <__stacktrace/stacktrace_entry.h>
 
 _LIBCPP_BEGIN_NAMESPACE_STD
 namespace __stacktrace {
 
-alloc::~alloc() {}
+struct context;
+
+struct unwind {
+  context& cx_;
+  void collect(size_t skip, size_t max_depth);
+};
 
 } // namespace __stacktrace
 _LIBCPP_END_NAMESPACE_STD
+
+#endif // _LIBCPP_STACKTRACE_UNWIND_H
