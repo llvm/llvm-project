b02b1e93dbb18fcb350d98557cfbd1052bfa7a60
Aaron Ballman <aaron@aaronballman.com>
2025-04-23 13:52:04 -0400

    Update the test
    Use a bookmark, per review request
    Add C++ RUN line to show we diagnose invalid constructs in C++

--- clang/test/Sema/implicit-void-ptr-cast.c
+++ clang/test/Sema/implicit-void-ptr-cast.c
@@ -1,5 +1,6 @@
-// RUN: %clang_cc1 -fsyntax-only -verify -Wimplicit-void-ptr-cast %s
-// RUN: %clang_cc1 -fsyntax-only -verify -Wc++-compat %s
+// RUN: %clang_cc1 -fsyntax-only -verify=c -Wimplicit-void-ptr-cast %s
+// RUN: %clang_cc1 -fsyntax-only -verify=c -Wc++-compat %s
+// RUN: %clang_cc1 -fsyntax-only -verify=cxx -x c++ %s
 // RUN: %clang_cc1 -fsyntax-only -verify=good %s
 // RUN: %clang_cc1 -fsyntax-only -verify=good -Wc++-compat -Wno-implicit-void-ptr-cast %s
 // good-no-diagnostics
@@ -7,22 +8,31 @@
 typedef __typeof__(sizeof(int)) size_t;
 extern void *malloc(size_t);
 
-void func(int *); // expected-note {{passing argument to parameter here}}
+void func(int *); // #func-param
 
 void test(void) {
-  int *x = malloc(sizeof(char)); // expected-warning {{implicit conversion when initializing 'int *' with an expression of type 'void *' is not permitted in C++}}
-  x = malloc(sizeof(char)); // expected-warning {{implicit conversion when assigning to 'int *' from type 'void *' is not permitted in C++}}
-  func(malloc(sizeof(char))); // expected-warning {{implicit conversion when passing 'void *' to parameter of type 'int *' is not permitted in C++}}
+  int *x = malloc(sizeof(char)); // c-warning {{implicit conversion when initializing 'int *' with an expression of type 'void *' is not permitted in C++}} \
+                                    cxx-error {{cannot initialize a variable of type 'int *' with an rvalue of type 'void *'}}
+  x = malloc(sizeof(char));      // c-warning {{implicit conversion when assigning to 'int *' from type 'void *' is not permitted in C++}} \
+                                    cxx-error {{assigning to 'int *' from incompatible type 'void *'}}
+  func(malloc(sizeof(char)));    // c-warning {{implicit conversion when passing 'void *' to parameter of type 'int *' is not permitted in C++}} \
+                                    c-note@#func-param {{passing argument to parameter here}} \
+                                    cxx-error {{no matching function for call to 'func'}} \
+                                    cxx-note@#func-param {{candidate function not viable: cannot convert argument of incomplete type 'void *' to 'int *' for 1st argument}}
   x = (int *)malloc(sizeof(char));
 
   void *vp = 0;
-  x = vp; // expected-warning {{implicit conversion when assigning to 'int *' from type 'void *' is not permitted in C++}}
+  x = vp; // c-warning {{implicit conversion when assigning to 'int *' from type 'void *' is not permitted in C++}} \
+             cxx-error {{assigning to 'int *' from incompatible type 'void *'}}
   vp = vp;
 
-  x = (void *)malloc(sizeof(char)); // expected-warning {{implicit conversion when assigning to 'int *' from type 'void *' is not permitted in C++}}
-  const int *y = vp; // expected-warning {{implicit conversion when initializing 'const int *' with an expression of type 'void *' is not permitted in C++}}
+  x = (void *)malloc(sizeof(char)); // c-warning {{implicit conversion when assigning to 'int *' from type 'void *' is not permitted in C++}} \
+                                       cxx-error {{assigning to 'int *' from incompatible type 'void *'}}
+  const int *y = vp;                // c-warning {{implicit conversion when initializing 'const int *' with an expression of type 'void *' is not permitted in C++}} \
+                                       cxx-error {{cannot initialize a variable of type 'const int *' with an lvalue of type 'void *'}}
 }
 
 int *other_func(void *ptr) {
-  return ptr; // expected-warning {{implicit conversion when returning 'void *' from a function with result type 'int *' is not permitted in C++}}
+  return ptr; // c-warning {{implicit conversion when returning 'void *' from a function with result type 'int *' is not permitted in C++}} \
+                 cxx-error {{cannot initialize return object of type 'int *' with an lvalue of type 'void *'}}
 }
