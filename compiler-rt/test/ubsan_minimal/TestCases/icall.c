3c490c0a2a5b51058478c39562d3a1a6bde1c138
Vitaly Buka <vitalybuka@google.com>
2025-04-24 12:51:34 -0700

    [𝘀𝗽𝗿] initial version
    Created using spr 1.3.4

---
+++ compiler-rt/test/ubsan_minimal/TestCases/icall.c
@@ -0,0 +1,11 @@
+// RUN: %clang -fsanitize=cfi-icall -fno-sanitize-trap=cfi-icall -flto -fvisibility=hidden %s -o %t && not %run %t 2>&1 | FileCheck %s
+
+// RUN: %clang -fsanitize=cfi-icall -fno-sanitize-trap=cfi-icall -fsanitize-recover=cfi-icall -flto -fvisibility=hidden %s -o %t &&  %run %t 2>&1 | FileCheck %s
+
+void f() {
+}
+
+int main() {
+  // CHECK: ubsan: cfi-check-fail by 0x
+  ((void (*)(int))f)(42);
+}
