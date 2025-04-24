c2506b5fb751fc0869ff0aef1689a36c4e79a58c
sgokhale <sgokhale@nvidia.com>
2025-04-24 03:35:33 -0700

    [AArch64][SVE] Generate asrd instruction for positive pow-2 divisors when SVE is available
    Currently,
              sdiv(x, y)  --> cmlt + usra + sshr      , where `y` is positive pow-2 integer
    
    Patch aims to transform this into
    
              sdiv(x, y)  --> ptrue + asrd            , where `y` is positive pow-2 integer

--- llvm/lib/Target/AArch64/AArch64ISelLowering.cpp
+++ llvm/lib/Target/AArch64/AArch64ISelLowering.cpp
@@ -18421,6 +18421,11 @@ AArch64TargetLowering::BuildSDIVPow2(SDNode *N, const APInt &Divisor,
 
   EVT VT = N->getValueType(0);
 
+  // For negative divisor, this yeilds (ptrue + asrd + subr) which is not
+  // profitable as compared to Neon sequence (cmlt + usra + sshr).
+  if (Subtarget->hasSVE() && !Divisor.isNegatedPowerOf2())
+    return SDValue(N, 0);
+
   // For scalable and fixed types, mark them as cheap so we can handle it much
   // later. This allows us to handle larger than legal types.
   if (VT.isScalableVector() ||
