79508a757168a8033a2369c9eb232ee67987d623
David Spickett <david.spickett@linaro.org>
2025-04-24 12:16:29 +0000

    [lldb] Disable reverse continue command test on Windows
    The new test added in https://github.com/llvm/llvm-project/pull/132783
    is timing out on our Windows on Arm bot
    https://lab.llvm.org/buildbot/#/builders/141/builds/8149
    
    Disable it there while I figure out the problem.

--- lldb/test/API/commands/process/reverse-continue/TestReverseContinueNotSupported.py
+++ lldb/test/API/commands/process/reverse-continue/TestReverseContinueNotSupported.py
@@ -11,6 +11,7 @@ from lldbsuite.test import lldbutil
 
 
 class TestReverseContinueNotSupported(TestBase):
+    @skipIfWindows
     def test_reverse_continue_not_supported(self):
         target = self.connect()
 
