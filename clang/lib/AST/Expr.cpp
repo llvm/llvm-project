99fa9a1f5fb74401c790871e6eb1ce473d048d74
Kadir Cetinkaya <kadircet@google.com>
2025-04-24 11:12:00 +0200

    [clang][CompundLiteralExpr] Don't defer evaluation for CLEs
    Previously we would defer evaluation of CLEs until LValue to RValue
    conversions, which would result in creating values within wrong scope
    and triggering use-after-frees.
    
    This patch instead eagerly evaluates CLEs, within the scope requiring
    them. This requires storing an extra pointer for CLE expressions with
    static storage.

--- clang/lib/AST/Expr.cpp
+++ clang/lib/AST/Expr.cpp
@@ -5467,3 +5467,12 @@ ConvertVectorExpr *ConvertVectorExpr::Create(
   return new (Mem) ConvertVectorExpr(SrcExpr, TI, DstType, VK, OK, BuiltinLoc,
                                      RParenLoc, FPFeatures);
 }
+
+APValue *CompoundLiteralExpr::getOrCreateStaticValue(ASTContext &Ctx) const {
+  assert(hasStaticStorage());
+  if (!StaticValue) {
+    StaticValue = new (Ctx) APValue;
+    Ctx.addDestruction(StaticValue);
+  }
+  return StaticValue;
+}
