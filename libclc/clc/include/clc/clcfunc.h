fcae18dd0b4af6f7517ba8eda368374fa693a5f9
Fraser Cormack <fraser@codeplay.com>
2025-04-24 11:24:30 +0100

    [libclc] Support the generic address space
    This commit provides definitions of builtins with the generic address
    space.
    
    It is assumed that all current libclc targets can support the generic
    address space.
    
    One concept to consider is the difference between supporting the generic
    address space from the user's perspective, and the requirement for
    libclc as a compiler implementation detail to define separate generic
    address space builtins. In practice a target (like NVPTX) might
    notionally support the generic address space, but it's mapped to the
    same LLVM target address space as the private address space. Therefore
    libclc may not define both private and generic overloads of the same
    builtin. We track these two concepts separately, and make the assumption
    that if the generic address space does clash with another, it's with the
    private one.

--- libclc/clc/include/clc/clcfunc.h
+++ libclc/clc/include/clc/clcfunc.h
@@ -23,4 +23,20 @@
 #define _CLC_DEF __attribute__((always_inline))
 #endif
 
+#if __OPENCL_C_VERSION__ == CL_VERSION_2_0 ||                                  \
+    (__OPENCL_C_VERSION__ >= CL_VERSION_3_0 &&                                 \
+     defined(__opencl_c_generic_address_space))
+#define _CLC_GENERIC_AS_SUPPORTED 1
+// Note that we hard-code the assumption that a non-distinct address space means
+// that the target maps the generic address space to the private address space.
+#ifdef __CLC_DISTINCT_GENERIC_ADDRSPACE__
+#define _CLC_DISTINCT_GENERIC_AS_SUPPORTED 1
+#else
+#define _CLC_DISTINCT_GENERIC_AS_SUPPORTED 0
+#endif
+#else
+#define _CLC_GENERIC_AS_SUPPORTED 0
+#define _CLC_DISTINCT_GENERIC_AS_SUPPORTED 0
+#endif
+
 #endif // __CLC_CLCFUNC_H_
