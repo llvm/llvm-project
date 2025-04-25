30ea7f16e5055b3ef7a061bedb9a1fdb16f6c4b0
hulxv <hulxxv@gmail.com>
2025-04-25 23:26:29 +0300

    [include-cleaner] testing display warnings of deprecated commands

--- clang-tools-extra/include-cleaner/test/tool.cpp
+++ clang-tools-extra/include-cleaner/test/tool.cpp
@@ -58,3 +58,20 @@ int x = foo();
 //        RUN: FileCheck --match-full-lines --check-prefix=EDIT3 %s < %t.cpp
 //      EDIT3: #include "foo.h"
 //  EDIT3-NOT: {{^}}#include "foobar.h"{{$}}
+
+//       RUN: clang-include-cleaner -insert=false -print=changes %s -- -I%S/Inputs/ 2>&1 | \
+//       RUN: FileCheck --check-prefix=DEPRECATED-INSERT %s
+// DEPRECATED-INSERT: [WARNING] -insert is deprecated in favor of `-disable-insert`
+// DEPRECATED-INSERT: The old flag was confusing since it suggested that inserts
+// DEPRECATED-INSERT: were disabled by default, when they were actually enabled
+
+//       RUN: clang-include-cleaner -remove=false -print=changes %s -- -I%S/Inputs/ 2>&1 | \
+//       RUN: FileCheck --check-prefix=DEPRECATED-REMOVE %s
+// DEPRECATED-REMOVE: [WARNING] -remove is deprecated in favor of `-disable-remove`
+// DEPRECATED-REMOVE: The old flag was confusing since it suggested that removes
+// DEPRECATED-REMOVE: were disabled by default, when they were actually enabled
+
+//       RUN: clang-include-cleaner -insert=false -remove=false -print=changes %s -- -I%S/Inputs/ 2>&1 | \
+//       RUN: FileCheck --check-prefix=DEPRECATED-BOTH %s
+// DEPRECATED-BOTH: [WARNING] -insert is deprecated in favor of `-disable-insert`
+// DEPRECATED-BOTH: [WARNING] -remove is deprecated in favor of `-disable-remove`
