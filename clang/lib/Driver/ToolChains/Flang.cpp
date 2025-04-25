d64d9d95fb97d6cfa4bf4192bfb20f5c8d6b3bc3
fanju110 <fanju110@163.com>
2025-04-25 11:53:47 +0800

    simplify push_back usage

--- clang/lib/Driver/ToolChains/Flang.cpp
+++ clang/lib/Driver/ToolChains/Flang.cpp
@@ -882,13 +882,9 @@ void Flang::ConstructJob(Compilation &C, const JobAction &JA,
   // TODO: Handle interactions between -w, -pedantic, -Wall, -WOption
   Args.AddLastArg(CmdArgs, options::OPT_w);
 
-
-  if (Args.hasArg(options::OPT_fprofile_generate)){ 
-    CmdArgs.push_back("-fprofile-generate");
-  }
-  if (const Arg *A = Args.getLastArg(options::OPT_fprofile_use_EQ)) {
-    CmdArgs.push_back(Args.MakeArgString(std::string("-fprofile-use=") + A->getValue()));
-  }
+  // recognise options: fprofile-generate -fprofile-use=
+  Args.addAllArgs(
+      CmdArgs, {options::OPT_fprofile_generate, options::OPT_fprofile_use_EQ});
 
   // Forward flags for OpenMP. We don't do this if the current action is an
   // device offloading action other than OpenMP.
