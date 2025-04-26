8ba3a232d1985a75cf771f8832e911b6f8abe86b
Kazu Hirata <kazu@google.com>
2025-04-26 15:50:38 -0700

    [llvm] Use llvm::copy (NFC) (#137470)

--- llvm/lib/Transforms/Scalar/NewGVN.cpp
+++ llvm/lib/Transforms/Scalar/NewGVN.cpp
@@ -1267,14 +1267,14 @@ NewGVN::createAggregateValueExpression(Instruction *I) const {
         AggregateValueExpression(I->getNumOperands(), II->getNumIndices());
     setBasicExpressionInfo(I, E);
     E->allocateIntOperands(ExpressionAllocator);
-    std::copy(II->idx_begin(), II->idx_end(), int_op_inserter(E));
+    llvm::copy(II->indices(), int_op_inserter(E));
     return E;
   } else if (auto *EI = dyn_cast<ExtractValueInst>(I)) {
     auto *E = new (ExpressionAllocator)
         AggregateValueExpression(I->getNumOperands(), EI->getNumIndices());
     setBasicExpressionInfo(EI, E);
     E->allocateIntOperands(ExpressionAllocator);
-    std::copy(EI->idx_begin(), EI->idx_end(), int_op_inserter(E));
+    llvm::copy(EI->indices(), int_op_inserter(E));
     return E;
   }
   llvm_unreachable("Unhandled type of aggregate value operation");
