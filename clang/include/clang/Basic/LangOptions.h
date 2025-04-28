394044966f8e570a65aeff078cc1da81c03b8aa7
Oliver Hunt <oliver@apple.com>
2025-04-28 08:17:12 -0700

    [clang] Remove FEM_Indeterminable (#137247)
    Remove FEM_Indeterminable as it is unused and cannot be stored safely in
    an unsigned bitfield

--- clang/include/clang/Basic/LangOptions.h
+++ clang/include/clang/Basic/LangOptions.h
@@ -304,10 +304,7 @@ public:
   };
 
   /// Possible float expression evaluation method choices.
-  enum FPEvalMethodKind {
-    /// The evaluation method cannot be determined or is inconsistent for this
-    /// target.
-    FEM_Indeterminable = -1,
+  enum FPEvalMethodKind : unsigned {
     /// Use the declared type for fp arithmetic.
     FEM_Source = 0,
     /// Use the type double for fp arithmetic.
