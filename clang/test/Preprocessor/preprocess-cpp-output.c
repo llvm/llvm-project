82c6897373140bb26a30440769d8f7d686d89646
Juan Manuel Martinez Caamaño <juamarti@amd.com>
2025-04-28 17:05:46 +0200

    Pre-commit test: [Preprocessor] Do not expand macros if the input is already preprocessed

---
+++ clang/test/Preprocessor/preprocess-cpp-output.c
@@ -0,0 +1,9 @@
+// RUN: %clang_cc1 -E -x c %s | FileCheck %s --check-prefixes=EXPANDED
+// RUN: %clang_cc1 -E -x cpp-output %s | FileCheck %s --check-prefixes=EXPANDED
+
+// EXPANDED: void __attribute__((__attribute__((always_inline)))) foo()
+
+#define always_inline __attribute__((always_inline))
+void __attribute__((always_inline)) foo() {
+    return 4;
+}
