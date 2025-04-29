356f692b5f9170723dc7be7d297929f616ebe797
Fraser Cormack <fraser@codeplay.com>
2025-04-29 15:48:12 +0100

    [libclc][NFC] Remove binary_decl_tt.inc
    This was performing the same role as binary_decl.inc.

--- libclc/generic/include/clc/math/fdim.h
+++ libclc/generic/include/clc/math/fdim.h
@@ -7,7 +7,7 @@
 //===----------------------------------------------------------------------===//
 
 #define __CLC_FUNCTION fdim
-#define __CLC_BODY <clc/math/binary_decl_tt.inc>
+#define __CLC_BODY <clc/shared/binary_decl.inc>
 
 #include <clc/math/gentype.inc>
 
