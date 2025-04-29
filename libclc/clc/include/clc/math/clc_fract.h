1448fa9d67696dd734f777fa97f91cb2e64f6dec
Fraser Cormack <fraser@codeplay.com>
2025-04-29 13:58:13 +0100

    [libclc] Move fract to the CLC library (#137785)
    The builtin was already vectorized so there's no difference to codegen
    for non-SPIR-V targets.

--- libclc/clc/include/clc/math/clc_fract.h
+++ libclc/clc/include/clc/math/clc_fract.h
@@ -6,9 +6,15 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include <clc/clc.h>
-#include <clc/math/clc_fract.h>
+#ifndef __CLC_MATH_CLC_FRACT_H__
+#define __CLC_MATH_CLC_FRACT_H__
 
-#define FUNCTION fract
-#define __CLC_BODY <clc/math/unary_def_with_ptr.inc>
+#define __CLC_FUNCTION __clc_fract
+#define __CLC_BODY <clc/math/unary_decl_with_ptr.inc>
+
 #include <clc/math/gentype.inc>
+
+#undef __CLC_BODY
+#undef __CLC_FUNCTION
+
+#endif // __CLC_MATH_CLC_FRACT_H__
