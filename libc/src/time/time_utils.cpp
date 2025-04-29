79a9e17cc9c5ce6c58efc13a572c809297497728
zishan <zmirza@posteo.ch>
2025-04-29 16:55:25 +0200

    format code with clang-format

--- libc/src/time/time_utils.cpp
+++ libc/src/time/time_utils.cpp
@@ -6,8 +6,8 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "config/app.h"
 #include "src/time/time_utils.h"
+#include "config/app.h"
 #include "src/__support/CPP/limits.h" // INT_MIN, INT_MAX
 #include "src/__support/common.h"
 #include "src/__support/macros/config.h"
