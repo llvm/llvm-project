6c412bedc4045ea59a636f1aa9a44a4777495490
Wenju He <wenju.he@intel.com>
2025-04-30 03:36:39 -0700

    remove redundant undef __CLC_DST_ADDR_SPACE __CLC_SRC_ADDR_SPACE

--- libclc/generic/include/clc/async/async_work_group_strided_copy.h
+++ libclc/generic/include/clc/async/async_work_group_strided_copy.h
@@ -10,30 +10,20 @@
 #define __CLC_SRC_ADDR_SPACE global
 #define __CLC_BODY <clc/async/async_work_group_strided_copy.inc>
 #include <clc/integer/gentype.inc>
-#undef __CLC_DST_ADDR_SPACE
-#undef __CLC_SRC_ADDR_SPACE
 #undef __CLC_BODY
-
-#define __CLC_DST_ADDR_SPACE local
-#define __CLC_SRC_ADDR_SPACE global
 #define __CLC_BODY <clc/async/async_work_group_strided_copy.inc>
 #include <clc/math/gentype.inc>
+#undef __CLC_BODY
 #undef __CLC_DST_ADDR_SPACE
 #undef __CLC_SRC_ADDR_SPACE
-#undef __CLC_BODY
 
 #define __CLC_DST_ADDR_SPACE global
 #define __CLC_SRC_ADDR_SPACE local
 #define __CLC_BODY <clc/async/async_work_group_strided_copy.inc>
 #include <clc/integer/gentype.inc>
-#undef __CLC_DST_ADDR_SPACE
-#undef __CLC_SRC_ADDR_SPACE
 #undef __CLC_BODY
-
-#define __CLC_DST_ADDR_SPACE global
-#define __CLC_SRC_ADDR_SPACE local
 #define __CLC_BODY <clc/async/async_work_group_strided_copy.inc>
 #include <clc/math/gentype.inc>
+#undef __CLC_BODY
 #undef __CLC_DST_ADDR_SPACE
 #undef __CLC_SRC_ADDR_SPACE
-#undef __CLC_BODY
