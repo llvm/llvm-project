fb2972ef284dcceb967c336fe92ebdebdc8a6a03
Prabhu Rajasekaran <prabhukr@google.com>
2025-04-30 10:07:57 -0700

    [clang] Fix UEFI Target info (#127290)
    For X64 UEFI targets set appropriate integer type sizes, and relevant
    ABI information. 
    
    ---------
    
    Co-authored-by: Petr Hosek <phosek@google.com>

--- clang/lib/Driver/ToolChains/Clang.cpp
+++ clang/lib/Driver/ToolChains/Clang.cpp
@@ -5977,7 +5977,8 @@ void Clang::ConstructJob(Compilation &C, const JobAction &JA,
   // We turn strict aliasing off by default if we're Windows MSVC since MSVC
   // doesn't do any TBAA.
   if (!Args.hasFlag(options::OPT_fstrict_aliasing, StrictAliasingAliasOption,
-                    options::OPT_fno_strict_aliasing, !IsWindowsMSVC))
+                    options::OPT_fno_strict_aliasing,
+                    !IsWindowsMSVC && !IsUEFI))
     CmdArgs.push_back("-relaxed-aliasing");
   if (Args.hasFlag(options::OPT_fno_pointer_tbaa, options::OPT_fpointer_tbaa,
                    false))
