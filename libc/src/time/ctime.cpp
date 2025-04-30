99b841a67c9a6e49e6b30d2bd89921e82a9a58b2
zishan <zmirza@posteo.ch>
2025-04-30 15:54:17 +0200

    revert: ctime

--- libc/src/time/ctime.cpp
+++ libc/src/time/ctime.cpp
@@ -16,12 +16,9 @@
 namespace LIBC_NAMESPACE_DECL {
 
 LLVM_LIBC_FUNCTION(char *, ctime, (const time_t *t_ptr)) {
-  static struct tm tm_out;
-
   if (t_ptr == nullptr || *t_ptr > cpp::numeric_limits<int32_t>::max()) {
     return nullptr;
   }
-
   static char buffer[time_constants::ASCTIME_BUFFER_SIZE];
   return time_utils::asctime(time_utils::localtime(t_ptr), buffer,
                              time_constants::ASCTIME_MAX_BYTES);
--- libc/src/time/ctime_r.cpp
+++ libc/src/time/ctime_r.cpp
@@ -16,8 +16,6 @@
 namespace LIBC_NAMESPACE_DECL {
 
 LLVM_LIBC_FUNCTION(char *, ctime_r, (const time_t *t_ptr, char *buffer)) {
-  static struct tm tm_out;
-
   if (t_ptr == nullptr || buffer == nullptr ||
       *t_ptr > cpp::numeric_limits<int32_t>::max()) {
     return nullptr;
