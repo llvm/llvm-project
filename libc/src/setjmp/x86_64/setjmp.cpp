b2a03218a95cf000fc9719af4215a743bdc0faf1
Schrodinger ZHU Yifan <yifanzhu@rochester.edu>
2025-04-30 16:27:54 -0400

    Update setjmp.cpp

--- libc/src/setjmp/x86_64/setjmp.cpp
+++ libc/src/setjmp/x86_64/setjmp.cpp
@@ -7,8 +7,8 @@
 //===----------------------------------------------------------------------===//
 
 // We use naked functions to avoid compiler-generated prologue and epilogue.
-// Despite GCC documentation listing this as an unsupported case for extended 
-// asm, the generated code is not wrong as we only pass in constant operands 
+// Despite GCC documentation listing this as an unsupported case for extended
+// asm, the generated code is not wrong as we only pass in constant operands
 // to extended asm.
 // See https://github.com/llvm/llvm-project/issues/137055 for related remarks.
 
