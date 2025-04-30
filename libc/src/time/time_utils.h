9b16b053bddfa18a817d9264c8b69a2f2acb081f
zishan <zmirza@posteo.ch>
2025-04-30 15:35:08 +0200

    revert ctime and remove timezone implementation

--- libc/src/time/time_utils.h
+++ libc/src/time/time_utils.h
@@ -50,9 +50,6 @@ cpp::optional<time_t> mktime_internal(const tm *tm_out);
 
 // Update the "tm" structure's year, month, etc. members from seconds.
 // "total_seconds" is the number of seconds since January 1st, 1970.
-extern int64_t update_from_seconds(int64_t total_seconds, struct tm *tm,
-                                   bool local);
-extern timezone::tzset *get_localtime(struct tm *tm);
 extern int64_t update_from_seconds(int64_t total_seconds, struct tm *tm);
 
 #ifdef LIBC_TARGET_OS_IS_LINUX
@@ -132,12 +129,6 @@ LIBC_INLINE struct tm *localtime_internal(const time_t *timer, struct tm *buf) {
     out_of_range();
     return nullptr;
   }
-
-#ifdef LIBC_TARGET_OS_IS_LINUX
-  // timezone::tzset *ptr = localtime_utils::get_localtime(buf);
-  // buf->tm_hour += ptr->global_offset;
-  // buf->tm_isdst = ptr->global_isdst;
-#endif
 
   return buf;
 }
