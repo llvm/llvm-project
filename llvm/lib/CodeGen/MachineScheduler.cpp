f8d9c7db1f19b58b32755ea6a24bd1cdf3ea7c65
Cullen Rhodes <cullen.rhodes@arm.com>
2025-04-30 16:24:43 +0000

    [MISched] Fix off-by-one error with -misched-cutoff=<n> flag
    This flag instructs the scheduler to stop scheduling after N
    instructions, but it currently schedules N+1 instructions, e.g.
    
      $ llc -misched-cutoff=10 -debug-only=machine-scheduler \
          example.ll 2>&1 | grep  "^Scheduling SU" | wc -l
      11

--- llvm/lib/CodeGen/MachineScheduler.cpp
+++ llvm/lib/CodeGen/MachineScheduler.cpp
@@ -965,7 +965,7 @@ void ScheduleDAGMI::moveInstruction(
 
 bool ScheduleDAGMI::checkSchedLimit() {
 #if LLVM_ENABLE_ABI_BREAKING_CHECKS && !defined(NDEBUG)
-  if (NumInstrsScheduled == MISchedCutoff && MISchedCutoff != ~0U) {
+  if ((NumInstrsScheduled + 1) == MISchedCutoff && MISchedCutoff != ~0U) {
     CurrentTop = CurrentBottom;
     return false;
   }
