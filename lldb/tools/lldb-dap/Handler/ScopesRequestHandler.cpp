d539169524a91cb51aca42393ee4ebebd699913d
Ebuka Ezike <yerimyah1@gmail.com>
2025-05-01 01:44:47 +0100

    [lldb][lldb-dap] Cleanup

--- lldb/tools/lldb-dap/Handler/ScopesRequestHandler.cpp
+++ lldb/tools/lldb-dap/Handler/ScopesRequestHandler.cpp
@@ -7,95 +7,43 @@
 //===----------------------------------------------------------------------===//
 
 #include "DAP.h"
-#include "EventHelper.h"
-#include "JSONUtils.h"
 #include "RequestHandler.h"
 
+using namespace lldb_dap::protocol;
 namespace lldb_dap {
 
-// "ScopesRequest": {
-//   "allOf": [ { "$ref": "#/definitions/Request" }, {
-//     "type": "object",
-//     "description": "Scopes request; value of command field is 'scopes'. The
-//     request returns the variable scopes for a given stackframe ID.",
-//     "properties": {
-//       "command": {
-//         "type": "string",
-//         "enum": [ "scopes" ]
-//       },
-//       "arguments": {
-//         "$ref": "#/definitions/ScopesArguments"
-//       }
-//     },
-//     "required": [ "command", "arguments"  ]
-//   }]
-// },
-// "ScopesArguments": {
-//   "type": "object",
-//   "description": "Arguments for 'scopes' request.",
-//   "properties": {
-//     "frameId": {
-//       "type": "integer",
-//       "description": "Retrieve the scopes for this stackframe."
-//     }
-//   },
-//   "required": [ "frameId" ]
-// },
-// "ScopesResponse": {
-//   "allOf": [ { "$ref": "#/definitions/Response" }, {
-//     "type": "object",
-//     "description": "Response to 'scopes' request.",
-//     "properties": {
-//       "body": {
-//         "type": "object",
-//         "properties": {
-//           "scopes": {
-//             "type": "array",
-//             "items": {
-//               "$ref": "#/definitions/Scope"
-//             },
-//             "description": "The scopes of the stackframe. If the array has
-//             length zero, there are no scopes available."
-//           }
-//         },
-//         "required": [ "scopes" ]
-//       }
-//     },
-//     "required": [ "body" ]
-//   }]
-// }
-
-/// Create a "Scope" JSON object as described in the debug adapter definition.
+/// Creates a `protocol::Scope` struct.
+///
 ///
 /// \param[in] name
 ///     The value to place into the "name" key
-//
+///
 /// \param[in] variablesReference
 ///     The value to place into the "variablesReference" key
-//
+///
 /// \param[in] namedVariables
 ///     The value to place into the "namedVariables" key
-//
+///
 /// \param[in] expensive
 ///     The value to place into the "expensive" key
 ///
 /// \return
-///     A "Scope" JSON object with that follows the formal JSON
-///     definition outlined by Microsoft.
-protocol::Scope CreateScope2(const llvm::StringRef name,
-                             int64_t variablesReference, int64_t namedVariables,
-                             bool expensive) {
-  protocol::Scope scope;
-
+///     A `protocol::Scope`
+static Scope CreateScope(const llvm::StringRef name, int64_t variablesReference,
+                         int64_t namedVariables, bool expensive) {
+  Scope scope;
   scope.name = name;
 
-  // TODO: Support "arguments" scope. At the moment lldb-dap includes the
-  // arguments into the "locals" scope.
-  // add presentation hint;
+  // TODO: Support "arguments" and "return value" scope.
+  // At the moment lldb-dap includes the arguments and return_value  into the
+  // "locals" scope. add presentation hint;
+  // vscode only expands the first non-expensive scope, this causes friction
+  // as the locals scope will not be expanded. It becomes more annoying when
+  // the scope has arguments, return_value and locals.
   if (variablesReference == VARREF_LOCALS)
-    scope.presentationHint = protocol::Scope::ePresentationHintLocals;
+    scope.presentationHint = Scope::ePresentationHintLocals;
   else if (variablesReference == VARREF_REGS)
-    scope.presentationHint = protocol::Scope::ePresentationHintRegisters;
+    scope.presentationHint = Scope::ePresentationHintRegisters;
 
   scope.variablesReference = variablesReference;
   scope.namedVariables = namedVariables;
@@ -104,21 +52,8 @@ protocol::Scope CreateScope2(const llvm::StringRef name,
   return scope;
 }
 
-static std::vector<protocol::Scope> CreateTopLevelScopes(DAP &dap) {
-  std::vector<protocol::Scope> scopes;
-  scopes.reserve(3);
-  scopes.emplace_back(CreateScope2("Locals", VARREF_LOCALS,
-                                   dap.variables.locals.GetSize(), false));
-  scopes.emplace_back(CreateScope2("Globals", VARREF_GLOBALS,
-                                   dap.variables.globals.GetSize(), false));
-  scopes.emplace_back(CreateScope2("Registers", VARREF_REGS,
-                                   dap.variables.registers.GetSize(), false));
-
-  return scopes;
-}
-
-llvm::Expected<protocol::ScopesResponseBody>
-ScopesRequestHandler::Run(const protocol::ScopesArguments &args) const {
+llvm::Expected<ScopesResponseBody>
+ScopesRequestHandler::Run(const ScopesArguments &args) const {
   lldb::SBFrame frame = dap.GetLLDBFrame(args.frameId);
 
   // As the user selects different stack frames in the GUI, a "scopes" request
@@ -129,9 +64,9 @@ ScopesRequestHandler::Run(const protocol::ScopesArguments &args) const {
   // are sent, this allows users to type commands in the debugger console
   // with a backtick character to run lldb commands and these lldb commands
   // will now have the right context selected as they are run. If the user
-  // types "`bt" into the debugger console and we had another thread selected
+  // types "`bt" into the debugger console, and we had another thread selected
   // in the LLDB library, we would show the wrong thing to the user. If the
-  // users switches threads with a lldb command like "`thread select 14", the
+  // users switch threads with a lldb command like "`thread select 14", the
   // GUI will not update as there are no "event" notification packets that
   // allow us to change the currently selected thread or frame in the GUI that
   // I am aware of.
@@ -149,7 +84,14 @@ ScopesRequestHandler::Run(const protocol::ScopesArguments &args) const {
                                              /*in_scope_only=*/true);
   dap.variables.registers = frame.GetRegisters();
 
-  return protocol::ScopesResponseBody{CreateTopLevelScopes(dap)};
+  std::vector scopes = {CreateScope("Locals", VARREF_LOCALS,
+                                    dap.variables.locals.GetSize(), false),
+                        CreateScope("Globals", VARREF_GLOBALS,
+                                    dap.variables.globals.GetSize(), false),
+                        CreateScope("Registers", VARREF_REGS,
+                                    dap.variables.registers.GetSize(), false)};
+
+  return ScopesResponseBody{std::move(scopes)};
 }
 
 } // namespace lldb_dap
