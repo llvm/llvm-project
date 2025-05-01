81cf36b7c7e87d1b4ccb4ca2c9991c5fcd7797fa
Vlad Serebrennikov <serebrennikov.vladislav@gmail.com>
2025-05-01 18:37:23 +0400

    [ADT] Remove `is_scoped_enum_v` (#138134)
    `std::underlying_type` is not SFINAE-friendly in Clang and GCC before 9,
    and it's too much of a hassle to make it work. Instead, I'm inlining the
    implementation in the only place it's needed. Fixes buildbot failure
    https://lab.llvm.org/buildbot/#/builders/134/builds/17904 caused by
    #138089. Demo: https://godbolt.org/z/9qo3csP98

--- clang/include/clang/Basic/Diagnostic.h
+++ clang/include/clang/Basic/Diagnostic.h
@@ -1429,16 +1429,19 @@ operator<<(const StreamingDiagnostic &DB, T *DC) {
   return DB;
 }
 
-// Convert scope enums to their underlying type, so that we don't have
+// Convert scoped enums to their underlying type, so that we don't have
 // clutter the emitting code with `llvm::to_underlying()`.
 // We also need to disable implicit conversion for the first argument,
 // because classes that derive from StreamingDiagnostic define their own
 // templated operator<< that accept a wide variety of types, leading
 // to ambiguity.
-template <typename T, typename U>
+template <typename T, typename U,
+          typename UnderlyingU = typename std::enable_if_t<
+              std::is_enum_v<std::remove_reference_t<U>>,
+              std::underlying_type<std::remove_reference_t<U>>>::type>
 inline std::enable_if_t<
     std::is_same_v<std::remove_const_t<T>, StreamingDiagnostic> &&
-        llvm::is_scoped_enum_v<std::remove_reference_t<U>>,
+        !std::is_convertible_v<U, UnderlyingU>,
     const StreamingDiagnostic &>
 operator<<(const T &DB, U &&SE) {
   DB << llvm::to_underlying(SE);
