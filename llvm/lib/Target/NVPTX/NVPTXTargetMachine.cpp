9f418d299292afbb9389a148cfb77ef1732b1bca
Alex MacLean <amaclean@nvidia.com>
2025-05-01 10:19:46 -0700

    [NVPTX] Pull invariant load identification into IR pass (#138015)
    Pull invariant load identification, which was previously part of
    DAGToDAG ISel, into a new IR pass NVPTXTagInvariantLoads. This makes it
    possible to disable this optimization at O0 and reduces the complexity
    of the SelectionDAG pass. Moving this logic to an IR pass also allows
    for implementing a more powerful traversal in the future.
    
    Fixes https://github.com/llvm/llvm-project/issues/138138

--- llvm/lib/Target/NVPTX/NVPTXTargetMachine.cpp
+++ llvm/lib/Target/NVPTX/NVPTXTargetMachine.cpp
@@ -112,6 +112,7 @@ extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeNVPTXTarget() {
   initializeNVPTXAAWrapperPassPass(PR);
   initializeNVPTXExternalAAWrapperPass(PR);
   initializeNVPTXPeepholePass(PR);
+  initializeNVPTXTagInvariantLoadLegacyPassPass(PR);
 }
 
 static std::string computeDataLayout(bool is64Bit, bool UseShortPointers) {
@@ -395,6 +396,7 @@ void NVPTXPassConfig::addIRPasses() {
     if (!DisableLoadStoreVectorizer)
       addPass(createLoadStoreVectorizerPass());
     addPass(createSROAPass());
+    addPass(createNVPTXTagInvariantLoadsPass());
   }
 
   if (ST.hasPTXASUnreachableBug()) {
