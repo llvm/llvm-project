7394dece81856fc14bf28fdf906f7d1256029d24
Erich Keane <ekeane@nvidia.com>
2025-05-02 09:01:31 -0700

    [OpenACC] Implement better dupe catching for device_type clauses (#138196)
    Previously we just checked duplicates for a handful of clauses between
    active device_type clauses. However, after looking into the
    implementation for 'collapse', I discovered that duplicate device_type
    identifiers with duplicate versions of htese clause are problematic.
    
    This patch corrects this and now catches these conflicts.

--- clang/test/SemaOpenACC/compute-construct-num_gangs-clause.c
+++ clang/test/SemaOpenACC/compute-construct-num_gangs-clause.c
@@ -71,3 +71,18 @@ void Test() {
 #pragma acc loop num_gangs(1)
   for(int i = 5; i < 10;++i);
 }
+
+void no_dupes_since_last_device_type() {
+  // expected-error@+4{{OpenACC 'num_gangs' clause applies to 'device_type' 'radeon', which conflicts with previous 'num_gangs' clause}}
+  // expected-note@+3{{active 'device_type' clause here}}
+  // expected-note@+2{{previous 'num_gangs' clause is here}}
+  // expected-note@+1{{which applies to 'device_type' clause here}}
+#pragma acc parallel device_type(nvidia, radeon) num_gangs(getS()) device_type(radeon) num_gangs(getS())
+  ;
+  // expected-error@+4{{OpenACC 'num_gangs' clause applies to 'device_type' 'nvidia', which conflicts with previous 'num_gangs' clause}}
+  // expected-note@+3{{active 'device_type' clause here}}
+  // expected-note@+2{{previous 'num_gangs' clause is here}}
+  // expected-note@+1{{which applies to 'device_type' clause here}}
+#pragma acc parallel device_type(nvidia) num_gangs(getS()) device_type(nvidia, radeon) num_gangs(getS())
+  ;
+}
