7d6a86e243e3badc4b6385814436fdeaafedff47
Tom Eccles <tom.eccles@arm.com>
2025-05-02 22:54:38 +0000

    [flang][OpenMP] share global variable initialization code
    Fixes #108136
    
    In #108136 (the new testcase), flang was missing the length parameter
    required for the variable length string when boxing the global variable.
    The code that is initializing global variables for OpenMP did not
    support types with length parameters.
    
    Instead of duplicating this initialization logic in OpenMP, I decided to
    use the exact same initialization as is used in the base language
    because this will already be well tested and will be updated for any new
    types. The difference for OpenMP is that the global variables will be
    zero initialized instead of left undefined.
    
    Previously `Fortran::lower::createGlobalInitialization` was used to
    share a smaller amount of the logic with the base language lowering. I
    think this bug has demonstrated that helper was too low level to be
    helpful, and it was only used in OpenMP so I have made it static inside
    of ConvertVariable.cpp.

--- flang/include/flang/Lower/ConvertVariable.h
+++ flang/include/flang/Lower/ConvertVariable.h
@@ -134,10 +134,11 @@ mlir::Value genInitialDataTarget(Fortran::lower::AbstractConverter &,
                                  const SomeExpr &initialTarget,
                                  bool couldBeInEquivalence = false);
 
-/// Call \p genInit to generate code inside \p global initializer region.
-void createGlobalInitialization(
-    fir::FirOpBuilder &builder, fir::GlobalOp global,
-    std::function<void(fir::FirOpBuilder &)> genInit);
+/// Create the global op and its init if it has one
+fir::GlobalOp defineGlobal(Fortran::lower::AbstractConverter &converter,
+                           const Fortran::lower::pft::Variable &var,
+                           llvm::StringRef globalName, mlir::StringAttr linkage,
+                           cuf::DataAttributeAttr dataAttr = {});
 
 /// Generate address \p addr inside an initializer.
 fir::ExtendedValue
