15bcc18a34944c8504ff4fda21c0ef489966a23d
Asher Mancinelli <ashermancinelli@gmail.com>
2025-05-02 09:03:20 -0700

    [flang] Hide strict volatility checks behind flag (#138183)
    Enabling volatility lowering by default revealed some issues in lowering
    and op verification.
    
    For example, given volatile variable of a nested type, accessing
    structure members of a structure member would result in a volatility
    mismatch when the inner structure member is designated (and thus a
    verification error at compile time).
    
    In other cases, I found correct codegen when the checks were disabled,
    also related to allocatable types and how we handle volatile references
    of boxes.
    
    This hides the strict verification of fir and hlfir ops behind a flag so
    I can iteratively improve lowering of volatile variables without causing
    compile-time failures, keeping the strict verification on when running
    tests.

--- flang/lib/Optimizer/HLFIR/IR/HLFIROps.cpp
+++ flang/lib/Optimizer/HLFIR/IR/HLFIROps.cpp
@@ -423,8 +423,9 @@ llvm::LogicalResult hlfir::DesignateOp::verify() {
   unsigned outputRank = 0;
   mlir::Type outputElementType;
   bool hasBoxComponent;
-  if (fir::isa_volatile_type(memrefType) !=
-      fir::isa_volatile_type(getResult().getType())) {
+  if (fir::useStrictVolatileVerification() &&
+      fir::isa_volatile_type(memrefType) !=
+          fir::isa_volatile_type(getResult().getType())) {
     return emitOpError("volatility mismatch between memref and result type")
            << " memref type: " << memrefType
            << " result type: " << getResult().getType();
