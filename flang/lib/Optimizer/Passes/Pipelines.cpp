580da48a93ea3065cced426bb37df65a933c21f7
Anchu Rajendran S <asudhaku@amd.com>
2025-05-02 07:38:44 -0700

    [flang][flang-driver] Support  flag -finstrument-functions (#137996)

--- flang/lib/Optimizer/Passes/Pipelines.cpp
+++ flang/lib/Optimizer/Passes/Pipelines.cpp
@@ -349,7 +349,8 @@ void createDefaultFIRCodeGenPassPipeline(mlir::PassManager &pm,
     framePointerKind = mlir::LLVM::framePointerKind::FramePointerKind::None;
 
   pm.addPass(fir::createFunctionAttr(
-      {framePointerKind, config.NoInfsFPMath, config.NoNaNsFPMath,
+      {framePointerKind, config.InstrumentFunctionEntry,
+       config.InstrumentFunctionExit, config.NoInfsFPMath, config.NoNaNsFPMath,
        config.ApproxFuncFPMath, config.NoSignedZerosFPMath, config.UnsafeFPMath,
        ""}));
 
