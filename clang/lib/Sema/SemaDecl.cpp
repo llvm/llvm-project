5cfdda81309648fc2cec0ed89f587de4aa77863e
Aaron Ballman <aaron@aaronballman.com>
2025-05-02 07:20:02 -0400

    [C] Diagnose use of C++ keywords in C (#137234)
    This adds a new diagnostic group, -Wc++-keyword, which is off by default
    and grouped under -Wc++-compat. The diagnostic catches use of C++
    keywords in C code.
    
    This change additionally fixes an issue with -Wreserved-identifier not
    diagnosing use of reserved identifiers in function parameter lists in a
    function declaration which is not a definition.
    
    Fixes https://github.com/llvm/llvm-project/issues/21898

--- clang/lib/Sema/SemaDecl.cpp
+++ clang/lib/Sema/SemaDecl.cpp
@@ -59,8 +59,10 @@
 #include "clang/Sema/SemaWasm.h"
 #include "clang/Sema/Template.h"
 #include "llvm/ADT/STLForwardCompat.h"
+#include "llvm/ADT/SmallPtrSet.h"
 #include "llvm/ADT/SmallString.h"
 #include "llvm/ADT/StringExtras.h"
+#include "llvm/ADT/StringSwitch.h"
 #include "llvm/Support/SaveAndRestore.h"
 #include "llvm/TargetParser/Triple.h"
 #include <algorithm>
@@ -10423,6 +10425,15 @@ Sema::ActOnFunctionDeclarator(Scope *S, Declarator &D, DeclContext *DC,
 
   // Finally, we know we have the right number of parameters, install them.
   NewFD->setParams(Params);
+
+  // If this declarator is a declaration and not a definition, its parameters
+  // will not be pushed onto a scope chain. That means we will not issue any
+  // reserved identifier warnings for the declaration, but we will for the
+  // definition. Handle those here.
+  if (!D.isFunctionDefinition()) {
+    for (const ParmVarDecl *PVD : Params)
+      warnOnReservedIdentifier(PVD);
+  }
 
   if (D.getDeclSpec().isNoreturnSpecified())
     NewFD->addAttr(
