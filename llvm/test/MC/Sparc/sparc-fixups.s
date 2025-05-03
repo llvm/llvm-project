bb1e3df7009af1bf4b60e1ff4ddb5b7f94d6bff9
Fangrui Song <i@maskray.me>
2025-05-03 16:57:52 -0700

    SparcInstPrinter: Support llvm-objdump --print-imm-hex
    ... to align with other targets, e.g., https://reviews.llvm.org/D77853
    (AArch64) and https://reviews.llvm.org/D83634 (AVR).
    
    binutils's sparc port uses %d when imm<=9, diverging from other ports.
    We do not follow the binutils sparc port behavior.

--- llvm/test/MC/Sparc/sparc-fixups.s
+++ llvm/test/MC/Sparc/sparc-fixups.s
@@ -5,26 +5,30 @@
 
 .set sym, 0xfedcba98
 
-! CHECK: sethi 4175662, %o0
+! CHECK:      sethi 0x3fb72e, %o0
+! CHECK-NEXT: xor %o0, 0x298, %o0
+! CHECK-NEXT: sethi 0x3b72ea, %o1
+! CHECK-NEXT: xor %o0, 0x188, %o1
 sethi %hi(sym), %o0
-! CHECK: xor %o0, 664, %o0
 xor %o0, %lo(sym), %o0
+sethi %hi(-0x12345678), %o1
+xor %o0, %lo(-0x12345678), %o1
 
-! CHECK: sethi 1019, %o0
+! CHECK:      sethi 0x3fb, %o0
+! CHECK-NEXT: or %o0, 0x1cb, %o0
+! CHECK-NEXT: ld [%o0+0xa98], %o0
 sethi %h44(sym), %o0
-! CHECK: or %o0, 459, %o0
 or %o0, %m44(sym), %o0
-! CHECK: ld [%o0+2712], %o0
 ld [%o0 + %l44(sym)], %o0
 
-! CHECK: sethi 0, %o0
+! CHECK:      sethi 0x0, %o0
+! CHECK-NEXT: sethi 0x3fb72e, %o0
+! CHECK-NEXT: or %o0, 0x0, %o0
 sethi %hh(sym), %o0
-! CHECK: sethi 4175662, %o0
 sethi %lm(sym), %o0
-! CHECK: or %o0, 0, %o0
 or %o0, %hm(sym), %o0
 
-! CHECK: sethi 18641, %o0
+! CHECK:      sethi 0x48d1, %o0
+! CHECK-NEXT: xor %o0, -0x168, %o0
 sethi %hix(sym), %o0
-! CHECK: xor %o0, -360, %o0
 xor %o0, %lox(sym), %o0
