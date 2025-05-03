170ab2924b1f11357a1f92179aa55cee9286d290
Paul Kirth <paulkirth@google.com>
2025-05-03 16:55:30 +0000

    [llvm][gvn-sink] Don't try to sink inline asm
    Fixes #138345. Before this patch, gvn-sink would try to sink inline
    assembly statements. Other GVN passes avoid them (see
    https://github.com/llvm/llvm-project/blob/b4fac94181c4cf17dbb7ecc2ae975712b0e4a6d1/llvm/lib/Transforms/Scalar/GVN.cpp#L2932
    Similarly, gvn-sink should skip these instructions, since they are not
    safe to move. To do this, we update the early exit in
    canReplaceOperandWithVariable, since it should have caught this case.
    It's more efficient to also skip numbering in GVNSink if the instruction
    is InlineAsm, but that should be infrequent.
    
    The test added is reduced from a failure when compiling Fuchsia with
    gvn-sink.

--- llvm/lib/Transforms/Utils/Local.cpp
+++ llvm/lib/Transforms/Utils/Local.cpp
@@ -4220,8 +4220,9 @@ bool llvm::canReplaceOperandWithVariable(const Instruction *I, unsigned OpIdx) {
     return false;
 
   // Early exit.
-  if (!isa<Constant>(I->getOperand(OpIdx)))
+  if (!isa<Constant, InlineAsm>(I->getOperand(OpIdx))) {
     return true;
+  }
 
   switch (I->getOpcode()) {
   default:
