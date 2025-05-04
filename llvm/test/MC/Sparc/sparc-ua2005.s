b2e8de55ea9e54239a017eb932f7107f29f465a4
Koakuma <koachan@protonmail.com>
2025-05-04 08:57:07 +0700

    Add other instructions & fix typo
    Created using spr 1.3.5

--- llvm/test/MC/Sparc/sparc-ua2005.s
+++ llvm/test/MC/Sparc/sparc-ua2005.s
@@ -6,3 +6,12 @@
 ! NO-UA2005: error: instruction requires a CPU feature not currently enabled
 ! UA2005: allclean                   ! encoding: [0x85,0x88,0x00,0x00]
 allclean
+! NO-UA2005: error: instruction requires a CPU feature not currently enabled
+! UA2005: invalw                     ! encoding: [0x8b,0x88,0x00,0x00]
+invalw
+! NO-UA2005: error: instruction requires a CPU feature not currently enabled
+! UA2005: otherw                     ! encoding: [0x87,0x88,0x00,0x00]
+otherw
+! NO-UA2005: error: instruction requires a CPU feature not currently enabled
+! UA2005: normalw                    ! encoding: [0x89,0x88,0x00,0x00]
+normalw
