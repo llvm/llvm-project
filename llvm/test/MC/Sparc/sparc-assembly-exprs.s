8c2eafa60c4d0312a39dc4b3e93e4138cabc0c8f
Fangrui Song <i@maskray.me>
2025-05-03 20:26:37 -0700

    Sparc: Improve tests
    * Remove unuseful -filetype=asm -show-encoding output from relocation tests.
    * Test STT_TLS for TLS relocations
    * Test r_offset for a few relocations
    * Test ASM output for many relocations

--- llvm/test/MC/Sparc/sparc-assembly-exprs.s
+++ llvm/test/MC/Sparc/sparc-assembly-exprs.s
@@ -1,33 +1,33 @@
-! RUN: llvm-mc %s -triple=sparc -show-encoding | FileCheck %s
+! RUN: llvm-mc %s -triple=sparc | FileCheck %s
 ! RUN: llvm-mc %s -triple=sparc -filetype=obj | llvm-objdump -r -d - | FileCheck %s --check-prefix=OBJDUMP
 
-        ! CHECK: mov 1033, %o1  ! encoding: [0x92,0x10,0x24,0x09]
+        ! CHECK: mov 1033, %o1
         mov      (0x400|9), %o1
-        ! CHECK: mov 60, %o2    ! encoding: [0x94,0x10,0x20,0x3c]
+        ! CHECK: mov 60, %o2
         mov      ((12+3)<<2), %o2
 
-        ! CHECK:   ba      symStart+4           ! encoding: [0x10,0b10AAAAAA,A,A]
+        ! CHECK:   ba      symStart+4
         ! OBJDUMP: ba    0x1
 symStart:
         b        symStart + 4
 
-        ! CHECK:   mov     symEnd-symStart, %g1 ! encoding: [0x82,0x10,0b001AAAAA,A]
+        ! CHECK:   mov     symEnd-symStart, %g1
         ! OBJDUMP: mov	   0x18, %g1
         mov      symEnd - symStart, %g1
 
-        ! CHECK:   sethi %hi(sym+10), %g2       ! encoding: [0x05,0b00AAAAAA,A,A]
+        ! CHECK:   sethi %hi(sym+10), %g2
         ! OBJDUMP: R_SPARC_HI22	sym+0xa
         sethi    %hi(sym + 10), %g2
 
-        ! CHECK:   call foo+40                  ! encoding: [0b01AAAAAA,A,A,A]
+        ! CHECK:   call foo+40
         ! OBJDUMP: R_SPARC_WDISP30 foo+0x28
         call     foo + 40
 
-        ! CHECK:   add %g1, val+100, %g1        ! encoding: [0x82,0x00,0b011AAAAA,A]
+        ! CHECK:   add %g1, val+100, %g1
         ! OBJDUMP: R_SPARC_13 val+0x64
         add      %g1, val + 100, %g1
 
-        ! CHECK:   add %g1, 100+val, %g2        ! encoding: [0x84,0x00,0b011AAAAA,A]
+        ! CHECK:   add %g1, 100+val, %g2
         ! OBJDUMP: R_SPARC_13	val+0x64
         add      %g1, 100 + val, %g2
 symEnd:
