c3ff3b2ba36aa638ab6101096799d3960bbd6f17
Alex Rønne Petersen <alex@alexrp.com>
2025-05-04 21:32:17 +0200

    [SPARC][MC] Fix `%gdop_hix22()` and `%gdop_lox10()` to use correct relocations (#137915)
    1bfc5e7 introduced support for `%gdop_hix22()` and `%gdop_lox10()`.
    However, it incorrectly mapped them to `R_SPARC_GOTDATA_HIX22` and
    `R_SPARC_GOTDATA_LOX10`. They should in fact emit
    `R_SPARC_GOTDATA_OP_HIX22` and `R_SPARC_GOTDATA_OP_LOX10`.
    
    This became a problem when assembling glibc's PIC startup code:
    
    ```asm
    sethi %gdop_hix22(main), %o0
    xor %o0, %gdop_lox10(main), %o0
    ldx [%l7 + %o0], %o0, %gdop(main)
    ```
    
    After the `xor`, `%o0` should contain the GOT offset for `main`, but
    because of the incorrect relocations, it actually ends up containing the
    address of `main`, which of course makes the following `ldx` fail.

--- llvm/lib/Target/Sparc/MCTargetDesc/SparcMCExpr.cpp
+++ llvm/lib/Target/Sparc/MCTargetDesc/SparcMCExpr.cpp
@@ -80,9 +80,9 @@ StringRef SparcMCExpr::getSpecifierName(SparcMCExpr::Specifier S) {
   case VK_TLS_LE_LOX10:  return "tle_lox10";
   case VK_HIX22:         return "hix";
   case VK_LOX10:         return "lox";
-  case VK_GOTDATA_HIX22: return "gdop_hix22";
-  case VK_GOTDATA_LOX10: return "gdop_lox10";
-  case VK_GOTDATA_OP:    return "gdop";
+  case VK_GOTDATA_OP_HIX22: return "gdop_hix22";
+  case VK_GOTDATA_OP_LOX10: return "gdop_lox10";
+  case VK_GOTDATA_OP:       return "gdop";
   }
   // clang-format on
   llvm_unreachable("Unhandled SparcMCExpr::Specifier");
@@ -126,8 +126,8 @@ SparcMCExpr::Specifier SparcMCExpr::parseSpecifier(StringRef name) {
       .Case("tle_lox10", VK_TLS_LE_LOX10)
       .Case("hix", VK_HIX22)
       .Case("lox", VK_LOX10)
-      .Case("gdop_hix22", VK_GOTDATA_HIX22)
-      .Case("gdop_lox10", VK_GOTDATA_LOX10)
+      .Case("gdop_hix22", VK_GOTDATA_OP_HIX22)
+      .Case("gdop_lox10", VK_GOTDATA_OP_LOX10)
       .Case("gdop", VK_GOTDATA_OP)
       .Default(VK_None);
 }
@@ -172,9 +172,9 @@ uint16_t SparcMCExpr::getFixupKind() const {
   case VK_TLS_LE_LOX10:  return ELF::R_SPARC_TLS_LE_LOX10;
   case VK_HIX22:         return Sparc::fixup_sparc_hix22;
   case VK_LOX10:         return Sparc::fixup_sparc_lox10;
-  case VK_GOTDATA_HIX22: return Sparc::fixup_sparc_gotdata_hix22;
-  case VK_GOTDATA_LOX10: return Sparc::fixup_sparc_gotdata_lox10;
-  case VK_GOTDATA_OP:    return Sparc::fixup_sparc_gotdata_op;
+  case VK_GOTDATA_OP_HIX22: return ELF::R_SPARC_GOTDATA_OP_HIX22;
+  case VK_GOTDATA_OP_LOX10: return ELF::R_SPARC_GOTDATA_OP_LOX10;
+  case VK_GOTDATA_OP:       return ELF::R_SPARC_GOTDATA_OP;
   }
   // clang-format on
 }
