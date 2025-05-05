6e6cf2e214f029697e45caa5303d36afc41c0e4c
Jake Egan <jake.egan@ibm.com>
2025-05-05 10:37:17 -0400

    procmaps

--- compiler-rt/lib/sanitizer_common/sanitizer_procmaps.h
+++ compiler-rt/lib/sanitizer_common/sanitizer_procmaps.h
@@ -17,7 +17,7 @@
 
 #if SANITIZER_LINUX || SANITIZER_FREEBSD || SANITIZER_NETBSD || \
     SANITIZER_APPLE || SANITIZER_SOLARIS || SANITIZER_HAIKU ||  \
-    SANITIZER_FUCHSIA
+    SANITIZER_FUCHSIA || SANITIZER_AIX
 
 #include "sanitizer_common.h"
 #include "sanitizer_internal_defs.h"
