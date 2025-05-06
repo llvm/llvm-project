17412ef0f5320f76c918e367e789c689e138752c
Amirhossein Pashaeehir <amirhosseinp@google.com>
2025-05-06 17:45:34 +0000

    Forward CFI directives alongside their associated instruction to a simple CFI analysis

---
+++ llvm/tools/llvm-mc/CFIAnalysisMCStreamer.h
@@ -0,0 +1,151 @@
+#ifndef LLVM_TOOLS_LLVM_MC_CFI_ANALYSIS_MC_STREAMER_H
+#define LLVM_TOOLS_LLVM_MC_CFI_ANALYSIS_MC_STREAMER_H
+
+#include "CFIAnalysis.h"
+#include "llvm/ADT/ArrayRef.h"
+#include "llvm/ADT/SmallVector.h"
+#include "llvm/MC/MCContext.h"
+#include "llvm/MC/MCDwarf.h"
+#include "llvm/MC/MCInstrInfo.h"
+#include "llvm/MC/MCRegisterInfo.h"
+#include "llvm/MC/MCStreamer.h"
+#include "llvm/Support/Debug.h"
+#include <cstdio>
+#include <optional>
+#include <set>
+
+namespace llvm {
+
+class CFIAnalysisMCStreamer : public MCStreamer {
+  struct CFIDirectivesState {
+    int FrameIndex; // TODO remove it, no need for it
+    int DirectiveIndex;
+
+    CFIDirectivesState() : FrameIndex(-1), DirectiveIndex(0) {}
+
+    CFIDirectivesState(int FrameIndex, int InstructionIndex)
+        : FrameIndex(FrameIndex), DirectiveIndex(InstructionIndex) {}
+  } LastCFIDirectivesState;
+  std::vector<int> FrameIndices;
+
+  struct ICFI {
+    MCInst Instruction;
+    ArrayRef<MCCFIInstruction> CFIDirectives;
+
+    ICFI(MCInst Instruction, ArrayRef<MCCFIInstruction> CFIDirectives)
+        : Instruction(Instruction), CFIDirectives(CFIDirectives) {}
+  };
+
+  std::optional<MCInst> LastInstruction;
+  std::optional<MCDwarfFrameInfo> LastDwarfFrameInfo;
+  CFIAnalysis CFIA;
+
+  std::optional<ICFI> getLastICFI() {
+    if (!LastInstruction)
+      return std::nullopt;
+
+    auto DwarfFrameInfos = getDwarfFrameInfos();
+    int FrameIndex = FrameIndices.back();
+    auto CurrentCFIDirectiveState =
+        hasUnfinishedDwarfFrameInfo()
+            ? CFIDirectivesState(
+                  FrameIndex, DwarfFrameInfos[FrameIndex].Instructions.size())
+            : CFIDirectivesState();
+    assert(CurrentCFIDirectiveState.FrameIndex ==
+           LastCFIDirectivesState.FrameIndex);
+    assert(CurrentCFIDirectiveState.DirectiveIndex >=
+           LastCFIDirectivesState.DirectiveIndex);
+
+    std::vector<MCCFIInstruction> CFIDirectives;
+    if (LastCFIDirectivesState.FrameIndex >= 0) {
+      auto CFIInstructions = DwarfFrameInfos[FrameIndex].Instructions;
+      CFIDirectives = std::vector<MCCFIInstruction>(
+          CFIInstructions.begin() + LastCFIDirectivesState.DirectiveIndex,
+          CFIInstructions.begin() + CurrentCFIDirectiveState.DirectiveIndex);
+    }
+
+    LastCFIDirectivesState = CurrentCFIDirectiveState;
+    return ICFI(LastInstruction.value(), CFIDirectives);
+  }
+
+  void feedCFIA() {
+    if (!LastDwarfFrameInfo) {
+      // TODO Maybe this corner case causes bugs, when the programmer did a
+      // mistake in the startproc, endprocs and also made a mistake in not
+      // adding cfi directives for a instruction. Then this would cause to
+      // ignore the instruction.
+      auto LastICFI = getLastICFI();
+      assert(!LastICFI || LastICFI->CFIDirectives.empty());
+      return;
+    }
+
+    if (auto ICFI = getLastICFI())
+      CFIA.update(LastDwarfFrameInfo.value(), ICFI->Instruction,
+                  ICFI->CFIDirectives);
+  }
+
+public:
+  CFIAnalysisMCStreamer(MCContext &Context, const MCInstrInfo &MCII)
+      : MCStreamer(Context), LastCFIDirectivesState(),
+        LastInstruction(std::nullopt), CFIA(Context, MCII) {
+    FrameIndices.push_back(-1);
+  }
+
+  /// @name MCStreamer Interface
+  /// @{
+
+  bool hasRawTextSupport() const override { return true; }
+  void emitRawTextImpl(StringRef String) override {}
+
+  bool emitSymbolAttribute(MCSymbol *Symbol, MCSymbolAttr Attribute) override {
+    return true;
+  }
+
+  void emitCommonSymbol(MCSymbol *Symbol, uint64_t Size,
+                        Align ByteAlignment) override {}
+  void beginCOFFSymbolDef(const MCSymbol *Symbol) override {}
+  void emitCOFFSymbolStorageClass(int StorageClass) override {}
+  void emitCOFFSymbolType(int Type) override {}
+  void endCOFFSymbolDef() override {}
+  void emitXCOFFSymbolLinkageWithVisibility(MCSymbol *Symbol,
+                                            MCSymbolAttr Linkage,
+                                            MCSymbolAttr Visibility) override {}
+
+  void emitInstruction(const MCInst &Inst,
+                       const MCSubtargetInfo &STI) override {
+    feedCFIA();
+    LastInstruction = Inst;
+    if (hasUnfinishedDwarfFrameInfo())
+      LastDwarfFrameInfo = getDwarfFrameInfos()[FrameIndices.back()];
+    else
+      LastDwarfFrameInfo = std::nullopt;
+  }
+
+  void emitCFIStartProcImpl(MCDwarfFrameInfo &Frame) override {
+    feedCFIA();
+    FrameIndices.push_back(getNumFrameInfos());
+    LastInstruction = std::nullopt;
+    LastDwarfFrameInfo = std::nullopt;
+    LastCFIDirectivesState.FrameIndex = FrameIndices.back();
+    LastCFIDirectivesState.DirectiveIndex = 0;
+    MCStreamer::emitCFIStartProcImpl(Frame);
+  }
+
+  void emitCFIEndProcImpl(MCDwarfFrameInfo &CurFrame) override {
+    feedCFIA();
+    // TODO this will break if the input frame are malformed.
+    FrameIndices.pop_back();
+    LastInstruction = std::nullopt;
+    LastDwarfFrameInfo = std::nullopt;
+    LastCFIDirectivesState.FrameIndex = FrameIndices.back();
+    LastCFIDirectivesState.DirectiveIndex =
+        LastCFIDirectivesState.FrameIndex >= 0
+            ? getDwarfFrameInfos()[LastCFIDirectivesState.FrameIndex]
+                  .Instructions.size()
+            : 0;
+    MCStreamer::emitCFIEndProcImpl(CurFrame);
+  }
+};
+
+} // namespace llvm
+#endif
