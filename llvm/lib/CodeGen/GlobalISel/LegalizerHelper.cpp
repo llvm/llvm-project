68da1a8905612f97de70453fecbaca2b9b4950b4
jyli0116 <yu.li@arm.com>
2025-05-06 09:57:01 +0100

    [GlobalISel][AArch64] Handles bitreverse to prevent falling back (#138150)
    Handles bitreverse for vector types which were previously falling back
    onto Selection DAG. Includes 8-bit element vectors greater than 128 bits
    and less than 64 bits: <32 x i8>, <4 x i8>, and odd vector types: <9 x
    i8>.

--- llvm/lib/CodeGen/GlobalISel/LegalizerHelper.cpp
+++ llvm/lib/CodeGen/GlobalISel/LegalizerHelper.cpp
@@ -6136,6 +6136,7 @@ LegalizerHelper::moreElementsVector(MachineInstr &MI, unsigned TypeIdx,
   case TargetOpcode::G_INTRINSIC_ROUND:
   case TargetOpcode::G_INTRINSIC_ROUNDEVEN:
   case TargetOpcode::G_INTRINSIC_TRUNC:
+  case TargetOpcode::G_BITREVERSE:
   case TargetOpcode::G_BSWAP:
   case TargetOpcode::G_FCANONICALIZE:
   case TargetOpcode::G_SEXT_INREG:
