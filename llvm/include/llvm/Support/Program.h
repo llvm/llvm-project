901c3b35ef1320e9424bfd88e5f27d518cd77c85
Andrew Rogers <andrurogerz@gmail.com>
2025-05-06 12:18:21 -0700

    PR feedback: fix file header comment formatting

--- llvm/include/llvm/Support/Program.h
+++ llvm/include/llvm/Support/Program.h
@@ -1,5 +1,4 @@
-//===- llvm/Support/Program.h ------------------------------------*- C++
-//-*-===//
+//===- llvm/Support/Program.h -----------------------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
--- llvm/include/llvm/Support/Signals.h
+++ llvm/include/llvm/Support/Signals.h
@@ -1,5 +1,4 @@
-//===- llvm/Support/Signals.h - Signal Handling support ----------*- C++
-//-*-===//
+//===- llvm/Support/Signals.h - Signal Handling support ---------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
