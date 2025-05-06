46626aa4c6f774929e3fee260e2090c164596ad7
Rohit Aggarwal <44664450+rohitaggarwal007@users.noreply.github.com>
2025-05-06 12:55:10 +0530

    [X86][DAGCombiner][SelectionDAG] - Fold Zext Build Vector to Bitcast of widen Build Vector (#135010)
    I am working on a problem in which a kernel is SLP vectorized and lead
    to generation of insertelements followed by zext. On lowering, the
    assembly looks like below:
    
        vmovd   %r9d, %xmm0
        vpinsrb $1, (%rdi,%rsi), %xmm0, %xmm0
        vpinsrb $2, (%rdi,%rsi,2), %xmm0, %xmm0
        vpinsrb $3, (%rdi,%rcx), %xmm0, %xmm0
        vpinsrb $4, (%rdi,%rsi,4), %xmm0, %xmm0
        vpinsrb $5, (%rdi,%rax), %xmm0, %xmm0
        vpinsrb $6, (%rdi,%rcx,2), %xmm0, %xmm0
        vpinsrb $7, (%rdi,%r8), %xmm0, %xmm0
    vpmovzxbw %xmm0, %xmm0 # xmm0 =
    xmm0[0],zero,xmm0[1],zero,xmm0[2],zero,xmm0[3],zero,xmm0[4],zero,xmm0[5],zero,xmm0[6],zero,xmm0[7],zero
        vpmaddwd        (%rdx), %xmm0, %xmm0
    After all the insrb, xmm0 looks like
    
    xmm0=xmm0[0],xmm0[1],xmm0[2],xmm0[3],xmm0[4],xmm0[5],xmm0[6],xmm0[7],zero,zero,zero,zero,zero,zero,zero,zero
    Here vpmovzxbw perform the extension of i8 to i16. But it is expensive
    operation and I want to remove it.
    
    Optimization
    Place the value in correct location while inserting so that zext can be
    avoid.
    While lowering, we can write a custom lowerOperation for
    zero_extend_vector_inreg opcode. We can override the current default
    operation with my custom in the legalization step.
    
    The changes proposed are state below:
    
        vpinsrb $2, (%rdi,%rsi), %xmm0, %xmm0
        vpinsrb $4, (%rdi,%rsi,2), %xmm0, %xmm0
        vpinsrb $6, (%rdi,%rcx), %xmm0, %xmm0
        vpinsrb $8, (%rdi,%rsi,4), %xmm0, %xmm0
        vpinsrb $a, (%rdi,%rax), %xmm0, %xmm0
        vpinsrb $c, (%rdi,%rcx,2), %xmm0, %xmm0
    vpinsrb $e, (%rdi,%r8), %xmm0, %xmm0 # xmm0 =
    xmm0[0],zero,xmm0[1],zero,xmm0[2],zero,xmm0[3],zero,xmm0[4],zero,xmm0[5],zero,xmm0[6],zero,xmm0[7],zero
        vpmaddwd        (%rdx), %xmm0, %xmm0
    
    More details in the discourse topic
    [https://discourse.llvm.org/t/improve-the-gathering-of-the-elements-so-that-unwanted-ext-operations-can-be-avoided/85443](url)
    
    ---------
    
    Co-authored-by: Rohit Aggarwal <Rohit.Aggarwal@amd.com>

--- llvm/lib/Target/X86/X86ISelLowering.cpp
+++ llvm/lib/Target/X86/X86ISelLowering.cpp
@@ -55934,6 +55934,79 @@ static SDValue combineFMADDSUB(SDNode *N, SelectionDAG &DAG,
                      NegN2);
 }
 
+// Try to widen the build vector and bitcast it to the type of zext.
+// This is a special case for the 128-bit vector types. Intention is to remove
+// the zext and replace it with a bitcast the wider type. While lowering
+// the bitcast is removed and extra commutation due to zext is avoided.
+// For example:
+// zext v4i16 ( v4i8 build_vector (x, y, z, w)) -> bitcast v4i16 ( v8i8
+// build_vector (x, 0, y, 0, z, w, 0)
+static SDValue widenBuildVec(SDNode *Extend, SelectionDAG &DAG) {
+
+  if (Extend->getOpcode() != ISD::ZERO_EXTEND)
+    return SDValue();
+
+  EVT ExtendVT = Extend->getValueType(0);
+
+  SDValue BV = Extend->getOperand(0);
+  if (BV.getOpcode() != ISD::BUILD_VECTOR || !BV.hasOneUse())
+    return SDValue();
+
+  if (any_of(BV->op_values(), [](SDValue Op) { return Op.isUndef(); })) {
+    // If the build vector has undef elements, we cannot widen it.
+    // The widening would create a vector with more undef elements, which
+    // is not valid.
+    return SDValue();
+  }
+
+  if (!all_of(BV->op_values(),
+              [](SDValue Op) { return Op.getOpcode() == ISD::LOAD; })) {
+    // If the build vector any element other than \ISD::LOAD, we cannot widen
+    // it.
+    return SDValue();
+  }
+
+  SDLoc dl(BV);
+  EVT VT = BV.getValueType();
+  EVT EltVT = BV.getOperand(0).getValueType();
+  unsigned NumElts = VT.getVectorNumElements();
+
+  const TargetLowering &TLI = DAG.getTargetLoweringInfo();
+
+  if (TLI.getTypeAction(*DAG.getContext(), VT) !=
+      TargetLowering::TypeWidenVector)
+    return SDValue();
+
+  EVT WidenVT = TLI.getTypeToTransformTo(*DAG.getContext(), VT);
+  unsigned WidenNumElts = WidenVT.getVectorNumElements();
+
+  SmallVector<SDValue, 16> NewOps(BV->op_begin(), BV->op_end());
+  assert(WidenNumElts >= NumElts && "Shrinking vector instead of widening!");
+  // Fill the new elements with Zero.
+  NewOps.append(WidenNumElts - NumElts, DAG.getConstant(0, dl, EltVT));
+  // Compute the step to place the elements in the right place and control the
+  // iteration.
+  unsigned step = WidenNumElts / NumElts;
+  if (WidenVT.is128BitVector()) {
+    if (step > 1 && Extend->getValueSizeInBits(0) == WidenVT.getSizeInBits()) {
+      for (int i = NumElts - 1, j = WidenNumElts - step; i > 0;
+           i--, j -= step) {
+        SDValue temp = NewOps[i];
+        NewOps[i] = NewOps[j];
+        NewOps[j] = temp;
+      }
+      // Create new build vector with WidenVT and NewOps
+      SDValue NewBV = DAG.getBuildVector(WidenVT, dl, NewOps);
+      // Replace the old build vector with the new one. Bitcast the
+      // new build vector to the type of the zext.
+      SDValue NewBVBitcast = DAG.getBitcast(ExtendVT, NewBV);
+      DAG.ReplaceAllUsesOfValueWith(SDValue(Extend, 0), NewBVBitcast);
+      return NewBV;
+    }
+  }
+  return SDValue();
+}
+
 static SDValue combineZext(SDNode *N, SelectionDAG &DAG,
                            TargetLowering::DAGCombinerInfo &DCI,
                            const X86Subtarget &Subtarget) {
@@ -55992,6 +56065,9 @@ static SDValue combineZext(SDNode *N, SelectionDAG &DAG,
       return concatSubVectors(N00, N01, DAG, dl);
     }
   }
+
+  if (SDValue V = widenBuildVec(N, DAG))
+    return V;
 
   return SDValue();
 }
