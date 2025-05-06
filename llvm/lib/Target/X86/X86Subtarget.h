06b5cb4909a6b2c55056972a23e9846611469646
Prabhu Rajasekaran <prabhukr@google.com>
2025-05-06 15:41:35 -0700

    [NFC][llvm] Drop isOsWindowsOrUEFI API (#138733)
    The Triple and SubTarget API functions isOsWindowsOrUEFI is not
    preferred. Dropping them.

--- llvm/lib/Target/X86/X86Subtarget.h
+++ llvm/lib/Target/X86/X86Subtarget.h
@@ -331,8 +331,6 @@ public:
 
   bool isOSWindows() const { return TargetTriple.isOSWindows(); }
 
-  bool isOSWindowsOrUEFI() const { return TargetTriple.isOSWindowsOrUEFI(); }
-
   bool isTargetUEFI64() const { return Is64Bit && isUEFI(); }
 
   bool isTargetWin64() const { return Is64Bit && isOSWindows(); }
