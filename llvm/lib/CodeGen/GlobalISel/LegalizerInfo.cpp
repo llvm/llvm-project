97592c525a948e56b9f6d32b184c51bdb27f5d30
David Green <david.green@arm.com>
2025-05-06 07:36:25 +0100

    Formatting and brackets

--- llvm/lib/CodeGen/GlobalISel/LegalizerInfo.cpp
+++ llvm/lib/CodeGen/GlobalISel/LegalizerInfo.cpp
@@ -217,24 +217,28 @@ LegalizeActionStep LegalizeRuleSet::apply(const LegalityQuery &Query) const {
 bool LegalizeRuleSet::verifyTypeIdxsCoverage(unsigned NumTypeIdxs) const {
 #ifndef NDEBUG
   if (Rules.empty()) {
-    if (VerboseVerifyLegalizerInfo)
-      LLVM_DEBUG(
-          dbgs() << ".. type index coverage check SKIPPED: no rules defined\n");
+    if (VerboseVerifyLegalizerInfo) {
+      LLVM_DEBUG(dbgs() << ".. type index coverage check SKIPPED: "
+                        << "no rules defined\n");
+    }
     return true;
   }
   const int64_t FirstUncovered = TypeIdxsCovered.find_first_unset();
   if (FirstUncovered < 0) {
-    if (VerboseVerifyLegalizerInfo)
+    if (VerboseVerifyLegalizerInfo) {
       LLVM_DEBUG(dbgs() << ".. type index coverage check SKIPPED:"
                            " user-defined predicate detected\n");
+    }
     return true;
   }
   const bool AllCovered = (FirstUncovered >= NumTypeIdxs);
-  if (NumTypeIdxs > 0)
-    if (VerboseVerifyLegalizerInfo)
+  if (NumTypeIdxs > 0) {
+    if (VerboseVerifyLegalizerInfo) {
       LLVM_DEBUG(dbgs() << ".. the first uncovered type index: "
                         << FirstUncovered << ", "
                         << (AllCovered ? "OK" : "FAIL") << "\n");
+    }
+  }
   return AllCovered;
 #else
   return true;
@@ -244,22 +248,25 @@ bool LegalizeRuleSet::verifyTypeIdxsCoverage(unsigned NumTypeIdxs) const {
 bool LegalizeRuleSet::verifyImmIdxsCoverage(unsigned NumImmIdxs) const {
 #ifndef NDEBUG
   if (Rules.empty()) {
-    if (VerboseVerifyLegalizerInfo)
-      LLVM_DEBUG(
-          dbgs() << ".. imm index coverage check SKIPPED: no rules defined\n");
+    if (VerboseVerifyLegalizerInfo) {
+      LLVM_DEBUG(dbgs() << ".. imm index coverage check SKIPPED: "
+                        << "no rules defined\n");
+    }
     return true;
   }
   const int64_t FirstUncovered = ImmIdxsCovered.find_first_unset();
   if (FirstUncovered < 0) {
-    if (VerboseVerifyLegalizerInfo)
+    if (VerboseVerifyLegalizerInfo) {
       LLVM_DEBUG(dbgs() << ".. imm index coverage check SKIPPED:"
                            " user-defined predicate detected\n");
+    }
     return true;
   }
   const bool AllCovered = (FirstUncovered >= NumImmIdxs);
-  if (VerboseVerifyLegalizerInfo)
+  if (VerboseVerifyLegalizerInfo) {
     LLVM_DEBUG(dbgs() << ".. the first uncovered imm index: " << FirstUncovered
                       << ", " << (AllCovered ? "OK" : "FAIL") << "\n");
+  }
   return AllCovered;
 #else
   return true;
@@ -287,9 +294,10 @@ unsigned LegalizerInfo::getOpcodeIdxForOpcode(unsigned Opcode) const {
 unsigned LegalizerInfo::getActionDefinitionsIdx(unsigned Opcode) const {
   unsigned OpcodeIdx = getOpcodeIdxForOpcode(Opcode);
   if (unsigned Alias = RulesForOpcode[OpcodeIdx].getAlias()) {
-    if (VerboseVerifyLegalizerInfo)
+    if (VerboseVerifyLegalizerInfo) {
       LLVM_DEBUG(dbgs() << ".. opcode " << Opcode << " is aliased to " << Alias
                         << "\n");
+    }
     OpcodeIdx = getOpcodeIdxForOpcode(Alias);
     assert(RulesForOpcode[OpcodeIdx].getAlias() == 0 && "Cannot chain aliases");
   }
@@ -410,12 +418,13 @@ void LegalizerInfo::verify(const MCInstrInfo &MII) const {
                      ? std::max(OpInfo.getGenericImmIndex() + 1U, Acc)
                      : Acc;
         });
-    if (VerboseVerifyLegalizerInfo)
+    if (VerboseVerifyLegalizerInfo) {
       LLVM_DEBUG(dbgs() << MII.getName(Opcode) << " (opcode " << Opcode
                         << "): " << NumTypeIdxs << " type ind"
                         << (NumTypeIdxs == 1 ? "ex" : "ices") << ", "
                         << NumImmIdxs << " imm ind"
                         << (NumImmIdxs == 1 ? "ex" : "ices") << "\n");
+    }
     const LegalizeRuleSet &RuleSet = getActionDefinitions(Opcode);
     if (!RuleSet.verifyTypeIdxsCoverage(NumTypeIdxs))
       FailedOpcodes.push_back(Opcode);
@@ -428,8 +437,9 @@ void LegalizerInfo::verify(const MCInstrInfo &MII) const {
       errs() << " " << MII.getName(Opcode);
     errs() << "\n";
 
-    report_fatal_error("ill-defined LegalizerInfo"
-                       ", try -debug-only=legalizer-info for details");
+    report_fatal_error("ill-defined LegalizerInfo, try "
+                       "-debug-only=legalizer-info and "
+                       "-verbose-gisel-verify-legalizer-info for details");
   }
 #endif
 }
