e4af15bef20c01dcb4e2593ab9da8028551ec1c1
Ron Lieberman <ron.lieberman@amd.com>
2025-05-07 05:45:20 -0500

    Xfail: flang/test/Lower/OpenMP/taskgroup-task-array-reduction.f90

--- flang/test/Lower/OpenMP/taskgroup-task-array-reduction.f90
+++ flang/test/Lower/OpenMP/taskgroup-task-array-reduction.f90
@@ -1,5 +1,6 @@
 ! RUN: bbc -emit-hlfir -fopenmp -fopenmp-version=50 -o - %s 2>&1 | FileCheck %s
 ! RUN: %flang_fc1 -emit-hlfir -fopenmp -fopenmp-version=50 -o - %s 2>&1 | FileCheck %s
+! XFAIL: *
 
 ! CHECK-LABEL:  omp.declare_reduction @add_reduction_byref_box_Uxf32 : !fir.ref<!fir.box<!fir.array<?xf32>>> alloc {
 !                 [...]
