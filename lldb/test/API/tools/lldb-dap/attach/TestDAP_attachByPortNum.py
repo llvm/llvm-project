e677701bb3cf9aa23a7b37c44df6b6f66ceee60f
Jonas Devlieghere <jonas@devlieghere.com>
2025-05-06 18:15:34 -0700

    [lldb-dap] Don't make stopOnAttach the default in attach tests
    Make stopOnAttach=False the default again and explicitly pass
    stopOnAttach=True where the tests relies on that. I changed the default
    in the launch sequence PR (#138219) because that was implicitly the
    assumption (the tests never send the configurationDone request).

--- lldb/test/API/tools/lldb-dap/attach/TestDAP_attachByPortNum.py
+++ lldb/test/API/tools/lldb-dap/attach/TestDAP_attachByPortNum.py
@@ -79,7 +79,9 @@ class TestDAP_attachByPortNum(lldbdap_testcase.DAPTestCaseBase):
             port, " Failed to read the port number from debug server pipe"
         )
 
-        self.attach(program=program, gdbRemotePort=port, sourceInitFile=True)
+        self.attach(
+            program=program, stopOnAttach=True, gdbRemotePort=port, sourceInitFile=True
+        )
         self.set_and_hit_breakpoint(continueToExit=True)
         self.process.terminate()
 
@@ -101,6 +103,7 @@ class TestDAP_attachByPortNum(lldbdap_testcase.DAPTestCaseBase):
         response = self.attach(
             program=program,
             pid=pid,
+            stopOnAttach=True,
             gdbRemotePort=port,
             sourceInitFile=True,
             expectFailure=True,
@@ -120,7 +123,11 @@ class TestDAP_attachByPortNum(lldbdap_testcase.DAPTestCaseBase):
 
         port = 0
         response = self.attach(
-            program=program, gdbRemotePort=port, sourceInitFile=True, expectFailure=True
+            program=program,
+            stopOnAttach=True,
+            gdbRemotePort=port,
+            sourceInitFile=True,
+            expectFailure=True,
         )
         if not (response and response["success"]):
             self.assertFalse(
@@ -144,7 +151,11 @@ class TestDAP_attachByPortNum(lldbdap_testcase.DAPTestCaseBase):
         )
 
         response = self.attach(
-            program=program, gdbRemotePort=port, sourceInitFile=True, expectFailure=True
+            program=program,
+            stopOnAttach=True,
+            gdbRemotePort=port,
+            sourceInitFile=True,
+            expectFailure=True,
         )
         if not (response and response["success"]):
             self.assertFalse(
