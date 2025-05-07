efdedcfd438d8c3d1fe162562473d069c5f6c840
Jake Egan <jake.egan@ibm.com>
2025-05-07 10:39:25 -0400

    Fix formatting

--- compiler-rt/lib/sanitizer_common/sanitizer_procmaps.h
+++ compiler-rt/lib/sanitizer_common/sanitizer_procmaps.h
@@ -19,13 +19,13 @@
     SANITIZER_APPLE || SANITIZER_SOLARIS || SANITIZER_HAIKU ||  \
     SANITIZER_FUCHSIA || SANITIZER_AIX
 
+#  include "sanitizer_aix.h"
 #  include "sanitizer_common.h"
 #  include "sanitizer_fuchsia.h"
 #  include "sanitizer_internal_defs.h"
 #  include "sanitizer_linux.h"
 #  include "sanitizer_mac.h"
 #  include "sanitizer_mutex.h"
-#  include "sanitizer_aix.h"
 
 namespace __sanitizer {
 
