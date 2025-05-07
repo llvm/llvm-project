llvm/monorepo-prototype

[compiler-rt][test] Apply CFLAGS from sanitizer_common to all tests
Sanitizer-specific tests don't use the sanitizer_common flags, but the
issues they address probably also apply to the individual sanitizers.

This was observed in #119071: moving a test from sanitizer_common to
msan broke it in builds with CMAKE_SYSROOT set, because the --sysroot
argument was no longer applied to the test.

compiler-rt/cmake/config-ix.cmake
@@ -307,6 +307,14 @@ macro(get_test_cc_for_arch arch cc_out cflags_out)
     if(APPLE)
       list(APPEND ${cflags_out} ${DARWIN_osx_CFLAGS})
     endif()
+    if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+      # ARM on Linux might use the slow unwinder as default and the unwind table
+      # is required to get a complete stacktrace.
+      list(APPEND ${cflags_out} -funwind-tables)
+      if(CMAKE_SYSROOT)
+        list(APPEND ${cflags_out} "--sysroot=${CMAKE_SYSROOT}")
+      endif()
+    endif()
     string(REPLACE ";" " " ${cflags_out} "${${cflags_out}}")
   endif()
 endmacro()

llvm/llvm-project

compiler-rt/cmake/config-ix.cmake
@@ -309,6 +309,14 @@ macro(get_test_cc_for_arch arch cc_out cflags_out)
     endif()
     string(REPLACE ";" " " ${cflags_out} "${${cflags_out}}")
   endif()
+  if(CMAKE_SYSTEM_NAME STREQUAL "Linux" AND NOT ANDROID)
+    # ARM on Linux might use the slow unwinder as default and the unwind table
+    # is required to get a complete stacktrace.
+    string(APPEND ${cflags_out} " -funwind-tables")
+    if(CMAKE_SYSROOT)
+      string(APPEND ${cflags_out} " --sysroot=${CMAKE_SYSROOT}")
+    endif()
+  endif()
 endmacro()
 
 # Returns CFLAGS that should be used to run tests for the