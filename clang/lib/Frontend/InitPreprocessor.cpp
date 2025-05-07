5ddb56ff656358f04cc434a78d356e5d26c952c5
Alex Voicu <alexandru.voicu@amd.com>
2025-05-07 14:14:25 +0100

    [HIP][HIPSTDPAR] Re-work allocation interposition for `hipstdpar` (#138790)
    The allocation interposition mode had a number of issues, which are
    primarily addressed in the library component via
    <https://github.com/ROCm/rocThrust/pull/543>. However, it is necessary
    to interpose some additional symbols, which this patch does.
    Furthermore, to implement this in a compatible way, we guard the new
    implementation under a V1 macro, which is defined in addition to the
    existing `__HIPSTDPAR_INTERPOSE_ALLOC__` one.

--- clang/lib/Frontend/InitPreprocessor.cpp
+++ clang/lib/Frontend/InitPreprocessor.cpp
@@ -618,8 +618,10 @@ static void InitializeStandardPredefinedMacros(const TargetInfo &TI,
     Builder.defineMacro("__HIP_MEMORY_SCOPE_SYSTEM", "5");
     if (LangOpts.HIPStdPar) {
       Builder.defineMacro("__HIPSTDPAR__");
-      if (LangOpts.HIPStdParInterposeAlloc)
+      if (LangOpts.HIPStdParInterposeAlloc) {
         Builder.defineMacro("__HIPSTDPAR_INTERPOSE_ALLOC__");
+        Builder.defineMacro("__HIPSTDPAR_INTERPOSE_ALLOC_V1__");
+      }
     }
     if (LangOpts.CUDAIsDevice) {
       Builder.defineMacro("__HIP_DEVICE_COMPILE__");
