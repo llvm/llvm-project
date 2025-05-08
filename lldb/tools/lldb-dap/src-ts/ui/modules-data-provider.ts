2d372f7ed38f2e5cf4c6d06ff78b043f2606c164
John Harrison <harjohn@google.com>
2025-05-08 15:25:28 -0700

    [lldb-dap] Adding a modules explorer to lldb-dap ext. (#138977)
    This creates a very basic module explorer for tracking and displaying
    loaded modules, reported by lldb-dap for the active debug session.
    
    This includes a basic session tracker that we can use to observe the
    debug session and collect specific information for additional
    visualizations in the lldb-dap ext.
    
    Here is a screenshot of the current visualization in the tree view.
    There is some unfortunate wrapping of the path, but it shows the basic
    support that could be extended in the future.
    
    <img width="1759" alt="Screenshot 2025-05-07 at 2 52 50 PM"
    src="https://github.com/user-attachments/assets/588baa2f-61d5-4434-8692-b1d0cce42875"
    />

---
+++ lldb/tools/lldb-dap/src-ts/ui/modules-data-provider.ts
@@ -0,0 +1,59 @@
+import * as vscode from "vscode";
+import { DebugProtocol } from "@vscode/debugprotocol";
+import { DebugSessionTracker } from "../debug-session-tracker";
+
+/** A tree data provider for listing loaded modules for the active debug session. */
+export class ModulesDataProvider
+  implements vscode.TreeDataProvider<DebugProtocol.Module>
+{
+  private changeTreeData = new vscode.EventEmitter<void>();
+  readonly onDidChangeTreeData = this.changeTreeData.event;
+
+  constructor(private readonly tracker: DebugSessionTracker) {
+    tracker.onDidChangeModules(() => this.changeTreeData.fire());
+    vscode.debug.onDidChangeActiveDebugSession(() =>
+      this.changeTreeData.fire(),
+    );
+  }
+
+  getTreeItem(module: DebugProtocol.Module): vscode.TreeItem {
+    let treeItem = new vscode.TreeItem(/*label=*/ module.name);
+    if (module.path) {
+      treeItem.description = `${module.id} -- ${module.path}`;
+    } else {
+      treeItem.description = `${module.id}`;
+    }
+
+    const tooltip = new vscode.MarkdownString();
+    tooltip.appendMarkdown(`# Module '${module.name}'\n\n`);
+    tooltip.appendMarkdown(`- **id** : ${module.id}\n`);
+    if (module.addressRange) {
+      tooltip.appendMarkdown(`- **load address** : ${module.addressRange}\n`);
+    }
+    if (module.path) {
+      tooltip.appendMarkdown(`- **path** : ${module.path}\n`);
+    }
+    if (module.version) {
+      tooltip.appendMarkdown(`- **version** : ${module.version}\n`);
+    }
+    if (module.symbolStatus) {
+      tooltip.appendMarkdown(`- **symbol status** : ${module.symbolStatus}\n`);
+    }
+    if (module.symbolFilePath) {
+      tooltip.appendMarkdown(
+        `- **symbol file path** : ${module.symbolFilePath}\n`,
+      );
+    }
+
+    treeItem.tooltip = tooltip;
+    return treeItem;
+  }
+
+  getChildren(): DebugProtocol.Module[] {
+    if (!vscode.debug.activeDebugSession) {
+      return [];
+    }
+
+    return this.tracker.debugSessionModules(vscode.debug.activeDebugSession);
+  }
+}
