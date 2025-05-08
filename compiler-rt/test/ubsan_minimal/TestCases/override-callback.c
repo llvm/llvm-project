95316c976ce39420b80480e8a45ae41a7dfea291
Vitaly Buka <vitalybuka@google.com>
2025-05-07 17:43:29 -0700

    [𝘀𝗽𝗿] initial version
    Created using spr 1.3.4

--- compiler-rt/test/ubsan_minimal/TestCases/override-callback.c
+++ compiler-rt/test/ubsan_minimal/TestCases/override-callback.c
@@ -1,4 +1,6 @@
-// RUN: %clang -fsanitize=implicit-integer-sign-change %s -o %t && %run %t 0 2>&1 | FileCheck %s
+// RUN: %clang -fsanitize=implicit-integer-sign-change                           %s -o %t &&             %run %t 2>&1 | FileCheck %s
+// RUN: %clang -fsanitize=implicit-integer-sign-change -fno-sanitize-recover=all %s -o %t && not --crash %run %t 2>&1 | FileCheck %s --check-prefixes=FATAL
+
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,7 +11,12 @@ void __ubsan_report_error(const char *kind, uintptr_t caller) {
   fprintf(stderr, "CUSTOM_CALLBACK: %s\n", kind);
 }
 
+void __ubsan_report_error_fatal(const char *kind, uintptr_t caller) {
+  fprintf(stderr, "FATAL_CALLBACK: %s\n", kind);
+}
+
 int main(int argc, const char **argv) {
   int32_t t0 = (~((uint32_t)0));
   // CHECK: CUSTOM_CALLBACK: implicit-conversion
+  // FATAL: FATAL_CALLBACK: implicit-conversion
 }
