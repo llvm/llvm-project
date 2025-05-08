6b522d505a94ad472fe1c69fe15de218015c41a6
Lei Wang <wlei@fb.com>
2025-05-08 10:51:38 -0700

    [Coverage] Support -fprofile-list for cold function coverage (#136333)
    Add a new instrumentation section type `[sample-coldcov]` to
    support`-fprofile-list` for sample pgo based cold function coverage.
    Note that the current cold function coverage is based on sampling PGO
    pipeline, which is incompatible with the existing [llvm] option(see
    [PGOOptions](https://github.com/llvm/llvm-project/blob/main/llvm/include/llvm/Support/PGOOptions.h#L27-L43)),
    so we can't reuse the IR-PGO(-fprofile-instrument=llvm) flag.

--- clang/include/clang/Basic/CodeGenOptions.h
+++ clang/include/clang/Basic/CodeGenOptions.h
@@ -86,6 +86,8 @@ public:
                        // to use with PGO.
     ProfileIRInstr,    // IR level PGO instrumentation in LLVM.
     ProfileCSIRInstr, // IR level PGO context sensitive instrumentation in LLVM.
+    ProfileIRSampleColdCov, // IR level sample pgo based cold function coverage
+                            // instrumentation in LLVM.
   };
 
   enum EmbedBitcodeKind {
