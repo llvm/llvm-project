4333e6c2c73a99ca946163149692c933a9d61494
zishan <zmirza@posteo.ch>
2025-05-09 18:35:32 +0200

    update comment

--- libc/src/time/linux/localtime_r.cpp
+++ libc/src/time/linux/localtime_r.cpp
@@ -1,4 +1,4 @@
-//===-- Implementation of localtime_r function ----------------------------===//
+//===-- Linux implementation of localtime_r function ----------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
