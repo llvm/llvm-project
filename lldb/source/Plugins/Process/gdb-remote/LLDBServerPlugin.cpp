095224c0df2782075d2ea587c61754ee6c277288
Greg Clayton <gclayton@fb.com>
2025-05-09 14:03:22 -0700

    Add the ability to halt the native process from the GPU plug-in.
    If the GPU plug-ins need to halt the process so that the GPU plug-in
    will receive a LLDBServerPlugin::NativeProcessIsStopping(...) call, then
    it can call this function. This allows synchronization with the native
    process and the GPU plug-in can return GPUActions to perform.

--- lldb/source/Plugins/Process/gdb-remote/LLDBServerPlugin.cpp
+++ lldb/source/Plugins/Process/gdb-remote/LLDBServerPlugin.cpp
@@ -8,6 +8,8 @@
 
 #include "LLDBServerPlugin.h"
 #include "GDBRemoteCommunicationServerLLGS.h"
+#include <chrono>
+#include <thread>
 
 using namespace lldb_private;
 using namespace lldb_server;
@@ -17,3 +19,23 @@ LLDBServerPlugin::LLDBServerPlugin(GDBServer &native_process) :
   m_native_process(native_process) {}
 
 LLDBServerPlugin::~LLDBServerPlugin() {}
+
+
+lldb::StateType 
+LLDBServerPlugin::HaltNativeProcessIfNeeded(bool &was_halted, 
+                                            uint32_t timeout_sec) {
+  using namespace std::chrono;
+  NativeProcessProtocol *process = m_native_process.GetCurrentProcess();
+  if (process->IsRunning()) {
+    was_halted = true;
+    process->Halt();
+ 
+    auto end_time = steady_clock::now() + seconds(timeout_sec);
+    while (std::chrono::steady_clock::now() < end_time) {
+      std::this_thread::sleep_for(milliseconds(250));
+      if (process->IsStopped())
+        break;
+    }
+  }
+  return process->GetState();
+}
