4b794400d431e9a181ba50a8d6b6f5804d53ab81
Jonas Devlieghere <jonas@devlieghere.com>
2025-05-09 13:39:00 -0700

    Fix discrepancy between black and darker

--- lldb/test/API/tools/lldb-dap/module-event/TestDAP_module_event.py
+++ lldb/test/API/tools/lldb-dap/module-event/TestDAP_module_event.py
@@ -7,7 +7,6 @@ import re
 
 
 class TestDAP_module_event(lldbdap_testcase.DAPTestCaseBase):
-
     def test_module_event(self):
         program = self.getBuildArtifact("a.out")
         self.build_and_launch(program, stopOnEntry=True)
