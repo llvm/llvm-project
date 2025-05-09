723c91371c27ce32a9f0a8717837ea06a6a13988
Philipp Jung <philipp.jung01@sap.com>
2025-05-09 16:04:38 +0200

    Add option to ignore unscoped enums in classes

--- clang-tools-extra/clang-tidy/cppcoreguidelines/UseEnumClassCheck.h
+++ clang-tools-extra/clang-tidy/cppcoreguidelines/UseEnumClassCheck.h
@@ -20,16 +20,19 @@ namespace clang::tidy::cppcoreguidelines {
 /// http://clang.llvm.org/extra/clang-tidy/checks/cppcoreguidelines/use-enum-class.html
 class UseEnumClassCheck : public ClangTidyCheck {
 public:
-  UseEnumClassCheck(StringRef Name, ClangTidyContext *Context)
-      : ClangTidyCheck(Name, Context) {}
+  UseEnumClassCheck(StringRef Name, ClangTidyContext *Context);
+  void storeOptions(ClangTidyOptions::OptionMap &Opts) override;
   void registerMatchers(ast_matchers::MatchFinder *Finder) override;
   void check(const ast_matchers::MatchFinder::MatchResult &Result) override;
   bool isLanguageVersionSupported(const LangOptions &LangOpts) const override {
-    return LangOpts.CPlusPlus;
+    return LangOpts.CPlusPlus11;
   }
   std::optional<TraversalKind> getCheckTraversalKind() const override {
     return TraversalKind::TK_IgnoreUnlessSpelledInSource;
   }
+
+private:
+  const bool IgnoreUnscopedEnumsInClasses;
 };
 
 } // namespace clang::tidy::cppcoreguidelines
