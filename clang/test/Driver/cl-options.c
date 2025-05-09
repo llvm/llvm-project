46662864a2a2ab4392284d9a2a0d1fbbae3c0228
Daniel Paoliello <danpao@microsoft.com>
2025-05-09 10:42:10 -0700

    [win][x64] Unwind v2 3/n: Add support for emitting unwind v2 information (equivalent to MSVC /d2epilogunwind) (#129142)
    Adds support for emitting Windows x64 Unwind V2 information, includes
    support `/d2epilogunwind` in clang-cl.
    
    Unwind v2 adds information about the epilogs in functions such that the
    unwinder can unwind even in the middle of an epilog, without having to
    disassembly the function to see what has or has not been cleaned up.
    
    Unwind v2 requires that all epilogs are in "canonical" form:
    * If there was a stack allocation (fixed or dynamic) in the prolog, then
    the first instruction in the epilog must be a stack deallocation.
    * Next, for each `PUSH` in the prolog there must be a corresponding
    `POP` instruction in exact reverse order.
    * Finally, the epilog must end with the terminator.
    
    This change adds a pass to validate epilogs in modules that have Unwind
    v2 enabled and, if they pass, emits new pseudo instructions to MC that
    1) note that the function is using unwind v2 and 2) mark the start of
    the epilog (this is either the first `POP` if there is one, otherwise
    the terminator instruction). If a function does not meet these
    requirements, it is downgraded to Unwind v1 (i.e., these new pseudo
    instructions are not emitted).
    
    Note that the unwind v2 table only marks the size of the epilog in the
    "header" unwind code, but it's possible for epilogs to use different
    terminator instructions thus they are not all the same size. As a work
    around for this, MC will assume that all terminator instructions are
    1-byte long - this still works correctly with the Windows unwinder as it
    is only using the size to do a range check to see if a thread is in an
    epilog or not, and since the instruction pointer will never be in the
    middle of an instruction and the terminator is always at the end of an
    epilog the range check will function correctly. This does mean, however,
    that the "at end" optimization (where an epilog unwind code can be
    elided if the last epilog is at the end of the function) can only be
    used if the terminator is 1-byte long.
    
    One other complication with the implementation is that the unwind table
    for a function is emitted during streaming, however we can't calculate
    the distance between an epilog and the end of the function at that time
    as layout hasn't been completed yet (thus some instructions may be
    relaxed). To work around this, epilog unwind codes are emitted via a
    fixup. This also means that we can't pre-emptively downgrade a function
    to Unwind v1 if one of these offsets is too large, so instead we raise
    an error (but I've passed through the location information, so the user
    will know which of their functions is problematic).

--- clang/test/Driver/cl-options.c
+++ clang/test/Driver/cl-options.c
@@ -820,4 +820,7 @@
 // RUN: %clang_cl -vctoolsdir "" /arm64EC /c -target x86_64-pc-windows-msvc  -### -- %s 2>&1 | FileCheck --check-prefix=ARM64EC_OVERRIDE %s
 // ARM64EC_OVERRIDE: warning: /arm64EC has been overridden by specified target: x86_64-pc-windows-msvc; option ignored
 
+// RUN: %clang_cl /d2epilogunwind /c -### -- %s 2>&1 | FileCheck %s --check-prefix=EPILOGUNWIND
+// EPILOGUNWIND: -fwinx64-eh-unwindv2
+
 void f(void) { }
