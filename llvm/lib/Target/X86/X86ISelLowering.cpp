dc8c51b09ec018a5144b8306558321c8d7a52539
Yingwei Zheng <dtcxzyw2333@gmail.com>
2025-05-10 13:14:01 +0800

    [X86][TargetLowering] Avoid deleting temporary nodes in `getNegatedExpression` (#139029)
    In the original case, the third call to `getCheaperNegatedExpression`
    deletes the SDNode returned by the first call.
    Similar to 74e6030bcbcc8e628f9a99a424342a0c656456f9, this patch uses
    `HandleSDNodes` to prevent nodes from being deleted by subsequent calls.
    Closes https://github.com/llvm/llvm-project/issues/138944.

--- llvm/lib/Target/X86/X86ISelLowering.cpp
+++ llvm/lib/Target/X86/X86ISelLowering.cpp
@@ -54726,12 +54726,19 @@ SDValue X86TargetLowering::getNegatedExpression(SDValue Op, SelectionDAG &DAG,
     if (!Flags.hasNoSignedZeros())
       break;
 
+    // Because getCheaperNegatedExpression can delete nodes we need a handle to
+    // keep temporary nodes alive.
+    std::list<HandleSDNode> Handles;
+
     // This is always negatible for free but we might be able to remove some
     // extra operand negations as well.
     SmallVector<SDValue, 4> NewOps(Op.getNumOperands(), SDValue());
-    for (int i = 0; i != 3; ++i)
+    for (int i = 0; i != 3; ++i) {
       NewOps[i] = getCheaperNegatedExpression(
           Op.getOperand(i), DAG, LegalOperations, ForCodeSize, Depth + 1);
+      if (!!NewOps[i])
+        Handles.emplace_back(NewOps[i]);
+    }
 
     bool NegA = !!NewOps[0];
     bool NegB = !!NewOps[1];
