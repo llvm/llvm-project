b547fe821f9c6908d027ce8ae952a51e04f44e7a
Jason Molenda <jmolenda@apple.com>
2025-05-09 20:18:37 -0700

    Stop running test on Linux for now
    Failed at compile time lldb-aarch64-ubuntu bot.
    It did clang -E -o interrupt-and-trap-funcs.s interrupt-and-trap-funcs.c
    and that added a bunch of standard C header typedefs to
    the output .s file which then turn into compile errors
    when it tries to compile the .s file as assembly.  Never saw
    that behavior in my testing on an ubuntu 24.04 system.
    
    It would have been nice to have the test run on Linux as well
    as Darwin, but it's not essential.

--- lldb/test/API/functionalities/unwind/frameless-faulted/TestUnwindFramelessFaulted.py
+++ lldb/test/API/functionalities/unwind/frameless-faulted/TestUnwindFramelessFaulted.py
@@ -10,7 +10,7 @@ import os
 class TestUnwindFramelessFaulted(TestBase):
     NO_DEBUG_INFO_TESTCASE = True
 
-    @skipIf(oslist=no_match([lldbplatformutil.getDarwinOSTriples(), "linux"]))
+    @skipIf(oslist=no_match([lldbplatformutil.getDarwinOSTriples()]))
     @skipIf(archs=no_match(["aarch64", "arm64", "arm64e"]))
     def test_frameless_faulted_unwind(self):
         self.build()
