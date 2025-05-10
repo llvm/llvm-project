baadbee5023bee2d7cc8c54a77a8124f37538b90
Kazu Hirata <kazu@google.com>
2025-05-09 21:50:27 -0700

    [MC] Use range-based for loops (NFC)

--- llvm/lib/MC/MCContext.cpp
+++ llvm/lib/MC/MCContext.cpp
@@ -477,10 +477,10 @@ MCSymbolXCOFF *MCContext::createXCOFFSymbolImpl(const MCSymbolTableEntry *Name,
 
   // Append the hex values of '_' and invalid characters with "_Renamed..";
   // at the same time replace invalid characters with '_'.
-  for (size_t I = 0; I < InvalidName.size(); ++I) {
-    if (!MAI->isAcceptableChar(InvalidName[I]) || InvalidName[I] == '_') {
-      raw_svector_ostream(ValidName).write_hex(InvalidName[I]);
-      InvalidName[I] = '_';
+  for (char &I : InvalidName) {
+    if (!MAI->isAcceptableChar(I) || I == '_') {
+      raw_svector_ostream(ValidName).write_hex(I);
+      I = '_';
     }
   }
 
