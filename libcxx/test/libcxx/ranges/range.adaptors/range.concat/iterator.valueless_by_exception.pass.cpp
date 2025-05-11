901a0ea67adcc498c5e67884558fac52a01a7835
changkhothuychung <nhat7203@gmail.com>
2025-05-11 03:51:32 -0400

    clang format

--- libcxx/test/libcxx/ranges/range.adaptors/range.concat/iterator.valueless_by_exception.pass.cpp
+++ libcxx/test/libcxx/ranges/range.adaptors/range.concat/iterator.valueless_by_exception.pass.cpp
@@ -22,133 +22,150 @@ int val[] = {1, 2, 3};
 
 bool flag = false;
 
-template<std::size_t N> struct Iter;
+template <std::size_t N>
+struct Iter;
 
-template<std::size_t N>
+template <std::size_t N>
 struct Iter {
-    using value_type = int;
-    using difference_type = std::ptrdiff_t;
-    using reference = int&;
-    using pointer = int*;
-    using iterator_category = std::random_access_iterator_tag;
-    using iterator_concept = std::random_access_iterator_tag;
+  using value_type        = int;
+  using difference_type   = std::ptrdiff_t;
+  using reference         = int&;
+  using pointer           = int*;
+  using iterator_category = std::random_access_iterator_tag;
+  using iterator_concept  = std::random_access_iterator_tag;
 
 private:
-    int* ptr_ = nullptr;
+  int* ptr_ = nullptr;
 
-    template<std::size_t M> friend struct Iter;
+  template <std::size_t M>
+  friend struct Iter;
 
 public:
-    Iter() = default;
-    Iter(int* ptr) : ptr_(ptr) {}
-    Iter(const Iter&) = default;
-    Iter(Iter&& other) noexcept : ptr_(other.ptr_) {}
+  Iter() = default;
+  Iter(int* ptr) : ptr_(ptr) {}
+  Iter(const Iter&) = default;
+  Iter(Iter&& other) noexcept : ptr_(other.ptr_) {}
 
-    template<std::size_t M>
-    Iter(const Iter<M>& other) : ptr_(other.ptr_) {}
+  template <std::size_t M>
+  Iter(const Iter<M>& other) : ptr_(other.ptr_) {}
 
-    Iter& operator=(const Iter&) = default;
-    Iter& operator=(Iter&& other) noexcept {
-        ptr_ = other.ptr_;
-        return *this;
-    }
+  Iter& operator=(const Iter&) = default;
+  Iter& operator=(Iter&& other) noexcept {
+    ptr_ = other.ptr_;
+    return *this;
+  }
 
-    reference operator*() const { return *ptr_; }
-    pointer operator->() const { return ptr_; }
-    reference operator[](difference_type n) const { return ptr_[n]; }
+  reference operator*() const { return *ptr_; }
+  pointer operator->() const { return ptr_; }
+  reference operator[](difference_type n) const { return ptr_[n]; }
 
-    Iter& operator++() { ++ptr_; return *this; }
-    Iter operator++(int) { auto tmp = *this; ++*this; return tmp; }
-    Iter& operator--() { --ptr_; return *this; }
-    Iter operator--(int) { auto tmp = *this; --*this; return tmp; }
+  Iter& operator++() {
+    ++ptr_;
+    return *this;
+  }
+  Iter operator++(int) {
+    auto tmp = *this;
+    ++*this;
+    return tmp;
+  }
+  Iter& operator--() {
+    --ptr_;
+    return *this;
+  }
+  Iter operator--(int) {
+    auto tmp = *this;
+    --*this;
+    return tmp;
+  }
 
-    Iter& operator+=(difference_type n) { ptr_ += n; return *this; }
-    Iter& operator-=(difference_type n) { ptr_ -= n; return *this; }
+  Iter& operator+=(difference_type n) {
+    ptr_ += n;
+    return *this;
+  }
+  Iter& operator-=(difference_type n) {
+    ptr_ -= n;
+    return *this;
+  }
 
-    template<std::size_t X>
-    friend Iter<X> operator+(Iter<X> it, difference_type n);
+  template <std::size_t X>
+  friend Iter<X> operator+(Iter<X> it, difference_type n);
 
-    template<std::size_t X>
-    friend Iter<X> operator+(difference_type n, Iter<X> it);
+  template <std::size_t X>
+  friend Iter<X> operator+(difference_type n, Iter<X> it);
 
-    template<std::size_t X>
-    friend Iter<X> operator-(Iter<X> it, difference_type n);
+  template <std::size_t X>
+  friend Iter<X> operator-(Iter<X> it, difference_type n);
 
-    template<std::size_t X, std::size_t Y>
-    friend difference_type operator-(Iter<X> a, Iter<Y> b);
+  template <std::size_t X, std::size_t Y>
+  friend difference_type operator-(Iter<X> a, Iter<Y> b);
 
-    friend bool operator==(Iter a, Iter b) = default;
-    friend bool operator<(Iter a, Iter b) { return a.ptr_ < b.ptr_; }
-    friend bool operator>(Iter a, Iter b) { return a.ptr_ > b.ptr_; }
-    friend bool operator<=(Iter a, Iter b) { return a.ptr_ <= b.ptr_; }
-    friend bool operator>=(Iter a, Iter b) { return a.ptr_ >= b.ptr_; }
+  friend bool operator==(Iter a, Iter b) = default;
+  friend bool operator<(Iter a, Iter b) { return a.ptr_ < b.ptr_; }
+  friend bool operator>(Iter a, Iter b) { return a.ptr_ > b.ptr_; }
+  friend bool operator<=(Iter a, Iter b) { return a.ptr_ <= b.ptr_; }
+  friend bool operator>=(Iter a, Iter b) { return a.ptr_ >= b.ptr_; }
 };
 
-template<std::size_t X>
+template <std::size_t X>
 inline Iter<X> operator+(Iter<X> it, std::ptrdiff_t n) {
-    return Iter<X>(it.ptr_ + n);
+  return Iter<X>(it.ptr_ + n);
 }
 
-template<std::size_t X>
+template <std::size_t X>
 inline Iter<X> operator+(std::ptrdiff_t n, Iter<X> it) {
-    return Iter<X>(it.ptr_ + n);
+  return Iter<X>(it.ptr_ + n);
 }
 
-template<std::size_t X>
+template <std::size_t X>
 inline Iter<X> operator-(Iter<X> it, std::ptrdiff_t n) {
-    return Iter<X>(it.ptr_ - n);
+  return Iter<X>(it.ptr_ - n);
 }
 
-template<std::size_t X, std::size_t Y>
+template <std::size_t X, std::size_t Y>
 inline std::ptrdiff_t operator-(Iter<X> a, Iter<Y> b) {
-    return a.ptr_ - b.ptr_;
+  return a.ptr_ - b.ptr_;
 }
 
-template<std::size_t N>
+template <std::size_t N>
 struct Range : std::ranges::view_base {
-    using iterator = Iter<N>;
-    using const_iterator = Iter<N>;
+  using iterator       = Iter<N>;
+  using const_iterator = Iter<N>;
 
-    int* data_;
-    std::size_t size_;
+  int* data_;
+  std::size_t size_;
 
-    Range() : data_(val), size_(4) {}
+  Range() : data_(val), size_(4) {}
 
-    Range(int* data, std::size_t size) : data_(data), size_(size) {}
+  Range(int* data, std::size_t size) : data_(data), size_(size) {}
 
-    iterator begin() { return iterator(data_); }
-    iterator end() { return iterator(data_ + size_); }
+  iterator begin() { return iterator(data_); }
+  iterator end() { return iterator(data_ + size_); }
 
-    const_iterator begin() const { return const_iterator(data_); }
-    const_iterator end() const { return const_iterator(data_ + size_); }
+  const_iterator begin() const { return const_iterator(data_); }
+  const_iterator end() const { return const_iterator(data_ + size_); }
 
-    std::size_t size() const { return size_; }
+  std::size_t size() const { return size_; }
 };
 
 static_assert(std::ranges::range<Range<0>>);
 static_assert(std::ranges::sized_range<Range<0>>);
 
 int main() {
-
   {
     //valueless by exception test operator*
     Range<0> r1;
     Range<1> r2;
 
-    auto cv = std::views::concat(r1, r2);
+    auto cv    = std::views::concat(r1, r2);
     auto iter1 = cv.begin();
     auto iter2 = std::ranges::next(cv.begin(), 4);
-    flag = true;
+    flag       = true;
     try {
-        iter1 = std::move(iter2);
+      iter1 = std::move(iter2);
     } catch (...) {
       auto f = std::ranges::distance(r1);
       (void)f;
-      TEST_LIBCPP_ASSERT_FAILURE(
-          [=] {
-            *iter1;
-          }(),
-          "valueless by exception");
+      TEST_LIBCPP_ASSERT_FAILURE([=] { *iter1; }(), "valueless by exception");
     }
   }
 
@@ -158,19 +175,15 @@ int main() {
     Range<0> r1;
     Range<1> r2;
 
-    auto cv = std::views::concat(r1, r2);
+    auto cv    = std::views::concat(r1, r2);
     auto iter1 = cv.begin();
     auto iter2 = std::ranges::next(cv.begin(), 4);
     auto iter3 = cv.begin();
-    flag = true;
+    flag       = true;
     try {
-        iter1 = std::move(iter2);
+      iter1 = std::move(iter2);
     } catch (...) {
-      TEST_LIBCPP_ASSERT_FAILURE(
-          [=] {
-            (void)(iter1 == iter3);
-          }(),
-          "valueless by exception");
+      TEST_LIBCPP_ASSERT_FAILURE([=] { (void)(iter1 == iter3); }(), "valueless by exception");
     }
   }
 
@@ -180,19 +193,15 @@ int main() {
     Range<0> r1;
     Range<1> r2;
 
-    auto cv = std::views::concat(r1, r2);
+    auto cv    = std::views::concat(r1, r2);
     auto iter1 = cv.begin();
     auto iter2 = std::ranges::next(cv.begin(), 4);
-    flag = true;
+    flag       = true;
     try {
-        iter1 = std::move(iter2);
+      iter1 = std::move(iter2);
     } catch (...) {
       //ASSERT_SAME_TYPE(decltype(iter1), int);
-      TEST_LIBCPP_ASSERT_FAILURE(
-          [&] {
-            iter1--;
-          }(),
-          "valueless by exception");
+      TEST_LIBCPP_ASSERT_FAILURE([&] { iter1--; }(), "valueless by exception");
     }
   }
 
@@ -202,18 +211,14 @@ int main() {
     Range<0> r1;
     Range<1> r2;
 
-    auto cv = std::views::concat(r1, r2);
+    auto cv    = std::views::concat(r1, r2);
     auto iter1 = cv.begin();
     auto iter2 = std::ranges::next(cv.begin(), 4);
-    flag = true;
+    flag       = true;
     try {
-        iter1 = std::move(iter2);
+      iter1 = std::move(iter2);
     } catch (...) {
-      TEST_LIBCPP_ASSERT_FAILURE(
-          [&] {
-            ++iter1;
-          }(),
-          "valueless by exception");
+      TEST_LIBCPP_ASSERT_FAILURE([&] { ++iter1; }(), "valueless by exception");
     }
   }
 
@@ -223,18 +228,14 @@ int main() {
     Range<0> r1;
     Range<1> r2;
 
-    auto cv = std::views::concat(r1, r2);
+    auto cv    = std::views::concat(r1, r2);
     auto iter1 = cv.begin();
     auto iter2 = std::ranges::next(cv.begin(), 4);
-    flag = true;
+    flag       = true;
     try {
-        iter1 = std::move(iter2);
+      iter1 = std::move(iter2);
     } catch (...) {
-      TEST_LIBCPP_ASSERT_FAILURE(
-          [&] {
-            iter1 += 1;
-          }(),
-          "valueless by exception");
+      TEST_LIBCPP_ASSERT_FAILURE([&] { iter1 += 1; }(), "valueless by exception");
     }
   }
 
@@ -244,12 +245,12 @@ int main() {
     Range<0> r1;
     Range<1> r2;
 
-    auto cv = std::views::concat(r1, r2);
+    auto cv    = std::views::concat(r1, r2);
     auto iter1 = cv.begin();
     auto iter2 = std::ranges::next(cv.begin(), 4);
-    flag = true;
+    flag       = true;
     try {
-        iter1 = std::move(iter2);
+      iter1 = std::move(iter2);
     } catch (...) {
       TEST_LIBCPP_ASSERT_FAILURE(
           [&] {
