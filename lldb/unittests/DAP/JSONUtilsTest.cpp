20003579295e7489f429593e8837000efa7561a4
Jonas Devlieghere <jonas@devlieghere.com>
2025-05-11 16:08:51 -0700

    [lldb-dap] Add unit tests for GetString and GetBoolean
    These are simple functions but they're used a lot. Add some simple unit
    tests for them.

--- lldb/unittests/DAP/JSONUtilsTest.cpp
+++ lldb/unittests/DAP/JSONUtilsTest.cpp
@@ -16,9 +16,72 @@ using namespace lldb;
 using namespace lldb_dap;
 
 TEST(JSONUtilsTest, GetAsString) {
-  StringRef str = "foo";
-  json::Value value("foo");
-  EXPECT_EQ(str, GetAsString(value));
+  json::Value string_value("foo");
+  EXPECT_EQ(GetAsString(string_value), "foo");
+
+  json::Value int_value(42);
+  EXPECT_EQ(GetAsString(int_value), "");
+
+  json::Value null_value(nullptr);
+  EXPECT_EQ(GetAsString(null_value), "");
+}
+
+TEST(JSONUtilsTest, GetString_Ref) {
+  json::Object obj;
+  obj.try_emplace("key", "value");
+
+  auto result = GetString(obj, "key");
+  ASSERT_TRUE(result.has_value());
+  EXPECT_EQ(result.value(), "value");
+
+  result = GetString(obj, "nonexistent_key");
+  EXPECT_FALSE(result.has_value());
+}
+
+TEST(JSONUtilsTest, GetString_Pointer) {
+  json::Object obj;
+  obj.try_emplace("key", "value");
+
+  auto result = GetString(&obj, "key");
+  ASSERT_TRUE(result.has_value());
+  EXPECT_EQ(result.value(), "value");
+
+  result = GetString(nullptr, "key");
+  EXPECT_FALSE(result.has_value());
+}
+
+TEST(JSONUtilsTest, GetBoolean_Ref) {
+  json::Object obj;
+  obj.try_emplace("key_true", true);
+  obj.try_emplace("key_false", false);
+  obj.try_emplace("key_int", 1);
+
+  auto result = GetBoolean(obj, "key_true");
+  ASSERT_TRUE(result.has_value());
+  EXPECT_TRUE(result.value());
+
+  result = GetBoolean(obj, "key_false");
+  ASSERT_TRUE(result.has_value());
+  EXPECT_FALSE(result.value());
+
+  result = GetBoolean(obj, "key_int");
+  ASSERT_TRUE(result.has_value());
+  EXPECT_TRUE(result.value());
+
+  result = GetBoolean(obj, "nonexistent_key");
+  EXPECT_FALSE(result.has_value());
+}
+
+TEST(JSONUtilsTest, GetBoolean_Pointer) {
+  json::Object obj;
+  obj.try_emplace("key", true);
+
+  auto result = GetBoolean(&obj, "key");
+  ASSERT_TRUE(result.has_value());
+  EXPECT_TRUE(result.value());
+
+  result = GetBoolean(nullptr, "key");
+  EXPECT_FALSE(result.has_value());
 }
 
 TEST(JSONUtilsTest, CreateModule) {
