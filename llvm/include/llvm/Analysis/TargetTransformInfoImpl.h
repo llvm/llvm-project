ec9f59bf3a91594b502292b6a6ac9c81d3dae6a5
Elvis Wang <elvis.wang@sifive.com>
2025-05-11 20:02:56 -0700

    [TTI] Add `preferFlattenControlFlow` for loop vectorizer. (NFC)
    This patch add a attribute in TTI to let LV knows which is better.
    
    Default value of preferControlFlowVectorization is false to match current TTI
    implementation.
    
    If preferFlattenControlFlow() return true, LV will try to generate
    conditional VPBB if possible.

--- llvm/include/llvm/Analysis/TargetTransformInfoImpl.h
+++ llvm/include/llvm/Analysis/TargetTransformInfoImpl.h
@@ -1180,6 +1180,8 @@ public:
 
   virtual bool shouldConsiderVectorizationRegPressure() const { return false; }
 
+  virtual bool preferControlFlowVectorization() const { return false; }
+
   virtual bool shouldExpandReduction(const IntrinsicInst *II) const {
     return true;
   }
