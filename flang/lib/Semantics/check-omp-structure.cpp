3e6d5c3cd14a9b688ec6e35a4bd4436fe944860b
Tom Eccles <tom.eccles@arm.com>
2025-05-12 10:13:08 +0000

    [flang][OpenMP] Allow flush of common block
    I think this was denied by accident in 68180d8.
    
    Flush of a common block is allowed by the standard on my reading. It is
    not allowed by classic-flang but is supported by gfortran and ifx.
    
    This doesn't need any lowering changes. The LLVM translation ignores the
    flush argument list because the openmp runtime library doesn't support
    flushing specific data.
    
    Depends upon #139522. Ignore the first commit in this PR.

--- flang/lib/Semantics/check-omp-structure.cpp
+++ flang/lib/Semantics/check-omp-structure.cpp
@@ -2304,13 +2304,6 @@ void OmpStructureChecker::Leave(const parser::OpenMPFlushConstruct &x) {
   auto &flushList{std::get<std::optional<parser::OmpArgumentList>>(x.v.t)};
 
   if (flushList) {
-    for (const parser::OmpArgument &arg : flushList->v) {
-      if (auto *sym{GetArgumentSymbol(arg)}; sym && !IsVariableListItem(*sym)) {
-        context_.Say(arg.source,
-            "FLUSH argument must be a variable list item"_err_en_US);
-      }
-    }
-
     if (FindClause(llvm::omp::Clause::OMPC_acquire) ||
         FindClause(llvm::omp::Clause::OMPC_release) ||
         FindClause(llvm::omp::Clause::OMPC_acq_rel)) {
