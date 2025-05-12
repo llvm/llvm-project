5e94e26a7afb8db00cc123e5fc5471c1125596e3
halbi2 <hehiralbi@gmail.com>
2025-05-12 16:35:05 -0400

    [libc++] Improve test coverage for containers' scary.pass.cpp (#131550)

--- libcxx/test/libcxx/containers/associative/unord.map/scary.compile.pass.cpp
+++ libcxx/test/libcxx/containers/associative/unord.map/scary.compile.pass.cpp
@@ -8,20 +8,21 @@
 
 // <unordered_map>
 
-// class unordered_map class unordered_multimap
+// class unordered_map
+// class unordered_multimap
 
-// Extension:  SCARY/N2913 iterator compatibility between unordered_map and unordered_multimap
+// Extension: SCARY/N2913 iterator compatibility between unordered_map and unordered_multimap
 
 #include <unordered_map>
 
 #include "test_macros.h"
 
-int main(int, char**) {
+void test() {
   typedef std::unordered_map<int, int> M1;
   typedef std::unordered_multimap<int, int> M2;
-  M2::iterator i;
-  M1::iterator j = i;
-  ((void)j);
 
-  return 0;
+  ASSERT_SAME_TYPE(M1::iterator, M2::iterator);
+  ASSERT_SAME_TYPE(M1::const_iterator, M2::const_iterator);
+  ASSERT_SAME_TYPE(M1::local_iterator, M2::local_iterator);
+  ASSERT_SAME_TYPE(M1::const_local_iterator, M2::const_local_iterator);
 }
