f56ba5d28ae8a59e5a660426bd31e6fb5e50af9b
Fraser Cormack <fraser@codeplay.com>
2025-05-13 11:47:35 +0100

    [libclc] Move logb/ilogb to CLC library; optimize (#128028)
    This commit moves the logb and ilogb builtins to the CLC library.
    
    It simultaneously optimizes them both for vector types and for half
    types. Vector types were being scalarized in some cases. Half types were
    previously promoting to float, whereas this commit provides them a
    native implementation.
    
    Everything passes the OpenCL-CTS.
    
    I had to intuit some magic numbers used by these implementations in
    order to generate the half variants. I gave them clearer definitions
    derived from what I believe are their actual component numbers, but
    named them 'magic' to convey that they weren't derived from first
    principles.

--- libclc/clc/include/clc/math/clc_logb.h
+++ libclc/clc/include/clc/math/clc_logb.h
@@ -6,9 +6,13 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include <clc/clc.h>
-#include <clc/math/clc_logb.h>
+#ifndef __CLC_MATH_CLC_LOGB_H__
+#define __CLC_MATH_CLC_LOGB_H__
 
-#define FUNCTION logb
-#define __CLC_BODY <clc/shared/unary_def.inc>
+#define __CLC_FUNCTION __clc_logb
+#define __CLC_BODY <clc/shared/unary_decl.inc>
 #include <clc/math/gentype.inc>
+
+#undef __CLC_FUNCTION
+
+#endif // __CLC_MATH_CLC_LOGB_H__
