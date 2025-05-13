406357f715b6e8de150b449a6d22718431ac8439
Jan Svoboda <jan_svoboda@apple.com>
2025-05-13 09:50:29 -0700

    Check for error messages

--- clang/test/Modules/sdk-settings-json-dep.m
+++ clang/test/Modules/sdk-settings-json-dep.m
@@ -46,4 +46,8 @@ void test() { foo(); }
 // RUN: sleep 1
 // RUN: cp %t/AppleTVOS15.0.sdk/SDKSettings-new.json %t/AppleTVOS15.0.sdk/SDKSettings.json
 // RUN: not %clang -target x86_64-apple-tvos13.1 -isysroot %t/AppleTVOS15.0.sdk \
-// RUN:   -fsyntax-only %t/tu.m -o %t/tu.o -fmodules -Xclang -fdisable-module-hash -fmodules-cache-path=%t/cache
+// RUN:   -fsyntax-only %t/tu.m -o %t/tu.o -fmodules -Xclang -fdisable-module-hash -fmodules-cache-path=%t/cache 2>&1 \
+// RUN:     | FileCheck %s
+// CHECK: M.h:2:15: error: 'foo' is unavailable: obsoleted in tvOS 13.1
+// CHECK: M.h:1:6: note: 'foo' has been explicitly marked unavailable here
+// CHECK: tu.m:1:10: fatal error: could not build module 'M'
