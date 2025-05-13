75f3b2dfa38ca24e274868ce7a5f5ab00cb58e7f
Amirhossein Pashaeehir <amirhosseinp@google.com>
2025-05-13 00:17:57 +0000

    Extract semantic info on MCInst from bolt

--- llvm/tools/llvm-mc/CFIAnalysis.h
+++ llvm/tools/llvm-mc/CFIAnalysis.h
@@ -4,17 +4,21 @@
 #include "bolt/Core/MCPlusBuilder.h"
 #include "llvm/ADT/ArrayRef.h"
 #include "llvm/ADT/SmallVector.h"
+#include "llvm/ADT/StringExtras.h"
 #include "llvm/MC/MCContext.h"
 #include "llvm/MC/MCDwarf.h"
 #include "llvm/MC/MCExpr.h"
 #include "llvm/MC/MCInst.h"
 #include "llvm/MC/MCInstrInfo.h"
+#include "llvm/MC/MCRegister.h"
 #include "llvm/MC/MCRegisterInfo.h"
 #include "llvm/MC/MCStreamer.h"
 #include "llvm/MC/MCSubtargetInfo.h"
 #include "llvm/MC/TargetRegistry.h"
 #include "llvm/Support/Debug.h"
+#include "llvm/Support/Error.h"
 #include <memory>
+#include <optional>
 #include <set>
 
 namespace llvm {
@@ -60,7 +64,7 @@ public:
                                    Context.getSubtargetInfo()));
   }
 
-  void update(const MCDwarfFrameInfo &DwarfFrame, const MCInst &Inst,
+  void update(const MCDwarfFrameInfo &DwarfFrame, MCInst &Inst,
               ArrayRef<MCCFIInstruction> CFIDirectives) {
 
     const MCInstrDesc &MCInstInfo = MCII.get(Inst.getOpcode());
@@ -71,8 +75,8 @@ public:
     // FIXME this way of extracting uses is buggy:
     // for (unsigned I = 0; I < MCInstInfo.NumImplicitUses; I++)
     //   Reads.insert(MCInstInfo.implicit_uses()[I]);
-    // for (unsigned I = 0; I < MCInstInfo.NumImplicitDefs; I++)
-    //   Writes.insert(MCInstInfo.implicit_defs()[I]);
+    for (unsigned I = 0; I < MCInstInfo.NumImplicitDefs; I++)
+      Writes.insert(MCInstInfo.implicit_defs()[I]);
 
     for (unsigned I = 0; I < Inst.getNumOperands(); I++) {
       auto &&Operand = Inst.getOperand(I);
@@ -113,20 +117,136 @@ public:
     dbgs() << "^^^^^^^^^^^^^^^^ InsCFIs ^^^^^^^^^^^^^^^^\n";
     printUntilNextLine(Inst.getLoc().getPointer());
     dbgs() << "\n";
-    dbgs() << "-----------------------------------------\n";
-    dbgs() << "writes into: { ";
-    for (auto Reg : Writes) {
-      dbgs() << (int)Reg << " ";
+    dbgs() << "Op code: " << Inst.getOpcode() << "\n";
+    dbgs() << "--------------Operands Info--------------\n";
+    auto DefCount = MCInstInfo.getNumDefs();
+    for (unsigned I = 0; I < Inst.getNumOperands(); I++) {
+      dbgs() << "Operand #" << I << ", which will be "
+             << (I < DefCount ? "defined" : "used") << ", is a";
+      if (I == MCPB->getMemoryOperandNo(Inst)) {
+        dbgs() << " memory access, details are:\n";
+        auto X86MemoryOperand = MCPB->evaluateX86MemoryOperand(Inst);
+        dbgs() << "  Base Register{ reg#" << X86MemoryOperand->BaseRegNum
+               << " }";
+        dbgs() << " + (Index Register{ reg#" << X86MemoryOperand->IndexRegNum
+               << " }";
+        dbgs() << " * Scale{ value " << X86MemoryOperand->ScaleImm << " }";
+        dbgs() << ") + Displacement{ "
+               << (X86MemoryOperand->DispExpr
+                       ? "an expression"
+                       : "value " + itostr(X86MemoryOperand->DispImm))
+               << " }\n";
+        // TODO, it's not correct always, it cannot be assumed (or should be
+        // checked) that memory operands are flatten into 4 operands in MCInc.
+        I += 4;
+        continue;
+      }
+      auto &&Operand = Inst.getOperand(I);
+      if (Operand.isImm()) {
+        dbgs() << "n immediate with value " << Operand.getImm() << "\n";
+        continue;
+      }
+      if (Operand.isReg()) {
+        dbgs() << " reg#" << Operand.getReg() << "\n";
+        continue;
+      }
+      assert(Operand.isExpr());
+      dbgs() << "n unknown expression \n";
     }
-    dbgs() << "}\n";
-    dbgs() << "-----------------------------------------\n";
-    dbgs() << "isMoveMem2Reg: " << MCPB->isMoveMem2Reg(Inst) << "\n";
-    dbgs() << "-----------------------------------------\n";
-    dbgs() << "The CFA register is: " << CFAReg << "\n";
-    dbgs() << "The instruction does " << (ChangedCFA ? "" : "NOT ")
-           << "change the CFA.\n";
-    dbgs() << "The CFI directives does " << (GoingToChangeCFA ? "" : "NOT ")
-           << "change the CFA.\n";
+    if (MCInstInfo.NumImplicitDefs) {
+      dbgs() << "implicitly defines: { ";
+      for (unsigned I = 0; I < MCInstInfo.NumImplicitDefs; I++) {
+        dbgs() << "reg#" << MCInstInfo.implicit_defs()[I] << " ";
+      }
+      dbgs() << "}\n";
+    }
+    if (MCInstInfo.NumImplicitUses) {
+      dbgs() << "implicitly uses: { ";
+      for (unsigned I = 0; I < MCInstInfo.NumImplicitUses; I++) {
+        dbgs() << "reg#" << MCInstInfo.implicit_uses()[I] << " ";
+      }
+      dbgs() << "}\n";
+    }
+    dbgs() << "----------------Move Info----------------\n";
+    {   // move
+      { // Reg2Reg
+        MCPhysReg From, To;
+        if (MCPB->isRegToRegMove(Inst, From, To)) {
+          dbgs() << "It's a reg to reg move.\nFrom reg#" << From << " to reg#"
+                 << To << "\n";
+        } else if (MCInstInfo.isMoveReg()) {
+          dbgs() << "It's reg to reg move from MCInstInfo view but not from "
+                    "MCPlus view.\n";
+        }
+      }
+      if (MCPB->isConditionalMove(Inst)) {
+        dbgs() << "Its a conditional move.\n";
+      }
+      if (MCPB->isMoveMem2Reg(Inst)) {
+        dbgs() << "It's a move from memory to register\n";
+        assert(MCPB->getMemoryOperandNo(Inst) == 1);
+      }
+    }
+
+    dbgs() << "---------------Stack Info----------------\n";
+    { // stack
+      int32_t SrcImm = 0;
+      MCPhysReg Reg = 0;
+      uint16_t StackPtrReg = 0;
+      int64_t StackOffset = 0;
+      uint8_t Size = 0;
+      bool IsSimple = false;
+      bool IsIndexed = false;
+      bool IsLoad = false;
+      bool IsStore = false;
+      bool IsStoreFromReg = false;
+      if (MCPB->isStackAccess(Inst, IsLoad, IsStore, IsStoreFromReg, Reg,
+                              SrcImm, StackPtrReg, StackOffset, Size, IsSimple,
+                              IsIndexed)) {
+        dbgs() << "This instruction accesses the stack, the details are:\n";
+        dbgs() << "  Source immediate: " << SrcImm << "\n";
+        dbgs() << "  Source reg#" << Reg << "\n";
+        dbgs() << "  Stack pointer: reg#" << StackPtrReg << "\n";
+        dbgs() << "  Stack offset: " << StackOffset << "\n";
+        dbgs() << "  size: " << (int)Size << "\n";
+        dbgs() << "  Is simple: " << (IsSimple ? "yes" : "no") << "\n";
+        dbgs() << "  Is indexed: " << (IsIndexed ? "yes" : "no") << "\n";
+        dbgs() << "  Is load: " << (IsLoad ? "yes" : "no") << "\n";
+        dbgs() << "  Is store: " << (IsStore ? "yes" : "no") << "\n";
+        dbgs() << "  Is store from reg: " << (IsStoreFromReg ? "yes" : "no")
+               << "\n";
+      }
+      if (MCPB->isPush(Inst)) {
+        dbgs() << "This is a push instruction with size "
+               << MCPB->getPushSize(Inst) << "\n";
+      }
+      if (MCPB->isPop(Inst)) {
+        dbgs() << "This is a pop instruction with size "
+               << MCPB->getPopSize(Inst) << "\n";
+      }
+    }
+
+    dbgs() << "---------------Arith Info----------------\n";
+    { // arith
+      MutableArrayRef<MCInst> MAR = {Inst};
+      if (MCPB->matchAdd(MCPB->matchAnyOperand(), MCPB->matchAnyOperand())
+              ->match(*RI, *MCPB, MutableArrayRef<MCInst>(), -1)) {
+        dbgs() << "It is an addition instruction.\n";
+      } else if (MCInstInfo.isAdd()) {
+        dbgs() << "It is an addition from MCInstInfo view but not from MCPlus "
+                  "view.\n";
+      }
+      if (MCPB->isSUB(Inst)) {
+        dbgs() << "This is a subtraction.\n";
+      }
+    }
+
+    // dbgs() << "-----------------------------------------\n";
+    // dbgs() << "The CFA register is: " << CFAReg << "\n";
+    // dbgs() << "The instruction does " << (ChangedCFA ? "" : "NOT ")
+    //        << "change the CFA.\n";
+    // dbgs() << "The CFI directives does " << (GoingToChangeCFA ? "" : "NOT ")
+    //        << "change the CFA.\n";
     dbgs() << "vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv\n";
     ChangedCFA = false;
   }
