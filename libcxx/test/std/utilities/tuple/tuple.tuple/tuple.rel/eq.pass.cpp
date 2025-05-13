ab710f4b2c1850b81003531e8e7f8cd2396feb36
Hristo Hristov <hghristov.rmm@gmail.com>
2025-05-13 10:58:13 +0300

    Implement tuple

--- libcxx/test/std/utilities/tuple/tuple.tuple/tuple.rel/eq.pass.cpp
+++ libcxx/test/std/utilities/tuple/tuple.tuple/tuple.rel/eq.pass.cpp
@@ -11,19 +11,61 @@
 // template <class... Types> class tuple;
 
 // template<class... TTypes, class... UTypes>
-//   bool
-//   operator==(const tuple<TTypes...>& t, const tuple<UTypes...>& u);
+// constexpr bool operator==(const tuple<TTypes...>&, const tuple<UTypes...>&); // constexpr since C++14
+
+// constexpr bool operator==(const tuple<TTypes...>&, const UTuple&);
+// template<class... TTypes, class... UTypes>                                   // Since C++23
 
 // UNSUPPORTED: c++03
 
-#include <tuple>
-#include <string>
+#include <array>
 #include <cassert>
+#include <tuple>
 
+#include "test_comparisons.h"
 #include "test_macros.h"
+
+#if TEST_STD_VER >= 26
+
+// Test SFINAE.
+
+// ==(const tuple<>&, const tuple<>&);
+
+static_assert(std::equality_comparable<std::tuple<EqualityComparable>>);
+static_assert(std::equality_comparable<std::tuple<EqualityComparable, EqualityComparable>>);
+
+static_assert(!std::equality_comparable<std::tuple<NonComparable>>);
+static_assert(!std::equality_comparable<std::tuple<EqualityComparable, NonComparable>>);
+static_assert(!std::equality_comparable<std::tuple<NonComparable, EqualityComparable>>);
+static_assert(!std::equality_comparable_with<std::tuple<EqualityComparable>, std::tuple<NonComparable>>);
+static_assert(!std::equality_comparable_with<std::tuple<NonComparable>, std::tuple<EqualityComparable>>);
+// Size mismatch.
+static_assert(!std::equality_comparable_with<std::tuple<EqualityComparable>, std::tuple<EqualityComparable, EqualityComparable>>);
+static_assert(!std::equality_comparable_with<std::tuple<EqualityComparable, EqualityComparable>, std::tuple<EqualityComparable>>);
+
+// ==(const tuple<>&, const tuple-like&);
+
+// static_assert(std::equality_comparable_with<std::tuple<EqualityComparable, EqualityComparable>, std::pair<EqualityComparable, EqualityComparable>>);
+// static_assert(std::equality_comparable_with<std::tuple<EqualityComparable, EqualityComparable>, std::array<EqualityComparable, 2>>);
+
+// static_assert(!std::equality_comparable_with<std::tuple<EqualityComparable, NonComparable>, std::pair<EqualityComparable, NonComparable>>);
+// static_assert(!std::equality_comparable_with<std::tuple<EqualityComparable, NonComparable>, std::array<EqualityComparable, 2>>);
+// // Size mismatch.
+// static_assert(!std::equality_comparable_with<std::tuple<EqualityComparable>, std::pair<EqualityComparable, EqualityComparable>>);
+// static_assert(!std::equality_comparable_with<std::tuple<EqualityComparable, EqualityComparable>, std::array<EqualityComparable, 94>>);
+
+#endif
 
 int main(int, char**)
 {
+    // {
+    //     using T1 = std::tuple<int>;
+    //     using T2 = std::array<int, 1>;
+    //     const T1 t1(1);
+    //     const T2 t2{1};
+    //     assert(t1 == t2);
+    //     assert(!(t1 != t2));
+    // }
     {
         typedef std::tuple<> T1;
         typedef std::tuple<> T2;
