52686fb37152379a9ab0fc3d9bf1816f0c838df4
Ming-Yi Lai <ming-yi.lai@mediatek.com>
2025-05-14 11:48:59 +0800

    [RISCV] Emit .note.gnu.property section when Zicfiss-based shadow stack is enabled (#127036)
    RISC-V Zicfiss-based shadow stack needs to let the linker/loader know if
    the binary is built with the mechanism enabled to support proper
    link-time/load-time management of this feature. The information is
    encoded as a bit in the `.note.gnu.property` section. This patch
    implements emitting the section for RISC-V targets when Zicfiss-based
    shadow stack is enabled.
    
    When Clang receives the `-fcf-protection=return` flag, it adds the
    `hw-shadow-stack` attribute to LLVM functions, and adds a non-zero
    valued attribute named `cf-protection-return` to the LLVM module it
    generates. The backend depends on the `hw-shadow-stack` attributes to
    generate Zicfiss-based shadow stack instructions for each function, but
    at the module scope, the `cf-protection-return` attribute is a better
    indication of whether the translation unit is built with Zicfiss-based
    shadow stack enabled, so this patch emits the `.note.gnu.property`
    section with the "Zicfiss-based shadow stack" bit toggled on when it
    sees the `cf-protection-return` attribute.

--- llvm/lib/Target/RISCV/RISCVAsmPrinter.cpp
+++ llvm/lib/Target/RISCV/RISCVAsmPrinter.cpp
@@ -112,6 +112,8 @@ public:
   void emitFunctionEntryLabel() override;
   bool emitDirectiveOptionArch();
 
+  void emitNoteGnuProperty(const Module &M);
+
 private:
   void emitAttributes(const MCSubtargetInfo &SubtargetInfo);
 
@@ -581,8 +583,10 @@ void RISCVAsmPrinter::emitEndOfAsmFile(Module &M) {
   RISCVTargetStreamer &RTS =
       static_cast<RISCVTargetStreamer &>(*OutStreamer->getTargetStreamer());
 
-  if (TM.getTargetTriple().isOSBinFormatELF())
+  if (TM.getTargetTriple().isOSBinFormatELF()) {
     RTS.finishAttributeSection();
+    emitNoteGnuProperty(M);
+  }
   EmitHwasanMemaccessSymbols(M);
 }
 
@@ -938,6 +942,15 @@ void RISCVAsmPrinter::EmitHwasanMemaccessSymbols(Module &M) {
 
     EmitToStreamer(*OutStreamer, MCInstBuilder(RISCV::PseudoCALL).addExpr(Expr),
                    MCSTI);
+  }
+}
+
+void RISCVAsmPrinter::emitNoteGnuProperty(const Module &M) {
+  if (const Metadata *const Flag = M.getModuleFlag("cf-protection-return");
+      Flag && !mdconst::extract<ConstantInt>(Flag)->isZero()) {
+    RISCVTargetStreamer &RTS =
+        static_cast<RISCVTargetStreamer &>(*OutStreamer->getTargetStreamer());
+    RTS.emitNoteGnuPropertySection(ELF::GNU_PROPERTY_RISCV_FEATURE_1_CFI_SS);
   }
 }
 
