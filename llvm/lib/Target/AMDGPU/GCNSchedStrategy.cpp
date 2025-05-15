566172d1006d51403e8d0bc22b41b43d01f34e27
root <root@bg-1w300-g1-2a.mkm.dcgpu>
2025-05-15 20:27:11 +0000

    switching
    Change-Id: Ib54872aa396664dbc734201a665b9fd544e0fec6

--- llvm/lib/Target/AMDGPU/GCNSchedStrategy.cpp
+++ llvm/lib/Target/AMDGPU/GCNSchedStrategy.cpp
@@ -83,12 +83,6 @@ static cl::opt<bool> DisableRemat(
     cl::init(false));
 
 
-static cl::opt<bool> DisableRemat(
-    "amdgpu-disable-remat", cl::Hidden,
-    cl::desc("Disable rematerialization during AMDGPU scheduling)"),
-    cl::init(false));
-
-
 const unsigned ScheduleMetrics::ScaleFactor = 100;
 
 GCNSchedStrategy::GCNSchedStrategy(const MachineSchedContext *C)
