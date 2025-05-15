4595137c7e0231e81c79d82ed10ece0719c2814a
Grigory Pastukhov <gpastukhov@meta.com>
2025-05-15 14:52:15 -0700

    Added tests with dead branches

--- clang/test/SemaCXX/warn-nrvo.cpp
+++ clang/test/SemaCXX/warn-nrvo.cpp
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -fsyntax-only -Wnrvo -verify %s
+// RUN: %clang_cc1 -fsyntax-only -Wnrvo -Wno-return-mismatch -verify %s
 struct MyClass {
     int value;
     int c;
@@ -37,7 +37,37 @@ auto create_object4(){
     return obj; // expected-warning{{not eliding copy on return}}
 }
 
+template<bool F>
+MyClass create_object5(){
+    MyClass obj1(1);
+    if constexpr (F){
+        MyClass obj2(2);
+        return obj2; // no warning
+    }
+  // Missed NRVO optimization by clang
+  return obj1; // expected-warning{{not eliding copy on return}}
+}
+
+constexpr bool Flag = false;
+
+MyClass create_object6(){
+  MyClass obj1(1);
+  if constexpr (Flag){
+    MyClass obj2(2);
+    return obj2; // expected-warning{{not eliding copy on return}}
+  }
+  return obj1; // no warning
+}
+
+void create_object7(){
+    if constexpr (Flag){
+        MyClass obj1(1);
+        return obj1; // no warning
+    }
+}
+
 void init_templates(){
     create_object3<MyClass>(); // no warning
     create_object4<MyClass>(); // expected-note {{in instantiation of function template specialization 'create_object4<MyClass>' requested here}}
+    create_object5<false>(); // expected-note {{in instantiation of function template specialization 'create_object5<false>' requested here}}
 }
