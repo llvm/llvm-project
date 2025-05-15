82e3c181b8a89f164d8f4f7e2a3aab26778605ad
Grigory Pastukhov <gpastukhov@meta.com>
2025-05-14 18:19:47 -0700

    Added more tests

--- clang/test/SemaCXX/warn-nrvo.cpp
+++ clang/test/SemaCXX/warn-nrvo.cpp
@@ -14,3 +14,30 @@ MyClass create_object(bool condition) {
     }
     return obj2; // expected-warning{{not eliding copy on return}}
 }
+
+MyClass create_object2(){
+    MyClass obj(1);
+    return obj; // no warning
+}
+
+template<typename T>
+T create_object3(){
+    T obj(1);
+    return obj; // no warning
+}
+
+// Known issue: if a function template uses a 
+// deduced return type (i.e. auto or decltype(auto)), 
+// then NRVO is not applied for any instantiation of 
+// that function template 
+// (see https://github.com/llvm/llvm-project/issues/95280).
+template<typename T>
+auto create_object4(){
+    T obj(1);
+    return obj; // expected-warning{{not eliding copy on return}}
+}
+
+void init_templates(){
+    create_object3<MyClass>(); // no warning
+    create_object4<MyClass>(); // expected-note {{in instantiation of function template specialization 'create_object4<MyClass>' requested here}}
+}
