45221ff230e00567b7208e151bbd8249457a27fd
Thurston Dang <thurston@google.com>
2025-05-16 02:28:31 +0000

    Rename unused return values to '_'

--- clang/lib/CodeGen/CGClass.cpp
+++ clang/lib/CodeGen/CGClass.cpp
@@ -2814,7 +2814,7 @@ void CodeGenFunction::EmitVTablePtrCheckForCall(const CXXRecordDecl *RD,
   if (!SanOpts.has(SanitizerKind::CFICastStrict))
     RD = LeastDerivedClassWithSameLayout(RD);
 
-  auto [Ordinal, SSK] = SanitizerInfoFromCFICheckKind(TCK);
+  auto [Ordinal, _] = SanitizerInfoFromCFICheckKind(TCK);
   ApplyDebugLocation ApplyTrapDI(*this, SanitizerAnnotateDebugInfo(Ordinal));
 
   EmitVTablePtrCheck(RD, VTable, TCK, Loc);
@@ -2839,7 +2839,7 @@ void CodeGenFunction::EmitVTablePtrCheckForCast(QualType T, Address Derived,
   if (!SanOpts.has(SanitizerKind::CFICastStrict))
     ClassDecl = LeastDerivedClassWithSameLayout(ClassDecl);
 
-  auto [Ordinal, SSK] = SanitizerInfoFromCFICheckKind(TCK);
+  auto [Ordinal, _] = SanitizerInfoFromCFICheckKind(TCK);
   ApplyDebugLocation ApplyTrapDI(*this, SanitizerAnnotateDebugInfo(Ordinal));
 
   llvm::BasicBlock *ContBlock = nullptr;
