189ab6f0fdc329092f657a3fe17b7d97de67b368
Tim Gymnich <tim@gymni.ch>
2025-05-16 11:01:35 -0400

    fix headers

--- llvm/lib/CodeGen/MachineFloatingPointPredicateUtils.cpp
+++ llvm/lib/CodeGen/MachineFloatingPointPredicateUtils.cpp
@@ -1,4 +1,4 @@
-//===- MachineConvergenceVerifier.cpp - Verify convergencectrl ------------===//
+//===- MachineFloatingPointPredicateUtils.cpp -----------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
