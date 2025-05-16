11aa948456f874680ebe8a7d6995124d98bd5c5f
Naveen Kumar <nk0187912@gmail.com>
2025-05-16 20:17:18 +0530

    add: mulifx functions

---
+++ libc/src/stdfix/muliulk.h
@@ -0,0 +1,21 @@
+//===-- Implementation header for muliulk -----------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_LIBC_SRC_STDFIX_MULIULK_H
+#define LLVM_LIBC_SRC_STDFIX_MULIULK_H
+
+#include "include/llvm-libc-macros/stdfix-macros.h" // unsigned accum
+#include "src/__support/macros/config.h"            // LIBC_NAMESPACE_DECL
+
+namespace LIBC_NAMESPACE_DECL {
+
+unsigned long int muliulk(unsigned long accum f, unsigned long int i);
+
+} // namespace LIBC_NAMESPACE_DECL
+
+#endif // LLVM_LIBC_SRC_STDFIX_MULIULK_H
