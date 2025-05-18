8c9d519f951bc6a45949c99bbce8ac5809a4445d
dl8sd11 <gcchen@google.com>
2025-05-18 05:35:47 +0000

    Fix test message location.

--- clang-tools-extra/test/clang-tidy/checkers/portability/avoid-pragma-once.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/portability/avoid-pragma-once.cpp
@@ -1,5 +1,5 @@
 // RUN: %check_clang_tidy %s portability-avoid-pragma-once %t \
-// RUN:   -header-filter=.* -- --  -I%S/Inputs/avoid-pragma-once
+// RUN:   -- --header-filter='.*' --  -I%S/Inputs/avoid-pragma-once
 
 #include "lib.h"
-// CHECK-MESSAGES: :[[@LINE-1]]:1: warning: Avoid pragma once. [portability-avoid-pragma-once]
+// CHECK-MESSAGES: lib.h:1:1: warning: Avoid pragma once.
