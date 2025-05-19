38c1e5e62efa56c8dbca4e087555695ad34fe469
Fraser Cormack <fraser@codeplay.com>
2025-05-19 11:10:08 +0100

    [libclc] Mov erf & erfc to CLC library
    This completes the set of maths builtins.
    
    No attempt to vectorize or optimize this code. The implementation is
    licensed to SunPro so will probably need to be replaced at some point in
    the future anyway. Calls to other builtins have been replaced with the
    CLC equivalents, and some bit-hacking was replaced with the fabs
    builtin.

--- libclc/clc/include/clc/math/clc_erfc.h
+++ libclc/clc/include/clc/math/clc_erfc.h
@@ -6,10 +6,14 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include <clc/clc.h>
-#include <clc/math/clc_erfc.h>
+#ifndef __CLC_MATH_CLC_ERFC_H__
+#define __CLC_MATH_CLC_ERFC_H__
 
-#define FUNCTION erfc
-#define __CLC_BODY <clc/shared/unary_def.inc>
+#define __CLC_BODY <clc/math/unary_decl.inc>
+#define __CLC_FUNCTION __clc_erfc
+
 #include <clc/math/gentype.inc>
 
+#undef __CLC_FUNCTION
+
+#endif // __CLC_MATH_CLC_ERFC_H__
