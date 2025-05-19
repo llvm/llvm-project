7b1a98fb1955c5242e6f3d356edd8cd41c4de2ab
Peng Liu <winner245@hotmail.com>
2025-05-19 18:02:05 -0400

    constexpr priority_queue

--- libcxx/test/std/containers/container.adaptors/priority.queue/priqueue.cons/ctor_comp_container.pass.cpp
+++ libcxx/test/std/containers/container.adaptors/priority.queue/priqueue.cons/ctor_comp_container.pass.cpp
@@ -20,14 +20,14 @@
 #endif
 
 template <class C>
-C make(int n) {
+TEST_CONSTEXPR_CXX26 C make(int n) {
   C c;
   for (int i = 0; i < n; ++i)
     c.push_back(i);
   return c;
 }
 
-int main(int, char**) {
+TEST_CONSTEXPR_CXX26 bool test() {
   typedef std::vector<int> Container;
   typedef std::greater<int> Compare;
   typedef std::priority_queue<int, Container, Compare> Q;
@@ -39,6 +39,15 @@ int main(int, char**) {
 #if TEST_STD_VER >= 11
   // It should be explicit, so not convertible before C++20.
   static_assert(test_convertible<Q, const Compare&, const Container&>(), "");
+#endif
+
+  return true;
+}
+
+int main(int, char**) {
+  assert(test());
+#if TEST_STD_VER >= 26
+  static_assert(test());
 #endif
 
   return 0;
