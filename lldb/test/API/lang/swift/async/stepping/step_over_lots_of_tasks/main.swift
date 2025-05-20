2fa016a20016be538c24a1a77de2323ddfa84b3a
Felipe de Azevedo Piovezan <fpiovezan@apple.com>
2025-05-20 09:02:03 -0700

    [lldb][swift] Increase number of Rows inspected when unwinding async functions (#10698)
    We were overly conservatives in our initial estimations: if callee saved
    registers are used, they are usually spilled first, increasing the
    number of rows in the prologue.
    
    This also revealed a bug in computing the offset of a vector iterator:
    
    ```
    -  int row_idx = fp_locs.end() - it;
    +  int row_idx = it - fp_locs.begin();
    ```
    
    Testing this is unfortunately difficult: a way of forcing register
    spilling is required. For now, this commit tweaks one of the existing
    tests in a way that seems to reliably spill more registers, but there
    are no guarantees this will continue to be the case. Other testing
    mechanisms need to be created.
    
    (cherry picked from commit 33079066425f4d100c0c4cb182f63a63c65c1ee8)

--- lldb/test/API/lang/swift/async/stepping/step_over_lots_of_tasks/main.swift
+++ lldb/test/API/lang/swift/async/stepping/step_over_lots_of_tasks/main.swift
@@ -12,13 +12,17 @@
 
 func fib(n: Int) async -> Int {
   if (n == 0) {
+    print("done!")
     return 1
   }
   if (n == 1) {
+    print("done!")
     return 1
   }
   async let n1_task = fib(n: n - 1)  // Breakpoint fib
   async let n2_task = fib(n: n - 2)
+  print(n)
+  if (n == 6) { do { try await Task.sleep(for: .seconds(1)) } catch {} }
   let n1 = await n1_task
   let n2 = await n2_task
   return n1 + n2
