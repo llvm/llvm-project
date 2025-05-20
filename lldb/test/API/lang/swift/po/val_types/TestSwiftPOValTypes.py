4ab2b5b8b6e138c3e5b3f5950164d77fbaf78b73
jimingham <jingham@apple.com>
2025-05-20 15:20:00 -0700

    Don't stop on breakpoints when running the swift Object Description function (#10693)
    * The Swift `object description` expression was being run without
    setting "IgnoreBreakpointsInExpressions", so it was stopping on
    breakpoints.  That's not how the ObjC object description is run,
    and is not what people expect here.
    
    I added the setting to the options in the Swift case, and added a
    test that we don't stop on breakpoints in the Object Description
    expression.
    
    * Mark the tests as @swiftTest and fix the test name.
    
    (cherry picked from commit d8693d30a62c2bb1dc97262fc26fc6d1c538f87b)

--- lldb/test/API/lang/swift/po/val_types/TestSwiftPOValTypes.py
+++ lldb/test/API/lang/swift/po/val_types/TestSwiftPOValTypes.py
@@ -9,7 +9,43 @@
 # See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
 #
 # ------------------------------------------------------------------------------
-import lldbsuite.test.lldbinline as lldbinline
+import lldb
+import lldbsuite.test
+import lldbsuite.test.lldbutil as lldbutil
+from lldbsuite.test.lldbtest import *
 from lldbsuite.test.decorators import *
 
-lldbinline.MakeInlineTest(__file__, globals(), decorators=[swiftTest])
+class TestSwiftPOValueTypes(TestBase):
+
+    @swiftTest
+    def test_value_types(self):
+        """Test 'po' on a variety of value types with and without custom descriptions."""
+        self.build()
+        (_,_,_,_) = lldbutil.run_to_source_breakpoint(self, "Break here to run tests", lldb.SBFileSpec("main.swift"))
+        
+        self.expect("po dm", substrs=['a', '12', 'b', '24'])
+        self.expect("po cm", substrs=['c', '36'])
+        self.expect("po cm", substrs=['12', '24'], matching=False)
+        self.expect("po cs", substrs=['CustomDebugStringConvertible'])
+        self.expect("po cs", substrs=['CustomStringConvertible'], matching=False)
+        self.expect("po cs", substrs=['a', '12', 'b', '24'])
+        self.expect("script lldb.frame.FindVariable('cs').GetObjectDescription()", substrs=['a', '12', 'b', '24'])
+        self.expect("po (12,24,36,48)", substrs=['12', '24', '36', '48'])
+        self.expect("po (dm as Any, cm as Any,48 as Any)", substrs=['12', '24', '36', '48'])
+        self.expect("po patatino", substrs=['foo'])
+
+    @swiftTest
+    def test_ignore_bkpts_in_po(self):
+        """Run a po expression with a breakpoint in the debugDescription, make sure we don't hit it."""
+
+        self.build()
+        main_spec = lldb.SBFileSpec("main.swift")
+        (target, process, thread, _) = lldbutil.run_to_source_breakpoint(self, "Break here to run tests", main_spec)
+        po_bkpt = target.BreakpointCreateBySourceRegex("Breakpoint in debugDescription", main_spec)
+
+        # As part of the po expression we should auto-continue past the breakpoint so this succeeds:
+        self.expect("po cs", substrs=['CustomDebugStringConvertible'])
+        self.assertEqual(po_bkpt.GetHitCount(), 1, "Did hit the breakpoint")
+
+        
+        
