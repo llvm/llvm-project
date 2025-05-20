eb29f927d0c8ffd0ae895f00b91038a34f8e43c3
Originns <68753984+Originns@users.noreply.github.com>
2025-05-20 11:33:50 +0200

    [flang-tidy] add bugprone module

--- flang/tools/flang-tidy/FlangTidyForceLinker.h
+++ flang/tools/flang-tidy/FlangTidyForceLinker.h
@@ -13,6 +13,11 @@
 
 namespace Fortran::tidy {
 
+// This anchor is used to force the linker to link the BugproneModule.
+extern volatile int BugproneModuleAnchorSource;
+static int LLVM_ATTRIBUTE_UNUSED BugproneModuleAnchorDestination =
+    BugproneModuleAnchorSource;
+
 } // namespace Fortran::tidy
 
 #endif // LLVM_FLANG_TOOLS_FLANG_TIDY_FLANGTIDYFORCELINKER_H
