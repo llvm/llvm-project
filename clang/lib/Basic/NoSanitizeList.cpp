898658cc3e9e4a4535ffefaa08f03d1bab3804fa
Qinkun Bao <qinkun@google.com>
2025-05-20 17:30:14 +0000

    remove the second lookup in containsFile.
    Created using spr 1.3.6

--- clang/lib/Basic/NoSanitizeList.cpp
+++ clang/lib/Basic/NoSanitizeList.cpp
@@ -45,12 +45,14 @@ bool NoSanitizeList::containsFunction(SanitizerMask Mask,
 bool NoSanitizeList::containsFile(SanitizerMask Mask, StringRef FileName,
                                   StringRef Category) const {
   unsigned NoSanLine = SSCL->inSectionBlame(Mask, "src", FileName, Category);
+  if (NoSanLine == 0)
+    return false;
   unsigned SanLine = SSCL->inSectionBlame(Mask, "src", FileName, "sanitize");
   // If we have two cases such as `src:a.cpp=sanitize` and `src:a.cpp`, the
   // current entry override the previous entry.
-  if (NoSanLine > 0 && SanLine > 0)
+  if (SanLine > 0)
     return NoSanLine > SanLine;
-  return SSCL->inSection(Mask, "src", FileName, Category);
+  return true;
 }
 
 bool NoSanitizeList::containsMainFile(SanitizerMask Mask, StringRef FileName,
