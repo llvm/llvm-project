503dac0e103699b8006261d6cb1a221867ea9400
Originns <68753984+Originns@users.noreply.github.com>
2025-05-20 11:50:27 +0200

    [flang-tidy] add performance module

---
+++ flang/tools/flang-tidy/performance/PerformanceTidyModule.cpp
@@ -0,0 +1,32 @@
+//===--- PerformanceTidyModule.cpp - flang-tidy ---------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "../FlangTidyModule.h"
+#include "../FlangTidyModuleRegistry.h"
+
+namespace Fortran::tidy {
+namespace performance {
+
+class PerformanceTidyModule : public FlangTidyModule {
+public:
+  void addCheckFactories(FlangTidyCheckFactories &CheckFactories) override {}
+};
+
+} // namespace performance
+
+// Register the PerformanceTidyModule using this statically initialized
+// variable.
+static FlangTidyModuleRegistry::Add<performance::PerformanceTidyModule>
+    X("performance-module", "Performance Tidy Module");
+
+// This anchor is used to force the linker to link in the generated object file
+// and thus register the PerformanceModule.
+// NOLINTNEXTLINE
+volatile int PerformanceModuleAnchorSource = 0;
+
+} // namespace Fortran::tidy
