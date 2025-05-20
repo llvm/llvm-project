87468b87060ebdcf97ba9075a5896e40daa6612c
Craig Topper <craig.topper@sifive.com>
2025-05-20 15:51:55 -0700

    fixup! Improve invalid vtype tests

--- llvm/test/MC/RISCV/rvv/xsfmm-invalid.s
+++ llvm/test/MC/RISCV/rvv/xsfmm-invalid.s
@@ -5,9 +5,11 @@
 # RUN:     --mattr=+xsfmm32a32f,+xsfmm32a8f,+xsfmm64a64f %s 2>&1 \
 # RUN:        | FileCheck %s --check-prefix=CHECK
 
-sf.vsettnt a0, a1, a2, e128, w1 # CHECK: :[[@LINE]]:20: error: operand must be e[8|16|16alt|32|64],w[1|2|4]
+sf.vsettnt a0, a1, e128, w1 # CHECK: :[[@LINE]]:20: error: operand must be e[8|16|16alt|32|64],w[1|2|4]
 
-sf.vsettnt a0, a1, a2, e8alt, w1 # CHECK: :[[@LINE]]:20: error: operand must be e[8|16|16alt|32|64],w[1|2|4]
+sf.vsettnt a0, a1, e8alt, w1 # CHECK: :[[@LINE]]:20: error: operand must be e[8|16|16alt|32|64],w[1|2|4]
+
+sf.vsettnt a0, a1, e16alt, w1, a2 # CHECK: :[[@LINE]]:20: error: operand must be e[8|16|16alt|32|64],w[1|2|4]
 
 sf.mm.f.f mt1, v8, v9 # CHECK: :[[@LINE]]:11: error: invalid operand for instruction
 
