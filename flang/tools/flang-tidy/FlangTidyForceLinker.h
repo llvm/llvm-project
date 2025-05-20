d2b5e75e27996b5f0816204d0af4a02e04257e8e
Originns <68753984+Originns@users.noreply.github.com>
2025-05-20 11:39:27 +0200

    [flang-tidy] add modernize module

--- flang/tools/flang-tidy/FlangTidyForceLinker.h
+++ flang/tools/flang-tidy/FlangTidyForceLinker.h
@@ -18,6 +18,11 @@ extern volatile int BugproneModuleAnchorSource;
 static int LLVM_ATTRIBUTE_UNUSED BugproneModuleAnchorDestination =
     BugproneModuleAnchorSource;
 
+// This anchor is used to force the linker to link the ModernizeModule.
+extern volatile int ModernizeModuleAnchorSource;
+static int LLVM_ATTRIBUTE_UNUSED ModernizeModuleAnchorDestination =
+    ModernizeModuleAnchorSource;
+
 } // namespace Fortran::tidy
 
 #endif // LLVM_FLANG_TOOLS_FLANG_TIDY_FLANGTIDYFORCELINKER_H
