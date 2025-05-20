296a105fa6eb6dbf19792229c349427330b50f0a
Charles Zablit <c_zablit@apple.com>
2025-05-20 17:50:48 +0100

    remove redundant check

--- lldb/include/lldb/Core/DemangledNameInfo.h
+++ lldb/include/lldb/Core/DemangledNameInfo.h
@@ -71,14 +71,12 @@ struct DemangledNameInfo {
 
   /// Returns \c true if this object holds a valid basename range.
   bool hasBasename() const {
-    return BasenameRange.second > BasenameRange.first &&
-           BasenameRange.second > 0;
+    return BasenameRange.second > BasenameRange.first;
   }
 
   /// Returns \c true if this object holds a valid arguments range.
   bool hasArguments() const {
-    return ArgumentsRange.second > ArgumentsRange.first &&
-           ArgumentsRange.second > 0;
+    return ArgumentsRange.second > ArgumentsRange.first;
   }
 };
 
