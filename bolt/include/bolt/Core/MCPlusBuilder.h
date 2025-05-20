efbf67b4855973102cd4995fcae3cf98b5525be4
Kostas Alvertis <konstantinos.alvertis@gmail.com>
2025-05-20 19:50:15 +0100

    [PPC][BOLT] Adding PCC factory function declaration

--- bolt/include/bolt/Core/MCPlusBuilder.h
+++ bolt/include/bolt/Core/MCPlusBuilder.h
@@ -2373,6 +2373,11 @@ MCPlusBuilder *createRISCVMCPlusBuilder(const MCInstrAnalysis *,
                                         const MCRegisterInfo *,
                                         const MCSubtargetInfo *);
 
+MCPlusBuilder *createPowerPCMCPlusBuilder(const MCInstrAnalysis *,
+                                        const MCInstrInfo *,
+                                        const MCRegisterInfo *,
+                                        const MCSubtargetInfo *);
+
 } // namespace bolt
 } // namespace llvm
 
