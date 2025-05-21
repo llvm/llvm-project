4da9f0ea0181eb17caedcf3e004ed1095c09b708
Gedare Bloom <gedare@rtems.org>
2025-05-21 17:39:58 -0600

    dump clang-format-style

--- clang/lib/Format/Format.cpp
+++ clang/lib/Format/Format.cpp
@@ -1123,6 +1123,8 @@ template <> struct MappingTraits<FormatStyle> {
                    Style.BreakAfterJavaFieldAnnotations);
     IO.mapOptional("BreakAfterOpenBracketBracedList",
                    Style.BreakAfterOpenBracketBracedList);
+    IO.mapOptional("BreakAfterOpenBracketFunction",
+                   Style.BreakAfterOpenBracketFunction);
     IO.mapOptional("BreakAfterOpenBracketIf", Style.BreakAfterOpenBracketIf);
     IO.mapOptional("BreakAfterOpenBracketLoop",
                    Style.BreakAfterOpenBracketLoop);
@@ -1134,6 +1136,8 @@ template <> struct MappingTraits<FormatStyle> {
                    Style.BreakBeforeBinaryOperators);
     IO.mapOptional("BreakBeforeCloseBracketBracedList",
                    Style.BreakBeforeCloseBracketBracedList);
+    IO.mapOptional("BreakBeforeCloseBracketFunction",
+                   Style.BreakBeforeCloseBracketFunction);
     IO.mapOptional("BreakBeforeCloseBracketIf",
                    Style.BreakBeforeCloseBracketIf);
     IO.mapOptional("BreakBeforeCloseBracketLoop",
