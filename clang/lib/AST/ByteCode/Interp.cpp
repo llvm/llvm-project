e9879d5fcbfceafae96419608331cf66a814198a
Timm Bäder <tbaeder@redhat.com>
2025-05-22 18:44:22 +0200

    [clang][bytecode] Diagnose placement-new'ing to a temporary
    ... that's been created in a different evaluation.

--- clang/lib/AST/ByteCode/Interp.cpp
+++ clang/lib/AST/ByteCode/Interp.cpp
@@ -1702,6 +1702,8 @@ bool CheckNewTypeMismatch(InterpState &S, CodePtr OpPC, const Expr *E,
                           std::optional<uint64_t> ArraySize) {
   const Pointer &Ptr = S.Stk.peek<Pointer>();
 
+  if (!CheckTemporary(S, OpPC, Ptr, AK_Construct))
+    return false;
   if (!CheckStore(S, OpPC, Ptr))
     return false;
 
