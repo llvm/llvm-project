c97d660d30ac4669e13cf4c301c7e374d11a8891
Kewen12 <Kewen.Meng@amd.com>
2025-05-21 21:07:38 -0700

    Revert "[LLVM] Use `reportFatalUsageError` for LTO usage errors" (#141000)
    The PR causes check-lld fail:
    >TEST 'lld :: COFF/lto-cache-errors.ll'
    
    Tested on local revert and pass the check. 
    
    Reverts llvm/llvm-project#140955

--- llvm/lib/LTO/LTOBackend.cpp
+++ llvm/lib/LTO/LTOBackend.cpp
@@ -290,8 +290,8 @@ static void runNewPMPasses(const Config &Conf, Module &Mod, TargetMachine *TM,
   if (!Conf.AAPipeline.empty()) {
     AAManager AA;
     if (auto Err = PB.parseAAPipeline(AA, Conf.AAPipeline)) {
-      reportFatalUsageError(Twine("unable to parse AA pipeline description '") +
-                            Conf.AAPipeline + "': " + toString(std::move(Err)));
+      report_fatal_error(Twine("unable to parse AA pipeline description '") +
+                         Conf.AAPipeline + "': " + toString(std::move(Err)));
     }
     // Register the AA manager first so that our version is the one used.
     FAM.registerPass([&] { return std::move(AA); });
@@ -331,9 +331,8 @@ static void runNewPMPasses(const Config &Conf, Module &Mod, TargetMachine *TM,
   // Parse a custom pipeline if asked to.
   if (!Conf.OptPipeline.empty()) {
     if (auto Err = PB.parsePassPipeline(MPM, Conf.OptPipeline)) {
-      reportFatalUsageError(
-          Twine("unable to parse pass pipeline description '") +
-          Conf.OptPipeline + "': " + toString(std::move(Err)));
+      report_fatal_error(Twine("unable to parse pass pipeline description '") +
+                         Conf.OptPipeline + "': " + toString(std::move(Err)));
     }
   } else if (IsThinLTO) {
     MPM.addPass(PB.buildThinLTODefaultPipeline(OL, ImportSummary));
@@ -416,8 +415,8 @@ static void codegen(const Config &Conf, TargetMachine *TM,
   if (!Conf.DwoDir.empty()) {
     std::error_code EC;
     if (auto EC = llvm::sys::fs::create_directories(Conf.DwoDir))
-      reportFatalUsageError(Twine("Failed to create directory ") + Conf.DwoDir +
-                            ": " + EC.message());
+      report_fatal_error(Twine("Failed to create directory ") + Conf.DwoDir +
+                         ": " + EC.message());
 
     DwoFile = Conf.DwoDir;
     sys::path::append(DwoFile, std::to_string(Task) + ".dwo");
@@ -429,14 +428,14 @@ static void codegen(const Config &Conf, TargetMachine *TM,
     std::error_code EC;
     DwoOut = std::make_unique<ToolOutputFile>(DwoFile, EC, sys::fs::OF_None);
     if (EC)
-      reportFatalUsageError(Twine("Failed to open ") + DwoFile + ": " +
-                            EC.message());
+      report_fatal_error(Twine("Failed to open ") + DwoFile + ": " +
+                         EC.message());
   }
 
   Expected<std::unique_ptr<CachedFileStream>> StreamOrErr =
       AddStream(Task, Mod.getModuleIdentifier());
   if (Error Err = StreamOrErr.takeError())
-    reportFatalUsageError(std::move(Err));
+    report_fatal_error(std::move(Err));
   std::unique_ptr<CachedFileStream> &Stream = *StreamOrErr;
   TM->Options.ObjectFilenameForDebug = Stream->ObjectPathName;
 
