85b127cc1c615ca734442ee64b1ca262a6285550
Orlando Cazalet-Hyams <orlando.hyams@sony.com>
2025-05-22 14:57:35 +0100

    test g += ++h;

--- clang/test/DebugInfo/KeyInstructions/assign-scalar.c
+++ clang/test/DebugInfo/KeyInstructions/assign-scalar.c
@@ -4,7 +4,7 @@
 // RUN: %clang_cc1 -gkey-instructions -x c %s -debug-info-kind=line-tables-only -emit-llvm -o - \
 // RUN: | FileCheck %s --implicit-check-not atomGroup --implicit-check-not atomRank
 
-unsigned long long g;
+unsigned long long g, h;
 void fun() {
 // CHECK: store i64 0, ptr @g{{.*}}, !dbg [[G1R1:!.*]]
     g = 0;
@@ -34,6 +34,16 @@ void fun() {
 // CHECK: %dec = add i64 %3, -1, !dbg [[G6R2:!.*]]
 // CHECK: store i64 %dec, ptr @g{{.*}}, !dbg [[G6R1:!.*]]
     g--;
+
+// Compound assignment with assignment on RHS, the assignments should have
+// their own separate atom groups.
+// CHECK: %4 = load i64, ptr @h{{.*}}, !dbg [[load_h_loc:!.*]]
+// CHECK: %inc1 = add i64 %4, 1, !dbg [[G8R2:!.*]]
+// CHECK: store i64 %inc1, ptr @h{{.*}}, !dbg [[G8R1:!.*]]
+// CHECK: %5 = load i64, ptr @g{{.*}}, !dbg [[load_g_loc:!.*]]
+// CHECK: %add2 = add i64 %5, %inc1, !dbg [[G7R2:!.*]]
+// CHECK: store i64 %add2, ptr @g{{.*}}, !dbg [[G7R1:!.*]]
+    g += ++h;
 }
 
 // CHECK: [[G1R1]] = !DILocation({{.*}}, atomGroup: 1, atomRank: 1)
@@ -46,3 +56,9 @@ void fun() {
 // CHECK: [[G5R1]] = !DILocation({{.*}}, atomGroup: 5, atomRank: 1)
 // CHECK: [[G6R2]] = !DILocation({{.*}}, atomGroup: 6, atomRank: 2)
 // CHECK: [[G6R1]] = !DILocation({{.*}}, atomGroup: 6, atomRank: 1)
+// CHECK: [[load_h_loc]] = !DILocation(line: [[#]], column: [[#]], scope: ![[#]])
+// CHECK: [[G8R2]] = !DILocation({{.*}}, atomGroup: 8, atomRank: 2)
+// CHECK: [[G8R1]] = !DILocation({{.*}}, atomGroup: 8, atomRank: 1)
+// CHECK: [[load_g_loc]] = !DILocation(line: [[#]], column: [[#]], scope: ![[#]])
+// CHECK: [[G7R2]] = !DILocation({{.*}}, atomGroup: 7, atomRank: 2)
+// CHECK: [[G7R1]] = !DILocation({{.*}}, atomGroup: 7, atomRank: 1)
