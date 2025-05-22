fbc51bfbcce5c5ec9ca6488335bf5d0dd810e3a3
Devon Loehr <dloehr@google.com>
2025-05-22 15:35:27 +0000

    Fix build more for non-clang compilers

--- llvm/include/llvm/Support/Compiler.h
+++ llvm/include/llvm/Support/Compiler.h
@@ -716,15 +716,21 @@ void AnnotateIgnoreWritesEnd(const char *file, int line);
 /// to declare such a function in `final` classes without triggering a warning.
 // clang-format off
 // Autoformatting makes this look awful.
-#if defined(__clang__) && __has_warning("-Wunnecessary-virtual-specifier")
-#define LLVM_DECLARE_VIRTUAL_ANCHOR_FUNCTION()                            \
-  _Pragma("clang diagnostic push")                                        \
-  _Pragma("clang diagnostic ignored \"-Wunnecessary-virtual-specifier\"") \
-  virtual void anchor()                                                   \
-  _Pragma("clang diagnostic pop")
-#else
-#define LLVM_DECLARE_VIRTUAL_ANCHOR_FUNCTION()                            \
-  virtual void anchor()
+#if defined(__clang__)
+  // Make sure this is only parsed if __clang__ is defined
+  #if __has_warning("-Wunnecessary-virtual-specifier")
+    #define LLVM_DECLARE_VIRTUAL_ANCHOR_FUNCTION()                            \
+      _Pragma("clang diagnostic push")                                        \
+      _Pragma("clang diagnostic ignored \"-Wunnecessary-virtual-specifier\"") \
+      virtual void anchor()                                                   \
+      _Pragma("clang diagnostic pop")
+  #else // __has_warning
+    #define LLVM_DECLARE_VIRTUAL_ANCHOR_FUNCTION()                            \
+      virtual void anchor()
+  #endif
+#else // defined(__clang__)
+  #define LLVM_DECLARE_VIRTUAL_ANCHOR_FUNCTION()                              \
+    virtual void anchor()
 #endif
 // clang-format on
 
