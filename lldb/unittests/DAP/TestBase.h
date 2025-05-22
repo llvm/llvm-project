35432bacdf7218797288f02791ab44f555982318
John Harrison <harjohn@google.com>
2025-05-22 08:58:16 -0700

    [lldb-dap] In DAP unit tests, add helpers for loading a CoreFile. (#140738)
    This allows us to have a SBTarget and SBProcess for creating unit tests.

--- lldb/unittests/DAP/TestBase.h
+++ lldb/unittests/DAP/TestBase.h
@@ -10,6 +10,8 @@
 #include "Protocol/ProtocolBase.h"
 #include "Transport.h"
 #include "lldb/Host/Pipe.h"
+#include "llvm/ADT/StringRef.h"
+#include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
 namespace lldb_dap_tests {
@@ -33,12 +35,32 @@ protected:
   void SetUp() override;
 };
 
+/// Matches an "output" event.
+inline auto OutputMatcher(const llvm::StringRef output,
+                          const llvm::StringRef category = "console") {
+  return testing::VariantWith<lldb_dap::protocol::Event>(testing::FieldsAre(
+      /*event=*/"output", /*body=*/testing::Optional<llvm::json::Value>(
+          llvm::json::Object{{"category", category}, {"output", output}})));
+}
+
 /// A base class for tests that interact with a `lldb_dap::DAP` instance.
 class DAPTestBase : public TransportBase {
 protected:
   std::unique_ptr<lldb_dap::DAP> dap;
+  std::optional<llvm::sys::fs::TempFile> core;
+  std::optional<llvm::sys::fs::TempFile> binary;
 
+  static constexpr llvm::StringLiteral k_linux_binary = "linux-x86_64.out.yaml";
+  static constexpr llvm::StringLiteral k_linux_core = "linux-x86_64.core.yaml";
+
+  static void SetUpTestSuite();
+  static void TeatUpTestSuite();
   void SetUp() override;
+  void TearDown() override;
+
+  bool GetDebuggerSupportsTarget(llvm::StringRef platform);
+  void CreateDebugger();
+  void LoadCore();
 
   /// Closes the DAP output pipe and returns the remaining protocol messages in
   /// the buffer.
