c0bf2de40336671d349d500d931569ca88c145cd
Ron Lieberman <ron.lieberman@amd.com>
2025-05-23 19:39:03 -0500

    Revert "Extend llvm objdump fatbin (#140286)"
    This reverts commit 51a03ed27282166d418399f2ade9814ea5be9944.

--- llvm/tools/llvm-objdump/OffloadDump.h
+++ llvm/tools/llvm-objdump/OffloadDump.h
@@ -10,18 +10,15 @@
 #define LLVM_TOOLS_LLVM_OBJDUMP_OFFLOADDUMP_H
 
 #include "llvm/Object/ObjectFile.h"
-//#include "llvm/Object/OffloadBinary.h"
-#include "llvm/Object/OffloadBundle.h"
+#include "llvm/Object/OffloadBinary.h"
 
 namespace llvm {
 
 void dumpOffloadSections(const object::OffloadBinary &OB);
-void dumpOffloadBinary(const object::ObjectFile &O, StringRef ArchName);
-
+void dumpOffloadBinary(const object::ObjectFile &O, std::string ArchName);
 
 /// Dump fat binary in binary clang-offload-bundler format
-void dumpOffloadBundleFatBinary(const object::ObjectFile &O,
-                                StringRef ArchName);
+void dumpOffloadBundleFatBinary(const object::ObjectFile &O, std::string ArchName);
 } // namespace llvm
 
 #endif
