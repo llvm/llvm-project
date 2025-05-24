08c7f41d594cbbfc27cb59012c3413fe2e4d8955
Luke Lau <luke@igalia.com>
2025-05-24 13:47:18 +0100

    [InstCombine] Don't cover up poison elements for shifts when folding shuffles thru binops (#141303)
    As noted in the TODO, we don't need to cover up the poison elements
    placed in the unused lanes for shifts, since it's not UB unlike div/rem.
    
    New poison elements are only introduced in cases like
    
    ShMask = <1,1,2,2> and C = <5,5,6,6> --> NewC = <poison,5,6,poison>
    
    And the resulting shuffle won't use the poison lanes.

--- llvm/lib/Transforms/InstCombine/InstructionCombining.cpp
+++ llvm/lib/Transforms/InstCombine/InstructionCombining.cpp
@@ -2257,11 +2257,9 @@ Instruction *InstCombinerImpl::foldVectorBinop(BinaryOperator &Inst) {
     }
     if (MayChange) {
       Constant *NewC = ConstantVector::get(NewVecC);
-      // It may not be safe to execute a binop on a vector with poison elements
-      // because the entire instruction can be folded to undef or create poison
-      // that did not exist in the original code.
-      // TODO: The shift case should not be necessary.
-      if (Inst.isIntDivRem() || (Inst.isShift() && ConstOp1))
+      // Lanes of NewC not used by the shuffle will be poison which will cause
+      // UB for div/rem. Mask them with a safe constant.
+      if (Inst.isIntDivRem())
         NewC = getSafeVectorConstantForBinop(Opcode, NewC, ConstOp1);
 
       // Op(shuffle(V1, Mask), C) -> shuffle(Op(V1, NewC), Mask)
