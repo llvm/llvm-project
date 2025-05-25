3a03a28d01a2e00cc05cca6a3755cccf84da3a3d
fubowen <fubowen@protomail.com>
2025-05-25 15:09:06 +0800

    improve test cases based on reviewd feedback

--- clang-tools-extra/test/clang-tidy/checkers/readability/convert-member-functions-to-static-deducing-this.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/readability/convert-member-functions-to-static-deducing-this.cpp
@@ -5,10 +5,18 @@ namespace std{
   void println(const char *format, const std::string &str) {}
 }
 
-namespace PR141381 {
 struct Hello {
   std::string str_;
 
-  void hello(this Hello &self) { std::println("Hello, {0}!", self.str_); }
+  void ByValueSelf(this Hello self) { std::println("Hello, {0}!", self.str_); }
+
+  void ByLRefSelf(this Hello &self) { std::println("Hello, {0}!", self.str_); }
+
+  void ByRRefSelf(this Hello&& self) {}
+
+  template<typename Self> void ByForwardRefSelf(this Self&& self) {}
+
+  void MultiParam(this Hello &self, int a, double b) {}
+
+  void UnnamedExplicitObjectParam(this Hello &) {}
 };
-}
