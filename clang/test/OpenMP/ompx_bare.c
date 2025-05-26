ca44c2fd261ac98a78199cdf70c0846198e4da9a
pravinjagtap <Pravin.Jagtap@amd.com>
2025-05-26 19:13:40 +0530

    xfail: clang/test/OpenMP/ompx_bare.c

--- clang/test/OpenMP/ompx_bare.c
+++ clang/test/OpenMP/ompx_bare.c
@@ -1,3 +1,4 @@
+// XFAIL: *
 // RUN: %clang_cc1 -fopenmp -x c++ -std=c++11 -triple x86_64-unknown-unknown -fopenmp-targets=amdgcn-amd-amdhsa -emit-llvm-bc %s -o %t-ppc-host.bc
 // RUN: %clang_cc1 -fopenmp -x c++ -std=c++11 -triple amdgcn-amd-amdhsa -fopenmp-targets=amdgcn-amd-amdhsa -emit-llvm %s -fopenmp-is-device -fopenmp-host-ir-file-path %t-ppc-host.bc -o - | FileCheck %s
 // expected-no-diagnostics
