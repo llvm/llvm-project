431ab2f5100a038668587d1264d8b700be664979
Peteris Ledins <peteris.ledins@zimperium.com>
2025-05-26 17:33:51 +0300

    Temporary fix for 141494
    See https://github.com/llvm/llvm-project/issues/141494

--- llvm/lib/ObjCopy/MachO/MachOLayoutBuilder.cpp
+++ llvm/lib/ObjCopy/MachO/MachOLayoutBuilder.cpp
@@ -173,7 +173,7 @@ uint64_t MachOLayoutBuilder::layoutSegments() {
         if (!Sec->hasValidOffset()) {
           Sec->Offset = 0;
         } else {
-          if (RequiresFirstSectionOutsideFirstPage) {
+          if (RequiresFirstSectionOutsideFirstPage && SectOffset < PageSize) {
             SectOffset = alignToPowerOf2(SectOffset, PageSize);
             RequiresFirstSectionOutsideFirstPage = false;
           }
@@ -293,7 +293,8 @@ Error MachOLayoutBuilder::layoutTail(uint64_t Offset) {
   uint64_t StartOfIndirectSymbols =
       updateOffset(sizeof(uint32_t) * O.IndirectSymTable.Symbols.size());
   uint64_t StartOfSymbolStrings = updateOffset(StrTableBuilder.getSize());
-  uint64_t StartOfDylibCodeSignDRs = updateOffset(O.DylibCodeSignDRs.Data.size());
+  uint64_t StartOfDylibCodeSignDRs =
+      updateOffset(O.DylibCodeSignDRs.Data.size());
 
   uint64_t StartOfCodeSignature = Offset;
   uint32_t CodeSignatureSize = 0;
