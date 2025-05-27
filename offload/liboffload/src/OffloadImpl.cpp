0be9fbffc03bda6562a6e4d5c14e8f8d410d28de
Joseph Huber <huberjn@outlook.com>
2025-05-27 12:14:44 -0500

    [Offload] Add specifier for the host type
    Summary:
    We use this sepcial type to indicate a host value, this will be refined
    later but for now it's used as a stand-in device for transfers and
    queues. It needs a special kind because it is not a device target as the
    other ones so we need to differentiate it between a CPU and GPU type.
    
    Fixes: https://github.com/llvm/llvm-project/issues/141436

--- offload/liboffload/src/OffloadImpl.cpp
+++ offload/liboffload/src/OffloadImpl.cpp
@@ -252,7 +252,8 @@ ol_impl_result_t olGetDeviceInfoImplDetail(ol_device_handle_t Device,
   case OL_DEVICE_INFO_PLATFORM:
     return ReturnValue(Device->Platform);
   case OL_DEVICE_INFO_TYPE:
-    return ReturnValue(OL_DEVICE_TYPE_GPU);
+    return Device == HostDevice() ? ReturnValue(OL_DEVICE_TYPE_HOST)
+                                  : ReturnValue(OL_DEVICE_TYPE_GPU);
   case OL_DEVICE_INFO_NAME:
     return ReturnValue(GetInfo({"Device Name"}).c_str());
   case OL_DEVICE_INFO_VENDOR:
