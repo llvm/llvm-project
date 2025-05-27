4a3d4c065b4f0aa56066502f32038e9a9380fe90
wldfngrs <wldfngrs@gmail.com>
2025-05-27 22:41:08 +0100

    changes post review

--- libc/src/math/atanf16.h
+++ libc/src/math/atanf16.h
@@ -1,8 +1,9 @@
 //===-- Implementation header for atanf16 -----------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.    //
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception.     //
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception.
+//
 //===----------------------------------------------------------------------===//
 
 #ifndef LLVM_LIBC_SRC_MATH_ATANF16_H
--- libc/test/src/math/atanf16_test.cpp
+++ libc/test/src/math/atanf16_test.cpp
@@ -1,3 +1,11 @@
+//===-- Exhaustive test for atanf16 ---------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
 #include "src/math/atanf16.h"
 #include "test/UnitTest/FPMatcher.h"
 #include "test/UnitTest/Test.h"
