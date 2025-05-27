476374cf30881da051efa45c3d84c8a8b9cf1f80
AmrDeveloper <amr96@programmer.net>
2025-05-27 19:20:23 +0200

    Address code review comment

--- clang/lib/CIR/CodeGen/CIRGenTypes.cpp
+++ clang/lib/CIR/CodeGen/CIRGenTypes.cpp
@@ -388,7 +388,7 @@ mlir::Type CIRGenTypes::convertType(QualType type) {
   }
 
   case Type::Complex: {
-    const auto *ct = mlir::cast<clang::ComplexType>(ty);
+    const auto *ct = cast<clang::ComplexType>(ty);
     mlir::Type elementTy = convertType(ct->getElementType());
     resultType = cir::ComplexType::get(elementTy);
     break;
