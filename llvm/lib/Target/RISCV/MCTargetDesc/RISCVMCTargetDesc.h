fb71497cdb34154b64c3d96a2238ad2061740b17
Francesco Petrogalli <francesco.petrogalli@apple.com>
2025-05-27 15:18:28 -0700

    [RISCV] Add basic MachO support.
    Based on a patch written by Tim
    Northover (https://github.com/TNorthover).

--- llvm/lib/Target/RISCV/MCTargetDesc/RISCVMCTargetDesc.h
+++ llvm/lib/Target/RISCV/MCTargetDesc/RISCVMCTargetDesc.h
@@ -36,6 +36,9 @@ MCAsmBackend *createRISCVAsmBackend(const Target &T, const MCSubtargetInfo &STI,
 
 std::unique_ptr<MCObjectTargetWriter> createRISCVELFObjectWriter(uint8_t OSABI,
                                                                  bool Is64Bit);
+std::unique_ptr<MCObjectTargetWriter>
+createRISCVMachObjectWriter(uint32_t CPUType, uint32_t CPUSubtype);
+
 } // namespace llvm
 
 // Defines symbolic names for RISC-V registers.
