llvm/llvm-project

commit 46281991dee56859885e3c269d3226d9408bfbb9

[LoopVectorizer] Prune VFs based on plan register pressure (#132190)
This PR moves the register usage checking to after the plans are
created, so that any recipes that optimise register usage (such as
partial reductions) can be properly costed and not have their VF pruned
unnecessarily.

Depends on https://github.com/llvm/llvm-project/pull/137746

llvm/lib/Transforms/Vectorize/LoopVectorize.cpp
@@ -996,12 +996,15 @@ public:
     /// Holds the maximum number of concurrent live intervals in the loop.
     /// The key is ClassID of target-provided register class.
     SmallMapVector<unsigned, unsigned, 4> MaxLocalUsers;
-  };
 
-  /// \return Returns information about the register usages of the loop for the
-  /// given vectorization factors.
-  SmallVector<RegisterUsage, 8>
-  calculateRegisterUsage(ArrayRef<ElementCount> VFs);
+    /// Check if any of the tracked live intervals exceeds the number of
+    /// available registers for the target.
+    bool exceedsMaxNumRegs(const TargetTransformInfo &TTI) const {
+      return any_of(MaxLocalUsers, [&TTI](auto &LU) {
+        return LU.second > TTI.getNumberOfRegisters(LU.first);
+      });
+    }
+  };
 
   /// Collect values we want to ignore in the cost model.
   void collectValuesToIgnore();
@@ -4013,29 +4016,8 @@ ElementCount LoopVectorizationCostModel::getMaximizedVFForTarget(
     auto MaxVectorElementCountMaxBW = ElementCount::get(
         llvm::bit_floor(WidestRegister.getKnownMinValue() / SmallestType),
         ComputeScalableMaxVF);
-    MaxVectorElementCountMaxBW = MinVF(MaxVectorElementCountMaxBW, MaxSafeVF);
+    MaxVF = MinVF(MaxVectorElementCountMaxBW, MaxSafeVF);
 
-    // Collect all viable vectorization factors larger than the default MaxVF
-    // (i.e. MaxVectorElementCount).
-    SmallVector<ElementCount, 8> VFs;
-    for (ElementCount VS = MaxVectorElementCount * 2;
-         ElementCount::isKnownLE(VS, MaxVectorElementCountMaxBW); VS *= 2)
-      VFs.push_back(VS);
-
-    // For each VF calculate its register usage.
-    auto RUs = calculateRegisterUsage(VFs);
-
-    // Select the largest VF which doesn't require more registers than existing
-    // ones.
-    for (int I = RUs.size() - 1; I >= 0; --I) {
-      const auto &MLU = RUs[I].MaxLocalUsers;
-      if (all_of(MLU, [&](decltype(MLU.front()) &LU) {
-            return LU.second <= TTI.getNumberOfRegisters(LU.first);
-          })) {
-        MaxVF = VFs[I];
-        break;
-      }
-    }
     if (ElementCount MinVF =
             TTI.getMinimumVF(SmallestType, ComputeScalableMaxVF)) {
       if (ElementCount::isKnownLT(MaxVF, MinVF)) {
@@ -4360,6 +4342,15 @@ static bool hasReplicatorRegion(VPlan &Plan) {
 }
 
 #ifndef NDEBUG
+/// Estimate the register usage for \p Plan and vectorization factors in \p VFs
+/// by calculating the highest number of values that are live at a single
+/// location as a rough estimate. Returns the register usage for each VF in \p
+/// VFs.
+static SmallVector<LoopVectorizationCostModel::RegisterUsage, 8>
+calculateRegisterUsage(VPlan &Plan, ArrayRef<ElementCount> VFs,
+                       const TargetTransformInfo &TTI,
+                       const SmallPtrSetImpl<const Value *> &ValuesToIgnore);
+
 VectorizationFactor LoopVectorizationPlanner::selectVectorizationFactor() {
   InstructionCost ExpectedCost = CM.expectedCost(ElementCount::getFixed(1));
   LLVM_DEBUG(dbgs() << "LV: Scalar loop costs: " << ExpectedCost << ".\n");
@@ -4383,9 +4374,17 @@ VectorizationFactor LoopVectorizationPlanner::selectVectorizationFactor() {
   }
 
   for (auto &P : VPlans) {
-    for (ElementCount VF : P->vectorFactors()) {
+    ArrayRef<ElementCount> VFs(P->vectorFactors().begin(),
+                               P->vectorFactors().end());
+    auto RUs = ::calculateRegisterUsage(*P, VFs, TTI, CM.ValuesToIgnore);
+    for (auto [VF, RU] : zip_equal(VFs, RUs)) {
       // The cost for scalar VF=1 is already calculated, so ignore it.
       if (VF.isScalar())
+        continue;
+
+      /// Don't consider the VF if it exceeds the number of registers for the
+      /// target.
+      if (RU.exceedsMaxNumRegs(TTI))
         continue;
 
       InstructionCost C = CM.expectedCost(VF);
@@ -4859,9 +4858,13 @@ calculateRegisterUsage(VPlan &Plan, ArrayRef<ElementCount> VFs,
             isa<VPCanonicalIVPHIRecipe, VPReplicateRecipe, VPDerivedIVRecipe,
                 VPScalarIVStepsRecipe>(R) ||
             (isa<VPInstruction>(R) &&
-             all_of(cast<VPSingleDefRecipe>(R)->users(), [&](VPUser *U) {
-               return cast<VPRecipeBase>(U)->usesScalars(R->getVPSingleValue());
-             }))) {
+             all_of(cast<VPSingleDefRecipe>(R)->users(),
+                    [&](VPUser *U) {
+                      return cast<VPRecipeBase>(U)->usesScalars(
+                          R->getVPSingleValue());
+                    })) ||
+            (isa<VPReductionPHIRecipe>(R) &&
+             (cast<VPReductionPHIRecipe>(R))->isInLoop())) {
           unsigned ClassID = TTI.getRegisterClassForType(
               false, TypeInfo.inferScalarType(R->getVPSingleValue()));
           // FIXME: The target might use more than one register for the type
@@ -5232,213 +5235,6 @@ LoopVectorizationCostModel::selectInterleaveCount(VPlan &Plan, ElementCount VF,
 
   LLVM_DEBUG(dbgs() << "LV: Not Interleaving.\n");
   return 1;
-}
-
-SmallVector<LoopVectorizationCostModel::RegisterUsage, 8>
-LoopVectorizationCostModel::calculateRegisterUsage(ArrayRef<ElementCount> VFs) {
-  // This function calculates the register usage by measuring the highest number
-  // of values that are alive at a single location. Obviously, this is a very
-  // rough estimation. We scan the loop in a topological order in order and
-  // assign a number to each instruction. We use RPO to ensure that defs are
-  // met before their users. We assume that each instruction that has in-loop
-  // users starts an interval. We record every time that an in-loop value is
-  // used, so we have a list of the first and last occurrences of each
-  // instruction. Next, we transpose this data structure into a multi map that
-  // holds the list of intervals that *end* at a specific location. This multi
-  // map allows us to perform a linear search. We scan the instructions linearly
-  // and record each time that a new interval starts, by placing it in a set.
-  // If we find this value in the multi-map then we remove it from the set.
-  // The max register usage is the maximum size of the set.
-  // We also search for instructions that are defined outside the loop, but are
-  // used inside the loop. We need this number separately from the max-interval
-  // usage number because when we unroll, loop-invariant values do not take
-  // more registers.
-  LoopBlocksDFS DFS(TheLoop);
-  DFS.perform(LI);
-
-  RegisterUsage RU;
-
-  // Each 'key' in the map opens a new interval. The values
-  // of the map are the index of the 'last seen' usage of the
-  // instruction that is the key.
-  using IntervalMap = SmallDenseMap<Instruction *, unsigned, 16>;
-
-  // Maps instruction to its index.
-  SmallVector<Instruction *, 64> IdxToInstr;
-  // Marks the end of each interval.
-  IntervalMap EndPoint;
-  // Saves the list of instruction indices that are used in the loop.
-  SmallPtrSet<Instruction *, 8> Ends;
-  // Saves the list of values that are used in the loop but are defined outside
-  // the loop (not including non-instruction values such as arguments and
-  // constants).
-  SmallSetVector<Instruction *, 8> LoopInvariants;
-
-  for (BasicBlock *BB : make_range(DFS.beginRPO(), DFS.endRPO())) {
-    for (Instruction &I : BB->instructionsWithoutDebug()) {
-      IdxToInstr.push_back(&I);
-
-      // Save the end location of each USE.
-      for (Value *U : I.operands()) {
-        auto *Instr = dyn_cast<Instruction>(U);
-
-        // Ignore non-instruction values such as arguments, constants, etc.
-        // FIXME: Might need some motivation why these values are ignored. If
-        // for example an argument is used inside the loop it will increase the
-        // register pressure (so shouldn't we add it to LoopInvariants).
-        if (!Instr)
-          continue;
-
-        // If this instruction is outside the loop then record it and continue.
-        if (!TheLoop->contains(Instr)) {
-          LoopInvariants.insert(Instr);
-          continue;
-        }
-
-        // Overwrite previous end points.
-        EndPoint[Instr] = IdxToInstr.size();
-        Ends.insert(Instr);
-      }
-    }
-  }
-
-  // Saves the list of intervals that end with the index in 'key'.
-  using InstrList = SmallVector<Instruction *, 2>;
-  SmallDenseMap<unsigned, InstrList, 16> TransposeEnds;
-
-  // Transpose the EndPoints to a list of values that end at each index.
-  for (auto &Interval : EndPoint)
-    TransposeEnds[Interval.second].push_back(Interval.first);
-
-  SmallPtrSet<Instruction *, 8> OpenIntervals;
-  SmallVector<RegisterUsage, 8> RUs(VFs.size());
-  SmallVector<SmallMapVector<unsigned, unsigned, 4>, 8> MaxUsages(VFs.size());
-
-  LLVM_DEBUG(dbgs() << "LV(REG): Calculating max register usage:\n");
-
-  const auto &TTICapture = TTI;
-  auto GetRegUsage = [&TTICapture](Type *Ty, ElementCount VF) -> unsigned {
-    if (Ty->isTokenTy() || !VectorType::isValidElementType(Ty) ||
-        (VF.isScalable() &&
-         !TTICapture.isElementTypeLegalForScalableVector(Ty)))
-      return 0;
-    return TTICapture.getRegUsageForType(VectorType::get(Ty, VF));
-  };
-
-  collectInLoopReductions();
-
-  for (unsigned int Idx = 0, Sz = IdxToInstr.size(); Idx < Sz; ++Idx) {
-    Instruction *I = IdxToInstr[Idx];
-
-    // Remove all of the instructions that end at this location.
-    InstrList &List = TransposeEnds[Idx];
-    for (Instruction *ToRemove : List)
-      OpenIntervals.erase(ToRemove);
-
-    // Ignore instructions that are never used within the loop and do not have
-    // side-effects.
-    if (!Ends.count(I) && !I->mayHaveSideEffects())
-      continue;
-
-    // Skip ignored values.
-    if (ValuesToIgnore.count(I))
-      continue;
-
-    // For each VF find the maximum usage of registers.
-    for (unsigned J = 0, E = VFs.size(); J < E; ++J) {
-      // Count the number of registers used, per register class, given all open
-      // intervals.
-      // Note that elements in this SmallMapVector will be default constructed
-      // as 0. So we can use "RegUsage[ClassID] += n" in the code below even if
-      // there is no previous entry for ClassID.
-      SmallMapVector<unsigned, unsigned, 4> RegUsage;
-
-      if (VFs[J].isScalar()) {
-        for (auto *Inst : OpenIntervals) {
-          unsigned ClassID =
-              TTI.getRegisterClassForType(false, Inst->getType());
-          // FIXME: The target might use more than one register for the type
-          // even in the scalar case.
-          RegUsage[ClassID] += 1;
-        }
-      } else {
-        collectNonVectorizedAndSetWideningDecisions(VFs[J]);
-        for (auto *Inst : OpenIntervals) {
-          // Skip ignored values for VF > 1.
-          if (VecValuesToIgnore.count(Inst))
-            continue;
-          if (isScalarAfterVectorization(Inst, VFs[J])) {
-            unsigned ClassID =
-                TTI.getRegisterClassForType(false, Inst->getType());
-            // FIXME: The target might use more than one register for the type
-            // even in the scalar case.
-            RegUsage[ClassID] += 1;
-          } else {
-            unsigned ClassID =
-                TTI.getRegisterClassForType(true, Inst->getType());
-            RegUsage[ClassID] += GetRegUsage(Inst->getType(), VFs[J]);
-          }
-        }
-      }
-
-      for (const auto &Pair : RegUsage) {
-        auto &Entry = MaxUsages[J][Pair.first];
-        Entry = std::max(Entry, Pair.second);
-      }
-    }
-
-    LLVM_DEBUG(dbgs() << "LV(REG): At #" << Idx << " Interval # "
-                      << OpenIntervals.size() << '\n');
-
-    // Add the current instruction to the list of open intervals.
-    OpenIntervals.insert(I);
-  }
-
-  for (unsigned Idx = 0, End = VFs.size(); Idx < End; ++Idx) {
-    // Note that elements in this SmallMapVector will be default constructed
-    // as 0. So we can use "Invariant[ClassID] += n" in the code below even if
-    // there is no previous entry for ClassID.
-    SmallMapVector<unsigned, unsigned, 4> Invariant;
-
-    for (auto *Inst : LoopInvariants) {
-      // FIXME: The target might use more than one register for the type
-      // even in the scalar case.
-      bool IsScalar = all_of(Inst->users(), [&](User *U) {
-        auto *I = cast<Instruction>(U);
-        return TheLoop != LI->getLoopFor(I->getParent()) ||
-               isScalarAfterVectorization(I, VFs[Idx]);
-      });
-
-      ElementCount VF = IsScalar ? ElementCount::getFixed(1) : VFs[Idx];
-      unsigned ClassID =
-          TTI.getRegisterClassForType(VF.isVector(), Inst->getType());
-      Invariant[ClassID] += GetRegUsage(Inst->getType(), VF);
-    }
-
-    LLVM_DEBUG({
-      dbgs() << "LV(REG): VF = " << VFs[Idx] << '\n';
-      dbgs() << "LV(REG): Found max usage: " << MaxUsages[Idx].size()
-             << " item\n";
-      for (const auto &pair : MaxUsages[Idx]) {
-        dbgs() << "LV(REG): RegisterClass: "
-               << TTI.getRegisterClassName(pair.first) << ", " << pair.second
-               << " registers\n";
-      }
-      dbgs() << "LV(REG): Found invariant usage: " << Invariant.size()
-             << " item\n";
-      for (const auto &pair : Invariant) {
-        dbgs() << "LV(REG): RegisterClass: "
-               << TTI.getRegisterClassName(pair.first) << ", " << pair.second
-               << " registers\n";
-      }
-    });
-
-    RU.LoopInvariantRegs = Invariant;
-    RU.MaxLocalUsers = MaxUsages[Idx];
-    RUs[Idx] = RU;
-  }
-
-  return RUs;
 }
 
 bool LoopVectorizationCostModel::useEmulatedMaskMemRefHack(Instruction *I,
@@ -7621,7 +7417,10 @@ VectorizationFactor LoopVectorizationPlanner::computeBestVF() {
   }
 
   for (auto &P : VPlans) {
-    for (ElementCount VF : P->vectorFactors()) {
+    ArrayRef<ElementCount> VFs(P->vectorFactors().begin(),
+                               P->vectorFactors().end());
+    auto RUs = ::calculateRegisterUsage(*P, VFs, TTI, CM.ValuesToIgnore);
+    for (auto [VF, RU] : zip_equal(VFs, RUs)) {
       if (VF.isScalar())
         continue;
       if (!ForceVectorization && !willGenerateVectors(*P, VF, TTI)) {
@@ -7642,6 +7441,13 @@ VectorizationFactor LoopVectorizationPlanner::computeBestVF() {
 
       InstructionCost Cost = cost(*P, VF);
       VectorizationFactor CurrentFactor(VF, Cost, ScalarCost);
+
+      if (RU.exceedsMaxNumRegs(TTI)) {
+        LLVM_DEBUG(dbgs() << "LV(REG): Not considering vector loop of width "
+                          << VF << " because it uses too many registers\n");
+        continue;
+      }
+
       if (isMoreProfitable(CurrentFactor, BestFactor, P->hasScalarTail()))
         BestFactor = CurrentFactor;
 

llvm/test/Transforms/LoopVectorize/AArch64/reg-usage.ll
@@ -74,8 +74,8 @@ define dso_local void @dotp_high_register_pressure(ptr %a, ptr %b, ptr %sum, i32
 ; CHECK:       LV(REG): VF = 16
 ; CHECK-NEXT:  LV(REG): Found max usage: 2 item
 ; CHECK-NEXT:  LV(REG): RegisterClass: Generic::ScalarRC, 3 registers
-; CHECK-NEXT:  LV(REG): RegisterClass: Generic::VectorRC, 40 registers
-; CHECK-NEXT:  LV(REG): Found invariant usage: 2 item
+; CHECK-NEXT:  LV(REG): RegisterClass: Generic::VectorRC, 48 registers
+; CHECK-NEXT:  LV(REG): Found invariant usage: 1 item
 entry:
   %cmp100 = icmp sgt i32 %n, 0
   br i1 %cmp100, label %for.body.lr.ph, label %for.cond.cleanup
@@ -193,7 +193,7 @@ define i32 @dotp_unrolled(i32 %num_out, i64 %num_in, ptr %a, ptr %b) {
 ; CHECK-NEXT:  LV(REG): Found max usage: 2 item
 ; CHECK-NEXT:  LV(REG): RegisterClass: Generic::ScalarRC, 9 registers
 ; CHECK-NEXT:  LV(REG): RegisterClass: Generic::VectorRC, 24 registers
-; CHECK-NEXT:  LV(REG): Found invariant usage: 0 item
+; CHECK-NEXT:  LV(REG): Found invariant usage: 1 item
 entry:
   br label %for.body
 

llvm/test/Transforms/LoopVectorize/ARM/mve-known-trip-count.ll
@@ -200,44 +200,16 @@ for.body:                                         ; preds = %entry, %for.body
 ; CHECK: LV: Selecting VF: 4
 define void @tripcount8(ptr nocapture readonly %in, ptr nocapture %out, ptr nocapture readonly %consts, i32 %n) #0 {
 entry:
-  %arrayidx20 = getelementptr inbounds i32, ptr %out, i32 1
-  %arrayidx38 = getelementptr inbounds i32, ptr %out, i32 2
-  %arrayidx56 = getelementptr inbounds i32, ptr %out, i32 3
-  %arrayidx74 = getelementptr inbounds i32, ptr %out, i32 4
-  %arrayidx92 = getelementptr inbounds i32, ptr %out, i32 5
-  %arrayidx110 = getelementptr inbounds i32, ptr %out, i32 6
-  %arrayidx128 = getelementptr inbounds i32, ptr %out, i32 7
   %out.promoted = load i32, ptr %out, align 4
-  %arrayidx20.promoted = load i32, ptr %arrayidx20, align 4
-  %arrayidx38.promoted = load i32, ptr %arrayidx38, align 4
-  %arrayidx56.promoted = load i32, ptr %arrayidx56, align 4
-  %arrayidx74.promoted = load i32, ptr %arrayidx74, align 4
-  %arrayidx92.promoted = load i32, ptr %arrayidx92, align 4
-  %arrayidx110.promoted = load i32, ptr %arrayidx110, align 4
-  %arrayidx128.promoted = load i32, ptr %arrayidx128, align 4
   br label %for.body
 
 for.cond.cleanup:                                 ; preds = %for.body
   store i32 %add12, ptr %out, align 4
-  store i32 %add30, ptr %arrayidx20, align 4
-  store i32 %add48, ptr %arrayidx38, align 4
-  store i32 %add66, ptr %arrayidx56, align 4
-  store i32 %add84, ptr %arrayidx74, align 4
-  store i32 %add102, ptr %arrayidx92, align 4
-  store i32 %add120, ptr %arrayidx110, align 4
-  store i32 %add138, ptr %arrayidx128, align 4
   ret void
 
 for.body:                                         ; preds = %entry, %for.body
   %hop.0236 = phi i32 [ 0, %entry ], [ %add139, %for.body ]
   %add12220235 = phi i32 [ %out.promoted, %entry ], [ %add12, %for.body ]
-  %add30221234 = phi i32 [ %arrayidx20.promoted, %entry ], [ %add30, %for.body ]
-  %add48222233 = phi i32 [ %arrayidx38.promoted, %entry ], [ %add48, %for.body ]
-  %add66223232 = phi i32 [ %arrayidx56.promoted, %entry ], [ %add66, %for.body ]
-  %add84224231 = phi i32 [ %arrayidx74.promoted, %entry ], [ %add84, %for.body ]
-  %add102225230 = phi i32 [ %arrayidx92.promoted, %entry ], [ %add102, %for.body ]
-  %add120226229 = phi i32 [ %arrayidx110.promoted, %entry ], [ %add120, %for.body ]
-  %add138227228 = phi i32 [ %arrayidx128.promoted, %entry ], [ %add138, %for.body ]
   %arrayidx = getelementptr inbounds i16, ptr %in, i32 %hop.0236
   %0 = load i16, ptr %arrayidx, align 2
   %conv = sext i16 %0 to i32
@@ -255,132 +227,6 @@ for.body:                                         ; preds = %entry, %for.body
   %conv9 = sext i16 %3 to i32
   %mul10 = mul nsw i32 %conv9, %conv6
   %add12 = add nsw i32 %mul10, %add
-  %add13 = or i32 %hop.0236, 2
-  %arrayidx14 = getelementptr inbounds i16, ptr %in, i32 %add13
-  %4 = load i16, ptr %arrayidx14, align 2
-  %conv15 = sext i16 %4 to i32
-  %arrayidx17 = getelementptr inbounds i16, ptr %consts, i32 %add13
-  %5 = load i16, ptr %arrayidx17, align 2
-  %conv18 = sext i16 %5 to i32
-  %mul19 = mul nsw i32 %conv18, %conv15
-  %add21 = add nsw i32 %mul19, %add30221234
-  %add22 = or i32 %hop.0236, 3
-  %arrayidx23 = getelementptr inbounds i16, ptr %in, i32 %add22
-  %6 = load i16, ptr %arrayidx23, align 2
-  %conv24 = sext i16 %6 to i32
-  %arrayidx26 = getelementptr inbounds i16, ptr %consts, i32 %add22
-  %7 = load i16, ptr %arrayidx26, align 2
-  %conv27 = sext i16 %7 to i32
-  %mul28 = mul nsw i32 %conv27, %conv24
-  %add30 = add nsw i32 %mul28, %add21
-  %add31 = or i32 %hop.0236, 4
-  %arrayidx32 = getelementptr inbounds i16, ptr %in, i32 %add31
-  %8 = load i16, ptr %arrayidx32, align 2
-  %conv33 = sext i16 %8 to i32
-  %arrayidx35 = getelementptr inbounds i16, ptr %consts, i32 %add31
-  %9 = load i16, ptr %arrayidx35, align 2
-  %conv36 = sext i16 %9 to i32
-  %mul37 = mul nsw i32 %conv36, %conv33
-  %add39 = add nsw i32 %mul37, %add48222233
-  %add40 = or i32 %hop.0236, 5
-  %arrayidx41 = getelementptr inbounds i16, ptr %in, i32 %add40
-  %10 = load i16, ptr %arrayidx41, align 2
-  %conv42 = sext i16 %10 to i32
-  %arrayidx44 = getelementptr inbounds i16, ptr %consts, i32 %add40
-  %11 = load i16, ptr %arrayidx44, align 2
-  %conv45 = sext i16 %11 to i32
-  %mul46 = mul nsw i32 %conv45, %conv42
-  %add48 = add nsw i32 %mul46, %add39
-  %add49 = or i32 %hop.0236, 6
-  %arrayidx50 = getelementptr inbounds i16, ptr %in, i32 %add49
-  %12 = load i16, ptr %arrayidx50, align 2
-  %conv51 = sext i16 %12 to i32
-  %arrayidx53 = getelementptr inbounds i16, ptr %consts, i32 %add49
-  %13 = load i16, ptr %arrayidx53, align 2
-  %conv54 = sext i16 %13 to i32
-  %mul55 = mul nsw i32 %conv54, %conv51
-  %add57 = add nsw i32 %mul55, %add66223232
-  %add58 = or i32 %hop.0236, 7
-  %arrayidx59 = getelementptr inbounds i16, ptr %in, i32 %add58
-  %14 = load i16, ptr %arrayidx59, align 2
-  %conv60 = sext i16 %14 to i32
-  %arrayidx62 = getelementptr inbounds i16, ptr %consts, i32 %add58
-  %15 = load i16, ptr %arrayidx62, align 2
-  %conv63 = sext i16 %15 to i32
-  %mul64 = mul nsw i32 %conv63, %conv60
-  %add66 = add nsw i32 %mul64, %add57
-  %add67 = or i32 %hop.0236, 8
-  %arrayidx68 = getelementptr inbounds i16, ptr %in, i32 %add67
-  %16 = load i16, ptr %arrayidx68, align 2
-  %conv69 = sext i16 %16 to i32
-  %arrayidx71 = getelementptr inbounds i16, ptr %consts, i32 %add67
-  %17 = load i16, ptr %arrayidx71, align 2
-  %conv72 = sext i16 %17 to i32
-  %mul73 = mul nsw i32 %conv72, %conv69
-  %add75 = add nsw i32 %mul73, %add84224231
-  %add76 = or i32 %hop.0236, 9
-  %arrayidx77 = getelementptr inbounds i16, ptr %in, i32 %add76
-  %18 = load i16, ptr %arrayidx77, align 2
-  %conv78 = sext i16 %18 to i32
-  %arrayidx80 = getelementptr inbounds i16, ptr %consts, i32 %add76
-  %19 = load i16, ptr %arrayidx80, align 2
-  %conv81 = sext i16 %19 to i32
-  %mul82 = mul nsw i32 %conv81, %conv78
-  %add84 = add nsw i32 %mul82, %add75
-  %add85 = or i32 %hop.0236, 10
-  %arrayidx86 = getelementptr inbounds i16, ptr %in, i32 %add85
-  %20 = load i16, ptr %arrayidx86, align 2
-  %conv87 = sext i16 %20 to i32
-  %arrayidx89 = getelementptr inbounds i16, ptr %consts, i32 %add85
-  %21 = load i16, ptr %arrayidx89, align 2
-  %conv90 = sext i16 %21 to i32
-  %mul91 = mul nsw i32 %conv90, %conv87
-  %add93 = add nsw i32 %mul91, %add102225230
-  %add94 = or i32 %hop.0236, 11
-  %arrayidx95 = getelementptr inbounds i16, ptr %in, i32 %add94
-  %22 = load i16, ptr %arrayidx95, align 2
-  %conv96 = sext i16 %22 to i32
-  %arrayidx98 = getelementptr inbounds i16, ptr %consts, i32 %add94
-  %23 = load i16, ptr %arrayidx98, align 2
-  %conv99 = sext i16 %23 to i32
-  %mul100 = mul nsw i32 %conv99, %conv96
-  %add102 = add nsw i32 %mul100, %add93
-  %add103 = or i32 %hop.0236, 12
-  %arrayidx104 = getelementptr inbounds i16, ptr %in, i32 %add103
-  %24 = load i16, ptr %arrayidx104, align 2
-  %conv105 = sext i16 %24 to i32
-  %arrayidx107 = getelementptr inbounds i16, ptr %consts, i32 %add103
-  %25 = load i16, ptr %arrayidx107, align 2
-  %conv108 = sext i16 %25 to i32
-  %mul109 = mul nsw i32 %conv108, %conv105
-  %add111 = add nsw i32 %mul109, %add120226229
-  %add112 = or i32 %hop.0236, 13
-  %arrayidx113 = getelementptr inbounds i16, ptr %in, i32 %add112
-  %26 = load i16, ptr %arrayidx113, align 2
-  %conv114 = sext i16 %26 to i32
-  %arrayidx116 = getelementptr inbounds i16, ptr %consts, i32 %add112
-  %27 = load i16, ptr %arrayidx116, align 2
-  %conv117 = sext i16 %27 to i32
-  %mul118 = mul nsw i32 %conv117, %conv114
-  %add120 = add nsw i32 %mul118, %add111
-  %add121 = or i32 %hop.0236, 14
-  %arrayidx122 = getelementptr inbounds i16, ptr %in, i32 %add121
-  %28 = load i16, ptr %arrayidx122, align 2
-  %conv123 = sext i16 %28 to i32
-  %arrayidx125 = getelementptr inbounds i16, ptr %consts, i32 %add121
-  %29 = load i16, ptr %arrayidx125, align 2
-  %conv126 = sext i16 %29 to i32
-  %mul127 = mul nsw i32 %conv126, %conv123
-  %add129 = add nsw i32 %mul127, %add138227228
-  %add130 = or i32 %hop.0236, 15
-  %arrayidx131 = getelementptr inbounds i16, ptr %in, i32 %add130
-  %30 = load i16, ptr %arrayidx131, align 2
-  %conv132 = sext i16 %30 to i32
-  %arrayidx134 = getelementptr inbounds i16, ptr %consts, i32 %add130
-  %31 = load i16, ptr %arrayidx134, align 2
-  %conv135 = sext i16 %31 to i32
-  %mul136 = mul nsw i32 %conv135, %conv132
-  %add138 = add nsw i32 %mul136, %add129
   %add139 = add nuw nsw i32 %hop.0236, 16
   %cmp = icmp ult i32 %hop.0236, 112
   br i1 %cmp, label %for.body, label %for.cond.cleanup

commit f79a4676b3276ba57dd049c55ebb221707c57157

[LV] Fix MVE regression from #132190
Register pressure was only considered if the vector bandwidth was being
maximised (chosen either by the target or user options), but #132190
inadvertently caused high pressure VFs to be pruned even when max
bandwidth wasn't enabled. This PR returns to the previous behaviour.

llvm/lib/Transforms/Vectorize/LoopVectorize.cpp
@@ -959,6 +959,10 @@ public:
     return expectedCost(UserVF).isValid();
   }
 
+  /// \return True if maximizing vector bandwidth is enabled by the target or
+  /// user options.
+  bool useMaxBandwidth(TargetTransformInfo::RegisterKind RegKind);
+
   /// \return The size (in bits) of the smallest and widest types in the code
   /// that needs to be vectorized. We ignore values that remain scalar such as
   /// 64 bit loop indices.
@@ -3944,6 +3948,14 @@ LoopVectorizationCostModel::computeMaxVF(ElementCount UserVF, unsigned UserIC) {
   return FixedScalableVFPair::getNone();
 }
 
+bool LoopVectorizationCostModel::useMaxBandwidth(
+    TargetTransformInfo::RegisterKind RegKind) {
+  return MaximizeBandwidth || (MaximizeBandwidth.getNumOccurrences() == 0 &&
+                               (TTI.shouldMaximizeVectorBandwidth(RegKind) ||
+                                (UseWiderVFIfCallVariantsPresent &&
+                                 Legal->hasVectorCallVariants())));
+}
+
 ElementCount LoopVectorizationCostModel::getMaximizedVFForTarget(
     unsigned MaxTripCount, unsigned SmallestType, unsigned WidestType,
     ElementCount MaxSafeVF, bool FoldTailByMasking) {
@@ -4009,10 +4021,7 @@ ElementCount LoopVectorizationCostModel::getMaximizedVFForTarget(
       ComputeScalableMaxVF ? TargetTransformInfo::RGK_ScalableVector
                            : TargetTransformInfo::RGK_FixedWidthVector;
   ElementCount MaxVF = MaxVectorElementCount;
-  if (MaximizeBandwidth ||
-      (MaximizeBandwidth.getNumOccurrences() == 0 &&
-       (TTI.shouldMaximizeVectorBandwidth(RegKind) ||
-        (UseWiderVFIfCallVariantsPresent && Legal->hasVectorCallVariants())))) {
+  if (useMaxBandwidth(RegKind)) {
     auto MaxVectorElementCountMaxBW = ElementCount::get(
         llvm::bit_floor(WidestRegister.getKnownMinValue() / SmallestType),
         ComputeScalableMaxVF);
@@ -4384,7 +4393,10 @@ VectorizationFactor LoopVectorizationPlanner::selectVectorizationFactor() {
 
       /// Don't consider the VF if it exceeds the number of registers for the
       /// target.
-      if (RU.exceedsMaxNumRegs(TTI))
+      if (CM.useMaxBandwidth(VF.isScalable()
+                                 ? TargetTransformInfo::RGK_ScalableVector
+                                 : TargetTransformInfo::RGK_FixedWidthVector) &&
+          RU.exceedsMaxNumRegs(TTI))
         continue;
 
       InstructionCost C = CM.expectedCost(VF);
@@ -7458,7 +7470,10 @@ VectorizationFactor LoopVectorizationPlanner::computeBestVF() {
       InstructionCost Cost = cost(*P, VF);
       VectorizationFactor CurrentFactor(VF, Cost, ScalarCost);
 
-      if (RU.exceedsMaxNumRegs(TTI)) {
+      if (CM.useMaxBandwidth(VF.isScalable()
+                                 ? TargetTransformInfo::RGK_ScalableVector
+                                 : TargetTransformInfo::RGK_FixedWidthVector) &&
+          RU.exceedsMaxNumRegs(TTI)) {
         LLVM_DEBUG(dbgs() << "LV(REG): Not considering vector loop of width "
                           << VF << " because it uses too many registers\n");
         continue;