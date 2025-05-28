33b25e656a22123b4a346ee839d8e9c3efa621bd
Dave Lee <davelee.com@gmail.com>
2025-05-28 15:08:58 -0700

    [lldb] Run TestFrameLanguageCommands.py only on Darwin (#141866)
    Tests using ObjC do not readily run on Linux.

--- lldb/test/API/commands/command/language/TestFrameLanguageCommands.py
+++ lldb/test/API/commands/command/language/TestFrameLanguageCommands.py
@@ -5,6 +5,7 @@ import lldbsuite.test.lldbutil as lldbutil
 
 
 class TestCase(TestBase):
+    @skipUnlessDarwin
     def test(self):
         self.build()
         _, _, thread, _ = lldbutil.run_to_source_breakpoint(
