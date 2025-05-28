873b71681f575175942cca070e694ba5e20da8a3
Sriya Pratipati <sriyap@google.com>
2025-05-28 23:48:58 +0000

    Fixed hyphens in first lines

--- libc/src/wchar/wmemcmp.h
+++ libc/src/wchar/wmemcmp.h
@@ -1,4 +1,4 @@
-//===-- Implementation header for wmemcmp ----------------------------------===//
+//===-- Implementation header for wmemcmp ---------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
