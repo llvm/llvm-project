763ab26fb9280adfab3e281a8d8f4d6b19176a69
Egor Zhdan <e_zhdan@apple.com>
2025-05-28 20:01:23 +0100

    [Clang][Sema] Adjust tests for the diagnostic message change
    Follow-up to 0565b024.
    
    rdar://152087076

--- clang/test/SemaCXX/noescape-attr.cpp
+++ clang/test/SemaCXX/noescape-attr.cpp
@@ -3,7 +3,7 @@
 template<typename T>
 void test1(T __attribute__((noescape)) arr, int size);
 
-void test2(int __attribute__((noescape)) a, int b); // expected-warning {{'noescape' attribute only applies to a pointer, reference, class, struct, or union (0 is invalid)}}
+void test2(int __attribute__((noescape)) a, int b); // expected-warning {{'noescape' attribute only applies to a pointer, reference, class, struct, or union ('int' is invalid)}}
 
 struct S { int *p; };
 void test3(S __attribute__((noescape)) s);
