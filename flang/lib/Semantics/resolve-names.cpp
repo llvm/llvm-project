27f08308ca10d2a85b2ed096f2c67932225177e8
Akash Banerjee <akash.banerjee@amd.com>
2025-05-28 14:32:17 +0100

    [OpenMP][Flang] Fix semantic check and scoping for declare mappers (#140560)
    The current semantic check in place is incorrect, this patch fixes this.
    
    Up to 1 **'default'** named mapper should be allowed for each derived
    type.
    The current semantic check only allows up to 1 **'default'** named
    mapper across all derived types.
    
    This also makes sure that declare mappers follow proper scoping rules
    for both default and named mappers.
    
    Co-authored-by: Raghu Maddhipatla <Raghu.Maddhipatla@amd.com>

--- flang/lib/Semantics/resolve-names.cpp
+++ flang/lib/Semantics/resolve-names.cpp
@@ -1766,15 +1766,9 @@ void OmpVisitor::ProcessMapperSpecifier(const parser::OmpMapperSpecifier &spec,
   // just following the natural flow, the map clauses gets processed before
   // the type has been fully processed.
   BeginDeclTypeSpec();
-  if (auto &mapperName{std::get<std::optional<parser::Name>>(spec.t)}) {
-    mapperName->symbol =
-        &MakeSymbol(*mapperName, MiscDetails{MiscDetails::Kind::ConstructName});
-  } else {
-    const parser::CharBlock defaultName{"default", 7};
-    MakeSymbol(
-        defaultName, Attrs{}, MiscDetails{MiscDetails::Kind::ConstructName});
-  }
-
+  auto &mapperName{std::get<std::string>(spec.t)};
+  MakeSymbol(parser::CharBlock(mapperName), Attrs{},
+      MiscDetails{MiscDetails::Kind::ConstructName});
   PushScope(Scope::Kind::OtherConstruct, nullptr);
   Walk(std::get<parser::TypeSpec>(spec.t));
   auto &varName{std::get<parser::Name>(spec.t)};
