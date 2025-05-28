462dc6afb219a4005e72d516e922c1c4ab0179d9
Andy Kaylor <akaylor@nvidia.com>
2025-05-28 13:49:25 -0700

    [CIR] Update testds for global vars
    This change updates a few tests for global variable handling to also check
    classic codegen output so we can easily verify consistency between the two
    and will be alerted if the classic codegen changes.
    
    This was useful in developing forthcoming changes to global linkage
    handling.

--- clang/test/CIR/global-var-linkage.cpp
+++ clang/test/CIR/global-var-linkage.cpp
@@ -1,11 +1,27 @@
 // Linkage types of global variables
-// RUN: %clang_cc1 -std=c++20 -triple x86_64-unknown-linux-gnu -fclangir -emit-cir %s -o -  | FileCheck %s
+// RUN: %clang_cc1 -std=c++20 -triple x86_64-unknown-linux-gnu -fclangir -emit-cir %s -o %t.cir
+// RUN: FileCheck %s -check-prefix=CIR --input-file %t.cir
+// RUN: %clang_cc1 -std=c++20 -triple x86_64-unknown-linux-gnu -fclangir -emit-llvm %s -o %t-cir.ll
+// RUN: FileCheck %s -check-prefix=LLVM --input-file %t-cir.ll
+// RUN: %clang_cc1 -std=c++20 -triple x86_64-unknown-linux-gnu -fclangir -emit-llvm %s -o %t.ll
+// RUN: FileCheck %s -check-prefix=OGCG --input-file %t.ll
 
 int aaaa;
-// CHECK: cir.global external @aaaa
+// CIR: cir.global external @aaaa
+// LLVM: @aaaa = dso_local global i32 0
+// OGCG: @aaaa = dso_local global i32 0
+
 static int bbbb;
-// CHECK: cir.global internal @_ZL4bbbb
+// CIR: cir.global internal @_ZL4bbbb
+// LLVM: @_ZL4bbbb = internal global i32 0
+// OGCG: @_ZL4bbbb = internal global i32 0
+
 inline int cccc;
-// CHECK: cir.global linkonce_odr @cccc
+// CIR: cir.global linkonce_odr @cccc
+// LLVM: @cccc = linkonce_odr dso_local global i32 0
+// OGCG: @cccc = linkonce_odr dso_local global i32 0
+
 [[gnu::selectany]] int dddd;
-// CHECK: cir.global weak_odr @dddd
+// CIR: cir.global weak_odr @dddd
+// LLVM: @dddd = weak_odr dso_local global i32 0
+// OGCG: @dddd = weak_odr dso_local global i32 0
