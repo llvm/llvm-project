40c12aca6e31044c10592d1b5b15f0fd132744a4
Tian, Shilei <Shilei.Tian@amd.com>
2025-05-29 12:40:31 -0400

    [AMDGPU] Enable t16 for `v_sqrt_bf16` (#2381)

--- llvm/test/MC/AMDGPU/gfx1250_asm_vop1_dpp16.s
+++ llvm/test/MC/AMDGPU/gfx1250_asm_vop1_dpp16.s
@@ -346,6 +346,10 @@ v_sqrt_bf16 v127, -|v127| row_xmask:15 row_mask:0x3 bank_mask:0x0 bound_ctrl:0 f
 // GFX1250: v_sqrt_bf16_dpp v127, -|v127| row_xmask:15 row_mask:0x3 bank_mask:0x0 fi:1 ; encoding: [0xfa,0xf4,0xfe,0x7e,0x7f,0x6f,0x35,0x30]
 // GFX12-ERR: :[[@LINE-2]]:1: error: instruction not supported on this GPU
 
+v_sqrt_bf16 v5.h, v1.h quad_perm:[3,2,1,0]
+// GFX1250: v_sqrt_bf16_dpp v5.h, v1.h quad_perm:[3,2,1,0] row_mask:0xf bank_mask:0xf ; encoding: [0xfa,0xf4,0x0a,0x7f,0x81,0x1b,0x00,0xff]
+// GFX12-ERR: :[[@LINE-2]]:1: error: instruction not supported on this GPU
+
 v_rsq_bf16 v5, v1 quad_perm:[3,2,1,0]
 // GFX1250: v_rsq_bf16_dpp v5, v1 quad_perm:[3,2,1,0] row_mask:0xf bank_mask:0xf ; encoding: [0xfa,0xf6,0x0a,0x7e,0x01,0x1b,0x00,0xff]
 // GFX12-ERR: :[[@LINE-2]]:1: error: instruction not supported on this GPU
