676f5277f0e96975b2c6ab3fc22776ef8c4c5bec
Andy Kaylor <akaylor@nvidia.com>
2025-05-28 17:12:03 -0700

    [CIR] Update tests for global vars (#141854)
    This change updates a few tests for global variable handling to also
    check classic codegen output so we can easily verify consistency between
    the two and will be alerted if the classic codegen changes.
    
    This was useful in developing forthcoming changes to global linkage
    handling.

--- clang/test/CIR/Lowering/hello.c
+++ clang/test/CIR/Lowering/hello.c
@@ -1,10 +1,18 @@
 // Smoke test for ClangIR-to-LLVM IR code generation
-// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fclangir -emit-llvm %s -o -  | FileCheck %s
+// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fclangir -emit-llvm %s -o %t-cir.ll
+// RUN: FileCheck %s --input-file %t-cir.ll
+// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -emit-llvm %s -o %t.ll
+// RUN: FileCheck %s -check-prefix=OGCG --input-file %t.ll
+
+// Note: The differences between CHECK and OGCG represent incorrect IR lowering
+//       with ClangIR enabled and will be fixed in a future patch.
 
 int b = 2;
 
-// CHECK: @b = dso_local global i32 2
+// CHECK: @b = dso_local global i32 2, align 4
+// OGCG:  @b = global i32 2, align 4
 
 int a;
 
-// CHECK: @a = dso_local global i32 0
+// CHECK: @a = dso_local global i32 0, align 4
+// OGCG:  @a = global i32 0, align 4
