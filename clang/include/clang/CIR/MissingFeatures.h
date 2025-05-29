d6e0ab92adad4110280ac86a2e5f33f5a690b65f
Andres Salamanca <andrealebarbaritos@gmail.com>
2025-05-29 16:17:09 -0500

    Add initial support for bitfields in structs and add tests

--- clang/include/clang/CIR/MissingFeatures.h
+++ clang/include/clang/CIR/MissingFeatures.h
@@ -149,6 +149,8 @@ struct MissingFeatures {
   static bool cxxabiUseARMGuardVarABI() { return false; }
   static bool cxxabiAppleARM64CXXABI() { return false; }
   static bool cxxabiStructorImplicitParam() { return false; }
+  static bool isDiscreteBitFieldABI() { return false; }
+  static bool isBigEndian() { return false; }
 
   // Address class
   static bool addressOffset() { return false; }
