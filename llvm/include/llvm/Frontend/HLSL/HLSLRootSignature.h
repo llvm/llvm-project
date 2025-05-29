832708139925eda0620f213efd81c9f432740816
Finn Plummer <finn.c.plum@gmail.com>
2025-05-29 11:21:20 -0700

    Revert "[HLSL][RootSignature] Implement serialization of `RootConstants` and `RootFlags`" (#142005)
    The commit caused build failures,
    [here](https://lab.llvm.org/buildbot/#/builders/10/builds/6308), due to
    a missing linked llvm library (HLSLFrontend) into
    `clang/unittests/Parse/CMakeLists.txt`.
    
    While it seems like the fix is straightforwardly to just add this
    library, I will revert now to build and verify locally it correctly
    fixes it.
    
    Reverts llvm/llvm-project#141130

--- llvm/include/llvm/Frontend/HLSL/HLSLRootSignature.h
+++ llvm/include/llvm/Frontend/HLSL/HLSLRootSignature.h
@@ -46,8 +46,6 @@ enum class RootFlags : uint32_t {
   ValidFlags = 0x00000fff
 };
 
-raw_ostream &operator<<(raw_ostream &OS, const RootFlags &Flags);
-
 enum class RootDescriptorFlags : unsigned {
   None = 0,
   DataVolatile = 0x2,
@@ -94,8 +92,6 @@ struct RootConstants {
   uint32_t Space = 0;
   ShaderVisibility Visibility = ShaderVisibility::All;
 };
-
-raw_ostream &operator<<(raw_ostream &OS, const RootConstants &Constants);
 
 enum class DescriptorType : uint8_t { SRV = 0, UAV, CBuffer };
 // Models RootDescriptor : CBV | SRV | UAV, by collecting like parameters
