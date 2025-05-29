edbbc5c2609387edfdb7f30393e5ad81028d66ca
Teresa Johnson <tejohnson@google.com>
2025-05-29 11:51:53 -0700

    [MemProf] Summary section cleanup (NFC) (#142003)
    Address post-commit review comments from PR141805. Misc cleanup but the
    biggest changes are moving some common utilities to new MemProfCommon
    files to reduce unnecessary includes.

--- llvm/include/llvm/ProfileData/MemProfSummary.h
+++ llvm/include/llvm/ProfileData/MemProfSummary.h
@@ -6,28 +6,17 @@
 //
 //===----------------------------------------------------------------------===//
 //
-// This file contains MemProf summary support and related interfaces.
+// This file contains MemProf summary support.
 //
 //===----------------------------------------------------------------------===//
 
 #ifndef LLVM_PROFILEDATA_MEMPROFSUMMARY_H
 #define LLVM_PROFILEDATA_MEMPROFSUMMARY_H
 
-#include "llvm/IR/ModuleSummaryIndex.h"
 #include "llvm/ProfileData/InstrProf.h"
-#include "llvm/ProfileData/MemProf.h"
 
 namespace llvm {
 namespace memprof {
-
-/// Return the allocation type for a given set of memory profile values.
-AllocationType getAllocType(uint64_t TotalLifetimeAccessDensity,
-                            uint64_t AllocCount, uint64_t TotalLifetime);
-
-/// Helper to generate a single hash id for a given callstack, used for emitting
-/// matching statistics and useful for uniquing such statistics across modules.
-/// Also used to dedup contexts when computing the summary.
-uint64_t computeFullStackId(ArrayRef<Frame> CallStack);
 
 class MemProfSummary {
 private:
