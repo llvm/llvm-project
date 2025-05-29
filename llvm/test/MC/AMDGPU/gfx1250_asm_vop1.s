1262f9d4769809aad9a0f9e2b3f19e9d11de2430
Tian, Shilei <Shilei.Tian@amd.com>
2025-05-29 14:26:31 -0400

    [AMDGPU] Enable t16 for `v_log_bf16` (#2385)

--- llvm/test/MC/AMDGPU/gfx1250_asm_vop1.s
+++ llvm/test/MC/AMDGPU/gfx1250_asm_vop1.s
@@ -403,6 +403,9 @@ v_log_bf16 v5, src_scc
 v_log_bf16 v127, 0x8000
 // GFX1250: v_log_bf16_e32 v127, 0x8000             ; encoding: [0xff,0xf8,0xfe,0x7e,0x00,0x80,0x00,0x00]
 
+v_log_bf16 v5.h, v1.h
+// GFX1250: v_log_bf16_e32 v5.h, v1.h               ; encoding: [0x81,0xf9,0x0a,0x7f]
+
 v_exp_bf16 v5, v1
 // GFX1250: v_exp_bf16_e32 v5, v1                   ; encoding: [0x01,0xfb,0x0a,0x7e]
 
