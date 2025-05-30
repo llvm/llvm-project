4f70d9fba7659491c3633e9dacd9b148bde5f7fe
Denis Mikhailov <denismikhaylov38@gmail.com>
2025-05-30 17:12:43 +0300

    WIP

--- clang-tools-extra/clang-tidy/performance/BoolBitwiseOperationCheck.cpp
+++ clang-tools-extra/clang-tidy/performance/BoolBitwiseOperationCheck.cpp
@@ -14,6 +14,19 @@ using namespace clang::ast_matchers;
 
 namespace clang::tidy::performance {
 namespace {
+bool isFullyInsideMacro(const BinaryOperator *BO, const SourceManager &SM) {
+  SourceLocation Begin = BO->getBeginLoc();
+  SourceLocation End = BO->getEndLoc();
+  
+  // Если хотя бы одна часть оператора не в макросе — считаем его "не макросным"
+  if (!Begin.isMacroID() || !End.isMacroID()) {
+      return false;
+  }
+  
+  // Проверяем, что начало и конец находятся в одном макросе
+  return SM.getImmediateMacroCallerLoc(Begin) == SM.getImmediateMacroCallerLoc(End);
+}
+
 std::string getSpellingOpcode(const BinaryOperator &Expr, const SourceManager &SM,
                                                           const clang::LangOptions &LO) {
   SourceLocation Loc = Expr.getOperatorLoc();
@@ -55,9 +68,9 @@ void BoolBitwiseOperationCheck::registerMatchers(MatchFinder *Finder) {
             expr(ignoringImpCasts(hasType(isVolatileQualified())))
                 .bind("vol"))),
         optionally(hasAncestor(
-            binaryOperator().bind("p"))), // TODO: check operator name later
+            binaryOperator().bind("p"))),
         optionally(hasRHS(ignoringParenCasts(
-            binaryOperator().bind("r")))), // TODO: check operator name later
+            binaryOperator().bind("r")))),
         optionally(hasLHS(ignoringParenCasts(
             declRefExpr().bind("l")
         )))
@@ -65,7 +78,6 @@ void BoolBitwiseOperationCheck::registerMatchers(MatchFinder *Finder) {
     .bind("op"), this);
 }
 
-// TODO: test for nontraditional xor
 void BoolBitwiseOperationCheck::check(const MatchFinder::MatchResult &Result) {
   const auto *MatchedExpr = Result.Nodes.getNodeAs<BinaryOperator>("op");
   const SourceManager &SM = *Result.SourceManager;
@@ -74,19 +86,20 @@ void BoolBitwiseOperationCheck::check(const MatchFinder::MatchResult &Result) {
   auto Diag = diag(MatchedExpr->getOperatorLoc(), "use logical operator instead of bitwise one for bool");
 
   const auto *VolatileOperand = Result.Nodes.getNodeAs<Expr>("vol");
-  if (VolatileOperand)
+  if (VolatileOperand || isFullyInsideMacro(MatchedExpr, SM))
     return;
 
   SourceLocation Loc = MatchedExpr->getOperatorLoc();
   if (Loc.isValid()) {
     // TODO: bear it in mind
-    // SourceLocation expansionLoc = Result.SourceManager->getExpansionLoc(Loc);
-    // if (expansionLoc.isValid()) {
-      Loc = SM.getSpellingLoc(Loc);
-      if (Loc.isValid() && !Loc.isMacroID()) {
-        const CharSourceRange TokenRange = CharSourceRange::getTokenRange(Loc);
+    //SourceLocation expansionLoc = Result.SourceManager->getExpansionLoc(Loc);
+    SourceLocation expansionLoc = Loc;
+    if (expansionLoc.isValid()) {
+      expansionLoc = SM.getFileLoc(expansionLoc);
+      if (expansionLoc.isValid() && !expansionLoc.isMacroID()) {
+        const CharSourceRange TokenRange = CharSourceRange::getTokenRange(expansionLoc);
         if (TokenRange.isValid()) {
-          const StringRef SpellingOpc = Lexer::getSourceText(TokenRange, SM, LO);
+          const std::string SpellingOpc = getSpellingOpcode(*MatchedExpr, SM, LO);
           if (SpellingOpc == "&=" || SpellingOpc == "|=" || SpellingOpc == "and_eq" || SpellingOpc == "or_eq") {
             const auto *DelcRefLHS = Result.Nodes.getNodeAs<DeclRefExpr>("l");
             if (!DelcRefLHS)
@@ -103,17 +116,16 @@ void BoolBitwiseOperationCheck::check(const MatchFinder::MatchResult &Result) {
           Diag << FixItHint::CreateReplacement(TokenRange, changeOpcode(SpellingOpc));
           const auto *Parent = Result.Nodes.getNodeAs<BinaryOperator>("p");
           const auto *RHS = Result.Nodes.getNodeAs<BinaryOperator>("r");
-          // TODO: here must be not spelling ops
           const std::string ParentSpellingOpc = Parent ? getSpellingOpcode(*Parent, SM, LO) : "";
           const std::string RightSpellingOpc = RHS ? getSpellingOpcode(*RHS, SM, LO) : "";
-          if ((SpellingOpc == "|" && ParentSpellingOpc == "&&") ||
-              (SpellingOpc == "&" && ParentSpellingOpc == "^")) {
+          if (((SpellingOpc == "|" || SpellingOpc == "bitor") && (ParentSpellingOpc == "&&" || ParentSpellingOpc == "and")) ||
+              ((SpellingOpc == "&" || SpellingOpc == "bitand") && (ParentSpellingOpc == "^" || ParentSpellingOpc == "xor"))) {
             const clang::SourceLocation StartLoc = MatchedExpr->getBeginLoc();
             const clang::SourceLocation EndLoc = // TODO: check for valid
                 clang::Lexer::getLocForEndOfToken(MatchedExpr->getEndLoc(), 0, SM, LO);
             Diag << FixItHint::CreateInsertion(StartLoc, "(")
                  << FixItHint::CreateInsertion(EndLoc, ")");
-          } else if (SpellingOpc == "&=" && RightSpellingOpc == "||") {
+          } else if ((SpellingOpc == "&=" || SpellingOpc == "and_eq") && (RightSpellingOpc == "||" || RightSpellingOpc == "or")) {
             const clang::SourceLocation StartLoc = RHS->getBeginLoc();
             const clang::SourceLocation EndLoc = // TODO: check for valid
                 clang::Lexer::getLocForEndOfToken(RHS->getEndLoc(), 0, SM, LO);
@@ -122,7 +134,7 @@ void BoolBitwiseOperationCheck::check(const MatchFinder::MatchResult &Result) {
           }
         }
       }
-    // }
+    }
   }
 }
 
--- clang-tools-extra/test/clang-tidy/checkers/performance/bool-bitwise-operation.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/performance/bool-bitwise-operation.cpp
@@ -120,6 +120,55 @@ void bad_with_priors_compound2() {
     // TODO: ^ and |
 }
 
+void bad_with_priors_nontraditional() {
+    bool a = false, b = true, c = true;
+    a and b bitor c;
+    // CHECK-MESSAGES: :[[@LINE-1]]:13: warning: use logical operator instead of bitwise one for bool [performance-bool-bitwise-operation]
+    // CHECK-FIXES: a and (b or c);
+    a and b bitand c;
+    // CHECK-MESSAGES: :[[@LINE-1]]:13: warning: use logical operator instead of bitwise one for bool [performance-bool-bitwise-operation]
+    // CHECK-FIXES: a and b and c;
+    a or b bitand c;
+    // CHECK-MESSAGES: :[[@LINE-1]]:12: warning: use logical operator instead of bitwise one for bool [performance-bool-bitwise-operation]
+    // CHECK-FIXES: a or b and c;
+    a or b bitor c;
+    // CHECK-MESSAGES: :[[@LINE-1]]:12: warning: use logical operator instead of bitwise one for bool [performance-bool-bitwise-operation]
+    // CHECK-FIXES: a or b or c;
+    b bitor c and a;
+    // CHECK-MESSAGES: :[[@LINE-1]]:7: warning: use logical operator instead of bitwise one for bool [performance-bool-bitwise-operation]
+    // CHECK-FIXES: (b or c) and a;
+}
+
+void bad_with_priors2_nontraditional() {
+    bool a = false, b = true, c = true;
+    a xor b bitand c;
+    // CHECK-MESSAGES: :[[@LINE-1]]:13: warning: use logical operator instead of bitwise one for bool [performance-bool-bitwise-operation]
+    // CHECK-FIXES: a xor (b and c);
+    a bitor b bitand c;
+    // CHECK-MESSAGES: :[[@LINE-1]]:7: warning: use logical operator instead of bitwise one for bool [performance-bool-bitwise-operation]
+    // CHECK-MESSAGES: :[[@LINE-2]]:15: warning: use logical operator instead of bitwise one for bool [performance-bool-bitwise-operation]
+    // CHECK-FIXES: a or b and c;
+    b bitand c xor a;
+    // CHECK-MESSAGES: :[[@LINE-1]]:7: warning: use logical operator instead of bitwise one for bool [performance-bool-bitwise-operation]
+    // CHECK-FIXES: (b and c) xor a;
+}
+
+void bad_with_priors_compound_nontraditional() {
+    bool a = false, b = true, c = true;
+    a and_eq b or c;
+    // CHECK-MESSAGES: :[[@LINE-1]]:7: warning: use logical operator instead of bitwise one for bool [performance-bool-bitwise-operation]
+    // CHECK-FIXES: a = a and (b or c);
+    a or_eq b or c;
+    // CHECK-MESSAGES: :[[@LINE-1]]:7: warning: use logical operator instead of bitwise one for bool [performance-bool-bitwise-operation]
+    // CHECK-FIXES: a = a or b or c;
+    a and_eq b and c;
+    // CHECK-MESSAGES: :[[@LINE-1]]:7: warning: use logical operator instead of bitwise one for bool [performance-bool-bitwise-operation]
+    // CHECK-FIXES: a = a and b and c;
+    a or_eq b and c;
+    // CHECK-MESSAGES: :[[@LINE-1]]:7: warning: use logical operator instead of bitwise one for bool [performance-bool-bitwise-operation]
+    // CHECK-FIXES: a = a or b and c;
+}
+
 void bad_no_fixit() {
     bool b = false;
     normal() |= b;
@@ -128,6 +177,62 @@ void bad_no_fixit() {
     normal() &= b;
     // CHECK-MESSAGES: :[[@LINE-1]]:14: warning: use logical operator instead of bitwise one for bool [performance-bool-bitwise-operation]
     // CHECK-MESSAGES-NOT: :[[@LINE-2]]:{{.*}}: note: FIX-IT applied suggested code changes
+}
+
+// TODO: nontraditional
+#define MY_OR |
+#define MY_AND &
+#define MY_OR_ASSIGN |=
+#define MY_AND_ASSIGN &=
+
+#define MY_OR_FUNC(a, b) ((a) | (b))
+#define MY_AND_FUNC(a, b) ((a) & (b))
+#define MY_OR_ASSIGN_FUNC(a, b) ((a) |= (b))
+#define MY_AND_ASSIGN_FUNC(a, b) ((a) &= (b))
+
+#define IDENT(a) (a)
+
+// TODO: check that braces will not be settled inside the macro
+// TODO: check that =a will not be sellted inside the macro(for both cases)
+
+void bad_in_macro() {
+    bool a = true, b = false;
+
+    // TODO: same for braces as in math check
+
+    a MY_OR b;
+    // CHECK-MESSAGES: :[[@LINE-1]]:7: warning: use logical operator instead of bitwise one for bool [performance-bool-bitwise-operation]
+    // CHECK-FIXES: a || b;
+    a MY_AND b;
+    // CHECK-MESSAGES: :[[@LINE-1]]:7: warning: use logical operator instead of bitwise one for bool [performance-bool-bitwise-operation]
+    // CHECK-FIXES: a && b;
+    a MY_OR_ASSIGN b;
+    // CHECK-MESSAGES: :[[@LINE-1]]:7: warning: use logical operator instead of bitwise one for bool [performance-bool-bitwise-operation]
+    // CHECK-FIXES: a = a || b;
+    a MY_AND_ASSIGN b;
+    // CHECK-MESSAGES: :[[@LINE-1]]:7: warning: use logical operator instead of bitwise one for bool [performance-bool-bitwise-operation]
+    // CHECK-FIXES: a = a && b;
+
+    // TODO: same but for partial(with hints and with no hints)
+    MY_OR_FUNC(a, b);
+    // CHECK-MESSAGES: :[[@LINE-1]]:5: warning: use logical operator instead of bitwise one for bool [performance-bool-bitwise-operation]
+    // CHECK-MESSAGES-NOT: :[[@LINE-2]]:{{.*}}: note: FIX-IT applied suggested code changes
+    MY_AND_FUNC(a, b);
+    // CHECK-MESSAGES: :[[@LINE-1]]:5: warning: use logical operator instead of bitwise one for bool [performance-bool-bitwise-operation]
+    // CHECK-MESSAGES-NOT: :[[@LINE-2]]:{{.*}}: note: FIX-IT applied suggested code changes
+    MY_OR_ASSIGN_FUNC(a, b);
+    // CHECK-MESSAGES: :[[@LINE-1]]:5: warning: use logical operator instead of bitwise one for bool [performance-bool-bitwise-operation]
+    // CHECK-MESSAGES-NOT: :[[@LINE-2]]:{{.*}}: note: FIX-IT applied suggested code changes
+    MY_AND_ASSIGN_FUNC(a, b);
+    // CHECK-MESSAGES: :[[@LINE-1]]:5: warning: use logical operator instead of bitwise one for bool [performance-bool-bitwise-operation]
+    // CHECK-MESSAGES-NOT: :[[@LINE-2]]:{{.*}}: note: FIX-IT applied suggested code changes
+
+    IDENT(a | b);
+    // CHECK-MESSAGES: :[[@LINE-1]]:13: warning: use logical operator instead of bitwise one for bool [performance-bool-bitwise-operation]
+    // CHECK-FIXES: IDENT(a || b);
+    IDENT(a & b);
+    // CHECK-MESSAGES: :[[@LINE-1]]:13: warning: use logical operator instead of bitwise one for bool [performance-bool-bitwise-operation]
+    // CHECK-FIXES: IDENT(a && b);
 }
 
 // TODO: all the same tests as in math-parentheses check
