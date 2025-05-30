acf83b39e60f4257356c417fa0697286e60e8c5b
Sriya Pratipati <sriyap@google.com>
2025-05-30 18:19:54 +0000

    Changed check for CopyZeroCharacters

--- libc/test/src/wchar/wmemcpy_test.cpp
+++ libc/test/src/wchar/wmemcpy_test.cpp
@@ -50,12 +50,12 @@ TEST(LlvmLibcWMemcpyTest, CopyPartialString) {
 
 TEST(LlvmLibcWMemcpyTest, CopyZeroCharacters) {
   // Copying 0 characters should not change the string
-  wchar_t dest[10] = {};
+  wchar_t dest[10] = {L'1', L'2', L'3', L'4', L'5', L'\0'};
   const wchar_t *src = L"abcde";
   LIBC_NAMESPACE::wmemcpy(dest, src, 0);
-  ASSERT_TRUE(src[0] != dest[0]);
-  ASSERT_TRUE(src[1] != dest[1]);
-  ASSERT_TRUE(src[2] != dest[2]);
-  ASSERT_TRUE(src[3] != dest[3]);
-  ASSERT_TRUE(src[4] != dest[4]);
+  ASSERT_TRUE(L'1' == dest[0]);
+  ASSERT_TRUE(L'2' == dest[1]);
+  ASSERT_TRUE(L'3' == dest[2]);
+  ASSERT_TRUE(L'4' == dest[3]);
+  ASSERT_TRUE(L'5' == dest[4]);
 }
