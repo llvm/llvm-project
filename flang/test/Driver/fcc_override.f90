f41f8ffe3d537291fd870fb649f4d4b31361c262
Abid Qadeer <haqadeer@amd.com>
2025-05-30 16:09:19 +0100

    Replace -target with --target= to address review comments.

--- flang/test/Driver/fcc_override.f90
+++ flang/test/Driver/fcc_override.f90
@@ -1,4 +1,4 @@
-! RUN: env FCC_OVERRIDE_OPTIONS="#+-Os +-Oz +-O +-O3 +-Oignore +a +b +c xb Xa Omagic ^-###  " %flang -target x86_64-unknown-linux-gnu %s -O2 b -O3 2>&1 | FileCheck %s
+! RUN: env FCC_OVERRIDE_OPTIONS="#+-Os +-Oz +-O +-O3 +-Oignore +a +b +c xb Xa Omagic ^-###  " %flang --target=x86_64-unknown-linux-gnu %s -O2 b -O3 2>&1 | FileCheck %s
 ! RUN: env FCC_OVERRIDE_OPTIONS="x-Werror +-g" %flang --target=x86_64-unknown-linux-gnu -Werror %s -c -### 2>&1 | FileCheck %s -check-prefix=RM-WERROR
 ! RUN: env FCC_OVERRIDE_OPTIONS="x-Werror" %flang --config=%S/Inputs/config-7.cfg -### %s -c  2>&1 | FileCheck %s -check-prefix=CONF
 
