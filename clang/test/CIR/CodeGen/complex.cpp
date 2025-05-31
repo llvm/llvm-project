8d4217eec4e41954bd7512c494d135701b79903b
AmrDeveloper <amr96@programmer.net>
2025-05-31 12:05:42 +0200

    Update ComplexType testing to not check for dso_local

--- clang/test/CIR/CodeGen/complex.cpp
+++ clang/test/CIR/CodeGen/complex.cpp
@@ -18,10 +18,10 @@ float _Complex cf2 = { 1.0f, 2.0f };
 // CIR: cir.global external @ci2 = #cir.const_complex<#cir.int<1> : !s32i, #cir.int<2> : !s32i> : !cir.complex<!s32i>
 // CIR: cir.global external @cf2 = #cir.const_complex<#cir.fp<1.000000e+00> : !cir.float, #cir.fp<2.000000e+00> : !cir.float> : !cir.complex<!cir.float>
 
-// LLVM: {{.*}} = dso_local global { i32, i32 } zeroinitializer, align 4
-// LLVM: {{.*}} = dso_local global { float, float } zeroinitializer, align 4
-// LLVM: {{.*}} = dso_local global { i32, i32 } { i32 1, i32 2 }, align 4
-// LLVM: {{.*}} = dso_local global { float, float } { float 1.000000e+00, float 2.000000e+00 }, align 4
+// LLVM: {{.*}} = global { i32, i32 } zeroinitializer, align 4
+// LLVM: {{.*}} = global { float, float } zeroinitializer, align 4
+// LLVM: {{.*}} = global { i32, i32 } { i32 1, i32 2 }, align 4
+// LLVM: {{.*}} = global { float, float } { float 1.000000e+00, float 2.000000e+00 }, align 4
 
 // OGCG: {{.*}} = global { i32, i32 } zeroinitializer, align 4
 // OGCG: {{.*}} = global { float, float } zeroinitializer, align 4
