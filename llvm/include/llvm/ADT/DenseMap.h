llvm/llvm-project

commit 4bf67cdf026478c4bc3e708153c02f82e2c22524

[DenseMap] Fix constness issues with lookup_or (#139247)
Also demonstrate its use in ScalarEvolution.

llvm/include/llvm/ADT/DenseMap.h
@@ -220,8 +220,7 @@ public:
   // Return the entry with the specified key, or \p Default. This variant is
   // useful, because `lookup` cannot be used with non-default-constructible
   // values.
-  ValueT lookup_or(const_arg_type_t<KeyT> Val,
-                   const_arg_type_t<ValueT> Default) const {
+  ValueT lookup_or(const_arg_type_t<KeyT> Val, ValueT &&Default) const {
     if (const BucketT *Bucket = doFind(Val))
       return Bucket->getSecond();
     return Default;

llvm/lib/Analysis/ScalarEvolution.cpp
@@ -15904,10 +15904,7 @@ const SCEV *ScalarEvolution::LoopGuards::rewrite(const SCEV *Expr) const {
     const SCEV *visitAddRecExpr(const SCEVAddRecExpr *Expr) { return Expr; }
 
     const SCEV *visitUnknown(const SCEVUnknown *Expr) {
-      auto I = Map.find(Expr);
-      if (I == Map.end())
-        return Expr;
-      return I->second;
+      return Map.lookup_or(Expr, Expr);
     }
 
     const SCEV *visitZeroExtendExpr(const SCEVZeroExtendExpr *Expr) {

llvm/unittests/ADT/DenseMapTest.cpp
@@ -671,6 +671,13 @@ TEST(DenseMapCustomTest, LookupOr) {
   EXPECT_EQ(M.lookup_or(2, 4u), 4u);
 }
 
+TEST(DenseMapCustomTest, LookupOrConstness) {
+  DenseMap<int, unsigned *> M;
+  unsigned Default = 3u;
+  unsigned *Ret = M.lookup_or(0, &Default);
+  EXPECT_EQ(Ret, &Default);
+}
+
 // Key traits that allows lookup with either an unsigned or char* key;
 // In the latter case, "a" == 0, "b" == 1 and so on.
 struct TestDenseMapInfo {

commit 089fad5b9017afbfc64905ff4bbe5524c390d64f

[DenseMap] Work around a MSVC bug in lookup_or
4bf67cd ([DenseMap] Fix constness issues with lookup_or) introduced a
buildbot failure:

  https://lab.llvm.org/buildbot/#/builders/63/builds/6559

This seems to be a bug in MSVC, where it doesn't bind an lvalue to an
rvalue reference. Work around it by introducing a const-lvalue-reference
variant of lookup_or.

llvm/include/llvm/ADT/DenseMap.h
@@ -226,6 +226,12 @@ public:
     return Default;
   }
 
+  ValueT lookup_or(const_arg_type_t<KeyT> Val, const ValueT &Default) const {
+    if (const BucketT *Bucket = doFind(Val))
+      return Bucket->getSecond();
+    return Default;
+  }
+
   /// at - Return the entry for the specified key, or abort if no such
   /// entry exists.
   const ValueT &at(const_arg_type_t<KeyT> Val) const {