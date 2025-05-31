cee9a32671749067a781d97a501ca28ad7a83e2d
alyelashram <alyahelashram@gmail.com>
2025-06-01 00:06:14 +0300

    Update endif comment

--- libc/test/src/string/memcmp_test.cpp
+++ libc/test/src/string/memcmp_test.cpp
@@ -73,6 +73,6 @@ TEST(LlvmLibcMemcmpTest, CrashOnNullPtr) {
                WITH_SIGNAL(-1));
 }
 
-#endif // defined(LIBC_TARGET_OS_IS_LINUX)
+#endif // defined(LIBC_ADD_NULL_CHECKS) && !defined(LIBC_HAS_SANITIZER)
 
 } // namespace LIBC_NAMESPACE_DECL
