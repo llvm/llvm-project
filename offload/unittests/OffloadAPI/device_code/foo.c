ba29f6964352ddb8eae8a0a9f3fbda1be8afe8c5
Ross Brunton <ross@codeplay.com>
2025-06-02 15:46:28 +0100

    Add test

--- offload/unittests/OffloadAPI/device_code/foo.c
+++ offload/unittests/OffloadAPI/device_code/foo.c
@@ -2,5 +2,11 @@
 #include <stdint.h>
 
 __gpu_kernel void foo(uint32_t *out) {
-  out[__gpu_thread_id(0)] = __gpu_thread_id(0);
+  int x = __gpu_block_id(0) * __gpu_num_threads(0) + __gpu_thread_id(0);
+  int xw = __gpu_num_blocks(0) * __gpu_num_threads(0);
+  int y = __gpu_block_id(1) * __gpu_num_threads(1) + __gpu_thread_id(1);
+  int yw = __gpu_num_blocks(1) * __gpu_num_threads(1);
+  int z = __gpu_block_id(2) * __gpu_num_threads(2) + __gpu_thread_id(2);
+  int offset = (z * yw * xw) + (y * xw) + x;
+  out[offset] = offset;
 }
