5dd74d5105e598ac66c93b647715653f61a7366f
Andrew Rogers <andrurogerz@gmail.com>
2025-06-02 09:37:57 -0700

    [llvm] annotate interfaces in FileCheck, FrontEnd, and FuzzMutate libraries for DLL export (#141864)
    ## Purpose
    
    This patch is one in a series of code-mods that annotate LLVM’s public
    interface for export. This patch annotates the `llvm/FileCheck`,
    `llvm/FrontEnd`, and `llvm/FuzzMutate libraries. These annotations
    currently have no meaningful impact on the LLVM build; however, they are
    a prerequisite to support an LLVM Windows DLL (shared library) build.
    
    ## Background
    
    This effort is tracked in #109483. Additional context is provided in
    [this
    discourse](https://discourse.llvm.org/t/psa-annotating-llvm-public-interface/85307),
    and documentation for `LLVM_ABI` and related annotations is found in the
    LLVM repo
    [here](https://github.com/llvm/llvm-project/blob/main/llvm/docs/InterfaceExportAnnotations.rst).
    
    The bulk of these changes were generated automatically using the
    [Interface Definition Scanner (IDS)](https://github.com/compnerd/ids)
    tool, followed formatting with `git clang-format`.
    
    The following manual adjustments were also applied after running IDS on
    Linux:
    - Add #include "llvm/Support/Compiler.h" where it was not auto-added by
    IDS due to no pre-existing block of include statements.
    
    ## Validation
    
    Local builds and tests to validate cross-platform compatibility. This
    included llvm, clang, and lldb on the following configurations:
    
    - Windows with MSVC
    - Windows with Clang
    - Linux with GCC
    - Linux with Clang
    - Darwin with Clang

--- llvm/include/llvm/Frontend/HLSL/HLSLRootSignature.h
+++ llvm/include/llvm/Frontend/HLSL/HLSLRootSignature.h
@@ -15,6 +15,7 @@
 #define LLVM_FRONTEND_HLSL_HLSLROOTSIGNATURE_H
 
 #include "llvm/ADT/ArrayRef.h"
+#include "llvm/Support/Compiler.h"
 #include "llvm/Support/DXILABI.h"
 #include "llvm/Support/raw_ostream.h"
 #include <variant>
@@ -150,7 +151,7 @@ struct DescriptorTable {
   uint32_t NumClauses = 0;
 };
 
-raw_ostream &operator<<(raw_ostream &OS, const DescriptorTable &Table);
+LLVM_ABI raw_ostream &operator<<(raw_ostream &OS, const DescriptorTable &Table);
 
 static const uint32_t NumDescriptorsUnbounded = 0xffffffff;
 static const uint32_t DescriptorTableOffsetAppend = 0xffffffff;
@@ -180,7 +181,8 @@ struct DescriptorTableClause {
   }
 };
 
-raw_ostream &operator<<(raw_ostream &OS, const DescriptorTableClause &Clause);
+LLVM_ABI raw_ostream &operator<<(raw_ostream &OS,
+                                 const DescriptorTableClause &Clause);
 
 struct StaticSampler {
   Register Reg;
@@ -217,7 +219,7 @@ using RootElement =
     std::variant<RootFlags, RootConstants, RootDescriptor, DescriptorTable,
                  DescriptorTableClause, StaticSampler>;
 
-void dumpRootElements(raw_ostream &OS, ArrayRef<RootElement> Elements);
+LLVM_ABI void dumpRootElements(raw_ostream &OS, ArrayRef<RootElement> Elements);
 
 class MetadataBuilder {
 public:
@@ -228,7 +230,7 @@ public:
   ///
   /// Accumulates the root signature and returns the Metadata node that is just
   /// a list of all the elements
-  MDNode *BuildRootSignature();
+  LLVM_ABI MDNode *BuildRootSignature();
 
 private:
   /// Define the various builders for the different metadata types
