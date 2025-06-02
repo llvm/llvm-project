29140030e33cc52e969bc61aeedba886a37f65ba
SharonXSharon <xiaoranxu.nju@gmail.com>
2025-06-02 10:40:33 -0700

    Update lld/Common/Utils.cpp
    Co-authored-by: Ellis Hoag <ellis.sparky.hoag@gmail.com>

--- lld/Common/Utils.cpp
+++ lld/Common/Utils.cpp
@@ -3,8 +3,10 @@
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//===----------------------------------------------------------------------===//
 //
-// The file defines untils functions that can be shared across archs.
+// The file defines utils functions that can be shared across archs.
+//
 //===----------------------------------------------------------------------===//
 
 #include "lld/Common/Utils.h"
