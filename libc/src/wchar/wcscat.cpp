530268ce420d2396d8c914e7e4742832eba71a7b
Sriya Pratipati <sriyap@google.com>
2025-06-02 19:14:27 +0000

    Cleaned up code.

--- libc/src/wchar/wcscat.cpp
+++ libc/src/wchar/wcscat.cpp
@@ -19,10 +19,11 @@ namespace LIBC_NAMESPACE_DECL {
 LLVM_LIBC_FUNCTION(wchar_t *, wcscat,
                    (wchar_t *__restrict s1, const wchar_t *__restrict s2)) {
   size_t size_1 = internal::string_length(s1);
-  size_t size_2 = internal::string_length(s2);
-  for (size_t i = 0; i <= size_2; i++) {
+  size_t i = 0;
+  for (; s2[i] != L'\0'; i++) {
     s1[size_1 + i] = s2[i];
   }
+  s1[size_1 + i] = L'\0';
   return s1;
 }
 
