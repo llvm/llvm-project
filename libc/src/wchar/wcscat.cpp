826e8bfb1f1b76def0ffb17c329bab63de048b92
Sriya Pratipati <sriyap@google.com>
2025-06-02 19:15:43 +0000

    Cleaned up code

--- libc/src/wchar/wcscat.cpp
+++ libc/src/wchar/wcscat.cpp
@@ -20,9 +20,8 @@ LLVM_LIBC_FUNCTION(wchar_t *, wcscat,
                    (wchar_t *__restrict s1, const wchar_t *__restrict s2)) {
   size_t size_1 = internal::string_length(s1);
   size_t i = 0;
-  for (; s2[i] != L'\0'; i++) {
+  for (; s2[i] != L'\0'; i++)
     s1[size_1 + i] = s2[i];
-  }
   s1[size_1 + i] = L'\0';
   return s1;
 }
