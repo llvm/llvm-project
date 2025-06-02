8005f2400e9df9ec192e3fa5b7d49833f58b5e84
Tikhomirova, Kseniya <kseniya.tikhomirova@intel.com>
2025-06-02 08:26:26 -0700

    update names
    Signed-off-by: Tikhomirova, Kseniya <kseniya.tikhomirova@intel.com>

--- libsycl/cmake/modules/FetchUnifiedRuntime.cmake
+++ libsycl/cmake/modules/FetchUnifiedRuntime.cmake
@@ -10,44 +10,44 @@
 
 # Options to override the default behaviour of the FetchContent to include UR
 # source code.
-set(SYCL_UR_OVERRIDE_FETCH_CONTENT_REPO
+set(LIBSYCL_UR_OVERRIDE_FETCH_CONTENT_REPO
   "" CACHE STRING "Override the Unified Runtime FetchContent repository")
-set(SYCL_UR_OVERRIDE_FETCH_CONTENT_TAG
+set(LIBSYCL_UR_OVERRIDE_FETCH_CONTENT_TAG
   "" CACHE STRING "Override the Unified Runtime FetchContent tag")
 
 # Options to disable use of FetchContent to include Unified Runtime source code
 # to improve developer workflow.
-option(SYCL_UR_USE_FETCH_CONTENT
+option(LIBSYCL_UR_USE_FETCH_CONTENT
   "Use FetchContent to acquire the Unified Runtime source code" ON)
-set(SYCL_UR_SOURCE_DIR
+set(LIBSYCL_UR_SOURCE_DIR
   "" CACHE PATH "Path to root of Unified Runtime repository")
 
-option(SYCL_UR_BUILD_TESTS "Build tests for UR" OFF)
-set(UR_BUILD_TESTS "${SYCL_UR_BUILD_TESTS}" CACHE BOOL "" FORCE)
+option(LIBSYCL_UR_BUILD_TESTS "Build tests for UR" OFF)
+set(UR_BUILD_TESTS "${LIBSYCL_UR_BUILD_TESTS}" CACHE BOOL "" FORCE)
 # UR tests require the examples to be built
-set(UR_BUILD_EXAMPLES "${SYCL_UR_BUILD_TESTS}" CACHE BOOL "" FORCE)
+set(UR_BUILD_EXAMPLES "${LIBSYCL_UR_BUILD_TESTS}" CACHE BOOL "" FORCE)
 
 set(UR_EXTERNAL_DEPENDENCIES "sycl-headers" CACHE STRING
   "List of external CMake targets for executables/libraries to depend on" FORCE)
 
-if("level_zero" IN_LIST SYCL_ENABLE_BACKENDS)
+if("level_zero" IN_LIST LIBSYCL_ENABLE_BACKENDS)
   set(UR_BUILD_ADAPTER_L0 ON)
 endif()
-if("level_zero_v2" IN_LIST SYCL_ENABLE_BACKENDS)
+if("level_zero_v2" IN_LIST LIBSYCL_ENABLE_BACKENDS)
   set(UR_BUILD_ADAPTER_L0_V2 ON)
 endif()
-if("cuda" IN_LIST SYCL_ENABLE_BACKENDS)
+if("cuda" IN_LIST LIBSYCL_ENABLE_BACKENDS)
   set(UR_BUILD_ADAPTER_CUDA ON)
 endif()
-if("hip" IN_LIST SYCL_ENABLE_BACKENDS)
+if("hip" IN_LIST LIBSYCL_ENABLE_BACKENDS)
   set(UR_BUILD_ADAPTER_HIP ON)
 endif()
-if("opencl" IN_LIST SYCL_ENABLE_BACKENDS)
+if("opencl" IN_LIST LIBSYCL_ENABLE_BACKENDS)
   set(UR_BUILD_ADAPTER_OPENCL ON)
   set(UR_OPENCL_ICD_LOADER_LIBRARY OpenCL-ICD CACHE FILEPATH
     "Path of the OpenCL ICD Loader library" FORCE)
 endif()
-if("native_cpu" IN_LIST SYCL_ENABLE_BACKENDS)
+if("native_cpu" IN_LIST LIBSYCL_ENABLE_BACKENDS)
   set(UR_BUILD_ADAPTER_NATIVE_CPU ON)
 endif()
 
@@ -65,7 +65,7 @@ else()
   set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wno-error")
 endif()
 
-if(SYCL_UR_USE_FETCH_CONTENT)
+if(LIBSYCL_UR_USE_FETCH_CONTENT)
   include(FetchContent)
 
   # The fetch_adapter_source function can be used to perform a separate content
@@ -73,7 +73,7 @@ if(SYCL_UR_USE_FETCH_CONTENT)
   # from each other.
   #
   # A separate content fetch will not be performed if:
-  # * The adapter name is not present in the SYCL_ENABLE_BACKENDS variable.
+  # * The adapter name is not present in the LIBSYCL_ENABLE_BACKENDS variable.
   # * The repo and tag provided match the values of the
   #   UNIFIED_RUNTIME_REPO/UNIFIED_RUNTIME_TAG variables
   #
@@ -82,7 +82,7 @@ if(SYCL_UR_USE_FETCH_CONTENT)
   #   * repo - A valid Git URL of a Unified Runtime repo
   #   * tag - A valid Git branch/tag/commit in the Unified Runtime repo
   function(fetch_adapter_source name repo tag)
-    if(NOT ${name} IN_LIST SYCL_ENABLE_BACKENDS)
+    if(NOT ${name} IN_LIST LIBSYCL_ENABLE_BACKENDS)
       return()
     endif()
     if(repo STREQUAL UNIFIED_RUNTIME_REPO AND
@@ -147,11 +147,11 @@ if(SYCL_UR_USE_FETCH_CONTENT)
     ${UNIFIED_RUNTIME_TAG}
   )
 
-  if(SYCL_UR_OVERRIDE_FETCH_CONTENT_REPO)
-    set(UNIFIED_RUNTIME_REPO "${SYCL_UR_OVERRIDE_FETCH_CONTENT_REPO}")
+  if(LIBSYCL_UR_OVERRIDE_FETCH_CONTENT_REPO)
+    set(UNIFIED_RUNTIME_REPO "${LIBSYCL_UR_OVERRIDE_FETCH_CONTENT_REPO}")
   endif()
-  if(SYCL_UR_OVERRIDE_FETCH_CONTENT_TAG)
-    set(UNIFIED_RUNTIME_TAG "${SYCL_UR_OVERRIDE_FETCH_CONTENT_TAG}")
+  if(LIBSYCL_UR_OVERRIDE_FETCH_CONTENT_TAG)
+    set(UNIFIED_RUNTIME_TAG "${LIBSYCL_UR_OVERRIDE_FETCH_CONTENT_TAG}")
   endif()
 
   message(STATUS "Will fetch Unified Runtime from ${UNIFIED_RUNTIME_REPO}")
@@ -166,21 +166,21 @@ if(SYCL_UR_USE_FETCH_CONTENT)
   set(UNIFIED_RUNTIME_SOURCE_DIR
     "${unified-runtime_SOURCE_DIR}" CACHE PATH
     "Path to Unified Runtime Headers" FORCE)
-elseif(SYCL_UR_SOURCE_DIR)
-  # SYCL_UR_USE_FETCH_CONTENT is OFF and SYCL_UR_SOURCE_DIR has been set,
+elseif(LIBSYCL_UR_SOURCE_DIR)
+  # LIBSYCL_UR_USE_FETCH_CONTENT is OFF and LIBSYCL_UR_SOURCE_DIR has been set,
   # use the external Unified Runtime source directory.
   set(UNIFIED_RUNTIME_SOURCE_DIR
-    "${SYCL_UR_SOURCE_DIR}" CACHE PATH
+    "${LIBSYCL_UR_SOURCE_DIR}" CACHE PATH
     "Path to Unified Runtime Headers" FORCE)
   add_subdirectory(
     ${UNIFIED_RUNTIME_SOURCE_DIR}
     ${CMAKE_CURRENT_BINARY_DIR}/unified-runtime)
 else()
   message(FATAL_ERROR
-    "SYCL_UR_USE_FETCH_CONTENT is disabled but no alternative Unified \
+    "LIBSYCL_UR_USE_FETCH_CONTENT is disabled but no alternative Unified \
     Runtime source directory has been provided, either:
 
-    * Set -DSYCL_UR_SOURCE_DIR=/path/to/unified-runtime
+    * Set -DLIBSYCL_UR_SOURCE_DIR=/path/to/unified-runtime
     * Clone the UR repo in ${CMAKE_CURRENT_SOURCE_DIR}/unified-runtime")
 endif()
 
@@ -237,32 +237,29 @@ function(add_sycl_ur_adapter NAME)
   add_custom_target(install-sycl-ur-adapter-${NAME}
     DEPENDS ${manifest_file} ur_adapter_${NAME}
   )
-
-  set_property(GLOBAL APPEND PROPERTY
-    SYCL_TOOLCHAIN_INSTALL_COMPONENTS ur_adapter_${NAME})
 endfunction()
 
-if("level_zero" IN_LIST SYCL_ENABLE_BACKENDS)
+if("level_zero" IN_LIST LIBSYCL_ENABLE_BACKENDS)
   add_sycl_ur_adapter(level_zero)
 endif()
 
-if("level_zero_v2" IN_LIST SYCL_ENABLE_BACKENDS)
+if("level_zero_v2" IN_LIST LIBSYCL_ENABLE_BACKENDS)
   add_sycl_ur_adapter(level_zero_v2)
 endif()
 
-if("cuda" IN_LIST SYCL_ENABLE_BACKENDS)
+if("cuda" IN_LIST LIBSYCL_ENABLE_BACKENDS)
   add_sycl_ur_adapter(cuda)
 endif()
 
-if("hip" IN_LIST SYCL_ENABLE_BACKENDS)
+if("hip" IN_LIST LIBSYCL_ENABLE_BACKENDS)
   add_sycl_ur_adapter(hip)
 endif()
 
-if("opencl" IN_LIST SYCL_ENABLE_BACKENDS)
+if("opencl" IN_LIST LIBSYCL_ENABLE_BACKENDS)
   add_sycl_ur_adapter(opencl)
 endif()
 
-if("native_cpu" IN_LIST SYCL_ENABLE_BACKENDS)
+if("native_cpu" IN_LIST LIBSYCL_ENABLE_BACKENDS)
   add_sycl_ur_adapter(native_cpu)
 
   # Deal with OCK option
@@ -274,113 +271,6 @@ if("native_cpu" IN_LIST SYCL_ENABLE_BACKENDS)
   else()
     message(WARNING "Compiling Native CPU adapter without OCK support.
     Some valid SYCL programs may not build or may have low performance.")
-  endif()
-endif()
-
-if(CMAKE_SYSTEM_NAME STREQUAL Windows)
-  # On Windows, also build/install debug libraries with the d suffix that are
-  # compiled with /MDd so users can link against these in debug builds.
-  include(ExternalProject)
-  set(URD_BINARY_DIR ${CMAKE_BINARY_DIR}/unified-runtimed)
-  set(URD_INSTALL_DIR ${URD_BINARY_DIR}/install)
-
-  # This creates a subbuild which can be used in dependencies with the
-  # unified-runtimed target. It invokes the install-unified-runtime-libraries
-  # target to install the UR runtime libraries.
-  ExternalProject_Add(unified-runtimed
-    SOURCE_DIR ${UNIFIED_RUNTIME_SOURCE_DIR}
-    BINARY_DIR ${URD_BINARY_DIR}
-    INSTALL_DIR ${URD_INSTALL_DIR}
-    INSTALL_COMMAND ${CMAKE_COMMAND}
-      --build <BINARY_DIR> --config Debug
-      --target install-unified-runtime-libraries
-    CMAKE_CACHE_ARGS
-      -DCMAKE_BUILD_TYPE:STRING=Debug
-      -DCMAKE_INSTALL_PREFIX:STRING=<INSTALL_DIR>
-      # Enable d suffix on libraries
-      -DUR_USE_DEBUG_POSTFIX:BOOL=ON
-      # Don't build unnecessary targets in subbuild.
-      -DUR_BUILD_EXAMPLES:BOOL=OFF
-      -DUR_BUILD_TESTS:BOOL=OFF
-      -DUR_BUILD_TOOLS:BOOL=OFF
-      # Sanitizer layer is not supported on Windows.
-      -DUR_ENABLE_SYMBOLIZER:BOOL=OFF
-      # Inherit settings from parent build.
-      -DUR_ENABLE_TRACING:BOOL=${UR_ENABLE_TRACING}
-      -DUR_ENABLE_COMGR:BOOL=${UR_ENABLE_COMGR}
-      -DUR_BUILD_ADAPTER_L0:BOOL=${UR_BUILD_ADAPTER_L0}
-      -DUR_BUILD_ADAPTER_L0_V2:BOOL=${UR_BUILD_ADAPTER_L0_V2}
-      -DUR_BUILD_ADAPTER_OPENCL:BOOL=${UR_BUILD_ADAPTER_OPENCL}
-      -DUR_BUILD_ADAPTER_CUDA:BOOL=${UR_BUILD_ADAPTER_CUDA}
-      -DUR_BUILD_ADAPTER_HIP:BOOL=${UR_BUILD_ADAPTER_HIP}
-      -DUR_BUILD_ADAPTER_NATIVE_CPU:BOOL=${UR_BUILD_ADAPTER_NATIVE_CPU}
-      -DUMF_BUILD_EXAMPLES:BOOL=${UMF_BUILD_EXAMPLES}
-      -DUMF_BUILD_SHARED_LIBRARY:BOOL=${UMF_BUILD_SHARED_LIBRARY}
-      -DUMF_LINK_HWLOC_STATICALLY:BOOL=${UMF_LINK_HWLOC_STATICALLY}
-      -DUMF_DISABLE_HWLOC:BOOL=${UMF_DISABLE_HWLOC}
-      # Enable d suffix in UMF
-      -DUMF_USE_DEBUG_POSTFIX:BOOL=ON
-  )
-
-  # Copy the debug UR runtime libraries to <build>/bin & <build>/lib for use in
-  # the parent build, e.g. integration testing.
-  set(URD_COPY_FILES)
-  macro(urd_copy_library_to_build library shared)
-    if(${shared})
-      list(APPEND URD_COPY_FILES
-        ${LLVM_BINARY_DIR}/bin/${library}.dll
-      )
-      add_custom_command(
-        OUTPUT
-          ${LLVM_BINARY_DIR}/bin/${library}.dll
-        COMMAND ${CMAKE_COMMAND} -E copy
-          ${URD_INSTALL_DIR}/bin/${library}.dll
-          ${LLVM_BINARY_DIR}/bin/${library}.dll
-      )
-    endif()
-
-    list(APPEND URD_COPY_FILES
-      ${LLVM_BINARY_DIR}/lib/${library}.lib
-    )
-    add_custom_command(
-      OUTPUT
-        ${LLVM_BINARY_DIR}/lib/${library}.lib
-      COMMAND ${CMAKE_COMMAND} -E copy
-        ${URD_INSTALL_DIR}/lib/${library}.lib
-        ${LLVM_BINARY_DIR}/lib/${library}.lib
-    )
-  endmacro()
-
-  urd_copy_library_to_build(ur_loaderd "NOT;${UR_STATIC_LOADER}")
-  foreach(adapter ${SYCL_ENABLE_BACKENDS})
-    if(adapter MATCHES "level_zero")
-      set(shared "NOT;${UR_STATIC_ADAPTER_L0}")
-    else()
-      set(shared TRUE)
-    endif()
-    urd_copy_library_to_build(ur_adapter_${adapter}d "${shared}")
-  endforeach()
-  # Also copy umfd.dll/umfd.lib
-  urd_copy_library_to_build(umfd ${UMF_BUILD_SHARED_LIBRARY})
-
-  add_custom_target(unified-runtimed-build ALL DEPENDS ${URD_COPY_FILES})
-  add_dependencies(unified-runtimed-build unified-runtimed)
-
-  # Add the debug UR runtime libraries to the parent install.
-  install(
-    FILES ${URD_INSTALL_DIR}/bin/ur_loaderd.dll
-    DESTINATION "bin" COMPONENT unified-runtime-loader)
-  foreach(adapter ${SYCL_ENABLE_BACKENDS})
-    install(
-      FILES ${URD_INSTALL_DIR}/bin/ur_adapter_${adapter}d.dll
-      DESTINATION "bin" COMPONENT ur_adapter_${adapter})
-    add_dependencies(install-sycl-ur-adapter-${adapter} unified-runtimed)
-  endforeach()
-  if(UMF_BUILD_SHARED_LIBRARY)
-    # Also install umfd.dll
-    install(
-      FILES ${URD_INSTALL_DIR}/bin/umfd.dll
-      DESTINATION "bin" COMPONENT unified-memory-framework)
   endif()
 endif()
 
