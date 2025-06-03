9ef23464d0f51278c5aecdea36edb6a71b65172f
Florian Hahn <flo@fhahn.com>
2025-06-03 10:38:19 +0100

    [SCEV] Fold zext(C+A)<nsw> -> (sext(C) + zext(A))<nsw> if possible.
    Simplify zext(C+A)<nsw> -> (sext(C) + zext(A))<nsw> if
     * zext (C + A)<nsw> >=s 0 and
     * A >=s V.
    
    For now this is limited to cases where the first operand is a constant,
    so the SExt can be folded to a new constant. This can be relaxed in the
    future.
    
    Alive2 proof of the general pattern and the test changes in zext-nuw.ll
    (times out in the online instance but verifies locally)
    
    https://alive2.llvm.org/ce/z/_BtyGy

--- llvm/lib/Analysis/ScalarEvolution.cpp
+++ llvm/lib/Analysis/ScalarEvolution.cpp
@@ -1795,6 +1795,18 @@ const SCEV *ScalarEvolution::getZeroExtendExprImpl(const SCEV *Op, Type *Ty,
       return getAddExpr(Ops, SCEV::FlagNUW, Depth + 1);
     }
 
+    const SCEVConstant *C;
+    const SCEV *A;
+    // zext (C + A)<nsw> -> (sext(C) + zext(A))<nsw> if zext (C + A)<nsw> >=s 0
+    // and A >=s V.
+    if (SA->hasNoSignedWrap() && isKnownNonNegative(SA) &&
+        match(SA, m_scev_Add(m_SCEVConstant(C), m_SCEV(A))) &&
+        isKnownPredicate(CmpInst::ICMP_SGE, A, C)) {
+      SmallVector<const SCEV *, 4> Ops = {getSignExtendExpr(C, Ty, Depth + 1),
+                                          getZeroExtendExpr(A, Ty, Depth + 1)};
+      return getAddExpr(Ops, SCEV::FlagNSW, Depth + 1);
+    }
+
     // zext(C + x + y + ...) --> (zext(D) + zext((C - D) + x + y + ...))
     // if D + (C - D + x + y + ...) could be proven to not unsigned wrap
     // where D maximizes the number of trailing zeros of (C - D + x + y + ...)
