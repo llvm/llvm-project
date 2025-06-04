ca657961ee07a4442420eb0d3780e9975a49596b
Kazu Hirata <kazu@google.com>
2025-06-04 15:34:53 -0700

    [Debuginfod] Restore an include
    A breakage has been reported in:
    
    https://github.com/llvm/llvm-project/issues/142852

--- llvm/lib/Debuginfod/HTTPServer.cpp
+++ llvm/lib/Debuginfod/HTTPServer.cpp
@@ -16,6 +16,7 @@
 #include "llvm/Debuginfod/HTTPServer.h"
 #include "llvm/ADT/StringExtras.h"
 #include "llvm/ADT/StringRef.h"
+#include "llvm/Support/Errc.h"
 #include "llvm/Support/Error.h"
 #include "llvm/Support/FileSystem.h"
 #include "llvm/Support/MemoryBuffer.h"
