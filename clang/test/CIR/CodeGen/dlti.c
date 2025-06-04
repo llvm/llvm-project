d56dadff8e5bde43f6256025d69c57861b2fc558
Andres Salamanca <andrealebarbaritos@gmail.com>
2025-06-04 14:11:52 -0500

    Add new test for big-endian format

--- clang/test/CIR/CodeGen/dlti.c
+++ clang/test/CIR/CodeGen/dlti.c
@@ -1,23 +1,23 @@
 // RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -emit-cir %s -o %t.cir
-// RUN: FileCheck --input-file=%t.cir %s
+// RUN: FileCheck --input-file=%t.cir %s --check-prefix=LITTLE
 
 void foo() {}
 
-//  CHECK-DAG: dlti.dl_spec =
-//  CHECK-DAG:   #dlti.dl_spec<
-//  CHECK-DAG:     i16 = dense<16> : vector<2xi64>,
-//  CHECK-DAG:     i32 = dense<32> : vector<2xi64>,
-//  CHECK-DAG:     i8 = dense<8> : vector<2xi64>,
-//  CHECK-DAG:     i1 = dense<8> : vector<2xi64>,
-//  CHECK-DAG:     !llvm.ptr = dense<64> : vector<4xi64>,
-//  CHECK-DAG:     f80 = dense<128> : vector<2xi64>,
-//  CHECK-DAG:     i128 = dense<128> : vector<2xi64>,
-//  CHECK-DAG:     !llvm.ptr<272> = dense<64> : vector<4xi64>,
-//  CHECK-DAG:     i64 = dense<64> : vector<2xi64>,
-//  CHECK-DAG:     !llvm.ptr<270> = dense<32> : vector<4xi64>,
-//  CHECK-DAG:     !llvm.ptr<271> = dense<32> : vector<4xi64>,
-//  CHECK-DAG:     f128 = dense<128> : vector<2xi64>,
-//  CHECK-DAG:     f16 = dense<16> : vector<2xi64>,
-//  CHECK-DAG:     f64 = dense<64> : vector<2xi64>,
-//  CHECK-DAG:     "dlti.stack_alignment" = 128 : i64
-//  CHECK-DAG:     "dlti.endianness" = "little"
+//  LITTLE-DAG: dlti.dl_spec =
+//  LITTLE-DAG:   #dlti.dl_spec<
+//  LITTLE-DAG:     i16 = dense<16> : vector<2xi64>,
+//  LITTLE-DAG:     i32 = dense<32> : vector<2xi64>,
+//  LITTLE-DAG:     i8 = dense<8> : vector<2xi64>,
+//  LITTLE-DAG:     i1 = dense<8> : vector<2xi64>,
+//  LITTLE-DAG:     !llvm.ptr = dense<64> : vector<4xi64>,
+//  LITTLE-DAG:     f80 = dense<128> : vector<2xi64>,
+//  LITTLE-DAG:     i128 = dense<128> : vector<2xi64>,
+//  LITTLE-DAG:     !llvm.ptr<272> = dense<64> : vector<4xi64>,
+//  LITTLE-DAG:     i64 = dense<64> : vector<2xi64>,
+//  LITTLE-DAG:     !llvm.ptr<270> = dense<32> : vector<4xi64>,
+//  LITTLE-DAG:     !llvm.ptr<271> = dense<32> : vector<4xi64>,
+//  LITTLE-DAG:     f128 = dense<128> : vector<2xi64>,
+//  LITTLE-DAG:     f16 = dense<16> : vector<2xi64>,
+//  LITTLE-DAG:     f64 = dense<64> : vector<2xi64>,
+//  LITTLE-DAG:     "dlti.stack_alignment" = 128 : i64
+//  LITTLE-DAG:     "dlti.endianness" = "little"
