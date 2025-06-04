44cc971f926dfb9b0ec99ec7ce4134ccd7cc1d78
Krzysztof Parzyszek <Krzysztof.Parzyszek@amd.com>
2025-06-04 08:09:21 -0500

    Fix test RUN line

--- flang/test/Semantics/OpenMP/atomic.f90
+++ flang/test/Semantics/OpenMP/atomic.f90
@@ -1,6 +1,6 @@
 ! REQUIRES: openmp_runtime
 
-! RUN: %python %S/../test_errors.py %s %flang -fopenmp -J /work2/kparzysz/git/llvm.org/b/x86/runtimes/runtimes-bins/openmp/runtime/src
+! RUN: %python %S/../test_errors.py %s %flang -fopenmp %openmp_flags
 use omp_lib
 ! Check OpenMP 2.13.6 atomic Construct
 
