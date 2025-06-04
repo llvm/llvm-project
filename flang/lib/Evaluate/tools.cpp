4b23d4c7ca4307f3d5c900782c78f1b66236d0c2
Peter Klausler <pklausler@nvidia.com>
2025-06-04 09:23:34 -0700

    [flang] Extension: allow override of inaccessible DEFERRED binding (#142691)
    Inaccessible procedure bindings can't be overridden, but DEFERRED
    bindings must be in a non-abstract extension. We presently emit an error
    for an attempt to override an inaccessible binding in this case. But
    some compilers accept this usage, and since it seems safe enough, I'll
    allow it with an optional warning. Codes can avoid this warning and
    conform to the standard by changing the deferred bindings to be public.

--- flang/lib/Evaluate/tools.cpp
+++ flang/lib/Evaluate/tools.cpp
@@ -1206,7 +1206,8 @@ parser::Message *AttachDeclaration(
   }
   if (const auto *binding{
           unhosted->detailsIf<semantics::ProcBindingDetails>()}) {
-    if (binding->symbol().name() != symbol.name()) {
+    if (!symbol.attrs().test(semantics::Attr::DEFERRED) &&
+        binding->symbol().name() != symbol.name()) {
       message.Attach(binding->symbol().name(),
           "Procedure '%s' of type '%s' is bound to '%s'"_en_US, symbol.name(),
           symbol.owner().GetName().value(), binding->symbol().name());
