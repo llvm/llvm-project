5121c1a589fc205065f68bb3ccc7d965675d478d
Mat Cross <mat.cross@amd.com>
2025-06-04 13:37:46 +0000

    JOB_NAME compatibility with the freestyle original.
    Change-Id: If2e2f3e6728989409b4e3b93d70919d730aa229e

--- .amd-ci/upstream/apptainer_run.sh
+++ .amd-ci/upstream/apptainer_run.sh
@@ -11,12 +11,16 @@ if test ! -f "${image_path}"
 then
   image_path=/proj/csse_jenkins2/swtools/${image_tail}
 fi
-
+job_name="presub_${CHANGE_TARGET:-${BRANCH_NAME:-NoSuchBranchName}}"
+if test -n "${CHANGE_BRANCH:-}"
+then
+  job_name+="_${BRANCH_NAME}" # Sic, the PR-<n> string.
+fi
 apptainer exec \
           --cleanenv \
           --env BUILD_NUMBER="${BUILD_NUMBER:-0}" \
           --env CI="${CI:-false}" \
-          --env JOB_NAME="${JOB_NAME:-NoSuchJobName}" \
+          --env JOB_NAME="${job_name}" \
           --bind "/proj:/proj:ro,$(pwd):${bind_point}:rw" \
           ${image_path} \
           bash -c "cd ${bind_point} && ${_script_dir}/run.sh"
