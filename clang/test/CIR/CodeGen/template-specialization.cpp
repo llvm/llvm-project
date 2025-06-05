34ea2d6dd39701f75be0b9a33e3b6cd79ed3f88a
Andy Kaylor <akaylor@nvidia.com>
2025-06-05 14:18:05 -0700

    Add partial template specialization test

--- clang/test/CIR/CodeGen/template-specialization.cpp
+++ clang/test/CIR/CodeGen/template-specialization.cpp
@@ -5,6 +5,23 @@
 // RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -Wno-unused-value -emit-llvm %s -o %t.ll
 // RUN: FileCheck --input-file=%t.ll %s -check-prefix=OGCG
 
+template<typename T, typename U>
+class Templ {};
+
+template<typename T>
+class Templ<T, int>{};
+
+Templ<int, int> t;
+
+// CIR: !rec_Templ3Cint2C_int3E = !cir.record<class "Templ<int, int>" padded {!u8i}>
+// CIR: cir.global external @t = #cir.zero : !rec_Templ3Cint2C_int3E
+
+// LLVM: %"class.Templ<int, int>" = type { i8 }
+// LLVM: @t = global %"class.Templ<int, int>" zeroinitializer
+
+// OGCG: %class.Templ = type { i8 }
+// OGCG: @t = global %class.Templ zeroinitializer
+
 template<class T>
 class X {
 public:
@@ -15,14 +32,6 @@ template<> class X<int> {
 public:
   int f() { return 1; }
 };
-
-// TODO: This will get dropped when we are deferring functions
-// The speecialization is instantiated first
-// CIR: cir.func{{.*}} @_ZN1XIiE1fEv
-// CIR:   cir.const #cir.int<1>
-
-// LLVM: define{{.*}} i32 @_ZN1XIiE1fEv
-// LLVM:   store i32 1
 
 void test_double() {
   X<double> d;
@@ -52,9 +61,15 @@ void test_int() {
   n.f();
 }
 
+// CIR: cir.func{{.*}} @_ZN1XIiE1fEv
+// CIR:   cir.const #cir.int<1>
+//
 // CIR: cir.func{{.*}} @_Z8test_intv()
 // CIR:   cir.call @_ZN1XIiE1fEv
 
+// LLVM: define{{.*}} i32 @_ZN1XIiE1fEv
+// LLVM:   store i32 1
+//
 // LLVM: define{{.*}} void @_Z8test_intv()
 // LLVM:   call i32 @_ZN1XIiE1fEv
 
