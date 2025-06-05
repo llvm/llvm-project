77ef35ccdfcd843f41ba43e775fcd4284ad9511e
Dmitry Nechitaev <nechda6@gmail.com>
2025-06-05 17:44:33 +0300

    Simplify test

--- clang-tools-extra/test/clang-tidy/checkers/performance/noexcept-move-constructor-allow-false-evaluated.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/performance/noexcept-move-constructor-allow-false-evaluated.cpp
@@ -1,5 +1,3 @@
-// RUN: %check_clang_tidy %s performance-noexcept-move-constructor %t -- -- -fexceptions
-
 // RUN: %check_clang_tidy -check-suffix=CONFIG %s performance-noexcept-move-constructor,performance-noexcept-destructor %t -- \
 // RUN: -config="{CheckOptions: {performance-noexcept-move-constructor.AllowFalseEvaluated: true}}" \
 // RUN: -- -fexceptions
@@ -16,8 +14,6 @@ namespace std
 struct ThrowOnAnything {
   ThrowOnAnything() noexcept(false);
   ThrowOnAnything(ThrowOnAnything&&) noexcept(false);
-  // CHECK-MESSAGES-NOT: :[[@LINE-1]]:3: warning: move constructors should be marked noexcept
-  // CHECK-MESSAGES-CONFIG-NOT: :[[@LINE-2]]:3: warning: move constructors should be marked noexcept
   ThrowOnAnything& operator=(ThrowOnAnything &&) noexcept(false);
   ~ThrowOnAnything() noexcept(false);
 };
@@ -25,12 +21,7 @@ struct ThrowOnAnything {
 struct C_1 {
     static constexpr bool kFalse = false;
     C_1(C_1&&) noexcept(kFalse) = default;
-    // CHECK-MESSAGES: :[[@LINE-1]]:25: warning: noexcept specifier on the move constructor evaluates to 'false' [performance-noexcept-move-constructor]
-    // CHECK-MESSAGES-CONFIG-NOT: :[[@LINE-2]]:25: warning: noexcept specifier on the move constructor evaluates to 'false' [performance-noexcept-move-constructor]
-
     C_1 &operator=(C_1 &&) noexcept(kFalse);
-    // CHECK-MESSAGES: :[[@LINE-1]]:37: warning: noexcept specifier on the move assignment operator evaluates to 'false' [performance-noexcept-move-constructor]
-    // CHECK-MESSAGES-CONFIG-NOT: :[[@LINE-2]]:37: warning: noexcept specifier on the move assignment operator evaluates to 'false' [performance-noexcept-move-constructor]
 };
 
 struct C_2 {
@@ -38,12 +29,7 @@ struct C_2 {
     static_assert(!kEval); // kEval == false;
 
     C_2(C_2&&) noexcept(kEval) = default;
-    // CHECK-MESSAGES: :[[@LINE-1]]:25: warning: noexcept specifier on the move constructor evaluates to 'false' [performance-noexcept-move-constructor]
-    // CHECK-MESSAGES-CONFIG-NOT: :[[@LINE-2]]:25: warning: noexcept specifier on the move constructor evaluates to 'false' [performance-noexcept-move-constructor]
-
     C_2 &operator=(C_2 &&) noexcept(kEval);
-    // CHECK-MESSAGES: :[[@LINE-1]]:37: warning: noexcept specifier on the move assignment operator evaluates to 'false' [performance-noexcept-move-constructor]
-    // CHECK-MESSAGES-CONFIG-NOT: :[[@LINE-2]]:37: warning: noexcept specifier on the move assignment operator evaluates to 'false' [performance-noexcept-move-constructor]
 
     ThrowOnAnything field;
 };
@@ -53,9 +39,6 @@ struct C_3 {
     static_assert(!kEval); // kEval == false;
 
     C_3(C_3&&) noexcept(kEval) = default;
-    // CHECK-MESSAGES: :[[@LINE-1]]:25: warning: noexcept specifier on the move constructor evaluates to 'false' [performance-noexcept-move-constructor]
-    // CHECK-MESSAGES-CONFIG-NOT: :[[@LINE-2]]:25: warning: noexcept specifier on the move constructor evaluates to 'false' [performance-noexcept-move-constructor]
-
     ~C_3() noexcept(kEval) = default;
     // CHECK-MESSAGES-CONFIG: :[[@LINE-1]]:21: warning: noexcept specifier on the destructor evaluates to 'false'
 
