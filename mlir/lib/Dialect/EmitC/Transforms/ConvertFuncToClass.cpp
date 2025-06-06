764ffad517646b5c5ba023dc6cb89a55637f8e61
Jaddyen <ajaden@google.com>
2025-06-06 17:22:04 +0000

    Removed unnecessary comments

--- mlir/lib/Dialect/EmitC/Transforms/ConvertFuncToClass.cpp
+++ mlir/lib/Dialect/EmitC/Transforms/ConvertFuncToClass.cpp
@@ -29,38 +29,8 @@ struct ConvertFuncToClassPass
     emitc::FuncOp funcOp = getOperation();
     MLIRContext *context = funcOp->getContext();
 
-    // Wrap each C operator op with an expression op.
     OpBuilder builder(context);
     createClass(funcOp, builder);
-
-    // // Create the new function inside the class
-    // auto funcType = FunctionType::get(funcOp.getContext(),
-    // funcOp.getFunctionType().getInputs(),
-    // funcOp.getFunctionType().getResults()); auto newFuncOp =
-    // builder.create<emitc::FuncOp>(
-    //     funcOp.getLoc(),builder.getStringAttr("execute"), funcType );
-
-    // builder.createBlock(&newFuncOp.getBody());
-    // builder.setInsertionPointToStart(&newFuncOp.getBody().front());
-
-    // // 7. Remap original arguments to field pointers
-    // IRMapping mapper;
-
-    // // 8. move or clone operations from original function
-    // for (Operation &opToClone :
-    // llvm::make_early_inc_range(funcOp.getBody().front())) {
-    //     if (isa<emitc::ConstantOp>(opToClone) ||
-    //         isa<emitc::SubscriptOp>(opToClone) ||
-    //         isa<emitc::LoadOp>(opToClone) ||
-    //         isa<emitc::AddOp>(opToClone) ||
-    //         isa<emitc::AssignOp>(opToClone) ||
-    //         isa<emitc::ReturnOp>(opToClone )) {
-    //     builder.clone(opToClone, mapper);
-    //     } else  {
-    //     opToClone.emitOpError("Unsupported operation found");
-    //     }
-    // }
-    // if (funcOp->use_empty()) funcOp->erase();
   }
 };
 
