8f1346cf976cfc1cb3b610d3fd0af4375c9cdbe3
Sarnie, Nick <nick.sarnie@intel.com>
2025-06-06 10:26:19 -0700

    improve test
    Signed-off-by: Sarnie, Nick <nick.sarnie@intel.com>

--- clang/test/AST/ast-dump-type-callingconv.cpp
+++ clang/test/AST/ast-dump-type-callingconv.cpp
@@ -1,24 +1,11 @@
-// Verify there is a space after the parens when priting callingconv attributes.
-// RUN: %clang_cc1 -DDEVICE -triple spirv64 -ast-dump -ast-dump-filter foo %s \
-// RUN: | FileCheck -check-prefix=CHECK-DEVICE --strict-whitespace %s
+// RUN: %clang_cc1 -triple aarch64 -ast-dump -ast-dump-filter foo %s \
+// RUN: | FileCheck --strict-whitespace %s
 
-// RUN: %clang_cc1 -DVECTOR -triple aarch64 -ast-dump -ast-dump-filter foo %s \
-// RUN: | FileCheck -check-prefix=CHECK-VECTOR --strict-whitespace %s
+// CHECK: {{foo1 'void \(\) __attribute__\(\(device_kernel\)\)'$}}
+void foo1() __attribute__((device_kernel));
 
-// RUN: %clang_cc1 -DSVE -triple aarch64 -ast-dump -ast-dump-filter foo %s \
-// RUN: | FileCheck -check-prefix=CHECK-SVE --strict-whitespace %s
+// CHECK: {{foo2 'void \(\) __attribute__\(\(aarch64_vector_pcs\)\)'$}}
+void foo2()  __attribute__((aarch64_vector_pcs));
 
-#ifdef DEVICE
-// CHECK-DEVICE-NOT: ()__attribute__((device_kernel))
-void foo() __attribute__((device_kernel));
-#endif
-
-#ifdef VECTOR
-// CHECK-VECTOR-NOT: ()__attribute__((aarch64_vector_pcs))
-void foo()  __attribute__((aarch64_vector_pcs));
-#endif
-
-#ifdef SVE
-// CHECK-SVE-NOT: ()__attribute__((aarch64_sve_pcs))
-void foo()  __attribute__((aarch64_sve_pcs));
-#endif
+// CHECK: {{foo3 'void \(\) __attribute__\(\(aarch64_sve_pcs\)\)'$}}
+void foo3()  __attribute__((aarch64_sve_pcs));
