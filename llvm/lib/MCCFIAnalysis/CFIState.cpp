b76fc6ea0a5ded5cb16db8f582a6a039113e1b0c
Amirhossein Pashaeehir <amirhosseinp@google.com>
2025-06-06 18:04:33 +0000

    Write a CFI directive to DWARF operation converter

--- llvm/lib/MCCFIAnalysis/CFIState.cpp
+++ llvm/lib/MCCFIAnalysis/CFIState.cpp
@@ -1,6 +1,8 @@
 // TODO check what includes to keep and what to remove
 #include "llvm/MCCFIAnalysis/CFIState.h"
 #include "llvm/ADT/DenseMap.h"
+#include "llvm/BinaryFormat/Dwarf.h"
+#include "llvm/DebugInfo/DWARF/DWARFDebugFrame.h"
 #include "llvm/MC/MCDwarf.h"
 #include "llvm/Support/FormatVariadic.h"
 #include <cassert>
@@ -9,6 +11,101 @@
 #include <string>
 
 using namespace llvm;
+
+dwarf::CFIProgram llvm::convertMC2DWARF(MCContext &Context,
+                                        MCCFIInstruction CFIDirective,
+                                        int64_t CFAOffset) {
+  //! FIXME, this way of instantiating CFI program does not look right, either
+  //! refactor CFIProgram to not depend on the Code/Data Alignment or add a new
+  //! type that is independent from this and is also feedable to UnwindTable.
+  auto DwarfOperations =
+      dwarf::CFIProgram(0, 0, Context.getTargetTriple().getArch());
+
+  switch (CFIDirective.getOperation()) {
+  case MCCFIInstruction::OpSameValue:
+    DwarfOperations.addInstruction(dwarf::DW_CFA_same_value,
+                                   CFIDirective.getRegister());
+    break;
+  case MCCFIInstruction::OpRememberState:
+    DwarfOperations.addInstruction(dwarf::DW_CFA_remember_state);
+    break;
+  case MCCFIInstruction::OpRestoreState:
+    DwarfOperations.addInstruction(dwarf::DW_CFA_restore_state);
+    break;
+  case MCCFIInstruction::OpOffset:
+    DwarfOperations.addInstruction(dwarf::DW_CFA_offset,
+                                   CFIDirective.getRegister(),
+                                   CFIDirective.getOffset());
+    break;
+  case MCCFIInstruction::OpLLVMDefAspaceCfa:
+    DwarfOperations.addInstruction(dwarf::DW_CFA_LLVM_def_aspace_cfa,
+                                   CFIDirective.getRegister());
+    break;
+  case MCCFIInstruction::OpDefCfaRegister:
+    DwarfOperations.addInstruction(dwarf::DW_CFA_def_cfa,
+                                   CFIDirective.getRegister());
+    break;
+  case MCCFIInstruction::OpDefCfaOffset:
+    DwarfOperations.addInstruction(dwarf::DW_CFA_def_cfa_offset,
+                                   CFIDirective.getOffset());
+    break;
+  case MCCFIInstruction::OpDefCfa:
+    DwarfOperations.addInstruction(dwarf::DW_CFA_def_cfa,
+                                   CFIDirective.getRegister(),
+                                   CFIDirective.getOffset());
+    break;
+  case MCCFIInstruction::OpRelOffset:
+    DwarfOperations.addInstruction(dwarf::DW_CFA_offset,
+                                   CFIDirective.getRegister(),
+                                   CFIDirective.getOffset() - CFAOffset);
+    break;
+  case MCCFIInstruction::OpAdjustCfaOffset:
+    DwarfOperations.addInstruction(dwarf::DW_CFA_def_cfa_offset,
+                                   CFIDirective.getOffset() + CFAOffset);
+    break;
+  case MCCFIInstruction::OpEscape:
+    // TODO It's now feasible but for now, I ignore it
+    break;
+  case MCCFIInstruction::OpRestore:
+    DwarfOperations.addInstruction(dwarf::DW_CFA_restore);
+    break;
+  case MCCFIInstruction::OpUndefined:
+    DwarfOperations.addInstruction(dwarf::DW_CFA_undefined,
+                                   CFIDirective.getRegister());
+    break;
+  case MCCFIInstruction::OpRegister:
+    DwarfOperations.addInstruction(dwarf::DW_CFA_register,
+                                   CFIDirective.getRegister(),
+                                   CFIDirective.getRegister2());
+    break;
+  case MCCFIInstruction::OpWindowSave:
+    // TODO make sure these are the same.
+    DwarfOperations.addInstruction(dwarf::DW_CFA_GNU_window_save);
+    break;
+  case MCCFIInstruction::OpNegateRAState:
+    // TODO make sure these are the same.
+    DwarfOperations.addInstruction(dwarf::DW_CFA_AARCH64_negate_ra_state);
+    break;
+  case MCCFIInstruction::OpNegateRAStateWithPC:
+    // TODO make sure these are the same.
+    DwarfOperations.addInstruction(
+        dwarf::DW_CFA_AARCH64_negate_ra_state_with_pc);
+    break;
+  case MCCFIInstruction::OpGnuArgsSize:
+    DwarfOperations.addInstruction(dwarf::DW_CFA_GNU_args_size);
+    break;
+  case MCCFIInstruction::OpLabel:
+    // TODO, I don't know what it is, I have to implement it.
+    break;
+  case MCCFIInstruction::OpValOffset:
+    DwarfOperations.addInstruction(dwarf::DW_CFA_val_offset,
+                                   CFIDirective.getRegister(),
+                                   CFIDirective.getOffset());
+    break;
+  }
+
+  return DwarfOperations;
+}
 
 using DWARFRegType = int64_t;
 
