69233c1156dd19da66a1d27061cbb420a81131a6
Balázs Kéri <balazs.keri@ericsson.com>
2025-06-06 09:30:06 +0200

    corrected formatting error

--- clang-tools-extra/clang-tidy/misc/VisibilityChangeToVirtualFunctionCheck.cpp
+++ clang-tools-extra/clang-tidy/misc/VisibilityChangeToVirtualFunctionCheck.cpp
@@ -1,5 +1,4 @@
-//===--- VisibilityChangeToVirtualFunctionCheck.cpp - clang-tidy
-//-------------------===//
+//===--- VisibilityChangeToVirtualFunctionCheck.cpp - clang-tidy ----------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
