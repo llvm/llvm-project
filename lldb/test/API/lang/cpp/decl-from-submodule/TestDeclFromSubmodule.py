b22de21b5562827e034ff1bb91db4abf52f304de
Michael Buch <michaelbuch12@gmail.com>
2025-06-06 11:19:50 +0100

    fixup! XFAIL on Linux

--- lldb/test/API/lang/cpp/decl-from-submodule/TestDeclFromSubmodule.py
+++ lldb/test/API/lang/cpp/decl-from-submodule/TestDeclFromSubmodule.py
@@ -11,6 +11,8 @@ from lldbsuite.test import lldbutil
 class DeclFromSubmoduleTestCase(TestBase):
     # Requires DWARF debug info which is not retained when linking with link.exe.
     @skipIfWindows
+    # Lookup for decls in submodules fails in Linux
+    @expectedFailureAll(oslist=["linux"])
     def test_expr(self):
         self.build()
         lldbutil.run_to_source_breakpoint(self, "return 0", lldb.SBFileSpec("main.cpp"))
