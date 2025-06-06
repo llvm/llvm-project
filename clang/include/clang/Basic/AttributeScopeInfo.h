1fd387a3c72184831899709c728b801339696072
Oleksandr Tarasiuk <oleksandr.tarasiuk@outlook.com>
2025-06-06 17:17:02 +0300

    fix formatting

--- clang/include/clang/Basic/AttributeScopeInfo.h
+++ clang/include/clang/Basic/AttributeScopeInfo.h
@@ -1,4 +1,4 @@
-//===- AttributeScopeInfo.h - Base info about an Attribute Scope -*- C++ -*-===//
+//==- AttributeScopeInfo.h - Base info about an Attribute Scope --*- C++ -*-==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
