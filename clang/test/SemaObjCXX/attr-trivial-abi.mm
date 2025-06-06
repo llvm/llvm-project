d4e5db938e4af98dda2dd4b518273fe339c64bfa
Corentin Jabot <corentinjabot@gmail.com>
2025-06-06 13:09:40 +0200

    [Clang] Non-polymorphic trivially relocatable types can have [[trivial_abi]]
    Use the definition of trivially relocatable types to short circuit
    some checks for trivial_abi.
    
    Note that this is mostly a no-op as there is a lot of overlap between
    trivial_abi and trivial relocatability (ie I can't envision a scenario in
    which there would be a trivially relocatable type that would not be
    eligible for trivial_abi based on its special member function...
    which is good!)
    
    Note that for bases and members we need to check CanPassInRegister
    rather than just relocation. So we do these checks first,
    which leads to better diagnostics.

--- clang/test/SemaObjCXX/attr-trivial-abi.mm
+++ clang/test/SemaObjCXX/attr-trivial-abi.mm
@@ -108,7 +108,8 @@ namespace deletedCopyMoveConstructor {
     CopyMoveDeleted(CopyMoveDeleted &&) = delete;
   };
 
-  struct __attribute__((trivial_abi)) S18 { // expected-warning {{'trivial_abi' cannot be applied to 'S18'}} expected-note {{copy constructors and move constructors are all deleted}}
+  struct __attribute__((trivial_abi)) S18 { // expected-warning {{'trivial_abi' cannot be applied to 'S18'}} \
+                                            // expected-note {{'trivial_abi' is disallowed on 'S18' because it has a field of a non-trivial class type}}
     CopyMoveDeleted a;
   };
 
