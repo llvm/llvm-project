81e5a79b90887d8da502d9c9855b32aaa264fce6
Nuri Amari <nuriamari@fb.com>
2025-06-06 12:18:17 -0700

    Fix introduced test

--- clang/test/CodeGen/distributed-thin-lto/cs-irpgo.c
+++ clang/test/CodeGen/distributed-thin-lto/cs-irpgo.c
@@ -1,7 +1,7 @@
 // RUN: mkdir -p %t
 // RUN: %clang -target x86_64-apple-darwin -fcs-profile-generate -flto=thin -c -o %t/main.bc %s
 // RUN: llvm-lto2 run --thinlto-distributed-indexes %t/main.bc -o %t/index -r=%t/main.bc,_main,px
-// RUN: %clang -target x86_64-apple-darwin -fcs-profile-generate -c %t/main.o -fthinlto-index=%t/main.bc.thinlto.bc %t/main.bc
+// RUN: %clang -target x86_64-apple-darwin -fcs-profile-generate -c -o %t/main.o -fthinlto-index=%t/main.bc.thinlto.bc %t/main.bc
 
 int main() {
     return 0;
