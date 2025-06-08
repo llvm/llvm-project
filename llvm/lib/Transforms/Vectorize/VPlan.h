7d5c357304cdddbfd45494c098db16e8994200cb
Florian Hahn <flo@fhahn.com>
2025-06-08 21:46:21 +0100

    [LV] Vectorize select min/max index.
    Add support for vectorizing loops that select the index of the minimum or
    maximum element. The patch implements vectorizing those patterns by
    combining Min/Max and FindFirstIV reductions.
    
    It extends matching Min/Max reductions to allow in-loop users that are
    FindLastIV reductions. It records a flag indicating that the Min/Max
    reduction is used by another reduction.
    
    When creating reduction recipes, we process any reduction that has other
    reduction users. The reduction using the min/max reduction needs adjusting
    to compute the correct result:
     1. We need to find the first IV for which the condition based on the
        min/max reduction is true,
     2. Compare the partial min/max reduction result to its final value and,
     3. Select the lanes of the partial FindLastIV reductions which correspond
        to the lanes matching the min/max reduction result.

--- llvm/lib/Transforms/Vectorize/VPlan.h
+++ llvm/lib/Transforms/Vectorize/VPlan.h
@@ -1830,7 +1830,8 @@ public:
   ~VPHeaderPHIRecipe() override = default;
 
   /// Method to support type inquiry through isa, cast, and dyn_cast.
-  static inline bool classof(const VPRecipeBase *B) {
+  static inline bool classof(const VPUser *U) {
+    auto *B = cast<VPRecipeBase>(U);
     return B->getVPDefID() >= VPDef::VPFirstHeaderPHISC &&
            B->getVPDefID() <= VPDef::VPLastHeaderPHISC;
   }
@@ -1838,6 +1839,10 @@ public:
     auto *B = V->getDefiningRecipe();
     return B && B->getVPDefID() >= VPRecipeBase::VPFirstHeaderPHISC &&
            B->getVPDefID() <= VPRecipeBase::VPLastHeaderPHISC;
+  }
+  static inline bool classof(const VPSingleDefRecipe *B) {
+    return B->getVPDefID() >= VPDef::VPFirstHeaderPHISC &&
+           B->getVPDefID() <= VPDef::VPLastHeaderPHISC;
   }
 
   /// Generate the phi nodes.
@@ -1900,7 +1905,7 @@ public:
     return R && classof(R);
   }
 
-  static inline bool classof(const VPHeaderPHIRecipe *R) {
+  static inline bool classof(const VPSingleDefRecipe *R) {
     return classof(static_cast<const VPRecipeBase *>(R));
   }
 
