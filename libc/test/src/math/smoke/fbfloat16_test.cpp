8aacba633f888bdbbfd556b60aa3f0c0fe47ce57
krishna2803 <kpandey81930@gmail.com>
2025-06-09 18:32:40 +0530

    chore: add dummy tests for fbfloat16
    Signed-off-by: krishna2803 <kpandey81930@gmail.com>

---
+++ libc/test/src/math/smoke/fbfloat16_test.cpp
@@ -0,0 +1,23 @@
+//===-- Unittests for fbfloat16 function ----------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "src/math/fbfloat16.h"
+#include "test/UnitTest/FPMatcher.h"
+#include "test/UnitTest/Test.h"
+
+// TODO: change this to bfloat16
+using LlvmLibcFBfloat16Test = LIBC_NAMESPACE::testing::FPTest<float>;
+
+TEST_F(LlvmLibcFBfloat16Test, SpecialNumbers) {
+  ASSERT_EQ(1, 2);
+  // TODO: implement this!
+  // x = some bfloat number
+  // float y = x as float (using our ctor?)
+  // float z = mfpr(x) as float
+  // check y == z
+}
