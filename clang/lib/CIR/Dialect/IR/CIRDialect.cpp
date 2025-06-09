e69a5b6e9d25ffc52fe839e8d3255ff773a3738c
AmrDeveloper <amr96@programmer.net>
2025-06-09 20:29:51 +0200

    Add direct test for VecCreateFolder

--- clang/lib/CIR/Dialect/IR/CIRDialect.cpp
+++ clang/lib/CIR/Dialect/IR/CIRDialect.cpp
@@ -1534,8 +1534,7 @@ LogicalResult cir::GetMemberOp::verify() {
 //===----------------------------------------------------------------------===//
 
 OpFoldResult cir::VecCreateOp::fold(FoldAdaptor adaptor) {
-  auto elements = getElements();
-  if (llvm::any_of(elements, [](mlir::Value attr) {
+  if (llvm::any_of(getElements(), [](mlir::Value attr) {
         return !mlir::isa<cir::ConstantOp>(attr.getDefiningOp());
       }))
     return {};
