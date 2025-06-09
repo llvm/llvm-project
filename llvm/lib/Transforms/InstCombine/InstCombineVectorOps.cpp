5d9e4d98bb5b116c0323e5229f54fd98bb44671e
Aaron Gorenstein <agorenstein@nvidia.com>
2025-06-09 12:14:51 -0400

    Removed unneeded check

--- llvm/lib/Transforms/InstCombine/InstCombineVectorOps.cpp
+++ llvm/lib/Transforms/InstCombine/InstCombineVectorOps.cpp
@@ -544,12 +544,10 @@ Instruction *InstCombinerImpl::visitExtractElementInst(ExtractElementInst &EI) {
     } else if (auto *SVI = dyn_cast<ShuffleVectorInst>(I)) {
       // extractelt (shufflevector %v1, %v2, splat-mask) idx ->
       // extractelt %v1, splat-mask[0]
-      if (isa<VectorType>(SVI->getType())) {
-        auto mask = SVI->getShuffleMask();
-        if (mask[0] != -1 && all_equal(mask))
-          return ExtractElementInst::Create(SVI->getOperand(0),
-                                            Builder.getInt64(mask[0]));
-      }
+      auto mask = SVI->getShuffleMask();
+      if (mask[0] != -1 && all_equal(mask))
+        return ExtractElementInst::Create(SVI->getOperand(0),
+                                          Builder.getInt64(mask[0]));
 
       // If this is extracting an element from a shufflevector, figure out where
       // it came from and extract from the appropriate input element instead.
