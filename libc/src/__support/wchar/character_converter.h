7c9ad0294004764c67d97716f3ff754eeaec8742
Uzair Nawaz <uzairnawaz@google.com>
2025-06-10 23:12:13 +0000

    license and include guards

--- libc/src/__support/wchar/character_converter.h
+++ libc/src/__support/wchar/character_converter.h
@@ -1,7 +1,19 @@
+//===-- Definition of a class for mbstate_t and conversion -----*-- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_LIBC_SRC___SUPPORT_CHARACTER_CONVERTER_H
+#define LLVM_LIBC_SRC___SUPPORT_CHARACTER_CONVERTER_H
 
 #include "hdr/types/wchar_t.h"
 #include "src/__support/wchar/mbstate.h"
 #include "src/__support/wchar/utf_ret.h"
+
+namespace LIBC_NAMESPACE_DECL {
 
 class CharacterConverter {
 private:
@@ -18,3 +30,7 @@ public:
   utf_ret<char> pop_utf8();
   utf_ret<wchar_t> pop_utf32();
 };
+
+} // namespace LIBC_NAMESPACE_DECL
+
+#endif // LLVM_LIBC_SRC___SUPPORT_CHARACTER_CONVERTER_H
--- libc/src/__support/wchar/mbstate.h
+++ libc/src/__support/wchar/mbstate.h
@@ -1,8 +1,25 @@
+//===-- Definition of mbstate_t -------------------------- -----*-- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_LIBC_SRC___SUPPORT_MBSTATE_H
+#define LLVM_LIBC_SRC___SUPPORT_MBSTATE_H
 
 #include "hdr/types/wchar_t.h"
+
+namespace LIBC_NAMESPACE_DECL {
 
 struct mbstate_t {
   wchar_t partial;
   unsigned char bits_processed;
   unsigned char total_bytes;
-};
+}; 
+
+} // namespace LIBC_NAMESPACE_DECL
+
+#endif // LLVM_LIBC_SRC___SUPPORT_MBSTATE_H
+
