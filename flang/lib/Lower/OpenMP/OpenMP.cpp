b994a4c04f38d8cfb13f3dbf3d99146cb778443e
Peter Klausler <pklausler@nvidia.com>
2025-06-10 14:44:41 -0700

    [flang][NFC] Clean up code in two new functions (#142037)
    Two recently-added functions in Semantics/tools.h need some cleaning up
    to conform to the coding style of the project. One of them should
    actually be in Parser/tools.{h,cpp}, the other doesn't need to be
    defined in the header.

--- flang/lib/Lower/OpenMP/OpenMP.cpp
+++ flang/lib/Lower/OpenMP/OpenMP.cpp
@@ -3200,8 +3200,8 @@ static void genAtomicCapture(lower::AbstractConverter &converter,
   firOpBuilder.createBlock(&(atomicCaptureOp->getRegion(0)));
   mlir::Block &block = atomicCaptureOp->getRegion(0).back();
   firOpBuilder.setInsertionPointToStart(&block);
-  if (semantics::checkForSingleVariableOnRHS(stmt1)) {
-    if (semantics::checkForSymbolMatch(semantics::GetExpr(stmt2Var),
+  if (parser::CheckForSingleVariableOnRHS(stmt1)) {
+    if (semantics::CheckForSymbolMatch(semantics::GetExpr(stmt2Var),
                                        semantics::GetExpr(stmt2Expr))) {
       // Atomic capture construct is of the form [capture-stmt, update-stmt]
       const semantics::SomeExpr &fromExpr = *semantics::GetExpr(stmt1Expr);
