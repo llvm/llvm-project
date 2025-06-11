652a0906fcec7fbe533de1e7d88f6d90aba6a41a
Morris Hafner <mmha@users.noreply.github.com>
2025-06-11 18:24:46 +0200

    [CIR] Upstream minimal builtin function call support (#142981)
    This patch adds all bits required to implement builtin function calls to
    ClangIR. It doesn't actually implement any of the builtins except those
    that fold to a constant ahead of CodeGen
    (`__builtin_is_constant_evaluated()` being one example).

--- clang/lib/CIR/CodeGen/CIRGenCall.h
+++ clang/lib/CIR/CodeGen/CIRGenCall.h
@@ -44,15 +44,24 @@ public:
 class CIRGenCallee {
   enum class SpecialKind : uintptr_t {
     Invalid,
+    Builtin,
 
-    Last = Invalid,
+    Last = Builtin,
+  };
+
+  struct BuiltinInfoStorage {
+    const clang::FunctionDecl *decl;
+    unsigned id;
   };
 
   SpecialKind kindOrFunctionPtr;
 
   union {
     CIRGenCalleeInfo abstractInfo;
+    BuiltinInfoStorage builtinInfo;
   };
+
+  explicit CIRGenCallee(SpecialKind kind) : kindOrFunctionPtr(kind) {}
 
 public:
   CIRGenCallee() : kindOrFunctionPtr(SpecialKind::Invalid) {}
@@ -67,6 +76,25 @@ public:
   forDirect(mlir::Operation *funcPtr,
             const CIRGenCalleeInfo &abstractInfo = CIRGenCalleeInfo()) {
     return CIRGenCallee(abstractInfo, funcPtr);
+  }
+
+  bool isBuiltin() const { return kindOrFunctionPtr == SpecialKind::Builtin; }
+
+  const clang::FunctionDecl *getBuiltinDecl() const {
+    assert(isBuiltin());
+    return builtinInfo.decl;
+  }
+  unsigned getBuiltinID() const {
+    assert(isBuiltin());
+    return builtinInfo.id;
+  }
+
+  static CIRGenCallee forBuiltin(unsigned builtinID,
+                                 const clang::FunctionDecl *builtinDecl) {
+    CIRGenCallee result(SpecialKind::Builtin);
+    result.builtinInfo.decl = builtinDecl;
+    result.builtinInfo.id = builtinID;
+    return result;
   }
 
   bool isOrdinary() const {
