a501b1c72db29f74f43953f61c97999dec560499
Jacques Pienaar <jpienaar@google.com>
2025-06-11 14:09:20 +0000

    Fix formatting

--- llvm/unittests/Support/DebugLogTest.cpp
+++ llvm/unittests/Support/DebugLogTest.cpp
@@ -1,4 +1,5 @@
-//===- llvm/unittest/Support/DebugLogTest.cpp ------------------------------===//
+//===- llvm/unittest/Support/DebugLogTest.cpp
+//------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
