6adb80d86101268ea7791b6a43e96b5605d66612
Michael Jones <michaelrj@google.com>
2025-06-11 16:01:51 -0700

    [libc] Always provide char8/16/32_t
    The current definitions only provide these types if the stdc version is
    2023 for char8_t or 2011 for char16/32_t. None of our other types are
    currently handled this way. If we want to provide these headers only
    under certain circumstances we should add guards to the types in the
    public header, since internal code should always have access to these
    types.

--- libc/include/llvm-libc-types/char16_t.h
+++ libc/include/llvm-libc-types/char16_t.h
@@ -9,9 +9,7 @@
 #ifndef LLVM_LIBC_TYPES_CHAR16_T_H
 #define LLVM_LIBC_TYPES_CHAR16_T_H
 
-#if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L
 #include "../llvm-libc-macros/stdint-macros.h"
 typedef uint_least16_t char16_t;
-#endif
 
 #endif // LLVM_LIBC_TYPES_CHAR16_T_H
--- libc/include/llvm-libc-types/char32_t.h
+++ libc/include/llvm-libc-types/char32_t.h
@@ -9,9 +9,7 @@
 #ifndef LLVM_LIBC_TYPES_CHAR32_T_H
 #define LLVM_LIBC_TYPES_CHAR32_T_H
 
-#if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L
 #include "../llvm-libc-macros/stdint-macros.h"
 typedef uint_least32_t char32_t;
-#endif
 
 #endif // LLVM_LIBC_TYPES_CHAR32_T_H
