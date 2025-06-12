8e28897e984ff557714f1d9e8b141d3e5c083082
Kostas <konstantinos.alvertis@gmail.com>
2025-06-13 00:16:49 +0100

    [BOLT][PPC] Add unit test.

--- bolt/lib/Target/PowerPC/PPCMCPlusBuilder.cpp
+++ bolt/lib/Target/PowerPC/PPCMCPlusBuilder.cpp
@@ -10,6 +10,7 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include "bolt/Target/PowerPC/PPCMCPlusBuilder.h"
 #include "bolt/Core/MCPlusBuilder.h"
 #include "llvm/MC/MCInst.h"
 #include "llvm/MC/MCRegisterInfo.h"
@@ -21,27 +22,21 @@
 namespace llvm {
 namespace bolt {
 
-class PPCMCPlusBuilder : public MCPlusBuilder {
-public:
-  using MCPlusBuilder::MCPlusBuilder;
+// Create instructions to push two registers onto the stack
+void PPCMCPlusBuilder::createPushRegisters(MCInst &Inst1, MCInst &Inst2,
+                                           MCPhysReg Reg1, MCPhysReg /*Reg2*/) {
+  Inst1.clear();
+  Inst1.setOpcode(PPC::STDU);
+  Inst1.addOperand(MCOperand::createReg(PPC::R1)); // destination (SP)
+  Inst1.addOperand(MCOperand::createReg(PPC::R1)); // base (SP)
+  Inst1.addOperand(MCOperand::createImm(-16));     // offset
 
-  // Create instructions to push two registers onto the stack
-  static void createPushRegisters(MCInst &Inst1, MCInst &Inst2, MCPhysReg Reg1,
-                                  MCPhysReg /*Reg2*/) {
-
-    Inst1.clear();
-    Inst1.setOpcode(PPC::STDU);
-    Inst1.addOperand(MCOperand::createReg(PPC::R1)); // destination (SP)
-    Inst1.addOperand(MCOperand::createReg(PPC::R1)); // base (SP)
-    Inst1.addOperand(MCOperand::createImm(-16));     // offset
-
-    Inst2.clear();
-    Inst2.setOpcode(PPC::STD);
-    Inst2.addOperand(MCOperand::createReg(Reg1));    // source register
-    Inst2.addOperand(MCOperand::createReg(PPC::R1)); // base (SP)
-    Inst2.addOperand(MCOperand::createImm(0));       // offset
-  }
-};
+  Inst2.clear();
+  Inst2.setOpcode(PPC::STD);
+  Inst2.addOperand(MCOperand::createReg(Reg1));    // source register
+  Inst2.addOperand(MCOperand::createReg(PPC::R1)); // base (SP)
+  Inst2.addOperand(MCOperand::createImm(0));       // offset
+}
 
 MCPlusBuilder *createPowerPCMCPlusBuilder(const MCInstrAnalysis *Analysis,
                                           const MCInstrInfo *Info,
