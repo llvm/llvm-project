57a8a4e937a6a069eec1fdaa4304f8b1781ede0d
Jacek Caban <jacek@codeweavers.com>
2025-06-12 15:18:49 +0200

    Remove temporary debug test changes

--- clang/test/Frontend/ir-support.c
+++ clang/test/Frontend/ir-support.c
@@ -4,16 +4,16 @@
 // We strip differing '.file' directives before comparing.
 
 // Reference output:
-// RUN: %clang_cc1 -triple=x86_64-windows -S -o - %s | grep -v '\.file' > %t.s
+// RUN: %clang_cc1 -S -o - %s | grep -v '\.file' > %t.s
 
 // LLVM bitcode:
-// RUN: %clang_cc1 -triple=x86_64-windows -emit-llvm-bc -o %t.bc %s
-// RUN: %clang_cc1 -triple=x86_64-windows -S -o - %t.bc | grep -v '\.file' > %t.bc.s
+// RUN: %clang_cc1 -emit-llvm-bc -o %t.bc %s
+// RUN: %clang_cc1 -S -o - %t.bc | grep -v '\.file' > %t.bc.s
 // RUN: diff %t.s %t.bc.s
 
 // LLVM IR source code:
-// RUN: %clang_cc1 -triple=x86_64-windows -emit-llvm -o %t.ll %s
-// RUN: %clang_cc1 -triple=x86_64-windows -S -o - %t.ll | grep -v '\.file' > %t.ll.s
+// RUN: %clang_cc1 -emit-llvm -o %t.ll %s
+// RUN: %clang_cc1 -S -o - %t.ll | grep -v '\.file' > %t.ll.s
 // RUN: diff %t.s %t.ll.s
 
 int f(void) { return 0; }
