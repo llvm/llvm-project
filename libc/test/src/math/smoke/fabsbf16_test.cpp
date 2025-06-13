ea6cdae5d7955c5eaf22f6f3219f6ed61bbdf2a6
krishna2803 <kpandey81930@gmail.com>
2025-06-13 05:49:03 +0530

    feat: implement fabsbf16 function
    Signed-off-by: krishna2803 <kpandey81930@gmail.com>

---
+++ libc/test/src/math/smoke/fabsbf16_test.cpp
@@ -0,0 +1,13 @@
+//===-- Unittests for fabsf16 ---------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "FAbsTest.h"
+
+#include "src/math/fabsbf16.h"
+
+LIST_FABS_TESTS(bfloat16, LIBC_NAMESPACE::fabsbf16)
