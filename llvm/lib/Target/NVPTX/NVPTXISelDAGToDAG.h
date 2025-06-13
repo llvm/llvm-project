75911a61afc38e868a7a448203b61bfab0878977
Alex MacLean <amaclean@nvidia.com>
2025-06-12 23:00:33 -0700

    [NVPTX] Consistently check fast-math flags when lowering fsqrt (#143776)
    Ensure that we check the global, function-level, and instruction-level
    flags when considering whether to use `sqrt.rn` or `sqrt.approx` to
    lower either `@llvm.sqrt.f32` or `@llvm.nvvm.sqrt.f`

--- llvm/lib/Target/NVPTX/NVPTXISelDAGToDAG.h
+++ llvm/lib/Target/NVPTX/NVPTXISelDAGToDAG.h
@@ -44,7 +44,7 @@ class LLVM_LIBRARY_VISIBILITY NVPTXDAGToDAGISel : public SelectionDAGISel {
   bool doMulWide;
 
   NVPTX::DivPrecisionLevel getDivF32Level(const SDNode *N) const;
-  bool usePrecSqrtF32() const;
+  bool usePrecSqrtF32(const SDNode *N) const;
   bool useF32FTZ() const;
   bool allowFMA() const;
   bool allowUnsafeFPMath() const;
