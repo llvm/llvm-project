f7f4fa95783ff67ceedf83e7bb7b8cdac695bc44
Florian Hahn <flo@fhahn.com>
2025-06-14 17:18:36 +0100

    Reapply "[VPlan] Set branch weight metadata on middle term in VPlan (NFC) (#143035)"
    This reverts commit bb7a3d2985b662e77b766d6507c511122b45e4a3.
    
    The recommitted version addresses post-commit comments and adjusts the
    place the branch weights are added. It now runs before VPlans are optimized
    for VF and UF, which may remove the vector loop region, causing a crash
    trying to get the middle block after that. Test case added in
    5c7e09c996b99c1.
    
    Original message:
    Manage branch weights for the BranchOnCond in the middle block in VPlan.
    This requires updating VPInstruction to inherit from VPIRMetadata, which
    in general makes sense as there are a number of opcodes that could take
    metadata.
    
    There are other branches (part of the skeleton) that also need branch
    weights adding.
    
    PR: https://github.com/llvm/llvm-project/pull/143035

--- llvm/lib/Transforms/Vectorize/VPlanTransforms.cpp
+++ llvm/lib/Transforms/Vectorize/VPlanTransforms.cpp
@@ -31,6 +31,7 @@
 #include "llvm/Analysis/LoopInfo.h"
 #include "llvm/Analysis/VectorUtils.h"
 #include "llvm/IR/Intrinsics.h"
+#include "llvm/IR/MDBuilder.h"
 #include "llvm/IR/PatternMatch.h"
 #include "llvm/Support/Casting.h"
 #include "llvm/Support/TypeSize.h"
@@ -3202,4 +3203,26 @@ void VPlanTransforms::narrowInterleaveGroups(VPlan &Plan, ElementCount VF,
   Plan.getVF().replaceAllUsesWith(
       Plan.getOrAddLiveIn(ConstantInt::get(CanIV->getScalarType(), 1)));
   removeDeadRecipes(Plan);
+}
+
+/// Add branch weight metadata, if the \p Plan's middle block is terminated by a
+/// BranchOnCond recipe.
+void VPlanTransforms::addBranchWeightToMiddleTerminator(VPlan &Plan,
+                                                        ElementCount VF) {
+  VPBasicBlock *MiddleVPBB = Plan.getMiddleBlock();
+  auto *MiddleTerm =
+      dyn_cast_or_null<VPInstruction>(MiddleVPBB->getTerminator());
+  // Only add branch metadata if there is a (conditional) terminator.
+  if (!MiddleTerm)
+    return;
+
+  assert(MiddleTerm->getOpcode() == VPInstruction::BranchOnCond &&
+         "must have a BranchOnCond");
+  // Assume that `TripCount % VectorStep ` is equally distributed.
+  unsigned VectorStep = Plan.getUF() * VF.getKnownMinValue();
+  assert(VectorStep > 0 && "trip count should not be zero");
+  MDBuilder MDB(Plan.getScalarHeader()->getIRBasicBlock()->getContext());
+  MDNode *BranchWeights =
+      MDB.createBranchWeights({1, VectorStep - 1}, /*IsExpected=*/false);
+  MiddleTerm->addMetadata(LLVMContext::MD_prof, BranchWeights);
 }
