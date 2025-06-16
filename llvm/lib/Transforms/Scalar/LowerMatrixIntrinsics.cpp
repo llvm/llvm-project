90c2640552fd04bfcc2d3785dac6b0ca28087f30
Jon Roelofs <jonathan_roelofs@apple.com>
2025-06-16 07:52:31 -0700

    review feedback: make reductions have 1x1 shape info

--- llvm/lib/Transforms/Scalar/LowerMatrixIntrinsics.cpp
+++ llvm/lib/Transforms/Scalar/LowerMatrixIntrinsics.cpp
@@ -298,6 +298,25 @@ computeShapeInfoForInst(Instruction *I,
       return OpShape->second;
   }
 
+  if (auto *II = dyn_cast<IntrinsicInst>(I)) {
+    switch (II->getIntrinsicID()) {
+    case Intrinsic::vector_reduce_fadd:
+    case Intrinsic::vector_reduce_fmul:
+    case Intrinsic::vector_reduce_fmax:
+    case Intrinsic::vector_reduce_fmaximum:
+    case Intrinsic::vector_reduce_fmin:
+    case Intrinsic::vector_reduce_fminimum:
+    case Intrinsic::vector_reduce_add:
+    case Intrinsic::vector_reduce_and:
+    case Intrinsic::vector_reduce_mul:
+    case Intrinsic::vector_reduce_or:
+    case Intrinsic::vector_reduce_xor:
+      return ShapeInfo(1, 1);
+    default:
+      break;
+    }
+  }
+
   if (isUniformShape(I)) {
     // Find the first operand that has a known shape and use that.
     for (auto &Op : I->operands()) {
@@ -669,7 +688,31 @@ public:
       case Intrinsic::matrix_transpose:
       case Intrinsic::matrix_column_major_load:
       case Intrinsic::matrix_column_major_store:
+      case Intrinsic::vector_reduce_fmax:
+      case Intrinsic::vector_reduce_fmaximum:
+      case Intrinsic::vector_reduce_fmin:
+      case Intrinsic::vector_reduce_fminimum:
+      case Intrinsic::vector_reduce_add:
+      case Intrinsic::vector_reduce_and:
+      case Intrinsic::vector_reduce_mul:
+      case Intrinsic::vector_reduce_or:
+      case Intrinsic::vector_reduce_xor:
         return true;
+      case Intrinsic::vector_reduce_fadd:
+      case Intrinsic::vector_reduce_fmul: {
+        FastMathFlags FMF = getFastMathFlags(Inst);
+        if (Inst->getType()->isFloatingPointTy() && !FMF.allowReassoc())
+          return false;
+
+        if (match(Inst, m_Intrinsic<Intrinsic::vector_reduce_fadd>(
+                            m_Unless(m_AnyZeroFP()), m_Value())))
+          return false;
+
+        if (match(Inst, m_Intrinsic<Intrinsic::vector_reduce_fmul>(
+                            m_Unless(m_FPOne()), m_Value())))
+          return false;
+        return true;
+      }
       default:
         return isUniformShape(II);
       }
@@ -1206,6 +1249,134 @@ public:
                        Builder);
       return;
     }
+    case Intrinsic::vector_reduce_fadd:
+    case Intrinsic::vector_reduce_fmul:
+    case Intrinsic::vector_reduce_fmax:
+    case Intrinsic::vector_reduce_fmaximum:
+    case Intrinsic::vector_reduce_fmin:
+    case Intrinsic::vector_reduce_fminimum:
+    case Intrinsic::vector_reduce_add:
+    case Intrinsic::vector_reduce_and:
+    case Intrinsic::vector_reduce_mul:
+    case Intrinsic::vector_reduce_or:
+    case Intrinsic::vector_reduce_xor: {
+      IRBuilder<> Builder(Inst);
+
+      FastMathFlags FMF = getFastMathFlags(Inst);
+      Value *Start = nullptr;
+      Value *Op = nullptr;
+      switch (Inst->getIntrinsicID()) {
+      case Intrinsic::vector_reduce_fadd:
+      case Intrinsic::vector_reduce_fmul: {
+        Start = Inst->getOperand(0);
+        Op = Inst->getOperand(1);
+      } break;
+      case Intrinsic::vector_reduce_fmax:
+      case Intrinsic::vector_reduce_fmaximum:
+      case Intrinsic::vector_reduce_fmin:
+      case Intrinsic::vector_reduce_fminimum:
+      case Intrinsic::vector_reduce_add:
+      case Intrinsic::vector_reduce_and:
+      case Intrinsic::vector_reduce_mul:
+      case Intrinsic::vector_reduce_or:
+      case Intrinsic::vector_reduce_xor:
+        Op = Inst->getOperand(0);
+        break;
+      default:
+        llvm_unreachable("unexpected reduction intrinsic");
+      }
+
+      auto *I = Inst2ColumnMatrix.find(Op);
+      assert(I != Inst2ColumnMatrix.end());
+      const MatrixTy &M = I->second;
+
+      auto CreateVReduce = [&](Value *LHS, Value *RHS) {
+        switch (Inst->getIntrinsicID()) {
+        case Intrinsic::vector_reduce_add:
+          return Builder.CreateAdd(LHS, RHS);
+        case Intrinsic::vector_reduce_and:
+          return Builder.CreateAnd(LHS, RHS);
+        case Intrinsic::vector_reduce_fadd:
+          return Builder.CreateFAdd(LHS, RHS);
+        case Intrinsic::vector_reduce_fmax:
+          return Builder.CreateMaximum(LHS, RHS);
+        case Intrinsic::vector_reduce_fmaximum:
+          return Builder.CreateMaximumNum(LHS, RHS);
+        case Intrinsic::vector_reduce_fmin:
+          return Builder.CreateMinimum(LHS, RHS);
+        case Intrinsic::vector_reduce_fminimum:
+          return Builder.CreateMinimumNum(LHS, RHS);
+        case Intrinsic::vector_reduce_fmul:
+          return Builder.CreateFMul(LHS, RHS);
+        case Intrinsic::vector_reduce_mul:
+          return Builder.CreateMul(LHS, RHS);
+        case Intrinsic::vector_reduce_or:
+          return Builder.CreateOr(LHS, RHS);
+        case Intrinsic::vector_reduce_xor:
+          return Builder.CreateXor(LHS, RHS);
+        default:
+          llvm_unreachable("unexpected reduction intrinsic");
+        }
+      };
+
+      Value *ResultV;
+      if (Inst->getIntrinsicID() == Intrinsic::vector_reduce_fadd ||
+          Inst->getIntrinsicID() == Intrinsic::vector_reduce_fmul) {
+        ResultV = Builder.CreateVectorSplat(
+            ElementCount::getFixed(M.getStride()), Start);
+        for (auto &Vector : M.vectors()) {
+          ResultV = CreateVReduce(ResultV, Vector);
+          if (isa<FPMathOperator>(ResultV))
+            if (auto *ResultVI = dyn_cast<Instruction>(ResultV))
+              ResultVI->setFastMathFlags(FMF);
+        }
+      } else {
+        ResultV = M.getVector(0);
+        for (auto &Vector : drop_begin(M.vectors())) {
+          ResultV = CreateVReduce(ResultV, Vector);
+          if (isa<FPMathOperator>(ResultV))
+            if (auto *ResultVI = dyn_cast<Instruction>(ResultV))
+              ResultVI->setFastMathFlags(FMF);
+        }
+      }
+
+      auto CreateHReduce = [&](Value *V) {
+        switch (Inst->getIntrinsicID()) {
+        case Intrinsic::vector_reduce_add:
+          return Builder.CreateAddReduce(V);
+        case Intrinsic::vector_reduce_and:
+          return Builder.CreateAndReduce(V);
+        case Intrinsic::vector_reduce_fadd:
+          return Builder.CreateFAddReduce(Start, V);
+        case Intrinsic::vector_reduce_fmax:
+          return Builder.CreateFPMaxReduce(V);
+        case Intrinsic::vector_reduce_fmaximum:
+          return Builder.CreateFPMaximumReduce(V);
+        case Intrinsic::vector_reduce_fmin:
+          return Builder.CreateFPMinReduce(V);
+        case Intrinsic::vector_reduce_fminimum:
+          return Builder.CreateFPMinimumReduce(V);
+        case Intrinsic::vector_reduce_fmul:
+          return Builder.CreateFMulReduce(Start, V);
+        case Intrinsic::vector_reduce_mul:
+          return Builder.CreateMulReduce(V);
+        case Intrinsic::vector_reduce_or:
+          return Builder.CreateOrReduce(V);
+        case Intrinsic::vector_reduce_xor:
+          return Builder.CreateXorReduce(V);
+        default:
+          llvm_unreachable("unexpected reduction intrinsic");
+        }
+      };
+
+      Value *Result = CreateHReduce(ResultV);
+      if (isa<FPMathOperator>(Result))
+        if (auto *ResultI = dyn_cast<Instruction>(Result))
+          ResultI->setFastMathFlags(FMF);
+      Inst->replaceAllUsesWith(Result);
+      Result->takeName(Inst);
+      finalizeLowering(Inst, {Result}, Builder);
+    } break;
     default:
       llvm_unreachable(
           "only intrinsics supporting shape info should be seen here");
@@ -1416,139 +1587,6 @@ public:
     return Builder.CreateAdd(Sum, Mul);
   }
 
-  bool tryLowerIntrinsic(IntrinsicInst *Inst) {
-    FastMathFlags FMF = getFastMathFlags(Inst);
-    Value *Start = nullptr;
-    Value *Op = nullptr;
-    switch (Inst->getIntrinsicID()) {
-    case Intrinsic::vector_reduce_fadd:
-    case Intrinsic::vector_reduce_fmul: {
-      if (Inst->getType()->isFloatingPointTy() && !FMF.allowReassoc())
-        return false;
-
-      if (match(Inst, m_Intrinsic<Intrinsic::vector_reduce_fadd>(
-                          m_Unless(m_AnyZeroFP()), m_Value())))
-        return false;
-
-      if (match(Inst, m_Intrinsic<Intrinsic::vector_reduce_fmul>(
-                          m_Unless(m_FPOne()), m_Value())))
-        return false;
-
-      Start = Inst->getOperand(0);
-      Op = Inst->getOperand(1);
-    } break;
-    case Intrinsic::vector_reduce_fmax:
-    case Intrinsic::vector_reduce_fmaximum:
-    case Intrinsic::vector_reduce_fmin:
-    case Intrinsic::vector_reduce_fminimum:
-    case Intrinsic::vector_reduce_add:
-    case Intrinsic::vector_reduce_and:
-    case Intrinsic::vector_reduce_mul:
-    case Intrinsic::vector_reduce_or:
-    case Intrinsic::vector_reduce_xor:
-      Op = Inst->getOperand(0);
-      break;
-    default:
-      return false;
-    }
-
-    auto *I = Inst2ColumnMatrix.find(Op);
-    if (I == Inst2ColumnMatrix.end())
-      return false;
-
-    IRBuilder<> Builder(Inst);
-
-    const MatrixTy &M = I->second;
-
-    auto CreateVReduce = [&](Value *LHS, Value *RHS) {
-      switch (Inst->getIntrinsicID()) {
-      case Intrinsic::vector_reduce_add:
-        return Builder.CreateAdd(LHS, RHS);
-      case Intrinsic::vector_reduce_and:
-        return Builder.CreateAnd(LHS, RHS);
-      case Intrinsic::vector_reduce_fadd:
-        return Builder.CreateFAdd(LHS, RHS);
-      case Intrinsic::vector_reduce_fmax:
-        return Builder.CreateMaximum(LHS, RHS);
-      case Intrinsic::vector_reduce_fmaximum:
-        return Builder.CreateMaximumNum(LHS, RHS);
-      case Intrinsic::vector_reduce_fmin:
-        return Builder.CreateMinimum(LHS, RHS);
-      case Intrinsic::vector_reduce_fminimum:
-        return Builder.CreateMinimumNum(LHS, RHS);
-      case Intrinsic::vector_reduce_fmul:
-        return Builder.CreateFMul(LHS, RHS);
-      case Intrinsic::vector_reduce_mul:
-        return Builder.CreateMul(LHS, RHS);
-      case Intrinsic::vector_reduce_or:
-        return Builder.CreateOr(LHS, RHS);
-      case Intrinsic::vector_reduce_xor:
-        return Builder.CreateXor(LHS, RHS);
-      default:
-        llvm_unreachable("unexpected intrinsic");
-      }
-    };
-
-    Value *ResultV;
-    if (Inst->getIntrinsicID() == Intrinsic::vector_reduce_fadd ||
-        Inst->getIntrinsicID() == Intrinsic::vector_reduce_fmul) {
-      ResultV = Builder.CreateVectorSplat(ElementCount::getFixed(M.getStride()),
-                                          Start);
-      for (auto &Vector : M.vectors()) {
-        ResultV = CreateVReduce(ResultV, Vector);
-        if (isa<FPMathOperator>(ResultV))
-          if (auto *ResultVI = dyn_cast<Instruction>(ResultV))
-            ResultVI->setFastMathFlags(FMF);
-      }
-    } else {
-      ResultV = M.getVector(0);
-      for (auto &Vector : drop_begin(M.vectors())) {
-        ResultV = CreateVReduce(ResultV, Vector);
-        if (isa<FPMathOperator>(ResultV))
-          if (auto *ResultVI = dyn_cast<Instruction>(ResultV))
-            ResultVI->setFastMathFlags(FMF);
-      }
-    }
-
-    auto CreateHReduce = [&](Value *V) {
-      switch (Inst->getIntrinsicID()) {
-      case Intrinsic::vector_reduce_add:
-        return Builder.CreateAddReduce(V);
-      case Intrinsic::vector_reduce_and:
-        return Builder.CreateAndReduce(V);
-      case Intrinsic::vector_reduce_fadd:
-        return Builder.CreateFAddReduce(Start, V);
-      case Intrinsic::vector_reduce_fmax:
-        return Builder.CreateFPMaxReduce(V);
-      case Intrinsic::vector_reduce_fmaximum:
-        return Builder.CreateFPMaximumReduce(V);
-      case Intrinsic::vector_reduce_fmin:
-        return Builder.CreateFPMinReduce(V);
-      case Intrinsic::vector_reduce_fminimum:
-        return Builder.CreateFPMinimumReduce(V);
-      case Intrinsic::vector_reduce_fmul:
-        return Builder.CreateFMulReduce(Start, V);
-      case Intrinsic::vector_reduce_mul:
-        return Builder.CreateMulReduce(V);
-      case Intrinsic::vector_reduce_or:
-        return Builder.CreateOrReduce(V);
-      case Intrinsic::vector_reduce_xor:
-        return Builder.CreateXorReduce(V);
-      default:
-        llvm_unreachable("unexpected intrinsic");
-      }
-    };
-
-    Value *Result = CreateHReduce(ResultV);
-    if (isa<FPMathOperator>(Result))
-      if (auto *ResultI = dyn_cast<Instruction>(Result))
-        ResultI->setFastMathFlags(FMF);
-    Inst->replaceAllUsesWith(Result);
-    Result->takeName(Inst);
-    Inst->eraseFromParent();
-    return true;
-  }
-
   /// Cache \p Matrix as result of \p Inst and update the uses of \p Inst. For
   /// users with shape information, there's nothing to do: they will use the
   /// cached value when they are lowered. For other users, \p Matrix is
@@ -1565,10 +1603,6 @@ public:
     for (Use &U : llvm::make_early_inc_range(Inst->uses())) {
       if (ShapeMap.contains(U.getUser()))
         continue;
-
-      if (auto *Intr = dyn_cast<IntrinsicInst>(U.getUser()))
-        if (tryLowerIntrinsic(Intr))
-          continue;
 
       if (!Flattened) {
         Flattened = Matrix.embedInVector(Builder);
