f3021e79fd5a4cab5537f37df2e6010a325d0a7c
Fangrui Song <i@maskray.me>
2025-06-15 23:29:06 -0700

    ARM: Rename ARMMCExpr::VK_ to ARM::S_
    Prepare for removing ARMMCExpr. Adopt the new naming convention (S_
    instead of VK_; the relocation specifier was previously named
    `VariantKind`)) used by most other targets.
    
    Make ARMMCAsmInfo.h include ARMMCExpr.h and change .cpp files to include
    ARMMCAsmInfo.h. We will eventually remove ARMMCExpr.h.

--- llvm/lib/Target/ARM/MCTargetDesc/ARMMCExpr.cpp
+++ llvm/lib/Target/ARM/MCTargetDesc/ARMMCExpr.cpp
@@ -7,6 +7,7 @@
 //===----------------------------------------------------------------------===//
 
 #include "ARMMCExpr.h"
+#include "ARMMCAsmInfo.h"
 #include "llvm/MC/MCAsmInfo.h"
 #include "llvm/MC/MCContext.h"
 #include "llvm/MC/MCStreamer.h"
@@ -22,22 +23,22 @@ const ARMMCExpr *ARMMCExpr::create(Specifier S, const MCExpr *Expr,
 void ARMMCExpr::printImpl(raw_ostream &OS, const MCAsmInfo *MAI) const {
   switch (specifier) {
   default: llvm_unreachable("Invalid kind!");
-  case VK_HI16:
+  case ARM::S_HI16:
     OS << ":upper16:";
     break;
-  case VK_LO16:
+  case ARM::S_LO16:
     OS << ":lower16:";
     break;
-  case VK_HI_8_15:
+  case ARM::S_HI_8_15:
     OS << ":upper8_15:";
     break;
-  case VK_HI_0_7:
+  case ARM::S_HI_0_7:
     OS << ":upper0_7:";
     break;
-  case VK_LO_8_15:
+  case ARM::S_LO_8_15:
     OS << ":lower8_15:";
     break;
-  case VK_LO_0_7:
+  case ARM::S_LO_0_7:
     OS << ":lower0_7:";
     break;
   }
@@ -48,4 +49,30 @@ void ARMMCExpr::printImpl(raw_ostream &OS, const MCAsmInfo *MAI) const {
   MAI->printExpr(OS, *Expr);
   if (Expr->getKind() != MCExpr::SymbolRef)
     OS << ')';
+}
+
+const ARMMCExpr *ARMMCExpr::createUpper16(const MCExpr *Expr, MCContext &Ctx) {
+  return ARMMCExpr::create(ARM::S_HI16, Expr, Ctx);
+}
+
+const ARMMCExpr *ARMMCExpr::createLower16(const MCExpr *Expr, MCContext &Ctx) {
+  return ARMMCExpr::create(ARM::S_LO16, Expr, Ctx);
+}
+
+const ARMMCExpr *ARMMCExpr::createUpper8_15(const MCExpr *Expr,
+                                            MCContext &Ctx) {
+  return ARMMCExpr::create(ARM::S_HI_8_15, Expr, Ctx);
+}
+
+const ARMMCExpr *ARMMCExpr::createUpper0_7(const MCExpr *Expr, MCContext &Ctx) {
+  return ARMMCExpr::create(ARM::S_HI_0_7, Expr, Ctx);
+}
+
+const ARMMCExpr *ARMMCExpr::createLower8_15(const MCExpr *Expr,
+                                            MCContext &Ctx) {
+  return ARMMCExpr::create(ARM::S_LO_8_15, Expr, Ctx);
+}
+
+const ARMMCExpr *ARMMCExpr::createLower0_7(const MCExpr *Expr, MCContext &Ctx) {
+  return ARMMCExpr::create(ARM::S_LO_0_7, Expr, Ctx);
 }
