7004970644e37d543f0968843e3b742be4879d2b
Yingwei Zheng <dtcxzyw2333@gmail.com>
2025-06-16 12:07:47 +0800

    [InstCombine] Propagate FMF from fptrunc when folding `fptrunc fabs(X) -> fabs(fptrunc X)` (#143352)
    Alive2: https://alive2.llvm.org/ce/z/DWV3G3
    fptrunc yields infinity when the input cannot fit in the target type. So
    ninf should be propagated from fptrunc. For other intrinsics, the
    previous check ensures that the result is never an infinity:
    
    https://github.com/llvm/llvm-project/blob/67591bb6e31f41a9fd3a971e1756f629352a5b96/llvm/lib/Transforms/InstCombine/InstCombineCasts.cpp#L1910-L1917
    
    Closes https://github.com/llvm/llvm-project/issues/143122.

--- llvm/lib/Transforms/InstCombine/InstCombineCasts.cpp
+++ llvm/lib/Transforms/InstCombine/InstCombineCasts.cpp
@@ -1917,7 +1917,9 @@ Instruction *InstCombinerImpl::visitFPTrunc(FPTruncInst &FPT) {
       II->getOperandBundlesAsDefs(OpBundles);
       CallInst *NewCI =
           CallInst::Create(Overload, {InnerTrunc}, OpBundles, II->getName());
-      NewCI->copyFastMathFlags(II);
+      // A normal value may be converted to an infinity. It means that we cannot
+      // propagate ninf from the intrinsic. So we propagate FMF from fptrunc.
+      NewCI->copyFastMathFlags(&FPT);
       return NewCI;
     }
     }
