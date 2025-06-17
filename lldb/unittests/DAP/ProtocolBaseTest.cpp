634fd1443afe4a7d8bc8c74e4ec7475b44046ed6
John Harrison <harjohn@google.com>
2025-06-17 13:13:20 -0700

    Fixing test doc comments.

--- lldb/unittests/DAP/ProtocolBaseTest.cpp
+++ lldb/unittests/DAP/ProtocolBaseTest.cpp
@@ -1,4 +1,4 @@
-//===-- ProtocolTypesTest.cpp -----------------------------------*- C++ -*-===//
+//===-- ProtocolBaseTest.cpp ----------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
