a16caf9834ad4001f7410b34ee613230cc3cf43d
Uzair Nawaz <uzairnawaz@google.com>
2025-06-17 20:34:25 +0000

    formatting

--- libc/src/wchar/wcrtomb.h
+++ libc/src/wchar/wcrtomb.h
@@ -9,9 +9,9 @@
 #ifndef LLVM_LIBC_SRC_WCHAR_WCRTOMB_H
 #define LLVM_LIBC_SRC_WCHAR_WCRTOMB_H
 
-#include "hdr/types/wchar_t.h"
 #include "hdr/types/mbstate_t.h"
 #include "hdr/types/size_t.h"
+#include "hdr/types/wchar_t.h"
 #include "src/__support/macros/config.h"
 
 namespace LIBC_NAMESPACE_DECL {
