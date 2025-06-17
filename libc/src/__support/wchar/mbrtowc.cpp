1ac2bff191b894ee64c4595443b55f117ce16909
Sriya Pratipati <sriyap@google.com>
2025-06-17 20:19:30 +0000

    created initial files and internal function

---
+++ libc/src/__support/wchar/mbrtowc.cpp
@@ -0,0 +1,44 @@
+//===-- Implementation for mbrtowc function ---------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "src/__support/wchar/mbrtowc.h"
+#include "hdr/types/wchar_t.h"
+#include "src/__support/common.h"
+#include "src/__support/error_or.h"
+#include "src/__support/macros/config.h"
+#include "src/__support/wchar/character_converter.h"
+#include "src/__support/wchar/mbstate.h"
+
+namespace LIBC_NAMESPACE_DECL {
+namespace internal {
+
+ErrorOr<size_t> mbrtowc(wchar_t *__restrict pwc, const char *__restrict s,
+                        size_t n, mbstate_t *__restrict ps) {
+  CharacterConverter char_conv((internal::mbstate *)ps);
+  if (s == nullptr)
+    return 0;
+  size_t i = 0;
+  // Reading in bytes until we have a complete wc or error
+  for (; i < n & !char_conv.isComplete(); ++i) {
+    int err = char_conv.push(static_cast<char8_t>(s[i]));
+    // Encoding error
+    if (err == -1)
+      return Error(-1);
+  }
+  auto wc = char_conv.pop_utf32();
+  if (wc.has_value()) {
+    *pwc = wc.value();
+    return i;
+  }
+  // Incomplete but potentially valid
+  return Error(-2);
+}
+
+} // namespace internal
+
+} // namespace LIBC_NAMESPACE_DECL
