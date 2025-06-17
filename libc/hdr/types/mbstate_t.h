fc563be2635bb8247e56ad325da674fe639ffb18
Uzair Nawaz <uzairnawaz@google.com>
2025-06-17 20:59:08 +0000

    fix mbstate_t in overlay mode

--- libc/hdr/types/mbstate_t.h
+++ libc/hdr/types/mbstate_t.h
@@ -9,6 +9,14 @@
 #ifndef LLVM_LIBC_HDR_TYPES_MBSTATE_T_H
 #define LLVM_LIBC_HDR_TYPES_MBSTATE_T_H
 
+#ifdef LIBC_FULL_BUILD
+
 #include "include/llvm-libc-types/mbstate_t.h"
+
+#else // Overlay mode
+
+#include "hdr/wchar_overlay.h"
+
+#endif // LLVM_LIBC_FULL_BUILD
 
 #endif // LLVM_LIBC_HDR_TYPES_MBSTATE_T_H
