55207cc1e5502bdaf5d114d109cb5fb71f33e98f
Finn Plummer <finn.c.plum@gmail.com>
2025-06-17 10:24:57 -0700

    [HLSL][RootSignature] Implement `ResourceRange` as an `IntervalMap` (#140957)
    A resource range consists of a closed interval, `[a;b]`, denoting which
    shader registers it is bound to.
    
    For instance:
     - `CBV(b1)`  corresponds to the resource range of `[1;1]`
     - `CBV(b0, numDescriptors = 3)` likewise to `[0;2]`
    
    We want to provide an error diagnostic when there is an overlap in the
    required registers (an overlap in the resource ranges).
    
    The goal of this pr is to implement a structure to model a set of
    resource ranges and provide an api to detect any overlap over a set of
    resource ranges.
    
    `ResourceRange` models this by implementing an `IntervalMap` to denote a
    mapping from an interval of registers back to a resource range. It
    allows for a new `ResourceRange` to be added to the mapping and it will
    report if and what the first overlap is.
    
    For the context of how this will be used in validation of a
    `RootSignatureDecl` please see the proceeding pull request here:
    https://github.com/llvm/llvm-project/pull/140962.
    
    - Implements `ResourceRange` as an `IntervalMap`
    - Adds unit testing of the various `insert` scenarios
    
    Note: it was also considered to implement this as an `IntervalTree`,
    this would allow reporting of a diagnostic for each overlap that is
    encountered, as opposed to just the first. However, error generation of
    just reporting the first error is already rather verbose, and adding the
    additional diagnostics only made this worse.
    
    Part 1 of https://github.com/llvm/llvm-project/issues/129942

--- llvm/lib/Frontend/HLSL/HLSLRootSignatureUtils.cpp
+++ llvm/lib/Frontend/HLSL/HLSLRootSignatureUtils.cpp
@@ -355,6 +355,67 @@ MDNode *MetadataBuilder::BuildStaticSampler(const StaticSampler &Sampler) {
   return MDNode::get(Ctx, Operands);
 }
 
+std::optional<const RangeInfo *>
+ResourceRange::getOverlapping(const RangeInfo &Info) const {
+  MapT::const_iterator Interval = Intervals.find(Info.LowerBound);
+  if (!Interval.valid() || Info.UpperBound < Interval.start())
+    return std::nullopt;
+  return Interval.value();
+}
+
+const RangeInfo *ResourceRange::lookup(uint32_t X) const {
+  return Intervals.lookup(X, nullptr);
+}
+
+std::optional<const RangeInfo *> ResourceRange::insert(const RangeInfo &Info) {
+  uint32_t LowerBound = Info.LowerBound;
+  uint32_t UpperBound = Info.UpperBound;
+
+  std::optional<const RangeInfo *> Res = std::nullopt;
+  MapT::iterator Interval = Intervals.begin();
+
+  while (true) {
+    if (UpperBound < LowerBound)
+      break;
+
+    Interval.advanceTo(LowerBound);
+    if (!Interval.valid()) // No interval found
+      break;
+
+    // Let Interval = [x;y] and [LowerBound;UpperBound] = [a;b] and note that
+    // a <= y implicitly from Intervals.find(LowerBound)
+    if (UpperBound < Interval.start())
+      break; // found interval does not overlap with inserted one
+
+    if (!Res.has_value()) // Update to be the first found intersection
+      Res = Interval.value();
+
+    if (Interval.start() <= LowerBound && UpperBound <= Interval.stop()) {
+      // x <= a <= b <= y implies that [a;b] is covered by [x;y]
+      //  -> so we don't need to insert this, report an overlap
+      return Res;
+    } else if (LowerBound <= Interval.start() &&
+               Interval.stop() <= UpperBound) {
+      // a <= x <= y <= b implies that [x;y] is covered by [a;b]
+      //  -> so remove the existing interval that we will cover with the
+      //  overwrite
+      Interval.erase();
+    } else if (LowerBound < Interval.start() && UpperBound <= Interval.stop()) {
+      // a < x <= b <= y implies that [a; x] is not covered but [x;b] is
+      //  -> so set b = x - 1 such that [a;x-1] is now the interval to insert
+      UpperBound = Interval.start() - 1;
+    } else if (Interval.start() <= LowerBound && Interval.stop() < UpperBound) {
+      // a < x <= b <= y implies that [y; b] is not covered but [a;y] is
+      //  -> so set a = y + 1 such that [y+1;b] is now the interval to insert
+      LowerBound = Interval.stop() + 1;
+    }
+  }
+
+  assert(LowerBound <= UpperBound && "Attempting to insert an empty interval");
+  Intervals.insert(LowerBound, UpperBound, &Info);
+  return Res;
+}
+
 } // namespace rootsig
 } // namespace hlsl
 } // namespace llvm
