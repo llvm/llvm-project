5dbfe279c3813e8aea29203585fc058b05d9935a
Tom Eccles <tom.eccles@arm.com>
2025-06-17 17:35:49 +0000

    Allow do concurrent with LOOP construct
    There's some obscure language in OpenMP 6.0:
    
    > If the collapsed loop is a DO CONCURRENT loop, neither the
    > data-sharing attribute clauses nor the collapse clause may be specified.
    
    From the surrounding context, I think "collapsed loop" just means the
    loop that the LOOP construct applies to. So I will interpret this to
    mean that DO CONCURRENT can only be used with the LOOP construct if it
    does not contain the COLLAPSE clause.
    
    This also fixes a bug where the associated clause was never cleared
    after it was set.

--- flang/test/Semantics/OpenMP/do-concurrent-collapse.f90
+++ flang/test/Semantics/OpenMP/do-concurrent-collapse.f90
@@ -16,4 +16,24 @@ do i = 1, 1
     print *, j
   end do
 end do
+
+!$omp parallel do
+! ERROR: DO CONCURRENT loops cannot form part of a loop nest.
+do concurrent (j = 1:2)
+  print *, j
+end do
+
+!$omp loop
+! Do concurrent is explicitly allowed inside of omp loop
+do concurrent (j = 1:2)
+  print *, j
+end do
+
+! ERROR: DO CONCURRENT loops cannot be used with the COLLAPSE clause.
+!$omp loop collapse(2)
+do i = 1, 1
+  do concurrent (j = 1:2)
+    print *, j
+  end do
+end do
 end
