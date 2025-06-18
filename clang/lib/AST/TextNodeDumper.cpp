015936646ce5e17ddb8f2191610784864109ae48
Finn Plummer <canadienfinn@gmail.com>
2025-06-18 22:48:58 +0000

    update serialization of root signature to output version

--- clang/lib/AST/TextNodeDumper.cpp
+++ clang/lib/AST/TextNodeDumper.cpp
@@ -3041,6 +3041,16 @@ void TextNodeDumper::VisitHLSLBufferDecl(const HLSLBufferDecl *D) {
 void TextNodeDumper::VisitHLSLRootSignatureDecl(
     const HLSLRootSignatureDecl *D) {
   dumpName(D);
+  OS << " version: ";
+  switch (D->getVersion()) {
+  case llvm::dxil::RootSignatureVersion::rootsig_1_0:
+    OS << "1.0";
+    break;
+  case llvm::dxil::RootSignatureVersion::rootsig_1_1:
+    OS << "1.1";
+    break;
+  }
+  OS << ", ";
   llvm::hlsl::rootsig::dumpRootElements(OS, D->getRootElements());
 }
 
