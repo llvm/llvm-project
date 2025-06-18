d008d5c9c0c6c4153f2c327f48f5a99a4b3d9edd
Ron Lieberman <ron.lieberman@amd.com>
2025-06-18 11:48:57 -0500

    Regen lits

--- flang/test/Semantics/OpenMP/nested-teams.f90
+++ flang/test/Semantics/OpenMP/nested-teams.f90
@@ -1,4 +1,4 @@
-! RUN: %python %S/../test_errors.py %s %flang -fopenmp
+! RUN: %python %S/../test_errors.py %s %flang -fopenmp -fopenmp-version=50
 
 ! OpenMP Version 5.0
 ! Check OpenMP construct validity for the following directives:
