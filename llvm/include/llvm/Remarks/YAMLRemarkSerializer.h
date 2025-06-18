fafe6e9e976c3670908eb05699c68d73013287ca
Tobias Stadler <mail@stadler-tobias.de>
2025-06-18 14:25:41 +0100

    [Remarks] Remove yaml-strtab format (#144527)
    Background: The yaml-strtab format looks just like the yaml format,
    except that the values in the key/value pairs of the remarks are
    deduplicated and replaced by indices into a string table (see removed
    test cases for examples). The motivation behind this format was to
    reduce size of the remarks files. However, it was quickly superseded by
    the bitstream format.
    
    Therefore, remove the yaml-strtab format, as it doesn't have a good
    usecase anymore:
      - It isn't particularly efficient
      - It isn't human-readable
      - It isn't straightforward to parse in external tools that can't use the
    remarks library. We don't even support it in opt-viewer.
    
    llvm-remarkutil is also missing options to parse/convert yaml-strtab, so
    the chance that anyone is actually using this format is low.

--- llvm/include/llvm/Remarks/YAMLRemarkSerializer.h
+++ llvm/include/llvm/Remarks/YAMLRemarkSerializer.h
@@ -63,47 +63,6 @@ struct LLVM_ABI YAMLMetaSerializer : public MetaSerializer {
   void emit() override;
 };
 
-/// Serialize the remarks to YAML using a string table. An remark entry looks
-/// like the regular YAML remark but instead of string entries it's using
-/// numbers that map to an index in the string table.
-struct LLVM_ABI YAMLStrTabRemarkSerializer : public YAMLRemarkSerializer {
-  /// Wether we already emitted the metadata in standalone mode.
-  /// This should be set to true after the first invocation of `emit`.
-  bool DidEmitMeta = false;
-
-  YAMLStrTabRemarkSerializer(raw_ostream &OS, SerializerMode Mode)
-      : YAMLRemarkSerializer(Format::YAMLStrTab, OS, Mode) {
-    // We always need a string table for this type of serializer.
-    StrTab.emplace();
-  }
-  YAMLStrTabRemarkSerializer(raw_ostream &OS, SerializerMode Mode,
-                             StringTable StrTab)
-      : YAMLRemarkSerializer(Format::YAMLStrTab, OS, Mode, std::move(StrTab)) {}
-
-  /// Override to emit the metadata if necessary.
-  void emit(const Remark &Remark) override;
-
-  std::unique_ptr<MetaSerializer> metaSerializer(
-      raw_ostream &OS,
-      std::optional<StringRef> ExternalFilename = std::nullopt) override;
-
-  static bool classof(const RemarkSerializer *S) {
-    return S->SerializerFormat == Format::YAMLStrTab;
-  }
-};
-
-struct LLVM_ABI YAMLStrTabMetaSerializer : public YAMLMetaSerializer {
-  /// The string table is part of the metadata.
-  const StringTable &StrTab;
-
-  YAMLStrTabMetaSerializer(raw_ostream &OS,
-                           std::optional<StringRef> ExternalFilename,
-                           const StringTable &StrTab)
-      : YAMLMetaSerializer(OS, ExternalFilename), StrTab(StrTab) {}
-
-  void emit() override;
-};
-
 } // end namespace remarks
 } // end namespace llvm
 
