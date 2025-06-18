61c8b31cb57d88263484c5509eb05846825dde98
Scott Linder <Scott.Linder@amd.com>
2025-06-18 23:01:32 +0000

    Reapply "[DebugInfo][RemoveDIs] Remove a swathe of debug-intrinsic code (#144389)"
    This reverts commit 9767ba624c6557c9afce29b91c44eb8c827cce55.

--- llvm/lib/Transforms/Scalar/LICM.cpp
+++ llvm/lib/Transforms/Scalar/LICM.cpp
@@ -1197,10 +1197,6 @@ bool llvm::canSinkOrHoistInst(Instruction &I, AAResults *AA, DominatorTree *DT,
 
     return !Invalidated;
   } else if (CallInst *CI = dyn_cast<CallInst>(&I)) {
-    // Don't sink or hoist dbg info; it's legal, but not useful.
-    if (isa<DbgInfoIntrinsic>(I))
-      return false;
-
     // Don't sink calls which can throw.
     if (CI->mayThrow())
       return false;
