a10e50ad76963f65d3b84ff152159755a7efdcfe
Akash Banerjee <Akash.Banerjee@amd.com>
2025-06-19 17:38:09 +0100

    Added license to header.

--- mlir/include/mlir/Conversion/ComplexToROCDL/ComplexToROCDL.h
+++ mlir/include/mlir/Conversion/ComplexToROCDL/ComplexToROCDL.h
@@ -1,3 +1,11 @@
+//===-- ComplexToROCDL.h - conversion from Complex to ROCDL calls ---------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
 #ifndef MLIR_CONVERSION_COMPLEXTOROCDL_COMPLEXTOROCDL_H_
 #define MLIR_CONVERSION_COMPLEXTOROCDL_COMPLEXTOROCDL_H_
 
