35bc469b086045f48c7e3b10895f2f9bd7df4ca9
MaheshRavishankar <mahesh.ravishankar@gmail.com>
2025-06-18 17:05:43 -0700

    [mlir][TilingInterface] Handle multi operand consumer fusion.
    For consumer fusion cases of this form
    
    ```
    %0:2 = scf.forall .. shared_outs(%arg0 = ..., %arg0 = ...) {
    
      tensor.parallel_insert_slice ... into %arg0
      tensor.parallel_insert_slice ... into %arg1
    }
    %1 = linalg.generic ... ins(%0#0, %0#1)
    ```
    
    the current consumer fusion that handles one slice at a time cannot
    fuse the consumer into the loop, since fusing along one slice will
    create and SSA violation on the other use from the `scf.forall`. The
    solution is to allow consumer fusion to allow considering multiple
    slices at once. This PR changes the `TilingInterface` methods related
    to consumer fusion, i.e.
    
    - `getTiledImplementationFromOperandTile`
    - `getIterationDomainFromOperandTile`
    
    to allow fusion while considering multiple operands. It is upto the
    `TilingInterface` implementation to return an error if a list of tiles
    of the operands cannot result in a consistent implementation of the
    tiled operation.
    
    The Linalg operation implementation of `TilingInterface` has been
    modified to account for these changes and allow cases where operand
    tiles that can result in a consistent tiling implementation are handled.
    
    Signed-off-by: MaheshRavishankar <mahesh.ravishankar@gmail.com>

--- mlir/lib/Dialect/Tensor/Transforms/SwapExtractSliceWithProducerPatterns.cpp
+++ mlir/lib/Dialect/Tensor/Transforms/SwapExtractSliceWithProducerPatterns.cpp
@@ -39,21 +39,48 @@ FailureOr<TilingResult> tensor::replaceExtractSliceWithTiledProducer(
   return *tiledResult;
 }
 
-FailureOr<TilingResult> tensor::replaceInsertSliceWithTiledConsumer(
-    OpBuilder &builder, OffsetSizeAndStrideOpInterface sliceOp,
-    OpOperand &consumer) {
-  auto consumerOp = dyn_cast<TilingInterface>(consumer.getOwner());
+FailureOr<TilingResult> tensor::replaceInsertSlicesWithTiledConsumer(
+    OpBuilder &builder, ArrayRef<tensor::InsertSliceOp> sliceOps,
+    ArrayRef<OpOperand *> consumerOperands) {
+  if (sliceOps.empty()) {
+    return emitError(builder.getUnknownLoc(),
+                     "expected candidate slices list to be non-empty");
+  }
+  if (sliceOps.size() != consumerOperands.size()) {
+    return sliceOps.front()->emitOpError(
+        "expected as many operands as the number of slices passed");
+  }
+  auto consumerOp =
+      dyn_cast<TilingInterface>(consumerOperands.front()->getOwner());
   if (!consumerOp)
     return failure();
+  for (auto opOperand : consumerOperands.drop_front()) {
+    if (opOperand->getOwner() != consumerOp) {
+      return consumerOp->emitOpError(
+          "expected all consumer operands to be from the same operation");
+    }
+  }
 
-  // `TilingInterface` currently only supports strides being 1.
-  if (!llvm::all_of(sliceOp.getMixedStrides(), isOneInteger))
-    return failure();
+  auto consumerOperandNums = llvm::map_to_vector(
+      consumerOperands, [](OpOperand *opOperand) -> unsigned {
+        return opOperand->getOperandNumber();
+      });
+  SmallVector<SmallVector<OpFoldResult>> allOffsets;
+  SmallVector<SmallVector<OpFoldResult>> allSizes;
+  for (auto sliceOp : sliceOps) {
 
+    // `TilingInterface` currently only supports strides being 1.
+    if (!llvm::all_of(sliceOp.getMixedStrides(), isOneInteger))
+      return failure();
+
+    SmallVector<OpFoldResult> offsets = sliceOp.getMixedOffsets();
+    SmallVector<OpFoldResult> sizes = sliceOp.getMixedSizes();
+    allOffsets.emplace_back(std::move(offsets));
+    allSizes.emplace_back(std::move(sizes));
+  }
   FailureOr<TilingResult> tiledResult =
-      consumerOp.getTiledImplementationFromOperandTile(
-          builder, consumer.getOperandNumber(), sliceOp.getMixedOffsets(),
-          sliceOp.getMixedSizes());
+      consumerOp.getTiledImplementationFromOperandTiles(
+          builder, consumerOperandNums, allOffsets, allSizes);
   if (failed(tiledResult))
     return failure();
 
