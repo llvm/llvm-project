5144a9159c8d029d7094638c937bf57a9b368d55
Caroline Tice <cmtice@google.com>
2025-06-19 23:14:08 -0700

    Add fake smart pointer test.
    Test synthetic format provider similar to LibCxx smart pointers,
    but independent of actual smart pointer implementation (so not brittle test).

---
+++ lldb/test/API/functionalities/data-formatter/fake-smart-ptr/TestFakeSmartPtrs.py
@@ -0,0 +1,33 @@
+"""
+Test code that should work on smart pointers, but make it impl independent.
+"""
+
+import lldb
+from lldbsuite.test.decorators import *
+from lldbsuite.test.lldbtest import *
+from lldbsuite.test import lldbutil
+
+
+class FakeSmartPtrDataFormatterTestCase(TestBase):
+    # If your test case doesn't stress debug info, then
+    # set this to true.  That way it won't be run once for
+    # each debug info format.
+    NO_DEBUG_INFO_TESTCASE = True
+
+    def test_frame_var(self):
+        self.build()
+        lldbutil.run_to_source_breakpoint(self, "Set a breakpoint here",
+                                          lldb.SBFileSpec("main.cpp"))
+
+        self.runCmd("settings set target.experimental.use-DIL true")
+        self.runCmd("script from smartPtrSynthProvider import *")
+        self.runCmd("type synth add -l smartPtrSynthProvider smart_ptr")
+
+        self.expect_var_path("ptr_node->value", value="1")
+        self.expect_var_path("ptr_node->next->value", value="2")
+        self.expect_var_path("(*ptr_node).value", value="1")
+        self.expect_var_path("(*(*ptr_node).next).value", value="2")
+
+        self.expect_var_path("ptr_node.__ptr_", type="NodeS *")
+        self.expect_var_path("ptr_node.__ptr_->value", value="1")
+        self.expect_var_path("ptr_node.__ptr_->next.__ptr_->value", value="2")
---
+++ lldb/test/API/functionalities/data-formatter/fake-smart-ptr/smartPtrSynthProvider.py
@@ -0,0 +1,31 @@
+import lldb
+
+class smartPtrSynthProvider:
+  def __init__(self, valobj, dict):
+    self.valobj = valobj
+
+  def num_children(self):
+    return 1
+
+  def get_child_at_index(self, index):
+    if index == 0:
+      return self.valobj.GetChildMemberWithName("__ptr_")
+    if index == 1:
+      internal_child = self.valobj.GetChildMemberWithName("__ptr_")
+      if not internal_child:
+        return None
+      value_type = internal_child.GetType().GetPointerType()
+      cast_ptr_sp = internal_child.Cast(value_type)
+      value = internal_child.Dereference()
+      return value
+    return None
+
+  def get_child_index(self, name):
+    if name == "__ptr_":
+      return 0
+    if name == "$$dereference$$":
+      return 1
+    return -1
+
+  def update(self):
+    return True
