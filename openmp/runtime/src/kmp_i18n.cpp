9ed39d4cc796ed996d981ffe0f8a45e9a702ebef
Brad Smith <brad@comstyle.com>
2025-06-22 07:45:18 -0400

    [OpenMP] Simplify GNU strerror_r check for Android
    Android 6 is the baseline so simplify the Android check.

--- openmp/runtime/src/kmp_i18n.cpp
+++ openmp/runtime/src/kmp_i18n.cpp
@@ -708,9 +708,7 @@ static char *sys_error(int err) {
      int    strerror_r( int, char *, size_t );  // XSI version
   */
 
-#if (defined(__GLIBC__) && defined(_GNU_SOURCE)) ||                            \
-    (defined(__BIONIC__) && defined(_GNU_SOURCE) &&                            \
-     __ANDROID_API__ >= __ANDROID_API_M__)
+#if (defined(__GLIBC__) || defined(__BIONIC__)) && defined(_GNU_SOURCE)
   // GNU version of strerror_r.
 
   char buffer[2048];
