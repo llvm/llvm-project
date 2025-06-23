d37325ea9536c965278b9c9ee56126996c2b49e1
S. VenkataKeerthy <31350914+svkeerthy@users.noreply.github.com>
2025-06-24 00:48:16 +0200

    Revert "[MLGO][IR2Vec] Integrating IR2Vec with MLInliner (#143479)" (#145418)
    This reverts commit af2c06ecd610735dfa5d236c6d5f109e4f2334e6 as it
    causes failure of lit test (Transforms/Inline/ML/interactive-mode.ll)

--- llvm/include/llvm/Analysis/InlineAdvisor.h
+++ llvm/include/llvm/Analysis/InlineAdvisor.h
@@ -331,10 +331,6 @@ public:
   };
 
   Result run(Module &M, ModuleAnalysisManager &MAM) { return Result(M, MAM); }
-
-private:
-  static bool initializeIR2VecVocabIfRequested(Module &M,
-                                               ModuleAnalysisManager &MAM);
 };
 
 /// Printer pass for the InlineAdvisorAnalysis results.
