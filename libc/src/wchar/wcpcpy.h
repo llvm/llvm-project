c1aecc5d3fae2e43e820d6addbe5f1db5ec32ecc
Sriya Pratipati <sriyap@google.com>
2025-06-23 23:20:09 +0000

    [libc] wcpncpy implementation
    Implented wcpncpy and tests.

--- libc/src/wchar/wcpcpy.h
+++ libc/src/wchar/wcpcpy.h
@@ -1,4 +1,4 @@
-//===-- Implementation header for wcpcpy ---------------------------------===//
+//===-- Implementation header for wcpcpy ----------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
