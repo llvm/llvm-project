1965863875cbdeff8bcbf719c34c96b6a10abb1d
Rahman Lavaee <rahmanl@google.com>
2025-06-23 09:25:14 -0700

    [SHT_LLVM_BB_ADDR_MAP] Encode and decode callsite offsets in a newly-introduced SHT_LLVM_BB_ADDR_MAP version. (#144426)
    Recently, we have been looking at some optimizations targeting
    individual calls. In particular, we plan to extend the address mapping
    technique to map to individual callsites. For example, in this piece of
    code for a basic blocks:
    
    ```
    <BB>:
    1200:    lea 0x1(%rcx), %rdx
    1204:    callq foo
    1209:    cmpq 0x10, %rdx
    120d:    ja  L1
    ```
    
    We want to emit 0x9 as the call site offset for `callq foo` (the offset
    from the block entry to right after the call), so that we know if a
    sampled address is before the call or after.
    
    This PR implements the decode/encode/emit capability. The Codegen change
    will be implemented in a later PR.

--- llvm/lib/CodeGen/AsmPrinter/AsmPrinter.cpp
+++ llvm/lib/CodeGen/AsmPrinter/AsmPrinter.cpp
@@ -1426,9 +1426,12 @@ getBBAddrMapFeature(const MachineFunction &MF, int NumMBBSectionRanges) {
         "BB entries info is required for BBFreq and BrProb "
         "features");
   }
-  return {FuncEntryCountEnabled, BBFreqEnabled, BrProbEnabled,
+  return {FuncEntryCountEnabled,
+          BBFreqEnabled,
+          BrProbEnabled,
           MF.hasBBSections() && NumMBBSectionRanges > 1,
-          static_cast<bool>(BBAddrMapSkipEmitBBEntries)};
+          static_cast<bool>(BBAddrMapSkipEmitBBEntries),
+          false};
 }
 
 void AsmPrinter::emitBBAddrMapSection(const MachineFunction &MF) {
