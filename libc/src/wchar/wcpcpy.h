2be5bd3c0d256ed36e40d31e172df2059eee01c2
Sriya Pratipati <sriyap@google.com>
2025-06-23 21:42:49 +0000

    created function declaration

--- libc/src/wchar/wcpcpy.h
+++ libc/src/wchar/wcpcpy.h
@@ -1,4 +1,4 @@
-//===-- Implementation header for wcpcpy ----------------------------------===//
+//===-- Implementation header for wcpncpy ---------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,16 +6,18 @@
 //
 //===----------------------------------------------------------------------===//
 
-#ifndef LLVM_LIBC_SRC_WCHAR_WCPCPY_H
-#define LLVM_LIBC_SRC_WCHAR_WCPCPY_H
+#ifndef LLVM_LIBC_SRC_WCHAR_WCPNCPY_H
+#define LLVM_LIBC_SRC_WCHAR_WCPNCPY_H
 
+#include "hdr/types/size_t.h"
 #include "hdr/types/wchar_t.h"
 #include "src/__support/macros/config.h"
 
 namespace LIBC_NAMESPACE_DECL {
 
-wchar_t *wcpcpy(wchar_t *__restrict ws1, const wchar_t *__restrict ws2);
+wchar_t *wcpncpy(wchar_t *__restrict ws1, const wchar_t *__restrict ws2,
+                 size_t n);
 
 } // namespace LIBC_NAMESPACE_DECL
 
-#endif // LLVM_LIBC_SRC_WCHAR_WCPCPY_H
+#endif // LLVM_LIBC_SRC_WCHAR_WCPNsCPY_H
