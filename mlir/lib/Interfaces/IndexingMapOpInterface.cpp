6c53f214e7910e6dc9114469db1f327a353b4257
Nicolas Vasilache <nico.vasilache@amd.com>
2025-06-23 15:48:11 +0200

    [mlir][Interface] Factor out common IndexingMapOpInterface behavior in a new generic interface

---
+++ mlir/lib/Interfaces/IndexingMapOpInterface.cpp
@@ -0,0 +1,15 @@
+//===- IndexingMapOpInterface.cpp -- IndexingMapOpInterface impl ----------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "mlir/Interfaces/IndexingMapOpInterface.h"
+
+using namespace mlir;
+
+namespace mlir {
+#include "mlir/Interfaces/IndexingMapOpInterface.cpp.inc"
+} // namespace mlir
