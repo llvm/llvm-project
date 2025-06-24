e42907f0aba11dff2292ec1f4cb2b91c815e7a8f
Amirhossein Pashaeehir <amirhosseinp@google.com>
2025-06-24 22:13:45 +0000

    Add file headers

--- llvm/include/llvm/UnwindInfoChecker/UnwindInfoHistory.h
+++ llvm/include/llvm/UnwindInfoChecker/UnwindInfoHistory.h
@@ -1,3 +1,15 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+///
+/// This file declares UnwindInfoHistory class.
+///
+//===----------------------------------------------------------------------===//
+
 #ifndef LLVM_UNWINDINFOCHECKER_UNWINDINFOHISTORY_H
 #define LLVM_UNWINDINFOCHECKER_UNWINDINFOHISTORY_H
 
