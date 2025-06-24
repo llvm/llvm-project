21f31c6f7460902a33783ca742f9bd36bf7400e8
Sriya Pratipati <sriyap@google.com>
2025-06-24 22:36:46 +0000

    [libc] Implemented wcsnlen
    Implemented wcsnlen and tests for the function.

---
+++ libc/src/wchar/wcsnlen.cpp
@@ -0,0 +1,24 @@
+//===-- Implementation of wcsnlen -----------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "src/wchar/wcsnlen.h"
+
+#include "hdr/types/size_t.h"
+#include "hdr/types/wchar_t.h"
+#include "src/__support/common.h"
+#include "src/__support/macros/config.h"
+#include "src/string/string_utils.h" // string_length_trivial
+
+namespace LIBC_NAMESPACE_DECL {
+
+LLVM_LIBC_FUNCTION(size_t, wcsnlen, (const wchar_t *src, size_t maxlen)) {
+  size_t temp = internal::string_length(src);
+  return temp > maxlen ? maxlen : temp;
+}
+
+} // namespace LIBC_NAMESPACE_DECL
