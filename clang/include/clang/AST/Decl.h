e93a0d0d1ed790173e23247fa2833cdac30b8268
Finn Plummer <finn.c.plum@gmail.com>
2025-06-24 16:21:24 -0700

    [HLSL][RootSignature] Add `fdx-rootsignature-version` option to specify root signature version (#144813)
    This pr provides the ability to specify the root signature version as a
    compiler option and to retain this in the root signature decl.
    
    It also updates the methods to serialize the version when dumping the
    declaration and to output the version when generating the metadata.
    
    - Update `DXContainer.hI` to define the root signature versions
    - Update `Options.td` and `LangOpts.h` to define the
    `fdx-rootsignature-version` compiler option
    - Update `Options.td` to provide an alias `force-rootsig-ver` in
    clang-dxc
    - Update `Decl.[h|cpp]` and `SeamHLSL.cpp` so that `RootSignatureDecl`
    will retain its version type
    - Updates `CGHLSLRuntime.cpp` to generate the extra metadata field
    - Add tests to illustrate
    
    Resolves https://github.com/llvm/llvm-project/issues/126557.
    
    Note: this does not implement validation based on versioning.
    https://github.com/llvm/llvm-project/issues/129940 is required to
    retrieve the version and use it for validations.

--- clang/include/clang/AST/Decl.h
+++ clang/include/clang/AST/Decl.h
@@ -41,6 +41,7 @@
 #include "llvm/ADT/PointerUnion.h"
 #include "llvm/ADT/StringRef.h"
 #include "llvm/ADT/iterator_range.h"
+#include "llvm/BinaryFormat/DXContainer.h"
 #include "llvm/Frontend/HLSL/HLSLRootSignature.h"
 #include "llvm/Support/Casting.h"
 #include "llvm/Support/Compiler.h"
@@ -5181,6 +5182,8 @@ class HLSLRootSignatureDecl final
                                     llvm::hlsl::rootsig::RootElement> {
   friend TrailingObjects;
 
+  llvm::dxbc::RootSignatureVersion Version;
+
   unsigned NumElems;
 
   llvm::hlsl::rootsig::RootElement *getElems() { return getTrailingObjects(); }
@@ -5190,15 +5193,19 @@ class HLSLRootSignatureDecl final
   }
 
   HLSLRootSignatureDecl(DeclContext *DC, SourceLocation Loc, IdentifierInfo *ID,
+                        llvm::dxbc::RootSignatureVersion Version,
                         unsigned NumElems);
 
 public:
   static HLSLRootSignatureDecl *
   Create(ASTContext &C, DeclContext *DC, SourceLocation Loc, IdentifierInfo *ID,
+         llvm::dxbc::RootSignatureVersion Version,
          ArrayRef<llvm::hlsl::rootsig::RootElement> RootElements);
 
   static HLSLRootSignatureDecl *CreateDeserialized(ASTContext &C,
                                                    GlobalDeclID ID);
+
+  llvm::dxbc::RootSignatureVersion getVersion() const { return Version; }
 
   ArrayRef<llvm::hlsl::rootsig::RootElement> getRootElements() const {
     return {getElems(), NumElems};
