ac69bb38a1628d6a27998122b7fe30de976be032
Abhishek Kaushik <abhishek.kaushik@intel.com>
2025-06-24 14:55:08 +0530

    [DAG] Add `SDPatternMatch::m_Load`

--- llvm/include/llvm/CodeGen/SDPatternMatch.h
+++ llvm/include/llvm/CodeGen/SDPatternMatch.h
@@ -515,6 +515,12 @@ m_VSelect(const T0_P &Cond, const T1_P &T, const T2_P &F) {
 
 template <typename T0_P, typename T1_P, typename T2_P>
 inline TernaryOpc_match<T0_P, T1_P, T2_P>
+m_Load(const T0_P &Ch, const T1_P &Ptr, const T2_P &Offset) {
+  return TernaryOpc_match<T0_P, T1_P, T2_P>(ISD::LOAD, Ch, Ptr, Offset);
+}
+
+template <typename T0_P, typename T1_P, typename T2_P>
+inline TernaryOpc_match<T0_P, T1_P, T2_P>
 m_InsertElt(const T0_P &Vec, const T1_P &Val, const T2_P &Idx) {
   return TernaryOpc_match<T0_P, T1_P, T2_P>(ISD::INSERT_VECTOR_ELT, Vec, Val,
                                             Idx);
