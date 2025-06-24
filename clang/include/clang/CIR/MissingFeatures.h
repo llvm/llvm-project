f3927e3fd07cad46f183e434129f6dc91cb89e3e
Andy Kaylor <akaylor@nvidia.com>
2025-06-24 09:53:54 -0700

    [CIR] Restore the underscore in dso_local
    The CIR handling of `dso_local` for globals was upstreamed without the
    underscore, making it inconsistent with the incubator and LLVM IR. This
    change restores the underscore.

--- clang/include/clang/CIR/MissingFeatures.h
+++ clang/include/clang/CIR/MissingFeatures.h
@@ -74,7 +74,7 @@ struct MissingFeatures {
   static bool opFuncOpenCLKernelMetadata() { return false; }
   static bool opFuncCallingConv() { return false; }
   static bool opFuncExtraAttrs() { return false; }
-  static bool opFuncDsolocal() { return false; }
+  static bool opFuncDsoLocal() { return false; }
   static bool opFuncLinkage() { return false; }
   static bool opFuncVisibility() { return false; }
   static bool opFuncNoProto() { return false; }
