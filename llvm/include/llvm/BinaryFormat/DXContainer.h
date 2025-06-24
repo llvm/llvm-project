371f1bf99975232bd9f725983247dcf8c28b9d92
Finn Plummer <finn.c.plum@gmail.com>
2025-06-24 16:21:24 -0700

    [HLSL][RootSignature] Add `fdx-rootsignature-version` option to specify root signature version (#144813)
    This pr provides the ability to specify the root signature version as a
    compiler option and to retain this in the root signature decl.
    
    It also updates the methods to serialize the version when dumping the
    declaration and to output the version when generating the metadata.
    
    - Update `DXContainer.hI` to define the root signature versions
    - Update `Options.td` and `LangOpts.h` to define the
    `fdx-rootsignature-version` compiler option
    - Update `Options.td` to provide an alias `force-rootsig-ver` in
    clang-dxc
    - Update `Decl.[h|cpp]` and `SeamHLSL.cpp` so that `RootSignatureDecl`
    will retain its version type
    - Updates `CGHLSLRuntime.cpp` to generate the extra metadata field
    - Add tests to illustrate
    
    Resolves https://github.com/llvm/llvm-project/issues/126557.
    
    Note: this does not implement validation based on versioning.
    https://github.com/llvm/llvm-project/issues/129940 is required to
    retrieve the version and use it for validations.

--- llvm/include/llvm/BinaryFormat/DXContainer.h
+++ llvm/include/llvm/BinaryFormat/DXContainer.h
@@ -759,6 +759,12 @@ struct DescriptorRange {
 } // namespace v2
 } // namespace RTS0
 
+// D3D_ROOT_SIGNATURE_VERSION
+enum class RootSignatureVersion {
+  V1_0 = 0x1,
+  V1_1 = 0x2,
+};
+
 } // namespace dxbc
 } // namespace llvm
 
