2aedde06875e9a98aa2bdd8a39bbc90db296bbef
changkhothuychung <nhat7203@gmail.com>
2025-06-25 01:52:40 -0400

    fix

--- libcxx/test/std/ranges/range.adaptors/range.concat/ctor.default.pass.cpp
+++ libcxx/test/std/ranges/range.adaptors/range.concat/ctor.default.pass.cpp
@@ -71,10 +71,10 @@ constexpr bool test() {
 
   // check when input is an output range, which should be well-formed
   {
-    std::vector<int> v{1,2,3};
+    std::vector<int> v{1, 2, 3};
     auto r = std::views::counted(std::back_inserter(v), 3);
     static_assert(!std::ranges::input_range<decltype(r)>);
-  } 
+  }
 
   return true;
 }
