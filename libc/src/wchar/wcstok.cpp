a484c4b859cc92c1912072a3e5167e708a90d093
Uzair Nawaz <uzairnawaz@google.com>
2025-06-25 23:27:07 +0000

    set up build files

---
+++ libc/src/wchar/wcstok.cpp
@@ -0,0 +1,22 @@
+//===-- Implementation of wcstok ------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "src/wchar/wcstok.h"
+
+#include "hdr/types/wchar_t.h"
+#include "src/__support/common.h"
+
+namespace LIBC_NAMESPACE_DECL {
+
+LLVM_LIBC_FUNCTION(wchar_t *, wcstok,
+                   (wchar_t *__restrict str, const wchar_t *__restrict delim,
+                    wchar_t **__restrict ptr)) {
+  
+}
+
+} // namespace LIBC_NAMESPACE_DECL
