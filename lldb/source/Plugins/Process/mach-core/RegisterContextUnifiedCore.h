3a0a5557c7143097d4356cb7ce78f15fb92cd98a
Jason Molenda <github-mail@molenda.com>
2025-06-24 18:56:49 -0700

    Update lldb/source/Plugins/Process/mach-core/RegisterContextUnifiedCore.h
    Co-authored-by: Jonas Devlieghere <jonas@devlieghere.com>

--- lldb/source/Plugins/Process/mach-core/RegisterContextUnifiedCore.h
+++ lldb/source/Plugins/Process/mach-core/RegisterContextUnifiedCore.h
@@ -1,4 +1,4 @@
-//===-- RegisterContextUnifiedCore.h ----------------------------*- C++ -*-===//
+//===-- RegisterContextUnifiedCore.h --------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
