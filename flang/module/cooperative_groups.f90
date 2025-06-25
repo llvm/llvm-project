6b2a2425a9f3b89e0f327a71a5d46e698cccbff8
Valentin Clement <clementval@gmail.com>
2025-06-25 15:20:38 -0700

    Fix header

--- flang/module/cooperative_groups.f90
+++ flang/module/cooperative_groups.f90
@@ -1,4 +1,4 @@
-!===-- module/cudedevice.f90 -----------------------------------------------===!
+!===-- module/cooperative_groups.f90 ---------------------------------------===!
 !
 ! Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 ! See https://llvm.org/LICENSE.txt for license information.
