72183ce352af7bcce2db037ba791d3b78466ad9e
Roy Shi <royshi@meta.com>
2025-06-25 11:13:24 -0700

    Remove windows code from hello.cpp

--- lldb/test/API/macosx/simulator/hello.cpp
+++ lldb/test/API/macosx/simulator/hello.cpp
@@ -1,16 +1,8 @@
 #include <stdio.h>
 #include <thread>
-#if !defined(_WIN32)
 #include <unistd.h>
-#endif
 
-static void print_pid() {
-#if defined(_WIN32)
-  fprintf(stderr, "PID: %d\n", ::GetCurrentProcessId());
-#else
-  fprintf(stderr, "PID: %d\n", getpid());
-#endif
-}
+static void print_pid() { fprintf(stderr, "PID: %d\n", getpid()); }
 
 static void sleep() { std::this_thread::sleep_for(std::chrono::seconds(10)); }
 
