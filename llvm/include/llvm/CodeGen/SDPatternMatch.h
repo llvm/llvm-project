c961ba3d5c592877055932d5ad99d4aeee40cecb
Abhishek Kaushik <abhishek.kaushik@intel.com>
2025-06-26 22:08:43 +0530

    Use m_Result to match only the loaded value

--- llvm/include/llvm/CodeGen/SDPatternMatch.h
+++ llvm/include/llvm/CodeGen/SDPatternMatch.h
@@ -514,9 +514,10 @@ m_VSelect(const T0_P &Cond, const T1_P &T, const T2_P &F) {
 }
 
 template <typename T0_P, typename T1_P, typename T2_P>
-inline TernaryOpc_match<T0_P, T1_P, T2_P>
+inline Result_match<0, TernaryOpc_match<T0_P, T1_P, T2_P>>
 m_Load(const T0_P &Ch, const T1_P &Ptr, const T2_P &Offset) {
-  return TernaryOpc_match<T0_P, T1_P, T2_P>(ISD::LOAD, Ch, Ptr, Offset);
+  return m_Result<0>(
+      TernaryOpc_match<T0_P, T1_P, T2_P>(ISD::LOAD, Ch, Ptr, Offset));
 }
 
 template <typename T0_P, typename T1_P, typename T2_P>
--- llvm/unittests/CodeGen/SelectionDAGPatternMatchTest.cpp
+++ llvm/unittests/CodeGen/SelectionDAGPatternMatchTest.cpp
@@ -240,7 +240,7 @@ TEST_F(SelectionDAGPatternMatchTest, matchTernaryOp) {
   EXPECT_TRUE(sd_match(
       Load, m_Load(m_Specific(Ch), m_Specific(BasePtr), m_Specific(Offset))));
 
-  EXPECT_TRUE(
+  EXPECT_FALSE(
       sd_match(Load.getValue(1), m_Load(m_Specific(Ch), m_Specific(BasePtr),
                                         m_Specific(Offset))));
 }
