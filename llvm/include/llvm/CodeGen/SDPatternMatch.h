abf8e25ac7644dd035361383ec4f74e0456c1b3e
Abhishek Kaushik <abhishek.kaushik@intel.com>
2025-06-26 10:33:31 -0700

    [DAG] Add `SDPatternMatch::m_Load` (#145481)
    Add SDPatternMatch matcher and unit test coverage for `ISD::LOAD`
    opcode.
    This only matches the loaded value i.e. ResNo 0 and not the output
    chain.
    e.g.
    ```
    m_Load(m_Value(), m_Value(), m_Value())
    ```
    
    The first value is the input chain, the second is the base pointer, and
    the last value is the offset.

--- llvm/include/llvm/CodeGen/SDPatternMatch.h
+++ llvm/include/llvm/CodeGen/SDPatternMatch.h
@@ -531,6 +531,13 @@ m_VSelect(const T0_P &Cond, const T1_P &T, const T2_P &F) {
 }
 
 template <typename T0_P, typename T1_P, typename T2_P>
+inline Result_match<0, TernaryOpc_match<T0_P, T1_P, T2_P>>
+m_Load(const T0_P &Ch, const T1_P &Ptr, const T2_P &Offset) {
+  return m_Result<0>(
+      TernaryOpc_match<T0_P, T1_P, T2_P>(ISD::LOAD, Ch, Ptr, Offset));
+}
+
+template <typename T0_P, typename T1_P, typename T2_P>
 inline TernaryOpc_match<T0_P, T1_P, T2_P>
 m_InsertElt(const T0_P &Vec, const T1_P &Val, const T2_P &Idx) {
   return TernaryOpc_match<T0_P, T1_P, T2_P>(ISD::INSERT_VECTOR_ELT, Vec, Val,
