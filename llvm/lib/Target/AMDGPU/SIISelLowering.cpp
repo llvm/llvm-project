10e2de6c1c076b3e828d066169c8ad47cd183fcd
Janek van Oirschot <Janek.vanOirschot@amd.com>
2025-06-26 08:54:56 -0700

    Remove explicitly setting BUILD_VECTOR 64b elt to legal as it is the default

--- llvm/lib/Target/AMDGPU/SIISelLowering.cpp
+++ llvm/lib/Target/AMDGPU/SIISelLowering.cpp
@@ -357,9 +357,7 @@ SITargetLowering::SITargetLowering(const TargetMachine &TM,
   // Most operations are naturally 32-bit vector operations. We only support
   // load and store of i64 vectors, so promote v2i64 vector operations to v4i32.
   for (MVT Vec64 : {MVT::v2i64, MVT::v2f64}) {
-    if (STI.hasMovB64())
-      setOperationAction(ISD::BUILD_VECTOR, Vec64, Legal);
-    else {
+    if (!STI.hasMovB64()) {
       setOperationAction(ISD::BUILD_VECTOR, Vec64, Promote);
       AddPromotedToType(ISD::BUILD_VECTOR, Vec64, MVT::v4i32);
     }
@@ -374,9 +372,7 @@ SITargetLowering::SITargetLowering(const TargetMachine &TM,
   }
 
   for (MVT Vec64 : {MVT::v3i64, MVT::v3f64}) {
-    if (STI.hasMovB64())
-      setOperationAction(ISD::BUILD_VECTOR, Vec64, Legal);
-    else {
+    if (!STI.hasMovB64()) {
       setOperationAction(ISD::BUILD_VECTOR, Vec64, Promote);
       AddPromotedToType(ISD::BUILD_VECTOR, Vec64, MVT::v6i32);
     }
@@ -391,9 +387,7 @@ SITargetLowering::SITargetLowering(const TargetMachine &TM,
   }
 
   for (MVT Vec64 : {MVT::v4i64, MVT::v4f64}) {
-    if (STI.hasMovB64())
-      setOperationAction(ISD::BUILD_VECTOR, Vec64, Legal);
-    else {
+    if (!STI.hasMovB64()) {
       setOperationAction(ISD::BUILD_VECTOR, Vec64, Promote);
       AddPromotedToType(ISD::BUILD_VECTOR, Vec64, MVT::v8i32);
     }
@@ -408,9 +402,7 @@ SITargetLowering::SITargetLowering(const TargetMachine &TM,
   }
 
   for (MVT Vec64 : {MVT::v8i64, MVT::v8f64}) {
-    if (STI.hasMovB64())
-      setOperationAction(ISD::BUILD_VECTOR, Vec64, Legal);
-    else {
+    if (!STI.hasMovB64()) {
       setOperationAction(ISD::BUILD_VECTOR, Vec64, Promote);
       AddPromotedToType(ISD::BUILD_VECTOR, Vec64, MVT::v16i32);
     }
@@ -425,9 +417,7 @@ SITargetLowering::SITargetLowering(const TargetMachine &TM,
   }
 
   for (MVT Vec64 : {MVT::v16i64, MVT::v16f64}) {
-    if (STI.hasMovB64())
-      setOperationAction(ISD::BUILD_VECTOR, Vec64, Legal);
-    else {
+    if (!STI.hasMovB64()) {
       setOperationAction(ISD::BUILD_VECTOR, Vec64, Promote);
       AddPromotedToType(ISD::BUILD_VECTOR, Vec64, MVT::v32i32);
     }
