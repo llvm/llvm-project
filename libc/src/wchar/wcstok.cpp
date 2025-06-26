930e37e5de03c3879d5794b19219e211a7d8a469
Uzair Nawaz <uzairnawaz@google.com>
2025-06-26 21:29:47 +0000

    started impl

--- libc/src/wchar/wcstok.cpp
+++ libc/src/wchar/wcstok.cpp
@@ -16,7 +16,15 @@ namespace LIBC_NAMESPACE_DECL {
 LLVM_LIBC_FUNCTION(wchar_t *, wcstok,
                    (wchar_t *__restrict str, const wchar_t *__restrict delim,
                     wchar_t **__restrict ptr)) {
-  
+    if (str == nullptr)
+        str = *ptr;
+    
+    while (*str != L'\0') {
+        bool inDelim = false;
+        for (const wchar_t* delim_ptr = delim; delim_ptr != L'\0'; delim_ptr++) {
+            
+        }
+    }
 }
 
 } // namespace LIBC_NAMESPACE_DECL
