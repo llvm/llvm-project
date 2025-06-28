48341114fa8504dcbfd803ea9531de0b619444f5
Eric Fiselier <eric@efcs.ca>
2025-06-28 15:12:47 -0400

    try again; this time with the config we think works

--- libcxx/cmake/caches/Generic-tsan.cmake
+++ libcxx/cmake/caches/Generic-tsan.cmake
@@ -1,3 +1,2 @@
 set(LLVM_USE_SANITIZER "Thread" CACHE STRING "")
 set(LIBCXXABI_USE_LLVM_UNWINDER OFF CACHE BOOL "") # TSAN is compiled against the system unwinder, which leads to false positives
-set(LIBCXX_INCLUDE_BENCHMARKS OFF CACHE BOOL "FIXME: This is a temporary workaround to get tsan running again")
