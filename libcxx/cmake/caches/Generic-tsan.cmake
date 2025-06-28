9f57fbdf8a52fd6fffe218a95ee99b00fe32005a
Eric Fiselier <eric@efcs.ca>
2025-06-28 15:53:25 -0400

    redisable the sanitizers for reasons I don't understand

--- libcxx/cmake/caches/Generic-tsan.cmake
+++ libcxx/cmake/caches/Generic-tsan.cmake
@@ -1,2 +1,3 @@
 set(LLVM_USE_SANITIZER "Thread" CACHE STRING "")
 set(LIBCXXABI_USE_LLVM_UNWINDER OFF CACHE BOOL "") # TSAN is compiled against the system unwinder, which leads to false positives
+set(LIBCXX_INCLUDE_BENCHMARKS OFF CACHE BOOL "FIXME: This is a temporary workaround to get tsan running again")
