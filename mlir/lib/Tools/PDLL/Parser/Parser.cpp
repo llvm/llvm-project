8c569c362ad94a81d3351ee0896fc854fe2f71f6
Kazu Hirata <kazu@google.com>
2025-06-29 00:31:56 -0700

    [llvm] Remove unused includes (NFC)
    These are identified by misc-include-cleaner.  I've filtered out those
    that break builds.  Also, I'm staying away from llvm-config.h,
    config.h, and Compiler.h, which likely cause platform- or
    compiler-specific build failures.

--- mlir/lib/Tools/PDLL/Parser/Parser.cpp
+++ mlir/lib/Tools/PDLL/Parser/Parser.cpp
@@ -25,7 +25,6 @@
 #include "llvm/ADT/StringExtras.h"
 #include "llvm/ADT/TypeSwitch.h"
 #include "llvm/Support/FormatVariadic.h"
-#include "llvm/Support/ManagedStatic.h"
 #include "llvm/Support/SaveAndRestore.h"
 #include "llvm/Support/ScopedPrinter.h"
 #include "llvm/TableGen/Error.h"
