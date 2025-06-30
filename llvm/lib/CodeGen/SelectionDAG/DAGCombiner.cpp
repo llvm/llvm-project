262994c111f5327f2b31bca203f530201f282d67
pvanhout <pierre.vanhoutryve@amd.com>
2025-06-30 10:19:44 +0200

    comments

--- llvm/lib/CodeGen/SelectionDAG/DAGCombiner.cpp
+++ llvm/lib/CodeGen/SelectionDAG/DAGCombiner.cpp
@@ -28992,7 +28992,7 @@ static SDValue matchMergedBFX(SDValue Root, SelectionDAG &DAG,
 
   EVT VT = Root.getValueType();
 
-  if (Root.getOpcode() != ISD::AND)
+  if (!VT.isScalarInteger() || Root.getOpcode() != ISD::AND)
     return SDValue();
 
   SDValue N0 = Root.getOperand(0);
@@ -29002,8 +29002,6 @@ static SDValue matchMergedBFX(SDValue Root, SelectionDAG &DAG,
     return SDValue();
 
   APInt RootMask = cast<ConstantSDNode>(N1)->getAsAPIntVal();
-  if (!RootMask.isMask())
-    return SDValue();
 
   SDValue Src;
   const auto IsSrc = [&](SDValue V) {
@@ -29019,7 +29017,7 @@ static SDValue matchMergedBFX(SDValue Root, SelectionDAG &DAG,
   APInt PartsMask(VT.getSizeInBits(), 0);
   while (!Worklist.empty()) {
     SDValue V = Worklist.pop_back_val();
-    if (!V.hasOneUse() && Src != V)
+    if (!V.hasOneUse() && (Src && Src != V))
       return SDValue();
 
     if (V.getOpcode() == ISD::OR) {
@@ -29035,7 +29033,11 @@ static SDValue matchMergedBFX(SDValue Root, SelectionDAG &DAG,
       if (!IsSrc(ShiftSrc) || !isa<ConstantSDNode>(ShiftAmt))
         return SDValue();
 
-      PartsMask |= (RootMask << cast<ConstantSDNode>(ShiftAmt)->getAsZExtVal());
+      auto ShiftAmtVal = cast<ConstantSDNode>(ShiftAmt)->getAsZExtVal();
+      if (ShiftAmtVal > RootMask.getBitWidth())
+        return SDValue();
+
+      PartsMask |= (RootMask << ShiftAmtVal);
       continue;
     }
 
@@ -29047,7 +29049,7 @@ static SDValue matchMergedBFX(SDValue Root, SelectionDAG &DAG,
     return SDValue();
   }
 
-  if (!RootMask.isMask() || !Src)
+  if (!Src)
     return SDValue();
 
   SDLoc DL(Root);
