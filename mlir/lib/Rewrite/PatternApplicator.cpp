79688e214e75a5d374853ddb70047b5d577780a6
Jeremy Kun <j2kun@users.noreply.github.com>
2025-06-30 10:35:32 -0700

    formatting

--- mlir/lib/Rewrite/PatternApplicator.cpp
+++ mlir/lib/Rewrite/PatternApplicator.cpp
@@ -212,10 +212,11 @@ LogicalResult PatternApplicator::matchAndRewrite(
 #ifndef NDEBUG
             OpBuilder::Listener *oldListener = rewriter.getListener();
             auto catalogingListener =
-                std::make_unique<RewriterBase::CatalogingListener>(oldListener,
-                                                     pattern->getDebugName());
+                std::make_unique<RewriterBase::CatalogingListener>(
+                    oldListener, pattern->getDebugName());
             rewriter.setListener(catalogingListener.get());
-            llvm::make_scope_exit([&] { rewriter.setListener(oldListener); };
+            llvm::make_scope_exit([&] {
+          rewriter.setListener(oldListener); };
 #endif
             result = pattern->matchAndRewrite(op, rewriter);
             LLVM_DEBUG(llvm::dbgs()
