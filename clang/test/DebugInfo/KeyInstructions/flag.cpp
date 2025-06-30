e6a423eab19a0d0c594c220aab03663bdd0d12aa
Orlando Cazalet-Hyams <orlando.hyams@sony.com>
2025-06-30 08:43:12 +0100

    improve test readability

--- clang/test/DebugInfo/KeyInstructions/flag.cpp
+++ clang/test/DebugInfo/KeyInstructions/flag.cpp
@@ -3,16 +3,16 @@
 //// Default: Off.
 // RUN: %clang -### -target x86_64 -c -gdwarf %s 2>&1 | FileCheck %s --check-prefixes=NO-KEY-INSTRUCTIONS
 
-//// Help hidden.
+// KEY-INSTRUCTIONS: "-gkey-instructions"
+// NO-KEY-INSTRUCTIONS-NOT: key-instructions
+
+//// Help hidden: flag should not be visible.
 // RUN: %clang --help | FileCheck %s --check-prefix=HELP
 // HELP-NOT: key-instructions
 
-// KEY-INSTRUCTIONS: "-gkey-instructions"
-
-// NO-KEY-INSTRUCTIONS-NOT: key-instructions
-
-// RUN: %clang_cc1 %s -triple x86_64-linux-gnu -debug-info-kind=line-tables-only -emit-llvm -o - | FileCheck %s --check-prefix=SMOKETEST-OFF
+// Smoke test: check for Key Instructions keywords in the IR.
 void f() {}
+// RUN: %clang_cc1 %s -triple x86_64-linux-gnu -debug-info-kind=line-tables-only -emit-llvm -o - | FileCheck %s --check-prefix=SMOKETEST-OFF
 // SMOKETEST-OFF-NOT: keyInstructions:
 // SMOKETEST-OFF-NOT: atomGroup
 
