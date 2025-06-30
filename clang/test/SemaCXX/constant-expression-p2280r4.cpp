cf30130162a045ccbd2d05f4e5972a9ca4374534
Eli Friedman <efriedma@quicinc.com>
2025-06-29 20:56:15 -0700

    Fixup tests.

--- clang/test/SemaCXX/constant-expression-p2280r4.cpp
+++ clang/test/SemaCXX/constant-expression-p2280r4.cpp
@@ -286,10 +286,14 @@ namespace dynamic {
 }
 
 namespace unsized_array {
-  void f(int (&a)[], int (&b)[]) {
+  void f(int (&a)[], int (&b)[], int (&c)[4]) {
     constexpr int t1 = a - a;
-    constexpr int t2 = a - b; // expected-error {{constexpr variable 't2' must be initialized by a constant expression}}
-                              // expected-note {{arithmetic involving unrelated objects '&a[0]' and '&b[0]' has unspecified value}}
+    constexpr int t2 = a - b; // expected-error {{constexpr variable 't2' must be initialized by a constant expression}} \
+                              // nointerpreter-note {{arithmetic involving unrelated objects '&a[0]' and '&b[0]' has unspecified value}} \
+                              // interpreter-note {{arithmetic involving unrelated objects 'a' and 'b' has unspecified value}}
+    constexpr int t3 = a - &c[2];  // expected-error {{constexpr variable 't3' must be initialized by a constant expression}} \
+                              // nointerpreter-note {{arithmetic involving unrelated objects '&a[0]' and '&c[2]' has unspecified value}} \
+                              // interpreter-note {{arithmetic involving unrelated objects 'a' and '*((char*)&c + 8)' has unspecified value}}
   }
 }
 
