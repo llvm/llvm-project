540a6fd0eba86664325cc1da32f06c65ea833083
Narayan Sreekumar <nsreekumar6@gmail.com>
2025-07-02 01:00:41 +0530

    [LLVMABI] Scaffolding the SysV ABI

--- llvm/include/llvm/ABI/ABIInfo.h
+++ llvm/include/llvm/ABI/ABIInfo.h
@@ -17,19 +17,50 @@
 #include "llvm/ABI/ABIFunctionInfo.h"
 #include "llvm/ABI/Types.h"
 #include <cassert>
+#include <climits>
+#include <cstdint>
 
 namespace llvm {
 namespace abi {
 
+struct ABICompatInfo {
+  unsigned Version = UINT_MAX;
+
+  struct ABIFlags {
+    bool PassInt128VectorsInMem : 1;
+    bool ReturnCXXRecordGreaterThan128InMem : 1;
+    bool ClassifyIntegerMMXAsSSE : 1;
+    bool HonorsRevision98 : 1;
+
+    ABIFlags()
+        : PassInt128VectorsInMem(true),
+          ReturnCXXRecordGreaterThan128InMem(true),
+          ClassifyIntegerMMXAsSSE(true), HonorsRevision98(true) {}
+
+  } Flags;
+
+  ABICompatInfo() : Version(UINT_MAX) {}
+  ABICompatInfo(unsigned Ver) : Version(Ver) {}
+};
+
 /// Abstract base class for target-specific ABI information.
 class ABIInfo {
+private:
+  ABICompatInfo CompatInfo;
+
 public:
+  ABIInfo() : CompatInfo() {}
+  explicit ABIInfo(const ABICompatInfo &Info) : CompatInfo(Info) {}
+
   virtual ~ABIInfo() = default;
 
   virtual ABIArgInfo classifyReturnType(const Type *RetTy) const = 0;
   virtual ABIArgInfo classifyArgumentType(const Type *ArgTy) const = 0;
   virtual void computeInfo(ABIFunctionInfo &FI) const = 0;
   virtual bool isPassByRef(const Type *Ty) const { return false; }
+  const ABICompatInfo &getABICompatInfo() const { return CompatInfo; }
+
+  void setABICompatInfo(const struct ABICompatInfo &Info) { CompatInfo = Info; }
 };
 
 } // namespace abi
