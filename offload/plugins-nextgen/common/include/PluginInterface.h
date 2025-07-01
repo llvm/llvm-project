782aa093abb8f470d3fc96580fd45792d390d189
Ross Brunton <ross@codeplay.com>
2025-07-01 16:41:40 +0100

    Copy the null terminator explicitly

--- offload/plugins-nextgen/common/include/PluginInterface.h
+++ offload/plugins-nextgen/common/include/PluginInterface.h
@@ -296,10 +296,11 @@ public:
 struct GenericKernelTy {
   /// Construct a kernel with a name and a execution mode.
   GenericKernelTy(const char *Name)
-      : Name(Name), PreferredNumThreads(0), MaxNumThreads(0) {
-    // Ensure that the name is null terminated so getName() can just return the
-    // pointer
-    this->Name.push_back('\0');
+      : Name(Name, &Name[strlen(Name) + 1]), PreferredNumThreads(0),
+        MaxNumThreads(0) {
+    // The null terminator from the input string was also copied to ensure that
+    // Name.data() will always be null terminated. Pop the last character to
+    // ensure that Name.size is accurate.
     this->Name.pop_back();
   }
 
