d1ac3f8c8a9a996936ad2c7444ec1b2e89961d9e
Ashwin Banwari <ashwinkbanwari@gmail.com>
2025-07-02 00:36:31 -0700

    clang-format

--- clang/lib/Sema/SemaDecl.cpp
+++ clang/lib/Sema/SemaDecl.cpp
@@ -12488,7 +12488,8 @@ void Sema::CheckMain(FunctionDecl *FD, const DeclSpec &DS) {
     }
 
     // [basic.start.main]p3:
-    // A program that declares a function main that belongs to the global scope and is attached to a named module is ill-formed.
+    // A program that declares a function main that belongs to the global scope
+    // and is attached to a named module is ill-formed.
     if (FD->isInNamedModule()) {
       const SourceLocation start = FD->getTypeSpecStartLoc();
       Diag(start, diag::warn_main_in_named_module)
