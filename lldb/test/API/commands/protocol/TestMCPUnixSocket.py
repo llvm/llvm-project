93a27307c78b6c49db630aec4080c02447e9b55e
Alexandre Perez <alexandreperez@meta.com>
2025-07-02 10:42:50 -0700

    Make sure we don't test with sockets over 104 chars

--- lldb/test/API/commands/protocol/TestMCPUnixSocket.py
+++ lldb/test/API/commands/protocol/TestMCPUnixSocket.py
@@ -1,8 +1,13 @@
 import os
 import tempfile
+import unittest
 
 from lldbsuite.test.decorators import *
 from lldbsuite.test.lldbtest import *
+
+# To be safe and portable, Unix domain socket paths should be kept at or below
+# 108 characters on Linux, and around 104 characters on macOS:
+MAX_SOCKET_PATH_LENGTH = 104
 
 
 class MCPUnixSocketCommandTestCase(TestBase):
@@ -15,6 +20,11 @@ class MCPUnixSocketCommandTestCase(TestBase):
 
         temp_directory = tempfile.TemporaryDirectory()
         socket_file = os.path.join(temp_directory.name, "mcp.sock")
+
+        if len(socket_file) >= MAX_SOCKET_PATH_LENGTH:
+            self.skipTest(
+                f"Socket path {socket_file} exceeds the {MAX_SOCKET_PATH_LENGTH} character limit"
+            )
 
         self.expect(
             f"protocol-server start MCP accept://{socket_file}",
