66a4fdb156d493bf994fd5c0050cfd2d7352732d
Michael Buch <michaelbuch12@gmail.com>
2025-07-02 15:01:27 +0100

    [lldb][test] Turn (most) libcxx data-formatters tests into generic tests
    This patch moves all the data-formatter tests that were only run for
    libc++ to the generic directory. There's nothing libc++ specific
    about these tests. For aynthing tied to libcxx we can have a dedicated
    subdirectory. But for now lets move as much into `generic` as possible.
    
    This involved renaming a bunch of files and passing the `USE_LIBCPP` and
    `USE_LIBSTDCPP` Makefile variables from within the python test-cases
    instead of the Makefiles. This is how the existing `generic` tests
    already worked.
    
    The only tests I haven't turned into "generic" ones are the ones for
    `std::string`.
    
    Next steps:
    1. Move libstdcpp tests into the `generic` directory
    2. Move as much of the `std::string` libc++ tests into `generic`

---
+++ lldb/test/API/functionalities/data-formatter/data-formatter-stl/generic/tuple/TestDataFormatterTuple.py
@@ -0,0 +1,53 @@
+"""
+Test lldb data formatter subsystem.
+"""
+
+
+import lldb
+from lldbsuite.test.decorators import *
+from lldbsuite.test.lldbtest import *
+from lldbsuite.test import lldbutil
+
+
+class TestDataFormatterStdTuple(TestBase):
+    def setUp(self):
+        TestBase.setUp(self)
+        self.line = line_number("main.cpp", "// break here")
+        self.namespace = "std"
+
+    def do_test(self):
+        """Test that std::tuple is displayed correctly"""
+        lldbutil.run_to_source_breakpoint(
+            self, "// break here", lldb.SBFileSpec("main.cpp", False)
+        )
+
+        tuple_name = self.namespace + "::tuple"
+        self.expect("frame variable empty", substrs=[tuple_name, "size=0", "{}"])
+
+        self.expect(
+            "frame variable one_elt",
+            substrs=[tuple_name, "size=1", "{", "[0] = 47", "}"],
+        )
+
+        self.expect(
+            "frame variable three_elts",
+            substrs=[
+                tuple_name,
+                "size=3",
+                "{",
+                "[0] = 1",
+                "[1] = 47",
+                '[2] = "foo"',
+                "}",
+            ],
+        )
+
+    @add_test_categories(["libc++"])
+    def test_libcxx(self):
+        self.build(dictionary={"USE_LIBCPP" : 1})
+        self.do_test()
+        
+    @add_test_categories(["libstdcxx"])
+    def test_libstdcxx(self):
+        self.build(dictionary={"USE_LIBSTDCPP" : 1})
+        self.do_test()
