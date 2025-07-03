33dee487cdf366c89b53b9d4e0807b98635b680d
Adam Siemieniuk <adam.siemieniuk@intel.com>
2025-07-03 13:19:43 +0200

    [mlir][linalg] Vectorize directly to a named contraction
    Extends linalg vectorizer with a path to lower contraction ops
    directly into `vector.contract`.
    
    The direct rewriting preserves high-level op semantics and provides
    more progressive lowering compared to reconstructing contraction back
    from multi dimensional reduction.
    The added lowering focuses on named linalg ops and leverages their
    well defined semantics to avoid complex precondition verification.
    
    The new path is optional and disabled by default to avoid changing
    the default vectorizer behavior.

--- mlir/lib/Dialect/Linalg/TransformOps/LinalgTransformOps.cpp
+++ mlir/lib/Dialect/Linalg/TransformOps/LinalgTransformOps.cpp
@@ -3920,7 +3920,9 @@ DiagnosedSilenceableFailure transform::VectorizeOp::apply(
     }
     FailureOr<VectorizationResult> vectorResults =
         linalg::vectorize(rewriter, target, vectorSizes, getScalableSizes(),
-                          getVectorizeNdExtract().value_or(false));
+                          getVectorizeNdExtract().value_or(false),
+                          getFlatten1DDepthwiseConv().value_or(false),
+                          getCreateNamedContraction().value_or(false));
     if (failed(vectorResults)) {
       return mlir::emitSilenceableFailure(target->getLoc())
              << "Attempted to vectorize, but failed";
