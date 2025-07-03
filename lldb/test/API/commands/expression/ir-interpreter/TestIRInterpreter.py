563bea91222f534d90c2baa645a5e2bc4132e9a8
David Spickett <david.spickett@linaro.org>
2025-07-03 09:40:57 +0000

    [lldb][test] Enable TestIRInterpreter on Windows
    Relates to https://github.com/llvm/llvm-project/issues/22139
    
    This used to be broken because the expressions didn't work,
    but the test also used getpid which isn't avaialable on Windows.
    
    So when the expressions started working, evaluation still failed
    due to getpid.
    
    I made it call GetCurrentProcessId and it worked.

--- lldb/test/API/commands/expression/ir-interpreter/TestIRInterpreter.py
+++ lldb/test/API/commands/expression/ir-interpreter/TestIRInterpreter.py
@@ -105,8 +105,6 @@ class IRInterpreterTestCase(TestBase):
         self.runCmd("run", RUN_SUCCEEDED)
 
     @add_test_categories(["pyapi"])
-    # getpid() is POSIX, among other problems, see bug
-    @expectedFailureAll(oslist=["windows"], bugnumber="http://llvm.org/pr21765")
     def test_ir_interpreter(self):
         self.build_and_run()
 
@@ -136,6 +134,14 @@ class IRInterpreterTestCase(TestBase):
 
         for expression in set_up_expressions:
             self.frame().EvaluateExpression(expression, options)
+
+        func_call = "(int)getpid"
+        if lldbplatformutil.getPlatform() == "windows":
+            func_call = "(int)GetCurrentProcessId()"
+
+        for expression in expressions:
+            interp_expression = expression
+            jit_expression = func_call + "; " + expression
 
         for expression in expressions:
             interp_expression = expression
