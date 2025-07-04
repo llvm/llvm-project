0510c784394a21e0a1ec06e15588848912dc568b
Hristo Hristov <hghristov.rmm@gmail.com>
2025-07-04 18:22:03 +0300

    WIP [libc++][string] P3044R2: sub-`string_view` from `string`
    Implements [P3044R2](https://wg21.link/P3044R2)
    
    -  https://github.com/cplusplus/draft/pull/7975

---
+++ libcxx/test/std/strings/string.view/string.view.ops/substr_subview.pass.cpp
@@ -0,0 +1,158 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: !stdlib=libc++ && (c++03 || c++11 || c++14)
+
+// <string_view>
+
+// constexpr basic_string_view substr(size_type pos = 0, size_type n = npos) const;
+// constexpr basic_string_view subview(size_type pos = 0,
+//                                     size_type n = npos) const;      // freestanding-deleted
+
+// subview is alternative name of substr
+
+// Throws: out_of_range if pos > size().
+// Effects: Determines the effective length rlen of the string to reference as the smaller of n and size() - pos.
+// Returns: basic_string_view(data()+pos, rlen).
+
+#include <algorithm>
+#include <cassert>
+#include <cstddef>
+#include <stdexcept>
+#include <string_view>
+
+#include "test_macros.h"
+
+template <typename CharT>
+struct Test {
+  typedef std::basic_string_view<CharT> (std::basic_string_view<CharT>::*Sub)(
+      typename std::basic_string_view<CharT>::size_type, typename std::basic_string_view<CharT>::size_type) const;
+};
+
+template <typename CharT>
+void testDetail(std::basic_string_view<CharT> sv, typename Test<CharT>::Sub testSub, std::size_t n, size_t pos) {
+  (void)testSub; // Avoid unused parameter warning
+  std::basic_string_view<CharT> sv1;
+#ifdef TEST_HAS_NO_EXCEPTIONS
+  if (pos > sv.size())
+    return; // would throw if exceptions were enabled
+  sv1 = (sv.*testSub)(pos, n);
+#else
+  try {
+    sv1 = (sv.*testSub)(pos, n);
+    assert(pos <= sv.size());
+  } catch (const std::out_of_range&) {
+    assert(pos > sv.size());
+    return;
+  }
+#endif
+  const std::size_t rlen = std::min(n, sv.size() - pos);
+  assert(sv1.size() == rlen);
+  for (std::size_t i = 0; i < rlen; ++i)
+    assert(sv[pos + i] == sv1[i]);
+}
+
+template <typename CharT>
+void testCases(std::basic_string_view<CharT> sv, typename Test<CharT>::Sub testSub) {
+  testDetail(sv, testSub, 0, 0);
+  testDetail(sv, testSub, 1, 0);
+  testDetail(sv, testSub, 20, 0);
+  testDetail(sv, testSub, sv.size(), 0);
+
+  testDetail(sv, testSub, 100, 3);
+
+  testDetail(sv, testSub, 0, std::basic_string_view<CharT>::npos);
+  testDetail(sv, testSub, 2, std::basic_string_view<CharT>::npos);
+  testDetail(sv, testSub, sv.size(), std::basic_string_view<CharT>::npos);
+
+  testDetail(sv, testSub, sv.size() + 1, 0);
+  testDetail(sv, testSub, sv.size() + 1, 1);
+  testDetail(sv, testSub, sv.size() + 1, std::basic_string_view<CharT>::npos);
+}
+
+template <typename CharT>
+void testSubs(const CharT* s) {
+  std::basic_string_view<CharT> sv(s);
+
+  testCases(sv, &std::basic_string_view<CharT>::substr);
+#if TEST_STD_VER >= 26
+  testCases(sv, &std::basic_string_view<CharT>::subview);
+#endif // TEST_STD_VER >= 26
+}
+
+void test() {
+  testSubs("ABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDE");
+  testSubs("ABCDE");
+  testSubs("a");
+  testSubs("");
+
+#ifndef TEST_HAS_NO_WIDE_CHARACTERS
+  testSubs(
+      L"ABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDE");
+  testSubs(L"ABCDE");
+  testSubs(L"a");
+  testSubs(L"");
+#endif
+
+#if TEST_STD_VER >= 11
+  testSubs(
+      u"ABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDE");
+  testSubs(u"ABCDE");
+  testSubs(u"a");
+  testSubs(u"");
+
+  testSubs(
+      U"ABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDE");
+  testSubs(U"ABCDE");
+  testSubs(U"a");
+  testSubs(U"");
+#endif
+}
+
+#if TEST_STD_VER >= 14
+template <typename Test<char>::Sub TestSub>
+constexpr void testConstexprDetail() {
+  constexpr std::string_view sv{"ABCDE", 5};
+  {
+    constexpr std::string_view sv2 = (sv.*TestSub)(0, 3);
+
+    static_assert(sv2.size() == 3, "");
+    static_assert(sv2[0] == 'A', "");
+    static_assert(sv2[1] == 'B', "");
+    static_assert(sv2[2] == 'C', "");
+  }
+
+  {
+    constexpr std::string_view sv2 = (sv.*TestSub)(3, 0);
+    static_assert(sv2.size() == 0, "");
+  }
+
+  {
+    constexpr std::string_view sv2 = (sv.*TestSub)(3, 3);
+    static_assert(sv2.size() == 2, "");
+    static_assert(sv2[0] == 'D', "");
+    static_assert(sv2[1] == 'E', "");
+  }
+}
+
+void test_constexpr() {
+  testConstexprDetail<&std::string_view::substr>();
+#  if TEST_STD_VER >= 26
+  testConstexprDetail<&std::string_view::subview>();
+#  endif
+}
+#endif
+
+int main(int, char**) {
+  test();
+#if TEST_STD_VER >= 14
+  test_constexpr();
+#endif
+
+  return 0;
+}
