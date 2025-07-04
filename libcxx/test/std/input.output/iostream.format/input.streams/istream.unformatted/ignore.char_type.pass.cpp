519cf0128796091eea589e8b8d925cd33ae503f1
Hristo Hristov <hghristov.rmm@gmail.com>
2025-07-04 07:57:26 +0300

    [libc++][istream] P3223R2 Making `std::istream::ignore` less surprising
    Implements [P3223R2](https://wg21.link/P3223R2)
    
    # References
    
    - https://github.com/cplusplus/draft/pull/7996

---
+++ libcxx/test/std/input.output/iostream.format/input.streams/istream.unformatted/ignore.char_type.pass.cpp
@@ -0,0 +1,41 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// REQUIRES: std-at-least-c++26
+
+// Requires 396145d in the built library.
+// XFAIL: using-built-library-before-llvm-9
+
+// <istream>
+
+// basic_istream& ignore(streamsize n, char_type delim);
+
+#include <cassert>
+#include <istream>
+#include <sstream>
+#include <string>
+
+#include "test_macros.h"
+
+int main(int, char**) {
+  std::istringstream in("\xF0\x9F\xA4\xA1 Clown Face");
+  in.ignore(100, '\xA1'); // ignore up to '\xA1' delimiter,
+                                   // previously might have ignored to EOF
+
+  assert(in.gcount() == 4);        // 4 bytes were ignored
+  assert(in.peek() == ' ');        // next character is a space
+
+  std::string s1;                  // read the next word
+  in >> s1;
+  assert(s1 == "Clown");
+
+  in.ignore(100, -1L);    // ambiguous overload,
+                                   // previously equivalent to (int)-1L
+
+  return 0;
+}
