ea54904cdc29c624cd40ecff7f4ce5fe0af17af6
joaosaffran <joao.saffran@microsoft.com>
2025-07-04 19:28:15 +0000

    formating

--- llvm/lib/Target/DirectX/DXILRootSignature.h
+++ llvm/lib/Target/DirectX/DXILRootSignature.h
@@ -47,7 +47,7 @@ public:
   RootSignatureBindingInfo() = default;
   RootSignatureBindingInfo(
       SmallDenseMap<const Function *, mcdxbc::RootSignatureDesc> Map)
-      : FuncToRsMap(Map){};
+      : FuncToRsMap(Map) {};
 
   iterator find(const Function *F) { return FuncToRsMap.find(F); }
 
