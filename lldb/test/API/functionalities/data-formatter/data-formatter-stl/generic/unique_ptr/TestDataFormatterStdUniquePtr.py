d86233958e374fa9d6dfa10c206eaf8c627c6d41
Michael Buch <michaelbuch12@gmail.com>
2025-07-04 18:13:12 +0100

    fixup! loosen test assertions for type-names

--- lldb/test/API/functionalities/data-formatter/data-formatter-stl/generic/unique_ptr/TestDataFormatterStdUniquePtr.py
+++ lldb/test/API/functionalities/data-formatter/data-formatter-stl/generic/unique_ptr/TestDataFormatterStdUniquePtr.py
@@ -9,28 +9,6 @@ from lldbsuite.test import lldbutil
 
 
 class TestCase(TestBase):
-    def make_expected_type(self, pointee_type: str, qualifiers: str = "") -> str:
-        if qualifiers:
-            qualifiers = " " + qualifiers
-
-        if self.expectedCompiler(["clang"]) and self.expectedCompilerVersion(
-            [">", "16.0"]
-        ):
-            return f"std::unique_ptr<{pointee_type}>{qualifiers}"
-        else:
-            return f"std::unique_ptr<{pointee_type}, std::default_delete<{pointee_type}> >{qualifiers}"
-
-    def make_expected_basic_string_ptr(self) -> str:
-        if self.expectedCompiler(["clang"]) and self.expectedCompilerVersion(
-            [">", "16.0"]
-        ):
-            return f"std::unique_ptr<std::string>"
-        else:
-            return (
-                "std::unique_ptr<std::basic_string<char, std::char_traits<char>, std::allocator<char> >, "
-                "std::default_delete<std::basic_string<char, std::char_traits<char>, std::allocator<char> > > >"
-            )
-
     def do_test(self):
         """Test `frame variable` output for `std::unique_ptr` types."""
 
@@ -40,7 +18,6 @@ class TestCase(TestBase):
 
         valobj = self.expect_var_path(
             "up_empty",
-            type=self.make_expected_type("int"),
             summary="nullptr",
             children=[ValueCheck(name="pointer")],
         )
@@ -54,7 +31,6 @@ class TestCase(TestBase):
 
         valobj = self.expect_var_path(
             "up_int",
-            type=self.make_expected_type("int"),
             summary="10",
             children=[ValueCheck(name="pointer")],
         )
@@ -62,7 +38,6 @@ class TestCase(TestBase):
 
         valobj = self.expect_var_path(
             "up_int_ref",
-            type=self.make_expected_type("int", qualifiers="&"),
             summary="10",
             children=[ValueCheck(name="pointer")],
         )
@@ -70,7 +45,6 @@ class TestCase(TestBase):
 
         valobj = self.expect_var_path(
             "up_int_ref_ref",
-            type=self.make_expected_type("int", qualifiers="&&"),
             summary="10",
             children=[ValueCheck(name="pointer")],
         )
@@ -78,12 +52,11 @@ class TestCase(TestBase):
 
         valobj = self.expect_var_path(
             "up_str",
-            type=self.make_expected_basic_string_ptr(),
             summary='"hello"',
             children=[ValueCheck(name="pointer", summary='"hello"')],
         )
 
-        valobj = self.expect_var_path("up_user", type=self.make_expected_type("User"))
+        valobj = self.expect_var_path("up_user")
         self.assertRegex(valobj.summary, "^User @ 0x0*[1-9a-f][0-9a-f]+$")
         self.assertNotEqual(valobj.child[0].unsigned, 0)
 
