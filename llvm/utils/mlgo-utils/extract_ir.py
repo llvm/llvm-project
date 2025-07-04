d51bd453fe228504f5cf06db2836798860399880
Vincent Lee <leevince@fb.com>
2025-07-04 11:20:34 -0700

    format

--- llvm/utils/mlgo-utils/extract_ir.py
+++ llvm/utils/mlgo-utils/extract_ir.py
@@ -3,7 +3,7 @@
 import re
 import sys
 from mlgo.corpus.extract_ir import parse_args_and_run
+
 if __name__ == '__main__':
     sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
     sys.exit(parse_args_and_run())
-
