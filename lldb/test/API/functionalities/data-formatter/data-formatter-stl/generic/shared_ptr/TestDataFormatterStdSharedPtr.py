c5380d0632ad29e6aedcbf07dfb74b9264ffc5c9
Michael Buch <michaelbuch12@gmail.com>
2025-07-05 12:22:01 +0100

    [lldb][test] Combine libstdc++ and libc++ std::shared_ptr tests into generic test
    This combines the libc++ and libstdc++ test cases.
    The libstdcpp tests were a subset of the libc++ test, so this patch
    moves the libcxx test into `generic` and removes the libstdcpp test
    entirely.
    
    Split out from https://github.com/llvm/llvm-project/pull/146740

--- lldb/test/API/functionalities/data-formatter/data-formatter-stl/generic/shared_ptr/TestDataFormatterStdSharedPtr.py
+++ lldb/test/API/functionalities/data-formatter/data-formatter-stl/generic/shared_ptr/TestDataFormatterStdSharedPtr.py
@@ -1,5 +1,5 @@
 """
-Test lldb data formatter for libc++ std::shared_ptr.
+Test lldb data formatter for std::shared_ptr.
 """
 
 import lldb
@@ -9,11 +9,8 @@ from lldbsuite.test import lldbutil
 
 
 class TestCase(TestBase):
-    @add_test_categories(["libc++"])
-    def test_shared_ptr_variables(self):
+    def do_test(self):
         """Test `frame variable` output for `std::shared_ptr` types."""
-        self.build()
-
         (_, process, _, bkpt) = lldbutil.run_to_source_breakpoint(
             self, "// break here", lldb.SBFileSpec("main.cpp")
         )
@@ -22,7 +19,7 @@ class TestCase(TestBase):
             "sp_empty",
             type="std::shared_ptr<int>",
             summary="nullptr",
-            children=[ValueCheck(name="__ptr_")],
+            children=[ValueCheck(name="pointer")],
         )
         self.assertEqual(
             valobj.child[0].GetValueAsUnsigned(lldb.LLDB_INVALID_ADDRESS), 0
@@ -35,7 +32,7 @@ class TestCase(TestBase):
         valobj = self.expect_var_path(
             "sp_int",
             type="std::shared_ptr<int>",
-            children=[ValueCheck(name="__ptr_")],
+            children=[ValueCheck(name="pointer")],
         )
         self.assertRegex(valobj.summary, r"^10( strong=1)? weak=0$")
         self.assertNotEqual(valobj.child[0].unsigned, 0)
@@ -43,7 +40,7 @@ class TestCase(TestBase):
         valobj = self.expect_var_path(
             "sp_int_ref",
             type="std::shared_ptr<int> &",
-            children=[ValueCheck(name="__ptr_")],
+            children=[ValueCheck(name="pointer")],
         )
         self.assertRegex(valobj.summary, r"^10( strong=1)? weak=0$")
         self.assertNotEqual(valobj.child[0].unsigned, 0)
@@ -51,29 +48,21 @@ class TestCase(TestBase):
         valobj = self.expect_var_path(
             "sp_int_ref_ref",
             type="std::shared_ptr<int> &&",
-            children=[ValueCheck(name="__ptr_")],
+            children=[ValueCheck(name="pointer")],
         )
         self.assertRegex(valobj.summary, r"^10( strong=1)? weak=0$")
         self.assertNotEqual(valobj.child[0].unsigned, 0)
 
-        if self.expectedCompiler(["clang"]) and self.expectedCompilerVersion(
-            [">", "16.0"]
-        ):
-            string_type = "std::string"
-        else:
-            string_type = "std::basic_string<char, std::char_traits<char>, std::allocator<char> > "
-
         valobj = self.expect_var_path(
             "sp_str",
-            type="std::shared_ptr<" + string_type + ">",
-            children=[ValueCheck(name="__ptr_", summary='"hello"')],
+            children=[ValueCheck(name="pointer", summary='"hello"')],
         )
         self.assertRegex(valobj.summary, r'^"hello"( strong=1)? weak=0$')
 
         valobj = self.expect_var_path("sp_user", type="std::shared_ptr<User>")
         self.assertRegex(
             valobj.summary,
-            "^std(::__[^:]*)?::shared_ptr<User>::element_type @ 0x0*[1-9a-f][0-9a-f]+( strong=1)? weak=0",
+            "element_type @ 0x0*[1-9a-f][0-9a-f]+( strong=1)? weak=0",
         )
         self.assertNotEqual(valobj.child[0].unsigned, 0)
 
@@ -85,7 +74,7 @@ class TestCase(TestBase):
                 ValueCheck(name="name", summary='"steph"'),
             ],
         )
-        self.assertEqual(str(valobj), '(User) *__ptr_ = (id = 30, name = "steph")')
+        self.assertEqual(str(valobj), '(User) *pointer = (id = 30, name = "steph")')
 
         self.expect_var_path("sp_user->id", type="int", value="30")
         self.expect_var_path("sp_user->name", type="std::string", summary='"steph"')
@@ -115,3 +104,13 @@ class TestCase(TestBase):
         self.expect_var_path("ptr_node->next->value", value="2")
         self.expect_var_path("(*ptr_node).value", value="1")
         self.expect_var_path("(*(*ptr_node).next).value", value="2")
+
+    @add_test_categories(["libc++"])
+    def test_libcxx(self):
+        self.build(dictionary={"USE_LIBCPP": 1})
+        self.do_test()
+
+    @add_test_categories(["libstdcxx"])
+    def test_libstdcxx(self):
+        self.build(dictionary={"USE_LIBSTDCPP": 1})
+        self.do_test()
