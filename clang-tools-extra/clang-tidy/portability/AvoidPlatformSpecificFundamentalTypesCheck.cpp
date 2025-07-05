5dd5c722485d85022988f0948d955ed62680f2df
JJ Marr <jjmarr@gmail.com>
2025-07-05 15:21:33 -0400

    Merge comment with previous line

--- clang-tools-extra/clang-tidy/portability/AvoidPlatformSpecificFundamentalTypesCheck.cpp
+++ clang-tools-extra/clang-tidy/portability/AvoidPlatformSpecificFundamentalTypesCheck.cpp
@@ -1,5 +1,4 @@
-//===--- AvoidPlatformSpecificFundamentalTypesCheck.cpp - clang-tidy
-//---------------===//
+//===--- AvoidPlatformSpecificFundamentalTypesCheck.cpp - clang-tidy ------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
