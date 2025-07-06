cbbc9241e0b620f7cc8e925fd83fe36dd94536a4
ultimateforce21 <abdullahmohammad155@gmail.com>
2025-07-06 14:19:12 -0400

    Add tests for disassembly variable annotations and decoding edge cases
    This patch adds API tests to verify that DWARF variable location
    annotations are correctly displayed in the disassembly output.
    
    The tests cover:
    - Local variables in loops and functions
    - Multiple stack variables
    - Control flow edge cases
    - Different optimization levels (-O0, -O1, -O2)
    - Ensuring decoding errors are excluded from output

---
+++ lldb/test/API/functionalities/rich-disassembler/a_loop_with_local_variable.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    for (int i = 0; i < 3; ++i) {
+        puts("Hi");
+    }
+    return 0;
+}
---
+++ lldb/test/API/functionalities/rich-disassembler/e_control_flow_edge.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a = 1;
+    if (a > 0) {
+        int b = 2;
+        return b;
+    }
+    return a;
+}
