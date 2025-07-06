c35fbb5460b476ecd210da024eb7c0dda4ad662b
Florian Hahn <flo@fhahn.com>
2025-07-06 20:49:34 +0100

    [VPlan] Use VPReductionPHIRecipe::isOrdered instead of CM (NFC).
    Directly retrieve isOrdered from the reduction phi recipe instead of
    going through the legacy cost model.
    
    Split off as suggested in
    https://github.com/llvm/llvm-project/pull/142322.

--- llvm/lib/Transforms/Vectorize/LoopVectorize.cpp
+++ llvm/lib/Transforms/Vectorize/LoopVectorize.cpp
@@ -9181,7 +9181,7 @@ void LoopVectorizationPlanner::adjustRecipesForReductions(
                                : FastMathFlags();
       auto *RedRecipe = new VPReductionRecipe(
           Kind, FMFs, CurrentLinkI, PreviousLink, VecOp, CondOp,
-          CM.useOrderedReductions(RdxDesc), CurrentLinkI->getDebugLoc());
+          PhiR->isOrdered(), CurrentLinkI->getDebugLoc());
       // Append the recipe to the end of the VPBasicBlock because we need to
       // ensure that it comes after all of it's inputs, including CondOp.
       // Delete CurrentLink as it will be invalid if its operand is replaced
