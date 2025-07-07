272d28b2dbf504cc6440e8b7b6dd24c7366d14c4
amsen <ahph1380@gmail.com>
2025-07-06 17:43:59 -0700

    Match starting docs for cpp files with new style guide

--- llvm/lib/DebugInfo/DWARF/DWARFUnwindTablePrinter.cpp
+++ llvm/lib/DebugInfo/DWARF/DWARFUnwindTablePrinter.cpp
@@ -1,4 +1,4 @@
-//=== DWARFUnwindTablePrinter.cpp - Print the cfi-portions of .debug_frame ===//
+//===----------------------------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
