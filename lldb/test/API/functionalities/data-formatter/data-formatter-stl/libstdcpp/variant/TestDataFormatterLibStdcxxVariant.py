be3eb2431b0649ce2730a4cf832dc474e262f49e
Michael Buch <michaelbuch12@gmail.com>
2025-07-07 12:40:37 +0100

    [lldb][test] Fix libstdc++ std::variant formatter for empty variant
    Instead of using the byte-size to make a guess at what the
    `std::variant_npos` value is, just look it up in debug-info.
    
    Unblocks https://github.com/llvm/llvm-project/pull/147253

--- lldb/test/API/functionalities/data-formatter/data-formatter-stl/libstdcpp/variant/TestDataFormatterLibStdcxxVariant.py
+++ lldb/test/API/functionalities/data-formatter/data-formatter-stl/libstdcpp/variant/TestDataFormatterLibStdcxxVariant.py
@@ -2,7 +2,6 @@
 Test lldb data formatter for LibStdC++ std::variant.
 """
 
-
 import lldb
 from lldbsuite.test.decorators import *
 from lldbsuite.test.lldbtest import *
@@ -62,9 +61,6 @@ class LibStdcxxVariantDataFormatterTestCase(TestBase):
             "frame variable v3",
             substrs=["v3 =  Active Type = char  {", "Value = 'A'", "}"],
         )
-        """
-        TODO: temporarily disable No Value tests as they seem to fail on ubuntu/debian
-        bots. Pending reproduce and investigation.
 
         self.expect("frame variable v_no_value", substrs=["v_no_value =  No Value"])
 
@@ -72,7 +68,6 @@ class LibStdcxxVariantDataFormatterTestCase(TestBase):
             "frame variable v_many_types_no_value",
             substrs=["v_many_types_no_value =  No Value"],
         )
-        """
 
     @add_test_categories(["libstdcxx"])
     def test_invalid_variant_index(self):
