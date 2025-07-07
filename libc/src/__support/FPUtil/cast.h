d6ae7dd8c82bc88f4d730b5d4efd27d150fc768e
Krishna Pandey <kpandey81930@gmail.com>
2025-07-07 19:49:06 +0530

    chore: fix typo `InType` -> `OutType`
    Signed-off-by: Krishna Pandey <kpandey81930@gmail.com>

--- libc/src/__support/FPUtil/cast.h
+++ libc/src/__support/FPUtil/cast.h
@@ -36,7 +36,7 @@ cast(InType x) {
   if constexpr (cpp::is_same_v<OutType, bfloat16> ||
                 cpp::is_same_v<InType, bfloat16>
 #if (defined(LIBC_TYPES_HAS_FLOAT16) && !defined(__LIBC_USE_FLOAT16_CONVERSION))
-                || cpp::is_same_v<InType, float16> ||
+                || cpp::is_same_v<OutType, float16> ||
                 cpp::is_same_v<InType, float16>
 #endif
   ) {
