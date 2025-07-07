a4078d7719f6a32521f2c0ad2f777d6b3cffcda4
Tom Sepez <tsepez@chromium.org>
2025-07-07 18:49:49 +0000

    next

--- clang/test/SemaCXX/warn-unsafe-buffer-usage-source-ranges.cpp
+++ clang/test/SemaCXX/warn-unsafe-buffer-usage-source-ranges.cpp
@@ -6,45 +6,45 @@ void foo(int i) {
   int * ptr;
 
   ptr++;
-  // CHECK: {[[@LINE-1]]:3-[[@LINE-1]]:6}
+  // CHECK: {[[@LINE-1]]:3-[[@LINE-1]]:8}
   ptr--;
-  // CHECK: {[[@LINE-1]]:3-[[@LINE-1]]:6}
+  // CHECK: {[[@LINE-1]]:3-[[@LINE-1]]:8}
   ++ptr;
-  // CHECK: {[[@LINE-1]]:5-[[@LINE-1]]:8}
+  // CHECK: {[[@LINE-1]]:3-[[@LINE-1]]:8}
   --ptr;
-  // CHECK: {[[@LINE-1]]:5-[[@LINE-1]]:8}
+  // CHECK: {[[@LINE-1]]:3-[[@LINE-1]]:8}
 
 
   ptr + 1;
-  // CHECK: {[[@LINE-1]]:3-[[@LINE-1]]:6}
+  // CHECK: {[[@LINE-1]]:3-[[@LINE-1]]:10}
   2 + ptr;
-  // CHECK: {[[@LINE-1]]:7-[[@LINE-1]]:10}
+  // CHECK: {[[@LINE-1]]:3-[[@LINE-1]]:10}
   ptr + i;
-  // CHECK: {[[@LINE-1]]:3-[[@LINE-1]]:6}
+  // CHECK: {[[@LINE-1]]:3-[[@LINE-1]]:10}
   i + ptr;
-  // CHECK: {[[@LINE-1]]:7-[[@LINE-1]]:10}
+  // CHECK: {[[@LINE-1]]:3-[[@LINE-1]]:10}
 
 
   ptr - 3;
-  // CHECK: {[[@LINE-1]]:3-[[@LINE-1]]:6}
+  // CHECK: {[[@LINE-1]]:3-[[@LINE-1]]:10}
   ptr - i;
-  // CHECK: {[[@LINE-1]]:3-[[@LINE-1]]:6}
+  // CHECK: {[[@LINE-1]]:3-[[@LINE-1]]:10}
 
 
   ptr += 4;
-  // CHECK: {[[@LINE-1]]:3-[[@LINE-1]]:6}
+  // CHECK: {[[@LINE-1]]:3-[[@LINE-1]]:11}
   ptr += i;
-  // CHECK: {[[@LINE-1]]:3-[[@LINE-1]]:6}
+  // CHECK: {[[@LINE-1]]:3-[[@LINE-1]]:11}
 
 
   ptr -= 5;
-  // CHECK: {[[@LINE-1]]:3-[[@LINE-1]]:6}
+  // CHECK: {[[@LINE-1]]:3-[[@LINE-1]]:11}
   ptr -= i;
-  // CHECK: {[[@LINE-1]]:3-[[@LINE-1]]:6}
+  // CHECK: {[[@LINE-1]]:3-[[@LINE-1]]:11}
 
 
   ptr[5];
-  // CHECK: {[[@LINE-1]]:3-[[@LINE-1]]:6}
+  // CHECK: {[[@LINE-1]]:3-[[@LINE-1]]:9}
   5[ptr];
-  // CHECK: {[[@LINE-1]]:5-[[@LINE-1]]:8}
+  // CHECK: {[[@LINE-1]]:3-[[@LINE-1]]:9}
 }
