41fb4e9e8c6a15e4ae080a60f3b311124366775c
Jacques Pienaar <jpienaar@google.com>
2025-07-08 14:20:14 +0000

    Fix cmake

--- llvm/unittests/Support/DebugLogTest.cpp
+++ llvm/unittests/Support/DebugLogTest.cpp
@@ -1,5 +1,4 @@
-//===- llvm/unittest/Support/DebugLogTest.cpp
-//------------------------------===//
+//===- llvm/unittest/Support/DebugLogTest.cpp -----------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
