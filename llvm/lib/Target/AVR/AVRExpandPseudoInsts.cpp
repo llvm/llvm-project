2a3a9507704efc64b9e6d152b1da4aca6aaa15ad
Tom Vijlbrief <tvijlbrief@gmail.com>
2025-07-08 11:44:03 +0200

    Fix another SPL only test

--- llvm/lib/Target/AVR/AVRExpandPseudoInsts.cpp
+++ llvm/lib/Target/AVR/AVRExpandPseudoInsts.cpp
@@ -2554,7 +2554,7 @@ bool AVRExpandPseudo::expand<AVR::SPWRITE>(Block &MBB, BlockIt MBBI) {
       .setMIFlags(Flags);
 
   if (STI.getELFArch() < 102 &&
-      STI.getIORegSPH() >= 0) { // Not an XMEGA device and not just SPL
+      STI.getIORegSPH() >= 0) // Not an XMEGA device and not just SPL
     buildMI(MBB, MBBI, AVR::OUTARr)
         .addImm(STI.getIORegSREG())
         .addReg(STI.getTmpRegister(), RegState::Kill)
