f2d1a28630f377bcc22765c6c0090bcb049b7069
Andrew Rogers <andrurogerz@gmail.com>
2025-07-08 09:37:33 -0700

    [llvm] manual fix-ups to Demangle library code-mod

--- llvm/include/llvm/Demangle/MicrosoftDemangleNodes.h
+++ llvm/include/llvm/Demangle/MicrosoftDemangleNodes.h
@@ -13,7 +13,7 @@
 #ifndef LLVM_DEMANGLE_MICROSOFTDEMANGLENODES_H
 #define LLVM_DEMANGLE_MICROSOFTDEMANGLENODES_H
 
-#include "llvm/Demangle/Visibility.h"
+#include "Visibility.h"
 #include <array>
 #include <cstdint>
 #include <string>
