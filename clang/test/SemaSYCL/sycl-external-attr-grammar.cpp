1b3a198584d5ec1c2f8ef2917531a8d386fe2aa1
Sindhu Chittireddy <sindhu.chittireddy@intel.com>
2025-07-08 10:42:37 -0700

    Update diagnostic messages in tests

--- clang/test/SemaSYCL/sycl-external-attr-grammar.cpp
+++ clang/test/SemaSYCL/sycl-external-attr-grammar.cpp
@@ -1,15 +1,15 @@
 // RUN: %clang_cc1 -fsycl-is-device -fsyntax-only -verify %s
 
-// expected-error@+1{{'sycl_external' attribute only applies to functions}}
+// expected-error@+1{{'clang::sycl_external' attribute only applies to functions}}
 [[clang::sycl_external]] int a;
 
 
-// expected-error@+2{{'sycl_external' attribute only applies to functions}}
+// expected-error@+2{{'clang::sycl_external' attribute only applies to functions}}
 struct s {
 [[clang::sycl_external]] int b;
 };
 
-// expected-error@+1{{'sycl_external' attribute takes no arguments}}
+// expected-error@+1{{'clang::sycl_external' attribute takes no arguments}}
 [[clang::sycl_external(3)]] void bar() {}
 
 // FIXME: The first declaration of a function is required to have the attribute.
