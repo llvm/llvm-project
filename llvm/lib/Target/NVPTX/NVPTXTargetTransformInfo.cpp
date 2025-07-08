1102a58fc1bb83cf973e1c552a7f25f03d6a0588
Abhilash Majumder <abmajumder@nvidia.com>
2025-07-08 12:13:14 +0530

    refresh 2

--- llvm/lib/Target/NVPTX/NVPTXTargetTransformInfo.cpp
+++ llvm/lib/Target/NVPTX/NVPTXTargetTransformInfo.cpp
@@ -589,7 +589,13 @@ Value *NVPTXTTIImpl::rewriteIntrinsicWithAddressSpace(IntrinsicInst *II,
     return nullptr;
   }
   case Intrinsic::nvvm_prefetch_tensormap: {
-    return nullptr;
+    IRBuilder<> Builder(II);
+    Module *M = II->getModule();
+    Function *NewIntrinsic = Intrinsic::getDeclaration(
+        M, Intrinsic::nvvm_prefetch_tensormap, {NewV->getType()});
+    CallInst *NewCall = Builder.CreateCall(NewIntrinsic, {NewV});
+    NewCall->setAttributes(II->getAttributes());
+    return NewCall;
   }
   }
   return nullptr;
