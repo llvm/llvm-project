033d5f13583bd27baff51f9cf3f0b9e68ca0f58a
s-watanabe314 <watanabe.shu-06@fujitsu.com>
2025-07-08 15:50:51 +0900

    [clang][driver] Improve warning message for complex range overrides and GCC incompatibility
    This patch improves the warnings to show which user options overrides
    the complex range. It also warns when Clang's last-flag-wins rule causes
    incompatibility with GCC. When both warnings are necessary, a combined
    warning message is output by default, and the warning message can be
    changed using the -W option.
    
    See also the discussion in the following discourse post:
    https://discourse.llvm.org/t/the-priority-of-fno-fast-math-regarding-complex-number-calculations/84679

--- clang/lib/Driver/ToolChains/CommonArgs.cpp
+++ clang/lib/Driver/ToolChains/CommonArgs.cpp
@@ -3435,6 +3435,9 @@ std::string tools::complexRangeKindToStr(LangOptions::ComplexRangeKind Range) {
   case LangOptions::ComplexRangeKind::CX_Promoted:
     return "promoted";
     break;
+  case LangOptions::ComplexRangeKind::CX_None:
+    return "none";
+    break;
   default:
     return "";
   }
