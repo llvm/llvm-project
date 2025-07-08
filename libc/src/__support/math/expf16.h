8437b69e453db2facd0f850cf2bc678df4596952
bassiounix <muhammad.m.bassiouni@gmail.com>
2025-07-08 19:09:42 +0300

    Remove `expf16_constants.h`

--- libc/src/__support/math/expf16.h
+++ libc/src/__support/math/expf16.h
@@ -9,8 +9,6 @@
 #ifndef LLVM_LIBC_SRC___SUPPORT_MATH_EXPF16_H
 #define LLVM_LIBC_SRC___SUPPORT_MATH_EXPF16_H
 
-#include "exp_float16_constants.h"
-
 #include "hdr/errno_macros.h"
 #include "hdr/fenv_macros.h"
 #include "src/__support/FPUtil/FEnvImpl.h"
@@ -28,7 +26,27 @@ namespace LIBC_NAMESPACE_DECL {
 
 namespace math {
 
-static constexpr float16 expf16(float16 x) {
+LIBC_INLINE static constexpr float16 expf16(float16 x) {
+#ifndef LIBC_MATH_HAS_SKIP_ACCURATE_PASS
+  constexpr fputil::ExceptValues<float16, 2> EXPF16_EXCEPTS_LO = {{
+      // (input, RZ output, RU offset, RD offset, RN offset)
+      // x = 0x1.de4p-8, expf16(x) = 0x1.01cp+0 (RZ)
+      {0x1f79U, 0x3c07U, 1U, 0U, 0U},
+      // x = 0x1.73cp-6, expf16(x) = 0x1.05cp+0 (RZ)
+      {0x25cfU, 0x3c17U, 1U, 0U, 0U},
+  }};
+
+  constexpr fputil::ExceptValues<float16, 3> EXPF16_EXCEPTS_HI = {{
+      // (input, RZ output, RU offset, RD offset, RN offset)
+      // x = 0x1.c34p+0, expf16(x) = 0x1.74cp+2 (RZ)
+      {0x3f0dU, 0x45d3U, 1U, 0U, 1U},
+      // x = -0x1.488p-5, expf16(x) = 0x1.ebcp-1 (RZ)
+      {0xa922U, 0x3bafU, 1U, 0U, 0U},
+      // x = -0x1.55p-5, expf16(x) = 0x1.ebp-1 (RZ)
+      {0xa954U, 0x3bacU, 1U, 0U, 0U},
+  }};
+#endif // !LIBC_MATH_HAS_SKIP_ACCURATE_PASS
+
   using FPBits = fputil::FPBits<float16>;
   FPBits x_bits(x);
 
