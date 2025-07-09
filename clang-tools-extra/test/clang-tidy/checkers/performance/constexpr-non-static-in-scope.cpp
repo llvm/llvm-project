1850a9964c7d1213e395daaa5e522c8a56cc3908
Spaarsh <spaarshthakkar11010@gmail.com>
2025-07-10 01:02:00 +0530

    [ADDED] Tests

--- clang-tools-extra/test/clang-tidy/checkers/performance/constexpr-non-static-in-scope.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/performance/constexpr-non-static-in-scope.cpp
@@ -1,14 +1,31 @@
-// RUN: %check_clang_tidy %s performance-constexpr-non-static-in-scope %t
+// RUN: %check_clang_tidy %s performance-constexpr-non-static-in-scope %t -- -- -std=c++23
 
-// FIXME: Add something that triggers the check here.
-void f();
-// CHECK-MESSAGES: :[[@LINE-1]]:6: warning: function 'f' is insufficiently awesome [performance-constexpr-non-static-in-scope]
+// This should trigger the check (constexpr local, not static).
+void f() {
+  constexpr int x = 42;
+  // CHECK-MESSAGES: :[[@LINE-1]]:3: warning: constexpr variable in function scope should be static to ensure static lifetime [performance-constexpr-non-static-in-scope]
+  // CHECK-FIXES: static constexpr int x = 42;
+}
 
-// FIXME: Verify the applied fix.
-//   * Make the CHECK patterns specific enough and try to make verified lines
-//     unique to avoid incorrect matches.
-//   * Use {{}} for regular expressions.
-// CHECK-FIXES: {{^}}void awesome_f();{{$}}
+// This should trigger if WarnInConstexprFuncCpp23 is true and C++23 or newer.
+constexpr int g() {
+  constexpr int y = 123;
+  // CHECK-MESSAGES: :[[@LINE-1]]:3: warning: constexpr variable in function scope should be static to ensure static lifetime [performance-constexpr-non-static-in-scope]
+  // CHECK-FIXES: static constexpr int y = 123;
+  return y;
+}
 
-// FIXME: Add something that doesn't trigger the check here.
-void awesome_f2();
+// This should NOT trigger the check (already static).
+void h() {
+  static constexpr int z = 99;
+}
+
+// This should NOT trigger the check (not constexpr).
+void i() {
+  int w = 100;
+}
+
+// This should  NOT trigger the check (not declared inside a function)
+namespace ns {
+    inline constexpr int MAX_SIZE = 1024;
+}
