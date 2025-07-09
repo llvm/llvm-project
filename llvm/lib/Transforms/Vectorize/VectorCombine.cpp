b9b87480bf8ba3db8450b3c66d8c6a185a0cdb79
Leon Clark <leoclark@amd.com>
2025-07-09 01:31:49 +0100

    Formatting.

--- llvm/lib/Transforms/Vectorize/VectorCombine.cpp
+++ llvm/lib/Transforms/Vectorize/VectorCombine.cpp
@@ -4059,9 +4059,8 @@ bool VectorCombine::shrinkPhiOfShuffles(Instruction &I) {
   auto OldCost =
       std::max(TTI.getShuffleCost(Kind, ResultVT, InputVT, Mask0, CostKind),
                TTI.getShuffleCost(Kind, ResultVT, InputVT, Mask1, CostKind));
-  auto NewCost =
-      TTI.getShuffleCost(Kind, InputVT, InputVT, NewMask, CostKind) +
-      TTI.getShuffleCost(Kind, ResultVT, InputVT, Mask1, CostKind);
+  auto NewCost = TTI.getShuffleCost(Kind, InputVT, InputVT, NewMask, CostKind) +
+                 TTI.getShuffleCost(Kind, ResultVT, InputVT, Mask1, CostKind);
 
   if (NewCost > OldCost)
     return false;
