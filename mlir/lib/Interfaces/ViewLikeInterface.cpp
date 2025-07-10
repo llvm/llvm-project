41481dfdc1a159efe1724712c96f1a680a4d4fa6
Maya Amrami <mayaam88@gmail.com>
2025-07-10 12:05:48 +0300

    [mlir] ViewLikeInterface - verify ranks in verifyOffsetSizeAndStrideOp
    getMixedOffsets() calls getMixedValues() with `static_offsets` and `offsets`.
    It is assumed that the number of dynamic offsets in `static_offsets` equals
    the rank of `offsets`. Otherwise, we fail on assert when trying to
    access an array out of its bounds.
    The same applies to getMixedStrides() and getMixedOffsets().
    
    A verification of this assumption is added to verifyOffsetSizeAndStrideOp()
    and a clear assert is added in getMixedValues().

--- mlir/lib/Interfaces/ViewLikeInterface.cpp
+++ mlir/lib/Interfaces/ViewLikeInterface.cpp
@@ -94,6 +94,32 @@ SliceBoundsVerificationResult mlir::verifyInBoundsSlice(
 
 LogicalResult
 mlir::detail::verifyOffsetSizeAndStrideOp(OffsetSizeAndStrideOpInterface op) {
+  // A dynamic size is represented as ShapedType::kDynamic in `static_sizes`.
+  // Its corresponding Value appears in `sizes`. Thus, the number of dynamic
+  // dimensions in `static_sizes` must equal the rank of `sizes`.
+  // The same applies to strides and offsets.
+  unsigned int numDynamicDims =
+      llvm::count_if(op.getStaticSizes(), ShapedType::isDynamic);
+  if (op.getSizes().size() != numDynamicDims) {
+    return op->emitError("expected sizes rank to match the number of dynamic "
+                         "dimensions (")
+           << op.getSizes().size() << " vs " << numDynamicDims << ")";
+  }
+  unsigned int numDynamicStrides =
+      llvm::count_if(op.getStaticStrides(), ShapedType::isDynamic);
+  if (op.getStrides().size() != numDynamicStrides) {
+    return op->emitError("expected strides rank to match the number of dynamic "
+                         "strides (")
+           << op.getStrides().size() << " vs " << numDynamicStrides << ")";
+  }
+  unsigned int numDynamicOffsets =
+      llvm::count_if(op.getStaticOffsets(), ShapedType::isDynamic);
+  if (op.getOffsets().size() != numDynamicOffsets) {
+    return op->emitError("expected offsets rank to match the number of dynamic "
+                         "offsets (")
+           << op.getOffsets().size() << " vs " << numDynamicOffsets << ")";
+  }
+
   std::array<unsigned, 3> maxRanks = op.getArrayAttrMaxRanks();
   // Offsets can come in 2 flavors:
   //   1. Either single entry (when maxRanks == 1).
