75289e703e02bf9005736e095d26ba23a16c3189
Dunbobbin <Ben.Dunbobbin@sony.com>
2025-07-10 17:00:00 +0100

    Use shared response files for all FileCheck invocations.

--- clang/test/Driver/DTLTO/dtlto.c
+++ clang/test/Driver/DTLTO/dtlto.c
@@ -2,6 +2,13 @@
 
 /// Check DTLTO options are forwarded to the linker.
 
+/// Create a response file for all FileCheck invocations to share. These implicit
+/// checks ensure that all lines which mention DTLTO options are checked,
+/// and that no unexpected warnings appear.
+// RUN: echo " \"%s\" --implicit-check-not=distributor \
+// RUN:   --implicit-check-not=remote-compiler \
+// RUN:   --implicit-check-not=warning:" > %t_f.rsp
+
 // RUN: echo "--target=x86_64-linux-gnu \
 // RUN:   -Xthinlto-distributor=a1 \
 // RUN:   -Xthinlto-distributor=a2,a3 \
@@ -9,9 +16,7 @@
 
 /// Check that options are forwarded as expected with --thinlto-distributor=.
 // RUN: %clang -### @%t.rsp -fthinlto-distributor=d.exe %s 2>&1 | \
-// RUN:   FileCheck %s --implicit-check-not=distributor \
-// RUN:   --implicit-check-not=remote-compiler \
-// RUN:   --implicit-check-not=warning: --check-prefix=FORWARD
+// RUN:   FileCheck @%t_f.rsp --check-prefix=FORWARD
 
 // FORWARD: ld.lld
 // FORWARD-SAME: "--thinlto-distributor=d.exe"
@@ -25,9 +30,7 @@
 /// that there is an unused option warning issued for -Xthinlto-distributor=
 /// options. We specify -flto here as these options should be unaffected by it.
 // RUN: %clang -### @%t.rsp -flto=thin %s 2>&1 | \
-// RUN:   FileCheck %s --check-prefix=NODIST --implicit-check-not=distributor \
-// RUN:     --implicit-check-not=remote-compiler \
-// RUN:     --implicit-check-not=warning:
+// RUN:   FileCheck @%t_f.rsp --check-prefix=NODIST
 
 // NODIST: warning: argument unused during compilation: '-Xthinlto-distributor=a1'
 // NODIST: warning: argument unused during compilation: '-Xthinlto-distributor=a2,a3'
@@ -38,9 +41,7 @@
 /// --thinlto-distributor=.
 // RUN: %clang --target=x86_64-linux-gnu -fthinlto-distributor=d.exe \
 // RUN:   -fuse-ld=lld -Werror -### %s 2>&1 | \
-// RUN:   FileCheck %s --check-prefix=DEFAULT --implicit-check-not=distributor \
-// RUN:     --implicit-check-not=remote-compiler \
-// RUN:     --implicit-check-not=warning:
+// RUN:   FileCheck @%t_f.rsp --check-prefix=DEFAULT
 
 // DEFAULT: ld.lld
 // DEFAULT-SAME: "--thinlto-distributor=d.exe"
