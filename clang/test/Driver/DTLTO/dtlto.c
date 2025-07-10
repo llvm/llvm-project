cdce7ffdee915ac9b9723106f35e872df62b0aa7
Dunbobbin <Ben.Dunbobbin@sony.com>
2025-07-10 16:57:42 +0100

    NOMORE -> --implicit-check-not

--- clang/test/Driver/DTLTO/dtlto.c
+++ clang/test/Driver/DTLTO/dtlto.c
@@ -9,7 +9,9 @@
 
 /// Check that options are forwarded as expected with --thinlto-distributor=.
 // RUN: %clang -### @%t.rsp -fthinlto-distributor=d.exe %s 2>&1 | \
-// RUN:   FileCheck %s --implicit-check-not=warning --check-prefix=FORWARD
+// RUN:   FileCheck %s --implicit-check-not=distributor \
+// RUN:   --implicit-check-not=remote-compiler \
+// RUN:   --implicit-check-not=warning: --check-prefix=FORWARD
 
 // FORWARD: ld.lld
 // FORWARD-SAME: "--thinlto-distributor=d.exe"
@@ -23,20 +25,22 @@
 /// that there is an unused option warning issued for -Xthinlto-distributor=
 /// options. We specify -flto here as these options should be unaffected by it.
 // RUN: %clang -### @%t.rsp -flto=thin %s 2>&1 | \
-// RUN:   FileCheck %s --check-prefixes=NODIST,NOMORE --implicit-check-not=warning
+// RUN:   FileCheck %s --check-prefix=NODIST --implicit-check-not=distributor \
+// RUN:     --implicit-check-not=remote-compiler \
+// RUN:     --implicit-check-not=warning:
 
 // NODIST: warning: argument unused during compilation: '-Xthinlto-distributor=a1'
 // NODIST: warning: argument unused during compilation: '-Xthinlto-distributor=a2,a3'
 // NODIST: ld.lld
-// NOMORE-NOT: distributor
-// NOMORE-NOT: remote-compiler
 
 
 /// Check the expected arguments are forwarded by default with only
 /// --thinlto-distributor=.
 // RUN: %clang --target=x86_64-linux-gnu -fthinlto-distributor=d.exe \
 // RUN:   -fuse-ld=lld -Werror -### %s 2>&1 | \
-// RUN:   FileCheck %s --check-prefixes=DEFAULT,NOMORE
+// RUN:   FileCheck %s --check-prefix=DEFAULT --implicit-check-not=distributor \
+// RUN:     --implicit-check-not=remote-compiler \
+// RUN:     --implicit-check-not=warning:
 
 // DEFAULT: ld.lld
 // DEFAULT-SAME: "--thinlto-distributor=d.exe"
