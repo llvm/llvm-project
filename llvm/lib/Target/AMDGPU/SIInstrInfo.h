2eb21efa60ba9ea43600f6fc563bb12b1542fbb5
shore <372660931@qq.com>
2025-07-10 10:55:01 +0800

    fis jmmartinez's comments

--- llvm/lib/Target/AMDGPU/SIInstrInfo.h
+++ llvm/lib/Target/AMDGPU/SIInstrInfo.h
@@ -1562,7 +1562,7 @@ loadMBUFScalarOperandsFromVGPR(const SIInstrInfo &TII, MachineInstr &MI,
                                MachineDominatorTree *MDT,
                                MachineBasicBlock::iterator Begin = nullptr,
                                MachineBasicBlock::iterator End = nullptr,
-                               SmallVector<Register> PhySGPRs = {});
+                               ArrayRef<Register> PhySGPRs = {});
 
 namespace AMDGPU {
 
