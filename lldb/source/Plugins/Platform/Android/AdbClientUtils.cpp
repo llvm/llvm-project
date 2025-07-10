b4f72a418c603ef7bf941c7313971310f1334270
Chad Smith <cs01@users.noreply.github.com>
2025-07-09 22:24:25 -0700

    fix headers

--- lldb/source/Plugins/Platform/Android/AdbClientUtils.cpp
+++ lldb/source/Plugins/Platform/Android/AdbClientUtils.cpp
@@ -1,3 +1,10 @@
+//===-- AdbClientUtils.cpp ------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
 #include "lldb/Utility/Connection.h"
 #include "AdbClientUtils.h"
 #include "lldb/Utility/LLDBLog.h"
