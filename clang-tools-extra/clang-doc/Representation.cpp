cb06509878ceb4e7e256070451d87d3656442327
Erick Velez <erickvelez7@gmail.com>
2025-07-11 13:39:41 -0700

    [clang-doc] Serialize record files with mangled name (#148021)
    This patch changes JSON file serialization. Now, files are serialized
    to a single directory instead of nesting them based on namespaces. The
    global namespace retains the "index.json" name.
    
    This solves the problem of class template specializations being serialized to the
    same file as its base template. This is also planned as part of
    future integration with the Mustache generator which will consume the JSON files.

--- clang-tools-extra/clang-doc/Representation.cpp
+++ clang-tools-extra/clang-doc/Representation.cpp
@@ -290,6 +290,8 @@ void SymbolInfo::merge(SymbolInfo &&Other) {
   auto *Last = llvm::unique(Loc);
   Loc.erase(Last, Loc.end());
   mergeBase(std::move(Other));
+  if (MangledName.empty())
+    MangledName = std::move(Other.MangledName);
 }
 
 NamespaceInfo::NamespaceInfo(SymbolID USR, StringRef Name, StringRef Path)
