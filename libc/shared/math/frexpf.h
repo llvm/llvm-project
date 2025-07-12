accacf1b2b36fe05680d8c5faece7b24bbb92a2c
Muhammad Bassiouni <60100307+bassiounix@users.noreply.github.com>
2025-07-12 18:08:58 +0300

    [libc][math] Refactor frexpf implementation to header-only in src/__support/math folder. (#147893)
    Part of #147386
    
    in preparation for:
    https://discourse.llvm.org/t/rfc-make-clang-builtin-math-functions-constexpr-with-llvm-libc-to-support-c-23-constexpr-math-functions/86450

--- libc/shared/math/frexpf.h
+++ libc/shared/math/frexpf.h
@@ -1,4 +1,5 @@
-//===-- Implementation of frexpf function ---------------------------------===//
+//===-- Shared frexpf function ------------------------------------*- C++
+//-*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,14 +7,18 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "src/math/frexpf.h"
+#ifndef LLVM_LIBC_SHARED_MATH_FREXPF_H
+#define LLVM_LIBC_SHARED_MATH_FREXPF_H
 
+#include "shared/libc_common.h"
 #include "src/__support/math/frexpf.h"
 
 namespace LIBC_NAMESPACE_DECL {
+namespace shared {
 
-LLVM_LIBC_FUNCTION(float, frexpf, (float x, int *exp)) {
-  return math::frexpf(x, exp);
-}
+using math::frexpf;
 
+} // namespace shared
 } // namespace LIBC_NAMESPACE_DECL
+
+#endif // LLVM_LIBC_SHARED_MATH_FREXPF_H
