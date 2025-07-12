5826e7baa8153ef175fe12e69fb9d78e837abbfd
Amr Hesham <amr96@programmer.net>
2025-07-12 17:22:57 +0200

    [CIR] Implement SubOp for ComplexType (#148025)
    This change adds support for SubOp for ComplexType
    
    https://github.com/llvm/llvm-project/issues/141365

--- clang/lib/CIR/CodeGen/CIRGenExprComplex.cpp
+++ clang/lib/CIR/CodeGen/CIRGenExprComplex.cpp
@@ -74,6 +74,7 @@ public:
   mlir::Value emitPromotedComplexOperand(const Expr *e, QualType promotionTy);
 
   mlir::Value emitBinAdd(const BinOpInfo &op);
+  mlir::Value emitBinSub(const BinOpInfo &op);
 
   QualType getPromotionType(QualType ty, bool isDivOpCode = false) {
     if (auto *complexTy = ty->getAs<ComplexType>()) {
@@ -105,6 +106,7 @@ public:
   }
 
   HANDLEBINOP(Add)
+  HANDLEBINOP(Sub)
 #undef HANDLEBINOP
 };
 } // namespace
@@ -345,6 +347,7 @@ mlir::Value ComplexExprEmitter::emitPromoted(const Expr *e,
   case BO_##OP:                                                                \
     return emitBin##OP(emitBinOps(bo, promotionTy));
       HANDLE_BINOP(Add)
+      HANDLE_BINOP(Sub)
 #undef HANDLE_BINOP
     default:
       break;
@@ -388,6 +391,12 @@ mlir::Value ComplexExprEmitter::emitBinAdd(const BinOpInfo &op) {
   assert(!cir::MissingFeatures::fastMathFlags());
   assert(!cir::MissingFeatures::cgFPOptionsRAII());
   return builder.create<cir::ComplexAddOp>(op.loc, op.lhs, op.rhs);
+}
+
+mlir::Value ComplexExprEmitter::emitBinSub(const BinOpInfo &op) {
+  assert(!cir::MissingFeatures::fastMathFlags());
+  assert(!cir::MissingFeatures::cgFPOptionsRAII());
+  return builder.create<cir::ComplexSubOp>(op.loc, op.lhs, op.rhs);
 }
 
 LValue CIRGenFunction::emitComplexAssignmentLValue(const BinaryOperator *e) {
