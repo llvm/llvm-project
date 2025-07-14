2e9043820993cb7f1b5d8eafd8fdd09a594009e2
zebullax <zebullax@gmail.com>
2025-07-14 21:16:17 +0900

    Fix formatting
    Signed-off-by: zebullax <zebullax@gmail.com>

--- clang/lib/Sema/SemaDeclAttr.cpp
+++ clang/lib/Sema/SemaDeclAttr.cpp
@@ -2921,8 +2921,8 @@ static void handleWarnUnusedResult(Sema &S, Decl *D, const ParsedAttr &AL) {
       // If this is spelled as the standard C++17 attribute, but not in C++17,
       // warn about using it as an extension. If there are attribute arguments,
       // then claim it's a C++20 extension instead. C23 supports this attribute
-     // with the message; no extension warning is needed there beyond the one
-     // already issued for accepting attributes in older modes.
+      // with the message; no extension warning is needed there beyond the one
+      // already issued for accepting attributes in older modes.
       const LangOptions &LO = S.getLangOpts();
       if (AL.getNumArgs() == 1) {
         if (LO.CPlusPlus && !LO.CPlusPlus20)
