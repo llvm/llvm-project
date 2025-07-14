264c2bf933d13819429326ebed6ba366025dae26
Muhammad Bassiouni <60100307+bassiounix@users.noreply.github.com>
2025-07-14 19:21:27 +0300

    [libc][math] Refactor exp implementation to header-only in src/__support/math folder. (#148091)
    Part of #147386
    
    in preparation for:
    https://discourse.llvm.org/t/rfc-make-clang-builtin-math-functions-constexpr-with-llvm-libc-to-support-c-23-constexpr-math-functions/86450

--- libc/shared/math/exp.h
+++ libc/shared/math/exp.h
@@ -1,4 +1,4 @@
-//===-- Double-precision e^x function -------------------------------------===//
+//===-- Shared exp function -------------------------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,10 +6,18 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "src/math/exp.h"
+#ifndef LLVM_LIBC_SHARED_MATH_EXP_H
+#define LLVM_LIBC_SHARED_MATH_EXP_H
+
+#include "shared/libc_common.h"
 #include "src/__support/math/exp.h"
+
 namespace LIBC_NAMESPACE_DECL {
+namespace shared {
 
-LLVM_LIBC_FUNCTION(double, exp, (double x)) { return math::exp(x); }
+using math::exp;
 
+} // namespace shared
 } // namespace LIBC_NAMESPACE_DECL
+
+#endif // LLVM_LIBC_SHARED_MATH_EXP_H
