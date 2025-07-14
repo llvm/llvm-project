bcaaa69acac90eceb3d09c81df9ba4af959021ae
Shilei Tian <i@tianshilei.me>
2025-07-14 16:30:18 -0400

    [AMDGPU] Add support for `v_tanh_bf16` on gfx1250 (#147425)
    Co-authored-by: Mekhanoshin, Stanislav <Stanislav.Mekhanoshin@amd.com>

--- llvm/lib/Target/AMDGPU/AMDGPURegisterBankInfo.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPURegisterBankInfo.cpp
@@ -4546,6 +4546,7 @@ AMDGPURegisterBankInfo::getInstrMapping(const MachineInstr &MI) const {
     case Intrinsic::amdgcn_rcp_legacy:
     case Intrinsic::amdgcn_rsq_legacy:
     case Intrinsic::amdgcn_rsq_clamp:
+    case Intrinsic::amdgcn_tanh:
     case Intrinsic::amdgcn_fmul_legacy:
     case Intrinsic::amdgcn_fma_legacy:
     case Intrinsic::amdgcn_frexp_mant:
