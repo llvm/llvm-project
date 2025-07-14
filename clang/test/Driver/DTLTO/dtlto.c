93dda6fc8414a37b1bdede308b0902655903fb0b
Dunbobbin <Ben.Dunbobbin@sony.com>
2025-07-14 10:57:24 +0100

    Use -Werror and remove FileCheck response file

--- clang/test/Driver/DTLTO/dtlto.c
+++ clang/test/Driver/DTLTO/dtlto.c
@@ -2,13 +2,6 @@
 
 /// Check DTLTO options are forwarded to the linker.
 
-/// Create a response file for all FileCheck invocations to share. These implicit
-/// checks ensure that all lines which mention DTLTO options are checked,
-/// and that no unexpected warnings appear.
-// RUN: echo " \"%/s\" --implicit-check-not=distributor \
-// RUN:   --implicit-check-not=remote-compiler \
-// RUN:   --implicit-check-not=warning:" > %t_f.rsp
-
 /// Create a response file to check that explicitly specified -Xthinlto-distributor
 /// options are forwarded correctly.
 // RUN: echo "-flto=thin \"%/s\" -### -fuse-ld=lld --target=x86_64-linux-gnu \
@@ -16,8 +9,8 @@
 // RUN:   -Xthinlto-distributor=a2,a3" > %t_l1.rsp
 
 /// Check that options are forwarded as expected with --thinlto-distributor=.
-// RUN: %clang @%t_l1.rsp -fthinlto-distributor=d.exe 2>&1 | \
-// RUN:   FileCheck @%t_f.rsp --check-prefix=FORWARD
+// RUN: %clang @%t_l1.rsp -fthinlto-distributor=d.exe -Werror 2>&1 | \
+// RUN:   FileCheck %s --check-prefix=FORWARD
 
 // FORWARD: ld.lld
 // FORWARD-SAME: "--thinlto-distributor=d.exe"
@@ -29,7 +22,7 @@
 /// Check that options are not added without --thinlto-distributor= and
 /// that a warning is issued for unused -Xthinlto-distributor options.
 // RUN: %clang @%t_l1.rsp 2>&1 | \
-// RUN:   FileCheck @%t_f.rsp --check-prefix=NODIST
+// RUN:   FileCheck %s --check-prefix=NODIST
 
 // NODIST: warning: argument unused during compilation: '-Xthinlto-distributor=a1'
 // NODIST: warning: argument unused during compilation: '-Xthinlto-distributor=a2,a3'
@@ -41,8 +34,8 @@
 
 /// Check the expected arguments are forwarded by default with only
 /// --thinlto-distributor=.
-// RUN: %clang -flto=thin @%t_l2.rsp 2>&1 | \
-// RUN:   FileCheck @%t_f.rsp --check-prefix=DEFAULT
+// RUN: %clang -flto=thin @%t_l2.rsp -Werror 2>&1 | \
+// RUN:   FileCheck %s --check-prefix=DEFAULT
 
 // DEFAULT: ld.lld
 // DEFAULT-SAME: "--thinlto-distributor=d.exe"
@@ -51,7 +44,7 @@
 /// Check that nothing is forwarded when the compiler is not in LTO mode, and that
 /// appropriate unused option warnings are issued.
 // RUN: %clang @%t_l2.rsp 2>&1 | \
-// RUN:   FileCheck @%t_f.rsp --check-prefix=NOFLTO
+// RUN:   FileCheck %s --check-prefix=NOFLTO
 
 // NOFLTO: warning: argument unused during compilation: '-fthinlto-distributor=d.exe'
 // NOFLTO: ld.lld
