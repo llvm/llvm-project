149c37b390b05a8159f0d14611bd19854f318dca
Corentin Jabot <corentinjabot@gmail.com>
2025-07-14 15:20:00 +0300

    [Clang] Do not emit -Wmissing-noreturn when [[noreturn]] is present (#148552)
    Fix a false positve warning which was introduced by #146234.

--- clang/test/SemaCXX/wmissing-noreturn-suggestion.cpp
+++ clang/test/SemaCXX/wmissing-noreturn-suggestion.cpp
@@ -21,3 +21,25 @@ int ensureZero(int i) {
   if (i == 0) return 0;
   throwError("ERROR"); // no-warning
 }
+
+
+template <typename Ex>
+[[noreturn]]
+void tpl_throws(Ex const& e) {
+    throw e;
+}
+
+[[noreturn]]
+void tpl_throws_test() {
+    tpl_throws(0);
+}
+
+[[gnu::noreturn]]
+int gnu_throws() {
+    throw 0;
+}
+
+[[noreturn]]
+int cxx11_throws() {
+    throw 0;
+}
