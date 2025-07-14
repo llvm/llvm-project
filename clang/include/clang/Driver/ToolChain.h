3fae484f7e569e42ae99146791cbe2c422269205
Jakub Chlanda <jakub@codeplay.com>
2025-07-14 13:30:57 +0000

    [HIP][Clang][Driver] Move BC preference logic into ROCm detection
    This patch provides a single point for handling the logic behind
    choosing common bitcode libraries. The intention is that the users of
    ROCm installation detector will not have to rewrite options handling
    code each time the bitcode libraries are queried. This is not too
    distant from detectors for other architecture that encapsulate the
    similar decision making process, providing cleaner interface. The only
    flag left in `getCommonBitcodeLibs` (main point of entry) is
    `NeedsASanRT`, this is deliberate, as in order to calculate it we need
    to consult `ToolChain`.

--- clang/include/clang/Driver/ToolChain.h
+++ clang/include/clang/Driver/ToolChain.h
@@ -802,7 +802,8 @@ public:
 
   /// Get paths for device libraries.
   virtual llvm::SmallVector<BitCodeLibraryInfo, 12>
-  getDeviceLibs(const llvm::opt::ArgList &Args) const;
+  getDeviceLibs(const llvm::opt::ArgList &Args,
+                const Action::OffloadKind DeviceOffloadingKind) const;
 
   /// Add the system specific linker arguments to use
   /// for the given HIP runtime library type.
