5683cabac94b23efd3da833ed34cecf75163a546
Rana Pratap Reddy N <RanaPratapReddy.Nimmakayala@amd.com>
2025-07-15 00:47:24 -0500

    Revert "[clang] Add -Wuninitialized-const-pointer (#148337)"
    This reverts commit 00dacf8c22f065cb52efb14cd091d441f19b319e.

--- clang/lib/Sema/AnalysisBasedWarnings.cpp
+++ clang/lib/Sema/AnalysisBasedWarnings.cpp
@@ -994,14 +994,6 @@ static bool DiagnoseUninitializedConstRefUse(Sema &S, const VarDecl *VD,
   return !S.getDiagnostics().isLastDiagnosticIgnored();
 }
 
-/// Diagnose uninitialized const pointer usages.
-static bool DiagnoseUninitializedConstPtrUse(Sema &S, const VarDecl *VD,
-                                             const UninitUse &Use) {
-  S.Diag(Use.getUser()->getBeginLoc(), diag::warn_uninit_const_pointer)
-      << VD->getDeclName() << Use.getUser()->getSourceRange();
-  return !S.getDiagnostics().isLastDiagnosticIgnored();
-}
-
 /// DiagnoseUninitializedUse -- Helper function for diagnosing uses of an
 /// uninitialized variable. This manages the different forms of diagnostic
 /// emitted for particular types of uses. Returns true if the use was diagnosed
@@ -1607,9 +1599,9 @@ private:
     // a stable ordering.
     llvm::sort(*vec, [](const UninitUse &a, const UninitUse &b) {
       // Prefer the direct use of an uninitialized variable over its use via
-      // constant reference or pointer.
-      if (a.isConstRefOrPtrUse() != b.isConstRefOrPtrUse())
-        return b.isConstRefOrPtrUse();
+      // constant reference.
+      if (a.isConstRefUse() != b.isConstRefUse())
+        return b.isConstRefUse();
       // Prefer a more confident report over a less confident one.
       if (a.getKind() != b.getKind())
         return a.getKind() > b.getKind();
@@ -1619,9 +1611,6 @@ private:
     for (const auto &U : *vec) {
       if (U.isConstRefUse()) {
         if (DiagnoseUninitializedConstRefUse(S, vd, U))
-          return;
-      } else if (U.isConstPtrUse()) {
-        if (DiagnoseUninitializedConstPtrUse(S, vd, U))
           return;
       } else {
         // If we have self-init, downgrade all uses to 'may be uninitialized'.
@@ -2839,8 +2828,7 @@ void clang::sema::AnalysisBasedWarnings::IssueWarnings(
   if (!Diags.isIgnored(diag::warn_uninit_var, D->getBeginLoc()) ||
       !Diags.isIgnored(diag::warn_sometimes_uninit_var, D->getBeginLoc()) ||
       !Diags.isIgnored(diag::warn_maybe_uninit_var, D->getBeginLoc()) ||
-      !Diags.isIgnored(diag::warn_uninit_const_reference, D->getBeginLoc()) ||
-      !Diags.isIgnored(diag::warn_uninit_const_pointer, D->getBeginLoc())) {
+      !Diags.isIgnored(diag::warn_uninit_const_reference, D->getBeginLoc())) {
     if (CFG *cfg = AC.getCFG()) {
       UninitValsDiagReporter reporter(S);
       UninitVariablesAnalysisStats stats;
