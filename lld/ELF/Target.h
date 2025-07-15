08f36fab88fd8cac3048cb04fcd46833622db190
Brian Cain <brian.cain@oss.qualcomm.com>
2025-07-14 22:36:05 -0500

    [LLD] Merge .hexagon.attributes sections (#148098)
    Merge the attributes of object files being linked together. The
    `.hexagon.attributes` section can be used by loaders and analysis tools.
    This is similar to the .riscv.attributes, introduced in
    8a900f2438b4a167b98404565ad4da2645cc9330 /
    https://reviews.llvm.org/D138550.

--- lld/ELF/Target.h
+++ lld/ELF/Target.h
@@ -245,6 +245,7 @@ template <typename ELFT> void writeARMCmseImportLib(Ctx &);
 uint64_t getLoongArchPageDelta(uint64_t dest, uint64_t pc, RelType type);
 void riscvFinalizeRelax(int passes);
 void mergeRISCVAttributesSections(Ctx &);
+void mergeHexagonAttributesSections(Ctx &);
 void addArmInputSectionMappingSymbols(Ctx &);
 void addArmSyntheticSectionMappingSymbol(Defined *);
 void sortArmMappingSymbols(Ctx &);
