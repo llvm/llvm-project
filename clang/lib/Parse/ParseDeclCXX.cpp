c80347a825264474a51ee6e638434d3454a0a7e3
Corentin Jabot <corentinjabot@gmail.com>
2025-07-15 09:38:59 +0200

    [Clang] Fix a crash when parsing an invalid `decltype`
    We would try to exact an annotated token before checking
    if it was valid, leading to a crash when `decltype` was the only
    token that was parsed (which can happen in the absense of opening
    paren)
    
    Fixes #114815

--- clang/lib/Parse/ParseDeclCXX.cpp
+++ clang/lib/Parse/ParseDeclCXX.cpp
@@ -1132,14 +1132,14 @@ void Parser::AnnotateExistingDecltypeSpecifier(const DeclSpec &DS,
                                                SourceLocation EndLoc) {
   // make sure we have a token we can turn into an annotation token
   if (PP.isBacktrackEnabled()) {
-    PP.RevertCachedTokens(1);
-    if (DS.getTypeSpecType() == TST_error) {
+    if (DS.getTypeSpecType() == TST_error)
       // We encountered an error in parsing 'decltype(...)' so lets annotate all
       // the tokens in the backtracking cache - that we likely had to skip over
       // to get to a token that allows us to resume parsing, such as a
       // semi-colon.
       EndLoc = PP.getLastCachedTokenLocation();
-    }
+    else
+      PP.RevertCachedTokens(1);
   } else
     PP.EnterToken(Tok, /*IsReinject*/ true);
 
