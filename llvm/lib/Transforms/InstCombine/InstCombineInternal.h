9e0cb9199e578a88bb89d6d4a400f58a79a42d0a
Ahmed Bougacha <ahmed@bougacha.org>
2025-07-15 14:39:53 -0700

    [InstCombine] Combine ptrauth intrin. callee into same-key bundle. (#94707)
    Try to optimize a call to the result of a ptrauth intrinsic, potentially
    into the ptrauth call bundle:
        call(ptrauth.resign(p)), ["ptrauth"()] ->  call p, ["ptrauth"()]
        call(ptrauth.sign(p)),   ["ptrauth"()] ->  call p
    
    as long as the key/discriminator are the same in sign and auth-bundle,
    and we don't change the key in the bundle (to a potentially-invalid
    key.)
    
    Generating a plain call to a raw unauthenticated pointer is generally
    undesirable, but if we ended up seeing a naked ptrauth.sign in the first
    place, we already have suspicious code. Unauthenticated calls are also
    easier to spot than naked signs, so let the indirect call shine.
    
    
    Note that there is an arguably unsafe extension to this, where we don't
    bother checking that the key in bundle and intrinsic are the same (and
    also allow folding away an auth into a bundle.)
    
    This can end up generating calls with a bundle that has an invalid key
    (which an informed frontend wouldn't have otherwise done), which can be
    problematic. The C that generates that is straightforward but arguably
    unreasonable. That wouldn't be an issue if we were to bite the bullet
    and make these fully AArch64-specific, allowing key knowledge to be
    embedded here.

--- llvm/lib/Transforms/InstCombine/InstCombineInternal.h
+++ llvm/lib/Transforms/InstCombine/InstCombineInternal.h
@@ -283,6 +283,14 @@ private:
   Instruction *transformCallThroughTrampoline(CallBase &Call,
                                               IntrinsicInst &Tramp);
 
+  /// Try to optimize a call to the result of a ptrauth intrinsic, potentially
+  /// into the ptrauth call bundle:
+  /// - call(ptrauth.resign(p)), ["ptrauth"()] ->  call p, ["ptrauth"()]
+  /// - call(ptrauth.sign(p)),   ["ptrauth"()] ->  call p
+  /// as long as the key/discriminator are the same in sign and auth-bundle,
+  /// and we don't change the key in the bundle (to a potentially-invalid key.)
+  Instruction *foldPtrAuthIntrinsicCallee(CallBase &Call);
+
   /// Try to optimize a call to a ptrauth constant, into its ptrauth bundle:
   ///   call(ptrauth(f)), ["ptrauth"()] ->  call f
   /// as long as the key/discriminator are the same in constant and bundle.
