7fa27c9802890a99a92fddab810e9361e918ec84
Artem Belevich <tra@google.com>
2025-07-15 12:17:50 -0700

    clang-format

--- clang/lib/Headers/cuda_wrappers/__utility/declval.h
+++ clang/lib/Headers/cuda_wrappers/__utility/declval.h
@@ -11,20 +11,17 @@ _LIBCPP_BEGIN_NAMESPACE_STD
 // Suppress deprecation notice for volatile-qualified return type resulting
 // from volatile-qualified types _Tp.
 _LIBCPP_SUPPRESS_DEPRECATED_PUSH
-template <class _Tp>
-__attribute__((device))
-_Tp&& __declval(int);
-template <class _Tp>
-__attribute__((device))
-_Tp __declval(long);
+template <class _Tp> __attribute__((device)) _Tp &&__declval(int);
+template <class _Tp> __attribute__((device)) _Tp __declval(long);
 _LIBCPP_SUPPRESS_DEPRECATED_POP
 
 template <class _Tp>
-__attribute__((device))
-_LIBCPP_HIDE_FROM_ABI decltype(std::__declval<_Tp>(0)) declval() _NOEXCEPT {
+__attribute__((device)) _LIBCPP_HIDE_FROM_ABI decltype(std::__declval<_Tp>(0))
+declval() _NOEXCEPT {
   static_assert(!__is_same(_Tp, _Tp),
                 "std::declval can only be used in an unevaluated context. "
-                "It's likely that your current usage is trying to extract a value from the function.");
+                "It's likely that your current usage is trying to extract a "
+                "value from the function.");
 }
 
 _LIBCPP_END_NAMESPACE_STD
