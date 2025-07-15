bc92ab670b6ea52e61fddd2fa72b4479f521eaa3
Corentin Jabot <corentinjabot@gmail.com>
2025-07-15 14:34:32 +0300

    [Clang] Remove explicit object from non member function. (#148807)
    To avoid crashing later (as we assume only member functions can have
    explicit object parameters)
    
    Fixes #113185

--- clang/lib/Sema/SemaType.cpp
+++ clang/lib/Sema/SemaType.cpp
@@ -4860,7 +4860,9 @@ static TypeSourceInfo *GetFullTypeForDeclarator(TypeProcessingState &state,
             S.Diag(First->getBeginLoc(),
                    diag::err_explicit_object_parameter_invalid)
                 << First->getSourceRange();
-
+          // Do let non-member function have explicit parameters
+          // to not break assumptions elsewhere in the code.
+          First->setExplicitObjectParameterLoc(SourceLocation());
           D.setInvalidType();
           AreDeclaratorChunksValid = false;
         }
