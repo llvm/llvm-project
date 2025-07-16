f61809d910d16c967f0ce16776a786b22f684464
prabhukr <prabhukr@google.com>
2025-07-16 16:22:12 -0700

    Add public header wctype

--- libc/src/wctype/iswalpha.cpp
+++ libc/src/wctype/iswalpha.cpp
@@ -1,4 +1,4 @@
-//===-- Implementation of wctob -------------------------------------------===//
+//===-- Implementation of iswcalpha ---------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
