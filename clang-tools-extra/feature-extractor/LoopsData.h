0e3d022638e5fd87b1ebeb73e92b70ac66ef01c1
Majid Kamali <majidkamali1370@gmail.com>
2025-07-16 01:00:54 +0100

    Improve LoopsData to accept more data other than Stmt pointer

--- clang-tools-extra/feature-extractor/LoopsData.h
+++ clang-tools-extra/feature-extractor/LoopsData.h
@@ -12,20 +12,29 @@ using namespace clang;
 
 class LoopsData {
 public:
-  using TreeType = NaryTree<const Stmt *>;
+  struct MetaData {
+    const Stmt *for_stmt;
+
+    MetaData(const Stmt *fs) : for_stmt(fs) {}
+
+    friend bool operator==(const MetaData &lhs, const MetaData &rhs) {
+      return lhs.for_stmt == rhs.for_stmt;
+    }
+  };
+
+  using TreeType = NaryTree<MetaData>;
 
   void add_for(clang::ASTContext *context, const Stmt *parent,
-               const Stmt *self) {
-    ids[self] = self->getID(*context);
+               const MetaData &self) {
+    ids[self.for_stmt] = self.for_stmt->getID(*context);
 
     if (parent == nullptr) {
       loops.push_back({});
       loops.back().add_node(nullptr, self);
     } else {
-      auto ntree = std::find_if(loops.begin(), loops.end(),
-                                [&parent](const NaryTree<const Stmt *> &tree) {
-                                  return tree.contains(parent);
-                                });
+      auto ntree = std::find_if(
+          loops.begin(), loops.end(),
+          [&parent](const TreeType &tree) { return tree.contains(parent); });
 
       if (ntree != loops.end()) {
         ntree->add_node(parent, self);
