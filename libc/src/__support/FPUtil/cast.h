ad8191295483ab15de07511f2cf3201753dac450
Krishna Pandey <kpandey81930@gmail.com>
2025-07-16 21:27:58 +0530

    chore: add `ShouldSignalExceptions` comment in the templates
    Signed-off-by: Krishna Pandey <kpandey81930@gmail.com>

--- libc/src/__support/FPUtil/cast.h
+++ libc/src/__support/FPUtil/cast.h
@@ -65,7 +65,7 @@ cast(InType x) {
     constexpr size_t MAX_FRACTION_LEN =
         cpp::max(OutFPBits::FRACTION_LEN, InFPBits::FRACTION_LEN);
     DyadicFloat<cpp::bit_ceil(MAX_FRACTION_LEN)> xd(x);
-    return xd.template as<OutType, true>();
+    return xd.template as<OutType, /*ShouldSignalExceptions=*/true>();
   }
 
   return static_cast<OutType>(x);
