7b30a801ea1bd46b6d96de0058f6bac4f7e21e53
h2h <h2h@meta.com>
2025-07-16 08:28:07 -0700

    fix unittest

--- clang/test/CodeGen/unique-internal-linkage-names-alias.c
+++ clang/test/CodeGen/unique-internal-linkage-names-alias.c
@@ -6,5 +6,10 @@ static long foo(const struct A*p);
 long bar(const struct A*p);
 long bar(const struct A*p) __attribute__((__alias__("foo")));
 
-// CHECK: define internal i64 @_ZL3fooPK1A.__uniq.[[ATTR:[0-9]+]](ptr noundef %p) #0 {
+// CHECK: ; Function Attrs: noinline nounwind optnone
+// CHECK-NEXT: define internal i64 @foo(ptr noundef %0) #0 {
+// CHECK-NEXT:   %2 = call i64 @_ZL3fooPK1A.__uniq.[[ATTR:[0-9]+]](ptr %0)
+// CHECK-NEXT:   ret i64 %2
+// CHECK-NEXT: }
+// CHECK: define internal i64 @_ZL3fooPK1A.__uniq.[[ATTR:[0-9]+]](ptr noundef %p) #1 {
 static long foo(const struct A*p) {return 1;}
