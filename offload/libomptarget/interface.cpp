c35d7a0e987aa0ebf7c5ba234d40f9e6f0e4ad54
Abhinav Gaba <abhinav.gaba@intel.com>
2025-07-16 00:53:09 -0700

    [WIP][Offload] Introduce ATTACH map-type support for pointer attachment.
    This patch introduces libomptarget support for the ATTACH map-type,
    which can be used to implement OpenMP conditional compliant pointer attachment,
    based on whether the pointer/pointee is newly mapped on a given construct.
    
    For example, for the following:
    
    ```c
      int *p;
      #pragma omp target enter data map(p[1:10])
    ```
    
    The following maps can be emitted by clang:
    ```
      (A)
      &p[0], &p[1], 10 * sizeof(p[1]), TO | FROM
      &p, &p[1], sizeof(p), ATTACH
    ```
    
    Without this map-type, the two possible maps emitted by clang:
    ```
      (B)
      &p[0], &p[1], 10 * sizeof(p[1]), TO | FROM
    
      (C)
      &p, &p[1], 10 * sizeof(p[1]), TO | FROM | PTR_AND_OBJ
    ````
    
    (B) does not perform any pointer attachment, while (C) also maps the
    pointer p, which are both incorrect.
    
    In terms of implementation, maps with the ATTACH map-type are handled after
    all other maps have been processed, as it requires knowledge of which new
    allocations happened as part of the construct. As per OpenMP 5.0, an
    attachment should happen only when either the pointer or the pointee was
    newly mapped while handling the construct.
    
    Maps with ATTACH map-type-bit do not increase/decrease the ref-count.
    
    With OpenMP 6.1, `attach(always/never)` can be used to force/prevent
    attachment. For `attach(always)`, the compiler will insert the ALWAYS
    map-type, which would let libomptarget bypass the check about one of the
    pointer/pointee being new. With `attach(never)`, the ATTACH map will not
    be emitted at all.
    
    The size argument of the ATTACH map-type can specify values greater than
    `sizeof(void*)` which can be used to support pointer attachment on Fortran
    descriptors. Note that this also requires shadow-pointer tracking to also
    support them. That has not been implemented in this patch.
    
    This was worked upon in coordination with Ravi Narayanaswamy, who has
    since retired. Happy retirement, Ravi!

--- offload/libomptarget/interface.cpp
+++ offload/libomptarget/interface.cpp
@@ -165,12 +165,28 @@ targetData(ident_t *Loc, int64_t DeviceId, int32_t ArgNum, void **ArgsBase,
                                              OMPT_GET_RETURN_ADDRESS);)
 
   int Rc = OFFLOAD_SUCCESS;
+
+  // Only allocate AttachInfo for targetDataBegin
+  AttachInfoTy *AttachInfo = nullptr;
+  if (TargetDataFunction == targetDataBegin) {
+    AttachInfo = new AttachInfoTy();
+  }
+
   Rc = TargetDataFunction(Loc, *DeviceOrErr, ArgNum, ArgsBase, Args, ArgSizes,
                           ArgTypes, ArgNames, ArgMappers, AsyncInfo,
-                          false /*FromMapper=*/);
+                          AttachInfo, false /*FromMapper=*/);
 
-  if (Rc == OFFLOAD_SUCCESS)
-    Rc = AsyncInfo.synchronize();
+  if (Rc == OFFLOAD_SUCCESS) {
+    // Process deferred ATTACH entries BEFORE synchronization
+    if (AttachInfo && !AttachInfo->AttachEntries.empty())
+      Rc = processAttachEntries(*DeviceOrErr, *AttachInfo, AsyncInfo);
+
+    if (Rc == OFFLOAD_SUCCESS)
+      Rc = AsyncInfo.synchronize();
+  }
+
+  if (AttachInfo)
+    delete AttachInfo;
 
   handleTargetOutcome(Rc == OFFLOAD_SUCCESS, Loc);
 }
