6824bcfdb4c8315a990f4b5ce2cb9f528281a823
Min-Yih Hsu <min.hsu@sifive.com>
2025-07-16 13:46:02 -0700

    [IA] Relax the requirement of having ExtractValue users on deinterleave intrinsic (#148716)
    There are cases where InstCombine / InstSimplify might sink extractvalue
    instructions that use a deinterleave intrinsic into successor blocks,
    which prevents InterleavedAccess from kicking in because the current
    pattern requires deinterleave intrinsic to be used by extractvalue.
    However, this requirement is bit too strict while we could have just
    replaced the users of deinterleave intrinsic with whatever generated by
    the target TLI hooks.

--- llvm/lib/Target/AArch64/AArch64ISelLowering.h
+++ llvm/lib/Target/AArch64/AArch64ISelLowering.h
@@ -218,9 +218,8 @@ public:
   bool lowerInterleavedStore(StoreInst *SI, ShuffleVectorInst *SVI,
                              unsigned Factor) const override;
 
-  bool lowerDeinterleaveIntrinsicToLoad(
-      Instruction *Load, Value *Mask,
-      ArrayRef<Value *> DeinterleaveValues) const override;
+  bool lowerDeinterleaveIntrinsicToLoad(Instruction *Load, Value *Mask,
+                                        IntrinsicInst *DI) const override;
 
   bool lowerInterleaveIntrinsicToStore(
       StoreInst *SI, ArrayRef<Value *> InterleaveValues) const override;
