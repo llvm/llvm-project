0257934ef5c978d1fc9c60c1bdea7ec732932c07
Jan Svoboda <jan_svoboda@apple.com>
2025-07-16 07:11:13 -0700

    [clang] Move `ExceptionHandling` from `LangOptions` to `CodeGenOptions` (#148982)
    This PR removes the command line parsing workaround introduced in
    https://github.com/llvm/llvm-project/pull/146342 by moving
    `LangOptions::ExceptionHandling` to `CodeGenOptions` that get parsed
    even for IR input. Additionally, this improves layering, where the
    codegen library now checks `CodeGenOptions` instead of `LangOptions` for
    exception handling. (This got enabled by
    https://github.com/llvm/llvm-project/pull/146422.)

--- clang/include/clang/Basic/CodeGenOptions.h
+++ clang/include/clang/Basic/CodeGenOptions.h
@@ -176,6 +176,9 @@ public:
     llvm_unreachable("invalid FramePointerKind");
   }
 
+  /// Possible exception handling behavior.
+  enum class ExceptionHandlingKind { None, SjLj, WinEH, DwarfCFI, Wasm };
+
   enum class SwiftAsyncFramePointerKind {
     Auto, // Choose Swift async extended frame info based on deployment target.
     Always, // Unconditionally emit Swift async extended frame info.
@@ -550,6 +553,22 @@ public:
 
   const std::vector<std::string> &getNoBuiltinFuncs() const {
     return NoBuiltinFuncs;
+  }
+
+  bool hasSjLjExceptions() const {
+    return getExceptionHandling() == ExceptionHandlingKind::SjLj;
+  }
+
+  bool hasSEHExceptions() const {
+    return getExceptionHandling() == ExceptionHandlingKind::WinEH;
+  }
+
+  bool hasDWARFExceptions() const {
+    return getExceptionHandling() == ExceptionHandlingKind::DwarfCFI;
+  }
+
+  bool hasWasmExceptions() const {
+    return getExceptionHandling() == ExceptionHandlingKind::Wasm;
   }
 
   /// Check if Clang profile instrumenation is on.
