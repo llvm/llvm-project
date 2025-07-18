61be17f4facde976b16f4a6f37e78db208479d15
Uzair Nawaz <uzairnawaz@google.com>
2025-07-18 18:14:07 +0000

    made internal function header only

--- libc/src/__support/wchar/wcsnrtombs.h
+++ libc/src/__support/wchar/wcsnrtombs.h
@@ -9,17 +9,53 @@
 #ifndef LLVM_LIBC_SRC__SUPPORT_WCHAR_WCSNRTOMBS_H
 #define LLVM_LIBC_SRC__SUPPORT_WCHAR_WCSNRTOMBS_H
 
+#include "hdr/types/char32_t.h"
 #include "hdr/types/size_t.h"
 #include "hdr/types/wchar_t.h"
-#include "src/__support/error_or.h"
+#include "src/__support/common.h"
+#include "src/__support/libc_errno.h"
 #include "src/__support/macros/config.h"
 #include "src/__support/wchar/mbstate.h"
+#include "src/__support/wchar/string_converter.h"
 
 namespace LIBC_NAMESPACE_DECL {
 namespace internal {
 
 ErrorOr<size_t> wcsnrtombs(char *__restrict s, const wchar_t **__restrict pwcs,
-                           size_t nwc, size_t len, mbstate *ps);
+                           size_t nwc, size_t len, mbstate *ps) {
+  CharacterConverter cr(ps);
+  if (!cr.isValidState())
+    return Error(EINVAL);
+
+  if (s == nullptr)
+    len = SIZE_MAX;
+
+  StringConverter<char32_t> str_conv(reinterpret_cast<const char32_t *>(*pwcs),
+                                     ps, len, nwc);
+  size_t dst_idx = 0;
+  ErrorOr<char8_t> converted = str_conv.popUTF8();
+  while (converted.has_value()) {
+    if (s != nullptr)
+      s[dst_idx] = converted.value();
+
+    if (converted.value() == '\0') {
+      if (s != nullptr)
+        *pwcs = nullptr;
+      return dst_idx;
+    }
+
+    dst_idx++;
+    converted = str_conv.popUTF8();
+  }
+
+  if (s != nullptr)
+    *pwcs += str_conv.getSourceIndex();
+    
+  if (converted.error() == -1) // if we hit conversion limit
+    return dst_idx;
+
+  return Error(converted.error());
+}
 
 } // namespace internal
 } // namespace LIBC_NAMESPACE_DECL
