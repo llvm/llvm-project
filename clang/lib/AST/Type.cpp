628f2919fb804d39a1f7e5b1b14377cd5dcbf117
Justin Stitt <justinstitt@google.com>
2025-07-17 17:19:31 -0700

    rename NSAT to OBT
    These used to make sense when this feature set was called
    "NoSanitizeAttributedType". They are now called OverflowBehaviorTypes.
    
    Signed-off-by: Justin Stitt <justinstitt@google.com>

--- clang/lib/AST/Type.cpp
+++ clang/lib/AST/Type.cpp
@@ -2289,8 +2289,8 @@ bool Type::isSignedIntegerType() const {
   if (const auto *IT = dyn_cast<DependentBitIntType>(CanonicalType))
     return IT->isSigned();
 
-  if (const auto *NSAT = dyn_cast<OverflowBehaviorType>(CanonicalType))
-    return NSAT->getUnderlyingType()->isSignedIntegerType();
+  if (const auto *OBT = dyn_cast<OverflowBehaviorType>(CanonicalType))
+    return OBT->getUnderlyingType()->isSignedIntegerType();
 
   return false;
 }
@@ -2311,8 +2311,8 @@ bool Type::isSignedIntegerOrEnumerationType() const {
   if (const auto *IT = dyn_cast<DependentBitIntType>(CanonicalType))
     return IT->isSigned();
 
-  if (const auto *NSAT = dyn_cast<OverflowBehaviorType>(CanonicalType))
-    return NSAT->getUnderlyingType()->isSignedIntegerOrEnumerationType();
+  if (const auto *OBT = dyn_cast<OverflowBehaviorType>(CanonicalType))
+    return OBT->getUnderlyingType()->isSignedIntegerOrEnumerationType();
 
   return false;
 }
@@ -2345,8 +2345,8 @@ bool Type::isUnsignedIntegerType() const {
   if (const auto *IT = dyn_cast<DependentBitIntType>(CanonicalType))
     return IT->isUnsigned();
 
-  if (const auto *NSAT = dyn_cast<OverflowBehaviorType>(CanonicalType))
-    return NSAT->getUnderlyingType()->isUnsignedIntegerType();
+  if (const auto *OBT = dyn_cast<OverflowBehaviorType>(CanonicalType))
+    return OBT->getUnderlyingType()->isUnsignedIntegerType();
 
   return false;
 }
@@ -2367,8 +2367,8 @@ bool Type::isUnsignedIntegerOrEnumerationType() const {
   if (const auto *IT = dyn_cast<DependentBitIntType>(CanonicalType))
     return IT->isUnsigned();
 
-  if (const auto *NSAT = dyn_cast<OverflowBehaviorType>(CanonicalType))
-    return NSAT->getUnderlyingType()->isUnsignedIntegerOrEnumerationType();
+  if (const auto *OBT = dyn_cast<OverflowBehaviorType>(CanonicalType))
+    return OBT->getUnderlyingType()->isUnsignedIntegerOrEnumerationType();
 
   return false;
 }
@@ -3161,8 +3161,8 @@ bool Type::isLiteralType(const ASTContext &Ctx) const {
   if (const auto *AT = BaseTy->getAs<AtomicType>())
     return AT->getValueType()->isLiteralType(Ctx);
 
-  if (const auto *NSAT = BaseTy->getAs<OverflowBehaviorType>())
-    return NSAT->getUnderlyingType()->isLiteralType(Ctx);
+  if (const auto *OBT = BaseTy->getAs<OverflowBehaviorType>())
+    return OBT->getUnderlyingType()->isLiteralType(Ctx);
 
   // If this type hasn't been deduced yet, then conservatively assume that
   // it'll work out to be a literal type.
