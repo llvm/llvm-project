b319ee3ba1afc94adcb42da60fd65ea39164b62e
Muhammad Bassiouni <60100307+bassiounix@users.noreply.github.com>
2025-07-19 05:21:34 +0300

    [libc][math] Refactor acos implementation to header-only in src/__support/math folder. (#148409)
    Part of #147386
    
    in preparation for:
    https://discourse.llvm.org/t/rfc-make-clang-builtin-math-functions-constexpr-with-llvm-libc-to-support-c-23-constexpr-math-functions/86450

--- libc/shared/math/acos.h
+++ libc/shared/math/acos.h
@@ -1,4 +1,4 @@
-//===-- Double-precision acos function ------------------------------------===//
+//===-- Shared acos function ------------------------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,11 +6,18 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "src/math/acos.h"
+#ifndef LLVM_LIBC_SHARED_MATH_ACOS_H
+#define LLVM_LIBC_SHARED_MATH_ACOS_H
+
+#include "shared/libc_common.h"
 #include "src/__support/math/acos.h"
 
 namespace LIBC_NAMESPACE_DECL {
+namespace shared {
 
-LLVM_LIBC_FUNCTION(double, acos, (double x)) { return math::acos(x); }
+using math::acos;
 
+} // namespace shared
 } // namespace LIBC_NAMESPACE_DECL
+
+#endif // LLVM_LIBC_SHARED_MATH_ACOS_H
