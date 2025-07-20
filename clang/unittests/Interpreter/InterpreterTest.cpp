33db46d43223b53ec0ba7bc11cd8addbd6d5a743
kr-2003 <kumar.kr.abhinav@gmail.com>
2025-07-20 14:00:39 +0530

    formatting

--- clang/unittests/Interpreter/InterpreterTest.cpp
+++ clang/unittests/Interpreter/InterpreterTest.cpp
@@ -88,7 +88,8 @@ static std::string getOrcRuntimePath() {
   if (SystemTriple.isOSDarwin()) {
     llvm::sys::path::append(RuntimePath, "darwin", "liborc_rt_osx.a");
   } else if (SystemTriple.isOSLinux()) {
-    llvm::sys::path::append(RuntimePath, "x86_64-unknown-linux-gnu", "liborc_rt.a");
+    llvm::sys::path::append(RuntimePath, "x86_64-unknown-linux-gnu",
+                            "liborc_rt.a");
   } else {
     // Add other platforms as needed
     llvm::sys::path::append(RuntimePath, "liborc_rt.a");
