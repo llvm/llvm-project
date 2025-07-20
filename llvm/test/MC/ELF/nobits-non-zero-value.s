5c143851495c37e160fdda6cc142acb8ff69c52d
Fangrui Song <i@maskray.me>
2025-07-20 00:09:24 -0700

    MC: Optimize emitInstruction and simplify fragment-in-BSS check
    Move the FT_Relaxable-in-BSS check from frequently-called
    MCObjectStreamer::emitInstruction to MCAssembler::writeSectionData,
    along with existing checks for other fragment types. For the uncommon
    diagnostics, losing the location information is acceptable.

--- llvm/test/MC/ELF/nobits-non-zero-value.s
+++ llvm/test/MC/ELF/nobits-non-zero-value.s
@@ -1,26 +1,28 @@
-# RUN: not llvm-mc -filetype=obj -triple=x86_64 %s -o /dev/null 2>&1 | FileCheck %s --implicit-check-not=error:
+# RUN: not llvm-mc -filetype=obj -triple=x86_64 %s -o /dev/null 2>&1 | FileCheck %s --implicit-check-not=error: --implicit-check-not=warning:
 
 ## -filetype=asm does not check the error.
 # RUN: llvm-mc -triple=x86_64 %s
 
 .section .tbss,"aw",@nobits
-# MCRelaxableFragment
-# CHECK: {{.*}}.s:[[#@LINE+1]]:3: error: SHT_NOBITS section '.tbss' cannot have instructions
   jmp foo
 
 .bss
-# CHECK: {{.*}}.s:[[#@LINE+1]]:3: error: SHT_NOBITS section '.bss' cannot have instructions
   addb %al,(%rax)
 
 # CHECK: {{.*}}.s:[[#@LINE+1]]:11: warning: ignoring non-zero fill value in SHT_NOBITS section '.bss'
 .align 4, 42
 
-# CHECK-NOT: {{.*}}.s:[[#@LINE+1]]:11: warning: ignoring non-zero fill value in SHT_NOBITS section '.bss'
 .align 4, 0
 
-# CHECK: <unknown>:0: error: SHT_NOBITS section '.bss' cannot have non-zero initializers
   .long 1
 
+.section .bss0,"aw",%nobits
+addb %al,(%rax)
+
 .section .bss1,"aw",%nobits
-# CHECK: <unknown>:0: error: SHT_NOBITS section '.bss1' cannot have fixups
 .quad foo
+
+## Location is not tracked for efficiency.
+# CHECK: <unknown>:0: error: SHT_NOBITS section '.tbss' cannot have non-zero bytes
+# CHECK: <unknown>:0: error: SHT_NOBITS section '.bss' cannot have non-zero bytes
+# CHECK: <unknown>:0: error: SHT_NOBITS section '.bss1' cannot have fixups
