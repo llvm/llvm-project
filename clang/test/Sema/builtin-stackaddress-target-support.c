88db438e9f35cde48f1799126f8fe2127eefb950
moorabbit <moorabbit@proton.me>
2025-07-20 19:36:49 -0400

    [Clang] Refactor test case

--- clang/test/Sema/builtin-stackaddress-target-support.c
+++ clang/test/Sema/builtin-stackaddress-target-support.c
@@ -1,16 +1,10 @@
-// RUN: %clang_cc1 -verify %s -triple x86_64-unknown-unknown -DTEST_x64
-// RUN: %clang_cc1 -verify %s -triple i386-unknown-unknown -DTEST_x86
-// RUN: %clang_cc1 -verify %s -triple riscv32-unknown-unknown -DTEST_riscv32
-// RUN: %clang_cc1 -verify %s -triple riscv64-unknown-unknown -DTEST_riscv64
-// RUN: %clang_cc1 -verify %s -triple aarch64-unknown-unknown -DTEST_aarch64
+// RUN: %clang_cc1 -verify=x86 %s -triple x86_64-unknown-unknown
+// RUN: %clang_cc1 -verify=x86 %s -triple i386-unknown-unknown
+// RUN: %clang_cc1 -verify %s -triple riscv32-unknown-unknown
+// RUN: %clang_cc1 -verify %s -triple riscv64-unknown-unknown
+// RUN: %clang_cc1 -verify %s -triple aarch64-unknown-unknown
 
-#if defined(TEST_x64) || defined(TEST_x86)
-// expected-no-diagnostics
-void *a() {
-return __builtin_stack_address();
-}
-#else
 void *a() {
 return __builtin_stack_address(); // expected-error {{builtin is not supported on this target}}
+                                  // x86-no-diagnostics
 }
-#endif
