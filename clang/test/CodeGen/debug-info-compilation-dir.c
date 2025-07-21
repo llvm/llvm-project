9bb801bcf10be2c8f9ee80e16996d492cda32e19
Steven Wu <stevenwu@apple.com>
2025-07-21 14:55:13 -0700

    Fixtest on Windows
    Created using spr 1.3.6

--- clang/test/CodeGen/debug-info-compilation-dir.c
+++ clang/test/CodeGen/debug-info-compilation-dir.c
@@ -9,8 +9,8 @@
 
 /// Test path remapping.
 // RUN: %clang_cc1 -fdebug-compilation-dir %S -main-file-name %s -emit-llvm -debug-info-kind=limited %s -o - | FileCheck -check-prefix=CHECK-ABS %s -DPREFIX=%S
-// CHECK-ABS: DIFile(filename: "[[PREFIX]]/debug-info-compilation-dir.c", directory: "[[PREFIX]]")
+// CHECK-ABS: DIFile(filename: "[[PREFIX]]{{/|\\}}debug-info-compilation-dir.c", directory: "[[PREFIX]]")
 
 // RUN: %clang_cc1 -fno-compilation-dir -main-file-name %s -emit-llvm -debug-info-kind=limited %s -o - | FileCheck -check-prefix=CHECK-NOMAP %s -DPREFIX=%S
-// CHECK-NOMAP: DIFile(filename: "[[PREFIX]]/debug-info-compilation-dir.c", directory: "")
+// CHECK-NOMAP: DIFile(filename: "[[PREFIX]]{{/|\\}}debug-info-compilation-dir.c", directory: "")
 
