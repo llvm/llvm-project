97757a89b9ca7c180358a80cdf2510b01dd497e6
woruyu <1214539920@qq.com>
2025-07-21 10:43:20 +0800

    fix: review

--- llvm/lib/Target/ARM/ARMISelLowering.cpp
+++ llvm/lib/Target/ARM/ARMISelLowering.cpp
@@ -20110,9 +20110,9 @@ void ARMTargetLowering::computeKnownBitsForTargetNode(const SDValue Op,
     KnownBits KnownLHS = DAG.computeKnownBits(Op.getOperand(0), Depth + 1);
 
     unsigned Encoded = Op.getConstantOperandVal(1);
-    unsigned ElemSize = Op.getValueType().getScalarSizeInBits();
+    unsigned ElemSize = Op.getScalarValueSizeInBits();
     uint64_t DecodedVal = ARM_AM::decodeVMOVModImm(Encoded, ElemSize);
-    APInt Imm(Known.getBitWidth(), DecodedVal);
+    APInt Imm(ElemSize, DecodedVal);
 
     Known.One = KnownLHS.One | Imm;
     Known.Zero = KnownLHS.Zero & ~Imm;
@@ -20122,9 +20122,9 @@ void ARMTargetLowering::computeKnownBitsForTargetNode(const SDValue Op,
     KnownBits KnownLHS = DAG.computeKnownBits(Op.getOperand(0), Depth + 1);
 
     unsigned Encoded = Op.getConstantOperandVal(1);
-    unsigned ElemSize = Op.getValueType().getScalarSizeInBits();
+    unsigned ElemSize = Op.getScalarValueSizeInBits();
     uint64_t DecodedVal = ARM_AM::decodeVMOVModImm(Encoded, ElemSize);
-    APInt Imm(Known.getBitWidth(), DecodedVal);
+    APInt Imm(ElemSize, DecodedVal);
 
     APInt NotImm = ~Imm;
     Known.One = KnownLHS.One & NotImm;
