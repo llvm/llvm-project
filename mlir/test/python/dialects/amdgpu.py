f96ff80ed52706e3dc25c2d399f33b609e62dd84
Stanley Winata <stanley.winata@amd.com>
2025-07-21 16:10:30 -0700

    add TODO and lint python test
    Signed-off-by: Stanley Winata <stanley.winata@amd.com>

--- mlir/test/python/dialects/amdgpu.py
+++ mlir/test/python/dialects/amdgpu.py
@@ -29,9 +29,7 @@ def testFatRawBufferCastOpParams():
         [ShapedType.get_dynamic_size(), ShapedType.get_dynamic_size()],
         F32Type.get(),
     )
-    f = func.FuncOp(
-        "test_raw_buffer_cast_params", ([memref_type], [])
-    )
+    f = func.FuncOp("test_raw_buffer_cast_params", ([memref_type], []))
     with InsertionPoint(f.add_entry_block()):
         block_args = f.arguments
         amdgpu.FatRawBufferCastOp(block_args[0])
@@ -40,8 +38,8 @@ def testFatRawBufferCastOpParams():
         amdgpu.FatRawBufferCastOp(block_args[0], boundsCheck=False, resetOffset=True)
         func.ReturnOp([])
 
-    #CHECK:     func.func @test_raw_buffer_cast_params(%[[ARG0:.+]]: memref<?x?xf32>) {
-    #CHECK:        amdgpu.fat_raw_buffer_cast %[[ARG0]] : memref<?x?xf32> to memref<?x?xf32, #amdgpu.address_space<fat_raw_buffer>>
-    #CHECK-NEXT:   amdgpu.fat_raw_buffer_cast %[[ARG0]] resetOffset : memref<?x?xf32> to memref<?x?xf32, #amdgpu.address_space<fat_raw_buffer>>
-    #CHECK-NEXT:   amdgpu.fat_raw_buffer_cast %[[ARG0]] boundsCheck(false) : memref<?x?xf32> to memref<?x?xf32, #amdgpu.address_space<fat_raw_buffer>>
-    #CHECK-NEXT:   amdgpu.fat_raw_buffer_cast %[[ARG0]] boundsCheck(false) resetOffset : memref<?x?xf32> to memref<?x?xf32, #amdgpu.address_space<fat_raw_buffer>>
+    # CHECK:     func.func @test_raw_buffer_cast_params(%[[ARG0:.+]]: memref<?x?xf32>) {
+    # CHECK:        amdgpu.fat_raw_buffer_cast %[[ARG0]] : memref<?x?xf32> to memref<?x?xf32, #amdgpu.address_space<fat_raw_buffer>>
+    # CHECK-NEXT:   amdgpu.fat_raw_buffer_cast %[[ARG0]] resetOffset : memref<?x?xf32> to memref<?x?xf32, #amdgpu.address_space<fat_raw_buffer>>
+    # CHECK-NEXT:   amdgpu.fat_raw_buffer_cast %[[ARG0]] boundsCheck(false) : memref<?x?xf32> to memref<?x?xf32, #amdgpu.address_space<fat_raw_buffer>>
+    # CHECK-NEXT:   amdgpu.fat_raw_buffer_cast %[[ARG0]] boundsCheck(false) resetOffset : memref<?x?xf32> to memref<?x?xf32, #amdgpu.address_space<fat_raw_buffer>>
