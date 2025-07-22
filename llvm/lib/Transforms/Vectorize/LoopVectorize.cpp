64aa25f1c11a79b45063eb1a4cfbcb6bad5fa05e
Samuel Tebbs <samuel.tebbs@arm.com>
2025-07-22 13:45:26 +0100

    Fix interleaving case

--- llvm/lib/Transforms/Vectorize/LoopVectorize.cpp
+++ llvm/lib/Transforms/Vectorize/LoopVectorize.cpp
@@ -9038,6 +9038,7 @@ void LoopVectorizationPlanner::adjustRecipesForReductions(
         VPWidenRecipe *Sub = new VPWidenRecipe(
             Instruction::Sub, {Zero, CurrentLink->getOperand(1)}, {}, VPIRMetadata(),
             CurrentLinkI->getDebugLoc());
+        Sub->setUnderlyingValue(CurrentLinkI);
         LinkVPBB->insert(Sub, CurrentLink->getIterator());
         VecOp = Sub;
       } else {
