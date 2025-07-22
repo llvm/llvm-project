1a5a44524ed253d9cc7846792380d874620878aa
sivadeilra <ardavis@microsoft.com>
2025-07-22 09:18:13 -0700

    Fix Windows EH IP2State tables (remove +1 bias) (#144745)
    This changes how LLVM constructs certain data structures that relate to
    exception handling (EH) on Windows. Specifically this changes how
    IP2State tables for functions are constructed. The purpose of this
    change is to align LLVM to the requires of the Windows AMD64 ABI, which
    requires that the IP2State table entries point to the boundaries between
    instructions.
    
    On most Windows platforms (AMD64, ARM64, ARM32, IA64, but *not* x86-32),
    exception handling works by looking up instruction pointers in lookup
    tables. These lookup tables are stored in `.xdata` sections in
    executables. One element of the lookup tables are the `IP2State` tables
    (Instruction Pointer to State).
    
    If a function has any instructions that require cleanup during exception
    unwinding, then it will have an IP2State table. Each entry in the
    IP2State table describes a range of bytes in the function's instruction
    stream, and associates an "EH state number" with that range of
    instructions. A value of -1 means "the null state", which does not
    require any code to execute. A value other than -1 is an index into the
    State table.
    
    The entries in the IP2State table contain byte offsets within the
    instruction stream of the function. The Windows ABI requires that these
    offsets are aligned to instruction boundaries; they are not permitted to
    point to a byte that is not the first byte of an instruction.
    
    Unfortunately, CALL instructions present a problem during unwinding.
    CALL instructions push the address of the instruction after the CALL
    instruction, so that execution can resume after the CALL. If the CALL is
    the last instruction within an IP2State region, then the return address
    (on the stack) points to the *next* IP2State region. This means that the
    unwinder will use the wrong cleanup funclet during unwinding.
    
    To fix this problem, compilers should insert a NOP after a CALL
    instruction, if the CALL instruction is the last instruction within an
    IP2State region. The NOP is placed within the same IP2State region as
    the CALL, so that the return address points to the NOP and the unwinder
    will locate the correct region.
    
    This PR modifies LLVM so that it inserts NOP instructions after CALL
    instructions, when needed. In performance tests, the NOP has no
    detectable significance. The NOP is rarely inserted, since it is only
    inserted when the CALL is the last instruction before an IP2State
    transition or the CALL is the last instruction before the function
    epilogue.
    
    NOP padding is only necessary on Windows AMD64 targets. On ARM64 and
    ARM32, instructions have a fixed size so the unwinder knows how to "back
    up" by one instruction.
    
    Interaction with Import Call Optimization (ICO):
    
    Import Call Optimization (ICO) is a compiler + OS feature on Windows
    which improves the performance and security of DLL imports. ICO relies
    on using a specific CALL idiom that can be replaced by the OS DLL
    loader. This removes a load and indirect CALL and replaces it with a
    single direct CALL.
    
    To achieve this, ICO also inserts NOPs after the CALL instruction. If
    the end of the CALL is aligned with an EH state transition, we *also*
    insert a single-byte NOP. **Both forms of NOPs must be preserved.** They
    cannot be combined into a single larger NOP; nor can the second NOP be
    removed.
    
    This is necessary because, if ICO is active and the call site is
    modified by the loader, the loader will end up overwriting the NOPs that
    were inserted for ICO. That means that those NOPs cannot be used for the
    correct termination of the exception handling region (the IP2State
    transition), so we still need an additional NOP instruction. The NOPs
    cannot be combined into a longer NOP (which is ordinarily desirable)
    because then ICO would split one instruction, producing a malformed
    instruction after the ICO call.

--- llvm/lib/CodeGen/AsmPrinter/AsmPrinter.cpp
+++ llvm/lib/CodeGen/AsmPrinter/AsmPrinter.cpp
@@ -1868,6 +1868,7 @@ void AsmPrinter::emitFunctionBody() {
         OutStreamer->emitLabel(MI.getOperand(0).getMCSymbol());
         break;
       case TargetOpcode::EH_LABEL:
+        OutStreamer->AddComment("EH_LABEL");
         OutStreamer->emitLabel(MI.getOperand(0).getMCSymbol());
         // For AsynchEH, insert a Nop if followed by a trap inst
         //   Or the exception won't be caught.
