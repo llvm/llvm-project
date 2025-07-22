f9efc16976cf10bdd2941e0d5061db5caa793005
Hristo Hristov <hghristov.rmm@gmail.com>
2025-07-23 01:21:11 +0300

    Updated test

--- libcxx/test/std/strings/basic.string/string.ops/string_substr/subview.pass.cpp
+++ libcxx/test/std/strings/basic.string/string.ops/string_substr/subview.pass.cpp
@@ -15,12 +15,49 @@
 #include <cassert>
 #include <string>
 
+#include "asan_testing.h"
+#include "constexpr_char_traits.h"
+#include "make_string.h"
+#include "min_allocator.h"
+#include "test_allocator.h"
+#include "test_macros.h"
+
+#define CS(S) MAKE_CSTRING(CharT, S)
+
+template <typename CharT, typename TraitsT, typename AllocT>
+constexpr void test() {
+  std::basic_string<CharT, TraitsT, AllocT> s{CS("Hello cruel world!"), AllocT{}};
+
+  std::same_as<std::basic_string_view<CharT, TraitsT>> decltype(auto) sv = s.subview(6);
+  assert(sv == CS("cruel world!"));
+
+  std::same_as<std::basic_string_view<CharT, TraitsT>> decltype(auto) subsv = sv.subview(0, 5);
+  assert(subsv == CS("cruel"));
+}
+
+template <typename CharT>
+constexpr void test() {
+  test<CharT, std::char_traits<CharT>, std::allocator<CharT>>();
+  test<CharT, std::char_traits<CharT>, min_allocator<CharT>>();
+  test<CharT, std::char_traits<CharT>, safe_allocator<CharT>>();
+  test<CharT, std::char_traits<CharT>, test_allocator<CharT>>();
+
+  test<CharT, constexpr_char_traits<CharT>, std::allocator<CharT>>();
+  test<CharT, constexpr_char_traits<CharT>, min_allocator<CharT>>();
+  test<CharT, constexpr_char_traits<CharT>, safe_allocator<CharT>>();
+  test<CharT, constexpr_char_traits<CharT>, test_allocator<CharT>>();
+}
+
 constexpr bool test() {
-  std::string s{"Hello cruel world!"};
-  auto sub = s.subview(6);
-  assert(sub == "cruel world!");
-  auto subsub = sub.subview(0, 5);
-  assert(subsub == "cruel");
+  test<char>();
+#ifndef TEST_HAS_NO_WIDE_CHARACTERS
+  test<wchar_t>();
+#endif
+#ifndef TEST_HAS_NO_CHAR8_T
+  test<char8_t>();
+#endif
+  test<char16_t>();
+  test<char32_t>();
 
   return true;
 }
