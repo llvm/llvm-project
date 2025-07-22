e20d45090672bc07dee6c41eb035d9d73566585c
Uzair Nawaz <uzairnawaz@google.com>
2025-07-22 21:58:52 +0000

    more header typos

--- libc/hdr/types/pthread_barrierattr_t.h
+++ libc/hdr/types/pthread_barrierattr_t.h
@@ -1,4 +1,4 @@
-//===-- Definition of macros from pthread_barrier_t.h ---------------------===//
+//===-- Definition of macros from pthread_barrierattr_t.h -----------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
--- libc/src/pthread/pthread_barrier_wait.cpp
+++ libc/src/pthread/pthread_barrier_wait.cpp
@@ -1,4 +1,4 @@
-//===-- Linux implementation of the pthread_barrier_init function ---------===//
+//===-- Implementation of the pthread_barrier_wait function ---------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
