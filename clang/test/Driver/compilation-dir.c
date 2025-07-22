075ebf724c7ffd09723fa56fed9bbeca0d21e683
Steven Wu <stevenwu@apple.com>
2025-07-22 15:27:18 -0700

    [𝘀𝗽𝗿] changes to main this commit is based on
    Created using spr 1.3.6
    
    [skip ci]

--- clang/test/Driver/compilation-dir.c
+++ clang/test/Driver/compilation-dir.c
@@ -8,3 +8,7 @@
 // RUN: %clang -### -integrated-as -ffile-compilation-dir=. -x assembler %s 2>&1 | FileCheck -check-prefixes=CHECK-DEBUG-COMPILATION-DIR %s
 // CHECK-DEBUG-COMPILATION-DIR: "-fdebug-compilation-dir=."
 // CHECK-DEBUG-COMPILATION-DIR-NOT: "-ffile-compilation-dir=."
+
+// RUN: cd %S
+// RUN: %clang -### -S %s 2>&1 | FileCheck -check-prefix=CHECK-CWD %s
+// CHECK-CWD: -fdebug-compilation-dir={{.*}}Driver
