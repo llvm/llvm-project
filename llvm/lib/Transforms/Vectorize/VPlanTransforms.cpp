d95e7cd48b4f55ce187540fb63cbcf0433fa1ae7
Luke Lau <luke@igalia.com>
2025-07-22 22:41:24 +0800

    Remove redundant setUnderlyingValue, adjust comment

--- llvm/lib/Transforms/Vectorize/VPlanTransforms.cpp
+++ llvm/lib/Transforms/Vectorize/VPlanTransforms.cpp
@@ -2711,16 +2711,14 @@ void VPlanTransforms::convertToConcreteRecipes(VPlan &Plan,
         continue;
       }
 
-      // Expand VPBlendRecipe into VPInstruction::Select
+      // Expand VPBlendRecipe into VPInstruction::Select.
       VPBuilder Builder(&R);
       if (auto *Blend = dyn_cast<VPBlendRecipe>(&R)) {
         VPValue *Select = Blend->getIncomingValue(0);
-        for (unsigned I = 1; I != Blend->getNumIncomingValues(); ++I) {
+        for (unsigned I = 1; I != Blend->getNumIncomingValues(); ++I)
           Select = Builder.createSelect(Blend->getMask(I),
                                         Blend->getIncomingValue(I), Select,
                                         R.getDebugLoc(), "predphi");
-          Select->setUnderlyingValue(Blend->getUnderlyingValue());
-        }
         Blend->replaceAllUsesWith(Select);
         ToRemove.push_back(Blend);
       }
