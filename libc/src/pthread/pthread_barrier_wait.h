4373ef77363c199826f9ebdadf9339988ed890fc
Uzair Nawaz <uzairnawaz@google.com>
2025-07-22 22:59:42 +0000

    formatting

--- libc/src/pthread/pthread_barrier_wait.h
+++ libc/src/pthread/pthread_barrier_wait.h
@@ -9,8 +9,8 @@
 #ifndef LLVM_LIBC_SRC_PTHREAD_PTHREAD_BARRIER_WAIT_H
 #define LLVM_LIBC_SRC_PTHREAD_PTHREAD_BARRIER_WAIT_H
 
-#include "src/__support/macros/config.h"
 #include "hdr/types/pthread_barrier_t.h"
+#include "src/__support/macros/config.h"
 
 namespace LIBC_NAMESPACE_DECL {
 
