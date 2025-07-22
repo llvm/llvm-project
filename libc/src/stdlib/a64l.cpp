acf7adb880063651f9ba5cd00a7eb889bed2ba3b
Tue Ly <lntue.h@gmail.com>
2025-07-22 14:43:42 +0000

    [libc] Use int in a64l instead of int32_t.

--- libc/src/stdlib/a64l.cpp
+++ libc/src/stdlib/a64l.cpp
@@ -12,14 +12,12 @@
 #include "src/__support/ctype_utils.h"
 #include "src/__support/macros/config.h"
 
-#include <stdint.h>
-
 namespace LIBC_NAMESPACE_DECL {
 
 // I'm not sure this should go in ctype_utils since the specific ordering of
 // base64 is so very implementation specific, and also this set is unusual.
 // Returns -1 on any char without a specified value.
-constexpr static int32_t b64_char_to_int(char ch) {
+constexpr static int b64_char_to_int(char ch) {
   // from the standard: "The characters used to represent digits are '.' (dot)
   // for 0, '/' for 1, '0' through '9' for [2,11], 'A' through 'Z' for [12,37],
   // and 'a' through 'z' for [38,63]."
@@ -44,10 +42,10 @@ constexpr static int32_t b64_char_to_int(char ch) {
 LLVM_LIBC_FUNCTION(long, a64l, (const char *s)) {
   // the standard says to only use up to 6 characters.
   constexpr size_t MAX_LENGTH = 6;
-  int32_t result = 0;
+  int result = 0;
 
   for (size_t i = 0; i < MAX_LENGTH && s[i] != '\0'; ++i) {
-    int32_t cur_val = b64_char_to_int(s[i]);
+    int cur_val = b64_char_to_int(s[i]);
     // The standard says what happens on an unspecified character is undefined,
     // here we treat it as the end of the string.
     if (cur_val == -1)
