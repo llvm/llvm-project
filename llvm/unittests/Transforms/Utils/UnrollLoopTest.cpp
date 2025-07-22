c9c1bc5c4e0ec2a9da5626534d648133acde7cb7
Marek Sedlacek <msedlacek@azul.com>
2025-07-22 10:01:38 +0000

    Fixed incorrect call in unit test

--- llvm/unittests/Transforms/Utils/UnrollLoopTest.cpp
+++ llvm/unittests/Transforms/Utils/UnrollLoopTest.cpp
@@ -73,7 +73,6 @@ while.end:                                        ; preds = %while.cond
 
   bool ret =
       UnrollRuntimeLoopRemainder(L, 4, true, false, false, false, &LI, &SE, &DT,
-                                 &AC, /*TTI=*/nullptr, PreserveLCSSA, 4, false,
-                                 false);
+                                 &AC, /*TTI=*/nullptr, PreserveLCSSA, 4, false);
   EXPECT_FALSE(ret);
 }
