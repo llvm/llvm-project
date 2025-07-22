8191b1bfc99f4cc39f04bdd07992adee8785bd6e
David Green <david.green@arm.com>
2025-07-22 15:37:28 +0100

    [AArch64] Generalize costing for FP16 instructions
    This extracts the code for modelling a fp16 operation as
    fptrunc(fpop(fpext,fpext) into a new function named getFP16BF16PromoteCost so
    that it can be reused by the arithmetic instructions. The function takes a
    lambda to calculate the cost of the operation with the promoted type.

--- llvm/lib/Target/AArch64/AArch64TargetTransformInfo.h
+++ llvm/lib/Target/AArch64/AArch64TargetTransformInfo.h
@@ -435,6 +435,13 @@ public:
 
   bool preferPredicatedReductionSelect() const override { return ST->hasSVE(); }
 
+  /// FP16 and BF16 operations are lowered to
+  std::optional<InstructionCost>
+  getFP16BF16PromoteCost(Type *Ty, TTI::TargetCostKind CostKind,
+                         TTI::OperandValueInfo Op1Info,
+                         TTI::OperandValueInfo Op2Info, bool IncludeTrunc,
+                         std::function<InstructionCost(Type *)> InstCost) const;
+
   InstructionCost
   getArithmeticReductionCost(unsigned Opcode, VectorType *Ty,
                              std::optional<FastMathFlags> FMF,
