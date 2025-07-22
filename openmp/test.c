c9b469ae4b9a9c1c2c3d0f1411eeb4d5ba1403de
Shruti05-MS <shruti.m.sinha05@gmail.com>
2025-07-22 15:12:36 +0530

    Create test.c

---
+++ openmp/test.c
@@ -0,0 +1,12 @@
+#include <omp.h>
+#include <stdio.h>
+
+int main() {
+    int x = 0;
+    #pragma omp parallel for
+    for (int i = 0; i < 10; i++) {
+        x += i;
+    }
+    printf("x = %d\n", x);
+    return 0;
+}
