945c6ae4788dbefe4bc0700fd9dc5ea624a8afc9
Jonas Devlieghere <jonas@devlieghere.com>
2025-07-22 15:51:00 -0700

    [lldb] Eliminate namespace lldb_private::dwarf (NFC) (#150073)
    Eliminate the `lldb_private::dwarf` namespace, in favor of using
    `llvm::dwarf` directly. The latter is shorter, and this avoids ambiguity
    in the ABI plugins that define a `dwarf` namespace inside an anonymous
    namespace.

--- lldb/source/Plugins/SymbolFile/DWARF/SymbolFileDWARF.cpp
+++ lldb/source/Plugins/SymbolFile/DWARF/SymbolFileDWARF.cpp
@@ -98,8 +98,8 @@
 
 using namespace lldb;
 using namespace lldb_private;
-using namespace lldb_private::dwarf;
 using namespace lldb_private::plugin::dwarf;
+using namespace llvm::dwarf;
 
 LLDB_PLUGIN_DEFINE(SymbolFileDWARF)
 
