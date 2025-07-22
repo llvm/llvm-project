31224391c08a0d2acfebf6454b439527917d14da
Uzair Nawaz <uzairnawaz@google.com>
2025-07-22 21:48:54 +0000

    header typo

--- libc/include/llvm-libc-types/__barrier_type.h
+++ libc/include/llvm-libc-types/__barrier_type.h
@@ -1,4 +1,4 @@
-//===-- Definition of pthread_barrier_t type ------------------------------===//
+//===-- Definition of __barrier_type type ---------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
