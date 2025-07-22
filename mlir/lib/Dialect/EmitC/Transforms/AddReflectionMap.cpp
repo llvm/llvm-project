cff69e848af4ddffc45673f06631cd3e8b91398d
Jaddyen <ajaden@google.com>
2025-07-22 22:33:45 +0000

    Add an argument

--- mlir/lib/Dialect/EmitC/Transforms/AddReflectionMap.cpp
+++ mlir/lib/Dialect/EmitC/Transforms/AddReflectionMap.cpp
@@ -1,15 +1,10 @@
-/* Copyright 2025 The TensorFlow Authors. All Rights Reserved.
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-
+//===- AddReflectionMap.cpp - Add a reflection map to a class -------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
 #include "mlir/Dialect/EmitC/IR/EmitC.h"
 #include "mlir/Dialect/EmitC/Transforms/Passes.h"
 #include "mlir/Dialect/EmitC/Transforms/Transforms.h"
@@ -35,7 +30,7 @@ class AddReflectionMapPass
     Operation *rootOp = getOperation();
 
     RewritePatternSet patterns(&getContext());
-    populateAddReflectionMapPatterns(patterns);
+    populateAddReflectionMapPatterns(patterns, namedAttribute);
 
     walkAndApplyPatterns(rootOp, std::move(patterns));
   }
@@ -47,8 +42,8 @@ class AddReflectionMapPass
 
 class AddReflectionMapClass : public OpRewritePattern<emitc::ClassOp> {
 public:
-  AddReflectionMapClass(MLIRContext *context)
-      : OpRewritePattern<emitc::ClassOp>(context) {}
+  AddReflectionMapClass(MLIRContext *context, StringRef attrName)
+      : OpRewritePattern<emitc::ClassOp>(context), attributeName(attrName) {}
 
   LogicalResult matchAndRewrite(mlir::emitc::ClassOp classOp,
                                 PatternRewriter &rewriter) const override {
@@ -73,23 +68,23 @@ public:
     rewriter.setInsertionPointToStart(funcBody);
 
     // Collect all field names
-    SmallVector<std::string> fieldNames;
+    std::vector<std::pair<std::string, std::string>> fieldNames;
     classOp.walk([&](mlir::emitc::FieldOp fieldOp) {
       if (mlir::Attribute attrsAttr =
               fieldOp->getAttrDictionary().get("attrs")) {
         if (DictionaryAttr innerDictAttr =
                 dyn_cast<mlir::DictionaryAttr>(attrsAttr)) {
-          auto indexPathAttr =
-              innerDictAttr.getNamed("tf_saved_model.index_path");
+          auto indexPathAttr = innerDictAttr.getNamed(attributeName);
           ArrayAttr arrayAttr =
               dyn_cast<mlir::ArrayAttr>(indexPathAttr->getValue());
           if (!arrayAttr.empty()) {
             StringAttr stringAttr = dyn_cast<mlir::StringAttr>(arrayAttr[0]);
             std::string indexPath = stringAttr.getValue().str();
-            fieldNames.push_back(indexPath);
+            fieldNames.emplace_back(indexPath, fieldOp.getName().str());
           }
           if (arrayAttr.size() > 1) {
-            fieldOp.emitError() << "tf_saved_model.index_path attribute must "
+            fieldOp.emitError() << attributeName
+                                << " attribute must "
                                    "contain at most one value, but found "
                                 << arrayAttr.size() << " values.";
             return;
@@ -98,64 +93,54 @@ public:
       }
     });
 
-    std::string mapInitializer = "{{";
+    std::string mapInitializer = "{ ";
     for (size_t i = 0; i < fieldNames.size(); ++i) {
-      mapInitializer += "\"" + fieldNames[i] + "\", " +
-                        "reinterpret_cast<char*>(&" + fieldNames[i] + ")",
-          mapInitializer += "}";
+      mapInitializer += " { \"" + fieldNames[i].first + "\", " +
+                        "reinterpret_cast<char*>(&" + fieldNames[i].second +
+                        ")",
+          mapInitializer += " }";
       if (i < fieldNames.size() - 1)
-        mapInitializer += ", {";
+        mapInitializer += ", ";
     }
-    mapInitializer += "}";
+    mapInitializer += " }";
 
-    auto iteratorType = mlir::emitc::OpaqueType::get(
+    emitc::OpaqueType iteratorType = mlir::emitc::OpaqueType::get(
         context, "std::map<std::string, char*>::const_iterator");
-    auto boolType = rewriter.getI1Type();
-    // 5. Create the constant map
-    auto bufferMap = rewriter.create<emitc::ConstantOp>(
+
+    emitc::ConstantOp bufferMap = rewriter.create<emitc::ConstantOp>(
         classOp.getLoc(), mapType,
         emitc::OpaqueAttr::get(context, mapInitializer));
 
-    // 6. Get the function argument
     mlir::Value nameArg = getBufferFunc.getArgument(0);
-
-    // 7. Create the find call
-    auto it = rewriter.create<emitc::CallOpaqueOp>(
+    emitc::CallOpaqueOp it = rewriter.create<emitc::CallOpaqueOp>(
         classOp.getLoc(), iteratorType, rewriter.getStringAttr("find"),
         mlir::ValueRange{bufferMap.getResult(), nameArg});
-
-    // 8. Create the end call
-    auto endIt = rewriter.create<emitc::CallOpaqueOp>(
+    emitc::CallOpaqueOp endIt = rewriter.create<emitc::CallOpaqueOp>(
         classOp.getLoc(), iteratorType, rewriter.getStringAttr("end"),
         bufferMap.getResult());
-
-    // 9. Create the operator== call
-    auto isEnd = rewriter.create<emitc::CallOpaqueOp>(
-        classOp.getLoc(), boolType,
+    emitc::CallOpaqueOp isEnd = rewriter.create<emitc::CallOpaqueOp>(
+        classOp.getLoc(), rewriter.getI1Type(),
         "operator==", mlir::ValueRange{it.getResult(0), endIt.getResult(0)});
-
-    // 10. Create the nullptr constant
-    auto nullPtr = rewriter.create<emitc::ConstantOp>(
+    emitc::ConstantOp nullPtr = rewriter.create<emitc::ConstantOp>(
         classOp.getLoc(), charPtrType,
         emitc::OpaqueAttr::get(context, "nullptr"));
-
-    // 11. Create the second call
-    auto second = rewriter.create<emitc::CallOpaqueOp>(
+    emitc::CallOpaqueOp second = rewriter.create<emitc::CallOpaqueOp>(
         classOp.getLoc(), charPtrType, "second", it.getResult(0));
 
-    // 12. Create the conditional
-    auto result = rewriter.create<emitc::ConditionalOp>(
+    emitc::ConditionalOp result = rewriter.create<emitc::ConditionalOp>(
         classOp.getLoc(), charPtrType, isEnd.getResult(0), nullPtr.getResult(),
         second.getResult(0));
 
-    // 13. Create return
     rewriter.create<emitc::ReturnOp>(classOp.getLoc(), result.getResult());
 
     return success();
   }
+
+private:
+  StringRef attributeName;
 };
 
-void mlir::emitc::populateAddReflectionMapPatterns(
-    RewritePatternSet &patterns) {
-  patterns.add<AddReflectionMapClass>(patterns.getContext());
+void mlir::emitc::populateAddReflectionMapPatterns(RewritePatternSet &patterns,
+                                                   StringRef namedAttribute) {
+  patterns.add<AddReflectionMapClass>(patterns.getContext(), namedAttribute);
 }
