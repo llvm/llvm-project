39dfbcfbf0f2d914801ea09d8f5f37363fad3d59
Daniel Sanders <daniel_l_sanders@apple.com>
2025-07-22 17:45:13 -0700

    [lldb] Fix UB cast when encountering DW_LANG_* >= eNumLanguageTypes (#150132)
    LanguageType has two kinds of enumerators in it. The first is
    DWARF-assigned enumerators which must be consecutive and match DW_LANG
    values. The second is the vendor-assigned enumerators which must be
    unique and must follow on from the DWARF-assigned values (i.e. the first
    one is currently eLanguageTypeMojo + 1) even if that collides with
    DWARF-assigned values that lldb is not yet aware of
    
    Only the DWARF-assigned enumerators may be static_cast from DW_LANG
    since their values match. The vendor-assigned enumerators must be
    explicitly converted since their values do not match. This needs to
    handle new languages added to DWARF and not yet implemented in lldb.
    
    This fixes a crash when encountering a DW_LANG value >=
    eNumLanguageTypes and wrong behaviour when encountering DW_LANG values
    that have not yet been added to LanguageType but happen to coincide with
    a vendor-assigned enumerator due to the consecutive values requirement
    described above.
    
    Another way to fix the crash is to add the language to LanguageType (and
    fill any preceeding gaps in the number space) so that the DW_LANG being
    encountered is correctly handled but this just moves the problem to a
    new subset of DW_LANG values.
    
    Also fix an unnecessary static-cast from LanguageType to LanguageType.
    
    (cherry picked from commit 726502d668d12d917411d48d0b2210f8592b23a9)

--- lldb/source/Plugins/SymbolFile/DWARF/SymbolFileDWARF.cpp
+++ lldb/source/Plugins/SymbolFile/DWARF/SymbolFileDWARF.cpp
@@ -4637,13 +4637,16 @@ SymbolFileDWARF::GetContainingDeclContext(const DWARFDIE &die) {
 }
 
 LanguageType SymbolFileDWARF::LanguageTypeFromDWARF(uint64_t val) {
+  if (val <= eLanguageTypeLastStandardLanguage)
+    return static_cast<LanguageType>(val);
+
   // Note: user languages between lo_user and hi_user must be handled
   // explicitly here.
   switch (val) {
   case DW_LANG_Mips_Assembler:
     return eLanguageTypeMipsAssembler;
   default:
-    return static_cast<LanguageType>(val);
+    return eLanguageTypeUnknown;
   }
 }
 
