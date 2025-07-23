65facef4f4893faca8900f4684ab661e555997bb
Sterling-Augustine <56981066+Sterling-Augustine@users.noreply.github.com>
2025-07-23 10:43:38 -0700

    Support SFrame command-line and .cfi_section syntax (#149935)
    This PR adds support for the llvm-mc command-line flag "--gsframe" and
    adds ".sframe" to the legal values passed ".cfi_section". It plumbs the
    option through the cfi handling code a fair amount. Code to support
    actual section generation follows in a future PR.
    
    These options match the gnu-assembler's support syntax for sframes, on
    both the command line and in assembly files.
    
    First in a series of changes that will allow llvm-mc to produce sframe
    .cfi sections. For more information about sframes, see
    https://sourceware.org/binutils/docs-2.44/sframe-spec.html
    
    and the llvm-RFC here:
    https://discourse.llvm.org/t/rfc-adding-sframe-support-to-llvm/86900

--- llvm/lib/MC/MCParser/AsmParser.cpp
+++ llvm/lib/MC/MCParser/AsmParser.cpp
@@ -4093,27 +4093,30 @@ bool AsmParser::parseDirectiveCVFPOData() {
 }
 
 /// parseDirectiveCFISections
-/// ::= .cfi_sections section [, section]
+/// ::= .cfi_sections section [, section][, section]
 bool AsmParser::parseDirectiveCFISections() {
   StringRef Name;
   bool EH = false;
   bool Debug = false;
+  bool SFrame = false;
 
   if (!parseOptionalToken(AsmToken::EndOfStatement)) {
     for (;;) {
       if (parseIdentifier(Name))
-        return TokError("expected .eh_frame or .debug_frame");
+        return TokError("expected .eh_frame, .debug_frame, or .sframe");
       if (Name == ".eh_frame")
         EH = true;
       else if (Name == ".debug_frame")
         Debug = true;
+      else if (Name == ".sframe")
+        SFrame = true;
       if (parseOptionalToken(AsmToken::EndOfStatement))
         break;
       if (parseComma())
         return true;
     }
   }
-  getStreamer().emitCFISections(EH, Debug);
+  getStreamer().emitCFISections(EH, Debug, SFrame);
   return false;
 }
 
