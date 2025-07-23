dd8efd3a4d860549c4a0585f96589d1ae0aebc0c
Qinkun Bao <qinkun@google.com>
2025-07-23 17:28:20 -0400

    [clang-repl] Disable new test after #148701 (#150294)
    https://github.com/llvm/llvm-project/pull/148701 adds new tests.
    However, these tests never pass on msan buildbots.
    https://lab.llvm.org/buildbot/#/builders/94/builds/9132
    https://lab.llvm.org/buildbot/#/builders/94/builds/9131
    https://lab.llvm.org/buildbot/#/builders/94/builds/9130

--- clang/test/Interpreter/pretty-print.c
+++ clang/test/Interpreter/pretty-print.c
@@ -3,7 +3,7 @@
 // RUN: cat %s | clang-repl -Xcc -xc  | FileCheck %s
 // RUN: cat %s | clang-repl -Xcc -std=c++11 | FileCheck %s
 
-// UNSUPPORTED: hwasan
+// UNSUPPORTED: hwasan, msan
 
 
 char c = 'a'; c
