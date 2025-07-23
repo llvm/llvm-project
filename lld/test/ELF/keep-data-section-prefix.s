b66eceebbf47540539f9aa9b4f4d544b0bfed897
mingmingl <mingmingl@google.com>
2025-07-23 12:27:40 -0700

    use {{.*}} to omit irrelevant columns in the test case

--- lld/test/ELF/keep-data-section-prefix.s
+++ lld/test/ELF/keep-data-section-prefix.s
@@ -38,12 +38,12 @@
 
 ## The first RW PT_LOAD segment has FileSiz 0x126f (0x1000 + 0x200 + 0x60 + 0xf),
 ## and its p_offset p_vaddr p_paddr p_filesz should match PT_GNU_RELRO.
-#           Type           Offset   VirtAddr           PhysAddr           FileSiz  MemSiz   Flg Align
-# SEG:      LOAD           0x0001c8 0x00000000002011c8 0x00000000002011c8 0x000001 0x000001 R E 0x1000
-# SEG-NEXT: LOAD           0x0001c9 0x00000000002021c9 0x00000000002021c9 0x00126f 0x001e37 RW  0x1000
-# SEG-NEXT: LOAD           0x001438 0x0000000000204438 0x0000000000204438 0x000001 0x000002 RW  0x1000
-# SEG-NEXT: GNU_RELRO      0x0001c9 0x00000000002021c9 0x00000000002021c9 0x00126f 0x001e37 R   0x1
-# SEG-NEXT: GNU_STACK      0x000000 0x0000000000000000 0x0000000000000000 0x000000 0x000000 RW  0x0
+#           Type           {{.*}}           FileSiz  MemSiz   Flg
+# SEG:      LOAD           {{.*}}           0x000001 0x000001 R E
+# SEG-NEXT: LOAD           {{.*}}           0x00126f 0x001e37 RW
+# SEG-NEXT: LOAD           {{.*}}           0x000001 0x000002 RW
+# SEG-NEXT: GNU_RELRO      {{.*}}           0x00126f 0x001e37 R
+# SEG-NEXT: GNU_STACK      {{.*}}           0x000000 0x000000 RW
 
 ## Input to output mapping per linker script
 ##   .data.rel.ro.split -> .data.rel.ro
@@ -54,13 +54,13 @@
 # LS-NEXT: .data.rel.ro.hot .data.rel.ro .data.rel.ro.unlikely .relro_padding
 # LS-NEXT: .data .bss
 
-#        [Nr] Name                    Type            Address          Off    Size
-# CHECK-LS:      .data.rel.ro.hot        PROGBITS        00000000002021c9 0001c9 00000f
-# CHECK-LS-NEXT: .data.rel.ro            PROGBITS        00000000002021d8 0001d8 000260
-# CHECK-LS-NEXT: .data.rel.ro.unlikely   PROGBITS        0000000000202438 000438 001000
-# CHECK-LS-NEXT: .relro_padding          NOBITS          0000000000203438 001438 000bc8
-# CHECK-LS-NEXT: .data                   PROGBITS        0000000000204438 001438 000001
-# CHECK-LS-NEXT: .bss                    NOBITS          0000000000204439 001439 000001
+#        [Nr] Name                    Type           {{.*}}    Size
+# CHECK-LS:      .data.rel.ro.hot        PROGBITS    {{.*}} 00000f
+# CHECK-LS-NEXT: .data.rel.ro            PROGBITS    {{.*}} 000260
+# CHECK-LS-NEXT: .data.rel.ro.unlikely   PROGBITS    {{.*}} 001000
+# CHECK-LS-NEXT: .relro_padding          NOBITS      {{.*}} 000bc8
+# CHECK-LS-NEXT: .data                   PROGBITS    {{.*}} 000001
+# CHECK-LS-NEXT: .bss                    NOBITS      {{.*}} 000001
 
 ## Linker script is not provided to map data sections.
 ## So all input sections with prefix .data.rel.ro will map to .data.rel.ro in the output.
@@ -68,11 +68,11 @@
 # PRE-NEXT: .data.rel.ro .relro_padding
 # PRE-NEXT: .data .bss
 
-#        [Nr] Name                    Type            Address          Off    Size
-# CHECK-PRE:      .data.rel.ro            PROGBITS        00000000002021c9 0001c9 00126f
-# CHECK-PRE-NEXT: .relro_padding          NOBITS          0000000000203438 001438 000bc8
-# CHECK-PRE-NEXT: .data                   PROGBITS        0000000000204438 001438 000001
-# CHECK-PRE-NEXT: .bss                    NOBITS          0000000000204439 001439 000001
+#        [Nr] Name                    Type            {{.*}}    Size
+# CHECK-PRE:      .data.rel.ro            PROGBITS    {{.*}}    00126f
+# CHECK-PRE-NEXT: .relro_padding          NOBITS      {{.*}}    000bc8
+# CHECK-PRE-NEXT: .data                   PROGBITS    {{.*}}    000001
+# CHECK-PRE-NEXT: .bss                    NOBITS      {{.*}}    000001
 
 # BASIC:         [Nr] Name              Type            {{.*}}   Size              
 # BASIC:         [ 1] .text       
