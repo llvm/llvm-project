140f4053e6601585a3e27b285f1c00204fca0728
Bar Soloveychik <barsolo@fb.com>
2025-07-23 15:17:29 -0700

    [LLDB] added getName method in SBModule

--- lldb/source/API/SBModule.cpp
+++ lldb/source/API/SBModule.cpp
@@ -671,3 +671,11 @@ void SBModule::GarbageCollectAllocatedModules() {
   const bool mandatory = false;
   ModuleList::RemoveOrphanSharedModules(mandatory);
 }
+
+const char *SBModule::GetName() const {
+  LLDB_INSTRUMENT_VA(this);
+  if (!m_opaque_sp) {
+    return nullptr;
+  }
+  return m_opaque_sp->GetObjectName().AsCString();
+}
