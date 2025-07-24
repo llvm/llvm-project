ee5078d66420c7670814accb1340783a177ff944
Sriya Pratipati <sriyap@google.com>
2025-07-24 19:59:46 +0000

    fixed implementation header

--- libc/fuzzing/math/f16sqrt_fuzz.cpp
+++ libc/fuzzing/math/f16sqrt_fuzz.cpp
@@ -1,5 +1,4 @@
-//===-- f16sqrt_fuzz.cpp
-//----------------------------------------------------===//
+//===-- f16sqrt_fuzz.cpp --------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
