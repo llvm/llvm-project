41af146f9c6a65980f85a302b59dcb488987b7f2
Andrew Jenner <Andrew.Jenner@amd.com>
2025-07-24 13:01:55 -0400

    Fix and test expansions.

--- llvm/lib/Target/AMDGPU/SIISelLowering.cpp
+++ llvm/lib/Target/AMDGPU/SIISelLowering.cpp
@@ -18531,14 +18531,7 @@ static bool globalMemoryFPAtomicIsLegal(const GCNSubtarget &Subtarget,
 
 /// \return Action to perform on AtomicRMWInsts for integer operations.
 static TargetLowering::AtomicExpansionKind
-atomicSupportedIfLegalIntType(const AtomicRMWInst *RMW, bool Allow64 = true) {
-  if (!Allow64) {
-    if (auto *IT = dyn_cast<IntegerType>(RMW->getType())) {
-      if (IT->getBitWidth() == 32)
-        return TargetLowering::AtomicExpansionKind::None;
-    }
-    return TargetLowering::AtomicExpansionKind::CmpXChg;
-  }
+atomicSupportedIfLegalIntType(const AtomicRMWInst *RMW) {
   return isAtomicRMWLegalIntTy(RMW->getType())
              ? TargetLowering::AtomicExpansionKind::None
              : TargetLowering::AtomicExpansionKind::CmpXChg;
@@ -18600,8 +18593,21 @@ SITargetLowering::shouldExpandAtomicRMWInIR(AtomicRMWInst *RMW) const {
     // PCIe supports add and xchg for system atomics.
     return atomicSupportedIfLegalIntType(RMW);
   case AtomicRMWInst::USubCond:
+    if (Subtarget->hasCondSubInsts()) {
+      if (auto *IT = dyn_cast<IntegerType>(RMW->getType())) {
+        if (IT->getBitWidth() == 32)
+          return TargetLowering::AtomicExpansionKind::None;
+      }
+    }
+    return TargetLowering::AtomicExpansionKind::CmpXChg;
   case AtomicRMWInst::USubSat:
-    return atomicSupportedIfLegalIntType(RMW, false);
+    if (Subtarget->hasSubClampInsts()) {
+      if (auto *IT = dyn_cast<IntegerType>(RMW->getType())) {
+        if (IT->getBitWidth() == 32)
+          return TargetLowering::AtomicExpansionKind::None;
+      }
+    }
+    return TargetLowering::AtomicExpansionKind::CmpXChg;
   case AtomicRMWInst::Sub:
   case AtomicRMWInst::And:
   case AtomicRMWInst::Or:
