3aac143dc0450ee74a859ede46cf4ab74099cfdb
hanhanW <hanhan0912@gmail.com>
2025-07-24 10:56:15 -0700

    add includes back because of canFoldSlice method
    Signed-off-by: hanhanW <hanhan0912@gmail.com>

--- mlir/include/mlir/Dialect/Linalg/IR/Linalg.h
+++ mlir/include/mlir/Dialect/Linalg/IR/Linalg.h
@@ -10,6 +10,7 @@
 #define MLIR_DIALECT_LINALG_IR_LINALG_H
 
 #include "mlir/Bytecode/BytecodeOpInterface.h"
+#include "mlir/Dialect/Tensor/IR/Tensor.h"
 #include "mlir/Dialect/Utils/ReshapeOpsUtils.h"
 #include "mlir/Dialect/Utils/StructuredOpsUtils.h"
 #include "mlir/IR/AffineExpr.h"
