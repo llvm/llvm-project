62399b6a35432cf26e5f53854361bc69934020c7
lntue <lntue@google.com>
2025-07-24 00:19:52 +0000

    [libc][NFC] Add stdint.h proxy header to fix dependency issue with <stdint.h> includes. (#150303)
    https://github.com/llvm/llvm-project/issues/149993

--- libc/benchmarks/gpu/timing/nvptx/timing.h
+++ libc/benchmarks/gpu/timing/nvptx/timing.h
@@ -9,6 +9,7 @@
 #ifndef LLVM_LIBC_UTILS_GPU_TIMING_NVPTX
 #define LLVM_LIBC_UTILS_GPU_TIMING_NVPTX
 
+#include "hdr/stdint_proxy.h"
 #include "src/__support/CPP/array.h"
 #include "src/__support/CPP/atomic.h"
 #include "src/__support/CPP/type_traits.h"
@@ -16,8 +17,6 @@
 #include "src/__support/common.h"
 #include "src/__support/macros/attributes.h"
 #include "src/__support/macros/config.h"
-
-#include <stdint.h>
 
 namespace LIBC_NAMESPACE_DECL {
 
