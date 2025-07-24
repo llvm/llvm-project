386243eae3986e5f3f02d5ade3ee73953ae5629d
Jacques Pienaar <jacques+gh@japienaar.info>
2025-07-24 21:16:03 +0000

    Add test for opt mode

--- llvm/unittests/Support/DebugLogTest.cpp
+++ llvm/unittests/Support/DebugLogTest.cpp
@@ -13,6 +13,7 @@
 
 #include <string>
 using namespace llvm;
+using testing::Eq;
 using testing::HasSubstr;
 
 #ifndef NDEBUG
@@ -50,6 +51,28 @@ TEST(DebugLogTest, Basic) {
       DEBUGLOG_WITH_STREAM_AND_TYPE(os, "A") << "B";
     DEBUGLOG_WITH_STREAM_AND_TYPE(os, "B") << "B";
     EXPECT_THAT(os.str(), AllOf(HasSubstr("A\n"), Not(HasSubstr("B\n"))));
+
+    int count = 0;
+    auto inc = [&]() { return ++count; };
+    EXPECT_THAT(count, Eq(0));
+    DEBUGLOG_WITH_STREAM_AND_TYPE(os, "A") << inc();
+    EXPECT_THAT(count, Eq(1));
+    DEBUGLOG_WITH_STREAM_AND_TYPE(os, "B") << inc();
+    EXPECT_THAT(count, Eq(1));
   }
+
+}
+#else
+TEST(DebugLogTest, Basic) {
+  // LDBG should be compiled out in NDEBUG, so just check it compiles and has
+  // no effect.
+  llvm::DebugFlag = true;
+  static const char *DT[] = {};
+  setCurrentDebugTypes(DT, 0);
+  int count = 0;
+  auto inc = [&]() { return ++count; };
+  EXPECT_THAT(count, Eq(0));
+  LDBG() << inc();
+  EXPECT_THAT(count, Eq(0));
 }
 #endif
