0aadd571cb21ffb1d43390b9fa2b2b9aed4c5555
William Huynh <William.Huynh@arm.com>
2025-07-24 16:29:11 +0100

    [libc] Fix issue with sigjmp_buf.h not being found
    When trying to use <setjmp.h>, it will try to include
    llvm-libc-types/sigjmp_buf.h due to the way that headergen works.
    This commit creates a dummy file, as the real implementation is
    found in llvm-libc-types/jmp_buf.h.

---
+++ libc/include/llvm-libc-types/sigjmp_buf.h
@@ -0,0 +1,15 @@
+//===-- Definition of type sigjmp_buf -------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_LIBC_TYPES_SIGJMP_BUF_H
+#define LLVM_LIBC_TYPES_SIGJMP_BUF_H
+
+// Intentionally left blank. sigjmp_buf is defined in jmp_buf.h, but due to the
+// way headergen works, this file is required.
+
+#endif // LLVM_LIBC_TYPES_SIGJMP_BUF_H
