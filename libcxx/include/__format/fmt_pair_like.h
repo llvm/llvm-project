b0a2167fb926fc2a0b62ed804c2db1d4cb94d3fc
A. Jiang <de34@live.cn>
2025-07-25 16:02:09 +0800

    Switching `__fmt_pair_like` back to the original implementation

--- libcxx/include/__format/fmt_pair_like.h
+++ libcxx/include/__format/fmt_pair_like.h
@@ -13,6 +13,8 @@
 #include <__config>
 #include <__fwd/pair.h>
 #include <__fwd/tuple.h>
+#include <__tuple/tuple_size.h>
+#include <__type_traits/is_specialization.h>
 
 #if !defined(_LIBCPP_HAS_NO_PRAGMA_SYSTEM_HEADER)
 #  pragma GCC system_header
@@ -29,16 +31,9 @@ _LIBCPP_BEGIN_NAMESPACE_STD
 
 // [format.range.fmtkind]/2.2.1 and [tab:formatter.range.type]:
 // "U is either a specialization of pair or a specialization of tuple such that tuple_size_v<U> is 2."
-// We use an alternative approach to avoid dragging in many templates.
-template <class>
-inline constexpr bool __is_fmt_pair_like_v = false;
-template <class _Tp, class _Up>
-inline constexpr bool __is_fmt_pair_like_v<pair<_Tp, _Up>> = true;
-template <class _Tp, class _Up>
-inline constexpr bool __is_fmt_pair_like_v<tuple<_Tp, _Up>> = true;
-
 template <class _Tp>
-concept __fmt_pair_like = __is_fmt_pair_like_v<_Tp>;
+concept __fmt_pair_like =
+    __is_specialization_v<_Tp, pair> || (__is_specialization_v<_Tp, tuple> && tuple_size_v<_Tp> == 2);
 
 #endif // _LIBCPP_STD_VER >= 23
 
