cb37916a2596e0936c76bbd9927f8353faac9fa8
dyung <douglas.yung@sony.com>
2025-07-25 23:21:59 -0400

    Revert "MC: Allocate initial fragment and define section symbol in changeSection" (#150736)
    Reverts llvm/llvm-project#150574
    
    This is causing a test failure on AArch64 MacOS bots:
    https://lab.llvm.org/buildbot/#/builders/190/builds/24187

--- llvm/test/MC/ELF/section-sym2.s
+++ llvm/test/MC/ELF/section-sym2.s
@@ -1,40 +1,27 @@
 # RUN: llvm-mc -filetype=obj -triple x86_64 %s -o %t
-# RUN: llvm-readelf -SrsX %t | FileCheck %s
+# RUN: llvm-readelf -Srs %t | FileCheck %s
 
 ## Test that we can forward reference a section.
 
 mov .rodata, %rsi
-mov data, %rsi
 mov .debug_info, %rsi
-mov .debug_abbrev, %rsi
 
 .section .rodata,"a"
-.pushsection data, 2; .long 2; .popsection
-.section data; .long 1
 .section .debug_info,"G",@progbits,11,comdat; .long x1
 .section .debug_info,"G",@progbits,22,comdat; .long x2
 .section .debug_info,"",@progbits; .long x0
 
-.text
-mov data, %rdi
-
-# CHECK:      Relocation section '.rela.text'
-# CHECK:      R_X86_64_32S {{.*}} data + 0
-# CHECK:      R_X86_64_32S {{.*}} data + 0
-
 # CHECK:      Relocation section '.rela.debug_info' at offset {{.*}} contains 1
 # CHECK:      Relocation section '.rela.debug_info' at offset {{.*}} contains 1
 # CHECK:      Relocation section '.rela.debug_info' at offset {{.*}} contains 1
 
-# CHECK:      Symbol table '.symtab' contains 10 entries:
-# CHECK-NEXT:    Num:
+# CHECK:      Symbol table '.symtab' contains 8 entries:
+# CHECK-NEXT:    Num:    Value          Size Type    Bind   Vis       Ndx Name
 # CHECK-NEXT:  0000000000000000     0 NOTYPE  LOCAL  DEFAULT   UND
-# CHECK-NEXT:  0000000000000000     0 SECTION LOCAL  DEFAULT [[#]] (.rodata) .rodata
-# CHECK-NEXT:  0000000000000000     0 SECTION LOCAL  DEFAULT [[#]] (data) data
-# CHECK-NEXT:  0000000000000000     0 SECTION LOCAL  DEFAULT [[#]] (.debug_info) .debug_info
-# CHECK-NEXT:  0000000000000000     0 NOTYPE  LOCAL  DEFAULT [[#]] (.group) 11
-# CHECK-NEXT:  0000000000000000     0 NOTYPE  LOCAL  DEFAULT [[#]] (.group) 22
-# CHECK-NEXT:  0000000000000000     0 NOTYPE  GLOBAL DEFAULT   UND .debug_abbrev
+# CHECK-NEXT:  0000000000000000     0 SECTION LOCAL  DEFAULT     4 .rodata
+# CHECK-NEXT:  0000000000000000     0 SECTION LOCAL  DEFAULT    11 .debug_info
+# CHECK-NEXT:  0000000000000000     0 NOTYPE  LOCAL  DEFAULT     5 11
+# CHECK-NEXT:  0000000000000000     0 NOTYPE  LOCAL  DEFAULT     8 22
 # CHECK-NEXT:  0000000000000000     0 NOTYPE  GLOBAL DEFAULT   UND x1
 # CHECK-NEXT:  0000000000000000     0 NOTYPE  GLOBAL DEFAULT   UND x2
 # CHECK-NEXT:  0000000000000000     0 NOTYPE  GLOBAL DEFAULT   UND x0
