230228d29dfbe01a12c8945898aff47888965418
Mehdi Amini <joker.eph@gmail.com>
2025-07-26 11:34:10 +0200

    Strip the full path from __FILE__ in the LDBG macro and keep only the filename (#150677)

--- llvm/include/llvm/Support/DebugLog.h
+++ llvm/include/llvm/Support/DebugLog.h
@@ -26,10 +26,17 @@ namespace llvm {
 //              << "] " << "Bitset contains: " << Bitset << "\n");
 #define LDBG() DEBUGLOG_WITH_STREAM_AND_TYPE(llvm::dbgs(), DEBUG_TYPE)
 
+#if defined(__SHORT_FILE__)
+#define DEBUGLOG_WITH_STREAM_AND_TYPE(STREAM, TYPE)                            \
+  for (bool _c = (::llvm::DebugFlag && ::llvm::isCurrentDebugType(TYPE)); _c;  \
+       _c = false)                                                             \
+  ::llvm::impl::LogWithNewline(TYPE, __SHORT_FILE__, __LINE__, (STREAM))
+#else
 #define DEBUGLOG_WITH_STREAM_AND_TYPE(STREAM, TYPE)                            \
   for (bool _c = (::llvm::DebugFlag && ::llvm::isCurrentDebugType(TYPE)); _c;  \
        _c = false)                                                             \
   ::llvm::impl::LogWithNewline(TYPE, __FILE__, __LINE__, (STREAM))
+#endif
 
 namespace impl {
 class LogWithNewline {
@@ -37,6 +44,9 @@ public:
   LogWithNewline(const char *debug_type, const char *file, int line,
                  raw_ostream &os)
       : os(os) {
+#if !defined(__SHORT_FILE__)
+    file = ::llvm::impl::LogWithNewline::getShortFileName(file);
+#endif
     if (debug_type)
       os << "[" << debug_type << "] ";
     os << file << ":" << line << " ";
@@ -51,6 +61,16 @@ public:
   LogWithNewline(const LogWithNewline &) = delete;
   LogWithNewline &operator=(const LogWithNewline &) = delete;
   LogWithNewline &operator=(LogWithNewline &&) = delete;
+  static constexpr const char *getShortFileName(const char *path) {
+    // Remove the path prefix from the file name.
+    const char *filename = path;
+    for (const char *p = path; *p != '\0'; ++p) {
+      if (*p == '/' || *p == '\\') {
+        filename = p + 1;
+      }
+    }
+    return filename;
+  }
 
 private:
   raw_ostream &os;
