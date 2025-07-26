1cac44eaa17f3430eec282e6f8fd1d7d5c882695
Fangrui Song <i@maskray.me>
2025-07-26 10:24:06 -0700

    MCSection: Remove SectionVariant
    The object file format specific derived classes are used in context like
    MCStreamer and MCObjectTargetWriter where the type is statically known.
    We don't use isa/dyn_cast.

--- llvm/include/llvm/MC/MCSectionELF.h
+++ llvm/include/llvm/MC/MCSectionELF.h
@@ -58,8 +58,8 @@ private:
                unsigned entrySize, const MCSymbolELF *group, bool IsComdat,
                unsigned UniqueID, MCSymbol *Begin,
                const MCSymbolELF *LinkedToSym)
-      : MCSection(SV_ELF, Name, flags & ELF::SHF_EXECINSTR,
-                  type == ELF::SHT_NOBITS, Begin),
+      : MCSection(Name, flags & ELF::SHF_EXECINSTR, type == ELF::SHT_NOBITS,
+                  Begin),
         Type(type), Flags(flags), UniqueID(UniqueID), EntrySize(entrySize),
         Group(group, IsComdat), LinkedToSym(LinkedToSym) {
     assert((!(Flags & ELF::SHF_GROUP) || Group.getPointer()) &&
--- llvm/include/llvm/MC/MCSectionWasm.h
+++ llvm/include/llvm/MC/MCSectionWasm.h
@@ -51,7 +51,7 @@ class MCSectionWasm final : public MCSection {
   friend class MCContext;
   MCSectionWasm(StringRef Name, SectionKind K, unsigned SegmentFlags,
                 const MCSymbolWasm *Group, unsigned UniqueID, MCSymbol *Begin)
-      : MCSection(SV_Wasm, Name, K.isText(), /*IsVirtual=*/false, Begin),
+      : MCSection(Name, K.isText(), /*IsVirtual=*/false, Begin),
         UniqueID(UniqueID), Group(Group),
         IsWasmData(K.isReadOnly() || K.isWriteable()),
         IsMetadata(K.isMetadata()), SegmentFlags(SegmentFlags) {}
