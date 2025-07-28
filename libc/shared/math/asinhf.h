a9db5d1627fe0ed239a50572df5fad8e83a4d3bf
Muhammad Bassiouni <60100307+bassiounix@users.noreply.github.com>
2025-07-28 18:14:47 +0300

    [libc][math] Refactor asinhf implementation to header-only in src/__support/math folder. (#150843)
    Part of #147386
    
    in preparation for: https://discourse.llvm.org/t/rfc-make-clang-builtin-math-functions-constexpr-with-llvm-libc-to-support-c-23-constexpr-math-functions/86450

--- libc/shared/math/asinhf.h
+++ libc/shared/math/asinhf.h
@@ -1,4 +1,4 @@
-//===-- Single-precision asinh function -----------------------------------===//
+//===-- Shared asinhf function ----------------------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,11 +6,18 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "src/math/asinhf.h"
+#ifndef LLVM_LIBC_SHARED_MATH_ASINHF_H
+#define LLVM_LIBC_SHARED_MATH_ASINHF_H
+
+#include "shared/libc_common.h"
 #include "src/__support/math/asinhf.h"
 
 namespace LIBC_NAMESPACE_DECL {
+namespace shared {
 
-LLVM_LIBC_FUNCTION(float, asinhf, (float x)) { return math::asinhf(x); }
+using math::asinhf;
 
+} // namespace shared
 } // namespace LIBC_NAMESPACE_DECL
+
+#endif // LLVM_LIBC_SHARED_MATH_ASINHF_H
