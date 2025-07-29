1c67334f4629588031b4bf8987ceb9d5ff0efec2
Rana Pratap Reddy N <RanaPratapReddy.Nimmakayala@amd.com>
2025-07-29 03:51:25 -0500

    Revert "[Clang] Reland '__has_builtin should return false for aux triple builtins' (#126324)"
    This reverts commit 0efcb83626362213fb6cc99c4af42a93e74e6afe.

--- clang/lib/Lex/PPMacroExpansion.cpp
+++ clang/lib/Lex/PPMacroExpansion.cpp
@@ -1760,8 +1760,7 @@ void Preprocessor::ExpandBuiltinMacro(Token &Tok) {
               Tok, *this, diag::err_feature_check_malformed);
           if (!II)
             return false;
-          unsigned BuiltinID = II->getBuiltinID();
-          if (BuiltinID != 0) {
+          else if (II->getBuiltinID() != 0) {
             switch (II->getBuiltinID()) {
             case Builtin::BI__builtin_cpu_is:
               return getTargetInfo().supportsCpuIs();
@@ -1775,11 +1774,8 @@ void Preprocessor::ExpandBuiltinMacro(Token &Tok) {
               // usual allocation and deallocation functions. Required by libc++
               return 201802;
             default:
-              // __has_builtin should return false for aux builtins.
-              if (getBuiltinInfo().isAuxBuiltinID(BuiltinID))
-                return false;
               return Builtin::evaluateRequiredTargetFeatures(
-                  getBuiltinInfo().getRequiredFeatures(BuiltinID),
+                  getBuiltinInfo().getRequiredFeatures(II->getBuiltinID()),
                   getTargetInfo().getTargetOpts().FeatureMap);
             }
             return true;
