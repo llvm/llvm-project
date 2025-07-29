6f32572c2de010052d9f771a012437ff9d6c6b1a
Adam Siemieniuk <adam.siemieniuk@intel.com>
2025-07-29 21:50:26 +0200

    Fix typo

--- mlir/lib/Conversion/VectorToAMX/VectorToAMX.cpp
+++ mlir/lib/Conversion/VectorToAMX/VectorToAMX.cpp
@@ -1,4 +1,4 @@
-//===- VectorToXeGPU.cpp - Convert vector to AMX dialect --------*- C++ -*-===//
+//===- VectorToAMX.cpp - Convert vector to AMX dialect ----------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,7 +6,7 @@
 //
 //===----------------------------------------------------------------------===//
 //
-// This file implements lowering of vector operations to XeGPU dialect ops.
+// This file implements lowering of vector operations to AMX dialect ops.
 //
 //===----------------------------------------------------------------------===//
 
