846aea69414b1d72bd6cff25d7c9e850ec0be417
Hristo Hristov <hristo.goshev.hristov@gmail.com>
2025-07-29 17:25:30 +0300

    Update libcxx/test/std/input.output/iostream.format/input.streams/istream.unformatted/ignore.char_type.pass.cpp

--- libcxx/test/std/input.output/iostream.format/input.streams/istream.unformatted/ignore.char_type.pass.cpp
+++ libcxx/test/std/input.output/iostream.format/input.streams/istream.unformatted/ignore.char_type.pass.cpp
@@ -22,19 +22,19 @@
 
 int main(int, char**) {
   std::istringstream in("\xF0\x9F\xA4\xA1 Clown Face");
-  in.ignore(100, '\xA1'); // ignore up to '\xA1' delimiter,
-                          // previously might have ignored to EOF
+  in.ignore(100, '\xA1'); // Ignore up to '\xA1' delimiter,
+                          // previously might have ignored to EOF.
 
-  assert(in.gcount() == 4); // 4 bytes were ignored
-  assert(in.peek() == ' '); // next character is a space
+  assert(in.gcount() == 4); // 4 bytes were ignored.
+  assert(in.peek() == ' '); // Next character is a space.
 
-  std::string str; // read the next word
+  std::string str; // Read the next word.
   in >> str;
   assert(str == "Clown");
 
   // Parameter value "-1L" doesn't cause ambiguity with the char_type overload.
-  in.ignore(100, -1L); // ignore up to EOF, which is the default behavior
-  assert(in.eof());    // stream should be at EOF now
+  in.ignore(100, -1L); // Ignore up to EOF, which is the default behavior.
+  assert(in.eof());    // Stream should be at EOF now.
   assert(in.gcount() == 5);
 
   return 0;
