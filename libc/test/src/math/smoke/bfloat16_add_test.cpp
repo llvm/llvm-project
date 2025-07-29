72f23bc472f3ffea60e5f056b996f009976c63e8
Krishna Pandey <kpandey81930@gmail.com>
2025-07-30 01:58:25 +0530

    chore: update bf16 add test header
    Signed-off-by: Krishna Pandey <kpandey81930@gmail.com>

--- libc/test/src/math/smoke/bfloat16_add_test.cpp
+++ libc/test/src/math/smoke/bfloat16_add_test.cpp
@@ -1,4 +1,4 @@
-//===-- Unittests for f16add ----------------------------------------------===//
+//===-- Unittests for bfloat16 addition -----------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -10,6 +10,6 @@
 
 #include "src/__support/FPUtil/bfloat16.h"
 
-bfloat16 add_func(bfloat16 x, bfloat16 y) { return x + y; }
+static inline bfloat16 add_func(bfloat16 x, bfloat16 y) { return x + y; }
 
 LIST_ADD_TESTS(bfloat16, bfloat16, add_func)
