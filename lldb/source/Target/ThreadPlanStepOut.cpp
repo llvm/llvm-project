llvm/llvm-project

commit cbb4e99f3613549c2168f52d6f348e3a7ee3cf55

[lldb] Update ThreadPlanStepOut to handle new breakpoint behavior (#126838)
I will be changing breakpoint hitting behavior soon, where currently
lldb reports a breakpoint as being hit when a thread is *at* a
BreakpointSite, but possibly has not executed the breakpoint instruction
and trapped yet, to having lldb only report a breakpoint hit when the
breakpoint instruction has actually been executed.

One corner case bug with this change is that when you are stopped at a
breakpoint (that has been hit) on the last instruction of a function,
and you do `finish`, a ThreadPlanStepOut is pushed to the thread's plan
stack to put a breakpoint on the return address and resume execution.
And when the thread is asked to resume, it sees that it is at a
BreakpointSite that has been hit, and pushes a
ThreadPlanStepOverBreakpoint on the thread.   The StepOverBreakpoint
plan sees that the thread's state is eStateRunning (not eStateStepping),
so it marks itself as "auto continue" -- so once the breakpoint has
been stepped over, we will execution on the thread.

With current lldb stepping behavior ("a thread *at* a BreakpointSite is
said to have stopped with a breakpoint-hit stop reason, even if the
breakpoint hasn't been executed yet"),
`ThreadPlanStepOverBreakpoint::DoPlanExplainsStop` has a special bit of
code which detects when the thread stops with a eStopReasonBreakpoint.
It first checks if the pc is the same as when we started -- did our
"step instruction" not actually step? -- says the stop reason is
explained. Otherwise it sets auto-continue to false (because we've hit
an *unexpected* breakpoint, and we have advanced past our original pc,
and returns false - the stop reason is not explained.

So we do the "finish", lldb instruction steps, we stop *at* the
return-address breakpoint and lldb sets the thread's stop reason to
breakpoint-hit. ThreadPlanStepOverBreakpoint sees an
eStopReasonBreakpoint, sets its auto-continue to false, and says we

lldb/source/Target/ThreadPlanStepOut.cpp
@@ -364,8 +364,11 @@ bool ThreadPlanStepOut::ShouldStop(Event *event_ptr) {
   }
 
   if (!done) {
-    StackID frame_zero_id = GetThread().GetStackFrameAtIndex(0)->GetStackID();
-    done = !(frame_zero_id < m_step_out_to_id);
+    StopInfoSP stop_info_sp = GetPrivateStopInfo();
+    if (stop_info && stop_info_sp->GetStopReason() == eStopReasonBreakpoint) {
+      StackID frame_zero_id = GetThread().GetStackFrameAtIndex(0)->GetStackID();
+      done = !(frame_zero_id < m_step_out_to_id);
+    }
   }
 
   // The normal step out computations think we are done, so all we need to do

lldb/test/API/functionalities/thread/finish-from-empty-func/Makefile
@@ -1,0 +1,3 @@
+C_SOURCES := main.c
+
+include Makefile.rules

lldb/test/API/functionalities/thread/finish-from-empty-func/TestEmptyFuncThreadStepOut.py
@@ -1,0 +1,43 @@
+"""
+Test finish out of an empty function (may be one-instruction long)
+"""
+
+import lldb
+from lldbsuite.test.decorators import *
+from lldbsuite.test.lldbtest import *
+from lldbsuite.test import lldbutil
+
+
+class FinishFromEmptyFunctionTestCase(TestBase):
+    NO_DEBUG_INFO_TESTCASE = True
+
+    def test_finish_from_empty_function(self):
+        """Test that when stopped at a breakpoint in an empty function, finish leaves it correctly."""
+        self.build()
+        exe = self.getBuildArtifact("a.out")
+        target, process, thread, _ = lldbutil.run_to_name_breakpoint(
+            self, "done", exe_name=exe
+        )
+        if self.TraceOn():
+            self.runCmd("bt")
+
+        correct_stepped_out_line = line_number("main.c", "leaving main")
+        return_statement_line = line_number("main.c", "return 0")
+        safety_bp = target.BreakpointCreateByLocation(
+            lldb.SBFileSpec("main.c"), return_statement_line
+        )
+        self.assertTrue(safety_bp.IsValid())
+
+        error = lldb.SBError()
+        thread.StepOut(error)
+        self.assertTrue(error.Success())
+
+        if self.TraceOn():
+            self.runCmd("bt")
+
+        frame = thread.GetSelectedFrame()
+        self.assertEqual(
+            frame.line_entry.GetLine(),
+            correct_stepped_out_line,
+            "Step-out lost control of execution, ran too far",
+        )

lldb/test/API/functionalities/thread/finish-from-empty-func/main.c
@@ -1,0 +1,8 @@
+#include <stdio.h>
+void done() {}
+int main() {
+  puts("in main");
+  done();
+  puts("leaving main");
+  return 0;
+}

commit 8189d590e71984243923922c8d974c6f55613c0e

[lldb] enable ThreadPlanStepOut internal breakpoint in any case
Commit cbb4e99f3613549c2168f52d6f348e3a7ee3cf55 introduced a regression
in lldb software watchpoint tests.

The issue occurs in the following scenario:
    1. Set a software watchpoint
    2. Step over a function call
    3. The watchpoint triggers inside the function being stepped over
    4. After continuing execution, we expect to stop after returning
from
the function (as with a normal step-over operation)

However, with the mentioned patch, execution no longer stops at the end
of the function. The patch seems to have modified ThreadPlanStepOut's
stop condition - it now only stops when the stop reason is
eStopReasonBreakpoint.

The problem occurs because ThreadPlanStepOut only enables its internal
breakpoint when it's at the top of the ThreadPlanStack. With software
watchpoints, execution proceeds step-by-step while checking if any
watched values change after each step. In this case,
ThreadPlanWatchpointStepInstruction remains higher on the stack than
ThreadPlanStepOut, therefore the internal breakpoint stays disabled. As
a result, we stop due to eStopReasonTrace instead of the desired
eStopReasonBreakpoint, and consequently fail to stop at the intended
location.

This patch removes the requirement for ThreadPlanStepOut to be the
current plan and enables the internal breakpoint unconditionally. I
believe this check is legacy code that can be safely removed - I was
quite surprised to find this breakpoint isn't always enabled by default.

However, if issues arise, we could consider modifying the condition
introduced by the patch. For example, instead of checking the stop
reason, we could verify whether the stop address matches the target
address of the breakpoint that ThreadPlanStepOut uses to stop at the end
of the step.

lldb/source/Target/ThreadPlanStepOut.cpp
@@ -440,11 +440,10 @@ bool ThreadPlanStepOut::DoWillResume(StateType resume_state,
   if (m_return_bp_id == LLDB_INVALID_BREAK_ID)
     return false;
 
-  if (current_plan) {
-    Breakpoint *return_bp = GetTarget().GetBreakpointByID(m_return_bp_id).get();
-    if (return_bp != nullptr)
-      return_bp->SetEnabled(true);
-  }
+  Breakpoint *return_bp = GetTarget().GetBreakpointByID(m_return_bp_id).get();
+  if (return_bp != nullptr)
+    return_bp->SetEnabled(true);
+
   return true;
 }
 