b12c23446a36f87e9df963e6b8cc5e0a4b5efb7c
Mel Chen <mel.chen@sifive.com>
2025-07-30 03:05:33 -0700

    remove braces

--- llvm/lib/Transforms/Vectorize/VPlanRecipes.cpp
+++ llvm/lib/Transforms/Vectorize/VPlanRecipes.cpp
@@ -948,9 +948,8 @@ Value *VPInstruction::generate(VPTransformState &State) {
   }
   case VPInstruction::ResumeForEpilogue:
     return State.get(getOperand(0), true);
-  case VPInstruction::Reverse: {
+  case VPInstruction::Reverse:
     return Builder.CreateVectorReverse(State.get(getOperand(0)), "reverse");
-  }
   default:
     llvm_unreachable("Unsupported opcode for instruction");
   }
