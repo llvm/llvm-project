fcdb2f599254b0fee664de9e3e624b759f2640a8
Abhinav Gaba <abhinav.gaba@intel.com>
2025-07-30 11:34:28 -0700

    Use unique_ptr, avoid c-style casts.

--- offload/libomptarget/interface.cpp
+++ offload/libomptarget/interface.cpp
@@ -30,6 +30,7 @@
 #include <cstdint>
 #include <cstdio>
 #include <cstdlib>
+#include <memory>
 
 #ifdef OMPT_SUPPORT
 using namespace llvm::omp::target::ompt;
@@ -167,13 +168,13 @@ targetData(ident_t *Loc, int64_t DeviceId, int32_t ArgNum, void **ArgsBase,
   int Rc = OFFLOAD_SUCCESS;
 
   // Only allocate AttachInfo for targetDataBegin
-  AttachInfoTy *AttachInfo = nullptr;
+  std::unique_ptr<AttachInfoTy> AttachInfo;
   if (TargetDataFunction == targetDataBegin)
-    AttachInfo = new AttachInfoTy();
+    AttachInfo = std::make_unique<AttachInfoTy>();
 
   Rc = TargetDataFunction(Loc, *DeviceOrErr, ArgNum, ArgsBase, Args, ArgSizes,
-                          ArgTypes, ArgNames, ArgMappers, AsyncInfo, AttachInfo,
-                          /*FromMapper=*/false);
+                          ArgTypes, ArgNames, ArgMappers, AsyncInfo,
+                          AttachInfo.get(), /*FromMapper=*/false);
 
   if (Rc == OFFLOAD_SUCCESS) {
     // Process deferred ATTACH entries BEFORE synchronization
@@ -183,9 +184,6 @@ targetData(ident_t *Loc, int64_t DeviceId, int32_t ArgNum, void **ArgsBase,
     if (Rc == OFFLOAD_SUCCESS)
       Rc = AsyncInfo.synchronize();
   }
-
-  if (AttachInfo)
-    delete AttachInfo;
 
   handleTargetOutcome(Rc == OFFLOAD_SUCCESS, Loc);
 }
