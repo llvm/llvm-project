29b586ef3644e91194de8ef3f31f4ac87a5199ff
Jonas Devlieghere <jonas@devlieghere.com>
2025-07-30 09:20:37 -0700

    [lldb] Support DW_OP_WASM_location in DWARFExpression (#151010)
    Add support for DW_OP_WASM_location in DWARFExpression. This PR rebases
    #78977 and cleans up the unit test. The DWARF extensions are documented
    at https://yurydelendik.github.io/webassembly-dwarf/ and supported by
    LLVM-based toolchains such as Clang, Swift, Emscripten, and Rust.

--- lldb/source/Plugins/SymbolFile/DWARF/SymbolFileDWARF.h
+++ lldb/source/Plugins/SymbolFile/DWARF/SymbolFileDWARF.h
@@ -329,6 +329,8 @@ public:
 
   virtual bool ParseVendorDWARFOpcode(uint8_t op, const DataExtractor &opcodes,
                                       lldb::offset_t &offset,
+                                      RegisterContext *reg_ctx,
+                                      lldb::RegisterKind reg_kind,
                                       std::vector<Value> &stack) const {
     return false;
   }
@@ -556,6 +558,7 @@ protected:
   /// an index that identifies the .DWO or .o file.
   std::optional<uint64_t> m_file_index;
 };
+
 } // namespace dwarf
 } // namespace lldb_private::plugin
 
