9ef51c9aeab17c45a6e62b144ec80f462ca5366d
Krishna Pandey <kpandey81930@gmail.com>
2025-07-31 01:24:42 +0530

    feat: exhaustive tests for bfloat16 addition
    Signed-off-by: Krishna Pandey <kpandey81930@gmail.com>

--- libc/test/src/math/exhaustive/bfloat16_add_test.cpp
+++ libc/test/src/math/exhaustive/bfloat16_add_test.cpp
@@ -1,4 +1,4 @@
-//===-- Exhaustive test for hypotf16 --------------------------------------===//
+//===-- Exhaustive test for bfloat16 addition -----------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -8,16 +8,19 @@
 
 #include "exhaustive_test.h"
 #include "src/__support/FPUtil/FPBits.h"
-#include "src/__support/FPUtil/Hypot.h"
-#include "src/math/hypotf16.h"
+#include "src/__support/FPUtil/bfloat16.h"
 #include "test/UnitTest/FPMatcher.h"
+#include "utils/MPFRWrapper/MPCommon.h"
 #include "utils/MPFRWrapper/MPFRUtils.h"
 
 namespace mpfr = LIBC_NAMESPACE::testing::mpfr;
+using LIBC_NAMESPACE::fputil::BFloat16;
 
-struct Hypotf16Checker : public virtual LIBC_NAMESPACE::testing::Test {
-  using FloatType = float16;
-  using FPBits = LIBC_NAMESPACE::fputil::FPBits<float16>;
+static inline BFloat16 add_func(BFloat16 x, BFloat16 y) { return x + y; }
+
+struct Bfloat16Checker : public virtual LIBC_NAMESPACE::testing::Test {
+  using FloatType = BFloat16;
+  using FPBits = LIBC_NAMESPACE::fputil::FPBits<bfloat16>;
   using StorageType = typename FPBits::StorageType;
 
   uint64_t check(uint16_t x_start, uint16_t x_stop, uint16_t y_start,
@@ -28,18 +31,13 @@ struct Hypotf16Checker : public virtual LIBC_NAMESPACE::testing::Test {
     uint16_t xbits = x_start;
     uint64_t failed = 0;
     do {
-      float16 x = FPBits(xbits).get_val();
+      BFloat16 x = FPBits(xbits).get_val();
       uint16_t ybits = xbits;
       do {
-        float16 y = FPBits(ybits).get_val();
-        bool correct = TEST_FP_EQ(LIBC_NAMESPACE::fputil::hypot<float16>(x, y),
-                                  LIBC_NAMESPACE::hypotf16(x, y));
-        // Using MPFR will be much slower.
-        // mpfr::BinaryInput<float16> input{x, y};
-        // bool correct = TEST_MPFR_MATCH_ROUNDING_SILENTLY(
-        //  mpfr::Operation::Hypot, input, LIBC_NAMESPACE::hypotf16(x, y),
-        // 0.5,
-        //  rounding);
+        BFloat16 y = FPBits(ybits).get_val();
+        mpfr::BinaryInput<BFloat16> input{x, y};
+        bool correct = TEST_MPFR_MATCH_ROUNDING_SILENTLY(
+            mpfr::Operation::Add, input, add_func(x, y), 0.5, rounding);
         failed += (!correct);
       } while (ybits++ < y_stop);
     } while (xbits++ < x_stop);
@@ -47,21 +45,21 @@ struct Hypotf16Checker : public virtual LIBC_NAMESPACE::testing::Test {
   }
 };
 
-using LlvmLibcHypotf16ExhaustiveTest =
-    LlvmLibcExhaustiveMathTest<Hypotf16Checker, 1 << 2>;
+using LlvmLibcBfloat16ExhaustiveTest =
+    LlvmLibcExhaustiveMathTest<Bfloat16Checker, 1 << 2>;
 
-// Range of both inputs: [0, inf]
+// range: [0, inf]
 static constexpr uint16_t POS_START = 0x0000U;
-static constexpr uint16_t POS_STOP = 0x7C00U;
+static constexpr uint16_t POS_STOP = 0x7f80U;
 
-TEST_F(LlvmLibcHypotf16ExhaustiveTest, PositiveRange) {
+// range: [-0, -inf]
+static constexpr uint16_t NEG_START = 0x8000U;
+static constexpr uint16_t NEG_STOP = 0xff80U;
+
+TEST_F(LlvmLibcBfloat16ExhaustiveTest, PositiveRange) {
   test_full_range_all_roundings(POS_START, POS_STOP, POS_START, POS_STOP);
 }
 
-// Range of both inputs: [-0, -inf]
-static constexpr uint16_t NEG_START = 0x8000U;
-static constexpr uint16_t NEG_STOP = 0xFC00U;
-
-TEST_F(LlvmLibcHypotf16ExhaustiveTest, NegativeRange) {
+TEST_F(LlvmLibcBfloat16ExhaustiveTest, NegativeRange) {
   test_full_range_all_roundings(NEG_START, NEG_STOP, NEG_START, NEG_STOP);
 }
