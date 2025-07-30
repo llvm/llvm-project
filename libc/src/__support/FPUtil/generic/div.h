f8b1a497cd5f933de128723538e353e89f02d7ea
Krishna Pandey <kpandey81930@gmail.com>
2025-07-31 01:03:05 +0530

    fix: deps
    Signed-off-by: Krishna Pandey <kpandey81930@gmail.com>

--- libc/src/__support/FPUtil/generic/div.h
+++ libc/src/__support/FPUtil/generic/div.h
@@ -11,6 +11,7 @@
 
 #include "hdr/errno_macros.h"
 #include "hdr/fenv_macros.h"
+#include "src/__support/CPP/algorithm.h"
 #include "src/__support/CPP/bit.h"
 #include "src/__support/CPP/type_traits.h"
 #include "src/__support/FPUtil/BasicOperations.h"
