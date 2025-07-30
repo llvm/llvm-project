168dd13f4b9b7a69fe33b070a94ee40bf797fb3c
Tobias Gysi <tobias.gysi@nextsilicon.com>
2025-07-30 15:26:41 +0200

    Reapply "[mlir][llvm] Add intrinsic arg and result attribute support … (#151324)
    …(… (#151099)
    
    This reverts commit 2780b8f22058b35a8e70045858b87a1966df8df3 and relands
    b7bfbc0c4c7b20d6623a5b0b4a7fea8ae08a62da.
    
    Adds the following fixes compared to the original PR
    (https://github.com/llvm/llvm-project/pull/150783):
    - A bazel fix
    - Use `let methods` instead of `list<InterfaceMethod> methods`
    
    The missing forward declaration has been added in meantime:
    https://github.com/llvm/llvm-project/commit/9164d206b33d61c93f5fc4628797485f96d654ca.

--- mlir/include/mlir/Target/LLVMIR/ModuleTranslation.h
+++ mlir/include/mlir/Target/LLVMIR/ModuleTranslation.h
@@ -308,10 +308,16 @@ public:
                             /*recordInsertions=*/false);
   }
 
-  /// Translates parameter attributes of a call and adds them to the returned
-  /// AttrBuilder. Returns failure if any of the translations failed.
-  FailureOr<llvm::AttrBuilder> convertParameterAttrs(mlir::Location loc,
-                                                     DictionaryAttr paramAttrs);
+  /// Converts argument and result attributes from `attrsOp` to LLVM IR
+  /// attributes on the `call` instruction. Returns failure if conversion fails.
+  /// The `immArgPositions` parameter is only relevant for intrinsics. It
+  /// specifies the positions of immediate arguments, which do not have
+  /// associated argument attributes in MLIR and should be skipped during
+  /// attribute mapping.
+  LogicalResult
+  convertArgAndResultAttrs(ArgAndResultAttrsOpInterface attrsOp,
+                           llvm::CallBase *call,
+                           ArrayRef<unsigned> immArgPositions = {});
 
   /// Gets the named metadata in the LLVM IR module being constructed, creating
   /// it if it does not exist.
@@ -390,6 +396,11 @@ private:
   LogicalResult
   convertDialectAttributes(Operation *op,
                            ArrayRef<llvm::Instruction *> instructions);
+
+  /// Translates parameter attributes of a call and adds them to the returned
+  /// AttrBuilder. Returns failure if any of the translations failed.
+  FailureOr<llvm::AttrBuilder> convertParameterAttrs(mlir::Location loc,
+                                                     DictionaryAttr paramAttrs);
 
   /// Translates parameter attributes of a function and adds them to the
   /// returned AttrBuilder. Returns failure if any of the translations failed.
