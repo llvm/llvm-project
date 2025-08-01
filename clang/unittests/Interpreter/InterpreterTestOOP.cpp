4f1d203df9aef48d8a393321ee0f49e356812c7a
kr-2003 <kumar.kr.abhinav@gmail.com>
2025-08-01 07:43:40 +0530

    separate file for oop tests

--- clang/unittests/Interpreter/InterpreterTestOOP.cpp
+++ clang/unittests/Interpreter/InterpreterTestOOP.cpp
@@ -1,4 +1,5 @@
-//===- unittests/Interpreter/InterpreterTestOOP.cpp --- Interpreter tests when Out-of-Process ----===//
+//===- unittests/Interpreter/InterpreterTestOOP.cpp --- Interpreter tests when
+//Out-of-Process ----===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
