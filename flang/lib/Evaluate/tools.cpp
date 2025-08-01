27659b18a4e055414c7f9a92970b3470a72e912d
Krzysztof Parzyszek <Krzysztof.Parzyszek@amd.com>
2025-08-01 07:50:51 -0500

    [flang][Evaluate] OperationCode cleanup, fix for Constant<T> (#151566)
    Make the OperationCode overloads take the derived operation instead of
    the Operation base class instance. This makes them usable from visitors
    of "Expr<T>.u".
    
    Also, fix small bug: OperationCode(Constant<T>) shoud be "Constant".

--- flang/lib/Evaluate/tools.cpp
+++ flang/lib/Evaluate/tools.cpp
@@ -1693,17 +1693,17 @@ struct ArgumentExtractor
       // to int(kind=4) for example.
       return (*this)(x.template operand<0>());
     } else {
-      return std::make_pair(operation::OperationCode(x),
+      return std::make_pair(operation::OperationCode(x.derived()),
           OperationArgs(x, std::index_sequence_for<Os...>{}));
     }
   }
 
   template <typename T> Result operator()(const Designator<T> &x) const {
-    return {operation::Operator::Identity, {AsSomeExpr(x)}};
+    return {operation::OperationCode(x), {AsSomeExpr(x)}};
   }
 
   template <typename T> Result operator()(const Constant<T> &x) const {
-    return {operation::Operator::Identity, {AsSomeExpr(x)}};
+    return {operation::OperationCode(x), {AsSomeExpr(x)}};
   }
 
   template <typename... Rs>
@@ -1791,6 +1791,10 @@ std::string operation::ToString(operation::Operator op) {
     return ".TRUE.";
   }
   llvm_unreachable("Unhandler operator");
+}
+
+operation::Operator operation::OperationCode(const Relational<SomeType> &op) {
+  return common::visit([](auto &&s) { return OperationCode(s); }, op.u);
 }
 
 operation::Operator operation::OperationCode(const ProcedureDesignator &proc) {
