16215b1f398e8ecfb9ca14a4def55a454e5bca13
Aaron Ballman <aaron@aaronballman.com>
2025-08-01 12:12:52 -0400

    Update based on review feedback

--- clang/test/Analysis/engine/gh151711.cpp
+++ clang/test/Analysis/engine/gh151711.cpp
@@ -1,18 +1,18 @@
-// RUN: %clang_analyze_cc1 -analyzer-checker=core -verify %s
-// RUN: %clang_analyze_cc1 -analyzer-checker=core -verify -x c -std=c2y %s
-// expected-no-diagnostics
+// RUN: %clang_analyze_cc1 -analyzer-checker=core,debug.ExprInspection -verify %s
+// RUN: %clang_analyze_cc1 -analyzer-checker=core,debug.ExprInspection -verify -x c %s
+
+void clang_analyzer_dump(int);
 
 // Ensure that VLA types are correctly handled by unary type traits in the
 // expression engine. Previously, __datasizeof and _Countof both caused failed
 // assertions.
 void gh151711(int i) {
-  (void)sizeof(int[i++]);
-
+  clang_analyzer_dump(sizeof(int[i++]));       // expected-warning {{Unknown}}
 #ifdef __cplusplus
   // __datasizeof is only available in C++.
-  (void)__datasizeof(int[i++]);
+  clang_analyzer_dump(__datasizeof(int[i++])); // expected-warning {{Unknown}}
 #else
   // _Countof is only available in C.
-  (void)_Countof(int[i++]);
+  clang_analyzer_dump(_Countof(int[i++]));     // expected-warning {{Unknown}}
 #endif
 }
