0fa1036ee5556bd6d4d2e104a15a43fc98f08471
Rose <gfunni234@gmail.com>
2025-08-01 16:20:46 -0400

    Fix comment

--- llvm/lib/CodeGen/SelectionDAG/DAGCombiner.cpp
+++ llvm/lib/CodeGen/SelectionDAG/DAGCombiner.cpp
@@ -28973,7 +28973,7 @@ SDValue DAGCombiner::SimplifySelectCC(const SDLoc &DL, SDValue N0, SDValue N1,
                        DAG.getSExtOrTrunc(CC == ISD::SETLT ? N3 : N2, DL, VT));
   }
 
-  // Fold sign pattern select_cc setgt X, -1, 1, -1 -> (ashr X, BW-1), 1
+  // Fold sign pattern select_cc setgt X, -1, 1, -1 -> or (ashr X, BW-1), 1
   if (CC == ISD::SETGT && N1C && N2C && N3C && N1C->isAllOnes() &&
       N2C->isOne() && N3C->isAllOnes() &&
       !TLI.shouldAvoidTransformToShift(CmpOpVT,
