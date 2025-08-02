5a7b87324546fbaefd9c196417694614258f0d0d
Iris Shi <0.0@owo.li>
2025-08-02 15:14:28 +0800

    address review comments

--- clang/lib/Sema/SemaTemplateVariadic.cpp
+++ clang/lib/Sema/SemaTemplateVariadic.cpp
@@ -1387,7 +1387,7 @@ static void CheckFoldOperand(Sema &S, Expr *E) {
     S.Diag(E->getExprLoc(), diag::err_fold_expression_bad_operand)
         << E->getSourceRange()
         << FixItHint::CreateInsertion(E->getBeginLoc(), "(")
-        << FixItHint::CreateInsertion(E->getEndLoc().getLocWithOffset(1), ")");
+        << FixItHint::CreateInsertion(S.getLocForEndOfToken(E->getEndLoc()), ")");
   }
 }
 
--- clang/test/FixIt/fixit-c++17.cpp
+++ clang/test/FixIt/fixit-c++17.cpp
@@ -7,7 +7,23 @@
    apply in C++17. */
 template<int... args>
 int foo() {
-    return (args + 1 + ...); // expected-error {{expression not permitted as operand of fold expression}}
+    int a = (args + 1 + ...); // expected-error {{expression not permitted as operand of fold expression}}
+    // CHECK: fix-it:"{{.*}}":{[[@LINE-1]]:14-[[@LINE-1]]:14}:"("
+    // CHECK: fix-it:"{{.*}}":{[[@LINE-2]]:22-[[@LINE-2]]:22}:")"
+    int b = (args + 123 + ...); // expected-error {{expression not permitted as operand of fold expression}}
+    // CHECK: fix-it:"{{.*}}":{[[@LINE-1]]:14-[[@LINE-1]]:14}:"("
+    // CHECK: fix-it:"{{.*}}":{[[@LINE-2]]:24-[[@LINE-2]]:24}:")"
+    int c = (args + 1 + 2 + ...); // expected-error {{expression not permitted as operand of fold expression}}
+    // CHECK: fix-it:"{{.*}}":{[[@LINE-1]]:14-[[@LINE-1]]:14}:"("
+    // CHECK: fix-it:"{{.*}}":{[[@LINE-2]]:26-[[@LINE-2]]:26}:")"
+    int e = (... + 1 + args); // expected-error {{expression not permitted as operand of fold expression}}
+    // CHECK: fix-it:"{{.*}}":{[[@LINE-1]]:20-[[@LINE-1]]:20}:"("
+    // CHECK: fix-it:"{{.*}}":{[[@LINE-2]]:28-[[@LINE-2]]:28}:")"
+    int f = (1 + ... + args + 1); // expected-error {{expression not permitted as operand of fold expression}}
+    // CHECK: fix-it:"{{.*}}":{[[@LINE-1]]:24-[[@LINE-1]]:24}:"("
+    // CHECK: fix-it:"{{.*}}":{[[@LINE-2]]:32-[[@LINE-2]]:32}:")"
+    int g = (args + 1 + ... + 1); // expected-error {{expression not permitted as operand of fold expression}}
+    // CHECK: fix-it:"{{.*}}":{[[@LINE-1]]:14-[[@LINE-1]]:14}:"("
+    // CHECK: fix-it:"{{.*}}":{[[@LINE-2]]:22-[[@LINE-2]]:22}:")"
+    return a + b + c + e + f + g;
 }
-// CHECK: fix-it:"{{.*}}":{[[@LINE-2]]:13-[[@LINE-2]]:13}:"("
-// CHECK: fix-it:"{{.*}}":{[[@LINE-3]]:21-[[@LINE-3]]:21}:")"
