77f437527325c5fbdbe8bc617ad38e241f3ab4d1
Connector Switch <c8ef@outlook.com>
2025-08-03 22:32:00 +0800

    [NFC] Fix `assignment` typo. (#151864)

--- flang/lib/Semantics/check-omp-atomic.cpp
+++ flang/lib/Semantics/check-omp-atomic.cpp
@@ -448,7 +448,7 @@ OmpStructureChecker::CheckUpdateCapture(
     // If det != 0, then the checks unambiguously suggest a specific
     // categorization.
     // If det == 0, then this function should be called only if the
-    // checks haven't ruled out any possibility, i.e. when both assigments
+    // checks haven't ruled out any possibility, i.e. when both assignments
     // could still be either updates or captures.
     if (det > 0) {
       // as1 is update, as2 is capture
@@ -508,7 +508,7 @@ OmpStructureChecker::CheckUpdateCapture(
 
   // The remaining cases are that
   // - no candidate for update, or for capture,
-  // - one of the assigments cannot be anything.
+  // - one of the assignments cannot be anything.
 
   if (!cbu1 && !cbu2) {
     context_.Say(source,
