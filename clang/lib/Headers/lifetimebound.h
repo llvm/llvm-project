ce978901f42b3a7d34530a7f8398eb5907acad57
Egor Zhdan <e_zhdan@apple.com>
2025-08-04 12:46:04 +0100

    [Clang] Use [[clang::lifetimebound]] syntax when supported
    This allows the usage of `__lifetimebound` for the implicit self parameter, while keeping it possible to use it in C code.
    
    rdar://157316328

--- clang/lib/Headers/lifetimebound.h
+++ clang/lib/Headers/lifetimebound.h
@@ -10,10 +10,28 @@
 #ifndef __LIFETIMEBOUND_H
 #define __LIFETIMEBOUND_H
 
+#if defined(__cplusplus) && defined(__has_cpp_attribute)
+#define __use_cpp_spelling(x) __has_cpp_attribute(x)
+#else
+#define __use_cpp_spelling(x) 0
+#endif
+
+#if __use_cpp_spelling(clang::lifetimebound)
+#define __lifetimebound [[clang::lifetimebound]]
+#else
 #define __lifetimebound __attribute__((lifetimebound))
+#endif
 
+#if __use_cpp_spelling(clang::lifetime_capture_by)
+#define __lifetime_capture_by(X) [[clang::lifetime_capture_by(X)]]
+#else
 #define __lifetime_capture_by(X) __attribute__((lifetime_capture_by(X)))
+#endif
 
+#if __use_cpp_spelling(clang::noescape)
+#define __noescape [[clang::noescape]]
+#else
 #define __noescape __attribute__((noescape))
+#endif
 
 #endif /* __LIFETIMEBOUND_H */
