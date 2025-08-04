ffb7239d2ebbc9df1d5823ff0d2fe2fd257dd4b0
Michael Halkenhaeuser <MichaelGerald.Halkenhauser@amd.com>
2025-08-04 11:20:46 -0500

    Revert "[SLP] Fix a check for main/alternate interchanged instruction"
    This reverts commit 3ee8d047109ea4bb479095f4b153c2120a8d726c.
    
    Revert reason: FAILED build for openmp-offload-amdgpu-runtime-2
    https://lab.llvm.org/buildbot/#/builders/10/builds/10827

--- llvm/lib/Transforms/Vectorize/SLPVectorizer.cpp
+++ llvm/lib/Transforms/Vectorize/SLPVectorizer.cpp
@@ -1238,12 +1238,6 @@ public:
     BinOpSameOpcodeHelper Converter(MainOp);
     if (!Converter.add(I) || !Converter.add(MainOp))
       return nullptr;
-    if (!Converter.hasCandidateOpcode(MainOp->getOpcode()) && isAltShuffle()) {
-      BinOpSameOpcodeHelper AltConverter(AltOp);
-      if (AltConverter.add(I) && AltConverter.add(AltOp) &&
-          AltConverter.hasCandidateOpcode(AltOp->getOpcode()))
-        return AltOp;
-    }
     if (Converter.hasAltOp() && !isAltShuffle())
       return nullptr;
     return Converter.hasAltOp() ? AltOp : MainOp;
