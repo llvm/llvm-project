e0df5f8c1abda78eb294b43bb8bf5c91ca3268a8
Corentin Jabot <corentinjabot@gmail.com>
2025-08-05 11:02:04 +0200

    [libc++][NFC] Remove comments incorrectly added in #151935.

--- libcxx/test/libcxx/utilities/expected/expected.expected/value.observers.verify.cpp
+++ libcxx/test/libcxx/utilities/expected/expected.expected/value.observers.verify.cpp
@@ -95,7 +95,6 @@ void test() {
       std::expected<int, CopyConstructibleButNotMoveConstructible> e;
       [[maybe_unused]] auto val = std::move(e).value();
       // expected-error-re@*:* {{static assertion failed {{.*}}error_type has to be both copy constructible and constructible from decltype(std::move(error()))}}
-      // The following diagnostic is emmitted in expected.h:
     }
   }
 
@@ -119,7 +118,6 @@ void test() {
       const std::expected<int, CopyConstructibleButNotMoveConstructible> e;
       [[maybe_unused]] auto val = std::move(e).value();
       // expected-error-re@*:* {{static assertion failed {{.*}}error_type has to be both copy constructible and constructible from decltype(std::move(error()))}}
-      // The following diagnostic is emmitted in expected.h:
     }
   }
 // These diagnostics happen when we try to construct bad_expected_access from the non copy-constructible error type.
