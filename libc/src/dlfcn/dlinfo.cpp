6f1f9a2118e6aaefff3b40db709410035d697155
Caslyn Tonelli <6718161+Caslyn@users.noreply.github.com>
2025-08-05 13:34:30 -0700

    [libc] Add dlinfo + RTLD_DI_* macros to dlfcn.h (#149938)
    An initial commit for #149911, this adds a stub implementation for
    dlinfo and the enums list of `RTLD_DI_*` values.
    
    While the dlinfo implementation relies on dynamic linker support, this
    patch will add its prototype in the generated dlfcn.h header so that it
    can be used by downstream platforms that have their own dlinfo
    implementation.

---
+++ libc/src/dlfcn/dlinfo.cpp
@@ -0,0 +1,23 @@
+
+//===-- Implementation of dlinfo ------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "dlinfo.h"
+
+#include "src/__support/common.h"
+#include "src/__support/macros/config.h"
+
+namespace LIBC_NAMESPACE_DECL {
+
+// TODO: https://github.com/llvm/llvm-project/issues/149911
+LLVM_LIBC_FUNCTION(int, dlinfo,
+                   (void *restrict handle, int request, void *restrict info)) {
+  return -1;
+}
+
+} // namespace LIBC_NAMESPACE_DECL
