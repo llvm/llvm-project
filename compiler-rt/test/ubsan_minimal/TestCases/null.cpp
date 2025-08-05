e8e84a77d8e297b6cb7e72aac714074332967846
Andrew Lazarev <alazarev@google.com>
2025-08-05 21:01:42 +0000

    clang-format

--- compiler-rt/test/ubsan_minimal/TestCases/null.cpp
+++ compiler-rt/test/ubsan_minimal/TestCases/null.cpp
@@ -1,11 +1,11 @@
 // RUN: %clang_min_runtime -fsanitize=null %s -o %t && %run %t 2>&1 | FileCheck %s --check-prefixes=CHECK
 
-void f(int& n) {}
+void f(int &n) {}
 
-int* t;
+int *t;
 
 int main() {
-    // CHECK: ubsan: type-mismatch by 0x{{[[:xdigit:]]+}} address 0x{{[[:xdigit:]]+$}}
-    // CHECK-NOT: type-mismatch
-    f(*t);
+  // CHECK: ubsan: type-mismatch by 0x{{[[:xdigit:]]+}} address 0x{{[[:xdigit:]]+$}}
+  // CHECK-NOT: type-mismatch
+  f(*t);
 }
