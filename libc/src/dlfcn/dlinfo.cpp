c09b543441b9a97df8269332435229609d10d390
Caslyn Tonelli <caslyn@google.com>
2025-08-04 17:17:21 -0700

    use parameter names in the definition

--- libc/src/dlfcn/dlinfo.cpp
+++ libc/src/dlfcn/dlinfo.cpp
@@ -15,7 +15,8 @@
 namespace LIBC_NAMESPACE_DECL {
 
 // TODO: https://github.com/llvm/llvm-project/issues/149911
-LLVM_LIBC_FUNCTION(int, dlinfo, (void *restrict, int, void *restrict)) {
+LLVM_LIBC_FUNCTION(int, dlinfo,
+                   (void *restrict handle, int request, void *restrict info)) {
   return -1;
 }
 
