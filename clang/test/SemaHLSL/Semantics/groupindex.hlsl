d90b78fb28be3c444fcc0d8113350264759080c4
Nathan Gauër <brioche@google.com>
2025-08-06 18:16:58 +0200

    [HLSL] Rewrite semantics parsing
    This is the first PR to implement the semantics proposal:
    https://github.com/llvm/wg-hlsl/blob/main/proposals/0031-semantics.md
    
    This PR focuses on the changes required to handle user semantics, but
    tried to be almost NFC.
    What changes is the error messages as the semantics case is not kept
    when reporting error messages.
    
    You might notice the SV_GroupIndex semantic is not properly validated as
    are others. This is an existing behavior that we'll need to fix, but
    wanted to keep this separated from this rewrite to stay as-close as an
    NFC as possible.
    
    The next PR will add support on the different kinds of I/O we can have
    using semantics (input, inout param, structs).

--- clang/test/SemaHLSL/Semantics/groupindex.hlsl
+++ clang/test/SemaHLSL/Semantics/groupindex.hlsl
@@ -4,26 +4,26 @@
 [shader("compute")][numthreads(32,1,1)]
 void compute(int GI : SV_GroupIndex) {}
 
-// expected-error@+2 {{attribute 'SV_GroupIndex' is unsupported in 'pixel' shaders}}
+// expected-error@+2 {{attribute 'SV_GROUPINDEX' is unsupported in 'pixel' shaders}}
 [shader("pixel")]
 void pixel(int GI : SV_GroupIndex) {}
 
-// expected-error@+2 {{attribute 'SV_GroupIndex' is unsupported in 'vertex' shaders}}
+// expected-error@+2 {{attribute 'SV_GROUPINDEX' is unsupported in 'vertex' shaders}}
 [shader("vertex")]
 void vertex(int GI : SV_GroupIndex) {}
 
-// expected-error@+2 {{attribute 'SV_GroupIndex' is unsupported in 'geometry' shaders}}
+// expected-error@+2 {{attribute 'SV_GROUPINDEX' is unsupported in 'geometry' shaders}}
 [shader("geometry")]
 void geometry(int GI : SV_GroupIndex) {}
 
-// expected-error@+2 {{attribute 'SV_GroupIndex' is unsupported in 'domain' shaders}}
+// expected-error@+2 {{attribute 'SV_GROUPINDEX' is unsupported in 'domain' shaders}}
 [shader("domain")]
 void domain(int GI : SV_GroupIndex) {}
 
-// expected-error@+2 {{attribute 'SV_GroupIndex' is unsupported in 'amplification' shaders}}
+// expected-error@+2 {{attribute 'SV_GROUPINDEX' is unsupported in 'amplification' shaders}}
 [shader("amplification")][numthreads(32,1,1)]
 void amplification(int GI : SV_GroupIndex) {}
 
-// expected-error@+2 {{attribute 'SV_GroupIndex' is unsupported in 'mesh' shaders}}
+// expected-error@+2 {{attribute 'SV_GROUPINDEX' is unsupported in 'mesh' shaders}}
 [shader("mesh")][numthreads(32,1,1)]
 void mesh(int GI : SV_GroupIndex) {}
