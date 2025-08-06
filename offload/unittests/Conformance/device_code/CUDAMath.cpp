2f7887c12f4901dbc9a00fafaaff28947f3f00fc
Leandro Augusto Lacerda Campos <leandrolcampos@yahoo.com.br>
2025-08-06 15:26:09 -0300

    Add support for `cuda-math` and `hip-math` providers

--- offload/unittests/Conformance/device_code/CUDAMath.cpp
+++ offload/unittests/Conformance/device_code/CUDAMath.cpp
@@ -8,15 +8,16 @@
 ///
 /// \file
 /// This file contains the implementation of the device kernels that wrap the
-/// math functions from the llvm-libm provider.
+/// math functions from the cuda-math provider.
 ///
 //===----------------------------------------------------------------------===//
+
+#ifdef CUDA_MATH_FOUND
 
 #include "Conformance/device_code/DeviceAPIs.hpp"
 #include "Conformance/device_code/KernelRunner.hpp"
 
 #include <gpuintrin.h>
-#include <math.h>
 #include <stddef.h>
 
 using namespace kernels;
@@ -27,13 +28,13 @@ using namespace kernels;
 
 static inline float sincosfSin(float X) {
   float SinX, CosX;
-  sincosf(X, &SinX, &CosX);
+  __nv_sincosf(X, &SinX, &CosX);
   return SinX;
 }
 
 static inline float sincosfCos(float X) {
   float SinX, CosX;
-  sincosf(X, &SinX, &CosX);
+  __nv_sincosf(X, &SinX, &CosX);
   return CosX;
 }
 
@@ -45,107 +46,102 @@ extern "C" {
 
 __gpu_kernel void acosfKernel(const float *X, float *Out,
                               size_t NumElements) noexcept {
-  runKernelBody<acosf>(NumElements, Out, X);
+  runKernelBody<__nv_acosf>(NumElements, Out, X);
 }
 
 __gpu_kernel void acoshfKernel(const float *X, float *Out,
                                size_t NumElements) noexcept {
-  runKernelBody<acoshf>(NumElements, Out, X);
+  runKernelBody<__nv_acoshf>(NumElements, Out, X);
 }
 
 __gpu_kernel void asinfKernel(const float *X, float *Out,
                               size_t NumElements) noexcept {
-  runKernelBody<asinf>(NumElements, Out, X);
+  runKernelBody<__nv_asinf>(NumElements, Out, X);
 }
 
 __gpu_kernel void asinhfKernel(const float *X, float *Out,
                                size_t NumElements) noexcept {
-  runKernelBody<asinhf>(NumElements, Out, X);
+  runKernelBody<__nv_asinhf>(NumElements, Out, X);
 }
 
 __gpu_kernel void atanfKernel(const float *X, float *Out,
                               size_t NumElements) noexcept {
-  runKernelBody<atanf>(NumElements, Out, X);
+  runKernelBody<__nv_atanf>(NumElements, Out, X);
 }
 
 __gpu_kernel void atanhfKernel(const float *X, float *Out,
                                size_t NumElements) noexcept {
-  runKernelBody<atanhf>(NumElements, Out, X);
+  runKernelBody<__nv_atanhf>(NumElements, Out, X);
 }
 
 __gpu_kernel void cbrtfKernel(const float *X, float *Out,
                               size_t NumElements) noexcept {
-  runKernelBody<cbrtf>(NumElements, Out, X);
+  runKernelBody<__nv_cbrtf>(NumElements, Out, X);
 }
 
 __gpu_kernel void cosfKernel(const float *X, float *Out,
                              size_t NumElements) noexcept {
-  runKernelBody<cosf>(NumElements, Out, X);
+  runKernelBody<__nv_cosf>(NumElements, Out, X);
 }
 
 __gpu_kernel void coshfKernel(const float *X, float *Out,
                               size_t NumElements) noexcept {
-  runKernelBody<coshf>(NumElements, Out, X);
+  runKernelBody<__nv_coshf>(NumElements, Out, X);
 }
 
 __gpu_kernel void cospifKernel(const float *X, float *Out,
                                size_t NumElements) noexcept {
-  runKernelBody<cospif>(NumElements, Out, X);
+  runKernelBody<__nv_cospif>(NumElements, Out, X);
 }
 
 __gpu_kernel void erffKernel(const float *X, float *Out,
                              size_t NumElements) noexcept {
-  runKernelBody<erff>(NumElements, Out, X);
+  runKernelBody<__nv_erff>(NumElements, Out, X);
 }
 
 __gpu_kernel void expfKernel(const float *X, float *Out,
                              size_t NumElements) noexcept {
-  runKernelBody<expf>(NumElements, Out, X);
+  runKernelBody<__nv_expf>(NumElements, Out, X);
 }
 
 __gpu_kernel void exp10fKernel(const float *X, float *Out,
                                size_t NumElements) noexcept {
-  runKernelBody<exp10f>(NumElements, Out, X);
+  runKernelBody<__nv_exp10f>(NumElements, Out, X);
 }
 
 __gpu_kernel void exp2fKernel(const float *X, float *Out,
                               size_t NumElements) noexcept {
-  runKernelBody<exp2f>(NumElements, Out, X);
+  runKernelBody<__nv_exp2f>(NumElements, Out, X);
 }
 
 __gpu_kernel void expm1fKernel(const float *X, float *Out,
                                size_t NumElements) noexcept {
-  runKernelBody<expm1f>(NumElements, Out, X);
-}
-
-__gpu_kernel void hypotf16Kernel(const float16 *X, float16 *Y, float16 *Out,
-                                 size_t NumElements) noexcept {
-  runKernelBody<hypotf16>(NumElements, Out, X, Y);
+  runKernelBody<__nv_expm1f>(NumElements, Out, X);
 }
 
 __gpu_kernel void logfKernel(const float *X, float *Out,
                              size_t NumElements) noexcept {
-  runKernelBody<logf>(NumElements, Out, X);
+  runKernelBody<__nv_logf>(NumElements, Out, X);
 }
 
 __gpu_kernel void log10fKernel(const float *X, float *Out,
                                size_t NumElements) noexcept {
-  runKernelBody<log10f>(NumElements, Out, X);
+  runKernelBody<__nv_log10f>(NumElements, Out, X);
 }
 
 __gpu_kernel void log1pfKernel(const float *X, float *Out,
                                size_t NumElements) noexcept {
-  runKernelBody<log1pf>(NumElements, Out, X);
+  runKernelBody<__nv_log1pf>(NumElements, Out, X);
 }
 
 __gpu_kernel void log2fKernel(const float *X, float *Out,
                               size_t NumElements) noexcept {
-  runKernelBody<log2f>(NumElements, Out, X);
+  runKernelBody<__nv_log2f>(NumElements, Out, X);
 }
 
 __gpu_kernel void sinfKernel(const float *X, float *Out,
                              size_t NumElements) noexcept {
-  runKernelBody<sinf>(NumElements, Out, X);
+  runKernelBody<__nv_sinf>(NumElements, Out, X);
 }
 
 __gpu_kernel void sincosfSinKernel(const float *X, float *Out,
@@ -160,26 +156,23 @@ __gpu_kernel void sincosfCosKernel(const float *X, float *Out,
 
 __gpu_kernel void sinhfKernel(const float *X, float *Out,
                               size_t NumElements) noexcept {
-  runKernelBody<sinhf>(NumElements, Out, X);
+  runKernelBody<__nv_sinhf>(NumElements, Out, X);
 }
 
 __gpu_kernel void sinpifKernel(const float *X, float *Out,
                                size_t NumElements) noexcept {
-  runKernelBody<sinpif>(NumElements, Out, X);
+  runKernelBody<__nv_sinpif>(NumElements, Out, X);
 }
 
 __gpu_kernel void tanfKernel(const float *X, float *Out,
                              size_t NumElements) noexcept {
-  runKernelBody<tanf>(NumElements, Out, X);
+  runKernelBody<__nv_tanf>(NumElements, Out, X);
 }
 
 __gpu_kernel void tanhfKernel(const float *X, float *Out,
                               size_t NumElements) noexcept {
-  runKernelBody<tanhf>(NumElements, Out, X);
-}
-
-__gpu_kernel void tanpifKernel(const float *X, float *Out,
-                               size_t NumElements) noexcept {
-  runKernelBody<tanpif>(NumElements, Out, X);
+  runKernelBody<__nv_tanhf>(NumElements, Out, X);
 }
 } // extern "C"
+
+#endif // CUDA_MATH_FOUND
