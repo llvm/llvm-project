339374a4c17bffe0ece738ffab6a8218648e88d9
Tom Vijlbrief <tvijlbrief@gmail.com>
2025-08-06 13:41:51 +0200

    fix 80 column

--- llvm/lib/Target/AVR/AVRTargetTransformInfo.h
+++ llvm/lib/Target/AVR/AVRTargetTransformInfo.h
@@ -1,4 +1,4 @@
-//===- AVRTargetTransformInfo.h - AVR specific TTI ---------*- C++ -*-===//
+//===- AVRTargetTransformInfo.h - AVR specific TTI --------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
