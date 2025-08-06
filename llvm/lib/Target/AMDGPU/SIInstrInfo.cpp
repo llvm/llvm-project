edc26d49ab64b9b268e5f8f8bf09b5a0ad909bbc
Changpeng Fang <changpeng.fang@amd.com>
2025-08-06 09:42:05 -0700

    Update llvm/lib/Target/AMDGPU/SIInstrInfo.cpp
    Co-authored-by: Matt Arsenault <arsenm2@gmail.com>

--- llvm/lib/Target/AMDGPU/SIInstrInfo.cpp
+++ llvm/lib/Target/AMDGPU/SIInstrInfo.cpp
@@ -6122,7 +6122,7 @@ bool SIInstrInfo::isOperandLegal(const MachineInstr &MI, unsigned OpIdx,
 
       // Do not fold a non-inlineable and non-register operand into an
       // instruction that already has a frame index. The frame index handling
-      // code could not handle well when a fame index co-exists with another
+      // code could not handle well when a frame index co-exists with another
       // non-register operand, unless that operand is an inlineable immediate.
       if (Op.isFI())
         return false;
