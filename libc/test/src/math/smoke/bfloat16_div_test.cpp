ae2b7fcffddab7bb77a0c263f8b237052f3758e5
Krishna Pandey <kpandey81930@gmail.com>
2025-08-06 19:40:22 +0530

    fix: typo

--- libc/test/src/math/smoke/bfloat16_div_test.cpp
+++ libc/test/src/math/smoke/bfloat16_div_test.cpp
@@ -1,4 +1,4 @@
-//===-- Unittests for bfloat16 addition -----------------------------------===//
+//===-- Unittests for bfloat16 division -----------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
