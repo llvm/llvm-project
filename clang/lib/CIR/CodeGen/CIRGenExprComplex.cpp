47e3caaaa57efbb82c7ae8229fced95abc638b7a
Amr Hesham <amr96@programmer.net>
2025-08-07 19:42:15 +0200

    [CIR] Mul CompoundAssignment support for ComplexType (#152354)
    This change adds support for Mul CompoundAssignment for ComplexType
    
    https://github.com/llvm/llvm-project/issues/141365

--- clang/lib/CIR/CodeGen/CIRGenExprComplex.cpp
+++ clang/lib/CIR/CodeGen/CIRGenExprComplex.cpp
@@ -171,6 +171,10 @@ public:
   mlir::Value VisitBinSubAssign(const CompoundAssignOperator *e) {
     return emitCompoundAssign(e, &ComplexExprEmitter::emitBinSub);
   }
+
+  mlir::Value VisitBinMulAssign(const CompoundAssignOperator *e) {
+    return emitCompoundAssign(e, &ComplexExprEmitter::emitBinMul);
+  }
 };
 } // namespace
 
@@ -813,7 +817,7 @@ using CompoundFunc =
 static CompoundFunc getComplexOp(BinaryOperatorKind op) {
   switch (op) {
   case BO_MulAssign:
-    llvm_unreachable("getComplexOp: BO_MulAssign");
+    return &ComplexExprEmitter::emitBinMul;
   case BO_DivAssign:
     llvm_unreachable("getComplexOp: BO_DivAssign");
   case BO_SubAssign:
