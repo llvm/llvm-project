af9740d389201e52a36f6ba1af53ca35b592cbf2
A. Jiang <de34@live.cn>
2025-08-07 14:47:37 +0800

    [libc++] LWG3187: P0591R4 reverted DR 2586 fixes to ...
    ... `scoped_allocator_adaptor::construct()`
    
    Applying it (adding missing `const``) to pre-C++20 dispatching
    mechanisms.
    
    Also
    - make dispatch mechanisms in pre-C++20 `construct` more consistent, and
    - add fallback dispatching and overloads to emit better error messages.

--- libcxx/test/std/utilities/allocator.adaptor/allocator.adaptor.members/construct_pair_const_lvalue_pair.pass.cpp
+++ libcxx/test/std/utilities/allocator.adaptor/allocator.adaptor.members/construct_pair_const_lvalue_pair.pass.cpp
@@ -48,15 +48,9 @@ void test_no_inner_alloc() {
     A.construct(ptr, in);
     assert(checkConstruct<int&>(ptr->first, UA_AllocArg, CA));
     assert(checkConstruct<int const&>(ptr->second, UA_AllocLast, CA));
-#if TEST_STD_VER >= 20
-    assert((P.checkConstruct<std::piecewise_construct_t&&,
+    assert((P.checkConstruct<piecewise_construct_ref_t,
                              std::tuple<std::allocator_arg_t, const SA&, int&>&&,
                              std::tuple<int const&, const SA&>&& >(CA, ptr)));
-#else
-    assert((P.checkConstruct<std::piecewise_construct_t const&,
-                             std::tuple<std::allocator_arg_t, SA&, int&>&&,
-                             std::tuple<int const&, SA&>&& >(CA, ptr)));
-#endif
     A.destroy(ptr);
     std::free(ptr);
   }
@@ -79,15 +73,9 @@ void test_no_inner_alloc() {
     A.construct(ptr, in);
     assert(checkConstruct<int const&>(ptr->first, UA_AllocArg, CA));
     assert(checkConstruct<int const&>(ptr->second, UA_None));
-#if TEST_STD_VER >= 20
-    assert((P.checkConstruct<std::piecewise_construct_t&&,
+    assert((P.checkConstruct<piecewise_construct_ref_t,
                              std::tuple<std::allocator_arg_t, const SA&, int const&>&&,
                              std::tuple<int const&>&& >(CA, ptr)));
-#else
-    assert((P.checkConstruct<std::piecewise_construct_t const&,
-                             std::tuple<std::allocator_arg_t, SA&, int const&>&&,
-                             std::tuple<int const&>&& >(CA, ptr)));
-#endif
     A.destroy(ptr);
     std::free(ptr);
   }
@@ -120,15 +108,9 @@ void test_with_inner_alloc() {
     A.construct(ptr, in);
     assert(checkConstruct<int&>(ptr->first, UA_AllocArg, I));
     assert(checkConstruct<int const&>(ptr->second, UA_AllocLast));
-#if TEST_STD_VER >= 20
-    assert((POuter.checkConstruct<std::piecewise_construct_t&&,
+    assert((POuter.checkConstruct<piecewise_construct_ref_t,
                                   std::tuple<std::allocator_arg_t, const SAInner&, int&>&&,
                                   std::tuple<int const&, const SAInner&>&& >(O, ptr)));
-#else
-    assert((POuter.checkConstruct<std::piecewise_construct_t const&,
-                                  std::tuple<std::allocator_arg_t, SAInner&, int&>&&,
-                                  std::tuple<int const&, SAInner&>&& >(O, ptr)));
-#endif
     A.destroy(ptr);
     std::free(ptr);
   }
@@ -156,15 +138,9 @@ void test_with_inner_alloc() {
     A.construct(ptr, in);
     assert(checkConstruct<int const&>(ptr->first, UA_AllocArg, I));
     assert(checkConstruct<int const&>(ptr->second, UA_None));
-#if TEST_STD_VER >= 20
-    assert((POuter.checkConstruct<std::piecewise_construct_t&&,
+    assert((POuter.checkConstruct<piecewise_construct_ref_t,
                                   std::tuple<std::allocator_arg_t, const SAInner&, int const&>&&,
                                   std::tuple<int const&>&& >(O, ptr)));
-#else
-    assert((POuter.checkConstruct<std::piecewise_construct_t const&,
-                                  std::tuple<std::allocator_arg_t, SAInner&, int const&>&&,
-                                  std::tuple<int const&>&& >(O, ptr)));
-#endif
     A.destroy(ptr);
     std::free(ptr);
   }
