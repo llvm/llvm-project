cb3baf97308c66a33023b690b68201010b3d2391
David Spickett <david.spickett@linaro.org>
2025-08-07 10:08:08 +0000

    [llvm][MC][test] Disable many-instructons.s on 32-bit systems
    Added by https://github.com/llvm/llvm-project/pull/150846.
    
    Checks the size of a structure, which is only correct for 64-bit
    systems.

--- llvm/test/MC/ELF/many-instructions.s
+++ llvm/test/MC/ELF/many-instructions.s
@@ -1,4 +1,5 @@
-# REQUIRES: asserts
+## Checks the size of an internal MC structure that is different on 32-bit.
+# REQUIRES: asserts, llvm-64-bits
 # RUN: llvm-mc -filetype=obj -triple=x86_64 %s -o /dev/null -debug-only=mc-dump 2>&1 | grep -E -o '[0-9]+ Data Size:[0-9]+' | FileCheck %s
 
 ## Test that encodeInstruction may cause a new fragment to be created.
