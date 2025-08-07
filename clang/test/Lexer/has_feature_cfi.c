64556aa4cef0fce6345aa543652326b2a240a4c7
moorabbit <moorabbit@proton.me>
2025-08-07 07:11:31 -0400

    Specify the target platform the test is allowed to run on

--- clang/test/Lexer/has_feature_cfi.c
+++ clang/test/Lexer/has_feature_cfi.c
@@ -1,4 +1,4 @@
-// REQUIRES: system-linux
+// REQUIRES: target={{x86_64.*-linux.*}}
 
 // RUN: %clang -E -fvisibility=hidden -flto -fno-sanitize-ignorelist -fsanitize=cfi -c %s -o - | FileCheck %s --check-prefix=CHECK-CFI
 // RUN: %clang -E -fvisibility=hidden -flto -fno-sanitize-ignorelist -fsanitize=cfi -fsanitize-cfi-cross-dso -c %s -o - | FileCheck %s --check-prefix=CHECK-CFI
