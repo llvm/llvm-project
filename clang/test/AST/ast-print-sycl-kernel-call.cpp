919cd88a003bea179c89cf45a3f6c17e276bccbe
Tom Honermann <tom.honermann@intel.com>
2025-08-06 19:26:54 -0700

    [SYCL] SYCL host kernel launch support for the sycl_kernel_entry_point attribute.
    The `sycl_kernel_entry_point` attribute facilitates the generation of an
    offload kernel entry point function with parameters corresponding to the
    (potentially decomposed) kernel arguments and a body that (potentially
    reconstructs the arguments and) executes the kernel. This change adds
    symmetric support for the SYCL host through an interface that provides
    symbol names and (potentially decomposed) kernel arguments to the SYCL
    library.
    
    Consider the following function declared with the `sycl_kernel_entry_point`
    attribute with a call to this function occurring in the implementation of
    a SYCL kernel invocation function such as `sycl::handler::single_task()`.
      template<typename KernelNameType, typename KernelType>
      [[clang::sycl_kernel_entry_point(KernelNameType)]]
      void kernel_entry_point(KernelType kerne) {
        kernel();
      }
    
    The body of the above function specifies the parameters and body of the
    generated offload kernel entry point. Clearly, a call to the above function
    by a SYCL kernel invocation function is not intended to execute the body
    as written. Previously, code generation emitted an empty function body so
    that calls to the function had no effect other than to trigger the generation
    of the offload kernel entry point. The function body is therefore available
    to hook for SYCL library support and is now substituted with a call to a
    (SYCL library provided) function template named `sycl_enqueue_kernel_launch()`
    with the kernel name type passed as the first template argument, the
    symbol name of the offload kernel entry point passed as a string literal for
    the first function argument, and the (possibly decomposed) parameters passed
    as the remaining explicit function arguments. Given a call like this:
      kernel_entry_point<struct KN>([]{})
    the body of the instantiated `kernel_entry_point()` specialization would be
    substituted as follows with "kernel-symbol-name" substituted for the
    generated symbol name and `kernel` forwarded (This assumes no kernel
    argument decomposition; if decomposition was required, `kernel` would be
    replaced with its corresponding decomposed arguments).
      sycl_enqueue_kernel_launch<KN>("kernel-symbol-name", kernel)
    
    Name lookup and overload resolution for the `sycl_enqueue_kernel_launch()`
    function is performed at the point of definition of the
    `sycl_kernel_entry_point` attributed function (or the point of instantiation
    for an instantiated function template specialization). If overload
    resolution fails, the program is ill-formed.
    
    Implementation of the `sycl_enqueue_kernel_launch()` function might require
    additional information provided by the SYCL library. This is facilitated by
    removing the previous prohibition against use of the `sycl_kernel_entry_point`
    attribute with a non-static member function. If the `sycl_kernel_entry_point`
    attributed function is a non-static member function, then overload resolution
    for the `sycl_enqueue_kernel_launch()` function template may select a
    non-static member function in which case, `this` will be implicitly passed
    as the implicit object argument.
    
    If a `sycl_kernel_entry_point` attributed function is a non-static member
    function, use of `this` in a potentially evaluated expression is prohibited
    in the definition (since `this` is not a kernel argument and will not be
    available within the generated offload kernel entry point function).
    
    Support for kernel argument decomposition and reconstruction is not yet
    implemented.

---
+++ clang/test/AST/ast-print-sycl-kernel-call.cpp
@@ -0,0 +1,22 @@
+// RUN: %clang_cc1 -fsycl-is-host -ast-print %s -o - | FileCheck %s
+// RUN: %clang_cc1 -fsycl-is-device -ast-print %s -o - | FileCheck %s
+
+struct sycl_kernel_launcher {
+  template<typename KernelName, typename... Ts>
+  void sycl_enqueue_kernel_launch(const char *, Ts...) {}
+
+  template<typename KernelName, typename KernelType>
+  void kernel_entry_point(KernelType kernel) {
+    kernel();
+  }
+// CHECK:      template <typename KernelName, typename KernelType> void kernel_entry_point(KernelType kernel) {
+// CHECK-NEXT:     kernel();
+// CHECK-NEXT: }
+// CHECK:      template<> void kernel_entry_point<KN, (lambda at {{.*}})>((lambda at {{.*}}) kernel) {
+// CHECK-NEXT:     kernel();
+// CHECK-NEXT: }
+};
+
+void f(sycl_kernel_launcher skl) {
+  skl.kernel_entry_point<struct KN>([]{});
+}
