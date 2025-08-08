4fc8ccffca66b7801bb11a12c9e0ebdc2d70be2a
Krishna Pandey <kpandey81930@gmail.com>
2025-08-09 01:39:05 +0530

    feat: implement bf16mul{,f,l,f128} math function
    Signed-off-by: Krishna Pandey <kpandey81930@gmail.com>

---
+++ libc/src/math/generic/bf16mull.cpp
@@ -0,0 +1,22 @@
+//===-- Implementation of bf16mull function -------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "src/math/bf16mull.h"
+
+#include "src/__support/common.h"
+#include "src/__support/FPUtil/bfloat16.h"
+#include "src/__support/FPUtil/generic/mul.h"
+#include "src/__support/macros/config.h"
+
+namespace LIBC_NAMESPACE_DECL {
+
+LLVM_LIBC_FUNCTION(bfloat16, bf16mull, (long double x, long double y)) {
+  return fputil::generic::mul<bfloat16>(x, y);
+}
+
+} // namespace LIBC_NAMESPACE_DECL
