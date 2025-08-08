93905f77f00133332c746a01a7f628cb335c001d
Muhammad Bassiouni <60100307+bassiounix@users.noreply.github.com>
2025-08-08 18:28:50 +0300

    [libc][math] Refactor cbrtf implementation to header-only in src/__support/math folder. (#151846)
    Part of #147386
    
    in preparation for: https://discourse.llvm.org/t/rfc-make-clang-builtin-math-functions-constexpr-with-llvm-libc-to-support-c-23-constexpr-math-functions/86450

--- libc/shared/math/cbrtf.h
+++ libc/shared/math/cbrtf.h
@@ -1,4 +1,4 @@
-//===-- Implementation of cbrtf function ----------------------------------===//
+//===-- Shared cbrtf function -----------------------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,11 +6,18 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "src/math/cbrtf.h"
+#ifndef LIBC_SHARED_MATH_CBRTF_H
+#define LIBC_SHARED_MATH_CBRTF_H
+
+#include "shared/libc_common.h"
 #include "src/__support/math/cbrtf.h"
 
 namespace LIBC_NAMESPACE_DECL {
+namespace shared {
 
-LLVM_LIBC_FUNCTION(float, cbrtf, (float x)) { return math::cbrtf(x); }
+using math::cbrtf;
 
+} // namespace shared
 } // namespace LIBC_NAMESPACE_DECL
+
+#endif // LIBC_SHARED_MATH_CBRTF_H
