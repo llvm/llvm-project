cb2f1ce68e30239dbc4a4e5b0aeaa356678065af
lakshayk-nv <lakshayk@nvidia.com>
2025-08-08 03:57:48 -0700

    [llvm-exegesis] Add test for whole assembly debug

--- llvm/test/tools/llvm-exegesis/AArch64/debug-gen-asm.s
+++ llvm/test/tools/llvm-exegesis/AArch64/debug-gen-asm.s
@@ -10,3 +10,12 @@ PREVIEW:      ...      ({{[0-9]+}} more instructions)
 PREVIEW-NEXT: {{.*}}   addv    h{{[0-9]+}}, v{{[0-9]+}}.4h
 PREVIEW:      {{.*}}   ret
 PREVIEW-NEXT:```
+
+RUN: llvm-exegesis -mcpu=neoverse-v2 --benchmark-phase=measure --min-instructions=100 --mode=latency --debug-only=print-gen-assembly  --opcode-name=ADDVv4i16v 2>&1 | FileCheck %s -check-prefix=PRINT
+PRINT: Generated assembly snippet:
+PRINT-NEXT: ```
+PRINT:      {{[04]}}: {{.*}}      movi    d{{[0-9]+}}, #0000000000000000
+PRINT-NEXT: {{[48]}}: {{.*}}      addv    h{{[0-9]+}}, v{{[0-9]+}}.4h
+PRINT-NEXT: {{.*}}   addv    h{{[0-9]+}}, v{{[0-9]+}}.4h
+PRINT:      {{.*}}   ret
+PRINT-NEXT:```
