a169b80e737542977e361ff31a1096776dc64711
Fangrui Song <i@maskray.me>
2025-08-07 19:13:43 -0700

    test
    Created using spr 1.3.5-bogner

--- llvm/test/MC/RISCV/Relocations/align-after-relax.s
+++ llvm/test/MC/RISCV/Relocations/align-after-relax.s
@@ -4,18 +4,22 @@
 # RUN: llvm-mc -filetype=obj -triple=riscv64 -mattr=+c,+relax %s -o %t0
 # RUN: llvm-objdump -dr --no-show-raw-insn -M no-aliases %t0 | FileCheck %s --check-prefix=CHECK0
 
-# CHECK:            6: c.nop
+# CHECK:            4: 00 00 01 00 .word 0x00010000
 # CHECK-EMPTY:
+# CHECK:            8: 78 56 34 12 .word 0x12345678
+# CHECK-NEXT:       c: 00 00 00 00 .word 0x00000000
 # CHECK:           10: auipc   ra, 0x0
 # CHECK-NEXT:                R_RISCV_CALL_PLT     foo
 # CHECK-NEXT:                R_RISCV_RELAX        *ABS*
-# CHECK:           20: c.nop
+# CHECK:           18: c.nop
 # CHECK-NEXT:                R_RISCV_ALIGN        *ABS*+0x6
 
 ## Alignment directives in a lower-numbered subsection may be conservatively treated as linker-relaxable.
-# CHECK0:           6: c.nop
-# CHECK0-NEXT:               R_RISCV_ALIGN        *ABS*+0x6
-# CHECK0:          20: c.nop
+# CHECK0:           4: 00 00 01 00 .word 0x00010000
+# CHECK0-NEXT:               000000006: R_RISCV_ALIGN        *ABS*+0x6
+# CHECK0-NEXT:      8: 13 00 00 00 .word 0x00000013
+# CHECK0:          14: auipc   ra, 0x0
+# CHECK0:          1c: c.nop
 # CHECK0-NEXT:               R_RISCV_ALIGN        *ABS*+0x6
 
 .text 2
@@ -28,16 +32,12 @@ l2:
 .option pop
 
 .text 1
-  .space 4
+  .org .+1
+  .org .+3
 .ifdef LATE
-  .space 0
+  .org .+0
 .endif
   call foo
-.ifdef LATE
-  .space 8
-.else
-  .space 4
-.endif
 
 .text 0
 _start:
