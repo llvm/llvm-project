216b61dcafa101a5c744eb61905349f71ac98444
Pavel Labath <pavel@labath.sk>
2025-08-08 08:28:11 +0200

    Revert "[Object] Parsing and dumping of SFrame Frame Row Entries (#151301)"
    This reverts commit a82ca1b5603a4ed9598b784f703d908f32e970b8.

--- llvm/include/llvm/Object/SFrameParser.h
+++ llvm/include/llvm/Object/SFrameParser.h
@@ -10,7 +10,6 @@
 #define LLVM_OBJECT_SFRAME_H
 
 #include "llvm/ADT/ArrayRef.h"
-#include "llvm/ADT/fallible_iterator.h"
 #include "llvm/BinaryFormat/SFrame.h"
 #include "llvm/Support/Compiler.h"
 #include "llvm/Support/Error.h"
@@ -20,8 +19,6 @@ namespace llvm {
 namespace object {
 
 template <endianness E> class SFrameParser {
-  class FallibleFREIterator;
-
 public:
   static Expected<SFrameParser> create(ArrayRef<uint8_t> Contents,
                                        uint64_t SectionAddress);
@@ -45,21 +42,6 @@ public:
   // objects returned by the `fdes()` function.
   uint64_t getAbsoluteStartAddress(typename FDERange::iterator FDE) const;
 
-  struct FrameRowEntry {
-    uint32_t StartAddress;
-    sframe::FREInfo<endianness::native> Info;
-    SmallVector<int32_t, 3> Offsets;
-  };
-
-  using fre_iterator = fallible_iterator<FallibleFREIterator>;
-  iterator_range<fre_iterator> fres(const sframe::FuncDescEntry<E> &FDE,
-                                    Error &Err) const;
-
-  std::optional<int32_t> getCFAOffset(const FrameRowEntry &FRE) const;
-  std::optional<int32_t> getRAOffset(const FrameRowEntry &FRE) const;
-  std::optional<int32_t> getFPOffset(const FrameRowEntry &FRE) const;
-  ArrayRef<int32_t> getExtraOffsets(const FrameRowEntry &FRE) const;
-
 private:
   ArrayRef<uint8_t> Data;
   uint64_t SectionAddress;
@@ -72,39 +54,6 @@ private:
   uint64_t getFDEBase() const {
     return sizeof(Header) + Header.AuxHdrLen + Header.FDEOff;
   }
-
-  uint64_t getFREBase() const {
-    return getFDEBase() + Header.NumFDEs * sizeof(sframe::FuncDescEntry<E>);
-  }
-};
-
-template <endianness E> class SFrameParser<E>::FallibleFREIterator {
-public:
-  // NB: This iterator starts out in the before_begin() state. It must be
-  // ++'ed to reach the first element.
-  FallibleFREIterator(ArrayRef<uint8_t> Data, sframe::FREType FREType,
-                      uint32_t Idx, uint32_t Size, uint64_t Offset)
-      : Data(Data), FREType(FREType), Idx(Idx), Size(Size), Offset(Offset) {}
-
-  Error inc();
-  const FrameRowEntry &operator*() const { return FRE; }
-
-  friend bool operator==(const FallibleFREIterator &LHS,
-                         const FallibleFREIterator &RHS) {
-    assert(LHS.Data.data() == RHS.Data.data());
-    assert(LHS.Data.size() == RHS.Data.size());
-    assert(LHS.FREType == RHS.FREType);
-    assert(LHS.Size == RHS.Size);
-    return LHS.Idx == RHS.Idx;
-  }
-
-private:
-  ArrayRef<uint8_t> Data;
-  sframe::FREType FREType;
-  uint32_t Idx;
-  uint32_t Size;
-  uint64_t Offset;
-  FrameRowEntry FRE;
 };
 
 extern template class LLVM_TEMPLATE_ABI SFrameParser<endianness::big>;
