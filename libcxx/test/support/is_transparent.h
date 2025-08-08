00e4c6d805a084c4310d4e27bb87892411bf659a
A. Jiang <de34@live.cn>
2025-08-08 10:17:33 +0800

    [libc++] Fix uses of non-empty transparent comparator with in `<map>`
    The `__get_value()` member function was removed in LLVM 21, but the
    calls in `<map>` weren't removed. This patch completes the removal and
    adds regression test cases.

--- libcxx/test/support/is_transparent.h
+++ libcxx/test/support/is_transparent.h
@@ -36,6 +36,19 @@ struct transparent_less_not_referenceable
     using is_transparent = void () const &;  // it's a type; a weird one, but a type
 };
 
+// Prevent regression when empty base class optimization is not suitable.
+// See https://github.com/llvm/llvm-project/issues/152543.
+struct transparent_less_nonempty {
+  template <class T, class U>
+  constexpr auto operator()(T&& t, U&& u) const                   //
+      noexcept(noexcept(std::forward<T>(t) < std::forward<U>(u))) //
+      -> decltype /**/ (std::forward<T>(t) < std::forward<U>(u)) {
+    return /*--------*/ std::forward<T>(t) < std::forward<U>(u);
+  }
+  struct is_transparent {
+  } pad_; // making this comparator non-empty
+};
+
 struct transparent_less_no_type
 {
     template <class T, class U>
