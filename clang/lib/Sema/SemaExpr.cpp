824084e8abb12ac6d48548a41be41f0db3ea3463
tinnamchoi <tinnam.choi@gmail.com>
2025-08-09 18:59:33 +0800

    [Clang] Suggest using `std::to_underlying` for invalid scoped enum operations in C++23

--- clang/lib/Sema/SemaExpr.cpp
+++ clang/lib/Sema/SemaExpr.cpp
@@ -10761,23 +10761,26 @@ static void diagnoseScopedEnums(Sema &S, const SourceLocation Loc,
     return;
   if (BinaryOperator::isAssignmentOp(Opc) && LHSIsScoped)
     return;
+  bool isCxx23 = S.getLangOpts().CPlusPlus23;
+  unsigned diagID =
+      isCxx23 ? diag::note_no_implicit_conversion_for_scoped_enum_cxx23
+              : diag::note_no_implicit_conversion_for_scoped_enum;
+  auto diagnosticHelper = [&S, isCxx23, diagID](const Expr *expr, const QualType type) {
+    SourceLocation beginLoc = expr->getBeginLoc();
+    QualType intType =
+        type->castAs<EnumType>()->getDecl()->getIntegerType();
+    std::string insertionString =
+        isCxx23 ? "std::to_underlying("
+                : "static_cast<" + intType.getAsString() + ">(";
+    S.Diag(beginLoc, diagID)
+        << FixItHint::CreateInsertion(beginLoc, insertionString)
+        << FixItHint::CreateInsertion(expr->getEndLoc(), ")");
+  };
   if (LHSIsScoped) {
-    SourceLocation LHSBegin = LHSExpr->getBeginLoc();
-    QualType LHSIntType =
-        LHSType->castAs<EnumType>()->getDecl()->getIntegerType();
-    S.Diag(LHSBegin, diag::note_no_implicit_conversion_for_scoped_enum)
-        << FixItHint::CreateInsertion(
-               LHSBegin, "static_cast<" + LHSIntType.getAsString() + ">(")
-        << FixItHint::CreateInsertion(LHSExpr->getEndLoc(), ")");
+    diagnosticHelper(LHSExpr, LHSType);
   }
   if (RHSIsScoped) {
-    SourceLocation RHSBegin = RHSExpr->getBeginLoc();
-    QualType RHSIntType =
-        RHSType->castAs<EnumType>()->getDecl()->getIntegerType();
-    S.Diag(RHSBegin, diag::note_no_implicit_conversion_for_scoped_enum)
-        << FixItHint::CreateInsertion(
-               RHSBegin, "static_cast<" + RHSIntType.getAsString() + ">(")
-        << FixItHint::CreateInsertion(RHSExpr->getEndLoc(), ")");
+    diagnosticHelper(RHSExpr, RHSType);
   }
 }
 
