f894ff3519563036082caaa3efc46b682bf78513
Muhammad Bassiouni <60100307+bassiounix@users.noreply.github.com>
2025-08-09 18:51:54 +0300

    [libc][math] Refactor cos implementation to header-only in src/__support/math folder. (#151883)
    Part of #147386
    
    in preparation for: https://discourse.llvm.org/t/rfc-make-clang-builtin-math-functions-constexpr-with-llvm-libc-to-support-c-23-constexpr-math-functions/86450

--- libc/shared/math/cos.h
+++ libc/shared/math/cos.h
@@ -1,4 +1,4 @@
-//===-- Double-precision cos function -------------------------------------===//
+//===-- Shared cos function -------------------------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,11 +6,18 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "src/math/cos.h"
+#ifndef LLVM_LIBC_SHARED_MATH_COS_H
+#define LLVM_LIBC_SHARED_MATH_COS_H
+
+#include "shared/libc_common.h"
 #include "src/__support/math/cos.h"
 
 namespace LIBC_NAMESPACE_DECL {
+namespace shared {
 
-LLVM_LIBC_FUNCTION(double, cos, (double x)) { return math::cos(x); }
+using math::cos;
 
+} // namespace shared
 } // namespace LIBC_NAMESPACE_DECL
+
+#endif // LLVM_LIBC_SHARED_MATH_COS_H
