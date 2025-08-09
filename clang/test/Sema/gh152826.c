e58c0aeda1aadeef6a1d01ce3857d8fe23cfb8b1
Vincent <llvm@viceroygroup.ca>
2025-08-09 23:07:30 +0800

    [clang] Fix Variable Length Array `_Countof` Crash
    Check for missing VLA size expressions (e.g. in int a[*][10])
    before evaluation to avoid crashes in _Countof and constant
    expression checks.
    
    fixes #152826

---
+++ clang/test/Sema/gh152826.c
@@ -0,0 +1,6 @@
+// RUN: %clang_cc1 -std=c2y -verify %s
+// RUN: %clang_cc1 -std=c2y -verify -fexperimental-new-constant-interpreter %s
+// expected-no-diagnostics
+
+void gh152826(char (*a)[*][5], int (*x)[_Countof (*a)]);
+void more_likely_in_practice(unsigned long size_one, int (*a)[*][5], int b[_Countof(*a)]);
