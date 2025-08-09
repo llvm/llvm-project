e05b3e29b1db957da678a0bbcd4f988e5efe61ab
Muhammad Bassiouni <60100307+bassiounix@users.noreply.github.com>
2025-08-09 18:51:54 +0300

    [libc][math] Refactor cos implementation to header-only in src/__support/math folder. (#151883)
    Part of #147386
    
    in preparation for: https://discourse.llvm.org/t/rfc-make-clang-builtin-math-functions-constexpr-with-llvm-libc-to-support-c-23-constexpr-math-functions/86450

--- libc/src/__support/math/cos.h
+++ libc/src/__support/math/cos.h
@@ -1,4 +1,4 @@
-//===-- Double-precision sin function -------------------------------------===//
+//===-- Implementation header for cos ---------------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,65 +6,54 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "src/math/sin.h"
-#include "hdr/errno_macros.h"
+#ifndef LIBC_SRC___SUPPORT_MATH_COS_H
+#define LIBC_SRC___SUPPORT_MATH_COS_H
+
+#include "range_reduction_double_common.h"
+#include "sincos_eval.h"
 #include "src/__support/FPUtil/FEnvImpl.h"
 #include "src/__support/FPUtil/FPBits.h"
 #include "src/__support/FPUtil/double_double.h"
 #include "src/__support/FPUtil/dyadic_float.h"
-#include "src/__support/FPUtil/multiply_add.h"
-#include "src/__support/FPUtil/rounding_mode.h"
-#include "src/__support/common.h"
+#include "src/__support/FPUtil/except_value_utils.h"
 #include "src/__support/macros/config.h"
 #include "src/__support/macros/optimization.h"            // LIBC_UNLIKELY
 #include "src/__support/macros/properties/cpu_features.h" // LIBC_TARGET_CPU_HAS_FMA
-#include "src/__support/math/range_reduction_double_common.h"
-#include "src/__support/math/sincos_eval.h"
 
 #ifdef LIBC_TARGET_CPU_HAS_FMA_DOUBLE
-#include "src/__support/math/range_reduction_double_fma.h"
+#include "range_reduction_double_fma.h"
 #else
-#include "src/__support/math/range_reduction_double_nofma.h"
+#include "range_reduction_double_nofma.h"
 #endif // LIBC_TARGET_CPU_HAS_FMA_DOUBLE
 
 namespace LIBC_NAMESPACE_DECL {
 
-using DoubleDouble = fputil::DoubleDouble;
-using Float128 = typename fputil::DyadicFloat<128>;
+namespace math {
 
-LLVM_LIBC_FUNCTION(double, sin, (double x)) {
-  using namespace math::range_reduction_double_internal;
+LIBC_INLINE static constexpr double cos(double x) {
+  using namespace range_reduction_double_internal;
+  using DoubleDouble = fputil::DoubleDouble;
   using FPBits = typename fputil::FPBits<double>;
   FPBits xbits(x);
 
   uint16_t x_e = xbits.get_biased_exponent();
 
   DoubleDouble y;
-  unsigned k;
-  LargeRangeReduction range_reduction_large{};
+  unsigned k = 0;
+  LargeRangeReduction range_reduction_large;
 
-  // |x| < 2^16
+  // |x| < 2^16.
   if (LIBC_LIKELY(x_e < FPBits::EXP_BIAS + FAST_PASS_EXPONENT)) {
     // |x| < 2^-7
     if (LIBC_UNLIKELY(x_e < FPBits::EXP_BIAS - 7)) {
-      // |x| < 2^-26, |sin(x) - x| < ulp(x)/2.
-      if (LIBC_UNLIKELY(x_e < FPBits::EXP_BIAS - 26)) {
+      // |x| < 2^-27
+      if (LIBC_UNLIKELY(x_e < FPBits::EXP_BIAS - 27)) {
         // Signed zeros.
         if (LIBC_UNLIKELY(x == 0.0))
-          return x + x; // Make sure it works with FTZ/DAZ.
+          return 1.0;
 
-#ifdef LIBC_TARGET_CPU_HAS_FMA_DOUBLE
-        return fputil::multiply_add(x, -0x1.0p-54, x);
-#else
-        if (LIBC_UNLIKELY(x_e < 4)) {
-          int rounding_mode = fputil::quick_get_round();
-          if (rounding_mode == FE_TOWARDZERO ||
-              (xbits.sign() == Sign::POS && rounding_mode == FE_DOWNWARD) ||
-              (xbits.sign() == Sign::NEG && rounding_mode == FE_UPWARD))
-            return FPBits(xbits.uintval() - 1).get_val();
-        }
-        return fputil::multiply_add(x, -0x1.0p-54, x);
-#endif // LIBC_TARGET_CPU_HAS_FMA_DOUBLE
+        // For |x| < 2^-27, |cos(x) - 1| < |x|^2/2 < 2^-54 = ulp(1 - 2^-53)/2.
+        return fputil::round_result_slightly_down(1.0);
       }
       // No range reduction needed.
       k = 0;
@@ -77,12 +66,11 @@ LLVM_LIBC_FUNCTION(double, sin, (double x)) {
   } else {
     // Inf or NaN
     if (LIBC_UNLIKELY(x_e > 2 * FPBits::EXP_BIAS)) {
-      // sin(+-Inf) = NaN
       if (xbits.is_signaling_nan()) {
         fputil::raise_except_if_required(FE_INVALID);
         return FPBits::quiet_nan().get_val();
       }
-
+      // cos(+-Inf) = NaN
       if (xbits.get_mantissa() == 0) {
         fputil::set_errno_if_required(EDOM);
         fputil::raise_except_if_required(FE_INVALID);
@@ -111,30 +99,30 @@ LLVM_LIBC_FUNCTION(double, sin, (double x)) {
     }
     return ans;
   };
-  DoubleDouble sin_k = get_idx_dd(k);
+  DoubleDouble msin_k = get_idx_dd(k + 128);
   DoubleDouble cos_k = get_idx_dd(k + 64);
 #else
   // Fast look up version, but needs 256-entry table.
+  // -sin(k * pi/128) = sin((k + 128) * pi/128)
   // cos(k * pi/128) = sin(k * pi/128 + pi/2) = sin((k + 64) * pi/128).
-  DoubleDouble sin_k = SIN_K_PI_OVER_128[k & 255];
+  DoubleDouble msin_k = SIN_K_PI_OVER_128[(k + 128) & 255];
   DoubleDouble cos_k = SIN_K_PI_OVER_128[(k + 64) & 255];
-#endif
+#endif // LIBC_MATH_HAS_SMALL_TABLES
 
   // After range reduction, k = round(x * 128 / pi) and y = x - k * (pi / 128).
   // So k is an integer and -pi / 256 <= y <= pi / 256.
-  // Then sin(x) = sin((k * pi/128 + y)
-  //             = sin(y) * cos(k*pi/128) + cos(y) * sin(k*pi/128)
-  DoubleDouble sin_k_cos_y = fputil::quick_mult(cos_y, sin_k);
-  DoubleDouble cos_k_sin_y = fputil::quick_mult(sin_y, cos_k);
+  // Then cos(x) = cos((k * pi/128 + y)
+  //             = cos(y) * cos(k*pi/128) - sin(y) * sin(k*pi/128)
+  DoubleDouble cos_k_cos_y = fputil::quick_mult(cos_y, cos_k);
+  DoubleDouble msin_k_sin_y = fputil::quick_mult(sin_y, msin_k);
 
-  DoubleDouble rr = fputil::exact_add<false>(sin_k_cos_y.hi, cos_k_sin_y.hi);
-  rr.lo += sin_k_cos_y.lo + cos_k_sin_y.lo;
+  DoubleDouble rr = fputil::exact_add<false>(cos_k_cos_y.hi, msin_k_sin_y.hi);
+  rr.lo += msin_k_sin_y.lo + cos_k_cos_y.lo;
 
 #ifdef LIBC_MATH_HAS_SKIP_ACCURATE_PASS
   return rr.hi + rr.lo;
 #else
-  // Accurate test and pass for correctly rounded implementation.
-
+  using Float128 = typename fputil::DyadicFloat<128>;
   double rlp = rr.lo + err;
   double rlm = rr.lo - err;
 
@@ -161,14 +149,15 @@ LLVM_LIBC_FUNCTION(double, sin, (double x)) {
     return ans;
   };
 
+  // -sin(k * pi/128) = sin((k + 128) * pi/128)
   // cos(k * pi/128) = sin(k * pi/128 + pi/2) = sin((k + 64) * pi/128).
-  Float128 sin_k_f128 = get_sin_k(k);
+  Float128 msin_k_f128 = get_sin_k(k + 128);
   Float128 cos_k_f128 = get_sin_k(k + 64);
 
-  // sin(x) = sin(k * pi/128 + u)
-  //        = sin(u) * cos(k*pi/128) + cos(u) * sin(k*pi/128)
-  Float128 r = fputil::quick_add(fputil::quick_mul(sin_k_f128, cos_u),
-                                 fputil::quick_mul(cos_k_f128, sin_u));
+  // cos(x) = cos((k * pi/128 + u)
+  //        = cos(u) * cos(k*pi/128) - sin(u) * sin(k*pi/128)
+  Float128 r = fputil::quick_add(fputil::quick_mul(cos_k_f128, cos_u),
+                                 fputil::quick_mul(msin_k_f128, sin_u));
 
   // TODO: Add assertion if Ziv's accuracy tests fail in debug mode.
   // https://github.com/llvm/llvm-project/issues/96452.
@@ -177,4 +166,8 @@ LLVM_LIBC_FUNCTION(double, sin, (double x)) {
 #endif // !LIBC_MATH_HAS_SKIP_ACCURATE_PASS
 }
 
+} // namespace math
+
 } // namespace LIBC_NAMESPACE_DECL
+
+#endif // LIBC_SRC___SUPPORT_MATH_COS_H
