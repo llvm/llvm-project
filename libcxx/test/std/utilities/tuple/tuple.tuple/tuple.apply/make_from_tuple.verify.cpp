7d345a59e0a3b3b0be34634d2431cc6f4835dbd7
yronglin <yronglin777@gmail.com>
2025-08-10 00:47:22 +0800

    [libc++] Implement std::make_from_tuple mandates: If tuple_size_v<remove_reference_t<Tuple>> is 1, then reference_constructs_from_temporary_v<T, decltype(get<0>(declval<Tuple>()))> is false.
    Signed-off-by: yronglin <yronglin777@gmail.com>

---
+++ libcxx/test/std/utilities/tuple/tuple.tuple/tuple.apply/make_from_tuple.verify.cpp
@@ -0,0 +1,23 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++03, c++11, c++14
+
+// <tuple>
+
+// template <class T, class Tuple> constexpr T make_from_tuple(Tuple&&);
+// Mandates: If tuple_size_v<remove_reference_t<Tuple>> is 1, then reference_constructs_from_temporary_v<T, decltype(get<0>(declval<Tuple>()))> is false.
+
+#include <tuple>
+#include <utility>
+
+void test() {
+  // expected-error@*:* {{static assertion failed}}
+  // expected-error@*:* {{returning reference to local temporary object}}
+  std::ignore = std::make_from_tuple<const int&>(std::tuple<char>{});
+}
