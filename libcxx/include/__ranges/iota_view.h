e66aca58e43cfb9fcb55bc4025b12e6b57031f72
Hristo Hristov <hghristov.rmm@gmail.com>
2025-08-09 11:45:35 +0300

    [libc++][ranges] LWG4096: `views::iota(views::iota(0))` should be rejected
    Fixes: #105352

--- libcxx/include/__ranges/iota_view.h
+++ libcxx/include/__ranges/iota_view.h
@@ -30,6 +30,7 @@
 #include <__ranges/movable_box.h>
 #include <__ranges/view_interface.h>
 #include <__type_traits/conditional.h>
+#include <__type_traits/decay.h>
 #include <__type_traits/is_nothrow_constructible.h>
 #include <__type_traits/make_unsigned.h>
 #include <__type_traits/type_identity.h>
@@ -377,7 +378,7 @@ struct __fn {
   _LIBCPP_HIDE_FROM_ABI constexpr auto operator()(_Start&& __start) const
       noexcept(noexcept(ranges::iota_view(std::forward<_Start>(__start))))
           -> decltype(ranges::iota_view(std::forward<_Start>(__start))) {
-    return ranges::iota_view(std::forward<_Start>(__start));
+    return ranges::iota_view<decay_t<_Start>>(std::forward<_Start>(__start));
   }
 
   template <class _Start, class _BoundSentinel>
---
+++ libcxx/test/std/ranges/range.factories/range.iota.view/views_iota.verify.cpp
@@ -0,0 +1,29 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// REQUIRES: std-at-least-c++20
+
+// views::iota
+
+#include <ranges>
+
+#include "types.h"
+
+void test() {
+  // LWG4096
+  {
+    [[maybe_unused]] auto i1 = std::views::iota(0); // OK
+    [[maybe_unused]] auto i2 = std::views::iota(std::views::iota(0));
+    // expected-error-re@*:* {{constraints not satisfied for class template 'iota_view'{{.*}}}}
+  }
+  {
+    [[maybe_unused]] auto i1 = std::views::iota(SomeInt(0)); // OK
+    [[maybe_unused]] auto i2 = std::views::iota(std::views::iota(SomeInt(0)));
+    //expected-error-re@*:* {{constraints not satisfied for class template 'iota_view'{{.*}}}}
+  }
+}
