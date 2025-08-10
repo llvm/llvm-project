ad9be29dfad441a0a3bc9b3730e8e53f4300be4b
Thurston Dang <thurston@google.com>
2025-08-10 06:24:32 +0000

    Simplify test case by removing useless steps

--- compiler-rt/test/asan/TestCases/fakestack_alignment.cpp
+++ compiler-rt/test/asan/TestCases/fakestack_alignment.cpp
@@ -17,29 +17,8 @@ struct alignas(16384) larry {
 
 bool misaligned = false;
 
-void grandchild(void) {
-  larry l2;
-  uint alignment = (unsigned long)&l2 % alignof(larry);
-  if (alignment != 0)
-    misaligned = true;
-
-  printf("Grandchild: address modulo alignment %u\n", alignment);
-}
-
-// Even if the FakeStack frame is aligned by chance to 16384, we can use an
-// intervening stack frame to knock it out of alignment.
-void child(void) {
-  page p1;
-  uint alignment = (unsigned long)&p1 % alignof(page);
-  printf("Child: address modulo alignment is %u\n", alignment);
-  if (alignment != 0)
-    misaligned = true;
-
-  grandchild();
-}
-
 // Check whether the FakeStack frame is sufficiently aligned. Alignment can
-// happen by chance, so try this on many threads if you don't want
+// happen by chance, so try this on many threads.
 void *Thread(void *unused) {
   larry l1;
   uint alignment = (unsigned long)&l1 % alignof(larry);
@@ -56,12 +35,12 @@ int main(int argc, char **argv) {
   pthread_attr_t attr;
   pthread_attr_init(&attr);
 
-  pthread_t t[10];
-  for (int i = 0; i < 10; i++) {
+  pthread_t t[32];
+  for (int i = 0; i < 32; i++) {
     pthread_create(&t[i], &attr, Thread, 0);
   }
   pthread_attr_destroy(&attr);
-  for (int i = 0; i < 10; i++) {
+  for (int i = 0; i < 32; i++) {
     pthread_join(t[i], 0);
   }
 
