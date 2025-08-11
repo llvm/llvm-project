f3383e7ada42d0682d77399e12afd0a13e0d83e0
Fraser Cormack <fraser@codeplay.com>
2025-08-11 14:15:27 +0100

    formatting

--- .ci/compute_projects_test.py
+++ .ci/compute_projects_test.py
@@ -90,7 +90,8 @@ class TestComputeProjects(unittest.TestCase):
             "check-clang check-clang-tools",
         )
         self.assertEqual(
-            env_variables["runtimes_to_build"], "compiler-rt;libclc;libcxx;libcxxabi;libunwind"
+            env_variables["runtimes_to_build"],
+            "compiler-rt;libclc;libcxx;libcxxabi;libunwind",
         )
         self.assertEqual(
             env_variables["runtimes_check_targets"],
@@ -165,7 +166,8 @@ class TestComputeProjects(unittest.TestCase):
             "check-clang check-clang-cir check-clang-tools",
         )
         self.assertEqual(
-            env_variables["runtimes_to_build"], "compiler-rt;libclc;libcxx;libcxxabi;libunwind"
+            env_variables["runtimes_to_build"],
+            "compiler-rt;libclc;libcxx;libcxxabi;libunwind",
         )
         self.assertEqual(
             env_variables["runtimes_check_targets"],
