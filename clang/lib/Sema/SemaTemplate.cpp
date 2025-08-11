f05e54dac506feb3a333babb4b33c76aa57db740
Ilya Biryukov <ibiryukov@google.com>
2025-08-11 17:38:48 +0200

    Pass the builtin name in the error message

--- clang/lib/Sema/SemaTemplate.cpp
+++ clang/lib/Sema/SemaTemplate.cpp
@@ -332,7 +332,7 @@ TemplateNameKind Sema::isTemplateName(Scope *S,
 
   if (isPackProducingBuiltinTemplateName(Template) &&
       S->getTemplateParamParent() == nullptr) {
-    Diag(Name.getBeginLoc(), diag::err_builtin_pack_outside_template);
+    Diag(Name.getBeginLoc(), diag::err_builtin_pack_outside_template) << TName;
     // Recover by returning the template, even though we would never be able to
     // substitute it.
   }
