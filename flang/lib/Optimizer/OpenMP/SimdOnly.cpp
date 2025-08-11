773fb19214f8d25dd40a545edd54caef2b660a37
Kajetan Puchalski <kajetan.puchalski@arm.com>
2025-08-11 13:04:42 +0000

    Redesign with parse tree rewriting

--- flang/lib/Optimizer/OpenMP/SimdOnly.cpp
+++ flang/lib/Optimizer/OpenMP/SimdOnly.cpp
@@ -29,6 +29,11 @@ namespace {
 
 #define DEBUG_TYPE "omp-simd-only-pass"
 
+/// Rewrite and remove OpenMP operations left after the parse tree rewriting for
+/// -fopenmp-simd is done. If possible, OpenMP constructs should be rewritten at
+/// the parse tree stage. This pass is supposed to only handle complexities
+/// around untangling composite simd constructs, and perform the necessary
+/// cleanup.
 class SimdOnlyConversionPattern : public mlir::RewritePattern {
 public:
   SimdOnlyConversionPattern(mlir::MLIRContext *ctx)
@@ -53,6 +58,7 @@ public:
 
     if (op->getParentOfType<mlir::omp::SimdOp>() &&
         (mlir::isa<mlir::omp::YieldOp>(op) ||
+         mlir::isa<mlir::omp::ScanOp>(op) ||
          mlir::isa<mlir::omp::LoopNestOp>(op) ||
          mlir::isa<mlir::omp::TerminatorOp>(op)))
       return rewriter.notifyMatchFailure(op, "Op is part of a simd construct");
@@ -62,21 +68,6 @@ public:
          mlir::isa<mlir::omp::YieldOp>(op)))
       return rewriter.notifyMatchFailure(op,
                                          "Non top-level yield or terminator");
-
-    if (mlir::isa<mlir::omp::UnrollHeuristicOp>(op))
-      return rewriter.notifyMatchFailure(
-          op, "UnrollHeuristic has special handling");
-
-    // SectionOp overrides its BlockArgInterface based on the parent SectionsOp.
-    // We need to make sure we only rewrite omp.sections once all omp.section
-    // ops inside it have been rewritten, otherwise the individual omp.section
-    // ops will not be able to access their argument values.
-    if (auto sectionsOp = mlir::dyn_cast<mlir::omp::SectionsOp>(op)) {
-      for (auto &opInSections : sectionsOp.getRegion().getOps())
-        if (mlir::isa<mlir::omp::SectionOp>(opInSections))
-          return rewriter.notifyMatchFailure(
-              op, "SectionsOp still contains individual sections");
-    }
 
     LLVM_DEBUG(llvm::dbgs() << "SimdOnlyPass matched OpenMP op:\n");
     LLVM_DEBUG(op->dump());
@@ -98,192 +89,19 @@ public:
       return eraseUnlessUsedBySimd(ompOp, ompOp.getSymNameAttr());
     if (auto ompOp = mlir::dyn_cast<mlir::omp::DeclareReductionOp>(op))
       return eraseUnlessUsedBySimd(ompOp, ompOp.getSymNameAttr());
-    if (auto ompOp = mlir::dyn_cast<mlir::omp::CriticalDeclareOp>(op))
-      return eraseUnlessUsedBySimd(ompOp, ompOp.getSymNameAttr());
-    if (auto ompOp = mlir::dyn_cast<mlir::omp::DeclareMapperOp>(op))
-      return eraseUnlessUsedBySimd(ompOp, ompOp.getSymNameAttr());
 
-    // Erase ops that don't need any special handling
-    if (mlir::isa<mlir::omp::BarrierOp>(op) ||
-        mlir::isa<mlir::omp::FlushOp>(op) ||
-        mlir::isa<mlir::omp::TaskyieldOp>(op) ||
-        mlir::isa<mlir::omp::MapBoundsOp>(op) ||
-        mlir::isa<mlir::omp::TargetEnterDataOp>(op) ||
-        mlir::isa<mlir::omp::TargetExitDataOp>(op) ||
-        mlir::isa<mlir::omp::TargetUpdateOp>(op) ||
-        mlir::isa<mlir::omp::OrderedOp>(op) ||
-        mlir::isa<mlir::omp::CancelOp>(op) ||
-        mlir::isa<mlir::omp::CancellationPointOp>(op) ||
-        mlir::isa<mlir::omp::ScanOp>(op) ||
-        mlir::isa<mlir::omp::TaskwaitOp>(op)) {
+    // Might be left over from rewriting composite simd with target map
+    if (mlir::isa<mlir::omp::MapBoundsOp>(op)) {
       rewriter.eraseOp(op);
       return mlir::success();
     }
-
-    fir::FirOpBuilder builder(rewriter, op);
-    mlir::Location loc = op->getLoc();
-
-    if (auto ompOp = mlir::dyn_cast<mlir::omp::LoopNestOp>(op)) {
-      mlir::Type indexType = builder.getIndexType();
-      mlir::Type oldIndexType = ompOp.getIVs().begin()->getType();
-      builder.setInsertionPoint(op);
-      auto one = mlir::arith::ConstantIndexOp::create(builder, loc, 1);
-
-      // Generate the new loop nest
-      mlir::Block *nestBody = nullptr;
-      fir::DoLoopOp outerLoop = nullptr;
-      llvm::SmallVector<mlir::Value> loopIndArgs;
-      for (auto extent : ompOp.getLoopUpperBounds()) {
-        auto ub = builder.createConvert(loc, indexType, extent);
-        auto doLoop = fir::DoLoopOp::create(builder, loc, one, ub, one, false);
-        nestBody = doLoop.getBody();
-        builder.setInsertionPointToStart(nestBody);
-        // Convert the indices to the type used inside the loop if needed
-        if (oldIndexType != indexType) {
-          auto convertedIndVar = builder.createConvert(
-              loc, oldIndexType, doLoop.getInductionVar());
-          loopIndArgs.push_back(convertedIndVar);
-        } else {
-          loopIndArgs.push_back(doLoop.getInductionVar());
-        }
-        if (!outerLoop)
-          outerLoop = doLoop;
-      }
-
-      // Move the omp loop body into the new loop body
-      if (ompOp->getRegion(0).getBlocks().size() == 1) {
-        auto &block = *ompOp->getRegion(0).getBlocks().begin();
-        rewriter.mergeBlocks(&block, nestBody, loopIndArgs);
-
-        // Find the new loop block terminator and move it before the end of the
-        // block
-        for (auto &loopBodyOp : nestBody->getOperations()) {
-          if (auto resultOp = mlir::dyn_cast<fir::ResultOp>(loopBodyOp)) {
-            rewriter.moveOpBefore(resultOp.getOperation(), &nestBody->back());
-            break;
-          }
-        }
-
-        // Remove omp.yield at the end of the loop body
-        if (auto yieldOp =
-                mlir::dyn_cast<mlir::omp::YieldOp>(nestBody->back())) {
-          assert("omp.loop_nests's omp.yield has no operands" &&
-                 yieldOp->getNumOperands() == 0);
-          rewriter.eraseOp(yieldOp);
-        }
-      } else {
-        rewriter.inlineRegionBefore(ompOp->getRegion(0), nestBody);
-        auto indVarArg = outerLoop->getRegion(0).front().getArgument(0);
-        // fir::convertDoLoopToCFG expects the induction variable to be of type
-        // index while the OpenMP LoopNestOp can have indices of different
-        // types. We need to work around it.
-        if (indVarArg.getType() != indexType)
-          indVarArg.setType(indexType);
-
-        // fir.do_loop, unlike omp.loop_nest does not support multi-block
-        // regions. If we're dealing with multiple blocks inside omp.loop_nest,
-        // we need to convert it into basic control-flow operations instead.
-        auto loopBlocks =
-            fir::convertDoLoopToCFG(outerLoop, rewriter, false, false);
-        auto *conditionalBlock = loopBlocks.first;
-        auto *firstBlock =
-            conditionalBlock->getNextNode(); // Start of the loop body
-        auto *lastBlock = loopBlocks.second; // Incrementing induction variables
-
-        // If the induction variable is used within the loop and was originally
-        // not of type index, then we need to add a convert to the original type
-        // and replace its uses inside the loop body.
-        if (oldIndexType != indexType) {
-          indVarArg = conditionalBlock->getArgument(0);
-          builder.setInsertionPointToStart(firstBlock);
-          auto convertedIndVar =
-              builder.createConvert(loc, oldIndexType, indVarArg);
-          rewriter.replaceUsesWithIf(
-              indVarArg, convertedIndVar, [&](auto &use) -> bool {
-                return use.getOwner() != convertedIndVar.getDefiningOp() &&
-                       use.getOwner()->getBlock() != lastBlock;
-              });
-        }
-
-        // There might be an unused convert and an unused argument to the block.
-        // If so, remove them.
-        if (lastBlock->front().getUses().empty())
-          lastBlock->front().erase();
-        for (auto arg : lastBlock->getArguments()) {
-          if (arg.getUses().empty())
-            lastBlock->eraseArgument(arg.getArgNumber());
-        }
-
-        // Any loop blocks that end in omp.yield should just branch to
-        // lastBlock.
-        for (auto *loopBlock = conditionalBlock; loopBlock != lastBlock;
-             loopBlock = loopBlock->getNextNode()) {
-          if (auto yieldOp =
-                  mlir::dyn_cast<mlir::omp::YieldOp>(loopBlock->back())) {
-            builder.setInsertionPointToEnd(loopBlock);
-            mlir::cf::BranchOp::create(builder, loc, lastBlock);
-            assert("omp.loop_nests's omp.yield has no operands" &&
-                   yieldOp->getNumOperands() == 0);
-            rewriter.eraseOp(yieldOp);
-          }
-        }
-      }
-
-      rewriter.eraseOp(ompOp);
-      return mlir::success();
-    }
-
     if (auto mapInfoOp = mlir::dyn_cast<mlir::omp::MapInfoOp>(op)) {
       mapInfoOp.getResult().replaceAllUsesWith(mapInfoOp.getVarPtr());
       rewriter.eraseOp(mapInfoOp);
       return mlir::success();
     }
 
-    if (auto atomicReadOp = mlir::dyn_cast<mlir::omp::AtomicReadOp>(op)) {
-      builder.setInsertionPoint(op);
-      auto loadOp = fir::LoadOp::create(builder, loc, atomicReadOp.getX());
-      auto storeOp = fir::StoreOp::create(builder, loc, loadOp.getResult(),
-                                          atomicReadOp.getV());
-      rewriter.replaceOp(op, storeOp);
-      return mlir::success();
-    }
-
-    if (auto atomicWriteOp = mlir::dyn_cast<mlir::omp::AtomicWriteOp>(op)) {
-      auto storeOp = fir::StoreOp::create(builder, loc, atomicWriteOp.getExpr(),
-                                          atomicWriteOp.getX());
-      rewriter.replaceOp(op, storeOp);
-      return mlir::success();
-    }
-
-    if (auto atomicUpdateOp = mlir::dyn_cast<mlir::omp::AtomicUpdateOp>(op)) {
-      assert("one block in region" &&
-             atomicUpdateOp.getRegion().getBlocks().size() == 1);
-      auto &block = *atomicUpdateOp.getRegion().getBlocks().begin();
-      builder.setInsertionPointToStart(&block);
-
-      // Load the update `x` operand and replace its uses within the block
-      auto loadOp = fir::LoadOp::create(builder, loc, atomicUpdateOp.getX());
-      rewriter.replaceUsesWithIf(
-          block.getArgument(0), loadOp.getResult(),
-          [&](auto &op) { return op.get().getParentBlock() == &block; });
-
-      // Store the result back into `x` in line with omp.yield semantics for
-      // this op
-      auto yieldOp = mlir::cast<mlir::omp::YieldOp>(block.back());
-      assert("only one yield operand" && yieldOp->getNumOperands() == 1);
-      builder.setInsertionPointAfter(yieldOp);
-      fir::StoreOp::create(builder, loc, yieldOp->getOperand(0),
-                           atomicUpdateOp.getX());
-      rewriter.eraseOp(yieldOp);
-
-      // Inline the final block and remove the now-empty op
-      assert("only one block argument" && block.getNumArguments() == 1);
-      block.eraseArguments(0, block.getNumArguments());
-      rewriter.inlineBlockBefore(&block, atomicUpdateOp, {});
-      rewriter.eraseOp(op);
-      return mlir::success();
-    }
-
+    // Might be leftover after parse tree rewriting
     if (auto threadPrivateOp = mlir::dyn_cast<mlir::omp::ThreadprivateOp>(op)) {
       threadPrivateOp.getTlsAddr().replaceAllUsesWith(
           threadPrivateOp.getSymAddr());
@@ -291,128 +109,8 @@ public:
       return mlir::success();
     }
 
-    if (auto cLoopOp = mlir::dyn_cast<mlir::omp::CanonicalLoopOp>(op)) {
-      assert("CanonicalLoopOp has one region" && cLoopOp->getNumRegions() == 1);
-      auto cli = cLoopOp.getCli();
-      auto tripCount = cLoopOp.getTripCount();
-
-      builder.setInsertionPoint(cLoopOp);
-      mlir::Type indexType = builder.getIndexType();
-      mlir::Type oldIndexType = tripCount.getType();
-      auto one = mlir::arith::ConstantIndexOp::create(builder, loc, 1);
-      auto ub = builder.createConvert(loc, indexType, tripCount);
-
-      llvm::SmallVector<mlir::Value> loopIndArgs;
-      auto doLoop = fir::DoLoopOp::create(builder, loc, one, ub, one, false);
-      builder.setInsertionPointToStart(doLoop.getBody());
-      if (oldIndexType != indexType) {
-        auto convertedIndVar =
-            builder.createConvert(loc, oldIndexType, doLoop.getInductionVar());
-        loopIndArgs.push_back(convertedIndVar);
-      } else {
-        loopIndArgs.push_back(doLoop.getInductionVar());
-      }
-
-      if (cLoopOp.getRegion().getBlocks().size() == 1) {
-        auto &block = *cLoopOp.getRegion().getBlocks().begin();
-        // DoLoopOp will handle incrementing the induction variable
-        if (auto addIOp = mlir::dyn_cast<mlir::arith::AddIOp>(block.front())) {
-          rewriter.replaceOpUsesWithinBlock(addIOp, addIOp.getLhs(), &block);
-          rewriter.eraseOp(addIOp);
-        }
-
-        rewriter.mergeBlocks(&block, doLoop.getBody(), loopIndArgs);
-
-        // Find the new loop block terminator and move it before the end of the
-        // block
-        for (auto &loopBodyOp : doLoop.getBody()->getOperations()) {
-          if (auto resultOp = mlir::dyn_cast<fir::ResultOp>(loopBodyOp)) {
-            rewriter.moveOpBefore(resultOp.getOperation(),
-                                  &doLoop.getBody()->back());
-            break;
-          }
-        }
-
-        // Remove omp.terminator at the end of the loop body
-        if (auto terminatorOp = mlir::dyn_cast<mlir::omp::TerminatorOp>(
-                doLoop.getBody()->back())) {
-          rewriter.eraseOp(terminatorOp);
-        }
-      } else {
-        rewriter.inlineRegionBefore(cLoopOp->getRegion(0), doLoop.getBody());
-        auto indVarArg = doLoop.getBody()->getArgument(0);
-        // fir::convertDoLoopToCFG expects the induction variable to be of type
-        // index while the OpenMP CanonicalLoopOp can have indices of different
-        // types. We need to work around it.
-        if (indVarArg.getType() != indexType)
-          indVarArg.setType(indexType);
-
-        // fir.do_loop, unlike omp.canonical_loop does not support multi-block
-        // regions. If we're dealing with multiple blocks inside omp.loop_nest,
-        // we need to convert it into basic control-flow operations instead.
-        auto loopBlocks =
-            fir::convertDoLoopToCFG(doLoop, rewriter, false, false);
-        auto *conditionalBlock = loopBlocks.first;
-        auto *firstBlock =
-            conditionalBlock->getNextNode(); // Start of the loop body
-        auto *lastBlock = loopBlocks.second; // Incrementing induction variables
-
-        // Incrementing the induction variable is handled elsewhere
-        if (auto addIOp =
-                mlir::dyn_cast<mlir::arith::AddIOp>(firstBlock->front())) {
-          rewriter.replaceOpUsesWithinBlock(addIOp, addIOp.getLhs(),
-                                            firstBlock);
-          rewriter.eraseOp(addIOp);
-        }
-
-        // If the induction variable is used within the loop and was originally
-        // not of type index, then we need to add a convert to the original type
-        // and replace its uses inside the loop body.
-        if (oldIndexType != indexType) {
-          indVarArg = conditionalBlock->getArgument(0);
-          builder.setInsertionPointToStart(firstBlock);
-          auto convertedIndVar =
-              builder.createConvert(loc, oldIndexType, indVarArg);
-          rewriter.replaceUsesWithIf(
-              indVarArg, convertedIndVar, [&](auto &use) -> bool {
-                return use.getOwner() != convertedIndVar.getDefiningOp() &&
-                       use.getOwner()->getBlock() != lastBlock;
-              });
-        }
-
-        // There might be an unused convert and an unused argument to the block.
-        // If so, remove them.
-        if (lastBlock->front().getUses().empty())
-          lastBlock->front().erase();
-        for (auto arg : lastBlock->getArguments()) {
-          if (arg.getUses().empty())
-            lastBlock->eraseArgument(arg.getArgNumber());
-        }
-
-        // Any loop blocks that end in omp.terminator should just branch to
-        // lastBlock.
-        for (auto *loopBlock = conditionalBlock; loopBlock != lastBlock;
-             loopBlock = loopBlock->getNextNode()) {
-          if (auto terminatorOp =
-                  mlir::dyn_cast<mlir::omp::TerminatorOp>(loopBlock->back())) {
-            builder.setInsertionPointToEnd(loopBlock);
-            mlir::cf::BranchOp::create(builder, loc, lastBlock);
-            rewriter.eraseOp(terminatorOp);
-          }
-        }
-      }
-
-      rewriter.eraseOp(cLoopOp);
-      // Handle the optional omp.new_cli op
-      if (cli) {
-        // cli will be used by omp.unroll_heuristic ops
-        for (auto *user : cli.getUsers())
-          rewriter.eraseOp(user);
-        rewriter.eraseOp(cli.getDefiningOp());
-      }
-
-      return mlir::success();
-    }
+    fir::FirOpBuilder builder(rewriter, op);
+    mlir::Location loc = op->getLoc();
 
     auto inlineSimpleOp = [&](mlir::Operation *ompOp) -> bool {
       if (!ompOp)
@@ -472,23 +170,14 @@ public:
       return true;
     };
 
+    // Remove ops that will be surrounding simd once a composite simd construct
+    // goes through the codegen stage. All of the other ones should have alredy
+    // been removed in the parse tree rewriting stage.
     if (inlineSimpleOp(mlir::dyn_cast<mlir::omp::TeamsOp>(op)) ||
         inlineSimpleOp(mlir::dyn_cast<mlir::omp::ParallelOp>(op)) ||
-        inlineSimpleOp(mlir::dyn_cast<mlir::omp::SingleOp>(op)) ||
-        inlineSimpleOp(mlir::dyn_cast<mlir::omp::SectionOp>(op)) ||
-        inlineSimpleOp(mlir::dyn_cast<mlir::omp::SectionsOp>(op)) ||
-        inlineSimpleOp(mlir::dyn_cast<mlir::omp::WsloopOp>(op)) ||
-        inlineSimpleOp(mlir::dyn_cast<mlir::omp::LoopOp>(op)) ||
         inlineSimpleOp(mlir::dyn_cast<mlir::omp::TargetOp>(op)) ||
-        inlineSimpleOp(mlir::dyn_cast<mlir::omp::TargetDataOp>(op)) ||
-        inlineSimpleOp(mlir::dyn_cast<mlir::omp::DistributeOp>(op)) ||
-        inlineSimpleOp(mlir::dyn_cast<mlir::omp::TaskOp>(op)) ||
-        inlineSimpleOp(mlir::dyn_cast<mlir::omp::TaskloopOp>(op)) ||
-        inlineSimpleOp(mlir::dyn_cast<mlir::omp::MasterOp>(op)) ||
-        inlineSimpleOp(mlir::dyn_cast<mlir::omp::CriticalOp>(op)) ||
-        inlineSimpleOp(mlir::dyn_cast<mlir::omp::OrderedRegionOp>(op)) ||
-        inlineSimpleOp(mlir::dyn_cast<mlir::omp::AtomicCaptureOp>(op)) ||
-        inlineSimpleOp(mlir::dyn_cast<mlir::omp::MaskedOp>(op)))
+        inlineSimpleOp(mlir::dyn_cast<mlir::omp::WsloopOp>(op)) ||
+        inlineSimpleOp(mlir::dyn_cast<mlir::omp::DistributeOp>(op)))
       return mlir::success();
 
     op->emitOpError("left unhandled after SimdOnly pass.");
@@ -515,7 +204,7 @@ public:
 
     if (mlir::failed(
             mlir::applyPatternsGreedily(module, std::move(patterns), config))) {
-      mlir::emitError(module.getLoc(), "error in simd-only conversion pass");
+      mlir::emitError(module.getLoc(), "Error in SimdOnly conversion pass");
       signalPassFailure();
     }
   }
