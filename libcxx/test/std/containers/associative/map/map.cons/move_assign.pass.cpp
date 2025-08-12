ee0a6b5bf38a363a41687148d97156acd8a0d98e
Vinay Deshmukh <32487576+vinay-deshmukh@users.noreply.github.com>
2025-08-11 20:43:54 -0400

    breaks correctly

--- libcxx/test/std/containers/associative/map/map.cons/move_assign.pass.cpp
+++ libcxx/test/std/containers/associative/map/map.cons/move_assign.pass.cpp
@@ -23,7 +23,7 @@
 #include "test_allocator.h"
 #include "min_allocator.h"
 
-// TEST_CONSTEXPR_CXX26
+TEST_CONSTEXPR_CXX26
  bool test() {
   {
     typedef std::pair<MoveOnly, MoveOnly> V;
@@ -104,7 +104,7 @@ int main(int, char**) {
   assert(test());
 #if TEST_STD_VER >= 26
 // FIXME: Within __tree, it is not allowed to move from a `const MoveOnly` which prevents this from executing during constant evaluation
-//  static_assert(test());
+ static_assert(test());
 #endif
   return 0;
 }
