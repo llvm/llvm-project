0da9984d89a36eeef5960c5c7f4b1e099af47634
harkgill-amd <harkirat.gill@amd.com>
2025-08-12 11:22:02 -0400

    Seperate hipcc and hipconfig into different binaries

--- amd/hipcc/src/hipBin.h
+++ amd/hipcc/src/hipBin.h
@@ -1,25 +1,3 @@
-/*
-Copyright (c) 2021 Advanced Micro Devices, Inc. All rights reserved.
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-*/
-
 #include "hipBin_util.h"
 #include "hipBin_amd.h"
 #include "hipBin_nvidia.h"
@@ -88,21 +66,6 @@ vector<HipBinBase*>& HipBin::getHipBinPtrs() {
   return hipBinBasePtrs_;  // Return the populated device pointers.
 }
 
-
-void HipBin::executeHipBin(string filename, int argc, char* argv[]) {
-  if (hipBinUtilPtr_->substringPresent(filename, "hipconfig")) {
-    executeHipConfig(argc, argv);
-  } else if (hipBinUtilPtr_->substringPresent(filename, "hipcc")) {
-    executeHipCC(argc, argv);
-  } else {
-    std::cerr << "Command " << filename
-    << " not supported. Name the exe as hipconfig"
-    << " or hipcc and then try again ..." << endl;
-    exit(-1);
-  }
-}
-
-
 void HipBin::executeHipCC(int argc, char* argv[]) {
   vector<HipBinBase*>& platformPtrs = getHipBinPtrs();
   vector<string> argvcc;
@@ -113,7 +76,6 @@ void HipBin::executeHipCC(int argc, char* argv[]) {
   // In the near future this vector will contain mulitple devices
   platformPtrs.at(0)->executeHipCCCmd(argvcc);
 }
-
 
 void HipBin::executeHipConfig(int argc, char* argv[]) {
   vector<HipBinBase*>& platformPtrs = getHipBinPtrs();
@@ -158,15 +120,4 @@ void HipBin::executeHipConfig(int argc, char* argv[]) {
       }
     }
   }
-}
-
-//===========================================================================
-//===========================================================================
-
-int main(int argc, char* argv[]) {
-  fs::path filename(argv[0]);
-  filename = filename.filename();
-
-  HipBin hipBin;
-  hipBin.executeHipBin(filename.string(), argc, argv);
 }
---
+++ amd/hipcc/src/hipconfig.cpp
@@ -0,0 +1,6 @@
+#include "hipBin.h"
+
+int main(int argc, char* argv[]){
+    HipBin hipbin;
+    hipbin.executeHipConfig(argc, argv);
+}
