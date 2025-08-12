561c9ee5dea2005341faba9196545437d3c2f190
Jonas Devlieghere <jonas@devlieghere.com>
2025-08-11 22:23:20 -0500

    Revert "[LLDB][NativePDB] Find functions by basename" (#153131)
    Reverts llvm/llvm-project#152295

--- lldb/test/Shell/SymbolFile/NativePDB/find-functions.cpp
+++ lldb/test/Shell/SymbolFile/NativePDB/find-functions.cpp
@@ -6,52 +6,24 @@
 
 // RUN: lldb-test symbols --find=function --name=main --function-flags=full %t.exe \
 // RUN:     | FileCheck %s --check-prefix=FIND-MAIN
-// RUN: lldb-test symbols --find=function --name=main --function-flags=method %t.exe \
-// RUN:     | FileCheck %s --check-prefix=FIND-NO-FUNCTION
-// RUN: lldb-test symbols --find=function --name=main --function-flags=base %t.exe \
-// RUN:     | FileCheck %s --check-prefix=FIND-MAIN
 
 // RUN: lldb-test symbols --find=function --name=static_fn --function-flags=full %t.exe \
-// RUN:     | FileCheck %s --check-prefix=FIND-STATIC
-// RUN: lldb-test symbols --find=function --name=static_fn --function-flags=method %t.exe \
-// RUN:     | FileCheck %s --check-prefix=FIND-NO-FUNCTION
-// RUN: lldb-test symbols --find=function --name=static_fn --function-flags=base %t.exe \
 // RUN:     | FileCheck %s --check-prefix=FIND-STATIC
 
 // RUN: lldb-test symbols --find=function --name=varargs_fn --function-flags=full %t.exe \
 // RUN:     | FileCheck %s --check-prefix=FIND-VAR
-// RUN: lldb-test symbols --find=function --name=varargs_fn --function-flags=method %t.exe \
-// RUN:     | FileCheck %s --check-prefix=FIND-NO-FUNCTION
-// RUN: lldb-test symbols --find=function --name=varargs_fn --function-flags=base %t.exe \
-// RUN:     | FileCheck %s --check-prefix=FIND-VAR
 
 // RUN: lldb-test symbols --find=function --name=Struct::simple_method --function-flags=full %t.exe \
-// RUN:     | FileCheck %s --check-prefix=FIND-SIMPLE
-// RUN: lldb-test symbols --find=function --name=Struct::simple_method --function-flags=method %t.exe \
-// RUN:     | FileCheck %s --check-prefix=FIND-SIMPLE
-// RUN: lldb-test symbols --find=function --name=Struct::simple_method --function-flags=base %t.exe \
 // RUN:     | FileCheck %s --check-prefix=FIND-SIMPLE
 
 // RUN: lldb-test symbols --find=function --name=Struct::virtual_method --function-flags=full %t.exe \
 // RUN:     | FileCheck %s --check-prefix=FIND-VIRTUAL
-// RUN: lldb-test symbols --find=function --name=Struct::virtual_method --function-flags=method %t.exe \
-// RUN:     | FileCheck %s --check-prefix=FIND-VIRTUAL
-// RUN: lldb-test symbols --find=function --name=Struct::virtual_method --function-flags=base %t.exe \
-// RUN:     | FileCheck %s --check-prefix=FIND-VIRTUAL
 
 // RUN: lldb-test symbols --find=function --name=Struct::static_method --function-flags=full %t.exe \
 // RUN:     | FileCheck %s --check-prefix=FIND-STATIC-METHOD
-// RUN: lldb-test symbols --find=function --name=Struct::static_method --function-flags=method %t.exe \
-// RUN:     | FileCheck %s --check-prefix=FIND-NO-FUNCTION
-// RUN: lldb-test symbols --find=function --name=Struct::static_method --function-flags=base %t.exe \
-// RUN:     | FileCheck %s --check-prefix=FIND-STATIC-METHOD
 
 // RUN: lldb-test symbols --find=function --name=Struct::overloaded_method --function-flags=full %t.exe \
-// RUN:     | FileCheck %s --check-prefix=FIND-OVERLOAD-FULL
-// RUN: lldb-test symbols --find=function --name=Struct::overloaded_method --function-flags=method %t.exe \
-// RUN:     | FileCheck %s --check-prefix=FIND-OVERLOAD-METHOD
-// RUN: lldb-test symbols --find=function --name=Struct::overloaded_method --function-flags=base %t.exe \
-// RUN:     | FileCheck %s --check-prefix=FIND-OVERLOAD-BASE
+// RUN:     | FileCheck %s --check-prefix=FIND-OVERLOAD
 
 struct Struct {
   int simple_method() {
@@ -79,28 +51,7 @@ private:
   }
 };
 
-class Class {
-public:
-  bool overloaded_method() {
-    return false;
-  }
-  bool overloaded_method(int i) {
-    return i > 0;
-  }
-  static int overloaded_method(bool b) {
-    return b ? 1 : 2;
-  }
-};
-
-char overloaded_method() {
-  return 0;
-}
-char overloaded_method(int i) {
-  return 0;
-}
-
 Struct s;
-Class c;
 
 static int static_fn() {
   return 42;
@@ -112,15 +63,11 @@ int varargs_fn(int x, int y, ...) {
 
 int main(int argc, char **argv) {
   return static_fn() + varargs_fn(argc, argc) + s.simple_method() +
-  Struct::static_method() + s.virtual_method() + s.overloaded_method() + 
-  Class::overloaded_method(false) + c.overloaded_method(1) + c.overloaded_method() 
-  + overloaded_method() + overloaded_method(1);
+  Struct::static_method() + s.virtual_method() + s.overloaded_method();
 }
 
 // FIND-MAIN:      Function: id = {{.*}}, name = "main"
 // FIND-MAIN-NEXT: FuncType: id = {{.*}}, compiler_type = "int (int, char **)"
-
-// FIND-NO-FUNCTION: Found 0 functions
 
 // FIND-STATIC:      Function: id = {{.*}}, name = "{{.*}}static_fn{{.*}}"
 // FIND-STATIC-NEXT: FuncType: id = {{.*}}, compiler_type = "int (void)"
@@ -137,29 +84,7 @@ int main(int argc, char **argv) {
 // FIND-STATIC-METHOD:      Function: id = {{.*}}, name = "{{.*}}Struct::static_method{{.*}}"
 // FIND-STATIC-METHOD-NEXT: FuncType: id = {{.*}}, compiler_type = "int (void)"
 
-// FIND-OVERLOAD-FULL-NOT: "Class::overloaded_method"
-// FIND-OVERLOAD-FULL-NOT: "overloaded_method"
-// FIND-OVERLOAD-FULL: Function: id = {{.*}}, name = "{{.*}}Struct::overloaded_method{{.*}}"
-// FIND-OVERLOAD-FULL: FuncType: id = {{.*}}, compiler_type = "int (void)"
-// FIND-OVERLOAD-FULL: FuncType: id = {{.*}}, compiler_type = "int (char)"
-// FIND-OVERLOAD-FULL: FuncType: id = {{.*}}, compiler_type = "int (char, int, ...)"
-
-// FIND-OVERLOAD-BASE-DAG: Function: id = {{.*}}, name = "{{.*}}Struct::overloaded_method{{.*}}"
-// FIND-OVERLOAD-BASE-DAG: FuncType: id = {{.*}}, compiler_type = "int (void)"
-// FIND-OVERLOAD-BASE-DAG: FuncType: id = {{.*}}, compiler_type = "int (char)"
-// FIND-OVERLOAD-BASE-DAG: FuncType: id = {{.*}}, compiler_type = "int (char, int, ...)"
-// FIND-OVERLOAD-BASE-DAG: Function: id = {{.*}}, name = "Class::overloaded_method"
-// FIND-OVERLOAD-BASE-DAG: FuncType: id = {{.*}}, compiler_type = "_Bool (void)"
-// FIND-OVERLOAD-BASE-DAG: FuncType: id = {{.*}}, compiler_type = "_Bool (int)"
-// FIND-OVERLOAD-BASE-DAG: FuncType: id = {{.*}}, compiler_type = "int (_Bool)"
-// FIND-OVERLOAD-BASE-DAG: Function: id = {{.*}}, name = "overloaded_method"
-// FIND-OVERLOAD-BASE-DAG: FuncType: id = {{.*}}, compiler_type = "char (void)"
-// FIND-OVERLOAD-BASE-DAG: FuncType: id = {{.*}}, compiler_type = "char (int)"
-
-// FIND-OVERLOAD-METHOD-NOT: "overloaded_method"
-// FIND-OVERLOAD-METHOD-DAG: Function: id = {{.*}}, name = "{{.*}}Struct::overloaded_method{{.*}}"
-// FIND-OVERLOAD-METHOD-DAG: FuncType: id = {{.*}}, compiler_type = "int (void)"
-// FIND-OVERLOAD-METHOD-DAG: FuncType: id = {{.*}}, compiler_type = "int (char)"
-// FIND-OVERLOAD-METHOD-DAG: Function: id = {{.*}}, name = "Class::overloaded_method"
-// FIND-OVERLOAD-METHOD-DAG: FuncType: id = {{.*}}, compiler_type = "_Bool (void)"
-// FIND-OVERLOAD-METHOD-DAG: FuncType: id = {{.*}}, compiler_type = "_Bool (int)"
+// FIND-OVERLOAD: Function: id = {{.*}}, name = "{{.*}}Struct::overloaded_method{{.*}}"
+// FIND-OVERLOAD: FuncType: id = {{.*}}, compiler_type = "int (void)"
+// FIND-OVERLOAD: FuncType: id = {{.*}}, compiler_type = "int (char)"
+// FIND-OVERLOAD: FuncType: id = {{.*}}, compiler_type = "int (char, int, ...)"
