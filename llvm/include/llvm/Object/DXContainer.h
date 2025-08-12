ee9d4dfe8eefee8811e5df7deeb12272ef3214c6
Chris B <chris.bieneman@me.com>
2025-08-12 17:00:14 -0500

    [DirectX][objdump] Add support for printing signatures (#152531)
    This adds support for printing the signature sections as part of the
    `-p` flag for printing private headers.
    
    The formatting aims to roughly match the formatting used by DXC's
    `/dumpbin` flag.
    
    Resolves #152380.

--- llvm/include/llvm/Object/DXContainer.h
+++ llvm/include/llvm/Object/DXContainer.h
@@ -603,6 +603,8 @@ private:
   }
 
 public:
+  const DXContainer &getDXContainer() const { return Container; }
+
   static bool classof(const Binary *v) { return v->isDXContainer(); }
 
   Expected<StringRef> getSymbolName(DataRefImpl) const override;
