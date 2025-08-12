1a052a38b0204af49f85e3532fbdcec5472e3d63
Krishna Pandey <kpandey81930@gmail.com>
2025-08-12 19:09:55 +0530

    fix: typo div->add in bf16add_test
    Signed-off-by: Krishna Pandey <kpandey81930@gmail.com>

--- libc/test/src/math/smoke/bf16add_test.cpp
+++ libc/test/src/math/smoke/bf16add_test.cpp
@@ -1,4 +1,4 @@
-//===-- Unittests for bf16div ---------------------------------------------===//
+//===-- Unittests for bf16add ---------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,9 +6,9 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "DivTest.h"
+#include "AddTest.h"
 
 #include "src/__support/FPUtil/bfloat16.h"
-#include "src/math/bf16div.h"
+#include "src/math/bf16add.h"
 
-LIST_DIV_TESTS(bfloat16, double, LIBC_NAMESPACE::bf16div)
+LIST_ADD_TESTS(bfloat16, double, LIBC_NAMESPACE::bf16add)
