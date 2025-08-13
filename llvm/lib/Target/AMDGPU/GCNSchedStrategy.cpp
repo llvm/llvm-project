84ce4ee3be546a7e4dbc0b72b9e062315966c6b7
Lucas Ramirez <lucas.rami@proton.me>
2025-08-13 13:19:01 +0000

    Remove REMAT_DEBUG and break ties in score

--- llvm/lib/Target/AMDGPU/GCNSchedStrategy.cpp
+++ llvm/lib/Target/AMDGPU/GCNSchedStrategy.cpp
@@ -1104,16 +1104,16 @@ bool ClusteredLowOccStage::initGCNSchedStage() {
 void PreRARematStage::printTargetRegions(bool PrintAll) const {
   if (PrintAll) {
     for (auto [I, Target] : enumerate(RPTargets))
-      REMAT_DEBUG(dbgs() << "  [" << I << "] " << Target << '\n');
+      dbgs() << REMAT_PREFIX << "  [" << I << "] " << Target << '\n';
     return;
   }
   if (TargetRegions.none()) {
-    REMAT_DEBUG(dbgs() << "No target regions\n");
+    dbgs() << REMAT_PREFIX << "No target regions\n";
     return;
   }
-  REMAT_DEBUG(dbgs() << "Target regions:\n");
+  dbgs() << REMAT_PREFIX << "Target regions:\n";
   for (unsigned I : TargetRegions.set_bits())
-    REMAT_DEBUG(dbgs() << "  [" << I << "] " << RPTargets[I] << '\n');
+    dbgs() << REMAT_PREFIX << "  [" << I << "] " << RPTargets[I] << '\n';
 }
 
 void PreRARematStage::RematReg::print(
@@ -1229,8 +1229,8 @@ bool PreRARematStage::initGCNSchedStage() {
   }
   unsetSatisifedRPTargets(RescheduleRegions);
 
+  LLVM_DEBUG(printTargetRegions());
 #ifndef NDEBUG
-  printTargetRegions();
   unsigned RoundNum = 0;
 #endif
 
@@ -1241,7 +1241,7 @@ bool PreRARematStage::initGCNSchedStage() {
     // (Re-)Score and (re-)sort all remats in increasing score order.
     for (ScoredRemat &Remat : ScoredRemats)
       Remat.update(TargetRegions, RPTargets, RegionFreq, !TargetOcc);
-    stable_sort(ScoredRemats);
+    sort(ScoredRemats);
 
     REMAT_DEBUG({
       dbgs() << "==== ROUND " << RoundNum << " ====\n";
@@ -1293,8 +1293,8 @@ bool PreRARematStage::initGCNSchedStage() {
       unsetSatisifedRPTargets(Remat.Live);
     }
 
+    LLVM_DEBUG(printTargetRegions());
 #ifndef NDEBUG
-    printTargetRegions();
     ++RoundNum;
 #endif
 
