82452dd64ba6f7d4396b8a2f3fdefbe6aaac32bc
Catherine Moore <catherine.moore@amd.com>
2025-08-14 17:26:13 -0500

    Update printf statement with missing argument.

--- openmp/libompd/gdb-plugin/ompdModule.c
+++ openmp/libompd/gdb-plugin/ompdModule.c
@@ -967,7 +967,7 @@ static PyObject *call_ompd_get_task_parallel_handle(PyObject *self,
 
   if (retVal != ompd_rc_ok) {
     _printf("An error occurred when calling ompd_get_task_parallel_handle! "
-            "Error code: %d");
+            "Error code: %d", retVal);
     return Py_BuildValue("l", retVal);
   }
   return PyCapsule_New(taskParallelHandle, "ParallelHandle",
