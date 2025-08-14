6cd84865703310651f910f5f467204a354f49715
Shafik Yaghmour <shafik.yaghmour@intel.com>
2025-08-13 19:46:09 -0700

    [Clang][NFC] Clarify some SourceManager related code
    Static analysis flagged the columns - 1 code, it was correct but the assumption
    was not obvious. I document the assumption w/ assertions.
    
    While digging through related code I found getColumnNumber that looks wrong
    at first inspection and adding parentheses makes it clearer.

--- clang/lib/Frontend/TextDiagnostic.cpp
+++ clang/lib/Frontend/TextDiagnostic.cpp
@@ -1095,6 +1095,8 @@ prepareAndFilterRanges(const SmallVectorImpl<CharSourceRange> &Ranges,
 
     unsigned StartColumn = SM.getExpansionColumnNumber(Begin);
     unsigned EndColumn = SM.getExpansionColumnNumber(End);
+    assert(StartColumn && "StartColumn has a value of 0");
+    assert(EndColumn && "EndColumn has a value of 0");
     if (R.isTokenRange())
       EndColumn += Lexer::MeasureTokenLength(End, SM, LangOpts);
 
