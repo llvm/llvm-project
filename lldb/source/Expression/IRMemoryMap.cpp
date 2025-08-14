36dcebb09eecd46f649feb4073c4f10b75423699
Felipe de Azevedo Piovezan <fpiovezan@apple.com>
2025-08-14 08:22:44 -0700

    fixup! Improve comment

--- lldb/source/Expression/IRMemoryMap.cpp
+++ lldb/source/Expression/IRMemoryMap.cpp
@@ -641,7 +641,8 @@ void IRMemoryMap::WritePointerToMemory(lldb::addr_t process_address,
   error.Clear();
 
   /// Only ask the Process to fix the address if this address belongs to the
-  /// process (host allocations are stored in m_data).
+  /// process. An address belongs to the process if the Allocation contains a
+  /// non-empty m_data member.
   if (auto it = FindAllocation(process_address, 1);
       it != m_allocations.end() && it->second.m_data.GetByteSize() == 0)
     if (auto process_sp = GetProcessWP().lock())
