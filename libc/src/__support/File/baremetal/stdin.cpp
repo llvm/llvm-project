9f1846fdb83c3baef6492818a9092d3a16a965c8
William Huynh <William.Huynh@arm.com>
2025-08-14 16:13:41 +0100

    Apply suggestions from @mysterymath

--- libc/src/__support/File/baremetal/stdin.cpp
+++ libc/src/__support/File/baremetal/stdin.cpp
@@ -1,4 +1,4 @@
-//===--- Definition of baremetal stderr -----------------------------------===//
+//===--- Definition of baremetal stdin ------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -14,21 +14,21 @@
 
 namespace LIBC_NAMESPACE_DECL {
 
-cookie_io_functions_t io_func = {.read = nullptr,
-                                 .write = __llvm_libc_stdio_write,
+cookie_io_functions_t io_func = {.read = __llvm_libc_stdio_read,
+                                 .write = nullptr,
                                  .seek = nullptr,
                                  .close = nullptr};
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wglobal-constructors"
 // Buffering is implementation defined. Therefore to save RAM, we use no
 // buffering
-static CookieFile StdErr(&__llvm_libc_stderr_cookie, io_func, nullptr, 0,
-                         _IONBF, File::mode_flags("w"));
+static CookieFile StdIn(&__llvm_libc_stdin_cookie, io_func, nullptr, 0, _IONBF,
+                        File::mode_flags("r"));
 #pragma clang diagnostic pop
-File *stderr = &StdErr;
+File *stdin = &StdIn;
 
 } // namespace LIBC_NAMESPACE_DECL
 
 extern "C" {
-FILE *stderr = reinterpret_cast<FILE *>(&LIBC_NAMESPACE::StdErr);
+FILE *stdin = reinterpret_cast<FILE *>(&LIBC_NAMESPACE::StdIn);
 }
--- libc/src/__support/File/baremetal/stdout.cpp
+++ libc/src/__support/File/baremetal/stdout.cpp
@@ -1,4 +1,4 @@
-//===--- Definition of baremetal stderr -----------------------------------===//
+//===--- Definition of baremetal stdout -----------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -22,13 +22,13 @@ cookie_io_functions_t io_func = {.read = nullptr,
 #pragma clang diagnostic ignored "-Wglobal-constructors"
 // Buffering is implementation defined. Therefore to save RAM, we use no
 // buffering
-static CookieFile StdErr(&__llvm_libc_stderr_cookie, io_func, nullptr, 0,
+static CookieFile StdOut(&__llvm_libc_stdout_cookie, io_func, nullptr, 0,
                          _IONBF, File::mode_flags("w"));
 #pragma clang diagnostic pop
-File *stderr = &StdErr;
+File *stdout = &StdOut;
 
 } // namespace LIBC_NAMESPACE_DECL
 
 extern "C" {
-FILE *stderr = reinterpret_cast<FILE *>(&LIBC_NAMESPACE::StdErr);
+FILE *stdout = reinterpret_cast<FILE *>(&LIBC_NAMESPACE::StdOut);
 }
