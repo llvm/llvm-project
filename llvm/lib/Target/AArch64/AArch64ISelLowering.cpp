2ac80d09a4a9f18dcca563308fc50a45bccaa974
AZero13 <gfunni234@gmail.com>
2025-08-15 15:35:47 -0400

    [AArch64] Fold CSEL a, b, cc, SUBS(ADD(x,y), 0) -> CSEL a, b, cc, ADDS(x,y)
    As long as we don't use overflow flags, we should be good.
    
    In fact, this is more likely to happen than having a comparison where you have x - y < 0 because LLVM tends to canonicalize this to x < y when possible, but it does not do the same with
    x + y < 0 -> x < -y.

--- llvm/lib/Target/AArch64/AArch64ISelLowering.cpp
+++ llvm/lib/Target/AArch64/AArch64ISelLowering.cpp
@@ -25486,25 +25486,31 @@ static SDValue performCSELCombine(SDNode *N,
     }
   }
 
-  // CSEL a, b, cc, SUBS(SUB(x,y), 0) -> CSEL a, b, cc, SUBS(x,y) if cc doesn't
-  // use overflow flags, to avoid the comparison with zero. In case of success,
-  // this also replaces the original SUB(x,y) with the newly created SUBS(x,y).
-  // NOTE: Perhaps in the future use performFlagSettingCombine to replace SUB
-  // nodes with their SUBS equivalent as is already done for other flag-setting
+  // CSEL a, b, cc, SUBS(SUB(x,y), 0) -> CSEL a, b, cc, SUBS(x,y) or // CSEL a,
+  // b, cc, SUBS(ADD(x,y), 0) -> CSEL a, b, cc, ADDS(x,y) if cc doesn't use
+  // overflow flags, to avoid the comparison with zero. In case of success, this
+  // also replaces the original SUB(x,y) with the newly created SUBS(x,y). NOTE:
+  // Perhaps in the future use performFlagSettingCombine to replace SUB nodes
+  // with their SUBS equivalent as is already done for other flag-setting
   // operators, in which case doing the replacement here becomes redundant.
   if (Cond.getOpcode() == AArch64ISD::SUBS && Cond->hasNUsesOfValue(1, 1) &&
       isNullConstant(Cond.getOperand(1))) {
-    SDValue Sub = Cond.getOperand(0);
+    SDValue SubOrAdd = Cond.getOperand(0);
     AArch64CC::CondCode CC =
         static_cast<AArch64CC::CondCode>(N->getConstantOperandVal(2));
-    if (Sub.getOpcode() == ISD::SUB &&
-        (CC == AArch64CC::EQ || CC == AArch64CC::NE || CC == AArch64CC::MI ||
-         CC == AArch64CC::PL)) {
+    if ((SubOrAdd.getOpcode() == ISD::SUB ||
+         SubOrAdd.getOpcode() == ISD::ADD) &&
+        ((CC == AArch64CC::EQ || CC == AArch64CC::NE || CC == AArch64CC::MI ||
+          CC == AArch64CC::PL))) {
       SDLoc DL(N);
-      SDValue Subs = DAG.getNode(AArch64ISD::SUBS, DL, Cond->getVTList(),
-                                 Sub.getOperand(0), Sub.getOperand(1));
-      DCI.CombineTo(Sub.getNode(), Subs);
-      DCI.CombineTo(Cond.getNode(), Subs, Subs.getValue(1));
+
+      SDValue Result =
+          DAG.getNode((SubOrAdd.getOpcode() == ISD::SUB ? AArch64ISD::SUBS
+                                                        : AArch64ISD::ADDS),
+                      DL, Cond->getVTList(), SubOrAdd.getOperand(0),
+                      SubOrAdd.getOperand(1));
+      DCI.CombineTo(SubOrAdd.getNode(), Result);
+      DCI.CombineTo(Cond.getNode(), Result, Result.getValue(1));
       return SDValue(N, 0);
     }
   }
