0b0cb2e158a17dbf9b762bb7627daeda3ecdc2d9
Yang Bai <yangb@nvidia.com>
2025-08-15 03:12:45 -0700

    move UnrollFromElements to a separate file

--- mlir/lib/Dialect/Vector/Transforms/LowerVectorToFromElementsToShuffleTree.cpp
+++ mlir/lib/Dialect/Vector/Transforms/LowerVectorToFromElementsToShuffleTree.cpp
@@ -735,52 +735,10 @@ struct LowerVectorToFromElementsToShuffleTreePass
   }
 };
 
-/// Unrolls 2 or more dimensional `vector.from_elements` ops by unrolling the
-/// outermost dimension. For example:
-/// ```
-/// %v = vector.from_elements %e0, %e1, %e2, %e3, %e4, %e5 : vector<2x3xf32>
-///
-/// ==>
-///
-/// %0   = ub.poison : vector<2x3xf32>
-/// %v0  = vector.from_elements %e0, %e1, %e2 : vector<3xf32>
-/// %1   = vector.insert %v0, %0 [0] : vector<3xf32> into vector<2x3xf32>
-/// %v1  = vector.from_elements %e3, %e4, %e5 : vector<3xf32>
-/// %v   = vector.insert %v1, %1 [1] : vector<3xf32> into vector<2x3xf32>
-/// ```
-///
-/// When applied exhaustively, this will produce a sequence of 1-d from_elements
-/// ops.
-struct UnrollFromElements : OpRewritePattern<vector::FromElementsOp> {
-  using OpRewritePattern::OpRewritePattern;
-
-  LogicalResult matchAndRewrite(vector::FromElementsOp op,
-                                PatternRewriter &rewriter) const override {
-    ValueRange allElements = op.getElements();
-
-    auto unrollFromElementsFn = [&](PatternRewriter &rewriter, Location loc,
-                                    VectorType subTy, int64_t index) {
-      size_t subTyNumElements = subTy.getNumElements();
-      assert((index + 1) * subTyNumElements <= allElements.size() &&
-             "out of bounds");
-      ValueRange subElements =
-          allElements.slice(index * subTyNumElements, subTyNumElements);
-      return vector::FromElementsOp::create(rewriter, loc, subTy, subElements);
-    };
-
-    return unrollVectorOp(op, rewriter, unrollFromElementsFn);
-  }
-};
-
 } // namespace
 
 void mlir::vector::populateVectorToFromElementsToShuffleTreePatterns(
     RewritePatternSet &patterns, PatternBenefit benefit) {
   patterns.add<ToFromElementsToShuffleTreeRewrite>(patterns.getContext(),
                                                    benefit);
-}
-
-void mlir::vector::populateVectorFromElementsLoweringPatterns(
-    RewritePatternSet &patterns, PatternBenefit benefit) {
-  patterns.add<UnrollFromElements>(patterns.getContext(), benefit);
 }
