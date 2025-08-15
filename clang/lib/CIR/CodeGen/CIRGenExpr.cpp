d21ca8ce12cbc1d9c68a2386b553851b2a377a33
Morris Hafner <mhafner@nvidia.com>
2025-08-15 19:59:21 +0200

    revert line change

--- clang/lib/CIR/CodeGen/CIRGenExpr.cpp
+++ clang/lib/CIR/CodeGen/CIRGenExpr.cpp
@@ -1178,7 +1178,8 @@ static void pushTemporaryCleanup(CIRGenFunction &cgf,
     if (const auto *classDecl = dyn_cast<CXXRecordDecl>(
             rt->getOriginalDecl()->getDefinitionOrSelf())) {
       if (!classDecl->hasTrivialDestructor())
-        referenceTemporaryDtor = classDecl->getDestructor();
+        referenceTemporaryDtor =
+            classDecl->getDefinitionOrSelf()->getDestructor();
     }
   }
 
