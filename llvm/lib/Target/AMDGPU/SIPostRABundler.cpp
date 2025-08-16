ff531eaa27636094a967c5f50460904520830663
Yoonseo Choi <Yoonseo.Choi@amd.com>
2025-08-15 21:57:14 -0500

    Update lit-tests

--- llvm/lib/Target/AMDGPU/SIPostRABundler.cpp
+++ llvm/lib/Target/AMDGPU/SIPostRABundler.cpp
@@ -190,7 +190,7 @@ bool SIPostRABundler::run(MachineFunction &MF) {
           if (I->getNumExplicitDefs() != 0)
             Defs.insert(I->defs().begin()->getReg());
           ++ClauseLength;
-        } else if (!!I->isMetaInstruction()) {
+        } else if (!I->isMetaInstruction()) {
           // Allow meta instructions in between bundle candidates, but do not
           // start or end a bundle on one.
           //
