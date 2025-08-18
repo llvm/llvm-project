bc19ce5c1d436eb7ab0d87293313bfdcc4400172
Jonas Devlieghere <jonas@devlieghere.com>
2025-08-18 15:53:12 -0500

    Revert "[lldb] Relax the error message in TestProcessCrashInfo.py" (#154197)
    Reverts llvm/llvm-project#153653 because older versions of macOS do not
    use the same prefix.

--- lldb/test/API/functionalities/process_crash_info/TestProcessCrashInfo.py
+++ lldb/test/API/functionalities/process_crash_info/TestProcessCrashInfo.py
@@ -38,7 +38,7 @@ class PlatformProcessCrashInfoTestCase(TestBase):
             patterns=[
                 "Extended Crash Information",
                 "Crash-Info Annotations",
-                "BUG IN CLIENT OF LIBMALLOC",
+                "pointer being freed was not allocated",
             ],
         )
 
@@ -67,7 +67,7 @@ class PlatformProcessCrashInfoTestCase(TestBase):
 
         self.assertTrue(crash_info.IsValid())
 
-        self.assertIn("BUG IN CLIENT OF LIBMALLOC", stream.GetData())
+        self.assertIn("pointer being freed was not allocated", stream.GetData())
 
     # dyld leaves permanent crash_info records when testing on device.
     @skipIfDarwinEmbedded
