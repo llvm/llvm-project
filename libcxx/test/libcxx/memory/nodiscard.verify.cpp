b88cad5f89c0e312c29fc685ee374692a6038f44
lukasx999 <116069013+lukasx999@users.noreply.github.com>
2025-08-18 20:30:10 +0200

    added missing test cases

--- libcxx/test/libcxx/memory/nodiscard.verify.cpp
+++ libcxx/test/libcxx/memory/nodiscard.verify.cpp
@@ -9,25 +9,31 @@
 // check that functions are marked [[nodiscard]] as an extension in C++17
 
 // [[nodiscard]] std::make_unique(Args&&...);
-// [[nodiscard]] std::make_shared(Args&&...);
-//
 // [[nodiscard]] std::make_unique(size_t);
-// [[nodiscard]] std::make_shared(size_t);
-//
 // [[nodiscard]] std::make_unique_for_overwrite();
+// [[nodiscard]] std::make_unique_for_overwrite(size_t);
+
+// [[nodiscard]] std::make_shared(Args&&...);
+// [[nodiscard]] std::make_shared(size_t);
 // [[nodiscard]] std::make_shared_for_overwrite();
+// [[nodiscard]] std::make_shared_for_overwrite(size_t);
+// [[nodiscard]] std::make_shared(size_t, const remove_extent_t<_Tp>&);
+// [[nodiscard]] std::make_shared(const remove_extent_t<_Tp>&);
 
 #include <memory>
 
 void f() {
 
     std::make_unique<int>(1); // expected-warning {{ignoring return value of function}}
-    std::make_shared<int>(1); // expected-warning {{ignoring return value of function}}
-
     std::make_unique<int[]>(5); // expected-warning {{ignoring return value of function}}
-    std::make_shared<int[]>(5); // expected-warning {{ignoring return value of function}}
-
     std::make_unique_for_overwrite<int>(); // expected-warning {{ignoring return value of function}}
+    std::make_unique_for_overwrite<int[]>(5); // expected-warning {{ignoring return value of function}}
+
+    std::make_shared<int>(1); // expected-warning {{ignoring return value of function}}
+    std::make_shared<int[]>(5); // expected-warning {{ignoring return value of function}}
     std::make_shared_for_overwrite<int>(); // expected-warning {{ignoring return value of function}}
+    std::make_shared_for_overwrite<int[]>(5); // expected-warning {{ignoring return value of function}}
+    std::make_shared<int[]>(5, 1); // expected-warning {{ignoring return value of function}}
+    std::make_shared<int[]>(1); // expected-warning {{ignoring return value of function}}
 
 }
