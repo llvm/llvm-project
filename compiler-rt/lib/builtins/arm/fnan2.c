f4447a1f83c4b20a662bbf0c30fa80ed1627b9ff
Simon Tatham <simon.tatham@arm.com>
2025-08-18 17:13:59 +0100

    Add comments in fnan2

--- compiler-rt/lib/builtins/arm/fnan2.c
+++ compiler-rt/lib/builtins/arm/fnan2.c
@@ -16,14 +16,21 @@
 //===----------------------------------------------------------------------===//
 
 unsigned __fnan2(unsigned a, unsigned b) {
+  // Make shifted-left copies of a and b to discard the sign bit. Then add 1 at
+  // the bit position where the quiet vs signalling bit ended up. This squashes
+  // all the signalling NaNs to the top of the range of 32-bit values, from
+  // 0xff800001 to 0xffffffff inclusive; meanwhile, all the quiet NaN values
+  // wrap round to the bottom, from 0 to 0x007fffff inclusive. So we can detect
+  // a signalling NaN by asking if it's greater than 0xff800000, and a quiet
+  // one by asking if it's less than 0x00800000.
   unsigned aadj = (a << 1) + 0x00800000;
   unsigned badj = (b << 1) + 0x00800000;
-  if (aadj > 0xff800000)
-    return a | 0x00400000;
-  if (badj > 0xff800000)
-    return b | 0x00400000;
-  if (aadj < 0x00800000)
-    return a;
+  if (aadj > 0xff800000)   // a is a signalling NaN?
+    return a | 0x00400000; //   if so, return it with the quiet bit set
+  if (badj > 0xff800000)   // b is a signalling NaN?
+    return b | 0x00400000; //   if so, return it with the quiet bit set
+  if (aadj < 0x00800000)   // a is a quiet NaN?
+    return a;              // if so, return it
   else /* expect (badj < 0x00800000) */
-    return b;
+    return b;              // in that case b must be a quiet NaN
 }
