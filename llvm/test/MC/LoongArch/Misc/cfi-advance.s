03a5a0a2796c381426ca22adab89840eb7b90389
ZhaoQi <zhaoqi01@loongson.cn>
2025-08-18 14:54:27 +0800

    [LoongArch][MC] Refine conditions for emitting ALIGN relocations (#153365)
    According to the suggestions in
    https://github.com/llvm/llvm-project/pull/150816, this commit refine the
    conditions for emitting R_LARCH_ALIGN relocations.
    
    Some existing tests are updated to avoid being affected by this
    optimization. New tests are added to verify: removal of redundant ALIGN
    relocations, ALIGN emitted after the first linker-relaxable instruction,
    and conservatively emitted ALIGN in lower-numbered subsections.

--- llvm/test/MC/LoongArch/Misc/cfi-advance.s
+++ llvm/test/MC/LoongArch/Misc/cfi-advance.s
@@ -5,13 +5,13 @@
 # RUN:     | llvm-readobj -r - | FileCheck --check-prefix=RELAX %s
 
 # RELOC:       Relocations [
-# RELOC-NEXT:    .rela.eh_frame {
+# RELOC:         .rela.eh_frame {
 # RELOC-NEXT:       0x1C R_LARCH_32_PCREL .text 0x0
 # RELOC-NEXT:    }
 # RELOC-NEXT:  ]
-# DWARFDUMP:       DW_CFA_advance_loc: 4
+# DWARFDUMP:       DW_CFA_advance_loc: 8
 # DWARFDUMP-NEXT:  DW_CFA_def_cfa_offset: +8
-# DWARFDUMP-NEXT:  DW_CFA_advance_loc: 8
+# DWARFDUMP-NEXT:  DW_CFA_advance_loc: 4
 # DWARFDUMP-NEXT:  DW_CFA_def_cfa_offset: +8
 
 # RELAX:       Relocations [
@@ -19,6 +19,8 @@
 # RELAX-NEXT:       0x1C R_LARCH_32_PCREL .L{{.*}} 0x0
 # RELAX-NEXT:       0x20 R_LARCH_ADD32 .L{{.*}} 0x0
 # RELAX-NEXT:       0x20 R_LARCH_SUB32 .L{{.*}} 0x0
+# RELAX-NEXT:       0x25 R_LARCH_ADD6 .L{{.*}} 0x0
+# RELAX-NEXT:       0x25 R_LARCH_SUB6 .L{{.*}} 0x0
 # RELAX-NEXT:       0x28 R_LARCH_ADD6 .L{{.*}} 0x0
 # RELAX-NEXT:       0x28 R_LARCH_SUB6 .L{{.*}} 0x0
 # RELAX-NEXT:    }
@@ -30,7 +32,7 @@
         .type   test,@function
 test:
         .cfi_startproc
-        nop
+        call36 foo
         .cfi_def_cfa_offset 8
         .p2align 3
         nop
