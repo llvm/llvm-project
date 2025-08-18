5e2cf58ae35a32582c69b0884b5f9f2be3cc02ca
Matthias Springer <me@m-sp.org>
2025-08-18 10:23:26 +0200

    [mlir][builtin] Make `unrealized_conversion_cast` inlineable (#139722)
    Until now, `builtin.unrealized_conversion_cast` ops could not be inlined
    by the Inliner pass.

--- mlir/lib/Transforms/Utils/InliningUtils.cpp
+++ mlir/lib/Transforms/Utils/InliningUtils.cpp
@@ -13,6 +13,7 @@
 #include "mlir/Transforms/InliningUtils.h"
 
 #include "mlir/IR/Builders.h"
+#include "mlir/IR/BuiltinOps.h"
 #include "mlir/IR/IRMapping.h"
 #include "mlir/IR/Operation.h"
 #include "mlir/Interfaces/CallInterfaces.h"
@@ -182,6 +183,11 @@ static bool isLegalToInline(InlinerInterface &interface, Region *src,
                             IRMapping &valueMapping) {
   for (auto &block : *src) {
     for (auto &op : block) {
+      // UnrealizedConversionCastOp is inlineable but cannot implement the
+      // inliner interface due to layering constraints.
+      if (isa<UnrealizedConversionCastOp>(op))
+        continue;
+
       // Check this operation.
       if (!interface.isLegalToInline(&op, insertRegion,
                                      shouldCloneInlinedRegion, valueMapping)) {
