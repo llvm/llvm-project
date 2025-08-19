3b6592ce5de7bc04195ec17163e788b8ff463ca1
Jacob Lalonde <jalalonde@fb.com>
2025-08-19 16:46:22 -0700

    Make tpidr a generic thread pointer register on aarch

--- lldb/source/Plugins/Process/Utility/RegisterInfoPOSIX_arm64.cpp
+++ lldb/source/Plugins/Process/Utility/RegisterInfoPOSIX_arm64.cpp
@@ -79,7 +79,8 @@ static lldb_private::RegisterInfo g_register_infos_mte[] = {
     DEFINE_EXTENSION_REG(mte_ctrl)};
 
 static lldb_private::RegisterInfo g_register_infos_tls[] = {
-    DEFINE_EXTENSION_REG(tpidr),
+    {"tpidr", nullptr, 8, 0, lldb::eEncodingUint, lldb::eFormatHex,
+     {LLDB_INVALID_REGNUM, LLDB_INVALID_REGNUM, LLDB_REGNUM_GENERIC_TP}, nullptr, nullptr, nullptr},
     // Only present when SME is present
     DEFINE_EXTENSION_REG(tpidr2)};
 
