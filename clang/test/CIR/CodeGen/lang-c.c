f11b73044b5a02ae60ce18761443424b7ca32be3
seven-mile <i@7li.moe>
2025-08-19 04:51:47 +0000

    combine tests

--- clang/test/CIR/CodeGen/lang-c.c
+++
@@ -1,8 +0,0 @@
-// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fclangir -emit-cir %s -o %t.cir
-// RUN: FileCheck --check-prefix=CIR --input-file=%t.cir %s
-
-// CIR: module attributes {{{.*}}cir.lang = #cir.lang<c>{{.*}}}
-
-int main() {
-  return 0;
-}
