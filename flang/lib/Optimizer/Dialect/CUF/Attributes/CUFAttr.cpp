a1dec1e3cb5346400e7939f180214b41a5efef39
Valentin Clement <clementval@gmail.com>
2025-08-20 10:53:17 -0700

    [flang][cuda][NFC] Add getDataAttr helper

--- flang/lib/Optimizer/Dialect/CUF/Attributes/CUFAttr.cpp
+++ flang/lib/Optimizer/Dialect/CUF/Attributes/CUFAttr.cpp
@@ -30,19 +30,24 @@ void CUFDialect::registerAttributes() {
                 LaunchBoundsAttr, ProcAttributeAttr>();
 }
 
-bool hasDataAttr(mlir::Operation *op, cuf::DataAttribute value) {
+cuf::DataAttributeAttr getDataAttr(mlir::Operation *op) {
   if (!op)
-    return false;
+    return {};
 
-  cuf::DataAttributeAttr dataAttr =
-      op->getAttrOfType<cuf::DataAttributeAttr>(cuf::getDataAttrName());
+  if (auto dataAttr = op->getAttrOfType<cuf::DataAttributeAttr>(cuf::getDataAttrName()))
+    return dataAttr;
+
   // When the attribute is declared on the operation, it doesn't have a prefix.
-  if (!dataAttr)
-    dataAttr = op->getAttrOfType<cuf::DataAttributeAttr>(cuf::dataAttrName);
-  if (!dataAttr)
-    return false;
+  if (auto dataAttr = op->getAttrOfType<cuf::DataAttributeAttr>(cuf::dataAttrName))
+    return dataAttr;
+    
+  return {};
+}
 
-  return dataAttr.getValue() == value;
+bool hasDataAttr(mlir::Operation *op, cuf::DataAttribute value) {
+  if (auto dataAttr = getDataAttr(op))
+    return dataAttr.getValue() == value;
+  return false;
 }
 
 } // namespace cuf
