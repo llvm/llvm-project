450a5d7310e06294c89eb84afb42a398cce25836
Eugene Epshteyn <eepshteyn@nvidia.com>
2025-08-19 21:49:06 -0400

    WIP: new API MayNeedCopy()

--- flang/include/flang/Evaluate/call.h
+++ flang/include/flang/Evaluate/call.h
@@ -110,6 +110,7 @@ public:
 
   std::optional<DynamicType> GetType() const;
   int Rank() const;
+  bool IsArray() const ;
   bool operator==(const ActualArgument &) const;
   llvm::raw_ostream &AsFortran(llvm::raw_ostream &) const;
 
