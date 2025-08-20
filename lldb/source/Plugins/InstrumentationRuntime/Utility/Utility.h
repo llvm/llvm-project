e28ba6b6f65ca52dfac56c8c10a93a3e05dfdb65
Julian Lettner <yln@users.noreply.github.com>
2025-08-20 14:33:27 -0700

    [lldb] Fix source line annotations for libsanitizers traces (#154247)
    When providing allocation and deallocation traces,
    the ASan compiler-rt runtime already provides call
    addresses (`TracePCType::Calls`).
    
    On Darwin, system sanitizers (libsanitizers)
    provides return address.  It also discards a few
    non-user frames at the top of the stack, because
    these internal libmalloc/libsanitizers stack
    frames do not provide any value when diagnosing
    memory errors.
    
    Introduce and add handling for
    `TracePCType::ReturnsNoZerothFrame` to cover this
    case and enable libsanitizers traces line-level
    testing.
    
    rdar://157596927
    
    ---
    Commit 1 is a mechanical refactoring to introduce
    and adopt `TracePCType` enum to replace
    `pcs_are_call_addresses` bool.  It preserve the
    current behavior:
    ```
    pcs_are_call_addresses:
      false  ->  TracePCType::Returns (default)
      true   ->  TracePCType::Calls
    ```
    
    Best reviewed commit by commit.
    
    (cherry picked from commit 484d0408f9d448ca57e96827bf8042acab999e38)

--- lldb/source/Plugins/InstrumentationRuntime/Utility/Utility.h
+++ lldb/source/Plugins/InstrumentationRuntime/Utility/Utility.h
@@ -9,18 +9,20 @@
 #ifndef LLDB_SOURCE_PLUGINS_INSTRUMENTATIONRUNTIME_UTILITY_UTILITY_H
 #define LLDB_SOURCE_PLUGINS_INSTRUMENTATIONRUNTIME_UTILITY_UTILITY_H
 
+#include <tuple>
+
 #include "lldb/lldb-forward.h"
+#include "lldb/lldb-private-enumerations.h"
 
 namespace lldb_private {
-
-class Target;
 
 /// On Darwin, if LLDB loaded libclang_rt, it's coming from a locally built
 /// compiler-rt, and we should prefer it in favour of the system sanitizers
 /// when running InstrumentationRuntime utility expressions that use symbols
 /// from the sanitizer libraries. This helper searches the target for such a
 /// dylib. Returns nullptr if no such dylib was found.
-lldb::ModuleSP GetPreferredAsanModule(const Target &target);
+std::tuple<lldb::ModuleSP, HistoryPCType>
+GetPreferredAsanModule(const Target &target);
 
 } // namespace lldb_private
 
