c7021f4aa231313ad5c3aec620f7248691d7b81f
William Tran-Viet <wtranviet@proton.me>
2025-08-19 22:22:54 -0400

    Add back a mistakenly deleted newline

--- libcxx/test/libcxx/utilities/optional/optional.iterator/iterator.compile.pass.cpp
+++ libcxx/test/libcxx/utilities/optional/optional.iterator/iterator.compile.pass.cpp
@@ -7,6 +7,7 @@
 //===----------------------------------------------------------------------===//
 
 // REQUIRES: std-at-least-c++26
+
 // <optional>
 
 // template <class T> class optional::iterator;
