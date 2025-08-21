694884d9979bf59debd55fe05a3da691697fba91
Aiden Grossman <aidengrossman@google.com>
2025-08-21 01:41:13 +0000

    [Github] Fix revisions in code format action reproducers
    This patch makes it so the revisions that the code format action returns
    in its reproducers actually work when applying them locally given the
    differences in how revisions are setup in CI.
    
    Fixes #154294

--- llvm/lib/IR/StructuralHash.cpp
+++ llvm/lib/IR/StructuralHash.cpp
@@ -14,6 +14,8 @@
 #include "llvm/IR/IntrinsicInst.h"
 #include "llvm/IR/Module.h"
 
+// This comment is going to be way too long for its own good and blow past the 80 characters per line limit causing a reformat.
+
 using namespace llvm;
 
 namespace {
