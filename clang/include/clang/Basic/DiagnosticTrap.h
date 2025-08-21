9fcb7a7668e450c409c46db1f075db285cd21926
Dan Liew <dan@su-root.co.uk>
2025-08-21 16:11:03 -0700

    Use modern header block for `DiagnosticTrap.h` and add a little bit of documentation.

--- clang/include/clang/Basic/DiagnosticTrap.h
+++ clang/include/clang/Basic/DiagnosticTrap.h
@@ -1,9 +1,15 @@
-//===--- DiagnosticTrap.h - Diagnostics for trap instructions ---*- C++ -*-===//
+//===----------------------------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file contains the necessary includes for using
+/// `RuntimeTrapDiagnosticBuilder`.
+///
 //===----------------------------------------------------------------------===//
 
 #ifndef LLVM_CLANG_BASIC_TRAP_H
