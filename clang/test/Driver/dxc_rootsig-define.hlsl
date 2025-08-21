137bd96c6d3c37e8cf1df315a3b6139ea3919f46
Finn Plummer <mail@inbelic.dev>
2025-08-21 12:48:12 -0700

    review: add clang checks

--- clang/test/Driver/dxc_rootsig-define.hlsl
+++ clang/test/Driver/dxc_rootsig-define.hlsl
@@ -2,6 +2,18 @@
 // RUN: %clang_dxc -T cs_6_0 -fcgl -rootsig-define EmptyRS %s | FileCheck %s --check-prefixes=CHECK,EMPTY
 // RUN: %clang_dxc -T cs_6_0 -fcgl -rootsig-define CmdRS -D CmdRS='"SRV(t0)"' %s | FileCheck %s --check-prefixes=CHECK,CMD
 
+// Equivalent clang checks:
+// RUN: %clang -target dxil-unknown-shadermodel6.0-compute -S -emit-llvm -o - %s \
+// RUN:  | FileCheck %s --check-prefixes=CHECK,REG
+
+// RUN: %clang -target dxil-unknown-shadermodel6.0-compute -S -emit-llvm -o - %s \
+// RUN:  -fdx-rootsignature-define=EmptyRS \
+// RUN:  | FileCheck %s --check-prefixes=CHECK,EMPTY
+
+// RUN: %clang -target dxil-unknown-shadermodel6.0-compute -S -emit-llvm -o - %s \
+// RUN:  -fdx-rootsignature-define=CmdRS -D CmdRS='"SRV(t0)"' \
+// RUN:  | FileCheck %s --check-prefixes=CHECK,CMD
+
 #define EmptyRS ""
 #define NotEmptyRS "CBV(b0)"
 
