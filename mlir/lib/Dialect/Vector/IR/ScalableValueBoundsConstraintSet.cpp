7c82b902e7657fc4735b7bd18026534c85d4a451
Mehdi Amini <joker.eph@gmail.com>
2025-08-21 09:53:40 -0700

    [MLIR] Apply clang-tidy fixes for performance-unnecessary-value-param in ScalableValueBoundsConstraintSet.cpp (NFC)

--- mlir/lib/Dialect/Vector/IR/ScalableValueBoundsConstraintSet.cpp
+++ mlir/lib/Dialect/Vector/IR/ScalableValueBoundsConstraintSet.cpp
@@ -43,7 +43,7 @@ FailureOr<ConstantOrScalableBound>
 ScalableValueBoundsConstraintSet::computeScalableBound(
     Value value, std::optional<int64_t> dim, unsigned vscaleMin,
     unsigned vscaleMax, presburger::BoundType boundType, bool closedUB,
-    StopConditionFn stopCondition) {
+    const StopConditionFn &stopCondition) {
   using namespace presburger;
   assert(vscaleMin <= vscaleMax);
 
