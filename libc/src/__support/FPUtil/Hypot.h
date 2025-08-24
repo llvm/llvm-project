7d0656e09205cb9de88b8cbecc510ae7aef6dd2a
Tue Ly <lntue.h@gmail.com>
2025-08-24 21:18:37 +0000

    Omit hypot float16 check when targets do not have float16.

--- libc/src/__support/FPUtil/Hypot.h
+++ libc/src/__support/FPUtil/Hypot.h
@@ -135,15 +135,16 @@ LIBC_INLINE T hypot(T x, T y) {
   uint16_t b_exp = b_bits.get_biased_exponent();
 
   if ((a_exp - b_exp >= FPBits_t::FRACTION_LEN + 2) || (x == 0) || (y == 0)) {
+#ifdef LIBC_TYPES_HAS_FLOAT16
     if constexpr (cpp::is_same_v<T, float16>) {
       // Compiler runtime for basic operations of float16 might not be correctly
       // rounded for all rounding modes.
       float af = fputil::cast<float>(x_abs.get_val());
       float bf = fputil::cast<float>(y_abs.get_val());
       return fputil::cast<float16>(af + bf);
-    } else {
+    } else
+#endif // LIBC_TYPES_HAS_FLOAT16
       return x_abs.get_val() + y_abs.get_val();
-    }
   }
 
   uint64_t out_exp = a_exp;
