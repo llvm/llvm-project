f161f3c76d1ecaf96e504aa1a49dc2a72a6344e4
Tomáš Slanina <itzexpoexpo@gmail.com>
2025-08-24 15:24:59 +0200

    Use consistently named enum values

--- clang/lib/Format/UnwrappedLineParser.cpp
+++ clang/lib/Format/UnwrappedLineParser.cpp
@@ -4161,7 +4161,7 @@ void UnwrappedLineParser::parseRecord(bool ParseAsExpr, bool IsJavaRecord) {
     if (ParseAsExpr) {
       parseChildBlock();
     } else {
-      if (Style.AllowShortRecordsOnASingleLine != FormatStyle::SRS_All &&
+      if (Style.AllowShortRecordsOnASingleLine != FormatStyle::SRS_Always &&
           ShouldBreakBeforeBrace(Style, InitialToken, *Tokens->peekNextToken(),
                                  IsJavaRecord)) {
         addUnwrappedLine();
