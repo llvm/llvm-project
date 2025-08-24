9b66d3bc569d02280da45e9823b7395bb9b99ca3
Tomáš Slanina <itzexpoexpo@gmail.com>
2025-08-24 15:24:59 +0200

    Use consistently named enum values

--- clang/include/clang/Format/Format.h
+++ clang/include/clang/Format/Format.h
@@ -1011,7 +1011,7 @@ struct FormatStyle {
     ///   struct foo {};
     ///   struct bar { int i; };
     /// \endcode
-    SRS_All
+    SRS_Always
   };
 
   /// Dependent on the value, ``struct bar { int i; }`` can be put on a single
