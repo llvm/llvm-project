0870e476b30775b88f5dabaf719b7b458fd5d415
Jonas Devlieghere <jonas@devlieghere.com>
2025-08-25 16:18:21 -0700

    [lldb] Adopt JSONTransport in the MCP Server (Reland) (#155322)
    This PR adopts JSONTransport in the MCP server implementation. It
    required a slight change in design in the relationship between the two
    server classes. Previously, these two had an "is-a" connection, while
    now they have a "has-a" connection.
    
    The "generic" protocol server in Protocol/MCP now operates using a
    single connection (Transport). This matches the design in DAP where each
    DAP instance has its own connection. The protocol server in Plugins
    still supports multiple clients and creates a new server instance for
    each connection.
    
    I believe the new design makes sense in the long term (as proved by DAP)
    and allows us to make the server stateful if we choose to do so. There's
    no reason that multiple client support can't live in the generic
    protocol library, but for now I kept it in ProtocolServerMCP to avoid
    creating unnecessary abstractions.
    
    This is a reland of #155034 but with significant changes to the tests.
    The unit tests now test the generic server implementation, which matches
    the original intent. This also means the test are now single threaded
    and therefore fully deterministic using the MainLoop.

--- lldb/source/Protocol/MCP/Server.cpp
+++ lldb/source/Protocol/MCP/Server.cpp
@@ -12,8 +12,11 @@
 using namespace lldb_protocol::mcp;
 using namespace llvm;
 
-Server::Server(std::string name, std::string version)
-    : m_name(std::move(name)), m_version(std::move(version)) {
+Server::Server(std::string name, std::string version,
+               std::unique_ptr<MCPTransport> transport_up,
+               lldb_private::MainLoop &loop)
+    : m_name(std::move(name)), m_version(std::move(version)),
+      m_transport_up(std::move(transport_up)), m_loop(loop) {
   AddRequestHandlers();
 }
 
@@ -30,7 +33,7 @@ void Server::AddRequestHandlers() {
                                                 this, std::placeholders::_1));
 }
 
-llvm::Expected<Response> Server::Handle(Request request) {
+llvm::Expected<Response> Server::Handle(const Request &request) {
   auto it = m_request_handlers.find(request.method);
   if (it != m_request_handlers.end()) {
     llvm::Expected<Response> response = it->second(request);
@@ -44,7 +47,7 @@ llvm::Expected<Response> Server::Handle(Request request) {
       llvm::formatv("no handler for request: {0}", request.method).str());
 }
 
-void Server::Handle(Notification notification) {
+void Server::Handle(const Notification &notification) {
   auto it = m_notification_handlers.find(notification.method);
   if (it != m_notification_handlers.end()) {
     it->second(notification);
@@ -52,49 +55,7 @@ void Server::Handle(Notification notification) {
   }
 }
 
-llvm::Expected<std::optional<Message>>
-Server::HandleData(llvm::StringRef data) {
-  auto message = llvm::json::parse<Message>(/*JSON=*/data);
-  if (!message)
-    return message.takeError();
-
-  if (const Request *request = std::get_if<Request>(&(*message))) {
-    llvm::Expected<Response> response = Handle(*request);
-
-    // Handle failures by converting them into an Error message.
-    if (!response) {
-      Error protocol_error;
-      llvm::handleAllErrors(
-          response.takeError(),
-          [&](const MCPError &err) { protocol_error = err.toProtocolError(); },
-          [&](const llvm::ErrorInfoBase &err) {
-            protocol_error.code = MCPError::kInternalError;
-            protocol_error.message = err.message();
-          });
-      Response error_response;
-      error_response.id = request->id;
-      error_response.result = std::move(protocol_error);
-      return error_response;
-    }
-
-    return *response;
-  }
-
-  if (const Notification *notification =
-          std::get_if<Notification>(&(*message))) {
-    Handle(*notification);
-    return std::nullopt;
-  }
-
-  if (std::get_if<Response>(&(*message)))
-    return llvm::createStringError("unexpected MCP message: response");
-
-  llvm_unreachable("all message types handled");
-}
-
 void Server::AddTool(std::unique_ptr<Tool> tool) {
-  std::lock_guard<std::mutex> guard(m_mutex);
-
   if (!tool)
     return;
   m_tools[tool->GetName()] = std::move(tool);
@@ -102,21 +63,17 @@ void Server::AddTool(std::unique_ptr<Tool> tool) {
 
 void Server::AddResourceProvider(
     std::unique_ptr<ResourceProvider> resource_provider) {
-  std::lock_guard<std::mutex> guard(m_mutex);
-
   if (!resource_provider)
     return;
   m_resource_providers.push_back(std::move(resource_provider));
 }
 
 void Server::AddRequestHandler(llvm::StringRef method, RequestHandler handler) {
-  std::lock_guard<std::mutex> guard(m_mutex);
   m_request_handlers[method] = std::move(handler);
 }
 
 void Server::AddNotificationHandler(llvm::StringRef method,
                                     NotificationHandler handler) {
-  std::lock_guard<std::mutex> guard(m_mutex);
   m_notification_handlers[method] = std::move(handler);
 }
 
@@ -182,7 +139,6 @@ llvm::Expected<Response> Server::ResourcesListHandler(const Request &request) {
 
   llvm::json::Array resources;
 
-  std::lock_guard<std::mutex> guard(m_mutex);
   for (std::unique_ptr<ResourceProvider> &resource_provider_up :
        m_resource_providers) {
     for (const Resource &resource : resource_provider_up->GetResources())
@@ -211,7 +167,6 @@ llvm::Expected<Response> Server::ResourcesReadHandler(const Request &request) {
   if (uri_str.empty())
     return llvm::createStringError("no resource uri");
 
-  std::lock_guard<std::mutex> guard(m_mutex);
   for (std::unique_ptr<ResourceProvider> &resource_provider_up :
        m_resource_providers) {
     llvm::Expected<ResourceResult> result =
@@ -231,4 +186,72 @@ llvm::Expected<Response> Server::ResourcesReadHandler(const Request &request) {
   return make_error<MCPError>(
       llvm::formatv("no resource handler for uri: {0}", uri_str).str(),
       MCPError::kResourceNotFound);
+}
+
+Capabilities Server::GetCapabilities() {
+  lldb_protocol::mcp::Capabilities capabilities;
+  capabilities.tools.listChanged = true;
+  // FIXME: Support sending notifications when a debugger/target are
+  // added/removed.
+  capabilities.resources.listChanged = false;
+  return capabilities;
+}
+
+llvm::Error Server::Run() {
+  auto handle = m_transport_up->RegisterMessageHandler(m_loop, *this);
+  if (!handle)
+    return handle.takeError();
+
+  lldb_private::Status status = m_loop.Run();
+  if (status.Fail())
+    return status.takeError();
+
+  return llvm::Error::success();
+}
+
+void Server::Received(const Request &request) {
+  auto SendResponse = [this](const Response &response) {
+    if (llvm::Error error = m_transport_up->Send(response))
+      m_transport_up->Log(llvm::toString(std::move(error)));
+  };
+
+  llvm::Expected<Response> response = Handle(request);
+  if (response)
+    return SendResponse(*response);
+
+  lldb_protocol::mcp::Error protocol_error;
+  llvm::handleAllErrors(
+      response.takeError(),
+      [&](const MCPError &err) { protocol_error = err.toProtocolError(); },
+      [&](const llvm::ErrorInfoBase &err) {
+        protocol_error.code = MCPError::kInternalError;
+        protocol_error.message = err.message();
+      });
+  Response error_response;
+  error_response.id = request.id;
+  error_response.result = std::move(protocol_error);
+  SendResponse(error_response);
+}
+
+void Server::Received(const Response &response) {
+  m_transport_up->Log("unexpected MCP message: response");
+}
+
+void Server::Received(const Notification &notification) {
+  Handle(notification);
+}
+
+void Server::OnError(llvm::Error error) {
+  m_transport_up->Log(llvm::toString(std::move(error)));
+  TerminateLoop();
+}
+
+void Server::OnClosed() {
+  m_transport_up->Log("EOF");
+  TerminateLoop();
+}
+
+void Server::TerminateLoop() {
+  m_loop.AddPendingCallback(
+      [](lldb_private::MainLoopBase &loop) { loop.RequestTermination(); });
 }
