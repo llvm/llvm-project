6e2fcec518ac1058fcb90c7fc33a5c7e73ae1a98
Krishna Pandey <kpandey81930@gmail.com>
2025-08-25 11:46:25 +0530

    [libc][math][c++23] Add {frexp,ilogb,ldexp,llogb,logb}bf16 math functions (#154427)
    This PR adds the following basic math functions for BFloat16 type along
    with the tests:
    - frexpbf16
    - ilobbf16
    - ldexpbf16
    - llogbbf16
    - logbbf16
    
    ---------
    
    Signed-off-by: Krishna Pandey <kpandey81930@gmail.com>

---
+++ libc/test/src/math/smoke/logbbf16_test.cpp
@@ -0,0 +1,14 @@
+//===-- Unittests for logbbf16 --------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "LogbTest.h"
+
+#include "src/__support/FPUtil/bfloat16.h"
+#include "src/math/logbbf16.h"
+
+LIST_LOGB_TESTS(bfloat16, LIBC_NAMESPACE::logbbf16)
