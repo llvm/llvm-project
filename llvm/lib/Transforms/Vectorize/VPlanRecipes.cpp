8f44611e0c21bb28bdf5923f0ab6aeed690fe294
David Sherwood <david.sherwood@arm.com>
2025-08-26 10:17:23 +0100

    [LV] Stop using the legacy cost model for udiv + friends (#152707)
    In VPWidenRecipe::computeCost for the instructions udiv, sdiv, urem and
    srem we fall back on the legacy cost unnecessarily. At this point we
    know that the vplan must be functionally correct, i.e. if the
    divide/remainder is not safe to speculatively execute then we must have
    either:
    
    1. Scalarised the operation, in which case we wouldn't be using a
    VPWidenRecipe, or
    2. We've inserted a select for the second operand to ensure we don't
    fault through divide-by-zero.
    
    For 2) it's necessary to add the select operation to
    VPInstruction::computeCost so that we mirror the cost of the legacy cost
    model. The only problem with this is that we also generate selects in
    vplan for predicated loops with reductions, which *aren't* accounted for
    in the legacy cost model. In order to prevent asserts firing I've also
    added the selects to precomputeCosts to ensure the legacy costs match
    the vplan costs for reductions.
    
    (cherry picked from commit d606eae2ced1884a95df9511a867e3faee09ee3c)

--- llvm/lib/Transforms/Vectorize/VPlanRecipes.cpp
+++ llvm/lib/Transforms/Vectorize/VPlanRecipes.cpp
@@ -990,6 +990,19 @@ InstructionCost VPInstruction::computeCost(ElementCount VF,
   }
 
   switch (getOpcode()) {
+  case Instruction::Select: {
+    // TODO: It may be possible to improve this by analyzing where the
+    // condition operand comes from.
+    CmpInst::Predicate Pred = CmpInst::BAD_ICMP_PREDICATE;
+    auto *CondTy = Ctx.Types.inferScalarType(getOperand(0));
+    auto *VecTy = Ctx.Types.inferScalarType(getOperand(1));
+    if (!vputils::onlyFirstLaneUsed(this)) {
+      CondTy = toVectorTy(CondTy, VF);
+      VecTy = toVectorTy(VecTy, VF);
+    }
+    return Ctx.TTI.getCmpSelInstrCost(Instruction::Select, VecTy, CondTy, Pred,
+                                      Ctx.CostKind);
+  }
   case Instruction::ExtractElement: {
     // Add on the cost of extracting the element.
     auto *VecTy = toVectorTy(Ctx.Types.inferScalarType(getOperand(0)), VF);
@@ -2044,8 +2057,10 @@ InstructionCost VPWidenRecipe::computeCost(ElementCount VF,
   case Instruction::SDiv:
   case Instruction::SRem:
   case Instruction::URem:
-    // More complex computation, let the legacy cost-model handle this for now.
-    return Ctx.getLegacyCost(cast<Instruction>(getUnderlyingValue()), VF);
+    // If the div/rem operation isn't safe to speculate and requires
+    // predication, then the only way we can even create a vplan is to insert
+    // a select on the second input operand to ensure we use the value of 1
+    // for the inactive lanes. The select will be costed separately.
   case Instruction::FNeg:
   case Instruction::Add:
   case Instruction::FAdd:
