0a260a2bbc97e4ad9cdfb5215827fa325b150182
Originns <68753984+Originns@users.noreply.github.com>
2025-08-26 15:35:49 +0200

    [flang-tidy][bugprone] add test & documentation for short circuit check

--- flang/tools/test/flang-tidy/precisionloss01.f90
+++ flang/tools/test/flang-tidy/precisionloss01.f90
@@ -3,7 +3,6 @@ subroutine s
   real(8) :: i
   real(4) :: j
   i = 1.0
-  ! CHECK-MESSAGES: :[[@LINE-1]]:3: warning: Possible loss of precision in implicit conversion (REAL(4) to REAL(8))
   j = 2.0
   j = i
   ! CHECK-MESSAGES: :[[@LINE-1]]:3: warning: Possible loss of precision in implicit conversion (REAL(8) to REAL(4))
