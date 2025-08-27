63c72f4e20a4a30ae2fd140e17fd36015c49d315
Augusto Noronha <anoronha@apple.com>
2025-08-27 16:43:30 -0700

    [lldb] Disable TestClosureShortcuts
    This test is failing because of DIL.

--- lldb/test/API/lang/swift/closure_shortcuts/main.swift
+++ lldb/test/API/lang/swift/closure_shortcuts/main.swift
@@ -15,19 +15,15 @@ func main() -> Int {
   let names = ["foo", "patatino"]
 
   var reversedNames = names.sorted(by: {
-    $0 > $1 } //%self.expect('expr $0', substrs=['patatino'])
-              //%self.expect('expr $1', substrs=['foo'])
-              //%self.expect('frame var $0', substrs=['patatino'])
-              //%self.expect('frame var $1', substrs=['foo'])
-  )
-
-  var tinky = [1,2].map({
-    $0 * 2 //%self.expect('expr [12, 14].map({$0 + 2})', substrs=['[0] = 14', '[1] = 16'])
+    return $0 > $1 // break here for anonymous variable
   })
 
-  return 0 //%self.expect('expr tinky.map({$0 * 2})', substrs=['[0] = 4', '[1] = 8'])
-           //%self.expect('expr [2,4].map({$0 * 2})', substrs=['[0] = 4', '[1] = 8'])
-           //%self.expect('expr $0', substrs=['cannot find \'$0\' in scope'], error=True)
+  var tinky = [1,2].map({
+    $0 * 2 // break here for tinky
+  })
+
+  _ = tinky // break here for outer scope
+  return 0 // break here for outer scope
 }
 
 _ = main()
