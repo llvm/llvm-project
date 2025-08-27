8093a608df0285c00622c2a2ec38cad485e1bfb9
David Spickett <david.spickett@linaro.org>
2025-08-27 12:17:50 +0000

    more stray lines

--- lldb/source/Plugins/Process/Utility/NativeRegisterContextDBReg.h
+++ lldb/source/Plugins/Process/Utility/NativeRegisterContextDBReg.h
@@ -87,7 +87,6 @@ protected:
   };
   virtual std::optional<WatchpointDetails>
   AdjustWatchpoint(const WatchpointDetails &details) = 0;
-
   virtual uint32_t MakeBreakControlValue(size_t size) = 0;
   virtual uint32_t MakeWatchControlValue(size_t size, uint32_t watch_flags) = 0;
   virtual uint32_t GetWatchpointSize(uint32_t wp_index) = 0;
