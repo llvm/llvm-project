60addf833cdb77d31ead68e334b8cab584b7f5da
Eugene Epshteyn <eepshteyn@nvidia.com>
2025-08-28 14:52:44 -0400

    Simplify

--- flang-rt/lib/runtime/pointer.cpp
+++ flang-rt/lib/runtime/pointer.cpp
@@ -267,13 +267,9 @@ bool RTDEF(PointerIsAssociatedWith)(
   if (!target) {
     return pointer.raw().base_addr != nullptr;
   }
-  if (target->ElementBytes() == 0) {
+  if (!target->raw().base_addr || target->ElementBytes() == 0) {
     // F2023, 16.9.20, p5, case (v)-(vi): don't associate pointers with
     // targets that have zero sized storage sequence.
-    return false;
-  }
-  if (!target->raw().base_addr ||
-      (target->raw().type != CFI_type_struct && target->ElementBytes() == 0)) {
     return false;
   }
   int rank{pointer.rank()};
