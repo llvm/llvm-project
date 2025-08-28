92a30831667f2dd8d529bf36ad32273148225e7b
Gaëtan Bossu <gaetan.bossu@arm.com>
2025-08-28 08:19:29 +0000

    Replace SVEBitsPerBlock with plain 128

--- llvm/lib/Target/AArch64/AArch64TargetTransformInfo.cpp
+++ llvm/lib/Target/AArch64/AArch64TargetTransformInfo.cpp
@@ -5751,12 +5751,13 @@ AArch64TTIImpl::getShuffleCost(TTI::ShuffleKind Kind, VectorType *DstTy,
   Kind = improveShuffleKindFromMask(Kind, Mask, SrcTy, Index, SubTp);
   bool IsExtractSubvector = Kind == TTI::SK_ExtractSubvector;
   // A subvector extract can be implemented with a NEON/SVE ext (or trivial
-  // extract, if from lane 0). This currently only handles low or high extracts
-  // to prevent SLP vectorizer regressions.
+  // extract, if from lane 0) for 128-bit NEON vectors or legal SVE vectors.
+  // This currently only handles low or high extracts to prevent SLP vectorizer
+  // regressions.
   // Note that SVE's ext instruciton is destructive, but it can be fused with
   // a movprfx to act like a constructive instruction.
   if (IsExtractSubvector && LT.second.isFixedLengthVector()) {
-    if (LT.second.getFixedSizeInBits() >= AArch64::SVEBitsPerBlock &&
+    if (LT.second.getFixedSizeInBits() >= 128 &&
         cast<FixedVectorType>(SubTp)->getNumElements() ==
             LT.second.getVectorNumElements() / 2) {
       if (Index == 0)
