fcfcd521d41cccc12b1b8a46e524741327469970
Aleksandr Popov <42888396+aleks-tmb@users.noreply.github.com>
2025-08-28 10:13:21 +0200

    Revert "[X86] Fix incorrect NOP insertion between fused instructions that breaks macro fusion" (#155780)
    Reverts llvm/llvm-project#155316

--- llvm/test/MC/X86/align-branch-fused.s
+++ llvm/test/MC/X86/align-branch-fused.s
@@ -1,10 +1,7 @@
-# RUN: rm -rf %t && split-file %s %t && cd %t
-# RUN: llvm-mc -filetype=obj -triple x86_64 --x86-align-branch-boundary=32 --x86-align-branch=fused+jcc foo.s | llvm-objdump -d --no-show-raw-insn - | FileCheck foo.s
-# RUN: llvm-mc -filetype=obj -triple x86_64 --x86-align-branch-boundary=32 --x86-align-branch=fused+jcc -x86-pad-max-prefix-size=1 bar.s | llvm-objdump -d --no-show-raw-insn - | FileCheck bar.s
+# RUN: llvm-mc -filetype=obj -triple x86_64 --x86-align-branch-boundary=32 --x86-align-branch=fused+jcc %s | llvm-objdump -d --no-show-raw-insn - | FileCheck %s
 
   # Exercise cases where fused instructions need to be aligned.
 
-#--- foo.s
   .text
   .globl  foo
 foo:
@@ -43,18 +40,3 @@ foo:
   cmp  %rax, %rbp
   jne foo
   int3
-
-# Exercise the case where fused instructions need to be aligned,
-# ensuring fusion is not broken by a NOP
-
-#--- bar.s
-  .text
-	.globl	bar
-bar:
-  .nops 27
-# CHECK:      20:       testq   %rcx, %rcx
-# CHECK:      23:       je
-	testq	%rcx, %rcx
-	je	.EXIT
-.EXIT:
-	ret
