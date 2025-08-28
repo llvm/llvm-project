c8d05f5613ac6daa06049933e4f0a5e96ea62453
Albert Havliček <ahavlicek@azul.com>
2025-08-28 06:45:01 +0000

    Remove MLIR from header

--- llvm/include/llvm/Support/LSP/Logging.h
+++ llvm/include/llvm/Support/LSP/Logging.h
@@ -1,4 +1,4 @@
-//===- Logging.h - MLIR LSP Server Logging ----------------------*- C++ -*-===//
+//===- Logging.h - LSP Server Logging ----------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
