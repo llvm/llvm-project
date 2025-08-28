6254024909f036b80f920e0c0a2f0626b9f04283
Joao Saffran <joaosaffranllvm@gmail.com>
2025-08-27 18:16:43 -0700

    clean up 3

--- llvm/include/llvm/Frontend/HLSL/HLSLBinding.h
+++ llvm/include/llvm/Frontend/HLSL/HLSLBinding.h
@@ -113,12 +113,12 @@ struct Binding {
         Cookie(Cookie) {}
 
   bool isUnbounded() const { return UpperBound == ~0U; }
-  bool operator==(const Binding &RHS) const {
-    return std::tie(RC, Space, LowerBound, UpperBound, Cookie) ==
-            std::tie(RHS.RC, RHS.Space, RHS.LowerBound, RHS.UpperBound,
-                    RHS.Cookie);
-  }
-  bool operator!=(const Binding &RHS) const { return !(*this == RHS); }
+    bool operator==(const Binding &RHS) const {
+      return std::tie(RC, Space, LowerBound, UpperBound, Cookie) ==
+             std::tie(RHS.RC, RHS.Space, RHS.LowerBound, RHS.UpperBound,
+                      RHS.Cookie);
+    }
+    bool operator!=(const Binding &RHS) const { return !(*this == RHS); }
 
   bool operator<(const Binding &RHS) const {
     return std::tie(RC, Space, LowerBound) <
