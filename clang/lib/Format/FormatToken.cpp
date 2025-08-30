c3baf987180323118880de1691f5ef9cadb48549
Gedare Bloom <gedare@rtems.org>
2025-08-29 19:48:12 -0600

    Trying Hazardy's suggestions

--- clang/lib/Format/FormatToken.cpp
+++ clang/lib/Format/FormatToken.cpp
@@ -181,7 +181,7 @@ void CommaSeparatedList::precomputeFormattingInfos(const FormatToken *Token) {
     return;
 
   // Column format doesn't really make sense if we don't align after brackets.
-  if (Style.AlignAfterOpenBracket == FormatStyle::BAS_DontAlign)
+  if (Style.AlignAfterOpenBracket == false)
     return;
 
   FormatToken *ItemBegin = Token->Next;
