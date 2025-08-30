a8f7457beef6ce9d8e208af32a603acda63522b9
zishan <zmirza@posteo.ch>
2025-08-31 01:45:55 +0200

    add tests for localtime and localtime_r

--- libc/test/src/time/localtime_r_test.cpp
+++ libc/test/src/time/localtime_r_test.cpp
@@ -11,9 +11,9 @@
 
 TEST(LlvmLibcLocaltimeR, ValidUnixTimestamp0) {
   struct tm *input = nullptr;
-  time_t t_ptr = 0;
+  const time_t timer = 0;
 
-  struct tm *result = LIBC_NAMESPACE::localtime_r(&t_ptr, input);
+  struct tm *result = LIBC_NAMESPACE::localtime_r(&timer, input);
 
   ASSERT_EQ(70, result->tm_year);
   ASSERT_EQ(0, result->tm_mon);
@@ -23,5 +23,42 @@ TEST(LlvmLibcLocaltimeR, ValidUnixTimestamp0) {
   ASSERT_EQ(0, result->tm_sec);
   ASSERT_EQ(4, result->tm_wday);
   ASSERT_EQ(0, result->tm_yday);
+  ASSERT_EQ(0, result->tm_isdst);
+}
+
+// TODO(zimirza): These tests does not expect the correct output of localtime as per
+// specification. This is due to timezone functions removed from
+// https://github.com/llvm/llvm-project/pull/110363.
+// This will be resolved a new pull request.
+
+TEST(LlvmLibcLocaltimeR, ValidUnixTimestamp) {
+  struct tm *input = nullptr;
+  const time_t timer = 1756595338;
+  struct tm *result = LIBC_NAMESPACE::localtime_r(&timer, input);
+
+  ASSERT_EQ(125, result->tm_year);
+  ASSERT_EQ(7, result->tm_mon);
+  ASSERT_EQ(30, result->tm_mday);
+  ASSERT_EQ(23, result->tm_hour);
+  ASSERT_EQ(8, result->tm_min);
+  ASSERT_EQ(58, result->tm_sec);
+  ASSERT_EQ(6, result->tm_wday);
+  ASSERT_EQ(241, result->tm_yday);
+  ASSERT_EQ(0, result->tm_isdst);
+}
+
+TEST(LlvmLibcLocaltimeR, ValidUnixTimestampNegative) {
+  struct tm *input = nullptr;
+  const time_t timer = -1756595338;
+  struct tm *result = LIBC_NAMESPACE::localtime_r(&timer, input);
+
+  ASSERT_EQ(14, result->tm_year);
+  ASSERT_EQ(4, result->tm_mon);
+  ASSERT_EQ(4, result->tm_mday);
+  ASSERT_EQ(0, result->tm_hour);
+  ASSERT_EQ(51, result->tm_min);
+  ASSERT_EQ(2, result->tm_sec);
+  ASSERT_EQ(1, result->tm_wday);
+  ASSERT_EQ(123, result->tm_yday);
   ASSERT_EQ(0, result->tm_isdst);
 }
--- libc/test/src/time/localtime_test.cpp
+++ libc/test/src/time/localtime_test.cpp
@@ -10,8 +10,8 @@
 #include "test/UnitTest/Test.h"
 
 TEST(LlvmLibcLocaltime, ValidUnixTimestamp0) {
-  time_t t_ptr = 0;
-  struct tm *result = LIBC_NAMESPACE::localtime(&t_ptr);
+  const time_t timer = 0;
+  struct tm *result = LIBC_NAMESPACE::localtime(&timer);
 
   ASSERT_EQ(70, result->tm_year);
   ASSERT_EQ(0, result->tm_mon);
@@ -21,5 +21,40 @@ TEST(LlvmLibcLocaltime, ValidUnixTimestamp0) {
   ASSERT_EQ(0, result->tm_sec);
   ASSERT_EQ(4, result->tm_wday);
   ASSERT_EQ(0, result->tm_yday);
+  ASSERT_EQ(0, result->tm_isdst);
+}
+
+// TODO(zimirza): These tests does not expect the correct output of localtime as per
+// specification. This is due to timezone functions removed from
+// https://github.com/llvm/llvm-project/pull/110363.
+// This will be resolved a new pull request.
+
+TEST(LlvmLibcLocaltime, ValidUnixTimestamp) {
+  const time_t timer = 1756595338;
+  struct tm *result = LIBC_NAMESPACE::localtime(&timer);
+
+  ASSERT_EQ(125, result->tm_year);
+  ASSERT_EQ(7, result->tm_mon);
+  ASSERT_EQ(30, result->tm_mday);
+  ASSERT_EQ(23, result->tm_hour);
+  ASSERT_EQ(8, result->tm_min);
+  ASSERT_EQ(58, result->tm_sec);
+  ASSERT_EQ(6, result->tm_wday);
+  ASSERT_EQ(241, result->tm_yday);
+  ASSERT_EQ(0, result->tm_isdst);
+}
+
+TEST(LlvmLibcLocaltime, ValidUnixTimestampNegative) {
+  const time_t timer = -1756595338;
+  struct tm *result = LIBC_NAMESPACE::localtime(&timer);
+
+  ASSERT_EQ(14, result->tm_year);
+  ASSERT_EQ(4, result->tm_mon);
+  ASSERT_EQ(4, result->tm_mday);
+  ASSERT_EQ(0, result->tm_hour);
+  ASSERT_EQ(51, result->tm_min);
+  ASSERT_EQ(2, result->tm_sec);
+  ASSERT_EQ(1, result->tm_wday);
+  ASSERT_EQ(123, result->tm_yday);
   ASSERT_EQ(0, result->tm_isdst);
 }
