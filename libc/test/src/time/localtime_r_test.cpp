9f68eeac298e44def6843c05eebb1dd5f6d5c971
zishan <zmirza@posteo.ch>
2025-08-31 02:52:29 +0200

    format code with clang-format

--- libc/test/src/time/localtime_r_test.cpp
+++ libc/test/src/time/localtime_r_test.cpp
@@ -10,17 +10,15 @@
 #include "test/UnitTest/Test.h"
 
 TEST(LlvmLibcLocaltimeR, ValidUnixTimestamp0) {
-  struct tm input = {
-    .tm_sec = 0,
-    .tm_min = 0,
-    .tm_hour = 0,
-    .tm_mday = 0,
-    .tm_mon = 0,
-    .tm_year = 0,
-    .tm_wday = 0,
-    .tm_yday = 0,
-    .tm_isdst = 0
-  };
+  struct tm input = {.tm_sec = 0,
+                     .tm_min = 0,
+                     .tm_hour = 0,
+                     .tm_mday = 0,
+                     .tm_mon = 0,
+                     .tm_year = 0,
+                     .tm_wday = 0,
+                     .tm_yday = 0,
+                     .tm_isdst = 0};
   const time_t timer = 0;
 
   struct tm *result = LIBC_NAMESPACE::localtime_r(&timer, &input);
@@ -42,17 +40,15 @@ TEST(LlvmLibcLocaltimeR, ValidUnixTimestamp0) {
 // This will be resolved a new pull request.
 
 TEST(LlvmLibcLocaltimeR, ValidUnixTimestamp) {
-  struct tm input = {
-    .tm_sec = 0,
-    .tm_min = 0,
-    .tm_hour = 0,
-    .tm_mday = 0,
-    .tm_mon = 0,
-    .tm_year = 0,
-    .tm_wday = 0,
-    .tm_yday = 0,
-    .tm_isdst = 0
-  };
+  struct tm input = {.tm_sec = 0,
+                     .tm_min = 0,
+                     .tm_hour = 0,
+                     .tm_mday = 0,
+                     .tm_mon = 0,
+                     .tm_year = 0,
+                     .tm_wday = 0,
+                     .tm_yday = 0,
+                     .tm_isdst = 0};
   const time_t timer = 1756595338;
   struct tm *result = LIBC_NAMESPACE::localtime_r(&timer, &input);
 
@@ -68,17 +64,15 @@ TEST(LlvmLibcLocaltimeR, ValidUnixTimestamp) {
 }
 
 TEST(LlvmLibcLocaltimeR, ValidUnixTimestampNegative) {
-  struct tm input = {
-    .tm_sec = 0,
-    .tm_min = 0,
-    .tm_hour = 0,
-    .tm_mday = 0,
-    .tm_mon = 0,
-    .tm_year = 0,
-    .tm_wday = 0,
-    .tm_yday = 0,
-    .tm_isdst = 0
-  };
+  struct tm input = {.tm_sec = 0,
+                     .tm_min = 0,
+                     .tm_hour = 0,
+                     .tm_mday = 0,
+                     .tm_mon = 0,
+                     .tm_year = 0,
+                     .tm_wday = 0,
+                     .tm_yday = 0,
+                     .tm_isdst = 0};
   const time_t timer = -1756595338;
   struct tm *result = LIBC_NAMESPACE::localtime_r(&timer, &input);
 
