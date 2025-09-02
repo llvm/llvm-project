e2b72b9cc99e7aab5e26194ac422ceb56e550978
Anthony Latsis <alatsis@apple.com>
2025-09-02 22:06:55 +0100

    [rebranch] lldb: Skip remaining failing tests

--- lldb/test/API/lang/swift/cxx_interop/forward/expressions/TestSwiftForwardInteropExpressions.py
+++ lldb/test/API/lang/swift/cxx_interop/forward/expressions/TestSwiftForwardInteropExpressions.py
@@ -5,7 +5,7 @@ Test that evaluating expressions works on forward interop mode.
 from lldbsuite.test.lldbtest import *
 from lldbsuite.test.decorators import *
 
-
+@skipIf(bugnumber = "rdar://159675331")
 class TestSwiftForwardInteropExpressions(TestBase):
 
     def setup(self, bkpt_str):
