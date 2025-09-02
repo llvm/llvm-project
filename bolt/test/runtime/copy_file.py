llvm/llvm-project

commit 69ccc394713aca2c031b748c5020c9da2c31de53

[BOLT] Enable lit internal shell by default
This patch enables lit's internal shell by default now that all
REQUIRES: shell tests have been update to not require shell features not
available in lit and all unresolved tests have been fixed.

This should speed up test runtime by a bit and will give nicer error
messaging.

Fixes #102701.

Reviewers:
aaupov, yota9, maksfb, paschalis-mpeis, yozhu, rafaelauler, ayermolo

Reviewed By: maksfb

Pull Request: https://github.com/llvm/llvm-project/pull/156083

bolt/test/lit.cfg.py
@@ -18,11 +18,22 @@ from lit.llvm.subst import FindTool
 # name: The name of this test suite.
 config.name = "BOLT"
 
+# TODO: Consolidate the logic for turning on the internal shell by default for all LLVM test suites.
+# See https://github.com/llvm/llvm-project/issues/106636 for more details.
+#
+# We prefer the lit internal shell which provides a better user experience on failures
+# and is faster unless the user explicitly disables it with LIT_USE_INTERNAL_SHELL=0
+# env var.
+use_lit_shell = True
+lit_shell_env = os.environ.get("LIT_USE_INTERNAL_SHELL")
+if lit_shell_env:
+    use_lit_shell = lit.util.pythonize_bool(lit_shell_env)
+
 # testFormat: The test format to use to interpret tests.
 #
 # For now we require '&&' between commands, until they get globally killed and
 # the test runner updated.
-config.test_format = lit.formats.ShTest(not llvm_config.use_lit_shell)
+config.test_format = lit.formats.ShTest(execute_external=not use_lit_shell)
 
 # suffixes: A list of file extensions to treat as test files.
 config.suffixes = [

commit a6442b35c88573e488fbc30f71f3018f336505b0

[BOLT] Port additional test to internal shell
This test was broken by #156083 because it was never ported to the
internal shell. It requires fuser which is not installed by default on
premerge and none of the BOLT buildbots have been online in a while.

This was actually causing a timeout because of #156484, worked around
using a manual bash invocation with a wait call to ensure all of the
subprocesses have exited.

bolt/test/runtime/copy_file.py
@@ -1,0 +1,15 @@
+import sys
+import shutil
+
+with open(sys.argv[1] + ".output") as log_file:
+    lines = log_file.readlines()
+    for line in lines:
+        if line.startswith(sys.argv[2]):
+            pid = line.split(" ")[1].strip()
+            shutil.copy(
+                sys.argv[1] + "." + pid + ".fdata",
+                sys.argv[1] + "." + sys.argv[3] + ".fdata",
+            )
+            sys.exit(0)
+
+sys.exit(1)

bolt/test/runtime/instrumentation-indirect-2.c
@@ -50,7 +50,7 @@ int main() {
   return 0;
 }
 /*
-REQUIRES: system-linux,shell,fuser
+REQUIRES: system-linux,fuser
 
 RUN: %clang %cflags %s -o %t.exe -Wl,-q -pie -fpie
 
@@ -61,10 +61,14 @@ RUN: --instrumentation-wait-forks
 
 # Instrumented program needs to finish returning zero
 # Both output and profile must contain all 16 functions
-RUN: %t.instrumented_conservative > %t.output
-# Wait for profile and output to be fully written
-RUN: bash %S/wait_file.sh %t.output
-RUN: bash %S/wait_file.sh %t.fdata
+# We need to use bash to invoke this as otherwise we hang inside a
+# popen.communicate call in lit's internal shell. Eventually we should not
+# need this.
+# TODO(boomanaiden154): Remove once
+# https://github.com/llvm/llvm-project/issues/156484 is fixed.
+RUN: bash -c "%t.instrumented_conservative; wait" > %t.output
+# We can just read because we ensure the profile will be fully written by
+# calling wait inside the bash invocation.
 RUN: cat %t.output | FileCheck %s --check-prefix=CHECK-OUTPUT
 RUN: cat %t.fdata | FileCheck %s --check-prefix=CHECK-COMMON-PROF
 
@@ -112,14 +116,8 @@ RUN: bash %S/wait_file.sh %t.output
 # Make sure all functions were called
 RUN: cat %t.output | FileCheck %s --check-prefix=CHECK-OUTPUT
 
-RUN: child_pid=$(cat %t.output | grep funcA | awk '{print $2;}')
-RUN: par_pid=$(cat %t.output | grep funcB | awk '{print $2;}')
-
-RUN: bash %S/wait_file.sh %t.$child_pid.fdata
-RUN: bash %S/wait_file.sh %t.$par_pid.fdata
-
-RUN: mv %t.$child_pid.fdata %t.child.fdata
-RUN: mv %t.$par_pid.fdata %t.parent.fdata
+RUN: %python %S/copy_file.py %t funcA child
+RUN: %python %S/copy_file.py %t funcB parent
 
 # Instrumented binary must produce two profiles with only local calls
 # recorded. Functions called only in child should not appear in parent's