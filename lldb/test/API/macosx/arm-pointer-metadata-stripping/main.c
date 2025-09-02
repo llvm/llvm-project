e96470ae39d16a1ebd0f94595b65e22cace5d829
Felipe de Azevedo Piovezan <fpiovezan@apple.com>
2025-09-02 11:23:40 -0700

    fixup! Add test

---
+++ lldb/test/API/macosx/arm-pointer-metadata-stripping/main.c
@@ -0,0 +1,20 @@
+#include <assert.h>
+#include <stdint.h>
+#include <stdio.h>
+
+
+int myglobal = 41;
+
+uint64_t get_high_bits(void *ptr) {
+  uint64_t mask = ~((1ULL << 48) - 1);
+  uint64_t ptrtoint = (uint64_t)ptr;
+  uint64_t high_bits = ptrtoint & mask;
+  printf("Higher bits are = %llx\n", high_bits);
+  return high_bits;
+}
+
+int main (){
+  int x = 42;
+  assert(0 == get_high_bits(&x));
+  return 0; //break here
+}
