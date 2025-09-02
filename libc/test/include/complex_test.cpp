008cd98bc25abad09b7eae384512d319fa89b35a
c8ef <c8ef@outlook.com>
2025-09-02 22:28:39 +0800

    [libc] Implement CMPLX related macros

--- libc/test/include/complex_test.cpp
+++ libc/test/include/complex_test.cpp
@@ -26,11 +26,11 @@ TEST(LlvmLibcComplexTest, CMPLXMacro) {
   EXPECT_CFP_EQ(CMPLXL(0, 1.0l), I);
   EXPECT_CFP_EQ(CMPLXL(1.0l, 0), 1.0l);
 
-// TODO: Uncomment these after issues#156463 is resolved.
-// #ifdef LIBC_TYPES_HAS_FLOAT16
-//   EXPECT_CFP_EQ(CMPLXF16(0, 1.0), I);
-//   EXPECT_CFP_EQ(CMPLXF16(1.0, 0), 1.0);
-// #endif // LIBC_TYPES_HAS_FLOAT16
+  // TODO: Uncomment these after issues#156463 is resolved.
+  // #ifdef LIBC_TYPES_HAS_FLOAT16
+  //   EXPECT_CFP_EQ(CMPLXF16(0, 1.0), I);
+  //   EXPECT_CFP_EQ(CMPLXF16(1.0, 0), 1.0);
+  // #endif // LIBC_TYPES_HAS_FLOAT16
 
 #ifdef LIBC_TYPES_HAS_FLOAT128
   EXPECT_CFP_EQ(CMPLXF128(0, 1.0), I);
