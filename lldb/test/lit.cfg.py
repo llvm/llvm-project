9edcaf14ce8c7ea6070c60363c0bacd1564eb0e4
Caroline Tice <cmtice@google.com>
2025-09-03 11:36:52 -0700

    [LLDB] Make internal shell the default for running LLDB lit tests.
    This patch updates the lld lit test config to use the internal shell by
    default. This has some performance advantages (~10-15%) and also
    produces nicer failure output. It also updates the two LLDB tests to
    not require shell (so that they run under the internal shell), after
    first verifying that they run and pass using the internal shell; and it
    fixes one test that was not passing under the internal shell.

--- lldb/test/lit.cfg.py
+++ lldb/test/lit.cfg.py
@@ -13,3 +13,15 @@ from lit.llvm import llvm_config
 config.name = "lldb"
 config.test_source_root = os.path.dirname(__file__)
 config.test_exec_root = os.path.join(config.lldb_obj_root, "test")
+
+# We prefer the lit internal shell which provides a better user experience on
+# failures and is faster unless the user explicitly disables it with
+# LIT_USE_INTERNAL_SHELL=0 env var.
+
+use_lit_shell = True
+lit_shell_env = os.environ.get("LIT_USE_INTERNAL_SHELL")
+if lit_shell_env:
+    use_lit_shell = lit.util.pythonize_bool(lit_shell_env)
+
+if use_lit_shell:
+    os.environ["LIT_USE_INTERNAL_SHELL"] = "1"
