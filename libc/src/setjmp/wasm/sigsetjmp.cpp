b5a96287e6f7bbe69d31e42be6ca7b008c8e9563
google-yfyang <yfyang@google.com>
2025-09-03 11:21:24 -0400

    Remove emscripten specific error messages

--- libc/src/setjmp/wasm/sigsetjmp.cpp
+++ libc/src/setjmp/wasm/sigsetjmp.cpp
@@ -9,14 +9,10 @@
 #include "src/setjmp/sigsetjmp.h"
 #include "hdr/offsetof_macros.h"
 #include "src/__support/common.h"
-#include "src/__support/macros/config.h"
-
-#if !defined(LIBC_TARGET_ARCH_IS_WASM)
-#error "Invalid file include"
-#endif
 
 namespace LIBC_NAMESPACE_DECL {
-[[gnu::returns_twice]] int sigsetjmp(jmp_buf sigjmp_buf, int savesigs) {
-    return setjmp(sigjmp_buf);
+[[gnu::returns_twice]] int sigsetjmp(jmp_buf sigjmp_buf,
+                                     [[maybe_unused]] int savesigs) {
+  return setjmp(sigjmp_buf);
 }
-}
+} // namespace LIBC_NAMESPACE_DECL
