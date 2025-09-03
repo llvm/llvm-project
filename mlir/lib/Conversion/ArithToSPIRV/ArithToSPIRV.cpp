c5a141bb8b5dfc4be06bb165d88e724e66bf5c4c
Ian Li <ianayl.work@gmail.com>
2025-09-03 12:48:53 -0400

    [mlir][spirv] Add pattern matching for arith.index_cast index to i1 for ArithToSPIRV (#156031)
    Currently, `arith.index_cast` gets converted to `OpSConvert`:
    https://github.com/llvm/llvm-project/blob/9bf5bf3baf3c7aec82cdd235c6a2fd57b4dd55ab/mlir/lib/Conversion/ArithToSPIRV/ArithToSPIRV.cpp#L1331
    [OpSConvert requires its operands to be of integer
    type](https://registry.khronos.org/SPIR-V/specs/unified1/SPIRV.html#OpSConvert),
    which poses an issue for `i1` since SPIRV distinguishes between booleans
    and integers. As a result, the following example doesn't get converted,
    leaving behind illegal ops:
    ```
    %0 = arith.index_cast %arg0 : index to i1
    ```
    This PR adds additional logic to convert `arith.index_casts` to SPIRV
    dialect when casting from `index` to `i1`. Converting `index_cast`s from
    `i1` to `index` is submitted as
    https://github.com/llvm/llvm-project/pull/155729.
    
    ---------
    
    Co-authored-by: Md Abdullah Shahneous Bari <98356296+mshahneo@users.noreply.github.com>
    Co-authored-by: Jakub Kuderski <kubakuderski@gmail.com>

--- mlir/lib/Conversion/ArithToSPIRV/ArithToSPIRV.cpp
+++ mlir/lib/Conversion/ArithToSPIRV/ArithToSPIRV.cpp
@@ -608,6 +608,34 @@ struct UIToFPI1Pattern final : public OpConversionPattern<arith::UIToFPOp> {
 };
 
 //===----------------------------------------------------------------------===//
+// IndexCastOp
+//===----------------------------------------------------------------------===//
+
+// Converts arith.index_cast to spirv.INotEqual if the target type is i1.
+struct IndexCastIndexI1Pattern final
+    : public OpConversionPattern<arith::IndexCastOp> {
+  using OpConversionPattern::OpConversionPattern;
+
+  LogicalResult
+  matchAndRewrite(arith::IndexCastOp op, OpAdaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+    if (!isBoolScalarOrVector(op.getType()))
+      return failure();
+
+    Type dstType = getTypeConverter()->convertType(op.getType());
+    if (!dstType)
+      return getTypeConversionFailure(rewriter, op);
+
+    Location loc = op.getLoc();
+    Value zeroIdx =
+        spirv::ConstantOp::getZero(adaptor.getIn().getType(), loc, rewriter);
+    rewriter.replaceOpWithNewOp<spirv::INotEqualOp>(op, dstType, zeroIdx,
+                                                    adaptor.getIn());
+    return success();
+  }
+};
+
+//===----------------------------------------------------------------------===//
 // ExtSIOp
 //===----------------------------------------------------------------------===//
 
@@ -1328,7 +1356,7 @@ void mlir::arith::populateArithToSPIRVPatterns(
     TypeCastingOpPattern<arith::SIToFPOp, spirv::ConvertSToFOp>,
     TypeCastingOpPattern<arith::FPToUIOp, spirv::ConvertFToUOp>,
     TypeCastingOpPattern<arith::FPToSIOp, spirv::ConvertFToSOp>,
-    TypeCastingOpPattern<arith::IndexCastOp, spirv::SConvertOp>,
+    TypeCastingOpPattern<arith::IndexCastOp, spirv::SConvertOp>, IndexCastIndexI1Pattern,
     TypeCastingOpPattern<arith::IndexCastUIOp, spirv::UConvertOp>,
     TypeCastingOpPattern<arith::BitcastOp, spirv::BitcastOp>,
     CmpIOpBooleanPattern, CmpIOpPattern,
