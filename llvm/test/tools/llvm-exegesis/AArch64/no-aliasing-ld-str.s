d6f2ae17b8af3a15aa7d17002bd77906881f0d19
Sjoerd Meijer <smeijer@nvidia.com>
2025-09-03 20:43:01 +0100

    Revert "[llvm-exegesis] Exclude loads/stores from aliasing instruction set" (#156735)
    Reverts llvm/llvm-project#156300
    
    Need to look at the X86 test failures.

--- llvm/test/tools/llvm-exegesis/AArch64/no-aliasing-ld-str.s
+++
@@ -1,8 +0,0 @@
-REQUIRES: aarch64-registered-target
-
-RUN: llvm-exegesis -mtriple=aarch64 -mcpu=neoverse-v2 -mode=latency --dump-object-to-disk=%d --opcode-name=FMOVWSr --benchmark-phase=assemble-measured-code 2>&1
-RUN: llvm-objdump -d %d > %t.s
-RUN: FileCheck %s < %t.s
-
-CHECK-NOT: ld{{[1-4]}}
-CHECK-NOT: st{{[1-4]}}
