19d27b40c44cfcbd2c86ac3d9ce6a89de90477c7
Lang Hames <lhames@gmail.com>
2025-09-04 14:35:36 +1000

    [orc-rt] Fix WrapperFunctionResultTest comments. NFC. (#156797)

--- orc-rt/unittests/WrapperFunctionResultTest.cpp
+++ orc-rt/unittests/WrapperFunctionResultTest.cpp
@@ -1,4 +1,4 @@
-//===-- wrapper_function_utils_test.cpp -----------------------------------===//
+//===-- WrapperFunctionResultTest.cpp -------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,7 +6,7 @@
 //
 //===----------------------------------------------------------------------===//
 //
-// This file is a part of the ORC runtime.
+// Test WrapperFunctionResult APIs.
 //
 //===----------------------------------------------------------------------===//
 
