2590576843300c68ac44f5709241607512b664e7
Jason Molenda <jmolenda@apple.com>
2025-09-04 11:42:26 -0700

    [lldb] Add utility to create Mach-O corefile from YAML desc (#153911)
    I've wanted a utility to create a corefile for test purposes given a bit
    of memory and regsters, for a while. I've written a few API tests over
    the years that needed exactly this capability -- we have several one-off
    Mach-O corefile creator utility in the API testsuite to do this. But
    it's a lot of boilerplate when you only want to specify some register
    contents and memory contents, to create an API test.
    
    This adds yaml2mach-core, a tool that should build on any system, takes
    a yaml description of register values for one or more threads,
    optionally memory values for one or more memory regions, and can take a
    list of UUIDs that will be added as LC_NOTE "load binary" metadata to
    the corefile so binaries can be loaded into virtual address space in a
    test scenario.
    
    The format of the yaml file looks like
    
    ```
    cpu: armv7m
    
    addressable-bits:
        num-bits: 39
    
    binaries:
      - name: debug-binary.development
        uuid: 67942352-5857-3D3D-90CB-A3F80BA67B04
        virtual-address: 0xfffffff01840c000
    
    threads:
      - regsets:
         - flavor: gpr
            registers: [{name: sp, value: 0x2000fe70}, {name: r7, value: 0x2000fe80},
                        {name: pc, value: 0x0020392c}, {name: lr, value: 0x0020392d}]
    
    memory-regions:
      # stack memory
      - addr: 0x2000fe70
        UInt32: [ 0x0000002a, 0x20010e58, 0x00203923,
                  0x00000001, 0x2000fe88, 0x00203911,
                  0x2000ffdc, 0xfffffff9 ]
      # instructions of a function
      - addr: 0x203910
         UInt8: [ 0xf8, 0xb5, 0x04, 0xaf, 0x06, 0x4c, 0x07, 0x49,
                  0x74, 0xf0, 0x2e, 0xf8, 0x01, 0xac, 0x74, 0xf0 ]
    ```
    
    and that's all that is needed to specify a corefile where four register
    values are specified (the others will be set to 0), and two memory
    regions will be emitted.
    
    The memory can be specified as an array of UInt8, UInt32, or UInt64, I
    anticipate that some of these corefiles may have stack values
    constructed manually and it may be simpler for a human to write them in
    a particular grouping of values.
    
    I needed this utility for an upcoming patch for ARM Cortex-M processors,
    to create a test for the change. I took the opportunity to remove two of
    the "trivial mach-o corefile" creator utilities I've written in the
    past, which also restricted the tests to only run on Darwin systems
    because I was using the system headers for Mach-O constant values.
    
    rdar://110663219
    (cherry picked from commit a0c2d6e369a1fb4d8b3ed46baed7a2de2fb3d882)

---
+++ lldb/tools/yaml2macho-core/yaml2macho.cpp
@@ -0,0 +1,251 @@
+//===-- main.cppp ---------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "CoreSpec.h"
+#include "LCNoteWriter.h"
+#include "MemoryWriter.h"
+#include "ThreadWriter.h"
+#include "Utility.h"
+#include "llvm/BinaryFormat/MachO.h"
+#include "llvm/Support/CommandLine.h"
+#include <getopt.h>
+#include <stdio.h>
+#include <string>
+#include <sys/stat.h>
+
+std::vector<std::string> get_fields_from_delimited_string(std::string str,
+                                                          const char delim) {
+  std::vector<std::string> result;
+  std::string::size_type prev = std::string::npos;
+  std::string::size_type next = str.find(delim);
+  if (str.empty()) {
+    return result;
+  }
+  if (next == std::string::npos) {
+    result.push_back(str);
+  } else {
+    result.push_back(std::string(str, 0, next));
+    prev = next;
+    while ((next = str.find(delim, prev + 1)) != std::string::npos) {
+      result.push_back(std::string(str, prev + 1, next - prev - 1));
+      prev = next;
+    }
+    result.push_back(std::string(str, prev + 1));
+  }
+  return result;
+}
+
+llvm::cl::opt<std::string> InputFilename("i", llvm::cl::Required,
+                                         llvm::cl::desc("input yaml filename"),
+                                         llvm::cl::value_desc("input"));
+llvm::cl::opt<std::string>
+    OutputFilename("o", llvm::cl::Required,
+                   llvm::cl::desc("output core filenames"),
+                   llvm::cl::value_desc("output"));
+llvm::cl::list<std::string>
+    UUIDs("u", llvm::cl::desc("uuid of binary loaded at slide 0"),
+          llvm::cl::value_desc("uuid"));
+llvm::cl::list<std::string>
+    UUIDAndVAs("L", llvm::cl::desc("UUID,virtual-address-loaded-at"),
+               llvm::cl::value_desc("--uuid-and-load-addr"));
+llvm::cl::opt<int>
+    AddressableBitsOverride("A",
+                            llvm::cl::desc("number of bits used in addressing"),
+                            llvm::cl::value_desc("--address-bits"));
+
+int main(int argc, char **argv) {
+  llvm::cl::ParseCommandLineOptions(argc, argv);
+
+  if (InputFilename.empty() || OutputFilename.empty()) {
+    fprintf(stderr, "Missing input or outpur file.\n");
+    exit(1);
+  }
+
+  struct stat sb;
+
+  if (stat(InputFilename.c_str(), &sb) == -1) {
+    fprintf(stderr, "Unable to stat %s, exiting\n", InputFilename.c_str());
+    exit(1);
+  }
+
+  FILE *input = fopen(InputFilename.c_str(), "r");
+  if (!input) {
+    fprintf(stderr, "Unable to open %s, exiting\n", InputFilename.c_str());
+    exit(1);
+  }
+  auto file_corespec = std::make_unique<char[]>(sb.st_size);
+  if (fread(file_corespec.get(), sb.st_size, 1, input) != 1) {
+    fprintf(stderr, "Unable to read all of %s, exiting\n",
+            InputFilename.c_str());
+    exit(1);
+  }
+  CoreSpec spec = from_yaml(file_corespec.get(), sb.st_size);
+  fclose(input);
+
+  for (const std::string &uuid : UUIDs) {
+    Binary binary;
+    binary.uuid = uuid;
+    binary.value = 0;
+    binary.value_is_slide = true;
+    spec.binaries.push_back(binary);
+  }
+
+  for (const std::string &uuid_and_va : UUIDAndVAs) {
+    std::vector<std::string> parts =
+        get_fields_from_delimited_string(uuid_and_va, ',');
+
+    std::string uuid = parts[0];
+    uint64_t va = std::strtoull(parts[1].c_str(), nullptr, 16);
+    Binary binary;
+    binary.uuid = uuid;
+    binary.value = va;
+    binary.value_is_slide = false;
+    spec.binaries.push_back(binary);
+  }
+
+  if (AddressableBitsOverride) {
+    AddressableBits bits;
+    bits.lowmem_bits = bits.highmem_bits = AddressableBitsOverride;
+    spec.addressable_bits = bits;
+  }
+
+  // An array of load commands
+  std::vector<std::vector<uint8_t>> load_commands;
+
+  // An array of corefile contents (memory regions)
+  std::vector<uint8_t> payload;
+
+  // First add all the load commands / payload so we can figure out how large
+  // the load commands will be.
+
+  add_lc_threads(spec, load_commands);
+  for (size_t i = 0; i < spec.memory_regions.size(); i++) {
+    std::vector<uint8_t> segment_command_bytes;
+    create_lc_segment_cmd(spec, segment_command_bytes, spec.memory_regions[i],
+                          0);
+    load_commands.push_back(segment_command_bytes);
+  }
+
+  if (spec.binaries.size() > 0)
+    for (const Binary &binary : spec.binaries) {
+      std::vector<uint8_t> segment_command_bytes;
+      std::vector<uint8_t> payload_bytes;
+      create_lc_note_binary_load_cmd(spec, segment_command_bytes, binary,
+                                     payload_bytes, 0);
+      load_commands.push_back(segment_command_bytes);
+    }
+  if (spec.addressable_bits) {
+    std::vector<uint8_t> segment_command_bytes;
+    std::vector<uint8_t> payload_bytes;
+    create_lc_note_addressable_bits(spec, segment_command_bytes,
+                                    *spec.addressable_bits, payload_bytes, 0);
+    load_commands.push_back(segment_command_bytes);
+  }
+
+  off_t size_of_load_commands = 0;
+  for (const auto &lc : load_commands)
+    size_of_load_commands += lc.size();
+
+  off_t header_and_load_cmd_room =
+      sizeof(llvm::MachO::mach_header_64) + size_of_load_commands;
+  off_t initial_payload_fileoff = header_and_load_cmd_room;
+  initial_payload_fileoff = (initial_payload_fileoff + 4096 - 1) & ~(4096 - 1);
+  off_t payload_fileoff = initial_payload_fileoff;
+
+  // Erase the load commands / payload now that we know how much space is
+  // needed, redo it with real values.
+  load_commands.clear();
+  payload.clear();
+
+  add_lc_threads(spec, load_commands);
+  for (size_t i = 0; i < spec.memory_regions.size(); i++) {
+    std::vector<uint8_t> segment_command_bytes;
+    create_lc_segment_cmd(spec, segment_command_bytes, spec.memory_regions[i],
+                          payload_fileoff);
+    load_commands.push_back(segment_command_bytes);
+    payload_fileoff += spec.memory_regions[i].size;
+    payload_fileoff = (payload_fileoff + 4096 - 1) & ~(4096 - 1);
+  }
+
+  off_t payload_fileoff_before_lcnotes = payload_fileoff;
+  std::vector<uint8_t> lc_note_payload_bytes;
+  if (spec.binaries.size() > 0)
+    for (const Binary &binary : spec.binaries) {
+      std::vector<uint8_t> segment_command_bytes;
+      std::vector<uint8_t> payload_bytes;
+      create_lc_note_binary_load_cmd(spec, segment_command_bytes, binary,
+                                     lc_note_payload_bytes, payload_fileoff);
+      payload_fileoff =
+          payload_fileoff_before_lcnotes + lc_note_payload_bytes.size();
+      load_commands.push_back(segment_command_bytes);
+    }
+  if (spec.addressable_bits) {
+    std::vector<uint8_t> segment_command_bytes;
+    std::vector<uint8_t> payload_bytes;
+    create_lc_note_addressable_bits(spec, segment_command_bytes,
+                                    *spec.addressable_bits,
+                                    lc_note_payload_bytes, payload_fileoff);
+    payload_fileoff =
+        payload_fileoff_before_lcnotes + lc_note_payload_bytes.size();
+    load_commands.push_back(segment_command_bytes);
+  }
+
+  // Realign our payload offset if we added any LC_NOTEs.
+  if (lc_note_payload_bytes.size() > 0)
+    payload_fileoff = (payload_fileoff + 4096 - 1) & ~(4096 - 1);
+
+  FILE *f = fopen(OutputFilename.c_str(), "w");
+  if (f == nullptr) {
+    fprintf(stderr, "Unable to open file %s for writing\n",
+            OutputFilename.c_str());
+    exit(1);
+  }
+
+  std::vector<uint8_t> mh;
+  // Write the fields of a mach_header_64 struct
+  if (spec.wordsize == 8)
+    add_uint32(mh, llvm::MachO::MH_MAGIC_64); // magic
+  else
+    add_uint32(mh, llvm::MachO::MH_MAGIC); // magic
+  add_uint32(mh, spec.cputype);            // cputype
+  add_uint32(mh, spec.cpusubtype);         // cpusubtype
+  add_uint32(mh, llvm::MachO::MH_CORE);    // filetype
+  add_uint32(mh, load_commands.size());    // ncmds
+  add_uint32(mh, size_of_load_commands);   // sizeofcmds
+  add_uint32(mh, 0);                       // flags
+  if (spec.wordsize == 8)
+    add_uint32(mh, 0); // reserved
+
+  fwrite(mh.data(), mh.size(), 1, f);
+
+  for (const auto &lc : load_commands)
+    fwrite(lc.data(), lc.size(), 1, f);
+
+  // Reset the payload offset back to the first one.
+  payload_fileoff = initial_payload_fileoff;
+  if (spec.memory_regions.size() > 0) {
+    for (size_t i = 0; i < spec.memory_regions.size(); i++) {
+      std::vector<uint8_t> bytes;
+      create_memory_bytes(spec, spec.memory_regions[i], bytes);
+      fseek(f, payload_fileoff, SEEK_SET);
+      fwrite(bytes.data(), bytes.size(), 1, f);
+
+      payload_fileoff += bytes.size();
+      payload_fileoff = (payload_fileoff + 4096 - 1) & ~(4096 - 1);
+    }
+  }
+
+  if (lc_note_payload_bytes.size() > 0) {
+    fseek(f, payload_fileoff, SEEK_SET);
+    fwrite(lc_note_payload_bytes.data(), lc_note_payload_bytes.size(), 1, f);
+    payload_fileoff += lc_note_payload_bytes.size();
+    payload_fileoff = (payload_fileoff + 4096 - 1) & ~(4096 - 1);
+  }
+
+  fclose(f);
+}
