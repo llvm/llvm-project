18cfc9e6f9283a64b369cbf1adc4a38b9a478478
Julian Morillo <julian.morillo@bsc.es>
2025-09-04 17:41:23 +0200

    Mark as UNSUPPORTED some OMPT tests that fail on RISC-V

--- openmp/runtime/test/ompt/misc/control_tool.c
+++ openmp/runtime/test/ompt/misc/control_tool.c
@@ -2,6 +2,7 @@
 // RUN: %libomp-compile-and-run | FileCheck %s
 // REQUIRES: ompt
 // UNSUPPORTED: gcc-4, gcc-5, gcc-6, gcc-7
+// UNSUPPORTED: riscv64
 // clang-format on
 #define TEST_NEED_PRINT_FRAME_FROM_OUTLINED_FN
 #include "callback.h"
