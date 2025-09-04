7421d37bca6e9271abad631542ec468fc4336e25
c8ef <c8ef@outlook.com>
2025-09-05 00:28:28 +0800

    address review comments

--- libc/include/llvm-libc-macros/complex-macros.h
+++ libc/include/llvm-libc-macros/complex-macros.h
@@ -9,7 +9,8 @@
 #ifndef __LLVM_LIBC_MACROS_COMPLEX_MACROS_H
 #define __LLVM_LIBC_MACROS_COMPLEX_MACROS_H
 
-#include "float16-macros.h"
+#include "cfloat128-macros.h"
+#include "cfloat16-macros.h"
 
 #ifndef __STDC_NO_COMPLEX__
 
@@ -25,14 +26,19 @@
 #define CMPLXF(x, y) __builtin_complex((float)(x), (float)(y))
 #define CMPLXL(x, y) __builtin_complex((long double)(x), (long double)(y))
 
-#ifdef LIBC_TYPES_HAS_FLOAT16
-#define CMPLXF16(x, y) __builtin_complex((float16)(x), (float16)(y))
-#endif // LIBC_TYPES_HAS_FLOAT16
-
-#ifdef LIBC_TYPES_HAS_FLOAT128
-#define CMPLXF128(x, y) __builtin_complex((float128)(x), (float128)(y))
-#endif // LIBC_TYPES_HAS_FLOAT128
-
+#ifdef LIBC_TYPES_HAS_CFLOAT16
+#if !defined(__clang__) || (__clang_major__ >= 22 && __clang_minor__ > 0)
+#define CMPLXF16(x, y) __builtin_complex((_Float16)(x), (_Float16)(y))
+#else
+#define CMPLXF16(x, y)                                                         \
+  ((complex _Float16)(__builtin_complex((float)(x), (float)(y))))
 #endif
+#endif // LIBC_TYPES_HAS_CFLOAT16
+
+#ifdef LIBC_TYPES_HAS_CFLOAT128
+#define CMPLXF128(x, y) __builtin_complex((float128)(x), (float128)(y))
+#endif // LIBC_TYPES_HAS_CFLOAT128
+
+#endif // __STDC_NO_COMPLEX__
 
 #endif // __LLVM_LIBC_MACROS_COMPLEX_MACROS_H
