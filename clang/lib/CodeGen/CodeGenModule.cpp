33a92e00d7acf0041a7d1579cf7a7d3bc64386d4
Anatoly Trosinenko <atrosinenko@accesssoftek.com>
2025-09-05 00:11:23 +0300

    [WIP] Set common attributes on the resolver functions

--- clang/lib/CodeGen/CodeGenModule.cpp
+++ clang/lib/CodeGen/CodeGenModule.cpp
@@ -4537,6 +4537,8 @@ void CodeGenModule::emitMultiVersionFunctions() {
     llvm::Function *ResolverFunc = cast<llvm::Function>(ResolverConstant);
 
     ResolverFunc->setLinkage(getMultiversionLinkage(*this, GD));
+    setGlobalVisibility(ResolverFunc, /*D=*/nullptr);
+    setDSOLocal(ResolverFunc);
 
     if (!ResolverFunc->hasLocalLinkage() && supportsCOMDAT())
       ResolverFunc->setComdat(
@@ -4601,6 +4603,9 @@ void CodeGenModule::emitCPUDispatchDefinition(GlobalDecl GD) {
   auto *ResolverFunc = cast<llvm::Function>(GetOrCreateLLVMFunction(
       ResolverName, ResolverType, ResolverGD, /*ForVTable=*/false));
   ResolverFunc->setLinkage(getMultiversionLinkage(*this, GD));
+  setGlobalVisibility(ResolverFunc, /*D=*/nullptr);
+  setDSOLocal(ResolverFunc);
+
   if (supportsCOMDAT())
     ResolverFunc->setComdat(
         getModule().getOrInsertComdat(ResolverFunc->getName()));
