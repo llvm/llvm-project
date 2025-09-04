21c889c5848f93c340c23c237f13570e200be34e
Jason Molenda <jmolenda@apple.com>
2025-09-04 11:42:26 -0700

    [lldb] Add utility to create Mach-O corefile from YAML desc (#153911)
    I've wanted a utility to create a corefile for test purposes given a bit
    of memory and regsters, for a while. I've written a few API tests over
    the years that needed exactly this capability -- we have several one-off
    Mach-O corefile creator utility in the API testsuite to do this. But
    it's a lot of boilerplate when you only want to specify some register
    contents and memory contents, to create an API test.
    
    This adds yaml2mach-core, a tool that should build on any system, takes
    a yaml description of register values for one or more threads,
    optionally memory values for one or more memory regions, and can take a
    list of UUIDs that will be added as LC_NOTE "load binary" metadata to
    the corefile so binaries can be loaded into virtual address space in a
    test scenario.
    
    The format of the yaml file looks like
    
    ```
    cpu: armv7m
    
    # optionally specify the number of bits used for addressing
    # (this line is from a different, 64-bit, yaml file)
    addressable-bits: 
        num-bits: 39
    
    # optionally specify one or more binary UUID and slide/virtual address to be added as an LC_NOTE
    # (this line is from a different, 64-bit, yaml file)
    binaries:
      - name: debug-binary.development
        uuid: 67942352-5857-3D3D-90CB-A3F80BA67B04
        virtual-address: 0xfffffff01840c000
    
    threads:
      - regsets: 
         - flavor: gpr 
            registers: [{name: sp, value: 0x2000fe70}, {name: r7, value: 0x2000fe80}, 
                        {name: pc, value: 0x0020392c}, {name: lr, value: 0x0020392d}]
    
    memory-regions:
      # stack memory
      - addr: 0x2000fe70 
        UInt32: [ 0x0000002a, 0x20010e58, 0x00203923, 
                  0x00000001, 0x2000fe88, 0x00203911, 
                  0x2000ffdc, 0xfffffff9 ]
      # instructions of a function
      - addr: 0x203910 
         UInt8: [ 0xf8, 0xb5, 0x04, 0xaf, 0x06, 0x4c, 0x07, 0x49, 
                  0x74, 0xf0, 0x2e, 0xf8, 0x01, 0xac, 0x74, 0xf0 ]
    ```
    
    and that's all that is needed to specify a corefile where four register
    values are specified (the others will be set to 0), and two memory
    regions will be emitted.
    
    The memory can be specified as an array of UInt8, UInt32, or UInt64, I
    anticipate that some of these corefiles may have stack values
    constructed manually and it may be simpler for a human to write them in
    a particular grouping of values.
    
    I needed this utility for an upcoming patch for ARM Cortex-M processors,
    to create a test for the change. I took the opportunity to remove two of
    the "trivial mach-o corefile" creator utilities I've written in the
    past, which also restricted the tests to only run on Darwin systems
    because I was using the system headers for Mach-O constant values.
    
    rdar://110663219

---
+++ lldb/tools/yaml2macho-core/CoreSpec.cpp
@@ -0,0 +1,165 @@
+//===-- CoreSpec.cpp ------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "CoreSpec.h"
+#include "llvm/BinaryFormat/MachO.h"
+#include "llvm/Support/YAMLTraits.h"
+#include <stdio.h>
+#include <string>
+
+using llvm::yaml::Input;
+using llvm::yaml::IO;
+using llvm::yaml::MappingTraits;
+
+template <> struct MappingTraits<RegisterNameAndValue> {
+  static void mapping(IO &io, RegisterNameAndValue &name_value) {
+    io.mapRequired("name", name_value.name);
+    io.mapRequired("value", name_value.value);
+  }
+};
+LLVM_YAML_IS_SEQUENCE_VECTOR(RegisterNameAndValue)
+
+template <> struct llvm::yaml::ScalarEnumerationTraits<RegisterFlavor> {
+  static void enumeration(IO &io, RegisterFlavor &flavor) {
+    io.enumCase(flavor, "gpr", RegisterFlavor::GPR);
+    io.enumCase(flavor, "fpr", RegisterFlavor::FPR);
+    io.enumCase(flavor, "exc", RegisterFlavor::EXC);
+  }
+};
+
+template <> struct MappingTraits<RegisterSet> {
+  static void mapping(IO &io, RegisterSet &regset) {
+    io.mapRequired("flavor", regset.flavor);
+    io.mapRequired("registers", regset.registers);
+  }
+};
+LLVM_YAML_IS_SEQUENCE_VECTOR(RegisterSet)
+
+template <> struct MappingTraits<Thread> {
+  static void mapping(IO &io, Thread &thread) {
+    io.mapRequired("regsets", thread.regsets);
+  }
+};
+LLVM_YAML_IS_SEQUENCE_VECTOR(Thread)
+
+template <> struct MappingTraits<MemoryRegion> {
+  static void mapping(IO &io, MemoryRegion &memory) {
+    io.mapRequired("addr", memory.addr);
+    io.mapOptional("UInt8", memory.bytes);
+    io.mapOptional("UInt32", memory.words);
+    io.mapOptional("UInt64", memory.doublewords);
+
+    if (memory.bytes.size()) {
+      memory.type = MemoryType::UInt8;
+      memory.size = memory.bytes.size();
+    } else if (memory.words.size()) {
+      memory.type = MemoryType::UInt32;
+      memory.size = memory.words.size() * 4;
+    } else if (memory.doublewords.size()) {
+      memory.type = MemoryType::UInt64;
+      memory.size = memory.doublewords.size() * 8;
+    }
+  }
+};
+LLVM_YAML_IS_SEQUENCE_VECTOR(MemoryRegion)
+
+template <> struct MappingTraits<Binary> {
+  static void mapping(IO &io, Binary &binary) {
+    io.mapOptional("name", binary.name);
+    io.mapRequired("uuid", binary.uuid);
+    std::optional<uint64_t> va, slide;
+    io.mapOptional("virtual-address", va);
+    io.mapOptional("slide", slide);
+    if (va && *va != UINT64_MAX) {
+      binary.value_is_slide = false;
+      binary.value = *va;
+    } else if (slide && *slide != UINT64_MAX) {
+      binary.value_is_slide = true;
+      binary.value = *slide;
+    } else {
+      fprintf(stderr,
+              "No virtual-address or slide specified for binary %s, aborting\n",
+              binary.uuid.c_str());
+      exit(1);
+    }
+  }
+};
+LLVM_YAML_IS_SEQUENCE_VECTOR(Binary)
+
+template <> struct llvm::yaml::MappingTraits<AddressableBits> {
+  static void mapping(IO &io, AddressableBits &addr_bits) {
+    std::optional<int> addressable_bits;
+    io.mapOptional("num-bits", addressable_bits);
+    if (addressable_bits) {
+      addr_bits.lowmem_bits = *addressable_bits;
+      addr_bits.highmem_bits = *addressable_bits;
+    } else {
+      io.mapOptional("lowmem-num-bits", addr_bits.lowmem_bits);
+      io.mapOptional("highmem-num-bits", addr_bits.highmem_bits);
+    }
+  }
+};
+
+template <> struct MappingTraits<CoreSpec> {
+  static void mapping(IO &io, CoreSpec &corespec) {
+    std::string cpuname;
+    io.mapRequired("cpu", cpuname);
+    if (cpuname == "armv7m") {
+      corespec.cputype = llvm::MachO::CPU_TYPE_ARM;
+      corespec.cpusubtype = llvm::MachO::CPU_SUBTYPE_ARM_V7M;
+    } else if (cpuname == "armv7") {
+      corespec.cputype = llvm::MachO::CPU_TYPE_ARM;
+      corespec.cpusubtype = llvm::MachO::CPU_SUBTYPE_ARM_ALL;
+    } else if (cpuname == "riscv") {
+      corespec.cputype = llvm::MachO::CPU_TYPE_RISCV;
+      corespec.cpusubtype = llvm::MachO::CPU_SUBTYPE_RISCV_ALL;
+    } else if (cpuname == "arm64") {
+      corespec.cputype = llvm::MachO::CPU_TYPE_ARM64;
+      corespec.cpusubtype = llvm::MachO::CPU_SUBTYPE_ARM64_ALL;
+    } else {
+      fprintf(stderr, "Unrecognized cpu name %s, exiting.\n", cpuname.c_str());
+      exit(1);
+    }
+    io.mapOptional("threads", corespec.threads);
+    io.mapOptional("memory-regions", corespec.memory_regions);
+    if (corespec.cputype == llvm::MachO::CPU_TYPE_ARM ||
+        corespec.cputype == llvm::MachO::CPU_TYPE_RISCV)
+      corespec.wordsize = 4;
+    else if (corespec.cputype == llvm::MachO::CPU_TYPE_ARM64)
+      corespec.wordsize = 8;
+    else {
+      fprintf(stderr,
+              "Unrecognized cputype, could not set wordsize, exiting.\n");
+      exit(1);
+    }
+    io.mapOptional("addressable-bits", corespec.addressable_bits);
+    io.mapOptional("binaries", corespec.binaries);
+    if (corespec.addressable_bits) {
+      if (!corespec.addressable_bits->lowmem_bits)
+        corespec.addressable_bits->lowmem_bits = corespec.wordsize * 8;
+      if (!corespec.addressable_bits->highmem_bits)
+        corespec.addressable_bits->highmem_bits = corespec.wordsize * 8;
+    }
+  }
+};
+
+CoreSpec from_yaml(char *buf, size_t len) {
+  llvm::StringRef file_corespec_strref(buf, len);
+
+  Input yin(file_corespec_strref);
+
+  CoreSpec v;
+  yin >> v;
+
+  if (yin.error()) {
+    fprintf(stderr, "Unable to parse YAML, exiting\n");
+    exit(1);
+  }
+
+  return v;
+}
