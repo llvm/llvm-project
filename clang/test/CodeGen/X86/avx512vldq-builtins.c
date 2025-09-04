a6a93ba68fd536e96a3219b9e8d79350b35ff2ac
moorabbit <moorabbit@proton.me>
2025-09-04 16:12:08 -0400

    _mm_maskz_cvtepi64_pd

--- clang/test/CodeGen/X86/avx512vldq-builtins.c
+++ clang/test/CodeGen/X86/avx512vldq-builtins.c
@@ -458,6 +458,8 @@ __m128d test_mm_maskz_cvtepi64_pd(__mmask8 __U, __m128i __A) {
   return _mm_maskz_cvtepi64_pd(__U, __A); 
 }
 
+TEST_CONSTEXPR(match_m128d(_mm_maskz_cvtepi64_pd(/*01=*/0x1, (__m128i)(__v2di){-1, -1}), -1.0, 0.0));
+
 __m256d test_mm256_cvtepi64_pd(__m256i __A) {
   // CHECK-LABEL: test_mm256_cvtepi64_pd
   // CHECK: sitofp <4 x i64> %{{.*}} to <4 x double>
