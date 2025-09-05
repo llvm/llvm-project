ee418d8330e042bbb690f791f0000e9ff9f23f25
Shaoce SUN <sunshaoce@outlook.com>
2025-09-05 21:16:35 +0800

    [RISCV][GISel] Lower G_ABDS and G_ABDU (#155888)
    Implementation follows the `ISD::ABDS` handling in
    `RISCVTargetLowering`.

--- llvm/include/llvm/CodeGen/GlobalISel/LegalizerHelper.h
+++ llvm/include/llvm/CodeGen/GlobalISel/LegalizerHelper.h
@@ -522,6 +522,8 @@ public:
   LLVM_ABI LegalizeResult lowerAbsToAddXor(MachineInstr &MI);
   LLVM_ABI LegalizeResult lowerAbsToMaxNeg(MachineInstr &MI);
   LLVM_ABI LegalizeResult lowerAbsToCNeg(MachineInstr &MI);
+  LLVM_ABI LegalizeResult lowerAbsDiffToSelect(MachineInstr &MI);
+  LLVM_ABI LegalizeResult lowerAbsDiffToMinMax(MachineInstr &MI);
   LLVM_ABI LegalizeResult lowerFAbs(MachineInstr &MI);
   LLVM_ABI LegalizeResult lowerVectorReduction(MachineInstr &MI);
   LLVM_ABI LegalizeResult lowerMemcpyInline(MachineInstr &MI);
