084c55d34b8cb07be6b23dfa132bd17dec12365d
Jan Patrick Lehr <JanPatrick.Lehr@amd.com>
2025-09-05 22:23:20 +0200

    [Offload] Run tests 16-way parallel on AMDGPU (#156627)
    Reduce the number of paralell tests run to align with the typical number
    of VMIDs provided by the kernel driver.

--- offload/cmake/caches/AMDGPUBot.cmake
+++ offload/cmake/caches/AMDGPUBot.cmake
@@ -15,7 +15,7 @@ set(LLVM_ENABLE_RUNTIMES "compiler-rt;openmp;offload;flang-rt" CACHE STRING "")
 set(LLVM_ENABLE_PER_TARGET_RUNTIME_DIR ON CACHE BOOL "")
 set(LLVM_ENABLE_ASSERTIONS ON CACHE BOOL "")
 set(LLVM_TARGETS_TO_BUILD "host;AMDGPU;SPIRV" CACHE STRING "")
-set(LLVM_LIT_ARGS "-v --show-unsupported --timeout 100 --show-xfail -j 32" CACHE STRING "")
+set(LLVM_LIT_ARGS "-v --show-unsupported --timeout 100 --show-xfail -j 16" CACHE STRING "")
 
 set(CLANG_DEFAULT_LINKER "lld" CACHE STRING "")
 set(CLANG_DEFAULT_RTLIB "compiler-rt" STRING "")
