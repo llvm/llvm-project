ddbec97bd06c962eb66f8886228aa991d516f27f
kikairoya <kikairoya@gmail.com>
2025-09-05 22:14:53 +0900

    not to use ld.lld

--- lld/test/COFF/reloc-undefined-weak.s
+++ lld/test/COFF/reloc-undefined-weak.s
@@ -3,17 +3,17 @@
 // Check that base-relocations for unresolved weak symbols will be omitted.
 
 // RUN: rm -rf %t.dir && split-file %s %t.dir && cd %t.dir
-// RUN: llvm-mc -filetype=obj -triple=x86_64-mingw main.s -o main.o
-// RUN: llvm-mc -filetype=obj -triple=x86_64-mingw other.s -o other.o
+// RUN: llvm-mc -filetype=obj -triple=x86_64-mingw main.s -o main.obj
+// RUN: llvm-mc -filetype=obj -triple=x86_64-mingw other.s -o other.obj
 
-// RUN: ld.lld -m i386pep -dll -o other.dll other.o -entry= --export-all-symbols --out-implib other.dll.a
-// RUN: ld.lld -m i386pep -o main.exe main.o other.dll.a -e entry --wrap foo --verbose
+// RUN: lld-link -lldmingw -machine:x64 -dll -out:other.dll other.obj -noentry -export:foo -implib:other.lib
+// RUN: lld-link -lldmingw -machine:x64 -out:main.exe main.obj other.lib -entry:entry -wrap:foo -debug:symtab
 // RUN: llvm-readobj --sections --symbols --coff-imports --coff-basereloc main.exe | FileCheck %s --implicit-check-not=other.dll
 
-// CHECK:      Number: 4
+// CHECK:      Number: 3
 // CHECK-NEXT: Name: .data
 // CHECK-NEXT: VirtualSize:
-// CHECK-NEXT: VirtualAddress: 0x[[#%x,SECTOP:0x4000]]
+// CHECK-NEXT: VirtualAddress: 0x[[#%x,SECTOP:0x3000]]
 // CHECK:      Name: ref_foo
 // CHECK-NEXT: Value: [[#%d,SYMVAL:]]
 // CHECK:      BaseReloc [
