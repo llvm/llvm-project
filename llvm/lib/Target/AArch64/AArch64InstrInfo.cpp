4c5692d4f8b5cdd5d6d83b801f30a2a290118c17
Kerry McLaughlin <kerry.mclaughlin@arm.com>
2025-09-05 09:04:03 +0000

    - Run clang-format

--- llvm/lib/Target/AArch64/AArch64InstrInfo.cpp
+++ llvm/lib/Target/AArch64/AArch64InstrInfo.cpp
@@ -1499,7 +1499,7 @@ AArch64InstrInfo::canRemovePTestInstr(MachineInstr *PTest, MachineInstr *Mask,
     // mask.
     if (isPTrueOpcode(MaskOpcode) && Mask->getOperand(1).getImm() == 31 &&
         getElementSizeForOpcode(MaskOpcode) ==
-        getElementSizeForOpcode(PredOpcode))
+            getElementSizeForOpcode(PredOpcode))
       return PredOpcode;
 
     return {};
