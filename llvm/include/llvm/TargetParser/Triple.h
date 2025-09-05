e1aa79537fb6771d90dcf5b7d474825fdc2e967f
Farzon Lotfi <farzonlotfi@microsoft.com>
2025-09-05 14:02:48 -0400

    [DirectX] Add isinf f16 emulation for SM6.8 and lower (#156932)
    fixes #156068
    
    - We needed to add a new sub arch to the target tripple so we can test
    that emulation does not happen when targeting SM6.9
    - The HLSL toolchain needed to be updated to handle the conversion of
    strings to enums for the new sub arch.
    - The emulation is done in DXILIntrinsicExpansion.cpp and needs to be
    able to convert both llvm.is.fpclass and lvm.dx.isinf to the proper
    emulation
    - test updates in TargetParser/TripleTest.cpp, isinf.ll, is_fpclass.ll,
    and DXCModeTest.cpp

--- llvm/include/llvm/TargetParser/Triple.h
+++ llvm/include/llvm/TargetParser/Triple.h
@@ -180,7 +180,8 @@ public:
     DXILSubArch_v1_6,
     DXILSubArch_v1_7,
     DXILSubArch_v1_8,
-    LatestDXILSubArch = DXILSubArch_v1_8,
+    DXILSubArch_v1_9,
+    LatestDXILSubArch = DXILSubArch_v1_9,
   };
   enum VendorType {
     UnknownVendor,
