c621084b9140745e584f483fb7fe27bec4e5461a
Lang Hames <lhames@gmail.com>
2025-09-05 22:39:54 +1000

    [orc-rt] Introduce WrapperFunction APIs. (#157091)
    Introduces the following key APIs:
    
    `orc_rt_WrapperFunction` defines the signature of an ORC asynchronous
    wrapper function:
    
    ```
    typedef void (*orc_rt_WrapperFunctionReturn)(
        orc_rt_SessionRef Session, void *CallCtx,
        orc_rt_WrapperFunctionBuffer ResultBytes);
    
    typedef void (*orc_rt_WrapperFunction)(orc_rt_SessionRef Session, void *CallCtx,
                                           orc_rt_WrapperFunctionReturn Return,
                                           orc_rt_WrapperFunctionBuffer ArgBytes);
    ```
    
    A wrapper function takes a reference to the session object, a context
    pointer for the call being made, and a pointer to an
    orc_rt_WrapperFunctionReturn function that can be used to send the
    result bytes.
    
    The `orc_rt::WrapperFunction` utility simplifies the writing of wrapper
    functions whose arguments and return values are serialized/deserialized
    using an abstract serialization utility.
    
    The `orc_rt::SPSWrapperFunction` utility provides a specialized version
    of `orc_rt::WrapperFunction` that uses SPS serialization.

--- orc-rt/include/orc-rt/WrapperFunction.h
+++ orc-rt/include/orc-rt/WrapperFunction.h
@@ -14,6 +14,8 @@
 #define ORC_RT_WRAPPERFUNCTION_H
 
 #include "orc-rt-c/WrapperFunction.h"
+#include "orc-rt/Error.h"
+#include "orc-rt/bind.h"
 
 #include <utility>
 
@@ -96,6 +98,164 @@ public:
 
 private:
   orc_rt_WrapperFunctionBuffer B;
+};
+
+namespace detail {
+
+template <typename C>
+struct WFCallableTraits
+    : public WFCallableTraits<
+          decltype(&std::remove_cv_t<std::remove_reference_t<C>>::operator())> {
+};
+
+template <typename RetT> struct WFCallableTraits<RetT()> {
+  typedef void HeadArgType;
+};
+
+template <typename RetT, typename ArgT, typename... ArgTs>
+struct WFCallableTraits<RetT(ArgT, ArgTs...)> {
+  typedef ArgT HeadArgType;
+  typedef std::tuple<ArgTs...> TailArgTuple;
+};
+
+template <typename ClassT, typename RetT, typename... ArgTs>
+struct WFCallableTraits<RetT (ClassT::*)(ArgTs...)>
+    : public WFCallableTraits<RetT(ArgTs...)> {};
+
+template <typename ClassT, typename RetT, typename... ArgTs>
+struct WFCallableTraits<RetT (ClassT::*)(ArgTs...) const>
+    : public WFCallableTraits<RetT(ArgTs...)> {};
+
+template <typename Serializer> class StructuredYieldBase {
+public:
+  StructuredYieldBase(orc_rt_SessionRef Session, void *CallCtx,
+                      orc_rt_WrapperFunctionReturn Return, Serializer &&S)
+      : Session(Session), CallCtx(CallCtx), Return(Return),
+        S(std::forward<Serializer>(S)) {}
+
+protected:
+  orc_rt_SessionRef Session;
+  void *CallCtx;
+  orc_rt_WrapperFunctionReturn Return;
+  std::decay_t<Serializer> S;
+};
+
+template <typename RetT, typename Serializer>
+class StructuredYield : public StructuredYieldBase<Serializer> {
+public:
+  using StructuredYieldBase<Serializer>::StructuredYieldBase;
+  void operator()(RetT &&R) {
+    if (auto ResultBytes = this->S.resultSerializer()(std::forward<RetT>(R)))
+      this->Return(this->Session, this->CallCtx, ResultBytes->release());
+    else
+      this->Return(this->Session, this->CallCtx,
+                   WrapperFunctionBuffer::createOutOfBandError(
+                       "Could not serialize wrapper function result data")
+                       .release());
+  }
+};
+
+template <typename Serializer>
+class StructuredYield<void, Serializer>
+    : public StructuredYieldBase<Serializer> {
+public:
+  using StructuredYieldBase<Serializer>::StructuredYieldBase;
+  void operator()() {
+    this->Return(this->Session, this->CallCtx,
+                 WrapperFunctionBuffer().release());
+  }
+};
+
+template <typename T, typename Serializer> struct ResultDeserializer;
+
+template <typename T, typename Serializer>
+struct ResultDeserializer<Expected<T>, Serializer> {
+  static Expected<T> deserialize(WrapperFunctionBuffer ResultBytes,
+                                 Serializer &S) {
+    T Val;
+    if (S.resultDeserializer()(ResultBytes, Val))
+      return std::move(Val);
+    else
+      return make_error<StringError>("Could not deserialize result");
+  }
+};
+
+template <typename Serializer> struct ResultDeserializer<Error, Serializer> {
+  static Error deserialize(WrapperFunctionBuffer ResultBytes, Serializer &S) {
+    assert(ResultBytes.empty());
+    return Error::success();
+  }
+};
+
+} // namespace detail
+
+/// Provides call and handle utilities to simplify writing and invocation of
+/// wrapper functions in C++.
+struct WrapperFunction {
+
+  /// Make a call to a wrapper function.
+  ///
+  /// This utility serializes and deserializes arguments and return values
+  /// (using the given Serializer), and calls the wrapper function via the
+  /// given Caller object.
+  template <typename Caller, typename Serializer, typename ResultHandler,
+            typename... ArgTs>
+  static void call(Caller &&C, Serializer &&S, ResultHandler &&RH,
+                   ArgTs &&...Args) {
+    typedef detail::WFCallableTraits<ResultHandler> ResultHandlerTraits;
+    static_assert(
+        std::tuple_size_v<typename ResultHandlerTraits::TailArgTuple> == 0,
+        "Expected one argument to result-handler");
+    typedef typename ResultHandlerTraits::HeadArgType ResultType;
+
+    if (auto ArgBytes = S.argumentSerializer()(std::forward<ArgTs>(Args)...)) {
+      C(
+          [RH = std::move(RH),
+           S = std::move(S)](orc_rt_SessionRef Session,
+                             WrapperFunctionBuffer ResultBytes) mutable {
+            if (const char *ErrMsg = ResultBytes.getOutOfBandError())
+              RH(make_error<StringError>(ErrMsg));
+            else
+              RH(detail::ResultDeserializer<
+                  ResultType, Serializer>::deserialize(std::move(ResultBytes),
+                                                       S));
+          },
+          std::move(*ArgBytes));
+    } else
+      RH(make_error<StringError>(
+          "Could not serialize wrapper function call arguments"));
+  }
+
+  /// Simplifies implementation of wrapper functions in C++.
+  ///
+  /// This utility deserializes and serializes arguments and return values
+  /// (using the given Serializer), and calls the given handler.
+  template <typename Serializer, typename Handler>
+  static void handle(orc_rt_SessionRef Session, void *CallCtx,
+                     orc_rt_WrapperFunctionReturn Return,
+                     WrapperFunctionBuffer ArgBytes, Serializer &&S,
+                     Handler &&H) {
+    typedef detail::WFCallableTraits<Handler> HandlerTraits;
+    typedef typename HandlerTraits::HeadArgType Yield;
+    typedef typename HandlerTraits::TailArgTuple ArgTuple;
+    typedef typename detail::WFCallableTraits<Yield>::HeadArgType RetType;
+
+    if (ArgBytes.getOutOfBandError())
+      return Return(Session, CallCtx, ArgBytes.release());
+
+    ArgTuple Args;
+    if (std::apply(bind_front(S.argumentDeserializer(), std::move(ArgBytes)),
+                   Args))
+      std::apply(bind_front(std::forward<Handler>(H),
+                            detail::StructuredYield<RetType, Serializer>(
+                                Session, CallCtx, Return, std::move(S))),
+                 std::move(Args));
+    else
+      Return(Session, CallCtx,
+             WrapperFunctionBuffer::createOutOfBandError(
+                 "Could not deserialize wrapper function arg data")
+                 .release());
+  }
 };
 
 } // namespace orc_rt
