24e06354db23260d9060f7f947104f155b73b6bc
David Spickett <david.spickett@linaro.org>
2025-09-08 14:07:48 +0000

    platform select instead

--- lldb/test/API/functionalities/gdb_remote_client/TestqOffsets.py
+++ lldb/test/API/functionalities/gdb_remote_client/TestqOffsets.py
@@ -10,13 +10,10 @@ class TestqOffsets(GDBRemoteTestBase):
         def qOffsets(self):
             return "Text=470000;Data=470000"
 
-        def qfThreadInfo(self):
-            # Prevent LLDB defaulting to PID of 1 and looking up some other
-            # process when on an AArch64 host.
-            return "m-1"
-
     def test(self):
         self.server.responder = TestqOffsets.Responder()
+        # This ensures that we do not pick up any binaries on the host.
+        self.runCmd("platform select remote-linux")
         target = self.createTarget("qOffsets.yaml")
         text = target.modules[0].FindSection(".text")
         self.assertEqual(text.GetLoadAddress(target), lldb.LLDB_INVALID_ADDRESS)
