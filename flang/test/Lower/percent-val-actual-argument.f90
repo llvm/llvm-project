b65fad21c576b138895d1fe77b493804a6a7094b
Carlos Seo <carlos.seo@linaro.org>
2025-09-08 15:18:37 +0000

    Fix the testcase to reflect latest changes
     * Fix test to reflect the passing by loaded value change.
     * Check the actual call.
     * Use flang -fc1 -emit-hlfir instead of bbc.

--- flang/test/Lower/percent-val-actual-argument.f90
+++ flang/test/Lower/percent-val-actual-argument.f90
@@ -1,11 +1,13 @@
-! RUN: bbc %s -emit-fir -o - | FileCheck %s
+! RUN: flang -fc1 -emit-hlfir %s -o - | FileCheck %s
 
 program main
   logical::a1
   data a1/.true./
   call sa(%val(a1))
-! CHECK: fir.load %3 : !fir.ref<!fir.logical<4>>
-! CHECK: fir.convert %13 : (!fir.logical<4>) -> i1
+! CHECK: %[[A1_ADDR:.*]] = fir.address_of(@_QFEa1) : !fir.ref<!fir.logical<4>>
+! CHECK: %[[A1_DECL:.*]]:2 = hlfir.declare %[[A1_ADDR]] {uniq_name = "_QFEa1"} : (!fir.ref<!fir.logical<4>>) -> (!fir.ref<!fir.logical<4>>, !fir.ref<!fir.logical<4>>)
+! CHECK: fir.call @_QPsa(%[[A1_DECL]]#0) fastmath<contract> : (!fir.ref<!fir.logical<4>>) -> ()
+! CHECK: func.func @_QPsa(%[[SA_ARG:.*]]: !fir.ref<!fir.logical<4>> {fir.bindc_name = "x1"}) {
   write(6,*) "a1 = ", a1
 end program main
 
