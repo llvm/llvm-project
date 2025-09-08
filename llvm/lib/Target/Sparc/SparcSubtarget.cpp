4d0a2de9aff1aac5a44f30155567036dd8c47d3e
Matt Arsenault <Matthew.Arsenault@amd.com>
2025-09-08 23:44:47 +0900

    Sparc: Add 64-bit feature (#157445)
    Add a 64-bit feature so a subtarget feature check can tell the
    pointer size, for future use with HwMode.
    
    This is kind of a hack, but this is closer to what other targets do.
    To use HwModes, there needs to be a subtarget feature. Every other
    target kludges the module level properties into a subtarget feature
    for use here, which requires pre/post processing the subtarget features.
    The APIs for this aren't great. I tried doing something different,
    closer to what hexagon does, rather than what x86 does to see if it
    was any nicer. It almost is, except for some reason we don't have an
    API to directly set a bit in the FeatureBitset.
    
    Also the test coverage for the different ABI options isn't great.
    e.g. v9 as a feature almost works, except a single test breaks
    that uses a sparc32 triple with an explicit v9 feature.

--- llvm/lib/Target/Sparc/SparcSubtarget.cpp
+++ llvm/lib/Target/Sparc/SparcSubtarget.cpp
@@ -28,16 +28,23 @@ void SparcSubtarget::anchor() { }
 
 SparcSubtarget &SparcSubtarget::initializeSubtargetDependencies(
     StringRef CPU, StringRef TuneCPU, StringRef FS) {
+  const Triple &TT = getTargetTriple();
   // Determine default and user specified characteristics
   std::string CPUName = std::string(CPU);
   if (CPUName.empty())
-    CPUName = getTargetTriple().isSPARC64() ? "v9" : "v8";
+    CPUName = TT.isSPARC64() ? "v9" : "v8";
 
   if (TuneCPU.empty())
     TuneCPU = CPUName;
 
   // Parse features string.
   ParseSubtargetFeatures(CPUName, TuneCPU, FS);
+
+  if (!Is64Bit && TT.isSPARC64()) {
+    FeatureBitset Features = getFeatureBits();
+    setFeatureBits(Features.set(Sparc::Feature64Bit));
+    Is64Bit = true;
+  }
 
   // Popc is a v9-only instruction.
   if (!IsV9)
@@ -50,7 +57,6 @@ SparcSubtarget::SparcSubtarget(const StringRef &CPU, const StringRef &TuneCPU,
                                const StringRef &FS, const TargetMachine &TM)
     : SparcGenSubtargetInfo(TM.getTargetTriple(), CPU, TuneCPU, FS),
       ReserveRegister(TM.getMCRegisterInfo()->getNumRegs()),
-      Is64Bit(TM.getTargetTriple().isSPARC64()),
       InstrInfo(initializeSubtargetDependencies(CPU, TuneCPU, FS)),
       TLInfo(TM, *this), FrameLowering(*this) {
   TSInfo = std::make_unique<SparcSelectionDAGInfo>();
