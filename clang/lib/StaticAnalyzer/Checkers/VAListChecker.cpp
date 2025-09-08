a3db20fdf3e75998cefaa7b0deeca7e1ad437609
Donát Nagy <donat.nagy@ericsson.com>
2025-09-08 15:06:46 +0200

    [NFC][analyzer] Rename `ValistChecker.cpp` to `VAListChecker.cpp` (#157438)
    ...to follow the capitalization style that was already applied within
    the file by recent commit a80c393a9c498279a1ec9fd630535b9ff139b49f.

--- clang/lib/StaticAnalyzer/Checkers/VAListChecker.cpp
+++ clang/lib/StaticAnalyzer/Checkers/VAListChecker.cpp
@@ -1,4 +1,4 @@
-//== ValistChecker.cpp - stdarg.h macro usage checker -----------*- C++ -*--==//
+//== VAListChecker.cpp - stdarg.h macro usage checker -----------*- C++ -*--==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
