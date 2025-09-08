c0f08a5823327e83f17e921e65d6234ce98fce6b
David Spickett <david.spickett@linaro.org>
2025-09-08 11:41:33 +0000

    [lldb][test] Prevent TestqOffsets.py picking up host binaries
    Due to a fallback in GDBRemoteCommunicationClient.cpp, on Linux we
    will assume a PID of 1 if the remote does not respond in some
    way that tells us the real PID.
    
    So if PID 1 happened to be a process that the current user could
    read, we would try to debug that instead. On my current machine,
    PID 1 was sshd run by root so we would ignore it. If I changed
    the fallback to some process ID run by my user, the test would fail.
    
    To prevent this, return something to LLDB that tells it there is
    no remote PID, preventing the host lookup.
    
    Fixes #155895

--- lldb/test/API/functionalities/gdb_remote_client/TestqOffsets.py
+++ lldb/test/API/functionalities/gdb_remote_client/TestqOffsets.py
@@ -10,6 +10,11 @@ class TestqOffsets(GDBRemoteTestBase):
         def qOffsets(self):
             return "Text=470000;Data=470000"
 
+        def qfThreadInfo(self):
+            # Prevent LLDB defaulting to PID of 1 and looking up some other
+            # process when on an AArch64 host.
+            return "m-1"
+
     def test(self):
         self.server.responder = TestqOffsets.Responder()
         target = self.createTarget("qOffsets.yaml")
