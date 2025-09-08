4f5d348f649114a8514a8c154a6245af6e78c15a
Aiden Grossman <aidengrossman@google.com>
2025-09-08 17:29:07 +0000

    [𝘀𝗽𝗿] changes introduced through rebase
    Created using spr 1.3.6
    
    [skip ci]

--- llvm/test/MC/COFF/stdin.py
+++ llvm/test/MC/COFF/stdin.py
@@ -1,14 +1,22 @@
 # RUN: echo "// comment" > %t.input
 # RUN: which llvm-mc | %python %s %t
 
+import argparse
 import subprocess
 import sys
 
+parser = argparse.ArgumentParser()
+parser.add_argument("temp_file")
+arguments = parser.parse_args()
+
 llvm_mc_binary = sys.stdin.readlines()[0].strip()
-temp_file = sys.argv[1]
+temp_file = arguments.temp_file
 input_file = temp_file + ".input"
 
 with open(temp_file, "w") as mc_stdout:
+    ## We need to test that starting on an input stream with a non-zero offset
+    ## does not trigger an assertion in WinCOFFObjectWriter.cpp, so we seek
+    ## past zero for STDOUT.
     mc_stdout.seek(4)
     subprocess.run(
         [llvm_mc_binary, "-filetype=obj", "-triple", "i686-pc-win32", input_file],
