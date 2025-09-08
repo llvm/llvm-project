f7dacb33be0ca6fb17ef8cf4559f3d4cdea9ebad
Pranav Bhandarkar <pranav.bhandarkar@amd.com>
2025-09-08 14:11:21 -0500

    Address more comments from tblah and meinersbur

--- mlir/include/mlir/Dialect/LLVMIR/Transforms/OpenMPOffloadPrivatizationPrepare.h
+++ mlir/include/mlir/Dialect/LLVMIR/Transforms/OpenMPOffloadPrivatizationPrepare.h
@@ -1,5 +1,4 @@
-//===- OpenMPOffloadPrivatizationPrepare.h - Prepare for OpenMP Offload
-// Privatization -*- C++ -*-===//
+//===- OpenMPOffloadPrivatizationPrepare.h -*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
