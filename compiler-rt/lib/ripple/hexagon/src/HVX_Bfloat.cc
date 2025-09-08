df1f5b807e860052be18259d9d0b57fb5aae53a1
Benoist Meister <benoit.meister@qti.qualcomm.com>
2025-09-08 01:13:06 -0400

    [Ripple][RT] Full bit-equivalence of to_bf16_round
    - Also optimized to_bf16_nan
    
    Reviewed-by: Muntasir Mallick <muntasir.mallick@qti.qualcomm.com>
    Reviewed-by: Sergei Larin <slarin@qti.qualcomm.com>

--- compiler-rt/lib/ripple/hexagon/src/HVX_Bfloat.cc
+++ compiler-rt/lib/ripple/hexagon/src/HVX_Bfloat.cc
@@ -20,15 +20,42 @@
 
 #if __has_bf16__
 
+/// \brief extracts either the 16-bit even (oe=e) or odd (oe=o) elements of x
+#define split(x, oe) Q6_Vh_vpack##oe##_VwVw(Q6_V_hi_W((x)), Q6_V_lo_W((x)))
+
 /// \brief Bare truncation:
 ///  - assumes that the elements of `in` are non-NaNs.
 ///  - Gets a sub-optimal rounding error from just truncating
 RIPPLE_INTRIN_INLINE v64bf16 ripple_ew_pure_to_bf16_trunc(v64f32 In) {
-  HVX_VectorPair Vin = hvx_cast_to_i32(In);
-  HVX_Vector Truncated = Q6_Vh_vpacko_VwVw(Q6_V_hi_W(Vin), Q6_V_lo_W(Vin));
-  __bf16 ForType = 0.0f;
-  return hvx_cast_from_i32(Truncated, ForType);
+  __bf16 ForType = 0;
+  return hvx_cast_from_i32(split(hvx_cast_to_i32(In), o), ForType);
 }
+
+/// \brief Truncation that's aware of NaNs.
+/// Use if there is a possibility that `in`
+/// contains NaNs, but if you don't care about the rounding error introduced
+/// by a bare truncation.
+#define shiftNan(V64I32In, b)                                                  \
+  ({                                                                           \
+    uint16_t ForType;                                                          \
+    v64u16 Trunc = hvx_cast_from_i32(split((V64I32In), o), ForType);           \
+    v64u16 CutMant = hvx_cast_from_i32(split((V64I32In), e), ForType);         \
+    ripple_block_t b = ripple_set_block_shape(0, 64);                          \
+    uint16_t Truncated = vec_to_ripple<64, uint16_t>(b, Trunc);                \
+    uint16_t CutMantissa = vec_to_ripple<64, uint16_t>(b, CutMant);            \
+    constexpr uint16_t Signless = 0x7FFF;                                      \
+    constexpr uint16_t Inf = 0x7F80;                                           \
+    constexpr uint16_t U16Nan = 0x7FC0;                                        \
+    uint16_t Sign = 0x8000 & Truncated;                                        \
+    uint16_t SignedNan = U16Nan | Sign;                                        \
+    bool TruncsToInf = (Signless & Truncated) == Inf;                          \
+    bool NonZeroCutMantissa = CutMantissa != 0;                                \
+    uint16_t TruncWithNan =                                                    \
+        (TruncsToInf & NonZeroCutMantissa) ? SignedNan : Truncated;            \
+    __bf16 Result; /* bitcast to __bf16 */                                     \
+    __builtin_memcpy(&Result, &TruncWithNan, sizeof(Result));                  \
+    ripple_to_vec<64, __bf16>(b, Result);                                      \
+  })
 
 /// \brief Truncation that's aware of NaNs.
 /// Use if there is a possibility that `in`
@@ -36,32 +63,26 @@ RIPPLE_INTRIN_INLINE v64bf16 ripple_ew_pure_to_bf16_trunc(v64f32 In) {
 /// by a bare truncation.
 RIPPLE_INTRIN_INLINE v64bf16 ripple_ew_pure_to_bf16_nan(v64f32 Vin) {
   ripple_block_t b = ripple_set_block_shape(0, 64);
-  float In = vec_to_ripple<64, float>(b, Vin);
-  __bf16 Trunc =
-      vec_to_ripple<64, __bf16>(b, ripple_ew_pure_to_bf16_trunc(Vin));
-  // An issue happens for NaNs whose highest mantissa bits are zero,
-  // unless all of them are zero.
-  __bf16 Bf16Nan = std::numeric_limits<__bf16>::quiet_NaN();
-  return ripple_to_vec<64, __bf16>(b, std::isnan(In) ? Bf16Nan : Trunc);
+  return shiftNan(hvx_cast_to_i32(Vin), b);
 }
 
-/// \brief most accurate conversion from float to __bf16.
+/// \brief most accurate conversion from float to __bf16: even rounding,
+/// NaN-aware.
 RIPPLE_INTRIN_INLINE v64bf16 ripple_ew_pure_to_bf16_round(v64f32 Vin) {
-  constexpr uint16_t Oxeight = 0x8000;
-  constexpr uint32_t Oxonef = 0x1FFFF;
+  constexpr uint32_t OxeightOOO = 0x00008000u;
+  constexpr uint32_t Oxonef = 0x0001FFFF;
   ripple_block_t b = ripple_set_block_shape(0, 64);
   float In = vec_to_ripple<64, float>(b, Vin);
   uint32_t bits;
   __builtin_memcpy(&bits, &In, sizeof(bits));
-  // no round up if exactly .5 and even already
-  if (((bits & Oxonef) != Oxeight) & ((bits & Oxeight) == Oxeight)) {
-    bits += Oxeight;
+  // no round up if exactly .5 and even already,
+  // or if it's a NaN or +/- inf
+  if (((bits & Oxonef) != OxeightOOO) & ((bits & OxeightOOO) == OxeightOOO) &
+      ((bits & 0x7F800000) != 0x7F800000)) {
+    bits += OxeightOOO;
   }
-  constexpr uint16_t u16_silent_nan = 0x7FC0;
-  uint16_t IntRes = isnan(In) ? u16_silent_nan : bits >> 16;
-  __bf16 Res;
-  __builtin_memcpy(&Res, &IntRes, sizeof(Res));
-  return ripple_to_vec<64, __bf16>(b, Res);
+  v64i32 PreRounded = hvx_cast_to_i32(ripple_to_vec<64, uint32_t>(b, bits));
+  return shiftNan(PreRounded, b);
 }
 
 #endif // __has_bf16__
