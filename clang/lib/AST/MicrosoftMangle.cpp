95f26e20f15cebd0ff71fbe0dbb3647e35ff0ebe
Michael Buch <michaelbuch12@gmail.com>
2025-09-09 09:50:11 +0100

    [clang][DebugInfo] Emit unified (Itanium) mangled name to structor declarations (#154142)
    Depends on https://github.com/llvm/llvm-project/pull/154137
    
    This patch is motivated by
    https://github.com/llvm/llvm-project/pull/149827, where we plan on using
    mangled names on structor declarations to find the exact structor
    definition that LLDB's expression evaluator should call.
    
    Given a `DW_TAG_subprogram` for a function declaration, the most
    convenient way for a debugger to find the corresponding definition is to
    use the `DW_AT_linkage_name` (i.e., the mangled name). However, we
    currently can't do that for constructors/destructors because Clang
    doesn't attach linkage names to them. This is because, depending on ABI,
    there can be multiple definitions for a single constructor/destructor
    declaration. The way GCC works around this is by producing a `C4`/`D4`
    "unified" mangling for structor declarations (see
    [godbolt](https://godbolt.org/z/Wds6cja9K)). GDB uses this to locate the
    relevant definitions.
    
    This patch aligns Clang with GCC's DWARF output and allows us to
    implement the same lookup scheme in LLDB.
    
    (cherry picked from commit 06d202b6cb13b38165af03dbf12a3beaac0b38ea)

--- clang/lib/AST/MicrosoftMangle.cpp
+++ clang/lib/AST/MicrosoftMangle.cpp
@@ -1497,6 +1497,8 @@ void MicrosoftCXXNameMangler::mangleCXXDtorType(CXXDtorType T) {
   // it.
   case Dtor_Comdat:
     llvm_unreachable("not expecting a COMDAT");
+  case Dtor_Unified:
+    llvm_unreachable("not expecting a unified dtor type");
   }
   llvm_unreachable("Unsupported dtor type?");
 }
