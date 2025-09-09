caddca5d0b1f6edece90b0ce49a63e004753dc7b
Joseph Huber <huberjn@outlook.com>
2025-09-09 18:52:48 -0500

    undo

--- libc/test/src/__support/CPP/simd_test.cpp
+++ libc/test/src/__support/CPP/simd_test.cpp
@@ -9,7 +9,6 @@
 #include "src/__support/CPP/simd.h"
 #include "src/__support/CPP/utility.h"
 
-#include "test/UnitTest/FPMatcher.h"
 #include "test/UnitTest/Test.h"
 
 static_assert(LIBC_HAS_VECTOR_TYPE, "compiler needs ext_vector_type support");
@@ -40,36 +39,14 @@ TEST(LlvmLibcSIMDTest, TypeTraits) {
 TEST(LlvmLibcSIMDTest, ElementwiseOperations) {
   cpp::simd<int> vi1 = cpp::splat(1);
   cpp::simd<int> vi2 = cpp::splat(-1);
-  cpp::simd<float> vf1 = cpp::splat(1.0f);
-  cpp::simd<float> vf2 = cpp::splat(-1.0f);
 
   cpp::simd<int> v_abs = cpp::abs(vi2);
   cpp::simd<int> v_min = cpp::min(vi1, vi2);
   cpp::simd<int> v_max = cpp::max(vi1, vi2);
-  cpp::simd<float> v_fma = cpp::fma(vf1, vf2, vf1);
-  cpp::simd<float> v_ceil = cpp::ceil(cpp::splat(1.2f));
-  cpp::simd<float> v_floor = cpp::floor(cpp::splat(1.8f));
-  cpp::simd<float> v_roundeven = cpp::roundeven(cpp::splat(2.5f));
-  cpp::simd<float> v_round = cpp::round(cpp::splat(2.5f));
-  cpp::simd<float> v_trunc = cpp::trunc(cpp::splat(-2.9f));
-  cpp::simd<float> v_nearbyint = cpp::nearbyint(cpp::splat(3.4f));
-  cpp::simd<float> v_rint = cpp::rint(cpp::splat(3.6f));
-  cpp::simd<float> v_canonicalize = cpp::canonicalize(cpp::splat(1.0f));
-  cpp::simd<float> v_copysign = cpp::copysign(vf1, vf2);
 
   EXPECT_EQ(v_abs[0], 1);
   EXPECT_EQ(v_min[0], -1);
   EXPECT_EQ(v_max[0], 1);
-  EXPECT_FP_EQ(v_fma[0], 0.0f);
-  EXPECT_FP_EQ(v_ceil[0], 2.0f);
-  EXPECT_FP_EQ(v_floor[0], 1.0f);
-  EXPECT_FP_EQ(v_roundeven[0], 2.0f);
-  EXPECT_FP_EQ(v_round[0], 3.0f);
-  EXPECT_FP_EQ(v_trunc[0], -2.0f);
-  EXPECT_FP_EQ(v_nearbyint[0], 3.0f);
-  EXPECT_FP_EQ(v_rint[0], 4.0f);
-  EXPECT_FP_EQ(v_canonicalize[0], 1.0f);
-  EXPECT_FP_EQ(v_copysign[0], -1.0f);
 }
 
 TEST(LlvmLibcSIMDTest, ReductionOperations) {
