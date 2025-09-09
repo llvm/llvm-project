240326f07e216b44b29ba2778f241671969c39b6
Harrison Hao <tsworld1314@gmail.com>
2025-09-09 20:59:32 +0800

    [AMDGPU] Support image atomic no return instructions

--- llvm/lib/Target/AMDGPU/AMDGPUInstrInfo.h
+++ llvm/lib/Target/AMDGPU/AMDGPUInstrInfo.h
@@ -50,6 +50,7 @@ const D16ImageDimIntrinsic *lookupD16ImageDimIntrinsic(unsigned Intr);
 struct ImageDimIntrinsicInfo {
   unsigned Intr;
   unsigned BaseOpcode;
+  unsigned NoRetBaseOpcode;
   MIMGDim Dim;
 
   uint8_t NumOffsetArgs;
