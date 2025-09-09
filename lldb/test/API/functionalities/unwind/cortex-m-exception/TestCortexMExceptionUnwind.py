9b277b67923e1486e8fbbc9040a95f80e6795372
Jason Molenda <jmolenda@apple.com>
2025-09-09 14:11:39 -0700

    [lldb] Unwind through ARM Cortex-M exceptions automatically (#153922)
    When a processor faults/is interrupted/gets an exception, it will stop
    running code and jump to an exception catcher routine. Most processors
    will store the pc that was executing in a system register, and the
    catcher functions have special instructions to retrieve that & possibly
    other registers. It may then save those values to stack, and the author
    can add .cfi directives to tell lldb's unwinder where to find those
    saved values.
    
    ARM Cortex-M (microcontroller) processors have a simpler mechanism where
    a fixed set of registers are saved to the stack on an exception, and a
    unique value is put in the link register to indicate to the caller that
    this has taken place. No special handling needs to be written into the
    exception catcher, unless it wants to inspect these preserved values.
    And it is possible for a general stack walker to walk the stack with no
    special knowledge about what the catch function does.
    
    This patch adds an Architecture plugin method to allow an Architecture
    to override/augment the UnwindPlan that lldb would use for a stack
    frame, given the contents of the return address register. It resembles a
    feature where the LanguageRuntime can replace/augment the unwind plan
    for a function, but it is doing it at offset by one level. The
    LanguageRuntime is looking at the local register context and/or symbol
    name to decide if it will override the unwind rules. For the Cortex-M
    exception unwinds, we need to modify THIS frame's unwind plan if the
    CALLER's LR had a specific value. RegisterContextUnwind has to retrieve
    the caller's LR value before it has completely decided on the UnwindPlan
    it will use for THIS stack frame.
    
    This does mean that we will need one additional read of stack memory
    than we currently do when unwinding, on Armv7 Cortex-M targets. The
    unwinder walks the stack lazily, as stack frames are requested, and so
    now if you ask for 2 stack frames, we will read enough stack to walk 2
    frames, plus we will read one extra word of memory, the spilled LR value
    from the stack. In practice, with 512-byte memory cache reads, this is
    unlikely to be a real performance hit.
    
    This PR includes a test with a yaml corefile description and a JSON
    ObjectFile, incorporating all of the necessary stack memory and symbol
    names from a real debug session I worked on. The architectural default
    unwind plans are used for all stack frames except the 0th because
    there's no instructions for the functions, and no unwind info. I may
    need to add an encoding of unwind fules to ObjectFileJSON in the future
    as we create more test cases like this.
    
    This PR depends on the yaml2macho-core utility from
    https://github.com/llvm/llvm-project/pull/153911 to run its API test.
    
    rdar://110663219

---
+++ lldb/test/API/functionalities/unwind/cortex-m-exception/TestCortexMExceptionUnwind.py
@@ -0,0 +1,50 @@
+"""
+Test that we can backtrace up an ARM Cortex-M Exception return stack
+"""
+
+import lldb
+import json
+from lldbsuite.test.decorators import *
+from lldbsuite.test.lldbtest import *
+from lldbsuite.test import lldbutil
+
+
+class TestCortexMExceptionUnwind(TestBase):
+    NO_DEBUG_INFO_TESTCASE = True
+
+    def test_no_fpu(self):
+        """Test that we can backtrace correctly through an ARM Cortex-M Exception return stack"""
+
+        target = self.dbg.CreateTarget("")
+        exe = "binary.json"
+        with open(exe) as f:
+            exe_json = json.load(f)
+            exe_uuid = exe_json["uuid"]
+
+        target.AddModule(exe, "", exe_uuid)
+        self.assertTrue(target.IsValid())
+
+        core = self.getBuildArtifact("core")
+        self.yaml2macho_core("armv7m-nofpu-exception.yaml", core, exe_uuid)
+
+        process = target.LoadCore(core)
+        self.assertTrue(process.IsValid())
+
+        if self.TraceOn():
+            self.runCmd("image list")
+            self.runCmd("target modules dump sections")
+            self.runCmd("target modules dump symtab")
+            self.runCmd("bt")
+
+        thread = process.GetThreadAtIndex(0)
+        self.assertTrue(thread.IsValid())
+
+        self.assertEqual(thread.GetNumFrames(), 6)
+        stackframe_names = [
+            "exception_catcher",
+            "exception_catcher",
+            "exception_thrower",
+            "main",
+        ]
+        for i, name in enumerate(stackframe_names):
+            self.assertEqual(name, thread.GetFrameAtIndex(i).GetSymbol().GetName())
