d013e7618f4499cec7ae001a3161c890b9e92bdd
Wael Yehia <wyehia@ca.ibm.com>
2025-09-10 14:25:15 +0000

    create a zero-length entry in the ifunc_sec section to satisfy the start/stop symbol references when user code does not use ifuncs

--- compiler-rt/lib/builtins/ppc/init_ifuncs.c
+++ compiler-rt/lib/builtins/ppc/init_ifuncs.c
@@ -2,12 +2,20 @@ typedef void* Ptr;
 typedef struct { Ptr addr, toc, env; } Descr;
 typedef struct { Descr* desc; Ptr (*resolver)(); } IFUNC_PAIR;
 
+// A zero-length entry in section "ifunc_sec" to satisfy the __start_ifunc_sec
+// and __stop_ifunc_sec references in this file, when no user code has any.
+__attribute__((section("ifunc_sec"))) static int dummy_ifunc_sec[0];
+
 extern IFUNC_PAIR __start_ifunc_sec, __stop_ifunc_sec;
 
 __attribute__((constructor))
 void __init_ifuncs() {
-  for (IFUNC_PAIR *pair = &__start_ifunc_sec;
-       pair != &__stop_ifunc_sec;
+  void *volatile ref = &dummy_ifunc_sec; // hack to keep dummy_ifunc_sec alive
+
+  // hack to prevent compiler from assuming __start_ifunc_sec and
+  // __stop_ifunc_sec occupy different addresses.
+  IFUNC_PAIR *volatile volatile_end = &__stop_ifunc_sec;
+  for (IFUNC_PAIR *pair = &__start_ifunc_sec, *end = volatile_end; pair != end;
        pair++)
     pair->desc->addr = ((Descr*)(pair->resolver()))->addr;
 }
