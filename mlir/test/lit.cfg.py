8ba23b7b52f53020c3df0efb8aebb71616682d73
makslevental <maksim.levental@gmail.com>
2025-09-10 12:57:54 -0700

    [MLIR][Standalone] test Standalone against install distributions

--- mlir/test/lit.cfg.py
+++ mlir/test/lit.cfg.py
@@ -383,3 +383,6 @@ if config.arm_emulator_executable:
 
 if sys.version_info >= (3, 11):
     config.available_features.add("python-ge-311")
+
+if "GITHUB_ACTIONS" in os.environ:
+    config.available_features.add("github-actions")
