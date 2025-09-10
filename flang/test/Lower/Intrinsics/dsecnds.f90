97567ea8c7f043906b6af396ec25a58aa35bda88
Sarka Holendova <sarka.holendova@gmail.com>
2025-09-10 12:59:37 -0400

    [flang] Adjust DSECNDS test to HLFIR and update FileCheck patterns

--- flang/test/Lower/Intrinsics/dsecnds.f90
+++ flang/test/Lower/Intrinsics/dsecnds.f90
@@ -1,4 +1,4 @@
-! RUN: bbc -emit-fir -hlfir=false %s -o - | FileCheck %s
+! RUN: bbc -emit-hlfir %s -o - | FileCheck %s
 
 ! CHECK-LABEL: func.func @_QPuse_dsecnds(
 ! CHECK-SAME: %[[arg0:.*]]: !fir.ref<f64>
@@ -13,11 +13,10 @@ end function
 ! CHECK: %[[FNAME8:.*]] = fir.convert %[[STRADDR]] : (!fir.ref<!fir.char<1,{{.*}}>>) -> !fir.ref<i8>
 
 ! Call the runtime DSECNDS with (refTime, file, line)
-! CHECK: %[[CALL:.*]] = fir.call @_FortranADsecnds(%[[arg0]], %[[FNAME8]], %[[LINE]]) {{.*}} : (!fir.ref<f64>, !fir.ref<i8>, i32) -> f64
+! CHECK: %[[CALL:.*]] = fir.call @_FortranADsecnds(%{{.*}}, %{{.*}}, %{{.*}}) {{.*}} : (!fir.ref<f64>, !fir.ref<i8>, i32) -> f64
 
 ! Guard: no illegal ref conversion
 ! CHECK-NOT: fir.convert {{.*}} : (f64) -> !fir.ref<f64>
 
 ! Function returns f64
 ! CHECK: return {{.*}} : f64
-
