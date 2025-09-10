c814888aa7910c2661e0d6f18fda93b1ed013bcb
Krishna Pandey <kpandey81930@gmail.com>
2025-09-10 23:16:06 +0530

    refactor: error headers and unused cast
    Signed-off-by: Krishna Pandey <kpandey81930@gmail.com>

--- libc/test/src/math/smoke/log_bf16_test.cpp
+++ libc/test/src/math/smoke/log_bf16_test.cpp
@@ -6,10 +6,9 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include "hdr/errno_macros.h"
 #include "hdr/fenv_macros.h"
 #include "src/__support/FPUtil/bfloat16.h"
-#include "src/__support/FPUtil/cast.h"
-#include "src/__support/libc_errno.h"
 #include "src/__support/macros/properties/types.h"
 #include "src/math/log_bf16.h"
 #include "test/UnitTest/FPMatcher.h"
