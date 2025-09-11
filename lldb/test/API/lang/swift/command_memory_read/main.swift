d6f9d81c15125ceae9068ba9aab3dc47617791ef
Dave Lee <davelee.com@gmail.com>
2025-09-11 15:53:15 -0700

    Add Swift tests for memory read -t <typename>

---
+++ lldb/test/API/lang/swift/command_memory_read/main.swift
@@ -0,0 +1,10 @@
+@main enum Entry {
+    static func main() {
+        var name: String = "dirk"
+        var number: Int = 41
+        var fact: Bool = true
+        var maybe: UInt64? = nil
+        var bytes: [UInt8] = [1, 2, 4, 8]
+        print("break here", name, number, fact, maybe, bytes)
+    }
+}
