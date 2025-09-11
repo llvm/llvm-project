1b69ff6a57ffba93690f1b73cb2f7935f6c6804b
makslevental <maksim.levental@gmail.com>
2025-09-11 16:08:29 -0700

    remove pre

--- mlir/test/lit.cfg.py
+++ mlir/test/lit.cfg.py
@@ -187,7 +187,7 @@ for dirs in tool_dirs:
     llvm_config.with_environment("PATH", dirs, append_path=True)
 
 tools = [
-    ToolSubst("mlir-tblgen", pre=";", post=";", unresolved="fatal"),
+    ToolSubst("mlir-tblgen", post=";", unresolved="fatal"),
     "mlir-translate",
     "mlir-lsp-server",
     "mlir-capi-execution-engine-test",
@@ -318,7 +318,7 @@ elif "MLIR_GENERATE_PATTERN_CATALOG" in os.environ:
         ]
     )
 else:
-    tools.extend([ToolSubst("mlir-opt", pre=";", post=";", unresolved="fatal")])
+    tools.extend([ToolSubst("mlir-opt", post=";", unresolved="fatal")])
 
 llvm_config.add_tool_substitutions(tools, tool_dirs)
 
