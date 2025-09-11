520cecd2e77273d302432561eaa9dd5cfc6ee0bd
Abid Qadeer <haqadeer@amd.com>
2025-09-11 18:02:26 +0100

    [flang][driver] Support -gdwarf-N option.
    This PR adds the support for -gdwarf-N option where allows user to
    choose the version of the dwarf. Currently N can be 2, 3, 4, or 5.
    This is only the driver part of the change. Later PRs will propogate
    it to the IR.

---
+++ flang/test/Driver/flang-dwarf-version.f90
@@ -0,0 +1,27 @@
+// RUN: %flang -### -S %s -g -gdwarf-5  2>&1 \
+// RUN:             | FileCheck --check-prefix=CHECK-WITH-G-DWARF5 %s
+// RUN: %flang -### -S %s -g1 -gdwarf-5  2>&1 \
+// RUN:             | FileCheck --check-prefix=CHECK-WITH-G1-DWARF5 %s
+// RUN: %flang -### -S %s -gdwarf-5  2>&1 \
+// RUN:             | FileCheck --check-prefix=CHECK-WITHOUT-G-DWARF5 %s
+// RUN: %flang -### -S %s -gdwarf-4  2>&1 \
+// RUN:             | FileCheck --check-prefix=CHECK-DWARF4 %s
+// RUN: %flang -### -S %s -gdwarf-3  2>&1 \
+// RUN:             | FileCheck --check-prefix=CHECK-DWARF3 %s
+// RUN: %flang -### -S %s -gdwarf-2  2>&1 \
+// RUN:             | FileCheck --check-prefix=CHECK-DWARF2 %s
+
+// CHECK-WITH-G-DWARF5: -debug-info-kind=standalone
+// CHECK-WITH-G-DWARF5-SAME: -dwarf-version=5
+
+// CHECK-WITH-G1-DWARF5: -debug-info-kind=line-tables-only
+// CHECK-WITH-G1-DWARF5-SAME: -dwarf-version=5
+
+// CHECK-WITHOUT-G-DWARF5: -debug-info-kind=standalone
+// CHECK-WITHOUT-G-DWARF5: -dwarf-version=5
+
+// CHECK-DWARF4: -dwarf-version=4
+
+// CHECK-DWARF3: -dwarf-version=3
+
+// CHECK-DWARF2: -dwarf-version=2
