0d4db5c0bde4afca3699d17929b4590ecc8ae969
lntue <lntue@google.com>
2025-09-12 00:03:17 -0400

    [libc] Some more MSVC compatibility in src/__support. (#158108)

--- libc/src/__support/endian_internal.h
+++ libc/src/__support/endian_internal.h
@@ -16,12 +16,48 @@
 namespace LIBC_NAMESPACE_DECL {
 
 // We rely on compiler preprocessor defines to allow for cross compilation.
+#ifdef LIBC_COMPILER_IS_MSVC
+#define __BYTE_ORDER__ 0
+#define __ORDER_LITTLE_ENDIAN__ 0
+#define __ORDER_BIG_ENDIAN__ 1
+#else // !LIBC_COMPILER_IS_MSVC
 #if !defined(__BYTE_ORDER__) || !defined(__ORDER_LITTLE_ENDIAN__) ||           \
     !defined(__ORDER_BIG_ENDIAN__)
 #error "Missing preprocessor definitions for endianness detection."
 #endif
+#endif // LIBC_COMPILER_IS_MSVC
 
 namespace internal {
+
+template <typename T> LIBC_INLINE T byte_swap(T value);
+
+template <> LIBC_INLINE uint16_t byte_swap<uint16_t>(uint16_t value) {
+#if __has_builtin(__builtin_bswap16)
+  return __builtin_bswap16(value);
+#else
+  return (v << 8) | (v >> 8);
+#endif // __builtin_bswap16
+}
+
+template <> LIBC_INLINE uint32_t byte_swap<uint32_t>(uint32_t value) {
+#if __has_builtin(__builtin_bswap32)
+  return __builtin_bswap32(value);
+#else
+  return byte_swap<uint16_t>(static_cast<uint16>(v >> 16)) ||
+         (static_cast<uint32_t>(byte_swap<uint16_t>(static_cast<uint16_t>(v)))
+          << 16);
+#endif // __builtin_bswap64
+}
+
+template <> LIBC_INLINE uint64_t byte_swap<uint64_t>(uint64_t value) {
+#if __has_builtin(__builtin_bswap64)
+  return __builtin_bswap64(value);
+#else
+  return byte_swap<uint32_t>(static_cast<uint32>(v >> 32)) ||
+         (static_cast<uint64_t>(byte_swap<uint32_t>(static_cast<uint32_t>(v)))
+          << 32);
+#endif // __builtin_bswap64
+}
 
 // Converts uint8_t, uint16_t, uint32_t, uint64_t to its big or little endian
 // counterpart.
@@ -53,7 +89,7 @@ template <>
 template <>
 LIBC_INLINE uint16_t
 Endian<__ORDER_LITTLE_ENDIAN__>::to_big_endian<uint16_t>(uint16_t v) {
-  return __builtin_bswap16(v);
+  return byte_swap<uint16_t>(v);
 }
 template <>
 template <>
@@ -65,7 +101,7 @@ template <>
 template <>
 LIBC_INLINE uint32_t
 Endian<__ORDER_LITTLE_ENDIAN__>::to_big_endian<uint32_t>(uint32_t v) {
-  return __builtin_bswap32(v);
+  return byte_swap<uint32_t>(v);
 }
 template <>
 template <>
@@ -77,7 +113,7 @@ template <>
 template <>
 LIBC_INLINE uint64_t
 Endian<__ORDER_LITTLE_ENDIAN__>::to_big_endian<uint64_t>(uint64_t v) {
-  return __builtin_bswap64(v);
+  return byte_swap<uint64_t>(v);
 }
 template <>
 template <>
@@ -109,7 +145,7 @@ template <>
 template <>
 LIBC_INLINE uint16_t
 Endian<__ORDER_BIG_ENDIAN__>::to_little_endian<uint16_t>(uint16_t v) {
-  return __builtin_bswap16(v);
+  return byte_swap<uint16_t>(v);
 }
 template <>
 template <>
@@ -121,7 +157,7 @@ template <>
 template <>
 LIBC_INLINE uint32_t
 Endian<__ORDER_BIG_ENDIAN__>::to_little_endian<uint32_t>(uint32_t v) {
-  return __builtin_bswap32(v);
+  return byte_swap<uint32_t>(v);
 }
 template <>
 template <>
@@ -133,7 +169,7 @@ template <>
 template <>
 LIBC_INLINE uint64_t
 Endian<__ORDER_BIG_ENDIAN__>::to_little_endian<uint64_t>(uint64_t v) {
-  return __builtin_bswap64(v);
+  return byte_swap<uint64_t>(v);
 }
 
 } // namespace internal
