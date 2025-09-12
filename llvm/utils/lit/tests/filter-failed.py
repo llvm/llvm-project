63d3a548e774fcdbf767a404c305dc0edb96c175
Michael Buch <michaelbuch12@gmail.com>
2025-09-12 09:48:58 +0100

    fixup! add 'newly added tests' test case

--- llvm/utils/lit/tests/filter-failed.py
+++ llvm/utils/lit/tests/filter-failed.py
@@ -1,18 +1,25 @@
 # Checks that --filter-failed only runs tests that previously failed.
 
-# RUN: not %{lit} %{inputs}/ignore-fail
-# RUN: not %{lit} --filter-failed %{inputs}/ignore-fail | FileCheck %s
+# RUN: not %{lit} %{inputs}/filter-failed
+#
+# RUN: rm -f %{inputs}/filter-failed/new-fail.txt
+# RUN: echo "RUN: false" > %{inputs}/filter-failed/new-fail.txt
+#
+# RUN: rm -f %{inputs}/filter-failed/new-pass.txt
+# RUN: echo "RUN: true" > %{inputs}/filter-failed/new-pass.txt
+#
+# RUN: not %{lit} --filter-failed %{inputs}/filter-failed | FileCheck %s
 
 # END.
 
-# CHECK: Testing: 3 of 5 tests
-# CHECK-DAG: FAIL: ignore-fail :: fail.txt
-# CHECK-DAG: UNRESOLVED: ignore-fail :: unresolved.txt
-# CHECK-DAG: XPASS: ignore-fail :: xpass.txt
+# CHECK: Testing: 3 of 7 tests
+# CHECK-DAG: FAIL: filter-failed :: fail.txt
+# CHECK-DAG: UNRESOLVED: filter-failed :: unresolved.txt
+# CHECK-DAG: XPASS: filter-failed :: xpass.txt
 
 # CHECK: Testing Time:
 # CHECK: Total Discovered Tests:
-# CHECK-NEXT:   Excluded : 2 {{\([0-9]*\.[0-9]*%\)}}
+# CHECK-NEXT:   Excluded : 4 {{\([0-9]*\.[0-9]*%\)}}
 # CHECK-NEXT:   Unresolved : 1 {{\([0-9]*\.[0-9]*%\)}}
 # CHECK-NEXT:   Failed : 1 {{\([0-9]*\.[0-9]*%\)}}
 # CHECK-NEXT:   Unexpectedly Passed: 1 {{\([0-9]*\.[0-9]*%\)}}
