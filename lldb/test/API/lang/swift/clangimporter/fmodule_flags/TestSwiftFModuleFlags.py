adb0936d7b2af4c94d2f075bc7f701504be52991
Adrian Prantl <aprantl@apple.com>
2025-09-12 16:13:33 -0700

    [lldb] Fix TestSwiftFModuleFlags.py

--- lldb/test/API/lang/swift/clangimporter/fmodule_flags/TestSwiftFModuleFlags.py
+++ lldb/test/API/lang/swift/clangimporter/fmodule_flags/TestSwiftFModuleFlags.py
@@ -4,7 +4,6 @@ from lldbsuite.test.decorators import *
 import lldbsuite.test.lldbutil as lldbutil
 import os
 
-@skipIf(bugnumber = "rdar://159531233")
 class TestSwiftFModuleFlags(TestBase):
     @skipIf(macos_version=["<", "14.0"])
     @skipIfDarwinEmbedded
