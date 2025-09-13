bb6ae6399be0776e5682eac6bf7fc14aca575a40
Aiden Grossman <aidengrossman@google.com>
2025-09-13 21:33:36 +0000

    feedback

--- llvm/utils/lit/tests/shtest-ulimit.py
+++ llvm/utils/lit/tests/shtest-ulimit.py
@@ -4,7 +4,6 @@
 # UNSUPPORTED: system-windows
 
 # RUN: not %{lit} -a -v %{inputs}/shtest-ulimit | FileCheck %s
-# RUN: %if system-linux %{ not %{lit} -a -v %{inputs}/shtest-ulimit-linux | FileCheck %s --check-prefix CHECK-LINUX %}
 
 # CHECK: -- Testing: 2 tests{{.*}}
 
@@ -15,9 +14,3 @@
 # CHECK-LABEL: FAIL: shtest-ulimit :: ulimit_okay.txt ({{[^)]*}})
 # CHECK: ulimit -n 50
 # CHECK: RLIMIT_NOFILE=50
-
-# CHECK-LINUX: -- Testing: 1 tests{{.*}}
-
-# CHECK-LINUX-LABEL: FAIL: shtest-ulimit :: ulimit_okay.txt ({{[^)]*}})
-# CHECK-LINUX: ulimit -v 1048576
-# CHECK-LINUX: RLIMIT_AS=1073741824
