9c9c454b19d22d5713417b38dc738e8cc953c9b3
Owen Anderson <resistor@mac.com>
2025-09-13 22:45:11 +0800

    [TableGen, CodeGen, CHERI] Add support for the cPTR wildcard value type.
    cPTR is a wildcard CHERI capability value type, used analogously to iPTR. This allows TableGen patterns to abstract over CHERI capability widths.
    
    Co-authored-by: Jessica Clarke <jrtc27@jrtc27.com>

--- llvm/utils/TableGen/DAGISelMatcherOpt.cpp
+++ llvm/utils/TableGen/DAGISelMatcherOpt.cpp
@@ -519,9 +519,9 @@ static void FactorScope(std::unique_ptr<Matcher> &MatcherPtr) {
       CheckTypeMatcher *CTM = cast_or_null<CheckTypeMatcher>(
           FindNodeWithKind(Optn, Matcher::CheckType));
       if (!CTM ||
-          // iPTR checks could alias any other case without us knowing, don't
-          // bother with them.
-          CTM->getType() == MVT::iPTR ||
+          // iPTR/cPTR checks could alias any other case without us knowing,
+          // don't bother with them.
+          CTM->getType() == MVT::iPTR || CTM->getType() == MVT::cPTR ||
           // SwitchType only works for result #0.
           CTM->getResNo() != 0 ||
           // If the CheckType isn't at the start of the list, see if we can move
