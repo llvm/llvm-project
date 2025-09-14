545304170b2d2bc5289e3b1c70abb40f4a377b1e
Victor Chernyakin <chernyakin.victor.j@outlook.com>
2025-09-14 14:06:20 -0600

    [clang-tidy][NFC] Switch to new file header style (#158497)
    As decided in #118553 and following up #153942.
    
    `rename_check.py` has small logic changes, everything else is a
    mechanical replacement.

--- clang-tools-extra/clang-tidy/bugprone/UndefinedMemoryManipulationCheck.h
+++ clang-tools-extra/clang-tidy/bugprone/UndefinedMemoryManipulationCheck.h
@@ -1,4 +1,4 @@
-//===--- UndefinedMemoryManipulationCheck.h - clang-tidy---------*- C++ -*-===//
+//===----------------------------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
