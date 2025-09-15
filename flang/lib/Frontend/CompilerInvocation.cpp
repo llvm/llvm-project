23ecdd7dc31411eb1d784357c7caf2d30731720a
Abid Qadeer <haqadeer@amd.com>
2025-09-15 15:57:33 +0100

    [flang][driver] Support -gdwarf-N option. (#158314)
    This PR adds the support for -gdwarf-N option where allows user to
    choose the version of the dwarf. Currently N can be 2, 3, 4, or 5. This
    is only the driver part of the change. Later PRs will propogate it to
    the IR.
    
    Fixes https://github.com/llvm/llvm-project/issues/112910.

--- flang/lib/Frontend/CompilerInvocation.cpp
+++ flang/lib/Frontend/CompilerInvocation.cpp
@@ -157,6 +157,10 @@ static bool parseDebugArgs(Fortran::frontend::CodeGenOptions &opts,
           clang::DiagnosticsEngine::Warning, "Unsupported debug option: %0");
       diags.Report(debugWarning) << arg->getValue();
     }
+    // The default value of 2 here is to match clang.
+    opts.DwarfVersion =
+        getLastArgIntValue(args, clang::driver::options::OPT_dwarf_version_EQ,
+                           /*Default=*/2, diags);
   }
   return true;
 }
