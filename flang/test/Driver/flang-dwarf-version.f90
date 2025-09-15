7030b3f396cecf8844f44df34851be9371c24431
Abid Qadeer <haqadeer@amd.com>
2025-09-15 15:15:53 +0100

    Clean up test prefixes.

--- flang/test/Driver/flang-dwarf-version.f90
+++ flang/test/Driver/flang-dwarf-version.f90
@@ -1,9 +1,9 @@
 // RUN: %flang -### -S %s -g -gdwarf-5  2>&1 \
-// RUN:             | FileCheck --check-prefix=CHECK-WITH-G-DWARF5 %s
+// RUN:             | FileCheck --check-prefix=CHECK-DWARF5 %s
+// RUN: %flang -### -S %s -gdwarf-5  2>&1 \
+// RUN:             | FileCheck --check-prefix=CHECK-DWARF5 %s
 // RUN: %flang -### -S %s -g1 -gdwarf-5  2>&1 \
 // RUN:             | FileCheck --check-prefix=CHECK-WITH-G1-DWARF5 %s
-// RUN: %flang -### -S %s -gdwarf-5  2>&1 \
-// RUN:             | FileCheck --check-prefix=CHECK-WITHOUT-G-DWARF5 %s
 // RUN: %flang -### -S %s -gdwarf-4  2>&1 \
 // RUN:             | FileCheck --check-prefix=CHECK-DWARF4 %s
 // RUN: %flang -### -S %s -gdwarf-3  2>&1 \
@@ -11,14 +11,11 @@
 // RUN: %flang -### -S %s -gdwarf-2  2>&1 \
 // RUN:             | FileCheck --check-prefix=CHECK-DWARF2 %s
 
-// CHECK-WITH-G-DWARF5: -debug-info-kind=standalone
-// CHECK-WITH-G-DWARF5-SAME: -dwarf-version=5
+// CHECK-DWARF5: -debug-info-kind=standalone
+// CHECK-DWARF5-SAME: -dwarf-version=5
 
 // CHECK-WITH-G1-DWARF5: -debug-info-kind=line-tables-only
 // CHECK-WITH-G1-DWARF5-SAME: -dwarf-version=5
-
-// CHECK-WITHOUT-G-DWARF5: -debug-info-kind=standalone
-// CHECK-WITHOUT-G-DWARF5-SAME: -dwarf-version=5
 
 // CHECK-DWARF4: -dwarf-version=4
 
