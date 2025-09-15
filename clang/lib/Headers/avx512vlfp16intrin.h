fd99649d09fcc262e22e26d33b9f43402486d394
sskzakaria <ssskzakaria@proton.me>
2025-09-15 18:13:46 -0400

    _mm256_mask_cvtepi32_ph

--- clang/lib/Headers/avx512vlfp16intrin.h
+++ clang/lib/Headers/avx512vlfp16intrin.h
@@ -1041,7 +1041,7 @@ _mm256_cvtepi32_ph(__m256i __A) {
   return (__m128h) __builtin_convertvector((__v8si)__A, __v8hf);
 }
 
-static __inline__ __m128h __DEFAULT_FN_ATTRS256
+static __inline__ __m128h __DEFAULT_FN_ATTRS256_CONSTEXPR
 _mm256_mask_cvtepi32_ph(__m128h __W, __mmask8 __U, __m256i __A) {
   return (__m128h)__builtin_ia32_selectph_128(
       (__mmask8)__U, (__v8hf)_mm256_cvtepi32_ph(__A), (__v8hf)__W);
