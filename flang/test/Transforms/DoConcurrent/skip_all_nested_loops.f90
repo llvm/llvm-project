67c335c66395d15841cd540e0e8cb04e9efb666b
Kareem Ergawy <kareem.ergawy@amd.com>
2025-09-16 09:41:39 +0200

    [NFC][flang][OpenMP] `do concurrent` to device mapping lit tests (#155992)
    Adds more lit tests for `do concurrent` device mapping.
    
    PR stack:
    - https://github.com/llvm/llvm-project/pull/155754
    - https://github.com/llvm/llvm-project/pull/155987
    - https://github.com/llvm/llvm-project/pull/155992 ◀️
    - https://github.com/llvm/llvm-project/pull/155993
    - https://github.com/llvm/llvm-project/pull/157638
    - https://github.com/llvm/llvm-project/pull/156610
    - https://github.com/llvm/llvm-project/pull/156837

--- flang/test/Transforms/DoConcurrent/skip_all_nested_loops.f90
+++ flang/test/Transforms/DoConcurrent/skip_all_nested_loops.f90
@@ -1,9 +1,8 @@
-! Tests that if `do concurrent` is not perfectly nested in its parent loop, that
-! we skip converting the not-perfectly nested `do concurrent` loop.
-
+! Tests that if `do concurrent` is indirectly nested in its parent loop, that we
+! skip converting the indirectly nested `do concurrent` loop.
 
 ! RUN: %flang_fc1 -emit-hlfir -fopenmp -fdo-concurrent-to-openmp=host %s -o - \
-! RUN:   | FileCheck %s --check-prefixes=COMMON
+! RUN:   | FileCheck %s --check-prefixes=HOST,COMMON
 
 ! RUN: %flang_fc1 -emit-hlfir -fopenmp -fdo-concurrent-to-openmp=device %s -o - \
 ! RUN:   | FileCheck %s --check-prefixes=DEVICE,COMMON
@@ -16,26 +15,30 @@ program main
    integer :: a(n, m, l)
 
    do concurrent(i=1:n)
-     x = 10
-     do concurrent(j=1:m, k=1:l)
-       a(i,j,k) = i * j + k
+     do j=1,m
+       do concurrent(k=1:l)
+         a(i,j,k) = i * j + k
+       end do
      end do
    end do
 end
 
-
+! HOST: %[[ORIG_J_ALLOC:.*]] = fir.alloca i32 {bindc_name = "j", {{.*}}}
+! HOST: %[[ORIG_J_DECL:.*]]:2 = hlfir.declare %[[ORIG_J_ALLOC]]
 
 ! DEVICE: omp.target {{.*}}map_entries(
 ! DEVICE-SAME:   %{{[[:alnum:]]+}} -> %{{[^,]+}},
 ! DEVICE-SAME:   %{{[[:alnum:]]+}} -> %{{[^,]+}},
 ! DEVICE-SAME:   %{{[[:alnum:]]+}} -> %{{[^,]+}},
 ! DEVICE-SAME:   %{{[^[:space:]]+}} -> %[[I_ARG:[^,]+]],
-! DEVICE-SAME:   %{{[^[:space:]]+}} -> %[[X_ARG:[^,]+]],
+! DEVICE-SAME:   %{{[^[:space:]]+}} -> %[[J_ARG:[^,]+]],
 ! DEVICE-SAME:   %{{[^[:space:]]+}} -> %[[A_ARG:[^,]+]],
 ! DEVICE-SAME:   %{{[^[:space:]]+}} -> %{{[^,]+}},
 ! DEVICE-SAME:   %{{[^[:space:]]+}} -> %{{[^,]+}},
 ! DEVICE-SAME:   %{{[^[:space:]]+}} -> %{{[^:]+}} :
 ! DEVICE-SAME:   {{.*}}) {
+
+! DEVICE: %[[TARGET_J_DECL:.*]]:2 = hlfir.declare %[[J_ARG]] {uniq_name = "_QFEj"}
 
 ! DEVICE: omp.teams
 
@@ -45,20 +48,17 @@ end
 
 ! COMMON: omp.wsloop {
 ! COMMON: omp.loop_nest ({{[^[:space:]]+}}) {{.*}} {
-! COMMON:   fir.do_concurrent {
+! COMMON:   fir.do_loop {{.*}} iter_args(%[[J_IV:.*]] = {{.*}}) -> {{.*}} {
+! HOST:       fir.store %[[J_IV]] to %[[ORIG_J_DECL]]#0
+! DEVICE:     fir.store %[[J_IV]] to %[[TARGET_J_DECL]]#0
 
-! COMMON:     %[[ORIG_J_ALLOC:.*]] = fir.alloca i32 {bindc_name = "j"}
-! COMMON:     %[[ORIG_J_DECL:.*]]:2 = hlfir.declare %[[ORIG_J_ALLOC]]
-
-! COMMON:     %[[ORIG_K_ALLOC:.*]] = fir.alloca i32 {bindc_name = "k"}
-! COMMON:     %[[ORIG_K_DECL:.*]]:2 = hlfir.declare %[[ORIG_K_ALLOC]]
-
-! COMMON:     fir.do_concurrent.loop (%[[J_IV:.*]], %[[K_IV:.*]]) = {{.*}} {
-! COMMON:       %[[J_IV_CONV:.*]] = fir.convert %[[J_IV]] : (index) -> i32
-! COMMON:       fir.store %[[J_IV_CONV]] to %[[ORIG_J_DECL]]#0
-
-! COMMON:       %[[K_IV_CONV:.*]] = fir.convert %[[K_IV]] : (index) -> i32
-! COMMON:       fir.store %[[K_IV_CONV]] to %[[ORIG_K_DECL]]#0
+! COMMON:     fir.do_concurrent {
+! COMMON:         %[[ORIG_K_ALLOC:.*]] = fir.alloca i32 {bindc_name = "k"}
+! COMMON:         %[[ORIG_K_DECL:.*]]:2 = hlfir.declare %[[ORIG_K_ALLOC]]
+! COMMON:       fir.do_concurrent.loop (%[[K_IV:.*]]) = {{.*}} {
+! COMMON:         %[[K_IV_CONV:.*]] = fir.convert %[[K_IV]] : (index) -> i32
+! COMMON:           fir.store %[[K_IV_CONV]] to %[[ORIG_K_DECL]]#0
+! COMMON:       }
 ! COMMON:     }
 ! COMMON:   }
 ! COMMON: omp.yield
