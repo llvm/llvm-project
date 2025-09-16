efe6a1c285bedec049ee6ec7045ecd404fb8e689
Dave Bartolomeo <dave_bartolomeo@apple.com>
2025-09-16 17:08:38 -0400

    Generate plist and HTML to temp directory

--- clang/test/Analysis/diagnostics/plist-html.c
+++ clang/test/Analysis/diagnostics/plist-html.c
@@ -1,5 +1,5 @@
-// RUN: %clang_analyze_cc1 -analyzer-checker=core -analyzer-output=plist-html %s -o %t.plist -verify
-// RUN: tail -n +11 %t.plist | %normalize_plist | diff -ub %S/Inputs/expected-plists/plist-html.c.plist -
+// RUN: rm -rf %t && mkdir %t && %clang_analyze_cc1 -analyzer-checker=core -analyzer-output=plist-html %s -o %t/plist-html.c.plist -verify
+// RUN: tail -n +11 %t/plist-html.c.plist | %normalize_plist | diff -ub %S/Inputs/expected-plists/plist-html.c.plist -
 
 int foo(int *p) {
     if (p) {
