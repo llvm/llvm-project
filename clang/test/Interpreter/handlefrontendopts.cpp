f1810acc7eba72dda589dffb72196203c1410b5a
anutosh491 <andersonbhat491@gmail.com>
2025-09-16 15:31:23 +0530

    delete unused test

--- clang/test/Interpreter/handlefrontendopts.cpp
+++
@@ -1,5 +0,0 @@
-// RUN: not clang-repl --Xcc -Xclang --Xcc -help 2>&1 | FileCheck %s --check-prefix=HELP
-// RUN: not clang-repl --Xcc -Xclang --Xcc -version 2>&1 | FileCheck %s --check-prefix=VERSION
-
-// HELP: Help displayed
-// VERSION: Version displayed
