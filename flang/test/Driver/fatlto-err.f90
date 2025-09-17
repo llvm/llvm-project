05a17305c7a16e8b81f17df497fd33024a559a20
Anchu Rajendran <asudhaku@amd.com>
2025-09-17 15:37:51 -0500

    Removing white space

--- flang/test/Driver/fatlto-err.f90
+++ flang/test/Driver/fatlto-err.f90
@@ -1,4 +1,3 @@
-
 ! RUN: not %flang_fc1 -triple x86_64-apple-macos10.13 -flto -ffat-lto-objects -emit-llvm-bc %s 2>&1 | FileCheck %s --check-prefix=ERROR
 ! ERROR: error: unsupported option '-ffat-lto-objects' for target 'x86_64-apple-macos10.13'
 
