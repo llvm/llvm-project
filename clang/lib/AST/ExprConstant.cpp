2d5d7adbc74bc45f44798257dcb5b4103a3179d5
Martin Storsjö <martin@martin.st>
2025-09-17 20:44:26 +0300

    [clang] Add unreachable after fully covered switches, avoid GCC warnings. NFC. (#159330)
    This avoids the following warnings:
    
        ../../clang/lib/AST/ExprConstant.cpp: In member function ‘bool {anonymous}::IntExprEvaluator::VisitBuiltinCallExpr(const clang::CallExpr*, unsigned int)’:
        ../../clang/lib/AST/ExprConstant.cpp:14104:3: warning: this statement may fall through [-Wimplicit-fallthrough=]
        14104 |   }
              |   ^
        ../../clang/lib/AST/ExprConstant.cpp:14105:3: note: here
        14105 |   case Builtin::BIstrlen:
              |   ^~~~
        ../../clang/lib/Driver/ToolChains/CommonArgs.cpp: In function ‘std::string clang::driver::tools::complexRangeKindToStr(clang::LangOptionsBase::ComplexRangeKind ’:
        ../../clang/lib/Driver/ToolChains/CommonArgs.cpp:3523:1: warning: control reaches end of non-void function [-Wreturn-type]
         3523 | }
              | ^

--- clang/lib/AST/ExprConstant.cpp
+++ clang/lib/AST/ExprConstant.cpp
@@ -14235,6 +14235,7 @@ bool IntExprEvaluator::VisitBuiltinCallExpr(const CallExpr *E,
       return Success(Result, E);
     }
     }
+    llvm_unreachable("Fully covered switch above");
   }
   case Builtin::BIstrlen:
   case Builtin::BIwcslen:
