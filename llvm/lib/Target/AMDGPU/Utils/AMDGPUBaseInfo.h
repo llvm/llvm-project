294ce1db8601519fea15aab5cc8f356fc12c306a
Matt Arsenault <Matthew.Arsenault@amd.com>
2025-09-17 20:19:34 +0900

    inline

--- llvm/lib/Target/AMDGPU/Utils/AMDGPUBaseInfo.h
+++ llvm/lib/Target/AMDGPU/Utils/AMDGPUBaseInfo.h
@@ -1569,7 +1569,7 @@ bool hasMAIInsts(const MCSubtargetInfo &STI);
 bool hasVOPD(const MCSubtargetInfo &STI);
 bool hasDPPSrc1SGPR(const MCSubtargetInfo &STI);
 
-bool supportsWave32(const MCSubtargetInfo &STI) {
+inline bool supportsWave32(const MCSubtargetInfo &STI) {
   return AMDGPU::isGFX10Plus(STI) && !AMDGPU::isGFX1250(STI);
 }
 
