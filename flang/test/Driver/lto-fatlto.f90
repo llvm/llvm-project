d3c30cfc77f11655d741d7e43777472c64ade417
Anchu Rajendran <asudhaku@amd.com>
2025-09-18 11:59:46 -0500

    Fixing upstream failure by removing a test

--- flang/test/Driver/lto-fatlto.f90
+++ flang/test/Driver/lto-fatlto.f90
@@ -2,11 +2,9 @@
 ! checks fatlto objects: that valid bitcode is included in the object file generated. 
 
 ! RUN: %flang -fc1 -triple x86_64-unknown-linux-gnu -flto -ffat-lto-objects -emit-obj %s -o %t.o
-! RUN: llvm-readelf -S %t.o | FileCheck %s --check-prefixes=ELF
 ! RUN: llvm-objcopy --dump-section=.llvm.lto=%t.bc %t.o
 ! RUN: llvm-dis %t.bc -o - | FileCheck %s --check-prefixes=DIS
 
-! ELF: .llvm.lto
 ! DIS: define void @_QQmain()
 ! DIS-NEXT:  ret void
 ! DIS-NEXT: }
