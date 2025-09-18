f0283ca5f4ed806fe5cb88cf768b7b21d3049710
Erick Ochoa <erick.ochoalopez@amd.com>
2025-09-18 08:43:13 -0400

    Update documentation

--- mlir/include/mlir/Dialect/Vector/Transforms/VectorRewritePatterns.h
+++ mlir/include/mlir/Dialect/Vector/Transforms/VectorRewritePatterns.h
@@ -322,15 +322,11 @@ void populateVectorUnrollPatterns(RewritePatternSet &patterns,
                                   const UnrollVectorOptions &options,
                                   PatternBenefit benefit = 1);
 
-/// Populate the pattern set with the following patterns:
-///
-/// [UnrollToElements]
+/// Unrolls 2 or more dimensional `vector.to_elements` ops by unrolling the
+/// outermost dimension of the operand.
 void populateVectorToElementsUnrollPatterns(RewritePatternSet &patterns,
                                             PatternBenefit benefit = 1);
 
-/// Populate the pattern set with the following patterns:
-///
-/// [UnrollFromElements]
 /// Unrolls 2 or more dimensional `vector.from_elements` ops by unrolling the
 /// outermost dimension.
 void populateVectorFromElementsUnrollPatterns(RewritePatternSet &patterns,
