9d64cbcad77df78c1bb5b8c3dd610db4e1a5c09c
Jongmyeong Choi <cheesechoi@gmail.com>
2025-09-18 19:34:12 +0900

    Remove redundant RUN line that only checked compilation success

--- clang/test/CodeGen/attr-cleanup.c
+++ clang/test/CodeGen/attr-cleanup.c
@@ -1,13 +1,10 @@
-// RUN: %clang_cc1 -emit-llvm %s -o %t
+// RUN: %clang_cc1 -std=c89 -emit-llvm %s -o - | FileCheck %s --check-prefix=C89
+// RUN: %clang_cc1 -std=c99 -emit-llvm %s -o - | FileCheck %s --check-prefix=C99
 
 void f(void* arg);
 void g(void) {
   __attribute__((cleanup(f))) void *g;
 }
-
-// Test for cleanup in for-loop initialization (PR #154624)
-// RUN: %clang_cc1 -std=c89 -emit-llvm %s -o - | FileCheck %s --check-prefix=C89
-// RUN: %clang_cc1 -std=c99 -emit-llvm %s -o - | FileCheck %s --check-prefix=C99
 
 void cleaner(int *p);
 
