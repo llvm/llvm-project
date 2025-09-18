c71d56bb4954bad82574841d3f4546a7762a1a4f
Fazlay Rabbi <fazlay.rabbi@intel.com>
2025-09-18 11:53:59 -0700

    [OpenMP 60] Update parsing and semantic support for `nowait` clause to accept optional argument
    This PR enhances the OpenMP `nowait` clause implementation by adding support
    for optional argument in both parsing and semantic analysis phases.
    
    Reference:
    1. OpenMP 6.0 Specification, page 481

--- clang/test/OpenMP/target_teams_distribute_parallel_for_simd_nowait_messages.cpp
+++ clang/test/OpenMP/target_teams_distribute_parallel_for_simd_nowait_messages.cpp
@@ -6,14 +6,22 @@ void foo() {
 }
 
 int main(int argc, char **argv) {
-  int i;
-#pragma omp target teams distribute parallel for simd nowait( // expected-warning {{extra tokens at the end of '#pragma omp target teams distribute parallel for simd' are ignored}}
+  int i, z;
+#pragma omp target teams distribute parallel for simd nowait( //  expected-error {{expected expression}} //  expected-error {{expected ')'}} // expected-note {{to match this '('}}
   for (i = 0; i < argc; ++i) foo();
 #pragma omp target teams distribute parallel for simd nowait (argc)) // expected-warning {{extra tokens at the end of '#pragma omp target teams distribute parallel for simd' are ignored}}
   for (i = 0; i < argc; ++i) foo();
 #pragma omp target teams distribute parallel for simd nowait device (-10u)
   for (i = 0; i < argc; ++i) foo();
-#pragma omp target teams distribute parallel for simd nowait (3.14) device (-10u) // expected-warning {{extra tokens at the end of '#pragma omp target teams distribute parallel for simd' are ignored}}
+#pragma omp target teams distribute parallel for simd nowait (3.14) device (-10u)
+  for (i = 0; i < argc; ++i) foo();
+#pragma omp target teams distribute parallel for simd nowait (argc>> z)
+  for (i = 0; i < argc; ++i) foo();
+#pragma omp target teams distribute parallel for simd nowait (argv[1] = 2) // expected-error {{expected ')'}} expected-note {{to match this '('}}
+  for (i = 0; i < argc; ++i) foo();
+#pragma omp target teams distribute parallel for simd nowait (argc > 0 ? argv[1] : argv[2])
+  for (i = 0; i < argc; ++i) foo();
+#pragma omp target teams distribute parallel for simd nowait (S1) // expected-error {{use of undeclared identifier 'S1'}}
   for (i = 0; i < argc; ++i) foo();
 
   return 0;
