876b8d9710f9c3584570edcc1b3323c968dcc7d0
Tue Ly <lntue.h@gmail.com>
2025-09-18 04:29:31 +0000

    Remove unused variable.

--- libc/test/src/math/smoke/FmaTest.h
+++ libc/test/src/math/smoke/FmaTest.h
@@ -86,7 +86,6 @@ public:
     }
 
     // Test overflow.
-    OutType z = out.max_normal;
     InType in_z = LIBC_NAMESPACE::fputil::cast<InType>(out.max_normal);
     EXPECT_FP_EQ_ALL_ROUNDING(func(InType(-0.25), in_z, in_z),
                               func(InType(1.75), in_z, -in_z));
