e149407b9cd238d92ac4e19906bc79bbd2a21e31
Fazlay Rabbi <fazlay.rabbi@intel.com>
2025-09-18 11:53:59 -0700

    [OpenMP 60] Update parsing and semantic support for `nowait` clause to accept optional argument
    This PR enhances the OpenMP `nowait` clause implementation by adding support
    for optional argument in both parsing and semantic analysis phases.
    
    Reference:
    1. OpenMP 6.0 Specification, page 481

--- clang/lib/Serialization/ASTReader.cpp
+++ clang/lib/Serialization/ASTReader.cpp
@@ -11684,7 +11684,10 @@ void OMPClauseReader::VisitOMPDetachClause(OMPDetachClause *C) {
   C->setLParenLoc(Record.readSourceLocation());
 }
 
-void OMPClauseReader::VisitOMPNowaitClause(OMPNowaitClause *) {}
+void OMPClauseReader::VisitOMPNowaitClause(OMPNowaitClause *C) {
+  C->setCondition(Record.readSubExpr());
+  C->setLParenLoc(Record.readSourceLocation());
+}
 
 void OMPClauseReader::VisitOMPUntiedClause(OMPUntiedClause *) {}
 
