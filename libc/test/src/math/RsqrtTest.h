0d8e57a9cdc07a920e65a78e69e0211e3e9d85f1
Anton Shepelev <shepelev777@gmail.com>
2025-09-19 12:22:09 -0700

    - Added comments to RsqrtTest.h template, to clarify use case

--- libc/test/src/math/RsqrtTest.h
+++ libc/test/src/math/RsqrtTest.h
@@ -1,4 +1,4 @@
-//===-- Utility class to test rsqrt -----------------------------*- C++ -*-===//
+//===-- Utility class to test rsqrt[f|l] ------------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
