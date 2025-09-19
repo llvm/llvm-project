588c3949a43cc97882af354e384d7580ea62d7cb
Nicole Aschenbrenner <nicole.aschenbrenner@amd.com>
2025-09-19 04:09:24 -0500

    Fixes formatting

--- offload/libomptarget/OpenMP/API.cpp
+++ offload/libomptarget/OpenMP/API.cpp
@@ -197,7 +197,7 @@ EXTERN int omp_target_is_present(const void *Ptr, int DeviceNum) {
 }
 
 /// Check whether a pointer is accessible from a device.
-/// Returns true when accessibility is guaranteed otherwise returns false. 
+/// Returns true when accessibility is guaranteed otherwise returns false.
 EXTERN int omp_target_is_accessible(const void *Ptr, size_t Size,
                                     int DeviceNum) {
   TIMESCOPE();
