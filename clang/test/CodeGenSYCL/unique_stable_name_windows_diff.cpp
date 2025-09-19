b9a581ab101959f8fd9d45d5ff464ffcf7996ef1
Zahira Ammarguellat <zahira.ammarguellat@intel.com>
2025-09-19 06:25:17 -0700

    Added a new line for clarity

--- clang/test/CodeGenSYCL/unique_stable_name_windows_diff.cpp
+++ clang/test/CodeGenSYCL/unique_stable_name_windows_diff.cpp
@@ -54,6 +54,7 @@ int main() {
   // DEVICE: private unnamed_addr addrspace(1) constant [17 x i8] c"_ZTSZ4mainEUlvE_\00"
   // DEVICE: private unnamed_addr addrspace(1) constant [17 x i8] c"_ZTSZ4mainEUliE_\00"
   // DEVICE: private unnamed_addr addrspace(1) constant [17 x i8] c"_ZTSZ4mainEUldE_\00"
+
   // DEVICE: define dso_local spir_kernel void @_ZTSZ4mainE2K1
   // DEVICE: call spir_func void @_ZZ4mainENKUlvE_clEv
   // DEVICE: define internal spir_func void @_ZZ4mainENKUlvE_clEv
