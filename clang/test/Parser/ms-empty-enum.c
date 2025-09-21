191eacf08e22c443bd1b091fc97d4a3ca13d9cee
yicuixi <qin_17914@126.com>
2025-09-21 23:17:18 +0800

    Fix test
    Signed-off-by: yicuixi <qin_17914@126.com>

--- clang/test/Parser/ms-empty-enum.c
+++ clang/test/Parser/ms-empty-enum.c
@@ -1,8 +1,7 @@
 // RUN: %clang_cc1 %s -fsyntax-only -Wmicrosoft -verify -fms-extensions
 // RUN: %clang_cc1 %s -fsyntax-only -Wmicrosoft -verify -fms-compatibility
-// expected-no-diagnostics
 
 typedef enum tag1 { } A;
 typedef enum tag2 { } B;
-typedef enum : unsigned { } C;
+typedef enum : unsigned { } C; // expected-warning {{enumeration types with a fixed underlying type are a Microsoft extension}}
 
