d81fd364de1d2050408fdf5385a0e46a45c07abb
Farzon Lotfi <farzonlotfi@microsoft.com>
2025-09-22 16:34:23 -0400

    switch to an HLSL lang opt for now

--- clang/lib/Sema/HLSLExternalSemaSource.cpp
+++ clang/lib/Sema/HLSLExternalSemaSource.cpp
@@ -159,7 +159,7 @@ void HLSLExternalSemaSource::defineHLSLMatrixAlias() {
                                                   SourceLocation(), ColsParam));
   TemplateParams.emplace_back(ColsParam);
 
-  const unsigned MaxMatDim = SemaPtr->getLangOpts().MaxMatrixDimension;
+  const unsigned MaxMatDim = SemaPtr->getLangOpts().HLSLMaxMatrixDimension;
   auto *MaxRow = IntegerLiteral::Create(
       AST, llvm::APInt(AST.getIntWidth(AST.IntTy), MaxMatDim), AST.IntTy,
       SourceLocation());
