5156c07bcdf47a9c47f096e5feb38955a8332d4f
Krishna Pandey <kpandey81930@gmail.com>
2025-09-22 23:39:56 +0530

    refactor: use FEnvSafeTest
    Signed-off-by: Krishna Pandey <kpandey81930@gmail.com>

--- libc/test/src/math/smoke/log_bf16_test.cpp
+++ libc/test/src/math/smoke/log_bf16_test.cpp
@@ -11,37 +11,42 @@
 #include "src/__support/FPUtil/bfloat16.h"
 #include "src/__support/macros/properties/types.h"
 #include "src/math/log_bf16.h"
+#include "test/UnitTest/FEnvSafeTest.h"
 #include "test/UnitTest/FPMatcher.h"
 #include "test/UnitTest/Test.h"
 
-using LlvmLibcLogBf16Test = LIBC_NAMESPACE::testing::FPTest<bfloat16>;
+class LlvmLibcLogBf16Test : public LIBC_NAMESPACE::testing::FEnvSafeTest {
+  DECLARE_SPECIAL_CONSTANTS(bfloat16)
 
-TEST_F(LlvmLibcLogBf16Test, SpecialNumbers) {
-  libc_errno = 0;
+public:
+  void test_special_numbers() {
+    EXPECT_FP_EQ_ALL_ROUNDING(aNaN, LIBC_NAMESPACE::log_bf16(aNaN));
+    EXPECT_MATH_ERRNO(0);
 
-  EXPECT_FP_EQ_ALL_ROUNDING(aNaN, LIBC_NAMESPACE::log_bf16(aNaN));
-  EXPECT_MATH_ERRNO(0);
+    EXPECT_FP_EQ_WITH_EXCEPTION(aNaN, LIBC_NAMESPACE::log_bf16(sNaN),
+                                FE_INVALID);
+    EXPECT_MATH_ERRNO(0);
 
-  EXPECT_FP_EQ_WITH_EXCEPTION(aNaN, LIBC_NAMESPACE::log_bf16(sNaN), FE_INVALID);
-  EXPECT_MATH_ERRNO(0);
+    EXPECT_FP_EQ_ALL_ROUNDING(inf, LIBC_NAMESPACE::log_bf16(inf));
+    EXPECT_MATH_ERRNO(0);
 
-  EXPECT_FP_EQ_ALL_ROUNDING(inf, LIBC_NAMESPACE::log_bf16(inf));
-  EXPECT_MATH_ERRNO(0);
+    EXPECT_FP_EQ_ALL_ROUNDING(aNaN, LIBC_NAMESPACE::log_bf16(neg_inf));
+    EXPECT_MATH_ERRNO(EDOM);
 
-  EXPECT_FP_EQ_ALL_ROUNDING(aNaN, LIBC_NAMESPACE::log_bf16(neg_inf));
-  EXPECT_MATH_ERRNO(EDOM);
+    EXPECT_FP_EQ_WITH_EXCEPTION_ALL_ROUNDING(
+        neg_inf, LIBC_NAMESPACE::log_bf16(zero), FE_DIVBYZERO);
+    EXPECT_MATH_ERRNO(0);
 
-  EXPECT_FP_EQ_WITH_EXCEPTION_ALL_ROUNDING(
-      neg_inf, LIBC_NAMESPACE::log_bf16(zero), FE_DIVBYZERO);
-  EXPECT_MATH_ERRNO(0);
+    EXPECT_FP_EQ_WITH_EXCEPTION_ALL_ROUNDING(
+        neg_inf, LIBC_NAMESPACE::log_bf16(neg_zero), FE_DIVBYZERO);
+    EXPECT_MATH_ERRNO(0);
 
-  EXPECT_FP_EQ_WITH_EXCEPTION_ALL_ROUNDING(
-      neg_inf, LIBC_NAMESPACE::log_bf16(neg_zero), FE_DIVBYZERO);
-  EXPECT_MATH_ERRNO(0);
+    EXPECT_FP_EQ_ALL_ROUNDING(zero, LIBC_NAMESPACE::log_bf16(bfloat16(1.0)));
+    EXPECT_MATH_ERRNO(0);
 
-  EXPECT_FP_EQ_ALL_ROUNDING(zero, LIBC_NAMESPACE::log_bf16(bfloat16(1.0)));
-  EXPECT_MATH_ERRNO(0);
+    EXPECT_FP_EQ_ALL_ROUNDING(aNaN, LIBC_NAMESPACE::log_bf16(bfloat16(-1.0)));
+    EXPECT_MATH_ERRNO(EDOM);
+  }
+};
 
-  EXPECT_FP_EQ_ALL_ROUNDING(aNaN, LIBC_NAMESPACE::log_bf16(bfloat16(-1.0)));
-  EXPECT_MATH_ERRNO(EDOM);
-}
+TEST_F(LlvmLibcLogBf16Test, SpecialNumbers) { test_special_numbers(); }
