b7b8a42565dadc5085b34bc709bc732aca21f6dd
Adam Yang <hanbyang@microsoft.com>
2025-09-22 00:00:38 -0700

    formatting

--- llvm/tools/llvm-debuginfo-analyzer/DebuggerView.cpp
+++ llvm/tools/llvm-debuginfo-analyzer/DebuggerView.cpp
@@ -1,3 +1,15 @@
+//===-- DebuggerView.cpp ---------------------------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// Options and functions related to --debugger-view for llvm-debuginfo-analyzer
+//
+//===----------------------------------------------------------------------===//
+
 #include "DebuggerView.h"
 #include "Options.h"
 #include "llvm/ADT/SetVector.h"
