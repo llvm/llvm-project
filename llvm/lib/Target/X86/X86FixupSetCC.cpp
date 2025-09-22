c4a6bdd420070b411dd56716dabffeecc0b2f817
Orlando Cazalet-Hyams <orlando.hyams@sony.com>
2025-09-22 09:59:46 +0100

    comments

--- llvm/lib/Target/X86/X86FixupSetCC.cpp
+++ llvm/lib/Target/X86/X86FixupSetCC.cpp
@@ -137,6 +137,7 @@ bool X86FixupSetCCPass::runOnMachineFunction(MachineFunction &MF) {
           .addReg(Reg0)
           .addImm(X86::sub_8bit);
 
+      // Transfer the debug-instr-number to the setcc.
       if (unsigned InstrNum = ZExt->peekDebugInstrNum())
         ZExt->getParent()->getParent()->makeDebugValueSubstitution(
             {InstrNum, 0}, {MI.getDebugInstrNum(), 0});
