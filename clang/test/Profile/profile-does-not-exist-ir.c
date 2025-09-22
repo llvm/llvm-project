7b2eeaf45b6645de67ff339a7472d6cfabeb8612
Jan Svoboda <jan_svoboda@apple.com>
2025-09-22 09:42:55 -0700

    Try to fix FileCheck case mismatch

--- clang/test/Profile/profile-does-not-exist-ir.c
+++ clang/test/Profile/profile-does-not-exist-ir.c
@@ -1,6 +1,6 @@
 ; RUN: not %clang_cc1 -emit-llvm -x ir %s -o - -fprofile-instrument-use=llvm -fprofile-instrument-use-path=%t.nonexistent.profdata 2>&1 | FileCheck %s
 
-; CHECK: error: {{.*}}.nonexistent.profdata: No such file or directory
+; CHECK: error: {{.*}}.nonexistent.profdata:
 ; CHECK-NOT: Assertion failed
 
 define i32 @main() {
