689f95b2ba46d6ca319e0735b5f5474bfc14f999
jjmarr-amd <JJ.Marr@amd.com>
2025-09-22 09:58:59 -0400

    Fix headers
    on-behalf-of: @amd <JJ.Marr@amd.com>

--- clang-tools-extra/clang-tidy/bugprone/DefaultLambdaCaptureCheck.h
+++ clang-tools-extra/clang-tidy/bugprone/DefaultLambdaCaptureCheck.h
@@ -1,3 +1,11 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
 #ifndef LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_BUGPRONE_DEFAULT_LAMBDA_CAPTURE_H
 #define LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_BUGPRONE_DEFAULT_LAMBDA_CAPTURE_H
 
