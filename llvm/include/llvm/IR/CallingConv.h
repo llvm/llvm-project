9540be40bfec7787c2fcf9da11d7df35271250eb
Owen Anderson <resistor@mac.com>
2025-09-22 20:58:56 +0900

    Update comments.

--- llvm/include/llvm/IR/CallingConv.h
+++ llvm/include/llvm/IR/CallingConv.h
@@ -287,16 +287,14 @@ namespace CallingConv {
     // Calling convention for AMDGPU whole wave functions.
     AMDGPU_Gfx_WholeWave = 124,
 
-    /// CHERIoT_CompartmentCall - Calling convention used for CHERI when
-    /// crossing a protection boundary.
+    /// Calling convention used for CHERIoT when crossing a protection boundary.
     CHERIoT_CompartmentCall = 125,
-    /// CHERIoT_CompartmentCallee - Calling convention used for the callee of
-    /// CHERIoT_CompartmentCall.
+    /// Calling convention used for the callee of CHERIoT_CompartmentCall.
     /// Ignores the first two capability arguments and the first integer
     /// argument, zeroes all unused return registers on return.
     CHERIoT_CompartmentCallee = 126,
-    /// CHERIoT_LibraryCall - Calling convention used for cross-library calls to
-    /// a stateless compartment.
+    /// Calling convention used for CHERIoT for cross-library calls to a
+    /// stateless compartment.
     CHERIoT_LibraryCall = 127,
 
     /// The highest possible ID. Must be some 2^k - 1.
