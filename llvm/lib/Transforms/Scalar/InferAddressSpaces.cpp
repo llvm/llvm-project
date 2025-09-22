f689f415a1c451604a2594003cb0b0f46b222f94
Wenju He <wenju.he@intel.com>
2025-09-22 10:43:22 +0200

    add vector test and negative tests, update PtrOps.push_back

--- llvm/lib/Transforms/Scalar/InferAddressSpaces.cpp
+++ llvm/lib/Transforms/Scalar/InferAddressSpaces.cpp
@@ -406,8 +406,8 @@ getPointerOperands(const Value &V, const DataLayout &DL,
           GV, cast<LoadInst>(&V)->getFunction()));
       SmallVector<Value *, 2> PtrOps;
       for (const auto *U : GV->users())
-        if (isa<StoreInst>(U))
-          PtrOps.push_back(cast<Operator>(U)->getOperand(0));
+        if (auto *SI = dyn_cast<StoreInst>(U))
+          PtrOps.push_back(const_cast<Value *>(SI->getValueOperand()));
       return PtrOps;
     }
     return {};
