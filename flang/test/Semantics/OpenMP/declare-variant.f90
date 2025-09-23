38cf6a6c62918458888ec4899ae9faf357b9a633
Krzysztof Parzyszek <Krzysztof.Parzyszek@amd.com>
2025-09-23 13:40:10 -0500

    [flang][OpenMP] Use OmpDirectiveSpecification in DECLARE_VARIANT

--- flang/test/Semantics/OpenMP/declare-variant.f90
+++ flang/test/Semantics/OpenMP/declare-variant.f90
@@ -1,9 +1,11 @@
 ! RUN: %python %S/../test_errors.py %s %flang -fopenmp -fopenmp-version=51
 
 subroutine sub0
-!ERROR: Implicit subroutine declaration 'vsub1' in !$OMP DECLARE VARIANT
+!ERROR: The name 'vsub1' should refer to a procedure
+!ERROR: The name 'vsub1' has been implicitly declared
   !$omp declare variant (sub:vsub1) match (construct={parallel})
-!ERROR: Implicit subroutine declaration 'sub1' in !$OMP DECLARE VARIANT
+!ERROR: The name 'sub1' should refer to a procedure
+!ERROR: The name 'sub1' has been implicitly declared
   !$omp declare variant (sub1:vsub) match (construct={parallel})
 contains
   subroutine vsub
