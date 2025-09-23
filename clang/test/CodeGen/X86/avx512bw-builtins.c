fd1470e61b9558744935813387533290af8c01de
kimyounhoex1 <dordyd11@gmail.com>
2025-09-24 01:45:37 +0900

    [clang] VectorExprEvaluator::VisitCallExpr - add constant folding for X86 pslldqi/psrldqi intrinsics

--- clang/test/CodeGen/X86/avx512bw-builtins.c
+++ clang/test/CodeGen/X86/avx512bw-builtins.c
@@ -2610,7 +2610,6 @@ void test_mm512_mask_cvtusepi16_storeu_epi8 (void * __P, __mmask32 __M, __m512i 
 
 __m512i test_mm512_bslli_epi16(__m512i a) {
   // CHECK-LABEL: @test_bslli
-  // CHECK: shufflevector
   // CHECK: shufflevector <64 x i8> %{{.*}}, <64 x i8> zeroinitializer, <64 x i32> <i32 64, i32 65, i32 66, i32 67, i32 0, i32 1, i32 2, i32 3, i32 4, i32 5, i32 6, i32 7, i32 8, i32 9, i32 10, i32 11, i32 68, i32 69, i32 70, i32 71, i32 16, i32 17, i32 18, i32 19, i32 20, i32 21, i32 22, i32 23, i32 24, i32 25, i32 26, i32 27, i32 72, i32 73, i32 74, i32 75, i32 32, i32 33, i32 34, i32 35, i32 36, i32 37, i32 38, i32 39, i32 40, i32 41, i32 42, i32 43, i32 76, i32 77, i32 78, i32 79, i32 48, i32 49, i32 50, i32 51, i32 52, i32 53, i32 54, i32 55, i32 56, i32 57, i32 58, i32 59>
   return _mm512_bslli_epi128(a, 4);
 }
