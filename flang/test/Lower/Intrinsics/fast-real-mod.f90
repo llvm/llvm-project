2572cc07389281d00ed0129d07e84bc9bf4d79b4
Michael Klemm <michael.klemm@amd.com>
2025-09-23 14:30:18 +0200

    Don't use hard-coded register numbers

--- flang/test/Lower/Intrinsics/fast-real-mod.f90
+++ flang/test/Lower/Intrinsics/fast-real-mod.f90
@@ -14,8 +14,8 @@ subroutine mod_real4(r, a, p)
 ! CHECK: %[[DIV:.*]] = arith.divf %[[A_LOAD]], %[[P_LOAD]] fastmath<contract> : f32
 ! CHECK: %[[CV1:.*]] = fir.convert %[[DIV]] : (f32) -> si32
 ! CHECK: %[[CV2:.*]] = fir.convert %[[CV1]] : (si32) -> f32
-! CHECK: %[[MUL:.*]] = arith.mulf %8, %5 fastmath<contract> : f32
-! CHECK: %[[SUB:.*]] = arith.subf %4, %9 fastmath<contract> : f32
+! CHECK: %[[MUL:.*]] = arith.mulf %[[CV2]], %[[P_LOAD]] fastmath<contract> : f32
+! CHECK: %[[SUB:.*]] = arith.subf %[[A_LOAD]], %[[MUL]] fastmath<contract> : f32
 ! CHECK: fir.store %[[SUB]] to %[[R]] : !fir.ref<f32>
     r = mod(a, p)
 end subroutine mod_real4
@@ -32,8 +32,8 @@ subroutine mod_real8(r, a, p)
 ! CHECK: %[[DIV:.*]] = arith.divf %[[A_LOAD]], %[[P_LOAD]] fastmath<contract> : f64
 ! CHECK: %[[CV1:.*]] = fir.convert %[[DIV]] : (f64) -> si64
 ! CHECK: %[[CV2:.*]] = fir.convert %[[CV1]] : (si64) -> f64
-! CHECK: %[[MUL:.*]] = arith.mulf %8, %5 fastmath<contract> : f64
-! CHECK: %[[SUB:.*]] = arith.subf %4, %9 fastmath<contract> : f64
+! CHECK: %[[MUL:.*]] = arith.mulf %[[CV2]], %[[P_LOAD]] fastmath<contract> : f64
+! CHECK: %[[SUB:.*]] = arith.subf %[[A_LOAD]], %[[MUL]] fastmath<contract> : f64
 ! CHECK: fir.store %[[SUB]] to %[[R]] : !fir.ref<f64>
     r = mod(a, p)
 end subroutine mod_real8
@@ -50,8 +50,8 @@ subroutine mod_real10(r, a, p)
 ! CHECK-KIND10: %[[DIV:.*]] = arith.divf %[[A_LOAD]], %[[P_LOAD]] fastmath<contract> : f80
 ! CHECK-KIND10: %[[CV1:.*]] = fir.convert %[[DIV]] : (f80) -> si80
 ! CHECK-KIND10: %[[CV2:.*]] = fir.convert %[[CV1]] : (si80) -> f80
-! CHECK-KIND10: %[[MUL:.*]] = arith.mulf %8, %5 fastmath<contract> : f80
-! CHECK-KIND10: %[[SUB:.*]] = arith.subf %4, %9 fastmath<contract> : f80
+! CHECK-KIND10: %[[MUL:.*]] = arith.mulf %[[CV2]], %[[P_LOAD]] fastmath<contract> : f80
+! CHECK-KIND10: %[[SUB:.*]] = arith.subf %[[A_LOAD]], %[[MUL]] fastmath<contract> : f80
 ! CHECK-KIND10: fir.store %[[SUB]] to %[[R]] : !fir.ref<f80>
     r = mod(a, p)
 end subroutine mod_real10
@@ -68,8 +68,8 @@ subroutine mod_real16(r, a, p)
 ! CHECK-KIND16: %[[DIV:.*]] = arith.divf %[[A_LOAD]], %[[P_LOAD]] fastmath<contract> : f128
 ! CHECK-KIND16: %[[CV1:.*]] = fir.convert %[[DIV]] : (f128) -> si128
 ! CHECK-KIND16: %[[CV2:.*]] = fir.convert %[[CV1]] : (si128) -> f128
-! CHECK-KIND16: %[[MUL:.*]] = arith.mulf %8, %5 fastmath<contract> : f128
-! CHECK-KIND16: %[[SUB:.*]] = arith.subf %4, %9 fastmath<contract> : f128
+! CHECK-KIND16: %[[MUL:.*]] = arith.mulf %[[CV2]], %[[P_LOAD]] fastmath<contract> : f128
+! CHECK-KIND16: %[[SUB:.*]] = arith.subf %[[A_LOAD]], %[[MUL]] fastmath<contract> : f128
 ! CHECK-KIND16: fir.store %[[SUB]] to %[[R]] : !fir.ref<f128>
     r = mod(a, p)
 end subroutine mod_real16
