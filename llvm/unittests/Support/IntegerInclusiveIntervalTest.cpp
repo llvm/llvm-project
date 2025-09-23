23766b73eb3cfb3ac8e1fa41600bdd54d64ea363
Yonah Goldberg <ygoldberg@nvidia.com>
2025-09-23 21:12:53 +0000

    format

--- llvm/unittests/Support/IntegerInclusiveIntervalTest.cpp
+++ llvm/unittests/Support/IntegerInclusiveIntervalTest.cpp
@@ -1,5 +1,5 @@
 //===- llvm/unittests/Support/IntegerInclusiveIntervalTest.cpp - Integer
-//inclusive interval tests ----------------===//
+// inclusive interval tests ----------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
