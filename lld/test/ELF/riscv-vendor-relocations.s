f8664fd576f20b432b3096d41e7e2b983a744313
Owen Anderson <resistor@mac.com>
2025-09-24 10:52:23 +0900

    Use a fake vendor relocation in the testcase.

--- lld/test/ELF/riscv-vendor-relocations.s
+++ lld/test/ELF/riscv-vendor-relocations.s
@@ -1,28 +1,16 @@
-# RUN: llvm-mc -triple riscv32 -mattr=+experimental-xqcibi,+xandesperf %s -filetype=obj -o %t.o
+# RUN: llvm-mc -triple riscv32 %s -filetype=obj -o %t.o
 # RUN: not ld.lld -pie %t.o -o /dev/null 2>&1 | FileCheck %s
 
   .option exact
 
-  qc.e.bgeui s0, 20, TARGET
-# CHECK: error: {{.*}} unknown vendor-specific relocation (193) in vendor namespace "QUALCOMM" against symbol TARGET
-
-  .global QUALCOMM
-QUALCOMM:
-  nop
-
-  qc.e.bgeui s0, 20, TARGET
-# CHECK: error: {{.*}} unknown vendor-specific relocation (193) in vendor namespace "QUALCOMM" against symbol TARGET
-
-  nds.bbc t0, 7, TARGET
-# CHECK: error: {{.*}} unknown vendor-specific relocation (241) in vendor namespace "ANDES" against symbol TARGET
-
-  .global ANDES
-ANDES:
-  nop
-
-  nds.bbs t0, 7, TARGET
-# CHECK: error: {{.*}} unknown vendor-specific relocation (241) in vendor namespace "ANDES" against symbol TARGET
-
   .global TARGET
 TARGET:
   nop
+
+.global INVALID_VENDOR
+.reloc 1f, R_RISCV_VENDOR, INVALID_VENDOR+0
+.reloc 1f, R_RISCV_CUSTOM255, TARGET
+1:
+  nop
+
+# CHECK: error: {{.*}} unknown vendor-specific relocation (255) in vendor namespace "INVALID_VENDOR" against symbol TARGET
