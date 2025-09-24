5aa301b5b032700198ffb044c69b82d22d511536
Sh0g0-1758 <shouryagoel10000@gmail.com>
2025-09-24 21:45:44 +0530

    reorder instructions

--- mlir/lib/Conversion/Normalize/Normalize.cpp
+++ mlir/lib/Conversion/Normalize/Normalize.cpp
@@ -23,6 +23,7 @@
 #include "llvm/ADT/DenseMap.h"
 #include "llvm/ADT/TypeSwitch.h"
 #include "llvm/Support/DebugLog.h"
+#include "llvm/ADT/Hashing.h"
 
 namespace mlir {
 #define GEN_PASS_DEF_NORMALIZE
@@ -35,15 +36,118 @@ using namespace mlir;
 
 namespace {
 struct NormalizePass: public impl::NormalizeBase<NormalizePass> {
-  NormalizePass() = default;
+    NormalizePass() = default;
 
-  void runOnOperation() override;
+    void runOnOperation() override;
+private:
+    const uint64_t MagicHashConstant = 0x6acaa36bef8325c5ULL;
+    void collectOutputOperations(Block &block, SmallVector<Operation *, 16>& Output);
+    bool isOutput(Operation& op);
+    void reorderOperations(SmallVector<Operation*, 16>& Outputs);
+    void reorderOperation(
+        mlir::Operation *used,
+        mlir::Operation *user,
+        llvm::SmallPtrSet<const mlir::Operation *, 32> &visited);
+    void RenameOperations(Block &block, SmallVector<Operation*, 16>& Outputs);
+
 };
 } // namespace
 
 #include <iostream>
 
 void NormalizePass::runOnOperation() {
-  std::cout << "OH HEY MAN!!" << std::endl;
-  ModuleOp module = getOperation();
+    ModuleOp module = getOperation();
+
+    for (Operation &op : module.getOps()) {
+        llvm::outs() << "Operation name: " << op.getName() << "\n";
+        SmallVector<Operation *, 16> Outputs;
+
+        for (Region &region : op.getRegions()) {
+            for (Block &block : region) {
+                collectOutputOperations(block, Outputs);
+            }
+        }
+        for (auto& op : Outputs) llvm::outs() << op->getName() << "\n";
+
+        reorderOperations(Outputs);
+    }
+
+    for (Operation &op : module.getOps()) {
+        llvm::outs() << "Operation name: " << op.getName() << "\n";
+        SmallVector<Operation *, 16> Outputs;
+
+        for (Region &region : op.getRegions()) {
+            for (Block &block : region) {
+              RenameOperations(block, Outputs);
+            }
+        }
+    }
+}
+
+void NormalizePass::RenameOperations(Block &block, SmallVector<Operation*, 16>& Outputs) {
+  static size_t VarCounter = 0;
+  for(Operation& innerOp : block) {
+    /**
+     * TODO: Renaming scheme
+     */
+  }
+}
+
+
+void NormalizePass::reorderOperations(SmallVector<Operation*, 16>& Outputs) {
+  llvm::SmallPtrSet<const mlir::Operation *, 32> visited;
+  for (auto *op : Outputs) {
+    for (mlir::Value operand : op->getOperands()) {
+      if (mlir::Operation *defOp = operand.getDefiningOp()) {
+        reorderOperation(defOp, op, visited);
+      }
+    }
+  }
+}
+
+void NormalizePass::reorderOperation(
+    mlir::Operation *used, mlir::Operation *user,
+    llvm::SmallPtrSet<const mlir::Operation *, 32> &visited) {
+
+  if (!visited.count(used)) {
+    visited.insert(used);
+
+    mlir::Block *usedBlock = used->getBlock();
+    mlir::Block *userBlock = user->getBlock();
+
+    if (usedBlock == userBlock) {
+      used->moveBefore(user);
+    } else {
+      used->moveBefore(&usedBlock->back());
+    }
+
+    for (mlir::Value operand : used->getOperands()) {
+      if (mlir::Operation *defOp = operand.getDefiningOp()) {
+        reorderOperation(defOp, used, visited);
+      }
+    }
+  }
+}
+
+void NormalizePass::collectOutputOperations(Block &block, SmallVector<Operation*, 16>& Outputs) {
+  for(Operation& innerOp : block) {
+    if(isOutput(innerOp)) {
+        Outputs.emplace_back(&innerOp);
+    }
+  }
+}
+
+bool NormalizePass::isOutput(Operation& op) {
+    if (op.hasTrait<OpTrait::IsTerminator>())
+        return true;
+
+    if (auto memOp = dyn_cast<MemoryEffectOpInterface>(&op)) {
+        SmallVector<MemoryEffects::EffectInstance, 4> effects;
+        memOp.getEffects(effects);
+        for (auto &effect : effects) {
+            if (isa<MemoryEffects::Write>( effect.getEffect() )) return true;
+        }
+    }
+
+    return false;
 }
