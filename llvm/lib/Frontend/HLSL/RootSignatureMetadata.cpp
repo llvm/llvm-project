c25fee8431b123e879d8ff3d7d93e37498c108e0
Joao Saffran <joaosaffranllvm@gmail.com>
2025-09-24 13:48:32 -0700

    format

--- llvm/lib/Frontend/HLSL/RootSignatureMetadata.cpp
+++ llvm/lib/Frontend/HLSL/RootSignatureMetadata.cpp
@@ -212,8 +212,7 @@ MDNode *MetadataBuilder::BuildStaticSampler(const StaticSampler &Sampler) {
       ConstantAsMetadata::get(Builder.getInt32(Sampler.Space)),
       ConstantAsMetadata::get(
           Builder.getInt32(to_underlying(Sampler.Visibility))),
-      ConstantAsMetadata::get(Builder.getInt32(0))
-  };
+      ConstantAsMetadata::get(Builder.getInt32(0))};
   return MDNode::get(Ctx, Operands);
 }
 
