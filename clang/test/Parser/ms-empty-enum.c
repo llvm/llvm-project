c106aaa0a5657f213b382e0677a7ce22826eeed8
yicuixi <qin_17914@126.com>
2025-09-25 01:48:13 +0800

    address comments

--- clang/test/Parser/ms-empty-enum.c
+++ clang/test/Parser/ms-empty-enum.c
@@ -1,7 +1,6 @@
 // RUN: %clang_cc1 %s -fsyntax-only -Wmicrosoft -verify -fms-extensions
-// RUN: %clang_cc1 %s -fsyntax-only -Wmicrosoft -verify -fms-compatibility
 
-typedef enum tag1 { } A;
-typedef enum tag2 { } B;
-typedef enum : unsigned { } C; // expected-warning {{enumeration types with a fixed underlying type are a Microsoft extension}}
-
+typedef enum tag1 { } A; // expected-warning {{empty enumeration types are a Microsoft extension}}
+typedef enum tag2 { } B; // expected-warning {{empty enumeration types are a Microsoft extension}}
+typedef enum : unsigned { } C; // expected-warning {{enumeration types with a fixed underlying type are a Microsoft extension}}\
+                               // expected-warning {{empty enumeration types are a Microsoft extension}}
