fa580b98e4954f5721632d7c615e305dc1f8130a
kmpeng <kaitlinpeng@microsoft.com>
2025-09-24 03:17:27 -0700

    undo frontend changes, change pattern matching to use mi_match, add constant tests

--- llvm/lib/Target/SPIRV/SPIRVCombinerHelper.cpp
+++ llvm/lib/Target/SPIRV/SPIRVCombinerHelper.cpp
@@ -72,13 +72,13 @@ void SPIRVCombinerHelper::applySPIRVDistance(MachineInstr &MI) const {
 }
 
 /// This match is part of a combine that
-/// rewrites select(fcmp(dot(I, Ng), 0), N, 0 - N) to faceforward(N, I, Ng)
+/// rewrites select(fcmp(dot(I, Ng), 0), N, -N) to faceforward(N, I, Ng)
 ///   (vXf32 (g_select
 ///             (g_fcmp
 ///                (g_intrinsic dot(vXf32 I) (vXf32 Ng)
 ///                 0)
 ///             (vXf32 N)
-///             (vXf32 g_fsub (0) (vXf32 N))))
+///             (vXf32 g_fneg (vXf32 N))))
 /// ->
 ///   (vXf32 (g_intrinsic faceforward
 ///             (vXf32 N) (vXf32 I) (vXf32 Ng)))
@@ -86,65 +86,70 @@ void SPIRVCombinerHelper::applySPIRVDistance(MachineInstr &MI) const {
 /// This only works for Vulkan targets.
 ///
 bool SPIRVCombinerHelper::matchSelectToFaceForward(MachineInstr &MI) const {
-  const SPIRVSubtarget &ST = MI.getMF()->getSubtarget<SPIRVSubtarget>();
-  if (!ST.isShader())
+  if (!MI.getMF()->getSubtarget<SPIRVSubtarget>().isShader())
     return false;
 
-  if (MI.getOpcode() != TargetOpcode::G_SELECT)
+  // Match overall select pattern.
+  Register CondReg, TrueReg, FalseReg;
+  if (!mi_match(MI.getOperand(0).getReg(), MRI,
+                m_GISelect(m_Reg(CondReg), m_Reg(TrueReg), m_Reg(FalseReg))))
     return false;
 
-  // Check if select's condition is a comparison between a dot product and 0.
-  Register CondReg = MI.getOperand(1).getReg();
-  MachineInstr *CondInstr = MRI.getVRegDef(CondReg);
-  if (!CondInstr || CondInstr->getOpcode() != TargetOpcode::G_FCMP)
+  // Match the FCMP condition.
+  Register DotReg, CondZeroReg;
+  CmpInst::Predicate Pred;
+  if (!mi_match(CondReg, MRI,
+                m_GFCmp(m_Pred(Pred), m_Reg(DotReg), m_Reg(CondZeroReg))) ||
+      Pred != CmpInst::FCMP_OLT)
     return false;
 
-  Register DotReg = CondInstr->getOperand(2).getReg();
+  // Check if FCMP is a comparison between a dot product and 0.
   MachineInstr *DotInstr = MRI.getVRegDef(DotReg);
   if ((DotInstr->getOpcode() != TargetOpcode::G_INTRINSIC ||
        cast<GIntrinsic>(DotInstr)->getIntrinsicID() != Intrinsic::spv_fdot)) {
-    if (DotInstr->getOpcode() != TargetOpcode::G_FMUL)
-      return false;
-    Register DotOperand1 = DotInstr->getOperand(1).getReg();
-    Register DotOperand2 = DotInstr->getOperand(2).getReg();
-    if (MRI.getType(DotOperand1).isVector() ||
-        MRI.getType(DotOperand2).isVector())
+    Register DotOperand1, DotOperand2;
+    // Check for scalar dot product.
+    if (!mi_match(DotReg, MRI,
+                  m_GFMul(m_Reg(DotOperand1), m_Reg(DotOperand2))) ||
+        !MRI.getType(DotOperand1).isScalar() ||
+        !MRI.getType(DotOperand2).isScalar())
       return false;
   }
 
-  Register CondZeroReg = CondInstr->getOperand(3).getReg();
-  MachineInstr *CondZeroInstr = MRI.getVRegDef(CondZeroReg);
-  if (CondZeroInstr->getOpcode() != TargetOpcode::G_FCONSTANT ||
-      !CondZeroInstr->getOperand(1).getFPImm()->isZero())
+  const ConstantFP *ZeroVal;
+  if (!mi_match(CondZeroReg, MRI, m_GFCst(ZeroVal)) || !ZeroVal->isZero())
     return false;
 
   // Check if select's false operand is the negation of the true operand.
-  Register TrueReg = MI.getOperand(2).getReg();
-  Register FalseReg = MI.getOperand(3).getReg();
-  MachineInstr *FalseInstr = MRI.getVRegDef(FalseReg);
-  if (FalseInstr->getOpcode() != TargetOpcode::G_FNEG)
-    return false;
-  if (TrueReg != FalseInstr->getOperand(1).getReg())
-    return false;
+  if (!mi_match(FalseReg, MRI, m_GFNeg(m_SpecificReg(TrueReg))) &&
+      !mi_match(TrueReg, MRI, m_GFNeg(m_SpecificReg(FalseReg)))) {
+    // Check if they're constant opposites.
+    std::optional<FPValueAndVReg> TrueVal, FalseVal;
+    if (!mi_match(TrueReg, MRI, m_GFCstOrSplat(TrueVal)) ||
+        !mi_match(FalseReg, MRI, m_GFCstOrSplat(FalseVal)))
+      return false;
+    APFloat TrueValNegated = TrueVal->Value;
+    TrueValNegated.changeSign();
+    if (FalseVal->Value.compare(TrueValNegated) != APFloat::cmpEqual)
+      return false;
+  }
 
   return true;
 }
 
 void SPIRVCombinerHelper::applySPIRVFaceForward(MachineInstr &MI) const {
   // Extract the operands for N, I, and Ng from the match criteria.
-  Register CondReg = MI.getOperand(1).getReg();
-  MachineInstr *CondInstr = MRI.getVRegDef(CondReg);
-  Register DotReg = CondInstr->getOperand(2).getReg();
+  Register CondReg, TrueReg, DotReg, DotOperand1, DotOperand2;
+  if (!mi_match(MI.getOperand(0).getReg(), MRI,
+                m_GISelect(m_Reg(CondReg), m_Reg(TrueReg), m_Reg())))
+    return;
+  if (!mi_match(CondReg, MRI, m_GFCmp(m_Pred(), m_Reg(DotReg), m_Reg())))
+    return;
   MachineInstr *DotInstr = MRI.getVRegDef(DotReg);
-  Register DotOperand1, DotOperand2;
-  if (DotInstr->getOpcode() == TargetOpcode::G_FMUL) {
-    DotOperand1 = DotInstr->getOperand(1).getReg();
-    DotOperand2 = DotInstr->getOperand(2).getReg();
-  } else {
+  if (!mi_match(DotReg, MRI, m_GFMul(m_Reg(DotOperand1), m_Reg(DotOperand2)))) {
     DotOperand1 = DotInstr->getOperand(2).getReg();
     DotOperand2 = DotInstr->getOperand(3).getReg();
   }
-  Register TrueReg = MI.getOperand(2).getReg();
 
   // Remove the original `select` instruction.
   Register ResultReg = MI.getOperand(0).getReg();
@@ -164,7 +169,11 @@ void SPIRVCombinerHelper::applySPIRVFaceForward(MachineInstr &MI) const {
 
   SPIRVGlobalRegistry *GR =
       MI.getMF()->getSubtarget<SPIRVSubtarget>().getSPIRVGlobalRegistry();
-  removeAllUses(CondReg, MRI, GR); // remove all uses of FCMP Result
+  removeAllUses(CondReg, MRI, GR); // Remove all uses of FCMP result
+  MachineInstr *CondInstr = MRI.getVRegDef(CondReg);
   GR->invalidateMachineInstr(CondInstr);
-  CondInstr->eraseFromParent(); // remove FCMP instruction
+  CondInstr->eraseFromParent();   // Remove FCMP instruction
+  removeAllUses(DotReg, MRI, GR); // Remove all uses of dot product result
+  GR->invalidateMachineInstr(DotInstr);
+  DotInstr->eraseFromParent(); // Remove dot product instruction
 }
