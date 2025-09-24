9e9cc9b92cf4039ca70c53d0b3dc027ab41466da
swote <kst7703@gmail.com>
2025-09-25 06:52:36 +0900

    fix: to observe clang format

--- mlir/include/mlir/IR/Builders.h
+++ mlir/include/mlir/IR/Builders.h
@@ -515,10 +515,10 @@ public:
   /// the results of the operation.
   ///
   /// Note: This performs opportunistic eager folding during IR construction.
-  /// The folders are designed to operate efficiently on canonical IR, which this
-  /// API does not enforce. Complete folding isn't only expected in the context
-  /// of canonicalization which intertwine folders with pattern rewrites until
-  /// fixed-point.
+  /// The folders are designed to operate efficiently on canonical IR, which 
+  /// this API does not enforce. Complete folding isn't only expected in the
+  /// context of canonicalization which intertwine folders with pattern 
+  /// rewrites until fixed-point.
   template <typename OpTy, typename... Args>
   void createOrFold(SmallVectorImpl<Value> &results, Location location,
                     Args &&...args) {
