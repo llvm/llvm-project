52c05545849b57f9b10edfb76fb6aba9917dbae3
Georgios Pinitas <georgios.pinitas@arm.com>
2025-09-25 13:36:55 +0100

    Update ParallelLoopMapper.cpp

--- mlir/lib/Dialect/GPU/Transforms/ParallelLoopMapper.cpp
+++ mlir/lib/Dialect/GPU/Transforms/ParallelLoopMapper.cpp
@@ -1,4 +1,4 @@
-f//===- ParallelLoopMapper.cpp - Utilities for mapping parallel loops to GPU =//
+//===- ParallelLoopMapper.cpp - Utilities for mapping parallel loops to GPU =//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
