520c37cb4b3a03e9fae2fe48caaff23dfd5a01d3
Krzysztof Drewniak <Krzysztof.Drewniak@amd.com>
2025-09-26 18:07:22 +0000

    Fix the other typo

--- llvm/lib/Target/AMDGPU/AMDGPULowerBufferFatPointers.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPULowerBufferFatPointers.cpp
@@ -2562,7 +2562,7 @@ bool AMDGPULowerBufferFatPointers::run(Module &M, const TargetMachine &TM) {
   for (Function *F : NeedsPostProcess)
     Splitter.processFunction(*F);
   for (Function *F : Intrinsics) {
-    // use_empty() can also occor with cases like masked load, which will
+    // use_empty() can also occur with cases like masked load, which will
     // have been rewritten out of the module by now but not erased.
     if (F->use_empty() || isRemovablePointerIntrinsic(F->getIntrinsicID())) {
       F->eraseFromParent();
