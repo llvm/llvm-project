c0c68c5a03c5724b2195218ad4b927b3e77eb302
Demetrius Kanios <demetrius@kanios.net>
2025-09-29 11:23:22 -0700

    Finish initial pass over regbankselect

--- llvm/lib/Target/WebAssembly/GISel/WebAssemblyLegalizerInfo.cpp
+++ llvm/lib/Target/WebAssembly/GISel/WebAssemblyLegalizerInfo.cpp
@@ -89,13 +89,16 @@ WebAssemblyLegalizerInfo::WebAssemblyLegalizerInfo(
       .clampScalar(0, s32, s64);
 
   getActionDefinitionsBuilder({G_ASHR, G_LSHR, G_SHL, G_CTLZ, G_CTLZ_ZERO_UNDEF,
-                               G_CTTZ, G_CTTZ_ZERO_UNDEF, G_CTPOP, G_FSHL,
-                               G_FSHR})
+                               G_CTTZ, G_CTTZ_ZERO_UNDEF, G_CTPOP})
       .legalFor({{s32, s32}, {s64, s64}})
       .widenScalarToNextPow2(0)
       .clampScalar(0, s32, s64)
       .minScalarSameAs(1, 0)
       .maxScalarSameAs(1, 0);
+
+  getActionDefinitionsBuilder({G_FSHL, G_FSHR})
+      .legalFor({{s32, s32}, {s64, s64}})
+      .lower();
 
   getActionDefinitionsBuilder({G_SCMP, G_UCMP}).lower();
 
@@ -122,6 +125,12 @@ WebAssemblyLegalizerInfo::WebAssemblyLegalizerInfo(
   getActionDefinitionsBuilder({G_FMA, G_FREM})
       .libcallFor({s32, s64})
       .minScalar(0, s32);
+
+  getActionDefinitionsBuilder(G_LROUND).libcallForCartesianProduct({s32},
+                                                                   {s32, s64});
+
+  getActionDefinitionsBuilder(G_LLROUND).libcallForCartesianProduct({s64},
+                                                                    {s32, s64});
 
   getActionDefinitionsBuilder(G_FCOPYSIGN)
       .legalFor({s32, s64})
@@ -154,9 +163,8 @@ WebAssemblyLegalizerInfo::WebAssemblyLegalizerInfo(
                                  {s64, p0, s8, 1},
                                  {s64, p0, s16, 1},
                                  {s64, p0, s32, 1}})
-      .widenScalarToNextPow2(0)
-      .lowerIfMemSizeNotByteSizePow2()
-      .clampScalar(0, s32, s64);
+      .clampScalar(0, s32, s64)
+      .lowerIfMemSizeNotByteSizePow2();
 
   getActionDefinitionsBuilder(G_STORE)
       .legalForTypesWithMemDesc(
@@ -167,9 +175,8 @@ WebAssemblyLegalizerInfo::WebAssemblyLegalizerInfo(
                                  {s64, p0, s8, 1},
                                  {s64, p0, s16, 1},
                                  {s64, p0, s32, 1}})
-      .widenScalarToNextPow2(0)
-      .lowerIfMemSizeNotByteSizePow2()
-      .clampScalar(0, s32, s64);
+      .clampScalar(0, s32, s64)
+      .lowerIfMemSizeNotByteSizePow2();
 
   getActionDefinitionsBuilder({G_ZEXTLOAD, G_SEXTLOAD})
       .legalForTypesWithMemDesc({{s32, p0, s8, 1},
@@ -178,10 +185,8 @@ WebAssemblyLegalizerInfo::WebAssemblyLegalizerInfo(
                                  {s64, p0, s8, 1},
                                  {s64, p0, s16, 1},
                                  {s64, p0, s32, 1}})
-      .widenScalarToNextPow2(0)
-      .lowerIfMemSizeNotByteSizePow2()
       .clampScalar(0, s32, s64)
-      .lower();
+      .lowerIfMemSizeNotByteSizePow2();
 
   if (ST.hasBulkMemoryOpt()) {
     getActionDefinitionsBuilder(G_BZERO).unsupported();
@@ -204,7 +209,7 @@ WebAssemblyLegalizerInfo::WebAssemblyLegalizerInfo(
 
   // TODO: figure out how to combine G_ANYEXT of G_ASSERT_{S|Z}EXT (or
   // appropriate G_AND and G_SEXT_IN_REG?) to a G_{S|Z}EXT + G_ASSERT_{S|Z}EXT
-  // for better optimization (since G_ANYEXT lowers to a ZEXT or SEXT
+  // for better optimization (since G_ANYEXT will lower to a ZEXT or SEXT
   // instruction anyway).
 
   getActionDefinitionsBuilder(G_ANYEXT)
@@ -221,8 +226,7 @@ WebAssemblyLegalizerInfo::WebAssemblyLegalizerInfo(
   if (ST.hasSignExt()) {
     getActionDefinitionsBuilder(G_SEXT_INREG)
         .clampScalar(0, s32, s64)
-        .customFor({s32, s64})
-        .lower();
+        .customFor({s32, s64});
   } else {
     getActionDefinitionsBuilder(G_SEXT_INREG).lower();
   }
@@ -242,23 +246,42 @@ WebAssemblyLegalizerInfo::WebAssemblyLegalizerInfo(
       .legalForCartesianProduct({s32, s64}, {p0})
       .clampScalar(0, s32, s64);
 
+  getActionDefinitionsBuilder(G_DYN_STACKALLOC).lowerFor({{p0, p0s}});
+
+  getActionDefinitionsBuilder({G_STACKSAVE, G_STACKRESTORE}).lower();
+
   getLegacyLegalizerInfo().computeTables();
 }
 
 bool WebAssemblyLegalizerInfo::legalizeCustom(
     LegalizerHelper &Helper, MachineInstr &MI,
     LostDebugLocObserver &LocObserver) const {
+  auto &MRI = *Helper.MIRBuilder.getMRI();
+  auto &MIRBuilder = Helper.MIRBuilder;
+
   switch (MI.getOpcode()) {
   case TargetOpcode::G_SEXT_INREG: {
+    assert(MI.getOperand(2).isImm() && "Expected immediate");
+
     // Mark only 8/16/32-bit SEXT_INREG as legal
-    auto [DstType, SrcType] = MI.getFirst2LLTs();
+    auto [DstReg, SrcReg] = MI.getFirst2Regs();
+    auto DstType = MRI.getType(DstReg);
     auto ExtFromWidth = MI.getOperand(2).getImm();
 
     if (ExtFromWidth == 8 || ExtFromWidth == 16 ||
         (DstType.getScalarSizeInBits() == 64 && ExtFromWidth == 32)) {
       return true;
     }
-    return false;
+
+    Register TmpRes = MRI.createGenericVirtualRegister(DstType);
+
+    auto MIBSz = MIRBuilder.buildConstant(
+        DstType, DstType.getScalarSizeInBits() - ExtFromWidth);
+    MIRBuilder.buildShl(TmpRes, SrcReg, MIBSz->getOperand(0));
+    MIRBuilder.buildAShr(DstReg, TmpRes, MIBSz->getOperand(0));
+    MI.eraseFromParent();
+
+    return true;
   }
   case TargetOpcode::G_MEMSET: {
     // Anyext the value being set to 32 bit (only the bottom 8 bits are read by
