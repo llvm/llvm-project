227d45e09c1fa20f35ca0142ba93276fe37e6ab0
Shafik Yaghmour <shafik.yaghmour@intel.com>
2025-09-29 13:54:54 -0700

    [Clang][Sema] Fix crash in CheckUsingDeclQualifier due to diagnostic missing an argument
    Crash report came in and it was pretty obvious the diagnostic line was just
    missing an argument. I supplied the argument and added a test.
    
    Fixes: https://github.com/llvm/llvm-project/issues/161072

--- clang/test/SemaCXX/cxx98-compat.cpp
+++ clang/test/SemaCXX/cxx98-compat.cpp
@@ -1,6 +1,7 @@
 // RUN: %clang_cc1 -fsyntax-only -std=c++11 -Wc++98-compat -verify %s
 // RUN: %clang_cc1 -fsyntax-only -std=c++14 -Wc++98-compat -verify %s -DCXX14COMPAT
 // RUN: %clang_cc1 -fsyntax-only -std=c++17 -Wc++98-compat -verify %s -DCXX14COMPAT -DCXX17COMPAT
+// RUN: %clang_cc1 -fsyntax-only -std=c++20 -Wc++98-compat -verify %s -DCXX14COMPAT -DCXX17COMPAT -DCXX20COMPAT
 
 namespace std {
   struct type_info;
@@ -225,9 +226,12 @@ void TrivialButNonPODThroughEllipsis() {
   Ellipsis(1, TrivialButNonPOD()); // expected-warning {{passing object of trivial but non-POD type 'TrivialButNonPOD' through variadic function is incompatible with C++98}}
 }
 
+// FIXME I think we generate this diagnostic in C++20
+#ifndef CXX20COMPAT
 struct HasExplicitConversion {
   explicit operator bool(); // expected-warning {{explicit conversion functions are incompatible with C++98}}
 };
+#endif
 
 struct Struct {};
 enum Enum { enum_val = 0 };
