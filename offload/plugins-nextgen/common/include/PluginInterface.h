0cd6b0b2561a1f4d8705d63fc024db1ce0d1daf8
Dhruva Chakrabarti <Dhruva.Chakrabarti@amd.com>
2025-09-29 18:05:14 -0500

    [OpenMP][amdgpu] For generic-SPMD kernels, reduce blocksize but maintain occupancy.
    If envar LIBOMPTARGET_AMDGPU_GENERIC_SPMD_USE_SMALL_BLOCKSIZE is set, reduce
    blocksize upto wavefront size as long as occupancy can be maintained by increasing
    the number of teams proportionally.

--- offload/plugins-nextgen/common/include/PluginInterface.h
+++ offload/plugins-nextgen/common/include/PluginInterface.h
@@ -1164,6 +1164,9 @@ struct GenericDeviceTy : public DeviceAllocatorTy {
   virtual uint32_t getOMPXAdjustNumTeamsForXteamRedSmallBlockSize() const {
     llvm_unreachable("Unimplemented");
   }
+  virtual bool getOMPXGenericSpmdUseSmallBlockSize() const {
+    llvm_unreachable("Unimplemented");
+  }
 
   /// Get target compute unit kind (e.g., sm_80, or gfx908).
   virtual std::string getComputeUnitKind() const { return "unknown"; }
