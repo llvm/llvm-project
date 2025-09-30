096c6e4dd039947366051d6c41320ffab56974fb
Shafik Yaghmour <shafik.yaghmour@intel.com>
2025-09-30 13:37:40 -0700

    Update test based on feedback.

--- clang/test/SemaCXX/cxx98-compat.cpp
+++ clang/test/SemaCXX/cxx98-compat.cpp
@@ -1,7 +1,7 @@
 // RUN: %clang_cc1 -fsyntax-only -std=c++11 -Wc++98-compat -verify %s
 // RUN: %clang_cc1 -fsyntax-only -std=c++14 -Wc++98-compat -verify %s -DCXX14COMPAT
 // RUN: %clang_cc1 -fsyntax-only -std=c++17 -Wc++98-compat -verify %s -DCXX14COMPAT -DCXX17COMPAT
-// RUN: %clang_cc1 -fsyntax-only -std=c++20 -Wc++98-compat -verify %s -DCXX14COMPAT -DCXX17COMPAT -DCXX20COMPAT
+// RUN: %clang_cc1 -fsyntax-only -std=c++20 -Wc++98-compat -verify=expected,cpp20 %s -DCXX14COMPAT -DCXX17COMPAT -DCXX20COMPAT
 
 namespace std {
   struct type_info;
@@ -226,7 +226,7 @@ void TrivialButNonPODThroughEllipsis() {
   Ellipsis(1, TrivialButNonPOD()); // expected-warning {{passing object of trivial but non-POD type 'TrivialButNonPOD' through variadic function is incompatible with C++98}}
 }
 
-// FIXME I think we generate this diagnostic in C++20
+// FIXME I think we should generate this diagnostic in C++20
 #ifndef CXX20COMPAT
 struct HasExplicitConversion {
   explicit operator bool(); // expected-warning {{explicit conversion functions are incompatible with C++98}}
@@ -441,8 +441,7 @@ struct S {
   using E::A; // expected-warning {{enumeration type in nested name specifier is incompatible with C++98}}
 #ifndef CXX20COMPAT 
 	      // expected-error@-2 {{using declaration refers to its own class}}
-#else
-              // expected-warning@-4 {{member using declaration naming non-class ''E'' enumerator is incompatible with C++ standards before C++20}}
 #endif
+              // cpp20-warning@-4 {{member using declaration naming non-class ''E'' enumerator is incompatible with C++ standards before C++20}}
 };
 }
