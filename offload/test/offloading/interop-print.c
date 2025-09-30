54544d93d7896dd00c0a1ae53531fe2a42506cff
JP Lehr <JanPatrick.Lehr@amd.com>
2025-09-30 15:23:29 -0500

    [OpenMP] Add test to print interop identifiers
    The test covers some of the identifier symbols in the interop runtime.

---
+++ offload/test/offloading/interop-print.c
@@ -0,0 +1,71 @@
+// RUN: %libomptarget-compile-amdgcn-amd-amdhsa
+// RUN:   %libomptarget-run-generic 2>&1 | \
+// RUN:   %fcheck-generic -check-prefix=AMDCHECK
+
+// REQUIRES: amdgpu
+
+#include <omp.h>
+#include <stdio.h>
+
+const char *interop_int_to_string(const int interop_int) {
+  switch (interop_int) {
+  case 1:
+    return "cuda";
+  case 2:
+    return "cuda_driver";
+  case 3:
+    return "opencl";
+  case 4:
+    return "sycl";
+  case 5:
+    return "hip";
+  case 6:
+    return "level_zero";
+  case 7:
+    return "hsa";
+  default:
+    return "unknown";
+  }
+}
+
+int main(int argc, char **argv) {
+  omp_interop_t iobj = omp_interop_none;
+#pragma omp interop init(targetsync : iobj)
+
+  int err;
+  int interop_int = omp_get_interop_int(iobj, omp_ipr_fr_id, &err);
+
+  if (err) {
+    fprintf(stderr, "omp_get_interop_int failed: %d\n", err);
+    return -1;
+  }
+
+  // FIXME: This should be hsa instead of hip
+  // AMDCHECK: {{.*}} hip
+  printf("omp_get_interop_int returned %s\n",
+         interop_int_to_string(interop_int));
+
+  const char *interop_vendor =
+      omp_get_interop_str(iobj, omp_ipr_vendor_name, &err);
+  if (err) {
+    fprintf(stderr, "omp_get_interop_str failed: %d\n", err);
+    return -1;
+  }
+
+  // AMDCHECK: {{.*}} amd
+  printf("omp_get_interop_str returned %s\n", interop_vendor);
+
+  const char *interop_fr_name =
+      omp_get_interop_str(iobj, omp_ipr_fr_name, &err);
+  if (err) {
+    fprintf(stderr, "omp_get_interop_str failed: %d\n", err);
+    return -1;
+  }
+
+  // FIXME: This should be hsa instead of hip
+  // AMDCHECK: {{.*}} hip
+  printf("omp_get_interop_str returned %s\n", interop_fr_name);
+
+#pragma omp interop destroy(iobj)
+  return 0;
+}
