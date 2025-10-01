1271ba3c8ab57674742b018f9ba56593f167e755
Mariya Podchishchaeva <mariya.podchishchaeva@intel.com>
2025-10-01 19:52:56 +0200

    [SYCL-Upstreaming] Fix a crash (#52)
    In case a function with skep attribute is instantiated two times with
    the same kernel name the attribute is invalid due to the conflicting name.
    Make sure to exit from instantiation of UnresolvedSYCLKernelCallStmt in
    this case.

--- clang/lib/Sema/TreeTransform.h
+++ clang/lib/Sema/TreeTransform.h
@@ -12936,6 +12936,11 @@ ExprResult TreeTransform<Derived>::TransformSYCLUniqueStableNameExpr(
 template <typename Derived>
 StmtResult TreeTransform<Derived>::TransformUnresolvedSYCLKernelCallStmt(
     UnresolvedSYCLKernelCallStmt *S) {
+  auto *FD = cast<FunctionDecl>(SemaRef.CurContext);
+  const auto *SKEPAttr = FD->getAttr<SYCLKernelEntryPointAttr>();
+  if (!SKEPAttr || SKEPAttr->isInvalidAttr())
+    return StmtError();
+
   ExprResult IdExpr = getDerived().TransformExpr(S->getKernelLaunchIdExpr());
 
   if (IdExpr.isInvalid())
