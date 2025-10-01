c6b0cd3308cc6882ebd5bbb223a25d5fef7f86b3
Henry Baba-Weiss <henry.babaweiss@gmail.com>
2025-09-30 22:05:35 -0700

    fixup! [clang][CodeGen][MSVC] Return vector types from methods indirectly
    Rework the test to also cover 64-bit vector types.

--- clang/test/CodeGenCXX/microsoft-abi-vector-types.cpp
+++ clang/test/CodeGenCXX/microsoft-abi-vector-types.cpp
@@ -11,45 +11,83 @@
 // RUN: %clang_cc1 -ffreestanding -emit-llvm %s -o - -triple=aarch64-pc-windows-msvc \
 // RUN:   -fclang-abi-compat=21 | FileCheck --check-prefixes=CHECK,CLANG21 %s
 
-// To match the MSVC ABI, vector types must be returned indirectly from member
-// functions on x86 and x86-64 (as long as they do not use the vectorcall
-// calling convention), but must be returned directly everywhere else.
+// To match the MSVC ABI, vector types are usually returned directly, but on x86
+// and x86-64 they must be returned indirectly from member functions (unless
+// they use the vectorcall calling convention and the vector type is > 64 bits).
 
 #if defined(__i386__) || defined(__x86_64__)
 #include <xmmintrin.h>
-#define VECTOR_TYPE __m128
+
+#define VECTOR64_TYPE __m64
+#define VECTOR128_TYPE __m128
+
+#define VECTORCALL __vectorcall
 #endif
 
 #ifdef __aarch64__
 #include <arm_neon.h>
-#define VECTOR_TYPE float32x4_t
+
+// These were chosen such that they lower to the same types that the x86 vector
+// types lower to (e.g. int64x1_t and __m64 both lower to <1 x i64>).
+#define VECTOR64_TYPE int64x1_t
+#define VECTOR128_TYPE float32x4_t
+
+#define VECTORCALL
 #endif
 
 struct Foo {
-  VECTOR_TYPE method_ret_vec();
-  VECTOR_TYPE __vectorcall vectorcall_method_ret_vec();
+  VECTOR64_TYPE method_ret_vec64();
+  VECTOR128_TYPE method_ret_vec128();
+
+  VECTOR64_TYPE VECTORCALL vc_method_ret_vec64();
+  VECTOR128_TYPE VECTORCALL vc_method_ret_vec128();
 };
 
-VECTOR_TYPE Foo::method_ret_vec() {
-  return VECTOR_TYPE{};
-// GH104
+VECTOR64_TYPE Foo::method_ret_vec64() {
+  return VECTOR64_TYPE{};
+// X86: store <1 x i64>
+// X86: ret void
+// AARCH64: ret <1 x i64>
+// CLANG21: ret <1 x i64>
+}
+
+VECTOR128_TYPE Foo::method_ret_vec128() {
+  return VECTOR128_TYPE{};
 // X86: store <4 x float>
 // X86: ret void
 // AARCH64: ret <4 x float>
 // CLANG21: ret <4 x float>
 }
 
-VECTOR_TYPE __vectorcall Foo::vectorcall_method_ret_vec() {
-  return VECTOR_TYPE{};
+VECTOR64_TYPE VECTORCALL Foo::vc_method_ret_vec64() {
+  return VECTOR64_TYPE{};
+// X86: store <1 x i64>
+// X86: ret void
+// AARCH64: ret <1 x i64>
+// CLANG21: ret <1 x i64>
+}
+
+VECTOR128_TYPE VECTORCALL Foo::vc_method_ret_vec128() {
+  return VECTOR128_TYPE{};
 // CHECK: ret <4 x float>
 }
 
-VECTOR_TYPE func_ret_vec() {
-  return VECTOR_TYPE{};
+VECTOR64_TYPE func_ret_vec64() {
+  return VECTOR64_TYPE{};
+// CHECK: ret <1 x i64>
+}
+
+VECTOR128_TYPE func_ret_vec128() {
+  return VECTOR128_TYPE{};
 // CHECK: ret <4 x float>
 }
 
-VECTOR_TYPE __vectorcall vectorcall_func_ret_vec() {
-  return VECTOR_TYPE{};
+VECTOR64_TYPE VECTORCALL vc_func_ret_vec64() {
+  return VECTOR64_TYPE{};
+// CHECK: ret <1 x i64>
+}
+
+VECTOR128_TYPE VECTORCALL vc_func_ret_vec128() {
+  return VECTOR128_TYPE{};
 // CHECK: ret <4 x float>
 }
