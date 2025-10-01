7520b5751a72a2e577f39874c693e570086271a0
Augusto Noronha <anoronha@apple.com>
2025-10-01 11:22:57 -0700

    Revert "[lldb] look up function names in the swift language plugin (#11477)"
    This reverts commit 13c116b3de9bb6f2f637ec1f39c25efe43335696.

--- lldb/source/Core/Module.cpp
+++ lldb/source/Core/Module.cpp
@@ -661,11 +661,7 @@ Module::LookupInfo::LookupInfo(ConstString name,
     if (language != eLanguageTypeUnknown)
       lang_types.push_back(language);
     else
-      lang_types = {eLanguageTypeObjC, eLanguageTypeC_plus_plus,
-#ifdef LLDB_ENABLE_SWIFT
-                    eLanguageTypeSwift
-#endif
-      };
+      lang_types = {eLanguageTypeObjC, eLanguageTypeC_plus_plus};
 
     for (LanguageType lang_type : lang_types) {
       if (Language *lang = Language::FindPlugin(lang_type))
