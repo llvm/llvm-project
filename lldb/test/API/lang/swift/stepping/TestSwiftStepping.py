17d68132f20e33ebbf9fc63da73f406242aa59ea
Ebuka Ezike <yerimyah1@gmail.com>
2025-10-01 15:56:16 +0100

    [lldb][test] skip on other archs on linux (#11539)

--- lldb/test/API/lang/swift/stepping/TestSwiftStepping.py
+++ lldb/test/API/lang/swift/stepping/TestSwiftStepping.py
@@ -24,6 +24,7 @@ class TestSwiftStepping(lldbtest.TestBase):
     mydir = lldbtest.TestBase.compute_mydir(__file__)
 
     @swiftTest
+    @skipIf(oslist=["linux"], archs=no_match("x86_64"))
     def test_swift_stepping(self):
         """Tests that we can step reliably in swift code."""
         self.build()
