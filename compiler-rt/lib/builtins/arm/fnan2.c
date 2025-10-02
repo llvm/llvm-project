a6f62633913f676e3fe9f46d4002a30a12e20ed8
Simon Tatham <simon.tatham@arm.com>
2025-10-02 14:24:35 +0100

    Changes to fnan2 to be consistent wih #161546

--- compiler-rt/lib/builtins/arm/fnan2.c
+++ compiler-rt/lib/builtins/arm/fnan2.c
@@ -15,7 +15,9 @@
 //
 //===----------------------------------------------------------------------===//
 
-unsigned __compiler_rt_fnan2(unsigned a, unsigned b) {
+#include <stdint.h>
+
+uint32_t __compiler_rt_fnan2(uint32_t a, uint32_t b) {
   // Make shifted-left copies of a and b to discard the sign bit. Then add 1 at
   // the bit position where the quiet vs signalling bit ended up. This squashes
   // all the signalling NaNs to the top of the range of 32-bit values, from
@@ -23,14 +25,13 @@ unsigned __compiler_rt_fnan2(unsigned a, unsigned b) {
   // wrap round to the bottom, from 0 to 0x007fffff inclusive. So we can detect
   // a signalling NaN by asking if it's greater than 0xff800000, and a quiet
   // one by asking if it's less than 0x00800000.
-  unsigned aadj = (a << 1) + 0x00800000;
-  unsigned badj = (b << 1) + 0x00800000;
+  uint32_t aadj = (a << 1) + 0x00800000;
+  uint32_t badj = (b << 1) + 0x00800000;
   if (aadj > 0xff800000)   // a is a signalling NaN?
     return a | 0x00400000; //   if so, return it with the quiet bit set
   if (badj > 0xff800000)   // b is a signalling NaN?
     return b | 0x00400000; //   if so, return it with the quiet bit set
   if (aadj < 0x00800000)   // a is a quiet NaN?
     return a;              // if so, return it
-  else /* expect (badj < 0x00800000) */
-    return b;              // in that case b must be a quiet NaN
+  return b;                // otherwise we expect b must be a quiet NaN
 }
