280ff82dc1b8de4d5a0159843b84a96265cb9dd8
Patrick Simmons <psimmons@pensando.io>
2025-10-06 11:57:59 -0500

    Revert changes to variant.h

--- flang/include/flang/Common/variant.h
+++ flang/include/flang/Common/variant.h
@@ -24,24 +24,6 @@ using cuda::std::variant_size_v;
 using cuda::std::visit;
 } // namespace std
 #else // !RT_USE_LIBCUDACXX
-
-// initializer_list is included to load bits/c++config, which can't be included
-// directly and which defines a macro we need to redefine.
-#include <initializer_list>
-
-// The macro _GLIBCXX_THROW_OR_ABORT is used by libstdc++ to not throw
-// exceptions in -fno-exceptions mode, but immediatly kill the program. Since
-// libstdc++ 15.1 the macro uses (void)(_EXC) after calling abort() to silence
-// compiler warnings of an parameter. In its use in <variant>, _EXC is the
-// construction of `std::bad_variant_access`. In non-optimized builds, some
-// compilers including Clang will emit a call to that constructor. The
-// constructor is implemented in libstdc++.a/.so which Flang-RT must not depend
-// on (to avoid compatibility problems if a Fortran application itself has parts
-// implemented in C++). Note that _GLIBCXX_THROW_OR_ABORT is not on the list of
-// libstdc++'s documented user-configurable macros.
-#undef _GLIBCXX_THROW_OR_ABORT
-#define _GLIBCXX_THROW_OR_ABORT(_EXC) (__builtin_abort())
-
 #include <variant>
 #endif // !RT_USE_LIBCUDACXX
 
