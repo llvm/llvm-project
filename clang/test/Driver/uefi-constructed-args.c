e888f567db6ac4152c2790a98f266f9133db06f0
prabhukr <prabhukr@google.com>
2025-10-07 22:54:03 +0000

    fix test failure.

--- clang/test/Driver/uefi-constructed-args.c
+++ clang/test/Driver/uefi-constructed-args.c
@@ -13,7 +13,7 @@
 // CHECK-SAME: "/tsaware:no"
 // CHECK-SAME: "/debug"
 
-// RUN: %clang -### --target=x86_64-unknown-uefi -print-search-dirs 2>&1
+// RUN: %clang -### --target=x86_64-unknown-uefi -print-search-dirs 2>&1 \
 // RUN:     | FileCheck -check-prefixes=PROGPATH %s
 // PROGPATH-DAG: InstalledDir: [[DRIVER_INSTALLED_DIR:.*]]
 // PROGPATH: programs: =[[DRIVER_INSTALLED_DIR]]
