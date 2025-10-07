0d8252ff23d7ff94ec879094da104798bc58daf4
makslevental <maksim.levental@gmail.com>
2025-10-07 08:45:00 -0700

    Revert "[MLIR][Python] use `FetchContent_Declare` for nanobind and remove pybind (#161230)"
    This reverts commit 84a214856ad989f37af19f5e8aaa9ec2346dde6f.

--- mlir/test/python/dialects/python_test.py
+++ mlir/test/python/dialects/python_test.py
@@ -1,4 +1,5 @@
-# RUN: %PYTHON %s | FileCheck %s
+# RUN: %PYTHON %s pybind11 | FileCheck %s
+# RUN: %PYTHON %s nanobind | FileCheck %s
 import sys
 import typing
 from typing import Union, Optional
@@ -9,14 +10,26 @@ import mlir.dialects.python_test as test
 import mlir.dialects.tensor as tensor
 import mlir.dialects.arith as arith
 
-from mlir._mlir_libs._mlirPythonTestNanobind import (
-    TestAttr,
-    TestType,
-    TestTensorValue,
-    TestIntegerRankedTensorType,
-)
+if sys.argv[1] == "pybind11":
+    from mlir._mlir_libs._mlirPythonTestPybind11 import (
+        TestAttr,
+        TestType,
+        TestTensorValue,
+        TestIntegerRankedTensorType,
+    )
 
-test.register_python_test_dialect(get_dialect_registry())
+    test.register_python_test_dialect(get_dialect_registry(), use_nanobind=False)
+elif sys.argv[1] == "nanobind":
+    from mlir._mlir_libs._mlirPythonTestNanobind import (
+        TestAttr,
+        TestType,
+        TestTensorValue,
+        TestIntegerRankedTensorType,
+    )
+
+    test.register_python_test_dialect(get_dialect_registry(), use_nanobind=True)
+else:
+    raise ValueError("Expected pybind11 or nanobind as argument")
 
 
 def run(f):
