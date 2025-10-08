b4c2c6a4a0b04f616d2b5b6d51f159d87339c6ee
Louis Dionne <ldionne.2@gmail.com>
2025-10-08 09:53:46 -0400

    Subset boost-math using the script

--- third-party/boost-math/test/compile_test/tools_cxx03_warn_incl_test.cpp
+++
@@ -1,9 +0,0 @@
-//  Copyright Matt Borland 2021.
-//  Use, modification and distribution are subject to the
-//  Boost Software License, Version 1.0. (See accompanying file
-//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-//
-// Basic sanity check that header
-// #includes all the files that it needs to.
-//
-#include <boost/math/tools/cxx03_warn.hpp>
