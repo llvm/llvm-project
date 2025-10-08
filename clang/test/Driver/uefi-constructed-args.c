7b48b2a20c6508cff6941c5553a40b95bacabc91
prabhukr <prabhukr@google.com>
2025-10-08 16:23:12 +0000

    Remove unnecessary noop DAG

--- clang/test/Driver/uefi-constructed-args.c
+++ clang/test/Driver/uefi-constructed-args.c
@@ -15,5 +15,5 @@
 
 // RUN: %clang -### --target=x86_64-unknown-uefi -print-search-dirs 2>&1 \
 // RUN:     | FileCheck -check-prefixes=PROGPATH %s
-// PROGPATH-DAG: InstalledDir: [[DRIVER_INSTALLED_DIR:.*]]
+// PROGPATH: InstalledDir: [[DRIVER_INSTALLED_DIR:.*]]
 // PROGPATH: programs: =[[DRIVER_INSTALLED_DIR]]
