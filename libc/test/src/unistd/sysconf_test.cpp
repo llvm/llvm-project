8c22e9d9c3e512d4cade6e5053e71cbe524fbbb7
Schrodinger ZHU Yifan <yifanzhu@rochester.edu>
2025-10-09 10:48:06 -0400

    [libc] skip sysconf test for rv32

--- libc/test/src/unistd/sysconf_test.cpp
+++ libc/test/src/unistd/sysconf_test.cpp
@@ -13,5 +13,9 @@
 
 TEST(LlvmLibcSysconfTest, PagesizeTest) {
   long pagesize = LIBC_NAMESPACE::sysconf(_SC_PAGESIZE);
+  // TODO: fix page size support on RV32
+  // (https://github.com/llvm/llvm-project/issues/162671)
+#ifndef LIBC_TARGET_ARCH_IS_RISCV32
   ASSERT_GT(pagesize, 0l);
+#endif
 }
