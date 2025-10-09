578dcd573781a6e12d2ef4122f71d77682623eb6
Patrick Simmons <psimmons@pensando.io>
2025-10-09 14:27:53 -0500

    Add unconditional-branch-to-next

--- llvm/lib/Target/AMDGPU/SILowerControlFlow.cpp
+++ llvm/lib/Target/AMDGPU/SILowerControlFlow.cpp
@@ -807,7 +807,7 @@ bool SILowerControlFlow::run(MachineFunction &MF) {
     }
   }
 
-  bool Changed = false;
+  bool Changed = makeEverySuccessorBeBranchTarget(MF);
   MachineFunction::iterator NextBB;
   for (MachineFunction::iterator BI = MF.begin();
        BI != MF.end(); BI = NextBB) {
