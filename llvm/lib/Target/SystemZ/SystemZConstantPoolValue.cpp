8350d95579dd4a23f639e4faa6fbf5b107c1e180
Ismael Marisclan <Ismael.Marisclan@ibm.com>
2025-10-09 04:47:54 -0700

    indent fix

--- llvm/lib/Target/SystemZ/SystemZConstantPoolValue.cpp
+++ llvm/lib/Target/SystemZ/SystemZConstantPoolValue.cpp
@@ -14,8 +14,8 @@
 using namespace llvm;
 
 SystemZConstantPoolValue::SystemZConstantPoolValue(
-  const GlobalValue *GV, SystemZCP::SystemZCPModifier Modifier)
-  : MachineConstantPoolValue(GV->getType()), GV(GV), Modifier(Modifier) {}
+    const GlobalValue *GV, SystemZCP::SystemZCPModifier Modifier)
+    : MachineConstantPoolValue(GV->getType()), GV(GV), Modifier(Modifier) {}
 
 SystemZConstantPoolValue *
 SystemZConstantPoolValue::Create(const GlobalValue *GV,
