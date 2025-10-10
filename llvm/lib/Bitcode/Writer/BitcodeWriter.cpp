e9a8d505cffcc3f5c2ec356502d3cd176755743b
Juan Manuel Martinez Caamaño <juamarti@amd.com>
2025-10-10 10:40:58 +0200

    [Review] Forgot again to run clang-format

--- llvm/lib/Bitcode/Writer/BitcodeWriter.cpp
+++ llvm/lib/Bitcode/Writer/BitcodeWriter.cpp
@@ -5849,9 +5849,7 @@ static const char *getSectionNameForCommandline(const Triple &T) {
 }
 
 static auto globalInUsedHasName(StringRef Name) {
-  return [Name](Constant* C) {
-    return C->getName() == Name;
-  };
+  return [Name](Constant *C) { return C->getName() == Name; };
 }
 
 void llvm::embedBitcodeInModule(llvm::Module &M, llvm::MemoryBufferRef Buf,
