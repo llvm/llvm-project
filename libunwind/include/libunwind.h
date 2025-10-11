cb0413eb40a513317af42597726ae6e8f94c8762
trcrsired <uwgghhbcad@gmail.com>
2025-10-11 14:40:14 +0800

    [libunwind][libcxx][libcxxabi] Fix Exception Handling build for wasm
    The wasm unwind build appears to be dysfunctional, likely because the author has only supplied a customized LLVM build on request, rather than a fully functional patch.
    
    This patch fixes the build
    
    Apply formatting patch proposed by github bot
    
    use "" to prevent CMAKE_SYSTEM_PROCESSOR not defined
    
    [libunwind] logAPI functions should also be built
    
    [libcxxabi] Fix function signatures for wasm
    
    wasm does not define the function signatures correctly for cxxabi
    Fix them
    
    Fix formatting issues for libcxxabi's wasm eh change
    
    Merge remote-tracking branch 'parent/main' into wasmlibunwindfix
    
    remove unwanted changes in unwind-wasm.c
    
    Make Unwind-wasm.c compile correctly without workaround in
    CMakeLists.txt
    
    using __wasm__ macro to guard against all wasm eh build
    
    fix UnwindLevel.c's formatting issue
    
    ISO C requires a translation unit to contain at least one declaration [-Werror,-Wempty-translation-unit]
    
    compiler-rt does not define CMP_RESULT correct on wasm64
    Fixed
    
    Merge code

--- libunwind/include/libunwind.h
+++ libunwind/include/libunwind.h
@@ -15,6 +15,7 @@
 
 #include <__libunwind_config.h>
 
+#ifndef __wasm__
 #include <stdint.h>
 #include <stddef.h>
 
@@ -1300,5 +1301,6 @@ enum {
   UNW_LOONGARCH_F30 = 62,
   UNW_LOONGARCH_F31 = 63,
 };
+#endif
 
 #endif
