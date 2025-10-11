404527f36421be7ac2bc12c4f55b6aa53a51f890
Krzysztof Parzyszek <Krzysztof.Parzyszek@amd.com>
2025-10-11 15:25:15 -0500

    [flang][OpenMP] Frontend support for DEVICE_SAFESYNC
    Add parsing and semantic checks for DEVICE_SAFESYNC clause. No lowering.

--- flang/lib/Parser/openmp-parsers.cpp
+++ flang/lib/Parser/openmp-parsers.cpp
@@ -1150,6 +1150,9 @@ TYPE_PARSER( //
             construct<OmpDestroyClause>(Parser<OmpObject>{}))))) ||
     "DEVICE" >> construct<OmpClause>(construct<OmpClause::Device>(
                     parenthesized(Parser<OmpDeviceClause>{}))) ||
+    "DEVICE_SAFESYNC" >>
+        construct<OmpClause>(construct<OmpClause::DeviceSafesync>(
+            maybe(parenthesized(scalarLogicalConstantExpr)))) ||
     "DEVICE_TYPE" >> construct<OmpClause>(construct<OmpClause::DeviceType>(
                          parenthesized(Parser<OmpDeviceTypeClause>{}))) ||
     "DIST_SCHEDULE" >>
