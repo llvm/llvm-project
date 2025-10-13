5c0753dc5200452adaff1285274e7364ccfbb222
Perry Gibson <perry@fractile.ai>
2025-10-13 17:32:00 +0100

    refactor: replace templated forEachAttr with std::function version

--- mlir/lib/Bindings/Python/IRCore.cpp
+++ mlir/lib/Bindings/Python/IRCore.cpp
@@ -2730,8 +2730,8 @@ public:
         operation->get(), toMlirStringRef(name)));
   }
 
-  template <typename F>
-  auto forEachAttr(F fn) {
+  void
+  forEachAttr(const std::function<void(MlirStringRef, MlirAttribute)> &fn) {
     intptr_t n = mlirOperationGetNumAttributes(operation->get());
     for (intptr_t i = 0; i < n; ++i) {
       MlirNamedAttribute na = mlirOperationGetAttribute(operation->get(), i);
