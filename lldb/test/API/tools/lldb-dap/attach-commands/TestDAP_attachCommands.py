3aed18b11ab9f774f23e78829b2b5e4cc87fa2c8
Jonas Devlieghere <jonas@devlieghere.com>
2025-10-14 14:01:31 -0700

    Update test name

--- lldb/test/API/tools/lldb-dap/attach-commands/TestDAP_attachCommands.py
+++ lldb/test/API/tools/lldb-dap/attach-commands/TestDAP_attachCommands.py
@@ -8,7 +8,7 @@ from lldbsuite.test import lldbutil
 import lldbdap_testcase
 import time
 
-class TestDAP_attach(lldbdap_testcase.DAPTestCaseBase):
+class TestDAP_attachCommands(lldbdap_testcase.DAPTestCaseBase):
     @skipIfNetBSD  # Hangs on NetBSD as well
     def test_commands(self):
         """
