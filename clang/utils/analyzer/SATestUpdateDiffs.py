54624f702d26cf682fbb29a1df80451fff0647e7
Ross Burton <ross.burton@arm.com>
2025-10-15 15:04:40 +0100

    [clang] Unify python shebangs
    As per PEP-0394[1], there is no real concensus over what binary names
    Python has, specifically 'python' could be Python 3, Python 2, or not
    exist.
    
    However, everyone has a python3 interpreter and the scripts are all
    written for Python 3.  Unify the shebangs so that the ~50% of shebangs
    that use python now use python3.
    
    [1] https://peps.python.org/pep-0394/

--- clang/utils/analyzer/SATestUpdateDiffs.py
+++ clang/utils/analyzer/SATestUpdateDiffs.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 """
 Update reference results for static analyzer.
--- clang/utils/analyzer/SumTimerInfo.py
+++ clang/utils/analyzer/SumTimerInfo.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 """
 Script to Summarize statistics in the scan-build output.
