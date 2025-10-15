cb6e3a77a8bd17eb3b26a2d52a779154bd5489be
A. Jiang <de34@live.cn>
2025-10-15 19:09:17 +0800

    Update to use `__is_segmented_iterator_v`, take 2

--- libcxx/include/__algorithm/fill.h
+++ libcxx/include/__algorithm/fill.h
@@ -45,7 +45,7 @@ __fill(_RandomAccessIterator __first, _RandomAccessIterator __last, const _Tp& _
 #ifndef _LIBCPP_CXX03_LANG
 template <class _SegmentedIterator,
           class _Tp,
-          __enable_if_t<__is_segmented_iterator<_SegmentedIterator>::value, int> = 0>
+          __enable_if_t<__is_segmented_iterator_v<_SegmentedIterator>, int> = 0>
 inline _LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX20 _SegmentedIterator
 __fill(_SegmentedIterator __first, _SegmentedIterator __last, const _Tp& __value) {
   using __local_iterator_t = typename __segmented_iterator_traits<_SegmentedIterator>::__local_iterator;
