7d2ccb00780b5e7a5ba602ec922312528b145066
Milczek, Szymon <szymon.milczek@intel.com>
2025-10-15 20:08:04 +0200

    Fix hdrs

--- llvm/unittests/MC/Disassembler.cpp
+++ llvm/unittests/MC/Disassembler.cpp
@@ -1,4 +1,4 @@
-//===- llvm/unittest/Object/Disassembler.cpp ------------------------------===//
+//===- llvm/unittest/MC/Disassembler.cpp ----------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
