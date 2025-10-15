49e76c0023f21dde7e17c10ef90220162c194c3c
c8ef <c8ef@outlook.com>
2025-10-16 02:14:01 +0800

    namespace

--- libcxx/include/__algorithm/generate.h
+++ libcxx/include/__algorithm/generate.h
@@ -19,6 +19,8 @@
 
 _LIBCPP_BEGIN_NAMESPACE_STD
 
+namespace __generate {
+
 template <typename _Generator>
 struct __fn {
   _LIBCPP_HIDE_FROM_ABI __fn(_Generator& __gen) : __gen_(__gen) {}
@@ -31,10 +33,12 @@ struct __fn {
   _Generator& __gen_;
 };
 
+} // namespace __generate
+
 template <class _ForwardIterator, class _Generator>
 inline _LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX20 void
 generate(_ForwardIterator __first, _ForwardIterator __last, _Generator __gen) {
-  std::for_each(__first, __last, __fn<_Generator>(__gen));
+  std::for_each(__first, __last, __generate::__fn<_Generator>(__gen));
 }
 
 _LIBCPP_END_NAMESPACE_STD
