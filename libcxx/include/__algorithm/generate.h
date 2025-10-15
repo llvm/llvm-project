cea8b3d690bd566eb756d9b01a66833ccb7e9cc5
c8ef <c8ef@outlook.com>
2025-10-16 01:47:03 +0800

    include

--- libcxx/include/__algorithm/generate.h
+++ libcxx/include/__algorithm/generate.h
@@ -11,6 +11,7 @@
 
 #include <__algorithm/for_each.h>
 #include <__config>
+#include <__iterator/iterator_traits.h>
 #include <__utility/forward.h>
 
 #if !defined(_LIBCPP_HAS_NO_PRAGMA_SYSTEM_HEADER)
