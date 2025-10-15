6b63e05f10ded71f764ec7ac86175159401cf73a
Hristo Hristov <hghristov.rmm@gmail.com>
2025-10-15 08:32:39 +0300

    [libc+][stack] P3372R3: `constexpr <stack>`
    Implements `<stack>` as per P3372R3: `constexpr` containers and adaptors
    - https://wg21.link/P3372R3
    
    Closes #128672
    
    This should be a fairly straightforward implementation, as `std::stack` is a
    container adaptor, and most of the work is done in the underlying container.
    
    Depends on #128656
    Part of #127876
    
    - https://wg21.link/P3372R3
    - https://eel.is/c++draft/#containers
      - https://eel.is/c++draft/container.adaptors#stack.syn
        - https://eel.is/c++draft/stack.syn
        - https://eel.is/c++draft/stack

--- libcxx/test/std/containers/container.adaptors/stack/stack.defn/size.pass.cpp
+++ libcxx/test/std/containers/container.adaptors/stack/stack.defn/size.pass.cpp
@@ -15,11 +15,20 @@
 
 #include "test_macros.h"
 
-int main(int, char**) {
+TEST_CONSTEXPR_CXX26 bool test() {
   std::stack<int> q;
   assert(q.size() == 0);
   q.push(1);
   assert(q.size() == 1);
+
+  return 0;
+}
+
+int main(int, char**) {
+  test();
+#if TEST_STD_VER >= 26
+  static_assert(test());
+#endif
 
   return 0;
 }
