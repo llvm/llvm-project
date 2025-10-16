370104deddcee4e723db69add52c5b97b111d2f9
bassiounix <muhammad.m.bassiouni@gmail.com>
2025-10-17 02:06:05 +0300

    change clarifying comment

--- libc/include/llvm-libc-types/errno_t.h
+++ libc/include/llvm-libc-types/errno_t.h
@@ -9,9 +9,12 @@
 #ifndef LLVM_LIBC_INCLUDE_LLVM_LIBC_TYPES_ERRNO_T_H
 #define LLVM_LIBC_INCLUDE_LLVM_LIBC_TYPES_ERRNO_T_H
 
-// LIBC_HAS_ANNEX_K is a necessary check guard here because errno_t is only
-// defined when Annex K is enabled. We use LIBC_HAS_ANNEX_K internally to
-// indicate whether Annex K is enabled or not.
+// LIBC_HAS_ANNEX_K is used to check whether C11 Annex K (the optional
+// “Bounds-checking interfaces”) is enabled in this libc implementation. Annex K
+// introduces additional types and functions, including `errno_t` (a typedef
+// used by the *_s functions). Since `errno_t` is *not defined* in the standard
+// library unless Annex K is enabled, we must guard any code that uses it with
+// LIBC_HAS_ANNEX_K.
 #ifdef LIBC_HAS_ANNEX_K
 
 typedef int errno_t;
