4d5e0d830a8082e277696b7e82fea232bb76afbf
Jonathan Thackray <jonathan.thackray@arm.com>
2025-10-17 00:45:14 +0100

    fixup! [AArch64][llvm] Armv9.7-A: Add support for SVE2p3 CVT operations
    Remove superfluous RUN lines

--- llvm/test/MC/AArch64/SVE2p3/cvt.s
+++ llvm/test/MC/AArch64/SVE2p3/cvt.s
@@ -241,21 +241,6 @@ scvtflt z31.d, z31.s
 // CHECK-ENCODING: encoding: [0xff,0x3b,0xcc,0x65]
 // CHECK-ERROR: instruction requires: sme2p3 or sve2p3
 // CHECK-UNKNOWN: 65cc3bff <unknown>
-// RUN: llvm-mc -triple=aarch64 -show-encoding -mattr=+sve2p3 < %s \
-// RUN:        | FileCheck %s --check-prefixes=CHECK-ENCODING,CHECK-INST
-// RUN: llvm-mc -triple=aarch64 -show-encoding -mattr=+sme2p3 < %s \
-// RUN:        | FileCheck %s --check-prefixes=CHECK-ENCODING,CHECK-INST
-// RUN: not llvm-mc -triple=aarch64 -show-encoding < %s 2>&1 \
-// RUN:        | FileCheck %s --check-prefix=CHECK-ERROR
-// RUN: llvm-mc -triple=aarch64 -filetype=obj -mattr=+sve2p3 < %s \
-// RUN:        | llvm-objdump -d --mattr=+sve2p3 --no-print-imm-hex - | FileCheck %s --check-prefix=CHECK-INST
-// RUN: llvm-mc -triple=aarch64 -filetype=obj -mattr=+sve2p3 < %s \
-// RUN:        | llvm-objdump -d --mattr=-sve2p3 --no-print-imm-hex - | FileCheck %s --check-prefix=CHECK-UNKNOWN
-// Disassemble encoding and check the re-encoding (-show-encoding) matches.
-// RUN: llvm-mc -triple=aarch64 -show-encoding -mattr=+sve2p3 < %s \
-// RUN:        | sed '/.text/d' | sed 's/.*encoding: //g' \
-// RUN:        | llvm-mc -triple=aarch64 -mattr=+sve2p3 -disassemble -show-encoding \
-// RUN:        | FileCheck %s --check-prefixes=CHECK-ENCODING,CHECK-INST
 
 // -----------------------------------------------------------------------
 // Unsigned integer convert to floating-point (bottom, unpredicated)
