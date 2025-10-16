0935f5d8b65c1d0e3c7f7752b654d4953ebafa70
Fabrizio Indirli <Fabrizio.Indirli@arm.com>
2025-10-16 16:22:10 +0100

    [mlir][scf] Add parallelLoopUnrollByFactors()
    - In the SCF Utils, add the parallelLoopUnrollByFactors() function
      to unroll scf::ParallelOp loops according to the specified unroll factors
    - Add a test pass "TestParallelLoopUnrolling" and the related LIT test
    - Expose mlir::parallelLoopUnrollByFactors(), mlir::generateUnrolledLoop(),
      and mlir::scf::computeUbMinusLb() functions in the
      mlir/Dialect/SCF/Utils/Utils.h header to make thme available to other passes.
    - In mlir::generateUnrolledLoop(), add also an optional `IRMapping *clonedToSrcOpsMap`
      argument to map the new cloned operations to their original ones.
      In the function body, change the default `AnnotateFn` type to `static const`
      to silence potential warnings about dangling references when a function_ref
      is assigned  to a variable with automatic storage.
    
    Signed-off-by: Fabrizio Indirli <Fabrizio.Indirli@arm.com>

--- mlir/tools/mlir-opt/mlir-opt.cpp
+++ mlir/tools/mlir-opt/mlir-opt.cpp
@@ -139,6 +139,7 @@ void registerTestOneShotModuleBufferizePass();
 void registerTestOpaqueLoc();
 void registerTestOpLoweringPasses();
 void registerTestPadFusion();
+void registerTestParallelLoopUnrollingPass();
 void registerTestRecursiveTypesPass();
 void registerTestSCFUpliftWhileToFor();
 void registerTestSCFUtilsPass();
@@ -286,6 +287,7 @@ void registerTestPasses() {
   mlir::test::registerTestOpaqueLoc();
   mlir::test::registerTestOpLoweringPasses();
   mlir::test::registerTestPadFusion();
+  mlir::test::registerTestParallelLoopUnrollingPass();
   mlir::test::registerTestRecursiveTypesPass();
   mlir::test::registerTestSCFUpliftWhileToFor();
   mlir::test::registerTestSCFUtilsPass();
