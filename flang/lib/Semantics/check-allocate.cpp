36c9b4fd6de9cb5665e98b816e54e8aec5592f05
Peter Klausler <pklausler@nvidia.com>
2025-10-16 12:21:14 -0700

    [flang] Fix character length checking in ALLOCATE (#163657)
    The known character length compatibility check for ALLOCATE statements
    needs to allow for negative lengths, which are effectively zero.
    
    Fixes https://github.com/llvm/llvm-project/issues/163242.

--- flang/lib/Semantics/check-allocate.cpp
+++ flang/lib/Semantics/check-allocate.cpp
@@ -441,7 +441,7 @@ static bool HaveCompatibleLengths(
         evaluate::ToInt64(type1.characterTypeSpec().length().GetExplicit())};
     auto v2{
         evaluate::ToInt64(type2.characterTypeSpec().length().GetExplicit())};
-    return !v1 || !v2 || *v1 == *v2;
+    return !v1 || !v2 || (*v1 >= 0 ? *v1 : 0) == (*v2 >= 0 ? *v2 : 0);
   } else {
     return true;
   }
@@ -454,7 +454,7 @@ static bool HaveCompatibleLengths(
     auto v1{
         evaluate::ToInt64(type1.characterTypeSpec().length().GetExplicit())};
     auto v2{type2.knownLength()};
-    return !v1 || !v2 || *v1 == *v2;
+    return !v1 || !v2 || (*v1 >= 0 ? *v1 : 0) == (*v2 >= 0 ? *v2 : 0);
   } else {
     return true;
   }
