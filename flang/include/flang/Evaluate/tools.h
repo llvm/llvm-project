6085e7f0c5d436e90cb2edcfcdc581638b472f38
Valentin Clement (バレンタイン クレメン) <clementval@gmail.com>
2025-10-16 07:01:11 -1000

    [flang][cuda] Move function result assignment to managed variable on host (#163705)
    - Update data transfer detection to let an assignment with a host rhs
    and managed lhs to be performed on the host. This helps if the rhs is a
    function result.
    - Fix test `cuda-maanaged-cuf` introduces on eef4b5a. The test was not
    checking for the implicit transfer but the explicit transfer that was
    part of the first loop.

--- flang/include/flang/Evaluate/tools.h
+++ flang/include/flang/Evaluate/tools.h
@@ -1342,10 +1342,12 @@ inline bool IsCUDADataTransfer(const A &lhs, const B &rhs) {
   int rhsNbManagedSymbols = {GetNbOfCUDAManagedOrUnifiedSymbols(rhs)};
   int rhsNbSymbols{GetNbOfCUDADeviceSymbols(rhs)};
 
-  // Special case where only managed or unifed symbols are involved. This is
-  // performed on the host.
-  if (lhsNbManagedSymbols == 1 && rhsNbManagedSymbols == 1 &&
-      rhsNbSymbols == 1) {
+  // Special cases perforemd on the host:
+  // - Only managed or unifed symbols are involved on RHS and LHS.
+  // - LHS is managed or unified and the RHS is host only.
+  if ((lhsNbManagedSymbols == 1 && rhsNbManagedSymbols == 1 &&
+          rhsNbSymbols == 1) ||
+      (lhsNbManagedSymbols == 1 && rhsNbSymbols == 0)) {
     return false;
   }
   return HasCUDADeviceAttrs(lhs) || rhsNbSymbols > 0;
