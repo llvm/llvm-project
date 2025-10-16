31b4b2f7d4fa18deb538e71b5941689324974a05
Asher Mancinelli <ashermancinelli@gmail.com>
2025-10-15 17:16:02 -0700

    Add uses of create/copyin/delete/copyout

--- mlir/test/python/dialects/openacc.py
+++ mlir/test/python/dialects/openacc.py
@@ -1,4 +1,5 @@
 # RUN: python %s | FileCheck %s
+from unittest import result
 from mlir.ir import (
     Context,
     FunctionType,
@@ -18,6 +19,7 @@ from mlir.ir import (
     ShapedType,
 )
 from mlir.dialects import openacc, func, arith, memref
+from mlir.extras import types
 
 
 def run(f):
@@ -28,18 +30,15 @@ def run(f):
 
 
 @run
-def testManualReconstructedKernel():
+def testParallelMemcpy():
     module = Module.create()
 
-    i32 = IntegerType.get_signless(32)
-    i64 = IntegerType.get_signless(64)
-    f32 = F32Type.get()
     dynamic = ShapedType.get_dynamic_size()
-    memref_f32_1d_any = MemRefType.get([dynamic], f32)
+    memref_f32_1d_any = MemRefType.get([dynamic], types.f32())
 
     with InsertionPoint(module.body):
         function_type = FunctionType.get(
-            [memref_f32_1d_any, memref_f32_1d_any, i64], []
+            [memref_f32_1d_any, memref_f32_1d_any, types.i64()], []
         )
         f = func.FuncOp(
             type=function_type,
@@ -48,8 +47,29 @@ def testManualReconstructedKernel():
         f.attributes["sym_visibility"] = StringAttr.get("public")
 
     with InsertionPoint(f.add_entry_block()):
-        c1024 = arith.ConstantOp(i32, 1024)
-        c128 = arith.ConstantOp(i32, 128)
+        c1024 = arith.ConstantOp(types.i32(), 1024)
+        c128 = arith.ConstantOp(types.i32(), 128)
+
+        arg0, arg1, arg2 = f.arguments
+
+        copied = openacc.copyin(
+            acc_var=arg0.type,
+            var=arg0,
+            var_type=types.f32(),
+            bounds=[],
+            async_operands=[],
+            implicit=False,
+            structured=True,
+        )
+        created = openacc.create_(
+            acc_var=arg1.type,
+            var=arg1,
+            var_type=types.f32(),
+            bounds=[],
+            async_operands=[],
+            implicit=False,
+            structured=True,
+        )
 
         parallel_op = openacc.ParallelOp(
             asyncOperands=[],
@@ -72,8 +92,8 @@ def testManualReconstructedKernel():
         parallel_block = Block.create_at_start(parent=parallel_op.region, arg_types=[])
 
         with InsertionPoint(parallel_block):
-            c0 = arith.ConstantOp(i64, 0)
-            c1 = arith.ConstantOp(i64, 1)
+            c0 = arith.ConstantOp(types.i64(), 0)
+            c1 = arith.ConstantOp(types.i64(), 1)
 
             loop_op = openacc.LoopOp(
                 results_=[],
@@ -95,34 +115,57 @@ def testManualReconstructedKernel():
             loop_op.gang = acc_device_none
             loop_op.independent = acc_device_none
 
-            loop_block = Block.create_at_start(parent=loop_op.region, arg_types=[i64])
+            loop_block = Block.create_at_start(
+                parent=loop_op.region, arg_types=[types.i64()]
+            )
 
             with InsertionPoint(loop_block):
                 idx = arith.index_cast(out=IndexType.get(), in_=loop_block.arguments[0])
-                val = memref.load(memref=f.arguments[1], indices=[idx])
-                memref.store(value=val, memref=f.arguments[0], indices=[idx])
+                val = memref.load(memref=copied, indices=[idx])
+                memref.store(value=val, memref=created, indices=[idx])
                 openacc.YieldOp([])
 
             openacc.YieldOp([])
 
+        deleted = openacc.delete(
+            acc_var=copied,
+            bounds=[],
+            async_operands=[],
+            implicit=False,
+            structured=True,
+        )
+        copied = openacc.copyout(
+            acc_var=created,
+            var=arg1,
+            var_type=types.f32(),
+            bounds=[],
+            async_operands=[],
+            implicit=False,
+            structured=True,
+        )
         func.ReturnOp([])
 
     print(module)
 
+    # CHECK: TEST: testParallelMemcpy
     # CHECK-LABEL:   func.func public @memcpy_idiom(
     # CHECK-SAME:      %[[ARG0:.*]]: memref<?xf32>, %[[ARG1:.*]]: memref<?xf32>, %[[ARG2:.*]]: i64) {
     # CHECK:           %[[CONSTANT_0:.*]] = arith.constant 1024 : i32
     # CHECK:           %[[CONSTANT_1:.*]] = arith.constant 128 : i32
+    # CHECK:           %[[COPYIN_0:.*]] = acc.copyin varPtr(%[[ARG0]] : memref<?xf32>) -> memref<?xf32>
+    # CHECK:           %[[CREATE_0:.*]] = acc.create varPtr(%[[ARG1]] : memref<?xf32>) -> memref<?xf32>
     # CHECK:           acc.parallel num_gangs({%[[CONSTANT_0]] : i32}) vector_length(%[[CONSTANT_1]] : i32) {
     # CHECK:             %[[CONSTANT_2:.*]] = arith.constant 0 : i64
     # CHECK:             %[[CONSTANT_3:.*]] = arith.constant 1 : i64
     # CHECK:             acc.loop gang control(%[[VAL_0:.*]] : i64) = (%[[CONSTANT_2]] : i64) to (%[[ARG2]] : i64)  step (%[[CONSTANT_3]] : i64) {
     # CHECK:               %[[INDEX_CAST_0:.*]] = arith.index_cast %[[VAL_0]] : i64 to index
-    # CHECK:               %[[LOAD_0:.*]] = memref.load %[[ARG1]]{{\[}}%[[INDEX_CAST_0]]] : memref<?xf32>
-    # CHECK:               memref.store %[[LOAD_0]], %[[ARG0]]{{\[}}%[[INDEX_CAST_0]]] : memref<?xf32>
+    # CHECK:               %[[LOAD_0:.*]] = memref.load %[[COPYIN_0]]{{\[}}%[[INDEX_CAST_0]]] : memref<?xf32>
+    # CHECK:               memref.store %[[LOAD_0]], %[[CREATE_0]]{{\[}}%[[INDEX_CAST_0]]] : memref<?xf32>
     # CHECK:               acc.yield
     # CHECK:             } attributes {independent = [#acc.device_type<none>]}
     # CHECK:             acc.yield
     # CHECK:           }
+    # CHECK:           acc.delete accPtr(%[[COPYIN_0]] : memref<?xf32>)
+    # CHECK:           acc.copyout accPtr(%[[CREATE_0]] : memref<?xf32>) to varPtr(%[[ARG1]] : memref<?xf32>)
     # CHECK:           return
     # CHECK:         }
