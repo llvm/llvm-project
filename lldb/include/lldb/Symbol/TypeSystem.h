ba7e7521be17b961401c3fa225d6a65515508e05
Ilia Kuklin <ikuklin@accesssoftek.com>
2025-10-17 21:56:59 +0500

    Add comments to integral promotion functions

--- lldb/include/lldb/Symbol/TypeSystem.h
+++ lldb/include/lldb/Symbol/TypeSystem.h
@@ -413,8 +413,13 @@ public:
 
   // DIL
 
+  /// Checks if the type is eligible for integral promotion.
   virtual bool IsPromotableIntegerType(lldb::opaque_compiler_type_t type);
 
+  /// Perform integral promotion on a given type.
+  /// This promotes eligible types (boolean, integers, unscoped enumerations)
+  /// to a larger integer type according to type system rules.
+  /// \returns Promoted type.
   virtual llvm::Expected<CompilerType>
   DoIntegralPromotion(CompilerType from, ExecutionContextScope *exe_scope);
 
