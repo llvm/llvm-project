52e5a38e3a226230a7894a3eee2f509f1c36efef
Benjamin Maxwell <benjamin.maxwell@arm.com>
2025-10-17 16:28:55 +0000

    Relax one use check for constants

--- llvm/lib/CodeGen/SelectionDAG/DAGCombiner.cpp
+++ llvm/lib/CodeGen/SelectionDAG/DAGCombiner.cpp
@@ -28017,8 +28017,9 @@ SDValue DAGCombiner::visitINSERT_SUBVECTOR(SDNode *N) {
   auto *BV0 = dyn_cast<BuildVectorSDNode>(N1);
   if (N0.isUndef() && (N1.getOpcode() == ISD::SPLAT_VECTOR || BV0)) {
     SDValue Splat = BV0 ? BV0->getSplatValue() : N1.getOperand(0);
-    if (Splat &&
-        (N1.hasOneUse() || (!BV0 && DAG.isConstantValueOfAnyType(Splat))))
+    bool SplatLegal = TLI.isOperationLegalOrCustom(ISD::SPLAT_VECTOR, VT);
+    if (Splat && (N1.hasOneUse() || (DAG.isConstantValueOfAnyType(Splat) &&
+                                     (!BV0 || SplatLegal))))
       return DAG.getNode(ISD::SPLAT_VECTOR, SDLoc(N), VT, N1.getOperand(0));
   }
 
