f2a115f1f5e1c9267c30860a2d7cab9354ddca51
qxy11 <qxy11@fb.com>
2025-10-17 06:28:40 -0700

    darker lint

--- lldb/test/API/tools/lldb-dap/attach/TestDAP_attach.py
+++ lldb/test/API/tools/lldb-dap/attach/TestDAP_attach.py
@@ -85,6 +85,4 @@ class TestDAP_attach(lldbdap_testcase.DAPTestCaseBase):
 
         resp = self.attach(targetId=99999, expectFailure=True)
         self.assertFalse(resp["success"])
-        self.assertIn(
-            "Unable to find existing target", resp["body"]["error"]["format"]
-        )
+        self.assertIn("Unable to find existing target", resp["body"]["error"]["format"])
