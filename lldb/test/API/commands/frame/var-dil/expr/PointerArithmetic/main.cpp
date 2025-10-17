15c8cfb172ccb7ebf5271a29e71b61dc386f4bc8
Ilia Kuklin <ikuklin@accesssoftek.com>
2025-10-17 22:00:47 +0500

    Break on a stop function in the test

--- lldb/test/API/commands/frame/var-dil/expr/PointerArithmetic/main.cpp
+++ lldb/test/API/commands/frame/var-dil/expr/PointerArithmetic/main.cpp
@@ -1,8 +1,11 @@
+void stop() {}
+
 int main(int argc, char **argv) {
   int array[10];
   array[0] = 0;
   int (&array_ref)[10] = array;
   int *p_int0 = &array[0];
 
-  return 0; // Set a breakpoint here
+  stop(); // Set a breakpoint here
+  return 0;
 }
