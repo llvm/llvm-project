7b1c4a03ce0b7d6d08e3ba43d59c6ac80f501558
c8ef <c8ef@outlook.com>
2025-10-17 22:56:19 +0800

    revert test

--- libcxx/test/std/algorithms/alg.modifying.operations/alg.generate/generate.pass.cpp
+++ libcxx/test/std/algorithms/alg.modifying.operations/alg.generate/generate.pass.cpp
@@ -21,29 +21,35 @@
 #include "test_macros.h"
 #include "test_iterators.h"
 
-struct gen_test {
-  TEST_CONSTEXPR int operator()() const { return 1; }
+struct gen_test
+{
+    TEST_CONSTEXPR int operator()() const {return 1;}
 };
+
 
 #if TEST_STD_VER > 17
 TEST_CONSTEXPR bool test_constexpr() {
-  int ia[] = {0, 1, 2, 3, 4};
+    int ia[] = {0, 1, 2, 3, 4};
 
-  std::generate(std::begin(ia), std::end(ia), gen_test());
+    std::generate(std::begin(ia), std::end(ia), gen_test());
 
-  return std::all_of(std::begin(ia), std::end(ia), [](int x) { return x == 1; });
-}
+    return std::all_of(std::begin(ia), std::end(ia), [](int x) { return x == 1; })
+        ;
+    }
 #endif
 
+
 template <class Iter>
-void test() {
-  const unsigned n = 4;
-  int ia[n]        = {0};
-  std::generate(Iter(ia), Iter(ia + n), gen_test());
-  assert(ia[0] == 1);
-  assert(ia[1] == 1);
-  assert(ia[2] == 1);
-  assert(ia[3] == 1);
+void
+test()
+{
+    const unsigned n = 4;
+    int ia[n] = {0};
+    std::generate(Iter(ia), Iter(ia+n), gen_test());
+    assert(ia[0] == 1);
+    assert(ia[1] == 1);
+    assert(ia[2] == 1);
+    assert(ia[3] == 1);
 }
 
 void deque_test() {
@@ -55,15 +61,16 @@ void deque_test() {
   }
 }
 
-int main(int, char**) {
-  test<forward_iterator<int*> >();
-  test<bidirectional_iterator<int*> >();
-  test<random_access_iterator<int*> >();
-  test<int*>();
-  deque_test();
+int main(int, char**)
+{
+    test<forward_iterator<int*> >();
+    test<bidirectional_iterator<int*> >();
+    test<random_access_iterator<int*> >();
+    test<int*>();
+    deque_test();
 
 #if TEST_STD_VER > 17
-  static_assert(test_constexpr());
+    static_assert(test_constexpr());
 #endif
 
   return 0;
