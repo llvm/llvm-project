80cb7e4bfd39f4fe75c663c74f7b4a252cffbb2c
Carl Ritson <carl.ritson@amd.com>
2025-10-17 14:28:51 +0900

    - Reorder isUse() escape for PHI nodes until confirmed register

--- llvm/lib/Target/AMDGPU/GCNRegPressure.cpp
+++ llvm/lib/Target/AMDGPU/GCNRegPressure.cpp
@@ -611,10 +611,10 @@ bool GCNDownwardRPTracker::advanceBeforeNext(MachineInstr *MI,
   // Remove dead registers or mask bits.
   SmallSet<Register, 8> SeenRegs;
   for (auto &MO : CurrMI->operands()) {
-    if (MO.isUse() && CurrMI->getOpcode() == AMDGPU::PHI)
-      break;
     if (!MO.isReg() || !MO.getReg().isVirtual())
       continue;
+    if (MO.isUse() && CurrMI->getOpcode() == AMDGPU::PHI)
+      break;
     if (MO.isUse() && !MO.readsReg())
       continue;
     if (!UseInternalIterator && MO.isDef())
