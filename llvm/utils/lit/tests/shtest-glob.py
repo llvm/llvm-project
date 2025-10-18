10db6920d8bbc73d916add4b3556e9f5f070c8bf
kikairoya <kikairoya@gmail.com>
2025-10-18 19:35:16 +0900

    fix test

--- llvm/utils/lit/tests/shtest-glob.py
+++ llvm/utils/lit/tests/shtest-glob.py
@@ -8,6 +8,6 @@
 # CHECK: TypeError: string argument expected, got 'GlobItem'
 
 # CHECK:      FAIL: shtest-glob :: glob-mkdir.txt ({{[^)]*}})
-# CHECK:      # | Error: 'mkdir' command failed, {{.*}}Inputs/shtest-glob/example_file1.input'
-# CHECK-NEXT: # | Error: 'mkdir' command failed, {{.*}}Inputs/shtest-glob/example_file2.input'
+# CHECK:      # | Error: 'mkdir' command failed, {{.*}}example_file1.input'
+# CHECK-NEXT: # | Error: 'mkdir' command failed, {{.*}}example_file2.input'
 # CHECK:      # error: command failed with exit status: 1
