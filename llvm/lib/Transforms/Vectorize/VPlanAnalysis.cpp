2a648bce75767680e87c0cf317f59b9dbe951d8b
Florian Hahn <flo@fhahn.com>
2025-10-18 21:25:33 +0100

    [VPlan] Add VPRecipeBase::getRegion helper (NFC).
    Multiple places retrieve the region for a recipe. Add a helper to make
    the code more compact and clearer.

--- llvm/lib/Transforms/Vectorize/VPlanAnalysis.cpp
+++ llvm/lib/Transforms/Vectorize/VPlanAnalysis.cpp
@@ -377,7 +377,7 @@ bool VPDominatorTree::properlyDominates(const VPRecipeBase *A,
 
 #ifndef NDEBUG
   auto GetReplicateRegion = [](VPRecipeBase *R) -> VPRegionBlock * {
-    auto *Region = dyn_cast_or_null<VPRegionBlock>(R->getParent()->getParent());
+    VPRegionBlock *Region = R->getRegion();
     if (Region && Region->isReplicator()) {
       assert(Region->getNumSuccessors() == 1 &&
              Region->getNumPredecessors() == 1 && "Expected SESE region!");
