e0e9f9f5d07cbcdfcb640ba8da0d7207b6af20b2
Vinay Deshmukh <vinay_deshmukh@outlook.com>
2025-10-19 07:02:17 -0400

    remove the second //

--- libcxx/test/std/containers/associative/map/map.ops/equal_range_transparent.pass.cpp
+++ libcxx/test/std/containers/associative/map/map.ops/equal_range_transparent.pass.cpp
@@ -13,10 +13,10 @@
 // class map
 
 // template<typename K>
-//         pair<iterator,iterator>             equal_range(const K& x); // C++14 // constexpr since C++26
+//         pair<iterator,iterator>             equal_range(const K& x); // C++14, constexpr since C++26
 // template<typename K>
 //         pair<const_iterator,const_iterator> equal_range(const K& x) const;
-//         // C++14 // constexpr since C++26
+//         // C++14, constexpr since C++26
 
 #include <cassert>
 #include <map>
