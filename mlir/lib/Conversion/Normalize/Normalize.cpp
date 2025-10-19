57c0292375d4dd8793f50d8d28d5daee369ce8a1
Sh0g0-1758 <shouryagoel10000@gmail.com>
2025-10-20 01:50:07 +0530

    camelCase

--- mlir/lib/Conversion/Normalize/Normalize.cpp
+++ mlir/lib/Conversion/Normalize/Normalize.cpp
@@ -42,15 +42,15 @@ struct NormalizePass : public impl::NormalizeBase<NormalizePass> {
 
 private:
   // Random constant for hashing, so the state isn't zero.
-  const uint64_t MagicHashConstant = 0x6acaa36bef8325c5ULL;
+  const uint64_t magicHashConstant = 0x6acaa36bef8325c5ULL;
   void
   collectOutputOperations(Block &block,
-                          SmallVector<Operation *, 16> &Output) const noexcept;
+                          SmallVector<Operation *, 16> &outputs) const noexcept;
   bool isOutput(Operation &op) const noexcept;
-  void reorderOperations(const SmallVector<Operation *, 16> &Outputs);
+  void reorderOperations(const SmallVector<Operation *, 16> &outputs);
   void reorderOperation(Operation *used, Operation *user,
                         llvm::SmallPtrSet<const Operation *, 32> &visited);
-  void renameOperations(const SmallVector<Operation *, 16> &Outputs);
+  void renameOperations(const SmallVector<Operation *, 16> &outputs);
   void renameOperation(Operation *op,
                        SmallPtrSet<const Operation *, 32> &visited);
   bool isInitialOperation(Operation *const op) const noexcept;
@@ -64,7 +64,7 @@ private:
   llvm::SetVector<int>
   getOutputFootprint(Operation *op,
                      llvm::SmallPtrSet<const Operation *, 32> &visited) const;
-  void appendRenamedOperands(Operation *op, SmallString<512> &opName);
+  void appendRenamedOperands(Operation *op, SmallString<512> &name);
   void reorderOperationOperandsByName(Operation *op);
   OpPrintingFlags flags{};
 };
@@ -77,22 +77,22 @@ void NormalizePass::runOnOperation() {
   ModuleOp module = getOperation();
 
   for (auto &op : module.getOps()) {
-    SmallVector<Operation *, 16> Outputs;
+    SmallVector<Operation *, 16> outputs;
 
     for (auto &region : op.getRegions())
       for (auto &block : region)
-        collectOutputOperations(block, Outputs);
+        collectOutputOperations(block, outputs);
 
-    reorderOperations(Outputs);
-    renameOperations(Outputs);
+    reorderOperations(outputs);
+    renameOperations(outputs);
   }
 }
 
 void NormalizePass::renameOperations(
-    const SmallVector<Operation *, 16> &Outputs) {
+    const SmallVector<Operation *, 16> &outputs) {
   llvm::SmallPtrSet<const Operation *, 32> visited;
 
-  for (auto *op : Outputs)
+  for (auto *op : outputs)
     renameOperation(op, visited);
 }
 
@@ -131,7 +131,7 @@ bool NormalizePass::hasOnlyImmediateOperands(
   return true;
 }
 
-std::string inline to_string(uint64_t const hash) noexcept {
+std::string inline toString(uint64_t const hash) noexcept {
   std::ostringstream oss;
   oss << std::hex << std::setw(5) << std::setfill('0') << hash;
   std::string tmp = oss.str();
@@ -181,48 +181,48 @@ void NormalizePass::nameAsInitialOperation(
     if (Operation *defOp = operand.getDefiningOp())
       renameOperation(defOp, visited);
 
-  uint64_t Hash = MagicHashConstant;
+  uint64_t hash = magicHashConstant;
 
   uint64_t opcodeHash = strHash(op->getName().getStringRef().str());
-  Hash = llvm::hashing::detail::hash_16_bytes(Hash, opcodeHash);
+  hash = llvm::hashing::detail::hash_16_bytes(hash, opcodeHash);
 
-  SmallPtrSet<const Operation *, 32> Visited;
-  SetVector<int> OutputFootprint = getOutputFootprint(op, Visited);
+  SmallPtrSet<const Operation *, 32> visitedLocal;
+  SetVector<int> outputFootprint = getOutputFootprint(op, visitedLocal);
 
-  for (const auto &Output : OutputFootprint)
-    Hash = llvm::hashing::detail::hash_16_bytes(Hash, Output);
+  for (const auto &output : outputFootprint)
+    hash = llvm::hashing::detail::hash_16_bytes(hash, output);
 
-  SmallString<512> Name;
-  Name.append("vl" + std::to_string(Hash).substr(0, 5));
+  SmallString<512> name;
+  name.append("vl" + std::to_string(hash).substr(0, 5));
 
   if (auto call = dyn_cast<func::CallOp>(op)) {
     llvm::StringRef callee = call.getCallee();
-    Name.append(callee.str());
+    name.append(callee.str());
   }
 
   if (op->getNumOperands() == 0) {
-    Name.append("$");
+    name.append("$");
     if (auto call = dyn_cast<func::CallOp>(op)) {
-      Name.append("void");
+      name.append("void");
     } else {
-      std::string TextRepresentation;
+      std::string textRepresentation;
       AsmState state(op, flags);
-      llvm::raw_string_ostream Stream(TextRepresentation);
-      op->print(Stream, state);
-      std::string hash = to_string(strHash(split(Stream.str(), '=', 1)));
-      Name.append(hash);
+      llvm::raw_string_ostream stream(textRepresentation);
+      op->print(stream, state);
+      std::string hashStr = toString(strHash(split(stream.str(), '=', 1)));
+      name.append(hashStr);
     }
-    Name.append("$");
+    name.append("$");
 
     OpBuilder b(op->getContext());
-    StringAttr sat = b.getStringAttr(Name);
+    StringAttr sat = b.getStringAttr(name);
     Location newLoc = NameLoc::get(sat, op->getLoc());
     op->setLoc(newLoc);
 
     return;
   }
 
-  appendRenamedOperands(op, Name);
+  appendRenamedOperands(op, name);
 }
 
 /// Names operation following the scheme:
@@ -239,36 +239,35 @@ void NormalizePass::nameAsRegularOperation(
     if (Operation *defOp = operand.getDefiningOp())
       renameOperation(defOp, visited);
 
-  uint64_t Hash = MagicHashConstant;
+  uint64_t hash = magicHashConstant;
 
   uint64_t opcodeHash = strHash(op->getName().getStringRef().str());
-  Hash = llvm::hashing::detail::hash_16_bytes(Hash, opcodeHash);
+  hash = llvm::hashing::detail::hash_16_bytes(hash, opcodeHash);
 
-  SmallVector<uint64_t, 4> OperandsOpcodes;
+  SmallVector<uint64_t, 4> operandOpcodes;
 
   for (Value operand : op->getOperands())
     if (Operation *defOp = operand.getDefiningOp())
-      OperandsOpcodes.push_back(strHash(defOp->getName().getStringRef().str()));
+      operandOpcodes.push_back(strHash(defOp->getName().getStringRef().str()));
 
   if (op->hasTrait<OpTrait::IsCommutative>())
-    llvm::sort(OperandsOpcodes.begin(), OperandsOpcodes.end());
+    llvm::sort(operandOpcodes.begin(), operandOpcodes.end());
 
-  for (const uint64_t Code : OperandsOpcodes)
-    Hash = llvm::hashing::detail::hash_16_bytes(Hash, Code);
+  for (const uint64_t code : operandOpcodes)
+    hash = llvm::hashing::detail::hash_16_bytes(hash, code);
 
-  SmallString<512> Name;
-  Name.append("op" + std::to_string(Hash).substr(0, 5));
+  SmallString<512> name;
+  name.append("op" + std::to_string(hash).substr(0, 5));
 
   if (auto call = dyn_cast<func::CallOp>(op)) {
     llvm::StringRef callee = call.getCallee();
-    Name.append(callee.str());
+    name.append(callee.str());
   }
 
-  appendRenamedOperands(op, Name);
+  appendRenamedOperands(op, name);
 }
 
-bool inline starts_with(std::string_view base,
-                        std::string_view check) noexcept {
+bool inline startsWith(std::string_view base, std::string_view check) noexcept {
   return base.size() >= check.size() &&
          std::equal(check.begin(), check.end(), base.begin());
 }
@@ -280,58 +279,58 @@ bool inline starts_with(std::string_view base,
 /// first 7 characters of the renamed operand. The operand could also have been
 /// a block/function argument which is handled separately.
 void NormalizePass::appendRenamedOperands(Operation *op,
-                                          SmallString<512> &Name) {
+                                          SmallString<512> &name) {
   if (op->getNumOperands() == 0)
     return;
 
-  SmallVector<std::string, 4> Operands;
+  SmallVector<std::string, 4> operands;
 
   for (Value operand : op->getOperands()) {
     if (Operation *defOp = operand.getDefiningOp()) {
-      std::string TextRepresentation;
+      std::string textRepresentation;
       AsmState state(defOp, flags);
-      llvm::raw_string_ostream Stream(TextRepresentation);
-      defOp->print(Stream, state);
-      auto name = split(Stream.str(), '=', 0);
+      llvm::raw_string_ostream stream(textRepresentation);
+      defOp->print(stream, state);
+      auto operandName = split(stream.str(), '=', 0);
 
       bool hasNormalName =
-          (starts_with(name, "%op") || starts_with(name, "%vl"));
+          (startsWith(operandName, "%op") || startsWith(operandName, "%vl"));
 
       if (hasNormalName) {
-        Operands.push_back(name.substr(1, 7));
+        operands.push_back(operandName.substr(1, 7));
       } else {
-        Operands.push_back(name);
+        operands.push_back(operandName);
       }
     } else if (auto ba = dyn_cast<BlockArgument>(operand)) {
       Block *ownerBlock = ba.getOwner();
       unsigned argIndex = ba.getArgNumber();
       if (auto func = dyn_cast<func::FuncOp>(ownerBlock->getParentOp())) {
         if (&func.front() == ownerBlock) {
-          Operands.push_back(std::string("funcArg" + std::to_string(argIndex)));
+          operands.push_back(std::string("funcArg" + std::to_string(argIndex)));
         } else {
-          Operands.push_back(
+          operands.push_back(
               std::string("blockArg" + std::to_string(argIndex)));
         }
       } else {
-        Operands.push_back(std::string("blockArg" + std::to_string(argIndex)));
+        operands.push_back(std::string("blockArg" + std::to_string(argIndex)));
       }
     }
   }
 
   if (op->hasTrait<OpTrait::IsCommutative>())
-    llvm::sort(Operands.begin(), Operands.end());
+    llvm::sort(operands.begin(), operands.end());
 
-  Name.append("$");
-  for (size_t i = 0, size_ = Operands.size(); i < size_; ++i) {
-    Name.append(Operands[i]);
+  name.append("$");
+  for (size_t i = 0, size_ = operands.size(); i < size_; ++i) {
+    name.append(operands[i]);
 
     if (i < size_ - 1)
-      Name.append("-");
+      name.append("-");
   }
-  Name.append("$");
+  name.append("$");
 
   OpBuilder b(op->getContext());
-  Location newLoc = NameLoc::get(b.getStringAttr(Name), op->getLoc());
+  Location newLoc = NameLoc::get(b.getStringAttr(name), op->getLoc());
   op->setLoc(newLoc);
 }
 
@@ -341,33 +340,33 @@ void NormalizePass::reorderOperationOperandsByName(Operation *op) {
   if (op->getNumOperands() == 0)
     return;
 
-  SmallVector<std::pair<std::string, Value>, 4> Operands;
+  SmallVector<std::pair<std::string, Value>, 4> operands;
 
   for (Value operand : op->getOperands()) {
-    std::string TextRepresentation;
-    llvm::raw_string_ostream Stream(TextRepresentation);
-    operand.printAsOperand(Stream, flags);
-    Operands.push_back({Stream.str(), operand});
+    std::string textRepresentation;
+    llvm::raw_string_ostream stream(textRepresentation);
+    operand.printAsOperand(stream, flags);
+    operands.push_back({stream.str(), operand});
   }
 
   if (op->hasTrait<OpTrait::IsCommutative>()) {
     llvm::sort(
-        Operands.begin(), Operands.end(), [](const auto &a, const auto &b) {
+        operands.begin(), operands.end(), [](const auto &a, const auto &b) {
           return llvm::StringRef(a.first).compare_insensitive(b.first) < 0;
         });
   }
 
-  for (size_t i = 0, size_ = Operands.size(); i < size_; i++) {
-    op->setOperand(i, Operands[i].second);
+  for (size_t i = 0, size_ = operands.size(); i < size_; i++) {
+    op->setOperand(i, operands[i].second);
   }
 }
 
 /// Reorders operations by walking up the tree from each operand of an output
 /// operation and reducing the def-use distance.
 void NormalizePass::reorderOperations(
-    const SmallVector<Operation *, 16> &Outputs) {
+    const SmallVector<Operation *, 16> &outputs) {
   llvm::SmallPtrSet<const Operation *, 32> visited;
-  for (auto *const op : Outputs)
+  for (auto *const op : outputs)
     for (Value operand : op->getOperands())
       if (Operation *defOp = operand.getDefiningOp())
         reorderOperation(defOp, op, visited);
@@ -394,17 +393,16 @@ void NormalizePass::reorderOperation(
 }
 
 void NormalizePass::collectOutputOperations(
-    Block &block, SmallVector<Operation *, 16> &Outputs) const noexcept {
+    Block &block, SmallVector<Operation *, 16> &outputs) const noexcept {
   for (auto &innerOp : block)
     if (isOutput(innerOp))
-      Outputs.emplace_back(&innerOp);
+      outputs.emplace_back(&innerOp);
 }
 
 /// The following Operations are termed as output:
 ///  - Terminator operations are outputs
 ///  - Any operation that implements MemoryEffectOpInterface and reports at
-///  least
-///    one MemoryEffects::Write effect is an output
+///    least one MemoryEffects::Write effect is an output
 ///  - func::CallOp is treated as an output (calls are conservatively assumed to
 ///    possibly produce side effects).
 bool NormalizePass::isOutput(Operation &op) const noexcept {
@@ -430,34 +428,34 @@ bool NormalizePass::isOutput(Operation &op) const noexcept {
 /// def-use tree recursively
 llvm::SetVector<int> NormalizePass::getOutputFootprint(
     Operation *op, llvm::SmallPtrSet<const Operation *, 32> &visited) const {
-  llvm::SetVector<int> Outputs;
+  llvm::SetVector<int> outputsVec;
   if (!visited.count(op)) {
     visited.insert(op);
 
     if (isOutput(*op)) {
       func::FuncOp func = op->getParentOfType<func::FuncOp>();
 
-      unsigned Count = 0;
+      unsigned count = 0;
       for (Block &block : func.getRegion())
         for (Operation &innerOp : block) {
           if (&innerOp == op)
-            Outputs.insert(Count);
-          Count++;
+            outputsVec.insert(count);
+          count++;
         }
 
-      return Outputs;
+      return outputsVec;
     }
 
     for (OpOperand &use : op->getUses()) {
       Operation *useOp = use.getOwner();
       if (useOp) {
-        llvm::SetVector<int> OutputsUsingUop =
+        llvm::SetVector<int> outputsUsingUop =
             getOutputFootprint(useOp, visited);
 
-        Outputs.insert(OutputsUsingUop.begin(), OutputsUsingUop.end());
+        outputsVec.insert(outputsUsingUop.begin(), outputsUsingUop.end());
       }
     }
   }
 
-  return Outputs;
+  return outputsVec;
 }
