1ca3d0f118e7f8537bcce4b4cc106c187620c371
Vinay Deshmukh <vinay_deshmukh@outlook.com>
2025-10-19 05:54:45 -0400

    try again

--- libc/test/src/sys/socket/linux/sendto_recvfrom_test.cpp
+++ libc/test/src/sys/socket/linux/sendto_recvfrom_test.cpp
@@ -53,8 +53,8 @@ TEST_F(LlvmLibcSendToRecvFromTest, SendToFails) {
   const size_t MESSAGE_LEN = sizeof(TEST_MESSAGE);
 
   ASSERT_THAT(
-      LIBC_NAMESPACE::sendto(ssize_t(-1), TEST_MESSAGE, MESSAGE_LEN, 0, nullptr, 0),
-      Fails(ssize_t(EBADF)));
+      LIBC_NAMESPACE::sendto(-1, TEST_MESSAGE, MESSAGE_LEN, 0, nullptr, 0),
+      Fails(ssize_t(EBADF), ssize_t(-1)));
 }
 
 TEST_F(LlvmLibcSendToRecvFromTest, RecvFromFails) {
