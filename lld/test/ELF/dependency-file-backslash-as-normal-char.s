b59f1f8da99567ef21fc51777b2e7af1ecbbbd5c
Ruoyu Zhong <zhongruoyu@outlook.com>
2025-10-20 14:02:25 +0800

    [lld][ELF] Check indentation in dependency file tests

--- lld/test/ELF/dependency-file-backslash-as-normal-char.s
+++ lld/test/ELF/dependency-file-backslash-as-normal-char.s
@@ -2,12 +2,12 @@
 # RUN: mkdir -p %t
 # RUN: llvm-mc -filetype=obj -triple=x86_64 %s -o "%t/back\\slash.o"
 # RUN: ld.lld -o %t/foo.exe "%t/back\\slash.o" --dependency-file=%t/foo.d
-# RUN: FileCheck --match-full-lines -DFILE=%t %s < %t/foo.d
+# RUN: FileCheck --match-full-lines --strict-whitespace -DFILE=%t %s < %t/foo.d
 
-# CHECK:      [[FILE]]/foo.exe: \
-# CHECK-NEXT:   [[FILE]]/back\slash.o
+#       CHECK:[[FILE]]/foo.exe: \
+#  CHECK-NEXT:  [[FILE]]/back\slash.o
 # CHECK-EMPTY:
-# CHECK-NEXT: [[FILE]]/back\slash.o:
+#  CHECK-NEXT:[[FILE]]/back\slash.o:
 
 .global _start
 _start:
