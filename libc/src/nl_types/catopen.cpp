2b965969f94c3191be24088ad97ddabfd815c977
Alexey Samsonov <vonosmas@gmail.com>
2025-10-21 05:20:00 +0000

    add implementation for catclose and catgets

--- libc/src/nl_types/catopen.cpp
+++ libc/src/nl_types/catopen.cpp
@@ -8,15 +8,17 @@
 
 #include "src/nl_types/catopen.h"
 #include "include/llvm-libc-types/nl_catd.h"
+#include "src/__support/common.h"
 #include "src/__support/libc_errno.h"
 #include "src/__support/macros/config.h"
 
 namespace LIBC_NAMESPACE_DECL {
 
-LLVM_LIBC_FUNCTION(nl_catd, catopen, (const char *name, int flag)) {
-  // Catalog descriptors are not implemented. Return error regardless of input.
+LLVM_LIBC_FUNCTION(nl_catd, catopen, ([[maybe_unused]] const char *name,
+			              [[maybe_unused]] int flag)) {
+  // Message catalogs are not implemented. Return error regardless of input.
   libc_errno = EINVAL;
-  return static_cast<nl_catd>(-1);
+  return reinterpret_cast<nl_catd>(-1);
 }
 
 } // namespace LIBC_NAMESPACE_DECL
