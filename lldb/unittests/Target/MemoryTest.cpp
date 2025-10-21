52aae17338cd97d094cb7885b0ba92dc102737ee
Felipe de Azevedo Piovezan <fpiovezan@apple.com>
2025-10-21 07:41:48 -0700

    fixup! Add test for reading fewer bytes than requested

--- lldb/unittests/Target/MemoryTest.cpp
+++ lldb/unittests/Target/MemoryTest.cpp
@@ -231,8 +231,16 @@ TEST_F(MemoryTest, TesetMemoryCacheRead) {
 /// the least significant byte of X.
 class DummyReaderProcess : public Process {
 public:
+  // If true, `DoReadMemory` will not return all requested bytes.
+  // It's not possible to control exactly how many bytes will be read, because
+  // Process::ReadMemoryFromInferior tries to fulfill the entire request by
+  // reading smaller chunks until it gets nothing back.
+  bool read_less_than_requested = false;
+
   size_t DoReadMemory(lldb::addr_t vm_addr, void *buf, size_t size,
                       Status &error) override {
+    if (read_less_than_requested && size > 0)
+      size--;
     uint8_t *buffer = static_cast<uint8_t *>(buf);
     for (size_t addr = vm_addr; addr < vm_addr + size; addr++)
       buffer[addr - vm_addr] = static_cast<uint8_t>(addr); // LSB of addr.
@@ -264,21 +272,39 @@ TEST_F(MemoryTest, TestReadMemoryRanges) {
       std::make_shared<DummyReaderProcess>(target_sp, listener_sp);
   ASSERT_TRUE(process_sp);
 
-  llvm::SmallVector<uint8_t, 0> buffer(1024, 0);
 
-  // Read 8 ranges of 128 bytes with arbitrary base addresses.
-  llvm::SmallVector<Range<addr_t, size_t>> ranges = {
-      {0x12345, 128},      {0x11112222, 128}, {0x77777777, 128},
-      {0xffaabbccdd, 128}, {0x0, 128},        {0x4242424242, 128},
-      {0x17171717, 128},   {0x99999, 128}};
+  {
+    llvm::SmallVector<uint8_t, 0> buffer(1024, 0);
+    // Read 8 ranges of 128 bytes with arbitrary base addresses.
+    llvm::SmallVector<Range<addr_t, size_t>> ranges = {
+        {0x12345, 128},      {0x11112222, 128}, {0x77777777, 128},
+        {0xffaabbccdd, 128}, {0x0, 128},        {0x4242424242, 128},
+        {0x17171717, 128},   {0x99999, 128}};
 
-  llvm::SmallVector<llvm::MutableArrayRef<uint8_t>> read_results =
-      process_sp->ReadMemoryRanges(ranges, buffer);
+    llvm::SmallVector<llvm::MutableArrayRef<uint8_t>> read_results =
+        process_sp->ReadMemoryRanges(ranges, buffer);
 
-  for (auto [range, memory] : llvm::zip(ranges, read_results)) {
-    ASSERT_EQ(memory.size(), 128u);
-    addr_t range_base = range.GetRangeBase();
-    for (auto [idx, byte] : llvm::enumerate(memory))
-      ASSERT_EQ(byte, static_cast<uint8_t>(range_base + idx));
+    for (auto [range, memory] : llvm::zip(ranges, read_results)) {
+      ASSERT_EQ(memory.size(), 128u);
+      addr_t range_base = range.GetRangeBase();
+      for (auto [idx, byte] : llvm::enumerate(memory))
+        ASSERT_EQ(byte, static_cast<uint8_t>(range_base + idx));
+    }
+  }
+
+  auto &dummy_process = static_cast<DummyReaderProcess&>(*process_sp);
+  dummy_process.read_less_than_requested = true;
+  {
+    llvm::SmallVector<uint8_t, 0> buffer(1024, 0);
+    llvm::SmallVector<Range<addr_t, size_t>> ranges = {
+        {0x12345, 128}, {0x11112222, 128}, {0x77777777, 128}};
+    llvm::SmallVector<llvm::MutableArrayRef<uint8_t>> read_results =
+        dummy_process.ReadMemoryRanges(ranges, buffer);
+    for (auto [range, memory] : llvm::zip(ranges, read_results)) {
+      ASSERT_LT(memory.size(), 128u);
+      addr_t range_base = range.GetRangeBase();
+      for (auto [idx, byte] : llvm::enumerate(memory))
+        ASSERT_EQ(byte, static_cast<uint8_t>(range_base + idx));
+    }
   }
 }
