a616fdf8d1809599c2469670c41e5fb6eb55021e
c8ef <c8ef@outlook.com>
2025-10-21 23:00:32 +0800

    Remove undef macros

--- libcxx/include/__algorithm/for_each.h
+++ libcxx/include/__algorithm/for_each.h
@@ -21,9 +21,6 @@
 #  pragma GCC system_header
 #endif
 
-_LIBCPP_PUSH_MACROS
-#include <__undef_macros>
-
 _LIBCPP_BEGIN_NAMESPACE_STD
 
 template <class _InputIterator, class _Sent, class _Func, class _Proj>
@@ -58,7 +55,5 @@ for_each(_InputIterator __first, _InputIterator __last, _Func __f) {
 }
 
 _LIBCPP_END_NAMESPACE_STD
-
-_LIBCPP_POP_MACROS
 
 #endif // _LIBCPP___ALGORITHM_FOR_EACH_H
