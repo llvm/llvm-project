c9b795cdbb94a18e50a64fd04eba2e1c9ff259b2
Jonathan Thackray <jonathan.thackray@arm.com>
2025-10-22 21:34:03 +0100

    fixup! [AArch64][llvm] Armv9.7-A: Add support for SVE2p3 DOT and MLA operations
    Add diagnostic for test following predicated movprfx

--- llvm/test/MC/AArch64/SVE2p2/fmmla-diagnostics.s
+++ llvm/test/MC/AArch64/SVE2p2/fmmla-diagnostics.s
@@ -7,3 +7,9 @@ fmmla z0.b, z0.b, z0.b
 // CHECK: [[@LINE-1]]:{{[0-9]+}}: error: invalid element width
 // CHECK-NEXT: fmmla z0.b, z0.b, z0.b
 // CHECK-NOT: [[@LINE-1]]:{{[0-9]+}}:
+
+movprfx z0.d, p0/z, z6.d
+fmmla z0.h, z2.h, z3.h
+// CHECK: [[@LINE-1]]:{{[0-9]+}}: error: instruction is unpredictable when following a predicated movprfx, suggest using unpredicated movprfx
+// CHECK-NEXT: fmmla z0.h, z2.h, z3.h
+// CHECK-NOT: [[@LINE-1]]:{{[0-9]+}}:
