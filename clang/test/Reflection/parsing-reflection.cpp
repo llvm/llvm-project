442feeb451f3cea5047a6d200343306a156c00c3
changkhothuychung <nhat7203@gmail.com>
2025-10-23 15:53:27 -0400

    add more test cases and rename test file

---
+++ clang/test/Reflection/parsing-reflection.cpp
@@ -0,0 +1,40 @@
+// RUN: %clang_cc1 %s -std=c++26 -freflection -fsyntax-only -verify
+
+namespace a {
+struct T {};
+namespace b {
+struct U {};
+int x;
+}
+}
+
+
+int main()
+{
+    (void)(^^::);
+    (void)(^^void);
+    (void)(^^bool);
+    (void)(^^char);
+    (void)(^^signed char);
+    (void)(^^unsigned char);
+    (void)(^^short);
+    (void)(^^unsigned short);
+    (void)(^^int);
+    (void)(^^unsigned int);
+    (void)(^^long);
+    (void)(^^unsigned long);
+    (void)(^^long long);
+    (void)(^^float);
+    (void)(^^double);
+
+    // Not supported yet. 
+    (void)^^a; // expected-error {{expected reflectable entity}}
+    (void)^^a::; // expected-error {{expected reflectable entity}}
+    (void)^^a::b::T; // expected-error {{expected reflectable entity}}
+    (void)^^a::T::; // expected-error {{expected reflectable entity}}
+    (void)(^^a::b); // expected-error {{expected reflectable entity}}
+    (void)^^a::b::; // expected-error {{expected reflectable entity}}
+    (void)^^a::b::U; // expected-error {{parsing of this type-id is not implemented yet}}
+    (void)^^a::b::x; // expected-error {{expected reflectable entity}}
+    (void)^^a::b::U::; // expected-error {{expected reflectable entity}}
+}
