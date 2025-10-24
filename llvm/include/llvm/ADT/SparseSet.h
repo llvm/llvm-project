5db89f0fbe72febd96a03802a1c14a6c7f871cba
Kazu Hirata <kazu@google.com>
2025-10-24 15:30:35 -0700

    [ADT] Consolidate SparseSetValFunctor implementations (NFC) (#164926)
    This patch consolidates the two implementations of SparseSetValFunctor
    with "if constexpr".  std::is_same_v<KeyT, ValueT> is more readable
    than "KeyT, KeyT" in the template parameter list.

--- llvm/include/llvm/ADT/SparseSet.h
+++ llvm/include/llvm/ADT/SparseSet.h
@@ -59,22 +59,18 @@ template <typename ValueT> struct SparseSetValTraits {
   }
 };
 
-/// SparseSetValFunctor - Helper class for selecting SparseSetValTraits. The
-/// generic implementation handles ValueT classes which either provide
-/// getSparseSetIndex() or specialize SparseSetValTraits<>.
+/// SparseSetValFunctor - Helper class for getting a value's index.
 ///
+/// In the generic case, this is done via SparseSetValTraits. When the value
+/// type is the same as the key type, the KeyFunctor is used directly.
 template <typename KeyT, typename ValueT, typename KeyFunctorT>
 struct SparseSetValFunctor {
   unsigned operator()(const ValueT &Val) const {
-    return SparseSetValTraits<ValueT>::getValIndex(Val);
+    if constexpr (std::is_same_v<KeyT, ValueT>)
+      return KeyFunctorT()(Val);
+    else
+      return SparseSetValTraits<ValueT>::getValIndex(Val);
   }
-};
-
-/// SparseSetValFunctor<KeyT, KeyT> - Helper class for the common case of
-/// identity key/value sets.
-template <typename KeyT, typename KeyFunctorT>
-struct SparseSetValFunctor<KeyT, KeyT, KeyFunctorT> {
-  unsigned operator()(const KeyT &Key) const { return KeyFunctorT()(Key); }
 };
 
 /// SparseSet - Fast set implementation for objects that can be identified by
