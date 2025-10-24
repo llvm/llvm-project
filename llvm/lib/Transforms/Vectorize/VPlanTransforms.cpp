2e662f72783eea283f8ddb51ec3bf43ee34d7347
Ramkumar Ramachandra <ramkumar.ramachandra@codasip.com>
2025-10-24 11:43:08 +0100

    [VPlan] Fix error in comment (NFC)

--- llvm/lib/Transforms/Vectorize/VPlanTransforms.cpp
+++ llvm/lib/Transforms/Vectorize/VPlanTransforms.cpp
@@ -82,7 +82,7 @@ bool VPlanTransforms::tryToConvertVPInstructionsToVPRecipes(
               vputils::getOrCreateVPValueForSCEVExpr(Plan, II->getStep());
           // It is always safe to copy over the NoWrap and FastMath flags. In
           // particular, when folding tail by masking, the masked-off lanes are
-          // never executed, so it is safe.
+          // never used, so it is safe.
           VPIRFlags Flags = vputils::getFlagsFromIndDesc(*II);
           NewRecipe = new VPWidenIntOrFpInductionRecipe(
               Phi, Start, Step, &Plan.getVF(), *II, Flags,
