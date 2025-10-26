876dd674049cdd14a912bc060b486907d4e1e7c7
Victor Baranov <bar.victor.2002@gmail.com>
2025-10-26 23:06:24 +0300

    fix windows test

--- clang-tools-extra/test/clang-tidy/infrastructure/default-header-filter.cpp
+++ clang-tools-extra/test/clang-tidy/infrastructure/default-header-filter.cpp
@@ -10,14 +10,14 @@
 // CHECK-EMPTY-NOT: header1.h:1:12: warning:
 // CHECK-EXPLICIT: header1.h:1:12: warning: single-argument constructors must be marked explicit
 // CHECK-NO-SYSTEM: header1.h:1:12: warning: single-argument constructors must be marked explicit
-// CHECK-WITH-SYSTEM: header1.h:1:12: warning: single-argument constructors must be marked explicit
+// CHECK-WITH-SYSTEM-DAG: header1.h:1:12: warning: single-argument constructors must be marked explicit
 
 #include <system-header.h>
 // CHECK-DEFAULT-NOT: system-header.h:1:12: warning:
 // CHECK-EMPTY-NOT: system-header.h:1:12: warning:
 // CHECK-EXPLICIT-NOT: system-header.h:1:12: warning:
 // CHECK-NO-SYSTEM-NOT: system-header.h:1:12: warning:
-// CHECK-WITH-SYSTEM: system-header.h:1:12: warning: single-argument constructors must be marked explicit
+// CHECK-WITH-SYSTEM-DAG: system-header.h:1:12: warning: single-argument constructors must be marked explicit
 
 class A { A(int); };
 // CHECK-DEFAULT: :[[@LINE-1]]:11: warning: single-argument constructors must be marked explicit
