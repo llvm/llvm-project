7b1b82ff7bcdba9f856c72083e7c6bcbc34b87a7
Adrian Prantl <aprantl@apple.com>
2025-10-27 12:41:05 -0700

    Revert "[LLDB] Add debug output to test to diagnose bot failure"
    This reverts commit d8184343755ababad6479d07451f36dd695f75c1.

--- lldb/test/API/macosx/posix_spawn/TestLaunchProcessPosixSpawn.py
+++ lldb/test/API/macosx/posix_spawn/TestLaunchProcessPosixSpawn.py
@@ -21,7 +21,6 @@ def rosetta_debugserver_installed():
     import platform
     version = platform.mac_ver()
     # Workaround for an undiagnosed problem on green dragon.
-    print(version)
     if version[0] and version[0][0] == '15' and version[0][1] == '5':
         return False
     return exists("/Library/Apple/usr/libexec/oah/debugserver")
