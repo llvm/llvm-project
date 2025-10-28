81226b15e4e152614df37b032825b67e1c2f1b7c
Steve O'Brien <steve@obrien.cc>
2025-10-28 11:27:11 -0400

    More formatting and generated-file fixes

--- libcxx/include/__stacktrace/basic_stacktrace.h
+++ libcxx/include/__stacktrace/basic_stacktrace.h
@@ -131,14 +131,12 @@ public:
   // Creation and assignment [stacktrace.basic.cons]
 
   _LIBCPP_ALWAYS_INLINE // Omit this function from the trace
-  static basic_stacktrace
-  current(const allocator_type& __alloc = allocator_type()) noexcept {
+  static basic_stacktrace current(const allocator_type& __alloc = allocator_type()) noexcept {
     return current(0, __default_max_depth, __alloc);
   }
 
   _LIBCPP_ALWAYS_INLINE // Omit this function from the trace
-  static basic_stacktrace
-  current(size_type __skip, const allocator_type& __alloc = allocator_type()) noexcept {
+  static basic_stacktrace current(size_type __skip, const allocator_type& __alloc = allocator_type()) noexcept {
     return current(__skip, __default_max_depth, __alloc);
   }
 
