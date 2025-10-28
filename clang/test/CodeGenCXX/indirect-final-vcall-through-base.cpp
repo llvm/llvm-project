9e57865389c57e16bf1d6cd7c45acd26dd5d0255
Oliver Hunt <oliver@apple.com>
2025-10-27 21:13:30 -0700

    Added the strict-vtable test results, and added tests
    New tests for multiple and virtual inheritance to verify that
    we still perform this adjustments even when  devirtualising the
    calls

--- clang/test/CodeGenCXX/indirect-final-vcall-through-base.cpp
+++ clang/test/CodeGenCXX/indirect-final-vcall-through-base.cpp
@@ -45,6 +45,7 @@ public:
 };
 
 struct Base2 {
+    unsigned value = 0;
     virtual int64_t sharedGet(size_t i) const = 0;
     virtual int64_t get2(size_t i) const = 0;
     virtual int64_t getBatch2(size_t offset, size_t len, int64_t arr[]) const {
@@ -54,6 +55,13 @@ struct Base2 {
         arr[i] = get2(offset + i);
       }
       return result;
+    }
+    virtual int64_t getValue() = 0;
+    virtual int64_t callGetValue() {
+      return getValue();
+    }
+    virtual int64_t useBase(Base *b) {
+      return b->get(0);
     }
 };
 
@@ -69,6 +77,9 @@ struct Derived2 final : Base, Base2 {
     virtual int64_t getSumLen(size_t offset, size_t len) const override;
     int64_t getBatch2(size_t offset, size_t len, int64_t arr[]) const override;
     virtual int64_t useSharedGet(size_t i) override;
+    virtual int64_t useBase(Base *b) override;
+    virtual int64_t getValue() override { return value; }
+    virtual int64_t callGetValue() override;
 };
 
 struct IntermediateA: virtual Base {
@@ -93,6 +104,9 @@ struct Derived3 final: Derived3Part1, Derived3Part2 {
     virtual int64_t get2(size_t i) const override { return 29; }
     virtual int64_t getBatch2(size_t offset, size_t len, int64_t arr[]) const override;
     virtual int64_t useSharedGet(size_t i) override;
+    virtual int64_t useBase(Base *b) override;
+    virtual int64_t getValue() override { return value; }
+    virtual int64_t callGetValue() override;
 };
 
 int64_t Derived1::directCall(size_t offset, size_t len) {
@@ -131,6 +145,14 @@ int64_t Derived2::useSharedGet(size_t i) {
   return Base::useSharedGet(i);
 }
 
+int64_t Derived2::useBase(Base *b) {
+  return Base2::useBase(this);
+}
+
+int64_t Derived2::callGetValue() {
+  return Base2::callGetValue();
+}
+
 int64_t Derived3::getBatch(size_t offset, size_t len, int64_t arr[]) const {
   return Base::getBatch(offset, len, arr);
 }
@@ -140,6 +162,13 @@ int64_t Derived3::getBatch2(size_t offset, size_t len, int64_t arr[]) const {
 
 int64_t Derived3::useSharedGet(size_t i) {
   return Base::useSharedGet(i);
+}
+int64_t Derived3::useBase(Base *b) {
+  return Base2::useBase(this);
+}
+
+int64_t Derived3::callGetValue() {
+  return Base2::callGetValue();
 }
 
 // CHECK-LABEL: i64 @_ZN8Derived110directCallEmm(
@@ -263,15 +292,22 @@ int64_t Derived3::useSharedGet(size_t i) {
 // CHECK: ret i64
 
 // CHECK-LABEL: i64 @_ZTv0_n40_NK8Derived39getBatch2EmmPl
+// CHECK: entry:
+  // %vtable = load ptr, ptr %this, align 8, !tbaa !6
 // CHECK: [[VTABLE:%.*]] = load ptr, ptr %this
+  // %0 = getelementptr inbounds i8, ptr %vtable, i64 -40
 // CHECK: [[THISOFFSET_VSLOT:%.*]] = getelementptr inbounds i8, ptr [[VTABLE]], i64 -40
+  // %1 = load i64, ptr %0, align 8
 // CHECK: [[THIS_OFFSET:%.*]] = load i64, ptr [[THISOFFSET_VSLOT]]
-// CHECK: [[THIS:%.*]] = getelementptr inbounds i8, ptr %this, i64 [[THIS_OFFSET]]
-// CHECK: [[VTABLE_ADDR:%.*]] = getelementptr inbounds nuw i8, ptr [[THIS]], i64 8
-// CHECK: [[VTABLE1:%.*]] = load ptr, ptr [[VTABLE_ADDR]]
+  // %2 = getelementptr inbounds i8, ptr %this, i64 %1
+// CHECK: [[BASE:%.*]] = getelementptr inbounds i8, ptr %this, i64 [[THIS_OFFSET]]
+         // %add.ptr.i = getelementptr inbounds nuw i8, ptr %2, i64 16
+// CHECK: [[THIS:%.*]] = getelementptr inbounds nuw i8, ptr [[BASE]], i64 16
+// CHECK: {{for.body.*:}}
+// CHECK: [[VTABLE1:%.*]] = load ptr, ptr [[THIS]]
 // CHECK: [[VFN_SLOT1:%.*]] = getelementptr inbounds nuw i8, ptr [[VTABLE1]]
 // CHECK: [[VFN1:%.*]] = load ptr, ptr [[VFN_SLOT1]]
-// CHECK: [[VTABLE2:%.*]] = load ptr, ptr [[VTABLE_ADDR]]
+// CHECK: [[VTABLE2:%.*]] = load ptr, ptr [[THIS]]
 // CHECK: [[VFN_SLOT2:%.*]] = getelementptr inbounds nuw i8, ptr [[VTABLE2]]
 // CHECK: [[VFN2:%.*]] = load ptr, ptr [[VFN_SLOT2]]
 // CHECK: tail call noundef i64 [[VFN2]](
@@ -318,6 +354,21 @@ int64_t Derived3::useSharedGet(size_t i) {
 // STRICT-NOT: call
 // STRICT: ret i64
 
+// STRICT-LABEL: i64 @_ZN8Derived27useBaseEP4Base
+// STRICT-NOT: call
+// STRICT: ret i64
+
+// STRICT-LABEL: i64 @_ZN8Derived212callGetValueEv(
+// STRICT: [[OFFSET_THIS:%.*]] = getelementptr inbounds nuw i8, ptr %this, i64 8
+// STRICT: [[INVARIANT_THIS:%.*]] = tail call ptr @llvm.strip.invariant.group.p0(ptr nonnull [[OFFSET_THIS]])
+// STRICT: [[VALUE_PTR:%.*]] = getelementptr inbounds nuw i8, ptr [[INVARIANT_THIS]], i64 8
+// STRICT: ret i64
+
+// STRICT-LABEL: i64 @_ZThn8_N8Derived212callGetValueEv
+// STRICT: [[INVARIANT_THIS:%.*]] = tail call ptr @llvm.strip.invariant.group.p0(ptr nonnull readonly %this)
+// STRICT: [[VALUE_PTR:%.*]] = getelementptr inbounds nuw i8, ptr [[INVARIANT_THIS]], i64 8
+// STRICT: ret i64
+
 // STRICT-LABEL: i64 @_ZNK8Derived38getBatchEmmPl
 // STRICT-NOT: call
 // STRICT: ret i64
@@ -340,4 +391,24 @@ int64_t Derived3::useSharedGet(size_t i) {
 
 // STRICT-LABEL: i64 @_ZTv0_n56_N8Derived312useSharedGetEm
 // STRICT-NOT: call
+// STRICT: ret i64
+
+// STRICT-LABEL: i64 @_ZN8Derived37useBaseEP4Base
+// STRICT-NOT: call
+// STRICT: ret i64
+
+// STRICT-LABEL: i64 @_ZN8Derived312callGetValueEv(
+// STRICT: [[TRUE_THIS:%.*]] = getelementptr inbounds nuw i8, ptr %this, i64 16
+// STRICT: [[INVARIANT_THIS:%.*]] = tail call ptr @llvm.strip.invariant.group.p0(ptr nonnull [[TRUE_THIS]])
+// STRICT: [[VALUE_PTR:%.*]] = getelementptr inbounds nuw i8, ptr [[INVARIANT_THIS]], i64 8
+// STRICT: ret i64
+
+// STRICT-LABEL: i64 @_ZTv0_n56_N8Derived312callGetValueEv
+// STRICT: [[VTABLE:%.*]] = load ptr, ptr %this
+// STRICT: [[THISOFFSET_VSLOT:%.*]] = getelementptr inbounds i8, ptr [[VTABLE]], i64 -48
+// STRICT: [[THIS_OFFSET:%.*]] = load i64, ptr [[THISOFFSET_VSLOT]]
+// STRICT: [[VIRTUAL_BASE:%.*]] = getelementptr inbounds i8, ptr %this, i64 [[THIS_OFFSET]]
+// STRICT: [[TRUE_THIS:%.*]] = getelementptr inbounds nuw i8, ptr [[VIRTUAL_BASE]], i64 16
+// STRICT: [[INVARIANT_THIS:%.*]] = tail call ptr @llvm.strip.invariant.group.p0(ptr nonnull [[TRUE_THIS]])
+// STRICT: [[VALUE_PTR:%.*]] = getelementptr inbounds nuw i8, ptr [[INVARIANT_THIS]], i64 8
 // STRICT: ret i64
