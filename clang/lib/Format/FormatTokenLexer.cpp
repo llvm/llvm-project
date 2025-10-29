8d6f6905e1d28ced6a89b63f65f6cb4e0aa07e2e
sstwcw <su3e8a96kzlver@posteo.net>
2025-10-29 16:38:24 +0000

    [clang-format] Recognize Verilog followed-by operators
    When formatting Verilog code, the program changes the hash to
    `kw_verilogHash` and the backtick to `tok::hash`.  The developer did not
    take that into account when writing the part for recognizing the `#-#`
    and `#=#` operators.  The part did not work.  The program would add a
    space within the operator.
    
    after
    
    ```SystemVerilog
    ##[0 : 5] done #-# always !rst;
    ```
    
    before
    
    ```SystemVerilog
    ##[0 : 5] done #- #always !rst;
    ```

--- clang/lib/Format/FormatTokenLexer.cpp
+++ clang/lib/Format/FormatTokenLexer.cpp
@@ -318,11 +318,18 @@ void FormatTokenLexer::tryMergePreviousTokens() {
                            {tok::equal, tok::greater},
                            {tok::star, tok::greater},
                            {tok::pipeequal, tok::greater},
-                           {tok::pipe, tok::arrow},
-                           {tok::hash, tok::minus, tok::hash},
-                           {tok::hash, tok::equal, tok::hash}},
+                           {tok::pipe, tok::arrow}},
                           TT_BinaryOperator) ||
         Tokens.back()->is(tok::arrow)) {
+      Tokens.back()->ForcedPrecedence = prec::Comma;
+      return;
+    }
+    if (Tokens.size() >= 3 &&
+        Tokens[Tokens.size() - 3]->is(Keywords.kw_verilogHash) &&
+        Tokens[Tokens.size() - 2]->isOneOf(tok::minus, tok::equal) &&
+        Tokens[Tokens.size() - 1]->is(Keywords.kw_verilogHash) &&
+        tryMergeTokens(3, TT_BinaryOperator)) {
+      Tokens.back()->setFinalizedType(TT_BinaryOperator);
       Tokens.back()->ForcedPrecedence = prec::Comma;
       return;
     }
