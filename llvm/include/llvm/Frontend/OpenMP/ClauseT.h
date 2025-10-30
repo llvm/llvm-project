3898f54aec861e7d7cd7db661e31f3a53cc1db58
Ritanya-B-Bharadwaj <ritanya.b.bharadwaj@gmail.com>
2025-10-30 12:11:26 +0530

    Update ClauseT.h

--- llvm/include/llvm/Frontend/OpenMP/ClauseT.h
+++ llvm/include/llvm/Frontend/OpenMP/ClauseT.h
@@ -1359,10 +1359,10 @@ using WrapperClausesT = std::variant<
     PartialT<T, I, E>, PriorityT<T, I, E>, PrivateT<T, I, E>,
     ProcBindT<T, I, E>, ReverseOffloadT<T, I, E>, SafelenT<T, I, E>,
     SelfMapsT<T, I, E>, SeverityT<T, I, E>, SharedT<T, I, E>, SimdlenT<T, I, E>,
-    SizesT<T, I, E>, PermutationT<T, I, E>, ThreadLimitT<T, I, E>, ThreadsetT<T, I, E>,
-    UnifiedAddressT<T, I, E>, UnifiedSharedMemoryT<T, I, E>, UniformT<T, I, E>,
-    UpdateT<T, I, E>, UseDeviceAddrT<T, I, E>, UseDevicePtrT<T, I, E>,
-    UsesAllocatorsT<T, I, E>>;
+    SizesT<T, I, E>, PermutationT<T, I, E>, ThreadLimitT<T, I, E>,
+    ThreadsetT<T, I, E>, UnifiedAddressT<T, I, E>,
+    UnifiedSharedMemoryT<T, I, E>, UniformT<T, I, E>, UpdateT<T, I, E>,
+    UseDeviceAddrT<T, I, E>, UseDevicePtrT<T, I, E>, UsesAllocatorsT<T, I, E>>;
 
 template <typename T, typename I, typename E>
 using UnionOfAllClausesT = typename type::Union< //
