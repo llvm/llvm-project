0c8334ef292b1967cda479db397b36b03e016935
Sarah Spall <sarahspall@microsoft.com>
2025-10-30 09:25:09 -0700

    update test again

--- clang/test/Driver/dxc_enable16bittypes.hlsl
+++ clang/test/Driver/dxc_enable16bittypes.hlsl
@@ -1,5 +1,4 @@
 // RUN: %clang_dxc -enable-16bit-types -T lib_6_7 %s -### %s 2>&1 | FileCheck %s
-// RUN: %clang_dxc -enable-16bit-types -T lib_6_7 %s -Xclang -verify
 
 // Make sure enable-16bit-types flag translates into '-fnative-half-type' and 'fnative-int16-type'
 // CHECK: "-fnative-half-type"
