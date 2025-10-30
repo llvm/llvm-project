acd5faa5d98b93e03e9b84ea91f42f51cf6859ef
Raul Tambre <raul@tambre.ee>
2025-10-30 21:08:11 +0200

    [NFCI][lldb][test] Fix mismatched C/C++ substitutions
    Most of the cases were where a C++ file was being compiled with the C substitution.
    There were a few cases of the opposite though.
    
    LLDB seems to be the only real culprit in the LLVM codebase for these mismatches.
    Rest of the LLVM presumably sticks at least language-specific options in the common substitutions
    making the mistakes immediately apparent.
    
    I found these by using Clang frontend configuration files containing language-specific options for
    both C and C++ (e.g. `-std=c2y` and `-std=c++26`).

--- lldb/test/Shell/SymbolFile/DWARF/x86/dwo-not-found-warning.cpp
+++ lldb/test/Shell/SymbolFile/DWARF/x86/dwo-not-found-warning.cpp
@@ -1,4 +1,4 @@
-// RUN: %clang --target=x86_64-pc-linux -g -gsplit-dwarf -c %s -o %t.o
+// RUN: %clangxx --target=x86_64-pc-linux -g -gsplit-dwarf -c %s -o %t.o
 // RUN: rm %t.dwo
 // RUN: %lldb %t.o -o "br set -n main" -o exit 2>&1 | FileCheck %s
 
