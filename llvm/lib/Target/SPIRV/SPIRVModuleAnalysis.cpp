f91b71f5ee5de60a870acb604f69ba8bde0062f1
Alex Voicu <alexandru.voicu@amd.com>
2025-10-31 19:55:06 +0200

    Fix formatting.

--- llvm/lib/Target/SPIRV/SPIRVModuleAnalysis.cpp
+++ llvm/lib/Target/SPIRV/SPIRVModuleAnalysis.cpp
@@ -2183,8 +2183,7 @@ static void collectReqs(const Module &M, SPIRV::ModuleAnalysisInfo &MAI,
     if (F.getMetadata("max_work_group_size"))
       MAI.Reqs.getAndAddRequirements(
           SPIRV::OperandCategory::ExecutionModeOperand,
-          SPIRV::ExecutionMode::MaxWorkgroupSizeINTEL,
-          ST);
+          SPIRV::ExecutionMode::MaxWorkgroupSizeINTEL, ST);
     if (F.getMetadata("vec_type_hint"))
       MAI.Reqs.getAndAddRequirements(
           SPIRV::OperandCategory::ExecutionModeOperand,
