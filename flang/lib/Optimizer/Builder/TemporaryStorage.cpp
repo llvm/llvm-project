72fb4e0d9646cf165071c3bd37d79d5a1dc0ec8e
cdchen-ca <cdchen@ca.ibm.com>
2025-10-31 10:20:14 -0400

    To address review comments to simplify the code.

--- flang/lib/Optimizer/Builder/TemporaryStorage.cpp
+++ flang/lib/Optimizer/Builder/TemporaryStorage.cpp
@@ -258,19 +258,9 @@ void fir::factory::AnyVariableStack::pushValue(mlir::Location loc,
                                                fir::FirOpBuilder &builder,
                                                mlir::Value variable) {
   hlfir::Entity entity{variable};
-  if (mlir::isa<fir::BaseBoxType>(entity.getType())) {
-    mlir::Value box =
-        hlfir::genVariableBox(loc, builder, entity, entity.getBoxType());
-    fir::runtime::genPushDescriptor(loc, builder, opaquePtr, fir::getBase(box));
-  } else {
-    mlir::Type storageElementType =
-        hlfir::getFortranElementType(retValueBox.getType());
-    auto [box, maybeCleanUp] =
-        hlfir::convertToBox(loc, builder, entity, storageElementType);
-    fir::runtime::genPushDescriptor(loc, builder, opaquePtr, fir::getBase(box));
-    if (maybeCleanUp)
-      (*maybeCleanUp)();
-  }
+  mlir::Value box =
+      hlfir::genVariableBox(loc, builder, entity, entity.getBoxType());
+  fir::runtime::genPushDescriptor(loc, builder, opaquePtr, fir::getBase(box));
 }
 
 void fir::factory::AnyVariableStack::resetFetchPosition(
