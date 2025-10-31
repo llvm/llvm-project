20bcce5e6ba4b1cf9143addf817f707b471c7f3e
Adrian Vogelsgesang <avogelsgesang@salesforce.com>
2025-10-31 16:13:32 +0000

    Fix test case file names

--- libcxx/test/std/language.support/support.exception/propagation/exception_ptr_move_ctr.pass.cpp
+++ libcxx/test/std/language.support/support.exception/propagation/exception_ptr_move_ctr.pass.cpp
@@ -6,13 +6,13 @@
 //
 //===----------------------------------------------------------------------===//
 
-// UNSUPPORTED: no-exceptions std-at-least-c++11
+// UNSUPPORTED: no-exceptions, std-at-least-c++11
 
 // <exception>
 
 // typedef unspecified exception_ptr;
 
-// Test the move assignment of exception_ptr
+// Test the move constructor of exception_ptr
 
 #include <exception>
 #include <utility>
@@ -24,14 +24,12 @@ int main(int, char**) {
   std::exception_ptr p = std::make_exception_ptr(42);
   std::exception_ptr p2{p};
   assert(p2 == p);
-  // Under test: the move assignment
-  std::exception_ptr p3;
-  p3 = std::move(p2);
+  // Under test: The move constructor
+  std::exception_ptr p3{std::move(p2)};
   assert(p3 == p);
 // `p2` was moved from. In libc++ it will be nullptr, but
 // this is not guaranteed by the standard.
 #if defined(_LIBCPP_VERSION) && !defined(_LIBCPP_ABI_MICROSOFT)
-  assert(p2 == nullptr);
   assert(p2 == nullptr);
 #endif
 
