425762e2efa7c15f852ae21b8434b190449a49f4
Andrew Haberlandt <ahaberlandt@apple.com>
2025-11-03 14:29:51 -0800

    fix test string

--- compiler-rt/test/asan/TestCases/Darwin/asan-verify-module-map.cpp
+++ compiler-rt/test/asan/TestCases/Darwin/asan-verify-module-map.cpp
@@ -1,10 +1,10 @@
-// This test simply checks that the "Invalid module map" warning is not printed
+// This test simply checks that the "Invalid dyld module map" warning is not printed
 // in the output of a backtrace.
 
 // RUN: %clangxx_asan -O0 -g %s -o %t.executable
 // RUN: %env_asan_opts="print_module_map=2" not %run %t.executable 2>&1 | FileCheck %s
 
-// CHECK-NOT: WARN: Invalid module map
+// CHECK-NOT: WARN: Invalid dyld module map
 
 #include <cstdlib>
 
