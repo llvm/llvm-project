3d3fab17f5ea8a14eb390f53075c094f5e1f19fa
Krzysztof Parzyszek <Krzysztof.Parzyszek@amd.com>
2025-11-03 07:37:13 -0600

    [flang][OpenMP] Use OmpDirectiveSpecification in ALLOCATE (#165865)
    The ALLOCATE directive has two forms:
    - A declarative form with a standalone directive:
      ```
      !$OMP ALLOCATE (variable-list-item...)
      ```
    - An executable form that consists of several directives followed by an
    ALLOCATE statement:
      ```
      !$OMP ALLOCATE (variable-list-item...)
      !$OMP ALLOCATE (variable-list-item...)
      ...
      ALLOCATE (...)
      ```
    
    The second form was deprecated in OpenMP 5.2 in favor of the ALLOCATORS
    construct.
    
    Since in the parse tree every type corresponding to a directive only
    corresponds to a single directive, the executable form is represented by
    a sequence of nested OmpAllocateDirectives, e.g.
    ```
       !$OMP ALLOCATE(x)
       !$OMP ALLOCATE(y)
       ALLOCATE(x, y)
    ```
    will become
    ```
       OmpAllocateDirective
       |- ALLOCATE(x)            // begin directive
       `- OmpAllocateDirective   // block
          |- ALLOCATE(y)            // begin directive
          `- ALLOCATE(x, y)         // block
    ```
    With this change all AST nodes for directives use
    OmpDirectiveSpecification as the directive representation.

--- flang/test/Semantics/OpenMP/allocate10.f90
+++ flang/test/Semantics/OpenMP/allocate10.f90
@@ -4,8 +4,8 @@ subroutine f00
   integer, allocatable :: x, y
 
   continue
-  !ERROR: If multiple directives are present in an executable ALLOCATE directive, at most one of them may specify no list items
   !$omp allocate
+  !ERROR: If multiple directives are present in an executable ALLOCATE directive, at most one of them may specify no list items
   !$omp allocate
   allocate(x, y)
 end
