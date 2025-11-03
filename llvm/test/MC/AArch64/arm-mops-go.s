114e418d5b46cde7c28e76ea79e742c1aceb0061
Jonathan Thackray <jonathan.thackray@arm.com>
2025-11-03 22:39:11 +0000

    fixup! [AArch64][llvm] Add instructions for FEAT_MOPS_GO
    Adjust assembly so that `Rm` is ignored, since it is always 0b11111.

--- llvm/test/MC/AArch64/arm-mops-go.s
+++ llvm/test/MC/AArch64/arm-mops-go.s
@@ -16,74 +16,74 @@
 // FEAT_MOPS_GO Extension instructions
 //------------------------------------------------------------------------------
 
-setgop [x3]!, x2!, x1
-// CHECK-INST:    setgop [x3]!, x2!, x1
-// CHECK-ENCODING: [0x43,0x00,0xc1,0x1d]
-// CHECK-UNKNOWN: 1dc10043
+setgop [x3]!, x2!
+// CHECK-INST:    setgop [x3]!, x2!
+// CHECK-ENCODING: [0x43,0x00,0xdf,0x1d]
+// CHECK-UNKNOWN: 1ddf0043
 // CHECK-ERROR: instruction requires: mops-go mte
 
-setgom [x3]!, x2!, x1
-// CHECK-INST:    setgom [x3]!, x2!, x1
-// CHECK-ENCODING: [0x43,0x40,0xc1,0x1d]
-// CHECK-UNKNOWN: 1dc14043
+setgom [x3]!, x2!
+// CHECK-INST:    setgom [x3]!, x2!
+// CHECK-ENCODING: [0x43,0x40,0xdf,0x1d]
+// CHECK-UNKNOWN: 1ddf4043
 // CHECK-ERROR: instruction requires: mops-go mte
 
-setgoe [x3]!, x2!, x1
-// CHECK-INST:    setgoe [x3]!, x2!, x1
-// CHECK-ENCODING: [0x43,0x80,0xc1,0x1d]
-// CHECK-UNKNOWN: 1dc18043
+setgoe [x3]!, x2!
+// CHECK-INST:    setgoe [x3]!, x2!
+// CHECK-ENCODING: [0x43,0x80,0xdf,0x1d]
+// CHECK-UNKNOWN: 1ddf8043
 // CHECK-ERROR: instruction requires: mops-go mte
 
-setgopn [x3]!, x2!, x1
-// CHECK-INST:    setgopn [x3]!, x2!, x1
-// CHECK-ENCODING: [0x43,0x20,0xc1,0x1d]
-// CHECK-UNKNOWN: 1dc12043
+setgopn [x3]!, x2!
+// CHECK-INST:    setgopn [x3]!, x2!
+// CHECK-ENCODING: [0x43,0x20,0xdf,0x1d]
+// CHECK-UNKNOWN: 1ddf2043
 // CHECK-ERROR: instruction requires: mops-go mte
 
-setgomn [x3]!, x2!, x1
-// CHECK-INST:    setgomn [x3]!, x2!, x1
-// CHECK-ENCODING: [0x43,0x60,0xc1,0x1d]
-// CHECK-UNKNOWN: 1dc16043
+setgomn [x3]!, x2!
+// CHECK-INST:    setgomn [x3]!, x2!
+// CHECK-ENCODING: [0x43,0x60,0xdf,0x1d]
+// CHECK-UNKNOWN: 1ddf6043
 // CHECK-ERROR: instruction requires: mops-go mte
 
-setgoen [x3]!, x2!, x1
-// CHECK-INST:    setgoen [x3]!, x2!, x1
-// CHECK-ENCODING: [0x43,0xa0,0xc1,0x1d]
-// CHECK-UNKNOWN: 1dc1a043
+setgoen [x3]!, x2!
+// CHECK-INST:    setgoen [x3]!, x2!
+// CHECK-ENCODING: [0x43,0xa0,0xdf,0x1d]
+// CHECK-UNKNOWN: 1ddfa043
 // CHECK-ERROR: instruction requires: mops-go mte
 
-setgopt [x3]!, x2!, x1
-// CHECK-INST:    setgopt [x3]!, x2!, x1
-// CHECK-ENCODING: [0x43,0x10,0xc1,0x1d]
-// CHECK-UNKNOWN: 1dc11043
+setgopt [x3]!, x2!
+// CHECK-INST:    setgopt [x3]!, x2!
+// CHECK-ENCODING: [0x43,0x10,0xdf,0x1d]
+// CHECK-UNKNOWN: 1ddf1043
 // CHECK-ERROR: instruction requires: mops-go mte
 
-setgomt [x3]!, x2!, x1
-// CHECK-INST:    setgomt [x3]!, x2!, x1
-// CHECK-ENCODING: [0x43,0x50,0xc1,0x1d]
-// CHECK-UNKNOWN: 1dc15043
+setgomt [x3]!, x2!
+// CHECK-INST:    setgomt [x3]!, x2!
+// CHECK-ENCODING: [0x43,0x50,0xdf,0x1d]
+// CHECK-UNKNOWN: 1ddf5043
 // CHECK-ERROR: instruction requires: mops-go mte
 
-setgoet [x3]!, x2!, x1
-// CHECK-INST:    setgoet [x3]!, x2!, x1
-// CHECK-ENCODING: [0x43,0x90,0xc1,0x1d]
-// CHECK-UNKNOWN: 1dc19043
+setgoet [x3]!, x2!
+// CHECK-INST:    setgoet [x3]!, x2!
+// CHECK-ENCODING: [0x43,0x90,0xdf,0x1d]
+// CHECK-UNKNOWN: 1ddf9043
 // CHECK-ERROR: instruction requires: mops-go mte
 
-setgoptn [x3]!, x2!, x1
-// CHECK-INST:    setgoptn [x3]!, x2!, x1
-// CHECK-ENCODING: [0x43,0x30,0xc1,0x1d]
-// CHECK-UNKNOWN: 1dc13043
+setgoptn [x3]!, x2!
+// CHECK-INST:    setgoptn [x3]!, x2!
+// CHECK-ENCODING: [0x43,0x30,0xdf,0x1d]
+// CHECK-UNKNOWN: 1ddf3043
 // CHECK-ERROR: instruction requires: mops-go mte
 
-setgomtn [x3]!, x2!, x1
-// CHECK-INST:    setgomtn [x3]!, x2!, x1
-// CHECK-ENCODING: [0x43,0x70,0xc1,0x1d]
-// CHECK-UNKNOWN: 1dc17043
+setgomtn [x3]!, x2!
+// CHECK-INST:    setgomtn [x3]!, x2!
+// CHECK-ENCODING: [0x43,0x70,0xdf,0x1d]
+// CHECK-UNKNOWN: 1ddf7043
 // CHECK-ERROR: instruction requires: mops-go mte
 
-setgoetn [x3]!, x2!, x1
-// CHECK-INST:    setgoetn [x3]!, x2!, x1
-// CHECK-ENCODING: [0x43,0xb0,0xc1,0x1d]
-// CHECK-UNKNOWN: 1dc1b043
+setgoetn [x3]!, x2!
+// CHECK-INST:    setgoetn [x3]!, x2!
+// CHECK-ENCODING: [0x43,0xb0,0xdf,0x1d]
+// CHECK-UNKNOWN: 1ddfb043
 // CHECK-ERROR: instruction requires: mops-go mte
