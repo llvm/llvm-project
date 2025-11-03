f919b68db1e9932fa30ea461bdf918b16f5ff31a
Hanbum Park <kese111@gmail.com>
2025-11-03 23:33:37 +0900

    [IVDesc] Check loop-preheader for loop-legality when pass-remarks enabled
    When `-pass-remarks=loop-vectorize` is specified, the subsequent
    logic is executed to display detailed debug messages even
    if no PreHeader exists in the loop.
    
    Therefore, an assert occurs when the `getLoopPreHeader()` function
    is called. This commit resolves that issue.
    
    Fixed: #165377

--- llvm/lib/Analysis/IVDescriptors.cpp
+++ llvm/lib/Analysis/IVDescriptors.cpp
@@ -282,7 +282,9 @@ bool RecurrenceDescriptor::AddReductionVar(
 
   // Obtain the reduction start value from the value that comes from the loop
   // preheader.
-  Value *RdxStart = Phi->getIncomingValueForBlock(TheLoop->getLoopPreheader());
+  Value *RdxStart;
+  if (TheLoop->getLoopPreheader())
+    RdxStart = Phi->getIncomingValueForBlock(TheLoop->getLoopPreheader());
 
   // ExitInstruction is the single value which is used outside the loop.
   // We only allow for a single reduction value to be used outside the loop.
@@ -648,6 +650,9 @@ bool RecurrenceDescriptor::AddReductionVar(
   //       kept simple enough.
   collectCastInstrs(TheLoop, ExitInstruction, RecurrenceType, CastInsts,
                     MinWidthCastToRecurrenceType);
+
+  if (!RdxStart)
+    return false;
 
   // We found a reduction var if we have reached the original phi node and we
   // only have a single instruction with out-of-loop users.
@@ -1689,6 +1694,9 @@ bool InductionDescriptor::isInductionPHI(
   // the current Phi is not present inside the loop header.
   assert(Phi->getParent() == TheLoop->getHeader() &&
          "Invalid Phi node, not present in loop header");
+
+  if (!TheLoop->getLoopPreheader())
+    return false;
 
   Value *StartValue =
       Phi->getIncomingValueForBlock(TheLoop->getLoopPreheader());
