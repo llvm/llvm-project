16414b96e0ca5ee44b0332bc387ecbd0046eacd4
Susikrishna <krishnasusi323@gmail.com>
2025-11-04 12:49:38 +0530

    [Clang] Use ExtWarn for static local variable in extern inline function (fixes #39524)

---
+++ clang/test/Sema/inline-static-var.c
@@ -0,0 +1,7 @@
+// RUN: %clang_cc1 -std=c11 -Wno-unused-variable -Wno-deprecated-non-prototype -Wno-inline -pedantic-errors -verify %s
+
+inline void f(void) { // expected-note {{use 'static' to give inline function 'f' internal linkage}}
+  static int x; // expected-error {{non-constant static local variable in inline function may be different in different files}}
+}
+
+int main(void) { return 0; }
