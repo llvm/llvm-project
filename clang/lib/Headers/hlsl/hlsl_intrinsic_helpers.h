af26e93430b3f9442992f39fa336ea4ed3d3080c
Deric Cheung <cheung.deric@gmail.com>
2025-11-05 10:52:00 -0800

    Fix return type of firstbithigh_impl after the refactor

--- clang/lib/Headers/hlsl/hlsl_intrinsic_helpers.h
+++ clang/lib/Headers/hlsl/hlsl_intrinsic_helpers.h
@@ -149,7 +149,7 @@ template <typename T> constexpr T ldexp_impl(T X, T Exp) {
 }
 
 template <typename K, typename T, int BitWidth>
-constexpr uint firstbithigh_impl(T X) {
+constexpr K firstbithigh_impl(T X) {
   K FBH = __builtin_hlsl_elementwise_firstbithigh(X);
 #if defined(__DIRECTX__)
   // The firstbithigh DXIL ops count bits from the wrong side, so we need to
