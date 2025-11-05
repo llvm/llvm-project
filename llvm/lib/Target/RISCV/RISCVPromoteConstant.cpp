4526e4cc8c46d5b9aa2419969f1dad06f4eb3bd4
Alex Bradbury <asb@igalia.com>
2025-11-05 13:30:56 +0000

    Add missed include

--- llvm/lib/Target/RISCV/RISCVPromoteConstant.cpp
+++ llvm/lib/Target/RISCV/RISCVPromoteConstant.cpp
@@ -23,6 +23,7 @@
 #include "llvm/IR/InstIterator.h"
 #include "llvm/IR/Instruction.h"
 #include "llvm/IR/Instructions.h"
+#include "llvm/IR/IntrinsicInst.h"
 #include "llvm/IR/Module.h"
 #include "llvm/IR/Type.h"
 #include "llvm/InitializePasses.h"
