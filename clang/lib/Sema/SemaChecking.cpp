8f6cbbaa37261e8fdc56f9446027fd91f7497ae4
clingfei <1599101385@qq.com>
2025-11-05 09:46:04 +0800

    update according to the comments

--- clang/lib/Sema/SemaChecking.cpp
+++ clang/lib/Sema/SemaChecking.cpp
@@ -2240,11 +2240,10 @@ static bool BuiltinBswapg(Sema &S, CallExpr *TheCall) {
         << ArgTy;
     return true;
   }
-  const auto *bitIntType = dyn_cast<BitIntType>(ArgTy);
-  if (bitIntType != nullptr) {
-    if (bitIntType->getNumBits() % 16 != 0 && bitIntType->getNumBits() != 8) {
-      S.Diag(Arg->getBeginLoc(), diag::err_bswapg_bitint_not_16bit_aligned)
-          << ArgTy << bitIntType->getNumBits();
+  if (const auto *BT = dyn_cast<BitIntType>(ArgTy)) {
+    if (BT->getNumBits() % 16 != 0 && BT->getNumBits() != 8) {
+      S.Diag(Arg->getBeginLoc(), diag::err_bswapg_invalid_bit_width)
+          << ArgTy << BT->getNumBits();
       return true;
     }
   }
