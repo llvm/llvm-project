fb74ffdee0191b77671d930fb07ff0d65116ff09
Susan Tan <zujunt@nvidia.com>
2025-11-06 13:48:11 -0800

    remove cuf

--- flang/lib/Optimizer/Dialect/CUF/CUFOps.cpp
+++ flang/lib/Optimizer/Dialect/CUF/CUFOps.cpp
@@ -67,14 +67,6 @@ static llvm::LogicalResult checkCudaAttr(Op op) {
 
 llvm::LogicalResult cuf::AllocOp::verify() { return checkCudaAttr(*this); }
 
-// Attach value-scoped MemAlloc to the result value.
-void cuf::AllocOp::getEffects(
-    llvm::SmallVectorImpl<mlir::MemoryEffects::EffectInstance> &effects) {
-  effects.emplace_back(mlir::MemoryEffects::Allocate::get(),
-                       mlir::cast<mlir::OpResult>(getPtr()),
-                       mlir::SideEffects::DefaultResource::get());
-}
-
 //===----------------------------------------------------------------------===//
 // FreeOp
 //===----------------------------------------------------------------------===//
