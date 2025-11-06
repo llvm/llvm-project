ef92dedeef2a1684faa1bb2828ff1e38fb2b99ac
Patrick Simmons <patrick.simmons@amd.com>
2025-11-06 13:14:40 -0500

    clang-format

--- llvm/lib/Target/AMDGPU/AMDGPUISelLowering.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPUISelLowering.cpp
@@ -5305,7 +5305,7 @@ SDValue AMDGPUTargetLowering::expandABS(SDNode *N, SelectionDAG &DAG,
   if (!IsNegative)
     return TruncResult;
 
-  return DAG.getNegative(TruncResult,DL,N->getValueType(0));
+  return DAG.getNegative(TruncResult, DL, N->getValueType(0));
 }
 
 SDValue AMDGPUTargetLowering::PerformDAGCombine(SDNode *N,
