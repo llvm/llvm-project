5b74ede96c8dbce193e5ab7d2bd30572e5ae1019
ahmed <ahmednour.mohamed2012@gmail.com>
2025-11-06 19:40:24 +0200

    Update tests

--- clang/test/CodeGen/X86/mmx-inline-asm-error.c
+++ clang/test/CodeGen/X86/mmx-inline-asm-error.c
@@ -1,13 +1,13 @@
 // RUN: %clang_cc1 -verify -triple x86_64-unknown-unknown -emit-llvm-only %s
-// RUN: %clang_cc1 -verify -triple x86_64-unknown-unknown -emit-llvm-only -fopenmp %s
+// RUN: %clang_cc1 -verify=omp -triple x86_64-unknown-unknown -emit-llvm-only -fopenmp %s
 typedef int vec256 __attribute__((ext_vector_type(8)));
 
 vec256 foo(vec256 in) {
   vec256 out;
 
-  asm("something %0" : : "y"(in)); // expected-error {{invalid type 'vec256' (vector of 8 'int' values) in asm input for constraint 'y'}}
-  asm("something %0" : "=y"(out)); // expected-error {{invalid type 'vec256' (vector of 8 'int' values) in asm output for constraint '=y'}}
-  asm("something %0, %0" : "+y"(out)); // expected-error {{invalid type 'vec256' (vector of 8 'int' values) in asm output for constraint '+y'}}
+  asm("something %0" : : "y"(in)); // expected-error-re {{invalid {{.*}} constraint 'y'}}
+  asm("something %0" : "=y"(out)); // expected-error-re {{invalid {{.*}} constraint '=y'}} omp-error-re {{invalid {{.*}} constraint 'y'}}
+  asm("something %0, %0" : "+y"(out)); // expected-error-re {{invalid {{.*}}}} omp-error-re {{invalid {{.*}}}}
 
   return out;
 }
