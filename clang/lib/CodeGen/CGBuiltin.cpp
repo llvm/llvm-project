40469ae3beffca393b7a713c6559544573ed61f5
Kees Cook <kees@kernel.org>
2025-11-06 15:58:29 -0800

    CAT->getKind() checking is redundant if we want all possible kinds

--- clang/lib/CodeGen/CGBuiltin.cpp
+++ clang/lib/CodeGen/CGBuiltin.cpp
@@ -1211,16 +1211,10 @@ llvm::Value *CodeGenFunction::emitCountedByPointerSize(
         getContext().getTypeSizeInChars(ElementTy->getPointeeType());
 
     if (ElementSize.isZero()) {
-      // This might be a __sized_by (or __counted_by in GNU mode) on a
+      // This might be a __sized_by (or __counted_by) on a
       // 'void *', which counts bytes, not elements.
       auto *CAT = ElementTy->getAs<CountAttributedType>();
-      if (!CAT || (CAT->getKind() != CountAttributedType::SizedBy &&
-                   CAT->getKind() != CountAttributedType::SizedByOrNull &&
-                   CAT->getKind() != CountAttributedType::CountedBy &&
-                   CAT->getKind() != CountAttributedType::CountedByOrNull))
-        // Okay, not sure what it is now.
-        // FIXME: Should this be an assert?
-        return std::optional<CharUnits>();
+      assert(CAT && "must have an CountAttributedType");
 
       ElementSize = CharUnits::One();
     }
