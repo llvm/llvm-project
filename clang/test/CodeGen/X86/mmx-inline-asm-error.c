91650a4d65eb2ffe9a403f439c4f4709998a1009
ahmed <ahmednour.mohamed2012@gmail.com>
2025-11-06 11:57:25 +0200

    Revert to frontend check

--- clang/test/CodeGen/X86/mmx-inline-asm-error.c
+++ clang/test/CodeGen/X86/mmx-inline-asm-error.c
@@ -1,4 +1,8 @@
 // RUN: %clang_cc1 -verify -triple x86_64-unknown-unknown -emit-llvm-only %s
+
+// Test that MMX register constraint 'y' with mismatched vector sizes
+// produces a proper error message instead of an assertion failure.
+
 typedef int vec256 __attribute__((ext_vector_type(8)));
 
 vec256 foo(vec256 in) {
@@ -11,3 +15,17 @@ vec256 foo(vec256 in) {
   return out;
 }
 
+// Additional tests for different vector sizes
+typedef int vec128 __attribute__((ext_vector_type(4)));
+typedef int vec64  __attribute__((ext_vector_type(2)));
+
+void test_128bit_mismatch() {
+  vec128 out;
+  __asm__("nop" : "=y"(out)); // expected-error {{invalid output size for constraint '=y'}}
+}
+
+void test_64bit_valid() {
+  // This should work - 64-bit vector matches MMX register size
+  vec64 out;
+  __asm__("nop" : "=y"(out));
+}
