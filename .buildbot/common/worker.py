2d928562d0d9f7c8bde70748d35d8e0bd8fa6a9e
Michael Kruse <llvm-project@meinersbur.de>
2025-11-07 13:01:02 +0100

    Summary test
    Rework common logic
    
    No ccache stats for checks
    
    indention fix
    
    diagnostic print
    
    Again print debugging
    
    Print cwd
    
    remove debg

--- .buildbot/common/worker.py
+++ .buildbot/common/worker.py
@@ -1,12 +1,254 @@
 import argparse
+import filecmp
+import tempfile
 import os
 import subprocess
 import sys
 import traceback
-import util
-import traceback
+import pathlib
 import subprocess
+import errno
+import os
+import re
+import shutil
+import subprocess
+import sys
 from contextlib import contextmanager
+
+
+
+
+
+
+def cmake_pjoin(*args):
+    """
+    Join paths like safe_pjoin, but replace backslashes with forward
+    slashes on platforms where they are path separators. This prevents
+    CMake from choking when trying to decode what it thinks are escape
+    sequences in filenames.
+    """
+    result = safe_pjoin(*args)
+    if os.sep == '\\':
+        return result.replace('\\', '/')
+    else:
+        return result
+
+
+def report(msg):
+    sys.stderr.write(msg + '\n')
+    sys.stderr.flush()
+
+
+def report_run_cmd(cmd, shell=False, *args, **kwargs):
+    """
+    Print a command, then executes it using subprocess.check_call.
+    """
+    report('Running: %s' % ((cmd if shell else shquote_cmd(cmd)),))
+    sys.stderr.flush()
+    subprocess.check_call(cmd, shell=shell, *args, **kwargs)
+
+
+def mkdirp(path):
+    """Create directory path if it does not already exist."""
+    try:
+        os.makedirs(path)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+
+
+def rmtree(path):
+    """
+    Remove directory path and all its subdirectories. This differs from
+    shutil.rmtree() in that it tries to adjust permissions so that deletion
+    will succeed.
+    """
+    # Some files will not be deletable, so we set permissions that allow
+    # deletion before we try deleting files.
+    for root, dirs, files in os.walk(path):
+        os.chmod(root, 0o755)
+        for f in files:
+            p = os.path.join(root, f)
+            os.chmod(p, 0o644)
+            os.unlink(p)
+    # At this point, we should have a tree of deletable directories.
+    shutil.rmtree(path)
+
+
+def safe_pjoin(dirname, *args):
+    """
+    Join path components with os.path.join, skipping the first component
+    if it is None.
+    """
+    if dirname is None:
+        return os.path.join(*args)
+    else:
+        return os.path.join(dirname, *args)
+
+
+def _shquote_impl(txt, escaped_chars, quoted_chars):
+    quoted = re.sub(escaped_chars, r'\\\1', txt)
+    if len(quoted) == len(txt) and not quoted_chars.search(txt):
+        return txt
+    else:
+        return '"' + quoted + '"'
+
+
+_SHQUOTE_POSIX_ESCAPEDCHARS = re.compile(r'(["`$\\])')
+_SHQUOTE_POSIX_QUOTEDCHARS = re.compile('[|&;<>()\' \t\n]')
+
+
+def shquote_posix(txt):
+    """Return txt, appropriately quoted for POSIX shells."""
+    return _shquote_impl(
+        txt, _SHQUOTE_POSIX_ESCAPEDCHARS, _SHQUOTE_POSIX_QUOTEDCHARS)
+
+
+_SHQUOTE_WINDOWS_ESCAPEDCHARS = re.compile(r'(["\\])')
+_SHQUOTE_WINDOWS_QUOTEDCHARS = re.compile('[ \t\n]')
+
+
+def shquote_windows(txt):
+    """Return txt, appropriately quoted for Windows's cmd.exe."""
+    return _shquote_impl(
+        txt.replace('%', '%%'),
+        _SHQUOTE_WINDOWS_ESCAPEDCHARS, _SHQUOTE_WINDOWS_QUOTEDCHARS)
+
+
+def shquote(txt):
+    """Return txt, appropriately quoted for use in a shell command."""
+    if os.name in set(('nt', 'os2', 'ce')):
+        return shquote_windows(txt)
+    else:
+        return shquote_posix(txt)
+
+
+def shquote_cmd(cmd):
+    """Convert a list of shell arguments to an appropriately quoted string."""
+    return ' '.join(map(shquote, cmd))
+
+
+
+def clean_dir(path):
+    """
+    Removes directory at path (and all its subdirectories) if it exists,
+    and creates an empty directory in its place.
+    """
+    try:
+        rmtree(path)
+    except OSError as e:
+        if e.errno != errno.ENOENT:
+            raise
+    mkdirp(path)
+
+
+def cmake_pjoin(*args):
+    """
+    Join paths like safe_pjoin, but replace backslashes with forward
+    slashes on platforms where they are path separators. This prevents
+    CMake from choking when trying to decode what it thinks are escape
+    sequences in filenames.
+    """
+    result = safe_pjoin(*args)
+    if os.sep == '\\':
+        return result.replace('\\', '/')
+    else:
+        return result
+
+
+def report(msg):
+    sys.stderr.write(msg + '\n')
+    sys.stderr.flush()
+
+
+def report_run_cmd(cmd, shell=False, *args, **kwargs):
+    """
+    Print a command, then executes it using subprocess.check_call.
+    """
+    report('Running: %s' % ((cmd if shell else shquote_cmd(cmd)),))
+    sys.stderr.flush()
+    subprocess.check_call([str(c) for c in  cmd], shell=shell, *args, **kwargs)
+
+
+def mkdirp(path):
+    """Create directory path if it does not already exist."""
+    try:
+        os.makedirs(path)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+
+
+def rmtree(path):
+    """
+    Remove directory path and all its subdirectories. This differs from
+    shutil.rmtree() in that it tries to adjust permissions so that deletion
+    will succeed.
+    """
+    # Some files will not be deletable, so we set permissions that allow
+    # deletion before we try deleting files.
+    for root, dirs, files in os.walk(path):
+        os.chmod(root, 0o755)
+        for f in files:
+            p = os.path.join(root, f)
+            os.chmod(p, 0o644)
+            os.unlink(p)
+    # At this point, we should have a tree of deletable directories.
+    shutil.rmtree(path)
+
+
+def safe_pjoin(dirname, *args):
+    """
+    Join path components with os.path.join, skipping the first component
+    if it is None.
+    """
+    if dirname is None:
+        return os.path.join(*args)
+    else:
+        return os.path.join(dirname, *args)
+
+
+def _shquote_impl(txt, escaped_chars, quoted_chars):
+    quoted = re.sub(escaped_chars, r'\\\1', txt)
+    if len(quoted) == len(txt) and not quoted_chars.search(txt):
+        return txt
+    else:
+        return '"' + quoted + '"'
+
+
+_SHQUOTE_POSIX_ESCAPEDCHARS = re.compile(r'(["`$\\])')
+_SHQUOTE_POSIX_QUOTEDCHARS = re.compile('[|&;<>()\' \t\n]')
+
+
+def shquote_posix(txt):
+    """Return txt, appropriately quoted for POSIX shells."""
+    return _shquote_impl(
+        txt, _SHQUOTE_POSIX_ESCAPEDCHARS, _SHQUOTE_POSIX_QUOTEDCHARS)
+
+
+_SHQUOTE_WINDOWS_ESCAPEDCHARS = re.compile(r'(["\\])')
+_SHQUOTE_WINDOWS_QUOTEDCHARS = re.compile('[ \t\n]')
+
+
+def shquote_windows(txt):
+    """Return txt, appropriately quoted for Windows's cmd.exe."""
+    return _shquote_impl(
+        txt.replace('%', '%%'),
+        _SHQUOTE_WINDOWS_ESCAPEDCHARS, _SHQUOTE_WINDOWS_QUOTEDCHARS)
+
+
+def shquote(txt):
+    """Return txt, appropriately quoted for use in a shell command."""
+    if os.name in set(('nt', 'os2', 'ce')):
+        return shquote_windows(txt)
+    else:
+        return shquote_posix(txt)
+
+
+def shquote_cmd(cmd):
+    """Convert a list of shell arguments to an appropriately quoted string."""
+    return ' '.join(map(shquote, cmd))
+
 
 def relative_if_possible(path, relative_to):
         path = os.path.normpath(path)
@@ -17,47 +259,149 @@ def relative_if_possible(path, relative_to):
             return  path
 
 
-def common_init(jobs=None):
-    # For information
+
+
+def first_true(*args):
+    for a in args:
+        if a:
+            return a
+    return None
+
+
+def first_nonnull(*args):
+    for a in args:
+        if a:
+            return a
+    return None
+
+
+class Worker:
+    def __init__(self,args,clean,clobber,workdir,jobs,cachefile,llvmsrcroot):
+        self.args=args
+        self.clean =clean
+        self.clobber=clobber
+        self.workdir=workdir
+        self.jobs=jobs
+        self.cachefile =cachefile
+        self.llvmsrcroot=llvmsrcroot
+
+    def in_llvmsrc(self, path):
+        return os.path.join(self.llvmsrcroot, path)
+
+    def in_workdir(self, path):
+        return os.path.join(self.workdir, path)
+
+    @contextmanager
+    def step(self, step_name, halt_on_fail=False):
+        with step(step_name,halt_on_fail=halt_on_fail) as s:
+            yield s
+
+    def run_cmake(self, cmakeargs):
+        report_run_cmd(['cmake', *cmakeargs])
+
+    def run_ninja(args, targets, ccache_stats=False, **kwargs):
+        cmd = ['ninja', *targets]
+        if args.jobs:
+            args .append(f'-j{args.jobs}')
+        if ccache_stats:
+                report_run_cmd(['ccache', '-z'])
+        report_run_cmd(cmd, **kwargs)
+        if ccache_stats:
+                report_run_cmd(['ccache', '-sv'])
+
+    def checkout (self,giturl, sourcepath):
+            return checkout(giturl, sourcepath)
+
+ 
+
+
+
+
+@contextmanager
+def run(scriptname, llvmsrcroot, parser=None ,clobberpaths=[], workerjobs=None):
     for k,v in os.environ.items():
         print(f"{k}={v}")
 
-    jobs_default = None
-    if jobsenv := os.environ.get('BUILDBOT_JOBS'):
-                jobs_default = int(jobsenv)
-    if not jobs_default:
-            jobs_default = jobs
+    os.environ['NINJA_STATUS'] = "[%p/%es :: %u->%r->%f (of %t)] "
 
-    parser = argparse.ArgumentParser(allow_abbrev=True, description="Run the buildbot builder configuration; when executed without arguments, builds the llvm-project checkout this file is in, using cwd to write all files")
-    parser.add_argument('--jobs', '-j', default=jobs_default, help='Override the number fo default jobs')
-    parser.add_argument('--clean', type=bool, default=os.environ.get('BUILDBOT_CLEAN'), help='Whether to delete source-, build-, and install-dirs (i.e. remove any files leftover from a previous run, if any) before running')
-    parser.add_argument('--clobber', type=bool, default=os.environ.get('BUILDBOT_CLOBBER'), help='Whether to delete build- and install-dirs before running')
-    parser.add_argument('--workdir', help="Use this dir as workdir (default: current working directory)" )
-    return parser
+    jobs_default = first_true(  os.environ.get('BUILDBOT_JOBS') , workerjobs )
+
+
+
+    stem = pathlib.Path(scriptname).stem
+
+
+    parser =  parser or argparse.ArgumentParser(allow_abbrev=True, description="Run the buildbot builder configuration; when executed without arguments, builds the llvm-project checkout this file is in, using cwd to write all files")
+    parser.add_argument('--jobs', '-j', default=jobs_default, help='Override the number of default jobs')
+    parser.add_argument('--clean', type=bool, default=os.environ.get('BUILDBOT_CLEAN') , help='Whether to delete source-, build-, and install-dirs (i.e. remove any files leftover from a previous run, if any) before running')
+    parser.add_argument('--clobber', type=bool, default=os.environ.get('BUILDBOT_CLOBBER') or os.environ.get('BUILDBOT_CLEAN_OBJ'), help='Whether to delete build- and install-dirs before running')
+    parser.add_argument('--workdir', default=f'{stem}.workdir', help="Use this dir as workdir")
+    parser.add_argument('--cachefile', default=relative_if_possible(pathlib.Path(scriptname).with_suffix('.cmake'), llvmsrcroot), help='CMake cache seed')
+    args = parser.parse_args()
+
+    print("cwd:",os.getcwd())
+
+    workdir =  args.workdir
+    clobber = args.clobber
+    cachefile = os.path.join(llvmsrcroot, args.cachefile)
+    oldcwd = os.getcwd()
+
+
+    prevcachepath = os.path.join(workdir, 'prevcache.cmake')
+    if cachefile and os.path.exists(prevcachepath):
+        # Force clobber if cache file has changed; a new cachefile does not override entries already present in CMakeCache.txt
+        if   not filecmp.cmp( os.path.join(llvmsrcroot, args.cachefile), prevcachepath, shallow=False):
+            clobber = True
+        #shutil.copyfile(cachefile)
+
+    w = Worker(args, clean=args.clean , clobber=clobber, workdir=workdir, jobs=args.jobs, cachefile=cachefile, llvmsrcroot=llvmsrcroot)
+
+    #if workdir:
+    if args.clean:
+            with w.step(f'clean'):
+                clean_dir(workdir)
+    elif clobber:
+       with w.step(f'clobber'):
+            for d in clobberpaths:
+                    clean_dir(os.path.join(workdir, d))
+            os.path.unlink(prevcachepath)
+
+    os.makedirs(workdir, exist_ok=True)
+    os.chdir(workdir)
+
+    # Remember used cachefile in case it changes
+    if cachefile:
+        shutil.copy2(os.path.join(oldcwd,llvmsrcroot, args.cachefile), os.path.join(oldcwd, prevcachepath ))
+
+    yield w
+    #else:
+    #    with tempfile.TemporaryDirectory(prefix = stem) as tmpdir:
+    #                os.chdir(tmpdir)
+    #                yield Worker(args)
+
+
+
+
 
 
 @contextmanager
 def step(step_name, halt_on_fail=False):
     sys.stderr.flush()
     sys.stdout.flush()
-    util.report('@@@BUILD_STEP {}@@@'.format(step_name))
+    report(f'@@@BUILD_STEP {step_name}@@@')
     if halt_on_fail:
-        util.report('@@@HALT_ON_FAILURE@@@')
+        report('@@@HALT_ON_FAILURE@@@')
     try:
         yield
     except Exception as e:
         if isinstance(e, subprocess.CalledProcessError):
-            util.report(
-                '{} exited with return code {}.'.format(e.cmd, e.returncode)
-            )
+            report(f'{e.cmd} exited with return code {e.returncode}.' )
+            report('@@@STEP_FAILURE@@@')
         else:
-            util.report('The build step threw an exception...')
             traceback.print_exc()
-
-        util.report('@@@STEP_FAILURE@@@')
+            report('@@@STEP_EXCEPTION@@@')
         if halt_on_fail:
             exit(1)
-
 
 
 def get_steps(makefile):
@@ -68,6 +412,7 @@ def get_steps(makefile):
     except:
         return []
 
+
 def build_make_cmd(makefile, target, make_vars={}):
     make_cmd = ['make', '-f', makefile]
     if not target is None:
@@ -76,22 +421,15 @@ def build_make_cmd(makefile, target, make_vars={}):
         make_cmd += ["{}={}".format(k, v)]
     return make_cmd
 
+
 def capture_cmd_stdout(cmd, **kwargs):
     return subprocess.run(cmd, shell=False, check=True, stdout=subprocess.PIPE, **kwargs).stdout
 
+
 def run_command(cmd, **kwargs):
-    util.report_run_cmd(cmd, **kwargs)
+    report_run_cmd(cmd, **kwargs)
 
 
-def run_ninja(args, targets, ccache_stats=False, **kwargs):
-    cmd = ['ninja', *targets]
-    if args.jobs:
-        args .append(f'-j{args.jobs}')
-    if ccache_stats:
-            util.report_run_cmd(['ccache', '-z'])
-    util.report_run_cmd(cmd, **kwargs)
-    if ccache_stats:
-            util.report_run_cmd(['ccache', '-sv'])
 
 
 
@@ -118,5 +456,5 @@ def clean_on_request(args, always=[],on_clobber=[],on_clean=[]):
 
     for d in cleanset:
         with step(f'delete-{os.path.basename(d)}'):
-          util.clean_dir(d)
+          clean_dir(d)
 
--- polly/.buildbot/polly-x86_64-linux-test-suite.py
+++ polly/.buildbot/polly-x86_64-linux-test-suite.py
@@ -12,80 +12,55 @@ import worker
 
 
 
-parser = worker.common_init()
-parser.add_argument('--cachefile', default=worker.relative_if_possible(pathlib.Path(__file__).with_suffix('.cmake'), llvmsrcroot), help='CMake cache seed')
-args, _ = parser.parse_known_args()
-
-if args.workdir:
- os.chdir(args.workdir)
-
-cwd = os.getcwd()
-
-
-
-
-
-buildbot_buildername = os.environ.get('BUILDBOT_BUILDERNAME')
-buildbot_revision = os.environ.get('BUILDBOT_REVISION', 'origin/main')
-
-
-os.environ['NINJA_STATUS'] = "[%p/%es :: %u->%r->%f (of %t)] "
-
-llvmbuilddir = "build-llvm"
+llvmbuilddir = "llvm.build"
+llvminstalldir = 'llvm.install'
 testsuitesrcdir = "testsuite.src"
-testsuitebuilddir = "build-testsuite"
-llvminstalldir = 'install-llvm'
-print(f"Using build directory: {cwd}")
+testsuitebuilddir = "testsuite.build"
 
-# NEVER clean llvmsrcroot or cwd!
-worker.clean_on_request(args, always=[llvminstalldir,testsuitebuilddir],on_clobber=[llvmbuilddir],on_clean=[testsuitesrcdir])
+with worker.run(scriptname=__file__,llvmsrcroot=llvmsrcroot,clobberpaths = [llvmbuilddir,testsuitebuilddir, llvminstalldir]) as w:
+    with w.step('configure-llvm', halt_on_fail=True):
+        cmakeargs =[
+            '-S',  w.in_llvmsrc( 'llvm'),
+            '-B', llvmbuilddir,
+            '-G', 'Ninja',
+            '-C',  w.in_llvmsrc( w.cachefile),
+            f'-DCMAKE_INSTALL_PREFIX={llvminstalldir}'
+        ]
+        if w.jobs:
+            cmakeargs.append(      f'-DLLVM_LIT_ARGS=-sv;-j{w.jobs}')
+        w.run_cmake(cmakeargs)
 
+    with w.step('build-llvm', halt_on_fail=True):
+        w.run_ninja(['-C', llvmbuilddir], ccache_stats=True)
 
-with worker.step('configure-llvm', halt_on_fail=True):
-    cmd = ['cmake',
-        '-S', os.path.join(llvmsrcroot,'llvm'),
-        '-B', llvmbuilddir,
-        '-G', 'Ninja',
-        '-C', os.path.join(llvmsrcroot, args.cachefile),
-        f'-DCMAKE_INSTALL_PREFIX={llvminstalldir}'
-    ]
-    if args.jobs:
-        cmd.append(f'-DLLVM_LIT_ARGS=-svj{args.jobs}')
-    worker.run_command(cmd)
+    with w.step('check-polly'):
+        w.run_ninja(['-C', llvmbuilddir, 'check-polly'])
 
-with worker.step('build-llvm', halt_on_fail=True):
-    worker.run_ninja(args, ['-C', llvmbuilddir], ccache_stats=True)
+    with w.step('install-llvm', halt_on_fail=True):
+        w.run_ninja(['-C', llvmbuilddir, 'install'], ccache_stats=True)
 
-with worker.step('check-polly'):
-    worker.run_ninja(args, ['-C', llvmbuilddir, 'check-polly'], ccache_stats=True)
+    with w. step('checkout-testsuite', halt_on_fail=True):
+        w. checkout('https://github.com/llvm/llvm-test-suite',testsuitesrcdir)
 
-with worker.step('install-llvm', halt_on_fail=True):
-    worker.run_ninja(args, ['-C', llvmbuilddir, 'install'], ccache_stats=True)
+    with w.step('configure-testsuite', halt_on_fail=True):
+        jobsarg =  f';-j{w.jobs}' if w.jobs else ''
+        w.run_cmake(['cmake',
+            '-S', testsuitesrcdir,
+            '-B', testsuitebuilddir,
+            '-G', 'Ninja',
+            '-DCMAKE_BUILD_TYPE=Release',
+            f'-DCMAKE_C_COMPILER={os.path.abspath(llvminstalldir)}/bin/clang',
+            f'-DCMAKE_CXX_COMPILER={os.path.abspath(llvminstalldir)}/bin/clang++',
+            f'-DTEST_SUITE_LIT={os.path.abspath(llvmbuilddir)}/bin/llvm-lit',
+            f'-DTEST_SUITE_LLVM_SIZE={os.path.abspath(llvmbuilddir)}/bin/llvm-size',
+            "-DTEST_SUITE_EXTRA_C_FLAGS=-Wno-unused-command-line-argument -mllvm -polly",
+            "-DTEST_SUITE_EXTRA_CXX_FLAGS=-Wno-unused-command-line-argument -mllvm -polly",
+            '-DLLVM_LIT_ARGS=-sv{jobsarg};-o;report.json'
+        ])
 
-with worker. step('clone-testsuite', halt_on_fail=True):
-    worker.checkout('https://github.com/llvm/llvm-test-suite',testsuitesrcdir)
+    with w.step('build-testsuite', halt_on_fail=True):
+        w. run_ninja( ['-C', testsuitebuilddir], ccache_stats=True)
 
-with worker.step('configure-testsuite', halt_on_fail=True):
-    cmd = ['cmake',
-        '-S', testsuitesrcdir,
-        '-B', testsuitebuilddir,
-        '-G', 'Ninja',
-        '-C', os.path.join(llvmsrcroot, args.cachefile),
-        '-DCMAKE_BUILD_TYPE=Release',
-        f'-DCMAKE_C_COMPILER={os.path.abspath(llvminstalldir)}/bin/clang',
-        f'-DCMAKE_CXX_COMPILER={os.path.abspath(llvminstalldir)}/bin/clang++',
-        f'-DTEST_SUITE_LIT={os.path.abspath(llvmbuilddir)}/bin/llvm-lit',
-        f'-DTEST_SUITE_LLVM_SIZE={os.path.abspath(llvmbuilddir)}/bin/llvm-size',
-        "-DTEST_SUITE_EXTRA_C_FLAGS=-Wno-unused-command-line-argument -mllvm -polly",
-        "-DTEST_SUITE_EXTRA_CXX_FLAGS=-Wno-unused-command-line-argument -mllvm -polly",
-    ]
-    if args.jobs:
-        cmd.append(f'-DLLVM_LIT_ARGS=-svj{args.jobs};-o;report.json')
-    worker.run_command(cmd)
-
-with worker.step('build-testsuite', halt_on_fail=True):
-    worker. run_ninja(args, ['-C', testsuitebuilddir], ccache_stats=True)
-
-with worker.step('check-testsuite'):
-    worker.run_ninja(args, ['-C', testsuitebuilddir, 'check'], ccache_stats=True)
+    with w.step('check-testsuite'):
+        w.run_ninja( ['-C', testsuitebuilddir, 'check'])
 
