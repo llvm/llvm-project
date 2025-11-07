d899e1eb8d5a7bee7be1bdccab114b081da7564d
Sai Deepak <deepaksana9030@gmail.com>
2025-11-07 17:45:23 +0000

    test(readability-constant-operand-order): add CHECK-LEFT expectations for ok_eq

--- clang-tools-extra/test/clang-tidy/checkers/readability/constant-operand-order.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/readability/constant-operand-order.cpp
@@ -1,9 +1,12 @@
+// RUN: %check_clang_tidy %s readability-constant-operand-order %t -- -- -std=c++17
+// RUN: %check_clang_tidy -check-suffixes=LEFT %s readability-constant-operand-order %t -- \
+// RUN:   --config="{CheckOptions:[{key: readability-constant-operand-order.PreferredConstantSide, value: Left}, {key: readability-constant-operand-order.BinaryOperators, value: '==,!='}]}" -- -std=c++17
+
 void ok_eq(int a) {
   if (a == 0) {} // CHECK-MESSAGES-NOT: readability-constant-operand-order
                  // CHECK-LEFT-MESSAGES: warning: constant operand should be on the Left side
                  // CHECK-LEFT-FIXES: if (0 == a) {} //
 }
-
 
 void swap_eq(int a) {
   if (0 == a) {} // CHECK-MESSAGES: warning: constant operand should be on the Right side
