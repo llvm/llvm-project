70dc7b74fb79cb7f4b9352c0f3c7195eecf47c09
Susikrishna <krishnasusi323@gmail.com>
2025-11-07 18:55:07 +0530

    [LLVM][InstCombine] Simplify zext(sub(0, trunc(x))) -> and(sub(0, x), (bitwidth-1))

--- llvm/lib/Transforms/InstCombine/InstCombineCasts.cpp
+++ llvm/lib/Transforms/InstCombine/InstCombineCasts.cpp
@@ -1217,6 +1217,22 @@ static bool canEvaluateZExtd(Value *V, Type *Ty, unsigned &BitsToClear,
 }
 
 Instruction *InstCombinerImpl::visitZExt(ZExtInst &Zext) {
+  {
+    Value *TruncSrc = nullptr;
+    if (match(&Zext, m_ZExt(m_Sub(m_Zero(), m_Trunc(m_Value(TruncSrc)))))) {
+      IRBuilder<> Builder(&Zext);
+      Type *Ty = TruncSrc->getType();
+      unsigned BitWidth = Ty->getScalarSizeInBits();
+      unsigned MaskVal = BitWidth - 1;
+
+      Value *Zero = ConstantInt::get(Ty, 0);
+      Value *Neg = Builder.CreateSub(Zero, TruncSrc);
+      Value *Mask = ConstantInt::get(Ty, MaskVal);
+      Value *Masked = Builder.CreateAnd(Neg, Mask);
+      return replaceInstUsesWith(Zext, Masked);
+    }
+  }
+
   // If this zero extend is only used by a truncate, let the truncate be
   // eliminated before we try to optimize this zext.
   if (Zext.hasOneUse() && isa<TruncInst>(Zext.user_back()) &&
