aa8cce622f3f8a501a940cd01b9d005ba3fbcaa5
ahmed <ahmednour.mohamed2012@gmail.com>
2025-11-07 09:02:55 +0200

    Add -verify

--- clang/test/CodeGen/X86/mmx-inline-asm-error.c
+++ clang/test/CodeGen/X86/mmx-inline-asm-error.c
@@ -1,5 +1,5 @@
 // RUN: %clang_cc1 -verify -triple x86_64-unknown-unknown -emit-llvm-only %s
-// RUN: %clang_cc1 -triple x86_64-unknown-unknown -emit-llvm-only -fopenmp %s
+// RUN: %clang_cc1 -verify -triple x86_64-unknown-unknown -emit-llvm-only -fopenmp %s
 typedef int vec256 __attribute__((ext_vector_type(8)));
 
 vec256 foo(vec256 in) {
