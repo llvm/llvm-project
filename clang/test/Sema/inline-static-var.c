28b67f8d7ce81c811f7ccbc3f38ad0159e097409
Susikrishna <krishnasusi323@gmail.com>
2025-11-07 10:54:49 +0530

    [Clang][Sema] Use Linux triple for inline-static-var.c to ensure consistent diagnostics

--- clang/test/Sema/inline-static-var.c
+++ clang/test/Sema/inline-static-var.c
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -std=c11 -pedantic-errors -verify %s
+// RUN: %clang_cc1 -triple x86_64-pc-linux-gnu -std=c11 -pedantic-errors -verify %s
 
 inline void f(void) { // expected-note {{use 'static' to give inline function 'f' internal linkage}}
   static int x; // expected-error {{non-constant static local variable in inline function may be different in different files}}
