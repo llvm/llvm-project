7f346b255e521bbd835e491a3ea14dd72d8b0c0f
Tsche <che@pydong.org>
2025-11-08 23:31:03 +0100

    fix formatting issues

--- libcxx/utils/generate_feature_test_macro_components.py
+++ libcxx/utils/generate_feature_test_macro_components.py
@@ -766,7 +766,7 @@ feature_test_macros = [
             "name": "__cpp_lib_integer_sequence",
             "values": {
                 "c++14": 201304,
-                "c++26": 202511,  # P1789R3 Library Support for Expansion Statements 
+                "c++26": 202511,  # P1789R3 Library Support for Expansion Statements
             },
             "headers": ["utility"],
         },
