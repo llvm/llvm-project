486cc8a92f71166d4045d084247a6c3a64e9719c
AZero13 <gfunni234@gmail.com>
2025-11-11 10:12:05 -0500

    [AArch64] AArch64::ADCWr and friends map to their sForm, but their sForms do not map to themselves
    Fix this inconsistency.

--- llvm/lib/Target/AArch64/AArch64InstrInfo.cpp
+++ llvm/lib/Target/AArch64/AArch64InstrInfo.cpp
@@ -1979,6 +1979,10 @@ static unsigned sForm(MachineInstr &Instr) {
   case AArch64::BICSXrr:
   case AArch64::BICSWrs:
   case AArch64::BICSXrs:
+  case AArch64::ADCSWr:
+  case AArch64::ADCSXr:
+  case AArch64::SBCSWr:
+  case AArch64::SBCSXr:
     return Instr.getOpcode();
 
   case AArch64::ADDWrr:
