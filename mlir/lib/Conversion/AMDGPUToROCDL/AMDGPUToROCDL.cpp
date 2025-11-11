1f79bdd99637a7be7765df808c3e3a63199ca524
Erick Ochoa <erick.ochoalopez@amd.com>
2025-11-11 10:38:08 -0500

    Add case for pk16.bf16.bf6

--- mlir/lib/Conversion/AMDGPUToROCDL/AMDGPUToROCDL.cpp
+++ mlir/lib/Conversion/AMDGPUToROCDL/AMDGPUToROCDL.cpp
@@ -1882,10 +1882,21 @@ LogicalResult ScaledExtPacked816OpLowering::matchAndRewrite(
     rewriter.replaceOp(op, newOp);
     return success();
   }
+  if (isa<Float6E3M2FNType>(srcElemType) and isa<BFloat16Type>(tgtElemType)) {
+    // CvtPkScale16Bf16Bf6Op
+    Value source = adaptor.getSource();
+    VectorType v3xi32 = VectorType::get(3, i32);
+    Value castedSource = LLVM::BitcastOp::create(rewriter, loc, v3xi32, source);
+
+    auto newOp = ROCDL::CvtPkScalePk16Bf16Bf6Op::create(
+        rewriter, loc, op.getResult().getType(), castedSource, castedScale,
+        scaleSel);
+    rewriter.replaceOp(op, newOp);
+    return success();
+  }
 
   /*
 
-  CvtPkScale16Bf16Bf6Op
 
   CvtPkScale16F32Fp6Op
   CvtPkScale16F32Bf6Op
