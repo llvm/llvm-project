7c921f805899c8d46cec3c5a268373bf0174e01c
Thirumalai-Shaktivel <thirumalaishaktivel@gmail.com>
2025-11-11 08:38:30 +0000

    Fix the tests

--- flang/test/Integration/prefetch.f90
+++ flang/test/Integration/prefetch.f90
@@ -14,24 +14,24 @@
 
 subroutine test_prefetch_01()
     ! LLVM: {{.*}} = alloca i32, i64 1, align 4
-    ! LLVM: %[[L_J:.*]] = alloca i32, i64 1, align 4
-    ! LLVM: %[[L_I:.*]] = alloca i32, i64 1, align 4
-    ! LLVM: %[[L_A:.*]] = alloca [256 x i32], i64 1, align 4
+    ! LLVM: %[[VAR_J:.*]] = alloca i32, i64 1, align 4
+    ! LLVM: %[[VAR_I:.*]] = alloca i32, i64 1, align 4
+    ! LLVM: %[[VAR_A:.*]] = alloca [256 x i32], i64 1, align 4
 
     integer :: i, j
     integer :: a(256)
 
     a = 23
-    ! LLVM: call void @llvm.prefetch.p0(ptr %6, i32 0, i32 3, i32 1)
+    ! LLVM: call void @llvm.prefetch.p0(ptr %[[VAR_A]], i32 0, i32 3, i32 1)
     !dir$ prefetch a
     i = sum(a)
-    ! LLVM: %[[L_LOAD:.*]] = load i32, ptr %5, align 4
-    ! LLVM: %[[L_ADD:.*]] = add nsw i32 %[[L_LOAD]], 64
-    ! LLVM: %[[L_GEP:.*]] = getelementptr i32, ptr %[[L_A]], i64 {{.*}}
 
-    ! LLVM: call void @llvm.prefetch.p0(ptr %[[L_GEP]], i32 0, i32 3, i32 1)
-    ! LLVM: call void @llvm.prefetch.p0(ptr %[[L_J]], i32 0, i32 3, i32 1)
+    ! LLVM: %[[LOAD_I:.*]] = load i32, ptr %[[VAR_I]], align 4
+    ! LLVM: %{{.*}} = add nsw i32 %[[LOAD_I]], 64
+    ! LLVM: %[[GEP_A:.*]] = getelementptr i32, ptr %[[VAR_A]], i64 {{.*}}
 
+    ! LLVM: call void @llvm.prefetch.p0(ptr %[[GEP_A]], i32 0, i32 3, i32 1)
+    ! LLVM: call void @llvm.prefetch.p0(ptr %[[VAR_J]], i32 0, i32 3, i32 1)
     do i = 1, (256 - 64)
       !dir$ prefetch a(i+64), j
       a(i) = a(i-32) + a(i+32) + j
