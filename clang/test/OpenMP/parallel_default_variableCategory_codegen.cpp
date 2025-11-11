d4ee726f51d4d3b1f02c85ff614231bef3d53907
Sunil Kuravinakop <kuravina@pe31.hpc.amslabs.hpecorp.net>
2025-11-11 03:52:46 -0600

    Taking care of llvm-lit failure on windows.

--- clang/test/OpenMP/parallel_default_variableCategory_codegen.cpp
+++ clang/test/OpenMP/parallel_default_variableCategory_codegen.cpp
@@ -1,4 +1,4 @@
-// RUN: %clangxx -DOMP60 -Xclang -verify -Wno-vla -fopenmp -fopenmp-version=60 -x c++ -S -emit-llvm %s -o - | FileCheck --check-prefixes=OMP60 %s
+// RUN: %clangxx -Xclang -verify -Wno-vla -fopenmp -fopenmp-version=60 -x c++ -S -emit-llvm %s -o - | FileCheck %s
 // expected-no-diagnostics
 #ifndef HEADER
 #define HEADER
@@ -73,41 +73,41 @@ int main (int argc, char **argv) {
 
 #endif
 
-// OMP60-LABEL: define {{.*}}main.omp_outlined{{.*}}
-// OMP60-NEXT:  entry:
-// OMP60: %x.addr = alloca{{.*}}
-// OMP60: %xPtr = alloca{{.*}}
-// OMP60: store ptr null, ptr %xPtr{{.*}}
-// OMP60: store ptr %xPtr{{.*}}
-// OMP60: store ptr %x.addr{{.*}}
-// OMP60-NEXT:  {{.*}}call{{.*}}__kmpc_omp_task_alloc{{.*}}
-// OMP60: ret void
+// CHECK-LABEL: define {{.*}}main.omp_outlined{{.*}}
+// CHECK-NEXT:  entry:
+// CHECK: %x.addr = alloca{{.*}}
+// CHECK: %xPtr = alloca{{.*}}
+// CHECK: store ptr null, ptr %xPtr{{.*}}
+// CHECK: store ptr %xPtr{{.*}}
+// CHECK: store ptr %x.addr{{.*}}
+// CHECK-NEXT:  {{.*}}call{{.*}}__kmpc_omp_task_alloc{{.*}}
+// CHECK: ret void
 //
-// OMP60: define {{.*}}main.omp_outlined{{.*}}
-// OMP60-NEXT:  entry:
-// OMP60: %i.addr = alloca{{.*}}
-// OMP60-NEXT:  %n.addr = alloca{{.*}}
-// OMP60-NEXT:  %aggregate.addr = alloca{{.*}}
-// OMP60-NEXT:  %x.addr = alloca{{.*}}
-// OMP60-NEXT:  %arr.addr = alloca{{.*}}
-// OMP60: [[TMP0:%.*]] = load{{.*}}%i.addr{{.*}}
-// OMP60-NEXT:  [[TMP1:%.*]] = load{{.*}}%n.addr{{.*}}
-// OMP60-NEXT:  [[TMP2:%.*]] = load{{.*}}%aggregate.addr{{.*}}
-// OMP60-NEXT:  [[TMP3:%.*]] = load{{.*}}%x.addr{{.*}}
-// OMP60-NEXT:  [[TMP4:%.*]] = load{{.*}}%arr.addr{{.*}}
-// OMP60: store ptr [[TMP2]]{{.*}}
-// OMP60-NEXT:  {{.*}}call{{.*}}__kmpc_omp_task_alloc{{.*}}
-// OMP60: store ptr [[TMP2]]{{.*}}
-// OMP60: store ptr [[TMP3]]{{.*}}
-// OMP60-NEXT:  {{.*}}call{{.*}}__kmpc_omp_task_alloc{{.*}}
-// OMP60: store ptr [[TMP4]]{{.*}}
-// OMP60-NEXT:  {{.*}}call{{.*}}__kmpc_omp_task_alloc{{.*}}
-// OMP60: store ptr [[TMP4]]{{.*}}
-// OMP60: store ptr [[TMP3]]{{.*}}
-// OMP60-NEXT:  {{.*}}call{{.*}}__kmpc_omp_task_alloc{{.*}}
-// OMP60: store ptr [[TMP0]]{{.*}}
-// OMP60: store ptr [[TMP1]]{{.*}}
-// OMP60: store ptr [[TMP2]]{{.*}}
-// OMP60: store ptr [[TMP3]]{{.*}}
-// OMP60-NEXT:  {{.*}}call{{.*}}__kmpc_omp_task_alloc{{.*}}
-// OMP60: ret void
+// CHECK: define {{.*}}main.omp_outlined{{.*}}
+// CHECK-NEXT:  entry:
+// CHECK-DAG: %i.addr = alloca{{.*}}
+// CHECK-DAG:  %n.addr = alloca{{.*}}
+// CHECK-DAG:  %aggregate.addr = alloca{{.*}}
+// CHECK-DAG:  %x.addr = alloca{{.*}}
+// CHECK-DAG:  %arr.addr = alloca{{.*}}
+// CHECK: [[TMP0:%.*]] = load{{.*}}%i.addr{{.*}}
+// CHECK-NEXT:  [[TMP1:%.*]] = load{{.*}}%n.addr{{.*}}
+// CHECK-NEXT:  [[TMP2:%.*]] = load{{.*}}%aggregate.addr{{.*}}
+// CHECK-NEXT:  [[TMP3:%.*]] = load{{.*}}%x.addr{{.*}}
+// CHECK-NEXT:  [[TMP4:%.*]] = load{{.*}}%arr.addr{{.*}}
+// CHECK: store ptr [[TMP2]]{{.*}}
+// CHECK-NEXT:  {{.*}}call{{.*}}__kmpc_omp_task_alloc{{.*}}
+// CHECK: store ptr [[TMP2]]{{.*}}
+// CHECK: store ptr [[TMP3]]{{.*}}
+// CHECK-NEXT:  {{.*}}call{{.*}}__kmpc_omp_task_alloc{{.*}}
+// CHECK: store ptr [[TMP4]]{{.*}}
+// CHECK-NEXT:  {{.*}}call{{.*}}__kmpc_omp_task_alloc{{.*}}
+// CHECK: store ptr [[TMP4]]{{.*}}
+// CHECK: store ptr [[TMP3]]{{.*}}
+// CHECK-NEXT:  {{.*}}call{{.*}}__kmpc_omp_task_alloc{{.*}}
+// CHECK: store ptr [[TMP0]]{{.*}}
+// CHECK: store ptr [[TMP1]]{{.*}}
+// CHECK: store ptr [[TMP2]]{{.*}}
+// CHECK: store ptr [[TMP3]]{{.*}}
+// CHECK-NEXT:  {{.*}}call{{.*}}__kmpc_omp_task_alloc{{.*}}
+// CHECK: ret void
