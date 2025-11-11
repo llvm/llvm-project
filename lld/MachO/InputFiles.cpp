6ec35a9bd309e6a9d0b329c3e2fb481584cf9a5d
prabhukr <prabhukr@google.com>
2025-11-11 11:59:19 -0800

    Fix the error messages as suggested.

--- lld/MachO/InputFiles.cpp
+++ lld/MachO/InputFiles.cpp
@@ -809,15 +809,15 @@ void ObjFile::parseSymbols(ArrayRef<typename LP::section> sectionHeaders,
 
     if ((sym.n_type & N_TYPE) == N_SECT) {
       if (sym.n_sect == 0) {
-        fatal("Section symbol " + StringRef(strtab + sym.n_strx) + " in " +
+        fatal("section symbol " + StringRef(strtab + sym.n_strx) + " in " +
               toString(this) + " has an invalid section index [0]");
       }
       if (sym.n_sect > sections.size()) {
-        fatal("Section symbol " + StringRef(strtab + sym.n_strx) + " in " +
+        fatal("section symbol " + StringRef(strtab + sym.n_strx) + " in " +
               toString(this) + " has an invalid section index [" +
               Twine(static_cast<unsigned>(sym.n_sect)) +
               "] greater than the total number of sections [" +
-              Twine(sections.size()) + "].");
+              Twine(sections.size()) + "]");
       }
       Subsections &subsections = sections[sym.n_sect - 1]->subsections;
       // parseSections() may have chosen not to parse this section.
