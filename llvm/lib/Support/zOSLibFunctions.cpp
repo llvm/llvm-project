14a2af363130b821b1393e12b6d6e0262495f489
Sean Perry <perry@ca.ibm.com>
2025-11-12 14:59:10 +0000

    clean up make file & source

--- llvm/lib/Support/zOSLibFunctions.cpp
+++ llvm/lib/Support/zOSLibFunctions.cpp
@@ -21,45 +21,45 @@
 
 const char *signalName[] = {
   /*  0 */ nullptr,
-  /*  1 */ "Hangup", // SIGHUP - Posix
-  /*  2 */ "Interrupt", // SIGINT
-  /*  3 */ "Aborted", // SIGABRT
-  /*  4 */ "Illegal instruction", // SIGILL
-  /*  5 */ "Polling event", // SIGPOLL
-  /*  6 */ "Socket data available", // SIGURG
-  /*  7 */ "Stopped (signal)", // SIGSTOP - Posix
-  /*  8 */ "Floating point exception", // SIGFPE
-  /*  9 */ "Killed", // SIGKILL - Posix
-  /* 10 */ "Bus error", // SIGBUS
-  /* 11 */ "Segmentation fault", // SIGSEGV
-  /* 12 */ "Bad system call", // SIGSYS
-  /* 13 */ "Broken pipe", // SIGPIPE - Posix
-  /* 14 */ "Alarm clock", // SIGALRM - Posix
-  /* 15 */ "Terminated", // SIGTERM
-  /* 16 */ "User defined signal 1", // SIGUSR1
-  /* 17 */ "User defined signal 2", // SIGUSR2
-  /* 18 */ "Abend", // SIGABND
-  /* 19 */ "Continued", // SIGCONT - Posix
-  /* 20 */ "Child exited", // SIGCHLD - Posix
-  /* 21 */ "Stopped (tty input)", // SIGTTIN - Posix
-  /* 22 */ "Stopped (tty output)", // SIGTTOU - Posix
-  /* 23 */ "I/O complete", // SIGIO
-  /* 24 */ "Quit", // SIGQUIT - Posix
-  /* 25 */ "Stopped", // SIGTSTP - Posix
-  /* 26 */ "Trace/breakpoint trap", // SIGTRAP
-  /* 27 */ "I/O error", // SIGIOERR
-  /* 28 */ "Window changed", // SIGWINCH
-  /* 29 */ "CPU time limit exceeded", // SIGXCPU
-  /* 30 */ "File size limit exceeded", // SIGXFSZ
-  /* 31 */ "Virtual timer expired", // SIGVTALRM
-  /* 32 */ "Profiling timer expired",
-  /* 33 */ "OMVS subsystem shutdown",  // SIGDANGER
-  /* 34 */ "Thread stop", // SIGTHSTOP
-  /* 35 */ "Thread resume", // SIGTHCONT
+  /*  1 */ "Hangup",                     // SIGHUP
+  /*  2 */ "Interrupt",                  // SIGINT
+  /*  3 */ "Aborted",                    // SIGABRT
+  /*  4 */ "Illegal instruction",        // SIGILL
+  /*  5 */ "Polling event",              // SIGPOLL
+  /*  6 */ "Socket data available",      // SIGURG
+  /*  7 */ "Stopped (signal)",           // SIGSTOP
+  /*  8 */ "Floating point exception",   // SIGFPE
+  /*  9 */ "Killed",                     // SIGKILL
+  /* 10 */ "Bus error",                  // SIGBUS
+  /* 11 */ "Segmentation fault",         // SIGSEGV
+  /* 12 */ "Bad system call",            // SIGSYS
+  /* 13 */ "Broken pipe",                // SIGPIPE
+  /* 14 */ "Alarm clock",                // SIGALRM
+  /* 15 */ "Terminated",                 // SIGTERM
+  /* 16 */ "User defined signal 1",      // SIGUSR1
+  /* 17 */ "User defined signal 2",      // SIGUSR2
+  /* 18 */ "Abend",                      // SIGABND
+  /* 19 */ "Continued",                  // SIGCONT
+  /* 20 */ "Child exited",               // SIGCHLD
+  /* 21 */ "Stopped (tty input)",        // SIGTTIN
+  /* 22 */ "Stopped (tty output)",       // SIGTTOU
+  /* 23 */ "I/O complete",               // SIGIO
+  /* 24 */ "Quit",                       // SIGQUIT
+  /* 25 */ "Stopped",                    // SIGTSTP
+  /* 26 */ "Trace/breakpoint trap",      // SIGTRAP
+  /* 27 */ "I/O error",                  // SIGIOERR
+  /* 28 */ "Window changed",             // SIGWINCH
+  /* 29 */ "CPU time limit exceeded",    // SIGXCPU
+  /* 30 */ "File size limit exceeded",   // SIGXFSZ
+  /* 31 */ "Virtual timer expired",      // SIGVTALRM
+  /* 32 */ "Profiling timer expired",    // SIGPROF
+  /* 33 */ "OMVS subsystem shutdown",    // SIGDANGER
+  /* 34 */ "Thread stop",                // SIGTHSTOP
+  /* 35 */ "Thread resume",              // SIGTHCONT
   /* 36 */ nullptr,
-  /* 37 */ "Toggle syscall trace", // SIGTRACE
-  /* 38 */ nullptr, // SIGDCE
-  /* 39 */ "System dump", // SIGDUMP - Posix
+  /* 37 */ "Toggle syscall trace",       // SIGTRACE
+  /* 38 */ nullptr,                      // SIGDCE
+  /* 39 */ "System dump",                // SIGDUMP
 };
 
 // z/OS Unix System Services does not have strsignal() support, so the
