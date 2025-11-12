6a2f2d4992c6961671e38b7686cc3a9df0e82cee
BStott <Benjamin.Stott@sony.com>
2025-11-12 09:26:16 +0000

    Add #if __x86_64__ around target_clones attribute in the test

--- compiler-rt/test/tsan/target_clones_segfault.c
+++ compiler-rt/test/tsan/target_clones_segfault.c
@@ -1,9 +1,9 @@
 // https://github.com/llvm/llvm-project/issues/163369
 // RUN: %clang_tsan %s -o %t && %run %t
 
-__attribute__((target_clones("default,avx"))) static int
-has_target_clones(void) {
-  return 0;
-}
+#if __x86_64__
+__attribute__((target_clones("avx,default")))
+#endif
+static int has_target_clones(void) { return 0; }
 
 int main(void) { has_target_clones(); }
