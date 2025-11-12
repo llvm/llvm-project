4d87c937214aea1efd02f5e60f77ec3ecc12bb67
Ben <ben.dunbobbin@sony.com>
2025-11-12 12:19:44 +0000

    [DTLTO][PS5] Suppress system headers directory warning
    The PS5 driver warns under various circumstances if SDK directories
    can't be found.
    
    This warning is not applicable in ThinLTO codegen mode
    (-fthinlto-index=). Suppress it.
    
    The motivation for doing this is that we sometimes see this warning
    emitted when DTLTO invokes the compiler on a remote machine to do the
    LTO backend compilations (with -fthinlto-index=).
    
    Internal Ref: TOOLCHAIN-20592

--- clang/test/Driver/ps4-sdk-root.c
+++ clang/test/Driver/ps4-sdk-root.c
@@ -11,6 +11,9 @@
 ///
 /// The default <SDKROOT> for both headers and libraries is taken from the
 /// SCE_ORBIS_SDK_DIR environment variable.
+///
+/// In ThinLTO code generation mode (-fthinlto-index=) SDK files are not required
+/// so all warnings are suppressed.
 
 // RUN: echo "-### -Winvalid-or-nonexistent-directory -target x86_64-scei-ps4" > %t.rsp
 
@@ -30,6 +33,11 @@
 /// When compiling and linking, we should see a warnings about both missing
 /// headers and libraries.
 // RUN: env SCE_ORBIS_SDK_DIR=.. %clang @%t.rsp %s 2>&1 | FileCheck -check-prefixes=WARN-SYS-HEADERS,WARN-SYS-LIBS,NO-WARN %s
+
+/// -fthinlto-index= warning suppression.
+// RUN: %clang -O2 %s -flto=thin -c -o %t.o
+// RUN: llvm-lto -thinlto -o %t %t.o
+// RUN: env SCE_ORBIS_SDK_DIR=.. %clang @%t.rsp %t.o -fthinlto-index=%t.thinlto.bc 2>&1 | FileCheck -check-prefixes=NO-WARN %s
 
 /// If `-c`, `-S`, `-E` or `-emit-ast` is supplied, the existence check for SDK
 /// libraries is skipped because no linking will be performed. We only expect
