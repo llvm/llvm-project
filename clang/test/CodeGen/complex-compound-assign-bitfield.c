76b16c92b5efee9bc556673dd4e9a2a924da7f06
BStott <Benjamin.Stott@sony.com>
2025-11-12 14:16:24 +0000

    Clarify test

--- clang/test/CodeGen/complex-compound-assign-bitfield.c
+++ clang/test/CodeGen/complex-compound-assign-bitfield.c
@@ -8,18 +8,18 @@ struct Bits {
 
 int main(void) {
 // CHECK-LABEL: define dso_local i32 @main() #0 {
-    struct Bits b;
-    b.b = b.b += __builtin_complex(-1.0f, 0.0f);
-// CHECK: %bf.load = load i32, ptr %b, align 4
+    struct Bits x;
+    x.b += __builtin_complex(-1.0f, 0.0f);
+// CHECK: %bf.load = load i32, ptr %x, align 4
 // CHECK-NEXT: %bf.lshr = lshr i32 %bf.load, 30
 // CHECK-NEXT: %bf.clear = and i32 %bf.lshr, 1
 // CHECK-NEXT: %bf.cast = trunc i32 %bf.clear to i1
 // CHECK-NEXT: %conv = uitofp i1 %bf.cast to float
 
 // CHECK: %0 = zext i1 %tobool1 to i32
-// CHECK-NEXT: %bf.load2 = load i32, ptr %b, align 4
+// CHECK-NEXT: %bf.load2 = load i32, ptr %x, align 4
 // CHECK-NEXT: %bf.shl = shl i32 %0, 30
 // CHECK-NEXT: %bf.clear3 = and i32 %bf.load2, -1073741825
 // CHECK-NEXT: %bf.set = or i32 %bf.clear3, %bf.shl
-// CHECK-NEXT: store i32 %bf.set, ptr %b, align 4
+// CHECK-NEXT: store i32 %bf.set, ptr %x, align 4
 }
