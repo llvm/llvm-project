6b7a0155383d5b6b3ea9ea67e8ef87cdd68a8169
Nathan Gauër <brioche@google.com>
2025-11-12 21:45:56 +0100

    Revert "[clang][HLSL] Fix crash issue due to Twine usage"
    This reverts commit cc54ee854f8a081861eb75ad788934705fc54878.

--- clang/lib/Sema/SemaHLSL.cpp
+++ clang/lib/Sema/SemaHLSL.cpp
@@ -940,7 +940,8 @@ void SemaHLSL::checkSemanticAnnotation(
       DiagnoseAttrStageMismatch(SemanticAttr, ST, {llvm::Triple::Compute});
 
     if (SemanticAttr->getSemanticIndex() != 0) {
-      Twine PrettyName = "'" + SemanticAttr->getSemanticName() + "'";
+      Twine PrettyName =
+          std::string("'") + SemanticAttr->getSemanticName() + "'";
       Diag(SemanticAttr->getLoc(),
            diag::err_hlsl_semantic_indexing_not_supported)
           << PrettyName.str();
