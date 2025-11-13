bc8423d40099520cc39c82731ee9964fc3dfa1fd
Kevin Sala Penades <salapenades1@llnl.gov>
2025-11-13 11:00:12 -0800

    [Offload] Add device info for shared memory (#167817)

--- offload/plugins-nextgen/common/include/PluginInterface.h
+++ offload/plugins-nextgen/common/include/PluginInterface.h
@@ -869,6 +869,10 @@ struct GenericDeviceTy : public DeviceAllocatorTy {
   /// Get the unique identifier of the device.
   const char *getDeviceUid() const { return DeviceUid.c_str(); }
 
+  /// Get the total shared memory per block (in bytes) that can be used in any
+  /// kernel.
+  size_t getMaxBlockSharedMemSize() const { return MaxBlockSharedMemSize; }
+
   /// Set the context of the device if needed, before calling device-specific
   /// functions. Plugins may implement this function as a no-op if not needed.
   virtual Error setContext() = 0;
@@ -1579,6 +1583,9 @@ private:
   std::unordered_map<std::string, TuningMetadataTy> TuningData;
   /// Internal representation for OMPT device (initialize & finalize)
   std::atomic<bool> OmptInitialized;
+
+  /// The total per-block native shared memory that a kernel may use.
+  size_t MaxBlockSharedMemSize = 0;
 };
 
 /// Class implementing common functionalities of offload plugins. Each plugin
