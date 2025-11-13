0cebb7a2d385140159ac5e6d5c288a0810460510
Chenguang Wang <w3cing@gmail.com>
2025-11-13 13:37:50 -0800

    [libc++] Support using Newlib as libc with locale support.
    Tested on Newlib v4.5.0. The newlib.h was mostly copied from linux.h
    within the same directory.
    
    Newlib locale support was initially added in D5385 [1], but removed in
    revision cfde4fb [2], because of revision 78b6374 [3].
    
    [1]: https://reviews.llvm.org/D5385
    [2]: https://github.com/llvm/llvm-project/pull/113721
    [3]: https://reviews.llvm.org/D49927

--- libcxx/include/__locale_dir/locale_base_api.h
+++ libcxx/include/__locale_dir/locale_base_api.h
@@ -123,6 +123,8 @@
 #    include <__locale_dir/support/fuchsia.h>
 #  elif defined(__linux__)
 #    include <__locale_dir/support/linux.h>
+#  elif _LIBCPP_HAS_NEWLIB_LIBC
+#    include <__locale_dir/support/newlib.h>
 #  else
 
 // TODO: This is a temporary definition to bridge between the old way we defined the locale base API
