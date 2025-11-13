e89cee48eaa986a52820191e8021cc5b99650747
Aleksandr Nogikh <nogikh@google.com>
2025-11-13 10:51:09 +0000

    fixup: expand the diagnostic message

--- clang/test/Sema/attr-malloc_span.c
+++ clang/test/Sema/attr-malloc_span.c
@@ -1,7 +1,7 @@
 // RUN: %clang_cc1 -verify -fsyntax-only %s
 // RUN: %clang_cc1 -emit-llvm -o %t %s
 
-#include <stddef.h>
+typedef __SIZE_TYPE__ size_t;
 
 typedef struct {
   void *ptr;
@@ -21,11 +21,11 @@ typedef struct {
   void *ptr;
   void *ptr2;
 } invalid_span1;
-invalid_span1  returns_non_std_span1  (void) __attribute((malloc_span)); // expected-warning {{attribute only applies to return values that are span-like structures}}
+invalid_span1  returns_non_std_span1  (void) __attribute((malloc_span)); // expected-warning {{attribute only applies to functions that return span-like structures}}
 
 typedef struct {
   void *ptr;
   size_t n;
   size_t n2;
 } invalid_span2;
-invalid_span2  returns_non_std_span2  (void) __attribute((malloc_span)); // expected-warning {{attribute only applies to return values that are span-like structures}}
+invalid_span2  returns_non_std_span2  (void) __attribute((malloc_span)); // expected-warning {{attribute only applies to functions that return span-like structures}}
