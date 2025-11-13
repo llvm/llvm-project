5e7032e9bc20fdd79c683f6f3514cd5a50f449c9
Andy Kaylor <akaylor@nvidia.com>
2025-11-13 14:31:05 -0800

    [CIR][NFC] Add missing code markers for Dtor_VectorDeleting
    This adds some minimal code to mark locations where handling is needed
    for Dtor_VectorDeleting type dtors, which were added in
    https://github.com/llvm/llvm-project/pull/165598
    
    This is not a comprehensive mark-up of the missing code, as some code
    will be needed in places where the surrounding function has larger
    missing pieces in CIR currently.
    
    This fixes a warning for an uncovered switch case that was causing CI
    builds to fail.

--- clang/lib/CIR/CodeGen/CIRGenExprCXX.cpp
+++ clang/lib/CIR/CodeGen/CIRGenExprCXX.cpp
@@ -708,6 +708,13 @@ void CIRGenFunction::emitCXXDeleteExpr(const CXXDeleteExpr *e) {
   deleteTy = getContext().getBaseElementType(deleteTy);
   ptr = ptr.withElementType(builder, convertTypeForMem(deleteTy));
 
+  if (e->isArrayForm() &&
+      cgm.getContext().getTargetInfo().emitVectorDeletingDtors(
+          cgm.getContext().getLangOpts())) {
+    cgm.errorNYI(e->getSourceRange(),
+                 "emitCXXDeleteExpr: emitVectorDeletingDtors");
+  }
+
   if (e->isArrayForm()) {
     assert(!cir::MissingFeatures::deleteArray());
     cgm.errorNYI(e->getSourceRange(), "emitCXXDeleteExpr: array delete");
