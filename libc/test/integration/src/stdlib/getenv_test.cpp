faa226feae379d2d209fe1ff893cf0fda6ae0c2e
Jeff Bailey <jbailey@raspberryginger.com>
2025-11-13 11:59:38 +0000

    [libc][stdlib] Update getenv_test to use inline_strcmp for string comparison

--- libc/test/integration/src/stdlib/getenv_test.cpp
+++ libc/test/integration/src/stdlib/getenv_test.cpp
@@ -7,21 +7,22 @@
 //===----------------------------------------------------------------------===//
 
 #include "src/stdlib/getenv.h"
-#include "src/string/strcmp.h"
+#include "src/string/memory_utils/inline_strcmp.h"
 
 #include "test/IntegrationTest/test.h"
 
 TEST_MAIN([[maybe_unused]] int argc, [[maybe_unused]] char **argv,
           [[maybe_unused]] char **envp) {
+  auto comp = [](char l, char r) -> int { return l - r; };
   ASSERT_TRUE(LIBC_NAMESPACE::getenv("") == nullptr);
   ASSERT_TRUE(LIBC_NAMESPACE::getenv("=") == nullptr);
   ASSERT_TRUE(LIBC_NAMESPACE::getenv("MISSING ENV VARIABLE") == nullptr);
   ASSERT_FALSE(LIBC_NAMESPACE::getenv("PATH") == nullptr);
-  ASSERT_EQ(LIBC_NAMESPACE::strcmp(LIBC_NAMESPACE::getenv("FRANCE"), "Paris"),
+  ASSERT_EQ(LIBC_NAMESPACE::inline_strcmp(LIBC_NAMESPACE::getenv("FRANCE"), "Paris", comp),
             0);
-  ASSERT_NE(LIBC_NAMESPACE::strcmp(LIBC_NAMESPACE::getenv("FRANCE"), "Berlin"),
+  ASSERT_NE(LIBC_NAMESPACE::inline_strcmp(LIBC_NAMESPACE::getenv("FRANCE"), "Berlin", comp),
             0);
-  ASSERT_EQ(LIBC_NAMESPACE::strcmp(LIBC_NAMESPACE::getenv("GERMANY"), "Berlin"),
+  ASSERT_EQ(LIBC_NAMESPACE::inline_strcmp(LIBC_NAMESPACE::getenv("GERMANY"), "Berlin", comp),
             0);
   ASSERT_TRUE(LIBC_NAMESPACE::getenv("FRANC") == nullptr);
   ASSERT_TRUE(LIBC_NAMESPACE::getenv("FRANCE1") == nullptr);
