384c35f7fccdb90fa08a8450202c2e836fa091b7
gbMattN <matthew.nagy@sony.com>
2025-11-14 11:03:31 +0000

    Fixup tests

--- clang/test/CodeGen/sanitize-type-outlined.cpp
+++ clang/test/CodeGen/sanitize-type-outlined.cpp
@@ -1,13 +1,14 @@
 // UNSUPPORTED: target={{.*}}-windows-{{.*}}
 
 // RUN: %clang -S -fsanitize=type -emit-llvm -o - -fsanitize=type %s \
-// RUN:     | FileCheck %s --check-prefixes=CHECK-NO-OUTLINE
-// RUN: %clang -S -fsanitize=type -emit-llvm -o - -fsanitize=type %s \
 // RUN:     -fsanitize-type-outline-instrumentation \
 // RUN:     | FileCheck %s --check-prefixes=CHECK-OUTLINE
+// RUN: %clang -S -fsanitize=type -emit-llvm -o - -fsanitize=type %s \
+// RUN:     -fno-sanitize-type-outline-instrumentation \
+// RUN:     | FileCheck %s --check-prefixes=CHECK-NO-OUTLINE
 
-// CHECK-NO-OUTLINE-NOT: call{{.*}}@__tysan_instrument_mem_inst
 // CHECK-OUTLINE: call{{.*}}@__tysan_instrument_mem_inst
+// CHECK-NO-OUTLINE-NOT: call{{.*}}@__tysan_instrument_mem_inst
 
 float alias(int *ptr){
     float *aliasedPtr = (float *)ptr;
