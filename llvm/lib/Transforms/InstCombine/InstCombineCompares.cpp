40b06bf2d42940f6ccb502a6f9a3050935e8e901
Peter Collingbourne <pcc@google.com>
2025-11-14 14:27:37 -0800

    Fix commit message and simplify change
    Created using spr 1.3.6-beta.1

--- llvm/lib/Transforms/InstCombine/InstCombineCompares.cpp
+++ llvm/lib/Transforms/InstCombine/InstCombineCompares.cpp
@@ -2639,16 +2639,6 @@ Instruction *InstCombinerImpl::foldICmpShrConstant(ICmpInst &Cmp,
     return new ICmpInst(Pred, X, ConstantInt::get(ShrTy, C << ShAmtVal));
 
   if (Shr->hasOneUse()) {
-    if (0) {
-      // == 0 is u< 1.
-      if (Pred == CmpInst::ICMP_EQ)
-        return new ICmpInst(CmpInst::ICMP_ULT, X,
-                            ConstantInt::get(ShrTy, (C + 1).shl(ShAmtVal)));
-      else
-        return new ICmpInst(CmpInst::ICMP_UGT, X,
-                            ConstantInt::get(ShrTy, (C + 1).shl(ShAmtVal) - 1));
-    }
-
     // Canonicalize the shift into an 'and':
     // icmp eq/ne (shr X, ShAmt), C --> icmp eq/ne (and X, HiMask), (C << ShAmt)
     APInt Val(APInt::getHighBitsSet(TypeBits, TypeBits - ShAmtVal));
