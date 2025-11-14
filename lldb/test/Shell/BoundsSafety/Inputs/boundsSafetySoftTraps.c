7b9b164a649085542274448f0c477b44f3ef9786
Dan Liew <dan@su-root.co.uk>
2025-11-13 16:36:27 -0800

    [NFC][BoundsSafety][LLDB] Refactor the soft trap runtime tests
    This refactors the soft trap runtime test so the soft trap runtime
    implementation exists in its own file. This has several advantages:
    
    * It let's the runtime be built without debug info which will be the
      common case uses hit
    * It prevents the risk of infinite recursion because it isn't safe
      to build the soft trap runtime with -fbounds-safety soft trap mode
      enabled.

--- lldb/test/Shell/BoundsSafety/Inputs/boundsSafetySoftTraps.c
+++ lldb/test/Shell/BoundsSafety/Inputs/boundsSafetySoftTraps.c
@@ -1,17 +1,4 @@
 #include <stdio.h>
-#include <bounds_safety_soft_traps.h>
-
-#if __CLANG_BOUNDS_SAFETY_SOFT_TRAP_API_VERSION > 0
-#error API version changed
-#endif
-
-void __bounds_safety_soft_trap_s(const char *reason) {
-    printf("BoundsSafety check FAILED: message:\"%s\"\n", reason? reason: "");
-}
-
-void __bounds_safety_soft_trap(void) {
-    printf("BoundsSafety check FAILED\n");
-}
 
 int bad_read(int index) {
   int array[] = {0, 1, 2};
