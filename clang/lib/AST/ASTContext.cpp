1c42262dc348664be14724734ea1639632507425
ranapratap55 <RanaPratapReddy.Nimmakayala@amd.com>
2025-11-14 15:09:42 +0530

    Moving removal of 'e' handling to a separate PR

--- clang/lib/AST/ASTContext.cpp
+++ clang/lib/AST/ASTContext.cpp
@@ -12402,8 +12402,12 @@ static QualType DecodeTypeFromStr(const char *&Str, const ASTContext &Context,
 
   // Read the base type.
   switch (*Str++) {
-  default:
-    llvm_unreachable("Unknown builtin type letter!");
+  default: llvm_unreachable("Unknown builtin type letter!");
+  case 'e':
+    assert(HowLong == 0 && !Signed && !Unsigned &&
+           "Bad modifiers used with 'e'!");
+    Type = Context.getLangOpts().OpenCL ? Context.HalfTy : Context.Float16Ty;
+    break;
   case 'x':
     assert(HowLong == 0 && !Signed && !Unsigned &&
            "Bad modifiers used with 'x'!");
