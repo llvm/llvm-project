65ec4ba4b0b7c226b29d3977a9e0922841a175fd
Thibault-Monnier <thibaultmonni@gmail.com>
2025-11-15 15:04:05 +0100

    Remove duplicate definition

--- clang/lib/CIR/Lowering/DirectToLLVM/LowerToLLVM.cpp
+++ clang/lib/CIR/Lowering/DirectToLLVM/LowerToLLVM.cpp
@@ -670,29 +670,6 @@ mlir::LogicalResult CIRToLLVMASinOpLowering::matchAndRewrite(
   return mlir::success();
 }
 
-mlir::LogicalResult CIRToLLVMLLVMIntrinsicCallOpLowering::matchAndRewrite(
-    cir::LLVMIntrinsicCallOp op, OpAdaptor adaptor,
-    mlir::ConversionPatternRewriter &rewriter) const {
-  mlir::Type llvmResTy =
-      getTypeConverter()->convertType(op->getResultTypes()[0]);
-  if (!llvmResTy)
-    return op.emitError("expected LLVM result type");
-  StringRef name = op.getIntrinsicName();
-  // Some llvm intrinsics require ElementType attribute to be attached to
-  // the argument of pointer type. That prevents us from generating LLVM IR
-  // because from LLVM dialect, we have LLVM IR like the below which fails
-  // LLVM IR verification.
-  // %3 = call i64 @llvm.aarch64.ldxr.p0(ptr %2)
-  // The expected LLVM IR should be like
-  // %3 = call i64 @llvm.aarch64.ldxr.p0(ptr elementtype(i32) %2)
-  // TODO(cir): MLIR LLVM dialect should handle this part as CIR has no way
-  //  to set LLVM IR attribute.
-  assert(!cir::MissingFeatures::llvmIntrinsicElementTypeSupport());
-  replaceOpWithCallLLVMIntrinsicOp(rewriter, op, "llvm." + name, llvmResTy,
-                                   adaptor.getOperands());
-  return mlir::success();
-}
-
 mlir::LogicalResult CIRToLLVMAssumeOpLowering::matchAndRewrite(
     cir::AssumeOp op, OpAdaptor adaptor,
     mlir::ConversionPatternRewriter &rewriter) const {
