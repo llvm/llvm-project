4b3d5a5961c077245aff065bf62cfddcc41f395c
Sergej Salnikov <skill@google.com>
2025-11-17 19:22:18 +0100

    Another attempt to fix the test

--- clang/test/Preprocessor/print_line_track.c
+++ clang/test/Preprocessor/print_line_track.c
@@ -1,16 +1,16 @@
-/* RUN: %clang_cc1 -E %s | awk '/a/,/3/{print; exit 0} {exit 1}'
- * RUN: %clang_cc1 -E %s | awk '/b/,/16/{print; exit 0} {exit 1}'
- * RUN: %clang_cc1 -E -P %s | awk '/a/,/3/{print; exit 0} {exit 1}'
- * RUN: %clang_cc1 -E -P %s | awk '/b/,/16/{print; exit 0} {exit 1}'
+/* RUN: %clang_cc1 -E %s | grep 'a 3'
+ * RUN: %clang_cc1 -E %s | grep 'b 16'
+ * RUN: %clang_cc1 -E -P %s | grep 'a 3'
+ * RUN: %clang_cc1 -E -P %s | grep 'b 16'
  * RUN: %clang_cc1 -E %s | not grep '# 0 '
- * RUN: %clang_cc1 -E -P %s | count 4
+ * RUN: %clang_cc1 -E -P %s | count 2
  * PR1848 PR3437 PR7360
 */
 
 #define t(x) x
 
-t(a
-3)
+t(
+  a 3)
 
-t(b
-__LINE__)
+t(
+  b __LINE__)
