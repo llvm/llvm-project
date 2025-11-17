117a1b753e2838d62a4b298d337a5f7f61c6ef05
Steven Wu <stevenwu@apple.com>
2025-11-17 10:13:24 -0800

    [lldb][Swift] Update swift performIRGen API
    Update lldb's usage of swift compiler API after update.

--- lldb/source/Plugins/ExpressionParser/Swift/SwiftExpressionParser.cpp
+++ lldb/source/Plugins/ExpressionParser/Swift/SwiftExpressionParser.cpp
@@ -2087,7 +2087,8 @@ SwiftExpressionParser::Parse(DiagnosticManager &diagnostic_manager,
         &parsed_expr->module, IRGenOpts, m_swift_ast_ctx.GetTBDGenOptions(),
         std::move(sil_module), "lldb_module",
         swift::PrimarySpecificPaths("", parsed_expr->main_filename),
-        llvm::ArrayRef<std::string>(), llvm::ArrayRef<std::string>());
+        /*CAS=*/nullptr, llvm::ArrayRef<std::string>(),
+        llvm::ArrayRef<std::string>());
 
     if (GenModule) {
       swift::performLLVMOptimizations(
