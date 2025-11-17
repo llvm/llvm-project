a9ce782a46c81646ed19b0d7c5918fa841f36984
Mat Cross <mat.cross@amd.com>
2025-11-17 19:19:30 +0000

    CPUPC-7979: CI sync from aocc_llvm21.
    Change-Id: Id60872f01e6d9622aa908d097b53b5534844b66c

--- .amd-ci/upstream/apptainer_run.sh
+++ .amd-ci/upstream/apptainer_run.sh
@@ -11,12 +11,41 @@ if test ! -f "${image_path}"
 then
   image_path=/proj/csse_jenkins2/swtools/${image_tail}
 fi
+envs=(
+  --env AOCC_DUAL_FLANG_BUILD="${AOCC_DUAL_FLANG_BUILD:-false}"
+  --env BRANCH_NAME="${BRANCH_NAME:-NoSuchBranchName}"
+  --env BUILD_NUMBER="${BUILD_NUMBER:-0}"
+  --env CI="${CI:-false}"
+  --env JOB_NAME="${JOB_NAME:-NoSuchJobName}"
+)
+if test -n "${BUILD_TYPE:-}"
+then
+  envs+=(--env BUILD_TYPE="${BUILD_TYPE}")
+fi
+if test -n "${CHANGE_TARGET:-}"
+then
+  envs+=(--env CHANGE_TARGET="${CHANGE_TARGET}")
+fi
+if test -n "${REL_MODE:-}"
+then
+  envs+=(--env REL_MODE="${REL_MODE}")
+fi
+if test -n "${STAGE_BUILD_NUM:-}"
+then
+  envs+=(--env STAGE_BUILD_NUM="${STAGE_BUILD_NUM}")
+fi
+if test -n "${VERSION_VAL:-}"
+then
+  envs+=(--env VERSION_VAL="${VERSION_VAL}")
+fi
+binds="/proj:/proj:ro,$(pwd):${bind_point}:rw"
+if test -n "${JENKINS_REMOTE_GIT_CACHE:-}"
+then
+  binds+=",${JENKINS_REMOTE_GIT_CACHE}:${JENKINS_REMOTE_GIT_CACHE}:ro"
+fi
 apptainer exec \
           --cleanenv \
-          --env AOCC_DUAL_FLANG_BUILD="${AOCC_DUAL_FLANG_BUILD:-false}" \
-          --env BUILD_NUMBER="${BUILD_NUMBER:-0}" \
-          --env CI="${CI:-false}" \
-          --env JOB_NAME="${JOB_NAME:-NoSuchJobName}" \
-          --bind "/proj:/proj:ro,$(pwd):${bind_point}:rw" \
+          "${envs[@]}" \
+          --bind "${binds}" \
           ${image_path} \
           bash -c "cd ${bind_point} && ${_script_dir}/run.sh"
