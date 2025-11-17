53e3f8e3c50ee28e33181514933c5632a001033b
Aiden Grossman <aidengrossman@google.com>
2025-11-17 14:50:16 +0000

    [XRay] Prefix setting XRAY_OPTIONS with env
    So setting the environment variable works with the new internal shell.
    This does not fix all the XRay tests because some of them are using
    subshells and need to be rewritten to not use subshells.

--- compiler-rt/test/xray/TestCases/Posix/fdr-mode-inmemory.cpp
+++ compiler-rt/test/xray/TestCases/Posix/fdr-mode-inmemory.cpp
@@ -2,9 +2,9 @@
 // RUN: rm -rf %t.dir
 // RUN: mkdir -p %t.dir
 // RUN: cd %t.dir
-// RUN: XRAY_OPTIONS="patch_premain=false xray_logfile_base=fdr-inmemory-test- \
+// RUN: env XRAY_OPTIONS="patch_premain=false xray_logfile_base=fdr-inmemory-test- \
 // RUN:     verbosity=1" \
-// RUN: XRAY_FDR_OPTIONS="no_file_flush=true func_duration_threshold_us=0" \
+// RUN: env XRAY_FDR_OPTIONS="no_file_flush=true func_duration_threshold_us=0" \
 // RUN:     %run %t 2>&1 | FileCheck %s
 // RUN: FILES=`find %t.dir -name 'fdr-inmemory-test-*' | wc -l`
 // RUN: [ $FILES -eq 0 ]
--- compiler-rt/test/xray/TestCases/Posix/profiling-multi-threaded.cpp
+++ compiler-rt/test/xray/TestCases/Posix/profiling-multi-threaded.cpp
@@ -4,9 +4,9 @@
 // FIXME: Make -fxray-modes=xray-profiling part of the default?
 // RUN: %clangxx_xray -std=c++11 %s -o %t -fxray-modes=xray-profiling
 // RUN: rm -f xray-log.profiling-multi-*
-// RUN: XRAY_OPTIONS=verbosity=1 \
+// RUN: env XRAY_OPTIONS=verbosity=1 \
 // RUN:     XRAY_PROFILING_OPTIONS=no_flush=1 %run %t
-// RUN: XRAY_OPTIONS=verbosity=1 %run %t
+// RUN: env XRAY_OPTIONS=verbosity=1 %run %t
 // RUN: PROFILES=`ls xray-log.profiling-multi-* | wc -l`
 // RUN: [ $PROFILES -eq 1 ]
 // RUN: rm -f xray-log.profiling-multi-*
