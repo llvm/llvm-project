ae3dc6d2bcf18c5ebf7216eed01287252eead8b5
Greg von Winckel <gvonwin@sandia.gov>
2025-11-17 12:44:04 -0700

    Fixed terminal color formattinng
    Signed-off-by: Greg von Winckel <gvonwin@sandia.gov>

--- custom-functions-dev.sh
+++ custom-functions-dev.sh
@@ -26,11 +26,11 @@
 set -e  # Exit on error
 
 # Colors for output
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
-BLUE='\033[0;34m'
-NC='\033[0m' # No Color
+RED=$'\033[0;31m'
+GREEN=$'\033[0;32m'
+YELLOW=$'\033[1;33m'
+BLUE=$'\033[0;34m'
+NC=$'\033[0m' # No Color
 
 # Directories
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
