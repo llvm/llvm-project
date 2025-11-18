db71cc58ec9471c67c6b80996930a19222dd9f03
Jackson Stogel <jtstogel@gmail.com>
2025-11-18 14:30:15 -0800

    [libc] Implement pkey_alloc/free/get/set/mprotect for x86_64 linux (#162362)
    This patch provides definitions for `pkey_*` functions for linux x86_64.
    
    `pkey_alloc`, `pkey_free`, and `pkey_mprotect` are simple syscall
    wrappers. `pkey_set` and `pkey_get` modify architecture-specific
    registers. The logic for these live in architecture specific
    directories:
    
    * `libc/src/sys/mman/linux/x86_64/pkey_common.h` has a real
    implementation
    * `libc/src/sys/mman/linux/generic/pkey_common.h` contains stubs that
    just return `ENOSYS`.

--- libc/src/sys/mman/linux/pkey_free.cpp
+++ libc/src/sys/mman/linux/pkey_free.cpp
@@ -1,4 +1,4 @@
-//===---------- Linux implementation of the POSIX mprotect function -------===//
+//===---------- Linux implementation of the Linux pkey_free function ------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,27 +6,30 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "src/sys/mman/mprotect.h"
+#include "src/sys/mman/pkey_free.h"
 
+#include "hdr/errno_macros.h"             // For ENOSYS
 #include "src/__support/OSUtil/syscall.h" // For internal syscall function.
 #include "src/__support/common.h"
-
-#include "src/__support/error_or.h"
 #include "src/__support/libc_errno.h"
 #include "src/__support/macros/config.h"
-#include "src/sys/mman/linux/mprotect_common.h"
+
 #include <sys/syscall.h> // For syscall numbers.
 
 namespace LIBC_NAMESPACE_DECL {
 
-LLVM_LIBC_FUNCTION(int, mprotect, (void *addr, size_t size, int prot)) {
-  ErrorOr<int> result =
-      LIBC_NAMESPACE::mprotect_common::mprotect_impl(addr, size, prot);
-  if (!result.has_value()) {
-    libc_errno = result.error();
+LLVM_LIBC_FUNCTION(int, pkey_free, (int pkey)) {
+#if !defined(SYS_pkey_free)
+  libc_errno = ENOSYS;
+  return -1;
+#else
+  int ret = LIBC_NAMESPACE::syscall_impl<int>(SYS_pkey_free, pkey);
+  if (ret < 0) {
+    libc_errno = -ret;
     return -1;
   }
-  return result.value();
+  return 0;
+#endif
 }
 
 } // namespace LIBC_NAMESPACE_DECL
