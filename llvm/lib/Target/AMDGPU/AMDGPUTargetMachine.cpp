9017c1c153fb92c9093454f43b9296801448bef0
vikhegde <vikram.hegde@amd.com>
2025-11-18 11:02:41 +0530

    [AMDGPU][NPM] Enable SIModeRegister and SIInsertHardclauses passes

--- llvm/lib/Target/AMDGPU/AMDGPUTargetMachine.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPUTargetMachine.cpp
@@ -2398,10 +2398,10 @@ void AMDGPUCodeGenPassBuilder::addPreEmitPass(AddMachinePass &addPass) const {
   addPass(SIMemoryLegalizerPass());
   addPass(SIInsertWaitcntsPass());
 
-  // TODO: addPass(SIModeRegisterPass());
+  addPass(SIModeRegisterPass());
 
   if (TM.getOptLevel() > CodeGenOptLevel::None) {
-    // TODO: addPass(SIInsertHardClausesPass());
+    addPass(SIInsertHardClausesPass());
   }
 
   addPass(SILateBranchLoweringPass());
