693ac07ef8cbf36ad120ebc87260ce54bb4cfaa1
Loïc Yhuel <loic.yhuel@softathome.com>
2025-11-18 11:10:04 +0100

    fixup! [llvm][ARM] Allow MOVT and MOVW on the offset between two labels

--- llvm/test/MC/ARM/arm-movt-movw-absolute-pass.s
+++ llvm/test/MC/ARM/arm-movt-movw-absolute-pass.s
@@ -1,14 +1,9 @@
-@RUN: llvm-mc -triple armv7-eabi -filetype obj -o - %s 2>&1 | FileCheck %s
+@RUN: llvm-mc -triple armv7-eabi -filetype obj %s -o - | llvm-objdump -d --triple armv7-eabi - | FileCheck %s
 
-    .text
 a:
     movw    r1, #:lower16:b - a + 65536
     movt    r1, #:upper16:b - a + 65536
 b:
 
-@CHECK-NOT: error: Relocation Not In Range
-@CHECK-NOT: movw    r1, #:lower16:b - a + 65536
-@CHECK-NOT: ^
-@CHECK-NOT: error: Relocation Not In Range
-@CHECK-NOT: movt    r1, #:upper16:b - a + 65536
-@CHECK-NOT: ^
+@CHECK: 0: e3001008 movw r1, #0x8
+@CHECK: 4: e3401001 movt r1, #0x1
