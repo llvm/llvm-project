94a8b14ae6da2de963e66d16a78d9522f2b5d19f
Hristo Hristov <hghristov.rmm@gmail.com>
2025-11-18 19:53:48 +0200

    Formatting

--- libcxx/test/libcxx/utilities/smartptr/nodiscard.verify.cpp
+++ libcxx/test/libcxx/utilities/smartptr/nodiscard.verify.cpp
@@ -49,21 +49,21 @@ void test() {
     *sPtr;            // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
     sPtr.use_count(); // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
 #if TEST_STD_VER <= 20
-    sPtr.unique();    // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
+    sPtr.unique(); // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
 #endif
     // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
     sPtr.owner_before(std::shared_ptr<int>());
     // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
     sPtr.owner_before(std::weak_ptr<int>());
-#  if TEST_STD_VER >= 17
+#if TEST_STD_VER >= 17
     sPtr[0]; // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
-#  endif
+#endif
 
     // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
     std::allocate_shared<int>(std::allocator<int>(), 5);
     // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
     std::make_shared<int>();
-#  if TEST_STD_VER >= 20
+#if TEST_STD_VER >= 20
     // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
     std::allocate_shared_for_overwrite<int>(std::allocator<int>());
     // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
@@ -96,7 +96,7 @@ void test() {
     std::make_shared<int[]>(5, 82);
     // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
     std::make_shared_for_overwrite<int[]>(5);
-#  endif
+#endif
 
     // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
     std::static_pointer_cast<int[]>(sPtr);
@@ -116,10 +116,10 @@ void test() {
     std::reinterpret_pointer_cast<int[]>(sPtr);
     // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
     std::reinterpret_pointer_cast<int[]>(std::move(sPtr));
-#  if !defined(TEST_HAS_NO_RTTI)
+#if !defined(TEST_HAS_NO_RTTI)
     // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
     std::get_deleter<int[]>(sPtr);
-#  endif
+#endif
   }
   { // [util.smartptr.weak]
     std::weak_ptr<int> wPtr;
@@ -139,12 +139,12 @@ void test() {
 
     es.shared_from_this(); // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
     ces.shared_from_this(); // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
-#  if TEST_STD_VER >= 17
+#if TEST_STD_VER >= 17
     es.weak_from_this();  // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
     ces.weak_from_this(); // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
-#  endif
+#endif
   }
-#  if TEST_STD_VER >= 23
+#if TEST_STD_VER >= 23
   { // [smartptr.adapt]
     std::unique_ptr<int> uPtr;
     // [inout.ptr]
@@ -152,5 +152,5 @@ void test() {
     // [out.ptr]
     std::out_ptr(uPtr); // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
   }
-#  endif
+#endif
 }
