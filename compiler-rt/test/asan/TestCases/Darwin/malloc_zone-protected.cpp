4ff28c9fd24e424bb304e60540b557888b0ea94d
Aiden Grossman <aidengrossman@google.com>
2025-11-18 16:01:06 +0000

    formatting
    Created using spr 1.3.7

--- compiler-rt/test/asan/TestCases/Darwin/malloc_zone-protected.cpp
+++ compiler-rt/test/asan/TestCases/Darwin/malloc_zone-protected.cpp
@@ -5,7 +5,6 @@
 // RUN: %clangxx_asan %s -o %t
 // RUN: env ASAN_OPTIONS="abort_on_error=1" not --crash %run %t 2>&1 | FileCheck %s
 
-
 void *pwn(malloc_zone_t *unused_zone, size_t unused_size) {
   printf("PWNED\n");
   return NULL;
--- compiler-rt/test/asan_abi/TestCases/Darwin/llvm_interface_symbols.cpp
+++ compiler-rt/test/asan_abi/TestCases/Darwin/llvm_interface_symbols.cpp
@@ -24,7 +24,7 @@
 // RUN: diff %t.imports-sorted %t.exports-sorted
 
 // Ensure that there is no dynamic dylib linked.
-// RUN: otool -L %t > %t.libs 
+// RUN: otool -L %t > %t.libs
 // RUN: not grep -q "dynamic.dylib" < %t.libs
 
 // UNSUPPORTED: ios
