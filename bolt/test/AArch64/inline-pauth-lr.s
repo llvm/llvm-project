267c93a90176ce20a6263e7995d4dcae9d0fc84b
Gergely Balint <gergely.balint@arm.com>
2025-11-19 13:02:23 +0000

    [BOLT] Add unittest for inliner using retaasppc <label>

--- bolt/test/AArch64/inline-pauth-lr.s
+++ bolt/test/AArch64/inline-pauth-lr.s
@@ -1,5 +1,5 @@
-# This test checks that inlining functions with fused pointer-auth-and-return
-# instructions is properly handled by BOLT.
+# This test checks that inlining functions with the pauth-lr variants of
+# fused pointer-auth-and-return instructions is properly handled by BOLT.
 
 # REQUIRES: system-linux
 
@@ -7,13 +7,17 @@
 # RUN: llvm-bolt --inline-all --print-inline  --print-only=_Z3barP1A  \
 # RUN: %t.exe -o %t.bolt  | FileCheck %s
 
-# CHECK: BOLT-INFO: inlined 0 calls at 1 call sites in 2 iteration(s). Change in binary size: 8 bytes.
+# CHECK: BOLT-INFO: inlined 0 calls at 2 call sites in 2 iteration(s). Change in binary size: 16 bytes.
 # CHECK: Binary Function "_Z3barP1A" after inlining {
 # CHECK-NOT:  bl	_Z3fooP1A
 # CHECK:      paciasppc
 # CHECK-NEXT: ldr	x8, [x0]
 # CHECK-NEXT: ldr	w0, [x8]
 # CHECK-NEXT: autiasppcr x28
+# CHECK-NEXT: paciasppc
+# CHECK-NEXT: ldr	x7, [x0]
+# CHECK-NEXT: ldr	w0, [x7]
+# CHECK-NEXT: autiasppc _Z3bazP1A
 
 	.text
 	.globl	_Z3fooP1A
@@ -25,12 +29,24 @@ _Z3fooP1A:
 	retaasppcr x28
 	.size	_Z3fooP1A, .-_Z3fooP1A
 
+	.text
+	.globl	_Z3bazP1A
+	.type	_Z3bazP1A,@function
+_Z3bazP1A:
+0:
+    paciasppc
+	ldr	x7, [x0]
+	ldr	w0, [x7]
+	retaasppc 0b
+	.size	_Z3bazP1A, .-_Z3bazP1A
+
 	.globl	_Z3barP1A
 	.type	_Z3barP1A,@function
 _Z3barP1A:
 	stp	x29, x30, [sp, #-16]!
 	mov	x29, sp
 	bl	_Z3fooP1A
+	bl	_Z3bazP1A
 	mul	w0, w0, w0
 	ldp	x29, x30, [sp], #16
 	ret
