6b7b7da97d39b91138f5919e37e378a9a7d24079
Nathan Gauër <brioche@google.com>
2025-11-19 16:51:00 +0100

    [HLSL][SPIR-V] Add support for SV_Target semantic
    This PR adds the support for the SV_Target semantic and improved the
    diagnostics when the stage is correct, but the direction is disallowed.

--- clang/test/SemaHLSL/Semantics/target.ps.input.hlsl
+++ clang/test/SemaHLSL/Semantics/target.ps.input.hlsl
@@ -1,7 +1,7 @@
 // RUN: %clang_cc1 -triple dxil-pc-shadermodel6.3-pixel -finclude-default-header -x hlsl -verify -o - %s
 // RUN: %clang_cc1 -triple spirv-pc-vulkan1.3-pixel -finclude-default-header -x hlsl -verify -o - %s
 
-float4 main(float4 a : A) : SV_Position {
-// expected-error@-1 {{semantic 'SV_Position' is unsupported as output for stage pixel}}
+float4 main(float4 a : SV_Target) : A {
+// expected-error@-1 {{semantic 'SV_Target' is unsupported as input for stage pixel}}
   return a;
 }
