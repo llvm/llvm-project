10eb5769b876032b2b1935c88b6c52d524099217
dsalinas_amdeng <david.salinas@amd.com>
2025-11-20 19:52:40 +0000

    Add llvm-extract-bundle-entry to extend llvm-objcopy
      This commit creates llvm-extract-bundle-entry as a wrapper to llvm-objcopy,
      to allow extracting HIP offload fatbin bundles given a URI argument.

--- llvm/tools/llvm-objcopy/llvm-objcopy.cpp
+++ llvm/tools/llvm-objcopy/llvm-objcopy.cpp
@@ -79,8 +79,10 @@ static Expected<DriverConfig> getDriverConfig(ArrayRef<const char *> Args) {
 
   if (Is("install-name-tool") || Is("install_name_tool"))
     return parseInstallNameToolOptions(Args);
-
-  return parseObjcopyOptions(Args, reportWarning);
+  else if (Is("llvm-extract-bundle-entry"))
+    return parseExtractBundleEntryOptions(Args, reportWarning);
+  else
+    return parseObjcopyOptions(Args, reportWarning);
 }
 
 /// The function executeObjcopyOnIHex does the dispatch based on the format
