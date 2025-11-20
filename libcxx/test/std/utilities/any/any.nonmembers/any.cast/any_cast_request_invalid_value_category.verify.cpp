7910416b106183e2aec9c87cbaacad0e6d22cb92
Hristo Hristov <hghristov.rmm@gmail.com>
2025-11-20 07:05:59 +0200

    [libc++][any] Applied `[[nodiscard]]`
    `[[nodiscard]]` should be applied to functions where discarding the return value is most likely a correctness issue.
    - https://libcxx.llvm.org/CodingGuidelines.html#apply-nodiscard-where-relevant

--- libcxx/test/std/utilities/any/any.nonmembers/any.cast/any_cast_request_invalid_value_category.verify.cpp
+++ libcxx/test/std/utilities/any/any.nonmembers/any.cast/any_cast_request_invalid_value_category.verify.cpp
@@ -24,23 +24,23 @@ void test_const_lvalue_cast_request_non_const_lvalue()
     const std::any a;
     // expected-error-re@any:* {{static assertion failed{{.*}}ValueType is required to be a const lvalue reference or a CopyConstructible type}}
     // expected-error@any:* {{drops 'const' qualifier}}
-    std::any_cast<TestType &>(a); // expected-note {{requested here}}
+    (void)std::any_cast<TestType &>(a); // expected-note {{requested here}}
 
     const std::any a2(42);
     // expected-error-re@any:* {{static assertion failed{{.*}}ValueType is required to be a const lvalue reference or a CopyConstructible type}}
     // expected-error@any:* {{drops 'const' qualifier}}
-    std::any_cast<int&>(a2); // expected-note {{requested here}}
+    (void)std::any_cast<int&>(a2); // expected-note {{requested here}}
 }
 
 void test_lvalue_any_cast_request_rvalue()
 {
     std::any a;
     // expected-error-re@any:* {{static assertion failed{{.*}}ValueType is required to be an lvalue reference or a CopyConstructible type}}
-    std::any_cast<TestType &&>(a); // expected-note {{requested here}}
+    (void)std::any_cast<TestType &&>(a); // expected-note {{requested here}}
 
     std::any a2(42);
     // expected-error-re@any:* {{static assertion failed{{.*}}ValueType is required to be an lvalue reference or a CopyConstructible type}}
-    std::any_cast<int&&>(a2); // expected-note {{requested here}}
+    (void)std::any_cast<int&&>(a2); // expected-note {{requested here}}
 }
 
 void test_rvalue_any_cast_request_lvalue()
@@ -48,18 +48,9 @@ void test_rvalue_any_cast_request_lvalue()
     std::any a;
     // expected-error-re@any:* {{static assertion failed{{.*}}ValueType is required to be an rvalue reference or a CopyConstructible type}}
     // expected-error@any:* {{non-const lvalue reference to type 'TestType' cannot bind to a temporary}}
-    std::any_cast<TestType &>(std::move(a)); // expected-note {{requested here}}
+    (void)std::any_cast<TestType &>(std::move(a)); // expected-note {{requested here}}
 
     // expected-error-re@any:* {{static assertion failed{{.*}}ValueType is required to be an rvalue reference or a CopyConstructible type}}
     // expected-error@any:* {{non-const lvalue reference to type 'int' cannot bind to a temporary}}
-    std::any_cast<int&>(42);
-}
-
-int main(int, char**)
-{
-    test_const_lvalue_cast_request_non_const_lvalue();
-    test_lvalue_any_cast_request_rvalue();
-    test_rvalue_any_cast_request_lvalue();
-
-  return 0;
+    (void)std::any_cast<int&>(42);
 }
--- libcxx/test/std/utilities/any/any.nonmembers/any.cast/reference_types.verify.cpp
+++ libcxx/test/std/utilities/any/any.nonmembers/any.cast/reference_types.verify.cpp
@@ -18,35 +18,32 @@
 
 #include <any>
 
-int main(int, char**)
-{
+void test() {
     std::any a = 1;
 
     // expected-error-re@any:* 1 {{static assertion failed{{.*}}_ValueType may not be a reference.}}
-    std::any_cast<int &>(&a); // expected-note {{requested here}}
+    (void)std::any_cast<int &>(&a); // expected-note {{requested here}}
 
     // expected-error-re@any:* 1 {{static assertion failed{{.*}}_ValueType may not be a reference.}}
-    std::any_cast<int &&>(&a); // expected-note {{requested here}}
+    (void)std::any_cast<int &&>(&a); // expected-note {{requested here}}
 
     // expected-error-re@any:* 1 {{static assertion failed{{.*}}_ValueType may not be a reference.}}
-    std::any_cast<int const &>(&a); // expected-note {{requested here}}
+    (void)std::any_cast<int const &>(&a); // expected-note {{requested here}}
 
     // expected-error-re@any:* 1 {{static assertion failed{{.*}}_ValueType may not be a reference.}}
-    std::any_cast<int const&&>(&a); // expected-note {{requested here}}
+    (void)std::any_cast<int const&&>(&a); // expected-note {{requested here}}
 
     const std::any& a2 = a;
 
     // expected-error-re@any:* 1 {{static assertion failed{{.*}}_ValueType may not be a reference.}}
-    std::any_cast<int &>(&a2); // expected-note {{requested here}}
+    (void)std::any_cast<int &>(&a2); // expected-note {{requested here}}
 
     // expected-error-re@any:* 1 {{static assertion failed{{.*}}_ValueType may not be a reference.}}
-    std::any_cast<int &&>(&a2); // expected-note {{requested here}}
+    (void)std::any_cast<int &&>(&a2); // expected-note {{requested here}}
 
     // expected-error-re@any:* 1 {{static assertion failed{{.*}}_ValueType may not be a reference.}}
-    std::any_cast<int const &>(&a2); // expected-note {{requested here}}
+    (void)std::any_cast<int const &>(&a2); // expected-note {{requested here}}
 
     // expected-error-re@any:* 1 {{static assertion failed{{.*}}_ValueType may not be a reference.}}
-    std::any_cast<int const &&>(&a2); // expected-note {{requested here}}
-
-  return 0;
+    (void)std::any_cast<int const &&>(&a2); // expected-note {{requested here}}
 }
