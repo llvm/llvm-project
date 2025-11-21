f99cd3875ae8a1dbd54063fb81c55561ef3bb7e7
Krzysztof Parzyszek <Krzysztof.Parzyszek@amd.com>
2025-11-21 07:48:35 -0600

    Update loop-transformation-construct03.f90

--- flang/test/Semantics/OpenMP/loop-transformation-construct03.f90
+++ flang/test/Semantics/OpenMP/loop-transformation-construct03.f90
@@ -4,17 +4,17 @@
 
 subroutine loop_transformation_construct1
   implicit none
-  integer :: i = 5
-  integer :: y
+  integer, parameter :: i = 5
+  integer :: x
   integer :: v(i)
 
   ! Only 1 do loop is associated with the OMP DO directive so the END DO directive is unmatched
   !$omp do
   do x = 1, i
-    v(x) = x(x) * 2
+    v(x) = v(x) * 2
   end do
   do x = 1, i
-    v(x) = x(x) * 2
+    v(x) = v(x) * 2
   end do
   !ERROR: The END DO directive must follow the DO loop associated with the loop construct
   !$omp end do
@@ -22,17 +22,17 @@ end subroutine
 
 subroutine loop_transformation_construct2
   implicit none
-  integer :: i = 5
-  integer :: y
+  integer, parameter :: i = 5
+  integer :: x
   integer :: v(i)
 
   ! Only 1 do loop is associated with the OMP TILE directive so the END TILE directive is unmatched
   !$omp tile sizes(2)
   do x = 1, i
-    v(x) = x(x) * 2
+    v(x) = v(x) * 2
   end do
   do x = 1, i
-    v(x) = x(x) * 2
+    v(x) = v(x) * 2
   end do
   !ERROR: The END TILE directive must follow the DO loop associated with the loop construct
   !$omp end tile
