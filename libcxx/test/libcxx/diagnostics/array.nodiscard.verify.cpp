679fbf0b4fdaf9c8fe335e0950afd9cf18340b98
Hristo Hristov <hghristov.rmm@gmail.com>
2025-11-21 18:18:03 +0200

    Lengthen test

--- libcxx/test/libcxx/diagnostics/array.nodiscard.verify.cpp
+++ libcxx/test/libcxx/diagnostics/array.nodiscard.verify.cpp
@@ -15,39 +15,53 @@
 
 #include <test_macros.h>
 
-template <typename ArrT>
+template <std::size_t N>
 void test_members() {
-  ArrT a{};
+  std::array<int, N> a;
+  const std::array<int, N> ca{};
 
-  a.begin();   // expected-warning 4 {{ignoring return value of function declared with 'nodiscard' attribute}}
-  a.end();     // expected-warning 4 {{ignoring return value of function declared with 'nodiscard' attribute}}
-  a.rbegin();  // expected-warning 4 {{ignoring return value of function declared with 'nodiscard' attribute}}
-  a.rend();    // expected-warning 4 {{ignoring return value of function declared with 'nodiscard' attribute}}
-  a.cbegin();  // expected-warning 4 {{ignoring return value of function declared with 'nodiscard' attribute}}
-  a.cend();    // expected-warning 4 {{ignoring return value of function declared with 'nodiscard' attribute}}
-  a.crbegin(); // expected-warning 4 {{ignoring return value of function declared with 'nodiscard' attribute}}
-  a.crend();   // expected-warning 4 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  a.begin();    // expected-warning 2 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  ca.begin();   // expected-warning 2 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  a.end();      // expected-warning 2 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  ca.end();     // expected-warning 2 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  a.rbegin();   // expected-warning 2 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  ca.rbegin();  // expected-warning 2 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  a.rend();     // expected-warning 2 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  ca.rend();    // expected-warning 2 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  a.cbegin();   // expected-warning 2 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  ca.cbegin();  // expected-warning 2 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  a.cend();     // expected-warning 2 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  ca.cend();    // expected-warning 2 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  a.crbegin();  // expected-warning 2 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  ca.crbegin(); // expected-warning 2 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  a.crend();    // expected-warning 2 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  ca.crend();   // expected-warning 2 {{ignoring return value of function declared with 'nodiscard' attribute}}
 
-  a.size();     // expected-warning 4 {{ignoring return value of function declared with 'nodiscard' attribute}}
-  a.max_size(); // expected-warning 4 {{ignoring return value of function declared with 'nodiscard' attribute}}
-  a.empty();    // expected-warning 4 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  a.size();     // expected-warning 2 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  a.max_size(); // expected-warning 2 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  a.empty();    // expected-warning 2 {{ignoring return value of function declared with 'nodiscard' attribute}}
 
-  a[0];    // expected-warning 4 {{ignoring return value of function declared with 'nodiscard' attribute}}
-  a.at(0); // expected-warning 4 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  a[0];     // expected-warning 2 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  ca[0];    // expected-warning 2 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  a.at(0);  // expected-warning 2 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  ca.at(0); // expected-warning 2 {{ignoring return value of function declared with 'nodiscard' attribute}}
 
-  a.front(); // expected-warning 4 {{ignoring return value of function declared with 'nodiscard' attribute}}
-  a.back();  // expected-warning 4 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  a.front();  // expected-warning 2 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  ca.front(); // expected-warning 2 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  a.back();   // expected-warning 2 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  ca.back();  // expected-warning 2 {{ignoring return value of function declared with 'nodiscard' attribute}}
 
-  a.data(); // expected-warning 4 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  a.data();  // expected-warning 2 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  ca.data(); // expected-warning 2 {{ignoring return value of function declared with 'nodiscard' attribute}}
 }
 
 template <typename ArrT>
 void test_get() {
-  ArrT a{};
+  std::array<int> a{};
 
-  // expected-warning@+1 2 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
   std::get<0>(a);
-  // expected-warning@+1 2 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
   std::get<0>(std::move(a));
 }
 
@@ -61,11 +75,6 @@ void test_to_array() {
 #endif
 
 void test() {
-  test_members<std::array<int, 82>>();
-  test_members<const std::array<int, 82>>();
-  test_members<std::array<int, 0>>();
-  test_members<const std::array<int, 0>>();
-
-  test_get<std::array<int, 82>>();
-  test_get<const std::array<int, 82>>();
+  test_members<0>();
+  test_members<82>();
 }
