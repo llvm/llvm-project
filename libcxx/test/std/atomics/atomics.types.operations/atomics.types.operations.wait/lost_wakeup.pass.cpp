1ff15343aed5f005203bca46df4d8999294adda9
Hui Xie <hui.xie1990@gmail.com>
2025-11-22 13:42:22 +0000

    ci

--- libcxx/test/std/atomics/atomics.types.operations/atomics.types.operations.wait/lost_wakeup.pass.cpp
+++ libcxx/test/std/atomics/atomics.types.operations/atomics.types.operations.wait/lost_wakeup.pass.cpp
@@ -23,34 +23,35 @@
 constexpr int num_waiters    = 8;
 constexpr int num_iterations = 10'000;
 
-void wait(std::atomic<int>& waiter_ready, const std::atomic<int>& state) {
-  for (int i = 0; i < num_iterations; ++i) {
-    auto old_state = state.load(std::memory_order_acquire);
-    waiter_ready.fetch_add(1, std::memory_order_acq_rel);
-    state.wait(old_state, std::memory_order_acquire);
-  }
-}
-
-void notify(std::atomic<int>& waiter_ready, std::atomic<int>& state) {
-  for (int i = 0; i < num_iterations; ++i) {
-    while (waiter_ready.load(std::memory_order_acquire) < num_waiters) {
-      std::this_thread::yield();
-    }
-    waiter_ready.store(0, std::memory_order_release);
-    state.fetch_add(1, std::memory_order_acq_rel);
-    state.notify_all();
-  }
-}
-
 int main(int, char**) {
   for (int run = 0; run < 20; ++run) {
     std::atomic<int> waiter_ready(0);
     std::atomic<int> state(0);
-    std::vector<std::jthread> threads;
-    for (int i = 0; i < 8; ++i)
-      threads.push_back(support::make_test_jthread(wait, std::ref(waiter_ready), std::cref(state)));
 
-    threads.push_back(support::make_test_jthread(notify, std::ref(waiter_ready), std::ref(state)));
+    auto wait = [&]() {
+      for (int i = 0; i < num_iterations; ++i) {
+        auto old_state = state.load(std::memory_order_acquire);
+        waiter_ready.fetch_add(1, std::memory_order_acq_rel);
+        state.wait(old_state, std::memory_order_acquire);
+      }
+    };
+
+    auto notify = [&] {
+      for (int i = 0; i < num_iterations; ++i) {
+        while (waiter_ready.load(std::memory_order_acquire) < num_waiters) {
+          std::this_thread::yield();
+        }
+        waiter_ready.store(0, std::memory_order_release);
+        state.fetch_add(1, std::memory_order_acq_rel);
+        state.notify_all();
+      }
+    };
+
+    std::vector<std::jthread> threads;
+    for (int i = 0; i < num_waiters; ++i)
+      threads.push_back(support::make_test_jthread(wait));
+
+    threads.push_back(support::make_test_jthread(notify));
   }
 
   return 0;
