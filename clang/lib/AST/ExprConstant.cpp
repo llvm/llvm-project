974ddac3a7c98f37d78875dd9c55450d9a434581
ahmed <ahmednour.mohamed2012@gmail.com>
2025-11-23 16:32:30 +0200

    Update ExprConstant.cpp

--- clang/lib/AST/ExprConstant.cpp
+++ clang/lib/AST/ExprConstant.cpp
@@ -13499,8 +13499,8 @@ bool VectorExprEvaluator::VisitCallExpr(const CallExpr *E) {
       return false;
 
     // Extract bits 0 and 4 from imm8
-    bool SelectUpperA = (Imm8.getZExtValue() & 0x01) != 0;
-    bool SelectUpperB = (Imm8.getZExtValue() & 0x10) != 0;
+    bool SelectUpperA = (Imm8 & 0x01) != 0;
+    bool SelectUpperB = (Imm8 & 0x10) != 0;
 
     unsigned NumElems = SourceLHS.getVectorLength();
     SmallVector<APValue, 8> ResultElements;
