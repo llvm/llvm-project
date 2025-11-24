47299d89a3ba7057552e63b0492999169fad1a75
Jean-Didier Pailleux <jean-didier.pailleux@sipearl.com>
2025-11-24 16:27:48 +0100

    Use constexpr int + remove unused variable + use terminator instead of fprintf stderr

--- flang/include/flang/Runtime/extensions.h
+++ flang/include/flang/Runtime/extensions.h
@@ -105,7 +105,7 @@ float RTNAME(Secnds)(float *refTime, const char *sourceFile, int line);
 int RTNAME(Irand)(int *i);
 
 // GNU extension function RAND(I)
-float RTNAME(Rand)(int *i);
+float RTNAME(Rand)(int *i, const char *sourceFile, int line);
 
 // GNU extension subroutine SRAND(SEED)
 void FORTRAN_PROCEDURE_NAME(srand)(int *seed);
