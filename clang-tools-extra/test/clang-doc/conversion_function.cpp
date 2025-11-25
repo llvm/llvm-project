79d907096de92cd07ccd3fb7ec72f16970b6dc57
Erick Velez <erickvelez7@gmail.com>
2025-11-25 13:36:00 -0800

    [clang-doc] Add Mustache output to conversion function test

--- clang-tools-extra/test/clang-doc/conversion_function.cpp
+++ clang-tools-extra/test/clang-doc/conversion_function.cpp
@@ -6,6 +6,9 @@
 // RUN: clang-doc --format=html --output=%t --executor=standalone %s 
 // RUN: FileCheck %s < %t/GlobalNamespace/MyStruct.html --check-prefix=CHECK-HTML
 
+// RUN: clang-doc --format=mustache --output=%t --executor=standalone %s 
+// RUN: FileCheck %s < %t/html/GlobalNamespace/_ZTV8MyStruct.html --check-prefix=CHECK-MUSTACHE
+
 template <typename T>
 struct MyStruct {
   operator T();
@@ -16,3 +19,7 @@ struct MyStruct {
 
 // CHECK-HTML: <h3 id="{{[0-9A-F]*}}">operator T</h3>
 // CHECK-HTML: <p>public T operator T()</p>
+
+// CHECK-MUSTACHE: <div id="{{([0-9A-F]{40})}}">
+// CHECK-MUSTACHE:     <pre><code class="language-cpp code-clang-doc">T operator T ()</code></pre>
+// CHECK-MUSTACHE: </div>
