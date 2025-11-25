b817adf559272b70d346596cdea1c205c1300cf3
amtiwari <amtiwari@amd.com>
2025-11-25 04:14:56 -0500

    test3

--- clang/test/OpenMP/target_update_strided_ptr_multiple_messages_from.c
+++ clang/test/OpenMP/target_update_strided_ptr_multiple_messages_from.c
@@ -1,6 +1,8 @@
 // RUN: %clang_cc1 -verify -fopenmp -ferror-limit 100 %s -Wuninitialized
 // RUN: %clang_cc1 -verify -fopenmp-simd -ferror-limit 100 %s -Wuninitialized
 
+#include <stdlib.h>
+
 double *data;
 double *data1;
 double *data2;
