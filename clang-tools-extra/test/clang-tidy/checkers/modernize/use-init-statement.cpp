094cf136296120a89b6b39146050d3708fb9b758
denzor200 <denismikhaylov38@gmail.com>
2025-11-26 01:41:55 +0300

    improve tests

--- clang-tools-extra/test/clang-tidy/checkers/modernize/use-init-statement.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/modernize/use-init-statement.cpp
@@ -2,8 +2,11 @@
 
 void do_some();
 int get_with_possible_side_effects();
+enum class INITIALIZE_STATUS { OK, FAIL, PENDING };
+INITIALIZE_STATUS initialize(int& val);
+#define DUMMY_TOKEN // crutch because CHECK-FIXES unable to match empty string
 
-void normal() {
+void good() {
     int i1 = 0;
     if (i1 == 0) {
         do_some();
@@ -22,17 +25,20 @@ void normal() {
 // TODO: implement case for already if-init statement used
 // TODO: implement case for const and constexpr variables
 // TODO: implement case for multiple variables in one line
+// TODO: test for case when unused in condition but used in body (this is for abother check)
 void bad1() {
-    int i1 = 0;
+    int i1 = 0; DUMMY_TOKEN
     if (i1 == 0) {
 // CHECK-MESSAGES: [[@LINE-2]]:5: warning: variable 'i1' declaration before if statement could be moved into if init statement [modernize-use-init-statement]
-// CHECK-FIXES: if (int i1 = 0; i1 == 0) {
+// CHECK-FIXES: DUMMY_TOKEN
+// CHECK-FIXES-NEXT: if (int i1 = 0; i1 == 0) {
         do_some();
     }
-    int i2 = 0;
+    int i2 = 0; DUMMY_TOKEN
     switch (i2) {
 // CHECK-MESSAGES: [[@LINE-2]]:5: warning: variable 'i2' declaration before switch statement could be moved into switch init statement [modernize-use-init-statement]
-// CHECK-FIXES: switch (int i2 = 0; i2) {
+// CHECK-FIXES: DUMMY_TOKEN
+// CHECK-FIXES-NEXT: switch (int i2 = 0; i2) {
         case 0:
             do_some();
             break;
@@ -40,17 +46,19 @@ void bad1() {
 }
 
 void bad2() {
-    int i1 = 0;
+    int i1 = 0; DUMMY_TOKEN
     if (i1 == 0) {
 // CHECK-MESSAGES: [[@LINE-2]]:5: warning: variable 'i1' declaration before if statement could be moved into if init statement [modernize-use-init-statement]
-// CHECK-FIXES: if (int i1 = 0; i1 == 0) {
+// CHECK-FIXES: DUMMY_TOKEN
+// CHECK-FIXES-NEXT: if (int i1 = 0; i1 == 0) {
         do_some();
         ++i1;
     }
-    int i2 = 0;
+    int i2 = 0; DUMMY_TOKEN
     switch (i2) {
 // CHECK-MESSAGES: [[@LINE-2]]:5: warning: variable 'i2' declaration before switch statement could be moved into switch init statement [modernize-use-init-statement]
-// CHECK-FIXES: switch (int i2 = 0; i2) {
+// CHECK-FIXES: DUMMY_TOKEN
+// CHECK-FIXES-NEXT: switch (int i2 = 0; i2) {
         case 0:
             do_some();
             ++i2;
@@ -73,3 +81,22 @@ void bad2() {
 //             break;
 //     }
 // }
+
+void bad_unitialized() {
+    int i1; DUMMY_TOKEN
+    if (initialize(i1) == INITIALIZE_STATUS::OK) {
+// CHECK-MESSAGES: [[@LINE-2]]:5: warning: variable 'i1' declaration before if statement could be moved into if init statement [modernize-use-init-statement]
+// CHECK-FIXES: DUMMY_TOKEN
+// CHECK-FIXES-NEXT: if (int i1; initialize(i1) == INITIALIZE_STATUS::OK) {
+        do_some();
+    }
+    int i2; DUMMY_TOKEN
+    switch (initialize(i2)) {
+// CHECK-MESSAGES: [[@LINE-2]]:5: warning: variable 'i2' declaration before switch statement could be moved into switch init statement [modernize-use-init-statement]
+// CHECK-FIXES: DUMMY_TOKEN
+// CHECK-FIXES-NEXT: switch (int i2; initialize(i2)) {
+        case INITIALIZE_STATUS::OK:
+            do_some();
+            break;
+    }
+}
