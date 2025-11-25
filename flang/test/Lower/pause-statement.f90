b73c6c77f8a613323edb7f303f527c844b9308e9
Sathvik Reddy <sathvikreddy853@gmail.com>
2025-11-25 23:00:32 +0530

    [flang] Fix pause-statement testcase
    Remove hlfir and add an additional check for hlfir.declare

--- flang/test/Lower/pause-statement.f90
+++ flang/test/Lower/pause-statement.f90
@@ -1,9 +1,9 @@
-! RUN: bbc %s -emit-fir -hlfir=false --canonicalize -o - | FileCheck %s
+! RUN: bbc %s -emit-fir --canonicalize -o - | FileCheck %s
 
 ! CHECK-LABEL: pause_test
 subroutine pause_test()
   pause
-  ! CHECK: fir.call @_Fortran{{.*}}PauseStatement()
+  ! CHECK: fir.call @_FortranA{{.*}}PauseStatement()
   ! CHECK-NEXT: return
 end subroutine
 
@@ -11,21 +11,22 @@ end subroutine
 subroutine pause_code()
   pause 42
   ! CHECK: %[[c42:.*]] = arith.constant 42 : i32
-  ! CHECK: fir.call @_Fortran{{.*}}PauseStatementInt(%[[c42]])
+  ! CHECK: fir.call @_FortranA{{.*}}PauseStatementInt(%[[c42]])
   ! CHECK-NEXT: return
 end subroutine
 
 ! CHECK-LABEL: pause_msg
 subroutine pause_msg()
-  pause 'hello'
+  pause "hello"
   ! CHECK-DAG: %[[five:.*]] = arith.constant 5 : index
-  ! CHECK-DAG: %[[lit:.*]] = fir.address_of(@_QQ{{.*}}) : !fir.ref<!fir.char<1,5>>
-  ! CHECK-DAG: %[[buff:.*]] = fir.convert %[[lit]] : (!fir.ref<!fir.char<1,5>>) -> !fir.ref<i8>
+  ! CHECK-DAG: %[[addr:.*]] = fir.address_of(@_QQ{{.*}}) : !fir.ref<!fir.char<1,5>>
+  ! CHECK-DAG: %[[str:.*]]:2 = hlfir.declare %[[addr]] typeparams %[[five]] {fortran_attrs = #fir.var_attrs<parameter>, uniq_name = "_QQ{{.*}}"} : (!fir.ref<!fir.char<1,5>>, index) -> (!fir.ref<!fir.char<1,5>>, !fir.ref<!fir.char<1,5>>)
+  ! CHECK-DAG: %[[buff:.*]] = fir.convert %[[str]]#0 : (!fir.ref<!fir.char<1,5>>) -> !fir.ref<i8>
   ! CHECK-DAG: %[[len:.*]] = fir.convert %[[five]] : (index) -> i64
-  ! CHECK: fir.call @_Fortran{{.*}}PauseStatementText(%[[buff]], %[[len]])
+  ! CHECK: fir.call @_FortranA{{.*}}PauseStatementText(%[[buff]], %[[len]])
   ! CHECK-NEXT: return
 end subroutine
 
-! CHECK-DAG: func private @_Fortran{{.*}}PauseStatement
-! CHECK-DAG: func private @_Fortran{{.*}}PauseStatementInt
-! CHECK-DAG: func private @_Fortran{{.*}}PauseStatementText
+! CHECK-DAG: func private @_FortranA{{.*}}PauseStatement
+! CHECK-DAG: func private @_FortranA{{.*}}PauseStatementInt
+! CHECK-DAG: func private @_FortranA{{.*}}PauseStatementText
