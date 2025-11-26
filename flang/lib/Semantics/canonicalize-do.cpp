d01e2e9e06ba5c641b07f2fd47f2f8bea93e78be
Krzysztof Parzyszek <Krzysztof.Parzyszek@amd.com>
2025-11-26 07:49:12 -0600

    [flang][OpenMP] Reject END DO on construct that crosses label-DO
    In a label-DO construct where two or more loops share the same teminator,
    an OpenMP construct must enclose all the loops if an end-directive is
    present. E.g.
    
    ```
      do 100 i = 1,10
    !$omp do
        do 100 j = 1,10
        100 continue
    !$omp end do    ! Error, but ok if this line is removed
    ```
    
    OpenMP 5.1 and later no longer has this restriction.
    
    Fixes https://github.com/llvm/llvm-project/issues/169536.

--- flang/lib/Semantics/canonicalize-do.cpp
+++ flang/lib/Semantics/canonicalize-do.cpp
@@ -92,8 +92,11 @@ public:
                 [&](common::Indirection<OpenMPConstruct> &construct) {
                   // If the body of the OpenMP construct ends with a label,
                   // treat the label as ending the construct itself.
-                  CanonicalizeIfMatch(
-                      block, stack, i, omp::GetFinalLabel(construct.value()));
+                  OpenMPConstruct &omp{construct.value()};
+                  if (CanonicalizeIfMatch(
+                          block, stack, i, omp::GetFinalLabel(omp))) {
+                    MarkOpenMPConstruct(omp);
+                  }
                 },
             },
             executableConstruct->u);
@@ -103,12 +106,12 @@ public:
 
 private:
   template <typename T>
-  void CanonicalizeIfMatch(Block &originalBlock, std::vector<LabelInfo> &stack,
+  bool CanonicalizeIfMatch(Block &originalBlock, std::vector<LabelInfo> &stack,
       Block::iterator &i, Statement<T> &statement) {
-    CanonicalizeIfMatch(originalBlock, stack, i, statement.label);
+    return CanonicalizeIfMatch(originalBlock, stack, i, statement.label);
   }
 
-  void CanonicalizeIfMatch(Block &originalBlock, std::vector<LabelInfo> &stack,
+  bool CanonicalizeIfMatch(Block &originalBlock, std::vector<LabelInfo> &stack,
       Block::iterator &i, std::optional<Label> label) {
     if (!stack.empty() && label && stack.back().label == *label) {
       auto currentLabel{stack.back().label};
@@ -141,7 +144,26 @@ private:
         stack.pop_back();
       } while (!stack.empty() && stack.back().label == currentLabel);
       i = --next;
+      return true;
+    } else {
+      return false;
     }
+  }
+
+  void MarkOpenMPConstruct(OpenMPConstruct &omp) {
+    common::visit(
+        [](const auto &s) {
+          using S = std::decay_t<decltype(s)>;
+          if constexpr (std::is_base_of_v<OmpBlockConstruct, S> ||
+              std::is_same_v<OpenMPLoopConstruct, S>) {
+            const OmpDirectiveSpecification &beginSpec{s.BeginDir()};
+            auto &flags{
+                std::get<OmpDirectiveSpecification::Flags>(beginSpec.t)};
+            const_cast<OmpDirectiveSpecification::Flags &>(flags).set(
+                OmpDirectiveSpecification::Flag::CrossesLabelDo);
+          }
+        },
+        omp.u);
   }
 };
 
