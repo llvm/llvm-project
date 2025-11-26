c19124a3d4ce8a07f5be0b4c8f63fb77947e62c1
Ramkumar Ramachandra <ramkumar.ramachandra@codasip.com>
2025-11-26 15:07:04 +0000

    [MapVector] Introduce {keys,values} iterator, use in VPlan
    Similar to DenseMap::{keys,values}, introduce MapVector::{keys,values},
    and use it to simplify some code in VPlan.

--- llvm/include/llvm/ADT/MapVector.h
+++ llvm/include/llvm/ADT/MapVector.h
@@ -99,6 +99,26 @@ public:
     return try_emplace_impl(Key).first->second;
   }
 
+  [[nodiscard]] inline auto keys() {
+    return map_range(
+        Vector, [](const std::pair<KeyT, ValueT> &KV) { return KV.first; });
+  }
+
+  [[nodiscard]] inline auto values() {
+    return map_range(
+        Vector, [](const std::pair<KeyT, ValueT> &KV) { return KV.second; });
+  }
+
+  [[nodiscard]] inline auto keys() const {
+    return map_range(
+        Vector, [](const std::pair<KeyT, ValueT> &KV) { return KV.first; });
+  }
+
+  [[nodiscard]] inline auto values() const {
+    return map_range(
+        Vector, [](const std::pair<KeyT, ValueT> &KV) { return KV.second; });
+  }
+
   // Returns a copy of the value.  Only allowed if ValueT is copyable.
   [[nodiscard]] ValueT lookup(const KeyT &Key) const {
     static_assert(std::is_copy_constructible_v<ValueT>,
