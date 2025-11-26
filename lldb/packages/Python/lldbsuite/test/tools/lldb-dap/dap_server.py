5f995ef66902a997fe3a726a8d749732e04174ad
Janet Yang <qxy11@meta.com>
2025-11-25 17:10:33 -0800

    Add basic startDebugging test

--- lldb/packages/Python/lldbsuite/test/tools/lldb-dap/dap_server.py
+++ lldb/packages/Python/lldbsuite/test/tools/lldb-dap/dap_server.py
@@ -785,6 +785,7 @@ class DebugCommunication(object):
         *,
         program: Optional[str] = None,
         pid: Optional[int] = None,
+        debuggerId: Optional[int] = None,
         targetId: Optional[int] = None,
         waitFor=False,
         initCommands: Optional[list[str]] = None,
@@ -805,6 +806,8 @@ class DebugCommunication(object):
             args_dict["pid"] = pid
         if program is not None:
             args_dict["program"] = program
+        if debuggerId is not None:
+            args_dict["debuggerId"] = debuggerId
         if targetId is not None:
             args_dict["targetId"] = targetId
         if waitFor:
