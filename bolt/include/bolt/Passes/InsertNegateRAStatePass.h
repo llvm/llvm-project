b23fbd1723ce1af2e872b048aef031c8a804308b
Gergely Balint <gergely.balint@arm.com>
2025-11-26 12:51:04 +0000

    [BOLT] Update InsertNegateRAStatePass.h
    - remove unused declarations
    - update comments

--- bolt/include/bolt/Passes/InsertNegateRAStatePass.h
+++ bolt/include/bolt/Passes/InsertNegateRAStatePass.h
@@ -30,30 +30,19 @@ public:
 private:
   /// Because states are tracked as MCAnnotations on individual instructions,
   /// newly inserted instructions do not have a state associated with them.
-  /// Uses fillUnknownStateInBB, fillUnknownBlocksInCFG and fillUnknownStubs in
-  /// this order of priority.
+  /// Uses fillUnknownStateInBB and fillUnknownStubs.
   void inferUnknownStates(BinaryFunction &BF);
 
   /// Simple case: copy RAStates to unknown insts from previous inst.
-  /// Account for signing and authenticating insts.
+  /// If the first inst has unknown state, copy set it to the first known state.
+  /// Accounts for signing and authenticating insts.
   void fillUnknownStateInBB(BinaryContext &BC, BinaryBasicBlock &BB);
 
-  /// Fill unknown RAStates in BBs with no successors/predecessors. These are
-  /// Stubs inserted by LongJmp. As of #160989, we have to copy the RAState from
-  /// the previous BB in the layout, because CFIs are already incorrect here.
+  /// Fill in RAState in BasicBlocks consisting entirely of new instructions.
+  /// As of #160989, we have to copy the RAState from the previous BB in the
+  /// layout, because CFIs are already incorrect here.
   void fillUnknownStubs(BinaryFunction &BF);
 
-  /// Fills unknowns RAStates of BBs with successors/predecessors. Uses
-  /// getRAStateByCFG to determine the RAState. Does more than one iteration if
-  /// needed. Reports an error, if it cannot find the RAState for all BBs with
-  /// predecessors/successors.
-  void fillUnknownBlocksInCFG(BinaryFunction &BF);
-
-  /// For BBs which only hold instructions with unknown RAState, we check
-  /// CFG neighbors (successors, predecessors) of the BB. If they have different
-  /// RAStates, we report an inconsistency. Otherwise, we return the found
-  /// RAState.
-  std::optional<bool> getRAStateByCFG(BinaryBasicBlock &BB, BinaryFunction &BF);
   /// Returns the first known RAState from \p BB, or std::nullopt if all are
   /// unknown.
   std::optional<bool> getFirstKnownRAState(BinaryContext &BC,
