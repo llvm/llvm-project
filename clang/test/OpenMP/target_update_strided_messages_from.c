55b3977614bf93a078cc85b37af71e6728897350
amtiwari <amtiwari@amd.com>
2025-11-26 03:58:44 -0500

    non-contiguous_update_to_tests

--- clang/test/OpenMP/target_update_strided_messages_from.c
+++ clang/test/OpenMP/target_update_strided_messages_from.c
@@ -1,17 +1,12 @@
 // RUN: %clang_cc1 -verify -fopenmp -ferror-limit 100 %s -Wuninitialized
 // RUN: %clang_cc1 -verify -fopenmp-simd -ferror-limit 100 %s -Wuninitialized
 
-void foo(void) {}
-
 int main(int argc, char **argv) {
   int len = 8;
   double data[len];
   
   // Valid strided array sections
   #pragma omp target update from(data[0:4:2]) // OK
-  {}
-  
-  #pragma omp target update to(data[0:len/2:2]) // OK
   {}
   
   #pragma omp target update from(data[1:3:2]) // OK
