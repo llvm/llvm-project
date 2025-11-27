c69a74e435beb2a178cbf301f2498fe9a17d7ad8
BStott <Benjamin.Stott@sony.com>
2025-11-27 15:26:53 +0000

    Add regression test for pointer typename formatting

---
+++ compiler-rt/test/tysan/pointer_typename_demangling.cpp
@@ -0,0 +1,14 @@
+// RUN: %clangxx_tysan %s -o %t && %run %t 10 >%t.out.0 2>&1
+// RUN: FileCheck %s < %t.out.0
+
+namespace fancy_namespace {
+struct fancy_struct {
+  int member;
+};
+} // namespace fancy_namespace
+
+int main() {
+  fancy_namespace::fancy_struct *x = new fancy_namespace::fancy_struct{42};
+  *(float *)&x = 1.0f;
+  // CHECK: WRITE of size 4 at {{.*}} with type float accesses an existing object of type fancy_namespace::fancy_struct*
+}
