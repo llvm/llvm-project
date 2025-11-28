74ed72e67cac1e39424a22d63ba8363b7c94d380
Shreeyash Pandey <shreeyash335@gmail.com>
2025-11-28 23:19:18 +0530

    [libc][darwin] use os_sync_*_timeouts and clear tests

--- libc/test/src/__support/threads/darwin/mutex_test.cpp
+++ libc/test/src/__support/threads/darwin/mutex_test.cpp
@@ -13,13 +13,33 @@
 
 TEST(LlvmLibcSupportThreadsMutexTest, SmokeTest) {
   LIBC_NAMESPACE::Mutex mutex(0, 0, 0, 0);
-  ;
   ASSERT_EQ(mutex.lock(), LIBC_NAMESPACE::MutexError::NONE);
   ASSERT_EQ(mutex.unlock(), LIBC_NAMESPACE::MutexError::NONE);
   ASSERT_EQ(mutex.try_lock(), LIBC_NAMESPACE::MutexError::NONE);
   ASSERT_EQ(mutex.try_lock(), LIBC_NAMESPACE::MutexError::BUSY);
   ASSERT_EQ(mutex.unlock(), LIBC_NAMESPACE::MutexError::NONE);
   ASSERT_EQ(mutex.unlock(), LIBC_NAMESPACE::MutexError::UNLOCK_WITHOUT_LOCK);
+}
+
+TEST(LlvmLibcSupportThreadsRawMutexTest, Timeout) {
+  LIBC_NAMESPACE::RawMutex mutex;
+  ASSERT_TRUE(mutex.lock());
+  timespec ts;
+  LIBC_NAMESPACE::internal::clock_gettime(CLOCK_MONOTONIC, &ts);
+  ts.tv_sec += 1;
+  // Timeout will be respected when deadlock happens.
+  auto timeout = LIBC_NAMESPACE::internal::AbsTimeout::from_timespec(ts, false);
+  ASSERT_TRUE(timeout.has_value());
+  // The following will timeout
+  ASSERT_FALSE(mutex.lock(*timeout));
+  ASSERT_TRUE(mutex.unlock());
+  // Test that the mutex works after the timeout.
+  ASSERT_TRUE(mutex.lock());
+  ASSERT_TRUE(mutex.unlock());
+  // If a lock can be acquired directly, expired timeout will not count.
+  // Notice that the timeout is already reached during preivous deadlock.
+  ASSERT_TRUE(mutex.lock(*timeout));
+  ASSERT_TRUE(mutex.unlock());
 }
 
 // TODO(bojle): add other tests a la linux
