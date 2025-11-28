d3785dd740884b20159cb9b0c047fb96ab7bf9a4
Vedran Miletić <vedran@miletic.net>
2025-11-28 08:56:14 +0100

    Format

--- llvm/include/llvm/ADT/iterator.h
+++ llvm/include/llvm/ADT/iterator.h
@@ -267,7 +267,8 @@ public:
     return *static_cast<DerivedT *>(this);
   }
   using BaseT::operator-;
-  template <bool Enabled = bool(BaseT::IsRandomAccess), std::enable_if_t<Enabled, int> = 0>
+  template <bool Enabled = bool(BaseT::IsRandomAccess),
+            std::enable_if_t<Enabled, int> = 0>
   difference_type operator-(const DerivedT &RHS) const {
     static_assert(
         BaseT::IsRandomAccess,
