de10e3331c4be3ca348fc5527a437a63e4d467ae
skc7 <Krishna.Sankisa@amd.com>
2025-11-28 21:28:56 +0530

    Remove bitcast for resultPtr

--- mlir/lib/Target/LLVMIR/Dialect/OpenMP/OpenMPToLLVMIRTranslation.cpp
+++ mlir/lib/Target/LLVMIR/Dialect/OpenMP/OpenMPToLLVMIRTranslation.cpp
@@ -7218,10 +7218,6 @@ convertOmpGroupprivate(Operation &opInst, llvm::IRBuilderBase &builder,
       resultPtr = globalValue;
   }
 
-  llvm::Type *ptrTy = builder.getPtrTy();
-  if (resultPtr->getType() != ptrTy)
-    resultPtr = builder.CreateBitCast(resultPtr, ptrTy);
-
   moduleTranslation.mapValue(opInst.getResult(0), resultPtr);
   return success();
 }
