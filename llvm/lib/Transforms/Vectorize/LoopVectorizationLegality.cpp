b98557115ebd3e2f045c98875d0710fed9b70319
Florian Hahn <flo@fhahn.com>
2025-11-28 22:26:19 +0000

    [LV] Vectorize selecting last IV of min/max element. (#141431)
    Add support for vectorizing loops that select the index of the minimum
    or maximum element. The patch implements vectorizing those patterns by
    combining Min/Max and FindFirstIV reductions.
    
    It extends matching Min/Max reductions to allow in-loop users that are
    FindLastIV reductions. It records a flag indicating that the Min/Max
    reduction is used by another reduction. The extra user is then check as
    part of the new `handleMultiUseReductions` VPlan transformation.
    
    It processes any reduction that has other reduction users. The reduction
    using the min/max reduction currently must be a FindLastIV reduction,
    which needs adjusting to compute the correct result:
     1. We need to find the last IV for which the condition based on the
         min/max reduction is true,
     2. Compare the partial min/max reduction result to its final value and,
     3. Select the lanes of the partial FindLastIV reductions which
         correspond to the lanes matching the min/max reduction result.
    
    Depends on https://github.com/llvm/llvm-project/pull/140451
    
    PR: https://github.com/llvm/llvm-project/pull/141431

--- llvm/lib/Transforms/Vectorize/LoopVectorizationLegality.cpp
+++ llvm/lib/Transforms/Vectorize/LoopVectorizationLegality.cpp
@@ -877,6 +877,11 @@ bool LoopVectorizationLegality::canVectorizeInstr(Instruction &I) {
       Requirements->addExactFPMathInst(RedDes.getExactFPMathInst());
       AllowedExit.insert(RedDes.getLoopExitInstr());
       Reductions[Phi] = RedDes;
+      assert((!RedDes.hasUsesOutsideReductionChain() ||
+              RecurrenceDescriptor::isMinMaxRecurrenceKind(
+                  RedDes.getRecurrenceKind())) &&
+             "Only min/max recurrences are allowed to have multiple uses "
+             "currently");
       return true;
     }
 
