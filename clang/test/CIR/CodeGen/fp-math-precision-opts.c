516bd4b8cffdbf8dddb591c31771586a56f986fa
hhuebner <hendrik.huebner18@gmail.com>
2025-11-29 17:45:46 +0100

    fix test

--- clang/test/CIR/CodeGen/fp-math-precision-opts.c
+++ clang/test/CIR/CodeGen/fp-math-precision-opts.c
@@ -3,14 +3,39 @@
 // RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -Wno-unused-value -fclangir -emit-cir -ffast-math %s -o %t.cir
 // RUN: FileCheck --input-file=%t.cir %s -check-prefix=CIR-NO-ERRNO
 
+// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -Wno-unused-value -fclangir -emit-cir -fmath-errno -O1 %s -o %t.cir
+// RUN: FileCheck --input-file=%t.cir %s -check-prefix=CIR-ERRNO-O1
+// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -Wno-unused-value -fclangir -emit-cir -ffast-math -O1 %s -o %t.cir
+// RUN: FileCheck --input-file=%t.cir %s -check-prefix=CIR-NO-ERRNO-O1
+
+// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -Wno-unused-value -fclangir -emit-llvm -fmath-errno %s -o %t.cir
+// RUN: FileCheck --input-file=%t.cir %s -check-prefix=LLVM-ERRNO
+// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -Wno-unused-value -fclangir -emit-llvm -ffast-math %s -o %t.cir
+// RUN: FileCheck --input-file=%t.cir %s -check-prefix=LLVM-NO-ERRNO
+
+// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -Wno-unused-value -fclangir -emit-llvm -fmath-errno -O1 %s -o %t.cir
+// RUN: FileCheck --input-file=%t.cir %s -check-prefix=LLVM-ERRNO-O1
+// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -Wno-unused-value -fclangir -emit-llvm -ffast-math -O1 %s -o %t.cir
+// RUN: FileCheck --input-file=%t.cir %s -check-prefix=LLVM-NO-ERRNO-O1
+
+// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -Wno-unused-value -emit-llvm -fmath-errno %s -o %t.cir
+// RUN: FileCheck --input-file=%t.cir %s -check-prefix=OGCG-ERRNO
+// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -Wno-unused-value -emit-llvm -ffast-math %s -o %t.cir
+// RUN: FileCheck --input-file=%t.cir %s -check-prefix=OGCG-NO-ERRNO
+
+// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -Wno-unused-value -emit-llvm -fmath-errno -O1 %s -o %t.cir
+// RUN: FileCheck --input-file=%t.cir %s -check-prefix=OGCG-ERRNO-O1
+// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -Wno-unused-value -emit-llvm -ffast-math -O1 %s -o %t.cir
+// RUN: FileCheck --input-file=%t.cir %s -check-prefix=OGCG-NO-ERRNO-O1
+
 float test_normal(float f) {
   return __builtin_cosf(f);
   // CIR-ERRNO: cir.call @cosf
   // CIR-NO-ERRNO: cir.cos
   // LLVM-ERRNO: call float @cosf
   // LLVM-NO-ERRNO: call float @llvm.cos.f32
-  // OGCG-ERRNO: call float @cosf
-  // OGCG-NO-ERRNO: call float @llvm.cos.f32
+  // OGCG-ERRNO: call {{.*}} float @cosf
+  // OGCG-NO-ERRNO: call {{.*}} float @llvm.cos.f32
 }
 
 float test_precise(float f) {
@@ -20,9 +45,9 @@ float test_precise(float f) {
   // CIR-ERRNO: cir.call @cosf
   // CIR-NO-ERRNO: cir.call @cosf
   // LLVM-ERRNO: call float @cosf
-  // LLVM-NO-ERRNO: cir.call @cosf
-  // OGCG-ERRNO: call float @cosf
-  // OGCG-NO-ERRNO: cir.call @cosf
+  // LLVM-NO-ERRNO: call float @llvm.cos.f32
+  // OGCG-ERRNO: call {{.*}} float @cosf
+  // OGCG-NO-ERRNO: call {{.*}} float @llvm.cos.f32
 }
 
 float test_fast(float f) {
@@ -33,8 +58,8 @@ float test_fast(float f) {
   // CIR-NO-ERRNO-O1: cir.cos
   // LLVM-ERRNO-O1: call float @llvm.cos.f32
   // LLVM-NO-ERRNO-O1: call float @llvm.cos.f32
-  // OGCG-ERRNO-O1: call float @llvm.cos.f32
-  // OGCG-NO-ERRNO-O1: call float @llvm.cos.f32
+  // OGCG-ERRNO-O1: call {{.*}} float @llvm.cos.f32
+  // OGCG-NO-ERRNO-O1: call {{.*}} float @llvm.cos.f32
 }
 
 __attribute__((optnone))
@@ -44,7 +69,7 @@ float test_optnone(float f) {
   // CIR-ERRNO: cir.call @cosf
   // CIR-NO-ERRNO: cir.call @cosf
   // LLVM-ERRNO: call float @cosf
-  // LLVM-NO-ERRNO: cir.call @cosf
-  // OGCG-ERRNO: call float @cosf
-  // OGCG-NO-ERRNO: cir.call @cosf
+  // LLVM-NO-ERRNO: call float @cosf
+  // OGCG-ERRNO: call {{.*}} float @cosf
+  // OGCG-NO-ERRNO: call {{.*}} float @cosf
 }
