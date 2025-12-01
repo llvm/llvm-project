c4403e62e8b960ad925814fdc84e30b71b9707ca
Steven Wu <stevenwu@apple.com>
2025-12-01 09:33:45 +0800

    [𝘀𝗽𝗿] initial version
    Created using spr 1.3.7

--- llvm/unittests/CAS/CASTestConfig.h
+++ llvm/unittests/CAS/CASTestConfig.h
@@ -15,6 +15,10 @@
 #include "gtest/gtest.h"
 #include <memory>
 
+#ifdef _WIN32
+#include "llvm/Support/Windows/WindowsSupport.h"
+#endif
+
 namespace llvm::unittest::cas {
 class MockEnv {
   void anchor();
@@ -68,6 +72,10 @@ protected:
   }
 
   void SetUp() override {
+#ifdef _WIN32
+    if (GetWindowsOSVersion() < llvm::VersionTuple(10, 0, 0, 17763))
+      GTEST_SKIP() << "CAS tests skipped on older windows version";
+#endif
     NextCASIndex = 0;
     setMaxOnDiskCASMappingSize();
   }
