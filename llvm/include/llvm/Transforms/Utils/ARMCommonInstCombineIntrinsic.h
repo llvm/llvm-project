c52757d85b9a534ebd5bc3c4e5c1902dfe4037e8
valadaptive <valadaptive@protonmail.com>
2025-12-01 14:37:46 -0500

    [AArch64][ARM] Shorten file descriptions

--- llvm/include/llvm/Transforms/Utils/ARMCommonInstCombineIntrinsic.h
+++ llvm/include/llvm/Transforms/Utils/ARMCommonInstCombineIntrinsic.h
@@ -1,5 +1,4 @@
-//===- ARMCommonInstCombineIntrinsic.h -
-// instCombineIntrinsic opts for both ARM and AArch64 -----------*- C++ -*-===//
+//===- ARMCommonInstCombineIntrinsic.h - Shared ARM/AArch64 opts *- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
--- llvm/lib/Transforms/Utils/ARMCommonInstCombineIntrinsic.cpp
+++ llvm/lib/Transforms/Utils/ARMCommonInstCombineIntrinsic.cpp
@@ -1,5 +1,4 @@
-//===- ARMCommonInstCombineIntrinsic.cpp -
-//                  instCombineIntrinsic opts for both ARM and AArch64  ---===//
+//===- ARMCommonInstCombineIntrinsic.cpp - Shared ARM/AArch64 opts  -------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
