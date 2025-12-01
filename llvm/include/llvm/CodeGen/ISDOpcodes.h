75aa01b89553bf4213a3b0e83829b6d0689941b9
Phoebe Wang <phoebe.wang@intel.com>
2025-12-01 09:35:00 +0800

    Revert "LangRef: Clarify llvm.minnum and llvm.maxnum about sNaN and signed zero (#112852)" (#168838)
    This reverts commit 363b05944f9212511ee6811d0eb1af841c177226.
    
    This is a follow up of #166912. Sorry for not noticing the change at the
    beginning, but I disagree with both sNaN and signed zero semantics
    change.
    
    I have 3 justifications:
    
    - llvm.minnum and llvm.maxnum are common intrinsics, we cannot change
    the definition just because "some architectures" support the changed
    semantic. For example, X86 min/max instructions neither distinguish sNaN
    nor signed zero. We have to add couples of extra instructions to match
    with the new definition, which makes the intrinsics less efficient. But
    efficient is not the reason for the objection. I object because such
    cost is unnecessary;
    - As the example ``minnum(fadd(sNaN, -0.0), 1.0)`` shows, minnum/maxnum
    themself cannot guarantee consistent result if multiple FP arithmetic
    operations involved. It makes the sacrifice of performance totally
    unnecessary. `Behavior of Floating-Point NaN values` notes all NaNs can
    be treated as quiet NaNs unless using Constrained Floating-Point
    Intrinsics. So the cost is only worth for constrained minnum/maxnum ones
    if we want to define them;
    - Signed zero handling is unnecessary either, because even the C
    functions don't require it. If any other front ends require, they can
    use the existing fminnum_ieee/fmaxnum_ieee or define new intrinsics;
    
    Fixes: https://github.com/llvm/llvm-project/issues/138303 and
    https://github.com/llvm/llvm-project/issues/169122

--- llvm/include/llvm/CodeGen/ISDOpcodes.h
+++ llvm/include/llvm/CodeGen/ISDOpcodes.h
@@ -1048,20 +1048,13 @@ enum NodeType {
   LRINT,
   LLRINT,
 
-  /// FMINNUM/FMAXNUM - Perform floating-point minimum maximum on two values,
-  /// following IEEE-754 definitions except for signed zero behavior.
+  /// FMINNUM/FMAXNUM - Perform floating-point minimum or maximum on two
+  /// values.
   ///
-  /// If one input is a signaling NaN, returns a quiet NaN. This matches
-  /// IEEE-754 2008's minNum/maxNum behavior for signaling NaNs (which differs
-  /// from 2019).
+  /// In the case where a single input is a NaN (either signaling or quiet),
+  /// the non-NaN input is returned.
   ///
-  /// These treat -0 as ordered less than +0, matching the behavior of IEEE-754
-  /// 2019's minimumNumber/maximumNumber.
-  ///
-  /// Note that that arithmetic on an sNaN doesn't consistently produce a qNaN,
-  /// so arithmetic feeding into a minnum/maxnum can produce inconsistent
-  /// results. FMAXIMUN/FMINIMUM or FMAXIMUMNUM/FMINIMUMNUM may be better choice
-  /// for non-distinction of sNaN/qNaN handling.
+  /// The return value of (FMINNUM 0.0, -0.0) could be either 0.0 or -0.0.
   FMINNUM,
   FMAXNUM,
 
@@ -1075,9 +1068,6 @@ enum NodeType {
   ///
   /// These treat -0 as ordered less than +0, matching the behavior of IEEE-754
   /// 2019's minimumNumber/maximumNumber.
-  ///
-  /// Deprecated, and will be removed soon, as FMINNUM/FMAXNUM have the same
-  /// semantics now.
   FMINNUM_IEEE,
   FMAXNUM_IEEE,
 
