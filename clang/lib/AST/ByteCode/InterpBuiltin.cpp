f66695b33cf464886f24c2eebc44444e95507d38
adbox53 <123413623+adbox53@users.noreply.github.com>
2025-12-02 09:06:36 -0500

    Update clang/lib/AST/ByteCode/InterpBuiltin.cpp
    Co-authored-by: Timm Baeder <tbaeder@redhat.com>

--- clang/lib/AST/ByteCode/InterpBuiltin.cpp
+++ clang/lib/AST/ByteCode/InterpBuiltin.cpp
@@ -4626,8 +4626,8 @@ bool InterpretBuiltin(InterpState &S, CodePtr OpPC, const CallExpr *Call,
   case clang::X86::BI__builtin_ia32_pblendd256:
     return interp__builtin_ia32_shuffle_generic(
       S, OpPC, Call, [](unsigned DstIdx, unsigned ShuffleMask) {
-        // bit index for mask
-        unsigned maskBit = (ShuffleMask >> (DstIdx % 8)) & 0x1;
+        // Bit index for mask.
+        unsigned MaskBit = (ShuffleMask >> (DstIdx % 8)) & 0x1;
         unsigned SrcVecIdx = maskBit ? 1 : 0;  // 1 = TrueVec, 0 = FalseVec
         return std::pair<unsigned, int>{SrcVecIdx, static_cast<int>(DstIdx)};
       });
