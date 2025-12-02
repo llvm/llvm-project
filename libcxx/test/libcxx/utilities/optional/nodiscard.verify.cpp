6e305c14c9f16cc1379a3a9c95a4390c133325da
Hristo Hristov <hghristov.rmm@gmail.com>
2025-12-02 11:18:05 +0200

    Simplified tests

--- libcxx/test/libcxx/utilities/optional/nodiscard.verify.cpp
+++ libcxx/test/libcxx/utilities/optional/nodiscard.verify.cpp
@@ -18,38 +18,14 @@
 
 #include "test_macros.h"
 
-struct LVal {
-  constexpr std::optional<int> operator()(int&) { return 1; }
-  std::optional<int> operator()(const int&)  = delete;
-  std::optional<int> operator()(int&&)       = delete;
-  std::optional<int> operator()(const int&&) = delete;
-};
-
-struct CLVal {
-  std::optional<int> operator()(int&) = delete;
-  constexpr std::optional<int> operator()(const int&) { return 1; }
-  std::optional<int> operator()(int&&)       = delete;
-  std::optional<int> operator()(const int&&) = delete;
-};
-
-struct RVal {
-  std::optional<int> operator()(int&)       = delete;
-  std::optional<int> operator()(const int&) = delete;
-  constexpr std::optional<int> operator()(int&&) { return 1; }
-  std::optional<int> operator()(const int&&) = delete;
-};
-
-struct CRVal {
-  std::optional<int> operator()(int&)       = delete;
-  std::optional<int> operator()(const int&) = delete;
-  std::optional<int> operator()(int&&)      = delete;
-  constexpr std::optional<int> operator()(const int&&) { return 1; }
-};
-
 void test() {
+  // [optional.bad.access]
+
   std::bad_optional_access ex;
 
   ex.what(); // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
+
+  // [optional.optional]
 
   std::optional<int> opt;
   const std::optional<int> cOpt;
@@ -74,38 +50,40 @@ void test() {
   std::move(cOpt).value(); // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
 
   // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
-  opt.value_or(82);
+  opt.value_or(94);
   // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
-  cOpt.value_or(82);
+  cOpt.value_or(94);
   // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
-  std::move(opt).value_or(82);
+  std::move(opt).value_or(94);
   // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
-  std::move(cOpt).value_or(82);
+  std::move(cOpt).value_or(94);
 
 #if TEST_STD_VER >= 23
   // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
-  opt.and_then(LVal{});
+  opt.and_then([](int&) { return std::optional<int>{82}; });
   // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
-  cOpt.and_then(CLVal{});
+  cOpt.and_then([](const int&) { return std::optional<int>{82}; });
   // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
-  std::move(opt).and_then(RVal{});
+  std::move(opt).and_then([](int&&) { return std::optional<int>{82}; });
   // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
-  std::move(cOpt).and_then(CRVal{});
+  std::move(cOpt).and_then([](const int&&) { return std::optional<int>{82}; });
 
   // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
-  opt.transform(LVal{});
+  opt.transform([](int&) { return std::optional<int>{94}; });
   // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
-  cOpt.transform(CLVal{});
+  cOpt.transform([](const int&) { return std::optional<int>{94}; });
   // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
-  std::move(opt).transform(RVal{});
+  std::move(opt).transform([](int&&) { return std::optional<int>{94}; });
   // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
-  std::move(cOpt).transform(CRVal{});
+  std::move(cOpt).transform([](const int&&) { return std::optional<int>{94}; });
 
   // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
   opt.or_else([] { return std::optional<int>{82}; });
   // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
   std::move(opt).or_else([] { return std::optional<int>{82}; });
 #endif // TEST_STD_VER >= 23
+
+  // [optional.optional.ref]
 
 #if TEST_STD_VER >= 26
   int z = 94;
@@ -128,9 +106,9 @@ void test() {
   cOptRef.value_or(z);
 
   // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
-  optRef.and_then([](int&) { return std::optional<int>{82}; });
+  optRef.and_then([](int&) { return std::optional<int>{94}; });
   // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
-  cOptRef.and_then([](int&) { return std::optional<int>{82}; });
+  cOptRef.and_then([](int&) { return std::optional<int>{94}; });
 
   // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
   optRef.transform([](int&) { return std::optional<int>{82}; });
@@ -143,12 +121,16 @@ void test() {
   std::move(optRef).or_else([] { return std::optional<int&>{}; });
 #endif // TEST_STD_VER >= 26
 
+  // [optional.specalg]
+
   // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
   std::make_optional(82);
   // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
   std::make_optional<int>('h');
   // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
   std::make_optional<std::string>({'z', 'm', 't'});
+
+  // [optional.hash]
 
   std::hash<std::optional<int>> hash;
 
