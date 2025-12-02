2da2524e161a8d84dc1a3cdd9de6350b33830502
ebinjose02 <ebin371@gmail.com>
2025-12-02 06:28:01 +0000

    Modified CGBuiltins Added test to an already existin test file

--- clang/test/CodeGen/builtin_bswapg.c
+++
@@ -1,8 +0,0 @@
-// RUN: %clang_cc1 -triple x86_64 -O0 -emit-llvm -o - %s | FileCheck %s
-#include <stdbool.h>
-bool test_bswapg(bool c) {
-  return __builtin_bswapg(c);
-}
-
-// CHECK-LABEL: define{{.*}} i1 @_Z11test_bswapgb(
-// CHECK: ret i1 %{{.*}}
