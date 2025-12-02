909027d9596762c02f891fd46f29c8600a21922e
changkhothuychung <nhat7203@gmail.com>
2025-12-02 17:58:28 -0500

    fix test default constructor

--- libcxx/test/std/ranges/range.adaptors/range.concat/ctor.default.pass.cpp
+++ libcxx/test/std/ranges/range.adaptors/range.concat/ctor.default.pass.cpp
@@ -11,9 +11,6 @@
 #include <cassert>
 #include <ranges>
 #include <type_traits>
-#include <vector>
-
-constexpr int buff[] = {1, 2, 3, 4};
 
 struct DefaultConstructibleView : std::ranges::view_base {
   constexpr DefaultConstructibleView() : begin_(buff), end_(buff + 4) {}
@@ -31,94 +28,14 @@ struct NoDefaultView : std::ranges::view_base {
   int* end() const;
 };
 
-struct NoexceptView : std::ranges::view_base {
-  NoexceptView() noexcept;
-  int const* begin() const;
-  int const* end() const;
-};
+using DefaultView = std::ranges::concat_view<DefaultConstructibleView, DefaultConstructibleView>;
+using BadView1    = std::ranges::concat_view<DefaultConstructibleView, NoDefaultView>;
+using BadView2    = std::ranges::concat_view<NoDefaultView, NoDefaultView>;
 
-struct HelperView : std::ranges::view_base {
-  constexpr HelperView(const int* begin, const int* end) : begin_(begin), end_(end) {}
-  constexpr int const* begin() const { return begin_; }
-  constexpr int const* end() const { return end_; }
-
-private:
-  int const* begin_;
-  int const* end_;
-};
-
-constexpr void test_with_one_view() {
-  {
-    using View = std::ranges::concat_view<DefaultConstructibleView>;
-    View view;
-    auto it  = view.begin();
-    auto end = view.end();
-    assert(*it++ == 1);
-    assert(*it++ == 2);
-    assert(*it++ == 3);
-    assert(*it++ == 4);
-    assert(it == end);
-  }
-}
-
-constexpr void test_with_more_than_one_view() {
-  {
-    // 2 views are of same type
-    using View = std::ranges::concat_view<HelperView, HelperView>;
-    int arr1[] = {1, 2};
-    int arr2[] = {3, 4};
-    HelperView range1(arr1, arr1 + 2);
-    HelperView range2(arr2, arr2 + 2);
-    View view(range1, range2);
-    auto it  = view.begin();
-    auto end = view.end();
-    assert(*it++ == 1);
-    assert(*it++ == 2);
-    assert(*it++ == 3);
-    assert(*it++ == 4);
-    assert(it == end);
-  }
-
-  {
-    // 2 views are of different types
-    using View = std::ranges::concat_view<HelperView, DefaultConstructibleView>;
-    int arr1[] = {1, 2};
-    HelperView range1(arr1, arr1 + 2);
-    DefaultConstructibleView range2;
-    View view(range1, range2);
-    auto it  = view.begin();
-    auto end = view.end();
-    assert(*it++ == 1);
-    assert(*it++ == 2);
-    assert(*it++ == 1);
-    assert(*it++ == 2);
-    assert(*it++ == 3);
-    assert(*it++ == 4);
-    assert(it == end);
-  }
-}
-
-constexpr bool tests() {
-  test_with_one_view();
-  test_with_more_than_one_view();
-
-  // Check cases where the default constructor isn't provided
-  {
-    static_assert(!std::is_default_constructible_v<std::ranges::concat_view<NoDefaultView >>);
-  }
-
-  // Check noexcept-ness
-  {
-    {
-      using View = std::ranges::concat_view<DefaultConstructibleView>;
-      static_assert(!noexcept(View()));
-    }
-    {
-      using View = std::ranges::concat_view<NoexceptView>;
-      static_assert(noexcept(View()));
-    }
-  }
-
+constexpr bool test() {
+  static_assert(std::is_default_constructible_v<DefaultView>);
+  static_assert(!std::is_default_constructible_v<BadView1>);
+  static_assert(!std::is_default_constructible_v<BadView2>);
   return true;
 }
 
