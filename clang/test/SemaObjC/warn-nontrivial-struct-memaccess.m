a92d09040831c7b83326d6141ea68e8548229173
Oliver Hunt <oliver@apple.com>
2025-12-03 15:59:48 -0800

    [clang] warn_cstruct_memaccess and warn_cxxstruct_memaccess are too agressive about initializers
    These warnings are triggered for zeroing initializers of non-trivially
    initializable and non-trivially copyable types.
    
    This results in significant numbers of warnings in idiomatic object
    initialization code, where memset and similar are used to ensure no
    stale data is present in fields or padding bytes.
    
    Addresses #156996

--- clang/test/SemaObjC/warn-nontrivial-struct-memaccess.m
+++ clang/test/SemaObjC/warn-nontrivial-struct-memaccess.m
@@ -12,28 +12,51 @@ struct Trivial {
 
 struct NonTrivial0 {
   int f0;
-  __weak id f1; // expected-note 2 {{non-trivial to default-initialize}} expected-note 2 {{non-trivial to copy}}
+  __weak id f1;// #NonTrivial0_f1
   volatile int f2;
   id f3[10]; // expected-note 2 {{non-trivial to default-initialize}} expected-note 2 {{non-trivial to copy}}
 };
 
 struct NonTrivial1 {
-  id f0; // expected-note 2 {{non-trivial to default-initialize}} expected-note 2 {{non-trivial to copy}}
+  id f0; // #NonTrivial1_f0
   int f1;
   struct NonTrivial0 f2;
 };
 
-void testTrivial(struct Trivial *d, struct Trivial *s) {
+void testNonTrivial0(struct NonTrivial1 *d, struct NonTrivial1 *s, int i) {
+  memset(d, 0, sizeof(struct NonTrivial1));
+}
+void testTrivial(struct Trivial *d, struct Trivial *s, int i) {
   memset(d, 0, sizeof(struct Trivial));
+  memset(d, 1, sizeof(struct Trivial));
+  memset(d, i, sizeof(struct Trivial));
   bzero(d, sizeof(struct Trivial));
   memcpy(d, s, sizeof(struct Trivial));
   memmove(d, s, sizeof(struct Trivial));
 }
 
-void testNonTrivial1(struct NonTrivial1 *d, struct NonTrivial1 *s) {
-  memset(d, 0, sizeof(struct NonTrivial1)); // expected-warning {{that is not trivial to primitive-default-initialize}} expected-note {{explicitly cast the pointer to silence}}
+void testNonTrivial1(struct NonTrivial1 *d, struct NonTrivial1 *s, int i) {
+  memset(d, 0, sizeof(struct NonTrivial1));
+  memset(d, 1, sizeof(struct NonTrivial1)); // #memset_d_1
+  // expected-warning@#memset_d_1 {{that is not trivial to primitive-default-initialize}}
+  // expected-note@#NonTrivial1_f0 {{non-trivial to default-initialize}}
+  // expected-note@#NonTrivial0_f1 {{non-trivial to default-initialize}}
+  // expected-note@#memset_d_1 {{explicitly cast the pointer to silence}}
+  memset(d, i, sizeof(struct NonTrivial1)); // #memset_d_i
+  // expected-warning@#memset_d_i {{that is not trivial to primitive-default-initialize}}
+  // expected-note@#NonTrivial1_f0 {{non-trivial to default-initialize}}
+  // expected-note@#NonTrivial0_f1 {{non-trivial to default-initialize}}
+  // expected-note@#memset_d_i {{explicitly cast the pointer to silence}}
   memset((void *)d, 0, sizeof(struct NonTrivial1));
-  bzero(d, sizeof(struct NonTrivial1)); // expected-warning {{that is not trivial to primitive-default-initialize}} expected-note {{explicitly cast the pointer to silence}}
-  memcpy(d, s, sizeof(struct NonTrivial1)); // expected-warning {{that is not trivial to primitive-copy}} expected-note {{explicitly cast the pointer to silence}}
-  memmove(d, s, sizeof(struct NonTrivial1)); // expected-warning {{that is not trivial to primitive-copy}} expected-note {{explicitly cast the pointer to silence}}
+  bzero(d, sizeof(struct NonTrivial1));
+  memcpy(d, s, sizeof(struct NonTrivial1)); // #memcpy_d
+  // expected-warning@#memcpy_d {{that is not trivial to primitive-copy}}
+  // expected-note@#NonTrivial1_f0 {{non-trivial to copy}}
+  // expected-note@#NonTrivial0_f1 {{non-trivial to copy}}
+  // expected-note@#memcpy_d {{explicitly cast the pointer to silence}}
+  memmove(d, s, sizeof(struct NonTrivial1)); // #memmove_d
+  // expected-warning@#memmove_d {{that is not trivial to primitive-copy}}
+  // expected-note@#NonTrivial1_f0 {{non-trivial to copy}}
+  // expected-note@#NonTrivial0_f1 {{non-trivial to copy}}
+  // expected-note@#memmove_d {{explicitly cast the pointer to silence}}
 }
