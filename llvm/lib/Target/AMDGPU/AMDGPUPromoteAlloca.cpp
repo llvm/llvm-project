80fb5a41eb4d5f0a2dbba5a5d51e192492d6e6da
Kevin Choi <kevin.choi@amd.com>
2025-12-03 12:38:44 -0600

    addressing feedback

--- llvm/lib/Target/AMDGPU/AMDGPUPromoteAlloca.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPUPromoteAlloca.cpp
@@ -86,7 +86,7 @@ static cl::opt<unsigned>
                    cl::init(4));
 
 static cl::opt<unsigned> DynIdxNumElmLimit(
-    "dynamic-index-num-element-limit",
+    "amdgpu-dynamic-index-num-element-limit",
     cl::desc("Maximum number of elements for promoting alloca with dynamic"
              " index"),
     cl::init(8));
@@ -927,13 +927,8 @@ bool AMDGPUPromoteAllocaImpl::tryPromoteAllocaToVector(AllocaInst &Alloca) {
         return RejectUser(Inst, "cannot compute vector index for GEP");
 
       if (!isa<ConstantInt>(Index)) {
-        bool UsedInLoad = false;
-        for (auto *U : GEP->users()) {
-          if (isa<LoadInst>(U)) {
-            UsedInLoad = true;
-            break;
-          }
-        }
+        bool UsedInLoad = llvm::any_of(GEP->users(), 
+            [&](const auto *U){ return isa<LoadInst>(U); });
         if (auto *UserVecTy =
                 dyn_cast<FixedVectorType>(GEP->getSourceElementType())) {
           if (UsedInLoad && UserVecTy->getNumElements() > DynIdxNumElmLimit) {
