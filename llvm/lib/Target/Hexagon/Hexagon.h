ef7337cb2a6e6dd5dd7b707d271d5b7c4028e272
Fateme Hosseini <quic_fhossein@quicinc.com>
2025-12-03 16:14:38 -0600

    [Hexagon] Passes for widening vector operations and shuffle opt (#169559)
    Introduce Hexagon-specific passes to generate widening vector
    instructions for integer and floating-point operations using generic
    LLVM intrinsics. This enables widening operations for short vectors and
    improves type legalization by allowing operands to be widened to
    appropriate types. The patch also includes a shuffle optimization pass
    to relocate and validate shufflevector instructions during widening
    legalization.
    
    Co-authored-by: Jyotsna Verma <jverma@qti.qualcomm.com>
    Co-authored-by: Yashas Andaluri <yandalur@qti.qualcomm.com>
    Co-authored-by: Fateme Hosseini <fhossein@qti.qualcomm.com>
    Co-authored-by: Muntasir Mallick <mallick@qti.qualcomm.com>
    Co-authored-by: Tatiana Larina <larina@qti.qualcomm.com>
    Co-authored-by: Kaushik Kulkarni <kauskulk@qti.qualcomm.com>
    
    Co-authored-by: Jyotsna Verma <jverma@qti.qualcomm.com>
    Co-authored-by: Yashas Andaluri <yandalur@qti.qualcomm.com>
    Co-authored-by: Muntasir Mallick <mallick@qti.qualcomm.com>
    Co-authored-by: Tatiana Larina <larina@qti.qualcomm.com>
    Co-authored-by: Kaushik Kulkarni <kauskulk@qti.qualcomm.com>

--- llvm/lib/Target/Hexagon/Hexagon.h
+++ llvm/lib/Target/Hexagon/Hexagon.h
@@ -92,6 +92,9 @@ FunctionPass *createHexagonGenInsert();
 FunctionPass *createHexagonGenMemAbsolute();
 FunctionPass *createHexagonGenMux();
 FunctionPass *createHexagonGenPredicate();
+FunctionPass *
+createHexagonGenWideningVecFloatInstr(const HexagonTargetMachine &);
+FunctionPass *createHexagonGenWideningVecInstr(const HexagonTargetMachine &);
 FunctionPass *createHexagonHardwareLoops();
 FunctionPass *createHexagonISelDag(HexagonTargetMachine &TM,
                                    CodeGenOptLevel OptLevel);
@@ -102,6 +105,7 @@ FunctionPass *createHexagonMergeActivateWeight();
 FunctionPass *createHexagonNewValueJump();
 FunctionPass *createHexagonOptAddrMode();
 FunctionPass *createHexagonOptimizeSZextends();
+FunctionPass *createHexagonOptShuffleVector(const HexagonTargetMachine &);
 FunctionPass *createHexagonPacketizer(bool Minimal);
 FunctionPass *createHexagonPeephole();
 FunctionPass *createHexagonRDFOpt();
