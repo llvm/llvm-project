8647b5c719a7d91c3dbd3954b022621c3b550aaf
Priyanshu Kumar <10b.priyanshu@gmail.com>
2025-12-04 19:41:21 +0530

    Update clang/test/CIR/CodeGen/X86/cir-sqrt-builtins.c
    Co-authored-by: Andy Kaylor <akaylor@nvidia.com>

--- clang/test/CIR/CodeGen/X86/cir-sqrt-builtins.c
+++ clang/test/CIR/CodeGen/X86/cir-sqrt-builtins.c
@@ -2,7 +2,11 @@
 // Test X86-specific sqrt builtins
 
 // RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fclangir -emit-cir %s -o %t.cir
-// RUN: FileCheck --input-file=%t.cir %s
+// RUN: FileCheck --check-prefix=CIR --input-file=%t.cir %s
+// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fclangir -emit-llvm %s -o %t-cir.ll
+// RUN: FileCheck --check-prefix=LLVM --input-file=%t-cir.ll %s
+// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -emit-llvm %s -o %t.ll
+// RUN: FileCheck --check-prefix=OGCG --input-file=%t.ll %s
 
 // Test __builtin_ia32_sqrtph512
 __m512h test_sqrtph512(__m512h a) {
