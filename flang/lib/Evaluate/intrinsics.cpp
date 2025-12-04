4b2714f12f592ef3237056759dbe735d43fbeec1
Valery Dmitriev <valeryd@nvidia.com>
2025-12-04 08:29:31 -0800

    [flang/flang-rt] Implement show_descriptor intrinsic, a non-standard extension. (#170389)
    This is a reapply the original patch (#169137) with the flang-rt unit
    test changes limiting it to linux platform only.
    Additionally accommodated style changes from Peter Klausler (#170227)
        
    show_descriptor intrinsic prints details of a descriptor (extended
    Fortran pointer).
    It accepts a descriptor for any type and rank, including scalars.
    Requires use of flang_debug module.
        
    Example:
    ```
        program test
          use flang_debug
          implicit none
          integer :: a(4) = (/ 1,3,5,7 /)
          call show_descriptor(a(1:3))
        end program test
    ```    
    and its output:
    ```
        Descriptor @ 0x7ffe01ec6a98:
          base_addr 0x563b7035103c
          elem_len  4
          version   20240719
          rank      1
          type      9 "INTEGER(kind=4)"
          attribute 0
          extra     0
            addendum  0
            alloc_idx 0
    ```

--- flang/lib/Evaluate/intrinsics.cpp
+++ flang/lib/Evaluate/intrinsics.cpp
@@ -1715,6 +1715,8 @@ static const IntrinsicInterface intrinsicSubroutine[]{
         {}, Rank::scalar, IntrinsicClass::impureSubroutine},
     {"second", {{"time", DefaultReal, Rank::scalar}}, {}, Rank::scalar,
         IntrinsicClass::impureSubroutine},
+    {"__builtin_show_descriptor", {{"d", AnyData, Rank::anyOrAssumedRank}}, {},
+        Rank::elemental, IntrinsicClass::impureSubroutine},
     {"system",
         {{"command", DefaultChar, Rank::scalar},
             {"exitstat", DefaultInt, Rank::scalar, Optionality::optional,
