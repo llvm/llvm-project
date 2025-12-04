888b1ff522b1798682c43f0d048539e7fc42f6b0
Paul Kirth <paulkirth@google.com>
2025-12-04 13:35:53 -0800

    Ignore unnecessary parts of IR

--- clang/test/CodeGenCXX/aggregate-lifetime-invoke.cpp
+++ clang/test/CodeGenCXX/aggregate-lifetime-invoke.cpp
@@ -1,4 +1,6 @@
-// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -emit-llvm -o - %s -O1 -fexceptions -fcxx-exceptions | FileCheck %s
+// RUN: %clang_cc1 -emit-llvm -o - %s -O1 -fexceptions -fcxx-exceptions | FileCheck %s
+
+extern "C" {
 
 struct Trivial {
   int x[100];
@@ -6,23 +8,23 @@ struct Trivial {
 
 void func_that_throws(Trivial t);
 
-// CHECK-LABEL: define dso_local void @_Z4testv(){{.*}} personality ptr @__gxx_personality_v0
+// CHECK-LABEL: define{{.*}} void @test()
 void test() {
-  // CHECK: %[[AGG1:.*]] = alloca %struct.Trivial, align
-  // CHECK: %[[AGG2:.*]] = alloca %struct.Trivial, align
+  // CHECK: %[[AGG1:.*]] = alloca %struct.Trivial
+  // CHECK: %[[AGG2:.*]] = alloca %struct.Trivial
 
-  // CHECK: call void @llvm.lifetime.start.p0(ptr nonnull %[[AGG1]])
-  // CHECK: invoke void @_Z16func_that_throws7Trivial(ptr noundef nonnull byval(%struct.Trivial) align 8 %[[AGG1]])
+  // CHECK: call void @llvm.lifetime.start.p0(ptr{{.*}} %[[AGG1]])
+  // CHECK: invoke void @func_that_throws(ptr{{.*}} %[[AGG1]])
   // CHECK-NEXT: to label %[[CONT1:.*]] unwind label %[[LPAD1:.*]]
 
   // CHECK: [[CONT1]]:
-  // CHECK-NEXT: call void @llvm.lifetime.start.p0(ptr nonnull %[[AGG2]])
-  // CHECK: invoke void @_Z16func_that_throws7Trivial(ptr noundef nonnull byval(%struct.Trivial) align 8 %[[AGG2]])
+  // CHECK-NEXT: call void @llvm.lifetime.start.p0(ptr{{.*}} %[[AGG2]])
+  // CHECK: invoke void @func_that_throws(ptr{{.*}} %[[AGG2]])
   // CHECK-NEXT: to label %[[CONT2:.*]] unwind label %[[LPAD2:.*]]
 
   // CHECK: [[CONT2]]:
-  // CHECK-DAG: call void @llvm.lifetime.end.p0(ptr nonnull %[[AGG2]])
-  // CHECK-DAG: call void @llvm.lifetime.end.p0(ptr nonnull %[[AGG1]])
+  // CHECK-DAG: call void @llvm.lifetime.end.p0(ptr{{.*}} %[[AGG2]])
+  // CHECK-DAG: call void @llvm.lifetime.end.p0(ptr{{.*}} %[[AGG1]])
   // CHECK: br label %[[TRY_CONT:.*]]
 
   // CHECK: [[LPAD1]]:
@@ -31,15 +33,15 @@ void test() {
 
   // CHECK: [[LPAD2]]:
   // CHECK: landingpad
-  // CHECK: call void @llvm.lifetime.end.p0(ptr nonnull %[[AGG2]])
+  // CHECK: call void @llvm.lifetime.end.p0(ptr{{.*}} %[[AGG2]])
   // CHECK: br label %[[EHCLEANUP]]
 
   // CHECK: [[EHCLEANUP]]:
-  // CHECK: call void @llvm.lifetime.end.p0(ptr nonnull %[[AGG1]])
-  // CHECK: call ptr @__cxa_begin_catch
+  // CHECK: call void @llvm.lifetime.end.p0(ptr{{.*}} %[[AGG1]])
   try {
     func_that_throws(Trivial{0});
     func_that_throws(Trivial{0});
   } catch (...) {
   }
 }
+} // end extern "C"
