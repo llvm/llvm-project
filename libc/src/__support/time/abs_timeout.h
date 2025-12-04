2f62af3fe4a1bfd7bfef14e7ea2379304942550b
Shreeyash Pandey <shreeyash335@gmail.com>
2025-12-04 18:55:46 +0530

    [libc] header to 80

--- libc/src/__support/time/abs_timeout.h
+++ libc/src/__support/time/abs_timeout.h
@@ -1,10 +1,10 @@
-//===--- absolute timeout ---------------------------------*- C++ -*-===//
+//===--- absolute timeout ---------------------------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
-//===----------------------------------------------------------------===//
+//===----------------------------------------------------------------------===//
 
 #ifndef LLVM_LIBC_SRC___SUPPORT_TIME_ABS_TIMEOUT_H
 #define LLVM_LIBC_SRC___SUPPORT_TIME_ABS_TIMEOUT_H
--- libc/src/__support/time/monotonicity.h
+++ libc/src/__support/time/monotonicity.h
@@ -1,10 +1,10 @@
-//===--- timeout implementation ---------------------------*- C++ -*-===//
+//===--- timeout implementation ---------------------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
-//===----------------------------------------------------------------===//
+//===----------------------------------------------------------------------===//
 
 #ifndef LLVM_LIBC_SRC___SUPPORT_TIME_MONOTONICITY_H
 #define LLVM_LIBC_SRC___SUPPORT_TIME_MONOTONICITY_H
