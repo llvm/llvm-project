c4b3104ddda1d1ce4b91c88a5094a48744d3cd5a
Luke Lau <luke@igalia.com>
2025-12-04 14:27:56 +0800

    Include <cmath> for std::round
    I guess the AArch64 system headers transitively include it somehow?

--- llvm/lib/Transforms/Vectorize/LoopVectorize.cpp
+++ llvm/lib/Transforms/Vectorize/LoopVectorize.cpp
@@ -146,6 +146,7 @@
 #include "llvm/Transforms/Vectorize/LoopVectorizationLegality.h"
 #include <algorithm>
 #include <cassert>
+#include <cmath>
 #include <cstdint>
 #include <functional>
 #include <iterator>
