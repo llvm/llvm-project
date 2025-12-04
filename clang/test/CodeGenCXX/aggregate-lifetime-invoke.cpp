c459e4cc9f662a855a436c0f4fa918447d77130e
Paul Kirth <paulkirth@google.com>
2025-12-04 14:51:46 -0800

    Use triple in RUN line

--- clang/test/CodeGenCXX/aggregate-lifetime-invoke.cpp
+++ clang/test/CodeGenCXX/aggregate-lifetime-invoke.cpp
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -emit-llvm -o - %s -O1 -fexceptions -fcxx-exceptions | FileCheck %s
+// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -emit-llvm -o - %s -O1 -fexceptions -fcxx-exceptions | FileCheck %s
 
 extern "C" {
 
