7d07f66e98a9625bad42ebee49180cd1da09904f
Andre Kuhlenschmidt <akuhlenschmi@nvidia.com>
2025-12-05 13:52:42 -0800

    reformat

--- flang/include/flang/Semantics/symbol.h
+++ flang/include/flang/Semantics/symbol.h
@@ -875,20 +875,20 @@ public:
   const Scope *scope() const { return scope_; }
   void set_scope(Scope *scope) { scope_ = scope; }
   std::optional<std::size_t> sizeOpt() const { return size_; }
-  std::size_t size() const { 
-    // NOTE: I tried to insert the following check, but there are places where we are
-    // relying on the fact that the size is 0 if it is not set.
+  std::size_t size() const {
+    // NOTE: I tried to insert the following check, but there are places where
+    // we are relying on the fact that the size is 0 if it is not set.
     // CHECK(size_.has_value());
-    return size_.value_or(0); 
+    return size_.value_or(0);
   }
   void set_size(std::size_t size) { size_ = size; }
   void set_size(std::optional<std::size_t> size) { size_ = size; }
   std::optional<std::size_t> offsetOpt() const { return offset_; }
-  std::size_t offset() const { 
-    // NOTE: I tried to insert the following check, but there are places where we are
-    // relying on the fact that the offset is 0 if it is not set.
+  std::size_t offset() const {
+    // NOTE: I tried to insert the following check, but there are places where
+    // we are relying on the fact that the offset is 0 if it is not set.
     // CHECK(offset_.has_value());
-    return offset_.value_or(0); 
+    return offset_.value_or(0);
   }
   void set_offset(std::size_t offset) { offset_ = offset; }
   void set_offset(std::optional<std::size_t> offset) { offset_ = offset; }
