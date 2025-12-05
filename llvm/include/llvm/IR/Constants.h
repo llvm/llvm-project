336f9cb7f247cf5f9b9234d72e9beda93e779ef3
Nikita Popov <npopov@redhat.com>
2025-12-05 15:53:50 +0100

    [IR] Add ImplicitTrunc argument to ConstantInt::get()
    Add an ImplicitTrunc argument to ConstantInt::get(), which allows
    controlling whether implicit truncation of the value is permitted.
    
    This argument currently defaults to true, but will be switched to
    false in the future to guard against signed/unsigned confusion,
    similar to what has already happened for APInt.
    
    The argument gives an opt-out for cases where the truncation is
    intended. The patch contains one illustrative example where this
    happens.

--- llvm/include/llvm/IR/Constants.h
+++ llvm/include/llvm/IR/Constants.h
@@ -112,16 +112,21 @@ public:
 
   /// If Ty is a vector type, return a Constant with a splat of the given
   /// value. Otherwise return a ConstantInt for the given value.
-  LLVM_ABI static Constant *get(Type *Ty, uint64_t V, bool IsSigned = false);
+  /// \param ImplicitTrunc Whether to allow implicit truncation of the value.
+  // TODO: Make ImplicitTrunc default to false.
+  LLVM_ABI static Constant *get(Type *Ty, uint64_t V, bool IsSigned = false,
+                                bool ImplicitTrunc = true);
 
   /// Return a ConstantInt with the specified integer value for the specified
   /// type. If the type is wider than 64 bits, the value will be zero-extended
   /// to fit the type, unless IsSigned is true, in which case the value will
   /// be interpreted as a 64-bit signed integer and sign-extended to fit
   /// the type.
-  /// Get a ConstantInt for a specific value.
+  /// \param ImplicitTrunc Whether to allow implicit truncation of the value.
+  // TODO: Make ImplicitTrunc default to false.
   LLVM_ABI static ConstantInt *get(IntegerType *Ty, uint64_t V,
-                                   bool IsSigned = false);
+                                   bool IsSigned = false,
+                                   bool ImplicitTrunc = true);
 
   /// Return a ConstantInt with the specified value for the specified type. The
   /// value V will be canonicalized to a an unsigned APInt. Accessing it with
