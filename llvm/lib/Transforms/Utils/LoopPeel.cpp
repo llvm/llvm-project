238a3c980c88f8904c4d06073315774458158c9b
Craig Topper <craig.topper@sifive.com>
2025-12-05 09:36:54 -0800

    Update llvm/lib/Transforms/Utils/LoopPeel.cpp
    Co-authored-by: Nikita Popov <github@npopov.com>

--- llvm/lib/Transforms/Utils/LoopPeel.cpp
+++ llvm/lib/Transforms/Utils/LoopPeel.cpp
@@ -419,7 +419,7 @@ std::optional<unsigned> PhiAnalyzer::calculateIterationsToPeel() {
 // the remainder loop after peeling. The load must also be used (transitively)
 // by an exit condition. Returns the number of iterations to peel off (at the
 // moment either 0 or 1).
-static unsigned peelToTurnInvariantLoadsDerefenceable(Loop &L,
+static unsigned peelToTurnInvariantLoadsDereferenceable(Loop &L,
                                                       DominatorTree &DT,
                                                       AssumptionCache *AC) {
   // Skip loops with a single exiting block, because there should be no benefit
