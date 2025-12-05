0398feb8b636b8d9a4deababbd79fa54f2fdcb9c
Amichaxx <amina.chabane@arm.com>
2025-12-05 15:33:45 +0000

    Included further fp to int/int to fp patterns and updated relevant tests. Removed predicate in AArch64Subtarget.h.

--- llvm/lib/Target/AArch64/AArch64Subtarget.h
+++ llvm/lib/Target/AArch64/AArch64Subtarget.h
@@ -217,11 +217,6 @@ public:
     return isSVEAvailable() || (isSVEorStreamingSVEAvailable() && hasSME2());
   }
 
-  bool isFPRCVTStreamingSafe() const {
-    return hasFPRCVT() &&
-           (!hasSMEFA64() && (isStreaming() || isStreamingCompatible()));
-  }
-
   unsigned getMinVectorRegisterBitWidth() const {
     // Don't assume any minimum vector size when PSTATE.SM may not be 0, because
     // we don't yet support streaming-compatible codegen support that we trust
