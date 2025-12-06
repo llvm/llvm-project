014434fb6f23aace3b39a8aed0ccccf6dc96ed23
Félix-Antoine Constantin <felix-antoine.constantin@bidgroup.ca>
2025-12-06 12:15:49 -0500

    fixup! [clang-tidy] New option to remove arguments from the command line
    Fixed unit tests

--- clang-tools-extra/test/clang-tidy/infrastructure/removed-args.cpp
+++ clang-tools-extra/test/clang-tidy/infrastructure/removed-args.cpp
@@ -1,8 +1,8 @@
-// RUN: not clang-tidy %s -- -fnot-an-option | FileCheck %s -check-prefix=INVALID-A
-// RUN: clang-tidy %s --config="{RemovedArgs: ['-fnot-an-option']}" -- -fnot-an-option
-// RUN: clang-tidy %s --config="{RemovedArgs: ['-fnot-another-option', '-fnot-an-option']}" -- -fnot-an-option -fnot-another-option
-// RUN: clang-tidy %s --removed-arg="-fnot-an-option" -- -fnot-an-option
-// RUN: not clang-tidy %s --removed-arg="-fnot-an-option" -- -fnot-an-option -fnot-another-option | FileCheck %s -check-prefix=INVALID-B
+// RUN: not clang-tidy %s --allow-no-checks -- -fnot-an-option | FileCheck %s -check-prefix=INVALID-A
+// RUN: clang-tidy %s --allow-no-checks --config="{RemovedArgs: ['-fnot-an-option']}" -- -fnot-an-option
+// RUN: clang-tidy %s --allow-no-checks --config="{RemovedArgs: ['-fnot-another-option', '-fnot-an-option']}" -- -fnot-an-option -fnot-another-option
+// RUN: clang-tidy %s --allow-no-checks --removed-arg="-fnot-an-option" -- -fnot-an-option
+// RUN: not clang-tidy %s --allow-no-checks --removed-arg="-fnot-an-option" -- -fnot-an-option -fnot-another-option | FileCheck %s -check-prefix=INVALID-B
 
 // INVALID-A: error: unknown argument: '-fnot-an-option' [clang-diagnostic-error]
 // INVALID-B: error: unknown argument: '-fnot-another-option' [clang-diagnostic-error]
