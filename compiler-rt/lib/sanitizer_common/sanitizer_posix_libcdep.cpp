724fb363d59a28ace80d81d66dba86067f6d4740
Andrew Haberlandt <ahaberlandt@apple.com>
2025-12-09 13:17:37 -0800

    [sanitizer_common] Fix SIGPIPE when symbolizer started by StartSubprocess dies
    Closes #120915

--- compiler-rt/lib/sanitizer_common/sanitizer_posix_libcdep.cpp
+++ compiler-rt/lib/sanitizer_common/sanitizer_posix_libcdep.cpp
@@ -523,9 +523,6 @@ pid_t StartSubprocess(const char *program, const char *const argv[],
                       const char *const envp[], fd_t stdin_fd, fd_t stdout_fd,
                       fd_t stderr_fd) {
   auto file_closer = at_scope_exit([&] {
-    if (stdin_fd != kInvalidFd) {
-      internal_close(stdin_fd);
-    }
     if (stdout_fd != kInvalidFd) {
       internal_close(stdout_fd);
     }
--- compiler-rt/lib/sanitizer_common/sanitizer_symbolizer_posix_libcdep.cpp
+++ compiler-rt/lib/sanitizer_common/sanitizer_symbolizer_posix_libcdep.cpp
@@ -176,10 +176,6 @@ bool SymbolizerProcess::StartSymbolizerSubprocess() {
       internal_close(outfd[1]);
       return false;
     }
-
-    // We intentionally hold on to the read-end so that we don't get a SIGPIPE
-    child_stdin_fd_ = outfd[0];
-
 #  else   // SANITIZER_APPLE
     UNIMPLEMENTED();
 #  endif  // SANITIZER_APPLE
@@ -195,6 +191,8 @@ bool SymbolizerProcess::StartSymbolizerSubprocess() {
 
   input_fd_ = infd[0];
   output_fd_ = outfd[1];
+  // We intentionally hold on to the read-end so that we don't get a SIGPIPE
+  child_stdin_fd_ = outfd[0];
 
   CHECK_GT(pid, 0);
 
