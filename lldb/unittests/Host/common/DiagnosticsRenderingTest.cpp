409c6fef0b7824ab5256ddb17a1b0312b0afd6ba
Charles Zablit <c_zablit@apple.com>
2025-12-09 19:06:16 +0000

    [lldb] improve the heuristics for checking if a terminal supports Unicode

--- lldb/unittests/Host/common/DiagnosticsRenderingTest.cpp
+++ lldb/unittests/Host/common/DiagnosticsRenderingTest.cpp
@@ -10,7 +10,7 @@ class ErrorDisplayTest : public ::testing::Test {};
 
 std::string Render(std::vector<DiagnosticDetail> details) {
   StreamString stream;
-  RenderDiagnosticDetails(stream, 0, true, details);
+  RenderDiagnosticDetails(stream, 0, true, details, true);
   return stream.GetData();
 }
 } // namespace
