e82c7ef0f8c584909d5254a11c5b78b46d3e63d3
William Tran-Viet <wtranviet@proton.me>
2025-12-09 17:14:44 -0500

    Macro formatting

--- libcxx/include/__iterator/capacity_aware_iterator.h
+++ libcxx/include/__iterator/capacity_aware_iterator.h
@@ -7,8 +7,8 @@
 //
 //===----------------------------------------------------------------------===//
 
-#ifndef _LIBCPP_CAPACITY_AWARE_ITERATOR_H
-#define _LIBCPP_CAPACITY_AWARE_ITERATOR_H
+#ifndef _LIBCPP___CAPACITY_AWARE_ITERATOR_H
+#define _LIBCPP___CAPACITY_AWARE_ITERATOR_H
 
 #include <__assert>
 #include <__compare/ordering.h>
@@ -201,4 +201,4 @@ _LIBCPP_END_NAMESPACE_STD
 
 _LIBCPP_POP_MACROS
 
-#endif // _LIBCPP_CAPACITY_AWARE_ITERATOR_H
+#endif // _LIBCPP___CAPACITY_AWARE_ITERATOR_H
