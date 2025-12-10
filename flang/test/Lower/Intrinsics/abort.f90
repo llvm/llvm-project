0b4fae9fd2d9fe67649577fdcc320be26e3622e9
Eugene Epshteyn <eepshteyn@nvidia.com>
2025-12-09 23:18:27 -0500

    Fixed abort.f90 test again

--- flang/test/Lower/Intrinsics/abort.f90
+++ flang/test/Lower/Intrinsics/abort.f90
@@ -1,9 +1,10 @@
-! RUN: bbc -emit-fir %s -o - | FileCheck %s
+! RUN: %flang_fc1 -emit-fir %s -o - | FileCheck %s
 
 ! CHECK-LABEL: func.func @_QPabort_test() {
 ! CHECK:         fir.call @_FortranAAbort() {{.*}}: () -> ()
 ! CHECK-NEXT:    fir.unreachable
-
+! CHECK-NEXT:    }
+! CHECK: func.func private @_FortranAAbort{{.*}} attributes {{.*}}noreturn
 
 subroutine abort_test()
   call abort
