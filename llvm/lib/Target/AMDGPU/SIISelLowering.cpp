5830f16370c533c5678fa68ba188b1158d7163ea
Jay Foad <jay.foad@amd.com>
2025-12-10 21:38:53 +0000

    Avoid AMDGPU::isGFX11Plus

--- llvm/lib/Target/AMDGPU/SIISelLowering.cpp
+++ llvm/lib/Target/AMDGPU/SIISelLowering.cpp
@@ -6841,7 +6841,8 @@ SDValue SITargetLowering::LowerOperation(SDValue Op, SelectionDAG &DAG) const {
     return splitTernaryVectorOp(Op, DAG);
   case ISD::FP_TO_SINT:
   case ISD::FP_TO_UINT:
-    if (AMDGPU::isGFX11Plus(*Subtarget) && Op.getValueType() == MVT::i16 &&
+    if (Subtarget->getGeneration() >= AMDGPUSubtarget::GFX11 &&
+        Op.getValueType() == MVT::i16 &&
         Op.getOperand(0).getValueType() == MVT::f32) {
       // Make f32->i16 legal so we can select V_CVT_PK_[IU]16_F32.
       return Op;
