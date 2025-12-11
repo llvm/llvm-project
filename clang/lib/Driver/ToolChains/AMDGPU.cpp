911051bd5c3e8b699701926767a055542df07299
Manuel Carrasco <Manuel.Carrasco@amd.com>
2025-12-11 09:54:45 -0600

    [Review] Skip device lib linking in ROCMToolChain for SPIR-V.

--- clang/lib/Driver/ToolChains/AMDGPU.cpp
+++ clang/lib/Driver/ToolChains/AMDGPU.cpp
@@ -967,6 +967,8 @@ void ROCMToolChain::addClangTargetOptions(
                           true))
     return;
 
+    // For SPIR-V (SPIRVAMDToolChain) we must not link any device libraries so we skip it.
+  if (!this->getEffectiveTriple().isSPIRV()){
   // Get the device name and canonicalize it
   const StringRef GpuArch = getGPUArch(DriverArgs);
   auto Kind = llvm::AMDGPU::parseArchAMDGCN(GpuArch);
@@ -994,6 +996,7 @@ void ROCMToolChain::addClangTargetOptions(
       CC1Args.push_back("-mlink-bitcode-file");
     CC1Args.push_back(DriverArgs.MakeArgString(BCFile));
   }
+}
 }
 
 bool RocmInstallationDetector::checkCommonBitcodeLibs(
