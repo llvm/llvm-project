42a36f08d389dede5fba50c583adba7ddf465fd2
bhuvan1527 <balabhuvanvarma@gmail.com>
2025-12-11 17:59:15 +0530

    [clang-tidy] Moved Multiple Inheritence check from fuchsia to misc module
    Updated the documentation in the commit,
    1. ReleaseNotes.rst
    2. respective docs in the docs/clang-tidy/checks/
    3. list.rst

--- clang-tools-extra/clang-tidy/misc/MultipleInheritanceCheck.h
+++ clang-tools-extra/clang-tidy/misc/MultipleInheritanceCheck.h
@@ -16,7 +16,7 @@ namespace clang::tidy::misc {
 /// Multiple implementation inheritance is discouraged.
 ///
 /// For the user-facing documentation see:
-/// https://clang.llvm.org/extra/clang-tidy/checks/fuchsia/multiple-inheritance.html
+/// https://clang.llvm.org/extra/clang-tidy/checks/misc/multiple-inheritance.html
 class MultipleInheritanceCheck : public ClangTidyCheck {
 public:
   MultipleInheritanceCheck(StringRef Name, ClangTidyContext *Context)
