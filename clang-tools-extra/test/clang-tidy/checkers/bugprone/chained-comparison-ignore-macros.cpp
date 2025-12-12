c3699bab28d68bca6f76a74312061c357f575205
mtx <mitchell.xu2@gmail.com>
2025-12-12 18:42:36 +0800

    Add more testcases

--- clang-tools-extra/test/clang-tidy/checkers/bugprone/chained-comparison-ignore-macros.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/bugprone/chained-comparison-ignore-macros.cpp
@@ -6,7 +6,32 @@ void macro_test(int x, int y, int z) {
     bool result = CHAINED_COMPARE(x, y, z);
 }
 
-void normal_test(int x, int y, int z) {
-    bool result = x < y < z;
-    // CHECK-MESSAGES: :[[@LINE-1]]:19: warning: chained comparison 'v0 < v1 < v2' may generate unintended results
+#define NESTED_LESS(a, b) a < b
+#define NESTED_CHAIN(a, b, c) NESTED_LESS(a, b) < c
+
+void nested_macro_test(int x, int y, int z) {
+    bool result = NESTED_CHAIN(x, y, z);
+}
+
+#define LESS_OP <
+
+void operator_macro_test(int x, int y, int z) {
+    bool result = x LESS_OP y LESS_OP z;
+}
+// CHECK-MESSAGES: :[[@LINE-2]]:19: warning: chained comparison 'v0 < v1 < v2' may generate unintended results
+
+#define PARTIAL_LESS(a, b) a < b
+
+void mixed_macro_test(int x, int y, int z) {
+    bool result = PARTIAL_LESS(x, y) < z;
+}
+
+void if_macro_test(int x, int y, int z) {
+    if (CHAINED_COMPARE(x, y, z)) {}
+}
+
+#define LONG_CHAIN_MACRO(v) v[0] < v[1] < v[2] < v[3]
+
+void long_chain_macro_test(int v[4]) {
+    bool result = LONG_CHAIN_MACRO(v);
 }
