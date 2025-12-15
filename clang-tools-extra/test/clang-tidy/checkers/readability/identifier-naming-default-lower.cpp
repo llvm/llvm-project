3b9411b142fedb6a78b3b9134c86d60d1c7bf47d
Dominik Kaszewski <dakaszewski@gmail.com>
2025-12-15 20:10:32 +0100

    Add failing test for warning message

--- clang-tools-extra/test/clang-tidy/checkers/readability/identifier-naming-default-lower.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/readability/identifier-naming-default-lower.cpp
@@ -6,38 +6,38 @@
 
 // DefaultCase enables every type of identifier to be checked with same case
 #define MyMacro
-// CHECK-MESSAGES: :[[@LINE-1]]:9: warning: invalid case style for default 'MyMacro' [readability-identifier-naming]
+// CHECK-MESSAGES: :[[@LINE-1]]:9: warning: invalid case style for identifier 'MyMacro' [readability-identifier-naming]
 // CHECK-FIXES: #define my_macro
 
 namespace MyNamespace {
-// CHECK-MESSAGES: :[[@LINE-1]]:11: warning: invalid case style for default 'MyNamespace' [readability-identifier-naming]
+// CHECK-MESSAGES: :[[@LINE-1]]:11: warning: invalid case style for identifier 'MyNamespace' [readability-identifier-naming]
 // CHECK-FIXES: namespace my_namespace {
 
 using MyAlias = int;
-// CHECK-MESSAGES: :[[@LINE-1]]:7: warning: invalid case style for default 'MyAlias' [readability-identifier-naming]
+// CHECK-MESSAGES: :[[@LINE-1]]:7: warning: invalid case style for identifier 'MyAlias' [readability-identifier-naming]
 // CHECK-FIXES: using my_alias = int;
 
 int MyGlobal;
-// CHECK-MESSAGES: :[[@LINE-1]]:5: warning: invalid case style for default 'MyGlobal' [readability-identifier-naming]
+// CHECK-MESSAGES: :[[@LINE-1]]:5: warning: invalid case style for identifier 'MyGlobal' [readability-identifier-naming]
 // CHECK-FIXES: int my_global;
 
 struct MyStruct {
-// CHECK-MESSAGES: :[[@LINE-1]]:8: warning: invalid case style for default 'MyStruct' [readability-identifier-naming]
+// CHECK-MESSAGES: :[[@LINE-1]]:8: warning: invalid case style for identifier 'MyStruct' [readability-identifier-naming]
 // CHECK-FIXES: struct my_struct {
   int MyField;
-// CHECK-MESSAGES: :[[@LINE-1]]:7: warning: invalid case style for default 'MyField' [readability-identifier-naming]
+// CHECK-MESSAGES: :[[@LINE-1]]:7: warning: invalid case style for identifier 'MyField' [readability-identifier-naming]
 // CHECK-FIXES: int my_field;
 };
 
 template <typename MyTypename>
-// CHECK-MESSAGES: :[[@LINE-1]]:20: warning: invalid case style for default 'MyTypename' [readability-identifier-naming]
+// CHECK-MESSAGES: :[[@LINE-1]]:20: warning: invalid case style for identifier 'MyTypename' [readability-identifier-naming]
 // CHECK-FIXES: template <typename my_typename>
 int MyFunction(int MyArgument) {
-// CHECK-MESSAGES: :[[@LINE-1]]:5: warning: invalid case style for default 'MyFunction' [readability-identifier-naming]
-// CHECK-MESSAGES: :[[@LINE-2]]:20: warning: invalid case style for default 'MyArgument' [readability-identifier-naming]
+// CHECK-MESSAGES: :[[@LINE-1]]:5: warning: invalid case style for identifier 'MyFunction' [readability-identifier-naming]
+// CHECK-MESSAGES: :[[@LINE-2]]:20: warning: invalid case style for identifier 'MyArgument' [readability-identifier-naming]
 // CHECK-FIXES: int my_function(int my_argument) {
   int MyVariable = MyArgument;
-// CHECK-MESSAGES: :[[@LINE-1]]:7: warning: invalid case style for default 'MyVariable' [readability-identifier-naming]
+// CHECK-MESSAGES: :[[@LINE-1]]:7: warning: invalid case style for identifier 'MyVariable' [readability-identifier-naming]
 // CHECK-FIXES: int my_variable = my_argument;
   return MyVariable;
 // CHECK-FIXES: return my_variable;
