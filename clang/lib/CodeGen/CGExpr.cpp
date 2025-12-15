2294f90af33e93c5a8228e5c87b6a52a0e14dfc9
Félix Cloutier <fcloutier@apple.com>
2025-12-15 19:55:36 +0000

    Fix strict bool effect on unpacked vector loads (HLSL only), reorganize logic to decide if converting with nonzero method

--- clang/lib/CodeGen/CGExpr.cpp
+++ clang/lib/CodeGen/CGExpr.cpp
@@ -2083,6 +2083,17 @@ static bool getRangeForType(CodeGenFunction &CGF, QualType Ty, llvm::APInt &Min,
   return true;
 }
 
+static bool IsOptimizedBuild(const CodeGenOptions &CGOpt) {
+  return CGOpt.OptimizationLevel > 0;
+}
+
+static bool ConvertBoolByCmpZero(const CodeGenOptions &CGOpt) {
+  if (CGOpt.getLoadBoolFromMem() == CodeGenOptions::BoolFromMem::NonZero)
+    return true;
+
+  return false;
+}
+
 llvm::MDNode *CodeGenFunction::getRangeForLoadFromType(QualType Ty) {
   llvm::APInt Min, End;
   bool IsBool = Ty->hasBooleanRepresentation() && !Ty->isVectorType();
@@ -2102,7 +2113,7 @@ void CodeGenFunction::maybeAttachRangeForLoad(llvm::LoadInst *Load, QualType Ty,
   if (EmitScalarRangeCheck(Load, Ty, Loc)) {
     // In order to prevent the optimizer from throwing away the check, don't
     // attach range metadata to the load.
-  } else if (CGM.getCodeGenOpts().OptimizationLevel > 0) {
+  } else if (IsOptimizedBuild(CGM.getCodeGenOpts())) {
     if (llvm::MDNode *RangeInfo = getRangeForLoadFromType(Ty)) {
       Load->setMetadata(llvm::LLVMContext::MD_range, RangeInfo);
       Load->setMetadata(llvm::LLVMContext::MD_noundef,
@@ -2291,13 +2302,12 @@ llvm::Value *CodeGenFunction::EmitFromMemory(llvm::Value *Value, QualType Ty) {
   }
 
   llvm::Type *ResTy = ConvertType(Ty);
-  bool HasBoolRep = Ty->hasBooleanRepresentation();
-  if (HasBoolRep && CGM.getCodeGenOpts().getLoadBoolFromMem() ==
-                        CodeGenOptions::BoolFromMem::NonZero) {
+  bool HasBoolRep = Ty->hasBooleanRepresentation() || Ty->isExtVectorBoolType();
+  if (HasBoolRep && ConvertBoolByCmpZero(CGM.getCodeGenOpts())) {
     return Builder.CreateICmpNE(
         Value, llvm::Constant::getNullValue(Value->getType()), "loadedv");
   }
-  if (HasBoolRep || Ty->isBitIntType() || Ty->isExtVectorBoolType())
+  if (HasBoolRep || Ty->isBitIntType())
     return Builder.CreateTrunc(Value, ResTy, "loadedv");
 
   return Value;
@@ -2579,7 +2589,7 @@ RValue CodeGenFunction::EmitLoadOfLValue(LValue LV, SourceLocation Loc) {
     QualType EltTy = LV.getType();
     if (const auto *MatTy = EltTy->getAs<ConstantMatrixType>()) {
       EltTy = MatTy->getElementType();
-      if (CGM.getCodeGenOpts().OptimizationLevel > 0) {
+      if (IsOptimizedBuild(CGM.getCodeGenOpts())) {
         llvm::MatrixBuilder MB(Builder);
         MB.CreateIndexAssumption(Idx, MatTy->getNumElementsFlattened());
       }
@@ -2863,7 +2873,7 @@ void CodeGenFunction::EmitStoreThroughLValue(RValue Src, LValue Dst,
       }
 
       llvm::Value *Idx = Dst.getMatrixIdx();
-      if (CGM.getCodeGenOpts().OptimizationLevel > 0) {
+      if (IsOptimizedBuild(CGM.getCodeGenOpts())) {
         const auto *const MatTy = Dst.getType()->castAs<ConstantMatrixType>();
         llvm::MatrixBuilder MB(Builder);
         MB.CreateIndexAssumption(Idx, MatTy->getNumElementsFlattened());
@@ -4154,7 +4164,7 @@ static void emitCheckHandlerCall(CodeGenFunction &CGF,
                                llvm::AttributeList::FunctionIndex, B),
       /*Local=*/true);
   llvm::CallInst *HandlerCall = CGF.EmitNounwindRuntimeCall(Fn, FnArgs);
-  NoMerge = NoMerge || !CGF.CGM.getCodeGenOpts().OptimizationLevel ||
+  NoMerge = NoMerge || !IsOptimizedBuild(CGF.CGM.getCodeGenOpts()) ||
             (CGF.CurCodeDecl && CGF.CurCodeDecl->hasAttr<OptimizeNoneAttr>());
   if (NoMerge)
     HandlerCall->addFnAttr(llvm::Attribute::NoMerge);
@@ -4546,7 +4556,7 @@ void CodeGenFunction::EmitTrapCheck(llvm::Value *Checked,
         TrapLocation, TrapCategory, TrapMessage);
   }
 
-  NoMerge = NoMerge || !CGM.getCodeGenOpts().OptimizationLevel ||
+  NoMerge = NoMerge || !IsOptimizedBuild(CGM.getCodeGenOpts()) ||
             (CurCodeDecl && CurCodeDecl->hasAttr<OptimizeNoneAttr>());
 
   llvm::MDBuilder MDHelper(getLLVMContext());
--- clang/test/CodeGen/strict-bool.c
+++ clang/test/CodeGen/strict-bool.c
@@ -6,12 +6,15 @@
 // RUN: %clang_cc1 -triple armv7-apple-darwin -O1 -fsanitize=bool -load-bool-from-mem=strict -emit-llvm -o - %s | FileCheck %s -check-prefixes=CHECK,CHECK-UBSAN-STRICT
 // RUN: %clang_cc1 -triple armv7-apple-darwin -O1 -fsanitize=bool -load-bool-from-mem=truncate -emit-llvm -o - %s | FileCheck %s -check-prefixes=CHECK,CHECK-UBSAN-TRUNCATE
 
+typedef _Bool bool4_t __attribute__((ext_vector_type(4)));
+
 struct has_bool {
     _Bool b;
     unsigned _BitInt(1) c;
+    bool4_t v;
 };
 
-// CHECK: @foo
+// CHECK-LABEL: @foo
 int foo(struct has_bool *b) {
     // CHECK-STRICT: [[BOOL:%.+]] = load i8, ptr {{.+}}, !range ![[RANGE_BOOL:[0-9]+]]
     // CHECK-STRICT-NOT: and i8 [[BOOL]], 1
@@ -35,7 +38,7 @@ int foo(struct has_bool *b) {
     return b->b;
 }
 
-// CHECK: @bar
+// CHECK-LABEL: @bar
 int bar(struct has_bool *c) {
     // CHECK-STRICT: [[BITINT:%.+]] = load i8, ptr {{.+}}, !range ![[RANGE_BOOL:[0-9]+]]
     // CHECK-STRICT-NOT: and i8 [[BITINT]], 1
@@ -58,5 +61,12 @@ int bar(struct has_bool *c) {
     // CHECK-UBSAN-TRUNCATE: icmp ult i8 [[BITINT]], 2
     return c->c;
 }
+
+// CHECK-LABEL: @vec
+bool4_t vec(struct has_bool *c) {
+    // CHECK: [[BITS:%.+]] = load <{{[0-9]+}} x i1>
+    return c->v;
+}
+
 
 // CHECK_STRICT: ![[RANGE_BOOL]] = !{i8 0, i8 2}
