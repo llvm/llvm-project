407013420b702724afdac75116262337206908da
erichkeane <ekeane@nvidia.com>
2025-12-15 09:15:30 -0800

    Add example to test given by @alexey-bataev

--- clang/test/CIR/CodeGenOpenMP/parallel.c
+++ clang/test/CIR/CodeGenOpenMP/parallel.c
@@ -30,3 +30,21 @@ void emit_simple_parallel() {
   // CHECK: %[[I_LOAD:.*]] = cir.load{{.*}}
   // CHECK-NEXT: cir.call @after(%[[I_LOAD]])
 }
+
+void parallel_with_operations() {
+  // CHECK: cir.func{{.*}}@parallel_with_operations
+  int a, b;
+  // CHECK-NEXT: cir.alloca{{.*}}["a"]
+  // CHECK-NEXT: cir.alloca{{.*}}["b"]
+  // TODO(OMP): At the moment this results in 3 NYI diagnostics, 1 each for the
+  // clauses + 1 for the CapturedStmt. When those are implemented, the check
+  // lines will need updating.
+#pragma omp parallel shared(a) firstprivate(b)
+  {
+   ++a;
+   ++b;
+  }
+  // CHECK-NEXT: omp.parallel {
+  // CHECK-NEXT: omp.terminator
+  // CHECK-NEXT: }
+}
