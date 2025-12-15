e7af9d5af5c90c426ec5f1bbd7553735ad993486
Mel Chen <mel.chen@sifive.com>
2025-12-15 02:52:53 -0800

    updated the pattern match for masked store

--- llvm/lib/Transforms/Vectorize/VPlanTransforms.cpp
+++ llvm/lib/Transforms/Vectorize/VPlanTransforms.cpp
@@ -2907,22 +2907,19 @@ static VPRecipeBase *optimizeMaskToEVL(VPValue *HeaderMask,
     return new VPWidenStoreEVLRecipe(cast<VPWidenStoreRecipe>(CurRecipe), Addr,
                                      EVL, Mask);
 
-  VPValue *StoredVal;
-  if (match(&CurRecipe, m_MaskedStore(m_VPValue(EndPtr), m_VPValue(StoredVal),
-                                      m_RemoveMask(HeaderMask, Mask))) &&
+  if (match(&CurRecipe,
+            m_MaskedStore(m_VPValue(EndPtr), m_Reverse(m_VPValue(ReversedVal)),
+                          m_RemoveMask(HeaderMask, Mask))) &&
       match(EndPtr, m_VecEndPtr(m_VPValue(Addr), m_Specific(&Plan->getVF()))) &&
       cast<VPWidenStoreRecipe>(CurRecipe).isReverse()) {
-    if (match(StoredVal, m_Reverse(m_VPValue(ReversedVal)))) {
-      auto *NewReverse = new VPWidenIntrinsicRecipe(
-          Intrinsic::experimental_vp_reverse,
-          {ReversedVal, Plan->getTrue(), &EVL},
-          TypeInfo.inferScalarType(ReversedVal), {}, {},
-          cast<VPInstruction>(StoredVal)->getDebugLoc());
-      NewReverse->insertBefore(&CurRecipe);
-      return new VPWidenStoreEVLRecipe(cast<VPWidenStoreRecipe>(CurRecipe),
-                                       AdjustEndPtr(EndPtr), NewReverse, EVL,
-                                       Mask);
-    }
+    auto *NewReverse = new VPWidenIntrinsicRecipe(
+        Intrinsic::experimental_vp_reverse,
+        {ReversedVal, Plan->getTrue(), &EVL},
+        TypeInfo.inferScalarType(ReversedVal), {}, {}, DL);
+    NewReverse->insertBefore(&CurRecipe);
+    return new VPWidenStoreEVLRecipe(cast<VPWidenStoreRecipe>(CurRecipe),
+                                     AdjustEndPtr(EndPtr), NewReverse, EVL,
+                                     Mask);
   }
 
   if (auto *Rdx = dyn_cast<VPReductionRecipe>(&CurRecipe))
