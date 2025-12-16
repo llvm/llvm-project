b3d5e24ab0a638e0f1a67a5f1c51c41bc0e7052a
Eugene Epshteyn <eepshteyn@nvidia.com>
2025-12-15 19:53:28 -0500

    Updated LIT test to properly test -fno-unsafe-cray-pointers

--- flang/test/Driver/unsafe-cray-pointers.f90
+++ flang/test/Driver/unsafe-cray-pointers.f90
@@ -1,4 +1,5 @@
-! RUN: %flang -funsafe-cray-pointers -### %s 2>&1 | FileCheck %s
+! RUN: %flang -funsafe-cray-pointers -### %s 2>&1 | FileCheck %s --check-prefix=CHECK-ON
+! RUN: %flang -fno-unsafe-cray-pointers -### %s 2>&1 | FileCheck %s --check-prefix=CHECK-OFF
 
-! CHECK: "-mmlir" "-funsafe-cray-pointers"
-! CHECK-NOT: "-mmlir" "-funsafe-cray-pointers"
+! CHECK-ON: "-mmlir" "-funsafe-cray-pointers"
+! CHECK-OFF-NOT: "-mmlir" "-funsafe-cray-pointers"
