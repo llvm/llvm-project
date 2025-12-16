da0cabc74a31adc93109f5b2a306217d322e8eb7
Igor S. Gerasimov <foxtranigor@gmail.com>
2025-12-16 12:02:12 +0100

    Update flang/test/Semantics/bind-c10.f90 test

--- flang/test/Semantics/bind-c10.f90
+++ flang/test/Semantics/bind-c10.f90
@@ -1,10 +1,14 @@
 ! RUN: %python %S/test_errors.py %s %flang_fc1
-!ERROR: Symbol has a BIND(C) name that is not a valid C language identifier
 subroutine bang() bind(C,name='!')
 end
-!ERROR: Symbol has a BIND(C) name that is not a valid C language identifier
+!ERROR: Symbol has a BIND(C) name containing non-visible ASCII character(s)
 subroutine cr() bind(C,name=achar(13))
 end
-!ERROR: Symbol has a BIND(C) name that is not a valid C language identifier
 subroutine beast() bind(C,name="666")
+end
+!ERROR: Symbol has a BIND(C) name with leading dot that may have special meaning to the toolchain
+subroutine llvm() bind(C,name=".L1")
+end
+!ERROR: Symbol has a BIND(C) name with leading dot that may have special meaning to the toolchain
+subroutine ld() bind(C,name=".text")
 end
