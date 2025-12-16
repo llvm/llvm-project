7e7c2dd38923fb62ca13c88f2f353b426ff96cec
Abhinav Gaba <abhinav.gaba@intel.com>
2025-12-16 11:55:29 -0800

    Do the debug print before starting individual attach entries.

--- offload/libomptarget/omptarget.cpp
+++ offload/libomptarget/omptarget.cpp
@@ -778,6 +778,11 @@ int processAttachEntries(DeviceTy &Device, AttachInfoTy &AttachInfo,
 
   ODBG(ODT_Mapping) << "Processing " << AttachInfo.AttachEntries.size();
 
+  bool TreatAttachAutoAsAlways = MappingConfig::get().TreatAttachAutoAsAlways;
+  if (TreatAttachAutoAsAlways)
+    ODBG(ODT_Mapping) << "Treating ATTACH(auto) as ATTACH(always) because "
+                      << "LIBOMPTARGET_TREAT_ATTACH_AUTO_AS_ALWAYS is true";
+
   int Ret = OFFLOAD_SUCCESS;
   bool IsFirstPointerAttachment = true;
   for (size_t EntryIdx = 0; EntryIdx < AttachInfo.AttachEntries.size();
@@ -798,15 +803,8 @@ int processAttachEntries(DeviceTy &Device, AttachInfoTy &AttachInfo,
                       << ", PtrSize=" << PtrSize << ", MapType=0x"
                       << llvm::utohexstr(MapType);
 
-    bool IsAttachAlways = MapType & OMP_TGT_MAPTYPE_ALWAYS;
-
-    // Treat ATTACH(auto) as ATTACH(always) if environment variable is set
-    if (!IsAttachAlways && MappingConfig::get().TreatAttachAutoAsAlways) {
-      IsAttachAlways = true;
-      ODBG(ODT_Mapping) << "ATTACH(auto) will be treated as ATTACH(always) "
-                        << " because LIBOMPTARGET_TREAT_ATTACH_AUTO_AS_ALWAYS "
-                        << " is true";
-    }
+    bool IsAttachAlways =
+        (MapType & OMP_TGT_MAPTYPE_ALWAYS) || TreatAttachAutoAsAlways;
 
     // Lambda to check if a pointer was newly allocated
     auto WasNewlyAllocated = [&](void *Ptr, const char *PtrName) {
