e659d8306d529f8f0bedd3002599e99215d6c214
Mikhail Gudim <mgudim@gmail.com>
2025-12-16 15:29:38 -0500

    use twine to report error.

--- llvm/lib/Target/RISCV/RISCVFrameLowering.cpp
+++ llvm/lib/Target/RISCV/RISCVFrameLowering.cpp
@@ -1601,10 +1601,7 @@ void RISCVFrameLowering::determineUncondPrologCalleeSaves(
     if (!Reg)
       Reg = MatchRegisterAltName(Name);
     if (!Reg) {
-      std::string msg;
-      raw_string_ostream Msg(msg);
-      Msg << "Couldn't parse register: " << Name << "\n";
-      reportFatalUsageError(Twine(msg));
+      reportFatalUsageError(Twine("Couldn't parse register: ") + Name + "\n");
     }
     UncondPrologCSRs.set(Reg.id());
   }
