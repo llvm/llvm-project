0fadc7d1426f03d6dd217675c605f1f3e0a9eb05
Jan Svoboda <jan_svoboda@apple.com>
2025-12-17 12:18:28 -0800

    [clang] Remove pointless `hasDiagnostics()` checks (#172705)
    Calling `CompilerInstance::hasDiagnostics()` after
    `CompilerInstance::createDiagnostics()` is pointless, since the creation
    step cannot fail. This removes such calls.

--- clang/include/clang/DependencyScanning/DependencyScannerImpl.h
+++ clang/include/clang/DependencyScanning/DependencyScannerImpl.h
@@ -111,7 +111,7 @@ initVFSForByNameScanning(IntrusiveRefCntPtr<llvm::vfs::FileSystem> BaseFS,
                          ArrayRef<std::string> CommandLine,
                          StringRef WorkingDirectory, StringRef ModuleName);
 
-bool initializeScanCompilerInstance(
+void initializeScanCompilerInstance(
     CompilerInstance &ScanInstance,
     IntrusiveRefCntPtr<llvm::vfs::FileSystem> FS,
     DiagnosticConsumer *DiagConsumer, DependencyScanningService &Service,
