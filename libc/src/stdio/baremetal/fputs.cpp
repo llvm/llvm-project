4f8b132c1c0fdc37460749722cbc1e2017cb1da5
Petr Hosek <phosek@google.com>
2025-12-17 10:56:45 -0800

    [libc] Support opaque FILE* on baremetal (#168931)
    This change expands the stdio support on baremetal to support opaque
    FILE*. This builds on top of the existing baremetal embedding API; we
    treat the standard FILE* streams as pointers that point to the cookie
    symbols which are a part of the embedding API. This also allows users to
    define their own FILE* streams, but we don't (yet) support the API that
    return FILE* such as fopen or fopencookie.

--- libc/src/stdio/baremetal/fputs.cpp
+++ libc/src/stdio/baremetal/fputs.cpp
@@ -1,4 +1,4 @@
-//===-- Implementation of putchar for baremetal -----------------*- C++ -*-===//
+//===-- Implementation of fputs for baremetal -------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,9 +6,10 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "src/stdio/putchar.h"
+#include "src/stdio/fputs.h"
 
 #include "hdr/stdio_macros.h" // for EOF
+#include "hdr/types/FILE.h"
 #include "src/__support/common.h"
 #include "src/__support/libc_errno.h"
 #include "src/__support/macros/config.h"
@@ -16,13 +17,15 @@
 
 namespace LIBC_NAMESPACE_DECL {
 
-LLVM_LIBC_FUNCTION(int, putchar, (int c)) {
-  unsigned char uc = static_cast<unsigned char>(c);
-  auto result = write_internal(reinterpret_cast<char *>(&uc), 1, stdout);
+LLVM_LIBC_FUNCTION(int, fputs,
+                   (const char *__restrict str, ::FILE *__restrict stream)) {
+  cpp::string_view str_view(str);
+
+  auto result = write_internal(str_view.data(), str_view.size(), stream);
   if (result.has_error())
     libc_errno = result.error;
   size_t written = result.value;
-  if (written != 1) {
+  if (written != str_view.size()) {
     // The stream should be in an error state in this case.
     return EOF;
   }
