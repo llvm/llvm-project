61cae9bbbc8ff83d6dacd98b992ef66242130cf0
Valentin Clement (バレンタイン クレメン) <clementval@gmail.com>
2025-12-18 11:11:47 -0800

    Revert "Reland "[flang][cuda] Add support for derived-type initialization on device #172568"" (#172889)
    Reverts llvm/llvm-project#172741
    
    Failures in Fortran testsuite. 
    
    https://lab.llvm.org/buildbot/#/builders/199/builds/7943

--- flang/include/flang/Runtime/allocatable.h
+++ flang/include/flang/Runtime/allocatable.h
@@ -13,7 +13,6 @@
 
 #include "flang/Runtime/descriptor-consts.h"
 #include "flang/Runtime/entry-names.h"
-#include "flang/Runtime/freestanding-tools.h"
 
 namespace Fortran::runtime {
 
@@ -95,17 +94,10 @@ int RTDECL(AllocatableCheckLengthParameter)(Descriptor &,
 // Successfully allocated memory is initialized if the allocatable has a
 // derived type, and is always initialized by AllocatableAllocateSource().
 // Performs all necessary coarray synchronization and validation actions.
-#ifdef RT_DEVICE_COMPILATION
 int RTDECL(AllocatableAllocate)(Descriptor &,
     std::int64_t *asyncObject = nullptr, bool hasStat = false,
     const Descriptor *errMsg = nullptr, const char *sourceFile = nullptr,
-    int sourceLine = 0, MemcpyFct memcpyFct = &MemcpyWrapper);
-#else
-int RTDECL(AllocatableAllocate)(Descriptor &,
-    std::int64_t *asyncObject = nullptr, bool hasStat = false,
-    const Descriptor *errMsg = nullptr, const char *sourceFile = nullptr,
-    int sourceLine = 0, MemcpyFct memcpyFct = &Fortran::runtime::memcpy);
-#endif
+    int sourceLine = 0);
 int RTDECL(AllocatableAllocateSource)(Descriptor &, const Descriptor &source,
     bool hasStat = false, const Descriptor *errMsg = nullptr,
     const char *sourceFile = nullptr, int sourceLine = 0);
