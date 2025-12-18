e81309f0fa3a925ca9c043821a0ec37c96356d8f
saipubw <lizezheng.lzz@alibaba-inc.com>
2025-12-18 14:26:05 +0800

    Remove _LIBCPP_EXPORTED_FROM_ABI from align function

--- libcxx/src/memory.cpp
+++ libcxx/src/memory.cpp
@@ -134,7 +134,7 @@ __sp_mut& __get_sp_mut(const void* p) {
 
 #if !defined(_LIBCPP_ABI_DO_NOT_EXPORT_ALIGN)
 
-_LIBCPP_EXPORTED_FROM_ABI void* align(size_t alignment, size_t size, void*& ptr, size_t& space) {
+void* align(size_t alignment, size_t size, void*& ptr, size_t& space) {
   return __align_inline::align(alignment, size, ptr, space);
 }
 
