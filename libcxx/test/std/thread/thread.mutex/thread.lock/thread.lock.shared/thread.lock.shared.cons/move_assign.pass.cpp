71a27f4229b40af586663f17d22359a1550dafe1
Louis Dionne <ldionne.2@gmail.com>
2025-12-18 19:11:35 -0500

    Tweak tests

--- libcxx/test/std/thread/thread.mutex/thread.lock/thread.lock.shared/thread.lock.shared.cons/move_assign.pass.cpp
+++ libcxx/test/std/thread/thread.mutex/thread.lock/thread.lock.shared/thread.lock.shared.cons/move_assign.pass.cpp
@@ -5,7 +5,9 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
-//
+
+// XFAIL: FROZEN-CXX03-HEADERS-FIXME
+
 // UNSUPPORTED: no-threads
 // UNSUPPORTED: c++03, c++11
 
@@ -13,12 +15,13 @@
 
 // template <class Mutex> class shared_lock;
 
-// shared_lock& operator=(shared_lock&& u);
+// shared_lock& operator=(shared_lock&& u) noexcept;
 
-#include <shared_mutex>
 #include <cassert>
-#include "nasty_containers.h"
+#include <shared_mutex>
+#include <type_traits>
 
+#include "nasty_containers.h"
 #include "test_macros.h"
 
 int main(int, char**) {
@@ -28,22 +31,13 @@ int main(int, char**) {
     M m1;
     std::shared_lock<M> lk0(m0);
     std::shared_lock<M> lk1(m1);
-
-    // Test self move assignment for lk0.
-    lk0 = std::move(lk0);
-    assert(lk0.mutex() == std::addressof(m0));
-    assert(lk0.owns_lock() == true);
-
     lk1 = std::move(lk0);
     assert(lk1.mutex() == std::addressof(m0));
     assert(lk1.owns_lock() == true);
     assert(lk0.mutex() == nullptr);
     assert(lk0.owns_lock() == false);
 
-    // Test self move assignment for lk1.
-    lk1 = std::move(lk1);
-    assert(lk1.mutex() == std::addressof(m0));
-    assert(lk1.owns_lock() == true);
+    static_assert(std::is_nothrow_move_assignable<std::shared_lock<M> >::value, "");
   }
   {
     typedef nasty_mutex M;
@@ -56,6 +50,17 @@ int main(int, char**) {
     assert(lk1.owns_lock() == true);
     assert(lk0.mutex() == nullptr);
     assert(lk0.owns_lock() == false);
+
+    static_assert(std::is_nothrow_move_assignable<std::shared_lock<M> >::value, "");
+  }
+  {
+    // Test self move-assignment (LWG4172)
+    typedef std::shared_timed_mutex M;
+    M m0;
+    std::shared_lock<M> lk0(m0);
+    lk0 = std::move(lk0);
+    assert(lk0.mutex() == std::addressof(m0));
+    assert(lk0.owns_lock() == true);
   }
 
   return 0;
--- libcxx/test/std/thread/thread.mutex/thread.lock/thread.lock.unique/thread.lock.unique.cons/move_assign.pass.cpp
+++ libcxx/test/std/thread/thread.mutex/thread.lock/thread.lock.unique/thread.lock.unique.cons/move_assign.pass.cpp
@@ -6,40 +6,52 @@
 //
 //===----------------------------------------------------------------------===//
 
+// XFAIL: FROZEN-CXX03-HEADERS-FIXME
+
 // <mutex>
 
 // template <class Mutex> class unique_lock;
 
-// unique_lock& operator=(unique_lock&& u);
+// unique_lock& operator=(unique_lock&& u) noexcept;
 
 #include <cassert>
 #include <memory>
 #include <mutex>
+#include <type_traits>
 
 #include "checking_mutex.h"
 
 int main(int, char**) {
-  checking_mutex m0;
-  checking_mutex m1;
-  std::unique_lock<checking_mutex> lk0(m0);
-  std::unique_lock<checking_mutex> lk1(m1);
+  {
+    checking_mutex m0;
+    checking_mutex m1;
+    std::unique_lock<checking_mutex> lk0(m0);
+    std::unique_lock<checking_mutex> lk1(m1);
 
-  // Test self move assignment for lk0.
-  lk0 = std::move(lk0);
-  assert(lk0.mutex() == std::addressof(m0));
-  assert(lk0.owns_lock() == true);
+    // Test self move assignment for lk0.
+    lk0 = std::move(lk0);
+    assert(lk0.mutex() == std::addressof(m0));
+    assert(lk0.owns_lock() == true);
 
-  auto& result = (lk1 = std::move(lk0));
+    auto& result = (lk1 = std::move(lk0));
 
-  assert(&result == &lk1);
-  assert(lk1.mutex() == std::addressof(m0));
-  assert(lk1.owns_lock());
-  assert(lk0.mutex() == nullptr);
-  assert(lk0.owns_lock() == false);
+    assert(&result == &lk1);
+    assert(lk1.mutex() == std::addressof(m0));
+    assert(lk1.owns_lock());
+    assert(lk0.mutex() == nullptr);
+    assert(lk0.owns_lock() == false);
 
-  // Test self move assignment for lk1
-  lk1 = std::move(lk1);
-  assert(lk1.mutex() == std::addressof(m0));
-  assert(lk1.owns_lock() == true);
+    static_assert(std::is_nothrow_move_assignable<std::unique_lock<checking_mutex> >::value, "");
+  }
+
+  {
+    // Test self move-assignment (LWG4172)
+    checking_mutex m0;
+    std::unique_lock<checking_mutex> lk0(m0);
+    lk0 = std::move(lk0);
+    assert(lk0.mutex() == std::addressof(m0));
+    assert(lk0.owns_lock() == true);
+  }
+
   return 0;
 }
