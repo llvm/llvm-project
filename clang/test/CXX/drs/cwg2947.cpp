21a922403b08e213fd78b52b8634cae1b2fb0cdc
yronglin <yronglin777@gmail.com>
2025-12-19 22:03:51 +0800

    Refine cwg2947 test and update cxx status
    Signed-off-by: yronglin <yronglin777@gmail.com>

--- clang/test/CXX/drs/cwg2947.cpp
+++ clang/test/CXX/drs/cwg2947.cpp
@@ -12,6 +12,26 @@
 // RUN: %clang_cc1 -std=c++20 %t/cwg2947_ext2.cpp -fsyntax-only -verify
 // RUN: %clang_cc1 -std=c++20 %t/cwg2947_ext3.cpp -fsyntax-only -verify
 
+// RUN: %clang_cc1 -std=c++23 %t/cwg2947_example1.cpp -D'DOT_BAR=.bar' -fsyntax-only -verify
+// RUN: %clang_cc1 -std=c++23 %t/cwg2947_example2.cpp -D'MOD_ATTR=[[vendor::shiny_module]]' -fsyntax-only -verify
+// RUN: %clang_cc1 -std=c++23 %t/cwg2947_example3.cpp -fsyntax-only -verify
+// RUN: %clang_cc1 -std=c++23 %t/cwg2947_example4.cpp -fsyntax-only -verify
+// RUN: %clang_cc1 -std=c++23 %t/cwg2947_example5.cpp -fsyntax-only -verify
+// RUN: %clang_cc1 -std=c++23 %t/cwg2947_example6.cpp -fsyntax-only -verify
+// RUN: %clang_cc1 -std=c++23 %t/cwg2947_ext1.cpp -verify -E | FileCheck %t/cwg2947_ext1.cpp
+// RUN: %clang_cc1 -std=c++23 %t/cwg2947_ext2.cpp -fsyntax-only -verify
+// RUN: %clang_cc1 -std=c++23 %t/cwg2947_ext3.cpp -fsyntax-only -verify
+
+// RUN: %clang_cc1 -std=c++26 %t/cwg2947_example1.cpp -D'DOT_BAR=.bar' -fsyntax-only -verify
+// RUN: %clang_cc1 -std=c++26 %t/cwg2947_example2.cpp -D'MOD_ATTR=[[vendor::shiny_module]]' -fsyntax-only -verify
+// RUN: %clang_cc1 -std=c++26 %t/cwg2947_example3.cpp -fsyntax-only -verify
+// RUN: %clang_cc1 -std=c++26 %t/cwg2947_example4.cpp -fsyntax-only -verify
+// RUN: %clang_cc1 -std=c++26 %t/cwg2947_example5.cpp -fsyntax-only -verify
+// RUN: %clang_cc1 -std=c++26 %t/cwg2947_example6.cpp -fsyntax-only -verify
+// RUN: %clang_cc1 -std=c++26 %t/cwg2947_ext1.cpp -verify -E | FileCheck %t/cwg2947_ext1.cpp
+// RUN: %clang_cc1 -std=c++26 %t/cwg2947_ext2.cpp -fsyntax-only -verify
+// RUN: %clang_cc1 -std=c++26 %t/cwg2947_ext3.cpp -fsyntax-only -verify
+
 //--- cwg2947_example1.cpp
 // #define DOT_BAR .bar
 export module foo DOT_BAR; // error: expansion of DOT_BAR; does not begin with ; or [
@@ -29,16 +49,16 @@ export module a
 //--- cwg2947_example4.cpp
 export module M [[
   attr1,
+// expected-warning@-1 {{unknown attribute 'attr1' ignored}}
   attr2 ]] ;                 // OK
-// expected-warning@-2 {{unknown attribute 'attr1' ignored}}
-// expected-warning@-2 {{unknown attribute 'attr2' ignored}}
+// expected-warning@-1 {{unknown attribute 'attr2' ignored}}
 
 //--- cwg2947_example5.cpp
 export module M
   [[ attr1,
+// expected-warning@-1 {{unknown attribute 'attr1' ignored}}
   attr2 ]] ;                 // OK
-// expected-warning@-2 {{unknown attribute 'attr1' ignored}}
-// expected-warning@-2 {{unknown attribute 'attr2' ignored}}
+// expected-warning@-1 {{unknown attribute 'attr2' ignored}}
 
 //--- cwg2947_example6.cpp
 export module M; int // expected-warning {{extra tokens after semicolon in 'module' directive}}
@@ -46,12 +66,14 @@ export module M; int // expected-warning {{extra tokens after semicolon in 'modu
 
 //--- cwg2947_ext1.cpp
 // CHECK: export __preprocessed_module m; int x;
-// CHECK-NEXT: extern "C++" int *y = &x;
-export module m; int x; // expected-warning {{extra tokens after semicolon in 'module' directive}}
+// CHECK: extern "C++" int *y = &x;
+export module m; int x;
+// expected-warning@-1 {{extra tokens after semicolon in 'module' directive}}
 extern "C++" int *y = &x;
 
 //--- cwg2947_ext2.cpp
-export module x _Pragma("GCC warning \"Hi\""); // expected-warning {{Hi}}
+export module x _Pragma("GCC warning \"Hi\"");
+// expected-warning@-1 {{Hi}}
 
 //--- cwg2947_ext3.cpp
 export module x; _Pragma("GCC warning \"hi\""); // expected-warning {{hi}}
