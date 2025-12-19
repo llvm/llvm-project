f0d75b1e43c6687d44dfb5b0a01ed6ce47afa7a8
Balaji V. Iyer <bviyer@gmail.com>
2025-12-19 14:19:51 -0600

    Reformatted a expected-note string to fix windows CI

--- clang/test/AST/array-overflow-index.cpp
+++ clang/test/AST/array-overflow-index.cpp
@@ -5,6 +5,6 @@ constexpr int __attribute__((vector_size(4))) test_vector = {1};
 
 // expected-error@+1 {{constexpr function never produces a constant expression}}
 constexpr int get_last_element(void) {
-    // expected-note@+1 {{cannot refer to element 18446744073709551615 of array of 1 element in a constant expression}}
+    // expected-note@+1 {{cannot refer to element}}
     return test_vector[~0UL];
 }
