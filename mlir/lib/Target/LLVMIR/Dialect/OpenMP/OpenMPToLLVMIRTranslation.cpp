1764209d00638192795b5d3808351ef8cfa57795
skc7 <Krishna.Sankisa@amd.com>
2025-12-19 10:11:01 +0530

    comments fixes

--- mlir/lib/Target/LLVMIR/Dialect/OpenMP/OpenMPToLLVMIRTranslation.cpp
+++ mlir/lib/Target/LLVMIR/Dialect/OpenMP/OpenMPToLLVMIRTranslation.cpp
@@ -2030,7 +2030,8 @@ convertOmpTeams(omp::TeamsOp op, llvm::IRBuilderBase &builder,
   }
 
   if (op.hasThreadLimitDimsModifier()) {
-    return op.emitError("Lowering of thread_limit with dims modifier is NYI.");
+    return op.emitError(
+        "Lowering of thread_limit with dims modifier is not yet implemented.");
   }
 
   DenseMap<Value, llvm::Value *> reductionVariableMap;
@@ -6048,7 +6049,8 @@ extractHostEvalClauses(omp::TargetOp targetOp, Value &numThreads,
             assert(!teamsOp.hasNumTeamsDimsModifier() &&
                    "Lowering of num_teams with dims modifier is NYI.");
             assert(!teamsOp.hasThreadLimitDimsModifier() &&
-                   "Lowering of thread_limit with dims modifier is NYI.");
+                   "Lowering of thread_limit with dims modifier is not yet "
+                   "implemented.");
             if (teamsOp.getNumTeamsLower() == blockArg)
               numTeamsLower = hostEvalVar;
             else if (teamsOp.getNumTeamsUpper() == blockArg)
@@ -6172,10 +6174,12 @@ initTargetDefaultAttrs(omp::TargetOp targetOp, Operation *capturedOp,
     // ensures values are mapped and available inside of the target region.
     if (auto teamsOp = castOrGetParentOfType<omp::TeamsOp>(capturedOp)) {
       // num_teams dims and values are not yet supported
-      assert(!teamsOp.hasNumTeamsDimsModifier() &&
-             "Lowering of num_teams with dims modifier is NYI.");
+      assert(
+          !teamsOp.hasNumTeamsDimsModifier() &&
+          "Lowering of num_teams with dims modifier is not yet implemented.");
       assert(!teamsOp.hasThreadLimitDimsModifier() &&
-             "Lowering of thread_limit with dims modifier is NYI.");
+             "Lowering of thread_limit with dims modifier is not yet "
+             "implemented.");
       numTeamsLower = teamsOp.getNumTeamsLower();
       numTeamsUpper = teamsOp.getNumTeamsUpper();
       threadLimit = teamsOp.getThreadLimitDimensionValue(0);
