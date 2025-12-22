73db5e5cbc1a0c22df84a97da5084c573b16e210
Sergio Sánchez Ramírez <15837247+mofeing@users.noreply.github.com>
2025-12-22 01:40:27 +0100

    add capi for complex::NumberAttr

---
+++ mlir/include/mlir-c/Dialect/Complex.h
@@ -0,0 +1,53 @@
+//===-- mlir-c/Dialect/Complex.h - C API for Complex dialect ------*- C -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM
+// Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef MLIR_C_DIALECT_COMPLEX_H
+#define MLIR_C_DIALECT_COMPLEX_H
+
+#include "mlir-c/IR.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+MLIR_DECLARE_CAPI_DIALECT_REGISTRATION(Func, func);
+
+/// Checks whether the given attribute is a complex attribute.
+MLIR_CAPI_EXPORTED bool mlirAttributeIsAComplex(MlirAttribute attr);
+
+/// Creates a complex attribute in the given context with the given
+/// double real and imaginary values and double-precision FP semantics.
+MLIR_CAPI_EXPORTED MlirAttribute mlirComplexAttrDoubleGet(MlirContext ctx,
+                                                        MlirType type,
+                                                        double real,
+                                                        double imag);
+
+/// Same as "mlirComplexAttrDoubleGet", but if the type is not valid for a
+/// construction of a ComplexAttr, returns a null MlirAttribute.
+MLIR_CAPI_EXPORTED MlirAttribute mlirComplexAttrDoubleGetChecked(MlirLocation loc,
+                                                               MlirType type,
+                                                               double real,
+                                                               double imag);
+
+/// Returns the real value stored in the given complex attribute, interpreting
+/// the value as double. 
+MLIR_CAPI_EXPORTED double mlirComplexAttrGetRealDouble(MlirAttribute attr);
+
+/// Returns the imaginaryvalue stored in the given complex attribute,
+/// interpreting the value as double.
+MLIR_CAPI_EXPORTED double mlirComplexAttrGetImagDouble(MlirAttribute attr);
+
+/// Returns the typeID of a Complex attribute.
+MLIR_CAPI_EXPORTED MlirTypeID mlirComplexAttrGetTypeID(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // MLIR_C_DIALECT_COMPLEX_H
---
+++ mlir/lib/CAPI/Dialect/Complex.cpp
@@ -0,0 +1,44 @@
+//===- Complex.cpp - C Interface for Complex dialect ----------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "mlir-c/Dialect/Complex.h"
+#include "mlir-c/IR.h"
+#include "mlir-c/Support.h"
+#include "mlir/CAPI/Registration.h"
+
+MLIR_DEFINE_CAPI_DIALECT_REGISTRATION(Complex, complex,
+                                     mlir::complex::ComplexDialect)
+
+bool mlirAttributeIsAComplex(MlirAttribute attr) {
+  return llvm::isa<ComplexAttr>(unwrap(attr));
+}
+
+MlirAttribute mlirComplexAttrDoubleGet(MlirContext ctx, MlirType type,
+                                       double real, double imag) {
+  return wrap(
+      complex::NumberAttr::get(cast<ComplexType>(unwrap(type)), real, imag));
+}
+
+MlirAttribute mlirComplexAttrDoubleGetChecked(MlirLocation loc, MlirType type,
+                                              double real, double imag) {
+  return wrap(complex::NumberAttr::getChecked(
+      unwrap(loc), cast<ComplexType>(unwrap(type)), real, imag));
+}
+
+double mlirComplexAttrGetRealDouble(MlirAttribute attr) {
+  return llvm::cast<complex::NumberAttr>(unwrap(attr)).getRealAsDouble();
+}
+
+double mlirComplexAttrGetImagDouble(MlirAttribute attr) {
+  return llvm::cast<complex::NumberAttr>(unwrap(attr)).getImagAsDouble();
+}
+
+MlirTypeID mlirComplexAttrGetTypeID(void) {
+  return wrap(complex::NumberAttr::getTypeID());
+}
+
