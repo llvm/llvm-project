e880f13d4f06c8eaeee4912ea7455752d4125dc8
Sergio Sánchez Ramírez <mofeing+github@gmail.com>
2025-12-22 22:43:49 +0100

    add implementation files for CAPI

---
+++ mlir/lib/CAPI/Dialect/ArmSME.cpp
@@ -0,0 +1,14 @@
+//===- ArmSME.cpp - C Interface for ArmSME dialect ------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "mlir-c/Dialect/ArmSME.h"
+#include "mlir/CAPI/Registration.h"
+#include "mlir/Dialect/ArmSME/IR/ArmSME.h"
+
+MLIR_DEFINE_CAPI_DIALECT_REGISTRATION(ArmSME, arm_sme,
+                                      mlir::arm_sme::ArmSMEDialect)
---
+++ mlir/lib/CAPI/Dialect/ArmSVE.cpp
@@ -0,0 +1,14 @@
+//===- ArmSVE.cpp - C Interface for ArmSVE dialect ------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "mlir-c/Dialect/ArmSVE.h"
+#include "mlir/CAPI/Registration.h"
+#include "mlir/Dialect/ArmSVE/IR/ArmSVE.h"
+
+MLIR_DEFINE_CAPI_DIALECT_REGISTRATION(ArmSVE, arm_sve,
+                                      mlir::arm_sve::ArmSVEDialect)
