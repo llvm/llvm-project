d3761d09f13ba5a3047a4155517f82e371c3217d
Alexis Engelke <engelke@in.tum.de>
2025-12-22 11:42:27 +0100

    [LLVM][NFC] Move PassPlugin from Passes to Extensions lib
    This avoid pulling in the entire Passes library with all passes as
    dependencies when just referring to PassPlugin, which is in fact
    independent of the Passes themselves.
    
    Pull Request: https://github.com/llvm/llvm-project/pull/172478

--- llvm/lib/Extensions/PassPlugin.cpp
+++ llvm/lib/Extensions/PassPlugin.cpp
@@ -1,4 +1,4 @@
-//===- lib/Passes/PassPluginLoader.cpp - Load Plugins for New PM Passes ---===//
+//===----------------------------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,7 +6,7 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "llvm/Passes/PassPlugin.h"
+#include "llvm/Extensions/PassPlugin.h"
 #include "llvm/Support/raw_ostream.h"
 
 #include <cstdint>
