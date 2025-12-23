9b30c2c385b918a604ed9c0f24936ce111754fba
Leandro Lupori <leandro.lupori@linaro.org>
2025-12-23 13:42:22 -0300

    Add explicit type declarations

--- flang/test/Semantics/bug127425.f90
+++ flang/test/Semantics/bug127425.f90
@@ -1,6 +1,8 @@
 ! RUN: %python %S/test_errors.py %s %flang_fc1
 
 program main
+  implicit none
+  integer :: ja, jb, j3, j4, j5
   pointer (j1,ja)
   pointer (j2,jb)
   !ERROR: Values in array constructor must have the same declared type when no explicit type appears
