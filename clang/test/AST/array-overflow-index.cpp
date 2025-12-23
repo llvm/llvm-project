d7679cbd4a256fa1bbbc70b5919835403e23c224
Balaji V. Iyer <bviyer@gmail.com>
2025-12-22 18:36:25 -0600

    Added test without constant interpreter

--- clang/test/AST/array-overflow-index.cpp
+++ clang/test/AST/array-overflow-index.cpp
@@ -1,10 +1,10 @@
-// RUN: %clang_cc1  --std=c++17 -fexperimental-new-constant-interpreter -verify %s
+// RUN: %clang_cc1  --std=c++17  -verify=none %s
+// RUN: %clang_cc1  --std=c++17 -fexperimental-new-constant-interpreter -verify=experiment %s
 
+// none-no-diagnostics
 
 constexpr int __attribute__((vector_size(4))) test_vector = {1};
 
-// expected-error@+1 {{constexpr function never produces a constant expression}}
-constexpr int get_last_element(void) {
-    // expected-note@+1 {{cannot refer to element}}
-    return test_vector[~0UL];
+constexpr int get_last_element(void) { // experiment-error {{constexpr function never produces a constant expression}}
+    return test_vector[~0UL]; // experiment-note {{cannot refer to element}}
 }
