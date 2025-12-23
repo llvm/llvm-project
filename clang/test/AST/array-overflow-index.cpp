8b26452dc5ff40b71566cecaff0826785da4abba
Balaji V. Iyer <bviyer@gmail.com>
2025-12-23 16:53:58 -0600

    Added fix for CI

--- clang/test/AST/array-overflow-index.cpp
+++ clang/test/AST/array-overflow-index.cpp
@@ -1,10 +1,13 @@
-// RUN: %clang_cc1  --std=c++17  -verify=none %s
-// RUN: %clang_cc1  --std=c++17 -fexperimental-new-constant-interpreter -verify=experiment %s
+// RUN: %clang_cc1  --std=c++17  -fexperimental-new-constant-interpreter -verify=experiment %s
+// RUN: %clang_cc1  --std=c++17  -triple x86_64-pc-win32 -verify=experiment %s
+// RUN: %clang_cc1  --std=c++17  -triple x86_64-pc-linux -verify=none %s
 
+#ifndef _MSC_VER
 // none-no-diagnostics
+#endif
 
 constexpr int __attribute__((vector_size(4))) test_vector = {1};
-
 constexpr int get_last_element(void) { // experiment-error {{constexpr function never produces a constant expression}}
     return test_vector[~0UL]; // experiment-note {{cannot refer to element}}
 }
+
