8443a392a10a99b404c2cef1755d374f9bbd9afd
Joshua Batista <jbatista@microsoft.com>
2025-12-23 14:17:13 -0800

    remove the double quotes

--- clang/test/Options/all_resources_bound.hlsl
+++ clang/test/Options/all_resources_bound.hlsl
@@ -1,8 +1,8 @@
 // RUN: %clang_dxc -T lib_6_4 -all-resources-bound %s 2>&1 -### | FileCheck -check-prefix=ARB %s
 // RUN: %clang_dxc -T lib_6_4 %s 2>&1 -### | FileCheck -check-prefix=NO_ARB %s
 
-// ARB: "hlsl-all-resources-bound"
-// NO_ARB-NOT: "hlsl-all-resources-bound"
+// ARB: hlsl-all-resources-bound
+// NO_ARB-NOT: hlsl-all-resources-bound
 // assert expected CC1 option is present
 float4 main(float4 a : A) : SV_TARGET
 {
