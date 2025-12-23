000b23a498bc76d703d4820d38258eb4f7aca4fc
Victor Chernyakin <chernyakin.victor.j@outlook.com>
2025-12-23 11:35:10 -0700

    [clang-tidy][NFC] Prefer `static constexpr` over `static const` where possible

--- clang-tools-extra/clang-tidy/readability/StringCompareCheck.cpp
+++ clang-tools-extra/clang-tidy/readability/StringCompareCheck.cpp
@@ -19,12 +19,14 @@ namespace optutils = clang::tidy::utils::options;
 
 namespace clang::tidy::readability {
 
-static const StringRef CompareMessage = "do not use 'compare' to test equality "
-                                        "of strings; use the string equality "
-                                        "operator instead";
+static constexpr StringRef CompareMessage =
+    "do not use 'compare' to test equality "
+    "of strings; use the string equality "
+    "operator instead";
 
-static const StringRef DefaultStringLikeClasses = "::std::basic_string;"
-                                                  "::std::basic_string_view";
+static constexpr StringRef DefaultStringLikeClasses =
+    "::std::basic_string;"
+    "::std::basic_string_view";
 
 StringCompareCheck::StringCompareCheck(StringRef Name,
                                        ClangTidyContext *Context)
