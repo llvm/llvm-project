65bd2facb00d7f5eef4f67843a6ad4c026b21082
Aiden Grossman <aidengrossman@google.com>
2025-12-23 06:55:18 +0000

    [flang] Remove REQUIRES: shell lines form tests
    The shell feature only implies that we are not running on Windows now
    that the internal shell feature is available everywhere. Replace it with
    UNSUPPORTED: system-windows on non-portable tests so we can eventually
    get rid of the feature.

--- flang/test/Examples/print-fns-interfaces.f90
+++ flang/test/Examples/print-fns-interfaces.f90
@@ -2,7 +2,7 @@
 ! (It should only count definitions, which will appear elsewhere for interfaced functions/subroutines)
 ! This requires that the examples are built (LLVM_BUILD_EXAMPLES=ON) to access flangPrintFunctionNames.so
 
-! REQUIRES: plugins, examples, shell
+! REQUIRES: plugins, examples
 
 ! RUN: %flang_fc1 -load %llvmshlibdir/flangPrintFunctionNames%pluginext -plugin print-fns %s 2>&1 | FileCheck %s
 
