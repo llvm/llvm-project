4b65538b6eee4bd32e6cc2464e8558a1b52aef30
Enna1 <xumingjie.enna1@bytedance.com>
2025-12-24 10:12:30 +0800

    [IR] Change PHINode::removeIncomingValueIf() to loop incoming values backwards

--- llvm/lib/IR/Instructions.cpp
+++ llvm/lib/IR/Instructions.cpp
@@ -160,18 +160,17 @@ Value *PHINode::removeIncomingValue(unsigned Idx, bool DeletePHIIfEmpty) {
 void PHINode::removeIncomingValueIf(function_ref<bool(unsigned)> Predicate,
                                     bool DeletePHIIfEmpty) {
   unsigned NumOps = getNumIncomingValues();
-  unsigned Idx = 0;
-  while (Idx < NumOps) {
+
+  // Loop backwards in case the predicate is purely index based.
+  for (int Idx = NumOps - 1; Idx >= 0; --Idx) {
     if (Predicate(Idx)) {
-      unsigned LastIdx = NumOps - 1;
+      int LastIdx = NumOps - 1;
       if (Idx != LastIdx) {
         setIncomingValue(Idx, getIncomingValue(LastIdx));
         setIncomingBlock(Idx, getIncomingBlock(LastIdx));
       }
       getOperandUse(LastIdx).set(nullptr);
       NumOps--;
-    } else {
-      Idx++;
     }
   }
 
