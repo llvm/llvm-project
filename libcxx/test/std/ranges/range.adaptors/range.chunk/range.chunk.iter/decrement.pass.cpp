f6554ea2a0e1d9571cf82fc7b2f3d1115c466474
anonymous <shyeyian@petalmail.com>
2025-12-24 22:05:29 +0800

    Implement `views::chunk`.
    This commit squashes previous commits.

---
+++ libcxx/test/std/ranges/range.adaptors/range.chunk/range.chunk.iter/decrement.pass.cpp
@@ -0,0 +1,60 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// REQUIRES: std-at-least-c++23
+
+// <ranges>
+
+//   constexpr iterator& operator--()
+//     requires bidirectional_range<Base>;
+//   constexpr iterator operator--(int)
+//     requires bidirectional_range<Base>;
+//   constexpr iterator& operator-=(difference_type)
+//     requires random_access_range<Base>;
+
+#include <algorithm>
+#include <cassert>
+#include <iterator>
+#include <ranges>
+#include <vector>
+
+#include "test_range.h"
+
+constexpr bool test() {
+  std::vector<int> vector                                                  = {1, 2, 3, 4, 5, 6, 7, 8};
+  std::ranges::chunk_view<std::ranges::ref_view<std::vector<int>>> chunked = vector | std::views::chunk(2);
+
+  // Test `constexpr iterator& operator--();`
+  {
+    /*chunk_view::__outer_iterator*/ std::bidirectional_iterator auto it = chunked.end();
+    assert(std::ranges::equal(*--it, std::vector{7, 8}));
+  }
+
+  // Test `constexpr iterator operator--(int)`
+  {
+    /*chunk_view::__outer_iterator*/ std::bidirectional_iterator auto it = chunked.end();
+    it--;
+    assert(std::ranges::equal(*it, std::vector{7, 8}));
+  }
+
+  // Test `constexpr iterator& operator-=(difference_type)`
+  {
+    /*chunk_view::__iterator*/ std::random_access_iterator auto it = chunked.end();
+    it -= 3;
+    assert(std::ranges::equal(*it, std::vector{3, 4}));
+  }
+
+  return true;
+}
+
+int main(int, char**) {
+  test();
+  static_assert(test());
+
+  return 0;
+}
