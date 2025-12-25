2c60146ba115c292093d4fa7d4b6d9e97d5625b8
Susan Tan <zujunt@nvidia.com>
2025-12-24 16:20:54 -0800

    change description

--- flang/include/flang/Optimizer/Transforms/FIRToMemRefTypeConverter.h
+++ flang/include/flang/Optimizer/Transforms/FIRToMemRefTypeConverter.h
@@ -1,5 +1,4 @@
-//===- FIRToMemRefTypeConverter.h - FIR type conversion to MemRef -*- C++
-//-*-==//
+//===---- FIRToMemRefTypeConverter.h - FIR type conversion to MemRef ------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
