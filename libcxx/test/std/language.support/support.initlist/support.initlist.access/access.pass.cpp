4f532c29263b21de5ac1731cfcf2f237a028be70
A. Jiang <de34@live.cn>
2025-12-26 18:14:37 +0800

    [libc++] P3016R6: Resolve inconsistencies in begin/end for `valarray` and braced initializer lists
    This paper is implemented as a Defect Report against C++11 and later
    revisions.
    - Inclusion of `<typeinfo>` in `<functional>` should be considered as a
    patch to C++ TR1 [N1836](https://wg21.link/N1836), while the related
    parts were merged into C++11.
    - Changes of `initializer_list` should be considered as a patch to C++11
    paper [N2672](https://wg21.link/n2672).
    - Changes of `valarray`, `std::begin`, and `std::end` and availability
    of range access functions in `<valarray>` should be considered as a
    patch to C++11 paper [N2930](https://wg21.link/n2930).
    - Inclusion of `<typeinfo>` in `<typeindex>` should be considered as a
    patch to C++11 paper [N2932](https://wg21.link/n2932).
    - Changes of `std::cbegin`, `std::cend`, `std::rbegin`, `std::rend`,
    `std::crbegin`, and `std::crend` and the inclusion of
    `<initializer_list>` in `<iterator>` should be considered as a patch to
    [LWG2128](https://wg21.link/lwg2128), while LWG2128 is consistently
    considered to be a pure feature addition in C++14 by implementations.
    - Changes of `std::data`, `std::empty`, and `std::size` should be
    considered as a patch to C++17 paper [N4280](https://wg21.link/n4280).
    - Inclusion of `<initializer_list>` and `<typeinfo>` in `<any>` should
    be considered as a patch to C++17 paper
    [P0220R1](https://wg21.link/p0220r1).
    - Changes of `std::ssize` should be considered as a patch to C++20 paper
    [P1227R2](https://wg21.link/p1227r2).
    - Inclusion of `<initializer_list>` in `<functional>` should be
    considered as a patch to C++23 paper
    [P0288R9](https://wg21.link/p0288r9).
    - Availability of range access functions in `<stacktrace>` should be
    considered as a patch to C++23 paper
    [P0881R7](https://wg21.link/p0881r7).
    - Availability of range access functions in `<optional>` should be
    considered as a patch to C++26 paper
    [P3168R2](https://wg21.link/p3168r2).
    
    Notes for backported and missing parts:
    - Changes of `<stacktrace>` are not implemented because P0881R7 is not
    implemented. But I think P3016R6 should be considered complete with note
    added.
    - `constexpr` for member functions of `initializer_list` is only added
    since C++14, in the spirit of C++14 paper
    [N3471](https://wg21.link/n3471).
    - Changes of inclusions and `valarray` are unconditionally implemented,
    which follows the current convention of libc++.

--- libcxx/test/std/language.support/support.initlist/support.initlist.access/access.pass.cpp
+++ libcxx/test/std/language.support/support.initlist/support.initlist.access/access.pass.cpp
@@ -10,9 +10,11 @@
 
 // template<class E> class initializer_list;
 
-// const E* begin() const;
-// const E* end() const;
-// size_t size() const;
+// const E* begin() const noexcept; // constexpr since C++14
+// const E* end() const noexcept;   // constexpr since C++14
+// const E* data() const noexcept;  // constexpr since C++14
+// size_t size() const noexcept;    // constexpr since C++14
+// bool empty() const noexcept;     // constexpr since C++14
 
 #include <initializer_list>
 #include <cassert>
@@ -20,43 +22,78 @@
 
 #include "test_macros.h"
 
+ASSERT_SAME_TYPE(decltype(std::initializer_list<int>{}.begin()), const int*);
+ASSERT_SAME_TYPE(decltype(std::initializer_list<int>{}.end()), const int*);
+ASSERT_SAME_TYPE(decltype(std::initializer_list<int>{}.data()), const int*);
+ASSERT_SAME_TYPE(decltype(std::initializer_list<int>{}.size()), std::size_t);
+ASSERT_SAME_TYPE(decltype(std::initializer_list<int>{}.empty()), bool);
+
+ASSERT_NOEXCEPT(std::initializer_list<int>{}.begin());
+ASSERT_NOEXCEPT(std::initializer_list<int>{}.end());
+ASSERT_NOEXCEPT(std::initializer_list<int>{}.data());
+ASSERT_NOEXCEPT(std::initializer_list<int>{}.size());
+ASSERT_NOEXCEPT(std::initializer_list<int>{}.empty());
+
 struct A
 {
-    A(std::initializer_list<int> il)
+  TEST_CONSTEXPR_CXX14 A(std::initializer_list<int> il) {
     {
-        const int* b = il.begin();
-        const int* e = il.end();
-        assert(il.size() == 3);
-        assert(static_cast<std::size_t>(e - b) == il.size());
-        assert(*b++ == 3);
-        assert(*b++ == 2);
-        assert(*b++ == 1);
+      const int* b = il.begin();
+      const int* e = il.end();
+      assert(il.data() == b);
+      assert(il.size() == 3);
+      assert(!il.empty());
+      assert(static_cast<std::size_t>(e - b) == il.size());
+      assert(*b++ == 3);
+      assert(*b++ == 2);
+      assert(*b++ == 1);
     }
+    {
+      const auto cil = il;
+      const int* b   = cil.begin();
+      const int* e   = cil.end();
+      assert(cil.data() == b);
+      assert(cil.size() == 3);
+      assert(!cil.empty());
+      assert(static_cast<std::size_t>(e - b) == cil.size());
+      assert(*b++ == 3);
+      assert(*b++ == 2);
+      assert(*b++ == 1);
+    }
+  }
 };
 
-#if TEST_STD_VER > 11
-struct B
-{
-    constexpr B(std::initializer_list<int> il)
-    {
-        const int* b = il.begin();
-        const int* e = il.end();
-        assert(il.size() == 3);
-        assert(static_cast<std::size_t>(e - b) == il.size());
-        assert(*b++ == 3);
-        assert(*b++ == 2);
-        assert(*b++ == 1);
-    }
-};
+TEST_CONSTEXPR_CXX14 bool test_empty_ilist() {
+  {
+    std::initializer_list<int> il{};
+    const int* b = il.begin();
+    const int* e = il.end();
+    assert(il.data() == b);
+    assert(il.size() == 0);
+    assert(il.empty());
+    assert(static_cast<std::size_t>(e - b) == il.size());
+  }
+  {
+    const std::initializer_list<int> cil{};
+    const int* b = cil.begin();
+    const int* e = cil.end();
+    assert(cil.data() == b);
+    assert(cil.size() == 0);
+    assert(cil.empty());
+    assert(static_cast<std::size_t>(e - b) == cil.size());
+  }
 
-#endif // TEST_STD_VER > 11
+  return true;
+}
 
 int main(int, char**)
 {
     A test1 = {3, 2, 1}; (void)test1;
+    test_empty_ilist();
 #if TEST_STD_VER > 11
-    constexpr B test2 = {3, 2, 1};
+    constexpr A test2 = {3, 2, 1};
     (void)test2;
+    static_assert(test_empty_ilist(), "");
 #endif // TEST_STD_VER > 11
 
   return 0;
