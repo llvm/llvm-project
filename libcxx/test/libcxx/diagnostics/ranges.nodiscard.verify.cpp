a8c6045d4efd0eab94a27267662685d88f941934
Hristo Hristov <hghristov.rmm@gmail.com>
2025-12-27 10:46:49 +0200

    Revert changes

--- libcxx/test/libcxx/diagnostics/ranges.nodiscard.verify.cpp
+++ libcxx/test/libcxx/diagnostics/ranges.nodiscard.verify.cpp
@@ -10,40 +10,39 @@
 
 // UNSUPPORTED: c++03, c++11, c++14, c++17
 
+// clang-format off
+
 #include <ranges>
 #include <functional>
-#include <utility>
 #include <vector>
 
 #include "test_macros.h"
-
-// clang-format off
 
 void test() {
   std::vector<int> range;
   std::ranges::less_equal pred;
 
-#if TEST_STD_VER >= 23
-  auto rvalue_view = std::views::as_rvalue(range);
-  std::views::as_rvalue(range);       // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
-  std::views::as_rvalue(rvalue_view); // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
-#endif // TEST_STD_VER >= 23
-
-  std::views::drop(pred); // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
-
-#if TEST_STD_VER >= 23
-  std::views::drop(std::views::repeat(1)); // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
-
-  std::views::repeat(1, std::unreachable_sentinel); // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
-#endif // TEST_STD_VER >= 23
+  std::views::drop(pred);                  // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
 
   std::views::split(range, 3); // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
   std::views::split(1);        // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
 
-  std::views::take(range, 3);  // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
-  std::views::take(1);         // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
+  std::views::take(range, 3); // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
+  std::views::take(1);        // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
 
 #if TEST_STD_VER >= 23
+  std::views::drop(std::views::repeat(1)); // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
+
+  std::views::repeat(1);                            // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
+  std::views::repeat(1, std::unreachable_sentinel); // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
+
+  auto rvalue_view = std::views::as_rvalue(range);
+  std::views::as_rvalue(range);       // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
+  std::views::as_rvalue(rvalue_view); // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
+
+  std::views::chunk_by(pred);        // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
+  std::views::chunk_by(range, pred); // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
+
   std::views::take(std::views::repeat(3), 3);                            // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
   std::views::take(std::views::repeat(3, std::unreachable_sentinel), 3); // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
 
