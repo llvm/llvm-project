b3e08248a634c399056a663e5f2b92737dbbcd92
paperchalice <liujunchang97@outlook.com>
2025-12-28 08:29:06 +0800

    [CodeGen] Port gc-empty-basic-blocks to new pass manager (#137585)
    Co-authored-by: Aiden Grossman <aidengrossman@google.com>

--- llvm/lib/CodeGen/TargetPassConfig.cpp
+++ llvm/lib/CodeGen/TargetPassConfig.cpp
@@ -1246,7 +1246,7 @@ void TargetPassConfig::addMachinePasses() {
   }
 
   if (GCEmptyBlocks)
-    addPass(llvm::createGCEmptyBasicBlocksPass());
+    addPass(llvm::createGCEmptyBasicBlocksLegacyPass());
 
   if (EnableFSDiscriminator)
     addPass(createMIRAddFSDiscriminatorsPass(
