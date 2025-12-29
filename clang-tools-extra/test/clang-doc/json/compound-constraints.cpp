25c7a3694fb44142e0c31ba5bd790d631cbdad2d
Erick Velez <erickvelez7@gmail.com>
2025-12-29 13:27:28 -0800

    fix conflicts

--- clang-tools-extra/test/clang-doc/json/compound-constraints.cpp
+++ clang-tools-extra/test/clang-doc/json/compound-constraints.cpp
@@ -148,52 +148,60 @@ template<typename T> requires (Incrementable<T> && Decrementable<T>) || PreIncre
 // CHECK-HTML-NEXT:         <div>
 // CHECK-HTML-NEXT:             <pre><code class="language-cpp code-clang-doc">template &lt;typename T&gt; Incrementable requires (T a) { a++; }</code></pre> 
 // CHECK-HTML-NEXT:         </div>
-// CHECK-HTML-NEXT:         <div>
+// CHECK-HTML-NEXT:         <div class="nested-delimiter-container">
 // CHECK-HTML-NEXT:             <p> Concept for an incrementable value</p>
 // CHECK-HTML-NEXT:         </div>
 // CHECK-HTML-NEXT:         <h3>Template Parameters</h3>
-// CHECK-HTML-NEXT:         <div>
-// CHECK-HTML-NEXT:             <b>T</b>   A value that can be incremented.
+// CHECK-HTML-NEXT:         <div class="nested-delimiter-container">
+// CHECK-HTML-NEXT:             <div>
+// CHECK-HTML-NEXT:                 <b>T</b>   A value that can be incremented.
+// CHECK-HTML-NEXT:             </div>
 // CHECK-HTML-NEXT:         </div> 
-// CHECK-HTML-NEXT:         <p>Defined at line [[@LINE-149]] of file {{.*}}compound-constraints.cpp</p>
+// CHECK-HTML-NEXT:         <p>Defined at line [[@LINE-151]] of file {{.*}}compound-constraints.cpp</p>
 // CHECK-HTML-NEXT:     </div>
 // CHECK-HTML-NEXT:     <div id="" class="delimiter-container">
 // CHECK-HTML-NEXT:         <div>
 // CHECK-HTML-NEXT:             <pre><code class="language-cpp code-clang-doc">template &lt;typename T&gt; Decrementable requires (T a) { a--; }</code></pre> 
 // CHECK-HTML-NEXT:         </div>
-// CHECK-HTML-NEXT:         <div>
+// CHECK-HTML-NEXT:         <div class="nested-delimiter-container">
 // CHECK-HTML-NEXT:             <p> Concept for a decrementable value</p>
 // CHECK-HTML-NEXT:         </div>
 // CHECK-HTML-NEXT:         <h3>Template Parameters</h3>
-// CHECK-HTML-NEXT:         <div>
-// CHECK-HTML-NEXT:             <b>T</b>   A value that can be decremented
+// CHECK-HTML-NEXT:         <div class="nested-delimiter-container">
+// CHECK-HTML-NEXT:             <div>
+// CHECK-HTML-NEXT:                 <b>T</b>   A value that can be decremented
+// CHECK-HTML-NEXT:             </div>
 // CHECK-HTML-NEXT:         </div> 
-// CHECK-HTML-NEXT:         <p>Defined at line [[@LINE-155]] of file {{.*}}compound-constraints.cpp</p>
+// CHECK-HTML-NEXT:         <p>Defined at line [[@LINE-159]] of file {{.*}}compound-constraints.cpp</p>
 // CHECK-HTML-NEXT:     </div>
 // CHECK-HTML-NEXT:     <div id="" class="delimiter-container">
 // CHECK-HTML-NEXT:         <div>
 // CHECK-HTML-NEXT:             <pre><code class="language-cpp code-clang-doc">template &lt;typename T&gt; PreIncrementable requires (T a) { ++a; }</code></pre> 
 // CHECK-HTML-NEXT:         </div>
-// CHECK-HTML-NEXT:         <div>
+// CHECK-HTML-NEXT:         <div class="nested-delimiter-container">
 // CHECK-HTML-NEXT:             <p> Concept for a pre-incrementable value</p>
 // CHECK-HTML-NEXT:         </div>
 // CHECK-HTML-NEXT:         <h3>Template Parameters</h3>
-// CHECK-HTML-NEXT:         <div>
-// CHECK-HTML-NEXT:             <b>T</b>   A value that can be pre-incremented
+// CHECK-HTML-NEXT:         <div class="nested-delimiter-container">
+// CHECK-HTML-NEXT:             <div>
+// CHECK-HTML-NEXT:                 <b>T</b>   A value that can be pre-incremented
+// CHECK-HTML-NEXT:             </div>
 // CHECK-HTML-NEXT:         </div> 
-// CHECK-HTML-NEXT:         <p>Defined at line [[@LINE-161]] of file {{.*}}compound-constraints.cpp</p>
+// CHECK-HTML-NEXT:         <p>Defined at line [[@LINE-167]] of file {{.*}}compound-constraints.cpp</p>
 // CHECK-HTML-NEXT:     </div>
 // CHECK-HTML-NEXT:     <div id="" class="delimiter-container">
 // CHECK-HTML-NEXT:         <div>
 // CHECK-HTML-NEXT:             <pre><code class="language-cpp code-clang-doc">template &lt;typename T&gt; PreDecrementable requires (T a) { --a; }</code></pre> 
 // CHECK-HTML-NEXT:         </div>
-// CHECK-HTML-NEXT:         <div>
+// CHECK-HTML-NEXT:         <div class="nested-delimiter-container">
 // CHECK-HTML-NEXT:             <p> Concept for a -pre-decrementable value</p>
 // CHECK-HTML-NEXT:         </div>
 // CHECK-HTML-NEXT:         <h3>Template Parameters</h3>
-// CHECK-HTML-NEXT:         <div>
-// CHECK-HTML-NEXT:             <b>T</b>   A value that can be pre-decremented
+// CHECK-HTML-NEXT:         <div class="nested-delimiter-container">
+// CHECK-HTML-NEXT:             <div>
+// CHECK-HTML-NEXT:                 <b>T</b>   A value that can be pre-decremented
+// CHECK-HTML-NEXT:             </div>
 // CHECK-HTML-NEXT:         </div> 
-// CHECK-HTML-NEXT:         <p>Defined at line [[@LINE-167]] of file {{.*}}compound-constraints.cpp</p>
+// CHECK-HTML-NEXT:         <p>Defined at line [[@LINE-175]] of file {{.*}}compound-constraints.cpp</p>
 // CHECK-HTML-NEXT:     </div>
 // CHECK-HTML-NEXT: </section>
