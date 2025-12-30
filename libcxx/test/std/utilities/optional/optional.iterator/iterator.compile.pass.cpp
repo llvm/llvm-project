08cee47ee327a20cc10eecea41b482694278bf8e
William Tran-Viet <wtranviet@proton.me>
2025-12-29 21:27:40 -0500

    Implement LWG4308

--- libcxx/test/std/utilities/optional/optional.iterator/iterator.compile.pass.cpp
+++ libcxx/test/std/utilities/optional/optional.iterator/iterator.compile.pass.cpp
@@ -25,5 +25,6 @@ static_assert(has_iterator_aliases<std::optional<int>>);
 static_assert(has_iterator_aliases<std::optional<const int>>);
 static_assert(has_iterator_aliases<std::optional<int&>>);
 static_assert(has_iterator_aliases<std::optional<const int&>>);
-static_assert(!has_iterator_aliases<std::optional<int (&)[1]>>);
+static_assert(has_iterator_aliases<std::optional<int (&)[1]>>);
+static_assert(!has_iterator_aliases<std::optional<int (&)[]>>);
 static_assert(!has_iterator_aliases<std::optional<int (&)()>>);
