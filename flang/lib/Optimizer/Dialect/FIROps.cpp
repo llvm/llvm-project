c1fa5e8fb1a81aa412722c21e08999b1c85b2e65
Slava Zakharin <szakharin@nvidia.com>
2025-12-30 13:18:46 -0800

    Avoid calling `mayBeAbsentBox` for `fir.box_addr` with non-box/class input.

--- flang/lib/Optimizer/Dialect/FIROps.cpp
+++ flang/lib/Optimizer/Dialect/FIROps.cpp
@@ -1157,6 +1157,10 @@ std::optional<std::int64_t> fir::BoxAddrOp::getViewOffset(mlir::OpResult) {
 }
 
 mlir::Speculation::Speculatability fir::BoxAddrOp::getSpeculatability() {
+  // Do not speculate fir.box_addr with BoxProcType and BoxCharType
+  // inputs.
+  if (!mlir::isa<fir::BaseBoxType>(getVal().getType()))
+    mlir::Speculation::NotSpeculatable;
   return mayBeAbsentBox(getVal()) ? mlir::Speculation::NotSpeculatable
                                   : mlir::Speculation::Speculatable;
 }
