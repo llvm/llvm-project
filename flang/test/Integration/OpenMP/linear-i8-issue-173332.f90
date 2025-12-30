8091494ea9c310f89ff5f3540dfb354891fdcc5a
KrxGu <krishom70@gmail.com>
2025-12-30 18:54:06 +0530

    Fix integration test to prevent dead code elimination
    - Initialize variable j and use it in the loop body
    - Update CHECK-LABEL to match actual function name (repro_issue_173332_)
    - Ensures the compiler generates IR for the test case

--- flang/test/Integration/OpenMP/linear-i8-issue-173332.f90
+++ flang/test/Integration/OpenMP/linear-i8-issue-173332.f90
@@ -6,14 +6,16 @@ subroutine repro_issue_173332
     implicit none
     integer(8) :: i, j
 
+    j = 0
     ! This used to fail with:
     ! "Cannot create binary operator with two operands of differing type!"
     ! The fix ensures all arithmetic is normalized to the linear variable's type (i64)
     !$omp parallel do simd linear(j)
     do i = 1,100,1
+        j = j + 1
     end do
     !$omp end parallel do simd
 end subroutine repro_issue_173332
 
-! CHECK-LABEL: define {{.*}} @_QPrepro_issue_173332
+! CHECK-LABEL: define {{.*}} @repro_issue_173332_
 ! CHECK: ret void
