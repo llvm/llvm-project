llvm/llvm-project

commit 7f780046e2ae79218dd6abc2d008c1a9eeddedc7

[SelectionDAG] Use SLEB128 for signed integers in isel table instead of 'signed rotated'. NFC (#173936)
Previously, we used a VBR that stored the sign bit in bit 0 followed
by the absolute value in subsequent bits.

This patch changes it to use SLEB128 which discards redundant sign
bits, but keeps the bits in the same positions. This uses the same
number of bytes to encode values so doesn't change the table size.

My goal is to remove OPC_EmitStringInteger as a special opcode type.
Instead, we can print the string directly with OPC_EmitInteger for
any string that has an enum value of 0..63.

llvm/lib/CodeGen/SelectionDAG/SelectionDAGISel.cpp
@@ -96,6 +96,7 @@
 #include "llvm/Support/Debug.h"
 #include "llvm/Support/ErrorHandling.h"
 #include "llvm/Support/KnownBits.h"
+#include "llvm/Support/LEB128.h"
 #include "llvm/Support/Timer.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Target/TargetMachine.h"
@@ -2719,6 +2720,14 @@ GetVBR(uint64_t Val, const uint8_t *MatcherTable, unsigned &Idx) {
   return Val;
 }
 
+LLVM_ATTRIBUTE_ALWAYS_INLINE static int64_t
+GetSignedVBR(const unsigned char *MatcherTable, unsigned &Idx) {
+  unsigned Len;
+  int64_t Val = decodeSLEB128(&MatcherTable[Idx], &Len);
+  Idx += Len;
+  return Val;
+}
+
 /// getSimpleVT - Decode a value in MatcherTable, if it's a VBR encoded value,
 /// use GetVBR to decode it.
 LLVM_ATTRIBUTE_ALWAYS_INLINE static MVT::SimpleValueType
@@ -3022,24 +3031,9 @@ CheckValueType(const uint8_t *MatcherTable, unsigned &MatcherIndex, SDValue N,
   return VT == MVT::iPTR && cast<VTSDNode>(N)->getVT() == TLI->getPointerTy(DL);
 }
 
-// Bit 0 stores the sign of the immediate. The upper bits contain the magnitude
-// shifted left by 1.
-static uint64_t decodeSignRotatedValue(uint64_t V) {
-  if ((V & 1) == 0)
-    return V >> 1;
-  if (V != 1)
-    return -(V >> 1);
-  // There is no such thing as -0 with integers.  "-0" really means MININT.
-  return 1ULL << 63;
-}
-
 LLVM_ATTRIBUTE_ALWAYS_INLINE static bool
 CheckInteger(const uint8_t *MatcherTable, unsigned &MatcherIndex, SDValue N) {
-  int64_t Val = MatcherTable[MatcherIndex++];
-  if (Val & 128)
-    Val = GetVBR(Val, MatcherTable, MatcherIndex);
-
-  Val = decodeSignRotatedValue(Val);
+  int64_t Val = GetSignedVBR(MatcherTable, MatcherIndex);
 
   ConstantSDNode *C = dyn_cast<ConstantSDNode>(N);
   return C && C->getAPIntValue().trySExtValue() == Val;
@@ -3919,11 +3913,14 @@ void SelectionDAGISel::SelectCodeCommon(SDNode *NodeToMatch,
         VT = getSimpleVT(MatcherTable, MatcherIndex);
         break;
       }
-      int64_t Val = MatcherTable[MatcherIndex++];
-      if (Val & 128)
-        Val = GetVBR(Val, MatcherTable, MatcherIndex);
-      if (Opcode >= OPC_EmitInteger && Opcode <= OPC_EmitIntegerI64)
-        Val = decodeSignRotatedValue(Val);
+      int64_t Val;
+      if (Opcode >= OPC_EmitInteger && Opcode <= OPC_EmitIntegerI64) {
+        Val = GetSignedVBR(MatcherTable, MatcherIndex);
+      } else {
+        Val = MatcherTable[MatcherIndex++];
+        if (Val & 128)
+          Val = GetVBR(Val, MatcherTable, MatcherIndex);
+      }
       RecordedNodes.emplace_back(
           CurDAG->getSignedConstant(Val, SDLoc(NodeToMatch), VT,
                                     /*isTarget=*/true),

llvm/test/TableGen/DAGDefaultOps.td
@@ -83,7 +83,7 @@ def MulIRRPat : Pat<(mul i32:$x, i32:$y), (MulIRR Reg:$x, Reg:$y)>;
 // ADDINT-NEXT: OPC_CheckChild0Integer
 // ADDINT-NEXT: OPC_RecordChild1
 // ADDINT-NEXT: OPC_RecordChild2
-// ADDINT-NEXT: OPC_EmitIntegerI32, 2
+// ADDINT-NEXT: OPC_EmitIntegerI32, 1
 // ADDINT-NEXT: OPC_MorphNodeTo1None, TARGET_VAL(::AddRRI)
 
 // SUB: SwitchOpcode{{.*}}TARGET_VAL(ISD::SUB)

llvm/test/TableGen/dag-isel-regclass-emit-enum.td
@@ -25,13 +25,13 @@ def GPRAbove127 : RegisterClass<"TestTarget", [i32], 32,
 // CHECK-NEXT: OPC_RecordChild0, // #0 = $src
 // CHECK-NEXT: OPC_Scope, 12, /*->18*/ // 2 children in Scope
 // CHECK-NEXT: OPC_CheckChild1Integer, 0,
-// CHECK-NEXT: OPC_EmitIntegerI32, 0|128,2/*256*/,
+// CHECK-NEXT: OPC_EmitIntegerI32, 0|128,1/*128*/, // 128 #1
 // CHECK-NEXT: OPC_MorphNodeTo1None, TARGET_VAL(TargetOpcode::COPY_TO_REGCLASS),
 // CHECK-NEXT:     /*MVT::i32*/7, 2/*#Ops*/, 1, 0,
 def : Pat<(i32 (add i32:$src, (i32 0))),
           (COPY_TO_REGCLASS GPRAbove127, GPR0:$src)>;
 
-// CHECK:      OPC_CheckChild1Integer, 2,
+// CHECK:      OPC_CheckChild1Integer, 1,
 // CHECK-NEXT: OPC_EmitStringIntegerI32, TestNamespace::GPR127RegClassID,
 // CHECK-NEXT: OPC_MorphNodeTo1None, TARGET_VAL(TargetOpcode::COPY_TO_REGCLASS),
 // CHECK-NEXT:     /*MVT::i32*/7, 2/*#Ops*/, 1, 0,

llvm/test/TableGen/dag-isel-subregs.td
@@ -8,7 +8,7 @@ include "reg-with-subregs-common.td"
 def : Pat<(v2i32 (extract_subvector v32i32:$src, (i32 0))),
           (EXTRACT_SUBREG GPR_1024:$src, sub0_sub1)>;
 
-// CHECK: OPC_CheckChild1Integer, 30,
-// CHECK: OPC_EmitIntegerI32, 10|128,2/*266*/,
+// CHECK: OPC_CheckChild1Integer, 15,
+// CHECK: OPC_EmitIntegerI32, 5|128,1/*133*/,
 def : Pat<(v2i32 (extract_subvector v32i32:$src, (i32 15))),
           (EXTRACT_SUBREG GPR_1024:$src, sub30_sub31)>;

llvm/utils/TableGen/DAGISelMatcherEmitter.cpp
@@ -22,6 +22,7 @@
 #include "llvm/ADT/TinyPtrVector.h"
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/Format.h"
+#include "llvm/Support/LEB128.h"
 #include "llvm/Support/SourceMgr.h"
 #include "llvm/TableGen/Error.h"
 #include "llvm/TableGen/Record.h"
@@ -258,13 +259,18 @@ static unsigned EmitVBRValue(uint64_t Val, raw_ostream &OS) {
 /// Emit the specified signed value as a VBR. To improve compression we encode
 /// positive numbers shifted left by 1 and negative numbers negated and shifted
 /// left by 1 with bit 0 set.
-static unsigned EmitSignedVBRValue(uint64_t Val, raw_ostream &OS) {
-  if ((int64_t)Val >= 0)
-    Val = Val << 1;
-  else
-    Val = (-Val << 1) | 1;
+static unsigned EmitSignedVBRValue(int64_t Val, raw_ostream &OS) {
+  uint8_t Buffer[10];
+  unsigned Len = encodeSLEB128(Val, Buffer);
 
-  return EmitVBRValue(Val, OS);
+  for (unsigned i = 0; i != Len - 1; ++i)
+    OS << static_cast<unsigned>(Buffer[i] & 127) << "|128,";
+
+  OS << static_cast<unsigned>(Buffer[Len - 1]);
+  if ((Len > 1 || Val < 0) && !OmitComments)
+    OS << "/*" << Val << "*/";
+  OS << ", ";
+  return Len;
 }
 
 // This is expensive and slow.

commit 1c1310d02231eff75c96203eefe9145e5cabdd20

[SelectionDAG] Use a simpler version of decodeSLEB128 in GetSignedVBR to improve compile time. NFC
This reduces a compile time regression after 7f780046e2 seen here
https://llvm-compile-time-tracker.com/compare.php?from=01e75e2eafcea00c448e289154d8adeecc1c6c3a&to=7f780046e2ae79218dd6abc2d008c1a9eeddedc7&stat=instructions:u

decodeSLEB128 has some code to check for errors that we don't need.
Writing a simpler version seems to recover most of the regression.

llvm/lib/CodeGen/SelectionDAG/SelectionDAGISel.cpp
@@ -96,7 +96,6 @@
 #include "llvm/Support/Debug.h"
 #include "llvm/Support/ErrorHandling.h"
 #include "llvm/Support/KnownBits.h"
-#include "llvm/Support/LEB128.h"
 #include "llvm/Support/Timer.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Target/TargetMachine.h"
@@ -2722,9 +2721,18 @@ GetVBR(uint64_t Val, const uint8_t *MatcherTable, unsigned &Idx) {
 
 LLVM_ATTRIBUTE_ALWAYS_INLINE static int64_t
 GetSignedVBR(const unsigned char *MatcherTable, unsigned &Idx) {
-  unsigned Len;
-  int64_t Val = decodeSLEB128(&MatcherTable[Idx], &Len);
-  Idx += Len;
+  int64_t Val = 0;
+  unsigned Shift = 0;
+  uint64_t NextBits;
+  do {
+    NextBits = MatcherTable[Idx++];
+    Val |= (NextBits & 127) << Shift;
+    Shift += 7;
+  } while (NextBits & 128);
+
+  if (Shift < 64 && (NextBits & 0x40))
+    Val |= UINT64_MAX << Shift;
+
   return Val;
 }
 