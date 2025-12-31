e6206d567b1d2d324d26465547348fd8a59e2f4e
Aiden Grossman <aidengrossman@google.com>
2025-12-31 10:40:46 -0800

    [X86][NewPM] Port X86FixupLEAs to NPM
    This patch ports X86FixupLEAs to the new pass manager and adds some test
    coverage.
    
    Reviewers: paperchalice, arsenm, RKSimon, phoebewang
    
    Pull Request: https://github.com/llvm/llvm-project/pull/173744

--- llvm/lib/Target/X86/X86.h
+++ llvm/lib/Target/X86/X86.h
@@ -67,7 +67,13 @@ FunctionPass *createX86PadShortFunctions();
 /// Return a pass that selectively replaces certain instructions (like add,
 /// sub, inc, dec, some shifts, and some multiplies) by equivalent LEA
 /// instructions, in order to eliminate execution delays in some processors.
-FunctionPass *createX86FixupLEAs();
+class X86FixupLEAsPass : public PassInfoMixin<X86FixupLEAsPass> {
+public:
+  PreservedAnalyses run(MachineFunction &MF,
+                        MachineFunctionAnalysisManager &MFAM);
+};
+
+FunctionPass *createX86FixupLEAsLegacyPass();
 
 /// Return a pass that replaces equivalent slower instructions with faster
 /// ones.
@@ -249,7 +255,7 @@ FunctionPass *createX86SuppressAPXForRelocationPass();
 
 void initializeCompressEVEXPassPass(PassRegistry &);
 void initializeFixupBWInstPassPass(PassRegistry &);
-void initializeFixupLEAPassPass(PassRegistry &);
+void initializeFixupLEAsLegacyPass(PassRegistry &);
 void initializeX86ArgumentStackSlotPassPass(PassRegistry &);
 void initializeX86AsmPrinterPass(PassRegistry &);
 void initializeX86FixupInstTuningPassPass(PassRegistry &);
