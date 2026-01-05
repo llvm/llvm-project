4d1b1b702fb31d8ba11e569fa777ef80f341bb44
moorabbit <moorabbit@proton.me>
2026-01-05 08:43:59 -0500

    Remove inlining tests

--- clang/test/CodeGenCXX/builtin-stackaddress.cpp
+++ clang/test/CodeGenCXX/builtin-stackaddress.cpp
@@ -1,26 +1,22 @@
-// RUN: %clang_cc1 -triple x86_64-linux-gnu -emit-llvm %s -o - | llvm-cxxfilt | FileCheck %s --check-prefixes=COMMON,NO-OPT
-// RUN: %clang_cc1 -triple x86_64-linux-gnu -emit-llvm %s -O3 -o - | llvm-cxxfilt | FileCheck %s --check-prefixes=COMMON,OPT
+// RUN: %clang_cc1 -triple x86_64-linux-gnu -emit-llvm %s -o - | llvm-cxxfilt | FileCheck %s
 
 struct S {
   void *a();
 };
 
-// COMMON-LABEL: @S::a()
-// COMMON: call ptr @llvm.stackaddress.p0()
+// CHECK-LABEL: @S::a()
+// CHECK: call ptr @llvm.stackaddress.p0()
 void *S::a() {
   return __builtin_stack_address();
 }
 
-// COMMON-LABEL: define {{[^@]+}} @two()
+// CHECK-LABEL: define {{[^@]+}} @two()
+// CHECK: call {{[^@]+}} @"two()::$_0::operator()() const"
+//
+// CHECK-LABEL: define {{[^@]+}} @"two()::$_0::operator()() const"
+// CHECK: [[PTR:%.*]] = call ptr @llvm.stackaddress.p0()
+// CHECK: ret ptr [[PTR]]
 void *two() {
-
-  // The compiler is allowed to inline a function calling `__builtin_stack_address`.
-  //
-  // OPT-NOT: define {{[^@]+}} @"two()::$_0::operator()() const"
-  // OPT: call {{[^@]+}} @llvm.stackaddress.p0()
-  //
-  // NO-OPT-DAG: define {{[^@]+}} @"two()::$_0::operator()() const"
-  // NO-OPT-DAG: call {{[^@]+}} @"two()::$_0::operator()() const"
   auto l = []() { return __builtin_stack_address(); };
   return l();
 }
