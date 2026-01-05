53994b99a401db528d71bae333a374e46377819b
DhanashreePetare <dhanashreepetare8125@gmail.com>
2026-01-06 01:03:24 +0530

    Format changed files; add typedef test; update release note

--- clang/test/Sema/typeof-asm-noreturn.c
+++ clang/test/Sema/typeof-asm-noreturn.c
@@ -8,3 +8,12 @@ __typeof(memcmp) memcmp_alias __asm__("memory_compare") __attribute__((noreturn)
 void use(void) {
   (void)memcmp_alias(0, 0, 0);
 }
+
+// Also test a function-type typedef rather than __typeof
+typedef int memcmp_type(const void *, const void *, unsigned long);
+
+memcmp_type memcmp_alias2 __asm__("memory_compare2") __attribute__((noreturn));
+
+void use2(void) {
+  (void)memcmp_alias2(0, 0, 0);
+}
