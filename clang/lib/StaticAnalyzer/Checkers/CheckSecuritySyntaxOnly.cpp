e7112a7e8f4674a1a9252db5947cf5ed7bc0bf88
Endre Fülöp <endre.fulop@sigmatechnology.com>
2026-01-05 16:12:41 +0100

    cache the detection result per TU

--- clang/lib/StaticAnalyzer/Checkers/CheckSecuritySyntaxOnly.cpp
+++ clang/lib/StaticAnalyzer/Checkers/CheckSecuritySyntaxOnly.cpp
@@ -72,6 +72,26 @@ struct ChecksFilter {
   CheckerNameRef checkName_decodeValueOfObjCType;
 };
 
+// Determine whether to report Annex K related checks based on the
+// reporting policy.
+[[nodiscard]] bool shouldReportAnnexKRelated(BugReporter &BR,
+                                             const ChecksFilter &Filter) {
+  const bool IsAnnexKAvailable = analysis::isAnnexKAvailable(
+      &BR.getPreprocessor(), BR.getContext().getLangOpts());
+  const bool IsC11OrLaterStandard = BR.getContext().getLangOpts().C11;
+
+  switch (Filter.ReportMode) {
+  case ReportPolicy::All:
+    return true;
+  case ReportPolicy::Actionable:
+    return IsAnnexKAvailable;
+  case ReportPolicy::C11Only:
+    return IsC11OrLaterStandard;
+  default:
+    llvm_unreachable("Unknown ReportPolicy value");
+  }
+};
+
 class WalkAST : public StmtVisitor<WalkAST> {
   BugReporter &BR;
   AnalysisDeclContext* AC;
@@ -79,14 +99,18 @@ class WalkAST : public StmtVisitor<WalkAST> {
   IdentifierInfo *II_setid[num_setids];
 
   const bool CheckRand;
+
+  // Cache the result of Annex K availability check.
+  const bool ShouldReportAnnexKRelated;
+
   const ChecksFilter &filter;
 
 public:
-  WalkAST(BugReporter &br, AnalysisDeclContext* ac,
-          const ChecksFilter &f)
-  : BR(br), AC(ac), II_setid(),
-    CheckRand(isArc4RandomAvailable(BR.getContext())),
-    filter(f) {}
+  WalkAST(BugReporter &br, AnalysisDeclContext *ac, const ChecksFilter &f)
+      : BR(br), AC(ac), II_setid(),
+        CheckRand(isArc4RandomAvailable(BR.getContext())),
+        ShouldReportAnnexKRelated(shouldReportAnnexKRelated(br, f)), filter(f) {
+  }
 
   // Statement visitor methods.
   void VisitCallExpr(CallExpr *CE);
@@ -757,25 +781,8 @@ void WalkAST::checkCall_strcat(const CallExpr *CE, const FunctionDecl *FD) {
 
 void WalkAST::checkDeprecatedOrUnsafeBufferHandling(const CallExpr *CE,
                                                     const FunctionDecl *FD) {
-  if (!filter.check_DeprecatedOrUnsafeBufferHandling)
-    return;
-
-  const bool ShouldReport = [this] {
-    const bool IsAnnexKAvailable = analysis::isAnnexKAvailable(
-        &BR.getPreprocessor(), BR.getContext().getLangOpts());
-    const bool IsC11OrLaterStandard = BR.getContext().getLangOpts().C11;
-
-    switch (filter.ReportMode) {
-    case ReportPolicy::All:
-      return true;
-    case ReportPolicy::Actionable:
-      return IsAnnexKAvailable;
-    case ReportPolicy::C11Only:
-      return IsC11OrLaterStandard;
-    }
-  }();
-
-  if (!ShouldReport)
+  if (!filter.check_DeprecatedOrUnsafeBufferHandling ||
+      !ShouldReportAnnexKRelated)
     return;
 
   // Issue a warning. ArgIndex == -1: Deprecated but not unsafe (has size
@@ -1153,7 +1160,7 @@ void ento::registerDeprecatedOrUnsafeBufferHandling(CheckerManager &Mgr) {
   else
     Mgr.reportInvalidCheckerOptionValue(
         Checker, "ReportMode",
-        "ReportMode should be one of the folliwing values: \"all\", "
+        "ReportMode should be one of the following values: \"all\", "
         "\"actionable\" or \"c11-only\" (the default)");
 }
 
