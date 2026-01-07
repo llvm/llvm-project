49e78818118d98e022b35e8ac305f2075a242600
Michael Buch <michaelbuch12@gmail.com>
2026-01-07 17:48:07 +0000

    fixup! add to message

--- cross-project-tests/lit.cfg.py
+++ cross-project-tests/lit.cfg.py
@@ -340,7 +340,7 @@ if lldb_version_string:
         config.available_features.add("lldb-formatters-compatibility")
     else:
         print(
-            f'Marking LLDB LLVM data-formatter tests as unsupported: using version {lldb_version_string} whereas a version >= 1900.0 is required',
+            f'Marking some LLDB LLVM data-formatter tests as unsupported: using version {lldb_version_string} whereas a version >= 1900.0 is required',
             file=sys.stderr,
         )
 
