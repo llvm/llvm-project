f38e54ee83a8230be8a7f5514b3d112a74407f3d
Matt Arsenault <Matthew.Arsenault@amd.com>
2026-01-07 19:35:04 +0100

    InstCombine: Handle minimum/maximum in SimplifyDemandedFPClass (#173897)

--- llvm/lib/Support/KnownFPClass.cpp
+++ llvm/lib/Support/KnownFPClass.cpp
@@ -91,6 +91,98 @@ void KnownFPClass::propagateDenormal(const KnownFPClass &Src,
   }
 }
 
+KnownFPClass KnownFPClass::minMaxLike(const KnownFPClass &LHS_,
+                                      const KnownFPClass &RHS_, MinMaxKind Kind,
+                                      DenormalMode Mode) {
+  KnownFPClass KnownLHS = LHS_;
+  KnownFPClass KnownRHS = RHS_;
+
+  bool NeverNaN = KnownLHS.isKnownNeverNaN() || KnownRHS.isKnownNeverNaN();
+  KnownFPClass Known = KnownLHS | KnownRHS;
+
+  // If either operand is not NaN, the result is not NaN.
+  if (NeverNaN &&
+      (Kind == MinMaxKind::minnum || Kind == MinMaxKind::maxnum ||
+       Kind == MinMaxKind::minimumnum || Kind == MinMaxKind::maximumnum))
+    Known.knownNot(fcNan);
+
+  if (Kind == MinMaxKind::maxnum || Kind == MinMaxKind::maximumnum) {
+    // If at least one operand is known to be positive, the result must be
+    // positive.
+    if ((KnownLHS.cannotBeOrderedLessThanZero() &&
+         KnownLHS.isKnownNeverNaN()) ||
+        (KnownRHS.cannotBeOrderedLessThanZero() && KnownRHS.isKnownNeverNaN()))
+      Known.knownNot(KnownFPClass::OrderedLessThanZeroMask);
+  } else if (Kind == MinMaxKind::maximum) {
+    // If at least one operand is known to be positive, the result must be
+    // positive.
+    if (KnownLHS.cannotBeOrderedLessThanZero() ||
+        KnownRHS.cannotBeOrderedLessThanZero())
+      Known.knownNot(KnownFPClass::OrderedLessThanZeroMask);
+  } else if (Kind == MinMaxKind::minnum || Kind == MinMaxKind::minimumnum) {
+    // If at least one operand is known to be negative, the result must be
+    // negative.
+    if ((KnownLHS.cannotBeOrderedGreaterThanZero() &&
+         KnownLHS.isKnownNeverNaN()) ||
+        (KnownRHS.cannotBeOrderedGreaterThanZero() &&
+         KnownRHS.isKnownNeverNaN()))
+      Known.knownNot(KnownFPClass::OrderedGreaterThanZeroMask);
+  } else if (Kind == MinMaxKind::minimum) {
+    // If at least one operand is known to be negative, the result must be
+    // negative.
+    if (KnownLHS.cannotBeOrderedGreaterThanZero() ||
+        KnownRHS.cannotBeOrderedGreaterThanZero())
+      Known.knownNot(KnownFPClass::OrderedGreaterThanZeroMask);
+  } else
+    llvm_unreachable("unhandled intrinsic");
+
+  // Fixup zero handling if denormals could be returned as a zero.
+  //
+  // As there's no spec for denormal flushing, be conservative with the
+  // treatment of denormals that could be flushed to zero. For older
+  // subtargets on AMDGPU the min/max instructions would not flush the
+  // output and return the original value.
+  //
+  if ((Known.KnownFPClasses & fcZero) != fcNone &&
+      !Known.isKnownNeverSubnormal()) {
+    if (Mode != DenormalMode::getIEEE())
+      Known.KnownFPClasses |= fcZero;
+  }
+
+  if (Known.isKnownNeverNaN()) {
+    if (KnownLHS.SignBit && KnownRHS.SignBit &&
+        *KnownLHS.SignBit == *KnownRHS.SignBit) {
+      if (*KnownLHS.SignBit)
+        Known.signBitMustBeOne();
+      else
+        Known.signBitMustBeZero();
+    } else if ((Kind == MinMaxKind::maximum || Kind == MinMaxKind::minimum ||
+                Kind == MinMaxKind::maximumnum ||
+                Kind == MinMaxKind::minimumnum) ||
+               // FIXME: Should be using logical zero versions
+               ((KnownLHS.isKnownNeverNegZero() ||
+                 KnownRHS.isKnownNeverPosZero()) &&
+                (KnownLHS.isKnownNeverPosZero() ||
+                 KnownRHS.isKnownNeverNegZero()))) {
+      // Don't take sign bit from NaN operands.
+      if (!KnownLHS.isKnownNeverNaN())
+        KnownLHS.SignBit = std::nullopt;
+      if (!KnownRHS.isKnownNeverNaN())
+        KnownRHS.SignBit = std::nullopt;
+      if ((Kind == MinMaxKind::maximum || Kind == MinMaxKind::maximumnum ||
+           Kind == MinMaxKind::maxnum) &&
+          (KnownLHS.SignBit == false || KnownRHS.SignBit == false))
+        Known.signBitMustBeZero();
+      else if ((Kind == MinMaxKind::minimum || Kind == MinMaxKind::minimumnum ||
+                Kind == MinMaxKind::minnum) &&
+               (KnownLHS.SignBit == true || KnownRHS.SignBit == true))
+        Known.signBitMustBeOne();
+    }
+  }
+
+  return Known;
+}
+
 KnownFPClass KnownFPClass::canonicalize(const KnownFPClass &KnownSrc,
                                         DenormalMode DenormMode) {
   KnownFPClass Known;
