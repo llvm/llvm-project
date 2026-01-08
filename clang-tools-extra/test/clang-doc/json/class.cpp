5026c7f80d901fcc68a8e2ac9cf9e3b6251421f9
Erick Velez <erickvelez7@gmail.com>
2026-01-07 22:11:56 -0800

    [clang-doc] Add protected members to class template (#174883)
    There were already tags for protected members in the Mustache template,
    but didn't use the proper tags for the newer JSON scheme.

--- clang-tools-extra/test/clang-doc/json/class.cpp
+++ clang-tools-extra/test/clang-doc/json/class.cpp
@@ -170,6 +170,7 @@ private:
 // CHECK-NEXT:    "HasEnums": true,
 // CHECK-NEXT:    "HasFriends": true,
 // CHECK-NEXT:    "HasPrivateMembers": true,
+// CHECK-NEXT:    "HasProtectedMembers": true,
 // CHECK-NEXT:    "HasPublicFunctions": true,
 // CHECK-NEXT:    "HasPublicMembers": true,
 // CHECK-NEXT:    "HasRecords": true,
@@ -320,6 +321,14 @@ private:
 // HTML-NEXT:         </li>
 // HTML-NEXT:     </ul>
 // HTML-NEXT: </details>
+// HTML:      <section id="ProtectedMembers" class="section-container">
+// HTML-NEXT:     <h2>Protected Members</h2>
+// HTML-NEXT:     <div>
+// HTML-NEXT:         <div id="ProtectedField" class="delimiter-container">
+// HTML-NEXT:             <pre><code class="language-cpp code-clang-doc" >int ProtectedField</code></pre>
+// HTML-NEXT:         </div>
+// HTML-NEXT:     </div>
+// HTML-NEXT: </section>
 // HTML:      <section id="Classes" class="section-container">
 // HTML-NEXT:     <h2>Inner Classes</h2>
 // HTML-NEXT:     <ul class="class-container">
