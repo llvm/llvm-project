f92d9d9b7907e201ba380b26ae27a13892b419f5
Elvis Wang <elvis.wang@sifive.com>
2026-01-07 16:15:55 -0800

    Use VPI::getNumOperandsForOpcode() in VPPatternMatch.

--- llvm/lib/Transforms/Vectorize/VPlanTransforms.cpp
+++ llvm/lib/Transforms/Vectorize/VPlanTransforms.cpp
@@ -3894,8 +3894,7 @@ void VPlanTransforms::convertToConcreteRecipes(VPlan &Plan) {
 
       // Lower ExtractLane with single source to ExtractElement.
       VPValue *LaneToExtract, *Source;
-      if (R.getNumOperands() == 2 &&
-          match(&R,
+      if (match(&R,
                 m_ExtractLane(m_VPValue(LaneToExtract), m_VPValue(Source)))) {
         auto *ExtractLane = cast<VPInstruction>(&R);
         auto *ExtractElement = Builder.createNaryOp(Instruction::ExtractElement,
