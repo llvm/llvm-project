b90b1c0de8a3c1124ba9a592c4dd20fc8c6c0375
Adrian Prantl <aprantl@apple.com>
2026-01-08 10:14:13 -0800

    [LLDB] Improve testcoverage for typealiases in the expression evaluator
    and document several cases that aren't working correctly.
    
    (cherry picked from commit b067de0d4920adf777553d52bd6b47dfa13230f9)

--- lldb/test/API/lang/swift/expression/typealias/main.swift
+++ lldb/test/API/lang/swift/expression/typealias/main.swift
@@ -1,7 +1,35 @@
-func main() {
-  typealias Pair<T> = (T, T)
-  let patatino : Pair<Int> = (1, 2)
-  print(patatino) //%self.expect('expr -d run -- patatino', substrs=['Pair<Int>'])
+struct A {}
+
+private protocol MyProtocolBase<T> {
+    typealias MyAlias = A
+    associatedtype T
 }
 
-main()
+private protocol MyProtocol : MyProtocolBase<Int> {
+}
+
+extension MyProtocol {
+    func f1() {
+        typealias Pair<T> = (T, T)
+        let local : Pair<Int> = (1, 2)
+        print("break here")
+    }
+
+    func f2(_ associated : MyAlias) {
+        print("break here")
+    }
+}
+
+struct B : MyProtocol {
+}
+
+func f() {
+    typealias Pair<T> = (T, T)
+    let local : Pair<Int> = (1, 2)
+    print("break here")
+}
+
+f()
+let b = B()
+b.f1()
+b.f2(A())
