e9fc2497927d131dd3309edf278bf2656eb42a6f
Grigory Pastukhov <gpastukhov@meta.com>
2026-01-08 18:53:24 -0800

    Switch clang flatten to full flattening

--- clang/test/CodeGen/flatten.c
+++ clang/test/CodeGen/flatten.c
@@ -1,19 +1,8 @@
-// RUN: %clang_cc1 -triple=x86_64-linux-gnu %s -emit-llvm -o - | FileCheck %s
+// RUN: %clang_cc1 -triple=x86_64-linux-gnu -disable-llvm-passes %s -emit-llvm -o - | FileCheck %s
 
-void f(void) {}
-
-__attribute__((noinline)) void ni(void) {}
-
+// CHECK: define{{.*}} void @g() [[FLATTEN_ATTR:#[0-9]+]]
 __attribute__((flatten))
-// CHECK: define{{.*}} void @g()
 void g(void) {
-  // CHECK-NOT: call {{.*}} @f
-  f();
-  // CHECK: call {{.*}} @ni
-  ni();
 }
 
-void h(void) {
-  // CHECK: call {{.*}} @f
-  f();
-}
+// CHECK: attributes [[FLATTEN_ATTR]] = {{{.*}}flatten{{.*}}}
