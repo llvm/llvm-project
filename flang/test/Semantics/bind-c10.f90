8dbf7ed12c2a0f74e4d761708454f23dc3459b69
Igor S. Gerasimov <foxtranigor@gmail.com>
2026-01-09 08:21:44 +0100

    Add note about examples (and one more example)

--- flang/test/Semantics/bind-c10.f90
+++ flang/test/Semantics/bind-c10.f90
@@ -4,9 +4,15 @@ end
 !ERROR: Symbol has a BIND(C) name containing non-visible ASCII character(s)
 subroutine cr() bind(C,name=achar(13))
 end
+!! depending on used assembler it can be threated as error or not
 subroutine beast() bind(C,name="666")
 end
+!! depending on used assembler it can be threated as error or not
 subroutine llvm() bind(C,name=".L1")
 end
-subroutine ld() bind(C,name=".text")
+!! depending on used assembler it can be threated as error or not
+subroutine as() bind(C,name=".text")
+end
+!! depending on used assembler it can be threated as error or not
+subroutine printable_ascii() bind(C,name="! ""#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~'")
 end
