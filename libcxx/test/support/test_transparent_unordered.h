08a3e50d699b32feff814d6b4ac234d785601155
rsaddatimov <rafa.saddatimov@gmail.com>
2026-01-09 19:02:05 +0300

    Drop reformatting

--- libcxx/test/support/test_transparent_unordered.h
+++ libcxx/test/support/test_transparent_unordered.h
@@ -46,7 +46,7 @@ struct transparent_equal_final final : std::equal_to<> {};
 
 template <typename T>
 struct SearchedType {
-  explicit SearchedType(T value, int* counter) : value_(value), conversions_(counter) {}
+  explicit SearchedType(T value, int *counter) : value_(value), conversions_(counter) { }
 
   // Whenever a conversion is performed, increment the counter to keep track
   // of conversions.
@@ -55,40 +55,48 @@ struct SearchedType {
     return StoredType<T>{value_};
   }
 
-  T get_value() const { return value_; }
+  int get_value() const {
+    return value_;
+  }
 
 private:
   T value_;
-  int* conversions_;
+  int *conversions_;
 };
 
 template <typename T>
 struct StoredType {
   StoredType() = default;
-  StoredType(T value) : value_(value) {}
+  StoredType(T value) : value_(value) { }
 
-  friend bool operator==(StoredType const& lhs, StoredType const& rhs) { return lhs.value_ == rhs.value_; }
+  friend bool operator==(StoredType const& lhs, StoredType const& rhs) {
+    return lhs.value_ == rhs.value_;
+  }
 
   // If we're being passed a SearchedType<T> object, avoid the conversion
   // to T. This allows testing that the transparent operations are correctly
   // forwarding the SearchedType all the way to this comparison by checking
   // that we didn't have a conversion when we search for a SearchedType<T>
   // in a container full of StoredType<T>.
-  friend bool operator==(StoredType const& lhs, SearchedType<T> const& rhs) { return lhs.value_ == rhs.get_value(); }
+  friend bool operator==(StoredType const& lhs, SearchedType<T> const& rhs) {
+    return lhs.value_ == rhs.get_value();
+  }
 
-  T get_value() const { return value_; }
+  int get_value() const {
+    return value_;
+  }
 
 private:
   T value_;
 };
 
-template <template <class...> class UnorderedSet, class Hash, class Equal>
+template<template<class...> class UnorderedSet, class Hash, class Equal>
 using unord_set_type = UnorderedSet<StoredType<int>, Hash, Equal>;
 
-template <template <class...> class UnorderedMap, class Hash, class Equal>
+template<template<class...> class UnorderedMap, class Hash, class Equal>
 using unord_map_type = UnorderedMap<StoredType<int>, int, Hash, Equal>;
 
-template <class Container>
+template<class Container>
 void test_transparent_find(Container c) {
   int conversions = 0;
   assert(c.find(SearchedType<int>(1, &conversions)) != c.end());
@@ -97,7 +105,7 @@ void test_transparent_find(Container c) {
   assert(conversions == 0);
 }
 
-template <class Container>
+template<class Container>
 void test_non_transparent_find(Container c) {
   int conversions = 0;
   assert(c.find(SearchedType<int>(1, &conversions)) != c.end());
@@ -108,7 +116,7 @@ void test_non_transparent_find(Container c) {
   assert(conversions == 3);
 }
 
-template <class Container>
+template<class Container>
 void test_transparent_count(Container c) {
   int conversions = 0;
   assert(c.count(SearchedType<int>(1, &conversions)) > 0);
@@ -117,7 +125,7 @@ void test_transparent_count(Container c) {
   assert(conversions == 0);
 }
 
-template <class Container>
+template<class Container>
 void test_non_transparent_count(Container c) {
   int conversions = 0;
   assert(c.count(SearchedType<int>(1, &conversions)) > 0);
@@ -128,7 +136,7 @@ void test_non_transparent_count(Container c) {
   assert(conversions == 3);
 }
 
-template <class Container>
+template<class Container>
 void test_transparent_contains(Container c) {
   int conversions = 0;
   assert(c.contains(SearchedType<int>(1, &conversions)));
@@ -137,7 +145,7 @@ void test_transparent_contains(Container c) {
   assert(conversions == 0);
 }
 
-template <class Container>
+template<class Container>
 void test_non_transparent_contains(Container c) {
   int conversions = 0;
   assert(c.contains(SearchedType<int>(1, &conversions)));
@@ -148,10 +156,10 @@ void test_non_transparent_contains(Container c) {
   assert(conversions == 3);
 }
 
-template <class Container>
+template<class Container>
 void test_transparent_equal_range(Container c) {
   int conversions = 0;
-  auto iters      = c.equal_range(SearchedType<int>(1, &conversions));
+  auto iters = c.equal_range(SearchedType<int>(1, &conversions));
   assert(std::distance(iters.first, iters.second) > 0);
   iters = c.equal_range(SearchedType<int>(2, &conversions));
   assert(std::distance(iters.first, iters.second) > 0);
@@ -160,10 +168,10 @@ void test_transparent_equal_range(Container c) {
   assert(conversions == 0);
 }
 
-template <class Container>
+template<class Container>
 void test_non_transparent_equal_range(Container c) {
   int conversions = 0;
-  auto iters      = c.equal_range(SearchedType<int>(1, &conversions));
+  auto iters = c.equal_range(SearchedType<int>(1, &conversions));
   assert(std::distance(iters.first, iters.second) > 0);
   assert(conversions == 1);
   iters = c.equal_range(SearchedType<int>(2, &conversions));
