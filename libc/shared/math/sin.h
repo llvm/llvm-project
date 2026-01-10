43b8bc4b6f6f67f5f158daf910007f9ed223bbf4
lntue <lntue@google.com>
2026-01-09 23:45:24 -0500

    [libc][math] Refactor sin implementation to header-only in src/__support/math folder. (#175200)
    Part of #147386
    
    in preparation for:
    https://discourse.llvm.org/t/rfc-make-clang-builtin-math-functions-constexpr-with-llvm-libc-to-support-c-23-constexpr-math-functions/86450

--- libc/shared/math/sin.h
+++ libc/shared/math/sin.h
@@ -1,4 +1,4 @@
-//===-- Double-precision sin function -------------------------------------===//
+//===-- Shared sin function -------------------------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,11 +6,18 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "src/math/sin.h"
+#ifndef LLVM_LIBC_SHARED_MATH_SIN_H
+#define LLVM_LIBC_SHARED_MATH_SIN_H
+
+#include "shared/libc_common.h"
 #include "src/__support/math/sin.h"
 
 namespace LIBC_NAMESPACE_DECL {
+namespace shared {
 
-LLVM_LIBC_FUNCTION(double, sin, (double x)) { return math::sin(x); }
+using math::sin;
 
+} // namespace shared
 } // namespace LIBC_NAMESPACE_DECL
+
+#endif // LLVM_LIBC_SHARED_MATH_SIN_H
