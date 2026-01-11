1f7bf339c2741fe7202add40652b4911917b8e49
CarvedCoder <CarvedCoder@users.noreply.github.com>
2026-01-11 06:20:14 +0530

    added changes for cmake dependencies and log.h namespace depth and gaurd macros

--- libc/src/math/generic/log.cpp
+++ libc/src/math/generic/log.cpp
@@ -1,11 +1,3 @@
-//===-- Double-precision log(x) function ----------------------------------===//
-//
-// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-//
-//===----------------------------------------------------------------------===//
-
 #include "src/math/log.h"
 #include "src/__support/math/log.h"
 
