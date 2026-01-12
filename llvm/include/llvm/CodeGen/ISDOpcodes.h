70c889fd43c682aa4e0f37cbf45523cee414f3db
moorabbit <moorabbit@proton.me>
2026-01-12 04:01:57 -0500

    [Clang] Add `__builtin_stack_address` (#148281)
    Add support for `__builtin_stack_address` builtin. The semantics match
    those of GCC's builtin with the same name.
    
    `__builtin_stack_address` returns the starting address of the stack
    region that may be used by called functions. It may or may not include
    the space used for on-stack arguments passed to a callee (See [GCC
    Bug/121013](https://gcc.gnu.org/bugzilla/show_bug.cgi?id=121013)).
    
    Fixes #82632.

--- llvm/include/llvm/CodeGen/ISDOpcodes.h
+++ llvm/include/llvm/CodeGen/ISDOpcodes.h
@@ -121,6 +121,11 @@ enum NodeType {
   /// function calling this intrinsic.
   SPONENTRY,
 
+  /// STACKADDRESS - Represents the llvm.stackaddress intrinsic. Takes no
+  /// argument and returns the starting address of the stack region that may be
+  /// used by called functions.
+  STACKADDRESS,
+
   /// LOCAL_RECOVER - Represents the llvm.localrecover intrinsic.
   /// Materializes the offset from the local object pointer of another
   /// function to a particular local object passed to llvm.localescape. The
