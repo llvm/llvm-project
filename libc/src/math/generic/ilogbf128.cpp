8e2edfe7f1a5788ddee7a896baf35963d171634e
Ramshankar <87872158+Ramshankar07@users.noreply.github.com>
2026-01-12 00:07:08 -0500

    Update libc/src/math/generic/ilogbf128.cpp
    Co-authored-by: Muhammad Bassiouni <60100307+bassiounix@users.noreply.github.com>

--- libc/src/math/generic/ilogbf128.cpp
+++ libc/src/math/generic/ilogbf128.cpp
@@ -10,7 +10,7 @@
 #include "src/__support/FPUtil/ManipulationFunctions.h"
 #include "src/__support/common.h"
 #include "src/__support/macros/config.h"
-#include "lib/shared/math/ilogbf128.h"
+#include "src/__support/math/ilogbf128.h"
 namespace LIBC_NAMESPACE_DECL {
 
 LLVM_LIBC_FUNCTION(int, ilogbf128, (float128 x)) {
