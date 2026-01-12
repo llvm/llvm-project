707b1e02ba73f5d40b3d6a89e9ba13d484f2b0be
Fabrice de Gans <fabrice@thebrowser.company>
2026-01-12 16:28:00 +0100

    Address review comments

--- llvm/utils/git/ids-check-helper.py
+++ llvm/utils/git/ids-check-helper.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python3
 #
-# ====- ids-check-helper, runs ids-check from the ci or in a hook --*- python -*--==#
-#
 # Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-#
-# ==--------------------------------------------------------------------------------------==#
 
 import argparse
 import os
