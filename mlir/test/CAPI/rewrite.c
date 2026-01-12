8af049dff7dd2c24b3449d972f4cc19bec180ba7
alexandre <alexandre.eber07@gmail.com>
2026-01-12 00:20:22 -0500

    [mlir][capi] Fix non-portable int64_t printf formatting in greedy rewrite config test
    The C-API test prints int64_t values using %ld, which is not portable on LLP64
    platforms (Windows or some MacOS configurations) where long is 32-bit. Use <inttypes.h> and PRId64 to
    print int64_t portably.
    
    Test: ninja check-mlir

--- mlir/test/CAPI/rewrite.c
+++ mlir/test/CAPI/rewrite.c
@@ -14,7 +14,9 @@
 #include "mlir-c/IR.h"
 
 #include <assert.h>
+#include <inttypes.h>
 #include <stdio.h>
+
 
 MlirOperation createOperationWithName(MlirContext ctx, const char *name) {
   MlirStringRef nameRef = mlirStringRefCreateFromCString(name);
@@ -554,10 +556,10 @@ void testGreedyRewriteDriverConfig(MlirContext ctx) {
 
   // Test all configuration getters and verify values
   // CHECK: MaxIterations: 5
-  fprintf(stderr, "MaxIterations: %ld\n",
+  fprintf(stderr, "MaxIterations: %" PRId64 "\n",
           mlirGreedyRewriteDriverConfigGetMaxIterations(config));
   // CHECK: MaxNumRewrites: 100
-  fprintf(stderr, "MaxNumRewrites: %ld\n",
+  fprintf(stderr, "MaxNumRewrites: %" PRId64 "\n",
           mlirGreedyRewriteDriverConfigGetMaxNumRewrites(config));
   // CHECK: UseTopDownTraversal: 1
   fprintf(stderr, "UseTopDownTraversal: %d\n",
