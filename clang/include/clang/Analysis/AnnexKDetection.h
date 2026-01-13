82739181fffd2ceaf579d9a26524bd2ca866013a
Endre Fülöp <endre.fulop@sigmatechnology.com>
2026-01-14 00:06:46 +0100

    fix comment headers

--- clang/include/clang/Analysis/AnnexKDetection.h
+++ clang/include/clang/Analysis/AnnexKDetection.h
@@ -1,4 +1,4 @@
-//==- AnnexKDetection.h - Annex K availability detection --------*- C++ -*-==//
+//==- AnnexKDetection.h - Annex K availability detection ---------*- C++ -*-==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
--- clang/lib/Analysis/AnnexKDetection.cpp
+++ clang/lib/Analysis/AnnexKDetection.cpp
@@ -1,5 +1,4 @@
-//==- AnnexKDetection.cpp - Annex K availability detection --------*- C++
-//-*-==//
+//==- AnnexKDetection.cpp - Annex K availability detection -------*- C++ -*-==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
