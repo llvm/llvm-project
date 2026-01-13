ec74c9ed8349c98012924c1790de08a02d8aaf97
urvi-rav <urvi.rav@hpe.com>
2026-01-13 03:15:59 -0600

    updated lit-testcase

--- flang/test/Parser/type-keyword-space.f90
+++ flang/test/Parser/type-keyword-space.f90
@@ -1,10 +1,5 @@
 ! RUN: not %flang_fc1 -fsyntax-only %s 2>&1 | FileCheck %s
 
-! Test that a blank is required between type keywords and names in free-form
-! source per Fortran standard 6.3.2.2: "A blank shall be used to separate
-! names, constants, or labels from adjacent keywords, names, constants, or
-! labels."
-
 ! CHECK: error: Unexpected syntax while parsing the statement-function statement
 ! CHECK: reala
 program test_real
