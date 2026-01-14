bd2fef83958298ab7edf6675e1b63f0da9c2926a
Dasha Buka <dvbuka@proton.me>
2026-01-13 23:31:43 -0800

    Remove unnecessary header

--- libc/src/__support/math/ilogb.h
+++ libc/src/__support/math/ilogb.h
@@ -12,7 +12,6 @@
 #include "src/__support/FPUtil/ManipulationFunctions.h"
 #include "src/__support/common.h"
 #include "src/__support/macros/config.h"
-#include "src/math/ilogb.h"
 
 namespace LIBC_NAMESPACE_DECL {
 
