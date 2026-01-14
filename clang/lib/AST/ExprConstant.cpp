1f9be2be91b7b469b8da7752385532229f95ee47
Paul Kirth <paulkirth@google.com>
2026-01-13 17:44:39 -0800

    Use setExprNeedsCleanups in BuildCXXNew and avoid breaking c++98
    This approach is much cleaner, but broke checkICE reporting in c++98.
    Stepping through a debugger shows that this happend because the
    static_assert test didn not recognize ExprWithCleanups as transparent to
    constant evaluation. To addresse this, we update CheckICE to recurse
    into the sub-expression, and keep the old behavior.

--- clang/lib/AST/ExprConstant.cpp
+++ clang/lib/AST/ExprConstant.cpp
@@ -21384,7 +21384,6 @@ static ICEDiag CheckICE(const Expr* E, const ASTContext &Ctx) {
   case Expr::CXXInheritedCtorInitExprClass:
   case Expr::CXXStdInitializerListExprClass:
   case Expr::CXXBindTemporaryExprClass:
-  case Expr::ExprWithCleanupsClass:
   case Expr::CXXTemporaryObjectExprClass:
   case Expr::CXXUnresolvedConstructExprClass:
   case Expr::CXXDependentScopeMemberExprClass:
@@ -21442,6 +21441,10 @@ static ICEDiag CheckICE(const Expr* E, const ASTContext &Ctx) {
     }
     return ICEDiag(IK_NotICE, E->getBeginLoc());
   }
+
+  case Expr::ExprWithCleanupsClass:
+    // ExprWithCleanups is just a wrapper, so check the wrapped expression.
+    return CheckICE(cast<ExprWithCleanups>(E)->getSubExpr(), Ctx);
 
   case Expr::InitListExprClass: {
     // C++03 [dcl.init]p13: If T is a scalar type, then a declaration of the
