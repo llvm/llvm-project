4abaad43bdb1d0eac8e073470441902d5ff42333
cchen <chichun.chen@hpe.com>
2026-01-14 14:49:33 -0600

    Overhaul flang lower test: declare-simd.f90
    - remove redundancy with OMP60 macro
        - no duplicate check lines
            -  `omp declare simd` should generate same mlir as `omp
               delcare_simd`
        - no duplicate fortran subroutines for declare_simd syntax
    - make the check line of omp.declare_simd more robust

--- flang/test/Lower/OpenMP/declare-simd.f90
+++ flang/test/Lower/OpenMP/declare-simd.f90
@@ -5,10 +5,14 @@
 
 ! RUN: %flang_fc1 -emit-hlfir -fopenmp -fopenmp-version=52 %s -o - | FileCheck %s
 ! RUN: %flang_fc1 -emit-hlfir -fopenmp -fopenmp-version=60 %s -o - | FileCheck %s
-! RUN: %flang_fc1 -emit-hlfir -fopenmp -fopenmp-version=60 -cpp -DOMP_60 %s -o - | FileCheck %s --check-prefix=CHECK60
+! RUN: %flang_fc1 -emit-hlfir -fopenmp -fopenmp-version=60 -cpp -DOMP_60 %s -o - | FileCheck %s
 
 subroutine declare_simd_no_clause()
-  !$omp declare simd
+#ifdef OMP_60
+!$omp declare_simd
+#else
+!$omp declare simd
+#endif
 end subroutine declare_simd_no_clause
 
 ! CHECK-LABEL: func.func @_QPdeclare_simd_no_clause()
@@ -16,7 +20,11 @@ end subroutine declare_simd_no_clause
 ! CHECK: return
 
 subroutine declare_simd_aligned(x, y, n, i)
-  !$omp declare simd aligned(x, y : 64)
+#ifdef OMP_60
+!$omp declare_simd aligned(x, y : 64)
+#else
+!$omp declare simd aligned(x, y : 64)
+#endif
   real(8), pointer, intent(inout) :: x(:)
   real(8), pointer, intent(in)    :: y(:)
   integer, intent(in) :: n, i
@@ -29,14 +37,16 @@ end subroutine declare_simd_aligned
 ! CHECK: %[[N_A:.*]]:2 = hlfir.declare %{{.*}} dummy_scope %[[SCOPE_A]] arg 3 {{.*}} : (!fir.ref<i32>, !fir.dscope) -> (!fir.ref<i32>, !fir.ref<i32>)
 ! CHECK: %[[X_A:.*]]:2 = hlfir.declare %{{.*}} dummy_scope %[[SCOPE_A]] arg 1 {{.*pointer.*}} : (!fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>>, !fir.dscope) -> (!fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>>, !fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>>)
 ! CHECK: %[[Y_A:.*]]:2 = hlfir.declare %{{.*}} dummy_scope %[[SCOPE_A]] arg 2 {{.*pointer.*}} : (!fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>>, !fir.dscope) -> (!fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>>, !fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>>)
-! CHECK: omp.declare_simd
-! CHECK-SAME: aligned(%[[X_A]]#0 : !fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>> -> 64 : i64,
-! CHECK-SAME:         %[[Y_A]]#0 : !fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>> -> 64 : i64)
-! CHECK-NOT: {{[[:space:]]*(linear|simdlen)\(}}
+! CHECK: omp.declare_simd aligned(%[[X_A]]#0 : !fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>> -> 64 : i64,
+! CHECK-SAME:                    %[[Y_A]]#0 : !fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>> -> 64 : i64){{$}}
 ! CHECK: return
 
 subroutine declare_simd_linear(x, y, n, i)
-  !$omp declare simd linear(i)
+#ifdef OMP_60
+!$omp declare_simd linear(i)
+#else
+!$omp declare simd linear(i)
+#endif
   real(8), pointer, intent(inout) :: x(:)
   real(8), pointer, intent(in)    :: y(:)
   integer, intent(in) :: n, i
@@ -44,44 +54,38 @@ subroutine declare_simd_linear(x, y, n, i)
 end subroutine  declare_simd_linear
 
 ! CHECK-LABEL: func.func @_QPdeclare_simd_linear(
-! CHECK: %[[SCOPE_L:.*]] = fir.dummy_scope : !fir.dscope
-! CHECK: %[[I_L:.*]]:2 = hlfir.declare %{{.*}} dummy_scope %[[SCOPE_L]] arg 4 {{.*}} : (!fir.ref<i32>, !fir.dscope) -> (!fir.ref<i32>, !fir.ref<i32>)
-! CHECK: %[[N_L:.*]]:2 = hlfir.declare %{{.*}} dummy_scope %[[SCOPE_L]] arg 3 {{.*}} : (!fir.ref<i32>, !fir.dscope) -> (!fir.ref<i32>, !fir.ref<i32>)
-! CHECK: %[[X_L:.*]]:2 = hlfir.declare %{{.*}} dummy_scope %[[SCOPE_L]] arg 1 {{.*pointer.*}} : (!fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>>, !fir.dscope) -> (!fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>>, !fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>>)
-! CHECK: %[[Y_L:.*]]:2 = hlfir.declare %{{.*}} dummy_scope %[[SCOPE_L]] arg 2 {{.*pointer.*}} : (!fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>>, !fir.dscope) -> (!fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>>, !fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>>)
-! CHECK: %[[C1_L:.*]] = arith.constant 1 : i32
-! CHECK: omp.declare_simd
-! CHECK-NOT: {{[[:space:]]*(aligned|simdlen)\(}}
+! CHECK: %[[SCOPE:.*]] = fir.dummy_scope : !fir.dscope
+! CHECK: %[[I:.*]]:2 = hlfir.declare %{{.*}} dummy_scope %[[SCOPE]] arg 4 {{.*}} : (!fir.ref<i32>, !fir.dscope) -> (!fir.ref<i32>, !fir.ref<i32>)
+! CHECK: %[[C1:.*]] = arith.constant 1 : i32
+! CHECK: omp.declare_simd linear(%[[I]]#0 = %[[C1]] : !fir.ref<i32>) {linear_var_types = [i32]}{{$}}
 ! CHECK: return
 
 subroutine declare_simd_simdlen(x, y, n, i)
-  !$omp declare simd simdlen(8)
-  real(8), pointer, intent(inout) :: x(:)
-  real(8), pointer, intent(in)    :: y(:)
-  integer, intent(in) :: n, i
-  if (i <= n) x(i) = x(i) + y(i)
+#ifdef OMP_60
+!$omp declare_simd simdlen(8)
+#else
+!$omp declare simd simdlen(8)
+#endif
 end subroutine declare_simd_simdlen
 
 ! CHECK-LABEL: func.func @_QPdeclare_simd_simdlen(
 ! CHECK: %[[SCOPE_S:.*]] = fir.dummy_scope : !fir.dscope
-! CHECK: %[[I_S:.*]]:2 = hlfir.declare %{{.*}} dummy_scope %[[SCOPE_S]] arg 4 {{.*}} : (!fir.ref<i32>, !fir.dscope) -> (!fir.ref<i32>, !fir.ref<i32>)
-! CHECK: %[[N_S:.*]]:2 = hlfir.declare %{{.*}} dummy_scope %[[SCOPE_S]] arg 3 {{.*}} : (!fir.ref<i32>, !fir.dscope) -> (!fir.ref<i32>, !fir.ref<i32>)
-! CHECK: %[[X_S:.*]]:2 = hlfir.declare %{{.*}} dummy_scope %[[SCOPE_S]] arg 1 {{.*pointer.*}} : (!fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>>, !fir.dscope) -> (!fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>>, !fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>>)
-! CHECK: %[[Y_S:.*]]:2 = hlfir.declare %{{.*}} dummy_scope %[[SCOPE_S]] arg 2 {{.*pointer.*}} : (!fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>>, !fir.dscope) -> (!fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>>, !fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>>)
-! CHECK: omp.declare_simd
-! CHECK-NOT: {{[[:space:]]*(aligned|linear)\(}}
-! CHECK-SAME: simdlen(8)
-! CHECK: return
+! CHECK: omp.declare_simd{{.*}}simdlen(8){{$}}
+! CHECK-NEXT: return
 
 subroutine declare_simd_combined(x, y, n, i)
-    !$omp declare simd aligned(x, y : 64) linear(i) simdlen(8)
-    real(8), pointer, intent(inout) :: x(:)
-    real(8), pointer, intent(in)    :: y(:)
-    integer, intent(in) :: n, i
+#ifdef OMP_60
+!$omp declare_simd aligned(x, y : 64) linear(i) simdlen(8)
+#else
+!$omp declare simd aligned(x, y : 64) linear(i) simdlen(8)
+#endif
+  real(8), pointer, intent(inout) :: x(:)
+  real(8), pointer, intent(in)    :: y(:)
+  integer, intent(in) :: n, i
 
-    if (i <= n) then
-        x(i) = x(i) + y(i)
-    end if
+  if (i <= n) then
+      x(i) = x(i) + y(i)
+  end if
 end subroutine declare_simd_combined
 
 ! CHECK-LABEL: func.func @_QPdeclare_simd_combined(
@@ -102,102 +106,3 @@ end subroutine declare_simd_combined
 ! CHECK-SAME: linear(%[[I_DECL]]#0 = %[[C1]] : !fir.ref<i32>)
 ! CHECK-SAME: simdlen(8)
 ! CHECK-SAME: {linear_var_types = [i32]}
-
-#ifdef OMP_60
-
-subroutine declare_simd_60_no_clause()
-  !$omp declare_simd
-end subroutine declare_simd_60_no_clause
-
-! CHECK60-LABEL: func.func @_QPdeclare_simd_60_no_clause() {
-! CHECK60: omp.declare_simd
-
-subroutine declare_simd_aligned_60(x, y, n, i)
-  !$omp declare_simd aligned(x, y : 64)
-  real(8), pointer, intent(inout) :: x(:)
-  real(8), pointer, intent(in)    :: y(:)
-  integer, intent(in) :: n, i
-  if (i <= n) x(i) = x(i) + y(i)
-end subroutine declare_simd_aligned_60
-
-! CHECK60-LABEL: func.func @_QPdeclare_simd_aligned_60(
-! CHECK60: %[[SCOPE_A:.*]] = fir.dummy_scope : !fir.dscope
-! CHECK60: %[[I_A:.*]]:2 = hlfir.declare %{{.*}} dummy_scope %[[SCOPE_A]] arg 4 {{.*}} : (!fir.ref<i32>, !fir.dscope) -> (!fir.ref<i32>, !fir.ref<i32>)
-! CHECK60: %[[N_A:.*]]:2 = hlfir.declare %{{.*}} dummy_scope %[[SCOPE_A]] arg 3 {{.*}} : (!fir.ref<i32>, !fir.dscope) -> (!fir.ref<i32>, !fir.ref<i32>)
-! CHECK60: %[[X_A:.*]]:2 = hlfir.declare %{{.*}} dummy_scope %[[SCOPE_A]] arg 1 {{.*pointer.*}} : (!fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>>, !fir.dscope) -> (!fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>>, !fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>>)
-! CHECK60: %[[Y_A:.*]]:2 = hlfir.declare %{{.*}} dummy_scope %[[SCOPE_A]] arg 2 {{.*pointer.*}} : (!fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>>, !fir.dscope) -> (!fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>>, !fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>>)
-! CHECK60: omp.declare_simd
-! CHECK60-SAME: aligned(%[[X_A]]#0 : !fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>> -> 64 : i64,
-! CHECK60-SAME:         %[[Y_A]]#0 : !fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>> -> 64 : i64)
-! CHECK60-NOT: {{[[:space:]]*(linear|simdlen)\(}}
-! CHECK60: return
-
-subroutine declare_simd_linear_60(x, y, n, i)
-  !$omp declare_simd linear(i)
-  real(8), pointer, intent(inout) :: x(:)
-  real(8), pointer, intent(in)    :: y(:)
-  integer, intent(in) :: n, i
-  if (i <= n) x(i) = x(i) + y(i)
-end subroutine  declare_simd_linear_60
-
-! CHECK60-LABEL: func.func @_QPdeclare_simd_linear_60(
-! CHECK60: %[[SCOPE_L:.*]] = fir.dummy_scope : !fir.dscope
-! CHECK60: %[[I_L:.*]]:2 = hlfir.declare %{{.*}} dummy_scope %[[SCOPE_L]] arg 4 {{.*}} : (!fir.ref<i32>, !fir.dscope) -> (!fir.ref<i32>, !fir.ref<i32>)
-! CHECK60: %[[N_L:.*]]:2 = hlfir.declare %{{.*}} dummy_scope %[[SCOPE_L]] arg 3 {{.*}} : (!fir.ref<i32>, !fir.dscope) -> (!fir.ref<i32>, !fir.ref<i32>)
-! CHECK60: %[[X_L:.*]]:2 = hlfir.declare %{{.*}} dummy_scope %[[SCOPE_L]] arg 1 {{.*pointer.*}} : (!fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>>, !fir.dscope) -> (!fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>>, !fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>>)
-! CHECK60: %[[Y_L:.*]]:2 = hlfir.declare %{{.*}} dummy_scope %[[SCOPE_L]] arg 2 {{.*pointer.*}} : (!fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>>, !fir.dscope) -> (!fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>>, !fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>>)
-! CHECK60: %[[C1_L:.*]] = arith.constant 1 : i32
-! CHECK60: omp.declare_simd
-! CHECK60-NOT: {{[[:space:]]*(aligned|simdlen)\(}}
-! CHECK60: return
-
-subroutine declare_simd_simdlen_60(x, y, n, i)
-  !$omp declare_simd simdlen(8)
-  real(8), pointer, intent(inout) :: x(:)
-  real(8), pointer, intent(in)    :: y(:)
-  integer, intent(in) :: n, i
-  if (i <= n) x(i) = x(i) + y(i)
-end subroutine declare_simd_simdlen_60
-
-! CHECK60-LABEL: func.func @_QPdeclare_simd_simdlen_60(
-! CHECK60: %[[SCOPE_S:.*]] = fir.dummy_scope : !fir.dscope
-! CHECK60: %[[I_S:.*]]:2 = hlfir.declare %{{.*}} dummy_scope %[[SCOPE_S]] arg 4 {{.*}} : (!fir.ref<i32>, !fir.dscope) -> (!fir.ref<i32>, !fir.ref<i32>)
-! CHECK60: %[[N_S:.*]]:2 = hlfir.declare %{{.*}} dummy_scope %[[SCOPE_S]] arg 3 {{.*}} : (!fir.ref<i32>, !fir.dscope) -> (!fir.ref<i32>, !fir.ref<i32>)
-! CHECK60: %[[X_S:.*]]:2 = hlfir.declare %{{.*}} dummy_scope %[[SCOPE_S]] arg 1 {{.*pointer.*}} : (!fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>>, !fir.dscope) -> (!fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>>, !fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>>)
-! CHECK60: %[[Y_S:.*]]:2 = hlfir.declare %{{.*}} dummy_scope %[[SCOPE_S]] arg 2 {{.*pointer.*}} : (!fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>>, !fir.dscope) -> (!fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>>, !fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>>)
-! CHECK60: omp.declare_simd
-! CHECK60-NOT: {{[[:space:]]*(aligned|linear)\(}}
-! CHECK60-SAME: simdlen(8)
-! CHECK60: return
-
-subroutine declare_simd_combined_60(x, y, n, i)
-    !$omp declare_simd aligned(x, y : 64) linear(i) simdlen(8)
-    real(8), pointer, intent(inout) :: x(:)
-    real(8), pointer, intent(in)    :: y(:)
-    integer, intent(in) :: n, i
-
-    if (i <= n) then
-        x(i) = x(i) + y(i)
-    end if
-end subroutine declare_simd_combined_60
-
-! CHECK60-LABEL: func.func @_QPdeclare_simd_combined_60(
-! CHECK60-SAME: %{{.*}}: !fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>>{{.*}}fir.bindc_name = "x"
-! CHECK60-SAME: %{{.*}}: !fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>>{{.*}}fir.bindc_name = "y"
-! CHECK60-SAME: %{{.*}}: !fir.ref<i32>{{.*}}fir.bindc_name = "n"
-! CHECK60-SAME: %{{.*}}: !fir.ref<i32>{{.*}}fir.bindc_name = "i"
-! CHECK60: %[[SCOPE:.*]] = fir.dummy_scope : !fir.dscope
-! CHECK60: %[[I_DECL:.*]]:2 = hlfir.declare %{{.*}} dummy_scope %[[SCOPE]] arg 4 {{.*}} : (!fir.ref<i32>, !fir.dscope) -> (!fir.ref<i32>, !fir.ref<i32>)
-! CHECK60: %[[N_DECL:.*]]:2 = hlfir.declare %{{.*}} dummy_scope %[[SCOPE]] arg 3 {{.*}} : (!fir.ref<i32>, !fir.dscope) -> (!fir.ref<i32>, !fir.ref<i32>)
-! CHECK60: %[[X_DECL:.*]]:2 = hlfir.declare %{{.*}} dummy_scope %[[SCOPE]] arg 1 {{.*}} : (!fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>>, !fir.dscope) -> (!fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>>, !fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>>)
-! CHECK60: %[[Y_DECL:.*]]:2 = hlfir.declare %{{.*}} dummy_scope %[[SCOPE]] arg 2 {{.*}} : (!fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>>, !fir.dscope) -> (!fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>>, !fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>>)
-! CHECK60: %[[C1:.*]] = arith.constant 1 : i32
-
-! CHECK60: omp.declare_simd
-! CHECK60-SAME: aligned(%[[X_DECL]]#0 : !fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>> -> 64 : i64,
-! CHECK60-SAME:         %[[Y_DECL]]#0 : !fir.ref<!fir.box<!fir.ptr<!fir.array<?xf64>>>> -> 64 : i64)
-! CHECK60-SAME: linear(%[[I_DECL]]#0 = %[[C1]] : !fir.ref<i32>)
-! CHECK60-SAME: simdlen(8)
-! CHECK60-SAME: {linear_var_types = [i32]}
-
-#endif !OMP_60
