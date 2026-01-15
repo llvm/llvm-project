39c24174619c56623bf17254dd6cca10c987fbfe
Ramkumar Ramachandra <artagnon@tenstorrent.com>
2026-01-15 16:15:39 +0000

    [cmake] Add config for riscv(32|64)-unknown-elf
    Add a config to clang/cmake/caches to document building compiler-rt for
    a baremetal RISCV target. It was tested on macOS, and has necessitated
    some fixes to LLVMConfig and ClangConfig: the inclusion of LLVMExports
    relies on the target system supporting shared libraries, which is not
    the case for a baremetal target. However, since it is not necessary,
    guard the import with CMAKE_TARGET_SUPPORTS_SHARED_LIBS, which would be
    completely non-functional for all non-baremetal targets. Also add
    Generic-ELF to the CMAKE_SYSTEM_NAME matcher in HandleLLVMOptions, so we
    can use Generic-ELF to describe the riscv(32|64)-unknown-elf targets.
    
    Documenting building libcxx and libcxxabi for this target is left to a
    follow-up.

---
+++ clang/cmake/caches/BaremetalRISCV.cmake
@@ -0,0 +1,37 @@
+# Build clang and compiler-rt for Baremetal RISC-V (cross-compiling)
+#
+# Use this as:
+#
+#  $ cmake -GNinja -DCMAKE_BUILD_TYPE=Debug -DLLVM_ENABLE_PROJECTS=clang \
+#    -DLLVM_ENABLE_RUNTIMES=compiler-rt \
+#    -C../clang/cmake/caches/BaremetalRISCV.cmake ../llvm
+#  $ ninja runtimes-riscv64-unknown-elf runtimes-riscv32-unknown-elf
+#
+set(TRIPLES riscv64-unknown-elf;riscv32-unknown-elf)
+set(LLVM_BUILTIN_TARGETS ${TRIPLES} CACHE STRING "")
+set(LLVM_RUNTIME_TARGETS ${TRIPLES} CACHE STRING "")
+set(LLVM_ENABLE_PER_TARGET_RUNTIME_DIR ON CACHE BOOL "")
+
+foreach(target ${TRIPLES})
+  # builtins config
+  set(BUILTINS_${target}_CMAKE_BUILD_TYPE Release CACHE STRING "")
+  set(BUILTINS_${target}_CMAKE_SYSTEM_NAME Generic-ELF CACHE STRING "")
+  set(BUILTINS_${target}_COMPILER_RT_BAREMETAL_BUILD ON CACHE BOOL "")
+  set(BUILTINS_${target}_COMPILER_RT_OS_DIR ${target} CACHE STRING "")
+
+  # enable compiler-rt in runtimes
+  set(RUNTIMES_${target}_ENABLE_RUNTIMES compiler-rt CACHE STRING "")
+
+  # runtimes config
+  set(RUNTIMES_${target}_CMAKE_BUILD_TYPE Release CACHE STRING "")
+  set(RUNTIMES_${target}_CMAKE_SYSTEM_NAME Generic-ELF CACHE STRING "")
+  set(RUNTIMES_${target}_COMPILER_RT_BAREMETAL_BUILD ON CACHE BOOL "")
+  set(RUNTIMES_${target}_COMPILER_RT_OS_DIR ${target} CACHE STRING "")
+
+  # additional runtimes config for compiler-rt
+  set(RUNTIMES_${target}_COMPILER_RT_BUILD_LIBFUZZER OFF CACHE BOOL "")
+  set(RUNTIMES_${target}_COMPILER_RT_BUILD_PROFILE OFF CACHE BOOL "")
+  set(RUNTIMES_${target}_COMPILER_RT_BUILD_SANITIZERS OFF CACHE BOOL "")
+  set(RUNTIMES_${target}_COMPILER_RT_BUILD_XRAY OFF CACHE BOOL "")
+  set(RUNTIMES_${target}_COMPILER_RT_USE_BUILTINS_LIBRARY ON CACHE BOOL "")
+endforeach()
