5689ee798dad51f6066952532976419d54d06d3f
Stefan Weigl-Bosker <stefan@s00.xyz>
2026-01-15 12:59:53 -0500

    fixes

--- llvm/include/llvm/CodeGen/GlobalISel/GenericMachineInstrs.h
+++ llvm/include/llvm/CodeGen/GlobalISel/GenericMachineInstrs.h
@@ -797,14 +797,6 @@ public:
   };
 };
 
-/// Represents a logical xor.
-class GXor : public GLogicalBinOp {
-public:
-  static bool classof(const MachineInstr *MI) {
-    return MI->getOpcode() == TargetOpcode::G_XOR;
-  };
-};
-
 /// Represents an extract vector element.
 class GExtractVectorElement : public GenericMachineInstr {
 public:
