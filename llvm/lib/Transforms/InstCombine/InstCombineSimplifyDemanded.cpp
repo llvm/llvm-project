cccfd01ce01c4a115c211cd2a260d7bd263e9c45
Matt Arsenault <Matthew.Arsenault@amd.com>
2026-01-15 09:16:21 +0100

    InstCombine: Apply parameter nofpclass in SimplifyDemandedFPClass
    Apply the use operand's nofpclass to the demanded mask.

--- llvm/lib/Transforms/InstCombine/InstCombineSimplifyDemanded.cpp
+++ llvm/lib/Transforms/InstCombine/InstCombineSimplifyDemanded.cpp
@@ -3040,6 +3040,11 @@ bool InstCombinerImpl::SimplifyDemandedFPClass(Instruction *I, unsigned OpNo,
   if (Depth == MaxAnalysisRecursionDepth)
     return false;
 
+  if (const CallBase *CB = dyn_cast<CallBase>(VInst)) {
+    FPClassTest NoFPClass = CB->getParamNoFPClass(U.getOperandNo());
+    DemandedMask &= ~NoFPClass;
+  }
+
   Value *NewVal;
 
   if (VInst->hasOneUse()) {
