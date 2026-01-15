98c60abc78aef21c0f883a064ea596ce3dc4f139
Daniel Thornburgh <dthorn@google.com>
2026-01-15 14:10:25 -0800

    [NFC][LTO] Move isPreservedName out of IRSymtab into LTO's Symbol
    This resolves the FIXME in IRSymtab and cleans up the semantics of the
    IRSymtab. The list of preserved symbols really shouldn't be seen as a
    property of the IR symbol table, since it's an LTO-specific concern, and
    it's very tenuous to claim that this information is actually present in
    the bitcode file to be exposed through its symbol table.
    
    Instead, this PR moves this logic into LTO's view of the symbol, which
    allows consumers to determine preserved-ness themselves. This was broken
    out of #164916; this prevents that PR from introducing a circular
    dependency, but it still seems like an independently good idea by virtue
    of the above.

--- llvm/include/llvm/LTO/LTO.h
+++ llvm/include/llvm/LTO/LTO.h
@@ -16,6 +16,7 @@
 #define LLVM_LTO_LTO_H
 
 #include "llvm/IR/LLVMRemarkStreamer.h"
+#include "llvm/IR/RuntimeLibcalls.h"
 #include "llvm/Support/Compiler.h"
 #include <memory>
 
@@ -167,6 +168,8 @@ public:
     using irsymtab::Symbol::getSectionName;
     using irsymtab::Symbol::isExecutable;
     using irsymtab::Symbol::isUsed;
+
+    bool isPreserved(const RTLIB::RuntimeLibcallsInfo &Libcalls) const;
   };
 
   /// A range over the symbols in this InputFile.
@@ -574,7 +577,7 @@ private:
 
   void addModuleToGlobalRes(ArrayRef<InputFile::Symbol> Syms,
                             ArrayRef<SymbolResolution> Res, unsigned Partition,
-                            bool InSummary);
+                            bool InSummary, const Triple &TT);
 
   // These functions take a range of symbol resolutions and consume the
   // resolutions used by a single input module. Functions return ranges refering
