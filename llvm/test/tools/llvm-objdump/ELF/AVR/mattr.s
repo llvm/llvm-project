230351019c18a5ebebe20556dd7ec069e2629925
RuoyuQiu <cabbaken@outlook.com>
2026-01-16 08:33:30 +0000

    Optimize format of test file
    Signed-off-by: RuoyuQiu <cabbaken@outlook.com>

--- llvm/test/tools/llvm-objdump/ELF/AVR/mattr.s
+++ llvm/test/tools/llvm-objdump/ELF/AVR/mattr.s
@@ -1,15 +1,17 @@
 ## When --mattr and --mcpu are both empty, disassemble all known instructions.
 # RUN: llvm-mc -filetype=obj -triple=avr -mattr=+special %s -o %t
-# RUN: llvm-objdump -d --no-show-raw-insn %t | FileCheck %s --check-prefixes=CHECK,ALL
+# RUN: llvm-objdump -d --no-show-raw-insn %t | FileCheck %s --check-prefixes=ALL
 
-## If --mattr or --mcpu is specified, don't default to --mattr=+special.
-# RUN: llvm-objdump -d --no-show-raw-insn --mattr=+avr2 %t | FileCheck %s --check-prefixes=CHECK,UNKNOWN
-# RUN: llvm-objdump -d --no-show-raw-insn --mcpu=avr2 %t | FileCheck %s --check-prefixes=CHECK,UNKNOWN
-
-# CHECK-LABEL: <_start>:
+# ALL: <_start>:
 # ALL-NEXT:      call    0x0
 # ALL-NEXT:      jmp     0x0
 # ALL-NEXT:      rjmp    .-2
+
+## If --mattr or --mcpu is specified, don't default to --mattr=+special.
+# RUN: llvm-objdump -d --no-show-raw-insn --mattr=+avr2 %t | FileCheck %s --check-prefixes=UNKNOWN
+# RUN: llvm-objdump -d --no-show-raw-insn --mcpu=avr2 %t | FileCheck %s --check-prefixes=UNKNOWN
+
+# UNKNOWN: <_start>:
 # UNKNOWN-COUNT-2: <unknown>
 # UNKNOWN:       rjmp    .-2
 
