19349c58b3f994ca856d749c49ea75970967a775
nishant_sachdeva <nishant.sachdeva@research.iiit.ac.in>
2026-01-16 15:11:39 +0530

    Nit commit - utils.h files header had incorrect name

--- llvm/tools/llvm-ir2vec/utils/utils.h
+++ llvm/tools/llvm-ir2vec/utils/utils.h
@@ -1,4 +1,4 @@
-//===- llvm-ir2vec.h - IR2Vec/MIR2Vec Tool Classes ----------------------===//
+//===- utils.h - IR2Vec/MIR2Vec Tool Classes ----------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
