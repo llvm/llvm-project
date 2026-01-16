9e7cb4d8f1c486e9d5d6d7e773a8720841695d05
Ramkumar Ramachandra <artagnon@tenstorrent.com>
2026-01-16 23:17:58 +0000

    BaremetalRISCV: fix some issues

--- clang/cmake/caches/BaremetalRISCV.cmake
+++ clang/cmake/caches/BaremetalRISCV.cmake
@@ -1,4 +1,4 @@
-# Build clang and compiler-rt targeting Baremetal RISC-V (cross-compiling)
+# Build compiler-rt for Baremetal RISC-V (cross-compiling)
 #
 # Use this as:
 #
@@ -10,7 +10,6 @@
 set(TRIPLES riscv64-unknown-elf;riscv32-unknown-elf)
 set(LLVM_BUILTIN_TARGETS ${TRIPLES} CACHE STRING "")
 set(LLVM_RUNTIME_TARGETS ${TRIPLES} CACHE STRING "")
-set(LLVM_ENABLE_PER_TARGET_RUNTIME_DIR ON CACHE BOOL "")
 
 foreach(target ${TRIPLES})
   # builtins config
@@ -29,11 +28,4 @@ foreach(target ${TRIPLES})
   set(RUNTIMES_${target}_COMPILER_RT_BAREMETAL_BUILD ON CACHE BOOL "")
   set(RUNTIMES_${target}_COMPILER_RT_OS_DIR ${target} CACHE STRING "")
   set(RUNTIMES_${target}_LLVM_CONFIG_NO_EXPORTS ON CACHE BOOL "")
-
-  # additional runtimes config for compiler-rt
-  set(RUNTIMES_${target}_COMPILER_RT_BUILD_LIBFUZZER OFF CACHE BOOL "")
-  set(RUNTIMES_${target}_COMPILER_RT_BUILD_PROFILE OFF CACHE BOOL "")
-  set(RUNTIMES_${target}_COMPILER_RT_BUILD_SANITIZERS OFF CACHE BOOL "")
-  set(RUNTIMES_${target}_COMPILER_RT_BUILD_XRAY OFF CACHE BOOL "")
-  set(RUNTIMES_${target}_COMPILER_RT_USE_BUILTINS_LIBRARY ON CACHE BOOL "")
 endforeach()
