c9645f7be26e52de57dddccf1dfd8b1609a02ada
RuoyuQiu <cabbaken@outlook.com>
2026-01-16 08:48:40 +0000

    Fix format of test file again
    Signed-off-by: RuoyuQiu <cabbaken@outlook.com>

--- llvm/test/tools/llvm-objdump/ELF/AVR/mattr.s
+++ llvm/test/tools/llvm-objdump/ELF/AVR/mattr.s
@@ -2,18 +2,18 @@
 # RUN: llvm-mc -filetype=obj -triple=avr -mattr=+special %s -o %t
 # RUN: llvm-objdump -d --no-show-raw-insn %t | FileCheck %s --check-prefixes=ALL
 
-# ALL: <_start>:
-# ALL-NEXT:      call    0x0
-# ALL-NEXT:      jmp     0x0
-# ALL-NEXT:      rjmp    .-2
+# ALL:      <_start>:
+# ALL-NEXT:   call    0x0
+# ALL-NEXT:   jmp     0x0
+# ALL-NEXT:   rjmp    .-2
 
 ## If --mattr or --mcpu is specified, don't default to --mattr=+special.
 # RUN: llvm-objdump -d --no-show-raw-insn --mattr=+avr2 %t | FileCheck %s --check-prefixes=UNKNOWN
 # RUN: llvm-objdump -d --no-show-raw-insn --mcpu=avr2 %t | FileCheck %s --check-prefixes=UNKNOWN
 
-# UNKNOWN: <_start>:
-# UNKNOWN-COUNT-2: <unknown>
-# UNKNOWN:       rjmp    .-2
+# UNKNOWN:         <_start>:
+# UNKNOWN-COUNT-2:   <unknown>
+# UNKNOWN:           rjmp    .-2
 
 .globl _start
 _start:
