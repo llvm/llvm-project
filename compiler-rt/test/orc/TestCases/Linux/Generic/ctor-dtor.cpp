b64f9d80b4359961866fbd478a7bdbb1f992fef4
Yaxing Cai <caiyaxing666@gmail.com>
2026-01-16 22:14:08 +0800

    upd test

--- compiler-rt/test/orc/TestCases/Linux/Generic/ctor-dtor.cpp
+++ compiler-rt/test/orc/TestCases/Linux/Generic/ctor-dtor.cpp
@@ -5,27 +5,82 @@
 //
 // REQUIRES: system-linux && host-arch-compatible
 
-// CHECK: constructor #1
-// CHECK: constructor #2
-// CHECK: constructor #N
-// CHECK: destructor #N
-// CHECK: destructor #2
-// CHECK: destructor #1
-
+// CHECK: <init_array.101>
+// CHECK: <init_array.102>
+// CHECK: <init_array.103>
+// CHECK: <init_array>
+// CHECK: <ctors.103>
+// CHECK: <ctors.102>
+// CHECK: <ctors.101>
+// CHECK: <ctors>
+// CHECK: <dtors>
+// CHECK: <dtors.101>
+// CHECK: <dtors.102>
+// CHECK: <dtors.103>
+// CHECK: <fini_array>
+// CHECK: <fini_array.103>
+// CHECK: <fini_array.102>
+// CHECK: <fini_array.101>
 #include <stdio.h>
 
-int x = 0;
+typedef void (*ctor_t)(void);
+typedef void (*dtor_t)(void);
 
-__attribute__((constructor(101))) void constructor1() {
-  puts("constructor #1");
-}
-__attribute__((constructor(102))) void constructor2() {
-  puts("constructor #2");
-}
-__attribute__((constructor)) void constructorN() { puts("constructor #N"); }
+__attribute__((constructor)) void init_array() { puts("<init_array>"); }
 
-__attribute__((destructor(101))) void destructor1() { puts("destructor #1"); }
-__attribute__((destructor(102))) void destructor2() { puts("destructor #2"); }
-__attribute__((destructor)) void destructorN() { puts("destructor #N"); }
+__attribute__((constructor(101))) void init_array_101() {
+  puts("<init_array.101>");
+}
+
+__attribute__((constructor(102))) void init_array_102() {
+  puts("<init_array.102>");
+}
+__attribute__((constructor(103))) void init_array_103() {
+  puts("<init_array.103>");
+}
+
+static void ctors(void) { puts("<ctors>"); }
+__attribute__((section(".ctors"), used)) static ctor_t ctors_ptr = ctors;
+
+static void ctors_101(void) { puts("<ctors.101>"); }
+__attribute__((section(".ctors.101"), used)) static ctor_t ctors_1_ptr =
+    ctors_101;
+
+static void ctors_102(void) { puts("<ctors.102>"); }
+__attribute__((section(".ctors.102"), used)) static ctor_t ctors_2_ptr =
+    ctors_102;
+
+static void ctors_103(void) { puts("<ctors.103>"); }
+__attribute__((section(".ctors.103"), used)) static ctor_t ctors_3_ptr =
+    ctors_103;
+
+__attribute__((destructor)) void fini_array() { puts("<fini_array>"); }
+
+__attribute__((destructor(101))) void fini_array_101() {
+  puts("<fini_array.101>");
+}
+
+__attribute__((destructor(102))) void fini_array_102() {
+  puts("<fini_array.102>");
+}
+
+__attribute__((destructor(103))) void fini_array_103() {
+  puts("<fini_array.103>");
+}
+
+static void dtors(void) { puts("<dtors>"); }
+__attribute__((section(".dtors"), used)) static dtor_t dtors_ptr = dtors;
+
+static void dtors_101(void) { puts("<dtors.101>"); }
+__attribute__((section(".dtors.101"), used)) static dtor_t dtors_1_ptr =
+    dtors_101;
+
+static void dtors_102(void) { puts("<dtors.102>"); }
+__attribute__((section(".dtors.102"), used)) static dtor_t dtors_2_ptr =
+    dtors_102;
+
+static void dtors_103(void) { puts("<dtors.103>"); }
+__attribute__((section(".dtors.103"), used)) static dtor_t dtors_3_ptr =
+    dtors_103;
 
 int main(void) { return 0; }
