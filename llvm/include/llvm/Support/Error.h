07e384291719af6ddd80654e2491762c636d09d5
Michael Buch <michaelbuch12@gmail.com>
2026-01-16 15:55:45 +0000

    fixup! clang-format

--- llvm/include/llvm/Support/Error.h
+++ llvm/include/llvm/Support/Error.h
@@ -1341,7 +1341,8 @@ inline Error createStringError(std::errc EC, char const *Fmt,
 template <typename... Ts>
 inline Error createStringErrorV(std::error_code EC, char const *Fmt,
                                 Ts &&...Vals) {
-  return make_error<StringError>(formatv(Fmt, std::forward<Ts>(Vals)...).str(), EC, true);
+  return make_error<StringError>(formatv(Fmt, std::forward<Ts>(Vals)...).str(),
+                                 EC, true);
 }
 
 template <typename... Ts>
