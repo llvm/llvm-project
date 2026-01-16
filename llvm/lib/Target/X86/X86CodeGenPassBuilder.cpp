llvm/llvm-project

commit 1d03535c8f0e22c81970c6e671d9dc12f5d790cf

[X86][NewPM] Fill out X86CodeGenPassBuilder (#176061)
This patch fills out X86CodeGenPassBuilder a decent amount of the way.
Comments are added where we either need more CodeGenPassBuilder hooks or
have not yet ported passes. This is intended to be a simple mapping of
the existing X86PassConfig and does not intend to make any improvements
or fix any existing issues (like using non fast-regalloc at O0 with AMX
instructions).

llvm/lib/Target/X86/X86CodeGenPassBuilder.cpp
@@ -14,44 +14,248 @@
 #include "X86ISelDAGToDAG.h"
 #include "X86TargetMachine.h"
 
+#include "llvm/CodeGen/AtomicExpand.h"
+#include "llvm/CodeGen/EarlyIfConversion.h"
+#include "llvm/CodeGen/IndirectBrExpand.h"
+#include "llvm/CodeGen/InterleavedAccess.h"
+#include "llvm/CodeGen/JMCInstrumenter.h"
 #include "llvm/MC/MCStreamer.h"
 #include "llvm/Passes/CodeGenPassBuilder.h"
 #include "llvm/Passes/PassBuilder.h"
+#include "llvm/Support/CodeGen.h"
+#include "llvm/Transforms/CFGuard.h"
 
 using namespace llvm;
+
+extern cl::opt<bool> EnableMachineCombinerPass;
 
 namespace {
 
 class X86CodeGenPassBuilder
     : public CodeGenPassBuilder<X86CodeGenPassBuilder, X86TargetMachine> {
+  using Base = CodeGenPassBuilder<X86CodeGenPassBuilder, X86TargetMachine>;
+
 public:
   explicit X86CodeGenPassBuilder(X86TargetMachine &TM,
                                  const CGPassBuilderOption &Opts,
                                  PassInstrumentationCallbacks *PIC)
       : CodeGenPassBuilder(TM, Opts, PIC) {}
+
+  void addIRPasses(PassManagerWrapper &PMW) const;
   void addPreISel(PassManagerWrapper &PMW) const;
-  void addAsmPrinter(PassManagerWrapper &PMW, CreateMCStreamer) const;
   Error addInstSelector(PassManagerWrapper &PMW) const;
+  void addILPOpts(PassManagerWrapper &PMW) const;
+  void addMachineSSAOptimization(PassManagerWrapper &PMW) const;
+  void addPreRegAlloc(PassManagerWrapper &PMW) const;
+  // TODO(boomanaiden154): We need to add addPostFastRegAllocRewrite here once
+  // it is available to support AMX.
+  void addPostRegAlloc(PassManagerWrapper &PMW) const;
   void addPreSched2(PassManagerWrapper &PMW) const;
+  void addPreEmitPass(PassManagerWrapper &PMW) const;
+  void addPreEmitPass2(PassManagerWrapper &PMW) const;
+  // TODO(boomanaiden154): We need to add addRegAssignAndRewriteOptimized here
+  // once it is available to support AMX.
+  void addAsmPrinter(PassManagerWrapper &PMW, CreateMCStreamer) const;
 };
 
+void X86CodeGenPassBuilder::addIRPasses(PassManagerWrapper &PMW) const {
+  addFunctionPass(AtomicExpandPass(TM), PMW);
+
+  // We add both pass anyway and when these two passes run, one will be a
+  // no-op based on the optimization level/attributes.
+  addFunctionPass(X86LowerAMXIntrinsicsPass(&TM), PMW);
+  addFunctionPass(X86LowerAMXTypePass(&TM), PMW);
+
+  Base::addIRPasses(PMW);
+
+  if (getOptLevel() != CodeGenOptLevel::None) {
+    addFunctionPass(InterleavedAccessPass(TM), PMW);
+    addFunctionPass(X86PartialReductionPass(&TM), PMW);
+  }
+
+  // Add passes that handle indirect branch removal and insertion of a retpoline
+  // thunk. These will be a no-op unless a function subtarget has the retpoline
+  // feature enabled.
+  addFunctionPass(IndirectBrExpandPass(TM), PMW);
+
+  // Add Control Flow Guard checks.
+  const Triple &TT = TM.getTargetTriple();
+  if (TT.isOSWindows())
+    addFunctionPass(CFGuardPass(TT.isX86_64() ? CFGuardPass::Mechanism::Dispatch
+                                              : CFGuardPass::Mechanism::Check),
+                    PMW);
+
+  if (TM.Options.JMCInstrument) {
+    flushFPMsToMPM(PMW);
+    addModulePass(JMCInstrumenterPass(), PMW);
+  }
+}
+
 void X86CodeGenPassBuilder::addPreISel(PassManagerWrapper &PMW) const {
-  // TODO: Add passes pre instruction selection.
+  // Only add this pass for 32-bit x86 Windows.
+  const Triple &TT = TM.getTargetTriple();
+  if (TT.isOSWindows() && TT.isX86_32()) {
+    // TODO(boomanaiden154): Add X86WinEHStatePass here once it has been ported.
+  }
+}
+
+Error X86CodeGenPassBuilder::addInstSelector(PassManagerWrapper &PMW) const {
+  addMachineFunctionPass(X86ISelDAGToDAGPass(TM), PMW);
+
+  // For ELF, cleanup any local-dynamic TLS accesses
+  if (TM.getTargetTriple().isOSBinFormatELF() &&
+      getOptLevel() != CodeGenOptLevel::None) {
+    // TODO(boomanaiden154): Add CleanupLocalDynamicTLSPass here once it has
+    // been ported.
+  }
+
+  // TODO(boomanaiden154): Add X86GlobalPassRegPass here once it has been
+  // ported.
+  addMachineFunctionPass(X86ArgumentStackSlotPass(), PMW);
+  return Error::success();
+}
+
+void X86CodeGenPassBuilder::addILPOpts(PassManagerWrapper &PMW) const {
+  addMachineFunctionPass(EarlyIfConverterPass(), PMW);
+  if (EnableMachineCombinerPass) {
+    // TODO(boomanaiden154): Add the MachineCombinerPass here once it has been
+    // ported to the new pass manager.
+  }
+  addMachineFunctionPass(X86CmovConversionPass(), PMW);
+}
+
+void X86CodeGenPassBuilder::addMachineSSAOptimization(
+    PassManagerWrapper &PMW) const {
+  // TODO(boomanaiden154): Add X86DomainReassignmentPass here once it has been
+  // ported.
+  Base::addMachineSSAOptimization(PMW);
+}
+
+void X86CodeGenPassBuilder::addPreRegAlloc(PassManagerWrapper &PMW) const {
+  if (getOptLevel() != CodeGenOptLevel::None) {
+    addMachineFunctionPass(LiveRangeShrinkPass(), PMW);
+    addMachineFunctionPass(X86FixupSetCCPass(), PMW);
+    addMachineFunctionPass(X86CallFrameOptimizationPass(), PMW);
+    addMachineFunctionPass(X86AvoidStoreForwardingBlocksPass(), PMW);
+  }
+
+  addMachineFunctionPass(X86SuppressAPXForRelocationPass(), PMW);
+  addMachineFunctionPass(X86SpeculativeLoadHardeningPass(), PMW);
+  addMachineFunctionPass(X86FlagsCopyLoweringPass(), PMW);
+  addMachineFunctionPass(X86DynAllocaExpanderPass(), PMW);
+
+  if (getOptLevel() != CodeGenOptLevel::None)
+    addMachineFunctionPass(X86PreTileConfigPass(), PMW);
+  else
+    addMachineFunctionPass(X86FastPreTileConfigPass(), PMW);
+}
+
+void X86CodeGenPassBuilder::addPostRegAlloc(PassManagerWrapper &PMW) const {
+  addMachineFunctionPass(X86LowerTileCopyPass(), PMW);
+  addMachineFunctionPass(X86FPStackifierPass(), PMW);
+  // When -O0 is enabled, the Load Value Injection Hardening pass will fall back
+  // to using the Speculative Execution Side Effect Suppression pass for
+  // mitigation. This is to prevent slow downs due to
+  // analyses needed by the LVIHardening pass when compiling at -O0.
+  if (getOptLevel() != CodeGenOptLevel::None) {
+    // TODO(boomanaiden154): Add X86LoadValueInjectionLoadHardeningPass here
+    // once it has been ported.
+  }
+}
+
+void X86CodeGenPassBuilder::addPreSched2(PassManagerWrapper &PMW) const {
+  addMachineFunctionPass(X86ExpandPseudoPass(), PMW);
+  // TODO(boomanaiden154): Add KCFGPass here once it has been ported.
+}
+
+void X86CodeGenPassBuilder::addPreEmitPass(PassManagerWrapper &PMW) const {
+  if (getOptLevel() != CodeGenOptLevel::None) {
+    // TODO(boomanaiden154): Add X86ExecutionDomainFixPass here once it has
+    // been ported.
+    addMachineFunctionPass(BreakFalseDepsPass(), PMW);
+  }
+
+  // TODO(boomanaiden154): Add X86IndirectBranchTrackingPass here once it has
+  // been ported.
+  // TODO(boomanaiden154): Add X86IssueVZeroUpperPass here once it has been
+  // ported.
+
+  if (getOptLevel() != CodeGenOptLevel::None) {
+    addMachineFunctionPass(X86FixupBWInstsPass(), PMW);
+    // TODO(boomanaiden154): Add X86PadShortFunctionsPass here once it has been
+    // ported.
+    addMachineFunctionPass(X86FixupLEAsPass(), PMW);
+    addMachineFunctionPass(X86FixupInstTuningPass(), PMW);
+    addMachineFunctionPass(X86FixupVectorConstantsPass(), PMW);
+  }
+  addMachineFunctionPass(X86CompressEVEXPass(), PMW);
+  // TODO(boomanaiden154): Add InsertX86WaitPass here once it has been ported.
+}
+
+void X86CodeGenPassBuilder::addPreEmitPass2(PassManagerWrapper &PMW) const {
+  const Triple &TT = TM.getTargetTriple();
+  const MCAsmInfo *MAI = TM.getMCAsmInfo();
+
+  // The X86 Speculative Execution Pass must run after all control
+  // flow graph modifying passes. As a result it was listed to run right before
+  // the X86 Retpoline Thunks pass. The reason it must run after control flow
+  // graph modifications is that the model of LFENCE in LLVM has to be updated
+  // (FIXME: https://bugs.llvm.org/show_bug.cgi?id=45167). Currently the
+  // placement of this pass was hand checked to ensure that the subsequent
+  // passes don't move the code around the LFENCEs in a way that will hurt the
+  // correctness of this pass. This placement has been shown to work based on
+  // hand inspection of the codegen output.
+  // TODO(boomanaiden154): Add X86SpeculativeExecutionSideEffectSuppresionPass
+  // here once it has been ported.
+  // TODO(boomanaiden154): Add X86IndirectThunksPass here
+  // once it has been ported.
+  // TODO(boomanaiden154): Add X86ReturnThunksPass here
+  // once it has been ported.
+
+  // Insert extra int3 instructions after trailing call instructions to avoid
+  // issues in the unwinder.
+  if (TT.isOSWindows() && TT.isX86_64())
+    addMachineFunctionPass(X86AvoidTrailingCallPass(), PMW);
+
+  // Verify basic block incoming and outgoing cfa offset and register values and
+  // correct CFA calculation rule where needed by inserting appropriate CFI
+  // instructions.
+  if (!TT.isOSDarwin() &&
+      (!TT.isOSWindows() ||
+       MAI->getExceptionHandlingType() == ExceptionHandling::DwarfCFI))
+    addMachineFunctionPass(CFIInstrInserterPass(), PMW);
+
+  if (TT.isOSWindows()) {
+    // Identify valid longjmp targets for Windows Control Flow Guard.
+    addMachineFunctionPass(CFGuardLongjmpPass(), PMW);
+    // Identify valid eh continuation targets for Windows EHCont Guard.
+    // TODO(boomanaiden154): Add EHContGuardTargetsPass when it has been
+    // ported.
+  }
+
+  // TODO(boomanaiden154): Add X86LoadValueInjectionRetHardeningPass here once
+  // it has been ported.
+
+  // Insert pseudo probe annotation for callsite profiling
+  // TODO(boomanaiden154): Add PseudoProberInserterPass here once it has been
+  // ported.
+
+  // KCFI indirect call checks are lowered to a bundle, and on Darwin platforms,
+  // also CALL_RVMARKER.
+  // TODO(boomanaiden154): Add UnpackMachineBundlesPass here once it has been
+  // ported.
+
+  // Analyzes and emits pseudos to support Win x64 Unwind V2. This pass must run
+  // after all real instructions have been added to the epilog.
+  if (TT.isOSWindows() && TT.isX86_64()) {
+    // TODO(boomanaiden154): Add X86WinEHUnwindV2Pass here once it has been
+    // ported.
+  }
 }
 
 void X86CodeGenPassBuilder::addAsmPrinter(PassManagerWrapper &PMW,
                                           CreateMCStreamer) const {
   // TODO: Add AsmPrinter.
-}
-
-Error X86CodeGenPassBuilder::addInstSelector(PassManagerWrapper &PMW) const {
-  // TODO: Add instruction selector related passes.
-  addMachineFunctionPass(X86ISelDAGToDAGPass(TM), PMW);
-  return Error::success();
-}
-
-void X86CodeGenPassBuilder::addPreSched2(PassManagerWrapper &PMW) const {
-  addMachineFunctionPass(X86ExpandPseudoPass(), PMW);
 }
 
 } // namespace

llvm/test/CodeGen/X86/llc-pipeline-npm.ll
@@ -1,0 +1,343 @@
+; RUN: llc -enable-new-pm -mtriple=x86_64 -O0 -print-pipeline-passes %s 2>&1 \
+; RUN:   | tr ',' '\n' | FileCheck -check-prefix=O0 %s
+
+; RUN: llc -enable-new-pm -mtriple=x86_64 -O2 -print-pipeline-passes %s 2>&1 \
+; RUN:   | tr ',' '\n' | FileCheck -check-prefix=O2 %s
+
+; RUN: llc -enable-new-pm -mtriple=x86_64-windows -O0 -print-pipeline-passes %s 2>&1 \
+; RUN:   | tr ',' '\n' | FileCheck -check-prefix=O0-WINDOWS %s
+
+; RUN: llc -enable-new-pm -mtriple=x86_64-windows -O3 -print-pipeline-passes %s 2>&1 \
+; RUN:   | tr ',' '\n' | FileCheck -check-prefix=O3-WINDOWS %s
+
+; O0: require<MachineModuleAnalysis>
+; O0: require<profile-summary>
+; O0: require<collector-metadata>
+; O0: require<runtime-libcall-info>
+; O0: pre-isel-intrinsic-lowering
+; O0: function(expand-ir-insts<O0>
+; O0: atomic-expand
+; O0: x86-lower-amx-intrinsics
+; O0: x86-lower-amx-type
+; O0: verify
+; O0: gc-lowering)
+; O0: shadow-stack-gc-lowering
+; O0: function(unreachableblockelim
+; O0: ee-instrument<post-inline>
+; O0: scalarize-masked-mem-intrin
+; O0: expand-reductions
+; O0: indirectbr-expand
+; O0: dwarf-eh-prepare
+; O0: callbr-prepare
+; O0: safe-stack
+; O0: stack-protector
+; O0: verify)
+; O0: function(machine-function(x86-isel
+; O0: x86-argument-stack-slot
+; O0: finalize-isel
+; O0: localstackalloc
+; O0: x86-suppress-apx-for-relocation
+; O0: x86-slh
+; O0: x86-flags-copy-lowering
+; O0: x86-dyn-alloca-expander
+; O0: x86-fast-pre-tile-config
+; O0: phi-node-elimination
+; O0: two-address-instruction
+; O0: regallocfast
+; O0: x86-lower-tile-copy
+; O0: x86-fp-stackifier
+; O0: remove-redundant-debug-values
+; O0: fixup-statepoint-caller-saved
+; O0: prolog-epilog
+; O0: post-ra-pseudos
+; O0: x86-expand-pseudo
+; O0: fentry-insert
+; O0: xray-instrumentation
+; O0: patchable-function
+; O0: x86-compress-evex
+; O0: FuncletLayoutPass
+; O0: remove-loads-into-fake-uses
+; O0: StackMapLivenessPass
+; O0: live-debug-values<emit-debug-entry-values>
+; O0: machine-sanmd
+; O0: stack-frame-layout
+; O0: CFIInstrInserterPass
+; O0: verify)
+; O0: free-machine-function)
+
+; O2: require<MachineModuleAnalysis>
+; O2: require<profile-summary>
+; O2: require<collector-metadata>
+; O2: require<runtime-libcall-info>
+; O2: pre-isel-intrinsic-lowering
+; O2: function(expand-ir-insts<O2>
+; O2: atomic-expand
+; O2: x86-lower-amx-intrinsics
+; O2: x86-lower-amx-type
+; O2: verify
+; O2: loop-mssa(canon-freeze
+; O2: loop-reduce)
+; O2: mergeicmps
+; O2: expand-memcmp
+; O2: gc-lowering)
+; O2: shadow-stack-gc-lowering
+; O2: function(unreachableblockelim
+; O2: consthoist
+; O2: replace-with-veclib
+; O2: partially-inline-libcalls
+; O2: ee-instrument<post-inline>
+; O2: scalarize-masked-mem-intrin
+; O2: expand-reductions
+; O2: interleaved-access
+; O2: x86-partial-reduction
+; O2: indirectbr-expand
+; O2: codegenprepare
+; O2: dwarf-eh-prepare
+; O2: objc-arc-contract
+; O2: callbr-prepare
+; O2: safe-stack
+; O2: stack-protector
+; O2: verify)
+; O2: function(machine-function(x86-isel
+; O2: x86-argument-stack-slot
+; O2: finalize-isel
+; O2: early-tailduplication
+; O2: opt-phis
+; O2: stack-coloring
+; O2: localstackalloc
+; O2: dead-mi-elimination
+; O2: early-ifcvt
+; O2: x86-cmov-conversion
+; O2: early-machinelicm
+; O2: machine-cse
+; O2: machine-sink
+; O2: peephole-opt
+; O2: dead-mi-elimination
+; O2: LiveRangeShrinkPass
+; O2: x86-fixup-setcc
+; O2: x86-cf-opt
+; O2: x86-avoid-sfb
+; O2: x86-suppress-apx-for-relocation
+; O2: x86-slh
+; O2: x86-flags-copy-lowering
+; O2: x86-dyn-alloca-expander
+; O2: x86-pre-tile-config
+; O2: detect-dead-lanes
+; O2: init-undef
+; O2: process-imp-defs
+; O2: unreachable-mbb-elimination
+; O2: require<live-vars>
+; O2: require<machine-loops>
+; O2: phi-node-elimination
+; O2: two-address-instruction
+; O2: register-coalescer
+; O2: rename-independent-subregs
+; O2: machine-scheduler
+; O2: greedy<all>
+; O2: virt-reg-rewriter
+; O2: stack-slot-coloring
+; O2: stack-slot-coloring
+; O2: machine-cp
+; O2: machinelicm
+; O2: x86-lower-tile-copy
+; O2: x86-fp-stackifier
+; O2: remove-redundant-debug-values
+; O2: fixup-statepoint-caller-saved
+; O2: postra-machine-sink
+; O2: shrink-wrap
+; O2: prolog-epilog
+; O2: machine-latecleanup
+; O2: branch-folder
+; O2: tailduplication
+; O2: machine-cp
+; O2: post-ra-pseudos
+; O2: x86-expand-pseudo
+; O2: post-RA-sched
+; O2: block-placement
+; O2: fentry-insert
+; O2: xray-instrumentation
+; O2: patchable-function
+; O2: BreakFalseDepsPass
+; O2: x86-fixup-bw-insts
+; O2: x86-fixup-leas
+; O2: x86-fixup-inst-tuning
+; O2: x86-fixup-inst-tuning
+; O2: x86-compress-evex
+; O2: FuncletLayoutPass
+; O2: remove-loads-into-fake-uses
+; O2: StackMapLivenessPass
+; O2: live-debug-values<emit-debug-entry-values>
+; O2: machine-sanmd
+; O2: stack-frame-layout
+; O2: CFIInstrInserterPass
+; O2: verify)
+; O2: free-machine-function)
+
+; O0-WINDOWS: require<MachineModuleAnalysis>
+; O0-WINDOWS: require<profile-summary>
+; O0-WINDOWS: require<collector-metadata>
+; O0-WINDOWS: require<runtime-libcall-info>
+; O0-WINDOWS: pre-isel-intrinsic-lowering
+; O0-WINDOWS: function(expand-ir-insts<O0>
+; O0-WINDOWS: atomic-expand
+; O0-WINDOWS: x86-lower-amx-intrinsics
+; O0-WINDOWS: x86-lower-amx-type
+; O0-WINDOWS: verify
+; O0-WINDOWS: gc-lowering)
+; O0-WINDOWS: shadow-stack-gc-lowering
+; O0-WINDOWS: function(unreachableblockelim
+; O0-WINDOWS: ee-instrument<post-inline>
+; O0-WINDOWS: scalarize-masked-mem-intrin
+; O0-WINDOWS: expand-reductions
+; O0-WINDOWS: indirectbr-expand
+; O0-WINDOWS: cfguard
+; O0-WINDOWS: win-eh-prepare
+; O0-WINDOWS: dwarf-eh-prepare
+; O0-WINDOWS: callbr-prepare
+; O0-WINDOWS: safe-stack
+; O0-WINDOWS: stack-protector
+; O0-WINDOWS: verify)
+; O0-WINDOWS: function(machine-function(x86-isel
+; O0-WINDOWS: x86-argument-stack-slot
+; O0-WINDOWS: finalize-isel
+; O0-WINDOWS: localstackalloc
+; O0-WINDOWS: x86-suppress-apx-for-relocation
+; O0-WINDOWS: x86-slh
+; O0-WINDOWS: x86-flags-copy-lowering
+; O0-WINDOWS: x86-dyn-alloca-expander
+; O0-WINDOWS: x86-fast-pre-tile-config
+; O0-WINDOWS: phi-node-elimination
+; O0-WINDOWS: two-address-instruction
+; O0-WINDOWS: regallocfast
+; O0-WINDOWS: x86-lower-tile-copy
+; O0-WINDOWS: x86-fp-stackifier
+; O0-WINDOWS: remove-redundant-debug-values
+; O0-WINDOWS: fixup-statepoint-caller-saved
+; O0-WINDOWS: prolog-epilog
+; O0-WINDOWS: post-ra-pseudos
+; O0-WINDOWS: x86-expand-pseudo
+; O0-WINDOWS: fentry-insert
+; O0-WINDOWS: xray-instrumentation
+; O0-WINDOWS: patchable-function
+; O0-WINDOWS: x86-compress-evex
+; O0-WINDOWS: FuncletLayoutPass
+; O0-WINDOWS: remove-loads-into-fake-uses
+; O0-WINDOWS: StackMapLivenessPass
+; O0-WINDOWS: live-debug-values<emit-debug-entry-values>
+; O0-WINDOWS: machine-sanmd
+; O0-WINDOWS: stack-frame-layout
+; O0-WINDOWS: x86-avoid-trailing-call
+; O0-WINDOWS: CFGuardLongjmpPass
+; O0-WINDOWS: verify)
+; O0-WINDOWS: free-machine-function)
+
+; O3-WINDOWS: require<MachineModuleAnalysis>
+; O3-WINDOWS: require<profile-summary>
+; O3-WINDOWS: require<collector-metadata>
+; O3-WINDOWS: require<runtime-libcall-info>
+; O3-WINDOWS: pre-isel-intrinsic-lowering
+; O3-WINDOWS: function(expand-ir-insts<O3>
+; O3-WINDOWS: atomic-expand
+; O3-WINDOWS: x86-lower-amx-intrinsics
+; O3-WINDOWS: x86-lower-amx-type
+; O3-WINDOWS: verify
+; O3-WINDOWS: loop-mssa(canon-freeze
+; O3-WINDOWS: loop-reduce)
+; O3-WINDOWS: mergeicmps
+; O3-WINDOWS: expand-memcmp
+; O3-WINDOWS: gc-lowering)
+; O3-WINDOWS: shadow-stack-gc-lowering
+; O3-WINDOWS: function(unreachableblockelim
+; O3-WINDOWS: consthoist
+; O3-WINDOWS: replace-with-veclib
+; O3-WINDOWS: partially-inline-libcalls
+; O3-WINDOWS: ee-instrument<post-inline>
+; O3-WINDOWS: scalarize-masked-mem-intrin
+; O3-WINDOWS: expand-reductions
+; O3-WINDOWS: interleaved-access
+; O3-WINDOWS: x86-partial-reduction
+; O3-WINDOWS: indirectbr-expand
+; O3-WINDOWS: cfguard
+; O3-WINDOWS: codegenprepare
+; O3-WINDOWS: win-eh-prepare
+; O3-WINDOWS: dwarf-eh-prepare
+; O3-WINDOWS: objc-arc-contract
+; O3-WINDOWS: callbr-prepare
+; O3-WINDOWS: safe-stack
+; O3-WINDOWS: stack-protector
+; O3-WINDOWS: verify)
+; O3-WINDOWS: function(machine-function(x86-isel
+; O3-WINDOWS: x86-argument-stack-slot
+; O3-WINDOWS: finalize-isel
+; O3-WINDOWS: early-tailduplication
+; O3-WINDOWS: opt-phis
+; O3-WINDOWS: stack-coloring
+; O3-WINDOWS: localstackalloc
+; O3-WINDOWS: dead-mi-elimination
+; O3-WINDOWS: early-ifcvt
+; O3-WINDOWS: x86-cmov-conversion
+; O3-WINDOWS: early-machinelicm
+; O3-WINDOWS: machine-cse
+; O3-WINDOWS: machine-sink
+; O3-WINDOWS: peephole-opt
+; O3-WINDOWS: dead-mi-elimination
+; O3-WINDOWS: LiveRangeShrinkPass
+; O3-WINDOWS: x86-fixup-setcc
+; O3-WINDOWS: x86-cf-opt
+; O3-WINDOWS: x86-avoid-sfb
+; O3-WINDOWS: x86-suppress-apx-for-relocation
+; O3-WINDOWS: x86-slh
+; O3-WINDOWS: x86-flags-copy-lowering
+; O3-WINDOWS: x86-dyn-alloca-expander
+; O3-WINDOWS: x86-pre-tile-config
+; O3-WINDOWS: detect-dead-lanes
+; O3-WINDOWS: init-undef
+; O3-WINDOWS: process-imp-defs
+; O3-WINDOWS: unreachable-mbb-elimination
+; O3-WINDOWS: require<live-vars>
+; O3-WINDOWS: require<machine-loops>
+; O3-WINDOWS: phi-node-elimination
+; O3-WINDOWS: two-address-instruction
+; O3-WINDOWS: register-coalescer
+; O3-WINDOWS: rename-independent-subregs
+; O3-WINDOWS: machine-scheduler
+; O3-WINDOWS: greedy<all>
+; O3-WINDOWS: virt-reg-rewriter
+; O3-WINDOWS: stack-slot-coloring
+; O3-WINDOWS: stack-slot-coloring
+; O3-WINDOWS: machine-cp
+; O3-WINDOWS: machinelicm
+; O3-WINDOWS: x86-lower-tile-copy
+; O3-WINDOWS: x86-fp-stackifier
+; O3-WINDOWS: remove-redundant-debug-values
+; O3-WINDOWS: fixup-statepoint-caller-saved
+; O3-WINDOWS: postra-machine-sink
+; O3-WINDOWS: shrink-wrap
+; O3-WINDOWS: prolog-epilog
+; O3-WINDOWS: machine-latecleanup
+; O3-WINDOWS: branch-folder
+; O3-WINDOWS: tailduplication
+; O3-WINDOWS: machine-cp
+; O3-WINDOWS: post-ra-pseudos
+; O3-WINDOWS: x86-expand-pseudo
+; O3-WINDOWS: post-RA-sched
+; O3-WINDOWS: block-placement
+; O3-WINDOWS: fentry-insert
+; O3-WINDOWS: xray-instrumentation
+; O3-WINDOWS: patchable-function
+; O3-WINDOWS: BreakFalseDepsPass
+; O3-WINDOWS: x86-fixup-bw-insts
+; O3-WINDOWS: x86-fixup-leas
+; O3-WINDOWS: x86-fixup-inst-tuning
+; O3-WINDOWS: x86-fixup-inst-tuning
+; O3-WINDOWS: x86-compress-evex
+; O3-WINDOWS: FuncletLayoutPass
+; O3-WINDOWS: remove-loads-into-fake-uses
+; O3-WINDOWS: StackMapLivenessPass
+; O3-WINDOWS: live-debug-values<emit-debug-entry-values>
+; O3-WINDOWS: machine-sanmd
+; O3-WINDOWS: stack-frame-layout
+; O3-WINDOWS: x86-avoid-trailing-call
+; O3-WINDOWS: CFGuardLongjmpPass
+; O3-WINDOWS: verify)
+; O3-WINDOWS: free-machine-function)

commit 47082321362a10f1256cc7f2f6d11b478ea64a44

Fix build breakage caused by #176061
https://github.com/llvm/llvm-project/pull/176061 added an extern
declaration to `EnableMachineCombinerPass`, which is `static`, causing
missing symbol errors in the debug build. This PR fixes this by making
that symbol non-static. Furthermore, this PR renames that symbol to
`EnableMachineCombinerPassX86` as `EnableMachineCombinerPass` exists in
other places.

llvm/lib/Target/X86/X86CodeGenPassBuilder.cpp
@@ -27,7 +27,7 @@
 
 using namespace llvm;
 
-extern cl::opt<bool> EnableMachineCombinerPass;
+extern cl::opt<bool> EnableMachineCombinerPassX86;
 
 namespace {
 
@@ -117,7 +117,7 @@ Error X86CodeGenPassBuilder::addInstSelector(PassManagerWrapper &PMW) const {
 
 void X86CodeGenPassBuilder::addILPOpts(PassManagerWrapper &PMW) const {
   addMachineFunctionPass(EarlyIfConverterPass(), PMW);
-  if (EnableMachineCombinerPass) {
+  if (EnableMachineCombinerPassX86) {
     // TODO(boomanaiden154): Add the MachineCombinerPass here once it has been
     // ported to the new pass manager.
   }

llvm/lib/Target/X86/X86TargetMachine.cpp
@@ -53,7 +53,7 @@
 
 using namespace llvm;
 
-static cl::opt<bool> EnableMachineCombinerPass("x86-machine-combiner",
+cl::opt<bool> EnableMachineCombinerPassX86("x86-machine-combiner",
                                cl::desc("Enable the machine combiner pass"),
                                cl::init(true), cl::Hidden);
 
@@ -497,7 +497,7 @@ void X86PassConfig::addPreLegalizeMachineIR() {
 
 bool X86PassConfig::addILPOpts() {
   addPass(&EarlyIfConverterLegacyID);
-  if (EnableMachineCombinerPass)
+  if (EnableMachineCombinerPassX86)
     addPass(&MachineCombinerID);
   addPass(createX86CmovConversionLegacyPass());
   return true;