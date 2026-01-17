f878bda30191613347205d611dfe590cd096ca26
crisiumnih <fasfasmag@proton.me>
2026-01-17 22:05:04 +0530

    [libc][math] Refactor f16fmal to header-only

--- libc/src/math/generic/f16fmal.cpp
+++ libc/src/math/generic/f16fmal.cpp
@@ -7,15 +7,13 @@
 //===----------------------------------------------------------------------===//
 
 #include "src/math/f16fmal.h"
-#include "src/__support/FPUtil/FMA.h"
-#include "src/__support/common.h"
-#include "src/__support/macros/config.h"
+#include "src/__support/math/f16fmal.h"
 
 namespace LIBC_NAMESPACE_DECL {
 
 LLVM_LIBC_FUNCTION(float16, f16fmal,
                    (long double x, long double y, long double z)) {
-  return fputil::fma<float16>(x, y, z);
+  return math::f16fmal(x, y, z);
 }
 
 } // namespace LIBC_NAMESPACE_DECL
