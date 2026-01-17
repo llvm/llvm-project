bf71aab89f372461e3f85f571ab82dfd8dfd9b2e
denzor200 <denismikhaylov38@gmail.com>
2026-01-17 16:30:28 +0300

    lint

--- clang-tools-extra/clang-tidy/misc/BoolBitwiseOperationCheck.cpp
+++ clang-tools-extra/clang-tidy/misc/BoolBitwiseOperationCheck.cpp
@@ -88,7 +88,7 @@ AST_MATCHER_P(Expr, hasAllLeavesOfBitwiseSatisfying,
   auto Condition = [&](const clang::Expr *E) -> bool {
     return InnerMatcher.matches(*E, Finder, Builder);
   };
-  return leavesOfBitwiseSatisfy(&Node, Condition, std::logical_and<bool>());
+  return leavesOfBitwiseSatisfy(&Node, Condition, std::logical_and{});
 }
 
 /// Custom matcher that checks if any leaf node in a bitwise expression
@@ -102,7 +102,7 @@ AST_MATCHER_P(Expr, hasAnyLeafOfBitwiseSatisfying,
   auto Condition = [&](const clang::Expr *E) -> bool {
     return InnerMatcher.matches(*E, Finder, Builder);
   };
-  return leavesOfBitwiseSatisfy(&Node, Condition, std::logical_or<bool>());
+  return leavesOfBitwiseSatisfy(&Node, Condition, std::logical_or{});
 }
 } // namespace
 
