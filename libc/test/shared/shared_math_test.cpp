fca286f3d709eddb0444c394470943f8e10edb3e
Dasha Buka <dvbuka@proton.me>
2026-01-16 21:27:18 -0800

    Add shared test

--- libc/test/shared/shared_math_test.cpp
+++ libc/test/shared/shared_math_test.cpp
@@ -94,6 +94,7 @@ TEST(LlvmLibcSharedMathTest, AllDouble) {
   EXPECT_FP_EQ(0x0p+0, LIBC_NAMESPACE::shared::expm1(0.0));
   EXPECT_FP_EQ(0x0p+0, LIBC_NAMESPACE::shared::log(1.0));
   EXPECT_FP_EQ(0.0, LIBC_NAMESPACE::shared::sin(0.0));
+  EXPECT_EQ(0, LIBC_NAMESPACE::shared::ilogb(0.0));
 }
 
 #ifdef LIBC_TYPES_HAS_FLOAT128
