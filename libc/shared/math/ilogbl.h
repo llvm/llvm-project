6397207d613d003f6e6bc7f7ed06fba98a872d90
Anonmiraj <nabilmalek48@gmail.com>
2026-01-17 02:17:30 +0200

    [libc][math] Refactor ilogbl to Header Only. (#176500)
    builds with both Clang and GCC 12.2.
    
    Closes https://github.com/llvm/llvm-project/issues/175349.

--- libc/shared/math/ilogbl.h
+++ libc/shared/math/ilogbl.h
@@ -1,4 +1,4 @@
-//===-- Implementation of ilogbl function ---------------------------------===//
+//===-- Shared ilogbl function ----------------------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,11 +6,18 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "src/math/ilogbl.h"
+#ifndef LLVM_LIBC_SHARED_MATH_ILOGBL_H
+#define LLVM_LIBC_SHARED_MATH_ILOGBL_H
+
+#include "shared/libc_common.h"
 #include "src/__support/math/ilogbl.h"
 
 namespace LIBC_NAMESPACE_DECL {
+namespace shared {
 
-LLVM_LIBC_FUNCTION(int, ilogbl, (long double x)) { return math::ilogbl(x); }
+using math::ilogbl;
 
+} // namespace shared
 } // namespace LIBC_NAMESPACE_DECL
+
+#endif // LLVM_LIBC_SHARED_MATH_ILOGBL_H
