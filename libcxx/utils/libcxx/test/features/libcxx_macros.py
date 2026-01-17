a23b29e398d88dac0536e2b8d45238862a5afc8d
William Tran-Viet <wtranviet@proton.me>
2026-01-17 17:17:41 -0500

    Formatting 2

--- libcxx/utils/libcxx/test/features/libcxx_macros.py
+++ libcxx/utils/libcxx/test/features/libcxx_macros.py
@@ -32,7 +32,7 @@ macros = {
     "_LIBCPP_DEPRECATED_ABI_DISABLE_PAIR_TRIVIAL_COPY_CTOR": "libcpp-deprecated-abi-disable-pair-trivial-copy-ctor",
     "_LIBCPP_ABI_NO_COMPRESSED_PAIR_PADDING": "libcpp-abi-no-compressed-pair-padding",
     "_LIBCPP_PSTL_BACKEND_LIBDISPATCH": "libcpp-pstl-backend-libdispatch",
-    "_LIBCPP_ABI_BOUNDED_ITERATORS_IN_OPTIONAL" : "libcpp-has-abi-bounded-iterators-in-optional",
+    "_LIBCPP_ABI_BOUNDED_ITERATORS_IN_OPTIONAL": "libcpp-has-abi-bounded-iterators-in-optional",
 }
 for macro, feature in macros.items():
     features.append(
