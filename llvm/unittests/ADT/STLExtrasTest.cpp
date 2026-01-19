b298b01bcde917057d2ef0c7bca685d3ff055ba2
BStott <Benjamin.Stott@sony.com>
2026-01-19 16:05:35 +0000

    Surround test assertions in a test function

--- llvm/unittests/ADT/STLExtrasTest.cpp
+++ llvm/unittests/ADT/STLExtrasTest.cpp
@@ -1777,37 +1777,43 @@ TEST(STLExtrasTest, ReverseConditionally) {
 struct Foo;
 struct Bar {};
 
-static_assert(is_incomplete_v<Foo>, "Foo is incomplete");
-static_assert(!is_incomplete_v<Bar>, "Bar is defined");
+TEST(STLExtrasTest, HasEqualityComparison) {
+  static_assert(is_incomplete_v<Foo>, "Foo is incomplete");
+  static_assert(!is_incomplete_v<Bar>, "Bar is defined");
 
-struct NoEqualityComparison {};
-static_assert(!has_equality_comparison_v<NoEqualityComparison>);
+  struct NoEqualityComparison {};
+  static_assert(!has_equality_comparison_v<NoEqualityComparison>);
 
-// Mutating equality comparison doesn't count.
-struct MutatingEqualityComparison {
-  bool operator==(MutatingEqualityComparison &Other) { return false; }
-};
-static_assert(!has_equality_comparison_v<MutatingEqualityComparison>);
+  // Mutating equality comparison doesn't count.
+  struct MutatingEqualityComparison {
+    bool operator==(MutatingEqualityComparison &Other) { return false; }
+  };
+  static_assert(!has_equality_comparison_v<MutatingEqualityComparison>);
 
-struct PrivateEqualityComparison {
-private:
-  bool operator==(const PrivateEqualityComparison &Other) const {
-    return false;
-  }
-};
-static_assert(!has_equality_comparison_v<PrivateEqualityComparison>);
+  struct PrivateEqualityComparison {
+  private:
+    bool operator==(const PrivateEqualityComparison &Other) const {
+      return false;
+    }
+  };
+  static_assert(!has_equality_comparison_v<PrivateEqualityComparison>);
 
-struct PublicEqualityComparison {
-  bool operator==(const PublicEqualityComparison &Other) const { return false; }
-};
-static_assert(has_equality_comparison_v<PublicEqualityComparison>);
+  struct PublicEqualityComparison {
+    bool operator==(const PublicEqualityComparison &Other) const {
+      return false;
+    }
+  };
+  static_assert(has_equality_comparison_v<PublicEqualityComparison>);
 
-struct StructA {};
-struct StructB {
-  bool operator==(const StructA &Other) const { return false; }
-};
-static_assert(!has_equality_comparison_v<StructA, StructB>);
-static_assert(has_equality_comparison_v<StructB, StructA>);
+  struct StructA {};
+  struct StructB {
+    bool operator==(const StructA &Other) const { return false; }
+  };
+  static_assert(!has_equality_comparison_v<StructA, StructB>);
+  static_assert(has_equality_comparison_v<StructB, StructA>);
+
+  SUCCEED();
+}
 
 TEST(STLExtrasTest, Search) {
   // Test finding a subsequence in the middle.
