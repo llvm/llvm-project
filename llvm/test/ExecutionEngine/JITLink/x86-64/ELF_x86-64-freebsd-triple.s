41e968c418a4fa64ed23b59b6020e9cdcd9f189d
Lang Hames <lhames@gmail.com>
2026-01-20 08:37:44 +1100

    Try double quotes to avoid glob expansion

--- llvm/test/ExecutionEngine/JITLink/x86-64/ELF_x86-64-freebsd-triple.s
+++ llvm/test/ExecutionEngine/JITLink/x86-64/ELF_x86-64-freebsd-triple.s
@@ -1,6 +1,6 @@
 # RUN: llvm-mc -triple=x86_64-unknown-freebsd -position-independent \
 # RUN:     -filetype=obj -o %t.o %s
-# RUN: llvm-jitlink -noexec -show-graphs '.*' %t.o | FileCheck %s
+# RUN: llvm-jitlink -noexec -show-graphs ".*" %t.o | FileCheck %s
 
 # Make sure that the LinkGraph triple correctly reports FreeBSD (tests
 # ba6a59c8750).
