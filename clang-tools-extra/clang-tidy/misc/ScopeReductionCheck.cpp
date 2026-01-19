ce93a8448d93a4ea55efb072c6dfef251b2404b2
Vince Bridgers <vince.a.bridgers@ericsson.com>
2026-01-19 13:36:23 +0100

    Update
    * Fix clang-formatting issue

--- clang-tools-extra/clang-tidy/misc/ScopeReductionCheck.cpp
+++ clang-tools-extra/clang-tidy/misc/ScopeReductionCheck.cpp
@@ -69,13 +69,12 @@ collectVariableUses(const clang::Stmt *S, const clang::VarDecl *Var,
 
 void ScopeReductionCheck::registerMatchers(MatchFinder *Finder) {
   Finder->addMatcher(
-      varDecl(hasLocalStorage(),
-              unless(hasGlobalStorage()),
-              hasAncestor(functionDecl()),
-              unless(parmVarDecl()),
+      varDecl(hasLocalStorage(), unless(hasGlobalStorage()),
+              hasAncestor(functionDecl()), unless(parmVarDecl()),
               unless(hasParent(declStmt(hasParent(forStmt())))),
               unless(hasParent(declStmt(hasParent(cxxForRangeStmt())))),
-              unless(hasInitializer(anyOf(callExpr(), cxxMemberCallExpr(), cxxOperatorCallExpr()))))
+              unless(hasInitializer(anyOf(callExpr(), cxxMemberCallExpr(),
+                                          cxxOperatorCallExpr()))))
           .bind("var"),
       this);
 }
