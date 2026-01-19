4bd07fdac84e16b9a12e9612f6a39e4acedde1e8
Fine, Gregory <gregory.fine@intel.com>
2026-01-19 14:57:42 -0800

    Applied different work around for a build break

--- clang/lib/Headers/spirvintrin.h
+++ clang/lib/Headers/spirvintrin.h
@@ -22,7 +22,7 @@ _Pragma("omp begin declare variant match(device = {arch(spirv64)})");
 
 // Type aliases to the address spaces used by the SPIR-V backend.
 #define __gpu_private __attribute__((address_space(0)))
-#define __gpu_constant /*__attribute__((address_space(2)))*/
+#define __gpu_constant __attribute__((address_space(2)))
 #define __gpu_local __attribute__((address_space(3)))
 #define __gpu_global __attribute__((address_space(1)))
 #define __gpu_generic __attribute__((address_space(4)))
