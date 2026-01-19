1c7044c29d162fcce231743e4a0ea84839d6aab9
Dhruv <62135445+dhr412@users.noreply.github.com>
2026-01-19 21:37:03 +0530

    [libc][math] Fix comment alignment

--- libc/shared/math/logb.h
+++ libc/shared/math/logb.h
@@ -1,4 +1,4 @@
-//===-- Shared logb function --------------------------------*- C++ -*-===//
+//===-- Shared logb function ------------------------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
