dee420651dc4592938c36358b40f186478b66102
Ramshankar07 <picographer0214@gmail.com>
2026-01-19 11:53:51 -0500

    Fixing code formatter issue

--- libc/shared/math/ilogbf128.h
+++ libc/shared/math/ilogbf128.h
@@ -13,6 +13,7 @@
 
 #ifdef LIBC_TYPES_HAS_FLOAT128
 
+#include "shared/libc_common.h"
 #include "src/__support/math/ilogbf128.h"
 
 namespace LIBC_NAMESPACE_DECL {
@@ -25,4 +26,4 @@ using math::ilogbf128;
 
 #endif // LIBC_TYPES_HAS_FLOAT128
 
-#endif // LLVM_LIBC_SHARED_MATH_ILogBF128_H
+#endif // LLVM_LIBC_SHARED_MATH_ILOGBF128_H
