a83a4127895b2d9de387d9d247ab1dc174ad96ae
Matt Arsenault <Matthew.Arsenault@amd.com>
2026-01-20 20:52:07 +0100

    Apply suggestion from @cdevadas
    Co-authored-by: Christudasan Devadasan <christudasan.devadasan@amd.com>

--- llvm/lib/Target/AMDGPU/SIISelLowering.cpp
+++ llvm/lib/Target/AMDGPU/SIISelLowering.cpp
@@ -2307,9 +2307,9 @@ SDValue SITargetLowering::convertArgType(SelectionDAG &DAG, EVT VT, EVT MemVT,
   }
 
   if (MemVT.isFloatingPoint()) {
-    if (VT.isFloatingPoint())
+    if (VT.isFloatingPoint()) {
       Val = getFPExtOrFPRound(DAG, Val, SL, VT);
-    else {
+    }  else {
       assert(!MemVT.isVector());
       EVT IntVT = EVT::getIntegerVT(*DAG.getContext(), MemVT.getSizeInBits());
       SDValue Cast = DAG.getBitcast(IntVT, Val);
