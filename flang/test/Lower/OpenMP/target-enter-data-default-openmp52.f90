d41d83b55806566eda807e9ea3a30e243dc078dc
agozillon <Andrew.Gozillon@amd.com>
2026-01-20 12:53:38 -0600

    [Flang][MLIR][OpenMP] Add distinct var_ptr_ptr_type to omp.map.info operations & remove ref_ptr_ptee
    This is a precursor patch to attach and ref_ptr/ptee mapping that I intend to upstream
    over the next few weeks. The attach maps require both the type of the descriptor and
    the pointed to data to calculate the appropriate offload/base pointers and size. In
    the base case of ref_ptr_ptee all of this information can be gathered from the pointer
    and pointee maps, but in cases where we have only one (i.e. ref_ptr/ref_ptee) we will
    be missing one of the key elements required to create an corresponding attach map.
    
    So, this PR basically adds the ability to ferry around the type of both var_ptr and
    var_ptr_ptr as opposed to just var_ptr, then we can emit attach maps as seperate
    map.info's that carry all the pre-requisite informaion for lowering to LLVM-IR. But,
    otherwise it seems reasonable to have var_ptr_ptr mirror var_ptr in all aspects for
    consistency.
    
    It also removes ref_ptr_ptee, instead opting to use the setting of both ref_ptr and
    ref_ptee to mean ref_ptr_ptee.

--- flang/test/Lower/OpenMP/target-enter-data-default-openmp52.f90
+++ flang/test/Lower/OpenMP/target-enter-data-default-openmp52.f90
@@ -10,7 +10,7 @@ contains
   subroutine initialize()
   allocate(A)
   !$omp target enter data map(A)
-  !CHECK-52: omp.map.info var_ptr(%2 : !fir.ref<!fir.box<!fir.heap<f32>>>, f32) map_clauses(to) capture(ByRef) var_ptr_ptr(%5 : !fir.llvm_ptr<!fir.ref<f32>>) -> !fir.llvm_ptr<!fir.ref<f32>> {name = ""}
+  !CHECK-52: omp.map.info var_ptr(%2 : !fir.ref<!fir.box<!fir.heap<f32>>>, !fir.box<!fir.heap<f32>>) map_clauses(to) capture(ByRef) var_ptr_ptr(%5 : !fir.llvm_ptr<!fir.ref<f32>>, f32) -> !fir.llvm_ptr<!fir.ref<f32>> {name = ""}
   !CHECK-52: omp.map.info var_ptr(%2 : !fir.ref<!fir.box<!fir.heap<f32>>>, !fir.box<!fir.heap<f32>>) map_clauses(always, to) capture(ByRef) members(%6 : [0] : !fir.llvm_ptr<!fir.ref<f32>>) -> !fir.ref<!fir.box<!fir.heap<f32>>> {name = "a"}
   !CHECK-51: to and alloc map types are permitted
 
@@ -18,7 +18,7 @@ contains
 
   subroutine finalize()
   !$omp target exit data map(A)
-  !CHECK-52: omp.map.info var_ptr(%2 : !fir.ref<!fir.box<!fir.heap<f32>>>, f32) map_clauses(from) capture(ByRef) var_ptr_ptr(%3 : !fir.llvm_ptr<!fir.ref<f32>>) -> !fir.llvm_ptr<!fir.ref<f32>> {name = ""}
+  !CHECK-52: omp.map.info var_ptr(%2 : !fir.ref<!fir.box<!fir.heap<f32>>>, !fir.box<!fir.heap<f32>>) map_clauses(from) capture(ByRef) var_ptr_ptr(%3 : !fir.llvm_ptr<!fir.ref<f32>>, f32) -> !fir.llvm_ptr<!fir.ref<f32>> {name = ""}
   !CHECK-52: omp.map.info var_ptr(%2 : !fir.ref<!fir.box<!fir.heap<f32>>>, !fir.box<!fir.heap<f32>>) map_clauses(from) capture(ByRef) members(%4 : [0] : !fir.llvm_ptr<!fir.ref<f32>>) -> !fir.ref<!fir.box<!fir.heap<f32>>> {name = "a"}
   !CHECK-51: from, release and delete map types are permitted
   deallocate(A)
