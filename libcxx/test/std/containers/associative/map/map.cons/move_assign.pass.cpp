b148bb55fbd0de4a450e55153520de1ae2b4adf2
Vinay Deshmukh <vinay_deshmukh@outlook.com>
2026-01-20 22:20:16 +0000

    attempt/run CI for checking _LIBCPP_ASSERT

--- libcxx/test/std/containers/associative/map/map.cons/move_assign.pass.cpp
+++ libcxx/test/std/containers/associative/map/map.cons/move_assign.pass.cpp
@@ -103,8 +103,8 @@ bool test() {
 int main(int, char**) {
   test();
 #if TEST_STD_VER >= 26
-// FIXME: Within __tree, it is not allowed to move from a `const MoveOnly` which prevents this from executing during constant evaluation
-//  static_assert(test());
+  // FIXME: Within __tree, it is not allowed to move from a `const MoveOnly` which prevents this from executing during constant evaluation
+  static_assert(test());
 #endif
   return 0;
 }
