f0e766cc003929a06ed3641d9023b0fae58fa8df
Kiran Chandramohan <kiran.chandramohan@arm.com>
2026-01-21 21:09:25 +0000

    Address review comments : piping test, new line and using clang_group

--- clang/lib/Driver/ToolChains/Flang.cpp
+++ clang/lib/Driver/ToolChains/Flang.cpp
@@ -1141,7 +1141,6 @@ void Flang::ConstructJob(Compilation &C, const JobAction &JA,
   }
 
   renderGlobalISelOptions(D, Args, CmdArgs, Triple);
-
   renderCommonIntegerOverflowOptions(Args, CmdArgs);
 
   assert((Output.isFilename() || Output.isNothing()) && "Invalid output.");
