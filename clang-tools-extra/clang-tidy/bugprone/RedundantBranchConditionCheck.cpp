88ca987bfffe89ae69dfa2285d4fcf05050ae19c
mtx <mitchell.xu2@gmail.com>
2026-01-21 18:30:51 +0800

    Fix bugprone-redundant-branch-condition check

--- clang-tools-extra/clang-tidy/bugprone/RedundantBranchConditionCheck.cpp
+++ clang-tools-extra/clang-tidy/bugprone/RedundantBranchConditionCheck.cpp
@@ -135,15 +135,16 @@ void RedundantBranchConditionCheck::check(
     if (OtherSide && OtherSide->HasSideEffects(*Result.Context)) {
       const SourceLocation BeforeOtherSide =
           OtherSide->getBeginLoc().getLocWithOffset(-1);
-      const SourceLocation AfterOtherSide =
-          utils::lexer::findNextTokenSkippingComments(
-              OtherSide->getEndLoc(), *Result.SourceManager, getLangOpts())
-              ->getLocation();
-      Diag << FixItHint::CreateRemoval(
-                  CharSourceRange::getTokenRange(IfBegin, BeforeOtherSide))
-           << FixItHint::CreateInsertion(AfterOtherSide, ";")
-           << FixItHint::CreateRemoval(
-                  CharSourceRange::getTokenRange(AfterOtherSide, IfEnd));
+      const auto NextToken = utils::lexer::findNextTokenSkippingComments(
+          OtherSide->getEndLoc(), *Result.SourceManager, getLangOpts());
+      if (NextToken) {
+        const SourceLocation AfterOtherSide = NextToken->getLocation();
+        Diag << FixItHint::CreateRemoval(
+                    CharSourceRange::getTokenRange(IfBegin, BeforeOtherSide))
+             << FixItHint::CreateInsertion(AfterOtherSide, ";")
+             << FixItHint::CreateRemoval(
+                    CharSourceRange::getTokenRange(AfterOtherSide, IfEnd));
+      }
     } else {
       Diag << FixItHint::CreateRemoval(
           CharSourceRange::getTokenRange(IfBegin, IfEnd));
@@ -167,13 +168,13 @@ void RedundantBranchConditionCheck::check(
       Diag << FixItHint::CreateRemoval(CharSourceRange::getTokenRange(
           CondOp->getLHS()->getBeginLoc(), BeforeRHS));
     } else {
-      const SourceLocation AfterLHS =
-          utils::lexer::findNextTokenSkippingComments(
-              CondOp->getLHS()->getEndLoc(), *Result.SourceManager,
-              getLangOpts())
-              ->getLocation();
-      Diag << FixItHint::CreateRemoval(CharSourceRange::getTokenRange(
-          AfterLHS, CondOp->getRHS()->getEndLoc()));
+      const auto NextToken = utils::lexer::findNextTokenSkippingComments(
+          CondOp->getLHS()->getEndLoc(), *Result.SourceManager, getLangOpts());
+      if (NextToken) {
+        const SourceLocation AfterLHS = NextToken->getLocation();
+        Diag << FixItHint::CreateRemoval(CharSourceRange::getTokenRange(
+            AfterLHS, CondOp->getRHS()->getEndLoc()));
+      }
     }
   }
 }
