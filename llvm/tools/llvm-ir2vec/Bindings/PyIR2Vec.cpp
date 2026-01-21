6e677f11fe845df3df9f5709737413cff2ef4d84
nishant-sachdeva <nishant.sachdeva@research.iiit.ac.in>
2026-01-21 11:20:48 +0530

    nit commit , PyIR2Vec.cpp doc fixup

--- llvm/tools/llvm-ir2vec/Bindings/PyIR2Vec.cpp
+++ llvm/tools/llvm-ir2vec/Bindings/PyIR2Vec.cpp
@@ -1,4 +1,4 @@
-//===- ir2vec_bindings.cpp - Python Bindings for IR2Vec ------------------===//
+//===- PyIR2Vec.cpp - Python Bindings for IR2Vec ------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
