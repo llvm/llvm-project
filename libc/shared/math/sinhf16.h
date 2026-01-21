41e79a5cdb756a64f82f55be98b53da92359d83e
Anikesh Parashar <anikeshparashar@gmail.com>
2026-01-21 17:18:45 +0530

    Refractor sinhf16 ##147386

--- libc/shared/math/sinhf16.h
+++ libc/shared/math/sinhf16.h
@@ -1,4 +1,5 @@
-//===-- Half-precision sinh(x) function -----------------------------------===//
+//===-- Shared sinhf16 function ------------------------------------*- C++
+//-*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,11 +7,18 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "src/math/sinhf16.h"
+#ifndef LLVM_LIBC_SHARED_MATH_SINHF16_H
+#define LLVM_LIBC_SHARED_MATH_SINHF16_H
+
+#include "shared/libc_common.h"
 #include "src/__support/math/sinhf16.h"
 
 namespace LIBC_NAMESPACE_DECL {
 
-LLVM_LIBC_FUNCTION(float16, sinhf16, (float16 x)) { return math::sinhf16(x); }
+namespace shared {
+using math::sinhf16;
+} // namespace shared
 
 } // namespace LIBC_NAMESPACE_DECL
+
+#endif // LLVM_LIBC_SHARED_MATH_SINHF16_H
