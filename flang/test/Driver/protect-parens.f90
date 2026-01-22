a42027f6e74593ab1fda21aae66b596c2c263f1d
Eugene Epshteyn <eepshteyn@nvidia.com>
2026-01-22 17:18:13 -0500

    Apply suggestions from code review
    Simplify Driver/protect-parens.f90
    
    Co-authored-by: Tarun Prabhu <tarunprabhu@gmail.com>

--- flang/test/Driver/protect-parens.f90
+++ flang/test/Driver/protect-parens.f90
@@ -1,8 +1,8 @@
 ! RUN: %flang -fprotect-parens -### %s -o %t 2>&1 | FileCheck %s -check-prefix=PROTECT
 ! RUN: %flang -fno-protect-parens -### %s -o %t 2>&1 | FileCheck %s -check-prefix=NO-PROTECT
-! RUN: %flang -### %s -o %t 2>&1 | FileCheck %s -check-prefix=DEFAULT
-! RUN: %flang -Ofast -### %s -o %t 2>&1 | FileCheck %s -check-prefix=OFAST
-! RUN: %flang -Ofast -fprotect-parens -### %s -o %t 2>&1 | FileCheck %s -check-prefix=OFAST-PROTECT
+! RUN: %flang -### %s -o %t 2>&1 | FileCheck %s -check-prefix=NO-PROTECT
+! RUN: %flang -Ofast -### %s -o %t 2>&1 | FileCheck %s -check-prefix=NO-PROTECT
+! RUN: %flang -Ofast -fprotect-parens -### %s -o %t 2>&1 | FileCheck %s -check-prefix=PROTECT
 
 ! Note: -fprotect-parens is not passed to the frontend, because it's the
 ! default. Only -fno-protect-parens is passed to turn off the default.
