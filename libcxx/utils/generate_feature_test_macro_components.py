e4730e5af4f4b967abd3f8a94200a97269d1272a
eiytoq <eiytoq@outlook.com>
2026-01-22 17:06:32 +0800

    Implement P3383R3 mdspan.at()

--- libcxx/utils/generate_feature_test_macro_components.py
+++ libcxx/utils/generate_feature_test_macro_components.py
@@ -949,7 +949,7 @@ feature_test_macros = [
             "name": "__cpp_lib_mdspan",
             "values": {
                 "c++23": 202207,
-                "c++26": 202406,  # P2389R2 dextents Index Type Parameter
+                "c++26": 202506,  # P3383R3 mdspan.at()
             },
             "headers": ["mdspan"],
         },
