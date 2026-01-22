7b5adeb2e31c5ef10eaf31fff75a73caf9c20671
Jonas Devlieghere <jonas@devlieghere.com>
2026-01-21 17:31:45 -0800

    [lldb] Align command and settings in apropos output
    Align the output of commands and settings in the apropos output. Right
    now, padding is added between the command and its description to match
    the longest setting, while settings are not padded at all. With this
    patch, both are padded by the same amount.
    
    Fixes #177284

--- lldb/source/Commands/CommandObjectApropos.cpp
+++ lldb/source/Commands/CommandObjectApropos.cpp
@@ -11,6 +11,7 @@
 #include "lldb/Interpreter/CommandReturnObject.h"
 #include "lldb/Interpreter/Property.h"
 #include "lldb/Utility/Args.h"
+#include "lldb/Utility/StreamString.h"
 
 using namespace lldb;
 using namespace lldb_private;
@@ -32,13 +33,27 @@ void CommandObjectApropos::DoExecute(Args &args, CommandReturnObject &result) {
   if (argc == 1) {
     auto search_word = args[0].ref();
     if (!search_word.empty()) {
-      // The bulk of the work must be done inside the Command Interpreter,
-      // since the command dictionary is private.
+      // Find all commands matching the search word and compute the max length.
       StringList commands_found;
       StringList commands_help;
 
       m_interpreter.FindCommandsForApropos(
           search_word, commands_found, commands_help, true, true, true, true);
+
+      size_t max_len = commands_found.GetMaxStringLength();
+
+      // Find all the properties matching the search word and compute the max
+      // length.
+      std::vector<const Property *> properties;
+      const size_t num_properties =
+          GetDebugger().Apropos(search_word, properties);
+      for (const Property *prop : properties) {
+        StreamString qualified_name;
+        prop->DumpQualifiedName(qualified_name);
+        max_len = std::max(max_len, qualified_name.GetString().size());
+      }
+
+      ReturnStatus return_status = eReturnStatusSuccessFinishNoResult;
 
       if (commands_found.GetSize() == 0) {
         result.AppendMessageWithFormat("No commands found pertaining to '%s'. "
@@ -46,32 +61,36 @@ void CommandObjectApropos::DoExecute(Args &args, CommandReturnObject &result) {
                                        "debugger commands.\n",
                                        args[0].c_str());
       } else {
-        if (commands_found.GetSize() > 0) {
-          result.AppendMessageWithFormat(
-              "The following commands may relate to '%s':\n", args[0].c_str());
-          const size_t max_len = commands_found.GetMaxStringLength();
-
-          for (size_t i = 0; i < commands_found.GetSize(); ++i)
-            m_interpreter.OutputFormattedHelpText(
-                result.GetOutputStream(), commands_found.GetStringAtIndex(i),
-                "--", commands_help.GetStringAtIndex(i), max_len);
-        }
+        result.AppendMessageWithFormat(
+            "The following commands may relate to '%s':\n", args[0].c_str());
+        for (size_t i = 0; i < commands_found.GetSize(); ++i)
+          m_interpreter.OutputFormattedHelpText(
+              result.GetOutputStream(), commands_found.GetStringAtIndex(i),
+              "--", commands_help.GetStringAtIndex(i), max_len);
+        return_status = eReturnStatusSuccessFinishResult;
       }
 
-      std::vector<const Property *> properties;
-      const size_t num_properties =
-          GetDebugger().Apropos(search_word, properties);
-      if (num_properties) {
-        const bool dump_qualified_name = true;
+      if (num_properties == 0) {
+        result.AppendMessageWithFormat(
+            "No settings found pertaining to '%s'. "
+            "Try 'settings show' to see a complete list of "
+            "debugger settings.\n",
+            args[0].c_str());
+
+      } else {
         result.AppendMessageWithFormatv(
             "\nThe following settings variables may relate to '{0}': \n\n",
-            args[0].ref());
+            search_word);
+
+        const bool dump_qualified_name = true;
         for (size_t i = 0; i < num_properties; ++i)
-          properties[i]->DumpDescription(
-              m_interpreter, result.GetOutputStream(), 0, dump_qualified_name);
+          properties[i]->DumpDescription(m_interpreter,
+                                         result.GetOutputStream(), max_len,
+                                         dump_qualified_name);
+        return_status = eReturnStatusSuccessFinishResult;
       }
 
-      result.SetStatus(eReturnStatusSuccessFinishNoResult);
+      result.SetStatus(return_status);
     } else {
       result.AppendError("'' is not a valid search word.\n");
     }
