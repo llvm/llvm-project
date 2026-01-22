e3fb3bd01360b5ab3ba4babdc9295256f34b3918
Riyaz Ahmad <riyaz.ahmad@ibm.com>
2026-01-22 02:43:18 -0500

    [Frontend][Sema] Address review comments.

--- clang/test/Sema/MicrosoftAnonymousStructs.c
+++ clang/test/Sema/MicrosoftAnonymousStructs.c
@@ -11,89 +11,43 @@
 
 typedef struct notnested {
   long bad1;
-  long bad2;
 } NOTNESTED;
-
 
 typedef struct nested1 {
   long a;
-  struct notnested var1;
   NOTNESTED var2;
 } NESTED1;
 
 struct nested2 {
   long b;
-  NESTED1;  // ms-anonymous-warning {{anonymous structs are a Microsoft extension}}
-            // ms-anonymous-dis-warning@-1 {{declaration does not declare anything}}
-};
-
-struct nested2 PR20573 = { .a = 3 };  // ms-anonymous-dis-error {{field designator 'a' does not refer to any field in type 'struct nested2'}}
-
-struct nested3 {
-  long d;
-  struct nested4 { // ms-anonymous-warning {{anonymous structs are a Microsoft extension}}
+  NESTED1;         // ms-anonymous-warning {{anonymous structs are a Microsoft extension}}
                    // ms-anonymous-dis-warning@-1 {{declaration does not declare anything}}
-    long e;
-  };
-  union nested5 { // ms-anonymous-warning {{anonymous unions are a Microsoft extension}}
-                  // ms-anonymous-dis-warning@-1 {{declaration does not declare anything}}
-    long f;
-  };
 };
 
-typedef union nested6 {
+typedef union nested3 {
   long f;
-} NESTED6;
+} NESTED3;
 
 struct test {
   int c;
   struct nested2;   // ms-anonymous-warning {{anonymous structs are a Microsoft extension}}
                     // ms-anonymous-dis-warning@-1 {{declaration does not declare anything}}
-  NESTED6;   // ms-anonymous-warning {{anonymous unions are a Microsoft extension}}
-            // ms-anonymous-dis-warning@-1 {{declaration does not declare anything}}
+  NESTED3;          // ms-anonymous-warning {{anonymous unions are a Microsoft extension}}
+                    // ms-anonymous-dis-warning@-1 {{declaration does not declare anything}}
+};
+
+struct nested4 {
+  long d;
+  struct nested5 { // ms-anonymous-warning {{anonymous structs are a Microsoft extension}}
+                   // ms-anonymous-dis-warning@-1 {{declaration does not declare anything}}
+    long e;
+  };
 };
 
 void foo(void)
 {
   struct test var;
-  var.a;      // ms-anonymous-dis-error {{no member named 'a' in 'struct test'}}
-  var.b;      // ms-anonymous-dis-error {{no member named 'b' in 'struct test'}}
   var.c;
-  var.bad1;   // ms-anonymous-error {{no member named 'bad1' in 'struct test'}}
-              // ms-anonymous-dis-error@-1 {{no member named 'bad1' in 'struct test'}}
-  var.bad2;   // ms-anonymous-error {{no member named 'bad2' in 'struct test'}}
-              // ms-anonymous-dis-error@-1 {{no member named 'bad2' in 'struct test'}}
+  var.b;          // ms-anonymous-dis-error {{no member named 'b' in 'struct test'}}
+  var.f;          // ms-anonymous-dis-error {{no member named 'f' in 'struct test'}}
 }
-
-// Enumeration types with a fixed underlying type.
-const int seventeen = 17;
-typedef int Int;
-
-void pointer_to_integral_type_conv(char* ptr) {
-  char ch = (char)ptr;
-  short sh = (short)ptr;
-  ch = (char)ptr;
-  sh = (short)ptr;
-
-  // This is valid ISO C.
-  _Bool b = (_Bool)ptr;
-}
-
-typedef struct {
-  UNKNOWN u; // ms-anonymous-error {{unknown type name 'UNKNOWN'}}
-             // ms-anonymous-dis-error@-1 {{unknown type name 'UNKNOWN'}}
-} AA;
-
-typedef struct {
-  AA; // ms-anonymous-warning {{anonymous structs are a Microsoft extension}}
-      // ms-anonymous-dis-warning@-1 {{declaration does not declare anything}}
-} BB;
-
-struct anon_fault {
-  struct undefined; // ms-anonymous-warning {{anonymous structs are a Microsoft extension}}
-                    // ms-anonymous-error@-1 {{field has incomplete type 'struct undefined'}}
-                    // ms-anonymous-note@-2 {{forward declaration of 'struct undefined'}}
-                    // ms-anonymous-dis-warning@-3 {{declaration does not declare anything}}
-};
-
-const int anon_falt_size = sizeof(struct anon_fault);
