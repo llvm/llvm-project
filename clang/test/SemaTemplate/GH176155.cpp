e0a77852d9e09559790a56e128a14d269e77009b
Krystian Stasiowski <sdkrystian@gmail.com>
2026-01-22 15:13:29 -0500

    [Clang][Sema] Only call PerformDependentDiagnostics for dependent contexts

---
+++ clang/test/SemaTemplate/GH176155.cpp
@@ -0,0 +1,21 @@
+// RUN: %clang_cc1 -std=c++20 -fsyntax-only -verify %s
+
+template <int> struct bad {
+  template <class T, auto =
+                         [] { // #lambda
+                           for (int i = 0; i < 100; ++i) { // #i
+                             // expected-error@-1 {{variable 'i' cannot be implicitly captured in a lambda with no capture-default specified}}
+                             // expected-note@#i {{'i' declared here}}
+                             // expected-note@#lambda {{lambda expression begins here}}
+                             // expected-error@-4 {{variable 'i' cannot be implicitly captured in a lambda with no capture-default specified}}
+                             // expected-note@#i {{'i' declared here}}
+                             // expected-note@#lambda {{lambda expression begins here}}
+                             struct LoopHelper {
+                               static constexpr void process() {}
+                             };
+                           }
+                         }>
+  static void f(T) {} // expected-note {{in instantiation of default argument for 'f<int>' required here}}
+};
+
+int main() { bad<0>::f(0); } // expected-note {{while substituting deduced template arguments into function template 'f'}}
