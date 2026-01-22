de8fdcacd5f72e7a4e33eefe1a567cd82fafc0c1
nataliakokoromyti <nataliakokoromyti@gmail.com>
2026-01-21 20:39:04 -0800

    rm whitespace

--- clang/lib/Sema/SemaStmtAsm.cpp
+++ clang/lib/Sema/SemaStmtAsm.cpp
@@ -241,7 +241,7 @@ ExprResult Sema::ActOnGCCAsmStmtString(Expr *Expr, bool ForAsmLabel) {
 
   if (auto *SL = dyn_cast<StringLiteral>(Expr)) {
     if (!SL->isOrdinary()) {
-      Diag(SL->getBeginLoc(), 
+      Diag(SL->getBeginLoc(),
            diag::err_asm_operand_wide_string_literal << (SL->isWide() ? 1 : 0));
       return ExprError();
     }
