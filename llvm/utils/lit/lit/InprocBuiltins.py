83a7fb275f6aa83993c9e870ea088b67d2ac7597
BStott <Benjamin.Stott@sony.com>
2026-01-22 11:13:02 +0000

    Apply formatting suggestions

--- llvm/utils/lit/lit/InprocBuiltins.py
+++ llvm/utils/lit/lit/InprocBuiltins.py
@@ -1,13 +1,22 @@
 import getopt
-import os, subprocess
-import stat
+import os
 import pathlib
 import platform
 import shutil
+import stat
+import subprocess
 from io import StringIO
 
-from lit.ShellEnvironment import expand_glob_expressions, InternalShellError, kIsWindows, processRedirects, ShellCommandResult, updateEnv
 import lit.util
+from lit.ShellEnvironment import (
+    InternalShellError,
+    ShellCommandResult,
+    expand_glob_expressions,
+    kIsWindows,
+    processRedirects,
+    updateEnv,
+)
+
 
 
 def executeBuiltinCd(cmd, shenv):
@@ -98,7 +107,7 @@ def executeBuiltinEcho(cmd, shenv):
     if write_newline:
         stdout.write("\n")
 
-    for (name, mode, f, path) in opened_files:
+    for name, mode, f, path in opened_files:
         f.close()
 
     output = "" if is_redirected else stdout.getvalue()
@@ -187,10 +196,17 @@ def executeBuiltinRm(cmd, cmd_shenv):
                     # NOTE: use ctypes to access `SHFileOperationsW` on Windows to
                     # use the NT style path to get access to long file paths which
                     # cannot be removed otherwise.
+                    from ctypes import (
+                        POINTER,
+                        Structure,
+                        WinError,
+                        addressof,
+                        byref,
+                        c_void_p,
+                        create_unicode_buffer,
+                        windll,
+                    )
                     from ctypes.wintypes import BOOL, HWND, LPCWSTR, UINT, WORD
-                    from ctypes import addressof, byref, c_void_p, create_unicode_buffer
-                    from ctypes import Structure
-                    from ctypes import windll, WinError, POINTER
 
                     class SHFILEOPSTRUCTW(Structure):
                         _fields_ = [
