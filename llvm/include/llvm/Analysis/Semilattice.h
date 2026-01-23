081658cf2fbce37b7089ca64f11dfea9dfa46a7d
Ramkumar Ramachandra <artagnon@tenstorrent.com>
2026-01-23 12:34:31 +0000

    [Semilattice] Add LLVM_ABI annotations, public API

--- llvm/include/llvm/Analysis/Semilattice.h
+++ llvm/include/llvm/Analysis/Semilattice.h
@@ -19,6 +19,7 @@
 #include "llvm/IR/User.h"
 #include "llvm/IR/Value.h"
 #include "llvm/Support/Allocator.h"
+#include "llvm/Support/Compiler.h"
 #include "llvm/Support/KnownBits.h"
 
 namespace llvm {
@@ -33,6 +34,23 @@ class SemilatticeNode {
   SmallVector<SemilatticeNode *, 4> Children;
 
 public:
+  using iterator = SmallVectorImpl<SemilatticeNode *>::iterator;
+  LLVM_ABI iterator child_begin() { return Children.begin(); } // NOLINT
+  LLVM_ABI iterator child_end() { return Children.end(); }     // NOLINT
+  LLVM_ABI iterator_range<iterator> children() {
+    return make_range(child_begin(), child_end());
+  }
+  LLVM_ABI bool isLeaf() const { return Children.empty(); }
+  LLVM_ABI bool isRoot() const { return Parents.empty(); }
+  LLVM_ABI Value *getValue() const { return ValHasKnownBits.getPointer(); }
+  LLVM_ABI bool hasKnownBits() const { return ValHasKnownBits.getInt(); }
+  LLVM_ABI void setHasKnownBits() { ValHasKnownBits.setInt(1); }
+  LLVM_ABI KnownBits getKnownBits() const { return Known; }
+  LLVM_ABI void unionKnownWith(const KnownBits &NewKnown) {
+    Known = Known.unionWith(NewKnown);
+  }
+
+protected:
   SemilatticeNode() : ValHasKnownBits(nullptr, 0) {}
   SemilatticeNode(Value *V)
       : ValHasKnownBits(V, 0),
@@ -42,42 +60,22 @@ public:
   SemilatticeNode(const SemilatticeNode &) = delete;
   SemilatticeNode &operator=(const SemilatticeNode &) = delete;
 
-public:
-  using iterator = SmallVectorImpl<SemilatticeNode *>::iterator;
-  iterator child_begin() { return Children.begin(); } // NOLINT
-  iterator child_end() { return Children.end(); }     // NOLINT
-  iterator_range<iterator> children() {
-    return make_range(child_begin(), child_end());
-  }
-  bool isLeaf() const { return Children.empty(); }
-  bool isRoot() const { return Parents.empty(); }
-
-  bool hasParent(SemilatticeNode *N) const { return is_contained(Parents, N); }
-  Value *getValue() const { return ValHasKnownBits.getPointer(); }
-  bool hasKnownBits() const { return ValHasKnownBits.getInt(); }
-  void setHasKnownBits() { ValHasKnownBits.setInt(1); }
-  KnownBits getKnownBits() const { return Known; }
-  void unionKnownWith(const KnownBits &NewKnown) {
-    Known = Known.unionWith(NewKnown);
-  }
   void resetKnownBits() { Known.resetAll(); }
-
   SemilatticeNode *addParent(SemilatticeNode *N) {
     if (!hasParent(N))
       Parents.push_back(N);
     return this;
   }
-
   SemilatticeNode *addChild(SemilatticeNode *N) {
     if (!is_contained(Children, N))
       Children.push_back(N);
     return this;
   }
-
   SemilatticeNode *rauw(Value *NewV) {
     ValHasKnownBits.setPointer(NewV);
     return this;
   }
+  bool hasParent(SemilatticeNode *N) const { return is_contained(Parents, N); }
 };
 
 class Semilattice {
@@ -100,34 +98,38 @@ class Semilattice {
   void initialize(Function &F);
 
 public:
-  Semilattice(Function &F);
-  Semilattice(const Semilattice &) = delete;
-  Semilattice &operator=(const Semilattice &) = delete;
-  Semilattice(Semilattice &&) = default;
-  Semilattice &operator=(Semilattice &&) = default;
+  LLVM_ABI Semilattice(Function &F);
+  LLVM_ABI Semilattice(const Semilattice &) = delete;
+  LLVM_ABI Semilattice &operator=(const Semilattice &) = delete;
+  LLVM_ABI Semilattice(Semilattice &&) = default;
+  LLVM_ABI Semilattice &operator=(Semilattice &&) = default;
 
-  NodeT *getRootNode() const { return RootNode; }
-  bool empty() const { return RootNode->isLeaf(); }
-  bool contains(const Value *V) const { return NodeMap.contains(V); }
-  NodeT *lookup(const Value *V) const { return NodeMap.lookup_or(V, RootNode); }
-  size_t size() const { return NodeMap.size(); }
+  LLVM_ABI NodeT *getRootNode() const { return RootNode; }
+  LLVM_ABI bool empty() const { return RootNode->isLeaf(); }
+  LLVM_ABI bool contains(const Value *V) const { return NodeMap.contains(V); }
+  LLVM_ABI NodeT *lookup(const Value *V) const {
+    return NodeMap.lookup_or(V, RootNode);
+  }
+  LLVM_ABI size_t size() const { return NodeMap.size(); }
 
-  bool hasKnownBits(const Value *V) const { return lookup(V)->hasKnownBits(); }
-  KnownBits getKnownBits(const Value *V) const {
+  LLVM_ABI bool hasKnownBits(const Value *V) const {
+    return lookup(V)->hasKnownBits();
+  }
+  LLVM_ABI KnownBits getKnownBits(const Value *V) const {
     return lookup(V)->getKnownBits();
   }
-  void updateKnownBits(const Value *V, const KnownBits &Known) const {
+  LLVM_ABI void updateKnownBits(const Value *V, const KnownBits &Known) const {
     if (!contains(V))
       return;
     NodeT *LookupV = lookup(V);
     LookupV->unionKnownWith(Known);
     LookupV->setHasKnownBits();
   }
-  SmallVector<NodeT *> invalidateKnownBits(Value *V);
-  SmallVector<NodeT *> rauw(Value *OldV, Value *NewV);
+  LLVM_ABI SmallVector<NodeT *> invalidateKnownBits(Value *V);
+  LLVM_ABI SmallVector<NodeT *> rauw(Value *OldV, Value *NewV);
 
-  void reset(Function &F);
-  void print(raw_ostream &OS) const;
+  LLVM_ABI void reset(Function &F);
+  LLVM_ABI void print(raw_ostream &OS) const;
 #if !defined(NDEBUG) || defined(LLVM_ENABLE_DUMP)
   LLVM_DUMP_METHOD void dump() const;
 #endif
@@ -141,9 +143,9 @@ template <typename NodeRef> struct NodeGraphTraitsBase {
   static ChildIteratorType child_begin(NodeRef N) { // NOLINT
     return N->child_begin();
   }
-  static ChildIteratorType child_end(NodeRef N) {
+  static ChildIteratorType child_end(NodeRef N) { // NOLINT
     return N->child_end();
-  } // NOLINT
+  }
 };
 
 template <>
