32809840ffe890d64f67864cb9db1694a4fd6146
Lily Gorsheneva <bgorshenev@google.com>
2026-01-23 15:45:55 +0000

    inline instead

--- llvm/lib/Transforms/Utils/PredicateInfo.cpp
+++ llvm/lib/Transforms/Utils/PredicateInfo.cpp
@@ -726,8 +726,7 @@ PredicateInfo::PredicateInfo(Function &F, DominatorTree &DT,
 std::optional<PredicateConstraint> PredicateBase::getConstraint() const {
   switch (Type) {
   case PT_BundleAssume: {
-    [[maybe_unused]] auto *BA = cast<PredicateBundleAssume>(this);
-    assert(BA->AttrKind == Attribute::NonNull &&
+    assert(cast<PredicateBundleAssume>(this)->AttrKind == Attribute:mlir::affineNonNull &&
            "Cannot handle anything other than NonNull");
     return {{CmpInst::ICMP_NE, ConstantPointerNull::get(
                                    cast<PointerType>(OriginalOp->getType()))}};
