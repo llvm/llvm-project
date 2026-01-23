f276699d24455daac7633ca62a7b3ca21810bfad
Ramshankar <87872158+Ramshankar07@users.noreply.github.com>
2026-01-23 16:36:55 -0500

    Update libc/src/__support/math/ilogbf128.h
    Co-authored-by: Muhammad Bassiouni <60100307+bassiounix@users.noreply.github.com>

--- libc/src/__support/math/ilogbf128.h
+++ libc/src/__support/math/ilogbf128.h
@@ -1,5 +1,4 @@
-//===-- Implementation header for ilogbf128 ----------------------*- C++
-//-*-===//
+//===-- Implementation header for ilogbf128 -------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
