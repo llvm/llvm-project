6d68d6e042ee1ebdb6e5bf90b3ec35b72c3ff9e1
Frank Schlimbach <frank.schlimbach@intel.com>
2026-01-23 16:29:14 +0100

    Update mlir/include/mlir/Dialect/Bufferization/Extensions/AllExtensions.h
    Co-authored-by: Adam Siemieniuk <adam.siemieniuk@intel.com>

--- mlir/include/mlir/Dialect/Bufferization/Extensions/AllExtensions.h
+++ mlir/include/mlir/Dialect/Bufferization/Extensions/AllExtensions.h
@@ -1,4 +1,4 @@
-//===- AllExtensions.h - All bufferization Extensions -----------*- C++ -*-===//
+//===- AllExtensions.h - All Bufferization Extensions -----------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
