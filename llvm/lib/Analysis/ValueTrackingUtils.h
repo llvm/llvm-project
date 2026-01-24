0a48b68b93d171cdf1ecf0edf1c9d1d6e8e9e3d0
Ramkumar Ramachandra <artagnon@tenstorrent.com>
2026-01-24 23:11:46 +0000

    [VTUtils] Fix some issues; better?

--- llvm/lib/Analysis/ValueTrackingUtils.h
+++ llvm/lib/Analysis/ValueTrackingUtils.h
@@ -51,25 +51,11 @@ bool cmpExcludesZero(CmpInst::Predicate Pred, const Value *RHS);
 void breakSelfRecursivePHI(const Use *U, const PHINode *PHI, Value *&ValOut,
                            Instruction *&CtxIOut,
                            const PHINode **PhiOut = nullptr);
-bool isKnownNonZeroFromAssume(const Value *V, const SimplifyQuery &Q);
-void computeKnownBitsFromCmp(const Value *V, CmpInst::Predicate Pred,
-                             Value *LHS, Value *RHS, KnownBits &Known,
-                             const SimplifyQuery &Q);
-void computeKnownBitsFromICmpCond(const Value *V, ICmpInst *Cmp,
-                                  KnownBits &Known, const SimplifyQuery &SQ,
-                                  bool Invert);
-bool isKnownNonNullFromDominatingCondition(const Value *V,
-                                           const Instruction *CtxI,
-                                           const DominatorTree *DT);
 bool isSignedMinMaxClamp(const Value *Select, const Value *&In,
                          const APInt *&CLow, const APInt *&CHigh);
 bool isSignedMinMaxIntrinsicClamp(const IntrinsicInst *II, const APInt *&CLow,
                                   const APInt *&CHigh);
 void unionWithMinMaxIntrinsicClamp(const IntrinsicInst *II, KnownBits &Known);
-bool isImpliedToBeAPowerOfTwoFromCond(const Value *V, bool OrZero,
-                                      const Value *Cond, bool CondIsTrue);
-bool isNonZeroRecurrence(const PHINode *PN);
-bool matchOpWithOpEqZero(Value *Op0, Value *Op1);
 bool isNonEqualPointersWithRecursiveGEP(const Value *A, const Value *B,
                                         const SimplifyQuery &Q);
 bool isKnownNonNaN(const Value *V, FastMathFlags FMF);
@@ -82,13 +68,8 @@ ConstantRange getRangeForSelectPattern(const SelectInst &SI,
                                        const InstrInfoQuery &IIQ);
 void setLimitsForBinOp(const BinaryOperator &BO, APInt &Lower, APInt &Upper,
                        const InstrInfoQuery &IIQ, bool PreferSignedRange);
-std::optional<bool> isImpliedCondOperands(CmpInst::Predicate Pred,
-                                          const Value *ALHS, const Value *ARHS,
-                                          const Value *BLHS, const Value *BRHS);
-std::optional<bool> isImpliedCondCommonOperandWithCR(CmpPredicate LPred,
-                                                     const ConstantRange &LCR,
-                                                     CmpPredicate RPred,
-                                                     const ConstantRange &RCR);
+bool isTruePredicate(CmpInst::Predicate Pred, const Value *LHS,
+                     const Value *RHS);
 std::optional<std::pair<Value *, Value *>>
 getInvertibleOperands(const Operator *Op1, const Operator *Op2);
 } // namespace llvm::vtutils
