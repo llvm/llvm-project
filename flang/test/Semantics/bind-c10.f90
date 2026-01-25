c5bab5857423b91ebaa1aa130038101cc609f9d6
Igor S. Gerasimov <foxtranigor@gmail.com>
2026-01-25 15:01:38 +0100

    Update tests for being valid on many platforms

--- flang/test/Semantics/bind-c10.f90
+++ flang/test/Semantics/bind-c10.f90
@@ -1,18 +1,9 @@
 ! RUN: %python %S/test_errors.py %s %flang_fc1
-subroutine bang() bind(C,name='!')
+subroutine currency() bind(C,name='$')
 end
 !ERROR: Symbol has a BIND(C) name containing non-visible ASCII character(s)
 subroutine cr() bind(C,name=achar(13))
 end
-!! depending on used assembler it can be threated as error or not
-subroutine beast() bind(C,name="666")
-end
-!! depending on used assembler it can be threated as error or not
-subroutine llvm() bind(C,name=".L1")
-end
-!! depending on used assembler it can be threated as error or not
-subroutine as() bind(C,name=".text")
-end
-!! depending on used assembler it can be threated as error or not
-subroutine printable_ascii() bind(C,name="! ""#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~'")
+!ERROR: Symbol has a BIND(C) name containing non-visible ASCII character(s)
+subroutine null_terminator() bind(C,name="null_terminator" // achar(0))
 end
