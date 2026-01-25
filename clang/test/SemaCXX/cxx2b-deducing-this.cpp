6bd851f0de84d858ed26f9c8c71f49bf8cecf50c
Luohao Wang <luohaothu@live.com>
2026-01-25 22:20:23 +0800

    Move test to cxx2b-deducing-this.cpp

--- clang/test/SemaCXX/cxx2b-deducing-this.cpp
+++ clang/test/SemaCXX/cxx2b-deducing-this.cpp
@@ -1400,3 +1400,19 @@ a void Bar(this int) { // expected-note {{candidate function}}
 }
 
 }
+
+namespace GH177741 {
+
+struct S {
+  static int operator()(this S) { return 0; }
+  // expected-error@-1 {{an explicit object parameter cannot appear in a static function}}
+  // expected-note@-2 {{candidate function not viable}}
+};
+
+void foo() {
+  S s{};
+  s(0);
+  // expected-error@-1 {{no matching function for call to object of type 'S'}}
+}
+
+}
