cce0e46aec2d9bfdaa243b4d80124fb07539043a
ash <ashkernel02@gmail.com>
2026-01-24 20:23:02 -0700

    update shared

--- libc/test/shared/shared_math_test.cpp
+++ libc/test/shared/shared_math_test.cpp
@@ -87,6 +87,8 @@ TEST(LlvmLibcSharedMathTest, AllFloat) {
 }
 
 TEST(LlvmLibcSharedMathTest, AllDouble) {
+  double sin, cos;
+  LIBC_NAMESPACE::shared::sincos(0.0, &sin, &cos);
   EXPECT_FP_EQ(0x1.921fb54442d18p+0, LIBC_NAMESPACE::shared::acos(0.0));
   EXPECT_FP_EQ(0x0p+0, LIBC_NAMESPACE::shared::asin(0.0));
   EXPECT_FP_EQ(0x0p+0, LIBC_NAMESPACE::shared::atan(0.0));
@@ -101,6 +103,8 @@ TEST(LlvmLibcSharedMathTest, AllDouble) {
   EXPECT_FP_EQ(0x0p+0, LIBC_NAMESPACE::shared::fsqrt(0.0));
   EXPECT_FP_EQ(0x0p+0, LIBC_NAMESPACE::shared::log(1.0));
   EXPECT_FP_EQ(0.0, LIBC_NAMESPACE::shared::sin(0.0));
+  EXPECT_FP_EQ(1.0, cos);
+  EXPECT_FP_EQ(0.0, sin);
 }
 
 TEST(LlvmLibcSharedMathTest, AllLongDouble) {
