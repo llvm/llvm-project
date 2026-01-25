53966967a787fc8f86c193c52370b9a1db72e501
anonmiraj <ezzibrahimx@gmail.com>
2026-01-25 22:05:16 +0200

    [libc][math] Refactor powf to Header Only.

--- libc/shared/math/powf.h
+++ libc/shared/math/powf.h
@@ -1,4 +1,4 @@
-//===-- Single-precision x^y function -------------------------------------===//
+//===-- Shared powf function ------------------------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,11 +6,18 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "src/math/powf.h"
+#ifndef LLVM_LIBC_SHARED_MATH_POWF_H
+#define LLVM_LIBC_SHARED_MATH_POWF_H
+
+#include "shared/libc_common.h"
 #include "src/__support/math/powf.h"
 
 namespace LIBC_NAMESPACE_DECL {
+namespace shared {
 
-LLVM_LIBC_FUNCTION(float, powf, (float x, float y)) { return math::powf(x, y); }
+using math::powf;
 
+} // namespace shared
 } // namespace LIBC_NAMESPACE_DECL
+
+#endif // LLVM_LIBC_SHARED_MATH_POWF_H
