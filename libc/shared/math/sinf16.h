d7f8f1e9099be304cf7dee3a466d12c21a573c6a
Nico Weber <thakis@chromium.org>
2026-01-26 16:53:09 -0500

    [libc][math] Refactor sinf16 implementation to header-only in src/__support/math folder.
    Part of #147386
    
    in preparation for:
    https://discourse.llvm.org/t/rfc-make-clang-builtin-math-functions-constexpr-with-llvm-libc-to-support-c-23-constexpr-math-functions/86450

--- libc/shared/math/sinf16.h
+++ libc/shared/math/sinf16.h
@@ -1,4 +1,4 @@
-//===-- Half-precision sin(x) function ------------------------------------===//
+//===-- Shared sinf16 function ----------------------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,11 +6,18 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "src/math/sinf16.h"
+#ifndef LLVM_LIBC_SHARED_MATH_SINF16_H
+#define LLVM_LIBC_SHARED_MATH_SINF16_H
+
+#include "shared/libc_common.h"
 #include "src/__support/math/sinf16.h"
 
 namespace LIBC_NAMESPACE_DECL {
+namespace shared {
 
-LLVM_LIBC_FUNCTION(float16, sinf16, (float16 x)) { return math::sinf16(x); }
+using math::sinf16;
 
+} // namespace shared
 } // namespace LIBC_NAMESPACE_DECL
+
+#endif // LLVM_LIBC_SHARED_MATH_SINF16_H
