366c8b30af03d579040501bf8f5f94c822c04cd4
Ben <ben.dunbobbin@sony.com>
2026-01-26 16:12:44 +0000

    Improve test_temps.py to address review comments.
    Add a usage comment block describing how to run the script and the
    supported actions. Also, simplify the local_codegen_and_wait.py
    description and direct readers to test_temps.py for more information.
    
    Make "lock" explicitly Windows-only and report an error if invoked on
    non-Windows platforms.
    
    Centralize management of send-signal1/send-signal2 in the script by
    removing any stale files at startup.
    
    Add a brief comment explaining the "kill" exit code handling.
    
    Some minor improvements to the script to aid readability, including
    listing the imports in a more standard way.

--- cross-project-tests/dtlto/local_codegen_and_wait.py
+++ cross-project-tests/dtlto/local_codegen_and_wait.py
@@ -2,11 +2,7 @@
 This simple distributor performs code generation locally, creates the
 "send-signal1" file, and then waits for the "send-signal2" file to appear
 before exiting. It is intended to be used in tandem with test_temps.py.
-
-By coordinating via the "send-signal*" files, the scripts ensure that the
-requested actions are performed after all DTLTO backend compilations have
-completed but before DTLTO itself finishes. At this point, DTLTO temporary
-files have not yet been cleaned up.
+Please see test_temps.py for more information.
 """
 
 import json, subprocess, sys, time, os, pathlib
