b8aa8de03602e6f1abe6b90d4b1397c1167f4473
Muhammad Bassiouni <60100307+bassiounix@users.noreply.github.com>
2026-01-26 21:43:09 +0200

    Update libc/src/__support/math/logf.h

--- libc/src/__support/math/logf.h
+++ libc/src/__support/math/logf.h
@@ -56,7 +56,7 @@ namespace LIBC_NAMESPACE_DECL {
 
 namespace math {
 
-static constexpr float logf(float x) {
+LIBC_INLINE static float logf(float x) {
   using namespace common_constants_internal;
   constexpr double LOG_2 = 0x1.62e42fefa39efp-1;
   using FPBits = typename fputil::FPBits<float>;
