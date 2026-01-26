87106d5875a30fefdd17320238808906e8b852b0
Louis Dionne <ldionne.2@gmail.com>
2026-01-26 15:28:38 -0500

    Drop nonallocating, add nonblocking to definition

--- libcxx/src/experimental/log_hardening_failure.cpp
+++ libcxx/src/experimental/log_hardening_failure.cpp
@@ -16,7 +16,7 @@
 
 _LIBCPP_BEGIN_NAMESPACE_STD
 
-void __log_hardening_failure(const char* message) noexcept {
+void __log_hardening_failure(const char* message) noexcept [[_Clang::__nonblocking__]] {
   // Always log the message to `stderr` in case the platform-specific system calls fail.
   std::fputs(message, stderr);
 
