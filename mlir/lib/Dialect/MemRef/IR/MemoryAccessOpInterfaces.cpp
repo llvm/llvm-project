ca8e98ea45cb1c398e4a45bf8cb3d60162898e71
Krzysztof Drewniak <Krzysztof.Drewniak@amd.com>
2026-01-27 22:03:16 +0000

    Revew comments that weren't Github suggestions, clang-format ran on my .td

--- mlir/lib/Dialect/MemRef/IR/MemoryAccessOpInterfaces.cpp
+++ mlir/lib/Dialect/MemRef/IR/MemoryAccessOpInterfaces.cpp
@@ -17,14 +17,12 @@
 
 namespace mlir::memref {
 #include "mlir/Dialect/MemRef/IR/MemoryAccessOpInterfaces.cpp.inc"
-} // namespace mlir::memref
 
-using namespace mlir;
-using namespace mlir::memref;
-
-LogicalResult
-mlir::memref::detail::verifyIndexedAccessOpInterface(Operation *op) {
+LogicalResult detail::verifyIndexedAccessOpInterface(Operation *op) {
   auto iface = dyn_cast<IndexedAccessOpInterface>(op);
+  if (!iface)
+    return failure();
+
   TypedValue<MemRefType> memref = iface.getAccessedMemref();
   if (!memref) {
     // Some operations can carry tensors, this is fine.
@@ -33,15 +31,17 @@ mlir::memref::detail::verifyIndexedAccessOpInterface(Operation *op) {
   if (memref.getType().getRank() !=
       static_cast<int64_t>(iface.getIndices().size()))
     return op->emitOpError(
-        "invalid number of indices for accessed memref, expected ") <<
-        memref.getType().getRank() << " but got " <<
-        iface.getIndices().size());
+               "invalid number of indices for accessed memref, expected ")
+           << memref.getType().getRank() << " but got "
+           << iface.getIndices().size();
   return success();
 }
 
-LogicalResult
-mlir::memref::detail::verifyIndexedMemCopyOpInterface(Operation *op) {
+LogicalResult detail::verifyIndexedMemCopyOpInterface(Operation *op) {
   auto iface = dyn_cast<IndexedMemCopyOpInterface>(op);
+  if (!iface)
+    return failure();
+
   TypedValue<MemRefType> src = iface.getSrc();
   TypedValue<MemRefType> dst = iface.getDst();
   if (!src || !dst) {
@@ -57,8 +57,9 @@ mlir::memref::detail::verifyIndexedMemCopyOpInterface(Operation *op) {
   if (dst.getType().getRank() !=
       static_cast<int64_t>(iface.getDstIndices().size()))
     return op->emitOpError(
-        "invalid number of indices for destination memref, expected " +
-        Twine(dst.getType().getRank()) + ", got " +
-        Twine(iface.getDstIndices().size()));
+               "invalid number of indices for destination memref, expected ")
+           << dst.getType().getRank() << ", got "
+           << iface.getDstIndices().size();
   return success();
 }
+} // namespace mlir::memref
