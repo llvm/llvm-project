0de69817e6fbca8bd7ce299217fa608a98b18bd9
Ash <97464181+tnuha@users.noreply.github.com>
2026-01-27 03:12:51 +0000

    Update libc/src/__support/math/sincosf.h
    Co-authored-by: Muhammad Bassiouni <60100307+bassiounix@users.noreply.github.com>

--- libc/src/__support/math/sincosf.h
+++ libc/src/__support/math/sincosf.h
@@ -18,7 +18,6 @@
 #include "src/__support/macros/config.h"
 #include "src/__support/macros/optimization.h"            // LIBC_UNLIKELY
 #include "src/__support/macros/properties/cpu_features.h" // LIBC_TARGET_CPU_HAS_FMA
-#include "src/__support/math/sincosf_utils.h"
 
 namespace LIBC_NAMESPACE_DECL {
 
