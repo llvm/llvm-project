2ff620d707e72c66a62ff0ddae720b5211ba8440
Serosh <janmejayapanda400@gmail.com>
2026-01-28 22:54:57 +0530

    Update test to expect comma operator warning

--- clang/test/AST/ByteCode/gh176549.cpp
+++ clang/test/AST/ByteCode/gh176549.cpp
@@ -1,8 +1,7 @@
 // RUN: %clang_cc1 -fexperimental-new-constant-interpreter -verify %s
-// expected-no-diagnostics
 
 const char a[4] = "abc";
 void foo() {
   int i = 0;
-  i = 1 > (a + 1, sizeof(a));
+  i = 1 > (a + 1, sizeof(a)); // expected-warning {{left operand of comma operator has no effect}}
 }
