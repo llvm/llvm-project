7ed9563f49cc0f0daca2697c0868d81ee527880b
NAKAMURA Takumi <geek4civic@gmail.com>
2026-01-28 19:45:31 +0900

    hasSkipCounter

--- clang/lib/CodeGen/CodeGenPGO.cpp
+++ clang/lib/CodeGen/CodeGenPGO.cpp
@@ -1187,16 +1187,15 @@ CodeGenPGO::applyFunctionAttributes(llvm::IndexedInstrProfReader *PGOReader,
   Fn->setEntryCount(FunctionCount);
 }
 
-std::pair<bool, bool>
-CodeGenPGO::getExecSkipCounterExistence(const Stmt *S) const {
+bool CodeGenPGO::hasSkipCounter(const Stmt *S) const {
   if (!RegionCounterMap)
-    return {false, false};
+    return false;
 
   auto I = RegionCounterMap->find(S);
   if (I == RegionCounterMap->end())
-    return {false, false};
+    return false;
 
-  return {I->second.Executed.hasValue(), I->second.Skipped.hasValue()};
+  return I->second.Skipped.hasValue();
 }
 
 void CodeGenPGO::emitCounterSetOrIncrement(CGBuilderTy &Builder, const Stmt *S,
@@ -1568,9 +1567,8 @@ void CodeGenFunction::incrementProfileCounter(CounterForIncrement ExecSkip,
   PGO->setCurrentStmt(S);
 }
 
-std::pair<bool, bool>
-CodeGenFunction::getExecSkipCounterExistence(const Stmt *S) const {
-  return PGO->getExecSkipCounterExistence(S);
+bool CodeGenFunction::hasSkipCounter(const Stmt *S) const {
+  return PGO->hasSkipCounter(S);
 }
 void CodeGenFunction::markStmtAsUsed(bool Skipped, const Stmt *S) {
   PGO->markStmtAsUsed(Skipped, S);
