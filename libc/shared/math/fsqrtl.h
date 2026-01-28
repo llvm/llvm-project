d7a04e24db86fdf9b15399d32e76db0e0f805e50
Atharva062006 <72563240+Atharva062006@users.noreply.github.com>
2026-01-28 13:00:25 +0530

    applied suggested change

--- libc/shared/math/fsqrtl.h
+++ libc/shared/math/fsqrtl.h
@@ -1,5 +1,4 @@
-//===-- Shared header for fsqrtl ---------------------------------*- C++
-//-*-===//
+//===-- Shared header for fsqrtl ---------------------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
