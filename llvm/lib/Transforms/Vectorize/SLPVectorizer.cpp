87aa5487053de6a32f09ed518048d019d7184459
Soumik15630m <soumik15630m@gmail.com>
2026-01-28 16:38:03 +0530

    [SLP] Fix crash on extractelement with out-of-bounds index.....Fixes … (#176918)
    …The cose modeling logic was attempting to set a bit in APInt for an
    out-of-bounds index, causing an assertion failure. This patch ignores
    OOB indices as they produce poison- which is already handled.
    Fixes #176780  
    
    this is the same test result which produces this bug 
    
    <img width="1600" height="964" alt="image"
    src="https://github.com/user-attachments/assets/80593902-9d15-4e18-850b-a558bca8518e"
    />

--- llvm/lib/Transforms/Vectorize/SLPVectorizer.cpp
+++ llvm/lib/Transforms/Vectorize/SLPVectorizer.cpp
@@ -902,7 +902,14 @@ static std::optional<unsigned> getExtractIndex(const Instruction *E) {
     auto *CI = dyn_cast<ConstantInt>(E->getOperand(1));
     if (!CI)
       return std::nullopt;
-    return CI->getZExtValue();
+    // Check if the index is out of bound  - we can get the source vector from
+    // operand 0
+    unsigned Idx = CI->getZExtValue();
+    auto *EE = cast<ExtractElementInst>(E);
+    const unsigned VF = ::getNumElements(EE->getVectorOperandType());
+    if (Idx >= VF)
+      return std::nullopt;
+    return Idx;
   }
   auto *EI = cast<ExtractValueInst>(E);
   if (EI->getNumIndices() != 1)
