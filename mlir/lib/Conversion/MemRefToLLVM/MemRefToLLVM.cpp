328f997d93c65b7ca1825ce0a1e4dc31fe94d278
Ayush3941 <ayushkgaur1@gmail.com>
2026-01-29 12:21:14 -0500

    [MLIR][MemRefToLLVM] Diagnose rank/index mismatch in generic_atomic_rmw lowering with general format

--- mlir/lib/Conversion/MemRefToLLVM/MemRefToLLVM.cpp
+++ mlir/lib/Conversion/MemRefToLLVM/MemRefToLLVM.cpp
@@ -724,9 +724,9 @@ struct GenericAtomicRMWOpLowering
     auto memRefType = cast<MemRefType>(atomicOp.getMemref().getType());
     if (adaptor.getIndices().size() != (size_t)memRefType.getRank()) {
       return atomicOp.emitError()
-               << "index count (" << adaptor.getIndices().size()
-               << ") does not match memref rank (" << memRefType.getRank() << ")";
-      }
+             << "index count (" << adaptor.getIndices().size()
+             << ") does not match memref rank (" << memRefType.getRank() << ")";
+    }
     auto dataPtr = getStridedElementPtr(
         rewriter, loc, memRefType, adaptor.getMemref(), adaptor.getIndices());
     Value init = LLVM::LoadOp::create(
