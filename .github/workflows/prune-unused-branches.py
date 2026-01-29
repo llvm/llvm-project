efdd5df6d956e7dcb14036a24ec3d70d5495436d
Aiden Grossman <aidengrossman@google.com>
2026-01-29 00:08:06 +0000

    fix
    Created using spr 1.3.7

--- .github/workflows/prune-unused-branches.py
+++ .github/workflows/prune-unused-branches.py
@@ -73,7 +73,7 @@ def get_user_branches_to_remove(
 
 
 def generate_patch_for_branch(branch_name: str) -> bytes:
-    command_vector = ["git", "diff", f"main...origin/{branch_name}"]
+    command_vector = ["git", "diff", f"origin/main...origin/{branch_name}"]
     try:
         result = subprocess.run(
             command_vector, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True
