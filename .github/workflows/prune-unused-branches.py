721932f9f4c5c3d5562261eb5a1733fef725368f
Aiden Grossman <aidengrossman@google.com>
2026-01-29 00:24:03 +0000

    fix
    Created using spr 1.3.7

--- .github/workflows/prune-unused-branches.py
+++ .github/workflows/prune-unused-branches.py
@@ -99,6 +99,7 @@ def generate_patches_for_all_branches(branches_to_remove: list[str], patches_pat
 
 
 def main(github_token):
+    print("Starting app")
     if len(sys.argv) != 2:
         print(
             "Invalid invocation. Correct usage: python3 prune-unused-branches.py <patch output diectory>"
