8f3d1a3ea4ae2e8b48747156825d560772a74256
grchz <grqz.dev@proton.me>
2026-01-29 17:32:35 +1300

    Expect -Wnonportable-include-path warning

--- clang/test/Frontend/case-insensitive-paths.c
+++ clang/test/Frontend/case-insensitive-paths.c
@@ -1,8 +1,10 @@
-// RUN: %clang_cc1 -E -P -verify --show-includes -Wno-nonportable-include-path \
-// RUN:     -fcase-insensitive-paths %s 2>&1 | FileCheck %s
-
-#include "inPuts/eMptY.H" // expected-no-diagnostics
-
+// RUN: %clang_cc1 -E -P --show-includes -verify=suppressed \
+// RUN:   -fcase-insensitive-paths -Wno-nonportable-include-path %s 2>&1 \
+// RUN:   | FileCheck %s
 // Make sure the exact spelling used in the #include directive is preserved
 // when printing header dependencies (consistent with MSVC /showIncludes).
 // CHECK: Note: including file: {{.*}}inPuts/eMptY.H
+// suppressed-no-diagnostics@+1
+#include "inPuts/eMptY.H"
+// expected-warning@-1 {{non-portable path to file '"Inputs/empty.h"'; specified path differs in case from file name on disk}}
+// RUN: %clang_cc1 -E -P -verify -fcase-insensitive-paths %s
