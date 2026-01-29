a81b640f7a648b3d82409ba38eea09271714d8f1
Demetrius Kanios <demetrius@kanios.net>
2026-01-29 00:54:20 -0800

    Change `QualType::isWebAssemblyFuncrefType` to accept types attributed with `WebAssemblyFuncref`

--- clang/lib/AST/Type.cpp
+++ clang/lib/AST/Type.cpp
@@ -2947,7 +2947,8 @@ bool QualType::isWebAssemblyExternrefType() const {
 
 bool QualType::isWebAssemblyFuncrefType() const {
   return getTypePtr()->isFunctionPointerType() &&
-         getAddressSpace() == LangAS::wasm_funcref;
+         (getAddressSpace() == LangAS::wasm_funcref ||
+          getTypePtr()->hasAttr(attr::WebAssemblyFuncref));
 }
 
 QualType::PrimitiveDefaultInitializeKind
