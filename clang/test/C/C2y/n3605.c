d7a4743fcf16d5cfb5615512a8f6bced69055f19
Henry Kleynhans <henry.kleynhans@gmail.com>
2026-01-29 18:47:43 +0000

    Test C versions from C11

--- clang/test/C/C2y/n3605.c
+++ clang/test/C/C2y/n3605.c
@@ -1,44 +1,48 @@
 // RUN: %clang_cc1 -verify -std=c2y -Wall -pedantic %s
+// RUN: %clang_cc1 -verify -std=c23 -Wall -pedantic %s
 // expected-no-diagnostics
 // RUN: %clang_cc1 -std=c2y %s -triple x86_64 --embed-dir=%S/Inputs -emit-llvm -o - | FileCheck %s
+// RUN: %clang_cc1 -std=c23 %s -triple x86_64 --embed-dir=%S/Inputs -emit-llvm -o - | FileCheck %s
 
 enum A{ a=(int)(_Generic(0, int: (2.5))) };
 enum B{ b=(int)(_Generic(0, int: (2 + 1))) };
 
-constexpr int c  = _Generic((float*)0, default: 0);
-
+#if (__STDC_VERSION__ >= 202311L)
+constexpr int c = _Generic((float*)0, default: 0);
 constexpr int d = _Generic((float*)0, default: (sizeof(c)));
+#else
+int c = _Generic((float*)0, default: 0);
+int d = _Generic((float*)0, default: (sizeof(c)));
+#endif
 
 char s[] = _Generic(0, default: ("word"));
 
-// static_assert(1, _Generic(1, default: "Error Message"));
-
-int value_of_a() {
+int value_of_a(void) {
 	// CHECK: ret i32 2
 	return a;
 }
 
-int value_of_b() {
+int value_of_b(void) {
 	// CHECK: ret i32 3
 	return b;
 }
 
-int value_of_c() {
+int value_of_c(void) {
 	// CHECK: ret i32 0
 	return c;
 }
 
-int value_of_d() {
+int value_of_d(void) {
 	// CHECK: ret i32 4
 	return d;
 }
 
-char *value_of_s() {
+char *value_of_s(void) {
 	// CHECK: ret ptr @s
     return s;
 }
 
-float value_of_float() {
+float value_of_float(void) {
 	// CHECK: %f = alloca ptr, align 8
 	// CHECK: store ptr null, ptr %f, align 8
 	// CHECK: %0 = load ptr, ptr %f, align 8
