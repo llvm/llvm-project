ff001e16ebd1f600eb6365fd485d738dca83d591
Ayush3941 <ayushkgaur1@gmail.com>
2026-01-29 06:06:52 -0500

    [clang][Sema] Fix crash when diagnosing address-space qualified new/delete with fixed format v3 with more relevant test

--- clang/test/SemaCXX/address-space-new-delete.cpp
+++ clang/test/SemaCXX/address-space-new-delete.cpp
@@ -1,13 +1,11 @@
-// RUN: %clang_cc1 -std=c++17 %s -verify
+// RUN: %clang_cc1 -std=c++17 %s -verify -Wno-unknown-attributes
 
-typedef int FOO __attribute__((address_space(1)));
+typedef int LocalInt __attribute__((opencl_local));
 
 void test_new() {
-  int *p = new FOO[1];
-  // expected-error@-1 {{cannot allocate objects of type 'int' in address space}}
+  int *p = new LocalInt[1]; // expected-error {{cannot allocate objects of type 'int' in address space}}
 }
 
-void test_delete(FOO *p) {
-  delete p;
-  // expected-error@-1 {{cannot delete objects of type 'int' in address space}}
+void test_delete(LocalInt *p) {
+  delete p; // expected-error {{cannot delete objects of type 'int' in address space}}
 }
