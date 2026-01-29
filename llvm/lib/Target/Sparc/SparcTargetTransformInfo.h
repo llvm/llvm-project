442ef8e9d9bb60969769a5a487d1bdf689366729
Koakuma <koachan@protonmail.com>
2026-01-29 18:53:40 +0700

    [SPARC] Add basic TTI implementation
    This is to inform the middle-end transform passes about the capabilities
    of the processor.
    
    This should solve two issues:
    - Popcount loops now gets converted properly to `popc`, if available
      (https://github.com/llvm/llvm-project/issues/171969); and
    - Fixes the `find-last` test, which fails to build because LoopVectorizePass
      mistakenly thinks that the backend understands vector types, when in fact
      it doesn't (https://github.com/sparclinux/issues/issues/69).

---
+++ llvm/lib/Target/Sparc/SparcTargetTransformInfo.h
@@ -0,0 +1,72 @@
+//===-- SparcTargetTransformInfo.cpp - SPARC specific TTI--------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+/// \file
+/// This file a TargetTransformInfoImplBase conforming object specific to the
+/// SPARC target machine. It uses the target's detailed information to
+/// provide more precise answers to certain TTI queries, while letting the
+/// target independent and default TTI implementations handle the rest.
+///
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_LIB_TARGET_SPARC_SPARCTARGETTRANSFORMINFO_H
+#define LLVM_LIB_TARGET_SPARC_SPARCTARGETTRANSFORMINFO_H
+
+#include "SparcTargetMachine.h"
+#include "llvm/Analysis/TargetTransformInfo.h"
+#include "llvm/CodeGen/BasicTTIImpl.h"
+
+namespace llvm {
+
+class SparcTTIImpl final : public BasicTTIImplBase<SparcTTIImpl> {
+  typedef BasicTTIImplBase<SparcTTIImpl> BaseT;
+  typedef TargetTransformInfo TTI;
+  friend BaseT;
+
+  const SparcSubtarget *ST;
+  const SparcTargetLowering *TLI;
+
+  const SparcSubtarget *getST() const { return ST; }
+  const SparcTargetLowering *getTLI() const { return TLI; }
+
+public:
+  explicit SparcTTIImpl(const SparcTargetMachine *TM, const Function &F)
+      : BaseT(TM, F.getDataLayout()), ST(TM->getSubtargetImpl(F)),
+        TLI(ST->getTargetLowering()) {}
+
+  /// \name Scalar TTI Implementations
+  /// @{
+  InstructionCost getIntImmCost(const APInt &Imm, Type *Ty,
+                                TTI::TargetCostKind CostKind) const override;
+
+  InstructionCost getIntImmCostInst(unsigned Opcode, unsigned Idx,
+                                    const APInt &Imm, Type *Ty,
+                                    TTI::TargetCostKind CostKind,
+                                    Instruction *Inst = nullptr) const override;
+  InstructionCost
+  getIntImmCostIntrin(Intrinsic::ID IID, unsigned Idx, const APInt &Imm,
+                      Type *Ty, TTI::TargetCostKind CostKind) const override {
+    return getIntImmCost(Imm, Ty, CostKind);
+  };
+
+  TTI::PopcntSupportKind getPopcntSupport(unsigned TyWidth) const override;
+  /// @}
+
+  /// \name Vector TTI Implementations
+  /// @{
+  enum SparcRegisterClass { GPRRC, FPRRC, FP128RRC };
+  unsigned getNumberOfRegisters(unsigned ClassID) const override;
+  unsigned getRegisterClassForType(bool Vector,
+                                   Type *Ty = nullptr) const override;
+  TypeSize
+  getRegisterBitWidth(TargetTransformInfo::RegisterKind K) const override;
+  /// @}
+};
+
+} // end namespace llvm
+
+#endif
