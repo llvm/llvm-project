c932334759a62fb8cff6079e9b0ecad9beeceae6
nishant-sachdeva <nishant.sachdeva@research.iiit.ac.in>
2026-01-29 09:55:50 +0530

    Moving input.ll to a proper Inputs directory with the test tree

--- llvm/test/tools/llvm-ir2vec/bindings/ir2vec-bindings.py
+++ llvm/test/tools/llvm-ir2vec/bindings/ir2vec-bindings.py
@@ -1,4 +1,4 @@
-# RUN: env PYTHONPATH=%llvm_lib_dir %python %s %S/input.ll %ir2vec_test_vocab_dir/dummy_3D_nonzero_opc_vocab.json | FileCheck %s
+# RUN: env PYTHONPATH=%llvm_lib_dir %python %s %S/../Inputs/input.ll %ir2vec_test_vocab_dir/dummy_3D_nonzero_opc_vocab.json | FileCheck %s
 
 import sys
 import ir2vec
