c13bdad10dc68c7bf303205dd611e141461e3feb
Ayush3941 <ayushkgaur1@gmail.com>
2026-01-28 22:37:05 -0500

    [clang][Sema] Fix crash when diagnosing address-space qualified new/delete with fixed format v3

--- clang/lib/Sema/SemaExprCXX.cpp
+++ clang/lib/Sema/SemaExprCXX.cpp
@@ -50,7 +50,6 @@
 #include "llvm/ADT/StringExtras.h"
 #include "llvm/Support/ErrorHandling.h"
 #include "llvm/Support/TypeSize.h"
-#include "llvm/Support/raw_ostream.h"
 #include <optional>
 using namespace clang;
 using namespace sema;
