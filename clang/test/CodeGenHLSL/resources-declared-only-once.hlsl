37a7cc3fdfdc508be40d2fc72a8545b8348fb23a
Joao Saffran <joaosaffranllvm@gmail.com>
2026-01-30 15:26:53 -0800

    rollback run line

--- clang/test/CodeGenHLSL/resources-declared-only-once.hlsl
+++ clang/test/CodeGenHLSL/resources-declared-only-once.hlsl
@@ -1,4 +1,5 @@
-// RUN: %clang_dxc -T lib_6_7 -Wmissing-declarations %s -### %s -Xclang -verify
+// RUN: %clang_dxc -Wmissing-declarations -T lib_6_7  %s -Xclang -verify \
+// RUN:   -O3 %s -Xclang -verify
 
 // expected-warning@+1{{declaration does not declare anything}}
 RWStructuredBuffer<float>;
