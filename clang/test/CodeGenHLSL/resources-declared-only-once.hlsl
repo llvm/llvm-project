5bb874c8550d333a8935fd89cfc11a6ac4b58235
Joao Saffran <joaosaffranllvm@gmail.com>
2026-01-30 13:30:32 -0800

    address comments from bogner

--- clang/test/CodeGenHLSL/resources-declared-only-once.hlsl
+++ clang/test/CodeGenHLSL/resources-declared-only-once.hlsl
@@ -1,34 +1,11 @@
 // RUN: %clang_dxc -Wmissing-declarations -T lib_6_7 \
-// RUN:  -O3 %s -Xclang -verify -DRES=RWBuffer
-// RUN: %clang_dxc -Wmissing-declarations -T lib_6_7 \
-// RUN:  -O3 %s -Xclang -verify -DRES=Buffer
-// RUN: %clang_dxc -Wmissing-declarations -T lib_6_7 \
-// RUN:  -O3 %s -Xclang -verify -DRES=RasterizerOrderedBuffer
-// RUN: %clang_dxc -Wmissing-declarations -T lib_6_7 \
-// RUN:  -O3 %s -Xclang -verify -DRES=StructuredBuffer
-// RUN: %clang_dxc -Wmissing-declarations -T lib_6_7 \
-// RUN:  -O3 %s -Xclang -verify -DRES=RWStructuredBuffer
-// RUN: %clang_dxc -Wmissing-declarations -T lib_6_7 \
-// RUN:  -O3 %s -Xclang -verify -DRES=AppendStructuredBuffer -DUSE_APPEND=1
-// RUN: %clang_dxc -Wmissing-declarations -T lib_6_7 \
-// RUN:  -O3 %s -Xclang -verify -DRES=ConsumeStructuredBuffer -DUSE_CONSUME=1
-// RUN: %clang_dxc -Wmissing-declarations -T lib_6_7 \
-// RUN:  -O3 %s -Xclang -verify -DRES=RasterizerOrderedStructuredBuffer
+// RUN:  -O3 %s -Xclang -verify
 
 // expected-warning@+1{{declaration does not declare anything}}
-RES<float>;
-RES<uint> a; 
-RES<float> b;
-
-RWStructuredBuffer<uint> Out;
+RWStructuredBuffer<float>;
+RWStructuredBuffer<uint> a; 
+RWStructuredBuffer<float> b;
 
 void run() {
-    #ifdef USE_APPEND
-    a.Append(Out[0]);
-    b.Append(asfloat(Out[0]));
-    #elif USE_CONSUME
-    Out[0] = a.Consume() + asuint(b.Consume());
-    #else
-    Out[0] = a[0] + asuint(b[0]);
-    #endif
+    a[0] = asuint(b[0]);
 }
