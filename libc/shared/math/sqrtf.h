77975f9b2f65a334d59d3111dade99ce73ca0a08
Saina Daneshmand <66389676+SainaDaneshmandjahromi@users.noreply.github.com>
2026-01-29 22:25:06 -0700

    Apply suggestions from code review
    Co-authored-by: Muhammad Bassiouni <60100307+bassiounix@users.noreply.github.com>

--- libc/shared/math/sqrtf.h
+++ libc/shared/math/sqrtf.h
@@ -1,4 +1,4 @@
-//===-- Shared header for sqrtf ----------------------------------*- C++ -*-===//
+//===-- Shared header for sqrtf ---------------------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
