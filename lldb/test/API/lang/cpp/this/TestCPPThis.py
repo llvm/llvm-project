db397cbcc61c9966ae480e68ad01c422cca91445
Michael Buch <michaelbuch12@gmail.com>
2026-01-30 11:21:53 +0000

    fixup! python format

--- lldb/test/API/lang/cpp/this/TestCPPThis.py
+++ lldb/test/API/lang/cpp/this/TestCPPThis.py
@@ -48,7 +48,8 @@ class CPPThisTestCase(TestBase):
 
         self.expect("expression -- (int)getpid(); m_a", startstr="(const int) $1 = 3")
         self.expect(
-            "expression --cpp-ignore-context-qualifiers -- m_a = 2", startstr="(int) $2 = 2"
+            "expression --cpp-ignore-context-qualifiers -- m_a = 2",
+            startstr="(int) $2 = 2",
         )
 
         self.runCmd("process continue")
