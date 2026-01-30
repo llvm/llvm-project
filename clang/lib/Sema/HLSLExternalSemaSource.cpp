6d72694883c483a5544ca9521b623626a7c02965
João Saffran <joaosaffranllvm@gmail.com>
2026-01-29 17:00:41 -0800

    make function call before removing decl

--- clang/lib/Sema/HLSLExternalSemaSource.cpp
+++ clang/lib/Sema/HLSLExternalSemaSource.cpp
@@ -521,6 +521,6 @@ void HLSLExternalSemaSource::CompleteType(TagDecl *Tag) {
   auto It = Completions.find(Record);
   if (It == Completions.end())
     return;
-  Completions.erase(It);
   It->second(Record);
+  Completions.erase(It);
 }
