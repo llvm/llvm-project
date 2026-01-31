5ecb10c8a571b610dd6ba3413d99651b7c02bad8
Wenju He <wenju.he@intel.com>
2026-01-31 07:37:02 +0100

    [IR] Add `fpmath` to keep list of dropUBImplyingAttrsAndMetadata
    `fpmath` is precision metadata rather than UB-implying metadata.
    This avoids `fpmath` from dropped in InstCombine FoldOpIntoSelect.

--- llvm/lib/IR/Instruction.cpp
+++ llvm/lib/IR/Instruction.cpp
@@ -576,7 +576,8 @@ void Instruction::dropUBImplyingAttrsAndMetadata(ArrayRef<unsigned> Keep) {
   // immediate undefined behavior.
   static const unsigned KnownIDs[] = {
       LLVMContext::MD_annotation, LLVMContext::MD_range,
-      LLVMContext::MD_nonnull, LLVMContext::MD_align, LLVMContext::MD_prof};
+      LLVMContext::MD_nonnull,    LLVMContext::MD_align,
+      LLVMContext::MD_fpmath,     LLVMContext::MD_prof};
   SmallVector<unsigned> KeepIDs;
   KeepIDs.reserve(Keep.size() + std::size(KnownIDs));
   append_range(KeepIDs, (!ProfcheckDisableMetadataFixes ? KnownIDs
