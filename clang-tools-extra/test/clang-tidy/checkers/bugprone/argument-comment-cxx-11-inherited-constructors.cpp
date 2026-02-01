5e5f841e04e3f064f9704fe01dca569c50f6cfa8
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2026-02-01 20:04:27 +0100

    struct Derived : public Over

--- clang-tools-extra/test/clang-tidy/checkers/bugprone/argument-comment-cxx-11-inherited-constructors.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/bugprone/argument-comment-cxx-11-inherited-constructors.cpp
@@ -8,15 +8,25 @@ struct Over : public Base {
   using Base::Base;
 };
 
+struct Derived : public Over {
+  using Over::Over;
+};
+
 int wrong() {
-  Base b{/*vall=*/2};
-// CHECK-NOTES: [[@LINE-1]]:10: warning: argument name 'vall' in comment does not match parameter name 'val'
-// CHECK-NOTES: [[@LINE-10]]:21: note: 'val' declared here
+  Base b{/*val2=*/2};
+// CHECK-NOTES: [[@LINE-1]]:10: warning: argument name 'val2' in comment does not match parameter name 'val'
+// CHECK-NOTES: [[@LINE-14]]:21: note: 'val' declared here
 // CHECK-FIXES: Base b{/*val=*/2};
 
-  Over o{/*vall=*/3};
-// CHECK-NOTES: [[@LINE-1]]:10: warning: argument name 'vall' in comment does not match parameter name 'val'
-// CHECK-NOTES: [[@LINE-15]]:21: note: 'val' declared here
-// CHECK-NOTES: [[@LINE-12]]:15: note: actual callee ('Base') is declared here
+  Over o{/*val3=*/3};
+// CHECK-NOTES: [[@LINE-1]]:10: warning: argument name 'val3' in comment does not match parameter name 'val'
+// CHECK-NOTES: [[@LINE-19]]:21: note: 'val' declared here
+// CHECK-NOTES: [[@LINE-16]]:15: note: actual callee ('Base') is declared here
 // CHECK-FIXES: Over o{/*val=*/3};
+
+  Derived d{/*val4=*/4};
+// CHECK-NOTES: [[@LINE-1]]:13: warning: argument name 'val4' in comment does not match parameter name 'val'
+// CHECK-NOTES: [[@LINE-25]]:21: note: 'val' declared here
+// CHECK-NOTES: [[@LINE-18]]:15: note: actual callee ('Base') is declared here
+// CHECK-FIXES: Derived d{/*val=*/4};
 }
