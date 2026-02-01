1b2c334af52d92400a1582efb9b18dd742c3ba8c
Sergei Druzhkov <serzhdruzhok@gmail.com>
2026-02-01 18:29:42 +0300

    Fix format

--- lldb/source/Plugins/InstrumentationRuntime/TSan/InstrumentationRuntimeTSan.cpp
+++ lldb/source/Plugins/InstrumentationRuntime/TSan/InstrumentationRuntimeTSan.cpp
@@ -89,6 +89,7 @@ extern "C"
     void *dlsym(void* handle, const char* symbol);
     int (*ptr__tsan_get_report_loc_object_type)(void *report, unsigned long idx, const char **object_type);
 )"
+// clang-format off
 #if defined(__linux__)
 R"(
     void *const RTLD_DEFAULT = (void *)0;
@@ -100,6 +101,7 @@ R"(
   }
 )";
 #endif
+// clang-format on
 
 const char *thread_sanitizer_retrieve_report_data_command = R"(
 
