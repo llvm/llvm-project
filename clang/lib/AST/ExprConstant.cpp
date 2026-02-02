b09dbe536deef1704d5eb5ddf037d14b95b6c3a0
woruyu <1214539920@qq.com>
2026-02-02 16:29:36 +0800

    Update clang/lib/AST/ExprConstant.cpp
    Co-authored-by: Cheng Lingfei <53817093+clingfei@users.noreply.github.com>

--- clang/lib/AST/ExprConstant.cpp
+++ clang/lib/AST/ExprConstant.cpp
@@ -16170,7 +16170,6 @@ bool IntExprEvaluator::VisitBuiltinCallExpr(const CallExpr *E,
     APSInt Val;
     if (!EvaluateInteger(E->getArg(0), Val, Info))
       return false;
-    // if (Val.getBitWidth() <= 8)
     if (Val.getBitWidth() == 8 || Val.getBitWidth() == 1)
       return Success(Val, E);
 
