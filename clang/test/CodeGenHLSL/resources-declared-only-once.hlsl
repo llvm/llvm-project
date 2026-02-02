858ad83f6d394b413b90701dc003152e66e6ee53
Joao Saffran <joaosaffranllvm@gmail.com>
2026-02-02 10:09:04 -0800

    add -emit-llvm

--- clang/test/CodeGenHLSL/resources-declared-only-once.hlsl
+++ clang/test/CodeGenHLSL/resources-declared-only-once.hlsl
@@ -1,5 +1,5 @@
-// RUN: %clang_dxc -Wmissing-declarations -T lib_6_7  %s -Xclang -verify \
-// RUN:   -O3 %s -Xclang -verify
+// RUN: %clang_dxc -Wmissing-declarations -T lib_6_7 \
+// RUN:  -O3 %s -Xclang -verify  -Xclang -emit-llvm
 
 // expected-warning@+1{{declaration does not declare anything}}
 RWStructuredBuffer<float>;
