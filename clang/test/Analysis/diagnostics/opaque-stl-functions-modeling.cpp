ee7318cdb0e1cfcc4d125c2f8367ecefc637025b
Endre Fülöp <endre.fulop@sigmatechnology.com>
2026-02-02 21:34:12 +0100

    only 1 run-line is enough

--- clang/test/Analysis/diagnostics/opaque-stl-functions-modeling.cpp
+++ clang/test/Analysis/diagnostics/opaque-stl-functions-modeling.cpp
@@ -1,26 +1,16 @@
-// RUN: %clang --analyze -Xclang -verify %s
-//
-// RUN: %clang --analyze -Xclang -analyzer-dump-egraph=%t.dot -std=c++11 %s
+// RUN: %clang --analyze -Xclang -analyzer-dump-egraph=%t.dot -std=c++11 -Xclang -verify %s
 // RUN: cat %t.dot | FileCheck %s
 
 #include "../Inputs/system-header-simulator-cxx-std-suppression.h"
 
 // expected-no-diagnostics
 
-void test_sort() {
-  int arr[5];
+void test_opaque_handling() {
+  int arr[5] = {};
   std::sort(arr, arr + 5); // no-warning
-  // CHECK: \"tag\": \"apiModeling.OpaqueSTLFunctionsModeling
-}
-
-void test_stable_sort() {
-  int arr[5];
+// CHECK: \"tag\": \"apiModeling.OpaqueSTLFunctionsModeling\"
   std::stable_sort(arr, arr + 5); // no-warning
-  // CHECK: \"tag\": \"apiModeling.OpaqueSTLFunctionsModeling
-}
-
-void test_inplace_merge() {
-  int arr[5];
+// CHECK: \"tag\": \"apiModeling.OpaqueSTLFunctionsModeling\"
   std::inplace_merge(arr, arr + 2, arr + 5); // no-warning
-  // CHECK: \"tag\": \"apiModeling.OpaqueSTLFunctionsModeling
+// CHECK: \"tag\": \"apiModeling.OpaqueSTLFunctionsModeling\"
 }
