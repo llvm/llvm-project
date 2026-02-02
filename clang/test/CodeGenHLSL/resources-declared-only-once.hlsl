534e6e017b0e363ed5eb7b857829fba946c602fa
Joao Saffran <joaosaffranllvm@gmail.com>
2026-02-02 12:21:17 -0800

    make run line a one liner

--- clang/test/CodeGenHLSL/resources-declared-only-once.hlsl
+++ clang/test/CodeGenHLSL/resources-declared-only-once.hlsl
@@ -1,5 +1,4 @@
-// RUN: %clang_cc1 -triple dxilv1.7-unknown-shadermodel6.7-library \
-// RUN:   -finclude-default-header -verify -emit-llvm -o - -x hlsl %s
+// RUN: %clang_cc1 -triple dxilv1.7-unknown-shadermodel6.7-library -finclude-default-header -verify -emit-llvm -o - -x hlsl %s
 
 // expected-warning@+1{{declaration does not declare anything}}
 RWStructuredBuffer<float>;
