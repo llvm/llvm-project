f07af7bbbf19de24f33620f7a019ee9be0c28166
Endre Fülöp <endre.fulop@sigmatechnology.com>
2026-02-02 19:22:49 +0100

    update tests

---
+++ clang/test/Analysis/diagnostics/opaque-stl-functions-modeling.cpp
@@ -0,0 +1,24 @@
+// RUN: %clang --analyze -Xclang -verify %s
+//
+// RUN: %clang --analyze -Xclang -analyzer-dump-egraph=%t.dot -std=c++11 %s
+// RUN: cat %t.dot | FileCheck %s
+
+#include "../Inputs/system-header-simulator-cxx-std-suppression.h"
+
+void test_sort() {
+  int arr[5];
+  std::sort(arr, arr + 5); // no-warning
+  // CHECK: \"tag\": \"apiModeling.OpaqueSTLFunctionsModeling
+}
+
+void test_stable_sort() {
+  int arr[5];
+  std::stable_sort(arr, arr + 5); // no-warning
+  // CHECK: \"tag\": \"apiModeling.OpaqueSTLFunctionsModeling
+}
+
+void test_inplace_merge() {
+  int arr[5];
+  std::inplace_merge(arr, arr + 2, arr + 5); // no-warning
+  // CHECK: \"tag\": \"apiModeling.OpaqueSTLFunctionsModeling
+}
