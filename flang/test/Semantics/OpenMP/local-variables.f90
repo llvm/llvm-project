90ce6aa00fbebbc6955e5c8c3488c07473cca18e
Krzysztof Parzyszek <Krzysztof.Parzyszek@amd.com>
2026-02-03 11:02:17 -0600

    Add more checks for local variables

--- flang/test/Semantics/OpenMP/local-variables.f90
+++ flang/test/Semantics/OpenMP/local-variables.f90
@@ -2,29 +2,29 @@
 
 !Make sure that the local `bbb`s are their own entities.
 
-!CHECK:      !DEF: /foo (Subroutine) Subprogram
-!CHECK-NEXT: subroutine foo
-!CHECK-NEXT:  !DEF: /foo/i ObjectEntity INTEGER(4)
-!CHECK-NEXT:  integer i
-!CHECK-NEXT: !$omp parallel
-!CHECK-NEXT:  block
-!CHECK-NEXT:   block
-!CHECK-NEXT:    !DEF: /foo/OtherConstruct1/BlockConstruct1/BlockConstruct1/bbb ObjectEntity INTEGER(4)
-!CHECK-NEXT:    integer bbb
-!CHECK-NEXT:    !REF: /foo/OtherConstruct1/BlockConstruct1/BlockConstruct1/bbb
-!CHECK-NEXT:    bbb = 1
-!CHECK-NEXT:   end block
-!CHECK-NEXT:   block
-!CHECK-NEXT:    !DEF: /foo/OtherConstruct1/BlockConstruct1/BlockConstruct2/bbb ObjectEntity INTEGER(4)
-!CHECK-NEXT:    integer bbb
-!CHECK-NEXT:    !REF: /foo/OtherConstruct1/BlockConstruct1/BlockConstruct2/bbb
-!CHECK-NEXT:    bbb = 2
-!CHECK-NEXT:   end block
-!CHECK-NEXT:  end block
-!CHECK-NEXT: !$omp end parallel
-!CHECK-NEXT: end subroutine
+!CHECK-LABEL:  !DEF: /f00 (Subroutine) Subprogram
+!CHECK-NEXT:   subroutine f00
+!CHECK-NEXT:    !DEF: /f00/i ObjectEntity INTEGER(4)
+!CHECK-NEXT:    integer i
+!CHECK-NEXT:   !$omp parallel
+!CHECK-NEXT:    block
+!CHECK-NEXT:     block
+!CHECK-NEXT:      !DEF: /f00/OtherConstruct1/BlockConstruct1/BlockConstruct1/bbb ObjectEntity INTEGER(4)
+!CHECK-NEXT:      integer bbb
+!CHECK-NEXT:      !REF: /f00/OtherConstruct1/BlockConstruct1/BlockConstruct1/bbb
+!CHECK-NEXT:      bbb = 1
+!CHECK-NEXT:     end block
+!CHECK-NEXT:     block
+!CHECK-NEXT:      !DEF: /f00/OtherConstruct1/BlockConstruct1/BlockConstruct2/bbb ObjectEntity INTEGER(4)
+!CHECK-NEXT:      integer bbb
+!CHECK-NEXT:      !REF: /f00/OtherConstruct1/BlockConstruct1/BlockConstruct2/bbb
+!CHECK-NEXT:      bbb = 2
+!CHECK-NEXT:     end block
+!CHECK-NEXT:    end block
+!CHECK-NEXT:   !$omp end parallel
+!CHECK-NEXT:   end subroutine
 
-subroutine foo()
+subroutine f00()
   integer :: i
   !$omp parallel
   block
@@ -38,4 +38,58 @@ subroutine foo()
     end block
   end block
   !$omp end parallel
+end subroutine
+
+
+!CHECK-LABEL:  !DEF: /f01 (Subroutine) Subprogram
+!CHECK-NEXT:   subroutine f01
+!CHECK-NEXT:    !DEF: /f01/i ObjectEntity INTEGER(4)
+!CHECK-NEXT:    integer i
+!CHECK-NEXT:    !DEF: /f01/bbb ObjectEntity INTEGER(4)
+!CHECK-NEXT:    integer bbb
+!CHECK-NEXT:    !REF: /f01/bbb
+!CHECK-NEXT:    bbb = 0
+!CHECK-NEXT:   !$omp parallel
+!CHECK-NEXT:    block
+!CHECK-NEXT:     !DEF: /f01/OtherConstruct1/bbb (OmpShared) HostAssoc INTEGER(4)
+!CHECK-NEXT:     bbb = 1234
+!CHECK-NEXT:     block
+!CHECK-NEXT:      !DEF: /f01/OtherConstruct1/BlockConstruct1/BlockConstruct1/bbb ObjectEntity INTEGER(4)
+!CHECK-NEXT:      integer bbb
+!CHECK-NEXT:      !REF: /f01/OtherConstruct1/BlockConstruct1/BlockConstruct1/bbb
+!CHECK-NEXT:      bbb = 1
+!CHECK-NEXT:     end block
+!CHECK-NEXT:     block
+!CHECK-NEXT:      !DEF: /f01/OtherConstruct1/BlockConstruct1/BlockConstruct2/bbb ObjectEntity INTEGER(4)
+!CHECK-NEXT:      integer bbb
+!CHECK-NEXT:      !REF: /f01/OtherConstruct1/BlockConstruct1/BlockConstruct2/bbb
+!CHECK-NEXT:      bbb = 2
+!CHECK-NEXT:     end block
+!CHECK-NEXT:    end block
+!CHECK-NEXT:   !$omp end parallel
+!CHECK-NEXT:    !REF: /f01/bbb
+!CHECK-NEXT:    print *, bbb
+!CHECK-NEXT:   end subroutine
+
+subroutine f01()
+  integer :: i
+  integer :: bbb
+
+  bbb = 0
+
+  !$omp parallel
+  block
+    bbb = 1234
+    block
+      integer :: bbb
+      bbb = 1
+    end block
+    block
+      integer :: bbb
+      bbb = 2
+    end block
+  end block
+  !$omp end parallel
+
+  print *, bbb
 end subroutine
