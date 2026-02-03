9a56247b9c1132ea0b804daa5fd04f05a8a92ec6
shubhe25p <shubhp@mbm3a24.local>
2026-02-02 23:35:32 -0800

    [libc] Support %ls in printf
    Add support for %ls in printf by calling internal
    wcrtomb function and relevant end-to-end sprintf
    test. Additionally, modified printf parser for
    recognizing length modifier. This also disables
    wide string support on windows platform.

--- libc/src/stdio/printf_core/string_converter.h
+++ libc/src/stdio/printf_core/string_converter.h
@@ -9,11 +9,20 @@
 #ifndef LLVM_LIBC_SRC_STDIO_PRINTF_CORE_STRING_CONVERTER_H
 #define LLVM_LIBC_SRC_STDIO_PRINTF_CORE_STRING_CONVERTER_H
 
-#include "src/__support/CPP/string_view.h"
+#ifndef LIBC_COPT_PRINTF_DISABLE_WIDE
+#include "hdr/types/wchar_t.h"
+#include "hdr/types/wint_t.h"
+#include "hdr/wchar_macros.h"
+#include "src/__support/wchar/mbstate.h"
+#include "src/__support/wchar/wcrtomb.h"
+#endif // LIBC_COPT_PRINTF_DISABLE_WIDE
+
+#include "src/__support/CPP/algorithm.h" // cpp::min
 #include "src/__support/macros/config.h"
 #include "src/stdio/printf_core/converter_utils.h"
 #include "src/stdio/printf_core/core_structs.h"
 #include "src/stdio/printf_core/writer.h"
+#include "src/string/string_length.h"
 
 #include <stddef.h>
 
@@ -24,7 +33,41 @@ template <WriteMode write_mode>
 LIBC_INLINE int convert_string(Writer<write_mode> *writer,
                                const FormatSection &to_conv) {
   size_t string_len = 0;
-  const char *str_ptr = reinterpret_cast<const char *>(to_conv.conv_val_ptr);
+
+  if (to_conv.length_modifier == LengthModifier::l) {
+#ifndef LIBC_COPT_PRINTF_DISABLE_WIDE
+    const wchar_t *wstr_ptr =
+        reinterpret_cast<const wchar_t *>(to_conv.conv_val_ptr);
+
+#ifndef LIBC_COPT_PRINTF_NO_NULLPTR_CHECKS
+    if (wstr_ptr == nullptr) {
+      wstr_ptr = L"(null)";
+    }
+#endif // LIBC_COPT_PRINTF_NO_NULLPTR_CHECKS
+
+    char buffer[MB_LEN_MAX];
+    internal::mbstate mbstate;
+    size_t written = 0;
+    for (const wchar_t *cur_str = (wstr_ptr); cur_str[string_len];
+         ++string_len) {
+      wchar_t wc = cur_str[string_len];
+
+      auto ret = internal::wcrtomb(buffer, wc, &mbstate);
+      if (!ret.has_value()) {
+        return MB_CONVERSION_ERROR;
+      }
+      written += ret.value();
+
+      if (to_conv.precision >= 0 &&
+          static_cast<size_t>(to_conv.precision) < written) {
+        written -= ret.value();
+        break;
+      }
+    }
+    string_len = written;
+#else
+
+    const char *str_ptr = reinterpret_cast<const char *>(to_conv.conv_val_ptr);
 
 #ifndef LIBC_COPT_PRINTF_NO_NULLPTR_CHECKS
   if (str_ptr == nullptr) {
@@ -32,13 +75,21 @@ LIBC_INLINE int convert_string(Writer<write_mode> *writer,
   }
 #endif // LIBC_COPT_PRINTF_NO_NULLPTR_CHECKS
 
-  for (const char *cur_str = (str_ptr); cur_str[string_len]; ++string_len) {
-    ;
-  }
+  string_len = cpp::min(internal::string_length(str_ptr),
+                        static_cast<size_t>(to_conv.precision));
 
-  if (to_conv.precision >= 0 &&
-      static_cast<size_t>(to_conv.precision) < string_len)
-    string_len = to_conv.precision;
+#endif // LIBC_COPT_PRINTF_DISABLE_WIDE
+  } else {
+
+    const char *str_ptr = reinterpret_cast<const char *>(to_conv.conv_val_ptr);
+#ifndef LIBC_COPT_PRINTF_NO_NULLPTR_CHECKS
+    if (str_ptr == nullptr) {
+      str_ptr = "(null)";
+    }
+#endif // LIBC_COPT_PRINTF_NO_NULLPTR_CHECKS
+    string_len = cpp::min(internal::string_length(str_ptr),
+                          static_cast<size_t>(to_conv.precision));
+  }
 
   size_t padding_spaces = to_conv.min_width > static_cast<int>(string_len)
                               ? to_conv.min_width - string_len
@@ -50,7 +101,48 @@ LIBC_INLINE int convert_string(Writer<write_mode> *writer,
     RET_IF_RESULT_NEGATIVE(writer->write(' ', padding_spaces));
   }
 
-  RET_IF_RESULT_NEGATIVE(writer->write({(str_ptr), string_len}));
+  if (to_conv.length_modifier == LengthModifier::l) {
+#ifndef LIBC_COPT_PRINTF_DISABLE_WIDE
+    const wchar_t *wstr_ptr =
+        reinterpret_cast<const wchar_t *>(to_conv.conv_val_ptr);
+
+#ifndef LIBC_COPT_PRINTF_NO_NULLPTR_CHECKS
+    if (wstr_ptr == nullptr) {
+      wstr_ptr = L"(null)";
+    }
+#endif // LIBC_COPT_PRINTF_NO_NULLPTR_CHECKS
+
+    size_t written = 0;
+    char buffer[MB_LEN_MAX];
+    internal::mbstate mbstate;
+    for (size_t i = 0; written < string_len; ++i) {
+      // We don't need to check errors/precision here; Pass 1 guaranteed safety.
+      auto ret = internal::wcrtomb(buffer, wstr_ptr[i], &mbstate);
+      size_t mb_len = ret.value();
+
+      RET_IF_RESULT_NEGATIVE(writer->write({buffer, mb_len}));
+      written += mb_len;
+    }
+#else
+    const char *str_ptr = reinterpret_cast<const char *>(to_conv.conv_val_ptr);
+
+#ifndef LIBC_COPT_PRINTF_NO_NULLPTR_CHECKS
+    if (str_ptr == nullptr) {
+      str_ptr = "(null)";
+    }
+#endif // LIBC_COPT_PRINTF_NO_NULLPTR_CHECKS
+    RET_IF_RESULT_NEGATIVE(writer->write({(str_ptr), string_len}));
+#endif // LIBC_COPT_PRINTF_DISABLE_WIDE
+  } else {
+    const char *str_ptr = reinterpret_cast<const char *>(to_conv.conv_val_ptr);
+
+#ifndef LIBC_COPT_PRINTF_NO_NULLPTR_CHECKS
+    if (str_ptr == nullptr) {
+      str_ptr = "(null)";
+    }
+#endif // LIBC_COPT_PRINTF_NO_NULLPTR_CHECKS
+    RET_IF_RESULT_NEGATIVE(writer->write({(str_ptr), string_len}));
+  }
 
   // If the padding is on the right side, write the spaces last.
   if (padding_spaces > 0 &&
