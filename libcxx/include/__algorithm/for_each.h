f2f48734159925124781eb0af891a97894e5b0e2
c8ef <c8ef@outlook.com>
2026-02-03 21:09:22 +0800

    restore func; remove a forward

--- libcxx/include/__algorithm/for_each.h
+++ libcxx/include/__algorithm/for_each.h
@@ -30,23 +30,23 @@ _LIBCPP_BEGIN_NAMESPACE_STD
 
 template <class _InputIterator, class _Sent, class _Func, class _Proj>
 _LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX20 _InputIterator
-__for_each(_InputIterator __first, _Sent __last, _Func&& __f, _Proj& __proj) {
+__for_each(_InputIterator __first, _Sent __last, _Func&& __func, _Proj& __proj) {
 #ifndef _LIBCPP_CXX03_LANG
   if constexpr (using _SpecialAlg =
                     __specialized_algorithm<_Algorithm::__for_each, __iterator_pair<_InputIterator, _Sent>>;
                 _SpecialAlg::__has_algorithm) {
-    _SpecialAlg()(__first, __last, std::forward<_Func>(__f), __proj);
+    _SpecialAlg()(__first, __last, __func, __proj);
     return __last;
   } else if constexpr (is_same<_InputIterator, _Sent>::value && __is_segmented_iterator_v<_InputIterator>) {
     using __local_iterator_t = typename __segmented_iterator_traits<_InputIterator>::__local_iterator;
     std::__for_each_segment(__first, __last, [&](__local_iterator_t __lfirst, __local_iterator_t __llast) {
-      std::__for_each(__lfirst, __llast, __f, __proj);
+      std::__for_each(__lfirst, __llast, __func, __proj);
     });
     return __last;
   }
 #endif
   for (; __first != __last; ++__first)
-    std::__invoke(__f, std::__invoke(__proj, *__first));
+    std::__invoke(__func, std::__invoke(__proj, *__first));
   return __first;
 }
 
