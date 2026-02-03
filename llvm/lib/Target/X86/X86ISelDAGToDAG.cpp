e16ff3a72fd28fed888dfae955472ad1289c220b
Fangrui Song <i@maskray.me>
2026-02-02 21:49:52 -0800

    [X86] Don't fold offset into address mode in 16-bit mode
    ```
    extern int X;
    unsigned f(void) { return (unsigned)&X - 0xf0000; }
    ```
    compiles (-O1 or above, -fno-pic) to
    
    ```
    .code16
    leal X-983040, %eax  # R_386_16 relocatin, [-65535,65535]
    ```
    in 16-bit mode, which is rejected by both GNU Assembler and integrated
    assembler since #176827 .
    
    Conservatively disable offset folding entirely in 16-bit mode to avoid
    generating invalid displacements.

--- llvm/lib/Target/X86/X86ISelDAGToDAG.cpp
+++ llvm/lib/Target/X86/X86ISelDAGToDAG.cpp
@@ -1852,10 +1852,13 @@ bool X86DAGToDAGISel::foldOffsetIntoAddress(uint64_t Offset,
         !isDispSafeForFrameIndexOrRegBase((uint32_t)Val) &&
         !AM.hasBaseOrIndexReg())
       return true;
-  } else if (AM.hasBaseOrIndexReg() && !isDispSafeForFrameIndexOrRegBase(Val))
-    // For 32-bit X86, make sure the displacement still isn't close to the
-    // expressible limit.
+  } else if (Subtarget->is16Bit() || (AM.hasBaseOrIndexReg() &&
+                                      !isDispSafeForFrameIndexOrRegBase(Val))) {
+    // In 16-bit mode, displacements are limited to [-65535,65535] (
+    // R_386_16 relocation). For simplicity, don't fold offsets. For
+    // 32-bit, ensure the displacement isn't close to the limit.
     return true;
+  }
   AM.Disp = Val;
   return false;
 }
