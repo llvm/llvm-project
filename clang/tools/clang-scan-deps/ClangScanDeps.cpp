2e51b884ee2752670f869cc17f8201e09b56283d
Jan Svoboda <jan_svoboda@apple.com>
2026-02-04 15:18:22 -0800

    [clang][deps] Parallelize module compilations

--- clang/tools/clang-scan-deps/ClangScanDeps.cpp
+++ clang/tools/clang-scan-deps/ClangScanDeps.cpp
@@ -94,6 +94,7 @@ static std::string TranslationUnitFile;
 static bool DeprecatedDriverCommand;
 static ResourceDirRecipeKind ResourceDirRecipe;
 static bool Verbose;
+static bool AsyncScanModules;
 static bool PrintTiming;
 static bool EmitVisibleModules;
 static llvm::BumpPtrAllocator Alloc;
@@ -238,6 +239,8 @@ static void ParseArgs(int argc, char **argv) {
   EmitVisibleModules = Args.hasArg(OPT_emit_visible_modules);
 
   Verbose = Args.hasArg(OPT_verbose);
+
+  AsyncScanModules = Args.hasArg(OPT_async_scan_modules);
 
   RoundTripArgs = Args.hasArg(OPT_round_trip_args);
 
@@ -1136,7 +1139,8 @@ int clang_scan_deps_main(int argc, char **argv, const llvm::ToolContext &) {
   };
 
   DependencyScanningService Service(ScanMode, Format, OptimizeArgs,
-                                    EagerLoadModules, /*TraceVFS=*/Verbose);
+                                    AsyncScanModules, EagerLoadModules,
+                                    /*TraceVFS=*/Verbose);
 
   llvm::Timer T;
   T.startTimer();
