ffb9af44b589bcaeecac28a8f38f8174357d6213
braam-76 <braam67.gamidov@yandex.com>
2026-02-05 16:44:09 +0300

    [mlir][ExecutionEngine] HWASan disabling on OwningMemRef test
    Pull request #179655
    
    https://github.com/llvm/llvm-project/pull/179655#issuecomment-3850858519
    https://github.com/llvm/llvm-project/pull/179655#issuecomment-3853452560

--- mlir/unittests/ExecutionEngine/OwningMemRef.cpp
+++ mlir/unittests/ExecutionEngine/OwningMemRef.cpp
@@ -13,7 +13,18 @@
 using namespace ::mlir;
 using namespace ::testing;
 
-TEST(OwningMemRef, assignOverloadChaining) {
+#ifndef __has_feature
+#  define __has_feature(x) 0
+#endif
+
+// hwaddress_sanitizer needs to be turned off for this move-assignment test
+#if __has_feature(hwaddress_sanitizer)
+#  define MAYBE_assignOverloadChaining DISABLED_assignOverloadChaining
+#else
+#  define MAYBE_assignOverloadChaining assignOverloadChaining
+#endif
+
+TEST(OwningMemRef, MAYBE_assignOverloadChaining) {
   int64_t mem1Shape[] = {3};
   int64_t mem2Shape[] = {4};
 
