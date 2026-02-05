73f83d1566b35e09ba73ffed4b30cc663e90df63
Minsoo Choo <minsoochoo0122@proton.me>
2026-02-05 10:39:39 -0500

    [lldb][Process] Remove mips64 POSIX support
    Signed-off-by: Minsoo Choo <minsoochoo0122@proton.me>

--- lldb/source/Plugins/Process/Utility/LinuxSignals.cpp
+++ lldb/source/Plugins/Process/Utility/LinuxSignals.cpp
@@ -8,11 +8,7 @@
 
 #include "LinuxSignals.h"
 
-// mips-linux debugging is not supported and mips uses different numbers for
-// some signals (e.g. SIGBUS) on linux, so we skip the static checks below. The
-// definitions here can be used for debugging non-mips targets on a mips-hosted
-// lldb.
-#if defined(__linux__) && !defined(__mips__)
+#if defined(__linux__)
 #include <csignal>
 
 #ifndef SEGV_BNDERR
@@ -61,7 +57,7 @@
 #else
 #define ADD_SIGCODE(signal_name, signal_value, code_name, code_value, ...)     \
   AddSignalCode(signal_value, code_value, __VA_ARGS__)
-#endif /* if defined(__linux__) && !defined(__mips__) */
+#endif /* if defined(__linux__) */
 // See siginfo.h in the Linux Kernel, these codes can be sent for any signal.
 #define ADD_LINUX_SIGNAL(signo, name, ...)                                     \
   AddSignal(signo, name, __VA_ARGS__);                                         \
