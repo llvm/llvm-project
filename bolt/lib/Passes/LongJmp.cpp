b62f0d8a7bf0ae2ad1bfa96f7c3ad4743d2c8464
Alexandros Lamprineas <alexandros.lamprineas@arm.com>
2026-02-05 16:15:09 +0000

    Improve error message for short range branch support
    Updated error message for unsupported short range branches.

--- bolt/lib/Passes/LongJmp.cpp
+++ bolt/lib/Passes/LongJmp.cpp
@@ -71,8 +71,9 @@ static BinaryBasicBlock *getBBAtHotColdSplitPoint(BinaryFunction &Func) {
 static bool mayNeedStub(const BinaryContext &BC, const MCInst &Inst) {
   if (BC.isAArch64() && BC.MIB->isShortRangeBranch(Inst) &&
       !opts::CompactCodeModel) {
-    BC.errs() << "Short range branch " << Inst
-              << " not supported outside compact code model\n";
+    BC.errs() << "BOLT-ERROR: short range branch not supported"
+              << " outside compact code model\n";
+    BC.printInstruction(BC.errs(), Inst);
     exit(1);
   }
   return (BC.MIB->isBranch(Inst) || BC.MIB->isCall(Inst)) &&
