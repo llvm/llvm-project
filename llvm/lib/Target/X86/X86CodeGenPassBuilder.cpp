eb9e98f62e0c147482efc310e2a7509c7b07ee4b
Kyungtak Woo <kevinwkt@google.com>
2026-02-05 18:31:01 -0600

    [NewPM] Port x86-cleanup-local-dynamic-tls (#179864)
    Port x86-cleanup-local-dynamic-tls to the new pass manager.
    I moved LDTLSCleanup to a new
    llvm/lib/Target/X86/X86CleanupLocalDynamicTLS.cpp file and renamed it to
    X86CleanupLocalDynamicTLSPass. Then I renamed the legacy pass, and
    adding a NewPM wrapper around the impl.
    
    No test coverage added for now as there are no MIR->MIR tests exercising
    this pass and we do not have enough ported to run any end to end tests.

--- llvm/lib/Target/X86/X86CodeGenPassBuilder.cpp
+++ llvm/lib/Target/X86/X86CodeGenPassBuilder.cpp
@@ -105,8 +105,7 @@ Error X86CodeGenPassBuilder::addInstSelector(PassManagerWrapper &PMW) const {
   // For ELF, cleanup any local-dynamic TLS accesses
   if (TM.getTargetTriple().isOSBinFormatELF() &&
       getOptLevel() != CodeGenOptLevel::None) {
-    // TODO(boomanaiden154): Add CleanupLocalDynamicTLSPass here once it has
-    // been ported.
+    addMachineFunctionPass(X86CleanupLocalDynamicTLSPass(), PMW);
   }
 
   // TODO(boomanaiden154): Add X86GlobalPassRegPass here once it has been
