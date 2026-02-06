11882f4502b7bd9aee3d7f8f21e39d10305244ca
Minsoo Choo <minsoochoo0122@proton.me>
2026-02-06 13:43:31 -0500

    fixup! [lldb][Process/FreeBSDKernel] Fix broken debugging on aarch64
    Signed-off-by: Minsoo Choo <minsoochoo0122@proton.me>

--- lldb/source/Plugins/Process/FreeBSDKernel/RegisterContextFreeBSDKernel_arm64.cpp
+++ lldb/source/Plugins/Process/FreeBSDKernel/RegisterContextFreeBSDKernel_arm64.cpp
@@ -9,7 +9,9 @@
 #include "RegisterContextFreeBSDKernel_arm64.h"
 #include "Plugins/Process/Utility/lldb-arm64-register-enums.h"
 
+#include "lldb/Symbol/Symbol.h"
 #include "lldb/Target/Process.h"
+#include "lldb/Target/Target.h"
 #include "lldb/Target/Thread.h"
 #include "lldb/Utility/RegisterValue.h"
 #include "llvm/Support/Endian.h"
@@ -49,41 +51,104 @@ bool RegisterContextFreeBSDKernel_arm64::ReadRegister(
     llvm::support::ulittle64_t sp;
   } pcb;
 
-  Status error;
-  size_t rd =
-      m_thread.GetProcess()->ReadMemory(m_pcb_addr, &pcb, sizeof(pcb), error);
-  if (rd != sizeof(pcb))
-    return false;
+  struct {
+    llvm::support::ulittle64_t x[30];
+    llvm::support::ulittle64_t lr;
+    llvm::support::ulittle64_t _reserved;
+    llvm::support::ulittle64_t sp;
+  } pcb13;
 
-  uint32_t reg = reg_info->kinds[lldb::eRegisterKindLLDB];
-  switch (reg) {
-  case gpr_x19_arm64:
-  case gpr_x20_arm64:
-  case gpr_x21_arm64:
-  case gpr_x22_arm64:
-  case gpr_x23_arm64:
-  case gpr_x24_arm64:
-  case gpr_x25_arm64:
-  case gpr_x26_arm64:
-  case gpr_x27_arm64:
-  case gpr_x28_arm64:
-  case gpr_fp_arm64:
-    static_assert(gpr_fp_arm64 - gpr_x19_arm64 == PCB_FP,
-                  "nonconsecutive arm64 register numbers");
-    value = pcb.x[reg - gpr_x19_arm64];
-    break;
-  case gpr_lr_arm64:
-  case gpr_pc_arm64:
-    // The pc of crashing thread is stored in lr.
-    static_assert(gpr_lr_arm64 - gpr_x19_arm64 == PCB_LR,
-                  "nonconsecutive arm64 register numbers");
-    value = pcb.x[reg - gpr_x19_arm64];
-    break;
-  case gpr_sp_arm64:
-    value = pcb.sp;
-    break;
-  default:
-    return false;
+  Status error;
+
+  // TODO: LLVM 24: Remove FreeBSD 13 support
+  if (GetOsreldate() > 1400084) {
+    size_t rd =
+        m_thread.GetProcess()->ReadMemory(m_pcb_addr, &pcb, sizeof(pcb), error);
+    if (rd != sizeof(pcb))
+      return false;
+
+    uint32_t reg = reg_info->kinds[lldb::eRegisterKindLLDB];
+    switch (reg) {
+    case gpr_x19_arm64:
+    case gpr_x20_arm64:
+    case gpr_x21_arm64:
+    case gpr_x22_arm64:
+    case gpr_x23_arm64:
+    case gpr_x24_arm64:
+    case gpr_x25_arm64:
+    case gpr_x26_arm64:
+    case gpr_x27_arm64:
+    case gpr_x28_arm64:
+    case gpr_fp_arm64:
+      static_assert(gpr_fp_arm64 - gpr_x19_arm64 == PCB_FP,
+                    "nonconsecutive arm64 register numbers");
+      value = pcb.x[reg - gpr_x19_arm64];
+      break;
+    case gpr_lr_arm64:
+    case gpr_pc_arm64:
+      // The pc of crashing thread is stored in lr.
+      static_assert(gpr_lr_arm64 - gpr_x19_arm64 == PCB_LR,
+                    "nonconsecutive arm64 register numbers");
+      value = pcb.x[reg - gpr_x19_arm64];
+      break;
+    case gpr_sp_arm64:
+      value = pcb.sp;
+      break;
+    default:
+      return false;
+    }
+  } else {
+    size_t rd = m_thread.GetProcess()->ReadMemory(m_pcb_addr, &pcb13,
+                                                  sizeof(pcb13), error);
+    if (rd != sizeof(pcb13))
+      return false;
+
+    uint32_t reg = reg_info->kinds[lldb::eRegisterKindLLDB];
+    switch (reg) {
+    case gpr_x0_arm64:
+    case gpr_x1_arm64:
+    case gpr_x2_arm64:
+    case gpr_x3_arm64:
+    case gpr_x4_arm64:
+    case gpr_x5_arm64:
+    case gpr_x6_arm64:
+    case gpr_x7_arm64:
+    case gpr_x8_arm64:
+    case gpr_x9_arm64:
+    case gpr_x10_arm64:
+    case gpr_x11_arm64:
+    case gpr_x12_arm64:
+    case gpr_x13_arm64:
+    case gpr_x14_arm64:
+    case gpr_x15_arm64:
+    case gpr_x16_arm64:
+    case gpr_x17_arm64:
+    case gpr_x18_arm64:
+    case gpr_x19_arm64:
+    case gpr_x20_arm64:
+    case gpr_x21_arm64:
+    case gpr_x22_arm64:
+    case gpr_x23_arm64:
+    case gpr_x24_arm64:
+    case gpr_x25_arm64:
+    case gpr_x26_arm64:
+    case gpr_x27_arm64:
+    case gpr_x28_arm64:
+    case gpr_fp_arm64:
+      static_assert(gpr_fp_arm64 - gpr_x0_arm64 == 29,
+                    "nonconsecutive arm64 register numbers");
+      value = pcb13.x[reg - gpr_x0_arm64];
+      break;
+    case gpr_sp_arm64:
+      value = pcb13.sp;
+      break;
+    case gpr_pc_arm64:
+      // The pc of crashing thread is stored in lr.
+      value = pcb13.lr;
+      break;
+    default:
+      return false;
+    }
   }
   return true;
 }
@@ -91,4 +156,36 @@ bool RegisterContextFreeBSDKernel_arm64::ReadRegister(
 bool RegisterContextFreeBSDKernel_arm64::WriteRegister(
     const RegisterInfo *reg_info, const RegisterValue &value) {
   return false;
+}
+
+int64_t RegisterContextFreeBSDKernel_arm64::GetOsreldate() {
+  ProcessSP process_sp = m_thread.GetProcess();
+  if (!process_sp)
+    return 0;
+
+  Target &target = process_sp->GetTarget();
+
+  SymbolContextList sc_list;
+  target.GetImages().FindSymbolsWithNameAndType(ConstString("osreldate"),
+                                                lldb::eSymbolTypeData, sc_list);
+
+  if (sc_list.GetSize() > 0) {
+    SymbolContext sc;
+    sc_list.GetContextAtIndex(0, sc);
+
+    if (sc.symbol) {
+      lldb::addr_t addr = sc.symbol->GetLoadAddress(&target);
+      if (addr != LLDB_INVALID_ADDRESS) {
+        Status error;
+        int64_t osreldate = 0;
+        size_t bytes_read =
+            process_sp->ReadMemory(addr, &osreldate, sizeof(osreldate), error);
+        if (bytes_read == sizeof(osreldate) && error.Success()) {
+          return osreldate;
+        }
+      }
+    }
+  }
+
+  return 0;
 }
