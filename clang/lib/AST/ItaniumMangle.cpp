f389bf215c4b34fdbba3c4394607f35166007e01
changkhothuychung <nhat7203@gmail.com>
2026-02-06 13:37:55 -0500

    make it a break

--- clang/lib/AST/ItaniumMangle.cpp
+++ clang/lib/AST/ItaniumMangle.cpp
@@ -4954,7 +4954,7 @@ recurse:
   case Expr::CXXReflectExprClass: {
     // TODO(Reflection): implement this after introducing std::meta::info
     assert(false && "unimplemented");
-    LLVM_FALLTHROUGH;
+    break;
   }
 
   // FIXME: invent manglings for all these.
