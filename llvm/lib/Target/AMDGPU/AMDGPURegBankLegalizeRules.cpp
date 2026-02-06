cea6c2b991755936822a0c97ad637ac5f22ee7d4
Abhinav Garg <39309352+abhigargrepo@users.noreply.github.com>
2026-02-06 15:43:37 +0530

    Adding support for G_STRICT_FMA in new reg bank select (#170330)
    This patch adds legalization rules for G_STRICT_FMA opcode.
    
    ---------
    
    Co-authored-by: Abhinav Garg <abhigarg@amd.com>

--- llvm/lib/Target/AMDGPU/AMDGPURegBankLegalizeRules.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPURegBankLegalizeRules.cpp
@@ -1155,7 +1155,7 @@ RegBankLegalizeRules::RegBankLegalizeRules(const GCNSubtarget &_ST,
       .Uni(S64, {{UniInVgprS64}, {Vgpr64, Vgpr32}})
       .Div(S64, {{Vgpr64}, {Vgpr64, Vgpr32}});
 
-  addRulesForGOpcs({G_FMA}, Standard)
+  addRulesForGOpcs({G_FMA, G_STRICT_FMA}, Standard)
       .Div(S16, {{Vgpr16}, {Vgpr16, Vgpr16, Vgpr16}})
       .Div(S32, {{Vgpr32}, {Vgpr32, Vgpr32, Vgpr32}})
       .Uni(S64, {{UniInVgprS64}, {Vgpr64, Vgpr64, Vgpr64}})
