b0d0e2ee38db064885bd3b8563e7d6009d0c431b
Anatoly Trosinenko <atrosinenko@accesssoftek.com>
2026-02-07 15:51:38 +0300

    Address review comments

--- clang/test/DebugInfo/CXX/ptrauth-member-function-pointer-debuglocs.cpp
+++ clang/test/DebugInfo/CXX/ptrauth-member-function-pointer-debuglocs.cpp
@@ -8,11 +8,19 @@
 // Check that compiler-generated *_vfpthunk_ function has a !dbg location
 // attached to the call instruction.
 
-// CHECK:      define {{.*}}@_ZN1A2f0Ev_vfpthunk_{{.*}} !dbg
+// CHECK:      define {{.*}}@_ZN1A2f0Ev_vfpthunk_({{.*}})
+// CHECK-SAME:     !dbg ![[SCOPE_INDEX:[0-9]+]]
+// CHECK-NOT:  define
+// CHECK:        %[[DISCR:[0-9]+]] = call i64 @llvm.ptrauth.blend(i64 %{{[0-9]+}}, i64 9385)
 // CHECK-NOT:  define
 // CHECK:        musttail call void %{{[0-9]+}}(ptr
-// CHECK-SAME:     [ "ptrauth"(i32 0, i64 %{{[0-9]+}}) ]
-// CHECK-SAME:     !dbg
+// CHECK-SAME:     [ "ptrauth"(i32 0, i64 %[[DISCR]]) ]
+// CHECK-SAME:     !dbg ![[LOCATION_INDEX:[0-9]+]]
+
+// CHECK:      ![[SCOPE_INDEX]] = distinct !DISubprogram(
+// CHECK-SAME:                        linkageName: "_ZN1A2f0Ev_vfpthunk_"
+// CHECK-SAME:                        flags: DIFlagArtificial | DIFlagThunk
+// CHECK:      ![[LOCATION_INDEX]] = !DILocation(line: 0, scope: ![[SCOPE_INDEX]])
 
 volatile long T;
 
