15832a69675a82192dccf78953a3c89849ae8d22
Iasonaskrpr <126663668+Iasonaskrpr@users.noreply.github.com>
2026-02-07 16:03:49 +0200

    [libc][math] Refactor sqrtf128 to header only (#177760)
    Closes #177652

--- libc/shared/math.h
+++ libc/shared/math.h
@@ -104,6 +104,7 @@
 #include "math/sinpif.h"
 #include "math/sqrt.h"
 #include "math/sqrtf.h"
+#include "math/sqrtf128.h"
 #include "math/sqrtf16.h"
 #include "math/tan.h"
 #include "math/tanf.h"
--- libc/test/shared/shared_math_test.cpp
+++ libc/test/shared/shared_math_test.cpp
@@ -171,6 +171,8 @@ TEST(LlvmLibcSharedMathTest, AllFloat128) {
   EXPECT_FP_EQ(float128(0.0), LIBC_NAMESPACE::shared::logbf128(float128(1.0)));
   EXPECT_FP_EQ(0.0, LIBC_NAMESPACE::shared::dfmaf128(
                         float128(0.0), float128(0.0), float128(0.0)));
+  EXPECT_FP_EQ(float128(0x1p+0),
+               LIBC_NAMESPACE::shared::sqrtf128(float128(1.0)));
 
   EXPECT_EQ(0L, LIBC_NAMESPACE::shared::llogbf128(float128(1.0)));
 }
