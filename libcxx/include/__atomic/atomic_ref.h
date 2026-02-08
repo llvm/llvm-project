dfce3c7d45c1dca9e1ac85b293321ef19e007b23
c8ef <c8ef@outlook.com>
2026-02-08 19:37:33 +0800

    address review comments: use builtin if possible

--- libcxx/include/__atomic/atomic_ref.h
+++ libcxx/include/__atomic/atomic_ref.h
@@ -301,20 +301,28 @@ struct atomic_ref<_Tp> : public __atomic_ref_base<_Tp> {
   }
 #  if _LIBCPP_STD_VER >= 26
   _LIBCPP_HIDE_FROM_ABI _Tp fetch_max(_Tp __arg, memory_order __order = memory_order_seq_cst) const noexcept {
+#    if __has_builtin(__atomic_fetch_max)
+    return __atomic_fetch_max(this->__ptr_, __arg, std::__to_gcc_order(__order));
+#    else
     _Tp __old = this->load(memory_order_relaxed);
     _Tp __new;
     do {
       __new = __old > __arg ? __old : __arg;
     } while (!this->compare_exchange_weak(__old, __new, __order, memory_order_relaxed));
     return __old;
+#    endif
   }
   _LIBCPP_HIDE_FROM_ABI _Tp fetch_min(_Tp __arg, memory_order __order = memory_order_seq_cst) const noexcept {
+#    if __has_builtin(__atomic_fetch_min)
+    return __atomic_fetch_min(this->__ptr_, __arg, std::__to_gcc_order(__order));
+#    else
     _Tp __old = this->load(memory_order_relaxed);
     _Tp __new;
     do {
       __new = __old < __arg ? __old : __arg;
     } while (!this->compare_exchange_weak(__old, __new, __order, memory_order_relaxed));
     return __old;
+#    endif
   }
 #  endif
 
