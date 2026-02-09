f792a492ac40ebbf187ae97c92dca05ccf6bf46b
jack <jackhuang1205@gmail.com>
2026-02-09 17:22:18 +0800

    [llvm/CAS] Put llvm_unreachable in end of function

--- llvm/lib/CAS/OnDiskGraphDB.cpp
+++ llvm/lib/CAS/OnDiskGraphDB.cpp
@@ -1422,11 +1422,14 @@ OnDiskGraphDB::FileBackedData
 StandaloneDataInMemory::getInternalFileBackedObjectData(
     StringRef RootPath) const {
   switch (SK) {
+  case TrieRecord::StorageKind::Unknown:
+  case TrieRecord::StorageKind::DataPool:
+    llvm_unreachable("unexpected storage kind");
   case TrieRecord::StorageKind::Standalone:
     return OnDiskGraphDB::FileBackedData{getContent().getData(),
                                          /*FileInfo=*/std::nullopt};
   case TrieRecord::StorageKind::StandaloneLeaf0:
-  case TrieRecord::StorageKind::StandaloneLeaf: {
+  case TrieRecord::StorageKind::StandaloneLeaf:
     bool IsFileNulTerminated = SK == TrieRecord::StorageKind::StandaloneLeaf0;
     SmallString<256> Path;
     ::getStandalonePath(RootPath, TrieRecord::getStandaloneFilePrefix(SK),
@@ -1435,11 +1438,7 @@ StandaloneDataInMemory::getInternalFileBackedObjectData(
         getContent().getData(), OnDiskGraphDB::FileBackedData::FileInfoTy{
                                     std::string(Path), IsFileNulTerminated}};
   }
-  case TrieRecord::StorageKind::Unknown:
-  case TrieRecord::StorageKind::DataPool:
-  default:
-    llvm_unreachable("unexpected storage kind");
-  }
+  llvm_unreachable("unexpected storage kind");
 }
 
 static Expected<MappedTempFile>
