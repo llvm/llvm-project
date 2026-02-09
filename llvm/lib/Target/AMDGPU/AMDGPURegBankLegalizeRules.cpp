404f9e6c993cb26fff1888f943284f1f6e86adc0
vangthao95 <vang.thao@amd.com>
2026-02-09 09:18:03 -0800

    AMDGPU/GlobalISel: RegBankLegalize rules for amdgcn_sffbh (#180099)
    Change test to use update_llc_test_checks.py and make `v_flbit` test
    actually divergent.

--- llvm/lib/Target/AMDGPU/AMDGPURegBankLegalizeRules.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPURegBankLegalizeRules.cpp
@@ -1379,6 +1379,10 @@ RegBankLegalizeRules::RegBankLegalizeRules(const GCNSubtarget &_ST,
       .Any({{UniS32}, {{UniInVgprS32}, {IntrId, Vgpr32}}})
       .Any({{DivS32}, {{Vgpr32}, {IntrId, Vgpr32}}});
 
+  addRulesForIOpcs({amdgcn_sffbh}, Standard)
+      .Uni(S32, {{Sgpr32}, {IntrId, Sgpr32}})
+      .Div(S32, {{Vgpr32}, {IntrId, Vgpr32}});
+
   addRulesForIOpcs({amdgcn_ubfe, amdgcn_sbfe}, Standard)
       .Div(S32, {{Vgpr32}, {IntrId, Vgpr32, Vgpr32, Vgpr32}})
       .Uni(S32, {{Sgpr32}, {IntrId, Sgpr32, Sgpr32, Sgpr32}, S_BFE})
