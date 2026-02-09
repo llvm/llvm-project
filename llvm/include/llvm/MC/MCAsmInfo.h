f133ff9490ea91989051fc189cacce0181ba243f
Fangrui Song <i@maskray.me>
2026-02-08 17:04:30 -0800

    [MC] Move MCTargetOptions ownership from MCContext to MCAsmInfo
    Except MC-internal `MCAsmInfo()` uses, MCAsmInfo is always constructed
    with `const MCTargetOptions &` via `TargetRegistry::createMCAsmInfo`
    (https://reviews.llvm.org/D41349). Store the pointer in MCAsmInfo and
    change `MCContext::getTargetOptions()` to retrieve it from there,
    removing the `MCTargetOptions const *TargetOptions` member from
    MCContext.
    
    MCContext's constructor still accepts an MCTargetOptions parameter
    for now. An assert verifies it matches MCAsmInfo's copy when provided.
    A subsequent change will remove this parameter and update all callers.
    
    In addition, assert that MCTargetOptions is non-null in the MCAssembler
    constructor and remove the now-unnecessary null checks in
    ELFObjectWriter.

--- llvm/include/llvm/MC/MCAsmInfo.h
+++ llvm/include/llvm/MC/MCAsmInfo.h
@@ -434,6 +434,8 @@ protected:
   llvm::StringMap<uint32_t> NameToAtSpecifier;
   void initializeAtSpecifiers(ArrayRef<AtSpecifier>);
 
+  const MCTargetOptions *TargetOptions = nullptr;
+
 public:
   explicit MCAsmInfo();
   virtual ~MCAsmInfo();
@@ -441,6 +443,9 @@ public:
   // Explicitly non-copyable.
   MCAsmInfo(MCAsmInfo const &) = delete;
   MCAsmInfo &operator=(MCAsmInfo const &) = delete;
+
+  const MCTargetOptions *getTargetOptions() const { return TargetOptions; }
+  void setTargetOptions(const MCTargetOptions &TO) { TargetOptions = &TO; }
 
   /// Get the code pointer size in bytes.
   unsigned getCodePointerSize() const { return CodePointerSize; }
