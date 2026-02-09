70f5c20a68ff2e425624024045ae07f1a685b42c
Ruoyu Qiu <cabbaken@outlook.com>
2026-02-09 04:56:55 +0000

    Reland PR 174731, resolve cyclic dependency issue
    Signed-off-by: Ruoyu Qiu <cabbaken@outlook.com>

--- llvm/include/llvm/TargetParser/Triple.h
+++ llvm/include/llvm/TargetParser/Triple.h
@@ -1036,6 +1036,8 @@ public:
                : PointerWidth == 64;
   }
 
+  bool isAVR() const { return getArch() == Triple::avr; }
+
   /// Tests whether the target is 32-bit LoongArch.
   bool isLoongArch32() const { return getArch() == Triple::loongarch32; }
 
