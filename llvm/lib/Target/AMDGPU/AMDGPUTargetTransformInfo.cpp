77150507354f358e13c2ae5547e9ff174c4f1acd
Krzysztof Drewniak <Krzysztof.Drewniak@amd.com>
2026-02-09 17:15:32 +0000

    Remove the ashr case as non-canononical

--- llvm/lib/Target/AMDGPU/AMDGPUTargetTransformInfo.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPUTargetTransformInfo.cpp
@@ -1081,8 +1081,7 @@ bool GCNTTIImpl::isAlwaysUniform(const Value *V) const {
   auto MaybeCastTidX = m_CastOrSelf(MaybeMaskedTidX);
   auto MaybeMaskedCastTidX =
       m_CombineOr(m_c_And(MaybeCastTidX, m_Value()), MaybeCastTidX);
-  if (match(V, m_LShr(MaybeMaskedCastTidX, m_ConstantInt(C))) ||
-      match(V, m_AShr(MaybeMaskedCastTidX, m_ConstantInt(C)))) {
+  if (match(V, m_LShr(MaybeMaskedCastTidX, m_ConstantInt(C)))) {
     return C >= ST->getWavefrontSizeLog2() && XDimDoesntResetWithinWaves;
   }
 
