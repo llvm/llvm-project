3c55e4ff774c8a09d5be749a5b32362c76ccd598
Koakuma <koachan@protonmail.com>
2026-02-10 08:31:41 +0700

    [SPARC] Add TTI implementation for getting register numbers and widths
    Correctly inform transform passes about our registers; this prevents
    the issue with the `find-last` test where the loop vectorizer pass
    mistakenly thinks that the backend has vector capabilities.
    
    Fixes https://github.com/sparclinux/issues/issues/69

--- llvm/lib/Target/Sparc/SparcTargetTransformInfo.cpp
+++ llvm/lib/Target/Sparc/SparcTargetTransformInfo.cpp
@@ -20,3 +20,52 @@ SparcTTIImpl::getPopcntSupport(unsigned TyWidth) const {
     return TTI::PSK_FastHardware;
   return TTI::PSK_Software;
 }
+
+unsigned SparcTTIImpl::getRegisterClassForType(bool Vector, Type *Ty) const {
+  if (Vector)
+    return VRRC;
+  if (Ty &&
+      (Ty->getScalarType()->isFloatTy() || Ty->getScalarType()->isDoubleTy()))
+    return FPRRC;
+  if (Ty && (Ty->getScalarType()->isFP128Ty()))
+    return FP128RRC;
+  return GPRRC;
+}
+
+unsigned SparcTTIImpl::getNumberOfRegisters(unsigned ClassID) const {
+  switch (ClassID) {
+  case GPRRC:
+    // %g0, %g6, %g7, %o6, %i6, and %i7 are used for special purposes so we
+    // discount them here.
+    return 26;
+  case FPRRC:
+    return 32;
+  case FP128RRC:
+    return 16;
+  case VRRC:
+    // TODO We have vector capabilities as part of the VIS extensions, but the
+    // codegen doesn't currently use it. Revisit this when vector codegen is
+    // ready.
+    return 0;
+  }
+
+  llvm_unreachable("Unsupported register class");
+}
+
+TypeSize
+SparcTTIImpl::getRegisterBitWidth(TargetTransformInfo::RegisterKind K) const {
+  switch (K) {
+  case TargetTransformInfo::RGK_Scalar:
+    // TODO When targeting V8+ ABI, G and O registers are 64-bit.
+    return TypeSize::getFixed(ST->is64Bit() ? 64 : 32);
+  case TargetTransformInfo::RGK_FixedWidthVector:
+    // TODO We have vector capabilities as part of the VIS extensions, but the
+    // codegen doesn't currently use it. Revisit this when vector codegen is
+    // ready.
+    return TypeSize::getFixed(0);
+  case TargetTransformInfo::RGK_ScalableVector:
+    return TypeSize::getScalable(0);
+  }
+
+  llvm_unreachable("Unsupported register kind");
+}
