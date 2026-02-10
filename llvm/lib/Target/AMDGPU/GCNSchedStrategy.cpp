199f049ae6a3b27fb753e98c5a01d4c10f409476
Tony Linthicum <tlinthic@gmail.com>
2026-02-10 08:50:46 -0600

    Reenable AMDGPU MFMA rewrite scheduler stage.
    After performance testing, it was determined that the large number of copies that
    are inserted outside the loop are more than offset by better allocation within
    the loop as a result of the rewrite.  Additionally, there are two other fairly
    minor changes in this commit:
    
      - Fix for upstream issue #177696.  The need for this fix will go away when we
        remove the need to do an early conversion to MFMA form in order to get
        proper spill cost analysis of the potential rewritten code.
    
      - Convert cost logic to double from int64.  This cleans up the logic a bit.

--- llvm/lib/Target/AMDGPU/GCNSchedStrategy.cpp
+++ llvm/lib/Target/AMDGPU/GCNSchedStrategy.cpp
@@ -2184,7 +2184,6 @@ bool RewriteMFMAFormStage::initHeuristics(
 
       int ReplacementOp = AMDGPU::getMFMASrcCVDstAGPROp(MI.getOpcode());
       assert(ReplacementOp != -1);
-
       RewriteCands.push_back({&MI, MI.getOpcode()});
       MI.setDesc(TII->get(ReplacementOp));
 
