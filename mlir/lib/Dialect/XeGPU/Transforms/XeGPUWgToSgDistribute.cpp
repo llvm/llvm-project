6a01493a6bfabd54e05ba815e9d8bf23de162d3a
nbpatel <nishant.b.patel@intel.com>
2026-02-11 18:26:29 +0000

    Clean up

--- mlir/lib/Dialect/XeGPU/Transforms/XeGPUWgToSgDistribute.cpp
+++ mlir/lib/Dialect/XeGPU/Transforms/XeGPUWgToSgDistribute.cpp
@@ -1850,7 +1850,7 @@ void XeGPUWgToSgDistributePass::runOnOperation() {
 
   // Remove layout attributes from SCF ops
   getOperation()->walk([](Operation *op) {
-    if (isa<scf::IfOp, scf::ForOp, scf::WhileOp, scf::ConditionOp>(op)) {
+    if (isa<scf::SCFDialect>(op->getDialect())) {
       xegpu::removeLayoutAttrs(op);
     }
   });
