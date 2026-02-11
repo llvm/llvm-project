ab2e722ef7185ebd8f1ec1ae6ea427954634913c
Hui Xie <hui.xie1990@gmail.com>
2026-02-11 12:10:34 +0000

    [libc++] Fix native wait alignment

---
+++ libcxx/test/libcxx/atomics/atomics.syn/wait.native.compile.pass.cpp
@@ -0,0 +1,40 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++03, c++11, c++14, c++17
+// UNSUPPORTED: no-threads
+
+#include <atomic>
+#include <cstddef>
+#include <type_traits>
+
+template <std::size_t Size, std::size_t Align>
+struct alignas(Align) Data {
+  char buffer[Size];
+};
+
+static_assert(std::__has_native_atomic_wait<std::__cxx_contention_t>);
+
+#if defined(_LIBCPP_ABI_ATOMIC_WAIT_NATIVE_BY_SIZE) && defined(__APPLE__)
+
+static_assert(std::__has_native_atomic_wait<Data<4, 4>>);
+static_assert(std::__has_native_atomic_wait<Data<8, 8>>);
+
+static_assert(!std::has_unique_object_representations_v<Data<4, 8>>);
+static_assert(!std::__has_native_atomic_wait<Data<4, 8>>,
+              "Object with !has_unique_object_representations_v should not have native wait");
+
+static_assert(!std::__has_native_atomic_wait<Data<1, 1>>, "Should only support native wait for 4 and 8 byte types");
+
+static_assert(!std::__has_native_atomic_wait<Data<4, 1>>,
+              "Should only support native wait for types with properly aligned types");
+
+static_assert(!std::__has_native_atomic_wait<Data<8, 1>>,
+              "Should only support native wait for types with properly aligned types");
+
+#endif
