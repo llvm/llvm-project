54cdd903b87211f71972f684b8532357c435aa7a
Alexey Bataev <a.bataev@outlook.com>
2026-02-11 04:53:15 -0800

    [SLP]Skip operands comparing on non-matching (but compatible) instructions
    If the instructions are compatible but non-matching (zext-select pair as
    example), no need to perform operands analysis, just return that they
    are matching.

--- llvm/lib/Transforms/Vectorize/SLPVectorizer.cpp
+++ llvm/lib/Transforms/Vectorize/SLPVectorizer.cpp
@@ -2676,6 +2676,8 @@ public:
 
       // Recursion towards the operands of I1 and I2. We are trying all possible
       // operand pairs, and keeping track of the best score.
+      if (I1->getNumOperands() != I2->getNumOperands())
+        return LookAheadHeuristics::ScoreSameOpcode;
       for (unsigned OpIdx1 = 0, NumOperands1 = I1->getNumOperands();
            OpIdx1 != NumOperands1; ++OpIdx1) {
         // Try to pair op1I with the best operand of I2.
