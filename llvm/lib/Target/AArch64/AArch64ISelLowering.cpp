4b8f69a42779975c7b4b3ecf9dceb92df10bfdfa
Jonathan Thackray <jonathan.thackray@arm.com>
2026-02-12 13:26:16 +0000

    fixup! Adjust ConvertOps.push_back() call

--- llvm/lib/Target/AArch64/AArch64ISelLowering.cpp
+++ llvm/lib/Target/AArch64/AArch64ISelLowering.cpp
@@ -20719,7 +20719,7 @@ tryToReplaceScalarFPConversionWithSVE(SDNode *N, SelectionDAG &DAG,
   SmallVector<SDValue, 2> ConvertOps = {Vec};
   if (N->getOpcode() == ISD::FP_TO_SINT_SAT ||
       N->getOpcode() == ISD::FP_TO_UINT_SAT)
-    ConvertOps.push_back(N->getOperand(1));
+    ConvertOps.push_back(DAG.getValueType(DestVecTy.getVectorElementType()));
   SDValue Convert = DAG.getNode(N->getOpcode(), DL, DestVecTy, ConvertOps);
   return DAG.getNode(ISD::EXTRACT_VECTOR_ELT, DL, DestTy, Convert, ZeroIdx);
 }
