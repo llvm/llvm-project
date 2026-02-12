24acb8c7e568ca5e1d0968fb8edfcf66e0631400
Paul Kirth <paulkirth@google.com>
2026-02-12 13:38:20 -0800

    Remove redundant code

--- llvm/tools/llvm-lto2/llvm-lto2.cpp
+++ llvm/tools/llvm-lto2/llvm-lto2.cpp
@@ -334,7 +334,6 @@ static int run(int argc, char **argv) {
     Conf.TimeTraceGranularity = TimeTraceGranularity;
   }
   Conf.CPU = codegen::getMCPU();
-  Conf.ModifyTargetOptions = [](llvm::TargetOptions &TO) {};
   Conf.MAttrs = codegen::getMAttrs();
   if (auto RM = codegen::getExplicitRelocModel())
     Conf.RelocModel = *RM;
