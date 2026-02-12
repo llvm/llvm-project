e6d72baa0ab128a857d04e8bdced8a719cd9c40c
xinlongchen <xinlongchen@tencent.com>
2026-02-12 17:19:14 +0800

    delete useless config

--- libc/src/__support/math/tanhf16.h
+++ libc/src/__support/math/tanhf16.h
@@ -1,4 +1,4 @@
-//===-- Single-precision tanhf16 function --------------------------------===//
+//===-- Single-precision tanhf16 function ---------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
