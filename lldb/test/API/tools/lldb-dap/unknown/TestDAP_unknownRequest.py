6d045c19d70caf5882b0daf9fd3fa9dcd548d715
Sergei Druzhkov <serzhdruzhok@gmail.com>
2026-02-12 19:52:07 +0300

    Update tests

--- lldb/test/API/tools/lldb-dap/unknown/TestDAP_unknownRequest.py
+++ lldb/test/API/tools/lldb-dap/unknown/TestDAP_unknownRequest.py
@@ -11,13 +11,25 @@ class TestDAP_unknown_request(lldbdap_testcase.DAPTestCaseBase):
     Tests handling of unknown request.
     """
 
-    def test(self):
+    def test_no_arguments(self):
         program = self.getBuildArtifact("a.out")
         self.build_and_launch(program, stopOnEntry=True)
         self.dap_server.request_configurationDone()
         self.dap_server.wait_for_stopped()
 
         response = self.dap_server.request_custom("unknown")
+        self.assertFalse(response["success"])
+        self.assertEqual(response["body"]["error"]["format"], "Unknown request")
+
+        self.continue_to_exit()
+
+    def test_with_arguments(self):
+        program = self.getBuildArtifact("a.out")
+        self.build_and_launch(program, stopOnEntry=True)
+        self.dap_server.request_configurationDone()
+        self.dap_server.wait_for_stopped()
+
+        response = self.dap_server.request_custom("unknown", {"foo": "bar", "id": 42})
         self.assertFalse(response["success"])
         self.assertEqual(response["body"]["error"]["format"], "Unknown request")
 
