27ed41c626b955fff6e843c78e00be9a92b1afa8
SiHuaN <liyongtai@iscas.ac.cn>
2026-02-13 10:14:32 +0800

    Fix list alphabetization and line alignment

--- clang/lib/Headers/riscv_simd.h
+++ clang/lib/Headers/riscv_simd.h
@@ -1,4 +1,4 @@
-/*===---- riscv_simd.h - RISC-V P intrinsics -----------------===
+/*===---- riscv_simd.h - RISC-V P intrinsics -------------------------------===
  *
  * Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
  * See https://llvm.org/LICENSE.txt for license information.
