55d6986c9c454150d3b3f4e158fd0f2f1e43b098
Viktor Moros <vmoros@users.noreply.github.com>
2026-02-13 09:47:15 -0500

    Fix header comment in libc/src/__support/math/bf16addf.h

--- libc/src/__support/math/bf16addf.h
+++ libc/src/__support/math/bf16addf.h
@@ -1,4 +1,4 @@
-//===-- Implementation header for bf16addf -------------------*- C++ -*-===//
+//===-- Implementation header for bf16addf ----------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
