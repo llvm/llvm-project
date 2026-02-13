aa8c6e250f44b5e519432a9f430821b1d677709f
Jason Molenda <jmolenda@apple.com>
2026-02-13 13:48:17 -0800

    ws fix

--- lldb/unittests/ObjectFile/MachO/TestObjectFileMachO.cpp
+++ lldb/unittests/ObjectFile/MachO/TestObjectFileMachO.cpp
@@ -41,8 +41,7 @@ TEST_F(ObjectFileMachOTest, ModuleFromSharedCacheInfo) {
 
   ModuleSpec mod_spec;
   mod_spec.GetFileSpec() = FileSpec("/usr/lib/libobjc.A.dylib");
-  SharedCacheImageInfo image_info =
-      HostInfo::GetSharedCacheImageInfo(mod_spec);
+  SharedCacheImageInfo image_info = HostInfo::GetSharedCacheImageInfo(mod_spec);
   EXPECT_TRUE(image_info.GetUUID());
   EXPECT_TRUE(image_info.GetExtractor());
 
@@ -91,8 +90,7 @@ TEST_F(ObjectFileMachOTest, ModuleFromSharedCacheInfo) {
 TEST_F(ObjectFileMachOTest, IndirectSymbolsInTheSharedCache) {
   ModuleSpec mod_spec;
   mod_spec.GetFileSpec() = FileSpec("/System/Library/Frameworks/AppKit.framework/Versions/C/AppKit");
-  SharedCacheImageInfo image_info = HostInfo::GetSharedCacheImageInfo(
-      mod_spec);
+  SharedCacheImageInfo image_info = HostInfo::GetSharedCacheImageInfo(mod_spec);
   ModuleSpec spec(FileSpec(), UUID(), image_info.GetExtractor());
   lldb::ModuleSP module = std::make_shared<Module>(spec);
 
