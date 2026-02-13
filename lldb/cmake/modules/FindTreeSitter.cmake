e79276a78e3ae98df2b08a095688954a2c8b15e7
Jonas Devlieghere <jonas@devlieghere.com>
2026-02-13 11:28:40 -0800

    Require tree-sitter cli binary

--- lldb/cmake/modules/FindTreeSitter.cmake
+++ lldb/cmake/modules/FindTreeSitter.cmake
@@ -8,11 +8,15 @@ find_path(TreeSitter_INCLUDE_DIR
 find_library(TreeSitter_LIBRARY
   NAMES tree-sitter treesitter)
 
+find_program(TreeSitter_BINARY
+  NAMES tree-sitter)
+
 find_package_handle_standard_args(TreeSitter
-  REQUIRED_VARS TreeSitter_LIBRARY TreeSitter_INCLUDE_DIR
+  REQUIRED_VARS TreeSitter_LIBRARY TreeSitter_INCLUDE_DIR TreeSitter_BINARY
 )
 
 mark_as_advanced(
   TreeSitter_INCLUDE_DIR
   TreeSitter_LIBRARY
+  TreeSitter_BINARY
 )
