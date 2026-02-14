da20fc4992ed4912c071d76041dc7ee7a612d0f2
Seraphimt <svet58585@mail.ru>
2026-02-14 18:14:56 +0300

    Add Wno-sequence-point in tests x2.

--- clang/test/CodeGen/SystemZ/builtins-systemz-zvector5.c
+++ clang/test/CodeGen/SystemZ/builtins-systemz-zvector5.c
@@ -1,10 +1,10 @@
 // REQUIRES: systemz-registered-target
 // RUN: %clang_cc1 -target-cpu z17 -triple s390x-linux-gnu \
 // RUN: -O2 -fzvector -flax-vector-conversions=none \
-// RUN: -Wall -Wno-unused -Werror -emit-llvm %s -o - | FileCheck %s
+// RUN: -Wall -Wno-sequence-point -Wno-unused -Werror -emit-llvm %s -o - | FileCheck %s
 // RUN: %clang_cc1 -target-cpu z17 -triple s390x-linux-gnu \
 // RUN: -O2 -fzvector -flax-vector-conversions=none \
-// RUN: -Wall -Wno-unused -Werror -S %s -o - | FileCheck %s --check-prefix=CHECK-ASM
+// RUN: -Wall -Wno-sequence-point -Wno-unused -Werror -S %s -o - | FileCheck %s --check-prefix=CHECK-ASM
 
 #include <vecintrin.h>
 
