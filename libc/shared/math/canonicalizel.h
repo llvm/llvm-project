1da1762be6e855d76a787c6a3e0af185e39e113e
Ezzeldin Ibrahim <ezzibrahimx@gmail.com>
2026-02-14 10:19:43 +0200

    [libc][math] Refactor canonicalize family to header-only

--- libc/shared/math/canonicalizel.h
+++ libc/shared/math/canonicalizel.h
@@ -1,4 +1,4 @@
-//===-- Implementation of canonicalizel function---------------------------===//
+//===-- Shared canonicalizel function ---------------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,14 +6,18 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "src/math/canonicalizel.h"
+#ifndef LLVM_LIBC_SHARED_MATH_CANONICALIZEL_H
+#define LLVM_LIBC_SHARED_MATH_CANONICALIZEL_H
+
+#include "shared/libc_common.h"
 #include "src/__support/math/canonicalizel.h"
 
 namespace LIBC_NAMESPACE_DECL {
+namespace shared {
 
-LLVM_LIBC_FUNCTION(int, canonicalizel,
-                   (long double *cx, const long double *x)) {
-  return math::canonicalizel(cx, x);
-}
+using math::canonicalizel;
 
+} // namespace shared
 } // namespace LIBC_NAMESPACE_DECL
+
+#endif // LLVM_LIBC_SHARED_MATH_CANONICALIZEL_H
