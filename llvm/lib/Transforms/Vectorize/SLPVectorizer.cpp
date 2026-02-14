edf66fb7bca293b3849e9e242560344450be87f8
Ryan Buchner <buchner.ryan@gmail.com>
2026-02-13 16:49:49 -0800

    [SLP] Support UDiv in BinOpSameOpcodeHelper
    UDiv X, 1 is an identity

--- llvm/lib/Transforms/Vectorize/SLPVectorizer.cpp
+++ llvm/lib/Transforms/Vectorize/SLPVectorizer.cpp
@@ -952,9 +952,9 @@ class BinOpSameOpcodeHelper {
   using MaskType = std::uint_fast16_t;
   /// Sort SupportedOp because it is used by binary_search.
   constexpr static std::initializer_list<unsigned> SupportedOp = {
-      Instruction::Add, Instruction::Sub,  Instruction::Mul,
-      Instruction::Shl, Instruction::LShr, Instruction::AShr,
-      Instruction::And, Instruction::Or,   Instruction::Xor};
+      Instruction::Add, Instruction::Sub,  Instruction::Mul,  Instruction::UDiv,
+      Instruction::Shl, Instruction::LShr, Instruction::AShr, Instruction::And,
+      Instruction::Or,  Instruction::Xor};
   static_assert(llvm::is_sorted_constexpr(SupportedOp) &&
                 "SupportedOp is not sorted.");
   enum : MaskType {
@@ -967,7 +967,8 @@ class BinOpSameOpcodeHelper {
     OrBIT = 0b1000000,
     XorBIT = 0b10000000,
     LShrBIT = 0b100000000,
-    MainOpBIT = 0b1000000000,
+    UDivBIT = 0b1000000000,
+    MainOpBIT = 0b10000000000,
     LLVM_MARK_AS_BITMASK_ENUM(MainOpBIT)
   };
   /// Return a non-nullptr if either operand of I is a ConstantInt.
@@ -984,7 +985,8 @@ class BinOpSameOpcodeHelper {
     if (auto *CI = dyn_cast<ConstantInt>(BinOp->getOperand(1)))
       return {CI, 1};
     if (Opcode == Instruction::Sub || Opcode == Instruction::Shl ||
-        Opcode == Instruction::AShr || Opcode == Instruction::LShr)
+        Opcode == Instruction::AShr || Opcode == Instruction::LShr ||
+        Opcode == Instruction::UDiv)
       return {nullptr, 0};
     if (auto *CI = dyn_cast<ConstantInt>(BinOp->getOperand(0)))
       return {CI, 0};
@@ -994,7 +996,7 @@ class BinOpSameOpcodeHelper {
     const Instruction *I = nullptr;
     /// The bit it sets represents whether MainOp can be converted to.
     MaskType Mask = MainOpBIT | XorBIT | OrBIT | AndBIT | SubBIT | AddBIT |
-                    MulBIT | AShrBIT | ShlBIT | LShrBIT;
+                    MulBIT | AShrBIT | ShlBIT | LShrBIT | UDivBIT;
     /// We cannot create an interchangeable instruction that does not exist in
     /// VL. For example, VL [x + 0, y * 1] can be converted to [x << 0, y << 0],
     /// but << does not exist in VL. In the end, we convert VL to [x * 1, y *
@@ -1037,6 +1039,8 @@ class BinOpSameOpcodeHelper {
         return Instruction::Xor;
       if (Candidate & LShrBIT)
         return Instruction::LShr;
+      if (Candidate & UDivBIT)
+        return Instruction::UDiv;
       llvm_unreachable("Cannot find interchangeable instruction.");
     }
 
@@ -1062,11 +1066,12 @@ class BinOpSameOpcodeHelper {
         return Candidate & XorBIT;
       case Instruction::LShr:
         return Candidate & LShrBIT;
+      case Instruction::UDiv:
+        return Candidate & UDivBIT;
       case Instruction::FAdd:
       case Instruction::FSub:
       case Instruction::FMul:
       case Instruction::SDiv:
-      case Instruction::UDiv:
       case Instruction::FDiv:
       case Instruction::SRem:
       case Instruction::URem:
@@ -1102,6 +1107,7 @@ class BinOpSameOpcodeHelper {
         }
         break;
       case Instruction::Mul:
+      case Instruction::UDiv:
         assert(FromCIValue.isPowerOf2() && "Cannot convert the instruction.");
         if (ToOpcode == Instruction::Shl) {
           RHS = ConstantInt::get(
@@ -1171,7 +1177,8 @@ public:
            "BinOpSameOpcodeHelper only accepts BinaryOperator.");
     unsigned Opcode = I->getOpcode();
     MaskType OpcodeInMaskForm;
-    // Prefer Shl, AShr, Mul, Add, Sub, And, Or, Xor, and LShr over MainOp.
+    // Prefer Shl, AShr, Mul, Add, Sub, And, Or, Xor, LShr, and UDiv over
+    // MainOp.
     switch (Opcode) {
     case Instruction::Shl:
       OpcodeInMaskForm = ShlBIT;
@@ -1200,6 +1207,9 @@ public:
     case Instruction::LShr:
       OpcodeInMaskForm = LShrBIT;
       break;
+    case Instruction::UDiv:
+      OpcodeInMaskForm = UDivBIT;
+      break;
     default:
       return MainOp.equal(Opcode) ||
              (initializeAltOp(I) && AltOp.equal(Opcode));
@@ -1208,7 +1218,8 @@ public:
     ConstantInt *CI = isBinOpWithConstantInt(I).first;
     if (CI) {
       constexpr MaskType CanBeAll = XorBIT | OrBIT | AndBIT | SubBIT | AddBIT |
-                                    MulBIT | AShrBIT | ShlBIT | LShrBIT;
+                                    MulBIT | AShrBIT | ShlBIT | LShrBIT |
+                                    UDivBIT;
       const APInt &CIValue = CI->getValue();
       switch (Opcode) {
       case Instruction::Shl:
