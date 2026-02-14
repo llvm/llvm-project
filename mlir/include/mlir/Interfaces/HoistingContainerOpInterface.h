2f02dd090c31ff4c329a1cab593c2141f3ccf16a
linuxlonelyeagle <2020382038@qq.com>
2026-02-14 08:07:47 +0000

    add comment and fix nit.

--- mlir/include/mlir/Interfaces/HoistingContainerOpInterface.h
+++ mlir/include/mlir/Interfaces/HoistingContainerOpInterface.h
@@ -6,14 +6,18 @@
 //
 //===----------------------------------------------------------------------===//
 
-#ifndef MLIR_INTERFACES_HOISTING_CONTAINER_OP_INTERFACE
-#define MLIR_INTERFACES_HOISTING_CONTAINER_OP_INTERFACE
+#ifndef MLIR_INTERFACES_HOISTING_CONTAINER_OP_INTERFACE_H_
+#define MLIR_INTERFACES_HOISTING_CONTAINER_OP_INTERFACE_H_
 
 #include "mlir/IR/OpDefinition.h"
 #include "mlir/Interfaces/HoistingContainerOpInterface.h.inc"
 
 namespace mlir {
+/// Returns true if the given operation implements HoistingContainerOpInterface
+/// and its implementation allows hosting hoisted operations. Returns false
+/// if the operation does not implement the interface, or if the operation
+/// explicitly disallows hoisting.
 bool canContainHoistedOps(Operation *op);
 } // namespace mlir
 
-#endif // MLIR_INTERFACES_HOISTING_CONTAINER_OP_INTERFACE
+#endif // MLIR_INTERFACES_HOISTING_CONTAINER_OP_INTERFACE_H_
