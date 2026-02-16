d63ec8b68adcc82632f039b562173a1287e23091
Ezzeldin Ibrahim <ezzibrahimx@gmail.com>
2026-02-16 17:27:19 +0200

    [libc][math] Refactor nexttoward family to header-only

--- libc/shared/math/nexttowardbf16.h
+++ libc/shared/math/nexttowardbf16.h
@@ -1,4 +1,4 @@
-//===-- Implementation of nexttowardbf16 function -------------------------===//
+//===-- Shared nexttowardbf16 function --------------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,13 +6,18 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "src/math/nexttowardbf16.h"
+#ifndef LLVM_LIBC_SHARED_MATH_NEXTTOWARDBF16_H
+#define LLVM_LIBC_SHARED_MATH_NEXTTOWARDBF16_H
+
+#include "shared/libc_common.h"
 #include "src/__support/math/nexttowardbf16.h"
 
 namespace LIBC_NAMESPACE_DECL {
+namespace shared {
 
-LLVM_LIBC_FUNCTION(bfloat16, nexttowardbf16, (bfloat16 x, long double y)) {
-  return math::nexttowardbf16(x, y);
-}
+using math::nexttowardbf16;
 
+} // namespace shared
 } // namespace LIBC_NAMESPACE_DECL
+
+#endif // LLVM_LIBC_SHARED_MATH_NEXTTOWARDBF16_H
