820bf60c666e2e0c08dd75e3f55b7fa08144680b
mtx <mitchell.xu2@gmail.com>
2026-02-17 02:22:28 +0800

    fixup the testcases...

--- clang/test/Tooling/serialize-diagnostics.cpp
+++ clang/test/Tooling/serialize-diagnostics.cpp
@@ -1,5 +1,9 @@
-// RUN: clang-check %s -- -Wunknown-warning-option --serialize-diagnostics %t.dia 2>&1 | FileCheck %s --allow-empty
+// RUN: clang-check %s -- -Wdoes-not-exist --serialize-diagnostics %t.dia 2>&1 | FileCheck %s
 // RUN: ls %t.dia
+
+// CHECK: warning: unknown warning option '-Wdoes-not-exist'
+// CHECK-NOT: Assertion failed
+// CHECK-NOT: Stack dump
 
 int main() {
   return 0;
