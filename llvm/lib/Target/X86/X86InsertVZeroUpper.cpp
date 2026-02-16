c802df996195bd4b109e76a45d784eb20277d72e
Kyungtak Woo <kevinwkt@google.com>
2026-02-16 03:35:05 +0000

    rename issuevzeroupper to insertvzeroupper

--- llvm/lib/Target/X86/X86InsertVZeroUpper.cpp
+++ llvm/lib/Target/X86/X86InsertVZeroUpper.cpp
@@ -1,4 +1,4 @@
-//===--- X86InsertVZeroUpper.cpp - AVX vzeroupper instruction inserter ----===//
+//===---- X86InsertVZeroUpper.cpp - AVX vzeroupper instruction inserter ---===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
