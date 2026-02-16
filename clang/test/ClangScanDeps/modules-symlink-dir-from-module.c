21e391cde8096028fc8753b001a7d7ce0a316dc9
Cyndy Ishida <cyndy_ishida@apple.com>
2026-02-16 14:49:00 -0800

    [clang-scan-deps] Enable Module Relocation check
    * Fix tests that assumed no header search lookup was used.
    * Most of the test fixes were updating FileDeps that resolved files
      relative to search paths.

--- clang/test/ClangScanDeps/modules-symlink-dir-from-module.c
+++ clang/test/ClangScanDeps/modules-symlink-dir-from-module.c
@@ -10,6 +10,8 @@
 // RUN: sed -e "s|DIR|%/t|g" %t/cdb.json.in > %t/cdb.json
 // RUN: ln -s module %t/include/symlink-to-module
 
+// RUN: touch %t/session.timestamp
+
 // RUN: clang-scan-deps -compilation-database %t/cdb.json -j 1 \
 // RUN:   -format experimental-full  -mode=preprocess-dependency-directives \
 // RUN:   -optimize-args=all -module-files-dir %t/build > %t/deps.json
@@ -28,7 +30,7 @@
 //--- cdb.json.in
 [{
   "directory": "DIR",
-  "command": "clang -fsyntax-only DIR/test.c -F DIR/Frameworks -I DIR/include -fmodules -fimplicit-module-maps -fmodules-cache-path=DIR/module-cache",
+  "command": "clang -fsyntax-only DIR/test.c -F DIR/Frameworks -I DIR/include -fmodules -fimplicit-module-maps -fmodules-cache-path=DIR/module-cache -fbuild-session-file=DIR/session.timestamp -fmodules-validate-once-per-build-session",
   "file": "DIR/test.c"
 }]
 
