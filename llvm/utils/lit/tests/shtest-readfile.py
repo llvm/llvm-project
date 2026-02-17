1fcc0715e4a6653b1fafaaa0c025503624c6ada3
Junji Watanabe <jwata@google.com>
2026-02-17 15:00:50 +0900

    relax path maches for lit tests

--- llvm/utils/lit/tests/shtest-readfile.py
+++ llvm/utils/lit/tests/shtest-readfile.py
@@ -3,13 +3,13 @@
 # TODO(boomanaiden154): This sometimes fails, possibly due to buffers not being flushed.
 # ALLOW_RETRIES: 2
 
-# RUN: env LIT_USE_INTERNAL_SHELL=1  not %{lit} -v %{inputs}/shtest-readfile | FileCheck -match-full-lines -DTEMP_PATH=%S%{fs-sep}Inputs%{fs-sep}shtest-readfile%{fs-sep}Output %s
+# RUN: env LIT_USE_INTERNAL_SHELL=1  not %{lit} -v %{inputs}/shtest-readfile | FileCheck -match-full-lines %s
 
 # CHECK: -- Testing: 5 tests{{.*}}
 
 # CHECK-LABEL: FAIL: shtest-readfile :: absolute-paths.txt ({{[^)]*}})
 # CHECK: echo hello
-# CHECK: # executed command: echo '%{readfile:[[TEMP_PATH]]{{[\\\/]}}absolute-paths.txt.tmp}'
+# CHECK: # executed command: echo '%{readfile:{{.*}}absolute-paths.txt.tmp}'
 
 # CHECK-LABEL: FAIL: shtest-readfile :: env.txt ({{[^)]*}})
 # CHECK: env TEST=hello {{.*}} -c "import os; print(os.environ['TEST'])"
@@ -25,4 +25,4 @@
 
 # CHECK-LABEL: FAIL: shtest-readfile :: two-same-line.txt ({{[^)]*}})
 # CHECK: echo hello bye
-# CHECK: # executed command: echo '%{readfile:[[TEMP_PATH]]{{[\\\/]}}two-same-line.txt.tmp.1}' '%{readfile:[[TEMP_PATH]]{{[\\\/]}}two-same-line.txt.tmp.2}'
+# CHECK: # executed command: echo '%{readfile:{{.*}}two-same-line.txt.tmp.1}' '%{readfile:{{.*}}two-same-line.txt.tmp.2}'
