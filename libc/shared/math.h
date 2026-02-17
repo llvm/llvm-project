9bf11863d14ebaeeb9c58a0733b89a67e91a629d
Dhruv <62135445+dhr412@users.noreply.github.com>
2026-02-18 03:04:38 +0530

    [libc][math] Refactor logb implementation to header-only (#175719)
    Fixes #175365

--- libc/shared/math.h
+++ libc/shared/math.h
@@ -107,6 +107,7 @@
 #include "math/log2.h"
 #include "math/log2f.h"
 #include "math/log2f16.h"
+#include "math/logb.h"
 #include "math/logbf.h"
 #include "math/logbf128.h"
 #include "math/logbf16.h"
