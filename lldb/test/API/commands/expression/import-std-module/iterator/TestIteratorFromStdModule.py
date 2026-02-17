360f826a89f6be296fa941464b169581dee39d19
William Tran-Viet <wtranviet@proton.me>
2026-02-16 23:57:23 -0500

    Did wrong thing

--- lldb/test/API/commands/expression/import-std-module/iterator/TestIteratorFromStdModule.py
+++ lldb/test/API/commands/expression/import-std-module/iterator/TestIteratorFromStdModule.py
@@ -9,8 +9,9 @@ from lldbsuite.test import lldbutil
 
 class TestCase(TestBase):
     @add_test_categories(["libc++"])
-    @skipIf
-    def test(self):
+    @skipIf(compiler=no_match("clang"))
+    @expectedFailureAll(bugnumber="https://github.com/llvm/llvm-project/issues/149477")
+    def test_xfail(self):
         self.build()
 
         lldbutil.run_to_source_breakpoint(
@@ -18,13 +19,6 @@ class TestCase(TestBase):
         )
 
         self.runCmd("settings set target.import-std-module true")
-
-        iter_type = "std::move_iterator<std::__wrap_iter<int *> >"
-
-        self.expect_expr("move_begin", result_type=iter_type)
-        self.expect_expr("move_begin[0]", result_type="int", result_value="1")
-
-        self.expect_expr("move_begin + 3 == move_end", result_value="true")
 
         self.expect("expr move_begin++")
         self.expect_expr("move_begin + 2 == move_end", result_value="true")
