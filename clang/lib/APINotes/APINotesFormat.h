85eb3ba5ee3d1f8b15a896bbcf6f4d2626eafec7
flash1729 <sherlockedaditya@gmail.com>
2026-02-18 07:14:48 +0530

    [APINotes][NFC] Fix typos and header comment errors

--- clang/lib/APINotes/APINotesFormat.h
+++ clang/lib/APINotes/APINotesFormat.h
@@ -1,4 +1,4 @@
-//===-- APINotesWriter.h - API Notes Writer ---------------------*- C++ -*-===//
+//===-- APINotesFormat.h - API Notes Format ---------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
