cddc78e9c4391755d263580bf448811395b69ac4
Nick Sarnie <nick.sarnie@intel.com>
2026-02-18 13:04:42 -0800

    use unsupported for libc malloc
    Signed-off-by: Nick Sarnie <nick.sarnie@intel.com>

--- offload/test/libc/malloc_parallel.c
+++ offload/test/libc/malloc_parallel.c
@@ -1,6 +1,7 @@
 // RUN: %libomptarget-compile-run-and-check-generic
 // RUN: %libomptarget-compileopt-run-and-check-generic
-// REQUIRES: libc
+// https://github.com/llvm/llvm-project/issues/182119
+// UNSUPPORTED: intelgpu
 
 #include <omp.h>
 #include <stdio.h>
