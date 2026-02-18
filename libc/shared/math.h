d58d805028417bc6b23885b8af0259d3a647bb8a
hulxv <hulxxv@gmail.com>
2026-02-18 23:26:46 +0200

    [libc][math] Refactor dmul family to header-only
    Refactored functions:
      - dmulf128
      - dmull

--- libc/shared/math.h
+++ libc/shared/math.h
@@ -83,6 +83,8 @@
 #include "math/daddl.h"
 #include "math/dfmaf128.h"
 #include "math/dfmal.h"
+#include "math/dmulf128.h"
+#include "math/dmull.h"
 #include "math/dsqrtl.h"
 #include "math/erfcf16.h"
 #include "math/erff.h"
--- libc/shared/math/dmull.h
+++ libc/shared/math/dmull.h
@@ -1,4 +1,4 @@
-//===-- Implementation of dmulf128 function -------------------------------===//
+//===-- Shared dmull function -----------------------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,13 +6,17 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "src/math/dmulf128.h"
-#include "src/__support/math/dmulf128.h"
+#ifndef LLVM_LIBC_SHARED_MATH_DMULL_H
+#define LLVM_LIBC_SHARED_MATH_DMULL_H
+
+#include "src/__support/math/dmull.h"
 
 namespace LIBC_NAMESPACE_DECL {
+namespace shared {
 
-LLVM_LIBC_FUNCTION(double, dmulf128, (float128 x, float128 y)) {
-  return math::dmulf128(x, y);
-}
+using math::dmull;
 
+} // namespace shared
 } // namespace LIBC_NAMESPACE_DECL
+
+#endif // LLVM_LIBC_SHARED_MATH_DMULL_H
