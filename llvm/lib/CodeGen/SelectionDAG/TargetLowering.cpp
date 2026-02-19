b345fcf32f85b607f1bbf980823f9731b1896c85
Simon Pilgrim <llvm-dev@redking.me.uk>
2026-02-19 16:47:02 +0000

    Limit the fold to pow2 types and allow general shift amount bounds

--- llvm/lib/CodeGen/SelectionDAG/TargetLowering.cpp
+++ llvm/lib/CodeGen/SelectionDAG/TargetLowering.cpp
@@ -2256,28 +2256,32 @@ bool TargetLowering::SimplifyDemandedBits(
       }
     }
 
-    // Fold FSHR(Op0,Op1,Op2) -> SRL(Op1,Op2)
-    // iff we're guaranteed not to use Op0.
-    // TODO: Add FSHL equivalent?
-    if (!IsFSHL && !DemandedBits.isAllOnes() &&
-        (!TLO.LegalOperations() || isOperationLegal(ISD::SRL, VT))) {
-      KnownBits KnownAmt =
-          TLO.DAG.computeKnownBits(Op2, DemandedElts, Depth + 1);
-      if (KnownAmt.getMaxValue().ult(BitWidth)) {
-        unsigned MaxShiftAmt = KnownAmt.getMaxValue().getZExtValue();
+    if (isPowerOf2_32(BitWidth)) {
+      // Fold FSHR(Op0,Op1,Op2) -> SRL(Op1,Op2)
+      // iff we're guaranteed not to use Op0.
+      // TODO: Add FSHL equivalent?
+      if (!IsFSHL && !DemandedBits.isAllOnes() &&
+          (!TLO.LegalOperations() || isOperationLegal(ISD::SRL, VT))) {
+        unsigned MaxShiftAmt = BitWidth - 1; // urem(Op2, BitWidth)
+        KnownBits KnownAmt =
+            TLO.DAG.computeKnownBits(Op2, DemandedElts, Depth + 1);
+        if (KnownAmt.getMaxValue().ult(BitWidth))
+          MaxShiftAmt = KnownAmt.getMaxValue().getZExtValue();
         // Check we don't demand any shifted bits outside Op1.
         if (DemandedBits.countl_zero() >= MaxShiftAmt) {
-          SDValue NewOp = TLO.DAG.getNode(ISD::SRL, dl, VT, Op1, Op2);
+          EVT AmtVT = Op2.getValueType();
+          SDValue NewAmt =
+              TLO.DAG.getNode(ISD::AND, dl, AmtVT, Op2,
+                              TLO.DAG.getConstant(BitWidth - 1, dl, AmtVT));
+          SDValue NewOp = TLO.DAG.getNode(ISD::SRL, dl, VT, Op1, NewAmt);
           return TLO.CombineTo(Op, NewOp);
         }
       }
-    }
 
-    // For pow-2 bitwidths we only demand the bottom modulo amt bits.
-    if (isPowerOf2_32(BitWidth)) {
+      // For pow-2 bitwidths we only demand the bottom modulo amt bits.
       APInt DemandedAmtBits(Op2.getScalarValueSizeInBits(), BitWidth - 1);
-      if (SimplifyDemandedBits(Op2, DemandedAmtBits, DemandedElts,
-                               Known2, TLO, Depth + 1))
+      if (SimplifyDemandedBits(Op2, DemandedAmtBits, DemandedElts, Known2, TLO,
+                               Depth + 1))
         return true;
     }
     break;
