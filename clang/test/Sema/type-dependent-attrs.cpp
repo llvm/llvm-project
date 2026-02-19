688d584e8e39b872b1944b5592807a2aee42aaf9
Tony Guillot <tony.guillot@protonmail.com>
2026-02-19 16:46:29 +0100

    Added test case with init_priority being used with a templated type

--- clang/test/Sema/type-dependent-attrs.cpp
+++ clang/test/Sema/type-dependent-attrs.cpp
@@ -8,4 +8,15 @@ namespace InitPriorityAttribute {
   [[gnu::init_priority(1000)]] S2 invalid_var = s1; // expected-error {{no viable conversion from 'struct S1' to 'S2'}} \
                                                        expected-note@#S2_INIT_PRIORITY {{candidate constructor (the implicit copy constructor) not viable: no known conversion from 'struct S1' to 'const S2 &' for 1st argument}} \
                                                        expected-note@#S2_INIT_PRIORITY {{candidate constructor (the implicit move constructor) not viable: no known conversion from 'struct S1' to 'S2 &&' for 1st argument}}
+
+  template<typename T>
+  struct ST { // #ST_INIT_PRIORITY
+    T inner;
+  };
+
+  [[gnu::init_priority(2000)]] auto auto_t_var = ST<int>{0};
+  [[gnu::init_priority(2000)]] ST<int> struct_t_var = ST<int>{0};
+  [[gnu::init_priority(2000)]] ST<float> invalid_t_var = ST<int>{0}; // expected-error {{no viable conversion from 'ST<int>' to 'ST<float>'}} \
+                                                                             expected-note@#ST_INIT_PRIORITY {{candidate constructor (the implicit copy constructor) not viable: no known conversion from 'ST<int>' to 'const ST<float> &' for 1st argument}} \
+                                                                             expected-note@#ST_INIT_PRIORITY {{candidate constructor (the implicit move constructor) not viable: no known conversion from 'ST<int>' to 'ST<float> &&' for 1st argument}}
 }
