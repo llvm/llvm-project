939d95a41567c523ad0b289373b7c55351bcb62f
xinlongchen <xinlongchen@tencent.com>
2026-02-19 10:24:01 +0800

    [X86] computeKnownBitsForTargetNode - add X86ISD::FXOR coverage

--- llvm/lib/Target/X86/X86ISelLowering.cpp
+++ llvm/lib/Target/X86/X86ISelLowering.cpp
@@ -39340,6 +39340,14 @@ void X86TargetLowering::computeKnownBitsForTargetNode(const SDValue Op,
     Known |= Known2;
     break;
   }
+  case X86ISD::FXOR: {
+    KnownBits Known2;
+    Known = DAG.computeKnownBits(Op.getOperand(0), DemandedElts, Depth + 1);
+    Known2 = DAG.computeKnownBits(Op.getOperand(1), DemandedElts, Depth + 1);
+
+    Known ^= Known2;
+    break;
+  }
   case X86ISD::PSADBW: {
     SDValue LHS = Op.getOperand(0);
     SDValue RHS = Op.getOperand(1);
