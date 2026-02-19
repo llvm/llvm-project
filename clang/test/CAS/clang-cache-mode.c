7c9408d60b0bc5eeb22517080f811fce45f636f9
Steven Wu <stevenwu@apple.com>
2026-02-19 12:50:57 -0800

    [TEST-ONLY] Avoid writing to system default CAS
    Fix a test case that writes to system default CAS that leaks its
    temporary output outside the build directory.

--- clang/test/CAS/clang-cache-mode.c
+++ clang/test/CAS/clang-cache-mode.c
@@ -1,3 +1,3 @@
-// RUN: %clang-cache %clang -v -fsyntax-only -x c %s 2>&1 | FileCheck %s
+// RUN: env LLVM_CACHE_CAS_PATH=%t/cas %clang-cache %clang -v -fsyntax-only -x c %s 2>&1 | FileCheck %s
 
 // CHECK: -fdepscan
