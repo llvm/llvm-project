4bfa6364a384250bdd5ba7ebb9b796d77a48f36f
Khatavkar, Yashasvi <yashasvi.khatavkar@intel.com>
2026-02-19 06:43:38 -0800

    Apply clang-format

--- clang/lib/Parse/ParseOpenMP.cpp
+++ clang/lib/Parse/ParseOpenMP.cpp
@@ -5109,7 +5109,7 @@ bool Parser::ParseOpenMPVarList(OpenMPDirectiveKind DKind,
       if (!T.consumeClose())
         Data.RLoc = T.getCloseLocation();
       return false; // Success
-    } 
+    }
     if (Tok.is(tok::comma)) {
       Vars.push_back(FirstExpr.get());
       while (Tok.is(tok::comma)) {
