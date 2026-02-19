46880609196dfa759ae566f1193219e8976cd339
Florian Hahn <flo@fhahn.com>
2026-02-19 22:13:45 +0000

    !fixup restore assert, thanks

--- llvm/lib/Transforms/Vectorize/VPlanTransforms.cpp
+++ llvm/lib/Transforms/Vectorize/VPlanTransforms.cpp
@@ -2108,6 +2108,7 @@ static bool simplifyBranchConditionForVFAndUF(VPlan &Plan, ElementCount BestVF,
                                 {}, {}, Term->getDebugLoc());
   ExitingVPBB->appendRecipe(BOC);
   Term->eraseFromParent();
+
   return true;
 }
 
@@ -2712,9 +2713,8 @@ void VPlanTransforms::removeBranchOnConst(VPlan &Plan) {
     if (VPBB->empty() || !match(&VPBB->back(), m_BranchOnCond(m_VPValue(Cond))))
       continue;
 
-    // BranchOnCond requires exactly 2 successors.
-    if (VPBB->getNumSuccessors() != 2)
-      continue;
+    assert(VPBB->getNumSuccessors() == 2 &&
+           "Two successors expected for BranchOnCond");
     unsigned RemovedIdx;
     if (match(Cond, m_True()))
       RemovedIdx = 1;
