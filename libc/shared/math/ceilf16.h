5dc1c2fe25e9cce94a8c58d21fbbffeea079ac56
Mohamed Emad <hulxxv@gmail.com>
2026-02-20 01:32:17 +0200

    [libc][math] Refactor ceil family to header-only (#182121)
    Refactors the ceil math family to be header-only.
    
    Closes https://github.com/llvm/llvm-project/issues/182120
    
    Target Functions:
      - ceil
      - ceilbf16
      - ceilf
      - ceilf128
      - ceilf16
      - ceill

--- libc/shared/math/ceilf16.h
+++ libc/shared/math/ceilf16.h
@@ -1,4 +1,4 @@
-//===-- Implementation of ceilf16 function --------------------------------===//
+//===-- Shared ceilf16 function ---------------------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,11 +6,23 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "src/math/ceilf16.h"
+#ifndef LLVM_LIBC_SHARED_MATH_CEILF16_H
+#define LLVM_LIBC_SHARED_MATH_CEILF16_H
+
+#include "include/llvm-libc-macros/float16-macros.h"
+
+#ifdef LIBC_TYPES_HAS_FLOAT16
+
 #include "src/__support/math/ceilf16.h"
 
 namespace LIBC_NAMESPACE_DECL {
+namespace shared {
 
-LLVM_LIBC_FUNCTION(float16, ceilf16, (float16 x)) { return math::ceilf16(x); }
+using math::ceilf16;
 
+} // namespace shared
 } // namespace LIBC_NAMESPACE_DECL
+
+#endif // LIBC_TYPES_HAS_FLOAT16
+
+#endif // LLVM_LIBC_SHARED_MATH_CEILF16_H
