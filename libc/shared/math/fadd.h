9914d6297258d59ab97ac7506f5b0612c4ad4f1f
Mohamed Emad <hulxxv@gmail.com>
2026-02-20 01:59:33 +0200

    [libc][math] Refactor fadd family to header-only (#182185)
    Refactors the fadd math family to be header-only.
    
    Closes https://github.com/llvm/llvm-project/issues/182184
    
    Target Functions:
      - fadd
      - faddf128
      - faddl

--- libc/shared/math/fadd.h
+++ libc/shared/math/fadd.h
@@ -1,4 +1,4 @@
-//===-- Implementation of faddl function ----------------------------------===//
+//===-- Shared fadd function ------------------------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,13 +6,17 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "src/math/faddl.h"
-#include "src/__support/math/faddl.h"
+#ifndef LLVM_LIBC_SHARED_MATH_FADD_H
+#define LLVM_LIBC_SHARED_MATH_FADD_H
+
+#include "src/__support/math/fadd.h"
 
 namespace LIBC_NAMESPACE_DECL {
+namespace shared {
 
-LLVM_LIBC_FUNCTION(float, faddl, (long double x, long double y)) {
-  return math::faddl(x, y);
-}
+using math::fadd;
 
+} // namespace shared
 } // namespace LIBC_NAMESPACE_DECL
+
+#endif // LLVM_LIBC_SHARED_MATH_FADD_H
