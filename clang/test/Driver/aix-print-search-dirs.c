b0942663237ae87478fbaac459d49ccbc1557b41
Tony Varghese <tonypalampalliyil@gmail.com>
2026-02-20 04:56:31 +0530

    Update clang/test/Driver/aix-print-search-dirs.c
    Co-authored-by: David Tenty <daltenty.dev@gmail.com>

--- clang/test/Driver/aix-print-search-dirs.c
+++ clang/test/Driver/aix-print-search-dirs.c
@@ -2,18 +2,13 @@
 
 // RUN: %clang -print-search-dirs --target=powerpc-ibm-aix7.3.0.0 \
 // RUN:        --sysroot=%S/Inputs/aix_ppc_tree 2>&1 \
-// RUN:   | FileCheck --check-prefix=CHECK-PRINT-SEARCH-DIRS-32 %s
-
-// CHECK-PRINT-SEARCH-DIRS-32: programs: =
-// CHECK-PRINT-SEARCH-DIRS-32: libraries: =
-// CHECK-PRINT-SEARCH-DIRS-32-SAME: {{.*}}/usr/lib
-// CHECK-PRINT-SEARCH-DIRS-32-SAME: {{.*}}/lib
+// RUN:   | FileCheck --check-prefix=CHECK-PRINT-SEARCH-DIRS %s
 
 // RUN: %clang -print-search-dirs --target=powerpc64-ibm-aix7.3.0.0 \
 // RUN:        --sysroot=%S/Inputs/aix_ppc_tree 2>&1 \
-// RUN:   | FileCheck --check-prefix=CHECK-PRINT-SEARCH-DIRS-64 %s
+// RUN:   | FileCheck --check-prefix=CHECK-PRINT-SEARCH-DIRS %s
 
-// CHECK-PRINT-SEARCH-DIRS-64: programs: =
-// CHECK-PRINT-SEARCH-DIRS-64: libraries: =
-// CHECK-PRINT-SEARCH-DIRS-64-SAME: {{.*}}/usr/lib
-// CHECK-PRINT-SEARCH-DIRS-64-SAME: {{.*}}/lib
+// CHECK-PRINT-SEARCH-DIRS: programs: =
+// CHECK-PRINT-SEARCH-DIRS: libraries: =
+// CHECK-PRINT-SEARCH-DIRS-SAME: {{.*}}/usr/lib
+// CHECK-PRINT-SEARCH-DIRS-SAME: {{.*}}/lib
