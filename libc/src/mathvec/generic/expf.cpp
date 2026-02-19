82d00f3827326069da4fccda563e45eb2c6135cc
Sterling Augustine <saugustine@google.com>
2026-02-18 17:01:46 -0800

    Simpler method.

--- libc/src/mathvec/generic/expf.cpp
+++ libc/src/mathvec/generic/expf.cpp
@@ -8,23 +8,12 @@
 
 #include "src/mathvec/expf.h"
 #include "src/__support/mathvec/expf.h"
-#include <typeinfo>
 
 namespace LIBC_NAMESPACE_DECL {
 
 cpp::simd<float> expf(cpp::simd<float> x) { return mathvec::expf(x); }
 
-#ifdef __clang__
-#define EXPF_MANGLED_NAME _ZN22__llvm_libc_22_0_0_git5expfEDv8_f
-#else
-#ifdef __some_other_compiler__
-#define EXPF_MANGLED_NAME other_mangled_name
-#endif
-#endif
-extern expf_simd_float_ftype EXPF_MANGLED_NAME;
-
 } // namespace LIBC_NAMESPACE_DECL
 
-extern const LIBC_NAMESPACE::expf_simd_float_ftype __expf_simd_float =
-    reinterpret_cast<LIBC_NAMESPACE::expf_simd_float_ftype>(
-        &LIBC_NAMESPACE::EXPF_MANGLED_NAME);
+LIBC_NAMESPACE::cpp::simd<float> (*__llvm_libc_expf_cpp_simd_float)(
+    LIBC_NAMESPACE::cpp::simd<float> x) = LIBC_NAMESPACE::expf;
