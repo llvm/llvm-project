14646d609b29711c2afb34e57bf05ec0fe508591
Razvan Lupusoru <rlupusoru@nvidia.com>
2026-02-18 18:16:21 -0800

    [mlir][acc] Add attributes for parallelism dimensions
    As OpenACC gets lowered to eventually mapping to GPU (via
    GPU dialect), we need to track parallelism assignment which
    we can use in how variables get privatized, how barriers
    and synchronizations are inserted to ensure appropriate
    OpenACC execution model, and for loop work-sharing. This
    adds GPUParallelDimAttr and GPUParallelDimsAttr for this.

--- mlir/include/mlir/Dialect/OpenACC/OpenACC.h
+++ mlir/include/mlir/Dialect/OpenACC/OpenACC.h
@@ -21,6 +21,7 @@
 
 #include "mlir/Bytecode/BytecodeOpInterface.h"
 #include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/Dialect/GPU/IR/GPUDialect.h"
 #include "mlir/Dialect/OpenACC/OpenACCOpsDialect.h.inc"
 #include "mlir/Dialect/OpenACC/OpenACCOpsEnums.h.inc"
 #include "mlir/Dialect/OpenACC/OpenACCOpsInterfaces.h.inc"
