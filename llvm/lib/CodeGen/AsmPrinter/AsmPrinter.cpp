6767bfeec5ede8a878792e39aedff053c36854b2
Peter Collingbourne <pcc@google.com>
2026-02-20 11:22:51 -0800

    CodeGen: Emit .prefalign directives based on the prefalign attribute.
    The result of the MachineFunction preferred alignment query is emitted
    as a .prefalign directive if supported, otherwise it gets combined into
    the minimum alignment.
    
    Part of this RFC:
    https://discourse.llvm.org/t/rfc-enhancing-function-alignment-attributes/88019
    
    Reviewers: nikic, vitalybuka
    
    Reviewed By: vitalybuka
    
    Pull Request: https://github.com/llvm/llvm-project/pull/155529

--- llvm/lib/CodeGen/AsmPrinter/AsmPrinter.cpp
+++ llvm/lib/CodeGen/AsmPrinter/AsmPrinter.cpp
@@ -999,8 +999,20 @@ void AsmPrinter::emitFunctionHeader() {
     emitVisibility(CurrentFnSym, F.getVisibility());
 
   emitLinkage(&F, CurrentFnSym);
-  if (MAI->hasFunctionAlignment())
-    emitAlignment(MF->getPreferredAlignment(), &F);
+  if (MAI->hasFunctionAlignment()) {
+    // The preferred alignment directive will not have the intended effect
+    // unless function sections are enabled.
+    if (MAI->useIntegratedAssembler() && MAI->hasPreferredAlignment() &&
+        TM.getFunctionSections()) {
+      Align Alignment = MF->getAlignment();
+      Align PrefAlignment = MF->getPreferredAlignment();
+      emitAlignment(Alignment, &F);
+      if (Alignment != PrefAlignment)
+        OutStreamer->emitPrefAlign(PrefAlignment);
+    } else {
+      emitAlignment(MF->getPreferredAlignment(), &F);
+    }
+  }
 
   if (MAI->hasDotTypeDotSizeDirective())
     OutStreamer->emitSymbolAttribute(CurrentFnSym, MCSA_ELF_TypeFunction);
