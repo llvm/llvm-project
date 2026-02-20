4d7a81e0f0d2cdc7ac7188ce3ed4482154c37b89
Sy Brand <sy.brand@fastly.com>
2026-02-20 11:11:32 +0000

    Enable component model context in WASIP3 in LLVM

--- llvm/lib/Target/WebAssembly/WebAssemblySubtarget.cpp
+++ llvm/lib/Target/WebAssembly/WebAssemblySubtarget.cpp
@@ -40,6 +40,15 @@ WebAssemblySubtarget::initializeSubtargetDependencies(StringRef CPU,
 
   ParseSubtargetFeatures(CPU, /*TuneCPU*/ CPU, FS);  
   
+  // WASIP3 implies using the component model thread context intrinsics by
+  // default, unless explicitly disabled.
+  if (!FS.contains("component-model-thread-context") && 
+      !HasComponentModelThreadContext && 
+      TargetTriple.getOS() == Triple::WASIp3) {
+    ToggleFeature(WebAssembly::FeatureComponentModelThreadContext);
+    HasComponentModelThreadContext = true;
+  }
+
   FeatureBitset Bits = getFeatureBits();
 
   // bulk-memory implies bulk-memory-opt
