9e89d8dbab46aab97e43a4765923f84a383c5b68
Jason Molenda <jmolenda@apple.com>
2026-02-20 21:50:23 -0800

    [lldb] A few small code modernizations and cleanups [NFC]
    I was reading through ObjectContainerBSDArchive and came
    across some dead method decls, a less-than-completely-clear
    `shared_ptr` typedef in `ObjectContainerBSDArchive::Archive`
    for a shared_ptr<Archive> which was a little unclear when
    reading a decl like `shared_ptr archive_sp;` for a local
    variable.

--- lldb/source/Plugins/ObjectContainer/Universal-Mach-O/ObjectContainerUniversalMachO.cpp
+++ lldb/source/Plugins/ObjectContainer/Universal-Mach-O/ObjectContainerUniversalMachO.cpp
@@ -74,9 +74,10 @@ ObjectContainerUniversalMachO::ObjectContainerUniversalMachO(
 ObjectContainerUniversalMachO::~ObjectContainerUniversalMachO() = default;
 
 bool ObjectContainerUniversalMachO::ParseHeader() {
-  bool success = ParseHeader(*m_extractor_sp.get(), m_header, m_fat_archs);
+  bool success = ParseHeader(*m_extractor_sp, m_header, m_fat_archs);
   // We no longer need any data, we parsed all we needed to parse and cached it
-  // in m_header and m_fat_archs
+  // in m_header and m_fat_archs.  Need to have an empty DataExtractor for code
+  // that assumes it is non-null.
   m_extractor_sp = std::make_shared<DataExtractor>();
   return success;
 }
