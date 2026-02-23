5df427ea0a54f09bdc2f1c589b615976640af65e
Oleksandr Tarasiuk <oleksandr.tarasiuk@outlook.com>
2026-02-23 16:49:31 +0200

    specify -triple

--- clang/test/CodeGen/c23-constexpr-member-access.c
+++ clang/test/CodeGen/c23-constexpr-member-access.c
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -std=c23 -emit-llvm -o - %s | FileCheck %s
+// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -std=c23 -emit-llvm -o - %s | FileCheck %s
 struct S {
   int s;
   int e;
