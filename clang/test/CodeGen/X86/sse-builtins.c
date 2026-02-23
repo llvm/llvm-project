072805bf846d64fdf68aab68b15e744736c20841
NagaChaitanya Vellanki <pnagato@protonmail.com>
2026-02-23 06:03:57 -0800

    [X86][Clang] Add constexpr support for _mm_min_ss/_mm_max_ss/_mm_min_sd/_mm_max_sd/_mm_min_sh/_mm_max_sh intrinsics (#178029)
    - Added boolean IsScalar argument to the helper functions in
    InterpBuiltin/ExprConstant
    - Made minsh_round_mask, maxsh_round_mask constexpr only for
    _MM_FROUND_CUR_DIRECTION rounding mode.
    - Added helper function for scalar round mask in
    InterpBuiltin/ExprConstant
    
    Resolves #175198

--- clang/test/CodeGen/X86/sse-builtins.c
+++ clang/test/CodeGen/X86/sse-builtins.c
@@ -524,6 +524,8 @@ __m128 test_mm_max_ss(__m128 A, __m128 B) {
   // CHECK: @llvm.x86.sse.max.ss(<4 x float> %{{.*}}, <4 x float> %{{.*}})
   return _mm_max_ss(A, B);
 }
+TEST_CONSTEXPR(match_m128(_mm_max_ss((__m128){+1.0f, +2.0f, +3.0f, +4.0f}, (__m128){+4.0f, +3.0f, +2.0f, +1.0f}), +4.0f, +2.0f, +3.0f, +4.0f));
+TEST_CONSTEXPR(match_m128(_mm_max_ss((__m128){+0.0f, -0.0f, +0.0f, -0.0f}, (__m128){-0.0f, +0.0f, -0.0f, +0.0f}), -0.0f, -0.0f, +0.0f, -0.0f));
 
 __m128 test_mm_min_ps(__m128 A, __m128 B) {
   // CHECK-LABEL: test_mm_min_ps
@@ -539,6 +541,8 @@ __m128 test_mm_min_ss(__m128 A, __m128 B) {
   // CHECK: @llvm.x86.sse.min.ss(<4 x float> %{{.*}}, <4 x float> %{{.*}})
   return _mm_min_ss(A, B);
 }
+TEST_CONSTEXPR(match_m128(_mm_min_ss((__m128){+1.0f, +2.0f, +3.0f, +4.0f}, (__m128){+4.0f, +3.0f, +2.0f, +1.0f}), +1.0f, +2.0f, +3.0f, +4.0f));
+TEST_CONSTEXPR(match_m128(_mm_min_ss((__m128){+0.0f, -0.0f, +0.0f, -0.0f}, (__m128){-0.0f, +0.0f, -0.0f, +0.0f}), -0.0f, -0.0f, +0.0f, -0.0f));
 
 __m128 test_mm_move_ss(__m128 A, __m128 B) {
   // CHECK-LABEL: test_mm_move_ss
