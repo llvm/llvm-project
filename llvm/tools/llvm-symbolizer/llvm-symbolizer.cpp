38593298c1c8f865d54823f53ccb920769a7fa3e
Midhunesh <midhuensh.p@ibm.com>
2026-02-23 05:24:20 -0500

    Address review comments

--- llvm/tools/llvm-symbolizer/llvm-symbolizer.cpp
+++ llvm/tools/llvm-symbolizer/llvm-symbolizer.cpp
@@ -1,5 +1,4 @@
-//===-- llvm-symbolizer.cpp - Simple addr2line-like symbolizer
-//------------===//
+//===-- llvm-symbolizer.cpp - Simple addr2line-like symbolizer ------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
