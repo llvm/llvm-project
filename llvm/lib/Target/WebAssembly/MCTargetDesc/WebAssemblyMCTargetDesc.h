8047c7634ef1cfddd78e5bc15c0ad7972cf08ef8
Sy Brand <sy.brand@fastly.com>
2026-02-23 16:44:09 +0000

    Update comment

--- llvm/lib/Target/WebAssembly/MCTargetDesc/WebAssemblyMCTargetDesc.h
+++ llvm/lib/Target/WebAssembly/MCTargetDesc/WebAssemblyMCTargetDesc.h
@@ -106,7 +106,8 @@ enum TOF {
   MO_MEMORY_BASE_REL,
 
   // On a symbol operand this indicates that the immediate is the symbol
-  // address relative the __tls_base wasm global.
+  // address relative to the TLS base. This is stored in thread.context[1] 
+  // when using the component model thread context, and the __tls_base global otherwise.
   // Only applicable to data symbols.
   MO_TLS_BASE_REL,
 
