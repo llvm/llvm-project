afad513ead7378dc5f12ea7909097f93a37750f7
Louis Dionne <ldionne.2@gmail.com>
2026-02-23 12:13:18 -0500

    Handle MSVC

--- libcxx/src/math.cpp
+++ libcxx/src/math.cpp
@@ -16,9 +16,13 @@
 _LIBCPP_BEGIN_NAMESPACE_STD
 
 __lgamma_result __lgamma_thread_safe_impl(double __d) noexcept {
+#if defined(_LIBCPP_MSVCRT_LIKE) // reentrant version is not available on Windows
+  return ::lgamma(__d);
+#else
   int __sign;
   double __result = ::lgamma_r(__d, &__sign);
   return __lgamma_result{__result, __sign};
+#endif
 }
 
 _LIBCPP_END_NAMESPACE_STD
