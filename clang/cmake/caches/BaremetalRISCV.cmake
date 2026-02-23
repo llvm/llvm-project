6fdf13d2a1d4ff5d83f1c36a25576bd9fcedded6
Ramkumar Ramachandra <artagnon@tenstorrent.com>
2026-02-23 09:28:02 +0000

    [cmake/BaremetalRISCV] Use MinSizeRel, strip OS_DIR

--- clang/cmake/caches/BaremetalRISCV.cmake
+++ clang/cmake/caches/BaremetalRISCV.cmake
@@ -15,17 +15,15 @@ set(LLVM_RUNTIME_TARGETS ${TRIPLES} CACHE STRING "")
 
 foreach(target ${TRIPLES})
   # builtins config
-  set(BUILTINS_${target}_CMAKE_BUILD_TYPE Release CACHE STRING "")
+  set(BUILTINS_${target}_CMAKE_BUILD_TYPE MinSizeRel CACHE STRING "")
   set(BUILTINS_${target}_CMAKE_SYSTEM_NAME Generic CACHE STRING "")
   set(BUILTINS_${target}_COMPILER_RT_BAREMETAL_BUILD ON CACHE BOOL "")
-  set(BUILTINS_${target}_COMPILER_RT_OS_DIR ${target} CACHE STRING "")
 
   # enable compiler-rt in runtimes
   set(RUNTIMES_${target}_ENABLE_RUNTIMES compiler-rt CACHE STRING "")
 
   # runtimes config
-  set(RUNTIMES_${target}_CMAKE_BUILD_TYPE Release CACHE STRING "")
+  set(RUNTIMES_${target}_CMAKE_BUILD_TYPE MinSizeRel CACHE STRING "")
   set(RUNTIMES_${target}_CMAKE_SYSTEM_NAME Generic CACHE STRING "")
   set(RUNTIMES_${target}_COMPILER_RT_BAREMETAL_BUILD ON CACHE BOOL "")
-  set(RUNTIMES_${target}_COMPILER_RT_OS_DIR ${target} CACHE STRING "")
 endforeach()
