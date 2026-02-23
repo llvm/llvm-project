4ed9efd928530098c5867ad34d089bac371e4502
wengliqin <liqin.weng@spacemit.com>
2026-02-23 16:59:13 +0800

    1. address the comments 2. rebase the upstream

--- llvm/lib/Target/RISCV/AsmParser/RISCVAsmParser.cpp
+++ llvm/lib/Target/RISCV/AsmParser/RISCVAsmParser.cpp
@@ -356,7 +356,6 @@ struct RISCVOperand final : public MCParsedAsmOperand {
 
   struct RegOp {
     MCRegister Reg;
-    std::optional<std::pair<MCRegister, unsigned>> RegPairNum;
     bool IsGPRAsFPR;
   };
 
@@ -1097,13 +1096,10 @@ public:
   }
 
   static std::unique_ptr<RISCVOperand>
-  createReg(MCRegister Reg, SMLoc S, SMLoc E, bool IsGPRAsFPR = false,
-            std::optional<std::pair<MCRegister, unsigned>> RegPairNum =
-                std::nullopt) {
+  createReg(MCRegister Reg, SMLoc S, SMLoc E, bool IsGPRAsFPR = false) {
     auto Op = std::make_unique<RISCVOperand>(KindTy::Register);
     Op->Reg.Reg = Reg;
     Op->Reg.IsGPRAsFPR = IsGPRAsFPR;
-    Op->Reg.RegPairNum = RegPairNum;
     Op->StartLoc = S;
     Op->EndLoc = E;
     return Op;
@@ -1329,15 +1325,6 @@ static MCRegister convertFPR64ToFPR256(MCRegister Reg) {
   return Reg - RISCV::F0_D + RISCV::F0_Q2;
 }
 
-static int getLMUL(unsigned Kind) {
-  if (Kind == MCK_VRM2)
-    return 2;
-  else if (Kind == MCK_VRM4)
-    return 4;
-  else
-    return 8;
-}
-
 unsigned RISCVAsmParser::validateTargetOperandClass(MCParsedAsmOperand &AsmOp,
                                                     unsigned Kind) {
   RISCVOperand &Op = static_cast<RISCVOperand &>(AsmOp);
@@ -1393,14 +1380,7 @@ unsigned RISCVAsmParser::validateTargetOperandClass(MCParsedAsmOperand &AsmOp,
   // As the parser couldn't differentiate an VRM2/VRM4/VRM8 from an VR, coerce
   // the register from VR to VRM2/VRM4/VRM8 if necessary.
   if (IsRegVR && (Kind == MCK_VRM2 || Kind == MCK_VRM4 || Kind == MCK_VRM8)) {
-    // The dest reg requires multiple registers to be combined to complete the
-    // instruction operation. In this case, it is necessary to retain the
-    // register num ​​of the dest register before the conversion to ensure
-    // that 'the destination vector register group cannot overlap the source
-    // vector register group'
     Op.Reg.Reg = convertVRToVRMx(*getContext().getRegisterInfo(), Reg, Kind);
-    auto DstGroup = std::make_pair(Reg, getLMUL(Kind));
-    Op.Reg.RegPairNum.emplace(DstGroup);
     if (!Op.Reg.Reg)
       return Match_InvalidOperand;
     return Match_Success;
@@ -3809,13 +3789,14 @@ std::unique_ptr<RISCVOperand> RISCVAsmParser::defaultFRMArgLegacyOp() const {
                                     llvm::SMLoc());
 }
 
-static MCRegister getBeforeConvertRegNum(MCParsedAsmOperand &Operand,
-                                         MCRegister defaultReg) {
-  RISCVOperand &Op = static_cast<RISCVOperand &>(Operand);
-  if (Op.Reg.RegPairNum.has_value())
-    return Op.Reg.RegPairNum->first;
-
-  return defaultReg;
+unsigned getLMULFromVectorRegister(MCRegister Reg) {
+  if (RISCVMCRegisterClasses[RISCV::VRM2RegClassID].contains(Reg))
+    return 2;
+  if (RISCVMCRegisterClasses[RISCV::VRM4RegClassID].contains(Reg))
+    return 4;
+  if (RISCVMCRegisterClasses[RISCV::VRM8RegClassID].contains(Reg))
+    return 8;
+  return 1;
 }
 
 bool RISCVAsmParser::validateInstruction(MCInst &Inst,
@@ -3860,13 +3841,6 @@ bool RISCVAsmParser::validateInstruction(MCInst &Inst,
 
   int DestIdx = RISCV::getNamedOperandIdx(Inst.getOpcode(), RISCV::OpName::vd);
   MCRegister DestReg = Inst.getOperand(DestIdx).getReg();
-  MCParsedAsmOperand &VDIdx = *Operands[1];
-  RISCVOperand &Dst = static_cast<RISCVOperand &>(VDIdx);
-  unsigned Lmul = 1;
-  if (Dst.Reg.RegPairNum.has_value()) {
-    DestReg = Dst.Reg.RegPairNum->first;
-    Lmul = Dst.Reg.RegPairNum->second;
-  }
 
   // Operands[1] or Operands[2] will be the first operand, DestReg.
   const MCParsedAsmOperand *ParsedOp = Operands[1].get();
@@ -3878,16 +3852,19 @@ bool RISCVAsmParser::validateInstruction(MCInst &Inst,
   assert(ParsedOp->getReg() == DestReg && "Can't find parsed dest operand");
   SMLoc Loc = ParsedOp->getStartLoc();
 
+  unsigned Lmul = getLMULFromVectorRegister(DestReg);
+  unsigned DestEncoding =
+      getContext().getRegisterInfo()->getEncodingValue(DestReg);
   if (MCID.TSFlags & RISCVII::VS2Constraint) {
     int VS2Idx =
         RISCV::getNamedOperandIdx(Inst.getOpcode(), RISCV::OpName::vs2);
     assert(VS2Idx >= 0 && "No vs2 operand?");
-    MCRegister CheckReg = getBeforeConvertRegNum(
-        *Operands[2], Inst.getOperand(VS2Idx).getReg());
+    unsigned CheckEncoding = getContext().getRegisterInfo()->getEncodingValue(
+        Inst.getOperand(VS2Idx).getReg());
     for (unsigned i = 0; i < Lmul; i++) {
-      if ((DestReg + i) == CheckReg)
+      if ((DestEncoding + i) == CheckEncoding)
         return Error(Loc, "the destination vector register group cannot overlap"
-                          " the VS2 source vector register group");
+                          " the source vector register group");
     }
   }
   if (MCID.TSFlags & RISCVII::VS1Constraint) {
@@ -3896,11 +3873,13 @@ bool RISCVAsmParser::validateInstruction(MCInst &Inst,
     // FIXME: The vs1 constraint is used on scalar and imm instructions so we
     // need to check that the operand exists.
     if (VS1Idx >= 0) {
-      MCRegister CheckReg = Inst.getOperand(VS1Idx).getReg();
+      unsigned CheckEncoding = getContext().getRegisterInfo()->getEncodingValue(
+          Inst.getOperand(VS1Idx).getReg());
       for (unsigned i = 0; i < Lmul; i++) {
-        if ((DestReg + i) == CheckReg)
-          return Error(Loc, "the destination vector register group cannot overlap"
-                            " the VS1 source vector register group");
+        if ((DestEncoding + i) == CheckEncoding)
+          return Error(Loc,
+                       "the destination vector register group cannot overlap"
+                       " the source vector register group");
       }
     }
   }
--- llvm/test/MC/RISCV/xsmtvdot-invalid.s
+++ llvm/test/MC/RISCV/xsmtvdot-invalid.s
@@ -3,19 +3,19 @@
 
 # NoSlide
 smt.vmadot   v1, v2, v4
-// CHECK: :[[@LINE-1]]:14: error: invalid operand for instruction
+// CHECK: :[[@LINE-1]]:14: error: invalid operand for instruction{{$}}
 
 smt.vmadot   v2, v2, v4
-// CHECK: :[[@LINE-1]]:14: error: the destination vector register group cannot overlap the source vector register group
+// CHECK: :[[@LINE-1]]:14: error: the destination vector register group cannot overlap the source vector register group{{$}}
 
 smt.vmadot   v4, v2, v4
-// CHECK: :[[@LINE-1]]:14: error: the destination vector register group cannot overlap the source vector register group
+// CHECK: :[[@LINE-1]]:14: error: the destination vector register group cannot overlap the source vector register group{{$}}
 
 smt.vmadot   v2, v3, v4
-// CHECK: :[[@LINE-1]]:14: error: the destination vector register group cannot overlap the source vector register group
+// CHECK: :[[@LINE-1]]:14: error: the destination vector register group cannot overlap the source vector register group{{$}}
 
 smt.vmadot   v2, v4, v3
-// CHECK: :[[@LINE-1]]:14: error: the destination vector register group cannot overlap the source vector register group
+// CHECK: :[[@LINE-1]]:14: error: the destination vector register group cannot overlap the source vector register group{{$}}
 
 # slide
 smt.vmadot1   v1, v2, v2
@@ -25,13 +25,13 @@ smt.vmadot1   v2, v5, v4
 // CHECK: :[[@LINE-1]]:19: error: invalid operand for instruction
 
 smt.vmadot1   v2, v2, v4
-// CHECK: :[[@LINE-1]]:15: error: the destination vector register group cannot overlap the source vector register group
+// CHECK: :[[@LINE-1]]:15: error: the destination vector register group cannot overlap the source vector register group{{$}}
 
 smt.vmadot1   v2, v4, v2
-// CHECK: :[[@LINE-1]]:15: error: the destination vector register group cannot overlap the source vector register group
+// CHECK: :[[@LINE-1]]:15: error: the destination vector register group cannot overlap the source vector register group{{$}}
 
 smt.vmadot1   v0, v1, v2
 // CHECK: :[[@LINE-1]]:19: error: invalid operand for instruction
 
 smt.vmadot1   v0, v2, v1
-// CHECK: :[[@LINE-1]]:15: error: the destination vector register group cannot overlap the source vector register group
+// CHECK: :[[@LINE-1]]:15: error: the destination vector register group cannot overlap the source vector register group{{$}}
