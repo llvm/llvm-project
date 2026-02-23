5650909f1c7db697058cbaf2bc92f809bad760c0
Deric Cheung <cheung.deric@gmail.com>
2026-02-23 12:02:57 -0800

    Revert "Remove unnecessary intermediate `RowMajorIdx` variable"
    This reverts commit ea2c2f5d35436318d14fc345ab499f535ca50a00.
    
    It turns out the intermediate variable is actually necessary.

--- clang/lib/Sema/SemaInit.cpp
+++ clang/lib/Sema/SemaInit.cpp
@@ -1917,8 +1917,9 @@ void InitListChecker::CheckMatrixType(const InitializedEntity &Entity,
     // Not a sublist: just consume directly.
     // Note: In HLSL, elements of the InitListExpr are in row-major order, so no
     // change is needed to the Index.
-    ElemEnt.setElementIndex(Index);
-    CheckSubElementType(ElemEnt, IList, ElemTy, Index, StructuredList,
+    unsigned RowMajorIdx = Index;
+    ElemEnt.setElementIndex(RowMajorIdx);
+    CheckSubElementType(ElemEnt, IList, ElemTy, RowMajorIdx, StructuredList,
                         StructuredIndex);
     ++Index;
   }
