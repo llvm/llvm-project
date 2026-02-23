344812801f2e3f941a140ff9c8eabebf7bb7843d
Paul Kirth <paulkirth@google.com>
2026-02-23 13:47:36 -0800

    Add test case

---
+++ compiler-rt/test/asan/TestCases/Posix/multiple_sigaltstack.cpp
@@ -0,0 +1,24 @@
+// RUN: %clangxx_asan %s -o %t && %env_asan_opts=use_sigaltstack=1 %run %t
+
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+char global_alt_stack[4096 * 4];
+
+int main() {
+  stack_t altstack;
+  altstack.ss_sp = global_alt_stack;
+  altstack.ss_size = sizeof(global_alt_stack);
+  altstack.ss_flags = 0;
+  if (sigaltstack(&altstack, nullptr) != 0) {
+    perror("sigaltstack");
+    exit(1);
+  }
+
+  // UnsetAlternateSignalStack will get called when the thread exists. If we
+  // don't *only* unmap a signal stack the runtime owns, we'll get a fault on
+  // the munmap operation, since that memory isn't mmaped.
+  return 0;
+}
