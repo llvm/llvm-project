099f4975c2a7c256424f2e54fb019403d4db5e7c
Sukumarsawant <sawantsukumar@gmail.com>
2026-02-24 23:07:47 +0530

    formatted

--- libc/test/src/__support/FPUtil/bfloat16_test.cpp
+++ libc/test/src/__support/FPUtil/bfloat16_test.cpp
@@ -70,11 +70,10 @@ TEST_F(LlvmLibcBfloat16ConversionTest, FromInteger) {
 
 TEST_F(LlvmLibcBfloat16ConversionTest, MultiplyAssign) {
 
-  constexpr BFloat16 VAL[] = {
-      zero,           neg_zero,        inf,
-      neg_inf,        min_normal,      max_normal,
-      bfloat16(1.0f), bfloat16(-1.0f), bfloat16(2.0f),
-      bfloat16(3.0f)};
+  constexpr BFloat16 VAL[] = {zero,           neg_zero,        inf,
+                              neg_inf,        min_normal,      max_normal,
+                              bfloat16(1.0f), bfloat16(-1.0f), bfloat16(2.0f),
+                              bfloat16(3.0f)};
   for (const bfloat16 &x : VAL) {
     for (const bfloat16 &y : VAL) {
       BFloat16 a = x, b = y;
