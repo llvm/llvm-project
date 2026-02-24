099922813797a9c04a0fdf248c76a17320f4b702
Aviral Goel <agoel26@apple.com>
2026-02-24 12:13:07 -0800

    Remove superfluous include

--- clang/unittests/Analysis/Scalable/Serialization/JSONFormatTest/JSONFormatTest.h
+++ clang/unittests/Analysis/Scalable/Serialization/JSONFormatTest/JSONFormatTest.h
@@ -21,7 +21,6 @@
 #include "llvm/Support/MemoryBuffer.h"
 #include "llvm/Support/Path.h"
 #include "llvm/Support/raw_ostream.h"
-#include "gtest/gtest.h"
 #ifndef _WIN32
 #include <unistd.h>
 #endif
