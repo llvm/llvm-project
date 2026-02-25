3272bd46192c7938ed434c200daaa9d890ecc168
mencotton <mencotton0410@gmail.com>
2026-02-25 20:42:46 +0900

    add an error match

--- clang/test/CIR/CodeGenHLSL/matrix-element-expr-load.hlsl
+++ clang/test/CIR/CodeGenHLSL/matrix-element-expr-load.hlsl
@@ -2,6 +2,7 @@
 // RUN:   -fclangir -emit-cir -disable-llvm-passes -verify
 
 // expected-error@*:* {{ClangIR code gen Not Yet Implemented: processing of type: ConstantMatrix}}
+// expected-error@*:* {{ClangIR code gen Not Yet Implemented: Matrix type conversion}}
 float test_zero_indexed(float2x2 M) {
   // expected-error@+1 {{ClangIR code gen Not Yet Implemented: ScalarExprEmitter: matrix element}}
   return M._m00;
