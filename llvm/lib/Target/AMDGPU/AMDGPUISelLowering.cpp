eba5c1a9ea21609875f75499fda49693eabc96c9
addmisol <218448340+addmisol@users.noreply.github.com>
2026-02-25 12:06:07 +0530

    Update llvm/lib/Target/AMDGPU/AMDGPUISelLowering.cpp
    Co-authored-by: Matt Arsenault <arsenm2@gmail.com>

--- llvm/lib/Target/AMDGPU/AMDGPUISelLowering.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPUISelLowering.cpp
@@ -5981,11 +5981,7 @@ unsigned AMDGPUTargetLowering::ComputeNumSignBitsForTargetNode(
     if (!Width)
       return 1;
 
-    unsigned WidthVal = Width->getZExtValue() & 0x1f;
-    if (WidthVal == 0)
-      return 32;
-
-    unsigned SignBits = 32 - WidthVal + 1;
+    unsigned SignBits = 32 - (Width->getZExtValue() & 0x1f) + 1;
     if (!isNullConstant(Op.getOperand(1)))
       return SignBits;
 
