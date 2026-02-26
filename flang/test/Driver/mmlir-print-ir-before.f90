10c4c92be746ce0683036a198fa106b31eff4298
Ted Johnson <tedmjohnson@protonmail.com>
2026-02-26 08:31:47 -0600

    [flang] Expand test to cover both -before and -after

--- flang/test/Driver/mmlir-print-ir-before.f90
+++
@@ -1,8 +0,0 @@
-! Verify that --mlir-print-ir-before=<pass> works for registered passes.
-
-! RUN: %flang_fc1 -emit-llvm -mmlir --mlir-print-ir-before=cse -o /dev/null %s 2>&1 | FileCheck %s
-
-! CHECK: IR Dump Before CSE
-! CHECK: func.func @_QQmain
-
-end program
