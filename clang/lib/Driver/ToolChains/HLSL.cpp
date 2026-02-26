ac9ce0aa0519eb7220632078241b4b8092add89a
Chris B <chris.bieneman@me.com>
2026-02-26 13:47:33 -0600

    [Metal][HLSL] Add support for dumping reflection (#181258)
    The Metal Shader converter can output shader reflection information into
    a JSON file. This connects the -Fre flag (DXC's flag for reflection) to
    the Metal Shader Converter tool step to produce the JSON file. As a
    temporary state the -Fre flag will error when used without the -metal
    flag.
    
    This is required to address
    https://github.com/llvm/offload-test-suite/issues/452

--- clang/lib/Driver/ToolChains/HLSL.cpp
+++ clang/lib/Driver/ToolChains/HLSL.cpp
@@ -327,6 +327,13 @@ void tools::hlsl::MetalConverter::ConstructJob(
   CmdArgs.push_back("-o");
   CmdArgs.push_back(Output.getFilename());
 
+  StringRef Reflection = Args.getLastArgValue(options::OPT_dxc_Fre);
+  if (!Reflection.empty()) {
+    const char *ReflectionStr =
+        Args.MakeArgString(StringRef("--output-reflection-file=") + Reflection);
+    CmdArgs.push_back(ReflectionStr);
+  }
+
   const char *Exec = Args.MakeArgString(MSCPath);
   C.addCommand(std::make_unique<Command>(JA, *this, ResponseFileSupport::None(),
                                          Exec, CmdArgs, Inputs, Input));
@@ -528,6 +535,21 @@ HLSLToolChain::TranslateArgs(const DerivedArgList &Args, StringRef BoundArch,
                         Opts.getOption(options::OPT_fmatrix_memory_layout_EQ),
                         "row-major");
       A->claim();
+      continue;
+    }
+
+    // This is a temporary check until we support reflection generation for
+    // other targets.
+    if (A->getOption().getID() == options::OPT_dxc_Fre) {
+      if (Args.hasArg(options::OPT_metal)) {
+        if (!Args.hasArg(options::OPT_dxc_Fo))
+          getDriver().Diag(diag::err_drv_dxc_Fre_requires_Fo_metal);
+      } else
+        getDriver().Diag(diag::err_drv_unsupported_opt_for_target)
+            << "-Fre" << getTriple().getArchName();
+      A->claim();
+      DAL->AddSeparateArg(nullptr, Opts.getOption(options::OPT_dxc_Fre),
+                          A->getValue());
       continue;
     }
 
