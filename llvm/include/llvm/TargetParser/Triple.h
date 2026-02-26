fc93a99e9161d93153994d0fd49b5e9b492c45fb
Ankit Aggarwal <aankit@qti.qualcomm.com>
2026-02-26 01:46:16 -0600

    [Clang][Hexagon] Add QURT as recognized OS in target triple
    Add support for the QURT as a recognized OS type in the LLVM triple system,
    and define the __qurt__ predefined macro when targeting it.
    
    Change-Id: I2894ff26c84e0eebf7d3e3c5db7af56fb02ce0c9

--- llvm/include/llvm/TargetParser/Triple.h
+++ llvm/include/llvm/TargetParser/Triple.h
@@ -255,7 +255,8 @@ public:
     OpenCL,
     ChipStar,
     Firmware,
-    LastOSType = Firmware
+    QURT,
+    LastOSType = QURT
   };
   enum EnvironmentType {
     UnknownEnvironment,
@@ -793,6 +794,11 @@ public:
 
   bool isOSSerenity() const {
     return getOS() == Triple::Serenity;
+  }
+
+  /// Tests whether the OS is QURT.
+  bool isOSQurt() const {
+    return getOS() == Triple::QURT;
   }
 
   /// Tests whether the OS uses the ELF binary format.
