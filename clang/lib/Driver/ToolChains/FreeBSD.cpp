b04179a86c7cdf32281ebe9321821931bdc145d0
ShengYi Hung <aokblast@FreeBSD.org>
2026-02-26 21:08:35 +0800

    fixup! [Toolchains][FreeBSD] Honor system libgcc

--- clang/lib/Driver/ToolChains/FreeBSD.cpp
+++ clang/lib/Driver/ToolChains/FreeBSD.cpp
@@ -520,6 +520,12 @@ std::string FreeBSD::getCompilerRT(const llvm::opt::ArgList &Args,
         ToolChain::getCompilerRT(Args, Component, Type, IsFortran);
     if (getVFS().exists(DetectedPath))
       return DetectedPath;
+    for (const auto &FilePath : getFilePaths()) {
+      SmallString<128> Path(FilePath);
+      llvm::sys::path::append(Path, "libgcc.a");
+      if (getVFS().exists(Path))
+        return std::string(Path);
+    }
     SmallString<128> Path(getDriver().SysRoot);
     llvm::sys::path::append(Path, "/usr/lib/libgcc.a");
     return std::string(Path);
