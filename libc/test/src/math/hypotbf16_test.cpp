ddc7ea45a80d9bb4f7c289bf1798d3992f99bcfe
Sukumarsawant <sawantsukumar@gmail.com>
2026-02-26 11:46:36 +0530

    added true exhaustive test

--- libc/test/src/math/hypotbf16_test.cpp
+++ libc/test/src/math/hypotbf16_test.cpp
@@ -1,4 +1,4 @@
-//===-- Exhaustive tests for hypotbf16 ------------------------------------===//
+//===-- Unittests tests for hypotbf16 ------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
