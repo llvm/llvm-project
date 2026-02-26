1dceb70aec8b89a38876046fdbf1c3315af7eb66
ShengYi Hung <aokblast@FreeBSD.org>
2026-02-26 22:36:50 +0800

    fixup! Merge branch 'users/aokblast/elf/fix_error' into users/aokblast/readelf/pxnum_support

--- llvm/include/llvm/Object/ELF.h
+++ llvm/include/llvm/Object/ELF.h
@@ -1,4 +1,4 @@
-]//===- ELF.h - ELF object file implementation -------------------*- C++ -*-===//
+//===- ELF.h - ELF object file implementation -------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
