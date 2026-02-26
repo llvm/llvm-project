a2c522dbb04a6359e2dcfdc83171f766da131a21
Iasonaskrpr <iaskarapro@gmail.com>
2026-02-26 23:34:52 +0200

    Added OrZero argument and removed SRL handling in visitREM

--- llvm/lib/CodeGen/SelectionDAG/SelectionDAG.cpp
+++ llvm/lib/CodeGen/SelectionDAG/SelectionDAG.cpp
@@ -4725,7 +4725,7 @@ bool SelectionDAG::isKnownToBeAPowerOfTwo(SDValue Val,
     auto *C = isConstOrConstSplat(Val.getOperand(0), DemandedElts);
     if (C && C->getAPIntValue().isSignMask())
       return true;
-    return (OrZero || isKnownNeverZero(Val, Depth)) &&
+    return (OrZero || isKnownNeverZero(Val, DemandedElts, Depth)) &&
            isKnownToBeAPowerOfTwo(Val.getOperand(0), DemandedElts, OrZero,
                                   Depth + 1);
   }
