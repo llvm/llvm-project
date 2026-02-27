8b83358d7f8ed91d3ed46ef519a6f37bccdfb73c
Dmitrii Makarenko <dmitrii.makarenko@intel.com>
2026-02-27 18:31:01 +0000

    format

--- llvm/unittests/ADT/ArrayRefTest.cpp
+++ llvm/unittests/ADT/ArrayRefTest.cpp
@@ -468,7 +468,6 @@ TEST(ArrayRefTest, MutableArrayRefDeductionGuides) {
   }
 }
 
-
 static_assert(
     !std::is_constructible_v<MutableArrayRef<int>, const SmallVector<int>>,
     "cannot construct MutableArrayRef from const std::SmallVector<int>");
@@ -490,7 +489,7 @@ static_assert(
     std::is_constructible_v<std::span<const int>, MutableArrayRef<int>>,
     "should be able to construct const std::span from MutableArrayRef");
 static_assert(
-    std::is_constructible_v<MutableArrayRef<int>, std::span<int>&>,
+    std::is_constructible_v<MutableArrayRef<int>, std::span<int> &>,
     "should be able to construct MutableArrayRef from mutable std::span");
 static_assert(
     std::is_constructible_v<MutableArrayRef<int>, const std::span<int>>,
