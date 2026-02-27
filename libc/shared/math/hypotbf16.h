022bffdb843583fbd29215173c2bd2e0331792c4
Sukumarsawant <sawantsukumar@gmail.com>
2026-02-28 01:36:26 +0530

    REFACTOR: refactored hypotbf16 to header

--- libc/shared/math/hypotbf16.h
+++ libc/shared/math/hypotbf16.h
@@ -1,4 +1,4 @@
-//===-- Implementation of hypotbf16 function ------------------------------===//
+//===-- Shared hypotbf16 function -------------------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,13 +6,21 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "src/math/hypotbf16.h"
+#ifndef LLVM_LIBC_SHARED_MATH_HYPOTBF16_H
+#define LLVM_LIBC_SHARED_MATH_HYPOTBF16_H
+
+#include "shared/libc_common.h"
+
 #include "src/__support/math/hypotbf16.h"
 
 namespace LIBC_NAMESPACE_DECL {
 
-LLVM_LIBC_FUNCTION(bfloat16, hypotbf16, (bfloat16 x, bfloat16 y)) {
-  return math::hypotbf16(x, y);
-}
+namespace shared {
+
+using math::hypotbf16;
+
+} // namespace shared
 
 } // namespace LIBC_NAMESPACE_DECL
+
+#endif // LLVM_LIBC_SHARED_MATH_HYPOTBF16_H
