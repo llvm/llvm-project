d7a7df08be2704526af50943c1e1d8fdab285750
NeKon69 <nobodqwe@gmail.com>
2026-02-27 11:53:40 +0300

    [clang][Sema] use a label in the test

--- clang/test/CodeGen/incompatible-ptr-function-to-ptr-decay.c
+++ clang/test/CodeGen/incompatible-ptr-function-to-ptr-decay.c
@@ -3,11 +3,11 @@
 // Issue 182534
 int a();
 
-void b(__attribute__((opencl_global)) int(*) );
-// expected-note@-1 {{passing argument to parameter here}}
+void b(__attribute__((opencl_global)) int(*) ); // #bdecl
 
 void c() {
   b(a);
   // expected-error@-1 {{passing 'int (*)()' to parameter of type '__global int (*)' changes address space of pointer}}
+  // expected-note@#bdecl {{passing argument to parameter here}}
 }
 
