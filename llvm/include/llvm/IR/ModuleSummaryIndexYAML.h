c06b2a4227e99a1e7136451561d2dd11042a79e7
yonghong-song <yhs@fb.com>
2026-02-27 09:09:54 -0800

    [ThinLTO] Reduce the number of renaming due to promotions (#178587)
    Currently for thin-lto, the imported static global values (functions,
    variables, etc) will be promoted/renamed from e.g., foo() to
    foo.llvm.<hash>(). Such a renaming caused difficulties in live patching
    since function name is changed ([1]).
    
    It is possible that some global value names have to be promoted to avoid
    name collision and linker failure. But in practice, majority of name
    promotions can be avoided.
    
    In [2], the suggestion is that thin-lto pre-link decides whether
    a particular global value needs name promotion or not. If yes, later on
    in thinBackend() the name will be promoted.
    
    I compiled a particular linux kernel version (latest bpf-next tree)
    and found 1216 global values with suffix .llvm.<hash>. With this patch,
    the number of promoted functions is 2, 98% reduction from the
    original kernel build.
    
    If some native objects are not participating with LTO, name promotions
    have to be done to avoid potential linker issues. So the current
    implementation cannot be on by default. But in certain cases, e.g., linux kernel
    build, people can enable lld flag --lto-whole-program-visibility to reduce the
    number of functions like foo.llvm.<hash>().
    
    For ThinLTOCodeGenerator.cpp which is used by llvm-lto tool and a
    few other rare cases, reducing the number of renaming due to promotion,
    is not implemented as lld flag '-lto-whole-program-visibility' is not supported
    in ThinLTOCodeGenerator.cpp for now. In summary, this pull request
    only supports llvm-lto2 style workflow.
    
      [1] https://lpc.events/event/19/contributions/2212
      [2] https://discourse.llvm.org/t/rfc-avoid-functions-like-foo-llvm-for-kernel-live-patch/89400

--- llvm/include/llvm/IR/ModuleSummaryIndexYAML.h
+++ llvm/include/llvm/IR/ModuleSummaryIndexYAML.h
@@ -140,7 +140,7 @@ template <> struct MappingTraits<TypeIdSummary> {
 struct GlobalValueSummaryYaml {
   // Commonly used fields
   unsigned Linkage, Visibility;
-  bool NotEligibleToImport, Live, IsLocal, CanAutoHide;
+  bool NotEligibleToImport, Live, IsLocal, CanAutoHide, NoRenameOnPromotion;
   unsigned ImportType;
   // Fields for AliasSummary
   std::optional<uint64_t> Aliasee;
@@ -191,6 +191,7 @@ template <> struct MappingTraits<GlobalValueSummaryYaml> {
     io.mapOptional("Local", summary.IsLocal);
     io.mapOptional("CanAutoHide", summary.CanAutoHide);
     io.mapOptional("ImportType", summary.ImportType);
+    io.mapOptional("NoRenameOnPromotion", summary.NoRenameOnPromotion);
     io.mapOptional("Aliasee", summary.Aliasee);
     io.mapOptional("Refs", summary.Refs);
     io.mapOptional("TypeTests", summary.TypeTests);
@@ -228,7 +229,8 @@ template <> struct CustomMappingTraits<GlobalValueSummaryMapTy> {
           static_cast<GlobalValue::VisibilityTypes>(GVSum.Visibility),
           GVSum.NotEligibleToImport, GVSum.Live, GVSum.IsLocal,
           GVSum.CanAutoHide,
-          static_cast<GlobalValueSummary::ImportKind>(GVSum.ImportType));
+          static_cast<GlobalValueSummary::ImportKind>(GVSum.ImportType),
+          GVSum.NoRenameOnPromotion);
       if (GVSum.Aliasee) {
         auto ASum = std::make_unique<AliasSummary>(GVFlags);
         V.try_emplace(*GVSum.Aliasee, /*IsAnalysis=*/false);
@@ -272,9 +274,10 @@ template <> struct CustomMappingTraits<GlobalValueSummaryMapTy> {
               static_cast<bool>(FSum->flags().Live),
               static_cast<bool>(FSum->flags().DSOLocal),
               static_cast<bool>(FSum->flags().CanAutoHide),
-              FSum->flags().ImportType, /*Aliasee=*/std::nullopt, Refs,
-              FSum->type_tests(), FSum->type_test_assume_vcalls(),
-              FSum->type_checked_load_vcalls(),
+              FSum->flags().ImportType,
+              static_cast<bool>(FSum->flags().NoRenameOnPromotion),
+              /*Aliasee=*/std::nullopt, Refs, FSum->type_tests(),
+              FSum->type_test_assume_vcalls(), FSum->type_checked_load_vcalls(),
               FSum->type_test_assume_const_vcalls(),
               FSum->type_checked_load_const_vcalls()});
         } else if (auto *ASum = dyn_cast<AliasSummary>(Sum.get());
@@ -286,6 +289,7 @@ template <> struct CustomMappingTraits<GlobalValueSummaryMapTy> {
               static_cast<bool>(ASum->flags().DSOLocal),
               static_cast<bool>(ASum->flags().CanAutoHide),
               ASum->flags().ImportType,
+              static_cast<bool>(ASum->flags().NoRenameOnPromotion),
               /*Aliasee=*/ASum->getAliaseeGUID()});
         }
       }
