73e8810482d74deae73552b4a9e93b254db41126
Nikolas Klauser <nikolasklauser@berlin.de>
2026-02-27 11:12:17 +0100

    [libc++] Fix vector::append_range growing before the capacity is reached (#183264)
    Currently `vector::append_range` grows even when appending a number of
    elements that is exactly equal to its spare capacity, which is
    guaranteed by the standard to _not_ happen.
    
    Fixes #183256
    
    (cherry picked from commit d6fcf47a893475f6f3dbcace29093297b7e9f366)

--- libcxx/include/__vector/vector.h
+++ libcxx/include/__vector/vector.h
@@ -487,7 +487,7 @@ public:
   _LIBCPP_HIDE_FROM_ABI constexpr void append_range(_Range&& __range) {
     if constexpr (ranges::forward_range<_Range> || ranges::sized_range<_Range>) {
       auto __len = ranges::distance(__range);
-      if (__len < __cap_ - __end_) {
+      if (__len <= __cap_ - __end_) {
         __construct_at_end(ranges::begin(__range), ranges::end(__range), __len);
       } else {
         __split_buffer<value_type, allocator_type> __buffer(__recommend(size() + __len), size(), __alloc_);
