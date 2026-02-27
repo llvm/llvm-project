926fae3abc314ac38e09ff905857c313ca91c9fe
Akash Dutta <Akash.Dutta@amd.com>
2026-02-27 11:23:55 -0600

    modify SmallVector to SmallBitVector;update tests

--- llvm/lib/Transforms/Vectorize/SLPVectorizer.cpp
+++ llvm/lib/Transforms/Vectorize/SLPVectorizer.cpp
@@ -13421,8 +13421,7 @@ bool BoUpSLP::matchesShlZExt(const TreeEntry &TE, OrdersType &Order,
     const unsigned VF = RhsTE->getVectorFactor();
     Order.assign(VF, VF);
     // Track which logical positions we've seen; reject duplicate shift amounts.
-    SmallVector<unsigned, 4> SeenPositions;
-    SeenPositions.assign(VF, VF);
+    SmallBitVector SeenPositions(VF);
     // Check if need to reorder Rhs to make it in form (0, Stride, 2 * Stride,
     // ..., Sz-Stride).
     if (VF * Stride != Sz)
@@ -13437,9 +13436,9 @@ bool BoUpSLP::matchesShlZExt(const TreeEntry &TE, OrdersType &Order,
       if (Val.isNegative() || Val.uge(Sz) || Val.getZExtValue() % Stride != 0)
         return false;
       unsigned Pos = Val.getZExtValue() / Stride;
-      if (SeenPositions[Pos] != VF)
+      if (SeenPositions.test(Pos))
         return false;
-      SeenPositions[Pos] = Pos;
+      SeenPositions.set(Pos);
       // TODO: Support Pos >= VF, in this case need to shift the final value.
       if (Order[Idx] != VF || Pos >= VF)
         return false;
