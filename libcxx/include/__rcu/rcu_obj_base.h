2f502049073ab625a8c8ceb98705665f9ba8bb64
Hui Xie <hui.xie1990@gmail.com>
2026-02-28 15:57:23 +0000

    refactor to hide implementation into dylib

--- libcxx/include/__rcu/rcu_obj_base.h
+++ libcxx/include/__rcu/rcu_obj_base.h
@@ -11,9 +11,10 @@
 #define _LIBCPP___RCU_RCU_OBJ_BASE_H
 
 #include <__config>
+#include <__functional/function.h>
 #include <__memory/unique_ptr.h> // for default_delete
 #include <__rcu/rcu_domain.h>
-#include <__rcu/rcu_list.h>
+#include <__utility/move.h>
 
 #if !defined(_LIBCPP_HAS_NO_PRAGMA_SYSTEM_HEADER)
 #  pragma GCC system_header
@@ -23,23 +24,22 @@ _LIBCPP_BEGIN_NAMESPACE_STD
 
 #if _LIBCPP_STD_VER >= 26 && _LIBCPP_HAS_THREADS && _LIBCPP_HAS_EXPERIMENTAL_RCU
 
-template <class T, class D = default_delete<T>>
+template <class _Tp, class _Dp = default_delete<_Tp>>
 class rcu_obj_base : private __rcu_node {
 public:
-  void retire(D d = D(), rcu_domain& dom = rcu_default_domain()) noexcept {
-    auto ptr = static_cast<T*>(this);
-    dom.__retire_callback(
-        [ptr, d = std::move(d)]() mutable { d(ptr); });
+  _LIBCPP_HIDE_FROM_ABI void retire(_Dp __deleter = _Dp(), rcu_domain& __dom = rcu_default_domain()) noexcept {
+    auto __ptr  = static_cast<_Tp*>(this);
+    __callback_ = [__ptr, __deleter = std::move(__deleter)]() mutable { __deleter(__ptr); };
+    __dom.__retire(this);
   }
 
 protected:
-  rcu_obj_base()                               = default;
-  rcu_obj_base(const rcu_obj_base&)            = default;
-  rcu_obj_base(rcu_obj_base&&)                 = default;
-  rcu_obj_base& operator=(const rcu_obj_base&) = default;
-  rcu_obj_base& operator=(rcu_obj_base&&)      = default;
-  ~rcu_obj_base()                              = default;
-
+  _LIBCPP_HIDE_FROM_ABI rcu_obj_base()                               = default;
+  _LIBCPP_HIDE_FROM_ABI rcu_obj_base(const rcu_obj_base&)            = default;
+  _LIBCPP_HIDE_FROM_ABI rcu_obj_base(rcu_obj_base&&)                 = default;
+  _LIBCPP_HIDE_FROM_ABI rcu_obj_base& operator=(const rcu_obj_base&) = default;
+  _LIBCPP_HIDE_FROM_ABI rcu_obj_base& operator=(rcu_obj_base&&)      = default;
+  _LIBCPP_HIDE_FROM_ABI ~rcu_obj_base()                              = default;
 };
 
 #endif // _LIBCPP_STD_VER >= 26 && _LIBCPP_HAS_THREADS && _LIBCPP_HAS_EXPERIMENTAL_RCU
