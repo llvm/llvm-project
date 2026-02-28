fd0ceee9c13e4b1c943b510777342ed78f6cbf8f
16srivarshitha <mvarshitha17874@gmail.com>
2026-02-28 23:17:19 +0530

    [CIR] Update expressions.cpp to use CIR prefix

--- clang/test/CIR/CodeGen/expressions.cpp
+++ clang/test/CIR/CodeGen/expressions.cpp
@@ -1,11 +1,11 @@
 // RUN: %clang_cc1 -std=c++17 -triple x86_64-unknown-linux-gnu -fclangir -emit-cir %s -o %t.cir
-// RUN: FileCheck --input-file=%t.cir %s
+// RUN: FileCheck --input-file=%t.cir %s --check-prefix=CIR
 
 void test(int a) {
-// CHECK: cir.func {{.*}} @{{.+}}test
+// CIR: cir.func {{.*}} @{{.+}}test
 
   // Should generate LValue parenthesis expression.
   (a) = 1;
-  // CHECK: %[[#C:]] = cir.const #cir.int<1> : !s32i
-  // CHECK: cir.store{{.*}} %[[#C]], %{{.+}} : !s32i, !cir.ptr<!s32i>
+  // CIR: %[[CONST:.*]] = cir.const #cir.int<1> : !s32i
+  // CIR: cir.store{{.*}} %[[CONST]], %{{.+}} : !s32i, !cir.ptr<!s32i>
 }
