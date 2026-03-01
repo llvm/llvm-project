ecc60a878be6eebc09d4c0d8b49fe05381204d48
Muhammad Bassiouni <60100307+bassiounix@users.noreply.github.com>
2026-03-01 04:08:52 +0200

    [libc][math] Cleanup shared/math (#183971)

--- libc/shared/math/floorl.h
+++ libc/shared/math/floorl.h
@@ -9,6 +9,7 @@
 #ifndef LLVM_LIBC_SHARED_MATH_FLOORL_H
 #define LLVM_LIBC_SHARED_MATH_FLOORL_H
 
+#include "shared/libc_common.h"
 #include "src/__support/math/floorl.h"
 
 namespace LIBC_NAMESPACE_DECL {
