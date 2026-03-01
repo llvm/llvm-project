f467e4e78f027997a520a5ee0fa9c07adf06de7c
Sukumarsawant <sawantsukumar@gmail.com>
2026-03-01 23:57:50 +0530

    remove: removed redundancy

--- libc/test/src/math/exhaustive/hypotbf16_test.cpp
+++ libc/test/src/math/exhaustive/hypotbf16_test.cpp
@@ -24,15 +24,7 @@ static constexpr uint16_t POS_STOP = 0x7f80U;
 static constexpr uint16_t NEG_START = 0x8000U;
 static constexpr uint16_t NEG_STOP = 0xff80U;
 
-// Subnormal range (positive)
-static constexpr uint16_t SUBNORM_POS_START = 0x0001U;
-static constexpr uint16_t SUBNORM_POS_STOP = 0x007FU;
-
-// Subnormal range (negative)
-static constexpr uint16_t SUBNORM_NEG_START = 0x8001U;
-static constexpr uint16_t SUBNORM_NEG_STOP = 0x807FU;
-
-TEST_F(LlvmLibcHypotBf16Test, NormalPositiveRange) {
+TEST_F(LlvmLibcHypotBf16Test, PositiveRange) {
   for (uint16_t v1 = POS_START; v1 <= POS_STOP; v1++) {
     for (uint16_t v2 = v1; v2 <= POS_STOP; v2++) {
 
@@ -46,7 +38,7 @@ TEST_F(LlvmLibcHypotBf16Test, NormalPositiveRange) {
   }
 }
 
-TEST_F(LlvmLibcHypotBf16Test, NormalNegativeRange) {
+TEST_F(LlvmLibcHypotBf16Test, NegativeRange) {
   for (uint16_t v1 = NEG_START; v1 <= NEG_STOP; v1++) {
     for (uint16_t v2 = v1; v2 <= NEG_STOP; v2++) {
 
@@ -60,33 +52,6 @@ TEST_F(LlvmLibcHypotBf16Test, NormalNegativeRange) {
   }
 }
 
-TEST_F(LlvmLibcHypotBf16Test, SubnormalNegativeRange) {
-  for (uint16_t v1 = SUBNORM_NEG_START; v1 <= SUBNORM_NEG_STOP; v1++) {
-    for (uint16_t v2 = v1; v2 <= SUBNORM_NEG_STOP; v2++) {
-
-      bfloat16 x = FPBits(v1).get_val();
-      bfloat16 y = FPBits(v2).get_val();
-      mpfr::BinaryInput<bfloat16> input{x, y};
-
-      EXPECT_MPFR_MATCH_ALL_ROUNDING(mpfr::Operation::Hypot, input,
-                                     LIBC_NAMESPACE::hypotbf16(x, y), 0.5);
-    }
-  }
-}
-
-TEST_F(LlvmLibcHypotBf16Test, SubnormalPositiveRange) {
-  for (uint16_t v1 = SUBNORM_POS_START; v1 <= SUBNORM_POS_STOP; v1++) {
-    for (uint16_t v2 = v1; v2 <= SUBNORM_POS_STOP; v2++) {
-
-      bfloat16 x = FPBits(v1).get_val();
-      bfloat16 y = FPBits(v2).get_val();
-      mpfr::BinaryInput<bfloat16> input{x, y};
-
-      EXPECT_MPFR_MATCH_ALL_ROUNDING(mpfr::Operation::Hypot, input,
-                                     LIBC_NAMESPACE::hypotbf16(x, y), 0.5);
-    }
-  }
-}
 TEST_F(LlvmLibcHypotBf16Test, SpecialNumbers) {
   constexpr bfloat16 VALUES[] = {zero,    neg_zero,   inf,
                                  neg_inf, min_normal, max_normal};
