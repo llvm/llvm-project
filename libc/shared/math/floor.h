a5c508b9967dc1da9ed7d5e25a69b9313b3ae7f7
Muhammad Bassiouni <60100307+bassiounix@users.noreply.github.com>
2026-03-01 04:08:52 +0200

    [libc][math] Cleanup shared/math (#183971)

--- libc/shared/math/floor.h
+++ libc/shared/math/floor.h
@@ -9,6 +9,7 @@
 #ifndef LLVM_LIBC_SHARED_MATH_FLOOR_H
 #define LLVM_LIBC_SHARED_MATH_FLOOR_H
 
+#include "shared/libc_common.h"
 #include "src/__support/math/floor.h"
 
 namespace LIBC_NAMESPACE_DECL {
--- libc/shared/math/floorbf16.h
+++ libc/shared/math/floorbf16.h
@@ -9,6 +9,7 @@
 #ifndef LLVM_LIBC_SHARED_MATH_FLOORBF16_H
 #define LLVM_LIBC_SHARED_MATH_FLOORBF16_H
 
+#include "shared/libc_common.h"
 #include "src/__support/math/floorbf16.h"
 
 namespace LIBC_NAMESPACE_DECL {
