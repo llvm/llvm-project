06f5c0d65ff6071974f8f7b3a4456881ccb423b6
Jim Ingham <jingham@apple.com>
2026-03-02 13:50:22 -0800

    Add a test that we recover from a crashing breakpoint condition.

---
+++ lldb/test/API/functionalities/breakpoint/breakpoint_conditions/crashing_condition/main.c
@@ -0,0 +1,14 @@
+unsigned int
+do_crash() {
+  char *bad_ptr = (char *) (0x20);
+  bad_ptr[0] = 'a';
+  return bad_ptr[0] == 'a';
+}
+
+int
+main()
+{
+  char *filler = "Set a start breakpoint here";
+  do_crash(); // Set the test breakpoint here
+  return 0;
+}
