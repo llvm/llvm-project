d04e9639d3552a7248731002b1dd0a829bc74ffd
Abhinav Gaba <abhinav.gaba@intel.com>
2026-03-02 13:55:38 -0800

    Reland "[OpenMP][Offload] Handle `present/to/from` when a different entry did `alloc/delete`."
    Some tests that were checking for prints inside/outside `target` regions
    needed to be updated to work on systems where the ordering wasn't
    deterministic.
    
    Original description:
    -----
    
    OpenMP allows cases like the following:
    
    ```c
      int *p1, *p2, x;
      p1 = p2 = &x;
      ...
      #pragma omp target_exit_data map(delete: p1[:]) from(p2[0])
    ```
    
    Which means, when the runtime encounters the `from` entry, the ref-count may
    not be zero, but it will go down to zero at the end of the current construct,
    which should cause the "from" transfer to happen.
    
    Similarly, a user may have:
    
    ```c
      struct S {
        int *p;
      };
    
      #pragma omp declare_mapper (id1: S s) map(s.p) map(present, alloc: s.p[0:10])
      #pragma omp declare_mapper (id2: S s) map(s.p, s.p[0:10])
    
      S s1;
    
     // present-check should fail here
     #pragma omp target_enter_data map(alloc: s.p[0:10]) map(mapper(id1), to: s)
     // "to" should be honored here
     #pragma omp target_enter_data map(alloc: s.p[0:10]) map(mapper(id2), to: s)
    ```
    
    Where the allocation happens before the "to" entry is encountered by the
    runtime. Or, an allocation happens before a "present" entry is encountered.
    
    To handle cases like this, we need to use the state information of previously
    seen new allocations, deletions, "from" entries, when honoring
    `to`/`from`/`present` map entries.
    
    -----
    
    This reverts commit 42a0fbc2c792f414e844328a363dbebe720a5255.

---
+++ offload/test/mapping/map_ordering_tgt_data_alloc_to_from.c
@@ -0,0 +1,18 @@
+// RUN: %libomptarget-compile-run-and-check-generic
+// XFAIL: intelgpu
+
+#include <stdio.h>
+
+int main() {
+  int x = 111;
+#pragma omp target data map(alloc : x) map(to : x) map(from : x) map(alloc : x)
+  {
+#pragma omp target map(present, alloc : x)
+    {
+      printf("%d\n", x); // CHECK: 111
+      x = x + 111;
+    }
+  }
+
+  printf("%d\n", x); // CHECK: 222
+}
