c6173991dd63ee53eb0a22d28730c190ae3106e0
TPPPP <906483498@qq.com>
2026-03-02 15:10:24 +0800

    add -ast-dump parameter

--- clang/test/Sema/gh182737.c
+++ clang/test/Sema/gh182737.c
@@ -1,7 +1,10 @@
-// RUN: %clang_cc1 -fsyntax-only -Wdocumentation -verify %s
+// RUN: %clang_cc1 -fsyntax-only -Wdocumentation -ast-dump -verify %s
 
-// expected-warning@+3 {{empty paragraph passed to '@param' command}}
-// expected-warning@+2 {{'@param' command used in a comment that is not attached to a function declaration}}
+#include <stdint.h>
+#include <stdbool.h>
+
+// expected-warning@+3 2 {{empty paragraph passed to '@param' command}}
+// expected-warning@+2 2 {{'@param' command used in a comment that is not attached to a function declaration}}
 /**
  * @param a
  */
@@ -16,7 +19,3 @@ typedef int my_int;
  *      - false: failure
  */
 typedef bool (*func_t)(uint8_t a);
-// expected-error@-1 {{type specifier missing, defaults to 'int'; ISO C99 and later do not support implicit int}}
-// expected-error@-2 {{unknown type name 'uint8_t'}}
-// expected-error@-3 {{type specifier missing, defaults to 'int'; ISO C99 and later do not support implicit int}}
-// expected-error@-4 {{function cannot return function type 'int (int)'}}
