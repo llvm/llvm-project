77d7fbb82762b20a3ea6fdf7020e903031d63626
Sukumarsawant <sawantsukumar@gmail.com>
2026-03-03 00:56:06 +0530

    chore: clang

--- libc/test/src/math/smoke/HypotTest.h
+++ libc/test/src/math/smoke/HypotTest.h
@@ -22,12 +22,10 @@ struct HypotTestTemplate : public LIBC_NAMESPACE::testing::Test {
   void test_special_numbers(Func func) {
     constexpr int N = 4;
     // Pythagorean triples.
-    constexpr T PYT[N][3] = {
-        {T(3), T(4), T(5)},
-        {T(5), T(12), T(13)},
-        {T(8), T(15), T(17)},
-        {T(7), T(24), T(25)},
-    };
+    constexpr T PYT[N][3] = {{T(3), T(4), T(5)},
+                             {T(5), T(12), T(13)},
+                             {T(8), T(15), 17},
+                             {T(7), T(24), T(25)}};
 
 #ifndef LIBC_TARGET_ARCH_IS_NVPTX
     // TODO: Investigate why sNaN tests are failing on nVidia.
