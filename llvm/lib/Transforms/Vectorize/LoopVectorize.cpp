8d91428e7ad4dd28b9ccf54540d4364b3e564c45
Tomer Shafir <tomer.shafir8@gmail.com>
2026-03-02 10:21:58 +0200

    [LV] Add debug print for TTI.MaxInterleaveFactor (NFC) (#183309)
    As its not currently visible in the debug output.
    
    ---------
    
    Co-authored-by: Sander de Smalen <sander.desmalen@arm.com>

--- llvm/lib/Transforms/Vectorize/LoopVectorize.cpp
+++ llvm/lib/Transforms/Vectorize/LoopVectorize.cpp
@@ -4766,6 +4766,8 @@ LoopVectorizationPlanner::selectInterleaveCount(VPlan &Plan, ElementCount VF,
 
   // Clamp the interleave ranges to reasonable counts.
   unsigned MaxInterleaveCount = TTI.getMaxInterleaveFactor(VF);
+  LLVM_DEBUG(dbgs() << "LV: MaxInterleaveFactor for the target is "
+                    << MaxInterleaveCount << "\n");
 
   // Check if the user has overridden the max.
   if (VF.isScalar()) {
