5145a3a77b82646ce189bb4ba0fb07d91563cb3e
Michael Buch <michaelbuch12@gmail.com>
2026-03-02 17:06:13 +0000

    [lldb][Target] Allow eLanguageTypeAssembly to use ScratchTypeSystemClang (#183771)
    After cleaning up some of our `LanguageType`/`SourceLangage`
    round-tripping (see `7f51a2a47d2e706d04855b0e41690ebafa2b3238`), a CU
    with `DW_LANG_MIPS_Assembler` will get a language type of
    `eLanguageTypeAssembly` (as opposed to `eLanguageTypeMipsAssembler`).
    Reason being that there is no `DW_LNAME_` (DWARFv6 language code) for
    `MIPS Assembler`, only for generic `Assembly`. So it's not possible to
    round-trip cleanly between pre-DWARFv6 and DWARFv6 language codes, which
    LLDB relies on for storing language types (and will lean into more
    heavily in the future). This broke a special provision we have where we
    allow `ScratchTypeSystemClang` to be used when evaluating expressions in
    assembly CUs (i.e., CUs where the debug-info explicitly sets the
    language to assembly).
    
    If we ever want to distinguish MIPS from other Assembly, the proper way
    to do so is introduce a `DW_LNAME_Mips_Assembler`. For now, this patch
    adds another case for `eLanguageTypeAssembly` in
    `GetScratchTypeSystemForLanguage`.
    
    The test is a bit quirky because it checks for error messages in all
    cases. For the `TypeSystem`s to exist we would have to initialize the
    `TypeSystem` plugins, which I couldn't find a precedent for. But happy
    to hear other suggestions for testing
    
    Fixes https://github.com/llvm/llvm-project/issues/183388
    
    (cherry picked from commit 447eba88c8d7f219667c9914a0d91bf44c1d1512)

--- lldb/source/Target/Target.cpp
+++ lldb/source/Target/Target.cpp
@@ -2618,7 +2618,8 @@ Target::GetScratchTypeSystemForLanguage(lldb::LanguageType language,
 
   if (language == eLanguageTypeMipsAssembler // GNU AS and LLVM use it for all
                                              // assembly code
-      || language == eLanguageTypeUnknown) {
+      || language == eLanguageTypeAssembly ||
+      language == eLanguageTypeUnknown) {
     LanguageSet languages_for_expressions =
         Language::GetLanguagesSupportingTypeSystemsForExpressions();
 
