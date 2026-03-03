08bdccb56f09a061c92617e5182e76de2044230a
Ahmad Yasin <ahmad.yasin@apple.com>
2026-03-03 22:32:54 +0200

    New AArch64CodeLayoutOpt pass for code layout optimizations.
    For simplicity, this initial version  induces function alignment when a pair is
    detected for two cases:
    I.  FCMP-FCSEL
    II. CMP/CMN-CSEL, 32-bit only
    
    Beyond performance improvement, this optimization helps to reduce noise due to code layout
    thus stabilizes performance. For example, knock-out effects on a "sensitive function" won't
    be triggered by codegen changes outside it.
    
    Each case can be enabled/disabled individually through -aarch64-code-layout-opt bit-mask.

--- llvm/lib/Target/AArch64/AArch64.h
+++ llvm/lib/Target/AArch64/AArch64.h
@@ -56,6 +56,7 @@ FunctionPass *createFalkorHWPFFixPass();
 FunctionPass *createFalkorMarkStridedAccessesPass();
 FunctionPass *createAArch64PointerAuthPass();
 FunctionPass *createAArch64BranchTargetsPass();
+FunctionPass *createAArch64CodeLayoutOptPass();
 FunctionPass *createAArch64MIPeepholeOptPass();
 FunctionPass *createAArch64PostCoalescerPass();
 
@@ -97,6 +98,7 @@ void initializeAArch64DeadRegisterDefinitionsPass(PassRegistry&);
 void initializeAArch64ExpandPseudoPass(PassRegistry &);
 void initializeAArch64LoadStoreOptLegacyPass(PassRegistry &);
 void initializeAArch64LowerHomogeneousPrologEpilogPass(PassRegistry &);
+void initializeAArch64CodeLayoutOptPass(PassRegistry &);
 void initializeAArch64MIPeepholeOptPass(PassRegistry &);
 void initializeAArch64O0PreLegalizerCombinerPass(PassRegistry &);
 void initializeAArch64PostCoalescerPass(PassRegistry &);
