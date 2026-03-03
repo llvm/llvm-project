c4339c22c9e20012b3c6037d900ee70fb55b8ef1
Anshul Nigham <nigham@google.com>
2026-03-03 08:48:36 -0800

    Add LLVM logo to furo-doc

--- llvm/docs/conf.py
+++ llvm/docs/conf.py
@@ -135,7 +135,7 @@ html_title = "LLVM"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-# html_logo = None
+html_logo = "_static/LLVMWyvernSmall.png"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
