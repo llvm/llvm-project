c092d36c8860bbd463c63996bb87462756110d8b
Paul Kirth <paulkirth@google.com>
2026-03-02 16:31:47 -0800

    Add license header

--- clang-tools-extra/clang-doc/benchmarks/ClangDocBenchmark.cpp
+++ clang-tools-extra/clang-doc/benchmarks/ClangDocBenchmark.cpp
@@ -1,9 +1,23 @@
-#include "../BitcodeReader.h"
-#include "../BitcodeWriter.h"
-#include "../ClangDoc.h"
-#include "../Generators.h"
-#include "../Representation.h"
-#include "../Serialize.h"
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file contains basic benchmarks for clang-doc's implementation and
+/// library components.
+///
+//===----------------------------------------------------------------------===//
+
+#include "BitcodeReader.h"
+#include "BitcodeWriter.h"
+#include "ClangDoc.h"
+#include "Generators.h"
+#include "Representation.h"
+#include "Serialize.h"
 #include "benchmark/benchmark.h"
 #include "clang/AST/ASTContext.h"
 #include "clang/AST/RecursiveASTVisitor.h"
