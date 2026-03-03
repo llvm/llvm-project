28f5e9df6c37c0bc903302b346dfcfc691c1fb26
Jean-Didier Pailleux <jean-didier.pailleux@sipearl.com>
2026-03-03 14:45:01 +0100

    Update flang-rt/lib/runtime/coarray.cpp
        Co-authored-by: Dan Bonachea <dobonachea@lbl.gov>

--- flang-rt/lib/runtime/coarray.cpp
+++ flang-rt/lib/runtime/coarray.cpp
@@ -32,10 +32,10 @@ void RTDEF(ComputeLastUcobound)(
     index *= ucobounds_ptr[i] - lcobounds_ptr[i] + 1;
   }
   if (corank == 1)
-    ucobounds_ptr[0] = lcobounds_ptr[0] + num_images;
+    ucobounds_ptr[0] = lcobounds_ptr[0] + num_images - 1;
   else if (index < num_images)
-    ucobounds_ptr[corank - 1] =
-        (num_images / index) + (num_images % index != 0);
+    ucobounds_ptr[corank - 1] = lcobounds_ptr[corank - 1] +
+        (num_images / index) + (num_images % index != 0) - 1;
   else
     ucobounds_ptr[corank - 1] = lcobounds_ptr[corank - 1];
 }
