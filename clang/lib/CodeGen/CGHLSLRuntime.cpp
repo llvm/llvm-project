898839ba3f7d88bb215cfe1a364c6ccd61a44efe
Nathan Gauër <brioche@google.com>
2026-03-04 16:39:24 +0100

    [HLSL][Clang] Support SV_VertexID semantic
    VertexID semantic only allows uint as type. Looking at DXC, it seems
    the uint16 variant is allowed, but that's not documented. Until we
    figure out the real behavior, we restrict the type to only uint.

--- clang/lib/CodeGen/CGHLSLRuntime.cpp
+++ clang/lib/CodeGen/CGHLSLRuntime.cpp
@@ -766,6 +766,15 @@ llvm::Value *CGHLSLRuntime::emitSystemSemanticLoad(
     }
   }
 
+  if (SemanticName == "SV_VERTEXID") {
+    if (ST == Triple::EnvironmentType::Vertex) {
+      if (CGM.getTarget().getTriple().isSPIRV())
+        return createSPIRVBuiltinLoad(B, CGM.getModule(), Type, Semantic->getAttrName()->getName(), /* BuiltIn::VertexIndex */ 42);
+      else
+        return emitDXILUserSemanticLoad(B, Type, Semantic, Index);
+    }
+  }
+
   llvm_unreachable(
       "Load hasn't been implemented yet for this system semantic. FIXME");
 }
