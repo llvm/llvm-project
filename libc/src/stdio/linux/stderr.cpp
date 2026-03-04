1720bcaf6b69dd50fbedca5eab079fcc024a976e
Petr Hosek <phosek@google.com>
2026-03-04 10:55:12 -0800

    [libc] Make standard streams entrypoints
    The standard streams should be defined the same as other public
    symbols, even though they're variables rather than functions:
    we should have public and private symbol which are aliases, and
    all internal uses should use the private symbol to avoid GOT
    relocations.
    
    This change introduces a new macro LLVM_LIBC_VARIABLE which is the
    equivalent of LLVM_LIBC_FUNCTION and it refactors the existing
    definitions of standard streams for BareMetal, Linux and GPU to
    use this macro.

--- libc/src/stdio/linux/stderr.cpp
+++ libc/src/stdio/linux/stderr.cpp
@@ -1,4 +1,4 @@
-//===--- Definition of Linux stdout ---------------------------------------===//
+//===--- Definition of Linux stderr ---------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,21 +6,19 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "file.h"
-#include "hdr/stdio_macros.h"
+#include "src/stdio/stderr.h"
+
 #include "hdr/types/FILE.h"
+#include "src/__support/File/linux/file.h"
+#include "src/__support/common.h"
 #include "src/__support/macros/config.h"
 
 namespace LIBC_NAMESPACE_DECL {
 
-constexpr size_t STDOUT_BUFFER_SIZE = 1024;
-uint8_t stdout_buffer[STDOUT_BUFFER_SIZE];
-static LinuxFile StdOut(1, stdout_buffer, STDOUT_BUFFER_SIZE, _IOLBF, false,
+constexpr size_t STDERR_BUFFER_SIZE = 0;
+static LinuxFile StdErr(2, nullptr, STDERR_BUFFER_SIZE, _IONBF, false,
                         File::ModeFlags(File::OpenMode::APPEND));
-File *stdout = &StdOut;
+
+LLVM_LIBC_VARIABLE(FILE *, stderr) = reinterpret_cast<FILE *>(&StdErr);
 
 } // namespace LIBC_NAMESPACE_DECL
-
-extern "C" {
-FILE *stdout = reinterpret_cast<FILE *>(&LIBC_NAMESPACE::StdOut);
-} // extern "C"
