84f649b3e81f1304472dfe9157ffee94d3b2c852
Erick Ochoa <erick.ochoalopez@amd.com>
2026-03-04 11:38:10 -0500

    Revert "Add initial implementation"
    This reverts commit 2da8610ade0b030a71eb70575372e8e7dcca96fa.

--- mlir/lib/Dialect/Arith/Transforms/InferExactFromDLTI.cpp
+++ mlir/lib/Dialect/Arith/Transforms/InferExactFromDLTI.cpp
@@ -11,7 +11,6 @@
 
 #include "mlir/Dialect/Arith/IR/Arith.h"
 #include "mlir/IR/PatternMatch.h"
-#include "mlir/IR/TypeUtilities.h"
 #include "mlir/Interfaces/DataLayoutInterfaces.h"
 #include "mlir/Transforms/WalkPatternRewriteDriver.h"
 
@@ -25,59 +24,20 @@ namespace arith {
 using namespace mlir;
 using namespace mlir::arith;
 
-static unsigned getBitwidth(Type type, unsigned indexBitwidth) {
-  Type elemType = getElementTypeOrSelf(type);
-  if (isa<IndexType>(elemType))
-    return indexBitwidth;
-  return elemType.getIntOrFloatBitWidth();
-}
-
 namespace {
-template <typename CastOp>
-struct InferExactOnIndexCast final : OpRewritePattern<CastOp> {
-  InferExactOnIndexCast(MLIRContext *context, unsigned indexBitwidth)
-      : OpRewritePattern<CastOp>(context), indexBitwidth(indexBitwidth) {}
-
-  LogicalResult matchAndRewrite(CastOp op,
-                                PatternRewriter &rewriter) const override {
-    if (op.getExact())
-      return failure();
-
-    unsigned srcBW = getBitwidth(op.getIn().getType(), indexBitwidth);
-    unsigned dstBW = getBitwidth(op.getType(), indexBitwidth);
-    if (srcBW > dstBW)
-      return rewriter.notifyMatchFailure(op, "source is wider than dest");
-
-    rewriter.modifyOpInPlace(op, [&] { op.setExact(true); });
-    return success();
-  }
-
-private:
-  unsigned indexBitwidth;
-};
-
 struct ArithInferExactFromDLTIPass
     : public arith::impl::ArithInferExactFromDLTIBase<
           ArithInferExactFromDLTIPass> {
 
   void runOnOperation() override {
-    Operation *op = getOperation();
-    MLIRContext *ctx = op->getContext();
-
-    DataLayout layout = DataLayout::closest(op);
-    unsigned indexBitwidth = layout.getTypeSizeInBits(IndexType::get(ctx));
-
-    RewritePatternSet patterns(ctx);
-    populateInferExactFromDLTIPatterns(patterns, indexBitwidth);
-
-    walkAndApplyPatterns(op, std::move(patterns));
+    // TODO: Query DataLayout for index bitwidth and apply patterns to add
+    // exact on index casts where the source type is narrower than the dest.
   }
 };
 } // end anonymous namespace
 
 void mlir::arith::populateInferExactFromDLTIPatterns(
     RewritePatternSet &patterns, unsigned indexBitwidth) {
-  patterns.add<InferExactOnIndexCast<IndexCastOp>,
-               InferExactOnIndexCast<IndexCastUIOp>>(patterns.getContext(),
-                                                     indexBitwidth);
+  // TODO: Add patterns for IndexCastOp and IndexCastUIOp that set exact
+  // when the source type is narrower than the destination type.
 }
