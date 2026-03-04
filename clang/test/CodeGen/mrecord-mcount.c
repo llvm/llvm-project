27616c73eaf6c3ac2fcfc3f963ba3dd2ae15f3ee
Paul Murphy <murp@redhat.com>
2026-03-04 08:47:16 -0600

    [clang] enable -mnop-mcount -mrecord-mcount for x86_64
    These are gated by -mfentry.

--- clang/test/CodeGen/mrecord-mcount.c
+++ clang/test/CodeGen/mrecord-mcount.c
@@ -6,6 +6,14 @@
 // RUN:   %s 2>&1 | FileCheck -check-prefix=NOPG %s
 // RUN: %clang_cc1 -mrecord-mcount -triple s390x-ibm-linux -emit-llvm -o - %s \
 // RUN:   2>&1 | FileCheck -check-prefix=NOPG %s
+// RUN: %clang_cc1 -pg -mfentry -mrecord-mcount -triple x86_64-ibm-linux -emit-llvm \
+// RUN:   -o - %s 2>&1 | FileCheck  %s
+// RUN: not %clang_cc1 -pg -mrecord-mcount -triple x86_64-ibm-linux -emit-llvm -o - \
+// RUN:   %s 2>&1 | FileCheck -check-prefix=NOMFENTRY %s
+// RUN: %clang_cc1 -mfentry -mrecord-mcount -triple x86_64-ibm-linux -emit-llvm -o - \
+// RUN:   %s 2>&1 | FileCheck -check-prefix=NOPG %s
+// RUN: %clang_cc1 -mrecord-mcount -triple x86_64-ibm-linux -emit-llvm -o - %s \
+// RUN:   2>&1 | FileCheck -check-prefix=NOPG %s
 
 int foo(void) {
   return 0;
