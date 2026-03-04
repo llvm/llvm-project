803eba01ab0692ed137d202f8eaefa7a8560b6f9
Sarah Spall <sarahspall@microsoft.com>
2026-03-04 12:18:56 -0800

    [HLSL] groupshared variables should be implicitly extern and should not be initialized (#184459)
    Makes groupshared variables have extern storage class which prevents
    them from being initialized if no initializer is provided. If one is
    provided as below:
    ```
    groupshared uint A = 1;
    ```
    A warning, which is an error by default, is produced that says the
    initializer is ignored and the variable is NOT initialized.
    Closes #183602

--- clang/lib/Sema/SemaHLSL.cpp
+++ clang/lib/Sema/SemaHLSL.cpp
@@ -4535,6 +4535,11 @@ void SemaHLSL::ActOnVariableDeclarator(VarDecl *VD) {
         }
       }
     }
+
+    // Mark groupshared variables as extern so they will have
+    // external storage and won't be default initialized
+    if (VD->hasAttr<HLSLGroupSharedAddressSpaceAttr>())
+      VD->setStorageClass(StorageClass::SC_Extern);
   }
 
   deduceAddressSpace(VD);
