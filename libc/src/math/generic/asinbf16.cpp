119962d05911a9f940b958a5cab4ee546809f94c
Sukumarsawant <sawantsukumar@gmail.com>
2026-03-04 21:44:54 +0530

    chore: nit

--- libc/src/math/generic/asinbf16.cpp
+++ libc/src/math/generic/asinbf16.cpp
@@ -1,4 +1,4 @@
-//===-- Half-precision asinbf16(x) function -------------------------------===//
+//===-- Implementation for asinbf16(x) function ---------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
