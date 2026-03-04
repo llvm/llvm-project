1e75bfed29cb9e48ceb3ff95a592bbe7362323e2
Stefan Gränitz <stefan.graenitz@gmail.com>
2026-03-04 09:54:13 +0100

    Fix file headers

--- llvm/include/llvm/Support/HTTP/HTTPClient.h
+++ llvm/include/llvm/Support/HTTP/HTTPClient.h
@@ -1,4 +1,4 @@
-//===-- llvm/Support/HTTPClient.h - HTTP client library ---------*- C++ -*-===//
+//===--- HTTPClient.h - HTTP client library ---------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
--- llvm/lib/Support/HTTP/HTTPClient.cpp
+++ llvm/lib/Support/HTTP/HTTPClient.cpp
@@ -1,4 +1,4 @@
-//===-- llvm/Debuginfod/HTTPClient.cpp - HTTP client library ----*- C++ -*-===//
+//===--- HTTPClient.cpp - HTTP client library -----------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
