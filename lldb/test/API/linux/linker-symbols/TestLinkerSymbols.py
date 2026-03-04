32f0466a4e0a7091bb620c7ef6bee3e719f35086
Caroline Tice <cmtice@google.com>
2026-03-04 14:22:16 -0800

    Update test to only run on linux.

--- lldb/test/API/linux/linker-symbols/TestLinkerSymbols.py
+++ lldb/test/API/linux/linker-symbols/TestLinkerSymbols.py
@@ -14,8 +14,7 @@ class TestLinkerSymbols(TestBase):
     # each debug info format.
     NO_DEBUG_INFO_TESTCASE = True
 
-    @skipIf(oslist=["darwin", "macos"])
-    @skipIfWindows
+    @skipIf(oslist=nomatch(["linux"]))
     def test_linker_symbols(self):
         build_dict = dict(LD_EXTRAS="-Wl,-T," + self.getSourcePath("linker.script"))
         self.build(dictionary=build_dict)
