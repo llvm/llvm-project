d563bab02665278e579253fc5312023fd3873939
Sindhu Chittireddy <sindhu.chittireddy@intel.com>
2026-03-05 14:18:35 -0800

    Change test to show that memset is correctly called with these changes.

--- clang/test/CodeGenCXX/microsoft-abi-diamond-template-multiple-vbptrs.cpp
+++ clang/test/CodeGenCXX/microsoft-abi-diamond-template-multiple-vbptrs.cpp
@@ -7,7 +7,7 @@ namespace test {
 
 class Base {
 public:
-    virtual ~Base() {}
+  virtual ~Base() {}
 };
 
 class Left : public virtual Base {
@@ -19,21 +19,63 @@ class Right : public virtual Base {
 class Diamond : public Left, public Right {
 };
 
-// Template class that triggers the bug
+// Test 1: Diamond inheritance in a template that triggers the bug
 template<typename T>
 class Derived : public Diamond {
 public:
-    // CHECK-LABEL: define {{.*}} @"??0?$Derived@H@test@@QEAA@XZ"
-    // CHECK: call {{.*}} @"??0Diamond@test@@QEAA@XZ"
-    // EmitNullBaseClassInitialization now correctly calculates memory regions
-    // around the vbptrs without hitting negative size assertion
+  // CHECK-LABEL: define {{.*}} @"??0?$Derived@H@test@@QEAA@XZ"
 
-    // CHECK: ret
+  // Layout of Derived<int>:
+  // offset 0: vbptr for Left (8 bytes)
+  // offset 8: vbptr for Right (8 bytes)
+  // offset 16+: virtual base Base
 
-    Derived() : Diamond() {}
+  // CHECK: call {{.*}} @"??0Diamond@test@@QEAA@XZ"
+  // EmitNullBaseClassInitialization now correctly calculates memory regions
+  // around the vbptrs without hitting negative size assertion
+
+  // No memset is generated since there are no gaps to zero
+  // CHECK-NOT: call void @llvm.memset
+  // CHECK: ret
+
+  Derived() : Diamond() {}
 };
 
 // Explicit instantiation to trigger code generation
 template class Derived<int>;
+
+// Test 2: Diamond in a template, with data members (calls memset)
+class DiamondWithData : public Left, public Right {
+public:
+  int x;
+  int y;
+};
+
+template<typename T>
+class DerivedWithData : public DiamondWithData {
+public:
+  DerivedWithData();
+};
+
+// CHECK-LABEL: define {{.*}} @"??0?$DerivedWithData@H@test@@QEAA@XZ"
+//
+// Layout of DerivedWithData<int>:
+// offset 0: vbptr for Left (8 bytes)
+// offset 8: vbptr for Right (8 bytes)
+// offset 16: x (4 bytes)
+// offset 20: y (4 bytes)
+// offset 24+: virtual base Base
+//
+// EmitNullBaseClassInitialization zero-initializes the data members [16, 24)
+// while skipping both vbptrs [0, 16)
+//
+// memset zeros 8 bytes for x and y at offset 16
+// CHECK: call void @llvm.memset.p0.i64(ptr {{.*}}, i8 0, i64 8, i1 false)
+
+template<typename T>
+DerivedWithData<T>::DerivedWithData() : DiamondWithData() {
+}
+
+template struct DerivedWithData<int>;
 
 } // namespace test
