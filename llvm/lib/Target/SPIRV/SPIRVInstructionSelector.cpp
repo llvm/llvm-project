c54bc306c38b695e6e713002ef9608c36c63ce9a
Juan Manuel Martinez Caamaño <jmartinezcaamao@gmail.com>
2026-03-05 15:21:34 -0300

    [NFC][SPIRV] Remove `MachineInstr` from `DeadMIs` set after erasing it (#184795)

--- llvm/lib/Target/SPIRV/SPIRVInstructionSelector.cpp
+++ llvm/lib/Target/SPIRV/SPIRVInstructionSelector.cpp
@@ -837,6 +837,7 @@ bool SPIRVInstructionSelector::select(MachineInstr &I) {
     // erase it
     LLVM_DEBUG(dbgs() << "Instruction is folded and dead.\n");
     removeDeadInstruction(I);
+    DeadMIs.erase(&I);
     return true;
   }
 
