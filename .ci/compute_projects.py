d3387fe406476f783d12fde40bffd0fa0fa62b03
JP Lehr <JanPatrick.Lehr@amd.com>
2026-03-05 06:42:46 -0600

    Integrate reviewer feedback
    re-added OpenMP -> check-openmp mapping
    Added Offload -> check-offload mapping

--- .ci/compute_projects.py
+++ .ci/compute_projects.py
@@ -163,8 +163,10 @@ PROJECT_CHECK_TARGETS = {
     "libc": "check-libc",
     "libclc": "check-libclc",
     "mlir": "check-mlir",
+    "openmp": "check-openmp",
     "polly": "check-polly",
     "lit": "check-lit",
+    "offload": "check-offload",
 }
 
 RUNTIMES = {
