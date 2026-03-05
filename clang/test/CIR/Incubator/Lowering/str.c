e71135e8bc7fcfbd62bec6ba8c96a82fb5543131
16srivarshitha <mvarshitha17874@gmail.com>
2026-03-06 00:26:28 +0530

    Revert "[CIR] Remove duplicate cuda.h from CIR directory"
    This reverts commit f65e49e206918770e0570a3e40857daf5bb3a024.

--- clang/test/CIR/Incubator/Lowering/str.c
+++
@@ -1,9 +0,0 @@
-// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fclangir -emit-llvm %s -o %t.ll
-// RUN: FileCheck --input-file=%t.ll %s -check-prefix=LLVM
-
-void f(char *fmt, ...);
-void test() {
-    f("test\0");
-}
-
-// LLVM: @.str = {{.*}}[6 x i8] c"test\00\00"
