ff633dddfee33944ed268be549ca6a6d2798230c
wanglei <wanglei@loongson.cn>
2026-03-06 16:08:29 +0800

    Address @MaskRay's comments
    Created using spr 1.3.7

--- lld/test/ELF/loongarch-relax-synthetic-in-text.s
+++ lld/test/ELF/loongarch-relax-synthetic-in-text.s
@@ -1,12 +1,22 @@
 # REQUIRES: loongarch
 # RUN: rm -rf %t && split-file %s %t
-# RUN: llvm-mc --filetype=obj -triple=loongarch64 %t/a.s -o %t/a.o
-# RUN: ld.lld -T %t/a.ld %t/a.o -o /dev/null
+# RUN: llvm-mc --filetype=obj -triple=loongarch64 -mattr=+relax %t/a.s -o %t/a.o
 
-## This test ensures that the relaxation pass does not crash when it encounters
-## a synthetic section (like .got) that has been placed inside an executable
-## output section via a linker script. Synthetic sections do not have relaxAux
-## data structures initialized.
+## Do not crash when we encounter a synthetic section (like .got) that has
+## been placed inside an executable output section via a linker script.
+## Synthetic sections do not have relaxAux data structures initialized.
+
+# RUN: ld.lld -T %t/a.ld %t/a.o -o %t/nopie
+# RUN: llvm-objdump -s -j.text %t/nopie | FileCheck %s --check-prefixes=CHECK-NOPIE
+
+# RUN: ld.lld -pie -T %t/a.ld %t/a.o -o %t/pie
+# RUN: llvm-objdump -s -j.text %t/pie | FileCheck %s --check-prefix=CHECK-PIE
+
+# CHECK-NOPIE:      Contents of section .text:
+# CHECK-NOPIE-NEXT: 0000 0400001a 8440c002 10000000 00000000
+
+# CHECK-PIE:        Contents of section .text:
+# CHECK-PIE-NEXT:   0060 0400001a 8400c502 00000000 00000000
 
 #--- a.s
 .global _start
--- lld/test/ELF/riscv-relax-synthetic-in-text.s
+++ lld/test/ELF/riscv-relax-synthetic-in-text.s
@@ -1,12 +1,24 @@
 # REQUIRES: riscv
 # RUN: rm -rf %t && split-file %s %t
-# RUN: llvm-mc --filetype=obj -triple=riscv64 %t/a.s -o %t/a.o
-# RUN: ld.lld -T %t/a.ld %t/a.o -o /dev/null
+# RUN: llvm-mc --filetype=obj -triple=riscv64 -mattr=+relax %t/a.s -o %t/a.o
 
-## This test ensures that the relaxation pass does not crash when it encounters
-## a synthetic section (like .got) that has been placed inside an executable
-## output section via a linker script. Synthetic sections do not have relaxAux
-## data structures initialized.
+## Do not crash when we encounter a synthetic section (like .got) that has
+## been placed inside an executable output section via a linker script.
+## Synthetic sections do not have relaxAux data structures initialized.
+
+# RUN: ld.lld -T %t/a.ld %t/a.o -o %t/nopie
+# RUN: llvm-objdump -s -j.text %t/nopie | FileCheck %s --check-prefixes=CHECK-NOPIE
+
+# RUN: ld.lld -pie -T %t/a.ld %t/a.o -o %t/pie
+# RUN: llvm-objdump -s -j.text %t/pie | FileCheck %s --check-prefix=CHECK-PIE
+
+# CHECK-NOPIE:      Contents of section .text:
+# CHECK-NOPIE-NEXT:  0000 17050000 03350501 00000000 00000000
+# CHECK-NOPIE-NEXT:  0010 18000000 00000000
+
+# CHECK-PIE:        Contents of section .text:
+# CHECK-PIE-NEXT:    0060 17050000 03350501 78000000 00000000
+# CHECK-PIE-NEXT:    0070 00000000 00000000
 
 #--- a.s
 .global _start
