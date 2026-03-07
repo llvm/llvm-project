1687ad77540316f1ced4431348be050c35bfea21
Kai Huang <kcloudy0717@gmail.com>
2026-03-07 20:08:55 +0800

    [HLSL][DXIL][SPIRV] QuadReadAcrossY intrinsic support

--- llvm/lib/Target/DirectX/DXILShaderFlags.cpp
+++ llvm/lib/Target/DirectX/DXILShaderFlags.cpp
@@ -107,6 +107,7 @@ static bool checkWaveOps(Intrinsic::ID IID) {
   case Intrinsic::dx_wave_prefix_uproduct:
     // Quad Op Variants
   case Intrinsic::dx_quad_read_across_x:
+  case Intrinsic::dx_quad_read_across_y:
     return true;
   }
 }
