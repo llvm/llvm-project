7204b33b8e56b14bae224f5a364d94bf8363ae1d
albertbolt <albertboltinfinity@gmail.com>
2026-03-08 01:01:20 +0530

    added iswlower entry point

---
+++ libc/test/src/wctype/iswlower_test.cpp
@@ -0,0 +1,22 @@
+//===-- Unittests for iswlower --------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "src/__support/CPP/span.h"
+#include "src/wctype/iswlower.h"
+
+#include "test/UnitTest/Test.h"
+
+TEST(LlvmLibciswlower, SimpleTest) {
+  EXPECT_NE(LIBC_NAMESPACE::iswlower('a'), 0);
+  EXPECT_NE(LIBC_NAMESPACE::iswlower('b'), 0);
+  EXPECT_NE(LIBC_NAMESPACE::iswlower('z'), 0);
+
+  EXPECT_EQ(LIBC_NAMESPACE::iswlower('A'), 0);
+  EXPECT_EQ(LIBC_NAMESPACE::iswlower('B'), 0);
+  EXPECT_EQ(LIBC_NAMESPACE::iswlower('Z'), 0);
+}
