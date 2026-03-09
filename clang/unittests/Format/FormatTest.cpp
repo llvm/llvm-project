81e6247e4b23453e4379aeaa786ad3fe7f09f056
Owen Pan <owenpiano@gmail.com>
2026-03-08 22:48:17 -0700

    [clang-format] Fix an assertion failure on invalid C++ lambda
    Fixes #185268

--- clang/unittests/Format/FormatTest.cpp
+++ clang/unittests/Format/FormatTest.cpp
@@ -29245,6 +29245,10 @@ TEST_F(FormatTest, UnbalancedAngleBrackets) {
                 getLLVMStyleWithColumns(50));
 }
 
+TEST_F(FormatTest, LambdaArrowAsTrailingReturnArrow) {
+  verifyNoCrash("void foo()([] consteval -> int {}())");
+}
+
 } // namespace
 } // namespace test
 } // namespace format
