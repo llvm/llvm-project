b5df086c5d5962c448f1ede05aba045bbcf5a74f
Louis Dionne <ldionne.2@gmail.com>
2026-03-10 16:27:52 -0400

    [libc++] Rename a few benchmarks to allow identifying what's being benchmarked from the name

--- libcxx/test/benchmarks/adjacent_view_begin.bench.cpp
+++ libcxx/test/benchmarks/adjacent_view_begin.bench.cpp
@@ -27,17 +27,17 @@ void BM_adjacent_full(benchmark::State& state) {
   }
 }
 
-BENCHMARK(BM_adjacent_full<2>);
-BENCHMARK(BM_adjacent_full<3>);
-BENCHMARK(BM_adjacent_full<4>);
-BENCHMARK(BM_adjacent_full<5>);
-BENCHMARK(BM_adjacent_full<6>);
-BENCHMARK(BM_adjacent_full<7>);
-BENCHMARK(BM_adjacent_full<8>);
-BENCHMARK(BM_adjacent_full<9>);
-BENCHMARK(BM_adjacent_full<10>);
-BENCHMARK(BM_adjacent_full<100>);
-BENCHMARK(BM_adjacent_full<1000>);
+BENCHMARK(BM_adjacent_full<2>)->Name("rng::adjacent_view::begin() (full, size 2)");
+BENCHMARK(BM_adjacent_full<3>)->Name("rng::adjacent_view::begin() (full, size 3)");
+BENCHMARK(BM_adjacent_full<4>)->Name("rng::adjacent_view::begin() (full, size 4)");
+BENCHMARK(BM_adjacent_full<5>)->Name("rng::adjacent_view::begin() (full, size 5)");
+BENCHMARK(BM_adjacent_full<6>)->Name("rng::adjacent_view::begin() (full, size 6)");
+BENCHMARK(BM_adjacent_full<7>)->Name("rng::adjacent_view::begin() (full, size 7)");
+BENCHMARK(BM_adjacent_full<8>)->Name("rng::adjacent_view::begin() (full, size 8)");
+BENCHMARK(BM_adjacent_full<9>)->Name("rng::adjacent_view::begin() (full, size 9)");
+BENCHMARK(BM_adjacent_full<10>)->Name("rng::adjacent_view::begin() (full, size 10)");
+BENCHMARK(BM_adjacent_full<100>)->Name("rng::adjacent_view::begin() (full, size 100)");
+BENCHMARK(BM_adjacent_full<1000>)->Name("rng::adjacent_view::begin() (full, size 1000)");
 
 template <size_t N>
 void BM_adjacent_empty(benchmark::State& state) {
@@ -49,17 +49,17 @@ void BM_adjacent_empty(benchmark::State& state) {
   }
 }
 
-BENCHMARK(BM_adjacent_empty<2>);
-BENCHMARK(BM_adjacent_empty<3>);
-BENCHMARK(BM_adjacent_empty<4>);
-BENCHMARK(BM_adjacent_empty<5>);
-BENCHMARK(BM_adjacent_empty<6>);
-BENCHMARK(BM_adjacent_empty<7>);
-BENCHMARK(BM_adjacent_empty<8>);
-BENCHMARK(BM_adjacent_empty<9>);
-BENCHMARK(BM_adjacent_empty<10>);
-BENCHMARK(BM_adjacent_empty<100>);
-BENCHMARK(BM_adjacent_empty<1000>);
+BENCHMARK(BM_adjacent_empty<2>)->Name("rng::adjacent_view::begin() (empty, size 2)");
+BENCHMARK(BM_adjacent_empty<3>)->Name("rng::adjacent_view::begin() (empty, size 3)");
+BENCHMARK(BM_adjacent_empty<4>)->Name("rng::adjacent_view::begin() (empty, size 4)");
+BENCHMARK(BM_adjacent_empty<5>)->Name("rng::adjacent_view::begin() (empty, size 5)");
+BENCHMARK(BM_adjacent_empty<6>)->Name("rng::adjacent_view::begin() (empty, size 6)");
+BENCHMARK(BM_adjacent_empty<7>)->Name("rng::adjacent_view::begin() (empty, size 7)");
+BENCHMARK(BM_adjacent_empty<8>)->Name("rng::adjacent_view::begin() (empty, size 8)");
+BENCHMARK(BM_adjacent_empty<9>)->Name("rng::adjacent_view::begin() (empty, size 9)");
+BENCHMARK(BM_adjacent_empty<10>)->Name("rng::adjacent_view::begin() (empty, size 10)");
+BENCHMARK(BM_adjacent_empty<100>)->Name("rng::adjacent_view::begin() (empty, size 100)");
+BENCHMARK(BM_adjacent_empty<1000>)->Name("rng::adjacent_view::begin() (empty, size 1000)");
 
 } // namespace
 
