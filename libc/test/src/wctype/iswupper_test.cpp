fbadfa05c2d682308593a2fdc87187439167d2dc
Junyu Liu <3221693279@qq.com>
2026-03-10 09:19:07 +0000

    [libc] Update outdated comments in iswupper unittest

--- libc/test/src/wctype/iswupper_test.cpp
+++ libc/test/src/wctype/iswupper_test.cpp
@@ -21,33 +21,5 @@ TEST(LlvmLibciswupper, SimpleTest) {
   EXPECT_EQ(LIBC_NAMESPACE::iswupper(-1), 0);
 }
 
-// TODO: once iswupper supports more than just ascii-range characters add a
-// proper test.
-
-// namespace {
-
-// // TODO: Merge the wctype tests using this framework.
-// constexpr char WUPPER_ARRAY[] = {
-//     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-//     'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-// };
-
-// bool in_span(int ch, LIBC_NAMESPACE::cpp::span<const char> arr) {
-//   for (size_t i = 0; i < arr.size(); ++i)
-//     if (static_cast<int>(arr[i]) == ch)
-//       return true;
-//   return false;
-// }
-
-// } // namespace
-
-// TEST(LlvmLibciswupper, DefaultLocale) {
-//   // Loops through all characters, verifying that letters return
-//   // true and everything else returns false.
-//   for (int ch = -255; ch < 255; ++ch) {
-//     if (in_span(ch, WUPPER_ARRAY))
-//       EXPECT_TRUE(LIBC_NAMESPACE::iswupper(ch));
-//     else
-//       EXPECT_FALSE(LIBC_NAMESPACE::iswupper(ch));
-//   }
-// }
+// Tests for the extended range are located in
+// libc/test/src/__support/wctype_utils_test.cpp.
