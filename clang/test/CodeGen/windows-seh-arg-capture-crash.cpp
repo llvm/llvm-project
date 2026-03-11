d770a3b8717e0730576b687c4bb3d6a8bc9b6da4
GkvJwa <gkvjwa@gmail.com>
2026-03-12 00:00:30 +0800

    Add localrecover and load

--- clang/test/CodeGen/windows-seh-arg-capture-crash.cpp
+++ clang/test/CodeGen/windows-seh-arg-capture-crash.cpp
@@ -18,6 +18,14 @@ void f(span_a input) {
 // CHECK: entry:
 // CHECK:   call void (...) @llvm.localescape(ptr %input)
 
+// CHECK-LABEL: define internal noundef i32 @"?filt$0@0@f@@"(ptr noundef %exception_pointers
+// CHECK: entry:
+// CHECK:   %frame_pointer.addr = alloca ptr, align 8
+// CHECK:   %exception_pointers.addr = alloca ptr, align 8
+// CHECK:   %0 = call ptr @llvm.eh.recoverfp(ptr @"?f@@YAXVspan_a@@@Z", ptr %frame_pointer)
+// CHECK:   %input = call ptr @llvm.localrecover(ptr @"?f@@YAXVspan_a@@@Z", ptr %0, i32 0)
+
+
 typedef __SIZE_TYPE__ size_t;
 
 class span_b {
@@ -38,3 +46,11 @@ void f(span_b input) {
 // CHECK: entry:
 // CHECK:   %input.spill = alloca ptr, align 8
 // CHECK:   call void (...) @llvm.localescape(ptr %input.spill)
+
+// CHECK-LABEL: define internal noundef i32 @"?filt$0@0@f@@.1"(ptr noundef %exception_pointers
+// CHECK: entry:
+// CHECK:   %frame_pointer.addr = alloca ptr, align 8
+// CHECK:   %exception_pointers.addr = alloca ptr, align 8
+// CHECK:   %0 = call ptr @llvm.eh.recoverfp(ptr @"?f@@YAXVspan_b@@@Z", ptr %frame_pointer)
+// CHECK:   %1 = call ptr @llvm.localrecover(ptr @"?f@@YAXVspan_b@@@Z", ptr %0, i32 0)
+// CHECK:   %input = load ptr, ptr %1, align 8
