b1dbad85f190901306bd4f37b7780f9d47afb96c
Saleem Abdulrasool <compnerd@compnerd.org>
2026-03-11 14:41:21 -0700

    test: correct clang-cache-pch-cl-mode
    Fix the test as reported by @drodriguez.
    
    (cherry picked from commit 241b3fcdc7d938426b15d985a19c24f7d955e8e5)

--- clang/test/CAS/clang-cache-pch-cl-mode.c
+++ clang/test/CAS/clang-cache-pch-cl-mode.c
@@ -1,9 +1,9 @@
 // RUN: rm -rf %t
 // RUN: split-file %s %t
 
-// RUN: env LLVM_CACHE_CAS_PATH=%t/cas %clang-cache %clang_cl /Yc%t/cmake_pch.hh /Fp%t/cmake_pch.pch /FI%t/cmake_pch.hh /Fo%t/cmake_pch.cc.obj /Fd%t -Xclang -Rcompile-job-cache -c -- %t/cmake_pch.cc 2>&1 | FileCheck -check-prefix CHECK-CACHE-MISS
-// RUN: env LLVM_CACHE_CAS_PATH=%t/cas %clang-cache %clang_cl /Yc%t/cmake_pch.hh /Fp%t/cmake_pch.pch /FI%t/cmake_pch.hh /Fo%t/cmake_pch.cc.obj /Fd%t -Xclang -Rcompile-job-cache -c -- %t/cmake_pch.cc 2>&1 | FileCheck -check-prefix CHECK-CACHE-HIT
-// RUN: env LLVM_CACHE_CAS_PATH=%t/cas %clang-cache %clang_cl /Yu%t/cmake_pch.hh /Fp%t/cmake_pch.pch /FI%t/cmake_pch.hh /Fo%t/test.cc.obj /Fd%t -Xclang -Rcompile-job-cache -c -- %t/test.cc 2>&1 | FileCheck -check-prefix CHECK-CACHE-HIT
+// RUN: env LLVM_CACHE_CAS_PATH=%t/cas %clang-cache %clang_cl /Yc%t/cmake_pch.hh /Fp%t/cmake_pch.pch /FI%t/cmake_pch.hh /Fo%t/cmake_pch.cc.obj /Fd%t -Xclang -Rcompile-job-cache -c -- %t/cmake_pch.cc 2>&1 | FileCheck %s -check-prefix CHECK-CACHE-MISS
+// RUN: env LLVM_CACHE_CAS_PATH=%t/cas %clang-cache %clang_cl /Yc%t/cmake_pch.hh /Fp%t/cmake_pch.pch /FI%t/cmake_pch.hh /Fo%t/cmake_pch.cc.obj /Fd%t -Xclang -Rcompile-job-cache -c -- %t/cmake_pch.cc 2>&1 | FileCheck %s -check-prefix CHECK-CACHE-HIT
+// RUN: env LLVM_CACHE_CAS_PATH=%t/cas %clang-cache %clang_cl /Yu%t/cmake_pch.hh /Fp%t/cmake_pch.pch /FI%t/cmake_pch.hh /Fo%t/test.cc.obj /Fd%t -Xclang -Rcompile-job-cache -c -- %t/test.cc 2>&1 | FileCheck %s -check-prefix CHECK-CACHE-MISS
 
 // CHECK-CACHE-HIT: remark: compile job cache hit
 // CHECK-CACHE-MISS: remark: compile job cache miss
