e1fd951f506f0378574a974f0535789f9a699cca
Pengxiang Huang <huangpengxiang70@gmail.com>
2026-03-11 12:34:26 -0400

    format some files

--- libc/src/sys/sem/semctl.h
+++ libc/src/sys/sem/semctl.h
@@ -1,5 +1,4 @@
-//===-- Implementation header for semctl -------------------------*- C++
-//-*-===//
+//===-- Implementation header for semctl ------------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
--- libc/src/sys/sem/semop.h
+++ libc/src/sys/sem/semop.h
@@ -1,5 +1,4 @@
-//===-- Implementation header for semop --------------------------*- C++
-//-*-===//
+//===-- Implementation header for semop -------------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
