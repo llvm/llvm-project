7701606ef879fd6283e097a0171b505edfa7887b
David Tenty <daltenty@ibm.com>
2026-03-11 12:02:23 -0400

    [clang][Headers] add endian.h
    POSIX.1-2024 defines a header called
    `endian.h`` which contains macros and helpers for
    handling byte order conversions.
    
    Unfortunately it is not available on all platforms,
    however LLVM libc has a useful endian.h
    implementation that is essentially only casts and
    builtins.
    
    This PR draws on that implementation to add a
    clang header so applications on
    platforms without it can make use of this header.
    The clang header forwards to the system header if
    available, so existing usages are not affected.

---
+++ clang/test/Headers/endian.c
@@ -0,0 +1,40 @@
+// RUN: %clang_cc1 -triple powerpc64-unknown -fsyntax-only -verify -ffreestanding %s
+// RUN: %clang_cc1 -triple ppc64le-unknown -fsyntax-only -verify -xc++ -ffreestanding %s
+// expected-no-diagnostics
+
+#include <endian.h>
+
+
+#if BYTE_ORDER == BIG_ENDIAN
+
+_Static_assert(htobe16(0xBEEF) == 0xBEEF, "");
+_Static_assert(htobe32(0xDEADBEEF) == 0xDEADBEEF, "");
+_Static_assert(htobe64(0xDEADBEEFDEADBEEF) == 0xDEADBEEFDEADBEEF, "");
+_Static_assert(htole16(0xBEEF) == 0xEFBE, "");
+_Static_assert(htole32(0xDEADBEEF) == 0xEFBEADDE, "");
+_Static_assert(htole64(0xDEADBEEFDEADBEEF) == 0xEFBEADDEEFBEADDE, "");
+_Static_assert(be16toh(0xBEEF) == 0xBEEF, "");
+_Static_assert(be32toh(0xDEADBEEF) == 0xDEADBEEF, "");
+_Static_assert(be64toh(0xDEADBEEFDEADBEEF) == 0xDEADBEEFDEADBEEF, "");
+_Static_assert(le16toh(0xBEEF) == 0xEFBE, "");
+_Static_assert(le32toh(0xDEADBEEF) == 0xEFBEADDE, "");
+_Static_assert(le64toh(0xDEADBEEFDEADBEEF) == 0xEFBEADDEEFBEADDE, "");
+
+#elif BYTE_ORDER == LITTLE_ENDIAN
+
+_Static_assert(htobe16(0xBEEF) == 0xEFBE, "");
+_Static_assert(htobe32(0xDEADBEEF) == 0xEFBEADDE, "");
+_Static_assert(htobe64(0xDEADBEEFDEADBEEF) == 0xEFBEADDEEFBEADDE, "");
+_Static_assert(htole16(0xBEEF) == 0xBEEF, "");
+_Static_assert(htole32(0xDEADBEEF) == 0xDEADBEEF, "");
+_Static_assert(htole64(0xDEADBEEFDEADBEEF) == 0xDEADBEEFDEADBEEF, "");
+_Static_assert(be16toh(0xBEEF) == 0xEFBE, "");
+_Static_assert(be32toh(0xDEADBEEF) == 0xEFBEADDE, "");
+_Static_assert(be64toh(0xDEADBEEFDEADBEEF) == 0xEFBEADDEEFBEADDE, "");
+_Static_assert(le16toh(0xBEEF) == 0xBEEF, "");
+_Static_assert(le32toh(0xDEADBEEF) == 0xDEADBEEF, "");
+_Static_assert(le64toh(0xDEADBEEFDEADBEEF) == 0xDEADBEEFDEADBEEF, "");
+
+#else
+#error "Invalid byte order"
+#endif
