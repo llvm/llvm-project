705925b037877fd9a1678ade3ce4737850bc5d1b
Duran, Alex <alejandro.duran@intel.com>
2026-03-11 15:45:38 -0700

    fix test

--- clang/test/Tooling/clang-linker-wrapper-spirv.cpp
+++ clang/test/Tooling/clang-linker-wrapper-spirv.cpp
@@ -5,9 +5,8 @@
 // RUN: %clangxx -fopenmp -fopenmp-targets=spirv64-intel -nogpulib -o %t %s
 // RUN: llvm-objdump --offloading %t | FileCheck -check-prefix=CHECK %s
 
-// CHECK: [Nested OffloadBinary
-// CHECK: Number of inner images: 1
-// CHECK: spirv64-intel
+// CHECK: nested images   1
+// CHECK:   triple          spirv64-intel
 
 int main(int argc, char** argv) {
   return 0;
