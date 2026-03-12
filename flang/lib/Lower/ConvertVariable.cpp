5ca39658278d527d24e8bc6b96dd4a3780f78be7
Zhen Wang <zhenw@nvidia.com>
2026-03-12 10:51:34 -0700

    restore unecessary formatting change

--- flang/lib/Lower/ConvertVariable.cpp
+++ flang/lib/Lower/ConvertVariable.cpp
@@ -585,14 +585,15 @@ fir::GlobalOp Fortran::lower::defineGlobal(
     if (details && details->init()) {
       auto sym{*details->init()};
       if (sym) // Has a procedure target.
-        createGlobalInitialization(builder, global, [&](fir::FirOpBuilder &b) {
-          Fortran::lower::StatementContext stmtCtx(
-              /*cleanupProhibited=*/true);
-          auto box{Fortran::lower::convertProcedureDesignatorInitialTarget(
-              converter, loc, *sym)};
-          auto castTo{builder.createConvert(loc, symTy, box)};
-          fir::HasValueOp::create(b, loc, castTo);
-        });
+        createGlobalInitialization(
+            builder, global, [&](fir::FirOpBuilder &b) {
+              Fortran::lower::StatementContext stmtCtx(
+                  /*cleanupProhibited=*/true);
+              auto box{Fortran::lower::convertProcedureDesignatorInitialTarget(
+                  converter, loc, *sym)};
+              auto castTo{builder.createConvert(loc, symTy, box)};
+              fir::HasValueOp::create(b, loc, castTo);
+            });
       else { // Has NULL() target.
         createGlobalInitialization(builder, global, [&](fir::FirOpBuilder &b) {
           auto box{fir::factory::createNullBoxProc(b, loc, symTy)};
