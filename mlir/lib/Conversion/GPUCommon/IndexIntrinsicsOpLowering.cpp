745e2d0577f3e095acb35da084c8573d29b4f9d3
Krzysztof Drewniak <Krzysztof.Drewniak@amd.com>
2026-03-12 20:34:16 +0000

    Fix header

--- mlir/lib/Conversion/GPUCommon/IndexIntrinsicsOpLowering.cpp
+++ mlir/lib/Conversion/GPUCommon/IndexIntrinsicsOpLowering.cpp
@@ -1,4 +1,4 @@
-//===- IndexIntrinsicsOpLowering.cpp - GPU Index Op Lowering ------*- C++ -*-===//
+//===- IndexIntrinsicsOpLowering.cpp - GPU Index Op Lowering --------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
