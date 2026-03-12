6815a37c7b2a7bc027206fcf198bfe2b039ba70d
Deric Cheung <cheung.deric@gmail.com>
2026-03-12 15:16:08 -0700

    Remove unnecessary exports

--- clang/test/SemaHLSL/BuiltIns/transpose-errors.hlsl
+++ clang/test/SemaHLSL/BuiltIns/transpose-errors.hlsl
@@ -2,31 +2,31 @@
 
 // Test transpose error cases
 
-export float test_transpose_scalar(float a) {
+float test_transpose_scalar(float a) {
   return transpose(a);
   // expected-error@-1 {{no matching function for call to 'transpose'}}
 }
 // expected-note@*:* {{candidate template ignored}}
 // expected-note@*:* {{candidate template ignored}}
 
-export float3 test_transpose_vector(float3 a) {
+float3 test_transpose_vector(float3 a) {
   return transpose(a);
   // expected-error@-1 {{no matching function for call to 'transpose'}}
 }
 // expected-note@*:* {{candidate template ignored}}
 // expected-note@*:* {{candidate template ignored}}
 
-export void test_transpose_scalar_builtin(float a) {
+void test_transpose_scalar_builtin(float a) {
   __builtin_hlsl_transpose(a);
   // expected-error@-1 {{1st argument must be a matrix type (was 'float')}}
 }
 
-export void test_transpose_vector_builtin(float3 a) {
+void test_transpose_vector_builtin(float3 a) {
   __builtin_hlsl_transpose(a);
   // expected-error@-1 {{1st argument must be a matrix type (was 'float3' (aka 'vector<float, 3>'))}}
 }
 
-export void test_transpose_int_builtin(int a) {
+void test_transpose_int_builtin(int a) {
   __builtin_hlsl_transpose(a);
   // expected-error@-1 {{1st argument must be a matrix type (was 'int')}}
 }
