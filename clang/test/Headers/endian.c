685edcaf147bb4a2185993298b4d054b783b3167
David Tenty <daltenty@ibm.com>
2026-03-12 11:30:05 -0400

    Update 64-bit test pattern

--- clang/test/Headers/endian.c
+++ clang/test/Headers/endian.c
@@ -11,31 +11,31 @@
 
 _Static_assert(htobe16(0xBEEF) == 0xBEEF, "");
 _Static_assert(htobe32(0xDEADBEEF) == 0xDEADBEEF, "");
-_Static_assert(htobe64(0xDEADBEEFDEADBEEF) == 0xDEADBEEFDEADBEEF, "");
+_Static_assert(htobe64(0xDEADBEEFBAADF00D) == 0xDEADBEEFBAADF00D, "");
 _Static_assert(htole16(0xBEEF) == 0xEFBE, "");
 _Static_assert(htole32(0xDEADBEEF) == 0xEFBEADDE, "");
-_Static_assert(htole64(0xDEADBEEFDEADBEEF) == 0xEFBEADDEEFBEADDE, "");
+_Static_assert(htole64(0xDEADBEEFBAADF00D) == 0xDF0ADBAEFBEADDE, "");
 _Static_assert(be16toh(0xBEEF) == 0xBEEF, "");
 _Static_assert(be32toh(0xDEADBEEF) == 0xDEADBEEF, "");
-_Static_assert(be64toh(0xDEADBEEFDEADBEEF) == 0xDEADBEEFDEADBEEF, "");
+_Static_assert(be64toh(0xDEADBEEFBAADF00D) == 0xDEADBEEFBAADF00D, "");
 _Static_assert(le16toh(0xBEEF) == 0xEFBE, "");
 _Static_assert(le32toh(0xDEADBEEF) == 0xEFBEADDE, "");
-_Static_assert(le64toh(0xDEADBEEFDEADBEEF) == 0xEFBEADDEEFBEADDE, "");
+_Static_assert(le64toh(0xDEADBEEFBAADF00D) == 0xDF0ADBAEFBEADDE, "");
 
 #elif BYTE_ORDER == LITTLE_ENDIAN
 
 _Static_assert(htobe16(0xBEEF) == 0xEFBE, "");
 _Static_assert(htobe32(0xDEADBEEF) == 0xEFBEADDE, "");
-_Static_assert(htobe64(0xDEADBEEFDEADBEEF) == 0xEFBEADDEEFBEADDE, "");
+_Static_assert(htobe64(0xDEADBEEFBAADF00D) == 0xDF0ADBAEFBEADDE, "");
 _Static_assert(htole16(0xBEEF) == 0xBEEF, "");
 _Static_assert(htole32(0xDEADBEEF) == 0xDEADBEEF, "");
-_Static_assert(htole64(0xDEADBEEFDEADBEEF) == 0xDEADBEEFDEADBEEF, "");
+_Static_assert(htole64(0xDEADBEEFBAADF00D) == 0xDEADBEEFBAADF00D, "");
 _Static_assert(be16toh(0xBEEF) == 0xEFBE, "");
 _Static_assert(be32toh(0xDEADBEEF) == 0xEFBEADDE, "");
-_Static_assert(be64toh(0xDEADBEEFDEADBEEF) == 0xEFBEADDEEFBEADDE, "");
+_Static_assert(be64toh(0xDEADBEEFBAADF00D) == 0xDF0ADBAEFBEADDE, "");
 _Static_assert(le16toh(0xBEEF) == 0xBEEF, "");
 _Static_assert(le32toh(0xDEADBEEF) == 0xDEADBEEF, "");
-_Static_assert(le64toh(0xDEADBEEFDEADBEEF) == 0xDEADBEEFDEADBEEF, "");
+_Static_assert(le64toh(0xDEADBEEFBAADF00D) == 0xDEADBEEFBAADF00D, "");
 
 #else
 #error "Invalid byte order"
