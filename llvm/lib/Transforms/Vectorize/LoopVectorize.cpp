e80beda6e2558f03f17351f6805e95c8687a82a9
Florian Hahn <flo@fhahn.com>
2026-03-12 21:51:23 +0000

    [VPlan] Account for any-of costs in legacy cost model
    Some VPlan transforms, like vectorizing fmin without fast-math,
    introduce AnyOfs, which have costs assigned in the VPlan-based cost
    model, but not the legacy cost model. Account for their cost like done
    for other similar VPInstrctions, like EVL.
    
    Fixes https://github.com/llvm/llvm-project/issues/185867.
    
    (cherry picked from commit 475cc4fe0b4065775db470bb512c9c9142242e55)

--- llvm/lib/Transforms/Vectorize/LoopVectorize.cpp
+++ llvm/lib/Transforms/Vectorize/LoopVectorize.cpp
@@ -4282,6 +4282,7 @@ VectorizationFactor LoopVectorizationPlanner::selectVectorizationFactor() {
             break;
           }
           case VPInstruction::ExplicitVectorLength:
+          case VPInstruction::AnyOf:
             C += VPI->cost(VF, CostCtx);
             break;
           default:
