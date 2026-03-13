9a001983a1ed6b626dbd42e1bc84c8db09e21e1a
John Harrison <harjohn@google.com>
2026-03-13 10:53:21 -0700

    [lldb-dap] Improve support for variables with anonymous fields and types.
    This improves support for variables with anonymous fields and anonymous types.
    
    * Changed the name of anonymous fields from '<null>' to '<anonymous>', which I think is more descriptive.
    * Adjusts variables to not return an 'evaluateName' for anonymous fields.
    * Adjusted '[raw]' values to be marked as 'internal' which deemphasizes them in the UI.
    
    While working in this area, I also consolidated some helpers that are only used within Variables.cpp.

--- lldb/tools/lldb-dap/JSONUtils.cpp
+++ lldb/tools/lldb-dap/JSONUtils.cpp
@@ -244,7 +244,7 @@ llvm::json::Object CreateEventObject(const llvm::StringRef event_name) {
 
 llvm::StringRef GetNonNullVariableName(lldb::SBValue &v) {
   const llvm::StringRef name = v.GetName();
-  return !name.empty() ? name : "<null>";
+  return !name.empty() ? name : "<anonymous>";
 }
 
 std::string CreateUniqueVariableNameForDisplay(lldb::SBValue &v,
@@ -311,9 +311,11 @@ VariableDescription::VariableDescription(
     }
   }
 
-  lldb::SBStream evaluateStream;
-  val.GetExpressionPath(evaluateStream);
-  evaluate_name = llvm::StringRef(evaluateStream.GetData()).str();
+  if (!llvm::StringRef(val.GetName()).empty()) {
+    lldb::SBStream evaluateStream;
+    val.GetExpressionPath(evaluateStream);
+    evaluate_name = llvm::StringRef(evaluateStream.GetData()).str();
+  }
 }
 
 std::string VariableDescription::GetResult(protocol::EvaluateContext context) {
