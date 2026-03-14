0c02c766cda8a5fd36b11942d63ef9b4bb1dbbcd
hulxv <hulxxv@gmail.com>
2026-03-14 02:17:20 +0200

    missed libc_common

--- libc/shared/math/daddl.h
+++ libc/shared/math/daddl.h
@@ -9,6 +9,7 @@
 #ifndef LLVM_LIBC_SHARED_MATH_DADDL_H
 #define LLVM_LIBC_SHARED_MATH_DADDL_H
 
+#include "shared/libc_common.h"
 #include "src/__support/math/daddl.h"
 
 namespace LIBC_NAMESPACE_DECL {
