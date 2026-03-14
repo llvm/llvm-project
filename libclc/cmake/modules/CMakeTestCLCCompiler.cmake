81e9b2423d705c3616a691319f56a354fee2faa8
Wenju He <wenju.he@intel.com>
2026-03-15 00:31:31 +0100

    Revert "Test that the compiler works for all targets in LIBCLC_TARGETS_TO_BUILD"
    This reverts commit 55ee855f8a62d84ab1c1916205e9cf4b22819651.

--- libclc/cmake/modules/CMakeTestCLCCompiler.cmake
+++ libclc/cmake/modules/CMakeTestCLCCompiler.cmake
@@ -2,8 +2,6 @@ configure_file(${CMAKE_CURRENT_LIST_DIR}/CMakeCLCCompiler.cmake.in
   ${CMAKE_PLATFORM_INFO_DIR}/CMakeCLCCompiler.cmake @ONLY)
 include(${CMAKE_PLATFORM_INFO_DIR}/CMakeCLCCompiler.cmake)
 
-include(AddLibclc)
-
 if(CMAKE_CLC_COMPILER_FORCED)
   set(CMAKE_CLC_COMPILER_WORKS TRUE)
   return()
@@ -16,29 +14,21 @@ file(MAKE_DIRECTORY "${_test_dir}")
 
 message(STATUS "Check for working CLC compiler: ${CMAKE_CLC_COMPILER}")
 
-# Test that the compiler works for all targets in LIBCLC_TARGETS_TO_BUILD
-foreach(_target ${LIBCLC_TARGETS_TO_BUILD})
-  # Convert libclc target to clang triple
-  libclc_target_to_clang_triple(${_target} _clang_triple)
+execute_process(
+  COMMAND "${CMAKE_CLC_COMPILER}" -target spir64-unknown-unknown -x cl -c -flto
+          -o "${_test_out}" "${_test_file}"
+  RESULT_VARIABLE _clc_result
+  ERROR_VARIABLE _clc_error
+)
 
-  execute_process(
-    COMMAND "${CMAKE_CLC_COMPILER}" --target=${_clang_triple} -x cl -c -flto
-            -nostdlib -nostdlibinc -cl-no-stdinc -o "${_test_out}" "${_test_file}"
-    RESULT_VARIABLE _clc_result
-    ERROR_VARIABLE _clc_error
-    OUTPUT_QUIET
-  )
-
-  if(NOT _clc_result EQUAL 0)
-    message(FATAL_ERROR
-      "The CLC compiler\n"
-      "  ${CMAKE_CLC_COMPILER}\n"
-      "is not able to compile a simple OpenCL test program for ${_target}.\n"
-      "Output:\n${_clc_error}")
-  endif()
-
+if(_clc_result EQUAL 0)
+  set(CMAKE_CLC_COMPILER_WORKS TRUE)
+  message(STATUS "Check for working CLC compiler: ${CMAKE_CLC_COMPILER} - works")
   file(REMOVE "${_test_out}")
-endforeach()
-
-set(CMAKE_CLC_COMPILER_WORKS TRUE)
-message(STATUS "Check for working CLC compiler: ${CMAKE_CLC_COMPILER} - works")
+else()
+  message(FATAL_ERROR
+    "The CLC compiler\n"
+    "  ${CMAKE_CLC_COMPILER}\n"
+    "is not able to compile a simple OpenCL test program.\n"
+    "Output:\n${_clc_error}")
+endif()
