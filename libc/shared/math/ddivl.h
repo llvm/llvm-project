fc4fb1f4dadcabeb8e93396ae1c678a58126437f
hulxv <hulxxv@gmail.com>
2026-03-14 02:15:55 +0200

    missed libc_common

--- libc/shared/math/ddivl.h
+++ libc/shared/math/ddivl.h
@@ -9,6 +9,7 @@
 #ifndef LLVM_LIBC_SHARED_MATH_DDIVL_H
 #define LLVM_LIBC_SHARED_MATH_DDIVL_H
 
+#include "shared/libc_common.h"
 #include "src/__support/math/ddivl.h"
 
 namespace LIBC_NAMESPACE_DECL {
