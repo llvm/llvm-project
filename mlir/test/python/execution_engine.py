9bfd619d94fe058600258df7f75bb95dae9d2533
Sam <srcarroll314@gmail.com>
2026-03-15 10:52:32 -0500

    Revert "Support float8_e3m4 and float8_e4m3 in np_to_memref (#186453)"
    This reverts commit 57427f84fe5fdda71aef4be257ed28d7b4f55d05.

--- mlir/test/python/execution_engine.py
+++ mlir/test/python/execution_engine.py
@@ -8,7 +8,7 @@ from mlir.execution_engine import *
 from mlir.runtime import *
 
 try:
-    from ml_dtypes import bfloat16, float8_e5m2, float8_e3m4, float8_e4m3
+    from ml_dtypes import bfloat16, float8_e5m2
 
     HAS_ML_DTYPES = True
 except ModuleNotFoundError:
@@ -621,90 +621,6 @@ if HAS_ML_DTYPES:
     run(testF8E5M2Memref)
 else:
     log("TEST: testF8E5M2Memref")
-
-
-# Test f8E3M4 memrefs
-# CHECK-LABEL: TEST: testF8E3M4Memref
-def testF8E3M4Memref():
-    with Context():
-        module = Module.parse(
-            """
-    module  {
-      func.func @main(%arg0: memref<1xf8E3M4>,
-                      %arg1: memref<1xf8E3M4>) attributes { llvm.emit_c_interface } {
-        %0 = arith.constant 0 : index
-        %1 = memref.load %arg0[%0] : memref<1xf8E3M4>
-        memref.store %1, %arg1[%0] : memref<1xf8E3M4>
-        return
-      }
-    } """
-        )
-
-        arg1 = np.array([0.5]).astype(float8_e3m4)
-        arg2 = np.array([0.0]).astype(float8_e3m4)
-
-        arg1_memref_ptr = ctypes.pointer(
-            ctypes.pointer(get_ranked_memref_descriptor(arg1))
-        )
-        arg2_memref_ptr = ctypes.pointer(
-            ctypes.pointer(get_ranked_memref_descriptor(arg2))
-        )
-
-        execution_engine = ExecutionEngine(lowerToLLVM(module))
-        execution_engine.invoke("main", arg1_memref_ptr, arg2_memref_ptr)
-
-        # test to-numpy utility
-        x = ranked_memref_to_numpy(arg2_memref_ptr[0])
-        assert len(x) == 1
-        assert x[0] == 0.5
-
-
-if HAS_ML_DTYPES:
-    run(testF8E3M4Memref)
-else:
-    log("TEST: testF8E3M4Memref")
-
-
-# Test f8E4M3 memrefs
-# CHECK-LABEL: TEST: testF8E4M3Memref
-def testF8E4M3Memref():
-    with Context():
-        module = Module.parse(
-            """
-    module  {
-      func.func @main(%arg0: memref<1xf8E4M3>,
-                      %arg1: memref<1xf8E4M3>) attributes { llvm.emit_c_interface } {
-        %0 = arith.constant 0 : index
-        %1 = memref.load %arg0[%0] : memref<1xf8E4M3>
-        memref.store %1, %arg1[%0] : memref<1xf8E4M3>
-        return
-      }
-    } """
-        )
-
-        arg1 = np.array([0.5]).astype(float8_e4m3)
-        arg2 = np.array([0.0]).astype(float8_e4m3)
-
-        arg1_memref_ptr = ctypes.pointer(
-            ctypes.pointer(get_ranked_memref_descriptor(arg1))
-        )
-        arg2_memref_ptr = ctypes.pointer(
-            ctypes.pointer(get_ranked_memref_descriptor(arg2))
-        )
-
-        execution_engine = ExecutionEngine(lowerToLLVM(module))
-        execution_engine.invoke("main", arg1_memref_ptr, arg2_memref_ptr)
-
-        # test to-numpy utility
-        x = ranked_memref_to_numpy(arg2_memref_ptr[0])
-        assert len(x) == 1
-        assert x[0] == 0.5
-
-
-if HAS_ML_DTYPES:
-    run(testF8E4M3Memref)
-else:
-    log("TEST: testF8E4M3Memref")
 
 
 #  Test addition of two 2d_memref
