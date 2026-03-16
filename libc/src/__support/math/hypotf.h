f55b7ddd371bccd45ff24f3a57b510c69c2190e7
Tue Ly <lntue.h@gmail.com>
2026-03-16 06:03:21 +0000

    Fix rounding condition and add another quick check for exact cases.

--- libc/src/__support/math/hypotf.h
+++ libc/src/__support/math/hypotf.h
@@ -30,9 +30,6 @@ LIBC_INLINE float hypotf(float x, float y) {
   uint32_t x_a = FPBits(x).uintval() & 0x7fff'ffff;
   uint32_t y_a = FPBits(y).uintval() & 0x7fff'ffff;
 
-  float x_abs = FPBits(x_a).get_val();
-  float y_abs = FPBits(y_a).get_val();
-
   // Note: replacing `x_a >= FPBits::EXP_MASK` with `x_bits.is_inf_or_nan()`
   // generates extra exponent bit masking instructions on x86-64.
   if (LIBC_UNLIKELY(x_a >= FPBits::EXP_MASK || y_a >= FPBits::EXP_MASK)) {
@@ -48,53 +45,76 @@ LIBC_INLINE float hypotf(float x, float y) {
     return x + y;
   }
 
-  bool x_abs_larger = y_abs < x_abs;
+  double xd = static_cast<double>(x);
+  double yd = static_cast<double>(y);
 
-  float a = x_abs_larger ? x_abs : y_abs;
-  float b = x_abs_larger ? y_abs : x_abs;
+  // x^2 and y^2 are exact in double precision.
+  double x_sq = xd * xd;
 
-  double ad = static_cast<double>(a);
-  double bd = static_cast<double>(b);
-
-  // These squares are exact.
-  double a_sq = ad * ad;
-
-  DoubleDouble sum_sq;
+  double sum_sq;
 #ifdef LIBC_TARGET_CPU_HAS_FMA_DOUBLE
-  sum_sq.hi = fputil::multiply_add(bd, bd, a_sq);
-  sum_sq.lo = fputil::multiply_add(bd, bd, a_sq - sum_sq.hi);
+  sum_sq = fputil::multiply_add(yd, yd, x_sq);
 #else
-  double b_sq = bd * bd;
-  sum_sq = fputil::exact_add(a_sq, b_sq);
+  double y_sq = yd * yd;
+  sum_sq = x_sq + y_sq;
 #endif
 
   // Take sqrt in double precision.
-  DoubleBits result(fputil::sqrt<double>(sum_sq.hi));
+  DoubleBits result(fputil::sqrt<double>(sum_sq));
+  double r = result.get_val();
+  float r_f = static_cast<float>(r);
 
-  // We only need to update the result if the sum of squares exceed double
-  // precision.
-  if (LIBC_UNLIKELY(sum_sq.lo != 0.0)) {
-    double r_d = result.get_val();
+  // If any of the sticky bits of the result are non-zero, except the LSB, then
+  // the rounded result is correct.
+  uint64_t r_u = result.uintval();
+  uint32_t r_u32 = static_cast<uint32_t>(r_u);
 
-    // Perform rounding correction.
+  if (LIBC_UNLIKELY(((r_u32 + 1) & 0x0FFF'FFFE) == 0)) {
+    // Almost all the sticky bits of the results are non-zero, extra checks are
+    // needed to make sure rounding is correct.
+
+    // Perform a quick cheeck to see if the result rounded to float is already
+    // correct.  Majority of hard-to-round cases fall in this case.  If not, we
+    // will need to perform more expensive computations to get the correct error
+    // terms.
+    double r_d = static_cast<double>(r_f);
+    bool y_a_smaller = y_a < x_a;
+
 #ifdef LIBC_TARGET_CPU_HAS_FMA_DOUBLE
-    double err = sum_sq.lo - fputil::multiply_add(r_d, r_d, -sum_sq.hi);
+    // Compute the missing y_sq variable for FMA code path.
+    double y_sq = yd * yd;
+#endif // LIBC_TARGET_CPU_HAS_FMA_DOUBLE
+
+    double a = y_a_smaller ? x_sq : y_sq;
+    double b = y_a_smaller ? y_sq : x_sq;
+    double e = b - fputil::multiply_add(r_d, r_d, -a);
+    if (e == 0.0)
+      return r_f;
+
+    // Rounding correction is needed.
+    // The errors come from two parts:
+    // - rounding errors from sqrt(sum_sq) -> D(sum_sq)
+    // - rounding errors from x_sq + y_sq -> sum_sq
+    // We use FastTwoSum algorithm to compute those errors and then combine.
+#ifdef LIBC_TARGET_CPU_HAS_FMA_DOUBLE
+    double sum_sq_lo = (b - (sum_sq - a));
+    double err = sum_sq_lo - fputil::multiply_add(r, r, -sum_sq);
 #else
-    fputil::DoubleDouble r_sq = fputil::exact_mult(r_d, r_d);
-    double err = (sum_sq.hi - r_sq.hi) + (sum_sq.lo - r_sq.lo);
+    fputil::DoubleDouble r_sq = fputil::exact_mult(r, r);
+    double sum_sq_lo = (b - (sum_sq - a));
+    double err = (sum_sq - r_sq.hi) + (sum_sq_lo - r_sq.lo);
 #endif
 
-    uint64_t r_u = result.uintval();
     if (err > 0) {
       r_u |= 1;
-    } else if ((err < 0) && (r_u & 1) == 0) {
+    } else if ((err < 0) && ((r_u32 & 0x0FFF'FFFF) == 0)) {
       r_u -= 1;
     }
 
     return static_cast<float>(DoubleBits(r_u).get_val());
   }
 
-  return static_cast<float>(result.get_val());
+  return r_f;
 }
 
 } // namespace math
