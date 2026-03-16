c3e67dc4d084a4623f9ceadfaca77510c629bf72
Ron Lieberman <ron.lieberman@amd.com>
2026-03-16 15:11:37 -0500

    Revert "[mlir][llvmir][OpenMP] Translate affinity clause in task construct to llvmir (#182223)"
    This reverts commit 2ad51ffbfae77ecb67e64ed8e7e833e54285f4c2.

--- mlir/lib/Conversion/OpenMPToLLVM/OpenMPToLLVM.cpp
+++ mlir/lib/Conversion/OpenMPToLLVM/OpenMPToLLVM.cpp
@@ -154,9 +154,6 @@ void mlir::populateOpenMPToLLVMConversionPatterns(LLVMTypeConverter &converter,
   // discarded on lowering to LLVM-IR from the OpenMP dialect.
   converter.addConversion(
       [&](omp::MapBoundsType type) -> Type { return type; });
-  converter.addConversion(
-      [&](omp::AffinityEntryType type) -> Type { return type; });
-  converter.addConversion([&](omp::IteratedType type) -> Type { return type; });
 
   // Add conversions for all OpenMP operations.
   addOpenMPOpConversions<
