cd845123cfbd17ce4debb1458e01f65ba3b6b0ba
Jan Svoboda <jan_svoboda@apple.com>
2026-03-17 12:53:15 -0700

    Remove symlink test case

--- clang/test/Modules/relative-module-cache.c
+++ clang/test/Modules/relative-module-cache.c
@@ -32,25 +32,12 @@
                // expected-remark{{importing module 'b' into 'c'}} \
                // expected-remark{{importing module 'a' into 'b'}}
 
-// Module cache via symlink.
-// RUN: ln -s %t/cache %t/cache-symlink
-// RUN: cd %t/tmp && %clang_cc1 -fmodules -fimplicit-module-maps -fsyntax-only -I %t %t/tu4.c -Rmodule-build -Rmodule-import -verify -fmodules-cache-path=%t/cache-symlink
+// Module cache path absolute again.
+// RUN: cd %t/tmp && %clang_cc1 -fmodules -fimplicit-module-maps -fsyntax-only -I %t %t/tu4.c -Rmodule-build -Rmodule-import -verify -fmodules-cache-path=%t/cache
 //--- tu4.c
 #include "e.h" // expected-remark{{building module 'e'}}           \
                // expected-remark{{finished building module 'e'}}  \
                // expected-remark{{importing module 'e'}}          \
-               // expected-remark{{importing module 'd' into 'e'}} \
-               // expected-remark{{importing module 'c' into 'd'}} \
-               // expected-remark{{importing module 'b' into 'c'}} \
-               // expected-remark{{importing module 'a' into 'b'}}
-
-// Module cache path absolute again.
-// RUN: cd %t/tmp && %clang_cc1 -fmodules -fimplicit-module-maps -fsyntax-only -I %t %t/tu5.c -Rmodule-build -Rmodule-import -verify -fmodules-cache-path=%t/cache
-//--- tu5.c
-#include "f.h" // expected-remark{{building module 'f'}}           \
-               // expected-remark{{finished building module 'f'}}  \
-               // expected-remark{{importing module 'f'}}          \
-               // expected-remark{{importing module 'e' into 'f'}} \
                // expected-remark{{importing module 'd' into 'e'}} \
                // expected-remark{{importing module 'c' into 'd'}} \
                // expected-remark{{importing module 'b' into 'c'}} \
@@ -62,7 +49,6 @@ module b { header "b.h" }
 module c { header "c.h" }
 module d { header "d.h" }
 module e { header "e.h" }
-module f { header "f.h" }
 //--- a.h
 //--- b.h
 #include "a.h"
@@ -72,5 +58,3 @@ module f { header "f.h" }
 #include "c.h"
 //--- e.h
 #include "d.h"
-//--- f.h
-#include "e.h"
