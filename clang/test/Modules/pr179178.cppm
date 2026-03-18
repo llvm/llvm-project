llvm/llvm-project

commit 87d73f74713039cacf9d174a9d74de0db8eccb8b

[Hexagon] Fix encoding of packets with fixups followed by alignment (#179168)
When a packet containing extended immediates and new-value compare-jump
instructions with fixups was followed by a .p2align directive, we would
incorrectly add nops to the packet. After reshuffling, the fixup offsets
would become invalid, causing corrupted encodings.

Fixes round-trip assembly for patterns like:

    {
      r18 = ##65536
      if (!cmp.gtu(r1,r18.new)) jump:t .L1
    }
    .p2align 4

llvm/lib/Target/Hexagon/MCTargetDesc/HexagonAsmBackend.cpp
@@ -596,6 +596,13 @@ public:
               auto &RF = *Frags[K];
               MCInst Inst = RF.getInst();
 
+              // Don't add nops to packets that have fixups, as reshuffling can
+              // invalidate fixup offsets.
+              if (!RF.getVarFixups().empty()) {
+                Size = 0;
+                break;
+              }
+
               const bool WouldTraverseLabel = llvm::any_of(
                   Asm->symbols(), [&RF, &Inst, Asm = Asm](MCSymbol const &sym) {
                     uint64_t Offset = 0;

llvm/test/MC/Hexagon/newvalue_jump_ext_imm.s
@@ -1,0 +1,49 @@
+# RUN: llvm-mc -triple=hexagon -filetype=obj %s | llvm-objdump -d - | FileCheck %s
+# Test that packets with extended immediates and new-value compare-jumps
+# followed by alignment directives are encoded and decoded correctly.
+
+# CHECK-LABEL: <test1>:
+# CHECK: immext(#0x10000)
+# CHECK-NEXT: r18 = ##0x10000
+# CHECK-NEXT: if (!cmp.gtu(r1,r18.new)) jump:t
+test1:
+  .p2align 4
+  {
+    r18 = ##65536
+    if (!cmp.gtu(r1,r18.new)) jump:t .L1
+  }
+  .p2align 4
+.L1:
+  nop
+
+# CHECK-LABEL: <test2>:
+# CHECK: immext(#0x20000)
+# CHECK-NEXT: r19 = ##0x20000
+# CHECK-NEXT: if (cmp.eq(r19.new,r2)) jump:nt
+test2:
+  .p2align 4
+  {
+    r19 = ##131072
+    if (cmp.eq(r19.new,r2)) jump:nt .L2
+  }
+  .p2align 4
+.L2:
+  nop
+
+# CHECK-LABEL: <test3>:
+# CHECK: allocframe(#0x10)
+# CHECK-NEXT: memd(r29+#0x0) = r19:18
+# CHECK: immext(#0x10000)
+# CHECK-NEXT: r18 = ##0x10000
+# CHECK-NEXT: if (!cmp.gtu(r1,r18.new)) jump:t
+test3:
+  .p2align 4
+  allocframe(#16)
+  memd(r29+#0) = r19:18
+  {
+    r18 = ##65536
+    if (!cmp.gtu(r1,r18.new)) jump:t .L3
+  }
+  .p2align 4
+.L3:
+  nop

commit 05d6b4a863ab5942dd34d43a9f31663b6cdf6a7c

[Clang][Modules] Add regression test for #179178
This patch adds a regression test for a clang header modules failure we
saw after #179178 landed.

I'm too familiar with the norms for clang modules tests, so feel free to
nit-pick.

I've verified that this test passes at HEAD and fails with #179168
applied (i.e., the revert reverted).

clang/test/Modules/pr179178.cppm
@@ -1,0 +1,120 @@
+// RUN: rm -rf %t
+// RUN: mkdir %t
+// RUN: split-file %s %t
+// RUN: cd %t
+//
+// RUN: %clang -fmodule-name=//b -Xclang=-fno-cxx-modules -Xclang=-fmodule-map-file-home-is-cwd -xc++ -Xclang=-emit-module -fmodules -fno-implicit-modules -fno-implicit-module-maps -c b.cppmap -o b.pic.pcm
+// RUN: %clang -fmodule-name=//c -Xclang=-fno-cxx-modules -Xclang=-fmodule-map-file-home-is-cwd -xc++ -Xclang=-emit-module -fmodules -fno-implicit-modules -fno-implicit-module-maps -Xclang=-fmodule-file=b.pic.pcm -c c.cppmap -o c.pic.pcm
+// RUN: %clang -fmodule-name=//d -Xclang=-fno-cxx-modules -Xclang=-fmodule-map-file-home-is-cwd -xc++ -Xclang=-emit-module -fmodules -fno-implicit-modules -fno-implicit-module-maps -Xclang=-fmodule-file=c.pic.pcm -c d.cppmap -o d.pic.pcm
+// RUN: %clang -fmodule-name=//e -Xclang=-fno-cxx-modules -Xclang=-fmodule-map-file-home-is-cwd -xc++ -Xclang=-emit-module -fmodules -fno-implicit-modules -fno-implicit-module-maps -Xclang=-fmodule-file=d.pic.pcm -c e.cppmap -o e.pic.pcm
+// RUN: %clang -fmodule-name=//f -Xclang=-fno-cxx-modules -Xclang=-fmodule-map-file-home-is-cwd -xc++ -Xclang=-emit-module -fmodules -fno-implicit-modules -fno-implicit-module-maps -Xclang=-fmodule-file=e.pic.pcm -c f.cppmap -o f.pic.pcm
+// RUN: %clang -fmodule-name=//g -Xclang=-fno-cxx-modules -Xclang=-fmodule-map-file-home-is-cwd -xc++ -Xclang=-emit-module -fmodules -fno-implicit-modules -fno-implicit-module-maps -Xclang=-fmodule-file=f.pic.pcm -c g.cppmap -o g.pic.pcm
+// RUN: %clang -fmodule-name=//h -Xclang=-fno-cxx-modules -Xclang=-fmodule-map-file-home-is-cwd -xc++ -Xclang=-emit-module -fmodules -fno-implicit-modules -fno-implicit-module-maps -Xclang=-fmodule-file=g.pic.pcm -c h.cppmap -o h.pic.pcm
+// RUN: %clang -fmodule-name=//i -Xclang=-fno-cxx-modules -Xclang=-fmodule-map-file-home-is-cwd -xc++ -Xclang=-emit-module -fmodules -fno-implicit-modules -fno-implicit-module-maps -Xclang=-fmodule-file=h.pic.pcm -c i.cppmap -o i.pic.pcm
+// RUN: %clang -fmodule-name=//j -Xclang=-fno-cxx-modules -Xclang=-fmodule-map-file-home-is-cwd -xc++ -Xclang=-emit-module -fmodules -fno-implicit-modules -fno-implicit-module-maps -Xclang=-fmodule-file=i.pic.pcm -c j.cppmap -o j.pic.pcm
+// RUN: %clang -fmodule-name=//k -Xclang=-fno-cxx-modules -Xclang=-fmodule-map-file-home-is-cwd -xc++ -Xclang=-emit-module -fmodules -fno-implicit-modules -fno-implicit-module-maps -c k.cppmap -o k.pic.pcm
+// RUN: %clang -fmodule-name=//l -Xclang=-fno-cxx-modules -Xclang=-fmodule-map-file-home-is-cwd -xc++ -Xclang=-emit-module -fmodules -fno-implicit-modules -fno-implicit-module-maps -Xclang=-fmodule-file=j.pic.pcm -c l.cppmap -o l.pic.pcm
+// RUN: %clang -fmodule-name=//m -Xclang=-fno-cxx-modules -Xclang=-fmodule-map-file-home-is-cwd -xc++ -Xclang=-emit-module -fmodules -fno-implicit-modules -fno-implicit-module-maps -Xclang=-fmodule-file=k.pic.pcm -c m.cppmap -o m.pic.pcm
+// RUN: %clang -fmodule-name=//n -Xclang=-fno-cxx-modules -Xclang=-fmodule-map-file-home-is-cwd -xc++ -Xclang=-emit-module -fmodules -fno-implicit-modules -fno-implicit-module-maps -Xclang=-fmodule-file=l.pic.pcm -Xclang=-fmodule-file=m.pic.pcm -c n.cppmap -o n.pic.pcm
+// RUN: %clang -fmodule-name=//o -Xclang=-fno-cxx-modules -Xclang=-fmodule-map-file-home-is-cwd -xc++ -Xclang=-emit-module -fmodules -fno-implicit-modules -fno-implicit-module-maps -Xclang=-fmodule-file=n.pic.pcm -c o.cppmap -o o.pic.pcm
+// RUN: %clang -Xclang=-fno-cxx-modules -Xclang=-fmodule-map-file-home-is-cwd -fmodules -fno-implicit-modules -fno-implicit-module-maps -Xclang=-fmodule-file=o.pic.pcm -c a.cc -o a.o
+
+//--- a.cc
+#include "k.h"
+namespace base {
+namespace internal {}  
+}  
+REGISTER_MODULE_INITIALIZER(, );
+
+
+//--- b.cppmap
+module "//b" {
+    header "b.h"
+}
+
+
+//--- b.h
+namespace base {}  
+
+
+//--- c.cppmap
+module "//c" {
+}
+
+
+//--- d.cppmap
+module "//d" {
+}
+
+
+//--- e.cppmap
+module "//e" {
+}
+
+
+//--- f.cppmap
+module "//f" {
+}
+
+
+//--- g.cppmap
+module "//g" {
+}
+
+
+//--- h.cppmap
+module "//h" {
+}
+
+
+//--- i.cppmap
+module "//i" {
+}
+
+
+//--- j.cppmap
+module "//j" {
+}
+
+
+//--- k.cppmap
+module "//k" {
+    header "k.h"
+}
+
+
+//--- k.h
+namespace base {
+namespace internal {
+struct LiteralTag ;
+}  
+}  
+class FooInitializer ;
+#define REGISTER_INITIALIZER(type, name, body)              void foo_init__() ;   FooInitializer initializer_name( base::internal::LiteralTag\
+      foo_init__)
+#define REGISTER_MODULE_INITIALIZER(name, body) REGISTER_INITIALIZER(, , )
+
+
+//--- l.cppmap
+module "//l" {
+}
+
+
+//--- m.cppmap
+module "//m" {
+}
+
+
+//--- n.cppmap
+module "//n" {
+    header "n.h"
+}
+
+
+//--- n.h
+namespace base {}  
+
+
+//--- o.cppmap
+module "//o" {
+}