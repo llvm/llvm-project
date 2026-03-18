68361cc0883c34f8dc0fa1d5764417efc23806d4
Nikita Popov <npopov@redhat.com>
2026-03-18 12:13:50 +0100

    Work on KnownBits instead

--- llvm/lib/Target/X86/X86ISelLowering.cpp
+++ llvm/lib/Target/X86/X86ISelLowering.cpp
@@ -45385,21 +45385,22 @@ bool X86TargetLowering::SimplifyDemandedBitsForTargetNode(
   }
   case X86ISD::CMOV: {
     KnownBits Known2;
-    SDValue FalseOp = Op.getOperand(0);
-    SDValue TrueOp = Op.getOperand(1);
-    if (SimplifyDemandedBits(TrueOp, OriginalDemandedBits, OriginalDemandedElts,
-                             Known2, TLO, Depth + 1))
+    if (SimplifyDemandedBits(Op.getOperand(1), OriginalDemandedBits,
+                             OriginalDemandedElts, Known2, TLO, Depth + 1))
       return true;
-    if (SimplifyDemandedBits(FalseOp, OriginalDemandedBits,
+    if (SimplifyDemandedBits(Op.getOperand(0), OriginalDemandedBits,
                              OriginalDemandedElts, Known, TLO, Depth + 1))
       return true;
 
     // Check whether we can fold away a cond ? 0 : 1 cmov after shrinking
     // operands based on demanded bits.
-    ConstantSDNode *TrueC = dyn_cast<ConstantSDNode>(TrueOp);
-    ConstantSDNode *FalseC = dyn_cast<ConstantSDNode>(FalseOp);
-    if (TrueC && FalseC && FalseC->getAPIntValue().isZero() &&
-        (TrueC->getAPIntValue() & OriginalDemandedBits).isOne()) {
+    KnownBits KnownFalseVal =
+        Known & KnownBits::makeConstant(OriginalDemandedBits);
+    KnownBits KnownTrueVal =
+        Known2 & KnownBits::makeConstant(OriginalDemandedBits);
+    if (KnownFalseVal.isZero() && KnownTrueVal.isConstant() &&
+        KnownTrueVal.getConstant().isOne() &&
+        Op.getValueType().isScalarInteger()) {
       SDLoc DL(Op);
       X86::CondCode CC = (X86::CondCode)Op.getConstantOperandVal(2);
       SDValue SetCC = getSETCC(CC, Op.getOperand(3), DL, TLO.DAG);
