18ea8e95e162186f99d5f8c56d1cdd3974a46431
Raghu Maddhipatla <Raghu.Maddhipatla@amd.com>
2026-03-18 00:14:58 -0500

    Fix buildbot errors for the test program

--- flang/test/Lower/OpenMP/omp-declarative-allocate-align.f90
+++ flang/test/Lower/OpenMP/omp-declarative-allocate-align.f90
@@ -1,22 +1,21 @@
 ! This test checks lowering of OpenMP allocate Directive with align and allocator
 ! clauses to LLVM IR. Verifies code generation for:
 !   - align(16) only (null allocator)
-!   - allocator(omp_default_mem_alloc) only (no align)
-!   - align(64) allocator(omp_cgroup_mem_alloc) (both clauses, array variable)
+!   - allocator(1) only (no align)
+!   - align(64) allocator(6) (both clauses, array variable)
 !   - align(32) allocator(3) (both clauses, multiple variables)
 
 ! RUN: %flang_fc1 -emit-llvm %openmp_flags -fopenmp-version=51 %s -o - 2>&1 | FileCheck %s
 
 program main
-  use omp_lib
   integer :: x, y
   integer :: z(10)
   character c
-  real(kind = 16) :: r
-  complex cmplx
+  real :: r
+  complex :: cmplx
   !$omp allocate(x) align(16)
-  !$omp allocate(y) allocator(omp_default_mem_alloc)
-  !$omp allocate(z) align(64) allocator(omp_cgroup_mem_alloc)
+  !$omp allocate(y) allocator(1)
+  !$omp allocate(z) align(64) allocator(6)
   !$omp allocate(c, r, cmplx) align(32) allocator(3)
   x = 1
   y = 2
@@ -24,12 +23,11 @@ program main
   print *, "z : ", z
 end program
 
-! CHECK: define void @_QQmain()
 ! CHECK: call i32 @__kmpc_global_thread_num(
 
 ! CHECK: call ptr @__kmpc_aligned_alloc(i32 {{.*}}, i64 16, i64 {{.*}}, ptr null)
-! CHECK: call ptr @__kmpc_alloc(i32 {{.*}}, i64 {{.*}}, ptr inttoptr (i64 1 to ptr))
-! CHECK: call ptr @__kmpc_aligned_alloc(i32 {{.*}}, i64 64, i64 {{.*}}, ptr inttoptr (i64 6 to ptr))
+! CHECK: call ptr @__kmpc_alloc(i32 {{.*}}, i64 {{.*}}, ptr inttoptr (i32 1 to ptr))
+! CHECK: call ptr @__kmpc_aligned_alloc(i32 {{.*}}, i64 64, i64 {{.*}}, ptr inttoptr (i32 6 to ptr))
 ! CHECK: call ptr @__kmpc_aligned_alloc(i32 {{.*}}, i64 32, i64 {{.*}}, ptr inttoptr (i32 3 to ptr))
 ! CHECK: call ptr @__kmpc_aligned_alloc(i32 {{.*}}, i64 32, i64 {{.*}}, ptr inttoptr (i32 3 to ptr))
 ! CHECK: call ptr @__kmpc_aligned_alloc(i32 {{.*}}, i64 32, i64 {{.*}}, ptr inttoptr (i32 3 to ptr))
@@ -37,8 +35,8 @@ end program
 ! CHECK: call void @__kmpc_free(i32 {{.*}}, ptr {{.*}}, ptr inttoptr (i32 3 to ptr))
 ! CHECK: call void @__kmpc_free(i32 {{.*}}, ptr {{.*}}, ptr inttoptr (i32 3 to ptr))
 ! CHECK: call void @__kmpc_free(i32 {{.*}}, ptr {{.*}}, ptr inttoptr (i32 3 to ptr))
-! CHECK: call void @__kmpc_free(i32 {{.*}}, ptr {{.*}}, ptr inttoptr (i64 6 to ptr))
-! CHECK: call void @__kmpc_free(i32 {{.*}}, ptr {{.*}}, ptr inttoptr (i64 1 to ptr))
+! CHECK: call void @__kmpc_free(i32 {{.*}}, ptr {{.*}}, ptr inttoptr (i32 6 to ptr))
+! CHECK: call void @__kmpc_free(i32 {{.*}}, ptr {{.*}}, ptr inttoptr (i32 1 to ptr))
 ! CHECK: call void @__kmpc_free(i32 {{.*}}, ptr {{.*}}, ptr null)
 ! CHECK: ret void
 
