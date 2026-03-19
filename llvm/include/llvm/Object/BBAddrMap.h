c893ebb861789a8af3d253dd90f8246b737c2876
Haohai Wen <haohai.wen@intel.com>
2026-03-19 22:58:35 +0800

    Update header description to follow coding standard.

--- llvm/include/llvm/Object/BBAddrMap.h
+++ llvm/include/llvm/Object/BBAddrMap.h
@@ -1,13 +1,14 @@
-//===- BBAddrMap.h - Basic-block address map types -------------*- C++ -*-===//
+//===----------------------------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
-//
-// This file declares common types and utilities for basic-block address maps.
-//
+///
+/// \file
+/// This file declares common types and utilities for basic-block address maps.
+///
 //===----------------------------------------------------------------------===//
 
 #ifndef LLVM_OBJECT_BBADDRMAP_H
