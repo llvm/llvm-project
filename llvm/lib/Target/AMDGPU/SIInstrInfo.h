53809c1d1fb434a5d94979adc58c62e3e01810dd
Sam Elliott <aelliott@qti.qualcomm.com>
2026-03-19 13:33:02 -0700

    Introduce typedef for clarity

--- llvm/lib/Target/AMDGPU/SIInstrInfo.h
+++ llvm/lib/Target/AMDGPU/SIInstrInfo.h
@@ -1828,7 +1828,7 @@ namespace AMDGPU {
 } // end namespace AMDGPU
 
 namespace AMDGPU {
-enum AsmComments : uint8_t {
+enum AsmComments : MachineInstr::AsmPrinterFlagTy {
   // For sgpr to vgpr spill instructions
   SGPR_SPILL = MachineInstr::TAsmComments
 };
