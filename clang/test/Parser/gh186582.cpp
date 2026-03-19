f48ac79ef45ef4aec891288780f634fe1b4d6722
Serosh-commits <janmejayapanda400@gmail.com>
2026-03-20 03:33:32 +0530

    ci fix

--- clang/test/Parser/gh186582.cpp
+++ clang/test/Parser/gh186582.cpp
@@ -1,6 +1,7 @@
-// RUN: %clang_cc1 -fsyntax-only -std=c++23 -verify %s
-
-a(   ::template operator // expected-error {{a type specifier is required for all declarations}} \
+// RUN: %clang_cc1 -fsyntax-only -verify %s
+a(   ::template operator // expected-error 2 {{expected a type}} \
+                         // expected-error {{a type specifier is required for all declarations}} \
                          // expected-error {{expected ';' after top level declarator}}
-// expected-error@* {{expected a type}}
-// expected-error@* {{expected unqualified-id}}
+
+
+
