d250935490e2ec553f3dfa2def12e67fa25b1297
Deric Cheung <cheung.deric@gmail.com>
2026-03-19 18:20:59 -0700

    Remove file name from headers

--- clang/utils/TableGen/HLSLEmitter.cpp
+++ clang/utils/TableGen/HLSLEmitter.cpp
@@ -1,4 +1,4 @@
-//===--- HLSLEmitter.cpp - HLSL intrinsic header generator ----------------===//
+//===----------------------------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
