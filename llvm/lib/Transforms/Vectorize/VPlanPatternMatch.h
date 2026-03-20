3c470df3dac0e5b86725d9c1c1d8c7d93ca99b1c
Benjamin Maxwell <benjamin.maxwell@arm.com>
2026-03-20 09:29:28 +0000

    [LV] Simplify `matchExtendedReductionOperand()` (NFCI) (#185821)
    This updates `matchExtendedReductionOperand` so the simple case of
    `UpdateR(PrevValue, ext(...))` is matched first as an early exit. The
    binop matching is then flattened to remove the extra layer of the
    `MatchExtends` lambda.

--- llvm/lib/Transforms/Vectorize/VPlanPatternMatch.h
+++ llvm/lib/Transforms/Vectorize/VPlanPatternMatch.h
@@ -593,6 +593,10 @@ m_ZExtOrSExt(const Op0_t &Op0) {
   return m_CombineOr(m_ZExt(Op0), m_SExt(Op0));
 }
 
+template <typename Op0_t> inline auto m_AnyExtend(const Op0_t &Op0) {
+  return m_CombineOr(m_ZExtOrSExt(Op0), m_FPExt(Op0));
+}
+
 template <typename Op0_t>
 inline match_combine_or<AllRecipe_match<Instruction::ZExt, Op0_t>, Op0_t>
 m_ZExtOrSelf(const Op0_t &Op0) {
