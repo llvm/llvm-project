36912ebafb16fc2838ac6b4262b785e67a493be4
Igor Wodiany <igor.wodiany@amd.com>
2026-03-20 16:08:58 +0000

    [AMDGPU] Align GlobalISel with SelectionDAG for f16 to i1/i8 saturated conversions
    GlobaISel now also saturates i1 and i8 to f16 conversion at i16
    where available. This also causes two unfiform tests: i1_f16 and i8_f16
    to use vector instructions, intead of SALU instructions. This is
    potentially suboptimal but it makes it consistent with ISel and
    has been already highlighted in XYZABC.

--- llvm/lib/Target/AMDGPU/AMDGPULegalizerInfo.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPULegalizerInfo.cpp
@@ -1208,9 +1208,12 @@ AMDGPULegalizerInfo::AMDGPULegalizerInfo(const GCNSubtarget &ST_,
   // clang-format off
   auto &FPToISat = getActionDefinitionsBuilder({G_FPTOSI_SAT, G_FPTOUI_SAT})
     .legalFor({{S32, S32}, {S32, S64}})
+    .legalFor(ST.has16BitInsts(),{{S16, S16}})
     .narrowScalarFor({{S64, S16}}, changeTo(0, S32));
+
+  // If available, widen i1 and i8 to i16, intead of i32 so v_cvt_i16/u16_f16 can be used.
   if (ST.has16BitInsts())
-    FPToISat.legalFor({{S16, S16}});
+    FPToISat.widenScalarFor({{S1, S16}, {S8, S16}}, changeTo(0, S16));
 
   FPToISat.minScalar(1, S32);
   FPToISat.minScalar(0, S32)
