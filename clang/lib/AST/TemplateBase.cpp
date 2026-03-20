257c65605055e145638aeb851560ef4f4695762b
Matheus Izvekov <mizvekov@gmail.com>
2026-03-20 14:06:39 -0300

    [clang] fix #187352 breakage on 32-bit platforms
    Fixes breakage reported here: https://github.com/llvm/llvm-project/pull/187352#issuecomment-4096403838

--- clang/lib/AST/TemplateBase.cpp
+++ clang/lib/AST/TemplateBase.cpp
@@ -750,8 +750,8 @@ clang::TemplateArgumentLocInfo::TemplateArgumentLocInfo(
 clang::TemplateArgumentLocInfo::TemplateArgumentLocInfo(
     ASTContext &Ctx, SourceLocation TrivialLoc) {
   if constexpr (EmbedLocInPointer)
-    Pointer = reinterpret_cast<void *>((TrivialLoc.getRawEncoding() + 1u)
-                                       << LowBitsRequired);
+    Pointer = reinterpret_cast<LocOrPointer>((TrivialLoc.getRawEncoding() + 1u)
+                                             << LowBitsRequired);
   else
     Pointer = new (Ctx) SourceLocation(TrivialLoc);
 }
