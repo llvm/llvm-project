ac92e1aaf63583c72c547dfc49073ccc07abf3eb
NeKon69 <nobodqwe@gmail.com>
2026-03-23 20:50:45 +0300

    update tests

--- clang/test/FixIt/fixit-defaulted-comparison.cpp
+++ clang/test/FixIt/fixit-defaulted-comparison.cpp
@@ -1,12 +1,18 @@
+// RUN: %clang_cc1 -verify -std=c++23 %s
 // RUN: cp %s %t
 // RUN: not %clang_cc1 -std=c++23 -x c++ -fixit %t
 // RUN: %clang_cc1 -std=c++23 -x c++ %t
 // RUN: not %clang_cc1 -std=c++23 -x c++ -fsyntax-only -fdiagnostics-parseable-fixits %s 2>&1 | FileCheck %s
 
-#include <compare>
+namespace std {
+struct partial_ordering {};
+} // namespace std
 
 struct Box {
-  std::partial_ordering operator<=>(const Box& other) = default;
-  // expected-error@-1 {{defaulted member three-way comparison operator must be const-qualified}}
-  // CHECK: fix-it:{{.*}}:{7:54-7:54}:" const"
+  std::partial_ordering operator<=>(const Box& other) = default; // #ssdecl
+  bool operator==(const Box& other) = default; // #eqdecl
+  // expected-error@#ssdecl {{defaulted member three-way comparison operator must be const-qualified}}
+  // expected-error@#eqdecl {{defaulted member equality comparison operator must be const-qualified}}
+  // CHECK: fix-it:{{.*}}:{12:54-12:54}:" const"
+  // CHECK: fix-it:{{.*}}:{13:36-13:36}:" const"
 };
