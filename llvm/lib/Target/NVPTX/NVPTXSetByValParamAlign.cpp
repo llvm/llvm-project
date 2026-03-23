cdae4c0c71b12ea36e5b9c6e793a224db41061cb
Alex Maclean <amaclean@nvidia.com>
2026-03-23 19:50:28 +0000

    format

--- llvm/lib/Target/NVPTX/NVPTXSetByValParamAlign.cpp
+++ llvm/lib/Target/NVPTX/NVPTXSetByValParamAlign.cpp
@@ -1,4 +1,4 @@
-//===-- NVPTXSetByValParamAlign.cpp - Set byval param alignment ------------===//
+//===-- NVPTXSetByValParamAlign.cpp - Set byval param alignment -----------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
