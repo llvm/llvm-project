16981b106097c34d81d2c79522656aff515807a8
Jordan Rupprecht <rupprecht@google.com>
2026-03-24 20:08:28 +0000

    Always call RegisterPlugin

--- lldb/source/Target/ProcessTrace.cpp
+++ lldb/source/Target/ProcessTrace.cpp
@@ -108,10 +108,8 @@ void ProcessTrace::Clear() { m_thread_list.Clear(); }
 void ProcessTrace::Initialize() {
   static llvm::once_flag g_once_flag;
 
-  llvm::call_once(g_once_flag, []() {
-    PluginManager::RegisterPlugin(GetPluginNameStatic(),
-                                  GetPluginDescriptionStatic(), CreateInstance);
-  });
+  PluginManager::RegisterPlugin(GetPluginNameStatic(),
+                                GetPluginDescriptionStatic(), CreateInstance);
 }
 
 ArchSpec ProcessTrace::GetArchitecture() {
