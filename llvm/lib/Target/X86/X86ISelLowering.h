0d060a3944dd376da2c28cce28caf221b62d97a5
Simon Pilgrim <llvm-dev@redking.me.uk>
2026-03-25 22:43:25 +0000

    [DAG] Add command line option and TLI hook to enable DAG topological sorting
    The very first step towards #83422 - which will move DAG combines to be processed in topological order.
    
    There is a lot of churn on existing tests that need to be addressed before this can be switched on globally, this patch gives the ability to enable it both on a per-target basis, and via a command line option to assist with testing and triage.
    
    At the moment I'm focussing on addressing the x86 regressions (examples in the basic test coverage) as that's the target I'm most familiar with and will help with many other targets as well, but there might be other targets that would benefit from earlier handling.

--- llvm/lib/Target/X86/X86ISelLowering.h
+++ llvm/lib/Target/X86/X86ISelLowering.h
@@ -108,6 +108,8 @@ namespace llvm {
     explicit X86TargetLowering(const X86TargetMachine &TM,
                                const X86Subtarget &STI);
 
+    bool useTopologicalSorting() const override { return false; }
+
     unsigned getJumpTableEncoding() const override;
     bool useSoftFloat() const override;
 
