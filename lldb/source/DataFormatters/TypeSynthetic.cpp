2d4d931a31cf8dcbfd0520bb8f976d1453caf79f
Dave Lee <davelee.com@gmail.com>
2026-03-25 14:52:02 -0700

    Revert back to allowing @update control over reuse

--- lldb/source/DataFormatters/TypeSynthetic.cpp
+++ lldb/source/DataFormatters/TypeSynthetic.cpp
@@ -293,12 +293,33 @@ lldb::ChildCacheState BytecodeSyntheticChildren::FrontEnd::Update() {
     return ChildCacheState::eRefetch;
   }
 
+  std::optional<ChildCacheState> can_reuse = std::nullopt;
+  const FormatterBytecode::DataStackElement &top = data.back();
+  if (auto *u = std::get_if<uint64_t>(&top))
+    if (*u == 0 || *u == 1)
+      can_reuse = static_cast<ChildCacheState>(*u);
+  if (auto *i = std::get_if<int64_t>(&top))
+    if (*i == 0 || *i == 1)
+      can_reuse = static_cast<ChildCacheState>(*i);
+
+  if (can_reuse) {
+    data.pop_back();
+    LLDB_LOG(
+        GetLog(LLDBLog::DataFormatters),
+        "Bytecode formatter can reuse @update: {0} (type: `{1}`, name: `{2}`)",
+        can_reuse ? "true" : "false", m_backend.GetDisplayTypeName(),
+        m_backend.GetName());
+  } else {
+    LLDB_LOG(GetLog(LLDBLog::DataFormatters),
+             "Bytecode formatter did not return a valid reuse response from "
+             "@update (type: `{1}`, name: `{2}`)",
+             m_backend.GetDisplayTypeName(), m_backend.GetName());
+  }
+
   if (data.size() > 0)
     m_self = std::move(data);
 
-  // At this time it is assumed that synthetic bytecode formatters can always
-  // reuse the work performed in `update`.
-  return ChildCacheState::eReuse;
+  return can_reuse.value_or(ChildCacheState::eRefetch);
 }
 
 llvm::Expected<uint32_t>
