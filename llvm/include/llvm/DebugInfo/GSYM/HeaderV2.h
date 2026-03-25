48b76a82ffb9e6c2be0e4e4bebca92de0a0edf68
Roy Shi <royshi@meta.com>
2026-03-25 14:52:30 -0700

    Update V2 header struct

--- llvm/include/llvm/DebugInfo/GSYM/HeaderV2.h
+++ llvm/include/llvm/DebugInfo/GSYM/HeaderV2.h
@@ -25,13 +25,18 @@ class FileWriter;
 constexpr uint32_t GSYM_MAGIC = 0x4753594d; // 'GSYM'
 constexpr uint32_t GSYM_CIGAM = 0x4d595347; // 'MYSG'
 constexpr uint32_t GSYM_VERSION_2 = 2;
-constexpr size_t GSYM_MAX_UUID_SIZE = 20;
 
 /// The GSYM V2 header.
 ///
-/// The GSYM header is found at the start of a stand alone GSYM file, or as
+/// The GSYM V2 header is found at the start of a stand alone GSYM file, or as
 /// the first bytes in a section when GSYM is contained in a section of an
 /// executable file (ELF, mach-o, COFF).
+///
+/// The V2 file format consists of the following sections in order:
+///   - Header (this struct, 40 bytes)
+///   - AddrOffsets (address offset table, aligned to AddrOffSize)
+///   - AddrInfoOffsets (address info offset table, aligned to AddrInfoOffSize)
+///   - GlobalData (blob containing FunctionInfos, UUID, string table, etc.)
 ///
 /// The structure is encoded exactly as it appears in the structure definition
 /// with no gaps between members. Alignment should not change from system to
@@ -41,50 +46,38 @@ constexpr size_t GSYM_MAX_UUID_SIZE = 20;
 /// When endianness of the system loading a GSYM file matches, the file can
 /// be mmap'ed in and a pointer to the header can be cast to the first bytes
 /// of the file (stand alone GSYM file) or section data (GSYM in a section).
-/// When endianness is swapped, the HeaderV2::decode() function should be used to
-/// decode the header.
+/// When endianness is swapped, the HeaderV2::decode() function should be used
+/// to decode the header.
 struct HeaderV2 {
   /// The magic bytes should be set to GSYM_MAGIC. This helps detect if a file
   /// is a GSYM file by scanning the first 4 bytes of a file or section.
-  /// This value might appear byte swapped
+  /// This value might appear byte swapped.
   uint32_t Magic;
-  /// The version can number determines how the header is decoded and how each
+  /// The version number determines how the header is decoded and how each
   /// InfoType in FunctionInfo is encoded/decoded. As version numbers increase,
   /// "Magic" and "Version" members should always appear at offset zero and 4
   /// respectively to ensure clients figure out if they can parse the format.
   uint16_t Version;
   /// The size in bytes of each address offset in the address offsets table.
+  /// Valid values are 1, 2, 4, or 8.
   uint8_t AddrOffSize;
-  /// The size in bytes of the UUID encoded in the "UUID" member.
-  uint8_t UUIDSize;
+  /// The size in bytes of each entry in the address info offsets table.
+  /// Valid values are 1, 2, 4, or 8. These offsets point into GlobalData.
+  uint8_t AddrInfoOffSize;
   /// The 64 bit base address that all address offsets in the address offsets
   /// table are relative to. Storing a full 64 bit address allows our address
   /// offsets table to be smaller on disk.
   uint64_t BaseAddress;
-  /// The number of addresses stored in the address offsets table.
+  /// The number of addresses stored in the address offsets table and the
+  /// address info offsets table.
   uint32_t NumAddresses;
-  /// The file relative offset of the start of the string table for strings
-  /// contained in the GSYM file. If the GSYM in contained in a stand alone
-  /// file this will be the file offset of the start of the string table. If
-  /// the GSYM is contained in a section within an executable file, this can
-  /// be the offset of the first string used in the GSYM file and can possibly
-  /// span one or more executable string tables. This allows the strings to
-  /// share string tables in an ELF or mach-o file.
-  uint32_t StrtabOffset;
-  /// The size in bytes of the string table. For a stand alone GSYM file, this
-  /// will be the exact size in bytes of the string table. When the GSYM data
-  /// is in a section within an executable file, this size can span one or more
-  /// sections that contains strings. This allows any strings that are already
-  /// stored in the executable file to be re-used, and any extra strings could
-  /// be added to another string table and the string table offset and size
-  /// can be set to span all needed string tables.
-  uint32_t StrtabSize;
-  /// The UUID of the original executable file. This is stored to allow
-  /// matching a GSYM file to an executable file when symbolication is
-  /// required. Only the first "UUIDSize" bytes of the UUID are valid. Any
-  /// bytes in the UUID value that appear after the first UUIDSize bytes should
-  /// be set to zero.
-  uint8_t UUID[GSYM_MAX_UUID_SIZE];
+  /// Reserved for future use. Must be set to zero. Also serve as padding.
+  uint32_t Reserved;
+  /// The file offset of the start of the GlobalData blob. GlobalData contains
+  /// FunctionInfos, UUID, string table, and any other future sections.
+  uint64_t GlobalDataFileOffset;
+  /// The size in bytes of the GlobalData blob.
+  uint64_t GlobalDataFileSize;
 
   /// Check if a header is valid and return an error if anything is wrong.
   ///
@@ -95,7 +88,8 @@ struct HeaderV2 {
   ///   - check magic value
   ///   - check that version number is supported
   ///   - check that the address offset size is supported
-  ///   - check that the UUID size is valid
+  ///   - check that the address info offset size is supported
+  ///   - check that Reserved is zero
   ///
   /// \returns An error if anything is wrong in the header, or Error::success()
   /// if there are no errors.
