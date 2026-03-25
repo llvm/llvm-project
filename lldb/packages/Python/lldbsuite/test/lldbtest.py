52d29db0784fb2b8df993ae0df3025488dbe9879
Dave Lee <davelee.com@gmail.com>
2026-03-25 14:27:09 -0700

    Formatting

--- lldb/packages/Python/lldbsuite/test/lldbtest.py
+++ lldb/packages/Python/lldbsuite/test/lldbtest.py
@@ -1532,7 +1532,10 @@ class Base(unittest.TestCase):
     ):
         """Platform specific way to build binaries."""
         if debug_info or architecture or compiler or dictionary or make_targets:
-            self.assertFalse(self.SHARED_BUILD_TESTCASE, "shared build test cases reuse the same compilation artifacts for all test functions")
+            self.assertFalse(
+                self.SHARED_BUILD_TESTCASE,
+                "shared build tests reuse the same compilation artifacts for all test functions",
+            )
 
         if not architecture and configuration.arch:
             architecture = configuration.arch
