8c980ddab29ed4c1bb28e82faa667c85d3dd7f0e
Michael Buch <michaelbuch12@gmail.com>
2026-03-26 16:01:37 +0000

    fixup! shorten comment to make clang-format happy

--- cross-project-tests/debuginfo-tests/dexter/feature_tests/commands/perfect/expect_step_kind/direction.cpp
+++ cross-project-tests/debuginfo-tests/dexter/feature_tests/commands/perfect/expect_step_kind/direction.cpp
@@ -10,7 +10,7 @@
 // TODO: The dbgeng debugger does not support column step reporting at present.
 // XFAIL: system-windows
 //
-// TODO: fails in on Linux CI (https://github.com/llvm/llvm-project/issues/188775)
+// TODO: fails in on Linux CI (llvm-project/issues/188775)
 // XFAIL: system-linux
 //
 // RUN: %dexter_regression_test_cxx_build %s -o %t
