428dbe460c75cdb8280df8e06907ead36b9c0c11
Ron Lieberman <ron.lieberman@amd.com>
2026-03-26 14:47:16 -0500

    Revert "[OpenMP][flang] Fix crash in host offload (#187847)"
    This reverts commit 797916b221d13f79e3012fa31147262dae97e1db.

--- llvm/lib/Frontend/OpenMP/OMPIRBuilder.cpp
+++ llvm/lib/Frontend/OpenMP/OMPIRBuilder.cpp
@@ -169,10 +169,6 @@ static void restoreIPandDebugLoc(llvm::IRBuilderBase &Builder,
     Builder.SetCurrentDebugLocation(BB->back().getStableDebugLoc());
 }
 
-static bool hasGridValue(const Triple &T) {
-  return T.isAMDGPU() || T.isNVPTX() || T.isSPIRV();
-}
-
 static const omp::GV &getGridValue(const Triple &T, Function *Kernel) {
   if (T.isAMDGPU()) {
     StringRef Features =
@@ -8063,15 +8059,9 @@ OpenMPIRBuilder::InsertPointTy OpenMPIRBuilder::createTargetInit(
   // If MaxThreads not set, select the maximum between the default workgroup
   // size and the MinThreads value.
   int32_t MaxThreadsVal = Attrs.MaxThreads.front();
-  if (MaxThreadsVal < 0) {
-    if (hasGridValue(T)) {
-      MaxThreadsVal =
-          std::max(int32_t(getGridValue(T, Kernel).GV_Default_WG_Size),
-                   Attrs.MinThreads);
-    } else {
-      MaxThreadsVal = Attrs.MinThreads;
-    }
-  }
+  if (MaxThreadsVal < 0)
+    MaxThreadsVal = std::max(
+        int32_t(getGridValue(T, Kernel).GV_Default_WG_Size), Attrs.MinThreads);
 
   if (MaxThreadsVal > 0)
     writeThreadBoundsForKernel(T, *Kernel, Attrs.MinThreads, MaxThreadsVal);
