b9cca9ca78f07034a03a153e3faecffb4a42146b
Nick Sarnie <nick.sarnie@intel.com>
2026-03-27 03:58:47 +0900

    Revert "[HLSL][SPIRV] Add support for -g to generate NonSemantic Debug Info" (#188771)
    Reverts llvm/llvm-project#187051
    
    Breaks some OpenMP offload tests

--- clang/lib/CodeGen/CGDebugInfo.cpp
+++ clang/lib/CodeGen/CGDebugInfo.cpp
@@ -694,9 +694,7 @@ static llvm::dwarf::SourceLanguage GetSourceLanguage(const CodeGenModule &CGM) {
 
   llvm::dwarf::SourceLanguage LangTag;
   if (LO.CPlusPlus) {
-    if (LO.HLSL)
-      LangTag = llvm::dwarf::DW_LANG_HLSL;
-    else if (LO.HIP)
+    if (LO.HIP)
       LangTag = llvm::dwarf::DW_LANG_HIP;
     else if (LO.ObjC)
       LangTag = llvm::dwarf::DW_LANG_ObjC_plus_plus;
@@ -734,9 +732,7 @@ GetDISourceLanguageName(const CodeGenModule &CGM) {
   uint32_t LangVersion = 0;
   llvm::dwarf::SourceLanguageName LangTag;
   if (LO.CPlusPlus) {
-    if (LO.HLSL) {
-      LangTag = llvm::dwarf::DW_LNAME_HLSL;
-    } else if (LO.HIP) {
+    if (LO.HIP) {
       LangTag = llvm::dwarf::DW_LNAME_HIP;
     } else if (LO.ObjC) {
       LangTag = llvm::dwarf::DW_LNAME_ObjC_plus_plus;
