2c96feac9b7c338bd8d5ba57edf0a1360044bfa3
Jonas Devlieghere <jonas@devlieghere.com>
2026-03-26 09:55:29 -0700

    Update llvm/test/MC/RISCV/Relocations/align-after-relax.s

--- llvm/test/MC/RISCV/Relocations/align-after-relax.s
+++ llvm/test/MC/RISCV/Relocations/align-after-relax.s
@@ -1,8 +1,10 @@
 # RUN: llvm-mc -filetype=obj -triple=riscv64 -mattr=+c,+relax %s --defsym LATE=1 -o %t1
 # RUN: llvm-objdump -dr --no-show-raw-insn -M no-aliases %t1 | FileCheck %s
 
+## Without LATE, the output is the same since the fragment layout order
+## does not change.
 # RUN: llvm-mc -filetype=obj -triple=riscv64 -mattr=+c,+relax %s -o %t0
-# RUN: llvm-objdump -dr --no-show-raw-insn -M no-aliases %t0 | FileCheck %s --check-prefix=CHECK0
+# RUN: llvm-objdump -dr --no-show-raw-insn -M no-aliases %t0 | FileCheck %s
 
 # CHECK:            4: 00 00 01 00 .word 0x00010000
 # CHECK-EMPTY:
@@ -13,14 +15,6 @@
 # CHECK-NEXT:                R_RISCV_RELAX        *ABS*
 # CHECK:           18: c.nop
 # CHECK-NEXT:                R_RISCV_ALIGN        *ABS*+0x6
-
-## Alignment directives in a lower-numbered subsection may be conservatively treated as linker-relaxable.
-# CHECK0:           4: 00 00 01 00 .word 0x00010000
-# CHECK0-NEXT:               000000006: R_RISCV_ALIGN        *ABS*+0x6
-# CHECK0-NEXT:      8: 13 00 00 00 .word 0x00000013
-# CHECK0:          14: auipc   ra, 0x0
-# CHECK0:          1c: c.nop
-# CHECK0-NEXT:               R_RISCV_ALIGN        *ABS*+0x6
 
 .text 2
 .option push
