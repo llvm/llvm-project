484e911b1b348f53ccbf04defa3636fc4846b6fb
Ryotaro Kasuga <kasuga.ryotaro@fujitsu.com>
2026-03-27 15:14:27 +0900

    [DA] Add precondition `0 <=s UB` to function `inferAffineDomain` (#187218)
    This patch fixes issues where the Exact SIV/RDIV tests can return
    incorrect results when the BTC is negative in a signed sense. The root
    cause is that BTCs should be interpreted in an unsigned sense, but
    `inferAffineDomain` uses the BTC in inequalities that are interpreted in
    a signed sense. These inequalities make sense only when the BTC is
    non-negative. Thus we need to check it beforehand.

--- llvm/include/llvm/Analysis/DependenceAnalysis.h
+++ llvm/include/llvm/Analysis/DependenceAnalysis.h
@@ -468,10 +468,11 @@ private:
   /// If no upper bound is available, return NULL.
   const SCEV *collectUpperBound(const Loop *l, Type *T) const;
 
-  /// collectConstantUpperBound - Calls collectUpperBound(), then
-  /// attempts to cast it to SCEVConstant. If the cast fails,
-  /// returns NULL.
-  const SCEVConstant *collectConstantUpperBound(const Loop *l, Type *T) const;
+  /// collectNonNegativeConstantUpperBound - Calls collectUpperBound(), then
+  /// attempts to cast it to APInt. If the cast fails, or the value is negative,
+  /// returns std::nullopt.
+  std::optional<APInt> collectNonNegativeConstantUpperBound(const Loop *L,
+                                                            Type *T) const;
 
   /// classifyPair - Examines the subscript pair (the Src and Dst SCEVs)
   /// and classifies it as either ZIV, SIV, RDIV, MIV, or Nonlinear.
