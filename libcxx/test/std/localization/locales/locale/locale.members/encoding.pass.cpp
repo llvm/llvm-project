5c9d37a263fe22358ba74d3141c7f4cc9c9bfe50
William Tran-Viet <wtranviet@proton.me>
2026-03-26 21:11:53 -0400

    Remove default locale encoding test

--- libcxx/test/std/localization/locales/locale/locale.members/encoding.pass.cpp
+++ libcxx/test/std/localization/locales/locale/locale.members/encoding.pass.cpp
@@ -17,43 +17,18 @@
 // text_encoding locale::encoding() const
 
 #include <cassert>
-#include <format>
-#include <iostream>
 #include <locale>
 #include <text_encoding>
 
 #include "platform_support.h"
 
-using id = std::text_encoding::id;
-
 int main(int, char**) {
   {
-    // 1. Default locale returns a text_encoding representing "ASCII", or "UTF-8 on Android.
-    const std::locale loc{};
-
-    std::text_encoding te = loc.encoding();
-#if !defined(__ANDROID__)
-    std::text_encoding classic_te = std::text_encoding(id::ASCII);
-    if (te != id::ASCII) {
-      std::cerr << std::format(
-          "Expected ASCII, received: {{{}, \"{}\"}}, locale: \"{}\"", int(te.mib()), te.name(), loc.name());
-      assert(false);
-    }
-    assert(te == classic_te);
-#else
-    auto utf8_te = std::text_encoding(id::UTF8);
-    assert(te == id::UTF8);
-    assert(te == utf8_te);
-#endif
-  }
-
-  {
-    // 2. Locale built with en_US.UTF-8 returns text_encoding representing "UTF-8"
+    // 1. Locale built with en_US.UTF-8 returns text_encoding representing "UTF-8"
     const std::locale utf8_locale(LOCALE_en_US_UTF_8);
-
     std::text_encoding te      = utf8_locale.encoding();
-    std::text_encoding utf8_te = std::text_encoding(id::UTF8);
-    assert(te == id::UTF8);
+    std::text_encoding utf8_te = std::text_encoding(std::text_encoding::id::UTF8);
+    assert(te == std::text_encoding::id::UTF8);
     assert(te == utf8_te);
   }
   return 0;
