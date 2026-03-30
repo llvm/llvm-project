a62b0090225d05e232ecc871d70103dce74b2d66
Owen Anderson <resistor@mac.com>
2026-03-30 10:49:31 +0200

    [CHERI] Establish addrspace(200) as the CHERI capability address space.
    This has a long precedent in the CHERI downstreams, including in the large corpus of test cases. It was originally chosen to minimize the likelihood of conflict with other use cases, and to be reusable across all backends implementing CHERI. It also enables support for "hybrid" CHERI mode (not planned for upstream at this time, but is supported downstream) in which addrspace(0) represents conventional pointers while addrspace(200) represents CHERI capabilities.

--- clang/include/clang/Basic/AddressSpaces.h
+++ clang/include/clang/Basic/AddressSpaces.h
@@ -71,7 +71,10 @@ enum class LangAS : unsigned {
   // This denotes the count of language-specific address spaces and also
   // the offset added to the target-specific address spaces, which are usually
   // specified by address space attributes __attribute__(address_space(n))).
-  FirstTargetAddressSpace
+  FirstTargetAddressSpace,
+
+  // 200 is used for CHERI capabilities on all targets.
+  cheri_capability = FirstTargetAddressSpace + 200,
 };
 
 /// The type of a lookup table which maps from language-specific address spaces
