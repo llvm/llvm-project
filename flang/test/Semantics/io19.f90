92c1699cdf7d5374ac1f108bf840bf7000b814a5
Shandong Lao <shandong.lao@hpe.com>
2026-03-31 12:13:20 -0500

    Remove obsolete comment; Add test cases to cover non-default character kinds.

--- flang/test/Semantics/io19.f90
+++ flang/test/Semantics/io19.f90
@@ -2,11 +2,18 @@
 ! Test AT edit descriptor (Fortran 202X)
 
   character(10) :: str
+  character(kind=2,len=10) :: str2
+  character(kind=4,len=10) :: str4
 
   ! Valid: AT with WRITE
   write(*, '(AT)') 'hello'
   write(*, '(AT)') str
   write(*, '(2AT)') 'abc', 'def'
+
+  ! Valid: AT with non-default character kinds
+  write(*, '(AT)') str2
+  write(*, '(AT)') str4
+  write(*, '(2AT)') str2, str4
 
   ! Valid: AT in FORMAT statement for WRITE
 1 format(AT)
@@ -15,6 +22,12 @@
   ! Error: AT must not be used for input
   !ERROR: 'AT' edit descriptor must not be used for input
   read(*, '(AT)') str
+
+  ! Error: AT must not be used for input with non-default kinds
+  !ERROR: 'AT' edit descriptor must not be used for input
+  read(*, '(AT)') str2
+  !ERROR: 'AT' edit descriptor must not be used for input
+  read(*, '(AT)') str4
 
   ! AT does not accept a width
   !ERROR: 'AT' edit descriptor does not accept a width value
