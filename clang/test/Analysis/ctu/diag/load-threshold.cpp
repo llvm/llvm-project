8a0bc37bd57a85545bb068a22bb3fdd499de4112
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2026-03-31 09:50:30 +0200

    Document the behavior for threshold-reached remark

--- clang/test/Analysis/ctu/diag/load-threshold.cpp
+++ clang/test/Analysis/ctu/diag/load-threshold.cpp
@@ -11,13 +11,18 @@
 // RUN: %clang_cc1 -std=c++14 -triple x86_64-pc-linux-gnu \
 // RUN:   -emit-pch -o %t/bar.cpp.ast \
 // RUN:   %S/Inputs/bar.cpp
+// RUN: %clang_cc1 -std=c++14 -triple x86_64-pc-linux-gnu \
+// RUN:   -emit-pch -o %t/third.cpp.ast \
+// RUN:   %S/Inputs/third.cpp
 // RUN: echo '11:c:@F@foo#I# simple.cpp.ast' > %t/externalDefMap.txt
 // RUN: echo '11:c:@F@bar#I# bar.cpp.ast' >> %t/externalDefMap.txt
+// RUN: echo '13:c:@F@third#I# third.cpp.ast' >> %t/externalDefMap.txt
 // RUN: %clang_analyze_cc1 -std=c++14 -triple x86_64-pc-linux-gnu \
 // RUN:   -analyzer-checker=core \
 // RUN:   -analyzer-config experimental-enable-naive-ctu-analysis=true \
 // RUN:   -analyzer-config ctu-dir=%t \
 // RUN:   -analyzer-config ctu-import-cpp-threshold=1 \
+// RUN:   -Rctu \
 // RUN:   -verify %s
 
 // foo is loaded successfully (first load, within threshold).
@@ -25,9 +30,11 @@
 
 int foo(int);
 int bar(int);
+int third(int);
 
 void test() {
   foo(1);
-  // expected-no-diagnostics
-  bar(1); // no-warning. Ignoring "Load threshold reached."
+  bar(1); // expected-remark {{reached a the CTU-import threshold before trying to import definition}}
+  bar(2); // expected-remark {{reached a the CTU-import threshold before trying to import definition}}
+  third(1); // expected-remark {{reached a the CTU-import threshold before trying to import definition}}
 }
