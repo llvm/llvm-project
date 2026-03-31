a2ec1b4ba3c2e081b957386de1fca4e83e6bc796
NeKon69 <nobodqwe@gmail.com>
2026-03-31 15:06:04 +0300

    add another testcase

--- clang/test/Sema/warn-lifetime-safety.cpp
+++ clang/test/Sema/warn-lifetime-safety.cpp
@@ -2073,13 +2073,16 @@ int* static_array() {
 void pointer_arithmetic_use_after_scope() {
   int* p;
   int* p2;
+  int* p3;
   {
     int a[10]{};
     p = a + 5;  // expected-warning {{object whose reference is captured does not live long enough}}
     p2 = a - 5; // expected-warning {{object whose reference is captured does not live long enough}}
-  }             // expected-note 2 {{destroyed here}}
+    p3 = 5 + a; // expected-warning {{object whose reference is captured does not live long enough}}
+  }             // expected-note 3 {{destroyed here}}
   (void)*p;     // expected-note {{later used here}}
   (void)*p2;    // expected-note {{later used here}}
+  (void)*p3;    // expected-note {{later used here}}
 }
 
 // FIXME: Copying a pointer value out of an array element is not tracked.
