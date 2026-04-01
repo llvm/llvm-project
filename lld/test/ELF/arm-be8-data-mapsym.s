7ac412ad42816b7928882a65da225c07b3bca7f6
Simi Pallipurath <simi.pallipurath@arm.com>
2026-04-01 16:12:21 +0100

    fixup! [LLD][ELF]Skip non-inputsections to avoid invalid cast in BE8 handling.

--- lld/test/ELF/arm-be8-data-mapsym.s
+++ lld/test/ELF/arm-be8-data-mapsym.s
@@ -5,25 +5,10 @@
 
 // RUN: llvm-mc -filetype=obj -triple=armv7aeb-none-linux-gnueabi %s -o %t.o
 // RUN: ld.lld --be8 %t.o -shared -o %t
-// RUN: llvm-readobj --file-headers --sections --section-data %t | FileCheck %s
+// RUN: llvm-readelf -S -x .merge %t | FileCheck %s
 
-// CHECK:      DataEncoding: BigEndian (0x2)
-// CHECK:      Name: .merge
-// CHECK-NEXT: Type: SHT_PROGBITS
-// CHECK-NEXT: Flags [
-// CHECK-NEXT:   SHF_ALLOC
-// CHECK-NEXT:   SHF_MERGE
-// CHECK-NEXT: ]
-// CHECK-NEXT: Address:
-// CHECK-NEXT: Offset:
-// CHECK-NEXT: Size: 4
-// CHECK-NEXT: Link: 0
-// CHECK-NEXT: Info: 0
-// CHECK-NEXT: AddressAlignment:
-// CHECK-NEXT: EntrySize: 4
-// CHECK-NEXT: SectionData (
-// CHECK-NEXT:   0000: 11223344 |
-// CHECK-NEXT: )
+// CHECK: Hex dump of section '.merge':
+// CHECK-NEXT: 0x0000014d 11223344
 
 .section .merge, "aM", %progbits, 4
 /// GNU assembler adds a mapping symbol for this SHF_MERGE data section.
