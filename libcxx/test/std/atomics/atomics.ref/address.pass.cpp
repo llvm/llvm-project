6bd6d6dfbd796863ba7f2ede65932e62ce2ed1bb
Hristo Hristov <hghristov.rmm@gmail.com>
2026-04-01 08:44:29 +0300

    Applied suggestion

--- libcxx/test/std/atomics/atomics.ref/address.pass.cpp
+++ libcxx/test/std/atomics/atomics.ref/address.pass.cpp
@@ -17,50 +17,10 @@
 #include <cassert>
 #include <concepts>
 #include <memory>
+#include <type_traits>
 
 #include "atomic_helpers.h"
 #include "test_macros.h"
-
-// Let COPYCV(FROM, TO) be an alias for type TO with the addition of FROM's
-// top-level cv-qualifiers.
-template <class _From>
-struct copy_cv {
-  template <class _To>
-  using apply = _To;
-};
-
-template <class _From>
-struct copy_cv<const _From> {
-  template <class _To>
-  using apply = const _To;
-};
-
-template <class _From>
-struct copy_cv<volatile _From> {
-  template <class _To>
-  using apply = volatile _To;
-};
-
-template <class _From>
-struct copy_cv<const volatile _From> {
-  template <class _To>
-  using apply = const volatile _To;
-};
-
-template <class _From, class _To>
-using copy_cv_t = typename copy_cv<_From>::template apply<_To>;
-
-template <class T>
-using identity_t = T;
-
-template <class T>
-using add_const_t = const T;
-
-template <class T>
-using add_volatile_t = volatile T;
-
-template <class T>
-using add_const_volatile_t = const volatile T;
 
 template <template <class TestArg> class TestFunctor, template <class> class AddQualifier>
 struct TestEachAtomicTypeWithCV {
@@ -88,7 +48,12 @@ struct TestAddress {
     alignas(std::atomic_ref<T>::required_alignment) T x(T(1));
     const std::atomic_ref<T> a(x);
 
-    using AddressReturnT = copy_cv_t<T, void>*;
+    using AddressReturnT =
+        std::conditional_t<std::is_const_v<T> && std::is_volatile_v<T>,
+                           const volatile void,
+                           std::conditional_t<std::is_volatile_v<T>,
+                                              volatile void,
+                                              std::conditional_t<std::is_const_v<T>, const void, void>>>*;
 
     std::same_as<AddressReturnT> decltype(auto) p = a.address();
     assert(std::addressof(x) == p);
