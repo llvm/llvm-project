cbd946c1170630572f92b62c08779e7cd448c9ea
Diana Picus <diana-magda.picus@amd.com>
2026-04-01 10:15:24 +0200

    [AMDGPU] Cleanup dVGPR subtarget feature properly. NFC
    Dynamic VGPR mode is expressed via an attribute, not a subtarget
    feature. We had some transitional code that looked for both, but we've
    migrated to using only the attribute long ago, so cleaning it up
    shouldn't affect anything.

--- llvm/lib/Target/AMDGPU/SIMachineFunctionInfo.cpp
+++ llvm/lib/Target/AMDGPU/SIMachineFunctionInfo.cpp
@@ -63,12 +63,7 @@ SIMachineFunctionInfo::SIMachineFunctionInfo(const Function &F,
   MaxNumWorkGroups = AMDGPU::getMaxNumWorkGroups(F);
   assert(MaxNumWorkGroups.size() == 3);
 
-  // Temporarily check both the attribute and the subtarget feature, until the
-  // latter is completely removed.
   DynamicVGPRBlockSize = AMDGPU::getDynamicVGPRBlockSize(F);
-  if (DynamicVGPRBlockSize == 0 && ST.isDynamicVGPREnabled())
-    DynamicVGPRBlockSize = ST.getDynamicVGPRBlockSize();
-
   Occupancy = ST.computeOccupancy(F, getLDSSize()).second;
   CallingConv::ID CC = F.getCallingConv();
 
