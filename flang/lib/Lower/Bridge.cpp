7670f5d1b91472a6d83a59d943a432b44e0923bf
Jack Styles <jack.styles@arm.com>
2026-04-01 16:34:53 +0100

    [Flang] Add `SIMD` Compiler Directive
    Similar to #192674, this adds support for the SIMD Compiler Directive,
    previously supported by legacy Fortran frontends such as classic-flang.
    This operates in the same way as `VECTORALWAYS`, marking a DO loop
    to be vectorised by the LLVM Backend.
    
    The missing support was highlighted while building an opensource benchmark,
    as build warnings were indicating that this compiler directive was being
    ignored.

--- flang/lib/Lower/Bridge.cpp
+++ flang/lib/Lower/Bridge.cpp
@@ -2825,6 +2825,11 @@ private:
                     mlir::LLVM::AccessGroupAttr::get(builder->getContext()));
                 has_attrs = true;
               },
+              [&](const Fortran::parser::CompilerDirective::Simd &simd) {
+                disableVecAttr =
+                    mlir::BoolAttr::get(builder->getContext(), false);
+                has_attrs = true;
+              },
               [&](const auto &) {}},
           dir->u);
     }
@@ -3584,6 +3589,9 @@ private:
               }
             },
             [&](const Fortran::parser::CompilerDirective::IVDep &) {
+              attachDirectiveToLoop(dir, &eval);
+            },
+            [&] (const Fortran::parser::CompilerDirective::Simd &) {
               attachDirectiveToLoop(dir, &eval);
             },
             [&](const auto &) {}},
