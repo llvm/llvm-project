llvm/llvm-project

commit 01bb749c38f1f027ab0123adb5de63cffe05fe4a

[AMDDevicelibs] Fix linking issues. (#1185)
- Remove force conversion of global symbols to 'linkonce_odr' from
prepare_builtins.cpp.
- Add weak attribute to '__ockl_dm_nna' symbol due to multiply defined
in both asanrtl.bc and ockl.bc.

amd/device-libs/utils/prepare-builtins/prepare-builtins.cpp
@@ -73,28 +73,6 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  // Set linkage of every external definition to linkonce_odr.
-  for (Module::iterator i = M->begin(), e = M->end(); i != e; ++i) {
-    if (!i->isDeclaration() && i->getLinkage() == GlobalValue::ExternalLinkage) {
-        i->setLinkage(GlobalValue::LinkOnceODRLinkage);
-    }
-  }
-
-  for (Module::global_iterator i = M->global_begin(), e = M->global_end();
-       i != e; ++i) {
-    if (!i->isDeclaration() && i->getLinkage() == GlobalValue::ExternalLinkage) {
-        i->setLinkage(GlobalValue::LinkOnceODRLinkage);
-    }
-  }
-
-  for (Module::alias_iterator i = M->alias_begin(), e = M->alias_end();
-       i != e; ++i) {
-    if (!i->isDeclaration() && i->getLinkage() == GlobalValue::ExternalLinkage) {
-        i->setLinkage(GlobalValue::LinkOnceODRLinkage);
-    }
-  }
-
-
   if (OutputFilename.empty()) {
     errs() << "no output file\n";
     return 1;

amd/device-libs/ockl/src/dm.cl
@@ -1280,7 +1280,7 @@ __ockl_dm_hinfo(ulong *rp)
 #if defined NON_SLAB_TRACKING
 // return a snapshot of the current number of nonslab allocations
 // which haven't been deallocated
-__attribute__((cold)) ulong
+__attribute__((cold,weak)) ulong
 __ockl_dm_nna(void)
 {
     __global heap_t *hp = get_heap_ptr();

commit 2c6ccd1c53d17d8d262e5ee129260ccb5b348202

fix regression in ASAN build for RCCL & rocSHMEM (#1969)
A regression was introduced in commit `01bb749c38f1` (2026-01-27) that
causes
duplicate symbol linker errors when building AMDGPU code with both
`-fsanitize=address` (ASan) and `-fgpu-rdc` (relocatable device code).

reverting the change to fix rccl & rocSHMEM ASAN builds.

ROCM-20881
ROCM-20490

Co-authored-by: Prasannakumar Murugesan <prmuruge@amd.com>

amd/device-libs/utils/prepare-builtins/prepare-builtins.cpp
@@ -73,6 +73,29 @@ int main(int argc, char **argv) {
     return 1;
   }
 
+  // Set linkage of every external definition to linkonce_odr.
+  // This is required to avoid duplicate symbol errors when linking
+  // device code from multiple translation units with -fgpu-rdc.
+  for (Module::iterator i = M->begin(), e = M->end(); i != e; ++i) {
+    if (!i->isDeclaration() && i->getLinkage() == GlobalValue::ExternalLinkage) {
+        i->setLinkage(GlobalValue::LinkOnceODRLinkage);
+    }
+  }
+
+  for (Module::global_iterator i = M->global_begin(), e = M->global_end();
+       i != e; ++i) {
+    if (!i->isDeclaration() && i->getLinkage() == GlobalValue::ExternalLinkage) {
+        i->setLinkage(GlobalValue::LinkOnceODRLinkage);
+    }
+  }
+
+  for (Module::alias_iterator i = M->alias_begin(), e = M->alias_end();
+       i != e; ++i) {
+    if (!i->isDeclaration() && i->getLinkage() == GlobalValue::ExternalLinkage) {
+        i->setLinkage(GlobalValue::LinkOnceODRLinkage);
+    }
+  }
+
   if (OutputFilename.empty()) {
     errs() << "no output file\n";
     return 1;