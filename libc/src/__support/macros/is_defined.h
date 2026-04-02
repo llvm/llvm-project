de7a9f77d4b9a2c322f0a777e29c4a1968c20918
Roland McGrath <mcgrathr@google.com>
2026-04-02 12:02:45 -0700

    Fix comment typo

--- libc/src/__support/macros/is_defined.h
+++ libc/src/__support/macros/is_defined.h
@@ -1,4 +1,4 @@
-//===-- LLVM_IS_DEFINED macro ----------------------------------*- C++ -*-===//
+//===-- LLVM_LIBC_IS_DEFINED macro -----------------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
