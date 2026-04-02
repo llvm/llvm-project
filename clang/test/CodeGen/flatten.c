8a8d4896f62a27a26fb706bc04b7c3c43e5b354e
Grigory Pastukhov <gpastukhov@meta.com>
2026-04-02 13:16:02 -0700

    Add inlining check to flatten.c CodeGen test

--- clang/test/CodeGen/flatten.c
+++ clang/test/CodeGen/flatten.c
@@ -1,8 +1,12 @@
-// RUN: %clang_cc1 -triple=x86_64-linux-gnu -disable-llvm-passes %s -emit-llvm -o - | FileCheck %s
+// RUN: %clang_cc1 -triple=x86_64-linux-gnu %s -emit-llvm -o - | FileCheck %s
 
-// CHECK: define{{.*}} void @g() [[FLATTEN_ATTR:#[0-9]+]]
+void f(void) {}
+
 __attribute__((flatten))
+// CHECK: define{{.*}} void @g() [[FLATTEN_ATTR:#[0-9]+]]
 void g(void) {
+  // CHECK-NOT: call {{.*}} @f
+  f();
 }
 
 // CHECK: attributes [[FLATTEN_ATTR]] = {{{.*}}flatten{{.*}}}
