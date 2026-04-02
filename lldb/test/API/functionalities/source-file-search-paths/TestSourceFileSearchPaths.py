61886c20d153d0d0223811eeae8397e55ddef12f
Tom Yang <toyang@fb.com>
2026-04-02 13:10:31 -0700

    improve testing
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- lldb/test/API/functionalities/source-file-search-paths/TestSourceFileSearchPaths.py
+++ lldb/test/API/functionalities/source-file-search-paths/TestSourceFileSearchPaths.py
@@ -11,105 +11,102 @@ import shutil
 
 
 class TestSourceFileSearchPaths(TestBase):
-    @no_debug_info_test
+    NO_DEBUG_INFO_TESTCASE = True
+
     def test_source_file_search_paths(self):
         """Test that target.source-file-search-paths finds relocated source
         files and auto-creates source mappings."""
-        # Use the yaml object from the source-map test. Its debug info
-        # references './Trivial/main.c' with comp_dir '.'.
-        src_dir = os.path.join(self.getSourceDir(), os.pardir, "source-map")
-        yaml_path = os.path.join(src_dir, "a.yaml")
-        obj_path = self.getBuildArtifact("main.o")
-        self.yaml2obj(yaml_path, obj_path)
 
-        target = self.dbg.CreateTarget(obj_path)
+        # Sources need to be relocated to the build dir first before we can
+        # build. The structure here must be consistent with the Makefile's
+        # expectations.
+        lldbutil.mkdir_p(self.getBuildArtifact("subdir"))
+        for relative_src_path in ["main.cpp", "subdir/foo.h", "subdir/foo.cpp"]:
+            shutil.copyfile(
+                self.getSourcePath(relative_src_path),
+                self.getBuildArtifact(relative_src_path),
+            )
+        self.build()
+
+        main_line_number = line_number("main.cpp", "// SOURCE THIS LINE")
+        foo_line_number = line_number("subdir/foo.cpp", "// SOURCE THIS LINE")
+
+        # We're going to relocate both source files from main.cpp and
+        # subdir/foo.cpp to relocated/main.cpp and relocated/foo.cpp. We don't
+        # use the header file `foo.h` in the test, so we don't bother with it.
+        relocated_dir = self.getBuildArtifact("relocated")
+        os.makedirs(relocated_dir, exist_ok=True)
+        # Full-path is needed to overwrite (subsequent test runs).
+        shutil.move(
+            self.getBuildArtifact("main.cpp"), os.path.join(relocated_dir, "main.cpp")
+        )
+        shutil.move(
+            self.getBuildArtifact("subdir/foo.cpp"),
+            os.path.join(relocated_dir, "foo.cpp"),
+        )
+
+        exe = self.getBuildArtifact("a.out")
+        target = self.dbg.CreateTarget(exe)
         self.assertTrue(target, VALID_TARGET)
 
-        # The debug info says the source is at ./Trivial/main.c (relative to
-        # comp_dir '.').  Create a relocated directory that contains
-        # Trivial/main.c so the suffix matcher can find it.
-        relocated_dir = os.path.join(self.getBuildDir(), "relocated")
-        relocated_trivial = os.path.join(relocated_dir, "Trivial")
-        os.makedirs(relocated_trivial, exist_ok=True)
-        shutil.copy(
-            os.path.join(src_dir, "Trivial", "main.c"),
-            os.path.join(relocated_trivial, "main.c"),
+        # Sanity-check that we're unable to get source prior to setting
+        # `target.source-file-search-path`.
+        source_manager = target.GetSourceManager()
+        stream = lldb.SBStream()
+        bytes = source_manager.DisplaySourceLinesWithLineNumbers(
+            lldb.SBFileSpec(self.getBuildArtifact("main.cpp")),
+            main_line_number,
+            1,
+            1,
+            "",
+            stream,
+        )
+        self.assertEqual(
+            bytes, 0, f"shouldn't be able to find main.cpp: {stream.GetData()}"
+        )
+        stream = lldb.SBStream()
+        bytes = source_manager.DisplaySourceLinesWithLineNumbers(
+            lldb.SBFileSpec(self.getBuildArtifact("subdir/foo.cpp")),
+            foo_line_number,
+            1,
+            1,
+            "",
+            stream,
+        )
+        self.assertEqual(
+            bytes, 0, f"shouldn't be able to find foo.cpp: {stream.GetData()}"
         )
 
         # Set source-file-search-paths to the relocated directory.
-        self.runCmd(
-            'settings set target.source-file-search-paths "%s"' % relocated_dir
+        self.runCmd('settings set target.source-file-search-paths "%s"' % relocated_dir)
+
+        # We should be able to find the sources now
+        stream = lldb.SBStream()
+        bytes = source_manager.DisplaySourceLinesWithLineNumbers(
+            lldb.SBFileSpec(self.getBuildArtifact("main.cpp")),
+            main_line_number,
+            1,
+            1,
+            "",
+            stream,
+        )
+        self.assertGreater(bytes, 0, "should find main.cpp")
+        self.assertRegex(
+            stream.GetData(), ".*multiply.*", "couldn't find the expected source lines"
         )
 
-        # Ask LLDB to list source for main.c.  The suffix matcher should
-        # discover <relocated_dir>/Trivial/main.c and auto-create a mapping.
-        retval = lldb.SBCommandReturnObject()
-        self.dbg.GetCommandInterpreter().HandleCommand(
-            "source list -f main.c -l 1", retval
+        stream = lldb.SBStream()
+        bytes = source_manager.DisplaySourceLinesWithLineNumbers(
+            lldb.SBFileSpec(self.getBuildArtifact("subdir/foo.cpp")),
+            foo_line_number,
+            1,
+            1,
+            "",
+            stream,
         )
-        self.assertTrue(
-            retval.Succeeded(),
-            "source list should succeed with source-file-search-paths",
-        )
-        self.assertIn(
-            "return",
-            retval.GetOutput(),
-            "source list should show the source file contents",
-        )
-
-    @no_debug_info_test
-    def test_source_file_search_paths_multiple(self):
-        """Test that target.source-file-search-paths searches multiple
-        directories in order and finds the file in the second directory."""
-        src_dir = os.path.join(self.getSourceDir(), os.pardir, "source-map")
-        yaml_path = os.path.join(src_dir, "a.yaml")
-        obj_path = self.getBuildArtifact("main.o")
-        self.yaml2obj(yaml_path, obj_path)
-
-        target = self.dbg.CreateTarget(obj_path)
-        self.assertTrue(target, VALID_TARGET)
-
-        # Create two relocated directories. Only the second one has the file.
-        empty_dir = os.path.join(self.getBuildDir(), "empty_relocated")
-        os.makedirs(empty_dir, exist_ok=True)
-
-        real_dir = os.path.join(self.getBuildDir(), "real_relocated")
-        real_trivial = os.path.join(real_dir, "Trivial")
-        os.makedirs(real_trivial, exist_ok=True)
-        shutil.copy(
-            os.path.join(src_dir, "Trivial", "main.c"),
-            os.path.join(real_trivial, "main.c"),
-        )
-
-        # Set source-file-search-paths to both directories (space-separated).
-        self.runCmd(
-            'settings set target.source-file-search-paths "%s" "%s"'
-            % (empty_dir, real_dir)
-        )
-
-        # The suffix matcher should skip empty_dir and find the file in
-        # real_dir.
-        retval = lldb.SBCommandReturnObject()
-        self.dbg.GetCommandInterpreter().HandleCommand(
-            "source list -f main.c -l 1", retval
-        )
-        self.assertTrue(
-            retval.Succeeded(),
-            "source list should succeed with multiple source-file-search-paths",
-        )
-        self.assertIn(
-            "return",
-            retval.GetOutput(),
-            "source list should show the source file contents",
-        )
-
-        # Verify the auto-created source mapping points to real_dir.
-        retval = lldb.SBCommandReturnObject()
-        self.dbg.GetCommandInterpreter().HandleCommand(
-            "settings show target.source-map", retval
-        )
-        self.assertIn(
-            real_dir,
-            retval.GetOutput(),
-            "source mapping should reference the second directory",
+        self.assertGreater(bytes, 0, "should find foo.cpp")
+        self.assertRegex(
+            stream.GetData(),
+            ".*result = add.*",
+            "couldn't find the expected source lines",
         )
