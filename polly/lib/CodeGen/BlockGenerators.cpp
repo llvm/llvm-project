203fc3cf414f6341182432de3a6455b94068a9e9
Michael Kruse <llvm-project@meinersbur.de>
2026-04-02 12:40:43 +0200

    Build fix

--- polly/lib/CodeGen/BlockGenerators.cpp
+++ polly/lib/CodeGen/BlockGenerators.cpp
@@ -48,7 +48,6 @@ static cl::opt<bool, true> TraceStmtsX(
     cl::desc("Add printf calls that print the statement being executed"),
     cl::location(TraceStmts), cl::Hidden, cl::cat(PollyCategory));
 
-bool TraceStmts;
 static cl::opt<bool> TraceScalars(
     "polly-codegen-trace-scalars",
     cl::desc("Add printf calls that print the values of all scalar values "
