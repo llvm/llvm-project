04f35464ca434edbb6c9f571ba0daa946426d681
Danil Sidoruk <kWppyjHVn4@levpegus.ru>
2026-04-03 18:03:31 +0300

    Fix test

--- clang/test/C/C89/c99_functions.c
+++ clang/test/C/C89/c99_functions.c
@@ -6,10 +6,9 @@
 int logf = 5;
 
 // But log does
-int log = 6;
-// expected-error {{redefinition of 'log' as different kind of symbol}}
-// expected-note {{unguarded header; consider using #ifdef guards or #pragma once}}
-// expected-note {{previous definition is here}}
+int log = 6; // expected-error {{redefinition of 'log' as different kind of symbol}} \
+		expected-note {{unguarded header; consider using #ifdef guards or #pragma once}} \
+		expected-note {{previous definition is here}}
 
 int main() {
     return logf;
