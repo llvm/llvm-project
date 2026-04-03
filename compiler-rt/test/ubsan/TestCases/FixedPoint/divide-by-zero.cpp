4e6cd438df92b69419f7ec2ef1322325837b9012
Victor Chernyakin <chernyakin.victor.j@outlook.com>
2026-04-03 05:17:51 +0000

    Adjust tests

--- compiler-rt/test/ubsan/TestCases/FixedPoint/divide-by-zero.cpp
+++ compiler-rt/test/ubsan/TestCases/FixedPoint/divide-by-zero.cpp
@@ -1,8 +1,14 @@
-// RUN: %clangxx -DOP="0.0R / 0"                 -O3 -ffixed-point -fsanitize=fixed-point-divide-by-zero %s -o %t1 && %run %t1 2>&1 | FileCheck %s
-// RUN: %clangxx -DOP="0.5R / 0"                 -O3 -ffixed-point -fsanitize=fixed-point-divide-by-zero %s -o %t2 && %run %t2 2>&1 | FileCheck %s
-// RUN: %clangxx -DOP="0.0R / 0.0R"              -O3 -ffixed-point -fsanitize=fixed-point-divide-by-zero %s -o %t3 && %run %t3 2>&1 | FileCheck %s
-// RUN: %clangxx -DOP="0.5R / 0.0R"              -O3 -ffixed-point -fsanitize=fixed-point-divide-by-zero %s -o %t4 && %run %t4 2>&1 | FileCheck %s
-// RUN: %clangxx -DOP="_Accum a = 10.0K; a /= 0" -O3 -ffixed-point -fsanitize=fixed-point-divide-by-zero %s -o %t5 && %run %t5 2>&1 | FileCheck %s
+// RUN: %clangxx -ffixed-point -fsanitize=fixed-point-divide-by-zero %s -o %t1 -DOP="0.0R / 0"
+// RUN: %clangxx -ffixed-point -fsanitize=fixed-point-divide-by-zero %s -o %t2 -DOP="0.5R / 0"
+// RUN: %clangxx -ffixed-point -fsanitize=fixed-point-divide-by-zero %s -o %t3 -DOP="0.0R / 0.0R"
+// RUN: %clangxx -ffixed-point -fsanitize=fixed-point-divide-by-zero %s -o %t4 -DOP="0.5R / 0.0R"
+// RUN: %clangxx -ffixed-point -fsanitize=fixed-point-divide-by-zero %s -o %t5 -DOP="_Accum a = 10.0K; a /= 0"
+
+// RUN: %env_ubsan_opts=halt_on_error=1 not %run %t1 2>&1 | FileCheck %s
+// RUN: %env_ubsan_opts=halt_on_error=1 not %run %t2 2>&1 | FileCheck %s
+// RUN: %env_ubsan_opts=halt_on_error=1 not %run %t3 2>&1 | FileCheck %s
+// RUN: %env_ubsan_opts=halt_on_error=1 not %run %t4 2>&1 | FileCheck %s
+// RUN: %env_ubsan_opts=halt_on_error=1 not %run %t5 2>&1 | FileCheck %s
 
 int main() {
   // CHECK: divide-by-zero.cpp:[[@LINE+1]]:3: runtime error: division by zero
