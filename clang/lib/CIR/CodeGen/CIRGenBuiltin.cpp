3781197a4ec22b35115d5c64aefce7b13d521f2a
Yeongu Choe <yeongu.choe@icloud.com>
2026-04-06 17:41:55 -0400

    [CIR][CodeGen] Implement __builtin_fpclassify (#187977)
    I implemented CIR version of __builtin_fpclassify function.

--- clang/lib/CIR/CodeGen/CIRGenBuiltin.cpp
+++ clang/lib/CIR/CodeGen/CIRGenBuiltin.cpp
@@ -1582,8 +1582,86 @@ RValue CIRGenFunction::emitBuiltinExpr(const GlobalDecl &gd, unsigned builtinID,
   }
   case Builtin::BI__builtin_flt_rounds:
   case Builtin::BI__builtin_set_flt_rounds:
-  case Builtin::BI__builtin_fpclassify:
     return errorBuiltinNYI(*this, e, builtinID);
+  case Builtin::BI__builtin_fpclassify: {
+    CIRGenFunction::CIRGenFPOptionsRAII fPOptsRAII(*this, e);
+    mlir::Location loc = getLoc(e->getBeginLoc());
+    mlir::Value value = emitScalarExpr(e->getArg(5));
+    mlir::Type resultTy = convertType(e->getType());
+    // if isZero then
+    //     result = FP_ZERO
+    // elseif isNan then
+    //     result = FP_NAN
+    // elseif isInfinity then
+    //     result = FP_INFINITE
+    // elseif isNormal then
+    //     result = FP_NORMAL
+    // else
+    //     result = FP_SUBNORMAL
+    auto isZero =
+        cir::IsFPClassOp::create(builder, loc, value, cir::FPClassTest::Zero);
+    mlir::Value result =
+        cir::TernaryOp::create(
+            builder, loc, isZero,
+            /*thenBuilder=*/
+            [&](mlir::OpBuilder &opBuilder, mlir::Location location) {
+              mlir::Value zeroLiteral = emitScalarExpr(e->getArg(4));
+              cir::YieldOp::create(opBuilder, location, zeroLiteral);
+            },
+            /*elseBuilder=*/
+            [&](mlir::OpBuilder &opBuilder, mlir::Location location) {
+              auto isNan = cir::IsFPClassOp::create(opBuilder, location, value,
+                                                    cir::FPClassTest::Nan);
+              mlir::Value nanResult =
+                  cir::TernaryOp::create(
+                      opBuilder, location, isNan,
+                      /*thenBuilder=*/
+                      [&](mlir::OpBuilder &opBuilder, mlir::Location location) {
+                        mlir::Value nanLiteral = emitScalarExpr(e->getArg(0));
+                        cir::YieldOp::create(opBuilder, location, nanLiteral);
+                      },
+                      /*elseBuilder=*/
+                      [&](mlir::OpBuilder &opBuilder, mlir::Location location) {
+                        auto isInfinity = cir::IsFPClassOp::create(
+                            opBuilder, location, value,
+                            cir::FPClassTest::Infinity);
+                        mlir::Value infResult =
+                            cir::TernaryOp::create(
+                                opBuilder, location, isInfinity,
+                                /*thenBuilder=*/
+                                [&](mlir::OpBuilder &opBuilder,
+                                    mlir::Location location) {
+                                  mlir::Value infinityLiteral =
+                                      emitScalarExpr(e->getArg(1));
+                                  cir::YieldOp::create(opBuilder, location,
+                                                       infinityLiteral);
+                                },
+                                /*elseBuilder=*/
+                                [&](mlir::OpBuilder &opBuilder,
+                                    mlir::Location location) {
+                                  auto isNormal = cir::IsFPClassOp::create(
+                                      opBuilder, location, value,
+                                      cir::FPClassTest::Normal);
+                                  mlir::Value fpNormal =
+                                      emitScalarExpr(e->getArg(2));
+                                  mlir::Value fpSubnormal =
+                                      emitScalarExpr(e->getArg(3));
+                                  mlir::Value returnValue =
+                                      cir::SelectOp::create(
+                                          opBuilder, location, resultTy,
+                                          isNormal, fpNormal, fpSubnormal);
+                                  cir::YieldOp::create(opBuilder, location,
+                                                       returnValue);
+                                })
+                                .getResult();
+                        cir::YieldOp::create(opBuilder, location, infResult);
+                      })
+                      .getResult();
+              cir::YieldOp::create(opBuilder, location, nanResult);
+            })
+            .getResult();
+    return RValue::get(result);
+  }
   case Builtin::BIalloca:
   case Builtin::BI_alloca:
   case Builtin::BI__builtin_alloca_uninitialized:
