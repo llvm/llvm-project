5cdbab87fbee50739c37f34cf617a4ceefaf3558
Zhen Wang <zhenw@nvidia.com>
2026-04-06 17:05:46 -0700

    Do not use double descriptor for unified allocatables

--- flang/lib/Lower/CUDA.cpp
+++ flang/lib/Lower/CUDA.cpp
@@ -120,7 +120,8 @@ bool Fortran::lower::hasDoubleDescriptor(mlir::Value addr) {
     if (mlir::isa_and_nonnull<fir::AddrOfOp>(
             declareOp.getMemref().getDefiningOp())) {
       if (declareOp.getDataAttr() &&
-          *declareOp.getDataAttr() == cuf::DataAttribute::Pinned)
+          (*declareOp.getDataAttr() == cuf::DataAttribute::Pinned ||
+           *declareOp.getDataAttr() == cuf::DataAttribute::Unified))
         return false;
       return true;
     }
