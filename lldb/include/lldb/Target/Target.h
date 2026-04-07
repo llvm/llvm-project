e45eae999cb3c088db305d480628ab4b2cc04a10
Michael Buch <michaelbuch12@gmail.com>
2026-04-07 14:34:32 +0100

    fixup! add doxygen comments for setters

--- lldb/include/lldb/Target/Target.h
+++ lldb/include/lldb/Target/Target.h
@@ -239,6 +239,9 @@ public:
 
   LoadScriptFromSymFile GetLoadScriptFromSymbolFile() const;
 
+  /// Set the target-wide target.load-script-from-symbol-file setting.
+  /// See \c SetAutoLoadScriptsForModule for overriding this setting
+  /// per-module.
   void SetLoadScriptFromSymbolFile(LoadScriptFromSymFile load_style);
 
   LoadCWDlldbinitFile GetLoadCWDlldbinitFile() const;
@@ -284,6 +287,10 @@ public:
   std::optional<LoadScriptFromSymFile>
   GetAutoLoadScriptsForModule(llvm::StringRef module_name) const;
 
+  /// Set the \c LoadScriptFromSymFile for a module called \c module_name
+  /// (excluding file extension). LLDB will prefer this over the target-wide
+  /// target.load-script-from-symbol-file setting
+  /// (see \c SetLoadScriptFromSymbolFile).
   void SetAutoLoadScriptsForModule(llvm::StringRef module_name,
                                    LoadScriptFromSymFile load_style);
 
