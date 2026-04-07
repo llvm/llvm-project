92c743db54ee9f0e6fddabda8016a37b6a653b8e
Schrodinger ZHU Yifan <i@zhuyi.fan>
2026-04-07 16:36:58 -0400

    header clean up

--- libc/src/stdlib/baremetal/abort_utils.h
+++ libc/src/stdlib/baremetal/abort_utils.h
@@ -1,4 +1,4 @@
-//===-- Internal header for baremetal abort ----------------------*- C++ -*-===//
+//===-- Internal header for baremetal abort -------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
