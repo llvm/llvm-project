9d8fe5c8a1688ee0ce85d93a5f922b3ae5eb503b
Fernando Pelliccioni <fpelliccioni@gmail.com>
2026-04-07 14:04:22 +0200

    [libc++] Address review: add LWG4477 tests for placement operator delete

--- libcxx/test/std/language.support/support.dynamic/new.delete/new.delete.placement/new_array.pass.cpp
+++ libcxx/test/std/language.support/support.dynamic/new.delete/new.delete.placement/new_array.pass.cpp
@@ -27,6 +27,10 @@ TEST_CONSTEXPR_OPERATOR_NEW void test_direct_call() {
   char ch = '*';
   assert(::operator new[](1, &ch) == &ch);
   assert(ch == '*');
+
+  // LWG4477: placement operator delete[] is a no-op and must be usable
+  // in constant expressions.
+  ::operator delete[](&ch, &ch);
 }
 
 #ifdef __cpp_lib_constexpr_new
