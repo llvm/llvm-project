348d55314f949fadb4f82e8ec820704866c45643
Paul Walker <paul.walker@arm.com>
2026-04-08 11:48:24 +0100

    [LLVM][CodeGen][AArch64] Improve generated code for SVE VLS truncates. (#190778)
    When SVE VLS is enabled we request custom lowering for all ISD::TRUNCATE
    operations involving legal types. However, we only custom lower all of
    them when NEON is not available and so there are variants that do not
    require SVE and can be done via NEON but we are instead falling back to
    default expansion, which is this case means scalarisation. This patch
    updates custom lowering to mark the variants that have isel patterns
    available.

--- llvm/lib/Target/AArch64/AArch64ISelLowering.cpp
+++ llvm/lib/Target/AArch64/AArch64ISelLowering.cpp
@@ -17301,6 +17301,10 @@ SDValue AArch64TargetLowering::LowerTRUNCATE(SDValue Op,
                                    !Subtarget->isNeonAvailable()))
     return LowerFixedLengthVectorTruncateToSVE(Op, DAG);
 
+  // We can select these directly.
+  if (VT.is64BitVector() && Op.getOperand(0).getValueType().is128BitVector())
+    return Op;
+
   return SDValue();
 }
 
