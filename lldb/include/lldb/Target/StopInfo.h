9b5183376e9a63437e52a39d64126962df8f8456
Philip DePetro <pdepetro@meta.com>
2026-04-08 12:45:29 -0700

    Revert "[lldb] Handle forking in expressions"
    This reverts commit ef1fe58c7bc375bb1eb11df926242a82836082d1.

--- lldb/include/lldb/Target/StopInfo.h
+++ lldb/include/lldb/Target/StopInfo.h
@@ -21,7 +21,6 @@ class StopInfo : public std::enable_shared_from_this<StopInfo> {
   friend class Process::ProcessEventData;
   friend class ThreadPlanBase;
   friend class ThreadPlanReverseContinue;
-  friend class ThreadPlanCallFunction;
 
 public:
   // Constructors and Destructors
