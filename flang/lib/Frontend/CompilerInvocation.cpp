4a9f5a7d2c77efd3b609e7d98181697fa4185a48
Michael Klemm <michael.klemm@amd.com>
2026-04-08 12:47:25 +0200

    Make clang-format happy

--- flang/lib/Frontend/CompilerInvocation.cpp
+++ flang/lib/Frontend/CompilerInvocation.cpp
@@ -1037,7 +1037,7 @@ static bool parseDiagArgs(CompilerInvocation &res, llvm::opt::ArgList &args,
   // this has to change when other -W<opt>'s are supported.
   if (args.hasArg(clang::options::OPT_W_Joined)) {
     const auto &wArgs = args.getAllArgValues(clang::options::OPT_W_Joined);
-     // TODO: Consider using std::string_view when moving to C++
+    // TODO: Consider using std::string_view when moving to C++
     for (const llvm::StringRef wArg : wArgs) {
       if (wArg == "error") {
         res.setWarnAsErr(true);
