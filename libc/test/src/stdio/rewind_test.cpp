ad9b00bb42b32f9fd366a323bd32c07bc3c1a4b2
Michael Jones <michaelrj@google.com>
2026-04-09 23:27:28 +0000

    address comments and simplify tests

--- libc/test/src/stdio/rewind_test.cpp
+++ libc/test/src/stdio/rewind_test.cpp
@@ -17,9 +17,7 @@
 #include "test/UnitTest/Test.h"
 
 using LlvmLibcRewindTest = LIBC_NAMESPACE::testing::ErrnoCheckingTest;
-using LIBC_NAMESPACE::testing::ErrnoSetterMatcher::EQ;
-using LIBC_NAMESPACE::testing::ErrnoSetterMatcher::NE;
-using LIBC_NAMESPACE::testing::ErrnoSetterMatcher::returns;
+using LIBC_NAMESPACE::testing::ErrnoSetterMatcher::Succeeds;
 
 TEST_F(LlvmLibcRewindTest, WriteRewindRead) {
   constexpr char FILENAME[] = APPEND_LIBC_TEST("testdata/rewind.test");
@@ -29,7 +27,7 @@ TEST_F(LlvmLibcRewindTest, WriteRewindRead) {
 
   constexpr char FIRST_DATA[] = "123456789";
   ASSERT_THAT(LIBC_NAMESPACE::fwrite(FIRST_DATA, 1, sizeof(FIRST_DATA), file),
-              returns(EQ(sizeof(FIRST_DATA))).with_errno(EQ(0)));
+              Succeeds(sizeof(FIRST_DATA)));
 
   // File state is "123456789"
 
@@ -40,14 +38,15 @@ TEST_F(LlvmLibcRewindTest, WriteRewindRead) {
   constexpr char SECOND_DATA[] = "abc";
   ASSERT_THAT(
       LIBC_NAMESPACE::fwrite(SECOND_DATA, 1, sizeof(SECOND_DATA) - 1, file),
-      returns(EQ(sizeof(SECOND_DATA) - 1)).with_errno(EQ(0)));
+      Succeeds(sizeof(SECOND_DATA) - 1));
 
   // File state is "abc456789"
 
   // attempt to read from write-only file causing error state
   char read_data[sizeof(FIRST_DATA)];
-  ASSERT_THAT(LIBC_NAMESPACE::fread(read_data, 1, sizeof(read_data), file),
-              returns(EQ(size_t(0))).with_errno(NE(0)));
+  ASSERT_EQ(LIBC_NAMESPACE::fread(read_data, 1, sizeof(read_data), file),
+            size_t(0));
+  ASSERT_ERRNO_FAILURE();
   ASSERT_NE(LIBC_NAMESPACE::ferror(file), 0);
 
   // rewind to start and check that that clears the error.
@@ -61,7 +60,7 @@ TEST_F(LlvmLibcRewindTest, WriteRewindRead) {
 
   // Read the file to check that it was written correctly.
   ASSERT_THAT(LIBC_NAMESPACE::fread(read_data, 1, 3, file),
-              returns(EQ(size_t(3))).with_errno(EQ(0)));
+              Succeeds(size_t(3)));
   read_data[3] = '\0';
   ASSERT_STREQ(read_data, "abc");
   ASSERT_EQ(LIBC_NAMESPACE::ferror(file), 0);
@@ -69,7 +68,7 @@ TEST_F(LlvmLibcRewindTest, WriteRewindRead) {
   // check that rewind also works on read files.
   LIBC_NAMESPACE::rewind(file);
   ASSERT_THAT(LIBC_NAMESPACE::fread(read_data, 1, sizeof(read_data), file),
-              returns(EQ(sizeof(FIRST_DATA))).with_errno(EQ(0)));
+              Succeeds(sizeof(FIRST_DATA)));
   read_data[sizeof(FIRST_DATA) - 1] = '\0';
   ASSERT_STREQ(read_data, "abc456789");
 
