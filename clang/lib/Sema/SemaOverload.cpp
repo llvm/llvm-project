651d626e79e28ca5d5c51cb49e17452324016a5a
Henrik G. Olsson <hnrklssn@gmail.com>
2026-04-09 22:17:20 -0700

    emplace_back
    Co-authored-by: Victor Chernyakin <chernyakin.victor.j@outlook.com>

--- clang/lib/Sema/SemaOverload.cpp
+++ clang/lib/Sema/SemaOverload.cpp
@@ -13889,8 +13889,7 @@ private:
       // If we're in C, we need to support types that aren't exactly identical.
       if (!S.getLangOpts().CPlusPlus ||
           candidateHasExactlyCorrectType(FunDecl)) {
-        Matches.push_back(
-            std::make_pair(CurAccessFunPair, FunDecl->getCanonicalDecl()));
+        Matches.emplace_back(CurAccessFunPair, FunDecl->getCanonicalDecl());
         FoundNonTemplateFunction = true;
         return true;
       }
