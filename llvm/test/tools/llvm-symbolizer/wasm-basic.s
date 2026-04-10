9e1d844c816817e710f1b0112c18d387c9187fd4
Derek Schuff <dschuff@chromium.org>
2026-04-10 15:50:17 +0000

    review comments

--- llvm/test/tools/llvm-symbolizer/wasm-basic.s
+++ llvm/test/tools/llvm-symbolizer/wasm-basic.s
@@ -1,11 +1,11 @@
 # REQUIRES: webassembly-registered-target, lld
 # RUN: llvm-mc -triple=wasm32-unknown-unknown -filetype=obj %s -o %t.o -g
-# RUN: llvm-symbolizer --basenames --output-style=GNU -e %t.o 0 1 2 3 4 5 6 7 8 9 10 11 12 13 0x44 | FileCheck %s
+# RUN: llvm-symbolizer --basenames --output-style=GNU -e %t.o 0 1 2 3 4 5 6 7 8 9 10 11 12 13 16 | FileCheck %s
 
-# This is the same test but on a linked wasm file, using file offsets rather
-# than section offsets.
+# This is the same test but on a linked wasm file, using file offsets rather than section offsets.
+# The code section starts at 0x41, so these are equivalent (see below for why the first one is 3).
 # RUN: wasm-ld %t.o -o %t.wasm --no-entry --export=foo --export=bar
-# RUN: llvm-symbolizer --basenames --output-style=GNU -e %t.wasm 3 0x42 0x43 0x44 0x45 0x46 0x47 0x48 0x49 0x4a 0x4b 0x4c 0x4d 0x4e 0x100 | FileCheck %s
+# RUN: llvm-symbolizer --basenames --output-style=GNU -e %t.wasm 3 0x42 0x43 0x44 0x45 0x46 0x47 0x48 0x49 0x4a 0x4b 0x4c 0x4d 0x4e 0x5e | FileCheck %s
 
 .globl foo
 foo:
@@ -27,7 +27,9 @@ bar:
 ## way to the next symbol start.
 ## TODO: create a loc for .functype? It could go with the local declarations.
 
-## Byte 0 (or 3, in the case of a linked file) never maps to a code section
+## For objects, byte 0 doesn't map to a function body.
+## For the linked file, 3 is chosen because if incorrectly interpreted as a
+## section offset, it will match a line.
 # CHECK: ??
 # CHECK-NEXT: ??:0
 
@@ -62,13 +64,13 @@ bar:
 ## Byte 10 is the nop
 # CHECK-NEXT: bar
 # CHECK-NEXT: wasm-basic.s:21
-## Byte b is the return
+## Byte 11 is the return
 # CHECK-NEXT: bar
 # CHECK-NEXT: wasm-basic.s:22
-## Byte c is end_function
+## Byte 12 is end_function
 # CHECK-NEXT: bar
 # CHECK-NEXT: wasm-basic.s:23
-## Byte d is past the end of the function
+## Byte 13 is past the end of the function
 # CHECK-NEXT: ??
 # CHECK-NEXT: ??:0
 
