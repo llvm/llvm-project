0d15f532274dc06bf85a975a7b5ab505ab120d33
skc7 <Krishna.Sankisa@amd.com>
2026-04-10 11:31:59 +0530

    add todo test

--- mlir/lib/Target/LLVMIR/Dialect/OpenMP/OpenMPToLLVMIRTranslation.cpp
+++ mlir/lib/Target/LLVMIR/Dialect/OpenMP/OpenMPToLLVMIRTranslation.cpp
@@ -519,9 +519,15 @@ static LogicalResult checkImplementationStatus(Operation &op) {
       result = todo("num_teams with multi-dimensional values");
   };
   auto checkNumThreads = [&todo](auto op, LogicalResult &result) {
-    // Check that we don't exceed the maximum supported dimensions (3)
-    if (op.getNumThreadsDimsCount() > 3)
+    if (op.getNumThreadsDimsCount() > 3) {
       result = todo("num_threads with more than 3 dimensions");
+      return;
+    }
+
+    if (op.hasNumThreadsMultiDim() &&
+        !op->template getParentOfType<omp::TargetOp>())
+      result = todo(
+          "num_threads with multi-dimensional values outside target region");
   };
 
   auto checkThreadLimit = [&todo](auto op, LogicalResult &result) {
@@ -8950,8 +8956,9 @@ static void extractHostEvalClauses(
                   break;
                 }
               }
-            } else
+            } else {
               llvm_unreachable("unsupported host_eval use");
+            }
           })
           .Case([&](omp::LoopNestOp loopOp) {
             auto processBounds =
