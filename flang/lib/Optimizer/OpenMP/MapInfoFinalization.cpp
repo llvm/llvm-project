eaa842f9928db64ccb6a927cc8e9318dfbe9ddaa
agozillon <Andrew.Gozillon@amd.com>
2026-04-10 03:17:16 +0200

    [Flang][OpenMP] Fix assertion found in LCOMPILER-1645 (#1885)
    This PR attempts to address the problem of running into a scenario where
    we have a map that isn't bound to a target yet when performing a step
    that requires this.
    
    This occurs primarily with dervied type member mapping.
    
    There's two methods we can go with to fix this, temporarily we can emit
    a return alongside a TODO as the triggering reproducer isn't an issue if
    we do this.
    
    Or we can instead process our map <-> target bindings earlier, or in
    this case twice, once at the start of the pass and once at the end.
    Making sure we enter the pass with all maps bound to the relevant target
    and end with new additions also bound to the target.
    
    I've opted for the bottom method in this initial fix.

--- flang/lib/Optimizer/OpenMP/MapInfoFinalization.cpp
+++ flang/lib/Optimizer/OpenMP/MapInfoFinalization.cpp
@@ -1225,6 +1225,12 @@ public:
       deferrableDesc.clear();
       expandedBaseAddr.clear();
 
+      func->walk([&](mlir::omp::MapInfoOp op) {
+        mlir::Operation *targetUser = getFirstTargetUser(op);
+        assert(targetUser && "expected user of map operation was not found");
+        addImplicitMembersToTarget(op, builder, targetUser);
+      });
+
       // Next, walk `omp.map.info` ops to see if any record members should be
       // implicitly mapped.
       // TODO/FIXME/UPDATE: I believe we need to add implicit capture of
@@ -1259,7 +1265,8 @@ public:
 
         int64_t mapVarIdx = mapClauseOwner.getOperandIndexForMap(op);
         assert(mapVarIdx >= 0 &&
-               mapVarIdx < static_cast<int64_t>(mapClauseOwner.getMapVars().size()));
+               mapVarIdx <
+                   static_cast<int64_t>(mapClauseOwner.getMapVars().size()));
 
         auto argIface =
             llvm::dyn_cast<mlir::omp::BlockArgOpenMPOpInterface>(*target);
@@ -1441,9 +1448,47 @@ public:
         // this pass to support multiple users, as we may wish to have a map
         // be re-used by multiple users (e.g. across multiple targets that map
         // the variable and have identical map properties).
-        assert(llvm::hasSingleElement(op->getUsers()) &&
-               "OMPMapInfoFinalization currently only supports single users "
-               "of a MapInfoOp");
+        auto assertCheck = [&](mlir::omp::MapInfoOp op) {
+          if (llvm::hasSingleElement(op->getUsers()))
+            return true;
+
+          if (llvm::range_size(op->getUsers()) > 2)
+            return false;
+
+          // We only allow a TargetOp or MapInfoOp when we have multiple users
+          // for the moment.
+          bool targetUser = false;
+          for (auto *user : op->getUsers()) {
+            if (!llvm::isa<
+                    mlir::omp::TargetOp, mlir::omp::TargetDataOp,
+                    mlir::omp::TargetUpdateOp, mlir::omp::TargetExitDataOp,
+                    mlir::omp::TargetEnterDataOp,
+                    mlir::omp::DeclareMapperInfoOp, mlir::omp::MapInfoOp>(
+                    user))
+              return false;
+
+
+            // We do not handle multiple target users currently.
+            if (targetUser &&
+                llvm::isa<mlir::omp::TargetDataOp, mlir::omp::TargetUpdateOp,
+                          mlir::omp::TargetExitDataOp,
+                          mlir::omp::TargetEnterDataOp>(user))
+              return false;
+
+            if (!targetUser)
+              targetUser =
+                  llvm::isa<mlir::omp::TargetDataOp, mlir::omp::TargetUpdateOp,
+                            mlir::omp::TargetExitDataOp,
+                            mlir::omp::TargetEnterDataOp>(user);
+          }
+
+          return true;
+        };
+
+        assert(assertCheck(op) &&
+               "OMPMapInfoFinalization currently only supports "
+               "single users or up to two users when those users"
+               "are a MapInfoOp and Target mapping directive");
 
         if (hasADescriptor(op.getVarPtr().getDefiningOp(),
                            fir::unwrapRefType(op.getVarPtrType()))) {
