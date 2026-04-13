33cbf356c2720347a06e4c24730780a5e0666a7e
Jeff Bailey <jbailey@raspberryginger.com>
2026-04-13 18:04:10 +0100

    [libc] Rename x86_64 register enum values for glibc compatibility
    Updated the register enum in include/llvm-libc-types/x86_64/mcontext_t.h to use names compatible with glibc's sys/ucontext.h (e.g., REG_R8).
    
    Added corresponding #define directives for each register.

--- libc/include/llvm-libc-types/x86_64/mcontext_t.h
+++ libc/include/llvm-libc-types/x86_64/mcontext_t.h
@@ -25,29 +25,52 @@ typedef long long int greg_t;
 typedef greg_t gregset_t[23];
 
 enum {
-  __LIBC_REG_R8 = 0,
-  __LIBC_REG_R9,
-  __LIBC_REG_R10,
-  __LIBC_REG_R11,
-  __LIBC_REG_R12,
-  __LIBC_REG_R13,
-  __LIBC_REG_R14,
-  __LIBC_REG_R15,
-  __LIBC_REG_RDI,
-  __LIBC_REG_RSI,
-  __LIBC_REG_RBP,
-  __LIBC_REG_RBX,
-  __LIBC_REG_RDX,
-  __LIBC_REG_RAX,
-  __LIBC_REG_RCX,
-  __LIBC_REG_RSP,
-  __LIBC_REG_RIP,
-  __LIBC_REG_EFL,
-  __LIBC_REG_CSGSFS,
-  __LIBC_REG_ERR,
-  __LIBC_REG_TRAPNO,
-  __LIBC_REG_OLDMASK,
-  __LIBC_REG_CR2
+  REG_R8 = 0,
+#define REG_R8 REG_R8
+  REG_R9,
+#define REG_R9 REG_R9
+  REG_R10,
+#define REG_R10 REG_R10
+  REG_R11,
+#define REG_R11 REG_R11
+  REG_R12,
+#define REG_R12 REG_R12
+  REG_R13,
+#define REG_R13 REG_R13
+  REG_R14,
+#define REG_R14 REG_R14
+  REG_R15,
+#define REG_R15 REG_R15
+  REG_RDI,
+#define REG_RDI REG_RDI
+  REG_RSI,
+#define REG_RSI REG_RSI
+  REG_RBP,
+#define REG_RBP REG_RBP
+  REG_RBX,
+#define REG_RBX REG_RBX
+  REG_RDX,
+#define REG_RDX REG_RDX
+  REG_RAX,
+#define REG_RAX REG_RAX
+  REG_RCX,
+#define REG_RCX REG_RCX
+  REG_RSP,
+#define REG_RSP REG_RSP
+  REG_RIP,
+#define REG_RIP REG_RIP
+  REG_EFL,
+#define REG_EFL REG_EFL
+  REG_CSGSFS,
+#define REG_CSGSFS REG_CSGSFS
+  REG_ERR,
+#define REG_ERR REG_ERR
+  REG_TRAPNO,
+#define REG_TRAPNO REG_TRAPNO
+  REG_OLDMASK,
+#define REG_OLDMASK REG_OLDMASK
+  REG_CR2
+#define REG_CR2 REG_CR2
 };
 
 // The following structures (_libc_fpxreg, _libc_xmmreg, _libc_fpstate)
