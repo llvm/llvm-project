a2339f5e6250d00150ef305087cd5d788c7858d5
Schrodinger ZHU Yifan <i@zhuyi.fan>
2026-04-13 09:54:27 -0400

    [NFC] cleanup

--- libc/src/__support/threads/mutex.h
+++ libc/src/__support/threads/mutex.h
@@ -22,7 +22,7 @@
 //
 // MutexError lock();
 // MutexError trylock();
-// MutexError timedlock(...);
+// MutexError timed_lock(...);
 // MutexError unlock();
 // MutexError reset(); // Used to reset inconsistent robust mutexes.
 //
@@ -54,12 +54,14 @@ namespace LIBC_NAMESPACE_DECL {
 /// complete Mutex locks in general cannot be implemented on the GPU, or on some
 /// baremetal platforms. We simply define the Mutex interface and require that
 /// only a single thread executes code requiring a mutex lock.
+// TODO: declare abstract interface for timed_lock
 struct Mutex {
   LIBC_INLINE constexpr Mutex(bool, bool, bool, bool) {}
 
   LIBC_INLINE MutexError lock() { return MutexError::NONE; }
   LIBC_INLINE MutexError unlock() { return MutexError::NONE; }
   LIBC_INLINE MutexError reset() { return MutexError::NONE; }
+  LIBC_INLINE MutexError trylock() { return MutexError::NONE; }
 };
 
 } // namespace LIBC_NAMESPACE_DECL
