04ba59b96f758dfa1754b87494df6f4cb387a591
mtx <mitchell.xu2@gmail.com>
2026-04-13 14:44:39 +0800

    fix tests

--- clang-tools-extra/test/clang-tidy/checkers/fuchsia/temporary-objects-deprecated-alias.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/fuchsia/temporary-objects-deprecated-alias.cpp
@@ -1,11 +1,9 @@
-// RUN: clang-tidy %s -checks='-*,zircon-temporary-objects' \
-// RUN:   -config="{CheckOptions: {zircon-temporary-objects.Names: 'Foo'}}" \
-// RUN:   -header-filter='.*' -- 2>&1 | FileCheck %s --check-prefix=CHECK-OLD
-// RUN: clang-tidy %s -checks='-*,zircon-temporary-objects,fuchsia-temporary-objects' \
+// RUN: %check_clang_tidy %s zircon-temporary-objects %t -- \
+// RUN:   -config="{CheckOptions: {zircon-temporary-objects.Names: 'Foo'}}"
+// RUN: %check_clang_tidy -check-suffix=BOTH %s \
+// RUN:   zircon-temporary-objects,fuchsia-temporary-objects %t -- \
 // RUN:   -config="{CheckOptions: {zircon-temporary-objects.Names: 'Foo', \
-// RUN:                            fuchsia-temporary-objects.Names: 'Foo'}}" \
-// RUN:   -header-filter='.*' -- 2>&1 | FileCheck %s --check-prefix=CHECK-BOTH \
-// RUN:   -implicit-check-not='deprecated and will be removed in a future release'
+// RUN:                            fuchsia-temporary-objects.Names: 'Foo'}}"
 
 class Foo {
 public:
@@ -14,7 +12,7 @@ public:
 
 void f() {
   Foo();
-  // CHECK-OLD: warning: zircon-temporary-objects is deprecated and will be removed in a future release; consider using fuchsia-temporary-objects instead [clang-tidy-config]
-  // CHECK-OLD: :[[@LINE-2]]:3: warning: creating a temporary object of type 'Foo' is prohibited [zircon-temporary-objects]
-  // CHECK-BOTH: :[[@LINE-3]]:3: warning: creating a temporary object of type 'Foo' is prohibited
+  // CHECK-MESSAGES: warning: zircon-temporary-objects is deprecated and will be removed in a future release; consider using fuchsia-temporary-objects instead [clang-tidy-config]
+  // CHECK-MESSAGES: :[[@LINE-2]]:3: warning: creating a temporary object of type 'Foo' is prohibited [zircon-temporary-objects]
+  // CHECK-MESSAGES-BOTH: :[[@LINE-3]]:3: warning: creating a temporary object of type 'Foo' is prohibited
 }
