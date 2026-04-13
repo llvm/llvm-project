d365124082dc69062632f7db68f1ce9873b8a2ab
Frederick Vu <100011202+FrederickVu@users.noreply.github.com>
2026-04-13 20:50:17 +0000

    Remove defensive undef check

--- llvm/lib/CodeGen/SelectionDAG/DAGCombiner.cpp
+++ llvm/lib/CodeGen/SelectionDAG/DAGCombiner.cpp
@@ -11589,13 +11589,11 @@ SDValue DAGCombiner::visitSRL(SDNode *N) {
       unsigned NumElts = BVVT.getVectorNumElements();
       if (N1C->getZExtValue() == (NumElts - 1) * EltSizeInBits) {
         SDValue LastElt = BV.getOperand(NumElts - 1);
-        if (!LastElt.isUndef()) {
-          EVT IntEltVT = EVT::getIntegerVT(*DAG.getContext(), EltSizeInBits);
-          LastElt = DAG.getBitcast(LastElt.getValueType().changeTypeToInteger(),
-                                   LastElt);
-          return DAG.getZExtOrTrunc(DAG.getZExtOrTrunc(LastElt, DL, IntEltVT),
-                                    DL, VT);
-        }
+        EVT IntEltVT = EVT::getIntegerVT(*DAG.getContext(), EltSizeInBits);
+        LastElt = DAG.getBitcast(LastElt.getValueType().changeTypeToInteger(),
+                                  LastElt);
+        return DAG.getZExtOrTrunc(DAG.getZExtOrTrunc(LastElt, DL, IntEltVT),
+                                  DL, VT);
       }
     }
   }
