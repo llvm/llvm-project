271e24f02e5bbf5beef56d314c656258ec052ea5
changkhothuychung <nhat7203@gmail.com>
2026-04-13 10:49:55 -0400

    update test

--- clang/test/Sema/reflection-meta-info.fail.cpp
+++ clang/test/Sema/reflection-meta-info.fail.cpp
@@ -2,20 +2,20 @@
 
 using info = decltype(^^int);
 
-struct X { int a; }; // expected-note {{candidate constructor (the implicit copy constructor) not viable: no known conversion from 'meta::info' to 'const X' for 1st argument}} \
-                     // expected-note {{candidate constructor (the implicit move constructor) not viable: no known conversion from 'meta::info' to 'X' for 1st argument}} \
+struct X { int a; }; // expected-note {{candidate constructor (the implicit copy constructor) not viable: no known conversion from 'std::meta::info' to 'const X' for 1st argument}} \
+                     // expected-note {{candidate constructor (the implicit move constructor) not viable: no known conversion from 'std::meta::info' to 'X' for 1st argument}} \
                      // expected-note {{candidate constructor (the implicit default constructor) not viable: requires 0 arguments, but 1 was provided}}
 template <typename T = X, auto ptm = &X::a>
-constexpr auto ptmOp = ((T)(^^int)).*ptm; // expected-error {{no matching conversion for C-style cast from 'meta::info' to 'X'}}
+constexpr auto ptmOp = ((T)(^^int)).*ptm; // expected-error {{no matching conversion for C-style cast from 'std::meta::info' to 'X'}}
 constexpr auto var = ptmOp<>; // expected-note {{in instantiation of variable template specialization 'ptmOp' requested here}}
 
 consteval void test()
 {
-    (^^char)++; // expected-error {{cannot increment value of type 'meta::info'}}
-    (^^short)++; // expected-error {{cannot increment value of type 'meta::info'}}
-    (^^int)++; // expected-error {{cannot increment value of type 'meta::info'}}
+    (^^char)++; // expected-error {{cannot increment value of type 'std::meta::info'}}
+    (^^short)++; // expected-error {{cannot increment value of type 'std::meta::info'}}
+    (^^int)++; // expected-error {{cannot increment value of type 'std::meta::info'}}
 
-    (^^char)--; // expected-error {{cannot decrement value of type 'meta::info'}}
-    (^^short)--; // expected-error {{cannot decrement value of type 'meta::info'}}
-    (^^int)--; // expected-error {{cannot decrement value of type 'meta::info'}}
+    (^^char)--; // expected-error {{cannot decrement value of type 'std::meta::info'}}
+    (^^short)--; // expected-error {{cannot decrement value of type 'std::meta::info'}}
+    (^^int)--; // expected-error {{cannot decrement value of type 'std::meta::info'}}
 }
