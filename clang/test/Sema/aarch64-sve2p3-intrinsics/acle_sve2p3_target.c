4ffa07e388411c382a5db9fee113af75e4f7b207
Jonathan Thackray <jonathan.thackray@arm.com>
2026-04-14 21:35:32 +0100

    fixup! Move tests

--- clang/test/Sema/aarch64-sve2p3-intrinsics/acle_sve2p3_target.c
+++ clang/test/Sema/aarch64-sve2p3-intrinsics/acle_sve2p3_target.c
@@ -13,7 +13,42 @@ svint8_t has_sve2p3_luti6(svint8x2_t table, svuint8_t indices) {
   return svluti6_s8(table, indices);
 }
 
-__attribute__((target("sve2p3")))
-svfloat32_t has_sve2p3_implied_sve2p2(svbool_t pg, svfloat16_t op) {
-  return svcvtlt_f32_f16_z(pg, op);
+__attribute__((target("sve2p3,bf16")))
+void has_sve2p3_luti6_lane_bf16(svfloat16x2_t f16_table,
+                                svbfloat16x2_t bf16_table,
+                                svuint8_t indices) {
+  (void)svluti6_lane_f16_x2(f16_table, indices, 0);
+  (void)svluti6_lane_bf16_x2(bf16_table, indices, 1);
+}
+
+__attribute__((target("sve2p3,sme")))
+svfloat16_t has_streaming_sve2p3_luti6_lane(svfloat16x2_t table,
+                                            svuint8_t indices)
+    __arm_streaming {
+  return svluti6_lane_f16_x2(table, indices, 1);
+}
+
+__attribute__((target("sve2p3,sme,bf16")))
+void has_streaming_sve2p3_luti6_lane_bf16(svfloat16x2_t f16_table,
+                                          svbfloat16x2_t bf16_table,
+                                          svuint8_t indices)
+    __arm_streaming {
+  (void)svluti6_lane_f16_x2(f16_table, indices, 1);
+  (void)svluti6_lane_bf16_x2(bf16_table, indices, 0);
+}
+
+__attribute__((target("sme2p3,sme")))
+svfloat16_t has_streaming_sme2p3_luti6_lane(svfloat16x2_t table,
+                                            svuint8_t indices)
+    __arm_streaming {
+  return svluti6_lane_f16_x2(table, indices, 0);
+}
+
+__attribute__((target("sme2p3,sme,bf16")))
+void has_streaming_sme2p3_luti6_lane_bf16(svfloat16x2_t f16_table,
+                                          svbfloat16x2_t bf16_table,
+                                          svuint8_t indices)
+    __arm_streaming {
+  (void)svluti6_lane_f16_x2(f16_table, indices, 0);
+  (void)svluti6_lane_bf16_x2(bf16_table, indices, 1);
 }
