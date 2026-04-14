92d04f1647ea9b1585e35701333b2bd7fb469799
David Rivera <davidriverg@gmail.com>
2026-04-14 09:56:29 -0400

    fix fmt

--- clang/lib/CIR/Dialect/Transforms/LoweringPrepare.cpp
+++ clang/lib/CIR/Dialect/Transforms/LoweringPrepare.cpp
@@ -10,8 +10,6 @@
 #include "mlir/IR/Attributes.h"
 #include "mlir/IR/BuiltinAttributeInterfaces.h"
 #include "mlir/IR/IRMapping.h"
-#include "mlir/IR/BuiltinAttributeInterfaces.h"
-#include "mlir/IR/IRMapping.h"
 #include "clang/AST/ASTContext.h"
 #include "clang/AST/Mangle.h"
 #include "clang/Basic/Cuda.h"
