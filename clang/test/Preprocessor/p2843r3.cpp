f374e17bf507f7786d3268c5fcdbb46a94d47894
yronglin <yronglin777@gmail.com>
2026-04-15 00:32:18 +0800

    Convert ext_embedded_directive to hard error and add more test
    Signed-off-by: yronglin <yronglin777@gmail.com>

--- clang/test/Preprocessor/p2843r3.cpp
+++ clang/test/Preprocessor/p2843r3.cpp
@@ -1,21 +1,37 @@
-// RUN: %clang_cc1 -std=c++26 -pedantic -verify -Wno-invalid-pp-token %s
-// RUN: %clang_cc1 -std=c++23 -pedantic -verify -Wno-invalid-pp-token %s
+// RUN: %clang_cc1 -std=c++26 -pedantic -verify=cxx26,expected -Wno-invalid-pp-token %s
+// RUN: %clang_cc1 -std=c++23 -pedantic -verify=cxx23,expected -Wno-invalid-pp-token %s
 
-// P2843R3: Preprocessing is never undefined.
-// These constructs were previously "undefined behavior" in the preprocessor;
-// as of C++26 they are ill-formed (diagnostic required). Clang already
-// diagnoses them under -pedantic, so this test just pins that behavior down.
+// P2843R3: Preprocessing is never undefined. The constructs this paper makes
+// ill-formed were previously undefined behavior; under C++26 Clang now
+// diagnoses them as errors, while retaining the pre-existing pedantic warning
+// in earlier language modes for compatibility.
 
-// [cpp.cond] A macro that expands to 'defined' in a conditional expression.
+// [cpp.cond] A macro expansion that produces 'defined' in a conditional
+// expression. P2843R3 makes this ill-formed; promoted to a hard error in
+// C++26.
 #define DEFINED defined
-#if DEFINED(bar) // expected-warning {{macro expansion producing 'defined' has undefined behavior}}
+// cxx26-error@+2 {{macro expansion producing 'defined' is not allowed}}
+// cxx23-warning@+1 {{macro expansion producing 'defined' has undefined behavior}}
+#if DEFINED(bar)
+#endif
+
+// Malformed 'defined' operands are ill-formed in all modes.
+#if defined()      // expected-error {{macro name must be an identifier}}
+#endif
+#if defined(a b)   // expected-error {{missing ')' after 'defined'}} expected-note {{to match this '('}}
+#endif
+#if defined(a, b)  // expected-error {{missing ')' after 'defined'}} expected-note {{to match this '('}}
 #endif
 
 // [cpp.replace.general] A preprocessing directive inside the arguments of a
-// function-like macro invocation.
+// function-like macro invocation. Promoted to a hard error in C++26.
 #define FUNCTION_MACRO(...)
+// cxx26-note@+1 2 {{expansion of macro 'FUNCTION_MACRO' requested here}}
 FUNCTION_MACRO(
-    #if 0 // expected-warning {{embedding a directive within macro arguments has undefined behavior}}
+    // cxx26-error@+2 {{embedding a #if directive within macro arguments is not supported}}
+    // cxx23-warning@+1 {{embedding a directive within macro arguments has undefined behavior}}
+    #if 0
+    // cxx26-error@+1 {{embedding a #endif directive within macro arguments is not supported}}
     #endif
 )
 
