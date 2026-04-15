d8523036bf6b76037553acc08e0d3324630efc7c
PrabbyDD <dhaliprabh@gmail.com>
2026-04-15 08:14:36 -0700

    I keep forgetting to modify the test in tandem with the Diagnostic report changes...fixing that so test passes

--- clang/test/Frontend/spirv-target-validation.c
+++ clang/test/Frontend/spirv-target-validation.c
@@ -1,4 +1,4 @@
 // RUN: not %clang_cc1 -target spirv %s 2>&1 | FileCheck %s
-// CHECK: error: SPIR-V target '%0' requires a Vulkan environment (e.g. '-target spirv64-unknown-vulkan1.3')
+// CHECK: error: SPIR-V target requires a Vulkan environment (e.g. '-target spirv64-unknown-vulkan1.3')
 
 int main() { return 0; }
