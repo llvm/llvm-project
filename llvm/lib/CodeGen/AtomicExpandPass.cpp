5313610c55a10610b7c3f32ef5ba9c2a9631afac
Yonah Goldberg <ygoldberg@nvidia.com>
2026-04-15 20:09:36 +0000

    format

--- llvm/lib/CodeGen/AtomicExpandPass.cpp
+++ llvm/lib/CodeGen/AtomicExpandPass.cpp
@@ -1997,7 +1997,10 @@ void AtomicExpandImpl::expandAtomicRMWToLibcall(AtomicRMWInst *I) {
           NewLoaded = Builder.CreateExtractValue(Pair, 0, "newloaded");
 
           // ...and then expand the CAS into a libcall.
-          expandAtomicCASToLibcall(Pair, "atomicrmw " + AtomicRMWInst::getOperationName(I->getOperation()), MetadataSrc);
+          expandAtomicCASToLibcall(
+              Pair,
+              "atomicrmw " + AtomicRMWInst::getOperationName(I->getOperation()),
+              MetadataSrc);
         });
   }
 }
