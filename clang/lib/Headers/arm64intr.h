7c0f36aaa594f28fbe35cb9de4a0846122f5af7e
Adhemerval Zanella <zatrazz@gmail.com>
2026-04-15 16:53:14 -0300

    [aarch64] Add support for the __cas{8|16|32|64} MS intrinsics
    Adds support for the following MSVC intrinsics:
    * `__cas8`  - maps to CASB.
    * `__cas16` - maps to CASH.
    * `__cas32` - maps to CAS.
    * `__cas64` - maps to CAS.
    
    The emit is done using new intrisincs to issue CAS* instruction
    directly, regardless of LSE target feature.  This mimics MSVC compiler
    and the idea of the builtin to mimic an inline asm.
    
    These are documented at:
    <https://learn.microsoft.com/en-us/cpp/intrinsics/arm64-intrinsics?view=msvc-180>

--- clang/lib/Headers/arm64intr.h
+++ clang/lib/Headers/arm64intr.h
@@ -77,6 +77,15 @@ unsigned __int8 __stlxr64(volatile unsigned __int64 *, unsigned __int64);
 
 void __clrex(unsigned __int8);
 
+unsigned __int8 __cas8(unsigned __int8 volatile *, unsigned __int8,
+                       unsigned __int8);
+unsigned __int16 __cas16(unsigned __int16 volatile *, unsigned __int16,
+                         unsigned __int16);
+unsigned __int32 __cas32(unsigned __int32 volatile *, unsigned __int32,
+                         unsigned __int32);
+unsigned __int64 __cas64(unsigned __int64 volatile *, unsigned __int64,
+                         unsigned __int64);
+
 #ifdef __cplusplus
 }
 #endif
