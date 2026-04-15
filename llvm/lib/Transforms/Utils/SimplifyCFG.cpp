89d3dbd1844fd0949adbdf3cdcd40bc5530149da
8051Enthusiast <8051Enthusiast@protonmail.com>
2026-04-15 19:30:50 +0200

    Revert "[SimplifyCFG] Reuse function comdat for switch lookup table (#190995)" (#192294)
    This reverts commit 4e3a074501cae5f132293d56f9987ded94ee72be.
    
    CI broke with errors that look very much like they're caused by #190995.
    I can't merge, so someone else will have to.
    
    https://lab.llvm.org/buildbot/#/builders/94/builds/16933
    https://lab.llvm.org/buildbot/#/builders/55/builds/26779

--- llvm/lib/Transforms/Utils/SimplifyCFG.cpp
+++ llvm/lib/Transforms/Utils/SimplifyCFG.cpp
@@ -6962,7 +6962,6 @@ Value *SwitchReplacement::replaceSwitch(Value *Index, IRBuilder<> &Builder,
     // Set the alignment to that of an array items. We will be only loading one
     // value out of it.
     Table->setAlignment(DL.getPrefTypeAlign(ValueType));
-    Table->setComdat(Func->getComdat());
     Type *IndexTy = DL.getIndexType(Table->getType());
     auto *ArrayTy = cast<ArrayType>(Table->getValueType());
 
