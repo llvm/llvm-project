93b7d8d49d5858410346d1a55afa4a5235d02c67
changkhothuychung <nhat7203@gmail.com>
2026-04-15 15:54:04 -0400

    rename codegen test file

--- clang/test/CodeGenCXX/reflection-emit-meta-info-ms.cpp
+++ clang/test/CodeGenCXX/reflection-emit-meta-info-ms.cpp
@@ -1,9 +1,5 @@
-// RUN: %clang_cc1 -std=c++26 -freflection -triple x86_64-unknown-linux-gnu \
+// RUN: %clang_cc1 -std=c++26 -freflection -triple x86_64-pc-windows-msvc \
 // RUN:   -emit-llvm -o - %s -verify
-
-constexpr auto r = ^^int;
-constexpr auto q = r;
-
 
 int main() {
   (void)(^^int); // expected-error {{cannot compile this scalar expression yet}}
