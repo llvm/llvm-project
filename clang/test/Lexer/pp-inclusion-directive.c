743d62260272acbf0866c354088980d2104495d4
🍌Shawn <m18824909883@163.com>
2026-04-15 20:40:53 +0800

    Update inclusion directive test cases

--- clang/test/Lexer/pp-inclusion-directive.c
+++ clang/test/Lexer/pp-inclusion-directive.c
@@ -1,8 +1,9 @@
-// RUN: %clang_cc1 -fsyntax-only -detailed-preprocessing-record %s
+// RUN: %clang_cc1 -fsyntax-only -detailed-preprocessing-record -I %S -I %S/Inputs %s
 // Test that PreprocessingRecord::InclusionDirective correctly handles various
 // inclusion directive kinds (include, import, include_next).
 
 #include "pp-inclusion-directive.h"
 #include <pp-inclusion-directive.h>
-@import pp-inclusion-directive;
-#include_next pp-inclusion-directive.h
+#import "pp-inclusion-directive.h"
+#include_next "pp-inclusion-directive.h"
+
