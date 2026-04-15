e8a1616f456d7757eaa62cb91447edcbaec7fd31
Balázs Kéri <balazs.keri@ericsson.com>
2026-04-15 10:47:56 +0200

    fixed test

--- clang-tools-extra/test/clang-tidy/checkers/bugprone/throwing-static-initialization-allow.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/bugprone/throwing-static-initialization-allow.cpp
@@ -1,22 +1,22 @@
 // RUN: %check_clang_tidy %s bugprone-throwing-static-initialization %t -- \
 // RUN:   -config="{CheckOptions: \
-// RUN:             {bugprone-throwing-static-initialization.IgnoredTypes: \"Ignore;^ns::S1$;^ns::Template<1>$\"}}" \
+// RUN:             {bugprone-throwing-static-initialization.AllowedTypes: \"Allow;^ns::S1$;^ns::Template<1>$\"}}" \
 // RUN:   -- -fexceptions
 
 struct S1 {
   S1() noexcept(false);
 };
 
-struct S1_Ignore {
-  S1_Ignore() noexcept(false);
+struct S1_Allow {
+  S1_Allow() noexcept(false);
 };
 
 namespace ns {
 struct S1 {
   S1();
 };
-struct S1_Ignore {
-  S1_Ignore();
+struct S1_Allow {
+  S1_Allow();
 };
 template<int>
 struct Template {
@@ -25,20 +25,20 @@ struct Template {
 }
 
 template<class>
-struct TemplateIgnored {
-  TemplateIgnored() noexcept(false);
+struct TemplateAllowed {
+  TemplateAllowed() noexcept(false);
 };
 
-S1_Ignore getS1() noexcept(false);
+S1_Allow getS1() noexcept(false);
 
 S1 VarThrow;
 // CHECK-MESSAGES: :[[@LINE-1]]:4: warning: initialization of 'VarThrow' with static storage duration may throw an exception that cannot be caught
 ns::Template<2> VarTempl;
 // CHECK-MESSAGES: :[[@LINE-1]]:17: warning: initialization of 'VarTempl' with static storage duration may throw an exception that cannot be caught
 
-S1_Ignore VarIgnoreConstr;
-S1_Ignore VarIgnoreInitF = getS1();
-ns::S1 VarIgnore2;
-ns::S1_Ignore VarIgnore3;
-TemplateIgnored<int> VarIgnore4;
-ns::Template<1> VarIgnore5;
+S1_Allow VarAllowedConstr;
+S1_Allow VarAllowedInitF = getS1();
+ns::S1 VarAllowed2;
+ns::S1_Allow VarAllowed3;
+TemplateAllowed<int> VarAllowed4;
+ns::Template<1> VarAllowed5;
