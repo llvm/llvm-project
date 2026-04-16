24a9a4e4eca5c779c10425f9f4a66fd503ef68ea
Kacper Fiedorowicz <kf@efab.pl>
2026-04-16 18:30:54 +0200

    fix header comment formatting

--- libc/src/signal/darwin/sigdelset.cpp
+++ libc/src/signal/darwin/sigdelset.cpp
@@ -1,4 +1,4 @@
-//===-- Darwin implementation of sigdelset ---------------------------------===//
+//===-- Darwin implementation of sigdelset --------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
--- libc/src/signal/darwin/sigemptyset.cpp
+++ libc/src/signal/darwin/sigemptyset.cpp
@@ -1,4 +1,4 @@
-//===-- Darwin implementation of sigemptyset -------------------------------===//
+//===-- Darwin implementation of sigemptyset ------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
