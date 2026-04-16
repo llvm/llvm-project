intel/llvm

commit 12a4adf6f2c3d4748a58fd89fcc210d820073c85

[offload] Fix kernel record/replay and add extensible mechanism (#190588)
This commit fixes the kernel record replay on both AMD and CUDA devices. It
also re-organizes the record replay code, moves the whole code to separate
files, and makes it extensible to support other record formats (potentially in
the future). The environment variables for controlling the recording have also
been modified.

offload/libomptarget/omptarget.cpp
@@ -2379,67 +2379,105 @@ int target(ident_t *Loc, DeviceTy &Device, void *HostPtr,
 /// and informing the record-replayer of whether to store the output
 /// in some file.
 int target_activate_rr(DeviceTy &Device, uint64_t MemorySize, void *VAddr,
-                       bool IsRecord, bool SaveOutput,
-                       uint64_t &ReqPtrArgOffset) {
-  return Device.RTL->initialize_record_replay(Device.DeviceID, MemorySize,
-                                              VAddr, IsRecord, SaveOutput,
-                                              ReqPtrArgOffset);
+                       bool IsRecord, bool SaveOutput, bool EmitReport,
+                       const char *OutputDirPath) {
+  return Device.RTL->initialize_record_replay(
+      Device.DeviceID, MemorySize, VAddr, IsRecord,
+      /*IsNative=*/true, SaveOutput, EmitReport, OutputDirPath);
 }
 
 /// Executes a kernel using pre-recorded information for loading to
 /// device memory to launch the target kernel with the pre-recorded
 /// configuration.
 int target_replay(ident_t *Loc, DeviceTy &Device, void *HostPtr,
-                  void *DeviceMemory, int64_t DeviceMemorySize, void **TgtArgs,
-                  ptrdiff_t *TgtOffsets, int32_t NumArgs, int32_t NumTeams,
-                  int32_t ThreadLimit, uint64_t LoopTripCount,
+                  void *DeviceMemory, int64_t DeviceMemorySize,
+                  const llvm::offloading::EntryTy *Globals, int32_t NumGlobals,
+                  void **TgtArgs, ptrdiff_t *TgtOffsets, int32_t NumArgs,
+                  int32_t NumTeams, int32_t ThreadLimit,
+                  uint32_t SharedMemorySize, uint64_t LoopTripCount,
                   AsyncInfoTy &AsyncInfo) {
   int32_t DeviceId = Device.DeviceID;
-  TableMap *TM = getTableMap(HostPtr);
-  // Fail if the table map fails to find the target kernel pointer for the
-  // provided host pointer.
-  if (!TM) {
-    REPORT() << "Host ptr " << HostPtr
-             << " does not have a matching target pointer.";
-    return OFFLOAD_FAIL;
+  int32_t NumSymbols = NumGlobals + 1;
+
+  struct SymbolDataTy {
+    void *DevPtr = nullptr;
+    TableMap *TM = nullptr;
+    __tgt_target_table *TargetTable = nullptr;
+  };
+  SmallVector<SymbolDataTy> Symbols(NumSymbols);
+
+  for (int32_t I = 0; I < NumSymbols; ++I) {
+    // The first symbol is the kernel entry.
+    void *SymbolHostPtr = (I == 0) ? HostPtr : Globals[I - 1].Address;
+
+    // Get the table map for each symbol.
+    Symbols[I].TM = getTableMap(SymbolHostPtr);
+    if (!Symbols[I].TM) {
+      REPORT() << "Host pointer " << SymbolHostPtr
+               << " does not have a matching target pointer.";
+      return OFFLOAD_FAIL;
+    }
   }
 
-  // Retrieve the target table of offloading entries.
-  __tgt_target_table *TargetTable = nullptr;
+  // Retrieve the target table for each symbol.
   {
     std::lock_guard<std::mutex> TrlTblLock(PM->TrlTblMtx);
     assert(TM->Table->TargetsTable.size() > (size_t)DeviceId &&
            "Not expecting a device ID outside the table's bounds!");
-    TargetTable = TM->Table->TargetsTable[DeviceId];
+    for (auto &S : Symbols) {
+      S.TargetTable = S.TM->Table->TargetsTable[DeviceId];
+      assert(S.TargetTable && "Global data has not been mapped\n");
+    }
   }
-  assert(TargetTable && "Global data has not been mapped\n");
 
-  // Retrieve the target kernel pointer, allocate and store the recorded device
-  // memory data, and launch device execution.
-  void *TgtEntryPtr = TargetTable->EntriesBegin[TM->Index].Address;
-  ODBG(ODT_Kernel) << "Launching target execution "
-                   << TargetTable->EntriesBegin[TM->Index].SymbolName
-                   << " with pointer " << TgtEntryPtr << " (index=" << TM->Index
-                   << ").";
+  // Retrieve the device pointers for each symbol.
+  for (auto &S : Symbols)
+    S.DevPtr = S.TargetTable->EntriesBegin[S.TM->Index].Address;
+
+  // Initialize the device memory of each global.
+  for (int32_t I = 0; I < NumGlobals; ++I) {
+    assert(Globals[I].AuxAddr && "Global has no AuxAddr.");
+
+    // Initialize the value of the global in the device.
+    int Ret = Device.submitData(Symbols[I + 1].DevPtr, Globals[I].AuxAddr,
+                                Globals[I].Size, AsyncInfo);
+    if (Ret != OFFLOAD_SUCCESS) {
+      REPORT() << "Failed to submit data to a global.";
+      return OFFLOAD_FAIL;
+    }
+  }
 
   void *TgtPtr = Device.allocData(DeviceMemorySize, /*HstPtr=*/nullptr,
                                   TARGET_ALLOC_DEFAULT);
-  Device.submitData(TgtPtr, DeviceMemory, DeviceMemorySize, AsyncInfo);
+  if (!TgtPtr) {
+    REPORT() << "Failed to allocate device memory.";
+    return OFFLOAD_FAIL;
+  }
+
+  int Ret =
+      Device.submitData(TgtPtr, DeviceMemory, DeviceMemorySize, AsyncInfo);
+  if (Ret != OFFLOAD_SUCCESS) {
+    REPORT() << "Failed to submit data to a global.";
+    return OFFLOAD_FAIL;
+  }
 
   KernelArgsTy KernelArgs{};
   KernelArgs.Version = OMP_KERNEL_ARG_VERSION;
   KernelArgs.NumArgs = NumArgs;
   KernelArgs.Tripcount = LoopTripCount;
   KernelArgs.NumTeams[0] = NumTeams;
+  KernelArgs.NumTeams[1] = 1;
+  KernelArgs.NumTeams[2] = 1;
   KernelArgs.ThreadLimit[0] = ThreadLimit;
+  KernelArgs.ThreadLimit[1] = 1;
+  KernelArgs.ThreadLimit[2] = 1;
+  KernelArgs.DynCGroupMem = SharedMemorySize;
 
-  int Ret = Device.launchKernel(TgtEntryPtr, TgtArgs, TgtOffsets, KernelArgs,
-                                AsyncInfo);
-
+  Ret = Device.launchKernel(Symbols[0].DevPtr, TgtArgs, TgtOffsets, KernelArgs,
+                            AsyncInfo);
   if (Ret != OFFLOAD_SUCCESS) {
-    REPORT() << "Executing target region abort target.";
+    REPORT() << "Failed to launch kernel replay.";
     return OFFLOAD_FAIL;
   }
-
   return OFFLOAD_SUCCESS;
 }

offload/plugins-nextgen/common/src/RecordReplay.cpp
@@ -1,0 +1,328 @@
+//===- RecordReplay.cpp - Target independent kernel record replay ---------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+//===----------------------------------------------------------------------===//
+
+#include "PluginInterface.h"
+
+#include "Shared/APITypes.h"
+
+#include "ErrorReporting.h"
+#include "Shared/Utils.h"
+
+#include "llvm/Support/Error.h"
+#include "llvm/Support/JSON.h"
+#include "llvm/Support/MemoryBuffer.h"
+#include "llvm/Support/raw_ostream.h"
+
+#include <cstdint>
+#include <functional>
+
+using namespace llvm;
+using namespace omp;
+using namespace target;
+using namespace plugin;
+using namespace error;
+
+RecordReplayTy::InstanceTy::InstanceTy(const GenericKernelTy &Kernel,
+                                       uint32_t NumTeams, uint32_t NumThreads,
+                                       uint32_t SharedMemorySize)
+    : Kernel(Kernel), NumTeams(NumTeams), NumThreads(NumThreads),
+      SharedMemorySize(SharedMemorySize) {
+  KernelHash = stable_hash_name(Kernel.getName());
+  LaunchConfigHash =
+      stable_hash_combine((stable_hash)NumTeams, (stable_hash)NumThreads,
+                          (stable_hash)SharedMemorySize);
+}
+
+Error RecordReplayTy::init(uint64_t MemSize, void *VAddr) {
+  if (!VAddr && isReplaying())
+    return Plugin::error(ErrorCode::INVALID_ARGUMENT,
+                         "VAddr cannot be null when replaying");
+  if (!VAddr)
+    VAddr = Device.getSuggestedVirtualAddress();
+
+  auto StartAddrOrErr = Device.allocateWithVirtualAddress(MemSize, VAddr);
+  if (!StartAddrOrErr)
+    return StartAddrOrErr.takeError();
+
+  if (!*StartAddrOrErr)
+    return Plugin::error(ErrorCode::OUT_OF_RESOURCES, "allocating memory");
+  if (isReplaying() && *StartAddrOrErr != VAddr)
+    return Plugin::error(ErrorCode::INVALID_ARGUMENT,
+                         "could not reserve recorded virtual address");
+
+  StartAddr = *StartAddrOrErr;
+  TotalSize = MemSize;
+
+  // Create the output directory if necessary.
+  std::error_code EC;
+  std::filesystem::create_directories(OutputDirectory, EC);
+  if (EC)
+    return Plugin::error(ErrorCode::HOST_IO, "creating output directory");
+
+  INFO(OMP_INFOTYPE_PLUGIN_KERNEL, Device.getDeviceId(),
+       "%s initialized with starting address %p, "
+       "memory size %lu bytes, and output directory in %s\n",
+       Status == StatusTy::Recording ? "Record" : "Replay", StartAddr,
+       TotalSize, OutputDirectory.c_str());
+
+  return Plugin::success();
+}
+
+Error RecordReplayTy::deinit() {
+  if (isRecording() && EmitReport)
+    if (auto Err = emitInstanceReport())
+      return Err;
+
+  if (StartAddr)
+    return Device.deallocateWithVirtualAddress(StartAddr, TotalSize);
+
+  return Plugin::success();
+}
+
+Error RecordReplayTy::emitInstanceReport() {
+  std::lock_guard<std::mutex> LG(InstancesLock);
+  llvm::outs() << "=== record report begin ===\n";
+  llvm::outs() << "directory: "
+               << std::filesystem::absolute(OutputDirectory).string() << "\n";
+  llvm::outs() << "kernels: " << Instances.size() << "\n";
+  for (const auto &Inst : Instances) {
+    llvm::outs() << getFilename(Inst, "json", /*IncludeDir=*/false) << ": "
+                 << Inst.Kernel.getName() << "\n";
+  }
+  llvm::outs() << "=== record report end ===\n";
+  return Plugin::success();
+}
+
+std::pair<const RecordReplayTy::InstanceTy &, bool>
+RecordReplayTy::registerInstance(const GenericKernelTy &Kernel,
+                                 uint32_t NumTeams, uint32_t NumThreads,
+                                 uint32_t SharedMemorySize) {
+  std::lock_guard<std::mutex> LG(InstancesLock);
+  auto [It, Inserted] =
+      Instances.emplace(Kernel, NumTeams, NumThreads, SharedMemorySize);
+  // Increase the number of occurrences.
+  It->Occurrences += 1;
+  return {*It, Inserted};
+}
+
+Expected<void *> RecordReplayTy::allocate(uint64_t Size) {
+  assert(StartAddr && "Expected memory has been pre-allocated");
+  constexpr int Alignment = 16;
+  // Assume alignment is a power of 2.
+  int64_t AlignedSize = (Size + (Alignment - 1)) & (~(Alignment - 1));
+
+  std::lock_guard<std::mutex> LG(AllocationLock);
+  if (CurrentSize + AlignedSize > TotalSize)
+    return Plugin::error(ErrorCode::OUT_OF_RESOURCES,
+                         "run out of record replay memory");
+  void *Alloc = (char *)StartAddr + CurrentSize;
+  CurrentSize += AlignedSize;
+  return Alloc;
+}
+
+Error RecordReplayTy::deallocate(void *Ptr) { return Plugin::success(); }
+
+Expected<RecordReplayTy::HandleTy> RecordReplayTy::recordPrologue(
+    const GenericKernelTy &Kernel, const KernelArgsTy &KernelArgs,
+    const KernelLaunchParamsTy &LaunchParams, uint32_t NumTeams[3],
+    uint32_t NumThreads[3], uint32_t SharedMemorySize) {
+  if (!isRecordingOrReplaying())
+    return HandleTy{nullptr, false};
+
+  // Register the instance and avoid recording if it is inactive or replaying.
+  auto [Instance, First] =
+      registerInstance(Kernel, NumTeams[0], NumThreads[0], SharedMemorySize);
+
+  HandleTy Handle{&Instance, First};
+  if (isReplaying() || !First)
+    return Handle;
+
+  if (auto Err = recordDescImpl(Kernel, Instance, KernelArgs, LaunchParams))
+    return Err;
+
+  if (auto Err = recordPrologueImpl(Kernel, Instance, KernelArgs, LaunchParams))
+    return Err;
+
+  return Handle;
+}
+
+Error RecordReplayTy::recordEpilogue(const GenericKernelTy &Kernel,
+                                     HandleTy Handle) {
+  if (!shouldRecordEpilogue() || !Handle.Active)
+    return Plugin::success();
+
+  return recordEpilogueImpl(Kernel, *Handle.Instance);
+}
+
+Error NativeRecordReplayTy::recordPrologueImpl(
+    const GenericKernelTy &Kernel, const InstanceTy &Instance,
+    const KernelArgsTy &KernelArgs, const KernelLaunchParamsTy &LaunchParams) {
+  std::string SnapshotFilename = getFilename(Instance, "record_input");
+  if (auto Err = recordSnapshot(SnapshotFilename))
+    return Err;
+
+  std::string GlobalsFilename = getFilename(Instance, "globals");
+  if (auto Err = recordGlobals(GlobalsFilename))
+    return Err;
+
+  std::string ImageFilename = getFilename(Instance, "image");
+  return recordImage(Kernel, ImageFilename);
+}
+
+Error NativeRecordReplayTy::recordEpilogueImpl(const GenericKernelTy &Kernel,
+                                               const InstanceTy &Instance) {
+  std::string SnapshotFilename =
+      getFilename(Instance, isRecording() ? "record_output" : "replay_output");
+  return recordSnapshot(SnapshotFilename);
+}
+
+Error NativeRecordReplayTy::recordDescImpl(
+    const GenericKernelTy &Kernel, const InstanceTy &Instance,
+    const KernelArgsTy &KernelArgs, const KernelLaunchParamsTy &LaunchParams) {
+  json::Object JsonKernelInfo;
+  JsonKernelInfo["Name"] = Kernel.getName();
+  JsonKernelInfo["NumArgs"] = KernelArgs.NumArgs;
+  JsonKernelInfo["NumTeamsClause"] = Instance.NumTeams;
+  JsonKernelInfo["ThreadLimitClause"] = Instance.NumThreads;
+  JsonKernelInfo["SharedMemorySize"] = Instance.SharedMemorySize;
+  JsonKernelInfo["LoopTripCount"] = KernelArgs.Tripcount;
+  JsonKernelInfo["DeviceId"] = Device.getDeviceId();
+  JsonKernelInfo["VAllocAddr"] = (intptr_t)StartAddr;
+  JsonKernelInfo["VAllocSize"] = TotalSize;
+
+  json::Array JsonArgPtrs;
+  for (uint32_t I = 0; I < KernelArgs.NumArgs; ++I)
+    JsonArgPtrs.push_back((intptr_t)(*(void **)LaunchParams.Ptrs[I]));
+  JsonKernelInfo["ArgPtrs"] = json::Value(std::move(JsonArgPtrs));
+
+  json::Array JsonArgOffsets;
+  for (uint32_t I = 0; I < KernelArgs.NumArgs; ++I)
+    JsonArgOffsets.push_back(0);
+  JsonKernelInfo["ArgOffsets"] = json::Value(std::move(JsonArgOffsets));
+
+  std::string JsonFilename = getFilename(Instance, "json");
+  std::error_code EC;
+  raw_fd_ostream JsonOS(JsonFilename, EC);
+  if (EC)
+    return Plugin::error(ErrorCode::HOST_IO, "saving kernel json file");
+  JsonOS << json::Value(std::move(JsonKernelInfo));
+  JsonOS.close();
+  return Plugin::success();
+}
+
+std::string NativeRecordReplayTy::getFilenameImpl(const InstanceTy &Instance,
+                                                  StringRef Suffix,
+                                                  bool IncludeDirectory) {
+  std::filesystem::path Filepath = IncludeDirectory ? OutputDirectory : "";
+  Filepath /= std::to_string(Instance.KernelHash) + "_" +
+              std::to_string(Instance.LaunchConfigHash);
+  Filepath.replace_extension(Suffix.data());
+  return Filepath.string();
+}
+
+Error NativeRecordReplayTy::recordSnapshot(const std::string &Filename) {
+  // Another thread may be allocating memory. The size can only increase.
+  AllocationLock.lock();
+  uint64_t RecordSize = CurrentSize;
+  AllocationLock.unlock();
+
+  ErrorOr<std::unique_ptr<WritableMemoryBuffer>> DeviceMemoryMB =
+      WritableMemoryBuffer::getNewUninitMemBuffer(RecordSize);
+  if (!DeviceMemoryMB)
+    return Plugin::error(ErrorCode::OUT_OF_RESOURCES,
+                         "creating MemoryBuffer for device memory");
+
+  if (auto Err = Device.dataRetrieve(DeviceMemoryMB.get()->getBufferStart(),
+                                     StartAddr, RecordSize, nullptr))
+    return Err;
+
+  StringRef DeviceMemory(DeviceMemoryMB.get()->getBufferStart(), RecordSize);
+
+  std::error_code EC;
+  raw_fd_ostream OS(Filename, EC);
+  if (EC)
+    return Plugin::error(ErrorCode::HOST_IO, "saving memory snapshot file");
+  OS << DeviceMemory;
+  OS.close();
+  return Plugin::success();
+}
+
+Error NativeRecordReplayTy::recordImage(const GenericKernelTy &Kernel,
+                                        const std::string &Filename) {
+  std::error_code EC;
+  raw_fd_ostream OS(Filename, EC);
+  if (EC)
+    return Plugin::error(ErrorCode::HOST_IO, "saving image file");
+  OS << Kernel.getImage().getMemoryBuffer().getBuffer();
+  OS.close();
+  return Plugin::success();
+}
+
+Error NativeRecordReplayTy::recordGlobals(const std::string &Filename) {
+  AllocationLock.lock();
+  // Copy the globals into a local vector so we can read it safely from this
+  // thread. This vector should have a few entries in general. No need to lock
+  // the entire function.
+  SmallVector<GlobalEntryTy> Globals = GlobalEntries;
+  AllocationLock.unlock();
+
+  uint64_t TotalSize = sizeof(uint32_t);
+  uint32_t NumGlobals = 0;
+  for (auto &Global : Globals) {
+    if (!Global.Size)
+      continue;
+    // Get the total size of the string and entry including the null byte.
+    TotalSize += Global.Size + sizeof(uint32_t) + sizeof(uint64_t) +
+                 Global.Name.length() + 1;
+    NumGlobals++;
+  }
+
+  ErrorOr<std::unique_ptr<WritableMemoryBuffer>> GlobalsMB =
+      WritableMemoryBuffer::getNewUninitMemBuffer(TotalSize);
+  if (!GlobalsMB)
+    return Plugin::error(ErrorCode::OUT_OF_RESOURCES,
+                         "creating MemoryBuffer for globals memory");
+
+  void *BufferPtr = GlobalsMB.get()->getBufferStart();
+  *((uint32_t *)(BufferPtr)) = NumGlobals;
+  BufferPtr = utils::advancePtr(BufferPtr, sizeof(uint32_t));
+
+  for (auto &Global : Globals) {
+    if (!Global.Size)
+      continue;
+
+    uint32_t NameLength = Global.Name.length() + 1;
+    *((uint32_t *)(BufferPtr)) = NameLength;
+    BufferPtr = utils::advancePtr(BufferPtr, sizeof(uint32_t));
+
+    *((uint64_t *)(BufferPtr)) = Global.Size;
+    BufferPtr = utils::advancePtr(BufferPtr, sizeof(uint64_t));
+
+    memcpy(BufferPtr, Global.Name.data(), NameLength);
+    BufferPtr = utils::advancePtr(BufferPtr, NameLength);
+
+    if (auto Err =
+            Device.dataRetrieve(BufferPtr, Global.Addr, Global.Size, nullptr))
+      return Err;
+    BufferPtr = utils::advancePtr(BufferPtr, Global.Size);
+  }
+  assert(BufferPtr == GlobalsMB->get()->getBufferEnd() &&
+         "Buffer over or under-filled.");
+  assert(TotalSize ==
+             utils::getPtrDiff(BufferPtr, GlobalsMB->get()->getBufferStart()) &&
+         "Buffer size mismatch.");
+
+  StringRef GlobalsMemory(GlobalsMB.get()->getBufferStart(), TotalSize);
+  std::error_code EC;
+  raw_fd_ostream OS(Filename, EC);
+  OS << GlobalsMemory;
+  OS.close();
+  return Plugin::success();
+}

offload/cmake/OpenMPTesting.cmake
@@ -12,6 +12,7 @@ endif()
 set(OFFLOAD_NOT_EXECUTABLE ${LLVM_TOOLS_BINARY_DIR}/not)
 set(OFFLOAD_DEVICE_INFO_EXECUTABLE ${LLVM_RUNTIME_OUTPUT_INTDIR}/llvm-offload-device-info)
 set(OFFLOAD_TBLGEN_EXECUTABLE ${LLVM_RUNTIME_OUTPUT_INTDIR}/offload-tblgen)
+set(OMP_KERNEL_REPLAY ${LLVM_RUNTIME_OUTPUT_INTDIR}/llvm-omp-kernel-replay)
 
 # Set the information that we know.
 set(OPENMP_TEST_COMPILER_ID "Clang")

offload/include/omptarget.h
@@ -428,9 +428,12 @@ void __tgt_target_nowait_query(void **AsyncHandle);
 /// device memory.
 int __tgt_target_kernel_replay(ident_t *Loc, int64_t DeviceId, void *HostPtr,
                                void *DeviceMemory, int64_t DeviceMemorySize,
-                               void **TgtArgs, ptrdiff_t *TgtOffsets,
-                               int32_t NumArgs, int32_t NumTeams,
-                               int32_t ThreadLimit, uint64_t LoopTripCount);
+                               const llvm::offloading::EntryTy *Globals,
+                               int32_t NumGlobals, void **TgtArgs,
+                               ptrdiff_t *TgtOffsets, int32_t NumArgs,
+                               int32_t NumTeams, int32_t ThreadLimit,
+                               uint32_t SharedMemorySize,
+                               uint64_t LoopTripCount);
 
 void __tgt_set_info_flag(uint32_t);
 
@@ -438,7 +441,7 @@ int __tgt_print_device_info(int64_t DeviceId);
 
 int __tgt_activate_record_replay(int64_t DeviceId, uint64_t MemorySize,
                                  void *VAddr, bool IsRecord, bool SaveOutput,
-                                 uint64_t &ReqPtrArgOffset);
+                                 bool EmitReport, const char *OutputDirPath);
 
 // Registers a callback for the RPC server. Expects this function type.
 // unsigned callback(rpc::Server::Port *Port, unsigned NumLanes). See the RPC

offload/libomptarget/device.cpp
@@ -89,12 +89,23 @@ llvm::Error DeviceTy::init() {
   // Enables recording kernels if set.
   BoolEnvar OMPX_RecordKernel("LIBOMPTARGET_RECORD", false);
   if (OMPX_RecordKernel) {
-    // Enables saving the device memory kernel output post execution if set.
-    BoolEnvar OMPX_ReplaySaveOutput("LIBOMPTARGET_RR_SAVE_OUTPUT", false);
+    BoolEnvar OMPX_RecordOutput("LIBOMPTARGET_RECORD_OUTPUT", true);
+    Int64Envar OMPX_RecordMemSize("LIBOMPTARGET_RECORD_MEMSIZE",
+                                  8 * 1024 * 1024 * 1024ULL);
+    Int32Envar OMPX_RecordDevice("LIBOMPTARGET_RECORD_DEVICE", 0);
+    StringEnvar OMPX_RecordOutputDir("LIBOMPTARGET_RECORD_DIR", "");
+    BoolEnvar OMPX_EmitRecordReport("LIBOMPTARGET_RECORD_REPORT", false);
+    if (OMPX_RecordDevice != RTLDeviceID)
+      return llvm::Error::success();
 
-    uint64_t ReqPtrArgOffset;
-    RTL->initialize_record_replay(RTLDeviceID, 0, nullptr, true,
-                                  OMPX_ReplaySaveOutput, ReqPtrArgOffset);
+    Ret = RTL->initialize_record_replay(
+        RTLDeviceID, OMPX_RecordMemSize, nullptr,
+        /*IsRecord=*/true, /*IsNative=*/true, OMPX_RecordOutput,
+        OMPX_EmitRecordReport, OMPX_RecordOutputDir.get().c_str());
+    if (Ret != OFFLOAD_SUCCESS)
+      return error::createOffloadError(error::ErrorCode::BACKEND_FAILURE,
+                                       "failed to initialize RR in device %d\n",
+                                       DeviceID);
   }
 
   return llvm::Error::success();

offload/libomptarget/interface.cpp
@@ -473,25 +473,32 @@ EXTERN int __tgt_target_kernel(ident_t *Loc, int64_t DeviceId, int32_t NumTeams,
 /// Activates the record replay mechanism.
 /// \param DeviceId The device identifier to execute the target region.
 /// \param MemorySize The number of bytes to be (pre-)allocated
-///                   by the bump allocator
+///                   by the record replay allocator.
 /// /param IsRecord Activates the record replay mechanism in
-///                 'record' mode or 'replay' mode.
+///                 'record' or 'replay' mode.
 /// /param SaveOutput Store the device memory after kernel
-///                   execution on persistent storage
+///                   execution on persistent storage.
+/// /param EmitReport Emit a summary report after the recording.
+/// /param OutputDirPath The output directory where the record replay files
+/// should be stored. An empty string or nullptr indicates the current working
+/// directory should be used.
 EXTERN int __tgt_activate_record_replay(int64_t DeviceId, uint64_t MemorySize,
                                         void *VAddr, bool IsRecord,
-                                        bool SaveOutput,
-                                        uint64_t &ReqPtrArgOffset) {
+                                        bool SaveOutput, bool EmitReport,
+                                        const char *OutputDirPath) {
   assert(PM && "Runtime not initialized");
   OMPT_IF_BUILT(ReturnAddressSetterRAII RA(__builtin_return_address(0)));
   auto DeviceOrErr = PM->getDevice(DeviceId);
   if (!DeviceOrErr)
     FATAL_MESSAGE(DeviceId, "%s", toString(DeviceOrErr.takeError()).c_str());
 
-  [[maybe_unused]] int Rc = target_activate_rr(
-      *DeviceOrErr, MemorySize, VAddr, IsRecord, SaveOutput, ReqPtrArgOffset);
-  assert(Rc == OFFLOAD_SUCCESS &&
-         "__tgt_activate_record_replay unexpected failure!");
+  int Rc = target_activate_rr(*DeviceOrErr, MemorySize, VAddr, IsRecord,
+                              SaveOutput, EmitReport, OutputDirPath);
+  if (Rc != OFFLOAD_SUCCESS) {
+    ODBG(ODT_Interface) << "Record replay failed to activate in device "
+                        << DeviceId;
+    return OMP_TGT_FAIL;
+  }
   return OMP_TGT_SUCCESS;
 }
 
@@ -512,12 +519,12 @@ EXTERN int __tgt_activate_record_replay(int64_t DeviceId, uint64_t MemorySize,
 ///                    execution.
 /// \param LoopTripCount The pre-recorded value of the loop tripcount, if any.
 /// \return OMP_TGT_SUCCESS on success, OMP_TGT_FAIL on failure.
-EXTERN int __tgt_target_kernel_replay(ident_t *Loc, int64_t DeviceId,
-                                      void *HostPtr, void *DeviceMemory,
-                                      int64_t DeviceMemorySize, void **TgtArgs,
-                                      ptrdiff_t *TgtOffsets, int32_t NumArgs,
-                                      int32_t NumTeams, int32_t ThreadLimit,
-                                      uint64_t LoopTripCount) {
+EXTERN int __tgt_target_kernel_replay(
+    ident_t *Loc, int64_t DeviceId, void *HostPtr, void *DeviceMemory,
+    int64_t DeviceMemorySize, const llvm::offloading::EntryTy *Globals,
+    int32_t NumGlobals, void **TgtArgs, ptrdiff_t *TgtOffsets, int32_t NumArgs,
+    int32_t NumTeams, int32_t ThreadLimit, uint32_t SharedMemorySize,
+    uint64_t LoopTripCount) {
   assert(PM && "Runtime not initialized");
   OMPT_IF_BUILT(ReturnAddressSetterRAII RA(__builtin_return_address(0)));
   if (checkDevice(DeviceId, Loc)) {
@@ -534,14 +541,18 @@ EXTERN int __tgt_target_kernel_replay(ident_t *Loc, int64_t DeviceId,
                     /*CodePtr=*/OMPT_GET_RETURN_ADDRESS);)
 
   AsyncInfoTy AsyncInfo(*DeviceOrErr);
-  int Rc = target_replay(Loc, *DeviceOrErr, HostPtr, DeviceMemory,
-                         DeviceMemorySize, TgtArgs, TgtOffsets, NumArgs,
-                         NumTeams, ThreadLimit, LoopTripCount, AsyncInfo);
+  int Rc =
+      target_replay(Loc, *DeviceOrErr, HostPtr, DeviceMemory, DeviceMemorySize,
+                    Globals, NumGlobals, TgtArgs, TgtOffsets, NumArgs, NumTeams,
+                    ThreadLimit, SharedMemorySize, LoopTripCount, AsyncInfo);
+
   if (Rc == OFFLOAD_SUCCESS)
     Rc = AsyncInfo.synchronize();
-  handleTargetOutcome(Rc == OFFLOAD_SUCCESS, Loc);
-  assert(Rc == OFFLOAD_SUCCESS &&
-         "__tgt_target_kernel_replay unexpected failure!");
+
+  if (Rc != OFFLOAD_SUCCESS) {
+    ODBG(ODT_Interface) << "Kernel replay failed in device " << DeviceId;
+    return OMP_TGT_FAIL;
+  }
   return OMP_TGT_SUCCESS;
 }
 

offload/libomptarget/private.h
@@ -27,14 +27,17 @@ extern int target(ident_t *Loc, DeviceTy &Device, void *HostPtr,
                   KernelArgsTy &KernelArgs, AsyncInfoTy &AsyncInfo);
 
 extern int target_activate_rr(DeviceTy &Device, uint64_t MemorySize,
-                              void *ReqAddr, bool isRecord, bool SaveOutput,
-                              uint64_t &ReqPtrArgOffset);
+                              void *ReqAddr, bool IsRecord, bool SaveOutput,
+                              bool EmitReport, const char *OutputDirPath);
 
 extern int target_replay(ident_t *Loc, DeviceTy &Device, void *HostPtr,
                          void *DeviceMemory, int64_t DeviceMemorySize,
-                         void **TgtArgs, ptrdiff_t *TgtOffsets, int32_t NumArgs,
+                         const llvm::offloading::EntryTy *Globals,
+                         int32_t NumGlobals, void **TgtArgs,
+                         ptrdiff_t *TgtOffsets, int32_t NumArgs,
                          int32_t NumTeams, int32_t ThreadLimit,
-                         uint64_t LoopTripCount, AsyncInfoTy &AsyncInfo);
+                         uint32_t SharedMemorySize, uint64_t LoopTripCount,
+                         AsyncInfoTy &AsyncInfo);
 
 extern void handleTargetOutcome(bool Success, ident_t *Loc);
 

llvm/llvm-project

commit d13186c32c2105a7c4399367f6237a13a4210a3d

[offload] Fix asserts in kernel record replay
This commit fixes issues introduced in PR #190588

offload/libomptarget/omptarget.cpp
@@ -2422,9 +2422,9 @@ int target_replay(ident_t *Loc, DeviceTy &Device, void *HostPtr,
   // Retrieve the target table for each symbol.
   {
     std::lock_guard<std::mutex> TrlTblLock(PM->TrlTblMtx);
-    assert(TM->Table->TargetsTable.size() > (size_t)DeviceId &&
-           "Not expecting a device ID outside the table's bounds!");
     for (auto &S : Symbols) {
+      assert(S.TM->Table->TargetsTable.size() > (size_t)DeviceId &&
+             "Not expecting a device ID outside the table's bounds!");
       S.TargetTable = S.TM->Table->TargetsTable[DeviceId];
       assert(S.TargetTable && "Global data has not been mapped\n");
     }

offload/plugins-nextgen/common/src/RecordReplay.cpp
@@ -315,8 +315,8 @@ Error NativeRecordReplayTy::recordGlobals(const std::string &Filename) {
   }
   assert(BufferPtr == GlobalsMB->get()->getBufferEnd() &&
          "Buffer over or under-filled.");
-  assert(TotalSize ==
-             utils::getPtrDiff(BufferPtr, GlobalsMB->get()->getBufferStart()) &&
+  assert(TotalSize == (uint64_t)utils::getPtrDiff(
+                          BufferPtr, GlobalsMB->get()->getBufferStart()) &&
          "Buffer size mismatch.");
 
   StringRef GlobalsMemory(GlobalsMB.get()->getBufferStart(), TotalSize);