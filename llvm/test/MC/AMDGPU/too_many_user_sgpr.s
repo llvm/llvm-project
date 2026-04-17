ac8d108af8ae3c0ee40b7c086241a4d1f4df454e
zwu-2025 <Zeng.Wu2@amd.com>
2026-04-17 22:38:45 +0000

    mc test

--- llvm/test/MC/AMDGPU/too_many_user_sgpr.s
+++ llvm/test/MC/AMDGPU/too_many_user_sgpr.s
@@ -1,7 +1,5 @@
 // RUN: not llvm-mc -triple=amdgcn-amd-amdhsa -mcpu=gfx1250 -filetype=null %s 2>&1 | FileCheck --check-prefix=GFX1250-ERR %s
 
-.text
-.amdgcn_target "amdgcn-amd-amdhsa--gfx1250"
 .amdhsa_code_object_version 6
 
 .amdhsa_kernel many_inreg_i32
