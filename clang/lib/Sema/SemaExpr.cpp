7b793e9504db1b94deba6809e65b4c142de5dc8f
Ammarguellat <zahira.ammarguellat@intel.com>
2026-04-17 12:05:48 -0700

    Fix format

--- clang/lib/Sema/SemaExpr.cpp
+++ clang/lib/Sema/SemaExpr.cpp
@@ -19707,7 +19707,7 @@ static bool captureInLambda(LambdaScopeInfo *LSI, ValueDecl *Var,
     ByRef = (LSI->ImpCaptureStyle == LambdaScopeInfo::ImpCap_LambdaByref);
   }
 
-  if (auto* BD = dyn_cast<BindingDecl>(Var)) {
+  if (auto *BD = dyn_cast<BindingDecl>(Var)) {
     // For structured bindings, capture the individual element type,
     // not the full decomposed type.
     CaptureType = BD->getType();
