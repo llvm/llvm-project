216e6e54bb7cd66d464482a5017a732e3e712132
PrabbyDD <dhaliprabh@gmail.com>
2026-04-16 18:30:18 -0700

    changed triple in test case, still failing second test

--- clang/test/Frontend/spirv-target-validation.c
+++ clang/test/Frontend/spirv-target-validation.c
@@ -1,5 +1,5 @@
 // RUN: not %clang -target spirv %s 2>&1 | FileCheck %s --check-prefix=CHECK-VULKAN
-// RUN: not %clang -target spirv-unknown-vulkan1.3 %s 2>&1 | FileCheck %s --check-prefix=CHECK-SHADER
+// RUN: not %clang -target spirv-vulkan-mlibc %s 2>&1 | FileCheck %s --check-prefix=CHECK-SHADER
 
 // CHECK-VULKAN: error: SPIR-V target requires a Vulkan environment
 // CHECK-SHADER: error: SPIR-V target requires a valid shader stage environment
