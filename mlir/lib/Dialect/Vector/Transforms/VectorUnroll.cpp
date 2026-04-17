fd4dc8f9e53f7313277232564288701ab159fd6c
Erick Ochoa <erick.ochoalopez@amd.com>
2026-04-17 12:28:23 -0400

    [mlir][vector] Skip redundant affine.apply when unrolling transfer ops.
    Unrolling transfer ops generates affine.apply ops for every
    non-broadcasted dimension, even when the offset being added is zero.
    Skip these.

--- mlir/lib/Dialect/Vector/Transforms/VectorUnroll.cpp
+++ mlir/lib/Dialect/Vector/Transforms/VectorUnroll.cpp
@@ -41,11 +41,12 @@ static SmallVector<Value> sliceTransferIndices(ArrayRef<int64_t> elementOffsets,
   // Compute 'sliceIndices' by adding 'sliceOffsets[i]' to 'indices[i]'.
   SmallVector<Value> slicedIndices(indices);
   for (const auto &dim : llvm::enumerate(permutationMap.getResults())) {
-    if (isBroadcast(dim.value()))
+    int64_t elementOffset = elementOffsets[dim.index()];
+    if (isBroadcast(dim.value()) || elementOffset == 0)
       continue;
     unsigned pos = cast<AffineDimExpr>(dim.value()).getPosition();
     auto expr = getAffineDimExpr(0, builder.getContext()) +
-                getAffineConstantExpr(elementOffsets[dim.index()], ctx);
+                getAffineConstantExpr(elementOffset, ctx);
     auto map = AffineMap::get(/*dimCount=*/1, /*symbolCount=*/0, expr);
     slicedIndices[pos] =
         affine::AffineApplyOp::create(builder, loc, map, indices[pos]);
