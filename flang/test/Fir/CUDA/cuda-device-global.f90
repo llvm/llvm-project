bc1afed99ce8b9764dbbc408e7335e664ce1e96e
Valentin Clement <clementval@gmail.com>
2026-04-17 14:03:20 -0700

    Remove test that has been moved

--- flang/test/Fir/CUDA/cuda-device-global.f90
+++ flang/test/Fir/CUDA/cuda-device-global.f90
@@ -64,34 +64,3 @@ module attributes {fir.defaultkind = "a1c4d8i4l4r4", fir.kindmap = "", gpu.conta
 // CHECK: fir.global linkonce_odr @_QMvector_typesE.dt.v2real2
 // CHECK-LABEL: gpu.module @cuda_device_mod
 // CHECK: fir.global linkonce_odr @_QMvector_typesE.dt.v2real2
-
-// -----
-
-// Test that dead declares in device functions are skipped by default.
-
-// RUN: fir-opt --split-input-file --cuf-device-global="skip-dead-declares=false" %s | FileCheck --check-prefix=PRESERVE %s
-
-module attributes {fir.defaultkind = "a1c4d8i4l4r4", fir.kindmap = "", gpu.container_module} {
-  fir.global @_QMiso_c_bindingECc_alert constant : !fir.char<1> {
-    %0 = fir.string_lit "\07"(1) : !fir.char<1>
-    fir.has_value %0 : !fir.char<1>
-  }
-  func.func @_QMrhsPkernel(%arg0: !fir.ref<f64>) attributes {cuf.proc_attr = #cuf.cuda_proc<global>} {
-    %c1 = arith.constant 1 : index
-    %0 = fir.address_of(@_QMiso_c_bindingECc_alert) : !fir.ref<!fir.char<1>>
-    %1 = fir.declare %0 typeparams %c1 {fortran_attrs = #fir.var_attrs<parameter>, uniq_name = "_QMiso_c_bindingECc_alert"} : (!fir.ref<!fir.char<1>>, index) -> !fir.ref<!fir.char<1>>
-    return
-  }
-  gpu.module @cuda_device_mod {
-  }
-}
-
-// With default skip-dead-declares=true, the global should NOT be in gpu.module.
-// CHECK-LABEL: gpu.module @cuda_device_mod
-// CHECK-NOT: fir.global @_QMiso_c_bindingECc_alert
-
-// With skip-dead-declares=false (preserveDeclare mode), the global should be copied.
-// PRESERVE: fir.global @_QMiso_c_bindingECc_alert
-// PRESERVE-LABEL: gpu.module @cuda_device_mod
-// PRESERVE: fir.global @_QMiso_c_bindingECc_alert
-
