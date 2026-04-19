15021a2c8383592bb1f04e999a35c1c7b9a1624a
Vitaly Buka <vitalybuka@google.com>
2026-04-19 11:09:10 -0700

    stableHashValue
    Created using spr 1.3.7

--- llvm/lib/CodeGen/MachineBlockHashInfo.cpp
+++ llvm/lib/CodeGen/MachineBlockHashInfo.cpp
@@ -11,110 +11,13 @@
 //===----------------------------------------------------------------------===//
 
 #include "llvm/CodeGen/MachineBlockHashInfo.h"
-#include "llvm/ADT/ArrayRef.h"
+#include "llvm/ADT/Hashing.h"
 #include "llvm/ADT/StringRef.h"
 #include "llvm/CodeGen/Passes.h"
-#include "llvm/IR/Constants.h"
 #include "llvm/InitializePasses.h"
 #include "llvm/Target/TargetMachine.h"
 
 using namespace llvm;
-
-template <typename T>
-std::enable_if_t<is_integral_or_enum<T>::value, uint64_t>
-hashValue(const T &Val) {
-  return static_cast<uint64_t>(Val);
-}
-
-static uint64_t hashValue(StringRef S) {
-  uint64_t Hash = 0;
-  for (char C : S)
-    Hash = hashing::detail::hash_16_bytes(Hash, C);
-  return Hash;
-}
-
-template <typename T> uint64_t hashValue(ArrayRef<T> A) {
-  uint64_t Hash = 0;
-  for (const auto &C : A)
-    Hash = hashing::detail::hash_16_bytes(Hash, C);
-  return Hash;
-}
-
-static uint64_t hashValue(const APInt &I) {
-  return hashValue(ArrayRef(I.getRawData(), I.getNumWords()));
-}
-
-template <typename T> static uint64_t ignoreValue(const T *) { return 1; }
-
-static uint64_t hashCombine() { return 0; }
-
-template <typename T, typename... Ts>
-uint64_t hashCombine(const T &Hash, const Ts &...Args) {
-  return hashing::detail::hash_16_bytes(hashValue(Hash), hashCombine(Args...));
-}
-
-// Similar to hash_code llvm::hash_value(const MachineOperand &MO) but stable.
-static uint64_t hashOperand(const MachineOperand &MO) {
-  switch (MO.getType()) {
-  case MachineOperand::MO_Register:
-    // Register operands don't have target flags.
-    return hashCombine(MO.getType(), MO.getReg().id(), MO.getSubReg(),
-                       MO.isDef());
-  case MachineOperand::MO_Immediate:
-    return hashCombine(MO.getType(), MO.getTargetFlags(), MO.getImm());
-  case MachineOperand::MO_CImmediate:
-    return hashCombine(MO.getType(), MO.getTargetFlags(),
-                       MO.getCImm()->getValue());
-  case MachineOperand::MO_FPImmediate:
-    return hashCombine(MO.getType(), MO.getTargetFlags(),
-                       MO.getFPImm()->getValue().bitcastToAPInt());
-  case MachineOperand::MO_MachineBasicBlock:
-    return hashCombine(MO.getType(), MO.getTargetFlags(),
-                       ignoreValue(MO.getMBB()));
-  case MachineOperand::MO_FrameIndex:
-    return hashCombine(MO.getType(), MO.getTargetFlags(), MO.getIndex());
-  case MachineOperand::MO_ConstantPoolIndex:
-  case MachineOperand::MO_TargetIndex:
-    return hashCombine(MO.getType(), MO.getTargetFlags(), MO.getIndex(),
-                       MO.getOffset());
-  case MachineOperand::MO_JumpTableIndex:
-    return hashCombine(MO.getType(), MO.getTargetFlags(), MO.getIndex());
-  case MachineOperand::MO_ExternalSymbol:
-    return hashCombine(MO.getType(), MO.getTargetFlags(), MO.getOffset(),
-                       StringRef(MO.getSymbolName()));
-  case MachineOperand::MO_GlobalAddress:
-    return hashCombine(MO.getType(), MO.getTargetFlags(),
-                       ignoreValue(MO.getGlobal()), MO.getOffset());
-  case MachineOperand::MO_BlockAddress:
-    return hashCombine(MO.getType(), MO.getTargetFlags(),
-                       ignoreValue(MO.getBlockAddress()), MO.getOffset());
-  case MachineOperand::MO_RegisterMask:
-  case MachineOperand::MO_RegisterLiveOut:
-    return hashCombine(MO.getType(), MO.getTargetFlags(),
-                       ignoreValue(MO.getRegMask()));
-  case MachineOperand::MO_Metadata:
-    return hashCombine(MO.getType(), MO.getTargetFlags(),
-                       ignoreValue(MO.getMetadata()));
-  case MachineOperand::MO_MCSymbol:
-    return hashCombine(MO.getType(), MO.getTargetFlags(),
-                       ignoreValue(MO.getMCSymbol()));
-  case MachineOperand::MO_DbgInstrRef:
-    return hashCombine(MO.getType(), MO.getTargetFlags(),
-                       MO.getInstrRefInstrIndex(), MO.getInstrRefOpIndex());
-  case MachineOperand::MO_CFIIndex:
-    return hashCombine(MO.getType(), MO.getTargetFlags(), MO.getCFIIndex());
-  case MachineOperand::MO_IntrinsicID:
-    return hashCombine(MO.getType(), MO.getTargetFlags(), MO.getIntrinsicID());
-  case MachineOperand::MO_Predicate:
-    return hashCombine(MO.getType(), MO.getTargetFlags(), MO.getPredicate());
-  case MachineOperand::MO_ShuffleMask:
-    return hashCombine(MO.getType(), MO.getTargetFlags(), MO.getShuffleMask());
-  case MachineOperand::MO_LaneMask:
-    return hashCombine(MO.getType(), MO.getTargetFlags(),
-                       MO.getLaneMask().getAsInteger());
-  }
-  llvm_unreachable("Invalid machine operand type");
-}
 
 static uint64_t hashBlock(const MachineBasicBlock &MBB, bool HashOperands) {
   uint64_t Hash = 0;
@@ -124,8 +27,8 @@ static uint64_t hashBlock(const MachineBasicBlock &MBB, bool HashOperands) {
     Hash = hashing::detail::hash_16_bytes(Hash, MI.getOpcode());
     if (HashOperands) {
       for (unsigned i = 0; i < MI.getNumOperands(); i++) {
-        Hash =
-            hashing::detail::hash_16_bytes(Hash, hashOperand(MI.getOperand(i)));
+        Hash = hashing::detail::hash_16_bytes(
+            Hash, stableHashValue(MI.getOperand(i)));
       }
     }
   }
