ef58daa3f19ba92277ea79d309ff28adbaf87f20
Alex Maclean <amaclean@nvidia.com>
2026-04-20 15:30:43 -0700

    fixup

--- llvm/unittests/CodeGen/GlobalISel/LegalizerHelperTest.cpp
+++ llvm/unittests/CodeGen/GlobalISel/LegalizerHelperTest.cpp
@@ -3320,9 +3320,9 @@ TEST_F(AArch64GISelMITest, LowerFFloor) {
   CHECK: [[COPY:%[0-9]+]]:_(s64) = COPY
   CHECK: [[TRUNC:%[0-9]+]]:_(s64) = ninf G_INTRINSIC_TRUNC [[COPY]]
   CHECK: [[ZERO:%[0-9]+]]:_(s64) = G_FCONSTANT double 0.000000e+00
-  CHECK: [[CMP0:%[0-9]+]]:_(s1) = ninf G_FCMP floatpred(olt), [[COPY]]:_(s64), [[ZERO]]:_
-  CHECK: [[CMP1:%[0-9]+]]:_(s1) = ninf G_FCMP floatpred(one), [[COPY]]:_(s64), [[TRUNC]]:_
-  CHECK: [[AND:%[0-9]+]]:_(s1) = G_AND [[CMP0]]:_, [[CMP1]]:_
+  CHECK: [[CMP0:%[0-9]+]]:_(i1) = ninf G_FCMP floatpred(olt), [[COPY]]:_(s64), [[ZERO]]:_
+  CHECK: [[CMP1:%[0-9]+]]:_(i1) = ninf G_FCMP floatpred(one), [[COPY]]:_(s64), [[TRUNC]]:_
+  CHECK: [[AND:%[0-9]+]]:_(i1) = G_AND [[CMP0]]:_, [[CMP1]]:_
   CHECK: [[ITOFP:%[0-9]+]]:_(s64) = G_SITOFP [[AND]]
   = ninf G_FADD [[TRUNC]]:_, [[ITOFP]]:_
   )";
