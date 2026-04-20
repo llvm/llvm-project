6886381e6bac3622b2d962fd34d25f7104600299
Pavel Labath <pavel@labath.sk>
2026-04-20 13:03:57 +0000

    [libc] Add struct sockaddr_storage (on linux)
    This deceptively simple patch has one catch. The POSIX standard states:
    
    """
    When a pointer to a sockaddr_storage structure is converted to a pointer
    to a sockaddr structure, or vice versa, the ss_family member of the
    sockaddr_storage structure shall map onto the sa_family member of the
    sockaddr structure. When a pointer to a sockaddr_storage structure is
    converted to a pointer to a protocol-specific address structure, or vice
    versa, the ss_family member shall map onto a member of that structure
    that is of type sa_family_t that identifies the protocol's address
    family. When a pointer to a sockaddr structure is converted to a pointer
    to a protocol-specific address structure, or vice versa, the sa_family
    member shall map onto a member of that structure that is of type
    sa_family_t that identifies the protocol's address family. Additionally,
    the structures shall be defined in such a way that the compiler treats
    an access to the stored value of the sa_family_t member of any of these
    structures, via an lvalue expression whose type involves any other one
    of these structures, as permissible, despite the more restrictive
    expression rules on stored value access as stated in the ISO C standard.
    
    ...
    
    Note that defining the sockaddr_storage and sockaddr structures using
    only mechanisms defined in early editions of the ISO C standard may
    produce aliasing diagnostics when applications use casting between
    pointers to the various socket address structures. Because of the large
    body of existing code utilizing sockets in a way that could trigger
    undefined behavior due to strict aliasing rules, this standard mandates
    that these structures can alias each other for accessing the sa_family_t
    member of the structures (or other members for protocol-specific
    structure references), so as to preserve well-defined semantics. An
    implementation's header files may need to use anonymous unions, or even
    an implementation-specific extension, to comply with the requirements of
    this standard.
    """
    
    This is real problem w.r.t string aliasing rules, but this patch does
    not attempt to guarantee the above requirements -- because I haven't
    seen any other implementation do that. I'm also not sure that unions are
    sufficient to guarantee the above properties under a strict reading of
    the aliasing and "common initial sequence" rules. (If we wanted to go
    the non-standard extension route, we could slap may_alias on the
    relevant structures.)

---
+++ libc/test/src/sys/socket/linux/sockaddr_storage_test.cpp
@@ -0,0 +1,32 @@
+//===-- Unittests for struct sockaddr_storage -----------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "hdr/types/struct_sockaddr_storage.h"
+#include "hdr/types/struct_sockaddr_un.h"
+#include "src/string/memcpy.h"
+
+#include "test/UnitTest/LibcTest.h"
+
+#include <sys/socket.h> // For AF_UNIX
+
+using LlvmLibcSockaddrStorageTest = LIBC_NAMESPACE::testing::Test;
+
+TEST_F(LlvmLibcSockaddrStorageTest, SizeAndAlignment) {
+  // TODO: Add other sockaddr_* types as they are defined.
+  static_assert(sizeof(struct sockaddr_un) <= sizeof(struct sockaddr_storage));
+  static_assert(alignof(struct sockaddr_un) <=
+                alignof(struct sockaddr_storage));
+}
+
+TEST_F(LlvmLibcSockaddrStorageTest, MemberAccess) {
+  struct sockaddr_un sun = {};
+  sun.sun_family = AF_UNIX;
+  struct sockaddr_storage ss;
+  LIBC_NAMESPACE::memcpy(&ss, &sun, sizeof(sun));
+  ASSERT_EQ(ss.ss_family, static_cast<sa_family_t>(AF_UNIX));
+}
