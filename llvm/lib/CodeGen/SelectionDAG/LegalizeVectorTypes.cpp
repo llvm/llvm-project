c887007932a94158d5d2f865948042db34aab8f1
David Green <david.green@arm.com>
2026-04-20 09:51:04 +0100

    [AArch64] Fix lowering of non-power2 uitofp (#190921)
    The code in DAGTypeLegalizer::SplitVecOp_TruncateHelper attempts to use
    getFloatingPointVT(InElementSize/2), which is invalid for non-power2
    type sizes. Fall back to the existing SplitVecOp_UnaryOp in this case.

--- llvm/lib/CodeGen/SelectionDAG/LegalizeVectorTypes.cpp
+++ llvm/lib/CodeGen/SelectionDAG/LegalizeVectorTypes.cpp
@@ -4714,8 +4714,8 @@ SDValue DAGTypeLegalizer::SplitVecOp_TruncateHelper(SDNode *N) {
   // If the input elements are only 1/2 the width of the result elements,
   // just use the normal splitting. Our trick only work if there's room
   // to split more than once.
-  if (isTypeLegal(LoOutVT) ||
-      InElementSize <= OutElementSize * 2)
+  if (isTypeLegal(LoOutVT) || InElementSize <= OutElementSize * 2 ||
+      (IsFloat && !isPowerOf2_32(InElementSize)))
     return SplitVecOp_UnaryOp(N);
   SDLoc DL(N);
 
