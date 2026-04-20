48c1da55f1810b8dc1ca68c3bd4fd6cf6b683f6f
Kiriti Ponduri <123718855+udaykiriti@users.noreply.github.com>
2026-04-21 03:33:30 +0530

    [libc][math] Qualify floor functions to constexpr (#192791)
    Signed-off-by: udaykiriti <udaykiriti624@gmail.com>
    Co-authored-by: Muhammad Bassiouni <60100307+bassiounix@users.noreply.github.com>

--- libc/test/shared/shared_math_constexpr_test.cpp
+++ libc/test/shared/shared_math_constexpr_test.cpp
@@ -16,8 +16,9 @@
 //===----------------------------------------------------------------------===//
 
 static_assert(0.0 == LIBC_NAMESPACE::shared::ceil(0.0));
-static_assert(0.0 == LIBC_NAMESPACE::shared::log(1.0));
 static_assert(0.0 == LIBC_NAMESPACE::shared::copysign(0.0, 0.0));
+static_assert(1.0 == LIBC_NAMESPACE::shared::floor(1.2));
+static_assert(0.0 == LIBC_NAMESPACE::shared::log(1.0));
 
 //===----------------------------------------------------------------------===//
 //                       Float Tests
@@ -25,6 +26,7 @@ static_assert(0.0 == LIBC_NAMESPACE::shared::copysign(0.0, 0.0));
 
 static_assert(0.0f == LIBC_NAMESPACE::shared::ceilf(0.0f));
 static_assert(0.0f == LIBC_NAMESPACE::shared::copysignf(0.0f, 0.0f));
+static_assert(0.0f == LIBC_NAMESPACE::shared::floorf(0.0f));
 
 //===----------------------------------------------------------------------===//
 //                       Float16 Tests
@@ -34,6 +36,7 @@ static_assert(0.0f == LIBC_NAMESPACE::shared::copysignf(0.0f, 0.0f));
 
 static_assert(0.0f16 == LIBC_NAMESPACE::shared::ceilf16(0.0f16));
 static_assert(0.0f16 == LIBC_NAMESPACE::shared::copysignf16(0.0f16, 0.0f16));
+static_assert(3.0f16 == LIBC_NAMESPACE::shared::floorf16(3.7f16));
 
 #endif // LIBC_TYPES_HAS_FLOAT16
 
@@ -46,6 +49,7 @@ static_assert(0.0f16 == LIBC_NAMESPACE::shared::copysignf16(0.0f16, 0.0f16));
 
 static_assert(0.0L == LIBC_NAMESPACE::shared::ceill(0.0L));
 static_assert(0.0L == LIBC_NAMESPACE::shared::copysignl(0.0L, 0.0L));
+static_assert(0.0L == LIBC_NAMESPACE::shared::floorl(0.0L));
 
 #endif
 
@@ -60,6 +64,9 @@ static_assert(float128(0.0) ==
               LIBC_NAMESPACE::shared::copysignf128(float128(0.0),
                                                    float128(0.0)));
 
+static_assert(float128(0.0) ==
+              LIBC_NAMESPACE::shared::floorf128(float128(0.0)));
+
 #endif // LIBC_TYPES_HAS_FLOAT128
 
 //===----------------------------------------------------------------------===//
@@ -71,6 +78,8 @@ static_assert(bfloat16(0.0) == LIBC_NAMESPACE::shared::ceilbf16(bfloat16(0.0)));
 static_assert(bfloat16(0.0) ==
               LIBC_NAMESPACE::shared::copysignbf16(bfloat16(0.0),
                                                    bfloat16(0.0)));
+static_assert(bfloat16(0.0) ==
+              LIBC_NAMESPACE::shared::floorbf16(bfloat16(0.0f)));
 static_assert(bfloat16(0.0) ==
               LIBC_NAMESPACE::shared::logbbf16(bfloat16(1.0f)));
 
