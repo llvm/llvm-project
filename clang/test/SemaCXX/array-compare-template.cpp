3bb1cc96f10feb66fe9c232753122d23613c44d8
PrabbyDD <dhaliprabh@gmail.com>
2026-04-20 13:39:44 -0700

    Update clang/test/SemaCXX/array-compare-template.cpp
    Co-authored-by: A. Jiang <de34@live.cn>

--- clang/test/SemaCXX/array-compare-template.cpp
+++ clang/test/SemaCXX/array-compare-template.cpp
@@ -1,6 +1,6 @@
 // RUN: %clang_cc1 -std=c++26 -pedantic-errors -fsyntax-only -verify %s
 
-int main() {
+void test() {
   int a[1]{}, b[1]{};
   [](const auto &x, const auto &y) {
     return x == y; // expected-error {{comparison between two arrays is ill-formed in C++26}}
