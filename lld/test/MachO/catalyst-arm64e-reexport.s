fd94248aea16b2cc9f14fb0010e96bb8aae22390
Nuri Amari <nuriamari@fb.com>
2026-04-20 14:20:18 -0700

    Address PR Feedback

--- lld/test/MachO/catalyst-arm64e-reexport.s
+++ lld/test/MachO/catalyst-arm64e-reexport.s
@@ -11,6 +11,12 @@
 # RUN: %no-arg-lld -syslibroot %t/sdk -lSystem -dylib -arch arm64 \
 # RUN:     -platform_version mac-catalyst 13.15.0 14.0 %t/test.o -o /dev/null
 
+## Explicitly linking an arm64e-macos-only library should still be an error.
+# RUN: not %no-arg-lld -syslibroot %t/sdk -lSystem -dylib -arch arm64 \
+# RUN:     -platform_version mac-catalyst 13.15.0 14.0 %t/test.o -o /dev/null \
+# RUN:     -L %t/explicit -larm64e_only 2>&1 | FileCheck %s
+# CHECK: libarm64e_only.dylib) is incompatible with arm64 (macCatalyst13.15.0)
+
 #--- sdk/usr/lib/libSystem.tbd
 --- !tapi-tbd
 tbd-version:      4
@@ -22,13 +28,23 @@ reexported-libraries:
     libraries:    [ '/usr/lib/system/libsystem_eligibility.dylib' ]
 --- !tapi-tbd
 tbd-version:      4
-targets:          [ arm64e-macos ]
+targets:          [ x86_64-macos, arm64e-macos ]
 install-name:     '/usr/lib/system/libsystem_eligibility.dylib'
 current-version:  0001.001.1
 parent-umbrella:
-  - targets:      [ arm64e-macos ]
+  - targets:      [ x86_64-macos, arm64e-macos ]
     umbrella:     System
 exports:
-  - targets:      [ arm64e-macos ]
+  - targets:      [ x86_64-macos, arm64e-macos ]
     symbols:      [ _os_eligibility_get_domain_answer ]
+...
+#--- explicit/libarm64e_only.tbd
+--- !tapi-tbd
+tbd-version:      4
+targets:          [ x86_64-macos, arm64e-macos ]
+install-name:     '/usr/lib/libarm64e_only.dylib'
+current-version:  0001.001.1
+exports:
+  - targets:      [ x86_64-macos, arm64e-macos ]
+    symbols:      [ _arm64e_only_func ]
 ...
