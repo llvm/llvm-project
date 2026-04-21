877a06ae1ce03c5217633981863be654e8f03483
Ammarguellat <zahira.ammarguellat@intel.com>
2026-04-21 14:47:59 -0700

    Addressed review comments

--- clang/lib/Sema/SemaStmt.cpp
+++ clang/lib/Sema/SemaStmt.cpp
@@ -4739,11 +4739,10 @@ buildCapturedStmtCaptureList(Sema &S, CapturedRegionScopeInfo *RSI,
       ValueDecl *CapVar = Cap.getVariable();
       if (auto *BD = dyn_cast<BindingDecl>(CapVar))
         CapVar = cast<VarDecl>(BD->getDecomposedDecl());
-      Captures.push_back(CapturedStmt::Capture(Cap.getLocation(),
-                                               Cap.isReferenceCapture()
-                                                   ? CapturedStmt::VCK_ByRef
-                                                   : CapturedStmt::VCK_ByCopy,
-                                               cast<VarDecl>(CapVar)));
+      Captures.emplace_back(Cap.getLocation(),
+                            Cap.isReferenceCapture() ? CapturedStmt::VCK_ByRef
+                                                     : CapturedStmt::VCK_ByCopy,
+                            cast<VarDecl>(CapVar));
     }
     CaptureInits.push_back(Init.get());
   }
