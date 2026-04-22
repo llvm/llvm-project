1d2a975eeba73df3f2daa429416b594913e631bc
hulxv <hulxxv@gmail.com>
2026-04-22 22:46:08 +0200

    make constexpr

--- libc/test/shared/shared_math_constexpr_test.cpp
+++ libc/test/shared/shared_math_constexpr_test.cpp
@@ -16,6 +16,7 @@
 //===----------------------------------------------------------------------===//
 
 static_assert(0.0 == LIBC_NAMESPACE::shared::ceil(0.0));
+static_assert(0.0f == LIBC_NAMESPACE::shared::fdiv(0.0, 1.0));
 static_assert(0.0 == LIBC_NAMESPACE::shared::log(1.0));
 
 //===----------------------------------------------------------------------===//
@@ -42,6 +43,7 @@ static_assert(0.0f16 == LIBC_NAMESPACE::shared::ceilf16(0.0f16));
 #if 0 // Temporarily disable long double tests
 
 static_assert(0.0L == LIBC_NAMESPACE::shared::ceill(0.0L));
+static_assert(0.0f == LIBC_NAMESPACE::shared::fdivl(0.0L, 1.0L));
 
 #endif
 
@@ -52,6 +54,7 @@ static_assert(0.0L == LIBC_NAMESPACE::shared::ceill(0.0L));
 #ifdef LIBC_TYPES_HAS_FLOAT128
 
 static_assert(float128(0.0) == LIBC_NAMESPACE::shared::ceilf128(float128(0.0)));
+static_assert(0.0f == LIBC_NAMESPACE::shared::fdivf128(float128(0.0), float128(1.0)));
 
 #endif // LIBC_TYPES_HAS_FLOAT128
 
