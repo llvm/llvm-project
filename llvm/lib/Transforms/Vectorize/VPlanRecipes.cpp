1f51e2505e1268ad0621a9cfa4770125beb2c483
John Brawn <john.brawn@arm.com>
2026-04-22 15:04:17 +0100

    Make FIXME comment more accurate

--- llvm/lib/Transforms/Vectorize/VPlanRecipes.cpp
+++ llvm/lib/Transforms/Vectorize/VPlanRecipes.cpp
@@ -1259,7 +1259,10 @@ InstructionCost VPInstruction::computeCost(ElementCount VF,
   }
   case Instruction::FCmp:
   case Instruction::ICmp:
-    // FIXME: Scalar compares are handled by the legacy cost model
+    // FIXME: We don't handle scalar compares here yet. Scalar compares used for
+    // the loop exit condition are handled by the legacy cost model, but other
+    // scalar compares (e.g. in the middle block deciding whether to execute the
+    // scalar epilogue) aren't accounted for.
     if (vputils::onlyFirstLaneUsed(this))
       return 0;
     return getCostForRecipeWithOpcode(getOpcode(), VF, Ctx);
