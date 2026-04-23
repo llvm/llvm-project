186e068d21e7b13e9141c6bf142f0aba3f690c65
Alex Dutka <97711898+dutkalex@users.noreply.github.com>
2026-04-23 02:40:51 +0200

    improve test

--- clang-tools-extra/test/clang-tidy/checkers/readability/identifier-length-structured-bindings.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/readability/identifier-length-structured-bindings.cpp
@@ -1,6 +1,6 @@
 
 // RUN: %check_clang_tidy -std=c++17-or-later %s readability-identifier-length %t \
-// RUN: -config='{CheckOptions: {}}' \
+// RUN: -config='{CheckOptions: {readability-identifier-length.LineCountThreshold: 1}}' \
 // RUN: -- -fexceptions
 
 struct aggregate {
@@ -11,13 +11,18 @@ struct aggregate {
 
 aggregate get_data();
 
+template<typename... T>
+void doIt(T...);
+
 void shouldWarn() {
   auto [f, m, l] = get_data();
   // CHECK-MESSAGES: :[[@LINE-1]]:9: warning: binding variable name 'f' is too short, expected at least 2 characters [readability-identifier-length]
   // CHECK-MESSAGES: :[[@LINE-2]]:12: warning: binding variable name 'm' is too short, expected at least 2 characters [readability-identifier-length]
   // CHECK-MESSAGES: :[[@LINE-3]]:15: warning: binding variable name 'l' is too short, expected at least 2 characters [readability-identifier-length]
+  doIt(f, m, l);
 }
 
 void shouldNotWarn() {
-  auto [_, mid, last] = get_data();
+  auto [_, mid, last] = get_data(); // '_' is accepted by default
+  doIt(_, mid, last);
 }
