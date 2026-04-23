llvm/llvm-project

commit 5d78c8c939fabb48e3b97182323dd5f3e7102602

[DenseMap] Add memory barrier for sanitizers in getInlineBuckets/getLargeRep (#183457)
Add a compiler memory barrier to prevent optimizations from triggering
false positives on partially poisoned buckets in (HW)ASan.

Fixes #182720.

llvm/include/llvm/ADT/DenseMap.h
@@ -1037,6 +1037,12 @@ private:
     // Note that this cast does not violate aliasing rules as we assert that
     // the memory's dynamic type is the small, inline bucket buffer, and the
     // 'storage' is a POD containing a char buffer.
+#if defined(__SANITIZE_ADDRESS__) || defined(__SANITIZE_HWADDRESS__)
+    // Unless it's a sanitizer with container overflow detection. In this case
+    // some items in buckets can be partially poisoned, triggering sanitizer
+    // report on load.
+    __asm__ volatile("" ::: "memory");
+#endif
     return reinterpret_cast<const BucketT *>(&storage);
   }
 
@@ -1048,6 +1054,9 @@ private:
   const LargeRep *getLargeRep() const {
     assert(!Small);
     // Note, same rule about aliasing as with getInlineBuckets.
+#if defined(__SANITIZE_ADDRESS__) || defined(__SANITIZE_HWADDRESS__)
+    __asm__ volatile("" ::: "memory");
+#endif
     return reinterpret_cast<const LargeRep *>(&storage);
   }
 

commit 3553683e480fbaaace987b43c356e2f05a33a121

[DenseMap] Resolves asan + msvc build syntax errors
The problem was introduced by #183457 as an asan workaround for clang
builds to silence false positices, so the fix here just enables the
workaround for clang builds.

Fixes #189323

Signed-off-by: Davide Grohmann <davide.grohmann@arm.com>
Change-Id: Iad66b41f5a0a69269e3490bfc0e2cc2821122756

llvm/include/llvm/ADT/DenseMap.h
@@ -1037,7 +1037,8 @@ private:
     // Note that this cast does not violate aliasing rules as we assert that
     // the memory's dynamic type is the small, inline bucket buffer, and the
     // 'storage' is a POD containing a char buffer.
-#if defined(__SANITIZE_ADDRESS__) || defined(__SANITIZE_HWADDRESS__)
+#if defined(__clang__) &&                                                      \
+    (defined(__SANITIZE_ADDRESS__) || defined(__SANITIZE_HWADDRESS__))
     // Unless it's a sanitizer with container overflow detection. In this case
     // some items in buckets can be partially poisoned, triggering sanitizer
     // report on load.
@@ -1054,7 +1055,8 @@ private:
   const LargeRep *getLargeRep() const {
     assert(!Small);
     // Note, same rule about aliasing as with getInlineBuckets.
-#if defined(__SANITIZE_ADDRESS__) || defined(__SANITIZE_HWADDRESS__)
+#if defined(__clang__) &&                                                      \
+    (defined(__SANITIZE_ADDRESS__) || defined(__SANITIZE_HWADDRESS__))
     __asm__ volatile("" ::: "memory");
 #endif
     return reinterpret_cast<const LargeRep *>(&storage);