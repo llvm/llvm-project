11180bd6a3f54162352aa19f85e2c09aebd3a81e
Joshua Batista <jbatista@microsoft.com>
2026-04-23 10:46:17 -0700

    update volatile test

--- clang/test/SemaHLSL/Resources/Local-Resources/local_resource_volatile.hlsl
+++ clang/test/SemaHLSL/Resources/Local-Resources/local_resource_volatile.hlsl
@@ -10,9 +10,7 @@ RWByteAddressBuffer gBuf0 : register(u0);
 
 [numthreads(1,1,1)]
 void main(uint3 tid : SV_DispatchThreadID) {
-    volatile RWByteAddressBuffer buf = gBuf0;
-    // expected-note@*:* {{candidate function template not viable: 'this' argument has type 'volatile RWByteAddressBuffer', but method is not marked volatile}}
-    // expected-note@*:* {{candidate function not viable: 'this' argument has type 'volatile RWByteAddressBuffer', but method is not marked volatile}}
-    // expected-error@+1 {{no matching member function for call to 'Store'}}
-    buf.Store(tid.x * 4, 42);
+    // expected-error@+1 {{unknown type name 'volatile'}}
+    volatile RWByteAddressBuffer buf = gBuf0; // expected-error {{expected ';' at end of declaration}}
+    buf.Store(tid.x * 4, 42); // expected-error {{use of undeclared identifier 'buf'}}
 }
