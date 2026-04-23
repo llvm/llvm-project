cbba59eb5839303ef9f6be3e9344e357892dc9c6
Alexander Kornienko <alexfh@google.com>
2026-04-23 11:47:49 +0200

    Fix -Wformat diagnostic after #190965

--- libunwind/src/CompactUnwinder.hpp
+++ libunwind/src/CompactUnwinder.hpp
@@ -335,7 +335,7 @@ int CompactUnwinder_x86_64<A>::stepWithCompactEncodingRBPFrame(
     default:
       (void)functionStart;
       _LIBUNWIND_DEBUG_LOG("bad register for RBP frame, encoding=%08X for "
-                           "function starting at 0x%llX",
+                           "function starting at 0x%" PRIu64 "X",
                             compactEncoding, functionStart);
       _LIBUNWIND_ABORT("invalid compact unwind encoding");
     }
@@ -454,7 +454,7 @@ int CompactUnwinder_x86_64<A>::stepWithCompactEncodingFrameless(
       break;
     default:
       _LIBUNWIND_DEBUG_LOG("bad register for frameless, encoding=%08X for "
-                           "function starting at 0x%llX",
+                           "function starting at 0x%" PRIu64 "X",
                             encoding, functionStart);
       _LIBUNWIND_ABORT("invalid compact unwind encoding");
     }
