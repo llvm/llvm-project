4d085bb7bc1e0178c4624797afd04de69703184c
Jonathan Thackray <jonathan.thackray@arm.com>
2026-04-23 17:40:07 +0100

    fixup! Add `HasSVE_F16MM` and use `Has_F16MM` just for Neon.

--- llvm/test/MC/AArch64/SVE2p2/fmmla.s
+++ llvm/test/MC/AArch64/SVE2p2/fmmla.s
@@ -15,25 +15,25 @@
 fmmla z0.h, z0.h, z0.h
 // CHECK-INST: fmmla z0.h, z0.h, z0.h
 // CHECK-ENCODING: encoding: [0x00,0xe0,0xa0,0x64]
-// CHECK-ERROR: instruction requires: f16mm sve2p2
+// CHECK-ERROR: instruction requires: sve2p2 f16mm
 // CHECK-UNKNOWN: 64a0e000 <unknown>
 
 fmmla z10.h, z10.h, z10.h
 // CHECK-INST: fmmla z10.h, z10.h, z10.h
 // CHECK-ENCODING: encoding: [0x4a,0xe1,0xaa,0x64]
-// CHECK-ERROR: instruction requires: f16mm sve2p2
+// CHECK-ERROR: instruction requires: sve2p2 f16mm
 // CHECK-UNKNOWN: 64aae14a <unknown>
 
 fmmla z21.h, z21.h, z21.h
 // CHECK-INST: fmmla z21.h, z21.h, z21.h
 // CHECK-ENCODING: encoding: [0xb5,0xe2,0xb5,0x64]
-// CHECK-ERROR: instruction requires: f16mm sve2p2
+// CHECK-ERROR: instruction requires: sve2p2 f16mm
 // CHECK-UNKNOWN: 64b5e2b5 <unknown>
 
 fmmla z31.h, z31.h, z31.h
 // CHECK-INST: fmmla z31.h, z31.h, z31.h
 // CHECK-ENCODING: encoding: [0xff,0xe3,0xbf,0x64]
-// CHECK-ERROR: instruction requires: f16mm sve2p2
+// CHECK-ERROR: instruction requires: sve2p2 f16mm
 // CHECK-UNKNOWN: 64bfe3ff <unknown>
 
 movprfx z0, z7
@@ -41,5 +41,5 @@ fmmla z0.h, z1.h, z2.h
 // CHECK-INST: movprfx z0, z7
 // CHECK-INST: fmmla z0.h, z1.h, z2.h
 // CHECK-ENCODING: encoding: [0x20,0xe0,0xa2,0x64]
-// CHECK-ERROR: instruction requires: f16mm sve2p2
+// CHECK-ERROR: instruction requires: sve2p2 f16mm
 // CHECK-UNKNOWN: 64a2e020 <unknown>
