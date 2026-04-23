54d0a84687e865edb5acea6e9f2487b5148d412b
Naveen Seth Hanig <naveen.hanig@oulook.com>
2026-04-23 02:37:59 +0200

    Fix windows test

--- clang/test/Driver/modules-driver-filetype-mismatch.cpp
+++ clang/test/Driver/modules-driver-filetype-mismatch.cpp
@@ -2,7 +2,7 @@
 //
 // RUN: not %clang -### -fmodules-driver -std=c++20 -x c++ %s 2>&1 \
 // RUN:   | sed 's:\\\\\?:/:g' \
-// RUN:   | FileCheck -DTHISFILE=%s %s
+// RUN:   | FileCheck -DTHISFILE=%/s %s
 //
 // CHECK: clang: error: module 'M' is defined in file '[[THISFILE]]', but module declarations are only allowed in C++ module inputs; use the '.cppm' extension or '-x c++module'
 export module M;
