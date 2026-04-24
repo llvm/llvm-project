893a90e3993ca41801b7f7cfea8b269de8176461
Erdem Yılmaz <r3nzthecodegod@gmail.com>
2026-04-24 20:58:24 +0300

    Update llvm/lib/Transforms/InstCombine/InstCombineMulDivRem.cpp
    Co-authored-by: Yingwei Zheng <dtcxzyw@qq.com>

--- llvm/lib/Transforms/InstCombine/InstCombineMulDivRem.cpp
+++ llvm/lib/Transforms/InstCombine/InstCombineMulDivRem.cpp
@@ -1275,8 +1275,8 @@ static Value *foldIDivShl(BinaryOperator &I, InstCombiner::BuilderTy &Builder) {
   // X u/ (Y << Z) --> (X >> Z) u/ Y
   // The nuw flag ensures Y << Z == Y * 2^Z without overflow.
   // Requires one-use to avoid keeping the original shl alive.
-  if (!IsSigned && Op1->hasOneUse() &&
-      match(Op1, m_NUWShl(m_Value(Y), m_Value(Z)))) {
+  if (!IsSigned &&
+      match(Op1, m_OneUse(m_NUWShl(m_Value(Y), m_Value(Z))))) {
     Value *NewLShr = Builder.CreateLShr(Op0, Z, "", I.isExact());
     return Builder.CreateUDiv(NewLShr, Y, "", I.isExact());
   }
