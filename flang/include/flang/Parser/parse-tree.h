2d8966ed25cee19c9f114810b6d5b08af5365614
Jack Styles <jack.styles@arm.com>
2026-04-24 09:58:43 +0100

    [Flang] Add `INLINEALWAYS` Compiler Directive (#192674)
    Adds support for the INLINEALWAYS Compiler Directive to Flang. This was
    previously supported in Classic-Flang, and works in the same way as
    FORCEINLINE.
    
    It can either be defined at the call site, or within the function the
    user wishes to inline.
    
    The missing support was highlighted while building an opensource
    benchmark, as build warnings were indicating that this compiler
    directive was being ignored.

--- flang/include/flang/Parser/parse-tree.h
+++ flang/include/flang/Parser/parse-tree.h
@@ -3374,6 +3374,7 @@ struct StmtFunctionStmt {
 // !DIR$ FORCEINLINE
 // !DIR$ INLINE
 // !DIR$ NOINLINE
+// !DIR$ INLINEALWAYS
 // !DIR$ IVDEP
 // !DIR$ SIMD
 // !DIR$ <anything else>
@@ -3411,6 +3412,9 @@ struct CompilerDirective {
     WRAPPER_CLASS_BOILERPLATE(
         Prefetch, std::list<common::Indirection<Designator>>);
   };
+  struct InlineAlways {
+    WRAPPER_CLASS_BOILERPLATE(InlineAlways, std::optional<Name>);
+  };
   EMPTY_CLASS(NoVector);
   EMPTY_CLASS(NoUnroll);
   EMPTY_CLASS(NoUnrollAndJam);
@@ -3424,7 +3428,7 @@ struct CompilerDirective {
   std::variant<std::list<IgnoreTKR>, LoopCount, std::list<AssumeAligned>,
       VectorAlways, VectorLength, std::list<NameValue>, Unroll, UnrollAndJam,
       Unrecognized, NoVector, NoUnroll, NoUnrollAndJam, ForceInline, Inline,
-      NoInline, Prefetch, IVDep, Simd>
+      NoInline, InlineAlways, Prefetch, IVDep, Simd>
       u;
 };
 
