8a617da9906d9d043d02c979810932310e363de0
Eugene Epshteyn <eepshteyn@nvidia.com>
2026-04-23 23:01:19 -0400

    [flang][NFC] Add more CHECK details for hlfir.as_expr, hlfir.associate, and fir.absent
    Expand the previously condensed fir.if region check patterns in
    optional-value-caller.f90 to include the full hlfir.as_expr /
    hlfir.associate and fir.absent triples, including the fir.result
    and hlfir.end_associate operations. This improves coverage of the
    HLFIR optional-value call-site lowering.

--- flang/test/Lower/optional-value-caller.f90
+++ flang/test/Lower/optional-value-caller.f90
@@ -107,14 +107,19 @@ subroutine test_scalar_ptr(i)
 ! CHECK:  %[[VAL_5:.*]] = arith.constant 0 : i64
 ! CHECK:  %[[VAL_6:.*]] = arith.cmpi ne, %[[VAL_4]], %[[VAL_5]] : i64
 ! CHECK:  %[[VAL_7:.*]]:3 = fir.if %[[VAL_6]] -> (!fir.ref<i32>, !fir.ref<i32>, i1) {
-! CHECK:    fir.load %[[VAL_0]]#0 : !fir.ref<!fir.box<!fir.ptr<i32>>>
-! CHECK:    fir.box_addr
-! CHECK:    hlfir.as_expr
-! CHECK:    hlfir.associate
+! CHECK:    %[[VAL_PLOAD:.*]] = fir.load %[[VAL_0]]#0 : !fir.ref<!fir.box<!fir.ptr<i32>>>
+! CHECK:    %[[VAL_PADDR:.*]] = fir.box_addr %[[VAL_PLOAD]] : (!fir.box<!fir.ptr<i32>>) -> !fir.ptr<i32>
+! CHECK:    %[[VAL_EXPR:.*]] = hlfir.as_expr %[[VAL_PADDR]] : (!fir.ptr<i32>) -> !hlfir.expr<i32>
+! CHECK:    %[[VAL_ASSOC:.*]]:3 = hlfir.associate %[[VAL_EXPR]] {adapt.valuebyref} : (!hlfir.expr<i32>) -> (!fir.ref<i32>, !fir.ref<i32>, i1)
+! CHECK:    fir.result %[[VAL_ASSOC]]#0, %[[VAL_ASSOC]]#1, %[[VAL_ASSOC]]#2 : !fir.ref<i32>, !fir.ref<i32>, i1
 ! CHECK:  } else {
-! CHECK:    fir.absent !fir.ref<i32>
+! CHECK:    %[[ABSENT_0:.*]] = fir.absent !fir.ref<i32>
+! CHECK:    %[[ABSENT_1:.*]] = fir.absent !fir.ref<i32>
+! CHECK:    %[[FALSE:.*]] = arith.constant false
+! CHECK:    fir.result %[[ABSENT_0]], %[[ABSENT_1]], %[[FALSE]] : !fir.ref<i32>, !fir.ref<i32>, i1
 ! CHECK:  }
 ! CHECK:  fir.call @_QPscalar(%[[VAL_7]]#0) {{.*}}: (!fir.ref<i32>) -> ()
+! CHECK:  hlfir.end_associate %[[VAL_7]]#1, %[[VAL_7]]#2 : !fir.ref<i32>, i1
 end subroutine
 
 ! CHECK-LABEL: func @_QMtestPtest_scalar_simple_var(
@@ -141,12 +146,19 @@ subroutine test_scalar_alloc(i)
 ! CHECK:  %[[VAL_5:.*]] = arith.constant 0 : i64
 ! CHECK:  %[[VAL_6:.*]] = arith.cmpi ne, %[[VAL_4]], %[[VAL_5]] : i64
 ! CHECK:  %[[VAL_7:.*]]:3 = fir.if %[[VAL_6]] -> (!fir.ref<i32>, !fir.ref<i32>, i1) {
-! CHECK:    hlfir.as_expr
-! CHECK:    hlfir.associate
+! CHECK:    %[[VAL_HLOAD:.*]] = fir.load %[[VAL_0]]#0 : !fir.ref<!fir.box<!fir.heap<i32>>>
+! CHECK:    %[[VAL_HADDR:.*]] = fir.box_addr %[[VAL_HLOAD]] : (!fir.box<!fir.heap<i32>>) -> !fir.heap<i32>
+! CHECK:    %[[VAL_EXPR:.*]] = hlfir.as_expr %[[VAL_HADDR]] : (!fir.heap<i32>) -> !hlfir.expr<i32>
+! CHECK:    %[[VAL_ASSOC:.*]]:3 = hlfir.associate %[[VAL_EXPR]] {adapt.valuebyref} : (!hlfir.expr<i32>) -> (!fir.ref<i32>, !fir.ref<i32>, i1)
+! CHECK:    fir.result %[[VAL_ASSOC]]#0, %[[VAL_ASSOC]]#1, %[[VAL_ASSOC]]#2 : !fir.ref<i32>, !fir.ref<i32>, i1
 ! CHECK:  } else {
-! CHECK:    fir.absent !fir.ref<i32>
+! CHECK:    %[[ABSENT_0:.*]] = fir.absent !fir.ref<i32>
+! CHECK:    %[[ABSENT_1:.*]] = fir.absent !fir.ref<i32>
+! CHECK:    %[[FALSE:.*]] = arith.constant false
+! CHECK:    fir.result %[[ABSENT_0]], %[[ABSENT_1]], %[[FALSE]] : !fir.ref<i32>, !fir.ref<i32>, i1
 ! CHECK:  }
 ! CHECK:  fir.call @_QPscalar(%[[VAL_7]]#0) {{.*}}: (!fir.ref<i32>) -> ()
+! CHECK:  hlfir.end_associate %[[VAL_7]]#1, %[[VAL_7]]#2 : !fir.ref<i32>, i1
 end subroutine
 
 ! CHECK-LABEL: func @_QMtestPtest_ptr_2() {
@@ -162,12 +174,19 @@ subroutine test_ptr_2()
 ! CHECK:  %[[VAL_7:.*]] = arith.constant 0 : i64
 ! CHECK:  %[[VAL_8:.*]] = arith.cmpi ne, %[[VAL_6]], %[[VAL_7]] : i64
 ! CHECK:  %[[VAL_9:.*]]:3 = fir.if %[[VAL_8]] -> (!fir.ref<i32>, !fir.ref<i32>, i1) {
-! CHECK:    hlfir.as_expr
-! CHECK:    hlfir.associate
+! CHECK:    %[[VAL_RLOAD:.*]] = fir.load %[[VAL_3]]#0 : !fir.ref<!fir.box<!fir.ptr<i32>>>
+! CHECK:    %[[VAL_RADDR:.*]] = fir.box_addr %[[VAL_RLOAD]] : (!fir.box<!fir.ptr<i32>>) -> !fir.ptr<i32>
+! CHECK:    %[[VAL_EXPR:.*]] = hlfir.as_expr %[[VAL_RADDR]] : (!fir.ptr<i32>) -> !hlfir.expr<i32>
+! CHECK:    %[[VAL_ASSOC:.*]]:3 = hlfir.associate %[[VAL_EXPR]] {adapt.valuebyref} : (!hlfir.expr<i32>) -> (!fir.ref<i32>, !fir.ref<i32>, i1)
+! CHECK:    fir.result %[[VAL_ASSOC]]#0, %[[VAL_ASSOC]]#1, %[[VAL_ASSOC]]#2 : !fir.ref<i32>, !fir.ref<i32>, i1
 ! CHECK:  } else {
-! CHECK:    fir.absent !fir.ref<i32>
+! CHECK:    %[[ABSENT_0:.*]] = fir.absent !fir.ref<i32>
+! CHECK:    %[[ABSENT_1:.*]] = fir.absent !fir.ref<i32>
+! CHECK:    %[[FALSE:.*]] = arith.constant false
+! CHECK:    fir.result %[[ABSENT_0]], %[[ABSENT_1]], %[[FALSE]] : !fir.ref<i32>, !fir.ref<i32>, i1
 ! CHECK:  }
 ! CHECK:  fir.call @_QPscalar(%[[VAL_9]]#0) {{.*}}: (!fir.ref<i32>) -> ()
+! CHECK:  hlfir.end_associate %[[VAL_9]]#1, %[[VAL_9]]#2 : !fir.ref<i32>, i1
 end subroutine
 
 ! CHECK-LABEL: func @_QMtestPtest_array(
@@ -205,13 +224,19 @@ subroutine test_array2(i, n)
 ! CHECK:  %[[VAL_SHAPE:.*]] = fir.shape %[[VAL_6]] : (index) -> !fir.shape<1>
 ! CHECK:  %[[VAL_I:.*]]:2 = hlfir.declare %[[ARG0]](%[[VAL_SHAPE]])
 ! CHECK:  %[[VAL_7:.*]] = fir.is_present %[[VAL_I]]#0 : (!fir.box<!fir.array<?xi32>>) -> i1
-! CHECK:  %[[VAL_8:.*]]:3 = fir.if %[[VAL_7]]
-! CHECK:    hlfir.as_expr
-! CHECK:    hlfir.associate
+! CHECK:  %[[VAL_8:.*]]:3 = fir.if %[[VAL_7]] -> (!fir.ref<!fir.array<100xi32>>, !fir.ref<!fir.array<?xi32>>, i1) {
+! CHECK:    %[[VAL_EXPR:.*]] = hlfir.as_expr %[[VAL_I]]#0 : (!fir.box<!fir.array<?xi32>>) -> !hlfir.expr<?xi32>
+! CHECK:    %[[VAL_ASSOC:.*]]:3 = hlfir.associate %[[VAL_EXPR]](%[[VAL_SHAPE]]) {adapt.valuebyref} : (!hlfir.expr<?xi32>, !fir.shape<1>) -> (!fir.box<!fir.array<?xi32>>, !fir.ref<!fir.array<?xi32>>, i1)
+! CHECK:    %[[VAL_CAST:.*]] = fir.convert %[[VAL_ASSOC]]#1 : (!fir.ref<!fir.array<?xi32>>) -> !fir.ref<!fir.array<100xi32>>
+! CHECK:    fir.result %[[VAL_CAST]], %[[VAL_ASSOC]]#1, %[[VAL_ASSOC]]#2 : !fir.ref<!fir.array<100xi32>>, !fir.ref<!fir.array<?xi32>>, i1
 ! CHECK:  } else {
-! CHECK:    fir.absent
+! CHECK:    %[[ABSENT_0:.*]] = fir.absent !fir.ref<!fir.array<100xi32>>
+! CHECK:    %[[ABSENT_1:.*]] = fir.absent !fir.ref<!fir.array<?xi32>>
+! CHECK:    %[[FALSE:.*]] = arith.constant false
+! CHECK:    fir.result %[[ABSENT_0]], %[[ABSENT_1]], %[[FALSE]] : !fir.ref<!fir.array<100xi32>>, !fir.ref<!fir.array<?xi32>>, i1
 ! CHECK:  }
 ! CHECK:  fir.call @_QParray(%[[VAL_8]]#0) {{.*}}: (!fir.ref<!fir.array<100xi32>>) -> ()
+! CHECK:  hlfir.end_associate %[[VAL_8]]#1, %[[VAL_8]]#2 : !fir.ref<!fir.array<?xi32>>, i1
 end subroutine
 
 ! CHECK-LABEL: func @_QMtestPtest_dyn_array(
@@ -230,13 +255,18 @@ subroutine test_dyn_array(i, n)
 ! CHECK:  %[[VAL_SHAPE:.*]] = fir.shape %[[VAL_6]] : (index) -> !fir.shape<1>
 ! CHECK:  %[[VAL_I:.*]]:2 = hlfir.declare %[[ARG0]](%[[VAL_SHAPE]])
 ! CHECK:  %[[VAL_7:.*]] = fir.is_present %[[VAL_I]]#0 : (!fir.box<!fir.array<?xi32>>) -> i1
-! CHECK:  %[[VAL_8:.*]]:3 = fir.if %[[VAL_7]]
-! CHECK:    hlfir.as_expr
-! CHECK:    hlfir.associate
+! CHECK:  %[[VAL_8:.*]]:3 = fir.if %[[VAL_7]] -> (!fir.ref<!fir.array<?xi32>>, !fir.ref<!fir.array<?xi32>>, i1) {
+! CHECK:    %[[VAL_EXPR:.*]] = hlfir.as_expr %[[VAL_I]]#0 : (!fir.box<!fir.array<?xi32>>) -> !hlfir.expr<?xi32>
+! CHECK:    %[[VAL_ASSOC:.*]]:3 = hlfir.associate %[[VAL_EXPR]](%[[VAL_SHAPE]]) {adapt.valuebyref} : (!hlfir.expr<?xi32>, !fir.shape<1>) -> (!fir.box<!fir.array<?xi32>>, !fir.ref<!fir.array<?xi32>>, i1)
+! CHECK:    fir.result %[[VAL_ASSOC]]#1, %[[VAL_ASSOC]]#1, %[[VAL_ASSOC]]#2 : !fir.ref<!fir.array<?xi32>>, !fir.ref<!fir.array<?xi32>>, i1
 ! CHECK:  } else {
-! CHECK:    fir.absent
+! CHECK:    %[[ABSENT_0:.*]] = fir.absent !fir.ref<!fir.array<?xi32>>
+! CHECK:    %[[ABSENT_1:.*]] = fir.absent !fir.ref<!fir.array<?xi32>>
+! CHECK:    %[[FALSE:.*]] = arith.constant false
+! CHECK:    fir.result %[[ABSENT_0]], %[[ABSENT_1]], %[[FALSE]] : !fir.ref<!fir.array<?xi32>>, !fir.ref<!fir.array<?xi32>>, i1
 ! CHECK:  }
 ! CHECK:  fir.call @_QPdyn_array(%[[VAL_8]]#0, %[[VAL_N]]#0) {{.*}}: (!fir.ref<!fir.array<?xi32>>, !fir.ref<i64>) -> ()
+! CHECK:  hlfir.end_associate %[[VAL_8]]#1, %[[VAL_8]]#2 : !fir.ref<!fir.array<?xi32>>, i1
 end subroutine
 
 ! CHECK-LABEL: func @_QMtestPtest_dyn_array_from_assumed(
@@ -249,14 +279,20 @@ subroutine test_dyn_array_from_assumed(i, n)
 ! CHECK:  %[[VAL_I:.*]]:2 = hlfir.declare %[[ARG0]]
 ! CHECK:  %[[VAL_N:.*]]:2 = hlfir.declare %[[ARG1]]
 ! CHECK:  %[[VAL_2:.*]] = fir.is_present %[[VAL_I]]#0 : (!fir.box<!fir.array<?xi32>>) -> i1
-! CHECK:  %[[VAL_3:.*]]:3 = fir.if %[[VAL_2]]
-! CHECK:    hlfir.as_expr
-! CHECK:    fir.box_dims
-! CHECK:    hlfir.associate
+! CHECK:  %[[VAL_3:.*]]:3 = fir.if %[[VAL_2]] -> (!fir.ref<!fir.array<?xi32>>, !fir.ref<!fir.array<?xi32>>, i1) {
+! CHECK:    %[[VAL_EXPR:.*]] = hlfir.as_expr %[[VAL_I]]#0 : (!fir.box<!fir.array<?xi32>>) -> !hlfir.expr<?xi32>
+! CHECK:    %[[VAL_DIMS:.*]]:3 = fir.box_dims %[[VAL_I]]#0, %{{.*}} : (!fir.box<!fir.array<?xi32>>, index) -> (index, index, index)
+! CHECK:    %[[VAL_SHAPE:.*]] = fir.shape %[[VAL_DIMS]]#1 : (index) -> !fir.shape<1>
+! CHECK:    %[[VAL_ASSOC:.*]]:3 = hlfir.associate %[[VAL_EXPR]](%[[VAL_SHAPE]]) {adapt.valuebyref} : (!hlfir.expr<?xi32>, !fir.shape<1>) -> (!fir.box<!fir.array<?xi32>>, !fir.ref<!fir.array<?xi32>>, i1)
+! CHECK:    fir.result %[[VAL_ASSOC]]#1, %[[VAL_ASSOC]]#1, %[[VAL_ASSOC]]#2 : !fir.ref<!fir.array<?xi32>>, !fir.ref<!fir.array<?xi32>>, i1
 ! CHECK:  } else {
-! CHECK:    fir.absent
+! CHECK:    %[[ABSENT_0:.*]] = fir.absent !fir.ref<!fir.array<?xi32>>
+! CHECK:    %[[ABSENT_1:.*]] = fir.absent !fir.ref<!fir.array<?xi32>>
+! CHECK:    %[[FALSE:.*]] = arith.constant false
+! CHECK:    fir.result %[[ABSENT_0]], %[[ABSENT_1]], %[[FALSE]] : !fir.ref<!fir.array<?xi32>>, !fir.ref<!fir.array<?xi32>>, i1
 ! CHECK:  }
 ! CHECK:  fir.call @_QPdyn_array(%[[VAL_3]]#0, %[[VAL_N]]#0) {{.*}}: (!fir.ref<!fir.array<?xi32>>, !fir.ref<i64>) -> ()
+! CHECK:  hlfir.end_associate %[[VAL_3]]#1, %[[VAL_3]]#2 : !fir.ref<!fir.array<?xi32>>, i1
 end subroutine
 
 ! CHECK-LABEL: func @_QMtestPtest_array_ptr(
@@ -270,13 +306,22 @@ subroutine test_array_ptr(i)
 ! CHECK:  %[[VAL_4:.*]] = fir.convert %[[VAL_3]] : (!fir.ptr<!fir.array<?xi32>>) -> i64
 ! CHECK:  %[[VAL_5:.*]] = arith.constant 0 : i64
 ! CHECK:  %[[VAL_6:.*]] = arith.cmpi ne, %[[VAL_4]], %[[VAL_5]] : i64
-! CHECK:  %[[VAL_7:.*]]:3 = fir.if %[[VAL_6]]
-! CHECK:    hlfir.as_expr
-! CHECK:    hlfir.associate
+! CHECK:  %[[VAL_7:.*]]:3 = fir.if %[[VAL_6]] -> (!fir.ref<!fir.array<100xi32>>, !fir.ref<!fir.array<?xi32>>, i1) {
+! CHECK:    %[[VAL_PLOAD:.*]] = fir.load %[[VAL_0]]#0 : !fir.ref<!fir.box<!fir.ptr<!fir.array<?xi32>>>>
+! CHECK:    %[[VAL_EXPR:.*]] = hlfir.as_expr %[[VAL_PLOAD]] : (!fir.box<!fir.ptr<!fir.array<?xi32>>>) -> !hlfir.expr<?xi32>
+! CHECK:    %[[VAL_DIMS:.*]]:3 = fir.box_dims %[[VAL_PLOAD]], %{{.*}} : (!fir.box<!fir.ptr<!fir.array<?xi32>>>, index) -> (index, index, index)
+! CHECK:    %[[VAL_SHAPE:.*]] = fir.shape %[[VAL_DIMS]]#1 : (index) -> !fir.shape<1>
+! CHECK:    %[[VAL_ASSOC:.*]]:3 = hlfir.associate %[[VAL_EXPR]](%[[VAL_SHAPE]]) {adapt.valuebyref} : (!hlfir.expr<?xi32>, !fir.shape<1>) -> (!fir.box<!fir.array<?xi32>>, !fir.ref<!fir.array<?xi32>>, i1)
+! CHECK:    %[[VAL_CAST:.*]] = fir.convert %[[VAL_ASSOC]]#1 : (!fir.ref<!fir.array<?xi32>>) -> !fir.ref<!fir.array<100xi32>>
+! CHECK:    fir.result %[[VAL_CAST]], %[[VAL_ASSOC]]#1, %[[VAL_ASSOC]]#2 : !fir.ref<!fir.array<100xi32>>, !fir.ref<!fir.array<?xi32>>, i1
 ! CHECK:  } else {
-! CHECK:    fir.absent
+! CHECK:    %[[ABSENT_0:.*]] = fir.absent !fir.ref<!fir.array<100xi32>>
+! CHECK:    %[[ABSENT_1:.*]] = fir.absent !fir.ref<!fir.array<?xi32>>
+! CHECK:    %[[FALSE:.*]] = arith.constant false
+! CHECK:    fir.result %[[ABSENT_0]], %[[ABSENT_1]], %[[FALSE]] : !fir.ref<!fir.array<100xi32>>, !fir.ref<!fir.array<?xi32>>, i1
 ! CHECK:  }
 ! CHECK:  fir.call @_QParray(%[[VAL_7]]#0) {{.*}}: (!fir.ref<!fir.array<100xi32>>) -> ()
+! CHECK:  hlfir.end_associate %[[VAL_7]]#1, %[[VAL_7]]#2 : !fir.ref<!fir.array<?xi32>>, i1
 end subroutine
 
 ! CHECK-LABEL: func @_QMtestPtest_char(
@@ -287,13 +332,18 @@ subroutine test_char(c)
 ! CHECK:  %[[VAL_1:.*]]:2 = fir.unboxchar %[[ARG0]] : (!fir.boxchar<1>) -> (!fir.ref<!fir.char<1,?>>, index)
 ! CHECK:  %[[VAL_2:.*]]:2 = hlfir.declare %[[VAL_1]]#0 typeparams %[[VAL_1]]#1
 ! CHECK:  %[[VAL_3:.*]] = fir.is_present %[[VAL_2]]#0 : (!fir.boxchar<1>) -> i1
-! CHECK:  %[[VAL_4:.*]]:3 = fir.if %[[VAL_3]]
-! CHECK:    hlfir.as_expr
-! CHECK:    hlfir.associate
+! CHECK:  %[[VAL_4:.*]]:3 = fir.if %[[VAL_3]] -> (!fir.boxchar<1>, !fir.ref<!fir.char<1,?>>, i1) {
+! CHECK:    %[[VAL_EXPR:.*]] = hlfir.as_expr %[[VAL_2]]#0 : (!fir.boxchar<1>) -> !hlfir.expr<!fir.char<1,?>>
+! CHECK:    %[[VAL_ASSOC:.*]]:3 = hlfir.associate %[[VAL_EXPR]] typeparams %[[VAL_1]]#1 {adapt.valuebyref} : (!hlfir.expr<!fir.char<1,?>>, index) -> (!fir.boxchar<1>, !fir.ref<!fir.char<1,?>>, i1)
+! CHECK:    fir.result %[[VAL_ASSOC]]#0, %[[VAL_ASSOC]]#1, %[[VAL_ASSOC]]#2 : !fir.boxchar<1>, !fir.ref<!fir.char<1,?>>, i1
 ! CHECK:  } else {
-! CHECK:    fir.absent
+! CHECK:    %[[ABSENT_0:.*]] = fir.absent !fir.boxchar<1>
+! CHECK:    %[[ABSENT_1:.*]] = fir.absent !fir.ref<!fir.char<1,?>>
+! CHECK:    %[[FALSE:.*]] = arith.constant false
+! CHECK:    fir.result %[[ABSENT_0]], %[[ABSENT_1]], %[[FALSE]] : !fir.boxchar<1>, !fir.ref<!fir.char<1,?>>, i1
 ! CHECK:  }
 ! CHECK:  fir.call @_QPdyn_char(%[[VAL_4]]#0) {{.*}}: (!fir.boxchar<1>) -> ()
+! CHECK:  hlfir.end_associate %[[VAL_4]]#1, %[[VAL_4]]#2 : !fir.ref<!fir.char<1,?>>, i1
 end subroutine
 
 ! CHECK-LABEL: func @_QMtestPtest_char_ptr(
@@ -307,13 +357,23 @@ subroutine test_char_ptr(c)
 ! CHECK:  %[[VAL_4:.*]] = fir.convert %[[VAL_3]] : (!fir.ptr<!fir.char<1,?>>) -> i64
 ! CHECK:  %[[VAL_5:.*]] = arith.constant 0 : i64
 ! CHECK:  %[[VAL_6:.*]] = arith.cmpi ne, %[[VAL_4]], %[[VAL_5]] : i64
-! CHECK:  %[[VAL_7:.*]]:3 = fir.if %[[VAL_6]]
-! CHECK:    hlfir.as_expr
-! CHECK:    hlfir.associate
+! CHECK:  %[[VAL_7:.*]]:3 = fir.if %[[VAL_6]] -> (!fir.boxchar<1>, !fir.ref<!fir.char<1,?>>, i1) {
+! CHECK:    %[[VAL_PLOAD:.*]] = fir.load %[[VAL_0]]#0 : !fir.ref<!fir.box<!fir.ptr<!fir.char<1,?>>>>
+! CHECK:    %[[VAL_PADDR:.*]] = fir.box_addr %[[VAL_PLOAD]] : (!fir.box<!fir.ptr<!fir.char<1,?>>>) -> !fir.ptr<!fir.char<1,?>>
+! CHECK:    %[[VAL_LEN_BOX:.*]] = fir.load %[[VAL_0]]#0 : !fir.ref<!fir.box<!fir.ptr<!fir.char<1,?>>>>
+! CHECK:    %[[VAL_LEN:.*]] = fir.box_elesize %[[VAL_LEN_BOX]] : (!fir.box<!fir.ptr<!fir.char<1,?>>>) -> index
+! CHECK:    %[[VAL_EBC:.*]] = fir.emboxchar %[[VAL_PADDR]], %[[VAL_LEN]] : (!fir.ptr<!fir.char<1,?>>, index) -> !fir.boxchar<1>
+! CHECK:    %[[VAL_EXPR:.*]] = hlfir.as_expr %[[VAL_EBC]] : (!fir.boxchar<1>) -> !hlfir.expr<!fir.char<1,?>>
+! CHECK:    %[[VAL_ASSOC:.*]]:3 = hlfir.associate %[[VAL_EXPR]] typeparams %[[VAL_LEN]] {adapt.valuebyref} : (!hlfir.expr<!fir.char<1,?>>, index) -> (!fir.boxchar<1>, !fir.ref<!fir.char<1,?>>, i1)
+! CHECK:    fir.result %[[VAL_ASSOC]]#0, %[[VAL_ASSOC]]#1, %[[VAL_ASSOC]]#2 : !fir.boxchar<1>, !fir.ref<!fir.char<1,?>>, i1
 ! CHECK:  } else {
-! CHECK:    fir.absent
+! CHECK:    %[[ABSENT_0:.*]] = fir.absent !fir.boxchar<1>
+! CHECK:    %[[ABSENT_1:.*]] = fir.absent !fir.ref<!fir.char<1,?>>
+! CHECK:    %[[FALSE:.*]] = arith.constant false
+! CHECK:    fir.result %[[ABSENT_0]], %[[ABSENT_1]], %[[FALSE]] : !fir.boxchar<1>, !fir.ref<!fir.char<1,?>>, i1
 ! CHECK:  }
 ! CHECK:  fir.call @_QPdyn_char(%[[VAL_7]]#0) {{.*}}: (!fir.boxchar<1>) -> ()
+! CHECK:  hlfir.end_associate %[[VAL_7]]#1, %[[VAL_7]]#2 : !fir.ref<!fir.char<1,?>>, i1
 end subroutine
 
 ! CHECK-LABEL: func @_QMtestPtest_char_array(
@@ -325,13 +385,22 @@ subroutine test_char_array(c)
 ! CHECK:  %[[VAL_C:.*]]:2 = hlfir.declare %[[ARG0]]
 ! CHECK:  %[[VAL_N:.*]]:2 = hlfir.declare %{{.*}} {uniq_name = "_QMtestFtest_char_arrayEn"}
 ! CHECK:  %[[VAL_3:.*]] = fir.is_present %[[VAL_C]]#0 : (!fir.box<!fir.array<?x!fir.char<1,?>>>) -> i1
-! CHECK:  %[[VAL_4:.*]]:3 = fir.if %[[VAL_3]]
-! CHECK:    hlfir.as_expr
-! CHECK:    hlfir.associate
-! CHECK:    fir.emboxchar
+! CHECK:  %[[VAL_4:.*]]:3 = fir.if %[[VAL_3]] -> (!fir.boxchar<1>, !fir.ref<!fir.array<?x!fir.char<1,?>>>, i1) {
+! CHECK:    %[[VAL_EXPR:.*]] = hlfir.as_expr %[[VAL_C]]#0 : (!fir.box<!fir.array<?x!fir.char<1,?>>>) -> !hlfir.expr<?x!fir.char<1,?>>
+! CHECK:    %[[VAL_DIMS:.*]]:3 = fir.box_dims %[[VAL_C]]#0, %{{.*}} : (!fir.box<!fir.array<?x!fir.char<1,?>>>, index) -> (index, index, index)
+! CHECK:    %[[VAL_SHAPE:.*]] = fir.shape %[[VAL_DIMS]]#1 : (index) -> !fir.shape<1>
+! CHECK:    %[[VAL_LEN:.*]] = fir.box_elesize %[[VAL_C]]#1 : (!fir.box<!fir.array<?x!fir.char<1,?>>>) -> index
+! CHECK:    %[[VAL_ASSOC:.*]]:3 = hlfir.associate %[[VAL_EXPR]](%[[VAL_SHAPE]]) typeparams %[[VAL_LEN]] {adapt.valuebyref} : (!hlfir.expr<?x!fir.char<1,?>>, !fir.shape<1>, index) -> (!fir.box<!fir.array<?x!fir.char<1,?>>>, !fir.ref<!fir.array<?x!fir.char<1,?>>>, i1)
+! CHECK:    %[[VAL_BCAST:.*]] = fir.convert %[[VAL_ASSOC]]#1 : (!fir.ref<!fir.array<?x!fir.char<1,?>>>) -> !fir.ref<!fir.char<1,?>>
+! CHECK:    %[[VAL_EBC:.*]] = fir.emboxchar %[[VAL_BCAST]], %[[VAL_LEN]] : (!fir.ref<!fir.char<1,?>>, index) -> !fir.boxchar<1>
+! CHECK:    fir.result %[[VAL_EBC]], %[[VAL_ASSOC]]#1, %[[VAL_ASSOC]]#2 : !fir.boxchar<1>, !fir.ref<!fir.array<?x!fir.char<1,?>>>, i1
 ! CHECK:  } else {
-! CHECK:    fir.absent !fir.boxchar<1>
+! CHECK:    %[[ABSENT_0:.*]] = fir.absent !fir.boxchar<1>
+! CHECK:    %[[ABSENT_1:.*]] = fir.absent !fir.ref<!fir.array<?x!fir.char<1,?>>>
+! CHECK:    %[[FALSE:.*]] = arith.constant false
+! CHECK:    fir.result %[[ABSENT_0]], %[[ABSENT_1]], %[[FALSE]] : !fir.boxchar<1>, !fir.ref<!fir.array<?x!fir.char<1,?>>>, i1
 ! CHECK:  }
 ! CHECK:  fir.call @_QPdyn_char_array(%[[VAL_4]]#0, %[[VAL_N]]#0) {{.*}}: (!fir.boxchar<1>, !fir.ref<i64>) -> ()
+! CHECK:  hlfir.end_associate %[[VAL_4]]#1, %[[VAL_4]]#2 : !fir.ref<!fir.array<?x!fir.char<1,?>>>, i1
 end subroutine
 end
