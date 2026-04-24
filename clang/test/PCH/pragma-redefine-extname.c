24ee7743f8c38776d2c4f600c4670e4e43b74c8b
Rudolf Polzer <rpolzer@google.com>
2026-04-24 03:34:00 -0700

    Simplify comments.

--- clang/test/PCH/pragma-redefine-extname.c
+++ clang/test/PCH/pragma-redefine-extname.c
@@ -5,16 +5,13 @@
 // RUN: %clang_cc1 -triple=x86_64-unknown-linux -x c-header -emit-pch -o %t %S/pragma-redefine-extname.h
 // RUN: %clang_cc1 -triple=x86_64-unknown-linux -include-pch %t %s -verify -emit-llvm -o - | FileCheck %s
 
-/// Compile it again to check that the pch file is deterministic.
+/// Compile it a few times to check that the PCH file is deterministic.
 // RUN: %clang_cc1 -triple=x86_64-unknown-linux -x c-header -emit-pch -o %t.cmp %S/pragma-redefine-extname.h
 // RUN: diff %t %t.cmp >/dev/null
-/// Try it a second time to reduce chance of false negatives.
 // RUN: %clang_cc1 -triple=x86_64-unknown-linux -x c-header -emit-pch -o %t.cmp %S/pragma-redefine-extname.h
 // RUN: diff %t %t.cmp >/dev/null
-/// Try it a third time to reduce chance of false negatives.
 // RUN: %clang_cc1 -triple=x86_64-unknown-linux -x c-header -emit-pch -o %t.cmp %S/pragma-redefine-extname.h
 // RUN: diff %t %t.cmp >/dev/null
-/// Try it a fourth time to reduce chance of false negatives.
 // RUN: %clang_cc1 -triple=x86_64-unknown-linux -x c-header -emit-pch -o %t.cmp %S/pragma-redefine-extname.h
 // RUN: diff %t %t.cmp >/dev/null
 
