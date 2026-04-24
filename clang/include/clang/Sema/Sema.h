3bf14a172a90dedb1845fbc4c10b50ca93f27808
Shafik Yaghmour <shafik.yaghmour@intel.com>
2026-04-23 17:08:13 -0700

    [NFC][Clang][Sema] Apply rule of three to Sema helper classes (#193835)
    Static analysis flagged these classes. They declared a destructor but
    not copy constructor or copy assignment. Since these classes don't need
    them, this change declares them deleted.

--- clang/include/clang/Sema/Sema.h
+++ clang/include/clang/Sema/Sema.h
@@ -13684,6 +13684,9 @@ public:
     }
 
     ~ScopedCodeSynthesisContext() { S.popCodeSynthesisContext(); }
+    ScopedCodeSynthesisContext(const ScopedCodeSynthesisContext &) = delete;
+    ScopedCodeSynthesisContext &
+    operator=(const ScopedCodeSynthesisContext &) = delete;
   };
 
   /// List of active code synthesis contexts.
@@ -14160,6 +14163,8 @@ public:
   public:
     FPFeaturesStateRAII(Sema &S);
     ~FPFeaturesStateRAII();
+    FPFeaturesStateRAII(const FPFeaturesStateRAII &) = delete;
+    FPFeaturesStateRAII &operator=(const FPFeaturesStateRAII &) = delete;
     FPOptionsOverride getOverrides() { return OldOverrides; }
 
   private:
