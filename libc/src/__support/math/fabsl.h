5d9de261d0e10d9346b601b425f213aaa925d170
hulxv <hulxxv@gmail.com>
2026-04-24 16:45:55 +0300

    fix format

--- libc/src/__support/math/fabsl.h
+++ libc/src/__support/math/fabsl.h
@@ -15,7 +15,9 @@
 namespace LIBC_NAMESPACE_DECL {
 namespace math {
 
-LIBC_INLINE constexpr long double fabsl(long double x) { return fputil::abs(x); }
+LIBC_INLINE constexpr long double fabsl(long double x) {
+  return fputil::abs(x);
+}
 
 } // namespace math
 } // namespace LIBC_NAMESPACE_DECL
