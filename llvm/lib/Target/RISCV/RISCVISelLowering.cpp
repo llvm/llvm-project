85d0e7349c8473f91a89191f7818cbd070e51377
Luke Lau <luke@igalia.com>
2026-04-23 19:45:13 -0600

    [RISCV] Remove codegen for vp_fneg, vp_fma. NFC (#193214)
    Part of the work to remove trivial VP intrinsics from the RISC-V
    backend, see
    https://discourse.llvm.org/t/rfc-remove-codegen-support-for-trivial-vp-intrinsics-in-the-risc-v-backend/87999
    
    We began expanding these nodes in #190589 since vp_fadd/vp_fmul/vp_fsub
    could be combined into them. Now that these intrinsics are expanded too,
    the lowering for these is dead and can be removed.

--- llvm/lib/Target/RISCV/RISCVISelLowering.cpp
+++ llvm/lib/Target/RISCV/RISCVISelLowering.cpp
@@ -88,7 +88,6 @@ static cl::opt<bool>
                       cl::init(true));
 
 // TODO: Support more ops
-static const unsigned ZvfbfaVPOps[] = {ISD::VP_FNEG};
 static const unsigned ZvfbfaOps[] = {
     ISD::FNEG,        ISD::FABS,        ISD::FCOPYSIGN,   ISD::FADD,
     ISD::FSUB,        ISD::FMUL,        ISD::FMINNUM,     ISD::FMAXNUM,
@@ -881,8 +880,7 @@ RISCVTargetLowering::RISCVTargetLowering(const TargetMachine &TM,
         ISD::VP_CTTZ_ELTS,   ISD::VP_CTTZ_ELTS_ZERO_UNDEF};
 
     static const unsigned FloatingPointVPOps[] = {
-        ISD::VP_FNEG,
-        ISD::VP_FMA,         ISD::VP_REDUCE_FADD, ISD::VP_REDUCE_SEQ_FADD,
+        ISD::VP_REDUCE_FADD, ISD::VP_REDUCE_SEQ_FADD,
         ISD::VP_REDUCE_FMIN, ISD::VP_REDUCE_FMAX, ISD::VP_MERGE,
         ISD::VP_SELECT,
         ISD::VP_FP_ROUND,    ISD::VP_FP_EXTEND,
@@ -1200,7 +1198,6 @@ RISCVTargetLowering::RISCVTargetLowering(const TargetMachine &TM,
 
     // TODO: support more vp ops.
     static const unsigned ZvfhminZvfbfminPromoteVPOps[] = {
-        ISD::VP_FMA,
         ISD::VP_REDUCE_FMIN,
         ISD::VP_REDUCE_FMAX,
         ISD::VP_REDUCE_FMINIMUM,
@@ -1376,7 +1373,6 @@ RISCVTargetLowering::RISCVTargetLowering(const TargetMachine &TM,
       setOperationAction({ISD::STRICT_FADD, ISD::STRICT_FSUB, ISD::STRICT_FMUL,
                           ISD::STRICT_FMA},
                          VT, Legal);
-      setOperationAction(ZvfbfaVPOps, VT, Custom);
       setCondCodeAction(VFPCCToExpand, VT, Expand);
 
       setOperationAction({ISD::LOAD, ISD::STORE, ISD::MLOAD, ISD::MSTORE,
@@ -1707,7 +1703,6 @@ RISCVTargetLowering::RISCVTargetLowering(const TargetMachine &TM,
           }
           if (Subtarget.hasVInstructionsBF16()) {
             setOperationAction(ZvfbfaOps, VT, Custom);
-            setOperationAction(ZvfbfaVPOps, VT, Custom);
             setCondCodeAction(VFPCCToExpand, VT, Expand);
           }
           setOperationAction(
@@ -7556,14 +7551,12 @@ static unsigned getRISCVVLOp(SDValue Op) {
   VP_CASE(UDIV)       // VP_UDIV
   VP_CASE(UREM)       // VP_UREM
   VP_CASE(SHL)        // VP_SHL
-  VP_CASE(FNEG)       // VP_FNEG
   VP_CASE(SETCC)      // VP_SETCC
   case ISD::CTLZ_ZERO_UNDEF:
     return RISCVISD::CTLZ_VL;
   case ISD::CTTZ_ZERO_UNDEF:
     return RISCVISD::CTTZ_VL;
   case ISD::FMA:
-  case ISD::VP_FMA:
     return RISCVISD::VFMADD_VL;
   case ISD::STRICT_FMA:
     return RISCVISD::STRICT_VFMADD_VL;
@@ -7636,8 +7629,7 @@ static bool isPromotedOpNeedingSplit(SDValue Op,
         !Subtarget.hasVInstructionsF16()) &&
       !(EltVT == MVT::bf16 && Subtarget.hasVInstructionsBF16Minimal() &&
         (!Subtarget.hasVInstructionsBF16() ||
-         (!llvm::is_contained(ZvfbfaOps, Op.getOpcode()) &&
-          !llvm::is_contained(ZvfbfaVPOps, Op.getOpcode())))))
+         !llvm::is_contained(ZvfbfaOps, Op.getOpcode()))))
     return false;
   // Need to split when the same width f32 vector type isn't legal.
   return !TLI.isTypeLegal(
@@ -9018,11 +9010,6 @@ SDValue RISCVTargetLowering::LowerOperation(SDValue Op,
   case ISD::VP_OR:
   case ISD::VP_XOR:
     return lowerLogicVPOp(Op, DAG);
-  case ISD::VP_FNEG:
-  case ISD::VP_FMA:
-    if (isPromotedOpNeedingSplit(Op, Subtarget, *this))
-      return SplitVPOp(Op, DAG);
-    [[fallthrough]];
   case ISD::VP_SRA:
   case ISD::VP_SRL:
   case ISD::VP_SHL:
