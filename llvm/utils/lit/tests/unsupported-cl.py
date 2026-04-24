55005c7dd7051c9d1c3b2f9356d0ff71f2d778f3
Lei Huang <lei@ca.ibm.com>
2026-04-24 11:48:52 -0400

    remove unnecessary test lines

--- llvm/utils/lit/tests/unsupported-cl.py
+++ llvm/utils/lit/tests/unsupported-cl.py
@@ -1,39 +1,27 @@
 # Check that marking tests as UNSUPPORTED works via command line or env var.
 
-# RUN: %{lit} --unsupported 'false.txt;false2.txt;top-level-suite :: b :: test.txt' \
-# RUN:   --unsupported-not 'true-xfail.txt;top-level-suite :: a :: test-xfail.txt' \
-# RUN:   %{inputs}/xfail-cl \
-# RUN: | FileCheck --check-prefix=CHECK-FILTER %s
+# RUN: %{lit} --unsupported 'true.txt' \
+# RUN:   %{inputs}/xfail-cl/true.txt \
+# RUN: | FileCheck --check-prefix=CHECK-UNSUPPORTED %s
 
-# RUN: env LIT_UNSUPPORTED='false.txt;false2.txt;top-level-suite :: b :: test.txt' \
-# RUN:   LIT_UNSUPPORTED_NOT='true-xfail.txt;top-level-suite :: a :: test-xfail.txt' \
-# RUN: %{lit} %{inputs}/xfail-cl \
-# RUN: | FileCheck --check-prefix=CHECK-FILTER %s
+# RUN: env LIT_UNSUPPORTED='true.txt' \
+# RUN: %{lit} %{inputs}/xfail-cl/true.txt \
+# RUN: | FileCheck --check-prefix=CHECK-UNSUPPORTED %s
 
-# Check that --unsupported-not and LIT_UNSUPPORTED_NOT always have precedence.
+# Check that --unsupported-not and LIT_UNSUPPORTED_NOT override --unsupported.
 
-# RUN: env LIT_UNSUPPORTED=true-xfail.txt \
-# RUN: %{lit} --unsupported true-xfail.txt --unsupported-not true-xfail.txt \
-# RUN:   --unsupported true-xfail.txt %{inputs}/xfail-cl/true-xfail.txt \
-# RUN: | FileCheck --check-prefix=CHECK-OVERRIDE %s
+# RUN: %{lit} --unsupported 'true.txt' --unsupported-not 'true.txt' \
+# RUN:   %{inputs}/xfail-cl/true.txt \
+# RUN: | FileCheck --check-prefix=CHECK-NOT-UNSUPPORTED %s
 
-# RUN: env LIT_UNSUPPORTED_NOT=true-xfail.txt LIT_UNSUPPORTED=true-xfail.txt \
-# RUN: %{lit} --unsupported true-xfail.txt %{inputs}/xfail-cl/true-xfail.txt \
-# RUN: | FileCheck --check-prefix=CHECK-OVERRIDE %s
+# RUN: env LIT_UNSUPPORTED='true.txt' LIT_UNSUPPORTED_NOT='true.txt' \
+# RUN: %{lit} %{inputs}/xfail-cl/true.txt \
+# RUN: | FileCheck --check-prefix=CHECK-NOT-UNSUPPORTED %s
 
 # END.
 
-# CHECK-FILTER: Testing: 11 tests, {{[0-9]*}} workers
-# CHECK-FILTER-DAG: {{^}}PASS: top-level-suite :: a :: test.txt
-# CHECK-FILTER-DAG: {{^}}UNSUPPORTED: top-level-suite :: b :: test.txt
-# CHECK-FILTER-DAG: {{^}}UNSUPPORTED: top-level-suite :: a :: false.txt
-# CHECK-FILTER-DAG: {{^}}UNSUPPORTED: top-level-suite :: b :: false.txt
-# CHECK-FILTER-DAG: {{^}}UNSUPPORTED: top-level-suite :: false.txt
-# CHECK-FILTER-DAG: {{^}}UNSUPPORTED: top-level-suite :: false2.txt
-# CHECK-FILTER-DAG: {{^}}PASS: top-level-suite :: true.txt
-# CHECK-FILTER-DAG: {{^}}PASS: top-level-suite :: true-xfail.txt
-# CHECK-FILTER-DAG: {{^}}PASS: top-level-suite :: a :: test-xfail.txt
-# CHECK-FILTER-DAG: {{^}}UNSUPPORTED: top-level-suite :: b :: test-xfail.txt
+# CHECK-UNSUPPORTED: Testing: 1 tests, {{[0-9]*}} workers
+# CHECK-UNSUPPORTED: {{^}}UNSUPPORTED: top-level-suite :: true.txt
 
-# CHECK-OVERRIDE: Testing: 1 tests, {{[0-9]*}} workers
-# CHECK-OVERRIDE: {{^}}PASS: top-level-suite :: true-xfail.txt
+# CHECK-NOT-UNSUPPORTED: Testing: 1 tests, {{[0-9]*}} workers
+# CHECK-NOT-UNSUPPORTED: {{^}}PASS: top-level-suite :: true.txt
