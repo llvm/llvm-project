0eef120130b74e5a20d287a7cd7e707f184c1d88
Igor Kudrin <ikudrin@accesssoftek.com>
2026-04-24 19:40:22 -0700

    Revert "[Clang][CodeGen] Report when an alias points to an incompatible target" (#194106)
    Reverts llvm/llvm-project#192397

--- clang/test/SemaObjC/attr-alias.m
+++
@@ -1,6 +0,0 @@
-// RUN: %clang_cc1 -triple %itanium_abi_triple -fblocks -verify -emit-llvm-only %s
-
-// Compiler-generated functions are not valid alias targets.
-void foo() { void(^myBlock)(void) = ^{ }; }
-void bar() __attribute__((alias("__foo_block_invoke")));
-// expected-error@-1 {{alias must point to a defined variable or function}}
