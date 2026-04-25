82d8959be142e5466e815c08f90c0030ea90f390
Will Hawkins <hawkinsw@obs.cr>
2026-04-25 06:51:37 -0400

    [libc++] Implement P1899 `ranges::stride_view` (#65200)
    Implement `ranges::stride_view` in libc++. This PR was migrated from
    Phabricator (https://reviews.llvm.org/D156924).
    
    Closes #105198
    
    Co-authored-by: Louis Dionne <ldionne.2@gmail.com>
    Co-authored-by: A. Jiang <de34@live.cn>

--- libcxx/test/std/language.support/support.limits/support.limits.general/version.version.compile.pass.cpp
+++ libcxx/test/std/language.support/support.limits/support.limits.general/version.version.compile.pass.cpp
@@ -704,6 +704,10 @@
 #    error "__cpp_lib_ranges_starts_ends_with should not be defined before c++23"
 #  endif
 
+#  ifdef __cpp_lib_ranges_stride
+#    error "__cpp_lib_ranges_stride should not be defined before c++23"
+#  endif
+
 #  ifdef __cpp_lib_ranges_to_container
 #    error "__cpp_lib_ranges_to_container should not be defined before c++23"
 #  endif
@@ -1666,6 +1670,10 @@
 
 #  ifdef __cpp_lib_ranges_starts_ends_with
 #    error "__cpp_lib_ranges_starts_ends_with should not be defined before c++23"
+#  endif
+
+#  ifdef __cpp_lib_ranges_stride
+#    error "__cpp_lib_ranges_stride should not be defined before c++23"
 #  endif
 
 #  ifdef __cpp_lib_ranges_to_container
@@ -2795,6 +2803,10 @@
 
 #  ifdef __cpp_lib_ranges_starts_ends_with
 #    error "__cpp_lib_ranges_starts_ends_with should not be defined before c++23"
+#  endif
+
+#  ifdef __cpp_lib_ranges_stride
+#    error "__cpp_lib_ranges_stride should not be defined before c++23"
 #  endif
 
 #  ifdef __cpp_lib_ranges_to_container
@@ -4191,6 +4203,10 @@
 
 #  ifdef __cpp_lib_ranges_starts_ends_with
 #    error "__cpp_lib_ranges_starts_ends_with should not be defined before c++23"
+#  endif
+
+#  ifdef __cpp_lib_ranges_stride
+#    error "__cpp_lib_ranges_stride should not be defined before c++23"
 #  endif
 
 #  ifdef __cpp_lib_ranges_to_container
@@ -5806,6 +5822,13 @@
 #  endif
 #  if __cpp_lib_ranges_starts_ends_with != 202106L
 #    error "__cpp_lib_ranges_starts_ends_with should have the value 202106L in c++23"
+#  endif
+
+#  ifndef __cpp_lib_ranges_stride
+#    error "__cpp_lib_ranges_stride should be defined in c++23"
+#  endif
+#  if __cpp_lib_ranges_stride != 202207L
+#    error "__cpp_lib_ranges_stride should have the value 202207L in c++23"
 #  endif
 
 #  ifndef __cpp_lib_ranges_to_container
@@ -7745,6 +7768,13 @@
 #  endif
 #  if __cpp_lib_ranges_starts_ends_with != 202106L
 #    error "__cpp_lib_ranges_starts_ends_with should have the value 202106L in c++26"
+#  endif
+
+#  ifndef __cpp_lib_ranges_stride
+#    error "__cpp_lib_ranges_stride should be defined in c++26"
+#  endif
+#  if __cpp_lib_ranges_stride != 202207L
+#    error "__cpp_lib_ranges_stride should have the value 202207L in c++26"
 #  endif
 
 #  ifndef __cpp_lib_ranges_to_container
