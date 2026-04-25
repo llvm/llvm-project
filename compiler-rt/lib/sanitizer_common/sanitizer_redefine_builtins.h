e645adc8194a419cf647b61d28ca4d53ab1cb99f
Vitaly Buka <vitalybuka@google.com>
2026-04-25 12:27:44 -0700

    [sanitizer] Rename SANITIZER_USE_ASM_DOT_SET to SANITIZER_COMMON_REDEFINE_BUILTINS_USE_ASM_DOT_SET
    Make name very specific for this case, we don't want this used in other parts of compiler-rt.

--- compiler-rt/lib/sanitizer_common/sanitizer_redefine_builtins.h
+++ compiler-rt/lib/sanitizer_common/sanitizer_redefine_builtins.h
@@ -17,13 +17,11 @@
 // The asm hack only works with GCC and Clang.
 #    if !defined(_WIN32) && !defined(_AIX) && !defined(__APPLE__)
 
-#      if defined(SANITIZER_USE_ASM_DOT_SET)
-
+#      if defined(SANITIZER_COMMON_REDEFINE_BUILTINS_USE_ASM_DOT_SET)
 #        define SANITIZER_REDEFINE_BUILTIN_ASM(name) \
           asm(".set " #name ", __sanitizer_internal_" #name)
 
 #      else
-
 #        define SANITIZER_REDEFINE_BUILTIN_ASM(name) \
           asm("\"" #name "\" = \"__sanitizer_internal_" #name "\"")
 
