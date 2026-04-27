43a82f90d991b639664e1ad58f6339e181e4d98b
David Truby <david.truby@arm.com>
2026-04-27 12:49:41 +0100

    Remove extraneous space and header name

--- mlir/include/mlir/Support/Complex.h
+++ mlir/include/mlir/Support/Complex.h
@@ -1,5 +1,4 @@
-
-//===- Complex.h - Complex Number type for use in MLIR ----------*- C++ -*-===//
+//===- Complex Number type for use in MLIR ----------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
