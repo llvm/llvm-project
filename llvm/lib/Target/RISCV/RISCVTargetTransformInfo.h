60ea5f7ae3fc3c6cbe638f2ac4266ee627364034
Craig Topper <craig.topper@sifive.com>
2026-04-26 22:41:56 -0700

    Revert "[InstCombine/RISCV] Constant-fold bitcast(vmv.v.x) (#182630)"
    This reverts commit ffdb484752dca7a5c6679383b98c41c9da3ae6b0.
    
    We found an infinite loop from this in our downstream.

--- llvm/lib/Target/RISCV/RISCVTargetTransformInfo.h
+++ llvm/lib/Target/RISCV/RISCVTargetTransformInfo.h
@@ -554,9 +554,6 @@ public:
   bool
   shouldCopyAttributeWhenOutliningFrom(const Function *Caller,
                                        const Attribute &Attr) const override;
-
-  std::optional<Instruction *>
-  instCombineIntrinsic(InstCombiner &IC, IntrinsicInst &II) const override;
 };
 
 } // end namespace llvm
