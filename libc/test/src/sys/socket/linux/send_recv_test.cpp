8fc3598b3271c56d08f22935c86d773b560d1101
Pavel Labath <pavel@labath.sk>
2026-04-27 13:08:09 +0000

    [libc] Add the MSG_ flags
    I've included all of the flags defined on linux, even those not
    documented in POSIX (or the manpage). I've also added tests to exercise
    the flags, for cases where this can be done with domain sockets.

--- libc/test/src/sys/socket/linux/send_recv_test.cpp
+++ libc/test/src/sys/socket/linux/send_recv_test.cpp
@@ -10,13 +10,13 @@
 #include "src/sys/socket/send.h"
 #include "src/sys/socket/socketpair.h"
 
+#include "src/string/memset.h"
 #include "src/unistd/close.h"
 
+#include "hdr/sys_socket_macros.h"
 #include "test/UnitTest/ErrnoCheckingTest.h"
 #include "test/UnitTest/ErrnoSetterMatcher.h"
 #include "test/UnitTest/Test.h"
-
-#include <sys/socket.h> // For AF_UNIX and SOCK_DGRAM
 
 using LIBC_NAMESPACE::testing::ErrnoSetterMatcher::Fails;
 using LIBC_NAMESPACE::testing::ErrnoSetterMatcher::Succeeds;
@@ -44,6 +44,46 @@ TEST_F(LlvmLibcSendRecvTest, SucceedsWithSocketPair) {
   // close both ends of the socket
   ASSERT_THAT(LIBC_NAMESPACE::close(sockpair[0]), Succeeds(0));
   ASSERT_THAT(LIBC_NAMESPACE::close(sockpair[1]), Succeeds(0));
+}
+
+TEST_F(LlvmLibcSendRecvTest, MsgFlagsTest) {
+  int sockpair[2] = {0, 0};
+
+  ASSERT_THAT(LIBC_NAMESPACE::socketpair(AF_UNIX, SOCK_SEQPACKET, 0, sockpair),
+              Succeeds(0));
+
+  char buffer[256];
+
+  // MSG_DONTWAIT on an empty socket returns EAGAIN
+  ASSERT_THAT(
+      LIBC_NAMESPACE::recv(sockpair[1], buffer, sizeof(buffer), MSG_DONTWAIT),
+      Fails<ssize_t>(EAGAIN));
+
+  const char TEST_MESSAGE[] = "this is a long message";
+  const size_t MESSAGE_LEN = sizeof(TEST_MESSAGE);
+
+  ASSERT_THAT(LIBC_NAMESPACE::send(sockpair[0], TEST_MESSAGE, MESSAGE_LEN, 0),
+              Succeeds(static_cast<ssize_t>(MESSAGE_LEN)));
+
+  // MSG_PEEK does not remove the message from the socket
+  ASSERT_THAT(
+      LIBC_NAMESPACE::recv(sockpair[1], buffer, sizeof(buffer), MSG_PEEK),
+      Succeeds(static_cast<ssize_t>(MESSAGE_LEN)));
+  ASSERT_STREQ(buffer, TEST_MESSAGE);
+  LIBC_NAMESPACE::memset(buffer, 0, sizeof(buffer));
+
+  // Read the message again, but use a smaller buffer to test MSG_TRUNC. Return
+  // value should be real length.
+  ASSERT_THAT(LIBC_NAMESPACE::recv(sockpair[1], buffer, 5, MSG_TRUNC),
+              Succeeds(static_cast<ssize_t>(MESSAGE_LEN)));
+  ASSERT_STREQ(buffer, "this ");
+
+  // Sending with MSG_NOSIGNAL to a closed socket should fail with EPIPE
+  ASSERT_THAT(LIBC_NAMESPACE::close(sockpair[1]), Succeeds(0));
+  ASSERT_THAT(LIBC_NAMESPACE::send(sockpair[0], "x", 1, MSG_NOSIGNAL),
+              Fails<ssize_t>(EPIPE));
+
+  ASSERT_THAT(LIBC_NAMESPACE::close(sockpair[0]), Succeeds(0));
 }
 
 TEST_F(LlvmLibcSendRecvTest, SendFails) {
