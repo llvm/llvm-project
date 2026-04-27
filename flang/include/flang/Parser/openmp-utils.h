bba7811cd2efb0060b87bc80c612d14c488a51a3
Krzysztof Parzyszek <Krzysztof.Parzyszek@amd.com>
2026-04-27 15:32:40 -0500

    [flang][OpenMP] Move some utility functions from Semantics to Parser,… (#194434)
    … NFC
    
    Functions that only operate on AST nodes and not require any semantic
    information belong in the parser library.

--- flang/include/flang/Parser/openmp-utils.h
+++ flang/include/flang/Parser/openmp-utils.h
@@ -36,6 +36,14 @@ template <typename T> constexpr auto addr_if(const std::optional<T> &x) {
   return x ? &*x : nullptr;
 }
 
+const parser::Designator *GetDesignatorFromObj(const parser::OmpObject &object);
+const parser::DataRef *GetDataRefFromObj(const parser::OmpObject &object);
+const parser::ArrayElement *GetArrayElementFromObj(
+    const parser::OmpObject &object);
+std::optional<parser::CharBlock> GetObjectSource(
+    const parser::OmpObject &object);
+const parser::OmpObject *GetArgumentObject(const parser::OmpArgument &argument);
+
 namespace detail {
 struct DirectiveSpecificationScope {
   using ODS = OmpDirectiveSpecification;
