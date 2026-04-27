42caa80124288b8d9a456a409b5bf282c6c6121a
nasmnc01 <nashe.mncube@arm.com>
2026-04-27 10:43:05 +0100

    SME instructions for C1-Ultra
    Implements
    - SVE instructions added by SME and available when not in Streaming SVE mode
    - Instructions added by SME that aren't sent to CME
    
    Change-Id: I4ce8cc008d503f53af3fe54f6f2130f5c85047af

---
+++ llvm/test/tools/llvm-mca/AArch64/Inputs/sme-instructions.s
@@ -0,0 +1,43 @@
+addspl x0, x1, #10
+addspl x0, sp, #10
+addsvl x0, x1, #10
+addsvl x0, sp, #10
+rdsvl  x1, #10
+cntp x0, pn0.h, vlx2
+pext p0.h, pn8[0]
+pext { p0.h, p1.h }, pn8[0]
+ptrue pn8.h
+whilege pn8.h, x0, x0, vlx2
+whilegt pn8.h, x0, x0, vlx2
+whilehi pn8.h, x0, x0, vlx2
+whilehs pn8.h, x0, x0, vlx2
+whilele pn8.h, x0, x0, vlx2
+whilelo pn8.h, x0, x0, vlx2
+whilels pn8.h, x0, x0, vlx2
+whilelt pn8.h, x0, x0, vlx2
+bfmlslb z0.s, z1.h, z2.h
+bfmlslb z0.s, z1.h, z2.h[0]
+bfmlslt z0.s, z1.h, z2.h
+bfmlslt z0.s, z1.h, z2.h[0]
+fclamp z0.s, z1.s, z2.s
+fdot z0.s, z1.h, z2.h
+fdot z0.s, z1.h, z2.h[0]
+psel p0, p0, p0.b[w12, 0]
+revd z0.q, p0/m, z0.q
+sclamp z0.s, z1.s, z2.s
+uclamp z0.s, z1.s, z2.s
+sdot z0.s, z0.h, z0.h
+sdot z0.s, z0.h, z0.h[0]
+udot z0.s, z0.h, z0.h
+udot z0.s, z0.h, z0.h[0]
+sqcvtn z0.h, { z0.s, z1.s }
+sqcvtun z0.h, { z0.s, z1.s }
+uqcvtn z0.h, { z0.s, z1.s }
+whilege { p0.h, p1.h }, x0, x0
+whilegt { p0.h, p1.h }, x0, x0
+whilehi { p0.h, p1.h }, x0, x0
+whilehs { p0.h, p1.h }, x0, x0
+whilele { p0.h, p1.h }, x0, x0
+whilelo { p0.h, p1.h }, x0, x0
+whilels { p0.h, p1.h }, x0, x0
+whilelt { p0.h, p1.h }, x0, x0
