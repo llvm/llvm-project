ce18d063f2c51e94a30cc48300a549d7f48ba654
Vladimir Vereschaka <vvereschaka@accesssoftek.com>
2026-04-27 21:25:36 -0700

    [Clang] Produce determenistic hash for anonymous namespaces.
    This change adds a path substitution for the main module file during anonymous
    namespace hash generation using the prefix map specified by -fmacro-prefix-map
    option. That ensures deterministic symbol mangling for reproducible builds.

---
+++ clang/test/AST/anon-ns-determ-hash.cpp
@@ -0,0 +1,9 @@
+// Check anonymous namespace deterministic hash generation.
+// RUN: %clang_cc1 -std=c++2a -ast-dump=json -fmacro-prefix-map=%p=/static-path %s | FileCheck %s
+
+namespace {
+    int internal_ns_var = 0;
+} 
+
+// The 0xA110234F hash value must be identical on any system with proper path substitution.
+// CHECK: "mangledName": "?internal_ns_var@?A0xA110234F@@3HA",
