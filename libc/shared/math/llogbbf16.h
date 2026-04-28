868a531d152eebe02332a8a794e83133e28806cf
Anonmiraj <ezzibrahimx@gmail.com>
2026-04-28 04:21:02 +0300

    [libc][math] Refactor llogbbf16 family to header-only
    Refactored functions:
      - llogbbf16

--- libc/shared/math/llogbbf16.h
+++ libc/shared/math/llogbbf16.h
@@ -1,4 +1,4 @@
-//===-- Implementation of llogbbf16 function ------------------------------===//
+//===-- Shared llogbbf16 function -------------------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,11 +6,18 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "src/math/llogbbf16.h"
+#ifndef LLVM_LIBC_SHARED_MATH_LLOGBBF16_H
+#define LLVM_LIBC_SHARED_MATH_LLOGBBF16_H
+
+#include "shared/libc_common.h"
 #include "src/__support/math/llogbbf16.h"
 
 namespace LIBC_NAMESPACE_DECL {
+namespace shared {
 
-LLVM_LIBC_FUNCTION(long, llogbbf16, (bfloat16 x)) { return math::llogbbf16(x); }
+using math::llogbbf16;
 
+} // namespace shared
 } // namespace LIBC_NAMESPACE_DECL
+
+#endif // LLVM_LIBC_SHARED_MATH_LLOGBBF16_H
