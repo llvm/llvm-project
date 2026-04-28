930279d33dedeb66c26c624bc439a68816a699e6
Charles Zablit <c_zablit@apple.com>
2026-04-28 15:18:36 +0100

    fixup! [lldb][windows] fix race condition in ConPTY on process exit

--- lldb/source/Host/windows/ConnectionConPTYWindows.cpp
+++ lldb/source/Host/windows/ConnectionConPTYWindows.cpp
@@ -64,14 +64,14 @@ size_t ConnectionConPTY::Read(void *dst, size_t dst_len,
                               const Timeout<std::micro> &timeout,
                               lldb::ConnectionStatus &status,
                               Status *error_ptr) {
-  std::unique_lock<std::mutex> guard(m_pty->GetMutex());
-  if (m_pty->IsStopping())
-    m_pty->GetCV().wait(guard, [this] { return !m_pty->IsStopping(); });
-  guard.unlock();
-
-  if (!m_pty->IsConnected()) {
-    status = eConnectionStatusEndOfFile;
-    return 0;
+  {
+    std::unique_lock<std::mutex> guard(m_pty->GetMutex());
+    if (m_pty->IsStopping())
+      m_pty->GetCV().wait(guard, [this] { return !m_pty->IsStopping(); });
+    if (!m_pty->IsConnected()) {
+      status = eConnectionStatusEndOfFile;
+      return 0;
+    }
   }
 
   size_t bytes_read =
