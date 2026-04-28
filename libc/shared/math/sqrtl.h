149cb5366a528aad1faa518293ce62edf585fd99
Anonmiraj <ezzibrahimx@gmail.com>
2026-04-28 04:46:03 +0300

    [libc][math] Refactor sqrtl family to header-only
    Refactored functions:
      - sqrtl

--- libc/shared/math/sqrtl.h
+++ libc/shared/math/sqrtl.h
@@ -1,4 +1,4 @@
-//===-- Implementation of sqrtl function ----------------------------------===//
+//===-- Shared sqrtl function -----------------------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,13 +6,18 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "src/math/sqrtl.h"
+#ifndef LLVM_LIBC_SHARED_MATH_SQRTL_H
+#define LLVM_LIBC_SHARED_MATH_SQRTL_H
+
+#include "shared/libc_common.h"
 #include "src/__support/math/sqrtl.h"
 
 namespace LIBC_NAMESPACE_DECL {
+namespace shared {
 
-LLVM_LIBC_FUNCTION(long double, sqrtl, (long double x)) {
-  return math::sqrtl(x);
-}
+using math::sqrtl;
 
+} // namespace shared
 } // namespace LIBC_NAMESPACE_DECL
+
+#endif // LLVM_LIBC_SHARED_MATH_SQRTL_H
