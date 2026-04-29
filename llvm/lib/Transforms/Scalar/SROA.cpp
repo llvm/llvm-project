bf4dc8e36ba08fd33da3434bfd7472df70be2415
Yaxun (Sam) Liu <yaxun.liu@amd.com>
2026-04-29 19:12:20 -0400

    [SROA] Fix struct-to-vector helper formatting
    Wrap the LogSelection lambda parameters as requested by clang-format.

--- llvm/lib/Transforms/Scalar/SROA.cpp
+++ llvm/lib/Transforms/Scalar/SROA.cpp
@@ -5183,8 +5183,7 @@ static std::tuple<Type *, bool, VectorType *>
 selectPartitionType(Partition &P, const DataLayout &DL, AllocaInst &AI,
                     LLVMContext &C) {
   auto LogSelection = [&](StringRef Path, Type *SelectedTy,
-                          VectorType *SelectedVecTy,
-                          bool SelectedIntWidening) {
+                          VectorType *SelectedVecTy, bool SelectedIntWidening) {
     LLVM_DEBUG({
       dbgs() << "selectPartitionType path=" << Path
              << " func=" << AI.getFunction()->getName() << " alloca=";
