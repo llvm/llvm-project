a95016a7c06b903c9c7393a721cd2c6e40b0d89f
Kleis Auke Wolthuizen <github@kleisauke.nl>
2026-04-29 15:32:49 +0200

    [compiler-rt][UBSan] Add __ubsan_default_suppressions() hook
    In line with commit 5c62af5 and 83566da.

---
+++ compiler-rt/test/ubsan/TestCases/Misc/Posix/ubsan_suppressions.cpp
@@ -0,0 +1,7 @@
+// Test that we use the suppressions from __ubsan_default_suppressions.
+// RUN: %clangxx -fsanitize=undefined %s -o %t && not %run %t 2>&1 | FileCheck %s
+extern "C" {
+  const char *__ubsan_default_suppressions() { return "FooBar"; }
+}
+// CHECK: UndefinedBehaviorSanitizer: failed to parse suppressions
+int main() {}
