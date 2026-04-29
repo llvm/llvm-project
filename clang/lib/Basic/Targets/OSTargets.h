013f415d206cba733a6435c95544fc1c8abd9c97
Roland McGrath <mcgrathr@google.com>
2026-04-28 20:24:43 -0700

    [clang][Fuchsia] Factor getFuchsiaDefines out of FuchsiaTargetInfo class
    Most of the template class's getOSDefines definition is not
    template-dependent, so move it to a shared subroutine that's
    outside the header file and reused by all the FuchsiaTargetInfo
    instantiations.

--- clang/lib/Basic/Targets/OSTargets.h
+++ clang/lib/Basic/Targets/OSTargets.h
@@ -889,19 +889,16 @@ public:
   }
 };
 
+void getFuchsiaDefines(MacroBuilder &Builder, const LangOptions &Opts,
+                       const llvm::Triple &Triple);
+
 // Fuchsia Target
 template <typename Target>
 class LLVM_LIBRARY_VISIBILITY FuchsiaTargetInfo : public OSTargetInfo<Target> {
 protected:
   void getOSDefines(const LangOptions &Opts, const llvm::Triple &Triple,
                     MacroBuilder &Builder) const override {
-    Builder.defineMacro("__Fuchsia__");
-    if (Opts.POSIXThreads)
-      Builder.defineMacro("_REENTRANT");
-    // Required by the libc++ locale support.
-    if (Opts.CPlusPlus)
-      Builder.defineMacro("_GNU_SOURCE");
-    Builder.defineMacro("__Fuchsia_API_level__", Twine(Opts.FuchsiaAPILevel));
+    getFuchsiaDefines(Builder, Opts, Triple);
     this->PlatformName = "fuchsia";
     this->PlatformMinVersion = VersionTuple(Opts.FuchsiaAPILevel);
   }
