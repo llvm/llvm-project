d64a0ce141a48a0dcbb60228753cb1d01a06049e
Brandon Wu <brandon.wu@sifive.com>
2026-04-29 17:06:17 +0800

    fixup! revise heuristic

--- llvm/lib/CodeGen/SelectionDAG/LegalizeVectorOps.cpp
+++ llvm/lib/CodeGen/SelectionDAG/LegalizeVectorOps.cpp
@@ -2087,13 +2087,15 @@ SDValue VectorLegalizer::ExpandFABS(SDNode *Node) {
   //      a. Vector only has 1 element and target knows how to handle scalar
   //         FABS(either legal or custom expand or promote).
   //      b. Vector has more than 1 element and target supports scalar
-  //         FABS natively and vector length <= 3(2 AND + 1 OR).
+  //         FABS natively and vector length <= 2(1 AND + 1 CONST).
+  // FIXME: Scalar construction instruction count varies in every architecture,
+  // here we assume 1 instruction for now.
   if (VT.isFixedLengthVector()) {
     EVT EltVT = VT.getVectorElementType();
     if ((VT.getVectorNumElements() == 1 &&
-         TLI.isOperationLegalOrCustomOrPromote(ISD::FCOPYSIGN, EltVT)) ||
-        (VT.getVectorNumElements() < 4 &&
-         TLI.isOperationLegal(ISD::FCOPYSIGN, EltVT) &&
+         TLI.isOperationLegalOrCustomOrPromote(ISD::FABS, EltVT)) ||
+        (VT.getVectorNumElements() < 3 &&
+         TLI.isOperationLegal(ISD::FABS, EltVT) &&
          TLI.isExtractVecEltCheap(VT, 0)))
       return SDValue();
   }
