c1aae1085d71a11b3dbbf231f4979cd292a2715b
NimishMishra <42909663+NimishMishra@users.noreply.github.com>
2026-04-29 05:22:22 -0700

    Update flang/test/Lower/Intrinsics/timef.f90
    Co-authored-by: Tarun Prabhu <tarunprabhu@gmail.com>

--- flang/test/Lower/Intrinsics/timef.f90
+++ flang/test/Lower/Intrinsics/timef.f90
@@ -6,7 +6,7 @@ subroutine test_timef(t)
   t = timef()
 end subroutine
 ! CHECK-LABEL:   func.func @_QPtest_timef(
-! CHECK-SAME:                                %[[VAL_0:.*]]: !fir.ref<f64> {fir.bindc_name = "t"}) {
+! CHECK-SAME:      %[[VAL_0:.*]]: !fir.ref<f64> {fir.bindc_name = "t"}) {
 ! CHECK:           %[[VAL_2:.*]] = fir.dummy_scope : !fir.dscope
 ! CHECK:           %[[VAL_3:.*]]:2 = hlfir.declare %[[VAL_0]] dummy_scope %[[VAL_2]] arg {{[0-9]+}} {uniq_name = "_QFtest_timefEt"} : (!fir.ref<f64>, !fir.dscope) -> (!fir.ref<f64>, !fir.ref<f64>)
 ! CHECK:           %[[VAL_4:.*]] = fir.call @_FortranATimef() fastmath<contract> : () -> f64
