3e466db912a085a63082ff164bffc746e65a2d16
Kleis Auke Wolthuizen <github@kleisauke.nl>
2026-04-29 16:15:49 +0200

    Fix CI (2)

--- compiler-rt/test/ubsan/TestCases/Misc/Posix/ubsan_suppressions.cpp
+++ compiler-rt/test/ubsan/TestCases/Misc/Posix/ubsan_suppressions.cpp
@@ -1,7 +1,10 @@
 // Test that we use the suppressions from __ubsan_default_suppressions.
 // RUN: %clangxx -fsanitize=undefined %s -o %t && not %run %t 2>&1 | FileCheck %s
-extern "C" {
-  const char *__ubsan_default_suppressions() { return "FooBar"; }
-}
+
+// Temporarily unsupporting on TySan until suppressions are implemented
+// UNSUPPORTED: ubsan-tysan
+
+extern "C" const char *__ubsan_default_suppressions() { return "FooBar"; }
+
 // CHECK: {{.*}}Sanitizer: failed to parse suppressions
 int main() {}
