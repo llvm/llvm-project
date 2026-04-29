3dd6c0f7ad14642d2ab8f33a74f41daf091f8819
Yutong Zhu <y25zhu@uwaterloo.ca>
2026-04-28 20:03:42 -0400

    Address comments 1. Refactor the type check helper function into Type.cpp 2. Edit release notes

--- clang/lib/AST/Type.cpp
+++ clang/lib/AST/Type.cpp
@@ -2405,6 +2405,12 @@ bool Type::isUnsignedIntegerOrEnumerationType() const {
   return false;
 }
 
+bool Type::isUnsignedIntegerOrVectorType() const {
+  if (const auto *VT = getAs<VectorType>())
+    return VT->getElementType()->isUnsignedIntegerType();
+  return isUnsignedIntegerType();
+}
+
 bool Type::hasUnsignedIntegerRepresentation() const {
   if (const auto *VT = dyn_cast<VectorType>(CanonicalType))
     return VT->getElementType()->isUnsignedIntegerOrEnumerationType();
