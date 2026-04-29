a3e43c459893d5869d5230d4e11fac6e2d046c5b
Ammarguellat <zahira.ammarguellat@intel.com>
2026-04-29 09:23:50 -0700

    Added warning requested in review

--- clang/test/SemaCXX/decomposition-openmp.cpp
+++ clang/test/SemaCXX/decomposition-openmp.cpp
@@ -1,3 +1,4 @@
+// RUN: %clang_cc1 -fsyntax-only -verify=cxx17 -std=c++17 -fopenmp %s
 // RUN: %clang_cc1 -fsyntax-only -verify -std=c++20 -fopenmp %s
 
 // expected-no-diagnostics
@@ -5,8 +6,12 @@
 // Okay, not an OpenMP capture.
 auto f() {
   int i[2] = {};
+  // cxx17-note@+2{{'a' declared here}}
+  // cxx17-note@+1{{'b' declared here}}
   auto [a, b] = i;
+  // cxx17-warning@+1{{captured structured bindings are a C++20 extension}}
   return [=, &a] {
+    // cxx17-warning@+1{{captured structured bindings are a C++20 extension}}
     return a + b;
   };
 }
@@ -17,16 +22,24 @@ void g() {
   #pragma omp parallel
   {
     int i[2] = {};
+    // cxx17-note@+2{{'a' declared here}}
+    // cxx17-note@+1{{'b' declared here}}
     auto [a, b] = i;
+    // cxx17-warning@+2{{captured structured bindings are a C++20 extension}}
+    // cxx17-warning@+1{{captured structured bindings are a C++20 extension}}
     auto L = [&] { foo(a+b); };
   }
 }
 
 void h() {
   int i[2] = {};
+  // cxx17-note@+2{{'a' declared here}}
+  // cxx17-note@+1{{'b' declared here}}
   auto [a, b] = i;
   #pragma omp parallel
   {
+    // cxx17-warning@+2{{captured structured bindings are a C++20 extension}}
+    // cxx17-warning@+1{{captured structured bindings are a C++20 extension}}
     foo(a + b);
   }
 }
