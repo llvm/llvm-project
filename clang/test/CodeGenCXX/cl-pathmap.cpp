2508a52cc1c6f7770bcc5af97f81f91a0d76cd77
Vladimir Vereschaka <vvereschaka@accesssoftek.com>
2026-04-28 22:31:52 -0700

    [clang-cl] Add cl's /pathmap: option.

---
+++ clang/test/CodeGenCXX/cl-pathmap.cpp
@@ -0,0 +1,19 @@
+// RUN: %clang_cl /clang:-emit-llvm /pathmap:%p=x:/path-to/ /Fo:%t.bc /c %s
+// RUN: llvm-dis %t.bc -o - | FileCheck %s --check-prefix=CHECK-PREFIX-MAP 
+
+// CHECK-PREFIX-MAP: c"x:\\path-to\\cl-pathmap.cpp\00"
+
+namespace std {
+class source_location {
+public:
+struct __impl {
+  const char *_M_file_name;
+  const char *_M_function_name;
+  unsigned _M_line;
+  unsigned _M_column;
+};
+};
+} // namespace std
+
+const std::source_location::__impl *__m_impl = static_cast<const std::source_location::__impl *>(__builtin_source_location());
+const char* file = __m_impl->_M_file_name;
