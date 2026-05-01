2136a80ed3359ab9817c7af2b077ad0774b613e6
Rahul Joshi <rjoshi@nvidia.com>
2026-05-01 16:11:32 -0700

    [NFC][LLVM] Make `matchIntrinsicSignature` static and rename it
    Make `matchIntrinsicSignature` static and rename it to a file
    local `isSignatureValid` function, and change the sense of its
    return value to match the new name.

--- llvm/include/llvm/IR/Intrinsics.h
+++ llvm/include/llvm/IR/Intrinsics.h
@@ -262,18 +262,6 @@ namespace Intrinsic {
   LLVM_ABI void getIntrinsicInfoTableEntries(ID id,
                                              SmallVectorImpl<IITDescriptor> &T);
 
-  /// Match the specified function type with the type constraints specified by
-  /// the .td file. If the given type is an overloaded type it is pushed to the
-  /// OverloadTys vector.
-  ///
-  /// Returns false if the given type matches with the constraints, true
-  /// otherwise. If returning true, an error message to indicate the reason of
-  /// mismatch is printed to \p OS.
-  LLVM_ABI bool matchIntrinsicSignature(FunctionType *FTy,
-                                        ArrayRef<IITDescriptor> &Infos,
-                                        SmallVectorImpl<Type *> &OverloadTys,
-                                        raw_ostream &OS);
-
   /// Returns true if \p FT is a valid function type for intrinsic \p ID. If
   /// `ID` is an overloaded intrinsic, the overload types are pushed into the
   /// OverloadTys vector.
