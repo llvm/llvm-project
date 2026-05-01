ad6e4c400b9f719b85f06eaf4177e08c11a1df75
Matt Arsenault <Matthew.Arsenault@amd.com>
2026-05-01 19:46:39 +0100

    clang: Stop using replace_extension when there's no extension
    The offload case is building a fresh filepath and there's no file
    extension to replace. Just directly append the file extension to avoid
    clobbering part of the path name if the triple contains a period.
    
    Avoids confusing test updates in future triple patch.

--- clang/lib/Driver/Driver.cpp
+++ clang/lib/Driver/Driver.cpp
@@ -6037,10 +6037,11 @@ static void handleTimeTrace(Compilation &C, const ArgList &Args,
       if (Arg *FinalOutput = Args.getLastArg(options::OPT_o))
         Path = llvm::sys::path::parent_path(FinalOutput->getValue());
       llvm::sys::path::append(Path, TraceName);
+      Path += ".json";
     } else {
       Path = Result.getFilename();
+      llvm::sys::path::replace_extension(Path, "json");
     }
-    llvm::sys::path::replace_extension(Path, "json");
   }
   const char *ResultFile = C.getArgs().MakeArgString(Path);
   C.addTimeTraceFile(ResultFile, JA);
