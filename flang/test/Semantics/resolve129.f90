939d33cf33200b29a4922ce37c61d3c49b0400a8
Caroline Newcombe <caroline.newcombe@hpe.com>
2026-05-01 12:40:43 -0500

    Updates per review comments

--- flang/test/Semantics/resolve129.f90
+++ flang/test/Semantics/resolve129.f90
@@ -8,3 +8,11 @@ subroutine s1
   !ERROR: PARAMETER attribute not allowed on 'a'
   parameter(a=3)
 end subroutine
+
+subroutine s2
+  !ERROR: 'b' may not have both the POINTER and PARAMETER attributes
+  integer, pointer :: b
+  !ERROR: PARAMETER attribute not allowed on 'b'
+  parameter(b=3)
+end subroutine
+
