24a8b2fad3bd3c25ed5bfe5b76935629e012f690
Jonathan Thackray <jonathan.thackray@arm.com>
2026-05-01 15:16:35 +0100

    fixup! Update diff because SYSP definition has changed

--- llvm/test/MC/AArch64/armv9-sysp-diagnostics.s
+++ llvm/test/MC/AArch64/armv9-sysp-diagnostics.s
@@ -7,35 +7,33 @@
 // registers with 128-bit formats (op0, op1, Cn, Cm, op2)
 // For sysp, op0 is 0
 
-sysp #0, c8, c0, #0, x0, x2
+sysp #0, c2, c0, #0, x0, x2
 // ERRORS: error: expected second odd register of a consecutive 64-bit register pair
-sysp #4, c8, c4, #1, x1, x2
+sysp #4, c2, c0, #1, x1, x2
 // ERRORS: error: expected xzr/xzr or the first even register of a consecutive 64-bit register pair
-sysp #4, c8, c4, #1, x2, x4
+sysp #4, c2, c0, #1, x2, x4
 // ERRORS: error: expected second odd register of a consecutive 64-bit register pair
-sysp #0, c8, c0, #0, x29, x31
+sysp #0, c2, c0, #0, x29, x31
 // ERRORS: error: expected xzr/xzr or the first even register of a consecutive 64-bit register pair
-sysp #0, c8, c0, #0, x30, x30
+sysp #0, c2, c0, #0, x30, x30
 // ERRORS: error: expected second odd register of a consecutive 64-bit register pair
-sysp #0, c8, c0, #0, x31, x0
+sysp #0, c2, c0, #0, x31, x0
 // ERRORS: error: expected second xzr in xzr/xzr register pair
-sysp #4, c8, c4, #1, xzr, x1
+sysp #4, c2, c0, #1, xzr, x1
 // ERRORS: error: expected second xzr in xzr/xzr register pair
-sysp #0, c8, c0, #0, xzr, x30
+sysp #0, c2, c0, #0, xzr, x30
 // ERRORS: error: expected second xzr in xzr/xzr register pair
-sysp #0, c8, c0, #0, w0, w1
+sysp #0, c2, c0, #0, w0, w1
 // ERRORS: error: expected xzr/xzr or the first even register of a consecutive 64-bit register pair
-sysp #7, c8, c0, #0, x0, x1
-// ERRORS: error: immediate must be an integer in range [0, 6].
-sysp #0, c7, c0, #0, x0, x1
-// ERRORS: error: expected cN operand where 8 <= N <= 9
-sysp #0, c10, c0, #0, x0, x1
-// ERRORS: error: expected cN operand where 8 <= N <= 9
-sysp #0, c8, c8, #0, x0, x1
-// ERRORS: error: expected cN operand where 0 <= N <= 7
-sysp #0, c8, c0, #8, x0, x1
+sysp #8, c2, c0, #0, x0, x1
 // ERRORS: error: immediate must be an integer in range [0, 7].
-sysp #0, c8, c0, #0, xzr,
+sysp #0, c16, c0, #0, x0, x1
+// ERRORS: error: Expected cN operand where 0 <= N <= 15
+sysp #0, c2, c16, #0, x0, x1
+// ERRORS: error: Expected cN operand where 0 <= N <= 15
+sysp #0, c2, c0, #8, x0, x1
+// ERRORS: error: immediate must be an integer in range [0, 7].
+sysp #0, c2, c0, #0, xzr,
 // ERRORS: error: expected second xzr in xzr/xzr register pair
 
 tlbip RVAE3IS
@@ -49,7 +47,7 @@ tlbip IPAS2E1, x4, x8
 tlbip RVAE3, x11, x11
 // ERRORS: error: expected xzr/xzr or the first even register of a consecutive 64-bit register pair
 
-sysp #0, c8, c0, #0, x0
+sysp #0, c2, c0, #0, x0
 // ERRORS: error: expected comma
-sysp #0, c8, c0, #0, xzr
+sysp #0, c2, c0, #0, xzr
 // ERRORS: error: expected comma
