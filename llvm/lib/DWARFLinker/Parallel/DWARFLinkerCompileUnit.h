c73ea7dc0125b43c92e883817ce38bda9dcd4f63
Jeff Bailey <jbailey@raspberryginger.com>
2026-05-02 07:18:17 +0100

    [llvm][doc][nfc] Fix typo
    asinchronously -> asynchronously

--- llvm/lib/DWARFLinker/Parallel/DWARFLinkerCompileUnit.h
+++ llvm/lib/DWARFLinker/Parallel/DWARFLinkerCompileUnit.h
@@ -691,7 +691,7 @@ private:
 
   std::unique_ptr<DependencyTracker> Dependencies;
 
-  /// \defgroup Data Members accessed asinchronously.
+  /// \defgroup Data Members accessed asynchronously.
   ///
   /// @{
   OffsetToUnitTy getUnitFromOffset;
