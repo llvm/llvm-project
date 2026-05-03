61469343267f723443ebdd6436336da8c3c4b1ac
Caroline Tice <cmtice@google.com>
2026-05-03 11:33:56 -0700

    [LLDB] Fix UBSan issue with ValueType enums.
    ValueTypeSyntheticMask, when bitwise OR'd with ValueType enums,
    produces a value that is outside the official enum range for ValueTypes.
    This causes UBSan errors, when UBSan is set to check enum values.
    E.g. If you build LLDB with the Cmake flags
    
    -DCMAKE_CXX_FLAGS="-fsanitize=enum -fsanitize-trap=enum"
    -DCMAKE_C_FLAGS="-fsanitize=enum -fsanitize-trap=enum"
    
    Then try to run the LLDB test TestScripedFrameProvider, it crashes
    with a SIGILL from UBSan.
    
    This change fixes that by pulling ValueTypeSyntheticMask into the
    ValueType enums, expanding the valid enum range and making the
    bitwise OR'd values valid.

--- lldb/include/lldb/lldb-enumerations.h
+++ lldb/include/lldb/lldb-enumerations.h
@@ -342,13 +342,12 @@ enum ValueType {
   eValueTypeVariableThreadLocal = 8, ///< thread local storage variable
   eValueTypeVTable = 9,              ///< virtual function table
   eValueTypeVTableEntry = 10, ///< function pointer in virtual function table
+  /// A mask that we can use to check if the value type is synthetic or not.
+  // NOTE: This limits the number of value types to 31, but that's 3x more than
+  // what we currently have now. See lldb/Utility/ValueType.h for helpers for
+  // working with synthetic value types.
+  eValueTypeSyntheticMask = 0x20,
 };
-
-/// A mask that we can use to check if the value type is synthetic or not.
-// NOTE: This limits the number of value types to 31, but that's 3x more than
-// what we currently have now. See lldb/Utility/ValueType.h for helpers for
-// working with synthetic value types.
-static constexpr unsigned ValueTypeSyntheticMask = 0x20;
 
 /// Token size/granularities for Input Readers.
 
