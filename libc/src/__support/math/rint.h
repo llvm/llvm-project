cb948b97b713dfbfed9efda3b56633dff162918a
bassiounix <muhammad.m.bassiouni@gmail.com>
2026-05-04 05:59:44 +0300

    fix constexpr

--- libc/src/__support/math/rint.h
+++ libc/src/__support/math/rint.h
@@ -17,7 +17,7 @@ namespace math {
 
 LIBC_INLINE LIBC_CONSTEXPR double rint(double x) {
 #if defined(__LIBC_USE_BUILTIN_CEIL_FLOOR_RINT_TRUNC) &&                       \
-    !defined(LIBC_HAS_CONSTANT_EVALUATED)
+    !defined(LIBC_HAS_CONSTANT_EVALUATION)
   return __builtin_rint(x);
 #else
   return fputil::round_using_current_rounding_mode(x);
