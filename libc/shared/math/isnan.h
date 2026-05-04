52d790b77e1343f21fd3b3a9b1474f0b26d5e67d
Anonmiraj <ezzibrahimx@gmail.com>
2026-05-04 09:57:24 +0300

    [libc][math] Refactor isnan family to header-only
    Refactored functions:
      - isnan
      - isnanf

--- libc/shared/math/isnan.h
+++ libc/shared/math/isnan.h
@@ -1,4 +1,4 @@
-//===-- Implementation of isnanf function ---------------------------------===//
+//===-- Shared isnan function -----------------------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,11 +6,18 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "src/math/isnanf.h"
-#include "src/__support/math/isnanf.h"
+#ifndef LLVM_LIBC_SHARED_MATH_ISNAN_H
+#define LLVM_LIBC_SHARED_MATH_ISNAN_H
+
+#include "shared/libc_common.h"
+#include "src/__support/math/isnan.h"
 
 namespace LIBC_NAMESPACE_DECL {
+namespace shared {
 
-LLVM_LIBC_FUNCTION(int, isnanf, (float x)) { return math::isnanf(x); }
+using math::isnan;
 
+} // namespace shared
 } // namespace LIBC_NAMESPACE_DECL
+
+#endif // LLVM_LIBC_SHARED_MATH_ISNAN_H
