6571800f1301ac27b54ef670e894004bfaf5f40d
Adam Scott <adamscott200322@gmail.com>
2026-05-04 18:20:43 -0400

    Fix clang-format

--- llvm/lib/Target/AMDGPU/AMDGPURegBankLegalizeRules.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPURegBankLegalizeRules.cpp
@@ -1689,9 +1689,9 @@ RegBankLegalizeRules::RegBankLegalizeRules(const GCNSubtarget &_ST,
   addRulesForIOpcs({amdgcn_permlane_bcast, amdgcn_permlane_up,
                     amdgcn_permlane_down, amdgcn_permlane_xor},
                    Standard)
-      .Div(S32, {{Vgpr32},
-                 {IntrId, Vgpr32, SgprB32_ReadFirstLane,
-                  SgprB32_ReadFirstLane}});
+      .Div(S32,
+           {{Vgpr32},
+            {IntrId, Vgpr32, SgprB32_ReadFirstLane, SgprB32_ReadFirstLane}});
 
   addRulesForIOpcs({amdgcn_permlane_idx_gen}, Standard)
       .Div(S32, {{Vgpr32}, {IntrId, Vgpr32, SgprB32_ReadFirstLane}});
