96058ad3f2f0d84569f00ccd14e5ee62a98b0162
John Lu <John.Lu@amd.com>
2026-05-04 16:04:39 -0500

    Limit search for matching phi-node
    Signed-off-by: John Lu <John.Lu@amd.com>

--- llvm/include/llvm/Transforms/Utils/SSAUpdaterImpl.h
+++ llvm/include/llvm/Transforms/Utils/SSAUpdaterImpl.h
@@ -414,7 +414,18 @@ public:
   /// them match what is needed.
   void FindExistingPHI(BlkT *BB) {
     SmallVector<BBInfo *, 20> TaggedBlocks;
+    // SearchLimit is needed to guard against pathological cases (e.g.
+    // AMDGPU/large-phi-search.ll) where a large number of searches are done
+    // which all fail.  Each search adds another PHI node to be searched.  In a
+    // 3-stage build of LLVM the maximum search length was 53.
+    constexpr unsigned SearchLimit = 80;
+    unsigned Count = 0;
+
     for (auto &SomePHI : BB->phis()) {
+      // Abandon search for match.  FindAvailableVals will create a new
+      // phi-node.
+      if (++Count > SearchLimit)
+        break;
       if (CheckIfPHIMatches(&SomePHI, TaggedBlocks)) {
         RecordMatchingPHIs(TaggedBlocks);
         break;
