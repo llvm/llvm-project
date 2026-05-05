4c82193ebab59201483b3304078ae186d3960581
Schrodinger ZHU Yifan <yifanzhu@rochester.edu>
2026-05-05 14:25:14 -0400

    update

--- libc/test/src/__support/CPP/expected_test.cpp
+++ libc/test/src/__support/CPP/expected_test.cpp
@@ -13,6 +13,11 @@
 using LIBC_NAMESPACE::cpp::expected;
 using LIBC_NAMESPACE::cpp::unexpected;
 
+struct SimpleStruct {
+  int number;
+  char letter;
+};
+
 class MoveOnly {
   int *destroyed;
 
@@ -34,7 +39,19 @@ public:
 
 static_assert(
     LIBC_NAMESPACE::cpp::is_trivially_destructible_v<expected<int, int>>,
-    "expected should be trivially destructible for trivial payloads");
+    "expected should be trivially destructible for primitive payloads");
+static_assert(LIBC_NAMESPACE::cpp::is_trivially_destructible_v<
+                  expected<SimpleStruct, int>>,
+              "expected should be trivially destructible for a trivial value "
+              "payload");
+static_assert(LIBC_NAMESPACE::cpp::is_trivially_destructible_v<
+                  expected<int, SimpleStruct>>,
+              "expected should be trivially destructible for a trivial error "
+              "payload");
+static_assert(LIBC_NAMESPACE::cpp::is_trivially_destructible_v<
+                  expected<SimpleStruct, SimpleStruct>>,
+              "expected should be trivially destructible for trivial value and "
+              "error payloads");
 static_assert(
     !LIBC_NAMESPACE::cpp::is_trivially_destructible_v<expected<MoveOnly, int>>,
     "expected should be nontrivially destructible for nontrivial "
@@ -43,6 +60,39 @@ static_assert(
     !LIBC_NAMESPACE::cpp::is_trivially_destructible_v<expected<int, MoveOnly>>,
     "expected should be nontrivially destructible for nontrivial "
     "error payloads");
+static_assert(!LIBC_NAMESPACE::cpp::is_trivially_destructible_v<
+                  expected<MoveOnly, MoveOnly>>,
+              "expected should be nontrivially destructible for nontrivial "
+              "value and error payloads");
+
+TEST(LlvmLibcExpectedTest, PrimitiveValue) {
+  expected<int, int> value(42);
+  ASSERT_TRUE(value.has_value());
+  ASSERT_TRUE(static_cast<bool>(value));
+  ASSERT_EQ(value.value(), 42);
+  ASSERT_EQ(*value, 42);
+}
+
+TEST(LlvmLibcExpectedTest, PrimitiveError) {
+  expected<int, int> error(unexpected<int>(17));
+  ASSERT_FALSE(error.has_value());
+  ASSERT_FALSE(static_cast<bool>(error));
+  ASSERT_EQ(error.error(), 17);
+}
+
+TEST(LlvmLibcExpectedTest, SimpleStructValue) {
+  expected<SimpleStruct, int> value(SimpleStruct{123, 'a'});
+  ASSERT_TRUE(value.has_value());
+  ASSERT_EQ(value->number, 123);
+  ASSERT_EQ(value->letter, 'a');
+}
+
+TEST(LlvmLibcExpectedTest, SimpleStructError) {
+  expected<int, SimpleStruct> error(unexpected<SimpleStruct>({456, 'b'}));
+  ASSERT_FALSE(error.has_value());
+  ASSERT_EQ(error.error().number, 456);
+  ASSERT_EQ(error.error().letter, 'b');
+}
 
 TEST(LlvmLibcExpectedTest, DestroysNonTrivialValue) {
   int destroyed = 0;
