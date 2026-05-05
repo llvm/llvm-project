9e72d74728149197e6a25b9e078879d3eb60a500
Ron Lieberman <ron.lieberman@amd.com>
2026-05-04 19:53:22 -0500

    Revert "[flang][flang-rt] Add support for non-standard TIMEF intrinsic (#185377)"
    This reverts commit aeb14f780b174d55a7d60df4001c7da29c6c96ac.

--- flang/lib/Optimizer/Builder/IntrinsicCall.cpp
+++ flang/lib/Optimizer/Builder/IntrinsicCall.cpp
@@ -808,7 +808,6 @@ static constexpr IntrinsicHandler handlers[]{
        {"team", asBox, handleDynamicOptional}}},
      /*isElemental=*/false},
     {"time", &I::genTime, {}, /*isElemental=*/false},
-    {"timef", &I::genTimef, {}, /*isElemental=*/false},
     {"tokenize",
      &I::genTokenize,
      {{{"string", asAddr},
@@ -8266,13 +8265,6 @@ IntrinsicLibrary::genThisImage(mlir::Type resultType,
     TODO(loc, "this_image with coarray argument.");
   mlir::Value res = mif::ThisImageOp::create(builder, loc, team);
   return builder.createConvert(loc, resultType, res);
-}
-
-// TIMEF
-mlir::Value IntrinsicLibrary::genTimef(mlir::Type resultType,
-                                       llvm::ArrayRef<mlir::Value> args) {
-  assert(args.empty() && "timef does not accept any args");
-  return fir::runtime::genTimef(builder, loc);
 }
 
 // TRAILZ
