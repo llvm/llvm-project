4bed3f209f032a330ee80d3f070fa5ac1a7adc59
ShashwathiNavada <shashwathinavada@gmail.com>
2026-05-05 07:54:02 -0500

    Updated the Check

--- flang/lib/Semantics/tools.cpp
+++ flang/lib/Semantics/tools.cpp
@@ -330,7 +330,12 @@ const Symbol *FindExternallyVisibleObject(
   // TODO: Storage association with any object for which this predicate holds,
   // once EQUIVALENCE is supported.
   const Symbol &ultimate{GetAssociationRoot(object)};
-  if (IsDummy(ultimate)) {
+  if (ultimate.owner().IsDerivedType()) {
+    return nullptr;
+  } else if (!IsDummy(ultimate) && (IsUseAssociated(object, scope) ||
+      IsHostAssociatedIntoSubprogram(object, scope)) ){
+    return &object;
+  } else if (IsDummy(ultimate)) {
     if (IsIntentIn(ultimate)) {
       return &ultimate;
     }
@@ -338,16 +343,7 @@ const Symbol *FindExternallyVisibleObject(
         IsPureProcedure(ultimate.owner()) && IsFunction(ultimate.owner())) {
       return &ultimate;
     }
-  } else if (IsFunctionResult(ultimate)) {
-    // A function result variable is local to its function and not an
-    // externally visible object for C1594 checks.
-    return nullptr;
-  } else if (ultimate.owner().IsDerivedType()) {
-    return nullptr;
-  } else if (&GetProgramUnitContaining(ultimate) !=
-      &GetProgramUnitContaining(scope)) {
-    return &object;
-  } else if (const Symbol *block{FindCommonBlockContaining(ultimate)}) {
+  } else if (const Symbol *block {FindCommonBlockContaining(ultimate)}) {
     return block;
   }
   return nullptr;
