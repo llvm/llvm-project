3e10839a6bb63e1539b1a5632f7b5675a44121e7
Alexey Samsonov <vonosmas@gmail.com>
2026-05-05 06:53:33 +0000

    Restore function decls on Windows to fix build.

--- libcxx/include/__thread/support/windows.h
+++ libcxx/include/__thread/support/windows.h
@@ -86,6 +86,11 @@ _LIBCPP_EXPORTED_FROM_ABI int __libcpp_execute_once(__libcpp_exec_once_flag* __f
 //
 typedef long __libcpp_thread_id;
 
+// TODO: Clarify whether to keep these functions since they were _LIBCPP_EXPORTED_FROM_ABI.
+_LIBCPP_EXPORTED_FROM_ABI bool __libcpp_thread_id_equal(__libcpp_thread_id __t1, __libcpp_thread_id __t2);
+
+_LIBCPP_EXPORTED_FROM_ABI bool __libcpp_thread_id_less(__libcpp_thread_id __t1, __libcpp_thread_id __t2);
+
 //
 // Thread
 //
