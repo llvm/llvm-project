a25cc50d919a949e3403f91b499f2db76a0f463b
mlevine55 <mlevine55@bloomberg.net>
2026-05-05 11:15:47 -0400

    Added the optimizations
    Signed-off-by: mlevine55 <mlevine55@bloomberg.net>

--- libcxx/test/libcxx/type_traits/is_transparently_comparable.compile.pass.cpp
+++ libcxx/test/libcxx/type_traits/is_transparently_comparable.compile.pass.cpp
@@ -10,8 +10,10 @@
 
 #include <functional>
 #include <string>
+#include <string_view>
 #include <__type_traits/desugars_to.h>
 
+// basic_string with char pointers/arrays
 static_assert(std::__is_transparently_comparable_v<std::less<std::string>, std::string, const char*>, "");
 static_assert(std::__is_transparently_comparable_v<std::less<std::string>, std::string, char*>, "");
 static_assert(std::__is_transparently_comparable_v<std::less<std::string>, std::string, char[5]>, "");
@@ -30,3 +32,29 @@ static_assert(
 static_assert(
     !std::__is_transparently_comparable_v<std::less<std::reference_wrapper<std::string> >, char const*, std::string>,
     "");
+
+// basic_string_view with char pointers/arrays
+static_assert(
+    std::__is_transparently_comparable_v<std::less<std::string_view>, std::string_view, const char*>, "");
+static_assert(std::__is_transparently_comparable_v<std::less<std::string_view>, std::string_view, char*>, "");
+static_assert(std::__is_transparently_comparable_v<std::less<std::string_view>, std::string_view, char[5]>, "");
+
+static_assert(
+    std::__is_transparently_comparable_v<std::less<std::string_view>, const char*, std::string_view>, "");
+static_assert(std::__is_transparently_comparable_v<std::less<std::string_view>, char*, std::string_view>, "");
+static_assert(std::__is_transparently_comparable_v<std::less<std::string_view>, char[5], std::string_view>, "");
+
+static_assert(
+    !std::__is_transparently_comparable_v<std::less<std::reference_wrapper<std::string_view> >,
+                                          std::string_view,
+                                          const char*>,
+    "");
+static_assert(
+    !std::__is_transparently_comparable_v<std::less<std::reference_wrapper<std::string_view> >,
+                                          const char*,
+                                          std::string_view>,
+    "");
+
+// Cross-type: basic_string key with basic_string_view argument
+static_assert(std::__is_transparently_comparable_v<std::less<std::string>, std::string, std::string_view>, "");
+static_assert(std::__is_transparently_comparable_v<std::less<std::string>, std::string_view, std::string>, "");
