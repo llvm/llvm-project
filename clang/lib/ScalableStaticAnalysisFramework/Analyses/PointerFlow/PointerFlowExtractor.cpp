ce910bfbc0f563538915a5effc344b144491a956
Aiden Grossman <aidengrossman@google.com>
2026-05-06 01:40:00 +0000

    [SSAF] Fix -Wunused-variable
    Add [[maybe_unused]] given the operation is side effecting/returns
    multiple values.

--- clang/lib/ScalableStaticAnalysisFramework/Analyses/PointerFlow/PointerFlowExtractor.cpp
+++ clang/lib/ScalableStaticAnalysisFramework/Analyses/PointerFlow/PointerFlowExtractor.cpp
@@ -347,7 +347,7 @@ public:
       if (!ContributorName)
         llvm::reportFatalInternalError(makeEntityNameErr(Ctx, CD));
 
-      auto [_, InsertionSucceeded] = SummaryBuilder.addSummary(
+      [[maybe_unused]] auto [_, InsertionSucceeded] = SummaryBuilder.addSummary(
           addEntity(*ContributorName), std::move(*EntitySummary));
 
       assert(InsertionSucceeded && "duplicated contributor extraction");
