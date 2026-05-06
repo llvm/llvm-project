b0d1805e6edd8a938b64cb134224865594e6a989
Michael Jones <michaelrj@google.com>
2026-05-06 18:28:31 +0000

    [libc] implement putwc
    Add putwc function and tests. Part 9/11. All build file changes are in
    part 11.
    
    Assisted by Gemini

---
+++ libc/src/wchar/putwc.h
@@ -0,0 +1,27 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file contains the implementation header for the putwc function.
+///
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_LIBC_SRC_WCHAR_PUTWC_H
+#define LLVM_LIBC_SRC_WCHAR_PUTWC_H
+
+#include "hdr/types/FILE.h"
+#include "hdr/types/wint_t.h"
+#include "src/__support/macros/config.h"
+
+namespace LIBC_NAMESPACE_DECL {
+
+wint_t putwc(wchar_t wc, ::FILE *stream);
+
+} // namespace LIBC_NAMESPACE_DECL
+
+#endif // LLVM_LIBC_SRC_WCHAR_PUTWC_H
