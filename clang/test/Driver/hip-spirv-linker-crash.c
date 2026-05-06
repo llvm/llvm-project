480ba1e61df5dbe1d719e9b0a76bf0ca228840eb
Manuel Carrasco <Manuel.Carrasco@amd.com>
2026-05-06 08:40:01 -0500

    [Driver][HIP/SPIRV] Fix crash when llvm-link is executed.
    There is a design limitation that is forwarding flags to llvm-link
    when it shouldn't happen. This commit fixes this issue by sanitizing
    the arguments forwarded to llvm-link.
    
    This may happen when clang-linker-wrapper eventually calls clang.
    Crash reproducer is here: https://gcc.godbolt.org/z/rxvWcvan3.
    
    The fix is based on MrSidims's old PR (#183492).
    
    Co-authored-by: Dmitry Sidorov <18708689+MrSidims@users.noreply.github.com>
    Co-authored-by: Manuel Carrasco <manuel.carrasco@amd.com>

---
+++ clang/test/Driver/hip-spirv-linker-crash.c
@@ -0,0 +1,16 @@
+// Verify that SPIR-V compilation does not crash during the llvm-link step
+// due to extra args that are not meant to be forwarded there.
+//
+// RUN: %clang -### --target=spirv64-amd-amdhsa -use-spirv-backend \
+// RUN:   -Xlinker -opt-bisect-limit=-1 %s 2>&1 \
+// RUN:   | FileCheck %s --check-prefix=CHECK-LINKER-OPT
+//
+// RUN: %clang -### --target=spirv64-amd-amdhsa -use-spirv-backend \
+// RUN:   -Xlinker -mllvm -Xlinker -opt-bisect-limit=-1 %s 2>&1 \
+// RUN:   | FileCheck %s --check-prefix=CHECK-LINKER-MLLVM
+//
+// CHECK-LINKER-OPT: "{{.*}}llvm-link"
+// CHECK-LINKER-OPT-SAME: "-o" "{{.*}}" "{{.*}}"{{$}}
+//
+// CHECK-LINKER-MLLVM: "{{.*}}llvm-link"
+// CHECK-LINKER-MLLVM-SAME: "-o" "{{.*}}" "{{.*}}"{{$}}
