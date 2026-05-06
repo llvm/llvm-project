85544438d337bff1a50026b36db51f60fa0ba081
Victor Mustya <victor.mustya@intel.com>
2026-05-06 09:43:35 -0700

    Use Clang SPIR-V built-in functions, when it's possible

--- compiler-rt/lib/ubsan_minimal/targets/spirv64.cpp
+++ compiler-rt/lib/ubsan_minimal/targets/spirv64.cpp
@@ -1,45 +1,51 @@
 #include "ubsan_minimal_common.h"
 
-using uint3 = unsigned __attribute__((ext_vector_type(3)));
+#define __gpu_constant __attribute__((opencl_constant))
 
+using size_t3 = size_t [[clang::ext_vector_type(3)]];
 struct __ubsan_abort_info_t {
-  __attribute__((opencl_constant)) const char *fmt;
+  __gpu_constant const char *fmt;
   const char *kind;
   uintptr_t caller;
-  uint3 gid;
-  uint3 lid;
+  size_t3 gid;
+  size_t3 lid;
 };
 
 // OpenCL printf maps to OpExtInst printf (OpenCL extended instruction set).
-extern "C" int printf(__attribute__((opencl_constant)) const char *fmt, ...);
-
-// OpenCL work-item builtins map to SPIR-V BuiltIn variables.
-extern "C" unsigned get_group_id(unsigned dim);
-extern "C" unsigned get_local_id(unsigned dim);
-
-static __attribute__((opencl_constant)) const char ubsan_msg_simple[] = "%s";
-static __attribute__((opencl_constant)) const char ubsan_msg_fmt[] =
-    "ubsan: %s by 0x%lx at gid=[%v3u] lid=[%v3u]\n";
-
-void __ubsan_message(const char *msg) { printf(ubsan_msg_simple, msg); }
-
-void __ubsan_message(const char *kind, uintptr_t caller) {
-  uint3 gid = {get_group_id(0), get_group_id(1), get_group_id(2)};
-  uint3 lid = {get_local_id(0), get_local_id(1), get_local_id(2)};
-
-  printf(ubsan_msg_fmt, kind, caller, gid, lid);
-}
+extern "C" int printf(__gpu_constant const char *fmt, ...);
 
 // SPV_KHR_abort: OpAbortKHR terminates the invocation and passes a message
 // to the client API. The message is passed as a typed value.
 [[noreturn]] void __spirv_AbortKHR(__ubsan_abort_info_t info);
 
+static __gpu_constant const char ubsan_msg_simple[] = "%s";
+static __gpu_constant const char ubsan_msg_fmt[] =
+    "ubsan: %s by 0x%lx at gid=[%v3lu] lid=[%v3lu]\n";
+
+void __ubsan_message(const char *msg) { printf(ubsan_msg_simple, msg); }
+
+void __ubsan_message(const char *kind, uintptr_t caller) {
+  size_t3 gid = {__builtin_spirv_workgroup_id(0),
+                 __builtin_spirv_workgroup_id(1),
+                 __builtin_spirv_workgroup_id(2)};
+  size_t3 lid = {__builtin_spirv_local_invocation_id(0),
+                 __builtin_spirv_local_invocation_id(1),
+                 __builtin_spirv_local_invocation_id(2)};
+
+  printf(ubsan_msg_fmt, kind, caller, gid, lid);
+}
+
 [[noreturn]] void __ubsan_abort() { __ubsan_abort_with_message("abort", 0); }
 
 [[noreturn]] void __ubsan_abort_with_message(const char *kind,
                                              uintptr_t caller) {
-  uint3 gid = {get_group_id(0), get_group_id(1), get_group_id(2)};
-  uint3 lid = {get_local_id(0), get_local_id(1), get_local_id(2)};
+  size_t3 gid = {__builtin_spirv_workgroup_id(0),
+                 __builtin_spirv_workgroup_id(1),
+                 __builtin_spirv_workgroup_id(2)};
+  size_t3 lid = {__builtin_spirv_local_invocation_id(0),
+                 __builtin_spirv_local_invocation_id(1),
+                 __builtin_spirv_local_invocation_id(2)};
+
   __ubsan_abort_info_t info = {ubsan_msg_fmt, kind, caller, gid, lid};
   __spirv_AbortKHR(info);
 }
--- compiler-rt/lib/ubsan_minimal/ubsan_minimal_common.h
+++ compiler-rt/lib/ubsan_minimal/ubsan_minimal_common.h
@@ -13,14 +13,15 @@
 #ifndef UBSAN_MINIMAL_COMMON_H
 #define UBSAN_MINIMAL_COMMON_H
 
-#if defined(__UINTPTR_TYPE__)
+#if defined(__UINTPTR_TYPE__) && defined(__SIZE_TYPE__)
 typedef __UINTPTR_TYPE__ uintptr_t;
+typedef __SIZE_TYPE__ size_t;
 
 static_assert(sizeof(uintptr_t) == sizeof(void *),
               "uintptr_t must be the same size as void*");
-#else // defined(__UINTPTR_TYPE__)
+#else // defined(__UINTPTR_TYPE__) && defined(__SIZE_TYPE__)
 #include <stdint.h>
-#endif // defined(__UINTPTR_TYPE__)
+#endif // defined(__UINTPTR_TYPE__) && defined(__SIZE_TYPE__)
 
 void __ubsan_message(const char *msg);
 void __ubsan_message(const char *kind, uintptr_t caller);
