83e736eb7f036dea65c690defeb6f64a80b6e11d
GkvJwa <gkvjwa@gmail.com>
2026-05-06 15:05:21 +0800

    [clang] Add arm64_neon.h wrapper
    Add an MSVC-compatible <arm64_neon.h> resource header that forwards to
    Clang's generated <arm_neon.h>. This lets ARM64 Windows code using the
    MSVC header name lower NEON intrinsics through Clang builtins instead of
    leaving external neon_* calls such as neon_ld1m4_q32
    
    Fix #195683

---
+++ clang/lib/Headers/arm64_neon.h
@@ -0,0 +1,15 @@
+/*===---- arm64_neon.h - ARM64 NEON intrinsics -----------------------------===
+ *
+ * Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+ * See https://llvm.org/LICENSE.txt for license information.
+ * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+ *
+ *===-----------------------------------------------------------------------===
+ */
+
+#ifndef __ARM64_NEON_H
+#define __ARM64_NEON_H
+
+#include <arm_neon.h>
+
+#endif /* __ARM64_NEON_H */
