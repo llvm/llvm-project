425a086788c9ea5ffb42078a2edb36a4db5f7b8e
mlevine55 <mlevine55@bloomberg.net>
2026-05-06 13:42:26 -0400

    add more compile time tests
    Signed-off-by: mlevine55 <mlevine55@bloomberg.net>

--- libcxx/test/libcxx/type_traits/is_transparently_comparable.compile.pass.cpp
+++ libcxx/test/libcxx/type_traits/is_transparently_comparable.compile.pass.cpp
@@ -13,7 +13,7 @@
 #include <string_view>
 #include <__type_traits/desugars_to.h>
 
-// basic_string with char pointers/arrays
+// basic_string with char pointers/arrays (std::less)
 static_assert(std::__is_transparently_comparable_v<std::less<std::string>, std::string, const char*>, "");
 static_assert(std::__is_transparently_comparable_v<std::less<std::string>, std::string, char*>, "");
 static_assert(std::__is_transparently_comparable_v<std::less<std::string>, std::string, char[5]>, "");
@@ -22,6 +22,16 @@ static_assert(std::__is_transparently_comparable_v<std::less<std::string>, const
 static_assert(std::__is_transparently_comparable_v<std::less<std::string>, char*, std::string>, "");
 static_assert(std::__is_transparently_comparable_v<std::less<std::string>, char[5], std::string>, "");
 
+// basic_string with char pointers/arrays (std::greater)
+static_assert(std::__is_transparently_comparable_v<std::greater<std::string>, std::string, const char*>, "");
+static_assert(std::__is_transparently_comparable_v<std::greater<std::string>, std::string, char*>, "");
+static_assert(std::__is_transparently_comparable_v<std::greater<std::string>, std::string, char[5]>, "");
+
+static_assert(std::__is_transparently_comparable_v<std::greater<std::string>, const char*, std::string>, "");
+static_assert(std::__is_transparently_comparable_v<std::greater<std::string>, char*, std::string>, "");
+static_assert(std::__is_transparently_comparable_v<std::greater<std::string>, char[5], std::string>, "");
+
+// basic_string negative tests: wrong comparator
 static_assert(
     !std::__is_transparently_comparable_v<std::less<std::reference_wrapper<std::string> >, std::string, char[5]>, "");
 static_assert(
@@ -33,7 +43,11 @@ static_assert(
     !std::__is_transparently_comparable_v<std::less<std::reference_wrapper<std::string> >, char const*, std::string>,
     "");
 
-// basic_string_view with char pointers/arrays
+// basic_string negative tests: mismatched character type
+static_assert(!std::__is_transparently_comparable_v<std::less<std::string>, std::string, const wchar_t*>, "");
+static_assert(!std::__is_transparently_comparable_v<std::less<std::string>, const wchar_t*, std::string>, "");
+
+// basic_string_view with char pointers/arrays (std::less)
 static_assert(std::__is_transparently_comparable_v<std::less<std::string_view>, std::string_view, const char*>, "");
 static_assert(std::__is_transparently_comparable_v<std::less<std::string_view>, std::string_view, char*>, "");
 static_assert(std::__is_transparently_comparable_v<std::less<std::string_view>, std::string_view, char[5]>, "");
@@ -42,6 +56,18 @@ static_assert(std::__is_transparently_comparable_v<std::less<std::string_view>, 
 static_assert(std::__is_transparently_comparable_v<std::less<std::string_view>, char*, std::string_view>, "");
 static_assert(std::__is_transparently_comparable_v<std::less<std::string_view>, char[5], std::string_view>, "");
 
+// basic_string_view with char pointers/arrays (std::greater)
+static_assert(
+    std::__is_transparently_comparable_v<std::greater<std::string_view>, std::string_view, const char*>, "");
+static_assert(std::__is_transparently_comparable_v<std::greater<std::string_view>, std::string_view, char*>, "");
+static_assert(std::__is_transparently_comparable_v<std::greater<std::string_view>, std::string_view, char[5]>, "");
+
+static_assert(
+    std::__is_transparently_comparable_v<std::greater<std::string_view>, const char*, std::string_view>, "");
+static_assert(std::__is_transparently_comparable_v<std::greater<std::string_view>, char*, std::string_view>, "");
+static_assert(std::__is_transparently_comparable_v<std::greater<std::string_view>, char[5], std::string_view>, "");
+
+// basic_string_view negative tests: wrong comparator
 static_assert(!std::__is_transparently_comparable_v<std::less<std::reference_wrapper<std::string_view> >,
                                                     std::string_view,
                                                     const char*>,
@@ -50,7 +76,33 @@ static_assert(!std::__is_transparently_comparable_v<std::less<std::reference_wra
                                                     const char*,
                                                     std::string_view>,
               "");
+static_assert(!std::__is_transparently_comparable_v<std::less<std::reference_wrapper<std::string_view> >,
+                                                    std::string_view,
+                                                    char[5]>,
+              "");
+static_assert(!std::__is_transparently_comparable_v<std::less<std::reference_wrapper<std::string_view> >,
+                                                    char[5],
+                                                    std::string_view>,
+              "");
 
-// Cross-type: basic_string key with basic_string_view argument
+// basic_string_view negative tests: mismatched character type
+static_assert(!std::__is_transparently_comparable_v<std::less<std::string_view>, std::string_view, const wchar_t*>, "");
+static_assert(!std::__is_transparently_comparable_v<std::less<std::string_view>, const wchar_t*, std::string_view>, "");
+
+// Cross-type: basic_string key with basic_string_view argument (std::less)
 static_assert(std::__is_transparently_comparable_v<std::less<std::string>, std::string, std::string_view>, "");
 static_assert(std::__is_transparently_comparable_v<std::less<std::string>, std::string_view, std::string>, "");
+
+// Cross-type: basic_string key with basic_string_view argument (std::greater)
+static_assert(std::__is_transparently_comparable_v<std::greater<std::string>, std::string, std::string_view>, "");
+static_assert(std::__is_transparently_comparable_v<std::greater<std::string>, std::string_view, std::string>, "");
+
+// Cross-type negative: wrong comparator
+static_assert(!std::__is_transparently_comparable_v<std::less<std::reference_wrapper<std::string> >,
+                                                    std::string,
+                                                    std::string_view>,
+              "");
+static_assert(!std::__is_transparently_comparable_v<std::less<std::reference_wrapper<std::string> >,
+                                                    std::string_view,
+                                                    std::string>,
+              "");
