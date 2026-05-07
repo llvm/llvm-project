llvm/llvm-project

commit 7b4175c17f16f6597e5c80a017ea2f8aa1b09fcb

[Instrumentor] Add Instrumentor pass (#138958)
This commit adds the basic infrastructure for the Instrumentor pass, which
allows instrumenting code in a simple and customizable way. This commit
adds support for instrumenting load and store instructions. The
Instrumentor can be configured with a JSON file that describes what
should be instrumented, or can be used programmatically from another
pass.

The default JSON config file can be found in:
`llvm/test/Instrumentation/Instrumentor/default_config.json`. More
information about Instrumentor in the
[RFC](https://discourse.llvm.org/t/rfc-introducing-instrumentor-easily-customizable-code-instrumentation/86020).

This is only a squash commit of several contributions to the
Instrumentor. The authors and contributors of this pass are:

- Johannes Doerfert @jdoerfert
- Kevin Sala @kevinsala
- Ivan Radanov Ivanov @ivanradanov
- Ethan Luis McDonough @EthanLuisMcDonough

---------

Co-authored-by: Johannes Doerfert <johannes@jdoerfert.de>
Co-authored-by: Ivan Radanov Ivanov <ivanov.i.aa@m.titech.ac.jp>
Co-authored-by: Ethan Luis McDonough <ethanluismcdonough@gmail.com>

llvm/include/llvm/Transforms/IPO/InstrumentorUtils.h
@@ -1,0 +1,170 @@
+//===-- Transforms/IPO/InstrumentorUtils.h --------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// General utilities for the Instrumentor pass.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_TRANSFORMS_IPO_INSTRUMENTOR_UTILS_H
+#define LLVM_TRANSFORMS_IPO_INSTRUMENTOR_UTILS_H
+
+#include "llvm/ADT/DenseMap.h"
+#include "llvm/ADT/StringRef.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/DataLayout.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/Instruction.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+
+#include <bitset>
+#include <tuple>
+
+namespace llvm {
+namespace instrumentor {
+
+/// An IR builder augmented with extra information for the instrumentor pass.
+/// The underlying IR builder features an insertion callback to keep track of
+/// the new instructions.
+struct InstrumentorIRBuilderTy {
+  /// Construct an IR builder for the module \p M.
+  InstrumentorIRBuilderTy(Module &M)
+      : M(M), Ctx(M.getContext()),
+        IRB(Ctx, ConstantFolder(),
+            // Save the inserted instructions in a structure.
+            IRBuilderCallbackInserter(
+                [&](Instruction *I) { NewInsts[I] = Epoch; })) {}
+
+  /// Destroy the IR builder and remove all erasable instructions cached during
+  /// the process of instrumenting.
+  ~InstrumentorIRBuilderTy() {
+    for (auto *I : ErasableInstructions) {
+      if (!I->getType()->isVoidTy())
+        I->replaceAllUsesWith(PoisonValue::get(I->getType()));
+      I->eraseFromParent();
+    }
+  }
+
+  /// Get a temporary alloca to communicate (large) values with the runtime.
+  AllocaInst *getAlloca(Function *Fn, Type *Ty, bool MatchType = false) {
+    const DataLayout &DL = Fn->getDataLayout();
+    auto *&AllocaList = AllocaMap[{Fn, DL.getTypeAllocSize(Ty)}];
+    if (!AllocaList)
+      AllocaList = new AllocaListTy;
+    AllocaInst *AI = nullptr;
+    for (auto *&ListAI : *AllocaList) {
+      if (MatchType && ListAI->getAllocatedType() != Ty)
+        continue;
+      AI = ListAI;
+      ListAI = *AllocaList->rbegin();
+      break;
+    }
+    if (AI)
+      AllocaList->pop_back();
+    else
+      AI = new AllocaInst(Ty, DL.getAllocaAddrSpace(), "",
+                          Fn->getEntryBlock().begin());
+    UsedAllocas[AI] = AllocaList;
+    return AI;
+  }
+
+  /// Return the temporary allocas.
+  void returnAllocas() {
+    for (auto [AI, List] : UsedAllocas)
+      List->push_back(AI);
+    UsedAllocas.clear();
+  }
+
+  /// Save instruction \p I to be erased later. The instructions are erased when
+  /// the IR builder is destroyed.
+  void eraseLater(Instruction *I) { ErasableInstructions.insert(I); }
+
+  /// Commonly used values for IR inspection and creation.
+  ///{
+  Module &M;
+
+  LLVMContext &Ctx;
+
+  const DataLayout &DL = M.getDataLayout();
+
+  Type *VoidTy = Type::getVoidTy(Ctx);
+  PointerType *PtrTy = PointerType::get(Ctx, 0);
+  IntegerType *Int8Ty = Type::getInt8Ty(Ctx);
+  IntegerType *Int32Ty = Type::getInt32Ty(Ctx);
+  IntegerType *Int64Ty = Type::getInt64Ty(Ctx);
+  ///}
+
+  using AllocaListTy = SmallVector<AllocaInst *>;
+
+  /// Map that holds a list of currently available allocas for a function and
+  /// alloca size.
+  DenseMap<std::pair<Function *, unsigned>, AllocaListTy *> AllocaMap;
+
+  /// Map that holds the currently used allocas and the list where they belong.
+  /// Once an alloca has to be returned, it is returned directly to its list.
+  DenseMap<AllocaInst *, AllocaListTy *> UsedAllocas;
+
+  /// Instructions that should be erased later.
+  SmallPtrSet<Instruction *, 32> ErasableInstructions;
+
+  /// The underlying IR builder with insertion callback.
+  IRBuilder<ConstantFolder, IRBuilderCallbackInserter> IRB;
+
+  /// The current epoch number. Each instrumentation, e.g., of an instruction,
+  /// is happening in a dedicated epoch. The epoch allows to determine if
+  /// instrumentation instructions were already around, due to prior
+  /// instrumentations, or have been introduced to support the current
+  /// instrumentation, e.g., compute information about the current instruction.
+  unsigned Epoch = 0;
+
+  /// A mapping from instrumentation instructions to the epoch they have been
+  /// created.
+  DenseMap<Instruction *, unsigned> NewInsts;
+};
+
+/// Helper that represent the caches for instrumentation call arguments. The
+/// value of an argument may not need to be recomputed between the pre and post
+/// instrumentation calls.
+struct InstrumentationCaches {
+  /// A cache for direct and indirect arguments. The cache is indexed by the
+  /// epoch, the instrumentation opportunity name and the argument name. The
+  /// result is a value.
+  DenseMap<std::tuple<unsigned, StringRef, StringRef>, Value *> DirectArgCache;
+  DenseMap<std::tuple<unsigned, StringRef, StringRef>, Value *>
+      IndirectArgCache;
+};
+
+/// Boolean option bitset with a compile-time number of bits to store as many
+/// options as the enumeration type \p EnumTy defines. The enumeration type is
+/// expected to have an ascending and consecutive values, starting at zero, and
+/// the last value being artificial and named as NumConfig (i.e., the number of
+/// values in the enumeration).
+template <typename EnumTy> struct BaseConfigTy {
+  /// The bistset with as many bits as the enumeration's values.
+  std::bitset<static_cast<int>(EnumTy::NumConfig)> Options;
+
+  /// Construct the option bitset with all bits set to \p Enable. If not
+  /// provided, all options are enabled.
+  BaseConfigTy(bool Enable = true) {
+    if (Enable)
+      Options.set();
+  }
+
+  /// Check if the option \p Opt is enabled.
+  bool has(EnumTy Opt) const { return Options.test(static_cast<int>(Opt)); }
+
+  /// Set the boolean value of option \p Opt to \p Value.
+  void set(EnumTy Opt, bool Value = true) {
+    Options.set(static_cast<int>(Opt), Value);
+  }
+};
+
+} // namespace instrumentor
+} // end namespace llvm
+
+#endif // LLVM_TRANSFORMS_IPO_INSTRUMENTOR_UTILS_H

llvm/include/llvm/Transforms/IPO/InstrumentorConfigFile.h
@@ -1,0 +1,33 @@
+//===-- Transforms/IPO/InstrumentorConfigFile.h ---------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// Utilities for the Instrumentor JSON configuration file.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_TRANSFORMS_IPO_INSTRUMENTOR_CONFIGFILE_H
+#define LLVM_TRANSFORMS_IPO_INSTRUMENTOR_CONFIGFILE_H
+
+#include "llvm/ADT/StringRef.h"
+#include "llvm/Transforms/IPO/Instrumentor.h"
+
+namespace llvm {
+namespace instrumentor {
+
+/// Write the configuration in /p IConf to the file with path \p OutputFile.
+void writeConfigToJSON(InstrumentationConfig &IConf, StringRef OutputFile,
+                       LLVMContext &Ctx);
+
+/// Read the configuration from the file with path \p InputFile  into /p IConf.
+bool readConfigFromJSON(InstrumentationConfig &IConf, StringRef InputFile,
+                        LLVMContext &Ctx);
+
+} // end namespace instrumentor
+} // end namespace llvm
+
+#endif // LLVM_TRANSFORMS_IPO_INSTRUMENTOR_CONFIGFILE_H

llvm/lib/Passes/PassBuilder.cpp
@@ -231,6 +231,7 @@
 #include "llvm/Transforms/IPO/HotColdSplitting.h"
 #include "llvm/Transforms/IPO/IROutliner.h"
 #include "llvm/Transforms/IPO/InferFunctionAttrs.h"
+#include "llvm/Transforms/IPO/Instrumentor.h"
 #include "llvm/Transforms/IPO/Internalize.h"
 #include "llvm/Transforms/IPO/LoopExtractor.h"
 #include "llvm/Transforms/IPO/LowerTypeTests.h"

llvm/lib/Passes/PassBuilderPipelines.cpp
@@ -64,6 +64,7 @@
 #include "llvm/Transforms/IPO/IROutliner.h"
 #include "llvm/Transforms/IPO/InferFunctionAttrs.h"
 #include "llvm/Transforms/IPO/Inliner.h"
+#include "llvm/Transforms/IPO/Instrumentor.h"
 #include "llvm/Transforms/IPO/LowerTypeTests.h"
 #include "llvm/Transforms/IPO/MemProfContextDisambiguation.h"
 #include "llvm/Transforms/IPO/MergeFunctions.h"
@@ -220,6 +221,10 @@ static cl::opt<bool> EnableUnrollAndJam("enable-unroll-and-jam",
 static cl::opt<bool> EnableLoopFlatten("enable-loop-flatten", cl::init(false),
                                        cl::Hidden,
                                        cl::desc("Enable the LoopFlatten Pass"));
+
+static cl::opt<bool>
+    EnableInstrumentor("enable-instrumentor", cl::init(false), cl::Hidden,
+                       cl::desc("Enable the Instrumentor Pass"));
 
 static cl::opt<bool>
     EnableDFAJumpThreading("enable-dfa-jump-thread",
@@ -1641,6 +1646,10 @@ PassBuilder::buildModuleOptimizationPipeline(OptimizationLevel Level,
 
   invokeOptimizerLastEPCallbacks(MPM, Level, LTOPhase);
 
+  // Run the Instrumentor pass late.
+  if (EnableInstrumentor)
+    MPM.addPass(InstrumentorPass());
+
   // Split out cold code. Splitting is done late to avoid hiding context from
   // other optimizations and inadvertently regressing performance. The tradeoff
   // is that this has a higher code size cost than splitting early.
@@ -2418,6 +2427,9 @@ PassBuilder::buildO0DefaultPipeline(OptimizationLevel Level,
     MPM.addPass(AllocTokenPass());
 
   invokeOptimizerLastEPCallbacks(MPM, Level, Phase);
+
+  if (EnableInstrumentor)
+    MPM.addPass(InstrumentorPass());
 
   if (isLTOPreLink(Phase))
     addRequiredLTOPreLinkPasses(MPM);

llvm/lib/Passes/PassRegistry.def
@@ -102,6 +102,7 @@ MODULE_PASS("instrprof", InstrProfilingLoweringPass())
 MODULE_PASS("ctx-instr-lower", PGOCtxProfLoweringPass())
 MODULE_PASS("print<ctx-prof-analysis>", CtxProfAnalysisPrinterPass(errs()))
 MODULE_PASS("invalidate<all>", InvalidateAllAnalysesPass())
+MODULE_PASS("instrumentor", InstrumentorPass())
 MODULE_PASS("iroutliner", IROutlinerPass())
 MODULE_PASS("jmc-instrumenter", JMCInstrumenterPass())
 MODULE_PASS("lower-emutls", LowerEmuTLSPass())

llvm/lib/Transforms/IPO/CMakeLists.txt
@@ -27,6 +27,8 @@ add_llvm_component_library(LLVMipo
   IROutliner.cpp
   InferFunctionAttrs.cpp
   Inliner.cpp
+  Instrumentor.cpp
+  InstrumentorConfigFile.cpp
   Internalize.cpp
   LoopExtractor.cpp
   LowerTypeTests.cpp
@@ -62,6 +64,7 @@ add_llvm_component_library(LLVMipo
   BitReader
   BitWriter
   Core
+  Demangle
   FrontendOpenMP
   InstCombine
   IRReader

commit 65ad7a448e2d203cc7b6d2171aef7b95602f55a2

[llvm][Instrumentor] Fix memory leaks
Should fix memory leaks introduced in #138958

llvm/include/llvm/Transforms/IPO/Instrumentor.h
@@ -34,6 +34,7 @@
 
 #include <cstdint>
 #include <functional>
+#include <memory>
 #include <string>
 #include <tuple>
 
@@ -273,17 +274,15 @@ struct BaseConfigurationOption {
 
   /// Create a boolean option with \p Name name, \p Description description and
   /// \p DefaultValue as boolean default value.
-  static BaseConfigurationOption *createBoolOption(InstrumentationConfig &IC,
-                                                   StringRef Name,
-                                                   StringRef Description,
-                                                   bool DefaultValue);
+  static std::unique_ptr<BaseConfigurationOption>
+  createBoolOption(InstrumentationConfig &IC, StringRef Name,
+                   StringRef Description, bool DefaultValue);
 
   /// Create a string option with \p Name name, \p Description description and
   /// \p DefaultValue as string default value.
-  static BaseConfigurationOption *createStringOption(InstrumentationConfig &IC,
-                                                     StringRef Name,
-                                                     StringRef Description,
-                                                     StringRef DefaultValue);
+  static std::unique_ptr<BaseConfigurationOption>
+  createStringOption(InstrumentationConfig &IC, StringRef Name,
+                     StringRef Description, StringRef DefaultValue);
 
   /// Helper union that holds any possible option type.
   union ValueTy {
@@ -384,10 +383,10 @@ struct InstrumentationConfig {
   SmallVector<BaseConfigurationOption *> BaseConfigurationOptions;
 
   /// The base configuration options.
-  BaseConfigurationOption *RuntimePrefix;
-  BaseConfigurationOption *TargetRegex;
-  BaseConfigurationOption *HostEnabled;
-  BaseConfigurationOption *GPUEnabled;
+  std::unique_ptr<BaseConfigurationOption> RuntimePrefix;
+  std::unique_ptr<BaseConfigurationOption> TargetRegex;
+  std::unique_ptr<BaseConfigurationOption> HostEnabled;
+  std::unique_ptr<BaseConfigurationOption> GPUEnabled;
 
   /// The map registered instrumentation opportunities. The map is indexed by
   /// the instrumentation location kind and then by the opportunity name. Notice

llvm/include/llvm/Transforms/IPO/InstrumentorUtils.h
@@ -48,6 +48,12 @@ struct InstrumentorIRBuilderTy {
         I->replaceAllUsesWith(PoisonValue::get(I->getType()));
       I->eraseFromParent();
     }
+
+    // Delete the alloca lists that may have been allocated.
+    for (auto &KV : AllocaMap) {
+      if (KV.second)
+        delete KV.second;
+    }
   }
 
   /// Get a temporary alloca to communicate (large) values with the runtime.

llvm/lib/Transforms/IPO/Instrumentor.cpp
@@ -287,22 +287,26 @@ PreservedAnalyses InstrumentorPass::run(Module &M, ModuleAnalysisManager &MAM) {
   return PA;
 }
 
-BaseConfigurationOption *
+std::unique_ptr<BaseConfigurationOption>
 BaseConfigurationOption::createBoolOption(InstrumentationConfig &IConf,
                                           StringRef Name, StringRef Description,
                                           bool DefaultValue) {
-  auto *BCO = new BaseConfigurationOption(Name, Description, BOOLEAN);
+  auto BCO =
+      std::make_unique<BaseConfigurationOption>(Name, Description, BOOLEAN);
   BCO->setBool(DefaultValue);
-  IConf.addBaseChoice(BCO);
+  IConf.addBaseChoice(BCO.get());
   return BCO;
 }
 
-BaseConfigurationOption *BaseConfigurationOption::createStringOption(
-    InstrumentationConfig &IConf, StringRef Name, StringRef Description,
-    StringRef DefaultValue) {
-  auto *BCO = new BaseConfigurationOption(Name, Description, STRING);
+std::unique_ptr<BaseConfigurationOption>
+BaseConfigurationOption::createStringOption(InstrumentationConfig &IConf,
+                                            StringRef Name,
+                                            StringRef Description,
+                                            StringRef DefaultValue) {
+  auto BCO =
+      std::make_unique<BaseConfigurationOption>(Name, Description, STRING);
   BCO->setString(DefaultValue);
-  IConf.addBaseChoice(BCO);
+  IConf.addBaseChoice(BCO.get());
   return BCO;
 }
 