llvm/llvm-project

commit ad1a45b903b87f8e0c36a20f054d170ff2b5b2d5

[AArch64] Use GISel for optnone functions (#174746)
Currently, when SDAG is run on AArch64 and an `optnone` function is
encountered, the selector is chosen as FastISel. AArch64 makes use of
GlobalISel at O0 and this patch aims to align `optnone` with this
functionality.

A flag is exposed to enable this functionality for a given backend but,
as AArch64 is currently the only backend I could find using GlobalISel
at O0 this is the only one with it implemented. This flag is set when
the target supports GlobalISel & GlobalISel hasn't been forced by the
user, the target machine or by being at an optimisation level lower than
`EnableGlobalISelAtO`.

If this happens, the GlobalISel passes are included as shown in
`llvm/test/CodeGen/AArch64/O3-pipeline.ll` and skipped by IRTranslator
for functions not marked as `optnone`.

In updating the tests based on this functionality, I found some unused
check lines or run lines that mixed SDAG with GlobalISel pass names
which have been fixed.

---------

Co-authored-by: Matt Arsenault <arsenm2@gmail.com>

llvm/lib/Target/AArch64/GISel/AArch64CallLowering.cpp
@@ -19,6 +19,7 @@
 #include "AArch64RegisterInfo.h"
 #include "AArch64SMEAttributes.h"
 #include "AArch64Subtarget.h"
+#include "AArch64TargetMachine.h"
 #include "llvm/ADT/ArrayRef.h"
 #include "llvm/ADT/SmallVector.h"
 #include "llvm/Analysis/ObjCARCUtil.h"
@@ -577,6 +578,22 @@ static void handleMustTailForwardedRegisters(MachineIRBuilder &MIRBuilder,
 
 bool AArch64CallLowering::fallBackToDAGISel(const MachineFunction &MF) const {
   auto &F = MF.getFunction();
+  const auto &TM = static_cast<const AArch64TargetMachine &>(MF.getTarget());
+
+  const bool GlobalISelFlag =
+      getCGPassBuilderOption().EnableGlobalISelOption.value_or(false);
+
+  auto OptLevel = MF.getTarget().getOptLevel();
+  auto EnableGlobalISelAtO = TM.getEnableGlobalISelAtO();
+
+  // GlobalISel is currently only enabled when the opt level is less than or
+  // equal to EnableGlobalISelAt or it was explicitly enabled via the CLI. If we
+  // encounter this check, we know GlobalISel was enabled. If not by these two,
+  // it must have been used as part of the SDAG pipeline to use GlobalISel for
+  // optnone.
+  if (static_cast<unsigned>(OptLevel) > EnableGlobalISelAtO && !GlobalISelFlag)
+    return !F.hasOptNone();
+
   if (!EnableSVEGISel && (F.getReturnType()->isScalableTy() ||
                           llvm::any_of(F.args(), [](const Argument &A) {
                             return A.getType()->isScalableTy();

llvm/lib/CodeGen/GlobalISel/IRTranslator.cpp
@@ -4130,11 +4130,25 @@ static bool checkForMustTailInVarArgFn(bool IsVarArg, const BasicBlock &BB) {
 bool IRTranslator::runOnMachineFunction(MachineFunction &CurMF) {
   MF = &CurMF;
   const Function &F = MF->getFunction();
+  ORE = std::make_unique<OptimizationRemarkEmitter>(&F);
+  CLI = MF->getSubtarget().getCallLowering();
+
+  if (CLI->fallBackToDAGISel(*MF)) {
+    OptimizationRemarkMissed R("gisel-irtranslator", "GISelFailure",
+                               F.getSubprogram(), &F.getEntryBlock());
+    R << "unable to lower function: "
+      << ore::NV("Prototype", F.getFunctionType());
+
+    reportTranslationError(*MF, *ORE, R);
+    return false;
+  }
+
   GISelCSEAnalysisWrapper &Wrapper =
       getAnalysis<GISelCSEAnalysisWrapperPass>().getCSEWrapper();
   // Set the CSEConfig and run the analysis.
   GISelCSEInfo *CSEInfo = nullptr;
   TPC = &getAnalysis<TargetPassConfig>();
+
   bool EnableCSE = EnableCSEInIRTranslator.getNumOccurrences()
                        ? EnableCSEInIRTranslator
                        : TPC->isGISelCSEEnabled();
@@ -4157,7 +4171,6 @@ bool IRTranslator::runOnMachineFunction(MachineFunction &CurMF) {
   EntryBuilder->setMF(*MF);
   MRI = &MF->getRegInfo();
   DL = &F.getDataLayout();
-  ORE = std::make_unique<OptimizationRemarkEmitter>(&F);
   const TargetMachine &TM = MF->getTarget();
   TM.resetTargetOptions(F);
   EnableOpts = OptLevel != CodeGenOptLevel::None && !skipFunction(F);
@@ -4234,15 +4247,6 @@ bool IRTranslator::runOnMachineFunction(MachineFunction &CurMF) {
 
   // Make our arguments/constants entry block fallthrough to the IR entry block.
   EntryBB->addSuccessor(&getMBB(F.front()));
-
-  if (CLI->fallBackToDAGISel(*MF)) {
-    OptimizationRemarkMissed R("gisel-irtranslator", "GISelFailure",
-                               F.getSubprogram(), &F.getEntryBlock());
-    R << "unable to lower function: "
-      << ore::NV("Prototype", F.getFunctionType());
-    reportTranslationError(*MF, *ORE, R);
-    return false;
-  }
 
   // Lower the actual args into this basic block.
   SmallVector<ArrayRef<Register>, 8> VRegArgs;

llvm/lib/CodeGen/MachineDominators.cpp
@@ -99,6 +99,12 @@ MachineDominatorTreeWrapperPass::MachineDominatorTreeWrapperPass()
 char &llvm::MachineDominatorsID = MachineDominatorTreeWrapperPass::ID;
 
 bool MachineDominatorTreeWrapperPass::runOnMachineFunction(MachineFunction &F) {
+  if (F.empty()) {
+    assert(F.getProperties().hasFailedISel() &&
+           "Machine function should not be empty unless ISel failed.");
+    return false;
+  }
+
   DT = MachineDominatorTree(F);
   return false;
 }

llvm/lib/Target/AArch64/AArch64TargetMachine.cpp
@@ -387,12 +387,19 @@ AArch64TargetMachine::AArch64TargetMachine(const Target &T, const Triple &TT,
     // for the tiny code model, the maximum TLS size is 1MiB (< 16MiB)
     this->Options.TLSSize = 24;
 
-  // Enable GlobalISel at or below EnableGlobalISelAt0, unless this is
-  // MachO/CodeModel::Large, which GlobalISel does not support.
-  if (static_cast<int>(getOptLevel()) <= EnableGlobalISelAtO &&
+  const bool TargetSupportsGISel =
       TT.getArch() != Triple::aarch64_32 &&
       TT.getEnvironment() != Triple::GNUILP32 &&
-      !(getCodeModel() == CodeModel::Large && TT.isOSBinFormatMachO())) {
+      !(getCodeModel() == CodeModel::Large && TT.isOSBinFormatMachO());
+
+  const bool GlobalISelFlag =
+      getCGPassBuilderOption().EnableGlobalISelOption.value_or(false);
+
+  // Enable GlobalISel at or below EnableGlobalISelAt0, unless this is
+  // MachO/CodeModel::Large, which GlobalISel does not support.
+  if (TargetSupportsGISel &&
+      (static_cast<int>(getOptLevel()) <= EnableGlobalISelAtO ||
+       (!GlobalISelFlag && !Options.EnableGlobalISel))) {
     setGlobalISel(true);
     setGlobalISelAbort(GlobalISelAbortMode::Disable);
   }
@@ -409,6 +416,10 @@ AArch64TargetMachine::AArch64TargetMachine(const Target &T, const Triple &TT,
   // AArch64 supports fixing up the DWARF unwind information.
   if (!getMCAsmInfo()->usesWindowsCFI())
     setCFIFixup(true);
+}
+
+unsigned AArch64TargetMachine::getEnableGlobalISelAtO() const {
+  return EnableGlobalISelAtO;
 }
 
 AArch64TargetMachine::~AArch64TargetMachine() = default;
@@ -567,6 +578,9 @@ public:
   bool addRegAssignAndRewriteOptimized() override;
 
   std::unique_ptr<CSEConfigBase> getCSEConfig() const override;
+
+private:
+  bool isGlobalISelOptNone() const;
 };
 
 } // end anonymous namespace
@@ -596,6 +610,21 @@ TargetPassConfig *AArch64TargetMachine::createPassConfig(PassManagerBase &PM) {
 
 std::unique_ptr<CSEConfigBase> AArch64PassConfig::getCSEConfig() const {
   return getStandardCSEConfigForOpt(TM->getOptLevel());
+}
+
+// This function checks whether the opt level is explictly set to none,
+// or whether GlobalISel was enabled due to SDAG encountering an optnone
+// function. If the opt level is greater than the level we automatically enable
+// globalisel at, and it wasn't enabled via CLI, we know that it must be because
+// of an optnone function.
+bool AArch64PassConfig::isGlobalISelOptNone() const {
+  const bool GlobalISelFlag =
+      getCGPassBuilderOption().EnableGlobalISelOption.value_or(false);
+
+  return getOptLevel() == CodeGenOptLevel::None ||
+         (static_cast<unsigned>(getOptLevel()) >
+              getAArch64TargetMachine().getEnableGlobalISelAtO() &&
+          !GlobalISelFlag);
 }
 
 void AArch64PassConfig::addIRPasses() {
@@ -733,7 +762,7 @@ bool AArch64PassConfig::addIRTranslator() {
 }
 
 void AArch64PassConfig::addPreLegalizeMachineIR() {
-  if (getOptLevel() == CodeGenOptLevel::None) {
+  if (isGlobalISelOptNone()) {
     addPass(createAArch64O0PreLegalizerCombiner());
     addPass(new Localizer());
   } else {
@@ -750,9 +779,8 @@ bool AArch64PassConfig::addLegalizeMachineIR() {
 }
 
 void AArch64PassConfig::addPreRegBankSelect() {
-  bool IsOptNone = getOptLevel() == CodeGenOptLevel::None;
-  if (!IsOptNone) {
-    addPass(createAArch64PostLegalizerCombiner(IsOptNone));
+  if (!isGlobalISelOptNone()) {
+    addPass(createAArch64PostLegalizerCombiner(isGlobalISelOptNone()));
     if (EnableGISelLoadStoreOptPostLegal)
       addPass(new LoadStoreOpt());
   }
@@ -766,7 +794,7 @@ bool AArch64PassConfig::addRegBankSelect() {
 
 bool AArch64PassConfig::addGlobalInstructionSelect() {
   addPass(new InstructionSelect(getOptLevel()));
-  if (getOptLevel() != CodeGenOptLevel::None)
+  if (!isGlobalISelOptNone())
     addPass(createAArch64PostSelectOptimize());
   return false;
 }

llvm/lib/Target/AArch64/AArch64TargetMachine.h
@@ -82,6 +82,9 @@ public:
   /// Returns true if the new SME ABI lowering should be used.
   bool useNewSMEABILowering() const { return UseNewSMEABILowering; }
 
+  /// Returns the optimisation level that enables GlobalISel.
+  unsigned getEnableGlobalISelAtO() const;
+
 private:
   bool isLittle;
   bool UseNewSMEABILowering;

llvm/test/CodeGen/AArch64/GlobalISel/gisel-commandline-option.ll
@@ -36,16 +36,16 @@
 ; RUN: llc -mtriple=aarch64-- -debug-pass=Structure %s -o /dev/null 2>&1 \
 ; RUN:   --debugify-and-strip-all-safe=0 \
 ; RUN:   -verify-machineinstrs=0 -O1 -aarch64-enable-global-isel-at-O=0 \
-; RUN:   | FileCheck %s --check-prefix DISABLED
+; RUN:   | FileCheck %s --check-prefix NOT-ENABLED
 
 ; RUN: llc -mtriple=aarch64-- -debug-pass=Structure %s -o /dev/null 2>&1 \
 ; RUN:   --debugify-and-strip-all-safe=0 \
 ; RUN:   -verify-machineinstrs=0 -aarch64-enable-global-isel-at-O=-1 \
-; RUN:   | FileCheck %s --check-prefix DISABLED
+; RUN:   | FileCheck %s --check-prefix NOT-ENABLED
 
 ; RUN: llc -mtriple=aarch64-- -debug-pass=Structure %s -o /dev/null 2>&1 \
 ; RUN:   --debugify-and-strip-all-safe=0 \
-; RUN:   -verify-machineinstrs=0 | FileCheck %s --check-prefix DISABLED
+; RUN:   -verify-machineinstrs=0 | FileCheck %s --check-prefix NOT-ENABLED
 
 ; RUN: llc -mtriple=aarch64-- -fast-isel=0 -global-isel=false \
 ; RUN:   --debugify-and-strip-all-safe=0 \
@@ -81,6 +81,12 @@
 
 ; FALLBACK:       AArch64 Instruction Selection
 ; NOFALLBACK-NOT: AArch64 Instruction Selection
+
+; Should be included due to the potential for SDAG to use GlobalISel for optnone functions
+; NOT-ENABLED: IRTranslator
+
+; NOT-ENABLED: AArch64 Instruction Selection
+; NOT-ENABLED: Finalize ISel and expand pseudo-instructions
 
 ; DISABLED-NOT: IRTranslator
 

llvm/test/CodeGen/AArch64/GlobalISel/optnone-llc.ll
@@ -1,18 +1,13 @@
-; RUN: llc -O1 -debug %s -o /dev/null 2>&1 | FileCheck %s --check-prefix=LLC-Ox
-; RUN: llc -O2 -debug %s -o /dev/null 2>&1 | FileCheck %s --check-prefix=LLC-Ox
-; RUN: llc -O3 -debug %s -o /dev/null 2>&1 | FileCheck %s --check-prefix=LLC-Ox
-; RUN: llc -misched-postra -debug %s -o /dev/null 2>&1 | FileCheck %s --check-prefix=LLC-MORE
-; RUN: llc -O1 -debug-only=isel %s -o /dev/null 2>&1 | FileCheck %s --check-prefix=FAST
-; RUN: llc -O1 -debug-only=isel -fast-isel=false %s -o /dev/null 2>&1 | FileCheck %s --check-prefix=NOFAST
+; NOTE: Do not autogenerate
+; RUN: llc -O1 -debug -global-isel -mtriple=aarch64-unknown-linux-gnu -filetype=null %s 2>&1 | FileCheck %s --check-prefix=LLC-Ox
+; RUN: llc -O2 -debug -global-isel -mtriple=aarch64-unknown-linux-gnu -filetype=null %s 2>&1 | FileCheck %s --check-prefix=LLC-Ox
+; RUN: llc -O3 -debug -global-isel -mtriple=aarch64-unknown-linux-gnu -filetype=null %s 2>&1 | FileCheck %s --check-prefix=LLC-Ox
 
-; REQUIRES: asserts, default_triple
-; AArch64 uses GlobalISel for optnone functions meaning the output from 'isel' will be empty as it will not be run.
-; UNSUPPORTED: target=nvptx{{.*}}, target=aarch64{{.*}}
+; REQUIRES: asserts
 
 ; This test verifies that we don't run Machine Function optimizations
-; on optnone functions, and that we can turn off FastISel.
+; on optnone functions.
 
-; Function Attrs: noinline optnone
 define i32 @_Z3fooi(i32 %x) #0 {
 entry:
   %x.addr = alloca i32, align 4
@@ -35,9 +30,6 @@ while.end:                                        ; preds = %while.cond
 
 attributes #0 = { optnone noinline }
 
-; Nothing that runs at -O0 gets skipped.
-; LLC-O0-NOT: Skipping pass
-
 ; Machine Function passes run at -O1 and higher.
 ; LLC-Ox-DAG: Skipping pass 'Branch Probability Basic Block Placement'
 ; LLC-Ox-DAG: Skipping pass 'CodeGen Prepare'
@@ -53,10 +45,3 @@ attributes #0 = { optnone noinline }
 ; LLC-Ox-DAG: Skipping pass 'Post{{.*}}RA{{.*}}{{[Ss]}}cheduler'
 ; LLC-Ox-DAG: Skipping pass 'Remove dead machine instructions'
 ; LLC-Ox-DAG: Skipping pass 'Tail Duplication'
-
-; Alternate post-RA scheduler.
-; LLC-MORE: Skipping pass 'PostRA Machine Instruction Scheduler'
-
-; Selectively disable FastISel for optnone functions.
-; FAST:   FastISel is enabled
-; NOFAST: FastISel is disabled

llvm/test/CodeGen/AArch64/GlobalISel/optnone-mixed.ll
@@ -1,0 +1,28 @@
+; NOTE: Do not autogenerate
+; RUN: llc -mtriple=aarch64-linux-gnu -O2 -verify-machineinstrs -debug %s -o - 2>&1 | FileCheck %s --check-prefix=GISEL
+; REQUIRES: asserts
+
+; Check both an optnone function and a non-optnone function to ensure that only
+; the optnone functions is routed through GlobalISel
+
+define i32 @optnone_fn(i32 %a) #0 {
+entry:
+  %add = add nsw i32 %a, 1
+  ret i32 %add
+}
+
+define i32 @normal_fn(i32 %a) {
+entry:
+  %mul = mul nsw i32 %a, 2
+  ret i32 %mul
+}
+
+; There are two functions being checked here, one which is optnone and the other which isn't.
+; As we are running at O2, we default to using SDAG for selection. The optnone function however,
+; will make use of GlobalISel. The top check, checks that the GlobalISel pipeline is run on the 
+; optnone function, and the second line is checking that SDAG was run on the normal function.
+
+; GISEL: Skipping pass 'IRTranslator' on function optnone_fn
+; GISEL: Creating new node:
+
+attributes #0 = { noinline nounwind optnone uwtable }

commit 233dd6de755b63f5c54b9ecb3567b6dba603eb2b

[AArch64] Fall back to SelectionDAG ISel if GISel is not feasible.
A function like the one below should still result in an SME prologue
to set up ZA.

  void bar() __arm_inout("za");

  __attribute__((optnone)) __arm_new("za")
  void foo() {
      bar();
  }

https://godbolt.org/z/aEcoKea4b

This worked in LLVM 22, but got broken by #174746.

llvm/lib/Target/AArch64/GISel/AArch64CallLowering.cpp
@@ -582,20 +582,6 @@ bool AArch64CallLowering::fallBackToDAGISel(const MachineFunction &MF) const {
   auto &F = MF.getFunction();
   const auto &TM = static_cast<const AArch64TargetMachine &>(MF.getTarget());
 
-  const bool GlobalISelFlag =
-      getCGPassBuilderOption().EnableGlobalISelOption.value_or(false);
-
-  auto OptLevel = MF.getTarget().getOptLevel();
-  auto EnableGlobalISelAtO = TM.getEnableGlobalISelAtO();
-
-  // GlobalISel is currently only enabled when the opt level is less than or
-  // equal to EnableGlobalISelAt or it was explicitly enabled via the CLI. If we
-  // encounter this check, we know GlobalISel was enabled. If not by these two,
-  // it must have been used as part of the SDAG pipeline to use GlobalISel for
-  // optnone.
-  if (static_cast<unsigned>(OptLevel) > EnableGlobalISelAtO && !GlobalISelFlag)
-    return !F.hasOptNone();
-
   if (!EnableSVEGISel && (F.getReturnType()->isScalableTy() ||
                           llvm::any_of(F.args(), [](const Argument &A) {
                             return A.getType()->isScalableTy();
@@ -613,7 +599,14 @@ bool AArch64CallLowering::fallBackToDAGISel(const MachineFunction &MF) const {
       Attrs.hasStreamingCompatibleInterface())
     return true;
 
-  return false;
+  // Enable GlobalISel when feasible and when:
+  // * Forced by option to enable gisel
+  // * The current opt-level is supported by EnableGlobalISelAtO
+  // * The function is an optnone function.
+  auto OptLevel = MF.getTarget().getOptLevel();
+  return !(getCGPassBuilderOption().EnableGlobalISelOption.value_or(false) ||
+           static_cast<unsigned>(OptLevel) <= TM.getEnableGlobalISelAtO() ||
+           F.hasOptNone());
 }
 
 void AArch64CallLowering::saveVarArgRegisters(

llvm/test/CodeGen/AArch64/GlobalISel/optnone-sme.ll
@@ -11,7 +11,18 @@ define void @foo() noinline optnone "aarch64_new_za" "target-features"="+sme" {
 ; CHECK-NEXT:    str x30, [sp, #-16]! // 8-byte Folded Spill
 ; CHECK-NEXT:    .cfi_def_cfa_offset 16
 ; CHECK-NEXT:    .cfi_offset w30, -16
+; CHECK-NEXT:    mrs x8, TPIDR2_EL0
+; CHECK-NEXT:    cbnz x8, .LBB0_1
+; CHECK-NEXT:    b .LBB0_2
+; CHECK-NEXT:  .LBB0_1:
+; CHECK-NEXT:    bl __arm_tpidr2_save
+; CHECK-NEXT:    msr TPIDR2_EL0, xzr
+; CHECK-NEXT:    zero {za}
+; CHECK-NEXT:    b .LBB0_2
+; CHECK-NEXT:  .LBB0_2:
+; CHECK-NEXT:    smstart za
 ; CHECK-NEXT:    bl bar
+; CHECK-NEXT:    smstop za
 ; CHECK-NEXT:    ldr x30, [sp], #16 // 8-byte Folded Reload
 ; CHECK-NEXT:    ret
   call void @bar()