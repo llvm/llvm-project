954fb318700d56a59cbeba036f339aecd6e9fed2
Anonmiraj <ezzibrahimx@gmail.com>
2026-05-07 08:14:53 +0300

    fix nits

--- libc/shared/math/lgammaf16.h
+++ libc/shared/math/lgammaf16.h
@@ -9,6 +9,8 @@
 #ifndef LLVM_LIBC_SHARED_MATH_LGAMMAF16_H
 #define LLVM_LIBC_SHARED_MATH_LGAMMAF16_H
 
+#include "include/llvm-libc-macros/float16-macros.h"
+
 #ifdef LIBC_TYPES_HAS_FLOAT16
 
 #include "shared/libc_common.h"
