5fb7c55b2aa636c622b1e776928516275d70779f
Liao Chunyu <chunyu@iscas.ac.cn>
2026-05-07 03:39:35 +0000

    delete unknown and aligned

--- llvm/test/MC/RISCV/rvv/zvfqwdota8f.s
+++ llvm/test/MC/RISCV/rvv/zvfqwdota8f.s
@@ -5,36 +5,29 @@
 # RUN: llvm-mc -triple=riscv32 -filetype=obj --mattr=+v --mattr=+experimental-zvfqwdota8f %s \
 # RUN:        | llvm-objdump -d --mattr=+v --mattr=+experimental-zvfqwdota8f --no-print-imm-hex  - \
 # RUN:        | FileCheck %s --check-prefix=CHECK-INST
-# RUN: llvm-mc -triple=riscv32 -filetype=obj --mattr=+v --mattr=+experimental-zvfqwdota8f %s \
-# RUN:        | llvm-objdump -d - | FileCheck %s --check-prefix=CHECK-UNKNOWN
 
 vsetvli a2, a0, e8alt, m1, ta, ma
 # CHECK-INST: vsetvli a2, a0, e8alt, m1, ta, ma
 # CHECK-ENCODING: [0x57,0x76,0x05,0x1c]
 # CHECK-ERROR: instruction requires the following: 'V' (Vector Extension for Application Processors), 'Zve32x' (Vector Extensions for Embedded Processors){{$}}
-# CHECK-UNKNOWN: 1c057657 <unknown>
 
 vfqwdota.vv v10, v9, v8
 # CHECK-INST: vfqwdota.vv v10, v9, v8
 # CHECK-ENCODING: [0x77,0x15,0x94,0x9a]
 # CHECK-ERROR: instruction requires the following: 'Zvfqwdota8f' (OCP FP8 Dot-Product)
-# CHECK-UNKNOWN: 9a941577 <unknown>
 
 vfqwdota.vv v10, v9, v8, v0.t
 # CHECK-INST: vfqwdota.vv v10, v9, v8, v0.t
 # CHECK-ENCODING: [0x77,0x15,0x94,0x98]
 # CHECK-ERROR: instruction requires the following: 'Zvfqwdota8f' (OCP FP8 Dot-Product)
-# CHECK-UNKNOWN: 98941577 <unknown>
 
 vfqwdota.alt.vv v10, v9, v8
 # CHECK-INST: vfqwdota.alt.vv v10, v9, v8
 # CHECK-ENCODING: [0x77,0x15,0x94,0x9e]
 # CHECK-ERROR: instruction requires the following: 'Zvfqwdota8f' (OCP FP8 Dot-Product)
-# CHECK-UNKNOWN: 9e941577 <unknown>
 
 vfqwdota.alt.vv v10, v9, v8, v0.t
 # CHECK-INST: vfqwdota.alt.vv v10, v9, v8, v0.t
 # CHECK-ENCODING: [0x77,0x15,0x94,0x9c]
 # CHECK-ERROR: instruction requires the following: 'Zvfqwdota8f' (OCP FP8 Dot-Product)
-# CHECK-UNKNOWN: 9c941577 <unknown>
 
