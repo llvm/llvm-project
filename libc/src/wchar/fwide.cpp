2ae2119e1f168dd2c0040796b8f2d160f28dca86
Michael Jones <michaelrj@google.com>
2026-05-07 17:53:24 +0000

    fix header formatting, add build setup.

--- libc/src/wchar/fwide.cpp
+++ libc/src/wchar/fwide.cpp
@@ -1,9 +1,15 @@
-//===-- Implementation of fwide -------------------------------------------===//
+//===----------------------------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file contains the implementation of the fwide function, which sets and
+/// gets the orientation of a stream.
+///
 //===----------------------------------------------------------------------===//
 
 #include "src/wchar/fwide.h"
--- libc/src/wchar/fwide.h
+++ libc/src/wchar/fwide.h
@@ -1,9 +1,15 @@
-//===-- Implementation header for fwide -----------------------------------===//
+//===----------------------------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file contains the prototype of the fwide function, which sets and
+/// gets the orientation of a stream.
+///
 //===----------------------------------------------------------------------===//
 
 #ifndef LLVM_LIBC_SRC_WCHAR_FWIDE_H
