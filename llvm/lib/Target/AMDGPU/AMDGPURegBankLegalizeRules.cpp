52f09ef1cc0ffa2ff957024e8d0e3f95466b7528
Vang Thao <Vang.Thao@amd.com>
2026-05-07 12:36:16 -0400

    AMDGPU/GlobalISel: RegBankLegalize rules for cvt fp8 e5m3 intrinsics

--- llvm/lib/Target/AMDGPU/AMDGPURegBankLegalizeRules.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPURegBankLegalizeRules.cpp
@@ -1749,13 +1749,15 @@ RegBankLegalizeRules::RegBankLegalizeRules(const GCNSubtarget &_ST,
       .Div(S32, {{Vgpr32}, {IntrId, Vgpr32, Vgpr32}});
 
   addRulesForIOpcs({amdgcn_cvt_sr_bf8_f32, amdgcn_cvt_sr_fp8_f32,
-                    amdgcn_cvt_pk_bf8_f32, amdgcn_cvt_pk_fp8_f32},
+                    amdgcn_cvt_sr_fp8_f32_e5m3, amdgcn_cvt_pk_bf8_f32,
+                    amdgcn_cvt_pk_fp8_f32, amdgcn_cvt_pk_fp8_f32_e5m3},
                    Standard)
       .Uni(S32, {{UniInVgprS32}, {IntrId, Vgpr32, Vgpr32, Vgpr32}})
       .Div(S32, {{Vgpr32}, {IntrId, Vgpr32, Vgpr32, Vgpr32}});
 
-  addRulesForIOpcs(
-      {amdgcn_cvt_off_f32_i4, amdgcn_cvt_f32_bf8, amdgcn_cvt_f32_fp8}, Standard)
+  addRulesForIOpcs({amdgcn_cvt_off_f32_i4, amdgcn_cvt_f32_bf8,
+                    amdgcn_cvt_f32_fp8, amdgcn_cvt_f32_fp8_e5m3},
+                   Standard)
       .Uni(S32, {{UniInVgprS32}, {IntrId, Vgpr32}})
       .Div(S32, {{Vgpr32}, {IntrId, Vgpr32}});
 
