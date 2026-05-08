7ac31adce24ed050d95a09427a28b3cd22b9958b
Abhina Sreeskantharajan <Abhina.Sreeskantharajan@ibm.com>
2026-05-08 12:16:13 -0400

    This patch enables the fexec-charset option to control the execution charset of string literals. It sets the default internal charset, system charset, and execution charset for z/OS and UTF-8 for all other platforms.

---
+++ clang/lib/Lex/TextEncodingConfig.cpp
@@ -0,0 +1,45 @@
+//===--- TextEncodingConfig.cpp -------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "clang/Lex/TextEncodingConfig.h"
+#include "clang/Basic/DiagnosticDriver.h"
+
+using namespace llvm;
+
+llvm::TextEncodingConverter *
+TextEncodingConfig::getConverter(ConversionAction Action) const {
+  switch (Action) {
+  case CA_ToExecEncoding:
+    return ToExecEncodingConverter;
+  default:
+    return nullptr;
+  }
+}
+
+std::error_code
+TextEncodingConfig::setConvertersFromOptions(TextEncodingConfig &TEC,
+                                             const clang::LangOptions &Opts) {
+  using namespace llvm;
+
+  const char *UTF8 = "UTF-8";
+  TEC.ExecEncoding =
+      Opts.ExecEncoding.empty() ? UTF8 : Opts.ExecEncoding.c_str();
+
+  // Create converter between internal and exec encoding specified
+  // in fexec-charset option.
+  if (TEC.ExecEncoding == UTF8)
+    return std::error_code();
+  ErrorOr<TextEncodingConverter> ErrorOrConverter =
+      llvm::TextEncodingConverter::create(UTF8, TEC.ExecEncoding);
+  if (ErrorOrConverter)
+    TEC.ToExecEncodingConverter =
+        new TextEncodingConverter(std::move(*ErrorOrConverter));
+  else
+    return ErrorOrConverter.getError();
+  return std::error_code();
+}
