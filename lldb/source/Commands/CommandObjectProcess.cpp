cbbdfefeca07ce854b282cbe725ee92af6783d7c
David Spickett <david.spickett@arm.com>
2026-05-08 16:03:09 +0000

    [lldb] Add specific error message for "process plugin" with no plugin loaded
    Fixes #196535.
    
    The error was:
    > command is not implemented
    
    Which is incorrect. It is now:
    > no process plugin commands are currently registered
    
    Which is not very helpful either but it's not wrong at least.
    
    I don't think it's worth expanding it given how few people
    will ever see this error message anyway.

--- lldb/source/Commands/CommandObjectProcess.cpp
+++ lldb/source/Commands/CommandObjectProcess.cpp
@@ -952,6 +952,10 @@ public:
       return process->GetPluginCommandObject();
     return nullptr;
   }
+
+  llvm::StringRef GetUnsupportedError() override {
+    return "no process plugin commands are currently registered";
+  }
 };
 
 // CommandObjectProcessLoad
