c5fcc22d40c70f4ff1ec514230406ba37313fb7a
Jeremy Kun <jkun@google.com>
2026-05-08 12:03:13 -0700

    fix one more check

--- clang/test/CIR/mlprint.c
+++ clang/test/CIR/mlprint.c
@@ -8,7 +8,7 @@ int foo(void) {
 
 // CIR: IR Dump After CIRCanonicalize: cir-canonicalize
 // CIR:  cir.func{{.*}} @foo() -> !s32i
-// LLVM: IR Dump After cir::direct::ConvertCIRToLLVMPass (cir-flat-to-llvm)
+// LLVM: IR Dump After cir::direct::ConvertCIRToLLVMPass: cir-flat-to-llvm
 // LLVM: llvm.func @foo() -> i32
 // LLVM: IR Dump After
 // LLVM: define{{.*}} i32 @foo()
