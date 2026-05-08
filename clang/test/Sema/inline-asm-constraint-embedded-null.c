863282b7eb1bb82d42ccf6cbf95fe91ef5c76352
Iris Shi <0.0@owo.li>
2026-05-08 17:01:45 +0800

    ...

--- clang/test/Sema/inline-asm-constraint-embedded-null.c
+++ clang/test/Sema/inline-asm-constraint-embedded-null.c
@@ -3,14 +3,14 @@
 // Regression test for issue173900.
 
 void test_input(void) {
-  __asm__("" : : "f\0001"(0.0)); // expected-error {{input constraint contains embedded an null character}}
+  __asm__("" : : "f\0001"(0.0)); // expected-error {{input constraint contains an embedded null character}}
 }
 
 void test_output(void) {
   double x;
-  __asm__("" : "=r\0"(x)); // expected-error {{output constraint contains embedded an null character}}
+  __asm__("" : "=r\0"(x)); // expected-error {{output constraint contains an embedded null character}}
 }
 
 void test_clobber(void) {
-  __asm__("" : : : "rax\0"); // expected-error {{clobber contains embedded an null character}}
+  __asm__("" : : : "rax\0"); // expected-error {{clobber contains an embedded null character}}
 }
