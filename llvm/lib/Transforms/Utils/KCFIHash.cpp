4221477ec2409ccdcb2d133bf211de40e48135b8
Fangrui Song <i@maskray.me>
2026-05-09 23:58:08 -0700

    Move KCFI type ID hash helpers out of LLVMSupport
    PR #167254 inappropriately introduced llvm/Support/Hash.{h,cpp} for the
    KCFI helpers. The name is misleading - it has nothing to do with the
    generic hashing facility in llvm/ADT/Hashing.h. KCFI is a CodeGen/IR
    feature that does not belong in the foundational LLVMSupport.
    
    Move the files to llvm/lib/Transforms/Utils/KCFIHash.cpp, alongside
    setKCFIType, which is the only existing KCFI helper in TransformUtils.

--- llvm/lib/Transforms/Utils/KCFIHash.cpp
+++ llvm/lib/Transforms/Utils/KCFIHash.cpp
@@ -1,16 +1,12 @@
-//===- Hash.cpp - Hash functions ---------------------------------------===//
+//===----------------------------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
-//
-// This file implements hash functions.
-//
-//===----------------------------------------------------------------------===//
 
-#include "llvm/Support/Hash.h"
+#include "llvm/Transforms/Utils/KCFIHash.h"
 #include "llvm/Support/ErrorHandling.h"
 #include "llvm/Support/xxhash.h"
 
