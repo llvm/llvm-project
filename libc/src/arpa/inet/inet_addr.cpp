0c4527df822394971b0f53d70365638c81eb6347
Pavel Labath <pavel@labath.sk>
2026-05-11 13:04:08 +0000

    format

--- libc/src/arpa/inet/inet_addr.cpp
+++ libc/src/arpa/inet/inet_addr.cpp
@@ -8,8 +8,8 @@
 
 #include "src/arpa/inet/inet_addr.h"
 #include "include/llvm-libc-macros/netinet-in-macros.h"
-#include "include/llvm-libc-types/struct_in_addr.h"
 #include "include/llvm-libc-types/in_addr_t.h"
+#include "include/llvm-libc-types/struct_in_addr.h"
 #include "src/__support/common.h"
 #include "src/arpa/inet/inet_aton.h"
 
