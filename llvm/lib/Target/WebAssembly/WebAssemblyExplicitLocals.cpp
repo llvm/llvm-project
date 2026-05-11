7cf2aa7dc41ae9d14b300d0c189b4b38a2bcfed3
Sy Brand <sy.brand@fastly.com>
2026-05-11 09:50:40 +0100

    fmt

--- llvm/lib/Target/WebAssembly/WebAssemblyExplicitLocals.cpp
+++ llvm/lib/Target/WebAssembly/WebAssemblyExplicitLocals.cpp
@@ -366,16 +366,14 @@ bool WebAssemblyExplicitLocals::runOnMachineFunction(MachineFunction &MF) {
           const TargetRegisterClass *RC = MRI.getRegClass(OldReg);
           Register NewReg = MRI.createVirtualRegister(RC);
           auto InsertPt = std::next(MI.getIterator());
-          // When libcalls are emitted for thread context, the frame base vreg has
-          // an implicit use in the DW_AT_frame_base debug info, so we should not
-          // remove it.
+          // When libcalls are emitted for thread context, the frame base vreg
+          // has an implicit use in the DW_AT_frame_base debug info, so we
+          // should not remove it.
           bool NeedsRegForDebug =
-              MFI.isFrameBaseVirtual() &&
-              OldReg == MFI.getFrameBaseVreg() &&
+              MFI.isFrameBaseVirtual() && OldReg == MFI.getFrameBaseVreg() &&
               MF.getFunction().getSubprogram() &&
               MF.getSubtarget<WebAssemblySubtarget>().hasLibcallThreadContext();
-          if (UseEmpty[OldReg.virtRegIndex()] &&
-              !NeedsRegForDebug) {
+          if (UseEmpty[OldReg.virtRegIndex()] && !NeedsRegForDebug) {
             unsigned Opc = getDropOpcode(RC);
             MachineInstr *Drop =
                 BuildMI(MBB, InsertPt, MI.getDebugLoc(), TII->get(Opc))
