c09a903b900a04f388f94c67420b1569c10f3613
Alexey Samsonov <vonosmas@gmail.com>
2026-05-11 19:33:19 +0000

    [libc] Implement pthread_getthreadid_np and pthread_getunique_np.

---
+++ libc/include/llvm-libc-types/pthread_id_np_t.h
@@ -0,0 +1,16 @@
+//===-- Definition of pthread_id_np_t type --------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_LIBC_TYPES_PTHREAD_ID_NP_T_H
+#define LLVM_LIBC_TYPES_PTHREAD_ID_NP_T_H
+
+#include "../llvm-libc-macros/stdint-macros.h"
+
+typedef uintptr_t pthread_id_np_t;
+
+#endif // LLVM_LIBC_TYPES_PTHREAD_ID_NP_T_H
---
+++ libc/src/pthread/pthread_getunique_np.cpp
@@ -0,0 +1,29 @@
+//===-- Implementation of the pthread_getunique_np function ---------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "pthread_getunique_np.h"
+
+#include "src/__support/common.h"
+#include "src/__support/macros/config.h"
+#include "src/__support/macros/null_check.h"
+
+#include <pthread.h>
+
+namespace LIBC_NAMESPACE_DECL {
+
+LLVM_LIBC_FUNCTION(int, pthread_getunique_np,
+                   (pthread_t * thread, pthread_id_np_t *id)) {
+  LIBC_CRASH_ON_NULLPTR(id);
+  // We assume that unique thread ID is an integer value of a pointer to TCB.
+  *id = (thread == nullptr)
+            ? 0
+            : reinterpret_cast<pthread_id_np_t>(thread->__attrib);
+  return 0;
+}
+
+} // namespace LIBC_NAMESPACE_DECL
