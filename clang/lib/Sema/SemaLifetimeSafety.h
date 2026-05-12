9824c91d367a193035c4f47a840f80784670b97d
Utkarsh Saxena <usx@google.com>
2026-05-12 19:15:36 +0000

    renam to run lifetime sfety

--- clang/lib/Sema/SemaLifetimeSafety.h
+++ clang/lib/Sema/SemaLifetimeSafety.h
@@ -23,7 +23,7 @@
 namespace clang::lifetimes {
 
 inline bool IsLifetimeSafetyEnabled(Sema &S, const Decl *D) {
-  if (S.getLangOpts().EnableLifetimeSafety)
+  if (S.getLangOpts().RunLifetimeSafety)
     return true;
   DiagnosticsEngine &Diags = S.getDiagnostics();
   constexpr unsigned DiagIDs[] = {
