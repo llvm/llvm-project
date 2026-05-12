bf6161577c52c14f6d5b7f71629c52b2444bd7a6
Igor Kirillov <igor.kirillov@arm.com>
2026-05-12 09:56:49 +0100

    Address nit comment

--- llvm/lib/Transforms/Utils/LoopVersioning.cpp
+++ llvm/lib/Transforms/Utils/LoopVersioning.cpp
@@ -62,9 +62,9 @@ void LoopVersioning::versionLoop(
   const auto &RtPtrChecking = *LAI.getRuntimePointerChecking();
 
   SCEVExpander Exp2(*RtPtrChecking.getSE(), "induction");
-  MemRuntimeCheck = addRuntimeChecks(RuntimeCheckBB->getTerminator(),
-                                     VersionedLoop, AliasChecks, Exp2)
-                        .first;
+  std::pair<Value *, bool> CheckRes = addRuntimeChecks(
+      RuntimeCheckBB->getTerminator(), VersionedLoop, AliasChecks, Exp2);
+  MemRuntimeCheck = CheckRes.first;
 
   SCEVExpander Exp(*SE, "scev.check");
   SCEVRuntimeCheck =
