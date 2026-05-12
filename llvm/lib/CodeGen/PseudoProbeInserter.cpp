0860f8f777a30058ecc0d92bbdb8f23b8f7ead55
Henry Jiang <henry_jiang2@apple.com>
2026-05-12 10:10:54 -0700

    Preserve pseudoprobe for tailcall pseudo instrs

--- llvm/lib/CodeGen/PseudoProbeInserter.cpp
+++ llvm/lib/CodeGen/PseudoProbeInserter.cpp
@@ -54,7 +54,9 @@ public:
     for (MachineBasicBlock &MBB : MF) {
       MachineInstr *FirstInstr = nullptr;
       for (MachineInstr &MI : MBB) {
-        if (!MI.isPseudo())
+        // Pseudo instructions like TCRETURNbi results in a branch instruction
+        // and the call probe for that tail call should be preserved.
+        if (!MI.isPseudo() || MI.isCall())
           FirstInstr = &MI;
         if (MI.isCall()) {
           if (DILocation *DL = MI.getDebugLoc()) {
