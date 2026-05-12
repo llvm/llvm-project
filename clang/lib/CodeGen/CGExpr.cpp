dca700f71cfb258c5db325b113581723b11ea305
Ammarguellat <zahira.ammarguellat@intel.com>
2026-05-12 14:24:09 -0700

    Remove dup comment

--- clang/lib/CodeGen/CGExpr.cpp
+++ clang/lib/CodeGen/CGExpr.cpp
@@ -3834,7 +3834,6 @@ LValue CodeGenFunction::EmitDeclRefLValue(const DeclRefExpr *E) {
       if (CapturedStmtInfo &&
           CapturedStmtInfo->getKind() == CapturedRegionKind::CR_OpenMP &&
           CGM.getLangOpts().OpenMP) {
-        // OpenMP case: binding was captured via its decomposed decl.
         return EmitOMPCapturedBindingLValue(BD);
       }
       // Non-OpenMP case: lambda capture.
