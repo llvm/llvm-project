ad5f035b66df9a0cb43b5526171279f47a1350f8
Wenju He <wenju.he@intel.com>
2026-05-12 13:06:19 +0200

    [clang] Allow mesa3d OS in spirv32 and spirv64 targets
    Fix libclc `spirv32-mesa-mesa3d` target build error:
    error: unknown target triple 'spirv32-mesa-mesa3d'
    
    Fix libclc `spirv-mesa-mesa3d` target build error:
    error: SPIR-V target requires a Vulkan environment.
    
    mesa3d is a valid OS in llvm triple. This PR enables libclc build for
    spirv32-mesa-mesa3d and spirv64-mesa-mesa3d targets.
    
    Note previously spirv[64]-mesa3d- targets used spir[64]-unknown-unknown.
    This PR canonicalizes the triples to spirv32[64]-mesa-mesa3d.

--- clang/test/Frontend/spirv-target-validation.c
+++ clang/test/Frontend/spirv-target-validation.c
@@ -1,5 +1,8 @@
-// RUN: not %clang_cc1 -triple spirv %s 2>&1 | FileCheck %s --check-prefix=CHECK-VULKAN
+// RUN: not %clang_cc1 -triple spirv-unknown-vulkan1.0 %s 2>&1 | FileCheck %s --check-prefix=CHECK-VULKAN
 // RUN: not %clang_cc1 -triple spirv-vulkan-mlibc %s 2>&1 | FileCheck %s --check-prefix=CHECK-SHADER
+// RUN: %clang_cc1 -triple spirv32-mesa-mesa3d %s -fsyntax-only 2>&1
+// RUN: %clang_cc1 -triple spirv64-mesa-mesa3d %s -fsyntax-only 2>&1
+// RUN: %clang_cc1 -triple spirv-mesa-mesa3d %s -fsyntax-only 2>&1
 
 // CHECK-VULKAN: error: SPIR-V target requires a Vulkan environment
 // CHECK-SHADER: error: SPIR-V target requires a valid shader stage environment
