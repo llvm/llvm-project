92e2be064392c7ab2ab78ed2ee180668c855a82c
Balázs Kéri <balazs.keri@ericsson.com>
2026-05-13 16:50:35 +0200

    using split-file at test

--- clang/test/Analysis/ctu/constraintsatisfaction.cpp
+++ clang/test/Analysis/ctu/constraintsatisfaction.cpp
@@ -1,13 +1,16 @@
-// RUN: rm -rf %t && mkdir %t
+// RUN: rm -rf %t
 // RUN: mkdir -p %t/ctudir
+// RUN: split-file %s %t
+
 // RUN: %clang_cc1 -triple x86_64-pc-linux-gnu -std=c++20 \
-// RUN:   -emit-pch -o %t/ctudir/constraintsatisfaction-import.cpp.ast %S/Inputs/constraintsatisfaction-import.cpp
-// RUN: echo "14:c:@F@importee# constraintsatisfaction-import.cpp.ast" > %t/ctudir/externalDefMap.txt
+// RUN:   -emit-pch -o %t/ctudir/import.cpp.ast %t/import.cpp
 // RUN: %clang_analyze_cc1 -triple x86_64-pc-linux-gnu -std=c++20 \
 // RUN:   -analyzer-checker=core \
 // RUN:   -analyzer-config experimental-enable-naive-ctu-analysis=true \
 // RUN:   -analyzer-config ctu-dir=%t/ctudir \
-// RUN:   -verify %s
+// RUN:   -verify %t/main.cpp
+
+//--- main.cpp
 
 // Check that importing this code does not cause crash.
 // expected-no-diagnostics
@@ -23,3 +26,22 @@ using FalseCheck = BoolConstant<Container<int>>;
 
 void importee();
 void caller() { importee(); }
+
+//--- import.cpp
+
+template <typename T>
+concept Sizable = requires(T t) { t.size(); };
+
+template <typename T>
+concept Container = Sizable<T> && requires(T t) { t.begin(); };
+
+template <bool> struct BoolConstant {};
+using FalseCheck = BoolConstant<Container<int>>;
+
+void importee() {
+  FalseCheck f{};
+  (void)f;
+}
+
+//--- ctudir/externalDefMap.txt
+14:c:@F@importee# import.cpp.ast
