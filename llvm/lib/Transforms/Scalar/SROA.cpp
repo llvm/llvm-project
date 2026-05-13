1b02eca9f647393155d98aa3a92e6093159c7530
Yonah Goldberg <ygoldberg@nvidia.com>
2026-05-13 08:42:05 +0000

    format

--- llvm/lib/Transforms/Scalar/SROA.cpp
+++ llvm/lib/Transforms/Scalar/SROA.cpp
@@ -5094,19 +5094,22 @@ bool SROA::presplitLoadsAndStores(AllocaInst &AI, AllocaSlices &AS) {
 /// packed. This can sometimes eliminate allocas because structs cannot get
 /// promoted to LLVM values, but vectors can.
 ///
-/// We only apply this transformation when all users of the alloca are memory intrinsics.
-/// Otherwise, if there is a load or store of some other type to the partition, SROA
-/// would select that type.
+/// We only apply this transformation when all users of the alloca are memory
+/// intrinsics. Otherwise, if there is a load or store of some other type to the
+/// partition, SROA would select that type.
 ///
-/// Applying this transformation too early may hinder memcpyopt, which may generate
-/// better code when eliminating allocas. For example, see `struct-to-vector-fp-store-only-tail.ll`, which
-/// demonstrates that applying this before memcpyopt can initialize previously uninitialized memory when the
-/// alloca gets promoted to an SSA value. For another example, see `struct-to-vector-before-memcpyopt.ll`, which  
-/// demonstrates that applying this before memcpyopt can result in promoting an alloca so that we load a tempory
-/// value instead of copying the tempory value into memory, whereas memcpyopt eliminates the tempory altogether.
+/// Applying this transformation too early may hinder memcpyopt, which may
+/// generate better code when eliminating allocas. For example, see
+/// `struct-to-vector-fp-store-only-tail.ll`, which demonstrates that applying
+/// this before memcpyopt can initialize previously uninitialized memory when
+/// the alloca gets promoted to an SSA value. For another example, see
+/// `struct-to-vector-before-memcpyopt.ll`, which demonstrates that applying
+/// this before memcpyopt can result in promoting an alloca so that we load a
+/// tempory value instead of copying the tempory value into memory, whereas
+/// memcpyopt eliminates the tempory altogether.
 ///
-/// As such, we only apply this transformation after memcpyopt has run. We gate this transformation by the
-/// "CanonicalizeStructToVector" pass option.
+/// As such, we only apply this transformation after memcpyopt has run. We gate
+/// this transformation by the "CanonicalizeStructToVector" pass option.
 static FixedVectorType *tryCanonicalizeStructToVector(StructType *STy,
                                                       Partition &P,
                                                       const DataLayout &DL) {
