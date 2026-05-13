ae6c8487a9a41daa11a2063e4c4bdd762ddd3686
Adam Kallai <kadam@inf.u-szeged.hu>
2026-05-13 14:19:51 +0200

    Address reviewers

--- bolt/lib/Profile/DataAggregator.cpp
+++ bolt/lib/Profile/DataAggregator.cpp
@@ -490,7 +490,7 @@ std::error_code DataAggregator::parsePerfTextFileHeader() {
 }
 
 void DataAggregator::parsePerfTextData(BinaryContext &BC) {
-  outs() << "PERF2BOLT: parsing a hybrid perf-script events...\n";
+  outs() << "PERF2BOLT: parsing a textual perf-script events...\n";
   NamedRegionTimer T("parsePerfTextData", "Parsing perf-script events",
                      TimerGroupName, TimerGroupDesc, opts::TimeAggregator);
   if (!Filename.empty()) {
