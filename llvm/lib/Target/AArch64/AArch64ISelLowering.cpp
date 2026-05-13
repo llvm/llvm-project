5dd44c1456f714970fe93cc9b02e1a4117f3030c
Piotr Fusik <p.fusik@samsung.com>
2026-05-13 13:43:00 +0200

    [AArch64] clang-format

--- llvm/lib/Target/AArch64/AArch64ISelLowering.cpp
+++ llvm/lib/Target/AArch64/AArch64ISelLowering.cpp
@@ -4263,10 +4263,12 @@ static SDValue getAArch64Cmp(SDValue LHS, SDValue RHS, ISD::CondCode CC,
       // Fold into CMP WZR, reg, shift #amount
       unsigned Opc = LHS.getOpcode();
       if (Opc == ISD::SHL || Opc == ISD::SRL || Opc == ISD::SRA) {
-        if (ConstantSDNode *ShiftCst = dyn_cast<ConstantSDNode>(LHS.getOperand(1))) {
+        if (ConstantSDNode *ShiftCst =
+                dyn_cast<ConstantSDNode>(LHS.getOperand(1))) {
           uint64_t Shift = ShiftCst->getZExtValue();
           EVT VT = LHS.getValueType();
-          if ((VT == MVT::i32 && Shift <= 31) || (VT == MVT::i64 && Shift <= 63)) {
+          if ((VT == MVT::i32 && Shift <= 31) ||
+              (VT == MVT::i64 && Shift <= 63)) {
             std::swap(LHS, RHS);
             CC = ISD::getSetCCSwappedOperands(CC);
           }
