1436737d721c3978354d79cbe19f89d2ce9f25a7
Michael Jones <michaelrj@google.com>
2026-05-13 16:54:23 +0000

    address comments and update entrypoints

--- libc/src/wchar/fputwc.cpp
+++ libc/src/wchar/fputwc.cpp
@@ -1,9 +1,15 @@
-//===-- Implementation of fputwc ------------------------------------------===//
+//===----------------------------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file contains the implementation of the fputwc function, which writes a
+/// single wide character to a file.
+///
 //===----------------------------------------------------------------------===//
 
 #include "src/wchar/fputwc.h"
@@ -22,11 +28,14 @@ LLVM_LIBC_FUNCTION(wint_t, fputwc, (wchar_t wc, ::FILE *stream)) {
   LIBC_CRASH_ON_NULLPTR(stream);
   auto *f = reinterpret_cast<File *>(stream);
   FileIOResult result = f->write(&wc, 1);
-  if (result.has_error() || result.value < 1) {
-    if (result.has_error())
-      libc_errno = result.error;
+  if (result.has_error()) {
+    libc_errno = result.error;
     return WEOF;
   }
+  // It should be impossible for result.value (the number of characters written)
+  // to be any value other than 1 at this point. If the system failed to write
+  // then f->write should have returned an error.
+  LIBC_ASSERT(result.value == 1);
   return static_cast<wint_t>(wc);
 }
 
--- libc/src/wchar/fputwc.h
+++ libc/src/wchar/fputwc.h
@@ -1,9 +1,15 @@
-//===-- Implementation header for fputwc ----------------------------------===//
+//===----------------------------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file contains the prototype of the fputwc function, which writes a
+/// single wide character to a file.
+///
 //===----------------------------------------------------------------------===//
 
 #ifndef LLVM_LIBC_SRC_WCHAR_FPUTWC_H
