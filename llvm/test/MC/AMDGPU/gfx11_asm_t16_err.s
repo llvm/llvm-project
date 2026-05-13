6fe4a8e9605ddadfe3c89639f768cbf7f08338db
Igor Wodiany <igor.wodiany@amd.com>
2026-05-13 22:01:28 +0100

    Separate classes + asm parser

--- llvm/test/MC/AMDGPU/gfx11_asm_t16_err.s
+++ llvm/test/MC/AMDGPU/gfx11_asm_t16_err.s
@@ -2,10 +2,10 @@
 // RUN: not llvm-mc -triple=amdgcn -mcpu=gfx1100 -mattr=+real-true16 %s -filetype=null 2>&1 | FileCheck --check-prefix=GFX11 --implicit-check-not=error: %s
 
 v_mov_b16 v0.l, a0.h
-// GFX11: :[[@LINE-1]]:17: error: invalid operand for instruction
+// GFX11: :[[@LINE-1]]:17: error: invalid subregister
 
 v_mov_b16 v0.l, s0.h
-// GFX11: :[[@LINE-1]]:17: error: invalid operand for instruction
+// GFX11: :[[@LINE-1]]:17: error: invalid subregister
 
 v_mov_b16 v0.l, ttmp0.h
-// GFX11: :[[@LINE-1]]:17: error: invalid operand for instruction
+// GFX11: :[[@LINE-1]]:17: error: invalid subregister
