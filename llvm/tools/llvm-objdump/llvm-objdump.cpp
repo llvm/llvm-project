b88ba1f9204eed3da253e789c7b3db83824db711
Peter Collingbourne <peter@pcc.me.uk>
2026-05-13 10:47:09 -0700

    comments
    Created using spr 1.3.6-beta.1

--- llvm/tools/llvm-objdump/llvm-objdump.cpp
+++ llvm/tools/llvm-objdump/llvm-objdump.cpp
@@ -3718,7 +3718,7 @@ static void parseObjdumpOptions(const llvm::opt::InputArgList &InputArgs) {
   TracebackTable = InputArgs.hasArg(OBJDUMP_traceback_table);
   DisassembleSymbols =
       commaSeparatedValues(InputArgs, OBJDUMP_disassemble_symbols_EQ);
-  for (auto Sym : InputArgs.getAllArgValues(OBJDUMP_disassemble))
+  for (auto Sym : InputArgs.getAllArgValues(OBJDUMP_disassemble_EQ))
     DisassembleSymbols.push_back(Sym);
   DisassembleZeroes = InputArgs.hasArg(OBJDUMP_disassemble_zeroes);
   if (const opt::Arg *A = InputArgs.getLastArg(OBJDUMP_dwarf_EQ)) {
