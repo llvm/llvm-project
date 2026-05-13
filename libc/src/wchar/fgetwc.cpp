815dc1120e81cead28bb0fb4c74a4e6a6133fa61
Michael Jones <michaelrj@google.com>
2026-05-13 17:05:58 +0000

    address comments and update entrypoints

--- libc/src/wchar/fgetwc.cpp
+++ libc/src/wchar/fgetwc.cpp
@@ -1,9 +1,15 @@
-//===-- Implementation of fgetwc ------------------------------------------===//
+//===----------------------------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file contains the implementation of the fgetwc function, which reads a
+/// single wide character from a file.
+///
 //===----------------------------------------------------------------------===//
 
 #include "src/wchar/fgetwc.h"
