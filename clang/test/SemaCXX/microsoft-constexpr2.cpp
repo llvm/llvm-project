3fc9edea920b3f5cdad57e5e152058c84b6987a8
Evgeny Leviant <eleviant@accesssoftek.com>
2026-05-14 20:02:28 +0200

    Improved diagnostics

--- clang/test/SemaCXX/microsoft-constexpr2.cpp
+++ clang/test/SemaCXX/microsoft-constexpr2.cpp
@@ -3,10 +3,12 @@
 typedef long long LONG_PTR;
 typedef long LONG;
 #define FIELD_OFFSET(type, field) ((LONG_PTR)&(((type *)0)->field))
+#define FIELD_OFFSET2(type, field) (reinterpret_cast<LONG_PTR>(&(((type *)0)->field)))
 
 struct S {
   int x;
   int y;
 };
 
-constexpr long b = FIELD_OFFSET(S, y); // expected-warning {{folding this constant expression is a Microsoft extension}}
+constexpr long b = FIELD_OFFSET(S, y); // expected-warning {{folding constant expression involving cast that performs the conversions of a reinterpret_cast is a Microsoft extension}}
+constexpr long b2 = FIELD_OFFSET2(S, y); // expected-warning {{folding constant expression involving reinterpret_cast is a Microsoft extension}}
