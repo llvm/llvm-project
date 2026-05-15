e9e5fb2174e9e765e17c498fb40c2ef5f7c78d54
Michael Jones <michaelrj@google.com>
2026-05-15 18:42:31 +0000

    address comments

--- libc/test/src/wchar/ungetwc_test.cpp
+++ libc/test/src/wchar/ungetwc_test.cpp
@@ -1,9 +1,14 @@
-//===-- Unittests for ungetwc ---------------------------------------------===//
+//===----------------------------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// Unittests for ungetwc
+///
 //===----------------------------------------------------------------------===//
 
 #include "hdr/stdio_macros.h" // For SEEK_CUR
