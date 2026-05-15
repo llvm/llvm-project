784d8bfa4c6a992702d49f1bd2536874298436cb
Momchil Velikov <momchil.velikov@arm.com>
2026-05-15 15:49:51 +0100

    [fixup] Keep the LoopUnrollPass in the full LTO pipeline after vectorization

--- llvm/lib/Passes/PassBuilderPipelines.cpp
+++ llvm/lib/Passes/PassBuilderPipelines.cpp
@@ -1353,7 +1353,6 @@ void PassBuilder::addVectorPasses(OptimizationLevel Level,
 
   FPM.addPass(InferAlignmentPass());
   if (isFullLTOPostLink(LTOPhase)) {
-    LoopPassManager LPM;
     // The vectorizer may have significantly shortened a loop body; unroll
     // again. Unroll small loops to hide loop backedge latency and saturate any
     // parallel execution resources of an out-of-order processor. We also then
@@ -1363,15 +1362,11 @@ void PassBuilder::addVectorPasses(OptimizationLevel Level,
     // across the loop nests.
     // We do UnrollAndJam in a separate LPM to ensure it happens before unroll
     if (EnableUnrollAndJam && PTO.LoopUnrolling)
-      LPM.addPass(LoopUnrollAndJamPass(static_cast<int>(Level)));
-
-    // Unroll small loops and perform peeling.
-    LPM.addPass(LoopFullUnrollPass(static_cast<int>(Level),
-                                   /* OnlyWhenForced= */ !PTO.LoopUnrolling,
-                                   PTO.ForgetAllSCEVInLoopUnroll));
-    FPM.addPass(createFunctionToLoopPassAdaptor(std::move(LPM),
-                                                /*UseMemorySSA=*/false));
-
+      FPM.addPass(createFunctionToLoopPassAdaptor(
+          LoopUnrollAndJamPass(static_cast<int>(Level))));
+    FPM.addPass(LoopUnrollPass(LoopUnrollOptions(
+        static_cast<int>(Level), /*OnlyWhenForced=*/!PTO.LoopUnrolling,
+        PTO.ForgetAllSCEVInLoopUnroll)));
     FPM.addPass(WarnMissedTransformationsPass());
     // Now that we are done with loop unrolling, be it either by LoopVectorizer,
     // or LoopUnroll passes, some variable-offset GEP's into alloca's could have
