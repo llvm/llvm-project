030caf006f94f350d252b23bb2a26d756be9c370
Caroline Newcombe <caroline.newcombe@hpe.com>
2026-05-15 09:20:56 -0500

    Add CHARACTER test case

--- flang/test/Lower/OpenMP/lastprivate-equivalence.f90
+++ flang/test/Lower/OpenMP/lastprivate-equivalence.f90
@@ -1,11 +1,14 @@
-! Test that EQUIVALENCE'd arrays are correctly privatized in OpenMP.
-! Variables in EQUIVALENCE are lowered with fir.ptr addresses (see
-! castAliasToPointer in ConvertVariable.cpp), but they are not Fortran
-! POINTERs. The privatizer must allocate storage for the actual array type.
+! Test that EQUIVALENCE'd variables with lastprivate are correctly privatized.
+! EQUIVALENCE aliases use fir.ptr (via castAliasToPointer), but are not true
+! Fortran POINTERs. The privatizer must unwrap fir.ptr and allocate the
+! underlying data type.
 
 !RUN: %flang_fc1 -emit-hlfir -fopenmp -o - %s 2>&1 | FileCheck %s
 
-! Verify the privatizer operates on a box (array) type, not a fir.ptr type,
+! Privatizers appear at module scope; second subroutine's privatizer comes first.
+!CHECK: omp.private {type = private} @[[C1_PRIV:.*Ec1_private.*]] : !fir.char<1,10>
+
+! Verify the array privatizer operates on a box type, not a fir.ptr type,
 ! and that it allocates the full array.
 !CHECK: omp.private {type = private} @[[A_PRIV:.*Ea_private.*]] : !fir.box<!fir.array<10xi32>> init {
 !CHECK:   %[[SHAPE:.*]] = fir.shape %{{.*}} : (index) -> !fir.shape<1>
@@ -41,6 +44,31 @@ subroutine lastprivate_equivalence
   !$omp parallel do lastprivate(a)
   do i = 1, 10
     a(:) = i
+  end do
+  !$omp end parallel do
+end subroutine
+
+!CHECK-LABEL: func.func @_QPlastprivate_equiv_char()
+!CHECK: %[[CAGG:.*]] = fir.alloca !fir.array<10xi8>
+!CHECK: %[[C1_PTR:.*]] = fir.convert %{{.*}} : (!fir.ref<i8>) -> !fir.ptr<!fir.char<1,10>>
+!CHECK: %[[C1_DECL:.*]]:2 = hlfir.declare %[[C1_PTR]] typeparams %{{.*}} storage(%[[CAGG]][0]) {uniq_name = "_QFlastprivate_equiv_charEc1"}
+!CHECK: omp.parallel {
+!CHECK:   omp.wsloop private(@[[C1_PRIV]] %{{.*}}
+!CHECK:     omp.loop_nest
+!CHECK:       fir.if %{{.*}} {
+!CHECK:         hlfir.assign %{{.*}} to %[[C1_DECL]]#0 : !fir.ptr<!fir.char<1,10>>, !fir.ptr<!fir.char<1,10>>
+!CHECK:       }
+!CHECK:       omp.yield
+!CHECK:     }
+!CHECK:   }
+!CHECK:   omp.terminator
+!CHECK: }
+subroutine lastprivate_equiv_char
+  character(10) :: c1, c2
+  equivalence (c1, c2)
+  !$omp parallel do lastprivate(c1)
+  do i = 1, 4
+    c1 = 'test'
   end do
   !$omp end parallel do
 end subroutine
