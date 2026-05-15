468d5fc3e53a046574550b1b1a79b89f90ca1ea4
Ayokunle Amodu <ayokunle321@gmail.com>
2026-05-15 17:13:59 -0400

    add amdgcn exp2  buitins

--- clang/lib/CIR/CodeGen/CIRGenBuiltinAMDGPU.cpp
+++ clang/lib/CIR/CodeGen/CIRGenBuiltinAMDGPU.cpp
@@ -272,10 +272,10 @@ CIRGenFunction::emitAMDGPUBuiltinExpr(unsigned builtinId,
   }
   case AMDGPU::BI__builtin_amdgcn_exp2f:
   case AMDGPU::BI__builtin_amdgcn_exp2_bf16: {
-    cgm.errorNYI(expr->getSourceRange(),
-                 std::string("unimplemented AMDGPU builtin call: ") +
-                     getContext().BuiltinInfo.getName(builtinId));
-    return mlir::Value{};
+    mlir::Value src = emitScalarExpr(expr->getArg(0));
+    return builder.emitIntrinsicCallOp(getLoc(expr->getExprLoc()),
+                                       "amdgcn.exp2", src.getType(),
+                                       mlir::ValueRange{src});
   }
   case AMDGPU::BI__builtin_amdgcn_log_clampf: {
     cgm.errorNYI(expr->getSourceRange(),
