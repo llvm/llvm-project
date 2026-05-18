6c3c09d1b026f996ad01bbf5f6a732dd6ae54115
Razvan Lupusoru <rlupusoru@nvidia.com>
2026-05-18 16:23:52 -0700

    [flang][acc] Handle Fortran do loops as acc loops in acc routine
    As was previously done for do loops in acc compute constructs in
    https://github.com/llvm/llvm-project/issues/149614 , this PR
    does the same for do loops in `acc routine`. The rules are follows:
    - Do loops not marked with `acc loop` are considered `auto`
    - Do concurrent loops are considered `independent`
    - Any loops in an `acc routine seq` are considered `seq`
    
    This ensures that the IV is correctly privatized and attached to
    acc loop.

--- flang/lib/Lower/Bridge.cpp
+++ flang/lib/Lower/Bridge.cpp
@@ -2539,9 +2539,10 @@ private:
       return;
     }
 
-    // Loops with induction variables inside OpenACC compute constructs
-    // need special handling to ensure that the IVs are privatized.
-    if (Fortran::lower::isInsideOpenACCComputeConstruct(*builder)) {
+    // Loops with induction variables inside OpenACC compute constructs or
+    // explicit `!$acc routine` procedures need special handling to ensure that
+    // the IVs are privatized.
+    if (Fortran::lower::shouldLowerDoConstructAsAccLoop(*builder)) {
       // Open up a new scope for the loop variables.
       localSymbols.pushScope();
       llvm::scope_exit scopeGuard([&]() { localSymbols.popScope(); });
