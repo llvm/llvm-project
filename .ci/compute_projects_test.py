76ae1b79f5373865972940e7b287414c7168df49
Aiden Grossman <aidengrossman@google.com>
2026-05-18 13:18:33 +0000

    formatting

--- .ci/compute_projects_test.py
+++ .ci/compute_projects_test.py
@@ -90,7 +90,8 @@ class TestComputeProjects(unittest.TestCase):
             "check-clang check-clang-tools check-cross-project check-lldb",
         )
         self.assertEqual(
-            env_variables["runtimes_to_build"], "compiler-rt;libc;libcxx;libcxxabi;libunwind"
+            env_variables["runtimes_to_build"],
+            "compiler-rt;libc;libcxx;libcxxabi;libunwind",
         )
         self.assertEqual(
             env_variables["runtimes_check_targets"],
@@ -167,7 +168,8 @@ class TestComputeProjects(unittest.TestCase):
             "check-clang check-clang-cir check-clang-tools check-cross-project check-lldb",
         )
         self.assertEqual(
-            env_variables["runtimes_to_build"], "compiler-rt;libc;libcxx;libcxxabi;libunwind"
+            env_variables["runtimes_to_build"],
+            "compiler-rt;libc;libcxx;libcxxabi;libunwind",
         )
         self.assertEqual(
             env_variables["runtimes_check_targets"],
