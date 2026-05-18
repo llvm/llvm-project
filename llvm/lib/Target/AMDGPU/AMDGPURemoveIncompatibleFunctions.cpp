721fb423ebddd11561c44660bd8dbf0a0489f763
Mariusz Sikora <mariusz.sikora@amd.com>
2026-05-18 16:21:02 -0400

    Add HasWave32 & HasWave64

--- llvm/lib/Target/AMDGPU/AMDGPURemoveIncompatibleFunctions.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPURemoveIncompatibleFunctions.cpp
@@ -199,12 +199,16 @@ bool AMDGPURemoveIncompatibleFunctions::checkFunction(Function &F) {
   // gfx9 and below targets that don't support the mode.
   // gfx10, gfx11, gfx12 are implied to support both wave32 and 64 features.
   // They are not in the feature set. So, we need a separate check
-  if (ST->hasWave64Only() && ST->hasFeature(AMDGPU::FeatureWavefrontSize32)) {
+  bool HasWave64Only = ST->hasFeature(AMDGPU::FeatureHasWave64) &&
+                       !ST->hasFeature(AMDGPU::FeatureHasWave32);
+  if (HasWave64Only && ST->hasFeature(AMDGPU::FeatureWavefrontSize32)) {
     reportFunctionRemoved(F, AMDGPU::FeatureWavefrontSize32);
     return true;
   }
   // gfx125x only support FeatureWavefrontSize32.
-  if (ST->hasWave32Only() && ST->hasFeature(AMDGPU::FeatureWavefrontSize64)) {
+  bool HasWave32Only = ST->hasFeature(AMDGPU::FeatureHasWave32) &&
+                       !ST->hasFeature(AMDGPU::FeatureHasWave64);
+  if (HasWave32Only && ST->hasFeature(AMDGPU::FeatureWavefrontSize64)) {
     reportFunctionRemoved(F, AMDGPU::FeatureWavefrontSize64);
     return true;
   }
