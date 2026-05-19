c5ce51c83821ad5fc70224e8a93d8a26282ab69b
Pavel Labath <pavel@labath.sk>
2026-05-19 13:37:18 +0000

    rm send recv endpoint

--- libc/src/sys/socket/linux/recv.cpp
+++ libc/src/sys/socket/linux/recv.cpp
@@ -12,7 +12,7 @@
 //===----------------------------------------------------------------------===//
 #include "src/sys/socket/recv.h"
 #include "hdr/types/ssize_t.h"
-#include "src/__support/OSUtil/linux/syscall_wrappers/recv.h"
+#include "src/__support/OSUtil/linux/syscall_wrappers/recvfrom.h"
 #include "src/__support/common.h"
 #include "src/__support/libc_errno.h"
 #include "src/__support/macros/sanitizer.h"
@@ -21,7 +21,8 @@ namespace LIBC_NAMESPACE_DECL {
 
 LLVM_LIBC_FUNCTION(ssize_t, recv,
                    (int sockfd, void *buf, size_t len, int flags)) {
-  auto result = linux_syscalls::recv(sockfd, buf, len, flags);
+  auto result =
+      linux_syscalls::recvfrom(sockfd, buf, len, flags, nullptr, nullptr);
   if (!result.has_value()) {
     libc_errno = result.error();
     return -1;
--- libc/src/sys/socket/linux/send.cpp
+++ libc/src/sys/socket/linux/send.cpp
@@ -12,7 +12,7 @@
 //===----------------------------------------------------------------------===//
 #include "src/sys/socket/send.h"
 #include "hdr/types/ssize_t.h"
-#include "src/__support/OSUtil/linux/syscall_wrappers/send.h"
+#include "src/__support/OSUtil/linux/syscall_wrappers/sendto.h"
 #include "src/__support/common.h"
 #include "src/__support/libc_errno.h"
 
@@ -20,7 +20,7 @@ namespace LIBC_NAMESPACE_DECL {
 
 LLVM_LIBC_FUNCTION(ssize_t, send,
                    (int sockfd, const void *buf, size_t len, int flags)) {
-  auto result = linux_syscalls::send(sockfd, buf, len, flags);
+  auto result = linux_syscalls::sendto(sockfd, buf, len, flags, nullptr, 0);
   if (!result.has_value()) {
     libc_errno = result.error();
     return -1;
