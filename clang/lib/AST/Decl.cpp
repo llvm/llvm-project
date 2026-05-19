fbf989fb0db047a7467758730549250e93e3d8b3
Vlad Serebrennikov <serebrennikov.vladislav@gmail.com>
2026-05-19 15:58:12 +0300

    Add a comment

--- clang/lib/AST/Decl.cpp
+++ clang/lib/AST/Decl.cpp
@@ -679,6 +679,8 @@ LinkageComputer::getLVForNamespaceScopeDecl(const NamedDecl *D,
   //   scope and no storage-class specifier, its linkage is
   //   external.
   LinkageInfo LV = getExternalLinkageFor(D);
+
+  // Inherit linkage from the enclosing namespace, if there is any.
   if (const auto *ND = dyn_cast<NamespaceDecl>(
           D->getDeclContext()->getEnclosingNamespaceContext())) {
     LV.setLinkage(ND->getLinkageInternal());
