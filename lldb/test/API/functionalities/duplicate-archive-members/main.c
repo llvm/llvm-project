980dad4777dbd7e48709b7f9b5f4adbca1b8ef39
Charles Zablit <c_zablit@apple.com>
2026-05-19 17:46:36 +0100

    fix formatting

--- lldb/test/API/functionalities/duplicate-archive-members/main.c
+++ lldb/test/API/functionalities/duplicate-archive-members/main.c
@@ -2,8 +2,7 @@
 
 extern int a(int);
 extern int b(int);
-int main (int argc, char const *argv[])
-{
-    printf ("a(1) returns %d\n", a(1));
-    printf ("b(2) returns %d\n", b(2));
+int main(int argc, char const *argv[]) {
+  printf("a(1) returns %d\n", a(1));
+  printf("b(2) returns %d\n", b(2));
 }
