58e661103afc3d890733ea83e643b0aa38ce6a6c
Michael Kruse <llvm-project@meinersbur.de>
2026-05-19 14:39:51 +0200

    clang-format

--- flang/lib/Frontend/CompilerInvocation.cpp
+++ flang/lib/Frontend/CompilerInvocation.cpp
@@ -1906,7 +1906,6 @@ void CompilerInvocation::setFortranOpts() {
       preprocessorOptions.searchDirectoriesFromIntrModPath.begin(),
       preprocessorOptions.searchDirectoriesFromIntrModPath.end());
 
-
   // Add the directory supplied through -J/-module-dir to the list of search
   // directories
   if (moduleDirJ != ".")
