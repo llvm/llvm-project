5c2157b216ee2523d7644a6f6599280065b77e2e
tomershafir <tomer.shafir8@gmail.com>
2026-05-19 10:35:59 +0300

    add upscale-frac+rounging tests

--- llvm/test/tools/llvm-mca/reservation-station-scale-factor.s
+++ llvm/test/tools/llvm-mca/reservation-station-scale-factor.s
@@ -3,7 +3,12 @@
 
 ## Check upscaling and downscaling. BufferSize=-1 is not scaled
 # RUN: llvm-mca < %s -mtriple=aarch64 -mcpu=apple-m1 -sched-model-reservation-station-scale-factor=2 -debug 2>&1 | FileCheck %s --check-prefix=UPSCALE
+# RUN: llvm-mca < %s -mtriple=aarch64 -mcpu=apple-m1 -sched-model-reservation-station-scale-factor=1.5 -debug 2>&1 | FileCheck %s --check-prefix=UPSCALE-FRAC
 # RUN: llvm-mca < %s -mtriple=aarch64 -mcpu=apple-m1 -sched-model-reservation-station-scale-factor=0.5 -debug 2>&1 | FileCheck %s --check-prefix=DOWNSCALE
+
+## Check truncation toward zero when scaling produces non-integer results
+# RUN: llvm-mca < %s -mtriple=aarch64 -mcpu=apple-m1 -sched-model-reservation-station-scale-factor=0.7 -debug 2>&1 | FileCheck %s --check-prefix=DOWNSCALE-ROUND
+# RUN: llvm-mca < %s -mtriple=aarch64 -mcpu=apple-m1 -sched-model-reservation-station-scale-factor=1.7 -debug 2>&1 | FileCheck %s --check-prefix=UPSCALE-ROUND
 
 ## Default scale factor is 1
 # RUN: llvm-mca < %s -mtriple=aarch64 -mcpu=apple-m1 -debug 2>&1 | FileCheck %s --check-prefix=ORIGINAL
@@ -75,6 +80,57 @@ add x2, x0, x1
 # UPSCALE-NEXT: [12]  - 0x00000000000800 - CyUnitVD (BufferSize=32)
 # UPSCALE-NEXT: [13]  - 0x00000000001000 - CyUnitVM (BufferSize=64)
 # UPSCALE:      [0] Code Region - foo
+
+# UPSCALE-FRAC-COUNT-1: Processor resources:
+# UPSCALE-FRAC-NEXT: [ 0]  - 0x00000000000000 - InvalidUnit
+# UPSCALE-FRAC-NEXT: [ 1]  - 0x00000000000001 - CyUnitB (BufferSize=36)
+# UPSCALE-FRAC-NEXT: [ 2]  - 0x00000000000002 - CyUnitBR (BufferSize=-1)
+# UPSCALE-FRAC-NEXT: [ 3]  - 0x00000000000004 - CyUnitFloatDiv (BufferSize=-1)
+# UPSCALE-FRAC-NEXT: [ 4]  - 0x00000000000008 - CyUnitI (BufferSize=72)
+# UPSCALE-FRAC-NEXT: [ 5]  - 0x00000000000010 - CyUnitID (BufferSize=24)
+# UPSCALE-FRAC-NEXT: [ 6]  - 0x00000000000020 - CyUnitIM (BufferSize=48)
+# UPSCALE-FRAC-NEXT: [ 7]  - 0x00000000000040 - CyUnitIS (BufferSize=36)
+# UPSCALE-FRAC-NEXT: [ 8]  - 0x00000000000080 - CyUnitIntDiv (BufferSize=-1)
+# UPSCALE-FRAC-NEXT: [ 9]  - 0x00000000000100 - CyUnitLS (BufferSize=42)
+# UPSCALE-FRAC-NEXT: [10]  - 0x00000000000200 - CyUnitV (BufferSize=72)
+# UPSCALE-FRAC-NEXT: [11]  - 0x00000000000400 - CyUnitVC (BufferSize=24)
+# UPSCALE-FRAC-NEXT: [12]  - 0x00000000000800 - CyUnitVD (BufferSize=24)
+# UPSCALE-FRAC-NEXT: [13]  - 0x00000000001000 - CyUnitVM (BufferSize=48)
+# UPSCALE-FRAC:      [0] Code Region - foo
+
+# DOWNSCALE-ROUND-COUNT-1: Processor resources:
+# DOWNSCALE-ROUND-NEXT: [ 0]  - 0x00000000000000 - InvalidUnit
+# DOWNSCALE-ROUND-NEXT: [ 1]  - 0x00000000000001 - CyUnitB (BufferSize=16)
+# DOWNSCALE-ROUND-NEXT: [ 2]  - 0x00000000000002 - CyUnitBR (BufferSize=-1)
+# DOWNSCALE-ROUND-NEXT: [ 3]  - 0x00000000000004 - CyUnitFloatDiv (BufferSize=-1)
+# DOWNSCALE-ROUND-NEXT: [ 4]  - 0x00000000000008 - CyUnitI (BufferSize=33)
+# DOWNSCALE-ROUND-NEXT: [ 5]  - 0x00000000000010 - CyUnitID (BufferSize=11)
+# DOWNSCALE-ROUND-NEXT: [ 6]  - 0x00000000000020 - CyUnitIM (BufferSize=22)
+# DOWNSCALE-ROUND-NEXT: [ 7]  - 0x00000000000040 - CyUnitIS (BufferSize=16)
+# DOWNSCALE-ROUND-NEXT: [ 8]  - 0x00000000000080 - CyUnitIntDiv (BufferSize=-1)
+# DOWNSCALE-ROUND-NEXT: [ 9]  - 0x00000000000100 - CyUnitLS (BufferSize=19)
+# DOWNSCALE-ROUND-NEXT: [10]  - 0x00000000000200 - CyUnitV (BufferSize=33)
+# DOWNSCALE-ROUND-NEXT: [11]  - 0x00000000000400 - CyUnitVC (BufferSize=11)
+# DOWNSCALE-ROUND-NEXT: [12]  - 0x00000000000800 - CyUnitVD (BufferSize=11)
+# DOWNSCALE-ROUND-NEXT: [13]  - 0x00000000001000 - CyUnitVM (BufferSize=22)
+# DOWNSCALE-ROUND:      [0] Code Region - foo
+
+# UPSCALE-ROUND-COUNT-1: Processor resources:
+# UPSCALE-ROUND-NEXT: [ 0]  - 0x00000000000000 - InvalidUnit
+# UPSCALE-ROUND-NEXT: [ 1]  - 0x00000000000001 - CyUnitB (BufferSize=40)
+# UPSCALE-ROUND-NEXT: [ 2]  - 0x00000000000002 - CyUnitBR (BufferSize=-1)
+# UPSCALE-ROUND-NEXT: [ 3]  - 0x00000000000004 - CyUnitFloatDiv (BufferSize=-1)
+# UPSCALE-ROUND-NEXT: [ 4]  - 0x00000000000008 - CyUnitI (BufferSize=81)
+# UPSCALE-ROUND-NEXT: [ 5]  - 0x00000000000010 - CyUnitID (BufferSize=27)
+# UPSCALE-ROUND-NEXT: [ 6]  - 0x00000000000020 - CyUnitIM (BufferSize=54)
+# UPSCALE-ROUND-NEXT: [ 7]  - 0x00000000000040 - CyUnitIS (BufferSize=40)
+# UPSCALE-ROUND-NEXT: [ 8]  - 0x00000000000080 - CyUnitIntDiv (BufferSize=-1)
+# UPSCALE-ROUND-NEXT: [ 9]  - 0x00000000000100 - CyUnitLS (BufferSize=47)
+# UPSCALE-ROUND-NEXT: [10]  - 0x00000000000200 - CyUnitV (BufferSize=81)
+# UPSCALE-ROUND-NEXT: [11]  - 0x00000000000400 - CyUnitVC (BufferSize=27)
+# UPSCALE-ROUND-NEXT: [12]  - 0x00000000000800 - CyUnitVD (BufferSize=27)
+# UPSCALE-ROUND-NEXT: [13]  - 0x00000000001000 - CyUnitVM (BufferSize=54)
+# UPSCALE-ROUND:      [0] Code Region - foo
 
 # BUFFER-ZERO-COUNT-1: Processor resources:
 # BUFFER-ZERO-NEXT: [ 0]  - 0x00000000000000 - InvalidUnit
