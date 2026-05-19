8076d17b61028e7fd5723fa84fd5615c945ae46b
Pavel Labath <pavel@labath.sk>
2026-05-19 16:00:59 +0200

    [libc] Port remaining socket functions to syscall_wrappers (#198463)
    While in there:
    - fix file headers to conform to latest standards
    - add missing restrict qualifier to recvfrom
    
    Assisted by Gemini.

--- libc/src/__support/OSUtil/linux/syscall_wrappers/recvmsg.h
+++ libc/src/__support/OSUtil/linux/syscall_wrappers/recvmsg.h
@@ -7,26 +7,29 @@
 //===----------------------------------------------------------------------===//
 ///
 /// \file
-/// Linux implementation of sendmsg.
+/// Syscall wrapper for recvmsg.
 ///
 //===----------------------------------------------------------------------===//
-#include "src/sys/socket/sendmsg.h"
+
+#ifndef LLVM_LIBC_SRC___SUPPORT_OSUTIL_SYSCALL_WRAPPERS_RECVMSG_H
+#define LLVM_LIBC_SRC___SUPPORT_OSUTIL_SYSCALL_WRAPPERS_RECVMSG_H
+
 #include "hdr/types/ssize_t.h"
 #include "hdr/types/struct_msghdr.h"
-#include "src/__support/OSUtil/linux/syscall_wrappers/sendmsg.h"
+#include "src/__support/OSUtil/linux/syscall.h" // For syscall_checked
 #include "src/__support/common.h"
-#include "src/__support/libc_errno.h"
+#include "src/__support/error_or.h"
+#include "src/__support/macros/config.h"
+#include <sys/syscall.h> // For syscall numbers
 
 namespace LIBC_NAMESPACE_DECL {
+namespace linux_syscalls {
 
-LLVM_LIBC_FUNCTION(ssize_t, sendmsg,
-                   (int sockfd, const struct msghdr *msg, int flags)) {
-  auto result = linux_syscalls::sendmsg(sockfd, msg, flags);
-  if (!result.has_value()) {
-    libc_errno = result.error();
-    return -1;
-  }
-  return result.value();
+LIBC_INLINE ErrorOr<ssize_t> recvmsg(int sockfd, msghdr *msg, int flags) {
+  return syscall_checked<ssize_t>(SYS_recvmsg, sockfd, msg, flags);
 }
 
+} // namespace linux_syscalls
 } // namespace LIBC_NAMESPACE_DECL
+
+#endif // LLVM_LIBC_SRC___SUPPORT_OSUTIL_SYSCALL_WRAPPERS_RECVMSG_H
