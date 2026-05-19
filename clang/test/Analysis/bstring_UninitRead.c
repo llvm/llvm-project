b0fc90b8ec9f463de96eb6a882b54e8bd3aeef04
Endre Fülöp <endre.fulop@sigmatechnology.com>
2026-05-19 18:09:18 +0200

    move the tests up where they belong in the testfile

--- clang/test/Analysis/bstring_UninitRead.c
+++ clang/test/Analysis/bstring_UninitRead.c
@@ -43,6 +43,33 @@ void memcpy_array_partially_init_error(char *dst) {
   (void)buf;
 }
 
+void memcpy_offset_uninit(char *dst) {
+  char buf[10];
+  buf[0] = 'a';
+  // buf[5] is never written.
+  memcpy(dst, &buf[5], 3); // expected-warning{{The first element of the 2nd argument is undefined}}
+                           // expected-note@-1{{Other elements might also be undefined}}
+}
+
+void memcpy_offset_uninit_boundary(char *dst) {
+  char buf[10];
+  buf[5] = 'a';
+  // buf[6] and buf[7] are never written.
+  memcpy(dst, &buf[5], 3); // expected-warning{{The last accessed element (at index 2) in the 2nd argument is undefined}}
+                           // expected-note@-1{{Other elements might also be undefined}}
+}
+
+// Reduced false positive: memcpy from an offset into a partially initialized
+// buffer should not warn if the accessed range is initialized.
+void memcpy_offset_no_false_positive(char *dst) {
+  char buf[10];
+  buf[5] = 'a';
+  buf[6] = 'b';
+  buf[7] = 'c';
+  memcpy(dst, &buf[5], 3); // no-warning
+}
+
+
 // The interesting case here is that the portion we're copying is initialized,
 // but not the whole matrix. We need to be careful to extract buf[1], and not
 // buf when trying to peel region layers off from the source argument.
@@ -105,32 +132,6 @@ void mempcpy_struct_fully_uninit() {
   // FIXME: Maybe ask UninitializedObjectChecker whether s1 is fully
   // initialized?
   mempcpy(&s2, &s1, sizeof(struct st));
-}
-
-void memcpy_offset_uninit(char *dst) {
-  char buf[10];
-  buf[0] = 'a';
-  // buf[5] is never written.
-  memcpy(dst, &buf[5], 3); // expected-warning{{The first element of the 2nd argument is undefined}}
-                           // expected-note@-1{{Other elements might also be undefined}}
-}
-
-void memcpy_offset_uninit_boundary(char *dst) {
-  char buf[10];
-  buf[5] = 'a';
-  // buf[6] and buf[7] are never written.
-  memcpy(dst, &buf[5], 3); // expected-warning{{The last accessed element (at index 2) in the 2nd argument is undefined}}
-                           // expected-note@-1{{Other elements might also be undefined}}
-}
-
-// Reduced false positive: memcpy from an offset into a partially initialized
-// buffer should not warn if the accessed range is initialized.
-void memcpy_offset_no_false_positive(char *dst) {
-  char buf[10];
-  buf[5] = 'a';
-  buf[6] = 'b';
-  buf[7] = 'c';
-  memcpy(dst, &buf[5], 3); // no-warning
 }
 
 // Creduced crash. In this case, an symbolicregion is wrapped in an
