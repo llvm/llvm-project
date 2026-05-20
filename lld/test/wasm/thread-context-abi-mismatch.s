a9c8aa0d59da471a168e401bc8aacb79183b3025
Sy Brand <sy.brand@fastly.com>
2026-05-20 08:43:01 +0100

    Update test

--- lld/test/wasm/thread-context-abi-mismatch.s
+++ lld/test/wasm/thread-context-abi-mismatch.s
@@ -3,15 +3,13 @@
 # RUN: split-file %s %t
 
 # Test that the presence of an import of __stack_pointer from the env module is treated 
-# as an indication that the global thread context ABI is being used, even if the
-# libcall-thread-context feature is not disallowed.
+# as an indication that the global thread context ABI is being used.
 
 # RUN: llvm-mc -filetype=obj -triple=wasm32-unknown-unknown -o %t/start.o %t/start.s
 # RUN: llvm-mc -filetype=obj -triple=wasm32-unknown-unknown -o %t/stack-pointer.o %t/stack-pointer.s
 # RUN: not wasm-ld --libcall-thread-context %t/start.o %t/stack-pointer.o -o %t/fail.wasm 2>&1 | FileCheck %s
 
 # CHECK: stack-pointer.o: object file uses globals for thread context, but --libcall-thread-context was specified
-# | ^
 
 #--- start.s
 .globl _start
