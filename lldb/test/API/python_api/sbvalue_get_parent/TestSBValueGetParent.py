dc5649bcc62b93fcd82020f2ac2db50cbe149b3d
Dave Lee <davelee.com@gmail.com>
2026-05-20 10:05:55 -0700

    [lldb] Call SetSyntheticChildrenGenerated on GetSyntheticChildAtOffset

--- lldb/test/API/python_api/sbvalue_get_parent/TestSBValueGetParent.py
+++ lldb/test/API/python_api/sbvalue_get_parent/TestSBValueGetParent.py
@@ -17,7 +17,18 @@ class TestCase(TestBase):
         frame = thread.GetFrameAtIndex(0)
 
         self._do_test(frame, "parent", "child")
-        self._do_test(frame, "vec", 0)
+        self._do_test(frame, "vec", 1)
+
+    def test_create_child_at_offset(self):
+        """Values returned by CreateChildAtOffset should return the synthetic parent from GetParent."""
+        self.build()
+        _, _, thread, _ = lldbutil.run_to_source_breakpoint(
+            self, "break here", lldb.SBFileSpec("main.cpp")
+        )
+        frame = thread.GetFrameAtIndex(0)
+
+        self.runCmd("command script import MyContainer_synthetic.py")
+        self._do_test(frame, "container", 1)
 
     def _do_test(
         self, frame: lldb.SBFrame, parent_name: str, child_key: Union[str, int]
