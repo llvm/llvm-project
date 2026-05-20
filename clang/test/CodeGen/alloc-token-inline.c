bd8f87566d311fbee1d37176c581c4633075a340
Marco Elver <me@marcoelver.com>
2026-05-20 10:29:33 +0200

    fixup! [AllocToken] Propagate !alloc_token through inlined allocation wrappers

--- clang/test/CodeGen/alloc-token-inline.c
+++ clang/test/CodeGen/alloc-token-inline.c
@@ -1,5 +1,5 @@
-// RUN: %clang_cc1 -O -fsanitize=alloc-token -fsanitize-alloc-token-extended -triple x86_64-linux-gnu -emit-llvm %s -o - | FileCheck %s
-// RUN: %clang_cc1 -O -fsanitize=alloc-token -triple x86_64-linux-gnu -emit-llvm %s -o - | FileCheck %s
+// RUN: %clang_cc1 -O -fsanitize=alloc-token -fsanitize-alloc-token-extended -triple x86_64-linux-gnu -emit-llvm %s -o - | FileCheck %s --implicit-check-not=__alloc_token
+// RUN: %clang_cc1 -O -fsanitize=alloc-token -triple x86_64-linux-gnu -emit-llvm %s -o - | FileCheck %s --implicit-check-not=__alloc_token
 
 typedef __typeof(sizeof(int)) size_t;
 
@@ -17,12 +17,11 @@ __attribute__((malloc, always_inline)) inline void *wrapper(size_t size) {
 
 // CHECK-LABEL: @test_inlined_wrapper(
 // CHECK: call ptr @external_ptr_helper()
-// CHECK-NOT: __alloc_token_external_ptr_helper
 // CHECK: call void @external_void_helper()
-// CHECK-NOT: __alloc_token_external_void_helper
 // CHECK: call{{.*}} @__alloc_token_malloc(i64 noundef 4, i64 2689373973731826898){{.*}} !alloc_token [[META_INT:![0-9]+]]
 void test_inlined_wrapper(void) {
   sink = wrapper(sizeof(int));
 }
 
+// CHECK: declare{{.*}} @__alloc_token_malloc(
 // CHECK: [[META_INT]] = !{!"int", i1 false}
