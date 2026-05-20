d20cda19e2dcbb52193668153d0983dffdb86e32
Sy Brand <sy.brand@fastly.com>
2026-05-20 08:46:26 +0100

    Update thread context test

--- lld/test/wasm/thread-context-abi-mismatch.s
+++ lld/test/wasm/thread-context-abi-mismatch.s
@@ -1,23 +1,17 @@
 # Test that linking object files with mismatched thread context ABIs fails with an error.
-
-# RUN: split-file %s %t
-
-# Test that the presence of an import of __stack_pointer from the env module is treated 
+# The presence of an import of __stack_pointer from the env module should be treated 
 # as an indication that the global thread context ABI is being used.
 
-# RUN: llvm-mc -filetype=obj -triple=wasm32-unknown-unknown -o %t/start.o %t/start.s
-# RUN: llvm-mc -filetype=obj -triple=wasm32-unknown-unknown -o %t/stack-pointer.o %t/stack-pointer.s
-# RUN: not wasm-ld --libcall-thread-context %t/start.o %t/stack-pointer.o -o %t/fail.wasm 2>&1 | FileCheck %s
+# RUN: llvm-mc -filetype=obj -triple=wasm32-unknown-unknown -o %t.o %s
+# RUN: not wasm-ld --libcall-thread-context %t.o -o %t.wasm 2>&1 | FileCheck %s
 
-# CHECK: stack-pointer.o: object file uses globals for thread context, but --libcall-thread-context was specified
+# CHECK: object file uses globals for thread context, but --libcall-thread-context was specified
 
-#--- start.s
 .globl _start
 _start:
   .functype _start () -> ()
   end_function
 
-#--- stack-pointer.s
 .globaltype __stack_pointer, i32
 
 .globl use_stack_pointer
