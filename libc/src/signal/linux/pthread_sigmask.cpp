b996b4eebbedb3b96b584e4db244b6ce685fae45
Alexey Samsonov <vonosmas@gmail.com>
2026-05-20 15:25:53 -0700

    [libc] Implement pthread_sigmask (#198682)
    * Extract `rt_sigprocmask` syscall wrapper into the
    libc/src/__support/OSUtil/linux/syscall_wrappers/ directory
    * Convert all existing users of this syscall, and simplify the logic
    where applicable.
    * Implement `pthread_sigmask`, which is effectively another POSIX
    wrapper around `rt_sigprocmask` syscall similar to `sigprocmask`

--- libc/src/signal/linux/pthread_sigmask.cpp
+++ libc/src/signal/linux/pthread_sigmask.cpp
@@ -1,31 +1,33 @@
-//===-- Linux implementation of sigprocmask -------------------------------===//
+//===----------------------------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
+///
+/// \file
+/// Linux implementation of pthread_sigmask.
+///
+//===----------------------------------------------------------------------===//
 
-#include "src/signal/sigprocmask.h"
+#include "src/signal/pthread_sigmask.h"
 
 #include "hdr/types/sigset_t.h"
 #include "src/__support/OSUtil/linux/syscall_wrappers/rt_sigprocmask.h"
 #include "src/__support/common.h"
-#include "src/__support/libc_errno.h"
 #include "src/__support/macros/config.h"
-#include "src/signal/linux/signal_utils.h"
 
 namespace LIBC_NAMESPACE_DECL {
 
-LLVM_LIBC_FUNCTION(int, sigprocmask,
+LLVM_LIBC_FUNCTION(int, pthread_sigmask,
                    (int how, const sigset_t *__restrict set,
                     sigset_t *__restrict oldset)) {
   auto result = linux_syscalls::rt_sigprocmask(how, set, oldset);
   if (result.has_value())
     return 0;
 
-  libc_errno = result.error();
-  return -1;
+  return result.error();
 }
 
 } // namespace LIBC_NAMESPACE_DECL
