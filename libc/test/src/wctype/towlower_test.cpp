04678e108fb89fad7cec99d4186ac0062baf9255
Michael Jones <michaelrj@google.com>
2026-05-20 22:37:56 +0000

    fix test header comments, deps, yaml standard.

--- libc/test/src/wctype/towlower_test.cpp
+++ libc/test/src/wctype/towlower_test.cpp
@@ -1,9 +1,14 @@
-//===-- Unittests for towlower --------------------------------------------===//
+//===----------------------------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// Unit tests for towlower.
+///
 //===----------------------------------------------------------------------===//
 
 #include "hdr/wchar_macros.h" // for WEOF
--- libc/test/src/wctype/towupper_test.cpp
+++ libc/test/src/wctype/towupper_test.cpp
@@ -1,9 +1,14 @@
-//===-- Unittests for towupper --------------------------------------------===//
+//===----------------------------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// Unit tests for towupper.
+///
 //===----------------------------------------------------------------------===//
 
 #include "hdr/wchar_macros.h" // for WEOF
