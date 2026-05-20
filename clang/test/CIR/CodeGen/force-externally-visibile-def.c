1cc5e281b5722bbba9291153816c04e717de2ed1
ZakyHermawan <zaky.hermawan9615@gmail.com>
2026-05-21 05:45:27 +0700

    [CIR] fix test
    Signed-off-by: ZakyHermawan <zaky.hermawan9615@gmail.com>

--- clang/test/CIR/CodeGen/force-externally-visibile-def.c
+++ clang/test/CIR/CodeGen/force-externally-visibile-def.c
@@ -1,12 +1,18 @@
-// RUN: %clang_cc1 -fclangir -emit-cir -std=c99 %s -o - | FileCheck %s
+// RUN: %clang_cc1 -fclangir -emit-cir -std=c99 %s -o - | FileCheck %s --check-prefix=CIR
+// RUN: %clang_cc1 -fclangir -emit-llvm -std=c99 %s -o - | FileCheck %s --check-prefix=LLVM_OGCG
+// RUN: %clang_cc1 -emit-llvm -std=c99 %s -o - | FileCheck %s --check-prefix=LLVM_OGCG
 
 inline void my_func() {}
 
 // Force the externally visible definition
 extern inline void my_func();
 
-// CHECK: module {{.*}} attributes {cir.lang = #cir.lang<c>{{.*}} {
-// CHECK-NEXT:   cir.func no_inline no_proto @my_func() attributes {{{.*}}, nothrow} {
-// CHECK-NEXT:     cir.return
-// CHECK-NEXT:   }
-// CHECK-NEXT: }
+// CIR: module {{.*}} attributes {cir.lang = #cir.lang<c>{{.*}} {
+// CIR-NEXT:   cir.func no_inline no_proto{{.*}}@my_func() attributes {{{.*}}, nothrow} {
+// CIR-NEXT:     cir.return
+// CIR-NEXT:   }
+// CIR-NEXT: }
+
+// LLVM_OGCG: define void @my_func(){{.*}}{
+// LLVM_OGCG:   ret void
+// LLVM_OGCG: }
