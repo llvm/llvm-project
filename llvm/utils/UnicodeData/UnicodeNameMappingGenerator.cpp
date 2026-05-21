e97ebf2efc5fc1ab2fc7ca87a140af706b14415c
Charlie Lin <tuug@gmx.us>
2026-05-21 13:55:08 -0500

    Update Unicode data URL in UnicodeNameMappingGenerator.cpp

--- llvm/utils/UnicodeData/UnicodeNameMappingGenerator.cpp
+++ llvm/utils/UnicodeData/UnicodeNameMappingGenerator.cpp
@@ -8,7 +8,7 @@
 //
 // This file is used to generate lib/Support/UnicodeNameToCodepointGenerated.cpp
 // using UnicodeData.txt and NameAliases.txt available at
-// https://unicode.org/Public/draft/ucd/
+// https://unicode.org/Public/18.0.0/ucd/
 //===----------------------------------------------------------------------===//
 
 #include "llvm/ADT/DenseMap.h"
