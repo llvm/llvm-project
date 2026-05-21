d6ec2a9d4a13aa9ef7cadaf3a3bda1bc67d935df
Krzysztof Parzyszek <Krzysztof.Parzyszek@amd.com>
2026-05-21 10:41:38 -0500

    Update test

--- flang/test/Semantics/OpenMP/affected-loops.f90
+++ flang/test/Semantics/OpenMP/affected-loops.f90
@@ -2,7 +2,7 @@
 
 subroutine f
   integer :: i, j, k
-  !$omp do collapse(5)
+  !$omp do collapse(4)
   !$omp tile sizes(2, 2)
   do i = 1, 10
     do j = 1, 10
@@ -12,6 +12,8 @@ subroutine f
   end do
 end
 
+! Check that the k-loop is not affected.
+
 !CHECK: Subprogram scope: f size=12 alignment=4 sourceRange=139 bytes
 !CHECK:   f (Subroutine): HostAssoc => f (Subroutine): Subprogram ()
 !CHECK:   i size=4 offset=0: ObjectEntity type: INTEGER(4)
@@ -20,4 +22,5 @@ end
 !CHECK:   OtherConstruct scope: size=0 alignment=1 sourceRange=98 bytes
 !CHECK:     i (OmpPrivate, OmpPreDetermined): HostAssoc => i size=4 offset=0: ObjectEntity type: INTEGER(4)
 !CHECK:     j (OmpPrivate, OmpPreDetermined): HostAssoc => j size=4 offset=4: ObjectEntity type: INTEGER(4)
-!CHECK:     k (OmpPrivate, OmpPreDetermined): HostAssoc => k size=4 offset=8: ObjectEntity type: INTEGER(4)
+
+!CHECK-NOT:    k {{.*}}OmpPreDetermined
