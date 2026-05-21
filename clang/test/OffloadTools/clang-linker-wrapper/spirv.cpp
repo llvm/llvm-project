f02b08177489d27ab6f1deeccd4bab9e82cc84b1
Plyakhin, Yury <yury.plyakhin@intel.com>
2026-05-21 02:32:36 +0200

    fix

--- clang/test/OffloadTools/clang-linker-wrapper/spirv.cpp
+++ clang/test/OffloadTools/clang-linker-wrapper/spirv.cpp
@@ -1,12 +1,38 @@
 // Verify the ELF packaging of OpenMP SPIR-V device images.
 // REQUIRES: system-linux
-// REQUIRES: spirv-tools
 // REQUIRES: spirv-registered-target
-// RUN: %clangxx -fopenmp -fopenmp-targets=spirv64-intel -nogpulib -o %t %s
+// REQUIRES: x86-registered-target
+
+// RUN: %clang -cc1 %s -triple x86_64-unknown-linux-gnu -aux-triple spirv64-intel \
+// RUN:   -emit-llvm-bc -emit-llvm-uselists -disable-llvm-passes -fopenmp \
+// RUN:   --offload-new-driver --no-offloadlib --offload-targets=spirv64-intel \
+// RUN:   -fcxx-exceptions -fexceptions -x c++ -o %t.bc
+// RUN: %clang -cc1 %s -triple spirv64-intel -aux-triple x86_64-unknown-linux-gnu \
+// RUN:   -emit-obj -fopenmp --offload-new-driver --no-offloadlib \
+// RUN:   -fopenmp-is-target-device -fopenmp-host-ir-file-path %t.bc \
+// RUN:   -fcxx-exceptions -fexceptions -x c++ -o %t.device.o
+// RUN: llvm-offload-binary -o %t.bundle \
+// RUN:   --image=file=%t.device.o,triple=spirv64-intel,arch=generic,kind=openmp
+// RUN: %clang -cc1 -triple x86_64-unknown-linux-gnu -aux-triple spirv64-intel \
+// RUN:   -emit-obj -fopenmp --offload-new-driver --no-offloadlib \
+// RUN:   --offload-targets=spirv64-intel -fembed-offload-object=%t.bundle \
+// RUN:   -x ir %t.bc -o %t.host.o
+// RUN: clang-linker-wrapper --host-triple=x86_64-unknown-linux-gnu \
+// RUN:   --device-linker=spirv64-intel=--allow-partial-linkage \
+// RUN:   --device-linker=spirv64-intel=--create-library \
+// RUN:   --linker-path=/usr/bin/ld %t.host.o -o %t
 // RUN: llvm-objdump --offloading %t | FileCheck -check-prefix=CHECK %s
 
 // CHECK: nested images   1
 // CHECK:   triple          spirv64-intel
+
+// Stub symbols required by the OpenMP offload descriptor registration code
+// emitted by clang-linker-wrapper; avoids linking -lomp/-lomptarget/-lc.
+extern "C" {
+void __tgt_register_lib(void *) {}
+void __tgt_unregister_lib(void *) {}
+int atexit(void (*)(void)) { return 0; }
+}
 
 int main(int argc, char** argv) {
   return 0;
