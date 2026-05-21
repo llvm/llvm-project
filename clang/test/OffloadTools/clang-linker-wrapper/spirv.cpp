6af40e3aa802a6be1333b61c632a486509bcc2c9
Plyakhin, Yury <yury.plyakhin@intel.com>
2026-05-22 01:48:08 +0200

    fixed test

--- clang/test/OffloadTools/clang-linker-wrapper/spirv.cpp
+++ clang/test/OffloadTools/clang-linker-wrapper/spirv.cpp
@@ -1,26 +1,14 @@
 // Verify the ELF packaging of OpenMP SPIR-V device images.
 // REQUIRES: system-linux
+// REQUIRES: spirv-tools
 // REQUIRES: spirv-registered-target
 // REQUIRES: x86-registered-target
 
-// RUN: %clang -cc1 %s -triple x86_64-unknown-linux-gnu -aux-triple spirv64-intel \
-// RUN:   -emit-llvm-bc -emit-llvm-uselists -disable-llvm-passes -fopenmp \
-// RUN:   --offload-new-driver --no-offloadlib --offload-targets=spirv64-intel \
-// RUN:   -fcxx-exceptions -fexceptions -x c++ -o %t.bc
-// RUN: %clang -cc1 %s -triple spirv64-intel -aux-triple x86_64-unknown-linux-gnu \
-// RUN:   -emit-obj -fopenmp --offload-new-driver --no-offloadlib \
-// RUN:   -fopenmp-is-target-device -fopenmp-host-ir-file-path %t.bc \
-// RUN:   -fcxx-exceptions -fexceptions -x c++ -o %t.device.o
-// RUN: llvm-offload-binary -o %t.bundle \
-// RUN:   --image=file=%t.device.o,triple=spirv64-intel,arch=generic,kind=openmp
-// RUN: %clang -cc1 -triple x86_64-unknown-linux-gnu -aux-triple spirv64-intel \
-// RUN:   -emit-obj -fopenmp --offload-new-driver --no-offloadlib \
-// RUN:   --offload-targets=spirv64-intel -fembed-offload-object=%t.bundle \
-// RUN:   -x ir %t.bc -o %t.host.o
-// RUN: clang-linker-wrapper --host-triple=x86_64-unknown-linux-gnu \
-// RUN:   --device-linker=spirv64-intel=--allow-partial-linkage \
-// RUN:   --device-linker=spirv64-intel=--create-library \
-// RUN:   --linker-path=/usr/bin/ld %t.host.o -o %t
+// RUN: %clang -cc1 %s -triple x86_64-unknown-linux-gnu -emit-llvm-bc -o %t.bc
+// RUN: %clang -cc1 %s -triple spirv64-intel -fopenmp-is-target-device -o %t.device.o
+// RUN: llvm-offload-binary -o %t.bundle --image=file=%t.device.o,triple=spirv64-intel,arch=generic,kind=openmp
+// RUN: %clang -cc1 -triple x86_64-unknown-linux-gnu -emit-obj -fembed-offload-object=%t.bundle -x ir %t.bc -o %t.host.o
+// RUN: clang-linker-wrapper --host-triple=x86_64-unknown-linux-gnu --linker-path=/usr/bin/ld %t.host.o -o %t
 // RUN: llvm-objdump --offloading %t | FileCheck -check-prefix=CHECK %s
 
 // CHECK: nested images   1
