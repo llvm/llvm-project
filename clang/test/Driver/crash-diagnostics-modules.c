de2f684b6187a70259c89d90997dec3260ab83ec
Arthur Eubanks <aeubanks@google.com>
2026-05-22 00:00:20 +0000

    make tests work on windows

--- clang/test/Driver/crash-diagnostics-modules.c
+++ clang/test/Driver/crash-diagnostics-modules.c
@@ -1,9 +1,8 @@
-// UNSUPPORTED: system-windows
 // RUN: export LSAN_OPTIONS=detect_leaks=0
-// RUN: rm -rf %t
-// RUN: mkdir %t
-// RUN: not %crash_opt %clang -fcrash-diagnostics-tar=%t.tar -fmodules -fmodules-cache-path=%t/cache -I %S/Inputs -c %s -o /dev/null 2>&1 | FileCheck %s
-// RUN: tar -tf %t.tar | FileCheck %s --check-prefix=TAR
+// RUN: rm -rf %t && mkdir %t
+// RUN: cd %t
+// RUN: not %crash_opt %clang -fcrash-diagnostics-tar=repro.tar -fmodules -fmodules-cache-path=cache -I %S/Inputs -c %s -o /dev/null 2>&1 | FileCheck %s
+// RUN: tar -tf repro.tar | FileCheck %s --check-prefix=TAR
 
 #include "empty.h"
 
--- clang/test/Driver/crash-diagnostics-tar.c
+++ clang/test/Driver/crash-diagnostics-tar.c
@@ -1,12 +1,11 @@
-// UNSUPPORTED: system-windows
 // RUN: export LSAN_OPTIONS=detect_leaks=0
-// RUN: rm -rf %t.tar
-// RUN: not %crash_opt %clang -fcrash-diagnostics-tar=%t.tar -c %s -o /dev/null 2>&1 | FileCheck %s
-// RUN: rm -rf %t
-// RUN: mkdir %t
-// RUN: tar -xf %t.tar -C %t
-// RUN: FileCheck %s --check-prefix=SH < %t/*/crash-diagnostics-tar-*.sh
-// RUN: FileCheck %s --check-prefix=C < %t/*/crash-diagnostics-tar-*.c
+// RUN: rm -rf %t && mkdir %t
+// RUN: cd %t
+// RUN: not %crash_opt %clang -fcrash-diagnostics-tar=repro.tar -c %s -o /dev/null 2>&1 | FileCheck %s
+// RUN: mkdir extract
+// RUN: tar -xf repro.tar -C extract
+// RUN: FileCheck %s --check-prefix=SH < extract/*/crash-diagnostics-tar-*.sh
+// RUN: FileCheck %s --check-prefix=C < extract/*/crash-diagnostics-tar-*.c
 
 #pragma clang __debug parser_crash
 
