76b16e2310b8b49ab858b0e04071bb8f63fe8735
Dylan Fleming <Dylan.Fleming@arm.com>
2026-05-22 15:54:16 +0000

    Move Mathvec abi logic to header
    Removed the temporary ABI symbol macro from expf to a seperate
    header to be reused by other routines

--- libc/src/mathvec/generic/expf.cpp
+++ libc/src/mathvec/generic/expf.cpp
@@ -7,32 +7,13 @@
 //===----------------------------------------------------------------------===//
 
 #include "src/mathvec/expf.h"
-#include "src/__support/common.h"
-#include "src/__support/macros/properties/cpu_features.h"
 #include "src/__support/mathvec/expf.h"
-
-#ifndef LIBC_MATHVEC_EXPF_SYM
-#if defined(LIBC_TARGET_CPU_HAS_AVX512F)
-#define LIBC_MATHVEC_EXPF_SYM_PREFIX "_ZGVeN16v_"
-#elif defined(LIBC_TARGET_CPU_HAS_AVX2)
-#define LIBC_MATHVEC_EXPF_SYM_PREFIX "_ZGVdN8v_"
-#elif defined(LIBC_TARGET_CPU_HAS_AVX)
-#define LIBC_MATHVEC_EXPF_SYM_PREFIX "_ZGVcN8v_"
-#elif defined(LIBC_TARGET_CPU_HAS_SSE2)
-#define LIBC_MATHVEC_EXPF_SYM_PREFIX "_ZGVbN4v_"
-#elif defined(LIBC_TARGET_CPU_HAS_ARM_NEON)
-#define LIBC_MATHVEC_EXPF_SYM_PREFIX "_ZGVnN4v_"
-#else
-#define LIBC_MATHVEC_EXPF_SYM_PREFIX "__"
-#endif // LIBC_TARGET_CPU_HAS_*
-
-#define LIBC_MATHVEC_EXPF_SYM LIBC_MATHVEC_EXPF_SYM_PREFIX "expf"
-#endif // LIBC_MATHVEC_EXPF_SYM
+#include "src/mathvec/abi_prefix.h"
 
 namespace LIBC_NAMESPACE_DECL {
 
 LLVM_LIBC_FUNCTION(cpp::simd<float>, expf, (cpp::simd<float> x),
-                   LIBC_MATHVEC_EXPF_SYM) {
+                   LIBC_VFABI_FLOAT_SYMBOL(expf)) {
   return mathvec::expf(x);
 }
 
