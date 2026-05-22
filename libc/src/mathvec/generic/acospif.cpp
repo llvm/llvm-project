acec2677758e968c02c65ca1a03cf0d8245b3b60
Dylan Fleming <Dylan.Fleming@arm.com>
2026-05-22 19:15:10 +0000

    Add acospif/asinpif as loop over scalar

---
+++ libc/src/mathvec/generic/acospif.cpp
@@ -0,0 +1,20 @@
+//===-- Single-precision SIMD acospif vector function ---------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "src/mathvec/acospif.h"
+#include "src/__support/mathvec/acospif.h"
+#include "src/mathvec/abi_prefix.h"
+
+namespace LIBC_NAMESPACE_DECL {
+
+LLVM_LIBC_FUNCTION(cpp::simd<float>, acospif, (cpp::simd<float> x),
+                   LIBC_VFABI_FLOAT_SYMBOL(acospif)) {
+  return mathvec::acospif(x);
+}
+
+} // namespace LIBC_NAMESPACE_DECL
---
+++ libc/test/src/mathvec/acospif_test.cpp
@@ -0,0 +1,56 @@
+//===-- Unittests for acospif ---------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "hdr/math_macros.h"
+#include "src/__support/CPP/simd.h"
+#include "src/__support/FPUtil/FPBits.h"
+#include "src/math/acospif.h"
+#include "src/mathvec/acospif.h"
+#include "test/UnitTest/SIMDMatcher.h"
+#include "test/UnitTest/Test.h"
+#include "test/src/mathvec/UnitTestWrappers.h"
+
+#include "hdr/stdint_proxy.h"
+
+using LlvmLibcVecAcospifTest = LIBC_NAMESPACE::testing::FPTest<float>;
+
+using AcospifOp =
+    LIBC_NAMESPACE::testing::mathvec::UnaryOp<float, LIBC_NAMESPACE::acospif,
+                                              LIBC_NAMESPACE::acospif>;
+using LIBC_NAMESPACE::cpp::splat;
+using LIBC_NAMESPACE::testing::mathvec::wrap_ref;
+using LIBC_NAMESPACE::testing::mathvec::wrap_vector;
+
+TEST_F(LlvmLibcVecAcospifTest, SpecialNumbers) {
+  EXPECT_SIMD_EQ(splat(aNaN), wrap_vector<AcospifOp>(aNaN));
+
+  EXPECT_SIMD_EQ(splat(aNaN), wrap_vector<AcospifOp>(inf));
+
+  EXPECT_SIMD_EQ(splat(aNaN), wrap_vector<AcospifOp>(neg_inf));
+
+  EXPECT_SIMD_EQ(splat(aNaN), wrap_vector<AcospifOp>(2.0f));
+
+  EXPECT_SIMD_EQ(splat(aNaN), wrap_vector<AcospifOp>(-2.0f));
+
+  EXPECT_SIMD_EQ(splat(0.5f), wrap_vector<AcospifOp>(0.0f));
+
+  EXPECT_SIMD_EQ(splat(0.5f), wrap_vector<AcospifOp>(-0.0f));
+
+  EXPECT_SIMD_EQ(splat(0.0f), wrap_vector<AcospifOp>(1.0f));
+
+  EXPECT_SIMD_EQ(splat(1.0f), wrap_vector<AcospifOp>(-1.0f));
+}
+
+TEST_F(LlvmLibcVecAcospifTest, InFloatRange) {
+  constexpr uint32_t COUNT = 100'000;
+  constexpr uint32_t STEP = 0x7f80'0000U / COUNT;
+  for (uint32_t i = 0, v = 0; i <= COUNT; ++i, v += STEP) {
+    float x = FPBits(v).get_val();
+    TEST_VARIED_CASES(x, AcospifOp);
+  }
+}
