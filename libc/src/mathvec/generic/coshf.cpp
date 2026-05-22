22c637ee26e4dabed3b3a6fae8d34e6e6867f9d1
Dylan Fleming <Dylan.Fleming@arm.com>
2026-05-22 19:22:54 +0000

    Add coshf/sinhf/tanhf as loop over scalar

---
+++ libc/src/mathvec/generic/coshf.cpp
@@ -0,0 +1,20 @@
+//===-- Single-precision SIMD coshf vector function -----------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "src/mathvec/coshf.h"
+#include "src/__support/mathvec/coshf.h"
+#include "src/mathvec/abi_prefix.h"
+
+namespace LIBC_NAMESPACE_DECL {
+
+LLVM_LIBC_FUNCTION(cpp::simd<float>, coshf, (cpp::simd<float> x),
+                   LIBC_VFABI_FLOAT_SYMBOL(coshf)) {
+  return mathvec::coshf(x);
+}
+
+} // namespace LIBC_NAMESPACE_DECL
---
+++ libc/src/mathvec/generic/sinhf.cpp
@@ -0,0 +1,20 @@
+//===-- Single-precision SIMD sinhf vector function -----------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "src/mathvec/sinhf.h"
+#include "src/__support/mathvec/sinhf.h"
+#include "src/mathvec/abi_prefix.h"
+
+namespace LIBC_NAMESPACE_DECL {
+
+LLVM_LIBC_FUNCTION(cpp::simd<float>, sinhf, (cpp::simd<float> x),
+                   LIBC_VFABI_FLOAT_SYMBOL(sinhf)) {
+  return mathvec::sinhf(x);
+}
+
+} // namespace LIBC_NAMESPACE_DECL
