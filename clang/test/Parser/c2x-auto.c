686d0cdbdced9c3224935549b3c695129d14e80b
Osama Abdelkader <osama.abdelkader@gmail.com>
2026-05-23 19:51:47 +0200

    add c2x auto with typedef specifier
    Signed-off-by: Osama Abdelkader <osama.abdelkader@gmail.com>

--- clang/test/Parser/c2x-auto.c
+++ clang/test/Parser/c2x-auto.c
@@ -167,6 +167,13 @@ void t1() {
   int auto d2 = 0;
 }
 
+void typedef_type_specifiers(void) {
+  typedef int MyInt;
+
+  auto MyInt a = 0;
+  MyInt auto b = 0;
+}
+
 void t2() {
   auto long long a1 = 0;
   long auto long a2 = 0;
