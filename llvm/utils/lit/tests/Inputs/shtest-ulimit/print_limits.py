80fff3c38cb60f6fa13b6afa4df551ee07d56c95
Miro Hrončok <miro@hroncok.cz>
2026-05-25 21:39:46 +0200

    Fixup: Non need to call str() on already always str result

--- llvm/utils/lit/tests/Inputs/shtest-ulimit/print_limits.py
+++ llvm/utils/lit/tests/Inputs/shtest-ulimit/print_limits.py
@@ -9,14 +9,7 @@ def normalize_limit(limit_value):
     return "infinity" if limit_value == resource.RLIM_INFINITY else str(limit_value)
 
 
-print("RLIMIT_AS=" + str(normalize_limit(resource.getrlimit(resource.RLIMIT_AS)[0])))
-print(
-    "RLIMIT_NOFILE="
-    + str(normalize_limit(resource.getrlimit(resource.RLIMIT_NOFILE)[0]))
-)
-print(
-    "RLIMIT_STACK=" + str(normalize_limit(resource.getrlimit(resource.RLIMIT_STACK)[0]))
-)
-print(
-    "RLIMIT_FSIZE=" + str(normalize_limit(resource.getrlimit(resource.RLIMIT_FSIZE)[0]))
-)
+print("RLIMIT_AS=" + normalize_limit(resource.getrlimit(resource.RLIMIT_AS)[0]))
+print("RLIMIT_NOFILE=" + normalize_limit(resource.getrlimit(resource.RLIMIT_NOFILE)[0]))
+print("RLIMIT_STACK=" + normalize_limit(resource.getrlimit(resource.RLIMIT_STACK)[0]))
+print("RLIMIT_FSIZE=" + normalize_limit(resource.getrlimit(resource.RLIMIT_FSIZE)[0]))
