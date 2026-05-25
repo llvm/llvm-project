b741afed58a69460bbe4d9979f8143993fd801cf
Zibi Sarbinowski <zibi@ca.ibm.com>
2026-05-25 16:08:25 -0400

    Revert "Guard init_priority attribute within libc++"
    This reverts commit 2c7e24c4b6893a93ddb2b2cca91eaf5bf7956965.
    
     Conflicts:
    	clang/include/clang/Basic/Attr.td
    	clang/test/SemaCXX/init-priority-attr.cpp
    	libcxx/include/__config
    	libcxx/src/experimental/memory_resource.cpp
    	libcxx/src/iostream.cpp

--- libcxx/src/iostream_init.h
+++ libcxx/src/iostream_init.h
@@ -1,2 +1,2 @@
 #pragma GCC system_header
-_LIBCPP_HIDDEN ios_base::Init __start_std_streams _LIBCPP_INIT_PRIORITY_MAX;
+_LIBCPP_HIDDEN ios_base::Init __start_std_streams __attribute__((init_priority(101)));
