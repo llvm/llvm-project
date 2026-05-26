509f332308ecc9168bc44697ace944dd59155d21
Alex Richardson <alexrichardson@google.com>
2026-05-26 14:38:01 -0700

    [𝘀𝗽𝗿] initial version
    Created using spr 1.3.8-beta.1

--- llvm/lib/Target/RISCV/MCTargetDesc/RISCVBaseInfo.h
+++ llvm/lib/Target/RISCV/MCTargetDesc/RISCVBaseInfo.h
@@ -47,6 +47,7 @@ enum OperandType : unsigned {
   OPERAND_UIMM7,
   OPERAND_UIMM7_LSB00,
   OPERAND_UIMM7_LSB000,
+  OPERAND_UIMM7_SRLIY,
   OPERAND_UIMM8_LSB00,
   OPERAND_UIMM8,
   OPERAND_UIMM8_LSB000,
@@ -888,6 +889,12 @@ struct NDSVLNPseudo {
 #define GET_RISCVVSXTable_DECL
 #define GET_RISCVNDSVLNTable_DECL
 #include "RISCVGenSearchableTables.inc"
+
+inline bool isValidYBNDSWImm(int64_t Imm) {
+  return (Imm >= 1 && Imm <= 255) ||
+         (Imm >= 256 && Imm <= 508 && (Imm % 8) == 0) ||
+         (Imm >= 512 && Imm <= 4096 && (Imm % 16) == 0);
+}
 } // namespace RISCV
 
 } // namespace llvm
