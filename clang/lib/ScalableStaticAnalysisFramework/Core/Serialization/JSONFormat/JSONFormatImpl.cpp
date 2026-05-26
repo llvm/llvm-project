CSharperMantle/llvm-project-bolt

Reapply "[clang][ssaf][NFC] Rework how the Force linker anchors are defined and used" (#193451)
This reverts commit 582958c4337f539e650096c0257a322315298e1a.

clang/lib/ScalableStaticAnalysisFramework/Core/Serialization/JSONFormat/JSONFormatImpl.cpp
@@ -11,16 +11,21 @@
 #include "clang/ScalableStaticAnalysisFramework/Core/Serialization/SerializationFormatRegistry.h"
 #include "llvm/Support/Registry.h"
 
-// NOLINTNEXTLINE(misc-use-internal-linkage)
-volatile int SSAFJSONFormatAnchorSource = 0;
-LLVM_INSTANTIATE_REGISTRY(llvm::Registry<clang::ssaf::JSONFormat::FormatInfo>)
-LLVM_INSTANTIATE_REGISTRY(
-    llvm::Registry<clang::ssaf::JSONFormat::AnalysisResultRegistry::Codec>)
+using namespace clang;
+using namespace ssaf;
 
-static clang::ssaf::SerializationFormatRegistry::Add<clang::ssaf::JSONFormat>
+LLVM_INSTANTIATE_REGISTRY(
+    llvm::Registry<JSONFormat::AnalysisResultRegistry::Codec>)
+
+LLVM_INSTANTIATE_REGISTRY(llvm::Registry<JSONFormat::FormatInfo>)
+
+static SerializationFormatRegistry::Add<JSONFormat>
     RegisterJSONFormat("json", "JSON serialization format");
 
 namespace clang::ssaf {
+
+// NOLINTNEXTLINE(misc-use-internal-linkage)
+const volatile int JSONFormatAnchorSource = 0;
 
 //----------------------------------------------------------------------------
 // JSON Reader and Writer

clang/lib/ScalableStaticAnalysisFramework/Core/WholeProgramAnalysis/AnalysisRegistry.cpp
@@ -13,11 +13,12 @@
 using namespace clang;
 using namespace ssaf;
 
-using RegistryT = llvm::Registry<AnalysisBase>;
-
+namespace clang::ssaf {
 // NOLINTNEXTLINE(misc-use-internal-linkage)
-volatile int SSAFAnalysisRegistryAnchorSource = 0;
-LLVM_INSTANTIATE_REGISTRY(RegistryT)
+const volatile int AnalysisRegistryAnchorSource = 0;
+} // namespace clang::ssaf
+
+LLVM_INSTANTIATE_REGISTRY(llvm::Registry<AnalysisBase>)
 
 std::vector<AnalysisName> &AnalysisRegistry::getAnalysisNames() {
   static std::vector<AnalysisName> Names;
@@ -34,7 +35,7 @@ const std::vector<AnalysisName> &AnalysisRegistry::names() {
 
 llvm::Expected<std::unique_ptr<AnalysisBase>>
 AnalysisRegistry::instantiate(const AnalysisName &Name) {
-  for (const auto &Entry : RegistryT::entries()) {
+  for (const auto &Entry : llvm::Registry<AnalysisBase>::entries()) {
     if (Entry.getName() == Name.str()) {
       return std::unique_ptr<AnalysisBase>(Entry.instantiate());
     }

llvm/llvm-project

clang/lib/ScalableStaticAnalysisFramework/Core/Serialization/JSONFormat/JSONFormatImpl.cpp
@@ -11,16 +11,20 @@
 #include "clang/ScalableStaticAnalysisFramework/Core/Serialization/SerializationFormatRegistry.h"
 #include "llvm/Support/Registry.h"
 
-// NOLINTNEXTLINE(misc-use-internal-linkage)
-volatile int SSAFJSONFormatAnchorSource = 0;
-LLVM_DEFINE_REGISTRY(llvm::Registry<clang::ssaf::JSONFormat::FormatInfo>)
-LLVM_DEFINE_REGISTRY(
-    llvm::Registry<clang::ssaf::JSONFormat::AnalysisResultRegistry::Codec>)
+using namespace clang;
+using namespace ssaf;
 
-static clang::ssaf::SerializationFormatRegistry::Add<clang::ssaf::JSONFormat>
+LLVM_DEFINE_REGISTRY(llvm::Registry<JSONFormat::FormatInfo>)
+LLVM_DEFINE_REGISTRY(
+    llvm::Registry<JSONFormat::AnalysisResultRegistry::Codec>)
+
+static SerializationFormatRegistry::Add<JSONFormat>
     RegisterJSONFormat("json", "JSON serialization format");
 
 namespace clang::ssaf {
+
+// NOLINTNEXTLINE(misc-use-internal-linkage)
+const volatile int JSONFormatAnchorSource = 0;
 
 //----------------------------------------------------------------------------
 // JSON Reader and Writer

clang/lib/ScalableStaticAnalysisFramework/Core/WholeProgramAnalysis/AnalysisRegistry.cpp
@@ -13,11 +13,12 @@
 using namespace clang;
 using namespace ssaf;
 
-using RegistryT = llvm::Registry<AnalysisBase>;
-
+namespace clang::ssaf {
 // NOLINTNEXTLINE(misc-use-internal-linkage)
-volatile int SSAFAnalysisRegistryAnchorSource = 0;
-LLVM_DEFINE_REGISTRY(RegistryT)
+const volatile int AnalysisRegistryAnchorSource = 0;
+} // namespace clang::ssaf
+
+LLVM_DEFINE_REGISTRY(llvm::Registry<AnalysisBase>)
 
 std::vector<AnalysisName> &AnalysisRegistry::getAnalysisNames() {
   static std::vector<AnalysisName> Names;
@@ -34,7 +35,7 @@ const std::vector<AnalysisName> &AnalysisRegistry::names() {
 
 llvm::Expected<std::unique_ptr<AnalysisBase>>
 AnalysisRegistry::instantiate(const AnalysisName &Name) {
-  for (const auto &Entry : RegistryT::entries()) {
+  for (const auto &Entry : llvm::Registry<AnalysisBase>::entries()) {
     if (Entry.getName() == Name.str()) {
       return std::unique_ptr<AnalysisBase>(Entry.instantiate());
     }