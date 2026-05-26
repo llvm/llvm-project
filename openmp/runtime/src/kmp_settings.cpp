a6331561718937e7845bf8dc9bfd12f57f0d175b
Ryotaro Kasuga <kasuga.ryotaro@fujitsu.com>
2026-05-26 08:39:43 +0000

    [libomp] Set `KMP_USE_YIELD=0` when `OMP_WAIT_POLICY=active`

--- openmp/runtime/src/kmp_settings.cpp
+++ openmp/runtime/src/kmp_settings.cpp
@@ -807,6 +807,7 @@ static void __kmp_stg_print_inherit_fp_control(kmp_str_buf_t *buffer,
 
 // Used for OMP_WAIT_POLICY
 static char const *blocktime_str = NULL;
+static char const *use_yield_str = NULL;
 
 // -----------------------------------------------------------------------------
 // KMP_LIBRARY, OMP_WAIT_POLICY
@@ -828,6 +829,10 @@ static void __kmp_stg_parse_wait_policy(char const *name, char const *value,
       if (blocktime_str == NULL) {
         // KMP_BLOCKTIME not specified, so set default to "infinite".
         __kmp_dflt_blocktime = KMP_MAX_BLOCKTIME;
+      }
+      if (use_yield_str == NULL) {
+        // KMP_USE_YIELD not specified, so set default to 0.
+        __kmp_use_yield = 0;
       }
     } else if (__kmp_str_match("PASSIVE", 1, value)) {
       __kmp_library = library_throughput;
@@ -6147,8 +6152,10 @@ void __kmp_env_initialize(char const *string) {
     }
   }
 
-  // We need to know if blocktime was set when processing OMP_WAIT_POLICY
+  // We need to know if blocktime and use_yield were set when processing
+  // OMP_WAIT_POLICY
   blocktime_str = __kmp_env_blk_var(&block, "KMP_BLOCKTIME");
+  use_yield_str = __kmp_env_blk_var(&block, "KMP_USE_YIELD");
 
   // Special case. If we parse environment, not a string, process KMP_WARNINGS
   // first.
