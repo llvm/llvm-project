e51529c7182144bdf503cb1e1d34b60a9f96a895
Craig Topper <craig.topper@sifive.com>
2026-05-25 23:42:33 -0700

    Update llvm/lib/Target/RISCV/GISel/RISCVLegalizerInfo.cpp

--- llvm/lib/Target/RISCV/GISel/RISCVLegalizerInfo.cpp
+++ llvm/lib/Target/RISCV/GISel/RISCVLegalizerInfo.cpp
@@ -958,7 +958,7 @@ bool RISCVLegalizerInfo::legalizeVScale(MachineInstr &MI,
     uint64_t Log2 = Log2_64(Val);
     if (Log2 < 3) {
       auto VLENB = MIB.buildInstr(RISCV::G_READ_VLENB, {sXLen}, {});
-      MIB.buildLShr(Dst, VLENB, MIB.buildConstant(XLenTy, 3 - Log2),
+      MIB.buildLShr(Dst, VLENB, MIB.buildConstant(sXLen, 3 - Log2),
                     MachineInstr::IsExact);
     } else if (Log2 > 3) {
       auto VLENB = MIB.buildInstr(RISCV::G_READ_VLENB, {sXLen}, {});
