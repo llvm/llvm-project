9a132171351daf4aaa93b7022097ed571b55bc25
Evgeny Leviant <eleviant@accesssoftek.com>
2026-05-27 18:37:41 +0200

    Add test for -ftrapv

--- clang/test/CodeGen/wrapv.c
+++ clang/test/CodeGen/wrapv.c
@@ -3,9 +3,12 @@
 // RUN: %clang_cc1 -fno-wrapv -fms-compatibility -emit-llvm %s -o - | FileCheck %s --check-prefix=NOWRAP
 // RUN: %clang_cc1 -fwrapv -emit-llvm %s -o - | FileCheck %s --check-prefix=WRAP
 // RUN: %clang_cc1 -fms-compatibility -emit-llvm %s -o - | FileCheck %s --check-prefix=WRAP
+// RUN: %clang_cc1 -ftrapv -fms-compatibility -emit-llvm %s -o - | FileCheck %s --check-prefix=TRAP
+// RUN: %clang_cc1 -ftrapv -fwrapv -emit-llvm %s -o - | FileCheck %s --check-prefix=TRAP
 
 // NOWRAP: %add = add nsw i32 %0, 1
 // WRAP: %add = add i32 %0, 1
+// TRAP: llvm.sadd.with.overflow
 
 int add1(int x) {
   return x + 1;
