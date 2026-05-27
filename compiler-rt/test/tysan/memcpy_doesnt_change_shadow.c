e78b6a13191e86fc966011975dad6350505e3d57
gbMattN <matthew.nagy@sony.com>
2026-05-27 11:59:34 +0100

    [TySan] Remove incorrect memcpy shadow memory tracking

---
+++ compiler-rt/test/tysan/memcpy_doesnt_change_shadow.c
@@ -0,0 +1,25 @@
+// RUN: %clang_tysan -O0 %s -o %t && %run %t >%t.out 2>&1
+// RUN: FileCheck %s < %t.out
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+  int i = 0;
+  float f = 5.0f;
+
+  *(float *)&i = 3.0f;
+  *(int *)&f = 2;
+  // CHECK: WRITE of size 4 at 0x{{.*}} with type float accesses an existing object of type int
+  // CHECK: WRITE of size 4 at 0x{{.*}} with type int accesses an existing object of type float
+
+  memcpy(&i, &f, sizeof(int));
+
+  *(float *)&i = 3.0f;
+  *(int *)&f = 2;
+  // CHECK: WRITE of size 4 at 0x{{.*}} with type int accesses an existing object of type float
+  // CHECK: WRITE of size 4 at 0x{{.*}} with type float accesses an existing object of type int
+
+  return 0;
+}
