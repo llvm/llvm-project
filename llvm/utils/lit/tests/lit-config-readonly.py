5954e212a9138c5a18219ba3c1f486e89114467e
Vitaly Buka <vitalybuka@google.com>
2026-05-26 22:48:06 -0700

    feedback addressed
    Created using spr 1.3.7

--- llvm/utils/lit/tests/lit-config-readonly.py
+++ llvm/utils/lit/tests/lit-config-readonly.py
@@ -1,4 +1,4 @@
-# RUN: not %{lit} %{inputs}/lit-config-readonly > %t.out 2> %t.err
+# RUN: not %{lit} %{inputs}/lit-config-readonly 2> %t.err
 # RUN: FileCheck --check-prefix=CHECK-ERR < %t.err %s
 
 # CHECK-ERR: AttributeError: lit_config.maxIndividualTestTime is read-only. Use config.maxIndividualTestTime instead.
