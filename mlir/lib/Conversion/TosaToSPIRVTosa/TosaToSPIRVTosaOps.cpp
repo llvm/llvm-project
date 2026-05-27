0083c9423f9795a5c20f338bbeef3535b90a5fa9
Davide Grohmann <davide.grohmann@arm.com>
2026-05-27 17:40:33 +0200

    [mlir][spirv][tosa] Extend TOSA to SPIR-V TOSA op conversion
    Add conversion patterns for additional TOSA 1.0 operations targeting
    the SPIR-V TOSA extended instruction set.
    
    The newly covered operations include:
    * elementwise ops such as clamp, arithmetic_right_shift, mul, table,
      negate, and select
    * reductions and argmax
    * gather, scatter, resize
    * reshape, reverse, slice, tile, transpose
    * cast and const_shape
    
    Also add conversion tests.
    
    Change-Id: Ife2286c50cece52821037c950baf5885e7fa6931
    Signed-off-by: Davide Grohmann <davide.grohmann@arm.com>

--- mlir/lib/Conversion/TosaToSPIRVTosa/TosaToSPIRVTosaOps.cpp
+++ mlir/lib/Conversion/TosaToSPIRVTosa/TosaToSPIRVTosaOps.cpp
@@ -26,6 +26,19 @@ spirv::TosaExtNaNPropagationModeType getNanMode(OpAdaptor adaptor) {
       adaptor.getNanMode());
 }
 
+template <typename OpAdaptor>
+spirv::TosaExtResizeModeType getResizeMode(OpAdaptor adaptor) {
+  return static_cast<spirv::TosaExtResizeModeType>(adaptor.getMode());
+}
+
+DenseIntElementsAttr getI32TensorArmAttr(ArrayRef<int32_t> values,
+                                         ConversionPatternRewriter &rewriter) {
+  return DenseIntElementsAttr::get(
+      spirv::TensorArmType::get(static_cast<int64_t>(values.size()),
+                                IntegerType::get(rewriter.getContext(), 32)),
+      values);
+}
+
 template <typename SourceOp, typename Replacer>
 struct TosaOpConvert final : public OpConversionPattern<SourceOp> {
   using OpConversionPattern<SourceOp>::OpConversionPattern;
@@ -83,15 +96,193 @@ struct BinaryNanModeElementwiseReplace {
   }
 };
 
+template <typename TargetOp>
+struct ReductionReplace {
+  template <typename SourceOp>
+  static LogicalResult replace(SourceOp op, typename SourceOp::Adaptor adaptor,
+                               Type type, ConversionPatternRewriter &rewriter) {
+    rewriter.replaceOpWithNewOp<TargetOp>(op, type, adaptor.getAxis(),
+                                          adaptor.getInput());
+    return success();
+  }
+};
+
+template <typename TargetOp>
+struct NanModeReductionReplace {
+  template <typename SourceOp>
+  static LogicalResult replace(SourceOp op, typename SourceOp::Adaptor adaptor,
+                               Type type, ConversionPatternRewriter &rewriter) {
+    rewriter.replaceOpWithNewOp<TargetOp>(
+        op, type, adaptor.getAxis(), getNanMode(adaptor), adaptor.getInput());
+    return success();
+  }
+};
+
+struct ClampReplace {
+  static LogicalResult replace(tosa::ClampOp op, tosa::ClampOpAdaptor adaptor,
+                               Type type, ConversionPatternRewriter &rewriter) {
+    rewriter.replaceOpWithNewOp<spirv::TosaClampOp>(
+        op, type, adaptor.getMinVal(), adaptor.getMaxVal(), getNanMode(adaptor),
+        adaptor.getInput());
+    return success();
+  }
+};
+
+struct ArithmeticRightShiftReplace {
+  static LogicalResult replace(tosa::ArithmeticRightShiftOp op,
+                               tosa::ArithmeticRightShiftOpAdaptor adaptor,
+                               Type type, ConversionPatternRewriter &rewriter) {
+    rewriter.replaceOpWithNewOp<spirv::TosaArithmeticRightShiftOp>(
+        op, type, adaptor.getRound(), adaptor.getInput1(), adaptor.getInput2());
+    return success();
+  }
+};
+
+struct MulReplace {
+  static LogicalResult replace(tosa::MulOp op, tosa::MulOpAdaptor adaptor,
+                               Type type, ConversionPatternRewriter &rewriter) {
+    rewriter.replaceOpWithNewOp<spirv::TosaMulOp>(
+        op, type, adaptor.getInput1(), adaptor.getInput2(), adaptor.getShift());
+    return success();
+  }
+};
+
+struct TableReplace {
+  static LogicalResult replace(tosa::TableOp op, tosa::TableOpAdaptor adaptor,
+                               Type type, ConversionPatternRewriter &rewriter) {
+    rewriter.replaceOpWithNewOp<spirv::TosaTableOp>(
+        op, type, adaptor.getInput1(), adaptor.getTable());
+    return success();
+  }
+};
+
+struct NegateReplace {
+  static LogicalResult replace(tosa::NegateOp op, tosa::NegateOpAdaptor adaptor,
+                               Type type, ConversionPatternRewriter &rewriter) {
+    rewriter.replaceOpWithNewOp<spirv::TosaNegateOp>(
+        op, type, adaptor.getInput1(), adaptor.getInput1Zp(),
+        adaptor.getOutputZp());
+    return success();
+  }
+};
+
+struct SelectReplace {
+  static LogicalResult replace(tosa::SelectOp op, tosa::SelectOpAdaptor adaptor,
+                               Type type, ConversionPatternRewriter &rewriter) {
+    rewriter.replaceOpWithNewOp<spirv::TosaSelectOp>(
+        op, type, adaptor.getInput1(), adaptor.getInput2(),
+        adaptor.getInput3());
+    return success();
+  }
+};
+
+struct ReshapeReplace {
+  static LogicalResult replace(tosa::ReshapeOp op,
+                               tosa::ReshapeOpAdaptor adaptor, Type type,
+                               ConversionPatternRewriter &rewriter) {
+    rewriter.replaceOpWithNewOp<spirv::TosaReshapeOp>(
+        op, type, adaptor.getInput1(), adaptor.getShape());
+    return success();
+  }
+};
+
+struct ReverseReplace {
+  static LogicalResult replace(tosa::ReverseOp op,
+                               tosa::ReverseOpAdaptor adaptor, Type type,
+                               ConversionPatternRewriter &rewriter) {
+    rewriter.replaceOpWithNewOp<spirv::TosaReverseOp>(
+        op, type, adaptor.getAxis(), adaptor.getInput1());
+    return success();
+  }
+};
+
+struct SliceReplace {
+  static LogicalResult replace(tosa::SliceOp op, tosa::SliceOpAdaptor adaptor,
+                               Type type, ConversionPatternRewriter &rewriter) {
+    rewriter.replaceOpWithNewOp<spirv::TosaSliceOp>(
+        op, type, adaptor.getInput1(), adaptor.getStart(), adaptor.getSize());
+    return success();
+  }
+};
+
+struct TileReplace {
+  static LogicalResult replace(tosa::TileOp op, tosa::TileOpAdaptor adaptor,
+                               Type type, ConversionPatternRewriter &rewriter) {
+    rewriter.replaceOpWithNewOp<spirv::TosaTileOp>(
+        op, type, adaptor.getInput1(), adaptor.getMultiples());
+    return success();
+  }
+};
+
+struct TransposeReplace {
+  static LogicalResult replace(tosa::TransposeOp op,
+                               tosa::TransposeOpAdaptor adaptor, Type type,
+                               ConversionPatternRewriter &rewriter) {
+    DenseIntElementsAttr perms =
+        getI32TensorArmAttr(adaptor.getPerms(), rewriter);
+    rewriter.replaceOpWithNewOp<spirv::TosaTransposeOp>(op, type, perms,
+                                                        adaptor.getInput1());
+    return success();
+  }
+};
+
+struct GatherReplace {
+  static LogicalResult replace(tosa::GatherOp op, tosa::GatherOpAdaptor adaptor,
+                               Type type, ConversionPatternRewriter &rewriter) {
+    rewriter.replaceOpWithNewOp<spirv::TosaGatherOp>(
+        op, type, adaptor.getValues(), adaptor.getIndices());
+    return success();
+  }
+};
+
+struct ScatterReplace {
+  static LogicalResult replace(tosa::ScatterOp op,
+                               tosa::ScatterOpAdaptor adaptor, Type type,
+                               ConversionPatternRewriter &rewriter) {
+    rewriter.replaceOpWithNewOp<spirv::TosaScatterOp>(
+        op, type, adaptor.getValuesIn(), adaptor.getIndices(),
+        adaptor.getInput());
+    return success();
+  }
+};
+
+struct ResizeReplace {
+  static LogicalResult replace(tosa::ResizeOp op, tosa::ResizeOpAdaptor adaptor,
+                               Type type, ConversionPatternRewriter &rewriter) {
+    rewriter.replaceOpWithNewOp<spirv::TosaResizeOp>(
+        op, type, getResizeMode(adaptor), adaptor.getInput(),
+        adaptor.getScale(), adaptor.getOffset(), adaptor.getBorder());
+    return success();
+  }
+};
+
+struct ConstShapeReplace {
+  static LogicalResult replace(tosa::ConstShapeOp op,
+                               tosa::ConstShapeOpAdaptor adaptor, Type type,
+                               ConversionPatternRewriter &rewriter) {
+    SmallVector<int32_t> values;
+    for (const APInt &value : adaptor.getValues().getValues<APInt>())
+      values.push_back(value.getSExtValue());
+
+    rewriter.replaceOpWithNewOp<spirv::ConstantOp>(
+        op, type, getI32TensorArmAttr(values, rewriter));
+    return success();
+  }
+};
+
 } // namespace
 
 void populateTosaToSPIRVTosaOpsConversionPatterns(
     SPIRVTypeConverter &typeConverter, RewritePatternSet &patterns) {
   patterns.add<
+      TosaOpConvert<tosa::ArgMaxOp,
+                    NanModeReductionReplace<spirv::TosaArgMaxOp>>,
+      TosaOpConvert<tosa::ClampOp, ClampReplace>,
       TosaOpConvert<tosa::ErfOp, UnaryInputReplace<spirv::TosaErfOp>>,
       TosaOpConvert<tosa::SigmoidOp, UnaryInputReplace<spirv::TosaSigmoidOp>>,
       TosaOpConvert<tosa::TanhOp, UnaryInputReplace<spirv::TosaTanhOp>>,
       TosaOpConvert<tosa::AddOp, BinaryElementwiseReplace<spirv::TosaAddOp>>,
+      TosaOpConvert<tosa::ArithmeticRightShiftOp, ArithmeticRightShiftReplace>,
       TosaOpConvert<tosa::BitwiseAndOp,
                     BinaryElementwiseReplace<spirv::TosaBitwiseAndOp>>,
       TosaOpConvert<tosa::BitwiseOrOp,
@@ -114,8 +305,10 @@ void populateTosaToSPIRVTosaOpsConversionPatterns(
                     BinaryNanModeElementwiseReplace<spirv::TosaMaximumOp>>,
       TosaOpConvert<tosa::MinimumOp,
                     BinaryNanModeElementwiseReplace<spirv::TosaMinimumOp>>,
+      TosaOpConvert<tosa::MulOp, MulReplace>,
       TosaOpConvert<tosa::PowOp, BinaryElementwiseReplace<spirv::TosaPowOp>>,
       TosaOpConvert<tosa::SubOp, BinaryElementwiseReplace<spirv::TosaSubOp>>,
+      TosaOpConvert<tosa::TableOp, TableReplace>,
       TosaOpConvert<tosa::AbsOp, UnaryInput1Replace<spirv::TosaAbsOp>>,
       TosaOpConvert<tosa::BitwiseNotOp,
                     UnaryInput1Replace<spirv::TosaBitwiseNotOp>>,
@@ -127,16 +320,40 @@ void populateTosaToSPIRVTosaOpsConversionPatterns(
       TosaOpConvert<tosa::LogOp, UnaryInput1Replace<spirv::TosaLogOp>>,
       TosaOpConvert<tosa::LogicalNotOp,
                     UnaryInput1Replace<spirv::TosaLogicalNotOp>>,
+      TosaOpConvert<tosa::NegateOp, NegateReplace>,
       TosaOpConvert<tosa::ReciprocalOp,
                     UnaryInput1Replace<spirv::TosaReciprocalOp>>,
       TosaOpConvert<tosa::RsqrtOp, UnaryInput1Replace<spirv::TosaRsqrtOp>>,
       TosaOpConvert<tosa::SinOp, UnaryInput1Replace<spirv::TosaSinOp>>,
+      TosaOpConvert<tosa::SelectOp, SelectReplace>,
       TosaOpConvert<tosa::EqualOp,
                     BinaryElementwiseReplace<spirv::TosaEqualOp>>,
       TosaOpConvert<tosa::GreaterOp,
                     BinaryElementwiseReplace<spirv::TosaGreaterOp>>,
       TosaOpConvert<tosa::GreaterEqualOp,
-                    BinaryElementwiseReplace<spirv::TosaGreaterEqualOp>>>(
+                    BinaryElementwiseReplace<spirv::TosaGreaterEqualOp>>,
+      TosaOpConvert<tosa::ReduceAllOp,
+                    ReductionReplace<spirv::TosaReduceAllOp>>,
+      TosaOpConvert<tosa::ReduceAnyOp,
+                    ReductionReplace<spirv::TosaReduceAnyOp>>,
+      TosaOpConvert<tosa::ReduceMaxOp,
+                    NanModeReductionReplace<spirv::TosaReduceMaxOp>>,
+      TosaOpConvert<tosa::ReduceMinOp,
+                    NanModeReductionReplace<spirv::TosaReduceMinOp>>,
+      TosaOpConvert<tosa::ReduceProductOp,
+                    ReductionReplace<spirv::TosaReduceProductOp>>,
+      TosaOpConvert<tosa::ReduceSumOp,
+                    ReductionReplace<spirv::TosaReduceSumOp>>,
+      TosaOpConvert<tosa::ReshapeOp, ReshapeReplace>,
+      TosaOpConvert<tosa::ReverseOp, ReverseReplace>,
+      TosaOpConvert<tosa::SliceOp, SliceReplace>,
+      TosaOpConvert<tosa::TileOp, TileReplace>,
+      TosaOpConvert<tosa::TransposeOp, TransposeReplace>,
+      TosaOpConvert<tosa::GatherOp, GatherReplace>,
+      TosaOpConvert<tosa::ScatterOp, ScatterReplace>,
+      TosaOpConvert<tosa::ResizeOp, ResizeReplace>,
+      TosaOpConvert<tosa::CastOp, UnaryInputReplace<spirv::TosaCastOp>>,
+      TosaOpConvert<tosa::ConstShapeOp, ConstShapeReplace>>(
       typeConverter, patterns.getContext());
 }
 
