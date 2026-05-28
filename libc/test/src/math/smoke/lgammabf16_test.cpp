17978529af5438896b6c80c75b1492f893ad70ef
udaykiriti <udaykiriti9@gmail.com>
2026-05-28 11:09:06 +0530

    [libc] Fix double-rounding in lgammabf16 for small positive inputs
    Signed-off-by: udaykiriti <udaykiriti9@gmail.com>

--- libc/test/src/math/smoke/lgammabf16_test.cpp
+++ libc/test/src/math/smoke/lgammabf16_test.cpp
@@ -5,6 +5,7 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
+
 #include "hdr/errno_macros.h"
 #include "hdr/fenv_macros.h"
 #include "src/__support/FPUtil/bfloat16.h"
@@ -15,17 +16,50 @@
 
 class LlvmLibcLgammaBf16Test : public LIBC_NAMESPACE::testing::FEnvSafeTest {
   DECLARE_SPECIAL_CONSTANTS(bfloat16)
+
 public:
   void test_special_numbers() {
-    EXPECT_FP_EQ(aNaN, LIBC_NAMESPACE::lgammabf16(aNaN));
-    EXPECT_FP_EQ(inf, LIBC_NAMESPACE::lgammabf16(zero));
-    EXPECT_FP_EQ(inf, LIBC_NAMESPACE::lgammabf16(neg_zero));
-    EXPECT_FP_EQ(inf, LIBC_NAMESPACE::lgammabf16(inf));
-    EXPECT_FP_EQ(inf, LIBC_NAMESPACE::lgammabf16(neg_inf));
-    EXPECT_FP_EQ(zero, LIBC_NAMESPACE::lgammabf16(bfloat16(1.0f)));
-    EXPECT_FP_EQ(zero, LIBC_NAMESPACE::lgammabf16(bfloat16(2.0f)));
-    EXPECT_FP_EQ(inf, LIBC_NAMESPACE::lgammabf16(bfloat16(-1.0f)));
-    EXPECT_FP_EQ(inf, LIBC_NAMESPACE::lgammabf16(bfloat16(-2.0f)));
+    // aNaN -> aNaN, no exception
+    EXPECT_FP_EQ_ALL_ROUNDING(aNaN, LIBC_NAMESPACE::lgammabf16(aNaN));
+    EXPECT_MATH_ERRNO(0);
+
+    // sNaN -> aNaN, FE_INVALID
+    EXPECT_FP_EQ_WITH_EXCEPTION(aNaN, LIBC_NAMESPACE::lgammabf16(sNaN),
+                                FE_INVALID);
+    EXPECT_MATH_ERRNO(0);
+
+    // +Inf -> +Inf
+    EXPECT_FP_EQ_ALL_ROUNDING(inf, LIBC_NAMESPACE::lgammabf16(inf));
+    EXPECT_MATH_ERRNO(0);
+
+    // -Inf -> +Inf
+    EXPECT_FP_EQ_ALL_ROUNDING(inf, LIBC_NAMESPACE::lgammabf16(neg_inf));
+    EXPECT_MATH_ERRNO(0);
+
+    // +-0 -> +Inf, pole error
+    EXPECT_FP_EQ_WITH_EXCEPTION_ALL_ROUNDING(
+        inf, LIBC_NAMESPACE::lgammabf16(zero), FE_DIVBYZERO);
+    EXPECT_MATH_ERRNO(ERANGE);
+
+    EXPECT_FP_EQ_WITH_EXCEPTION_ALL_ROUNDING(
+        inf, LIBC_NAMESPACE::lgammabf16(neg_zero), FE_DIVBYZERO);
+    EXPECT_MATH_ERRNO(ERANGE);
+
+    // lgamma(1) = lgamma(2) = 0
+    EXPECT_FP_EQ_ALL_ROUNDING(zero, LIBC_NAMESPACE::lgammabf16(bfloat16(1.0f)));
+    EXPECT_MATH_ERRNO(0);
+
+    EXPECT_FP_EQ_ALL_ROUNDING(zero, LIBC_NAMESPACE::lgammabf16(bfloat16(2.0f)));
+    EXPECT_MATH_ERRNO(0);
+
+    // Negative integers -> +Inf, pole error
+    EXPECT_FP_EQ_WITH_EXCEPTION_ALL_ROUNDING(
+        inf, LIBC_NAMESPACE::lgammabf16(bfloat16(-1.0f)), FE_DIVBYZERO);
+    EXPECT_MATH_ERRNO(ERANGE);
+
+    EXPECT_FP_EQ_WITH_EXCEPTION_ALL_ROUNDING(
+        inf, LIBC_NAMESPACE::lgammabf16(bfloat16(-2.0f)), FE_DIVBYZERO);
+    EXPECT_MATH_ERRNO(ERANGE);
   }
 };
 
