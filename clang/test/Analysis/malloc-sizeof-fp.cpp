07d2bc5ad8f52f792e7fab354815496310155c52
Rashmi Mudduluru <r_mudduluru@apple.com>
2026-05-28 13:13:38 -0700

    minor test fixes

--- clang/test/Analysis/malloc-sizeof-fp.cpp
+++ clang/test/Analysis/malloc-sizeof-fp.cpp
@@ -6,8 +6,8 @@
 
 // expected-no-diagnostics
 
-#include <stddef.h>
-
+typedef int int32_t;
+using size_t = unsigned long long;
 void *malloc(size_t size);
 void free(void *ptr);
 
@@ -19,23 +19,23 @@ struct atomic {
 };
 } // namespace std
 
-typedef std::atomic<int> u_atomic_int_t;
+typedef std::atomic<int32_t> u_atomic_int32_t;
 
 struct s_int {
-  int c;
+  int32_t c;
 };
 
 template <typename T>
 void work() {
-  u_atomic_int_t *p1 = (u_atomic_int_t *)malloc(sizeof(int));
+  u_atomic_int32_t *p1 = (u_atomic_int32_t *)malloc(sizeof(int32_t));
   free(p1);
 
-  T *p2 = (T *)malloc(sizeof(int));
+  T *p2 = (T *)malloc(sizeof(int32_t));
   free(p2);
 }
 
 int main() {
-  work<u_atomic_int_t>();
+  work<u_atomic_int32_t>();
   work<int>();
   work<s_int>();
   return 0;
