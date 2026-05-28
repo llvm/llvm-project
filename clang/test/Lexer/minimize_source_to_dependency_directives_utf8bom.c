5fe28ef8a4145b41ccf46fd437b6bd10e57b258f
Ben Langmuir <blangmuir@apple.com>
2026-05-28 15:11:13 -0700

    [clang][deps] Fix printing of object-like macros from dep directives
    Preserve the necessary whitespace between the macro name and the body when an
    object-like macro starts with lparen. Otherwise it accidentally turns into an
    invalid function-like macro.

--- clang/test/Lexer/minimize_source_to_dependency_directives_utf8bom.c
+++ clang/test/Lexer/minimize_source_to_dependency_directives_utf8bom.c
@@ -1,6 +1,8 @@
 // Test UTF8 BOM at start of file
 // RUN: cat %S/Inputs/bom-directives.c | od -t x1 | grep -iq 'ef[[:space:]]*bb[[:space:]]*bf'
-// RUN: %clang_cc1 -DTEST -print-dependency-directives-minimized-source %S/Inputs/bom-directives.c 2>&1 | FileCheck %s
+// RUN: %clang_cc1 -DTEST -print-dependency-directives-minimized-source %S/Inputs/bom-directives.c > %t 2>&1
+// RUN: FileCheck %s -input-file %t
+// RUN: %clang_cc1 -Eonly %t.min.c
 
 ﻿// CHECK:      #ifdef TEST
 // CHECK-NEXT: #include <string>
