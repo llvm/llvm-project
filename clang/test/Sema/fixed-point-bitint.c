5cc09e5bcf862cb66a36754d4fee5752574ca5a1
superdusty <superdusty@foxmail.com>
2026-05-28 16:03:13 +0800

    Update fixed-point-bitint.c

--- clang/test/Sema/fixed-point-bitint.c
+++ clang/test/Sema/fixed-point-bitint.c
@@ -1,5 +1,4 @@
 // RUN: %%clang_cc1 -ffixed-point -fsyntax-only -verify %%s 
- 
+
 constexpr _BitInt(128) i = 42; 
-static_assert(i == 42.0k); 
-// expected-error@-1 {{invalid operands to binary expression}} 
+static_assert(i == 42.0k); // expected-error {{invalid operands to binary expression}} 
