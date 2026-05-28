447e92bdedeb0cabb2678be5a67751a5a0848aa8
Abhina Sreeskantharajan <Abhina.Sreeskantharajan@ibm.com>
2026-05-28 15:23:33 -0400

    Add test coverage for conversion errors in string literals

---
+++ clang/test/CodeGen/systemz-charset-diag.cpp
@@ -0,0 +1,3 @@
+// RUN: %clang_cc1 -triple s390x-none-zos -fexec-charset IBM-1047 %s -std=c++17 -emit-llvm -o - -verify
+
+const char* Computer = "🖥️"; // expected-error-re {{conversion to execution encoding failed: {{.*}}}}
