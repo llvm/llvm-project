c9a9b74277679bf550357a7afc8e3c6bf26a9fd7
Pavel Labath <pavel@labath.sk>
2026-05-29 12:12:06 +0000

    Copy input into temp buffer, resolve (some?) review comments

--- libc/fuzzing/arpa/inet/inet_aton_differential_fuzz.cpp
+++ libc/fuzzing/arpa/inet/inet_aton_differential_fuzz.cpp
@@ -11,26 +11,30 @@
 ///
 //===----------------------------------------------------------------------===//
 
+#include "src/__support/CPP/scope.h"
 #include "src/arpa/inet/inet_aton.h"
+#include "src/string/memcpy.h"
 #include <arpa/inet.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
-  if (size == 0 || data[size - 1] != '\0')
-    return -1;
+  // Create a null-terminated copy of the data
+  char *str = new char[size + 1];
+  LIBC_NAMESPACE::cpp::scope_exit delete_str([&] { delete[] str; });
+  LIBC_NAMESPACE::memcpy(str, data, size);
+  str[size] = '\0';
 
   struct in_addr ref_addr = {};
   struct in_addr impl_addr = {};
-  int ref = ::inet_aton(reinterpret_cast<const char *>(data), &ref_addr);
-  int impl = LIBC_NAMESPACE::inet_aton(reinterpret_cast<const char *>(data),
-                                       &impl_addr);
+  int ref = ::inet_aton(str, &ref_addr);
+  int impl = LIBC_NAMESPACE::inet_aton(str, &impl_addr);
 
   if (ref != impl) {
     fprintf(stderr,
             "Different result (reference: %d, implementation: %d) for \"%s\"\n",
-            ref, impl, data);
+            ref, impl, str);
     __builtin_trap();
   }
 
@@ -38,7 +42,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
     fprintf(
         stderr,
         "Different address (reference: %x, implementation: %x) for \"%s\"\n",
-        ref_addr.s_addr, impl_addr.s_addr, data);
+        ref_addr.s_addr, impl_addr.s_addr, str);
     __builtin_trap();
   }
 
