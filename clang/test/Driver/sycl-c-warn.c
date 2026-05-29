bb2c790e47038d113f01bf867a480b0d74ebe8e0
srividya sundaram <srividya.sundaram@intel.com>
2026-05-28 20:01:57 -0700

    Update test.

--- clang/test/Driver/sycl-c-warn.c
+++ clang/test/Driver/sycl-c-warn.c
@@ -1,7 +1,7 @@
 // Verify that a .c file compiled with -fsycl is treated as C++ with a warning.
 // RUN: %clang -### -fsycl %s 2>&1 | FileCheck -check-prefix WARN %s
-// WARN: warning: treating 'c' input as 'c++' when -fsycl is used
+// WARN: warning: treating 'c' input as 'c++' when -fsycl is used [-Winvalid-command-line-argument]
 
 // Verify that explicitly forcing -x c with -fsycl is an error.
-// RUN: %clang -### -fsycl -x c %s 2>&1 | FileCheck -check-prefix ERR %s
+// RUN: not %clang -### -fsycl -x c %s 2>&1 | FileCheck -check-prefix ERR %s
 // ERR: error: '-x c' must not be used in conjunction with '-fsycl', which expects C++ source
