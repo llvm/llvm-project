6ed3e9c7a201990762182ace14a1d65001f07e61
benedekaibas <kaibas.benedek02@gmail.com>
2026-05-31 00:40:48 +0200

    Addressed mentors feedback.

--- clang/lib/StaticAnalyzer/Checkers/LifetimeAnnotations.cpp
+++ clang/lib/StaticAnalyzer/Checkers/LifetimeAnnotations.cpp
@@ -3,12 +3,15 @@
 #include "clang/StaticAnalyzer/Core/PathSensitive/CallDescription.h"
 #include "clang/StaticAnalyzer/Core/PathSensitive/CallEvent.h"
 #include "clang/StaticAnalyzer/Core/PathSensitive/CheckerContext.h"
-#include <AllocationState.h>
+#include "AllocationState.h"
+#include "clang/Analysis/Analyses/LifetimeSafety/LifetimeAnnotations.h"
+
 
 using namespace clang;
 using namespace ento;
+using namespace clang::lifetimes;
 
-REGISTER_MAP_WITH_PROGRAMSTATE(LifetimeBoundMap, const MemRegion *,
+REGISTER_MAP_WITH_PROGRAMSTATE(LifetimeBoundMap, SymbolRef,
                                const MemRegion *);
 
 class LifetimeAnnotations : public Checker<check::PostCall> {
@@ -22,45 +25,62 @@ void LifetimeAnnotations::checkPostCall(const CallEvent &Call,
                                         CheckerContext &C) const {
   ProgramStateRef State = C.getState();
 
-  const auto *MethodDecl = dyn_cast_if_present<CXXMethodDecl>(Call.getDecl());
-
-  if (!MethodDecl)
+  const auto *FC = dyn_cast_if_present<AnyFunctionCall>(&Call);
+  if (!FC)
     return;
 
-  unsigned LBParamIdx = MethodDecl->getNumParams();
-  for (unsigned i = 0; i < MethodDecl->getNumParams(); i++) {
-    if (MethodDecl->getParamDecl(i)->hasAttr<LifetimeBoundAttr>()) {
-      LBParamIdx = i;
+  const FunctionDecl *FD = FC->getDecl();
+  if (!FD)
+    return;
+
+  unsigned LBParamIdx = FD->getNumParams();
+  // FIXME: Use range based for loop instead. Currently that would require
+  // to also change how we create ArgVal which would need a new logic to
+  // be implemented.
+  for (unsigned I = 0, E = FD->getNumParams(); I != E; I++) {
+    if (FD->getParamDecl(I)->hasAttr<LifetimeBoundAttr>()) {
+      LBParamIdx = I;
+      // FIXME: If multiple parameters are annotated this logic would
+      // prevent the analyzer to read after the first parameter.
       break;
     }
   }
-  if (LBParamIdx == MethodDecl->getNumParams())
-    return;
-
   SVal RetVal = Call.getReturnValue();
-  const MemRegion *RetValRegion = RetVal.getAsRegion();
-  if (!RetValRegion)
+
+  SymbolRef RetValSym = RetVal.getAsSymbol(/*IncludeBaseRegions=*/true);
+  if(!RetValSym)
     return;
 
-  SVal ArgVal = Call.getArgSVal(LBParamIdx);
-  const MemRegion *ArgValRegion = ArgVal.getAsRegion();
-  if (!ArgValRegion)
-    return;
+  if (LBParamIdx != FD->getNumParams()) {
+    SVal ArgVal = Call.getArgSVal(LBParamIdx);
+    const MemRegion *ArgValRegion = ArgVal.getAsRegion();
+    // FIXME: if(!ArgValRegion) should be also handled since in those cases
+    // the argument has no region, but still needs to be tracked.
+    if (ArgValRegion)
+        State = State->set<LifetimeBoundMap>(RetValSym, ArgValRegion);
+  }
 
-  State = State->set<LifetimeBoundMap>(RetValRegion, ArgValRegion);
+  if (const auto *IC = dyn_cast<CXXInstanceCall>(&Call)) {
+    if (implicitObjectParamIsLifetimeBound(FD)) {
+      const MemRegion *AttrRegion = IC->getCXXThisVal().getAsRegion();
+
+      if (AttrRegion)
+          State = State->set<LifetimeBoundMap>(RetValSym, AttrRegion);
+    }
+  }
   C.addTransition(State);
 }
 
 void LifetimeAnnotations::printState(raw_ostream &Out, ProgramStateRef State,
                                      const char *NL, const char *Sep) const {
-  auto LBTy = State->get<LifetimeBoundMap>();
+  auto LBVal = State->get<LifetimeBoundMap>();
 
-  if (!LBTy.isEmpty()) {
-    Out << Sep << "LifetimeBound objects: ";
+  if (LBVal.isEmpty())
+    return;
 
-    for (auto I : LBTy) {
-      Out << I.first << " bound to " << I.second << NL;
-    }
+  Out << Sep << "LifetimeBound bindings:" << NL;
+  for (auto&& [RetValSym, ArgValRegion] : LBVal) {
+    Out << " Origin " << RetValSym << " contains Loan " << ArgValRegion << NL;
   }
 }
 
