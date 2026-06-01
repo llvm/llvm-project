a7ba113ecf34e811a686dbcb30bef30c844434df
Sukumarsawant <sawantsukumar@gmail.com>
2026-06-02 00:31:17 +0530

    feat: Dfloat128 instead of float128 (1/n)

--- libc/test/src/__support/FPUtil/dyadic_float_test.cpp
+++ libc/test/src/__support/FPUtil/dyadic_float_test.cpp
@@ -13,7 +13,7 @@
 #include "test/UnitTest/Test.h"
 #include "utils/MPFRWrapper/MPFRUtils.h"
 
-using Float128 = LIBC_NAMESPACE::fputil::DyadicFloat<128>;
+using DFloat128 = LIBC_NAMESPACE::fputil::DyadicFloat<128>;
 using Float192 = LIBC_NAMESPACE::fputil::DyadicFloat<192>;
 using Float256 = LIBC_NAMESPACE::fputil::DyadicFloat<256>;
 using LIBC_NAMESPACE::Sign;
