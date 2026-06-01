9800966aea255ea08322c4751531307b031503d6
Zachary Yedidia <zyedidia@gmail.com>
2026-06-01 19:11:01 -0400

    Add CHECK-NOT to make sure .tlsdesccall moved properly

--- llvm/test/MC/AArch64/LFI/tlsdesccall.s
+++ llvm/test/MC/AArch64/LFI/tlsdesccall.s
@@ -5,6 +5,8 @@ ldr x1, [x0, :tlsdesc_lo12:var]
 add x0, x0, :tlsdesc_lo12:var
 .tlsdesccall var
 blr x1
+// CHECK:      add x0, x0, :tlsdesc_lo12:var
+// CHECK-NOT:  .tlsdesccall
 // CHECK:      add x28, x27, w1, uxtw
 // CHECK-NEXT: .tlsdesccall var
 // CHECK-NEXT: blr x28
