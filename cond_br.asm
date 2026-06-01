64b059e3c1adf1efad8e2e2ea52424755dd7dceb
luisacicolini <lc985@cam.ac.uk>
2026-06-01 19:08:08 +0100

    o0

--- cond_br.asm
+++ cond_br.asm
@@ -8,12 +8,27 @@
 cond_br_demo:                           # @cond_br_demo
 	.cfi_startproc
 # %bb.0:
-	andi	a2, a2, 1
-	beqz	a2, .LBB0_2
-# %bb.1:
+	addi	sp, sp, -16
+	.cfi_def_cfa_offset 16
+	.cfi_remember_state
+	mv	a3, a2
+	mv	a2, a1
+	mv	a1, a0
+	andi	a0, a3, 1
+	sd	a2, 0(sp)                       # 8-byte Folded Spill
+	sd	a1, 8(sp)                       # 8-byte Folded Spill
+	beqz	a0, .LBB0_2
+	j	.LBB0_1
+.LBB0_1:
+	ld	a0, 8(sp)                       # 8-byte Folded Reload
+	addi	sp, sp, 16
+	.cfi_def_cfa_offset 0
 	ret
 .LBB0_2:
-	mv	a0, a1
+	.cfi_restore_state
+	ld	a0, 0(sp)                       # 8-byte Folded Reload
+	addi	sp, sp, 16
+	.cfi_def_cfa_offset 0
 	ret
 .Lfunc_end0:
 	.size	cond_br_demo, .Lfunc_end0-cond_br_demo
