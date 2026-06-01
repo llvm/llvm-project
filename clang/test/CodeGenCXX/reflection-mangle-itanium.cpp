8e54b961f097dd1cb9640a051d3509627cac9f04
changkhothuychung <nhat7203@gmail.com>
2026-06-01 16:56:39 -0400

    more test coverage for reflection mangling

--- clang/test/CodeGenCXX/reflection-mangle-itanium.cpp
+++ clang/test/CodeGenCXX/reflection-mangle-itanium.cpp
@@ -8,8 +8,38 @@ void foo () {}
 
 int main() {
   foo <info {}> ();
+  // CHECK: @_Z3fooITnDaLDmnuEEvv
+  foo <^^void> ();
+  // CHECK: @_Z3fooITnDaLDmtyvEEvv
+  foo <^^bool> ();
+  // CHECK: @_Z3fooITnDaLDmtybEEvv
+  foo <^^char> ();
+  // CHECK: @_Z3fooITnDaLDmtycEEvv
+  foo <^^signed char> ();
+  // CHECK: @_Z3fooITnDaLDmtyaEEvv
+  foo <^^unsigned char> ();
+  // CHECK: @_Z3fooITnDaLDmtyhEEvv
+  foo <^^short> ();
+  // CHECK: @_Z3fooITnDaLDmtysEEvv
+  foo <^^unsigned short> ();
+  // CHECK: @_Z3fooITnDaLDmtytEEvv
   foo <^^int> ();
+  // CHECK: @_Z3fooITnDaLDmtyiEEvv
+  foo <^^unsigned int> ();
+  // CHECK: @_Z3fooITnDaLDmtyjEEvv
+  foo <^^long> ();
+  // CHECK: @_Z3fooITnDaLDmtylEEvv
+  foo <^^unsigned long> ();
+  // CHECK: @_Z3fooITnDaLDmtymEEvv
+  foo <^^long long> ();
+  // CHECK: @_Z3fooITnDaLDmtyxEEvv
+  foo <^^unsigned long long> ();
+  // CHECK: @_Z3fooITnDaLDmtyyEEvv
+  foo <^^float> ();
+  // CHECK: @_Z3fooITnDaLDmtyfEEvv
+  foo <^^double> ();
+  // CHECK: @_Z3fooITnDaLDmtydEEvv
+  foo <^^long double> ();
+  // CHECK: @_Z3fooITnDaLDmtyeEEvv
   return 0;
 }
-// CHECK: define linkonce_odr void @_Z3fooITnDaLDmnuEEvv()
-// CHECK: define linkonce_odr void @_Z3fooITnDaLDmtyiEEvv()
