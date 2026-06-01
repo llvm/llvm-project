04b0637e652c8e3a842e76aaab09e8ea9ca8d6d3
Zachary Yedidia <zyedidia@gmail.com>
2026-05-31 20:36:44 -0400

    [LFI][AArch64] Emit .tlsdesccall with the associated blr
    If a blr is marked with `.tlsdesccall`, then after rewriting there
    should be a `.tlsdesccall` placed on the blr after guarding.

---
+++ llvm/test/MC/AArch64/LFI/tlsdesccall.s
@@ -0,0 +1,10 @@
+// RUN: llvm-mc -triple aarch64_lfi %s | FileCheck %s
+
+adrp x0, :tlsdesc:var
+ldr x1, [x0, :tlsdesc_lo12:var]
+add x0, x0, :tlsdesc_lo12:var
+.tlsdesccall var
+blr x1
+// CHECK:      add x28, x27, w1, uxtw
+// CHECK-NEXT: .tlsdesccall var
+// CHECK-NEXT: blr x28
