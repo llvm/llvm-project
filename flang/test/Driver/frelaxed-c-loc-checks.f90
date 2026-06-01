8a2aea0a467317a445aabc586c4e967a5cc71416
ShashwathiNavada <shashwathinavada@gmail.com>
2026-06-01 09:51:59 -0500

    Simple change

--- flang/test/Driver/frelaxed-c-loc-checks.f90
+++ flang/test/Driver/frelaxed-c-loc-checks.f90
@@ -1,3 +1,4 @@
-RUN: %flang -### -frelaxed-c-loc-checks %s 2>&1 | FileCheck %s
-CHECK: "-fc1"
-CHECK-SAME: "-frelaxed-c-loc-checks"
+! Check that -frelaxed-c-loc-checks is forwarded from the driver to fc1.
+! RUN: %flang -### -frelaxed-c-loc-checks %s 2>&1 | FileCheck %s
+! CHECK: "-fc1"
+! CHECK-SAME: "-frelaxed-c-loc-checks"
