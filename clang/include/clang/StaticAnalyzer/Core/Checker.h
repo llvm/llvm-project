5efa9eacafecc75c09b42141fae27a640e41de48
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2026-06-02 16:23:53 +0200

    [NFC] Use more appropriate name for the internal private function proxy

--- clang/include/clang/StaticAnalyzer/Core/Checker.h
+++ clang/include/clang/StaticAnalyzer/Core/Checker.h
@@ -208,15 +208,16 @@ public:
 
 class LifetimeEnd {
   template <typename CHECKER>
-  static void _checkCall(void *checker, const VarDecl *D, CheckerContext &C) {
+  static void _checkLifetimeEnd(void *checker, const VarDecl *D,
+                                CheckerContext &C) {
     ((const CHECKER *)checker)->checkLifetimeEnd(D, C);
   }
 
 public:
   template <typename CHECKER>
   static void _register(CHECKER *checker, CheckerManager &mgr) {
-    mgr._registerForLifetimeEnd(
-        CheckerManager::CheckLifetimeEndFunc(checker, _checkCall<CHECKER>));
+    mgr._registerForLifetimeEnd(CheckerManager::CheckLifetimeEndFunc(
+        checker, _checkLifetimeEnd<CHECKER>));
   }
 };
 
