34ac5d8c2f952b6f951881d79c8109c642a5d310
Abid Qadeer <haqadeer@amd.com>
2026-06-02 16:52:28 +0100

    Handle review comment (2).
    Improve the tests and add a test for .rodata.

--- flang/test/Integration/function-sections.f90
+++ flang/test/Integration/function-sections.f90
@@ -3,8 +3,8 @@
 ! DEFINE: %{triple} =
 ! DEFINE: %{check-func-sect} = %flang_fc1 -triple %{triple} -S -ffunction-sections -o - %s | FileCheck %s --check-prefix=FUNC-SECT
 ! DEFINE: %{check-func-plain} = %flang_fc1 -triple %{triple} -S -o - %s | FileCheck %s --check-prefix=FUNC-PLAIN
-! DEFINE: %{check-data-sect} = %flang_fc1 -triple %{triple} -S -fdata-sections -o - %s | FileCheck %s --check-prefix=DATA-SECT
-! DEFINE: %{check-data-plain} = %flang_fc1 -triple %{triple} -S -o - %s | FileCheck %s --check-prefix=DATA-PLAIN
+! DEFINE: %{check-data-sect} = %flang_fc1 -triple %{triple} -S -fdata-sections -o - %s | FileCheck %s --check-prefix=DATA-SECT --check-prefix=RODATA-SECT
+! DEFINE: %{check-data-plain} = %flang_fc1 -triple %{triple} -S -o - %s | FileCheck %s --check-prefix=DATA-PLAIN --check-prefix=RODATA-PLAIN
 
 ! REDEFINE: %{triple} = aarch64-unknown-linux-gnu
 ! RUN: %if aarch64-registered-target %{ %{check-func-sect} %}
@@ -22,17 +22,27 @@ module data_sect_mod
   integer, save :: g = 1
 end module
 
+module param_mod
+  integer, parameter :: n = 42
+end module
+
 subroutine foo
 end subroutine
 
 program test
   use data_sect_mod
+  use param_mod
+  integer :: x
   call foo
+  x = n
 end program
 
-! FUNC-SECT: .section{{.*}}.text.
+! FUNC-SECT: .section{{.*}}.text.foo_
 ! FUNC-PLAIN-NOT: .section{{.*}}.text.
 
-! DATA-SECT: .section{{.*}}.data.
+! DATA-SECT: .section{{.*}}.data._QMdata_sect_modEg
 ! DATA-PLAIN: .data
 ! DATA-PLAIN-NOT: .section{{.*}}.data.
+
+! RODATA-SECT: .section{{.*}}.rodata._QMparam_modECn
+! RODATA-PLAIN-NOT: .section{{.*}}.rodata._QMparam_modECn
