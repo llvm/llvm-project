CSharperMantle/llvm-project-bolt

[CIR] Add Direct coerce-in-registers + cir.reinterpret_cast op
Fourth PR in the split of #192119/#192124. Implements the
Direct-with-coercion path in CallConvLowering and picks off
andykaylor's five inline review comments from the original PR.

The new cir.reinterpret_cast op is for same-bit-width in-register
reinterpretation (vector<2 x float> <-> complex<float>).
emitCoercion uses it when source and destination differ only in
vector-vs-non-vector shape and have identical bit width, instead
of going through memory.  For everything else (records, or shape
doesn't match) the helper still does alloca/store/ptr-cast/load.

Andy's comments, in order:
- Temporary alloca alignment is now max(srcAlign, dstAlign) from
  DataLayout instead of hardcoded.
- The alloca lives in the entry block via InsertionGuard so it
  composes with HoistAllocas regardless of pipeline order.
- isVolatile kept as UnitAttr-absence with an inline comment.
- vector<->complex now uses cir.reinterpret_cast.
- Memory path has three new .cir tests covering it.

In-body coercion (insertArgCoercion / insertReturnCoercion) folds
into the existing per-function rewriteFunctionDefinition method
introduced by the prior Direct/Ignore PR's review fixup.  It runs
ahead of the Ignore-arg drop in the same per-function inner-loop
window the pass driver already uses, so the per-function
window-of-invalidity invariant is unchanged: F's signature and
F's body coerce together; F's callers update inside the same
inner loop iteration.  The Ignore-arg drop reuses the existing
poison-stub idiom (the alloca/load-fallback that earlier drafts
used is unnecessary once the drop happens after coercion in the
same per-function window).

LowerToLLVM gets a stub for the new op: bitcast for same-shape
converted types, error-with-message for aggregates.  We don't
produce aggregates from CallConvLowering today, so the error
path is only reachable from hand-written IR; follow-up patch can
add an extract/insert lowering if needed.

Co-

clang/lib/CIR/Dialect/Transforms/CallConvLoweringPass.cpp
@@ -119,7 +119,7 @@ void CallConvLoweringPass::runOnOperation() {
   }
 
   DataLayout dl(module);
-  CIRABIRewriteContext rewriteCtx(module);
+  CIRABIRewriteContext rewriteCtx(module, dl);
 
   // Phase 1: classify every cir.func.  No IR mutation happens here, so
   // running this as a single up-front walk lets later phases consult any

llvm/llvm-project

clang/lib/CIR/Dialect/Transforms/CallConvLoweringPass.cpp
@@ -137,7 +137,7 @@ void CallConvLoweringPass::runOnOperation() {
   }
 
   DataLayout dl(moduleOp);
-  CIRABIRewriteContext rewriteCtx(moduleOp);
+  CIRABIRewriteContext rewriteCtx(moduleOp, dl);
   SymbolTable symbolTable(moduleOp);
 
   // Classify every cir.func up front.  No IR mutation happens here, so