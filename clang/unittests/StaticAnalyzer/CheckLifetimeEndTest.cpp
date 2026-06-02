45d68480656264bb38e7bfb17bf5d58cdc954743
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2026-06-02 16:49:11 +0200

    [NFC] Adjust comment length

--- clang/unittests/StaticAnalyzer/CheckLifetimeEndTest.cpp
+++ clang/unittests/StaticAnalyzer/CheckLifetimeEndTest.cpp
@@ -1,4 +1,4 @@
-//===- unittests/StaticAnalyzer/CheckLifetimeEndTest.cpp --===//
+//===- unittests/StaticAnalyzer/CheckLifetimeEndTest.cpp ------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
