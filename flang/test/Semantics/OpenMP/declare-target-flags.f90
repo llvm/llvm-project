dbc3667258db21b06cd53ee67ec6c4ec8e1774b0
Krzysztof Parzyszek <Krzysztof.Parzyszek@amd.com>
2026-06-03 08:59:41 -0500

    Add common block to tests

--- flang/test/Semantics/OpenMP/declare-target-flags.f90
+++ flang/test/Semantics/OpenMP/declare-target-flags.f90
@@ -3,12 +3,17 @@
 module m
 integer :: x
 !$omp declare_target link(x) device_type(nohost)
+real :: w(10), u(10)
+common /named_block/ w, u
+!$omp declare_target link(/named_block/)
+
 interface
   real function g(v)
     real :: v(10)
     !$omp declare_target
   end
 end interface
+
 contains
 subroutine f
   !$omp declare_target(f)
@@ -20,17 +25,19 @@ subroutine h
 end
 end module
 
-!CHECK:  Module scope: m size=4 alignment=4 sourceRange=295 bytes
-!CHECK:    f, PUBLIC (Subroutine): Subprogram () OmpDeclareTargetFlags:(enter)
-!CHECK:    g, EXTERNAL, PUBLIC (Function, OmpDeclareTarget): Subprogram isInterface result:REAL(4) g (REAL(4) v) OmpDeclareTargetFlags:(enter)
-!CHECK:    h, PUBLIC (Subroutine): Subprogram () OmpDeclareTargetFlags:(enter)
-!CHECK:    x, PUBLIC (OmpDeclareTarget) size=4 offset=0: ObjectEntity type: INTEGER(4) OmpDeclareTargetFlags:(device_type(nohost) link)
-!CHECK:    Subprogram scope: g size=44 alignment=4 sourceRange=57 bytes
-!CHECK:      g size=4 offset=0: ObjectEntity funcResult type: REAL(4)
-!CHECK:      v size=40 offset=4: ObjectEntity dummy type: REAL(4) shape: 1_8:10_8
-!CHECK:    Subprogram scope: f size=0 alignment=1 sourceRange=40 bytes
-!CHECK:      f (Subroutine, OmpDeclareTarget): HostAssoc => f, PUBLIC (Subroutine): Subprogram () OmpDeclareTargetFlags:(enter)
-!CHECK:    Subprogram scope: h size=40 alignment=4 sourceRange=81 bytes
-!CHECK:      a, SAVE (OmpDeclareTarget) size=40 offset=0: ObjectEntity type: INTEGER(4) shape: 1_8:10_8 OmpDeclareTargetFlags:(enter)
-!CHECK:      h (Subroutine, OmpDeclareTarget): HostAssoc => h, PUBLIC (Subroutine): Subprogram () OmpDeclareTargetFlags:(enter)
-
+!CHECK: Module scope: m size=4 alignment=4 sourceRange=383 bytes
+!CHECK:     f, PUBLIC (Subroutine): Subprogram () OmpDeclareTargetFlags:(enter)
+!CHECK:     g, EXTERNAL, PUBLIC (Function, OmpDeclareTarget): Subprogram isInterface result:REAL(4) g (REAL(4) v) OmpDeclareTargetFlags:(enter)
+!CHECK:     h, PUBLIC (Subroutine): Subprogram () OmpDeclareTargetFlags:(enter)
+!CHECK:     u, PUBLIC (InCommonBlock, OmpDeclareTarget) size=40 offset=40: ObjectEntity type: REAL(4) shape: 1_8:10_8
+!CHECK:     w, PUBLIC (InCommonBlock, OmpDeclareTarget) size=40 offset=0: ObjectEntity type: REAL(4) shape: 1_8:10_8
+!CHECK:     x, PUBLIC (OmpDeclareTarget) size=4 offset=0: ObjectEntity type: INTEGER(4) OmpDeclareTargetFlags:(device_type(nohost) link)
+!CHECK:     named_block size=80 offset=0: CommonBlockDetails alignment=4: w u OmpDeclareTargetFlags:(link)
+!CHECK:     Subprogram scope: g size=44 alignment=4 sourceRange=57 bytes
+!CHECK:       g size=4 offset=0: ObjectEntity funcResult type: REAL(4)
+!CHECK:       v size=40 offset=4: ObjectEntity dummy type: REAL(4) shape: 1_8:10_8
+!CHECK:     Subprogram scope: f size=0 alignment=1 sourceRange=40 bytes
+!CHECK:       f (Subroutine, OmpDeclareTarget): HostAssoc => f, PUBLIC (Subroutine): Subprogram () OmpDeclareTargetFlags:(enter)
+!CHECK:     Subprogram scope: h size=40 alignment=4 sourceRange=81 bytes
+!CHECK:       a, SAVE (OmpDeclareTarget) size=40 offset=0: ObjectEntity type: INTEGER(4) shape: 1_8:10_8 OmpDeclareTargetFlags:(enter)
+!CHECK:       h (Subroutine, OmpDeclareTarget): HostAssoc => h, PUBLIC (Subroutine): Subprogram () OmpDeclareTargetFlags:(enter)
--- flang/test/Semantics/OpenMP/declare-target-modfile.f90
+++ flang/test/Semantics/OpenMP/declare-target-modfile.f90
@@ -3,12 +3,17 @@
 module m
 integer :: x
 !$omp declare_target link(x) device_type(nohost)
+real :: w(10), u(10)
+common /named_block/ w, u
+!$omp declare_target link(/named_block/)
+
 interface
   real function g(v)
     real :: v(10)
     !$omp declare_target
   end
 end interface
+
 contains
 subroutine f
   !$omp declare_target(f)
@@ -23,16 +28,20 @@ end module
 !Expect: m.mod
 !module m
 !integer(4)::x
+!real(4)::w(1_8:10_8)
+!real(4)::u(1_8:10_8)
 !interface
 !function g(v)
 !real(4)::v(1_8:10_8)
 !real(4)::g
 !end
 !end interface
+!common/named_block/w,u
 !!$omp declare_target device_type(nohost) link(x)
 !!$omp declare_target enter(g)
 !!$omp declare_target enter(f)
 !!$omp declare_target enter(h)
+!!$omp declare_target link(named_block)
 !contains
 !subroutine f()
 !end
