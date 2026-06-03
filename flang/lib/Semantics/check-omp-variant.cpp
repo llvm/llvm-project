1023990a57d5824a6fc2f7a899b8f36977bbcf66
Abid Qadeer <haqadeer@amd.com>
2026-06-03 10:34:09 +0100

    [flang][OpenMP] Rename check-omp-metadirective.cpp (NFC). (#201159)
    Both METADIRECTIVE and DECLARE VARIANT fall into the "variant
    directives" category, so check-omp-variant.cpp is a more accurate name
    for the file that hosts their semantic checks.
    
    Suggested in
    https://github.com/llvm/llvm-project/pull/198799#issuecomment-4576970335

--- flang/lib/Semantics/check-omp-variant.cpp
+++ flang/lib/Semantics/check-omp-variant.cpp
@@ -1,4 +1,4 @@
-//===-- lib/Semantics/check-omp-metadirective.cpp -------------------------===//
+//===-- lib/Semantics/check-omp-variant.cpp -------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
