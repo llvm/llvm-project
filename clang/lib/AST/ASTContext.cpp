1fce476a251bf19b3e9264ab613c375e96d5dbc9
Evgeny Leviant <eleviant@accesssoftek.com>
2026-06-03 10:00:08 +0200

    Refactor and add comments

--- clang/lib/AST/ASTContext.cpp
+++ clang/lib/AST/ASTContext.cpp
@@ -16012,9 +16012,16 @@ ASTContext::getPaddingIntervals(QualType Ty) const {
 
   return itr->second;
 
+// MSVC permits certain C-style casts in constant expressions.
+// A common example is FIELD_OFFSET, implemented as
+// (LONG_PTR)(&((type*)0)->field). If MSVC compatibility is enabled
+// and the only constexpr evaluation failure is a ptr-to-int cast,
+// allow folding the expression and optionally emit a warning.
 bool ASTContext::maybeFoldConstexprWithCast(
     SmallVectorImpl<PartialDiagnosticAt> &Notes) const {
-  if (Notes.size() != 1 || !getLangOpts().MSVCCompat)
+  if (!getLangOpts().MSVCCompat)
+    return false;
+  if (Notes.size() != 1)
     return false;
   auto &PD = Notes[0].second;
   if (PD.getDiagID() != diag::note_constexpr_invalid_cast_ptrtoint)
@@ -16022,6 +16029,5 @@ bool ASTContext::maybeFoldConstexprWithCast(
   getDiagnostics().Report(Notes[0].first, diag::warn_relaxed_constant_fold)
       << !!PD.getValueArg(0);
   Notes.clear();
-
   return true;
 }
