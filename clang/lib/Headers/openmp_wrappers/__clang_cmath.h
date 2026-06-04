76917dfcf956114040a3ac037f233b3468cbd7f5
Fine, Gregory <gregory.fine@intel.com>
2026-06-03 20:11:17 -0700

    Fix incorrect header description

--- clang/lib/Headers/openmp_wrappers/__clang_cmath.h
+++ clang/lib/Headers/openmp_wrappers/__clang_cmath.h
@@ -1,4 +1,4 @@
-/*===---- __clang_spirv_cmath.h - SPIRV cmath decls -----------------------===
+/*===---- __clang_cmath.h - cmath decls ------------------------------------===
  *
  * Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
  * See https://llvm.org/LICENSE.txt for license information.
