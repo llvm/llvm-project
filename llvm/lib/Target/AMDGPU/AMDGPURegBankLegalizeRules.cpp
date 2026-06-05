f35b6cc120ea132fe7f343862612a62384645352
Jonathan Antenanie <jonathan03ant@gmail.com>
2026-06-05 17:39:01 +0000

    [AMDGPU][GlobalIsel] Adding new reg bank rules for amdgcn_s_quadmask

--- llvm/lib/Target/AMDGPU/AMDGPURegBankLegalizeRules.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPURegBankLegalizeRules.cpp
@@ -1696,6 +1696,10 @@ RegBankLegalizeRules::RegBankLegalizeRules(const GCNSubtarget &_ST,
   addRulesForIOpcs({amdgcn_readlane}, StandardB)
       .Uni(B32, {{SgprB32}, {IntrId, VgprB32, SgprB32_ReadFirstLane}});
 
+  addRulesForIOpcs({amdgcn_s_quadmask}, StandardB)
+      .Uni(B32, {{SgprB32}, {IntrId, SgprB32_ReadFirstLane}})
+      .Uni(B64, {{SgprB64}, {IntrId, SgprB64_ReadFirstLane}});
+
   addRulesForIOpcs({amdgcn_writelane}, StandardB)
       .Div(B32,
            {{VgprB32},
