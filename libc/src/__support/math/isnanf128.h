eda56e3652af56769f8e3c73953213494c47c9a3
Vedant Neve <vedantneve13@gmail.com>
2026-06-06 17:32:51 +0000

    Update libc/src/__support/math/isnanf128.h

--- libc/src/__support/math/isnanf128.h
+++ libc/src/__support/math/isnanf128.h
@@ -19,7 +19,7 @@
 namespace LIBC_NAMESPACE_DECL {
 namespace math {
 
-LIBC_INLINE constexpr int isnanf128(float128 x) {
+LIBC_INLINE LIBC_CONSTEXPR int isnanf128(float128 x) {
   return fputil::FPBits<float128>(x).is_nan();
 }
 
