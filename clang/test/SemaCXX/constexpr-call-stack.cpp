b47123e84512130bb3e0281b9225d153dfe446cc
Daniil Dudkin <unterumarmung@yandex.ru>
2026-06-06 20:49:19 +0300

    add test for experimental-new-constant-interpreter

--- clang/test/SemaCXX/constexpr-call-stack.cpp
+++ clang/test/SemaCXX/constexpr-call-stack.cpp
@@ -1,4 +1,5 @@
 // RUN: %clang_cc1 -std=c++20 -fsyntax-only -Wno-unused-value -Wno-stack-exhausted -fconstexpr-depth=1024 %s
+// RUN: %clang_cc1 -std=c++20 -fsyntax-only -Wno-unused-value -Wno-stack-exhausted -fconstexpr-depth=1024 -fexperimental-new-constant-interpreter %s
 
 int rand();
 
