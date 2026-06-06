0cbd4511bd28f842200dab84742f716d83dd3bbc
Vladimir Vereschaka <vvereschaka@accesssoftek.com>
2026-06-05 17:53:16 -0700

    Revert "[clang-cl] Add new option `/pathmap:<from>=<to>` to replace the path …"
    This reverts commit 832f4c41ae172950bbb5797803d6ec1a94edd146.

--- clang/test/Driver/cl-pathmap.c
+++
@@ -1,4 +0,0 @@
-// RUN: %clang_cl -E -### /pathmap:%p=. %s 2>&1 | FileCheck %s --check-prefix CHECK-CC1-OPTS
-// CHECK-CC1-OPTS: -fdebug-prefix-map=
-// CHECK-CC1-OPTS: -fmacro-prefix-map=
-// CHECK-CC1-OPTS: -fcoverage-prefix-map=
