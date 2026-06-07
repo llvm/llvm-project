bbaf731929872e19dde9da79c75d11fbff6e58b1
Sivapriya Venkateswarar <sivapriyaofficial2005@gmail.com>
2026-06-07 20:19:42 +0400

    CIR: fix test nits (RUN order, trailing newline)

--- clang/test/CIR/CodeGenCUDA/surface.cu
+++ clang/test/CIR/CodeGenCUDA/surface.cu
@@ -1,8 +1,8 @@
 // REQUIRES: x86-registered-target
 // REQUIRES: nvptx-registered-target
-// RUN: %clang_cc1 -fclangir -std=c++11 -fcuda-is-device -triple nvptx64-nvidia-cuda -emit-cir -o - %s | FileCheck --check-prefix=DEVICE-CIR %s
-// RUN: %clang_cc1 -fclangir -std=c++11 -fcuda-is-device -triple nvptx64-nvidia-cuda -emit-llvm -o - %s | FileCheck --check-prefix=DEVICE-CIR-LLVM %s
-// RUN: %clang_cc1 -std=c++11 -fcuda-is-device -triple nvptx64-nvidia-cuda -emit-llvm -o - %s | FileCheck --check-prefix=DEVICE-OGCG %s
+// RUN: %clang_cc1 -fclangir -std=c++11 -fcuda-is-device -triple nvptx64-nvidia-cuda -emit-cir -o - %s | FileCheck --check-prefix=CIR-DEVICE %s
+// RUN: %clang_cc1 -fclangir -std=c++11 -fcuda-is-device -triple nvptx64-nvidia-cuda -emit-llvm -o - %s | FileCheck --check-prefix=LLVM-DEVICE %s
+// RUN: %clang_cc1 -std=c++11 -fcuda-is-device -triple nvptx64-nvidia-cuda -emit-llvm -o - %s | FileCheck --check-prefix=OGCG-DEVICE %s
 
 struct surfaceReference {
   int desc;
@@ -18,8 +18,8 @@ struct __attribute__((device_builtin_surface_type)) surface<void, dim>
 
 surface<void, 2> surf;
 
-// DEVICE-CIR: cir.global external target_address_space(1) @surf = #cir.poison : !s64i
+// CIR-DEVICE: cir.global external target_address_space(1) @surf = #cir.poison : !s64i
 
 // CIR lowers poison to LLVM poison, while OG CodeGen emits undef.
-// DEVICE-CIR-LLVM: @surf ={{.*}} addrspace(1) externally_initialized global i64 poison
-// DEVICE-OGCG: @surf ={{.*}} addrspace(1) externally_initialized global i64 undef
+// LLVM-DEVICE: @surf ={{.*}} addrspace(1) externally_initialized global i64 poison
+// OGCG-DEVICE: @surf ={{.*}} addrspace(1) externally_initialized global i64 undef
