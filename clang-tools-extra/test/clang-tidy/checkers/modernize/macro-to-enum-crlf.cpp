d286a26f62ef479ae6ddda776a924c329318b2c3
Zeyi Xu <mitchell.xu2@gmail.com>
2026-06-07 23:25:10 +0800

    Revert "change the way of testing this"
    This reverts commit 7f0291b4e1816b767e45c57448bb9c14b87a75f5.

--- clang-tools-extra/test/clang-tidy/checkers/modernize/macro-to-enum-crlf.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/modernize/macro-to-enum-crlf.cpp
@@ -1,3 +1,4 @@
-// RUN: %python -c "from pathlib import Path; Path(r'%/t.cpp').write_bytes(b'#define RED 1\r\n#define GREEN 2\r\n')"
+// RUN: cp %S/Inputs/macro-to-enum/crlf.cpp %t.cpp
+// RUN: chmod u+w %t.cpp
 // RUN: clang-tidy %t.cpp -fix --checks='-*,modernize-macro-to-enum' --config={} -- --std=c++14 > %t.out 2>&1
-// RUN: %python -c "from pathlib import Path; data = Path(r'%/t.cpp').read_bytes(); expected = b'enum {\r\nRED = 1,\r\nGREEN = 2\r\n};\r\n'; assert data == expected, data"
+// RUN: diff %t.cpp %S/Inputs/macro-to-enum/crlf.cpp.expected
