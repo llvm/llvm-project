d0c9db62ffbe2a946c992354db8d0b0c87a37841
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2026-06-08 09:28:03 +0200

    Make the CFG-dump test stricter

--- clang/test/Analysis/lifetime-end-simple-cfg-output.cpp
+++ clang/test/Analysis/lifetime-end-simple-cfg-output.cpp
@@ -6,17 +6,27 @@
 void test_simple_variable() {
   int i = 0;
 }
-// CHECK:       void test_simple_variable()
-// CHECK:       [B2 (ENTRY)]
+// CHECK:      void test_simple_variable()
+// CHECK-NEXT: [B2 (ENTRY)]
 // CHECK-NEXT:    Succs (1): B1
-// CHECK:       [B1]
+// CHECK-EMPTY:
+// CHECK-NEXT: [B1]
 // CHECK-NEXT:    1: 0
 // CHECK-NEXT:    2: int i = 0;
 // CHECK-NEXT:    3: [B1.2] (Lifetime ends)
 // CHECK-NEXT:    Preds (1): B2
 // CHECK-NEXT:    Succs (1): B0
-// CHECK:       [B0 (EXIT)]
+// CHECK-EMPTY:
+// CHECK-NEXT: [B0 (EXIT)]
 // CHECK-NEXT:    Preds (1): B1
+// CHECK-EMPTY:
+// CHECK-NEXT: ~A() noexcept
+// CHECK-NEXT: [B1 (ENTRY)]
+// CHECK-NEXT:    Succs (1): B0
+// CHECK-EMPTY:
+// CHECK-NEXT: [B0 (EXIT)]
+// CHECK-NEXT:    Preds (1): B1
+// CHECK-EMPTY:
 
 struct A {
   ~A() {}
@@ -24,18 +34,21 @@ struct A {
 void test_nontrivial_dtor() {
   A a;
 }
-// CHECK:       void test_nontrivial_dtor()
-// CHECK:       [B2 (ENTRY)]
+// CHECK-NEXT: void test_nontrivial_dtor()
+// CHECK-NEXT: [B2 (ENTRY)]
 // CHECK-NEXT:    Succs (1): B1
-// CHECK:       [B1]
+// CHECK-EMPTY:
+// CHECK-NEXT: [B1]
 // CHECK-NEXT:    1:  (CXXConstructExpr, [B1.2], A)
 // CHECK-NEXT:    2: A a;
 // CHECK-NEXT:    3: [B1.2].~A() (Implicit destructor)
 // CHECK-NEXT:    4: [B1.2] (Lifetime ends)
 // CHECK-NEXT:    Preds (1): B2
 // CHECK-NEXT:    Succs (1): B0
-// CHECK:       [B0 (EXIT)]
+// CHECK-EMPTY:
+// CHECK-NEXT: [B0 (EXIT)]
 // CHECK-NEXT:    Preds (1): B1
+// CHECK-EMPTY:
 
 void test_multiple_variables_nested_scopes() {
   int a = 0;
@@ -44,10 +57,11 @@ void test_multiple_variables_nested_scopes() {
     int c = 0, d = 0;
   }
 }
-// CHECK:       void test_multiple_variables_nested_scopes()
-// CHECK:       [B2 (ENTRY)]
+// CHECK-NEXT: void test_multiple_variables_nested_scopes()
+// CHECK-NEXT: [B2 (ENTRY)]
 // CHECK-NEXT:    Succs (1): B1
-// CHECK:       [B1]
+// CHECK-EMPTY:
+// CHECK-NEXT: [B1]
 // CHECK-NEXT:    1: 0
 // CHECK-NEXT:    2: int a = 0;
 // CHECK-NEXT:    3: 0
@@ -62,33 +76,40 @@ void test_multiple_variables_nested_scopes() {
 // CHECK-NEXT:   12: [B1.2] (Lifetime ends)
 // CHECK-NEXT:    Preds (1): B2
 // CHECK-NEXT:    Succs (1): B0
-// CHECK:       [B0 (EXIT)]
+// CHECK-EMPTY:
+// CHECK-NEXT: [B0 (EXIT)]
 // CHECK-NEXT:    Preds (1): B1
+// CHECK-EMPTY:
 
 void test_local_static() {
   static int i = 0;
   int j = 0;
 }
-// CHECK:       void test_local_static()
-// CHECK:       [B4 (ENTRY)]
+// CHECK-NEXT: void test_local_static()
+// CHECK-NEXT: [B4 (ENTRY)]
 // CHECK-NEXT:    Succs (1): B3
-// CHECK:       [B1]
+// CHECK-EMPTY:
+// CHECK-NEXT: [B1]
 // CHECK-NEXT:    1: 0
 // CHECK-NEXT:    2: int j = 0;
 // CHECK-NEXT:    3: [B1.2] (Lifetime ends)
 // CHECK-NEXT:    Preds (2): B2 B3
 // CHECK-NEXT:    Succs (1): B0
-// CHECK:       [B2]
+// CHECK-EMPTY:
+// CHECK-NEXT: [B2]
 // CHECK-NEXT:    1: 0
 // CHECK-NEXT:    2: static int i = 0;
 // CHECK-NEXT:    Preds (1): B3
 // CHECK-NEXT:    Succs (1): B1
-// CHECK:       [B3]
+// CHECK-EMPTY:
+// CHECK-NEXT: [B3]
 // CHECK-NEXT:    T: static init i
 // CHECK-NEXT:    Preds (1): B4
 // CHECK-NEXT:    Succs (2): B1 B2
-// CHECK:       [B0 (EXIT)]
+// CHECK-EMPTY:
+// CHECK-NEXT: [B0 (EXIT)]
 // CHECK-NEXT:    Preds (1): B1
+// CHECK-EMPTY:
 
 void test_loop_body() {
   while (true) {
@@ -96,37 +117,44 @@ void test_loop_body() {
     break;
   }
 }
-// CHECK:       void test_loop_body()
-// CHECK:       [B5 (ENTRY)]
+// CHECK-NEXT: void test_loop_body()
+// CHECK-NEXT: [B5 (ENTRY)]
 // CHECK-NEXT:    Succs (1): B4
-// CHECK:       [B1]
+// CHECK-EMPTY:
+// CHECK-NEXT: [B1]
 // CHECK-NEXT:    Preds (1): B2
 // CHECK-NEXT:    Succs (1): B4
-// CHECK:       [B2]
+// CHECK-EMPTY:
+// CHECK-NEXT: [B2]
 // CHECK-NEXT:    1: [B3.2] (Lifetime ends)
 // CHECK-NEXT:    Succs (1): B1
-// CHECK:       [B3]
+// CHECK-EMPTY:
+// CHECK-NEXT: [B3]
 // CHECK-NEXT:    1: 0
 // CHECK-NEXT:    2: int i = 0;
 // CHECK-NEXT:    3: [B3.2] (Lifetime ends)
 // CHECK-NEXT:    T: break;
 // CHECK-NEXT:    Preds (1): B4
 // CHECK-NEXT:    Succs (1): B0
-// CHECK:       [B4]
+// CHECK-EMPTY:
+// CHECK-NEXT: [B4]
 // CHECK-NEXT:    1: true
 // CHECK-NEXT:    T: while [B4.1]
 // CHECK-NEXT:    Preds (2): B1 B5
 // CHECK-NEXT:    Succs (2): B3 NULL
-// CHECK:       [B0 (EXIT)]
+// CHECK-EMPTY:
+// CHECK-NEXT: [B0 (EXIT)]
 // CHECK-NEXT:    Preds (1): B3
+// CHECK-EMPTY:
 
 void test_lifetime_extended_temporary() {
   const int &r = 42;
 }
-// CHECK:       void test_lifetime_extended_temporary()
-// CHECK:       [B2 (ENTRY)]
+// CHECK-NEXT: void test_lifetime_extended_temporary()
+// CHECK-NEXT: [B2 (ENTRY)]
 // CHECK-NEXT:    Succs (1): B1
-// CHECK:       [B1]
+// CHECK-EMPTY:
+// CHECK-NEXT: [B1]
 // CHECK-NEXT:    1: 42
 // CHECK-NEXT:    2: [B1.1] (ImplicitCastExpr, NoOp, const int)
 // CHECK-NEXT:    3: [B1.2]
@@ -134,5 +162,6 @@ void test_lifetime_extended_temporary() {
 // CHECK-NEXT:    5: [B1.4] (Lifetime ends)
 // CHECK-NEXT:    Preds (1): B2
 // CHECK-NEXT:    Succs (1): B0
-// CHECK:       [B0 (EXIT)]
+// CHECK-EMPTY:
+// CHECK-NEXT: [B0 (EXIT)]
 // CHECK-NEXT:    Preds (1): B1
