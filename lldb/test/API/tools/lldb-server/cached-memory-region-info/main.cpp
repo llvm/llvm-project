215aa744a8c82ab9e8e2465aeca435e161fe48cd
Jason Molenda <jmolenda@apple.com>
2026-06-08 22:55:42 -0700

    ws

--- lldb/test/API/tools/lldb-server/cached-memory-region-info/main.cpp
+++ lldb/test/API/tools/lldb-server/cached-memory-region-info/main.cpp
@@ -1,25 +1,22 @@
 #include <thread>
 #include <unistd.h>
 
-int var() { 
+int var() {
   sleep(100); // break here
 }
 int baz() { return 10 + var(); }
 int bar() { return 15 + baz(); }
 int foo() { return 20 + bar(); }
-void work () {
-  foo();
-}
+void work() { foo(); }
 
-int main()
-{
+int main() {
   std::thread thread_1(work);
   std::thread thread_2(work);
   std::thread thread_3(work);
   std::thread thread_4(work);
   std::thread thread_5(work);
 
-  sleep (20);
+  sleep(20);
 
   thread_5.join();
   thread_4.join();
