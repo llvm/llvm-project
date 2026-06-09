eb11f9dedb5967b925f6edcfb116bad4c24cb998
LeeYoungJoon <dog3hk.dev@gmail.com>
2026-06-10 08:23:48 +0900

    Fix test case to be target-independent

--- clang-tools-extra/test/clang-tidy/checkers/bugprone/branch-clone-inline-asm.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/bugprone/branch-clone-inline-asm.cpp
@@ -3,31 +3,19 @@
 int test_asm1(int argc, char**) {
   if (argc > 1) {
 // CHECK-MESSAGES: :[[@LINE-1]]:3: warning: if with identical then and else branches [bugprone-branch-clone]
-    __asm__ volatile(
-      "addi %0, %0, -1"
-      : "+r" (argc)
-      );
+    __asm__ volatile("" : "+r" (argc));
   } else {
 // CHECK-MESSAGES: :[[@LINE-1]]:5: note: else branch starts here
-    __asm__ volatile(
-      "addi %0, %0, -1"
-      : "+r" (argc)
-      );
+    __asm__ volatile("" : "+r" (argc));
   }
   return argc;
 }
 
 int test_asm2(int argc, char**) {
   if (argc > 1) { // no-warning
-    __asm__ volatile(
-      "addi %0, %0, -1"
-      : "+r" (argc)
-      );
+    __asm__ volatile("foo" : "+r" (argc));
   } else {
-    __asm__ volatile(
-      "addi %0, %0, -2"
-      : "+r" (argc)
-      );
+    __asm__ volatile("bar" : "+r" (argc));
   }
   return argc;
 }
@@ -36,20 +24,10 @@ int test_asm3(int argc, char**) {
   int Test1 = 0;
   if (argc > 1) {
 // CHECK-MESSAGES: :[[@LINE-1]]:3: warning: if with identical then and else branches [bugprone-branch-clone]
-    __asm__ volatile(
-      "add %w0, %w0, -1"
-      : "+r" (argc)
-      : "r" (Test1)
-      : "w0"
-      );
+    __asm__ volatile("" : "+r" (argc) : "r" (Test1) : "memory");
   } else {
 // CHECK-MESSAGES: :[[@LINE-1]]:5: note: else branch starts here
-    __asm__ volatile(
-      "add %w0, %w0, -1"
-      : "+r" (argc)
-      : "r" (Test1)
-      : "w0"
-      );
+    __asm__ volatile("" : "+r" (argc) : "r" (Test1) : "memory");
   }
   return argc;
 }
@@ -57,19 +35,9 @@ int test_asm3(int argc, char**) {
 int test_asm4(int argc, char**) {
   int Test1 = 0;
   if (argc > 1) { // no-warning
-    __asm__ volatile(
-      "add %w0, %w0, -1"
-      : "+r" (argc)
-      : "r" (Test1)
-      : "w0"
-      );
+    __asm__ volatile("" : "+r" (argc) : "r" (Test1) : "memory");
   } else {
-    __asm__ volatile(
-      "add %w0, %w0, -1" 
-      : "+r" (argc)
-      : "r" (Test1)
-      : "w1"
-      );
+    __asm__ volatile("" : "+r" (argc) : "r" (Test1) : "cc");
   }
   return argc;
 }
