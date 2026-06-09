a0cd4fe1c5af313af8b0c595bd8b617ab0d1727e
pvanhout <pierre.vanhoutryve@amd.com>
2026-06-09 16:54:38 +0200

    [RFC][AMDGPU][InsertWaitCnt] Move WaitEventType into separate HWEvent header
    I propose to move `WaitEventType` into its own header to start a new
    component of the back-end targeted at analyzing and treating hardware events
    fired by instructions. Right now this just moves code around and renames things
    (NFCI) but over time, we should generalize the events so they can be reused
    by other passes instead of being hyper-specialized for InsertWaitCnt.

---
+++ llvm/lib/Target/AMDGPU/Utils/AMDGPUHWEvents.cpp
@@ -0,0 +1,34 @@
+//===- AMDGPUHWEvents.cpp ---------------------------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "AMDGPUHWEvents.h"
+#include "llvm/ADT/StringExtras.h"
+#include "llvm/Support/Debug.h"
+#include "llvm/Support/raw_ostream.h"
+
+namespace llvm {
+namespace AMDGPU {
+// NOLINTNEXTLINE
+iota_range<HWEvent> hw_events(HWEvent MaxEvent) {
+  return enum_seq(HWEvent::FIRST_WAIT_EVENT, MaxEvent);
+}
+
+void HWEventSet::print(raw_ostream &OS) const {
+  ListSeparator LS(", ");
+  for (HWEvent Event : hw_events()) {
+    if (contains(Event))
+      OS << LS << toString(Event);
+  }
+}
+
+void HWEventSet::dump() const {
+  print(dbgs());
+  dbgs() << "\n";
+}
+} // namespace AMDGPU
+} // namespace llvm
