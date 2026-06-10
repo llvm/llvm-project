a6c42004604af465509b68cd5fff3bbd5c8b7f74
Stephen Tozer <stephen.tozer@sony.com>
2026-06-10 13:13:18 +0100

    clang-format

--- cross-project-tests/debuginfo-tests/dexter/feature_tests/scripts/nested_wheres.cpp
+++ cross-project-tests/debuginfo-tests/dexter/feature_tests/scripts/nested_wheres.cpp
@@ -1,22 +1,20 @@
 // RUN: %dexter_regression_test_cxx_build %s -o %t
-// RUN: %dexter_regression_test_run --use-script --binary %t -- %s | FileCheck %s
+// RUN: %dexter_regression_test_run --use-script --binary %t -- %s \
+// RUN:   | FileCheck %s
 
 /// Test that we correctly interpret nested !where+!and nodes during debugging
 /// and evaluation.
 
 int fib(int n) {
-    if (n <= 1) {
-        return 1;
-    }
-    int first = fib(n - 2);
-    int second = fib(n - 1);
-    return first + second;
+  if (n <= 1) {
+    return 1;
+  }
+  int first = fib(n - 2);
+  int second = fib(n - 1);
+  return first + second;
 }
 
-int main() {
-  return fib(4);
-}
-
+int main() { return fib(4); }
 
 // CHECK: correct_steps: 9
 // CHECK: missing_values: 0
