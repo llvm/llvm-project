055d86ac70f53316bae75f4874a1d2cbcac64f54
Andrzej Warzynski <andrzej.warzynski@arm.com>
2026-06-10 16:47:36 +0000

    Swich from unconditional PURE to opt-in

--- mlir/lib/Dialect/EmitC/IR/EmitC.cpp
+++ mlir/lib/Dialect/EmitC/IR/EmitC.cpp
@@ -336,6 +336,21 @@ bool CastOp::areCastCompatible(TypeRange inputs, TypeRange outputs) {
        emitc::isSupportedFloatType(output) || isa<emitc::PointerType>(output)));
 }
 
+bool CastOp::isPure() { return static_cast<bool>(getPureAttr()); }
+
+Speculation::Speculatability emitc::CastOp::getSpeculatability() {
+  return getPure() ? Speculation::Speculatable : Speculation::NotSpeculatable;
+}
+
+void emitc::CastOp::getEffects(
+    SmallVectorImpl<MemoryEffects::EffectInstance> &effects) {
+  if (getPure())
+    return;
+
+  effects.emplace_back(MemoryEffects::Read::get());
+  effects.emplace_back(MemoryEffects::Write::get());
+}
+
 //===----------------------------------------------------------------------===//
 // CallOpaqueOp
 //===----------------------------------------------------------------------===//
--- mlir/test/Dialect/EmitC/canonicalize.mlir
+++ mlir/test/Dialect/EmitC/canonicalize.mlir
@@ -1,9 +1,13 @@
 // RUN: mlir-opt %s -canonicalize="test-convergence" -split-input-file | FileCheck %s
 
-// While there is no dedicated folder for CastOp, it is Pure and hence should
-// be "folded away".
-func.func @cast(%arg0: i32) {
-  // CHECK-NOT: emitc.cast
+func.func @no_fold_cast_to_f32(%arg0: i32) {
+  // CHECK: emitc.cast
   %1 = emitc.cast %arg0: i32 to f32
+  return
+}
+
+func.func @fold_cast_to_i64(%arg0: i32) {
+  // CHECK-NOT: emitc.cast
+  %1 = emitc.cast %arg0 {pure} : i32 to i64
   return
 }
