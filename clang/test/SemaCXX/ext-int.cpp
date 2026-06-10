7957adc263182e6f4162bc724babc6cf94313e1b
cry <2091136672@foxmail.com>
2026-06-10 14:31:27 +0800

    Fix expected-error case and remove expected-warning as suggested

--- clang/test/SemaCXX/ext-int.cpp
+++ clang/test/SemaCXX/ext-int.cpp
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -fsyntax-only -verify %s -Wimplicit-int-conversion -Wno-unused -Wunevaluated-expression -triple aarch64-unknown-unknown
+// RUN: %clang_cc1 -fsyntax-only -ffixed-point -verify %s -Wimplicit-int-conversion -Wno-unused -Wunevaluated-expression -triple aarch64-unknown-unknown
 
 template<int Bounds>
 struct HasExtInt {
@@ -319,6 +319,5 @@ void FromPaper2(_BitInt(8) a1, _BitInt(24) a2) {
 
 namespace GH196948{
   constexpr _BitInt(128) i = 42;
-  static_assert(i == 42.0k);// expected-error {{invalid operands to binary expression ('const _Bitint(128)' and '_Accum')}} \
-                            // expected-warning {{'static_assert' with no message is a C++17 extension}}
+  static_assert(i == 42.0k); // expected-error {{invalid operands to binary expression ('const _BitInt(128)' and '_Accum')}}
 }
