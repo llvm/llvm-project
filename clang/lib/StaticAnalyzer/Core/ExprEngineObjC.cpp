carbon-language/carbon-lang

commit 5481cfefa63624c4a91da0e05a1140e29ce6f65a

[analyzer] ObjCSelfInitChecker should always clean up in postCall checks.
ObjCSelfInitChecker stashes information in the GDM to persist it across
function calls; it is stored in pre-call checks and retrieved post-call.
The post-call check is supposed to clear out the stored state, but was
failing to do so in cases where the call did not have a symbolic return
value.

This was actually causing the inappropriate cache-out from r163361.
Per discussion with Anna, we should never actually cache out when
assuming the receiver of an Objective-C message is non-nil, because
we guarded that node generation by checking that the state has changed.
Therefore, the only states that could reach this exact ExplodedNode are
ones that should have merged /before/ making this assumption.

r163361 has been reverted and the test case removed, since it won't
actually test anything interesting now.

llvm-svn: 163449

clang/lib/StaticAnalyzer/Core/ExprEngineObjC.cpp
@@ -192,8 +192,10 @@ void ExprEngine::VisitObjCMessage(const ObjCMessageExpr *ME,
         }
         
         // Generate a transition to non-Nil state.
-        if (notNilState != State)
+        if (notNilState != State) {
           Pred = Bldr.generateNode(currStmt, Pred, notNilState);
+          assert(Pred && "Should have cached out already!");
+        }
       }
     } else {
       // Check for special class methods.
@@ -245,9 +247,7 @@ void ExprEngine::VisitObjCMessage(const ObjCMessageExpr *ME,
       }
     }
 
-    // Evaluate the call if we haven't cached out.
-    if (Pred)
-      defaultEvalCall(Bldr, Pred, *UpdatedMsg);
+    defaultEvalCall(Bldr, Pred, *UpdatedMsg);
   }
   
   ExplodedNodeSet dstPostvisit;

clang/lib/StaticAnalyzer/Checkers/ObjCSelfInitChecker.cpp
@@ -140,7 +140,8 @@ static void addSelfFlag(ProgramStateRef state, SVal val,
                         SelfFlagEnum flag, CheckerContext &C) {
   // We tag the symbol that the SVal wraps.
   if (SymbolRef sym = val.getAsSymbol())
-    C.addTransition(state->set<SelfFlag>(sym, getSelfFlags(val, C) | flag));
+    state = state->set<SelfFlag>(sym, getSelfFlags(val, state) | flag);
+  C.addTransition(state);
 }
 
 static bool hasSelfFlag(SVal val, SelfFlagEnum flag, CheckerContext &C) {
@@ -310,7 +311,7 @@ void ObjCSelfInitChecker::checkPostCall(const CallEvent &CE,
       const Expr *CallExpr = CE.getOriginExpr();
       if (CallExpr)
         addSelfFlag(state, state->getSVal(CallExpr, C.getLocationContext()),
-                                          prevFlags, C);
+                    prevFlags, C);
       return;
     }
   }

clang/test/Analysis/retain-release-crashes.m
@@ -1,62 +1,0 @@
-// RUN: %clang_cc1 -triple x86_64-apple-darwin10 -analyze -analyzer-checker=core,osx.cocoa.RetainCount,osx.cocoa.SelfInit,debug.ExprInspection -verify -w %s
-
-// This file contains crash regression tests; please do not remove any checkers
-// from the RUN line because they may have been necessary to produce the crash.
-// (Adding checkers should be fine.)
-
-void clang_analyzer_eval(int);
-
-@interface NSObject
-- (id)init;
-@end
-
-@interface Foo : NSObject
-@end
-
-void touch(id, SEL);
-id getObject();
-int getInt();
-
-
-@implementation Foo
-// Bizarre crash related to the ExprEngine reaching a previously-seen
-// ExplodedNode /during/ the processing of a message. Removing any
-// parts of this test case seem not to trigger the crash any longer.
-// <rdar://problem/12243648>
-- (id)init {
-  // Step 0: properly call the superclass's initializer
-  self = [super init];
-  if (!self) return self;
-
-  // Step 1: Perturb the state with a new conjured symbol.
-  int value = getInt();
-
-  // Step 2: Loop. Some loops seem to trigger this, some don't.
-  // The original used a for-in loop.
-  while (--value) {
-    // Step 3: Make it impossible to retain-count 'self' by calling
-    // a function that takes a "callback" (in this case, a selector).
-    // Note that this does not trigger the crash if you use a message!
-    touch(self, @selector(hi));
-  }
-
-  // Step 4: Use 'self', so that we know it's non-nil.
-  [self bar];
-
-  // Step 5: Once again, make it impossible to retain-count 'self'...
-  // ...while letting ObjCSelfInitChecker mark this as an interesting
-  // message, since 'self' is an argument...
-  // ...but this time do it in such a way that we'll also assume that
-  // 'other' is non-nil. Once we've made the latter assumption, we
-  // should cache out.
-  id other = getObject();
-  [other use:self withSelector:@selector(hi)];
-
-  // Step 6: Check that we did, in fact, keep the assumptions about 'self'
-  // and 'other' being non-nil.
-  clang_analyzer_eval(other != 0); // expected-warning{{TRUE}}
-  clang_analyzer_eval(self != 0); // expected-warning{{TRUE}}
-
-  return self;
-}
-@end

llvm/llvm-project

commit 8103de354cf7ae9056fe6d317911e93eed7e0c39

Remove some pointless assertions
The ancestor of these assertions was originally introduced in 2012 by
commit 5481cfefa63624c4a91da0e05a1140e29ce6f65a, to rule out regressions
of a bug where incorrect management of the analyzer state led to caching
out in a logically inconsistent place.

That original assertion was a simple and strong
```
  Pred = Bldr.generateNode(currStmt, Pred, notNilState);
  assert(Pred && "Should have cached out already!");
```
which provided the useful invariant that "Pred cannot be null here".

As the code evolved, the assertion was moved to the engine from
`ObjCSelfInitChecker`, duplicated and most importantly, weakened by
allowing that the new `Pred` may be null if the old `Pred` had a tag.

As the old `Pred` can easily have a tag (and this fact is independent of
everything else that happens around here), the new assertion does not
provide any useful invariant -- we still need to handle the case when
the new `Pred` is nullptr.

clang/lib/StaticAnalyzer/Core/ExprEngineObjC.cpp
@@ -196,11 +196,8 @@ void ExprEngine::VisitObjCMessage(const ObjCMessageExpr *ME,
 
       // Receiver is definitely nil, so run ObjCMessageNil callbacks and return.
       if (nilState && !notNilState) {
-        bool HasTag = Pred->getLocation().getTag();
         PreStmt PS(ME, Pred->getStackFrame(), nullptr);
         Pred = Engine.makeNode(PS, nilState, Pred);
-        assert((Pred || HasTag) && "Should have cached out already!");
-        (void)HasTag;
         if (!Pred)
           return;
 
@@ -215,10 +212,7 @@ void ExprEngine::VisitObjCMessage(const ObjCMessageExpr *ME,
       // Generate a transition to the non-nil state, dropping any potential
       // nil flow.
       if (notNilState != State) {
-        bool HasTag = Pred->getLocation().getTag();
         Pred = Engine.makePostStmtNode(ME, notNilState, Pred);
-        assert((Pred || HasTag) && "Should have cached out already!");
-        (void)HasTag;
         if (!Pred)
           return;
       }