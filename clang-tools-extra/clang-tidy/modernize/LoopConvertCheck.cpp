539f9f7843141db305242c2d07e27d21b51240c3
Gaurav Dhingra <gauravdhingra.gxyd@gmail.com>
2026-06-10 18:17:12 +0530

    just use `assert(PrevToken)`

--- clang-tools-extra/clang-tidy/modernize/LoopConvertCheck.cpp
+++ clang-tools-extra/clang-tidy/modernize/LoopConvertCheck.cpp
@@ -560,7 +560,7 @@ isPrecededByAdjacentIdentifierOrKeyword(const SourceManager &SourceMgr,
                                         SourceLocation BeginLocation) {
   std::optional<Token> PrevToken =
       Lexer::findPreviousToken(BeginLocation, SourceMgr, LangOpts, true);
-  assert(PrevToken && "Expected a token before the dereference operator");
+  assert(PrevToken);
   // Check whether the token at `BeginLocation` is immediately adjacent to
   // the previous token with no space between them.
   const bool IsAdjacentToPrevToken = PrevToken->getEndLoc() == BeginLocation;
