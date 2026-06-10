9b7d0c8f871b1fbc7d106ed4e2c7707f67628934
Gaurav Dhingra <gauravdhingra.gxyd@gmail.com>
2026-06-10 18:39:35 +0530

    revert to `if (!PrevToken)` for now

--- clang-tools-extra/clang-tidy/modernize/LoopConvertCheck.cpp
+++ clang-tools-extra/clang-tidy/modernize/LoopConvertCheck.cpp
@@ -560,7 +560,8 @@ isPrecededByAdjacentIdentifierOrKeyword(const SourceManager &SourceMgr,
                                         SourceLocation BeginLocation) {
   std::optional<Token> PrevToken =
       Lexer::findPreviousToken(BeginLocation, SourceMgr, LangOpts, true);
-  assert(PrevToken);
+  if (!PrevToken)
+    return false;
   // Check whether the token at `BeginLocation` is immediately adjacent to
   // the previous token with no space between them.
   const bool IsAdjacentToPrevToken = PrevToken->getEndLoc() == BeginLocation;
