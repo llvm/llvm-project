978ec5be96ad9af60051d2db5da29d6332917db8
Iasonaskrpr <iaskarapro@gmail.com>
2026-06-10 21:04:46 +0300

    [lldb][Fortran] Added handling for arrays in DWARFASTParser, DWARFExpressions not yet stored or handled

--- lldb/source/Plugins/SymbolFile/DWARF/DWARFASTParserFortran.cpp
+++ lldb/source/Plugins/SymbolFile/DWARF/DWARFASTParserFortran.cpp
@@ -19,6 +19,7 @@
 
 #include "lldb/Symbol/CompileUnit.h"
 #include "lldb/Utility/Log.h"
+#include "lldb/ValueObject/ValueObject.h"
 
 using namespace lldb;
 using namespace lldb_private;
@@ -29,6 +30,125 @@ DWARFASTParserFortran::DWARFASTParserFortran(TypeSystemFortran &ast)
     : DWARFASTParser(Kind::DWARFASTParserFortran), m_ast(ast) {}
 
 DWARFASTParserFortran::~DWARFASTParserFortran() {}
+
+// Struct holding extra info needed to characterize Fortran Arrays
+// TODO: Add comments explaining each one
+struct FortranArrayInfo {
+
+  bool is_star = false;
+  bool is_allocatable = false;
+  bool size_known = true;
+
+  llvm::SmallVector<std::optional<uint64_t>, 1> elements_per_dimension;
+  llvm::SmallVector<std::optional<uint64_t>, 1> byte_stride;
+  llvm::SmallVector<int64_t, 1> lower_bounds;
+};
+
+FortranArrayInfo ParseArray(const DWARFDIE &parent_die,
+                            const ExecutionContext *exe_ctx) {
+  FortranArrayInfo array_info;
+  for (DWARFDIE die : parent_die.children()) {
+    const dw_tag_t tag = die.Tag();
+    if (tag != DW_TAG_subrange_type)
+      continue;
+    // If a subrange of an array is a star meaning we can't infer how many
+    // elements it has it is always the last dimension and is identified by not
+    // having a DW_AT_count attribute, if this at the end is true then it is a
+    // star and needs to be treated as such
+    array_info.is_star = true;
+    DWARFAttributes attributes = die.GetAttributes();
+    if (attributes.Size() == 0)
+      continue;
+
+    std::optional<uint64_t> num_elements;
+    std::optional<uint64_t> byte_stride;
+    int64_t lower_bound = 1;
+    int64_t upper_bound = 0;
+    bool upper_bound_valid = false;
+    for (size_t i = 0; i < attributes.Size(); ++i) {
+      const dw_attr_t attr = attributes.AttributeAtIndex(i);
+      DWARFFormValue form_value;
+      if (attributes.ExtractFormValueAtIndex(i, form_value)) {
+        switch (attr) {
+        case DW_AT_name:
+          break;
+
+        case DW_AT_count:
+          array_info.is_star = false;
+          if (DWARFDIE var_die = die.GetReferencedDIE(DW_AT_count)) {
+            if (var_die.Tag() == DW_TAG_variable)
+              if (exe_ctx) {
+                if (auto frame = exe_ctx->GetFrameSP()) {
+                  Status error;
+                  lldb::VariableSP var_sp;
+                  auto valobj_sp = frame->GetValueForVariableExpressionPath(
+                      var_die.GetName(), eNoDynamicValues, 0, var_sp, error);
+                  if (valobj_sp) {
+                    num_elements = valobj_sp->GetValueAsUnsigned(0);
+                    break;
+                  }
+                }
+              }
+          } else if (DWARFFormValue::IsBlockForm(form_value.Form())) {
+            // TODO: Handle cases where we have to read the dope vector
+            array_info.size_known = false;
+          } else
+            num_elements = form_value.Unsigned();
+          break;
+
+        case DW_AT_byte_stride:
+          if (DWARFFormValue::IsBlockForm(form_value.Form())) {
+            // TODO: Handle cases where we have to read the dope vector
+            byte_stride = 0;
+            array_info.size_known = false;
+          } else
+            byte_stride = form_value.Unsigned();
+          break;
+
+        case DW_AT_lower_bound:
+          // TODO: Handle cases where we have to read the dope vector
+          if (DWARFFormValue::IsBlockForm(form_value.Form())) {
+            lower_bound = 1;
+            array_info.size_known = false;
+          }
+
+          else
+            lower_bound = form_value.Signed();
+          break;
+
+        case DW_AT_upper_bound:
+          upper_bound_valid = true;
+          // TODO: Handle cases where we have to read the dope vector
+          if (DWARFFormValue::IsBlockForm(form_value.Form())) {
+            array_info.size_known = false;
+            upper_bound = 1;
+          } else
+            upper_bound = form_value.Signed();
+          break;
+
+        case DW_AT_allocated:
+          array_info.is_allocatable = true;
+          array_info.size_known = false;
+          // TODO: Save the DWARFExpression so we can query the dope vector
+          // every time
+
+        default:
+          break;
+        }
+      }
+    }
+
+    if (!num_elements || *num_elements == 0) {
+      if (upper_bound_valid && upper_bound >= lower_bound)
+        num_elements = upper_bound - lower_bound + 1;
+    }
+
+    array_info.elements_per_dimension.push_back(num_elements);
+    array_info.byte_stride.push_back(byte_stride);
+    array_info.lower_bounds.push_back(lower_bound);
+  }
+  return array_info;
+}
 
 // TODO: Add more logging here there is not enough
 lldb::TypeSP DWARFASTParserFortran::ParseTypeFromDWARF(const SymbolContext &sc,
@@ -100,6 +220,61 @@ lldb::TypeSP DWARFASTParserFortran::ParseTypeFromDWARF(const SymbolContext &sc,
             dwarf->MakeType(die.GetID(), type_name, (bit_size + 7) / 8, nullptr,
                             LLDB_INVALID_UID, Type::eEncodingIsUID, decl,
                             compiler_type, Type::ResolveState::Full);
+      } break;
+      case DW_TAG_array_type: {
+        dwarf->GetDIEToType()[die.GetDIE()] = DIE_IS_BEING_PARSED;
+
+        uint32_t byte_stride = 0;
+        uint32_t bit_stride = 0;
+
+        DWARFDIE element_die = die.GetAttributeValueAsReferenceDIE(DW_AT_type);
+
+        Type *element_type = dwarf->ResolveTypeUID(element_die, true);
+        if (element_type) {
+
+          CompilerType array_element_type =
+              element_type->GetForwardCompilerType();
+          uint64_t total_array_size = 0;
+          uint64_t total_elements = 0;
+          if (array_element_type.GetCompleteType()) {
+            FortranArrayInfo array_info = ParseArray(die, nullptr);
+            // We need to calculate the total array size, if it is known
+            // at compile time
+            if (array_info.size_known) {
+              for (int idx = 0; idx < array_info.byte_stride.size(); idx++) {
+                total_elements += *array_info.elements_per_dimension[idx];
+                if (array_info.byte_stride[idx])
+                  total_array_size +=
+                      (*array_info.elements_per_dimension[idx]) *
+                      (*array_info.byte_stride[idx]);
+                else
+                  total_array_size +=
+                      (*array_info.elements_per_dimension[idx]) *
+                      *array_element_type.GetByteSize(nullptr);
+              }
+            }
+
+            compiler_type = m_ast.CreateArrayType(
+                array_info.elements_per_dimension, array_info.byte_stride,
+                array_info.lower_bounds, array_element_type,
+                array_info.is_allocatable, array_info.is_star,
+                total_array_size * 8, total_elements);
+
+            type_sp = dwarf->MakeType(
+                die.GetID(), compiler_type.GetTypeName(), total_array_size,
+                nullptr, LLDB_INVALID_UID, Type::eEncodingIsUID, decl,
+                compiler_type, Type::ResolveState::Full);
+            type_sp->SetEncodingType(element_type);
+          } else {
+            dwarf->GetObjectFile()->GetModule()->LogMessage(
+                log,
+                "DWARFASTParserFortran::ParseTypeFromDWARF (die = 0x%8.8x) %s "
+                "name "
+                "= '%s'), incomplete type array element not supported, yet!.",
+                die.GetOffset(), plugin::dwarf::DW_TAG_value_to_name(die.Tag()),
+                die.GetName());
+          }
+        }
       } break;
       case DW_TAG_subprogram:
       case DW_TAG_subroutine_type: {
