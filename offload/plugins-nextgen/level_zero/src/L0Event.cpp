c774b77fce258a0e6438e30fcdce94031e00ee64
Duran, Alex <alejandro.duran@intel.com>
2026-06-11 09:37:44 -0700

    rename

--- offload/plugins-nextgen/level_zero/src/L0Event.cpp
+++ offload/plugins-nextgen/level_zero/src/L0Event.cpp
@@ -26,14 +26,14 @@ Expected<ze_event_handle_t> EventPoolTy::getEventLocked() {
     Desc.flags = ZE_EVENT_POOL_FLAG_HOST_VISIBLE | Flags;
     Desc.count = static_cast<uint32_t>(PoolSize);
 
-    ze_event_pool_counter_based_exp_desc_t counterBasedDesc = {
+    ze_event_pool_counter_based_exp_desc_t CounterBasedDesc = {
         ZE_STRUCTURE_TYPE_COUNTER_BASED_EVENT_POOL_EXP_DESC,
         /* pNext */ nullptr,
         /* flags */ 0};
-    counterBasedDesc.flags = ZE_EVENT_POOL_COUNTER_BASED_EXP_FLAG_IMMEDIATE;
+    CounterBasedDesc.flags = ZE_EVENT_POOL_COUNTER_BASED_EXP_FLAG_IMMEDIATE;
 
     if (UseCounterBasedEvents)
-      Desc.pNext = &counterBasedDesc;
+      Desc.pNext = &CounterBasedDesc;
 
     ze_event_pool_handle_t Pool;
     CALL_ZE_RET_ERROR(zeEventPoolCreate, Context, &Desc, 0, nullptr, &Pool);
