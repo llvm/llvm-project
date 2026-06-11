a48b0fa2229ab5256fd231c9b6b2a9e906dfa894
Vladimir Vereschaka <vvereschaka@accesssoftek.com>
2026-06-10 17:02:05 -0700

    Update `cl-deterministic` test to run correctly on Darwin.

--- clang/test/Driver/cl-deterministic.c
+++ clang/test/Driver/cl-deterministic.c
@@ -1,15 +1,15 @@
-// RUN: %clang_cl -fno-integrated-cc1 -E /experimental:deterministic /d1nodatetime %s
-// RUN: %clang_cl -fno-integrated-cc1 -E /D IS_SYSHEADER=1 /experimental:deterministic /d1nodatetime %s
+// RUN: %clang_cl -fno-integrated-cc1 -E /experimental:deterministic /d1nodatetime -- %s
+// RUN: %clang_cl -fno-integrated-cc1 -E /D IS_SYSHEADER=1 /experimental:deterministic /d1nodatetime -- %s
 
-// RUN: %clang_cl -E /experimental:deterministic /d1nodatetime %s
-// RUN: %clang_cl -E /D IS_SYSHEADER=1 /experimental:deterministic /d1nodatetime %s
+// RUN: %clang_cl -E /experimental:deterministic /d1nodatetime -- %s
+// RUN: %clang_cl -E /D IS_SYSHEADER=1 /experimental:deterministic /d1nodatetime -- %s
 
 // not %clang_cc1 -Werror=date-time -Wno-builtin-macro-redefined %s -DIS_SYSHEADER -E 2>&1 | grep 'error: expansion' | count 3
 
-// RUN: %clang_cl -E -### /experimental:deterministic %s 2>&1 | FileCheck %s --check-prefix=WDATETIME
+// RUN: %clang_cl -E -### /experimental:deterministic -- %s 2>&1 | FileCheck %s --check-prefix=WDATETIME
 // WDATETIME: -Wdate-time
 // WDATETIME: -mno-incremental-linker-compatible
-// RUN: %clang_cl -E -### /d1nodatetime %s 2>&1 | FileCheck %s --check-prefix=MACROREDEF
+// RUN: %clang_cl -E -### /d1nodatetime -- %s 2>&1 | FileCheck %s --check-prefix=MACROREDEF
 // MACROREDEF: -init-datetime-macros=undefined
 
 #ifdef IS_HEADER
