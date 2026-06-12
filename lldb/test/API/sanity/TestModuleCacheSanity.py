c614eb8ac89686bdc9144f76e07468edd6bdf3ba
Leonard Grey <leonard@leonardgrey.com>
2026-06-12 19:23:03 -0400

    Simplify test assertions

--- lldb/test/API/sanity/TestModuleCacheSanity.py
+++ lldb/test/API/sanity/TestModuleCacheSanity.py
@@ -4,8 +4,6 @@ correctly and points inside the default test build directory.
 """
 
 
-import sys
-
 import lldb
 import lldbsuite.test.lldbutil as lldbutil
 from lldbsuite.test.lldbtest import *
@@ -15,10 +13,7 @@ class ModuleCacheSanityTestCase(TestBase):
     NO_DEBUG_INFO_TESTCASE = True
 
     def test(self):
-        build_dir_name = (
-            "lldb-test-build.noindex" if sys.platform == "darwin" else "lldb-test-build"
-        )
         self.expect(
             "settings show symbols.clang-modules-cache-path",
-            substrs=[build_dir_name, "module-cache-lldb"],
+            substrs=["lldb-test-build", "module-cache-lldb"],
         )
