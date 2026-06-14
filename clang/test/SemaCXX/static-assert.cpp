66e68710b4a2e4e220d45a54f18a891a52abe595
Fan Mo <w007878@hotmail.com>
2026-06-13 21:42:16 -0500

    fix test

--- clang/test/SemaCXX/static-assert.cpp
+++ clang/test/SemaCXX/static-assert.cpp
@@ -369,12 +369,10 @@ namespace GH203701 {
   struct S {
     constexpr S(auto) {}
     constexpr operator int() const { return 0; }
-    constexpr static f() const { return 0; }
   };
 
   constexpr auto a = [](this S) { return 1; };
 
-  static_assert((&decltype(a)::operator())(1) == 42); // expected-error {{static assertion failed}}
-  static_assert((&S::operator int) == nullptr, ""); // expected-error {{static assertion failed}}
-  static_assert((S::f() == 1)); // expected-error {{static assertion failed}}
+  static_assert((&decltype(a)::operator())(1) == 42, ""); // expected-error-re {{static assertion failed due to requirement '(&const GH203701::(lambda at {.*})::operator())(1) == 42'}}
+  static_assert((&S::operator int) == nullptr, ""); // expected-error {{static assertion failed due to requirement '(&GH203701::S::operator int) == nullptr'}}
 }
