beadaec428a65a901be47964619121cc5b889635
Yingwei Zheng <dtcxzyw2333@gmail.com>
2026-06-14 19:11:28 +0800

    [llubi] Add support for constant expressions (#203746)
    This patch adds support for most kinds of constant expressions, except
    for ptrtoint/inttoptr. Casting between pointers and integers is
    stateful, so they cannot be cached. I plan to implement them in
    subsequent patches. ptrtoaddr is also supported in this patch to block
    constant folding.
    
    The logic in `evaluateConstantExpression` duplicates the interpreter's
    code in `visit*` methods. But I think it is acceptable. Only the GEP
    computation is reused.

--- llvm/tools/llubi/lib/Context.cpp
+++ llvm/tools/llubi/lib/Context.cpp
@@ -11,6 +11,8 @@
 //===----------------------------------------------------------------------===//
 
 #include "Context.h"
+#include "llvm/IR/GetElementPtrTypeIterator.h"
+#include "llvm/IR/Instructions.h"
 #include "llvm/Support/MathExtras.h"
 
 namespace llvm::ubi {
@@ -93,7 +95,7 @@ std::optional<AnyValue> Context::getConstantValueImpl(Constant *C) {
     return AnyValue::getNullValue(*this, C->getType());
 
   if (isa<ConstantPointerNull>(C))
-    return Pointer::null(C->getType()->getPointerAddressSpace(), DL);
+    return AnyValue::getNullValue(*this, C->getType());
 
   if (auto *CI = dyn_cast<ConstantInt>(C)) {
     if (auto *VecTy = dyn_cast<VectorType>(CI->getType()))
@@ -142,7 +144,184 @@ std::optional<AnyValue> Context::getConstantValueImpl(Constant *C) {
   if (auto *F = dyn_cast<Function>(C))
     return FuncAddrMap.at(F);
 
+  if (auto *CE = dyn_cast<ConstantExpr>(C))
+    return evaluateConstantExpression(CE);
+
   return std::nullopt;
+}
+
+std::optional<AnyValue> Context::evaluateConstantExpression(ConstantExpr *CE) {
+  unsigned Opc = CE->getOpcode();
+  switch (Opc) {
+  case Instruction::Trunc: {
+    const AnyValue *Src = getConstantValue(CE->getOperand(0));
+    if (!Src)
+      return std::nullopt;
+    if (Src->isPoison())
+      return AnyValue::poison();
+    unsigned BitWidth = CE->getType()->getScalarSizeInBits();
+    if (Src->isInteger())
+      return AnyValue(Src->asInteger().trunc(BitWidth));
+    std::vector<AnyValue> Vec = Src->asAggregate();
+    for (auto &V : Vec) {
+      if (V.isInteger())
+        V = V.asInteger().trunc(BitWidth);
+    }
+    return AnyValue(std::move(Vec));
+  }
+  case Instruction::BitCast: {
+    Constant *SrcOp = CE->getOperand(0);
+    const AnyValue *Src = getConstantValue(SrcOp);
+    if (!Src)
+      return std::nullopt;
+    SmallVector<Byte> Bytes;
+    Bytes.resize(getEffectiveTypeStoreSize(CE->getType()), Byte::concrete(0));
+    toBytes(*Src, SrcOp->getType(), Bytes);
+    return fromBytes(Bytes, CE->getType());
+  }
+  case Instruction::InsertElement: {
+    const AnyValue *Src = getConstantValue(CE->getOperand(0));
+    if (!Src)
+      return std::nullopt;
+    const AnyValue *Val = getConstantValue(CE->getOperand(1));
+    if (!Val)
+      return std::nullopt;
+    const AnyValue *Idx = getConstantValue(CE->getOperand(2));
+    if (!Idx)
+      return std::nullopt;
+    auto &SrcVec = Src->asAggregate();
+    if (Idx->isPoison() || Idx->asInteger().uge(SrcVec.size()))
+      return AnyValue::getPoisonValue(*this, CE->getType());
+    std::vector<AnyValue> ResVec = SrcVec;
+    ResVec[Idx->asInteger().getZExtValue()] = *Val;
+    return AnyValue(std::move(ResVec));
+  }
+  case Instruction::ExtractElement: {
+    const AnyValue *Src = getConstantValue(CE->getOperand(0));
+    if (!Src)
+      return std::nullopt;
+    const AnyValue *Idx = getConstantValue(CE->getOperand(1));
+    if (!Idx)
+      return std::nullopt;
+    auto &SrcVec = Src->asAggregate();
+    if (Idx->isPoison() || Idx->asInteger().uge(SrcVec.size()))
+      return AnyValue::getPoisonValue(*this, CE->getType());
+    return SrcVec[Idx->asInteger().getZExtValue()];
+  }
+  case Instruction::ShuffleVector: {
+    const AnyValue *LHS = getConstantValue(CE->getOperand(0));
+    if (!LHS)
+      return std::nullopt;
+    const AnyValue *RHS = getConstantValue(CE->getOperand(1));
+    if (!RHS)
+      return std::nullopt;
+    auto &LHSVec = LHS->asAggregate();
+    auto &RHSVec = RHS->asAggregate();
+    uint32_t Size = cast<VectorType>(CE->getOperand(0)->getType())
+                        ->getElementCount()
+                        .getKnownMinValue();
+    std::vector<AnyValue> Res;
+    uint32_t DstLen =
+        getEVL(cast<VectorType>(CE->getType())->getElementCount());
+    Res.reserve(DstLen);
+    uint32_t Stride = CE->getShuffleMask().size();
+    // For scalable vectors, we need to repeat the shuffle mask until we fill
+    // the destination vector.
+    for (uint32_t Off = 0; Off != DstLen; Off += Stride) {
+      for (int Idx : CE->getShuffleMask()) {
+        if (Idx == PoisonMaskElem)
+          Res.push_back(AnyValue::poison());
+        else if (Idx < static_cast<int>(Size))
+          Res.push_back(LHSVec[Idx]);
+        else
+          Res.push_back(RHSVec[Idx - Size]);
+      }
+    }
+    return AnyValue(std::move(Res));
+  }
+  case Instruction::GetElementPtr: {
+    // Temporary variable for reference to poison values when the subexpression
+    // cannot be evaluated. As the reference will be consumed immediately, we
+    // don't need to store them into a list.
+    AnyValue PoisonValue;
+    AnyValue Res =
+        computeGEP(*cast<GEPOperator>(CE), [&](Value *V) -> const AnyValue & {
+          const AnyValue *Val = getConstantValue(cast<Constant>(V));
+          if (Val)
+            return *Val;
+          PoisonValue = AnyValue::getPoisonValue(*this, V->getType());
+          return PoisonValue;
+        });
+    if (!PoisonValue.isNone())
+      return std::nullopt;
+    return std::move(Res);
+  }
+  case Instruction::PtrToAddr: {
+    const AnyValue *Src = getConstantValue(CE->getOperand(0));
+    if (!Src)
+      return std::nullopt;
+    if (Src->isPoison())
+      return AnyValue::poison();
+    unsigned BitWidth = CE->getType()->getScalarSizeInBits();
+    if (Src->isPointer())
+      return Src->asPointer().address().trunc(BitWidth);
+    std::vector<AnyValue> Vec = Src->asAggregate();
+    for (auto &V : Vec) {
+      if (V.isPointer())
+        V = V.asPointer().address().trunc(BitWidth);
+    }
+    return AnyValue(std::move(Vec));
+  }
+  case Instruction::PtrToInt:
+  case Instruction::IntToPtr:
+  case Instruction::AddrSpaceCast:
+    return std::nullopt;
+  default:
+    assert(Instruction::isBinaryOp(Opc) && "Must be binary operator?");
+    const AnyValue *LHS = getConstantValue(CE->getOperand(0));
+    if (!LHS)
+      return std::nullopt;
+    const AnyValue *RHS = getConstantValue(CE->getOperand(1));
+    if (!RHS)
+      return std::nullopt;
+
+    bool HasNUW = false;
+    bool HasNSW = false;
+    if (auto *OBO = dyn_cast<OverflowingBinaryOperator>(CE)) {
+      HasNUW = OBO->hasNoUnsignedWrap();
+      HasNSW = OBO->hasNoSignedWrap();
+    }
+
+    auto ScalarEval = [&](const AnyValue &LHS,
+                          const AnyValue &RHS) -> AnyValue {
+      if (LHS.isPoison() || RHS.isPoison())
+        return AnyValue::poison();
+      auto &LHSVal = LHS.asInteger();
+      auto &RHSVal = RHS.asInteger();
+      switch (Opc) {
+      case Instruction::Add:
+        return addNoWrap(LHSVal, RHSVal, HasNSW, HasNUW);
+      case Instruction::Sub:
+        return subNoWrap(LHSVal, RHSVal, HasNSW, HasNUW);
+      case Instruction::Xor:
+        return LHSVal ^ RHSVal;
+      default:
+        llvm_unreachable("Unsupported opcode in constant expression.");
+      }
+    };
+
+    if (CE->getType()->isVectorTy()) {
+      auto &LHSVec = LHS->asAggregate();
+      auto &RHSVec = RHS->asAggregate();
+      std::vector<AnyValue> ResVec;
+      ResVec.reserve(LHSVec.size());
+      for (const auto &[ScalarLHS, ScalarRHS] : zip(LHSVec, RHSVec))
+        ResVec.push_back(ScalarEval(ScalarLHS, ScalarRHS));
+      return std::move(ResVec);
+    }
+
+    return ScalarEval(*LHS, *RHS);
+  }
 }
 
 const AnyValue *Context::getConstantValue(Constant *C) {
@@ -545,6 +724,172 @@ void Context::freeze(AnyValue &Val, Type *Ty) {
       llvm_unreachable("Invalid aggregate type");
     }
   }
+}
+
+AnyValue Context::computePtrAdd(const Pointer &Ptr, const APInt &Offset,
+                                GEPNoWrapFlags Flags,
+                                AnyValue &AccumulatedOffset) {
+  if (Offset.isZero())
+    return Ptr;
+  APInt IndexBits = Ptr.address().trunc(Offset.getBitWidth());
+  auto NewIndex =
+      addNoWrap(IndexBits, Offset, /*HasNSW=*/false, Flags.hasNoUnsignedWrap());
+  if (NewIndex.isPoison())
+    return AnyValue::poison();
+  if (Flags.hasNoUnsignedSignedWrap()) {
+    // The successive addition of the current address, truncated to the
+    // pointer index type and interpreted as an unsigned number, and each
+    // offset, interpreted as a signed number, does not wrap the pointer index
+    // type.
+    if (Offset.isNonNegative() ? NewIndex.asInteger().ult(IndexBits)
+                               : NewIndex.asInteger().ugt(IndexBits))
+      return AnyValue::poison();
+  }
+  APInt NewAddr = Ptr.address();
+  NewAddr.insertBits(NewIndex.asInteger(), 0);
+
+  MemoryObject *MO = nullptr;
+  if (Flags.isInBounds()) {
+    MO = checkProvenance(
+        Ptr, [](const Provenance &) { return true; },
+        /*HasSideEffect=*/false);
+    if (!MO || !MO->inBounds(NewAddr))
+      return AnyValue::poison();
+  }
+
+  if (!AccumulatedOffset.isPoison()) {
+    AccumulatedOffset =
+        addNoWrap(AccumulatedOffset.asInteger(), Offset,
+                  Flags.hasNoUnsignedSignedWrap(), Flags.hasNoUnsignedWrap());
+    if (AccumulatedOffset.isPoison())
+      return AnyValue::poison();
+  }
+
+  // Should not expose provenance here even if the new address doesn't point
+  // to the original object.
+  auto Res = Ptr.getWithNewAddr(NewAddr);
+  if (MO) {
+    auto &Prov = Res.provenance();
+    if (Prov.isWildcard() && !Prov.getMemoryObject())
+      Res = Res.getWithNewProvenance(Prov.getWithKnownMemoryObject(*MO));
+  }
+  return Res;
+}
+
+AnyValue Context::computePtrAdd(const AnyValue &Ptr, const APInt &Offset,
+                                GEPNoWrapFlags Flags,
+                                AnyValue &AccumulatedOffset) {
+  if (Ptr.isPoison())
+    return AnyValue::poison();
+  return computePtrAdd(Ptr.asPointer(), Offset, Flags, AccumulatedOffset);
+}
+
+AnyValue Context::computeScaledPtrAdd(const AnyValue &Ptr,
+                                      const AnyValue &Index, const APInt &Scale,
+                                      GEPNoWrapFlags Flags,
+                                      AnyValue &AccumulatedOffset) {
+  if (Ptr.isPoison() || Index.isPoison())
+    return AnyValue::poison();
+  assert(Ptr.isPointer() && Index.isInteger() && "Unexpected type.");
+  if (Scale.isOne())
+    return computePtrAdd(Ptr, Index.asInteger(), Flags, AccumulatedOffset);
+  auto ScaledOffset =
+      mulNoWrap(Index.asInteger(), Scale, Flags.hasNoUnsignedSignedWrap(),
+                Flags.hasNoUnsignedWrap());
+  if (ScaledOffset.isPoison())
+    return AnyValue::poison();
+  return computePtrAdd(Ptr, ScaledOffset.asInteger(), Flags, AccumulatedOffset);
+}
+
+static AnyValue canonicalizeIndex(const AnyValue &Idx, unsigned IndexBitWidth,
+                                  GEPNoWrapFlags Flags) {
+  if (Idx.isPoison())
+    return AnyValue::poison();
+  auto &IdxInt = Idx.asInteger();
+  if (IdxInt.getBitWidth() == IndexBitWidth)
+    return Idx;
+  if (IdxInt.getBitWidth() > IndexBitWidth) {
+    if (Flags.hasNoUnsignedSignedWrap() && !IdxInt.isSignedIntN(IndexBitWidth))
+      return AnyValue::poison();
+
+    if (Flags.hasNoUnsignedWrap() && !IdxInt.isIntN(IndexBitWidth))
+      return AnyValue::poison();
+
+    return IdxInt.trunc(IndexBitWidth);
+  }
+  return IdxInt.sext(IndexBitWidth);
+}
+
+AnyValue
+Context::computeGEP(GEPOperator &GEP,
+                    function_ref<const AnyValue &(Value *V)> GetValue) {
+  uint32_t IndexBitWidth =
+      DL.getIndexSizeInBits(GEP.getType()->getPointerAddressSpace());
+  GEPNoWrapFlags Flags = GEP.getNoWrapFlags();
+  AnyValue Res = GetValue(GEP.getPointerOperand());
+  AnyValue AccumulatedOffset = APInt(IndexBitWidth, 0);
+  if (Res.isAggregate())
+    AccumulatedOffset =
+        AnyValue::getVectorSplat(AccumulatedOffset, Res.asAggregate().size());
+  auto ApplyScaledOffset = [&](const AnyValue &Index, const APInt &Scale) {
+    if (Index.isAggregate() && !Res.isAggregate()) {
+      Res = AnyValue::getVectorSplat(Res, Index.asAggregate().size());
+      AccumulatedOffset = AnyValue::getVectorSplat(AccumulatedOffset,
+                                                   Index.asAggregate().size());
+    }
+    if (Index.isAggregate() && Res.isAggregate()) {
+      for (auto &&[ResElem, IndexElem, OffsetElem] :
+           zip(Res.asAggregate(), Index.asAggregate(),
+               AccumulatedOffset.asAggregate()))
+        ResElem = computeScaledPtrAdd(
+            ResElem, canonicalizeIndex(IndexElem, IndexBitWidth, Flags), Scale,
+            Flags, OffsetElem);
+    } else {
+      AnyValue CanonicalIndex = canonicalizeIndex(Index, IndexBitWidth, Flags);
+      if (Res.isAggregate()) {
+        for (auto &&[ResElem, OffsetElem] :
+             zip(Res.asAggregate(), AccumulatedOffset.asAggregate()))
+          ResElem = computeScaledPtrAdd(ResElem, CanonicalIndex, Scale, Flags,
+                                        OffsetElem);
+      } else {
+        Res = computeScaledPtrAdd(Res, CanonicalIndex, Scale, Flags,
+                                  AccumulatedOffset);
+      }
+    }
+  };
+
+  for (gep_type_iterator GTI = gep_type_begin(GEP), GTE = gep_type_end(GEP);
+       GTI != GTE; ++GTI) {
+    Value *V = GTI.getOperand();
+
+    // Fast path for zero offsets.
+    if (auto *CI = dyn_cast<ConstantInt>(V)) {
+      if (CI->isZero())
+        continue;
+    }
+    if (isa<ConstantAggregateZero>(V))
+      continue;
+
+    // Handle a struct index, which adds its field offset to the pointer.
+    if (StructType *STy = GTI.getStructTypeOrNull()) {
+      unsigned ElementIdx = cast<ConstantInt>(V)->getZExtValue();
+      const StructLayout *SL = DL.getStructLayout(STy);
+      // Element offset is in bytes.
+      ApplyScaledOffset(APInt(IndexBitWidth, SL->getElementOffset(ElementIdx)),
+                        APInt(IndexBitWidth, 1));
+      continue;
+    }
+
+    // Truncate if type size exceeds index space.
+    // TODO: Should be documented in LangRef: GEPs with nowrap flags should
+    // return poison when the type size exceeds index space.
+    TypeSize Offset = GTI.getSequentialElementStride(DL);
+    APInt Scale(IndexBitWidth, getEffectiveTypeSize(Offset),
+                /*isSigned=*/false, /*implicitTrunc=*/true);
+    if (!Scale.isZero())
+      ApplyScaledOffset(GetValue(V), Scale);
+  }
+  return Res;
 }
 
 MemoryObject::~MemoryObject() = default;
