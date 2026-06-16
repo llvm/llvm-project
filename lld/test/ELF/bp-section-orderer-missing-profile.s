ef7785ea6b8936bbd0ec6bd31971d01ef9414534
peter941221 <peter941221@gmail.com>
2026-06-16 08:26:54 +0800

    [lld][test] Accept platform-specific missing-file casing

--- lld/test/ELF/bp-section-orderer-missing-profile.s
+++ lld/test/ELF/bp-section-orderer-missing-profile.s
@@ -2,7 +2,7 @@
 # RUN: llvm-mc -filetype=obj -triple=x86_64-unknown-linux %s -o %t.o
 # RUN: not ld.lld %t.o --irpgo-profile=%t.missing.profdata --bp-startup-sort=function 2>&1 | FileCheck %s
 
-# CHECK: error: No such file or directory
+# CHECK: error: {{[Nn]}}o such file or directory
 
 .globl _start
 _start:
--- lld/test/MachO/bp-section-orderer-missing-profile.s
+++ lld/test/MachO/bp-section-orderer-missing-profile.s
@@ -2,7 +2,7 @@
 # RUN: llvm-mc -filetype=obj -triple=x86_64-apple-darwin %s -o %t.o
 # RUN: not %lld -e _main -o /dev/null %t.o --irpgo-profile=%t.missing.profdata --bp-startup-sort=function 2>&1 | FileCheck %s
 
-# CHECK: error: No such file or directory
+# CHECK: error: {{[Nn]}}o such file or directory
 
 .text
 .globl _main
