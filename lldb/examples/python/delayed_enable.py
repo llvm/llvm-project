04d6720d40aef428e1eaa7f62c59135af1d4ea31
Jim Ingham <jingham@apple.com>
2026-06-16 15:16:17 -0700

    Formatting

--- lldb/examples/python/delayed_enable.py
+++ lldb/examples/python/delayed_enable.py
@@ -2,26 +2,32 @@ import lldb
 from lldb.plugins.parsed_cmd import ParsedCommand
 import time
 
+
 class EnableThemLater(ParsedCommand):
     program = "delayed_enable"
-    def __init__(self, debugger : lldb.SBDebugger, internal_dict):
+
+    def __init__(self, debugger: lldb.SBDebugger, internal_dict):
         super().__init__(debugger, internal_dict)
 
     def setup_command_definition(self):
         ov_parser = self.get_parser()
-        ov_parser.add_argument_set([
-            ov_parser.make_argument_element(lldb.eArgTypeUnsignedInteger,
-                                                 repeat = "plain", groups=None)])
+        ov_parser.add_argument_set(
+            [
+                ov_parser.make_argument_element(
+                    lldb.eArgTypeUnsignedInteger, repeat="plain", groups=None
+                )
+            ]
+        )
 
     def __call__(self, debugger, args_array, exe_ctx, result):
         target = exe_ctx.target
-        
+
         breakpoints = []
         for breakpoint in target.breakpoints:
             if breakpoint.enabled:
                 breakpoints.append(breakpoint)
                 breakpoint.SetEnabled(False)
-                
+
         interval = 5
         process = exe_ctx.process
         # If the process is running we don't need to do anything, lldb
@@ -41,20 +47,23 @@ class EnableThemLater(ParsedCommand):
 
         for breakpoint in breakpoints:
             breakpoint.SetEnabled(True)
-            
+
     def get_short_help(self):
         return "Disable all enabled breakpoints, sleep and re-enable"
 
     def get_long_help(self):
-        return ("Disable all enabled breakpoints in the target passed to the "
-                "command; sleep for 5 seconds - or the interval passed as the "
-                "argument command; then reenable them.  "
-                "\nThis is useful when you need to set up a situation in a GUI app w/o "
-                "hitting breakpoints, and then start hitting them again w/o disturbing "
-                "the GUI state.  For instance, if you want to hit a breakpoint "
-                "while testing a drag and drop action, you can't interact with "
-                "the debugger once you've started the drag so you need a delayed "
-                "enable.")
+        return (
+            "Disable all enabled breakpoints in the target passed to the "
+            "command; sleep for 5 seconds - or the interval passed as the "
+            "argument command; then reenable them.  "
+            "\nThis is useful when you need to set up a situation in a GUI app w/o "
+            "hitting breakpoints, and then start hitting them again w/o disturbing "
+            "the GUI state.  For instance, if you want to hit a breakpoint "
+            "while testing a drag and drop action, you can't interact with "
+            "the debugger once you've started the drag so you need a delayed "
+            "enable."
+        )
 
-def __lldb_init_module(debugger, internal_dict): 
+
+def __lldb_init_module(debugger, internal_dict):
     ParsedCommand.do_register_cmd(EnableThemLater, debugger, __name__)
