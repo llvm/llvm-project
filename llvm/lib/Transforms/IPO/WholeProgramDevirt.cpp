b8c24f65086283338b785a6a8cffaabe95a2cc64
J. Ryan Stinnett <ryan@convolv.es>
2026-06-16 17:23:47 +0100

    Revise WholeProgramDevirt check

--- llvm/lib/Transforms/IPO/WholeProgramDevirt.cpp
+++ llvm/lib/Transforms/IPO/WholeProgramDevirt.cpp
@@ -1924,7 +1924,7 @@ bool DevirtModule::tryVirtualConstProp(
     if (!Fn)
       return false;
 
-    if (!Fn->hasExactDefinition() ||
+    if (Fn->isDeclaration() || Fn->hasFnAttribute(Attribute::NoIPA) ||
         !computeFunctionBodyMemoryAccess(*Fn, FAM.getResult<AAManager>(*Fn))
              .doesNotAccessMemory() ||
         Fn->arg_empty() || !Fn->arg_begin()->use_empty() ||
