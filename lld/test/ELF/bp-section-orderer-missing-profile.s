d5d31dfc2255e791ab95a45d673866d30f603b54
peter941221 <peter941221@gmail.com>
2026-06-16 10:33:07 +0800

    [lld][test] Generalize missing-profile error checks

--- lld/test/ELF/bp-section-orderer-missing-profile.s
+++
@@ -1,9 +0,0 @@
-# REQUIRES: x86
-# RUN: llvm-mc -filetype=obj -triple=x86_64-unknown-linux %s -o %t.o
-# RUN: not ld.lld %t.o --irpgo-profile=%t.missing.profdata --bp-startup-sort=function 2>&1 | FileCheck %s
-
-# CHECK: error: {{[Nn]}}o such file or directory
-
-.globl _start
-_start:
-  ret
--- lld/test/MachO/bp-section-orderer-missing-profile.s
+++ lld/test/MachO/bp-section-orderer-missing-profile.s
@@ -1,8 +1,8 @@
 # REQUIRES: x86
 # RUN: llvm-mc -filetype=obj -triple=x86_64-apple-darwin %s -o %t.o
-# RUN: not %lld -e _main -o /dev/null %t.o --irpgo-profile=%t.missing.profdata --bp-startup-sort=function 2>&1 | FileCheck %s
+# RUN: not %lld -e _main -o /dev/null %t.o --irpgo-profile=%t.missing.profdata --bp-startup-sort=function 2>&1 | FileCheck -DMSG=%errc_ENOENT %s --check-prefix=MISSING-PROFILE
 
-# CHECK: error: {{[Nn]}}o such file or directory
+# MISSING-PROFILE: error: [[MSG]]
 
 .text
 .globl _main
