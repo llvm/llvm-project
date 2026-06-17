cf9dac4cc4d9b590c449c99427c8f57b7eb4635f
Jeff Bailey <jbailey@raspberryginger.com>
2026-06-17 10:33:19 +0100

    [libc][NFC] use correct types in prlimit wrapper
    Assisted-by: Automated tooling, human reviewed.

--- libc/src/__support/OSUtil/linux/syscall_wrappers/prlimit.h
+++ libc/src/__support/OSUtil/linux/syscall_wrappers/prlimit.h
@@ -9,6 +9,8 @@
 #ifndef LLVM_LIBC_SRC___SUPPORT_OSUTIL_LINUX_SYSCALL_WRAPPERS_PRLIMIT_H
 #define LLVM_LIBC_SRC___SUPPORT_OSUTIL_LINUX_SYSCALL_WRAPPERS_PRLIMIT_H
 
+#include "hdr/types/pid_t.h"
+#include "hdr/types/struct_rlimit.h"
 #include "src/__support/OSUtil/linux/syscall.h" // For syscall_checked
 #include "src/__support/common.h"
 #include "src/__support/error_or.h"
@@ -18,8 +20,9 @@
 namespace LIBC_NAMESPACE_DECL {
 namespace linux_syscalls {
 
-LIBC_INLINE ErrorOr<int> prlimit(int pid, int resource, const void *new_limit,
-                                 void *old_limit) {
+LIBC_INLINE ErrorOr<int> prlimit(pid_t pid, int resource,
+                                 const struct rlimit *new_limit,
+                                 struct rlimit *old_limit) {
   return syscall_checked<int>(SYS_prlimit64, pid, resource, new_limit,
                               old_limit);
 }
