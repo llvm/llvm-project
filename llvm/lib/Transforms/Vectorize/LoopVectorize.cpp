abea54dfe19deac710646ce45b2024b649f5175d
rond <ron.dahan@mobileye.com>
2026-06-17 12:13:58 +0300

    Fix clang format

--- llvm/lib/Transforms/Vectorize/LoopVectorize.cpp
+++ llvm/lib/Transforms/Vectorize/LoopVectorize.cpp
@@ -6424,8 +6424,8 @@ VPHistogramRecipe *VPRecipeBuilder::widenIfHistogram(VPInstruction *VPI) {
   if (CM.isMaskRequired(HI->Store))
     HGramOps.push_back(VPI->getMask());
 
-  return new VPHistogramRecipe(*UpdateKind, HGramOps,
-                               cast<VPIRMetadata>(*VPI), VPI->getDebugLoc());
+  return new VPHistogramRecipe(*UpdateKind, HGramOps, cast<VPIRMetadata>(*VPI),
+                               VPI->getDebugLoc());
 }
 
 bool VPRecipeBuilder::replaceWithFinalIfReductionStore(
