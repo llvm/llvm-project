782736fc5f9bd311a43c60482046815e07cc81dc
Justice Adams <jadams23@apple.com>
2026-06-17 09:10:08 -0700

    [ci] run swiftlang clang stage 1 on arm64 (#13200)

--- .ci/apple-llvm/swiftlang-clang-stage1-RA.groovy
+++ .ci/apple-llvm/swiftlang-clang-stage1-RA.groovy
@@ -10,6 +10,7 @@ library identifier: "zorg-shared-lib@${branchName}",
 clangPipeline(
     jobName: env.JOB_NAME,
     zorgBranch: branchName,
+    defaultLabel: 'macos-arm64',
     buildConfig: [
         stage: 1,
         build_type: 'cmake',
