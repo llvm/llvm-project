5c77d9ef825c8866a65c6608b38e010bdce8e812
Abhina Sree <Abhina.Sreeskantharajan@ibm.com>
2026-06-17 09:26:22 -0400

    Apply suggestion from @zibi2
    Co-authored-by: Zibi Sarbinowski <zibi@ca.ibm.com>

--- clang/test/Driver/zos-err-options.c
+++ clang/test/Driver/zos-err-options.c
@@ -1,3 +1,3 @@
 // RUN: not %clang -msoft-float --target=s390x-none-zos 2>&1 %s | FileCheck %s
 
-// CHECK: error: unsupported option '-msoft-float' for target 's390x-unknown-zos'
+// CHECK: error: unsupported option '-msoft-float' for target 's390x-none-zos'
