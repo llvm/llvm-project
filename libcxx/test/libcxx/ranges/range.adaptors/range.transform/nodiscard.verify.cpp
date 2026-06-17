1e9875bda87483c2f3c2e9daa447dc84b57d1a56
Lucas Mellone <sxswt@protonmail.com>
2026-06-17 22:46:31 +0200

    WIP: format existing tests with the addition of the TestView

--- libcxx/test/libcxx/ranges/range.adaptors/range.transform/nodiscard.verify.cpp
+++ libcxx/test/libcxx/ranges/range.adaptors/range.transform/nodiscard.verify.cpp
@@ -11,13 +11,33 @@
 // Test the libc++ extension that std::views::transform is marked as [[nodiscard]].
 
 #include <ranges>
+#include <functional>
+
+struct TestView : std::ranges::view_interface<TestView> {
+  int* begin();
+  char* begin() const;
+  const int* end();
+  const char* end() const;
+};
 
 void test() {
   int range[] = {1, 2, 3};
-  auto f = [](int i) { return i; };
+  auto f      = [](int i) { return i; };
 
-  std::views::transform(f); // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
-  std::views::transform(range, f); // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
-  range | std::views::transform(f); // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
-  std::views::all | std::views::transform(f); // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
+  auto identity_view = TestView{} | std::views::transform(std::identity{});
+    
+  // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  identity_view.begin();
+
+  // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  std::views::transform(f);
+
+  // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  std::views::transform(range, f);
+
+  // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  range | std::views::transform(f);
+
+  // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  std::views::all | std::views::transform(f);
 }
