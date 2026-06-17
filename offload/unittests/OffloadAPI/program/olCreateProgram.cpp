c432c7a922873b9023f5e03425b8aeba2cba0ac7
Jan Trusiłło <113amper@gmail.com>
2026-06-17 13:22:06 +0000

    [offload] add tests for olCreateProgram validation

--- offload/unittests/OffloadAPI/program/olCreateProgram.cpp
+++ offload/unittests/OffloadAPI/program/olCreateProgram.cpp
@@ -14,6 +14,7 @@ using olCreateProgramTest = OffloadDeviceTest;
 OFFLOAD_TESTS_INSTANTIATE_DEVICE_FIXTURE(olCreateProgramTest);
 
 TEST_P(olCreateProgramTest, Success) {
+
   std::unique_ptr<llvm::MemoryBuffer> DeviceBin;
   ASSERT_TRUE(TestEnvironment::loadDeviceBinary("foo", Device, DeviceBin));
   ASSERT_GE(DeviceBin->getBufferSize(), 0lu);
@@ -27,6 +28,7 @@ TEST_P(olCreateProgramTest, Success) {
 }
 
 TEST_P(olCreateProgramTest, NullDeviceHandle) {
+
   std::unique_ptr<llvm::MemoryBuffer> DeviceBin;
   ASSERT_TRUE(TestEnvironment::loadDeviceBinary("foo", Device, DeviceBin));
   ASSERT_GE(DeviceBin->getBufferSize(), 0lu);
@@ -38,6 +40,7 @@ TEST_P(olCreateProgramTest, NullDeviceHandle) {
 }
 
 TEST_P(olCreateProgramTest, NullProgData) {
+
   std::unique_ptr<llvm::MemoryBuffer> DeviceBin;
   ASSERT_TRUE(TestEnvironment::loadDeviceBinary("foo", Device, DeviceBin));
   ASSERT_GE(DeviceBin->getBufferSize(), 0lu);
@@ -49,6 +52,7 @@ TEST_P(olCreateProgramTest, NullProgData) {
 }
 
 TEST_P(olCreateProgramTest, NullOutputProgram) {
+
   std::unique_ptr<llvm::MemoryBuffer> DeviceBin;
   ASSERT_TRUE(TestEnvironment::loadDeviceBinary("foo", Device, DeviceBin));
   ASSERT_GE(DeviceBin->getBufferSize(), 0lu);
@@ -56,27 +60,4 @@ TEST_P(olCreateProgramTest, NullOutputProgram) {
   ASSERT_ERROR(OL_ERRC_INVALID_NULL_POINTER,
                olCreateProgram(Device, DeviceBin->getBufferStart(),
                                DeviceBin->getBufferSize(), nullptr));
-}
-
-TEST_P(olCreateProgramTest, ZeroSizeBinary) {
-  std::unique_ptr<llvm::MemoryBuffer> DeviceBin;
-  ASSERT_TRUE(TestEnvironment::loadDeviceBinary("foo", Device, DeviceBin));
-  ASSERT_GT(DeviceBin->getBufferSize(), 0lu);
-
-  ol_program_handle_t Program = nullptr;
-
-  // backend rejection of a binary is not guaranteed to map to a specific
-  // ol_errc_t, so we ASSERT_ANY_ERROR for now
-  ASSERT_ANY_ERROR(
-      olCreateProgram(Device, DeviceBin->getBufferStart(), 0, &Program));
-  ASSERT_EQ(Program, nullptr);
-}
-
-TEST_P(olCreateProgramTest, InvalidBinary) {
-  const char InvalidBinary[] = "not an offload binary";
-
-  ol_program_handle_t Program = nullptr;
-  ASSERT_ANY_ERROR(olCreateProgram(Device, InvalidBinary,
-                                   sizeof(InvalidBinary) - 1, &Program));
-  ASSERT_EQ(Program, nullptr);
 }
