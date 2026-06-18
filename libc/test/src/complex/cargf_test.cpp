656e6a3886c2fd7ed847df57f81538ebcb7ecf9d
jinge90 <ge.jin@intel.com>
2026-06-18 15:37:47 +0800

    Add basic tests for normal inputs
    Signed-off-by: jinge90 <ge.jin@intel.com>

--- libc/test/src/complex/cargf_test.cpp
+++ libc/test/src/complex/cargf_test.cpp
@@ -9,5 +9,24 @@
 #include "CArgTest.h"
 
 #include "src/complex/cargf.h"
+#include "utils/MPCWrapper/MPCUtils.h"
+
+using LlvmLibcCargfMPCTest = LIBC_NAMESPACE::testing::FPTest<float>;
+
+namespace mpc = LIBC_NAMESPACE::testing::mpc;
+
+TEST_F(LlvmLibcCargfMPCTest, BasicForRounding) {
+  _Complex float test_values[] = {
+      1.0f + 1.0fi,  -1.0f + 1.0fi,  1.0f - 1.0fi,      -1.0f - 1.0fi,
+      3.0f + 4.0fi,  -3.0f + 4.0fi,  3.0f - 4.0fi,      -3.0f - 4.0fi,
+      0.5f + 0.5fi,  -0.5f + 0.5fi,  0.5f - 0.5fi,      -0.5f - 0.5fi,
+      1.0f + 0.0fi,  -1.0f + 0.0fi,  0.0f + 1.0fi,      0.0f - 1.0fi,
+      5.0f + 12.0fi, 100.0f + 1.0fi, 0.001f + 1000.0fi,
+  };
+  for (_Complex float val : test_values) {
+    EXPECT_MPC_MATCH_ALL_ROUNDING(mpc::Operation::Carg, val,
+                                  LIBC_NAMESPACE::cargf(val), 0.5);
+  }
+}
 
 LIST_CARG_TESTS(_Complex float, float, LIBC_NAMESPACE::cargf)
