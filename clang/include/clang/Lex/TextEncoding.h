785f4a51a8e7696a273ac12a6ae1fbbf30f04c1c
alisonzhang <alisonzhang@ibm.com>
2026-06-18 12:00:14 -0400

    Changes from fexec-charset PR

---
+++ clang/include/clang/Lex/TextEncoding.h
@@ -0,0 +1,23 @@
+//===-- clang/Lex/TextEncoding.h - Text Conversion Config -*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_CLANG_LEX_TEXTENCODING_H
+#define LLVM_CLANG_LEX_TEXTENCODING_H
+
+#include "clang/Basic/LangOptions.h"
+#include "llvm/ADT/StringRef.h"
+#include "llvm/Support/TextEncoding.h"
+
+enum ConversionAction { CA_NoConversion };
+
+class TextEncoding {
+public:
+  llvm::TextEncodingConverter *getConverter(ConversionAction Action) const;
+};
+
+#endif
---
+++ clang/lib/Lex/TextEncoding.cpp
@@ -0,0 +1,18 @@
+//===--- TextEncoding.cpp -------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "clang/Lex/TextEncoding.h"
+#include "clang/Basic/DiagnosticDriver.h"
+
+llvm::TextEncodingConverter *
+TextEncoding::getConverter(ConversionAction Action) const {
+  switch (Action) {
+  default:
+    return nullptr;
+  }
+}
