9637a5399748b17f8eabdd8f2370abc5ac9f8e40
Louis Dionne <ldionne.2@gmail.com>
2026-06-19 15:41:48 -0400

    Expand beyond C++11

--- libcxx/test/std/containers/sequences/forwardlist/forwardlist.cons/size.pass.cpp
+++ libcxx/test/std/containers/sequences/forwardlist/forwardlist.cons/size.pass.cpp
@@ -21,15 +21,10 @@
 
 template <class T, class Allocator>
 void check_allocator(unsigned n, Allocator const& alloc = Allocator()) {
-#if TEST_STD_VER >= 11
   typedef std::forward_list<T, Allocator> C;
   C d(n, alloc);
   assert(d.get_allocator() == alloc);
   assert(static_cast<std::size_t>(std::distance(d.begin(), d.end())) == n);
-#else
-  ((void)n);
-  ((void)alloc);
-#endif
 }
 
 int main(int, char**) {
@@ -54,20 +49,18 @@ int main(int, char**) {
     }
     assert(n == N);
   }
-#if TEST_STD_VER >= 11
   {
     typedef DefaultOnly T;
-    typedef std::forward_list<T, min_allocator<T>> C;
+    typedef std::forward_list<T, min_allocator<T> > C;
     unsigned N = 10;
     C c(N);
     unsigned n = 0;
     for (C::const_iterator i = c.begin(), e = c.end(); i != e; ++i, ++n)
       assert(*i == T());
     assert(n == N);
-    check_allocator<T, min_allocator<T>>(0);
-    check_allocator<T, min_allocator<T>>(3);
+    check_allocator<T, min_allocator<T> >(0);
+    check_allocator<T, min_allocator<T> >(3);
   }
-#endif
 
   return 0;
 }
