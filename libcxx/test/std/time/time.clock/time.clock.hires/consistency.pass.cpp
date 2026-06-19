c9f02bb298471d22112be8ea4cdd48f4a643e312
Paul <paulxicao7@gmail.com>
2026-06-19 22:08:51 +0200

    fixup (steady_clock only visible `#if _LIBCPP_HAS_MONOTONIC_CLOCK`)

--- libcxx/test/std/time/time.clock/time.clock.hires/consistency.pass.cpp
+++ libcxx/test/std/time/time.clock/time.clock.hires/consistency.pass.cpp
@@ -37,7 +37,15 @@ int main(int, char**)
     static_assert(!std::is_member_pointer<decltype(&C::is_steady)>::value, "is_steady must be static");
     TEST_CONSTEXPR_CXX14 const bool is_steady = C::is_steady; // "is_steady must be constexpr"
     (void)is_steady;
-    LIBCPP_ASSERT((C::is_steady == std::is_same<C, std::chrono::steady_clock>::value));
+
+    // high_resolution_clock typedefs to steady_clock when _LIBCPP_HAS_MONOTONIC_CLOCK,
+    // else system_clock (e.g. generic-llvm-libc, generic-no-threads).
+    #if _LIBCPP_HAS_MONOTONIC_CLOCK
+        LIBCPP_ASSERT(C::is_steady);
+    #else
+        LIBCPP_ASSERT(!C::is_steady);
+    #endif
+
     odr_use(C::is_steady);
 
     return 0;
