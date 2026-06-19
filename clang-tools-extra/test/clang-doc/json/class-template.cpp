935c9972738740f1420d107509848c4d77254820
Paul Kirth <paulkirth@google.com>
2026-06-19 00:45:28 +0000

    [clang-doc] Try to make testing more uniform
    Today clang-doc has tests for its various backends that use the same
    input files, and mix the checks for each format. This leads to very
    large test files that are quite hard to update or maintain. Thus far
    we've assumed that this is better than updating several files, but as we
    leverage mustache and JSON more and more to test feature completeness,
    much of the output complexity is now limited to each backend and its
    mustache templates. To make this simpler to maintain, we can lean into
    common test Inputs keeping the annotate source separate from the test
    checks, and split the checks out into their own directory hierarchy.
    This patch is mostly mechanical rewriting of code. This was done with
    the assistance of an LLM, but was checked by me, and verified with
    instrumentation based coverage that we did not lose any line coverage.

--- clang-tools-extra/test/clang-doc/json/class-template.cpp
+++ clang-tools-extra/test/clang-doc/json/class-template.cpp
@@ -1,11 +1,6 @@
 // RUN: rm -rf %t && mkdir -p %t
-// RUN: clang-doc --pretty-json --output=%t --format=json --executor=standalone %s
+// RUN: clang-doc --pretty-json --output=%t --format=json --executor=standalone %S/../Inputs/class-template.cpp
 // RUN: FileCheck %s < %t/json/GlobalNamespace/_ZTV7MyClass.json
-
-template<typename T> struct MyClass {
-  T MemberTemplate;
-  T method(T Param); 
-};
 
 // CHECK:         "Name": "MyClass",
 // CHECK:         "PublicMembers": [
