c05793d405bb826b25dc54bbb5800d62aee4f34d
Jon Roelofs <jonathan_roelofs@apple.com>
2026-06-18 18:13:35 -0700

    [libunwind] Fix exposure of UNW_AARCH64_RA_SIGN_STATE through _Unwind_GetGR
    The pseudo register was previously hacked in unrder kRegisterIsUndefined,
    leaving it in a state that was only accessible from within the unwinder itself.
    To fix this, allowing external access to the state (and therefore tests!), we
    invent a new register location kRegisterIsPseudo to record the additional state
    that the register has been defined by a .cfi_negate_ra_state opcode having been
    executed.

---
+++ libunwind/test/ra_sign_state.pass.cpp
@@ -0,0 +1,125 @@
+// -*- C++ -*-
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// REQUIRES: target={{aarch64.*}}
+// UNSUPPORTED: target={{.*-windows.*}}
+
+#undef NDEBUG
+#include "support/func_bounds.h"
+#include <assert.h>
+#include <inttypes.h>
+#include <libunwind.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <unwind.h>
+
+#if defined(__APPLE__)
+#include <sys/sysctl.h>
+#endif
+
+// Note: This test requires FEAT_PAuth (and is setup to pass on other targets).
+
+#if defined(__APPLE__)
+static bool checkHasPAuth() {
+  int has_pauth = 0;
+  size_t size = sizeof(has_pauth);
+  if (sysctlbyname("hw.optional.arm.FEAT_PAuth", &has_pauth, &size, NULL, 0))
+    return false;
+  return has_pauth != 0;
+}
+#else
+static bool checkHasPAuth() {
+  // TODO: Support other platforms.
+  return false;
+}
+#endif
+
+FUNC_BOUNDS_DECL(main_func);
+
+_Unwind_Reason_Code frame_handler(struct _Unwind_Context *ctx, void *arg) {
+  uint64_t ra_sign_state =
+      (uint64_t)_Unwind_GetGR(ctx, UNW_AARCH64_RA_SIGN_STATE);
+
+  uintptr_t ip = _Unwind_GetIP(ctx);
+  if (ip >= (uintptr_t)FUNC_START(main_func) &&
+      ip < (uintptr_t)FUNC_END(main_func)) {
+
+    // Collect the RA from the callee that will return to main.
+    *(uint64_t *)arg = ra_sign_state;
+
+    // Unwind until main is reached, above frames depend on the platform and
+    // architecture.
+    return _URC_END_OF_STACK;
+  }
+
+#if defined(__PTRAUTH__) || __has_feature(ptrauth_calls)
+  assert(ra_sign_state == 1);
+#else
+  assert(ra_sign_state == 0);
+#endif
+
+  return _URC_NO_REASON;
+}
+
+__attribute__((noinline)) extern "C" uintptr_t get_main_ra_sign_state() {
+  uint64_t sign_state = -1;
+  _Unwind_Backtrace(frame_handler, &sign_state);
+  printf("UNW_AARCH64_RA_SIGN_STATE = %" PRIu64 "\n", sign_state);
+  return sign_state;
+}
+
+__attribute__((noinline)) uint64_t check_vanilla() {
+  return get_main_ra_sign_state();
+}
+
+__attribute__((naked)) uint64_t check_negate() {
+  asm(".cfi_negate_ra_state\n"
+      "pacibsp\n"
+
+      "stp x29, x30, [sp, #-16]!\n"
+      ".cfi_def_cfa_offset 16\n"
+      ".cfi_offset x29, -16\n"
+      ".cfi_offset x30, -8\n"
+
+      "bl _get_main_ra_sign_state\n"
+
+      "ldp x29, x30, [sp], #16\n"
+      ".cfi_def_cfa_offset 0\n"
+      ".cfi_restore x29\n"
+      ".cfi_restore x30\n"
+
+      ".cfi_negate_ra_state\n"
+      "retab");
+}
+
+FUNC_ATTR(main_func) int main(int, char **) {
+  uint64_t ret;
+
+  ret = check_vanilla();
+  assert((ret & 2) == ret);
+
+#if defined(__PTRAUTH__) || __has_feature(ptrauth_calls)
+  assert(ret == 1);
+#else
+  assert(ret == 0);
+#endif
+
+  if (!checkHasPAuth()) {
+    printf("target does not have FEAT_PAuth\n");
+    return 0;
+  }
+
+  ret = check_negate();
+  assert(ret == 1);
+
+  printf("success\n");
+  return 0;
+}
