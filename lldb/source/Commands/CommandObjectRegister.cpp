98047a8c6b4fed9b5d4f736965ebfe7bfac97be2
Aleksandr Levin <alexander.levin03@mail.ru>
2026-06-20 17:35:09 +0200

    Comment change

--- lldb/source/Commands/CommandObjectRegister.cpp
+++ lldb/source/Commands/CommandObjectRegister.cpp
@@ -72,8 +72,8 @@ static size_t ComputeLongestRegisterName(Args &command,
 
   // Loop through all the arguments to find the longest register name.
   for (auto &entry : command) {
-    // In most LLDB commands we accept $rbx as the name for register RBX,
-    //  therefore internally it must be restricted to plain [ rbx ] format.
+    // In most LLDB commands we accept '$<register>' as well as '<register>'
+    // for example '$rbx' for 'rbx'. However internally the name does not have '$'.
     llvm::StringRef arg_str = entry.ref();
     arg_str.consume_front("$");
 
