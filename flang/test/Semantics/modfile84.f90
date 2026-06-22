02f5dbf32d27dc10f305228aa325e9b36bb740de
Valentin Clement <clementval@gmail.com>
2026-06-22 16:13:48 -0700

    fix test

--- flang/test/Semantics/modfile84.f90
+++ flang/test/Semantics/modfile84.f90
@@ -3,7 +3,7 @@
 ! RUN: not %flang_fc1 -fsyntax-only -fopenacc -module-dir %t %t/use.f90 2>&1 | FileCheck %s
 
 !--- m.cuf
-module m
+module modfile84m
   real, device :: d
 contains
   attributes(device) subroutine s()
@@ -11,7 +11,7 @@ contains
 end module
 
 !--- use.f90
-use m
+use modfile84m
 end
 
-! CHECK: error: Cannot use module file for module 'm': CUDA is not enabled, but '{{.*m.mod}}' defines CUDA symbols
+! CHECK: error: Cannot use module file for module 'modfile84m': CUDA is not enabled, but '{{.*modfile84m.mod}}' defines CUDA symbols
