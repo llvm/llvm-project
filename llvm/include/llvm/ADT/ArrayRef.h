8ef9b3b92ce0e256e70a9c08a89632aa4211f2b9
Ramkumar Ramachandra <artagnon@tenstorrent.com>
2026-06-22 20:21:19 +0100

    [ArrayRef] Relax iterator_range constructor (NFC)
    The enable_if guard is too restrictive, and stripping it does not lead
    to worse compile-time errors on modern C++ compilers, or incorrect
    results. The compiler would automatically check that the Data member is
    initializable with the type T *const *.

--- llvm/include/llvm/ADT/ArrayRef.h
+++ llvm/include/llvm/ADT/ArrayRef.h
@@ -114,12 +114,9 @@ public:
 #pragma GCC diagnostic pop
 #endif
 
-  /// Construct an ArrayRef<T> from iterator_range<U*>. This uses SFINAE
-  /// to ensure that this is only used for iterator ranges over plain pointer
-  /// iterators.
-  template <typename U, typename = std::enable_if_t<
-                            std::is_convertible_v<U *const *, T *const *>>>
-  ArrayRef(const iterator_range<U *> &Range)
+  /// Construct an ArrayRef from an iterator_range.
+  template <typename U>
+  ArrayRef(iterator_range<U> &&Range)
       : Data(Range.begin()), Length(llvm::size(Range)) {}
 
   /// @}
