c409f388c00c4cb1796649e3e4c121df3ae033a4
Ramkumar Ramachandra <artagnon@tenstorrent.com>
2026-06-22 18:18:42 +0100

    [VPlan] Constant-fold ActiveLaneMask intrinsics
    Constant-fold fixed-vector types via llvm::simplifyIntrinsic, making it
    accessible from InstSimplifyFolder, and use it for folding
    get.active.lane.mask in VPlan.

--- llvm/lib/Transforms/Vectorize/VPlan.h
+++ llvm/lib/Transforms/Vectorize/VPlan.h
@@ -5044,6 +5044,10 @@ public:
     return getScalarHeader()->getIRBasicBlock()->getDataLayout();
   }
 
+  Function *getIRFunction() const {
+    return getScalarHeader()->getIRBasicBlock()->getParent();
+  }
+
   void addVF(ElementCount VF) { VFs.insert(VF); }
 
   void setVF(ElementCount VF) {
