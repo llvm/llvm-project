6a0a22629a20ec82740677c13390de090b4f49b2
Andy Kaylor <akaylor@nvidia.com>
2026-06-22 10:51:01 -0700

    Add handling for struct initialization

--- clang/test/CIR/CodeGen/const-label-addr.c
+++ clang/test/CIR/CodeGen/const-label-addr.c
@@ -5,11 +5,13 @@
 // RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -emit-llvm %s -o %t.ll
 // RUN: FileCheck --check-prefix=LLVM --input-file=%t.ll %s
 
+// CIR: cir.global "private" internal dso_local @d.s = #cir.const_record<{#cir.block_addr_info<@d, "A"> : !cir.ptr<!void>, #cir.block_addr_info<@d, "B"> : !cir.ptr<!void>}> : !rec_S
 // CIR: cir.global "private" internal dso_local @c.tbl = #cir.const_array<[#cir.block_addr_info<@c, "A"> : !cir.ptr<!void>, #cir.block_addr_info<@c, "A"> : !cir.ptr<!void>, #cir.block_addr_info<@c, "B"> : !cir.ptr<!void>]> : !cir.array<!cir.ptr<!void> x 3>
 // CIR: cir.global "private" internal dso_local @a.a = #cir.block_addr_info<@a, "A"> : !cir.ptr<!void>
 
 // LLVM-DAG: @a.a = internal global ptr blockaddress(@a, %[[A_BLOCK:.*]]), align 8
 // LLVM-DAG: @c.tbl = internal global [3 x ptr] [ptr blockaddress(@c, %[[C_A:.*]]), ptr blockaddress(@c, %[[C_A]]), ptr blockaddress(@c, %[[C_B:.*]])], align 16
+// LLVM-DAG: @d.s = internal global %struct.S { ptr blockaddress(@d, %[[D_A:.*]]), ptr blockaddress(@d, %[[D_B:.*]]) }, align 8
 
 void a(void) {
 A:;
@@ -51,4 +53,26 @@ B:
 // LLVM:   %[[P:.*]] = load ptr, ptr %[[TARGET]], align 8
 // LLVM:   br label %[[C_B]]
 // LLVM: [[C_B]]:
+// LLVM:   ret void
+
+struct S { void *a, *b; };
+void d(void) {
+A:;
+B:;
+  static struct S s = {&&A, &&B};
+}
+
+// CIR: cir.func{{.*}} @d
+// CIR: [[LABEL_A:.*]]:
+// CIR:   cir.label "A"
+// CIR: [[LABEL_B:.*]]:
+// CIR:   cir.label "B"
+// CIR:   %[[S:.*]] = cir.get_global @d.s
+// CIR:   cir.return
+
+// LLVM: define dso_local void @d()
+// LLVM:   br label %[[D_A]]
+// LLVM: [[D_A]]:
+// LLVM:   br label %[[D_B]]
+// LLVM: [[D_B]]:
 // LLVM:   ret void
