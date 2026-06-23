6cb39ea3ad8782c3d536fd10d9dbccfa8a27a2a7
jannewger <jannewger@google.com>
2026-06-23 18:11:57 +0000

    [CopyProf] Implement minimal CopyProf runtime.
    The runtime tracks object copies using shadow memory (with a 1:8 compression scale).
    
    Each thread uses TLS to keep track of whether execution is currently inside a special
    member function. Copy function mark corresponding shadow memory as a copy.
    Upon destruction, if an object was marked as a copy and never modified, it is
    classified as an unnecessary copy, and a report with a stack trace is generated.

---
+++ compiler-rt/lib/copyprof/tests/driver.cpp
@@ -0,0 +1,19 @@
+//===-- driver.cpp ---------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+///
+/// This file implements the standalone test driver main function for running
+/// CopyProf runtime unit tests via Google Test.
+///
+//===----------------------------------------------------------------------===//
+
+#include "gtest/gtest.h"
+
+int main(int argc, char** argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
