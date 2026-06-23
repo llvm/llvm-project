5d963629251c787691c3799d62553c33f8497828
Lucas Mellone <sxswt@protonmail.com>
2026-06-23 23:11:13 +0200

    add: test file

---
+++ libcxx/test/std/ranges/range.adaptors/range.adjacent/nodiscard.verify.cpp
@@ -0,0 +1,11 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// REQUIRES: std-at-least-c++20
+
+// Test the libc++ extension that std::ranges::adjacent_view and std::views::adjacent are marked as [[nodiscard]].
