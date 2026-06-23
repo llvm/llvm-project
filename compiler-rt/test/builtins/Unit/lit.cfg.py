64684d56862184c61530c11b5246aa5ffdca395e
Kelvin Li <kli@ca.ibm.com>
2026-06-23 12:25:41 -0400

    [compiler-rt][test][builtins] Check if the linker supports --start-group (NFC)
    This patch is to add a check for the linker whether the --start-group
    option is supported before passing to the linker. This can accommodate
    different linker requirements.

--- compiler-rt/test/builtins/Unit/lit.cfg.py
+++ compiler-rt/test/builtins/Unit/lit.cfg.py
@@ -107,9 +107,16 @@ else:
     if config.target_os == "Haiku":
         config.substitutions.append(("%librt ", base_lib + " -lroot "))
     else:
-        config.substitutions.append(
-            ("%librt ", "-lm -Wl,--start-group " + base_lib + " -lc -Wl,--end-group ")
-        )
+        # Check if the linker supports --start-group and --end-group
+        linker_supports_start_group = get_required_attr(config, "linker_supports_start_group")
+        if linker_supports_start_group:
+            config.substitutions.append(
+                ("%librt ", "-lm -Wl,--start-group " + base_lib + " -lc -Wl,--end-group ")
+            )
+        else:
+            config.substitutions.append(
+                ("%librt ", "-lm " + base_lib + " -lc ")
+            )
 
 builtins_test_crt = get_required_attr(config, "builtins_test_crt")
 if builtins_test_crt:
