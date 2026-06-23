eeef872a2ea58d2b69956aa40ee239d92c1fbd6b
Wenju He <wenju.he@intel.com>
2026-06-24 01:51:42 +0200

    remove target options from test

--- clang/test/Driver/sycl-print-internal-defines.cpp
+++ clang/test/Driver/sycl-print-internal-defines.cpp
@@ -1,12 +1,8 @@
-// UNSUPPORTED: system-windows
 // Test that clang can print defines in SYCL mode.
-// REQUIRES: x86-registered-target
 
-// RUN: %clangxx -target x86_64-unknown-linux-gnu -fsycl -dM -E %s 2>&1 \
-// RUN: | FileCheck --check-prefix CHECK-PRINT-INTERNAL-DEFINES %s
+// RUN: %clangxx -fsycl -dM -E %s 2>&1 | FileCheck --check-prefix CHECK-PRINT-INTERNAL-DEFINES %s
 // CHECK-PRINT-INTERNAL-DEFINES: #define
 
 // Printing defines also works when input is stdin.
-// RUN: %clangxx -target x86_64-unknown-linux-gnu -fsycl -dM -E - < /dev/null 2>&1 \
-// RUN: | FileCheck --check-prefixes=CHECK-PRINT-INTERNAL-DEFINES,CHECK-NO-ERROR %s
+// RUN: %clangxx -fsycl -dM -E - < /dev/null 2>&1 | FileCheck --check-prefixes=CHECK-PRINT-INTERNAL-DEFINES,CHECK-NO-ERROR %s
 // CHECK-NO-ERROR-NOT: error:
