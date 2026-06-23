635cbc0ebb2851d4cb9d72dcd62f2ef2dd1c25c5
Pavel Labath <pavel@labath.sk>
2026-06-23 09:32:45 +0200

    Revert "[libc] Introduce the ioctl syscall wrapper and port all callers" (#205277)
    Reverts llvm/llvm-project#204640
    
    Breaks libc-x86_64-debian-fullbuild. Reverting while I investigate.

--- libc/src/unistd/linux/isatty.cpp
+++ libc/src/unistd/linux/isatty.cpp
@@ -8,11 +8,13 @@
 
 #include "src/unistd/isatty.h"
 
-#include "src/__support/OSUtil/linux/syscall_wrappers/ioctl.h"
+#include "src/__support/OSUtil/syscall.h" // For internal syscall function.
 #include "src/__support/common.h"
+
 #include "src/__support/libc_errno.h"
 #include "src/__support/macros/config.h"
-#include <sys/ioctl.h> // For ioctl numbers.
+#include <sys/ioctl.h>   // For ioctl numbers.
+#include <sys/syscall.h> // For syscall numbers.
 
 namespace LIBC_NAMESPACE_DECL {
 
@@ -21,11 +23,12 @@ LLVM_LIBC_FUNCTION(int, isatty, (int fd)) {
   int line_d_val = INIT_VAL;
   // This gets the line dicipline of the terminal. When called on something that
   // isn't a terminal it doesn't change line_d_val and returns -1.
-  auto result = linux_syscalls::ioctl(fd, TIOCGETD, &line_d_val);
-  if (result.has_value())
+  int result =
+      LIBC_NAMESPACE::syscall_impl<int>(SYS_ioctl, fd, TIOCGETD, &line_d_val);
+  if (result == 0)
     return 1;
 
-  libc_errno = result.error();
+  libc_errno = -result;
   return 0;
 }
 
