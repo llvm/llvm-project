51e07db577be768537bf60085f624a0e5644221f
Krzysztof Parzyszek <Krzysztof.Parzyszek@amd.com>
2026-06-23 15:14:10 -0500

    Modify test

--- flang/test/Semantics/OpenMP/if-clause-60.f90
+++ flang/test/Semantics/OpenMP/if-clause-60.f90
@@ -1,8 +1,8 @@
 !RUN: %python %S/../test_errors.py %s %flang -fopenmp -fopenmp-version=60
 
 subroutine f
-! Allowed in 6.0
-  !$omp taskgraph if(.true.)
+!ERROR: At most one IF clause can apply to each directive constituent
+  !$omp taskgraph if(.true.) if(.false.)
   !$omp end taskgraph
 
 !ERROR: PARALLEL LOOP is not a constituent of the TEAMS LOOP directive
