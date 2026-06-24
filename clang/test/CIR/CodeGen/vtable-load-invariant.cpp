8687ad40fc1b0c0e86720669a75ab7ab6df0ea08
skc7 <Krishna.Sankisa@amd.com>
2026-06-24 12:22:53 +0530

    add LLVM checks to tests

--- clang/test/CIR/CodeGen/vtable-load-invariant.cpp
+++ clang/test/CIR/CodeGen/vtable-load-invariant.cpp
@@ -2,6 +2,10 @@
 // RUN: FileCheck --check-prefix=CIR-STRICT --input-file=%t.cir %s
 // RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fclangir -emit-cir %s -o %t-plain.cir
 // RUN: FileCheck --check-prefix=CIR --input-file=%t-plain.cir %s
+// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -O1 -fstrict-vtable-pointers -disable-llvm-passes -fclangir -emit-llvm %s -o %t.ll
+// RUN: FileCheck --check-prefix=LLVM --input-file=%t.ll %s
+// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -O1 -fstrict-vtable-pointers -disable-llvm-passes -emit-llvm %s -o %t.ll
+// RUN: FileCheck --check-prefix=LLVM --input-file=%t.ll %s
 
 struct A {
   virtual void f(char);
@@ -21,3 +25,7 @@ void f1(A *a) {
 // Without the flags the same load must not be invariant.
 // CIR-LABEL: cir.func{{.*}}@_Z2f1P1A
 // CIR-NOT: cir.load invariant
+
+// LLVM-LABEL: define {{.*}}@_Z2f1P1A
+// LLVM: load ptr, ptr %{{.*}}!invariant.load ![[INV:[0-9]+]]
+// LLVM: ![[INV]] = !{}
