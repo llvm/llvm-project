dc6a7e204702c4f41d0e7457bc67df49946d903a
hulxv <hulxxv@gmail.com>
2026-06-25 19:08:28 +0300

    [libc] add shared subsf3 builtin

--- libc/shared/builtins.h
+++ libc/shared/builtins.h
@@ -25,6 +25,7 @@
 #include "builtins/muldf3.h"
 #include "builtins/multf3.h"
 #include "builtins/subdf3.h"
+#include "builtins/subsf3.h"
 #include "builtins/subtf3.h"
 
 #endif // LLVM_LIBC_SHARED_BUILTINS_H
