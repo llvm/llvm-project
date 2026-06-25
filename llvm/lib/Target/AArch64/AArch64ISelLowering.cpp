8aeeb9f67a4f314fb4f4e83788a62d69d4f3e742
Simon Tatham <simon.tatham@arm.com>
2026-06-25 11:48:02 +0100

    [AArch64] Don't call fma/fmaf for @llvm.fmuladd.f64/f32 in softfp (#205356)
    @llvm.fmuladd is the IR intrinsic that leaves it up to code generation
    whether to fuse an FP multiply+add pair or leave them separate.
    Generally you only fuse them if fused mul+add has good performance.
    
    On AArch64, for the float and double instances of this intrinsic, isel
    was unconditionally fusing the operations. This is sensible with
    hardware FP, but a bad idea for the rare case of AArch64 without
    hardware FP, because that leads to a call to the libm `fma()` or
    `fmaf()` function. That function generally (in multiple libcs) seems to
    be much slower than separate mul+add operations. So this patch checks
    for the presence of FP before reporting that fusing the operations is a
    performance win.

--- llvm/lib/Target/AArch64/AArch64ISelLowering.cpp
+++ llvm/lib/Target/AArch64/AArch64ISelLowering.cpp
@@ -19758,7 +19758,7 @@ bool AArch64TargetLowering::isFMAFasterThanFMulAndFAdd(
     return Subtarget->hasFullFP16();
   case MVT::f32:
   case MVT::f64:
-    return true;
+    return Subtarget->hasFPARMv8();
   case MVT::bf16:
     return VT.isScalableVector() && Subtarget->hasBF16() &&
            Subtarget->isNonStreamingSVEorSME2Available();
@@ -19774,7 +19774,7 @@ bool AArch64TargetLowering::isFMAFasterThanFMulAndFAdd(const Function &F,
   switch (Ty->getScalarType()->getTypeID()) {
   case Type::FloatTyID:
   case Type::DoubleTyID:
-    return true;
+    return Subtarget->hasFPARMv8();
   default:
     return false;
   }
