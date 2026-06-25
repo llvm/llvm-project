694f9e08f507a61d7f22d881bca8e5894227d9bc
LouisLu0602 <yaolu0602@gmail.com>
2026-06-25 09:38:02 +0800

    Fixing comments

--- mlir/lib/Dialect/Linalg/TransformOps/LinalgTransformOps.cpp
+++ mlir/lib/Dialect/Linalg/TransformOps/LinalgTransformOps.cpp
@@ -4174,6 +4174,7 @@ DiagnosedSilenceableFailure transform::VectorizeOp::apply(
       return mlir::emitSilenceableFailure(target->getLoc())
              << "Unsupported Op, cannot vectorize";
     }
+    FailureOr<VectorizationResult> vectorResults =
         linalg::vectorize(rewriter, target, vectorSizes, getScalableSizes(),
                           getVectorizeNdExtract().value_or(false),
                           /*flatten1DDepthwiseConv=*/false,
