4f28e5b951edd77767cbb733340f1f6c34b404fb
Kelvin Li <kli@ca.ibm.com>
2026-06-25 16:26:53 -0400

    Add logic to handle output from different default target setting

--- clang/test/Driver/aix-object-mode.c
+++ clang/test/Driver/aix-object-mode.c
@@ -16,13 +16,15 @@
 // RUN: %clang --target=powerpc-ibm-aix -print-target-triple | FileCheck -check-prefix=CHECK32 %s
 
 // RUN: env OBJECT_MODE=32_64 \
-// RUN: %clang -print-target-triple | FileCheck -check-prefix=CHECK64 %s
+// RUN: %clang -print-target-triple | FileCheck \
+// RUN:     -check-prefix=%if target={{powerpc64-.*}} %{CHECK64%} %else %{CHECK32%} %s
 
 // RUN: env OBJECT_MODE=any \
 // RUN: %clang --target=powerpc-ibm-aix -print-target-triple | FileCheck -check-prefix=CHECK32 %s
 
 // RUN: env OBJECT_MODE=any \
-// RUN: %clang -print-target-triple | FileCheck -check-prefix=CHECK64 %s
+// RUN: %clang -print-target-triple | FileCheck \
+// RUN:     -check-prefix=%if target={{powerpc64-.*}} %{CHECK64%} %else %{CHECK32%} %s
 
 // Command-line options win.
 // RUN: env OBJECT_MODE=64 \
