aab71c93649a2ebdd3f7bcb44802337efe480c8b
Sergio Afonso <Sergio.AfonsoFumero@amd.com>
2026-06-25 13:29:54 +0100

    [Offload][OpenMP][Flang] Update no-loop test
    Updates to the kernel type detection logic now allow `target parallel do` to
    be promoted to SPMD-No-Loop. An offload test that was affected by this
    change is updated here.

--- offload/test/offloading/fortran/target-no-loop.f90
+++ offload/test/offloading/fortran/target-no-loop.f90
@@ -48,7 +48,7 @@ program main
 
    errors = errors + check_errors(array)
 
-   ! SPMD kernel
+   ! No-loop kernel
    array = 1
    !$omp target parallel do
    do i = 1, 1024
@@ -88,7 +88,7 @@ end program main
 ! CHECK:  info: #Args: 3 Teams x Thrds:   3x  16 {{.*}}
 ! CHECK:  PluginInterface device {{[0-9]+}} info: Launching kernel {{.*}} SPMD-No-Loop mode
 ! CHECK:  info: #Args: 3 Teams x Thrds:   64x  16 {{.*}}
-! CHECK:  PluginInterface device {{[0-9]+}} info: Launching kernel {{.*}} SPMD mode
+! CHECK:  PluginInterface device {{[0-9]+}} info: Launching kernel {{.*}} SPMD-No-Loop mode
 ! CHECK:  info: #Args: 3 Teams x Thrds:   1x  16
 ! CHECK:  PluginInterface device {{[0-9]+}} info: Launching kernel {{.*}} Generic mode
 ! CHECK:  info: #Args: 3 Teams x Thrds:   16x  16 {{.*}}
