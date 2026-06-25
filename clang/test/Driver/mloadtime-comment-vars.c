ba7068838702adecb49ca200e68498ad2b7b802a
Tony Varghese <tony.varghese@ibm.com>
2026-06-25 12:56:50 +0530

    [Clang][AIX] Switch -mloadtime-comment-vars name matching to mangled IR names
    Replace source-qualified name matching in matchesLoadTimeCommentVarName with
    mangled IR symbol name matching via getMangledName(GlobalDecl(VD)).

--- clang/test/Driver/mloadtime-comment-vars.c
+++ clang/test/Driver/mloadtime-comment-vars.c
@@ -1,13 +1,11 @@
 // RUN: %clang -### -target powerpc-ibm-aix -mloadtime-comment-vars=sccsid,version %s 2>&1 | FileCheck %s
-// RUN: %clang -### -target powerpc64-ibm-aix -mloadtime-comment-vars=::x,N::x,A::x %s 2>&1 | FileCheck %s --check-prefix=SCOPE
 // RUN: %clang -### -target x86_64-linux-gnu -mloadtime-comment-vars=sccsid,version %s 2>&1 | FileCheck %s --check-prefix=NONAIX
 
+// Verify the option is forwarded verbatim to cc1 on AIX.
 // CHECK: "-cc1"
 // CHECK-SAME: "-mloadtime-comment-vars=sccsid,version"
 
-// SCOPE: "-cc1"
-// SCOPE-SAME: "-mloadtime-comment-vars=::x,N::x,A::x"
-
+// Verify a warning is emitted and the option is NOT forwarded on non-AIX targets.
 // NONAIX: warning: ignoring '-mloadtime-comment-vars=sccsid,version' option as it is not currently supported for target 'x86_64-unknown-linux-gnu'
 // NONAIX: "-cc1"
 // NONAIX-NOT: "-mloadtime-comment-vars=sccsid,version"
