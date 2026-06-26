544c3b6a840f64f03d2f74f8b490c1f65e08a97e
Pedro Vicente <pedro.costa.vicente@tecnico.ulisboa.pt>
2026-06-26 17:03:51 +0100

    apply sugestions about code style, etc

--- llvm/lib/CodeGen/TargetLoweringBase.cpp
+++ llvm/lib/CodeGen/TargetLoweringBase.cpp
@@ -1950,6 +1950,7 @@ int TargetLoweringBase::InstructionOpcodeToISD(unsigned Opcode) const {
 #define LAST_OTHER_INST(NUM) InstructionOpcodesCount = NUM
 #include "llvm/IR/Instruction.def"
   };
+  // clang-format off
   switch (static_cast<InstructionOpcodes>(Opcode)) {
   case Ret:            return 0;
   case UncondBr:       return 0;
@@ -2020,12 +2021,10 @@ int TargetLoweringBase::InstructionOpcodeToISD(unsigned Opcode) const {
   case InsertValue:    return ISD::MERGE_VALUES;
   case LandingPad:     return 0;
   case Freeze:         return ISD::FREEZE;
-  case BitInsert:
-    return 0;
-  case BitExtract:
-    return 0;
+  case BitInsert:      return 0;
+  case BitExtract:     return 0;
   }
-
+  // clang-format on
   llvm_unreachable("Unknown instruction type encountered!");
 }
 
