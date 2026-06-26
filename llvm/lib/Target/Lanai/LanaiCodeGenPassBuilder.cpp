4b0a61f6ac0a6b2adc4ed471e5929b9c4f0f07d6
Aiden Grossman <aidengrossman@google.com>
2026-06-26 22:55:57 +0000

    [𝘀𝗽𝗿] changes introduced through rebase
    Created using spr 1.3.7
    
    [skip ci]

--- llvm/lib/Target/Lanai/LanaiCodeGenPassBuilder.cpp
+++ llvm/lib/Target/Lanai/LanaiCodeGenPassBuilder.cpp
@@ -36,6 +36,8 @@ public:
 
 void LanaiCodeGenPassBuilder::addIRPasses(PassManagerWrapper &PMW) const {
   addFunctionPass(AtomicExpandPass(TM), PMW);
+
+  Base::addIRPasses(PMW);
 }
 
 Error LanaiCodeGenPassBuilder::addInstSelector(PassManagerWrapper &PMW) const {
