8bc4dcbfbb148733cede4e395240c7cf9e2cd261
Yusuke MINATO <minato.yusuke@fujitsu.com>
2026-06-26 13:08:04 +0900

    [NFC][clang] Add tests for --driver-mode=flang
    This patch intends to clarify the current behaviors,
    not to state the expected/desirable behaviors.

--- clang/test/Driver/flang/multiple-inputs-mixed.f90
+++ clang/test/Driver/flang/multiple-inputs-mixed.f90
@@ -5,3 +5,10 @@
 ! CHECK-SYNTAX-ONLY: "{{[^"]*}}/Inputs/one.f90"
 ! CHECK-SYNTAX-ONLY-LABEL: "{{[^"]*}}clang{{[^"/]*}}" "-cc1"
 ! CHECK-SYNTAX-ONLY: "{{[^"]*}}/Inputs/other.c"
+
+! RUN: not %clang --driver-mode=flang -### -Xflang -std=f2018 %S/Inputs/one.f90 -Xclang -std=c17 %S/Inputs/other.c 2>&1 | FileCheck --check-prefixes=MIXED-OPT %s
+! MIXED-OPT: clang: error: unknown argument '-Xclang'
+! MIXED-OPT-NOT: "{{[^"]*}}flang{{[^"/]*}}" "-fc1"
+! MIXED-OPT-NOT: "-std=f2018"
+! MIXED-OPT-NOT: "{{[^"]*}}clang{{[^"/]*}}" "-cc1"
+! MIXED-OPT-NOT: "-std=c17"
