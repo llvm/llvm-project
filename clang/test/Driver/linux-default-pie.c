32ccb0b298889fe8ca5a9e3df761aea4a5a4a4cf
ShengYi Hung <aokblast@FreeBSD.org>
2026-06-27 02:35:59 +0800

    [clang] Rename CLANG_DEFAULT_PIE_ON_LINUX to CLANG_DEFAULT_PIE
    Rename the platform-specific option CLANG_DEFAULT_PIE_ON_LINUX to the
    more generic CLANG_DEFAULT_PIE to allow enabling PIE by default on
    other platforms.
    
    This change only renames the option. The default values at each use site
    remain unchanged, so there is no functional behavior change.

--- clang/test/Driver/linux-default-pie.c
+++ clang/test/Driver/linux-default-pie.c
@@ -1,5 +1,5 @@
-// REQUIRES: default-pie-on-linux
-/// Test -DCLANG_DEFAULT_PIE_ON_LINUX=on.
+// REQUIRES: default-pie
+/// Test -DCLANG_DEFAULT_PIE=on.
 
 // RUN: %clang -### --target=aarch64-linux-gnu %s 2>&1 | FileCheck %s --check-prefix=PIE2
 
