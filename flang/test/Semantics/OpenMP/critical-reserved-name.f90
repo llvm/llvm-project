1c0d6df8ad9ab42eaa657c7006e0f96715eb340d
Krzysztof Parzyszek <Krzysztof.Parzyszek@amd.com>
2026-06-26 07:56:56 -0500

    Make OmpLocator parser only handle known locator names

--- flang/test/Semantics/OpenMP/critical-reserved-name.f90
+++
@@ -1,8 +0,0 @@
-! RUN: %python %S/../test_errors.py %s %flang_fc1 -fopenmp
-
-subroutine f
-  !ERROR: Invalid use of a reserved name 'OMP_C'
-  !$omp critical(omp_c)
-  !ERROR: Invalid use of a reserved name 'OMP_C'
-  !$omp end critical(omp_c)
-end
--- flang/test/Semantics/OpenMP/reserved-locator.f90
+++
@@ -1,6 +0,0 @@
-!RUN: %python %S/../test_errors.py %s %flang_fc1 -fopenmp -fopenmp-version=60
-
-subroutine f
-!ERROR: Invalid use of a reserved name 'OMP_SOME_MEMORY'
-  !$omp target update from(omp_some_memory)
-end
