6807528d969a57e720f58c7c57f53a5600ba36c1
Pedro Vicente <pedro.costa.vicente@tecnico.ulisboa.pt>
2026-06-26 15:48:24 +0100

    fix comments in instructions.h

--- llvm/include/llvm/IR/Instructions.h
+++ llvm/include/llvm/IR/Instructions.h
@@ -5393,7 +5393,7 @@ class BitInsertInst : public Instruction {
 protected:
   // Note: Instruction needs to be a friend here to call cloneImpl.
   friend class Instruction;
-  /// Clone an identical FreezeInst
+  /// Clone an identical BitInsertInst
   LLVM_ABI BitInsertInst *cloneImpl() const;
 
 public:
@@ -5434,7 +5434,7 @@ class BitExtractInst : public Instruction {
 protected:
   // Note: Instruction needs to be a friend here to call cloneImpl.
   friend class Instruction;
-  /// Clone an identical FreezeInst
+  /// Clone an identical BitExtractInst
   LLVM_ABI BitExtractInst *cloneImpl() const;
 
 public:
