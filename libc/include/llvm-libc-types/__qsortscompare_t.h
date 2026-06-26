1559d4c18d8172179f4ca943364ef7dedd65f518
Victor Campos <victor.campos@arm.com>
2026-06-26 18:21:23 +0100

    Fix file header to adhere to the standard

--- libc/include/llvm-libc-types/__qsortscompare_t.h
+++ libc/include/llvm-libc-types/__qsortscompare_t.h
@@ -1,9 +1,14 @@
-//===-- Definition of type __qsortscompare_t ------------------------------===//
+//===----------------------------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// Type definition for __qsortscompare_t.
+///
 //===----------------------------------------------------------------------===//
 
 #ifndef LLVM_LIBC_TYPES___QSORTSCOMPARE_T_H
