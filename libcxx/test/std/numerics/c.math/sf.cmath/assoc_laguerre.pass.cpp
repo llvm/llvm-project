a35b978e0601bea7a9accf072c175a4c8b182c97
Paul <paulxicao7@gmail.com>
2026-06-29 00:23:48 +0200

    test: assoc_laguerrel

--- libcxx/test/std/numerics/c.math/sf.cmath/assoc_laguerre.pass.cpp
+++ libcxx/test/std/numerics/c.math/sf.cmath/assoc_laguerre.pass.cpp
@@ -9,54 +9,44 @@
 // REQUIRES: std-at-least-c++17
 
 // <cmath>
-//
-// [sf.cmath.assoc.laguerre], associated Laguerre polynomials
-// float               assoc_laguerref(unsigned n, unsigned m, float x);
+
+// float       assoc_laguerref(unsigned n, unsigned m, float x);
+// long double assoc_laguerrel(unsigned n, unsigned m, long double x);
 
 #include <cassert>
 #include <cmath>
 #include <limits>
 
 #include "common.h"
-#include "type_algorithms.h"
 
-struct TestFloatingPoint {
-  template <class T>
-  void operator()() const {
-    // sample value testing
-    assert(between(0.99f, std::assoc_laguerref(0, 0, T(0.)), 1.01f));
-    assert(between(0.99f, std::assoc_laguerref(1, 1, T(1.)), 1.01f));
-    assert(between(-1.01f, std::assoc_laguerref(2, 0, T(2.)), -0.99f));
-    assert(between(-0.01f, std::assoc_laguerref(2, 2, T(2.)), 0.01f));
-    assert(between(60.124f, std::assoc_laguerref(2, 10, T(0.5)), 60.126f));
+// Each suffixed overload is fixed to a single floating-point type, so it is
+// tested directly with that type. The unsuffixed std::assoc_laguerre overloads
+// (and the integer-argument overload) are not implemented yet.
+template <class T, class Func>
+void test(Func assoc_laguerre) {
+  // sample values
+  assert(between(0.99, assoc_laguerre(0, 0, T(0)), 1.01));
+  assert(between(0.99, assoc_laguerre(1, 1, T(1)), 1.01));
+  assert(between(-1.01, assoc_laguerre(2, 0, T(2)), -0.99));
+  assert(between(-0.01, assoc_laguerre(2, 2, T(2)), 0.01));
+  assert(between(60.124, assoc_laguerre(2, 10, T(0.5)), 60.126));
 
-    // return type: float
-    static_assert(std::is_same_v<decltype(std::assoc_laguerref(0, 0, T(0.))), float>);
+  static_assert(std::is_same_v<decltype(assoc_laguerre(0, 0, T(0))), T>);
 
-    // NaN input -> NaN output (w/o domain error)
-    auto check_nan = [](T nan) {
-      check_no_domain_error([nan] { assert(std::isnan(std::assoc_laguerref(0, 0, nan))); });
-    };
-    if (std::numeric_limits<T>::has_quiet_NaN)
-      check_nan(std::numeric_limits<T>::quiet_NaN());
-    if (std::numeric_limits<T>::has_signaling_NaN)
-      check_nan(std::numeric_limits<T>::signaling_NaN());
+  // NaN argument -> NaN result, without a domain error ([sf.cmath.general]/1).
+  auto test_nan = [&](T nan) { check_no_domain_error([&] { assert(std::isnan(assoc_laguerre(0, 0, nan))); }); };
+  if (std::numeric_limits<T>::has_quiet_NaN)
+    test_nan(std::numeric_limits<T>::quiet_NaN());
+  if (std::numeric_limits<T>::has_signaling_NaN)
+    test_nan(std::numeric_limits<T>::signaling_NaN());
 
-    // negative x: no domain error
-    check_no_domain_error([] { assert(between(1.99f, std::assoc_laguerref(1, 0, T(-1)), 2.01f)); });
-  }
-};
+  // A negative argument is in the domain (no domain error).
+  check_no_domain_error([&] { assert(between(1.99, assoc_laguerre(1, 0, T(-1)), 2.01)); });
+}
 
-struct TestIntegral {
-  template <class T>
-  void operator()() const {
-    assert(between(0.99f, std::assoc_laguerref(0, 0, T(0.)), 1.01f));
+int main(int, char**) {
+  test<float>([](unsigned __n, unsigned __m, float __x) { return std::assoc_laguerref(__n, __m, __x); });
+  test<long double>([](unsigned __n, unsigned __m, long double __x) { return std::assoc_laguerrel(__n, __m, __x); });
 
-    static_assert(std::is_same_v<decltype(std::assoc_laguerref(0, 0, T(0.))), float>);
-  }
-};
-
-int main() {
-  types::for_each(types::floating_point_types{}, TestFloatingPoint{});
-  types::for_each(types::integral_types{}, TestIntegral{});
+  return 0;
 }
