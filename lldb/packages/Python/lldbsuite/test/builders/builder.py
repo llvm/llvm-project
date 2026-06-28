b5f8702d19e013fe615fd99ddebf578ab44725a9
Iasonaskrpr <iaskarapro@gmail.com>
2026-06-28 23:13:56 +0300

    [lldb] Added some spacing to builder.py

--- lldb/packages/Python/lldbsuite/test/builders/builder.py
+++ lldb/packages/Python/lldbsuite/test/builders/builder.py
@@ -202,15 +202,17 @@ class Builder:
 
         if lldbplatformutil.platformIsDarwin():
             utils.extend(["AR=%slibtool" % os.getenv("CROSS_COMPILE", "")])
+
         build_cmd = [
             "CC=%s" % cc,
             "CC_TYPE=%s" % cc_type,
             "CXX=%s" % cxx,
         ]
-        
+
         fc = configuration.fortran_compiler
         if fc:
             build_cmd.append("FC=%s" % fc)  
+            
         return build_cmd + utils
 
     def getSDKRootSpec(self):
