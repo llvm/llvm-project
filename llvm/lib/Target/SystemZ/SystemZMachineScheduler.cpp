33207c26459e1c762bae3778fabf9a711ab49126
Jonas Paulsson <paulson1@linux.ibm.com>
2026-06-28 13:53:55 +0200

    Use min-SU-lat=5. Tests updated and passing.

--- llvm/lib/Target/SystemZ/SystemZMachineScheduler.cpp
+++ llvm/lib/Target/SystemZ/SystemZMachineScheduler.cpp
@@ -54,10 +54,6 @@ bool SystemZPreRASchedStrategy::closesLiveRange(const SUnit *SU,
   return VR16PChange < 0 || (!VR16PChange && GRX32PChange < 0);
 }
 
-static cl::opt<unsigned> MinSULat("min-SU-lat", cl::init(0));
-static cl::opt<unsigned> MinRegionSize("min-region-size", cl::init(0));
-static cl::opt<unsigned> MinRegionSumLat("min-region-sumlat", cl::init(~0));
-
 bool SystemZPreRASchedStrategy::tryCandidate(SchedCandidate &Cand,
                                              SchedCandidate &TryCand,
                                              SchedBoundary *Zone) const {
@@ -84,22 +80,19 @@ bool SystemZPreRASchedStrategy::tryCandidate(SchedCandidate &Cand,
       return TryCand.Reason != NoCand;
   }
 
-  if (!RegionPolicy.DisableLatencyHeuristic)
+  if (!RegionPolicy.DisableLatencyHeuristic &&
+      std::max(TryCand.SU->Latency, Cand.SU->Latency) >= 5)
     if (const SUnit *HigherSU =
             TryCand.SU->getHeight() > Cand.SU->getHeight()   ? TryCand.SU
             : TryCand.SU->getHeight() < Cand.SU->getHeight() ? Cand.SU
                                                              : nullptr)
       if (HigherSU->getHeight() > Zone->getScheduledLatency() &&
           HigherSU->getDepth() < computeRemLatency(*Zone)) {
-
-        if ((std::max(TryCand.SU->Latency, Cand.SU->Latency) >= MinSULat) &&
-            (DAG->SUnits.size() >= MinRegionSize || SumLat >= MinRegionSumLat)) {
-          // The higher SU increases the scheduled latency but is not on the
-          // Critical Path by Depth, so put it above the other one.
-          tryLess(TryCand.SU->getHeight(), Cand.SU->getHeight(), TryCand, Cand,
-                  GenericSchedulerBase::BotHeightReduce);
-          return TryCand.Reason != NoCand;
-        }
+        // The higher SU increases the scheduled latency but is not on the
+        // Critical Path by Depth, so put it above the other one.
+        tryLess(TryCand.SU->getHeight(), Cand.SU->getHeight(), TryCand, Cand,
+                GenericSchedulerBase::BotHeightReduce);
+        return TryCand.Reason != NoCand;
       }
 
   // Weak edges help copy coalescing.
@@ -148,11 +141,8 @@ void SystemZPreRASchedStrategy::initialize(ScheduleDAGMI *dag) {
   Cmp0SrcReg = SystemZ::NoRegister;
 
   unsigned DAGHeight = 0;
-  SumLat = 0;
-  for (unsigned Idx = 0, End = DAG->SUnits.size(); Idx != End; ++Idx) {
+  for (unsigned Idx = 0, End = DAG->SUnits.size(); Idx != End; ++Idx)
     DAGHeight = std::max(DAGHeight, DAG->SUnits[Idx].getHeight());
-    SumLat += DAG->SUnits[Idx].Latency;
-  }
 
   if (RegionPolicy.ShouldTrackPressure)
     LivenessHeightCutOff = DAGHeight / (DAG->SUnits.size() < 50 ? 4 : 2);
