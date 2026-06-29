666f02dde023df406a8fd52002a0e8cf3815244d
rdevshp <rdevshp@gmail.com>
2026-06-29 15:02:07 +0000

    add GH issue reference to z3-unarysymexpr.c unary_operand_in_binary_op test case

--- clang/test/Analysis/z3/z3-unarysymexpr.c
+++ clang/test/Analysis/z3/z3-unarysymexpr.c
@@ -63,6 +63,7 @@ long z3_crash3(long a) {
 
 // Previously Z3 analysis crashed in this case, validate
 // that this no longer happens.
+// no-crash: GH #205037
 int unary_operand_in_binary_op(int size, int mask) {
   return size & ~mask;
 }
