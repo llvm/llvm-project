045cdfb239db018dee8673023fe389b4e7e1af61
Haibo Jiang <jianghaibo9@huawei.com>
2026-06-30 20:05:43 +0800

    [BOLT][AArch64] Split ICP tests

--- bolt/test/AArch64/icp-inline.s
+++ bolt/test/AArch64/icp-inline.s
@@ -1,4 +1,4 @@
-## This test verifies the effect of icp on aarch64 and inline after icp.
+## This test verifies inlining after indirect call promotion on AArch64.
 
 ## The assembly was produced from C code compiled with clang -O1 -S:
 
@@ -23,33 +23,15 @@
 # RUN: llvm-strip --strip-unneeded %t.o
 # RUN: %clang %cflags %t.o -o %t.exe -Wl,-q -nostdlib -pie
 
-# AArch64 jump table ICP is disabled until BOLT can reliably recover the
-# jump table index register and limits.
-# RUN: not llvm-bolt %t.exe --icp=jump-tables -o %t.null --lite=0 --assume-abi \
-# RUN:   --data %t.fdata \
-# RUN:   2>&1 | FileCheck %s --check-prefix=CHECK-ICP-JT-DISABLED
-# CHECK-ICP-JT-DISABLED: BOLT-ERROR: ICP jump table promotion is disabled on AArch64
-
-# Indirect call promotion without inline
-# RUN: llvm-bolt %t.exe --icp=calls --icp-calls-topn=1 \
-# RUN:   -o %t.null --lite=0 --assume-abi --print-icp --data %t.fdata \
-# RUN:   | FileCheck %s --check-prefix=CHECK-ICP-NO-INLINE
-# CHECK-ICP-NO-INLINE: Binary Function "indirectTailCall" after indirect-call-promotion
-# CHECK-ICP-NO-INLINE: b    bar
-# CHECK-ICP-NO-INLINE: End of Function "indirectTailCall"
-# CHECK-ICP-NO-INLINE: Binary Function "indirectCall" after indirect-call-promotion
-# CHECK-ICP-NO-INLINE: bl    bar
-# CHECK-ICP-NO-INLINE: End of Function "indirectCall"
-
 # Indirect call promotion with inline
 # RUN: llvm-bolt %t.exe --icp=calls --icp-calls-topn=1 --inline-small-functions \
 # RUN:   -o %t.null --lite=0 --assume-abi --inline-small-functions-bytes=12 \
 # RUN:   --print-inline --data %t.fdata \
-# RUN:   | FileCheck %s --check-prefix=CHECK-ICP-WITH-INLINE
-# CHECK-ICP-WITH-INLINE:     Binary Function "indirectTailCall" after inlining
-# CHECK-ICP-WITH-INLINE:     br    x1
-# CHECK-ICP-WITH-INLINE-NOT: b    bar
-# CHECK-ICP-WITH-INLINE:     End of Function "indirectTailCall"
+# RUN:   | FileCheck %s
+# CHECK:     Binary Function "indirectTailCall" after inlining
+# CHECK:     br    x1
+# CHECK-NOT: b    bar
+# CHECK:     End of Function "indirectTailCall"
     .globl  foo
     .type   foo,@function
 foo:
