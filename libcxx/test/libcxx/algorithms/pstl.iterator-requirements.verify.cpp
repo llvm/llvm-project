18881aea45556d321505c64ef3ebcbab78c331ec
Michael Kazakov <mike.kazakov@gmail.com>
2026-06-30 22:10:58 +0100

    Addressed the feedback

--- libcxx/test/libcxx/algorithms/pstl.iterator-requirements.verify.cpp
+++ libcxx/test/libcxx/algorithms/pstl.iterator-requirements.verify.cpp
@@ -91,9 +91,13 @@ void f(non_forward_iterator non_fwd, non_output_iterator non_output, std::execut
 
   {
     (void)std::find_first_of(
-        pol, non_fwd, non_fwd, non_fwd, non_fwd); // expected-error@*:* {{static assertion failed: find_first_of}}
+        pol, it, it, non_fwd, non_fwd); // expected-error@*:* {{static assertion failed: find_first_of}}
     (void)std::find_first_of(
-        pol, non_fwd, non_fwd, non_fwd, non_fwd, pred); // expected-error@*:* {{static assertion failed: find_first_of}}
+        pol, it, it, non_fwd, non_fwd, pred); // expected-error@*:* {{static assertion failed: find_first_of}}
+    (void)std::find_first_of(
+        pol, non_fwd, non_fwd, it, it); // expected-error@*:* {{static assertion failed: find_first_of}}
+    (void)std::find_first_of(
+        pol, non_fwd, non_fwd, it, it, pred); // expected-error@*:* {{static assertion failed: find_first_of}}
   }
 
   {
