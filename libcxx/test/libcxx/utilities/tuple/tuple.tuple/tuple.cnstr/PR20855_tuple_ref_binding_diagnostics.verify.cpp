958f25ad57ee72b3ef853b69856e04be8cba2477
yronglin <yronglin777@gmail.com>
2026-06-30 02:36:45 -0700

    Fix test
    Signed-off-by: yronglin <yronglin777@gmail.com>

--- libcxx/test/libcxx/utilities/tuple/tuple.tuple/tuple.cnstr/PR20855_tuple_ref_binding_diagnostics.verify.cpp
+++ libcxx/test/libcxx/utilities/tuple/tuple.tuple/tuple.cnstr/PR20855_tuple_ref_binding_diagnostics.verify.cpp
@@ -39,33 +39,35 @@ template <class ...Args>
 void F(typename CannotDeduce<std::tuple<Args...>>::type const&) {}
 
 void f() {
-  // Test that the public constructors are deleted when constructing a reference
-  // element would bind to a temporary.
+  // Constructing a reference element that would bind to a temporary is rejected. Since C++23
+  // ([tuple.cnstr]) the offending constructors are deleted, so the error is reported at the call;
+  // before C++23 the bind is caught by a static_assert in the library (with Clang additionally
+  // diagnosing the dangling reference member).
+#if TEST_STD_VER >= 23
+  // expected-error@*:* 8 {{deleted}}
+#else
+  // expected-error@tuple:* 8 {{Attempted construction of reference element binds to a temporary whose lifetime has ended}}
+  // expected-error@tuple:* 0+ {{reference member '__value_' binds to a temporary object whose lifetime would be shorter than the lifetime of the constructed object}}
+#endif
 
-  {
-    F<int, const std::string&>(std::make_tuple(1, "abc")); // expected-error {{deleted}}
-  }
-  {
-    std::tuple<int, const std::string&> t(1, "a"); // expected-error {{deleted}}
-  }
-  {
-    F<int, const std::string&>(std::tuple<int, const std::string&>(1, "abc")); // expected-error {{deleted}}
-  }
+  { F<int, const std::string&>(std::make_tuple(1, "abc")); }
+  { std::tuple<int, const std::string&> t(1, "a"); }
+  { F<int, const std::string&>(std::tuple<int, const std::string&>(1, "abc")); }
   {
     ConvertsTo<int&> ct;
-    std::tuple<const long&, int> t(ct, 42); // expected-error {{deleted}}
+    std::tuple<const long&, int> t(ct, 42);
   }
   {
     ConvertsTo<int> ct;
-    std::tuple<int const&, void*> t(ct, nullptr); // expected-error {{deleted}}
+    std::tuple<int const&, void*> t(ct, nullptr);
   }
   {
     ConvertsTo<Derived> ct;
-    std::tuple<Base const&, int> t(ct, 42); // expected-error {{deleted}}
+    std::tuple<Base const&, int> t(ct, 42);
   }
   {
     std::allocator<int> alloc;
-    std::tuple<std::string&&> t2("hello");                            // expected-error {{deleted}}
-    std::tuple<std::string&&> t3(std::allocator_arg, alloc, "hello"); // expected-error {{deleted}}
+    std::tuple<std::string&&> t2("hello");
+    std::tuple<std::string&&> t3(std::allocator_arg, alloc, "hello");
   }
 }
