242d4b60fe91f7343dd607b33606074382d6a957
Stephen Tozer <stephen.tozer@sony.com>
2026-06-30 19:56:47 +0100

    [Dexter] Write expects for variables in Debugger scopes (#203254)
    Following on from the previous patch, this patch adds support for
    writing expects from !value/all nodes, generating separate expects for
    each variable in the requested debugger scope, for each continuous range
    of lines it is live for.
    
    NB: As with the other patch that implements script-writing features,
    each test in this patch has a corresponding "expected" file, which is
    almost identical (including the `RUN` lines), and exists to be `diff`'d
    against the output of Dexter's script generation.

---
+++ cross-project-tests/debuginfo-tests/dexter/feature_tests/scripts/rewriting/Inputs/rewrite_scopes_expected.cpp
@@ -0,0 +1,49 @@
+// RUN: rm -rf %t
+// RUN: mkdir %t
+// RUN: %dexter_regression_test_cxx_build %s -o %t/test
+// RUN: %dexter_regression_test_run --use-script --binary %t/test \
+// RUN:   --results-directory %t/results -- %s 2>&1 | FileCheck %s
+// RUN: diff %t/results/%{s:basename} \
+// RUN:   %S/Inputs/rewrite_scopes_expected.cpp
+
+/// Tests that we can collect the values of all available variables with
+/// !value/all and produce corresponding variable expects.
+
+/// NB: The exact contents of this file are compared against the expect file in
+///     the Inputs/ directory; any changes to this file, including comments,
+///     will require updating the corresponding expected file.
+
+char There[] = "Here";
+
+int main() {
+  int One = 2;
+  char Red[] = "Blue";
+  return 0; // !dex_label ret
+}
+
+// CHECK: total_watched_steps: 3
+// CHECK: correct_steps: 3
+// CHECK: incorrect_steps: 0
+// CHECK: missing_var_steps: 0
+// CHECK: unexpected_value_steps: 0
+// CHECK: seen_values: 11
+// CHECK: missing_values: 0
+
+/*
+---
+? !where {lines: !label 'ret'}
+: !value 'One': '2'
+  !value 'Red':
+    '[0]': '''B'''
+    '[1]': '''l'''
+    '[2]': '''u'''
+    '[3]': '''e'''
+    '[4]': '''\0'''
+  !value '::There':
+    '[0]': '''H'''
+    '[1]': '''e'''
+    '[2]': '''r'''
+    '[3]': '''e'''
+    '[4]': '''\0'''
+...
+*/
