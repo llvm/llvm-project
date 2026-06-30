8d1201d17174529b4848845d5e0f356e7475ab07
Jan Newger <jannewger@gmail.com>
2026-06-30 10:53:09 +0000

    [CopyProf] Add CopyProf instrumentation passes.
    This patch introduces the instrumentation passes and corresponding tests
    for CopyProf, a profiling tool designed to identify unnecessary object
    copies in C++ applications.
    RFC at
    https://discourse.llvm.org/t/rfc-copysanitizer-csan-detecting-unneccessary-object-copies-at-runtime/91038.
    
    Three passes are added:
    - CopyProfPass inserts enter/exit callback around special member
    functions.
    - CopyPRofStoresPass instruments store instructions  to track memory
    modifications.
    - ModuleCopyProfPass inserts a module constructor to initialize the
    CopyProf runtime at program startup (will be added later).

---
+++ llvm/lib/Transforms/Instrumentation/CopyProf.cpp
@@ -0,0 +1,297 @@
+//===-- CopyProf.cpp ------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+///
+/// This file implements the LLVM IR instrumentation passes for CopyProf.
+/// It adds enter/exit callbacks to C++ special member functions, and
+/// instruments store instructions.
+///
+/// The basic idea of the CopyProf algorithm works like this:
+/// An object copy Y is made from original object X. The shadow memory
+/// corresponding to (and owned by) Y is marked as "copied". Any subsequent
+/// memory store to the memory corresponding to Y marks the shadow memory as
+/// "modified". When Y is destroyed and all of its corresponding shadow memory
+/// is marked as "copied", the object is reported as an unnecessary copy.
+///
+//===----------------------------------------------------------------------===//
+
+#include "llvm/Transforms/Instrumentation/CopyProf.h"
+
+#include "llvm/ADT/SmallVector.h"
+#include "llvm/ADT/StringExtras.h"
+#include "llvm/IR/Attributes.h"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/Instruction.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/PassManager.h"
+#include "llvm/Support/Casting.h"
+#include "llvm/Support/ErrorHandling.h"
+#include "llvm/Support/FormatVariadic.h"
+#include "llvm/Transforms/Utils/Instrumentation.h"
+#include "llvm/Transforms/Utils/ModuleUtils.h"
+#include <array>
+#include <cstddef>
+#include <cstdint>
+
+// TODO: Convert CopyProfPass and CopyProfStoresPass to module passes so that
+// the runtime callbacks can be cached, thus avoiding repetitive symbol table
+// lookups.
+
+using namespace llvm;
+
+// Names for the module c'tor to initialize the runtime, and the runtime
+// initialization function itself.
+constexpr StringRef CopyProfModuleCtorName = "copyprof.module_ctor";
+constexpr StringRef CopyProfInitName = "__copyprof_init";
+
+// Runtime callback function names.
+constexpr StringRef CopyProfCtorEnterCallbackName =
+    "__copyprof_ctor_enter_callback";
+constexpr StringRef CopyProfCtorExitCallbackName =
+    "__copyprof_ctor_exit_callback";
+constexpr StringRef CopyProfCopyCtorEnterCallbackName =
+    "__copyprof_copy_ctor_enter_callback";
+constexpr StringRef CopyProfCopyCtorExitCallbackName =
+    "__copyprof_copy_ctor_exit_callback";
+constexpr StringRef CopyProfCopyAssignOpEnterCallbackName =
+    "__copyprof_copy_assign_op_enter_callback";
+constexpr StringRef CopyProfCopyAssignOpExitCallbackName =
+    "__copyprof_copy_assign_op_exit_callback";
+constexpr StringRef CopyProfDtorEnterCallbackName =
+    "__copyprof_dtor_enter_callback";
+constexpr StringRef CopyProfDtorExitCallbackName =
+    "__copyprof_dtor_exit_callback";
+constexpr StringRef CopyProfStoreCallbackName = "__copyprof_store_callback";
+
+// Attribute strings used by the frontend to mark special member functions.
+constexpr StringRef CopyProfCtorAttr = "copyprof-ctor";
+constexpr StringRef CopyProfCopyCtorAttr = "copyprof-copy-ctor";
+constexpr StringRef CopyProfCopyAssignAttr = "copyprof-copy-assign-op";
+constexpr StringRef CopyProfDtorAttr = "copyprof-dtor";
+
+static bool insertModuleCtor(Module &M) {
+  bool Modified = false;
+  getOrCreateSanitizerCtorAndInitFunctions(
+      M, CopyProfModuleCtorName, CopyProfInitName,
+      /*InitArgTypes=*/{},
+      /*InitArgs=*/{}, [&](Function *Ctor, FunctionCallee) {
+        appendToGlobalCtors(M, Ctor, 0);
+        Modified = true;
+      });
+  return Modified;
+}
+
+// Returns the object size in bytes that was stored in the given function
+// attribute during parsing in the frontend.
+static size_t getAttrValueAsInt(const Function &F, StringRef Attr) {
+  size_t IntValue = 0;
+  if (!to_integer<size_t>(F.getFnAttribute(Attr).getValueAsString(), IntValue,
+                          /*Base=*/10)) {
+    report_fatal_error(formatv("Unable to parse integer value from function "
+                               "attribute value in '{0}': {1}:{2}",
+                               F.getName(), Attr,
+                               F.getFnAttribute(Attr).getValueAsString()));
+  }
+  return IntValue;
+}
+
+namespace {
+
+// Instruments special member functions to call into the CopyProf runtime.
+class CopyProf {
+public:
+  explicit CopyProf(Module &M);
+  bool instrumentFunction(Function &F);
+
+private:
+  void insertCallback(Function &F, size_t ObjSize, unsigned NumArgs,
+                      FunctionCallee Callback, FunctionCallee ExitCallback);
+
+  LLVMContext *Ctx;
+  Type *IntPtrTy;
+  FunctionCallee CtorEnterCallback;
+  FunctionCallee CtorExitCallback;
+  FunctionCallee CopyCtorEnterCallback;
+  FunctionCallee CopyCtorExitCallback;
+  FunctionCallee CopyAssignOpEnterCallback;
+  FunctionCallee CopyAssignOpExitCallback;
+  FunctionCallee DtorEnterCallback;
+  FunctionCallee DtorExitCallback;
+};
+
+// Late-stage pass that instruments store instructions after all optimizations
+// have run (to avoid instrumenting stores that would be eliminated).
+class CopyProfStores {
+public:
+  explicit CopyProfStores(Module &M);
+  bool instrumentFunction(Function &F);
+
+private:
+  Type *IntPtrTy;
+  FunctionCallee StoreCallback;
+};
+
+} // namespace
+
+CopyProf::CopyProf(Module &M) {
+  Ctx = &M.getContext();
+  IRBuilder<> IRB(*Ctx);
+  IntPtrTy = IRB.getIntPtrTy(M.getDataLayout());
+  Type *PtrTy = IRB.getPtrTy();
+  Type *VoidTy = IRB.getVoidTy();
+  // CopyProf callbacks never throw exceptions.
+  AttributeList Attr;
+  Attr = Attr.addFnAttribute(*Ctx, Attribute::NoUnwind);
+  CtorEnterCallback = M.getOrInsertFunction(CopyProfCtorEnterCallbackName, Attr,
+                                            VoidTy, PtrTy, IntPtrTy);
+  CtorExitCallback = M.getOrInsertFunction(CopyProfCtorExitCallbackName, Attr,
+                                           VoidTy, PtrTy, IntPtrTy);
+  CopyCtorEnterCallback = M.getOrInsertFunction(
+      CopyProfCopyCtorEnterCallbackName, Attr, VoidTy, PtrTy, PtrTy, IntPtrTy);
+  CopyCtorExitCallback = M.getOrInsertFunction(
+      CopyProfCopyCtorExitCallbackName, Attr, VoidTy, PtrTy, PtrTy, IntPtrTy);
+  CopyAssignOpEnterCallback =
+      M.getOrInsertFunction(CopyProfCopyAssignOpEnterCallbackName, Attr, VoidTy,
+                            PtrTy, PtrTy, IntPtrTy);
+  CopyAssignOpExitCallback =
+      M.getOrInsertFunction(CopyProfCopyAssignOpExitCallbackName, Attr, VoidTy,
+                            PtrTy, PtrTy, IntPtrTy);
+  DtorEnterCallback = M.getOrInsertFunction(CopyProfDtorEnterCallbackName, Attr,
+                                            VoidTy, PtrTy, IntPtrTy);
+  DtorExitCallback = M.getOrInsertFunction(CopyProfDtorExitCallbackName, Attr,
+                                           VoidTy, PtrTy, IntPtrTy);
+}
+
+bool CopyProf::instrumentFunction(Function &F) {
+  // Must not instrument our own module c'tor or functions that are explicitly
+  // disallowed for instrumentation.
+  if (F.isDeclaration() ||
+      F.hasFnAttribute(Attribute::DisableSanitizerInstrumentation) ||
+      F.getName() == CopyProfModuleCtorName)
+    return false;
+
+  bool Modified = true;
+  if (F.hasFnAttribute(CopyProfCtorAttr))
+    insertCallback(F, getAttrValueAsInt(F, CopyProfCtorAttr), /*NumArgs=*/1,
+                   CtorEnterCallback, CtorExitCallback);
+  else if (F.hasFnAttribute(CopyProfCopyCtorAttr))
+    insertCallback(F, getAttrValueAsInt(F, CopyProfCopyCtorAttr), /*NumArgs=*/2,
+                   CopyCtorEnterCallback, CopyCtorExitCallback);
+  else if (F.hasFnAttribute(CopyProfCopyAssignAttr))
+    insertCallback(F, getAttrValueAsInt(F, CopyProfCopyAssignAttr),
+                   /*NumArgs=*/2, CopyAssignOpEnterCallback,
+                   CopyAssignOpExitCallback);
+  else if (F.hasFnAttribute(CopyProfDtorAttr))
+    insertCallback(F, getAttrValueAsInt(F, CopyProfDtorAttr), /*NumArgs=*/1,
+                   DtorEnterCallback, DtorExitCallback);
+  else
+    Modified = false;
+
+  return Modified;
+}
+
+void CopyProf::insertCallback(Function &F, size_t ObjSize, unsigned NumArgs,
+                              FunctionCallee EntryCallback,
+                              FunctionCallee ExitCallback) {
+  auto InsertCallback = [IntPtrTy = IntPtrTy, ObjSize,
+                         NumArgs](Function &F, InstrumentationIRBuilder &&IRB,
+                                  FunctionCallee Callback) {
+    SmallVector<Value *, 3> Args;
+    // `this` is always the first argument to a special member function, but
+    // copy c'tor / copy assignment operator will have the other `this` ptr
+    // passed as their second argument.
+    assert(NumArgs == 1 || NumArgs == 2);
+    for (unsigned I = 0; I < NumArgs; ++I)
+      Args.push_back(F.getArg(I));
+    // The last argument to the callback is the static size of the object
+    // pointed at by `this`.
+    Args.push_back(ConstantInt::get(IntPtrTy, ObjSize));
+    IRB.CreateCall(Callback, Args);
+  };
+
+  InsertCallback(
+      F,
+      InstrumentationIRBuilder{&F.getEntryBlock(),
+                               F.getEntryBlock().getFirstNonPHIOrDbgOrAlloca()},
+      EntryCallback);
+  for (BasicBlock &BB : F) {
+    Instruction *Term = BB.getTerminator();
+    if (isa<ReturnInst>(Term) || isa<ResumeInst>(Term))
+      InsertCallback(F, InstrumentationIRBuilder{Term}, ExitCallback);
+  }
+}
+
+CopyProfStores::CopyProfStores(Module &M) {
+  LLVMContext &Ctx = M.getContext();
+  IRBuilder<> IRB(Ctx);
+  IntPtrTy = IRB.getIntPtrTy(M.getDataLayout());
+  Type *PtrTy = IRB.getPtrTy();
+  Type *VoidTy = IRB.getVoidTy();
+  // CopyProf callbacks never throw exceptions.
+  AttributeList Attr;
+  Attr = Attr.addFnAttribute(Ctx, Attribute::NoUnwind);
+  StoreCallback = M.getOrInsertFunction(CopyProfStoreCallbackName, Attr, VoidTy,
+                                        PtrTy, IntPtrTy);
+}
+
+bool CopyProfStores::instrumentFunction(Function &F) {
+  if (F.isDeclaration() ||
+      F.hasFnAttribute(Attribute::DisableSanitizerInstrumentation) ||
+      F.getName() == CopyProfModuleCtorName)
+    return false;
+
+  // TODO: handle all types of memory stores (memory intrinsics, masked store
+  // intrinsics, AtomicRMW, and AtomicCmpXchg).
+  SmallVector<StoreInst *, 16> ToInstrument;
+  for (BasicBlock &BB : F) {
+    for (Instruction &I : BB) {
+      if (auto *SI = dyn_cast<StoreInst>(&I);
+          SI != nullptr && SI->getPointerAddressSpace() == 0 &&
+          !SI->hasMetadata(LLVMContext::MD_nosanitize))
+        ToInstrument.push_back(SI);
+    }
+  }
+  if (ToInstrument.empty())
+    return false;
+
+  const DataLayout &DL = F.getParent()->getDataLayout();
+  for (StoreInst *SI : ToInstrument) {
+    uint64_t StoredSize = DL.getTypeStoreSize(SI->getValueOperand()->getType());
+    InstrumentationIRBuilder IRB(SI);
+    std::array<Value *, 2> Args = {SI->getPointerOperand(),
+                                   ConstantInt::get(IntPtrTy, StoredSize)};
+    IRB.CreateCall(StoreCallback, Args);
+  }
+  return true;
+}
+
+PreservedAnalyses CopyProfPass::run(Function &F, FunctionAnalysisManager &) {
+  CopyProf Impl(*F.getParent());
+  if (!Impl.instrumentFunction(F))
+    return PreservedAnalyses::all();
+  PreservedAnalyses PA;
+  PA.preserveSet<CFGAnalyses>();
+  return PA;
+}
+
+PreservedAnalyses ModuleCopyProfPass::run(Module &M, ModuleAnalysisManager &) {
+  return insertModuleCtor(M) ? PreservedAnalyses::none()
+                             : PreservedAnalyses::all();
+}
+
+PreservedAnalyses CopyProfStoresPass::run(Function &F,
+                                          FunctionAnalysisManager &) {
+  CopyProfStores Impl(*F.getParent());
+  if (!Impl.instrumentFunction(F))
+    return PreservedAnalyses::all();
+  PreservedAnalyses PA;
+  PA.preserveSet<CFGAnalyses>();
+  return PA;
+}
---
+++ llvm/test/Instrumentation/CopyProf/store-instrumentation.ll
@@ -0,0 +1,101 @@
+; Tests CopyProf store instrumentation.
+;
+; RUN: opt < %s -passes='function(copyprof-stores)' -S | FileCheck %s
+
+target datalayout = "e-p:64:64:64-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64-f32:32:32-f64:64:64-v64:64:64-v128:128:128-a0:0:64-s0:64:64-f80:128:128-n8:16:32:64"
+target triple = "x86_64-unknown-linux-gnu"
+
+;; Tests that a simple i32 store is instrumented.
+define void @test_store_i32(ptr %a) {
+entry:
+  store i32 42, ptr %a, align 4
+  ret void
+}
+; CHECK-LABEL: define void @test_store_i32(ptr %a)
+; CHECK:         call void @__copyprof_store_callback(ptr %a, i64 4)
+; CHECK-NEXT:    store i32 42, ptr %a
+
+;; Tests that an i64 store is instrumented with the correct size.
+define void @test_store_i64(ptr %a) {
+entry:
+  store i64 100, ptr %a, align 8
+  ret void
+}
+; CHECK-LABEL: define void @test_store_i64(ptr %a)
+; CHECK:         call void @__copyprof_store_callback(ptr %a, i64 8)
+; CHECK-NEXT:    store i64 100, ptr %a
+
+;; Tests that multiple stores in the same function are all instrumented.
+define void @test_multiple_stores(ptr %a, ptr %b) {
+entry:
+  store i32 1, ptr %a, align 4
+  store i32 2, ptr %b, align 4
+  ret void
+}
+; CHECK-LABEL: define void @test_multiple_stores(ptr %a, ptr %b)
+; CHECK:         call void @__copyprof_store_callback(ptr %a, i64 4)
+; CHECK-NEXT:    store i32 1, ptr %a
+; CHECK:         call void @__copyprof_store_callback(ptr %b, i64 4)
+; CHECK-NEXT:    store i32 2, ptr %b
+
+;; Tests that a function with no stores is not modified.
+define i32 @test_no_stores(ptr %a) {
+entry:
+  %val = load i32, ptr %a
+  ret i32 %val
+}
+; CHECK-LABEL: define i32 @test_no_stores(ptr %a)
+; CHECK-NOT:     call void @__copyprof_store_callback
+; CHECK:         ret i32
+
+;; Tests that stores to non-default address spaces are not instrumented.
+define void @test_addrspace_store(ptr addrspace(1) %a) {
+entry:
+  store i32 42, ptr addrspace(1) %a, align 4
+  ret void
+}
+; CHECK-LABEL: define void @test_addrspace_store(ptr addrspace(1) %a)
+; CHECK-NOT:     call void @__copyprof_store_callback
+; CHECK:         store i32 42, ptr addrspace(1) %a
+; CHECK-NEXT:    ret void
+
+;; Tests that a vector store is instrumented with the correct aggregate size.
+define void @test_store_vector(ptr %a) {
+entry:
+  store <4 x i32> zeroinitializer, ptr %a, align 16
+  ret void
+}
+; CHECK-LABEL: define void @test_store_vector(ptr %a)
+; CHECK:         call void @__copyprof_store_callback(ptr %a, i64 16)
+; CHECK-NEXT:    store <4 x i32> zeroinitializer, ptr %a
+
+;; Tests that an aggregate store is instrumented with the correct size.
+define void @test_store_struct(ptr %a) {
+entry:
+  store <{ i32, i16, i8 }> zeroinitializer, ptr %a
+  ret void
+}
+; CHECK-LABEL: define void @test_store_struct(ptr %a)
+; CHECK:         call void @__copyprof_store_callback(ptr %a, i64 7)
+; CHECK-NEXT:    store <{ i32, i16, i8 }> zeroinitializer, ptr %a
+
+;; Tests that volatile stores are instrumented.
+define void @test_store_volatile(ptr %a) {
+entry:
+  store volatile i32 42, ptr %a, align 4
+  ret void
+}
+; CHECK-LABEL: define void @test_store_volatile(ptr %a)
+; CHECK:         call void @__copyprof_store_callback(ptr %a, i64 4)
+; CHECK-NEXT:    store volatile i32 42, ptr %a
+
+;; Tests that atomic stores are instrumented.
+define void @test_store_atomic(ptr %a) {
+entry:
+  store atomic i32 42, ptr %a monotonic, align 4
+  ret void
+}
+; CHECK-LABEL: define void @test_store_atomic(ptr %a)
+; CHECK:         call void @__copyprof_store_callback(ptr %a, i64 4)
+; CHECK-NEXT:    store atomic i32 42, ptr %a monotonic
+
