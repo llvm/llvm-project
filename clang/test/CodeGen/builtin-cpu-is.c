b527fb6b5d0bcdf5968c04acc7f2e7c0dfe5b17d
Pirog, Mikolaj Maciej <mikolaj.maciej.pirog@intel.com>
2026-07-01 18:33:37 +0200

    add test

--- clang/test/CodeGen/builtin-cpu-is.c
+++ clang/test/CodeGen/builtin-cpu-is.c
@@ -53,3 +53,127 @@ void nehalem(void) {
   // CHECK: [[LOAD:%[^ ]+]] = load i32, ptr getelementptr inbounds nuw (i8, ptr @__cpu_model, i64 8)
   // CHECK: = icmp eq i32 [[LOAD]], 1
 }
+
+void other(void) {
+  if (__builtin_cpu_is("other"))
+    a("other");
+
+  // CHECK: [[LOAD:%[^ ]+]] = load i32, ptr @__cpu_model
+  // CHECK: = icmp eq i32 [[LOAD]], 4
+}
+
+// A type alias must map to the same ABI value as its canonical entry.
+void slm(void) {
+  if (__builtin_cpu_is("slm"))
+    a("slm");
+
+  // CHECK: [[LOAD:%[^ ]+]] = load i32, ptr getelementptr inbounds nuw (i8, ptr @__cpu_model, i64 4)
+  // CHECK: = icmp eq i32 [[LOAD]], 6
+}
+
+void amdfam10(void) {
+  if (__builtin_cpu_is("amdfam10"))
+    a("amdfam10");
+
+  // CHECK: [[LOAD:%[^ ]+]] = load i32, ptr getelementptr inbounds nuw (i8, ptr @__cpu_model, i64 4)
+  // CHECK: = icmp eq i32 [[LOAD]], 4
+}
+
+// Subtype ABI values below have gaps reserved for Zhaoxin CPUs; they must
+// match the values implemented in compiler-rt/libgcc exactly.
+void znver5(void) {
+  if (__builtin_cpu_is("znver5"))
+    a("znver5");
+
+  // CHECK: [[LOAD:%[^ ]+]] = load i32, ptr getelementptr inbounds nuw (i8, ptr @__cpu_model, i64 8)
+  // CHECK: = icmp eq i32 [[LOAD]], 36
+}
+
+void diamondrapids(void) {
+  if (__builtin_cpu_is("diamondrapids"))
+    a("diamondrapids");
+
+  // CHECK: [[LOAD:%[^ ]+]] = load i32, ptr getelementptr inbounds nuw (i8, ptr @__cpu_model, i64 8)
+  // CHECK: = icmp eq i32 [[LOAD]], 38
+}
+
+void novalake(void) {
+  if (__builtin_cpu_is("novalake"))
+    a("novalake");
+
+  // CHECK: [[LOAD:%[^ ]+]] = load i32, ptr getelementptr inbounds nuw (i8, ptr @__cpu_model, i64 8)
+  // CHECK: = icmp eq i32 [[LOAD]], 39
+}
+
+void znver6(void) {
+  if (__builtin_cpu_is("znver6"))
+    a("znver6");
+
+  // CHECK: [[LOAD:%[^ ]+]] = load i32, ptr getelementptr inbounds nuw (i8, ptr @__cpu_model, i64 8)
+  // CHECK: = icmp eq i32 [[LOAD]], 40
+}
+
+void c86_4g_m4(void) {
+  if (__builtin_cpu_is("c86-4g-m4"))
+    a("c86-4g-m4");
+
+  // CHECK: [[LOAD:%[^ ]+]] = load i32, ptr getelementptr inbounds nuw (i8, ptr @__cpu_model, i64 8)
+  // CHECK: = icmp eq i32 [[LOAD]], 41
+}
+
+void c86_4g_m8(void) {
+  if (__builtin_cpu_is("c86-4g-m8"))
+    a("c86-4g-m8");
+
+  // CHECK: [[LOAD:%[^ ]+]] = load i32, ptr getelementptr inbounds nuw (i8, ptr @__cpu_model, i64 8)
+  // CHECK: = icmp eq i32 [[LOAD]], 44
+}
+
+// Subtype aliases must map to the same ABI value as their canonical entry.
+void raptorlake(void) {
+  if (__builtin_cpu_is("raptorlake"))
+    a("raptorlake");
+
+  // CHECK: [[LOAD:%[^ ]+]] = load i32, ptr getelementptr inbounds nuw (i8, ptr @__cpu_model, i64 8)
+  // CHECK: = icmp eq i32 [[LOAD]], 25
+}
+
+void meteorlake(void) {
+  if (__builtin_cpu_is("meteorlake"))
+    a("meteorlake");
+
+  // CHECK: [[LOAD:%[^ ]+]] = load i32, ptr getelementptr inbounds nuw (i8, ptr @__cpu_model, i64 8)
+  // CHECK: = icmp eq i32 [[LOAD]], 25
+}
+
+void gracemont(void) {
+  if (__builtin_cpu_is("gracemont"))
+    a("gracemont");
+
+  // CHECK: [[LOAD:%[^ ]+]] = load i32, ptr getelementptr inbounds nuw (i8, ptr @__cpu_model, i64 8)
+  // CHECK: = icmp eq i32 [[LOAD]], 25
+}
+
+void emeraldrapids(void) {
+  if (__builtin_cpu_is("emeraldrapids"))
+    a("emeraldrapids");
+
+  // CHECK: [[LOAD:%[^ ]+]] = load i32, ptr getelementptr inbounds nuw (i8, ptr @__cpu_model, i64 8)
+  // CHECK: = icmp eq i32 [[LOAD]], 24
+}
+
+void lunarlake(void) {
+  if (__builtin_cpu_is("lunarlake"))
+    a("lunarlake");
+
+  // CHECK: [[LOAD:%[^ ]+]] = load i32, ptr getelementptr inbounds nuw (i8, ptr @__cpu_model, i64 8)
+  // CHECK: = icmp eq i32 [[LOAD]], 33
+}
+
+void wildcatlake(void) {
+  if (__builtin_cpu_is("wildcatlake"))
+    a("wildcatlake");
+
+  // CHECK: [[LOAD:%[^ ]+]] = load i32, ptr getelementptr inbounds nuw (i8, ptr @__cpu_model, i64 8)
+  // CHECK: = icmp eq i32 [[LOAD]], 34
+}
