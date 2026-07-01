9b8729b5245ee29e988afc450734330b39ae056f
Arthur Eubanks <ayueubanks@gmail.com>
2026-07-01 11:05:57 -0700

    Revert "Disable RelLookupTableConverter on AArch64 (#204669)"
    This reverts commit 58f086d0cd58252b2b18fa95f96a566a06e96a36.

--- llvm/include/llvm/CodeGen/BasicTTIImpl.h
+++ llvm/include/llvm/CodeGen/BasicTTIImpl.h
@@ -667,10 +667,9 @@ public:
     if (!TargetTriple.isArch64Bit())
       return false;
 
-    // Disable relative lookup tables for all AArch64 targets. Even AArch64's
-    // small code model allows a 4GB span of text + data, which might not fit
-    // in the 32-bit offsets relative lookup tables generate.
-    if (TargetTriple.isAArch64())
+    // TODO: Triggers issues on aarch64 on darwin, so temporarily disable it
+    // there.
+    if (TargetTriple.getArch() == Triple::aarch64 && TargetTriple.isOSDarwin())
       return false;
 
     return true;
