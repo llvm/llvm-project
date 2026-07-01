1d338d758eb3680a196e03697406c6a624615f5e
Dmitrii Martynov <martynovdmitryvladimirovich@yandex.ru>
2026-07-01 16:40:37 +0300

    [BOLT] ICF: Use nameStartsWith instead of getName
    The getName method returns the name of the first symbol for a given BinaryData.
    If a symbol is associated with the same address as a vtable symbol, the ICF
    check for vtable mangled names may behave unexpectedly. The correct approach is
    to use nameStartsWith, which checks all symbols associated with the BinaryData.

--- bolt/test/safe-icf-relative-vtable.cpp
+++ bolt/test/safe-icf-relative-vtable.cpp
@@ -6,9 +6,9 @@
 // RUN: llvm-bolt %t.so -o %t.bolt --no-threads --icf=safe \
 // RUN:   --debug-only=bolt-icf 2>&1 | FileCheck %s
 
-// RUN: %clang %cxxflags -o %t.so %s -Wl,-q -fno-rtti \
+// RUN: %clang %cxxflags -o %t.exp.so %s -Wl,-q -fno-rtti \
 // RUN:   -fexperimental-relative-c++-abi-vtables
-// RUN: llvm-bolt %t.so -o %t.bolt --no-threads --icf=safe \
+// RUN: llvm-bolt %t.exp.so -o %t.exp.bolt --no-threads --icf=safe \
 // RUN:   --debug-only=bolt-icf 2>&1 | FileCheck %s
 
 // CHECK: folding {{.*bar.*}} into {{.*foo.*}}
@@ -19,6 +19,12 @@ public:
   virtual int foo(int a) { return ++a; }
   virtual int bar(int a) { return ++a; }
 };
+
+// __init_array_end was added to check that ICF works correctly even if the
+// symbol has the same address as a vtable symbol
+extern void (*__init_array_end[])();
+__attribute__((used)) static void *dummy__init_array_end = __init_array_end;
+__attribute__((constructor)) void dummy_ctor() {};
 
 int main() {
   TT T;
