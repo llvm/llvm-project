fab11a2ac602645bb14a41ccea1f09a17462fb7b
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2026-07-01 14:16:02 +0200

    compiler-rt: ubsan: Add suppressions test for inlined functions

---
+++ compiler-rt/test/ubsan/TestCases/Integer/Inputs/wrappers.h
@@ -0,0 +1,16 @@
+#ifndef TEST__WRAPPERS_H
+#define TEST__WRAPPERS_H
+
+#include "make_signed.h"
+
+int my_wrapper(unsigned a) {
+  return my_make_signed(a);
+}
+
+int my_wrapper_2(unsigned a) {
+  volatile int test = a;
+  (void)test;
+  return my_make_signed(a);
+}
+
+#endif
