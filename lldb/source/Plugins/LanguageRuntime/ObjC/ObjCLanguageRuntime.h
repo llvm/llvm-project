1bc994620910481e04cab09d736dc566d27bf453
Jim Ingham <jingham@apple.com>
2026-07-01 10:27:57 -0700

    Formatting

--- lldb/source/Plugins/LanguageRuntime/ObjC/ObjCLanguageRuntime.h
+++ lldb/source/Plugins/LanguageRuntime/ObjC/ObjCLanguageRuntime.h
@@ -307,7 +307,7 @@ public:
 
   virtual ObjCISA GetParentClass(ObjCISA isa);
 
-  virtual ObjCISA GetPointerISA(ObjCISA isa) {return isa; };  
+  virtual ObjCISA GetPointerISA(ObjCISA isa) { return isa; };
 
   // Finds the byte offset of the child_type ivar in parent_type.  If it can't
   // find the offset, returns LLDB_INVALID_IVAR_OFFSET.
