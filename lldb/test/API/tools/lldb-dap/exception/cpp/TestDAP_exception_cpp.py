a38f3b96f47a7e674f89e5bec2862248a0f6b063
Ebuka Ezike <yerimyah1@gmail.com>
2026-07-01 17:35:20 +0100

    [lldb-dap] Migrate DAP exceptions test.
    Migrate all the tests in the `lldb-dap/exception` folder.

--- lldb/test/API/tools/lldb-dap/exception/cpp/TestDAP_exception_cpp.py
+++ lldb/test/API/tools/lldb-dap/exception/cpp/TestDAP_exception_cpp.py
@@ -2,13 +2,13 @@
 Test exception behavior in DAP with c++ throw.
 """
 
-from lldbsuite.test.decorators import *
-from lldbsuite.test.lldbtest import *
-import lldbdap_testcase
+from lldbsuite.test.decorators import skipIfWasm, skipIfWindows
+from lldbsuite.test.tools.lldb_dap import lldb_dap_testcase
+from lldbsuite.test.tools.lldb_dap.dap_types import LaunchArgs
 
 
-@skipIfWasm  # wasm inferiors are built with -fno-exceptions
-class TestDAP_exception_cpp(lldbdap_testcase.DAPTestCaseBase):
+@skipIfWasm  # wasm inferiors are built with -fno-exceptions.
+class TestDAP_exception_cpp(lldb_dap_testcase.DAPTestCaseBase):
     @skipIfWindows
     def test_stopped_description(self):
         """
@@ -16,11 +16,18 @@ class TestDAP_exception_cpp(lldbdap_testcase.DAPTestCaseBase):
         event.
         """
         program = self.getBuildArtifact("a.out")
-        self.build_and_launch(program)
-        self.dap_server.request_continue()
-        self.verify_stop_exception_info("signal SIGABRT")
-        exceptionInfo = self.get_exceptionInfo()
-        self.assertEqual(exceptionInfo["breakMode"], "always")
-        self.assertIn("signal SIGABRT", exceptionInfo["description"])
-        self.assertEqual(exceptionInfo["exceptionId"], "signal")
-        self.assertIsNotNone(exceptionInfo["details"])
+        session = self.build_and_create_session()
+        process_event = session.launch(LaunchArgs(program=program))
+
+        stopped_event = session.verify_stopped_on_exception(
+            expected_description="signal SIGABRT", after=process_event
+        )
+
+        thread_id = self.expect_not_none(stopped_event.body.threadId)
+        exception_info = session.get_exception_info(thread_id)
+
+        self.assertEqual(exception_info.breakMode, "always")
+        description = self.expect_not_none(exception_info.description)
+        self.assertIn("signal SIGABRT", description)
+        self.assertEqual(exception_info.exceptionId, "signal")
+        self.assertIsNotNone(exception_info.details)
