1f5002ad8824ed8873531365b0a89840862fbcad
Arseniy Obolenskiy <arseniy.obolenskiy@amd.com>
2026-07-01 14:36:35 +0200

    [SPIR-V] Use MathExtras helpers for bit arithmetic (NFC)

--- llvm/lib/Target/SPIRV/SPIRVUtils.cpp
+++ llvm/lib/Target/SPIRV/SPIRVUtils.cpp
@@ -24,6 +24,7 @@
 #include "llvm/Demangle/Demangle.h"
 #include "llvm/IR/IntrinsicInst.h"
 #include "llvm/IR/IntrinsicsSPIRV.h"
+#include "llvm/Support/MathExtras.h"
 #include <queue>
 #include <vector>
 
@@ -165,7 +166,7 @@ static uint32_t convertCharsToWord(const StringRef &Str, unsigned i) {
 
 // Get length including padding and null terminator.
 static size_t getPaddedLen(const StringRef &Str) {
-  return (Str.size() + 4) & ~3;
+  return alignTo(Str.size() + 1, 4);
 }
 
 void addStringImm(const StringRef &Str, MCInst &Inst) {
@@ -219,15 +220,13 @@ void addNumImm(const APInt &Imm, MachineInstrBuilder &MIB) {
     return;
   } else if (Bitwidth <= 64) {
     uint64_t FullImm = Imm.getZExtValue();
-    uint32_t LowBits = FullImm & 0xffffffff;
-    uint32_t HighBits = (FullImm >> 32) & 0xffffffff;
-    MIB.addImm(LowBits).addImm(HighBits);
+    MIB.addImm(Lo_32(FullImm)).addImm(Hi_32(FullImm));
     // Asm Printer needs this info to print 64-bit operands correctly
     MIB.getInstr()->setAsmPrinterFlag(SPIRV::ASM_PRINTER_WIDTH64);
     return;
   } else {
     // Emit ceil(Bitwidth / 32) words to conform SPIR-V spec.
-    unsigned NumWords = (Bitwidth + 31) / 32;
+    unsigned NumWords = divideCeil(Bitwidth, 32);
     for (unsigned I = 0; I < NumWords; ++I) {
       unsigned LimbIdx = I / 2;
       unsigned LimbShift = (I % 2) * 32;
