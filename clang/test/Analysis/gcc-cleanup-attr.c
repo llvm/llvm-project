787e995a858e4064ee5a8e133530f4d43ae072f8
Ella Ma <alansnape3058@gmail.com>
2026-07-02 23:52:03 +0200

    fix liveness issue

--- clang/test/Analysis/gcc-cleanup-attr.c
+++ clang/test/Analysis/gcc-cleanup-attr.c
@@ -4,8 +4,10 @@ void clang_analyzer_dump(int);
 
 void defined_cleanup(int *p) {
   clang_analyzer_dump(*p); // expected-warning {{42}}
+                           // expected-warning@-1 {{43}}
 }
 
 void vardecl_defined() {
   int x __attribute__((cleanup(defined_cleanup))) = 42;
+  int y __attribute__((cleanup(defined_cleanup))) = 43;
 }
