d35e9df266e578b568eccbd085d3347b491b1d57
Divyansh <anshmcs@gmail.com>
2026-07-02 19:09:21 +0530

    Fix clang-format and update test expectations for __builtin_nanf

--- clang/test/Sema/builtin-nan-wide-string.c
+++ clang/test/Sema/builtin-nan-wide-string.c
@@ -10,37 +10,46 @@
 #endif
 
 CONSTEXPR float f1 = __builtin_nanf(L"");
-// c-warning@-1 {{incompatible pointer types passing 'int[1]' to parameter of type 'const char *'}}
-// cxx-error@-2 {{cannot initialize a parameter of type 'const char *' with an lvalue of type 'const wchar_t[1]'}}
+// c-error@-1 {{incompatible pointer types passing 'int[1]' to parameter of type 'const char *'}}
+// c-error@-2 {{initializer element is not a compile-time constant}}
+// cxx-error@-3 {{cannot initialize a parameter of type 'const char *' with an lvalue of type 'const wchar_t[1]'}}
 
 CONSTEXPR double d1 = __builtin_nan(L"");
-// c-warning@-1 {{incompatible pointer types passing 'int[1]' to parameter of type 'const char *'}}
-// cxx-error@-2 {{cannot initialize a parameter of type 'const char *' with an lvalue of type 'const wchar_t[1]'}}
+// c-error@-1 {{incompatible pointer types passing 'int[1]' to parameter of type 'const char *'}}
+// c-error@-2 {{initializer element is not a compile-time constant}}
+// cxx-error@-3 {{cannot initialize a parameter of type 'const char *' with an lvalue of type 'const wchar_t[1]'}}
 
 CONSTEXPR long double ld1 = __builtin_nanl(L"");
-// c-warning@-1 {{incompatible pointer types passing 'int[1]' to parameter of type 'const char *'}}
-// cxx-error@-2 {{cannot initialize a parameter of type 'const char *' with an lvalue of type 'const wchar_t[1]'}}
+// c-error@-1 {{incompatible pointer types passing 'int[1]' to parameter of type 'const char *'}}
+// c-error@-2 {{initializer element is not a compile-time constant}}
+// cxx-error@-3 {{cannot initialize a parameter of type 'const char *' with an lvalue of type 'const wchar_t[1]'}}
 
 CONSTEXPR float f2 = __builtin_nanf(u"");
-// c-warning@-1 {{incompatible pointer types passing 'unsigned short[1]' to parameter of type 'const char *'}}
-// cxx-error@-2 {{cannot initialize a parameter of type 'const char *' with an lvalue of type 'const char16_t[1]'}}
+// c-error@-1 {{incompatible pointer types passing 'unsigned short[1]' to parameter of type 'const char *'}}
+// c-error@-2 {{initializer element is not a compile-time constant}}
+// cxx-error@-3 {{cannot initialize a parameter of type 'const char *' with an lvalue of type 'const char16_t[1]'}}
 
 CONSTEXPR double d2 = __builtin_nan(u"");
-// c-warning@-1 {{incompatible pointer types passing 'unsigned short[1]' to parameter of type 'const char *'}}
-// cxx-error@-2 {{cannot initialize a parameter of type 'const char *' with an lvalue of type 'const char16_t[1]'}}
+// c-error@-1 {{incompatible pointer types passing 'unsigned short[1]' to parameter of type 'const char *'}}
+// c-error@-2 {{initializer element is not a compile-time constant}}
+// cxx-error@-3 {{cannot initialize a parameter of type 'const char *' with an lvalue of type 'const char16_t[1]'}}
 
 CONSTEXPR long double ld2 = __builtin_nanl(u"");
-// c-warning@-1 {{incompatible pointer types passing 'unsigned short[1]' to parameter of type 'const char *'}}
-// cxx-error@-2 {{cannot initialize a parameter of type 'const char *' with an lvalue of type 'const char16_t[1]'}}
+// c-error@-1 {{incompatible pointer types passing 'unsigned short[1]' to parameter of type 'const char *'}}
+// c-error@-2 {{initializer element is not a compile-time constant}}
+// cxx-error@-3 {{cannot initialize a parameter of type 'const char *' with an lvalue of type 'const char16_t[1]'}}
 
 CONSTEXPR float f3 = __builtin_nanf(U"");
-// c-warning@-1 {{incompatible pointer types passing 'unsigned int[1]' to parameter of type 'const char *'}}
-// cxx-error@-2 {{cannot initialize a parameter of type 'const char *' with an lvalue of type 'const char32_t[1]'}}
+// c-error@-1 {{incompatible pointer types passing 'unsigned int[1]' to parameter of type 'const char *'}}
+// c-error@-2 {{initializer element is not a compile-time constant}}
+// cxx-error@-3 {{cannot initialize a parameter of type 'const char *' with an lvalue of type 'const char32_t[1]'}}
 
 CONSTEXPR double d3 = __builtin_nan(U"");
-// c-warning@-1 {{incompatible pointer types passing 'unsigned int[1]' to parameter of type 'const char *'}}
-// cxx-error@-2 {{cannot initialize a parameter of type 'const char *' with an lvalue of type 'const char32_t[1]'}}
+// c-error@-1 {{incompatible pointer types passing 'unsigned int[1]' to parameter of type 'const char *'}}
+// c-error@-2 {{initializer element is not a compile-time constant}}
+// cxx-error@-3 {{cannot initialize a parameter of type 'const char *' with an lvalue of type 'const char32_t[1]'}}
 
 CONSTEXPR long double ld3 = __builtin_nanl(U"");
-// c-warning@-1 {{incompatible pointer types passing 'unsigned int[1]' to parameter of type 'const char *'}}
-// cxx-error@-2 {{cannot initialize a parameter of type 'const char *' with an lvalue of type 'const char32_t[1]'}}
+// c-error@-1 {{incompatible pointer types passing 'unsigned int[1]' to parameter of type 'const char *'}}
+// c-error@-2 {{initializer element is not a compile-time constant}}
+// cxx-error@-3 {{cannot initialize a parameter of type 'const char *' with an lvalue of type 'const char32_t[1]'}}
