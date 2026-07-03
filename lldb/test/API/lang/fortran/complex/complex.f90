20deee87356385cf6944e2260827a506e814a166
Iasonaskrpr <iaskarapro@gmail.com>
2026-07-03 20:42:35 +0300

    [lldb][Fortran] Added Fortran Complex type tests

---
+++ lldb/test/API/lang/fortran/complex/complex.f90
@@ -0,0 +1,13 @@
+program Complex
+  implicit none
+
+  complex(4) :: complex_four
+  complex(8) :: complex_eight
+
+  complex_four = (2.0, 3.0)
+
+  complex_eight = (1.0, 4.0)
+
+  print *, "Done" ! Breakpoint here
+
+end program Complex
