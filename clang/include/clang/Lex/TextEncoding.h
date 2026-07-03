0e48de51557f7e8aa9b09e1a13caf600e57a2294
Abhina Sreeskantharajan <Abhina.Sreeskantharajan@ibm.com>
2026-07-03 08:13:17 -0400

    forward declare TextEncodingConverter

--- clang/include/clang/Lex/TextEncoding.h
+++ clang/include/clang/Lex/TextEncoding.h
@@ -11,7 +11,10 @@
 
 #include "clang/Basic/LangOptions.h"
 #include "llvm/ADT/StringRef.h"
-#include "llvm/Support/TextEncoding.h"
+
+namespace llvm {
+class TextEncodingConverter;
+} // namespace llvm
 
 namespace clang {
 enum ConversionAction { CA_NoConversion, CA_ToLiteralEncoding };
