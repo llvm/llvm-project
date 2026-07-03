34afe76619fe10f44b7f14ad483e017d5d89f9ed
Nikita Popov <npopov@redhat.com>
2026-07-03 09:05:58 +0200

    Fix test failure on AArch64
    The triple might not set any features by default, in which case
    just `module asm` gets printed. Only test for that for the default
    target, and add separate tests for RISCV with different features
    instead.

--- clang/test/CodeGen/2006-01-23-FileScopeAsm.c
+++ clang/test/CodeGen/2006-01-23-FileScopeAsm.c
@@ -1,7 +1,11 @@
 // UNSUPPORTED: target={{.*}}-zos{{.*}}
-// RUN: %clang_cc1 %s -emit-llvm -o - | FileCheck %s
+// RUN: %clang_cc1 %s -triple=riscv64 -emit-llvm -o - | FileCheck %s --check-prefixes=CHECK,RISCV64
+// RUN: %clang_cc1 %s -triple=riscv64 -target-feature +d -emit-llvm -o - | FileCheck %s --check-prefixes=CHECK,RISCV64-D
+// RUN: %clang_cc1 %s -emit-llvm -o - | FileCheck %s --check-prefixes=CHECK,DEFAULT
 
-// CHECK: module asm(target_features: "{{.*}}")
+// RISCV64: module asm(target_features: "+64bit,+i")
+// RISCV64-D: module asm(target_features: "+64bit,+d,+f,+i,+zicsr")
+// DEFAULT: module asm
 // CHECK-NEXT: "foo1"
 __asm__ ("foo1");
 // CHECK-NEXT: "foo2"
