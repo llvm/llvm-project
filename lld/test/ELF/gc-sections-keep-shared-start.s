3098d39ea9ab3fdcd12caa823d5563bd3634cc9a
punkyc <chenying@inchitech.com>
2026-07-06 15:54:14 +0800

    [lld][ELF] Fix FileCheck RUN line in gc-sections-keep-shared-start.s. NFC

--- lld/test/ELF/gc-sections-keep-shared-start.s
+++ lld/test/ELF/gc-sections-keep-shared-start.s
@@ -2,12 +2,12 @@
 
 # RUN: llvm-mc -filetype=obj -triple=x86_64-unknown-linux %s -o %t
 # RUN: ld.lld -shared --gc-sections -o %t1 %t
-# RUN: llvm-readelf --file-headers --symbols %t1
-#   | FileCheck %s
-# CHECK: Entry point address:               0x1000
-# CHECK: 0000000000001000     0 FUNC    LOCAL  HIDDEN     4 _start
-# CHECK: 0000000000001006     0 FUNC    LOCAL  HIDDEN     4 internal
-# CHECK: 0000000000001005     0 FUNC    GLOBAL DEFAULT    4 foobar
+# RUN: llvm-readelf --file-headers --symbols %t1 \
+# RUN:   | FileCheck %s
+# CHECK: Entry point address:               0x1238
+# CHECK: 0000000000001238     0 FUNC    LOCAL  HIDDEN     5 _start
+# CHECK: 000000000000123e     0 FUNC    LOCAL  HIDDEN     5 internal
+# CHECK: 000000000000123d     0 FUNC    GLOBAL DEFAULT    5 foobar
 
 .section .text.start,"ax"
 .globl _start
