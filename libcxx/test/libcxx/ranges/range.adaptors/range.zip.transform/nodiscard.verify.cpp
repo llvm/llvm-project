7595d99fcdce5c0f4856027608807187fd2a3713
Hristo Hristov <hghristov.rmm@gmail.com>
2026-07-06 08:32:19 +0300

    Try to fix

--- libcxx/test/libcxx/ranges/range.adaptors/range.zip.transform/nodiscard.verify.cpp
+++ libcxx/test/libcxx/ranges/range.adaptors/range.zip.transform/nodiscard.verify.cpp
@@ -14,17 +14,40 @@
 #include <utility>
 #include <vector>
 
-struct View : std::ranges::view_interface<View> {
+#include "test_iterators.h"
+
+struct CommonView : std::ranges::view_interface<CommonView> {
   int* begin();
-  const int* begin() const;
-  volatile int* end();
-  const volatile int* end() const;
+  int* begin() const;
+  int* end();
+  int* end() const;
 
   int size() const;
 };
-static_assert(!std::ranges::common_range<View>);
-static_assert(!std::same_as<std::ranges::iterator_t<View>, std::ranges::iterator_t<const View>>);
-static_assert(!std::same_as<std::ranges::sentinel_t<View>, std::ranges::sentinel_t<const View>>);
+static_assert(std::ranges::common_range<CommonView>);
+static_assert(std::same_as<std::ranges::iterator_t<CommonView>, std::ranges::iterator_t<const CommonView>>);
+static_assert(std::same_as<std::ranges::sentinel_t<CommonView>, std::ranges::sentinel_t<const CommonView>>);
+
+struct ForwardSizedNonCommon {
+  int* buffer_      = nullptr;
+  std::size_t size_ = 0;
+
+  template <std::size_t N>
+  constexpr ForwardSizedNonCommon(int (&b)[N]) : buffer_(b), size_(N) {}
+
+  constexpr ForwardSizedNonCommon(int* b, std::size_t s) : buffer_(b), size_(s) {}
+
+  using iterator = forward_sized_iterator<int*>;
+  using sentinel = sized_sentinel<iterator>;
+
+  constexpr iterator begin() const { return iterator(buffer_); }
+  constexpr sentinel end() const { return sentinel(iterator(buffer_ + size_)); }
+};
+static_assert(std::ranges::forward_range<ForwardSizedNonCommon>);
+static_assert(std::ranges::sized_range<ForwardSizedNonCommon>);
+static_assert(!std::ranges::common_range<ForwardSizedNonCommon>);
+static_assert(!std::ranges::random_access_range<ForwardSizedNonCommon>);
+// static_assert(simple_view<ForwardSizedNonCommon>);
 
 template <class... Args>
 struct Invocable {
@@ -32,51 +55,70 @@ struct Invocable {
 };
 
 void test() {
-  View range;
-  std::ranges::zip_transform_view ztv{[](int x) { return x; }, range};
+  CommonView commonRange;
+  std::ranges::zip_transform_view commonZtv{[](int x) { return x; }, commonRange};
+  static_assert(std::same_as<decltype(commonZtv.end()), decltype(commonZtv.begin())>);
 
   // [range.zip_transform.view]
 
   // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
-  ztv.begin();
+  commonZtv.begin();
   // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
-  std::as_const(ztv).begin();
+  std::as_const(commonZtv).begin();
   // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
-  ztv.end();
+  commonZtv.end();
   // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
-  std::as_const(ztv).end();
+  std::as_const(commonZtv).end();
   // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
-  ztv.size();
+  commonZtv.size();
   // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
-  std::as_const(ztv).size();
+  std::as_const(commonZtv).size();
 
   // [range.zip_transform.iterator]
+  {
+    auto it  = commonZtv.begin();
+    auto cIt = std::as_const(commonZtv).begin();
 
-  auto it  = ztv.begin();
-  auto cIt = std::as_const(ztv).begin();
+    // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
+    *cIt;
+    // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
+    cIt[0];
 
-  // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
-  *cIt;
-  // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
-  cIt[0];
-
-  // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
-  it + 1;
-  // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
-  1 + it;
-  // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
-  it - 1;
-  // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
-  it - it;
+    // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
+    it + 1;
+    // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
+    1 + it;
+    // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
+    it - 1;
+    // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
+    it - it;
+  }
 
   // [range.zip_transform.sentinel]
+  {
+    auto MakeTuple = [](auto&&... args) { return std::tuple(std::forward<decltype(args)>(args)...); };
+    int arr[]      = {1, 2, 3};
+    std::ranges::zip_transform_view nonCommonZtv{MakeTuple, std::views::iota(0, 3), ForwardSizedNonCommon(arr)};
+    static_assert(!std::ranges::common_range<decltype(nonCommonZtv)>);
+    static_assert(!std::same_as<decltype(nonCommonZtv.end()), decltype(nonCommonZtv.begin())>);
+    auto it  = nonCommonZtv.begin();
+    auto cIt = std::as_const(nonCommonZtv).begin();
+    auto st  = nonCommonZtv.end();
+    auto cSt = std::as_const(nonCommonZtv).end();
 
-  auto st = ztv.end();
-
-  // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
-  it - st;
-  // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
-  st - it;
+    // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
+    it - st;
+    // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
+    st - it;
+    // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
+    it - cSt;
+    // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
+    cSt - it;
+    // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
+    cIt - st;
+    // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
+    st - cIt;
+  }
 
   // [range.zip_transform.overview]
 
@@ -84,5 +126,5 @@ void test() {
   std::views::zip_transform(Invocable<>{});
 
   // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
-  std::views::zip_transform([](int x) { return x; }, range);
+  std::views::zip_transform([](int x) { return x; }, commonRange);
 }
