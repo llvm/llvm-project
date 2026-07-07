d21cae5d58949d754953080e5a71194ef8d2587f
Nikolas Klauser <nikolasklauser@berlin.de>
2026-07-07 14:27:43 +0200

    [libc++] Overhaul how we check ABI stability

---
+++ libcxx/test/libcxx/abilists/arm64-apple-darwin/abilist.sh.cpp
@@ -0,0 +1,18 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// FIXME: Encode whether the library was built with exceptions and check the abilist for that case as well
+
+// REQUIRES: target=arm64-apple-darwin{{.*}}
+
+// Check that the list of symbols exported from the dylib doesn't change unexpectedly
+
+// FILE_DEPENDENCIES: expected.abilist
+
+// RUN: nm --defined-only --extern-only %{lib-dir}/libc++.so.1.0 | cut -c 18- > %t.actual.abilist
+// RUN: diff expected.abilist %t.actual.abilist
