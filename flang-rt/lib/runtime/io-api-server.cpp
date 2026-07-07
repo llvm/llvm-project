34c8105a33883f68ed43f44f68143e7bc8394216
David Truby <david.truby@arm.com>
2026-07-07 16:02:03 +0100

    Fix formatting

--- flang-rt/lib/runtime/io-api-server.cpp
+++ flang-rt/lib/runtime/io-api-server.cpp
@@ -299,8 +299,9 @@ using RPCCallbackTy = std::uint32_t (*)(void *, std::uint32_t);
 
 // COFF workaround for the lack of weak symbols, copied from the sanitizers.
 #if defined(_MSC_VER)
-// Stub function to be replaced with __tgt_register_rpc_callback if present.
-extern "C" void register_rpc_callback_stub(RPCCallbackTy) { }
+extern "C" void register_rpc_callback_stub(RPCCallbackTy) {
+  // Stub function to be replaced with __tgt_register_rpc_callback if present.
+}
 #pragma comment(linker, \
     "/alternatename:__tgt_register_rpc_callback=" \
     "register_rpc_callback_stub")
