c181d014aa9a96cd9e13d9cc9786f13eed40aff3
Hugo <hugo.trachino@huawei.com>
2026-07-07 23:07:32 +0800

    fixup! [MLIR][Linalg] Fix FlattenElementwiseOp on broadcasted linalgs

--- mlir/lib/Dialect/Linalg/TransformOps/LinalgTransformOps.cpp
+++ mlir/lib/Dialect/Linalg/TransformOps/LinalgTransformOps.cpp
@@ -4291,16 +4291,16 @@ DiagnosedSilenceableFailure transform::FlattenElementwiseLinalgOp::applyToOne(
     return mlir::emitSilenceableFailure(target->getLoc())
            << "only elementwise flattening is supported";
 
-  // If rank <= 1, do nothing
-  if (target.getNumLoops() <= 1) {
-    results.push_back(target);
-    return DiagnosedSilenceableFailure::success();
-  }
-
-  // Bail out on linalgs with broadcasting semantics
   if (!llvm::all_of(target.getIndexingMapsArray(), [](AffineMap m) {
         return m.isProjectedPermutation(/*allowZeroInResults=*/false);
       })) {
+    results.push_back(target);
+    return mlir::emitSilenceableFailure(target->getLoc())
+           << "operators with broadcasting semantics are not supported";
+  }
+
+  // If rank <= 1, do nothing
+  if (target.getNumLoops() <= 1) {
     results.push_back(target);
     return DiagnosedSilenceableFailure::success();
   }
