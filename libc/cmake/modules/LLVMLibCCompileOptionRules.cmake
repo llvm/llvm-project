b9d0d1a747838136eb6ef3d2d9d68a20aaa23146
prabhukr <prabhukr@google.com>
2026-07-07 17:16:15 +0000

    [libc] Baremetal target builds should be static
    We are noticing that the baremetal target builds produce GOT relocations
    since we set `-fpie`. Excluding baremetal builds from setting this flag
    as they are typically just static builds.

--- libc/cmake/modules/LLVMLibCCompileOptionRules.cmake
+++ libc/cmake/modules/LLVMLibCCompileOptionRules.cmake
@@ -223,7 +223,9 @@ function(_get_common_compile_options output_var flags)
   set(compile_options ${LIBC_COMPILE_OPTIONS_DEFAULT} ${compile_flags} ${config_flags} ${arch_flags})
 
   if(LLVM_LIBC_COMPILER_IS_GCC_COMPATIBLE)
-    list(APPEND compile_options "-fpie")
+    if(NOT LIBC_TARGET_OS_IS_BAREMETAL)
+      list(APPEND compile_options "-fpie")
+    endif()
 
     if(LLVM_LIBC_FULL_BUILD)
       # Only add -ffreestanding flag in non-GPU full build mode.
