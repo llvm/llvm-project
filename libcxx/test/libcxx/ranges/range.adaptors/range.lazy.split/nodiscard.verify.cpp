f81b6437ddbec10579a87c58dca1286b63f10763
Lucas Mellone <github.snugness349@passinbox.com>
2026-07-08 16:32:21 +0200

    fix: __outer_iterator to __inner_iterator nodiscard testing

--- libcxx/test/libcxx/ranges/range.adaptors/range.lazy.split/nodiscard.verify.cpp
+++ libcxx/test/libcxx/ranges/range.adaptors/range.lazy.split/nodiscard.verify.cpp
@@ -37,18 +37,18 @@ void test() {
   // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
   std::as_const(v).end();
 
-  // [range.lazy.split.iterator]
+  // [range.lazy.split.outer-iterator]
 
-  auto c_it = std::as_const(v).begin();
-
-  // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
-  c_it.base();
+  auto c_it = as_const(v).begin();
 
   // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
-  *c_it;
+  (*c_it).begin().base();
 
   // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
-  iter_move(c_it);
+  (*c_it);
+
+  // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  iter_move((*c_it).begin());
 
   // [range.lazy.split.overview]
 
