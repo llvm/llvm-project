e2b903a11cfec1406594f707aa3043bb95b87ad3
benedekaibas <kaibas01@allegheny.edu>
2026-07-09 12:59:16 +0200

    [analyzer] Implemented VisitNode for BugReporterVisitor.

--- clang/test/Analysis/dangling-ptr-deref.cpp
+++ clang/test/Analysis/dangling-ptr-deref.cpp
@@ -1,7 +1,7 @@
 // RUN: %clang_analyze_cc1 -analyzer-checker=core,alpha.cplusplus.ReportDanglingPtrDeref \
-// RUN:   -analyzer-config cfg-lifetime=true -verify %s
+// RUN:   -analyzer-config cfg-lifetime=true -analyzer-output=text -verify %s
 // RUN: %clang_analyze_cc1 -analyzer-checker=core,alpha.cplusplus.ReportDanglingPtrDeref \
-// RUN:   -analyzer-config c++-container-inlining=false -analyzer-config cfg-lifetime=true -verify %s
+// RUN:   -analyzer-config c++-container-inlining=false -analyzer-config cfg-lifetime=true -analyzer-output=text -verify %s
 
 void test_case_one() {
   int *ptr = nullptr;
@@ -9,7 +9,9 @@ void test_case_one() {
     int num = 5;
     ptr = &num;
   }
-  *ptr = 6; // expected-warning {{Use of 'num' after its lifetime ended}}
+  *ptr = 6;
+  // expected-warning@-1 {{Use of 'num' after its lifetime ended}}
+  // expected-note@-2 {{Use of 'num' after its lifetime ended}}
 }
 
 void test_case_two() {
@@ -21,8 +23,12 @@ void test_case_two() {
     ptr_one = &n;
     ptr_two = &m;
   }
-  *ptr_one = 6; // expected-warning {{Use of 'n' after its lifetime ended}}
-  *ptr_two = 7; // expected-warning {{Use of 'm' after its lifetime ended}}
+  *ptr_one = 6;
+  *ptr_two = 7; 
+  // expected-warning@-2 {{Use of 'n' after its lifetime ended}}
+  // expected-warning@-2 {{Use of 'm' after its lifetime ended}}
+  // expected-note@-4 {{Use of 'n' after its lifetime ended}}
+  // expected-note@-4 {{Use of 'm' after its lifetime ended}}
 }
 
 void test_case_three() {
@@ -39,7 +45,9 @@ void test_case_four() {
     int num = 5;
     ptr = &num;
   }
-  int i = *ptr; // expected-warning {{Use of 'num' after its lifetime ended}}
+  int i = *ptr;
+  // expected-warning@-1 {{Use of 'num' after its lifetime ended}}
+  // expected-note@-2 {{Use of 'num' after its lifetime ended}}
   i += i;
 }
 
