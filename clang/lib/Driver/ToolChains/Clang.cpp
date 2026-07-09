95fa462ca468d7de06d251c971df4578170610e7
sujianIBM <Jian.Su@ibm.com>
2026-07-09 19:49:25 +0000

    Use TargetSpecific instead of manually emitting error.

--- clang/lib/Driver/ToolChains/Clang.cpp
+++ clang/lib/Driver/ToolChains/Clang.cpp
@@ -2169,14 +2169,9 @@ void Clang::AddSystemZTargetArgs(const ArgList &Args,
     CmdArgs.push_back("soft");
   }
 
-  if (Arg *A = Args.getLastArg(options::OPT_mzos_ppa1_name,
-                               options::OPT_mno_zos_ppa1_name)) {
-    if(!Triple.isOSzOS())
-      D.Diag(diag::err_drv_unsupported_opt_for_target)
-          << A->getSpelling() << Triple.str();
-    else
-      CmdArgs.push_back(A->getValue());
-  }
+  if(Triple.isOSzOS())
+    Args.AddLastArg(CmdArgs, options::OPT_mzos_ppa1_name,
+                    options::OPT_mno_zos_ppa1_name);
 }
 
 void Clang::AddX86TargetArgs(const ArgList &Args,
