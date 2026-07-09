f1d17d1a0052a6d7a3e923ea90d3f4817871161b
michaelselehov <michael.selehov@amd.com>
2026-07-09 18:12:59 +0200

    Update llvm/lib/Target/AMDGPU/AMDGPUTargetMachine.cpp
    Co-authored-by: Shilei Tian <i@tianshilei.me>

--- llvm/lib/Target/AMDGPU/AMDGPUTargetMachine.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPUTargetMachine.cpp
@@ -1113,9 +1113,10 @@ void AMDGPUTargetMachine::registerPassBuilderCallbacks(PassBuilder &PB) {
       [this](ModulePassManager &PM, OptimizationLevel Level) {
         // Clean up redundant memory round-trips that the full-LTO pipeline,
         // unlike the non-LTO/ThinLTO ones, otherwise leaves for codegen.
-        if (Level != OptimizationLevel::O0)
+        if (Level != OptimizationLevel::O0) {
           PM.addPass(createModuleToFunctionPassAdaptor(
               EarlyCSEPass(/*UseMemorySSA=*/true)));
+          }
 
         // When we are using -fgpu-rdc, we can only run accelerator code
         // selection after linking to prevent, otherwise we end up removing
