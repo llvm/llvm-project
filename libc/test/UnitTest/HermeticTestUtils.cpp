59a24f310755d834c98611a5c7722972ee64ee8b
yfzhu <yfzhu@google.com>
2026-07-09 16:44:33 -0700

    [libc][hermetic] use mutex-protected freelist heap  for hermetic test
    TAG=agy
    CONV=c9abb6ae-50f2-4602-87aa-fb0c972e80f0

--- libc/test/UnitTest/HermeticTestUtils.cpp
+++ libc/test/UnitTest/HermeticTestUtils.cpp
@@ -35,22 +35,6 @@ int atexit(void (*func)(void));
 
 } // namespace LIBC_NAMESPACE_DECL
 
-constexpr uint64_t ALIGNMENT = alignof(uintptr_t);
-
-namespace {
-
-// Integration tests cannot use the SCUDO standalone allocator as SCUDO pulls
-// various other parts of the libc. Since SCUDO development does not use
-// LLVM libc build rules, it is very hard to keep track or pull all that SCUDO
-// requires. Hence, as a work around for this problem, we use a simple allocator
-// which just hands out continuous blocks from a statically allocated chunk of
-// memory.
-static constexpr uint64_t MEMORY_SIZE = 65336;
-alignas(ALIGNMENT) static uint8_t memory[MEMORY_SIZE];
-static uint8_t *ptr = memory;
-
-} // anonymous namespace
-
 extern "C" {
 
 // Hermetic tests rely on the following memory functions. This is because the
@@ -78,31 +62,9 @@ void *memset(void *ptr, int value, size_t count) {
 // This is needed if the test was compiled with '-fno-use-cxa-atexit'.
 int atexit(void (*func)(void)) { return LIBC_NAMESPACE::atexit(func); }
 
-void *malloc(size_t s) {
-  // Keep the bump pointer aligned on an eight byte boundary.
-  s = ((s + ALIGNMENT - 1) / ALIGNMENT) * ALIGNMENT;
-  void *mem = ptr;
-  ptr += s;
-  return static_cast<uint64_t>(ptr - memory) >= MEMORY_SIZE ? nullptr : mem;
-}
-
-void free(void *) {}
-
-void *realloc(void *mem, size_t s) {
-  if (mem == nullptr)
-    return malloc(s);
-  uint8_t *newmem = reinterpret_cast<uint8_t *>(malloc(s));
-  if (newmem == nullptr)
-    return nullptr;
-  uint8_t *oldmem = reinterpret_cast<uint8_t *>(mem);
-  // We use a simple for loop to copy the data over.
-  // If |s| is less the previous alloc size, the copy works as expected.
-  // If |s| is greater than the previous alloc size, then garbage is copied
-  // over to the additional part in the new memory block.
-  for (size_t i = 0; i < s; ++i)
-    newmem[i] = oldmem[i];
-  return newmem;
-}
+void *malloc(size_t s) noexcept;
+void free(void *ptr) noexcept;
+void *realloc(void *mem, size_t s) noexcept;
 
 // The unit test framework uses pure virtual functions. Since hermetic tests
 // cannot depend C++ runtime libraries, implement dummy functions to support
