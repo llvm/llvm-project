3b3e42522257fb11bab1dd4c92d757fa91a83231
benedekaibas <kaibas01@allegheny.edu>
2026-07-09 13:11:43 +0200

    [analyzer] Updated the modeling checker.

--- clang/include/clang/StaticAnalyzer/Checkers/LifetimeModeling.h
+++ clang/include/clang/StaticAnalyzer/Checkers/LifetimeModeling.h
@@ -5,15 +5,16 @@
 #include "clang/StaticAnalyzer/Core/PathSensitive/SVals.h"
 #include <vector>
 
-namespace clang {
-namespace ento {
-namespace lifetimemodeling {
-
+namespace clang::ento::lifetime_modeling {
+// FIXME: Once #205951 lands this function might needs to be removed.
 std::vector<const MemRegion *> getLifetimeSourceSet(ProgramStateRef, SVal);
+
+/// Returns true if the SVal key is present in the map.
+bool isBoundToLifetimeSourceSet(ProgramStateRef State, SVal Val);
+
+// FIXME: Once PR #205951 lands this function might needs to be removed.
 bool isDeallocated(ProgramStateRef, const MemRegion *);
 
-} // namespace lifetimemodeling
-} // namespace ento
-} // namespace clang
+} // namespace clang::ento::lifetime_modeling
 
 #endif // LLVM_CLANG_INCLUDE_STATICANALYZER_CHECKERS_LIFETIMEMODELING_H
