dfaa58f0e3e210483aa0b032213bb3124b8793c4
yelidris <younes.elidrissiyazami@amd.com>
2026-07-09 00:13:18 -0400

    [AMDGPU] Init pointers, widen mask complements(NFC)
    - Initialize local pointers before use, silencing uninitialized local
      pointer warnings.
    - Compute mask complements at 64 bits to avoid zero-extending the result from a type to a
      type of greater size and dropping high bits.

--- llvm/lib/Target/AMDGPU/SIISelLowering.cpp
+++ llvm/lib/Target/AMDGPU/SIISelLowering.cpp
@@ -2682,7 +2682,7 @@ SDValue SITargetLowering::getPreloadedValue(
     SelectionDAG &DAG, const SIMachineFunctionInfo &MFI, EVT VT,
     AMDGPUFunctionArgInfo::PreloadedValue PVID) const {
   const ArgDescriptor *Reg = nullptr;
-  const TargetRegisterClass *RC;
+  const TargetRegisterClass *RC = nullptr;
   LLT Ty;
 
   CallingConv::ID CC = DAG.getMachineFunction().getFunction().getCallingConv();
