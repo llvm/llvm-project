39032404a221254a8ed826d5f5d666cad9d7fd6b
Sindhu Chittireddy <sindhu.chittireddy@intel.com>
2026-07-09 23:03:28 -0700

    Fix formatting

--- clang/lib/Basic/Targets/SPIR.h
+++ clang/lib/Basic/Targets/SPIR.h
@@ -215,7 +215,7 @@ public:
 
   CallingConvCheckResult checkCallingConvention(CallingConv CC) const override {
     return (CC == CC_SpirFunction || CC == CC_DeviceKernel) ? CCCR_OK
-                                                           : CCCR_Warning;
+                                                            : CCCR_Warning;
   }
 
   CallingConv getDefaultCallingConv() const override {
