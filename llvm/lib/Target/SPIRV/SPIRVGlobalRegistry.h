b76210f302b342504e7a1f0bf45163bea39ba3a0
Arseniy Obolenskiy <arseniy.obolenskiy@amd.com>
2026-07-10 13:39:10 +0200

    [NFC][SPIR-V] Add constrainSelectedInstRegOperands helper to dedup repeating pattern in global registry

--- llvm/lib/Target/SPIRV/SPIRVGlobalRegistry.h
+++ llvm/lib/Target/SPIRV/SPIRVGlobalRegistry.h
@@ -515,6 +515,8 @@ private:
                                  MachineIRBuilder &MIRBuilder);
   bool hasBlockDecoration(SPIRVTypeInst Type) const;
 
+  void constrainSelectedInstRegOperands(MachineInstrBuilder &MIB) const;
+
   SPIRVTypeInst
   getOrCreateOpTypeImage(MachineIRBuilder &MIRBuilder,
                          SPIRVTypeInst SampledType, SPIRV::Dim::Dim Dim,
