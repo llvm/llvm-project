40636d42b4e28a898a60ae1acca3680ff9bc5d40
Valentin Clement <clementval@gmail.com>
2026-07-10 11:51:12 -0700

    [flang][cuda] Restrict implicit use to non-cuda subprogram

--- flang/test/Driver/bbc-implicit-use-module.f90
+++ flang/test/Driver/bbc-implicit-use-module.f90
@@ -5,6 +5,8 @@
 ! RUN: bbc -module %t/self -implicit-use-module implicit_mod %t/implicit_mod.f90 -o /dev/null
 ! RUN: bbc -module %t %t/implicit_mod.f90 -o /dev/null
 ! RUN: not bbc -emit-hlfir -I %t -implicit-use-module implicit_mod %t/use_in_module.f90 -o - 2>&1 | FileCheck %s --check-prefix=MODULE-SCOPE
+! RUN: not bbc -emit-hlfir -fcuda -I %t -implicit-use-module implicit_mod %t/use_in_cuda_device.cuf -o - 2>&1 | FileCheck %s --check-prefix=CUDA-DEVICE
+! RUN: not bbc -emit-hlfir -fcuda -I %t -implicit-use-module implicit_mod %t/use_in_cuda_global.cuf -o - 2>&1 | FileCheck %s --check-prefix=CUDA-GLOBAL
 ! RUN: bbc -emit-hlfir -fopenacc -I %t -implicit-use-module implicit_mod %t/use_implicit.f90 -o - | FileCheck %s
 
 !--- implicit_mod.f90
@@ -34,7 +36,23 @@ module use_in_module
   integer :: x = module_value
 end module
 
+!--- use_in_cuda_device.cuf
+attributes(device) subroutine use_in_cuda_device
+  implicit none
+  integer :: x
+  x = module_value
+end subroutine
+
+!--- use_in_cuda_global.cuf
+attributes(global) subroutine use_in_cuda_global
+  implicit none
+  integer :: x
+  x = module_value
+end subroutine
+
 ! MODULE-SCOPE: No explicit type declared for 'module_value'
+! CUDA-DEVICE: No explicit type declared for 'module_value'
+! CUDA-GLOBAL: No explicit type declared for 'module_value'
 
 ! CHECK-LABEL: func.func @_QPuse_implicit()
 ! CHECK-DAG: fir.address_of(@_QMimplicit_modEmodule_value) : !fir.ref<i32>
