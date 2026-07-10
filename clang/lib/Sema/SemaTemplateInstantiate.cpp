6365249629b7e2b8a90de3a15c788d6e94f14421
Younan Zhang <zyn7109@gmail.com>
2026-07-10 16:47:55 +0800

    [Clang] Fix a bug in parameter mapping substitution
    When building parameter mapping, both substitution and CheckTemplateArguments
    were marked as InstantiatingTemplate::ParameterMappingSubstitution, while
    CheckTemplateArguments could also substitute into default arguments eagerly
    so that some type constrains can be also transformed too early.
    
    It turned out that we don't have to enforce a constraint rebuild in
    SubstTypeConstraints, so this reverts that behavior.

--- clang/lib/Sema/SemaTemplateInstantiate.cpp
+++ clang/lib/Sema/SemaTemplateInstantiate.cpp
@@ -3075,7 +3075,7 @@ bool Sema::SubstTypeConstraint(
   const ASTTemplateArgumentListInfo *TemplArgInfo =
       TC->getTemplateArgsAsWritten();
 
-  if (!EvaluateConstraints && !inParameterMappingSubstitution()) {
+  if (!EvaluateConstraints) {
     UnsignedOrNone Index = TC->getArgPackSubstIndex();
     bool ContainsUnexpandedPack =
         TemplArgInfo &&
