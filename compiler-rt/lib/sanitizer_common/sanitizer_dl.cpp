86832fc2851cc5347b3b391a9c59ace8f8d660b5
Mike Kruskal <mkruskal@google.com>
2026-07-10 16:22:02 -0700

    Remove braces

--- compiler-rt/lib/sanitizer_common/sanitizer_dl.cpp
+++ compiler-rt/lib/sanitizer_common/sanitizer_dl.cpp
@@ -37,9 +37,8 @@ const char *DladdrSelfFName(void) {
 char* DladdrElfHeaderBase(void* ld, char* addr) {
 #if SANITIZER_GLIBC
   Dl_info info;
-  if (dladdr(ld, &info) && info.dli_fbase) {
+  if (dladdr(ld, &info) && info.dli_fbase)
     addr = (char *)info.dli_fbase;
-  }
 #endif  // SANITIZER_GLIBC
   return addr;
 }
