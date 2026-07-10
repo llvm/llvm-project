13ac6a819d77e1916cfac6513772b9c9cd2cfb2c
Lin Runze <lrzlin@163.com>
2026-07-10 15:30:22 +0800

    [LoongArch] Add scheduler definitions for LoongArch with schedule model for LA464/LA664

---
+++ llvm/test/tools/llvm-mca/LoongArch/Inputs/memory.s
@@ -0,0 +1,22 @@
+ld.b $a0, $a1, 0
+ld.d $a0, $a1, 0
+ldx.d $a0, $a1, $a2
+ldptr.d $a0, $a1, 0
+st.d $a0, $a1, 0
+stx.d $a0, $a1, $a2
+stptr.d $a0, $a1, 0
+fld.s $fa0, $a1, 0
+fld.d $fa0, $a1, 0
+fldx.d $fa0, $a1, $a2
+fst.d $fa0, $a1, 0
+fstx.d $fa0, $a1, $a2
+vld $vr0, $a1, 0
+vldx $vr0, $a1, $a2
+vldrepl.d $vr0, $a1, 0
+vst $vr0, $a1, 0
+vstx $vr0, $a1, $a2
+vstelm.d $vr0, $a1, 0, 0
+xvld $xr0, $a1, 0
+xvldx $xr0, $a1, $a2
+xvst $xr0, $a1, 0
+xvstelm.d $xr0, $a1, 0, 0
