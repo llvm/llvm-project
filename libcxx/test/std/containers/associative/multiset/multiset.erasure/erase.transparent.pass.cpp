5e0a3ccaa941348caf560f5392fafd0299d63db8
rsaddatimov <rafa.saddatimov@gmail.com>
2026-07-11 12:28:18 +0300

    make multiset tests constexpr

--- libcxx/test/std/containers/associative/multiset/multiset.erasure/erase.transparent.pass.cpp
+++ libcxx/test/std/containers/associative/multiset/multiset.erasure/erase.transparent.pass.cpp
@@ -13,15 +13,25 @@
 // class multiset
 
 //    template<class K>
-//        size_type erase(K&& k) const;        // C++23
+//        size_type erase(K&& k) const;        // C++23, constexpr since C++26
 
 #include <set>
 #include "test_transparent_associative.h"
 
-int main(int, char**) {
+TEST_CONSTEXPR_CXX26 bool test() {
   test_transparent_erase<std::multiset<StoredType<int>, transparent_comparator_base>>({1, 2, 4, 5});
 
   test_transparent_erase<std::multiset<StoredType<int>, transparent_comparator_final>>({1, 2, 4, 5});
 
   test_non_transparent_erase<std::multiset<StoredType<int>, std::less<StoredType<int>>>>({1, 2});
+
+  return true;
+}
+
+int main(int, char**) {
+  test();
+#if TEST_STD_VER >= 26
+  static_assert(test());
+#endif
+  return 0;
 }
