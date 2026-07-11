25451f378cb8dbdc8543b477e047a46f85c907c4
kbrav <kbravjr@gmail.com>
2026-07-11 18:21:13 -0400

    [TableGen] Check for EOF in prepSkipRegion to avoid preprocessor crash.

--- llvm/lib/TableGen/TGLexer.cpp
+++ llvm/lib/TableGen/TGLexer.cpp
@@ -913,8 +913,9 @@ bool TGLexer::prepSkipRegion(bool MustNeverBeFalse) {
 
   do {
     // Skip all symbols to the line end.
-    while (*CurPtr != '\n')
+    while (CurPtr != CurBuf.end() && *CurPtr != '\n') {
       ++CurPtr;
+    }
 
     // Find the first non-whitespace symbol in the next line(s).
     if (!prepSkipLineBegin())
