de4502a3bac34eedba2d5687e67bb6be577e51e7
Michael Kazakov <mike.kazakov@gmail.com>
2026-07-11 21:19:44 +0100

    __desugars_to_v<__totally_ordered_less_tag, __less<>,...> strips cv-ref from _Tp

---
+++ libcxx/test/libcxx/algorithms/comp.pass.cpp
@@ -0,0 +1,27 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// REQUIRES: std-at-least-c++17
+
+// <__algorithm/comp.h>
+
+#include <__algorithm/comp.h>
+
+// check that __less<> desugars to __totally_ordered_less_tag for integral types regardless of their cv-ref
+static_assert(std::__desugars_to_v<std::__totally_ordered_less_tag, std::__less<>, int, int>);
+static_assert(std::__desugars_to_v<std::__totally_ordered_less_tag, std::__less<>, int&, int&>);
+static_assert(std::__desugars_to_v<std::__totally_ordered_less_tag, std::__less<>, const int, const int>);
+static_assert(std::__desugars_to_v<std::__totally_ordered_less_tag, std::__less<>, const int&, const int&>);
+static_assert(std::__desugars_to_v<std::__totally_ordered_less_tag, std::__less<>, volatile int&, volatile int&>);
+static_assert(
+    std::__desugars_to_v<std::__totally_ordered_less_tag, std::__less<>, volatile const int&, volatile const int&>);
+static_assert(
+    !std::
+        __desugars_to_v<std::__totally_ordered_less_tag, std::__less<>, volatile const float&, volatile const float&>);
+
+int main(int, char**) { return 0; }
