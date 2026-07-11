0e15f4edd6d5eee2c6fa2547cb5e019421a04a4d
Jon Roelofs <jonathan_roelofs@apple.com>
2026-07-10 17:18:56 -0700

    even moar clang-format. sigh. revert me

--- libunwind/src/config.h
+++ libunwind/src/config.h
@@ -220,12 +220,12 @@ extern bool logDWARF(void);
       _LIBUNWIND_LOG(msg, __VA_ARGS__);                                        \
   } while (0)
 #define _LIBUNWIND_TRACING_UNWINDING logUnwinding()
-  #define _LIBUNWIND_TRACING_DWARF logDWARF()
-  #define _LIBUNWIND_TRACE_UNWINDING(msg, ...)                                 \
-    do {                                                                       \
-      if (logUnwinding())                                                      \
-        _LIBUNWIND_LOG(msg, __VA_ARGS__);                                      \
-    } while (0)
+#define _LIBUNWIND_TRACING_DWARF logDWARF()
+#define _LIBUNWIND_TRACE_UNWINDING(msg, ...)                                   \
+  do {                                                                         \
+    if (logUnwinding())                                                        \
+      _LIBUNWIND_LOG(msg, __VA_ARGS__);                                        \
+  } while (0)
   #define _LIBUNWIND_TRACE_DWARF(...)                                          \
     do {                                                                       \
       if (logDWARF())                                                          \
