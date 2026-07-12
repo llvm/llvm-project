515ea70537d3e4889c2023afddd80ba6a0b15770
Aiden Grossman <aidengrossman@google.com>
2026-07-12 05:54:30 +0000

    update
    Created using spr 1.3.7

--- llvm/lib/Target/WebAssembly/WebAssemblyCodeGenPassBuilder.cpp
+++ llvm/lib/Target/WebAssembly/WebAssemblyCodeGenPassBuilder.cpp
@@ -81,6 +81,7 @@ void WebAssemblyCodeGenPassBuilder::addIRPasses(PassManagerWrapper &PMW) const {
   // Fix function bitcasts, as WebAssembly requires caller and callee signatures
   // to match.
   // TODO(boomanaiden154): WebAssemblyFixFunctionBitcasts
+  addModulePass(WebAssemblyFixFunctionBitcastsPass(), PMW);
 
   // Optimize "returned" function attributes.
   if (getOptLevel() != CodeGenOptLevel::None) {
