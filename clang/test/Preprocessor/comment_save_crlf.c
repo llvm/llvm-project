0c5ca59f8cd955ef66b10e2696f6d3795372fbe7
Joe Kirchoff <joe.kirchoff@epicgames.com>
2026-07-13 15:49:42 +0100

    Sanity check prepared header has carriage returns

--- clang/test/Preprocessor/comment_save_crlf.c
+++ clang/test/Preprocessor/comment_save_crlf.c
@@ -1,4 +1,9 @@
 // RUN: %{to-crlf} %S/Inputs/comment_save_crlf.h > %t.h
+
+// Verify that prepared header contains \r
+// RUN: tr -d '\n' < %t.h | %{reveal-cr} | FileCheck %s --check-prefix=SANITY
+// SANITY: <CR>
+
 // RUN: %clang_cc1 -E -C -o %t.i %t.h
 // RUN: %{reveal-cr} %t.i | FileCheck %s
 // CHECK-NOT: <CR>
