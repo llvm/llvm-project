65466bb54ad25ab13ca98ad56fe876e2100996c7
Louis Dionne <ldionne.2@gmail.com>
2026-07-13 13:34:09 -0400

    Comments

--- libcxx/include/__algorithm/any_of.h
+++ libcxx/include/__algorithm/any_of.h
@@ -13,7 +13,6 @@
 #include <__algorithm/find_if.h>
 #include <__config>
 #include <__functional/identity.h>
-#include <__utility/forward.h>
 #include <__utility/move.h>
 
 #if !defined(_LIBCPP_HAS_NO_PRAGMA_SYSTEM_HEADER)
@@ -28,8 +27,7 @@ _LIBCPP_BEGIN_NAMESPACE_STD
 template <class _Iter, class _Sent, class _Proj, class _Pred>
 _LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX14 bool
 __any_of(_Iter __first, _Sent __last, _Pred& __pred, _Proj& __proj) {
-  auto __found = std::__find_if(std::move(__first), __last, __pred, __proj);
-  return __found != __last;
+  return std::__find_if(std::move(__first), __last, __pred, __proj) != __last;
 }
 
 template <class _InputIterator, class _Predicate>
