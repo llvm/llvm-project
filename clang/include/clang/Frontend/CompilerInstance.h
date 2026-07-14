1d62249e5e1d602b90487ef4d151456177fd8318
alisonzhang <alisonzhang@ibm.com>
2026-07-14 13:36:17 -0400

    Changes related to finput-charset support

--- clang/include/clang/Frontend/CompilerInstance.h
+++ clang/include/clang/Frontend/CompilerInstance.h
@@ -866,6 +866,7 @@ public:
   ///
   /// \return True on success.
   static bool InitializeSourceManager(const FrontendInputFile &Input,
+                                      llvm::StringRef InputEncodingName,
                                       DiagnosticsEngine &Diags,
                                       FileManager &FileMgr,
                                       SourceManager &SourceMgr);
