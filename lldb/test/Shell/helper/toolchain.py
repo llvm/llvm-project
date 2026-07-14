c1a7192f9a92663c7057afde13e76ed9083ea1eb
Aiden Grossman <aidengrossman@google.com>
2026-07-14 20:51:11 +0000

    formatting
    Created using spr 1.3.7

--- lldb/test/Shell/helper/toolchain.py
+++ lldb/test/Shell/helper/toolchain.py
@@ -49,10 +49,10 @@ def get_lldb_args(config, suffix=""):
 
 
 class ShTestLldb(ShTest):
-    def __init__(
-        self, extra_substitutions=[], preamble_commands=[]
-    ):
-        super().__init__(extra_substitutions=extra_substitutions, premable_commands=preamble_commands)
+    def __init__(self, extra_substitutions=[], preamble_commands=[]):
+        super().__init__(
+            extra_substitutions=extra_substitutions, premable_commands=preamble_commands
+        )
 
     def execute(self, test, litConfig):
         exec_path = test.getExecPath()
