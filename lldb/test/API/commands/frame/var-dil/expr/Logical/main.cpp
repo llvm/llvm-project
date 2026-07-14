0ac9d2a3e4a53144a6cdef7e8093a49e5e3fa2d8
Ilia Kuklin <ikuklin@accesssoftek.com>
2026-07-14 22:38:56 +0500

    [lldb] Add logical operators to DIL

---
+++ lldb/test/API/commands/frame/var-dil/expr/Logical/main.cpp
@@ -0,0 +1,20 @@
+#include <cstdint>
+#include <limits>
+
+void stop() {}
+
+int main(int argc, char **argv) {
+  bool trueVar = true;
+  bool falseVar = false;
+
+  const char *p_ptr = "str";
+  const char *p_nullptr = nullptr;
+
+  int array[2] = {1, 2};
+
+  struct S {
+  } s;
+
+  stop(); // Set a breakpoint here
+  return 0;
+}
