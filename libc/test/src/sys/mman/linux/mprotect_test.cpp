0c15bc547a75083deb87789ec2be7efd0a6140bd
Pavel Labath <pavel@labath.sk>
2026-07-14 11:17:34 +0000

    [libc] Remove #include <sys/whatever.h>
    and replace with granular includes of type/macro proxy headers -- where
    those headers exist. I'm leaving the creation of new proxy headers for
    another patch.
    
    Assisted by Gemini.

--- libc/test/src/sys/mman/linux/mprotect_test.cpp
+++ libc/test/src/sys/mman/linux/mprotect_test.cpp
@@ -13,9 +13,7 @@
 #include "test/UnitTest/ErrnoCheckingTest.h"
 #include "test/UnitTest/ErrnoSetterMatcher.h"
 #include "test/UnitTest/Test.h"
-
 #include <signal.h>
-#include <sys/mman.h>
 
 using LIBC_NAMESPACE::testing::ErrnoSetterMatcher::Fails;
 using LIBC_NAMESPACE::testing::ErrnoSetterMatcher::Succeeds;
