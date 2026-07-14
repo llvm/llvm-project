c04d26f8df69dd577e068b569b5e43e8378dbb75
erichkeane <ekeane@nvidia.com>
2026-07-14 13:52:08 -0700

    Add triple to test

--- clang/test/SemaTemplate/class-template-spec.cpp
+++ clang/test/SemaTemplate/class-template-spec.cpp
@@ -1,7 +1,7 @@
 // RUN: %clang_cc1 -fsyntax-only -verify %s
 // RUN: %clang_cc1 -fsyntax-only -verify -std=c++98 %s
 // RUN: %clang_cc1 -fsyntax-only -verify -std=c++11 %s
-// RUN: %clang_cc1 -fsyntax-only -verify=expected,cxx14 -std=c++14 %s
+// RUN: %clang_cc1 -fsyntax-only -triple x86_64-linux-gnu -verify=expected,cxx14 -std=c++14 %s
 // RUN: %clang_cc1 -fsyntax-only -verify -std=c++26 %s
 
 template<typename T, typename U = int> struct A; // expected-note {{template is declared here}} \
