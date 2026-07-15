c96a8cf5f47a2566bdfc3b3ab8f59e3430a1e8ee
Joe Kirchoff <joe.kirchoff@epicgames.com>
2026-07-15 11:57:35 +0100

    Validate the file to preprocess has \r\n before running clang

--- clang/test/Preprocessor/comment_save_crlf.c
+++ clang/test/Preprocessor/comment_save_crlf.c
@@ -1,3 +1,4 @@
 // RUN: %python -c "open(r'%t.h','wb').write(open(r'%S/Inputs/comment_save_crlf.h','rb').read().replace(b'\r\n',b'\n').replace(b'\n',b'\r\n'))"
+// RUN: %python -c "import sys; sys.exit(b'\r\n' not in open(r'%t.h','rb').read())"
 // RUN: %clang_cc1 -E -C -o %t.i %t.h
-// RUN: %python -c "import sys; assert b'\r\n' in open(r'%t.h','rb').read(); sys.exit(b'\r\r\n' in open(r'%t.i','rb').read())"
+// RUN: %python -c "import sys; sys.exit(b'\r\r\n' in open(r'%t.i','rb').read())"
