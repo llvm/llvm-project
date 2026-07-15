532239961dbb09fbb0555b2ebe956ae3ff270eb6
Joe Kirchoff <joe.kirchoff@epicgames.com>
2026-07-15 11:44:49 +0100

    update test to use python rather than sed

--- clang/test/Preprocessor/comment_save_crlf.c
+++ clang/test/Preprocessor/comment_save_crlf.c
@@ -1,9 +1,3 @@
-// RUN: %{to-crlf} %S/Inputs/comment_save_crlf.h > %t.h
-
-// Verify that prepared header contains \r
-// RUN: tr -d '\n' < %t.h | %{reveal-cr} | FileCheck %s --check-prefix=SANITY
-// SANITY: <CR>
-
+// RUN: %python -c "open(r'%t.h','wb').write(open(r'%S/Inputs/comment_save_crlf.h','rb').read().replace(b'\r\n',b'\n').replace(b'\n',b'\r\n'))"
 // RUN: %clang_cc1 -E -C -o %t.i %t.h
-// RUN: %{reveal-cr} %t.i | FileCheck %s
-// CHECK-NOT: <CR>
+// RUN: %python -c "import sys; assert b'\r\n' in open(r'%t.h','rb').read(); sys.exit(b'\r\r\n' in open(r'%t.i','rb').read())"
--- clang/test/lit.cfg.py
+++ clang/test/lit.cfg.py
@@ -113,12 +113,6 @@ config.substitutions.append(
     )
 )
 
-cr = "\r" if platform.system() == "Darwin" else r"\r"
-# Rewrite LF to CRLF
-config.substitutions.append(("%{to-crlf}", f"{sed_cmd} -e 's|{cr}||g' -e 's|$|{cr}|'"))
-# Filtering command for testing for carriage return
-config.substitutions.append(("%{reveal-cr}", f"{sed_cmd} -e 's|{cr}|<CR>|g'"))
-
 # For each occurrence of a clang tool name, replace it with the full path to
 # the build directory holding that tool.  We explicitly specify the directories
 # to search to ensure that we get the tools just built and not some random
