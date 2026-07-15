1759d17216f82de691906a23e19876be0262b231
Michael Kazakov <mike.kazakov@gmail.com>
2026-07-15 21:26:32 +0100

    Use assert() in the runway_sample() unit test

--- libcxx/test/support/test.support/runway_sample.pass.cpp
+++ libcxx/test/support/test.support/runway_sample.pass.cpp
@@ -20,80 +20,56 @@
 
 constexpr bool test() {
   {
-    bool fail = false;
-    runway_sample(0, [&](size_t /*i*/) { fail = true; });
-    if (fail) {
-      return false;
-    }
+    runway_sample(0, [&](std::size_t /*i*/) { assert(false); });
   }
   {
-    size_t expected[] = {0};
-    size_t n          = 0;
-    bool fail         = false;
-    runway_sample(0, [&](size_t i) {
-      if (expected[n++] != i)
-        fail = true;
+    std::size_t expected[] = {0};
+    std::size_t n          = 0;
+    runway_sample(1, [&](std::size_t i) {
+      assert(expected[n] == i);
+      n++;
     });
-    if (fail) {
-      return false;
-    }
   }
   {
-    size_t expected[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    size_t n          = 0;
-    bool fail         = false;
-    runway_sample(10, [&](size_t i) {
-      if (expected[n++] != i)
-        fail = true;
+    std::size_t expected[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    std::size_t n          = 0;
+    runway_sample(10, [&](std::size_t i) {
+      assert(expected[n] == i);
+      n++;
     });
-    if (fail) {
-      return false;
-    }
   }
   {
-    size_t expected[] = {
+    std::size_t expected[] = {
         0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29};
-    size_t n  = 0;
-    bool fail = false;
-    runway_sample(30, [&](size_t i) {
-      if (expected[n++] != i)
-        fail = true;
+    std::size_t n = 0;
+    runway_sample(30, [&](std::size_t i) {
+      assert(expected[n] == i);
+      n++;
     });
-    if (fail) {
-      return false;
-    }
   }
   {
-    size_t expected[] = {0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13, 14, 15, 16,
-                         50, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99};
-    size_t n          = 0;
-    bool fail         = false;
-    runway_sample(100, [&](size_t i) {
-      if (expected[n++] != i)
-        fail = true;
+    std::size_t expected[] = {0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13, 14, 15, 16,
+                              50, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99};
+    std::size_t n          = 0;
+    runway_sample(100, [&](std::size_t i) {
+      assert(expected[n] == i);
+      n++;
     });
-    if (fail) {
-      return false;
-    }
   }
   {
-    size_t expected[] = {0,   1,   2,   3,   4,   5,   6,   7,   8,   9,   10,  11,  12,  13,  14,  15,  16,  50,
-                         157, 493, 984, 985, 986, 987, 988, 989, 990, 991, 992, 993, 994, 995, 996, 997, 998, 999};
-    size_t n          = 0;
-    bool fail         = false;
-    runway_sample(1000, [&](size_t i) {
-      if (expected[n++] != i)
-        fail = true;
+    std::size_t expected[] = {0,   1,   2,   3,   4,   5,   6,   7,   8,   9,   10,  11,  12,  13,  14,  15,  16,  50,
+                              157, 493, 984, 985, 986, 987, 988, 989, 990, 991, 992, 993, 994, 995, 996, 997, 998, 999};
+    std::size_t n          = 0;
+    runway_sample(1000, [&](std::size_t i) {
+      assert(expected[n] == i);
+      n++;
     });
-    if (fail) {
-      return false;
-    }
   }
   return true;
 }
 
 int main() {
-  assert(test());
+  test();
   static_assert(test());
   return 0;
 }
