436498e9d4268754d5b3e1f2da18a887ad445bf6
Finn Plummer <mail@inbelic.dev>
2026-07-15 18:45:54 +0000

    implement the toMetadata function

--- llvm/lib/Frontend/HLSL/SemanticSignatures.cpp
+++ llvm/lib/Frontend/HLSL/SemanticSignatures.cpp
@@ -12,7 +12,9 @@
 //===----------------------------------------------------------------------===//
 
 #include "llvm/Frontend/HLSL/SemanticSignatures.h"
+#include "llvm/IR/Constants.h"
 #include "llvm/IR/Metadata.h"
+#include "llvm/IR/Type.h"
 
 using namespace llvm;
 using namespace llvm::hlsl;
@@ -23,5 +25,25 @@ SemanticSignatureElement::fromMetadata(const MDNode *Node) {
 }
 
 MDNode *SemanticSignatureElement::toMetadata(LLVMContext &Ctx) const {
-  return MDNode::get(Ctx, {});
+  Type *I32Ty = Type::getInt32Ty(Ctx);
+  Type *I8Ty = Type::getInt8Ty(Ctx);
+  auto GetI32 = [&](uint32_t Val) -> Metadata * {
+    return ConstantAsMetadata::get(ConstantInt::get(I32Ty, Val));
+  };
+  auto GetI8 = [&](uint8_t Val) -> Metadata * {
+    return ConstantAsMetadata::get(ConstantInt::get(I8Ty, Val));
+  };
+
+  SmallVector<Metadata *> IndexOps;
+  for (uint32_t Index : SemanticIndices)
+    IndexOps.push_back(GetI32(Index));
+
+  return MDNode::get(
+      Ctx, {GetI32(SigId), MDString::get(Ctx, SemanticName),
+            GetI32(static_cast<uint32_t>(CompType)),
+            GetI32(static_cast<uint32_t>(SemanticKind)),
+            MDNode::get(Ctx, IndexOps),
+            GetI32(static_cast<uint32_t>(InterpMode)), GetI32(Rows),
+            GetI8(Cols), GetI32(StartRow), GetI8(StartCol), GetI8(UsageMask),
+            GetI8(DynIndexMask), GetI32(GSStream)});
 }
