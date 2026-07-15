3e59893eb147ac92781b965914b1c2895a1141c3
Ilia Kuklin <ikuklin@accesssoftek.com>
2026-07-15 18:24:08 +0500

    [lldb] Add bitwise operators to DIL

--- lldb/test/API/commands/frame/var-dil/expr/Bitwise/TestFrameVarDILBitwise.py
+++ lldb/test/API/commands/frame/var-dil/expr/Bitwise/TestFrameVarDILBitwise.py
@@ -19,6 +19,20 @@ class TestFrameVarDILBitwise(TestBase):
 
         self.runCmd("settings set target.experimental.use-DIL true")
 
+        # Check unary negation
+        self.expect_var_path("~(-1)", value="0")
+        self.expect_var_path("~~0", value="0")
+        self.expect_var_path("~0", value="-1")
+        self.expect_var_path("~1", value="-2")
+        self.expect_var_path("~0LL", value="-1")
+        self.expect_var_path("~1LL", value="-2")
+        self.expect_var_path("~true", value="-2")
+        self.expect_var_path("~false", value="-1")
+        self.expect_var_path("~var_true", value="-2")
+        self.expect_var_path("~var_false", value="-1")
+        self.expect_var_path("~ull_max", value="0")
+        self.expect_var_path("~0b1011", value="-12")
+
         # Check bitwise shifts
         self.expect_var_path("(1 << 5)", value="32")
         self.expect_var_path("(32 >> 2)", value="8")
@@ -33,7 +47,41 @@ class TestFrameVarDILBitwise(TestBase):
         self.expect_var_path("2 >> enum_one", value="1")
         self.expect_var_path("i64 << 63", type="uint64_t")
 
+        # Check And, Xor, Or
+        self.expect_var_path("0b1011 & 0xFF", value="11")
+        self.expect_var_path("0b1011 & mask_ff", value="11")
+        self.expect_var_path("0b1011 & 0b0111", value="3")
+        self.expect_var_path("0b1011 | 0b0111", value="15")
+        self.expect_var_path("-0b1011 | 0xFF", value="-1")
+        self.expect_var_path("-0b1011 | 0xFFu", value="4294967295")
+        self.expect_var_path("0b1011 ^ 0b0111", value="12")
+
         # Check errors
+        self.expect(
+            "frame var -- '~1.0'",
+            error=True,
+            substrs=["invalid argument type 'double' to unary expression"],
+        )
+        self.expect(
+            "frame var -- '~s'",
+            error=True,
+            substrs=["invalid argument type 'S' to unary expression"],
+        )
+        self.expect(
+            "frame var -- 's & 1.0'",
+            error=True,
+            substrs=["invalid operands to binary expression ('S' and 'double')"],
+        )
+        self.expect(
+            "frame var -- '1 ^ s'",
+            error=True,
+            substrs=["invalid operands to binary expression ('int' and 'S')"],
+        )
+        self.expect(
+            "frame var -- '1 | 1.0'",
+            error=True,
+            substrs=["invalid operands to binary expression ('int' and 'double')"],
+        )
         self.expect(
             "frame var -- '1 << 1.0'",
             error=True,
@@ -54,3 +102,12 @@ class TestFrameVarDILBitwise(TestBase):
             error=True,
             substrs=["invalid shift amount"],
         )
+
+        # Check that bitwise & is allowed only in full mode
+        frame = thread.GetFrameAtIndex(0)
+        simple = frame.GetValueForVariablePath("i64 & 1", lldb.eDILModeSimple)
+        legacy = frame.GetValueForVariablePath("i64 & 1", lldb.eDILModeLegacy)
+        full = frame.GetValueForVariablePath("i64 & 1", lldb.eDILModeFull)
+        self.assertFailure(simple.GetError())
+        self.assertFailure(legacy.GetError())
+        self.assertSuccess(full.GetError())
