22c5a0d1052f8e9ffafc6494a568d6117176f7bf
Pedro Vicente <pedro.costa.vicente@tecnico.ulisboa.pt>
2026-07-16 06:15:33 +0100

    small formating fix

--- llvm/include/llvm/IR/IRBuilderFolder.h
+++ llvm/include/llvm/IR/IRBuilderFolder.h
@@ -85,11 +85,6 @@ public:
 
   virtual Value *FoldBitExtract(Type *Ty, Value *Src, Value *Offset) const = 0;
 
-  virtual Value *FoldBitInsert(Value *Base, Value *Val,
-                               Value *Offset) const = 0;
-
-  virtual Value *FoldBitExtract(Type *Ty, Value *Src, Value *Offset) const = 0;
-
   //===--------------------------------------------------------------------===//
   // Cast/Conversion Operators
   //===--------------------------------------------------------------------===//
