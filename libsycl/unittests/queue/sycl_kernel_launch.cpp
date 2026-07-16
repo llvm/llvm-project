e8008fb79c64a89ae010ba343510fc1ebe058376
Tikhomirova, Kseniya <kseniya.tikhomirova@intel.com>
2026-07-16 07:42:22 -0700

    fix conflict with main
    Signed-off-by: Tikhomirova, Kseniya <kseniya.tikhomirova@intel.com>

--- libsycl/unittests/queue/sycl_kernel_launch.cpp
+++ libsycl/unittests/queue/sycl_kernel_launch.cpp
@@ -76,7 +76,7 @@ static ol_kernel_launch_size_args_t captureKernelLaunchArgs(
     mock::MockWrapper &Mock,
     std::function<void(sycl::detail::MockQueue &)> SetParams) {
   // Keep registration alive across all parameterized runs.
-  static ScopedKernelRegistration Reg{"DimSwapTestKernel"};
+  static sycl::unittests::ScopedKernelRegistration Reg{"DimSwapTestKernel"};
   sycl::detail::MockQueue Q;
   ol_kernel_launch_size_args_t Captured{};
   EXPECT_CALL(Mock.get(), olLaunchKernel(_, _, _, _, _, 1, _, _))
