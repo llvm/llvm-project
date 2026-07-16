8bda29a12fef7d86f53144f6fa48cf8de3cd8d2f
mentlerd <mentlerd@gmail.com>
2026-07-16 18:25:08 +0200

    Update test with explicit VO targeting

--- lldb/test/API/python_api/sbvalue_set_type_synthetic_override/TestSBValueSetTypeSyntheticOverride.py
+++ lldb/test/API/python_api/sbvalue_set_type_synthetic_override/TestSBValueSetTypeSyntheticOverride.py
@@ -32,19 +32,35 @@ class TestCase(TestBase):
         self.checkOverride(bar, before=None)
 
     def checkOverride(self, value, before):
+        foo = lldb.SBTypeSynthetic.CreateWithClassName(f"foo_bar_synths.FooSynthetic")
         bar = lldb.SBTypeSynthetic.CreateWithClassName(f"foo_bar_synths.BarSynthetic")
 
-        impl_before = value.GetTypeSyntheticImplementation()
+        # Target the static (non synthetic) ValueObject
+        static = value.GetNonSyntheticValue()
 
+        impl_before = static.GetSyntheticValue().GetTypeSyntheticImplementation()
         if not before:
             self.assertIsNone(impl_before)
         else:
             self.assertIsNotNone(impl_before)
             self.assertEqual(type(impl_before).__name__, before)
 
-        value.SetTypeSynthetic(bar)
-        self.assertEqual(value.GetTypeSynthetic(), bar)
+        static.SetTypeSynthetic(bar)
+        self.assertEqual(static.GetTypeSynthetic(), bar)
 
-        impl_after = value.GetTypeSyntheticImplementation()
+        impl_after = static.GetSyntheticValue().GetTypeSyntheticImplementation()
+        self.assertIsNotNone(impl_after)
+        self.assertEqual(type(impl_after).__name__, "BarSynthetic")
+
+        # Target the ValueObjectSynthetic of the original ValueObject
+        synth = value.GetSyntheticValue()
+
+        synth.SetTypeSynthetic(foo)
+        self.assertEqual(synth.GetTypeSynthetic(), foo)
+
+        # Even though the synthetic child provider choice of 'synth' was changed
+        # that does not retroactively alter the frontend it was created with when
+        # its parent's synthetic child provider choice was overridden above.
+        impl_after = synth.GetSyntheticValue().GetTypeSyntheticImplementation()
         self.assertIsNotNone(impl_after)
         self.assertEqual(type(impl_after).__name__, "BarSynthetic")
