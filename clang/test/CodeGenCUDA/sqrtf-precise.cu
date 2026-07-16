020600ffb3829b5e62d46303c44ddb356dcdeae5
Robert Virany <robertvirany@gmail.com>
2026-07-16 10:42:23 -0600

    [CUDA] Make sqrtf builtin test self-contained

--- clang/test/CodeGenCUDA/sqrtf-precise.cu
+++ clang/test/CodeGenCUDA/sqrtf-precise.cu
@@ -2,26 +2,36 @@
 
 // RUN: %clang_cc1 -fcuda-is-device -triple nvptx64-nvidia-cuda \
 // RUN:   -target-cpu sm_75 -O2 -S -o - %s \
-// RUN:   -include __clang_cuda_runtime_wrapper.h \
-// RUN:   -internal-isystem %S/../../lib/Headers/cuda_wrappers \
-// RUN:   -internal-isystem %S/../Headers/Inputs/include \
-// RUN:   | FileCheck --check-prefix=RN %s
+// RUN:   | FileCheck --check-prefix=DEFAULT %s
 
 // RUN: %clang_cc1 -fcuda-is-device -triple nvptx64-nvidia-cuda \
 // RUN:   -target-cpu sm_75 -O2 -fapprox-func -S -o - %s \
-// RUN:   -include __clang_cuda_runtime_wrapper.h \
-// RUN:   -internal-isystem %S/../../lib/Headers/cuda_wrappers \
-// RUN:   -internal-isystem %S/../Headers/Inputs/include \
 // RUN:   | FileCheck --check-prefix=APPROX %s
 
-#include <math.h>
+// RUN: %clang_cc1 -fcuda-is-device -triple nvptx64-nvidia-cuda \
+// RUN:   -target-cpu sm_75 -O2 -fapprox-func \
+// RUN:   -mllvm -nvptx-prec-sqrtf32=1 -S -o - %s \
+// RUN:   | FileCheck --check-prefix=FORCE-PRECISE %s
 
-// RN-LABEL: .func{{.*}}_Z1ff
-// RN: sqrt.rn.f32
-// RN-NOT: sqrt.approx.f32
+// RUN: %clang_cc1 -fcuda-is-device -triple nvptx64-nvidia-cuda \
+// RUN:   -target-cpu sm_75 -O2 -mllvm -nvptx-prec-sqrtf32=0 -S -o - %s \
+// RUN:   | FileCheck --check-prefix=FORCE-APPROX %s
+
+// DEFAULT-LABEL: .func{{.*}}builtin_sqrtf
+// DEFAULT: sqrt.rn.f32
+// DEFAULT-NOT: sqrt.approx.f32
 //
-// APPROX-LABEL: .func{{.*}}_Z1ff
+// APPROX-LABEL: .func{{.*}}builtin_sqrtf
 // APPROX: sqrt.approx.f32
-__device__ float f(float x) {
-  return sqrtf(x);
+// APPROX-NOT: sqrt.rn.f32
+//
+// FORCE-PRECISE-LABEL: .func{{.*}}builtin_sqrtf
+// FORCE-PRECISE: sqrt.rn.f32
+// FORCE-PRECISE-NOT: sqrt.approx.f32
+//
+// FORCE-APPROX-LABEL: .func{{.*}}builtin_sqrtf
+// FORCE-APPROX: sqrt.approx.f32
+// FORCE-APPROX-NOT: sqrt.rn.f32
+extern "C" __attribute__((device)) float builtin_sqrtf(float x) {
+  return __builtin_sqrtf(x);
 }
