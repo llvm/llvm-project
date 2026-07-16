9f68463aa80e3283c9d0f24cac60878ff24adc1b
Alexey Samsonov <vonosmas@gmail.com>
2026-07-16 21:19:44 +0000

    Update a test

--- libc/test/src/sys/time/gettimeofday_test.cpp
+++ libc/test/src/sys/time/gettimeofday_test.cpp
@@ -13,10 +13,14 @@
 
 #include "hdr/types/struct_timeval.h"
 #include "src/sys/time/gettimeofday.h"
+#include "test/UnitTest/ErrnoCheckingTest.h"
 #include "test/UnitTest/Test.h"
 
-TEST(LlvmLibcGettimeofday, SmokeTest) {
+using LlvmLibcGettimeofdayTest = LIBC_NAMESPACE::testing::ErrnoCheckingTest;
+
+TEST_F(LlvmLibcGettimeofdayTest, SmokeTest) {
   timeval tv;
   int ret = LIBC_NAMESPACE::gettimeofday(&tv, nullptr);
   ASSERT_EQ(ret, 0);
+  ASSERT_ERRNO_SUCCESS();
 }
