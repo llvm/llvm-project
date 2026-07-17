74e12793910d3262c34c57942ed824684f6b221a
Abhina Sreeskantharajan <Abhina.Sreeskantharajan@ibm.com>
2026-07-17 10:03:17 -0400

    limit testcase to z/OS so we don't hit other platforms max limit

--- clang/test/CodeGen/SystemZ/systemz-module.flags.c
+++ clang/test/CodeGen/SystemZ/systemz-module.flags.c
@@ -1,3 +1,5 @@
 // RUN: %clang_cc1 -source-date-epoch 253402300799 -triple s390x-ibm-zos -emit-llvm -o - %s | FileCheck %s
 // CHECK: {{.*}}"zos_cu_language", !"C"}
 // CHECK: {{.*}}"zos_translation_time", i64 253402300799}
+
+// REQUIRES: system-zos
