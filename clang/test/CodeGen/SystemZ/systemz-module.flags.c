aa8054b28e2786a4ad59a8e04208627bbcd1f778
Abhina Sree <Abhina.Sreeskantharajan@ibm.com>
2026-07-17 11:09:06 -0400

    Apply suggestion from @abhina-sree

--- clang/test/CodeGen/SystemZ/systemz-module.flags.c
+++ clang/test/CodeGen/SystemZ/systemz-module.flags.c
@@ -1,4 +1,4 @@
-//REQUIRES: clang-target-64-bits
+// REQUIRES: clang-target-64-bits
 
 // RUN: %clang_cc1 -source-date-epoch 253402300799 -triple s390x-ibm-zos -emit-llvm -o - %s | FileCheck %s
 // CHECK: {{.*}}"zos_cu_language", !"C"}
