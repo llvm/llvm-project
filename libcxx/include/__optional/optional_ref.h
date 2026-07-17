fec480eae52447a0ceefd7fa5776dbd7f6c016e2
William Tran-Viet <wtranviet@proton.me>
2026-07-16 20:15:12 -0400

    move rref failure-specialization back to optional.h

--- libcxx/include/__optional/optional_ref.h
+++ libcxx/include/__optional/optional_ref.h
@@ -357,11 +357,6 @@ class optional<_Tp&> {
   static_assert(false, "instantiation of optional with a reference type is ill-formed");
 };
 
-template <class _Tp>
-class optional<_Tp&&> {
-  static_assert(false, "instantiation of optional with an rvalue reference type is ill-formed");
-};
-
 _LIBCPP_END_NAMESPACE_STD
 
 #endif // _LIBCPP_STD_VER >= 26
