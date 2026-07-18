afcb24566ee2b0cdf27cc2582bb771f2980f6fd8
Ron Lieberman <ron.lieberman@amd.com>
2026-07-17 20:56:54 -0500

    [opt] disable -amdgpu-unroll-runtime-default
    rocprofiler-apps transferbench build time from 15min to 160min+
    Need to triage why, buying time,

--- llvm/lib/Target/AMDGPU/AMDGPUTargetTransformInfo.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPUTargetTransformInfo.cpp
@@ -50,6 +50,11 @@ static cl::opt<unsigned> UnrollThresholdIf(
   cl::desc("Unroll threshold increment for AMDGPU for each if statement inside loop"),
   cl::init(200), cl::Hidden);
 
+static cl::opt<bool>
+    UnrollRuntimeDefault("amdgpu-unroll-runtime-default",
+                         cl::desc("Set default runtime unrolling for AMDGPU "),
+                         cl::init(false), cl::Hidden);
+
 static cl::opt<bool> UnrollRuntimeLocal(
   "amdgpu-unroll-runtime-local",
   cl::desc("Allow runtime unroll for AMDGPU if local memory used in a loop"),
@@ -127,9 +132,10 @@ void AMDGPUTTIImpl::getUnrollingPreferences(
   // We want to run unroll even for the loops which have been vectorized.
   UP.UnrollVectorizedLoop = true;
 
-  // Enable runtime unrolling for loops whose trip count is not known at
-  // compile time.
-  UP.Runtime = true;
+  // If selecte Enable runtime unrolling for loops whose trip count
+  // is not known at compile time.
+  if (UnrollRuntimeDefault.getNumOccurrences())
+    UP.Runtime = UnrollRuntimeDefault;
 
   // Maximum alloca size than can fit registers. Reserve 16 registers.
   const unsigned MaxAlloca = (256 - 16) * 4;
