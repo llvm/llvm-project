4bfbaaf8f4df1db9d2c43008d0d63705345a2fc6
Sindhu Chittireddy <sindhu.chittireddy@intel.com>
2026-07-20 11:32:33 -0700

    Restore width check to fix tests

--- clang/lib/Basic/Targets/SPIR.h
+++ clang/lib/Basic/Targets/SPIR.h
@@ -288,8 +288,7 @@ public:
            "Invalid architecture for 32-bit SPIR.");
     PointerWidth = PointerAlign = 32;
     const TargetInfo *HostTarget = getHostTarget();
-    if (!HostTarget) {
-      PointerWidth = PointerAlign = 32;
+    if (!HostTarget || HostTarget->getPointerWidth(LangAS::Default) != 32) {
       SizeType = TargetInfo::UnsignedInt;
       PtrDiffType = IntPtrType = TargetInfo::SignedInt;
     }
