3fda98141eead3b4208d415f501f931fc3bc6a91
A. Jiang <de34@live.cn>
2026-07-21 13:35:28 +0800

    clang-format, again

--- libcxx/include/__iterator/reverse_access.h
+++ libcxx/include/__iterator/reverse_access.h
@@ -23,8 +23,8 @@
 _LIBCPP_BEGIN_NAMESPACE_STD
 
 template <class _Tp, size_t _Np>
-[[__nodiscard__]] _LIBCPP_HIDE_FROM_ABI
-_LIBCPP_CONSTEXPR_SINCE_CXX17 reverse_iterator<_Tp*> rbegin(_Tp (&__array)[_Np]) noexcept {
+[[__nodiscard__]] _LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX17 reverse_iterator<_Tp*>
+rbegin(_Tp (&__array)[_Np]) noexcept {
   return reverse_iterator<_Tp*>(__array + _Np);
 }
 
