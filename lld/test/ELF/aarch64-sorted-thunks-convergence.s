5cc386a03c0befbf2b2f0f75b275371c3b77e9c8
Pranav Kant <prka@google.com>
2026-07-21 13:01:25 -0700

    Fangrui review 2

--- lld/test/ELF/aarch64-sorted-thunks-convergence.s
+++ lld/test/ELF/aarch64-sorted-thunks-convergence.s
@@ -1,8 +1,9 @@
 // REQUIRES: aarch64
-// RUN: llvm-mc -filetype=obj -triple=aarch64 %s -o %t
-// RUN: ld.lld %t -z sort-thunksection -o /dev/null 2>&1 | FileCheck %s --allow-empty --check-prefix=SORT
-// RUN: not ld.lld -z nosort-thunksection %t -o /dev/null 2>&1 | FileCheck --check-prefix=NOSORT %s
-// RUN: rm %t
+// RUN: rm -rf %t && split-file %s %t && cd %t
+// RUN: llvm-mc -filetype=obj -triple=aarch64 a.s -o %t1.o
+// RUN: ld.lld %t1.o -T a.lds -z sort-thunksection -o /dev/null 2>&1 | FileCheck %s --allow-empty --check-prefix=SORT
+// RUN: not ld.lld %t1.o -T a.lds -z nosort-thunksection -o /dev/null 2>&1 | FileCheck --check-prefix=NOSORT %s
+// RUN: rm %t1.o
 
 // SORT-NOT: error: address assignment did not converge
 // NOSORT: error: address assignment did not converge
@@ -11,8 +12,18 @@
 // but as out of range thunks get promoted to long thunks, they force previous
 // thunks to go out of range in the next pass. Without sorting the thunks,
 // linker wouldn't converge. We only test absolute thunks which are the default
-// thunks in the absence of --pic-thunks.
+// thunks in the absence of --pic-thunks. Absolute thunks occupies 16 bytes.
 
+//--- a.lds
+SECTIONS {
+  .text 0x10000 : { *(.text) }
+  /* Position .text.targets such that initially only fn29 (the 30th call) is
+     out of range */
+  . = . + 0x8000000 - (29 * 16 + 4);
+  .text.targets : { *(.text.targets) }
+}
+
+//--- a.s
 .set NCALLS, 30
 
 .macro gen_call i
@@ -31,20 +42,18 @@ fn\i:
 .global _start
 _start:
 
+// Generates fn0, ... , fn29
 .rept NCALLS
   gen_call \+
 .endr
 
 // <Linker will create thunk section here>
 
-.section .text.space, "ax", %progbits
-// Position fn30 just past the 128 MiB (0x8000000) limit from created short thunk
-// while all other functions are within 128MiB from their respective short thunks
-// Absolute thunks are 16 bytes long.
-.space 0x8000000 - (NCALLS - 1) * 16
-
+// Linker script positions .text.targets such that initially only fn29 is out
+// of range.
 .section .text.targets, "ax", %progbits
 
+// Generates fn0, ... , fn29
 .rept NCALLS
   gen_target \+
 .endr
