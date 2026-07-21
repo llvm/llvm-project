a5f6b3cde11ec8ee5c1ae53b7629f91b784ceede
NimishMishra <neelam.nimish@gmail.com>
2026-07-21 19:19:51 +0530

    fix test

--- flang/test/Driver/finit-local-zero.f90
+++ flang/test/Driver/finit-local-zero.f90
@@ -1,8 +1,7 @@
-! Check that the driver passes through -finit-global-zero:
+! Check that the driver passes through -finit-local-zero:
 ! RUN: %flang -### -S -finit-local-zero %s -o - 2>&1 | FileCheck %s
 
 ! Check that the compiler accepts -finit-local-zero:
-! RUN: %flang_fc1 -emit-hlfir -finit-local-zero %s -o -
-
+! RUN: %flang_fc1 -emit-hlfir -finit-local-zero %s -o /dev/null
 
 ! CHECK: "-fc1"{{.*}}"-finit-local-zero"
