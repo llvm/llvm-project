261258bb3138cb878648464a08ae88a484d1892c
Josh Rodriguez <josh.rodriguez@arm.com>
2026-07-21 14:33:41 +0000

    [AArch64][GlobalISel] Use isKnownMulitpleOf
    This makes intention of if statement more explicit.

--- llvm/lib/CodeGen/GlobalISel/LegalizerHelper.cpp
+++ llvm/lib/CodeGen/GlobalISel/LegalizerHelper.cpp
@@ -5834,7 +5834,7 @@ LegalizerHelper::fewerElementsVector(MachineInstr &MI, unsigned TypeIdx,
       return UnableToLegalize;
 
     // If DstTy's size is not a multiple of NarrowTy's, then cannot legalize
-    if (DstTy.getNumElements() % NarrowTy.getNumElements() != 0)
+    if (!DstTy.getElementCount().isKnownMultipleOf(NarrowTy.getElementCount()))
       return UnableToLegalize;
 
     auto Unmerge = MIRBuilder.buildUnmerge(NarrowTy, SrcReg);
