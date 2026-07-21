12d2e442b71ebe99a367b326f359d5934d76dfa4
Pavel Labath <pavel@labath.sk>
2026-07-21 06:34:33 +0000

    [ilbc] Revert recent changes to the hermetic tests
    PR #209999 (9e2e9b33e14808541c0470b92ade34bee6608bf6) "broke" the GPU
    build by causing many/most tests to not run.
    
    The follow-up in #210715 (4bd1a447e136e60ea441370ac5d7b898aa4efab0)
    broke it for real
    (https://lab.llvm.org/buildbot/#/builders/10/builds/32254) by causing
    too many tests to run.
    
    I think we just need to disable the extra tests, but I want to start out
    by reverting both patches so that it's easier to compare the
    before/after state in the next attempt.

--- libc/cmake/modules/LLVMLibCTestRules.cmake
+++ libc/cmake/modules/LLVMLibCTestRules.cmake
@@ -782,23 +782,6 @@ function(add_libc_hermetic test_name)
       libc.src.strings.bzero
   )
 
-  # Syscalls used by death tests. See also libc/test/UnitTest/CMakeLists.txt.
-  if(${LIBC_TARGET_OS} STREQUAL "linux" OR ${LIBC_TARGET_OS} STREQUAL "darwin")
-    list(APPEND fq_deps_list
-        libc.src.poll.poll
-        libc.src.signal.kill
-        libc.src.stdio.fflush
-        libc.src.stdio.stderr
-        libc.src.stdio.stdout
-        libc.src.stdlib.exit
-        libc.src.string.strsignal
-        libc.src.sys.wait.waitpid
-        libc.src.unistd.close
-        libc.src.unistd.fork
-        libc.src.unistd.pipe
-    )
-  endif()
-
   if(libc.src.compiler.__stack_chk_fail IN_LIST TARGET_LLVMLIBC_ENTRYPOINTS)
     # __stack_chk_fail should always be included if supported to allow building
     # libc with the stack protector enabled.
@@ -1026,7 +1009,6 @@ function(add_libc_test test_name)
       ${test_name}.__hermetic__
       LINK_LIBRARIES
         LibcTest.hermetic
-        LibcDeathTestExecutors.hermetic
       ${LIBC_TEST_UNPARSED_ARGUMENTS}
     )
     get_fq_target_name(${test_name} fq_test_name)
