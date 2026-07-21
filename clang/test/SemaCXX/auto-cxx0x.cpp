fb38c9c7e0e631cf09040c6f743eaa69cc9a93b2
Osama Abdelkader <osama.abdelkader@gmail.com>
2026-07-21 15:48:51 +0200

    [Clang] update test case #GH210014
    Signed-off-by: Osama Abdelkader <osama.abdelkader@gmail.com>

--- clang/test/SemaCXX/auto-cxx0x.cpp
+++ clang/test/SemaCXX/auto-cxx0x.cpp
@@ -15,7 +15,7 @@ struct PR209000 {
 } auto; // expected-error {{'auto' cannot be combined with a type specifier}}
 
 auto union { // expected-error {{cannot combine with previous 'auto' declaration specifier}}
-} foo<>; // expected-error {{no template named 'foo'}}
+} foo<>(); // expected-error {{no template named 'foo'}}
 
 typedef auto PR25449(); // expected-error {{'auto' not allowed in typedef}}
 
