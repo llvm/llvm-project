daba8325e33ff89ae9b00ed1d0cc61146f02a7bb
Mimis Chlympatsos <mimischly@MIMIS-MAC-120.local>
2026-07-22 00:53:20 -0400

    Update getFunctionOrMethodParam to handle Decl's which have underlying FunctionProtoType but are not one of FunctionDecl, ObjCMethodDecl, and BlockDecl. Needed for correct source range calculation invoked by AddAllocAlignAttr.

--- clang/test/Misc/attr-source-range.cpp
+++ clang/test/Misc/attr-source-range.cpp
@@ -1,4 +1,4 @@
-// RUN: not %clang_cc1 -fsyntax-only -fdiagnostics-print-source-range-info %s 2>&1 | FileCheck %s
+// RUN: not %clang_cc1 -fblocks -fsyntax-only -fdiagnostics-print-source-range-info %s 2>&1 | FileCheck %s
 
 void f(int i) __attribute__((format_arg(1)));
 // CHECK: attr-source-range.cpp:3:30:{3:41-3:42}{3:8-3:13}
@@ -14,3 +14,26 @@ void i(int j) __attribute__((nonnull(1)));
 
 void j(__attribute__((nonnull)) int i);
 // CHECK: attr-source-range.cpp:15:23:{15:8-15:38}
+
+void alloc_align_function_pointer(
+    char *(*fn)(char *) __attribute__((alloc_align(1))));
+// CHECK: attr-source-range.cpp:[[@LINE-1]]:52:{[[@LINE-1]]:17-[[@LINE-1]]:23}: error: 'alloc_align' attribute argument may only refer to a function parameter of integer type
+
+// A top-level qualifier wraps the PointerTypeLoc in a QualifiedTypeLoc. Make
+// sure parameter lookup strips that wrapper before finding FunctionProtoTypeLoc.
+void alloc_align_qualified_function_pointer(
+    char *(*const fn)(char *) __attribute__((alloc_align(1))));
+// CHECK: attr-source-range.cpp:[[@LINE-1]]:58:{[[@LINE-1]]:23-[[@LINE-1]]:29}: error: 'alloc_align' attribute argument may only refer to a function parameter of integer type
+
+struct S;
+void alloc_align_member_function_pointer(
+    char *(S::*fn)(char *) __attribute__((alloc_align(1))));
+// CHECK: attr-source-range.cpp:[[@LINE-1]]:55:{[[@LINE-1]]:20-[[@LINE-1]]:26}: error: 'alloc_align' attribute argument may only refer to a function parameter of integer type
+
+void alloc_align_function_reference(
+    char *(&fn)(char *) __attribute__((alloc_align(1))));
+// CHECK: attr-source-range.cpp:[[@LINE-1]]:52:{[[@LINE-1]]:17-[[@LINE-1]]:23}: error: 'alloc_align' attribute argument may only refer to a function parameter of integer type
+
+void alloc_align_block_pointer(
+    char *(^fn)(char *) __attribute__((alloc_align(1))));
+// CHECK: attr-source-range.cpp:[[@LINE-1]]:52:{[[@LINE-1]]:17-[[@LINE-1]]:23}: error: 'alloc_align' attribute argument may only refer to a function parameter of integer type
