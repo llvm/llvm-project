ba73c41e69e0adca001ef6825b49ccabe8aaad84
Hristo Hristov <hghristov.rmm@gmail.com>
2026-07-22 11:34:21 +0300

    Added missing test `join_view`

---
+++ libcxx/test/std/ranges/range.adaptors/range.join/range.join.sentinel/ctor.parent.compile.pass.cpp
@@ -0,0 +1,21 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// REQUIRES: std-at-least-c++20
+
+// constexpr explicit sentinel(Parent& parent);
+
+// The constructor is now `private` (exposition-only) per P3059R2.
+
+#include <ranges>
+
+#include "../types.h"
+
+using Parent = std::ranges::join_view<ParentView<ChildView>>;
+static_assert(!std::is_constructible_v<std::ranges::sentinel_t<Parent>, Parent&>);
+static_assert(!std::is_convertible_v<std::ranges::sentinel_t<Parent>, Parent&>);
