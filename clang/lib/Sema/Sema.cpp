4288b03d86cfe10be92601ba92f910a2853e1d0f
Volodymyr Sapsai <vsapsai@apple.com>
2026-07-22 14:10:56 -0700

    Add a FIXME about a duplicate `ExternalSemaSource`.

--- clang/lib/Sema/Sema.cpp
+++ clang/lib/Sema/Sema.cpp
@@ -616,6 +616,8 @@ Sema::~Sema() {
   if (ExternalSemaSource *ExternalSema
         = dyn_cast_or_null<ExternalSemaSource>(Context.getExternalSource()))
     ExternalSema->ForgetSema();
+  // FIXME: keep just a single ExternalSemaSource instead of 2 with a slightly
+  // different behavior.
   if (ExternalSource)
     ExternalSource->ForgetSema();
 
