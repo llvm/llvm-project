e4b7d27f1c6ac08ca7e83404018ddf2926784d8c
Yifan Zhu <yfzhu@google.com>
2026-07-22 12:56:26 -0700

    fix

--- libc/test/src/__support/freelist_heap_death_test.cpp
+++ libc/test/src/__support/freelist_heap_death_test.cpp
@@ -19,30 +19,32 @@ asm(".globl _end, __llvm_libc_heap_limit\n_end:\n__llvm_libc_heap_limit:\n");
 using LIBC_NAMESPACE::FreeListHeap;
 using LIBC_NAMESPACE::cpp::byte;
 
-#if defined(ENABLE_SUBPROCESS_TESTS) && LIBC_COPT_HARDENING_MODE > LIBC_HARDENING_MODE_FAST
 TEST(LlvmLibcFreeListHeapDeathTest, DoubleFreeDeath) {
   byte buf[2048] = {byte(0)};
   FreeListHeap allocator(buf);
-
   void *ptr = allocator.allocate(512);
   ASSERT_NE(ptr, static_cast<void *>(nullptr));
   allocator.free(ptr);
+#if defined(ENABLE_SUBPROCESS_TESTS) &&                                        \
+    LIBC_COPT_HARDENING_MODE > LIBC_HARDENING_MODE_FAST
   EXPECT_EXITS(
       [&] {
         allocator.free(ptr);
         allocator.integrity_check();
       },
       255);
+#endif
 }
 
 TEST(LlvmLibcFreeListHeapDeathTest, UseAfterFreeDeath) {
   byte buf[2048] = {byte(0)};
   FreeListHeap allocator(buf);
-
   void *ptr = allocator.allocate(512);
   ASSERT_NE(ptr, static_cast<void *>(nullptr));
   allocator.free(ptr);
-  *reinterpret_cast<uintptr_t *>(ptr) = 0xDEADBEEF;
+#if defined(ENABLE_SUBPROCESS_TESTS) &&                                        \
+    LIBC_COPT_HARDENING_MODE > LIBC_HARDENING_MODE_FAST
+  new (ptr) uintptr_t{0xDEADBEEF};
   EXPECT_EXITS([&] { allocator.integrity_check(); }, 255);
+#endif
 }
-#endif // ENABLE_SUBPROCESS_TESTS
