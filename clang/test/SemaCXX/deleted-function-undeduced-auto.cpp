9a3a17cef2c0bb20b63bca6f297e040a4ef0fcd1
neharaj <neharajgautam07@gmail.com>
2026-07-22 00:09:58 +0000

    [clang] Add decltype(auto) regression test

--- clang/test/SemaCXX/deleted-function-undeduced-auto.cpp
+++ clang/test/SemaCXX/deleted-function-undeduced-auto.cpp
@@ -1,8 +1,13 @@
 // RUN: %clang_cc1 -fsyntax-only -verify %s
 
-auto f() = delete; // expected-note {{candidate function has been explicitly deleted}}
+auto f() = delete; // expected-note 2 {{candidate function has been explicitly deleted}}
 
 auto g() {
   auto x = f(); // expected-error {{call to deleted function 'f'}}
+  return x;
+}
+
+auto g2() {
+  decltype(auto) x = f(); // expected-error {{call to deleted function 'f'}}
   return x;
 }
