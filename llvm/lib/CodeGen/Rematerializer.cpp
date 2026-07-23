87a98e3836f48a32f14653e286b58f5cb06e733a
Gergely Futo <gergely.futo@hightec-rt.com>
2026-07-23 08:40:44 +0200

    [CodeGen] Fix -Wrange-loop-construct

--- llvm/lib/CodeGen/Rematerializer.cpp
+++ llvm/lib/CodeGen/Rematerializer.cpp
@@ -287,7 +287,7 @@ void Rematerializer::deleteReg(RegisterIdx RootIdx) {
         shrinkToUses(DepRegIdx);
       }
     }
-    for (const auto [Reg, Mask] : getUnrematableDeps(DeletedRegIdx)) {
+    for (const auto &[Reg, Mask] : getUnrematableDeps(DeletedRegIdx)) {
       if (ShrinkUnrematRegs.insert(Reg).second)
         shrinkToUsesUnremat(Reg);
     }
