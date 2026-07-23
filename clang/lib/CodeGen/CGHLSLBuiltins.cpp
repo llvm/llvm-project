80440c8cdf70dc7b9719d82353991bd4332a7438
Joshua Batista <jbatista@microsoft.com>
2026-07-23 12:07:43 -0700

    remove unused function

--- clang/lib/CodeGen/CGHLSLBuiltins.cpp
+++ clang/lib/CodeGen/CGHLSLBuiltins.cpp
@@ -332,34 +332,6 @@ static Value *handleInterlockedOp(CodeGenFunction &CGF, const CallExpr *E,
   return Call;
 }
 
-static Value *handleInterlockedOp(CodeGenFunction &CGF, const CallExpr *E,
-                                  Intrinsic::ID ID, const Twine &Name) {
-  // HLSL signatures (synthesized as overloads in HLSLExternalSemaSource):
-  //   void InterlockedOp(groupshared|device T &dest, T value);
-  //   void InterlockedOp(groupshared|device T &dest, T value,
-  //                      T &original_value);
-  // Both `dest` and `original_value` are plain references, so we can use
-  // the underlying lvalue directly without HLSLOutArgExpr unwrapping.
-  LValue DestLV = CGF.EmitLValue(E->getArg(0));
-  Value *Ptr = DestLV.getAddress().emitRawPointer(CGF);
-  Value *Val = CGF.EmitScalarExpr(E->getArg(1));
-  assert(E->getArg(1)->getType()->isIntegerType() &&
-         "Intrinsic InterlockedOp value operand must be an integer");
-
-  Value *Call = CGF.EmitRuntimeCall(
-      Intrinsic::getOrInsertDeclaration(&CGF.CGM.getModule(), ID,
-                                        {Val->getType(), Ptr->getType()}),
-      ArrayRef<Value *>{Ptr, Val}, Name);
-
-  // The 3-arg overload writes the old value (the intrinsic's return value)
-  // into the `original_value` reference parameter.
-  if (E->getNumArgs() == 3) {
-    LValue OrigLV = CGF.EmitLValue(E->getArg(2));
-    CGF.EmitStoreThroughLValue(RValue::get(Call), OrigLV);
-  }
-  return Call;
-}
-
 static Value *emitBufferStride(CodeGenFunction *CGF, const Expr *HandleExpr,
                                LValue &Stride) {
   // Figure out the stride of the buffer elements from the handle type.
