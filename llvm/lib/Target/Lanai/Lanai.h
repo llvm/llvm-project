b0c727b883768db20e4787c263866c2acdaf0c4a
Aiden Grossman <aidengrossman@google.com>
2026-07-24 18:12:58 +0000

    [𝘀𝗽𝗿] changes to main this commit is based on
    Created using spr 1.3.7
    
    [skip ci]

--- llvm/lib/Target/Lanai/Lanai.h
+++ llvm/lib/Target/Lanai/Lanai.h
@@ -37,7 +37,7 @@ FunctionPass *createLanaiISelDagLegacyPass(LanaiTargetMachine &TM);
 // createLanaiDelaySlotFillerPass - This pass fills delay slots
 // with useful instructions or nop's
 class LanaiDelaySlotFillerPass
-    : public PassInfoMixin<LanaiDelaySlotFillerPass> {
+    : public RequiredPassInfoMixin<LanaiDelaySlotFillerPass> {
 public:
   PreservedAnalyses run(MachineFunction &MF,
                         MachineFunctionAnalysisManager &MFAM);
@@ -48,7 +48,8 @@ createLanaiDelaySlotFillerLegacyPass(const LanaiTargetMachine &TM);
 
 // createLanaiMemAluCombinerPass - This pass combines loads/stores and
 // arithmetic operations.
-class LanaiMemAluCombinerPass : public PassInfoMixin<LanaiMemAluCombinerPass> {
+class LanaiMemAluCombinerPass
+    : public RequiredPassInfoMixin<LanaiMemAluCombinerPass> {
 public:
   PreservedAnalyses run(MachineFunction &MF,
                         MachineFunctionAnalysisManager &MFAM);
