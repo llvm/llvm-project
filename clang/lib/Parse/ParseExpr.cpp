0170afdb664994f9d125c25de937bb2a77d8face
Abdulmalek Almkainzi <aalmkainzi@gmail.com>
2026-07-25 01:17:14 +0300

    fixed `enum A : T {B} : 0` in C++ mode, where it used to parse T {B} as an expression, rather than the underlying type + enum body

--- clang/lib/Parse/ParseExpr.cpp
+++ clang/lib/Parse/ParseExpr.cpp
@@ -2665,6 +2665,8 @@ Parser::ParseParenExpression(ParenParseOption &ExprType, bool StopIfCastExpr,
                              ParsedType &CastTy, SourceLocation &RParenLoc) {
   assert(Tok.is(tok::l_paren) && "Not a paren expr!");
   ColonProtectionRAIIObject ColonProtection(*this, false);
+  GenericAssociationTypeRAIIObject NotParsingGenericAssociationType(*this, false);
+
   BalancedDelimiterTracker T(*this, tok::l_paren);
   if (T.consumeOpen())
     return ExprError();
