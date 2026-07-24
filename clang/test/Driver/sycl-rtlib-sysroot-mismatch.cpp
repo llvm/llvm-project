b9804bb149b1311431d7d3ebcb104322aa25be62
Nick Sarnie <nick.sarnie@intel.com>
2026-07-24 09:05:50 -0700

    add test for per-target-runtime-dir layout
    Signed-off-by: Nick Sarnie <nick.sarnie@intel.com>

--- clang/test/Driver/sycl-rtlib-sysroot-mismatch.cpp
+++ clang/test/Driver/sycl-rtlib-sysroot-mismatch.cpp
@@ -6,6 +6,13 @@
 // RUN: touch %t/lib/libLLVMSYCL.so
 // RUN: ln -s %clang %t/bin/clang
 // RUN: %t/bin/clang -### -no-canonical-prefixes --target=x86_64-unknown-linux-gnu -fsycl --sysroot=/nonexistent-prefix %s 2>&1 \
-// RUN:   | FileCheck %s
+// RUN:   | FileCheck -check-prefix=CHECK-FLAT %s
 
-// CHECK: "{{.*}}/bin/../lib/libLLVMSYCL.so"
+// RUN: rm -rf %t && mkdir -p %t/bin %t/lib/x86_64-unknown-linux-gnu
+// RUN: touch %t/lib/x86_64-unknown-linux-gnu/libLLVMSYCL.so
+// RUN: ln -s %clang %t/bin/clang
+// RUN: %t/bin/clang -### -no-canonical-prefixes --target=x86_64-unknown-linux-gnu -fsycl --sysroot=/nonexistent-prefix %s 2>&1 \
+// RUN:   | FileCheck -check-prefix=CHECK-PER-TARGET %s
+
+// CHECK-FLAT: "{{.*}}/bin/../lib/libLLVMSYCL.so"
+// CHECK-PER-TARGET: "{{.*}}/bin/../lib/x86_64-unknown-linux-gnu/libLLVMSYCL.so"
