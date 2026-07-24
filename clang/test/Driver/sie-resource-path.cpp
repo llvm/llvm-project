918e802d4124830ee6de6d8866e31a4892db3ee4
Russell Gallop <russell.gallop@sony.com>
2026-07-24 17:18:54 +0100

    Improve test to be sensitive to dots before and version numbers after.
    Previously the check not tests weren't actually doing anything, as the
    checked the strings after the CHECK succeeded.
    
    Added a . check before, and the hard end of the " should catch a stray
    version number.

--- clang/test/Driver/sie-resource-path.cpp
+++ clang/test/Driver/sie-resource-path.cpp
@@ -1,12 +1,8 @@
 // REQUIRES: clang-vendor=SIE
 
-// Test relative CLANG_RESOURCE_PATH configuration, as used on SIE toolchains.
+// Test relative CLANG_RESOURCE_PATH=../lib/clang configuration, as used on SIE toolchains.
 // --target shouldn't have an impact on this as it's a build config.
 
 // RUN: %clang -c -### %s 2>&1 | FileCheck %s
-// Expected resource path.
-// CHECK: "-resource-dir" "{{.*}}{{/|\\\\}}lib{{/|\\\\}}clang"
-// Check resource path doesn't have a version number at the end.
-// CHECK-NOT: "-resource-dir" "{{.*}}{{/|\\\\}}lib{{/|\\\\}}clang{{/|\\\\}}{{[0-9]}}"
-// Check resource path doesn't have .. before lib.
-// CHECK-NOT: "-resource-dir" "{{.*}}..{{/|\\\\}}lib{{/|\\\\}}clang"
+// Expected resource path doesn't have a . before, or a number after.
+// CHECK: "-resource-dir" "{{.*[^.]}}{{/|\\\\}}lib{{/|\\\\}}clang"
