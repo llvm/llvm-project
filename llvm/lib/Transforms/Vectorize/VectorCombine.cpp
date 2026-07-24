aab59f2eecf77588eb0a61abb445ad00f1eb1fe1
hanbeom <kese111@gmail.com>
2026-07-24 14:15:54 +0900

    formatting

--- llvm/lib/Transforms/Vectorize/VectorCombine.cpp
+++ llvm/lib/Transforms/Vectorize/VectorCombine.cpp
@@ -2059,8 +2059,7 @@ bool VectorCombine::foldInsertElementsToStores(Instruction &I) {
   for (auto [InsertVal, Idx] : InsertElements) {
     if (isa<ConstantInt>(Idx))
       continue;
-    const Value *GEPIndices[] = {
-        ConstantInt::get(Idx->getType(), 0), Idx};
+    const Value *GEPIndices[] = {ConstantInt::get(Idx->getType(), 0), Idx};
     NewCost += TTI.getGEPCost(VecTy, SI->getPointerOperand(), GEPIndices,
                               InsertVal->getType(), CostKind);
   }
