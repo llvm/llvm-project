eca4336c1545ad00344bbcc12aa5f0619f7d2687
ag <glisanov.andrej@yandex.ru>
2026-07-25 23:47:58 +0300

    added callback func

--- llvm/include/llvm/Transforms/Instrumentation/DefUseInstrumentation.h
+++ llvm/include/llvm/Transforms/Instrumentation/DefUseInstrumentation.h
@@ -1,9 +1,13 @@
 #ifndef LLVM_TRANSFORMS_INSTRUMENTATION_DEFUSEINSTRUMENTATION_H
 #define LLVM_TRANSFORMS_INSTRUMENTATION_DEFUSEINSTRUMENTATION_H
 
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/PassManager.h"
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/Module.h"
+#include "llvm/IR/Type.h"
 
 namespace llvm {
 
@@ -16,7 +20,13 @@ struct DefUseInstrumentationPass
     if (!Main || Main->isDeclaration()) {
       return PreservedAnalyses::all();
     }
-    return PreservedAnalyses::all();
+    LLVMContext& Ctx = M.getContext();
+
+    FunctionType* HookType = FunctionType::get(Type::getVoidTy(Ctx), false);
+
+    FunctionCallee funccall =  M.getOrInsertFunction("__def_use_trace_main_enter", HookType);
+
+    return PreservedAnalyses::none();
   }
 };
 
