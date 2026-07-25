5d32ff5424b490676c9771699233a396532fea1b
shyeyian <shyeyian@mail.shyeyian.com>
2026-07-25 15:37:21 +0800

    Add chunk_view inner iterator base() test

---
+++ libcxx/test/std/ranges/range.adaptors/range.chunk/range.chunk.inner.iter/base.pass.cpp
@@ -0,0 +1,44 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// REQUIRES: std-at-least-c++23
+
+// <ranges>
+
+// V models only input_range:
+//   constexpr const iterator_t<V> base() const&;
+
+#include <cassert>
+#include <concepts>
+#include <iterator>
+#include <ranges>
+
+#include "../types.h"
+
+constexpr bool test() {
+  int buffer[] = {1, 2, 3, 4};
+
+  std::ranges::chunk_view<input_span<int>> chunked(input_span<int>(buffer, 4), 2);
+  auto outer = chunked.begin();
+  auto inner = (*outer).begin();
+
+  std::same_as<const std::ranges::iterator_t<input_span<int>>> decltype(auto) base = inner.base();
+  assert(*base == 1);
+
+  ++inner;
+  assert(*inner.base() == 2);
+
+  return true;
+}
+
+int main(int, char**) {
+  test();
+  static_assert(test());
+
+  return 0;
+}
