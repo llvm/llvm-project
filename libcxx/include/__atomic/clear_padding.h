7d841c5c63ac7cb06c5e4b39c51a241d6394ccf9
Hui Xie <hui.xie1990@gmail.com>
2026-07-25 17:59:09 +0100

    inline

--- libcxx/include/__atomic/clear_padding.h
+++ libcxx/include/__atomic/clear_padding.h
@@ -46,11 +46,13 @@ _LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR _Tp& __clear_padding_if_needed(_Tp& __ob
   return __builtin_is_constant_evaluated() ? __obj : (__builtin_clear_padding(std::addressof(__obj)), __obj);
 }
 
+// clang fails to inline the function when the memory order is a constant
 template <class _Tp,
           class _Up,
           class _CasFunc,
           __enable_if_t<!__needs_clear_padding<__remove_cvref_t<_Tp> >::value, int> = 0>
-_LIBCPP_HIDE_FROM_ABI bool __atomic_cas_with_clear_padding(_Tp* __expected, _Up __value, _CasFunc&& __cas_func) {
+_LIBCPP_HIDE_FROM_ABI _LIBCPP_ALWAYS_INLINE bool
+__atomic_cas_with_clear_padding(_Tp* __expected, _Up __value, _CasFunc&& __cas_func) {
   return __cas_func(__expected, __value);
 }
 
@@ -78,7 +80,8 @@ _LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR _Tp& __clear_padding_if_needed(_Tp& __ob
 }
 
 template <class _Tp, class _Up, class _CasFunc>
-_LIBCPP_HIDE_FROM_ABI bool __atomic_cas_with_clear_padding(_Tp* __expected, _Up __value, _CasFunc&& __cas_func) {
+_LIBCPP_HIDE_FROM_ABI _LIBCPP_ALWAYS_INLINE bool
+__atomic_cas_with_clear_padding(_Tp* __expected, _Up __value, _CasFunc&& __cas_func) {
   return __cas_func(__expected, __value);
 }
 
