0227f5e2f61e0688b81221354a29fcfbd0dc3822
NimishMishra <neelam.nimish@gmail.com>
2026-07-26 18:35:00 +0530

    Do not run the entire pipeline for the test

--- flang/test/Driver/finit-local-zero.f90
+++ flang/test/Driver/finit-local-zero.f90
@@ -3,7 +3,7 @@
 ! RUN: %flang -### -S %s -o - 2>&1 | FileCheck %s --check-prefixes=NO-INIT
 
 ! Check that the compiler accepts -finit-local-zero:
-! RUN: %flang_fc1 -finit-local-zero %s -o /dev/null
+! RUN: %flang_fc1 -fsyntax-only -finit-local-zero %s -o /dev/null
 
 ! CHECK: "-fc1"{{.*}}"-finit-local-zero"
 ! NO-INIT-NOT: -finit-local-zero
