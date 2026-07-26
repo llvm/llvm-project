e9c98285c5c8337aafc69ad397b216ef2bcf0866
rdevshp <rdevshp@gmail.com>
2026-07-26 15:43:22 +0000

    remove -pedantic from atomic-auto.cpp

--- clang/test/SemaCXX/atomic-auto.cpp
+++ clang/test/SemaCXX/atomic-auto.cpp
@@ -1,8 +1,7 @@
-// RUN: %clang_cc1 -verify -pedantic %s -std=c++20
+// RUN: %clang_cc1 -verify %s -std=c++20
 
 template<typename T> concept C = false; // expected-note {{because 'false' evaluated to false}}
 
 void test() {
-  _Atomic C<> auto &foo = 42; // expected-warning {{'_Atomic' is a C11 extension}} \
-                              // expected-error {{deduced type 'int' does not satisfy 'C<>'}}
+  _Atomic C<> auto &foo = 42; // expected-error {{deduced type 'int' does not satisfy 'C<>'}}
 };
