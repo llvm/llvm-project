187687d4cda3cde7f6dfdd08096331c3500f7511
shyeyian <shyeyian@mail.shyeyian.com>
2026-07-26 11:41:18 +0800

    Add [[nodiscard]] tests for full coverage

--- libcxx/test/libcxx/ranges/range.adaptors/range.chunk/nodiscard.verify.cpp
+++ libcxx/test/libcxx/ranges/range.adaptors/range.chunk/nodiscard.verify.cpp
@@ -15,9 +15,17 @@
 #include <ranges>
 #include <utility>
 
+#include "types.h"
+
 void test() {
-  char range[6] = {'x', 'x', 'y', 'y', 'z', 'z'};
-  auto view     = range | std::views::chunk(2);
+  char range[6]      = {'x', 'x', 'y', 'y', 'z', 'z'};
+  auto view          = range | std::views::chunk(2);
+  auto it            = view.begin();
+  int input_range[]  = {1, 2, 3, 4, 5, 6};
+  auto input_view    = input_span(input_range, 6) | std::views::chunk(2);
+  auto outer         = input_view.begin();
+  auto value         = *outer;
+  auto inner         = value.begin();
 
   // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
   view.base();
@@ -49,9 +57,61 @@ void test() {
   std::ranges::iter_move(std::as_const(view).end());
 
   // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  view.size();
+  // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  std::as_const(view).size();
+
+  // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
   *view.begin();
   // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
   *std::as_const(view).begin();
+
+  // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  it.base();
+
+  // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  it[0];
+
+  // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  it + 1;
+  // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  1 + it;
+  // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  it - 1;
+  // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  view.end() - it;
+  // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  std::default_sentinel - it;
+  // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  it - std::default_sentinel;
+
+  // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  input_view.size();
+
+  // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  *outer;
+  // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  std::default_sentinel - outer;
+  // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  outer - std::default_sentinel;
+
+  // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  value.begin();
+  // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  value.end();
+  // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  value.size();
+
+  // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  inner.base();
+  // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  *inner;
+  // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  std::default_sentinel - inner;
+  // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  inner - std::default_sentinel;
+  // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  std::ranges::iter_move(inner);
 
   // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
   std::views::chunk(3);
