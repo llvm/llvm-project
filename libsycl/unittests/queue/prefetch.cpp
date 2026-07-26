95b3bd6bbd4c700d81205dd621b57ffd4a8659c3
Robertkq <robertvuia06@gmail.com>
2026-07-26 12:29:25 +0300

    remove unecessary includes for tests

--- libsycl/unittests/queue/prefetch.cpp
+++ libsycl/unittests/queue/prefetch.cpp
@@ -1,10 +1,6 @@
 #include <mock/helpers.hpp>
 
-#include <sycl/__impl/device.hpp>
 #include <sycl/__impl/queue.hpp>
-
-#include <detail/device_impl.hpp>
-#include <detail/queue_impl.hpp>
 
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
