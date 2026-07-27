c54d9e90157791057fc202eee114e041411c9ac2
Florian Hahn <flo@fhahn.com>
2026-07-27 10:54:59 +0100

    !fixup address comments, thanks

--- llvm/lib/Analysis/LoopAccessAnalysis.cpp
+++ llvm/lib/Analysis/LoopAccessAnalysis.cpp
@@ -1713,15 +1713,17 @@ uint64_t llvm::getBoundForConsecutiveLoad(const SCEV *PtrSCEV, Type *AccessTy,
   if (AccessTy->isScalableTy())
     return 0;
 
-  // `A[i % 2^N]` is `Base + ElemSize * zext({0,+,1}<iN>)`. For a byte
-  // element (ElemSize == 1) the multiply folds away.
+  // `A[i % 2^N]` is modeled as `Base + ElemSize * zext({0,+,1}<iN>)`by SCEV,
+  // with N being the bitwidth of the induction. For a byte element (ElemSize ==
+  // 1) the multiply folds away.
   const SCEV *Base, *Start;
   const APInt *Scale = nullptr;
   auto Index = m_scev_ZExt(
       m_scev_AffineAddRec(m_SCEV(Start), m_scev_One(), m_SpecificLoop(L)));
-  if (!match(PtrSCEV, m_scev_Add(m_scev_Mul(m_scev_APInt(Scale), Index),
-                                 m_SCEV(Base))) &&
-      !match(PtrSCEV, m_scev_Add(Index, m_SCEV(Base))))
+  if (!match(
+          PtrSCEV,
+          m_scev_Add(m_CombineOr(Index, m_scev_Mul(m_scev_APInt(Scale), Index)),
+                     m_SCEV(Base))))
     return 0;
 
   uint64_t AllocSize =
