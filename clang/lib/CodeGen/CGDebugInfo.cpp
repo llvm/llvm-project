b159355a642635af0b42b3ad42529df78b706878
Marc Auberer <marc.auberer@chillibits.com>
2026-07-27 12:42:46 +0200

    [IR][NFC] Rename insertDbgValueIntrinsic -> insertDbgValue (#212151)
    Rename `insertDbgValueIntrinsic` to `insertDbgValue`, which is
    consistent with the other insert helpers like `insertDbgAssign`,
    `insertDeclare`, etc.
    Also use debug record instead of intrinsic terminology in doc comments.
    
    (cherry picked from commit bd3551611ea19adae70a744be8d4f4ec096a114a)

--- clang/lib/CodeGen/CGDebugInfo.cpp
+++ clang/lib/CodeGen/CGDebugInfo.cpp
@@ -6411,8 +6411,8 @@ void CGDebugInfo::EmitPseudoVariable(CGBuilderTy &Builder,
                                   Type, false, llvm::DINode::FlagArtificial);
 
   if (auto InsertPoint = Value->getInsertionPointAfterDef()) {
-    DBuilder.insertDbgValueIntrinsic(Value, D, DBuilder.createExpression(), DIL,
-                                     *InsertPoint);
+    DBuilder.insertDbgValue(Value, D, DBuilder.createExpression(), DIL,
+                            *InsertPoint);
   }
 }
 
