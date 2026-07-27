a69d1c204ae8f6f86ab001b80409311e76912400
Russell Gallop <russell.gallop@sony.com>
2026-07-27 10:33:56 +0100

    Expand on commentary a bit.

--- clang/test/Driver/sie-resource-path.cpp
+++ clang/test/Driver/sie-resource-path.cpp
@@ -1,6 +1,7 @@
 // REQUIRES: clang-vendor=SIE
 
-// Test relative CLANG_RESOURCE_PATH=../lib/clang configuration, as used on SIE toolchains.
+// Test relative CLANG_RESOURCE_PATH=../lib/clang configuration, as used on
+// SIE toolchains (PS4/PS5).
 // --target shouldn't have an impact on this as it's a build config.
 
 // RUN: %clang -c -### %s 2>&1 | FileCheck %s
