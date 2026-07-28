2002de5465f214057a2110701e7c781092cbeb11
David Spickett <david.spickett@arm.com>
2026-07-28 14:01:19 +0000

    will be caught by compiler anyway

--- lldb/source/Plugins/Process/Linux/NativeRegisterContextLinux_arm64.cpp
+++ lldb/source/Plugins/Process/Linux/NativeRegisterContextLinux_arm64.cpp
@@ -84,8 +84,6 @@ NativeRegisterContextLinux_arm64::GetInvalidationMask(
     // both ZA and ZT being enabled.
     return RegisterSetType::ZA_HEADER | RegisterSetType::ZA |
            RegisterSetType::ZT;
-  default:
-    llvm_unreachable("Unhandled register set");
   }
 }
 
