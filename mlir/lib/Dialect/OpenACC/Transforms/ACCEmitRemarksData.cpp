befa4cb03642d20b212a0ed59b69c4d01dc7f147
Razvan Lupusoru <razvan.lupusoru@gmail.com>
2026-07-28 14:22:31 -0700

    Update mlir/lib/Dialect/OpenACC/Transforms/ACCEmitRemarksData.cpp
    Co-authored-by: Valentin Clement (バレンタイン クレメン) <clementval@gmail.com>

--- mlir/lib/Dialect/OpenACC/Transforms/ACCEmitRemarksData.cpp
+++ mlir/lib/Dialect/OpenACC/Transforms/ACCEmitRemarksData.cpp
@@ -1,4 +1,4 @@
-//===- ACCEmitRemarksData.cpp - Emit OpenACC data-mapping remarks --------===//
+//===- ACCEmitRemarksData.cpp - Emit OpenACC data-mapping remarks ---------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
