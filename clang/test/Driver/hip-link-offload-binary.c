e818de91ebab0ca6d0f27fc2612be4767a5d899d
Yaxun (Sam) Liu <yaxun.liu@amd.com>
2026-07-27 23:41:36 -0400

    [HIP] Specify Linux target in offload binary test

--- clang/test/Driver/hip-link-offload-binary.c
+++ clang/test/Driver/hip-link-offload-binary.c
@@ -10,7 +10,8 @@
 // RUN:   --image=file=%t.y.bc,triple=amdgcn-amd-amdhsa,arch=gfx906,kind=hip \
 // RUN:   --image=file=%t.y.bc,triple=amdgcn-amd-amdhsa,arch=gfx942,kind=hip
 
-// RUN: %clang -### -fgpu-rdc --hip-link --cuda-device-only \
+// RUN: %clang -### --target=x86_64-unknown-linux-gnu \
+// RUN:   -fgpu-rdc --hip-link --cuda-device-only \
 // RUN:   --offload-arch=gfx906 --offload-arch=gfx942 \
 // RUN:   %t.x.bundle.bc %t.y.bundle.bc -o %t.hipfb 2>&1 \
 // RUN:   | FileCheck %s --check-prefix=DRIVER
@@ -20,7 +21,8 @@
 // DRIVER-SAME: "--emit-fatbin-only" "-o" "{{.*}}.hipfb"
 // DRIVER-SAME: "{{.*}}.x.bundle.bc" "{{.*}}.y.bundle.bc"
 
-// RUN: %clang -### -v -fgpu-rdc --hip-link --cuda-device-only \
+// RUN: %clang -### -v --target=x86_64-unknown-linux-gnu \
+// RUN:   -fgpu-rdc --hip-link --cuda-device-only \
 // RUN:   --offload-arch=gfx906 %t.x.bundle.bc -o %t.hipfb 2>&1 \
 // RUN:   | FileCheck %s --check-prefix=VERBOSE
 // VERBOSE: "{{.*}}clang-linker-wrapper"
@@ -28,7 +30,8 @@
 // VERBOSE-SAME: "--wrapper-verbose"
 // VERBOSE-SAME: "--emit-fatbin-only"
 
-// RUN: %clang -fgpu-rdc --hip-link --cuda-device-only \
+// RUN: %clang --target=x86_64-unknown-linux-gnu \
+// RUN:   -fgpu-rdc --hip-link --cuda-device-only \
 // RUN:   --offload-arch=gfx906 --offload-arch=gfx942 \
 // RUN:   %t.x.bundle.bc %t.y.bundle.bc -o %t.hipfb
 // RUN: clang-offload-bundler -type=o -list -input=%t.hipfb \
@@ -36,16 +39,19 @@
 // ARCH-DAG: hip-amdgcn-amd-amdhsa--gfx906
 // ARCH-DAG: hip-amdgcn-amd-amdhsa--gfx942
 
-// RUN: %clang -### -c -fgpu-rdc --hip-link --cuda-device-only \
+// RUN: %clang -### -c --target=x86_64-unknown-linux-gnu \
+// RUN:   -fgpu-rdc --hip-link --cuda-device-only \
 // RUN:   --offload-arch=gfx906 %t.x.bundle.bc -o %t.o 2>&1 \
 // RUN:   | FileCheck %s --check-prefix=COMPILE
 // COMPILE: "-cc1"
 // COMPILE-NOT: "{{.*}}clang-linker-wrapper"
 
-// RUN: %clang -### -emit-llvm -fgpu-rdc --hip-link --cuda-device-only \
+// RUN: %clang -### -emit-llvm --target=x86_64-unknown-linux-gnu \
+// RUN:   -fgpu-rdc --hip-link --cuda-device-only \
 // RUN:   --offload-arch=gfx906 %t.x.bundle.bc -o %t.linked.bc 2>&1 \
 // RUN:   | FileCheck %s --check-prefix=NO-FATBIN --allow-empty
-// RUN: %clang -### --no-gpu-bundle-output -fgpu-rdc --hip-link \
+// RUN: %clang -### --no-gpu-bundle-output \
+// RUN:   --target=x86_64-unknown-linux-gnu -fgpu-rdc --hip-link \
 // RUN:   --cuda-device-only --offload-arch=gfx906 %t.x.bundle.bc 2>&1 \
 // RUN:   | FileCheck %s --check-prefix=NO-FATBIN --allow-empty
 // NO-FATBIN-NOT: "--emit-fatbin-only"
