dfbb198539575af6fa83831740796fb05412790b
Michael Kazakov <mike.kazakov@gmail.com>
2026-07-28 20:12:02 +0100

    Comments, ignore lack of 8-byte atomics on 32-bit AIX

--- libcxx/src/pstl/std_thread.cpp
+++ libcxx/src/pstl/std_thread.cpp
@@ -26,6 +26,14 @@
 #  include <stdio.h>
 #endif
 
+#if defined(_AIX)
+// on AIX (32-bit):
+// c++/v1/__atomic/support/c11.h:83:10: error: large atomic operation may incur significant performance penalty;
+// the access size (8 bytes) exceeds the max lock-free size (4 bytes) [-Werror,-Watomic-alignment]
+// ignore for now.
+_LIBCPP_CLANG_DIAGNOSTIC_IGNORED("-Watomic-alignment")
+#endif
+
 _LIBCPP_BEGIN_NAMESPACE_STD
 _LIBCPP_BEGIN_EXPLICIT_ABI_ANNOTATIONS
 namespace __pstl::__std_thread {
@@ -45,6 +53,8 @@ namespace __pstl::__std_thread {
 
 struct Task;
 
+// Implementation of "Dynamic Circular Work-Stealing Deque" by David Chase and Yossi Lev.
+// The element type is a pointer to Task, nullptr is not a valid value.
 struct alignas(std::hardware_destructive_interference_size) SPMCQueue {
   static constexpr unsigned log_initial_size = 6; // 64 elements by default
 
@@ -53,6 +63,8 @@ struct alignas(std::hardware_destructive_interference_size) SPMCQueue {
   SPMCQueue& operator=(const SPMCQueue&) = delete;
   ~SPMCQueue() { ::operator delete(buffer); }
 
+  // Pushes the new task at the bottom of the queue, only called by the owner.
+  // Can return false if the allocation of grown buffer fails.
   bool push(Task* task) noexcept {
     std::uint64_t b    = bottom.load();
     std::uint64_t t    = top.load();
@@ -76,6 +88,8 @@ struct alignas(std::hardware_destructive_interference_size) SPMCQueue {
     return true;
   }
 
+  // Pops a task from the bottom of the queue, only called by the owner.
+  // Returns nullptr if the queue is empty.
   Task* pop() noexcept {
     std::uint64_t b = bottom.load();
     if (b == 0)
@@ -100,6 +114,8 @@ struct alignas(std::hardware_destructive_interference_size) SPMCQueue {
     }
   }
 
+  // Steals a task from the top of the queue, can be called by any thread.
+  // Returns nullptr if the queue is empty or if the steal fails due to contention.
   Task* steal() noexcept {
     std::uint64_t t = top.load();
     std::uint64_t b = bottom.load();
@@ -172,29 +188,33 @@ private:
   std::mutex mut;
 };
 
+// Task represent the user request to apply a function to a range of iterations.
+// It is stored in the stack memory of the caller thread.
+// Multiple threads can participate in working on the same task by picking up the next iteration index and applying the
+// function to it.
 struct Task {
-  void* ctxt;
-  void (*func)(void*, std::size_t);
-  std::size_t iterations;
-  std::atomic_size_t index;
-  std::latch requests;
+  void* ctxt;                       // The context pointer to be passed to the function.
+  void (*func)(void*, std::size_t); // The function to be applied to each iteration.
+  std::size_t iterations;           // The total number of iterations to be processed.
+  std::atomic_size_t index;         // The next index of the iteration to be processed.
+  std::latch requests;              // Latch used to synchronize the caller with the worker threads.
 };
 
+// QueueID represents the ownership of a queue and the number of nested acquisitions.
 struct QueueID {
   static_assert(std::atomic<std::thread::id>::is_always_lock_free);
   std::atomic<std::thread::id> id = {};
   std::atomic_size_t aquire_count = 0;
 };
 
+// Sched is the scheduler that manages the worker threads and the queues.
 struct Sched {
 private:
   std::unique_ptr<std::thread[]> m_workers;
   std::unique_ptr<SPMCQueue[]> m_queues;
   std::unique_ptr<QueueID[]> m_queue_ids;
-
   size_t m_workers_count;
   size_t m_queues_count;
-
   std::mutex m_workers_sleep_mutex;
   std::condition_variable m_workers_sleep_cv;
 
@@ -271,12 +291,16 @@ static Sched* get_sched() {
   return g_sched;
 }
 
+// For cases when the number of iterations is less than 2 or when there are no worker threads available,
+// fallback to serial execution.
 static void apply_serial(std::size_t iterations, void* ctxt, void (*func)(void*, std::size_t)) noexcept {
   for (std::size_t i = 0; i < iterations; ++i) {
     func(ctxt, i);
   }
 }
 
+// Participate in working on the task by picking up the next iteration index and applying the function to it.
+// When all iterations are processed, the latch is counted down to signal completion.
 static void process_request(Task* task) noexcept {
   size_t const iterations                = task->iterations;
   void* const ctxt                       = task->ctxt;
@@ -331,6 +355,9 @@ void Sched::worker_thread(size_t worker_num) noexcept {
   }
 }
 
+// The entrypoint to parallel execution.
+// Apply the function to the range of iterations in parallel using worker threads.
+// The caller thread also participates in processing the workload.
 void Sched::apply(std::size_t iterations, void* ctxt, void (*func)(void*, std::size_t)) noexcept {
   if (m_workers_count == 0) {
     // No worker threads available, fallback to serial execution
@@ -338,14 +365,22 @@ void Sched::apply(std::size_t iterations, void* ctxt, void (*func)(void*, std::s
     return;
   }
 
+  // Identify ourselves.
+  // We can be either a guest thread or worker thread via nested parallelism.
   std::thread::id thread_id = std::this_thread::get_id();
-  SPMCQueue* queue          = acquire_queue(thread_id);
+
+  // Acquire a queue for this thread.
+  SPMCQueue* queue = acquire_queue(thread_id);
   if (queue == nullptr) {
+    // If we cannot acquire a queue, fallback to serial execution.
     apply_serial(iterations, ctxt, func);
     return;
   }
 
+  // Split the workload for potentially (workers_count + 1) participants (workers + caller thread).
   size_t num_requests = std::min(m_workers_count + 1, iterations);
+
+  // Create a task that will be published in the queue and then stolen by the workers.
   Task root_task{
       .ctxt       = ctxt,                             //
       .func       = func,                             //
@@ -357,6 +392,8 @@ void Sched::apply(std::size_t iterations, void* ctxt, void (*func)(void*, std::s
   LOG("New root task %p of %zu iterations with %zu requests\n", &root_task, iterations, num_requests);
 
   // Push all but one request to the queue, and process the last one directly in this thread.
+  // Also support an edge-case when the queue fails to grow, in this case the caller thread will process all remaining
+  // requests directly.
   while (num_requests > 1) {
     if (!queue->push(&root_task)) {
       break;
@@ -364,15 +401,19 @@ void Sched::apply(std::size_t iterations, void* ctxt, void (*func)(void*, std::s
     --num_requests;
   }
 
+  // Potentially wake up the sleeping workers to process the requests in the queue.
   wake_workers();
 
-  // Process the last request directly in this thread
+  // Process the last request directly in this thread.
   while (num_requests > 0) {
     LOG("Guest thread is directly processing task %p, num_requests left: %zu\n", &root_task, num_requests);
     process_request(&root_task);
     --num_requests;
   }
 
+  // Once the original task is completed, wait for all other requests to be completed by the workers.
+  // While waiting, the caller thread can also participate in processing other requests from the queue or steal from
+  // other queues.
   while (!root_task.requests.try_wait()) {
     Task* task = nullptr;
 
@@ -398,11 +439,14 @@ void Sched::apply(std::size_t iterations, void* ctxt, void (*func)(void*, std::s
     }
   }
 
+  // Synchronize with the workers to ensure nobody holds a reference to the task.
   root_task.requests.wait();
 
+  // Release the queue for this thread.
   release_queue(queue);
 }
 
+// The entrypoint to parallel execution.
 void __apply(size_t __iterations, void* __context, void (*__func)(void* __context, size_t __iteration)) noexcept {
   if (__iterations < 2) {
     apply_serial(__iterations, __context, __func);
@@ -412,6 +456,7 @@ void __apply(size_t __iterations, void* __context, void (*__func)(void* __contex
   sched->apply(__iterations, __context, __func);
 }
 
+// This actually doesn't strictly needs to be in the implementation file.
 __chunk_partitions __partition_chunks(ptrdiff_t element_count) noexcept {
   __chunk_partitions partitions;
   partitions.__chunk_count_      = std::max<ptrdiff_t>(1, element_count / 256);
