eb6fb22a4d54028250b5c5566dfc04d4fc581d00
Sukumarsawant <sawantsukumar@gmail.com>
2026-07-30 00:40:47 +0530

    test: try returning 0 instead of limit

--- libc/src/__support/FPUtil/dyadic_float.h
+++ libc/src/__support/FPUtil/dyadic_float.h
@@ -471,11 +471,13 @@ template <size_t Bits> struct DyadicFloat {
       // to avoid undefined behavior negating INT_MIN as an integer (although
       // exponents coming in to this function _shouldn't_ be that large). The
       // result should always end up as a positive size_t.
-      size_t exp = -static_cast<size_t>(exponent);
+      size_t shift = -static_cast<size_t>(exponent);
       size_t limit =
           static_cast<size_t>(cpp::numeric_limits<MantissaType>::digits);
-      size_t shift = exp < limit ? exp : limit;
-      new_mant >>= shift;
+      if (shift >= limit)
+        new_mant = 0;
+      else
+        new_mant >>= shift;
     }
 
     if (sign.is_neg()) {
