c445aeb08293030eef88302d6d37230c94da29b0
Sukumarsawant <sawantsukumar@gmail.com>
2026-07-29 14:37:23 +0530

    nit

--- libc/src/__support/FPUtil/dyadic_float.h
+++ libc/src/__support/FPUtil/dyadic_float.h
@@ -472,10 +472,9 @@ template <size_t Bits> struct DyadicFloat {
       // exponents coming in to this function _shouldn't_ be that large). The
       // result should always end up as a positive size_t.
       size_t exp = -static_cast<size_t>(exponent);
-      size_t limit = static_cast<size_t>(cpp::numeric_limits<MantissaType>::digits);
+      size_t limit =
+          static_cast<size_t>(cpp::numeric_limits<MantissaType>::digits);
       size_t shift = exp < limit ? exp : limit;
-      new_mant >>= shift;
-
       new_mant >>= shift;
     }
 
