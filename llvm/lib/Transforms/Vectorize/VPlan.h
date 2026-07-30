38acb5b2332d09a5637044fa0bcc237e5168c4fc
Florian Hahn <flo@fhahn.com>
2026-07-30 20:12:40 +0100

    [VPlan] Set correct VPIRFlags for FP selects when sinking stores. (#212973)
    Selects of floats carry fast-math flags. Update getDefaultFlags to take
    the result type and use it to handle selects. I am planning on extending
    this to other similar opcodes, including PHIs.
    
    Then update sinkPredicatedStores to pass through the correct default
    flags.
    
    This fixes a crash in cse when intersecting common metadata.
    
    Fixes https://github.com/llvm/llvm-project/issues/212811.
    
    PR: https://github.com/llvm/llvm-project/pull/212973
    (cherry picked from commit 40e33eb35417f565e2ed8a6c0dba63cbd618a926)

--- llvm/lib/Transforms/Vectorize/VPlan.h
+++ llvm/lib/Transforms/Vectorize/VPlan.h
@@ -1086,10 +1086,10 @@ private:
   }
 
 public:
-  /// Returns default flags for \p Opcode for opcodes that support it, asserts
-  /// otherwise. Opcodes not supporting default flags include compares and
-  /// ComputeReductionResult.
-  static VPIRFlags getDefaultFlags(unsigned Opcode);
+  /// Returns default flags for \p Opcode and scalar \p ResultTy for opcodes
+  /// that support it, asserts otherwise. Opcodes not supporting default flags
+  /// include compares and ComputeReductionResult.
+  static VPIRFlags getDefaultFlags(unsigned Opcode, Type *ResultTy = nullptr);
 
 #if !defined(NDEBUG)
   /// Returns true if the set flags are valid for \p Opcode.
