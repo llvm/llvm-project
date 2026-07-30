efe7ce980d05f155ca7ae3f0ce19a6d438f9afe3
David Spickett <david.spickett@arm.com>
2026-07-30 09:10:05 +0000

    the "sP" trick is not consistent, don't use it

--- lldb/test/API/python_api/value/TestValueAPI.py
+++ lldb/test/API/python_api/value/TestValueAPI.py
@@ -333,12 +333,6 @@ class ValueAPITestCase(TestBase):
                     self.assertTrue(rsp_with_name_index < num_registers)
                     self.assertEqual(sp_with_name_index, rsp_with_name_index)
 
-                    # FIXME: There is another bug that the conversion only looks for lower
-                    # case names. By using mixed case, you can reach the real "sp".
-                    sp_with_name_index_mixed = reg_set.GetIndexOfChildWithName("sP")
-                    self.assertTrue(sp_with_name_index_mixed < num_registers)
-                    self.assertEqual(sp_with_name_index_mixed, child_idx)
-
                     continue
 
                 # GetIndexOfChildWithName should return the same index.
