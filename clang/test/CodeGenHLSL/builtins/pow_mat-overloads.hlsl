e62bef0fb5501df5e2f7f81a12a819e9b13d5156
kmpeng <kaitlinpeng@microsoft.com>
2026-07-30 15:02:27 -0700

    address farzon's comments

--- clang/test/CodeGenHLSL/builtins/pow_mat-overloads.hlsl
+++ clang/test/CodeGenHLSL/builtins/pow_mat-overloads.hlsl
@@ -7,45 +7,45 @@
 
 
 // CHECK: define hidden [[SPIR_FUNC]]noundef nofpclass(nan inf) <12 x float> @_{{.*}}test_pow_double3x4{{.*}}(
-// CHECK:    [[CONVI:%.*]] = fptrunc {{.*}} <12 x double> %{{.*}} to <12 x float>
-// CHECK:    [[CONV1I:%.*]] = fptrunc {{.*}} <12 x double> %{{.*}} to <12 x float>
-// CHECK:    [[V5:%.*]] = call {{.*}} <12 x float> @llvm.pow.v12f32(<12 x float> [[CONVI]], <12 x float> [[CONV1I]])
+// CHECK:    [[CONVI:%.*]] = fptrunc [[FMF:reassoc nnan ninf nsz arcp afn]] <12 x double> %{{.*}} to <12 x float>
+// CHECK:    [[CONV1I:%.*]] = fptrunc [[FMF]] <12 x double> %{{.*}} to <12 x float>
+// CHECK:    [[V5:%.*]] = call [[FMF]] noundef nofpclass(nan inf) <12 x float> @llvm.pow.v12f32(<12 x float> [[CONVI]], <12 x float> [[CONV1I]])
 // CHECK:    ret <12 x float> [[V5]]
 float3x4 test_pow_double3x4 (double3x4 p0, double3x4 p1) {
   return pow(p0, p1);
 }
 
 // CHECK: define hidden [[SPIR_FUNC]]noundef nofpclass(nan inf) <12 x float> @_{{.*}}test_pow_uint3x4{{.*}}(
-// CHECK:    [[CONVI:%.*]] = uitofp {{.*}} <12 x i32> %{{.*}} to <12 x float>
-// CHECK:    [[CONV1I:%.*]] = uitofp {{.*}} <12 x i32> %{{.*}} to <12 x float>
-// CHECK:    [[V5:%.*]] = call {{.*}} <12 x float> @llvm.pow.v12f32(<12 x float> [[CONVI]], <12 x float> [[CONV1I]])
+// CHECK:    [[CONVI:%.*]] = uitofp [[FMF:reassoc nnan ninf nsz arcp afn]] <12 x i32> %{{.*}} to <12 x float>
+// CHECK:    [[CONV1I:%.*]] = uitofp [[FMF]] <12 x i32> %{{.*}} to <12 x float>
+// CHECK:    [[V5:%.*]] = call [[FMF]] noundef nofpclass(nan inf) <12 x float> @llvm.pow.v12f32(<12 x float> [[CONVI]], <12 x float> [[CONV1I]])
 // CHECK:    ret <12 x float> [[V5]]
 float3x4 test_pow_uint3x4 (uint3x4 p0, uint3x4 p1) {
   return pow(p0, p1);
 }
 
 // CHECK: define hidden [[SPIR_FUNC]]noundef nofpclass(nan inf) <12 x float> @_{{.*}}test_pow_int3x4{{.*}}(
-// CHECK:    [[CONVI:%.*]] = sitofp {{.*}} <12 x i32> %{{.*}} to <12 x float>
-// CHECK:    [[CONV1I:%.*]] = sitofp {{.*}} <12 x i32> %{{.*}} to <12 x float>
-// CHECK:    [[V5:%.*]] = call {{.*}} <12 x float> @llvm.pow.v12f32(<12 x float> [[CONVI]], <12 x float> [[CONV1I]])
+// CHECK:    [[CONVI:%.*]] = sitofp [[FMF:reassoc nnan ninf nsz arcp afn]] <12 x i32> %{{.*}} to <12 x float>
+// CHECK:    [[CONV1I:%.*]] = sitofp [[FMF]] <12 x i32> %{{.*}} to <12 x float>
+// CHECK:    [[V5:%.*]] = call [[FMF]] noundef nofpclass(nan inf) <12 x float> @llvm.pow.v12f32(<12 x float> [[CONVI]], <12 x float> [[CONV1I]])
 // CHECK:    ret <12 x float> [[V5]]
 float3x4 test_pow_int3x4 (int3x4 p0, int3x4 p1) {
   return pow(p0, p1);
 }
 
 // CHECK: define hidden [[SPIR_FUNC]]noundef nofpclass(nan inf) <12 x float> @_{{.*}}test_pow_int64_t3x4{{.*}}(
-// CHECK:    [[CONVI:%.*]] = sitofp {{.*}} <12 x i64> %{{.*}} to <12 x float>
-// CHECK:    [[CONV1I:%.*]] = sitofp {{.*}} <12 x i64> %{{.*}} to <12 x float>
-// CHECK:    [[V5:%.*]] = call {{.*}} <12 x float> @llvm.pow.v12f32(<12 x float> [[CONVI]], <12 x float> [[CONV1I]])
+// CHECK:    [[CONVI:%.*]] = sitofp [[FMF:reassoc nnan ninf nsz arcp afn]] <12 x i64> %{{.*}} to <12 x float>
+// CHECK:    [[CONV1I:%.*]] = sitofp [[FMF]] <12 x i64> %{{.*}} to <12 x float>
+// CHECK:    [[V5:%.*]] = call [[FMF]] noundef nofpclass(nan inf) <12 x float> @llvm.pow.v12f32(<12 x float> [[CONVI]], <12 x float> [[CONV1I]])
 // CHECK:    ret <12 x float> [[V5]]
 float3x4 test_pow_int64_t3x4 (int64_t3x4 p0, int64_t3x4 p1) {
   return pow(p0, p1);
 }
 
 // CHECK: define hidden [[SPIR_FUNC]]noundef nofpclass(nan inf) <12 x float> @_{{.*}}test_pow_uint64_t3x4{{.*}}(
-// CHECK:    [[CONVI:%.*]] = uitofp {{.*}} <12 x i64> %{{.*}} to <12 x float>
-// CHECK:    [[CONV1I:%.*]] = uitofp {{.*}} <12 x i64> %{{.*}} to <12 x float>
-// CHECK:    [[V5:%.*]] = call {{.*}} <12 x float> @llvm.pow.v12f32(<12 x float> [[CONVI]], <12 x float> [[CONV1I]])
+// CHECK:    [[CONVI:%.*]] = uitofp [[FMF:reassoc nnan ninf nsz arcp afn]] <12 x i64> %{{.*}} to <12 x float>
+// CHECK:    [[CONV1I:%.*]] = uitofp [[FMF]] <12 x i64> %{{.*}} to <12 x float>
+// CHECK:    [[V5:%.*]] = call [[FMF]] noundef nofpclass(nan inf) <12 x float> @llvm.pow.v12f32(<12 x float> [[CONVI]], <12 x float> [[CONV1I]])
 // CHECK:    ret <12 x float> [[V5]]
 float3x4 test_pow_uint64_t3x4 (uint64_t3x4 p0, uint64_t3x4 p1) {
   return pow(p0, p1);
