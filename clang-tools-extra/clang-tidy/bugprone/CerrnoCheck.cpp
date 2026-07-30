71afa5ceb5275897e9fadd35757c6863b336ef52
Csaba Zeitvogel <qtaq9s@inf.elte.hu>
2026-07-30 14:45:34 +0200

    [clang-tidy] Add cerrno check to bugprone
    This simple check is based on the cerrno portion of MSC38-C.

---
+++ clang-tools-extra/clang-tidy/bugprone/CerrnoCheck.cpp
@@ -0,0 +1,38 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "CerrnoCheck.h"
+#include "clang/ASTMatchers/ASTMatchFinder.h"
+
+using namespace clang::ast_matchers;
+
+namespace clang::tidy::bugprone {
+
+void CerrnoCheck::registerMatchers(MatchFinder *Finder) {
+  Finder->addMatcher(varDecl(hasType(asString("int")), hasName("errno"), hasExternalFormalLinkage()).bind("errnoDecl"), this);
+}
+
+void CerrnoCheck::check(const MatchFinder::MatchResult &Result) {
+  const auto *MatchedDecl = Result.Nodes.getNodeAs<VarDecl>("errnoDecl"); // NULL?
+  const SourceManager &SM = *Result.SourceManager;
+  const auto Location = MatchedDecl->getLocation();
+  const auto FileID = SM.getFileID(Location);
+
+  unsigned Line = SM.getSpellingLineNumber(MatchedDecl->getBeginLoc());
+
+  const auto Diag = diag(Location, "errno declaration detected, include cerrno instead")
+      << FixItHint::CreateRemoval(CharSourceRange::getCharRange(SM.translateLineCol(FileID, Line, 1), SM.translateLineCol(FileID, Line + 1, 1)));
+
+  if (alreadyInserted)
+    return;
+
+  Diag << FixItHint::CreateInsertion(SM.getLocForStartOfFile(FileID), "#include <cerrno>\n");
+  alreadyInserted = true;
+}
+
+} // namespace clang::tidy::bugprone
