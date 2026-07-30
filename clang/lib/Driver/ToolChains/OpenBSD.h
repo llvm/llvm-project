b26a40e7fc0cb2d36c7717a97ab96c27efc5cadf
Brad Smith <brad@comstyle.com>
2026-07-30 05:51:28 -0400

    [clang][Driver] Fix libc++ include path on OpenBSD
    `clang++` defaults to `-stdlib=libc++` on OpenBSD. When building with both
    `clang` and `libcxx` included, the freshly built `clang++` uses the system
    version of the `libc++` headers. However, this is from the bundled `libc++`
    22.1.8, thus inconsistent with the `libc++` being built.
    
    OpenBSD has its own version of addLibCxxIncludePaths which just includes
    `/usr/include/c++/v1`.
    
    This patch removes OpenBSD::addLibCxxIncludePaths in favour of the generic
    version in `Gnu.cpp`.

--- clang/lib/Driver/ToolChains/OpenBSD.h
+++ clang/lib/Driver/ToolChains/OpenBSD.h
@@ -73,9 +73,6 @@ public:
   void
   AddClangSystemIncludeArgs(const llvm::opt::ArgList &DriverArgs,
                             llvm::opt::ArgStringList &CC1Args) const override;
-
-  void addLibCxxIncludePaths(const llvm::opt::ArgList &DriverArgs,
-                             llvm::opt::ArgStringList &CC1Args) const override;
   void AddCXXStdlibLibArgs(const llvm::opt::ArgList &Args,
                            llvm::opt::ArgStringList &CmdArgs) const override;
 
