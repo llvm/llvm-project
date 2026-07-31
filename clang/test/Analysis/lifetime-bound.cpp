49ce0519ee1179a533c97cd1bd4c613945481770
benedekaibas <kaibas01@allegheny.edu>
2026-07-31 20:55:27 +0200

    Remove inline containers RUN lines.

--- clang/test/Analysis/lifetime-bound.cpp
+++ clang/test/Analysis/lifetime-bound.cpp
@@ -1,7 +1,5 @@
 // RUN: %clang_analyze_cc1 -analyzer-checker=core,alpha.cplusplus.UseAfterLifetimeEnd,debug.DebugLifetimeModeling \
 // RUN:   -analyzer-config cfg-lifetime=true -analyzer-output=text -verify %s
-// RUN: %clang_analyze_cc1 -analyzer-checker=core,alpha.cplusplus.UseAfterLifetimeEnd,debug.DebugLifetimeModeling \
-// RUN:   -analyzer-config c++-container-inlining=false -analyzer-config cfg-lifetime=true -analyzer-output=text -verify %s
 
 struct A {};
 
