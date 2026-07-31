8125e8eea41b6b226f4334255a054a759b2b6d57
Timothy Smith <timothy.smith@hpe.com>
2026-07-31 11:44:51 -0500

    Fix test mod

--- flang/test/Driver/compress-debug-sections.f90
+++ flang/test/Driver/compress-debug-sections.f90
@@ -12,8 +12,7 @@
 ! GZ-NONE-SAME: "--compress-debug-sections=none"
 
 ! RUN: not %flang -### -c -target x86_64-unknown-linux-gnu -g -gz=invalid %s 2>&1 | FileCheck %s --check-prefix=GZ-INVALID
-! GZ-INVALID: "-fc1"
-! GZ-INVALID-SAME: error: unsupported argument 'invalid' to option '-gz='
+! GZ-INVALID: error: unsupported argument 'invalid' to option '-gz='
 
 ! Test that -gz without -g still passes --compress-debug-sections to fc1.
 ! RUN: %flang -### -c -target x86_64-unknown-linux-gnu -gz=zlib %s 2>&1 | FileCheck %s --check-prefix=GZ-NO-G
