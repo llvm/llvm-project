cae29ae2430d06f380faeae112fa0d78f0242657
pandaaured <118323804+pandaaured@users.noreply.github.com>
2026-07-31 18:52:41 -0400

    [BoundsSafety][NFC] Add test for VisitTypedefType visiting
    With an implicit __single applied to a pointer type from a parameter
    decl, execution does not reach VisitTypedefType in -fbounds-safety.
    Adding function with another typedef to cover this case.
    
    (cherry picked from commit b0665fbb25b5b4620269eb83c47936022b8bdce8)

---
+++ clang/test/BoundsSafety/AST/typedef-function-desugar.c
@@ -0,0 +1,18 @@
+// RUN: %clang_cc1 -ast-dump -ast-dump-filter bar -fbounds-safety %s | FileCheck %s --check-prefix=FBOUNDS
+// RUN: %clang_cc1 -ast-dump -ast-dump-filter bar -fexperimental-bounds-safety-attributes %s | FileCheck %s --check-prefix=FATTR_ONLY
+
+#include <ptrcheck.h>
+
+typedef int * __single _Nonnull baz_t;
+
+void bar(int len, baz_t __counted_by(len) p);
+
+// FBOUNDS:      FunctionDecl {{.*}} bar 'void (int, int *__single __counted_by(len))'
+// FBOUNDS-NEXT: ParmVarDecl {{.*}} len 'int'
+// FBOUNDS-NEXT: DependerDeclsAttr
+// FBOUNDS-NEXT: ParmVarDecl {{.*}} p 'int *__single __counted_by(len)':'int *__single'
+
+// FATTR_ONLY:      FunctionDecl {{.*}} bar 'void (int, int * __counted_by(len))'
+// FATTR_ONLY-NEXT: ParmVarDecl {{.*}} len 'int'
+// FATTR_ONLY-NEXT: DependerDeclsAttr
+// FATTR_ONLY-NEXT: ParmVarDecl {{.*}} p 'int * __counted_by(len)':'int *'
