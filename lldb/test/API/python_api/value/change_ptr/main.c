7bd061ca17554bba3fdda45e5ab38885ff5c70bf
Igor Kudrin <ikudrin@accesssoftek.com>
2026-07-31 00:20:33 -0700

    fixup! Add more tests

--- lldb/test/API/python_api/value/change_ptr/main.c
+++ lldb/test/API/python_api/value/change_ptr/main.c
@@ -1,7 +1,28 @@
-int main() {
+int test1() {
   int a = 5;
   int b = 7;
   int *p = &a;
-  p = &b; // break here
+  p = &b; // break here 1
+  return *p;
+}
+
+char test2() {
+  struct S {char a; char b; };
+  struct S arr[2] = {{'a', 'b'}, {'c', 'd'}};
+  struct S *p = arr;
+  ++p; // break here 2
+  return p->b;
+}
+
+int test3() {
+  int a = 5;
+  int b = 7;
+  return a + b; // break here 3
+}
+
+int main() {
+  test1();
+  test2();
+  test3();
   return 0;
 }
