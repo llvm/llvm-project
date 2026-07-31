00e7c3564c2ae34d4ac2c6aaa838ac917472ed23
Ayokunle Amodu <ayokunle321@gmail.com>
2026-07-31 14:57:40 -0400

    [CIR][AMDGPU] Add support for AMDGCN tanh builtins (#197852)
    Adds codegen for the following AMDGCN tanh builtins:
    
    - __builtin_amdgcn_tanhf (float)
    - __builtin_amdgcn_tanhh (half)
    - __builtin_amdgcn_tanh_bf16 (bfloat16)
    
    These are lowered to the corresponding `llvm.amdgcn.tanh` intrinsic.

--- clang/lib/CIR/CodeGen/CIRGenBuiltinAMDGPU.cpp
+++ clang/lib/CIR/CodeGen/CIRGenBuiltinAMDGPU.cpp
@@ -358,10 +358,7 @@ CIRGenFunction::emitAMDGPUBuiltinExpr(unsigned builtinId,
   case AMDGPU::BI__builtin_amdgcn_tanhf:
   case AMDGPU::BI__builtin_amdgcn_tanhh:
   case AMDGPU::BI__builtin_amdgcn_tanh_bf16: {
-    cgm.errorNYI(expr->getSourceRange(),
-                 std::string("unimplemented AMDGPU builtin call: ") +
-                     getContext().BuiltinInfo.getName(builtinId));
-    return mlir::Value{};
+    return emitBuiltinWithOneOverloadedType<1>(expr, "amdgcn.tanh").getValue();
   }
   case AMDGPU::BI__builtin_amdgcn_uicmp:
   case AMDGPU::BI__builtin_amdgcn_uicmpl:
