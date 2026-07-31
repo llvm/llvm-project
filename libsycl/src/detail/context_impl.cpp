b8c8d23eab43a45b59c3a962782ea96d9bcef984
Tikhomirova, Kseniya <kseniya.tikhomirova@intel.com>
2026-07-31 04:21:52 -0700

    [libsycl] Add liboffload context handler to default context
    Signed-off-by: Tikhomirova, Kseniya <kseniya.tikhomirova@intel.com>

--- libsycl/src/detail/context_impl.cpp
+++ libsycl/src/detail/context_impl.cpp
@@ -13,15 +13,46 @@ _LIBSYCL_BEGIN_NAMESPACE_SYCL
 
 namespace detail {
 
-void ContextImpl::iterateDevices(
-    const std::function<void(DeviceImpl *)> &callback) const {
-  // TODO: Intentionally don't store devices in context now. This class should
-  // be reimplemented once liboffload adds context support. Treat context as
-  // default context that is associated with all devices in the platform.
-  return MPlatform.iterateDevices(info::device_type::all, callback);
+ContextImpl::ContextImpl(std::vector<DeviceImpl *> &&DeviceList,
+                         const async_handler &AsyncHandler,
+                         const property_list &PropList, Private)
+    : MAsyncHandler(AsyncHandler), MDevices(DeviceList) {
+  (void)PropList;
+
+  assert(!MDevices.empty() && "Device list must not be empty");
+  const PlatformImpl &RefPlatform = MDevices[0]->getPlatformImpl();
+
+  std::vector<ol_device_handle_t> DeviceIds;
+  DeviceIds.reserve(MDevices.size());
+  for (DeviceImpl *D : MDevices) {
+    assert(D && "Device list must not contain null entries");
+    if (D->getPlatformImpl().getOLHandleRef() != RefPlatform.getOLHandleRef())
+      throw exception(
+          make_error_code(errc::invalid),
+          "Can't add devices across platforms to a single context.");
+    DeviceIds.push_back(D->getOLHandle());
+  }
+
+  callAndThrow(olCreateContext, DeviceIds.size(), DeviceIds.data(),
+               &MOffloadContext);
 }
 
-backend ContextImpl::getBackend() const { return MPlatform.getBackend(); }
+ContextImpl::~ContextImpl() {
+  assert(MOffloadContext && "Context must be created in ctor");
+  std::ignore = olDestroyContext(MOffloadContext);
+}
+
+PlatformImpl &ContextImpl::getPlatformImpl() const {
+  return MDevices[0]->getPlatformImpl();
+}
+
+void ContextImpl::iterateDevices(
+    const std::function<void(DeviceImpl *)> &callback) const {
+  for (DeviceImpl *Device : MDevices)
+    callback(Device);
+}
+
+backend ContextImpl::getBackend() const { return MDevices[0]->getBackend(); }
 
 } // namespace detail
 _LIBSYCL_END_NAMESPACE_SYCL
