302786eb8f520f0fe9d1230d6b9bedfc124bc820
Rainer Orth <ro@gcc.gnu.org>
2026-08-02 13:14:40 +0200

    Allow for Windows path separator. Add install_tree_with_libcxx for reproducability.

--- clang/test/Driver/netbsd.cpp
+++ clang/test/Driver/netbsd.cpp
@@ -193,11 +193,11 @@
 
 // Test that NetBSD prefers install tree libc++ headers over system ones.
 // RUN: %clang -### %s --target=x86_64-unknown-netbsd -r 2>&1 \
-// RUN:     -ccc-install-dir %S/Inputs/basic_baremetal_tree/bin \
+// RUN:     -ccc-install-dir %S/Inputs/install_tree_with_libcxx/bin \
 // RUN:     --sysroot=%S/Inputs/basic_netbsd_tree \
 // RUN:   | FileCheck %s --check-prefix=DRIVER-INSTALL-INCLUDES
-// DRIVER-INSTALL-INCLUDES:      "-internal-isystem" "{{.*}}bin/../include/c++/v1"
-// DRIVER-INSTALL-INCLUDES-NOT:  "-internal-isystem" "{{.*}}/usr/include/c++/v1"
+// DRIVER-INSTALL-INCLUDES:      "-internal-isystem" "{{.*}}bin[[SEP:/|\\\\]]..[[SEP]]include[[SEP]]c++[[SEP]]v1"
+// DRIVER-INSTALL-INCLUDES-NOT:  "-internal-isystem" "{{.*}}usr[[SEP]]include[[SEP]]c++[[SEP]]v1"
 
 // Test NetBSD with libstdc++ when the sysroot path ends with `/`.
 // RUN: %clangxx -### %s 2>&1 \
