4070ed8855149143ab77940cadafcd62c75e99c3
Folkert de Vries <folkert@folkertdev.nl>
2026-08-03 01:32:55 +0200

    remove special-casing of _Complex ppcfp128, it is not needed

--- clang/test/CodeGen/PowerPC/powerpc-c99complex.c
+++ clang/test/CodeGen/PowerPC/powerpc-c99complex.c
@@ -77,12 +77,12 @@ _Complex long double foo3(_Complex long double x) {
 // PPC32LNX-CLANG22-NEXT:   store ppc_fp128 %{{.*}}, ptr [[RETREAL]], align 16
 // PPC32LNX-CLANG22-NEXT:   store ppc_fp128 %{{.*}}, ptr [[RETIMAG]], align 16
 
-// PPC32LNX-CLANG23-LABEL: define dso_local [2 x i128] @foo3(
-// PPC32LNX-CLANG23-SAME: [2 x i128] noundef [[X_COERCE:%.*]]) #[[ATTR0]] {
+// PPC32LNX-CLANG23-LABEL: define dso_local [8 x i32] @foo3(
+// PPC32LNX-CLANG23-SAME: [8 x i32] noundef [[X_COERCE:%.*]]) #[[ATTR0]] {
 // PPC32LNX-CLANG23-NEXT:  [[ENTRY:.*:]]
 // PPC32LNX-CLANG23-NEXT:    [[RETVAL:%.*]] = alloca { ppc_fp128, ppc_fp128 }, align 16
 // PPC32LNX-CLANG23-NEXT:    [[X:%.*]] = alloca { ppc_fp128, ppc_fp128 }, align 16
-// PPC32LNX-CLANG23-NEXT:    store [2 x i128] [[X_COERCE]], ptr [[X]], align 16
+// PPC32LNX-CLANG23-NEXT:    store [8 x i32] [[X_COERCE]], ptr [[X]], align 16
 // PPC32LNX-CLANG23-NEXT:    [[X_REALP:%.*]] = getelementptr inbounds nuw { ppc_fp128, ppc_fp128 }, ptr [[X]], i32 0, i32 0
 // PPC32LNX-CLANG23-NEXT:    [[X_REAL:%.*]] = load ppc_fp128, ptr [[X_REALP]], align 16
 // PPC32LNX-CLANG23-NEXT:    [[X_IMAGP:%.*]] = getelementptr inbounds nuw { ppc_fp128, ppc_fp128 }, ptr [[X]], i32 0, i32 1
@@ -91,6 +91,6 @@ _Complex long double foo3(_Complex long double x) {
 // PPC32LNX-CLANG23-NEXT:    [[RETVAL_IMAGP:%.*]] = getelementptr inbounds nuw { ppc_fp128, ppc_fp128 }, ptr [[RETVAL]], i32 0, i32 1
 // PPC32LNX-CLANG23-NEXT:    store ppc_fp128 [[X_REAL]], ptr [[RETVAL_REALP]], align 16
 // PPC32LNX-CLANG23-NEXT:    store ppc_fp128 [[X_IMAG]], ptr [[RETVAL_IMAGP]], align 16
-// PPC32LNX-CLANG23-NEXT:    [[TMP0:%.*]] = load [2 x i128], ptr [[RETVAL]], align 16
-// PPC32LNX-CLANG23-NEXT:    ret [2 x i128] [[TMP0]]
+// PPC32LNX-CLANG23-NEXT:    [[TMP0:%.*]] = load [8 x i32], ptr [[RETVAL]], align 16
+// PPC32LNX-CLANG23-NEXT:    ret [8 x i32] [[TMP0]]
 }
