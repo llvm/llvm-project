897b6d69c56839ca05c2172a11f67e07ce5d4091
Thrrreeeee <1379998393@qq.com>
2026-08-03 16:00:45 +0800

    [BOLT][RISCV][NFC] Document atomic-add operand order
    Clarify that RISC-V AMO operands are emitted as destination, value, and address registers.

--- bolt/lib/Target/RISCV/RISCVMCPlusBuilder.cpp
+++ bolt/lib/Target/RISCV/RISCVMCPlusBuilder.cpp
@@ -1059,6 +1059,7 @@ public:
 
   void atomicAdd(MCInst &Inst, MCPhysReg RegAtomic, MCPhysReg RegTo,
                  MCPhysReg RegCnt) const {
+    // AMO operands are ordered as rd, rs2 (value), rs1 (address).
     Inst = MCInstBuilder(atomicAddOpc())
                .addReg(RegAtomic)
                .addReg(RegCnt)
