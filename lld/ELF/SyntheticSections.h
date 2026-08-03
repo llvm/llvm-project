a6dd7f056df351b63a34da6f440e6563c786ff19
Daniil Kovalev <dkovalev@accesssoftek.com>
2026-08-03 13:40:39 +0300

    [PAC][lld] Do not emit AUTH relocs against undef weak non-preemptible symbols
    Undefined weak non-preemptible symbols should be statically resolved
    and not signed.
    
    See the corresponding relaxation described in docs:
    https://github.com/ARM-software/abi-aa/blob/6e0d6611ac977628af7b2444ff841e76931a3557/design-documents/pauthabi-tls.rst
    
    Previously, a dynamic relocation against such symbols was emitted,
    which is not a correct behavior.
    
    See also: https://github.com/ARM-software/abi-aa/pull/391
    
    Depends on: #198327
    
    Resolves #173296

--- lld/ELF/SyntheticSections.h
+++ lld/ELF/SyntheticSections.h
@@ -122,7 +122,7 @@ public:
   void addEntry(const Symbol &sym);
   void addAuthEntry(const Symbol &sym);
   bool addTlsDescEntry(const Symbol &sym);
-  void addTlsDescAuthEntry();
+  void addTlsDescAuthEntry(const Symbol &sym);
   bool addDynTlsEntry(const Symbol &sym);
   bool addTlsIndex();
   uint32_t getTlsDescOffset(const Symbol &sym) const;
@@ -143,6 +143,7 @@ protected:
   struct AuthEntryInfo {
     size_t offset;
     bool isSymbolFunc;
+    bool isUndefWeakNonPreemptible;
   };
   SmallVector<AuthEntryInfo, 0> authEntries;
 };
