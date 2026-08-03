02662c25923f8c1166835acfc2a41267858ee0a2
Louis Dionne <ldionne.2@gmail.com>
2026-08-03 12:42:47 -0400

    Create per-target modulemap

--- libcxx/test/libcxx/headers_in_modulemap.sh.py
+++ libcxx/test/libcxx/headers_in_modulemap.sh.py
@@ -1,13 +1,18 @@
-# RUN: %{python} %s %{libcxx-dir}/utils %{include-dir}
+# RUN: %{python} %s %{libcxx-dir}/utils %{include-dir} %{target-include-dir}
 
 import pathlib
 import sys
 sys.path.append(sys.argv[1])
 from libcxx.header_information import all_headers
 
+# Check both the generic modulemap and the target-specific modulemap, if there is one.
 include = pathlib.Path(sys.argv[2])
+target_include = pathlib.Path(sys.argv[3])
 with open(include / "module.modulemap") as f:
     modulemap = f.read()
+if target_include.resolve() != include.resolve():
+    with open(target_include / "module.modulemap") as f:
+        modulemap += f.read()
 
 isHeaderMissing = False
 for header in all_headers:
--- libcxx/utils/libcxx/header_information.py
+++ libcxx/utils/libcxx/header_information.py
@@ -15,6 +15,7 @@ def _is_header_file(file):
     """Returns whether the given file is a header file, i.e. not a directory or the modulemap file."""
     return not file.is_dir() and not file.name in [
         "module.modulemap.in",
+        "target.modulemap.in",
         "CMakeLists.txt",
         "libcxx.imp",
         "__config_site.in",
