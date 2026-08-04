ff9ffb00e30bc2ada13833c6ec833aa2b96a3744
Chi-Chun, Chen <chichun.chen@hpe.com>
2026-08-04 16:53:18 -0500

    Apply clang-format to iterator lowering

--- flang/lib/Lower/OpenMP/ClauseProcessor.cpp
+++ flang/lib/Lower/OpenMP/ClauseProcessor.cpp
@@ -2149,8 +2149,7 @@ bool ClauseProcessor::processMap(
       // extent and direction. Reject this until omp.target can represent those
       // captures separately from runtime map entries.
       if (directive == llvm::omp::Directive::OMPD_target)
-        TODO(clauseLocation,
-             "TARGET construct with MAP iterator modifier");
+        TODO(clauseLocation, "TARGET construct with MAP iterator modifier");
 
       llvm::SmallVector<IteratorRange> iteratorRanges;
       llvm::SmallPtrSet<const Fortran::semantics::Symbol *, 4> ivSyms;
