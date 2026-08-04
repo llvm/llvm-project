a5a9446eaf6615ce3c41c4f2c0996b40681b2e1e
prabhukr <prabhukr@google.com>
2026-08-04 15:54:22 -0700

    Only set float abi from hard/soft cmake files.

--- libc/cmake/caches/armv8m.main-none-eabi-hard.cmake
+++ libc/cmake/caches/armv8m.main-none-eabi-hard.cmake
@@ -1,8 +1,2 @@
-set(CMAKE_SYSTEM_PROCESSOR arm CACHE STRING "")
-set(RUNTIMES_TARGET_TRIPLE "armv8m.main-none-eabi" CACHE STRING "")
-
-foreach(lang C;CXX;ASM)
-    set(CMAKE_${lang}_FLAGS "-mfloat-abi=hard -march=armv8m.main+fp+dsp -mcpu=cortex-m33" CACHE STRING "")
-endforeach()
-
-include(${CMAKE_CURRENT_LIST_DIR}/baremetal_common.cmake)
+set(LIBC_FLOAT_ABI "hard")
+include(${CMAKE_CURRENT_LIST_DIR}/armv8m.main-none-eabi.cmake)
--- libc/cmake/caches/armv8m.main-none-eabi.cmake
+++ libc/cmake/caches/armv8m.main-none-eabi.cmake
@@ -1,1 +1,12 @@
-include(${CMAKE_CURRENT_LIST_DIR}/armv8m.main-none-eabi-softfp.cmake)
+set(CMAKE_SYSTEM_PROCESSOR arm CACHE STRING "")
+set(RUNTIMES_TARGET_TRIPLE "armv8m.main-none-eabi" CACHE STRING "")
+
+if(NOT DEFINED LIBC_FLOAT_ABI)
+  set(LIBC_FLOAT_ABI "softfp")
+endif()
+
+foreach(lang C;CXX;ASM)
+    set(CMAKE_${lang}_FLAGS "-mfloat-abi=${LIBC_FLOAT_ABI} -march=armv8m.main+fp+dsp -mcpu=cortex-m33" CACHE STRING "")
+endforeach()
+
+include(${CMAKE_CURRENT_LIST_DIR}/baremetal_common.cmake)
