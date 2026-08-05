b1e3ba9762dc0e1f7827113d4e5678b8aeefba66
Julian Lettner <jlettner@apple.com>
2026-08-05 16:02:53 -0700

    [lldb][Darwin] Remove libsanitizers ASan integration
    The OS-provided ASan runtime (in libsanitizers)
    was removed on Darwin, so remove LLDB's support
    for it.
    
    rdar://177086333

--- lldb/include/lldb/lldb-enumerations.h
+++ lldb/include/lldb/lldb-enumerations.h
@@ -661,7 +661,7 @@ enum InstrumentationRuntimeType {
   eInstrumentationRuntimeTypeUndefinedBehaviorSanitizer = 0x0002,
   eInstrumentationRuntimeTypeMainThreadChecker = 0x0003,
   eInstrumentationRuntimeTypeSwiftRuntimeReporting = 0x0004,
-  eInstrumentationRuntimeTypeLibsanitizersAsan = 0x0005,
+  eInstrumentationRuntimeTypeUnused = 0x0005,
   eInstrumentationRuntimeTypeBoundsSafety = 0x0006,
   eNumInstrumentationRuntimeTypes
 };
