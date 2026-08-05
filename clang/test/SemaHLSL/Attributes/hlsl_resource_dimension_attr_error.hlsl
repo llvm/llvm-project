bb0d9b6726123650b904a3f7d648cf68fedfc335
Justin Bogner <mail@justinbogner.com>
2026-08-04 17:14:22 -0700

    [HLSL] Consistently quote resource attribute args
    The `hlsl::resource_class` and `hlsl::dimension` attributes both take a
    single argument from a set of choices, but resource_class expects an
    unevaluated identifier and dimension expects a string literal.
    Consistently require the literal for both, and fix up the AST printers
    to match.

--- clang/test/SemaHLSL/Attributes/hlsl_resource_dimension_attr_error.hlsl
+++ clang/test/SemaHLSL/Attributes/hlsl_resource_dimension_attr_error.hlsl
@@ -6,7 +6,7 @@
 // expected-error@+1{{'hlsl::dimension' attribute takes one argument}}
 __hlsl_resource_t [[hlsl::dimension()]] e1;
 
-// expected-error@+1{{expected string literal as argument of 'dimension' attribute}}
+// expected-error@+1{{'hlsl::dimension' attribute requires a string}}
 __hlsl_resource_t [[hlsl::dimension(2)]] e2;
 
 // expected-warning@+1{{ResourceDimension attribute argument not supported: gibberish}}
