llvm/llvm-project

commit 56dc58dc2550fa23f4c812d28c30d3d34de2e772

[Clang][Preprocessor] Unify header-name lookahead for import and include (#191004)
Introduce Preprocessor::isNextPPTokenHeaderNameOrOneOf to centralize
lookahead logic for header-name formation and token classification under
ParsingFilename mode.
    
Refactor handling of C++20 module/import contextual keywords and
LexHeaderName to use the new helper, ensuring consistent behavior
between `import` and `#include`.
    
Try to form a valid header-name token during lookahead. If that fails
and the next token is not one of the expected alternatives, treat it as
not an `import` directive or as an invalid `#include` (missing
<FILENAME>/"FILENAME").

```cpp    
#define FOO foo>
#include <:FOO
```
Now such cases are rejected as expected.
    
Also adjusts peekNextPPToken to properly support dependency directive
lexers.
    
No functional change intended for valid code; improves correctness and
consistency in edge cases involving header-name lexing.

Fixes https://github.com/llvm/llvm-project/issues/190693.

---------

Signed-off-by: yronglin <yronglin777@gmail.com>
Signed-off-by: Yihan Wang <yronglin777@gmail.com>

clang/lib/Lex/Lexer.cpp
@@ -2466,6 +2466,8 @@ bool Lexer::LexRawStringLiteral(Token &Result, const char *CurPtr,
 
 /// LexAngledStringLiteral - Lex the remainder of an angled string literal,
 /// after having lexed the '<' character.  This is used for #include filenames.
+/// Returns false if failed to lex the angled string literal; so the caller can
+/// lex the '<' normally.
 bool Lexer::LexAngledStringLiteral(Token &Result, const char *CurPtr) {
   // Does this string contain the \0 character?
   const char *NulCharacter = nullptr;
@@ -2479,10 +2481,8 @@ bool Lexer::LexAngledStringLiteral(Token &Result, const char *CurPtr) {
 
     if (isVerticalWhitespace(C) ||               // Newline.
         (C == 0 && (CurPtr - 1 == BufferEnd))) { // End of file.
-      // If the filename is unterminated, then it must just be a lone <
-      // character.  Return this as such.
-      FormTokenWithChars(Result, AfterLessPos, tok::less);
-      return true;
+      // If the filename is unterminated, let the caller lex the '<' normally.
+      return false;
     }
 
     if (C == 0) {
@@ -4348,9 +4348,10 @@ LexStart:
     break;
   case '<':
     Char = getCharAndSize(CurPtr, SizeTmp);
-    if (ParsingFilename) {
-      return LexAngledStringLiteral(Result, CurPtr);
-    } else if (Char == '<') {
+    if (ParsingFilename && LexAngledStringLiteral(Result, CurPtr))
+      return true;
+
+    if (Char == '<') {
       char After = getCharAndSize(CurPtr+SizeTmp, SizeTmp2);
       if (After == '=') {
         Kind = tok::lesslessequal;
@@ -4393,7 +4394,7 @@ LexStart:
       }
       CurPtr = ConsumeChar(CurPtr, SizeTmp, Result);
       Kind = tok::lessequal;
-    } else if (LangOpts.Digraphs && Char == ':') {     // '<:' -> '['
+    } else if (LangOpts.Digraphs && Char == ':') { // '<:' -> '['
       if (LangOpts.CPlusPlus11 &&
           getCharAndSize(CurPtr + SizeTmp, SizeTmp2) == ':') {
         // C++0x [lex.pptoken]p3:
@@ -4413,7 +4414,7 @@ LexStart:
 
       CurPtr = ConsumeChar(CurPtr, SizeTmp, Result);
       Kind = tok::l_square;
-    } else if (LangOpts.Digraphs && Char == '%') {     // '<%' -> '{'
+    } else if (LangOpts.Digraphs && Char == '%') { // '<%' -> '{'
       CurPtr = ConsumeChar(CurPtr, SizeTmp, Result);
       Kind = tok::l_brace;
     } else if (Char == '#' && /*Not a trigraph*/ SizeTmp == 1 &&
@@ -4695,12 +4696,20 @@ bool Lexer::LexDependencyDirectiveToken(Token &Result) {
     MIOpt.ReadToken();
   }
 
-  if (ParsingFilename && DDTok.is(tok::less)) {
-    BufferPtr = BufferStart + DDTok.Offset;
-    LexAngledStringLiteral(Result, BufferPtr + 1);
-    if (Result.isNot(tok::header_name))
+  const char *DDTokPtr = BufferStart + DDTok.Offset;
+  if (ParsingFilename && *DDTokPtr == '<') {
+    Result.startToken();
+    Result.setFlag((Token::TokenFlags)DDTok.Flags);
+    Result.clearFlag(Token::NeedsCleaning);
+    BufferPtr = DDTokPtr;
+    if (!LexAngledStringLiteral(Result, BufferPtr + 1)) {
+      convertDependencyDirectiveToken(DDTok, Result);
       return true;
+    }
+
     // Advance the index of lexed tokens.
+    // FIXME: This will skip too many tokens if the header-name ended in the
+    // middle of a token, such as in '<foo>='.
     while (true) {
       const dependency_directives_scan::Token &NextTok =
           DepDirectives.front().Tokens[NextDepDirectiveTokenIndex];

clang/lib/Lex/Preprocessor.cpp
@@ -1360,32 +1360,33 @@ bool Preprocessor::HandleModuleContextualKeyword(Token &Result) {
   llvm::SaveAndRestore<bool> SavedParsingPreprocessorDirective(
       CurPPLexer->ParsingPreprocessorDirective, true);
 
-  // The next token may be an angled string literal after import keyword.
-  llvm::SaveAndRestore<bool> SavedParsingFilemame(
-      CurPPLexer->ParsingFilename,
-      Result.getIdentifierInfo()->isImportKeyword());
-
-  std::optional<Token> NextTok = peekNextPPToken();
-  if (!NextTok)
-    return false;
-
-  if (NextTok->is(tok::raw_identifier))
-    LookUpIdentifierInfo(*NextTok);
-
-  if (Result.getIdentifierInfo()->isImportKeyword()) {
-    if (NextTok->isOneOf(tok::identifier, tok::less, tok::colon,
-                         tok::header_name)) {
-      Result.setKind(tok::kw_import);
-      ModuleImportLoc = Result.getLocation();
-      return true;
+  if (II->isModuleKeyword()) {
+    if (auto NextTok = peekNextPPToken()) {
+      if (NextTok->is(tok::raw_identifier))
+        LookUpIdentifierInfo(*NextTok);
+      if (NextTok->isOneOf(tok::identifier, tok::colon, tok::semi)) {
+        Result.setKind(tok::kw_module);
+        ModuleDeclLoc = Result.getLocation();
+        return true;
+      }
     }
+    return false;
   }
 
-  if (Result.getIdentifierInfo()->isModuleKeyword() &&
-      NextTok->isOneOf(tok::identifier, tok::colon, tok::semi)) {
-    Result.setKind(tok::kw_module);
-    ModuleDeclLoc = Result.getLocation();
-    return true;
+  if (II->isImportKeyword()) {
+    llvm::SaveAndRestore<bool> SavedParsingFilename(CurPPLexer->ParsingFilename,
+                                                    true);
+    if (auto NextTok = peekNextPPToken()) {
+      if (NextTok->is(tok::raw_identifier))
+        LookUpIdentifierInfo(*NextTok);
+      if (NextTok->isOneOf(tok::header_name, tok::identifier, tok::colon,
+                           tok::less)) {
+        Result.setKind(tok::kw_import);
+        ModuleImportLoc = Result.getLocation();
+        return true;
+      }
+    }
+    return false;
   }
 
   // Ok, it's an identifier.

clang/lib/Parse/Parser.cpp
@@ -2506,6 +2506,16 @@ Decl *Parser::ParseModuleImport(SourceLocation AtLoc,
     break;
   }
 
+  // FIXME: If the previous token is tok::header_name like the following:
+  //
+  //  import <%%>
+  //
+  // The diagnostic location is incorrect.
+  //
+  //  <source file>:1:10: error: import directive must end with a ';'
+  //   1 | import <%%>
+  //     |          ^
+  //     |          ;
   bool LexedSemi = false;
   if (getLangOpts().CPlusPlusModules)
     LexedSemi =

clang/test/CXX/cpp/cpp.include/p3.cpp
@@ -1,0 +1,5 @@
+// RUN: %clang_cc1 %s -fsyntax-only -verify
+
+#define FOO foo>
+#include <:FOO
+// expected-error@-1 {{expected "FILENAME" or <FILENAME>}}

clang/test/CXX/module/cpp.pre/p1.cpp
@@ -38,8 +38,12 @@
 // RUN: %clang_cc1 -std=c++20 %t/func_like_macro.cpp -D'm(x)=x' -fsyntax-only -verify
 // RUN: %clang_cc1 -std=c++20 %t/lparen.cpp -D'm(x)=x' -D'LPAREN=(' -fsyntax-only -verify
 // RUN: %clang_cc1 -std=c++20 %t/control_line.cpp -fsyntax-only -verify
-
-
+// RUN: %clang_cc1 -std=c++20 %t/header_name1.cpp -fsyntax-only -verify
+// RUN: %clang_cc1 -std=c++20 %t/header_name2.cpp -fsyntax-only -verify
+// RUN: %clang_cc1 -std=c++20 %t/header_name3.cpp -fsyntax-only -verify
+// RUN: %clang_cc1 -std=c++20 %t/header_name4.cpp -fsyntax-only -verify
+// RUN: %clang_cc1 -std=c++20 %t/header_name5.cpp -fsyntax-only -verify
+// RUN: %clang_cc1 -std=c++20 %t/header_name6.cpp -fsyntax-only -verify
 //--- hash.cpp
 // expected-no-diagnostics
 #                       // preprocessing directive
@@ -205,3 +209,48 @@ export module m; // expected-error {{module directive lines are not allowed on l
                  // expected-error {{module declaration must occur at the start of the translation unit}} \
                  // expected-note@#1 {{add 'module;'}}
 #endif
+
+//--- header_name1.cpp
+// expected-no-diagnostics
+int
+import <:10
+:>;
+
+void foo() {
+    for (int i = 0; i < 10; ++i)
+        import[i] = i;
+}
+
+//--- header_name2.cpp
+// expected-no-diagnostics
+using import = int;
+
+void bar(int);
+
+void foo(int val =
+import <%
+%>
+) {
+   bar(val);
+}
+
+//--- header_name3.cpp
+export module M;
+import <%%>; // expected-error {{'%%' file not found}}
+
+//--- header_name4.cpp
+export module M;
+import <::>; // expected-error {{'::' file not found}}
+
+//--- header_name5.cpp
+export module M;
+#define FOO foo>;
+import <:FOO
+// expected-error@-1 {{use of undeclared identifier 'foo'}}
+// expected-error@-2 {{a type specifier is required for all declarations}}
+// expected-error@-3 {{expected expression}}
+
+//--- header_name6.cpp
+export module M;
+#define HEADER vector>
+import <HEADER; // expected-error {{file not found}}

clang/unittests/Lex/PPDependencyDirectivesTest.cpp
@@ -96,6 +96,35 @@ public:
   }
 };
 
+struct DepDirectives {
+  SmallVector<dependency_directives_scan::Token> Tokens;
+  SmallVector<dependency_directives_scan::Directive> Directives;
+};
+
+class TestDependencyDirectivesGetter : public DependencyDirectivesGetter {
+  FileManager &FileMgr;
+  SmallVector<std::unique_ptr<DepDirectives>> DepDirectivesObjects;
+
+public:
+  TestDependencyDirectivesGetter(FileManager &FileMgr) : FileMgr(FileMgr) {}
+
+  std::unique_ptr<DependencyDirectivesGetter>
+  cloneFor(FileManager &FileMgr) override {
+    return std::make_unique<TestDependencyDirectivesGetter>(FileMgr);
+  }
+
+  std::optional<ArrayRef<dependency_directives_scan::Directive>>
+  operator()(FileEntryRef File) override {
+    DepDirectivesObjects.push_back(std::make_unique<DepDirectives>());
+    StringRef Input = (*FileMgr.getBufferForFile(File))->getBuffer();
+    bool Err = scanSourceForDependencyDirectives(
+        Input, DepDirectivesObjects.back()->Tokens,
+        DepDirectivesObjects.back()->Directives);
+    EXPECT_FALSE(Err);
+    return DepDirectivesObjects.back()->Directives;
+  }
+};
+
 TEST_F(PPDependencyDirectivesTest, MacroGuard) {
   // "head1.h" has a macro guard and should only be included once.
   // "head2.h" and "head3.h" have tokens following the macro check, they should
@@ -126,34 +155,6 @@ TEST_F(PPDependencyDirectivesTest, MacroGuard) {
   SourceMgr.setMainFileID(
       SourceMgr.createFileID(*FE, SourceLocation(), SrcMgr::C_User));
 
-  struct DepDirectives {
-    SmallVector<dependency_directives_scan::Token> Tokens;
-    SmallVector<dependency_directives_scan::Directive> Directives;
-  };
-
-  class TestDependencyDirectivesGetter : public DependencyDirectivesGetter {
-    FileManager &FileMgr;
-    SmallVector<std::unique_ptr<DepDirectives>> DepDirectivesObjects;
-
-  public:
-    TestDependencyDirectivesGetter(FileManager &FileMgr) : FileMgr(FileMgr) {}
-
-    std::unique_ptr<DependencyDirectivesGetter>
-    cloneFor(FileManager &FileMgr) override {
-      return std::make_unique<TestDependencyDirectivesGetter>(FileMgr);
-    }
-
-    std::optional<ArrayRef<dependency_directives_scan::Directive>>
-    operator()(FileEntryRef File) override {
-      DepDirectivesObjects.push_back(std::make_unique<DepDirectives>());
-      StringRef Input = (*FileMgr.getBufferForFile(File))->getBuffer();
-      bool Err = scanSourceForDependencyDirectives(
-          Input, DepDirectivesObjects.back()->Tokens,
-          DepDirectivesObjects.back()->Directives);
-      EXPECT_FALSE(Err);
-      return DepDirectivesObjects.back()->Directives;
-    }
-  };
   TestDependencyDirectivesGetter GetDependencyDirectives(FileMgr);
 
   PreprocessorOptions PPOpts;
@@ -179,6 +180,77 @@ TEST_F(PPDependencyDirectivesTest, MacroGuard) {
   SmallVector<std::string> ExpectedIncludes{
       "main.c", "./head1.h", "./head2.h", "./head2.h", "./head3.h", "./head3.h",
   };
+  EXPECT_EQ(IncludedFilesSlash, ExpectedIncludes);
+}
+
+TEST_F(PPDependencyDirectivesTest, HeaderNamesStartingWithCompoundTokens) {
+  StringRef Source = "#if __has_include(<=>)\n"
+                     "#include <=>\n"
+                     "#endif\n"
+                     "#if __has_include(<<>)\n"
+                     "#include <<>\n"
+                     "#endif\n"
+                     "#include \\\n<=>";
+
+  SmallVector<dependency_directives_scan::Token> Tokens;
+  SmallVector<dependency_directives_scan::Directive> Directives;
+  ASSERT_FALSE(scanSourceForDependencyDirectives(Source, Tokens, Directives));
+
+  unsigned LessEqualCount = 0;
+  unsigned LessLessCount = 0;
+  for (const dependency_directives_scan::Token &Tok : Tokens) {
+    StringRef Spelling = Source.slice(Tok.Offset, Tok.getEnd());
+    if (Spelling == "<=") {
+      EXPECT_TRUE(Tok.is(tok::lessequal));
+      ++LessEqualCount;
+    } else if (Spelling == "<<") {
+      EXPECT_TRUE(Tok.is(tok::lessless));
+      ++LessLessCount;
+    }
+  }
+  EXPECT_EQ(LessEqualCount, 1u);
+  EXPECT_EQ(LessLessCount, 1u);
+
+  auto VFS = llvm::makeIntrusiveRefCnt<llvm::vfs::InMemoryFileSystem>();
+  VFS->setCurrentWorkingDirectory("/source");
+  VFS->addFile("/source/=", 0, llvm::MemoryBuffer::getMemBuffer(""));
+  VFS->addFile("/source/<", 0, llvm::MemoryBuffer::getMemBuffer(""));
+  VFS->addFile("/source/main.c", 0, llvm::MemoryBuffer::getMemBuffer(Source));
+  FileMgr.setVirtualFileSystem(VFS);
+
+  OptionalFileEntryRef FE;
+  ASSERT_THAT_ERROR(FileMgr.getFileRef("/source/main.c").moveInto(FE),
+                    llvm::Succeeded());
+  SourceMgr.setMainFileID(
+      SourceMgr.createFileID(*FE, SourceLocation(), SrcMgr::C_User));
+
+  TestDependencyDirectivesGetter GetDependencyDirectives(FileMgr);
+  PreprocessorOptions PPOpts;
+  HeaderSearchOptions HSOpts;
+  TrivialModuleLoader ModLoader;
+  HeaderSearch HeaderInfo(HSOpts, SourceMgr, Diags, LangOpts, Target.get());
+  auto DE = FileMgr.getOptionalDirectoryRef("/source");
+  ASSERT_TRUE(DE);
+  HeaderInfo.AddSearchPath(
+      DirectoryLookup(*DE, SrcMgr::C_User, /*isFramework=*/false),
+      /*isAngled=*/true);
+  Preprocessor PP(PPOpts, Diags, LangOpts, SourceMgr, HeaderInfo, ModLoader,
+                  /*IILookup =*/nullptr,
+                  /*OwnsHeaderSearch =*/false);
+  PP.Initialize(*Target);
+  PP.setDependencyDirectivesGetter(GetDependencyDirectives);
+
+  SmallVector<StringRef> IncludedFiles;
+  PP.addPPCallbacks(std::make_unique<IncludeCollector>(PP, IncludedFiles));
+  PP.EnterMainSourceFile();
+  PP.LexTokensUntilEOF();
+
+  SmallVector<std::string> IncludedFilesSlash;
+  for (StringRef IncludedFile : IncludedFiles)
+    IncludedFilesSlash.push_back(
+        llvm::sys::path::convert_to_slash(IncludedFile));
+  SmallVector<std::string> ExpectedIncludes{
+      "/source/main.c", "/source/=", "/source/<", "/source/="};
   EXPECT_EQ(IncludedFilesSlash, ExpectedIncludes);
 }
 

commit d84bdf2b85df934a9395579f57f0084587c44105

[clang][Lex] Fix ambiguous reference to Token error
At least with GCC 11.4.0 this is deemed an error.

Fixes: 56dc58dc2550 ("[Clang][Preprocessor] Unify header-name lookahead for import and include (#191004)")

clang/lib/Lex/Lexer.cpp
@@ -4699,8 +4699,8 @@ bool Lexer::LexDependencyDirectiveToken(Token &Result) {
   const char *DDTokPtr = BufferStart + DDTok.Offset;
   if (ParsingFilename && *DDTokPtr == '<') {
     Result.startToken();
-    Result.setFlag((Token::TokenFlags)DDTok.Flags);
-    Result.clearFlag(Token::NeedsCleaning);
+    Result.setFlag((clang::Token::TokenFlags)DDTok.Flags);
+    Result.clearFlag(clang::Token::NeedsCleaning);
     BufferPtr = DDTokPtr;
     if (!LexAngledStringLiteral(Result, BufferPtr + 1)) {
       convertDependencyDirectiveToken(DDTok, Result);