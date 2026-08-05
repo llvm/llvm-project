87d7851ec2ec28f419d72185a5029e8c9d6dbd14
prabhukr <prabhukr@google.com>
2026-08-05 10:47:05 -0700

    Use hf suffix in target triple to simplify this change.

--- libc/cmake/caches/armv8m.main-none-eabi-hard.cmake
+++
@@ -1,2 +0,0 @@
-set(LIBC_FLOAT_ABI "hard")
-include(${CMAKE_CURRENT_LIST_DIR}/armv8m.main-none-eabi.cmake)
--- libc/cmake/caches/armv8m.main-none-eabi-softfp.cmake
+++
@@ -1,2 +0,0 @@
-set(LIBC_FLOAT_ABI "softfp")
-include(${CMAKE_CURRENT_LIST_DIR}/armv8m.main-none-eabi.cmake)
