llvm/llvm-project

commit 500962298d624a74ade58c2aeb65313fb32dda5a

[InstCombine]Fold fcmp+select to min/max if its zero sign is missing
Look through phis, chained selects and the loop back edge to the
select itself and fold if no use can observe the sign of zero of the
result. This restores the fold for loop-carried and unrolled running
min/max values (fcsel -> fminnm on AArch64) lost to the signed zero
handling fix in matchSelectPattern (#210077).

Reviewers: nikic, dtcxzyw

Pull Request: https://github.com/llvm/llvm-project/pull/213133

llvm/lib/Transforms/InstCombine/InstCombineSelect.cpp
@@ -4511,6 +4511,34 @@ static Instruction *foldSelectNegNot(SelectInst &SI,
   return nullptr;
 }
 
+// Return true if no use can observe the sign of zero of the select result,
+// looking through phis, selects and the loop back edge to the select itself.
+static bool isSelectZeroSignInsignificant(SelectInst &SI) {
+  // Bound the number of uses to look through to keep the compile time in
+  // check.
+  constexpr unsigned MaxUsesToLookThrough = 16;
+  unsigned NumUses = 0;
+  SmallPtrSet<Instruction *, 4> Visited;
+  SmallVector<Instruction *> Worklist(1, &SI);
+  while (!Worklist.empty()) {
+    for (Use &U : Worklist.pop_back_val()->uses()) {
+      if (++NumUses > MaxUsesToLookThrough)
+        return false;
+      auto *User = cast<Instruction>(U.getUser());
+      if (User == &SI)
+        continue;
+      if (isa<PHINode, SelectInst>(User)) {
+        if (Visited.insert(User).second)
+          Worklist.push_back(User);
+        continue;
+      }
+      if (!canIgnoreSignBitOfZero(U))
+        return false;
+    }
+  }
+  return true;
+}
+
 Instruction *InstCombinerImpl::visitSelectInst(SelectInst &SI) {
   Value *CondVal = SI.getCondition();
   Value *TrueVal = SI.getTrueValue();
@@ -4785,9 +4813,7 @@ Instruction *InstCombinerImpl::visitSelectInst(SelectInst &SI) {
     // has the `nnan nsz` flags, which allow it to be lowered *back* to a
     // fcmp+select if that's the best way to express it on the target.
     if (FCmp && FCmp->hasNoNaNs() &&
-        (SIFPOp->hasNoSignedZeros() ||
-         (SIFPOp->hasOneUse() &&
-          canIgnoreSignBitOfZero(*SIFPOp->use_begin())))) {
+        (SIFPOp->hasNoSignedZeros() || isSelectZeroSignInsignificant(SI))) {
       Value *X, *Y;
       if (match(&SI, m_OrdOrUnordFMax(m_Value(X), m_Value(Y)))) {
         Value *BinIntr =
@@ -4798,9 +4824,8 @@ Instruction *InstCombinerImpl::visitSelectInst(SelectInst &SI) {
           BinIntrInst->setHasNoInfs(FCmp->hasNoInfs());
           // The `nsz` flag is a precondition, so let's ensure it's always added
           // to the min/max operation, even if it wasn't on the select. This
-          // could happen if `canIgnoreSignBitOfZero` is true--for instance, if
-          // the select doesn't have `nsz`, but the result is being used in an
-          // operation that doesn't care about signed zero.
+          // could happen if the select doesn't have `nsz`, but no use of the
+          // result can observe the sign of zero.
           BinIntrInst->setHasNoSignedZeros(true);
           // As mentioned above, `nnan` is also a precondition, so we always set
           // the flag.

llvm/test/Transforms/InstCombine/minmax-fp-loop-carried.ll
@@ -1,0 +1,242 @@
+; NOTE: Assertions have been autogenerated by utils/update_test_checks.py
+; RUN: opt -passes=instcombine -S < %s | FileCheck %s
+
+define i32 @loop_carried_min(ptr %xxtrt, ptr %n_ptr) {
+; CHECK-LABEL: @loop_carried_min(
+; CHECK-NEXT:  entry:
+; CHECK-NEXT:    [[N:%.*]] = load i32, ptr [[N_PTR:%.*]], align 4
+; CHECK-NEXT:    [[CMP0:%.*]] = icmp slt i32 [[N]], 2
+; CHECK-NEXT:    br i1 [[CMP0]], label [[EXIT:%.*]], label [[PH:%.*]]
+; CHECK:       ph:
+; CHECK-NEXT:    [[W:%.*]] = zext nneg i32 [[N]] to i64
+; CHECK-NEXT:    [[PM:%.*]] = getelementptr [4 x i8], ptr [[XXTRT:%.*]], i64 [[W]]
+; CHECK-NEXT:    [[VM_PRE:%.*]] = load float, ptr [[PM]], align 4
+; CHECK-NEXT:    br label [[LOOP:%.*]]
+; CHECK:       loop:
+; CHECK-NEXT:    [[VM:%.*]] = phi float [ [[VM_PRE]], [[PH]] ], [ [[MINVAL:%.*]], [[LOOP]] ]
+; CHECK-NEXT:    [[IV:%.*]] = phi i64 [ [[W]], [[PH]] ], [ [[IV_NEXT:%.*]], [[LOOP]] ]
+; CHECK-NEXT:    [[MINIDX:%.*]] = phi i32 [ [[N]], [[PH]] ], [ [[MINSEL:%.*]], [[LOOP]] ]
+; CHECK-NEXT:    [[IV_NEXT]] = add nsw i64 [[IV]], -1
+; CHECK-NEXT:    [[PI:%.*]] = getelementptr [4 x i8], ptr [[XXTRT]], i64 [[IV_NEXT]]
+; CHECK-NEXT:    [[VI:%.*]] = load float, ptr [[PI]], align 4
+; CHECK-NEXT:    [[C:%.*]] = fcmp nnan olt float [[VI]], [[VM]]
+; CHECK-NEXT:    [[T:%.*]] = trunc i64 [[IV_NEXT]] to i32
+; CHECK-NEXT:    [[MINSEL]] = select i1 [[C]], i32 [[T]], i32 [[MINIDX]]
+; CHECK-NEXT:    [[MINVAL]] = call nnan nsz float @llvm.minnum.f32(float [[VI]], float [[VM]])
+; CHECK-NEXT:    [[EC:%.*]] = icmp ugt i64 [[IV]], 2
+; CHECK-NEXT:    br i1 [[EC]], label [[LOOP]], label [[EXIT]]
+; CHECK:       exit:
+; CHECK-NEXT:    [[R:%.*]] = phi i32 [ [[N]], [[ENTRY:%.*]] ], [ [[MINSEL]], [[LOOP]] ]
+; CHECK-NEXT:    ret i32 [[R]]
+;
+entry:
+  %n = load i32, ptr %n_ptr, align 4
+  %start = add nsw i32 %n, -1
+  %cmp0 = icmp slt i32 %start, 1
+  br i1 %cmp0, label %exit, label %ph
+
+ph:
+  %w = zext nneg i32 %n to i64
+  %pm = getelementptr float, ptr %xxtrt, i64 %w
+  %vm.pre = load float, ptr %pm, align 4
+  br label %loop
+
+loop:
+  %vm = phi float [ %vm.pre, %ph ], [ %minval, %loop ]
+  %iv = phi i64 [ %w, %ph ], [ %iv.next, %loop ]
+  %minidx = phi i32 [ %n, %ph ], [ %minsel, %loop ]
+  %iv.next = add nsw i64 %iv, -1
+  %pi = getelementptr float, ptr %xxtrt, i64 %iv.next
+  %vi = load float, ptr %pi, align 4
+  %c = fcmp nnan olt float %vi, %vm
+  %t = trunc i64 %iv.next to i32
+  %minsel = select i1 %c, i32 %t, i32 %minidx
+  %minval = select i1 %c, float %vi, float %vm
+  %ec = icmp ugt i64 %iv, 2
+  br i1 %ec, label %loop, label %exit
+
+exit:
+  %r = phi i32 [ %n, %entry ], [ %minsel, %loop ]
+  ret i32 %r
+}
+
+define float @loop_carried_min_live_out(ptr %xxtrt, ptr %n_ptr) {
+; CHECK-LABEL: @loop_carried_min_live_out(
+; CHECK-NEXT:  entry:
+; CHECK-NEXT:    [[N:%.*]] = load i32, ptr [[N_PTR:%.*]], align 4
+; CHECK-NEXT:    [[CMP0:%.*]] = icmp slt i32 [[N]], 2
+; CHECK-NEXT:    br i1 [[CMP0]], label [[EXIT:%.*]], label [[PH:%.*]]
+; CHECK:       ph:
+; CHECK-NEXT:    [[W:%.*]] = zext nneg i32 [[N]] to i64
+; CHECK-NEXT:    [[PM:%.*]] = getelementptr [4 x i8], ptr [[XXTRT:%.*]], i64 [[W]]
+; CHECK-NEXT:    [[VM_PRE:%.*]] = load float, ptr [[PM]], align 4
+; CHECK-NEXT:    br label [[LOOP:%.*]]
+; CHECK:       loop:
+; CHECK-NEXT:    [[VM:%.*]] = phi float [ [[VM_PRE]], [[PH]] ], [ [[MINVAL:%.*]], [[LOOP]] ]
+; CHECK-NEXT:    [[IV:%.*]] = phi i64 [ [[W]], [[PH]] ], [ [[IV_NEXT:%.*]], [[LOOP]] ]
+; CHECK-NEXT:    [[MINIDX:%.*]] = phi i32 [ [[N]], [[PH]] ], [ [[MINSEL:%.*]], [[LOOP]] ]
+; CHECK-NEXT:    [[IV_NEXT]] = add nsw i64 [[IV]], -1
+; CHECK-NEXT:    [[PI:%.*]] = getelementptr [4 x i8], ptr [[XXTRT]], i64 [[IV_NEXT]]
+; CHECK-NEXT:    [[VI:%.*]] = load float, ptr [[PI]], align 4
+; CHECK-NEXT:    [[C:%.*]] = fcmp nnan olt float [[VI]], [[VM]]
+; CHECK-NEXT:    [[T:%.*]] = trunc i64 [[IV_NEXT]] to i32
+; CHECK-NEXT:    [[MINSEL]] = select i1 [[C]], i32 [[T]], i32 [[MINIDX]]
+; CHECK-NEXT:    [[MINVAL]] = select i1 [[C]], float [[VI]], float [[VM]]
+; CHECK-NEXT:    [[EC:%.*]] = icmp ugt i64 [[IV]], 2
+; CHECK-NEXT:    br i1 [[EC]], label [[LOOP]], label [[EXIT]]
+; CHECK:       exit:
+; CHECK-NEXT:    [[RF:%.*]] = phi float [ 0.000000e+00, [[ENTRY:%.*]] ], [ [[VM]], [[LOOP]] ]
+; CHECK-NEXT:    ret float [[RF]]
+;
+entry:
+  %n = load i32, ptr %n_ptr, align 4
+  %start = add nsw i32 %n, -1
+  %cmp0 = icmp slt i32 %start, 1
+  br i1 %cmp0, label %exit, label %ph
+
+ph:
+  %w = zext nneg i32 %n to i64
+  %pm = getelementptr float, ptr %xxtrt, i64 %w
+  %vm.pre = load float, ptr %pm, align 4
+  br label %loop
+
+loop:
+  %vm = phi float [ %vm.pre, %ph ], [ %minval, %loop ]
+  %iv = phi i64 [ %w, %ph ], [ %iv.next, %loop ]
+  %minidx = phi i32 [ %n, %ph ], [ %minsel, %loop ]
+  %iv.next = add nsw i64 %iv, -1
+  %pi = getelementptr float, ptr %xxtrt, i64 %iv.next
+  %vi = load float, ptr %pi, align 4
+  %c = fcmp nnan olt float %vi, %vm
+  %t = trunc i64 %iv.next to i32
+  %minsel = select i1 %c, i32 %t, i32 %minidx
+  %minval = select i1 %c, float %vi, float %vm
+  %ec = icmp ugt i64 %iv, 2
+  br i1 %ec, label %loop, label %exit
+
+exit:
+  %rf = phi float [ 0.0, %entry ], [ %vm, %loop ]
+  ret float %rf
+}
+
+define i64 @unrolled_chain_min(ptr %p, i64 %n) {
+; CHECK-LABEL: @unrolled_chain_min(
+; CHECK-NEXT:  entry:
+; CHECK-NEXT:    [[V0:%.*]] = load float, ptr [[P:%.*]], align 4
+; CHECK-NEXT:    br label [[LOOP:%.*]]
+; CHECK:       loop:
+; CHECK-NEXT:    [[IV:%.*]] = phi i64 [ 1, [[ENTRY:%.*]] ], [ [[IV_NEXT:%.*]], [[LOOP]] ]
+; CHECK-NEXT:    [[VMIN:%.*]] = phi float [ [[V0]], [[ENTRY]] ], [ [[SEL2:%.*]], [[LOOP]] ]
+; CHECK-NEXT:    [[MINIDX:%.*]] = phi i64 [ 0, [[ENTRY]] ], [ [[IDX2:%.*]], [[LOOP]] ]
+; CHECK-NEXT:    [[P1:%.*]] = getelementptr [4 x i8], ptr [[P]], i64 [[IV]]
+; CHECK-NEXT:    [[V1:%.*]] = load float, ptr [[P1]], align 4
+; CHECK-NEXT:    [[C1:%.*]] = fcmp nnan olt float [[V1]], [[VMIN]]
+; CHECK-NEXT:    [[SEL1:%.*]] = call nnan nsz float @llvm.minnum.f32(float [[V1]], float [[VMIN]])
+; CHECK-NEXT:    [[IDX1:%.*]] = select i1 [[C1]], i64 [[IV]], i64 [[MINIDX]]
+; CHECK-NEXT:    [[IV1:%.*]] = add nuw i64 [[IV]], 1
+; CHECK-NEXT:    [[P2:%.*]] = getelementptr [4 x i8], ptr [[P]], i64 [[IV1]]
+; CHECK-NEXT:    [[V2:%.*]] = load float, ptr [[P2]], align 4
+; CHECK-NEXT:    [[C2:%.*]] = fcmp nnan olt float [[V2]], [[SEL1]]
+; CHECK-NEXT:    [[SEL2]] = call nnan nsz float @llvm.minnum.f32(float [[V2]], float [[SEL1]])
+; CHECK-NEXT:    [[IDX2]] = select i1 [[C2]], i64 [[IV1]], i64 [[IDX1]]
+; CHECK-NEXT:    [[IV_NEXT]] = add nuw i64 [[IV]], 2
+; CHECK-NEXT:    [[EC:%.*]] = icmp ult i64 [[IV_NEXT]], [[N:%.*]]
+; CHECK-NEXT:    br i1 [[EC]], label [[LOOP]], label [[EXIT:%.*]]
+; CHECK:       exit:
+; CHECK-NEXT:    ret i64 [[IDX2]]
+;
+entry:
+  %v0 = load float, ptr %p
+  br label %loop
+
+loop:
+  %iv = phi i64 [ 1, %entry ], [ %iv.next, %loop ]
+  %vmin = phi float [ %v0, %entry ], [ %sel2, %loop ]
+  %minidx = phi i64 [ 0, %entry ], [ %idx2, %loop ]
+  %p1 = getelementptr float, ptr %p, i64 %iv
+  %v1 = load float, ptr %p1
+  %c1 = fcmp nnan olt float %v1, %vmin
+  %sel1 = select i1 %c1, float %v1, float %vmin
+  %idx1 = select i1 %c1, i64 %iv, i64 %minidx
+  %iv1 = add nuw i64 %iv, 1
+  %p2 = getelementptr float, ptr %p, i64 %iv1
+  %v2 = load float, ptr %p2
+  %c2 = fcmp nnan olt float %v2, %sel1
+  %sel2 = select i1 %c2, float %v2, float %sel1
+  %idx2 = select i1 %c2, i64 %iv1, i64 %idx1
+  %iv.next = add nuw i64 %iv, 2
+  %ec = icmp ult i64 %iv.next, %n
+  br i1 %ec, label %loop, label %exit
+
+exit:
+  ret i64 %idx2
+}
+
+define float @unrolled_chain_min_live_out(ptr %p, i64 %n) {
+; CHECK-LABEL: @unrolled_chain_min_live_out(
+; CHECK-NEXT:  entry:
+; CHECK-NEXT:    [[V0:%.*]] = load float, ptr [[P:%.*]], align 4
+; CHECK-NEXT:    br label [[LOOP:%.*]]
+; CHECK:       loop:
+; CHECK-NEXT:    [[IV:%.*]] = phi i64 [ 1, [[ENTRY:%.*]] ], [ [[IV_NEXT:%.*]], [[LOOP]] ]
+; CHECK-NEXT:    [[VMIN:%.*]] = phi float [ [[V0]], [[ENTRY]] ], [ [[SEL2:%.*]], [[LOOP]] ]
+; CHECK-NEXT:    [[P1:%.*]] = getelementptr [4 x i8], ptr [[P]], i64 [[IV]]
+; CHECK-NEXT:    [[V1:%.*]] = load float, ptr [[P1]], align 4
+; CHECK-NEXT:    [[C1:%.*]] = fcmp nnan olt float [[V1]], [[VMIN]]
+; CHECK-NEXT:    [[SEL1:%.*]] = select i1 [[C1]], float [[V1]], float [[VMIN]]
+; CHECK-NEXT:    [[TMP0:%.*]] = getelementptr [4 x i8], ptr [[P]], i64 [[IV]]
+; CHECK-NEXT:    [[P2:%.*]] = getelementptr i8, ptr [[TMP0]], i64 4
+; CHECK-NEXT:    [[V2:%.*]] = load float, ptr [[P2]], align 4
+; CHECK-NEXT:    [[C2:%.*]] = fcmp nnan olt float [[V2]], [[SEL1]]
+; CHECK-NEXT:    [[SEL2]] = select i1 [[C2]], float [[V2]], float [[SEL1]]
+; CHECK-NEXT:    [[IV_NEXT]] = add nuw i64 [[IV]], 2
+; CHECK-NEXT:    [[EC:%.*]] = icmp ult i64 [[IV_NEXT]], [[N:%.*]]
+; CHECK-NEXT:    br i1 [[EC]], label [[LOOP]], label [[EXIT:%.*]]
+; CHECK:       exit:
+; CHECK-NEXT:    ret float [[SEL2]]
+;
+entry:
+  %v0 = load float, ptr %p
+  br label %loop
+
+loop:
+  %iv = phi i64 [ 1, %entry ], [ %iv.next, %loop ]
+  %vmin = phi float [ %v0, %entry ], [ %sel2, %loop ]
+  %p1 = getelementptr float, ptr %p, i64 %iv
+  %v1 = load float, ptr %p1
+  %c1 = fcmp nnan olt float %v1, %vmin
+  %sel1 = select i1 %c1, float %v1, float %vmin
+  %iv1 = add nuw i64 %iv, 1
+  %p2 = getelementptr float, ptr %p, i64 %iv1
+  %v2 = load float, ptr %p2
+  %c2 = fcmp nnan olt float %v2, %sel1
+  %sel2 = select i1 %c2, float %v2, float %sel1
+  %iv.next = add nuw i64 %iv, 2
+  %ec = icmp ult i64 %iv.next, %n
+  br i1 %ec, label %loop, label %exit
+
+exit:
+  ret float %sel2
+}
+
+define i32 @multi_use_insignificant(float %a, float %b) {
+; CHECK-LABEL: @multi_use_insignificant(
+; CHECK-NEXT:  entry:
+; CHECK-NEXT:    [[S:%.*]] = call nnan nsz float @llvm.minnum.f32(float [[A:%.*]], float [[B:%.*]])
+; CHECK-NEXT:    [[I:%.*]] = fptosi float [[S]] to i32
+; CHECK-NEXT:    [[F:%.*]] = call float @llvm.fabs.f32(float [[S]])
+; CHECK-NEXT:    [[I2:%.*]] = fptosi float [[F]] to i32
+; CHECK-NEXT:    [[R:%.*]] = add i32 [[I]], [[I2]]
+; CHECK-NEXT:    ret i32 [[R]]
+;
+entry:
+  %c = fcmp nnan olt float %a, %b
+  %s = select i1 %c, float %a, float %b
+  %i = fptosi float %s to i32
+  %f = call float @llvm.fabs.f32(float %s)
+  %i2 = fptosi float %f to i32
+  %r = add i32 %i, %i2
+  ret i32 %r
+}
+
+declare float @llvm.fabs.f32(float)

commit 08a563eaea2fe9034f6b55304243f9604378449d

[InstCombine] Allow detection of nsz on phi in fcmp+select to min/max (#214156)
The nsz could be coming from the phi, so check for
canIgnoreSignBitOfZero first. Fixes a regression from #213133.

llvm/lib/Transforms/InstCombine/InstCombineSelect.cpp
@@ -4527,13 +4527,14 @@ static bool isSelectZeroSignInsignificant(SelectInst &SI) {
       auto *User = cast<Instruction>(U.getUser());
       if (User == &SI)
         continue;
+      if (canIgnoreSignBitOfZero(U))
+        continue;
       if (isa<PHINode, SelectInst>(User)) {
         if (Visited.insert(User).second)
           Worklist.push_back(User);
         continue;
       }
-      if (!canIgnoreSignBitOfZero(U))
-        return false;
+      return false;
     }
   }
   return true;

llvm/test/Transforms/InstCombine/minmax-fp-loop-carried.ll
@@ -239,4 +239,43 @@ entry:
   ret i32 %r
 }
 
-declare float @llvm.fabs.f32(float)
+define float @from_phi(ptr %arrayidx.i, i32 %mul, float %acc) {
+; CHECK-LABEL: @from_phi(
+; CHECK-NEXT:  entry:
+; CHECK-NEXT:    br label [[FOR_COND4:%.*]]
+; CHECK:       for.cond4:
+; CHECK-NEXT:    [[ACC_ADDR_1:%.*]] = phi nsz float [ [[ACC_ADDR_0:%.*]], [[FOR_BODY:%.*]] ], [ [[DOTSROA_SPECULATED:%.*]], [[FOR_BODY7:%.*]] ]
+; CHECK-NEXT:    [[HDR_ROW_0:%.*]] = phi ptr [ [[ARRAYIDX_I:%.*]], [[FOR_BODY]] ], [ [[INCDEC_PTR:%.*]], [[FOR_BODY7]] ]
+; CHECK-NEXT:    [[I_0:%.*]] = phi i32 [ 0, [[FOR_BODY]] ], [ [[INC:%.*]], [[FOR_BODY7]] ]
+; CHECK-NEXT:    [[CMP5:%.*]] = icmp slt i32 [[I_0]], [[MUL:%.*]]
+; CHECK-NEXT:    br i1 [[CMP5]], label [[FOR_BODY7]], label [[FOR_COND_CLEANUP6:%.*]]
+; CHECK:       for.body7:
+; CHECK-NEXT:    [[TMP10:%.*]] = load float, ptr [[HDR_ROW_0]], align 4
+; CHECK-NEXT:    [[DOTSROA_SPECULATED]] = call nnan ninf nsz float @llvm.minnum.f32(float [[TMP10]], float [[ACC_ADDR_1]])
+; CHECK-NEXT:    [[INC]] = add nuw nsw i32 [[I_0]], 1
+; CHECK-NEXT:    [[INCDEC_PTR]] = getelementptr inbounds nuw i8, ptr [[HDR_ROW_0]], i64 4
+; CHECK-NEXT:    br label [[FOR_COND4]]
+; CHECK:       for.cond.cleanup6:
+; CHECK-NEXT:    ret float [[ACC_ADDR_1]]
+;
+entry:
+  br label %for.cond4
+
+for.cond4:                                        ; preds = %for.body7, %for.body
+  %acc.addr.1 = phi nsz float [ %acc, %entry ], [ %.sroa.speculated, %for.body7 ]
+  %row.0 = phi ptr [ %arrayidx.i, %entry ], [ %incdec.ptr, %for.body7 ]
+  %i.0 = phi i32 [ 0, %entry ], [ %inc, %for.body7 ]
+  %cmp5 = icmp slt i32 %i.0, %mul
+  br i1 %cmp5, label %for.body7, label %for.cond.cleanup6
+
+for.body7:                                        ; preds = %for.cond4
+  %10 = load float, ptr %row.0, align 4
+  %cmp.i.i.i = fcmp fast olt float %10, %acc.addr.1
+  %.sroa.speculated = select i1 %cmp.i.i.i, float %10, float %acc.addr.1
+  %inc = add nuw nsw i32 %i.0, 1
+  %incdec.ptr = getelementptr inbounds nuw i8, ptr %row.0, i64 4
+  br label %for.cond4
+
+for.cond.cleanup6:                                ; preds = %for.cond4
+  ret float %acc.addr.1
+}