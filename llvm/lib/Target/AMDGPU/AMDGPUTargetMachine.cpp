04f018dc766e465a8e52e2b9202c1be3727e15ee
Patrick Simmons <patrick.simmons@amd.com>
2026-08-06 15:11:50 -0500

    Must run even at O0

--- llvm/lib/Target/AMDGPU/AMDGPUTargetMachine.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPUTargetMachine.cpp
@@ -1760,7 +1760,6 @@ void GCNPassConfig::addMachineSSAOptimization() {
   }
   addPass(&DeadMachineInstructionElimID);
   addPass(createSIShrinkInstructionsLegacyPass());
-  addPass(&SILowerControlFlowLegacyID);
 }
 
 bool GCNPassConfig::addILPOpts() {
@@ -1820,6 +1819,7 @@ void GCNPassConfig::addFastRegAlloc() {
   // FIXME: We have to disable the verifier here because of PHIElimination +
   // TwoAddressInstructions disabling it.
 
+  insertPass(&PHIEliminationID, &SILowerControlFlowLegacyID);
   insertPass(&TwoAddressInstructionPassID, &SIWholeQuadModeID);
 
   TargetPassConfig::addFastRegAlloc();
@@ -1831,6 +1831,8 @@ void GCNPassConfig::addPreRegAlloc() {
 }
 
 void GCNPassConfig::addOptimizedRegAlloc() {
+  insertPass(&PHIEliminationID, &SILowerControlFlowLegacyID);
+
   if (EnableDCEInRA)
     insertPass(&DetectDeadLanesID, &DeadMachineInstructionElimID);
 
