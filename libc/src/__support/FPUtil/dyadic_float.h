f0815a211ce84b6b74e06954fdbd71b4f8120e35
Sukumarsawant <sawantsukumar@gmail.com>
2026-08-06 17:03:13 +0530

    nit

--- libc/src/__support/FPUtil/dyadic_float.h
+++ libc/src/__support/FPUtil/dyadic_float.h
@@ -264,7 +264,8 @@ template <size_t Bits> struct DyadicFloat {
       }
 
       out_mantissa = static_cast<StorageType>(mantissa >> extra_fraction_len);
-      // Takes into consideration the explicit bit for number for types like float 80
+      // Takes into consideration the explicit bit for number for types like
+      // float 80
       if constexpr (get_fp_type<T>() == FPType::X86_Binary80)
         out_mantissa |= FPBits::EXPLICIT_BIT_MASK;
     }
