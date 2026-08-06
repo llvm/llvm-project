a91b21533a58888b1648b263c569ed80e3d05c31
Michael Kruse <llvm-project@meinersbur.de>
2026-08-06 12:39:13 +0200

    Disable test that requires an authentic cmdline

--- flang/test/Driver/compiler-options.f90
+++ flang/test/Driver/compiler-options.f90
@@ -1,10 +1,8 @@
 ! RUN: %flang -S -emit-llvm -o - %s | FileCheck %s
+! REQUIRES: flang-authentic-cmdline
+
 ! Test communication of COMPILER_OPTIONS from flang to flang -fc1.
-! In a Flang-standalone build, the driver also injects its own implicit
-! search-path options (e.g. -fintrinsic-modules-path=...) ahead of the
-! options given on the command line above, so allow (and ignore) an
-! arbitrary prefix before the flags under test here.
-! CHECK: [[OPTSVAR:@_QQclX[0-9A-Fa-f]+]] = {{[a-z]+}} constant [[[OPTSLEN:[0-9]+]] x i8] c"{{.*}}-S -emit-llvm -o -"
+! CHECK: [[OPTSVAR:@_QQclX[0-9A-Fa-f]+]] = {{[a-z]+}} constant [[[OPTSLEN:[0-9]+]] x i8] c"-S -emit-llvm -o -"
 program main
     use ISO_FORTRAN_ENV, only: compiler_options
     implicit none
