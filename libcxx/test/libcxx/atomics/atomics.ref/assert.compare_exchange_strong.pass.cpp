422422843df805ceb408910749bfd0064dad2d89
Michael Jones <michaelrj@google.com>
2026-08-06 22:39:08 +0000

    [testing] remove XFAIL from compare_exchange_strong

--- libcxx/test/libcxx/atomics/atomics.ref/assert.compare_exchange_strong.pass.cpp
+++ libcxx/test/libcxx/atomics/atomics.ref/assert.compare_exchange_strong.pass.cpp
@@ -9,10 +9,6 @@
 // REQUIRES: can-test-hardening-assertions-extensive
 // UNSUPPORTED: c++03, c++11, c++14, c++17
 // ADDITIONAL_COMPILE_FLAGS: -Wno-user-defined-warnings
-
-// undefined reference to symbol '__atomic_compare_exchange@@LIBATOMIC_1.0'
-// XFAIL: libcpp-hardening-mode={{extensive|debug}}
-
 // <atomic>
 
 // bool compare_exchange_strong(T& expected, T desired, memory_order success, memory_order failure) const noexcept;
