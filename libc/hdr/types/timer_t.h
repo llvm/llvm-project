2f500e632c58f33eee16f72b207478028de20fcd
Tue Ly <lntue.h@gmail.com>
2026-08-07 18:11:46 +0000

    Update to new header style.

--- libc/hdr/types/timer_t.h
+++ libc/hdr/types/timer_t.h
@@ -1,9 +1,14 @@
-//===-- Proxy for timer_t -------------------------------------------------===//
+//===----------------------------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// Proxy header for timer_t.
+///
 //===----------------------------------------------------------------------===//
 
 #ifndef LLVM_LIBC_HDR_TYPES_TIMER_T_H
