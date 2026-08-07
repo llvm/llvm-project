7e695e43d23526904569d33a7171b7e5fce2f8a2
Timm Baeder <tbaeder@redhat.com>
2026-08-07 09:47:37 +0200

    [clang][AST] Print OnePastTheEnd bit in `APValue::dump()` (#214450)
    For lvalue APValues.

--- clang/lib/AST/TextNodeDumper.cpp
+++ clang/lib/AST/TextNodeDumper.cpp
@@ -759,6 +759,7 @@ void TextNodeDumper::Visit(const APValue &Value, QualType Ty) {
     }
     OS << ", Null=" << Value.isNullPointer()
        << ", Offset=" << Value.getLValueOffset().getQuantity()
+       << ", OnePastTheEnd=" << Value.isLValueOnePastTheEnd()
        << ", HasPath=" << Value.hasLValuePath();
     if (Value.hasLValuePath()) {
       OS << ", PathLength=" << Value.getLValuePath().size();
