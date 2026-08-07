02207870652bc8bea41bf52704ee016a6ce0811e
ShengYi Hung <aokblast@FreeBSD.org>
2026-08-07 16:20:44 +0900

    fixup! [libcxx] Speed up xsgetn for always_noconv

--- libcxx/test/libcxx/input.output/iostream.objects/cin-read-stdio-sync.sh.cpp
+++ libcxx/test/libcxx/input.output/iostream.objects/cin-read-stdio-sync.sh.cpp
@@ -6,6 +6,15 @@
 //
 //===----------------------------------------------------------------------===//
 
+// QEMU does not detect EOF, when reading from stdin
+// "echo -n" suppresses any characters after the output and so the test hangs.
+// https://gitlab.com/qemu-project/qemu/-/issues/1963
+// UNSUPPORTED: LIBCXX-PICOLIBC-FIXME
+
+// This test hangs on Android devices that lack shell_v2, which was added in
+// Android N (API 24).
+// UNSUPPORTED: LIBCXX-ANDROID-FIXME && android-device-api={{2[1-3]}}
+
 // <iostream>
 
 // istream cin;
