fa5e4f47425f64571072de643dbeb83c68b444a2
Justin Bogner <mail@justinbogner.com>
2026-08-07 15:21:42 -0700

    Remove stale comment

--- llvm/lib/Target/DirectX/DXILShaderFlags.cpp
+++ llvm/lib/Target/DirectX/DXILShaderFlags.cpp
@@ -63,7 +63,6 @@ static bool hasUAVsAtEveryStage(const DXILResourceMap &DRM,
 
 static bool checkWaveOps(Intrinsic::ID IID) {
   // Currently unsupported intrinsics
-  // case Intrinsic::dx_wave_getlanecount:
   // case Intrinsic::dx_wave_readfirst:
   // case Intrinsic::dx_wave_reduce.and:
   // case Intrinsic::dx_wave_reduce.or:
