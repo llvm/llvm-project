9093b53d6162903bb9b050740132594fa29bd5ae
Nerixyz <nerixdev@outlook.de>
2026-08-07 21:07:27 +0200

    [lldb] Don't resolve dynamic type info if no named type exists (#214828)
    Intends to fix the failure from
    https://github.com/llvm/llvm-project/pull/212013#issuecomment-5220617065.
    
    I overlooked that the original code returned an empty `TypeAndOrName` if
    no type with the specified name was found:
    https://github.com/llvm/llvm-project/blob/9b1218ce69e06a22699bc196888fc0396d989c81/lldb/source/Plugins/LanguageRuntime/CPlusPlus/ItaniumABIRuntime.cpp#L84-L88
    
    In my PR, I always returned a `TypeAndOrName` with a name set. With this
    change, we only set return a non-empty `TypeAndOrName` if there was any
    type with the specified name.

--- lldb/source/Plugins/LanguageRuntime/CPlusPlus/CommonABIRuntime.h
+++ lldb/source/Plugins/LanguageRuntime/CPlusPlus/CommonABIRuntime.h
@@ -22,8 +22,14 @@ public:
 protected:
   CommonABIRuntime(Process *process);
 
+  /// Find a type by its name, preferably in `preferred_module`.
+  ///
+  /// `any_found` will be set to `true` if any type with the name is found.
+  /// Even if a type with the name was found, this function may return an empty
+  /// `TypeSP` if the type is not a C++ type.
   lldb::TypeSP LookupTypeByName(llvm::StringRef type_name,
-                                lldb::ModuleSP preferred_module) const;
+                                lldb::ModuleSP preferred_module,
+                                bool &any_found) const;
 
 protected:
   Process *m_process;
