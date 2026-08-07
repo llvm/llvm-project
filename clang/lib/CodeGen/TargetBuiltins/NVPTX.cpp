c552e1582237badd7c465862e6e1debb90b82081
Srinivasa Ravi <srinivasar@nvidia.com>
2026-08-07 10:53:10 +0000

    fix formatting

--- clang/lib/CodeGen/TargetBuiltins/NVPTX.cpp
+++ clang/lib/CodeGen/TargetBuiltins/NVPTX.cpp
@@ -981,43 +981,47 @@ Value *CodeGenFunction::EmitNVPTXBuiltinExpr(unsigned BuiltinID,
     return MakeHalfType(Intrinsic::nvvm_ff2f16x2_rz, BuiltinID, E, *this);
   case NVPTX::BI__nvvm_ff2f16x2_rz_relu:
     return MakeHalfType(Intrinsic::nvvm_ff2f16x2_rz_relu, BuiltinID, E, *this);
+
 #define PZO_CVT(cvt)                                                           \
   case NVPTX::BI__nvvm_##cvt##_pzo:                                            \
     return MakeHalfType(Intrinsic::nvvm_##cvt, BuiltinID, E, *this,            \
                         {Builder.getTrue()})
-  PZO_CVT(ff2f16x2_rn);
-  PZO_CVT(ff2f16x2_rn_relu);
-  PZO_CVT(ff2f16x2_rz);
-  PZO_CVT(ff2f16x2_rz_relu);
-  PZO_CVT(ff2f16x2_rn_satfinite);
-  PZO_CVT(ff2f16x2_rn_relu_satfinite);
-  PZO_CVT(ff2f16x2_rz_satfinite);
-  PZO_CVT(ff2f16x2_rz_relu_satfinite);
-  PZO_CVT(ff2bf16x2_rn);
-  PZO_CVT(ff2bf16x2_rn_relu);
-  PZO_CVT(ff2bf16x2_rz);
-  PZO_CVT(ff2bf16x2_rz_relu);
-  PZO_CVT(ff2bf16x2_rn_satfinite);
-  PZO_CVT(ff2bf16x2_rn_relu_satfinite);
-  PZO_CVT(ff2bf16x2_rz_satfinite);
-  PZO_CVT(ff2bf16x2_rz_relu_satfinite);
-  PZO_CVT(f2f16_rn);
-  PZO_CVT(f2f16_rn_relu);
-  PZO_CVT(f2f16_rz);
-  PZO_CVT(f2f16_rz_relu);
-  PZO_CVT(f2f16_rn_satfinite);
-  PZO_CVT(f2f16_rn_relu_satfinite);
-  PZO_CVT(f2f16_rz_satfinite);
-  PZO_CVT(f2f16_rz_relu_satfinite);
-  PZO_CVT(f2bf16_rn);
-  PZO_CVT(f2bf16_rn_relu);
-  PZO_CVT(f2bf16_rz);
-  PZO_CVT(f2bf16_rz_relu);
-  PZO_CVT(f2bf16_rn_satfinite);
-  PZO_CVT(f2bf16_rn_relu_satfinite);
-  PZO_CVT(f2bf16_rz_satfinite);
-  PZO_CVT(f2bf16_rz_relu_satfinite);
+
+    PZO_CVT(ff2f16x2_rn);
+    PZO_CVT(ff2f16x2_rn_relu);
+    PZO_CVT(ff2f16x2_rz);
+    PZO_CVT(ff2f16x2_rz_relu);
+    PZO_CVT(ff2f16x2_rn_satfinite);
+    PZO_CVT(ff2f16x2_rn_relu_satfinite);
+    PZO_CVT(ff2f16x2_rz_satfinite);
+    PZO_CVT(ff2f16x2_rz_relu_satfinite);
+    PZO_CVT(ff2bf16x2_rn);
+    PZO_CVT(ff2bf16x2_rn_relu);
+    PZO_CVT(ff2bf16x2_rz);
+    PZO_CVT(ff2bf16x2_rz_relu);
+    PZO_CVT(ff2bf16x2_rn_satfinite);
+    PZO_CVT(ff2bf16x2_rn_relu_satfinite);
+    PZO_CVT(ff2bf16x2_rz_satfinite);
+    PZO_CVT(ff2bf16x2_rz_relu_satfinite);
+    PZO_CVT(f2f16_rn);
+    PZO_CVT(f2f16_rn_relu);
+    PZO_CVT(f2f16_rz);
+    PZO_CVT(f2f16_rz_relu);
+    PZO_CVT(f2f16_rn_satfinite);
+    PZO_CVT(f2f16_rn_relu_satfinite);
+    PZO_CVT(f2f16_rz_satfinite);
+    PZO_CVT(f2f16_rz_relu_satfinite);
+    PZO_CVT(f2bf16_rn);
+    PZO_CVT(f2bf16_rn_relu);
+    PZO_CVT(f2bf16_rz);
+    PZO_CVT(f2bf16_rz_relu);
+    PZO_CVT(f2bf16_rn_satfinite);
+    PZO_CVT(f2bf16_rn_relu_satfinite);
+    PZO_CVT(f2bf16_rz_satfinite);
+    PZO_CVT(f2bf16_rz_relu_satfinite);
+
 #undef PZO_CVT
+
   case NVPTX::BI__nvvm_fma_rn_f16:
     return MakeHalfType(Intrinsic::nvvm_fma_rn_f16, BuiltinID, E, *this);
   case NVPTX::BI__nvvm_fma_rn_f16x2:
