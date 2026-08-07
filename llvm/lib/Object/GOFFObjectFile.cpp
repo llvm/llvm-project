53c64aa519aac1fda507720ce902851dde97d3d7
David Tenty <daltenty@ibm.com>
2026-08-07 13:49:02 -0400

    [llvm][object][GOFF] refine Global flag conditions (#211632)
    Currently when iterating across the symbol table with the object library (in tools like nm/ar), some symbols that we wouldn't expect to be Global to the binder get flagged as SF_Global. With GOFF, we should exclude ESD entries who's type or binding scope make them unavailable for binder to use at Global scope.
    
    The implementation currently only filters the Section binding scopes when setting the Global flag, so this change adds filters for SD/ED type records and Module scope as well (which restricts the def to only the current module).
    
    We also add a test that checks ESD records for various types and binding strengths to see that we get the expected values.
    
    (cherry picked from commit 3873a4da50256649d4be843aea172fd6507aa2e8)

--- llvm/lib/Object/GOFFObjectFile.cpp
+++ llvm/lib/Object/GOFFObjectFile.cpp
@@ -279,7 +279,13 @@ Expected<uint32_t> GOFFObjectFile::getSymbolFlags(DataRefImpl Symb) const {
   GOFF::ESDBindingScope BindingScope;
   ESDRecord::getBindingScope(Record, BindingScope);
 
-  if (BindingScope != GOFF::ESD_BSC_Section) {
+  GOFF::ESDSymbolType Type;
+  ESDRecord::getSymbolType(Record, Type);
+
+  if (Type != GOFF::ESD_ST_SectionDefinition &&
+      Type != GOFF::ESD_ST_ElementDefinition &&
+      BindingScope != GOFF::ESD_BSC_Section &&
+      BindingScope != GOFF::ESD_BSC_Module) {
     Expected<StringRef> Name = getSymbolName(Symb);
     if (Name && *Name != " ") { // Blank name is local.
       Flags |= SymbolRef::SF_Global;
