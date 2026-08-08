1475816ff488af78a39485936b86fe1d99b12a8f
Jean-Didier PAILLEUX <jean-didier.pailleux@sipearl.com>
2026-08-08 20:56:12 +0200

    [flang][MIF] Update prif_coarray_handle in accordance with PRIF 0.8 #214080 (#214747)
    In PRIF revision 0.8, the representation of `prif_coarray_handle` was
    changed. This PR updates this representation for this type and updates
    the MIFOpConversion pass for the relevant operations.
    Fixes issue #214080
    A minor fix has been made to the deallocation to ensure that, on the
    Flang side, the variable is properly deallocated, since previously the
    deallocation was only performed at the `coarray_handle` level.

--- flang/test/Lower/MIF/coarray_dealloc_not_alloc.f90
+++ flang/test/Lower/MIF/coarray_dealloc_not_alloc.f90
@@ -1,10 +1,9 @@
 ! RUN: %flang_fc1 -emit-llvm -fcoarray %s -o - 2>&1 | FileCheck %s --check-prefix=LLVM
 
-! LLVM: @_QFB1Ekk_coarray_handle = linkonce global { ptr, i64, i32, i8, i8, i8, i8, ptr, [1 x i64] } { ptr null, i64 40, i32 20240719, i8 0, i8 42, i8 0, i8 1, ptr @_QMprifEXdtXprif_coarray_handle, [1 x i64] zeroinitializer }, comdat
+! LLVM: @_QFB1Ekk_coarray_handle = linkonce global %_QM__fortran_builtinsT__builtin_prif_coarray_handle_type zeroinitializer, comdat
 
 ! LLVM-LABEL:  @_QQmain()
-! LLVM:       call void @llvm.memcpy.p0.p0.i32(ptr align 8 %[[VAL_1:.*]], ptr align 8 @_QFB1Ekk_coarray_handle, i32 40, i1 false)
-! LLVM-NEXT:  call void @_QMprifPprif_deallocate_coarray(ptr %[[VAL_1]], ptr null, ptr null, ptr null)
+! LLVM:  call void @_QMprifPprif_deallocate_coarray(ptr @_QFB1Ekk_coarray_handle, ptr null, ptr null, ptr null)
 
 
 block
