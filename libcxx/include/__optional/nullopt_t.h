6054b43ba23a10974757149490086b3585532348
Hristo Hristov <hghristov.rmm@gmail.com>
2026-08-08 19:15:36 +0300

    Fixed modules

--- libcxx/include/__optional/nullopt_t.h
+++ libcxx/include/__optional/nullopt_t.h
@@ -10,6 +10,7 @@
 #ifndef _LIBCPP___OPTIONAL_NULLOPT_T_H
 #define _LIBCPP___OPTIONAL_NULLOPT_T_H
 
+#include <__compare/ordering.h>
 #include <__config>
 
 #if !defined(_LIBCPP_HAS_NO_PRAGMA_SYSTEM_HEADER)
