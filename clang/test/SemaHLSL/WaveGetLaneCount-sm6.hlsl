c94f79feec9b2c71b7d4ab891e13303a70816980
Justin Bogner <mail@justinbogner.com>
2026-08-07 17:02:48 -0700

    Delete clang/test/SemaHLSL/WaveGetLaneCount-sm6.hlsl
    This test isn't really that valuable - we test availability attributes elsewhere.

--- clang/test/SemaHLSL/WaveGetLaneCount-sm6.hlsl
+++
@@ -1,9 +0,0 @@
-// RUN: %clang_cc1 -finclude-default-header -triple dxil-pc-shadermodel6.0-library -verify %s
-
-// expected-no-diagnostics
-[shader("compute")]
-[numthreads(8,8,1)]
-unsigned foo() {
-    return hlsl::WaveGetLaneCount();
-}
-
