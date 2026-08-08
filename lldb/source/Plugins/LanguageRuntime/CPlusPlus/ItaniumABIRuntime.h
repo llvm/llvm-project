e8ba5e17299c4834870ff5b5eb05042ab90bb631
Nerixyz <nerixdev@outlook.de>
2026-08-08 11:57:15 +0200

    [lldb] Allow multiple ABI runtimes for C++ (#212015)
    This allows `CPPLanguageRuntime` to contain more than one ABI runtime.
    For dynamic types, we ask each runtime if it knows how to deal with a
    vtable symbol. For exception breakpoints, we will create all
    breakpoints. Note that the MS ABI will only be added on Windows targets.
    So the MS ABI will not be added to `m_abi_runtimes` on other targets.

--- lldb/source/Plugins/LanguageRuntime/CPlusPlus/ItaniumABIRuntime.h
+++ lldb/source/Plugins/LanguageRuntime/CPlusPlus/ItaniumABIRuntime.h
@@ -21,23 +21,25 @@ class ItaniumABIRuntime : public CommonABIRuntime {
 public:
   ItaniumABIRuntime(Process *process);
 
-  bool IsVTableSymbol(Mangled &manged) const;
+  llvm::StringRef GetName() const override;
+
+  bool IsVTableSymbol(Mangled &manged) const override;
 
   bool GetDynamicTypeAndAddress(ValueObject &in_value,
                                 lldb::DynamicValueType use_dynamic,
                                 const LanguageRuntime::VTableInfo &vtable_info,
                                 TypeAndOrName &class_type_or_name,
-                                Address &dynamic_address,
-                                Value::ValueType &value_type);
+                                Address &dynamic_address) override;
 
   void AppendExceptionBreakpointFunctions(std::vector<const char *> &names,
                                           bool catch_bp, bool throw_bp,
-                                          bool for_expressions);
+                                          bool for_expressions) override;
 
   void AppendExceptionBreakpointFilterModules(FileSpecList &list,
-                                              const Target &target);
+                                              const Target &target) override;
 
-  lldb::ValueObjectSP GetExceptionObjectForThread(lldb::ThreadSP thread_sp);
+  lldb::ValueObjectSP
+  GetExceptionObjectForThread(lldb::ThreadSP thread_sp) override;
 
 private:
   TypeAndOrName GetTypeInfo(ValueObject &in_value,
