6e1b80eb637b6d988dfaf1d65153bc8d3112c00a
Daniel Chen <cdchen@ca.ibm.com>
2026-08-08 20:03:43 -0400

    [flang] To fix LIT test that AIX does not emit comdat. (#214950)
    PR #213890 added this new LIT test.
    However, AIX uses XCOFF and does not emit COFF COMDAT.

--- flang/test/Lower/MIF/coarray_dealloc_not_alloc.f90
+++ flang/test/Lower/MIF/coarray_dealloc_not_alloc.f90
@@ -1,6 +1,6 @@
 ! RUN: %flang_fc1 -emit-llvm -fcoarray %s -o - 2>&1 | FileCheck %s --check-prefix=LLVM
 
-! LLVM: @_QFB1Ekk_coarray_handle = linkonce global %_QM__fortran_builtinsT__builtin_prif_coarray_handle_type zeroinitializer, comdat
+! LLVM: @_QFB1Ekk_coarray_handle = linkonce global %_QM__fortran_builtinsT__builtin_prif_coarray_handle_type zeroinitializer{{.*}}
 
 ! LLVM-LABEL:  @_QQmain()
 ! LLVM:  call void @_QMprifPprif_deallocate_coarray(ptr @_QFB1Ekk_coarray_handle, ptr null, ptr null, ptr null)
@@ -11,4 +11,3 @@ integer :: kk
 allocatable :: kk(:)[:]
 endblock
 end
-
