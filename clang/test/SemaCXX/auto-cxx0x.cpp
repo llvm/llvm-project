d4b2b12e0b23e82fc1d34c328f63ad9df093ec2c
Tony Guillot <tony.guillot@protonmail.com>
2026-08-10 18:41:05 +0200

    Reapply "Reject auto combined with type specifiers in C++ (#208552)"
    This reverts commit 25a419fbe2062adb6cc3a9407948a9765f6fcb22.

--- clang/test/SemaCXX/auto-cxx0x.cpp
+++ clang/test/SemaCXX/auto-cxx0x.cpp
@@ -1,8 +1,14 @@
 // RUN: %clang_cc1 -fsyntax-only -verify %s -std=c++11
 // RUN: %clang_cc1 -fsyntax-only -verify %s -std=c++1y
 void f() {
-  auto int a; // expected-warning {{'auto' storage class specifier is not permitted in C++11, and will not be supported in future releases}}
-  int auto b; // expected-error {{cannot combine with previous 'int' declaration specifier}}
+  auto int a; // expected-error {{'auto' cannot be combined with a type specifier}}
+  int auto b; // expected-error {{'auto' cannot be combined with a type specifier}}
+  unsigned auto int x; // expected-error {{'auto' cannot be combined with a type specifier}} expected-error-re {{'{{.*}}' cannot be signed or unsigned}}
+  signed auto int s; // expected-error {{'auto' cannot be combined with a type specifier}} expected-error-re {{'{{.*}}' cannot be signed or unsigned}}
+  auto double y; // expected-error {{'auto' cannot be combined with a type specifier}}
+  auto float z; // expected-error {{'auto' cannot be combined with a type specifier}}
+  long auto int l; // expected-error {{'auto' cannot be combined with a type specifier}} expected-error-re {{'long {{.*}}' is invalid}}
+  auto int arr[10]; // expected-error {{'auto' cannot be combined with a type specifier}}
 }
 
 typedef auto PR25449(); // expected-error {{'auto' not allowed in typedef}}
@@ -23,3 +29,23 @@ void rdar47689465() {
   // expected-error@-2 {{'auto' not allowed in lambda parameter before C++14}}
 #endif
 }
+
+namespace auto_declarator_name_lookup {
+namespace llvm {
+  class Use;
+namespace rdf {
+  template <class T> struct NodeAddr {};
+  class UseNode;
+  using Use = NodeAddr<UseNode *>;
+} // namespace rdf
+} // namespace llvm
+
+using namespace llvm;
+
+namespace {
+void f() {
+  using namespace rdf;
+  auto Use = 0;
+}
+} // namespace
+} // namespace auto_declarator_name_lookup
