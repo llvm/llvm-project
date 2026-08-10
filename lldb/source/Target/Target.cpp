3fd375f4b0bfa26a56983536df5dfd054ffc1296
Alex Langford <alangford@apple.com>
2026-08-10 16:27:37 -0700

    [lldb] Use StringRef when creating Listeners (#214863)
    This is primarily motivated to remove the last ConstString from
    Debugger.cpp. It would have been possible to write this change without
    changing Listener, but then I noticed a few places where we would have
    benefitted from changing the interface.

--- lldb/source/Target/Target.cpp
+++ lldb/source/Target/Target.cpp
@@ -3653,8 +3653,8 @@ Status Target::Launch(ProcessLaunchInfo &launch_info, Stream *stream) {
   // its own hijacking listener or if the process is created by the target
   // manually, without the platform).
   if (!launch_info.GetHijackListener())
-    launch_info.SetHijackListener(Listener::MakeListener(
-        Process::LaunchSynchronousHijackListenerName.data()));
+    launch_info.SetHijackListener(
+        Listener::MakeListener(Process::LaunchSynchronousHijackListenerName));
 
   // If we're not already connected to the process, and if we have a platform
   // that can launch a process for debugging, go ahead and do that here.
@@ -3835,8 +3835,8 @@ Status Target::Attach(ProcessAttachInfo &attach_info, Stream *stream) {
   ListenerSP hijack_listener_sp;
   const bool async = attach_info.GetAsync();
   if (!async) {
-    hijack_listener_sp = Listener::MakeListener(
-        Process::AttachSynchronousHijackListenerName.data());
+    hijack_listener_sp =
+        Listener::MakeListener(Process::AttachSynchronousHijackListenerName);
     attach_info.SetHijackListener(hijack_listener_sp);
   }
 
