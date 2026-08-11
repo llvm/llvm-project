aa97e882137df7041b8e4b598b29fcc179d5a78f
Kenzo <kenzoeugeniotjandra@gmail.com>
2026-08-11 19:54:21 -0400

    [Clang] Add tests for bare template specializations, and noconvergent/alwaysinline attributes

--- clang/test/CodeGen/attr-noinline-always-inline.cpp
+++ clang/test/CodeGen/attr-noinline-always-inline.cpp
@@ -1,17 +1,22 @@
-// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -emit-llvm -o - %s | FileCheck %s
+// RUN: %clang_cc1 -O1 -disable-llvm-passes -triple x86_64-unknown-linux-gnu -emit-llvm -o - %s | FileCheck %s
 
 template <typename T>
 [[clang::noinline]] void foo(T) {}
 
-// Explicit specialization should not inherit noinline
+// Explicit specializations should not inherit noinline
 template <>
 [[clang::always_inline]] void foo<int>(int) {}
+
+template <>
+void foo<long>(long) {}
 
 void caller() {
   foo<float>(4.2f); // expect noinline on function
   foo<int>(42); // expect alwaysinline on function
+  foo<long>(42L); // expect no noinline attribute
 }
 // CHECK: define {{.*}}void @_Z3fooIiEvT_({{.*}}) #[[ALWAYSINLINE:[0-9]+]]
+// CHECK: define {{.*}}void @_Z3fooIlEvT_({{.*}}) #[[BARE:[0-9]+]]
 // CHECK: define {{.*}}void @_Z3fooIfEvT_({{.*}}) #[[NOINLINE:[0-9]+]]
 
 // Inner function should not clobber non-conflicting attributes
@@ -37,21 +42,48 @@ void outer_fn2() {
 }
 // CHECK: call void @_Z9inner_fn2v() #[[NOINLINE_NOMERGE]]
 
-// Inner function should clobber a conflicting attribute
+[[clang::convergent]]
 void inner_fn3();
 
 void outer_fn3() {
-  [[clang::noinline]]
+  [[clang::always_inline]]
   {
-    [[clang::always_inline]]
+    [[clang::noconvergent]] // unrelated to inlining
     inner_fn3();
   }
 }
 // CHECK: call void @_Z9inner_fn3v() #[[ALWAYSINLINE_ONLY:[0-9]+]]
 
+[[clang::convergent]]
+void inner_fn4();
+
+void outer_fn4() {
+  [[clang::noconvergent]]
+  {
+    [[clang::always_inline]] // unrelated to noconvergent
+    inner_fn4();
+  }
+}
+// CHECK: call void @_Z9inner_fn4v() #[[ALWAYSINLINE_ONLY]]
+
+// Inner function should clobber a conflicting attribute
+void inner_fn5();
+
+void outer_fn5() {
+  [[clang::noinline]]
+  {
+    [[clang::always_inline]]
+    inner_fn5();
+  }
+}
+// CHECK: call void @_Z9inner_fn5v() #[[ALWAYSINLINE_ONLY]]
+
 // CHECK: attributes #[[ALWAYSINLINE]] = {
 // CHECK-SAME: alwaysinline
 // CHECK-NOT:  noinline
+// CHECK: attributes #[[BARE]] = {
+// CHECK-NOT: alwaysinline
+// CHECK-NOT: noinline
 // CHECK: attributes #[[NOINLINE]] = {
 // CHECK-SAME: noinline
 // CHECK-NOT:  alwaysinline
