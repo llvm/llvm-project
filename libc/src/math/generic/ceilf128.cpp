e8760ed6cffd7b1521c801d97dd47d5fd77d9869
Sukumarsawant <sawantsukumar@gmail.com>
2026-08-12 01:14:44 +0530

    update guard

--- libc/src/math/generic/ceilf128.cpp
+++ libc/src/math/generic/ceilf128.cpp
@@ -9,9 +9,9 @@
 #include "src/math/ceilf128.h"
 #include "src/__support/math/ceilf128.h"
 
-#ifndef LIBC_TYPES_HAS_FLOAT128
+#ifndef LIBC_TYPES_HAS_NATIVE_FLOAT128
 using float128 = LIBC_NAMESPACE::fputil::Float128;
-#endif // LIBC_TYPES_HAS_FLOAT128
+#endif // LIBC_TYPES_HAS_NATIVE_FLOAT128
 
 namespace LIBC_NAMESPACE_DECL {
 
