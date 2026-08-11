retrochristian5000/LLVM

[clang-tidy] Add a new check `misc-header-guard`
Find and fix header guards

It respects option `misc-header-guard.HeaderDirs`, which contains a list
of one or more header directory names.

clang-tools-extra/clang-tidy/portability/AvoidPragmaOnceCheck.cpp
@@ -8,6 +8,7 @@
 
 #include "AvoidPragmaOnceCheck.h"
 
+#include "../utils/LexerUtils.h"
 #include "clang/Basic/SourceManager.h"
 #include "clang/Lex/PPCallbacks.h"
 #include "clang/Lex/Preprocessor.h"
@@ -23,14 +24,7 @@ public:
       : Check(Check), SM(SM) {}
   void PragmaDirective(SourceLocation Loc,
                        PragmaIntroducerKind Introducer) override {
-    auto Str = StringRef(SM.getCharacterData(Loc));
-    if (!Str.consume_front("#"))
-      return;
-    Str = Str.trim();
-    if (!Str.consume_front("pragma"))
-      return;
-    Str = Str.trim();
-    if (Str.starts_with("once"))
+    if (utils::lexer::hasPragmaOnce(Loc, SM))
       Check->diag(Loc,
                   "avoid 'pragma once' directive; use include guards instead");
   }

clang-tools-extra/clang-tidy/utils/LexerUtils.cpp
@@ -371,4 +371,15 @@ SourceLocation getLocationForNoexceptSpecifier(const FunctionDecl *FuncDecl,
   return {};
 }
 
+bool hasPragmaOnce(SourceLocation Loc, const SourceManager &SM) {
+  auto Str = StringRef(SM.getCharacterData(Loc));
+  if (!Str.consume_front("#"))
+    return false;
+  Str = Str.trim();
+  if (!Str.consume_front("pragma"))
+    return false;
+  Str = Str.trim();
+  return Str.starts_with("once");
+}
+
 } // namespace clang::tidy::utils::lexer

clang-tools-extra/clang-tidy/utils/LexerUtils.h
@@ -160,6 +160,9 @@ SourceLocation getUnifiedEndLoc(const Stmt &S, const SourceManager &SM,
 SourceLocation getLocationForNoexceptSpecifier(const FunctionDecl *FuncDecl,
                                                const SourceManager &SM);
 
+/// Returns ``true`` if the pragma directive at \p Loc is a ``#pragma once``.
+bool hasPragmaOnce(SourceLocation Loc, const SourceManager &SM);
+
 } // namespace tidy::utils::lexer
 } // namespace clang
 

llvm/llvm-project

clang-tools-extra/clang-tidy/portability/AvoidPragmaOnceCheck.cpp
@@ -8,6 +8,7 @@
 
 #include "AvoidPragmaOnceCheck.h"
 
+#include "../utils/LexerUtils.h"
 #include "clang/Basic/SourceManager.h"
 #include "clang/Lex/PPCallbacks.h"
 #include "clang/Lex/Preprocessor.h"
@@ -23,14 +24,7 @@ public:
       : Check(Check), SM(SM) {}
   void PragmaDirective(SourceLocation Loc,
                        PragmaIntroducerKind Introducer) override {
-    auto Str = StringRef(SM.getCharacterData(Loc));
-    if (!Str.consume_front("#"))
-      return;
-    Str = Str.trim();
-    if (!Str.consume_front("pragma"))
-      return;
-    Str = Str.trim();
-    if (Str.starts_with("once"))
+    if (utils::lexer::isPragmaOnce(Loc, SM))
       Check->diag(Loc,
                   "avoid 'pragma once' directive; use include guards instead");
   }

clang-tools-extra/clang-tidy/utils/LexerUtils.cpp
@@ -371,4 +371,15 @@ SourceLocation getLocationForNoexceptSpecifier(const FunctionDecl *FuncDecl,
   return {};
 }
 
+bool isPragmaOnce(SourceLocation Loc, const SourceManager &SM) {
+  auto Str = StringRef(SM.getCharacterData(Loc));
+  if (!Str.consume_front("#"))
+    return false;
+  Str = Str.trim();
+  if (!Str.consume_front("pragma"))
+    return false;
+  Str = Str.trim();
+  return Str.starts_with("once");
+}
+
 } // namespace clang::tidy::utils::lexer

clang-tools-extra/clang-tidy/utils/LexerUtils.h
@@ -160,6 +160,9 @@ SourceLocation getUnifiedEndLoc(const Stmt &S, const SourceManager &SM,
 SourceLocation getLocationForNoexceptSpecifier(const FunctionDecl *FuncDecl,
                                                const SourceManager &SM);
 
+/// Returns ``true`` if the pragma directive at \p Loc is a ``#pragma once``.
+bool isPragmaOnce(SourceLocation Loc, const SourceManager &SM);
+
 } // namespace tidy::utils::lexer
 } // namespace clang
 