50a3ffec6ffd380b454a84e91436edac5d6eab2e
Kieran Bailey <kieran.bailey@arm.com>
2026-08-12 14:21:31 +0000

    [AArch64][GISel] Allow import of DAG patterns using fixedpoint immediate
    Currently, fp_to_int(fmul) patterns only work for DAG, this is due to the use of fixedpoint immediate type which GISel could not import.
    Add GlobalISel matchers and renderers for fixed-point immediates, allowing these patterns to be imported and used during instruction selection.
    
    I originally created these to allow fp_to_int(fmul) fold patterns to import, but this does enable other existing patterns which use fixedpoint to work in GISel

--- llvm/lib/Target/AArch64/GISel/AArch64InstructionSelector.cpp
+++ llvm/lib/Target/AArch64/GISel/AArch64InstructionSelector.cpp
@@ -481,19 +481,31 @@ private:
   ComplexRendererFns selectArithExtendedRegister(MachineOperand &Root) const;
 
   ComplexRendererFns selectExtractHigh(MachineOperand &Root) const;
-
+  template <unsigned Width>
+  ComplexRendererFns selectCVTFixedPoint(MachineOperand &Root) const;
+  ComplexRendererFns selectCVTFixedPointBase(const MachineOperand &Root,
+                                             unsigned width,
+                                             bool isReciprocal = false) const;
   ComplexRendererFns selectCVTFixedPointVec(MachineOperand &Root) const;
   ComplexRendererFns
   selectCVTFixedPosRecipOperandVec(MachineOperand &Root) const;
-  ComplexRendererFns
-  selectCVTFixedPointVecBase(const MachineOperand &Root,
-                             bool isReciprocal = false) const;
   void renderFixedPointScalarXForm(MachineInstrBuilder &MIB,
                                    const MachineInstr &MI, int OpIdx) const;
+  unsigned getFixedPointWidthFromOperand(const MachineOperand &Root) const;
+  template <unsigned Width>
   void renderFixedPointXForm(MachineInstrBuilder &MIB, const MachineInstr &MI,
                              int OpIdx = -1) const;
-  void renderFixedPointRecipXForm(MachineInstrBuilder &MIB,
-                                  const MachineInstr &MI, int OpIdx = -1) const;
+  void renderFixedPointXForm32(MachineInstrBuilder &MIB, const MachineInstr &MI,
+                               int OpIdx = -1) const;
+  void renderFixedPointXForm64(MachineInstrBuilder &MIB, const MachineInstr &MI,
+                               int OpIdx = -1) const;
+  void renderFixedPointXFormVec(MachineInstrBuilder &MIB,
+                                const MachineInstr &MI, int OpIdx = -1) const;
+  void renderFixedPointRecipXFormVec(MachineInstrBuilder &MIB,
+                                     const MachineInstr &MI,
+                                     int OpIdx = -1) const;
+  void renderFixedPointImm(MachineInstrBuilder &MIB, const MachineOperand &Root,
+                           unsigned Width, bool isReciprocal) const;
   void renderTruncImm(MachineInstrBuilder &MIB, const MachineInstr &MI,
                       int OpIdx = -1) const;
   void renderLogicalImm32(MachineInstrBuilder &MIB, const MachineInstr &I,
@@ -7918,24 +7930,29 @@ AArch64InstructionSelector::selectExtractHigh(MachineOperand &Root) const {
 }
 
 InstructionSelector::ComplexRendererFns
-AArch64InstructionSelector::selectCVTFixedPointVecBase(
-    const MachineOperand &Root, bool isReciprocal) const {
+AArch64InstructionSelector::selectCVTFixedPointBase(const MachineOperand &Root,
+                                                    unsigned DstElementWidth,
+                                                    bool isReciprocal) const {
   if (!Root.isReg())
     return std::nullopt;
   const MachineRegisterInfo &MRI =
       Root.getParent()->getParent()->getParent()->getRegInfo();
 
-  MachineInstr *Dup = getDefIgnoringCopies(Root.getReg(), MRI);
-  if (Dup->getOpcode() != AArch64::G_DUP)
-    return std::nullopt;
+  Register Reg = Root.getReg();
+  MachineInstr *Dup = getDefIgnoringCopies(Reg, MRI);
+
+  if (Dup && Dup->getOpcode() == AArch64::G_DUP)
+    Reg = Dup->getOperand(1).getReg();
+
   std::optional<ValueAndVReg> CstVal =
-      getAnyConstantVRegValWithLookThrough(Dup->getOperand(1).getReg(), MRI);
+      getAnyConstantVRegValWithLookThrough(Reg, MRI);
+
   if (!CstVal)
     return std::nullopt;
 
-  unsigned RegWidth = MRI.getType(Root.getReg()).getScalarSizeInBits();
+  unsigned SrcElementWidth = MRI.getType(Reg).getScalarSizeInBits();
   APFloat FVal(0.0);
-  switch (RegWidth) {
+  switch (SrcElementWidth) {
   case 16:
     FVal = APFloat(APFloat::IEEEhalf(), CstVal->Value);
     break;
@@ -7949,21 +7966,38 @@ AArch64InstructionSelector::selectCVTFixedPointVecBase(
     return std::nullopt;
   };
   if (unsigned FBits =
-          CheckFixedPointOperandConstant(FVal, RegWidth, isReciprocal))
+          CheckFixedPointOperandConstant(FVal, DstElementWidth, isReciprocal))
     return {{[=](MachineInstrBuilder &MIB) { MIB.addImm(FBits); }}};
 
   return std::nullopt;
 }
 
+unsigned AArch64InstructionSelector::getFixedPointWidthFromOperand(
+    const MachineOperand &Root) const {
+  return Root.getParent()
+      ->getMF()
+      ->getRegInfo()
+      .getType(Root.getReg())
+      .getScalarSizeInBits();
+}
+
+template <unsigned Width>
+InstructionSelector::ComplexRendererFns
+AArch64InstructionSelector::selectCVTFixedPoint(MachineOperand &Root) const {
+  return selectCVTFixedPointBase(Root, Width, /*isReciprocal*/ false);
+}
+
 InstructionSelector::ComplexRendererFns
 AArch64InstructionSelector::selectCVTFixedPointVec(MachineOperand &Root) const {
-  return selectCVTFixedPointVecBase(Root, /*isReciprocal*/ false);
+  return selectCVTFixedPointBase(Root, getFixedPointWidthFromOperand(Root),
+                                 /*isReciprocal*/ false);
 }
 
 InstructionSelector::ComplexRendererFns
 AArch64InstructionSelector::selectCVTFixedPosRecipOperandVec(
     MachineOperand &Root) const {
-  return selectCVTFixedPointVecBase(Root, /*isReciprocal*/ true);
+  return selectCVTFixedPointBase(Root, getFixedPointWidthFromOperand(Root),
+                                 /*isReciprocal*/ true);
 }
 
 void AArch64InstructionSelector::renderFixedPointScalarXForm(
@@ -7973,26 +8007,54 @@ void AArch64InstructionSelector::renderFixedPointScalarXForm(
   MIB.addImm(MI.getOperand(OpIdx).getImm());
 }
 
-void AArch64InstructionSelector::renderFixedPointXForm(MachineInstrBuilder &MIB,
-                                                       const MachineInstr &MI,
-                                                       int OpIdx) const {
+void AArch64InstructionSelector::renderFixedPointImm(MachineInstrBuilder &MIB,
+                                                     const MachineOperand &Root,
+                                                     unsigned Width,
+                                                     bool isReciprocal) const {
   // FIXME: This is only needed to satisfy the type checking in tablegen, and
   // should be able to reuse the Renderers already calculated by
-  // selectCVTFixedPointVecBase.
+  // selectCVTFixedPointBase.
   InstructionSelector::ComplexRendererFns Renderer =
-      selectCVTFixedPointVecBase(MI.getOperand(OpIdx), /*isReciprocal*/ false);
+      selectCVTFixedPointBase(Root, Width, isReciprocal);
   assert((Renderer && Renderer->size() == 1) &&
-         "Expected selectCVTFixedPointVec to provide a function\n");
+         "Expected selectCVTFixedPointBase to provide a function\n");
   (Renderer->front())(MIB);
 }
 
-void AArch64InstructionSelector::renderFixedPointRecipXForm(
+template <unsigned Width>
+void AArch64InstructionSelector::renderFixedPointXForm(MachineInstrBuilder &MIB,
+                                                       const MachineInstr &MI,
+                                                       int OpIdx) const {
+  renderFixedPointImm(MIB, MI.getOperand(OpIdx), Width,
+                      /*isReciprocal*/ false);
+}
+
+void AArch64InstructionSelector::renderFixedPointXForm32(
     MachineInstrBuilder &MIB, const MachineInstr &MI, int OpIdx) const {
-  InstructionSelector::ComplexRendererFns Renderer =
-      selectCVTFixedPointVecBase(MI.getOperand(OpIdx), /*isReciprocal*/ true);
-  assert((Renderer && Renderer->size() == 1) &&
-         "Expected selectCVTFixedPosRecipOperandVec to provide a function\n");
-  (Renderer->front())(MIB);
+  // GIsel Renderers cannot use templates in .td so function name used to pass
+  // the width
+  renderFixedPointXForm<32>(MIB, MI, OpIdx);
+}
+
+void AArch64InstructionSelector::renderFixedPointXForm64(
+    MachineInstrBuilder &MIB, const MachineInstr &MI, int OpIdx) const {
+  renderFixedPointXForm<64>(MIB, MI, OpIdx);
+}
+
+void AArch64InstructionSelector::renderFixedPointXFormVec(
+    MachineInstrBuilder &MIB, const MachineInstr &MI, int OpIdx) const {
+  const MachineOperand &Root = MI.getOperand(OpIdx);
+  unsigned Width = getFixedPointWidthFromOperand(Root);
+
+  renderFixedPointImm(MIB, Root, Width, /*isReciprocal*/ false);
+}
+
+void AArch64InstructionSelector::renderFixedPointRecipXFormVec(
+    MachineInstrBuilder &MIB, const MachineInstr &MI, int OpIdx) const {
+  const MachineOperand &Root = MI.getOperand(OpIdx);
+  unsigned Width = getFixedPointWidthFromOperand(Root);
+
+  renderFixedPointImm(MIB, Root, Width, /*isReciprocal*/ true);
 }
 
 void AArch64InstructionSelector::renderTruncImm(MachineInstrBuilder &MIB,
