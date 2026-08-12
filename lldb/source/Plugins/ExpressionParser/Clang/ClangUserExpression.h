88154238ffede58a71acd06072a23c138d335751
Lang Hames <lhames@gmail.com>
2026-08-12 15:03:56 +1000

    Add a unit test.

--- lldb/source/Plugins/ExpressionParser/Clang/ClangUserExpression.h
+++ lldb/source/Plugins/ExpressionParser/Clang/ClangUserExpression.h
@@ -43,6 +43,9 @@ class ClangUserExpression : public LLVMUserExpression {
   // LLVM RTTI support
   static char ID;
 
+  /// Grants the unit test access to private members.
+  friend class ClangUserExpressionTest;
+
 public:
   bool isA(const void *ClassID) const override {
     return ClassID == &ID || LLVMUserExpression::isA(ClassID);
@@ -209,8 +212,10 @@ private:
                         std::vector<std::string> modules_to_import,
                         bool for_completion);
 
-  lldb::addr_t GetCppObjectPointer(lldb::StackFrameSP frame,
-                                   llvm::StringRef object_name, Status &err);
+  /// Get the object pointer ("this") for a C++ method context.
+  static lldb::addr_t GetCppObjectPointer(lldb::StackFrameSP frame,
+                                          llvm::StringRef object_name,
+                                          Status &err);
 
   void
   FixupCVRParseErrorDiagnostics(DiagnosticManager &diagnostic_manager) const;
