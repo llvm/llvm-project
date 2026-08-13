495d6be658b4b8af36b2d7f77517b299b183e696
Vladislav Dzhidzhoev <vdzhidzhoev@accesssoftek.com>
2026-08-13 18:26:19 +0200

    [clang][DebugInfo] Emit static local variables in their lexical block scope (#213153)
    With the backend support from #187927 in place, emit DIGlobalVariable
    nodes for static local variables in their corresponding DILexicalBlock
    scope. This allows debug info consumers to distinguish between static
    local variables with the same name declared in different lexical blocks.
    
    Fixes https://github.com/llvm/llvm-project/issues/19612.
    
    Authored-by: Kristina Bessonova <kbessonova@accesssoftek.com>

--- clang/lib/CodeGen/CGDebugInfo.cpp
+++ clang/lib/CodeGen/CGDebugInfo.cpp
@@ -4631,6 +4631,14 @@ void CGDebugInfo::collectVarDeclProps(const VarDecl *VD, llvm::DIFile *&Unit,
     TemplateParameters = nullptr;
   }
 
+  // Get context for static locals (that are technically globals) the same way
+  // we do for "local" locals -- by using current lexical block.
+  if (VD->isStaticLocal()) {
+    assert(!LexicalBlockStack.empty() && "Region stack mismatch, stack empty!");
+    VDContext = LexicalBlockStack.back();
+    return;
+  }
+
   // Since we emit declarations (DW_AT_members) for static members, place the
   // definition of those static members in the namespace they were declared in
   // in the source code (the lexical decl context).
