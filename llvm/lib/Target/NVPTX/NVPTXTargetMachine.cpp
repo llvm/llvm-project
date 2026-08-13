3dbb75f469f99842ce4c981ce4f09e6e9191d638
Aiden Grossman <aidengrossman@google.com>
2026-08-13 08:02:59 -0700

    [NewPM] Register AsmPrinter passes like normal (#215965)
    This avoids needing to do anything custom in
    registerPassBuilderCallbacks. Also does some cleanup around inconsistent
    naming.
    
    I'm not really sure why I didn't do this before. Eventually I think we
    want to delete AsmPrinterBegin and AsmPrinterEnd, but that requires a
    lot of work that we probably can't do until we delete the LegacyPM
    version of AsmPrinter.

--- llvm/lib/Target/NVPTX/NVPTXTargetMachine.cpp
+++ llvm/lib/Target/NVPTX/NVPTXTargetMachine.cpp
@@ -203,16 +203,6 @@ void NVPTXTargetMachine::registerPassBuilderCallbacks(PassBuilder &PB) {
 #define GET_PASS_REGISTRY "NVPTXPassRegistry.def"
 #include "llvm/Passes/TargetPassRegistry.inc"
 
-  // TODO: Move this into the base CodeGenPassBuilder once all targets that
-  // currently implement it have a ported asm-printer pass.
-  if (PIC) {
-    PIC->addClassToPassName(NVPTXAsmPrinterBeginPass::name(),
-                            "nvptx-asm-printer-begin");
-    PIC->addClassToPassName(NVPTXAsmPrinterPass::name(), "nvptx-asm-printer");
-    PIC->addClassToPassName(NVPTXAsmPrinterEndPass::name(),
-                            "nvptx-asm-printer-end");
-  }
-
   PB.registerPipelineStartEPCallback(
       [this](ModulePassManager &PM, OptimizationLevel Level) {
         // We do not want to fold out calls to nvvm.reflect early if the user
