f69d5f01b75b9f508dd563cd97d470f9e0d927d7
Max Desiatov <m_desiatov@apple.com>
2026-08-13 09:15:15 +0100

    [rebranch] locale_base_api.h: Add conditional include for ibm.h (#13771)
    Conditional inclusion of ibm.h for specific platforms.

--- libcxx/include/__locale_dir/locale_base_api.h
+++ libcxx/include/__locale_dir/locale_base_api.h
@@ -134,7 +134,9 @@
 //       (by providing global non-reserved names) and the new API. As we move individual platforms
 //       towards the new way of defining the locale base API, this should disappear since each platform
 //       will define those directly.
-#    include <__locale_dir/locale_base_api/ibm.h>
+#    if defined(_AIX) || defined(__MVS__)
+#      include <__locale_dir/locale_base_api/ibm.h>
+#    endif
 
 #    include <__locale_dir/locale_base_api/bsd_locale_fallbacks.h>
 
