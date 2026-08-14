8ff46ae30c4274cdcc6c6c08b47042ab7cefe84f
Orlando Cazalet-Hyams <orlando.hyams@sony.com>
2026-08-14 11:28:37 +0100

    Revert "[dyndbg][Clang] Implement nested-ELF dynamic debugging support" (#216298)
    Reverts llvm/llvm-project#194860
    
    failure example:
    https://lab.llvm.org/buildbot/#/builders/154/builds/35126

--- llvm/lib/Transforms/Utils/ModuleUtils.cpp
+++ llvm/lib/Transforms/Utils/ModuleUtils.cpp
@@ -381,10 +381,8 @@ std::string llvm::getUniqueModuleId(Module *M) {
   return ("." + Str).str();
 }
 
-GlobalVariable *llvm::embedBufferInModule(Module &M, MemoryBufferRef Buf,
-                                          StringRef SectionName,
-                                          Align Alignment,
-                                          bool SectionExclude) {
+void llvm::embedBufferInModule(Module &M, MemoryBufferRef Buf,
+                               StringRef SectionName, Align Alignment) {
   // Embed the memory buffer into the module.
   Constant *ModuleConstant = ConstantDataArray::get(
       M.getContext(), ArrayRef(Buf.getBufferStart(), Buf.getBufferSize()));
@@ -398,16 +396,11 @@ GlobalVariable *llvm::embedBufferInModule(Module &M, MemoryBufferRef Buf,
   NamedMDNode *MD = M.getOrInsertNamedMetadata("llvm.embedded.objects");
   Metadata *MDVals[] = {ConstantAsMetadata::get(GV),
                         MDString::get(Ctx, SectionName)};
-  MD->addOperand(llvm::MDNode::get(Ctx, MDVals));
 
-  if (SectionExclude)
-    GV->setMetadata(LLVMContext::MD_exclude, llvm::MDNode::get(Ctx, {}));
-  else
-    GV->setMetadata(LLVMContext::MD_metadata_section_kind,
-                    llvm::MDNode::get(Ctx, {}));
+  MD->addOperand(llvm::MDNode::get(Ctx, MDVals));
+  GV->setMetadata(LLVMContext::MD_exclude, llvm::MDNode::get(Ctx, {}));
 
   appendToCompilerUsed(M, GV);
-  return GV;
 }
 
 bool llvm::lowerGlobalIFuncUsersAsGlobalCtor(
