75a333b887fe96726716ea89bf2c6cb440dd4b43
WANG Rui <wangrui@loongson.cn>
2026-08-14 16:34:05 +0800

    Address weining's comments

--- llvm/lib/Target/LoongArch/LoongArchLateBranchOpt.cpp
+++ llvm/lib/Target/LoongArch/LoongArchLateBranchOpt.cpp
@@ -1,5 +1,4 @@
-//===-- LoongArchLateBranchOpt.cpp - Late Stage Branch Optimization
-//-----------===//
+//===---- LoongArchLateBranchOpt.cpp - Late Stage Branch Optimization -----===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
