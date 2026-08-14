04d371f62768674df7bc57cee120dc1c5d95cd77
pawandaware77 <pawandaware77@example.com>
2026-08-14 19:56:40 +0530

    [libc][math] Implement double-precision erfc

---
+++ libc/src/math/erfc.h
@@ -0,0 +1,12 @@
+#ifndef LLVM_LIBC_SRC_MATH_ERFC_H
+#define LLVM_LIBC_SRC_MATH_ERFC_H
+
+#include "src/__support/macros/config.h"
+
+namespace LIBC_NAMESPACE_DECL {
+
+double erfc(double x);
+
+} // namespace LIBC_NAMESPACE_DECL
+
+#endif // LLVM_LIBC_SRC_MATH_ERFC_H
