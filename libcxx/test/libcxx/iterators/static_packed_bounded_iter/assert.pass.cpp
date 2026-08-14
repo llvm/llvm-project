8ffefdb3d8854a6764e97a0baca4d88f5ac7ae51
William Tran-Viet <wtranviet@proton.me>
2026-08-14 19:16:15 -0400

    Use new test hardening assertions check

--- libcxx/test/libcxx/iterators/static_packed_bounded_iter/assert.pass.cpp
+++ libcxx/test/libcxx/iterators/static_packed_bounded_iter/assert.pass.cpp
@@ -7,7 +7,7 @@
 //===----------------------------------------------------------------------===//
 
 // REQUIRES: std-at-least-c++26
-// UNSUPPORTED: libcpp-hardening-mode=none
+// UNSUPPORTED: can-test-hardening-assertions-fast
 
 // template <class _Ptr, class _Tag, class _RangeCapacity>
 // class __static_packed_bounded_iterator;
