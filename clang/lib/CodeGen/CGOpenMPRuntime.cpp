221de9ecab858b43696b4b83c649cda816c4903e
Zahira Ammarguellat <zahira.ammarguellat@intel.com>
2026-08-14 07:08:16 -0700

    Removed getCanonicalDecl

--- clang/lib/CodeGen/CGOpenMPRuntime.cpp
+++ clang/lib/CodeGen/CGOpenMPRuntime.cpp
@@ -3355,10 +3355,10 @@ emitTaskPrivateMappingFunction(CodeGenModule &CGM, SourceLocation Loc,
             .withConst()
             .withRestrict(),
         ImplicitParamKind::Other));
-    const Decl *D = cast<DeclRefExpr>(E)->getDecl()->getCanonicalDecl();
+    const Decl *D = cast<DeclRefExpr>(E)->getDecl();
     const VarDecl *VD;
     if (const auto *BD = dyn_cast<BindingDecl>(D))
-      VD = cast<VarDecl>(BD->getDecomposedDecl()->getCanonicalDecl());
+      VD = cast<VarDecl>(BD->getDecomposedDecl());
     else
       VD = cast<VarDecl>(D);
     PrivateVarsPos[VD] = Counter;
@@ -3371,10 +3371,10 @@ emitTaskPrivateMappingFunction(CodeGenModule &CGM, SourceLocation Loc,
             .withConst()
             .withRestrict(),
         ImplicitParamKind::Other));
-    const Decl *D = cast<DeclRefExpr>(E)->getDecl()->getCanonicalDecl();
+    const Decl *D = cast<DeclRefExpr>(E)->getDecl();
     const VarDecl *VD;
     if (const auto *BD = dyn_cast<BindingDecl>(D))
-      VD = cast<VarDecl>(BD->getDecomposedDecl()->getCanonicalDecl());
+      VD = cast<VarDecl>(BD->getDecomposedDecl());
     else
       VD = cast<VarDecl>(D);
     PrivateVarsPos[VD] = Counter;
@@ -3387,10 +3387,10 @@ emitTaskPrivateMappingFunction(CodeGenModule &CGM, SourceLocation Loc,
             .withConst()
             .withRestrict(),
         ImplicitParamKind::Other));
-    const Decl *D = cast<DeclRefExpr>(E)->getDecl()->getCanonicalDecl();
+    const Decl *D = cast<DeclRefExpr>(E)->getDecl();
     const VarDecl *VD;
     if (const auto *BD = dyn_cast<BindingDecl>(D))
-      VD = cast<VarDecl>(BD->getDecomposedDecl()->getCanonicalDecl());
+      VD = cast<VarDecl>(BD->getDecomposedDecl());
     else
       VD = cast<VarDecl>(D);
     PrivateVarsPos[VD] = Counter;
@@ -3445,11 +3445,9 @@ emitTaskPrivateMappingFunction(CodeGenModule &CGM, SourceLocation Loc,
     const VarDecl *LookupVD;
     if (Privates[Counter].second.OriginalRef) {
       const Decl *OrigDecl =
-          cast<DeclRefExpr>(Privates[Counter].second.OriginalRef)
-              ->getDecl()
-              ->getCanonicalDecl();
+          cast<DeclRefExpr>(Privates[Counter].second.OriginalRef)->getDecl();
       if (const auto *BD = dyn_cast<BindingDecl>(OrigDecl)) {
-        LookupVD = cast<VarDecl>(BD->getDecomposedDecl()->getCanonicalDecl());
+        LookupVD = cast<VarDecl>(BD->getDecomposedDecl());
       } else {
         LookupVD = Privates[Counter].second.Original;
       }
