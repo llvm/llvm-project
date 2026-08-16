92e224009da1652778080e84430af8cc9c867e65
shyeyian <shyeyian@mail.shyeyian.com>
2026-08-16 22:18:35 +0800

    update deref.pass

--- libcxx/test/std/ranges/range.adaptors/range.chunk/range.chunk.inner.iter/deref.pass.cpp
+++ libcxx/test/std/ranges/range.adaptors/range.chunk/range.chunk.inner.iter/deref.pass.cpp
@@ -18,15 +18,22 @@
 #include <ranges>
 #include <vector>
 
-#include "../types.h"
+#include "test_iterators.h"
 
 constexpr bool test() {
-  std::vector<int> vector                                = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
-  std::ranges::chunk_view<input_span<int>> input_chunked = input_span<int>(vector) | std::views::chunk(3);
-
   // Test `constexpr range_reference_v<V> inner_iterator::operator*() const`
   {
-    /*chunk_view::__inner_iterator*/ std::input_iterator auto it = (*input_chunked.begin()).begin();
+    std::vector<int> vector = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    std::ranges::chunk_view<
+        std::ranges::subrange<cpp17_input_iterator<int*>, sentinel_wrapper<cpp17_input_iterator<int*>>>>
+        chunked =
+            std::ranges::subrange<cpp17_input_iterator<int*>, sentinel_wrapper<cpp17_input_iterator<int*>>>(
+                cpp17_input_iterator<int*>(vector.data()),
+                sentinel_wrapper<cpp17_input_iterator<int*>>(
+                    cpp17_input_iterator<int*>(vector.data() + vector.size()))) |
+            std::views::chunk(3);
+
+    /*chunk_view::__inner_iterator*/ std::input_iterator auto it = (*chunked.begin()).begin();
     std::same_as<int&> decltype(auto) v                          = *it;
     assert(v == 1);
   }
