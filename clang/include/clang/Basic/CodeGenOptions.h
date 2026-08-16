9cc4cbe819b0a6977aa108c2a2208217539906bf
Orlando Cazalet-Hyams <orlandoch.och@gmail.com>
2026-08-16 13:32:27 +0100

    Update CodeGenOptions.h
    rm whitespace (that doesn't seem to exist locally for me? but I can seem to edit it on GH editor)

--- clang/include/clang/Basic/CodeGenOptions.h
+++ clang/include/clang/Basic/CodeGenOptions.h
@@ -399,7 +399,7 @@ public:
 
   /// Prefix to use for -save-dynamic-debugging-temps output.
   std::string SaveDynDbgTempsFilePrefix;
-  
+
   /// Name of file passed with -foffload-include-binary option to forward to
   /// offloading runtime back-end for incorporating them into host-side object
   /// file.
