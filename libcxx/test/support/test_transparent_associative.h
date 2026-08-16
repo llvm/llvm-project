1c6a0a0aceb5234b381328dc55fd7fd1b862ab61
rsaddatimov <rafa.saddatimov@gmail.com>
2026-08-16 17:39:35 +0300

    revert to test_std_ver >= 23

--- libcxx/test/support/test_transparent_associative.h
+++ libcxx/test/support/test_transparent_associative.h
@@ -11,14 +11,12 @@
 
 #include "test_macros.h"
 
-#if TEST_STD_VER < 23
-#  error "This header is supported only since C++23"
-#endif // TEST_STD_VER < 23
+#if TEST_STD_VER >= 23
 
-#include <cassert>
-#include <compare>
-#include <concepts>
-#include <type_traits>
+#  include <cassert>
+#  include <compare>
+#  include <concepts>
+#  include <type_traits>
 
 template <typename T>
 struct StoredType;
@@ -141,5 +139,7 @@ constexpr void test_non_transparent_extract(Container c) {
   assert(c.extract(SearchedType<int>(3, &conversions)).empty());
   assert(conversions == 3);
 }
+
+#endif // TEST_STD_VER >= 23
 
 #endif // TEST_TRANSPARENT_ASSOCIATIVE_H
