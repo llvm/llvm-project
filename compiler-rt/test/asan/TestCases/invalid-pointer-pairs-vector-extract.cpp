92e120d535db865b1fe8e05352d24898fb8a4aaa
flovent <flbven@protonmail.com>
2026-08-16 19:27:38 +0800

    [ASan] Add xfail test for PR #213546 (#216532)
    Add xfail test for PR #213546

---
+++ compiler-rt/test/asan/TestCases/invalid-pointer-pairs-vector-extract.cpp
@@ -0,0 +1,35 @@
+// REQUIRES: asserts
+// RUN: %clangxx_asan -O2 %s -o %t -mllvm -asan-detect-invalid-pointer-pair
+// RUN: %env_asan_opts=detect_invalid_pointer_pairs=1:halt_on_error=0 %run %t 2>&1 | FileCheck %s
+// RUN: %env_asan_opts=detect_invalid_pointer_pairs=2:halt_on_error=0 %run %t 2>&1 | FileCheck %s
+
+// UNSUPPORTED: windows
+// XFAIL: *
+
+#include <cstdint>
+#include <stdio.h>
+#include <stdlib.h>
+
+__attribute__((noinline)) void
+span_lengths(const char **begins, const char **ends, int64_t *lengths) {
+  // CHECK: ERROR: AddressSanitizer: invalid-pointer-pair
+  // CHECK: #{{[0-9]+ .*}} in span_lengths
+  lengths[0] = ends[0] - begins[0];
+  // CHECK: ERROR: AddressSanitizer: invalid-pointer-pair
+  // CHECK: #{{[0-9]+ .*}} in span_lengths
+  lengths[1] = ends[1] - begins[1];
+}
+
+int main() {
+  char *a = (char *)malloc(16);
+  char *b = (char *)malloc(16);
+  const char *begins[2] = {a, b};
+  const char *ends[2] = {b + 2, a + 10};
+  int64_t lengths[2] = {0};
+  span_lengths(begins, ends, lengths);
+  fprintf(stderr, "lengths: %lld %lld\n", (long long)lengths[0],
+          (long long)lengths[1]);
+  free(a);
+  free(b);
+  return 0;
+}
