8130542c777dd1771b16419b5f60244a9697235c
Lucas Mellone <github.snugness349@passinbox.com>
2026-08-17 23:00:41 +0200

    add: working tests

--- libcxx/test/std/numerics/c.math/copysign.pass.cpp
+++ libcxx/test/std/numerics/c.math/copysign.pass.cpp
@@ -19,53 +19,72 @@
 struct TestFloat {
   template <class T>
   TEST_CONSTEXPR_CXX23 void operator()() const {
-    using lim                    = std::numeric_limits<T>;
-    TEST_CONSTEXPR_CXX23 T max   = lim::max();
-    TEST_CONSTEXPR_CXX23 T min   = lim::min();
-    TEST_CONSTEXPR_CXX23 T d_min = lim::denorm_min();
-    TEST_CONSTEXPR_CXX23 T inf   = lim::infinity();
-    TEST_CONSTEXPR_CXX23 T nan   = lim::quiet_NaN();
-    TEST_CONSTEXPR_CXX23 T s_nan = lim::signaling_NaN();
+    using lim = std::numeric_limits<T>;
 
-    //assert(std::copysign(nan) == FP_NAN);
-    //assert(std::copysign(s_nan) == FP_NAN);
+    assert(std::isnan(std::copysign(lim::quiet_NaN(), T(-1))));
+    assert(std::signbit(std::copysign(lim::signaling_NaN(), T(-1))));
 
-    assert(std::copysign(inf, -inf) == -FP_INFINITE);
-    assert(std::copysign(-inf, inf) == FP_INFINITE);
+    assert(std::copysign(lim::infinity(), -lim::infinity()) == -lim::infinity());
+    assert(std::copysign(-lim::infinity(), lim::infinity()) == lim::infinity());
 
-    assert(std::copysign(T(1), T(-1)) == FP_NORMAL);
-    assert(std::copysign(T(-1), T(1)) == FP_NORMAL);
-    assert(std::copysign(max, -max) == -max);
-    assert(std::copysign(-max, max) == max);
+    assert(std::copysign(T(1), T(-1)) == T(-1));
+    assert(std::copysign(T(-1), T(1)) == T(1));
 
-    assert(std::copysign(d_min) == FP_SUBNORMAL);
+    assert(std::copysign(lim::max(), -lim::max()) == -lim::max());
+    assert(std::copysign(-lim::max(), lim::max()) == lim::max());
 
-    assert(std::copysign(T(0)) == FP_ZERO);
-    assert(std::copysign(T(-0.0)) == FP_ZERO);
+    assert(std::copysign(lim::denorm_min(), T(-1)) == -lim::denorm_min());
+    assert(std::copysign(-lim::denorm_min(), T(1)) == lim::denorm_min());
+
+    assert(std::copysign(T(0), T(-0.0)) == T(0));
+    assert(std::copysign(T(-0.0), T(0)) == T(-0.0));
   }
 };
 
 struct TestInt {
   template <class T>
   TEST_CONSTEXPR_CXX23 void operator()() const {
-    using lim                  = std::numeric_limits<T>;
-    TEST_CONSTEXPR_CXX23 T max = lim::max();
-    TEST_CONSTEXPR_CXX23 T low = lim::lowest();
+    using lim = std::numeric_limits<T>;
 
-    assert(std::copysign(T(0)) == FP_ZERO);
-    assert(std::copysign(T(1)) == FP_NORMAL);
-    assert(std::copysign(max) == FP_NORMAL);
+    // no negative-zero bit pattern.
+    assert(std::copysign(lim::max(), T(-0)) == static_cast<double>(lim::max()));
+    assert(std::copysign(T(-0), lim::max()) == T(0));
 
-    if (std::is_signed<T>::value) {
-      assert(std::copysign(low) == FP_NORMAL);
-      assert(std::copysign(T(-1)) == FP_NORMAL);
+    if (lim::is_signed) {
+      assert(std::copysign(T(1), T(-1)) == T(-1));
+      assert(std::copysign(T(-1), T(1)) == T(1));
+
+      assert(std::copysign(T(1), lim::lowest()) == T(-1));
+      assert(std::copysign(lim::lowest(), T(-1)) == lim::lowest());
     }
   }
 };
 
 TEST_CONSTEXPR_CXX23 bool test() {
+  using lim = std::numeric_limits<double>;
+
   types::for_each(types::floating_point_types(), TestFloat());
   types::for_each(types::integral_types(), TestInt());
+
+  // Make sure we can call `std::copysign` with mixed-type promotions.
+  {
+    assert(std::copysign(2.0, -1) == -2.0);                  // double vs int
+    assert(std::copysign(-1, 2.0f) == 1);                    // int vs float
+    assert(std::copysign(2.0L, -1.0f) == -2.0L);             // long double vs float
+    assert(std::isnan(std::copysign(lim::quiet_NaN(), -1))); // NaN vs int
+  }
+
+  // std::copysignf(float, float).
+  {
+    assert(std::copysignf(2.0f, -1.0f) == -2.0f);
+    assert(std::copysignf(-2.0f, 1.0f) == 2.0f);
+  }
+
+  // std::copysignl(long double, long double).
+  {
+    assert(std::copysignl(2.0L, -1.0L) == -2.0L);
+    assert(std::copysignl(-2.0L, 1.0L) == 2.0L);
+  }
 
   return true;
 }
