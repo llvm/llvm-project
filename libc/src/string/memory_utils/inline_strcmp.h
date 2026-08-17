766e4e637b3cc7ddd90e690d8b651f18a2f58199
yahia ahmed <yahia.a.abdrabou@gmail.com>
2026-08-18 00:45:16 +0300

    [libc][string] Compare words in inline_strcmp to compare 8 bytes at a timeg

--- libc/src/string/memory_utils/inline_strcmp.h
+++ libc/src/string/memory_utils/inline_strcmp.h
@@ -19,6 +19,7 @@ namespace LIBC_NAMESPACE_DECL {
 
 constexpr int PAGE_MASK = 4095;
 constexpr int PAGE_SAFE_OFFSET = 4088;
+constexpr int BLOCK_SIZE = sizeof(uint64_t);
 
 LIBC_INLINE uint64_t is_null_terminated(uint64_t v) {
   return (v - 0x0101010101010101ULL) & ~v & 0x8080808080808080ULL;
@@ -34,15 +35,18 @@ template <typename Comp>
 LIBC_INLINE constexpr int inline_strcmp(const char *left, const char *right,
                                         Comp &&comp) {
   // Page boundry check fallback to generic version
+#if defined(LIBC_COPT_STRING_COMPARE_IMPL)
   if (LIBC_UNLIKELY((reinterpret_cast<uintptr_t>(left) & PAGE_MASK) >
                         PAGE_SAFE_OFFSET ||
                     (reinterpret_cast<uintptr_t>(right) & PAGE_MASK) >
                         PAGE_SAFE_OFFSET)) {
+#endif
     for (; *left && !comp(*left, *right); ++left, ++right)
       ;
     return comp(static_cast<unsigned char>(*left),
                 static_cast<unsigned char>(*right));
   }
+#if defined(LIBC_COPT_STRING_COMPARE_IMPL)
   while (1) {
     uint64_t val1 = load(left);
     uint64_t val2 = load(right);
@@ -51,24 +55,28 @@ LIBC_INLINE constexpr int inline_strcmp(const char *left, const char *right,
     // Check for character mismatch or null terminator
     uint64_t zero_or_diff = diff | null_mask;
     if (zero_or_diff != 0) {
-      size_t byte_pos = __builtin_ctzll(zero_or_diff) >> 3;
+      size_t byte_pos = __builtin_ctzll(zero_or_diff) / BLOCK_SIZE;
       unsigned char c1 = static_cast<unsigned char>(left[byte_pos]);
       unsigned char c2 = static_cast<unsigned char>(right[byte_pos]);
       return comp(c1, c2);
     }
-    left += 8;
-    right += 8;
+    left += BLOCK_SIZE;
+    right += BLOCK_SIZE;
   }
 }
+#endif
 
 template <typename Comp>
 LIBC_INLINE constexpr int inline_strncmp(const char *left, const char *right,
                                          size_t n, Comp &&comp) {
   if (n == 0)
     return 0;
-
-  if (LIBC_UNLIKELY((reinterpret_cast<uintptr_t>(left) & 4095) > 4088 ||
-                    (reinterpret_cast<uintptr_t>(right) & 4095) > 4088)) {
+#if defined(LIBC_COPT_STRING_COMPARE_IMPL)
+  if (LIBC_UNLIKELY((reinterpret_cast<uintptr_t>(left) & PAGE_MASK) >
+                        PAGE_SAFE_OFFSET ||
+                    (reinterpret_cast<uintptr_t>(right) & PAGE_MASK) >
+                        PAGE_SAFE_OFFSET)) {
+#endif
     for (; n > 1; --n, ++left, ++right) {
       char lc = *left;
       if (!comp(lc, '\0') || comp(lc, *right))
@@ -77,10 +85,9 @@ LIBC_INLINE constexpr int inline_strncmp(const char *left, const char *right,
     return comp(static_cast<unsigned char>(*left),
                 static_cast<unsigned char>(*right));
   }
-
-  constexpr size_t block_size = sizeof(uint64_t) / 8;
-  for (; n >= block_size;
-       n -= block_size, left += block_size, right += block_size) {
+#if defined(LIBC_COPT_STRING_COMPARE_IMPL)
+  for (; n >= BLOCK_SIZE;
+       n -= BLOCK_SIZE, left += BLOCK_SIZE, right += BLOCK_SIZE) {
     uint64_t val1 = load(left);
     uint64_t val2 = load(right);
     uint64_t diff = val1 ^ val2;
@@ -88,7 +95,7 @@ LIBC_INLINE constexpr int inline_strncmp(const char *left, const char *right,
 
     uint64_t zero_or_diff = diff | null_mask;
     if (zero_or_diff != 0) {
-      size_t byte_pos = __builtin_ctzll(zero_or_diff) >> 3;
+      size_t byte_pos = __builtin_ctzll(zero_or_diff) / BLOCK_SIZE;
       // If the difference happens past 'n' remaining bytes, they are equal up
       // to n
       if (byte_pos >= n)
@@ -107,7 +114,7 @@ LIBC_INLINE constexpr int inline_strncmp(const char *left, const char *right,
   return comp(static_cast<unsigned char>(*left),
               static_cast<unsigned char>(*right));
 }
-
+#endif
 } // namespace LIBC_NAMESPACE_DECL
 
 #endif // LLVM_LIBC_SRC_STRING_MEMORY_UTILS_INLINE_STRCMP_H
