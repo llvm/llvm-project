6ee252efa3e2a85cfedc3ffcae7f5951017f1758
shyeyian <shyeyian@mail.shyeyian.com>
2026-08-17 16:56:06 +0800

    update increment.pass

--- libcxx/test/std/ranges/range.adaptors/range.chunk/range.chunk.iter/increment.pass.cpp
+++ libcxx/test/std/ranges/range.adaptors/range.chunk/range.chunk.iter/increment.pass.cpp
@@ -18,31 +18,52 @@
 
 #include <algorithm>
 #include <cassert>
-#include <iterator>
+#include <concepts>
 #include <ranges>
 #include <vector>
 
+#include "test_iterators.h"
+
 constexpr bool test() {
-  std::vector<int> vector                                                  = {1, 2, 3, 4, 5, 6, 7, 8};
-  std::ranges::chunk_view<std::ranges::ref_view<std::vector<int>>> chunked = vector | std::views::chunk(2);
+  std::vector<int> vector = {1, 2, 3, 4, 5, 6, 7, 8};
 
-  // Test `constexpr iterator& operator++();`
-  {
-    /*chunk_view::__iterator*/ std::forward_iterator auto it = chunked.begin();
-    assert(std::ranges::equal(*++it, std::vector{3, 4}));
-  }
-
+  // Test `constexpr iterator& operator++()`
   // Test `constexpr iterator operator++(int)`
   {
-    /*chunk_view::__iterator*/ std::forward_iterator auto it = chunked.begin();
-    it++;
+    std::ranges::chunk_view<
+        std::ranges::
+            subrange<forward_iterator<int*>, forward_iterator<int*>, std::ranges::subrange_kind::sized>>
+        chunked(std::ranges::subrange<forward_iterator<int*>,
+                                      forward_iterator<int*>,
+                                      std::ranges::subrange_kind::sized>(
+                    forward_iterator<int*>(vector.data()),
+                    forward_iterator<int*>(vector.data() + vector.size()),
+                    vector.size()),
+                2);
+    auto it                                                                         = chunked.begin();
+    std::same_as<std::ranges::iterator_t<decltype(chunked)>&> decltype(auto) result = ++it;
+    assert(&result == &it);
     assert(std::ranges::equal(*it, std::vector{3, 4}));
+    std::same_as<std::ranges::iterator_t<decltype(chunked)>> decltype(auto) it2 = it++;
+    assert(std::ranges::equal(*it, std::vector{5, 6}));
+    assert(std::ranges::equal(*it2, std::vector{3, 4}));
   }
 
   // Test `constexpr iterator& operator+=(difference_type)`
   {
-    /*chunk_view::__iterator*/ std::random_access_iterator auto it = chunked.begin();
-    it += 3;
+    std::ranges::chunk_view<
+        std::ranges::
+            subrange<random_access_iterator<int*>, random_access_iterator<int*>>>
+        chunked(std::ranges::subrange<random_access_iterator<int*>,
+                                      random_access_iterator<int*>>(
+                    random_access_iterator<int*>(vector.data()),
+                    random_access_iterator<int*>(vector.data() + vector.size()),
+                    vector.size()),
+                2);
+    auto it = chunked.begin();
+    it += 1;
+    assert(std::ranges::equal(*it, std::vector{3, 4}));
+    it += 2;
     assert(std::ranges::equal(*it, std::vector{7, 8}));
   }
 
