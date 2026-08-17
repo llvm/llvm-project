f2608f0f4884eaef24fc7b68c7322166c64958b5
Bhavesh Mandalapu <bmandalapu@google.com>
2026-08-17 13:28:34 -0700

    Fix comment

--- llvm/cmake/modules/MLGOLower.cmake
+++ llvm/cmake/modules/MLGOLower.cmake
@@ -60,7 +60,9 @@ function(mlgo_lower_models models mlir_opt mlir_translate target_type
     # Pass pipeline to lower MLIR models to EmitC dialect
     # TODO: Simplify with builtin pipeline for translation.
     # TODO: It isn't ideal to hardcode this pass pipeline here. It would be better to use the transform dialect.
-    # TODO: This pipeline is tested independently in . Make sure that tests and this CMake logic share the definition.
+    # TODO: This pipeline is tested independently in the following files. Make sure that tests and this CMake logic share the definition.
+    # mlir/test/Integration/Dialect/EmitC/inline-oz-test-model-tosa.mlir
+    # mlir/test/Integration/Dialect/EmitC/regalloc-eviction-test-model-tosa.mlir
     set(mlir_passes
       "func.func(tosa-to-linalg-named,tosa-to-linalg,tosa-to-arith,tosa-to-tensor)"
       "symbol-privatize"
