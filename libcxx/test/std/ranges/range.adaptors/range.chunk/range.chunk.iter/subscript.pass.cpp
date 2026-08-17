d671a01ad0b144d1e86c2eb20ad3354f70df4911
shyeyian <shyeyian@mail.shyeyian.com>
2026-08-17 17:10:04 +0800

    update subscript.pass

--- libcxx/test/std/ranges/range.adaptors/range.chunk/range.chunk.iter/subscript.pass.cpp
+++ libcxx/test/std/ranges/range.adaptors/range.chunk/range.chunk.iter/subscript.pass.cpp
@@ -21,15 +21,80 @@
 #include <ranges>
 #include <vector>
 
-constexpr bool test() {
-  std::vector<int> vector                                                  = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
-  std::ranges::chunk_view<std::ranges::ref_view<std::vector<int>>> chunked = vector | std::views::chunk(3);
+#include "test_iterators.h"
 
-  // Test `constexpr value_type iterator::operator[](difference_type n) const`
+constexpr bool test() {
+  std::vector<int> vector = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+
+  // Test `constexpr value_type iterator::operator[](difference_type) const`
   {
-    using Iterator = std::ranges::iterator_t<decltype(chunked)>;
-    static_assert(std::same_as<decltype(chunked.begin()[1]), typename Iterator::value_type>);
-    assert(std::ranges::equal(chunked.begin()[1], std::vector{4, 5, 6}));
+    // General
+    {
+      static_assert(std::ranges::random_access_range<
+                    std::ranges::subrange<random_access_iterator<int*>, sentinel_wrapper<random_access_iterator<int*>>>>);
+
+      std::ranges::chunk_view<
+          std::ranges::subrange<random_access_iterator<int*>, sentinel_wrapper<random_access_iterator<int*>>>>
+          chunked(std::ranges::subrange<random_access_iterator<int*>, sentinel_wrapper<random_access_iterator<int*>>>(
+                      random_access_iterator<int*>(vector.data()),
+                      sentinel_wrapper<random_access_iterator<int*>>(
+                          random_access_iterator<int*>(vector.data() + vector.size()))),
+                  3);
+      static_assert(
+          std::same_as<decltype(chunked.begin()[1]), typename std::ranges::iterator_t<decltype(chunked)>::value_type>);
+      assert(std::ranges::equal(chunked.begin()[1], std::vector{4, 5, 6}));
+    }
+
+    // The range is not fully divisible by the chunk size.
+    {
+      std::ranges::chunk_view<
+          std::ranges::subrange<random_access_iterator<int*>, sentinel_wrapper<random_access_iterator<int*>>>>
+          chunked(
+              std::ranges::subrange<random_access_iterator<int*>, sentinel_wrapper<random_access_iterator<int*>>>(
+                  random_access_iterator<int*>(vector.data()),
+                  sentinel_wrapper<random_access_iterator<int*>>(
+                      random_access_iterator<int*>(vector.data() + vector.size()))),
+              5);
+      assert(std::ranges::equal(chunked.begin()[2], std::vector{11, 12}));
+    }
+
+    // The chunk size is 1.
+    {
+      std::ranges::chunk_view<
+          std::ranges::subrange<random_access_iterator<int*>, sentinel_wrapper<random_access_iterator<int*>>>>
+          chunked(std::ranges::subrange<random_access_iterator<int*>, sentinel_wrapper<random_access_iterator<int*>>>(
+                     random_access_iterator<int*>(vector.data()),
+                     sentinel_wrapper<random_access_iterator<int*>>(
+                         random_access_iterator<int*>(vector.data() + vector.size()))),
+                 1);
+      assert(std::ranges::equal(chunked.begin()[2], std::vector{3}));
+    }
+
+    // The chunk size is larger than the range.
+    {
+      std::ranges::chunk_view<
+          std::ranges::subrange<random_access_iterator<int*>, sentinel_wrapper<random_access_iterator<int*>>>>
+          chunked(
+              std::ranges::subrange<random_access_iterator<int*>, sentinel_wrapper<random_access_iterator<int*>>>(
+                  random_access_iterator<int*>(vector.data()),
+                  sentinel_wrapper<random_access_iterator<int*>>(
+                      random_access_iterator<int*>(vector.data() + vector.size()))),
+              100);
+      assert(std::ranges::equal(chunked.begin()[0], std::vector{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}));
+    }
+
+    // The range has a single element.
+    {
+      std::vector<int> single_vector = {42};
+      std::ranges::chunk_view<
+          std::ranges::subrange<random_access_iterator<int*>, sentinel_wrapper<random_access_iterator<int*>>>>
+          chunked(std::ranges::subrange<random_access_iterator<int*>, sentinel_wrapper<random_access_iterator<int*>>>(
+                        random_access_iterator<int*>(single_vector.data()),
+                        sentinel_wrapper<random_access_iterator<int*>>(
+                            random_access_iterator<int*>(single_vector.data() + single_vector.size()))),
+                    3);
+      assert(std::ranges::equal(chunked.begin()[0], std::vector{42}));
+    }
   }
 
   return true;
