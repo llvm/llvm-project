72617f825c707c187495562f7a9378e0d5e7f73e
Jan Svoboda <jan_svoboda@apple.com>
2026-08-17 21:08:48 +0200

    Path separators in test

--- clang/test/Modules/non-modular-with-module-file.c
+++ clang/test/Modules/non-modular-with-module-file.c
@@ -8,17 +8,17 @@
 
 // RUN: FileCheck --input-file=%t/tu.ii %s -DPREFIX=%/t
 
-// CHECK:      # 1 "[[PREFIX]]/tu.c"
+// CHECK:      # 1 "[[PREFIX]]{{/|\\}}tu.c"
 // CHECK-NEXT: # 1 "<built-in>" 1
 // CHECK-NEXT: # 1 "<built-in>" 3
 // CHECK-NEXT: # {{[0-9]+}} "<built-in>" 3
 // CHECK-NEXT: # 1 "<command line>" 1
 // CHECK-NEXT: # 1 "<built-in>" 2
-// CHECK-NEXT: # 1 "[[PREFIX]]/tu.c" 2
-// CHECK-NEXT: # 1 "[[PREFIX]]/Mod.h" 1
+// CHECK-NEXT: # 1 "[[PREFIX]]{{/|\\}}tu.c" 2
+// CHECK-NEXT: # 1 "[[PREFIX]]{{/|\\}}Mod.h" 1
 // CHECK-NEXT: #pragma clang module begin Mod
-// CHECK-NEXT: # 2 "[[PREFIX]]/tu.c" 2
-// CHECK-NEXT: # 1 "[[PREFIX]]/tu.c"
+// CHECK-NEXT: # 2 "[[PREFIX]]{{/|\\}}tu.c" 2
+// CHECK-NEXT: # 1 "[[PREFIX]]{{/|\\}}tu.c"
 // CHECK-NEXT: #pragma clang module end /*Mod*/
 // CHECK-NOT:  #pragma clang module import
 
