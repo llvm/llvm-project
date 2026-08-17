1faf331604de2755f9de9de54d199d49dace1754
shyeyian <shyeyian@mail.shyeyian.com>
2026-08-17 14:00:10 +0800

    update compare.pass

--- libcxx/test/std/ranges/range.adaptors/range.chunk/range.chunk.iter/compare.pass.cpp
+++ libcxx/test/std/ranges/range.adaptors/range.chunk/range.chunk.iter/compare.pass.cpp
@@ -27,62 +27,66 @@
 
 #include <cassert>
 #include <compare>
+#include <concepts>
 #include <iterator>
 #include <ranges>
 #include <vector>
 
-#include "test_range.h"
+#include "test_iterators.h"
 
 constexpr bool test() {
-  std::vector<int> vector                                                  = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
-  std::ranges::chunk_view<std::ranges::ref_view<std::vector<int>>> chunked = vector | std::views::chunk(3);
+  std::vector<int> vector = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
 
-  // Test `friend constexpr bool operator==(const iterator& x, const iterator& y)`
+  // Test `friend constexpr bool operator==(const iterator&, const iterator&)`
+  // Test `friend constexpr bool operator==(const iterator&, default_sentinel_t)`
   {
+    std::ranges::chunk_view<std::ranges::subrange<random_access_iterator<int*>, random_access_iterator<int*>>> chunked(
+        std::ranges::subrange<random_access_iterator<int*>, random_access_iterator<int*>>(
+            random_access_iterator<int*>(vector.data()),
+            random_access_iterator<int*>(vector.data() + vector.size())),
+        3);
+
     assert(chunked.begin() == chunked.begin());
-    assert(chunked.end() == chunked.end());
-    assert(chunked.begin() != chunked.end());
-  }
-
-  // Test `friend constexpr bool operator==(const iterator& x, default_sentinel)`
-  {
+    assert(chunked.begin() + 1 == chunked.end() - 3);
+    assert(chunked.begin() + 4 == chunked.end());
     assert(chunked.end() == std::default_sentinel);
-    assert(chunked.begin() != std::default_sentinel);
   }
 
-  // Test `friend constexpr bool operator<(const iterator& x, const iterator& y)`
+  // Test `friend constexpr bool operator<(const iterator&, const iterator&)`
+  // Test `friend constexpr bool operator>(const iterator&, const iterator&)`
+  // Test `friend constexpr bool operator<=(const iterator&, const iterator&)`
+  // Test `friend constexpr bool operator>=(const iterator&, const iterator&)`
   {
+    std::ranges::chunk_view<std::ranges::subrange<random_access_iterator<int*>, random_access_iterator<int*>>> chunked(
+        std::ranges::subrange<random_access_iterator<int*>, random_access_iterator<int*>>(
+            random_access_iterator<int*>(vector.data()),
+            random_access_iterator<int*>(vector.data() + vector.size())),
+        3);
+
     assert(chunked.begin() < chunked.end());
-    assert(!(chunked.end() < chunked.begin()));
-    assert(!(chunked.begin() < chunked.begin()));
-  }
-
-  // Test `friend constexpr bool operator>(const iterator& x, const iterator& y)`
-  {
+    assert(chunked.begin() < chunked.begin() + 1);
+    assert(chunked.begin() + 1 > chunked.begin());
     assert(chunked.end() > chunked.begin());
-    assert(!(chunked.begin() > chunked.end()));
-    assert(!(chunked.begin() > chunked.begin()));
-  }
-
-  // Test `friend constexpr bool operator<=(const iterator& x, const iterator& y)`
-  {
     assert(chunked.begin() <= chunked.begin());
-    assert(chunked.begin() <= chunked.end());
-    assert(!(chunked.end() <= chunked.begin()));
+    assert(chunked.begin() <= chunked.begin() + 1);
+    assert(chunked.begin() + 1 >= chunked.begin());
+    assert(chunked.begin() + 1 >= chunked.begin() + 1);
   }
 
-  // Test `friend constexpr bool operator>=(const iterator& x, const iterator& y)`
+  // Test `friend constexpr auto operator<=>(const iterator&, const iterator&)`
   {
-    assert(chunked.end() >= chunked.end());
-    assert(chunked.end() >= chunked.begin());
-    assert(!(chunked.begin() >= chunked.end()));
-  }
+    std::ranges::chunk_view<std::ranges::subrange<int*, int*>> chunked(
+        std::ranges::subrange<int*, int*>(
+            vector.data(),
+            vector.data() + vector.size()),
+        3);
 
-  // Test `friend constexpr auto operator<=>(const iterator& x, const iterator& y)`
-  {
-    assert((chunked.begin() <=> chunked.begin()) == std::strong_ordering::equal);
     assert((chunked.begin() <=> chunked.end()) == std::strong_ordering::less);
+    assert((chunked.begin() <=> chunked.begin() + 1) == std::strong_ordering::less);
+    assert((chunked.begin() <=> chunked.begin()) == std::strong_ordering::equal);
+    assert((chunked.begin() + 1 <=> chunked.end() - 3) == std::strong_ordering::equal);
     assert((chunked.end() <=> chunked.begin()) == std::strong_ordering::greater);
+    assert((chunked.begin() + 1 <=> chunked.begin()) == std::strong_ordering::greater);
   }
 
   return true;
