83b105b6fc8c7fe8ec70f1b524e8793a838e8318
shyeyian <shyeyian@mail.shyeyian.com>
2026-08-17 13:37:19 +0800

    update base.pass

--- libcxx/test/std/ranges/range.adaptors/range.chunk/range.chunk.iter/base.pass.cpp
+++ libcxx/test/std/ranges/range.adaptors/range.chunk/range.chunk.iter/base.pass.cpp
@@ -18,18 +18,30 @@
 #include <ranges>
 #include <vector>
 
+#include "test_iterators.h"
+
 constexpr bool test() {
-  std::vector<int> vector = {1, 2, 3, 4, 5, 6};
+  // Test `constexpr iterator_t<Base> base() const`
+  {
+    static_assert(std::same_as< decltype(std::declval<std::ranges::iterator_t< std::ranges::chunk_view<
+                                             std::ranges::subrange<forward_iterator<int*>,
+                                                                   sentinel_wrapper<forward_iterator<int*>>>>> const&>()
+                                             .base()),
+                                forward_iterator<int*>>);
 
-  std::ranges::chunk_view<std::ranges::ref_view<std::vector<int>>> chunked = vector | std::views::chunk(2);
-  auto it                                                                  = chunked.begin();
+    std::vector<int> vector = {1, 2, 3, 4, 5, 6};
+    std::ranges::chunk_view<std::ranges::subrange<forward_iterator<int*>, sentinel_wrapper<forward_iterator<int*>>>>
+        chunked(std::ranges::subrange<forward_iterator<int*>, sentinel_wrapper<forward_iterator<int*>>>(
+                    forward_iterator<int*>(vector.data()),
+                    sentinel_wrapper<forward_iterator<int*>>(forward_iterator<int*>(vector.data() + vector.size()))),
+                2);
+    auto it = chunked.begin();
 
-  std::same_as<std::vector<int>::iterator> decltype(auto) base = it.base();
-  assert(base == vector.begin());
-
-  ++it;
-  assert(it.base() == vector.begin() + 2);
-
+    assert(base(it.base()) == vector.data());
+    ++it;
+    assert(base(it.base()) == vector.data() + 2);
+  }
+  
   return true;
 }
 
