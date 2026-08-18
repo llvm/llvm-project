a5d6d8feb899d759277c82e318b10d2585b3ae55
Lucas Mellone <github.snugness349@passinbox.com>
2026-08-18 16:07:44 +0200

    add: sentinel wrapper and  NonSimpleNonCommonConvertibleView

--- libcxx/test/std/ranges/range.adaptors/range.enumerate/sentinel/ctor.convert.pass.cpp
+++ libcxx/test/std/ranges/range.adaptors/range.enumerate/sentinel/ctor.convert.pass.cpp
@@ -29,6 +29,32 @@
 
 #include "../types.h"
 
+template <class T>
+struct convertible_sentinel_wrapper {
+    explicit convertible_sentinel_wrapper() = default;
+    contexpr convertible_sentinel_wrapper(const T& it) : it_(it) {}
+
+    template <class U>
+        requires std::convertible_to<const U&, T>
+    constexpr convertible_sentinel_wrapper(const convertible_sentinel_wrapper<U>& other) : it_(other.it_) {}
+
+    constexpr friend bool operator==(convertible_sentinel_wrapper const& self, const T& other) {
+        return self.it_ == other;
+    }
+    T it_;
+};
+
+struct NonSimpleNonCommonConvertibleView : IntBufferView {
+    using IntBufferView::IntBufferView;
+
+    constexpr int* begin() { return buffer_; }
+    constexpr const int* begin() const { return buffer_; }
+    constexpr convertible_sentinel_wrapper<int*> end() { return convertible_sentinel_wrapper<int*>(buffer_ + size_; }
+    constexpr convertible_sentinel_wrapper<const int*> end() const {
+        return convertible_sentinel_wrapper<const int*>(buffer_ + size_);
+    }
+};
+
 template <class Iterator, class Sentinel = sentinel_wrapper<Iterator>>
 constexpr void test() {
   using View                   = MinimalView<Iterator, Sentinel>;
@@ -46,18 +72,22 @@ constexpr void test() {
 
   std::array array{0, 1, 2, 3, 84};
   auto view = make_enumerate_view(array.begin(), array.end());
+  
+  {
+    // Assigning a non-const sentinel to a const-sentinel-typed variable invokes
+    // the converting constructor.
+    std::same_as<EnumerateSentinel> decltype(auto) s = view.end();
+    std::same_as<Sentinel> decltype(auto) sResult    = s.base();
+    assert(base(base(sResult)) == std::to_address(base(array.end())));
 
-  std::same_as<EnumerateSentinel> decltype(auto) s = view.end();
-  std::same_as<Sentinel> decltype(auto) sResult    = s.base();
-  assert(base(base(sResult)) == std::to_address(base(array.end())));
-
-  // Test assignment
-  EnumerateConstSentinel cs                      = s;
-  std::same_as<Sentinel> decltype(auto) csResult = cs.base();
-  assert(base(base(csResult)) == std::to_address(base(array.end())));
+    // Verify assignment
+    EnumerateConstSentinel cs                      = s;
+    std::same_as<Sentinel> decltype(auto) csResult = cs.base();
+    assert(base(base(csResult)) == std::to_address(base(array.end())));
+  }
 }
 
-constexpr bool tests() {
+constexpr bool test() {
   test<cpp17_input_iterator<int*>>();
   test<cpp20_input_iterator<int*>>();
   test<forward_iterator<int*>>();
@@ -70,8 +100,8 @@ constexpr bool tests() {
 }
 
 int main(int, char**) {
-  tests();
-  static_assert(tests());
+  test();
+  static_assert(test());
 
   return 0;
 }
