1178bacff8ec9a8c99f9d03b53ed29c70cd32a27
Chris Apple <cja-private@pm.me>
2026-08-18 06:27:24 -0700

    [clang-tidy] Normalize windows line filter paths

--- clang-tools-extra/clang-tidy/tool/clang-tidy-diff.py
+++ clang-tools-extra/clang-tidy/tool/clang-tidy-diff.py
@@ -394,8 +394,9 @@ def main():
         common_clang_tidy_args.append("-warnings-as-errors=" + args.warnings_as_errors)
 
     for name in lines_by_file:
+        line_filter_name = str((Path.cwd() / name).resolve())
         line_filter_json = json.dumps(
-            [{"name": name, "lines": lines_by_file[name]}], separators=(",", ":")
+            [{"name": line_filter_name, "lines": lines_by_file[name]}], separators=(",", ":"),
         )
 
         # Run clang-tidy on files containing changes.
