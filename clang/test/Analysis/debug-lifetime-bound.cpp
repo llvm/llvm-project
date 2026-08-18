3e55ca8a9eee6539272e9b31efe36bfc3e21ad2b
benedekaibas <kaibas01@allegheny.edu>
2026-08-18 17:23:55 +0200

    Move UseAfterLifetimeEnd out from alpha.cplusplus to alpha.core

--- clang/test/Analysis/debug-lifetime-bound.cpp
+++ clang/test/Analysis/debug-lifetime-bound.cpp
@@ -1,4 +1,4 @@
-// RUN: %clang_analyze_cc1 -analyzer-checker=core,alpha.cplusplus.UseAfterLifetimeEnd,debug.DebugLifetimeModeling -verify %s
+// RUN: %clang_analyze_cc1 -analyzer-checker=core,alpha.core.UseAfterLifetimeEnd,debug.DebugLifetimeModeling -verify %s
 
 // expected-no-diagnostics
 
