5de87ee8a83b3e891f3e90bffe0825d5802c3159
Andrew Savonichev <andrew.savonichev@gmail.com>
2026-08-18 21:26:16 +0900

    Implement MacroFusion for MCA MicroOpQueueStage

--- llvm/lib/Target/AArch64/MCA/AArch64CustomBehaviour.cpp
+++ llvm/lib/Target/AArch64/MCA/AArch64CustomBehaviour.cpp
@@ -23,33 +23,71 @@
 using namespace llvm;
 using namespace mca;
 
-class AArch64InstrPostProcess : public InstrPostProcess {
-  mca::Instruction *PreviousInst;
-  const MCInst *PreviousMCI;
+// Same as AArch64_MC::hasExtendedReg, but for an MCA Instruction.
+static bool hasExtendedReg(const mca::Instruction &Inst) {
+  switch (Inst.getOpcode()) {
+  case AArch64::ADDWrx:
+  case AArch64::ADDXrx:
+  case AArch64::ADDSWrx:
+  case AArch64::ADDSXrx:
+  case AArch64::SUBWrx:
+  case AArch64::SUBXrx:
+  case AArch64::SUBSWrx:
+  case AArch64::SUBSXrx:
+  case AArch64::ADDXrx64:
+  case AArch64::ADDSXrx64:
+  case AArch64::SUBXrx64:
+  case AArch64::SUBSXrx64:
+    return Inst.getOperand(3)->getImm() != 0;
+  default:
+    return false;
+  } // end of switch-stmt
+}
 
-public:
-  AArch64InstrPostProcess(const MCSubtargetInfo &STI, const MCInstrInfo &MCII)
-      : InstrPostProcess(STI, MCII), PreviousInst(nullptr),
-        PreviousMCI(nullptr) {}
-
-  ~AArch64InstrPostProcess() override = default;
-
-  void postProcessInstruction(mca::Instruction &Inst,
-                              const MCInst &MCI) override;
-  void resetState() override;
-};
+// Same as AArch64_MC::hasShiftedReg, but for an MCA Instruction.
+static bool hasShiftedReg(const mca::Instruction &Inst) {
+  switch (Inst.getOpcode()) {
+  case AArch64::ADDWrs:
+  case AArch64::ADDXrs:
+  case AArch64::ADDSWrs:
+  case AArch64::ADDSXrs:
+  case AArch64::SUBWrs:
+  case AArch64::SUBXrs:
+  case AArch64::SUBSWrs:
+  case AArch64::SUBSXrs:
+  case AArch64::ANDWrs:
+  case AArch64::ANDXrs:
+  case AArch64::ANDSWrs:
+  case AArch64::ANDSXrs:
+  case AArch64::BICWrs:
+  case AArch64::BICXrs:
+  case AArch64::BICSWrs:
+  case AArch64::BICSXrs:
+  case AArch64::EONWrs:
+  case AArch64::EONXrs:
+  case AArch64::EORWrs:
+  case AArch64::EORXrs:
+  case AArch64::ORNWrs:
+  case AArch64::ORNXrs:
+  case AArch64::ORRWrs:
+  case AArch64::ORRXrs:
+    return Inst.getOperand(3)->getImm() != 0;
+  default:
+    return false;
+  }
+}
 
 /// AES encoding or decoding.
-static bool isAESPair(const MCInst &First, const MCInst &Second) {
+static bool isAESPair(const MCSubtargetInfo &STI, const mca::Instruction &First,
+                      const mca::Instruction &Second) {
   switch (Second.getOpcode()) {
   // AES encode.
   case AArch64::AESMCrr: {
     if (First.getOpcode() != AArch64::AESErr)
       return false;
-
-    MCRegister EDef = First.getOperand(0).getReg();
-    MCRegister MCDef = Second.getOperand(0).getReg();
-    MCRegister MCUse = Second.getOperand(1).getReg();
+    MCRegister EDef = First.getOperand(0)->getReg();
+    MCRegister MCDef = Second.getOperand(0)->getReg();
+    MCRegister MCUse = Second.getOperand(1)->getReg();
     return (EDef == MCDef && EDef == MCUse);
   }
   // AES decode.
@@ -57,9 +95,9 @@ static bool isAESPair(const MCInst &First, const MCInst &Second) {
     if (First.getOpcode() != AArch64::AESDrr)
       return false;
 
-    MCRegister DDef = First.getOperand(0).getReg();
-    MCRegister IMCDef = Second.getOperand(0).getReg();
-    MCRegister IMCUse = Second.getOperand(1).getReg();
+    MCRegister DDef = First.getOperand(0)->getReg();
+    MCRegister IMCDef = Second.getOperand(0)->getReg();
+    MCRegister IMCUse = Second.getOperand(1)->getReg();
     return (DDef == IMCDef && DDef == IMCUse);
   }
   }
@@ -67,22 +105,21 @@ static bool isAESPair(const MCInst &First, const MCInst &Second) {
 }
 
 /// Compare and conditional select.
-static bool isCmpCSelPair(const MCInst &First, const MCInst &Second) {
+static bool isCmpCSelPair(const MCSubtargetInfo &STI,
+                          const mca::Instruction &First,
+                          const mca::Instruction &Second) {
   switch (Second.getOpcode()) {
   case AArch64::CSELWr: {
     // 32 bits
-    if (!First.getNumOperands())
-      return false;
-
-    MCOperand Zero = First.getOperand(0);
-    if (!Zero.isReg() || Zero.getReg() != AArch64::WZR)
+    const MCAOperand *Zero = First.getOperand(0);
+    if (!Zero || !Zero->isReg() || Zero->getReg() != AArch64::WZR)
       return false;
 
     switch (First.getOpcode()) {
     case AArch64::SUBSWrs:
-      return !AArch64_MC::hasShiftedReg(First);
+      return !hasShiftedReg(First);
     case AArch64::SUBSWrx:
-      return !AArch64_MC::hasExtendedReg(First);
+      return !hasExtendedReg(First);
     case AArch64::SUBSWrr:
     case AArch64::SUBSWri:
       return true;
@@ -91,19 +128,16 @@ static bool isCmpCSelPair(const MCInst &First, const MCInst &Second) {
   }
   case AArch64::CSELXr: {
     // 64 bits
-    if (!First.getNumOperands())
-      return false;
-
-    MCOperand Zero = First.getOperand(0);
-    if (!Zero.isReg() || Zero.getReg() != AArch64::XZR)
+    const MCAOperand *Zero = First.getOperand(0);
+    if (!Zero || !Zero->isReg() || Zero->getReg() != AArch64::XZR)
       return false;
 
     switch (First.getOpcode()) {
     case AArch64::SUBSXrs:
-      return !AArch64_MC::hasShiftedReg(First);
+      return !hasShiftedReg(First);
     case AArch64::SUBSXrx:
     case AArch64::SUBSXrx64:
-      return !AArch64_MC::hasExtendedReg(First);
+      return !hasExtendedReg(First);
     case AArch64::SUBSXrr:
     case AArch64::SUBSXri:
       return true;
@@ -116,25 +150,27 @@ static bool isCmpCSelPair(const MCInst &First, const MCInst &Second) {
 }
 
 /// Compare and conditional set.
-static bool isCmpCSetPair(const MCInst &First, const MCInst &Second) {
+static bool isCmpCSetPair(const MCSubtargetInfo &STI,
+                          const mca::Instruction &First,
+                          const mca::Instruction &Second) {
   switch (Second.getOpcode()) {
   case AArch64::CSINCWr: {
     // 32 bits
-    MCOperand Op1 = Second.getOperand(1);
-    MCOperand Op2 = Second.getOperand(2);
-    if (!Op1.isReg() || Op1.getReg() != AArch64::WZR)
+    const MCAOperand *Op1 = Second.getOperand(1);
+    const MCAOperand *Op2 = Second.getOperand(2);
+    if (!Op1 || !Op1->isReg() || Op1->getReg() != AArch64::WZR)
       return false;
-    if (!Op2.isReg() || Op2.getReg() != AArch64::WZR)
+    if (!Op2 || !Op2->isReg() || Op2->getReg() != AArch64::WZR)
       return false;
 
     switch (First.getOpcode()) {
     case AArch64::SUBSWrs: {
-      if (AArch64_MC::hasShiftedReg(First))
+      if (hasShiftedReg(First))
         return false;
       break;
     }
     case AArch64::SUBSWrx: {
-      if (AArch64_MC::hasExtendedReg(First))
+      if (hasExtendedReg(First))
         return false;
       break;
     }
@@ -145,27 +181,27 @@ static bool isCmpCSetPair(const MCInst &First, const MCInst &Second) {
       return false;
     }
 
-    MCOperand Def = First.getOperand(0);
-    return (Def.isReg() && Def.getReg() == AArch64::WZR);
+    const MCAOperand *Def = First.getOperand(0);
+    return (Def && Def->isReg() && Def->getReg() == AArch64::WZR);
   }
   case AArch64::CSINCXr: {
     // 64 bits
-    MCOperand Op1 = Second.getOperand(1);
-    MCOperand Op2 = Second.getOperand(2);
-    if (!Op1.isReg() || Op1.getReg() != AArch64::XZR)
+    const MCAOperand *Op1 = Second.getOperand(1);
+    const MCAOperand *Op2 = Second.getOperand(2);
+    if (!Op1 || !Op1->isReg() || Op1->getReg() != AArch64::XZR)
       return false;
-    if (!Op2.isReg() || Op2.getReg() != AArch64::XZR)
+    if (!Op2 || !Op2->isReg() || Op2->getReg() != AArch64::XZR)
       return false;
 
     switch (First.getOpcode()) {
     case AArch64::SUBSXrs: {
-      if (AArch64_MC::hasShiftedReg(First))
+      if (hasShiftedReg(First))
         return false;
       break;
     }
     case AArch64::SUBSXrx:
     case AArch64::SUBSXrx64: {
-      if (AArch64_MC::hasExtendedReg(First))
+      if (hasExtendedReg(First))
         return false;
       break;
     }
@@ -174,27 +210,30 @@ static bool isCmpCSetPair(const MCInst &First, const MCInst &Second) {
       break;
     }
 
-    MCOperand Def = First.getOperand(0);
-    return (Def.isReg() && Def.getReg() == AArch64::XZR);
+    const MCAOperand *Def = First.getOperand(0);
+    return (Def && Def->isReg() && Def->getReg() == AArch64::XZR);
   }
   }
   return false;
 }
 
 /// CMN, CMP, TST followed by Bcc
-static bool isArithmeticBccPair(const MCInst &First, const MCInst &Second,
-                                bool CmpOnly) {
+static bool isArithmeticBccPair(const MCSubtargetInfo &STI,
+                                const mca::Instruction &First,
+                                const mca::Instruction &Second) {
   if (Second.getOpcode() != AArch64::Bcc)
     return false;
+
+  bool CmpOnly = !STI.hasFeature(AArch64::FeatureArithmeticBccFusion);
 
   // If we're in CmpOnly mode, we only fuse arithmetic instructions that
   // discard their result.
   if (CmpOnly) {
-    MCOperand Def = First.getOperand(0);
-    if (!Def.isReg())
+    const MCAOperand *Def = First.getOperand(0);
+    if (!Def || !Def->isReg())
       return false;
 
-    if (Def.getReg() != AArch64::XZR && Def.getReg() != AArch64::WZR) {
+    if (Def->getReg() != AArch64::XZR && Def->getReg() != AArch64::WZR) {
       return false;
     }
   }
@@ -223,117 +262,74 @@ static bool isArithmeticBccPair(const MCInst &First, const MCInst &Second,
   case AArch64::SUBSXrs:
   case AArch64::BICSWrs:
   case AArch64::BICSXrs:
-    return !AArch64_MC::hasShiftedReg(First);
+    return !hasShiftedReg(First);
   }
 
   return false;
 }
 
-// Move all writes from Second to First instruction.
-static void moveWrites(mca::Instruction &First, mca::Instruction &Second) {
-  for (WriteState &SecondDef : Second.getDefs()) {
-    bool AlreadyDefined = false;
-    for (WriteState &FirstDef : First.getDefs()) {
-      if (FirstDef.getRegisterID() == SecondDef.getRegisterID()) {
-        AlreadyDefined = true;
-        break;
-      }
-    }
+class AArch64CustomBehaviour : public CustomBehaviour {
+  SmallVector<MacroFusionPredicate, 32> FusionPredicates;
 
-    if (!AlreadyDefined)
-      First.getDefs().push_back(SecondDef);
+public:
+  AArch64CustomBehaviour(const MCSubtargetInfo &STI,
+                         const mca::SourceMgr &SrcMgr, const MCInstrInfo &MCII)
+      : CustomBehaviour(STI, SrcMgr, MCII) {
 
-    // Second instruction is going to be eliminated, so it cannot have
-    // any active writes.
-    SecondDef.setRegisterID(0);
-  }
-}
+    if (STI.hasFeature(AArch64::FeatureFuseAES))
+      FusionPredicates.push_back(isAESPair);
 
-// Move all reads from Second to First instruction.
-static void moveReads(mca::Instruction &First, mca::Instruction &Second) {
-  for (ReadState &SecondUse : Second.getUses()) {
-    bool AlreadyUsed = false;
-    for (ReadState &FirstUse : First.getUses()) {
-      if (SecondUse.getRegisterID() == FirstUse.getRegisterID()) {
-        AlreadyUsed = true;
-        break;
-      }
-    }
+    if (STI.hasFeature(AArch64::FeatureFuseCmpCSel))
+      FusionPredicates.push_back(isCmpCSelPair);
 
-    // Ignore reads of registers which are defined by the instruction
-    // we move them to.
-    bool IsDef = false;
-    for (WriteState &FirstDef : First.getDefs()) {
-      if (SecondUse.getRegisterID() == FirstDef.getRegisterID()) {
-        IsDef = true;
-        break;
-      }
-    }
+    if (STI.hasFeature(AArch64::FeatureFuseCmpCSet))
+      FusionPredicates.push_back(isCmpCSetPair);
 
-    if (!AlreadyUsed && !IsDef)
-      First.getUses().push_back(SecondUse);
-  }
-}
-
-static void fuseInstructions(mca::Instruction &First,
-                             mca::Instruction &Second) {
-  moveReads(First, Second);
-  moveWrites(First, Second);
-  Second.setEliminated();
-}
-
-static bool tryFuseInstructions(mca::Instruction &First, const MCInst &FirstMCI,
-                                mca::Instruction &Second,
-                                const MCInst &SecondMCI,
-                                const MCSubtargetInfo &STI) {
-  if (STI.hasFeature(AArch64::FeatureFuseAES) &&
-      isAESPair(FirstMCI, SecondMCI)) {
-    fuseInstructions(First, Second);
-    return true;
+    if (STI.hasFeature(AArch64::FeatureCmpBccFusion) ||
+        STI.hasFeature(AArch64::FeatureArithmeticBccFusion))
+      FusionPredicates.push_back(isArithmeticBccPair);
   }
 
-  if (STI.hasFeature(AArch64::FeatureFuseCmpCSel) &&
-      isCmpCSelPair(FirstMCI, SecondMCI)) {
-    fuseInstructions(First, Second);
-    return true;
-  }
+  ~AArch64CustomBehaviour() override = default;
 
-  if (STI.hasFeature(AArch64::FeatureFuseCmpCSet) &&
-      isCmpCSetPair(FirstMCI, SecondMCI)) {
-    fuseInstructions(First, Second);
-    return true;
+  ArrayRef<MacroFusionPredicate> getFusionPredicates() override {
+    return FusionPredicates;
   }
+};
 
-  if (STI.hasFeature(AArch64::FeatureCmpBccFusion) ||
-      STI.hasFeature(AArch64::FeatureArithmeticBccFusion)) {
-    bool CmpOnly = !STI.hasFeature(AArch64::FeatureArithmeticBccFusion);
-    if (isArithmeticBccPair(FirstMCI, SecondMCI, CmpOnly)) {
-      fuseInstructions(First, Second);
-      return true;
+class AArch64InstrPostProcess : public InstrPostProcess {
+public:
+  AArch64InstrPostProcess(const MCSubtargetInfo &STI, const MCInstrInfo &MCII)
+      : InstrPostProcess(STI, MCII) {}
+
+  ~AArch64InstrPostProcess() override = default;
+
+  void postProcessInstruction(mca::Instruction &Inst,
+                              const MCInst &MCI) override {
+    for (unsigned I = 0; I < MCI.getNumOperands(); ++I) {
+      MCOperand MCO = MCI.getOperand(I);
+      MCAOperand Op;
+      if (MCO.isReg())
+        Op = MCAOperand::createReg(MCO.getReg());
+      else if (MCO.isImm())
+        Op = MCAOperand::createImm(MCO.getImm());
+      else if (MCO.isSFPImm())
+        Op = MCAOperand::createSFPImm(MCO.getSFPImm());
+      else if (MCO.isDFPImm())
+        Op = MCAOperand::createDFPImm(MCO.getDFPImm());
+
+      Op.setIndex(I);
+      Inst.addOperand(Op);
     }
   }
+};
 
-  return false;
+static CustomBehaviour *
+createAArch64CustomBehaviour(const MCSubtargetInfo &STI,
+                             const mca::SourceMgr &SrcMgr,
+                             const MCInstrInfo &MCII) {
+  return new AArch64CustomBehaviour(STI, SrcMgr, MCII);
 }
-
-void AArch64InstrPostProcess::postProcessInstruction(mca::Instruction &Inst,
-                                                     const MCInst &MCI) {
-  if (!PreviousInst) {
-    PreviousInst = &Inst;
-    PreviousMCI = &MCI;
-    return;
-  }
-
-  if (tryFuseInstructions(*PreviousInst, *PreviousMCI, Inst, MCI, STI)) {
-    PreviousInst = nullptr;
-    PreviousMCI = nullptr;
-  } else {
-    PreviousInst = &Inst;
-    PreviousMCI = &MCI;
-  }
-}
-
-void AArch64InstrPostProcess::resetState() { PreviousInst = nullptr; }
 
 static InstrPostProcess *
 createAArch64InstrPostProcess(const MCSubtargetInfo &STI,
@@ -349,6 +345,7 @@ extern "C" LLVM_C_ABI void LLVMInitializeAArch64TargetMCA() {
   };
 
   for (Target *T : Targets) {
+    TargetRegistry::RegisterCustomBehaviour(*T, createAArch64CustomBehaviour);
     TargetRegistry::RegisterInstrPostProcess(*T, createAArch64InstrPostProcess);
   }
 }
