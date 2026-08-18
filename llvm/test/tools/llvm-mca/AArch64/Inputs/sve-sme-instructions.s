1f8541e88aad0008157a5a5c48939cb1ff588e7f
Nashe Mncube <nashe.mncube@arm.com>
2026-08-18 14:30:37 +0100

    [AArch64] Extend llvm-mca input tests
    Add extra coverage to tests for AArch64 llvm-mca
    
    Change-Id: I38cbb75ae5206d2f577969ce976467f061cd674a

--- llvm/test/tools/llvm-mca/AArch64/Inputs/sve-sme-instructions.s
+++ llvm/test/tools/llvm-mca/AArch64/Inputs/sve-sme-instructions.s
@@ -1,5 +1,14 @@
 // SVE instructions added by SME and available when not in Streaming SVE mode
 
 bfmlslb z0.s, z1.h, z2.h
+bfmlslt z0.s, z1.h, z2.h
+fclamp z0.s, z1.s, z2.s
 fdot z0.s, z1.h, z2.h
 psel p0, p0, p0.b[w12, 0]
+revd z0.q, p0/m, z0.q
+sclamp z0.s, z1.s, z2.s
+uclamp z0.s, z1.s, z2.s
+sdot z0.s, z0.h, z0.h
+udot z0.s, z0.h, z0.h
+sqcvtun z0.h, { z0.s, z1.s }
+sqrshrun z0.h, { z0.s, z1.s }, #16
