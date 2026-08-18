fdd864ae100fdfd7c51953ad15e87dd8bd42b5d7
Michael Klemm <michael.klemm@amd.com>
2026-08-18 10:24:12 +0200

    Remove obsolete compiler flags

--- flang/test/Driver/fopenmp-default-allocate.f90
+++ flang/test/Driver/fopenmp-default-allocate.f90
@@ -1,8 +1,8 @@
 ! Check that the driver passes -fopenmp-default-allocate= through to fc1
 ! and only adds -mmlir -use-alloc-runtime for target mode.
 
-! RUN: %flang -### -S -fopenmp-default-allocate=target %s -o - 2>&1 | FileCheck %s --check-prefix=TARGET
-! RUN: %flang -### -S -fopenmp-default-allocate=host %s -o - 2>&1 | FileCheck %s --check-prefix=HOST
+! RUN: %flang -### -fopenmp-default-allocate=target %s 2>&1 | FileCheck %s --check-prefix=TARGET
+! RUN: %flang -### -fopenmp-default-allocate=host %s 2>&1 | FileCheck %s --check-prefix=HOST
 
 ! TARGET: warning: -fopenmp-default-allocate= is an experimental feature
 ! TARGET: "-fc1"
