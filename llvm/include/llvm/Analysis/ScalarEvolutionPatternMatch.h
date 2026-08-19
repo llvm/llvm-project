4d3a65e32d8eab5476dd944afbde03c3cc154118
David Sherwood <david.sherwood@arm.com>
2026-08-19 08:25:13 +0000

    Address review comment

--- llvm/include/llvm/Analysis/ScalarEvolutionPatternMatch.h
+++ llvm/include/llvm/Analysis/ScalarEvolutionPatternMatch.h
@@ -270,9 +270,10 @@ m_scev_SMax(const Op0_t &Op0, const Op1_t &Op1) {
 }
 
 template <typename Op0_t, typename Op1_t>
-inline SCEVBinaryExpr_match<SCEVUMaxExpr, Op0_t, Op1_t>
+inline SCEVBinaryExpr_match<SCEVUMaxExpr, Op0_t, Op1_t, SCEV::FlagAnyWrap, true>
 m_scev_UMax(const Op0_t &Op0, const Op1_t &Op1) {
-  return m_scev_Binary<SCEVUMaxExpr>(Op0, Op1);
+  return m_scev_Binary<SCEVUMaxExpr, Op0_t, Op1_t, SCEV::FlagAnyWrap, true>(
+      Op0, Op1);
 }
 
 template <typename Op0_t, typename Op1_t>
