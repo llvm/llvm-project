8dc162636e3504cb02011954eed6e258e4184bf3
Sukumarsawant <sawantsukumar@gmail.com>
2026-08-19 18:06:40 +0530

    nit

--- libc/test/shared/shared_math_constexpr_test.cpp
+++ libc/test/shared/shared_math_constexpr_test.cpp
@@ -381,7 +381,6 @@ static_assert(0 == LIBC_NAMESPACE::shared::isnanl(0.0L));
 //===----------------------------------------------------------------------===//
 
 static_assert(Float128(0.0) == LIBC_NAMESPACE::shared::ceilf128(Float128(0.0)));
-
 static_assert(Float128(0.0) ==
               LIBC_NAMESPACE::shared::fmaxf128(Float128(0.0), Float128(0.0)));
 static_assert(Float128(0.0) ==
