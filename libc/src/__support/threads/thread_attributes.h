0a6e810b8573af15a8637dd531cf62b6b098ab66
Pavel Labath <pavel@labath.sk>
2026-08-19 07:12:04 +0000

    fix file header

--- libc/src/__support/threads/thread_attributes.h
+++ libc/src/__support/threads/thread_attributes.h
@@ -1,9 +1,14 @@
-//===-- Common thread attributes --------------------------------*- C++ -*-===//
+//===----------------------------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// Common thread attributes.
+///
 //===----------------------------------------------------------------------===//
 
 #ifndef LLVM_LIBC_SRC___SUPPORT_THREADS_THREAD_ATTRIBUTES_H
