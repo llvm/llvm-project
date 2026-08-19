616b908c1244fcfa5922c2d5f38fc53be49286f0
iaroslav <yborobkin@gmail.com>
2026-08-19 21:48:34 +0100

    [UBSan][Test] Fix anonymous struct function hashing test

--- clang/test/CodeGen/ubsan-function-anonymous-struct-windows.c
+++ clang/test/CodeGen/ubsan-function-anonymous-struct-windows.c
@@ -1,7 +1,6 @@
 // RUN: %clang_cc1 -triple x86_64-pc-windows-msvc -fsanitize=function -emit-llvm -o - %s | FileCheck %s
 
-// CHECK: define internal void @sample_tuple()
-// CHECK: !{{[0-9]+}} = !{i32 {{[0-9]+}}, !"typeid"}
+// CHECK: define internal {{.*}} @sample_tuple(){{.*}} !func_sanitize !{{[0-9]+}}
 
 static struct { int first; float second; } sample_tuple(void) {
     return (typeof(sample_tuple())){ .first = 10, .second = 0.1f };
