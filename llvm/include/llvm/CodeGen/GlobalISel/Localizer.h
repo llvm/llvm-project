ae99d6102e35ab9196f2cd0bd7772cbc2509b232
Aiden Grossman <aidengrossman@google.com>
2026-08-19 04:33:18 +0000

    [𝘀𝗽𝗿] changes to main this commit is based on
    Created using spr 1.3.7
    
    [skip ci]

--- llvm/include/llvm/CodeGen/GlobalISel/Localizer.h
+++ llvm/include/llvm/CodeGen/GlobalISel/Localizer.h
@@ -45,10 +45,6 @@ public:
   static char ID;
 
 private:
-  /// An input function to decide if the pass should run or not
-  /// on the given MachineFunction.
-  std::function<bool(const MachineFunction &)> DoNotRunPass;
-
   /// MRI contains all the register class/bank information that this
   /// pass uses and updates.
   MachineRegisterInfo *MRI = nullptr;
@@ -80,7 +76,6 @@ private:
 
 public:
   Localizer();
-  Localizer(std::function<bool(const MachineFunction &)>);
 
   StringRef getPassName() const override { return "Localizer"; }
 
