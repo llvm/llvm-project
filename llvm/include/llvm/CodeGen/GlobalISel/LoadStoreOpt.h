9b9a554a73feb350de2566f338fdbb17974f81de
Aiden Grossman <aidengrossman@google.com>
2026-08-19 04:13:29 +0000

    [𝘀𝗽𝗿] changes introduced through rebase
    Created using spr 1.3.7
    
    [skip ci]

--- llvm/include/llvm/CodeGen/GlobalISel/LoadStoreOpt.h
+++ llvm/include/llvm/CodeGen/GlobalISel/LoadStoreOpt.h
@@ -183,6 +183,10 @@ class LoadStoreOptPass : public RequiredPassInfoMixin<LoadStoreOptPass> {
 public:
   PreservedAnalyses run(MachineFunction &MF,
                         MachineFunctionAnalysisManager &MFAM);
+
+  MachineFunctionProperties getRequiredProperties() const {
+    return MachineFunctionProperties().setIsSSA();
+  }
 };
 
 } // End namespace llvm.
