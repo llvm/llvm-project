1d29e79809f11f2c33d1c239f7c473746aadb859
Bhavesh Mandalapu <bmandalapu@google.com>
2026-08-19 14:34:10 -0700

    Update file headers, create execve syscall wrapper, copy original varargs instead of creating a new one

--- libc/src/unistd/linux/execle.cpp
+++ libc/src/unistd/linux/execle.cpp
@@ -1,51 +1,55 @@
-//===-- Linux implementation of execle ------------------------------------===//
+//===----------------------------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
+///
+/// \file
+/// Linux implementation of execle
+///
+//===----------------------------------------------------------------------===//
 
 #include "src/unistd/execle.h"
-#include "src/__support/macros/config.h"
 
 #include "hdr/types/size_t.h"
-#include "src/__support/OSUtil/syscall.h" // For internal syscall function.
+#include "src/__support/OSUtil/linux/syscall_wrappers/execve.h"
 #include "src/__support/common.h"
 #include "src/__support/libc_errno.h"
+#include "src/__support/macros/config.h"
+
 #include <stdarg.h>
-#include <sys/syscall.h>
 
 namespace LIBC_NAMESPACE_DECL {
 
 LLVM_LIBC_FUNCTION(int, execle, (const char *path, const char *arg0, ...)) {
-  va_list varargs;
+  va_list varargs, varargs_copy;
   va_start(varargs, arg0);
+  va_copy(varargs_copy, varargs);
 
   size_t argc = 1;
   while (va_arg(varargs, const char *) != nullptr)
     ++argc;
   va_end(varargs);
 
-  char **argv =
-      static_cast<char **>(__builtin_alloca((argc + 1) * sizeof(char *)));
+  char *argv[argc + 1];
   argv[0] = const_cast<char *>(arg0);
 
-  va_start(varargs, arg0);
   for (size_t i = 1; i <= argc; ++i)
-    argv[i] = va_arg(varargs, char *);
-  char **envp = va_arg(varargs, char **);
-  va_end(varargs);
+    argv[i] = va_arg(varargs_copy, char *);
+  char **envp = va_arg(varargs_copy, char **);
+  va_end(varargs_copy);
 
-  int ret = LIBC_NAMESPACE::syscall_impl<int>(SYS_execve, path, argv, envp);
-  if (ret < 0) {
-    libc_errno = -ret;
+  auto ret = linux_syscalls::execve(path, argv, envp);
+  if (!ret) {
+    libc_errno = ret.error();
     return -1;
   }
 
   // Control will not reach here on success but have a return statement will
   // keep the compilers happy.
-  return ret;
+  return *ret;
 }
 
 } // namespace LIBC_NAMESPACE_DECL
--- libc/test/integration/src/unistd/execle_test.cpp
+++ libc/test/integration/src/unistd/execle_test.cpp
@@ -1,9 +1,13 @@
-//===-- Unittests for execle ----------------------------------------------===//
+//===----------------------------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
+//===----------------------------------------------------------------------===//
+/// 
+/// Unittests for execle
+///
 //===----------------------------------------------------------------------===//
 
 #include "src/sys/wait/waitpid.h"
