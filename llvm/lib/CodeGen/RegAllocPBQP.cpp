44e2c6becc4b0d0a343dad53841ea5d4a3bd1db7
nkotikal <nak00001@outlook.com>
2026-08-19 11:07:55 -0400

    reverted finalizeAlloc conversion to error

--- llvm/lib/CodeGen/RegAllocPBQP.cpp
+++ llvm/lib/CodeGen/RegAllocPBQP.cpp
@@ -63,7 +63,6 @@
 #include "llvm/CodeGen/TargetSubtargetInfo.h"
 #include "llvm/CodeGen/VirtRegMap.h"
 #include "llvm/Config/llvm-config.h"
-#include "llvm/IR/DiagnosticInfo.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/Module.h"
 #include "llvm/Pass.h"
@@ -755,20 +754,10 @@ void RegAllocPBQP::finalizeAlloc(MachineFunction &MF, LiveIntervals &LIS,
     Register PReg = MRI.getSimpleHint(LI.reg());
 
     if (PReg == 0) {
-      const TargetRegisterClass *RC = MRI.getRegClass(LI.reg());
-      ArrayRef<MCPhysReg> Order = RCI.getOrder(RC);
-      if (Order.empty()) {
-        bool EmitError = !MF.getProperties().hasFailedRegAlloc();
-        if (EmitError) {
-          MF.getProperties().setFailedRegAlloc();
-          MF.getFunction().getContext().diagnose(DiagnosticInfoRegAllocFailure(
-              "no registers from class available to allocate", MF.getFunction(),
-              DiagnosticLocation()));
-        }
-        PReg = RC->getRegisters().front();
-      } else {
-        PReg = Order.front();
-      }
+      ArrayRef<MCPhysReg> Order = RCI.getOrder(MRI.getRegClass(LI.reg()));
+      assert(!Order.empty() &&
+             "No un-reserved physical registers in this register class");
+      PReg = Order.front();
     }
 
     VRM.assignVirt2Phys(LI.reg(), PReg);
