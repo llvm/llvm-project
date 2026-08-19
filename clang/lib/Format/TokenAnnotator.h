fc62eca560595cdba499a4804a21213d4d48a664
ahmedkamel10 <amkzaher@gmail.com>
2026-08-19 23:31:45 +0300

    Apply clang-format fixes

--- clang/lib/Format/TokenAnnotator.h
+++ clang/lib/Format/TokenAnnotator.h
@@ -157,7 +157,6 @@ public:
            startsWith(tok::kw_export, tok::kw_namespace);
   }
 
-
   FormatToken *getFirstNonComment() const {
     assert(First);
     return First->is(tok::comment) ? First->getNextNonComment() : First;
