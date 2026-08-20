fb968ef7dfd05ed407022b93347b6fa41569de17
Leonard Chan <leonardchan@google.com>
2026-08-20 21:51:20 +0000

    [clang] Gate llvm-mt on if libxml2 is provided
    Otherwise this will fail on the cmake invocation because the target
    expects some libxml2 headers.

--- clang/cmake/caches/Fuchsia-stage2.cmake
+++ clang/cmake/caches/Fuchsia-stage2.cmake
@@ -485,7 +485,6 @@ set(LLVM_TOOLCHAIN_TOOLS
   llvm-libtool-darwin
   llvm-lipo
   llvm-ml
-  llvm-mt
   llvm-nm
   llvm-objcopy
   llvm-objdump
@@ -507,6 +506,10 @@ set(LLVM_TOOLCHAIN_TOOLS
   sancov
   scan-build-py
   CACHE STRING "")
+
+if(LLVM_ENABLE_LIBXML2)
+  list(APPEND LLVM_TOOLCHAIN_TOOLS llvm-mt)
+endif()
 
 set(LLVM_Toolchain_DISTRIBUTION_COMPONENTS
   bolt
