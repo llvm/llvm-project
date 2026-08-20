ea4e14bbd05a101c8ae05752d0899a47a4751f9b
Vadim Kotov <kotov@google.com>
2026-08-19 17:22:21 -0700

    Implement basic tests for getrusage

--- libc/test/src/sys/resource/getrusage_test.cpp
+++ libc/test/src/sys/resource/getrusage_test.cpp
@@ -11,12 +11,30 @@
 ///
 //===----------------------------------------------------------------------===//
 
+#include "hdr/sys_resource_macros.h"
 #include "hdr/types/struct_rusage.h"
 #include "src/sys/resource/getrusage.h"
+#include "test/UnitTest/ErrnoCheckingTest.h"
 #include "test/UnitTest/Test.h"
 
-TEST(LlvmLibcGetrusageTest, DummyTest) {
+
+TEST(LlvmLibcGetrusageTest, BasicTest) {
   struct rusage usage = {};
-  int res = LIBC_NAMESPACE::getrusage(0, &usage);
-  EXPECT_NE(res, -1);
+  int res = LIBC_NAMESPACE::getrusage(RUSAGE_SELF, &usage);
+  EXPECT_EQ(res, 0);
+  ASSERT_ERRNO_SUCCESS();
+}
+
+TEST(LlvmLibcGetrusageTest, TestWhoInvalid) {
+  struct rusage usage = {};
+  int res = LIBC_NAMESPACE::getrusage(99, &usage);
+  EXPECT_EQ(res, -1);
+  ASSERT_ERRNO_EQ(EINVAL);
+}
+
+TEST(LlvmLibcGetrusageTest, TestUsageBadPointer) {
+  int res = LIBC_NAMESPACE::getrusage(RUSAGE_SELF, (struct rusage*)-1L);
+  EXPECT_EQ(res, -1);
+  ASSERT_ERRNO_EQ(EFAULT);
+
 }
