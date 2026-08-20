d3ce6a7b9c6908695e9a1f46979638cec0c75a2b
Mohammed Zayer Alam <zayeralam09@gmail.com>
2026-08-20 19:55:46 +0530

    Fix CIR gen for x86 vfmadd builtins and add test case.

--- clang/test/CIR/CodeGen/builtins-x86.c
+++ clang/test/CIR/CodeGen/builtins-x86.c
@@ -313,15 +313,16 @@ double test_fma_f64(double a, double b, double c) {
   // OGCG: %[[R:.*]] = call double @llvm.fma.f64(double %[[A:.*]], double %[[B:.*]], double %[[C:.*]])
   return __builtin_fma(a, b, c);
 }
-// CHECK-LABEL: cir.func @test_vfmadd
-// CHECK: cir.call @__builtin_ia32_vfmaddpd
-// CHECK: cir.call @__builtin_ia32_vfmaddps
-// CHECK: cir.call @__builtin_ia32_vfmaddsubpd
-// CHECK: cir.call @__builtin_ia32_vfmaddsubps
-void test_vfmadd(double a, double b, double c) {
-  __builtin_ia32_vfmaddpd(a, b, c);
-    __builtin_ia32_vfmaddps(a, b, c);
-      __builtin_ia32_vfmaddsubpd(a, b, c);
-        __builtin_ia32_vfmaddsubps(a, b, c);
-        }
+void test_vfmadd(__m128d a, __m128d b, __m128d c) {
+    // CIR-LABEL: cl.func @test_vfmadd
+      // CIR: cir.call @_builtin_ia32_vfmaddpd
+        // LLVM-LABEL: @test_vfmadd
+          // LLVM: call <2 x double> @llvm.x86.fma.vfmadd.pd
+            // OGCG-LABEL: @test_vfmadd
+              // OGCG: call <2 x double> @llvm.x86.fma.vfmadd.pd
+                __builtin_ia32_vfmaddpd(a, b, c);
+                }
+              
+
+
 
