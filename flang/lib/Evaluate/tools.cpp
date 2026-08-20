85d10f4387e2a6661ebd2418990785d6c3359ecc
Tom Eccles <tom.eccles@arm.com>
2026-08-20 11:34:32 +0100

    Re-use FindImpureCall()

--- flang/lib/Evaluate/tools.cpp
+++ flang/lib/Evaluate/tools.cpp
@@ -1354,16 +1354,6 @@ struct HasProcedureRefHelper : public AnyTraverse<HasProcedureRefHelper> {
   bool operator()(const ProcedureRef &) const { return true; }
 };
 
-struct HasImpureProcedureRefHelper
-    : public AnyTraverse<HasImpureProcedureRefHelper> {
-  using Base = AnyTraverse<HasImpureProcedureRefHelper>;
-  HasImpureProcedureRefHelper() : Base{*this} {}
-  using Base::operator();
-  bool operator()(const ProcedureRef &ref) const {
-    return ref.proc().IsPure() ? Base::operator()(ref) : true;
-  }
-};
-
 struct HasVolatileOrAsynchronousSymbolHelper
     : public AnyTraverse<HasVolatileOrAsynchronousSymbolHelper> {
   using Base = AnyTraverse<HasVolatileOrAsynchronousSymbolHelper>;
@@ -1548,10 +1538,10 @@ static std::optional<Expr<SomeType>> tryBuildSplitSumExpressionTree(
 
 } // namespace
 
-bool CanBuildSplitSumExpressionTree(
+bool CanBuildSplitSumExpressionTree(FoldingContext &context,
     const Expr<SomeType> &lhs, const Expr<SomeType> &rhs) {
   return rhs.Rank() == 0 && lhs.Rank() == 0 && !HasVectorSubscript(rhs) &&
-      !HasVectorSubscript(lhs) && !HasImpureProcedureRefHelper{}(rhs) &&
+      !HasVectorSubscript(lhs) && !FindImpureCall(context, rhs) &&
       !HasProcedureRef(lhs) && !HasVolatileOrAsynchronousSymbol(rhs) &&
       !HasVolatileOrAsynchronousSymbol(lhs);
 }
