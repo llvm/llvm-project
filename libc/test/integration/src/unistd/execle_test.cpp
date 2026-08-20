9344320c4787aeedeb55e63b4c33b28f49ceff10
Bhavesh M <85327930+beamandala@users.noreply.github.com>
2026-08-19 20:50:46 -0700

    [libc] Implement `execle` (#217213)
    This implements `execle` and also creates a syscall wrapper for `execve`.

--- libc/test/integration/src/unistd/execle_test.cpp
+++ libc/test/integration/src/unistd/execle_test.cpp
@@ -1,13 +1,17 @@
-//===-- Unittests for execve ----------------------------------------------===//
+//===----------------------------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
+///
+/// Unittests for execle
+///
+//===----------------------------------------------------------------------===//
 
 #include "src/sys/wait/waitpid.h"
-#include "src/unistd/execve.h"
+#include "src/unistd/execle.h"
 #include "src/unistd/fork.h"
 
 #include "test/IntegrationTest/test.h"
@@ -15,16 +19,16 @@
 #include <signal.h>
 #include <sys/wait.h>
 
-void fork_and_execv_normal_exit(char **envp) {
+void fork_and_execle_normal_exit(char **envp) {
   pid_t pid = LIBC_NAMESPACE::fork();
   if (pid == 0) {
-    const char *path = "libc_execv_test_normal_exit";
-    char *const argv[] = {
-        const_cast<char *>("execv_test_normal_exit"),
-        nullptr,
-    };
-    LIBC_NAMESPACE::execve(path, argv, envp);
+    const char *path = "libc_execle_test_normal_exit";
+    LIBC_NAMESPACE::execle(path, const_cast<char *>("execle_test_normal_exit"),
+                           const_cast<char *>("first"),
+                           const_cast<char *>("second"),
+                           static_cast<char *>(nullptr), envp);
   }
+
   ASSERT_TRUE(pid > 0);
   int status;
   pid_t cpid = LIBC_NAMESPACE::waitpid(pid, &status, 0);
@@ -33,15 +37,12 @@ void fork_and_execv_normal_exit(char **envp) {
   ASSERT_TRUE(WIFEXITED(status));
 }
 
-void fork_and_execv_signal_exit(char **envp) {
+void fork_and_execle_signal_exit(char **envp) {
   pid_t pid = LIBC_NAMESPACE::fork();
   if (pid == 0) {
     const char *path = "libc_exec_test_signal_exit";
-    char *const argv[] = {
-        const_cast<char *>("exec_test_signal_exit"),
-        nullptr,
-    };
-    LIBC_NAMESPACE::execve(path, argv, envp);
+    LIBC_NAMESPACE::execle(path, const_cast<char *>("exec_test_signal_exit"),
+                           static_cast<char *>(nullptr), envp);
   }
   ASSERT_TRUE(pid > 0);
   int status;
@@ -54,7 +55,7 @@ void fork_and_execv_signal_exit(char **envp) {
 
 TEST_MAIN([[maybe_unused]] int argc, [[maybe_unused]] char **argv,
           char **envp) {
-  fork_and_execv_normal_exit(envp);
-  fork_and_execv_signal_exit(envp);
+  fork_and_execle_normal_exit(envp);
+  fork_and_execle_signal_exit(envp);
   return 0;
 }
