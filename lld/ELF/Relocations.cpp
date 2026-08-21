ef1a70adee66c0eba35a47badced5b991fb161f2
Jessica Clarke <jrtc27@jrtc27.com>
2026-08-21 10:37:10 +0100

    [ELF] Emit synthetic local in symbol table for non-preemptible IFUNC (#210616)
    Currently we create an internal alias of the original symbol and then
    rewrite the latter to point to the IPLT entry, which means we lose
    symbol table information for the actual resolver. In practice compilers
    also emit a normal function symbol for the resolver due to how IFUNCs
    are represented in GNU C so that at least shows up, but we shouldn't be
    relying on it. By emitting a synthetic local we can keep having a symbol
    for the resolver whilst still redirecting references to the IPLT entry.

--- lld/ELF/Relocations.cpp
+++ lld/ELF/Relocations.cpp
@@ -1264,9 +1264,8 @@ static bool handleNonPreemptibleIfunc(Ctx &ctx, Symbol &sym, uint16_t flags) {
   if (flags & HAS_DIRECT_RELOC) {
     // Change the value to the IPLT and redirect all references to it.
     auto &d = cast<Defined>(sym);
-    auto *irelativeSym = makeDefined(
-        ctx, d.file, d.getName(), std::as_const(d).binding, d.stOther,
-        std::as_const(d).type, d.value, d.size, d.section);
+    auto *irelativeSym = addSyntheticLocal(ctx, d.getName(), d.type, d.value,
+                                           d.size, *d.section);
     addIpltEntry(*irelativeSym);
     ctx.irelativeSyms.push_back(irelativeSym);
     sym.isInIplt = true;
