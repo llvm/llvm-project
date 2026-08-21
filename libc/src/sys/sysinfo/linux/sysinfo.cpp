75d5d50255a809604d1149ff377957ed9311ceb3
Michael Jones <michaelrj@google.com>
2026-08-21 13:16:53 -0700

    [libc] Implement sysinfo (#217471)
    Adds the public sysinfo function and tests. The internal wrapper already
    existed. I decided to avoid defining `struct sysinfo` since it's a
    kernel struct defined in the UAPI headers. Can revisit in future if
    necessary.
    
    Assisted-by: Automated tooling, human reviewed.

--- libc/src/sys/sysinfo/linux/sysinfo.cpp
+++ libc/src/sys/sysinfo/linux/sysinfo.cpp
@@ -7,28 +7,27 @@
 //===----------------------------------------------------------------------===//
 ///
 /// \file
-/// Syscall wrapper for sysinfo.
+/// Linux implementation of sysinfo.
 ///
 //===----------------------------------------------------------------------===//
 
-#ifndef LLVM_LIBC_SRC___SUPPORT_OSUTIL_SYSCALL_WRAPPERS_SYSINFO_H
-#define LLVM_LIBC_SRC___SUPPORT_OSUTIL_SYSCALL_WRAPPERS_SYSINFO_H
+#include "src/sys/sysinfo/sysinfo.h"
 
 #include "hdr/types/struct_sysinfo.h"
-#include "src/__support/OSUtil/linux/syscall.h" // For syscall_checked
+#include "src/__support/OSUtil/linux/syscall_wrappers/sysinfo.h"
 #include "src/__support/common.h"
-#include "src/__support/error_or.h"
+#include "src/__support/libc_errno.h"
 #include "src/__support/macros/config.h"
-#include <sys/syscall.h> // For syscall numbers
 
 namespace LIBC_NAMESPACE_DECL {
-namespace linux_syscalls {
 
-LIBC_INLINE ErrorOr<int> sysinfo(struct sysinfo *info) {
-  return syscall_checked<int>(SYS_sysinfo, info);
+LLVM_LIBC_FUNCTION(int, sysinfo, (struct sysinfo * info)) {
+  auto result = linux_syscalls::sysinfo(info);
+  if (!result) {
+    libc_errno = result.error();
+    return -1;
+  }
+  return 0;
 }
 
-} // namespace linux_syscalls
 } // namespace LIBC_NAMESPACE_DECL
-
-#endif // LLVM_LIBC_SRC___SUPPORT_OSUTIL_SYSCALL_WRAPPERS_SYSINFO_H
--- libc/src/sys/sysinfo/sysinfo.h
+++ libc/src/sys/sysinfo/sysinfo.h
@@ -7,28 +7,20 @@
 //===----------------------------------------------------------------------===//
 ///
 /// \file
-/// Syscall wrapper for sysinfo.
+/// Implementation header for sysinfo.
 ///
 //===----------------------------------------------------------------------===//
 
-#ifndef LLVM_LIBC_SRC___SUPPORT_OSUTIL_SYSCALL_WRAPPERS_SYSINFO_H
-#define LLVM_LIBC_SRC___SUPPORT_OSUTIL_SYSCALL_WRAPPERS_SYSINFO_H
+#ifndef LLVM_LIBC_SRC_SYS_SYSINFO_SYSINFO_H
+#define LLVM_LIBC_SRC_SYS_SYSINFO_SYSINFO_H
 
 #include "hdr/types/struct_sysinfo.h"
-#include "src/__support/OSUtil/linux/syscall.h" // For syscall_checked
-#include "src/__support/common.h"
-#include "src/__support/error_or.h"
 #include "src/__support/macros/config.h"
-#include <sys/syscall.h> // For syscall numbers
 
 namespace LIBC_NAMESPACE_DECL {
-namespace linux_syscalls {
 
-LIBC_INLINE ErrorOr<int> sysinfo(struct sysinfo *info) {
-  return syscall_checked<int>(SYS_sysinfo, info);
-}
+int sysinfo(struct sysinfo *info);
 
-} // namespace linux_syscalls
 } // namespace LIBC_NAMESPACE_DECL
 
-#endif // LLVM_LIBC_SRC___SUPPORT_OSUTIL_SYSCALL_WRAPPERS_SYSINFO_H
+#endif // LLVM_LIBC_SRC_SYS_SYSINFO_SYSINFO_H
