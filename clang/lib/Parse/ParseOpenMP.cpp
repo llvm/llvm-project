a5bfa4bad9252b385e168743492d0b7083235942
Shengxin Pei <TPPPP72@outlook.com>
2026-08-22 01:42:56 +0800

    fix ci

--- clang/lib/Parse/ParseOpenMP.cpp
+++ clang/lib/Parse/ParseOpenMP.cpp
@@ -2218,7 +2218,7 @@ Parser::DeclGroupPtrTy Parser::ParseOpenMPDeclarativeDirectiveWithExtDecl(
     DeclGroupRef DG = Ptr.get();
     SourceManager &SM = PP.getSourceManager();
     if (llvm::none_of(DG, [&](const Decl *D) {
-          return !SM.isBeforeInTranslationUnit(Loc, D->getBeginLoc());
+          return SM.isBeforeInTranslationUnit(Loc, D->getBeginLoc());
         })) {
       Diag(Loc, diag::err_omp_decl_in_declare_simd_variant)
           << (DKind == OMPD_declare_simd ? 0 : 1);
