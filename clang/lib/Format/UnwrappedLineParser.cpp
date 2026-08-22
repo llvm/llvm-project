8d47f5e1f466afe5a01dc55cea3d02a528a4fed4
Björn Schäpers <bjoern@hazardy.de>
2026-08-22 18:12:27 +0200

    [clang-format][NFC] Apply review comments
    That were missed while merging #212856.

--- clang/lib/Format/UnwrappedLineParser.cpp
+++ clang/lib/Format/UnwrappedLineParser.cpp
@@ -2675,7 +2675,7 @@ bool UnwrappedLineParser::parseParens(TokenType StarAndAmpTokenType,
   bool SeenComma = false;
   bool SeenEqual = false;
   bool MightBeFoldExpr = false;
-  auto ExcessLess = 0;
+  unsigned ExcessLess = 0;
   nextToken();
   const bool MightBeStmtExpr = FormatTok->is(tok::l_brace);
   if (!InMacroCall && Prev && Prev->is(TT_FunctionLikeMacro))
@@ -2804,7 +2804,7 @@ bool UnwrappedLineParser::parseParens(TokenType StarAndAmpTokenType,
       parseRequiresExpression();
       break;
     case tok::less:
-      // We have here no clue wether this is a less, or a template opener, opt
+      // We have here no clue whether this is a less, or a template opener, opt
       // out of the predefined StarAndAmpTokenType.
       ++ExcessLess;
       nextToken();
