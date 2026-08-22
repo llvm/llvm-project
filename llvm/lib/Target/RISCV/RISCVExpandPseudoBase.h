cfb58c728ebf1f2f6ce4d9e521b28e72c0320592
Sam Elliott <aelliott@qti.qualcomm.com>
2026-08-22 16:10:12 -0700

    [RISCV][NFCI] Introduce Base for Pseudo Expansion Passes
    This introduces a base class for the Pseudo Instruction Expansion Pass
    Implementations, which shares the boiler plate that ensures the function
    size estimate does not grow during expansion.
    
    This also makes many of those methods `const`, to more clearly
    illustrate how the Impl class works.
    
    This is used to simplify all four of the Pseudo Instruction Expansion
    passes. Additionally, many static functions in the Atomic Expand pass
    are moved onto the Impl class, so they can directly access the
    subtarget and instruction info.
    
    Assisted-by: AI

---
+++ llvm/lib/Target/RISCV/RISCVExpandPseudoBase.h
@@ -0,0 +1,41 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file contains the base class used by the several RISC-V Pseudo
+// Instruction Expansion passes. This avoids having to re-implement some of the
+// boilerplate needed in these passes.
+//
+//===----------------------------------------------------------------------===//
+
+#include "RISCV.h"
+#include "llvm/CodeGen/MachineFunction.h"
+
+namespace llvm {
+
+class RISCVSubtarget;
+class RISCVInstrInfo;
+
+class RISCVExpandPseudoImplBase {
+public:
+  bool run(MachineFunction &MF);
+
+  virtual ~RISCVExpandPseudoImplBase() = default;
+
+protected:
+  const RISCVSubtarget *STI;
+  const RISCVInstrInfo *TII;
+
+  virtual bool expandMI(MachineBasicBlock &MBB,
+                        MachineBasicBlock::iterator MBBI,
+                        MachineBasicBlock::iterator &NextMBBI) const;
+
+private:
+  bool expandMBB(MachineBasicBlock &MBB) const;
+};
+
+} // namespace llvm
