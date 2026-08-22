48ae7199b046763caf82661ef1f29b3c5d6d00ac
Pedro Vicente <pedro.costa.vicente@tecnico.ulisboa.pt>
2026-08-23 00:49:54 +0100

    update on Verifier

--- llvm/lib/IR/Verifier.cpp
+++ llvm/lib/IR/Verifier.cpp
@@ -4429,21 +4429,27 @@ void Verifier::visitShuffleVectorInst(ShuffleVectorInst &SV) {
 
 void Verifier::visitBitInsertInst(BitInsertInst &BII) {
   Check(BitInsertInst::isValidOperands(BII.getOperand(0), BII.getOperand(1),
-                                       BII.getOperand(2)),
+                                        BII.getOperand(2)),
         "Invalid bitinsert operands!", &BII);
   Check(DL.getTypeSizeInBits(BII.getOperand(0)->getType()) >=
-            DL.getTypeSizeInBits(BII.getOperand(1)->getType()),
+        DL.getTypeSizeInBits(BII.getOperand(1)->getType()),
         "bitinsert val type cannot be wider than base type!", &BII);
+  if (auto *PTy = dyn_cast<PointerType>(BII.getOperand(1)->getType()))
+    Check(!DL.isNonIntegralPointerType(PTy),
+          "bitinsert not supported for non-integral pointer types", &BII);
   visitInstruction(BII);
 }
 
 void Verifier::visitBitExtractInst(BitExtractInst &BEI) {
   Check(BitExtractInst::isValidOperands(BEI.getType(), BEI.getOperand(0),
-                                        BEI.getOperand(1)),
+                                         BEI.getOperand(1)),
         "Invalid bitextract operands!", &BEI);
   Check(DL.getTypeSizeInBits(BEI.getType()) <=
-            DL.getTypeSizeInBits(BEI.getOperand(0)->getType()),
+        DL.getTypeSizeInBits(BEI.getOperand(0)->getType()),
         "bitextract result type cannot be wider than source type!", &BEI);
+  if (auto *PTy = dyn_cast<PointerType>(BEI.getType()))
+    Check(!DL.isNonIntegralPointerType(PTy),
+          "bitextract not supported for non-integral pointer types", &BEI);
   visitInstruction(BEI);
 }
 
