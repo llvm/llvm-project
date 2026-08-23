llvm/llvm-project

commit a6d7b2f5a9310966a827db0741916d9fa9f50c9f

[DirectX] Add support for heap resources to `DXILResourceMap` (#216454)
`DXILResourceMap` now handles a new `llvm.dx.resource.handlefromheap`
intrisics and adds the heap resources to the resource map.

A new member `HeapResourceID` has been added to `ResourceInfo` to
distinguish between heap resource instances created from different
indices. The `HeapResourceID` is unique for each heap index `Value*`, so
multiple handle creation calls using the same index `Value*` resolve to
the same resource.

Heap resources do not have register bindings, so the `Binding` member on
`ResourceInfo` is now optional.

All places that were expecting binding to exist are updated to handle
bindless heap resources. In most cases that means skipping them, such as
when generating DXIL resource metadata, creating PSV resource entries or
pretty-printing the resource table comment for the module disassembly
output.

Also, diagnostics of conflicting increment and decrement operations now
works on heap resources.

Fixes #125126
Related to #208053

llvm/include/llvm/Analysis/DXILResource.h
@@ -21,6 +21,7 @@
 #include "llvm/Support/Compiler.h"
 #include "llvm/Support/DXILABI.h"
 #include <cstdint>
+#include <variant>
 
 namespace llvm {
 class CallInst;
@@ -402,7 +403,7 @@ public:
   };
 
 private:
-  ResourceBinding Binding;
+  std::variant<ResourceBinding, uint32_t> BindingOrHeapID;
   TargetExtType *HandleTy;
   StringRef Name;
   GlobalVariable *Symbol = nullptr;
@@ -415,16 +416,39 @@ public:
   ResourceInfo(uint32_t Space, uint32_t LowerBound, uint32_t Size,
                TargetExtType *HandleTy, StringRef Name = "",
                GlobalVariable *Symbol = nullptr)
-      : Binding{0, Space, LowerBound, Size}, HandleTy(HandleTy), Name(Name),
-        Symbol(Symbol) {}
+      : BindingOrHeapID{ResourceBinding{0, Space, LowerBound, Size}},
+        HandleTy(HandleTy), Name(Name), Symbol(Symbol) {}
 
-  void setBindingID(unsigned ID) { Binding.BindingID = ID; }
+  ResourceInfo(uint32_t HeapResourceID, TargetExtType *HandleTy)
+      : BindingOrHeapID{HeapResourceID}, HandleTy(HandleTy), Name(""),
+        Symbol(nullptr) {}
+
+  bool hasBinding() const {
+    return std::holds_alternative<ResourceBinding>(BindingOrHeapID);
+  }
+  void setBindingID(unsigned ID) {
+    assert(hasBinding() && "Resource does not have a binding");
+    std::get<ResourceBinding>(BindingOrHeapID).BindingID = ID;
+  }
 
   bool hasCounter() const {
     return CounterDirection != ResourceCounterDirection::Unknown;
   }
 
-  const ResourceBinding &getBinding() const { return Binding; }
+  const ResourceBinding &getBinding() const {
+    assert(hasBinding() && "Resource does not have a binding");
+    return std::get<ResourceBinding>(BindingOrHeapID);
+  }
+
+  uint32_t getHeapID() const {
+    assert(!hasBinding() && "Resource does not have a heap ID");
+    return std::get<uint32_t>(BindingOrHeapID);
+  }
+
+  uint32_t getSize() const {
+    return hasBinding() ? std::get<ResourceBinding>(BindingOrHeapID).Size : 1;
+  }
+
   TargetExtType *getHandleTy() const { return HandleTy; }
   StringRef getName() const { return Name; }
 
@@ -436,12 +460,12 @@ public:
   getAnnotateProps(Module &M, dxil::ResourceTypeInfo &RTI) const;
 
   bool operator==(const ResourceInfo &RHS) const {
-    return std::tie(Binding, HandleTy, Symbol, Name) ==
-           std::tie(RHS.Binding, RHS.HandleTy, RHS.Symbol, RHS.Name);
+    return std::tie(BindingOrHeapID, HandleTy, Symbol, Name) ==
+           std::tie(RHS.BindingOrHeapID, RHS.HandleTy, RHS.Symbol, RHS.Name);
   }
   bool operator!=(const ResourceInfo &RHS) const { return !(*this == RHS); }
   bool operator<(const ResourceInfo &RHS) const {
-    return Binding < RHS.Binding;
+    return BindingOrHeapID < RHS.BindingOrHeapID;
   }
 
   LLVM_ABI void print(raw_ostream &OS, dxil::ResourceTypeInfo &RTI,

llvm/include/llvm/IR/IntrinsicsDirectX.td
@@ -36,6 +36,12 @@ def int_dx_resource_handlefromimplicitbinding
           [llvm_i32_ty, llvm_i32_ty, llvm_i32_ty, llvm_i32_ty, llvm_ptr_ty],
           [IntrNoMem]>;
 
+// Create resource handle from a descriptor heap. Returns a `target("dx.")`
+// type appropriate for the kind of resource given a heap index. Type of
+// heap is determined by the intrinsic's resource overload type.
+def int_dx_resource_handlefromheap
+    : DefaultAttrsIntrinsic<[llvm_any_ty], [llvm_i32_ty], [IntrNoMem]>;
+
 def int_dx_resource_getpointer
     : DefaultAttrsIntrinsic<[llvm_anyptr_ty], [llvm_any_ty, llvm_any_ty],
                             [IntrConvergent, IntrNoMem]>;

llvm/lib/Analysis/DXILResource.cpp
@@ -634,7 +634,7 @@ void ResourceTypeInfo::print(raw_ostream &OS, const DataLayout &DL) const {
 GlobalVariable *ResourceInfo::createSymbol(Module &M, StructType *Ty) {
   assert(!Symbol && "Symbol has already been created");
   Type *ResTy = Ty;
-  int64_t Size = Binding.Size;
+  int64_t Size = getSize();
   if (Size != 1)
     // unbounded arrays are represented as zero-sized arrays in LLVM IR
     ResTy = ArrayType::get(Ty, Size == ~0u ? 0 : Size);
@@ -646,6 +646,9 @@ GlobalVariable *ResourceInfo::createSymbol(Module &M, StructType *Ty) {
 
 MDTuple *ResourceInfo::getAsMetadata(Module &M,
                                      dxil::ResourceTypeInfo &RTI) const {
+  assert(hasBinding() && "Resource must not be from heap to get metadata");
+  const ResourceBinding &Binding = getBinding();
+
   LLVMContext &Ctx = M.getContext();
   const DataLayout &DL = M.getDataLayout();
 
@@ -773,11 +776,16 @@ void ResourceInfo::print(raw_ostream &OS, dxil::ResourceTypeInfo &RTI,
     OS << "\n";
   }
 
-  OS << "  Binding:\n"
-     << "    Binding ID: " << Binding.BindingID << "\n"
-     << "    Space: " << Binding.Space << "\n"
-     << "    Lower Bound: " << Binding.LowerBound << "\n"
-     << "    Size: " << Binding.Size << "\n";
+  if (hasBinding()) {
+    const ResourceBinding &Binding = getBinding();
+    OS << "  Binding:\n"
+       << "    Binding ID: " << Binding.BindingID << "\n"
+       << "    Space: " << Binding.Space << "\n"
+       << "    Lower Bound: " << Binding.LowerBound << "\n"
+       << "    Size: " << Binding.Size << "\n";
+  } else {
+    OS << "  HeapIndexID: " << getHeapID() << "\n";
+  }
 
   OS << "  Globally Coherent: " << GloballyCoherent << "\n";
   OS << "  Has Atomic64 Use: " << HasAtomic64Use << "\n";
@@ -839,6 +847,12 @@ void DXILResourceMap::populateResourceInfos(Module &M,
                                             DXILResourceTypeMap &DRTM) {
   SmallVector<std::tuple<CallInst *, ResourceInfo, ResourceTypeInfo>> CIToInfos;
 
+  // We need to assign a unique ID to each resource that is created
+  // from a heap. The ID must be unique for each unique Index value so
+  // we can differentiate between resources instances of the same type.
+  SmallDenseMap<Value *, uint32_t, 8> IndexToHeapResID;
+  uint32_t NextHeapResID = 0;
+
   for (Function &F : M.functions()) {
     if (!F.isDeclaration())
       continue;
@@ -868,6 +882,28 @@ void DXILResourceMap::populateResourceInfos(Module &M,
           CIToInfos.emplace_back(CI, RI, RTI);
         }
 
+      break;
+    }
+    case Intrinsic::dx_resource_handlefromheap: {
+      auto *HandleTy = cast<TargetExtType>(F.getReturnType());
+      ResourceTypeInfo &RTI = DRTM[HandleTy];
+
+      for (User *U : F.users()) {
+        if (CallInst *CI = dyn_cast<CallInst>(U)) {
+          LLVM_DEBUG(dbgs() << "  Visiting: " << *U << "\n");
+          Value *Index = CI->getArgOperand(0);
+          uint32_t HeapResID;
+          auto Pos = IndexToHeapResID.find(Index);
+          if (Pos == IndexToHeapResID.end()) {
+            HeapResID = NextHeapResID++;
+            IndexToHeapResID[Index] = HeapResID;
+          } else {
+            HeapResID = Pos->second;
+          }
+          ResourceInfo RI = ResourceInfo{HeapResID, HandleTy};
+          CIToInfos.emplace_back(CI, RI, RTI);
+        }
+      }
       break;
     }
     }
@@ -912,8 +948,8 @@ void DXILResourceMap::populateResourceInfos(Module &M,
     FirstCBuffer = std::min({FirstCBuffer, FirstSampler});
     FirstUAV = std::min({FirstUAV, FirstCBuffer});
 
-    // Adjust the resource binding to use the next ID.
-    RI.setBindingID(NextID++);
+    if (RI.hasBinding())
+      RI.setBindingID(NextID++);
   }
 }
 
@@ -1020,9 +1056,11 @@ SmallVector<dxil::ResourceInfo *> DXILResourceMap::findByUse(const Value *Key) {
 
   switch (CI->getIntrinsicID()) {
   // Found the create, return the binding
-  case Intrinsic::dx_resource_handlefrombinding: {
+  case Intrinsic::dx_resource_handlefrombinding:
+  case Intrinsic::dx_resource_handlefromheap: {
     auto Pos = CallMap.find(CI);
-    assert(Pos != CallMap.end() && "HandleFromBinding must be in resource map");
+    assert(Pos != CallMap.end() &&
+           "handle initialization call must be in resource map");
     return {&Infos[Pos->second]};
   }
   default:

llvm/lib/Target/DirectX/DXContainerGlobals.cpp
@@ -285,17 +285,23 @@ void DXContainerGlobals::addResourcesForPSV(Module &M, PSVRuntimeInfo &PSV) {
       };
 
   for (const dxil::ResourceInfo &RI : DRM.cbuffers()) {
+    if (!RI.hasBinding())
+      continue;
     const dxil::ResourceInfo::ResourceBinding &Binding = RI.getBinding();
     PSV.Resources.push_back(MakeBinding(Binding, dxbc::PSV::ResourceType::CBV,
                                         dxil::ResourceKind::CBuffer));
   }
   for (const dxil::ResourceInfo &RI : DRM.samplers()) {
+    if (!RI.hasBinding())
+      continue;
     const dxil::ResourceInfo::ResourceBinding &Binding = RI.getBinding();
     PSV.Resources.push_back(MakeBinding(Binding,
                                         dxbc::PSV::ResourceType::Sampler,
                                         dxil::ResourceKind::Sampler));
   }
   for (const dxil::ResourceInfo &RI : DRM.srvs()) {
+    if (!RI.hasBinding())
+      continue;
     const dxil::ResourceInfo::ResourceBinding &Binding = RI.getBinding();
 
     dxil::ResourceTypeInfo &TypeInfo = DRTM[RI.getHandleTy()];
@@ -311,6 +317,8 @@ void DXContainerGlobals::addResourcesForPSV(Module &M, PSVRuntimeInfo &PSV) {
         MakeBinding(Binding, ResType, TypeInfo.getResourceKind()));
   }
   for (const dxil::ResourceInfo &RI : DRM.uavs()) {
+    if (!RI.hasBinding())
+      continue;
     const dxil::ResourceInfo::ResourceBinding &Binding = RI.getBinding();
 
     dxil::ResourceTypeInfo &TypeInfo = DRTM[RI.getHandleTy()];

llvm/lib/Target/DirectX/DXILPrettyPrinter.cpp
@@ -243,6 +243,8 @@ static void prettyPrintResources(raw_ostream &OS, const DXILResourceMap &DRM,
 
   // TODO: Do we want to sort these by binding or something like that?
   for (const dxil::ResourceInfo &RI : DRM) {
+    if (!RI.hasBinding())
+      continue;
     const dxil::ResourceTypeInfo &RTI = DRTM[RI.getHandleTy()];
 
     dxil::ResourceClass RC = RTI.getResourceClass();

llvm/lib/Target/DirectX/DXILShaderFlags.cpp
@@ -342,7 +342,7 @@ ModuleShaderFlags::gatherGlobalModuleFlags(const Module &M,
     if (MMDI.ValidatorVersion < VersionTuple(1, 6)) {
       NumUAVs++;
     } else { // MMDI.ValidatorVersion >= VersionTuple(1, 6)
-      uint32_t Size = UAV.getBinding().Size;
+      uint32_t Size = UAV.getSize();
       uint32_t NewNum = NumUAVs + (Size == 0 ? ~0U : Size);
       if (NewNum < NumUAVs)
         NewNum = ~0U;

llvm/lib/Target/DirectX/DXILTranslateMetadata.cpp
@@ -92,19 +92,23 @@ static NamedMDNode *emitResourceMetadata(Module &M, DXILResourceMap &DRM,
   LLVMContext &Context = M.getContext();
 
   for (ResourceInfo &RI : DRM)
-    if (!RI.hasSymbol())
+    if (RI.hasBinding() && !RI.hasSymbol())
       RI.createSymbol(M,
                       DRTM[RI.getHandleTy()].createElementStruct(RI.getName()));
 
   SmallVector<Metadata *> SRVs, UAVs, CBufs, Smps;
   for (const ResourceInfo &RI : DRM.srvs())
-    SRVs.push_back(RI.getAsMetadata(M, DRTM[RI.getHandleTy()]));
+    if (RI.hasBinding())
+      SRVs.push_back(RI.getAsMetadata(M, DRTM[RI.getHandleTy()]));
   for (const ResourceInfo &RI : DRM.uavs())
-    UAVs.push_back(RI.getAsMetadata(M, DRTM[RI.getHandleTy()]));
+    if (RI.hasBinding())
+      UAVs.push_back(RI.getAsMetadata(M, DRTM[RI.getHandleTy()]));
   for (const ResourceInfo &RI : DRM.cbuffers())
-    CBufs.push_back(RI.getAsMetadata(M, DRTM[RI.getHandleTy()]));
+    if (RI.hasBinding())
+      CBufs.push_back(RI.getAsMetadata(M, DRTM[RI.getHandleTy()]));
   for (const ResourceInfo &RI : DRM.samplers())
-    Smps.push_back(RI.getAsMetadata(M, DRTM[RI.getHandleTy()]));
+    if (RI.hasBinding())
+      Smps.push_back(RI.getAsMetadata(M, DRTM[RI.getHandleTy()]));
 
   Metadata *SRVMD = SRVs.empty() ? nullptr : MDNode::get(Context, SRVs);
   Metadata *UAVMD = UAVs.empty() ? nullptr : MDNode::get(Context, UAVs);

llvm/test/Analysis/DXILResource/buffer-fromheap.ll
@@ -1,0 +1,186 @@
+; RUN: opt -S -disable-output -passes="print<dxil-resources>" < %s 2>&1 | FileCheck %s
+
+%struct.S = type { <4 x float>, <4 x i32> }
+%P = type <{ float }>
+%Q = type <{ <{ [2 x <{ float, target("dx.Padding", 12) }>], float }> }>
+
+; The resources in this test are created in the same order as they appear after
+; sorting in the ResourceInfo list because FileCheck cannot match multiline sections
+; of text in arbitrary order.
+
+define void @test_typedbuffer() {
+
+  %idx = tail call i32 @llvm.dx.thread.id.in.group(i32 0)
+
+  ; ByteAddressBuffer Buf0 = ResourceDescriptorHeap[5];
+  %srv0 = tail call target("dx.RawBuffer", i8, 0, 0)
+      @llvm.dx.resource.handlefromheap.tdx.RawBuffer_i8_0_0t(i32 5)
+; CHECK: Resource [[SRV0:0]]:
+; CHECK-NEXT:   HeapIndexID: {{[0-9]+}}
+; CHECK-NEXT:   Globally Coherent: 0
+; CHECK-NEXT:   Has Atomic64 Use: 0
+; CHECK-NEXT:   Counter Direction: Unknown
+; CHECK-NEXT:   Class: SRV
+; CHECK-NEXT:   Kind: RawBuffer
+
+  ; struct S { float4 a; uint4 b; };
+  ; StructuredBuffer<S> Buf1 = ResourceDescriptorHeap[ID.x];
+  %srv1 = tail call target("dx.RawBuffer", %struct.S, 0, 0)
+      @llvm.dx.resource.handlefromheap.tdx.RawBuffer_s_struct.Ss_0_0t(i32 %idx)
+
+  ; Duplicated initialization should resolve to the same resource.
+  %srv1_dupl = tail call target("dx.RawBuffer", %struct.S, 0, 0)
+      @llvm.dx.resource.handlefromheap.tdx.RawBuffer_s_struct.Ss_0_0t(i32 %idx)
+; CHECK: Resource [[SRV1:1]]:
+; CHECK: HeapIndexID: {{[0-9]+}}
+; CHECK-NEXT:   Globally Coherent: 0
+; CHECK-NEXT:   Has Atomic64 Use: 0
+; CHECK-NEXT:   Counter Direction: Unknown
+; CHECK-NEXT:   Class: SRV
+; CHECK-NEXT:   Kind: StructuredBuffer
+; CHECK-NEXT:   Buffer Stride: 32
+; CHECK-NEXT:   Alignment: 4
+
+  ; Buffer<uint4> Buf2 = ResourceDescriptorHeap[ID.x + 1];
+  %add0 = add i32 %idx, 1
+  %srv2 = tail call target("dx.TypedBuffer", <4 x i32>, 0, 0, 0)
+      @llvm.dx.resource.handlefromheap.tdx.TypedBuffer_v4i32_0_0_0t(i32 %add0)
+; CHECK: Resource [[SRV2:2]]:
+; CHECK-NEXT:   HeapIndexID: {{[0-9]+}}
+; CHECK-NEXT:   Globally Coherent: 0
+; CHECK-NEXT:   Has Atomic64 Use: 0
+; CHECK-NEXT:   Counter Direction: Unknown
+; CHECK-NEXT:   Class: SRV
+; CHECK-NEXT:   Kind: Buffer
+; CHECK-NEXT:   Element Type: u32
+; CHECK-NEXT:   Element Count: 4
+
+
+; Make sure this was the last SRV resource in the list.
+; CHECK-NOT: Class: SRV
+
+  ; RWStructuredBuffer<double> Buf6 = ResourceDescriptorHeap[ID.x + 5];
+  %add1 = add i32 %idx, 5
+  %uav0 = tail call target("dx.RawBuffer", double, 1, 0)
+      @llvm.dx.resource.handlefromheap.tdx.RawBuffer_f64_1_0t(i32 %add1)
+; CHECK: Resource [[UAV0:3]]:
+; CHECK-NEXT:   HeapIndexID: {{[0-9]+}}
+; CHECK-NEXT:   Globally Coherent: 0
+; CHECK-NEXT:   Has Atomic64 Use: 0
+; CHECK-NEXT:   Counter Direction: Unknown
+; CHECK-NEXT:   Class: UAV
+; CHECK-NEXT:   Kind: StructuredBuffer
+; CHECK-NEXT:   IsROV: 0
+; CHECK-NEXT:   Buffer Stride: 8
+; CHECK-NEXT:   Alignment: 0
+
+  ; RWStructuredBuffer<float4> Buf4 = ResourceDescriptorHeap[ID.x + 3];
+  ; Buf4.DecrementCounter();
+  %add2 = add i32 %idx, 2
+  %uav1 = tail call target("dx.RawBuffer", <4 x float>, 1, 0)
+      @llvm.dx.resource.handlefromheap.tdx.RawBuffer_v4f32_1_0t(i32 %add2)
+  %count0 = tail call noundef i32 @llvm.dx.resource.updatecounter.tdx.RawBuffer_v4f32_1_0t(target("dx.RawBuffer", <4 x float>, 1, 0) %uav1, i8 -1)
+; CHECK: Resource [[UAV1:4]]:
+; CHECK-NEXT:   HeapIndexID: {{[0-9]+}}
+; CHECK-NEXT:   Globally Coherent: 0
+; CHECK-NEXT:   Has Atomic64 Use: 0
+; CHECK-NEXT:   Counter Direction: Decrement
+; CHECK-NEXT:   Class: UAV
+; CHECK-NEXT:   Kind: StructuredBuffer
+; CHECK-NEXT:   IsROV: 0
+; CHECK-NEXT:   Buffer Stride: 16
+; CHECK-NEXT:   Alignment: 0
+
+  ; RWStructuredBuffer<float4> Buf5 = ResourceDescriptorHeap[ID.x + 4];
+  ; Buf5.DecrementCounter();
+  ; Buf5.IncrementCounter();
+  %add3 = add i32 %idx, 3
+  %uav2 = tail call target("dx.RawBuffer", <4 x float>, 1, 0)
+      @llvm.dx.resource.handlefromheap.tdx.RawBuffer_v4f32_1_0t(i32 %add3)
+  %14 = tail call noundef i32 @llvm.dx.resource.updatecounter.tdx.RawBuffer_v4f32_1_0t(target("dx.RawBuffer", <4 x float>, 1, 0) %uav2, i8 -1)
+  %15 = tail call noundef i32 @llvm.dx.resource.updatecounter.tdx.RawBuffer_v4f32_1_0t(target("dx.RawBuffer", <4 x float>, 1, 0) %uav2, i8 1)
+; CHECK: Resource [[UAV2:5]]:
+; CHECK-NEXT:   HeapIndexID: {{[0-9]+}}
+; CHECK-NEXT:   Globally Coherent: 0
+; CHECK-NEXT:   Has Atomic64 Use: 0
+; CHECK-NEXT:   Counter Direction: Invalid
+; CHECK-NEXT:   Class: UAV
+; CHECK-NEXT:   Kind: StructuredBuffer
+; CHECK-NEXT:   IsROV: 0
+; CHECK-NEXT:   Buffer Stride: 16
+; CHECK-NEXT:   Alignment: 0
+
+  ; RWBuffer<int> Buf3 = ResourceDescriptorHeap[ID.x + 2];
+  %uav3 = tail call target("dx.TypedBuffer", i32, 1, 0, 1)
+      @llvm.dx.resource.handlefromheap.tdx.TypedBuffer_i32_1_0_1t(i32 10)
+
+  ; Duplicated initialization should resolve to the same resource.
+  %uav3_dupl = tail call target("dx.TypedBuffer", i32, 1, 0, 1)
+      @llvm.dx.resource.handlefromheap.tdx.TypedBuffer_i32_1_0_1t(i32 10)
+; CHECK: Resource [[UAV3:6]]:
+; CHECK-NEXT:   HeapIndexID: {{[0-9]+}}
+; CHECK-NEXT:   Globally Coherent: 0
+; CHECK-NEXT:   Has Atomic64 Use: 0
+; CHECK-NEXT:   Counter Direction: Unknown
+; CHECK-NEXT:   Class: UAV
+; CHECK-NEXT:   Kind: Buffer
+; CHECK-NEXT:   IsROV: 0
+; CHECK-NEXT:   Element Type: i32
+; CHECK-NEXT:   Element Count: 1
+
+; Make sure this was the last UAV resource in the list.
+; CHECK-NOT: Class: UAV
+
+  ; struct P { float a; };
+  ; ConstantBuffer<P> CB1 = ResourceDescriptorHeap[ID.x + 6];
+  %add4 = add i32 %idx, 4
+  %cbv0 = tail call target("dx.CBuffer", %P)
+      @llvm.dx.resource.handlefromheap.tdx.CBuffer_s_Pst(i32 %add4)
+; CHECK: Resource [[CBV0:7]]:
+; CHECK-NEXT:   HeapIndexID: {{[0-9]+}}
+; CHECK-NEXT:   Globally Coherent: 0
+; CHECK-NEXT:   Has Atomic64 Use: 0
+; CHECK-NEXT:   Counter Direction: Unknown
+; CHECK-NEXT:   Class: CBV
+; CHECK-NEXT:   Kind: CBuffer
+; CHECK-NEXT:   CBuffer size: 4
+
+  ; struct Q { float b[3]; };
+  ; ConstantBuffer<Q> CB2 = ResourceDescriptorHeap[ID.x + 7];
+  %add5 = add i32 %idx, 5
+  %cbv1 = tail call target("dx.CBuffer", %Q)
+      @llvm.dx.resource.handlefromheap.tdx.CBuffer_s_Qst(i32 %add5)
+
+  ; Duplicated initialization should resolve to the same resource.
+  %cbv1_dupl = tail call target("dx.CBuffer", %Q)
+      @llvm.dx.resource.handlefromheap.tdx.CBuffer_s_Qst(i32 %add5)
+
+; CHECK: Resource [[CBV1:8]]:
+; CHECK-NEXT:   HeapIndexID: {{[0-9]+}}
+; CHECK-NEXT:   Globally Coherent: 0
+; CHECK-NEXT:   Has Atomic64 Use: 0
+; CHECK-NEXT:   Counter Direction: Unknown
+; CHECK-NEXT:   Class: CBV
+; CHECK-NEXT:   Kind: CBuffer
+; CHECK-NEXT:   CBuffer size: 36
+
+; Make sure this was the last CBV resource in the list.
+; CHECK-NOT: Class: CBV
+
+  ret void
+}
+
+; CHECK-DAG: Call bound to [[SRV0]]:  %srv0 = tail call target("dx.RawBuffer", i8, 0, 0) @llvm.dx.resource.handlefromheap.tdx.RawBuffer_i8_0_0t(i32 5)
+; CHECK-DAG: Call bound to [[SRV1]]:  %srv1 = tail call target("dx.RawBuffer", %struct.S, 0, 0) @llvm.dx.resource.handlefromheap.tdx.RawBuffer_s_struct.Ss_0_0t(i32 %idx)
+; CHECK-DAG: Call bound to [[SRV2]]:  %srv2 = tail call target("dx.TypedBuffer", <4 x i32>, 0, 0, 0) @llvm.dx.resource.handlefromheap.tdx.TypedBuffer_v4i32_0_0_0t(i32 %add0)
+; CHECK-DAG: Call bound to [[UAV0]]:  %uav0 = tail call target("dx.RawBuffer", double, 1, 0) @llvm.dx.resource.handlefromheap.tdx.RawBuffer_f64_1_0t(i32 %add1)
+; CHECK-DAG: Call bound to [[UAV1]]:  %uav1 = tail call target("dx.RawBuffer", <4 x float>, 1, 0) @llvm.dx.resource.handlefromheap.tdx.RawBuffer_v4f32_1_0t(i32 %add2)
+; CHECK-DAG: Call bound to [[UAV2]]:  %uav2 = tail call target("dx.RawBuffer", <4 x float>, 1, 0) @llvm.dx.resource.handlefromheap.tdx.RawBuffer_v4f32_1_0t(i32 %add3)
+; CHECK-DAG: Call bound to [[UAV3]]:  %uav3 = tail call target("dx.TypedBuffer", i32, 1, 0, 1) @llvm.dx.resource.handlefromheap.tdx.TypedBuffer_i32_1_0_1t(i32 10)
+; CHECK-DAG: Call bound to [[CBV0]]:  %cbv0 = tail call target("dx.CBuffer", %P) @llvm.dx.resource.handlefromheap.tdx.CBuffer_s_Pst(i32 %add4)
+; CHECK-DAG: Call bound to [[CBV1]]:  %cbv1 = tail call target("dx.CBuffer", %Q) @llvm.dx.resource.handlefromheap.tdx.CBuffer_s_Qst(i32 %add5)
+
+; duplicate calls should map to existing resources in the list, not create new ones
+; CHECK-DAG: Call bound to [[SRV1]]:  %srv1_dupl = tail call target("dx.RawBuffer", %struct.S, 0, 0) @llvm.dx.resource.handlefromheap.tdx.RawBuffer_s_struct.Ss_0_0t(i32 %idx)
+; CHECK-DAG: Call bound to [[CBV1]]:  %cbv1_dupl = tail call target("dx.CBuffer", %Q) @llvm.dx.resource.handlefromheap.tdx.CBuffer_s_Qst(i32 %add5)
+; CHECK-DAG: Call bound to [[UAV3]]:  %uav3_dupl = tail call target("dx.TypedBuffer", i32, 1, 0, 1) @llvm.dx.resource.handlefromheap.tdx.TypedBuffer_i32_1_0_1t(i32 10)

commit 42a4000d3646b9d9cc60e3fc339702a59aa622b4

[NFC][DirectX] Fix GCC 7 build failure caused by a6d7b2f5a931
GCC 7 cannot determine default-constructibility of ResourceBinding
when it has a default member initializer and is used inside
std::variant. Add explicit default and parameterized constructors.

Co-Authored-By: Claude Opus 4.6 (1M context) <noreply@anthropic.com>

llvm/include/llvm/Analysis/DXILResource.h
@@ -377,6 +377,12 @@ public:
     uint32_t LowerBound;
     uint32_t Size;
 
+    ResourceBinding() : BindingID(0), Space(0), LowerBound(0), Size(0) {}
+    ResourceBinding(uint32_t BindingID, uint32_t Space, uint32_t LowerBound,
+                    uint32_t Size)
+        : BindingID(BindingID), Space(Space), LowerBound(LowerBound),
+          Size(Size) {}
+
     bool operator==(const ResourceBinding &RHS) const {
       return std::tie(BindingID, Space, LowerBound, Size) ==
              std::tie(RHS.BindingID, RHS.Space, RHS.LowerBound, RHS.Size);