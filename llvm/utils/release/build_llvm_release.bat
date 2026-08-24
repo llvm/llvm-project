9d0da47a1856dac4428eed65c4f16c1ca24fec6b
Tom Stellard <tstellar@redhat.com>
2026-08-24 11:31:48 -0700

    Build only certian bins

--- llvm/utils/release/build_llvm_release.bat
+++ llvm/utils/release/build_llvm_release.bat
@@ -326,8 +326,8 @@ if "%arch%"=="arm64" (
 cmake -GNinja %cmake_flags% ^
   -DLLVM_TARGETS_TO_BUILD=Native ^
   %llvm_src%\llvm || exit /b 1
+ninja clang-cl lld-link llvm-lib llvm-windres runtimes || exit /b 1
 if "%fast-build%" neq "true" (
-ninja || exit /b 1
 ninja check-llvm || exit /b 1
 ninja check-clang || exit /b 1
 ninja check-lld || exit /b 1
