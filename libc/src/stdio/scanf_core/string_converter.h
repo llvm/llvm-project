18114006584b959928a829a4f38af08ded970834
yahia ahmed <yahia.a.abdrabou@gmail.com>
2026-08-24 07:00:01 +0300

    [libc][stdio] Add support for the %m modifier
    Add support for the %m modifier in scanf by allocating memory
    and recursively allocate more till the object is.

--- libc/src/stdio/scanf_core/string_converter.h
+++ libc/src/stdio/scanf_core/string_converter.h
@@ -10,12 +10,18 @@
 #define LLVM_LIBC_SRC_STDIO_SCANF_CORE_STRING_CONVERTER_H
 
 #include "src/__support/CPP/limits.h"
+#include "src/__support/CPP/new.h"
+#include "src/__support/alloc-checker.h"
 #include "src/__support/ctype_utils.h"
 #include "src/__support/macros/config.h"
 #include "src/stdio/scanf_core/core_structs.h"
 #include "src/stdio/scanf_core/reader.h"
+#include "src/string/memory_utils/inline_memcpy.h"
 
 #include <stddef.h>
+
+constexpr int ALLOCATION_SCALE = 2;
+constexpr int ALLOCATION_BASE = 32;
 
 namespace LIBC_NAMESPACE_DECL {
 namespace scanf_core {
@@ -40,8 +46,26 @@ int convert_string(Reader<T> *reader, const FormatSection &to_conv) {
     }
   }
 
-  char *output = reinterpret_cast<char *>(to_conv.output_ptr);
-
+  char *output;
+ AllocChecker ac;
+#ifndef LIBC_COPT_SCANF_DISABLE_ALLOCATION
+  size_t alloc_size;
+  if ((to_conv.flags & NO_WRITE) == 0 && (to_conv.flags & ALLOCATE) != 0) {
+    if (to_conv.conv_name == 'c')
+      alloc_size = max_width + 1;
+    else
+      alloc_size =
+          (max_width < ALLOCATION_BASE) ? max_width + 1 : ALLOCATION_BASE;
+    output = new (ac) char[alloc_size];
+    if (!ac) {
+      return ALLOCATION_FAILURE;
+    }
+  } else {
+    output = reinterpret_cast<char *>(to_conv.output_ptr);
+  }
+#else
+  output = reinterpret_cast<char *>(to_conv.output_ptr);
+#endif
   char cur_char = reader->getc();
   size_t i = 0;
   for (; i < max_width && cur_char != '\0'; ++i) {
@@ -52,8 +76,26 @@ int convert_string(Reader<T> *reader, const FormatSection &to_conv) {
       break;
     }
     // if the NO_WRITE flag is not set, write to the output.
-    if ((to_conv.flags & NO_WRITE) == 0)
+    if ((to_conv.flags & NO_WRITE) == 0) {
       output[i] = cur_char;
+#ifndef LIBC_COPT_SCANF_DISABLE_ALLOCATION
+      if (((to_conv.flags & ALLOCATE) != 0) && (i + 1) == alloc_size &&
+          alloc_size < max_width) {
+        alloc_size *= ALLOCATION_SCALE;
+        if (alloc_size > max_width + 1)
+          alloc_size = max_width + 1;
+        char *tmp = new (ac) char[alloc_size];
+        if (!ac) {
+          delete[] output;
+          reader->ungetc(cur_char);
+          return ALLOCATION_FAILURE;
+        }
+        inline_memcpy(tmp, output, i + 1);
+        delete[] output;
+        output = tmp;
+      }
+#endif
+    }
     cur_char = reader->getc();
   }
 
@@ -61,8 +103,11 @@ int convert_string(Reader<T> *reader, const FormatSection &to_conv) {
   // last one back.
   reader->ungetc(cur_char);
 
+  bool null_terminate =
+      (to_conv.conv_name != 'c') || ((to_conv.flags & ALLOCATE) != 0);
+
   // If this is %s or %[]
-  if (to_conv.conv_name != 'c' && (to_conv.flags & NO_WRITE) == 0) {
+  if (null_terminate && (to_conv.flags & NO_WRITE) == 0) {
     // Always null terminate the string. This may cause a write to the
     // (max_width + 1) byte, which is correct. The max width describes the max
     // number of characters read from the input string, and doesn't necessarily
@@ -70,8 +115,20 @@ int convert_string(Reader<T> *reader, const FormatSection &to_conv) {
     output[i] = '\0';
   }
 
-  if (i == 0)
+  if (i == 0) {
+#ifndef LIBC_COPT_SCANF_DISABLE_ALLOCATION
+    if ((to_conv.flags & ALLOCATE) != 0 && output)
+      delete[] output;
+#endif
     return MATCHING_FAILURE;
+  }
+
+#ifndef LIBC_COPT_SCANF_DISABLE_ALLOCATION
+  if ((to_conv.flags & ALLOCATE) != 0) {
+    *reinterpret_cast<char **>(to_conv.output_ptr) = output;
+  }
+#endif
+
   return READ_OK;
 }
 
