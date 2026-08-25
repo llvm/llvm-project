42eb5a211d4c77370524941d74dc528ed49e0c15
Florian Hahn <flo@fhahn.com>
2026-08-25 10:25:18 +0100

    [SCEV] Consider IVMayOverflow to use cheap BE-count formula.
    If !IVMayOverflow, we proven RHS + stride - 1 does not wrap. This means
    adding the `Stride - 1` part of `RHS - (Start - Stride) - 1` does not wrap.
    
    Together with the other condition `Start - Stride < RHS`, this should
    guarantee `RHS - (Start - Stride) - 1` does not wrap and the cheap
    formula can be used.
    
    This triggers in a number of real-world cases, mostly resulting in
    simpler SCEV expansions/checks, and extra unrolling. In a small number
    of cases, expansion is slightly worse.
    
    https://github.com/dtcxzyw/llvm-opt-benchmark-nightly/pull/1043
    
    Alive2 Proof: https://alive2.llvm.org/ce/z/u_KUx5
    
    Depends on https://github.com/llvm/llvm-project/pull/218251 (included in
    PR)

--- llvm/lib/Analysis/ScalarEvolution.cpp
+++ llvm/lib/Analysis/ScalarEvolution.cpp
@@ -13546,8 +13546,10 @@ ScalarEvolution::howManyLessThans(const SCEV *LHS, const SCEV *RHS,
     assert(isAvailableAtLoopEntry(OrigStartMinusStride, L) && "Must be!");
     assert(isAvailableAtLoopEntry(OrigStart, L) && "Must be!");
     assert(isAvailableAtLoopEntry(OrigRHS, L) && "Must be!");
-    // Can we prove Start - Stride < Start and Start - Stride < RHS?
-    if (isLoopEntryGuardedByCond(L, Cond, OrigStartMinusStride, OrigStart) &&
+    // Can we prove Start - Stride < RHS, and either Start - Stride < Start or
+    // (via !IVMayOverflow) that RHS + Stride - 1 does not overflow?
+    if ((!IVMayOverflow ||
+         isLoopEntryGuardedByCond(L, Cond, OrigStartMinusStride, OrigStart)) &&
         isLoopEntryGuardedByCond(L, Cond, OrigStartMinusStride, OrigRHS)) {
       // In this case, we can use a refined formula for computing backedge
       // taken count.  The general formula remains:
@@ -13555,7 +13557,7 @@ ScalarEvolution::howManyLessThans(const SCEV *LHS, const SCEV *RHS,
       // We want to use the alternate formula:
       //   "((RHS - 1) - (Start - Stride)) /u Stride"
       // Let's do a quick case analysis to show these are equivalent under
-      // our preconditions that Start - Stride is below both Start and RHS.
+      // our preconditions.
       // * For RHS <= Start (End is Start), the backedge-taken count must be
       //   zero. Together with the precondition "Start - Stride < RHS", we have
       //   "Start - Stride < RHS <= Start". Subtracting Start - Stride from
@@ -13564,9 +13566,20 @@ ScalarEvolution::howManyLessThans(const SCEV *LHS, const SCEV *RHS,
       //   So dividing that by Stride gives zero.
       //
       // * For RHS > Start (End is RHS), the backedge count must be
-      //   "RHS-Start /uceil Stride". Together with the precondition
-      //   "Start - Stride < Start", we have "RHS > Start > Start - Stride".
-      //   As such RHS - (Start - Stride) - 1 does not overflow.
+      //   "RHS-Start /uceil Stride", so it is sufficient to show that the
+      //   numerator "((RHS - 1) - (Start - Stride))" does not overflow.
+      //
+      //   If "Start - Stride < Start" holds, we have
+      //   "RHS > Start > Start - Stride". As such
+      //   "RHS - (Start - Stride) - 1" does not overflow, which reassociates to
+      //   "((RHS - 1) - (Start - Stride))".
+      //
+      //   Otherwise !IVMayOverflow guarantees
+      //   "RHS <= max unsigned value - (Stride - 1)".
+      //   With "RHS > Start" we have
+      //   "RHS - Start < RHS <= max unsigned value - (Stride - 1)".
+      //   As such "(RHS - Start) + (Stride - 1)" does not overflow, which
+      //   reassociates to "((RHS - 1) - (Start - Stride))".
       const SCEV *MinusOne = getMinusOne(Stride->getType());
       const SCEV *Numerator =
           getMinusSCEV(getAddExpr(RHS, MinusOne), getMinusSCEV(Start, Stride));
