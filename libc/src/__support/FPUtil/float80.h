56b26799c92d3e2cfb1abc0121fa1420020e7f52
Zorojuro <sawantsukumar@gmail.com>
2026-08-25 08:23:12 +0530

    nits
    Co-authored-by: OverMighty <its.overmighty@gmail.com>

--- libc/src/__support/FPUtil/float80.h
+++ libc/src/__support/FPUtil/float80.h
@@ -1,4 +1,4 @@
-//===-- Definition for Float80 data type -----------------------*- C++ -*-===//
+//===-- Definition for Float80 data type ------------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
