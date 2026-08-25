fe37eeffd60ff2941e1774b8ac2285740c735c08
Paul <paulxicao7@gmail.com>
2026-08-25 22:52:40 +0200

    nodiscard tests: use `// expected-warning@+1` style

--- libcxx/test/libcxx/numerics/c.math/sf.cmath/nodiscard.verify.cpp
+++ libcxx/test/libcxx/numerics/c.math/sf.cmath/nodiscard.verify.cpp
@@ -17,14 +17,17 @@
 #include <cmath>
 
 void test() {
-  // clang-format off
   // assoc_laguerre
-  std::assoc_laguerre(0, 0, 0.0f);  // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
-  std::assoc_laguerre(0, 0, 0.0);   // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
-  std::assoc_laguerre(0, 0, 0.0l);  // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
-  std::assoc_laguerre(0, 0, 0);     // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
-
-  std::assoc_laguerref(0, 0, 0.0f); // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
-  std::assoc_laguerrel(0, 0, 0.0l); // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
-  // clang-format on
+  // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  std::assoc_laguerre(0, 0, 0.0f);
+  // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  std::assoc_laguerre(0, 0, 0.0);
+  // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  std::assoc_laguerre(0, 0, 0.0l);
+  // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  std::assoc_laguerre(0, 0, 0);
+  // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  std::assoc_laguerref(0, 0, 0.0f);
+  // expected-warning@+1 {{ignoring return value of function declared with 'nodiscard' attribute}}
+  std::assoc_laguerrel(0, 0, 0.0l);
 }
