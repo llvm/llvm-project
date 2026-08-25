68469a7d41ede403fd1e34861d860cb809ea6baa
Vadim Kotov <kotov@google.com>
2026-08-25 13:44:17 -0700

    Use Succeed and Failed in getrusage unit tests and add a soft faults assertion.

--- libc/test/src/sys/resource/getrusage_test.cpp
+++ libc/test/src/sys/resource/getrusage_test.cpp
@@ -15,24 +15,24 @@
 #include "hdr/types/struct_rusage.h"
 #include "src/sys/resource/getrusage.h"
 #include "test/UnitTest/ErrnoCheckingTest.h"
+#include "test/UnitTest/ErrnoSetterMatcher.h"
 #include "test/UnitTest/Test.h"
 
-TEST(LlvmLibcGetrusageTest, BasicTest) {
+using namespace LIBC_NAMESPACE::testing::ErrnoSetterMatcher;
+using LlvmLibcGetrusageTest = LIBC_NAMESPACE::testing::ErrnoCheckingTest;
+
+TEST_F(LlvmLibcGetrusageTest, BasicTest) {
   struct rusage usage = {};
-  int res = LIBC_NAMESPACE::getrusage(RUSAGE_SELF, &usage);
-  EXPECT_EQ(res, 0);
-  ASSERT_ERRNO_SUCCESS();
+  ASSERT_THAT(LIBC_NAMESPACE::getrusage(RUSAGE_SELF, &usage), Succeeds());
+  // The number of soft page faults should be stably greater than 0.
+  ASSERT_TRUE(usage.ru_minflt > 0);
 }
 
-TEST(LlvmLibcGetrusageTest, TestWhoInvalid) {
+TEST_F(LlvmLibcGetrusageTest, TestWhoInvalid) {
   struct rusage usage = {};
-  int res = LIBC_NAMESPACE::getrusage(99, &usage);
-  EXPECT_EQ(res, -1);
-  ASSERT_ERRNO_EQ(EINVAL);
+  ASSERT_THAT(LIBC_NAMESPACE::getrusage(99, &usage), Fails(EINVAL, -1));
 }
 
-TEST(LlvmLibcGetrusageTest, TestUsageBadPointer) {
-  int res = LIBC_NAMESPACE::getrusage(RUSAGE_SELF, (struct rusage *)-1L);
-  EXPECT_EQ(res, -1);
-  ASSERT_ERRNO_EQ(EFAULT);
+TEST_F(LlvmLibcGetrusageTest, TestUsageBadPointer) {
+  ASSERT_THAT(LIBC_NAMESPACE::getrusage(RUSAGE_SELF, (struct rusage *)-1L), Fails(EFAULT, -1));
 }
