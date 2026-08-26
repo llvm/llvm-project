b3e990b12a4dde1d2a4c53518d9d235ca41770ec
Joseph Huber <huberjn@outlook.com>
2026-08-26 13:41:47 -0500

    [Offload] Fix self-deadlock on RPC initialization
    Summary:
    Previous change fixed memory leaks, but I neglected the fact that
    internally we call a function `initRPCDoorbell` from the AMD plugin that
    itself takes the mutex under the mutex, causing a quite obvious
    self-deadlock. Do not hold the mutex any longer than necessary.

--- offload/plugins-nextgen/common/src/RPC.cpp
+++ offload/plugins-nextgen/common/src/RPC.cpp
@@ -207,10 +207,12 @@ RPCServerTy::isDeviceUsingRPC(plugin::GenericDeviceTy &Device,
 Error RPCServerTy::initDevice(plugin::GenericDeviceTy &Device,
                               plugin::GenericGlobalHandlerTy &Handler,
                               plugin::DeviceImageTy &Image) {
-  std::lock_guard<decltype(BufferMutex)> Lock(BufferMutex);
   uint64_t NumPorts =
       std::min(Device.requestedRPCPortCount(), rpc::MAX_PORT_COUNT);
-  void *RPCBuffer = Buffers[Device.getDeviceId()];
+  {
+    std::lock_guard<decltype(BufferMutex)> Lock(BufferMutex);
+    void *RPCBuffer = Buffers[Device.getDeviceId()];
+  }
   if (!RPCBuffer) {
     auto RPCBufferOrErr = Device.allocate(
         rpc::Server::allocation_size(Device.getRPCNumLanes(), NumPorts),
@@ -236,6 +238,7 @@ Error RPCServerTy::initDevice(plugin::GenericDeviceTy &Device,
         static_cast<uint8_t *>(RPCBuffer) + rpc::Server::doorbell_offset());
     std::memcpy(DoorbellPtr, &Doorbell, sizeof(rpc::Doorbell));
 
+    std::lock_guard<decltype(BufferMutex)> Lock(BufferMutex);
     Buffers[Device.getDeviceId()] = RPCBuffer;
     Devices[Device.getDeviceId()] = &Device;
   }
