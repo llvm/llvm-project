8c8bb575baf49094fdf438fa5e412339fde5b740
Florian Hahn <flo@fhahn.com>
2026-08-26 20:22:24 +0100

    !fixup add note that the expression may gain same flags later.

--- llvm/include/llvm/Analysis/ScalarEvolution.h
+++ llvm/include/llvm/Analysis/ScalarEvolution.h
@@ -128,7 +128,8 @@ struct SCEVUseT : private PointerIntPair<SCEVPtrT, 2> {
   SCEVUseT(SCEVPtrT S) : Base(S, 0) {}
   /// Construct with NoWrapFlags; only NUW/NSW are encoded, NW is dropped. \p S
   /// must be an expression supporting flags. Only flags not already present on
-  /// \p S are added.
+  /// \p S are added. Note that the expression may gain flags also part of the
+  /// SCEVUse later, via settNoWrapFlags.
   SCEVUseT(SCEVPtrT S, SCEVNoWrapFlags Flags);
   template <typename OtherPtrT, typename = std::enable_if_t<
                                     std::is_convertible_v<OtherPtrT, SCEVPtrT>>>
