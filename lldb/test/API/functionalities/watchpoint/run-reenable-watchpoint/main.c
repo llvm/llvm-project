0f600633b365102c54ca04626399d84e69868ead
Jason Molenda <jmolenda@apple.com>
2026-08-25 18:36:35 -0700

    ws

--- lldb/test/API/functionalities/watchpoint/run-reenable-watchpoint/main.c
+++ lldb/test/API/functionalities/watchpoint/run-reenable-watchpoint/main.c
@@ -1,7 +1,6 @@
 static int arr[] = {1, 2, 0, 3, 4, 0x55555555};
-int main()
-{
-    arr[0]++; // break here
-    arr[0]++;
-    return arr[4];
+int main() {
+  arr[0]++; // break here
+  arr[0]++;
+  return arr[4];
 }
