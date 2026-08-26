17e8ac59c05e962d3880dbe3408a8874b2d8b217
Jason Molenda <jmolenda@apple.com>
2026-08-26 16:42:30 -0700

    launch the inferior the same way both times, set two separate breakpoints and confirm that the watchpoints are stopping as one of the array members is modified correctly in both process launches.

--- lldb/test/API/functionalities/watchpoint/run-reenable-watchpoint/TestRunReEnableWatchpoint.py
+++ lldb/test/API/functionalities/watchpoint/run-reenable-watchpoint/TestRunReEnableWatchpoint.py
@@ -27,12 +27,22 @@ class RunReEnableWatchpointTestCase(TestBase):
     def test_rerun_enable_watchpoint(self):
         """Test set watchpoint, re-run, re-enable wp, hit it."""
         self.build()
-        self.main_source_file = lldb.SBFileSpec("main.c")
-        li = lldb.SBLaunchInfo(None)
-        li.SetLaunchFlags(lldb.eLaunchFlagDisableASLR)
-        target, process, thread, bkpt = lldbutil.run_to_source_breakpoint(
-            self, "break here", self.main_source_file, launch_info=li
-        )
+        exe = self.getBuildArtifact("a.out")
+        target = self.dbg.CreateTarget(exe)
+        self.assertTrue(target, VALID_TARGET)
+        self.assertGreater(target.GetNumModules(), 0)
+
+        src = lldb.SBFileSpec("main.c")
+        bkpt1 = target.BreakpointCreateBySourceRegex("break here launch1", src)
+        self.assertEqual(bkpt1.GetNumLocations(), 1)
+        self.assertTrue(bkpt1.GetLocationAtIndex(0).IsEnabled())
+
+        self.runCmd("process launch --disable-aslr true")
+        process = target.GetProcess()
+        self.assertTrue(process.IsValid())
+        self.assertEqual(process.GetState(), lldb.eStateStopped)
+        thread = process.GetSelectedThread()
+        self.assertEqual(thread.GetStopReason(), lldb.eStopReasonBreakpoint)
 
         frame = thread.GetFrameAtIndex(0)
         self.runCmd("watch set variable arr")
@@ -40,12 +50,33 @@ class RunReEnableWatchpointTestCase(TestBase):
         reason = self.continue_and_report_stop_reason(process, "continue first-launch")
         self.assertEqual(reason, lldb.eStopReasonWatchpoint)
 
+        var_list = target.FindGlobalVariables("arr", 1)
+        self.assertEqual(var_list.GetSize(), 1)
+        arr = var_list.GetValueAtIndex(0)
+        self.assertEqual(arr.GetNumChildren(), 6)
+        self.assertEqual(arr.GetChildAtIndex(0).GetValueAsUnsigned(), 2)
+
+        target.DeleteAllBreakpoints()
         process.Kill()
+
+        bkpt2 = target.BreakpointCreateBySourceRegex("break here launch2", src)
+        self.assertEqual(bkpt2.GetNumLocations(), 1)
+        self.assertTrue(bkpt2.GetLocationAtIndex(0).IsEnabled())
+
         self.runCmd("process launch --disable-aslr true")
         process = target.GetProcess()
         self.assertTrue(process.IsValid())
+        self.assertEqual(process.GetState(), lldb.eStateStopped)
+        thread = process.GetSelectedThread()
+        self.assertEqual(thread.GetStopReason(), lldb.eStopReasonBreakpoint)
 
         target.EnableAllWatchpoints()
 
         reason = self.continue_and_report_stop_reason(process, "continue second-launch")
         self.assertEqual(reason, lldb.eStopReasonWatchpoint)
+
+        var_list = target.FindGlobalVariables("arr", 1)
+        self.assertEqual(var_list.GetSize(), 1)
+        arr = var_list.GetValueAtIndex(0)
+        self.assertEqual(arr.GetNumChildren(), 6)
+        self.assertEqual(arr.GetChildAtIndex(0).GetValueAsUnsigned(), 4)
--- lldb/test/API/functionalities/watchpoint/run-reenable-watchpoint/main.c
+++ lldb/test/API/functionalities/watchpoint/run-reenable-watchpoint/main.c
@@ -1,6 +1,8 @@
 static int arr[] = {1, 2, 0, 3, 4, 0x55555555};
 int main() {
-  arr[0]++; // break here
+  arr[0]++; // break here launch1
+  arr[0]++;
+  arr[0]++; // break here launch2
   arr[0]++;
   return arr[4];
 }
