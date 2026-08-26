559c14d73804dedd48e480114ab170b8ba6d70a1
Arth Srivastava <arthsrivastava1@gmail.com>
2026-08-26 21:03:00 +0530

    [Flang][OpenMP] Support DO CONCURRENT device conversion for nested derived types
    In DoConcurrentConversion, avoid aborting when a live-in derived type
    contains nested derived-type components. Use fir::isRecordWithAllocatableMember
    to determine if implicit default declare mappers are required, and safely
    handle absent mangler callbacks during recursive mapper generation.
    
    Fixes #218760

--- flang/lib/Optimizer/OpenMP/DoConcurrentConversion.cpp
+++ flang/lib/Optimizer/OpenMP/DoConcurrentConversion.cpp
@@ -596,34 +596,17 @@ private:
     llvm::SmallVector<mlir::Value> boundsOps;
     genBoundsOps(builder, liveIn, rawAddr, boundsOps);
 
-    auto asRecordType = [&](mlir::Type eleType) {
-      return mlir::dyn_cast<fir::RecordType>(
-          fir::getDerivedType(fir::unwrapRefType(eleType)));
-    };
+    fir::RecordType recordType = mlir::dyn_cast<fir::RecordType>(
+        fir::getDerivedType(fir::unwrapRefType(eleType)));
 
-    fir::RecordType recordType = asRecordType(eleType);
-
-    bool requiresImplcitMapper = [&]() {
-      if (!recordType)
-        return false;
-
-      for (auto [fieldName, fieldType] : recordType.getTypeList()) {
-        if (fir::isAllocatableType(fieldType))
-          return true;
-
-        if (asRecordType(fieldType))
-          TODO(liveIn.getLoc(), "Nested record types are not supported yet.");
-      }
-
-      return false;
-    }();
+    bool requiresImplcitMapper =
+        recordType && fir::isRecordWithAllocatableMember(recordType);
 
     mlir::FlatSymbolRefAttr mapperId;
     if (requiresImplcitMapper) {
       std::string mapperIdName =
           Fortran::utils::openmp::getCanonicalDefaultDeclareMapperName(
               recordType);
-      // TODO Add a mangler callback once nested record types are supported.
       mapperId = Fortran::utils::openmp::getOrGenImplicitDefaultDeclareMapper(
           builder, liveIn.getLoc(), recordType, mapperIdName);
     }
