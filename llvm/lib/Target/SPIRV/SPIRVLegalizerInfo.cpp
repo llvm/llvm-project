26c5fe50ca21df58ec0c2d8cccb6a8b75959dab2
Farzon Lotfi <farzonlotfi@microsoft.com>
2026-08-27 01:01:46 -0400

    the previous change impacts vec3 by promoting them to vec4s via moreElementsToNextPow2. It also has broader impact on openCL code than I would have liked. instead making an allowedScalarsAndVectors we can ST.isShader() to enforce legal shader vectors and then do spliting behaviors

--- llvm/lib/Target/SPIRV/SPIRVLegalizerInfo.cpp
+++ llvm/lib/Target/SPIRV/SPIRVLegalizerInfo.cpp
@@ -124,6 +124,13 @@ SPIRVLegalizerInfo::SPIRVLegalizerInfo(const SPIRVSubtarget &ST) {
       v4s1,  v4s8,  v4s16, v4s32, v4s64,  v8s1,   v8s8,  v8s16,
       v8s32, v8s64, v16s1, v16s8, v16s16, v16s32, v16s64};
 
+  auto allShaderScalarsAndVectors = {
+      s1,   s8,   s16,   s32,   s64,   s128, v2s1, v2s8,  v2s16, v2s32, v2s64,
+      v3s1, v3s8, v3s16, v3s32, v3s64, v4s1, v4s8, v4s16, v4s32, v4s64};
+
+  auto &allowedScalarsAndVectors =
+      ST.isShader() ? allShaderScalarsAndVectors : allScalarsAndVectors;
+
   auto allIntScalarsAndVectors = {
       s8,    s16,   s32,   s64,   s128,   v2s8,   v2s16, v2s32, v2s64,
       v3s8,  v3s16, v3s32, v3s64, v4s8,   v4s16,  v4s32, v4s64, v8s8,
@@ -360,12 +367,12 @@ SPIRVLegalizerInfo::SPIRVLegalizerInfo(const SPIRVSubtarget &ST) {
       .legalIf(ExtendedScalarsAndVectorsProduct);
 
   getActionDefinitionsBuilder({G_TRUNC, G_ZEXT, G_SEXT, G_ANYEXT})
+      .legalForCartesianProduct(allowedScalarsAndVectors)
+      .legalIf(ExtendedScalarsAndVectorsProduct)
       .moreElementsToNextPow2(0)
       .fewerElementsIf(vectorElementCountIsGreaterThan(0, MaxVectorSize),
                        LegalizeMutations::changeElementCountTo(
-                           0, ElementCount::getFixed(MaxVectorSize)))
-      .legalForCartesianProduct(allScalarsAndVectors)
-      .legalIf(ExtendedScalarsAndVectorsProduct);
+                           0, ElementCount::getFixed(MaxVectorSize)));
 
   getActionDefinitionsBuilder(G_SEXT_INREG)
       .moreElementsToNextPow2(0)
