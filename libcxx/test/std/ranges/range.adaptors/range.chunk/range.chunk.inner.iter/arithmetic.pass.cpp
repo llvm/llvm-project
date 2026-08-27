3e080c0d137ed2b9a41089cdabb74a9ceb2efff1
shyeyian <shyeyian@mail.shyeyian.com>
2026-08-27 23:04:59 +0800

    update arithmetic tests to cover both general and empty branch

--- libcxx/test/std/ranges/range.adaptors/range.chunk/range.chunk.inner.iter/arithmetic.pass.cpp
+++ libcxx/test/std/ranges/range.adaptors/range.chunk/range.chunk.inner.iter/arithmetic.pass.cpp
@@ -25,50 +25,89 @@
 #include "test_iterators.h"
 
 constexpr bool test() {
+  std::vector<int> general_vector = {1, 2, 3, 4, 5, 6, 7, 8};
+  std::vector<int> single_vector  = {1};
+
   // Test `friend constexpr difference_type operator-(default_sentinel_t, const inner_iterator& x)`
   {
-    std::vector<int> vector = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
-    std::ranges::chunk_view<
-        std::ranges::subrange<cpp17_input_iterator<int*>, sized_sentinel<cpp17_input_iterator<int*>>>>
-        chunked = std::ranges::subrange(cpp17_input_iterator<int*>(vector.data()),
-                                        sized_sentinel(cpp17_input_iterator<int*>(vector.data() + vector.size()))) |
-                  std::views::chunk(3);
+    // General
+    {
+      std::ranges::chunk_view<
+          std::ranges::subrange<cpp17_input_iterator<int*>, sized_sentinel<cpp17_input_iterator<int*>>>>
+          general_chunked =
+              std::ranges::subrange(
+                  cpp17_input_iterator<int*>(general_vector.data()),
+                  sized_sentinel(cpp17_input_iterator<int*>(general_vector.data() + general_vector.size()))) |
+              std::views::chunk(3);
 
-    static_assert(
-        std::same_as<
-            decltype(std::default_sentinel - (*chunked.begin()).begin()),
-            typename std::ranges::iterator_t<std::ranges::range_reference_t<decltype(chunked)>>::difference_type>);
-    assert(std::default_sentinel - (*chunked.begin()).begin() == 3);
-  }
+      auto outer = general_chunked.begin();
+      static_assert(std::same_as< decltype(std::default_sentinel - (*outer).begin()),
+                                  typename std::ranges::iterator_t<
+                                      std::ranges::range_reference_t<decltype(general_chunked)>>::difference_type>);
+      assert(std::default_sentinel - (*outer).begin() == 3);
+      ++outer;
+      assert(std::default_sentinel - (*outer).begin() == 3);
+      ++outer;
+      assert(std::default_sentinel - (*outer).begin() == 2);
+    }
 
-  // Test `friend constexpr difference_type operator-(default_sentinel_t, const inner_iterator& x)` when chunk is at the end
-  {
-    std::vector<int> vector = {1, 2, 3, 4, 5, 6, 7, 8};
-    std::ranges::chunk_view<
-        std::ranges::subrange<cpp17_input_iterator<int*>, sized_sentinel<cpp17_input_iterator<int*>>>>
-        chunked =
-            std::ranges::subrange<cpp17_input_iterator<int*>, sized_sentinel<cpp17_input_iterator<int*>>>(
-                cpp17_input_iterator<int*>(vector.data()),
-                sized_sentinel<cpp17_input_iterator<int*>>(cpp17_input_iterator<int*>(vector.data() + vector.size()))) |
-            std::views::chunk(3);
-    auto outer = chunked.begin();
-    ++outer;
-    ++outer;
-    assert(std::default_sentinel - (*outer).begin() == 2);
+    // Single
+    {
+      std::ranges::chunk_view<
+          std::ranges::subrange<cpp17_input_iterator<int*>, sized_sentinel<cpp17_input_iterator<int*>>>>
+          single_chunked =
+              std::ranges::subrange(
+                  cpp17_input_iterator<int*>(single_vector.data()),
+                  sized_sentinel(cpp17_input_iterator<int*>(single_vector.data() + single_vector.size()))) |
+              std::views::chunk(3);
+
+      auto outer = single_chunked.begin();
+      static_assert(std::same_as< decltype(std::default_sentinel - (*outer).begin()),
+                                  typename std::ranges::iterator_t<
+                                      std::ranges::range_reference_t<decltype(single_chunked)>>::difference_type>);
+      assert(std::default_sentinel - (*outer).begin() == 1);
+    }
   }
 
   // Test `friend constexpr difference_type operator-(const inner_iterator& x, default_sentinel_t)`
   {
-    std::vector<int> vector = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
-    std::ranges::chunk_view<
-        std::ranges::subrange<cpp17_input_iterator<int*>, sized_sentinel<cpp17_input_iterator<int*>>>>
-        chunked =
-            std::ranges::subrange<cpp17_input_iterator<int*>, sized_sentinel<cpp17_input_iterator<int*>>>(
-                cpp17_input_iterator<int*>(vector.data()),
-                sized_sentinel<cpp17_input_iterator<int*>>(cpp17_input_iterator<int*>(vector.data() + vector.size()))) |
-            std::views::chunk(3);
+    // General
+    {
+      std::ranges::chunk_view<
+          std::ranges::subrange<cpp17_input_iterator<int*>, sized_sentinel<cpp17_input_iterator<int*>>>>
+          general_chunked =
+              std::ranges::subrange(
+                  cpp17_input_iterator<int*>(general_vector.data()),
+                  sized_sentinel(cpp17_input_iterator<int*>(general_vector.data() + general_vector.size()))) |
+              std::views::chunk(3);
 
-    assert((*chunked.begin()).begin() - std::default_sentinel == -3);
+      auto outer = general_chunked.begin();
+      static_assert(std::same_as< decltype((*outer).begin() - std::default_sentinel),
+                                  typename std::ranges::iterator_t<
+                                      std::ranges::range_reference_t<decltype(general_chunked)>>::difference_type>);
+      assert((*outer).begin() - std::default_sentinel == -3);
+      ++outer;
+      assert((*outer).begin() - std::default_sentinel == -3);
+      ++outer;
+      assert((*outer).begin() - std::default_sentinel == -2);
+    }
+
+    // Single
+    {
+      std::ranges::chunk_view<
+          std::ranges::subrange<cpp17_input_iterator<int*>, sized_sentinel<cpp17_input_iterator<int*>>>>
+          single_chunked =
+              std::ranges::subrange(
+                  cpp17_input_iterator<int*>(single_vector.data()),
+                  sized_sentinel(cpp17_input_iterator<int*>(single_vector.data() + single_vector.size()))) |
+              std::views::chunk(3);
+
+      auto outer = single_chunked.begin();
+      static_assert(std::same_as< decltype((*outer).begin() - std::default_sentinel),
+                                  typename std::ranges::iterator_t<
+                                      std::ranges::range_reference_t<decltype(single_chunked)>>::difference_type>);
+      assert((*outer).begin() - std::default_sentinel == -1);
+    }
   }
 
   return true;
