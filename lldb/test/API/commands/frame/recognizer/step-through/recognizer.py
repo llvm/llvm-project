6953897fbc69cad61051b578a23c5405ddf5f900
Jim Ingham <jingham@apple.com>
2026-08-27 10:24:01 -0700

    Try some remote debugging of the Windows failure.

--- lldb/test/API/commands/frame/recognizer/step-through/recognizer.py
+++ lldb/test/API/commands/frame/recognizer/step-through/recognizer.py
@@ -11,17 +11,20 @@ class NestedFrameRecognizer(ScriptedStackFrameRecognizer):
     def get_step_through_plan(self, thread):
         """Step through from baz to bar"""
         frame = thread.frames[0]
-
+        print(f"Asked to step through at {frame.name}")
         if frame.name.startswith("baz"):
             target = thread.process.target
             bar_funcs = target.FindFunctions("bar", lldb.eFunctionNameTypeFull)
             if bar_funcs.GetSize() == 0:
+                print("Found no functions matching bar")
                 return None
             if bar_funcs.GetSize() != 1:
+                print("Found more than one function matching bar")
                 return None
             bar_func = bar_funcs.functions[0]
             address = bar_func.addr
             if not address.IsValid():
+                print("Didn't get a valid address for bar")
                 return None
 
             load_addr = address.GetLoadAddress(target)
@@ -44,6 +47,7 @@ class StepThrough(ScriptedThreadPlan):
 
         addr_val = extra_args.GetValueForKey("address")
         if not addr_val.IsValid():
+            print("Missing addr_val key")
             thread_plan.SetPlanComplete(False)
             return
 
@@ -52,17 +56,20 @@ class StepThrough(ScriptedThreadPlan):
         addr_str = addr_val.GetStringValue(32)
         addr_int = int(addr_str)
         if addr_int == 0:
+            print("Got zero value for addr_int")
             thread_plan.SetPlanComplete(False)
             return
 
         address = lldb.SBAddress(addr_int, target)
         if not address.IsValid():
+            print("Got invalid value for address")
             thread_plan.SetPlanComplete(False)
             return
 
         error = lldb.SBError()
         self.addr_plan = thread_plan.QueueThreadPlanForRunToAddress(address, error)
         if error.Fail():
+            print(f"Couldn't queue run to address plan: {error.description}")
             thread_plan.SetPlanComplete(False)
             return
 
