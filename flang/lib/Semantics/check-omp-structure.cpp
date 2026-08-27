eeb9bb7473d368ac1acb52abe9fb67983f0cc63b
Krzysztof Parzyszek <Krzysztof.Parzyszek@amd.com>
2026-08-27 12:13:58 -0500

    [flang][OpenMP] Replace modifier verification with a generic one
    Implement verification of syntactic properties (i.e. required, unique,
    exclusive, ultimate) in a generic way, agnostic of the kind of element
    to which these properties are applied. The goal here is to reuse it
    for verifying clause properties in the future.
    
    Refactor the existing modifier verification code to use it. Modify
    the previous implementation (OmpVerifyModifiers) to always succeed
    to reduce the amount of necessary changes.

--- flang/lib/Semantics/check-omp-structure.cpp
+++ flang/lib/Semantics/check-omp-structure.cpp
@@ -2157,20 +2157,21 @@ void OmpStructureChecker::CheckInitOnDepobj(
           OmpGetUniqueModifier<parser::OmpDepinfoModifier>(modifiers)}) {
     auto depKind{std::get<common::OmpDependenceKind>(depInfo->t)};
     if (depKind == common::OmpDependenceKind::Depobj) {
-      auto &desc{OmpGetDescriptor<parser::OmpDepinfoModifier>()};
+      auto &desc{
+          llvm::omp::getDescriptor(llvm::omp::Modifier::DepinfoModifier)};
       context_.Say(OmpGetModifierSource(modifiers, depInfo),
           "'%s' is not an allowed value of the '%s' modifier"_err_en_US,
           parser::ToUpperCaseLetters(EnumToString(depKind)),
           desc.getName().str());
     }
   } else {
-    auto &desc{OmpGetDescriptor<parser::OmpDepinfoModifier>()};
+    auto &desc{llvm::omp::getDescriptor(llvm::omp::Modifier::DepinfoModifier)};
     context_.Say(initClause.source,
         "The '%s' modifier is required on a DEPOBJ construct"_err_en_US,
         desc.getName().str());
   }
   if (auto *prefType{OmpGetUniqueModifier<parser::OmpPreferType>(modifiers)}) {
-    auto &desc{OmpGetDescriptor<parser::OmpPreferType>()};
+    auto &desc{llvm::omp::getDescriptor(llvm::omp::Modifier::PreferType)};
     context_.Say(OmpGetModifierSource(modifiers, prefType),
         "The '%s' modifier is not allowed on a DEPOBJ construct"_err_en_US,
         desc.getName().str());
@@ -2947,47 +2948,14 @@ void OmpStructureChecker::ChecksOnOrderedAsStandalone() {
     context_.Say(GetContext().clauseSource,
         "THREADS and SIMD clauses are not allowed when ORDERED construct is a standalone construct with no ORDERED region"_err_en_US);
   }
-
-  int dependSinkCount{0}, dependSourceCount{0};
-  bool exclusiveShown{false}, duplicateSourceShown{false};
-
-  auto visitDoacross{[&](const parser::OmpDoacross &doa,
-                         const parser::CharBlock &src) {
-    // Modifiers should have been verified by now.
-    auto &modifiers{OmpGetModifiers(doa)};
-    if (auto *source{
-            OmpGetUniqueModifier<parser::OmpDependenceType>(modifiers)}) {
-      if (source->v == parser::OmpDependenceType::Value::Source) {
-        ++dependSourceCount;
-      } else {
-        ++dependSinkCount;
-      }
-    }
-    if (!exclusiveShown && dependSinkCount > 0 && dependSourceCount > 0) {
-      exclusiveShown = true;
-      context_.Say(src,
-          "The SINK and SOURCE dependence types are mutually exclusive"_err_en_US);
-    }
-    if (!duplicateSourceShown && dependSourceCount > 1) {
-      duplicateSourceShown = true;
-      context_.Say(src,
-          "At most one SOURCE dependence type can appear on ORDERED directive"_err_en_US);
-    }
-  }};
-
   // Visit the DEPEND and DOACROSS clauses.
   for (auto [_, clause] : FindClauses(llvm::omp::Clause::OMPC_depend)) {
     const auto &dependClause{std::get<parser::OmpClause::Depend>(clause->u)};
-    if (auto *doAcross{std::get_if<parser::OmpDoacross>(&dependClause.v.u)}) {
-      visitDoacross(*doAcross, clause->source);
-    } else {
+    using TaskDep = parser::OmpDependClause::TaskDep;
+    if (std::holds_alternative<TaskDep>(dependClause.v.u)) {
       context_.Say(clause->source,
           "Only SINK or SOURCE dependence types are allowed when ORDERED construct is a standalone construct with no ORDERED region"_err_en_US);
     }
-  }
-  for (auto [_, clause] : FindClauses(llvm::omp::Clause::OMPC_doacross)) {
-    auto &doaClause{std::get<parser::OmpClause::Doacross>(clause->u)};
-    visitDoacross(doaClause.v.v, clause->source);
   }
 
   bool isNestedInDoOrderedWithPara{false};
@@ -3904,6 +3872,7 @@ void OmpStructureChecker::Leave(const parser::OmpClauseList &x) {
 void OmpStructureChecker::Enter(const parser::OmpClause &x) {
   SetContextClause(x);
   CheckArgumentObjectKind(x);
+  VerifyModifiers(x);
 }
 
 // Restrictions specific to each clause are implemented apart from the
@@ -4669,7 +4638,8 @@ void OmpStructureChecker::Enter(const parser::OmpClause::If &x) {
     std::string dirName{parser::omp::GetUpperName(dir, version)};
 
     parser::CharBlock modifierSource{OmpGetModifierSource(modifiers, dnm)};
-    auto desc{OmpGetDescriptor<parser::OmpDirectiveNameModifier>()};
+    auto desc{
+        llvm::omp::getDescriptor(llvm::omp::Modifier::DirectiveNameModifier)};
     std::string modName{desc.getName().str()};
 
     if (!isConstituent(dir, sub)) {
@@ -4894,7 +4864,8 @@ void OmpStructureChecker::Enter(const parser::OmpClause::Map &x) {
         llvm::is_contained(leafs, Directive::OMPD_declare_mapper)};
 
     if (!mapEnteringConstructOrMapper || !IsMapEnteringType(mapType)) {
-      const auto &desc{OmpGetDescriptor<parser::OmpAttachModifier>()};
+      const auto &desc{
+          llvm::omp::getDescriptor(llvm::omp::Modifier::AttachModifier)};
       context_.Say(OmpGetModifierSource(modifiers, attach),
           "The '%s' modifier can only appear on a map-entering construct or on a DECLARE_MAPPER directive"_err_en_US,
           desc.getName().str());
@@ -5053,7 +5024,8 @@ void OmpStructureChecker::Enter(const parser::OmpClause::Device &x) {
             OmpGetUniqueModifier<parser::OmpDeviceModifier>(modifiers)}) {
       using Value = parser::OmpDeviceModifier::Value;
       if (dir != llvm::omp::OMPD_target && deviceMod->v == Value::Ancestor) {
-        auto name{OmpGetDescriptor<parser::OmpDeviceModifier>().getName()};
+        auto name{llvm::omp::getDescriptor(llvm::omp::Modifier::DeviceModifier)
+                .getName()};
         context_.Say(OmpGetModifierSource(modifiers, deviceMod),
             "The ANCESTOR %s must not appear on the DEVICE clause on any directive other than the TARGET construct. Found on %s construct."_err_en_US,
             name.str(), parser::omp::GetUpperName(dir, version));
@@ -5721,7 +5693,8 @@ void OmpStructureChecker::CheckUsesAllocatorsSpec(
     bool ok{
         memSpaceName && IsUsesAllocatorsMemSpaceName(*memSpaceName, version)};
     if (!ok) {
-      auto name{OmpGetDescriptor<parser::OmpMemSpace>().getName()};
+      auto name{
+          llvm::omp::getDescriptor(llvm::omp::Modifier::MemSpace).getName()};
       context_.Say(memSpaceSource,
           "The '%s' modifier must name a predefined memory space"_err_en_US,
           name.str());
@@ -6410,7 +6383,8 @@ void OmpStructureChecker::Enter(const parser::OmpClause::SelfMaps &x) {
 
 void OmpStructureChecker::CheckDimsModifier(parser::CharBlock source,
     size_t numValues, const parser::OmpDimsModifier &x) {
-  std::string name{OmpGetDescriptor<parser::OmpDimsModifier>().getName().str()};
+  auto &desc{llvm::omp::getDescriptor(llvm::omp::Modifier::DimsModifier)};
+  std::string name{desc.getName().str()};
 
   if (auto dimsVal{GetIntValue(x.v)}) {
     if (*dimsVal > 0) {
@@ -6585,7 +6559,8 @@ void OmpStructureChecker::Enter(const parser::OpenMPInteropConstruct &x) {
                 if (auto *depInfo{
                         OmpGetUniqueModifier<parser::OmpDepinfoModifier>(
                             modifiers)}) {
-                  auto &desc{OmpGetDescriptor<parser::OmpDepinfoModifier>()};
+                  auto &desc{llvm::omp::getDescriptor(
+                      llvm::omp::Modifier::DepinfoModifier)};
                   context_.Say(OmpGetModifierSource(modifiers, depInfo),
                       "The '%s' is not allowed on INTEROP construct"_err_en_US,
                       desc.getName().str());
