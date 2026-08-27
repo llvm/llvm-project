184203a695d638f8bf051167a2ed2c99e47fd482
Jim Ingham <jingham@apple.com>
2026-08-27 11:28:00 -0700

    Formatting

--- lldb/test/API/commands/frame/recognizer/step-through/TestFrameRecognizerStepThrough.py
+++ lldb/test/API/commands/frame/recognizer/step-through/TestFrameRecognizerStepThrough.py
@@ -13,7 +13,7 @@ import recognizer
 
 class TestFrameRecognizerStepThrough(TestBase):
     NO_DEBUG_INFO_TESTCASE = True
-    
+
     @skipIfWindows
     def test_frame_recognizer_step_through(self):
         """Test that the step through recognizer works"""
