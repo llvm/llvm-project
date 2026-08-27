llvm/llvm-project

commit ef9085f5bdb52b27258c150bd7e1fd812fc406c8

lldb: Linux: empty Ptrace.h pollyfill
As the comment in sources alludes to:

> System includes - They have to be included after framework includes because
> they define some macros which collide with variable names in other modules.

Mixing includes of sys/ptrace.h with other headers can lead to conflicts
that cause i386 lldb-server builds to fail when build against bionic.

Prepare to remove this header outright.

Link: https://github.com/llvm/llvm-project/pull/217435
Link: https://github.com/llvm/llvm-project/issues/217413
Link: https://lore.kernel.org/llvm/20260821-ptrace_uapi-v1-1-3de8638a29f2@google.com/

lldb/source/Plugins/Process/Linux/NativeProcessLinux.cpp
@@ -63,6 +63,22 @@
 #define HWCAP2_MTE (1 << 18)
 #endif
 
+#ifndef PTRACE_SETREGS
+#define PTRACE_SETREGS 13
+#endif
+
+#ifndef PTRACE_SETFPREGS
+#define PTRACE_SETFPREGS 15
+#endif
+
+#ifndef PTRACE_GETREGSET
+#define PTRACE_GETREGSET 0x4204
+#endif
+
+#ifndef PTRACE_SETREGSET
+#define PTRACE_SETREGSET 0x4205
+#endif
+
 using namespace lldb;
 using namespace lldb_private;
 using namespace lldb_private::process_linux;
@@ -131,7 +147,8 @@ static void MaybeLogLaunchInfo(const ProcessLaunchInfo &info) {
 
 static void DisplayBytes(StreamString &s, void *bytes, uint32_t count) {
   uint8_t *ptr = (uint8_t *)bytes;
-  const uint32_t loop_count = std::min<uint32_t>(DEBUG_PTRACE_MAXBYTES, count);
+  constexpr uint32_t kDebugPTraceMaxBytes = 20;
+  const uint32_t loop_count = std::min<uint32_t>(kDebugPTraceMaxBytes, count);
   for (uint32_t i = 0; i < loop_count; i++) {
     s.Printf("[%x]", *ptr);
     ptr++;

lldb/include/lldb/Host/linux/Ptrace.h
@@ -13,55 +13,6 @@
 
 #include <sys/ptrace.h>
 
-#ifndef __GLIBC__
-typedef int __ptrace_request;
-#endif
-
-#define DEBUG_PTRACE_MAXBYTES 20
-
 // Support ptrace extensions even when compiled without required kernel support
-#ifndef PTRACE_GETREGS
-#define PTRACE_GETREGS 12
-#endif
-#ifndef PTRACE_SETREGS
-#define PTRACE_SETREGS 13
-#endif
-#ifndef PTRACE_GETFPREGS
-#define PTRACE_GETFPREGS 14
-#endif
-#ifndef PTRACE_SETFPREGS
-#define PTRACE_SETFPREGS 15
-#endif
-#ifndef PTRACE_GETREGSET
-#define PTRACE_GETREGSET 0x4204
-#endif
-#ifndef PTRACE_SETREGSET
-#define PTRACE_SETREGSET 0x4205
-#endif
-
-#ifndef PTRACE_GET_THREAD_AREA
-#ifdef __arm__
-// Arm has a different value, see arch/arm/include/uapi/asm/ptrace.h.
-#define PTRACE_GET_THREAD_AREA 22
-#else
-#define PTRACE_GET_THREAD_AREA 25
-#endif // __arm__
-#endif // PTRACE_GET_THREAD_AREA
-
-#ifndef PTRACE_ARCH_PRCTL
-#define PTRACE_ARCH_PRCTL 30
-#endif
-#ifndef ARCH_GET_FS
-#define ARCH_SET_GS 0x1001
-#define ARCH_SET_FS 0x1002
-#define ARCH_GET_FS 0x1003
-#define ARCH_GET_GS 0x1004
-#endif
-#ifndef PTRACE_PEEKMTETAGS
-#define PTRACE_PEEKMTETAGS 33
-#endif
-#ifndef PTRACE_POKEMTETAGS
-#define PTRACE_POKEMTETAGS 34
-#endif
 
 #endif // liblldb_Host_linux_Ptrace_h_

lldb/source/Plugins/Process/Linux/NativeRegisterContextLinux.cpp
@@ -15,7 +15,32 @@
 #include "lldb/Host/common/NativeThreadProtocol.h"
 #include "lldb/Host/linux/Ptrace.h"
 #include "lldb/Utility/RegisterValue.h"
+
 #include <sys/uio.h>
+
+#ifndef PTRACE_GETREGS
+#define PTRACE_GETREGS 12
+#endif
+
+#ifndef PTRACE_SETREGS
+#define PTRACE_SETREGS 13
+#endif
+
+#ifndef PTRACE_GETFPREGS
+#define PTRACE_GETFPREGS 14
+#endif
+
+#ifndef PTRACE_SETFPREGS
+#define PTRACE_SETFPREGS 15
+#endif
+
+#ifndef PTRACE_GETREGSET
+#define PTRACE_GETREGSET 0x4204
+#endif
+
+#ifndef PTRACE_SETREGSET
+#define PTRACE_SETREGSET 0x4205
+#endif
 
 using namespace lldb_private;
 using namespace lldb_private::process_linux;

lldb/source/Plugins/Process/Linux/NativeRegisterContextLinux_arm.cpp
@@ -46,6 +46,10 @@
 #define PTRACE_TYPE_ARG4 void *
 #endif
 
+#ifndef PTRACE_GET_THREAD_AREA
+#define PTRACE_GET_THREAD_AREA 22
+#endif
+
 using namespace lldb;
 using namespace lldb_private;
 using namespace lldb_private::process_linux;

lldb/source/Plugins/Process/Linux/NativeRegisterContextLinux_arm64.cpp
@@ -53,6 +53,18 @@
 #define HWCAP2_POE (1ULL << 63)
 #endif
 
+#ifndef PTRACE_GETREGSET
+#define PTRACE_GETREGSET 0x4204
+#endif
+
+#ifndef PTRACE_PEEKMTETAGS
+#define PTRACE_PEEKMTETAGS 33
+#endif
+
+#ifndef PTRACE_POKEMTETAGS
+#define PTRACE_POKEMTETAGS 34
+#endif
+
 using namespace lldb;
 using namespace lldb_private;
 using namespace lldb_private::process_linux;

lldb/source/Plugins/Process/Linux/NativeRegisterContextLinux_arm64dbreg.cpp
@@ -17,6 +17,14 @@
 #include <elf.h>
 #include <sys/uio.h>
 
+#ifndef PTRACE_GETREGSET
+#define PTRACE_GETREGSET 0x4204
+#endif
+
+#ifndef PTRACE_SETREGSET
+#define PTRACE_SETREGSET 0x4205
+#endif
+
 using namespace lldb;
 using namespace lldb_private;
 using namespace lldb_private::process_linux;

lldb/source/Plugins/Process/Linux/NativeRegisterContextLinux_loongarch64.cpp
@@ -26,6 +26,14 @@
 // struct iovec definition
 #include <sys/uio.h>
 
+#ifndef PTRACE_GETREGSET
+#define PTRACE_GETREGSET 0x4204
+#endif
+
+#ifndef PTRACE_SETREGSET
+#define PTRACE_SETREGSET 0x4205
+#endif
+
 // LoongArch SIMD eXtension registers
 #ifndef NT_LOONGARCH_LSX
 #define NT_LOONGARCH_LSX 0xa02

lldb/source/Plugins/Process/Linux/NativeRegisterContextLinux_riscv64.cpp
@@ -26,6 +26,10 @@
 #include <sys/ptrace.h>
 #include <sys/uio.h>
 
+#ifndef PTRACE_GETREGSET
+#define PTRACE_GETREGSET 0x4204
+#endif
+
 using namespace lldb;
 using namespace lldb_private;
 using namespace lldb_private::process_linux;

commit 02244e78fe298f3b0527dc28b72c4a966c989182

lldb: Linux: restore __ptrace_request typedef for bionic+musl
glibc declares ptrace as taking an enum __ptrace_request as its first
argument. Because C++ does not allow implicit conversion from int to an
enumeration type, callers must cast int arguments to __ptrace_request
(e.g., static_cast<__ptrace_request>(req)).

However, bionic and musl declare ptrace as taking an int and do not
define __ptrace_request. Providing a fallback typedef int __ptrace_request
allows the static_cast to compile across all three libcs.

I removed this typedef by accident in ef9085f5bdb52b27258c150bd7e1fd812fc406c8.

Fixes: commit ef9085f5bdb5 ("lldb: Linux: empty Ptrace.h pollyfill")
Link: https://github.com/llvm/llvm-project/issues/217413

lldb/source/Plugins/Process/Linux/NativeProcessLinux.cpp
@@ -82,6 +82,13 @@
 #define PTRACE_SETREGSET 0x4205
 #endif
 
+// glibc declares ptrace taking enum __ptrace_request (which requires a cast in
+// C++), whereas bionic and musl declare it taking int and do not define
+// __ptrace_request.
+#ifndef __GLIBC__
+typedef int __ptrace_request;
+#endif
+
 using namespace lldb;
 using namespace lldb_private;
 using namespace lldb_private::process_linux;