38473537985656d78390cbcea44d917901e0233e
Benjamin Maxwell <benjamin.maxwell@arm.com>
2026-08-27 14:08:56 +0000

    Fixups

--- llvm/lib/Target/AArch64/AArch64ISelLowering.cpp
+++ llvm/lib/Target/AArch64/AArch64ISelLowering.cpp
@@ -1654,10 +1654,9 @@ AArch64TargetLowering::AArch64TargetLowering(const TargetMachine &TM,
       for (MVT VT : {MVT::nxv16i1, MVT::nxv8i1})
         setOperationAction(ISD::VECTOR_MATCH, VT, Custom);
 
-      for (MVT VT : {MVT::v16i1, MVT::v8i1, MVT::v16i8, MVT::v8i8})
+      for (MVT VT :
+           {MVT::v16i1, MVT::v8i1, MVT::v16i8, MVT::v8i8, MVT::v3i8, MVT::v4i8})
         setOperationAction(ISD::VECTOR_MATCH, VT, Custom);
-
-      setTargetDAGCombine(ISD::VECTOR_MATCH);
     }
 
     setOperationAction(ISD::GET_ACTIVE_LANE_MASK, MVT::nxv1i1, Custom);
@@ -6514,6 +6513,31 @@ static SDValue LowerVectorMatch(SDValue Op, SelectionDAG &DAG) {
   EVT Op1VT = Op1.getValueType();
   EVT Op2VT = Op2.getValueType();
   EVT ResVT = Op.getValueType();
+
+  if ((Op2VT == MVT::v3i8 || Op2VT == MVT::v4i8)) {
+    SDValue Needle = Op2;
+    EVT NeedleVT = Op2VT;
+
+    if (NeedleVT == MVT::v3i8) {
+      // Pad a v3i8 needle to v4i8.
+      SDValue Pad = DAG.getExtractVectorElt(DL, MVT::i8, Needle, 0);
+      Needle = DAG.getInsertSubvector(DL, DAG.getPOISON(MVT::v4i8), Needle, 0);
+      Needle = DAG.getInsertVectorElt(DL, Needle, Pad, 3);
+    }
+
+    // Extend the v4i8 needle to v16i8.
+    Needle = DAG.getBitcast(MVT::v1i32, Needle);
+    Needle = DAG.getExtractVectorElt(DL, MVT::i32, Needle, 0);
+    Needle = DAG.getSplatVector(MVT::v4i32, DL, Needle);
+    Needle = DAG.getBitcast(MVT::v16i8, Needle);
+
+    return DAG.getNode(ISD::VECTOR_MATCH, DL, Op.getValueType(), Op1, Needle,
+                       Mask);
+  }
+
+  if (!DAG.getTargetLoweringInfo().isTypeLegal(ResVT) ||
+      !DAG.getTargetLoweringInfo().isTypeLegal(Op1VT))
+    return SDValue();
 
   assert((Op1VT.getVectorElementType() == MVT::i8 ||
           Op1VT.getVectorElementType() == MVT::i16) &&
@@ -31457,40 +31481,6 @@ static SDValue performPredicateLoadCombine(SDNode *N,
   return LoadPred;
 }
 
-static SDValue performVectorMatchCombine(SDNode *N,
-                                         TargetLowering::DAGCombinerInfo &DCI,
-                                         SelectionDAG &DAG) {
-  // Widen v3i8/v4i8 match needles to v8i8. For needles >= 2 elements it's
-  // generally better to widen the needle and lower to a `match` (rather than
-  // expanding). This is handled by type legalization for all other types (> 2
-  // elements) but v4i8 is promoted rather than widened, hence this combine.
-  SDValue Needle = N->getOperand(1);
-  EVT NeedleVT = Needle.getValueType();
-
-  if (!DCI.isBeforeLegalize() ||
-      (NeedleVT != MVT::v3i8 && NeedleVT != MVT::v4i8))
-    return SDValue();
-
-  SDLoc DL(N);
-  if (NeedleVT == MVT::v3i8) {
-    // Pad a v3i8 needle to v4i8.
-    SDValue Pad = DAG.getExtractVectorElt(DL, MVT::i8, Needle, 0);
-    Needle = DAG.getInsertSubvector(DL, DAG.getPOISON(MVT::v4i8), Needle, 0);
-    Needle = DAG.getInsertVectorElt(DL, Needle, Pad, 3);
-  }
-
-  // Splat the needle to a full scalable vector (in such a way that the bitcasts
-  // and extracts should fold away).
-  Needle = DAG.getBitcast(MVT::v1i32, Needle);
-  Needle = DAG.getExtractVectorElt(DL, MVT::i32, Needle, 0);
-  Needle = DAG.getSplatVector(MVT::nxv4i32, DL, Needle);
-  Needle = DAG.getBitcast(MVT::nxv16i8, Needle);
-  Needle = DAG.getExtractSubvector(DL, MVT::v16i8, Needle, 0);
-
-  return DAG.getNode(ISD::VECTOR_MATCH, DL, N->getValueType(0),
-                     N->getOperand(0), Needle, N->getOperand(2));
-}
-
 SDValue AArch64TargetLowering::PerformDAGCombine(SDNode *N,
                                                  DAGCombinerInfo &DCI) const {
   SelectionDAG &DAG = DCI.DAG;
@@ -31854,8 +31844,6 @@ SDValue AArch64TargetLowering::PerformDAGCombine(SDNode *N,
     return performCTPOPCombine(N, DCI, DAG);
   case ISD::BITCAST:
     return performPredicateLoadCombine(N, DCI, DAG);
-  case ISD::VECTOR_MATCH:
-    return performVectorMatchCombine(N, DCI, DAG);
   }
   return SDValue();
 }
--- llvm/test/CodeGen/AArch64/intrinsic-vector-match-sve2.ll
+++ llvm/test/CodeGen/AArch64/intrinsic-vector-match-sve2.ll
@@ -450,4 +450,69 @@ define <2 x i1> @match_v2xi64_v2i64(<2 x i64> %op1, <2 x i64> %op2, <2 x i1> %ma
   ret <2 x i1> %r
 }
 
+; Ensure we don't try to custom lower nodes with v3i8 source operands.
+; TODO: It may be worth extending the source operand so this case can use match.
+define <3 x i1> @match_v3i8_v3i1(<3 x i8> %op1, <8 x i8> %op2, <3 x i1> %mask) #0 {
+; CHECK-LABEL: match_v3i8_v3i1:
+; CHECK:       // %bb.0:
+; CHECK-NEXT:    fmov s1, w0
+; CHECK-NEXT:    // kill: def $d0 killed $d0 def $q0
+; CHECK-NEXT:    umov w8, v0.b[1]
+; CHECK-NEXT:    umov w9, v0.b[0]
+; CHECK-NEXT:    umov w10, v0.b[2]
+; CHECK-NEXT:    umov w11, v0.b[3]
+; CHECK-NEXT:    umov w12, v0.b[4]
+; CHECK-NEXT:    umov w13, v0.b[5]
+; CHECK-NEXT:    mov v1.h[1], w1
+; CHECK-NEXT:    dup v2.4h, w8
+; CHECK-NEXT:    umov w8, v0.b[6]
+; CHECK-NEXT:    dup v3.4h, w9
+; CHECK-NEXT:    dup v4.4h, w10
+; CHECK-NEXT:    dup v5.4h, w11
+; CHECK-NEXT:    dup v6.4h, w12
+; CHECK-NEXT:    dup v7.4h, w13
+; CHECK-NEXT:    mov v1.h[2], w2
+; CHECK-NEXT:    dup v16.4h, w8
+; CHECK-NEXT:    bic v2.4h, #255, lsl #8
+; CHECK-NEXT:    bic v3.4h, #255, lsl #8
+; CHECK-NEXT:    bic v4.4h, #255, lsl #8
+; CHECK-NEXT:    bic v5.4h, #255, lsl #8
+; CHECK-NEXT:    bic v6.4h, #255, lsl #8
+; CHECK-NEXT:    bic v7.4h, #255, lsl #8
+; CHECK-NEXT:    umov w8, v0.b[7]
+; CHECK-NEXT:    bic v1.4h, #255, lsl #8
+; CHECK-NEXT:    bic v16.4h, #255, lsl #8
+; CHECK-NEXT:    cmeq v0.4h, v1.4h, v2.4h
+; CHECK-NEXT:    cmeq v2.4h, v1.4h, v3.4h
+; CHECK-NEXT:    cmeq v3.4h, v1.4h, v4.4h
+; CHECK-NEXT:    cmeq v4.4h, v1.4h, v5.4h
+; CHECK-NEXT:    cmeq v5.4h, v1.4h, v6.4h
+; CHECK-NEXT:    cmeq v6.4h, v1.4h, v7.4h
+; CHECK-NEXT:    orr v0.8b, v2.8b, v0.8b
+; CHECK-NEXT:    orr v2.8b, v3.8b, v4.8b
+; CHECK-NEXT:    orr v4.8b, v5.8b, v6.8b
+; CHECK-NEXT:    cmeq v5.4h, v1.4h, v16.4h
+; CHECK-NEXT:    dup v3.4h, w8
+; CHECK-NEXT:    orr v0.8b, v0.8b, v2.8b
+; CHECK-NEXT:    orr v2.8b, v4.8b, v5.8b
+; CHECK-NEXT:    fmov s4, w3
+; CHECK-NEXT:    bic v3.4h, #255, lsl #8
+; CHECK-NEXT:    mov v4.h[1], w4
+; CHECK-NEXT:    orr v0.8b, v0.8b, v2.8b
+; CHECK-NEXT:    cmeq v1.4h, v1.4h, v3.4h
+; CHECK-NEXT:    mov v4.h[2], w5
+; CHECK-NEXT:    orr v0.8b, v0.8b, v1.8b
+; CHECK-NEXT:    shl v0.4h, v0.4h, #8
+; CHECK-NEXT:    shl v1.4h, v4.4h, #15
+; CHECK-NEXT:    sshr v0.4h, v0.4h, #8
+; CHECK-NEXT:    cmlt v1.4h, v1.4h, #0
+; CHECK-NEXT:    and v0.8b, v0.8b, v1.8b
+; CHECK-NEXT:    umov w0, v0.h[0]
+; CHECK-NEXT:    umov w1, v0.h[1]
+; CHECK-NEXT:    umov w2, v0.h[2]
+; CHECK-NEXT:    ret
+  %r = tail call <3 x i1> @llvm.experimental.vector.match(<3 x i8> %op1, <8 x i8> %op2, <3 x i1> %mask)
+  ret <3 x i1> %r
+}
+
 attributes #0 = { "target-features"="+sve2" }
