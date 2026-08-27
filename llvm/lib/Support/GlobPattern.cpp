9a37bbe662cb3cef0196aacfb0ac0af4e5e0d01b
Peter Rong <peterrong96@gmail.com>
2026-08-27 10:18:31 -0700

    Update llvm/lib/Support/GlobPattern.cpp
    Co-authored-by: Ellis Hoag <ellis.sparky.hoag@gmail.com>

--- llvm/lib/Support/GlobPattern.cpp
+++ llvm/lib/Support/GlobPattern.cpp
@@ -231,7 +231,7 @@ GlobPattern::asLiteral(SmallVectorImpl<char> &Storage) const {
   // this pattern denotes a single string is decided by the sub-patterns, which
   // recorded it while parsing. No sub-pattern at all means there was no
   // metacharacter; more than one means brace expansion produced a choice.
-  if (!SubGlobs.empty() && !(SubGlobs.size() == 1 && SubGlobs[0].isLiteral()))
+  if (SubGlobs.size() > 1 || (SubGlobs.size() == 1 && !SubGlobs[0].isLiteral()))
     return std::nullopt;
 
   StringRef Literal = Pattern;
