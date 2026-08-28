2fccfd2a481d5d19734578f3f8165a682540e133
Hristo Hristov <hghristov.rmm@gmail.com>
2026-08-28 07:37:58 +0300

    Add `[[nodiscard]]`

--- libcxx/test/std/language.support/support.limits/support.limits.general/debugging.version.compile.pass.cpp
+++ libcxx/test/std/language.support/support.limits/support.limits.general/debugging.version.compile.pass.cpp
@@ -48,7 +48,7 @@
 #    error "__cpp_lib_debugging should not be defined before c++26"
 #  endif
 
-#elif TEST_STD_VER > 23
+#elif TEST_STD_VER == 26
 
 #  if !defined(_LIBCPP_VERSION) || _LIBCPP_AVAILABILITY_HAS_DEBUGGING
 #    ifndef __cpp_lib_debugging
@@ -63,6 +63,21 @@
 #    endif
 #  endif
 
-#endif // TEST_STD_VER > 23
+#elif TEST_STD_VER > 26
+
+#  if !defined(_LIBCPP_VERSION) || _LIBCPP_AVAILABILITY_HAS_DEBUGGING
+#    ifndef __cpp_lib_debugging
+#      error "__cpp_lib_debugging should be defined in c++29"
+#    endif
+#    if __cpp_lib_debugging != 202403L
+#      error "__cpp_lib_debugging should have the value 202403L in c++29"
+#    endif
+#  else
+#    ifdef __cpp_lib_debugging
+#      error "__cpp_lib_debugging should not be defined when the requirement '!defined(_LIBCPP_VERSION) || _LIBCPP_AVAILABILITY_HAS_DEBUGGING' is not met!"
+#    endif
+#  endif
+
+#endif // TEST_STD_VER > 26
 
 // clang-format on
