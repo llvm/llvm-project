51c6668c7841388b9aba808ca32ea015ffb3716d
Kaviya Rajendiran <kaviyara2000@gmail.com>
2026-08-28 14:39:50 +0530

    [Flang][Test] Simplify FileCheck patterns in tetscase

--- flang/test/Driver/fno-optimize-sibling-calls.f90
+++ flang/test/Driver/fno-optimize-sibling-calls.f90
@@ -1,13 +1,11 @@
 ! Test -f[no-]optimize-sibling-calls driver forwarding to flang -fc1.
 
-! RUN: %flang -### %s 2>&1 | FileCheck %s --check-prefix=OPTIMIZE-SIBLING
-! RUN: %flang -### -foptimize-sibling-calls %s 2>&1 | FileCheck %s --check-prefix=OPTIMIZE-SIBLING
-! RUN: %flang -### -fno-optimize-sibling-calls %s 2>&1 | FileCheck %s --check-prefix=NO-OPTIMIZE-SIBLING
-! RUN: %flang -### -fno-optimize-sibling-calls -foptimize-sibling-calls %s 2>&1 | FileCheck %s --check-prefix=OPTIMIZE-SIBLING
-! RUN: %flang -### -foptimize-sibling-calls -fno-optimize-sibling-calls %s 2>&1 | FileCheck %s --check-prefix=NO-OPTIMIZE-SIBLING
+! RUN: %flang -### %s 2>&1 | FileCheck %s --check-prefixes=FC1,OPTIMIZE-SIBLING
+! RUN: %flang -### -foptimize-sibling-calls %s 2>&1 | FileCheck %s --check-prefixes=FC1,OPTIMIZE-SIBLING
+! RUN: %flang -### -fno-optimize-sibling-calls %s 2>&1 | FileCheck %s --check-prefixes=FC1,NO-OPTIMIZE-SIBLING
+! RUN: %flang -### -fno-optimize-sibling-calls -foptimize-sibling-calls %s 2>&1 | FileCheck %s --check-prefixes=FC1,OPTIMIZE-SIBLING
+! RUN: %flang -### -foptimize-sibling-calls -fno-optimize-sibling-calls %s 2>&1 | FileCheck %s --check-prefixes=FC1,NO-OPTIMIZE-SIBLING
 
-! OPTIMIZE-SIBLING: "-fc1"
+! FC1: "-fc1"
 ! OPTIMIZE-SIBLING-NOT: "-fno-optimize-sibling-calls"
-
-! NO-OPTIMIZE-SIBLING: "-fc1"
 ! NO-OPTIMIZE-SIBLING-SAME: "-fno-optimize-sibling-calls"
