8b15a9b3394a42b4ac3cb26b84c895ae0dfc1b89
Lucas Mellone <github.snugness349@passinbox.com>
2026-08-28 15:42:43 +0200

    fix: clang-format

--- libcxx/test/std/ranges/range.adaptors/range.enumerate/iterator/iter_move.pass.cpp
+++ libcxx/test/std/ranges/range.adaptors/range.enumerate/iterator/iter_move.pass.cpp
@@ -64,7 +64,8 @@ constexpr void test() {
   {
     // !is_nothrow_move_constructible_v<range_rvalue_reference_t<Base>>
     // underlying iter_move may throw
-    auto throwingMoveRange = std::views::iota(0, 9) | std::views::transform([](auto) noexcept { return ThrowingMove{}; });
+    auto throwingMoveRange = 
+        std::views::iota(0, 9) | std::views::transform([](auto) noexcept { return ThrowingMove{}; });
     std::ranges::enumerate_view v(throwingMoveRange);
     auto it = v.begin();
     static_assert(!noexcept(std::ranges::iter_move(it)));
