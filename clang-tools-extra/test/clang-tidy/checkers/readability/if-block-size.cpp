41f9b89fbcf3354d9ae300a87bc6149a1b8f0776
Alex Dutka <alexandre.dutka@asplus.fr>
2026-08-28 21:37:35 +0200

    add else if test

--- clang-tools-extra/test/clang-tidy/checkers/readability/if-block-size.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/readability/if-block-size.cpp
@@ -47,6 +47,33 @@ void should_warn(){
                 + 21  // 19
                 + 22; // 20
     } //          23     21
+
+
+    if (true){ // 1
+        int sum = 2;
+    } else if (true){ // 1
+// CHECK-MESSAGES: :[[@LINE-1]]:12: warning: if block spans 21 lines of code, which exceeds the threshold of 20 lines [readability-if-block-size]
+        int sum = 5   // 3
+                + 6   // 4
+                + 7   // 5
+                + 8   // 6
+                + 9   // 7
+                + 10  // 8
+                + 11  // 9
+                + 12  // 10
+                + 13  // 11
+                + 14  // 12
+                + 15  // 13
+                + 16  // 14
+                + 17  // 15
+                + 18  // 16
+                + 19  // 17
+                + 20  // 18
+                + 21  // 19
+                + 22; // 20
+    } else { //   23     21
+        int sum = 24;
+    } //          25
 }
 
 void should_not_warn(){
