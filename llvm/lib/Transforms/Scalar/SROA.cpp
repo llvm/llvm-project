8f067bf57c1233fa2ee3dd122ec8d8ddc8b81431
Yonah Goldberg <ygoldberg@nvidia.com>
2026-08-28 07:41:25 +0000

    SROA: Look through selects when finding a common type

--- llvm/lib/Transforms/Scalar/SROA.cpp
+++ llvm/lib/Transforms/Scalar/SROA.cpp
@@ -1478,6 +1478,40 @@ LLVM_DUMP_METHOD void AllocaSlices::dump() const { print(dbgs()); }
 
 #endif // !defined(NDEBUG) || defined(LLVM_ENABLE_DUMP)
 
+/// Find a common load/store type used through a pointer select.
+///
+/// A select is an immediate user of each pointer operand, so looking only at
+/// the select itself loses the types of the memory operations that actually
+/// access the pointers. Look through a select when all of its users are loads
+/// or stores of one common type. This mirrors the uses that SROA can later
+/// rewrite when speculating the select.
+static Type *findCommonTypeThroughSelect(SelectInst &SI) {
+  Type *Ty = nullptr;
+
+  for (User *U : SI.users()) {
+    Value *Ptr = &SI;
+    if (auto *BC = dyn_cast<BitCastInst>(U); BC && BC->hasOneUse()) {
+      Ptr = BC;
+      U = *BC->user_begin();
+    }
+
+    Type *UserTy = nullptr;
+    if (auto *LI = dyn_cast<LoadInst>(U);
+        LI && LI->getPointerOperand() == Ptr) {
+      UserTy = LI->getType();
+    } else if (auto *Store = dyn_cast<StoreInst>(U);
+               Store && Store->getPointerOperand() == Ptr) {
+      UserTy = Store->getValueOperand()->getType();
+    }
+
+    if (!UserTy || (Ty && Ty != UserTy))
+      return nullptr;
+    Ty = UserTy;
+  }
+
+  return Ty;
+}
+
 /// Walk the range of a partitioning looking for a common type to cover this
 /// sequence of slices.
 static std::pair<Type *, IntegerType *>
@@ -1501,6 +1535,8 @@ findCommonType(AllocaSlices::const_iterator B, AllocaSlices::const_iterator E,
       UserTy = LI->getType();
     } else if (StoreInst *SI = dyn_cast<StoreInst>(U->getUser())) {
       UserTy = SI->getValueOperand()->getType();
+    } else if (auto *SI = dyn_cast<SelectInst>(U->getUser())) {
+      UserTy = findCommonTypeThroughSelect(*SI);
     }
 
     if (IntegerType *UserITy = dyn_cast_or_null<IntegerType>(UserTy)) {
