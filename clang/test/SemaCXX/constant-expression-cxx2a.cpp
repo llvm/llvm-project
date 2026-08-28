600ba4dd1e8ce4d3b9dc0a671c3b87277e3a3086
ahmedkamel10 <amkzaher@gmail.com>
2026-08-28 08:43:42 +0300

    [Clang][ConstExpr] Fixed test for member function call before base init

--- clang/test/SemaCXX/constant-expression-cxx2a.cpp
+++ clang/test/SemaCXX/constant-expression-cxx2a.cpp
@@ -1552,11 +1552,11 @@ namespace GH211286 {
   };
   struct B : A {
     constexpr int f() const { return 42; }
-    constexpr B() : A(f()) {} 
+    constexpr B() : A(f()) {} // expected-note 2{{subexpression not valid in a constant expression}}
   };
   constexpr int foo() {
-    constexpr B b{}; 
+    constexpr B b{}; // expected-error {{constexpr variable 'b' must be initialized by a constant expression}} expected-note 2{{in call to 'B()'}}
     return b.val;
   }
-  constexpr auto x = foo(); 
+  constexpr auto x = foo(); // expected-error {{constexpr variable 'x' must be initialized by a constant expression}} expected-note {{in call to 'foo()'}}
 }
