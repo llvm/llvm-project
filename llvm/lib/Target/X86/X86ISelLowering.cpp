5d0086126a343e24a10f3b6ac70b9a3508760ad7
AZero13 <gfunni234@gmail.com>
2026-08-28 12:35:52 -0400

    Freeze LHS

--- llvm/lib/Target/X86/X86ISelLowering.cpp
+++ llvm/lib/Target/X86/X86ISelLowering.cpp
@@ -59832,9 +59832,10 @@ static SDValue combineADC(SDNode *N, SelectionDAG &DAG,
   // Fold ADC(SHL(X,1),0,Carry) -> ADC(X,X,Carry)
   // iff the flag result is dead.
   if (LHS.getOpcode() == ISD::SHL && isOneConstant(LHS.getOperand(1)) && RHSC &&
-      RHSC->isZero() && !needCarryOrOverflowFlag(SDValue(N, 1)))
-    return DAG.getNode(X86ISD::ADC, SDLoc(N), N->getVTList(), LHS.getOperand(0),
-                       LHS.getOperand(0), CarryIn);
+      RHSC->isZero() && !needCarryOrOverflowFlag(SDValue(N, 1))) {
+    SDValue X = DAG.getFreeze(LHS.getOperand(0));
+    return DAG.getNode(X86ISD::ADC, SDLoc(N), N->getVTList(), X, X, CarryIn);
+  }
 
   return SDValue();
 }
