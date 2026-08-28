c0d58fd3b0c899031a5cca0b20422232de7de37b
ahmedkamel10 <amkzaher@gmail.com>
2026-08-28 06:33:09 +0300

    [Clang][ConstExpr] Reject member function calls before base initialization
    This patch diagnoses undefined behavior during constant evaluation when a
    member function is called for an object under construction before all
    base classes have been initialized, per C++20 [class.base.init]/16.
    
    Fixes #211286

---
+++ test.cpp
@@ -0,0 +1,20 @@
+struct A {
+    constexpr A(int x) : val(x) {}
+    int val;
+};
+
+struct B : A {
+    constexpr int f() const { return 42; }
+
+    constexpr B() : A(f()) {}
+};
+
+constexpr int foo() {
+    constexpr B b{};
+    return b.val;
+}
+
+int main() {
+    constexpr auto x = foo();
+    return 0;
+}
