6e6a98845e26199902cbe625bc7fdb6089a75bcc
Florian Hahn <flo@fhahn.com>
2026-08-29 21:26:48 +0100

    [InstCombine] Preserve no-wrap flags in trunc (shl X, C) fold. (#219443)
    Preserve flags when folding trunc (shl X, C) into shl (trunc X), C. If
    both ops have NUW/NSW, they can be carried over to the new shl/trunc.
    
    No major changes on llvm-opt-benchmark-nightly besides a number of
    preserved flags and a few minor fold changes:
    https://github.com/dtcxzyw/llvm-opt-benchmark-nightly/pull/1083
    
    Alive2 Proof: https://alive2.llvm.org/ce/z/EYivzg
    
    PR: https://github.com/llvm/llvm-project/pull/219443

--- llvm/lib/Transforms/InstCombine/InstCombineCasts.cpp
+++ llvm/lib/Transforms/InstCombine/InstCombineCasts.cpp
@@ -1235,9 +1235,20 @@ Instruction *InstCombinerImpl::visitTrunc(TruncInst &Trunc) {
       // FoldShiftByConstant and is the extend in reg pattern.
       APInt Threshold = APInt(C->getType()->getScalarSizeInBits(), DestWidth);
       if (match(C, m_SpecificInt_ICMP(ICmpInst::ICMP_ULT, Threshold))) {
-        Value *NewTrunc = Builder.CreateTrunc(A, DestTy, A->getName() + ".tr");
-        return BinaryOperator::Create(Instruction::Shl, NewTrunc,
-                                      ConstantExpr::getTrunc(C, DestTy));
+        // If neither the wide shift nor the truncate wrap, propagate the wrap
+        // flags on the new truncate.
+        auto *WideShl = cast<OverflowingBinaryOperator>(Src);
+        bool NUW = Trunc.hasNoUnsignedWrap() && WideShl->hasNoUnsignedWrap();
+        bool NSW = Trunc.hasNoSignedWrap() && WideShl->hasNoSignedWrap();
+        Value *NewTrunc = Builder.CreateTrunc(A, DestTy, A->getName() + ".tr",
+                                              /*IsNUW=*/NUW, /*IsNSW=*/NSW);
+        // The original flags from the truncate can be propagated directly to
+        // the shift.
+        auto *NewShl = BinaryOperator::Create(
+            Instruction::Shl, NewTrunc, ConstantExpr::getTrunc(C, DestTy));
+        NewShl->setHasNoUnsignedWrap(Trunc.hasNoUnsignedWrap());
+        NewShl->setHasNoSignedWrap(Trunc.hasNoSignedWrap());
+        return NewShl;
       }
     }
   }
