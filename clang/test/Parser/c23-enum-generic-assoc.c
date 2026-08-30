6b06a601a503e565543088aeec0d9a6fe9c4af07
Abdulmalek Almkainzi <aalmkainzi@gmail.com>
2026-08-30 03:30:15 +0300

    made test clearer + added one more static_assert

--- clang/test/Parser/c23-enum-generic-assoc.c
+++ clang/test/Parser/c23-enum-generic-assoc.c
@@ -3,8 +3,10 @@
 
 typedef long l;
 
-_Static_assert(_Generic(0L, enum E : long { A } : 0, int: 1) == 0, "");
+_Static_assert(_Generic(0L, enum E : long { A } : 1, int: 0), "");
 
 _Static_assert(_Generic(0L, enum E : 0, int: 1) == 0, "");
 
-_Static_assert(_Generic(0L, enum A : l { B } : 0, int: 1) == 0, "");
+_Static_assert(_Generic(0L, enum A : l { B } : 1, int: 0), "");
+
+_Static_assert(_Generic(0L, struct { enum A : l { B } a : 1; } : 0, long: 1), "");
