c23bc45a69755803a68daf12b870f389cac202b0
inconnu08 <taufiqrx8@gmail.com>
2026-08-31 01:38:34 -0400

    [clang] Make ast-dump-filter-path include test portable

--- clang/test/AST/ast-dump-filter-path-include.c
+++ clang/test/AST/ast-dump-filter-path-include.c
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -ast-dump -ast-dump-filter-path %s %s | FileCheck %s
+// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -ast-dump -ast-dump-filter-path "*ast-dump-filter-path-include.c" %s | FileCheck %s
 
 #include "filter-header.h"
 
