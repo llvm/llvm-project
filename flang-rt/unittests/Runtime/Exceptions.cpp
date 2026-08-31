0fdb2bf0d5f6a75d39ac0a7871bd3c4cbdf7cabd
Rainer Orth <ro@gcc.gnu.org>
2026-08-31 16:38:52 +0200

    Explain qualifier omissing.

--- flang-rt/unittests/Runtime/Exceptions.cpp
+++ flang-rt/unittests/Runtime/Exceptions.cpp
@@ -30,6 +30,8 @@ namespace {
 
 // Saves and restores the host floating-point environment so tests do not
 // leak exception flags into each other or into the rest of the suite.
+// Omit std qualifier on fegetenv, fesetenv: FreeBSD 15.1 defines them as
+// macros.
 class FenvScope {
 public:
   FenvScope() {
