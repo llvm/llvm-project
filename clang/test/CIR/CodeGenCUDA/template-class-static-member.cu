9a2d88a687b5c92f5f656dee0c2e8ae70224d779
Mariya Podchishchaeva <Mariya.Podchishchaeva@amd.com>
2026-08-31 05:18:40 -0500

    Check initializers of device-side variables, make the test a bit nicer

--- clang/test/CIR/CodeGenCUDA/template-class-static-member.cu
+++ clang/test/CIR/CodeGenCUDA/template-class-static-member.cu
@@ -54,17 +54,17 @@ const int A<T>::const_member;
 
 template class A<int>;
 
-//CIR-DEV-DAG: cir.global "private" internal comdat dso_local target_address_space(1) @_ZN1AIiE8d_memberE
-//CIR-DEV-DAG: cir.global "private" constant internal comdat dso_local target_address_space(4) @_ZN1AIiE8c_memberE
-//CIR-DEV-DAG: cir.global "private" internal comdat dso_local  target_address_space(1) @_ZN1AIiE8m_memberE
-//CIR-DEV-DAG: cir.global "private" constant internal comdat dso_local  target_address_space(4) @_ZN1AIiE12const_memberE
-//CIR-DEV-NEG-NOT: @_ZN1AIiE8h_memberE
+// CIR-DEV-DAG: cir.global "private" internal comdat dso_local  target_address_space(1) @_ZN1AIiE8d_memberE = #cir.int<0>
+// CIR-DEV-DAG: cir.global "private" constant internal comdat dso_local  target_address_space(4) @_ZN1AIiE8c_memberE = #cir.int<0>
+// CIR-DEV-DAG: cir.global "private" internal comdat dso_local  target_address_space(1) @_ZN1AIiE8m_memberE = #cir.int<0>
+// CIR-DEV-DAG: cir.global "private" constant internal comdat dso_local  target_address_space(4) @_ZN1AIiE12const_memberE = #cir.int<0>
+// CIR-DEV-NEG-NOT: @_ZN1AIiE8h_memberE
 
-//DEV-DAG: @_ZN1AIiE8d_memberE = internal addrspace(1) global i32 0, comdat, align 4
-//DEV-DAG: @_ZN1AIiE8c_memberE = internal addrspace(4) constant i32 0, comdat, align 4
-//DEV-DAG: @_ZN1AIiE8m_memberE = internal addrspace(1) externally_initialized global ptr addrspace(1) null
-//DEV-DAG: @_ZN1AIiE12const_memberE = internal addrspace(4) constant i32 0, comdat, align 4
-//DEV-NEG-NOT: @_ZN1AIiE8h_memberE
+// DEV-DAG: @_ZN1AIiE8d_memberE = internal addrspace(1) global i32 0, comdat, align 4
+// DEV-DAG: @_ZN1AIiE8c_memberE = internal addrspace(4) constant i32 0, comdat, align 4
+// DEV-DAG: @_ZN1AIiE8m_memberE = internal addrspace(1) externally_initialized global ptr addrspace(1) null
+// DEV-DAG: @_ZN1AIiE12const_memberE = internal addrspace(4) constant i32 0, comdat, align 4
+// DEV-NEG-NOT: @_ZN1AIiE8h_memberE
 
 // CIR-HOST-DAG:  cir.global weak_odr comdat @_ZN1AIiE8h_memberE = #cir.int<0>
 // CIR-HOST-DAG:  cir.global "private" internal comdat dso_local @_ZN1AIiE8d_memberE = #cir.undef
@@ -72,8 +72,8 @@ template class A<int>;
 // CIR-HOST-DAG:  cir.global "private" internal comdat dso_local @_ZN1AIiE8m_memberE = #cir.int<0>
 // CIR-HOST-DAG:  cir.global constant weak_odr comdat @_ZN1AIiE12const_memberE = #cir.int<0>
 
-//HOST-DAG: @_ZN1AIiE8h_memberE = weak_odr global i32 0, comdat, align 4
-//HOST-DAG: @_ZN1AIiE8d_memberE = internal global i32 undef, comdat, align 4
-//HOST-DAG: @_ZN1AIiE8c_memberE = internal global i32 undef, comdat, align 4
-//HOST-DAG: @_ZN1AIiE8m_memberE = internal externally_initialized global ptr null
-//HOST-DAG: @_ZN1AIiE12const_memberE = weak_odr constant i32 0, comdat, align 4
+// HOST-DAG: @_ZN1AIiE8h_memberE = weak_odr global i32 0, comdat, align 4
+// HOST-DAG: @_ZN1AIiE8d_memberE = internal global i32 undef, comdat, align 4
+// HOST-DAG: @_ZN1AIiE8c_memberE = internal global i32 undef, comdat, align 4
+// HOST-DAG: @_ZN1AIiE8m_memberE = internal externally_initialized global ptr null
+// HOST-DAG: @_ZN1AIiE12const_memberE = weak_odr constant i32 0, comdat, align 4
