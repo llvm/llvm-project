fbb50ddfec1f359fa09da7e85fcc78e2e4c8aabc
benedekaibas <kaibas01@allegheny.edu>
2026-08-31 22:33:31 +0200

    Add more test cases.

--- clang/test/Analysis/explicit-lambda-capture.cpp
+++ clang/test/Analysis/explicit-lambda-capture.cpp
@@ -1,4 +1,6 @@
-// RUN: %clang_analyze_cc1 -std=c++23 -analyzer-checker=core -verify %s
+// RUN: %clang_analyze_cc1 -std=c++23 -analyzer-checker=core,cplusplus.Move -verify %s
+
+#include "Inputs/system-header-simulator-cxx.h"
 
 int implicit_capture_by_value() {
   int d = 0;
@@ -40,4 +42,37 @@ int explicit_by_value_no_error() {
   int d = 9;
   auto lam = [d](this auto self) { return 1 / d; }; // no-warning 
   return lam();
+}
+
+auto by_val() {
+  std::vector<int> v;
+  auto lam = [v](this auto self) {
+    auto res = std::move(v);
+    return res;
+  };
+
+  lam();
+  lam();
+}
+
+auto by_lval() {
+  std::vector<int> v;
+  auto lam = [v](this auto &self) {
+    auto res = std::move(v); // expected-warning {{Moved-from object '' of type 'std::vector' is moved}}
+    return res;
+  };
+
+  lam();
+  lam();
+}
+
+auto by_rval() {
+  std::vector<int> v;
+  auto lam = [v](this auto &&self) {
+    auto res = std::move(v); // expected-warning {{Moved-from object '' of type 'std::vector' is moved}}
+    return res;
+  };
+
+  std::move(lam)();
+  std::move(lam)();
 }
