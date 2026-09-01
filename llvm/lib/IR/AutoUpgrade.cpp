421076f3f23c44a1beb548fa3a872183f71d05f1
DharuniRAcharya <dharunira@nvidia.com>
2026-09-01 06:18:46 +0000

    Revert "Example usecase with collector_b flag as default argument to tcgen05.mma intrinsics"
    This reverts commit 157b5770e5373bd65639ad7cba345bd959de3795.

--- llvm/lib/IR/AutoUpgrade.cpp
+++ llvm/lib/IR/AutoUpgrade.cpp
@@ -1388,6 +1388,18 @@ static Intrinsic::ID shouldUpgradeNVPTXBF16Intrinsic(StringRef Name) {
   return Intrinsic::not_intrinsic;
 }
 
+static Intrinsic::ID shouldUpgradeNVPTXTcgen05MMAIntrinsic(Function *F,
+                                                           StringRef Name) {
+  if (!Name.consume_front("tcgen05.mma."))
+    return Intrinsic::not_intrinsic;
+
+  // tcgen05.mma.ws.* variants do not need collector-b appended.
+  if (Name.starts_with("ws"))
+    return Intrinsic::not_intrinsic;
+
+  return F->getIntrinsicID();
+}
+
 static bool consumeNVVMPtrAddrSpace(StringRef &Name) {
   return Name.consume_front("local") || Name.consume_front("shared") ||
          Name.consume_front("global") || Name.consume_front("constant") ||
@@ -1952,6 +1964,13 @@ static bool upgradeIntrinsicFunction1(Function *F, Function *&NewFn,
         rename(F);
         NewFn = Intrinsic::getOrInsertDeclaration(F->getParent(), IID);
         return true;
+      }
+
+      // Upgrade tcgen05.mma intrinsics missing collector_usage_b.
+      IID = shouldUpgradeNVPTXTcgen05MMAIntrinsic(F, Name);
+      if (IID != Intrinsic::not_intrinsic) {
+        NewFn = Intrinsic::getOrInsertDeclaration(F->getParent(), IID);
+        return NewFn != F;
       }
 
       // The following nvvm intrinsics correspond exactly to an LLVM idiom, but
