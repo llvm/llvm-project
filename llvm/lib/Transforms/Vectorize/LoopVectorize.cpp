e5437479f288038f23e40d51e06bab8596e5ced3
Luke Lau <luke@igalia.com>
2026-09-01 21:46:31 +0800

    [VPlan] Don't replicate extractvalues that would require extracting lanes from a struct (#219941)
    In the added test cases, we have an extractvalue in a replicate region:
    
        vector.ph:
          WIDEN-INTRINSIC ir<%sincos> = call llvm.sincos(ir<0.000000e+00>)
    
        ...
    
        pred.store.if:
          EMIT vp<%3> = extractelement ir<%sincos>, ir<0>
          CLONE ir<%cos> = extractvalue vp<%3>
          CLONE store ir<%cos>, ir<%cos_dst>
    
    However the operand
    
    a) doesn't generate per lane
    b) isn't defined in the same replicate region
    
    so replicateRegionByVF tries to extract a lane from a struct, which
    doesn't work. This fixes it by choosing to widen the extractvalue
    earlier on when we know we'll end up trying to extract lanes otherwise.
    
    This is an alternative to #219819 
    
    Fixes https://github.com/llvm/llvm-project/issues/210376

--- llvm/lib/Transforms/Vectorize/LoopVectorize.cpp
+++ llvm/lib/Transforms/Vectorize/LoopVectorize.cpp
@@ -6445,6 +6445,15 @@ VPRecipeBuilder::tryToCreateWidenNonPhiRecipe(VPSingleDefRecipe *R,
                        VPI->getOpcode()) &&
          "Should have been handled prior to this!");
 
+  // We can only replicate an extractvalue if its operand generates per lane in
+  // the same block, otherwise we would need to extract a lane from its struct
+  // operand which is invalid.
+  if (VPI->getOpcode() == Instruction::ExtractValue)
+    if (VPRecipeBase *OpR = VPI->getOperand(0)->getDefiningRecipe())
+      if (!vputils::doesGeneratePerAllLanes(OpR) ||
+          OpR->getParent() != VPI->getParent())
+        return tryToWiden(VPI);
+
   if (!shouldWiden(Instr, Range))
     return nullptr;
 
