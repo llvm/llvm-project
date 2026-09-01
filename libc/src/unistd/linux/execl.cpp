539b8e0376e7f3c5cb81a6c66be72e0e5d05706d
Michael Jones <michaelrj@google.com>
2026-09-01 18:19:22 +0000

    [libc] Implement execl
    Basically the same as execle, but passes the global env pointer. POSIX
    standard here: https://pubs.opengroup.org/onlinepubs/9799919799/functions/exec.html

---
+++ libc/src/unistd/linux/execl.cpp
@@ -0,0 +1,67 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// Linux implementation of execl
+///
+//===----------------------------------------------------------------------===//
+
+#include "src/unistd/execl.h"
+
+#include "hdr/types/size_t.h"
+#include "src/__support/OSUtil/linux/syscall_wrappers/execve.h"
+#include "src/__support/common.h"
+#include "src/__support/libc_errno.h"
+#include "src/__support/macros/config.h"
+#include "src/unistd/environ.h"
+
+#include <stdarg.h>
+
+namespace LIBC_NAMESPACE_DECL {
+
+LLVM_LIBC_FUNCTION(int, execl, (const char *path, const char *arg0, ...)) {
+  if (arg0 == nullptr) {
+    char *argv[] = {nullptr};
+    auto ret = linux_syscalls::execve(
+        path, argv, const_cast<char *const *>(LIBC_NAMESPACE::environ));
+    if (!ret) {
+      libc_errno = ret.error();
+      return -1;
+    }
+    return *ret;
+  }
+
+  va_list varargs, varargs_copy;
+  va_start(varargs, arg0);
+  va_copy(varargs_copy, varargs);
+
+  size_t argc = 1;
+  while (va_arg(varargs, const char *) != nullptr)
+    ++argc;
+  va_end(varargs);
+
+  char *argv[argc + 1];
+  argv[0] = const_cast<char *>(arg0);
+
+  for (size_t i = 1; i <= argc; ++i)
+    argv[i] = va_arg(varargs_copy, char *);
+  va_end(varargs_copy);
+
+  auto ret = linux_syscalls::execve(
+      path, argv, const_cast<char *const *>(LIBC_NAMESPACE::environ));
+  if (!ret) {
+    libc_errno = ret.error();
+    return -1;
+  }
+
+  // Control will not reach here on success but having a return statement will
+  // keep the compilers happy.
+  return *ret;
+}
+
+} // namespace LIBC_NAMESPACE_DECL
