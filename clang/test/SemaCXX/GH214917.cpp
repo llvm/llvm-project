195af635fdec242bcfa8c5e9e0339693d3c84e8c
Akash Manna <akash.manna.mymail@gmail.com>
2026-09-01 23:45:56 +0530

    Update struct definitions and template usage in test case

--- clang/test/SemaCXX/GH214917.cpp
+++ clang/test/SemaCXX/GH214917.cpp
@@ -6,12 +6,38 @@ unknown_type a; // expected-error {{unknown type name 'unknown_type'}}
 unknown_type b;
 // expected-error@* {{too many errors emitted}}
 
+struct T {
+  int x, y;
+};
+
+void f() {
+  template for (auto i : T{42, 10}) {
+    (void)i;
+  }
+}
+
+struct S {
+  Foo(Foo &&);
+};
+
+template <int> std { struct tuple_size; };
+
 struct V {
   int i, j;
 };
 
-void bar() {
-  template for (auto i : V{42, 10}) {
-    (void)i;
-  }
-}
+template <> struct std::tuple_size<V> {
+  static const int value = 2;
+};
+template <int I> struct std::tuple_element<I, V> {
+  using type = int;
+};
+
+template <> struct std::tuple_size<V> {
+  static const int value = 2;
+};
+template <int I> struct std::tuple_element<I, V> {
+  using type = int;
+};
+
+void bar() { template for (auto i : V{42, 10}) i += (V.i == 42); }
