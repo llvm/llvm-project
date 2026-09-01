9078fbc96a24d8da6b59bb6940db8fa169ddbe37
Florian Hahn <flo@fhahn.com>
2026-09-01 20:48:58 +0100

    !fixup move to .cpp

--- llvm/include/llvm/CodeGenTypes/MachineValueType.h
+++ llvm/include/llvm/CodeGenTypes/MachineValueType.h
@@ -533,36 +533,26 @@ namespace llvm {
       return NFTable[SimpleTy - FIRST_VALUETYPE];
     }
 
-    static MVT getVectorVT(MVT VT, unsigned NumElements, bool IsScalable) {
-      constexpr unsigned MaxNumElements = 4096;
-      constexpr unsigned NumSlots = 32;
-      struct VecVTTable {
-        // Maps number of elements to a compact slot index. Only element counts
-        // used by any vector type get a slot; others stay 0. There are only a
-        // small number (< 32) of distinct element counts for vector types.
-        uint8_t Slots[MaxNumElements + 1] = {};
-        // The vector MVTs, indexed by [IsScalable][ElementType][Slot]. Element
-        // types are scalar MVTs, so only those need an entry. Other entries
-        // stay INVALID_SIMPLE_VALUE_TYPE.
-        SimpleValueType Tys[2][FIRST_VECTOR_VALUETYPE][NumSlots] = {};
-      };
-      static constexpr VecVTTable VecVTs = [] {
-        VecVTTable T{};
-        uint8_t NextSlot = 1;
-#define GET_VT_ATTR(Ty, Sz, Any, Int, FP, Vec, Sc, Tup, NF, NElem, EltTy)    \
-    if (Vec && !Tup && !T.Slots[NElem])                                        \
-      T.Slots[NElem] = NextSlot++;
-#include "llvm/CodeGen/GenVT.inc"
-#undef GET_VT_ATTR
-#define GET_VT_ATTR(Ty, Sz, Any, Int, FP, Vec, Sc, Tup, NF, NElem, EltTy)    \
-    if (Vec && !Tup)                                                           \
-      T.Tys[Sc][EltTy][T.Slots[NElem]] = Ty;
-#include "llvm/CodeGen/GenVT.inc"
-#undef GET_VT_ATTR
-        return T;
-      }();
+    /// Lookup table for getVectorVT. All simple vector types must fit into
+    /// the table; if a target has vector VTs that do not fit, building
+    /// MachineValueType.cpp, which defines the table, will fail.
+    struct VecVTTable {
+      static constexpr unsigned MaxNumElements = 4096;
+      static constexpr unsigned NumSlots = 32;
+      // Maps number of elements to a compact slot index. Only element counts
+      // used by any vector type get a slot; others stay 0. There are only a
+      // small number (< NumSlots) of distinct element counts for vector types.
+      uint8_t Slots[MaxNumElements + 1] = {};
+      // The vector MVTs, indexed by [IsScalable][ElementType][Slot]. Element
+      // types are scalar MVTs, so only those need an entry. Other entries
+      // stay INVALID_SIMPLE_VALUE_TYPE.
+      SimpleValueType Tys[2][FIRST_VECTOR_VALUETYPE][NumSlots] = {};
+    };
+    LLVM_ABI static const VecVTTable VecVTs;
 
-      if (VT.SimpleTy >= FIRST_VECTOR_VALUETYPE || NumElements > MaxNumElements)
+    static MVT getVectorVT(MVT VT, unsigned NumElements, bool IsScalable) {
+      if (VT.SimpleTy >= FIRST_VECTOR_VALUETYPE ||
+          NumElements > VecVTTable::MaxNumElements)
         return INVALID_SIMPLE_VALUE_TYPE;
       return VecVTs.Tys[IsScalable][VT.SimpleTy][VecVTs.Slots[NumElements]];
     }
