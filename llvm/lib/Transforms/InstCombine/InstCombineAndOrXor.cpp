f1ca6b1d7fea1413ed2c3a74f85db37e36f5c612
ryanashita <ryunosukeyanashita@gmail.com>
2026-09-01 14:59:16 -0700

    Address comments and make changes

--- llvm/lib/Transforms/InstCombine/InstCombineAndOrXor.cpp
+++ llvm/lib/Transforms/InstCombine/InstCombineAndOrXor.cpp
@@ -4321,27 +4321,28 @@ Instruction *InstCombinerImpl::visitOr(BinaryOperator &I) {
       }
 
       // (trunc (lshr X, S) & C0) | (lshr (trunc X), S & C1)
-      // --> (trunc (lshr X, S) & (C0 | C1) (and similar cases) 
+      // --> (trunc (lshr X, S) & (C0 | C1) (and similar cases)
       // A = trunc (lshr X, S) B = lshr (trunc X), S
       const APInt *ShiftAmt;
-      if (match(A, m_OneUse(m_Trunc(m_LShr(m_Value(X), m_APInt(ShiftAmt))))) &&
-          match(B, m_LShr(m_Trunc(m_Specific(X)),
-                          m_SpecificInt(ShiftAmt->getZExtValue())))) {
-        APInt CombinedMask = *C0 | *C1;
-        return BinaryOperator::CreateAnd(
-            A, ConstantInt::get(I.getType(), CombinedMask));
+      Value *Wide, *Narrow;
+      if (match(A, m_Trunc(m_LShr(m_Value(X), m_APInt(ShiftAmt))))) {
+        Wide = A;
+        Narrow = B;
+      } else if (match(B, m_Trunc(m_LShr(m_Value(X), m_APInt(ShiftAmt))))) {
+        Wide = B;
+        Narrow = A;
+      } else {
+        return nullptr;
       }
-      // A = lshr (trunc X), S
-      // B = trunc (lshr X, S)
-      const APInt *ReverseShiftAmt;
-      if (match(B, m_OneUse(m_Trunc(
-                       m_LShr(m_Value(X), m_APInt(ReverseShiftAmt))))) &&
-          match(A, m_LShr(m_Trunc(m_Specific(X)),
-                          m_SpecificInt(ReverseShiftAmt->getZExtValue())))) {
-        APInt CombinedMask = *C0 | *C1;
-        return BinaryOperator::CreateAnd(
-            B, ConstantInt::get(I.getType(), CombinedMask));
+
+      if (!match(Narrow,
+                 m_LShr(m_Trunc(m_Specific(X)), m_SpecificInt(*ShiftAmt)))) {
+        return nullptr;
       }
+
+      APInt CombinedMask = *C0 | *C1;
+      return BinaryOperator::CreateAnd(
+          Wide, ConstantInt::get(I.getType(), CombinedMask));
     }
 
     // Don't try to form a select if it's unlikely that we'll get rid of at
