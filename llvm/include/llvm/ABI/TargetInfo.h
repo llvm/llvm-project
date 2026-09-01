18ffe575ee1b03f147737ca7eba3c7b708210528
skc7 <Krishna.Sankisa@amd.com>
2026-09-01 12:35:15 +0530

    [ABI] Add AMDGPU target ABI classifier to the LLVM ABI library

--- llvm/include/llvm/ABI/TargetInfo.h
+++ llvm/include/llvm/ABI/TargetInfo.h
@@ -92,6 +92,10 @@ protected:
   /// return Ty unchanged.
   LLVM_ABI const Type *useFirstFieldIfTransparentUnion(const Type *Ty) const;
 
+  /// If the record reduces to a single scalar element,
+  /// return that element type; otherwise null.
+  LLVM_ABI const Type *isSingleElementStruct(const Type *Ty) const;
+
   /// Apply rules for classifying return types that are common to all targets.
   LLVM_ABI bool maybeCommonClassifyReturnType(FunctionInfo &FI) const;
 
@@ -126,6 +130,8 @@ protected:
 };
 
 LLVM_ABI std::unique_ptr<TargetInfo> createBPFTargetInfo(TypeBuilder &TB);
+
+LLVM_ABI std::unique_ptr<TargetInfo> createAMDGPUTargetInfo(TypeBuilder &TB);
 
 /// The AVX ABI level for X86 targets.
 enum class X86AVXABILevel {
