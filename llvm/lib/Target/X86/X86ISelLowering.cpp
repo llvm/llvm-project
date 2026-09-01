c334df7bb4c96c2a9fbb72467a2f61e21cb0f957
Rohan Shenoy <RohanRadhesh.Shenoy@amd.com>
2026-09-02 01:31:54 +0530

    format

--- llvm/lib/Target/X86/X86ISelLowering.cpp
+++ llvm/lib/Target/X86/X86ISelLowering.cpp
@@ -60601,8 +60601,8 @@ static SDValue combineSub(SDNode *N, SelectionDAG &DAG,
   }
 
   // Fold SUB(~A,~B) -> SUB(B,A)
-  if (isBitwiseNot(Op0) && Op0.hasOneUse() &&
-      isBitwiseNot(Op1) && Op1.hasOneUse()) {
+  if (isBitwiseNot(Op0) && Op0.hasOneUse() && isBitwiseNot(Op1) &&
+      Op1.hasOneUse()) {
     return DAG.getNode(ISD::SUB, DL, VT, Op1.getOperand(0), Op0.getOperand(0));
   }
 
