7622485e953e7ad4483ffb8b49925f8791322e4b
Tue Ly <lntue.h@gmail.com>
2026-09-01 19:40:49 +0000

    [libc] Fix char8_t public and proxy headers.

--- libc/hdr/types/char8_t.h
+++ libc/hdr/types/char8_t.h
@@ -9,6 +9,25 @@
 #ifndef LLVM_LIBC_HDR_TYPES_CHAR8_T_H
 #define LLVM_LIBC_HDR_TYPES_CHAR8_T_H
 
+#ifdef LIBC_FULL_BUILD
+
 #include "include/llvm-libc-types/char8_t.h"
+
+#else // Overlay mode
+
+// MacOS doesn't provide uchar.h so we use the types provided by LLVM-libc.
+#ifdef __APPLE__
+#include "include/llvm-libc-types/char8_t.h"
+#else
+#include "hdr/uchar_overlay.h"
+#endif
+
+// Define char8_t in C++ for internal usage if it is not provided by compiler
+// or system uchar.h header.
+#ifndef __cpp_char8_t
+using char8_t = unsigned char;
+#endif // !__cpp_char8_t
+
+#endif // LIBC_FULL_BUILD
 
 #endif // LLVM_LIBC_HDR_TYPES_CHAR8_T_H
