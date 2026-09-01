e9c566a21e10d63414f559d2f27cf3eaf2f40eb4
skc7 <Krishna.Sankisa@amd.com>
2026-09-01 12:35:15 +0530

    [ABI] Add AMDGPU target ABI classifier to the LLVM ABI library

--- llvm/include/llvm/ABI/TargetInfo.h
+++ llvm/include/llvm/ABI/TargetInfo.h
@@ -145,6 +145,8 @@ protected:
 
 LLVM_ABI std::unique_ptr<TargetInfo> createBPFTargetInfo(TypeBuilder &TB);
 
+LLVM_ABI std::unique_ptr<TargetInfo> createAMDGPUTargetInfo(TypeBuilder &TB);
+
 /// The AVX ABI level for X86 targets.
 enum class X86AVXABILevel {
   None,
