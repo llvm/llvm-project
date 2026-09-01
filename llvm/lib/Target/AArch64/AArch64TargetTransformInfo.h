af672b1509dc1c708bb2b4600b6d22e0f8ae94b0
Jack Styles <jack.styles@arm.com>
2026-09-01 14:58:40 +0100

    [AArch64] Allow Vectorization of Promoted types for compressstore
    When using SVE, types that are not 128bit widths will be promoted
    to be legal SVE Types. Currently, when using
    llvm.masked.compressstore these types are marked as invalid for
    SVE Only but these are legal types.
    
    Enable these types whether they are promoted or legal types.

--- llvm/lib/Target/AArch64/AArch64TargetTransformInfo.h
+++ llvm/lib/Target/AArch64/AArch64TargetTransformInfo.h
@@ -335,21 +335,12 @@ public:
 
   bool isElementTypeLegalForCompressStore(Type *Ty) const {
     assert(Ty->isIntegerTy() || Ty->isFloatingPointTy());
-    // 32-bit and 64-bit element types are legal if we have SVE.
-    if (is_contained({32u, 64u}, Ty->getScalarSizeInBits()))
-      return true;
-
-    // 8-bit and 16-bit types require +sve2p2 or +sme2p2.
-    if (is_contained({8u, 16u}, Ty->getScalarSizeInBits()))
-      return ST->hasSVE2p2() || ST->hasSME2p2();
-
-    return false;
+    return is_contained({8u, 16u, 32u, 64u}, Ty->getScalarSizeInBits());
   }
 
   bool isLegalMaskedCompressStore(Type *DataType,
                                   Align Alignment) const override {
-    if (!(ST->isSVEAvailable() ||
-          (ST->isSVEorStreamingSVEAvailable() && ST->hasSME2p2())))
+    if (!(ST->isSVEAvailable() || ST->isSVEorStreamingSVEAvailable()))
       return false;
 
     if (isa<FixedVectorType>(DataType) &&
