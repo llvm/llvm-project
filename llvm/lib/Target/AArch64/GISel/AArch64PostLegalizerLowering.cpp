56c4d9a2d80910ac6e9f7cc3eb93eba22ea3cd34
Josh Rodriguez <josh.rodriguez@arm.com>
2026-09-02 11:00:44 +0000

    [AArch64][GlobalISel] Combine GISel and SDAG definition of isUZP_v_undef_Mask
    Combine definition into a single one, which is stored in AArch64PerfectShuffle.h

--- llvm/lib/Target/AArch64/GISel/AArch64PostLegalizerLowering.cpp
+++ llvm/lib/Target/AArch64/GISel/AArch64PostLegalizerLowering.cpp
@@ -205,25 +205,6 @@ bool matchTRN(MachineInstr &MI, MachineRegisterInfo &MRI,
   return true;
 }
 
-/// isUZP_v_undef_Mask - Special case of isUZPMask for canonical form of
-/// "vector_shuffle v, v", i.e., "vector_shuffle v, undef".
-/// Mask is e.g., <0, 2, 0, 2> instead of <0, 2, 4, 6>,
-static bool isUZP_v_undef_Mask(ArrayRef<int> M, unsigned NumElts,
-                               unsigned &WhichResult) {
-  unsigned Half = NumElts / 2;
-  WhichResult = (M[0] == 0 ? 0 : 1);
-  for (unsigned j = 0; j != 2; ++j) {
-    unsigned Idx = WhichResult;
-    for (unsigned i = 0; i != Half; ++i) {
-      int MIdx = M[i + j * Half];
-      if (MIdx >= 0 && (unsigned)MIdx != Idx)
-        return false;
-      Idx += 2;
-    }
-  }
-
-  return true;
-}
 
 /// \return true if a G_SHUFFLE_VECTOR instruction \p MI can be replaced with
 /// a G_UZP1 or G_UZP2 instruction.
