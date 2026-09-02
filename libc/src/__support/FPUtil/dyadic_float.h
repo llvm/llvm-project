ffde43250299732784d5c6c1134f1f65e389dc3b
Sukumarsawant <sawantsukumar@gmail.com>
2026-09-02 17:51:46 +0530

    nit

--- libc/src/__support/FPUtil/dyadic_float.h
+++ libc/src/__support/FPUtil/dyadic_float.h
@@ -140,12 +140,10 @@ template <size_t Bits> struct DyadicFloat {
   // Used for aligning exponents.  Output might not be normalized.
   LIBC_INLINE constexpr DyadicFloat &shift_left(unsigned shift_length) {
     exponent -= static_cast<int>(shift_length);
-    if (shift_length < Bits) {
+    if (shift_length < Bits)
       mantissa <<= shift_length;
-    } else {
-      exponent = 0;
+    else
       mantissa = MantissaType(0);
-    }
     return *this;
   }
 
