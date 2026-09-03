a31fd7a7402f2602ea9b9ff2af88a8fe07a89b80
Lang Hames <lhames@gmail.com>
2026-09-03 05:39:50 +0000

    [orc-rt] Add very basic Linux triple detection.

--- orc-rt/lib/bedrock/linux/TargetTriple.cpp
+++ orc-rt/lib/bedrock/linux/TargetTriple.cpp
@@ -5,10 +5,43 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
+
 #include "orc-rt/bedrock/ExecutorProcessInfo.h"
+
+#include "orc-rt-internal/bedrock/TargetDetails.h"
 
 namespace orc_rt {
 
-std::string ExecutorProcessInfo::detectTargetTriple() noexcept { return {}; }
+std::string ExecutorProcessInfo::detectTargetTriple() noexcept {
+  static const std::string Cache = [] {
+    using namespace target_detail;
+
+#if defined(__aarch64__)
+    constexpr std::string_view Arch = arch::aarch64;
+#elif defined(__x86_64h__)
+    constexpr std::string_view Arch = arch::x86_64h;
+#elif defined(__x86_64__)
+    constexpr std::string_view Arch = arch::x86_64;
+#elif defined(__i386__)
+    constexpr std::string_view Arch = arch::i386;
+#else
+#error "Unsupported architecture"
+#endif
+
+#if defined(__x86_64__) || defined(__i386__)
+    constexpr std::string_view Vendor = vendor::pc;
+#else
+    constexpr std::string_view Vendor = vendor::unknown;
+#endif
+
+#if defined(__GLIBC__)
+    return makeTargetTriple({Arch, Vendor, "linux", "gnu"});
+#else
+    return makeTargetTriple({Arch, Vendor, "linux"});
+#endif
+  }();
+
+  return Cache;
+}
 
 } // namespace orc_rt
