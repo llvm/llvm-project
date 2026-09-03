8b3f42db87bf37b156d028c89ac941d1f63ef3ef
Kazu Hirata <kazu@google.com>
2026-09-03 09:18:44 -0700

    Address comments.

--- lldb/source/Plugins/Process/Linux/NativeProcessLinux.cpp
+++ lldb/source/Plugins/Process/Linux/NativeProcessLinux.cpp
@@ -403,6 +403,8 @@ void NativeProcessLinux::Manager::SigchldHandler() {
     // vice-versa. This means that if the child event arrives first, it may not
     // be handled by any process (because it doesn't know the thread belongs to
     // it).
+    // The loop below may modify m_processes (create or delete entries), so
+    // operate on a temporary copy.
     auto processes = llvm::to_vector(m_processes);
     bool handled = llvm::any_of(processes, [&](NativeProcessLinux *process) {
       return process->TryHandleWaitStatus(pid, status);
