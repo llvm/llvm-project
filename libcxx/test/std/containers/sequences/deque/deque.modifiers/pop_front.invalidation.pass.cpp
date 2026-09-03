b09c47a38d564da3a98c59df301b415c99c89d3b
A. Jiang <de34@live.cn>
2026-09-03 14:26:42 +0800

    Reduce pop invalidation tests for constant evaluation

--- libcxx/test/std/containers/sequences/deque/deque.modifiers/pop_front.invalidation.pass.cpp
+++ libcxx/test/std/containers/sequences/deque/deque.modifiers/pop_front.invalidation.pass.cpp
@@ -8,7 +8,7 @@
 
 // <deque>
 
-// void pop_front()
+// void pop_front(); // constexpr since C++26
 
 //  Erasing items from the beginning or the end of a deque shall not invalidate iterators
 //  to items that were not erased.
@@ -38,7 +38,12 @@ TEST_CONSTEXPR_CXX26 void test(C c) {
 
 TEST_CONSTEXPR_CXX26 bool tests() {
   std::deque<int> queue;
-  for (int i = 0; i < 4098; ++i)
+#if TEST_STD_VER >= 26
+  int bound = TEST_IS_CONSTANT_EVALUATED ? 200 : 4098;
+#else
+  int bound = 4098;
+#endif
+  for (int i = 0; i < bound; ++i)
     queue.push_back(i);
 
   while (queue.size() > 1) {
