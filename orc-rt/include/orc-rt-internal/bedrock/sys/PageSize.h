aa4e90041b3ad8edd87a0bcd1aa0589eafc5099c
Lang Hames <lhames@apple.com>
2026-09-04 08:56:30 +1000

    Fix file header formatting

--- orc-rt/include/orc-rt-internal/bedrock/sys/PageSize.h
+++ orc-rt/include/orc-rt-internal/bedrock/sys/PageSize.h
@@ -1,4 +1,4 @@
-//===- PageSize.h - Host page-size detection ---------------------*- C++ -*-===//
+//===- PageSize.h - Host page-size detection --------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
