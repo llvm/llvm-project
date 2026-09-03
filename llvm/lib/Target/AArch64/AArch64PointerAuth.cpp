704c6cf9be5760f53bdc5587f48b88e0a4a6f460
Jon Roelofs <jonathan_roelofs@apple.com>
2026-09-03 12:34:38 -0700

    combined terminator is only really safe when it's the next instruction

--- llvm/lib/Target/AArch64/AArch64PointerAuth.cpp
+++ llvm/lib/Target/AArch64/AArch64PointerAuth.cpp
@@ -316,7 +316,8 @@ void AArch64PointerAuthImpl::authenticateLR(
   // the DW_CFA_AARCH64_negate_ra_state can't be emitted. Additionally,
   // RET{A,B} requires the SP to match its incoming value on entry to the
   // function.
-  bool TerminatorIsCombinable = TI != MBB.end() &&
+  bool TerminatorIsCombinable = std::next(MBBI) == TI &&
+                                TI != MBB.end() &&
                                 TI->getOpcode() == AArch64::RET &&
                                 ArgumentStackToRestore == 0;
 
