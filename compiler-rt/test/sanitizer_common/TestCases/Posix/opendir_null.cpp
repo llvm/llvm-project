711809ca0d8c8699794026e8d9b0e8b0507dc570
Sharon Xu <sharonxu@fb.com>
2026-09-04 13:37:48 -0700

    address comment

--- compiler-rt/test/sanitizer_common/TestCases/Posix/opendir_null.cpp
+++ compiler-rt/test/sanitizer_common/TestCases/Posix/opendir_null.cpp
@@ -1,16 +1,19 @@
 // REQUIRES: linux
 
-// RUN: %clangxx -fno-sanitize=all -DSHARED_LIB -fPIC -shared %s -o %t.so
-// RUN: %clangxx %s -o %t
-// RUN: env LD_PRELOAD=%t.so %run %t
+// RUN: split-file %s %t
+// RUN: %clangxx -fno-sanitize=all -fPIC -shared %t/shared.cpp -o %t/shared.so
+// RUN: %clangxx %t/main.cpp -o %t/main
+// RUN: env LD_PRELOAD=%t/shared.so %run %t/main
 
+//--- shared.cpp
 #include <dirent.h>
 
-#if defined(SHARED_LIB)
 extern "C" DIR *opendir(const char *) { return nullptr; }
-#else
+
+//--- main.cpp
+#include <dirent.h>
+
 int main(int argc, char **argv) {
   const char *path = argc > 1 ? argv[1] : nullptr;
   return opendir(path) != nullptr;
 }
-#endif
