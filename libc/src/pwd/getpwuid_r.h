0a9c408adc3f4cb5dfa6fe90871c96ed87f97a0b
Jeff Bailey <jbailey@raspberryginger.com>
2026-09-04 08:59:01 +0100

    [libc] Add getpwnam_r and getpwuid_r entrypoints (#220833)
    Added reentrant getpwnam_r and getpwuid_r functions using the
    FlatFileDatabase lookup engine.
    
    * Implemented getpwnam_r and getpwuid_r entrypoints
    * Added find_by_name and find_by_uid lookups under namespace passwd
    * Added function specifications to include/pwd.yaml
    * Registered entrypoints in config/linux/*/entrypoints.txt
    * Added unit tests in libc/test/src/pwd/getpwnam_r_test.cpp and
    libc/test/src/pwd/getpwuid_r_test.cpp
    
    Assisted-by: Automated tooling, human reviewed.

--- libc/src/pwd/getpwuid_r.h
+++ libc/src/pwd/getpwuid_r.h
@@ -7,21 +7,24 @@
 //===----------------------------------------------------------------------===//
 ///
 /// \file
-/// Implementation of endpwent.
+/// Header file for getpwuid_r function.
 ///
 //===----------------------------------------------------------------------===//
 
-#include "src/pwd/endpwent.h"
-#include "src/__support/common.h"
-#include "src/__support/libc_errno.h"
-#include "src/pwd/pwd_utils.h"
+#ifndef LLVM_LIBC_SRC_PWD_GETPWUID_R_H
+#define LLVM_LIBC_SRC_PWD_GETPWUID_R_H
+
+#include "hdr/types/size_t.h"
+#include "hdr/types/struct_passwd.h"
+#include "hdr/types/uid_t.h"
+#include "src/__support/macros/config.h"
 
 namespace LIBC_NAMESPACE_DECL {
 
-LLVM_LIBC_FUNCTION(void, endpwent, ()) {
-  auto res = pwd::close();
-  if (!res.has_value())
-    libc_errno = res.error();
-}
+// Searches the password database for an entry with the matching user ID.
+int getpwuid_r(uid_t uid, struct passwd *pwd, char *buffer, size_t bufsize,
+               struct passwd **result);
 
 } // namespace LIBC_NAMESPACE_DECL
+
+#endif // LLVM_LIBC_SRC_PWD_GETPWUID_R_H
