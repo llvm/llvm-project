a181f3219ea411729a5cac13064b75169e81ca02
Ambrose Leeb <aleeb@nvidia.com>
2026-09-04 20:52:15 +0200

    Reapply "[Clang] Fix a number of issues involving expansion statements" (#220375)
    This reverts commit 7c44c505cd7688a8994779f1751e0f9b4b95ab4c.

--- clang/test/SemaCXX/expansion-statements-local-extern-decls.cpp
+++ clang/test/SemaCXX/expansion-statements-local-extern-decls.cpp
@@ -3,21 +3,19 @@
 int wibble(); // #wibble_decl
 
 void foo1() {
-  template for (auto x : {1}) { // #foo1_instantiation
+  template for (auto x : {1}) {
     void wibble();
     // expected-error@-1 {{functions that differ only in their return type cannot be overloaded}}
     // expected-note@#wibble_decl {{previous declaration is here}}
-    // expected-note@#foo1_instantiation {{in instantiation of expansion statement requested here}}
   }
 }
 
 void foo2() {
-  template for (auto x : {1}) { // #foo2_instantiation
+  template for (auto x : {1}) {
     template for (auto x : {1}) {
       void wibble();
       // expected-error@-1 {{functions that differ only in their return type cannot be overloaded}}
       // expected-note@#wibble_decl {{previous declaration is here}}
-      // expected-note@#foo2_instantiation {{in instantiation of expansion statement requested here}}
     }
   }
 }
@@ -25,21 +23,19 @@ void foo2() {
 int woffle; // #woffle_decl
 
 void foo3() {
-  template for (auto x : {1}) { // #foo3_instantiation
+  template for (auto x : {1}) {
     extern double woffle;
     // expected-error@-1 {{redeclaration of 'woffle' with a different type: 'double' vs 'int'}}
     // expected-note@#woffle_decl {{previous definition is here}}
-    // expected-note@#foo3_instantiation {{in instantiation of expansion statement requested here}}
   }
 }
 
 void foo4() {
-  template for (auto x : {1}) { // #foo4_instantiation
+  template for (auto x : {1}) {
     template for (auto x : {1}) {
       extern double woffle;
       // expected-error@-1 {{redeclaration of 'woffle' with a different type: 'double' vs 'int'}}
       // expected-note@#woffle_decl {{previous definition is here}}
-      // expected-note@#foo4_instantiation {{in instantiation of expansion statement requested here}}
     }
   }
 }
