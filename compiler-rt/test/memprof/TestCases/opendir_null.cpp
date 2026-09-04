a4899d663d9044cf314525f8b827d7dc96a43154
Sharon Xu <sharonxu@fb.com>
2026-09-04 11:14:05 -0700

    [memprof] check path in opendir interceptor

---
+++ compiler-rt/test/memprof/TestCases/opendir_null.cpp
@@ -0,0 +1,16 @@
+// REQUIRES: linux
+
+// RUN: %clangxx -DSHARED_LIB -fPIC -shared %s -o %t.so
+// RUN: %clangxx_memprof %s -o %t
+// RUN: env LD_PRELOAD=%t.so %run %t
+
+#include <dirent.h>
+
+#if defined(SHARED_LIB)
+extern "C" DIR *opendir(const char *) { return nullptr; }
+#else
+int main(int argc, char **argv) {
+  const char *path = argc > 1 ? argv[1] : nullptr;
+  return opendir(path) != nullptr;
+}
+#endif
