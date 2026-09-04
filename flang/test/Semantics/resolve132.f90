7425f9bfa88ce481bbacb0550170dede09a7ec9f
Shashwathi N <shashwathinavada@gmail.com>
2026-09-03 23:42:44 -0500

    Testcase added

--- flang/test/Semantics/resolve132.f90
+++ flang/test/Semantics/resolve132.f90
@@ -29,4 +29,20 @@ module parent_mod_9f
      inside_func = 0
    end function
  end submodule
- 
+
+module m2
+  interface
+    module subroutine sub()
+    end subroutine
+  end interface
+end module
+
+submodule (m2) s2
+contains
+   !PORTABILITY: Subprogram 'sub' in this submodule is missing the MODULE prefix to implement the module procedure interface from its parent; did you mean 'MODULE SUBROUTINE'? [-Wportability]
+  integer function sub()
+    sub = 2
+  end function
+end submodule
+Program abc
+end
