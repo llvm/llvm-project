1d39ce8e994ffbb324038304403a14887e9e402b
GabrielWu2004 <gabrielwu2004@gmail.com>
2026-09-03 22:52:19 -0400

    clang format

--- llvm/lib/Analysis/InstructionSimplify.cpp
+++ llvm/lib/Analysis/InstructionSimplify.cpp
@@ -6541,7 +6541,7 @@ Value *llvm::simplifyUnaryIntrinsic(Intrinsic::ID IID, Value *Op0,
       return Op0;
 
     if (KnownClass.cannotBeOrderedLessThanZero() &&
-        KnownClass.isKnownNeverNaN() && FMF.noSignedZeros() && 
+        KnownClass.isKnownNeverNaN() && FMF.noSignedZeros() &&
         KnownClass.isKnownNeverNegZero())
       return Op0;
 
