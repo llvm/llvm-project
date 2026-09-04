carbon-language/carbon-lang

commit ca7f5bb7674faef830dcb668dcd91d17f51f01b4

Full/partial tile separation for vectorization
We isolate full tiles from partial tiles to be able to, for example, vectorize
loops with parametric lower and/or upper bounds.

If we use -polly-vectorizer=stripmine, we can see execution-time improvements:
correlation from 1m7361s to 0m5720s (-67.05 %), covariance from 1m5561s to
0m5680s (-63.50 %), ary3 from 2m3201s to 1m2361s (-46.72 %), CrystalMk from
8m5565s to 7m4285s (-13.18 %).

The current full/partial tile separation increases compile-time more than
necessary. As a result, we see in compile time regressions, for example, for 3mm
from 0m6320s to 0m9881s (56.34%). Some of this compile time increase is expected
as we generate more IR and consequently more time is spent in the LLVM backends.
However, a first investiagation has shown that a larger portion of compile time
is unnecessarily spent inside Polly's parallelism detection and could be
eliminated by propagating existing knowledge about vector loop parallelism.
Before enabling -polly-vectorizer=stripmine by default, it is necessary to
address this compile-time issue.

Contributed-by: Roman Gareev <gareevroman@gmail.com>

Reviewers: jdoerfert, grosser

Subscribers: grosser, #polly

Differential Revision: http://reviews.llvm.org/D13779

llvm-svn: 250809

polly/lib/Transform/ScheduleOptimizer.cpp
@@ -160,6 +160,104 @@ static cl::list<int>
                       cl::Hidden, cl::ZeroOrMore, cl::CommaSeparated,
                       cl::cat(PollyCategory));
 
+/// @brief Create an isl_union_set, which describes the isolate option based
+///        on IsoalteDomain.
+///
+/// @param IsolateDomain An isl_set whose last dimension is the only one that
+///                      should belong to the current band node.
+static __isl_give isl_union_set *
+getIsolateOptions(__isl_take isl_set *IsolateDomain) {
+  auto Dims = isl_set_dim(IsolateDomain, isl_dim_set);
+  auto *IsolateRelation = isl_map_from_domain(IsolateDomain);
+  IsolateRelation = isl_map_move_dims(IsolateRelation, isl_dim_out, 0,
+                                      isl_dim_in, Dims - 1, 1);
+  auto *IsolateOption = isl_map_wrap(IsolateRelation);
+  auto *Id = isl_id_alloc(isl_set_get_ctx(IsolateOption), "isolate", NULL);
+  return isl_union_set_from_set(isl_set_set_tuple_id(IsolateOption, Id));
+}
+
+/// @brief Create an isl_union_set, which describes the atomic option for the
+///        dimension of the current node.
+///
+/// It may help to reduce the size of generated code.
+///
+/// @param Ctx An isl_ctx, which is used to create the isl_union_set.
+static __isl_give isl_union_set *getAtomicOptions(__isl_take isl_ctx *Ctx) {
+  auto *Space = isl_space_set_alloc(Ctx, 0, 1);
+  auto *AtomicOption = isl_set_universe(Space);
+  auto *Id = isl_id_alloc(Ctx, "atomic", NULL);
+  return isl_union_set_from_set(isl_set_set_tuple_id(AtomicOption, Id));
+}
+
+/// @brief Make the last dimension of Set to take values
+///        from 0 to VectorWidth - 1.
+///
+/// @param Set         A set, which should be modified.
+/// @param VectorWidth A parameter, which determines the constraint.
+static __isl_give isl_set *addExtentConstraints(__isl_take isl_set *Set,
+                                                int VectorWidth) {
+  auto Dims = isl_set_dim(Set, isl_dim_set);
+  auto Space = isl_set_get_space(Set);
+  auto *LocalSpace = isl_local_space_from_space(Space);
+  auto *ExtConstr =
+      isl_constraint_alloc_inequality(isl_local_space_copy(LocalSpace));
+  ExtConstr = isl_constraint_set_constant_si(ExtConstr, 0);
+  ExtConstr =
+      isl_constraint_set_coefficient_si(ExtConstr, isl_dim_set, Dims - 1, 1);
+  Set = isl_set_add_constraint(Set, ExtConstr);
+  ExtConstr = isl_constraint_alloc_inequality(LocalSpace);
+  ExtConstr = isl_constraint_set_constant_si(ExtConstr, VectorWidth - 1);
+  ExtConstr =
+      isl_constraint_set_coefficient_si(ExtConstr, isl_dim_set, Dims - 1, -1);
+  return isl_set_add_constraint(Set, ExtConstr);
+}
+
+/// @brief Build the desired set of partial tile prefixes.
+///
+/// We build a set of partial tile prefixes, which are prefixes of the vector
+/// loop that have exactly VectorWidth iterations.
+///
+/// 1. Get all prefixes of the vector loop.
+/// 2. Extend it to a set, which has exactly VectorWidth iterations for
+///    any prefix from the set that was built on the previous step.
+/// 3. Subtract loop domain from it, project out the vector loop dimension and
+///    get a set of prefixes, which don’t have exactly VectorWidth iterations.
+/// 4. Subtract it from all prefixes of the vector loop and get the desired
+///    set.
+///
+/// @param ScheduleRange A range of a map, which describes a prefix schedule
+///                      relation.
+static __isl_give isl_set *
+getPartialTilePrefixes(__isl_take isl_set *ScheduleRange, int VectorWidth) {
+  auto Dims = isl_set_dim(ScheduleRange, isl_dim_set);
+  auto *LoopPrefixes = isl_set_project_out(isl_set_copy(ScheduleRange),
+                                           isl_dim_set, Dims - 1, 1);
+  auto *ExtentPrefixes =
+      isl_set_add_dims(isl_set_copy(LoopPrefixes), isl_dim_set, 1);
+  ExtentPrefixes = addExtentConstraints(ExtentPrefixes, VectorWidth);
+  auto *BadPrefixes = isl_set_subtract(ExtentPrefixes, ScheduleRange);
+  BadPrefixes = isl_set_project_out(BadPrefixes, isl_dim_set, Dims - 1, 1);
+  return isl_set_subtract(LoopPrefixes, BadPrefixes);
+}
+
+__isl_give isl_schedule_node *ScheduleTreeOptimizer::isolateFullPartialTiles(
+    __isl_take isl_schedule_node *Node, int VectorWidth) {
+  assert(isl_schedule_node_get_type(Node) == isl_schedule_node_band);
+  Node = isl_schedule_node_child(Node, 0);
+  Node = isl_schedule_node_child(Node, 0);
+  auto *SchedRelUMap = isl_schedule_node_get_prefix_schedule_relation(Node);
+  auto *ScheduleRelation = isl_map_from_union_map(SchedRelUMap);
+  auto *ScheduleRange = isl_map_range(ScheduleRelation);
+  auto *IsolateDomain = getPartialTilePrefixes(ScheduleRange, VectorWidth);
+  auto *AtomicOption = getAtomicOptions(isl_set_get_ctx(IsolateDomain));
+  auto *IsolateOption = getIsolateOptions(IsolateDomain);
+  Node = isl_schedule_node_parent(Node);
+  Node = isl_schedule_node_parent(Node);
+  auto *Options = isl_union_set_union(IsolateOption, AtomicOption);
+  Node = isl_schedule_node_band_set_ast_build_options(Node, Options);
+  return Node;
+}
+
 __isl_give isl_schedule_node *
 ScheduleTreeOptimizer::prevectSchedBand(__isl_take isl_schedule_node *Node,
                                         unsigned DimToVectorize,
@@ -183,6 +281,7 @@ ScheduleTreeOptimizer::prevectSchedBand(__isl_take isl_schedule_node *Node,
   Sizes =
       isl_multi_val_set_val(Sizes, 0, isl_val_int_from_si(Ctx, VectorWidth));
   Node = isl_schedule_node_band_tile(Node, Sizes);
+  Node = isolateFullPartialTiles(Node, VectorWidth);
   Node = isl_schedule_node_child(Node, 0);
   // Make sure the "trivially vectorizable loop" is not unrolled. Otherwise,
   // we will have troubles to match it in the backend.

polly/include/polly/ScheduleOptimizer.h
@@ -64,6 +64,17 @@ public:
   static bool isProfitableSchedule(polly::Scop &S,
                                    __isl_keep isl_union_map *NewSchedule);
 
+  /// @brief Isolate a set of partial tile prefixes.
+  ///
+  /// This set should ensure that it contains only partial tile prefixes that
+  /// have exactly VectorWidth iterations.
+  ///
+  /// @param Node A schedule node band, which is a parent of a band node,
+  ///             that contains a vector loop.
+  /// @return Modified isl_schedule_node.
+  static __isl_give isl_schedule_node *
+  isolateFullPartialTiles(__isl_take isl_schedule_node *Node, int VectorWidth);
+
 private:
   /// @brief Tile a schedule node.
   ///

polly/test/ScheduleOptimizer/full_partial_tile_separation.ll
@@ -1,0 +1,92 @@
+; RUN: opt -S %loadPolly -polly-vectorizer=stripmine -polly-opt-isl -polly-ast -analyze < %s | FileCheck %s
+
+; CHECK:       // 1st level tiling - Tiles
+; CHECK:       #pragma known-parallel
+; CHECK:       for (int c0 = 0; c0 <= floord(ni - 1, 32); c0 += 1)
+; CHECK:         for (int c1 = 0; c1 <= floord(nj - 1, 32); c1 += 1)
+; CHECK:           for (int c2 = 0; c2 <= floord(nk - 1, 32); c2 += 1) {
+; CHECK:             // 1st level tiling - Points
+; CHECK:             for (int c3 = 0; c3 <= min(31, ni - 32 * c0 - 1); c3 += 1) {
+; CHECK:               for (int c4 = 0; c4 <= min(7, -8 * c1 + nj / 4 - 1); c4 += 1)
+; CHECK:                 for (int c5 = 0; c5 <= min(31, nk - 32 * c2 - 1); c5 += 1)
+; CHECK:                   #pragma simd
+; CHECK:                   for (int c6 = 0; c6 <= 3; c6 += 1)
+; CHECK:                     Stmt_for_body_6(32 * c0 + c3, 32 * c1 + 4 * c4 + c6, 32 * c2 + c5);
+; CHECK:               if (nj >= 32 * c1 + 4 && 32 * c1 + 31 >= nj) {
+; CHECK:                 for (int c5 = 0; c5 <= min(31, nk - 32 * c2 - 1); c5 += 1)
+; CHECK:                   #pragma simd
+; CHECK:                   for (int c6 = 0; c6 < nj % 4; c6 += 1)
+; CHECK:                     Stmt_for_body_6(32 * c0 + c3, -((nj - 1) % 4) + nj + c6 - 1, 32 * c2 + c5);
+; CHECK:               } else if (32 * c1 + 3 >= nj)
+; CHECK:                 for (int c5 = 0; c5 <= min(31, nk - 32 * c2 - 1); c5 += 1)
+; CHECK:                   #pragma simd
+; CHECK:                   for (int c6 = 0; c6 < nj - 32 * c1; c6 += 1)
+; CHECK:                     Stmt_for_body_6(32 * c0 + c3, 32 * c1 + c6, 32 * c2 + c5);
+; CHECK:             }
+; CHECK:           }
+
+; Function Attrs: nounwind uwtable
+define void @kernel_gemm(i32 %ni, i32 %nj, i32 %nk, double %alpha, double %beta, [1024 x double]* %C, [1024 x double]* %A, [1024 x double]* %B) #0 {
+entry:
+  %cmp.27 = icmp sgt i32 %ni, 0
+  br i1 %cmp.27, label %for.cond.1.preheader.lr.ph, label %for.end.22
+
+for.cond.1.preheader.lr.ph:                       ; preds = %entry
+  br label %for.cond.1.preheader
+
+for.cond.1.preheader:                             ; preds = %for.cond.1.preheader.lr.ph, %for.inc.20
+  %indvars.iv33 = phi i64 [ 0, %for.cond.1.preheader.lr.ph ], [ %indvars.iv.next34, %for.inc.20 ]
+  %cmp2.25 = icmp sgt i32 %nj, 0
+  br i1 %cmp2.25, label %for.cond.4.preheader.lr.ph, label %for.inc.20
+
+for.cond.4.preheader.lr.ph:                       ; preds = %for.cond.1.preheader
+  br label %for.cond.4.preheader
+
+for.cond.4.preheader:                             ; preds = %for.cond.4.preheader.lr.ph, %for.inc.17
+  %indvars.iv29 = phi i64 [ 0, %for.cond.4.preheader.lr.ph ], [ %indvars.iv.next30, %for.inc.17 ]
+  %cmp5.23 = icmp sgt i32 %nk, 0
+  br i1 %cmp5.23, label %for.body.6.lr.ph, label %for.inc.17
+
+for.body.6.lr.ph:                                 ; preds = %for.cond.4.preheader
+  br label %for.body.6
+
+for.body.6:                                       ; preds = %for.body.6.lr.ph, %for.body.6
+  %indvars.iv = phi i64 [ 0, %for.body.6.lr.ph ], [ %indvars.iv.next, %for.body.6 ]
+  %arrayidx8 = getelementptr inbounds [1024 x double], [1024 x double]* %A, i64 %indvars.iv33, i64 %indvars.iv
+  %0 = load double, double* %arrayidx8, align 8
+  %arrayidx12 = getelementptr inbounds [1024 x double], [1024 x double]* %B, i64 %indvars.iv, i64 %indvars.iv29
+  %1 = load double, double* %arrayidx12, align 8
+  %mul = fmul double %0, %1
+  %arrayidx16 = getelementptr inbounds [1024 x double], [1024 x double]* %C, i64 %indvars.iv33, i64 %indvars.iv29
+  %2 = load double, double* %arrayidx16, align 8
+  %add = fadd double %2, %mul
+  store double %add, double* %arrayidx16, align 8
+  %indvars.iv.next = add nuw nsw i64 %indvars.iv, 1
+  %lftr.wideiv = trunc i64 %indvars.iv.next to i32
+  %exitcond = icmp ne i32 %lftr.wideiv, %nk
+  br i1 %exitcond, label %for.body.6, label %for.cond.4.for.inc.17_crit_edge
+
+for.cond.4.for.inc.17_crit_edge:                  ; preds = %for.body.6
+  br label %for.inc.17
+
+for.inc.17:                                       ; preds = %for.cond.4.for.inc.17_crit_edge, %for.cond.4.preheader
+  %indvars.iv.next30 = add nuw nsw i64 %indvars.iv29, 1
+  %lftr.wideiv31 = trunc i64 %indvars.iv.next30 to i32
+  %exitcond32 = icmp ne i32 %lftr.wideiv31, %nj
+  br i1 %exitcond32, label %for.cond.4.preheader, label %for.cond.1.for.inc.20_crit_edge
+
+for.cond.1.for.inc.20_crit_edge:                  ; preds = %for.inc.17
+  br label %for.inc.20
+
+for.inc.20:                                       ; preds = %for.cond.1.for.inc.20_crit_edge, %for.cond.1.preheader
+  %indvars.iv.next34 = add nuw nsw i64 %indvars.iv33, 1
+  %lftr.wideiv35 = trunc i64 %indvars.iv.next34 to i32
+  %exitcond36 = icmp ne i32 %lftr.wideiv35, %ni
+  br i1 %exitcond36, label %for.cond.1.preheader, label %for.cond.for.end.22_crit_edge
+
+for.cond.for.end.22_crit_edge:                    ; preds = %for.inc.20
+  br label %for.end.22
+
+for.end.22:                                       ; preds = %for.cond.for.end.22_crit_edge, %entry
+  ret void
+}

llvm/llvm-project

commit 12dab69c125801673c37e811a54d89b7002843f6

[Polly] Isolate full tiles from partial tiles in the tiling path
Tiling an iteration space whose extent is not a multiple of the tile size
gives every point loop a min() upper bound. The loop vectorizer cannot commit
to a vector factor for such a loop without a runtime check, and it has to emit
a scalar epilogue inside the tile loop, so the loops that carry the whole
runtime of a tiled kernel are the ones it handles worst.

Isolating the full tiles from the partial ones gives the point loops of the
full tiles constant bounds. Polly already does this for the loop it strip-mines
in the prevectorization path, added in commit ca7f5bb7674f ("Full/partial tile
separation for vectorization"); this applies the same technique to the loops
produced by the regular tiling. getPartialTilePrefixes is generalized to
several point dimensions as getFullTilePrefixes, and the resulting isolate
option, combined with atomic for the remaining part, is attached to the tile
band.

Two options are added, both leaving the default behaviour unchanged:

 - -polly-isolate-full-tiles enables the separation. It is off by default
   because it grows the generated code considerably.
 - -polly-isolate-complete-tile-dims limits how many innermost dimensions of a
   tile have to be complete. Requiring fewer of them bounds the number of
   copies of the loop nest, at the price of leaving the outer point loops with
   a min() bound.

On PolyBench/C 4.2.1 with -O3 -march=native -mllvm -polly -mllvm
-polly-vectorizer=none on an AMD Ryzen 7 9700X, minimum of 5 to 41 runs per
kernel pinned to a single core, the separation changes the code of 23 of the 30
kernels and improves their geometric mean by 37.5%:

  2mm 0.146, 3mm 0.157, gemm 0.159, covariance 0.241, syrk 0.384,
  jacobi-1d 0.470, mvt 0.668, bicg 0.672, atax 0.673, gemver 0.691,
  jacobi-2d 0.744, symm 0.805, lu 0.844

Kernels whose code the option does not change give the noise floor:
floyd-warshall 0.993, nussinov 1.001, trisolv 0.991.

Two kernels regress: gesummv by 16% and deriche by 13%. In both the vectorizer
keeps the same vector factor and only gets more copies of the loop, so the
added code buys nothing: gesummv streams 27 MB at 24 GB/s against a measured
single-core peak of 29.8 GB/s, and deriche is dominated by eleven loops that
carry a dependence and never vectorize. Restricting the isolation to the
innermost dimension does not help them either (1.178 and 1.125), while it costs
performance elsewhere (geometric mean 0.698 instead of 0.625, with syrk falling
back from 0.384 to 0.964), which is why completeness in every dimension is the
default.

The cost is code size and compile time: .text of the benchmark binaries grows
by 81% to 384%, and compiling a single kernel takes 31% to 96% longer, which
mirrors the compile-time regression reported in ca7f5bb7674f. Requiring only

polly/include/polly/ScheduleTreeTransform.h
@@ -234,6 +234,24 @@ isl::schedule applyMaxFission(isl::schedule_node BandToFission);
 ///                      relation.
 isl::set getPartialTilePrefixes(isl::set ScheduleRange, int VectorWidth);
 
+/// Compute the prefixes of the complete tiles of a tiled band.
+///
+/// A tile is complete if every point of it belongs to @p ScheduleRange; those
+/// are the tiles whose point loops can be given constant bounds by isolating
+/// them from the partial tiles at the boundary of the iteration space.
+///
+/// @param ScheduleRange   A range of a map, which describes a prefix schedule
+///                        relation whose last @p TileSizes.size() dimensions
+///                        are the point dimensions of the tiling.
+/// @param TileSizes       The tile size of each tiled dimension.
+/// @param NumCompleteDims The number of innermost point dimensions that have to
+///                        be complete. Requiring fewer than all of them
+///                        isolates more tiles, but bounds the number of copies
+///                        of the loop nest that the AST generator creates.
+isl::set getFullTilePrefixes(isl::set ScheduleRange,
+                             llvm::ArrayRef<int> TileSizes,
+                             unsigned NumCompleteDims);
+
 /// Create an isl::union_set, which describes the isolate option based on
 /// IsolateDomain.
 ///

polly/lib/Transform/ScheduleOptimizer.cpp
@@ -171,6 +171,20 @@ static cl::list<int>
                                "with --polly-register-tile-size"),
                       cl::Hidden, cl::CommaSeparated, cl::cat(PollyCategory));
 
+static cl::opt<bool> IsolateFullTiles(
+    "polly-isolate-full-tiles",
+    cl::desc("Separate the full tiles of a tiled band from the partial ones, "
+             "so that the point loops of the full tiles have constant bounds"),
+    cl::Hidden, cl::init(false), cl::cat(PollyCategory));
+
+static cl::opt<int> IsolateCompleteTileDims(
+    "polly-isolate-complete-tile-dims",
+    cl::desc("Number of innermost tile dimensions that have to be complete for "
+             "a tile to be isolated (0: all of them). Requiring fewer of them "
+             "generates less code, but also gives fewer point loops a constant "
+             "bound"),
+    cl::Hidden, cl::init(0), cl::cat(PollyCategory));
+
 static cl::opt<bool> PragmaBasedOpts(
     "polly-pragma-based-opts",
     cl::desc("Apply user-directed transformation from metadata"),
@@ -405,6 +419,56 @@ ScheduleTreeOptimizer::isolateFullPartialTiles(isl::schedule_node Node,
   return Result;
 }
 
+/// Separate the full tiles of a tiled band from the partial ones.
+///
+/// The point loops of a full tile run over the whole tile, so isolating those
+/// tiles gives them constant loop bounds instead of the min() expressions that
+/// a tiling of an iteration space which is not a multiple of the tile size
+/// produces. The partial tiles are left to a single atomic copy of the loop
+/// nest to keep the code growth bounded.
+///
+/// @param Node      The point band of the tiling, as returned by tileNode.
+/// @param TileSizes The tile size of each tiled dimension.
+/// @return          The point band of the modified tree.
+static isl::schedule_node isolateFullTiles(isl::schedule_node Node,
+                                           ArrayRef<int> TileSizes) {
+  assert(isl_schedule_node_get_type(Node.get()) == isl_schedule_node_band);
+
+  // Below the point band, the prefix schedule covers the outer dimensions
+  // followed by the tile and the point dimensions of this tiling.
+  isl::union_set ScheduleRangeUSet =
+      Node.child(0).get_prefix_schedule_relation().range();
+  isl::set ScheduleRange{ScheduleRangeUSet};
+  if (ScheduleRange.is_null())
+    return Node;
+
+  unsigned NumCompleteDims = TileSizes.size();
+  if (IsolateCompleteTileDims > 0)
+    NumCompleteDims =
+        std::min<unsigned>(IsolateCompleteTileDims, TileSizes.size());
+
+  isl::set FullTilePrefixes =
+      getFullTilePrefixes(ScheduleRange, TileSizes, NumCompleteDims);
+  if (FullTilePrefixes.is_null())
+    return Node;
+
+  isl::union_set Options = getIsolateOptions(FullTilePrefixes, TileSizes.size())
+                               .unite(getDimOptions(Node.ctx(), "atomic"));
+
+  // The option describes the tile dimensions, so it belongs to the tile band,
+  // which sits above the marker separating it from the point band.
+  isl::schedule_node TileBand = Node.parent().parent();
+  if (!TileBand.isa<isl::schedule_node_band>())
+    return Node;
+
+  TileBand =
+      TileBand.as<isl::schedule_node_band>().set_ast_build_options(Options);
+  if (TileBand.is_null())
+    return Node;
+
+  return TileBand.child(0).child(0);
+}
+
 struct InsertSimdMarkers final : ScheduleNodeRewriter<InsertSimdMarkers> {
   isl::schedule_node visitBand(isl::schedule_node_band Band) {
     isl::schedule_node Node = visitChildren(Band);
@@ -528,9 +592,23 @@ bool ScheduleTreeOptimizer::isPMOptimizableBandNode(isl::schedule_node Node) {
 __isl_give isl::schedule_node
 ScheduleTreeOptimizer::applyTileBandOpt(isl::schedule_node Node) {
   if (FirstLevelTiling) {
+    // Resolve the tile size of every dimension before tiling splits the band.
+    SmallVector<int, 4> Sizes;
+    if (IsolateFullTiles) {
+      isl::space Space =
+          isl::manage(isl_schedule_node_band_get_space(Node.get()));
+      for (unsigned i : rangeIslSize(0, Space.dim(isl::dim::set)))
+        Sizes.push_back(i < FirstLevelTileSizes.size()
+                            ? FirstLevelTileSizes[i]
+                            : FirstLevelDefaultTileSize.getValue());
+    }
+
     Node = tileNode(Node, "1st level tiling", FirstLevelTileSizes,
                     FirstLevelDefaultTileSize);
     FirstLevelTileOpts++;
+
+    if (IsolateFullTiles)
+      Node = isolateFullTiles(Node, Sizes);
   }
 
   if (SecondLevelTiling) {

polly/lib/Transform/ScheduleTreeTransform.cpp
@@ -1127,6 +1127,65 @@ isl::set polly::getPartialTilePrefixes(isl::set ScheduleRange,
   return LoopPrefixes.subtract(BadPrefixes);
 }
 
+/// Restrict the last @p Sizes.size() dimensions of @p Set to a complete tile,
+/// that is, to 0 <= dim <= size - 1 for each of them.
+static isl::set addTileExtentConstraints(isl::set Set,
+                                         llvm::ArrayRef<int> Sizes) {
+  unsigned Dims = unsignedFromIslSize(Set.tuple_dim());
+  unsigned NumTileDims = Sizes.size();
+  assert(Dims >= NumTileDims);
+  isl::local_space LocalSpace = isl::local_space(Set.get_space());
+
+  for (unsigned i = 0; i < NumTileDims; ++i) {
+    unsigned Pos = Dims - NumTileDims + i;
+
+    isl::constraint LowerBound = isl::constraint::alloc_inequality(LocalSpace);
+    LowerBound = LowerBound.set_constant_si(0);
+    LowerBound = LowerBound.set_coefficient_si(isl::dim::set, Pos, 1);
+    Set = Set.add_constraint(LowerBound);
+
+    isl::constraint UpperBound = isl::constraint::alloc_inequality(LocalSpace);
+    UpperBound = UpperBound.set_constant_si(Sizes[i] - 1);
+    UpperBound = UpperBound.set_coefficient_si(isl::dim::set, Pos, -1);
+    Set = Set.add_constraint(UpperBound);
+  }
+
+  return Set;
+}
+
+isl::set polly::getFullTilePrefixes(isl::set ScheduleRange,
+                                    llvm::ArrayRef<int> TileSizes,
+                                    unsigned NumCompleteDims) {
+  unsigned Dims = unsignedFromIslSize(ScheduleRange.tuple_dim());
+  unsigned NumTileDims = TileSizes.size();
+  assert(Dims >= NumTileDims);
+  assert(NumCompleteDims >= 1 && NumCompleteDims <= NumTileDims);
+
+  // Same idea as getPartialTilePrefixes, but keeping the prefixes of the
+  // complete tiles instead of the incomplete ones: over-approximate the
+  // schedule range in the dimensions whose completeness is required, add the
+  // constraints of a complete tile there, and drop every prefix for which that
+  // tile leaves the schedule range.
+  //
+  // Requiring completeness in fewer than all point dimensions leaves the outer
+  // tile dimensions unconstrained in the result, so the AST generator has to
+  // separate the isolated part along the innermost dimensions only. That trades
+  // some of the constant loop bounds for a smaller number of copies of the loop
+  // nest.
+  isl::set Relaxed = ScheduleRange.drop_constraints_involving_dims(
+      isl::dim::set, Dims - NumCompleteDims, NumCompleteDims);
+  Relaxed =
+      addTileExtentConstraints(Relaxed, TileSizes.take_back(NumCompleteDims));
+
+  isl::set BadPrefixes = Relaxed.subtract(ScheduleRange);
+  BadPrefixes =
+      BadPrefixes.project_out(isl::dim::set, Dims - NumTileDims, NumTileDims);
+  isl::set LoopPrefixes =
+      ScheduleRange.project_out(isl::dim::set, Dims - NumTileDims, NumTileDims);
+
+  return LoopPrefixes.subtract(BadPrefixes);
+}
+
 isl::union_set polly::getIsolateOptions(isl::set IsolateDomain,
                                         unsigned OutDimsNum) {
   if (IsolateDomain.is_null())

polly/test/ScheduleOptimizer/isolate-full-tiles.ll
@@ -1,0 +1,111 @@
+; RUN: opt %loadNPMPolly '-passes=polly-custom<opt-isl;ast>' -polly-print-ast \
+; RUN:     -disable-output < %s | FileCheck %s --check-prefix=DEFAULT
+; RUN: opt %loadNPMPolly -polly-isolate-full-tiles \
+; RUN:     '-passes=polly-custom<opt-isl;ast>' -polly-print-ast \
+; RUN:     -disable-output < %s | FileCheck %s --check-prefix=ALL
+; RUN: opt %loadNPMPolly -polly-isolate-full-tiles \
+; RUN:     -polly-isolate-complete-tile-dims=1 \
+; RUN:     '-passes=polly-custom<opt-isl;ast>' -polly-print-ast \
+; RUN:     -disable-output < %s | FileCheck %s --check-prefix=INNERMOST
+;
+;    void foo(float *A, float *B) {
+;      for (long i = 0; i < 100; i++)
+;        for (long j = 0; j < 100; j++)
+;          A[100 * i + j] = B[100 * i + j] + 1;
+;    }
+;
+; 100 iterations tiled by 32 leave a partial tile of four in each dimension.
+;
+; Without isolation there is a single copy of the loop nest and both point
+; loops carry a min() upper bound.
+;
+; DEFAULT:      // 1st level tiling - Tiles
+; DEFAULT-NEXT: for (int c0 = 0; c0 <= 3; c0 += 1)
+; DEFAULT-NEXT:   for (int c1 = 0; c1 <= 3; c1 += 1) {
+; DEFAULT-NEXT:     // 1st level tiling - Points
+; DEFAULT-NEXT:     for (int c2 = 0; c2 <= min(31, -32 * c0 + 99); c2 += 1)
+; DEFAULT-NEXT:       for (int c3 = 0; c3 <= min(31, -32 * c1 + 99); c3 += 1)
+; DEFAULT-NEXT:         Stmt_for_j(32 * c0 + c2, 32 * c1 + c3);
+; DEFAULT-NOT:  // 1st level tiling - Points
+;
+; With isolation the complete tiles are separated from the two tails. The nine
+; complete tiles have constant bounds in both point loops.
+;
+; ALL:      // 1st level tiling - Tiles
+; ALL:      for (int c0 = 0; c0 <= 2; c0 += 1) {
+; ALL-NEXT:   for (int c1 = 0; c1 <= 2; c1 += 1) {
+; ALL-NEXT:     // 1st level tiling - Points
+; ALL-NEXT:     for (int c2 = 0; c2 <= 31; c2 += 1)
+; ALL-NEXT:       for (int c3 = 0; c3 <= 31; c3 += 1)
+; ALL-NEXT:         Stmt_for_j(32 * c0 + c2, 32 * c1 + c3);
+;
+; The tail of the second dimension, columns 96 to 99, runs once per complete
+; tile row and keeps its constant bound in the first dimension.
+;
+; ALL:      // 1st level tiling - Points
+; ALL-NEXT: for (int c2 = 0; c2 <= 31; c2 += 1)
+; ALL-NEXT:   for (int c3 = 0; c3 <= 3; c3 += 1)
+; ALL-NEXT:     Stmt_for_j(32 * c0 + c2, c3 + 96);
+;
+; The tail of the first dimension, rows 96 to 99, spans the whole width and is
+; the only part left with a min() bound.
+;
+; ALL:      for (int c1 = 0; c1 <= 3; c1 += 1) {
+; ALL-NEXT:   // 1st level tiling - Points
+; ALL-NEXT:   for (int c2 = 0; c2 <= 3; c2 += 1)
+; ALL-NEXT:     for (int c3 = 0; c3 <= min(31, -32 * c1 + 99); c3 += 1)
+; ALL-NEXT:       Stmt_for_j(c2 + 96, 32 * c1 + c3);
+; ALL-NOT:  // 1st level tiling - Points
+;
+; Requiring only the innermost dimension to be complete separates along that
+; dimension alone. The outer tile loop is not split, so the nest is copied
+; twice instead of three times and only the innermost point loop gets a
+; constant bound.
+;
+; INNERMOST:      // 1st level tiling - Tiles
+; INNERMOST-NEXT: for (int c0 = 0; c0 <= 3; c0 += 1) {
+; INNERMOST-NEXT:   for (int c1 = 0; c1 <= 2; c1 += 1) {
+; INNERMOST-NEXT:     // 1st level tiling - Points
+; INNERMOST-NEXT:     for (int c2 = 0; c2 <= min(31, -32 * c0 + 99); c2 += 1)
+; INNERMOST-NEXT:       for (int c3 = 0; c3 <= 31; c3 += 1)
+; INNERMOST-NEXT:         Stmt_for_j(32 * c0 + c2, 32 * c1 + c3);
+;
+; Its tail keeps the min() bound of the dimension that was left alone.
+;
+; INNERMOST:      // 1st level tiling - Points
+; INNERMOST-NEXT: for (int c2 = 0; c2 <= min(31, -32 * c0 + 99); c2 += 1)
+; INNERMOST-NEXT:   for (int c3 = 0; c3 <= 3; c3 += 1)
+; INNERMOST-NEXT:     Stmt_for_j(32 * c0 + c2, c3 + 96);
+; INNERMOST-NOT:  // 1st level tiling - Points
+
+target datalayout = "e-m:e-i64:64-f80:128-n8:16:32:64-S128"
+
+define void @foo(ptr %A, ptr %B) {
+entry:
+  br label %for.i
+
+for.i:
+  %i = phi i64 [ 0, %entry ], [ %i.next, %for.i.inc ]
+  br label %for.j
+
+for.j:
+  %j = phi i64 [ 0, %for.i ], [ %j.next, %for.j ]
+  %mul = mul nuw nsw i64 %i, 100
+  %idx = add nuw nsw i64 %mul, %j
+  %ptrB = getelementptr inbounds float, ptr %B, i64 %idx
+  %valB = load float, ptr %ptrB
+  %add = fadd float %valB, 1.000000e+00
+  %ptrA = getelementptr inbounds float, ptr %A, i64 %idx
+  store float %add, ptr %ptrA
+  %j.next = add nuw nsw i64 %j, 1
+  %j.cmp = icmp eq i64 %j.next, 100
+  br i1 %j.cmp, label %for.i.inc, label %for.j
+
+for.i.inc:
+  %i.next = add nuw nsw i64 %i, 1
+  %i.cmp = icmp eq i64 %i.next, 100
+  br i1 %i.cmp, label %exit, label %for.i
+
+exit:
+  ret void
+}