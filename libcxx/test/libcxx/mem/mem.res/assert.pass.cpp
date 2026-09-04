e65fd7755ae3d60168b603c405d08381a2637d2f
Nikolas Klauser <nikolasklauser@berlin.de>
2026-09-04 13:15:43 +0200

    [libc++] Assert that the alignment for memory_resource::allocate is a power of two (#219174)
    `do_allocate` has a precondition that the argument is a power of two. We
    can't assert that in general, since users can provide their own
    `do_allocate`s. However, we can assert in `allocate` instead, which has
    the added bonus that it's going to be inlined and the check can then
    most likely be folded away by the compiler.

--- libcxx/test/libcxx/mem/mem.res/assert.pass.cpp
+++ libcxx/test/libcxx/mem/mem.res/assert.pass.cpp
@@ -14,15 +14,25 @@
 // UNSUPPORTED: c++03, c++11, c++14
 
 // We're testing nullptr assertions
-// ADDITIONAL_COMPILE_FLAGS: -Wno-nonnull
+// ADDITIONAL_COMPILE_FLAGS: -Wno-nonnull -Wno-non-power-of-two-alignment
 
+#include <cassert>
 #include <memory_resource>
 
 #include "check_assertion.h"
 
+struct my_memory_resource : std::pmr::memory_resource {
+  void* do_allocate(std::size_t, std::size_t) override { assert(false); }
+  void do_deallocate(void*, std::size_t, std::size_t) override { assert(false); }
+  bool do_is_equal(const std::pmr::memory_resource&) const noexcept override { return false; }
+};
+
 int main(int, char**) {
   TEST_LIBCPP_ASSERT_FAILURE(
       std::pmr::polymorphic_allocator<int>(nullptr), "Attempted to pass a nullptr resource to polymorphic_alloator");
+
+  TEST_LIBCPP_ASSERT_FAILURE(my_memory_resource().allocate(0, 0), "The alignment has to be a power of two");
+  TEST_LIBCPP_ASSERT_FAILURE(my_memory_resource().allocate(0, 33), "The alignment has to be a power of two");
 
   return 0;
 }
