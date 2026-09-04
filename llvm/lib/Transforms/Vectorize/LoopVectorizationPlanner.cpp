86264965c484dcb95076211a68411ddf592e613f
Florian Hahn <flo@fhahn.com>
2026-09-04 11:35:51 +0100

    [LV] Add getMaxRuntimeElementCount helper (NFC). (#220397)
    Add a getMaxRuntimeElementCount to compute ElementCount * max vscale. It computes
    the value in uint64_t, to avoid overflow for very large vscale values.
    
    Returns std::nullopt, if there's no max vscale value. Migrate 3 existing
    users to use the shared helper.
    
    PR: https://github.com/llvm/llvm-project/pull/220397

--- llvm/lib/Transforms/Vectorize/LoopVectorizationPlanner.cpp
+++ llvm/lib/Transforms/Vectorize/LoopVectorizationPlanner.cpp
@@ -312,6 +312,18 @@ std::optional<unsigned> llvm::getMaxVScale(const Function &F,
   return std::nullopt;
 }
 
+std::optional<uint64_t>
+llvm::getMaxRuntimeElementCount(ElementCount EC, const Function &F,
+                                const TargetTransformInfo &TTI) {
+  if (EC.isFixed())
+    return EC.getFixedValue();
+
+  if (std::optional<unsigned> MaxVScale = getMaxVScale(F, TTI))
+    return uint64_t(EC.getKnownMinValue()) * *MaxVScale;
+
+  return std::nullopt;
+}
+
 bool VFSelectionContext::isScalableVectorizationAllowed() {
   if (IsScalableVectorizationAllowed)
     return *IsScalableVectorizationAllowed;
