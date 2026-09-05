486782139c8bd4287a1bf3f3821b00efeb582682
Dave MacLachlan <dmaclach@gmail.com>
2026-09-05 10:49:30 -0700

    Fixed up test to not use `echo -e` which is linux only.

--- lld/test/MachO/warn-missing-subsections-via-symbols.s
+++ lld/test/MachO/warn-missing-subsections-via-symbols.s
@@ -1,9 +1,7 @@
 # REQUIRES: x86
-
-# RUN: rm -rf %t && mkdir -p %t
-# RUN: llvm-mc -filetype=obj -triple=x86_64-apple-macos %s -o %t/no_subsections.o
-# RUN: echo -e ".text\n.globl _bar\n_bar:\nret\n.subsections_via_symbols" | \
-# RUN:     llvm-mc -filetype=obj -triple=x86_64-apple-macos - -o %t/with_subsections.o
+# RUN: rm -rf %t; split-file %s %t
+# RUN: llvm-mc -filetype=obj -triple=x86_64-apple-macos %t/no_subsections.s -o %t/no_subsections.o
+# RUN: llvm-mc -filetype=obj -triple=x86_64-apple-macos %t/with_subsections.s -o %t/with_subsections.o
 
 ## Default: no warning (even under -fatal_warnings)
 # RUN: %lld -dylib %t/no_subsections.o -o /dev/null 2>&1 | count 0
@@ -21,7 +19,15 @@
 
 # WARN: warning: {{.*}}no_subsections.o: missing MH_SUBSECTIONS_VIA_SYMBOLS
 
+#--- no_subsections.s
 .text
 .globl _foo
 _foo:
   ret
+
+#--- with_subsections.s
+.text
+.globl _bar
+_bar:
+  ret
+.subsections_via_symbols
