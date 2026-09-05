3de032cc2f22d0df7be2c30a066701e7ff1d09d7
Abdulmalek Almkainzi <aalmkainzi@gmail.com>
2026-09-05 05:37:24 +0300

    added test for nested colon (enum bit field)

--- clang/test/Parser/c23-enum-generic-assoc.c
+++ clang/test/Parser/c23-enum-generic-assoc.c
@@ -15,5 +15,7 @@ static_assert(_Generic(0L, enum E : long { X } : 1, int: 0), ""); // expected-no
 static_assert(_Generic(0L, enum E : 1, int: 0), ""); // expected-no-diagnostics
 static_assert(_Generic(0L, enum A : l { B } : 1, int: 0), ""); // expected-no-diagnostics
 static_assert(_Generic(0L, struct { enum A : l { B } a : 1; } : 0, long: 1), ""); // expected-no-diagnostics
+static_assert(_Generic(0, struct S { enum F : int {A} : 1; } : 0, int: 1)); // expected-no-diagnostics
+static_assert(_Generic(0, struct S { enum E : 1; } : 0, int: 1)); // expected-no-diagnostics
 
 #endif
