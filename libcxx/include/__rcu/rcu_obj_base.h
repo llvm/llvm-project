37c3891b2a0dabfb9690170ff92674b2a671d8ce
Hui Xie <hui.xie1990@gmail.com>
2026-09-06 09:55:52 +0100

    remove std::function, replace it with function_ref

--- libcxx/include/__rcu/rcu_obj_base.h
+++ libcxx/include/__rcu/rcu_obj_base.h
@@ -10,8 +10,9 @@
 #ifndef _LIBCPP___RCU_RCU_OBJ_BASE_H
 #define _LIBCPP___RCU_RCU_OBJ_BASE_H
 
+#include "__configuration/attributes.h"
 #include <__config>
-#include <__functional/function.h>
+#include <__functional/function_ref.h>
 #include <__memory/unique_ptr.h> // for default_delete
 #include <__rcu/rcu_domain.h>
 #include <__utility/move.h>
@@ -28,12 +29,8 @@ template <class _Tp, class _Dp = default_delete<_Tp>>
 class rcu_obj_base : private __rcu_node {
 public:
   _LIBCPP_HIDE_FROM_ABI void retire(_Dp __deleter = _Dp(), rcu_domain& __dom = rcu_default_domain()) noexcept {
-    auto __ptr = static_cast<_Tp*>(this);
-
-    // todo: std::function can throw on the assignment. Perhaps we can store
-    //       the deleter in the class and either use virtual function or use
-    //       function_ref here.
-    __callback_ = [__ptr, __deleter = std::move(__deleter)]() mutable { __deleter(__ptr); };
+    __deleter_  = std::move(__deleter);
+    __callback_ = function_ref<void()>(std::cw<&rcu_obj_base::__destroy>, this);
     __dom.__retire(this);
   }
 
@@ -44,6 +41,11 @@ protected:
   _LIBCPP_HIDE_FROM_ABI rcu_obj_base& operator=(const rcu_obj_base&) = default;
   _LIBCPP_HIDE_FROM_ABI rcu_obj_base& operator=(rcu_obj_base&&)      = default;
   _LIBCPP_HIDE_FROM_ABI ~rcu_obj_base()                              = default;
+
+private:
+  _LIBCPP_HIDE_FROM_ABI void __destroy() { __deleter_(static_cast<_Tp*>(this)); }
+
+  _LIBCPP_NO_UNIQUE_ADDRESS _Dp __deleter_ = _Dp();
 };
 
 #endif // _LIBCPP_STD_VER >= 26 && _LIBCPP_HAS_THREADS && _LIBCPP_HAS_EXPERIMENTAL_RCU
