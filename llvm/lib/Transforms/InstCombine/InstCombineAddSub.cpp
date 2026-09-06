54c96293fee53e63b4163e191ef038d210d770ab
GabrielWu2004 <gabrielwu2004@gmail.com>
2026-09-06 19:07:48 -0400

    address review comment

--- llvm/lib/Transforms/InstCombine/InstCombineAddSub.cpp
+++ llvm/lib/Transforms/InstCombine/InstCombineAddSub.cpp
@@ -3230,8 +3230,7 @@ Instruction *InstCombinerImpl::visitFNeg(UnaryOperator &I) {
     return replaceInstUsesWith(I, Reverse);
   }
 
-  // fneg (abs x) --> fneg (x) if we can prove that x is positive or -0.0 if nsz
-  // is set.
+  // fneg (fabs x) --> fneg (x) if we can prove that x is nonnegative (or that x is -0.0 when nsz is set)
   if (match(OneUse, m_FAbs(m_Value(X))) &&
       SimplifyDemandedInstructionFPClass(I))
     return &I;
