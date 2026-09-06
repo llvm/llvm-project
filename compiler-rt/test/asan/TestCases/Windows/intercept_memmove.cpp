07697871c0631ef829fb28d7f04c37ff745038d0
Alexandre Ganea <alex_toresh@yahoo.fr>
2026-09-06 12:01:55 -0400

    Fix test.

--- compiler-rt/test/asan/TestCases/Windows/intercept_memmove.cpp
+++ compiler-rt/test/asan/TestCases/Windows/intercept_memmove.cpp
@@ -1,16 +1,6 @@
 // RUN: %clang_cl_asan %Od %s %Fe%t
 // RUN: not %run %t 2>&1 | FileCheck %s
 
-// CHECK: Initial test OK
-// CHECK: AddressSanitizer: stack-buffer-overflow on address [[ADDR:0x[0-9a-f]+]]
-// CHECK: WRITE of size 6 at [[ADDR]] thread T0
-// CHECK-NEXT:  mem{{.*}}
-// CHECK-NEXT:  call_mem{{.*}}
-// CHECK-NEXT:  main {{.*}}intercept_memmove.cpp:[[@LINE-5]]
-// CHECK: Address [[ADDR]] is located in stack of thread T0 at offset {{.*}} in frame
-// CHECK-NEXT:   #0 {{.*}} main
-// CHECK: 'buff2'{{.*}} <== Memory access at offset {{.*}} overflows this variable
-
 // The CRT provides memmove in several shapes, depending on CRT version and
 // flavor: aliased to memcpy as a single implementation (x64 static CRT),
 // a distinct function (x86, and vcruntime140.dll 14.38+ where memcpy became
@@ -33,5 +23,15 @@ int main() {
     return 2;
   printf("Initial test OK\n");
   fflush(0);
+  // CHECK: Initial test OK
+
   call_memmove(&memmove, buff2, buff1, 6);
+  // CHECK: AddressSanitizer: stack-buffer-overflow on address [[ADDR:0x[0-9a-f]+]]
+  // CHECK: WRITE of size 6 at [[ADDR]] thread T0
+  // CHECK-NEXT:  mem{{.*}}
+  // CHECK-NEXT:  call_mem{{.*}}
+  // CHECK-NEXT:  main {{.*}}intercept_memmove.cpp:[[@LINE-5]]
+  // CHECK: Address [[ADDR]] is located in stack of thread T0 at offset {{.*}} in frame
+  // CHECK-NEXT:   #0 {{.*}} main
+  // CHECK: 'buff2'{{.*}} <== Memory access at offset {{.*}} overflows this variable
 }
