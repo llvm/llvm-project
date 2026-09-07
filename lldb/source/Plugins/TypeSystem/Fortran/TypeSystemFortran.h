e1fa907da413d82d2c1ee5a3ea2941329818d56c
Iasonaskrpr <iaskarapro@gmail.com>
2026-09-07 17:57:17 +0300

    [LLDB][Fortran] Added suggested changes

--- lldb/source/Plugins/TypeSystem/Fortran/TypeSystemFortran.h
+++ lldb/source/Plugins/TypeSystem/Fortran/TypeSystemFortran.h
@@ -5,11 +5,6 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
-///
-/// \file
-/// This file contains the definition of the Fortran Type System.
-///
-//===----------------------------------------------------------------------===//
 
 #ifndef LLDB_SOURCE_PLUGINS_TYPESYSTEM_FORTRAN_TYPESYSTEMFORTRAN_H
 #define LLDB_SOURCE_PLUGINS_TYPESYSTEM_FORTRAN_TYPESYSTEMFORTRAN_H
@@ -35,8 +30,6 @@ public:
   static void Initialize();
 
   static void Terminate();
-
-  plugin::dwarf::DWARFASTParser *GetDWARFParser() override;
 
   static lldb::TypeSystemSP CreateInstance(lldb::LanguageType language,
                                            Module *module, Target *target);
@@ -358,8 +351,6 @@ public:
     return CompilerType();
   }
 
-  // Lookup a child given a name. This function will match base class names and
-  // member member names in "clang_type" only, not descendants.
   llvm::Expected<uint32_t>
   GetIndexOfChildWithName(lldb::opaque_compiler_type_t type,
                           llvm::StringRef name,
@@ -414,13 +405,12 @@ public:
   void Dump(llvm::raw_ostream &output, llvm::StringRef filter,
             bool show_color) override {}
 
-  /// This is used by swift.
+  /// This is used by Swift.
   bool IsRuntimeGeneratedType(lldb::opaque_compiler_type_t type) override {
     return false;
   }
 
-  // TODO: Determine if these methods should move to TypeSystemClang.
-
+  // Pointer here means C-style pointers, not Fortran-style.
   bool IsPointerOrReferenceType(lldb::opaque_compiler_type_t type,
                                 CompilerType *pointee_type) override {
     return false;
@@ -489,10 +479,9 @@ public:
   }
 
 private:
-  std::unique_ptr<plugin::dwarf::DWARFASTParser> m_dwarf_ast_parser_up;
-
   TypeSystemFortran(const TypeSystemFortran &) = delete;
   const TypeSystemFortran &operator=(const TypeSystemFortran &) = delete;
 };
 } // namespace lldb_private
+
 #endif // LLDB_SOURCE_PLUGINS_TYPESYSTEM_FORTRAN_TYPESYSTEMFORTRAN_H
