aee08b784f7d84467c2b41745a32a313356683cc
mauiavalos9-ux <mauiavalos9@gmail.com>
2026-09-07 17:51:49 -0600

    Refactor decorators for test_symbol_file_json_address
    TODO EN orden

--- lldb/test/API/functionalities/json/symbol-file/TestSymbolFileJSON.py
+++ lldb/test/API/functionalities/json/symbol-file/TestSymbolFileJSON.py
@@ -13,8 +13,8 @@ class TargetSymbolsFileJSON(TestBase):
         self.source = "main.c"
 
     @no_debug_info_test
-    @skipIfWindows  # No 'strip'
-    @skipIfWasm  # Wasm modules have no UUID
+    @skipIfWindows  
+    @skipIfWasm 
     def test_symbol_file_json_address(self):
         """Test that 'target symbols add' can load the symbols from a JSON file using file addresses."""
 
