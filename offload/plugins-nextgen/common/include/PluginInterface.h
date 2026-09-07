04a0f5899e76898239318f93fe7d868ea7ffcf82
Duran, Alex <alejandro.duran@intel.com>
2026-09-07 13:59:31 -0700

    [offload][omp] Implement printDeviceInfo through liboffload

--- offload/plugins-nextgen/common/include/PluginInterface.h
+++ offload/plugins-nextgen/common/include/PluginInterface.h
@@ -1262,9 +1262,6 @@ struct GenericDeviceTy : public DeviceAllocatorTy {
   Expected<InfoTreeNode> obtainInfo();
   virtual Expected<InfoTreeNode> obtainInfoImpl() = 0;
 
-  /// Print information about the device.
-  Error printInfo();
-
   /// Return true if the device has work that is either queued or currently
   /// running
   ///
@@ -1777,9 +1774,6 @@ public:
 
   /// Obtain information about the given device.
   InfoTreeNode obtain_device_info(int32_t DeviceId);
-
-  /// Prints information about the given devices supported by the plugin.
-  void print_device_info(int32_t DeviceId);
 
   /// Remove the event from the plugin.
   void set_info_flag(uint32_t NewInfoLevel);
