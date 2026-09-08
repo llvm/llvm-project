2eae929a96bd08cf3f43e0023016c5d6a0793583
hulxv <hulxxv@gmail.com>
2026-09-08 19:41:25 +0300

    use the new name main gave the float128 macro

--- libc/src/__support/builtins/extendhftf2.h
+++ libc/src/__support/builtins/extendhftf2.h
@@ -18,7 +18,7 @@
 #include "include/llvm-libc-types/float128.h"
 #include "src/__support/macros/properties/types.h"
 
-#if defined(LIBC_TYPES_HAS_FLOAT128)
+#ifdef LIBC_TYPES_HAS_NATIVE_FLOAT128
 
 #include "hdr/stdint_proxy.h"
 #include "src/__support/FPUtil/FPBits.h"
@@ -36,6 +36,6 @@ LIBC_INLINE float128 extendhftf2(uint16_t bits) {
 } // namespace builtins
 } // namespace LIBC_NAMESPACE_DECL
 
-#endif // LIBC_TYPES_HAS_FLOAT128
+#endif // LIBC_TYPES_HAS_NATIVE_FLOAT128
 
 #endif // LLVM_LIBC_SRC___SUPPORT_BUILTINS_EXTENDHFTF2_H
--- libc/src/__support/builtins/trunctfhf2.h
+++ libc/src/__support/builtins/trunctfhf2.h
@@ -18,7 +18,7 @@
 #include "include/llvm-libc-types/float128.h"
 #include "src/__support/macros/properties/types.h"
 
-#ifdef LIBC_TYPES_HAS_FLOAT128
+#ifdef LIBC_TYPES_HAS_NATIVE_FLOAT128
 
 #include "hdr/stdint_proxy.h"
 #include "src/__support/FPUtil/FPBits.h"
@@ -36,6 +36,6 @@ LIBC_INLINE uint16_t trunctfhf2(float128 x) {
 } // namespace builtins
 } // namespace LIBC_NAMESPACE_DECL
 
-#endif // LIBC_TYPES_HAS_FLOAT128
+#endif // LIBC_TYPES_HAS_NATIVE_FLOAT128
 
 #endif // LLVM_LIBC_SRC___SUPPORT_BUILTINS_TRUNCTFHF2_H
