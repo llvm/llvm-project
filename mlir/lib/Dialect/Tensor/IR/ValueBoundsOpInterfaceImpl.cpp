e12ca8c78f6fce8e01fab9385e2454ab8161379c
Alexey Kreshchuk <alexey.kreshchuk@mobileye.com>
2026-09-08 15:12:32 +0300

    Bounds only on value

--- mlir/lib/Dialect/Tensor/IR/ValueBoundsOpInterfaceImpl.cpp
+++ mlir/lib/Dialect/Tensor/IR/ValueBoundsOpInterfaceImpl.cpp
@@ -141,11 +141,8 @@ struct ExtractSliceOpInterface
               cstr.getExpr(extractSliceOp.getSource(), i);
           cstr.bound(value)[dim] <=
               (sourceDimExpr - offsetExpr).ceilDiv(staticStride);
-          // To derive value[dim] <= source[i] - offset:
-          cstr.bound(extractSliceOp.getSource())[i] >= offsetExpr;
-          // To derive value[dim] <= source[i]:
-          if (auto offsetValue = llvm::dyn_cast_if_present<Value>(offset))
-            cstr.bound(offsetValue) >= 0;
+          cstr.bound(value)[dim] <= sourceDimExpr - offsetExpr;
+          cstr.bound(value)[dim] <= sourceDimExpr;
         }
         return;
       }
