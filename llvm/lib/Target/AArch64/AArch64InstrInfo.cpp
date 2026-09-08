938dc012c8551f15cb5e5fc20a1135f76604aa70
Feng Zou <feng.zou@intel.com>
2026-09-08 15:32:00 +0800

    Remove blank line.

--- llvm/lib/Target/AArch64/AArch64InstrInfo.cpp
+++ llvm/lib/Target/AArch64/AArch64InstrInfo.cpp
@@ -1722,7 +1722,6 @@ MachineInstr *AArch64InstrInfo::convertToCCMP(
     Opc = AArch64::CCMPXr;
     FirstOp = 1;
     break;
-
   }
 
   // The ccmp instruction should set the flags according to the comparison when
