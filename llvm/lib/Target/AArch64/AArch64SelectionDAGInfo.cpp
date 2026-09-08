cfe31adbd26cf44eb9a80d0ac69eb8812d8c8e43
Vimal Patel <vimal.patel@arm.com>
2026-09-08 12:55:19 +0000

    [AArch64] Lower fixed find_last_active to LASTP with SVE2.2
    Lower ISD::VECTOR_FIND_LAST_ACTIVE to LASTP when SVE2.2/SME2.2 is
    available. But, the existing extract(find_last_active) combiner will
    still get a chance to run if applicable and in that case it generates
    LASTB instead, even when SVE2.2/SME2.2 is available. The LASTP
    instruction returns -1, but to avoid undefined behaviour -1 will be
    replaced with 0.
    
    Additionally a new combiner for select node is added. In the context of
    `llvm.experimental.vector.extract.last.active` this select node selects
    between the passthrough value and the value extracted from the data
    vector. Computing the condition for this select could just rely on the
    result of LASTP under SVE2.2/SME2.2. So, the combiner added will rewrite
    the condition to rely on result of LASTP node instead of using UMAXV for
    it.

--- llvm/lib/Target/AArch64/AArch64SelectionDAGInfo.cpp
+++ llvm/lib/Target/AArch64/AArch64SelectionDAGInfo.cpp
@@ -50,6 +50,7 @@ void AArch64SelectionDAGInfo::verifyTargetNode(const SelectionDAG &DAG,
   switch (N->getOpcode()) {
   case AArch64ISD::CTTZ_ELTS:
   case AArch64ISD::CTTZ_ELTS_ZERO_POISON:
+  case AArch64ISD::LASTP:
     assert(N->getOperand(0).getValueType() == N->getOperand(1).getValueType() &&
            "Expected the general-predicate and mask to have matching types");
     break;
