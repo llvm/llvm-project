122f371e56220283613f1644afbea740e2cfe043
popie52 <popiesailor@gmail.com>
2026-09-08 15:20:32 +0530

    [libc] Modify NaN/payload functions for emulated float128 type

--- libc/test/src/math/smoke/setpayloadf128_test.cpp
+++ libc/test/src/math/smoke/setpayloadf128_test.cpp
@@ -8,6 +8,11 @@
 
 #include "SetPayloadTest.h"
 
+#include "src/__support/FPUtil/float128.h"
 #include "src/math/setpayloadf128.h"
+
+#ifndef LIBC_TYPES_HAS_NATIVE_FLOAT128
+using float128 = LIBC_NAMESPACE::fputil::Float128;
+#endif // LIBC_TYPES_HAS_NATIVE_FLOAT128
 
 LIST_SETPAYLOAD_TESTS(float128, LIBC_NAMESPACE::setpayloadf128)
