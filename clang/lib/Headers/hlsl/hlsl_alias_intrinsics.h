f6f0f37e7664461e954ab9c93d3b4a3780944919
Farzon Lotfi <farzonlotfi@microsoft.com>
2026-09-08 14:27:19 -0400

    fix formating

--- clang/lib/Headers/hlsl/hlsl_alias_intrinsics.h
+++ clang/lib/Headers/hlsl/hlsl_alias_intrinsics.h
@@ -148,9 +148,8 @@ T select(bool, T, T);
 template <typename T, int N>
 _HLSL_BUILTIN_ALIAS(__builtin_hlsl_select)
 vector<T, N> select(
-  __detail::enable_if_t<(N > 1),
-              __detail::type_identity_t<vector<bool, N>>>,
-  vector<T, N>, vector<T, N>);
+    __detail::enable_if_t<(N > 1), __detail::type_identity_t<vector<bool, N>>>,
+    vector<T, N>, vector<T, N>);
 
 /// \fn vector<T,Sz> select(vector<bool,Sz> Conds, T TrueVal,
 ///                         vector<T,Sz> FalseVals)
@@ -163,9 +162,8 @@ vector<T, N> select(
 template <typename T, int N>
 _HLSL_BUILTIN_ALIAS(__builtin_hlsl_select)
 vector<T, N> select(
-  __detail::enable_if_t<(N > 1),
-              __detail::type_identity_t<vector<bool, N>>>,
-  T, vector<T, N>);
+    __detail::enable_if_t<(N > 1), __detail::type_identity_t<vector<bool, N>>>,
+    T, vector<T, N>);
 
 /// \fn vector<T,Sz> select(vector<bool,Sz> Conds, vector<T,Sz> TrueVals,
 ///                         T FalseVal)
@@ -177,9 +175,8 @@ vector<T, N> select(
 template <typename T, int N>
 _HLSL_BUILTIN_ALIAS(__builtin_hlsl_select)
 vector<T, N> select(
-  __detail::enable_if_t<(N > 1),
-              __detail::type_identity_t<vector<bool, N>>>,
-  vector<T, N>, T);
+    __detail::enable_if_t<(N > 1), __detail::type_identity_t<vector<bool, N>>>,
+    vector<T, N>, T);
 
 /// \fn vector<T,Sz> select(vector<bool,Sz> Conds, T TrueVals,
 ///                         T FalseVal)
