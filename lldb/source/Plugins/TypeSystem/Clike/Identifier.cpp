edaa824109b6118c27c97aa348d4fe41f4dc6299
Raphael Isemann <rise@apple.com>
2026-09-08 11:59:37 +0100

    [lldb][Clike] Add IdentifierMap
    TypeSystemClike needs to store the name of structs, enums, member
    variables and similar entities. In the old TypeSystemClang this
    information was stored in Clang's IdentifierTable.
    
    This patch introduces an equivalent for TypeSystemClike called
    `IdentifierMap`. It turns strings into unique Identifier objects, which
    for now directly store the StringRef to the value. The actual storage
    of the string contents is either (A) in the IdentifierMap itself
    or (B) backed by ConstString/constant memory for strings. Option B
    exists mainly because we often already have a ConstString around that
    already contains the respective string, and it avoids us having to
    save a copy like with TypeSystemClang.
    
    Some design questions where I just picked one option:
    
    (a) Do we really need to deduplicate strings?
    
    There is no functional reason to do it, but it might save some memory
    for heavily templated code (where each instantiation is its own time
    with the same names for everything). We can benchmark this once the
    system is working to see what is the right tradeoff.
    
    (b) Can't we just store some small id in `Identifier` instead of a
    StringRef (and the id would be some offset into an IdentifierMap data
    structure)?
    
    We could, but then you would need to pass the right IdentifierMap to
    resolve the id in `Identifier::getName()`, and that is a bit fragile.
    Especially in the context of multiple TypeSystems being used at the
    same time, this can easily go wrong. Again, we might want to benchmark
    this at the end to see if storing something smaller is worth it in
    terms of memory.

---
+++ lldb/source/Plugins/TypeSystem/Clike/Identifier.cpp
@@ -0,0 +1,49 @@
+//===-- Identifier.cpp ----------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "Identifier.h"
+
+#include "llvm/Support/Compiler.h"
+#include "llvm/Support/ErrorHandling.h"
+
+#include <cstring>
+
+using namespace lldb_private;
+using namespace lldb_private::clike_typesystem;
+
+Identifier IdentifierMap::get(llvm::StringRef name) {
+  auto it = m_names.find(name);
+  if (it != m_names.end())
+    return Identifier(*it);
+
+  // Copy the string into our own storage.
+  // TODO: Would it be beneficial to add a null terminator into the buffer?
+  char *storage = m_string_storage.Allocate<char>(name.size());
+  std::memcpy(storage, name.data(), name.size());
+  llvm::StringRef owned(storage, name.size());
+  m_names.insert(owned);
+  return Identifier(owned);
+}
+
+Identifier IdentifierMap::getWithStaticStorageStr(llvm::StringRef name) {
+  // The caller promises the backing storage outlives this map.
+  m_names.insert(name);
+  return Identifier(name);
+}
+
+IdentifierMap::~IdentifierMap() {
+#if LLVM_ADDRESS_SANITIZER_BUILD
+  // Verify every identifier still points at live memory.
+  for (llvm::StringRef name : m_names) {
+    if (!name.empty() &&
+        __asan_region_is_poisoned(const_cast<char *>(name.data()), name.size()))
+      llvm::report_fatal_error("IdentifierMap holds a freed string "
+                               "(misuse of getWithStaticStorageStr?)");
+  }
+#endif
+}
