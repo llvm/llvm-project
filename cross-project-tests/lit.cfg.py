d6e553f92a340a63b9242320dfd904c91c4c5192
Konstantin Belochapka <konstantin.belochapka@sony.com>
2026-09-07 23:21:21 -0700

    [DTLTO] Remove obsolete serialization sync tests

--- cross-project-tests/lit.cfg.py
+++ cross-project-tests/lit.cfg.py
@@ -56,7 +56,6 @@ tools = [
         ),
     ),
     ToolSubst("%llvm_src_root", config.llvm_src_root),
-    ToolSubst("%llvm_obj_root", config.llvm_obj_root),
     ToolSubst("%llvm_tools_dir", config.llvm_tools_dir),
 ]
 
