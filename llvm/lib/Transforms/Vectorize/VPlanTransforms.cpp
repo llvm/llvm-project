6015194d0e7f5a535b0ce375f54658d873cdfca9
Luke Lau <luke@igalia.com>
2026-09-08 16:58:23 +0800

    No need to dyn_cast to singledef recipe anymore

--- llvm/lib/Transforms/Vectorize/VPlanTransforms.cpp
+++ llvm/lib/Transforms/Vectorize/VPlanTransforms.cpp
@@ -1735,9 +1735,7 @@ void VPlanTransforms::combineRecipes(VPlan &Plan) {
       // Replace the recipe with a new one.
       Def->replaceAllUsesWith(New);
       Def->eraseFromParent();
-      if (auto *NewR =
-              dyn_cast_if_present<VPSingleDefRecipe>(New->getDefiningRecipe()))
-        Worklist.push_back(NewR);
+      Worklist.push_back(New);
       // TODO: Append users to the worklist (might need a setvector)
     } else if (vputils::isDeadRecipe(*Def)) {
       // Recipe was modified - it may be dead now.
