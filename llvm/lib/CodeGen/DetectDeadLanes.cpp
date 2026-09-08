2a8060a17e89257c4177a61838eca650bd9b2813
Jake Daly <jake.daly@ymail.com>
2026-09-08 11:57:32 -0700

    Add some more tests; iterate all bundle operands; clang-format

--- llvm/lib/CodeGen/DetectDeadLanes.cpp
+++ llvm/lib/CodeGen/DetectDeadLanes.cpp
@@ -27,6 +27,7 @@
 
 #include "llvm/CodeGen/DetectDeadLanes.h"
 #include "llvm/CodeGen/MachineFunctionPass.h"
+#include "llvm/CodeGen/MachineInstrBundle.h"
 #include "llvm/CodeGen/MachineRegisterInfo.h"
 #include "llvm/CodeGen/TargetRegisterInfo.h"
 #include "llvm/InitializePasses.h"
@@ -496,31 +497,32 @@ DetectDeadLanes::modifySubRegisterOperandStatus(const DeadLaneDetector &DLD,
   bool Again = false;
   // Mark operands as dead/unused.
   for (MachineBasicBlock &MBB : MF) {
-    for (MachineInstr &MI : MBB.instrs()) {
-      for (MachineOperand &MO : MI.operands()) {
+    for (MachineInstr &MI : MBB) {
+      for (MachineOperand &MO : mi_bundle_ops(MI)) {
         if (!MO.isReg())
           continue;
         Register Reg = MO.getReg();
         if (!Reg.isVirtual())
           continue;
+        const MachineInstr &OpMI = *MO.getParent();
         unsigned RegIdx = Reg.virtRegIndex();
         const DeadLaneDetector::VRegInfo &RegInfo = DLD.getVRegInfo(RegIdx);
         if (MO.isDef() && !MO.isDead() && RegInfo.UsedLanes.none()) {
           LLVM_DEBUG(dbgs()
-                     << "Marking operand '" << MO << "' as dead in " << MI);
+                     << "Marking operand '" << MO << "' as dead in " << OpMI);
           MO.setIsDead();
           Changed = true;
         }
         if (MO.readsReg()) {
           bool CrossCopy = false;
           if (isUndefRegAtInput(MO, RegInfo)) {
-            LLVM_DEBUG(dbgs()
-                       << "Marking operand '" << MO << "' as undef in " << MI);
+            LLVM_DEBUG(dbgs() << "Marking operand '" << MO << "' as undef in "
+                              << OpMI);
             MO.setIsUndef();
             Changed = true;
-          } else if (isUndefInput(DLD, MI, MO, &CrossCopy)) {
-            LLVM_DEBUG(dbgs()
-                       << "Marking operand '" << MO << "' as undef in " << MI);
+          } else if (isUndefInput(DLD, OpMI, MO, &CrossCopy)) {
+            LLVM_DEBUG(dbgs() << "Marking operand '" << MO << "' as undef in "
+                              << OpMI);
             MO.setIsUndef();
             Changed = true;
             if (CrossCopy)
