d6cb511d4d1f18486f1221f7b57ea0cca7d7d026
Harald van Dijk <hdijk@accesssoftek.com>
2026-09-08 14:47:58 +0100

    Update manpage, remove --encoding=b/l/B/L and --unicode, add test

--- llvm/docs/CommandGuide/llvm-strings.md
+++ llvm/docs/CommandGuide/llvm-strings.md
@@ -12,8 +12,8 @@
 {program}`llvm-strings` is a tool intended as a drop-in replacement for GNU's
 {program}`strings`, which looks for printable strings in files and writes them
 to the standard output stream. A printable string is any sequence of four (by
-default) or more printable ASCII characters. The end of the file, or any other
-byte, terminates the current sequence.
+default) or more printable characters. The end of the file, or any other byte,
+terminates the current sequence.
 
 {program}`llvm-strings` looks for strings in each `input` file specified.
 Unlike GNU {program}`strings` it looks in the entire input file, regardless of
@@ -40,8 +40,15 @@ Silently ignored. Present for GNU {program}`strings` compatibility.
 :::
 
 :::{option} --bytes=<length>, -n
-Set the minimum number of printable ASCII characters required for a sequence of
-bytes to be considered a string. The default value is 4.
+Set the minimum number of printable characters required for a sequence to be
+considered a string. The default value is 4.
+:::
+
+:::{option} --encoding=<encoding>, -e
+Specifies the encoding of the input file. Valid arguments are:
+- s (ASCII characters)
+- S (characters in the system's or user's selected character set)
+- u (UTF-8 characters)
 :::
 
 :::{option} --help, -h
--- llvm/tools/llvm-strings/llvm-strings.cpp
+++ llvm/tools/llvm-strings/llvm-strings.cpp
@@ -75,14 +75,11 @@ static cl::list<std::string> InputFileNames(cl::Positional,
 static int MinLength = 4;
 static bool PrintFileName;
 
-enum class encoding { s, S, u, b, l, B, L };
+enum class encoding { s, S, u };
 static encoding Encoding;
 
 enum class radix { none, octal, hexadecimal, decimal };
 static radix Radix;
-
-enum class unicode { default_, invalid, locale, escape, hex, highlight };
-static unicode Unicode;
 
 [[noreturn]] static void reportCmdLineError(const Twine &Message) {
   WithColor::error(errs(), ToolName) << Message << "\n";
@@ -103,13 +100,6 @@ static void strings(raw_ostream &OS, StringRef FileName, StringRef Contents) {
   auto &cvt = std::use_facet<std::codecvt<wchar_t, char, std::mbstate_t>>(loc);
   auto &ctype = std::use_facet<std::ctype<wchar_t>>(loc);
   std::mbstate_t mbs{};
-
-  const bool PrintRawBytes =
-      Encoding == encoding::s ||
-      (Encoding == encoding::S &&
-       (Unicode == unicode::invalid || Unicode == unicode::locale)) ||
-      (Encoding == encoding::u &&
-       (Unicode == unicode::default_ || Unicode == unicode::invalid));
 
   auto read = [&cvt, &mbs](const char *&P, const char *E) -> UTF32 {
     UTF32 Ch;
@@ -159,55 +149,13 @@ static void strings(raw_ostream &OS, StringRef FileName, StringRef Contents) {
       P = reinterpret_cast<const char *>(UP);
       break;
     }
-
-    case encoding::b:
-    case encoding::l: {
-      const UTF16 *UP = reinterpret_cast<const UTF16 *>(P);
-      const UTF16 *UE = reinterpret_cast<const UTF16 *>(E);
-      const bool DoByteSwap =
-          (Encoding == encoding::b &&
-           endianness::native == endianness::little) ||
-          (Encoding == encoding::l && endianness::native == endianness::big);
-      UTF32 *Next = &Ch;
-      if (!DoByteSwap) {
-        ConvertUTF16toUTF32(&UP, UE, &Next, &Ch + 1, strictConversion);
-      } else {
-        // We never need more than two UTF16 words to make up one character.
-        const UTF16 Buf[2] = {byteswap(UP[0]),
-                              UP + 1 == UE ? UTF16(0) : byteswap(UP[1])};
-        const UTF16 *BufP = Buf;
-        ConvertUTF16toUTF32(&BufP, &Buf[2], &Next, &Ch + 1, strictConversion);
-        UP += BufP - Buf;
-      }
-      if (Next == &Ch || !Ch) {
-        // If there was any error, skip the current word to allow the next to
-        // start a character.
-        Ch = 0;
-        UP = std::next(reinterpret_cast<const UTF16 *>(P));
-      }
-      P = reinterpret_cast<const char *>(UP);
-      break;
-    }
-
-    case encoding::B:
-    case encoding::L: {
-      const UTF32 *UP = reinterpret_cast<const UTF32 *>(P);
-      const bool DoByteSwap =
-          (Encoding == encoding::B &&
-           endianness::native == endianness::little) ||
-          (Encoding == encoding::L && endianness::native == endianness::big);
-      Ch = DoByteSwap ? byteswap(*UP) : *UP;
-      ++UP;
-      P = reinterpret_cast<const char *>(UP);
-      break;
-    }
     }
 
     return Ch;
   };
 
-  auto print = [&OS, FileName, PrintRawBytes, &read,
-                &cvt](unsigned Offset, StringRef L, size_t N) {
+  auto print = [&OS, FileName, &read, &cvt](unsigned Offset, StringRef L,
+                                            size_t N) {
     if (N < static_cast<size_t>(MinLength))
       return;
     if (PrintFileName)
@@ -226,64 +174,7 @@ static void strings(raw_ostream &OS, StringRef FileName, StringRef Contents) {
       break;
     }
 
-    if (PrintRawBytes) {
-      OS << L << '\n';
-    } else {
-      mbstate_t mbs = {};
-
-      const char *P = L.begin();
-      const char *E = L.end();
-      while (P < E) {
-        const UTF32 Ch = read(P, E);
-        if (Unicode == unicode::invalid || Ch <= 0x7F) {
-          OS << (char)Ch;
-          continue;
-        }
-
-        if (Unicode == unicode::locale) {
-          // If we have a 16-bit wchar_t and the character does not fit, replace
-          // it with U+FFFD.
-          wchar_t WCh = Ch;
-          const wchar_t *WNext;
-          if ((UTF32)WCh != Ch)
-            WCh = 0xFFFD;
-          char mbstring[MB_LEN_MAX];
-          char *mbend;
-          [[maybe_unused]] const auto res =
-              cvt.out(mbs, &WCh, &WCh + 1, WNext, mbstring,
-                      &mbstring[MB_LEN_MAX], mbend);
-          assert(res == std::codecvt_base::ok);
-          OS << StringRef(mbstring, mbend - mbstring);
-          continue;
-        }
-
-        if (Unicode == unicode::escape || Unicode == unicode::highlight) {
-          WithColor COS(OS, raw_ostream::RED, false, false,
-                        Unicode == unicode::highlight ? ColorMode::Auto
-                                                      : ColorMode::Disable);
-          if (Ch <= 0xFFFF)
-            COS << "\\u" << utohexstr(Ch, false, 4);
-          else
-            COS << "\\U" << utohexstr(Ch, false, 8);
-          continue;
-        }
-
-        char UTF8[4];
-        char *UTF8end = UTF8;
-        ConvertCodePointToUTF8(Ch, UTF8end);
-        if (Unicode == unicode::hex) {
-          OS << "<0x";
-          for (unsigned char Byte : StringRef(UTF8, UTF8end - UTF8))
-            OS << utohexstr(Byte, true, 2);
-          OS << '>';
-          continue;
-        }
-
-        assert(Unicode == unicode::default_);
-        OS << StringRef(UTF8, UTF8end - UTF8);
-      }
-      OS << '\n';
-    }
+    OS << L << '\n';
   };
 
   std::size_t NumPrintable = 0;
@@ -296,9 +187,9 @@ static void strings(raw_ostream &OS, StringRef FileName, StringRef Contents) {
 
     const bool Printable =
         Ch == '\t' ||
-        (Unicode == unicode::invalid ? Ch <= 0x7f && isPrint(Ch)
-         : Encoding == encoding::S   ? ctype.is(std::ctype_base::print, Ch)
-                                     : sys::unicode::isPrintable(Ch));
+        (Encoding == encoding::s   ? Ch <= 0x7f && isPrint(Ch)
+         : Encoding == encoding::S ? ctype.is(std::ctype_base::print, Ch)
+                                   : sys::unicode::isPrintable(Ch));
 
     if (Printable) {
       if (S == nullptr)
@@ -341,31 +232,23 @@ int main(int argc, char **argv) {
   }
 
   const auto *EncodingArg = Args.getLastArg(OPT_encoding_EQ);
-  const auto *UnicodeArg = Args.getLastArg(OPT_unicode_EQ);
 
-  Encoding =
-      EncodingArg
-          ? llvm::StringSwitch<encoding>(EncodingArg->getValue())
-                .Case("s", encoding::s)
-                .Case("S", encoding::S)
-                .Case("u", encoding::u)
-                .Case("b", encoding::b)
-                .Case("l", encoding::l)
-                .Case("B", encoding::B)
-                .Case("L", encoding::L)
-                .Predicate(
-                    [](StringRef) -> bool {
-                      reportCmdLineError(
-                          "--encoding value should be one of: "
-                          "'s' (7-bit characters), "
-                          "'S' (8-bit characters), "
-                          "'u' (UTF-8 characters), "
-                          "'b', 'l' (16-bit big/little endian characters), "
-                          "'B', 'L' (32-bit big/little endian characters)");
-                    },
-                    encoding{})
-                .DefaultUnreachable()
-          : encoding::S;
+  Encoding = EncodingArg ? llvm::StringSwitch<encoding>(EncodingArg->getValue())
+                               .Case("s", encoding::s)
+                               .Case("S", encoding::S)
+                               .Case("u", encoding::u)
+                               .Predicate(
+                                   [](StringRef) -> bool {
+                                     reportCmdLineError(
+                                         "--encoding value should be one of: "
+                                         "'s' (ASCII characters), "
+                                         "'S' (characters in the system's or "
+                                         "user's selected character set), "
+                                         "'u' (UTF-8 characters)");
+                                   },
+                                   encoding{})
+                               .DefaultUnreachable()
+                         : encoding::S;
   parseIntArg(Args, OPT_bytes_EQ, MinLength);
   PrintFileName = Args.hasArg(OPT_print_file_name);
   StringRef R = Args.getLastArgValue(OPT_radix_EQ);
@@ -380,49 +263,6 @@ int main(int argc, char **argv) {
   else
     reportCmdLineError("--radix value should be one of: '' (no offset), 'o' "
                        "(octal), 'd' (decimal), 'x' (hexadecimal)");
-  Unicode = UnicodeArg ? llvm::StringSwitch<unicode>(UnicodeArg->getValue())
-                             .Case("default", unicode::default_)
-                             .Case("invalid", unicode::invalid)
-                             .Case("locale", unicode::locale)
-                             .Case("escape", unicode::escape)
-                             .Case("hex", unicode::hex)
-                             .Case("highlight", unicode::highlight)
-                             .Predicate(
-                                 [](StringRef) -> bool {
-                                   reportCmdLineError(
-                                       "--unicode value should be one of: "
-                                       "default, "
-                                       "invalid, "
-                                       "locale, "
-                                       "escape, "
-                                       "hex, "
-                                       "highlight");
-                                 },
-                                 unicode::default_)
-                             .DefaultUnreachable()
-                       : unicode::locale;
-
-  // The defaults are ugly to maintain compatibility with GNU strings in the
-  // common cases. The general idea is that --encoding specifies the input
-  // encoding, --unicode specifies the output encoding, but when one is
-  // specified, the other defaults to the best match with the limitation that
-  // the output encoding is never UTF-16 or UTF-32.
-  if (!EncodingArg && Unicode != unicode::invalid && Unicode != unicode::locale)
-    Encoding = encoding::u;
-
-  if (!UnicodeArg && Encoding != encoding::S)
-    Unicode = unicode::default_;
-
-  // With --encoding=s, all output encodings would give the same results. Use
-  // the simplest one.
-  if (Encoding == encoding::s)
-    Unicode = unicode::invalid;
-
-  // With --encoding=u --unicode=invalid, we do not need to spend time decoding
-  // UTF-8 only to reject whatever results we get, we can reject multibyte
-  // characters right away.
-  if (Encoding == encoding::u && Unicode == unicode::invalid)
-    Encoding = encoding::s;
 
   if (MinLength == 0) {
     errs() << "invalid minimum string length 0\n";
