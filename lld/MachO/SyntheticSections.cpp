3299c08ae991a6a88262c35722e70b22bda11f4d
Liza Burakova <liza@chromium.org>
2026-09-09 16:33:20 -0400

    set up stabSourceFiles to use fileId for lookup

--- lld/MachO/SyntheticSections.cpp
+++ lld/MachO/SyntheticSections.cpp
@@ -1268,16 +1268,16 @@ void SymtabSection::emitStabs() {
     if (stabFiles.empty() || stabFiles.back() != file)
       stabFiles.push_back(file);
   }
-  std::vector<std::string> stabSourceFiles(stabFiles.size());
+  std::vector<std::string> stabSourceFiles(
+      stabFiles.empty() ? 0 : stabFiles.back()->id + 1);
   parallelFor(0, stabFiles.size(), [&](size_t i) {
-    stabSourceFiles[i] = stabFiles[i]->sourceFile();
+    stabSourceFiles[stabFiles[i]->id] = stabFiles[i]->sourceFile();
   });
 
   // Emit STABS symbols so that dsymutil and/or the debugger can map address
   // regions in the final binary to the source and object files from which they
   // originated.
   InputFile *lastFile = nullptr;
-  size_t stabFileIdx = 0;
   for (SortingPair &pair : symbolsNeedingStabs) {
     Defined *defined = pair.first;
     // When emitting STABS entries for a symbol, always use the original
@@ -1293,8 +1293,7 @@ void SymtabSection::emitStabs() {
         emitEndSourceStab();
       lastFile = file;
 
-      assert(stabFiles[stabFileIdx] == file);
-      emitBeginSourceStab(stabSourceFiles[stabFileIdx++]);
+      emitBeginSourceStab(stabSourceFiles[file->id]);
       emitObjectFileStab(file);
     }
 
