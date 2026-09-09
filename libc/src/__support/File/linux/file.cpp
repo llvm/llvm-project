db720c24d27f0ae18e4bbc4dc13878665c47f617
tdadadavid <davidtofunmidada@gmail.com>
2026-09-09 20:48:51 +0100

    convert int to boolean and fix REQUIRES_READ flag
    Signed-off-by: tdadadavid <davidtofunmidada@gmail.com>

--- libc/src/__support/File/linux/file.cpp
+++ libc/src/__support/File/linux/file.cpp
@@ -140,10 +140,10 @@ ErrorOr<LinuxFile *> create_file_from_fd(int fd, const char *mode) {
   }
   int fd_flags = result.value();
 
-  int REQUIRES_WRITE = file_mode.write_allowed() ||
-                       file_mode.append_allowed() || file_mode.is_plus();
+  const bool REQUIRES_WRITE = file_mode.write_allowed() ||
+                              file_mode.append_allowed() || file_mode.is_plus();
 
-  int REQUIRES_READ = file_mode.write_allowed() || file_mode.is_plus();
+  const bool REQUIRES_READ = file_mode.read_allowed() || file_mode.is_plus();
 
   if (((fd_flags & O_ACCMODE) == O_RDONLY && REQUIRES_WRITE) ||
       ((fd_flags & O_ACCMODE) == O_WRONLY && REQUIRES_READ)) {
