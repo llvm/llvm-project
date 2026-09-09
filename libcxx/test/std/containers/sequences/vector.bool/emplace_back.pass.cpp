17fdabbd70dc8086ac7d81a30771ebf1294c705a
A. Jiang <de34@live.cn>
2026-09-09 09:22:10 +0800

    A bit more reverts on formatting

--- libcxx/test/std/containers/sequences/vector.bool/emplace_back.pass.cpp
+++ libcxx/test/std/containers/sequences/vector.bool/emplace_back.pass.cpp
@@ -54,7 +54,7 @@ TEST_CONSTEXPR_CXX20 bool tests() {
     assert(c.back() == true);
   }
   {
-    typedef std::vector<bool, min_allocator<bool> > C;
+    typedef std::vector<bool, min_allocator<bool>> C;
     C c;
 
 #if TEST_STD_VER > 14
