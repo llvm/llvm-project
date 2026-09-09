5d5b203a4e35bd616be31c91afe532292235a25f
Andres Salamanca <andrealebarbaritos@gmail.com>
2026-09-08 19:56:06 -0500

    [CIR] Add cir.coro.intrinsic.resume/destroy/done Ops

--- clang/lib/CIR/CodeGen/CIRGenFunction.h
+++ clang/lib/CIR/CodeGen/CIRGenFunction.h
@@ -1916,6 +1916,9 @@ public:
   cir::CoroAllocOp emitCoroAllocBuiltinCall(const CallExpr *e);
   cir::CoroBeginOp emitCoroBeginBuiltinCall(const CallExpr *e);
   cir::CoroPromiseOp emitCoroPromiseBuiltinCall(const CallExpr *e);
+  cir::CoroDoneOp emitCoroDoneBuiltinCall(const CallExpr *e);
+  cir::CoroResumeOp emitCoroResumeBuiltinCall(const CallExpr *e);
+  cir::CoroDestroyOp emitCoroDestroyBuiltinCall(const CallExpr *e);
 
   cir::CoroSizeOp emitCoroSizeBuiltinCall(const CallExpr *e);
   cir::CoroFreeOp emitCoroFreeBuiltin(const CallExpr *e);
