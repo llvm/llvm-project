632159027ba8f1dd60792c344fbd81a0086cdcae
erichkeane <ekeane@nvidia.com>
2026-09-08 17:54:44 -0700

    Add a few more pch uses, this time in TargetLowering, which gives a partial percent improvement

---
+++ clang/lib/CIR/Dialect/Transforms/TargetLowering/pch.h
@@ -0,0 +1,15 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+///
+/// Precompiled header for MLIRCIRTargetLowering. Uses private headers.
+///
+//===----------------------------------------------------------------------===//
+
+#include "CIRCXXABI.h"
+#include "LowerModule.h"
+#include "TargetLoweringInfo.h"
