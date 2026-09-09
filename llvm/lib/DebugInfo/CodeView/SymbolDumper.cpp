937a8d0324fb9a4867ed541fda3c2cea7311f251
Nerixyz <nerixdev@outlook.de>
2026-09-09 21:39:26 +0200

    fix: use `AssociationKind` in user facing text

--- llvm/lib/DebugInfo/CodeView/SymbolDumper.cpp
+++ llvm/lib/DebugInfo/CodeView/SymbolDumper.cpp
@@ -708,7 +708,7 @@ Error CVSymbolDumperImpl::visitKnownRecord(CVSymbol &CVR,
 
 Error CVSymbolDumperImpl::visitKnownRecord(CVSymbol &CVR,
                                            AssociationSym &Assoc) {
-  W.printEnum("AssocKind", static_cast<uint16_t>(Assoc.AssocKind),
+  W.printEnum("AssociationKind", static_cast<uint16_t>(Assoc.AssocKind),
               getAssociationKindNames());
   W.printHex("Offset", Assoc.CodeOffset);
   W.printHex("Segment", Assoc.Segment);
