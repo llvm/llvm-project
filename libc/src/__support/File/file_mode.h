c72870590987afe1980e077853fdd327e97ca9b6
tdadadavid <davidtofunmidada@gmail.com>
2026-09-09 21:31:01 +0100

    chore: add lincense headers to file mode class
    Signed-off-by: tdadadavid <davidtofunmidada@gmail.com>

--- libc/src/__support/File/file_mode.h
+++ libc/src/__support/File/file_mode.h
@@ -1,3 +1,10 @@
+//===--- A platform independent File mode class -------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
 
 #ifndef LLVM_LIBC_SRC___SUPPORT_FILE_FILE_MODE_H
 #define LLVM_LIBC_SRC___SUPPORT_FILE_FILE_MODE_H
