fe0798fd412d1c4de921ae100026d9381444ef6d
h2h <h2h@meta.com>
2026-09-08 21:31:26 -0700

    Avoid fallback in ThinLTO PreLink

--- llvm/lib/Transforms/IPO/SampleProfile.cpp
+++ llvm/lib/Transforms/IPO/SampleProfile.cpp
@@ -2295,7 +2295,8 @@ bool SampleProfileLoader::runOnFunction(Function &F,
       }
       // Try use flattened profile as a fallback when no profile is found as a
       // top-level function in Sample Reader nor Outlined function samples
-      if (!Samples && MatchingManager)
+      if (!Samples && MatchingManager &&
+          LTOPhase != ThinOrFullLTOPhase::ThinLTOPreLink)
         Samples = MatchingManager->getFlattenedSamplesFor(F);
     }
   }
