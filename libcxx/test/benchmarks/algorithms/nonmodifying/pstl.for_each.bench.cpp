1f238d23dc1f72aed753758923c20fd5d6e5857e
Michael Kazakov <mike.kazakov@gmail.com>
2026-09-09 09:08:46 +0100

    Updated the for_each benchmark

--- libcxx/test/benchmarks/algorithms/nonmodifying/pstl.for_each.bench.cpp
+++ libcxx/test/benchmarks/algorithms/nonmodifying/pstl.for_each.bench.cpp
@@ -19,37 +19,50 @@
 #include <type_traits>
 #include <vector>
 #include <execution>
+#include <utility>
 
 #include <benchmark/benchmark.h>
 
-// A function that does enough work per element to justify parallelization.
-static void f(double& x) { x = std::pow(std::exp(std::sqrt(std::sin(std::cos(x)) + 1.0)), 42.0); };
-
 int main(int argc, char** argv) {
-  auto bm = [](std::string name, auto&& policy) {
+  // A function that does just a bit more than a no-op
+  auto minimal = [](double& x) { x = 0.; };
+
+  // A function that does miniscule work per element
+  auto cheap = [](double& x) { x = std::sin(x); };
+
+  // A function that does significant work per element
+  auto expensive = [](double& x) { x = std::pow(std::exp(std::sqrt(std::sin(std::cos(x)) + 1.0)), 1.7); };
+
+  auto bm = [](std::string name, auto&& policy, auto&& func) {
     benchmark::RegisterBenchmark(
         name,
-        [&policy](auto& st) {
-          std::size_t const size = st.range(0);
-          std::vector<double> c(size, 42.);
+        [&policy, &func](auto& st) {
+          std::size_t size = st.range(0);
+          std::vector<double> c(size);
           std::iota(c.begin(), c.end(), 1.);
           auto first = c.begin();
           auto last  = c.end();
           for ([[maybe_unused]] auto _ : st) {
             benchmark::DoNotOptimize(c);
-            std::for_each(policy, first, last, f);
+            std::for_each(policy, first, last, func);
             benchmark::DoNotOptimize(c);
           }
         })
-        ->Arg(1024)
-        ->Arg(8192)
-        ->Arg(65536)
-        ->Arg(524288)
-        ->Arg(4194304)
-        ->Arg(33554432);
+        ->Arg(128)
+        ->Arg(1'024)
+        ->Arg(8'192)
+        ->Arg(65'536)
+        ->Arg(524'288)
+        ->Arg(4'194'304)
+        ->Arg(33'554'432)
+        ->UseRealTime();
   };
-  bm.operator()("std::for_each(std::execution::seq, vector<double>)", std::execution::seq);
-  bm.operator()("std::for_each(std::execution::par, vector<double>)", std::execution::par);
+  bm("std::for_each(std::execution::seq, vector<double>) (minimal)", std::execution::seq, minimal);
+  bm("std::for_each(std::execution::seq, vector<double>) (cheap)", std::execution::seq, cheap);
+  bm("std::for_each(std::execution::seq, vector<double>) (expensive)", std::execution::seq, expensive);
+  bm("std::for_each(std::execution::par, vector<double>) (minimal)", std::execution::par, minimal);
+  bm("std::for_each(std::execution::par, vector<double>) (cheap)", std::execution::par, cheap);
+  bm("std::for_each(std::execution::par, vector<double>) (expensive)", std::execution::par, expensive);
 
   benchmark::Initialize(&argc, argv);
   benchmark::RunSpecifiedBenchmarks();
