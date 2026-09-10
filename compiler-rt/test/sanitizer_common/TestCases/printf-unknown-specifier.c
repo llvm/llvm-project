634aae80b7754dc49501719bc862fa85cabf9e55
Dan Blackwell <dan_blackwell@apple.com>
2026-09-10 14:34:14 +0100

    [sanitizer_common] Log a backtrace for unexpected format printf specifiers
    Occasionally I find these warning messages in ASan logs, and it is not always easy to track down their source. This patch logs a backtrace along with the warning message in order to make them easier to locate.
    
    rdar://34028569

---
+++ compiler-rt/test/sanitizer_common/TestCases/printf-unknown-specifier.c
@@ -0,0 +1,24 @@
+// Check that the "unexpected format specifier" warning from the printf
+// interceptor gives a backtrace
+
+// RUN: %clang %s -Wno-format -Wno-format-invalid-specifier -o %t
+// RUN: %run %t 2>&1 | FileCheck %s
+
+// The warning is only emitted by tools that use the common printf interceptor.
+// UNSUPPORTED: ubsan, lsan
+
+#include <stdio.h>
+
+__attribute__((noinline)) void log_it(const char *fmt, int v) {
+  printf(fmt, v);
+}
+
+int main(void) {
+  log_it("value=%y\n", 42);
+  // CHECK: WARNING: unexpected format specifier in printf interceptor: %y
+  // CHECK: {{ }}log_it
+  // CHECK: {{ }}main
+  printf("done\n");
+  // CHECK: done
+  return 0;
+}
