79a37e5ee39e418239f30929e99da4e9f76ff434
tdadadavid <davidtofunmidada@gmail.com>
2026-09-10 22:01:39 +0100

    feat: add class for handling linux file flags
    Signed-off-by: tdadadavid <davidtofunmidada@gmail.com>

---
+++ libc/src/__support/File/linux/file_flags.h
@@ -0,0 +1,42 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+///
+/// file_flags.h
+/// This file contains the defualt constants of Linux file open flags.
+///
+//===----------------------------------------------------------------------===//
+
+#include "src/__support/macros/config.h"
+
+namespace LIBC_NAMESPACE_DECL {
+
+class LinuxFileFlags {
+public:
+  static constexpr int CREATE_AND_APPEND = O_CREAT | O_APPEND;
+  static constexpr int READ_AND_WRITE = O_RDWR;
+  static constexpr int WRITE_ONLY = O_WRONLY;
+  static constexpr int READ_ONLY = O_RDONLY;
+  static constexpr int CREATE_OR_TRUNCATE = O_CREAT | O_TRUNC;
+
+  // File created will have 0666 permissions.
+  static constexpr mode_t OPEN_MODE =
+      S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH;
+
+  static constexpr int is_file_descriptor_opened_in_read_only(int flag) {
+    return (flag & O_ACCMODE) == O_RDONLY;
+  }
+
+  static constexpr int is_file_descriptor_opened_in_write_only(int flag) {
+    return (flag & O_ACCMODE) == O_WRONLY;
+  }
+
+  static constexpr int file_has_append_flag(int flag) {
+    return flag & O_APPEND;
+  }
+};
+} // namespace LIBC_NAMESPACE_DECL
