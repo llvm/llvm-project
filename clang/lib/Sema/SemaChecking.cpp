98a6983e0cb9301fbd5c88ea6365828b0d91c6e2
im-lunex <thisissamir04@gmail.com>
2026-09-11 00:58:58 +0600

    [Sema] Check arg count for builtins with CustomTypeChecking

--- clang/lib/Sema/SemaChecking.cpp
+++ clang/lib/Sema/SemaChecking.cpp
@@ -4193,6 +4193,21 @@ Sema::CheckBuiltinFunctionCall(FunctionDecl *FDecl, unsigned BuiltinID,
   if (getLangOpts().HLSL && HLSL().CheckBuiltinFunctionCall(BuiltinID, TheCall))
     return ExprError();
 
+  // check if the builtin has CustomTypeChecking or not, if it dose we get the
+  // already parsed type string of the builtin and compare that with the
+  // caller's passed args and give error for too much args
+  if (Context.BuiltinInfo.hasCustomTypechecking(BuiltinID)) {
+    ASTContext::GetBuiltinTypeError Error;
+    QualType BuiltinFTy = Context.GetBuiltinType(BuiltinID, Error);
+    if (!BuiltinFTy.isNull() && !Error) {
+      if (const FunctionProtoType *FPT =
+              dyn_cast<FunctionProtoType>(BuiltinFTy.getTypePtr()))
+        if (!FPT->isVariadic() &&
+            checkArgCountAtMost(TheCall, FPT->getNumParams()))
+          return ExprError();
+    }
+  }
+
   // Since the target specific builtins for each arch overlap, only check those
   // of the arch we are compiling for.
   if (Context.BuiltinInfo.isTSBuiltin(BuiltinID)) {
---
+++ clang/test/Sema/PowerPC/builtins-too-many-args-check.cpp
@@ -0,0 +1,22 @@
+// Testfile for (https://github.com/llvm/llvm-project/issues/216669)
+
+// REQUIRES: powerpc-registered-target
+// RUN: %clang_cc1 -target-feature +power9-vector -triple powerpc64-unknown-unknown -fsyntax-only -verify %s
+// RUN: %clang_cc1 -target-feature +power9-vector -triple powerpc64le-unknown-unknown -fsyntax-only -verify %s
+// RUN: %clang_cc1 -target-feature +power9-vector -triple powerpc-unknown-unknown -fsyntax-only -verify %s
+
+vector unsigned char test_bcdsetsign_too_many(vector unsigned char a) {
+  return __builtin_ppc_bcdsetsign(a, 1, 2); // expected-error {{too many arguments to function call, expected at most 2, have 3}}
+}
+
+vector unsigned char test_national2packed_too_many(vector unsigned char a) {
+  return __builtin_ppc_national2packed(a, 1, 0); // expected-error {{too many arguments to function call, expected at most 2, have 3}}
+}
+
+vector unsigned char test_packed2zoned_too_many(vector unsigned char a) {
+  return __builtin_ppc_packed2zoned(a, 1, 0); // expected-error {{too many arguments to function call, expected at most 2, have 3}}
+}
+
+vector unsigned char test_zoned2packed_too_many(vector unsigned char a) {
+  return __builtin_ppc_zoned2packed(a, 1, 0); // expected-error {{too many arguments to function call, expected at most 2, have 3}}
+}
