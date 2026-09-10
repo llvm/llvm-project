2d7eef3fd2ac03e4fe581c6bb417d1c468e8bc51
Vadim Kotov <kotov@google.com>
2026-09-09 17:04:19 -0700

    Implement readdir loop.

--- libc/src/dirent/scandir.cpp
+++ libc/src/dirent/scandir.cpp
@@ -16,7 +16,10 @@
 #include "hdr/types/struct_dirent.h"
 #include "src/__support/common.h"
 #include "src/__support/macros/config.h"
+#include "src/__support/libc_errno.h"
+#include "src/dirent/closedir.h"
 #include "src/dirent/opendir.h"
+#include "src/dirent/readdir.h"
 
 namespace LIBC_NAMESPACE_DECL {
 
@@ -26,15 +29,31 @@ LLVM_LIBC_FUNCTION(int, scandir,
                     int (*compar)(const struct dirent **,
                                   const struct dirent **))) {
   (void)namelist;
-  (void)sel;
   (void)compar;
 
-  DIR *dir_fd = opendir(dir);
+  DIR *dir_fd = LIBC_NAMESPACE::opendir(dir);
   if (dir_fd == nullptr) {
-    // opendir set errno
+    // errno set by opendir
     return -1;
   }
 
+  struct dirent *entry = nullptr;
+  do {
+    libc_errno = 0;
+    entry = LIBC_NAMESPACE::readdir(dir_fd);
+    if (libc_errno != 0) {
+      // errno set by readdir
+      return -1;
+    }
+
+    if (sel != nullptr && !sel(entry)) {
+      continue;
+    }
+
+  } while (entry != nullptr);
+
+
+  LIBC_NAMESPACE::closedir(dir_fd);
 
   return 0;
 }
--- libc/test/src/dirent/scandir_test.cpp
+++ libc/test/src/dirent/scandir_test.cpp
@@ -20,6 +20,11 @@
 using namespace LIBC_NAMESPACE::testing::ErrnoSetterMatcher;
 using LlvmLibcScandirTest = LIBC_NAMESPACE::testing::ErrnoCheckingTest;
 
+TEST_F(LlvmLibcScandirTest, TestDummy) {
+  struct dirent **namelist;
+  ASSERT_THAT(LIBC_NAMESPACE::scandir("/tmp", &namelist, NULL, NULL), Succeeds());
+}
+
 TEST_F(LlvmLibcScandirTest, TestBadDirname) {
   struct dirent **namelist;
   ASSERT_THAT(LIBC_NAMESPACE::scandir("", &namelist, NULL, NULL), Fails(ENOENT, -1));
