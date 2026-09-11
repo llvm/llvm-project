e7e3fc1234e4e5829d96489c16922a88101d0c50
Ben Shi <bennshi@tencent.com>
2026-09-11 16:53:54 +0800

    [AVR] Implement selection of 'ISD::FRAMEADDR'

--- llvm/lib/Target/AVR/AVRFrameLowering.cpp
+++ llvm/lib/Target/AVR/AVRFrameLowering.cpp
@@ -245,15 +245,18 @@ StackOffset AVRFrameLowering::getFrameIndexReference(const MachineFunction &MF,
 //  - a register has been spilled
 //  - has allocas
 //  - input arguments are passed using the stack
+//  - has variable sized objects
+//  - the frame address is taken (llvm.frameaddress)
 //
 // Notice that strictly this is not a frame pointer because it contains SP after
 // frame allocation instead of having the original SP in function entry.
 bool AVRFrameLowering::hasFPImpl(const MachineFunction &MF) const {
+  const MachineFrameInfo &MFI = MF.getFrameInfo();
   const AVRMachineFunctionInfo *FuncInfo = MF.getInfo<AVRMachineFunctionInfo>();
 
   return (FuncInfo->getHasSpills() || FuncInfo->getHasAllocas() ||
-          FuncInfo->getHasStackArgs() ||
-          MF.getFrameInfo().hasVarSizedObjects());
+          FuncInfo->getHasStackArgs() || MFI.hasVarSizedObjects() ||
+          MFI.isFrameAddressTaken());
 }
 
 bool AVRFrameLowering::spillCalleeSavedRegisters(
--- llvm/lib/Target/AVR/AVRISelLowering.h
+++ llvm/lib/Target/AVR/AVRISelLowering.h
@@ -108,6 +108,7 @@ private:
   SDValue LowerSELECT_CC(SDValue Op, SelectionDAG &DAG) const;
   SDValue LowerSETCC(SDValue Op, SelectionDAG &DAG) const;
   SDValue LowerVASTART(SDValue Op, SelectionDAG &DAG) const;
+  SDValue LowerFRAMEADDR(SDValue Op, SelectionDAG &DAG) const;
 
   bool CanLowerReturn(CallingConv::ID CallConv, MachineFunction &MF,
                       bool isVarArg,
