dcf5f38df7511c36f88b18e9f8087a0b08b6a645
Vadim Kotov <kotov@google.com>
2026-09-11 12:29:18 -0700

    Minor changes and fixes, commit before sync.

--- libc/src/dirent/scandir.cpp
+++ libc/src/dirent/scandir.cpp
@@ -14,22 +14,24 @@
 #include "src/dirent/scandir.h"
 
 #include "hdr/types/struct_dirent.h"
+/*
+#include "include/llvm-libc-types/__scandir_compare_t.h"
+#include "include/llvm-libc-types/__scandir_filter_t.h"
+*/
 #include "src/__support/common.h"
 #include "src/__support/macros/config.h"
 #include "src/__support/libc_errno.h"
 #include "src/dirent/closedir.h"
 #include "src/dirent/opendir.h"
 #include "src/dirent/readdir.h"
+#include "src/stdlib/qsort_util.h"
 
 namespace LIBC_NAMESPACE_DECL {
 
 LLVM_LIBC_FUNCTION(int, scandir,
                    (const char *dir, struct dirent ***namelist,
-                    int (*sel)(const struct dirent *),
-                    int (*compar)(const struct dirent **,
-                                  const struct dirent **))) {
+                    __scandir_filter_t filter, __scandir_compare_t compare)) {
   (void)namelist;
-  (void)compar;
 
   DIR *dir_fd = LIBC_NAMESPACE::opendir(dir);
   if (dir_fd == nullptr) {
@@ -37,7 +39,12 @@ LLVM_LIBC_FUNCTION(int, scandir,
     return -1;
   }
 
+  struct dirent **namelist_local = {};
+  (void)namelist_local;
+
   struct dirent *entry = nullptr;
+  size_t count = 0;
+
   do {
     libc_errno = 0;
     entry = LIBC_NAMESPACE::readdir(dir_fd);
@@ -46,12 +53,14 @@ LLVM_LIBC_FUNCTION(int, scandir,
       return -1;
     }
 
-    if (sel != nullptr && !sel(entry)) {
+    if (filter != nullptr && !filter(entry)) {
       continue;
     }
 
   } while (entry != nullptr);
 
+  if (compare != nullptr) {
+  }
 
   LIBC_NAMESPACE::closedir(dir_fd);
 
