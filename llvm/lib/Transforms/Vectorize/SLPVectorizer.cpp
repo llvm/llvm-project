bd757dd1e45ca3a793205c8056a251bb97fd1765
Douglas Yung <douglas.yung@sony.com>
2026-09-11 18:46:34 +0000

    Fix lambda added in #222755 which did not build with VS2019.
    This should fix the bot failure on https://lab.llvm.org/buildbot/#/builders/46/builds/41157.

--- llvm/lib/Transforms/Vectorize/SLPVectorizer.cpp
+++ llvm/lib/Transforms/Vectorize/SLPVectorizer.cpp
@@ -33450,7 +33450,7 @@ bool SLPVectorizerPass::vectorizeNonVectorizableInsts(
   constexpr unsigned Limit = 32;
   Changed |= tryToVectorizeSequence<Value>(
       Operands, OperandSorter, AreCompatibleOperands,
-      [this, &R](ArrayRef<Value *> Candidates, bool MaxVFOnly) {
+      [this, &R, Limit](ArrayRef<Value *> Candidates, bool MaxVFOnly) {
         // Limit to StandaloneSeeds if !MaxVFOnly to avoid quadratic scan for
         // large set of candidates.
         return tryToVectorizeList(Candidates, R, MaxVFOnly,
