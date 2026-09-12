fe36473cb62cca28c14b7174c0797cb74bcdfc7e
Ayokunle Amodu <ayokunle321@gmail.com>
2026-09-11 21:16:18 -0400

    [CIR][AMDGPU] Let amdgcn_lerp and ds_swizzle use the generic codegen path (#222204)
    __builtin_amdgcn_lerp and __builtin_amdgcn_ds_swizzle both map 1:1 onto
    concrete (non-overloaded) intrinsics and carry the ClangBuiltin<> marker
    in IntrinsicsAMDGPU.td, so EmitBuiltinExpr already lowers them
    automatically by forwarding the arguments to the intrinsic.

--- clang/lib/CIR/CodeGen/CIRGenBuiltinAMDGPU.cpp
+++ clang/lib/CIR/CodeGen/CIRGenBuiltinAMDGPU.cpp
@@ -203,9 +203,6 @@ CIRGenFunction::emitAMDGPUBuiltinExpr(unsigned builtinId,
   case AMDGPU::BI__builtin_amdgcn_div_fmasf:
     return emitBuiltinWithOneOverloadedType<4>(expr, "amdgcn.div.fmas")
         .getValue();
-  case AMDGPU::BI__builtin_amdgcn_ds_swizzle:
-    return emitBuiltinWithOneOverloadedType<2>(expr, "amdgcn.ds.swizzle")
-        .getValue();
   case AMDGPU::BI__builtin_amdgcn_mov_dpp8:
   case AMDGPU::BI__builtin_amdgcn_mov_dpp:
   case AMDGPU::BI__builtin_amdgcn_update_dpp: {
