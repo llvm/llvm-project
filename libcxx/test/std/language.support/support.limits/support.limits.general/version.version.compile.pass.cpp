c1a27dc302c26693b1bd1ea196a07536f513d119
Steve O'Brien <steve@obrien.cc>
2026-09-12 15:20:27 -0400

    Re-gen some files

--- libcxx/test/std/language.support/support.limits/support.limits.general/version.version.compile.pass.cpp
+++ libcxx/test/std/language.support/support.limits/support.limits.general/version.version.compile.pass.cpp
@@ -10515,7 +10515,7 @@
 #    error "__cpp_lib_sstream_from_string_view should have the value 202306L in c++29"
 #  endif
 
-#  if !defined(_LIBCPP_VERSION)
+#  if !defined(_LIBCPP_VERSION) || _LIBCPP_AVAILABILITY_HAS_STACKTRACE
 #    ifndef __cpp_lib_stacktrace
 #      error "__cpp_lib_stacktrace should be defined in c++29"
 #    endif
@@ -10524,7 +10524,7 @@
 #    endif
 #  else
 #    ifdef __cpp_lib_stacktrace
-#      error "__cpp_lib_stacktrace should not be defined because it is unimplemented in libc++!"
+#      error "__cpp_lib_stacktrace should not be defined when the requirement '!defined(_LIBCPP_VERSION) || _LIBCPP_AVAILABILITY_HAS_STACKTRACE' is not met!"
 #    endif
 #  endif
 
