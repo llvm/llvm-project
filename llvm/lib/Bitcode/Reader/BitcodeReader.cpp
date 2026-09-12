945d59e5afd49d7bd106b56481f34faa817b7f13
Pedro Lobo <pedro.lobo@tecnico.ulisboa.pt>
2026-09-12 18:04:34 +0100

    Validate bit operation operands

--- llvm/lib/Bitcode/Reader/BitcodeReader.cpp
+++ llvm/lib/Bitcode/Reader/BitcodeReader.cpp
@@ -5647,6 +5647,8 @@ Error BitcodeReader::parseFunctionBody(Function *F) {
           getValueTypePair(Record, OpNum, NextValueNo, Offset, OffsetTypeID,
                            CurBB))
         return error("Invalid bitinsert record");
+      if (!BitInsertInst::isValidOperands(Base, Val, Offset))
+        return error("Invalid bitinsert operands");
       I = BitInsertInst::Create(Base, Val, Offset);
       ResTypeID = BaseTypeID;
       InstructionList.push_back(I);
@@ -5667,6 +5669,8 @@ Error BitcodeReader::parseFunctionBody(Function *F) {
           getValueTypePair(Record, OpNum, NextValueNo, Offset, OffsetTypeID,
                            CurBB))
         return error("Invalid bitextract record");
+      if (!BitExtractInst::isValidOperands(ResTy, Src, Offset))
+        return error("Invalid bitextract operands");
       I = BitExtractInst::Create(ResTy, Src, Offset);
       ResTypeID = TypeID;
       InstructionList.push_back(I);
