48e1234a49299c4b457d8bca8adbc7eac6eb3c9c
popie52 <popiesailor@gmail.com>
2026-09-14 00:45:14 +0530

    [libc] Update f128 NaN payload file headers and tests

--- libc/src/math/getpayloadf128.h
+++ libc/src/math/getpayloadf128.h
@@ -1,9 +1,14 @@
-//===-- Implementation header for getpayloadf128 ----------------*- C++ -*-===//
+//===----------------------------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// \brief Implementation header for getpayloadf128.
+///
 //===----------------------------------------------------------------------===//
 
 #ifndef LLVM_LIBC_SRC_MATH_GETPAYLOADF128_H
--- libc/src/math/nanf128.h
+++ libc/src/math/nanf128.h
@@ -1,9 +1,14 @@
-//===-- Implementation header for nanf128 -----------------------*- C++ -*-===//
+//===----------------------------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// \brief Implementation header for nanf128.
+///
 //===----------------------------------------------------------------------===//
 
 #ifndef LLVM_LIBC_SRC_MATH_NANF128_H
