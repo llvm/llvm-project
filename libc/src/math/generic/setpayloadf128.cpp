c1488ae926d908c540b17dc2aebf19dffa7674f8
popie52 <popiesailor@gmail.com>
2026-09-13 07:40:11 +0530

    [libc] Fix formatting for emulated f128 NaN payload functions

--- libc/src/math/generic/setpayloadf128.cpp
+++ libc/src/math/generic/setpayloadf128.cpp
@@ -15,7 +15,8 @@ namespace LIBC_NAMESPACE_DECL {
 using LIBC_NAMESPACE::fputil::Float128;
 
 LLVM_LIBC_FUNCTION(int, setpayloadf128, (float128 * res, float128 pl)) {
-  return math::setpayloadf128(cpp::bit_cast<Float128 *>(res), cpp::bit_cast<Float128>(pl));
+  return math::setpayloadf128(cpp::bit_cast<Float128 *>(res),
+                              cpp::bit_cast<Float128>(pl));
 }
 
 } // namespace LIBC_NAMESPACE_DECL
