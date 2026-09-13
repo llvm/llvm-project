a2f80b530c5fdcf4d91600512e0c0bf646ddd287
Hui Xie <hui.xie1990@gmail.com>
2026-09-13 07:59:25 +0100

    Revert "refactor the thread local queue"
    This reverts commit 02610850dd7131dc04427f6f3214a5c07552ed07.

--- libcxx/src/experimental/include/rcu/rcu_list.h
+++ libcxx/src/experimental/include/rcu/rcu_list.h
@@ -25,15 +25,14 @@ _LIBCPP_BEGIN_NAMESPACE_STD
 
 #if _LIBCPP_STD_VER >= 26 && _LIBCPP_HAS_THREADS && _LIBCPP_HAS_EXPERIMENTAL_RCU
 
+class rcu_thread_local_list_view;
+
 class rcu_singly_list_view {
 private:
   __rcu_node* head_ = nullptr;
   __rcu_node* tail_ = nullptr;
 
 public:
-  rcu_singly_list_view() = default;
-  rcu_singly_list_view(__rcu_node* head, __rcu_node* tail) : head_(head), tail_(tail) {}
-
   void splice_back(rcu_singly_list_view& other) noexcept {
     if (other.head_ == nullptr) {
       return;
@@ -49,6 +48,8 @@ public:
     other.tail_ = nullptr;
   }
 
+  void splice_back(rcu_thread_local_list_view& __other) noexcept;
+
   template <class Func>
   void for_each(Func&& f) noexcept {
     __rcu_node* current = head_;
@@ -61,40 +62,54 @@ public:
   }
 };
 
-struct alignas(2 * sizeof(void*)) rcu_atomic_list_view {
-  __rcu_node* head_ = nullptr;
-  __rcu_node* tail_ = nullptr;
+class rcu_thread_local_list_view {
+  struct alignas(2 * sizeof(void*)) thread_entry {
+    __rcu_node* head_ = nullptr;
+    __rcu_node* tail_ = nullptr;
+  };
 
-  static void push_front(atomic_ref<rcu_atomic_list_view> view_ref, __rcu_node* node) noexcept {
-    auto expected_view = view_ref.load(std::memory_order_relaxed);
-    auto original_next = node->__next_;
+  using per_thread_entries = thread_local_container<thread_entry>;
+
+  friend class rcu_singly_list_view;
+
+public:
+  void push_front(__rcu_node* node) noexcept {
+    atomic_ref<thread_entry> entry_ref = per_thread_entries::get_current_thread_instance();
+    auto expected_entry                = entry_ref.load(std::memory_order_relaxed);
+    auto original_next                 = node->__next_;
     while (true) {
-      auto new_view = [&] {
-        if (expected_view.head_ == nullptr) {
-          return rcu_atomic_list_view{node, node};
+      auto new_entry = [&] {
+        if (expected_entry.head_ == nullptr) {
+          return thread_entry{node, node};
         } else {
-          node->__next_ = expected_view.head_;
-          return rcu_atomic_list_view{node, expected_view.tail_};
+          node->__next_ = expected_entry.head_;
+          return thread_entry{node, expected_entry.tail_};
         }
       }();
-      if (view_ref.compare_exchange_weak(
-              expected_view, new_view, std::memory_order_acq_rel, std::memory_order_relaxed)) {
+      if (entry_ref.compare_exchange_weak(
+              expected_entry, new_entry, std::memory_order_acq_rel, std::memory_order_relaxed)) {
         break;
       } else {
         node->__next_ = original_next;
       }
     }
   }
+};
 
-  static void splice_back(atomic_ref<rcu_atomic_list_view> from_ref, rcu_singly_list_view& to) noexcept {
-    if (from_ref.load(std::memory_order_relaxed).head_ == nullptr) {
+void rcu_singly_list_view::splice_back(rcu_thread_local_list_view& __other) noexcept {
+  using thread_entry             = rcu_thread_local_list_view::thread_entry;
+  const auto splice_single_entry = [this](atomic_ref<thread_entry> entry_ref) noexcept {
+    if (entry_ref.load(std::memory_order_relaxed).head_ == nullptr) {
       return;
     }
-    auto from = from_ref.exchange(rcu_atomic_list_view{nullptr, nullptr}, std::memory_order_acq_rel);
-    rcu_singly_list_view tmp(from.head_, from.tail_);
-    to.splice_back(tmp);
-  }
-};
+    auto entry = entry_ref.exchange(thread_entry{nullptr, nullptr}, std::memory_order_acq_rel);
+    rcu_singly_list_view tmp;
+    tmp.head_ = entry.head_;
+    tmp.tail_ = entry.tail_;
+    this->splice_back(tmp);
+  };
+  rcu_thread_local_list_view::per_thread_entries::for_each(splice_single_entry);
+}
 
 #endif // _LIBCPP_STD_VER >= 26 && _LIBCPP_HAS_THREADS && _LIBCPP_HAS_EXPERIMENTAL_RCU
 
