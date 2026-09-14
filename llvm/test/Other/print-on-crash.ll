llvm/llvm-project

commit 16a770d01f8ef0b4bf15cb903775af64d5d9c8c7

Add cgscc/loop/machine-function variants of TriggerCrashPass (#220073)
For easier testing of e.g. #205682

Assisted-by: Gemini

llvm/include/llvm/CodeGen/TriggerCrashMachineFunction.h
@@ -1,0 +1,25 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM Exceptions
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_CODEGEN_TRIGGERCRASHMACHINEFUNCTION_H
+#define LLVM_CODEGEN_TRIGGERCRASHMACHINEFUNCTION_H
+
+#include "llvm/CodeGen/MachinePassManager.h"
+
+namespace llvm {
+
+class TriggerCrashMachineFunctionPass
+    : public OptionalPassInfoMixin<TriggerCrashMachineFunctionPass> {
+public:
+  LLVM_ABI PreservedAnalyses run(MachineFunction &MF,
+                                 MachineFunctionAnalysisManager &MFAM);
+};
+
+} // namespace llvm
+
+#endif // LLVM_CODEGEN_TRIGGERCRASHMACHINEFUNCTION_H

llvm/include/llvm/Passes/MachinePassRegistry.def
@@ -151,6 +151,8 @@ MACHINE_FUNCTION_PASS("stack-frame-layout", StackFrameLayoutAnalysisPass())
 MACHINE_FUNCTION_PASS("stack-slot-coloring", StackSlotColoringPass())
 MACHINE_FUNCTION_PASS("static-data-splitter", StaticDataSplitterPass())
 MACHINE_FUNCTION_PASS("tailduplication", TailDuplicatePass())
+MACHINE_FUNCTION_PASS("trigger-crash-machine-function",
+                      TriggerCrashMachineFunctionPass())
 MACHINE_FUNCTION_PASS("trigger-verifier-error", TriggerVerifierErrorPass())
 MACHINE_FUNCTION_PASS("two-address-instruction", TwoAddressInstructionPass())
 MACHINE_FUNCTION_PASS("unpack-mi-bundles", UnpackMachineBundlesPass())

llvm/include/llvm/Transforms/Utils/TriggerCrashPass.h
@@ -13,10 +13,14 @@
 #ifndef LLVM_TRANSFORMS_UTILS_TRIGGERCRASHPASS_H
 #define LLVM_TRANSFORMS_UTILS_TRIGGERCRASHPASS_H
 
+#include "llvm/Analysis/CGSCCPassManager.h"
+#include "llvm/Analysis/LazyCallGraph.h"
+#include "llvm/Analysis/LoopAnalysisManager.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/Module.h"
 #include "llvm/IR/PassManager.h"
 #include "llvm/Pass.h"
+#include "llvm/Transforms/Scalar/LoopPassManager.h"
 
 namespace llvm {
 
@@ -30,6 +34,22 @@ class TriggerCrashFunctionPass
     : public OptionalPassInfoMixin<TriggerCrashFunctionPass> {
 public:
   LLVM_ABI PreservedAnalyses run(Function &F, FunctionAnalysisManager &AM);
+};
+
+class TriggerCrashCGSCCPass
+    : public OptionalPassInfoMixin<TriggerCrashCGSCCPass> {
+public:
+  LLVM_ABI PreservedAnalyses run(LazyCallGraph::SCC &C,
+                                 CGSCCAnalysisManager &AM, LazyCallGraph &CG,
+                                 CGSCCUpdateResult &UR);
+};
+
+class TriggerCrashLoopPass
+    : public OptionalPassInfoMixin<TriggerCrashLoopPass> {
+public:
+  LLVM_ABI PreservedAnalyses run(Loop &L, LoopAnalysisManager &AM,
+                                 LoopStandardAnalysisResults &AR,
+                                 LPMUpdater &U);
 };
 
 LLVM_ABI FunctionPass *createTriggerCrashFunctionPass();

llvm/lib/CodeGen/CMakeLists.txt
@@ -258,6 +258,7 @@ add_llvm_component_library(LLVMCodeGen
   TargetRegisterInfo.cpp
   TargetSchedule.cpp
   TargetSubtargetInfo.cpp
+  TriggerCrashMachineFunction.cpp
   TwoAddressInstructionPass.cpp
   TypePromotion.cpp
   UnreachableBlockElim.cpp

llvm/lib/CodeGen/TriggerCrashMachineFunction.cpp
@@ -1,0 +1,18 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM Exceptions
+//
+//===----------------------------------------------------------------------===//
+
+#include "llvm/CodeGen/TriggerCrashMachineFunction.h"
+
+using namespace llvm;
+
+PreservedAnalyses
+TriggerCrashMachineFunctionPass::run(MachineFunction &,
+                                     MachineFunctionAnalysisManager &) {
+  abort();
+  return PreservedAnalyses::all();
+}

llvm/lib/Passes/CodeGenPassBuilder.cpp
@@ -90,6 +90,7 @@
 #include "llvm/CodeGen/StackSlotColoring.h"
 #include "llvm/CodeGen/TailDuplication.h"
 #include "llvm/CodeGen/TargetPassConfig.h"
+#include "llvm/CodeGen/TriggerCrashMachineFunction.h"
 #include "llvm/CodeGen/TwoAddressInstructionPass.h"
 #include "llvm/CodeGen/UnreachableBlockElim.h"
 #include "llvm/CodeGen/VirtRegMap.h"

llvm/lib/Passes/PassBuilder.cpp
@@ -200,6 +200,7 @@
 #include "llvm/CodeGen/StaticDataSplitter.h"
 #include "llvm/CodeGen/TailDuplication.h"
 #include "llvm/CodeGen/TargetPassConfig.h"
+#include "llvm/CodeGen/TriggerCrashMachineFunction.h"
 #include "llvm/CodeGen/TwoAddressInstructionPass.h"
 #include "llvm/CodeGen/TypePromotion.h"
 #include "llvm/CodeGen/UnreachableBlockElim.h"

llvm/lib/Passes/PassRegistry.def
@@ -325,6 +325,7 @@ CGSCC_PASS("invalidate<all>", InvalidateAllAnalysesPass())
 CGSCC_PASS("no-op-cgscc", NoOpCGSCCPass())
 CGSCC_PASS("openmp-opt-cgscc", OpenMPOptCGSCCPass())
 CGSCC_PASS("coro-annotation-elide", CoroAnnotationElidePass())
+CGSCC_PASS("trigger-crash-cgscc", TriggerCrashCGSCCPass())
 #undef CGSCC_PASS
 
 #ifndef CGSCC_PASS_WITH_PARAMS
@@ -799,6 +800,7 @@ LOOP_PASS("print<ddg>", DDGAnalysisPrinterPass(errs()))
 LOOP_PASS("print<hash-recognize>", HashRecognizePrinterPass(errs()))
 LOOP_PASS("print<iv-users>", IVUsersPrinterPass(errs()))
 LOOP_PASS("print<loopnest>", LoopNestPrinterPass(errs()))
+LOOP_PASS("trigger-crash-loop", TriggerCrashLoopPass())
 #undef LOOP_PASS
 
 #ifndef LOOP_PASS_WITH_PARAMS

commit 797d62d47c361d2658c57523f8a2de4d42906ff4

[llvm][test] Extend -print-on-crash testing granularity (#222725)
* Extends the testing for `-print-on-crash` based on the additional
granularity of `trigger-crash` introduced by
https://github.com/llvm/llvm-project/pull/220073

llvm/test/Other/print-on-crash.ll
@@ -17,6 +17,13 @@
 
 ; RUN: not --crash opt -print-on-crash -print-module-scope -passes=trigger-crash-module -filter-passes=blah < %s 2>&1 | FileCheck %s --check-prefix=CHECK_FILTERED
 
+; RUN: not --crash opt -print-on-crash -passes=trigger-crash-function < %s 2>&1 | FileCheck %s --check-prefix=CHECK_FUNCTION
+
+; RUN: not --crash opt -print-on-crash -passes=cgscc(trigger-crash-cgscc) < %s 2>&1 | FileCheck %s --check-prefix=CHECK_CGSCC
+
+; RUN: llc -stop-after=machine-cp %s -o %t.mir
+; RUN: not --crash llc -print-on-crash -passes=trigger-crash-machine-function %t.mir 2>&1 | FileCheck %s --check-prefix=CHECK_MACHINE_FUNCTION
+
 ; CHECK_SIMPLE: ; *** Dump of IR Before Last Pass {{.*}} Started ***
 ; CHECK_SIMPLE: @main
 ; CHECK_SIMPLE: entry:
@@ -24,6 +31,12 @@
 ; CHECK_MODULE: *** Dump of Module IR Before Last Pass {{.*}} Started ***
 ; CHECK_MODULE: ; ModuleID = {{.*}}
 ; CHECK_FILTERED: *** Dump of Module IR Before Last Pass {{.*}} Filtered Out ***
+; CHECK_FUNCTION: *** Dump of IR Before Last Pass {{.*}} Started ***
+; CHECK_FUNCTION: define i32 @main
+; CHECK_CGSCC: *** Dump of IR Before Last Pass {{.*}} Started ***
+; CHECK_CGSCC: define i32 @main
+; CHECK_MACHINE_FUNCTION: *** Dump of IR Before Last Pass {{.*}} Started ***
+; CHECK_MACHINE_FUNCTION: # Machine code for function main
 
 define i32 @main() {
 entry: