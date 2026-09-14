68dea7bbb656c300d90b726d32b8a883c21b361d
Amr Hesham <amr96@programmer.net>
2026-09-14 21:15:35 +0200

    [Clang][Sema] Improve diagnostic when using imag with non complex as lvalue

--- clang/test/SemaCXX/imag-lvalue-with-non-complex-operand.cpp
+++ clang/test/SemaCXX/imag-lvalue-with-non-complex-operand.cpp
@@ -2,19 +2,19 @@
 
 void lvalue_with_imag_int() {
   int i;
-  __imag__ i = 0;   // expected-error {{expression is not assignable}}
+  __imag__ i = 0;   // expected-error {{__imag operator with non-complex type operand is not assignable}}
 }
 
 void lvalue_with_imag_float() {
   float i;
-  __imag__ i = 0;   // expected-error {{expression is not assignable}}
+  __imag__ i = 0;   // expected-error {{__imag operator with non-complex type operand is not assignable}}
 }
 
 _Complex float foo()
 {
   float f;
   __real__ f = 0;
-  __imag__ f = 0;    // expected-error {{expression is not assignable}}
+  __imag__ f = 0;    // expected-error {{__imag operator with non-complex type operand is not assignable}}
   return f;
 }
 
@@ -31,6 +31,6 @@ C lvalue_with_imag_float_with_typedef()
 {
   C f;
   __real__ f = 0;
-  __imag__ f = 0;   // expected-error {{expression is not assignable}}
+  __imag__ f = 0;   // expected-error {{__imag operator with non-complex type operand is not assignable}}
   return f;
 }
