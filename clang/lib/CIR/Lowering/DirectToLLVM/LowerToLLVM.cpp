223aa1c1c326bc6d94bd70ffee6a4ca0c60ce0c5
Sivapriya <sivapriyaofficial2005@gmail.com>
2026-09-14 02:12:46 +0000

    [CIR][CUDA] Address review comments

--- clang/lib/CIR/Lowering/DirectToLLVM/LowerToLLVM.cpp
+++ clang/lib/CIR/Lowering/DirectToLLVM/LowerToLLVM.cpp
@@ -3855,6 +3855,8 @@ static void prepareTypeConverter(mlir::LLVMTypeConverter &converter,
     assert(!cir::MissingFeatures::addressSpace());
     return mlir::LLVM::LLVMPointerType::get(type.getContext());
   });
+  // On the device side, surface reference is represented as an object handle
+  // in 64-bit integer.
   converter.addConversion([&](cir::CUDADeviceSurfaceType type) -> mlir::Type {
     return mlir::IntegerType::get(type.getContext(), 64);
   });
