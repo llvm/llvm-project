b8a7f833017850d13b7cb50f987c45ab299d606e
tdadadavid <davidtofunmidada@gmail.com>
2026-09-14 20:28:02 +0100

    fix: add static back
    Signed-off-by: tdadadavid <davidtofunmidada@gmail.com>

--- libc/src/__support/File/linux/file_flags.h
+++ libc/src/__support/File/linux/file_flags.h
@@ -42,7 +42,7 @@ public:
     return (flag & O_ACCMODE) == O_WRONLY;
   }
 
-  LIBC_INLINE constexpr int file_has_append_flag(int flag) {
+  LIBC_INLINE static constexpr int file_has_append_flag(int flag) {
     return flag & O_APPEND;
   }
 };
