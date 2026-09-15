bf9d04b6a85ae216ff226a4641100588a5b7191a
Shivam Gupta <Shivam98.tkg@gmail.com>
2026-09-15 16:26:25 +0530

    adress review comment for test cases

--- lld/test/ELF/nobits-followed-by-nonzero-section.s
+++ lld/test/ELF/nobits-followed-by-nonzero-section.s
@@ -1,20 +1,17 @@
 # REQUIRES: x86
 ## A non-empty section following .bss must account for .bss's size.
 
-# RUN: llvm-mc -filetype=obj -triple=x86_64 %s -o %t.o
-# RUN: echo 'SECTIONS { \
-# RUN:   .text : {} \
-# RUN:   .data : {} \
-# RUN:   .bss : {} \
-# RUN:   .ldata : { *(.ldata) } \
-# RUN: }' > %t.lds
-# RUN: ld.lld -T %t.lds %t.o -o %t
-# RUN: llvm-readelf -S %t | FileCheck %s
+# RUN: rm -rf %t && split-file %s %t && cd %t
+# RUN: llvm-mc -filetype=obj -triple=x86_64 input.s -o input.o
+# RUN: ld.lld -T script.lds input.o -o output
+# RUN: llvm-readelf -S output | FileCheck %s
 
 ## The .ldata section's file offset must equal .bss's file offset
 ## plus .bss's size.
 # CHECK: .bss     NOBITS   {{[0-9a-f]+}} [[#%.6x,OFF:]] [[#%.6x,SZ:]]
 # CHECK-NEXT: .ldata  PROGBITS {{[0-9a-f]+}} [[#%.6x,OFF+SZ]] 000001
+
+#--- input.s
 
 .text
 .globl _start
@@ -29,3 +26,12 @@ _start:
 
 .section .ldata,"aw"
 .byte 2
+
+#--- script.lds
+
+SECTIONS {
+  .text : {}
+  .data : {}
+  .bss : {}
+  .ldata : { *(.ldata) }
+}
