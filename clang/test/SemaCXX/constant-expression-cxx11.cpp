3bfa3c4f8f44c8e9205e906b6707b300b0f36638
Akshay Dubey <akshaymani513@gmail.com>
2026-09-15 17:35:26 +0530

    [clang][test] Fix C++11 compatibility in GH223064 test case

--- clang/test/SemaCXX/constant-expression-cxx11.cpp
+++ clang/test/SemaCXX/constant-expression-cxx11.cpp
@@ -2752,20 +2752,19 @@ namespace GH223064 {
   struct A { int n; };
   struct B : A {} b[2];
 
-  constexpr int *f() {
-    A *p = b;
+  constexpr int *f(A *p) {
     return &static_cast<B*>(p)[1].n;
   }
-  static_assert(f() == &b[1].n, "");
+  static_assert(f(b) == &b[1].n, "");
 
   struct Base1 { int x; };
   struct Base2 : Base1 { int y; };
   struct Derived : Base2 { int z; } arr[3];
 
-  constexpr int *g() {
-    Base1 *p = arr;
+  constexpr int *g(Base1 *p) {
     return &static_cast<Derived*>(p)[2].z;
   }
-  static_assert(g() == &arr[2].z, "");
+  static_assert(g(arr) == &arr[2].z, "");
 }
+
 
