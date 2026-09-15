a86a8d947f56ee77182dd5a7351547c2f93497b7
Dave Lee <davelee.com@gmail.com>
2026-09-15 10:43:20 -0700

    [lldb] Add APSInt literal support to formatter_bytecode.py

--- lldb/examples/python/formatter_bytecode.py
+++ lldb/examples/python/formatter_bytecode.py
@@ -61,6 +61,7 @@ define_opcode(0x20, None, "lit_uint")
 define_opcode(0x21, None, "lit_int")
 define_opcode(0x22, None, "lit_string")
 define_opcode(0x23, None, "lit_selector")
+define_opcode(0x24, None, "lit_integer")
 
 define_opcode(0x2A, "as_int", "as_int")
 define_opcode(0x2B, "as_uint", "as_uint")
@@ -398,7 +399,9 @@ def assemble_tokens(tokens: list[str]) -> bytes:
                 emit(op_lit_uint)
                 emit(int(tok[:-1]))  # FIXME
             else:
-                emit(op_lit_int)
+                # With the introduction of op_lit_integer, op_lit_int is no
+                # longer emitted by the assembler.
+                emit(op_lit_integer)
                 emit(int(tok))  # FIXME
         elif tok[0] == "@":
             emit(op_lit_selector)
@@ -472,6 +475,9 @@ def disassemble(bytecode: bytes) -> Tuple[str, list[int]]:
             asm += str(b)  # FIXME uleb
             asm += "u"
         elif b == op_lit_int:
+            b = next_byte()
+            asm += str(b)
+        elif b == op_lit_integer:
             b = next_byte()
             asm += str(b)
         elif b == op_lit_selector:
@@ -616,6 +622,9 @@ def interpret(bytecode: bytes, control: list, data: list, tracing: bool = False)
             data.append(int(b))
         elif b == op_lit_int:
             b = next_byte()  # FIXME uleb
+            data.append(int(b))
+        elif b == op_lit_integer:
+            b = next_byte()  # FIXME sleb
             data.append(int(b))
         elif b == op_lit_selector:
             b = next_byte()
@@ -1251,7 +1260,7 @@ if __name__ == "__main__":
         def test_assemble(self):
             self.assertEqual(assemble("1u dup").hex(), "200101")
             self.assertEqual(assemble('"1u dup"').hex(), "2206317520647570")
-            self.assertEqual(assemble("16 < { dup } if").hex(), "21105210010111")
+            self.assertEqual(assemble("16 < { dup } if").hex(), "24105210010111")
             self.assertEqual(assemble('{ { " } " } }').hex(), "100710052203207d20")
 
             def roundtrip(asm):
