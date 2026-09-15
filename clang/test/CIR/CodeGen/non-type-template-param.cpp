765966be3835f54f04d3288f79a9a55128019334
amanmaurya92 <amanmaurya9209@gmail.com>
2026-09-15 11:11:38 +0530

    [CIR] Support SubstNonTypeTemplateParmExpr for aggregates
    Visit the replacement expression for aggregate NTTPs, matching classic Clang codegen and existing CIR NTTP emitters. The gap was analyzed with LLM assistance.
    
    Issue #223533
    
    Assisted-by: Antigravity

--- clang/test/CIR/CodeGen/non-type-template-param.cpp
+++ clang/test/CIR/CodeGen/non-type-template-param.cpp
@@ -1,8 +1,8 @@
-// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -Wno-unused-value -fclangir -emit-cir %s -o %t.cir
+// RUN: %clang_cc1 -std=c++20 -triple x86_64-unknown-linux-gnu -Wno-unused-value -fclangir -emit-cir %s -o %t.cir
 // RUN: FileCheck --input-file=%t.cir %s -check-prefix=CIR
-// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -Wno-unused-value -fclangir -emit-llvm %s -o %t-cir.ll
+// RUN: %clang_cc1 -std=c++20 -triple x86_64-unknown-linux-gnu -Wno-unused-value -fclangir -emit-llvm %s -o %t-cir.ll
 // RUN: FileCheck --input-file=%t-cir.ll %s -check-prefix=LLVM
-// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -Wno-unused-value -emit-llvm %s -o %t.ll
+// RUN: %clang_cc1 -std=c++20 -triple x86_64-unknown-linux-gnu -Wno-unused-value -emit-llvm %s -o %t.ll
 // RUN: FileCheck --input-file=%t.ll %s -check-prefix=OGCG
 
 template <const int N>
@@ -24,4 +24,31 @@ void template_foo() {
 
 void foo() {
   template_foo<1>();
+}
+
+struct Point {
+  int x;
+  int y;
+};
+
+template <Point P>
+void template_agg() {
+  Point a = P;
+}
+
+// CIR: %[[A:.*]] = cir.alloca {{.*}}
+// CIR: %[[GLOBAL:.*]] = cir.get_global @{{.*}}
+// CIR: cir.copy %[[GLOBAL]] to %[[A]] : !cir.ptr<{{.*}}>
+// CIR: cir.return
+
+// LLVM: %[[A:.*]] = alloca %struct.Point
+// LLVM: call void @llvm.memcpy.p0.p0.i64(ptr align {{[0-9]+}} %[[A]], ptr align {{[0-9]+}} @{{.*}}, i64 8, i1 false)
+// LLVM: ret void
+
+// OGCG: %[[A:.*]] = alloca %struct.Point
+// OGCG: call void @llvm.memcpy.p0.p0.i64(ptr align {{[0-9]+}} %[[A]], ptr align {{[0-9]+}} @{{.*}}, i64 8, i1 false)
+// OGCG: ret void
+
+void bar() {
+  template_agg<Point{1, 2}>();
 }
