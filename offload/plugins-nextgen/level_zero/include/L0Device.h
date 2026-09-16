16fa29479ab7f9d72a69d48d7d735790cdf4b560
Hansang Bae <hansang.bae@intel.com>
2026-09-16 10:19:04 -0500

    Fix format

--- offload/plugins-nextgen/level_zero/include/L0Device.h
+++ offload/plugins-nextgen/level_zero/include/L0Device.h
@@ -482,11 +482,11 @@ public:
   // Allocation related routines.
 
   /// Data alloc.
-  Expected<void *> dataAlloc(
-      size_t Size, size_t Align, int32_t Kind, intptr_t Offset, bool UserAlloc,
-      bool DevMalloc = false,
-      uint32_t MemAdvice = std::numeric_limits<uint32_t>::max(),
-      AllocOptionTy AllocOpt = AllocOptionTy::ALLOC_OPT_NONE);
+  Expected<void *>
+  dataAlloc(size_t Size, size_t Align, int32_t Kind, intptr_t Offset,
+            bool UserAlloc, bool DevMalloc = false,
+            uint32_t MemAdvice = std::numeric_limits<uint32_t>::max(),
+            AllocOptionTy AllocOpt = AllocOptionTy::ALLOC_OPT_NONE);
 
   /// Data delete.
   Error dataDelete(void *Ptr);
