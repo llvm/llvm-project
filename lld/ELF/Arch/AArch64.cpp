Zydak/leet-llvm-project

[PAC][lld] Do not emit AUTH relocs against undef weak non-preemptible symbols
Undefined weak non-preemptible symbols should be statically resolved
and not signed.

See the corresponding relaxation described in docs:
https://github.com/ARM-software/abi-aa/blob/6e0d6611ac977628af7b2444ff841e76931a3557/design-documents/pauthabi-tls.rst

Previously, a dynamic relocation against such symbols was emitted,
which is not a correct behavior.

See also: https://github.com/ARM-software/abi-aa/pull/391

Depends on: #198327

Resolves #173296

lld/ELF/Arch/AArch64.cpp
@@ -103,6 +103,8 @@ private:
   void relaxTlsGdToLe(uint8_t *loc, const Relocation &rel, uint64_t val) const;
   void relaxTlsGdToIe(uint8_t *loc, const Relocation &rel, uint64_t val) const;
   void relaxTlsIeToLe(uint8_t *loc, const Relocation &rel, uint64_t val) const;
+  void relaxAuthTlsDescForNonPreemptibleUndefWeak(uint8_t *loc,
+                                                  const Relocation &rel) const;
 };
 
 struct AArch64Relaxer {
@@ -212,6 +214,20 @@ void AArch64::scanSectionImpl(InputSectionBase &sec, Relocs<RelTy> rels,
     // Relocation types that only need a RelExpr set `expr` and break out of
     // the switch to reach rs.process(). Types that need special handling
     // (fast-path helpers, TLS) call a handler and use `continue`.
+
+    auto handleTlsDescAuth = [&sym, &sec, type, offset,
+                              addend](RelExpr tlsdescExpr) {
+      sym.setFlags(NEEDS_TLSDESC_AUTH);
+      if (sym.isUndefWeak() && !sym.isPreemptible) {
+        // Resolves statically to null. Handle in
+        // relaxAuthTlsDescForNonPreemptibleUndefWeak
+        sec.addReloc({R_TPREL, type, offset, addend, &sym});
+      } else {
+        sym.setFlags(NEEDS_TLSDESC);
+        sec.addReloc({tlsdescExpr, type, offset, addend, &sym});
+      }
+    };
+
     switch (type) {
     case R_AARCH64_NONE:
       continue;
@@ -363,13 +379,16 @@ void AArch64::scanSectionImpl(InputSectionBase &sec, Relocs<RelTy> rels,
     // only supports the descriptor based TLS (TLSDESC).
     // https://github.com/ARM-software/abi-aa/blob/main/pauthabielf64/pauthabielf64.rst#general-restrictions
     case R_AARCH64_AUTH_TLSDESC_ADR_PAGE21:
-      sym.setFlags(NEEDS_TLSDESC | NEEDS_TLSDESC_AUTH);
-      sec.addReloc({RE_AARCH64_TLSDESC_PAGE, type, offset, addend, &sym});
+      handleTlsDescAuth(RE_AARCH64_TLSDESC_PAGE);
       continue;
     case R_AARCH64_AUTH_TLSDESC_LD64_LO12:
     case R_AARCH64_AUTH_TLSDESC_ADD_LO12:
-      sym.setFlags(NEEDS_TLSDESC | NEEDS_TLSDESC_AUTH);
-      sec.addReloc({R_TLSDESC, type, offset, addend, &sym});
+      handleTlsDescAuth(R_TLSDESC);
+      continue;
+    case R_AARCH64_AUTH_TLSDESC_CALL:
+      sym.setFlags(NEEDS_TLSDESC_AUTH);
+      if (sym.isUndefWeak() && !sym.isPreemptible)
+        sec.addReloc({R_TPREL, type, offset, addend, &sym});
       continue;
 
     default:
@@ -654,11 +673,22 @@ void AArch64::relocate(uint8_t *loc, const Relocation &rel,
     write64(ctx, loc, val);
     break;
   case R_AARCH64_AUTH_ABS64:
-    // This is used for the addend of a .relr.auth.dyn entry,
-    // which is a 32-bit value; the upper 32 bits are used to
-    // encode the schema.
-    checkInt(ctx, loc, val, 32, rel);
-    write32(ctx, loc, val);
+    if (rel.sym->isUndefined() && !rel.sym->isPreemptible) {
+      // Undefined weak non-preemptible symbols are statically resolved to the
+      // addend. No dynamic relocation and corresponding signing schema encoding
+      // is needed.
+      //
+      // Note: at this point, binding of undefined weak non-preemptible symbols
+      // has already been changed from weak to local by computeBinding call, so
+      // just check against isUndefined().
+      write64(ctx, loc, val);
+    } else {
+      // This is used for the addend of a .relr.auth.dyn entry,
+      // which is a 32-bit value; the upper 32 bits are used to
+      // encode the schema.
+      checkInt(ctx, loc, val, 32, rel);
+      write32(ctx, loc, val);
+    }
     break;
   case R_AARCH64_TLS_DTPREL64:
     write64(ctx, loc, val);
@@ -817,6 +847,37 @@ void AArch64::relocate(uint8_t *loc, const Relocation &rel,
     break;
   default:
     llvm_unreachable("unknown relocation");
+  }
+}
+
+void AArch64::relaxAuthTlsDescForNonPreemptibleUndefWeak(
+    uint8_t *loc, const Relocation &rel) const {
+  // AUTH TLSDESC relocations are in the form:
+  //   adrp x0, :tlsdesc_auth:v             [R_AARCH64_AUTH_TLSDESC_ADR_PAGE21]
+  //   ldr  x16, [x0, :tlsdesc_auth_lo12:v] [R_AARCH64_AUTH_TLSDESC_LD64_LO12]
+  //   add  x0, x0, :tlsdesc_auth_lo12:v    [R_AARCH64_AUTH_TLSDESC_ADD_LO12]
+  //   .tlsdescauthcall v                   [R_AARCH64_AUTH_TLSDESC_CALL]
+  //   blraa x16, x0
+  // And it can optimized to:
+  //   mrs  x0, tpidr_el0
+  //   neg  x0, x0
+  //   nop
+  //   nop
+
+  switch (rel.type) {
+  case R_AARCH64_AUTH_TLSDESC_ADR_PAGE21:
+    write32le(loc, 0xd53bd040); // mrs x0, tpidr_el0
+    return;
+  case R_AARCH64_AUTH_TLSDESC_LD64_LO12:
+    write32le(loc, 0xcb0003e0); // neg x0, x0
+    return;
+  case R_AARCH64_AUTH_TLSDESC_ADD_LO12:
+  case R_AARCH64_AUTH_TLSDESC_CALL:
+    write32le(loc, 0xd503201f); // nop
+    return;
+  default:
+    llvm_unreachable("unsupported relocation for non-preemptible undefined "
+                     "weak AUTH TLSDESC relaxation");
   }
 }
 
@@ -1112,6 +1173,15 @@ void AArch64::relocateAlloc(InputSection &sec, uint8_t *buf) const {
         relaxTlsGdToLe(loc, rel, val);
       else if (rel.expr == RE_AARCH64_GOT_PAGE_PC || rel.expr == R_GOT)
         relaxTlsGdToIe(loc, rel, val);
+      else
+        relocate(loc, rel, val);
+      continue;
+    case R_AARCH64_AUTH_TLSDESC_ADR_PAGE21:
+    case R_AARCH64_AUTH_TLSDESC_LD64_LO12:
+    case R_AARCH64_AUTH_TLSDESC_ADD_LO12:
+    case R_AARCH64_AUTH_TLSDESC_CALL:
+      if (rel.expr == R_TPREL)
+        relaxAuthTlsDescForNonPreemptibleUndefWeak(loc, rel);
       else
         relocate(loc, rel, val);
       continue;

llvm/llvm-project

lld/ELF/Arch/AArch64.cpp
@@ -103,6 +103,8 @@ private:
   void relaxTlsGdToLe(uint8_t *loc, const Relocation &rel, uint64_t val) const;
   void relaxTlsGdToIe(uint8_t *loc, const Relocation &rel, uint64_t val) const;
   void relaxTlsIeToLe(uint8_t *loc, const Relocation &rel, uint64_t val) const;
+  void relaxAuthTlsDescForNonPreemptibleUndefined(uint8_t *loc,
+                                                  const Relocation &rel) const;
 };
 
 struct AArch64Relaxer {
@@ -212,6 +214,19 @@ void AArch64::scanSectionImpl(InputSectionBase &sec, Relocs<RelTy> rels,
     // Relocation types that only need a RelExpr set `expr` and break out of
     // the switch to reach rs.process(). Types that need special handling
     // (fast-path helpers, TLS) call a handler and use `continue`.
+
+    auto handleTlsDescAuth = [&sym, &sec, type, offset,
+                              addend](RelExpr tlsdescExpr) {
+      if (sym.isUndefined() && !sym.isPreemptible) {
+        // Resolves statically to null. Handle in
+        // relaxAuthTlsDescForNonPreemptibleUndefined
+        sec.addReloc({R_TPREL, type, offset, addend, &sym});
+      } else {
+        sym.setFlags(NEEDS_TLSDESC_AUTH);
+        sec.addReloc({tlsdescExpr, type, offset, addend, &sym});
+      }
+    };
+
     switch (type) {
     case R_AARCH64_NONE:
       continue;
@@ -362,13 +377,17 @@ void AArch64::scanSectionImpl(InputSectionBase &sec, Relocs<RelTy> rels,
     // only supports the descriptor based TLS (TLSDESC).
     // https://github.com/ARM-software/abi-aa/blob/main/pauthabielf64/pauthabielf64.rst#general-restrictions
     case R_AARCH64_AUTH_TLSDESC_ADR_PAGE21:
-      sym.setFlags(NEEDS_TLSDESC_AUTH);
-      sec.addReloc({RE_AARCH64_TLSDESC_PAGE, type, offset, addend, &sym});
+      handleTlsDescAuth(RE_AARCH64_TLSDESC_PAGE);
       continue;
     case R_AARCH64_AUTH_TLSDESC_LD64_LO12:
     case R_AARCH64_AUTH_TLSDESC_ADD_LO12:
-      sym.setFlags(NEEDS_TLSDESC_AUTH);
-      sec.addReloc({R_TLSDESC, type, offset, addend, &sym});
+      handleTlsDescAuth(R_TLSDESC);
+      continue;
+    case R_AARCH64_AUTH_TLSDESC_CALL:
+      if (sym.isUndefined() && !sym.isPreemptible)
+        sec.addReloc({R_TPREL, type, offset, addend, &sym});
+      else
+        sym.setFlags(NEEDS_TLSDESC_AUTH);
       continue;
 
     default:
@@ -653,11 +672,22 @@ void AArch64::relocate(uint8_t *loc, const Relocation &rel,
     write64(ctx, loc, val);
     break;
   case R_AARCH64_AUTH_ABS64:
-    // This is used for the addend of a .relr.auth.dyn entry,
-    // which is a 32-bit value; the upper 32 bits are used to
-    // encode the schema.
-    checkInt(ctx, loc, val, 32, rel);
-    write32(ctx, loc, val);
+    if (rel.sym->isUndefined() && !rel.sym->isPreemptible) {
+      // Undefined weak non-preemptible symbols are statically resolved to the
+      // addend. No dynamic relocation and corresponding signing schema encoding
+      // is needed.
+      //
+      // Note: at this point, binding of undefined weak non-preemptible symbols
+      // has already been changed from weak to local by computeBinding call, so
+      // just check against isUndefined().
+      write64(ctx, loc, val);
+    } else {
+      // This is used for the addend of a .relr.auth.dyn entry,
+      // which is a 32-bit value; the upper 32 bits are used to
+      // encode the schema.
+      checkInt(ctx, loc, val, 32, rel);
+      write32(ctx, loc, val);
+    }
     break;
   case R_AARCH64_TLS_DTPREL64:
     write64(ctx, loc, val);
@@ -816,6 +846,37 @@ void AArch64::relocate(uint8_t *loc, const Relocation &rel,
     break;
   default:
     llvm_unreachable("unknown relocation");
+  }
+}
+
+void AArch64::relaxAuthTlsDescForNonPreemptibleUndefined(
+    uint8_t *loc, const Relocation &rel) const {
+  // AUTH TLSDESC relocations are in the form:
+  //   adrp x0, :tlsdesc_auth:v             [R_AARCH64_AUTH_TLSDESC_ADR_PAGE21]
+  //   ldr  x16, [x0, :tlsdesc_auth_lo12:v] [R_AARCH64_AUTH_TLSDESC_LD64_LO12]
+  //   add  x0, x0, :tlsdesc_auth_lo12:v    [R_AARCH64_AUTH_TLSDESC_ADD_LO12]
+  //   .tlsauthdesccall v                   [R_AARCH64_AUTH_TLSDESC_CALL]
+  //   blraa x16, x0
+  // And it can optimized to:
+  //   mrs  x0, tpidr_el0
+  //   neg  x0, x0
+  //   nop
+  //   nop
+
+  switch (rel.type) {
+  case R_AARCH64_AUTH_TLSDESC_ADR_PAGE21:
+    write32le(loc, 0xd53bd040); // mrs x0, tpidr_el0
+    return;
+  case R_AARCH64_AUTH_TLSDESC_LD64_LO12:
+    write32le(loc, 0xcb0003e0); // neg x0, x0
+    return;
+  case R_AARCH64_AUTH_TLSDESC_ADD_LO12:
+  case R_AARCH64_AUTH_TLSDESC_CALL:
+    write32le(loc, 0xd503201f); // nop
+    return;
+  default:
+    llvm_unreachable("unsupported relocation for non-preemptible undefined "
+                     "weak AUTH TLSDESC relaxation");
   }
 }
 
@@ -1111,6 +1172,15 @@ void AArch64::relocateAlloc(InputSection &sec, uint8_t *buf) const {
         relaxTlsGdToLe(loc, rel, val);
       else if (rel.expr == RE_AARCH64_GOT_PAGE_PC || rel.expr == R_GOT)
         relaxTlsGdToIe(loc, rel, val);
+      else
+        relocate(loc, rel, val);
+      continue;
+    case R_AARCH64_AUTH_TLSDESC_ADR_PAGE21:
+    case R_AARCH64_AUTH_TLSDESC_LD64_LO12:
+    case R_AARCH64_AUTH_TLSDESC_ADD_LO12:
+    case R_AARCH64_AUTH_TLSDESC_CALL:
+      if (rel.expr == R_TPREL)
+        relaxAuthTlsDescForNonPreemptibleUndefined(loc, rel);
       else
         relocate(loc, rel, val);
       continue;