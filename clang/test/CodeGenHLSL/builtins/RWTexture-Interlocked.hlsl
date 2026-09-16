dcae8adaa34f566a9f40c9bcc1d2670bc8461e61
Joshua Batista <jbatista@microsoft.com>
2026-09-16 13:53:46 -0700

    Add InterlockedMax coverage to the texture test

--- clang/test/CodeGenHLSL/builtins/RWTexture-Interlocked.hlsl
+++ clang/test/CodeGenHLSL/builtins/RWTexture-Interlocked.hlsl
@@ -31,6 +31,10 @@ RWTexture2DArray<uint> UOut : register(u1);
 // DXCHECK:  atomicrmw xor ptr %[[PTR4]], i32 1 syncscope("device") monotonic
 // DXCHECK:  %[[PTR5:.*]] = call {{.*}} @llvm.dx.resource.getpointer.{{.*}}(target("dx.Texture", i32, 1, 0, 0, 7) %{{.*}}, <3 x i32> %{{.*}})
 // DXCHECK:  atomicrmw umin ptr %[[PTR5]], i32 1 syncscope("device") monotonic
+// DXCHECK:  %[[PTR6:.*]] = call {{.*}} @llvm.dx.resource.getpointer.{{.*}}(target("dx.Texture", i32, 1, 0, 1, 2) %{{.*}}, <2 x i32> %{{.*}})
+// DXCHECK:  atomicrmw max ptr %[[PTR6]], i32 1 syncscope("device") monotonic
+// DXCHECK:  %[[PTR7:.*]] = call {{.*}} @llvm.dx.resource.getpointer.{{.*}}(target("dx.Texture", i32, 1, 0, 0, 7) %{{.*}}, <3 x i32> %{{.*}})
+// DXCHECK:  atomicrmw umax ptr %[[PTR7]], i32 1 syncscope("device") monotonic
 // SPVCHECK: %[[PTR1:.*]] = call {{.*}} @llvm.spv.resource.getpointer.{{.*}}(target("spirv.SignedImage", i32, {{.*}}) %{{.*}}, <2 x i32> %{{.*}})
 // SPVCHECK: atomicrmw add ptr addrspace(11) %[[PTR1]], i32 1 syncscope("device") monotonic
 // SPVCHECK: %[[PTR2:.*]] = call {{.*}} @llvm.spv.resource.getpointer.{{.*}}(target("spirv.SignedImage", i32, {{.*}}) %{{.*}}, <2 x i32> %{{.*}})
@@ -41,6 +45,10 @@ RWTexture2DArray<uint> UOut : register(u1);
 // SPVCHECK: atomicrmw xor ptr addrspace(11) %[[PTR4]], i32 1 syncscope("device") monotonic
 // SPVCHECK: %[[PTR5:.*]] = call {{.*}} @llvm.spv.resource.getpointer.{{.*}}(target("spirv.Image", i32, {{.*}}) %{{.*}}, <3 x i32> %{{.*}})
 // SPVCHECK: atomicrmw umin ptr addrspace(11) %[[PTR5]], i32 1 syncscope("device") monotonic
+// SPVCHECK: %[[PTR6:.*]] = call {{.*}} @llvm.spv.resource.getpointer.{{.*}}(target("spirv.SignedImage", i32, {{.*}}) %{{.*}}, <2 x i32> %{{.*}})
+// SPVCHECK: atomicrmw max ptr addrspace(11) %[[PTR6]], i32 1 syncscope("device") monotonic
+// SPVCHECK: %[[PTR7:.*]] = call {{.*}} @llvm.spv.resource.getpointer.{{.*}}(target("spirv.Image", i32, {{.*}}) %{{.*}}, <3 x i32> %{{.*}})
+// SPVCHECK: atomicrmw umax ptr addrspace(11) %[[PTR7]], i32 1 syncscope("device") monotonic
 [shader("compute")]
 [numthreads(1,1,1)]
 void main(uint3 id : SV_DispatchThreadID) {
@@ -49,4 +57,6 @@ void main(uint3 id : SV_DispatchThreadID) {
   InterlockedOr(UOut[id], 1u);
   InterlockedXor(UOut[id], 1u);
   InterlockedMin(UOut[id], 1u);
+  InterlockedMax(Out[id.xy], 1);
+  InterlockedMax(UOut[id], 1u);
 }
