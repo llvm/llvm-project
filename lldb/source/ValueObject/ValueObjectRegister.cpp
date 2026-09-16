9a70ed9a8909204ecfc6c767c5af9a42996745be
Charles Zablit <c_zablit@apple.com>
2026-09-16 10:22:57 +0100

    Track a single match in LookupChildWithName

--- lldb/source/ValueObject/ValueObjectRegister.cpp
+++ lldb/source/ValueObject/ValueObjectRegister.cpp
@@ -141,9 +141,9 @@ ValueObjectRegisterSet::LookupChildWithName(llvm::StringRef name) {
 
   // See if this register is in this register set, falling back to the name as
   // written. The generic name may alias a register in another set while this
-  // set has a register of that exact name.
-  std::optional<std::pair<size_t, const RegisterInfo *>> name_match;
-  std::optional<std::pair<size_t, const RegisterInfo *>> alt_name_match;
+  // set has a register of that exact name. A match on the name itself always
+  // wins over one on the alt name.
+  std::optional<std::pair<size_t, const RegisterInfo *>> match;
   for (size_t i = 0; i < m_reg_set->num_registers; ++i) {
     const RegisterInfo *contained_reg_info =
         m_reg_ctx_sp->GetRegisterInfoAtIndex(m_reg_set->registers[i]);
@@ -151,14 +151,13 @@ ValueObjectRegisterSet::LookupChildWithName(llvm::StringRef name) {
       continue;
     if (IsSameRegister(contained_reg_info, reg_info))
       return std::make_pair(i, reg_info);
-    if (!name_match && name.equals_insensitive(contained_reg_info->name))
-      name_match = std::make_pair(i, contained_reg_info);
-    else if (!alt_name_match &&
-             name.equals_insensitive(contained_reg_info->alt_name))
-      alt_name_match = std::make_pair(i, contained_reg_info);
+    if (name.equals_insensitive(contained_reg_info->name))
+      match = std::make_pair(i, contained_reg_info);
+    else if (!match && name.equals_insensitive(contained_reg_info->alt_name))
+      match = std::make_pair(i, contained_reg_info);
   }
 
-  return name_match ? name_match : alt_name_match;
+  return match;
 }
 
 lldb::ValueObjectSP
