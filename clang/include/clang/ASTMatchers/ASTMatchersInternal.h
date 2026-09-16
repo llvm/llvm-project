3eb0723985a3fa374e21b268e034afc7288efd9a
Eli Friedman <efriedma@qti.qualcomm.com>
2026-09-15 19:45:35 -0700

    [clang][NFC] Mark some ASTMatchers constructors constexpr.
    Suppresses a bunch of -Wglobal-constructors warnings in
    ASTMatchersInternal.cpp .

--- clang/include/clang/ASTMatchers/ASTMatchersInternal.h
+++ clang/include/clang/ASTMatchers/ASTMatchersInternal.h
@@ -1307,7 +1307,7 @@ class VariadicDynCastAllOfMatcher
     : public VariadicFunction<BindableMatcher<SourceT>, Matcher<TargetT>,
                               makeDynCastAllOfComposite<SourceT, TargetT>> {
 public:
-  VariadicDynCastAllOfMatcher() {}
+  constexpr VariadicDynCastAllOfMatcher() {}
 };
 
 /// A \c VariadicAllOfMatcher<T> object is a variadic functor that takes
@@ -1325,7 +1325,7 @@ class VariadicAllOfMatcher
     : public VariadicFunction<BindableMatcher<T>, Matcher<T>,
                               makeAllOfComposite<T>> {
 public:
-  VariadicAllOfMatcher() {}
+  constexpr VariadicAllOfMatcher() {}
 };
 
 /// VariadicOperatorMatcher related types.
@@ -1886,7 +1886,7 @@ public:
 
   struct Func
       : public VariadicFunction<Self, Matcher<InnerTBase>, &Self::create> {
-    Func() {}
+    constexpr Func() {}
   };
 
 private:
