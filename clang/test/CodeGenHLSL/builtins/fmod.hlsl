5171e77ff7a8c0422c9ad79619c4fa26c8da13e3
Farzon Lotfi <farzonlotfi@microsoft.com>
2026-09-16 15:37:21 -0400

    [HLSL][LongVector] Implement fmod
    resolves https://github.com/llvm/llvm-project/issues/220649
    
    This is as much a refactor as it is adding long vector support.
    
    I noticed the fmod helper had many issues. THe SPIR-V
    implementatiion wasn't properly put into a SPIR-V macro
    
    fmod had a scalar \ vector version that was unecessary.
    
    The tests seemed to be raw register defines so
    source code changes caused the names to all change.
    
    Fixing this meant we could simplify the tablegen
    and helpers.
    
    Also the tests are much more clean with less -D defines

--- clang/test/CodeGenHLSL/builtins/fmod.hlsl
+++ clang/test/CodeGenHLSL/builtins/fmod.hlsl
@@ -4,16 +4,14 @@
 //
 // RUN: %clang_cc1 -finclude-default-header -x hlsl -triple \
 // RUN:   dxil-pc-shadermodel6.3-library %s -fnative-half-type -fnative-int16-type \
-// RUN:   -emit-llvm -o - | FileCheck %s -DFNATTRS="hidden noundef nofpclass(nan inf)" \
-// RUN:   -DTYPE=half -DINT_TYPE=f16 --check-prefixes=DXCHECK
+// RUN:   -emit-llvm -o - | FileCheck %s --check-prefixes=CHECK,DXCHECK
 
 //
 // ---------- No Native Half support test -----------
 //
 // RUN: %clang_cc1 -finclude-default-header -x hlsl -triple \
 // RUN:   dxil-pc-shadermodel6.3-library %s -emit-llvm \
-// RUN:   -o - | FileCheck %s -DFNATTRS="hidden noundef nofpclass(nan inf)" \
-// RUN:   -DTYPE=float -DINT_TYPE=f32 --check-prefixes=DXCHECK
+// RUN:   -o - | FileCheck %s --check-prefixes=CHECK,DXCHECK
 
 
 // Spirv target:
@@ -23,7 +21,7 @@
 // RUN: %clang_cc1 -finclude-default-header -x hlsl -triple \
 // RUN:   spirv-unknown-vulkan-library %s -fnative-half-type -fnative-int16-type \
 // RUN:   -emit-llvm -o - | FileCheck %s \
-// RUN:   -DFNATTRS="hidden spir_func noundef nofpclass(nan inf)" -DTYPE=half
+// RUN:   --check-prefixes=CHECK,SPVCHECK
 
 //
 // ---------- No Native Half support test -----------
@@ -31,119 +29,118 @@
 // RUN: %clang_cc1 -finclude-default-header -x hlsl -triple \
 // RUN:   spirv-unknown-vulkan-library %s -emit-llvm \
 // RUN:   -o - | FileCheck %s \
-// RUN:   -DFNATTRS="hidden spir_func noundef nofpclass(nan inf)" -DTYPE=float
+// RUN:   --check-prefixes=CHECK,SPVCHECK
 
 
 
-// DXCHECK: define [[FNATTRS]] [[TYPE]] @
-// DXCHECK: %div1.i = fdiv reassoc nnan ninf nsz arcp afn [[TYPE]] %{{.*}}, %{{.*}}
-// DXCHECK: %cmp.i = fcmp reassoc nnan ninf nsz arcp afn oge [[TYPE]] %{{.*}}, 0
-// DXCHECK: %elt.abs.i = call reassoc nnan ninf nsz arcp afn [[TYPE]] @llvm.fabs.[[INT_TYPE]]([[TYPE]] %{{.*}})
-// DXCHECK: %hlsl.frac.i = call reassoc nnan ninf nsz arcp afn [[TYPE]] @llvm.dx.frac.[[INT_TYPE]]([[TYPE]] %elt.abs.i)
-// DXCHECK: %fneg.i = fneg reassoc nnan ninf nsz arcp afn [[TYPE]] %{{.*}}
-// DXCHECK: %hlsl.select.i = select reassoc nnan ninf nsz arcp afn i1 %{{.*}}, [[TYPE]] %{{.*}}, [[TYPE]] %fneg.i
-// DXCHECK: %mul.i = fmul reassoc nnan ninf nsz arcp afn [[TYPE]] %hlsl.select.i, %{{.*}}
-// DXCHECK: ret [[TYPE]] %mul.i
-// CHECK: define [[FNATTRS]] [[TYPE]] @
-// CHECK: %fmod.i = frem reassoc nnan ninf nsz arcp afn [[TYPE]]
-// CHECK: ret [[TYPE]] %fmod.i
+// CHECK-LABEL: test_fmod_half
+// SPVCHECK: [[DIV:%.*]] = fdiv [[FMF:reassoc nnan ninf nsz arcp afn]] [[TYPE:half|float]] %{{.*}}, %{{.*}}
+// SPVCHECK: [[ABS:%.*]] = call [[FMF]] [[TYPE]] @llvm.fabs.[[INT_TYPE:f16|f32]]([[TYPE]] %{{.*}})
+// SPVCHECK: [[FRAC:%.*]] = call [[FMF]] [[TYPE]] @llvm.spv.frac.[[INT_TYPE]]([[TYPE]] [[ABS]])
+// SPVCHECK: [[CMP:%.*]] = fcmp [[FMF]] oge [[TYPE]] %{{.*}}, 0
+// SPVCHECK: [[NEG:%.*]] = fneg [[FMF]] [[TYPE]] %{{.*}}
+// SPVCHECK: [[SELECT:%.*]] = select [[FMF]] i1 [[CMP]], [[TYPE]] %{{.*}}, [[TYPE]] [[NEG]]
+// SPVCHECK: [[RESULT:%.*]] = fmul [[FMF]] [[TYPE]] [[SELECT]], %{{.*}}
+// DXCHECK: [[RESULT:%.*]] = frem [[FMF:reassoc nnan ninf nsz arcp afn]] [[TYPE:half|float]] %{{.*}}, %{{.*}}
+// CHECK: ret [[TYPE]] [[RESULT]]
 half test_fmod_half(half p0, half p1) { return fmod(p0, p1); }
 
-// DXCHECK: define [[FNATTRS]] <2 x [[TYPE]]> @
-// DXCHECK: %div1.i = fdiv reassoc nnan ninf nsz arcp afn <2 x [[TYPE]]> %{{.*}}, %{{.*}}
-// DXCHECK: %cmp.i = fcmp reassoc nnan ninf nsz arcp afn oge <2 x [[TYPE]]> %{{.*}}, zeroinitializer
-// DXCHECK: %elt.abs.i = call reassoc nnan ninf nsz arcp afn <2 x [[TYPE]]> @llvm.fabs.v2[[INT_TYPE]](<2 x [[TYPE]]> %{{.*}})
-// DXCHECK: %hlsl.frac.i = call reassoc nnan ninf nsz arcp afn <2 x [[TYPE]]> @llvm.dx.frac.v2[[INT_TYPE]](<2 x [[TYPE]]> %elt.abs.i)
-// DXCHECK: %fneg.i = fneg reassoc nnan ninf nsz arcp afn <2 x [[TYPE]]> %{{.*}}
-// DXCHECK: %hlsl.select.i = select reassoc nnan ninf nsz arcp afn <2 x i1> %{{.*}}, <2 x [[TYPE]]> %{{.*}}, <2 x [[TYPE]]> %fneg.i
-// DXCHECK: %mul.i = fmul reassoc nnan ninf nsz arcp afn <2 x [[TYPE]]> %hlsl.select.i, %{{.*}}
-// DXCHECK: ret <2 x [[TYPE]]> %mul.i
-// CHECK: define [[FNATTRS]] <2 x [[TYPE]]> @
-// CHECK: %fmod.i = frem reassoc nnan ninf nsz arcp afn <2 x [[TYPE]]>
-// CHECK: ret <2 x [[TYPE]]> %fmod.i
+// CHECK-LABEL: test_fmod_half2
+// SPVCHECK: [[DIV:%.*]] = fdiv [[FMF:reassoc nnan ninf nsz arcp afn]] <2 x [[TYPE:half|float]]> %{{.*}}, %{{.*}}
+// SPVCHECK: [[ABS:%.*]] = call [[FMF]] <2 x [[TYPE]]> @llvm.fabs.v2[[INT_TYPE:f16|f32]](<2 x [[TYPE]]> %{{.*}})
+// SPVCHECK: [[FRAC:%.*]] = call [[FMF]] <2 x [[TYPE]]> @llvm.spv.frac.v2[[INT_TYPE]](<2 x [[TYPE]]> [[ABS]])
+// SPVCHECK: [[CMP:%.*]] = fcmp [[FMF]] oge <2 x [[TYPE]]> %{{.*}}, zeroinitializer
+// SPVCHECK: [[NEG:%.*]] = fneg [[FMF]] <2 x [[TYPE]]> %{{.*}}
+// SPVCHECK: [[SELECT:%.*]] = select [[FMF]] <2 x i1> [[CMP]], <2 x [[TYPE]]> %{{.*}}, <2 x [[TYPE]]> [[NEG]]
+// SPVCHECK: [[RESULT:%.*]] = fmul [[FMF]] <2 x [[TYPE]]> [[SELECT]], %{{.*}}
+// DXCHECK: [[RESULT:%.*]] = frem [[FMF:reassoc nnan ninf nsz arcp afn]] <2 x [[TYPE:half|float]]> %{{.*}}, %{{.*}}
+// CHECK: ret <2 x [[TYPE]]> [[RESULT]]
 half2 test_fmod_half2(half2 p0, half2 p1) { return fmod(p0, p1); }
 
-// DXCHECK: define [[FNATTRS]] <3 x [[TYPE]]> @
-// DXCHECK: %div1.i = fdiv reassoc nnan ninf nsz arcp afn <3 x [[TYPE]]> %{{.*}}, %{{.*}}
-// DXCHECK: %cmp.i = fcmp reassoc nnan ninf nsz arcp afn oge <3 x [[TYPE]]> %{{.*}}, zeroinitializer
-// DXCHECK: %elt.abs.i = call reassoc nnan ninf nsz arcp afn <3 x [[TYPE]]> @llvm.fabs.v3[[INT_TYPE]](<3 x [[TYPE]]> %{{.*}})
-// DXCHECK: %hlsl.frac.i = call reassoc nnan ninf nsz arcp afn <3 x [[TYPE]]> @llvm.dx.frac.v3[[INT_TYPE]](<3 x [[TYPE]]> %elt.abs.i)
-// DXCHECK: %fneg.i = fneg reassoc nnan ninf nsz arcp afn <3 x [[TYPE]]> %{{.*}}
-// DXCHECK: %hlsl.select.i = select reassoc nnan ninf nsz arcp afn <3 x i1> %{{.*}}, <3 x [[TYPE]]> %{{.*}}, <3 x [[TYPE]]> %fneg.i
-// DXCHECK: %mul.i = fmul reassoc nnan ninf nsz arcp afn <3 x [[TYPE]]> %hlsl.select.i, %{{.*}}
-// DXCHECK: ret <3 x [[TYPE]]> %mul.i
-// CHECK: define [[FNATTRS]] <3 x [[TYPE]]> @
-// CHECK: %fmod.i = frem reassoc nnan ninf nsz arcp afn <3 x [[TYPE]]>
-// CHECK: ret <3 x [[TYPE]]> %fmod.i
+// CHECK-LABEL: test_fmod_half3
+// SPVCHECK: [[DIV:%.*]] = fdiv [[FMF:reassoc nnan ninf nsz arcp afn]] <3 x [[TYPE:half|float]]> %{{.*}}, %{{.*}}
+// SPVCHECK: [[ABS:%.*]] = call [[FMF]] <3 x [[TYPE]]> @llvm.fabs.v3[[INT_TYPE:f16|f32]](<3 x [[TYPE]]> %{{.*}})
+// SPVCHECK: [[FRAC:%.*]] = call [[FMF]] <3 x [[TYPE]]> @llvm.spv.frac.v3[[INT_TYPE]](<3 x [[TYPE]]> [[ABS]])
+// SPVCHECK: [[CMP:%.*]] = fcmp [[FMF]] oge <3 x [[TYPE]]> %{{.*}}, zeroinitializer
+// SPVCHECK: [[NEG:%.*]] = fneg [[FMF]] <3 x [[TYPE]]> %{{.*}}
+// SPVCHECK: [[SELECT:%.*]] = select [[FMF]] <3 x i1> [[CMP]], <3 x [[TYPE]]> %{{.*}}, <3 x [[TYPE]]> [[NEG]]
+// SPVCHECK: [[RESULT:%.*]] = fmul [[FMF]] <3 x [[TYPE]]> [[SELECT]], %{{.*}}
+// DXCHECK: [[RESULT:%.*]] = frem [[FMF:reassoc nnan ninf nsz arcp afn]] <3 x [[TYPE:half|float]]> %{{.*}}, %{{.*}}
+// CHECK: ret <3 x [[TYPE]]> [[RESULT]]
 half3 test_fmod_half3(half3 p0, half3 p1) { return fmod(p0, p1); }
 
-// DXCHECK: define [[FNATTRS]] <4 x [[TYPE]]> @
-// DXCHECK: %div1.i = fdiv reassoc nnan ninf nsz arcp afn <4 x [[TYPE]]> %{{.*}}, %{{.*}}
-// DXCHECK: %cmp.i = fcmp reassoc nnan ninf nsz arcp afn oge <4 x [[TYPE]]> %{{.*}}, zeroinitializer
-// DXCHECK: %elt.abs.i = call reassoc nnan ninf nsz arcp afn <4 x [[TYPE]]> @llvm.fabs.v4[[INT_TYPE]](<4 x [[TYPE]]> %{{.*}})
-// DXCHECK: %hlsl.frac.i = call reassoc nnan ninf nsz arcp afn <4 x [[TYPE]]> @llvm.dx.frac.v4[[INT_TYPE]](<4 x [[TYPE]]> %elt.abs.i)
-// DXCHECK: %fneg.i = fneg reassoc nnan ninf nsz arcp afn <4 x [[TYPE]]> %{{.*}}
-// DXCHECK: %hlsl.select.i = select reassoc nnan ninf nsz arcp afn <4 x i1> %{{.*}}, <4 x [[TYPE]]> %{{.*}}, <4 x [[TYPE]]> %fneg.i
-// DXCHECK: %mul.i = fmul reassoc nnan ninf nsz arcp afn <4 x [[TYPE]]> %hlsl.select.i, %{{.*}}
-// DXCHECK: ret <4 x [[TYPE]]> %mul.i
-// CHECK: define [[FNATTRS]] <4 x [[TYPE]]> @
-// CHECK: %fmod.i = frem reassoc nnan ninf nsz arcp afn <4 x [[TYPE]]>
-// CHECK: ret <4 x [[TYPE]]> %fmod.i
+// CHECK-LABEL: test_fmod_half4
+// SPVCHECK: [[DIV:%.*]] = fdiv [[FMF:reassoc nnan ninf nsz arcp afn]] <4 x [[TYPE:half|float]]> %{{.*}}, %{{.*}}
+// SPVCHECK: [[ABS:%.*]] = call [[FMF]] <4 x [[TYPE]]> @llvm.fabs.v4[[INT_TYPE:f16|f32]](<4 x [[TYPE]]> %{{.*}})
+// SPVCHECK: [[FRAC:%.*]] = call [[FMF]] <4 x [[TYPE]]> @llvm.spv.frac.v4[[INT_TYPE]](<4 x [[TYPE]]> [[ABS]])
+// SPVCHECK: [[CMP:%.*]] = fcmp [[FMF]] oge <4 x [[TYPE]]> %{{.*}}, zeroinitializer
+// SPVCHECK: [[NEG:%.*]] = fneg [[FMF]] <4 x [[TYPE]]> %{{.*}}
+// SPVCHECK: [[SELECT:%.*]] = select [[FMF]] <4 x i1> [[CMP]], <4 x [[TYPE]]> %{{.*}}, <4 x [[TYPE]]> [[NEG]]
+// SPVCHECK: [[RESULT:%.*]] = fmul [[FMF]] <4 x [[TYPE]]> [[SELECT]], %{{.*}}
+// DXCHECK: [[RESULT:%.*]] = frem [[FMF:reassoc nnan ninf nsz arcp afn]] <4 x [[TYPE:half|float]]> %{{.*}}, %{{.*}}
+// CHECK: ret <4 x [[TYPE]]> [[RESULT]]
 half4 test_fmod_half4(half4 p0, half4 p1) { return fmod(p0, p1); }
 
-// DXCHECK: define [[FNATTRS]] float @
-// DXCHECK: %div1.i = fdiv reassoc nnan ninf nsz arcp afn float %{{.*}}, %{{.*}}
-// DXCHECK: %cmp.i = fcmp reassoc nnan ninf nsz arcp afn oge float %{{.*}}, 0.000000e+00
-// DXCHECK: %elt.abs.i = call reassoc nnan ninf nsz arcp afn float @llvm.fabs.f32(float %{{.*}})
-// DXCHECK: %hlsl.frac.i = call reassoc nnan ninf nsz arcp afn float @llvm.dx.frac.f32(float %elt.abs.i)
-// DXCHECK: %fneg.i = fneg reassoc nnan ninf nsz arcp afn float %{{.*}}
-// DXCHECK: %hlsl.select.i = select reassoc nnan ninf nsz arcp afn i1 %{{.*}}, float %{{.*}}, float %fneg.i
-// DXCHECK: %mul.i = fmul reassoc nnan ninf nsz arcp afn float %hlsl.select.i, %{{.*}}
-// DXCHECK: ret float %mul.i
-// CHECK: define [[FNATTRS]] float @
-// CHECK: %fmod.i = frem reassoc nnan ninf nsz arcp afn float
-// CHECK: ret float %fmod.i
+// CHECK-LABEL: test_fmod_float
+// SPVCHECK: [[DIV:%.*]] = fdiv [[FMF:reassoc nnan ninf nsz arcp afn]] float %{{.*}}, %{{.*}}
+// SPVCHECK: [[ABS:%.*]] = call [[FMF]] float @llvm.fabs.f32(float %{{.*}})
+// SPVCHECK: [[FRAC:%.*]] = call [[FMF]] float @llvm.spv.frac.f32(float [[ABS]])
+// SPVCHECK: [[CMP:%.*]] = fcmp [[FMF]] oge float %{{.*}}, 0.000000e+00
+// SPVCHECK: [[NEG:%.*]] = fneg [[FMF]] float %{{.*}}
+// SPVCHECK: [[SELECT:%.*]] = select [[FMF]] i1 [[CMP]], float %{{.*}}, float [[NEG]]
+// SPVCHECK: [[RESULT:%.*]] = fmul [[FMF]] float [[SELECT]], %{{.*}}
+// DXCHECK: [[RESULT:%.*]] = frem [[FMF:reassoc nnan ninf nsz arcp afn]] float %{{.*}}, %{{.*}}
+// CHECK: ret float [[RESULT]]
 float test_fmod_float(float p0, float p1) { return fmod(p0, p1); }
 
-// DXCHECK: define [[FNATTRS]] <2 x float> @
-// DXCHECK: %div1.i = fdiv reassoc nnan ninf nsz arcp afn <2 x float> %{{.*}}, %{{.*}}
-// DXCHECK: %cmp.i = fcmp reassoc nnan ninf nsz arcp afn oge <2 x float> %{{.*}}, zeroinitializer
-// DXCHECK: %elt.abs.i = call reassoc nnan ninf nsz arcp afn <2 x float> @llvm.fabs.v2f32(<2 x float> %{{.*}})
-// DXCHECK: %hlsl.frac.i = call reassoc nnan ninf nsz arcp afn <2 x float> @llvm.dx.frac.v2f32(<2 x float> %elt.abs.i)
-// DXCHECK: %fneg.i = fneg reassoc nnan ninf nsz arcp afn <2 x float> %{{.*}}
-// DXCHECK: %hlsl.select.i = select reassoc nnan ninf nsz arcp afn <2 x i1> %{{.*}}, <2 x float> %{{.*}}, <2 x float> %fneg.i
-// DXCHECK: %mul.i = fmul reassoc nnan ninf nsz arcp afn <2 x float> %hlsl.select.i, %{{.*}}
-// DXCHECK: ret <2 x float> %mul.i
-// CHECK: define [[FNATTRS]] <2 x float> @
-// CHECK: %fmod.i = frem reassoc nnan ninf nsz arcp afn <2 x float>
-// CHECK: ret <2 x float> %fmod.i
+// CHECK-LABEL: test_fmod_float2
+// SPVCHECK: [[DIV:%.*]] = fdiv [[FMF:reassoc nnan ninf nsz arcp afn]] <2 x float> %{{.*}}, %{{.*}}
+// SPVCHECK: [[ABS:%.*]] = call [[FMF]] <2 x float> @llvm.fabs.v2f32(<2 x float> %{{.*}})
+// SPVCHECK: [[FRAC:%.*]] = call [[FMF]] <2 x float> @llvm.spv.frac.v2f32(<2 x float> [[ABS]])
+// SPVCHECK: [[CMP:%.*]] = fcmp [[FMF]] oge <2 x float> %{{.*}}, zeroinitializer
+// SPVCHECK: [[NEG:%.*]] = fneg [[FMF]] <2 x float> %{{.*}}
+// SPVCHECK: [[SELECT:%.*]] = select [[FMF]] <2 x i1> [[CMP]], <2 x float> %{{.*}}, <2 x float> [[NEG]]
+// SPVCHECK: [[RESULT:%.*]] = fmul [[FMF]] <2 x float> [[SELECT]], %{{.*}}
+// DXCHECK: [[RESULT:%.*]] = frem [[FMF:reassoc nnan ninf nsz arcp afn]] <2 x float> %{{.*}}, %{{.*}}
+// CHECK: ret <2 x float> [[RESULT]]
 float2 test_fmod_float2(float2 p0, float2 p1) { return fmod(p0, p1); }
 
-// DXCHECK: define [[FNATTRS]] <3 x float> @
-// DXCHECK: %div1.i = fdiv reassoc nnan ninf nsz arcp afn <3 x float> %{{.*}}, %{{.*}}
-// DXCHECK: %cmp.i = fcmp reassoc nnan ninf nsz arcp afn oge <3 x float> %{{.*}}, zeroinitializer
-// DXCHECK: %elt.abs.i = call reassoc nnan ninf nsz arcp afn <3 x float> @llvm.fabs.v3f32(<3 x float> %{{.*}})
-// DXCHECK: %hlsl.frac.i = call reassoc nnan ninf nsz arcp afn <3 x float> @llvm.dx.frac.v3f32(<3 x float> %elt.abs.i)
-// DXCHECK: %fneg.i = fneg reassoc nnan ninf nsz arcp afn <3 x float> %{{.*}}
-// DXCHECK: %hlsl.select.i = select reassoc nnan ninf nsz arcp afn <3 x i1> %{{.*}}, <3 x float> %{{.*}}, <3 x float> %fneg.i
-// DXCHECK: %mul.i = fmul reassoc nnan ninf nsz arcp afn <3 x float> %hlsl.select.i, %{{.*}}
-// DXCHECK: ret <3 x float> %mul.i
-// CHECK: define [[FNATTRS]] <3 x float> @
-// CHECK: %fmod.i = frem reassoc nnan ninf nsz arcp afn <3 x float>
-// CHECK: ret <3 x float> %fmod.i
+// CHECK-LABEL: test_fmod_float3
+// SPVCHECK: [[DIV:%.*]] = fdiv [[FMF:reassoc nnan ninf nsz arcp afn]] <3 x float> %{{.*}}, %{{.*}}
+// SPVCHECK: [[ABS:%.*]] = call [[FMF]] <3 x float> @llvm.fabs.v3f32(<3 x float> %{{.*}})
+// SPVCHECK: [[FRAC:%.*]] = call [[FMF]] <3 x float> @llvm.spv.frac.v3f32(<3 x float> [[ABS]])
+// SPVCHECK: [[CMP:%.*]] = fcmp [[FMF]] oge <3 x float> %{{.*}}, zeroinitializer
+// SPVCHECK: [[NEG:%.*]] = fneg [[FMF]] <3 x float> %{{.*}}
+// SPVCHECK: [[SELECT:%.*]] = select [[FMF]] <3 x i1> [[CMP]], <3 x float> %{{.*}}, <3 x float> [[NEG]]
+// SPVCHECK: [[RESULT:%.*]] = fmul [[FMF]] <3 x float> [[SELECT]], %{{.*}}
+// DXCHECK: [[RESULT:%.*]] = frem [[FMF:reassoc nnan ninf nsz arcp afn]] <3 x float> %{{.*}}, %{{.*}}
+// CHECK: ret <3 x float> [[RESULT]]
 float3 test_fmod_float3(float3 p0, float3 p1) { return fmod(p0, p1); }
 
-// DXCHECK: define [[FNATTRS]] <4 x float> @
-// DXCHECK: %div1.i = fdiv reassoc nnan ninf nsz arcp afn <4 x float> %{{.*}}, %{{.*}}
-// DXCHECK: %cmp.i = fcmp reassoc nnan ninf nsz arcp afn oge <4 x float> %{{.*}}, zeroinitializer
-// DXCHECK: %elt.abs.i = call reassoc nnan ninf nsz arcp afn <4 x float> @llvm.fabs.v4f32(<4 x float> %{{.*}})
-// DXCHECK: %hlsl.frac.i = call reassoc nnan ninf nsz arcp afn <4 x float> @llvm.dx.frac.v4f32(<4 x float> %elt.abs.i)
-// DXCHECK: %fneg.i = fneg reassoc nnan ninf nsz arcp afn <4 x float> %{{.*}}
-// DXCHECK: %hlsl.select.i = select reassoc nnan ninf nsz arcp afn <4 x i1> %{{.*}}, <4 x float> %{{.*}}, <4 x float> %fneg.i
-// DXCHECK: %mul.i = fmul reassoc nnan ninf nsz arcp afn <4 x float> %hlsl.select.i, %{{.*}}
-// DXCHECK: ret <4 x float> %mul.i
-// CHECK: define [[FNATTRS]] <4 x float> @
-// CHECK: %fmod.i = frem reassoc nnan ninf nsz arcp afn <4 x float>
-// CHECK: ret <4 x float> %fmod.i
+// CHECK-LABEL: test_fmod_float4
+// SPVCHECK: [[DIV:%.*]] = fdiv [[FMF:reassoc nnan ninf nsz arcp afn]] <4 x float> %{{.*}}, %{{.*}}
+// SPVCHECK: [[ABS:%.*]] = call [[FMF]] <4 x float> @llvm.fabs.v4f32(<4 x float> %{{.*}})
+// SPVCHECK: [[FRAC:%.*]] = call [[FMF]] <4 x float> @llvm.spv.frac.v4f32(<4 x float> [[ABS]])
+// SPVCHECK: [[CMP:%.*]] = fcmp [[FMF]] oge <4 x float> %{{.*}}, zeroinitializer
+// SPVCHECK: [[NEG:%.*]] = fneg [[FMF]] <4 x float> %{{.*}}
+// SPVCHECK: [[SELECT:%.*]] = select [[FMF]] <4 x i1> [[CMP]], <4 x float> %{{.*}}, <4 x float> [[NEG]]
+// SPVCHECK: [[RESULT:%.*]] = fmul [[FMF]] <4 x float> [[SELECT]], %{{.*}}
+// DXCHECK: [[RESULT:%.*]] = frem [[FMF:reassoc nnan ninf nsz arcp afn]] <4 x float> %{{.*}}, %{{.*}}
+// CHECK: ret <4 x float> [[RESULT]]
 float4 test_fmod_float4(float4 p0, float4 p1) { return fmod(p0, p1); }
+
+// CHECK-LABEL: test_fmod_float5
+// SPVCHECK: [[DIV:%.*]] = fdiv [[FMF:reassoc nnan ninf nsz arcp afn]] <5 x float> %{{.*}}, %{{.*}}
+// SPVCHECK: [[ABS:%.*]] = call [[FMF]] <5 x float> @llvm.fabs.v5f32(<5 x float> %{{.*}})
+// SPVCHECK: [[FRAC:%.*]] = call [[FMF]] <5 x float> @llvm.spv.frac.v5f32(<5 x float> [[ABS]])
+// SPVCHECK: [[CMP:%.*]] = fcmp [[FMF]] oge <5 x float> %{{.*}}, zeroinitializer
+// SPVCHECK: [[NEG:%.*]] = fneg [[FMF]] <5 x float> %{{.*}}
+// SPVCHECK: [[SELECT:%.*]] = select [[FMF]] <5 x i1> [[CMP]], <5 x float> %{{.*}}, <5 x float> [[NEG]]
+// SPVCHECK: [[RESULT:%.*]] = fmul [[FMF]] <5 x float> [[SELECT]], %{{.*}}
+// DXCHECK: [[RESULT:%.*]] = frem [[FMF:reassoc nnan ninf nsz arcp afn]] <5 x float> %{{.*}}, %{{.*}}
+// CHECK: ret <5 x float> [[RESULT]]
+vector<float, 5> test_fmod_float5(vector<float, 5> p0,
+                                  vector<float, 5> p1) {
+  return fmod(p0, p1);
+}
 
