cae98515705557a71cfc67c6fcf7ad7b0ed50100
Ramkumar Ramachandra <artagnon@tenstorrent.com>
2026-09-17 08:58:59 +0100

    [SCEV] NFC simplification

--- llvm/lib/Analysis/ScalarEvolution.cpp
+++ llvm/lib/Analysis/ScalarEvolution.cpp
@@ -11339,11 +11339,10 @@ bool ScalarEvolution::isKnownMultipleOf(
       return true;
 
   // Similarly, look through UMinMax, with no wrapping arithmetic to consider.
-  if (isa<SCEVUMinExpr, SCEVUMaxExpr>(S))
-    if (all_of(S->operands(), [&](SCEVUse Op) {
-          return isKnownMultipleOf(Op, M, Predicates);
-        }))
-      return true;
+  if (isa<SCEVUMinExpr, SCEVUMaxExpr>(S) &&
+      all_of(S->operands(),
+             [&](SCEVUse Op) { return isKnownMultipleOf(Op, M, Predicates); }))
+    return true;
 
   const SCEVPredicate *P = getComparePredicate(ICmpInst::ICMP_EQ, SmodM, Zero);
 
