c000d8607ec24529e225b7c1231dff1417bdb1f8
Ebuka Ezike <yerimyah1@gmail.com>
2026-09-17 03:31:58 +0100

    Get the data from the progress

--- lldb/unittests/API/SBProgressTest.cpp
+++ lldb/unittests/API/SBProgressTest.cpp
@@ -10,6 +10,8 @@
 
 #include "TestingSupport/SubsystemRAII.h"
 #include "lldb/API/LLDB.h"
+#include "lldb/lldb-enumerations.h"
+#include "llvm/Testing/Support/Error.h"
 
 using namespace lldb_private;
 using namespace lldb;
@@ -18,6 +20,7 @@ class SBProgressTest : public testing::Test {
 protected:
   void SetUp() override {
     debugger = SBDebugger::Create(/*source_init_files=*/false);
+    debugger.SetAsync(false);
   }
 
   void TearDown() override { SBDebugger::Destroy(debugger); }
@@ -31,7 +34,31 @@ TEST_F(SBProgressTest, Constructor) {
   SBProgress progress(nullptr, nullptr, debugger);
   SBProgress progress2(nullptr, nullptr, /*total_units=*/10, debugger);
 
+  SBListener listener("Test Listener");
+  SBBroadcaster broadcaster = debugger.GetBroadcaster();
+  broadcaster.AddListener(listener, lldb::eBroadcastBitExternalProgress);
+
   progress.Increment(2, nullptr);
   progress.Increment(2, "Other");
   progress.Finalize();
+
+  SBEvent event;
+  EXPECT_TRUE(listener.GetNextEvent(event));
+
+  const SBStructuredData sdata = SBDebugger::GetProgressDataFromEvent(event);
+  SBStream stream;
+  sdata.GetAsJSON(stream);
+  llvm::StringRef data(stream.GetData(), stream.GetSize());
+  llvm::Expected<llvm::json::Value> progress_data = llvm::json::parse(data);
+
+  ASSERT_THAT_EXPECTED(progress_data, llvm::Succeeded());
+  auto *proj_json = progress_data->getAsObject();
+  ASSERT_NE(proj_json, nullptr);
+  for (llvm::StringRef field : {"message", "title", "details"}) {
+    auto field_val = proj_json->getString(field);
+    ASSERT_TRUE(field_val.has_value());
+    EXPECT_EQ(field_val.value(), llvm::StringRef(""));
+  }
+
+  EXPECT_TRUE(event.GetDescription(stream));
 }
