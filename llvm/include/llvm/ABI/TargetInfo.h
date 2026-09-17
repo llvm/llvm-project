b132e04df4e9c945c0d22e342229456839264d19
skc7 <Krishna.Sankisa@amd.com>
2026-09-17 16:24:53 +0530

    support CoerceGenericPtrArgToGlobal

--- llvm/include/llvm/ABI/TargetInfo.h
+++ llvm/include/llvm/ABI/TargetInfo.h
@@ -131,7 +131,9 @@ protected:
 
 LLVM_ABI std::unique_ptr<TargetInfo> createBPFTargetInfo(TypeBuilder &TB);
 
-LLVM_ABI std::unique_ptr<TargetInfo> createAMDGPUTargetInfo(TypeBuilder &TB);
+LLVM_ABI std::unique_ptr<TargetInfo>
+createAMDGPUTargetInfo(TypeBuilder &TB,
+                       bool CoerceGenericPtrArgToGlobal = false);
 
 /// The AVX ABI level for X86 targets.
 enum class X86AVXABILevel {
