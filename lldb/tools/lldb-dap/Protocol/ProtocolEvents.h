be194b39fa7387bcfa3dc45586b353a7d9663d37
Ebuka Ezike <e_ezike@apple.com>
2026-09-17 15:44:56 +0100

    [lldb-dap] Refactor handling progress events.
    lldb-dap now only uses one thread to receive and throttle
    received progress events.
    For every progress with a progressId, we delay reporting the
    progress for 1000ms until we have recieved a new progress and only
    send an update if the progress has a new version after 250ms.
    
    This means for every progress integration test we do nothing for at
    least 1250ms. The previous `TestDAP_progress` runs for at least 8s
    regardless of how fast the computer is.
    
    Moved most of the test to unittest where we can simulate progress delays
    and time passing. We now have only one `TestDAP_progress` test to
    verify the client receives progress Events.
    
    When the ProgressEventThread reports a new progressEvent, the reporter
    then.
    - Create or update the `PendingProgress` with that progressId.
    - Determines the action to take based on the ProgressState, k_start_delay and k_update_interval.
    - Sends the apropriate Event based on the action calculated.
    
    Add new tests for the ProgressEventReporter and protocol
    progressXXXX POD.

--- lldb/tools/lldb-dap/Protocol/ProtocolEvents.h
+++ lldb/tools/lldb-dap/Protocol/ProtocolEvents.h
@@ -179,6 +179,74 @@ struct StoppedEventBody {
 };
 llvm::json::Value toJSON(const StoppedEventBody &);
 
+/// The event signals that a long running operation is about to start and
+/// provides additional information for the client to set up a corresponding
+/// progress and cancellation UI.
+///
+/// The client is free to delay the showing of the UI in order to reduce
+/// flicker.
+///
+/// This event should only be sent if the corresponding capability
+/// `supportsProgressReporting` is true
+struct ProgressStartEventBody {
+  /// An ID that can be used in subsequent `progressUpdate` and `progressEnd`
+  /// events to make them refer to the same progress reporting. IDs must be
+  /// unique within a debug session.
+  String progressId;
+
+  /// Short title of the progress reporting. Shown in the UI to describe the
+  /// long running operation.
+  String title;
+
+  /// More detailed progress message.
+  std::optional<String> message;
+
+  /// Progress percentage to display (value range: 0 to 100). If omitted no
+  /// percentage is shown.
+  std::optional<uint32_t> percentage;
+
+  /// If true, the request that reports progress may be cancelled with a
+  /// `cancel` request.
+  std::optional<bool> cancellable;
+};
+llvm::json::Value toJSON(const ProgressStartEventBody &);
+
+/// The event signals that the progress reporting needs to be updated with a new
+/// message and/or percentage.
+///
+/// The client does not have to update the UI immediately, but the clients needs
+/// to keep track of the message and/or percentage values.
+///
+/// This event should only be sent if the corresponding capability
+/// `supportsProgressReporting` is true.
+struct ProgressUpdateEventBody {
+  /// The ID that was introduced in the initial `ProgressStartEventBody` event.
+  String progressId;
+
+  /// More detailed progress message. If omitted, the previous message (if any)
+  /// is used.
+  std::optional<String> message;
+
+  /// Progress percentage to display (value range: 0 to 100). If omitted no
+  /// percentage is shown.
+  std::optional<uint32_t> percentage;
+};
+llvm::json::Value toJSON(const ProgressUpdateEventBody &);
+
+/// The event signals the end of the progress reporting with a final message.
+///
+/// This event should only be sent if the corresponding capability
+/// `supportsProgressReporting` is true.
+struct ProgressEndEventBody {
+  /// The ID that was introduced in the initial `ProgressStartEventBody` event.
+  String progressId;
+
+  /// More detailed progress message. If omitted, the previous message (if any)
+  /// is used.
+  std::optional<String> message;
+};
+llvm::json::Value toJSON(const ProgressEndEventBody &);
+
 } // end namespace lldb_dap::protocol
 
 #endif
