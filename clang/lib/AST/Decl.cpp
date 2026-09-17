ded426feff3c59cef6706b7fa29db438f758aadf
Dan Katz <katzdm@gmail.com>
2026-09-17 11:46:59 -0400

    Constinits have manifestly constant-evaluated initializers.

--- clang/lib/AST/Decl.cpp
+++ clang/lib/AST/Decl.cpp
@@ -2587,7 +2587,7 @@ VarDecl::evaluateValueImpl(SmallVectorImpl<PartialDiagnosticAt> *Notes,
   EStatus.Diag = Notes;
   EStatus.ExtendedDiag = &MSWarning;
   bool Result =
-      isConstexpr()
+      (isConstexpr() || hasAttr<ConstInitAttr>())
           ? Init->EvaluateAsMandatedConstantInitializer(EStatus, Ctx, *SP, this)
           : Init->EvaluateAsInitializer(Ctx, this, EStatus,
                                         IsConstantInitialization);
