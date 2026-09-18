85974515c3c46c8fbc6ce171997363de159a717a
Jonathan Thackray <jonathan.thackray@arm.com>
2026-09-18 11:24:10 +0100

    fixup! Slightly adjust message in testfile

--- llvm/test/MC/AArch64/retired-names.s
+++ llvm/test/MC/AArch64/retired-names.s
@@ -1,6 +1,9 @@
 // RUN: not llvm-mc -triple=aarch64 -mattr=+mpamv2,+tme < %s 2>&1 | FileCheck %s
 
-// Retired feature and system register names must not be reused.
+// Add negative tests preventing reuse of retired feature and system register names.
+//
+// When system registers or feature names are retired, they should be appended to
+// this file, to ensure they're never re-used in future.
 
 // CHECK:      '+mpamv2' is not a recognized feature for this target (ignoring feature)
 // CHECK-NEXT: '+tme' is not a recognized feature for this target (ignoring feature)
