387b680ba25b06aca31167d0edda2750ad36c74a
Florian Hahn <flo@fhahn.com>
2026-09-18 11:33:35 +0100

    !fixup restore dropped comment line, thanks

--- llvm/lib/Analysis/ScalarEvolution.cpp
+++ llvm/lib/Analysis/ScalarEvolution.cpp
@@ -13381,6 +13381,7 @@ const SCEV *ScalarEvolution::computeMaxBECountForLT(const SCEV *Start,
   APInt Delta = MaxEnd - MinStart;
 
   // Try to refine Delta in case End - Start gives a tighter bound after
+  // folding.
   Delta = APIntOps::umin(Delta, getUnsignedRangeMax(getMinusSCEV(End, Start)));
 
   return getUDivCeilSCEV(getConstant(Delta), getConstant(StrideForMaxBECount));
