ca94d9e55957a4554931344c03a8ddcad60ed765
Leandro Lupori <leandro.lupori@linaro.org>
2026-09-18 18:49:18 +0000

    Update regression test error

--- flang/test/Preprocessing/fixed-c-comment04.f
+++ flang/test/Preprocessing/fixed-c-comment04.f
@@ -4,7 +4,7 @@
       integer :: x, y
       y = 3
 ! The lines below should be parsed as distinct lines, with no continuation.
-! ERROR: error: obsolete legacy extension is not supported
+! ERROR: error: expected an executable statement
       x = y
 	/**/+2
       print *, x
