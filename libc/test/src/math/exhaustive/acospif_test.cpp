482e4a5fbe1d370ca93158b531db3411364e18e9
Tue Ly <lntue.h@gmail.com>
2026-09-18 16:42:00 +0000

    [libc][math] Fix "Postive" typo in exhaustive test folder.

--- libc/test/src/math/exhaustive/acospif_test.cpp
+++ libc/test/src/math/exhaustive/acospif_test.cpp
@@ -20,7 +20,7 @@ using LlvmLibcAcospifExhaustiveTest =
 static constexpr uint32_t POS_START = 0x0000'0000U;
 static constexpr uint32_t POS_STOP = 0x7f80'0000U;
 
-TEST_F(LlvmLibcAcospifExhaustiveTest, PostiveRange) {
+TEST_F(LlvmLibcAcospifExhaustiveTest, PositiveRange) {
   test_full_range_all_roundings(POS_START, POS_STOP);
 }
 
