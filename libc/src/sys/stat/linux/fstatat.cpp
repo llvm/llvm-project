a662bfc03a7f2c78f53e35af19b36b39cab6e435
amanmaurya92 <amanmaurya9209@gmail.com>
2026-09-18 19:28:39 +0530

    [libc] Update file headers to LLVM standard format in fstatat
    Update file headers in fstatat.h, linux/fstatat.cpp, and fstatat_test.cpp to match LLVM Coding Standards with \file Doxygen documentation blocks.

--- libc/src/sys/stat/linux/fstatat.cpp
+++ libc/src/sys/stat/linux/fstatat.cpp
@@ -1,9 +1,14 @@
-//===-- Linux implementation of fstatat -----------------------------------===//
+//===----------------------------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// Linux implementation of fstatat.
+///
 //===----------------------------------------------------------------------===//
 
 #include "src/sys/stat/fstatat.h"
