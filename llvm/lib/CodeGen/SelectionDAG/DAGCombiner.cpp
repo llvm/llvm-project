efb95fd0ca9d06d5c4e8e4b010c389e2d66f425e
Kamlesh Kumar <kamlesh.kumar@arm.com>
2026-09-18 16:05:06 +0000

    simplify checks

--- llvm/lib/CodeGen/SelectionDAG/DAGCombiner.cpp
+++ llvm/lib/CodeGen/SelectionDAG/DAGCombiner.cpp
@@ -27817,6 +27817,7 @@ SDValue DAGCombiner::visitCONCAT_VECTORS(SDNode *N) {
 SDValue DAGCombiner::visitVECTOR_INTERLEAVE(SDNode *N) {
   EVT VT = N->getValueType(0);
   SDValue Op0 = N->getOperand(0);
+  unsigned Factor = N->getNumOperands();
 
   // Fold an interleave of fixed-length BUILD_VECTORs by rearranging their
   // scalar operands directly.
@@ -27826,7 +27827,6 @@ SDValue DAGCombiner::visitVECTOR_INTERLEAVE(SDNode *N) {
           return Op.getOpcode() == ISD::BUILD_VECTOR &&
                  Op.getOperand(0).getValueType() == EltVT;
         })) {
-      unsigned Factor = N->getNumOperands();
       unsigned NumElts = VT.getVectorNumElements();
       SDLoc DL(N);
       SmallVector<SDValue, 4> Results;
@@ -27844,31 +27844,23 @@ SDValue DAGCombiner::visitVECTOR_INTERLEAVE(SDNode *N) {
 
   // Fold interleave(splat(S[J]), ..., splat(S[J + Factor - 1])) to shuffles
   // of S.
-  if (VT.isFixedLengthVector() && Op0.getOpcode() == ISD::VECTOR_SHUFFLE) {
-    unsigned Factor = N->getNumOperands();
-    // Non-power-of-two interleaves may be better lowered as structured stores.
-    if (isPowerOf2_32(Factor)) {
-      unsigned NumElts = VT.getVectorNumElements();
-      int FirstIndex;
-      SDValue Source = DAG.getSplatSourceVector(Op0, FirstIndex);
-      if (Source &&
-          llvm::all_of(llvm::enumerate(N->op_values()), [&](auto Item) {
-            int SplatIndex;
-            return DAG.getSplatSourceVector(Item.value(), SplatIndex) ==
-                       Source &&
-                   SplatIndex == FirstIndex + static_cast<int>(Item.index());
-          })) {
-        SmallVector<SDValue, 4> Results;
-        for (unsigned Result = 0; Result != Factor; ++Result) {
-          SmallVector<int, 16> Mask;
-          for (unsigned I = 0; I != NumElts; ++I)
-            Mask.push_back(FirstIndex + (Result * NumElts + I) % Factor);
-
-          Results.push_back(DAG.getVectorShuffle(VT, SDLoc(N), Source,
-                                                 DAG.getPOISON(VT), Mask));
-        }
-        return CombineTo(N, &Results);
-      }
+  if (Op0.getOpcode() == ISD::VECTOR_SHUFFLE &&
+      VT.getVectorElementCount().isKnownMultipleOf(Factor)) {
+    int FirstIndex;
+    unsigned NumElts = VT.getVectorNumElements();
+    SDValue Source = DAG.getSplatSourceVector(Op0, FirstIndex);
+    if (Source && llvm::all_of(llvm::enumerate(N->op_values()), [&](auto Item) {
+          int SplatIndex;
+          return DAG.getSplatSourceVector(Item.value(), SplatIndex) == Source &&
+                 SplatIndex == FirstIndex + static_cast<int>(Item.index());
+        })) {
+      SmallVector<int, 16> Mask;
+      for (unsigned I = 0; I != NumElts; ++I)
+        Mask.push_back(FirstIndex + I % Factor);
+      SDValue Shuffle =
+          DAG.getVectorShuffle(VT, SDLoc(N), Source, DAG.getPOISON(VT), Mask);
+      SmallVector<SDValue, 4> Results(Factor, Shuffle);
+      return CombineTo(N, &Results);
     }
   }
 
