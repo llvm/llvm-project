bcc52f1caef544ac8cbd301a32dd6a998f9b7740
serge-sans-paille <sguelton@mozilla.com>
2026-09-18 11:54:00 +0200

    [clang-tidy] Add detection of decltype(nullptr) => std::nullptr_t to modernize-use-nullptr
    Hidden behind an option switch, but on by default.

--- clang-tools-extra/test/clang-tidy/checkers/modernize/use-nullptr-basic.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/modernize/use-nullptr-basic.cpp
@@ -292,3 +292,17 @@ template<typename T>
 T *f2(T *a = NULL) {
   return a ? a : NULL;
 }
+
+void foo(decltype(nullptr));
+// CHECK-MESSAGES: :[[@LINE-1]]:10: warning: use std::nullptr_t instead
+// CHECK-FIXES: void foo(std::nullptr_t);
+void foo(const decltype(nullptr));
+// CHECK-MESSAGES: :[[@LINE-1]]:16: warning: use std::nullptr_t instead
+// CHECK-FIXES: void foo(const std::nullptr_t);
+decltype(nullptr) a;
+// CHECK-MESSAGES: :[[@LINE-1]]:1: warning: use std::nullptr_t instead
+// CHECK-FIXES: std::nullptr_t a;
+template<class T=decltype(nullptr)>
+struct bar {};
+// CHECK-MESSAGES: :[[@LINE-2]]:18: warning: use std::nullptr_t instead
+// CHECK-FIXES: template<class T=std::nullptr_t>
