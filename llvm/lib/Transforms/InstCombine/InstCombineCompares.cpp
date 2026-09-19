88f372575f5bdf113aa0948c81b81a27c32ab7f2
armandeep <armandeepsinghbains123@gmail.com>
2026-09-19 09:39:59 +0530

    Move fold after foldICmpWithCastOp call per review

--- llvm/lib/Transforms/InstCombine/InstCombineCompares.cpp
+++ llvm/lib/Transforms/InstCombine/InstCombineCompares.cpp
@@ -5826,24 +5826,6 @@ Instruction *InstCombinerImpl::foldICmpBinOp(ICmpInst &I,
   if (Value *V = foldShiftIntoShiftInAnotherHandOfAndInICmp(I, SQ, Builder))
     return replaceInstUsesWith(I, V);
 
-  // icmp (zext X), (and (trunc Y), Mask) -> icmp X, trunc Y IFF Mask exactly
-  // covers the bits of X
-  {
-    Value *Y;
-    const APInt *Mask;
-    if (match(I.getOperand(1), m_ZExt(m_Value(X))) &&
-        match(I.getOperand(0),
-              m_OneUse(m_And(m_Trunc(m_Value(Y)), m_APInt(Mask))))) {
-      Type *SmallType = X->getType();
-      unsigned SmallWidth = SmallType->getScalarSizeInBits();
-      if (Mask->isMask(SmallWidth) &&
-          shouldChangeType(Y->getType(), SmallType)) {
-        Value *NewTrunc = Builder.CreateTrunc(Y, SmallType);
-        return new ICmpInst(I.getUnsignedPredicate(), NewTrunc, X);
-      }
-    }
-  }
-
   return nullptr;
 }
 
@@ -8206,6 +8188,24 @@ Instruction *InstCombinerImpl::visitICmpInst(ICmpInst &I) {
   // TODO: Hoist this above the min/max bailout.
   if (Instruction *R = foldICmpWithCastOp(I))
     return R;
+
+  // icmp (zext X), (and (trunc Y), Mask) -> icmp X, trunc Y IFF Mask exactly
+  // covers the bits of X
+  {
+    Value *Y;
+    const APInt *Mask;
+    if (match(I.getOperand(1), m_ZExt(m_Value(X))) &&
+        match(I.getOperand(0),
+              m_OneUse(m_And(m_Trunc(m_Value(Y)), m_APInt(Mask))))) {
+      Type *SmallType = X->getType();
+      unsigned SmallWidth = SmallType->getScalarSizeInBits();
+      if (Mask->isMask(SmallWidth) &&
+          shouldChangeType(I.getOperand(0)->getType(), SmallType)) {
+        Value *NewTrunc = Builder.CreateTrunc(Y, SmallType);
+        return new ICmpInst(I.getUnsignedPredicate(), NewTrunc, X);
+      }
+    }
+  }
 
   {
     Value *X, *Y;
