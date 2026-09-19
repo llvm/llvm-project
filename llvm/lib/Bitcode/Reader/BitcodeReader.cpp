2cd77ce2342a4d37690e3dccc4a01d6fb30ac207
Pedro Vicente <pedro.costa.vicente@tecnico.ulisboa.pt>
2026-09-19 11:25:13 +0100

    minor changes in bitcode

--- llvm/lib/Bitcode/Reader/BitcodeReader.cpp
+++ llvm/lib/Bitcode/Reader/BitcodeReader.cpp
@@ -5643,8 +5643,7 @@ Error BitcodeReader::parseFunctionBody(Function *F) {
       break;
     }
 
-    case bitc::FUNC_CODE_INST_BITINSERT: { // BITINSERT: [ty, opval, opval,
-                                           // opval]
+    case bitc::FUNC_CODE_INST_BITINSERT: { // BITINSERT: [opval, opval, opval]
       unsigned OpNum = 0;
       Value *Base, *Val, *Offset;
       unsigned BaseTypeID, ValTypeID, OffsetTypeID;
