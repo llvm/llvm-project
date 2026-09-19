c259eb69b183e38e8e344250758f2d4911e85e55
udaykiriti <udaykiriti9@gmail.com>
2026-09-19 22:19:14 +0530

    Added freebsd entrypoint and updated with new header file for shared tests and Added test file entries to bazel
    Signed-off-by: udaykiriti <udaykiriti9@gmail.com>

--- libc/shared/math/lgammabf16.h
+++ libc/shared/math/lgammabf16.h
@@ -1,4 +1,4 @@
-//===-- Shared header for lgammabf16 ----------------------------*- C++ -*-===//
+//===-- Shared header for lgammabf16 --------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
