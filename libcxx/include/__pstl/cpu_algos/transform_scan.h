1b1b147e390c6038f4915ca8fdd411aa48a6fd17
Paul <paulxicao7@gmail.com>
2026-09-20 15:14:03 +0200

    use fine-grained include for <optional>

--- libcxx/include/__pstl/cpu_algos/transform_scan.h
+++ libcxx/include/__pstl/cpu_algos/transform_scan.h
@@ -14,11 +14,11 @@
 #include <__iterator/iterator_traits.h>
 #include <__numeric/transform_exclusive_scan.h>
 #include <__numeric/transform_inclusive_scan.h>
+#include <__optional/optional.h>
 #include <__pstl/backend_fwd.h>
 #include <__pstl/cpu_algos/cpu_traits.h>
 #include <__type_traits/is_execution_policy.h>
 #include <__utility/move.h>
-#include <optional>
 
 #if !defined(_LIBCPP_HAS_NO_PRAGMA_SYSTEM_HEADER)
 #  pragma GCC system_header
