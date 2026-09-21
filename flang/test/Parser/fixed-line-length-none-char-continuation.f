aed530b4808e27f5c522c3f171b40c3c084ec79b
Mattéo Rizza Murgier <matteo.rizza-murgier@sipearl.com>
2026-09-21 13:32:15 +0200

    Add strict whitespace flag to test

--- flang/test/Parser/fixed-line-length-none-char-continuation.f
+++ flang/test/Parser/fixed-line-length-none-char-continuation.f
@@ -1,7 +1,7 @@
-! RUN: %flang_fc1 -fdebug-unparse %s | FileCheck %s --check-prefix=DEFAULT
-! RUN: %flang_fc1 -fdebug-unparse -ffixed-line-length=132 %s | FileCheck %s --check-prefix=LEN132
-! RUN: %flang_fc1 -fdebug-unparse -ffixed-line-length=none %s | FileCheck %s --check-prefix=NONE
-! RUN: %flang_fc1 -fdebug-unparse -ffixed-line-length=0 %s | FileCheck %s --check-prefix=NONE
+! RUN: %flang_fc1 -fdebug-unparse %s | FileCheck %s --strict-whitespace --check-prefix=DEFAULT
+! RUN: %flang_fc1 -fdebug-unparse -ffixed-line-length=132 %s | FileCheck %s --strict-whitespace --check-prefix=LEN132
+! RUN: %flang_fc1 -fdebug-unparse -ffixed-line-length=none %s | FileCheck %s --strict-whitespace --check-prefix=NONE
+! RUN: %flang_fc1 -fdebug-unparse -ffixed-line-length=0 %s | FileCheck %s --strict-whitespace --check-prefix=NONE
       character*4 s
       s = 'ab
      +cd'
