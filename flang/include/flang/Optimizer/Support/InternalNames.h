8c4f51f5f2f634f2c8faea77bd0a419df401a7c9
Valentin Clement (バレンタイン クレメン) <clementval@gmail.com>
2026-09-21 11:55:49 -0700

    update comment
    Co-authored-by: Valentin Clement (バレンタイン クレメン) <clementval@gmail.com>

--- flang/include/flang/Optimizer/Support/InternalNames.h
+++ flang/include/flang/Optimizer/Support/InternalNames.h
@@ -198,6 +198,8 @@ struct NameUniquer {
   /// Returns true if the passed name denotes a compiler generated name.
   /// If \p excludeStringLiterals is true, string literals are excluded from the
   /// check.
+  /// If \p excludeArrayLiterals is true, array literals are excluded from the
+  /// check.
   static bool isCompilerGenerated(llvm::StringRef name,
                                   bool excludeStringLiterals = true,
                                   bool excludeArrayLiterals = true);
