017d946b5328ccdd7e5f58e093880f11e57ebf54
Jonathan Thackray <jonathan.thackray@arm.com>
2026-09-21 11:26:34 +0100

    fixup! Amend after PR comments

--- llvm/test/MC/AArch64/retired-register-names.s
+++ llvm/test/MC/AArch64/retired-register-names.s
@@ -1,12 +1,9 @@
-// RUN: not llvm-mc -triple=aarch64 -mattr=+mpamv2,+tme < %s 2>&1 | FileCheck %s
+// RUN: not llvm-mc -triple=aarch64 -mattr=+all < %s 2>&1 | FileCheck %s
 
-// Add negative tests preventing reuse of retired feature and system register names.
+// Add negative tests preventing reuse of retired system register names.
 //
-// When system registers or feature names are retired, they should be appended to
+// When system registers are retired, they should be appended to
 // this file, to ensure they're never re-used in future.
-
-// CHECK:      '+mpamv2' is not a recognized feature for this target (ignoring feature)
-// CHECK-NEXT: '+tme' is not a recognized feature for this target (ignoring feature)
 
 mrs x0, mpamvidcr_el2
 // CHECK:      :[[@LINE-1]]:9: error: expected readable system register
