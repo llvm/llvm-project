7dd0cdb31acde32807801b00468c886afd49fb94
Daniil Kovalev <dkovalev@accesssoftek.com>
2026-09-21 12:05:18 +0300

    Address review comments

--- llvm/test/MC/AArch64/tls-auth-relocs.s
+++ llvm/test/MC/AArch64/tls-auth-relocs.s
@@ -1,12 +1,12 @@
 // RUN: llvm-mc -triple=aarch64-none-linux-gnu -mattr=+pauth -show-encoding %s | FileCheck %s
 // RUN: llvm-mc -triple=aarch64-none-linux-gnu -mattr=+pauth -filetype=obj %s -o - | \
-// RUN:   llvm-readobj -r --symbols - | FileCheck --check-prefix=CHECK-ELF %s
+// RUN:   llvm-readelf -r -s - | FileCheck --check-prefix=CHECK-ELF %s
 
-        adrp x8, :tlsdesc_auth:var
-        ldr x7, [x6, :tlsdesc_auth_lo12:var]
-        add x5, x4, #:tlsdesc_auth_lo12:var
-        .tlsauthdesccall var
-        blraa x3, x2
+adrp x8, :tlsdesc_auth:var
+ldr x7, [x6, :tlsdesc_auth_lo12:var]
+add x5, x4, #:tlsdesc_auth_lo12:var
+.tlsauthdesccall var
+blraa x3, x2
 
 // CHECK:      adrp   x8, :tlsdesc_auth:var            // encoding: [0x08'A',A,A,0x90'A']
 // CHECK-NEXT:                                         // fixup A - offset: 0, value: :tlsdesc_auth:var, kind: fixup_aarch64_pcrel_adrp_imm21
@@ -18,19 +18,15 @@
 // CHECK-NEXT:                                         // fixup A - offset: 0, value: var, relocation type: 598
 // CHECK:      blraa  x3, x2                           // encoding: [0x62,0x08,0x3f,0xd7]
 
-// CHECK-ELF:      Relocations [
-// CHECK-ELF-NEXT:   Section {{.*}} .rela.text {
-// CHECK-ELF-NEXT:     0x0 R_AARCH64_AUTH_TLSDESC_ADR_PAGE21 [[VARSYM:[^ ]+]]
-// CHECK-ELF-NEXT:     0x4 R_AARCH64_AUTH_TLSDESC_LD64_LO12 [[VARSYM]]
-// CHECK-ELF-NEXT:     0x8 R_AARCH64_AUTH_TLSDESC_ADD_LO12 [[VARSYM]]
-// CHECK-ELF-NEXT:     0xC R_AARCH64_AUTH_TLSDESC_CALL [[VARSYM]]
+// CHECK-ELF:      Relocation section '.rela.text' at offset 0x98 contains 4 entries:
+// CHECK-ELF-NEXT: Offset           Info             Type                              Symbol's Value   Symbol's Name + Addend
+// CHECK-ELF-NEXT: 0000000000000000 0000000200000253 R_AARCH64_AUTH_TLSDESC_ADR_PAGE21 0000000000000000 var + 0
+// CHECK-ELF-NEXT: 0000000000000004 0000000200000254 R_AARCH64_AUTH_TLSDESC_LD64_LO12  0000000000000000 var + 0
+// CHECK-ELF-NEXT: 0000000000000008 0000000200000255 R_AARCH64_AUTH_TLSDESC_ADD_LO12   0000000000000000 var + 0
+// CHECK-ELF-NEXT: 000000000000000c 0000000200000256 R_AARCH64_AUTH_TLSDESC_CALL       0000000000000000 var + 0
 
 // Make sure symbol has type STT_TLS:
 
-// CHECK-ELF:      Symbols [
-// CHECK-ELF:        Symbol {
-// CHECK-ELF:          Name: var
-// CHECK-ELF-NEXT:     Value:
-// CHECK-ELF-NEXT:     Size:
-// CHECK-ELF-NEXT:     Binding: Global
-// CHECK-ELF-NEXT:     Type: TLS
+// CHECK-ELF:      Symbol table '.symtab' contains [[#]] entries:
+// CHECK-ELF-NEXT: Num:   Value            Size Type Bind   Vis     Ndx Name
+// CHECK-ELF:      [[#]]: 0000000000000000 0    TLS  GLOBAL DEFAULT UND var
