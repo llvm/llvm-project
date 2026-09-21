81b5c6909c49943a317d64c00166abb5e01781c7
Piotrek Jeremicz <piotrek@jeremicz.com>
2026-09-21 23:32:16 +0200

    [lldb] Added DWIM tests

--- lldb/test/API/commands/dwim-print/objc/TestDWIMPrintObjC.py
+++ lldb/test/API/commands/dwim-print/objc/TestDWIMPrintObjC.py
@@ -25,3 +25,29 @@ class TestCase(TestBase):
         self.runCmd("type summary add -s 'Parent of ${var._child._name}' 'Parent *'")
         self.expect("dwim-print parent", matching=False, substrs=["_child = 0x"])
         self.expect("dwim-print parent", substrs=['Parent of @"Seven"'])
+
+    @requireDarwin
+    def test_property_backing_storage(self):
+        """A property backed by a differently-named ivar (DW_TAG_property /
+        DW_AT_property_forward) should take the frame-variable fast path,
+        instead of unconditionally falling back to `expression`. Covers the
+        same three synthesis variants as
+        clang/test/DebugInfo/ObjC/property-backing-storage.m."""
+        self.build()
+        lldbutil.run_to_source_breakpoint(
+            self, "break here for backing storage", lldb.SBFileSpec("main.m")
+        )
+        self.runCmd("settings set dwim-print-verbosity full")
+
+        self.expect(
+            "dwim-print obj.declaredBacking",
+            substrs=["ran `frame variable obj._customDeclaredIvar`", "(int) 42"],
+        )
+        self.expect(
+            "dwim-print obj.undeclaredBacking",
+            substrs=["ran `frame variable obj._customUndeclaredIvar`", "(int) 7"],
+        )
+        self.expect(
+            "dwim-print obj.implicitBacking",
+            substrs=["ran `frame variable obj._implicitBacking`", "(int) 99"],
+        )
--- lldb/test/API/commands/dwim-print/objc/main.m
+++ lldb/test/API/commands/dwim-print/objc/main.m
@@ -14,11 +14,31 @@
 @implementation Parent
 @end
 
+@interface C : NSObject {
+  int _customDeclaredIvar;
+}
+@property int declaredBacking;
+@property int undeclaredBacking;
+@property int implicitBacking;
+@end
+
+@implementation C
+@synthesize declaredBacking = _customDeclaredIvar;
+@synthesize undeclaredBacking = _customUndeclaredIvar;
+// implicitBacking: no @synthesize -> compiler auto-creates _implicitBacking.
+@end
+
 int main(int argc, char **argv) {
   Child *child = [Child new];
   child.name = @"Seven";
   Parent *parent = [Parent new];
   parent.child = child;
   puts("break here");
+
+  C *obj = [C new];
+  obj.declaredBacking = 42;
+  obj.undeclaredBacking = 7;
+  obj.implicitBacking = 99;
+  puts("break here for backing storage");
   return 0;
 }
