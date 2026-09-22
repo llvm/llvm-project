7e7b0198fd900f54c15cb7bbb720c750be9db888
Nikita Popov <npopov@redhat.com>
2026-09-22 14:23:56 +0200

    [IR] Remove llvm.strip.invariant.group intrinsic

--- llvm/lib/Transforms/InstCombine/InstructionCombining.cpp
+++ llvm/lib/Transforms/InstCombine/InstructionCombining.cpp
@@ -3810,7 +3810,6 @@ isAllocSiteRemovable(Instruction *AI, SmallVectorImpl<Instruction *> &Users,
             Users.emplace_back(I);
             continue;
           case Intrinsic::launder_invariant_group:
-          case Intrinsic::strip_invariant_group:
             Users.emplace_back(I);
             Worklist.push_back(I);
             continue;
