80c6e7c653310708ce6411f16ee3fdc4782ccfb6
Marcos Ramirez Joos <mramirezjoos.oss@proton.me>
2026-09-22 10:40:24 +0100

    Revert "Added builtin types fexcept_t and fenv_t (fenv.h)"
    This reverts commit 31fee2867d589a1dbe1acd88fc66e18a3bbccf90.

--- clang/utils/TableGen/ClangBuiltinsEmitter.cpp
+++ clang/utils/TableGen/ClangBuiltinsEmitter.cpp
@@ -383,8 +383,6 @@ private:
                                .Case("uint64_t", "UWi")
                                .Case("void", "v")
                                .Case("wchar_t", "w")
-                               .Case("fexcept_t", "t")
-                               .Case("fenv_t", "e")
                                .Case("...", ".")
                                .Default("error");
       if (ReturnTypeVal == "error")
