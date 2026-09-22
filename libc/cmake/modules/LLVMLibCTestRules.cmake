481c6a4257b924f968133ed44f73040eba993359
Tue Ly <lntue.h@gmail.com>
2026-09-22 14:28:05 +0000

    Remove worst-case tests and its utilities.

--- libc/cmake/modules/LLVMLibCTestRules.cmake
+++ libc/cmake/modules/LLVMLibCTestRules.cmake
@@ -19,11 +19,6 @@ function(_get_common_test_compile_options output_var c_test flags)
       ${config_flags}
       ${arch_flags})
 
-  if(LIBC_TEST_MAX_CONCURRENCY)
-    libc_add_definition(compile_options
-      "LIBC_TEST_MAX_CONCURRENCY=${LIBC_TEST_MAX_CONCURRENCY}")
-  endif()
-
   # EXPECT_DEATH and ASSERT_DEATH might be quite slow.  LIBC_TEST_SKIP_DEATH_TESTS
   # will make those tests no-op to reduce the overall test time.
   if(LIBC_TEST_SKIP_DEATH_TESTS)
