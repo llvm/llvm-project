731f3382d86e076611ed784d05558610bf2c2c3e
Akash Manna <akash.manna.mymail@gmail.com>
2026-09-22 22:14:50 +0530

    [Clang] Update OpenACC test for empty declarations becoming null statements
    `using j;` is an error and yields no declaration, which is now a null
    statement rather than an invalid one, so 'acc loop' correctly reports
    that it was not applied to a 'for' loop.

--- clang/test/ParserOpenACC/directive-scope-setup.cpp
+++ clang/test/ParserOpenACC/directive-scope-setup.cpp
@@ -3,10 +3,12 @@
 void func() {
 #pragma acc parallel
   using i; // expected-error{{using declaration requires a qualified name}}
-#pragma acc loop
-  using j; // expected-error{{using declaration requires a qualified name}}
-#pragma acc parallel loop
-  using k; // expected-error{{using declaration requires a qualified name}}
+#pragma acc loop // expected-note{{'loop' construct is here}}
+  using j; // expected-error{{using declaration requires a qualified name}} \
+              expected-error{{OpenACC 'loop' construct can only be applied to a 'for' loop}}
+#pragma acc parallel loop // expected-note{{'parallel loop' construct is here}}
+  using k; // expected-error{{using declaration requires a qualified name}} \
+              expected-error{{OpenACC 'parallel loop' construct can only be applied to a 'for' loop}}
 #pragma acc data default(none)
   using l; // expected-error{{using declaration requires a qualified name}}
 }
