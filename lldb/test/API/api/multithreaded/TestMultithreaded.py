f78af3d2d4bf9830517697e5d3cf479f10768513
Raphael Isemann <rise@apple.com>
2026-09-22 10:00:02 +0100

    [lldb][test] Add test for concurrently accessing SBSymbol

--- lldb/test/API/api/multithreaded/TestMultithreaded.py
+++ lldb/test/API/api/multithreaded/TestMultithreaded.py
@@ -97,6 +97,17 @@ class SBBreakpointCallbackCase(TestBase):
             inferior_source="deep_stack.cpp",
         )
 
+    @skipIfRemote
+    # clang-cl does not support throw or catch (llvm.org/pr24538)
+    @skipIfWindows
+    @skipIfHostIncompatibleWithTarget
+    def test_concurrent_symbol_names(self):
+        """Test that several threads can read the name of the same symbol."""
+        self.build_and_test(
+            "driver.cpp test_concurrent_symbol_names.cpp",
+            "test_concurrent_symbol_names",
+        )
+
     def build_and_test(self, sources, test_name, inferior_source="inferior.cpp"):
         """Build LLDB test from sources, and run expecting 0 exit code"""
 
