2234e62f5a0a3dccb82fd509bd52f00155a3b989
Sarka Holendova <sarka.holendova@gmail.com>
2026-09-22 06:58:47 -0400

    [flang] Use FileCheck for IEEE SIMPLE function test

--- flang/test/Semantics/simple-ieee-arithmetic-functions.f90
+++ flang/test/Semantics/simple-ieee-arithmetic-functions.f90
@@ -1,15 +1,10 @@
-! Test that IEEE_ARITHMETIC module functions are classified SIMPLE.
-! RUN: %python %S/test_symbols.py %s %flang_fc1
+! Test that IEEE_ARITHMETIC module functions are classified SIMPLE
+! RUN: %flang_fc1 -fdebug-dump-symbols %s | FileCheck %s
 
-!DEF: /TEST_IEEE_ARITHMETIC_SIMPLE MainProgram
-program TEST_IEEE_ARITHMETIC_SIMPLE
-  !DEF: /ieee_arithmetic INTRINSIC (ModFile) Module
-  !DEF: /ieee_arithmetic/ieee_is_finite PUBLIC (Function) Generic
-  use :: ieee_arithmetic, only: ieee_is_finite
+program test_ieee_arithmetic_simple
+  use, intrinsic :: ieee_arithmetic, only: ieee_is_finite
   implicit none
-  !DEF: /TEST_IEEE_ARITHMETIC_SIMPLE/x ObjectEntity LOGICAL(4)
-  logical x
-  !REF: /TEST_IEEE_ARITHMETIC_SIMPLE/x
-  !DEF: /TEST_IEEE_ARITHMETIC_SIMPLE/ieee_arithmetic$ieee_arithmetic$ieee_is_finite_a4 ELEMENTAL, EXTERNAL, PRIVATE, SIMPLE Use LOGICAL(4)
+  logical :: x
   x = ieee_is_finite(1.0)
 end program
+! CHECK: ieee_is_finite_a4, ELEMENTAL, EXTERNAL, PRIVATE, SIMPLE (Function):
