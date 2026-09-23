0303bb4a0d66edbdb4dd39d92094c33447d6b22d
Florian Hahn <flo@fhahn.com>
2026-09-23 10:41:14 +0100

    !fixup restore file header comment after merge

--- llvm/lib/Transforms/Vectorize/VPlanTransforms.h
+++ llvm/lib/Transforms/Vectorize/VPlanTransforms.h
@@ -1,4 +1,4 @@
-/=- VPlanTransforms.h - Utility VPlan to VPlan transforms --------------===//
+//===- VPlanTransforms.h - Utility VPlan to VPlan transforms --------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
