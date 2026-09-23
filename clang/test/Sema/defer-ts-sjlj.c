995108a364b039bef658384a02cfe380e0bdf4a2
Expertcoderz <expertcoderzx@gmail.com>
2026-09-23 14:52:28 +0000

    Remove DefaultIgnore on -Wredundant-defer

--- clang/test/Sema/defer-ts-sjlj.c
+++ clang/test/Sema/defer-ts-sjlj.c
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -triple x86_64-windows-msvc -std=gnu23 -fdefer-ts -fsyntax-only -fblocks -verify %s
+// RUN: %clang_cc1 -triple x86_64-windows-msvc -std=gnu23 -fdefer-ts -Wno-redundant-defer -fsyntax-only -fblocks -verify %s
 
 typedef void** jmp_buf;
 typedef void** sigjmp_buf;
