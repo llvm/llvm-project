ce53b413944fe1c8c0534de496475ea37b8c8bd9
Aiden Grossman <aidengrossman@google.com>
2026-09-23 20:39:55 +0000

    [StaticDataLayout] Enable string literal partitioning by default
    We have started to roll this out internally, so enable it by default
    upstream as well. This will enable seamless removal of the flag
    internally with respect to our release process.

--- llvm/lib/Analysis/StaticDataProfileInfo.cpp
+++ llvm/lib/Analysis/StaticDataProfileInfo.cpp
@@ -18,7 +18,7 @@ namespace llvm {
 // AnnotateStaticDataSectionPrefix in MemProfUse.cpp and this option should be
 // cleaned up.
 cl::opt<bool> AnnotateStringLiteralSectionPrefix(
-    "memprof-annotate-string-literal-section-prefix", cl::init(false),
+    "memprof-annotate-string-literal-section-prefix", cl::init(true),
     cl::Hidden,
     cl::desc("If true, annotate the string literal data section prefix"));
 namespace memprof {
