79d45a50b75ff20d5dbebbcce52c77638435dd1c
Michael Kruse <llvm-project@meinersbur.de>
2026-09-23 12:06:08 +0200

    Print spelling (including dash)

--- clang/lib/Driver/Driver.cpp
+++ clang/lib/Driver/Driver.cpp
@@ -3527,12 +3527,12 @@ void Driver::handleArguments(Compilation &C, DerivedArgList &Args,
                getPreprocessedType(InputType) == types::TY_INVALID)
         Diag(clang::diag::warn_drv_preprocessed_input_file_unused)
             << InputArg->getAsString(Args) << !FinalPhaseArg
-            << (FinalPhaseArg ? FinalPhaseArg->getOption().getName() : "");
+            << (FinalPhaseArg ? FinalPhaseArg->getSpelling() : "");
       else
         Diag(clang::diag::warn_drv_input_file_unused)
             << InputArg->getAsString(Args) << getPhaseName(InitialPhase)
             << !FinalPhaseArg
-            << (FinalPhaseArg ? FinalPhaseArg->getOption().getName() : "");
+            << (FinalPhaseArg ? FinalPhaseArg->getSpelling() : "");
       continue;
     }
 
--- clang/test/Driver/preprocessed-input-file-unused.c
+++ clang/test/Driver/preprocessed-input-file-unused.c
@@ -4,12 +4,12 @@
 
 // RUN: %clang -E %S/Inputs/preprocessed-input-file-unused.i -o /dev/null 2>&1 \
 // RUN:   | FileCheck --check-prefix=CHECK-E %s
-// CHECK-E: warning: {{.*}}preprocessed-input-file-unused.i: previously preprocessed input unused when 'E' is present [-Wunused-command-line-argument]
+// CHECK-E: warning: {{.*}}preprocessed-input-file-unused.i: previously preprocessed input unused when '-E' is present [-Wunused-command-line-argument]
 
 // RUN: %clang -M %S/Inputs/preprocessed-input-file-unused.i -o /dev/null 2>&1 \
 // RUN:   | FileCheck --check-prefix=CHECK-M %s
-// CHECK-M: warning: {{.*}}preprocessed-input-file-unused.i: previously preprocessed input unused when 'M' is present [-Wunused-command-line-argument]
+// CHECK-M: warning: {{.*}}preprocessed-input-file-unused.i: previously preprocessed input unused when '-M' is present [-Wunused-command-line-argument]
 
 // RUN: %clang -MM %S/Inputs/preprocessed-input-file-unused.i -o /dev/null 2>&1 \
 // RUN:   | FileCheck --check-prefix=CHECK-MM %s
-// CHECK-MM: warning: {{.*}}preprocessed-input-file-unused.i: previously preprocessed input unused when 'MM' is present [-Wunused-command-line-argument]
+// CHECK-MM: warning: {{.*}}preprocessed-input-file-unused.i: previously preprocessed input unused when '-MM' is present [-Wunused-command-line-argument]
