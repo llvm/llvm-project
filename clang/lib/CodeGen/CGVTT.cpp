e0416baf412b44d09be9b300b50a15c57cdf148b
Nikita Popov <npopov@redhat.com>
2026-09-23 09:15:54 +0200

    [Clang] Use ConstantExpr::getGetElementPtr() with DataLayout (#225415)
    This switches Clang to use the API introduced in
    https://github.com/llvm/llvm-project/pull/224652. This produces
    getelementptr constant expressions in canonical ptradd form.

--- clang/lib/CodeGen/CGVTT.cpp
+++ clang/lib/CodeGen/CGVTT.cpp
@@ -70,7 +70,7 @@ CodeGenVTables::EmitVTTDefinition(llvm::GlobalVariable *VTT,
              "Did not find ctor vtable address point!");
     }
 
-     llvm::Value *Idxs[] = {
+     llvm::Constant *Idxs[] = {
        llvm::ConstantInt::get(CGM.Int32Ty, 0),
        llvm::ConstantInt::get(CGM.Int32Ty, AddressPoint.VTableIndex),
        llvm::ConstantInt::get(CGM.Int32Ty, AddressPoint.AddressPointIndex),
@@ -88,7 +88,8 @@ CodeGenVTables::EmitVTTDefinition(llvm::GlobalVariable *VTT,
          llvm::APInt(32, (int)-Offset, true),
          llvm::APInt(32, (int)(VTableSize - Offset), true));
      llvm::Constant *Init = llvm::ConstantExpr::getGetElementPtr(
-         VTable->getValueType(), VTable, Idxs, /*InBounds=*/true, InRange);
+         CGM.getDataLayout(), VTable->getValueType(), VTable, Idxs,
+         llvm::GEPNoWrapFlags::inBounds(), InRange);
 
      if (auto PAuthQual =
              CGM.getVTablePointerAuthentication(VTTVT.getBase(),
@@ -96,7 +97,8 @@ CodeGenVTables::EmitVTTDefinition(llvm::GlobalVariable *VTT,
        llvm::Constant *Address = nullptr;
        if (PAuthQual->isAddressDiscriminated())
          Address = llvm::ConstantExpr::getGetElementPtr(
-             VTT->getType(), VTT, llvm::ConstantInt::get(CGM.Int32Ty, Idx));
+             CGM.getDataLayout(), VTT->getType(), VTT,
+             llvm::ConstantInt::get(CGM.Int32Ty, Idx));
        auto *Discriminator = llvm::ConstantInt::get(
            CGM.IntPtrTy, PAuthQual->getExtraDiscriminator());
        Init = CGM.getConstantSignedPointer(Init, PAuthQual->getKey(), Address,
