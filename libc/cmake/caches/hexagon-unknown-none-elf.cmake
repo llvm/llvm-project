4c46050122ae35970225b0d7e9cc74cefd27030a
Kushal Pal <kushpal@qti.qualcomm.com>
2026-09-23 10:15:29 +0530

    [Github] Enable libc tests for Hexagon (baremetal)
    Signed-off-by: Kushal Pal <kushpal@qti.qualcomm.com>

---
+++ libc/cmake/caches/hexagon-unknown-none-elf.cmake
@@ -0,0 +1,32 @@
+set(CMAKE_SYSTEM_PROCESSOR hexagon CACHE STRING "")
+set(RUNTIMES_TARGET_TRIPLE "hexagon-unknown-none-elf" CACHE STRING "")
+
+# flag to build compiler-rt builtins
+set(CMAKE_C_FLAGS "-mv68 -G0" CACHE STRING "")
+set(CMAKE_CXX_FLAGS "-mv68 -G0" CACHE STRING "")
+set(CMAKE_ASM_FLAGS "-mv68 -G0" CACHE STRING "")
+
+set(LLVM_ENABLE_RUNTIMES "libc;compiler-rt" CACHE STRING "")
+set(LIBC_TEST_BUILTINS_TARGET "clang_rt.builtins-hexagon" CACHE STRING "")
+
+set(LIBC_COMPILE_OPTIONS_DEFAULT
+    "-mv68;-G0;-fuse-init-array"
+    CACHE STRING "")
+set(LIBC_LINK_OPTIONS_DEFAULT
+    "-mv68;-G0"
+    CACHE STRING "")
+set(LIBC_TEST_COMPILE_OPTIONS_DEFAULT "-O0" CACHE STRING "")
+
+# The bare-metal linker script requires explicit memory-region definitions for
+# the test image.
+set(LIBC_TEST_LINK_OPTIONS_DEFAULT
+    "-mv68;-G0;-fuse-ld=lld;-T;${CMAKE_CURRENT_LIST_DIR}/../../test/UnitTest/llvm-libc-baremetal.ld;-Wl,--defsym=__boot_flash=0x00100000;-Wl,--defsym=__boot_flash_size=0x00001000;-Wl,--defsym=__flash=0x00101000;-Wl,--defsym=__flash_size=0x003ff000;-Wl,--defsym=__ram=0x00500000;-Wl,--defsym=__ram_size=0x00800000;-Wl,--defsym=__stack_size=0x00040000"
+    CACHE STRING "")
+set(LIBC_TEST_CMD
+    "qemu-system-hexagon -M sim -cpu v68 -kernel @BINARY@"
+    CACHE STRING "")
+
+include(${CMAKE_CURRENT_LIST_DIR}/baremetal_common.cmake)
+
+# Enable hermetic tests for the QEMU-backed configuration.
+set(LLVM_INCLUDE_TESTS ON CACHE BOOL "" FORCE)
--- libc/cmake/modules/LLVMLibCTestRules.cmake
+++ libc/cmake/modules/LLVMLibCTestRules.cmake
@@ -644,8 +644,8 @@ function(add_integration_test test_name)
       "--cuda-path=${LIBC_CUDA_ROOT}")
   elseif(LIBC_CC_SUPPORTS_NOSTDLIBPP)
     set(link_options
-      -nolibc
       -nostartfiles
+      -nodefaultlibs
       -nostdlib++
       -static
       ${LIBC_LINK_OPTIONS_DEFAULT}
@@ -656,8 +656,8 @@ function(add_integration_test test_name)
     # Older version of gcc does not support `nostdlib++` flag.  We use
     # `nostdlib` and link against libgcc_s, which cannot be linked statically.
     set(link_options
-      -nolibc
       -nostartfiles
+      -nodefaultlibs
       -nostdlib
       ${LIBC_LINK_OPTIONS_DEFAULT}
       ${LIBC_TEST_LINK_OPTIONS_DEFAULT}
@@ -670,8 +670,13 @@ function(add_integration_test test_name)
     libc.startup.${LIBC_TARGET_OS}.crt1
     libc.test.IntegrationTest.test
     ${fq_target_name}.__libc__
+    ${LIBC_TEST_BUILTINS_LIBRARY}
     ${compiler_runtime}
   )
+  if(LIBC_TEST_BUILTINS_TARGET)
+    target_link_libraries(${fq_build_target_name} $<TARGET_FILE:${LIBC_TEST_BUILTINS_TARGET}>)
+    add_dependencies(${fq_build_target_name} ${LIBC_TEST_BUILTINS_TARGET})
+  endif()
   add_dependencies(${fq_build_target_name}
     libc.test.IntegrationTest.test
     ${INTEGRATION_TEST_DEPENDS})
@@ -958,8 +963,8 @@ function(add_libc_hermetic test_name)
       "--cuda-path=${LIBC_CUDA_ROOT}")
   elseif(LIBC_CC_SUPPORTS_NOSTDLIBPP)
     set(link_options
-      -nolibc
       -nostartfiles
+      -nodefaultlibs
       -nostdlib++
       -static
       ${LIBC_LINK_OPTIONS_DEFAULT}
@@ -976,8 +981,8 @@ function(add_libc_hermetic test_name)
     # Older version of gcc does not support `nostdlib++` flag.  We use
     # `nostdlib` and link against libgcc_s, which cannot be linked statically.
     set(link_options
-      -nolibc
       -nostartfiles
+      -nodefaultlibs
       -nostdlib
       ${LIBC_LINK_OPTIONS_DEFAULT}
       ${LIBC_TEST_LINK_OPTIONS_DEFAULT}
@@ -1007,8 +1012,13 @@ function(add_libc_hermetic test_name)
       ${HERMETIC_TEST_LINK_LIBRARIES}
       ${fq_target_name}.__libc__
       ${coverage_link_libs}
+      ${LIBC_TEST_BUILTINS_LIBRARY}
       ${compiler_runtime}
   )
+  if(LIBC_TEST_BUILTINS_TARGET)
+    target_link_libraries(${fq_build_target_name} PRIVATE $<TARGET_FILE:${LIBC_TEST_BUILTINS_TARGET}>)
+    add_dependencies(${fq_build_target_name} ${LIBC_TEST_BUILTINS_TARGET})
+  endif()
   add_dependencies(${fq_build_target_name} ${fq_deps_list})
 
   if(NOT HERMETIC_TEST_NO_RUN_POSTBUILD)
