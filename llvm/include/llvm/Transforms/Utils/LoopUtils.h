ce28845d39002e1dde040837cd65900067f7bdde
Benjamin Maxwell <benjamin.maxwell@arm.com>
2026-09-23 12:00:49 +0000

    Add LLVM_ABI marker

--- llvm/include/llvm/Transforms/Utils/LoopUtils.h
+++ llvm/include/llvm/Transforms/Utils/LoopUtils.h
@@ -714,10 +714,9 @@ hasPartialIVCondition(const Loop &L, unsigned MSSAThreshold,
 /// approximate SCEV expressions (assuming the monotonic PHI always increments)
 /// for the pointers are placed in \p CompressedPtrs. Returns true if all
 /// in-loop users of the conditional induction are loads/stores.
-bool collectCompressedPtrs(DenseMap<Value *, const SCEV *> &CompressedPtrs,
-                           const Loop &L,
-                           const ConditionalInductionDescriptor &CondID,
-                           ScalarEvolution &SE);
+LLVM_ABI bool collectCompressedPtrs(
+    DenseMap<Value *, const SCEV *> &CompressedPtrs, const Loop &L,
+    const ConditionalInductionDescriptor &CondID, ScalarEvolution &SE);
 
 } // end namespace llvm
 
