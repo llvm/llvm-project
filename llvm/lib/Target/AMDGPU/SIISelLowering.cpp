09e4d30644c771feac43a3d5d7f87240f328bb76
Jay Foad <jay.foad@amd.com>
2026-09-23 21:25:53 +0100

    Add a TODO

--- llvm/lib/Target/AMDGPU/SIISelLowering.cpp
+++ llvm/lib/Target/AMDGPU/SIISelLowering.cpp
@@ -7325,6 +7325,7 @@ SITargetLowering::EmitInstrWithCustomInserter(MachineInstr &MI,
   case AMDGPU::V_ADD_CO_U32_e32:
   case AMDGPU::V_SUB_CO_U32_e32:
   case AMDGPU::V_SUBREV_CO_U32_e32:
+    // TODO: Remove usesCustomInserter from these instructions.
     return BB;
   case AMDGPU::V_ADDC_U32_e32:
   case AMDGPU::V_SUBB_U32_e32:
