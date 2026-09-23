2e8bfb35be9033183371a8397cda4a1796ccd72a
Arvind Sudarsanam <asudarsa@qti.qualcomm.com>
2026-09-23 19:57:02 -0400

    Account for case-sensitive error message

--- clang/test/Interpreter/nonexistent-xcc-file.cpp
+++ clang/test/Interpreter/nonexistent-xcc-file.cpp
@@ -1,4 +1,4 @@
-// RUN: not clang-repl --Xcc=%t/nonexistent.cpp 2>&1 | FileCheck %s
+// RUN: not clang-repl --Xcc=%t/nonexistent.cpp 2>&1 | FileCheck --ignore-case %s
 
 // CHECK: error: error reading '{{.*}}nonexistent.cpp': No such file or directory
 // CHECK-NOT: Compiler instance not registered
