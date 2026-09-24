ef8d9d19fb232076b7a2070860666a65a2cf563e
oltolm <oleg.tolmatcev@gmail.com>
2026-09-24 23:12:54 +0200

    [compiler-rt] Retain sanitizer callbacks with MinGW GCC (#222711)
    MSVC and clang-cl use /include directives emitted by WIN_FORCE_LINK. GNU
    tools need an ordinary used relocation instead. Keep the sanitizer
    initialization callbacks and ASan DSO registration object reachable when
    linking with MinGW GCC.
    
    ---------
    
    Co-authored-by: Hannes Domani <ssbssa@yahoo.de>

--- compiler-rt/lib/asan/asan_win.cpp
+++ compiler-rt/lib/asan/asan_win.cpp
@@ -24,6 +24,7 @@
 #  include "asan_report.h"
 #  include "asan_stack.h"
 #  include "asan_thread.h"
+#  include "asan_win_common_runtime_thunk.h"
 #  include "sanitizer_common/sanitizer_libc.h"
 #  include "sanitizer_common/sanitizer_mutex.h"
 #  include "sanitizer_common/sanitizer_win.h"
