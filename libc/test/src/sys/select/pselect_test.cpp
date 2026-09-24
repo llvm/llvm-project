3eb9c00fa193e8e9f8a388e091fe3f5bf726a7ba
amanmaurya92 <amanmaurya9209@gmail.com>
2026-09-24 22:47:29 +0530

    [libc] Fix formatting in pselect_test
    Assisted by Gemini.

--- libc/test/src/sys/select/pselect_test.cpp
+++ libc/test/src/sys/select/pselect_test.cpp
@@ -60,20 +60,20 @@ TEST_F(LlvmLibcPSelectTest, SmokeFailureTest) {
 
 TEST_F(LlvmLibcPSelectTest, TimeoutNotMutated) {
   sigalrm_handler_called = false;
-  struct sigaction sa {};
+  struct sigaction sa{};
   sa.sa_handler = handle_sigalrm;
   LIBC_NAMESPACE::sigemptyset(&sa.sa_mask);
   sa.sa_flags = 0;
-  struct sigaction old_sa {};
+  struct sigaction old_sa{};
   ASSERT_EQ(LIBC_NAMESPACE::sigaction(SIGALRM, &sa, &old_sa), 0);
 
   LIBC_NAMESPACE::cpp::scope_exit restore_sa([&] {
     LIBC_NAMESPACE::sigaction(SIGALRM, &old_sa, nullptr);
-    struct itimerval disable_timer {};
+    struct itimerval disable_timer{};
     LIBC_NAMESPACE::setitimer(ITIMER_REAL, &disable_timer, nullptr);
   });
 
-  struct itimerval timer {};
+  struct itimerval timer{};
   timer.it_value.tv_sec = 0;
   timer.it_value.tv_usec = 100000; // 100ms
   ASSERT_EQ(LIBC_NAMESPACE::setitimer(ITIMER_REAL, &timer, nullptr), 0);
@@ -94,11 +94,11 @@ TEST_F(LlvmLibcPSelectTest, TimeoutNotMutated) {
 
 TEST_F(LlvmLibcPSelectTest, WithSigmask) {
   sigusr1_handler_called = false;
-  struct sigaction sa {};
+  struct sigaction sa{};
   sa.sa_handler = handle_sigusr1;
   LIBC_NAMESPACE::sigemptyset(&sa.sa_mask);
   sa.sa_flags = 0;
-  struct sigaction old_sa {};
+  struct sigaction old_sa{};
   ASSERT_EQ(LIBC_NAMESPACE::sigaction(SIGUSR1, &sa, &old_sa), 0);
 
   sigset_t block_mask{};
