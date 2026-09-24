342338a0308fb6f3a524947f868d2032db46cf47
amanmaurya92 <amanmaurya9209@gmail.com>
2026-09-24 10:16:41 +0530

    [libc] Address review: separate renameat syscall wrapper and update headers
    - Move renameat syscall wrapper to its own renameat.h header.
    - Add missing dependencies (macros.config, common, libc_errno) to renameat CMake entrypoint.
    - Update file headers to LLVM standard \file style.
    
    Assisted by Gemini.

--- libc/src/stdio/linux/renameat.cpp
+++ libc/src/stdio/linux/renameat.cpp
@@ -1,13 +1,18 @@
-//===-- Linux implementation of renameat ----------------------------------===//
+//===----------------------------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
+///
+/// \file
+/// Linux implementation of renameat.
+///
+//===----------------------------------------------------------------------===//
 
 #include "src/stdio/renameat.h"
-#include "src/__support/OSUtil/linux/syscall_wrappers/rename.h"
+#include "src/__support/OSUtil/linux/syscall_wrappers/renameat.h"
 #include "src/__support/common.h"
 #include "src/__support/libc_errno.h"
 #include "src/__support/macros/config.h"
--- libc/src/stdio/renameat.h
+++ libc/src/stdio/renameat.h
@@ -1,9 +1,14 @@
-//===-- Implementation header of renameat -----------------------*- C++ -*-===//
+//===----------------------------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// Implementation header of renameat.
+///
 //===----------------------------------------------------------------------===//
 
 #ifndef LLVM_LIBC_SRC_STDIO_RENAMEAT_H
