5b3d418cbbd323213e32fa08c37bd397306e5e4c
Jack Huang <jackhuang1205@gmail.com>
2026-09-25 01:53:19 +0800

    Use the original API to create instruction

--- llvm/lib/Transforms/Vectorize/VectorCombine.cpp
+++ llvm/lib/Transforms/Vectorize/VectorCombine.cpp
@@ -1445,33 +1445,42 @@ bool VectorCombine::scalarizeOpOrCmp(Instruction &I) {
           cast<Constant>(VecC), Builder.getInt64(*Index));
 
   Value *Scalar;
+  // We need to pass the flags during the creation of instrucitons. Constant
+  // folding might remove the instructions, so post setting the flags might
+  // pollute the later instructions.
   if (CI) {
-    FPMathOperator *FPMO = dyn_cast<FPMathOperator>(&I);
-    FMFSource FMF;
-    if (FPMO)
-      FMF = FPMO->getFastMathFlags();
-    Scalar =
-        Builder.CreateFlaggedCmp(CI->getPredicate(), ScalarOps[0], ScalarOps[1],
-                                 FMF, CI->getName() + ".scalar");
+    if (FPMathOperator *FPMO = dyn_cast<FPMathOperator>(&I))
+      Scalar = Builder.CreateFCmpFMF(CI->getPredicate(), ScalarOps[0],
+                                     ScalarOps[1], FPMO->getFastMathFlags(),
+                                     CI->getName() + ".scalar");
+    else
+      Scalar = Builder.CreateICmp(CI->getPredicate(), ScalarOps[0],
+                                  ScalarOps[1], CI->getName() + ".scalar");
   } else if (UO) {
     // FNeg is the only unary operator.
     Scalar = Builder.CreateFNegFMF(ScalarOps[0], UO, UO->getName() + ".scalar");
   } else if (BO) {
-    OverflowingBinaryOperator *OBO = dyn_cast<OverflowingBinaryOperator>(&I);
-    bool HasNUW = OBO ? OBO->hasNoUnsignedWrap() : false;
-    bool HasNSW = OBO ? OBO->hasNoSignedWrap() : false;
-    PossiblyDisjointInst *PDI = dyn_cast<PossiblyDisjointInst>(&I);
-    bool IsDisjoint = PDI ? PDI->isDisjoint() : false;
-    PossiblyExactOperator *PEO = dyn_cast<PossiblyExactOperator>(&I);
-    bool IsExact = PEO ? PEO->isExact() : false;
-    FPMathOperator *FPMO = dyn_cast<FPMathOperator>(&I);
-    FMFSource FMF;
-    if (FPMO)
-      FMF = FPMO->getFastMathFlags();
-
-    Scalar = Builder.CreateFlaggedBinOp(
-        BO->getOpcode(), ScalarOps[0], ScalarOps[1], BO->getName() + ".scalar",
-        HasNUW, HasNSW, IsExact, IsDisjoint, FMF);
+    if (OverflowingBinaryOperator *OBO =
+            dyn_cast<OverflowingBinaryOperator>(&I)) {
+      Scalar = Builder.CreateNoWrapBinOp(
+          BO->getOpcode(), ScalarOps[0], ScalarOps[1], OBO->hasNoUnsignedWrap(),
+          OBO->hasNoSignedWrap(), BO->getName() + ".scalar");
+    } else if (PossiblyDisjointInst *PDI = dyn_cast<PossiblyDisjointInst>(&I)) {
+      Scalar = Builder.CreateOr(ScalarOps[0], ScalarOps[1],
+                                BO->getName() + ".scalar", PDI->isDisjoint());
+    } else if (PossiblyExactOperator *PEO =
+                   dyn_cast<PossiblyExactOperator>(&I)) {
+      Scalar =
+          Builder.CreateExactBinOp(BO->getOpcode(), ScalarOps[0], ScalarOps[1],
+                                   PEO->isExact(), BO->getName() + ".scalar");
+    } else if (FPMathOperator *FPMO = dyn_cast<FPMathOperator>(&I)) {
+      Scalar = Builder.CreateBinOpFMF(BO->getOpcode(), ScalarOps[0],
+                                      ScalarOps[1], FPMO->getFastMathFlags(),
+                                      BO->getName() + ".scalar");
+    } else {
+      Scalar = Builder.CreateBinOp(BO->getOpcode(), ScalarOps[0], ScalarOps[1],
+                                   BO->getName() + ".scalar");
+    }
   } else {
     Scalar = Builder.CreateIntrinsic(ScalarTy, II->getIntrinsicID(), ScalarOps);
   }
