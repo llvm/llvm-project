551f45047d34085ccd5e543a7ce21fa7b22c3e6d
Cyndy Ishida <cyndy_ishida@apple.com>
2026-09-24 18:36:12 -0700

    [clang][Modules] Respect case sensitivity when hashing module map path
    Before this patch, the implicit module hash normalized the module map
    path, however explicit modules always respects the case in paths. This
    worked by depending on FileEntry caching & relocation checking to
    resolve correctly. This behavior was both subtle and difficult to understand. Instead,
    respect the casing in paths for hashing to match explicitly built
    modules so they can never diverage.
    
    This patch also updates `modules-context-hash-module-map-path.c` to
    match a downstream version that already tested for the case where vfs
    overlay enteries differ by case.

--- clang/test/ClangScanDeps/modules-context-hash-case-sensitive-vfs-paths.c
+++ clang/test/ClangScanDeps/modules-context-hash-case-sensitive-vfs-paths.c
@@ -1,12 +1,15 @@
-// Ensure the path to the modulemap input is included in the context hash
-// irrespective of other TU command-line arguments, as it effects the canonical
-// module build command. In this test we use the difference in case of a path.
+// Ensure the path to the modulemap input is included in the context hash.
+// This test uses different vfs overlay entries that only differ by case, 
+// but to point to different input paths.
 
 // RUN: rm -rf %t
 // RUN: split-file %s %t
 // RUN: sed "s|DIR|%/t|g" %t/cdb0.json.template > %t/cdb0.json
 // RUN: sed "s|DIR|%/t|g" %t/cdb1.json.template > %t/cdb1.json
 // RUN: sed "s|DIR|%/t|g" %t/overlay.json.template > %t/overlay.json
+// RUN: mkdir -p %t/foo
+// RUN: cp %t/Mod.h %t/foo/
+// RUN: cp %t/m.m %t/foo/
 
 // RUN: clang-scan-deps -compilation-database %t/cdb0.json -j 1 \
 // RUN:   -format experimental-full > %t/deps.json
@@ -17,6 +20,7 @@
 // RUN:   -format experimental-full >> %t/deps.json
 
 // RUN: cat %t/deps.json | sed 's:\\\\\?:/:g' | FileCheck -DPREFIX=%/t %s
+
 
 // CHECK:      {
 // CHECK-NEXT:   "modules": [
@@ -58,10 +62,7 @@
 // CHECK-NEXT:         }
 // CHECK-NEXT:       ]
 
-// This overlay is used just to force the filesystem to be case sensitive to
-// simulate case insensitive access via diffierent capitalizations.
 //--- overlay.json.template
-
 {
   "version": 0,
   "case-sensitive": true,
@@ -84,12 +85,12 @@
   {
      "contents": [
      {
-        "external-contents": "DIR/m.m",
+        "external-contents": "DIR/foo/m.m",
         "name": "module.modulemap",
         "type": "file"
      },
      {
-        "external-contents": "DIR/Mod.h",
+        "external-contents": "DIR/foo/Mod.h",
         "name": "Mod.h",
         "type": "file"
      }],
