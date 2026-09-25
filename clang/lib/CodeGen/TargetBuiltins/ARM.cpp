19d72fd6deab2ea3addd8b945807abd02b1bbb70
Kerry McLaughlin <kerry.mclaughlin@arm.com>
2026-09-25 21:03:59 +0100

    [AArch64] Rename memory hints to avoid clash with arm_acle.h macros (#226432)
    Renames the hints in AArch64MemoryHints.h, as HINT_STSHH_KEEP and
    HINT_STSHH_STRM are already defined in arm_acle.h.

--- clang/lib/CodeGen/TargetBuiltins/ARM.cpp
+++ clang/lib/CodeGen/TargetBuiltins/ARM.cpp
@@ -2112,7 +2112,7 @@ static Value *EmitAtomicStoreWithHintBuiltin(CodeGenFunction &CGF,
   unsigned HintArg = Result.Val.getInt().getExtValue();
 
   // Attach the hint if valid
-  if (toAArch64MemoryHint(HintArg) != AArch64MemoryHint::HINT_NONE) {
+  if (toAArch64MemoryHint(HintArg) != AArch64MemoryHint::NONE) {
     LLVMContext &Ctx = CGM.getLLVMContext();
     MDNode *MemHint = MDNode::get(
         Ctx, {MDString::get(Ctx, "aarch64.mem_hint"),
