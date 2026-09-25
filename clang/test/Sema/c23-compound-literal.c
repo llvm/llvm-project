24aa348132ef960fb63750b44b37b68a9e2112c0
Oleksandr Tarasiuk <oleksandr.tarasiuk@outlook.com>
2026-09-25 23:43:53 +0300

    rename tests. add additional test

--- clang/test/Sema/c23-compound-literal.c
+++ clang/test/Sema/c23-compound-literal.c
@@ -4,9 +4,9 @@
 
 #define M static
 struct S { int a; char b; };
-int f1(void);
+int test1(void);
 
-void test1(void) {
+void test2(void) {
   (void)(constexpr int){1};
   (void)&(static int){42};
   (void)(register int){0};
@@ -17,7 +17,7 @@ void test1(void) {
   (void)(register struct S){3, 'c'};
 }
 
-void test2(void) {
+void test3(void) {
   (void)(static constexpr int){1};
   (void)(constexpr static int){2};
   (void)(static thread_local int){3};
@@ -26,14 +26,14 @@ void test2(void) {
   (void)(constexpr static thread_local int){6}; // expected-error {{cannot combine with previous '_Thread_local' declaration specifier}}
 }
 
-void test3(void) {
+void test4(void) {
   (void)(static static int){1};             // expected-warning {{duplicate 'static' declaration specifier}}
   (void)(constexpr constexpr int){2};       // expected-warning {{duplicate 'constexpr' declaration specifier}}
   (void)(register register int){3};         // expected-warning {{duplicate 'register' declaration specifier}}
   (void)(thread_local thread_local int){4}; // expected-warning {{duplicate '_Thread_local' declaration specifier}} expected-error {{compound literal with 'thread_local' storage duration at block scope must also specify 'static'}}
 }
 
-void test4(void) {
+void test5(void) {
   (void)(register static int){1};       // expected-error {{cannot combine with previous 'register' declaration specifier}}
   (void)(register thread_local int){2}; // expected-error {{cannot combine with previous 'register' declaration specifier}}
   (void)(register constexpr int){3};
@@ -41,13 +41,13 @@ void test4(void) {
   (void)(register _Atomic int){5};
 }
 
-void test5(void) {
+void test6(void) {
   (void)&(thread_local int){1}; // expected-error {{compound literal with 'thread_local' storage duration at block scope must also specify 'static'}}
 }
 
 int *a1 = &(register int){42}; // expected-error {{file scope compound literal specifies 'register'}}
 
-void test6(void) {
+void test7(void) {
   (void)(constexpr volatile int){1}; // expected-error {{constexpr variable cannot have type 'const volatile int'}}
   (void)(constexpr _Atomic int){1};  // expected-error {{constexpr variable cannot have type 'const _Atomic(int)'}}
 
@@ -55,10 +55,10 @@ void test6(void) {
   (void)(constexpr int[c]){0}; // expected-error {{constexpr variable cannot have type 'const int[c]'}}
 }
 
-void test7(void) {
-  (void)(constexpr int){f1()};          // expected-error {{initializer of compound literal must be a constant expression}}
-  (void)(static int){f1()};             // expected-error {{initializer element is not a compile-time constant}}
-  (void)(register constexpr int){f1()}; // expected-error {{initializer of compound literal must be a constant expression}}
+void test8(void) {
+  (void)(constexpr int){test1()};          // expected-error {{initializer of compound literal must be a constant expression}}
+  (void)(static int){test1()};             // expected-error {{initializer element is not a compile-time constant}}
+  (void)(register constexpr int){test1()}; // expected-error {{initializer of compound literal must be a constant expression}}
   (void)(constexpr int){1 / 0};
   // expected-error@-1 {{initializer of compound literal must be a constant expression}}
   // expected-note@-2 {{division by zero}}
@@ -68,7 +68,7 @@ void test7(void) {
 const int a2 = 1;
 const double a3 = 1.0;
 
-void test8(void) {
+void test9(void) {
   (void)(constexpr int){a2};    // expected-error {{initializer of compound literal must be a constant expression}}
   (void)(constexpr double){a3}; // expected-error {{initializer of compound literal must be a constant expression}}
 
@@ -85,7 +85,7 @@ void test8(void) {
   (void)(constexpr int){(static int){1}};
 }
 
-void test9(void) {
+void test10(void) {
   int *a = &(register int){1};   // expected-error {{address of register compound literal requested}}
   int *b = (register int[1]){1};
   struct S2 { int a; };
@@ -107,11 +107,11 @@ struct S4 {
   int a[1];
 };
 
-int *test10(void) {
+int *test11(void) {
   return &((register struct S3){a5}.a[0]);
 }
 
-int *test11(void) {
+int *test12(void) {
   return &(register struct S4){{1}}.a[0]; // expected-error {{address of register compound literal requested}}
 }
 
@@ -138,7 +138,7 @@ enum {
   // expected-note@-2 {{read of member 'b' of union with active member 'a' is not allowed in a constant expression}}
 };
 
-void test12(void) {
+void test13(void) {
   static int a = (constexpr int){8};
   int b[(constexpr int){3}];
   switch ((constexpr int){1}) {
@@ -147,7 +147,7 @@ void test12(void) {
   }
 }
 
-void test13(void) {
+void test14(void) {
   (constexpr int){1} = 2;             // expected-error {{read-only variable is not assignable}}
   (constexpr int[1]){1}[0] = 2;       // expected-error {{read-only variable is not assignable}}
   (constexpr struct S){1, 'a'}.a = 2; // expected-error {{read-only variable is not assignable}}
@@ -157,7 +157,7 @@ int *a10 = &(static int){100};
 int *a11 = &(thread_local int){200}; // expected-error {{initializer element is not a compile-time constant}}
 const int *a12 = &(constexpr int){300};
 
-void test14(void) {
+void test15(void) {
   const int *a = (constexpr int[]){1, 2, 3};
   int *b = (static int[]){4, 5, 6};
 }
@@ -173,7 +173,7 @@ struct S7 {
   unsigned char a;
 };
 
-void test15(void) {
+void test16(void) {
   static int a;
   (void)(constexpr int *){&a};     // expected-error {{constexpr pointer initializer is not null}}
   (void)(constexpr struct S7){-1}; // expected-error {{constexpr initializer evaluates to -1 which is not exactly representable in type 'unsigned char'}}
@@ -187,123 +187,123 @@ static int *a22 = (constexpr int *){0};
 struct S8 {
   int *restrict a;
 };
-void test16(void) {
+void test17(void) {
   (void)(constexpr struct S8){0}; // expected-error {{constexpr variable cannot have type 'int *restrict'}}
 }
 
-void test17(void) {
+void test18(void) {
   (void)(constexpr char[]){"\xFF"};
   (void)(constexpr unsigned char[]){"\xFF"}; // expected-error {{constexpr initializer evaluates to -1 which is not exactly representable in type 'const unsigned char'}}
 }
 
-void test18(void) {
+void test19(void) {
   static const int *a = &(constexpr int){1}; // expected-error {{initializer element is not a compile-time constant}}
   static const int *b = &(static constexpr int){1};
 }
 
-void f2(int a[sizeof((static int){1})]);
-void f3(int a[sizeof((register int){1})]);
-void f4(int a[sizeof((constexpr int){1})]);
-void f5(int a[sizeof((thread_local static int){1})]);
-int f6(void);
+void test20(int a[sizeof((static int){1})]);
+void test21(int a[sizeof((register int){1})]);
+void test22(int a[sizeof((constexpr int){1})]);
+void test23(int a[sizeof((thread_local static int){1})]);
+int test24(void);
 
-void f7(int a[(constexpr int){f6()}]); // expected-error {{initializer of compound literal must be a constant expression}}
-void f8(int a[(static int){f6()}]);    // expected-error {{initializer element is not a compile-time constant}}
+void test25(int a[(constexpr int){test24()}]); // expected-error {{initializer of compound literal must be a constant expression}}
+void test26(int a[(static int){test24()}]);    // expected-error {{initializer element is not a compile-time constant}}
 
-typedef void F1(int a[(int){f6()}]);
+typedef void F1(int a[(int){test24()}]);
 typedef void F2(int a[(thread_local int){1}]); // expected-error {{compound literal with 'thread_local' storage duration at block scope must also specify 'static'}}
-void (*a23)(int a[(register int){f6()}]);
+void (*a23)(int a[(register int){test24()}]);
 
 typedef int T __attribute__((address_space(1)));
-void f9(int a[sizeof((T){0})]);  // expected-error {{compound literal in function scope may not be qualified with an address space}}
-int f10(int a[sizeof((T){0})]) { // expected-error {{compound literal in function scope may not be qualified with an address space}}
+void test27(int a[sizeof((T){0})]);  // expected-error {{compound literal in function scope may not be qualified with an address space}}
+int test28(int a[sizeof((T){0})]) { // expected-error {{compound literal in function scope may not be qualified with an address space}}
   return a[0];
 }
 
-void test19(void) {
+void test29(void) {
   (static T){0}; // expected-error {{compound literal in function scope may not be qualified with an address space}}
 }
 
-int f11(
+int test30(
     typeof(sizeof((register T){0})) a(void)) { // expected-error {{compound literal in function scope may not be qualified with an address space}}
   return a();
 }
 
-inline int f12(void) {    // expected-note {{use 'static' to give inline function 'f12' internal linkage}}
+inline int test31(void) {    // expected-note {{use 'static' to give inline function 'test31' internal linkage}}
   return (static int){1}; // expected-warning {{non-constant static local variable in inline function may be different in different files}}
 }
 
-inline int f13(int a[(static int){1}]) { // expected-warning {{non-constant static local variable in inline function may be different in different files}} \
-                                         // expected-note {{use 'static' to give inline function 'f13' internal linkage}}
+inline int test32(int a[(static int){1}]) { // expected-warning {{non-constant static local variable in inline function may be different in different files}} \
+                                         // expected-note {{use 'static' to give inline function 'test32' internal linkage}}
   return a[0];
 }
 
-inline int f14(int a[(static int){1}]);
+inline int test33(int a[(static int){1}]);
 
-inline int f15(int a[(static const int){1}]) {
+inline int test34(int a[(static const int){1}]) {
   return a[0];
 }
 
-extern inline int f16(int a[(static int){1}]) {
+extern inline int test35(int a[(static int){1}]) {
   return a[0];
 }
 
-static inline int f17(int a[(static int){1}]) {
+static inline int test36(int a[(static int){1}]) {
   return a[0];
 }
 
-inline int f18(int b(int a[sizeof((static int){1})])) {
+inline int test37(int b(int a[sizeof((static int){1})])) {
   return 0;
 }
 
-inline int f19(int (*a(void))[sizeof((static int){1})]) { // expected-warning {{non-constant static local variable in inline function may be different in different files}}
-                                                          // expected-note@-1 {{use 'static' to give inline function 'f19' internal linkage}}
+inline int test38(int (*a(void))[sizeof((static int){1})]) { // expected-warning {{non-constant static local variable in inline function may be different in different files}}
+                                                          // expected-note@-1 {{use 'static' to give inline function 'test38' internal linkage}}
   return 0;
 }
 
-inline typeof((static int){1}) f20(void) {
+inline typeof((static int){1}) test39(void) {
   return 0;
 }
-typeof((register int){1}) f21(void); // expected-error {{file scope compound literal specifies 'register'}}
+typeof((register int){1}) test40(void); // expected-error {{file scope compound literal specifies 'register'}}
 
-inline int f22(int a) __attribute__((enable_if((static int){1}, "enabled"))) { // expected-warning {{non-constant static local variable in inline function may be different in different files}} \
-                                                                               // expected-note {{use 'static' to give inline function 'f22' internal linkage}}
+inline int test41(int a) __attribute__((enable_if((static int){1}, "enabled"))) { // expected-warning {{non-constant static local variable in inline function may be different in different files}} \
+                                                                               // expected-note {{use 'static' to give inline function 'test41' internal linkage}}
   return a;
 }
 
-int f23(int a) __attribute__((enable_if(sizeof((register T){0}), "enabled"))) { // expected-error {{compound literal in function scope may not be qualified with an address space}}
+int test42(int a) __attribute__((enable_if(sizeof((register T){0}), "enabled"))) { // expected-error {{compound literal in function scope may not be qualified with an address space}}
   return a;
 }
 
-int f24(int a) __attribute__((enable_if( sizeof((register T){0}), "enabled"))); // expected-error {{compound literal in function scope may not be qualified with an address space}}
-int f24(int a) {
+int test43(int a) __attribute__((enable_if( sizeof((register T){0}), "enabled"))); // expected-error {{compound literal in function scope may not be qualified with an address space}}
+int test43(int a) {
   return a;
 }
 
 int a24;
 int a25 = sizeof((thread_local static int (*)[a24]){0}); // expected-error {{variably modified type declaration not allowed at file scope}}
 
-int *f25(void) {
+int *test44(void) {
   return &(static int){1};
 }
 
-int *f26(void) {
+int *test45(void) {
   return &(thread_local static int){1};
 }
 
-int *f27(void) {
+int *test46(void) {
   return &(int){1}; // expected-warning {{address of stack memory associated with compound literal '{1}' returned}}
 }
 
-const int *f28(void) {
+const int *test47(void) {
   return &(constexpr int){1}; // expected-warning {{address of stack memory associated with compound literal '{1}' returned}}
 }
 
-inline int f29(void) {                 // expected-note {{use 'static' to give inline function 'f29' internal linkage}}
+inline int test48(void) {                 // expected-note {{use 'static' to give inline function 'test48' internal linkage}}
   return (thread_local static int){1}; // expected-warning {{non-constant static local variable in inline function may be different in different files}}
 }
 
-void test20(void) {
+void test49(void) {
   (void)(M const int){1};
   (void)(constexpr unsigned long){2};
   (void)sizeof (static int){1};
@@ -311,12 +311,12 @@ void test20(void) {
   (void)sizeof (constexpr int){1};
 }
 
-void test21(void) {
+void test50(void) {
   (void)(const static int){1};   // expected-error {{type name does not allow storage class to be specified}}
   (void)(const int register){1}; // expected-error {{type name does not allow storage class to be specified}}
 }
 
-void test22(void) {
+void test51(void) {
   (void)(auto int){1};        // expected-error {{expected expression}}
   (void)(extern int){2};      // expected-error {{expected expression}}
   (void)(typedef int){3};     // expected-error {{expected expression}}
@@ -324,7 +324,7 @@ void test22(void) {
   (void)(auto){1};            // expected-error {{expected expression}}
 }
 
-void test23(void) {
+void test52(void) {
   (void)(static int constexpr){1};     // expected-error {{type name does not allow constexpr specifier to be specified}}
   (void)(static int thread_local){1};  // expected-error {{type name does not allow storage class to be specified}}
   (void)(static int _Thread_local){1}; // expected-error {{type name does not allow storage class to be specified}}
@@ -332,7 +332,7 @@ void test23(void) {
   (void)(int static){1};               // expected-error {{type name does not allow storage class to be specified}}
 }
 
-void test24(void) {
+void test53(void) {
   (void)(static int)1;              // expected-error {{type name does not allow storage class to be specified}}
   (void)sizeof(register int);       // expected-error {{type name does not allow storage class to be specified}}
   (void)_Alignof(thread_local int); // expected-error {{type name does not allow storage class to be specified}}
@@ -342,6 +342,10 @@ void test24(void) {
 int a26 = (static)3; // expected-error {{expected expression}}
 
 typedef int A __attribute__((aligned(64)));
-A *f30(void) {
+A *test54(void) {
   return &(thread_local static A){1}; // ps4-error {{alignment (64) of thread-local compound literal is greater than the maximum supported alignment (32) for thread-local storage on this target}}
+}
+
+int *test55(void) {
+  return (static int[]){1, 2};
 }
