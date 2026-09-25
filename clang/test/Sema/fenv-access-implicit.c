c603ed033106e61ab8f567297e7c4b37b09348b9
Marcos Ramirez Joos <mramirezjoos.oss@proton.me>
2026-09-25 20:40:47 +0200

    Enable recognition of funcions from the fenv.h header without adding the built-in types & fix concerning regression tests.

--- clang/test/Sema/fenv-access-implicit.c
+++ clang/test/Sema/fenv-access-implicit.c
@@ -12,24 +12,18 @@ void test_fenv_access_undeclared(void) {
   #pragma STDC FENV_ACCESS ON
   feclearexcept(FE_INVALID); // expected-note {{include the header <fenv.h> or explicitly provide a declaration for 'feclearexcept'}} \
    expected-error {{call to undeclared library function 'feclearexcept' with type 'int (int)'; ISO C99 and later do not support implicit function declarations}}
-  fegetexceptflag(flagp, FE_INVALID); // expected-note {{include the header <fenv.h> or explicitly provide a declaration for 'fegetexceptflag'}} \
-   expected-error {{call to undeclared library function 'fegetexceptflag' with type 'int (fexcept_t *, int)' (aka 'int (unsigned short *, int)'); ISO C99 and later do not support implicit function declarations}}
+  fegetexceptflag(flagp, FE_INVALID); // expected-error {{call to undeclared function 'fegetexceptflag'; ISO C99 and later do not support implicit function declarations}}
   feraiseexcept(FE_INVALID); // expected-note {{include the header <fenv.h> or explicitly provide a declaration for 'feraiseexcept'}} \
    expected-error {{call to undeclared library function 'feraiseexcept' with type 'int (int)'; ISO C99 and later do not support implicit function declarations}}
-  fesetexceptflag(flagp, FE_INVALID); // expected-note {{include the header <fenv.h> or explicitly provide a declaration for 'fesetexceptflag'}} \
-   expected-error {{call to undeclared library function 'fesetexceptflag' with type 'int (const fexcept_t *, int)' (aka 'int (const unsigned short *, int)'); ISO C99 and later do not support implicit function declarations}}
+  fesetexceptflag(flagp, FE_INVALID); // expected-error {{call to undeclared function 'fesetexceptflag'; ISO C99 and later do not support implicit function declarations}}
   fetestexcept(FE_INVALID); // expected-note {{include the header <fenv.h> or explicitly provide a declaration for 'fetestexcept'}} \
    expected-error {{call to undeclared library function 'fetestexcept' with type 'int (int)'; ISO C99 and later do not support implicit function declarations}}
   fegetround(); // expected-note {{include the header <fenv.h> or explicitly provide a declaration for 'fegetround'}} \
    expected-error {{call to undeclared library function 'fegetround' with type 'int (void)'; ISO C99 and later do not support implicit function declarations}}
   fesetround(0); // expected-note {{include the header <fenv.h> or explicitly provide a declaration for 'fesetround'}} \
    expected-error {{call to undeclared library function 'fesetround' with type 'int (int)'; ISO C99 and later do not support implicit function declarations}}
-  fegetenv(envp); // expected-note {{include the header <fenv.h> or explicitly provide a declaration for 'fegetenv'}} \
-   expected-error {{call to undeclared library function 'fegetenv' with type 'int (fenv_t *)'; ISO C99 and later do not support implicit function declarations}}
-  feholdexcept(envp); // expected-note {{include the header <fenv.h> or explicitly provide a declaration for 'feholdexcept'}} \
-   expected-error {{call to undeclared library function 'feholdexcept' with type 'int (fenv_t *)'; ISO C99 and later do not support implicit function declarations}}
-  fesetenv(envp); // expected-note {{include the header <fenv.h> or explicitly provide a declaration for 'fesetenv'}} \
-   expected-error {{call to undeclared library function 'fesetenv' with type 'int (const fenv_t *)'; ISO C99 and later do not support implicit function declarations}}
-  feupdateenv(envp); // expected-note {{include the header <fenv.h> or explicitly provide a declaration for 'feupdateenv'}} \
-   expected-error {{call to undeclared library function 'feupdateenv' with type 'int (const fenv_t *)'; ISO C99 and later do not support implicit function declarations}}
+  fegetenv(envp); // expected-error {{call to undeclared function 'fegetenv'; ISO C99 and later do not support implicit function declarations}}
+  feholdexcept(envp); // expected-error {{call to undeclared function 'feholdexcept'; ISO C99 and later do not support implicit function declarations}}
+  fesetenv(envp); // expected-error {{call to undeclared function 'fesetenv'; ISO C99 and later do not support implicit function declarations}}
+  feupdateenv(envp); // expected-error {{call to undeclared function 'feupdateenv'; ISO C99 and later do not support implicit function declarations}}
 }
